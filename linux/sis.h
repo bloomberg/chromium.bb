334e134918ee2ece3e845cd55c49c9709b67ee6c
David Dawes <dawes@xfree86.org>
2001-06-14 21:53:06 +0000

    Import of XFree86 4.1.0

--- linux/sis.h
+++ linux/sis.h
@@ -24,7 +24,7 @@
  * DEALINGS IN THE SOFTWARE.
  * 
  */
-/* $XFree86$ */
+/* $XFree86: xc/programs/Xserver/hw/xfree86/os-support/linux/drm/kernel/sis.h,v 1.1.2.1 2001/05/22 21:54:54 dawes Exp $ */
 
 #ifndef __SIS_H__
 #define __SIS_H__
