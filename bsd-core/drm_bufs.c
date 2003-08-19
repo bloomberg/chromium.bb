963ad33cb6b85189f3385bcba46905b6d4d329db
Eric Anholt <anholt@freebsd.org>
2003-08-19 02:22:57 +0000

    Fix the debug build.

--- bsd-core/drm_bufs.c
+++ bsd-core/drm_bufs.c
@@ -546,7 +546,7 @@ static int DRM(addbufs_pci)(drm_device_t *dev, drm_buf_desc_t *request)
 		for ( i = 0 ; i < (1 << page_order) ; i++ ) {
 			DRM_DEBUG( "page %d @ 0x%08lx\n",
 				   dma->page_count + page_count,
-				   page + PAGE_SIZE * i );
+				   (long)vaddr + PAGE_SIZE * i );
 			temp_pagelist[dma->page_count + page_count++] = 
 			    vaddr + PAGE_SIZE * i;
 		}
