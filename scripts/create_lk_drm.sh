dfc71cbd42fe4b4b445bd16087c05e525c38f509
Dave Airlie <airlied@linux.ie>
2004-03-23 11:06:47 +0000

    initial shell script to create linux kernel drm from the DRI one

---
+++ scripts/create_lk_drm.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+# script to create a Linux Kernel tree from the DRM tree for diffing etc..
+#
+# Original author - Dave Airlie (C) 2004 - airlied@linux.ie
+#
+
+if [ $# -lt 2 ] ;then
+	echo usage: $0 output_dir [2.4\|2.6]
+	exit 1
+fi
+
+OUTDIR=$1/drivers/char/drm/
+VERS=$2
+
+echo "Copying kernel independent files"
+mkdir -p $OUTDIR
+
+cp shared/*.[ch] $OUTDIR
+cp linux/*.[ch] $OUTDIR
+cp linux/Makefile.linux $OUTDIR/Makefile
+
+if [ $VERS = 2.4 ] ;then
+	echo "Copying 2.4 Kernel files"
+	cp linux/Config.in $OUTDIR/
+elif [ $VERS = 2.6 ] ;then
+	echo "Copying 2.6 Kernel files"
+	cp linux/Kconfig $OUTDIR/
+fi
+
