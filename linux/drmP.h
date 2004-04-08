13724f032e127fbaf6d47bb2b6ba8933df23ffe8
Dave Airlie <airlied@linux.ie>
2004-04-08 13:11:04 +0000

    2.6 sysfs patches + stubs in drmP.h for 2.4 compatibility

--- linux/drmP.h
+++ linux/drmP.h
@@ -243,6 +243,18 @@ static inline unsigned iminor(struct inode *inode)
 
 #define old_encode_dev(x) (x)
 
+struct class_simple;
+struct device;
+
+static inline void *class_simple_device_add(struct class_simple *cs, dev_t dev, struct device *device, const char *fmt, ...)
+{ return NULL;}
+
+static inline void class_simple_device_remove(dev_t dev){};
+
+static inline void class_simple_destroy(struct class_simple *cs){};
+
+static inline struct class_simple *class_simple_create(struct module *owner, char *name) { return NULL; }
+
 #endif
 
 #ifndef REMAP_PAGE_RANGE_5_ARGS
