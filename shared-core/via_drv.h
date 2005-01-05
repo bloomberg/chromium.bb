4818014ab10e5ab27fe7c87f7936a932526612a6
Thomas Hellstrom <unichrome@shipmail.org>
2005-01-05 17:46:34 +0000

    Bumped driver date and minor version.

--- shared-core/via_drv.h
+++ shared-core/via_drv.h
@@ -28,11 +28,11 @@
 
 #define DRIVER_NAME		"via"
 #define DRIVER_DESC		"VIA Unichrome"
-#define DRIVER_DATE		"20041231"
+#define DRIVER_DATE		"20050105"
 
 #define DRIVER_MAJOR		2
-#define DRIVER_MINOR		3
-#define DRIVER_PATCHLEVEL	4
+#define DRIVER_MINOR		4
+#define DRIVER_PATCHLEVEL	0
 
 typedef struct drm_via_ring_buffer {
 	drm_map_t map;
