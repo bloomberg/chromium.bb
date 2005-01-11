ee576367632b582e29f2b3d42f2eb9250cfdf219
Keith Whitwell <keith@tungstengraphics.com>
2005-01-11 10:59:01 +0000

    Import Thomas' changes upto 2.4.3 to shared. Small tweak to install target.

--- shared/via_drv.h
+++ shared/via_drv.h
@@ -25,6 +25,7 @@
 #define _VIA_DRV_H_
 
 #include "via_drm.h"
+#include "via_verifier.h"
 
 typedef struct drm_via_ring_buffer {
 	drm_map_t map;
@@ -49,6 +50,7 @@ typedef struct drm_via_private {
         struct timeval last_vblank;
         int last_vblank_valid;
         unsigned usec_per_vblank;
+	drm_via_state_t hc_state;
 } drm_via_private_t;
 
 /* VIA MMIO register access */
@@ -77,8 +79,6 @@ extern int via_dma_cleanup(drm_device_t * dev);
 extern int via_wait_idle(drm_via_private_t * dev_priv);
 extern int via_driver_dma_quiescent(drm_device_t * dev);
 extern void via_init_command_verifier( void );
-extern int via_verify_command_stream(const uint32_t * buf, unsigned int size,
-				     drm_device_t *dev);
 extern int via_fb_free(drm_via_mem_t * mem);
 extern int via_fb_alloc(drm_via_mem_t * mem);
 
