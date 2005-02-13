650c0fe391b09479afd787e0b1db0a3e22cc4fcd
Thomas Hellstrom <unichrome@shipmail.org>
2005-02-13 23:03:48 +0000

    Fixed Futex release bug. Bumped driver date and patchlevel.

--- shared-core/via_mm.c
+++ shared-core/via_mm.c
@@ -180,12 +180,12 @@ int via_final_context(struct drm_device *dev, int context)
 
 	for (i=0; i < VIA_NR_XVMC_LOCKS; ++i) {
 		lock = XVMCLOCKPTR(sAPriv, i);
-		if ( (_DRM_LOCKING_CONTEXT( *lock ) == i) && 
+		if ( (_DRM_LOCKING_CONTEXT( *lock ) == context) && 
 		     (_DRM_LOCK_IS_HELD( *lock ))) {
 			if ( *lock & _DRM_LOCK_CONT) {
 				DRM_WAKEUP( &(dev_priv->decoder_queue[i]));
 			}
-			*lock &= ~( _DRM_LOCK_HELD | _DRM_LOCK_CONT );
+			*lock = 0;
 		}
 	}
 			
