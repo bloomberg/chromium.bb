75cb43ccd73dd2c8c196d209862b8a3ea4e433cb
Ian Romanick <idr@us.ibm.com>
2005-05-21 02:31:08 +0000

    Forgot to bump the patchlevel and driver date on last commit.

--- shared-core/mga_drv.h
+++ shared-core/mga_drv.h
@@ -38,11 +38,11 @@
 
 #define DRIVER_NAME		"mga"
 #define DRIVER_DESC		"Matrox G200/G400"
-#define DRIVER_DATE		"20051013"
+#define DRIVER_DATE		"20050520"
 
 #define DRIVER_MAJOR		3
 #define DRIVER_MINOR		1
-#define DRIVER_PATCHLEVEL	1
+#define DRIVER_PATCHLEVEL	2
 
 typedef struct drm_mga_primary_buffer {
 	u8 *start;
