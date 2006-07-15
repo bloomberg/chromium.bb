d1b31a228b72b8dd8e588f0a0cc8eeabc3845f70
George Sapountzis <gsap7@yahoo.gr>
2006-07-16 01:02:06 +0300

    Bug 6209: [mach64] AGP DMA buffers not mapped correctly.
    Map the DMA buffers from the same linear address as the vertex bufs. If
    dev->agp_buffer_token is not set, mach64 drm maps the DMA buffers from
    linear address 0x0.

--- shared-core/mach64_dma.c
+++ shared-core/mach64_dma.c
@@ -834,6 +834,7 @@ static int mach64_do_dma_init(drm_device_t * dev, drm_mach64_init_t * init)
 			mach64_do_cleanup_dma(dev);
 			return DRM_ERR(ENOMEM);
 		}
+		dev->agp_buffer_token = init->buffers_offset;
 		dev->agp_buffer_map =
 		    drm_core_findmap(dev, init->buffers_offset);
 		if (!dev->agp_buffer_map) {
