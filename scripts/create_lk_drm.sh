df7378d7fa9d048aea6911a3fe4973050003401b
Dave Airlie <airlied@linux.ie>
2006-08-10 14:31:56 +1000

    clean up script to create an lk drm directory

--- scripts/create_lk_drm.sh
+++ scripts/create_lk_drm.sh
@@ -4,32 +4,26 @@
 # Original author - Dave Airlie (C) 2004 - airlied@linux.ie
 #
 
-if [ $# -lt 2 ] ;then
-	echo usage: $0 output_dir [2.4\|2.6]
+if [ $# -lt 1 ] ;then
+	echo usage: $0 output_dir
 	exit 1
 fi
 
-if [ ! -d shared -o ! -d linux ]  ;then
+if [ ! -d shared-core -o ! -d linux-core ]  ;then
 	echo not in DRM toplevel
 	exit 1
 fi
 
 OUTDIR=$1/drivers/char/drm/
-VERS=$2
 
 echo "Copying kernel independent files"
 mkdir -p $OUTDIR
 
-( cd linux/ ; make drm_pciids.h )
+( cd linux-core/ ; make drm_pciids.h )
 cp shared-core/*.[ch] $OUTDIR
 cp linux-core/*.[ch] $OUTDIR
 cp linux-core/Makefile.kernel $OUTDIR/Makefile
 
-if [ $VERS = 2.4 ] ;then
-	echo "Copying 2.4 Kernel files"
-	cp linux/Config.in $OUTDIR/
-elif [ $VERS = 2.6 ] ;then
-	echo "Copying 2.6 Kernel files"
-	cp linux-core/Kconfig $OUTDIR/
-fi
+echo "Copying 2.6 Kernel files"
+cp linux-core/Kconfig $OUTDIR/
 
