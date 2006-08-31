84b38b63f05e04ade8b1ddfb770047fd86de0d64
Michel Dänzer <michel@tungstengraphics.com>
2006-08-31 18:32:08 +0200

    Add definition of DRM_VBLANK_SECONDARY.

--- libdrm/xf86drm.h
+++ libdrm/xf86drm.h
@@ -252,6 +252,7 @@ typedef struct _drmTextureRegion {
 typedef enum {
     DRM_VBLANK_ABSOLUTE = 0x0,	/**< Wait for specific vblank sequence number */
     DRM_VBLANK_RELATIVE = 0x1,	/**< Wait for given number of vblanks */
+    DRM_VBLANK_SECONDARY = 0x20000000,	/**< Secondary display controller */
     DRM_VBLANK_SIGNAL   = 0x40000000	/* Send signal instead of blocking */
 } drmVBlankSeqType;
 
