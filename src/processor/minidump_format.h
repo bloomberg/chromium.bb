7daf246e4baf0837e25429668cc23e92b6afe3b3
mmentovai <mmentovai@4c0a9323-5329-0410-9bdc-e9ce6186880e>
2006-09-20 21:16:16 +0000

    Relicense to BSD (#29).  r=bryner
    http://groups.google.com/group/airbag-dev/browse_thread/thread/5f19f13fc172c4e0
    
    
    git-svn-id: http://google-breakpad.googlecode.com/svn/trunk@31 4c0a9323-5329-0410-9bdc-e9ce6186880e

--- src/processor/minidump_format.h
+++ src/processor/minidump_format.h
@@ -1,16 +1,31 @@
-/* Copyright (C) 2006 Google Inc.
+/* Copyright (c) 2006, Google Inc.
+ * All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. */
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
 /* minidump_format.h: A cross-platform reimplementation of minidump-related
  * portions of DbgHelp.h from the Windows Platform SDK.
@@ -424,7 +439,7 @@ typedef enum {
   MINIDUMP_FILTER_MEMORY                     = 0x00000008,
   MINIDUMP_SCAN_MEMORY                       = 0x00000010,
   MINIDUMP_WITH_UNLOADED_MODULES             = 0x00000020,
-  MINIDUMP_WITH_INDIRECTLY_REFERENCED_MEMORY = 0X00000040,
+  MINIDUMP_WITH_INDIRECTLY_REFERENCED_MEMORY = 0x00000040,
   MINIDUMP_FILTER_MODULE_PATHS               = 0x00000080,
   MINIDUMP_WITH_PROCESS_THREAD_DATA          = 0x00000100,
   MINIDUMP_WITH_PRIVATE_READ_WRITE_MEMORY    = 0x00000200,
