1a4b9294a29379ea6e9fd6fb315317f391232d4b
Jesse Barnes <jbarnes@jbarnes-mobile.amr.corp.intel.com>
2007-06-12 16:29:09 -0700

    Remove unnecessary (and uncommented!) read barrier from the interrupt path.  It doesn't appear to serve any useful purpose.

--- shared-core/i915_irq.c
+++ shared-core/i915_irq.c
@@ -323,7 +323,6 @@ irqreturn_t i915_driver_irq_handler(DRM_IRQ_ARGS)
 
 	I915_WRITE16(I915REG_INT_IDENTITY_R, temp);
 	(void) I915_READ16(I915REG_INT_IDENTITY_R);
-	DRM_READMEMORYBARRIER();
 
 	dev_priv->sarea_priv->last_dispatch = READ_BREADCRUMB(dev_priv);
 
