bloomberg/chromium.bb

commit 3f5ca12f9e2af4a6fc93824efcd52c69f3a85fba

WebCore:
        Reviewed by Adam Roben.

        - fix svg/W3C-SVG-1.1/fonts-glyph-04-t.svg failure on Windows

        The results differed between Mac OS X and Windows because the code was
        using a 0-size font, which Mac OS X changes to 12pt.

        * svg/SVGFont.cpp:
        (WebCore::floatWidthMissingGlyphCallback): Use the font description of
        the primary font to initialize the fallback font.
        (WebCore::drawTextMissingGlyphCallback): Ditto.

LayoutTests:

        Reviewed by Adam Roben.

        - updated results for a test that was failing on Windows

        * platform/mac/svg/W3C-SVG-1.1/fonts-glyph-04-t-expected.txt:



git-svn-id: svn://svn.chromium.org/blink/trunk@29838 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 0abcb2824f0f3a5f60f7ff6684b3d7d62ef3acfb

third_party/WebKit/WebCore/svg/SVGFont.cpp
@@ -334,7 +334,9 @@ bool floatWidthUsingSVGFontCallback(const SVGGlyphIdentifier& identifier, SVGTex
 void floatWidthMissingGlyphCallback(const TextRun& run, SVGTextRunWalkerMeasuredLengthData& data)
 {
     // Handle system font fallback
-    Font font(FontDescription(), 0, 0); // spacing handled by SVG text code.
+    FontDescription fontDescription(data.font->fontDescription());
+    fontDescription.setFamily(*new FontFamily());
+    Font font(fontDescription, 0, 0); // spacing handled by SVG text code.
     font.update(data.font->fontSelector());
 
     data.length += font.floatWidth(run);
@@ -444,7 +446,9 @@ bool drawTextUsingSVGFontCallback(const SVGGlyphIdentifier& identifier, SVGTextR
 void drawTextMissingGlyphCallback(const TextRun& run, SVGTextRunWalkerDrawTextData& data)
 {
     // Handle system font fallback
-    Font font(FontDescription(), 0, 0); // spacing handled by SVG text code.
+    FontDescription fontDescription(data.context->font().fontDescription());
+    fontDescription.setFamily(*new FontFamily());
+    Font font(fontDescription, 0, 0); // spacing handled by SVG text code.
     font.update(data.context->font().fontSelector());
 
     font.drawText(data.context, run, data.currentPoint);

third_party/WebKit/LayoutTests/platform/mac/svg/W3C-SVG-1.1/fonts-glyph-04-t-expected.txt
@@ -2,13 +2,13 @@ layer at (0,0) size 480x360
   RenderView at (0,0) size 480x360
 layer at (0,0) size 480x360
   RenderSVGRoot {svg} at (0.50,0.50) size 479x359
-    RenderSVGContainer {g} at (100,60) size 53x150
+    RenderSVGContainer {g} at (100,60) size 64x150
       RenderSVGHiddenContainer {defs} at (0,0) size 0x0
-      RenderSVGText {text} at (100,100) size 53x50 contains 1 chunk(s)
-        RenderSVGInlineText {#text} at (0,-40) size 53x50
+      RenderSVGText {text} at (100,100) size 64x50 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,-40) size 64x50
           chunk 1 text run 1 at (100.00,100.00) startOffset 0 endOffset 3 width 25.00: "ffl"
-      RenderSVGText {text} at (100,200) size 53x50 contains 1 chunk(s)
-        RenderSVGInlineText {#text} at (0,-40) size 53x50
+      RenderSVGText {text} at (100,200) size 64x50 contains 1 chunk(s)
+        RenderSVGInlineText {#text} at (0,-40) size 64x50
           chunk 1 text run 1 at (100.00,200.00) startOffset 0 endOffset 3 width 25.00: "ffl"
     RenderSVGText {text} at (10,340) size 284x46 contains 1 chunk(s)
       RenderSVGInlineText {#text} at (0,-36) size 284x46

commit 3adeb2a77e4e3d3ad616156ad67230caabc869da

Reviewed by Nikolas Zimmermann.
        - fix leaks introduced in r29838

        * svg/SVGFont.cpp:
        (WebCore::floatWidthMissingGlyphCallback):
        (WebCore::drawTextMissingGlyphCallback):



git-svn-id: svn://svn.chromium.org/blink/trunk@29840 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: a9547101b8a781e24eb91a07826e89bd76154cf0

third_party/WebKit/WebCore/svg/SVGFont.cpp
@@ -336,7 +336,7 @@ void floatWidthMissingGlyphCallback(const TextRun& run, SVGTextRunWalkerMeasured
 {
     // Handle system font fallback
     FontDescription fontDescription(data.font->fontDescription());
-    fontDescription.setFamily(*new FontFamily());
+    fontDescription.setFamily(FontFamily());
     Font font(fontDescription, 0, 0); // spacing handled by SVG text code.
     font.update(data.font->fontSelector());
 
@@ -450,7 +450,7 @@ void drawTextMissingGlyphCallback(const TextRun& run, SVGTextRunWalkerDrawTextDa
 {
     // Handle system font fallback
     FontDescription fontDescription(data.context->font().fontDescription());
-    fontDescription.setFamily(*new FontFamily());
+    fontDescription.setFamily(FontFamily());
     Font font(fontDescription, 0, 0); // spacing handled by SVG text code.
     font.update(data.context->font().fontSelector());
 