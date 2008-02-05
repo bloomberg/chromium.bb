ee15459483d50b2efe630051b45f36cfbb351683
Kristian Høgsberg <krh@redhat.com>
2008-02-05 12:27:48 -0500

    i915: Add I915_PARAM_CHIPSET_ID param to get chipset ID.

--- shared-core/i915_dma.c
+++ shared-core/i915_dma.c
@@ -1210,6 +1210,9 @@ static int i915_getparam(struct drm_device *dev, void *data,
 	case I915_PARAM_LAST_DISPATCH:
 		value = READ_BREADCRUMB(dev_priv);
 		break;
+	case I915_PARAM_CHIPSET_ID:
+		value = dev->pci_device;
+		break;
 	default:
 		DRM_ERROR("Unknown parameter %d\n", param->param);
 		return -EINVAL;
