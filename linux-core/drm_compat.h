709aa4629321533bfa30c72c4e33f229c895358a
Alan Hourihane <alanh@tungstengraphics.com>
2008-02-05 09:28:32 +0000

    Remove duplicate

--- linux-core/drm_compat.h
+++ linux-core/drm_compat.h
@@ -331,8 +331,4 @@ void idr_remove_all(struct idr *idp);
 void *idr_replace(struct idr *idp, void *ptr, int id);
 #endif
 
-#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,19))
-typedef _Bool                   bool;
-#endif
-
 #endif
