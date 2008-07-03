d5ca5c9cd379438ac303598677f3789adc3687b1
Maarten Maathuis <madman2003@gmail.com>
2008-07-03 08:07:35 +0200

    [drm-sysfs] connected is ambigious in the context of a connector, replace with enabled

--- linux-core/drm_sysfs.c
+++ linux-core/drm_sysfs.c
@@ -176,17 +176,16 @@ static ssize_t dpms_show(struct device *device,
 	return snprintf(buf, PAGE_SIZE, "%s", drm_get_dpms_name((int)dpms_status));
 }
 
-static ssize_t connected_show(struct device *device,
+static ssize_t enabled_show(struct device *device,
 			   struct device_attribute *attr,
 			   char *buf)
 {
 	struct drm_connector *connector = container_of(device, struct drm_connector, kdev);
-	struct drm_device *dev = connector->dev;
 
 	if (connector->encoder)
-		return snprintf(buf, PAGE_SIZE, "connected");
+		return snprintf(buf, PAGE_SIZE, "enabled");
 	else
-		return snprintf(buf, PAGE_SIZE, "disconnected");
+		return snprintf(buf, PAGE_SIZE, "disabled");
 }
 
 static ssize_t edid_show(struct kobject *kobj, struct bin_attribute *attr,
@@ -234,7 +233,7 @@ static ssize_t modes_show(struct device *device,
 
 static struct device_attribute connector_attrs[] = {
 	__ATTR_RO(status),
-	__ATTR_RO(connected),
+	__ATTR_RO(enabled),
 	__ATTR_RO(dpms),
 	__ATTR_RO(modes),
 };
