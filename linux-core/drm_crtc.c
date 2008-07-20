e51cd78cac24df15e32e23d8db73614524e6ff0d
Maarten Maathuis <madman2003@gmail.com>
2008-07-20 14:58:46 +0200

    modesetting-101: Only store property value when set_property was successful.

--- linux-core/drm_crtc.c
+++ linux-core/drm_crtc.c
@@ -2184,12 +2184,12 @@ int drm_mode_connector_property_set_ioctl(struct drm_device *dev,
 		}
 	}
 
-	/* store the property value */
-	drm_connector_property_set_value(connector, property, out_resp->value);
-
 	if (connector->funcs->set_property)
 		ret = connector->funcs->set_property(connector, property, out_resp->value);
 
+	/* store the property value if succesful */
+	if (!ret)
+		drm_connector_property_set_value(connector, property, out_resp->value);
 out:
 	mutex_unlock(&dev->mode_config.mutex);
 	return ret;
