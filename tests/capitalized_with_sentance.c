54c22172c3736362f169da01f63c834720018cdb
James Teh <jamie@jantrid.net>
2008-09-16 13:20:09 +0000

    * tests/inpos_compbrl.c: Update the test to expect the prefix cells to map to the first position of the input and the suffix cells to the last position instead of -1. This test now passes. 	* tests/capitalized_with_sentence.c: Fix incorrect expected result for the last cursor position. This test now passes.

--- tests/capitalized_with_sentance.c
+++ tests/capitalized_with_sentance.c
@@ -7,7 +7,7 @@ int
 main (int argc, char **argv)
 {
         const char *str1 = "This is a test ";
-        const int expected_pos1[]={0,1,2,3,2,3,4,5,6,7,8,9,10,11,9};
+        const int expected_pos1[]={0,1,2,3,2,3,4,5,6,7,8,9,10,11,11};
 
         return check_cursor_pos(str1, expected_pos1);        
 }
--- tests/inpos_compbrl.c
+++ tests/inpos_compbrl.c
@@ -8,6 +8,6 @@ main (int argc, char **argv)
 {
         const char *str1 = "user@example.com";
 		const int expected_inpos[] = 
-				{-1,-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,-1,-1};
+				{0,0,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,15,15};
         return check_outpos(str1, expected_inpos);        
 }
