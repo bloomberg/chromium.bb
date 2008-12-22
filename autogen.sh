a7b0db93f030a58bb6de3e41279b040d49fd7bd1
Christian Egli <christian.egli@alumni.ethz.ch>
2008-12-22 15:29:38 +0000

    Remove files generated by the autotools.

--- autogen.sh
+++ autogen.sh
@@ -12,6 +12,7 @@ find -type d -name autom4te.cache -print0 | xargs -0 rm -rf \;
 find -type f \( -name missing -o -name install-sh -o -name mkinstalldirs \
 	-o -name depcomp -o -name ltmain.sh -o -name configure \
 	-o -name config.sub -o -name config.guess -o -name config.h.in \
+        -o -name mdate-sh -o -name texinfo.tex \
 	-o -name Makefile.in -o -name aclocal.m4 \) -print0 | xargs -0 rm -f
 
 echo Running autoreconf...
