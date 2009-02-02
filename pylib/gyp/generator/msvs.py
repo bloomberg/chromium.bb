23b3f3e49298f37f9a409c8959fd34f7595c9165
bradnelson@google.com <bradnelson@google.com@78cadc50-ecff-11dd-a971-7dbc132099af>
2009-02-02 20:01:46 +0000

    Fixing lint warnings.
    git-svn-id: http://gyp.googlecode.com/svn/trunk@76 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/generator/msvs.py
+++ pylib/gyp/generator/msvs.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python
 
 
+import os.path
+import subprocess
+import sys
 import gyp
 import gyp.MSVSNew as MSVSNew
 import gyp.MSVSProject as MSVSProject
-import os.path
-import subprocess
-import re
-import sys
 
 
 generator_default_variables = {
-  'OS': 'win',
+    'OS': 'win',
 }
 
 
-def FixPath(path):
+def _FixPath(path):
+  """Convert paths to a form that will make sense in a vcproj file.
+
+  Arguments:
+    path: The path to convert, may contain / etc.
+  Returns:
+    The path with cygwinisms removed, and all slashes made into backslashes.
+  """
   if sys.platform == 'cygwin':
     path = subprocess.Popen(['cygpath', '-w', path],
                             stdout=subprocess.PIPE).communicate()[0]
@@ -25,7 +31,22 @@ def FixPath(path):
     return path.replace('/', '\\')
 
 
-def SourceInFolders(sources, prefix=[]):
+def _SourceInFolders(sources, prefix=None):
+  """Converts a list split source file paths into a vcproj folder hierarchy.
+
+  Arguments:
+    sources: A list of source file paths split.
+    prefix: A list of source file path layers meant to apply to each of sources.
+  Returns:
+    A hierarchy of filenames and MSVSProject.Filter objects that matches the
+    layout of the source tree.
+    For example:
+    _SourceInFolders([['a', 'bob1.c'], ['b', 'bob2.c']], prefix=['joe'])
+    -->
+    [MSVSProject.Filter('a', contents=['joe\\a\\bob1.c']),
+     MSVSProject.Filter('b', contents=['joe\\b\\bob2.c'])]
+  """
+  if not prefix: prefix = []
   result = []
   folders = dict()
   for s in sources:
@@ -37,14 +58,21 @@ def SourceInFolders(sources, prefix=[]):
         folders[s[0]] = []
       folders[s[0]].append(s[1:])
   for f in folders:
-    contents = SourceInFolders(folders[f], prefix + [f])
+    contents = _SourceInFolders(folders[f], prefix + [f])
     contents = MSVSProject.Filter(f, contents=contents)
     result.append(contents)
 
   return result
 
 
-def GenerateProject(vcproj_filename, build_file, spec):
+def _GenerateProject(vcproj_filename, build_file, spec):
+  """Generates a vcproj file.
+
+  Arguments:
+    vcproj_filename: Filename of the vcproj file to generate.
+    build_file: Filename of the .gyp file that the vcproj file comes from.
+    spec: The target dictionary containing the properties of the target.
+  """
   print 'Generating %s' % vcproj_filename
 
   p = MSVSProject.Writer(vcproj_filename)
@@ -56,30 +84,30 @@ def GenerateProject(vcproj_filename, build_file, spec):
   vsprops_dirs = spec.get('vs_props', [])
   # Make them relative to the gyp file.
   vsprops_dirs = [os.path.join(gyp_dir, i) for i in vsprops_dirs]
-  vsprops_dirs = [FixPath(i) for i in vsprops_dirs]
+  vsprops_dirs = [_FixPath(i) for i in vsprops_dirs]
 
   # Prepare compiler tool.
-  include_dirs = spec.get('include_dirs',[])
+  include_dirs = spec.get('include_dirs', [])
   include_dirs = [os.path.join(gyp_dir, i) for i in include_dirs]
-  include_dirs = [FixPath(i) for i in include_dirs]
-  defines = ['OS_WIN'] + spec.get('defines',[])
-  disabled_warnings = [str(i) for i in spec.get('vs_disabled_warnings',[])]
+  include_dirs = [_FixPath(i) for i in include_dirs]
+  defines = ['OS_WIN'] + spec.get('defines', [])
+  disabled_warnings = [str(i) for i in spec.get('vs_disabled_warnings', [])]
   compiler_tool = MSVSProject.Tool(
       'VCCLCompilerTool', {
-        'AdditionalIncludeDirectories': ';'.join(include_dirs),
-        'PreprocessorDefinitions': ';'.join(defines),
-        'DisableSpecificWarnings': ';'.join(disabled_warnings),
-        })
+          'AdditionalIncludeDirectories': ';'.join(include_dirs),
+          'PreprocessorDefinitions': ';'.join(defines),
+          'DisableSpecificWarnings': ';'.join(disabled_warnings),
+      })
 
   # Pre-build and Post-build.
   prebuild = MSVSProject.Tool(
       'VCPreBuildEventTool', {
-        'CommandLine': spec.get('vs_prebuild', ''),
-        })
+          'CommandLine': spec.get('vs_prebuild', ''),
+      })
   postbuild = MSVSProject.Tool(
       'VCPostBuildEventTool', {
-        'CommandLine': spec.get('vs_postbuild', ''),
-        })
+          'CommandLine': spec.get('vs_postbuild', ''),
+      })
 
   # Prepare tools.
   tools = [
@@ -129,7 +157,7 @@ def GenerateProject(vcproj_filename, build_file, spec):
   # Convert to folders and the right slashes.
   sources = [i.replace('/', '\\') for i in sources]
   sources = [i.split('\\') for i in sources]
-  sources = SourceInFolders(sources)
+  sources = _SourceInFolders(sources)
   # Add in files.
   p.AddFiles(sources)
   # Write it out.
@@ -139,8 +167,8 @@ def GenerateProject(vcproj_filename, build_file, spec):
   return p.guid
 
 
-def PopulateProjectObjects(projects, target_list, target_dicts,
-                           qualified_target):
+def _PopulateProjectObjects(projects, target_list, target_dicts,
+                            qualified_target):
   # Done if this project has an object.
   if projects[qualified_target].get('obj'): return
   # Get dependencies for this project.
@@ -148,18 +176,18 @@ def PopulateProjectObjects(projects, target_list, target_dicts,
   deps = spec.get('dependencies', [])
   # Populate all dependencies first.
   for d in deps:
-    PopulateProjectObjects(projects, target_list, target_dicts, d)
+    _PopulateProjectObjects(projects, target_list, target_dicts, d)
   # Get objects for each dependency.
   deps = [projects[d]['obj'] for d in deps]
   # Create object for this project.
   projects[qualified_target]['obj'] = MSVSNew.MSVSProject(
-      FixPath(projects[qualified_target]['vcproj_path']),
+      _FixPath(projects[qualified_target]['vcproj_path']),
       name=spec['name'],
       guid=projects[qualified_target]['guid'],
       dependencies=deps)
 
 
-def DependentProjects(target_dicts, roots):
+def _DependentProjects(target_dicts, roots):
   dependents = set()
   for r in roots:
     spec = target_dicts[r]
@@ -167,15 +195,23 @@ def DependentProjects(target_dicts, roots):
     for d in r_deps:
       if d not in roots:
         dependents.add(d)
-    for d in DependentProjects(target_dicts, r_deps):
+    for d in _DependentProjects(target_dicts, r_deps):
       if d not in roots:
         dependents.add(d)
   return list(dependents)
 
 
 def GenerateOutput(target_list, target_dicts, data):
+  """Generate .sln and .vcproj files.
+
+  This is the entry point for this generator.
+  Arguments:
+    target_list: List of target pairs: 'base/base.gyp:base'.
+    target_dicts: Dict of target properties keyed on target pair.
+    data: Dictionary containing per .gyp data.
+  """
   solutions = {}
-  for build_file, build_file_dict in data.iteritems():
+  for build_file, unused_build_file_dict in data.iteritems():
     if build_file[-4:] != '.gyp':
       # TODO(mark): Pick an exception class
       raise 'Build file name must end in .gyp'
@@ -186,18 +222,18 @@ def GenerateOutput(target_list, target_dicts, data):
 
   projects = {}
   for qualified_target in target_list:
-    [build_file, target] = gyp.BuildFileAndTarget('', qualified_target)[0:2]
+    build_file = gyp.BuildFileAndTarget('', qualified_target)[0]
     spec = target_dicts[qualified_target]
     vcproj_path = os.path.abspath(os.path.join(os.path.split(build_file)[0],
                                                spec['name'] + '_gyp.vcproj'))
     projects[qualified_target] = {
         'vcproj_path': vcproj_path,
-        'guid': GenerateProject(vcproj_path, build_file, spec),
+        'guid': _GenerateProject(vcproj_path, build_file, spec),
     }
 
   for qualified_target in target_list:
-    PopulateProjectObjects(projects, target_list, target_dicts,
-                           qualified_target)
+    _PopulateProjectObjects(projects, target_list, target_dicts,
+                            qualified_target)
 
   for s, s_dict in solutions.iteritems():
     sln_path = s_dict['sln_path']
@@ -205,16 +241,16 @@ def GenerateOutput(target_list, target_dicts, data):
     # Get projects in the solution, and their dependents in a separate bucket.
     sln_projects = [p for p in target_list if
                     gyp.BuildFileAndTarget('', p)[0] == s]
-    dep_projects = DependentProjects(target_dicts, sln_projects)
+    dep_projects = _DependentProjects(target_dicts, sln_projects)
     # Convert to entries.
     entries = [projects[e]['obj'] for e in sln_projects]
     dep_entries = [projects[e]['obj'] for e in dep_projects]
     entries.append(MSVSNew.MSVSFolder('dependencies', entries=dep_entries))
     # Create solution.
-    sln = MSVSNew.MSVSSolution(FixPath(sln_path),
+    sln = MSVSNew.MSVSSolution(_FixPath(sln_path),
                                entries=entries,
                                variants=[
-                                 'Debug|Win32',
-                                 'Release|Win32',
+                                   'Debug|Win32',
+                                   'Release|Win32',
                                ])
     sln.Write()
