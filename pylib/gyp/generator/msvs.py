fab70d38dff2267d8d4cfcb84f9c65c90a286b57
bradnelson@google.com <bradnelson@google.com@78cadc50-ecff-11dd-a971-7dbc132099af>
2009-02-04 05:38:58 +0000

    Switching visual studio projects to relative paths.
    git-svn-id: http://gyp.googlecode.com/svn/trunk@91 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/generator/msvs.py
+++ pylib/gyp/generator/msvs.py
@@ -20,15 +20,9 @@ def _FixPath(path):
   Arguments:
     path: The path to convert, may contain / etc.
   Returns:
-    The path with cygwinisms removed, and all slashes made into backslashes.
+    The path with all slashes made into backslashes.
   """
-  if sys.platform == 'cygwin':
-    path = subprocess.Popen(['cygpath', '-w', path],
-                            stdout=subprocess.PIPE).communicate()[0]
-    path = path.replace('\n', '')
-    return path
-  else:
-    return path.replace('/', '\\')
+  return path.replace('/', '\\')
 
 
 def _SourceInFolders(sources, prefix=None):
@@ -82,13 +76,10 @@ def _GenerateProject(vcproj_filename, build_file, spec):
   gyp_dir = os.path.split(vcproj_filename)[0]
   # Figure out which vsprops to use.
   vsprops_dirs = spec.get('vs_props', [])
-  # Make them relative to the gyp file.
-  vsprops_dirs = [os.path.join(gyp_dir, i) for i in vsprops_dirs]
   vsprops_dirs = [_FixPath(i) for i in vsprops_dirs]
 
   # Prepare compiler tool.
   include_dirs = spec.get('include_dirs', [])
-  include_dirs = [os.path.join(gyp_dir, i) for i in include_dirs]
   include_dirs = [_FixPath(i) for i in include_dirs]
   defines = ['OS_WIN'] + spec.get('defines', [])
   disabled_warnings = [str(i) for i in spec.get('vs_disabled_warnings', [])]
@@ -159,7 +150,7 @@ def _GenerateProject(vcproj_filename, build_file, spec):
   # Add in the gyp file.
   sources.append(os.path.split(build_file)[1])
   # Convert to folders and the right slashes.
-  sources = [i.replace('/', '\\') for i in sources]
+  sources = [_FixPath(i) for i in sources]
   sources = [i.split('\\') for i in sources]
   sources = _SourceInFolders(sources)
   # Add in files.
@@ -171,24 +162,28 @@ def _GenerateProject(vcproj_filename, build_file, spec):
   return p.guid
 
 
-def _PopulateProjectObjects(projects, target_list, target_dicts,
-                            qualified_target):
+def _ProjectObject(sln, qualified_target, project_objs, projects):
   # Done if this project has an object.
-  if projects[qualified_target].get('obj'): return
+  if project_objs.get(qualified_target):
+    return project_objs[qualified_target]
   # Get dependencies for this project.
-  spec = target_dicts[qualified_target]
+  spec = projects[qualified_target]['spec']
   deps = spec.get('dependencies', [])
-  # Populate all dependencies first.
-  for d in deps:
-    _PopulateProjectObjects(projects, target_list, target_dicts, d)
   # Get objects for each dependency.
-  deps = [projects[d]['obj'] for d in deps]
+  deps = [_ProjectObject(sln, d, project_objs, projects) for d in deps]
+  # Find relative path to vcproj from sln.
+  vcproj_rel_path = gyp.common.RelativePath(
+      projects[qualified_target]['vcproj_path'], os.path.split(sln)[0])
+  vcproj_rel_path = _FixPath(vcproj_rel_path)
   # Create object for this project.
-  projects[qualified_target]['obj'] = MSVSNew.MSVSProject(
-      _FixPath(projects[qualified_target]['vcproj_path']),
+  obj = MSVSNew.MSVSProject(
+      vcproj_rel_path,
       name=spec['name'],
       guid=projects[qualified_target]['guid'],
       dependencies=deps)
+  # Store it to the list of objects.
+  project_objs[qualified_target] = obj
+  return obj
 
 
 def _DependentProjects(target_dicts, roots):
@@ -214,44 +209,38 @@ def GenerateOutput(target_list, target_dicts, data):
     target_dicts: Dict of target properties keyed on target pair.
     data: Dictionary containing per .gyp data.
   """
-  solutions = {}
-  for build_file, unused_build_file_dict in data.iteritems():
-    if build_file[-4:] != '.gyp':
-      # TODO(mark): Pick an exception class
-      raise 'Build file name must end in .gyp'
-    build_file_stem = build_file[:-4]
-    solutions[build_file] = {
-        'sln_path': os.path.abspath(build_file_stem + '_gyp.sln'),
-    }
-
   projects = {}
   for qualified_target in target_list:
     build_file = gyp.common.BuildFileAndTarget('', qualified_target)[0]
     spec = target_dicts[qualified_target]
-    vcproj_path = os.path.abspath(os.path.join(os.path.split(build_file)[0],
-                                               spec['name'] + '_gyp.vcproj'))
+    vcproj_path = os.path.join(os.path.split(build_file)[0],
+                               spec['name'] + '_gyp.vcproj')
     projects[qualified_target] = {
         'vcproj_path': vcproj_path,
         'guid': _GenerateProject(vcproj_path, build_file, spec),
+        'spec': spec,
     }
 
-  for qualified_target in target_list:
-    _PopulateProjectObjects(projects, target_list, target_dicts,
-                            qualified_target)
-
-  for s, s_dict in solutions.iteritems():
-    sln_path = s_dict['sln_path']
+  for build_file in data.keys():
+    # Validate build_file extension
+    if build_file[-4:] != '.gyp':
+      # TODO(mark): Pick an exception class.
+      raise 'Build file name must end in .gyp'
+    sln_path = build_file[:-4] + '_gyp.sln'
     print 'Generating %s' % sln_path
     # Get projects in the solution, and their dependents in a separate bucket.
     sln_projects = [p for p in target_list if
-                    gyp.common.BuildFileAndTarget('', p)[0] == s]
+                    gyp.common.BuildFileAndTarget('', p)[0] == build_file]
     dep_projects = _DependentProjects(target_dicts, sln_projects)
     # Convert to entries.
-    entries = [projects[e]['obj'] for e in sln_projects]
-    dep_entries = [projects[e]['obj'] for e in dep_projects]
+    project_objs = {}
+    entries = [_ProjectObject(sln_path, p, project_objs, projects)
+               for p in sln_projects]
+    dep_entries = [_ProjectObject(sln_path, p, project_objs, projects)
+                   for p in dep_projects]
     entries.append(MSVSNew.MSVSFolder('dependencies', entries=dep_entries))
     # Create solution.
-    sln = MSVSNew.MSVSSolution(_FixPath(sln_path),
+    sln = MSVSNew.MSVSSolution(sln_path,
                                entries=entries,
                                variants=[
                                    'Debug|Win32',
