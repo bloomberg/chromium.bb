0208ed4c79ed9aa1a983d372248f781c27969082
Kristian Høgsberg <krh@redhat.com>
2009-02-22 22:30:10 -0500

    Don't close the pty master in forkpty child.

--- terminal.c
+++ terminal.c
@@ -506,7 +506,6 @@ terminal_run(struct terminal *terminal, const char *path)
 
 	pid = forkpty(&master, NULL, NULL, NULL);
 	if (pid == 0) {
-		close(master);
 		setenv("TERM", "vt100", 1);
 		if (execl(path, path, NULL)) {
 			printf("exec failed: %m\n");
