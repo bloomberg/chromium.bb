0954e99802e6c9c911b87c862c7ca179e4a4a114
bradnelson@google.com <bradnelson@google.com@78cadc50-ecff-11dd-a971-7dbc132099af>
2009-03-20 20:46:37 +0000

    Fixing cygwin specific assumptions in gyp diff writer.
    Review URL: http://codereview.chromium.org/49004
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@385 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/common.py
+++ pylib/gyp/common.py
@@ -5,6 +5,7 @@ import filecmp
 import os.path
 import re
 import tempfile
+import sys
 
 
 def BuildFileAndTarget(build_file, target):
@@ -259,6 +260,11 @@ def WriteOnDiff(filename):
           umask = os.umask(077)
           os.umask(umask)
           os.chmod(self.tmp_path, 0666 & ~umask)
+          if sys.platform == 'win32':
+            # NOTE: on windows (but not cygwin) rename will not replace an
+            # existing file, so it must be preceded with a remove. Sadly there
+            # is no way to make the switch atomic.
+            os.remove(filename)
           os.rename(self.tmp_path, filename)
       except Exception:
         # Don't leave turds behind.
