b9d5fd015d43839cd9f4e5b977db55a799d68e7c
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-06-12 00:19:16 +0000

    Revert 18198 because the root chromium codereview.settings has wrong try server settings. :( Will reapply in 3 days.
    TEST=gcl try works.
    BUG=none
    TBR=nsylvain
    Review URL: http://codereview.chromium.org/126020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@18232 0039d316-1c4b-4281-b951-d872f2087c98

--- tests/trychange_unittest.py
+++ tests/trychange_unittest.py
@@ -5,6 +5,7 @@
 
 """Unit tests for trychange.py."""
 
+import os
 import unittest
 
 # Local imports
@@ -22,19 +23,18 @@ class TryChangeUnittest(TryChangeTestsBase):
   """General trychange.py tests."""
   def testMembersChanged(self):
     members = [
-      'EscapeDot', 'GIT', 'GetSourceRoot',
-      'GetTryServerSettings', 'GuessVCS',
+      'EscapeDot', 'ExecuteTryServerScript', 'GIT', 'GetSourceRoot', 'GuessVCS',
       'HELP_STRING', 'InvalidScript', 'NoTryServerAccess', 'PathDifference',
-      'RunCommand', 'SCM', 'SVN', 'TryChange', 'USAGE',
+      'RunCommand', 'SCM', 'SCRIPT_PATH', 'SVN', 'TryChange', 'USAGE',
       'datetime', 'gcl', 'gclient', 'getpass', 'logging', 'optparse', 'os',
-      'shutil', 'socket', 'sys', 'tempfile', 'traceback', 'upload', 'urllib',
+      'shutil', 'sys', 'tempfile', 'traceback', 'urllib',
     ]
     # If this test fails, you should add the relevant test.
     self.compareMembers(trychange, members)
 
 
 class SVNUnittest(TryChangeTestsBase):
-  """trychange.SVN tests."""
+  """General trychange.py tests."""
   def testMembersChanged(self):
     members = [
       'GenerateDiff', 'ProcessOptions', 'options'
@@ -43,8 +43,8 @@ class SVNUnittest(TryChangeTestsBase):
     self.compareMembers(trychange.SVN(None), members)
 
 
-class GITUnittest(TryChangeTestsBase):
-  """trychange.GIT tests."""
+class TryChangeUnittest(TryChangeTestsBase):
+  """General trychange.py tests."""
   def testMembersChanged(self):
     members = [
       'GenerateDiff', 'GetEmail', 'GetPatchName', 'ProcessOptions', 'options'
