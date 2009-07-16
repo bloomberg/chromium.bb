062ae62811cf92354abf87f1637859d736b8fe0c
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-16 23:48:33 +0000

    Add the WebKit API for DOM Storage.  This is used to re-enter WebKit.  A later patch will implement the WebKitClient version of it.
    BUG=4360
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/149716
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@20918 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9b9f2e5d435484c6cb05c6a47604cbd0022455df

---
+++ webkit/api/public/WebStorageArea.h
@@ -0,0 +1,85 @@
+/*
+ * Copyright (C) 2009 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef WebStorageArea_h
+#define WebStorageArea_h
+
+#include "WebCommon.h"
+
+namespace WebKit {
+    class WebString;
+
+    // In WebCore, there's one distinct StorageArea per origin per StorageNamespace. This
+    // class wraps a StorageArea. With the possible exception of lock/unlock all of these
+    // methods map very obviously to the WebStorage (often called DOM Storage) spec.
+    // http://dev.w3.org/html5/webstorage/
+    class WebStorageArea {
+    public:
+        virtual ~WebStorageArea() { }
+
+        // Lock the storage area. Before calling any other other methods on this interface,
+        // you should always call lock and wait for it to return. InvalidateCache tells you
+        // that since the last time you locked the cache, someone else has modified it.
+        // BytesLeftInQuota tells you how many bytes are currently unused in the quota.
+        // These are both optimizations and can be ignored if you'd like.
+        virtual void lock(bool& invalidateCache, size_t& bytesLeftInQuota) = 0;
+
+        // Unlock the storage area. You should call this at the end of the JavaScript context
+        // or when you're about to execute anything synchronous per the DOM Storage spec.
+        virtual void unlock() = 0;
+
+        // The number of key/value pairs in the storage area.
+        virtual unsigned length() = 0;
+
+        // Get a value for a specific key. Valid key indices are 0 through length() - 1.
+        // Indexes may change on any set/removeItem call. KeyException is true if the
+        // index provided was out of range.  When KeyException is true, the returned
+        // string is undefined.
+        virtual WebString key(unsigned index, bool& keyException) = 0;
+
+        // Get the value that corresponds to a specific key. This returns null if there is
+        // no entry for that key.
+        virtual WebString getItem(const WebString& key) = 0;
+
+        // Set the value that corresponds to a specific key. QuotaException is set if we've
+        // the StorageArea would have exceeded its quota. The value is NOT set when there's
+        // an exception.
+        virtual void setItem(const WebString& key, const WebString& value, bool& quotaException) = 0;
+
+        // Remove the value associated with a particular key.
+        virtual void removeItem(const WebString& key) = 0;
+
+        // Clear all key/value pairs.
+        virtual void clear() = 0;
+    };
+
+} // namespace WebKit
+
+#endif // WebStorageArea_h
---
+++ webkit/api/src/WebStorageAreaImpl.cpp
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2009 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "config.h"
+#include "WebStorageAreaImpl.h"
+
+#if ENABLE(DOM_STORAGE)
+
+#include "ExceptionCode.h"
+#include "WebString.h"
+
+namespace WebKit {
+
+WebStorageAreaImpl::WebStorageAreaImpl(PassRefPtr<WebCore::StorageArea> storageArea)
+    : m_storageArea(storageArea)
+{
+}
+
+WebStorageAreaImpl::~WebStorageAreaImpl()
+{
+}
+
+void WebStorageAreaImpl::lock(bool& invalidateCache, size_t& bytesLeftInQuota)
+{
+    // FIXME: Enable locking.  http://crbug.com/16877
+    invalidateCache = false;
+    // FIXME: Enable quota support.  http://crbug.com/16876
+    bytesLeftInQuota = 0;
+}
+
+void WebStorageAreaImpl::unlock()
+{
+    // FIXME: Enable locking.  http://crbug.com/16877
+}
+
+unsigned WebStorageAreaImpl::length()
+{
+    return m_storageArea->length();
+}
+
+WebString WebStorageAreaImpl::key(unsigned index, bool& keyException)
+{
+    int exceptionCode = 0;
+    WebString value = m_storageArea->key(index, exceptionCode);
+    if (exceptionCode != 0) {
+        ASSERT(exceptionCode == WebCore::INDEX_SIZE_ERR);
+        keyException = true;
+    } else {
+        keyException = false;
+    }
+    return value;
+}
+
+WebString WebStorageAreaImpl::getItem(const WebString& key)
+{
+    return m_storageArea->getItem(key);
+}
+
+void WebStorageAreaImpl::setItem(const WebString& key, const WebString& value, bool& quotaException)
+{
+    int exceptionCode = 0;
+    // FIXME: Can we do any better than just passing 0 for the frame?
+    m_storageArea->setItem(key, value, exceptionCode, 0);
+    if (exceptionCode != 0) {
+        ASSERT(exceptionCode == WebCore::QUOTA_EXCEEDED_ERR);
+        quotaException = true;
+    } else {
+        quotaException = false;
+    }
+}
+
+void WebStorageAreaImpl::removeItem(const WebString& key)
+{
+    // FIXME: Can we do any better than just passing 0 for the frame?
+    m_storageArea->removeItem(key, 0);
+}
+
+void WebStorageAreaImpl::clear()
+{
+    // FIXME: Can we do any better than just passing 0 for the frame?
+    m_storageArea->clear(0);
+}
+
+} // namespace WebKit
+
+#endif // ENABLE(DOM_STORAGE)
