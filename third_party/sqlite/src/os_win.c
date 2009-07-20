e73c4b2aeafe44495c092312325c1ad8ae9d3941
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-20 18:04:04 +0000

    Stubs for DB-related methods in ChromiumBridge.
    Review URL: http://codereview.chromium.org/159044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21083 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: eca56d34420e0f72a472c928714d1fcc7ced24ea

--- third_party/sqlite/src/os_win.c
+++ third_party/sqlite/src/os_win.c
@@ -1635,4 +1635,11 @@ int sqlite3_os_end(void){
   return SQLITE_OK;
 }
 
+void chromium_sqlite3_initialize_win_sqlite3_file(sqlite3_file* file, HANDLE handle) {
+  winFile* winSQLite3File = (winFile*)file;
+  memset(file, 0, sizeof(*file));
+  winSQLite3File->pMethod = &winIoMethod;
+  winSQLite3File->h = handle;
+}
+
 #endif /* SQLITE_OS_WIN */
