16f7be35d06ff0a84b7015d72a5dff8f9b242b19
pinkerton@chromium.org <pinkerton@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-23 14:57:23 +0000

    Increase the close button offset so the text doesn't overlap it. BUG=13300 TEST=see bug. Review URL: http://codereview.chromium.org/159269
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21381 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0e2aad680a9b410101108f3604df896dbd0305ac

--- chrome/browser/cocoa/tab_cell.mm
+++ chrome/browser/cocoa/tab_cell.mm
@@ -25,14 +25,12 @@
 // Override drawing the button so that it looks like a Chromium tab instead
 // of just a normal MacOS button.
 - (void)drawWithFrame:(NSRect)cellFrame inView:(NSView *)controlView {
-
-
   // Inset where the text is drawn to keep it away from the sloping edges of the
   // tab, the close box, and the icon view. These constants are derived
   // empirically as the cell doesn't know about the surrounding view objects.
   // TODO(pinkerton/alcor): Fix this somehow?
   const int kIconXOffset = 28;
-  const int kCloseBoxXOffset = 18;
+  const int kCloseBoxXOffset = 21;
   NSRect frame = NSOffsetRect(cellFrame, kIconXOffset, 0);
   frame.size.width -= kCloseBoxXOffset + kIconXOffset;
   [self drawInteriorWithFrame:frame
