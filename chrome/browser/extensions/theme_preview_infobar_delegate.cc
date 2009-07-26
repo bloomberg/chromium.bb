dc9033636eb6f7348cbef8b35157da8f00c35f2d
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-26 03:58:38 +0000

    Fix broken tree.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21624 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 97cdbad260d0a97b1a57dc4e863607c409533412

--- chrome/browser/extensions/theme_preview_infobar_delegate.cc
+++ chrome/browser/extensions/theme_preview_infobar_delegate.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/views/extensions/theme_preview_infobar_delegate.h"
+#include "chrome/browser/extensions/theme_preview_infobar_delegate.h"
 
 #include "app/l10n_util.h"
 #include "base/string_util.h"
