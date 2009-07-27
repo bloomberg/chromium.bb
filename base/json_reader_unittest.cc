0dbab06405cea51ba7e55dcd1d5307485e05ed0e
deanm@chromium.org <deanm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-27 21:50:43 +0000

    Remove ifdef 32-bit clause in json tests.  ints are 32-bit everywhere.
    Review URL: http://codereview.chromium.org/159434
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@21716 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ef0258273377dcaf33058194241af360e0961f0e

--- base/json_reader_unittest.cc
+++ base/json_reader_unittest.cc
@@ -72,30 +72,16 @@ TEST(JSONReaderTest, Reading) {
   root.reset(JSONReader().JsonToValue("2147483648", false, false));
   ASSERT_TRUE(root.get());
   double real_val;
-#ifdef ARCH_CPU_32_BITS
   ASSERT_TRUE(root->IsType(Value::TYPE_REAL));
   real_val = 0.0;
   ASSERT_TRUE(root->GetAsReal(&real_val));
   ASSERT_DOUBLE_EQ(2147483648.0, real_val);
-#else
-  ASSERT_TRUE(root->IsType(Value::TYPE_INTEGER));
-  int_val = 0;
-  ASSERT_TRUE(root->GetAsInteger(&int_val));
-  ASSERT_EQ(2147483648, int_val);
-#endif
   root.reset(JSONReader().JsonToValue("-2147483649", false, false));
   ASSERT_TRUE(root.get());
-#ifdef ARCH_CPU_32_BITS
   ASSERT_TRUE(root->IsType(Value::TYPE_REAL));
   real_val = 0.0;
   ASSERT_TRUE(root->GetAsReal(&real_val));
   ASSERT_DOUBLE_EQ(-2147483649.0, real_val);
-#else
-  ASSERT_TRUE(root->IsType(Value::TYPE_INTEGER));
-  int_val = 0;
-  ASSERT_TRUE(root->GetAsInteger(&int_val));
-  ASSERT_EQ(-2147483649, int_val);
-#endif
 
   // Parse a double
   root.reset(JSONReader().JsonToValue("43.1", false, false));
