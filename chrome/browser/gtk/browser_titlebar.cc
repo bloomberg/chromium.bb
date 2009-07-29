b42f4ffbb78286e8ea3e58d3814181b3324d6d18
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-29 23:06:59 +0000

    gtk: Don't wrap around when scrolling the tab strip using the scroll event.  This matches native gtk behavior.
    BUG=17869
    TEST=Open multiple tabs.  Scroll the tabs using the scroll wheel.  The scroll should not wrap around.
    Review URL: http://codereview.chromium.org/160362
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22015 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 505f03b07b75abe85d28548e3f1878172c25e74e

--- chrome/browser/gtk/browser_titlebar.cc
+++ chrome/browser/gtk/browser_titlebar.cc
@@ -215,10 +215,12 @@ gboolean BrowserTitlebar::OnScroll(GtkWidget* widget, GdkEventScroll* event,
                                    BrowserTitlebar* titlebar) {
   TabStripModel* tabstrip_model =
       titlebar->browser_window_->browser()->tabstrip_model();
+  int index = tabstrip_model->selected_index();
   if (event->direction == GDK_SCROLL_LEFT ||
       event->direction == GDK_SCROLL_UP) {
-    tabstrip_model->SelectPreviousTab();
-  } else {
+    if (index != 0)
+      tabstrip_model->SelectPreviousTab();
+  } else if (index + 1 < tabstrip_model->count()) {
     tabstrip_model->SelectNextTab();
   }
   return TRUE;
