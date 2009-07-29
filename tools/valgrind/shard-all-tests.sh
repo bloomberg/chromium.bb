4b39f0e3f1bbf253ba86a3dbe7c6b618efab5991
dkegel@google.com <dkegel@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-29 23:01:46 +0000

    Fix typo that prevented invalid reads from matching Update to support make We now generate suppressions by default, so remove obsolete --generate_suppressions flag
    Review URL: http://codereview.chromium.org/155794
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22013 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 20c811b80e67f1f866f4191aaf3b75768bdbb2c6

--- tools/valgrind/shard-all-tests.sh
+++ tools/valgrind/shard-all-tests.sh
@@ -4,7 +4,7 @@
 # generate suppressions for them, to be later filed as bugs
 # and added to our suppressions file.
 #
-# FIXME: Layout tests are a bit funny - they have their own 
+# FIXME: Layout tests are a bit funny - they have their own
 # sharding control, and should probably be tweaked to obey
 # GTEST_SHARD_INDEX/GTEST_TOTAL_SHARDS like the rest,
 # but they take days and days to run, so they are left
@@ -20,7 +20,7 @@ set -x
 set -e
 
 # Regexp to match any valgrind error
-PATTERN="ERROR SUMMARY: [^0]|are definitely|uninitialised|Unhandled exception:\
+PATTERN="ERROR SUMMARY: [^0]|are definitely|uninitialised|Unhandled exception|\
 Invalid read|Invalid write|Invalid free|Source and desti|Mismatched free|\
 unaddressable byte|vex x86"
 
@@ -31,13 +31,19 @@ TESTS="base_unittests googleurl_unittests ipc_tests media_unittests \
        net_unittests printing_unittests startup_tests \
        test_shell_tests ui_tests unit_tests"
 
+TESTS_BUILDABLE=`echo $TESTS | tr ' ' '\012' | grep -v layout_tests`
+
 # Build the tests (if we know how)
 OS=`uname`
 case $OS in
 Linux)
-  cd build
-  hammer $TESTS
-  cd ..
+  # Lame way to autodetect whether 'make' or 'hammer' is in use
+  if test -d out
+  then
+    make -j4 chrome test_shell $TESTS_BUILDABLE
+  else
+    hammer chrome test_shell $TESTS_BUILDABLE
+  fi
   ;;
 *) echo "don't know how to build on os $OS"
   ;;
@@ -56,8 +62,7 @@ do
     while test $GTEST_SHARD_INDEX -lt $GTEST_TOTAL_SHARDS
     do
       i=$GTEST_SHARD_INDEX
-      sh tools/valgrind/chrome_tests.sh -t ${testname} --generate_suppressions\
-                                         > ${testname}_$i.vlog 2>&1
+      sh tools/valgrind/chrome_tests.sh -t ${testname} > ${testname}_$i.vlog 2>&1
       GTEST_SHARD_INDEX=`expr $GTEST_SHARD_INDEX + 1`
     done
   done
