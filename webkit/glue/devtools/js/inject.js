5d5193b7e3315e89917d05ee45affbcc976182c9
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-07-30 13:03:50 +0000

    DevTools: Show errors that happened while expanding object properties.
    Review URL: http://codereview.chromium.org/159634
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22051 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a0db36a7c15ad1810e4670e63f43c92608dea868

--- webkit/glue/devtools/js/inject.js
+++ webkit/glue/devtools/js/inject.js
@@ -102,34 +102,37 @@ devtools.Injected.prototype.getProperties =
       continue;
     }
 
+    var value;
+    var type;
     try {
-      var value = obj[name];
-      var type = typeof value;
-      result.push(type);
-      result.push(name);
-      if (type == 'string') {
-        var str = value;
-        result.push(str.length > 99 ? str.substr(0, 99) + '...' : str);
-        result.push(undefined);
-      } else if (type == 'function') {
-        result.push(undefined);
-        var str = Function.prototype.toString.call(value);
-        // Cut function signature (everything before first ')').
-        var signatureLength = str.search(/\)/);
-        str = str.substr(0, signatureLength + 1);
-        // Collapse each group of consecutive whitespaces into one whitespaces
-        // and add body brackets.
-        str = str.replace(/\s+/g, ' ') + ' {}';
-        result.push(str);
-      } else if (type == 'object') {
-        result.push(undefined);
-        result.push(this.getClassName_(value));
-      } else {
-        result.push(value);
-        result.push(undefined);
-      }
+      value = obj[name];
+      type = typeof value;
     } catch (e) {
-      // Mute exceptions from unsafe getters.
+      value = 'Exception: ' + e.toString();
+      type = 'string';
+    }
+    result.push(type);
+    result.push(name);
+    if (type == 'string') {
+      var str = value;
+      result.push(str.length > 99 ? str.substr(0, 99) + '...' : str);
+      result.push(undefined);
+    } else if (type == 'function') {
+      result.push(undefined);
+      var str = Function.prototype.toString.call(value);
+      // Cut function signature (everything before first ')').
+      var signatureLength = str.search(/\)/);
+      str = str.substr(0, signatureLength + 1);
+      // Collapse each group of consecutive whitespaces into one whitespaces
+      // and add body brackets.
+      str = str.replace(/\s+/g, ' ') + ' {}';
+      result.push(str);
+    } else if (type == 'object') {
+      result.push(undefined);
+      result.push(this.getClassName_(value));
+    } else {
+      result.push(value);
+      result.push(undefined);
     }
   }
   return result;
