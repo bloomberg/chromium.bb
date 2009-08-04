fbc8b6bb64dc3510088a2ad2be99156e237b6481
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-04 21:13:42 +0000

    linux: opening a download should not reuse your terminal
    When opening a text file, if the helper wants to just spawn "less"
    we want it to use a new window for it.
    
    BUG=18298
    
    Review URL: http://codereview.chromium.org/159867
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22404 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e2c2b560f36abd25ef9655858e5db238051687c4

--- chrome/common/platform_util_linux.cc
+++ chrome/common/platform_util_linux.cc
@@ -17,9 +17,16 @@ void XDGOpen(const FilePath& path) {
   std::vector<std::string> argv;
   argv.push_back("xdg-open");
   argv.push_back(path.value());
-  base::file_handle_mapping_vector no_files;
+
   base::environment_vector env;
   env.push_back(std::make_pair("GTK_PATH", getenv("CHROMIUM_SAVED_GTK_PATH")));
+  // xdg-open can fall back on mailcap which eventually might plumb through
+  // to a command that needs a terminal.  Set the environment variable telling
+  // it that we definitely don't have a terminal available and that it should
+  // bring up a new terminal if necessary.  See "man mailcap".
+  env.push_back(std::make_pair("MM_NOTTTY", "1"));
+
+  base::file_handle_mapping_vector no_files;
   base::ProcessHandle handle;
   if (base::LaunchApp(argv, env, no_files, false, &handle))
     ProcessWatcher::EnsureProcessGetsReaped(handle);
