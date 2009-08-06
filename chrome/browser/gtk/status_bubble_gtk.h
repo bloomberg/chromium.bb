873bf23b619850f49a15f8ff583e39229d96a1f4
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-06 20:37:12 +0000

    Revert "GTK: Rounded corner on status bubble."
    This reverts commit d98d01314c8b0905ac236d98dc39508ad8acfd05 (r22644).
    
    This should make the linux ui tests green.
    
    Review URL: http://codereview.chromium.org/164086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@22656 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4be51be54f1fce60e93b8eb659dc91088c219e13

--- chrome/browser/gtk/status_bubble_gtk.h
+++ chrome/browser/gtk/status_bubble_gtk.h
@@ -69,33 +69,26 @@ class StatusBubbleGtk : public StatusBubble,
   // Notification from the window that we should retheme ourself.
   void UserChangedTheme();
 
-  // Draws the inside border.
-  static gboolean OnExpose(GtkWidget* widget, GdkEventExpose* event,
-                           StatusBubbleGtk* bubble);
-
-  // When we have a new size, modify our widget shape.
-  static void OnSizeAllocate(GtkWidget* widget, GtkAllocation* allocation,
-                             StatusBubbleGtk* bubble);
-
   NotificationRegistrar registrar_;
 
   // Provides colors.
   GtkThemeProvider* theme_provider_;
 
-  // The toplevel event box.
+  // A GtkAlignment that is the child of |slide_widget_|.
   OwnedWidgetGtk container_;
 
   // The GtkLabel holding the text.
   GtkWidget* label_;
+
+  // The background event box. We keep this so we can change its background
+  // color.
+  GtkWidget* bg_box_;
 
   // The status text we want to display when there are no URLs to display.
   std::string status_text_;
 
   // The url we want to display when there is no status text to display.
   std::string url_text_;
-
-  // Color of the lighter border around the edge of the status bubble.
-  GdkColor border_color_;
 
   // A timer that hides our window after a delay.
   ScopedRunnableMethodFactory<StatusBubbleGtk> timer_factory_;
