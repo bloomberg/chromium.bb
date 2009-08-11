71946c60c16e2a38629cfc3356de0ab153461be2
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-11 23:54:26 +0000

    Some cleanup * use full paths in includes
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/165288
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23121 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 156fa0eb10b37260f40ec70ac50a7921ad55a3dd

--- webkit/appcache/manifest_parser.cc
+++ webkit/appcache/manifest_parser.cc
@@ -29,7 +29,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "manifest_parser.h"
+#include "webkit/appcache/manifest_parser.h"
 
 #include "base/logging.h"
 #include "base/string_util.h"
