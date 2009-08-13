f942d4824d51ab7a928e51ed3ef3f198b725ffbf
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-13 06:12:25 +0000

    Another attempt to enable the local storage ui test based layout tests.
    TBR=michaeln
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/164461
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23286 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2402112f07142d0af96ae5db0082067463490384

--- chrome/browser/in_process_webkit/dom_storage_uitest.cc
+++ chrome/browser/in_process_webkit/dom_storage_uitest.cc
@@ -2,11 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(jorlow): Reenable when https://bugs.webkit.org/show_bug.cgi?id=28094
-//               is fixed.  Until then, this will cause crashes even if the
-//               individual tests are disabled.
-#if 0
-
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/ui/ui_layout_test.h"
 
@@ -76,5 +71,3 @@ TEST_F(DOMStorageTest, SessionStorageLayoutTests) {
   for (size_t i=0; i<arraysize(kSubDirFiles); ++i)
     RunLayoutTest(kSubDirFiles[i], false);
 }
-
-#endif
