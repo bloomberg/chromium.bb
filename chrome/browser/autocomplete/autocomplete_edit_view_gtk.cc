22cfaaa748d4a7638defea776bc2eec349418b27
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-19 16:04:27 +0000

    Linux: Revert updating PRIMARY selection on Ctrl-C in omnibox.
    This backs out r23596.  I'm pretty sure that it worked when I tested it
    right before submitting, but it's totally breaking stuff now -- it looks
    like when I update the PRIMARY selection, GTK notices that it's lost the
    selection and automatically unhighlights the text in the omnibox, making
    it impossible to use Ctrl-C to copy omnibox text to the clipboard. :-(
    
    BUG=19648
    TESTED=Hit Ctrl-L, Ctrl-C and confirmed that text remains highlighted and becomes CLIPBOARD selection.
    
    Review URL: http://codereview.chromium.org/174058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23714 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 837f7b77e6076295090a39726a20065bed384b5f

--- chrome/browser/autocomplete/autocomplete_edit_view_gtk.cc
+++ chrome/browser/autocomplete/autocomplete_edit_view_gtk.cc
@@ -210,8 +210,6 @@ void AutocompleteEditViewGtk::Init() {
                    G_CALLBACK(&HandleDragDataReceivedThunk), this);
   g_signal_connect(text_view_, "backspace",
                    G_CALLBACK(&HandleBackSpaceThunk), this);
-  g_signal_connect(text_view_, "copy-clipboard",
-                   G_CALLBACK(&HandleCopyClipboardThunk), this);
 
 #if !defined(TOOLKIT_VIEWS)
   registrar_.Add(this,
@@ -921,20 +919,6 @@ void AutocompleteEditViewGtk::HandleBackSpace() {
   // Stop propagating the signal emission into GtkTextView.
   static guint signal_id = g_signal_lookup("backspace", GTK_TYPE_TEXT_VIEW);
   g_signal_stop_emission(text_view_, signal_id, 0);
-}
-
-void AutocompleteEditViewGtk::HandleCopyClipboard() {
-  // On copy, we manually update the PRIMARY selection to contain the
-  // highlighted text.  This matches Firefox -- we highlight the URL but don't
-  // update PRIMARY on Ctrl-L, so Ctrl-L, Ctrl-C and then middle-click is a
-  // convenient way to paste the current URL somewhere.
-  GtkTextIter start, end;
-  if (!gtk_text_buffer_get_selection_bounds(text_buffer_, &start, &end))
-    return;
-
-  gchar* text = gtk_text_buffer_get_text(text_buffer_, &start, &end, FALSE);
-  SavePrimarySelection(text);
-  g_free(text);
 }
 
 void AutocompleteEditViewGtk::SelectAllInternal(bool reversed,
