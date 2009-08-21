79344f4456802158079b0ad9c7f404d76f6e03b2
idanan@chromium.org <idanan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-08-21 18:57:08 +0000

    Cleanup Unused Includes
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/173210
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23993 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e718099e5cb97a6b49788c977ef157083e12334e

--- chrome/browser/privacy_blacklist/blacklist_observer.h
+++ chrome/browser/privacy_blacklist/blacklist_observer.h
@@ -5,11 +5,6 @@
 #ifndef CHROME_BROWSER_PRIVACY_BLACKLIST_BLACKLIST_OBSERVER_H_
 #define CHROME_BROWSER_PRIVACY_BLACKLIST_BLACKLIST_OBSERVER_H_
 
-#include "chrome/common/notification_details.h"
-#include "chrome/common/notification_observer.h"
-#include "chrome/common/notification_service.h"
-#include "chrome/common/notification_type.h"
-
 class URLRequest;
 
 class BlacklistObserver {
