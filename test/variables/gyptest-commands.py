c02278cfb5d8d068a26a503477bd09a600b2076a
sgk@chromium.org <sgk@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2009-08-27 15:37:00 +0000

    Make the variables test cross-platform w.r.t. to line endings in the output. Review URL: http://codereview.chromium.org/174611
    git-svn-id: http://gyp.googlecode.com/svn/trunk@613 78cadc50-ecff-11dd-a971-7dbc132099af

--- test/variables/gyptest-commands.py
+++ test/variables/gyptest-commands.py
@@ -21,6 +21,20 @@ test.run_gyp('commands.gyp',
              '--debug', 'variables', '--debug', 'general',
              stdout=expect)
 
-test.must_match('commands.gypd', test.read('commands.gypd.golden'))
+# Verify the commands.gypd against the checked-in expected contents.
+#
+# Normally, we should canonicalize line endings in the expected
+# contents file setting the Subversion svn:eol-style to native,
+# but that would still fail if multiple systems are sharing a single
+# workspace on a network-mounted file system.  Consequently, we
+# massage the Windows line endings ('\r\n') in the output to the
+# checked-in UNIX endings ('\n').
+
+contents = test.read('commands.gypd').replace('\r\n', '\n')
+expect = test.read('commands.gypd.golden')
+if not test.match(contents, expect):
+  print "Unexpected contents of `commands.gypd'"
+  self.diff(expect, contents, 'commands.gypd ')
+  test.fail_test()
 
 test.pass_test()
