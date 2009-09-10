a780d36f37cc830eaa5a4fc971732668db478e31
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-10 02:15:56 +0000

    Put the find bar back for mac and linux.
    BUG=21443
    
    Review URL: http://codereview.chromium.org/192060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25836 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b441dca34aa8b5df8f70b30529e2f6af9f25603c

--- chrome/browser/gtk/view_id_util_browsertest.cc
+++ chrome/browser/gtk/view_id_util_browsertest.cc
@@ -23,9 +23,7 @@ class ViewIDTest : public InProcessBrowserTest {
   GtkWidget* root_window_;
 };
 
-// Disabled since it is failing on buildbots.
-// http://code.google.com/p/chromium/issues/detail?id=21443
-IN_PROC_BROWSER_TEST_F(ViewIDTest, DISABLED_Basic) {
+IN_PROC_BROWSER_TEST_F(ViewIDTest, Basic) {
   for (int i = VIEW_ID_TOOLBAR; i < VIEW_ID_PREDEFINED_COUNT; ++i) {
     CheckViewID(static_cast<ViewID>(i), true);
   }
