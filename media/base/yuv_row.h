090dde2a5caabec32a2328f5f3ff281b7fbacbb1
mhm@chromium.org <mhm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-12 03:53:20 +0000

    Reverting 26070 from media tests failure
    6 failed media_unittests
    - CreateFrame
    - Clamp
    - YV12
    - YV16
    - YV12
    - YV16
    
    TBR=fbarchard
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/202068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26074 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8e5722d2311e0a91cca92692c012e64bb01fb2cf

--- media/base/yuv_row.h
+++ media/base/yuv_row.h
@@ -63,8 +63,9 @@ void ScaleYUVToRGB32Row(const uint8* y_buf,
 }  // extern "C"
 
 #if !defined(USE_MMX)
-// Windows, Mac and Linux x86 use MMX; x64 and other CPUs do not.
-#if defined(OS_WIN) || defined(ARCH_CPU_X86)
+#if defined(_MSC_VER)
+#define USE_MMX 1
+#elif defined(OS_LINUX) && defined(ARCH_CPU_X86)
 #define USE_MMX 1
 #else
 #define USE_MMX 0
--- media/base/yuv_row_linux.cc
+++ media/base/yuv_row_linux.cc
@@ -300,9 +300,9 @@ void FastConvertYUVToRGB32Row(const uint8* y_buf,
   "mov    0x2c(%esp),%esi\n"
   "mov    0x30(%esp),%ebp\n"
   "mov    0x34(%esp),%ecx\n"
-  "jmp    Lconvertend\n"
+  "jmp    convertend\n"
 
-"Lconvertloop:"
+"convertloop:"
   "movzbl (%edi),%eax\n"
   "add    $0x1,%edi\n"
   "movzbl (%esi),%ebx\n"
@@ -321,12 +321,12 @@ void FastConvertYUVToRGB32Row(const uint8* y_buf,
   "packuswb %mm2,%mm1\n"
   "movntq %mm1,0x0(%ebp)\n"
   "add    $0x8,%ebp\n"
-"Lconvertend:"
+"convertend:"
   "sub    $0x2,%ecx\n"
-  "jns    Lconvertloop\n"
+  "jns    convertloop\n"
 
   "and    $0x1,%ecx\n"
-  "je     Lconvertdone\n"
+  "je     convertdone\n"
 
   "movzbl (%edi),%eax\n"
   "movq   kCoefficientsRgbU(,%eax,8),%mm0\n"
@@ -338,7 +338,7 @@ void FastConvertYUVToRGB32Row(const uint8* y_buf,
   "psraw  $0x6,%mm1\n"
   "packuswb %mm1,%mm1\n"
   "movd   %mm1,0x0(%ebp)\n"
-"Lconvertdone:"
+"convertdone:"
   "popa\n"
   "ret\n"
 );
@@ -361,9 +361,9 @@ void ScaleYUVToRGB32Row(const uint8* y_buf,
   "mov    0x30(%esp),%ebp\n"
   "mov    0x34(%esp),%ecx\n"
   "xor    %ebx,%ebx\n"
-  "jmp    Lscaleend\n"
+  "jmp    scaleend\n"
 
-"Lscaleloop:"
+"scaleloop:"
   "mov    %ebx,%eax\n"
   "sar    $0x5,%eax\n"
   "movzbl (%edi,%eax,1),%eax\n"
@@ -389,12 +389,12 @@ void ScaleYUVToRGB32Row(const uint8* y_buf,
   "packuswb %mm2,%mm1\n"
   "movntq %mm1,0x0(%ebp)\n"
   "add    $0x8,%ebp\n"
-"Lscaleend:"
+"scaleend:"
   "sub    $0x2,%ecx\n"
-  "jns    Lscaleloop\n"
+  "jns    scaleloop\n"
 
   "and    $0x1,%ecx\n"
-  "je     Lscaledone\n"
+  "je     scaledone\n"
 
   "mov    %ebx,%eax\n"
   "sar    $0x5,%eax\n"
@@ -419,7 +419,7 @@ void ScaleYUVToRGB32Row(const uint8* y_buf,
   "packuswb %mm2,%mm1\n"
   "movd   %mm1,0x0(%ebp)\n"
 
-"Lscaledone:"
+"scaledone:"
   "popa\n"
   "ret\n"
 );
