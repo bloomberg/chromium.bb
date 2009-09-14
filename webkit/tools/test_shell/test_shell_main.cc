bloomberg/chromium.bb

commit 7d92f8fcaa303e6dcc60240559748de372f950bd

Hook up WebFrameClient, replacing many WebViewDelegate methods.
Moved NavigationGesture out of webview_delegate.h into its own
header in chrome/common since it is only needed by Chrome.

Adds WebFrame::isProcessingUserGesture to facilitate the removal
of NavigationGesture.

Cleaned up some TestShell methods related to URL loading.  The
method to load an URL now takes a GURL instead of a wchar_t*.

R=dglazkov
BUG=21332
TEST=none

Review URL: http://codereview.chromium.org/200054

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@25841 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 3d96893747d18a3c1bf06084d4605a4b34af732f

webkit/tools/test_shell/test_shell_main.cc
@@ -208,7 +208,7 @@ int main(int argc, char* argv[]) {
   StatsTable::set_current(table);
 
   TestShell* shell;
-  if (TestShell::CreateNewWindow(uri.ToWStringHack(), &shell)) {
+  if (TestShell::CreateNewWindow(net::FilePathToFileURL(uri), &shell)) {
     if (record_mode || playback_mode) {
       platform.SetWindowPositionForRecording(shell);
       WebKit::registerExtension(extensions_v8::PlaybackExtension::Get());

chrome/chrome.gyp
@@ -524,6 +524,7 @@
         'common/message_router.h',
         'common/modal_dialog_event.h',
         'common/mru_cache.h',
+        'common/navigation_gesture.h',
         'common/navigation_types.h',
         'common/native_web_keyboard_event.h',
         'common/native_web_keyboard_event_linux.cc',

chrome/common/navigation_gesture.h
@@ -1,0 +1,22 @@
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_COMMON_NAVIGATION_GESTURE_H_
+#define CHROME_COMMON_NAVIGATION_GESTURE_H_
+
+enum NavigationGesture {
+  NavigationGestureUser,    // User initiated navigation/load. This is not
+                            // currently used due to the untrustworthy nature
+                            // of userGestureHint (wasRunByUserGesture). See
+                            // bug 1051891.
+  NavigationGestureAuto,    // Non-user initiated navigation / load. For example
+                            // onload or setTimeout triggered document.location
+                            // changes, and form.submits. See bug 1046841 for
+                            // some cases that should be treated this way but
+                            // aren't yet.
+  NavigationGestureUnknown, // What we assign when userGestureHint returns true
+                            // because we can't trust it.
+};
+
+#endif  // CHROME_COMMON_NAVIGATION_GESTURE_H_

chrome/common/render_messages.h
@@ -21,6 +21,7 @@
 #include "chrome/common/extensions/url_pattern.h"
 #include "chrome/common/filter_policy.h"
 #include "chrome/common/modal_dialog_event.h"
+#include "chrome/common/navigation_gesture.h"
 #include "chrome/common/page_transition_types.h"
 #include "chrome/common/renderer_preferences.h"
 #include "chrome/common/transport_dib.h"

chrome/renderer/print_web_view_helper.cc
@@ -96,7 +96,7 @@ bool PrintWebViewHelper::CopyAndPrint(const ViewMsg_PrintPages_Params& params,
 
   print_web_view_.reset(WebView::Create(this, NULL));
   prefs.Apply(print_web_view_.get());
-  print_web_view_->InitializeMainFrame();
+  print_web_view_->InitializeMainFrame(NULL);
 
   print_pages_params_.reset(new ViewMsg_PrintPages_Params(params));
   print_pages_params_->pages.clear();  // Print all pages of selection.

chrome/renderer/render_view.h
@@ -20,6 +20,7 @@
 #include "base/values.h"
 #include "base/weak_ptr.h"
 #include "build/build_config.h"
+#include "chrome/common/navigation_gesture.h"
 #include "chrome/common/renderer_preferences.h"
 #include "chrome/common/view_types.h"
 #include "chrome/renderer/automation/dom_automation_controller.h"
@@ -31,6 +32,7 @@
 #include "testing/gtest/include/gtest/gtest_prod.h"
 #include "webkit/api/public/WebConsoleMessage.h"
 #include "webkit/api/public/WebEditingClient.h"
+#include "webkit/api/public/WebFrameClient.h"
 #include "webkit/api/public/WebTextDirection.h"
 #include "webkit/glue/dom_serializer_delegate.h"
 #include "webkit/glue/form_data.h"
@@ -102,6 +104,7 @@ typedef base::RefCountedData<int> SharedRenderViewCounter;
 class RenderView : public RenderWidget,
                    public WebViewDelegate,
                    public WebKit::WebEditingClient,
+                   public WebKit::WebFrameClient,
                    public webkit_glue::WebPluginPageDelegate,
                    public webkit_glue::DomSerializerDelegate,
                    public base::SupportsWeakPtr<RenderView> {
@@ -188,77 +191,15 @@ class RenderView : public RenderWidget,
                                    const std::wstring& source_id);
   virtual void DidStartLoading(WebView* webview);
   virtual void DidStopLoading(WebView* webview);
-  virtual void DidCreateDataSource(WebKit::WebFrame* frame,
-                                   WebKit::WebDataSource* ds);
-  virtual void DidStartProvisionalLoadForFrame(
-      WebView* webview,
-      WebKit::WebFrame* frame,
-      NavigationGesture gesture);
-  virtual void DidReceiveProvisionalLoadServerRedirect(WebView* webview,
-                                                       WebKit::WebFrame* frame);
-  virtual void DidFailProvisionalLoadWithError(
-      WebView* webview,
-      const WebKit::WebURLError& error,
-      WebKit::WebFrame* frame);
   virtual void LoadNavigationErrorPage(
       WebKit::WebFrame* frame,
       const WebKit::WebURLRequest& failed_request,
       const WebKit::WebURLError& error,
       const std::string& html,
       bool replace);
-  virtual void DidReceiveDocumentData(WebKit::WebFrame* frame, const char* data,
-                                      size_t data_len);
-  virtual void DidCommitLoadForFrame(WebView* webview, WebKit::WebFrame* frame,
-                                     bool is_new_navigation);
-  virtual void DidReceiveTitle(WebView* webview,
-                               const std::wstring& title,
-                               WebKit::WebFrame* frame);
-  virtual void DidFinishLoadForFrame(WebView* webview,
-                                     WebKit::WebFrame* frame);
-  virtual void DidFailLoadWithError(WebView* webview,
-                                    const WebKit::WebURLError& error,
-                                    WebKit::WebFrame* forFrame);
-  virtual void DidFinishDocumentLoadForFrame(WebView* webview,
-                                             WebKit::WebFrame* frame);
-  virtual bool DidLoadResourceFromMemoryCache(
-      WebView* webview,
-      const WebKit::WebURLRequest& request,
-      const WebKit::WebURLResponse& response,
-      WebKit::WebFrame* frame);
-  virtual void DidHandleOnloadEventsForFrame(WebView* webview,
-                                             WebKit::WebFrame* frame);
-  virtual void DidChangeLocationWithinPageForFrame(WebView* webview,
-                                                   WebKit::WebFrame* frame,
-                                                   bool is_new_navigation);
-  virtual void DidContentsSizeChange(WebKit::WebWidget* webwidget,
-                                     int new_width,
-                                     int new_height);
-  virtual void DidCompleteClientRedirect(WebView* webview,
-                                         WebKit::WebFrame* frame,
-                                         const GURL& source);
-  virtual void WillCloseFrame(WebView* webview, WebKit::WebFrame* frame);
-  virtual void WillSubmitForm(WebView* webview, WebKit::WebFrame* frame,
-                              const WebKit::WebForm& form);
-  virtual void WillSendRequest(WebKit::WebFrame* webframe,
-                               uint32 identifier,
-                               WebKit::WebURLRequest* request,
-                               const WebKit::WebURLResponse& redirect_response);
-  virtual void DidReceiveResponse(WebKit::WebFrame* webframe,
-                                  uint32 identifier,
-                                  const WebKit::WebURLResponse& response);
-  virtual void DidFinishLoading(WebKit::WebFrame* webframe, uint32 identifier);
-  virtual void WindowObjectCleared(WebKit::WebFrame* webframe);
-  virtual void DocumentElementAvailable(WebKit::WebFrame* webframe);
   virtual void DidCreateScriptContextForFrame(WebKit::WebFrame* webframe);
   virtual void DidDestroyScriptContextForFrame(WebKit::WebFrame* webframe);
   virtual void DidCreateIsolatedScriptContext(WebKit::WebFrame* webframe);
-  virtual WebKit::WebNavigationPolicy PolicyForNavigationAction(
-      WebView* webview,
-      WebKit::WebFrame* frame,
-      const WebKit::WebURLRequest& request,
-      WebKit::WebNavigationType type,
-      WebKit::WebNavigationPolicy default_policy,
-      bool is_redirect);
   virtual WebView* CreateWebView(WebView* webview,
                                  bool user_gesture,
                                  const GURL& creator_url);
@@ -268,18 +209,9 @@ class RenderView : public RenderWidget,
   virtual WebKit::WebWidget* CreatePopupWidgetWithInfo(
       WebView* webview,
       const WebKit::WebPopupMenuInfo& info);
-  virtual WebKit::WebPlugin* CreatePlugin(
-      WebKit::WebFrame* frame,
-      const WebKit::WebPluginParams& params);
-  virtual WebKit::WebWorker* CreateWebWorker(WebKit::WebWorkerClient* client);
-  virtual WebKit::WebMediaPlayer* CreateWebMediaPlayer(
-      WebKit::WebMediaPlayerClient* client);
   virtual void OnMissingPluginStatus(
       WebPluginDelegateProxy* delegate,
       int status);
-  virtual void OpenURL(WebView* webview, const GURL& url,
-                       const GURL& referrer,
-                       WebKit::WebNavigationPolicy policy);
   virtual void DidDownloadImage(int id,
                                 const GURL& image_url,
                                 bool errored,
@@ -303,7 +235,6 @@ class RenderView : public RenderWidget,
                              const WebKit::WebDragData& drag_data,
                              WebKit::WebDragOperationsMask operations_mask);
   virtual void TakeFocus(WebView* webview, bool reverse);
-  virtual void JSOutOfMemory();
   virtual void NavigateBackForwardSoon(int offset);
   virtual int GetHistoryBackListCount();
   virtual int GetHistoryForwardListCount();
@@ -361,6 +292,74 @@ class RenderView : public RenderWidget,
   virtual void didChangeContents() {}
   virtual void didExecuteCommand(const WebKit::WebString& command_name);
   virtual void didEndEditing() {}
+
+  // WebKit::WebFrameClient
+  virtual WebKit::WebPlugin* createPlugin(
+      WebKit::WebFrame* frame, const WebKit::WebPluginParams& params);
+  virtual WebKit::WebWorker* createWorker(
+      WebKit::WebFrame* frame, WebKit::WebWorkerClient* client);
+  virtual WebKit::WebMediaPlayer* createMediaPlayer(
+      WebKit::WebFrame* frame, WebKit::WebMediaPlayerClient* client);
+  virtual void willClose(WebKit::WebFrame* frame);
+  virtual void loadURLExternally(
+      WebKit::WebFrame* frame, const WebKit::WebURLRequest& request,
+      WebKit::WebNavigationPolicy policy);
+  virtual WebKit::WebNavigationPolicy decidePolicyForNavigation(
+      WebKit::WebFrame* frame, const WebKit::WebURLRequest& request,
+      WebKit::WebNavigationType type,
+      WebKit::WebNavigationPolicy default_policy, bool is_redirect);
+  virtual void willSubmitForm(WebKit::WebFrame* frame,
+      const WebKit::WebForm& form);
+  virtual void willPerformClientRedirect(
+      WebKit::WebFrame* frame, const WebKit::WebURL& from,
+      const WebKit::WebURL& to, double interval, double fire_time);
+  virtual void didCancelClientRedirect(WebKit::WebFrame* frame);
+  virtual void didCompleteClientRedirect(
+      WebKit::WebFrame* frame, const WebKit::WebURL& from);
+  virtual void didCreateDataSource(
+      WebKit::WebFrame* frame, WebKit::WebDataSource* datasource);
+  virtual void didStartProvisionalLoad(WebKit::WebFrame* frame);
+  virtual void didReceiveServerRedirectForProvisionalLoad(
+      WebKit::WebFrame* frame);
+  virtual void didFailProvisionalLoad(
+      WebKit::WebFrame* frame, const WebKit::WebURLError& error);
+  virtual void didReceiveDocumentData(
+      WebKit::WebFrame* frame, const char* data, size_t length,
+      bool& prevent_default);
+  virtual void didCommitProvisionalLoad(
+      WebKit::WebFrame* frame, bool is_new_navigation);
+  virtual void didClearWindowObject(WebKit::WebFrame* frame);
+  virtual void didCreateDocumentElement(WebKit::WebFrame* frame);
+  virtual void didReceiveTitle(
+      WebKit::WebFrame* frame, const WebKit::WebString& title);
+  virtual void didFinishDocumentLoad(WebKit::WebFrame* frame);
+  virtual void didHandleOnloadEvents(WebKit::WebFrame* frame);
+  virtual void didFailLoad(
+      WebKit::WebFrame* frame, const WebKit::WebURLError& error);
+  virtual void didFinishLoad(WebKit::WebFrame* frame);
+  virtual void didChangeLocationWithinPage(
+      WebKit::WebFrame* frame, bool is_new_navigation);
+  virtual void assignIdentifierToRequest(
+      WebKit::WebFrame* frame, unsigned identifier,
+      const WebKit::WebURLRequest& request);
+  virtual void willSendRequest(
+      WebKit::WebFrame* frame, unsigned identifier,
+      WebKit::WebURLRequest& request,
+      const WebKit::WebURLResponse& redirect_response);
+  virtual void didReceiveResponse(
+      WebKit::WebFrame* frame, unsigned identifier,
+      const WebKit::WebURLResponse& response);
+  virtual void didFinishResourceLoad(
+      WebKit::WebFrame* frame, unsigned identifier);
+  virtual void didFailResourceLoad(
+      WebKit::WebFrame* frame, unsigned identifier,
+      const WebKit::WebURLError& error);
+  virtual void didLoadResourceFromMemoryCache(
+      WebKit::WebFrame* frame, const WebKit::WebURLRequest& request,
+      const WebKit::WebURLResponse&);
+  virtual void didExhaustMemoryAvailableForScript(WebKit::WebFrame* frame);
+  virtual void didChangeContentsSize(
+      WebKit::WebFrame* frame, const WebKit::WebSize& size);
 
   // webkit_glue::WebPluginPageDelegate
   virtual webkit_glue::WebPluginDelegate* CreatePluginDelegate(
@@ -463,7 +462,7 @@ class RenderView : public RenderWidget,
             int32 routing_id);
 
   void UpdateURL(WebKit::WebFrame* frame);
-  void UpdateTitle(WebKit::WebFrame* frame, const std::wstring& title);
+  void UpdateTitle(WebKit::WebFrame* frame, const string16& title);
   void UpdateSessionHistory(WebKit::WebFrame* frame);
 
   // Update current main frame's encoding and send it to browser window.
@@ -481,6 +480,9 @@ class RenderView : public RenderWidget,
   // finally get right encoding of page.
   void UpdateEncoding(WebKit::WebFrame* frame,
                       const std::string& encoding_name);
+
+  void OpenURL(const GURL& url, const GURL& referrer,
+               WebKit::WebNavigationPolicy policy);
 
   // Captures the thumbnail and text contents for indexing for the given load
   // ID. If the view's load ID is different than the parameter, this call is

chrome/renderer/render_view_unittest.cc
@@ -863,9 +863,8 @@ TEST_F(RenderViewTest, DidFailProvisionalLoadWithErrorForCancellation) {
   error.reason = net::ERR_ABORTED;
   error.unreachableURL = GURL("http://foo");
   WebFrame* web_frame = GetMainFrame();
-  WebView* web_view = web_frame->view();
   // A cancellation occurred.
-  view_->DidFailProvisionalLoadWithError(web_view, error, web_frame);
+  view_->didFailProvisionalLoad(web_frame, error);
   // Frame should stay in view-source mode.
   EXPECT_TRUE(web_frame->isViewSourceModeEnabled());
 }

commit 08b0c54762293a39863adb242a823d26a74889c2

Restore support for loading URLs on the command line.
I broke this in r25841.

The fix makes TestShell support both URLs as well as file paths
on the command line.

R=dglazkov
BUG=none
TEST=none

Review URL: http://codereview.chromium.org/204007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26145 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: a0d2c0cc0e816e3f054a54dc22a2e000440f0077

webkit/tools/test_shell/test_shell_main.cc
@@ -170,20 +170,30 @@ int main(int argc, char* argv[]) {
   }
 
   // Treat the first loose value as the initial URL to open.
-  FilePath uri;
+  GURL starting_url;
 
   // Default to a homepage if we're interactive.
   if (!layout_test_mode) {
-    PathService::Get(base::DIR_SOURCE_ROOT, &uri);
-    uri = uri.AppendASCII("webkit");
-    uri = uri.AppendASCII("data");
-    uri = uri.AppendASCII("test_shell");
-    uri = uri.AppendASCII("index.html");
+    FilePath path;
+    PathService::Get(base::DIR_SOURCE_ROOT, &path);
+    path = path.AppendASCII("webkit");
+    path = path.AppendASCII("data");
+    path = path.AppendASCII("test_shell");
+    path = path.AppendASCII("index.html");
+    starting_url = net::FilePathToFileURL(path);
   }
 
   std::vector<std::wstring> loose_values = parsed_command_line.GetLooseValues();
-  if (loose_values.size() > 0)
-    uri = FilePath::FromWStringHack(loose_values[0]);
+  if (loose_values.size() > 0) {
+    GURL url(WideToUTF16Hack(loose_values[0]));
+    if (url.is_valid()) {
+      starting_url = url;
+    } else {
+      // Treat as a file path
+      starting_url =
+          net::FilePathToFileURL(FilePath::FromWStringHack(loose_values[0]));
+    }
+  }
 
   std::wstring js_flags =
     parsed_command_line.GetSwitchValue(test_shell::kJavaScriptFlags);
@@ -208,7 +218,7 @@ int main(int argc, char* argv[]) {
   StatsTable::set_current(table);
 
   TestShell* shell;
-  if (TestShell::CreateNewWindow(net::FilePathToFileURL(uri), &shell)) {
+  if (TestShell::CreateNewWindow(starting_url, &shell)) {
     if (record_mode || playback_mode) {
       platform.SetWindowPositionForRecording(shell);
       WebKit::registerExtension(extensions_v8::PlaybackExtension::Get());
@@ -255,7 +265,7 @@ int main(int argc, char* argv[]) {
           params.dump_tree = false;
       }
 
-      if (uri.empty()) {
+      if (!starting_url.is_valid()) {
         // Watch stdin for URLs.
         char filenameBuffer[kPathBufSize];
         while (fgets(filenameBuffer, sizeof(filenameBuffer), stdin)) {
@@ -297,7 +307,7 @@ int main(int argc, char* argv[]) {
       } else {
         // TODO(ojan): Provide a way for run-singly tests to pass
         // in a hash and then set params.pixel_hash here.
-        params.test_url = WideToUTF8(uri.ToWStringHack()).c_str();
+        params.test_url = starting_url.spec();
         TestShell::RunFileTest(params);
       }
 