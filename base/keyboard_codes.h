3b97b25152c378f64117bc4708809a79e4f2af71
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-22 01:01:08 +0000

    Revert 26756 - "Use windows keycodes under linux (and all non-windows platforms)."
    Caused BrowserFocusTest.FocusTraversal failures on Linux in interactive_ui_tests.
    
    TBR=erg
    
    BUG=22551
    TEST=Linux interactive_ui_tests
    
    Review URL: http://codereview.chromium.org/212045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@26777 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a032e66e5dc93334927ca2b1e1e8afb1ce934b15

--- base/keyboard_codes.h
+++ base/keyboard_codes.h
@@ -9,8 +9,10 @@
 
 #if defined(OS_WIN)
 #include "base/keyboard_codes_win.h"
-#elif defined(USE_X11) || defined(OS_MACOSX)
-#include "base/keyboard_codes_posix.h"
+#elif defined(USE_X11)
+#include "base/keyboard_codes_linux.h"
+#elif defined(OS_MACOSX)
+#include "base/keyboard_codes_mac.h"
 #endif
 
 #endif  // BASE_KEYBOARD_CODES_H_
