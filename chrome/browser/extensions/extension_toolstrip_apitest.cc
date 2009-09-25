a10ea503d4a5f19a54579822627c81be43f3fa87
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-25 23:04:40 +0000

    Enable ExtensionApiTest.Toolstrip
    Need debug output from http://codereview.chromium.org/242012
    
    BUG=22668
    
    Review URL: http://codereview.chromium.org/246011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27277 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c2b99a98d9b2e7de9ebc48ab0b72608145613576

--- chrome/browser/extensions/extension_toolstrip_apitest.cc
+++ chrome/browser/extensions/extension_toolstrip_apitest.cc
@@ -6,6 +6,6 @@
 
 // TODO(rafaelw,erikkay) disabled due to flakiness
 // BUG=22668 (probably the same bug)
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Toolstrip) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Toolstrip) {
   ASSERT_TRUE(RunExtensionTest("toolstrip")) << message_;
 }
