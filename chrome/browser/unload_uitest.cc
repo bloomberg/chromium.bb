39d8979af779d6afb9120fe562490a28887ece62
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-09-25 16:09:05 +0000

    Re-enable UnloadTest.BrowserCloseBeforeUnload* tests now that they are passing again.
    The http://trac.webkit.org/changeset/48732 fixed it.
    
    TBR=senorblanco
    TEST=none
    BUG=22696
    
    Review URL: http://codereview.chromium.org/242006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27192 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bd9a0b8154e618d3bc8e1c1a0259a67d44934e28

--- chrome/browser/unload_uitest.cc
+++ chrome/browser/unload_uitest.cc
@@ -240,7 +240,7 @@ TEST_F(UnloadTest, BrowserCloseUnload) {
 // Tests closing the browser with a beforeunload handler and clicking
 // OK in the beforeunload confirm dialog.
 #if !defined(OS_LINUX)
-TEST_F(UnloadTest, DISABLED_BrowserCloseBeforeUnloadOK) {
+TEST_F(UnloadTest, BrowserCloseBeforeUnloadOK) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   NavigateToDataURL(BEFORE_UNLOAD_HTML, L"beforeunload");
 
@@ -252,7 +252,7 @@ TEST_F(UnloadTest, DISABLED_BrowserCloseBeforeUnloadOK) {
 
 // Tests closing the browser with a beforeunload handler and clicking
 // CANCEL in the beforeunload confirm dialog.
-TEST_F(UnloadTest, DISABLED_BrowserCloseBeforeUnloadCancel) {
+TEST_F(UnloadTest, BrowserCloseBeforeUnloadCancel) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   NavigateToDataURL(BEFORE_UNLOAD_HTML, L"beforeunload");
 
