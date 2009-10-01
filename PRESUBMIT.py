363d28f5971c80df414893dd464e9e8a89635648
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-01 08:11:51 +0000

    Disable CheckSvnModifiedDirectories for now. It doesn't work well in third_party.
    BUG=none
    TEST=none
    TBR=maruel
    Review URL: http://codereview.chromium.org/246055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27710 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dd805fef306e6ee0d6bade640ea3689b10e64bc7

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -60,8 +60,9 @@ def CheckChangeOnCommit(input_api, output_api):
       input_api, output_api, sources))
   results.extend(input_api.canned_checks.CheckSvnForCommonMimeTypes(
       input_api, output_api))
-  results.extend(input_api.canned_checks.CheckSvnModifiedDirectories(
-      input_api, output_api, sources))
+  # TODO(thestig) temporarily disabled, doesn't work in third_party/
+  #results.extend(input_api.canned_checks.CheckSvnModifiedDirectories(
+  #    input_api, output_api, sources))
   # Make sure the tree is 'open'.
   # TODO(maruel): Run it in a separate thread to parallelize checks?
   results.extend(CheckTreeIsOpen(
