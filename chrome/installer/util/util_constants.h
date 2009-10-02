78682db7c3084a964aad1139ef7f2f9033e7093e
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-02 19:09:52 +0000

    Improving Chrome Frame uninstall process:
    1) Don't kill chrome.exe when uninstalling. This leads to immediate breakage of any running Chrome Frame windows and is also annoying if the user also happens to be using Google Chrome or some variant thereof.
    
    2) If files are in use at uninstall time, schedule them for deletion and prompt the user to reboot.
    
    3) Remember to remove any pending scheduled deletes at install time.
    
    
    Review URL: http://codereview.chromium.org/235060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27876 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c45d289dcd3209903c22afd8fcb546b417dabddf

--- chrome/installer/util/util_constants.h
+++ chrome/installer/util/util_constants.h
@@ -39,7 +39,8 @@ enum InstallStatus {
   EULA_REJECTED,         // EULA dialog was not accepted by user.
   EULA_ACCEPTED,         // EULA dialog was accepted by user.
   EULA_ACCEPTED_OPT_IN,  // EULA accepted wtih the crash optin selected.
-  INSTALL_DIR_IN_USE     // Installation directory is in use by another process
+  INSTALL_DIR_IN_USE,    // Installation directory is in use by another process
+  UNINSTALL_REQUIRES_REBOOT // Uninstallation required a reboot.
 };
 
 namespace switches {
