28bcb042e98ab85a854de835ad4b73d804e40180
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-02 07:02:10 +0000

    Disable slow termination ui test because it still fails on windows.
    I RDPed to a buildbot and it ran fine. So I think the problem is certain automation messages not working in plain ui tests. I will try putting this in an automated interactive ui test.
    
    TBR=nsylvain
    
    Review URL: http://codereview.chromium.org/242121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@27834 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c1a776f3018116216eef873d2692ca75d9c3ead0

--- chrome/test/ui/fast_shutdown_uitest.cc
+++ chrome/test/ui/fast_shutdown_uitest.cc
@@ -13,13 +13,19 @@
 #include "chrome/test/automation/window_proxy.h"
 #include "views/event.h"
 
+#if defined(OS_WIN)
+#define MAYBE_SlowTermination DISABLED_SlowTermination
+#else
+#define MAYBE_SlowTermination SlowTermination
+#endif
+
 class FastShutdown : public UITest {
 };
 
 // This tests for a previous error where uninstalling an onbeforeunload
 // handler would enable fast shutdown even if an onUnload handler still
 // existed.
-TEST_F(FastShutdown, SlowTermination) {
+TEST_F(FastShutdown, MAYBE_SlowTermination) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
   scoped_refptr<WindowProxy> window(browser->GetWindow());
