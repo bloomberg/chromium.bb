2d909be324ff899409d12c9256e7ea1351e9db8f
mmoss@chromium.org <mmoss@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2009-10-06 22:30:49 +0000

    Test proper build output path for make builds. Review URL: http://codereview.chromium.org/255097
    git-svn-id: http://gyp.googlecode.com/svn/trunk@681 78cadc50-ecff-11dd-a971-7dbc132099af

--- test/copies/gyptest-all.py
+++ test/copies/gyptest-all.py
@@ -20,6 +20,8 @@ test.must_match(['relocate', 'src', 'copies-out', 'file1'], "file1 contents\n")
 
 if sys.platform in ('darwin',):
   file2 = ['relocate', 'src', 'build', 'Default', 'copies-out', 'file2']
+elif test.format in ('make',):
+  file2 = ['relocate', 'src', 'out', 'Default', 'copies-out', 'file2']
 else:
   file2 = ['relocate', 'src', 'Default', 'copies-out', 'file2']
 test.must_match(file2, "file2 contents\n")
