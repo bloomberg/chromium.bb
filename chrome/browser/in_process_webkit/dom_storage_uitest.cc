1ff561fe0a9e95678666489f9be836b4c45e6f3b
jorlow@chromium.org <jorlow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-06 08:24:38 +0000

    Revert.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28098 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 27b69ce8a3aa1c70a0a91a02eb3ba888846951f6

--- chrome/browser/in_process_webkit/dom_storage_uitest.cc
+++ chrome/browser/in_process_webkit/dom_storage_uitest.cc
@@ -2,6 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// TODO(jorlow): Reenable when https://bugs.webkit.org/show_bug.cgi?id=28094
+//               is fixed.  Until then, this will cause crashes even if the
+//               individual tests are disabled.
+#if 0
+
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/ui/ui_layout_test.h"
 
@@ -14,7 +19,7 @@
 // TODO(jorlow): Enable these tests when we remove them from the
 //               test_exceptions.txt file.
 static const char* kSubDirFiles[] = {
-  //"clear.html",
+  "clear.html",
   "delete-removal.html",
   "enumerate-storage.html",
   "enumerate-with-length-and-key.html",
@@ -26,7 +31,7 @@ static const char* kSubDirFiles[] = {
   //"simple-events.html",
   "simple-usage.html",
   //"string-conversion.html",
-  //"window-open.html"
+//  "window-open.html"
 };
 
 class DOMStorageTest : public UILayoutTest {
@@ -71,3 +76,5 @@ TEST_F(DOMStorageTest, SessionStorageLayoutTests) {
   for (size_t i=0; i<arraysize(kSubDirFiles); ++i)
     RunLayoutTest(kSubDirFiles[i], false);
 }
+
+#endif
