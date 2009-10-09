21172852a9bf52885e0eb2f84e71319cef1a9b23
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 04:30:20 +0000

    whee
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28523 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b9885f901bc93a6b31caa0c89128d841a29819a9

--- chrome/browser/extensions/gtk_theme_installed_infobar_delegate.cc
+++ chrome/browser/extensions/gtk_theme_installed_infobar_delegate.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/extensions/gtk_theme_preview_infobar_delegate.h"
+#include "chrome/browser/extensions/gtk_theme_installed_infobar_delegate.h"
 
 #include "chrome/browser/profile.h"
 
