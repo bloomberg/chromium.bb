72ca87de06e118f15e6286a3c70c92e302094623
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-09 19:28:16 +0000

    Fix include.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28583 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 33be20b3c8285d5a6ce200e6ed024c4e53f8fe6f

--- chrome/installer/util/run_all_unittests.cc
+++ chrome/installer/util/run_all_unittests.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/test_suite.h"
+#include "base/test/test_suite.h"
 #include "chrome/common/chrome_paths.h"
 
 int main(int argc, char** argv) {
