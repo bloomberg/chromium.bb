90d66914e7de66c9c295fc9531c82018343b2de6
bsy@google.com <bsy@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2009-10-13 22:57:53 +0000

    export nacl_abi_* version of headers.
    Review URL: http://codereview.chromium.org/269069
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@855 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

--- src/trusted/service_runtime/export_header.py
+++ src/trusted/service_runtime/export_header.py
@@ -1,87 +1,79 @@
 #!/usr/bin/python
 #
-# Copyright 2008, Google Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#     * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
-# copyright notice, this list of conditions and the following disclaimer
-# in the documentation and/or other materials provided with the
-# distribution.
-#     * Neither the name of Google Inc. nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# Copyright 2008, 2009, The Native Client Authors.  All rights reserved.
+# Use of this source code is governed by a BSD-style license that can
+# be found in the LICENSE file.
 
 
-"""Tools for exporting NativeClient ABI header files.
+"""Tools for exporting Native Client ABI header files.
 
-This module is used to export NativeClient ABI header files -- which are in
-the native_client/src/trusted/service_runtime/include directory -- for use for NativeClient
-applications compilation.
+This module is used to export Native Client ABI header files -- which
+are in the native_client/src/trusted/service_runtime/include directory
+-- for use with the SDK and newlib to compile NaCl applications.
 """
 
 import os
 import re
 import sys
 
-def ProcessStreams(instr, outstr):
-  """Read internal version of header file from instr, write exported version
-  to outstr.
-  """
+
+UNMODIFIED_DIR=['nacl','abi']
+
+
+def ProcessStream(instr, outstr):
+  """Read internal version of header file from instr, write exported
+  version to outstr.  The two transformations are to remove nacl_abi_
+  prefixes (in its various incarnations), and to change include
+  directives from the Google coding style
+  "native_client/include/foo/bar.h" to <foo/bar.h>, and from
+  "native_client/src/trusted/service_runtime/include/baz/quux.h" to
+  <baz/quux.h>."""
 
   pat = r'\b(?:nacl_abi_|NaClAbi|NACL_ABI_)([A-Za-z0-9_]*)'
   cpat = re.compile(pat)
 
-  inc = r'^#\s*include\s+"native_client(?:/src/trusted/service_runtime)?/include/([^"]*)"'
-  incpat = re.compile(inc)
+  inc = (r'^#\s*include\s+"native_client(?:/src/trusted/service_runtime)?'+
+         r'/include/([^"]*)"')
+  cinc = re.compile(inc)
 
-  for str in instr:
-    if incpat.search(str):
-      print >>outstr, incpat.sub(r'#include <\1>', str)
+  for line in instr:
+    if cinc.search(line):
+      print >>outstr, cinc.sub(r'#include <\1>', line)
     else:
-      print >>outstr, cpat.sub(r'\1', str),
+      print >>outstr, cpat.sub(r'\1', line),
     # endif
   # endfor
 # enddef
 
-def ProcessFileList(flist):
-  for f in flist:
-    ProcessStreams(open(f), sys.stdout);
+
+def CopyStream(instr, outstr):
+  for line in instr:
+    outstr.write(line)
   # endfor
 # enddef
 
-def ProcessDir(srcdir, dstdir):
+
+def ProcessDir(srcdir, dstdir, unmodified_dstdir):
   if not os.path.isdir(srcdir):
     return
   # endif
   if not os.path.isdir(dstdir):
     os.makedirs(dstdir)
   # endif
+  if not os.path.isdir(unmodified_dstdir):
+    os.makedirs(unmodified_dstdir)
+  # endif
   for fn in os.listdir(srcdir):
     srcpath = os.path.join(srcdir, fn)
     dstpath = os.path.join(dstdir, fn)
+    undstpath = os.path.join(unmodified_dstdir, fn)
     if os.path.isfile(srcpath) and fn.endswith('.h'):
-      ProcessStreams(open(srcpath),
-                     open(dstpath, 'w'));
+      ProcessStream(open(srcpath),
+                    open(dstpath, 'w'))
+      CopyStream(open(srcpath),
+                 open(undstpath, 'w'))
     elif os.path.isdir(srcpath):
-      ProcessDir(srcpath, dstpath)
+      ProcessDir(srcpath, dstpath, undstpath)
     # endif
   # endfor
 # enddef
@@ -92,7 +84,8 @@ def main(argv):
                          ' dest/include/path')
     return 1
   # endif
-  ProcessDir(argv[1], argv[2]);
+  ProcessDir(argv[1], argv[2],
+             reduce(os.path.join, UNMODIFIED_DIR, argv[2]));
   return 0
 # enddef
 
