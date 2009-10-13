2bcb6deb37449c4d8b8bd0cd98f8a7975ddcbbad
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-13 22:56:53 +0000

    Empty files to kick off the diagnostic mode
    - Main entry point
    - sqlite error handler
    
    BUG=none
    TEST=none
    
    TBR=huanr
    
    
    Review URL: http://codereview.chromium.org/274033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28908 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e9403ab1516c5ada016249c1c124485640c2ae8f

---
+++ chrome/browser/diagnostics/sqlite_diagnostics.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_DIAGNOSTICS_SQLITE_DIAGNOSTICS_H_
+#define CHROME_BROWSER_DIAGNOSTICS_SQLITE_DIAGNOSTICS_H_
+
+
+#endif  // CHROME_BROWSER_DIAGNOSTICS_SQLITE_DIAGNOSTICS_H_
