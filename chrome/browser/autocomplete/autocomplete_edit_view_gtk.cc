a217ba6edbfea3533a2d7a8be76e07ec0ee78839
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-13 02:06:57 +0000

    Gtk: draw the secure scheme color over the correct char range.
    BUG=none
    TEST=go to gmail.com
    
    Review URL: http://codereview.chromium.org/272028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@28777 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3068566587d45eab5e6ffcb4ec737fe4838b9e13

--- chrome/browser/autocomplete/autocomplete_edit_view_gtk.cc
+++ chrome/browser/autocomplete/autocomplete_edit_view_gtk.cc
@@ -1199,13 +1199,15 @@ void AutocompleteEditViewGtk::EmphasizeURLComponents() {
   // Emphasize the scheme for security UI display purposes (if necessary).
   if (!model_->user_input_in_progress() && parts.scheme.is_nonempty() &&
       (scheme_security_level_ != ToolbarModel::NORMAL)) {
+    CharRange scheme_range = CharRange(GetUTF8Offset(text, parts.scheme.begin),
+                                       GetUTF8Offset(text, parts.scheme.end()));
+    ItersFromCharRange(scheme_range, &start, &end);
+
     if (scheme_security_level_ == ToolbarModel::SECURE) {
       gtk_text_buffer_apply_tag(text_buffer_, secure_scheme_tag_,
                                 &start, &end);
     } else {
-      strikethrough_ = CharRange(GetUTF8Offset(text, parts.scheme.begin),
-                                 GetUTF8Offset(text, parts.scheme.end()));
-      ItersFromCharRange(strikethrough_, &start, &end);
+      strikethrough_ = scheme_range;
       // When we draw the strikethrough, we don't want to include the ':' at the
       // end of the scheme.
       strikethrough_.cp_max--;
