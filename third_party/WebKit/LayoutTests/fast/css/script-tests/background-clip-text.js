bloomberg/chromium.bb

commit 5310181ac07d414369034d3e5116661c5ccdb249

WebCore:
2009-07-22  David Hyatt  <hyatt@apple.com>

        Reviewed by Beth Dakin.

        https://bugs.webkit.org/show_bug.cgi?id=27562
        Add the finalized versions of background-clip and background-origin.  Remove background-clip from
        the background shorthand and have it be auto-set based off background-origin's value.

        Three new tests added in fast/backgrounds/size

        * css/CSSComputedStyleDeclaration.cpp:
        (WebCore::):
        (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
        * css/CSSMutableStyleDeclaration.cpp:
        (WebCore::CSSMutableStyleDeclaration::getPropertyValue):
        * css/CSSParser.cpp:
        (WebCore::CSSParser::parseValue):
        (WebCore::CSSParser::parseFillShorthand):
        (WebCore::CSSParser::parseFillProperty):
        * css/CSSPropertyLonghand.cpp:
        (WebCore::initShorthandMap):
        * css/CSSPropertyNames.in:
        * css/CSSStyleSelector.cpp:
        (WebCore::CSSStyleSelector::applyProperty):
        * css/CSSValueKeywords.in:

LayoutTests:

2009-07-22  David Hyatt  <hyatt@apple.com>

        Reviewed by Beth Dakin.

        https://bugs.webkit.org/show_bug.cgi?id=27562
        Add some new layout tests that drop the prefix from the background properties and use origin
        in a shorthand.  Update shorthand results and computed style results to account for new properties.

        * fast/backgrounds/size/backgroundSize20.html: Added.
        * fast/backgrounds/size/backgroundSize21.html: Added.
        * fast/backgrounds/size/backgroundSize22.html: Added.
        * fast/css/background-position-serialize-expected.txt:
        * fast/css/remove-shorthand-expected.txt:
        * platform/mac/fast/backgrounds/size/backgroundSize20-expected.checksum: Added.
        * platform/mac/fast/backgrounds/size/backgroundSize20-expected.png: Added.
        * platform/mac/fast/backgrounds/size/backgroundSize20-expected.txt: Added.

third_party/WebKit/WebCore/css/CSSParser.cpp
@@ -859,9 +859,11 @@ bool CSSParser::parseValue(int propId, bool important)
     }
 
     case CSSPropertyBackgroundAttachment:
+    case CSSPropertyBackgroundClip:
     case CSSPropertyWebkitBackgroundClip:
     case CSSPropertyWebkitBackgroundComposite:
     case CSSPropertyBackgroundImage:
+    case CSSPropertyBackgroundOrigin:
     case CSSPropertyWebkitBackgroundOrigin:
     case CSSPropertyBackgroundPosition:
     case CSSPropertyBackgroundPositionX:
@@ -1507,15 +1509,15 @@ bool CSSParser::parseValue(int propId, bool important)
         // in quirks mode but it's usually the X coordinate of a position.
         // FIXME: Add CSSPropertyWebkitBackgroundSize to the shorthand.
         const int properties[] = { CSSPropertyBackgroundImage, CSSPropertyBackgroundRepeat, 
-                                   CSSPropertyBackgroundAttachment, CSSPropertyBackgroundPosition, CSSPropertyWebkitBackgroundClip,
-                                   CSSPropertyWebkitBackgroundOrigin, CSSPropertyBackgroundColor };
-        return parseFillShorthand(propId, properties, 7, important);
+                                   CSSPropertyBackgroundAttachment, CSSPropertyBackgroundPosition, CSSPropertyBackgroundOrigin, 
+                                   CSSPropertyBackgroundColor };
+        return parseFillShorthand(propId, properties, 6, important);
     }
     case CSSPropertyWebkitMask: {
         const int properties[] = { CSSPropertyWebkitMaskImage, CSSPropertyWebkitMaskRepeat, 
-                                   CSSPropertyWebkitMaskAttachment, CSSPropertyWebkitMaskPosition, CSSPropertyWebkitMaskClip,
+                                   CSSPropertyWebkitMaskAttachment, CSSPropertyWebkitMaskPosition,
                                    CSSPropertyWebkitMaskOrigin };
-        return parseFillShorthand(propId, properties, 6, important);
+        return parseFillShorthand(propId, properties, 5, important);
     }
     case CSSPropertyBorder:
         // [ 'border-width' || 'border-style' || <color> ] | inherit
@@ -1691,6 +1693,7 @@ bool CSSParser::parseFillShorthand(int propId, const int* properties, int numPro
 
     bool parsedProperty[cMaxFillProperties] = { false };
     RefPtr<CSSValue> values[cMaxFillProperties];
+    RefPtr<CSSValue> clipValue;
     RefPtr<CSSValue> positionYValue;
     int i;
 
@@ -1709,6 +1712,10 @@ bool CSSParser::parseFillShorthand(int propId, const int* properties, int numPro
                     addFillValue(values[i], CSSInitialValue::createImplicit());
                     if (properties[i] == CSSPropertyBackgroundPosition || properties[i] == CSSPropertyWebkitMaskPosition)
                         addFillValue(positionYValue, CSSInitialValue::createImplicit());
+                    if ((properties[i] == CSSPropertyBackgroundOrigin || properties[i] == CSSPropertyWebkitMaskOrigin) && !parsedProperty[i]) {
+                        // If background-origin wasn't present, then reset background-clip also.
+                        addFillValue(clipValue, CSSInitialValue::createImplicit());
+                    }
                 }
                 parsedProperty[i] = false;
             }
@@ -1727,6 +1734,13 @@ bool CSSParser::parseFillShorthand(int propId, const int* properties, int numPro
                     addFillValue(values[i], val1.release());
                     if (properties[i] == CSSPropertyBackgroundPosition || properties[i] == CSSPropertyWebkitMaskPosition)
                         addFillValue(positionYValue, val2.release());
+                    if (properties[i] == CSSPropertyBackgroundOrigin || properties[i] == CSSPropertyWebkitMaskOrigin) {
+                        // Reparse the value as a clip, and see if we succeed.
+                        if (parseFillProperty(CSSPropertyBackgroundClip, propId1, propId2, val1, val2))
+                            addFillValue(clipValue, val1.release()); // The property parsed successfully.
+                        else
+                            addFillValue(clipValue, CSSInitialValue::createImplicit()); // Some value was used for origin that is not supported by clip. Just reset clip instead.
+                    }
                 }
             }
         }
@@ -1743,6 +1757,10 @@ bool CSSParser::parseFillShorthand(int propId, const int* properties, int numPro
             addFillValue(values[i], CSSInitialValue::createImplicit());
             if (properties[i] == CSSPropertyBackgroundPosition || properties[i] == CSSPropertyWebkitMaskPosition)
                 addFillValue(positionYValue, CSSInitialValue::createImplicit());
+            if ((properties[i] == CSSPropertyBackgroundOrigin || properties[i] == CSSPropertyWebkitMaskOrigin) && !parsedProperty[i]) {
+                // If background-origin wasn't present, then reset background-clip also.
+                addFillValue(clipValue, CSSInitialValue::createImplicit());
+            }
         }
     }
     
@@ -1758,8 +1776,14 @@ bool CSSParser::parseFillShorthand(int propId, const int* properties, int numPro
             addProperty(CSSPropertyWebkitMaskPositionY, positionYValue.release(), important);
         } else
             addProperty(properties[i], values[i].release(), important);
+            
+        // Add in clip values when we hit the corresponding origin property.
+        if (properties[i] == CSSPropertyBackgroundOrigin)
+            addProperty(CSSPropertyBackgroundClip, clipValue.release(), important);
+        else  if (properties[i] == CSSPropertyWebkitMaskOrigin)
+            addProperty(CSSPropertyWebkitMaskClip, clipValue.release(), important);
     }
-    
+
     return true;
 }
 
@@ -2271,10 +2295,24 @@ bool CSSParser::parseFillProperty(int propId, int& propId1, int& propId2,
                 case CSSPropertyWebkitBackgroundOrigin:
                 case CSSPropertyWebkitMaskClip:
                 case CSSPropertyWebkitMaskOrigin:
-                    // The first three values here are deprecated and should not be allowed to apply when we drop the -webkit-
-                    // from the property names.
+                    // The first three values here are deprecated and do not apply to the version of the property that has
+                    // the -webkit- prefix removed.
                     if (val->id == CSSValueBorder || val->id == CSSValuePadding || val->id == CSSValueContent ||
-                        val->id == CSSValueBorderBox || val->id == CSSValuePaddingBox || val->id == CSSValueContentBox || val->id == CSSValueText) {
+                        val->id == CSSValueBorderBox || val->id == CSSValuePaddingBox || val->id == CSSValueContentBox ||
+                        ((propId == CSSPropertyWebkitBackgroundClip || propId == CSSPropertyWebkitMaskClip) &&
+                         (val->id == CSSValueText || val->id == CSSValueWebkitText))) {
+                        currValue = CSSPrimitiveValue::createIdentifier(val->id);
+                        m_valueList->next();
+                    }
+                    break;
+                case CSSPropertyBackgroundClip:
+                    if (val->id == CSSValueBorderBox || val->id == CSSValuePaddingBox || val->id == CSSValueWebkitText) {
+                        currValue = CSSPrimitiveValue::createIdentifier(val->id);
+                        m_valueList->next();
+                    }
+                    break;
+                case CSSPropertyBackgroundOrigin:
+                    if (val->id == CSSValueBorderBox || val->id == CSSValuePaddingBox || val->id == CSSValueContentBox) {
                         currValue = CSSPrimitiveValue::createIdentifier(val->id);
                         m_valueList->next();
                     }

third_party/WebKit/LayoutTests/fast/backgrounds/size/backgroundSize20.html
@@ -1,0 +1,15 @@
+<head>
+<style>
+    div {
+        border: 10px double #000000;
+        padding: 10px;
+        background: url('resources/flower.jpg');
+        -webkit-background-size: 50% auto;
+        background-origin: border-box;
+    }
+</style>
+</head>
+
+<body>
+<div style="width:500px; height:500px;"></div>
+</body>

third_party/WebKit/LayoutTests/fast/backgrounds/size/backgroundSize21.html
@@ -1,0 +1,16 @@
+<head>
+<style>
+    div {
+        border: 10px double #000000;
+        padding: 10px;
+        background: url('resources/flower.jpg');
+        -webkit-background-size: 25% auto;
+        background-position: center left;
+        background-origin: padding-box;
+    }
+</style>
+</head>
+
+<body>
+<div style="width:500px; height:500px;"></div>
+</body>

third_party/WebKit/LayoutTests/fast/backgrounds/size/backgroundSize22.html
@@ -1,0 +1,15 @@
+<head>
+<style>
+    div {
+        border: 10px double #000000;
+        padding: 10px;
+        background: url('resources/flower.jpg') content-box;
+        -webkit-background-size: 100px auto;
+        background-position: center;
+    }
+</style>
+</head>
+
+<body>
+<div style="width:500px; height:500px;"></div>
+</body>

third_party/WebKit/LayoutTests/fast/css/background-position-serialize-expected.txt
@@ -59,4 +59,4 @@ PASS: t.style.cssText should be background-position: 10px 15px, 20px 25px, 30px 
 t.setAttribute('style', 'background: url(about:blank) 80% 80%;');
 t.style.backgroundPositionY = '50px'
 style.cssText =
-background-image: url(about:blank); background-repeat: initial; background-attachment: initial; -webkit-background-clip: initial; -webkit-background-origin: initial; background-color: initial; background-position: 80% 50px;
+background-image: url(about:blank); background-repeat: initial; background-attachment: initial; background-origin: initial; background-clip: initial; background-color: initial; background-position: 80% 50px;

third_party/WebKit/LayoutTests/fast/css/remove-shorthand-expected.txt
@@ -2,7 +2,7 @@ Test for http://bugs.webkit.org/show_bug.cgi?id=9284 Quirksmode (CSS1): Removing
 
 Starting with a declaration containing all properties that are constituents of shortcuts, see what is removed when a shortcut property is removed. The shortcut’s constituents and only them should be removed.
 
-Removing background removes background-image, background-repeat, background-attachment, -webkit-background-clip, -webkit-background-origin, background-color, background-position.
+Removing background removes background-image, background-repeat, background-attachment, background-origin, background-clip, background-color, background-position.
 Removing background-position removes background-position.
 Removing border removes border-top-width, border-right-width, border-bottom-width, border-left-width, border-top-style, border-right-style, border-bottom-style, border-left-style, border-top-color, border-right-color, border-bottom-color, border-left-color.
 Removing border-top removes border-top-width, border-top-style, border-top-color.
@@ -20,7 +20,7 @@ Removing list-style removes list-style-type, list-style-position, list-style-ima
 Removing margin removes margin-top, margin-right, margin-bottom, margin-left.
 Removing -webkit-margin-collapse removes -webkit-margin-top-collapse, -webkit-margin-bottom-collapse.
 Removing -webkit-marquee removes -webkit-marquee-direction, -webkit-marquee-increment, -webkit-marquee-repetition, -webkit-marquee-style, -webkit-marquee-speed.
-Removing -webkit-mask removes -webkit-mask-image, -webkit-mask-repeat, -webkit-mask-attachment, -webkit-mask-position-x, -webkit-mask-position-y, -webkit-mask-clip, -webkit-mask-origin.
+Removing -webkit-mask removes -webkit-mask-image, -webkit-mask-repeat, -webkit-mask-attachment, -webkit-mask-position-x, -webkit-mask-position-y, -webkit-mask-origin, -webkit-mask-clip.
 Removing -webkit-mask-position removes -webkit-mask-position-x, -webkit-mask-position-y.
 Removing overflow removes overflow-x, overflow-y.
 Removing padding removes padding-top, padding-right, padding-bottom, padding-left.

third_party/WebKit/LayoutTests/platform/mac/fast/backgrounds/size/backgroundSize20-expected.checksum
@@ -1,0 +1,1 @@
+d56e6c311fe95a3e21e7558883b45c49

third_party/WebKit/LayoutTests/platform/mac/fast/backgrounds/size/backgroundSize20-expected.txt
@@ -1,0 +1,6 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x600
+  RenderBlock {HTML} at (0,0) size 800x600
+    RenderBody {BODY} at (8,8) size 784x584
+      RenderBlock {DIV} at (0,0) size 540x540 [border: (10px double #000000)]

commit 04d18ffa606b3cc629592c547178af418a43a3af

WebCore: Fixed background-clip parsing regressions introduced in r46240.
Reviewed by Sam Weinig.

Test: fast/css/background-clip-text.html

* css/CSSParser.cpp:
(WebCore::parseBackgroundClip): Added this helper method.
(WebCore::CSSParser::parseFillShorthand): Use parseBackgroundClip() to
reparse the value. The old code called parseFillProperty() again, which
didn’t work, because parseFillProperty() advances the value list.
(WebCore::CSSParser::parseFillProperty): Changed to use
parseBackgroundClip().
* css/CSSPrimitiveValueMappings.h:
(WebCore::CSSPrimitiveValue::operator EFillBox): Added a case for
CSSValueWebkitText.

LayoutTests: Added a background-clip parsing test that would have caught some
regressions that were introduced in r46240.

Reviewed by Sam Weinig.

* fast/css/background-clip-text-expected.txt: Added.
* fast/css/background-clip-text.html: Added.
* fast/css/script-tests: Added.
* fast/css/script-tests/TEMPLATE.html: Copied from LayoutTests/fast/backgrounds/repeat/script-tests/TEMPLATE.html.
* fast/css/script-tests/background-clip-text.js: Added.
(test):



git-svn-id: svn://svn.chromium.org/blink/trunk@49906 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 2eaceb6ee6ed30952f4e34032fc31c49642f5b88

third_party/WebKit/WebCore/css/CSSParser.cpp
@@ -1726,6 +1726,15 @@ void CSSParser::addFillValue(RefPtr<CSSValue>& lval, PassRefPtr<CSSValue> rval)
         lval = rval;
 }
 
+static bool parseBackgroundClip(CSSParserValue* parserValue, RefPtr<CSSValue>& cssValue)
+{
+    if (parserValue->id == CSSValueBorderBox || parserValue->id == CSSValuePaddingBox || parserValue->id == CSSValueWebkitText) {
+        cssValue = CSSPrimitiveValue::createIdentifier(parserValue->id);
+        return true;
+    }
+    return false;
+}
+
 const int cMaxFillProperties = 9;
 
 bool CSSParser::parseFillShorthand(int propId, const int* properties, int numProperties, bool important)
@@ -1777,6 +1786,7 @@ bool CSSParser::parseFillShorthand(int propId, const int* properties, int numPro
                 RefPtr<CSSValue> val1;
                 RefPtr<CSSValue> val2;
                 int propId1, propId2;
+                CSSParserValue* parserValue = m_valueList->current();
                 if (parseFillProperty(properties[i], propId1, propId2, val1, val2)) {
                     parsedProperty[i] = found = true;
                     addFillValue(values[i], val1.release());
@@ -1786,7 +1796,7 @@ bool CSSParser::parseFillShorthand(int propId, const int* properties, int numPro
                         addFillValue(repeatYValue, val2.release());
                     if (properties[i] == CSSPropertyBackgroundOrigin || properties[i] == CSSPropertyWebkitMaskOrigin) {
                         // Reparse the value as a clip, and see if we succeed.
-                        if (parseFillProperty(CSSPropertyBackgroundClip, propId1, propId2, val1, val2))
+                        if (parseBackgroundClip(parserValue, val1))
                             addFillValue(clipValue, val1.release()); // The property parsed successfully.
                         else
                             addFillValue(clipValue, CSSInitialValue::createImplicit()); // Some value was used for origin that is not supported by clip. Just reset clip instead.
@@ -2425,10 +2435,8 @@ bool CSSParser::parseFillProperty(int propId, int& propId1, int& propId2,
                     }
                     break;
                 case CSSPropertyBackgroundClip:
-                    if (val->id == CSSValueBorderBox || val->id == CSSValuePaddingBox || val->id == CSSValueWebkitText) {
-                        currValue = CSSPrimitiveValue::createIdentifier(val->id);
+                    if (parseBackgroundClip(val, currValue))
                         m_valueList->next();
-                    }
                     break;
                 case CSSPropertyBackgroundOrigin:
                     if (val->id == CSSValueBorderBox || val->id == CSSValuePaddingBox || val->id == CSSValueContentBox) {

third_party/WebKit/WebCore/css/CSSPrimitiveValueMappings.h
@@ -378,6 +378,7 @@ template<> inline CSSPrimitiveValue::operator EFillBox() const
         case CSSValueContentBox:
             return ContentFillBox;
         case CSSValueText:
+        case CSSValueWebkitText:
             return TextFillBox;
         default:
             ASSERT_NOT_REACHED();

third_party/WebKit/LayoutTests/fast/css/background-clip-text-expected.txt
@@ -1,0 +1,19 @@
+This tests checks that the '-webkit-text' and 'text' keywords are parsed correctly in the 'background-clip' and '-webkit-background-clip' properties, and that 'background-clip' is parsed correctly in the 'background' shorthand.
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS test("background-clip: -webkit-text", "background-clip") is "-webkit-text"
+PASS test("background-clip: -webkit-text", "-webkit-background-clip") is null
+PASS test("background-clip: text", "background-clip") is null
+PASS test("background-clip: text", "-webkit-background-clip") is null
+PASS test("-webkit-background-clip: -webkit-text", "background-clip") is null
+PASS test("-webkit-background-clip: -webkit-text", "-webkit-background-clip") is "-webkit-text"
+PASS test("-webkit-background-clip: text", "background-clip") is null
+PASS test("-webkit-background-clip: text", "-webkit-background-clip") is "text"
+PASS test("background: url() padding-box", "background-clip") is "padding-box"
+PASS test("background: url() padding-box", "-webkit-background-clip") is null
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

third_party/WebKit/LayoutTests/fast/css/background-clip-text.html
@@ -1,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<link rel="stylesheet" href="../js/resources/js-test-style.css">
+<script src="../js/resources/js-test-pre.js"></script>
+</head>
+<body>
+<p id="description"></p>
+<div id="console"></div>
+<script src="script-tests/background-clip-text.js"></script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/css/script-tests/TEMPLATE.html
@@ -1,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<link rel="stylesheet" href="../js/resources/js-test-style.css">
+<script src="../js/resources/js-test-pre.js"></script>
+</head>
+<body>
+<p id="description"></p>
+<div id="console"></div>
+<script src="YOUR_JS_FILE_HERE"></script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/css/script-tests/background-clip-text.js
@@ -1,0 +1,32 @@
+description("This tests checks that the '-webkit-text' and 'text' keywords are \
+    parsed correctly in the 'background-clip' and '-webkit-background-clip' \
+    properties, and that 'background-clip' is parsed correctly in the \
+    'background' shorthand.");
+
+function test(declaration, property)
+{
+    var div = document.createElement("div");
+    div.setAttribute("style", declaration);
+    document.body.appendChild(div);
+    
+    var result = div.style.getPropertyValue(property);
+    document.body.removeChild(div);
+    return result;
+}
+
+shouldBe('test("background-clip: -webkit-text", "background-clip")', '"-webkit-text"');
+shouldBe('test("background-clip: -webkit-text", "-webkit-background-clip")', 'null');
+
+shouldBe('test("background-clip: text", "background-clip")', 'null');
+shouldBe('test("background-clip: text", "-webkit-background-clip")', 'null');
+
+shouldBe('test("-webkit-background-clip: -webkit-text", "background-clip")', 'null');
+shouldBe('test("-webkit-background-clip: -webkit-text", "-webkit-background-clip")', '"-webkit-text"');
+
+shouldBe('test("-webkit-background-clip: text", "background-clip")', 'null');
+shouldBe('test("-webkit-background-clip: text", "-webkit-background-clip")', '"text"');
+
+shouldBe('test("background: url() padding-box", "background-clip")', '"padding-box"');
+shouldBe('test("background: url() padding-box", "-webkit-background-clip")', 'null');
+
+var successfullyParsed = true;