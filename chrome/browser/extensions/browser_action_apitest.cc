577216ea4325b0aae45313d57ddbac7b74eb546d
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-25 00:09:49 +0000

    Disable ExtensionApiTest.BrowserActionPopup because it crashes on the bot.
    BUG: 25746
    Review URL: http://codereview.chromium.org/330012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30019 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: af236bd292dcf42dba68e45ba424e41239608194

--- chrome/browser/extensions/browser_action_apitest.cc
+++ chrome/browser/extensions/browser_action_apitest.cc
@@ -140,7 +140,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, TabSpecificBrowserActionState) {
   ASSERT_EQ(L"hi!", text);
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, BrowserActionPopup) {
+// This test is disabled. See bug 25746
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_BrowserActionPopup) {
   ASSERT_TRUE(LoadExtension(test_data_dir_.AppendASCII("popup")));
 
   ResultCatcher catcher;
