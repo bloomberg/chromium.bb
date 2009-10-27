41c578cb891dd436d7dd7c0b835202dbae1c540e
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-27 02:17:53 +0000

    Revert 30149 - The existing file_util::AbsolutePath() function was already doing what we needed to do in the ExtensionResource class.
    BUG= http://crbug.com/25681 & http://crbug.com/25131
    
    Review URL: http://codereview.chromium.org/334028
    
    TBR=mad@chromium.org
    Review URL: http://codereview.chromium.org/335042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30152 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 71b0c7f62e9c674adb0ffc785ee0e8de05b55468

--- chrome/common/extensions/extension_resource.cc
+++ chrome/common/extensions/extension_resource.cc
@@ -40,26 +40,22 @@ FilePath ExtensionResource::GetFilePath(const FilePath& extension_root,
   std::vector<FilePath> l10n_relative_paths;
   extension_l10n_util::GetL10nRelativePaths(relative_path,
                                             &l10n_relative_paths);
-  // We need to resolve the parent references in the extension_root
-  // path on its own because IsParent doesn't like parent references.
-  FilePath clean_extension_root(extension_root);
-  if (!file_util::AbsolutePath(&clean_extension_root))
-    return FilePath();
 
   // Stat l10n file(s), and return new path if it exists.
   for (size_t i = 0; i < l10n_relative_paths.size(); ++i) {
-    FilePath full_path = clean_extension_root.Append(l10n_relative_paths[i]);
-    if (file_util::AbsolutePath(&full_path) &&
-        clean_extension_root.IsParent(full_path) &&
+    FilePath full_path;
+    if (extension_root.AppendAndResolveRelative(l10n_relative_paths[i],
+                                                &full_path) &&
+        extension_root.IsParent(full_path) &&
         file_util::PathExists(full_path)) {
       return full_path;
     }
   }
 
   // Fall back to root resource.
-  FilePath full_path = clean_extension_root.Append(relative_path);
-  if (file_util::AbsolutePath(&full_path) &&
-      clean_extension_root.IsParent(full_path)) {
+  FilePath full_path;
+  if (extension_root.AppendAndResolveRelative(relative_path, &full_path) &&
+      extension_root.IsParent(full_path)) {
     return full_path;
   }
 
