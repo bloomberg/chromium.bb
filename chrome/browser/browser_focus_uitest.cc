4e24226533571cfec32afeb2ed3ef11779b80b93
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-10-27 05:27:41 +0000

    Attempt to make interactive ui tests complete on Linux while I try to repro it. Review URL: http://codereview.chromium.org/333042
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@30169 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 47f782c2f1e3c1f3c95e345494b6f5a1a670fd21

--- chrome/browser/browser_focus_uitest.cc
+++ chrome/browser/browser_focus_uitest.cc
@@ -332,6 +332,7 @@ IN_PROC_BROWSER_TEST_F(BrowserFocusTest, DISABLED_TabsRememberFocusFindInPage) {
 }
 
 // Background window does not steal focus.
+#if !defined(OS_LINUX)
 IN_PROC_BROWSER_TEST_F(BrowserFocusTest, BackgroundBrowserDontStealFocus) {
   HTTPTestServer* server = StartHTTPServer();
 
@@ -379,6 +380,7 @@ IN_PROC_BROWSER_TEST_F(BrowserFocusTest, BackgroundBrowserDontStealFocus) {
   // Close the 2nd browser to avoid a DCHECK().
   browser2->window()->Close();
 }
+#endif
 
 // Page cannot steal focus when focus is on location bar.
 IN_PROC_BROWSER_TEST_F(BrowserFocusTest, LocationBarLockFocus) {
