347cfb30d9ec38ce3e823e5d3deceaf5d1f7290f
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-05 21:48:35 +0000

    Update sample screen shot extension to use a browser action instead of a toolsrip.
    BUG=none
    TEST=Manual testing on linux and windows.
    
    Review URL: http://codereview.chromium.org/353025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31132 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ae376ecbf0ff71046478481163cb77140e5026e9

--- chrome/test/data/extensions/samples/screenshot/screenshot.js
+++ chrome/test/data/extensions/samples/screenshot/screenshot.js
@@ -1,4 +1,4 @@
-function takeScreenshot() {  
+function takeScreenshot() {
   chrome.tabs.captureVisibleTab(null, function(img) {
     var screenshotUrl = img;
     var viewTabUrl = chrome.extension.getURL('screenshot.html');
@@ -6,18 +6,42 @@ function takeScreenshot() {
     chrome.tabs.create({url: viewTabUrl}, function(tab) {
       var targetId = tab.id;
 
-      chrome.tabs.onUpdated.addListener(function(tabId, changedProps) {
-        if (tabId == targetId &&
-            changedProps.status == "complete") {
-          var views = chrome.extension.getViews();
-          for (var i = 0; i < views.length; i++) {
-            var view = views[i];
-            if (view.location.href == viewTabUrl) {
-              view.setScreenshotUrl(screenshotUrl);
-            }
+      var addSnapshotImageToTab = function(tabId, changedProps) {
+        // We are waiting for the tab we opened to finish loading.
+        // Check that the the tab's id matches the tab we opened,
+        // and that the tab is done loading.
+        if (tabId != targetId || changedProps.status != "complete")
+          return;
+
+        // Passing the above test means this is the event we were waiting for.
+        // There is nothing we need to do for future onUpdated events, so we
+        // use removeListner to stop geting called when onUpdated events fire.
+        chrome.tabs.onUpdated.removeListener(addSnapshotImageToTab);
+
+        // Look through all views to find the window which will display
+        // the screenshot.
+        var views = chrome.extension.getViews();
+        for (var i = 0; i < views.length; i++) {
+          var view = views[i];
+          // If more than one screen shot tab is opened, we need to
+          // ensure that we do not change an existing screen shot image.
+          // view.imageAlreadySet is set to true when an image is set for
+          // the first time.  We never change an image in a window with
+          // this flag set.
+          if (view.location.href == viewTabUrl && !view.imageAlreadySet) {
+            view.setScreenshotUrl(screenshotUrl);
+            view.imageAlreadySet = true;
+            break;
           }
         }
-      });
-    })
+      };
+      chrome.tabs.onUpdated.addListener(addSnapshotImageToTab);
+
+    });
   });
-};
+}
+
+// Listen for a click on the camera icon.  On that click, take a screenshot.
+chrome.browserAction.onClicked.addListener(function(tab) {
+  takeScreenshot();
+});
