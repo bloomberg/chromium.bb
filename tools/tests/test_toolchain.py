cf7a0f0335da20294db8920f0d747168b82df58f
mseaborn@google.com <mseaborn@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2009-11-09 22:36:49 +0000

    gcc: Fix compilation of computed gotos
    This brings gcc-4.2.2.patch into line with gcc-4.4.1.patch.
    
    The "%A0" syntax adds a "*" in front of the register name which isn't
    the right syntax for nacljmp.
    
    Without this change, the compile will fail with an error like this:
    /tmp/ccH6hCel.s: Assembler messages:
    /tmp/ccH6hCel.s:14: Error: suffix or operands invalid for `nacljmp' 
    
    This change also adds a test case for the toolchain.
    The test can be run with, for example, the following command:
    ./scons MODE=opt-linux,nacl sdk_minimal_test
    
    Review URL: http://codereview.chromium.org/348032
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@962 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

---
+++ tools/tests/test_toolchain.py
@@ -0,0 +1,81 @@
+
+# Copyright 2009  The Native Client Authors.  All rights reserved.
+# Use of this source code is governed by a BSD-style license that can
+# be found in the LICENSE file.
+
+import os
+import shutil
+import subprocess
+import tempfile
+import unittest
+
+
+# From http://lackingrhoticity.blogspot.com/2008/11/tempdirtestcase-python-unittest-helper.html
+class TempDirTestCase(unittest.TestCase):
+
+  def setUp(self):
+    self._on_teardown = []
+
+  def make_temp_dir(self):
+    temp_dir = tempfile.mkdtemp(prefix="tmp-%s-" % self.__class__.__name__)
+    def tear_down():
+      shutil.rmtree(temp_dir)
+    self._on_teardown.append(tear_down)
+    return temp_dir
+
+  def tearDown(self):
+    for func in reversed(self._on_teardown):
+      func()
+
+
+def write_file(filename, data):
+  fh = open(filename, "w")
+  try:
+    fh.write(data)
+  finally:
+    fh.close()
+
+
+# TODO: use subprocess.check_call when we have Python 2.5 on Windows.
+def check_call(*args, **kwargs):
+  rc = subprocess.call(*args, **kwargs)
+  if rc != 0:
+    raise Exception("Failed with return code %i" % rc)
+
+
+class ToolchainTests(TempDirTestCase):
+
+  def test_computed_gotos(self):
+    # Test for toolchain bug.
+    # Bug 1:  gcc outputs "jmp *%eax", fails to validate.
+    # Bug 2:  gcc outputs "nacljmp *%eax", fails to assemble.
+    # Correct assembly output is "nacljmp %eax".
+    code = """
+int foo(int i) {
+  void *addr[] = { &&label1, &&label2 };
+  goto *addr[i];
+ label1:
+  return 1234;
+ label2:
+  return 4568;
+}
+
+int main() {
+  return 0;
+}
+"""
+    temp_dir = self.make_temp_dir()
+    write_file(os.path.join(temp_dir, "code.c"), code)
+    # ncval doesn't seem to accept .o files any more.
+    # TODO: fix ncval.
+    check_call(["nacl-gcc", # "-c",
+                os.path.join(temp_dir, "code.c"),
+                "-o", os.path.join(temp_dir, "prog")])
+    # ncval isn't returning a non-zero exit code on validator failure.
+    # TODO: fix ncval.
+    check_call(["ncval", os.path.join(temp_dir, "prog")],
+               stdout=open(os.devnull, "w"))
+
+
+if __name__ == "__main__":
+  unittest.main()
