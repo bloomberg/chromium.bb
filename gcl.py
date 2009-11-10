ebbf947334a92730c103a91b783b5c30f6afe442
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-10 20:26:30 +0000

    Fix PyDoc.
    Review URL: http://codereview.chromium.org/387005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@31592 0039d316-1c4b-4281-b951-d872f2087c98

--- gcl.py
+++ gcl.py
@@ -2,9 +2,9 @@
 # Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-#
-# Wrapper script around Rietveld's upload.py that groups files into
-# changelists.
+
+"""Wrapper script around Rietveld's upload.py that groups files into
+changelists."""
 
 import getpass
 import os
