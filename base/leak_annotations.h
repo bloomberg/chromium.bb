29def299e3fd7df3755db882cab834a08873a9e3
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-11 21:58:28 +0000

    Implement the memory leak annotations for heap leak checker.
    leak_annotations.h provides the ANNOTATE_SCOPED_MEMORY_LEAK macro
    that can be used for annotating known memory leaks in the tests.
    Those will be ignored by the heap leak checker.
    
    Patch contributed by ramosian.glider@gmail.com.
    Original review at http://codereview.chromium.org/346038/show
    
    Review URL: http://codereview.chromium.org/385049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31721 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5ca2744311af9962130fa27d2dd2c4ad869d7504

---
+++ base/leak_annotations.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_LEAK_ANNOTATIONS_H_
+#define BASE_LEAK_ANNOTATIONS_H_
+
+#if defined(LINUX_USE_TCMALLOC) && defined(LINUX_USE_HEAPCHECKER)
+
+#include "third_party/tcmalloc/heap-checker.h"
+
+// Annotate a program scope as having memory leaks. Tcmalloc's heap leak
+// checker will ignore them. Note that these annotations may mask real bugs
+// and should not be used in the production code.
+#define ANNOTATE_SCOPED_MEMORY_LEAK \
+    HeapLeakChecker::Disabler heap_leak_checker_disabler
+
+#else
+
+// If tcmalloc is not used, the annotations should be no-ops.
+#define ANNOTATE_SCOPED_MEMORY_LEAK
+
+#endif
+
+#endif  // BASE_LEAK_ANNOTATIONS_H_
