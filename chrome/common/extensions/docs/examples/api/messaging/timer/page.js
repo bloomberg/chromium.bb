e38aad99e275c90579b8e64ef2bd3083b18cd6ca
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-12 22:03:29 +0000

    Add a simple perf extension that times messaging roundtrips between content scripts and extensions.
    BUG=no
    
    Review URL: http://codereview.chromium.org/384093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31837 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a38f23d8ba893cef5cbada84ad1a2ec3eac463b3

---
+++ chrome/common/extensions/docs/examples/api/messaging/timer/page.js
@@ -0,0 +1,9 @@
+chrome.extension.onConnect.addListener(function(port) {
+  port.onMessage.addListener(function(msg) {
+    port.postMessage({counter: msg.counter+1});
+  });
+});
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+  sendResponse({counter: request.counter+1});
+});
