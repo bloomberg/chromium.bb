373f368401e6dd43ea95c7be396f03985b56d777
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-14 01:02:33 +0000

    Fix the messaging timing extension to only message the toplevel frame.
    BUG=no
    
    Review URL: http://codereview.chromium.org/384133
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@31978 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f9b6978b620a4297f0d27a6048a662213caaa412

--- chrome/common/extensions/docs/examples/api/messaging/timer/page.js
+++ chrome/common/extensions/docs/examples/api/messaging/timer/page.js
@@ -1,9 +1,12 @@
-chrome.extension.onConnect.addListener(function(port) {
-  port.onMessage.addListener(function(msg) {
-    port.postMessage({counter: msg.counter+1});
+if (window == top) {
+  chrome.extension.onConnect.addListener(function(port) {
+    port.onMessage.addListener(function(msg) {
+      port.postMessage({counter: msg.counter+1});
+    });
   });
-});
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-  sendResponse({counter: request.counter+1});
-});
+  chrome.extension.onRequest.addListener(
+    function(request, sender, sendResponse) {
+      sendResponse({counter: request.counter+1});
+    });
+}
