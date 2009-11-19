8e24e025f4b46c5d0dc768ce9b6a56cfc82705a8
mseaborn@google.com <mseaborn@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2009-11-19 00:23:32 +0000

    Tidy: Remove obsolete benchmark code
    Remove switch.c and related code.  This is old leftover code from
    when Bennet needed to measure the context switch overhead and the
    compiler wasn't ready to generate binaries that are 0-based for
    %cs confinement, and sel_ldr was, iirc, still doing section
    loading and relocations.  We have an explicit null syscall now,
    and can run nacl modules etc easily, so there's no need for
    switch.c.
    
    Review URL: http://codereview.chromium.org/408010
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1004 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: a6bc76e24e205c68270792262ba4c49c9fcf9155

--- src/trusted/service_runtime/benchmark/analyze.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-perl -ane 'if ($_ =~ /real/ && $F[1] =~ /0m([\\.0-9]*)s/) { print $F[1], ",", $1, "\n"; $sum += $1; $n++ } END { print $sum, ",", $n, "\n"; print "avg ", $sum/$n, "\n";} '
--- src/trusted/service_runtime/benchmark/run100.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-program=${1:-switch}
-(i=0; while [ $i -lt 100 ]; do time ./$program; i=$(($i + 1)); done) > timing.$program.stdout.txt 2> timing.$program.stderr.txt
