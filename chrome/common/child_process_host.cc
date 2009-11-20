32d03fb1e005117aed0d276a87b0ff66d51d3afb
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-11-20 22:52:49 +0000

    Linux: make --renderer-cmd-prefix work after r32486
    http://codereview.chromium.org/418028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@32700 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b9148774aa3ad1d550a2835d5441febc97a4770e

--- chrome/common/child_process_host.cc
+++ chrome/common/child_process_host.cc
@@ -93,21 +93,25 @@ FilePath ChildProcessHost::GetChildPath() {
   if (!child_path.empty())
     return child_path;
 
+#if defined(OS_MACOSX)
+  // On the Mac, the child executable lives at a predefined location within
+  // the app bundle's versioned directory.
+  return chrome::GetVersionedDirectory().
+      Append(chrome::kHelperProcessExecutablePath);
+#endif
+
 #if defined(OS_LINUX)
   // Use /proc/self/exe rather than our known binary path so updates
   // can't swap out the binary from underneath us.
-  child_path = FilePath("/proc/self/exe");
-#elif defined(OS_MACOSX)
-  // On the Mac, the child executable lives at a predefined location within
-  // the app bundle's versioned directory.
-  child_path = chrome::GetVersionedDirectory().
-      Append(chrome::kHelperProcessExecutablePath);
-#else
+  if (!CommandLine::ForCurrentProcess()->HasSwitch(
+        switches::kRendererCmdPrefix)) {
+    return FilePath("/proc/self/exe");
+  }
+#endif
+
   // On most platforms, the child executable is the same as the current
   // executable.
   PathService::Get(base::FILE_EXE, &child_path);
-#endif
-
   return child_path;
 }
 
