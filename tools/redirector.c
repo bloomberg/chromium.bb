596bb4982bcf35e72c188c186f463742c8b7a22d
khim@google.com <khim@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2009-11-27 16:20:35 +0000

    Hermetic build with Cygwin 1.7
    Review URL: http://codereview.chromium.org/446004
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1042 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: ba5bb40e3307af787ce3b7b511db2ed5ccdc9db2

---
+++ tools/redirector.c
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2008, Google Inc.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <wchar.h>
+#include <process.h>
+#include <windows.h>
+
+#undef MAX_PATH
+#define MAX_PATH 2048
+#define ERROR { wprintf(L"Can not find filename to execute!"); return 1; }
+
+/*
+ * The SDK redirector reads it's name and invokes the appropriate binary from a
+ * location with cygwin1.dll nearby so that all cygwin-oriented SDK programs can
+ * start (this works for Cygwin version >= 1.7).
+ *
+ * Supports arbitrary unicode names in it's base directory name.
+ *
+ * The executable must be placed in nacl/bin or nacl64/bin or just bin (in the
+ * latter case the name of the binary should be prefixed with nacl- or nacl64-)
+ *
+ * The binary should be built by visual studio (not cygwin) since when it starts
+ * cygwin has not been located yet.
+ */
+
+int wmain(int argc, wchar_t* argv[], wchar_t* envp[]) {
+  wchar_t newpath[MAX_PATH+64];
+  wchar_t oldpath[MAX_PATH];
+  wchar_t *delimeter = oldpath;
+  int nacl64 = 0, done;
+  
+  if (GetModuleFileNameW(NULL, oldpath, MAX_PATH) == 0)
+    ERROR;
+  while (*delimeter)
+    ++delimeter;
+  if (delimeter-oldpath>MAX_PATH)
+    ERROR;
+  while (delimeter>oldpath && *delimeter != L'/' && *delimeter != L'\\')
+    --delimeter;
+  if (delimeter == oldpath)
+    ERROR;
+
+  if ((delimeter[-4] == L'/' || delimeter[-4] == L'\\') &&
+      (delimeter[-3] == L'b' || delimeter[-3] == L'B') &&
+      (delimeter[-2] == L'i' || delimeter[-2] == L'I') &&
+      (delimeter[-1] == L'n' || delimeter[-1] == L'N') &&
+      (delimeter[1] == L'n' || delimeter[1] == L'N') &&
+      (delimeter[2] == L'a' || delimeter[2] == L'A') &&
+      (delimeter[3] == L'c' || delimeter[3] == L'C') &&
+      (delimeter[4] == L'l' || delimeter[4] == L'L') &&
+       delimeter[5] == L'-') {
+    delimeter-=4;
+  } else if ((delimeter[-4] == L'/' || delimeter[-4] == L'\\') &&
+           (delimeter[-3] == L'b' || delimeter[-3] == L'B') &&
+           (delimeter[-2] == L'i' || delimeter[-2] == L'I') &&
+           (delimeter[-1] == L'n' || delimeter[-1] == L'N') &&
+           (delimeter[1] == L'n' || delimeter[1] == L'N') &&
+           (delimeter[2] == L'a' || delimeter[2] == L'A') &&
+           (delimeter[3] == L'c' || delimeter[3] == L'C') &&
+           (delimeter[4] == L'l' || delimeter[4] == L'L') &&
+           (delimeter[5] == L'6' || delimeter[5] == L'6') &&
+           (delimeter[6] == L'4' || delimeter[6] == L'4') &&
+            delimeter[7] == L'-') {
+    delimeter-=4;
+    nacl64=1;
+  } else if ((delimeter[-9] == L'/' || delimeter[-9] == L'\\') &&
+             (delimeter[-8] == L'n' || delimeter[-8] == L'N') &&
+             (delimeter[-7] == L'a' || delimeter[-7] == L'A') &&
+             (delimeter[-6] == L'c' || delimeter[-6] == L'C') &&
+             (delimeter[-5] == L'l' || delimeter[-5] == L'L') &&
+             (delimeter[-4] == L'/' || delimeter[-4] == L'\\') &&
+             (delimeter[-3] == L'b' || delimeter[-3] == L'B') &&
+             (delimeter[-2] == L'i' || delimeter[-2] == L'I') &&
+             (delimeter[-1] == L'n' || delimeter[-1] == L'N')) {
+    delimeter-=9;
+  } else if ((delimeter[-11] == L'/' || delimeter[-11] == L'\\') &&
+             (delimeter[-10] == L'n' || delimeter[-10] == L'N') &&
+             (delimeter[-9] == L'a' || delimeter[-9] == L'A') &&
+             (delimeter[-8] == L'c' || delimeter[-8] == L'C') &&
+             (delimeter[-7] == L'l' || delimeter[-7] == L'L') &&
+             (delimeter[-6] == L'6' || delimeter[-6] == L'6') &&
+             (delimeter[-5] == L'4' || delimeter[-5] == L'4') &&
+             (delimeter[-4] == L'/' || delimeter[-4] == L'\\') &&
+             (delimeter[-3] == L'b' || delimeter[-3] == L'B') &&
+             (delimeter[-2] == L'i' || delimeter[-2] == L'I') &&
+             (delimeter[-1] == L'n' || delimeter[-1] == L'N')) {
+    delimeter-=11;
+    nacl64=1;
+  }
+  done = delimeter-oldpath;
+  wmemcpy(newpath, oldpath, done);
+  if (nacl64) {
+    wmemcpy(newpath + done, L"\\libexec\\nacl64-", 16);
+    done += 16; delimeter += 12;
+  } else {
+    wmemcpy(newpath + done, L"\\libexec\\nacl-", 14);
+    done += 14; delimeter += 10;
+  }
+  wcscpy(newpath + done, delimeter);
+  argv[0] = newpath;
+  return _wspawnvpe(_P_WAIT, newpath, argv, envp);
+}
