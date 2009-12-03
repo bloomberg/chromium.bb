bb641667d079b542a35a99e11ab436ba127da670
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2009-12-03 20:53:11 +0000

    intial check-in of llvm SDK build script
    Review URL: http://codereview.chromium.org/458007
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1068 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 49e53e93297b6604b7954ff8c703cb67f1711700

---
+++ tools/llvm/llvm-fake.py
@@ -0,0 +1,202 @@
+#!/usr/bin/python
+# Copyright 2009 The Native Client Authors.  All rights reserved.
+# Use of this source code is governed by a BSD-style license that can
+# be found in the LICENSE file.
+
+# this script is a replacement for llvm-gcc and llvm-g++, it detects
+# automatically which role it is supposed to assume.
+# The point of the script is to force compiler output through llc.
+#
+# TODO(robertm): newlib does currently not compile with the cortex settings
+
+import struct
+import subprocess
+import sys
+
+# this for manual debugging of this script only
+VERBOSE = 0
+
+BASE = '/usr/local/crosstool'
+
+
+LLVM_GCC_ASSEMBLER_FLAGS = [#'-march=armv7',
+                            '-march=armv6',
+                            #'-mcpu=cortex-a8',
+                            ]
+
+LLVM_PREFIX = BASE + '/arm-none-linux-gnueabi/llvm-gcc-4.2/bin/llvm'
+
+LLC_FLAGS_SHARED = ['-march=arm',
+                    # c.f. lib/Target/ARM/ARMGenSubtarget.inc
+                    '-mcpu=arm1156t2-s',
+                    #'-mcpu=cortex-a8',
+                    '-mtriple=armv6-*-*eabi*',
+                    #'-mtriple=armv7a-*-*eabi*',
+                    '-arm-reserve-r9',
+                    ]
+
+LLC = [BASE + '/arm-none-linux-gnueabi/llvm/bin/llc', ] + LLC_FLAGS_SHARED
+
+LLC_SFI = [BASE + '/arm-none-linux-gnueabi/llvm/bin/llc-sfi',
+           # The following options might come in hand and are left
+           # here as comments:
+           # TODO(robertm): describe their purpose
+           #     '-soft-float ',
+           #     '-aeabi-calls '
+           #     '-sfi-zero-mask',
+           '-sfi-cp-fudge',
+           '-sfi-cp-fudge-percent=90',
+           '-sfi-store',
+           '-sfi-stack',
+           '-sfi-branch',
+           '-sfi-data',
+           '-no-inline-jumptables'
+           ] + LLC_FLAGS_SHARED
+
+OPT = [BASE + '/arm-none-linux-gnueabi/llvm/bin/opt',
+       '-O3',
+       '-std-compile-opts'
+       ]
+
+AS = ([BASE + '/codesourcery/arm-2007q3/bin/arm-none-linux-gnueabi-as', ] +
+      LLVM_GCC_ASSEMBLER_FLAGS)
+
+
+
+def Run(a):
+  if VERBOSE: print a
+  ret = subprocess.call(a)
+  if ret:
+    print
+    print "ERROR"
+    print
+    sys.exit(ret)
+
+
+
+def SfiCompile(argv, out_pos, is_sfi):
+  filename = argv[out_pos]
+
+  argv[out_pos] = filename + '.bc'
+  argv.append('--emit-llvm')
+  Run(argv)
+
+  OPT.append(filename + '.bc')
+  OPT.append('-f')
+  OPT.append('-o')
+  OPT.append(filename + '.opt.bc')
+  Run(OPT)
+
+  if is_sfi:
+    llc = LLC_SFI
+  else:
+    llc = LLC
+  llc.append('-f')
+  llc.append(filename + '.opt.bc')
+  llc.append('-o')
+  llc.append(filename + '.s')
+  Run(llc)
+
+
+  AS.append(filename + '.s')
+  AS.append('-o')
+  AS.append(filename )
+  Run(AS)
+
+
+def PatchAbiVersionIntoElfHeader(filename):
+  # modeling Elf32_Ehdr
+  FORMAT = '16BHH5I6H'
+
+  fp = open(filename, 'rb')
+  data = fp.read(struct.calcsize(FORMAT))
+  fp.close()
+
+  ELF_OSABI = 7
+  ELF_ABIVERSION = 8
+  ELF_FLACS = 22
+  t = list(struct.unpack(FORMAT, data))
+  # c.f. nacl_elf.h
+  t[ELF_OSABI] = 123
+  t[ELF_ABIVERSION] = 6
+  # TODO(robertm): this currently breaks running images in sel_ldr
+  # t[ELF_FLACS] = t[ELF_FLACS] | 0x100000
+  data = struct.pack(FORMAT, *t)
+  fp = open(filename, 'rb+')
+  fp.write(data)
+  fp.close()
+
+# check whether compiler is invoked with -c but not on an .S file,
+# hijack the corresponding .o file
+def FindObjectFilePos(argv):
+  if '-c' not in argv:
+    return None
+
+  if '-o' not in argv:
+    return None
+
+  pos = 1 + argv.index('-o')
+  assert pos < len(argv)
+
+  if not argv[pos].endswith('.o'):
+    return None
+
+  return pos
+
+
+def HasAssemblerFiles(argv):
+  for a in argv:
+    if a.endswith('.S') or a.endswith('.s'):
+      return True
+  else:
+    return False
+
+
+def FindLinkPos(argv):
+  if '-o' not in argv:
+    return None
+
+  pos = 1 + argv.index('-o')
+  assert pos < len(argv)
+
+  if not argv[pos].endswith('.nexe'):
+    return None
+
+  return pos
+
+
+def GetActualLlvmBinary(argv):
+  if argv[0].endswith('gcc'):
+    return LLVM_PREFIX + '-gcc'
+  else:
+    assert argv[0].endswith('g++')
+    return LLVM_PREFIX + '-g++'
+
+
+def IsSfiInvocation(argv):
+  return argv[0].endswith('sfigcc') or argv[0].endswith('sfig++')
+
+
+def main(argv):
+  obj_pos = FindObjectFilePos(argv)
+  link_pos = FindLinkPos(argv)
+  is_sfi = IsSfiInvocation(argv)
+  # NOTE: we need to call this last, o.w. IsSfiInvocation() does not work
+  argv[0] = GetActualLlvmBinary(argv)
+
+  if link_pos:
+    Run(argv)
+    PatchAbiVersionIntoElfHeader(argv[link_pos])
+  elif obj_pos:
+    if HasAssemblerFiles(argv):
+      Run(argv + LLVM_GCC_ASSEMBLER_FLAGS)
+    else:
+        SfiCompile(argv, obj_pos, is_sfi)
+  else:
+    Run(argv)
+
+  return 0
+
+
+if __name__ == "__main__":
+  sys.exit(main(sys.argv))
--- tools/llvm/toolchain-creator.sh
+++ tools/llvm/toolchain-creator.sh
@@ -3,14 +3,20 @@
 # Use of this source code is governed by a BSD-style license that can
 # be found in the LICENSE file.
 #
-# This script must be run from the native_client/ directory.
+#@ This script build the arm SDK.
+#@ It must be run from the native_client/ directory.
 
+######################################################################
+# Config
+######################################################################
+
+set -o nounset
+set -o errexit
 
 readonly CS_TARBALL=arm-2007q3-51-arm-none-linux-gnueabi-i686-pc-linux-gnu.tar.bz2
 readonly LLVM_BUILD_SCRIPT=tools/llvm/build-install-linux.sh
 
 CS_URL=http://www.codesourcery.com/sgpp/lite/arm/portal/package1787/public/arm-none-linux-gnueabi/${CS_TARBALL}
-
 
 # TODO(robertm): temporarily use /usr/local/crosstool2 to not conflict
 #                with the working toolchain in /usr/local/crosstool
@@ -28,6 +34,63 @@ Banner() {
   echo "######################################################################"
   echo $*
   echo "######################################################################"
+}
+
+
+SubBanner() {
+  echo "......................................................................"
+  echo $*
+  echo "......................................................................"
+}
+
+
+Usage() {
+  egrep "^#@" $0 | cut --bytes=3-
+}
+
+######################################################################
+#
+######################################################################
+
+# some sanity checks to make sure this script is run from the right place
+PathSanityCheck() {
+  if [[ $(basename $(pwd)) != "native_client" ]] ; then
+    echo "ERROR: run this script from the native_client/ dir"
+    exit -1
+  fi
+
+  if [[ ! -d ${INSTALL_ROOT} ]] ; then
+     echo "ERROR: ${INSTALL_ROOT} does not exist"
+    exit -1
+  fi
+}
+
+
+# TODO(robertm): consider wiping all of ${BASE_DIR}
+ClearInstallDir() {
+  Banner "clearing dirs in ${INSTALL_ROOT}"
+  rm -rf ${INSTALL_ROOT}/*
+}
+
+
+CreateTarBall() {
+  local tarball=$1
+  Banner "creating tar ball ${tarball}"
+  tar cfz ${tarball} -C ${INSTALL_ROOT} .
+}
+
+
+# try to keep the tarball small
+PruneDirs() {
+  local CS=${INSTALL_ROOT}/codesourcery
+  Banner "pruning tree"
+
+  rm     ${INSTALL_ROOT}/arm-none-linux-gnueabi/llvm/lib/lib*.a
+  rm -rf ${CS}/arm-2007q3/share
+  # TODO(robertm): investigate these
+  #  rm -rf ${CS}/arm-2007q3/arm-none-linux-gnueabi/lib
+  #  rm -rf ${CS}/arm-2007q3/arm-none-linux-gnueabi/libc/armv4t
+  rm -rf ${CS}/arm-2007q3/arm-none-linux-gnueabi/libc/thumb2
 }
 
 
@@ -62,8 +125,7 @@ ExtractLlvmBuildScript() {
 # The mygcc32 and myg++32 trickery ensures that all binaries
 # are statically linked and 32-bit.
 ConfigureAndBuildLlvm() {
-  Banner "Running ${LLVM_BUILD_SCRIPT}"
-
+  Banner "Untar,Confiure,Build llvm/llvm-gcc ${LLVM_BUILD_SCRIPT}"
   export MAKE_OPTS="-j6 VERBOSE=1"
   export CC=$(readlink -f tools/llvm/mygcc32)
   export CXX=$(readlink -f tools/llvm/myg++32)
@@ -71,15 +133,156 @@ ConfigureAndBuildLlvm() {
   nice ${LLVM_BUILD_SCRIPT}
 }
 
+
+# Build a sfi version of llvm's llc backend
+# The mygcc32 and myg++32 trickery ensures that all binaries
+# are statically linked and 32-bit.
+UntarPatchConfigureAndBuildSfiLlc() {
+  export MAKE_OPTS="-j6 VERBOSE=1"
+  export CC=$(readlink -f tools/llvm/mygcc32)
+  export CXX=$(readlink -f tools/llvm/myg++32)
+  local patch=$(readlink -f tools/patches/llvm-r${LLVM_SVN_REV}.patch)
+  local saved_dir=$(pwd)
+  local tmpdir=/tmp/llvm.sfi
+  Banner "Building sfi lcc in ${tmpdir}"
+  rm -rf ${tmpdir}
+  mkdir ${tmpdir}
+  cd ${tmpdir}
+  SubBanner "Untaring"
+  tar jxf  ${LLVM_PKG_PATH}/llvm-${LLVM_SVN_REV}.tar.bz2
+  cd llvm
+  SubBanner "Patching"
+  patch -p0 < ${patch}
+  SubBanner "Configure"
+   ./configure --disable-jit --enable-optimized --target=arm-none-linux-gnueabi
+  SubBanner "Make"
+  nice make ${MAKE_OPTS} tools-only
+  SubBanner "Install"
+  cp Release/bin/llc ${INSTALL_ROOT}/arm-none-linux-gnueabi/llvm/bin/llc-sfi
+  cd ${saved_dir}
+}
+
+
+InstallTrustedLinkerScript() {
+  local trusted_ld_script=${INSTALL_ROOT}/codesourcery/ld_script_arm_trusted
+  local ld=${INSTALL_ROOT}/codesourcery/arm-2007q3/bin/arm-none-linux-gnueabi-ld
+  # We are using the output of "ld --verbose" which contains
+  # the linker script delimited by "=========".
+  # We are changing the image start address to 70000000
+  # to move the sel_ldr and other images "out of the way"
+  Banner "installing trusted linker script to ${trusted_ld_script}"
+
+  ${ld} --verbose |\
+      grep -A 10000 "=======" |\
+      grep -v "=======" |\
+      sed -e 's/00008000/70000000/g' > ${trusted_ld_script}
+}
+
+
+# We need to adjust the start address and aligment of nacl arm modules
+InstallUntrustedLinkerScript() {
+   Banner "installing untrusted ld script"
+   cp tools/llvm/ld_script_arm_untrusted ${INSTALL_ROOT}/arm-none-linux-gnueabi/
+ }
+
+
+# we copy some useful tools after building them first
+InstallMiscTools() {
+   Banner "building and installing misc tools"
+   SubBanner "sel loader"
+   # TODO(robertm): revisit some of these options
+  ./scons MODE=nacl,opt-linux platform=arm dangerous_debug_disable_inner_sandbox=1 sdl_mode=none sdl=none naclsdk_mode=manual naclsdk_validate=0
+   mkdir ${INSTALL_ROOT}/tools-arm
+   cp scons-out/opt-linux-arm/obj/src/trusted/service_runtime/sel_ldr\
+     ${INSTALL_ROOT}/tools-arm
+
+   SubBanner "validator"
+   ./scons MODE=opt-linux targetplatform=arm arm-ncval-core
+   mkdir ${INSTALL_ROOT}/tools-x86
+   cp scons-out/opt-linux-x86-32-to-arm/obj/src/trusted/validator_arm/v2/arm-ncval-core\
+       ${INSTALL_ROOT}/tools-x86
+}
+
+
+# the driver is a simple python script which changes its behavior
+# depending under the name it is invoked as
+InstallDriver() {
+  Banner "installing driver adaptors"
+  cp tools/llvm/llvm-fake.py ${INSTALL_ROOT}/arm-none-linux-gnueabi/
+  for s in gcc sfigcc g++ sfig++ ; do
+    local t="llvm-fake-$s"
+    echo "$t"
+    ln -s llvm-fake.py ${INSTALL_ROOT}/arm-none-linux-gnueabi/$t
+  done
+}
+
+
+#
+InstallNewlibAndNaClRuntime() {
+  Banner "building and installing nacl runtime"
+  SubBanner "building newib"
+   # TODO(robertm)
+  SubBanner "building extra sdk libs"
+  ./scons MODE=nacl_extra_sdk platform=arm sdl_mode=none sdl=none naclsdk_mode=manual naclsdk_validate=0 extra_sdk_clean extra_sdk_update_header install_libpthread extra_sdk_update
+  cp -r src/third_party/nacl_sdk/arm-newlib ${INSTALL_ROOT}
+}
+
+
+InstallExamples() {
+  Banner "installing examples into ${INSTALL_ROOT}/examples"
+  cp -r  tools/llvm/arm_examples ${INSTALL_ROOT}/examples
+}
+
 ######################################################################
 # Main
 ######################################################################
-
-if [[ ! -d ${INSTALL_ROOT} ]]; then
-  echo "ERROR: you must create the dir ${INSTALL_ROOT}"
-  exit
+if [ $# -eq 0 ] ; then
+  echo "you must specify a mode on the commandline:"
+  exit -1
 fi
 
-DownloadOrCopyCodeSourceryTarball
-ExtractLlvmBuildScript
-ConfigureAndBuildLlvm
+MODE=$1
+shift
+
+#@
+#@ help
+#@
+#@   print help for all modes
+if [ $MODE} = 'help' ] ; then
+  Usage
+  exit 0
+fi
+
+#@
+#@ make_tarball <tarball>
+#@
+#@   create sdk tarball
+if [ ${MODE} = 'make_tarball' ] ; then
+  PathSanityCheck
+  ClearInstallDir
+  DownloadOrCopyCodeSourceryTarball
+  ExtractLlvmBuildScript
+  ConfigureAndBuildLlvm
+  UntarPatchConfigureAndBuildSfiLlc
+  # TODO(robertm): install missing libs + headers
+  InstallTrustedLinkerScript
+  InstallUntrustedLinkerScript
+  InstallNewlibAndNaClRuntime
+  InstallMiscTools
+  InstallDriver
+  InstallExamples
+  PruneDirs
+  # TODO(robertm): install qemu
+  CreateTarBall $1
+  exit 0
+fi
+
+
+#@
+#@ llc-sfi
+#@
+#@   reinstall llc-sfi
+if [ ${MODE} = 'llc-sfi' ] ; then
+  UntarPatchConfigureAndBuildSfiLlc
+  exit 0
+fi
