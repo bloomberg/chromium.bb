1130de45c0da9c2005c205d03e858df72c877bfc
chase@chromium.org <chase@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-08 22:07:24 +0000

    Re-enable tab switching test.
    After some debugging on the interactive perf system, it
    appears the screen was locked (or at least locked up).
    Current theory is this is why OnPaint wasn't being
    called and the histogram wasn't being populated.
    
    BUG=28968
    TEST=tab switching test runs on slaves
    
    Review URL: http://codereview.chromium.org/463053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34093 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e67ed067f40dad801bfab37d9794b04186f1576b

--- chrome/test/tab_switching/tab_switching_test.cc
+++ chrome/test/tab_switching/tab_switching_test.cc
@@ -139,7 +139,7 @@ class TabSwitchingUITest : public UITest {
   DISALLOW_COPY_AND_ASSIGN(TabSwitchingUITest);
 };
 
-TEST_F(TabSwitchingUITest, DISABLED_GenerateTabSwitchStats) {
+TEST_F(TabSwitchingUITest, GenerateTabSwitchStats) {
   RunTabSwitchingUITest();
 }
 
