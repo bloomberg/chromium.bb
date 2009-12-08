36997500e81980f72f7b1b9994a1a901981aca87
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-08 23:47:14 +0000

    Try re-enabling the resize corner on Windows to see if it still causes perf regressions.
    BUG=458
    TEST=The bottom right corner of the window whould have a resize corner.
    Review URL: http://codereview.chromium.org/465133
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34108 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c34100d01eddc356741e2618d43b18d533ca9ac4

--- chrome/browser/views/frame/browser_view.cc
+++ chrome/browser/views/frame/browser_view.cc
@@ -229,11 +229,8 @@ class ResizeCorner : public views::View {
   }
 
   static gfx::Size GetSize() {
-    // This is disabled until we find what makes us slower when we let
-    // WebKit know that we have a resizer rect...
-    // return gfx::Size(views::NativeScrollBar::GetVerticalScrollBarWidth(),
-    //     views::NativeScrollBar::GetHorizontalScrollBarHeight());
-    return gfx::Size();
+    return gfx::Size(views::NativeScrollBar::GetVerticalScrollBarWidth(),
+                     views::NativeScrollBar::GetHorizontalScrollBarHeight());
   }
 
   virtual gfx::Size GetPreferredSize() {
