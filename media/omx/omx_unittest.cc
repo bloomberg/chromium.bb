f275765bb5e52687b20aeecece059d85336921dd
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-09 01:42:04 +0000

    Checking in the beginnings of omx_unittests.
    Also renamed omx_wrapper to the simpler (and more accurate!) omx.
    
    BUG=n/a
    TEST=n/a
    
    Review URL: http://codereview.chromium.org/467055
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34124 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: da4a37dfae588fc100816d1d3edf4b9772ac688c

---
+++ media/omx/omx_unittest.cc
@@ -0,0 +1,13 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace media {
+
+TEST(OmxTest, Pass) {
+  // TODO(scherkus): fill in with some sample OpenMAX tests.
+}
+
+}  // namespace media
