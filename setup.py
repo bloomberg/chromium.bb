85b3dceb2d7f0ead90f554ae38222fec440fd43c
agl@chromium.org <agl@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2009-12-11 19:32:21 +0000

    Add setup.py for installation.
    BUG=119
    Review URL: http://codereview.chromium.org/460153
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@767 78cadc50-ecff-11dd-a971-7dbc132099af

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from distutils.command.install import install
+from distutils.command.install_lib import install_lib
+from distutils.command.install_scripts import install_scripts
+
+setup(
+  name='gyp',
+  version='0.1',
+  description='Generate Your Projects',
+  author='Chromium Authors',
+  author_email='chromium-dev@googlegroups.com',
+  url='http://code.google.com/p/gyp',
+  package_dir = {'': 'pylib'},
+  packages=['gyp', 'gyp.generator'],
+
+  scripts = ['gyp'],
+  cmdclass = {'install': install,
+              'install_lib': install_lib,
+              'install_scripts': install_scripts},
+)
