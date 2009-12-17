7dc0a20e62d0c5a5fae309e0a1d9f94b09359b3e
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-17 04:27:24 +0000

    Workaround to prevent a crash checking extensions blacklist.
    Somehow the extensions dictionary is ending up null when we don't expect it to.
    This prevents the crash but may not prevent other problems. 
    
    BUG=29317
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/507030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34808 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: abac839b4a359636d6168d34d94ac6d043b6550b

--- chrome/browser/extensions/extension_prefs.cc
+++ chrome/browser/extensions/extension_prefs.cc
@@ -148,6 +148,12 @@ bool ExtensionPrefs::IsBlacklistBitSet(DictionaryValue* ext) {
 bool ExtensionPrefs::IsExtensionBlacklisted(const std::string& extension_id) {
   const DictionaryValue* extensions = prefs_->GetDictionary(kExtensionsPref);
   DCHECK(extensions);
+
+  // We've seen crash reports (crbug.com/29317) which seem to indicate
+  // that extensions can be null here, although we don't expect it to be.
+  if (!extensions)
+    return false;
+
   DictionaryValue* ext = NULL;
   if (!extensions->GetDictionary(ASCIIToWide(extension_id), &ext)) {
     // No such extension yet.
