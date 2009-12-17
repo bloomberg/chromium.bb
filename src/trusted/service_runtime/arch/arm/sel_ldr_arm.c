365ef7cfcc0480fe3082806ac38d2e29835b86bb
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2009-12-17 21:14:01 +0000

    Fixed bug that kept us from setting the alignment bit in elf headers for ARM Review URL: http://codereview.chromium.org/501088
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1149 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 5c14e3c19ddd542e8388dfe72d91cf2bc99c7e24

--- src/trusted/service_runtime/arch/arm/sel_ldr_arm.c
+++ src/trusted/service_runtime/arch/arm/sel_ldr_arm.c
@@ -36,8 +36,16 @@
 #include "native_client/src/trusted/service_runtime/tramp.h"
 #include "native_client/src/trusted/service_runtime/arch/arm/sel_ldr_arm.h"
 
+
+/* NOTE(robertm): the trampoline organization for ARM is currenly assuming
+ * NACL_TRAMPOLINE_SIZE == 32. This is contrary to the bundle size
+ * which is 16. The reason for this is tramp.S which has a payload
+ * 5 instr + one data item
+ */
+
 /*
  * Install a syscall trampoline at target_addr.  NB: Thread-safe.
+ * The code being patched is from tramp.S
  */
 void  NaClPatchOneTrampoline(struct NaClApp *nap,
                              uintptr_t      target_addr) {
@@ -111,13 +119,14 @@ void NaClFillEndOfTextRegion(struct NaClApp *nap) {
 /*
  * patch a tls hook, which is used by nacl module for obtaining its tls pointer,
  * to the end of trampoline region minus one slot.
+ * The code being patched is from nacl_tls_tramp.S
  */
 void NaClLoadTlsHook(struct NaClApp *nap) {
   struct NaClPatchInfo  patch_info;
   struct NaClPatch      abs32;
-  uintptr_t tls_hook_addr;
-
-  NaClLog(2, "Installing tls hook\n");
+  const uintptr_t       tls_hook_addr = NACL_TRAMPOLINE_END -
+                                        2 * NACL_SYSCALL_BLOCK_SIZE;
+  NaClLog(2, "Installing tls hook at 0x%08"PRIxPTR"\n", tls_hook_addr);
 
   patch_info.rel32 = 0;
   patch_info.num_rel32 = 0;
@@ -125,9 +134,6 @@ void NaClLoadTlsHook(struct NaClApp *nap) {
   patch_info.num_abs32 = 0;
   patch_info.abs16 = 0;
   patch_info.num_abs16 = 0;
-
-  tls_hook_addr = NACL_TRAMPOLINE_END - 2 * nap->bundle_size;
-
   patch_info.dst = nap->mem_start + tls_hook_addr;
   patch_info.src = (uintptr_t) &NaClReadTP_start;
   patch_info.nbytes = ((uintptr_t) &NaClReadTP_end
@@ -138,13 +144,18 @@ void NaClLoadTlsHook(struct NaClApp *nap) {
   NaClApplyPatchToMemory(&patch_info);
 }
 
+/*
+ * patch in springboard.S code into space in place of
+ * the last syscall in the trampoline region.
+ * The code being patched is from springboard.S
+ */
+
 void  NaClLoadSpringboard(struct NaClApp  *nap) {
-  /*
-   * patch in springboard.S code into space in place of
-   * the last syscall in the trampoline region.
-   */
   struct NaClPatchInfo  patch_info;
   struct NaClPatch      abs32;
+  const uintptr_t       springboard_addr = NACL_TRAMPOLINE_END -
+                                           NACL_SYSCALL_BLOCK_SIZE;
+  NaClLog(2, "Installing springboard at 0x%08"PRIxPTR"\n", springboard_addr);
 
   patch_info.rel32 = 0;
   patch_info.num_rel32 = 0;
@@ -152,15 +163,12 @@ void  NaClLoadSpringboard(struct NaClApp  *nap) {
   patch_info.num_abs32 = 0;
   patch_info.abs16 = 0;
   patch_info.num_abs16 = 0;
-
-  nap->springboard_addr = NACL_TRAMPOLINE_END - nap->bundle_size;
-
-  patch_info.dst = nap->mem_start + nap->springboard_addr;
+  patch_info.dst = nap->mem_start + springboard_addr;
   patch_info.src = (uintptr_t) &NaCl_springboard;
   patch_info.nbytes = ((uintptr_t) &NaCl_springboard_end
                        - (uintptr_t) &NaCl_springboard);
 
   NaClApplyPatchToMemory(&patch_info);
 
-  nap->springboard_addr += NACL_HALT_LEN; /* skip the hlt */
+  nap->springboard_addr = springboard_addr + NACL_HALT_LEN; /* skip the hlt */
 }
