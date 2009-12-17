a43a3533755186fbbb1475c5c2ceb2777837a403
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2009-12-17 01:20:20 +0000

    Fix path messed up by 34779.
    BUG=30507
    
    Review URL: http://codereview.chromium.org/500085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@34794 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c76019107ecd091ffe8e024fa0c02c6189ce01ad

--- chrome/test/data/extensions/api_test/tabs/basics/test.js
+++ chrome/test/data/extensions/api_test/tabs/basics/test.js
@@ -485,7 +485,7 @@ chrome.test.runTests([
 
   function setupConnect() {
     // The web page that our content script will be injected into.
-    var relativePath = '/files/extensions/api_test/tabs/relative.html';
+    var relativePath = '/files/extensions/api_test/tabs/basics/relative.html';
     var testUrl = 'http://localhost:1337' + relativePath;
 
     setupWindow([testUrl], pass(function(winId, tabIds) {
@@ -573,4 +573,3 @@ chrome.test.runTests([
   // It would be an improvement to check the captureVisibleTab results
   // against a known-good result.
 ]);
-
