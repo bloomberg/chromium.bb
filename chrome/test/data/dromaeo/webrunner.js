90c9b04e8a9c296c240fd5467caab9721f2f4fe0
antonm@chromium.org <antonm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-11 19:34:00 +0000

    Updating to the latest Dromaeo.
    Review URL: http://codereview.chromium.org/519009
    
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@35926 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c2c1d9a9c1643fdda7ee0335d7d9b0e9a4a92420

--- chrome/test/data/dromaeo/webrunner.js
+++ chrome/test/data/dromaeo/webrunner.js
@@ -234,7 +234,7 @@
 	var post_json = false;
 	
 	// Query String Parsing
-	var search = (window.location.search || "?").substr(1);
+	var search = window.limitSearch || (window.location.search || "?").substr(1);
 
 	search = search.replace(/&runStyle=([^&]+)/, function(all, type){
 		runStyle = type;
@@ -365,8 +365,25 @@
 			
 			$("#overview input").remove();
 			updateTimebar();
+
+			if ( window.limitSearch ) {
+				var summary = (runStyle === "runs/s" ? Math.pow(Math.E, maxTotal / maxTotalNum) : maxTotal).toFixed(2);
+
+				if ( typeof tpRecordTime !== "undefined" ) {
+					tpRecordTime( summary );
+
+				} else {
+					var pre = document.createElement("pre");
+					pre.style.display = "none";
+					pre.innerHTML = "__start_report" + summary + "__end_report";
+					document.body.appendChild( pre );
+				}
+
+				if ( typeof goQuitApplication !== "undefined" ) {
+					goQuitApplication();
+				}
 	
-			if ( dataStore && dataStore.length ) {
+			} else if ( dataStore && dataStore.length ) {
 				if (!automated || post_json) {
 					$("body").addClass("alldone");
 					var div = jQuery("<div class='results'>Saving...</div>").insertBefore("#overview");
@@ -421,6 +438,10 @@
 				.click(function(){});
 			interval = true;
 			dequeue();
+		}
+
+		if ( window.limitSearch ) {
+			$("#pause").click();
 		}
 	}
 
