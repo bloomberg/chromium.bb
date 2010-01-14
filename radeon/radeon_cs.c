74937cda177363ff4ede9e2b4ea2bec04cda892e
Jerome Glisse <jglisse@redhat.com>
2010-01-14 11:28:25 +0100

    radeon: indentation + trailing space cleanup

--- radeon/radeon_cs.c
+++ radeon/radeon_cs.c
@@ -3,42 +3,41 @@
 #include "radeon_cs.h"
 #include "radeon_cs_int.h"
 
-struct radeon_cs *radeon_cs_create(struct radeon_cs_manager *csm,
-			    uint32_t ndw)
+struct radeon_cs *radeon_cs_create(struct radeon_cs_manager *csm, uint32_t ndw)
 {
     struct radeon_cs_int *csi = csm->funcs->cs_create(csm, ndw);
     return (struct radeon_cs *)csi;
 }
 
 int radeon_cs_write_reloc(struct radeon_cs *cs,
-			  struct radeon_bo *bo,
-			  uint32_t read_domain,
-			  uint32_t write_domain,
-			  uint32_t flags)
+                          struct radeon_bo *bo,
+                          uint32_t read_domain,
+                          uint32_t write_domain,
+                          uint32_t flags)
 {
     struct radeon_cs_int *csi = (struct radeon_cs_int *)cs;
 
     return csi->csm->funcs->cs_write_reloc(csi,
-					   bo,
-					   read_domain,
-					   write_domain,
-					   flags);
+                                           bo,
+                                           read_domain,
+                                           write_domain,
+                                           flags);
 }
 
 int radeon_cs_begin(struct radeon_cs *cs,
-		    uint32_t ndw,
-		    const char *file,
-		    const char *func,
-		    int line)
+                    uint32_t ndw,
+                    const char *file,
+                    const char *func,
+                    int line)
 {
     struct radeon_cs_int *csi = (struct radeon_cs_int *)cs;
     return csi->csm->funcs->cs_begin(csi, ndw, file, func, line);
 }
 
 int radeon_cs_end(struct radeon_cs *cs,
-		  const char *file,
-		  const char *func,
-		  int line)
+                  const char *file,
+                  const char *func,
+                  int line)
 {
     struct radeon_cs_int *csi = (struct radeon_cs_int *)cs;
     return csi->csm->funcs->cs_end(csi, file, func, line);
@@ -78,9 +77,9 @@ void radeon_cs_set_limit(struct radeon_cs *cs, uint32_t domain, uint32_t limit)
 {
     struct radeon_cs_int *csi = (struct radeon_cs_int *)cs;
     if (domain == RADEON_GEM_DOMAIN_VRAM)
-	csi->csm->vram_limit = limit;
+        csi->csm->vram_limit = limit;
     else
-	csi->csm->gart_limit = limit;
+        csi->csm->gart_limit = limit;
 }
 
 void radeon_cs_space_set_flush(struct radeon_cs *cs, void (*fn)(void *), void *data)
@@ -89,4 +88,3 @@ void radeon_cs_space_set_flush(struct radeon_cs *cs, void (*fn)(void *), void *d
     csi->space_flush_fn = fn;
     csi->space_flush_data = data;
 }
-
