c5b28ec89c407edf84543a98f00e107d25f5cd0a
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-20 03:17:37 +0000

    Revert 36604 - Implemented AddressWatchTask for OS X.
    Added some utility methods to ServerConnectionManager.
    
    BUG=32430
    TEST=Manual testing with a laptop
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=36586
    
    Review URL: http://codereview.chromium.org/543098
    
    TBR=akalin@chromium.org
    Review URL: http://codereview.chromium.org/553025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36605 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2dccf1b6351bbcdbea1a81a2563f1a5840a29507

--- chrome/browser/sync/glue/sync_backend_host.h
+++ chrome/browser/sync/glue/sync_backend_host.h
@@ -189,8 +189,7 @@ class SyncBackendHost {
                              const std::wstring& test_user,
                              sync_api::HttpPostProviderFactory* factory,
                              sync_api::HttpPostProviderFactory* auth_factory) {
-        DoInitialize(GURL("http://www.example.com/example/path"),
-                     bookmark_model_worker, false, factory,
+        DoInitialize(GURL(), bookmark_model_worker, false, factory,
                      auth_factory, std::string());
         syncapi_->SetupForTestMode(test_user);
     }
