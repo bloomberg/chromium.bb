15564bd720ab7cf0f32e7bededfdcbc179f1a680
cira@chromium.org <cira@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-23 17:47:28 +0000

    Revert 36966 - Trying to reland. It's the same as http://codereview.chromium.org/546040. Moved IsValidName to public (where it was before the change), and made test cases for it cleaner by explicitly using std::string() around var name.
    Add reserved messages to ExtensionMessageBundle dictionary. They are of the form @@somename, i.e. @@ui_locale.
    
    It makes easier for developers to detect current UI locale (also available though window.navigator.language), or text
    direction. I'll use them for static message replacement later on too.
    
    Before this change developers would have to manualy add locale and text_dir messages and translate for all supported locales.
    
    Added 5 reserved messages:
    @@ui_locale
    @@bidi_dir
    @@bidi_reversed_dir
    @@bidi_start_edge
    @@bidi_end_edge
    
    See http://code.google.com/apis/gadgets/docs/i18n.html#BIDI on why are they usefull.
    
    Extended allowed charset for variable names with @. Added unittest for variable names.
    
    TBR=jshin@chromium.org
    
    Review URL: http://codereview.chromium.org/551120
    
    TBR=cira@chromium.org
    Review URL: http://codereview.chromium.org/545183
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36967 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fe9d18904e553876bc9c738753ac6f2dae2577d1

--- chrome/common/extensions/extension_constants.cc
+++ chrome/common/extensions/extension_constants.cc
@@ -205,8 +205,6 @@ const char* kLocalesMessagesFileMissing =
     "Messages file is missing for locale.";
 const char* kInvalidOptionsPage =
     "Invalid value for 'options_page'.";
-const char* kReservedMessageFound =
-    "Reserved key * found in message catalog.";
 }  // namespace extension_manifest_errors
 
 namespace extension_urls {
