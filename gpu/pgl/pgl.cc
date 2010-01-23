2eda372088e43e286d74272010d4df7b2d79940d
laforge@chromium.org <laforge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-23 22:08:50 +0000

    Revert 36399 - linux: enable pepper_test_plugin
    This is breaking official x64 builds, notified team of break earlier in the 
    week, but haven't seen a fix.  I'm reverting now.  
    
    Please fix before re-enabling.
    
    Original message:
    This includes a way to build it with full support on x64 (and arm ?), by specifying linux_fpic=1 in GYP_DEFINES and rerunning gyp.
    
    Review URL: http://codereview.chromium.org/551041
    
    TBR=piman@chromium.org
    Review URL: http://codereview.chromium.org/548133
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@36969 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b656d73aaa4d75163e1226d4c4096560187f2488

--- gpu/pgl/pgl.cc
+++ gpu/pgl/pgl.cc
@@ -152,7 +152,7 @@ PGLBoolean pglDestroyContext(PGLContext pgl_context) {
   if (!pgl_context)
     return false;
 
-  delete static_cast<PGLContextImpl*>(pgl_context);
+  delete pgl_context;
   return true;
 }
 
