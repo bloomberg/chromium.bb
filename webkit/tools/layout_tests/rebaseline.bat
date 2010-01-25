592ee2256255d39985e9b0e4adf22c6be435fdff
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-01-25 23:06:35 +0000

    Fix rebaseline.bat to work again
    TBR=dpranke
    TEST=rebaseline.bat works again!
    BUG=none
    
    Review URL: http://codereview.chromium.org/555086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@37054 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 55ac7ea2120b48f88040289986bb46d530e6c296

--- webkit/tools/layout_tests/rebaseline.bat
+++ webkit/tools/layout_tests/rebaseline.bat
@@ -1,1 +1,1 @@
-%~dp0..\..\..\third_party\python_24\python.exe %~dp0\webkitpy\layout_tests\rebaseline.py %*
+%~dp0..\..\..\third_party\python_24\python.exe %~dp0\webkitpy\layout_tests\rebaseline_chromium_webkit_tests.py %*
