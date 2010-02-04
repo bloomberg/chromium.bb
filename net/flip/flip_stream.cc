df6cdb0c416ff813c666deba6369616fb3b44985
vandebo@google.com <vandebo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-04 21:25:33 +0000

    Add a notion of 'eof' to UploadDataStream, replacing the use of its size property for detecting when an upload is finished.
    While this does prevent the crash described in the bug from occurring, this doesn't fully solve the problem as now the affected uploads don't complete. Fully resolving the issue will require implementing the chunked transfer encoding for requests.
    
    Patch from Vernon Tang <vt@foilhead.net>, original review: http://codereview.chromium.org/555194
    
    BUG=33501
    TEST=Create a file with a non-zero size and select that file in an HTML-based uploader. Before starting the upload, remove read permissions from that file. Check that the upload doesn't cause the browser to crash or hang. net_unittests: HttpNetworkTransactionTest.UploadFileSmallerThanLength, UploadDataStreamTest.*
    
    Review URL: http://codereview.chromium.org/578004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38129 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 95d88ffe2faa5ccceb1c4619f4e316fb7ad4d70f

--- net/flip/flip_stream.cc
+++ net/flip/flip_stream.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -381,7 +381,7 @@ int FlipStream::DoSendBodyComplete(int result) {
 
   request_body_stream_->DidConsume(result);
 
-  if (request_body_stream_->position() < request_body_stream_->size())
+  if (!request_body_stream_->eof())
     io_state_ = STATE_SEND_BODY;
   else
     io_state_ = STATE_READ_HEADERS;
