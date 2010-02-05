404d46ab5dc152be7feb16e27915c57a27355716
gregoryd@google.com <gregoryd@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-05 18:01:37 +0000

    Prepare for running nacl_ui_test on Mac and Linux. This is the NaCl-Chrome integration test that runs in Chrome waterfall, currently on Windows only. I am working on starting new bots that will run it on Mac and Linux.
    Review URL: http://codereview.chromium.org/563021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38218 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cd023e807914d91d64e325391bde25ff0bdea3f3

--- chrome/test/nacl/nacl_test.cc
+++ chrome/test/nacl/nacl_test.cc
@@ -29,6 +29,12 @@ static const FilePath::CharType kServerHtmlFileName[] =
 NaClTest::NaClTest()
     : UITest() {
   launch_arguments_.AppendSwitch(switches::kInternalNaCl);
+#if defined(OS_MACOSX) || defined(OS_LINUX)
+  launch_arguments_.AppendSwitch(switches::kNoSandbox);
+#endif
+#if defined(OS_MACOSX)
+  launch_arguments_.AppendSwitch(switches::kEnableNaClOnMac);
+#endif
 }
 
 NaClTest::~NaClTest() {}
