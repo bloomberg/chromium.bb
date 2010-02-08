28a7a94e52dc391488a6b9e79c4c39a60643f31c
mark@chromium.org <mark@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2010-02-08 19:50:27 +0000

    [Mac] Alters script generator to use absolute path to sysctl because it can cause issues with builds on the newest Xcode version (and sometimes older ones).
    BUG=none
    TEST=none
    
    Patch by Andy Bonventre <andybons@chromium.org>
    
    Review URL: http://codereview.chromium.org/588002
    
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@778 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/generator/xcode.py
+++ pylib/gyp/generator/xcode.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-# Copyright (c) 2009 Google Inc. All rights reserved.
+# Copyright (c) 2010 Google Inc. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -974,7 +974,7 @@ def GenerateOutput(target_list, target_dicts, data, params):
         # Xeons, a build can quickly run out of processes based on
         # scheduling/other tasks, and randomly failing builds are no good.
         script = \
-"""JOB_COUNT="$(sysctl -n hw.ncpu)"
+"""JOB_COUNT="$(/usr/sbin/sysctl -n hw.ncpu)"
 if [ "${JOB_COUNT}" -gt 4 ]; then
   JOB_COUNT=4
 fi
