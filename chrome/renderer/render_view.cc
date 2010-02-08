8a823ddf3c81e6faa9f99afaeb6677f129816e57
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-08 23:46:04 +0000

    Remove uses of the deprecated hideAutofillPopup and replace it with the new hideSuggestionsPopup.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/586011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38413 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4605325dfa68afbcf85385430d402da457684aea

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -3773,13 +3773,13 @@ void RenderView::AltErrorPageFinished(WebFrame* frame,
 
 void RenderView::OnMoveOrResizeStarted() {
   if (webview())
-    webview()->hideAutofillPopup();
+    webview()->hideSuggestionsPopup();
 }
 
 void RenderView::OnResize(const gfx::Size& new_size,
                           const gfx::Rect& resizer_rect) {
   if (webview())
-    webview()->hideAutofillPopup();
+    webview()->hideSuggestionsPopup();
   RenderWidget::OnResize(new_size, resizer_rect);
 }
 
