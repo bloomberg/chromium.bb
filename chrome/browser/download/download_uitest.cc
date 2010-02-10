e510d8a7289ef51a68bfc650d82f3ca6490fc8d6
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-10 17:53:04 +0000

    Mark download tests as flaky on all platform, just the sad truth.
    TBR=dglazkov, kuchhal
    TEST=still flaky
    BUG=35275, 20809
    
    Review URL: http://codereview.chromium.org/601030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38630 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 090648d673cf5767f308472bf3d3457404044127

--- chrome/browser/download/download_uitest.cc
+++ chrome/browser/download/download_uitest.cc
@@ -27,12 +27,6 @@
 #include "net/base/net_util.h"
 #include "net/url_request/url_request_unittest.h"
 
-#if defined(OS_WIN)
-#define FLAKYONWIN(Test) FLAKY_ ## Test
-#else
-#define FLAKYONWIN(Test) Test
-#endif
-
 namespace {
 
 const wchar_t kDocRoot[] = L"chrome/test/data";
@@ -182,8 +176,9 @@ class DownloadTest : public UITest {
 
 // Download a file with non-viewable content, verify that the
 // download tab opened and the file exists.
-// FLAKYONWIN: see http://crbug.com/20809
-TEST_F(DownloadTest, FLAKYONWIN(DownloadMimeType)) {
+// All download tests are flaky on all platforms, http://crbug.com/35275.
+// Additionally, there is Windows-specific flake, http://crbug.com/20809.
+TEST_F(DownloadTest, FLAKY_DownloadMimeType) {
   FilePath file(FILE_PATH_LITERAL("download-test1.lib"));
 
   EXPECT_EQ(1, GetTabCount());
@@ -204,8 +199,9 @@ TEST_F(DownloadTest, FLAKYONWIN(DownloadMimeType)) {
 
 // Access a file with a viewable mime-type, verify that a download
 // did not initiate.
-// FLAKYONWIN: see http://crbug.com/20809
-TEST_F(DownloadTest, FLAKYONWIN(NoDownload)) {
+// All download tests are flaky on all platforms, http://crbug.com/35275.
+// Additionally, there is Windows-specific flake, http://crbug.com/20809.
+TEST_F(DownloadTest, FLAKY_NoDownload) {
   FilePath file(FILE_PATH_LITERAL("download-test2.html"));
   FilePath file_path = download_prefix_.Append(file);
 
@@ -232,7 +228,8 @@ TEST_F(DownloadTest, FLAKYONWIN(NoDownload)) {
 // Download a 0-size file with a content-disposition header, verify that the
 // download tab opened and the file exists as the filename specified in the
 // header.  This also ensures we properly handle empty file downloads.
-// See bug http://crbug.com/20809
+// All download tests are flaky on all platforms, http://crbug.com/35275.
+// Additionally, there is Windows-specific flake, http://crbug.com/20809.
 TEST_F(DownloadTest, FLAKY_ContentDisposition) {
   FilePath file(FILE_PATH_LITERAL("download-test3.gif"));
   FilePath download_file(FILE_PATH_LITERAL("download-test3-attachment.gif"));
@@ -257,8 +254,9 @@ TEST_F(DownloadTest, FLAKY_ContentDisposition) {
 // tab, opening a second tab, closing the shelf, going back to the first tab,
 // and checking that the shelf is closed.
 // See bug http://crbug.com/26325
-// FLAKYONWIN: see http://crbug.com/20809
-TEST_F(DownloadTest, FLAKYONWIN(PerWindowShelf)) {
+// All download tests are flaky on all platforms, http://crbug.com/35275.
+// Additionally, there is Windows-specific flake, http://crbug.com/20809.
+TEST_F(DownloadTest, FLAKY_PerWindowShelf) {
   FilePath file(FILE_PATH_LITERAL("download-test3.gif"));
   FilePath download_file(FILE_PATH_LITERAL("download-test3-attachment.gif"));
 
@@ -302,9 +300,9 @@ TEST_F(DownloadTest, FLAKYONWIN(PerWindowShelf)) {
 // The test will first attempt to download a file; but the server will "pause"
 // in the middle until the server receives a second request for
 // "download-finish.  At that time, the download will finish.
-// Flaky on Linux: http://code.google.com/p/chromium/issues/detail?id=14746
-// FLAKYONWIN: see http://crbug.com/20809
-TEST_F(DownloadTest, FLAKYONWIN(UnknownSize)) {
+// All download tests are flaky on all platforms, http://crbug.com/35275.
+// Additionally, there is Windows-specific flake, http://crbug.com/20809.
+TEST_F(DownloadTest, FLAKY_UnknownSize) {
   GURL url(URLRequestSlowDownloadJob::kUnknownSizeUrl);
   FilePath filename;
   net::FileURLToFilePath(url, &filename);
@@ -325,8 +323,9 @@ TEST_F(DownloadTest, DISABLED_KnownSize) {
 
 // Test that when downloading an item in Incognito mode, we don't crash when
 // closing the last Incognito window (http://crbug.com/13983).
-// FLAKYONWIN: see http://crbug.com/20809
-TEST_F(DownloadTest, FLAKYONWIN(IncognitoDownload)) {
+// All download tests are flaky on all platforms, http://crbug.com/35275.
+// Additionally, there is Windows-specific flake, http://crbug.com/20809.
+TEST_F(DownloadTest, FLAKY_IncognitoDownload) {
   // Open a regular window and sanity check default values for window / tab
   // count and shelf visibility.
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
@@ -367,6 +366,3 @@ TEST_F(DownloadTest, FLAKYONWIN(IncognitoDownload)) {
 }
 
 }  // namespace
-
-#undef FLAKYONWIN
-
