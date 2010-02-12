b04d0f15cc6b71fc99879ab59536f9b2915ad3e5
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-12 21:40:25 +0000

    10.6 SDK compatibility fix for BookmarkMenuCocoaController following r37056.
    BookmarkMenuCocoaController should implement the NSMenuDelegate protocol.
    
    BUG=35569
    TEST=10.6 SDK build (mac_sdk=10.6)
    Review URL: http://codereview.chromium.org/603045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@38955 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bbac762de958104c049b35db17d83681aa2c2830

--- chrome/browser/cocoa/bookmark_menu_cocoa_controller.h
+++ chrome/browser/cocoa/bookmark_menu_cocoa_controller.h
@@ -11,10 +11,12 @@
 
 #import <Cocoa/Cocoa.h>
 
+#import "base/cocoa_protocols_mac.h"
+
 class BookmarkNode;
 class BookmarkMenuBridge;
 
-@interface BookmarkMenuCocoaController : NSObject {
+@interface BookmarkMenuCocoaController : NSObject<NSMenuDelegate> {
  @private
   BookmarkMenuBridge* bridge_;  // weak; owns me
 }
