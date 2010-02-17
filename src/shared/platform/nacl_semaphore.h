41e0ad5302c725ef27861f64afbca489908353ae
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-02-17 14:55:36 +0000

    Add missing EXTERN_C_BEGIN/EXTERN_C_END bracketing to header file
    Review URL: http://codereview.chromium.org/607012
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1513 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: b8707348ce06c064a3d41efd430128ba5f1ae43c

--- src/shared/platform/nacl_semaphore.h
+++ src/shared/platform/nacl_semaphore.h
@@ -10,6 +10,7 @@
 #ifndef NATIVE_CLIENT_SRC_TRUSTED_PLATFORM_NACL_SEMAPHORE_H_
 #define NATIVE_CLIENT_SRC_TRUSTED_PLATFORM_NACL_SEMAPHORE_H_
 
+#include "native_client/src/include/nacl_base.h"
 
 #if NACL_WINDOWS
 #include "native_client/src/shared/platform/win/nacl_semaphore.h"
@@ -20,6 +21,8 @@
 #else
 #error "Unknown platform!!!"
 #endif
+
+EXTERN_C_BEGIN
 
 int NaClSemCtor(struct NaClSemaphore *sem, int32_t value);
 
@@ -32,5 +35,7 @@ NaClSyncStatus NaClSemTryWait(struct NaClSemaphore *sem);
 NaClSyncStatus NaClSemPost(struct NaClSemaphore *sem);
 
 int NaClSemGetValue(struct NaClSemaphore *sem);
+
+EXTERN_C_END
 
 #endif  /* NATIVE_CLIENT_SRC_TRUSTED_PLATFORM_NACL_SEMAPHORE_H_ */
