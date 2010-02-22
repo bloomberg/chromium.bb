0c2a1a8c092f8d101d635ae5596cacc053d510e6
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-22 23:16:30 +0000

    Revert 39642 - Update some windowsonly browser tests to run on all platforms.
    TEST=Ran affected browser tests.
    BUG=None
    
    Review URL: http://codereview.chromium.org/618010
    
    TBR=skerner@chromium.org
    Review URL: http://codereview.chromium.org/652084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39657 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 91aecd4f50ae915ea4bb21b650d05304309ce977

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -4,19 +4,7 @@
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-#include "chrome/browser/browser.h"
-#include "chrome/browser/pref_service.h"
-#include "chrome/browser/profile.h"
-#include "chrome/common/pref_names.h"
-
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Tabs) {
   StartHTTPServer();
-
-  // The test creates a tab and checks that the URL of the new tab
-  // is that of the new tab page.  Make sure the pref that controls
-  // this is set.
-  browser()->profile()->GetPrefs()->SetBoolean(
-      prefs::kHomePageIsNewTabPage, true);
-
   ASSERT_TRUE(RunExtensionTest("tabs/basics")) << message_;
 }
