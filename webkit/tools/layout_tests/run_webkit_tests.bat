ae5260cf9be383a0fc57d36ed9398a0807ce49fb
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-22 23:25:25 +0000

    Update various test scripts to use the upstream version of run-chromium-webkit-tests and rebaseline-chromium-webkit-tests
      BUG=23099
      R=victorw
      TEST=bots don't explode
    
    Review URL: http://codereview.chromium.org/639001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39662 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d72b4870298a6f5e10179d757c061cea0f3d23a1

--- webkit/tools/layout_tests/run_webkit_tests.bat
+++ webkit/tools/layout_tests/run_webkit_tests.bat
@@ -1,1 +1,1 @@
-@%~dp0\..\..\..\third_party\python_24\python.exe %~dp0\run_webkit_tests.py %*
+@%~dp0\..\..\..\third_party\python_24\python.exe %~dp0\..\..\..\third_party\WebKit\WebKitTools\Scripts\run-chromium-webkit-tests %*
--- webkit/tools/layout_tests/run_webkit_tests.sh
+++ webkit/tools/layout_tests/run_webkit_tests.sh
@@ -5,6 +5,7 @@
 # found in the LICENSE file.
 
 exec_dir=$(dirname $0)
+script_dir=${exec_dir}/../../../third_party/WebKit/WebKitTools/Scripts
 
 if [ "$OSTYPE" = "cygwin" ]; then
   system_root=`cygpath "$SYSTEMROOT"`
@@ -13,10 +14,7 @@ if [ "$OSTYPE" = "cygwin" ]; then
   PYTHON_PROG="$exec_dir/../../../third_party/python_24/python.exe"
 else
   PYTHON_PROG=python
-  # Specifically, run_webkit_tests needs the paths in:
-  # third_party/python_24/Lib/site-packages/google.pth
-  PYTHONPATH="${exec_dir}/../../../tools/python:$PYTHONPATH"
   export PYTHONPATH
 fi
 
-"$PYTHON_PROG" "$exec_dir/run_webkit_tests.py" "$@"
+"$PYTHON_PROG" "$script_dir/run-chromium-webkit-tests" "$@"
