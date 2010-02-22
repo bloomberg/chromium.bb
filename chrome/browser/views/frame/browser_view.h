00f6336a3a9aa7395f2d8a8d4c11fffc35f928ad
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-22 22:56:57 +0000

    Build breakage fix
    TBR=sky@chromium.org
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/650179
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39650 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c4c12e9d1a8ee46eacc7252eda9cebc0b0250059

--- chrome/browser/views/frame/browser_view.h
+++ chrome/browser/views/frame/browser_view.h
@@ -210,7 +210,7 @@ class BrowserView : public BrowserBubbleHost,
   // Returns true if the Browser object associated with this BrowserView is a
   // popup window.
   bool IsBrowserTypePopup() const {
-    return browser_->type() & Browser::TYPE_POPUP != 0;
+    return (browser_->type() & Browser::TYPE_POPUP) != 0;
   }
 
   // Returns true if the frame containing this BrowserView should show the
