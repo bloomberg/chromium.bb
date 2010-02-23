edc6f5e8125266e2d8c53ea43b9cebac512efef3
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-23 11:10:03 +0000

    Revert back to using Send for now.
    Sadly, my attempts to isolate the performance problem were not fruitful.
    Tomorrow is another day...
    
    TBR=jam
    BUG=36310
    TEST=none
    
    Review URL: http://codereview.chromium.org/654007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39710 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: eb9dd4550b8594f08ad8c9353ab2013e7860780b

--- chrome/renderer/renderer_webkitclient_impl.cc
+++ chrome/renderer/renderer_webkitclient_impl.cc
@@ -131,14 +131,12 @@ WebString RendererWebKitClientImpl::cookies(
   // when there is no active script context.
   int32 routing_id = RenderThread::RoutingIDForCurrentContext();
 
-  // TODO(darin): This means that network load completions may run within the
-  // context of a document.cookie call.  This violates the "run to completion"
-  // gaurantee of JS.  This is just here as a temporary measure to isolate a
-  // performance regression.
-  RenderThread::current()->DoNotNotifyWebKitOfModalLoop();
+  // TODO(darin): We should use SendAndRunNestedMessageLoop here to avoid dead-
+  // locking the browser, but this causes a performance regression.  Switching
+  // back to Send to verify.  See http://crbug.com/36310.
 
   std::string value_utf8;
-  RenderThread::current()->SendAndRunNestedMessageLoop(
+  RenderThread::current()->Send(
       new ViewHostMsg_GetCookies(routing_id, url, first_party_for_cookies,
                                  &value_utf8));
 
