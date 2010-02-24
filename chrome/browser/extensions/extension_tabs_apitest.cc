d5329e9a6caecc84ac70a284be4304ff6cbafea6
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-24 19:47:31 +0000

    Update some windows-only browser tests to run on all platforms.
    TEST=Ran affected browser tests.
    BUG=None
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=39642
    
    Review URL: http://codereview.chromium.org/618010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39911 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dfd6ee455e4c99ca032d2a9fbb9ad668c82d2587

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -4,7 +4,26 @@
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Tabs) {
+#include "chrome/browser/browser.h"
+#include "chrome/browser/pref_service.h"
+#include "chrome/browser/profile.h"
+#include "chrome/common/pref_names.h"
+
+// TODO(skerner): This test is flaky in chromeos.  Figure out why and fix.
+#if defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
+#define MAYBE_Tabs DISABLED_Tabs
+#else
+#define MAYBE_Tabs Tabs
+#endif
+
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_Tabs) {
   StartHTTPServer();
+
+  // The test creates a tab and checks that the URL of the new tab
+  // is that of the new tab page.  Make sure the pref that controls
+  // this is set.
+  browser()->profile()->GetPrefs()->SetBoolean(
+      prefs::kHomePageIsNewTabPage, true);
+
   ASSERT_TRUE(RunExtensionTest("tabs/basics")) << message_;
 }
