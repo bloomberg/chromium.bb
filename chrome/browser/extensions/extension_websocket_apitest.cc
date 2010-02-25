e444e5f4830a2ce97da0da4a6218853fda35310f
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-25 02:38:44 +0000

    Flakify ExtensionApiTest.WebSocket
    Has been flaky on windows
    
    TBR=markus
    
    Review URL: http://codereview.chromium.org/660064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@39982 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: efeaf7e7f80c74b91aa87edbbb0ace868f3574a7

--- chrome/browser/extensions/extension_websocket_apitest.cc
+++ chrome/browser/extensions/extension_websocket_apitest.cc
@@ -7,7 +7,7 @@
 #include "chrome/test/ui_test_utils.h"
 #include "net/base/mock_host_resolver.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WebSocket) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, FLAKY_WebSocket) {
   FilePath websocket_root_dir;
   PathService::Get(chrome::DIR_TEST_DATA, &websocket_root_dir);
   websocket_root_dir = websocket_root_dir.AppendASCII("layout_tests")
