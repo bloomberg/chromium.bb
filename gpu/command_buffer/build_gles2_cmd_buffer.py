4791cbe84156598eedb12505753406764ecefe53
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-02-27 11:01:53 +0000

    Bullshit CL to get the buildbots to try again because the chrome's flaky ass tests that break random builds
    TEST=flaky
    BUG=none
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40221 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1b7e93b7e3ac1f74f61a83d6c6eb1575d427b578

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""code generator for GL command buffers."""
+"""code generator for GLES2 command buffers."""
 
 import os
 import os.path
