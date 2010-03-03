b955a05ef087ebac5894d3683e012148651c9b4d
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-03 23:51:19 +0000

    Remove debug info used to diagnose bug 31723.
    BUG=31723
    TEST=none
    
    Review URL: http://codereview.chromium.org/660431
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40570 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c6a4eb9dd92e391ea363d0e7d0eee5aad9c7a06a

--- chrome/browser/profile_manager.cc
+++ chrome/browser/profile_manager.cc
@@ -205,23 +205,9 @@ void ProfileManager::Observe(
 #endif
 }
 
-#if defined(OS_WIN)
-#pragma optimize("", off)
-#pragma warning(disable:4748)
-#endif
 void ProfileManager::SuspendProfile(Profile* profile) {
-  CHECK(profile);
+  DCHECK(profile);
   DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
-
-  // TODO(rvargas): remove this after finding the cause for bug 31723.
-  URLRequestJob* job_list[50] = { NULL };
-  int count = 0;
-  for (URLRequestJobTracker::JobIterator i = g_url_request_job_tracker.begin();
-       i != g_url_request_job_tracker.end(); ++i) {
-    if (count < 50)
-      job_list[count] = *i;
-    count++;
-  }
 
   for (URLRequestJobTracker::JobIterator i = g_url_request_job_tracker.begin();
        i != g_url_request_job_tracker.end(); ++i)
@@ -230,10 +216,6 @@ void ProfileManager::SuspendProfile(Profile* profile) {
   profile->GetRequestContext()->GetURLRequestContext()->
       http_transaction_factory()->Suspend(true);
 }
-#if defined(OS_WIN)
-#pragma warning(default:4748)
-#pragma optimize("", on)
-#endif
 
 void ProfileManager::ResumeProfile(Profile* profile) {
   DCHECK(profile);
