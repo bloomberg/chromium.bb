252f39718cd917692f2261fe46279fa1052d15be
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-03 01:17:09 +0000

    Bookmark manager: Add drop down arrows to the drop down buttons.
    This also moves the CSS out of main.html
    
    BUG=4890
    TEST=The "Organize" and "Tools" buttons should now have arrows next to them.
    
    
    Review URL: http://codereview.chromium.org/661427
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40471 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d87395d59ef04bf0182f6d957c2622b25fb37152

--- chrome/browser/resources/bookmark_manager/css/tree.css.js
+++ chrome/browser/resources/bookmark_manager/css/tree.css.js
@@ -1,4 +1,6 @@
-
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 (function() {
   var a = 7;
