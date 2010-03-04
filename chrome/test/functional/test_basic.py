eb46ac826169f4af89bca0805f8b86fe389b6365
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-04 00:47:10 +0000

    Create a placeholder for pyauto test scripts.
    Including a hello world script.
    
    Review URL: http://codereview.chromium.org/668004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40579 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: caf798489af53c17f81f8bceee097ad5c6091976

---
+++ chrome/test/functional/test_basic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from pyauto import PyUITest
+
+
+class SimpleTest(PyUITest):
+
+  def testCanOpenGoogle(self):
+    self.NavigateToURL("http://www.google.com")
+
+
+if __name__ == '__main__':
+  unittest.main()
