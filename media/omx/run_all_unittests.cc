6e296e407d7ec203e212189234bd8a5dfb7cc130
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-05 00:27:29 +0000

    omx_unittests to load OpenMAX library dynamically
    Now OpenMAX library has to be loaded dynamically.
    
    Review URL: http://codereview.chromium.org/667010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40693 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6b6ee027ab8952c404b42842ce797b459859145e

--- media/omx/run_all_unittests.cc
+++ media/omx/run_all_unittests.cc
@@ -1,9 +1,15 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/test/test_suite.h"
+#include "media/base/media.h"
 
 int main(int argc, char** argv) {
+  // Load the OpenMAX library.
+  if (!media::InitializeOpenMaxLibrary(FilePath())) {
+    LOG(ERROR) << "Unable to initialize OpenMAX library.";
+    return -1;
+  }
   return TestSuite(argc, argv).Run();
 }
