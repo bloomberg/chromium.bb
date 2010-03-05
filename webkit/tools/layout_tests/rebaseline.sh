e3462166fea7a4fe18c1482cfcab30cf39ffe9ae
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-05 00:21:28 +0000

    Revert 40679 - Flip the run_webkit_tests scripts to use the upstream versions of the test harness (WebKit/WebKitTools/Scripts/newrunwebkittests).
    This also adds an old_run_webkit_tests for the moment for testing purposes.
    I will remove this file when I obsolete everything under
    webkit/tools/layout_tests/webkitpy.
    
        BUG=23099
        TEST=bots stay green
        R=tony@chromium.org
    
    Review URL: http://codereview.chromium.org/669037
    
    TBR=dpranke@chromium.org
    Review URL: http://codereview.chromium.org/668121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40691 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 63d04b0ac3ac4c289715896f37aa974eb795a595

--- webkit/tools/layout_tests/rebaseline.sh
+++ webkit/tools/layout_tests/rebaseline.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2009 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 exec_dir=$(dirname $0)
 
 PYTHON_PROG=python
+# When not using the included python, we don't get automatic site.py paths.
+# Specifically, rebaseline.py needs the paths in:
+# third_party/python_24/Lib/site-packages/google.pth
+PYTHONPATH="${exec_dir}/../../../tools/python:$PYTHONPATH"
+export PYTHONPATH
 
-"$PYTHON_PROG" "$exec_dir/../../../third_party/WebKit/WebKitTools/Scripts/rebaseline-chromium-webkit-tests" "$@"
+"$PYTHON_PROG" "$exec_dir/webkitpy/layout_tests/rebaseline_chromium_webkit_tests.py" "$@"
