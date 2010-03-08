e817cda0a1848074415e2cb5e694e1ff8f26191e
markus@chromium.org <markus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-08 21:50:28 +0000

    Added missing copyright header.
    BUG=32501
    TEST=none
    Review URL: http://codereview.chromium.org/672011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40946 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cf1bf1eeec382a0375077737b980887cd083c40b

--- sandbox/linux/seccomp/debug.h
+++ sandbox/linux/seccomp/debug.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef DEBUG_H__
 #define DEBUG_H__
 
