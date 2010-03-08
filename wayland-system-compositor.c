fa548856007d0326632cc4baab66b705a38249ed
Kristian Høgsberg <krh@bitplanet.net>
2010-03-08 17:16:01 -0500

    Fix a couple of typos in rotation matrix

--- wayland-system-compositor.c
+++ wayland-system-compositor.c
@@ -305,8 +305,8 @@ wlsc_matrix_rotate(struct wlsc_matrix *matrix,
 	GLdouble s = sin(angle);
 	struct wlsc_matrix rotate = {
 		{ x * x * (1 - c) + c,     y * x * (1 - c) + z * s, x * z * (1 - c) - y * s, 0,
-		  x * y * (1 - c) - z * s, y * y * (1 - c) + c,     y * z * (1 - c) - x * s, 0, 
-		  x * z * (1 - c) + y * x, y * z * (1 - c) - x * s, z * z * (1 - c) + c,     0,
+		  x * y * (1 - c) - z * s, y * y * (1 - c) + c,     y * z * (1 - c) + x * s, 0, 
+		  x * z * (1 - c) + y * s, y * z * (1 - c) - x * s, z * z * (1 - c) + c,     0,
 		  0, 0, 0, 1 }
 	};
 
