550bd3e5d48029ee5849cd84b754a8ebd14ecac2
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-09 00:49:36 +0000

    Mac: Fix another crash in content exceptions dialog.
    Also add a couple unit tests. Also fix another potential crash and other problems uncovered by the tests.
    
    BUG=37208
    TEST=See bug. Also, unittests.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=40810
    
    Review URL: http://codereview.chromium.org/668046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@40973 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d92d3ce3a7a8ed672d2a4b97e455032ff8fcf57c

--- chrome/browser/cocoa/content_exceptions_window_controller_unittest.mm
+++ chrome/browser/cocoa/content_exceptions_window_controller_unittest.mm
@@ -17,12 +17,73 @@
 
 namespace {
 
+void ProcessEvents() {
+  for (;;) {
+    base::ScopedNSAutoreleasePool pool;
+    NSEvent* next_event = [NSApp nextEventMatchingMask:NSAnyEventMask
+                                             untilDate:nil
+                                                inMode:NSDefaultRunLoopMode
+                                               dequeue:YES];
+    if (!next_event) break;
+    [NSApp sendEvent:next_event];
+  }
+}
+
+void SendKeyEvents(NSString* characters) {
+  for (NSUInteger i = 0; i < [characters length]; ++i) {
+    unichar character = [characters characterAtIndex:i];
+    NSString* charString = [NSString stringWithCharacters:&character length:1];
+    NSEvent* event = [NSEvent keyEventWithType:NSKeyDown
+                                      location:NSZeroPoint
+                                 modifierFlags:0
+                                     timestamp:0.0
+                                  windowNumber:0
+                                       context:nil
+                                    characters:charString
+                   charactersIgnoringModifiers:charString
+                                     isARepeat:NO
+                                       keyCode:0];
+    [NSApp sendEvent:event];
+  }
+}
+
 class ContentExceptionsWindowControllerTest : public CocoaTest {
  public:
   virtual void SetUp() {
     CocoaTest::SetUp();
     TestingProfile* profile = browser_helper_.profile();
     settingsMap_ = new HostContentSettingsMap(profile);
+  }
+
+  ContentExceptionsWindowController* GetController(ContentSettingsType type) {
+    return [ContentExceptionsWindowController showForType:type
+                                              settingsMap:settingsMap_.get()];
+
+  }
+
+  void ClickAdd(ContentExceptionsWindowController* controller) {
+    [controller addException:nil];
+    ProcessEvents();
+  }
+
+  void ClickRemove(ContentExceptionsWindowController* controller) {
+    [controller removeException:nil];
+    ProcessEvents();
+  }
+
+  void ClickRemoveAll(ContentExceptionsWindowController* controller) {
+    [controller removeAllExceptions:nil];
+    ProcessEvents();
+  }
+
+  void EnterText(NSString* str) {
+    SendKeyEvents(str);
+    ProcessEvents();
+  }
+
+  void HitEscape(ContentExceptionsWindowController* controller) {
+    [controller cancel:nil];
+    ProcessEvents();
   }
 
  protected:
@@ -37,5 +98,138 @@ TEST_F(ContentExceptionsWindowControllerTest, Construction) {
           settingsMap:settingsMap_.get()];
   [controller close];  // Should autorelease.
 }
+
+// Regression test for http://crbug.com/37137
+TEST_F(ContentExceptionsWindowControllerTest, AddRemove) {
+  ContentExceptionsWindowController* controller =
+      GetController(CONTENT_SETTINGS_TYPE_PLUGINS);
+
+  HostContentSettingsMap::SettingsForOneType settings;
+
+  ClickAdd(controller);
+  settingsMap_->GetSettingsForOneType(CONTENT_SETTINGS_TYPE_PLUGINS,
+                                      &settings);
+  EXPECT_EQ(0u, settings.size());
+
+  ClickRemove(controller);
+
+  EXPECT_FALSE([controller editingNewException]);
+  [controller close];
+
+  settingsMap_->GetSettingsForOneType(CONTENT_SETTINGS_TYPE_PLUGINS,
+                                      &settings);
+  EXPECT_EQ(0u, settings.size());
+}
+
+// Regression test for http://crbug.com/37137
+TEST_F(ContentExceptionsWindowControllerTest, AddRemoveAll) {
+  ContentExceptionsWindowController* controller =
+      GetController(CONTENT_SETTINGS_TYPE_PLUGINS);
+  ClickAdd(controller);
+  ClickRemoveAll(controller);
+
+  EXPECT_FALSE([controller editingNewException]);
+  [controller close];
+
+  HostContentSettingsMap::SettingsForOneType settings;
+  settingsMap_->GetSettingsForOneType(CONTENT_SETTINGS_TYPE_PLUGINS,
+                                      &settings);
+  EXPECT_EQ(0u, settings.size());
+}
+
+TEST_F(ContentExceptionsWindowControllerTest, Add) {
+  ContentExceptionsWindowController* controller =
+      GetController(CONTENT_SETTINGS_TYPE_PLUGINS);
+
+  ClickAdd(controller);
+  EnterText(@"addedhost\n");
+
+  EXPECT_FALSE([controller editingNewException]);
+  [controller close];
+
+  HostContentSettingsMap::SettingsForOneType settings;
+  settingsMap_->GetSettingsForOneType(CONTENT_SETTINGS_TYPE_PLUGINS,
+                                      &settings);
+  EXPECT_EQ(1u, settings.size());
+  EXPECT_EQ("addedhost", settings[0].first);
+}
+
+TEST_F(ContentExceptionsWindowControllerTest, AddEscDoesNotAdd) {
+  ContentExceptionsWindowController* controller =
+      GetController(CONTENT_SETTINGS_TYPE_PLUGINS);
+
+  ClickAdd(controller);
+  EnterText(@"addedhost");  // but do not press enter
+  HitEscape(controller);
+
+  HostContentSettingsMap::SettingsForOneType settings;
+  settingsMap_->GetSettingsForOneType(CONTENT_SETTINGS_TYPE_PLUGINS,
+                                      &settings);
+  EXPECT_EQ(0u, settings.size());
+  EXPECT_FALSE([controller editingNewException]);
+
+  [controller close];
+}
+
+// Regression test for http://crbug.com/37208
+TEST_F(ContentExceptionsWindowControllerTest, AddEditAddAdd) {
+  ContentExceptionsWindowController* controller =
+      GetController(CONTENT_SETTINGS_TYPE_PLUGINS);
+
+  ClickAdd(controller);
+  EnterText(@"testtesttest");  // but do not press enter
+  ClickAdd(controller);
+  ClickAdd(controller);
+
+  EXPECT_TRUE([controller editingNewException]);
+  [controller close];
+
+  HostContentSettingsMap::SettingsForOneType settings;
+  settingsMap_->GetSettingsForOneType(CONTENT_SETTINGS_TYPE_PLUGINS,
+                                      &settings);
+  EXPECT_EQ(0u, settings.size());
+}
+
+TEST_F(ContentExceptionsWindowControllerTest, AddExistingEditAdd) {
+  settingsMap_->SetContentSetting(
+      "myhost", CONTENT_SETTINGS_TYPE_PLUGINS, CONTENT_SETTING_BLOCK);
+
+  ContentExceptionsWindowController* controller =
+      GetController(CONTENT_SETTINGS_TYPE_PLUGINS);
+
+  ClickAdd(controller);
+  EnterText(@"myhost");  // but do not press enter
+  ClickAdd(controller);
+
+  EXPECT_TRUE([controller editingNewException]);
+  [controller close];
+
+
+  HostContentSettingsMap::SettingsForOneType settings;
+  settingsMap_->GetSettingsForOneType(CONTENT_SETTINGS_TYPE_PLUGINS,
+                                      &settings);
+  EXPECT_EQ(1u, settings.size());
+}
+
+TEST_F(ContentExceptionsWindowControllerTest, AddExistingDoesNotOverwrite) {
+  settingsMap_->SetContentSetting(
+      "myhost", CONTENT_SETTINGS_TYPE_COOKIES, CONTENT_SETTING_ASK);
+
+  ContentExceptionsWindowController* controller =
+      GetController(CONTENT_SETTINGS_TYPE_COOKIES);
+
+  ClickAdd(controller);
+  EnterText(@"myhost\n");
+
+  EXPECT_FALSE([controller editingNewException]);
+  [controller close];
+
+  HostContentSettingsMap::SettingsForOneType settings;
+  settingsMap_->GetSettingsForOneType(CONTENT_SETTINGS_TYPE_COOKIES,
+                                      &settings);
+  EXPECT_EQ(1u, settings.size());
+  EXPECT_EQ(CONTENT_SETTING_ASK, settings[0].second);
+}
+
 
 }  // namespace
