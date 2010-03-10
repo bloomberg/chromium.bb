0ca15dabbf4f399bd0387b85d20ec9886e956f8f
gregoryd@google.com <gregoryd@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-03-10 22:19:33 +0000

    Allow including nacl_os_qualify.h from C++ files.
    Review URL: http://codereview.chromium.org/829002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1696 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 60c26eeb6621990743819b54e1d0f6cb14816360

--- src/trusted/platform_qualify/nacl_os_qualify.h
+++ src/trusted/platform_qualify/nacl_os_qualify.h
@@ -8,6 +8,10 @@
 #ifndef NATIVE_CLIENT_SRC_TRUSTED_PLATFORM_QUALIFY_NACL_OS_QUALIFY_H_
 #define NATIVE_CLIENT_SRC_TRUSTED_PLATFORM_QUALIFY_NACL_OS_QUALIFY_H_
 
+#include "native_client/src/include/nacl_base.h"
+
+EXTERN_C_BEGIN
+
 /*
  * Returns 1 if the operating system can run Native Client modules.
  */
@@ -25,5 +29,7 @@ int NaClOsIs64BitWindows();
  * solution is required to execute Native Client modules.
  */
 int NaClOsRestoresLdt();
+
+EXTERN_C_END
 
 #endif  /* NATIVE_CLIENT_SRC_TRUSTED_PLATFORM_QUALIFY_NACL_OS_QUALIFY_H_ */
