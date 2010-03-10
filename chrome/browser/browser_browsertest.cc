84a32a451ee84a8396ebab72f4eef7f70fa3d859
pam@chromium.org <pam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-10 20:25:21 +0000

    Add comment about flaky test.
    BUG=27039
    TEST=none
    TBR=phajdan.jr
    Review URL: http://codereview.chromium.org/778006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41199 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6bcecb8ac60108012bbe107a07b620ac712dca5b

--- chrome/browser/browser_browsertest.cc
+++ chrome/browser/browser_browsertest.cc
@@ -243,6 +243,7 @@ IN_PROC_BROWSER_TEST_F(BrowserTest, ReloadThenCancelBeforeUnload) {
 
 // Test for crbug.com/11647.  A page closed with window.close() should not have
 // two beforeunload dialogs shown.
+// Flaky: see http://crbug.com/27039
 IN_PROC_BROWSER_TEST_F(BrowserTest, FLAKY_SingleBeforeUnloadAfterWindowClose) {
   browser()->GetSelectedTabContents()->render_view_host()->
       ExecuteJavascriptInWebFrame(L"", OPEN_NEW_BEFOREUNLOAD_PAGE);
