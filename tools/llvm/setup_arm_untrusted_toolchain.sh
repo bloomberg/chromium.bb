a9e08f39b2e1ca330ca7403991e18a32611a27ef
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-03-11 22:45:27 +0000

    Add some *cosmetic* defines to make the "gpu extra sdk lib" compile. This needs to be looked into, though, c.f. http://code.google.com/p/nativeclient/issues/detail?id=234
    Review URL: http://codereview.chromium.org/787005
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1703 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 608d41146038e51e2ac09995d41bcd2d583a9591

--- tools/llvm/setup_arm_untrusted_toolchain.sh
+++ tools/llvm/setup_arm_untrusted_toolchain.sh
@@ -21,18 +21,36 @@ export NACL_SDK_INCLUDE="${NACL_SDK_INSTALL}/include"
 # NOTE: NACL_* defines are currently needed for building extra_sdk, e.g.
 #       for including sel_ldr.h and atomic_ops.h from nc_thread.s
 # TODO(robertm): fix this
+
+# TODO(robetrm): there is a gross hack involving the __GXX... and _IS...
+#                symbols below. This likely only fixes symptoms not
+#                the real cause and needs to be investigated
+#                c.f.: http://code.google.com/p/nativeclient/issues/detail?id=234
 SHARED_CFLAGS="-nostdinc \
                -D__native_client__=1 \
                -DNACL_TARGET_ARCH=arm \
                -DNACL_TARGET_SUBARCH=32 \
                -DNACL_LINUX=1 \
+               -U__GXX_MERGED_TYPEINFO_NAMES \
+               -D__GXX_MERGED_TYPEINFO_NAMES=1 \
+               -U__GXX_TYPEINFO_EQUALITY_INLINE \
+               -D__GXX_TYPEINFO_EQUALITY_INLINE=0 \
+               '-D_ISupper=_U' \
+               '-D_ISlower=_L' \
+               '-D_ISalpha=_U|_L' \
+               '-D_ISdigit=_N' \
+               '-D_ISxdigit=_X|_N' \
+               '-D_ISspace=_S' \
+               '-D_ISprint=_P|_U|_L|_N|_B' \
+               '-D_IScntrl=_C' \
+               '-D_ISpunct=_P' \
                -march=armv6 \
                -ffixed-r9 \
                -isystem ${NACL_SDK_INSTALL}/../newlib_extra_header \
                -isystem ${NACL_SDK_INSTALL}/../newlib_extra_header/c++/4.2.1 \
                -isystem ${NACL_SDK_INSTALL}/../newlib_extra_header/c++/4.2.1/arm-none-linux-gnueabi \
-               -isystem ${NACL_SDK_INCLUDE} \
-               -isystem ${NACL_SDK_INCLUDE}/sys"
+               -isystem ${NACL_SDK_INSTALL}/include \
+               -isystem ${NACL_SDK_INSTALL}/include/sys"
 
 # NOTE: -isystem ${NACL_SDK_INCLUDE}/sys is not very robust:
 #       as there are indentically names files in ${NACL_SDK_INCLUDE}
