858727591e5dabf42290960cfb8208dd9ca5cd67
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-16 18:44:21 +0000

    Factoring duplicate code from platform-specific LoginHandlers into a base LoginHandler class. As pointed out by erg, it would be cleaner to use a separate controller class, but this simple refactor should make that easier.
    Contributed by tonyg@chromium.org
    
    BUG=14909
    TEST=ui_tests --gtest_filter=LoginPromptTest*
    
    Review URL: http://codereview.chromium.org/834001
    Patch from tonyg@chromium.org.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@41739 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f5a00084ea8b4dd84388da88f57db562a3c913f7

--- chrome/browser/login_prompt.h
+++ chrome/browser/login_prompt.h
@@ -8,33 +8,27 @@
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/lock.h"
+#include "chrome/browser/password_manager/password_manager.h"
 
 namespace net {
 class AuthChallengeInfo;
 }
 
-namespace webkit_glue {
-struct PasswordForm;
-}
-
 class ConstrainedWindow;
 class GURL;
-class PasswordManager;
 class TabContents;
 class URLRequest;
 
-// This is the interface for the class that routes authentication info to
-// the URLRequest that needs it.  Used by the automation proxy for testing.
-// These functions should be (and are, in LoginHandlerImpl) implemented in
-// a thread safe manner.
-//
-// TODO(erg): Refactor the common code from all LoginHandler subclasses into a
-// common controller class. All the methods below have the same copy/pasted
-// implementation. This is more difficult then it should be because all these
-// subclasses are also base::RefCountedThreadSafe<> and I'm not sure how to get
-// ownership correct.  http://crbug.com/14909
-class LoginHandler {
+// This is the base implementation for the OS-specific classes that route
+// authentication info to the URLRequest that needs it. These functions must
+// be implemented in a thread safe manner.
+class LoginHandler : public base::RefCountedThreadSafe<LoginHandler>,
+                     public LoginModelObserver {
  public:
+  explicit LoginHandler(URLRequest* request);
+  virtual ~LoginHandler();
+
   // Builds the platform specific LoginHandler. Used from within
   // CreateLoginPrompt() which creates tasks.
   static LoginHandler* Create(URLRequest* request);
@@ -45,27 +39,82 @@ class LoginHandler {
 
   // Sets information about the authentication type (|form|) and the
   // |password_manager| for this profile.
-  virtual void SetPasswordForm(const webkit_glue::PasswordForm& form) = 0;
-  virtual void SetPasswordManager(PasswordManager* password_manager) = 0;
+  void SetPasswordForm(const webkit_glue::PasswordForm& form);
+  void SetPasswordManager(PasswordManager* password_manager);
 
   // Returns the TabContents that needs authentication.
-  virtual TabContents* GetTabContentsForLogin() = 0;
+  TabContents* GetTabContentsForLogin() const;
 
   // Resend the request with authentication credentials.
   // This function can be called from either thread.
-  virtual void SetAuth(const std::wstring& username,
-                       const std::wstring& password) = 0;
+  void SetAuth(const std::wstring& username, const std::wstring& password);
 
   // Display the error page without asking for credentials again.
   // This function can be called from either thread.
-  virtual void CancelAuth() = 0;
+  void CancelAuth();
 
   // Notify the handler that the request was cancelled.
   // This function can only be called from the IO thread.
-  virtual void OnRequestCancelled() = 0;
+  void OnRequestCancelled();
 
  protected:
-  virtual ~LoginHandler() {}
+
+  void SetModel(LoginModel* model);
+
+  void SetDialog(ConstrainedWindow* dialog);
+
+  // Notify observers that authentication is needed or received.  The automation
+  // proxy uses this for testing.
+  void SendNotifications();
+
+  void ReleaseSoon();
+
+ private:
+
+  // Returns whether authentication had been handled (SetAuth or CancelAuth).
+  // If |set_handled| is true, it will mark authentication as handled.
+  bool WasAuthHandled(bool set_handled);
+
+  // Calls SetAuth from the IO loop.
+  void SetAuthDeferred(const std::wstring& username,
+                       const std::wstring& password);
+
+  // Calls CancelAuth from the IO loop.
+  void CancelAuthDeferred();
+
+  // Closes the view_contents from the UI loop.
+  void CloseContentsDeferred();
+
+  // The request that wants login data.
+  // This should only be accessed on the IO loop.
+  URLRequest* request_;
+
+  // The PasswordForm sent to the PasswordManager. This is so we can refer to it
+  // when later notifying the password manager if the credentials were accepted
+  // or rejected.
+  // This should only be accessed on the UI loop.
+  webkit_glue::PasswordForm password_form_;
+
+  // Points to the password manager owned by the TabContents requesting auth.
+  // Can be null if the TabContents is not a TabContents.
+  // This should only be accessed on the UI loop.
+  PasswordManager* password_manager_;
+
+  // If not null, points to a model we need to notify of our own destruction
+  // so it doesn't try and access this when its too late.
+  LoginModel* login_model_;
+
+  // Cached from the URLRequest, in case it goes NULL on us.
+  int render_process_host_id_;
+  int tab_contents_id_;
+
+  // True if we've handled auth (SetAuth or CancelAuth has been called).
+  bool handled_auth_;
+  Lock handled_auth_lock_;
+
+  // The ConstrainedWindow that is hosting our LoginView.
+  // This should only be accessed on the UI loop.
+  ConstrainedWindow* dialog_;
 };
 
 // Details to provide the NotificationObserver.  Used by the automation proxy
--- chrome/browser/login_prompt_mac.mm
+++ chrome/browser/login_prompt_mac.mm
@@ -33,46 +33,21 @@ using webkit_glue::PasswordForm;
 // This class uses ref counting to ensure that it lives until all InvokeLaters
 // have been called.
 class LoginHandlerMac : public LoginHandler,
-                        public base::RefCountedThreadSafe<LoginHandlerMac>,
-                        public ConstrainedWindowMacDelegateCustomSheet,
-                        public LoginModelObserver {
+                        public ConstrainedWindowMacDelegateCustomSheet {
  public:
-  LoginHandlerMac(URLRequest* request)
-      : handled_auth_(false),
-        dialog_(NULL),
-        request_(request),
-        password_manager_(NULL),
-        sheet_controller_(nil),
-        login_model_(NULL) {
-    // This constructor is called on the I/O thread, so we cannot load the nib
-    // here. BuildViewForPasswordManager() will be invoked on the UI thread
-    // later, so wait with loading the nib until then.
-    DCHECK(request_) << "LoginHandlerMac constructed with NULL request";
-
-    AddRef();  // matched by ReleaseLater.
-    if (!ResourceDispatcherHost::RenderViewForRequest(request_,
-                                                      &render_process_host_id_,
-                                                      &tab_contents_id_)) {
-      NOTREACHED();
-    }
+  explicit LoginHandlerMac(URLRequest* request)
+      : LoginHandler(request),
+        sheet_controller_(nil) {
   }
 
   virtual ~LoginHandlerMac() {
-    if (login_model_)
-      login_model_->SetObserver(NULL);
-  }
-
-  void SetModel(LoginModel* model) {
-    if (login_model_)
-      login_model_->SetObserver(NULL);
-    login_model_ = model;
-    if (login_model_)
-      login_model_->SetObserver(this);
   }
 
   // LoginModelObserver implementation.
   virtual void OnAutofillDataAvailable(const std::wstring& username,
                                        const std::wstring& password) {
+    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
+
     [sheet_controller_ autofillLogin:base::SysWideToNSString(username)
                             password:base::SysWideToNSString(password)];
   }
@@ -97,212 +72,45 @@ class LoginHandlerMac : public LoginHandler,
     // control).  However, that's OK since any UI interaction in those functions
     // will occur via an InvokeLater on the UI thread, which is guaranteed
     // to happen after this is called (since this was InvokeLater'd first).
-    dialog_ = GetTabContentsForLogin()->CreateConstrainedDialog(this);
+    SetDialog(GetTabContentsForLogin()->CreateConstrainedDialog(this));
 
     SendNotifications();
   }
 
-  virtual void SetPasswordForm(const webkit_glue::PasswordForm& form) {
-    password_form_ = form;
-  }
-
-  virtual void SetPasswordManager(PasswordManager* password_manager) {
-    password_manager_ = password_manager;
-  }
-
-  virtual TabContents* GetTabContentsForLogin() {
-    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
-
-    return tab_util::GetTabContentsByID(render_process_host_id_,
-                                        tab_contents_id_);
-  }
-
-  virtual void SetAuth(const std::wstring& username,
-                       const std::wstring& password) {
-    if (WasAuthHandled(true))
-      return;
-
-    // Tell the password manager the credentials were submitted / accepted.
-    if (password_manager_) {
-      password_form_.username_value = WideToUTF16Hack(username);
-      password_form_.password_value = WideToUTF16Hack(password);
-      password_manager_->ProvisionallySavePassword(password_form_);
-    }
-
-    ChromeThread::PostTask(
-        ChromeThread::UI, FROM_HERE,
-            NewRunnableMethod(this, &LoginHandlerMac::CloseContentsDeferred));
-    ChromeThread::PostTask(
-        ChromeThread::UI, FROM_HERE,
-        NewRunnableMethod(this, &LoginHandlerMac::SendNotifications));
-    ChromeThread::PostTask(
-        ChromeThread::IO, FROM_HERE,
-        NewRunnableMethod(
-            this, &LoginHandlerMac::SetAuthDeferred, username, password));
-  }
-
-  virtual void CancelAuth() {
-    if (WasAuthHandled(true))
-      return;
-
-    ChromeThread::PostTask(
-        ChromeThread::UI, FROM_HERE,
-        NewRunnableMethod(this, &LoginHandlerMac::CloseContentsDeferred));
-    ChromeThread::PostTask(
-        ChromeThread::UI, FROM_HERE,
-        NewRunnableMethod(this, &LoginHandlerMac::SendNotifications));
-    ChromeThread::PostTask(
-        ChromeThread::IO, FROM_HERE,
-        NewRunnableMethod(this, &LoginHandlerMac::CancelAuthDeferred));
-  }
-
-  virtual void OnRequestCancelled() {
-    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO)) <<
-        "Why is OnRequestCancelled called from the UI thread?";
-
-    // Reference is no longer valid.
-    request_ = NULL;
-
-    // Give up on auth if the request was cancelled.
-    CancelAuth();
-  }
-
   // Overridden from ConstrainedWindowMacDelegate:
   virtual void DeleteDelegate() {
-    if (!WasAuthHandled(true)) {
-      ChromeThread::PostTask(
-          ChromeThread::IO, FROM_HERE,
-          NewRunnableMethod(this, &LoginHandlerMac::CancelAuthDeferred));
-      ChromeThread::PostTask(
-          ChromeThread::UI, FROM_HERE,
-          NewRunnableMethod(this, &LoginHandlerMac::SendNotifications));
-    }
+    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
+
+    // The constrained window is going to delete itself; clear our pointer.
+    SetDialog(NULL);
+    SetModel(NULL);
 
     // Close sheet if it's still open, as required by
     // ConstrainedWindowMacDelegate.
-    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
     if (is_sheet_open())
       [NSApp endSheet:sheet()];
 
-    SetModel(NULL);
-
-    // Delete this object once all InvokeLaters have been called.
-    ChromeThread::ReleaseSoon(ChromeThread::IO, FROM_HERE, this);
+    ReleaseSoon();
   }
 
   void OnLoginPressed(const std::wstring& username,
                       const std::wstring& password) {
     DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
+
     SetAuth(username, password);
   }
 
   void OnCancelPressed() {
     DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
+
     CancelAuth();
   }
 
  private:
   friend class LoginPrompt;
 
-  // Calls SetAuth from the IO loop.
-  void SetAuthDeferred(const std::wstring& username,
-                       const std::wstring& password) {
-    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
-
-    if (request_) {
-      request_->SetAuth(username, password);
-      ResetLoginHandlerForRequest(request_);
-    }
-  }
-
-  // Calls CancelAuth from the IO loop.
-  void CancelAuthDeferred() {
-    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::IO));
-
-    if (request_) {
-      request_->CancelAuth();
-      // Verify that CancelAuth does destroy the request via our delegate.
-      DCHECK(request_ != NULL);
-      ResetLoginHandlerForRequest(request_);
-    }
-  }
-
-  // Closes the view_contents from the UI loop.
-  void CloseContentsDeferred() {
-    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
-
-    // The hosting ConstrainedWindow may have been freed.
-    if (dialog_)
-      dialog_->CloseConstrainedWindow();
-  }
-
-  // Returns whether authentication had been handled (SetAuth or CancelAuth).
-  // If |set_handled| is true, it will mark authentication as handled.
-  bool WasAuthHandled(bool set_handled) {
-    AutoLock lock(handled_auth_lock_);
-    bool was_handled = handled_auth_;
-    if (set_handled)
-      handled_auth_ = true;
-    return was_handled;
-  }
-
-  // Notify observers that authentication is needed or received.  The automation
-  // proxy uses this for testing.
-  void SendNotifications() {
-    DCHECK(ChromeThread::CurrentlyOn(ChromeThread::UI));
-
-    NotificationService* service = NotificationService::current();
-    TabContents* requesting_contents = GetTabContentsForLogin();
-    if (!requesting_contents)
-      return;
-
-    NavigationController* controller = &requesting_contents->controller();
-
-    if (!WasAuthHandled(false)) {
-      LoginNotificationDetails details(this);
-      service->Notify(NotificationType::AUTH_NEEDED,
-                      Source<NavigationController>(controller),
-                      Details<LoginNotificationDetails>(&details));
-    } else {
-      service->Notify(NotificationType::AUTH_SUPPLIED,
-                      Source<NavigationController>(controller),
-                      NotificationService::NoDetails());
-    }
-  }
-
-  // True if we've handled auth (SetAuth or CancelAuth has been called).
-  bool handled_auth_;
-  Lock handled_auth_lock_;
-
-  // The ConstrainedWindow that is hosting our LoginView.
-  // This should only be accessed on the UI loop.
-  ConstrainedWindow* dialog_;
-
-  // The request that wants login data.
-  // This should only be accessed on the IO loop.
-  URLRequest* request_;
-
-  // The PasswordForm sent to the PasswordManager. This is so we can refer to it
-  // when later notifying the password manager if the credentials were accepted
-  // or rejected.
-  // This should only be accessed on the UI loop.
-  PasswordForm password_form_;
-
-  // Points to the password manager owned by the TabContents requesting auth.
-  // Can be null if the TabContents is not a TabContents.
-  // This should only be accessed on the UI loop.
-  PasswordManager* password_manager_;
-
-  // Cached from the URLRequest, in case it goes NULL on us.
-  int render_process_host_id_;
-  int tab_contents_id_;
-
   // The Cocoa controller of the GUI.
   LoginHandlerSheet* sheet_controller_;
-
-  // If not null, points to a model we need to notify of our own destruction
-  // so it doesn't try and access this when its too late.
-  LoginModel* login_model_;
 
   DISALLOW_COPY_AND_ASSIGN(LoginHandlerMac);
 };
