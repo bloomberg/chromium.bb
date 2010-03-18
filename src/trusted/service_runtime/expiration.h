8b62caf7715ab25e5619aa80b2fdb55733765b64
bradnelson@google.com <bradnelson@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-03-18 02:16:17 +0000

    Bumping forward expiration date.
    BUG=None
    TEST=None
    TBR=bradchen
    
    
    Review URL: http://codereview.chromium.org/1074001
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1751 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: caae683285860e9db298885e8a66186ac2761696

--- src/trusted/service_runtime/expiration.h
+++ src/trusted/service_runtime/expiration.h
@@ -23,7 +23,7 @@
 #ifdef NACL_STANDALONE  /* NaCl in Chrome does not expire */
 #define EXPIRATION_CHECK 1
 #define EXPIRATION_YEAR 2010
-#define EXPIRATION_MONTH 3
+#define EXPIRATION_MONTH 5
 #define EXPIRATION_DAY 17
 #endif
 
