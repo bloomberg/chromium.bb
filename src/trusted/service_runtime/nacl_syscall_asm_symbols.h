2b3b57e35b69f11f5ac9c5212bc8c09a1af62146
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-03-24 13:07:43 +0000

    Tidy: Remove unused special case for Mac OS X
    The special case was dead code anyway because it tested the wrong preprocessor symbol (NACL_MACOSX instead of NACL_OSX).
    
    Review URL: http://codereview.chromium.org/1051001
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@1792 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: eaea595bb0310b19f41d1b2eece7fbe4d5dbf14e

--- src/trusted/service_runtime/nacl_syscall_asm_symbols.h
+++ src/trusted/service_runtime/nacl_syscall_asm_symbols.h
@@ -8,10 +8,6 @@
 #ifndef SERVICE_RUNTIME_NACL_SYSCALL_H__
 #define SERVICE_RUNTIME_NACL_SYSCALL_H__
 
-#if !NACL_MACOSX || defined(NACL_STANDALONE)
 extern int NaClSyscallSeg();
-#else
-// This declaration is used only on Mac OSX for Chrome build
-extern int NaClSyscallSeg() __attribute__((weak_import));
-#endif
+
 #endif
