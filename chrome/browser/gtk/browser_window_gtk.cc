54c5eaf81c611dea938fd87a3adfc5036a5b3644
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-24 00:04:24 +0000

    Automation provider IsBookmarkBarVisible() always returned yes!
    BUG=39013
    
    TEST=pyautolib bookmark_bar.py script will now work on Linux
    
    Review URL: http://codereview.chromium.org/1227003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42409 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f071548404a9d3b6645b9dd4278fb04d20a4073b

--- chrome/browser/gtk/browser_window_gtk.cc
+++ chrome/browser/gtk/browser_window_gtk.cc
@@ -785,7 +785,8 @@ void BrowserWindowGtk::FocusToolbar() {
 
 bool BrowserWindowGtk::IsBookmarkBarVisible() const {
   return browser_->SupportsWindowFeature(Browser::FEATURE_BOOKMARKBAR) &&
-      bookmark_bar_.get();
+      bookmark_bar_.get() &&
+      browser_->profile()->GetPrefs()->GetBoolean(prefs::kShowBookmarkBar);
 }
 
 bool BrowserWindowGtk::IsBookmarkBarAnimating() const {
