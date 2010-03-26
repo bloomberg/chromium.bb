78b3a54173b2a2038ee84051aed203ee070eb819
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-26 21:42:20 +0000

    Marking the CrossSiteInfiniteBeforeUnloadAsync test as flaky as it fails randomly on the valgrind Linux builder.
    It appears that this was already marked flaky in revision 42358. I synced to head and still can't get
    that change.
    
    Bug=39057
    TBR=amit
    
    Review URL: http://codereview.chromium.org/1468002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42826 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 90ef2e048af8572f9b8dbac6d8385b7c70bb80b7

--- chrome/browser/unload_uitest.cc
+++ chrome/browser/unload_uitest.cc
@@ -250,7 +250,8 @@ TEST_F(UnloadTest, DISABLED_CrossSiteInfiniteUnloadAsyncInputEvent) {
 // Navigate to a page with an infinite beforeunload handler.
 // Then two two async crosssite requests to ensure
 // we don't get confused and think we're closing the tab.
-TEST_F(UnloadTest, CrossSiteInfiniteBeforeUnloadAsync) {
+// This test is flaky on the valgrind UI bots. http://crbug.com/39057
+TEST_F(UnloadTest, FLAKY_CrossSiteInfiniteBeforeUnloadAsync) {
   // Tests makes no sense in single-process mode since the renderer is hung.
   if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kSingleProcess))
     return;
