1baf9c140a5f309178b59572c332439c6d095ea4
markus@chromium.org <markus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-03-29 20:47:23 +0000

    Update/add copyright headers.
    BUG=32501
    TEST=none
    Review URL: http://codereview.chromium.org/1574001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@42992 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bb20bfdc0036e870e87fd9067f1f7f310d7de3c6

--- sandbox/linux/seccomp/syscall_table.c
+++ sandbox/linux/seccomp/syscall_table.c
@@ -1,3 +1,7 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include <asm/unistd.h>
 #include "sandbox_impl.h"
 #include "syscall_table.h"
