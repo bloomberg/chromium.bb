5fa198f09d4ebce5f8ffdba13176333787a28720
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-01 22:23:17 +0000

    Enable tests of chrome.tab.captureVisibleTab() on mac, and all tab tests on linux.
    BUG=39746,37387
    
    Review URL: http://codereview.chromium.org/1540010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43393 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9cc92df2e18c1a62e037decf5326e32fecc96b35

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -10,8 +10,7 @@
 #include "chrome/common/pref_names.h"
 
 // TODO(skerner): This test is flaky on chrome os: http://crbug.com/39843
-// TODO(skerner): Crash observed on linux as well: http://crbug.com/39746
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) && defined(TOOLKIT_VIEWS)
 #define MAYBE_Tabs DISABLED_Tabs
 #else
 #define MAYBE_Tabs Tabs
@@ -29,14 +28,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_Tabs) {
   ASSERT_TRUE(RunExtensionTest("tabs/basics")) << message_;
 }
 
-// chrome.tabs.captureVisibleTab fails on the 10.6 bots.
-// http://crbug.com/37387
-#if defined(OS_MACOSX)
-#define MAYBE_CaptureVisibleTab DISABLED_CaptureVisible
-#else
-#define MAYBE_CaptureVisibleTab CaptureVisibleTab
-#endif
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_CaptureVisibleTab) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, CaptureVisibleTab) {
   StartHTTPServer();
 
   ASSERT_TRUE(RunExtensionTest("tabs/capture_visible_tab")) << message_;
