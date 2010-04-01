0b724586aafd4a9801067164ee904c905b33e14a
skerner@chromium.org <skerner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-01 14:56:19 +0000

    Break the test of chrome.tab.captureVisibleTab() out of ExtensionApiTest.Tabs.
    Tests of chrome.tab.captureVisibleTab() fail on mac 10.6, and disabling all tab tests because of this is unfortunate.  Breaking the test this way makes the 10.6 failure easier to debug.
    
    BUG=37387
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=43101
    
    Review URL: http://codereview.chromium.org/1520005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43349 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: da9754bccd331b877a25272f2daae6d05b2f81e7

--- chrome/test/data/extensions/api_test/tabs/basics/test.js
+++ chrome/test/data/extensions/api_test/tabs/basics/test.js
@@ -253,53 +253,6 @@ chrome.test.runTests([
   },
   */
 
-  // Open a window with one tab, take a snapshot.
-  function captureVisibleTabSimple() {
-    // Keep the resulting image small by making the window small.
-    createWindow([pageUrl("a")], {"width": 300, "height": 150},
-                 pass(function(winId, tabIds) {
-      waitForAllTabs(pass(function() {
-        chrome.tabs.getSelected(winId, pass(function(tab) {
-          assertEq('complete', tab.status);  // waitForAllTabs ensures this.
-          chrome.tabs.captureVisibleTab(winId, pass(function(imgDataUrl) {
-            // The URL should be a data URL with has a JPEG mime type.
-            assertEq('data:image/jpg;base64,', imgDataUrl.substr(0,22));
-          }));
-        }));
-      }));
-    }));
-  },
-
-  // Open a window with three tabs, take a snapshot of each.
-  function captureVisibleTabMultiTab() {
-    var snapshotAndRemoveSelectedTab = function(winId, callback) {
-      chrome.tabs.getSelected(winId, function(tab) {
-        chrome.tabs.captureVisibleTab(winId, function(imgDataUrl) {
-          // Test that the URL we got is a data URL which encodes a JPEG image.
-          assertEq('data:image/jpg;base64,', imgDataUrl.substr(0,22));
-
-          // TODO(skerner): Once an option allows captureVisibleTab to
-          // take a lossless snapshot with a set color depth, use
-          // a canvas to compare |imgDataUrl| to an image of the tab
-          // we expect.  This can't be done with JPEG, as the results
-          // vary based on the display settings.
-          chrome.tabs.remove(tab.id, callback);
-        });
-      });
-    };
-
-    createWindow(["a", "b", "c"].map(pageUrl), {"width": 300, "height": 150},
-      function(winId, tabIds){
-        waitForAllTabs(pass(function() {
-          snapshotAndRemoveSelectedTab(winId, pass(function() {
-            snapshotAndRemoveSelectedTab(winId, pass(function() {
-              snapshotAndRemoveSelectedTab(winId, pass(function() {}));
-            }));
-          }));
-        }));
-      });
-  },
-
   function tabsOnCreated() {
     chrome.test.listenOnce(chrome.tabs.onCreated, function(tab) {
       assertEq(pageUrl("f"), tab.url);
