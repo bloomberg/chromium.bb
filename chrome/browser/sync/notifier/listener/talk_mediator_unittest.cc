9291070b1d5f43e16cb7879a22f4bb5582a7637e
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-07 23:39:08 +0000

    TBR: chron@chromium.org
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43895 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 31d1394965e2ccfd940ace6cc705b9456de6e534

--- chrome/browser/sync/notifier/listener/talk_mediator_unittest.cc
+++ chrome/browser/sync/notifier/listener/talk_mediator_unittest.cc
@@ -123,16 +123,16 @@ TEST_F(TalkMediatorImplTest, SendNotification) {
   talk1->OnSubscriptionSuccess();
   ASSERT_TRUE(talk1->state_.subscribed == 1);
   ASSERT_TRUE(talk1->SendNotification() == true);
-  ASSERT_TRUE(mock->send_calls == 1);
-  ASSERT_TRUE(talk1->SendNotification() == true);
   ASSERT_TRUE(mock->send_calls == 2);
+  ASSERT_TRUE(talk1->SendNotification() == true);
+  ASSERT_TRUE(mock->send_calls == 3);
 
   ASSERT_TRUE(talk1->Logout() == true);
   ASSERT_TRUE(mock->logout_calls == 1);
 
   // Failure due to being logged out.
   ASSERT_TRUE(talk1->SendNotification() == false);
-  ASSERT_TRUE(mock->send_calls == 2);
+  ASSERT_TRUE(mock->send_calls == 3);
 }
 
 TEST_F(TalkMediatorImplTest, MediatorThreadCallbacks) {
@@ -162,7 +162,7 @@ TEST_F(TalkMediatorImplTest, MediatorThreadCallbacks) {
   // After subscription success is receieved, the talk mediator will allow
   // sending of notifications.
   ASSERT_TRUE(talk1->SendNotification() == true);
-  ASSERT_TRUE(mock->send_calls == 1);
+  ASSERT_TRUE(mock->send_calls == 2);
 
   // |MSG_NOTIFICATION_RECEIVED| from the MediatorThread triggers a callback
   // of type |NOTIFICATION_RECEIVED|.
