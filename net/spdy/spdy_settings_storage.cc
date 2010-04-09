fb4ce3f3b8d000ce55516df32d02caa94bb07921
mbelshe@chromium.org <mbelshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-09 20:18:50 +0000

    Take 2 at landing the SPDY SETTINGS frame patch.
    BUG=34749
    TEST=SpdyNetworkTransactionTest.Setting*
    
    Review URL: http://codereview.chromium.org/1547028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44118 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 74188f235dd5b4f8e80ca30d6e3a4d4c3061d84d

---
+++ net/spdy/spdy_settings_storage.cc
@@ -0,0 +1,47 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/spdy/spdy_settings_storage.h"
+
+#include <utility>
+
+namespace net {
+
+SpdySettingsStorage::SpdySettingsStorage() {
+}
+
+const spdy::SpdySettings& SpdySettingsStorage::Get(
+    const HostPortPair& host_port_pair) const {
+  SettingsMap::const_iterator it = settings_map_.find(host_port_pair);
+  if (it == settings_map_.end()) {
+    static const spdy::SpdySettings kEmpty;
+    return kEmpty;
+  }
+  return it->second;
+}
+
+void SpdySettingsStorage::Set(const HostPortPair& host_port_pair,
+                              const spdy::SpdySettings& settings) {
+  spdy::SpdySettings persistent_settings;
+
+  // Iterate through the list, and only copy those settings which are marked
+  // for persistence.
+  spdy::SpdySettings::const_iterator it;
+  for (it = settings.begin(); it != settings.end(); ++it) {
+    spdy::SettingsFlagsAndId id = it->first;
+    if (id.flags() & spdy::SETTINGS_FLAG_PLEASE_PERSIST) {
+      id.set_flags(spdy::SETTINGS_FLAG_PERSISTED);
+      persistent_settings.push_back(std::make_pair(id, it->second));
+    }
+  }
+
+  // If we didn't persist anything, then we are done.
+  if (persistent_settings.empty())
+    return;
+
+  settings_map_[host_port_pair] = persistent_settings;
+}
+
+}  // namespace net
+
---
+++ net/spdy/spdy_settings_storage.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_SPDY_SPDY_SETTING_STORAGE_H_
+#define NET_SPDY_SPDY_SETTING_STORAGE_H_
+
+#include <map>
+#include "base/basictypes.h"
+#include "net/base/host_port_pair.h"
+#include "net/spdy/spdy_framer.h"
+
+namespace net {
+
+// SpdySettingsStorage stores SpdySettings which have been transmitted between
+// endpoints for the SPDY SETTINGS frame.
+class SpdySettingsStorage {
+ public:
+  SpdySettingsStorage();
+
+  // Get a copy of the SpdySettings stored for a host.
+  // If no settings are stored, returns an empty set of settings.
+  const spdy::SpdySettings& Get(const HostPortPair& host_port_pair) const;
+
+  // Save settings for a host.
+  void Set(const HostPortPair& host_port_pair,
+           const spdy::SpdySettings& settings);
+
+ private:
+  typedef std::map<HostPortPair, spdy::SpdySettings> SettingsMap;
+
+  SettingsMap settings_map_;
+
+  DISALLOW_COPY_AND_ASSIGN(SpdySettingsStorage);
+};
+
+}  // namespace net
+
+#endif  // NET_SPDY_SPDY_SETTING_STORAGE_H_
+
