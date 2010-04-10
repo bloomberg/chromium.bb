7deefd7556b07a304e4ecfe6e78555f062b729de
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-10 00:26:07 +0000

    Bookmark manager - Change the tree expand icon behavior to match Brian's requests.
    BUG=None
    TEST=Open bookmarks manager. The tree should show the expand icons all the time (given that there are nested folders).
    
    
    Review URL: http://codereview.chromium.org/1639001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44169 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 61360defd9c57121f5045e82452beb9e371e8986

--- chrome/browser/resources/bookmark_manager/css/tree.css.js
+++ chrome/browser/resources/bookmark_manager/css/tree.css.js
@@ -5,49 +5,18 @@
 (function() {
   var a = 7;
   var a2 = a / 2;
-  var ctx = document.getCSSCanvasContext('2d', 'triangle-filled', a2 + 2, a + 1);
+  var ctx = document.getCSSCanvasContext('2d', 'tree-triangle', a + 1, a2 + 2);
 
   ctx.fillStyle = '#000';
   ctx.translate(.5, .5);
+
   ctx.beginPath();
   ctx.moveTo(0, 0);
-  ctx.lineTo(0, a);
-  ctx.lineTo(a2, a2);
+  ctx.lineTo(0, 1);
+  ctx.lineTo(a2, 1 + a2);
+  ctx.lineTo(a, 1);
+  ctx.lineTo(a, 0);
   ctx.closePath();
   ctx.fill();
-  ctx.stroke();
-
-  var ctx = document.getCSSCanvasContext('2d', 'triangle-empty', a2 + 2, a + 1);
-
-  ctx.strokeStyle = '#999';
-  ctx.lineWidth  = 1.2;
-  ctx.translate(.5, .5);
-  ctx.fillStyle = '#000';
-  ctx.beginPath();
-
-
-  ctx.moveTo(0, 0);
-  ctx.lineTo(0, a);
-  ctx.lineTo(a2, a2);
-  ctx.closePath();
-  ctx.stroke();
-
-  var ctx = document.getCSSCanvasContext('2d', 'triangle-hover', a2 + 2 + 4, a + 1 + 4);
-
-  ctx.shadowColor = 'hsl(214,91%,89%)'
-  ctx.shadowBlur = 3;
-  ctx.shadowOffsetX = 1;
-  ctx.shadowOffsetY = 0;
-
-  ctx.strokeStyle = 'hsl(214,91%,79%)';
-  ctx.lineWidth  = 1.2;
-  ctx.translate(.5 + 2, .5 + 2);
-  ctx.fillStyle = '#000';
-  ctx.beginPath();
-
-  ctx.moveTo(0, 0);
-  ctx.lineTo(0, a);
-  ctx.lineTo(a2, a2);
-  ctx.closePath();
   ctx.stroke();
 })();
