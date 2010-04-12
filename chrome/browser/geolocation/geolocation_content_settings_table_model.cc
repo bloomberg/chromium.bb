6dad8b3833fc9daecdb1437c6cfde63d93311b58
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-12 17:53:24 +0000

    Trivial: Change STL-container-is-empty check from "size() > 0" to "!empty()".
    TBR=thakis
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/1575029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44259 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 21e5541bdb7bbf7de3acca55640f623036684ed3

--- chrome/browser/geolocation/geolocation_content_settings_table_model.cc
+++ chrome/browser/geolocation/geolocation_content_settings_table_model.cc
@@ -70,7 +70,7 @@ bool GeolocationContentSettingsTableModel::CanRemoveExceptions(
       }
     }
   }
-  return rows.size() > 0;
+  return !rows.empty();
 }
 
 void GeolocationContentSettingsTableModel::RemoveExceptions(const Rows& rows) {
