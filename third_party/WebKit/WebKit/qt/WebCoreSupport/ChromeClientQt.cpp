bloomberg/chromium.bb

commit 8c373134e7aef5849fb988ac2e66c1b04821143b

[Qt] Implement ChromeClient::windowResizerRect()
Reviewed by Simon Hausmann.

https://bugs.webkit.org/show_bug.cgi?id=21300

We assume the resize corner to be in the lower right corner of
the window and having the width and height of the scrollbars.

The helper function geometryRelativeToOwnerWidget() in the page
client is used to clip the resize rect to the actual size of the
viewport, not the size of the QGraphicsView.

git-svn-id: svn://svn.chromium.org/blink/trunk@56552 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 19e3088d2287eabf51656e13b20dcb2cbaa7058f

third_party/WebKit/WebKit/qt/WebCoreSupport/ChromeClientQt.cpp
@@ -38,6 +38,7 @@
 #include "HitTestResult.h"
 #include "Icon.h"
 #include "NotImplemented.h"
+#include "ScrollbarTheme.h"
 #include "WindowFeatures.h"
 #include "DatabaseTracker.h"
 #include "QtFallbackWebPopup.h"
@@ -325,7 +326,35 @@ bool ChromeClientQt::tabsToLinks() const
 
 IntRect ChromeClientQt::windowResizerRect() const
 {
-    return IntRect();
+    if (!m_webPage)
+        return IntRect();
+
+    QWebPageClient* pageClient = platformPageClient();
+    if (!pageClient)
+        return IntRect();
+
+    QWidget* ownerWidget = pageClient->ownerWidget();
+    if (!ownerWidget)
+        return IntRect();
+
+    QWidget* topLevelWidget = ownerWidget->topLevelWidget();
+    QRect topLevelGeometry(topLevelWidget->geometry());
+
+    // There's no API in Qt to query for the size of the resizer, so we assume
+    // it has the same width and height as the scrollbar thickness.
+    int scollbarThickness = ScrollbarTheme::nativeTheme()->scrollbarThickness();
+
+    // There's no API in Qt to query for the position of the resizer. Sometimes
+    // it's drawn by the system, and sometimes it's a QSizeGrip. For RTL locales
+    // it might even be on the lower left side of the window, but in WebKit we
+    // always draw scrollbars on the right hand side, so we assume this to be the
+    // location when computing the resize rect to reserve for WebKit.
+    QPoint resizeCornerTopLeft = ownerWidget->mapFrom(topLevelWidget,
+            QPoint(topLevelGeometry.width(), topLevelGeometry.height())
+            - QPoint(scollbarThickness, scollbarThickness));
+
+    QRect resizeCornerRect = QRect(resizeCornerTopLeft, QSize(scollbarThickness, scollbarThickness));
+    return resizeCornerRect.intersected(pageClient->geometryRelativeToOwnerWidget());
 }
 
 void ChromeClientQt::invalidateWindow(const IntRect&, bool)

third_party/WebKit/WebCore/platform/qt/QWebPageClient.h
@@ -82,6 +82,7 @@ public:
     virtual QPalette palette() const = 0;
     virtual int screenNumber() const = 0;
     virtual QWidget* ownerWidget() const = 0;
+    virtual QRect geometryRelativeToOwnerWidget() const = 0;
 
     virtual QObject* pluginParent() const = 0;
 

third_party/WebKit/WebKit/qt/Api/qgraphicswebview.cpp
@@ -112,6 +112,7 @@ public:
     virtual QPalette palette() const;
     virtual int screenNumber() const;
     virtual QWidget* ownerWidget() const;
+    virtual QRect geometryRelativeToOwnerWidget() const;
 
     virtual QObject* pluginParent() const;
 
@@ -349,6 +350,19 @@ QWidget* QGraphicsWebViewPrivate::ownerWidget() const
         return views.value(0);
     }
     return 0;
+}
+
+QRect QGraphicsWebViewPrivate::geometryRelativeToOwnerWidget() const
+{
+    if (!q->scene())
+        return QRect();
+
+    QList<QGraphicsView*> views = q->scene()->views();
+    if (views.isEmpty())
+        return QRect();
+
+    QGraphicsView* view = views.at(0);
+    return view->mapFromScene(q->boundingRect()).boundingRect();
 }
 
 QObject* QGraphicsWebViewPrivate::pluginParent() const

third_party/WebKit/WebKit/qt/Api/qwebpage.cpp
@@ -226,6 +226,7 @@ public:
     virtual QPalette palette() const;
     virtual int screenNumber() const;
     virtual QWidget* ownerWidget() const;
+    virtual QRect geometryRelativeToOwnerWidget() const;
 
     virtual QObject* pluginParent() const;
 
@@ -291,6 +292,11 @@ int QWebPageWidgetClient::screenNumber() const
 QWidget* QWebPageWidgetClient::ownerWidget() const
 {
     return view;
+}
+
+QRect QWebPageWidgetClient::geometryRelativeToOwnerWidget() const
+{
+    return view->geometry();
 }
 
 QObject* QWebPageWidgetClient::pluginParent() const

commit 0908f8afcdd76fc60908c4b87f8e2bb4fde66338

REGRESSION(r56552): Broken scrollbars size https://bugs.webkit.org/show_bug.cgi?id=36853
Reviewed by Kenneth Christiansen and Tor Arne Vestbø.
Patch by Antonio Gomes <tonikitoo@webkit.org>

The regression was caused by r56552, which introduced a fix to bug
webkit.org/b/21300. The bug solved an issue with the resize handle on mac,
but did it in a way that affected all Qt platforms and thus broke the behavior
on non-mac platforms.

This patch makes the mac specific change ifdef'ed and only applied for the mac
platform.

* WebCoreSupport/ChromeClientQt.cpp:
(WebCore::ChromeClientQt::windowResizerRect):

git-svn-id: svn://svn.chromium.org/blink/trunk@57656 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 1d4553fdc829778161a1c84529c02a8a28390b14

third_party/WebKit/WebKit/qt/WebCoreSupport/ChromeClientQt.cpp
@@ -331,6 +331,7 @@ bool ChromeClientQt::tabsToLinks() const
 
 IntRect ChromeClientQt::windowResizerRect() const
 {
+#if defined(Q_WS_MAC)
     if (!m_webPage)
         return IntRect();
 
@@ -360,6 +361,9 @@ IntRect ChromeClientQt::windowResizerRect() const
 
     QRect resizeCornerRect = QRect(resizeCornerTopLeft, QSize(scollbarThickness, scollbarThickness));
     return resizeCornerRect.intersected(pageClient->geometryRelativeToOwnerWidget());
+#else
+    return IntRect();
+#endif
 }
 
 void ChromeClientQt::invalidateWindow(const IntRect&, bool)