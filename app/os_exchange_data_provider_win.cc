bloomberg/chromium.bb

commit 108ea90d4a266b706709e59fb052db13efe726bf

Win32 plumbing for dragend event.
This plumbs the actual dropEffect that occurred back into WebKit so that dragend is dispatched with the correct dropEffect. Note that this only helps with drags that start in Chrome and end outside of Chrome. Since WebDropTarget always lies and claims that no drop occurred, drags that start in Chrome and end in Chrome will still be incorrect.

BUG=39399
TEST=Manual testing using the attached test case on the bug.

Review URL: http://codereview.chromium.org/1136007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@43726 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 13a1166eb46a5b98e1cf5fce21ae327f3e059aa5

chrome/browser/views/tab_contents/tab_contents_drag_win.cc
@@ -294,14 +294,16 @@ void TabContentsDragWin::DoDragging(const WebDropData& drop_data,
       data.SetString(drop_data.plain_text);
   }
 
-  DWORD effects = 0;
-
   // We need to enable recursive tasks on the message loop so we can get
   // updates while in the system DoDragDrop loop.
   bool old_state = MessageLoop::current()->NestableTasksAllowed();
+  DWORD effect;
   MessageLoop::current()->SetNestableTasksAllowed(true);
   DoDragDrop(OSExchangeDataProviderWin::GetIDataObject(data), drag_source_,
-             WebDragOpToWinDragOp(ops), &effects);
+             WebDragOpToWinDragOp(ops), &effect);
+  // This works because WebDragSource::OnDragSourceDrop uses PostTask to
+  // dispatch the actual event.
+  drag_source_->set_effect(effect);
   MessageLoop::current()->SetNestableTasksAllowed(old_state);
 }
 

chrome/browser/tab_contents/web_drag_source_win.cc
@@ -8,11 +8,11 @@
 #include "chrome/browser/chrome_thread.h"
 #include "chrome/browser/renderer_host/render_view_host.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/browser/tab_contents/web_drag_utils_win.h"
 #include "chrome/common/notification_type.h"
 #include "chrome/common/notification_service.h"
 
 using WebKit::WebDragOperationNone;
-using WebKit::WebDragOperationCopy;
 
 namespace {
 
@@ -34,7 +34,8 @@ WebDragSource::WebDragSource(gfx::NativeWindow source_wnd,
                              TabContents* tab_contents)
     : BaseDragSource(),
       source_wnd_(source_wnd),
-      render_view_host_(tab_contents->render_view_host()) {
+      render_view_host_(tab_contents->render_view_host()),
+      effect_(DROPEFFECT_NONE) {
   registrar_.Add(this, NotificationType::TAB_CONTENTS_SWAPPED,
                  Source<TabContents>(tab_contents));
   registrar_.Add(this, NotificationType::TAB_CONTENTS_DISCONNECTED,
@@ -79,10 +80,9 @@ void WebDragSource::DelayedOnDragSourceDrop() {
   gfx::Point client;
   gfx::Point screen;
   GetCursorPositions(source_wnd_, &client, &screen);
-  render_view_host_->DragSourceEndedAt(client.x(), client.y(),
-                                       screen.x(), screen.y(),
-                                       WebDragOperationCopy);
-  // TODO(jpa): This needs to be fixed to send the actual drag operation.
+  render_view_host_->DragSourceEndedAt(
+      client.x(), client.y(), screen.x(), screen.y(),
+      web_drag_utils_win::WinDragOpToWebDragOp(effect_));
 }
 
 void WebDragSource::OnDragSourceMove() {

chrome/browser/tab_contents/web_drag_source_win.h
@@ -36,6 +36,8 @@ class WebDragSource : public BaseDragSource,
                        const NotificationSource& source,
                        const NotificationDetails& details);
 
+  void set_effect(DWORD effect) { effect_ = effect; }
+
  protected:
   // BaseDragSource
   virtual void OnDragSourceCancel();
@@ -59,6 +61,8 @@ class WebDragSource : public BaseDragSource,
   RenderViewHost* render_view_host_;
 
   NotificationRegistrar registrar_;
+
+  DWORD effect_;
 
   DISALLOW_COPY_AND_ASSIGN(WebDragSource);
 };

chrome/browser/tab_contents/web_drag_utils_win.cc
@@ -1,0 +1,29 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/tab_contents/web_drag_utils_win.h"
+
+#include <oleidl.h>
+
+using WebKit::WebDragOperationsMask;
+using WebKit::WebDragOperationNone;
+using WebKit::WebDragOperationCopy;
+using WebKit::WebDragOperationLink;
+using WebKit::WebDragOperationMove;
+
+namespace web_drag_utils_win {
+
+WebDragOperationsMask WinDragOpToWebDragOp(DWORD effect) {
+  WebDragOperationsMask op = WebDragOperationNone;
+  if (effect & DROPEFFECT_COPY)
+    op = static_cast<WebDragOperationsMask>(op | WebDragOperationCopy);
+  if (effect & DROPEFFECT_LINK)
+    op = static_cast<WebDragOperationsMask>(op | WebDragOperationLink);
+  if (effect & DROPEFFECT_MOVE)
+    op = static_cast<WebDragOperationsMask>(op | WebDragOperationMove);
+  return op;
+}
+
+}  // namespace web_drag_utils_win
+

chrome/browser/tab_contents/web_drag_utils_win.h
@@ -1,0 +1,19 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_TAB_CONTENTS_WEB_DRAG_UTILS_WIN_H_
+#define CHROME_BROWSER_TAB_CONTENTS_WEB_DRAG_UTILS_WIN_H_
+
+#include "third_party/WebKit/WebKit/chromium/public/WebDragOperation.h"
+
+#include <windows.h>
+
+namespace web_drag_utils_win {
+
+WebKit::WebDragOperationsMask WinDragOpToWebDragOp(DWORD effect);
+
+}  // namespace web_drag_utils_win
+
+#endif  // CHROME_BROWSER_TAB_CONTENTS_WEB_DRAG_UTILS_WIN_H_
+

chrome/browser/tab_contents/web_drop_target_win.cc
@@ -13,6 +13,7 @@
 #include "chrome/browser/bookmarks/bookmark_drag_data.h"
 #include "chrome/browser/renderer_host/render_view_host.h"
 #include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/browser/tab_contents/web_drag_utils_win.h"
 #include "gfx/point.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_util.h"
@@ -46,17 +47,6 @@ DWORD GetPreferredDragCursor(WebDragOperationsMask op) {
   if (op & WebDragOperationMove)
     return DROPEFFECT_MOVE;
   return DROPEFFECT_NONE;
-}
-
-WebDragOperationsMask WinDragOpToWebDragOp(DWORD effect) {
-  WebDragOperationsMask op = WebDragOperationNone;
-  if (effect & DROPEFFECT_COPY)
-    op = static_cast<WebDragOperationsMask>(op | WebDragOperationCopy);
-  if (effect & DROPEFFECT_LINK)
-    op = static_cast<WebDragOperationsMask>(op | WebDragOperationLink);
-  if (effect & DROPEFFECT_MOVE)
-    op = static_cast<WebDragOperationsMask>(op | WebDragOperationMove);
-  return op;
 }
 
 }  // anonymous namespace
@@ -142,7 +132,7 @@ DWORD WebDropTarget::OnDragEnter(IDataObject* data_object,
   tab_contents_->render_view_host()->DragTargetDragEnter(drop_data,
       gfx::Point(client_pt.x, client_pt.y),
       gfx::Point(cursor_position.x, cursor_position.y),
-      WinDragOpToWebDragOp(effect));
+      web_drag_utils_win::WinDragOpToWebDragOp(effect));
 
   // This is non-null if tab_contents_ is showing an ExtensionDOMUI with
   // support for (at the moment experimental) drag and drop extensions.
@@ -174,7 +164,7 @@ DWORD WebDropTarget::OnDragOver(IDataObject* data_object,
   tab_contents_->render_view_host()->DragTargetDragOver(
       gfx::Point(client_pt.x, client_pt.y),
       gfx::Point(cursor_position.x, cursor_position.y),
-      WinDragOpToWebDragOp(effect));
+      web_drag_utils_win::WinDragOpToWebDragOp(effect));
 
   if (tab_contents_->GetBookmarkDragDelegate()) {
     OSExchangeData os_exchange_data(new OSExchangeDataProviderWin(data_object));

chrome/chrome_browser.gypi
@@ -2057,6 +2057,8 @@
         'browser/tab_contents/web_drag_source_win.h',
         'browser/tab_contents/web_drag_dest_gtk.cc',
         'browser/tab_contents/web_drag_dest_gtk.h',
+        'browser/tab_contents/web_drag_utils_win.cc',
+        'browser/tab_contents/web_drag_utils_win.h',
         'browser/tab_contents/web_drop_target_win.cc',
         'browser/tab_contents/web_drop_target_win.h',
         'browser/tab_menu_model.cc',

commit 0e66ceeb04c1ae9688286b55a526626dce3a02f4

Fix 2 drag-out problems: 1) Does not work if dragging out to XP desktop. We need to return an error code    when waiting for XP Shell to start a background thread. 2) Occasional crash due to regression caused by r43726. Fix it by holding a    local reference to drag_source_.
BUG=none
TEST=none

Review URL: http://codereview.chromium.org/1619023

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44833 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 13897f5adc091bdb1000e61fcec730cfcfd53a9f

app/os_exchange_data_provider_win.cc
@@ -633,24 +633,25 @@ HRESULT DataObjectImpl::GetData(FORMATETC* format_etc, STGMEDIUM* medium) {
           }
         }
 
-        if (wait_for_data) {
-          // Notify the observer we start waiting for the data. This gives
-          // an observer a chance to end the drag and drop.
-          if (observer_)
-            observer_->OnWaitForData();
+        if (!wait_for_data)
+          return DV_E_FORMATETC;
 
-          // Now we can start the download.
-          if ((*iter)->downloader.get()) {
-            if (!(*iter)->downloader->Start(this)) {
-              is_aborting_ = true;
-              return DV_E_FORMATETC;
-            }
+        // Notify the observer we start waiting for the data. This gives
+        // an observer a chance to end the drag and drop.
+        if (observer_)
+          observer_->OnWaitForData();
+
+        // Now we can start the download.
+        if ((*iter)->downloader.get()) {
+          if (!(*iter)->downloader->Start(this)) {
+            is_aborting_ = true;
+            return DV_E_FORMATETC;
           }
-
-          // The stored data should have been updated with the final version.
-          // So we just need to call this function again to retrieve it.
-          return GetData(format_etc, medium);
         }
+
+        // The stored data should have been updated with the final version.
+        // So we just need to call this function again to retrieve it.
+        return GetData(format_etc, medium);
       }
       return S_OK;
     }

chrome/browser/views/tab_contents/tab_contents_drag_win.cc
@@ -284,17 +284,22 @@ void TabContentsDragWin::DoDragging(const WebDropData& drop_data,
       data.SetString(drop_data.plain_text);
   }
 
+  // Keep a local reference to drag_source_ in case that EndDragging is called
+  // before DoDragDrop returns.
+  scoped_refptr<WebDragSource> drag_source(drag_source_);
+
   // We need to enable recursive tasks on the message loop so we can get
   // updates while in the system DoDragDrop loop.
   bool old_state = MessageLoop::current()->NestableTasksAllowed();
-  DWORD effect;
   MessageLoop::current()->SetNestableTasksAllowed(true);
+  DWORD effect;
   DoDragDrop(OSExchangeDataProviderWin::GetIDataObject(data), drag_source_,
              web_drag_utils_win::WebDragOpMaskToWinDragOpMask(ops), &effect);
+  MessageLoop::current()->SetNestableTasksAllowed(old_state);
+
   // This works because WebDragSource::OnDragSourceDrop uses PostTask to
   // dispatch the actual event.
-  drag_source_->set_effect(effect);
-  MessageLoop::current()->SetNestableTasksAllowed(old_state);
+  drag_source->set_effect(effect);
 }
 
 void TabContentsDragWin::EndDragging(bool restore_suspended_state) {