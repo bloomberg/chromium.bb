9b8c04edbb564a8b69d2d8962d22e3e34f005a74
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-19 17:01:35 +0000

    Revert 44898 - Disable two test cases in the BeginningTransaction test until I fix them.
    TEST=none
    BUG=none
    TBR=amit
    
    Review URL: http://codereview.chromium.org/1633020
    
    TBR=tommi@chromium.org
    Review URL: http://codereview.chromium.org/1648017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@44920 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7a20911efec922a7b37e1d79c2d10a6b45b73d50

--- chrome_frame/test/http_negotiate_unittest.cc
+++ chrome_frame/test/http_negotiate_unittest.cc
@@ -67,27 +67,24 @@ TEST_F(HttpNegotiateTest, BeginningTransaction) {
 
   EXPECT_NE(std::wstring::npos, cf_ua.find(cf_tag));
 
-  // TODO(tommi): Fix the two test cases that are currently
-  // commented out.
-
   struct TestCase {
     const std::wstring original_headers_;
     const std::wstring delegate_additional_;
     const std::wstring expected_additional_;
     HRESULT delegate_return_value_;
   } test_cases[] = {
-    //{ L"Accept: */*\r\n\r\n",
-    //  L"",
-    //  cf_ua + L"\r\n\r\n",
-    //  S_OK },
+    { L"Accept: */*\r\n\r\n",
+      L"",
+      cf_ua + L"\r\n\r\n",
+      S_OK },
     { L"Accept: */*\r\n\r\n",
       L"",
       L"",
       E_OUTOFMEMORY },
-    //{ L"",
-    //  L"Accept: */*\r\n\r\n",
-    //  L"Accept: */*\r\n" + cf_ua + L"\r\n\r\n",
-    //  S_OK },
+    { L"",
+      L"Accept: */*\r\n\r\n",
+      L"Accept: */*\r\n" + cf_ua + L"\r\n\r\n",
+      S_OK },
     { L"User-Agent: Bingo/1.0\r\n\r\n",
       L"",
       L"User-Agent: Bingo/1.0 " + cf_tag + L"\r\n\r\n",
@@ -114,8 +111,7 @@ TEST_F(HttpNegotiateTest, BeginningTransaction) {
 
     if (additional) {
       // Check against the expected additional headers.
-      EXPECT_EQ(test.expected_additional_, std::wstring(additional))
-          << " case: " << i;
+      EXPECT_EQ(test.expected_additional_, std::wstring(additional));
       ::CoTaskMemFree(additional);
     }
   }
