5b2062502412bcb09f282cefce117769f600edf5
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-22 17:54:09 +0000

    Enable worker-close.html on Linux. Possible after changing the test to be more reliable on slow machines (http://trac.webkit.org/changeset/57916). BUG=36630 TEST=none
    Review URL: http://codereview.chromium.org/1703006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45327 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c35de2ba4ad094f3b688897c48a932d8c818509e

--- chrome/worker/worker_uitest.cc
+++ chrome/worker/worker_uitest.cc
@@ -217,11 +217,6 @@ TEST_F(WorkerTest, DISABLED_WorkerClonePort) {
   RunWorkerFastLayoutTest("worker-cloneport.html");
 }
 
-// Bug 36630
-#if defined(OS_LINUX)
-#define WorkerCloseFast DISABLED_WorkerCloseFast
-#endif
-
 TEST_F(WorkerTest, WorkerCloseFast) {
   RunWorkerFastLayoutTest("worker-close.html");
 }
