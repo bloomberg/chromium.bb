dd98a54fb351ff4042a124923157b4273e3cb926
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-04-23 00:34:51 +0000

    TCMalloc: Experiment with setting FLAGS_tcmalloc_release_rate to 50.  This is a performance / memory usage tradeoff.  50 is pretty high.  I'm going to revert this after a run or two on the buildbots. BUG=28385
    Review URL: http://codereview.chromium.org/1771002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@45389 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 096f7353ece3c42af366ed4fa2f451d6e826aa5c

--- third_party/tcmalloc/chromium/src/page_heap.cc
+++ third_party/tcmalloc/chromium/src/page_heap.cc
@@ -37,7 +37,7 @@
 #include "system-alloc.h"
 
 DEFINE_double(tcmalloc_release_rate,
-              EnvToDouble("TCMALLOC_RELEASE_RATE", 1.0),
+              EnvToDouble("TCMALLOC_RELEASE_RATE", 50.0),
               "Rate at which we release unused memory to the system.  "
               "Zero means we never release memory back to the system.  "
               "Increase this flag to return memory faster; decrease it "
