bloomberg/chromium.bb

commit ceceecfede87e2ba1f7e65a055e14d8d96269f69

Add syscall for loading code dynamically
 * Add implementation of dyncode_copy() syscall.
 * Add dynamic_load_mutex to struct NaClApp.
 * Add concurrency_ops.h to contain NaClWriteMemoryBarrier().
   I considered putting this in atomic_ops.h, but this is more about
   ordering of memory operations than atomicity.
 * Use /tmp rather than shm_open() for shared memory on Mac OS X.
   This is because shm_open() doesn't give us file descriptors that
   we can map with PROT_EXEC.
 * Enable nacl_text.c by setting use_shm_for_dynamic_text.
   (This code was previously inaccessible.)
 * Enable dynamic_load_test.c (previously disabled).

BUG=http://code.google.com/p/nativeclient/issues/detail?id=318
TEST=run_dynamic_load_test

Review URL: http://codereview.chromium.org/652032

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2097 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: cf65b7211f3796946f4ac6ead0d72f7ff1f881fe

src/shared/imc/nacl_imc_unistd.cc
@@ -41,7 +41,16 @@ namespace nacl {
 namespace {
 
 // The pathname prefix for memory objects created by CreateMemoryObject().
+#if NACL_OSX
+// On Mac OS X, shm_open() gives us file descriptors that the OS won't
+// mmap() with PROT_EXEC, which is no good for the dynamic code
+// region, so use /tmp instead.
+const char kShmPrefix[] = "/tmp/google-nacl-shm-";
+# define SHM_OPEN open
+#else
 const char kShmPrefix[] = "/google-nacl-shm-";
+# define SHM_OPEN shm_open
+#endif
 
 }  // namespace
 
@@ -81,7 +90,7 @@ Handle CreateMemoryObject(size_t length) {
     snprintf(name, sizeof name, "%s-%u.%u", kShmPrefix,
              getpid(),
              static_cast<uint32_t>(AtomicIncrement(&memory_object_count, 1)));
-    int m = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0);
+    int m = SHM_OPEN(name, O_RDWR | O_CREAT | O_EXCL, 0);
     if (0 <= m) {
       (void) shm_unlink(name);
       if (ftruncate(m, length) == -1) {

src/include/concurrency_ops.h
@@ -1,0 +1,53 @@
+/*
+ * Copyright 2010  The Native Client Authors.  All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can
+ * be found in the LICENSE file.
+ */
+
+
+#ifndef NATIVE_CLIENT_SRC_INCLUDE_CONCURRENCY_OPS_H_
+#define NATIVE_CLIENT_SRC_INCLUDE_CONCURRENCY_OPS_H_ 1
+
+
+#include "native_client/src/include/nacl_base.h"
+#include "native_client/src/include/portability.h"
+
+#if NACL_ARCH(NACL_BUILD_ARCH) == NACL_x86
+
+static INLINE void NaClWriteMemoryBarrier() {
+#if NACL_WINDOWS
+  /* Inline assembly is not available in x86-64 MSVC.  Use built-in. */
+  _mm_sfence();
+#else
+  __asm__ __volatile__("sfence");
+#endif
+}
+
+#elif NACL_ARCH(NACL_BUILD_ARCH) == NACL_arm
+
+static INLINE void NaClWriteMemoryBarrier() {
+  /*
+   * This depends on ARMv7.  Ideally we should pass "-march=armv7-a"
+   * to gcc instead of using ".arch", but that breaks the linking of
+   * the plugin .so, which fails to build all its required libraries
+   * with -fPIC.  TODO(mseaborn): Fix build to use -fPIC when needed.
+   * See http://code.google.com/p/nativeclient/issues/detail?id=121
+   */
+  __asm__ __volatile__(".arch armv7-a\n"
+                       "dsb");
+
+  /*
+   * We could support ARMv6 by instead using:
+   * __asm__ __volatile__("mcr p15, 0, %0, c7, c10, 5"
+   *                      : : "r" (0) : "memory");
+   */
+}
+
+#else
+
+#error "Define for other architectures"
+
+#endif
+
+
+#endif  /* NATIVE_CLIENT_SRC_INCLUDE_CONCURRENCY_OPS_H_ */

src/trusted/service_runtime/arch/arm/sel_ldr_arm.h
@@ -18,6 +18,7 @@
 #define NACL_NOOP_OPCODE        0xe1a00000  /* mov r0, r0 */
 #define NACL_HALT_OPCODE        0xe1266676  /* bkpt 6666 */
 #define NACL_HALT_LEN           4           /* length of halt instruction */
+#define NACL_HALT_WORD          NACL_HALT_OPCODE
 
 struct NaClApp;
 struct NaClThreadContext;

src/trusted/service_runtime/arch/x86/sel_ldr_x86.h
@@ -32,5 +32,6 @@
 #define NACL_NOOP_OPCODE    0x90
 #define NACL_HALT_OPCODE    0xf4
 #define NACL_HALT_LEN       1           /* length of halt instruction */
+#define NACL_HALT_WORD      0xf4f4f4f4U
 
 #endif /* SERVICE_RUNTIME_ARCH_X86_SEL_LDR_H__ */

src/trusted/service_runtime/arch/x86_32/nacl_app_32.c
@@ -21,7 +21,7 @@
 
 static uint16_t NaClAllocateSegmentForCodeRegion(struct NaClApp *nap) {
   uintptr_t code_start = nap->mem_start;
-  size_t    code_bytes = nap->static_text_end;
+  size_t    code_bytes = nap->dynamic_text_end;
   size_t    code_pages = code_bytes >> NACL_PAGESHIFT;
 
   VCHECK((code_bytes & ((1 << NACL_PAGESHIFT) - 1)) == 0,

src/trusted/service_runtime/linux/nacl_syscall_impl.c
@@ -38,6 +38,7 @@
 #include "native_client/src/trusted/service_runtime/nacl_memory_object.h"
 #include "native_client/src/trusted/service_runtime/nacl_syscall_handlers.h"
 #include "native_client/src/trusted/service_runtime/nacl_syscall_common.h"
+#include "native_client/src/trusted/service_runtime/nacl_text.h"
 #include "native_client/src/trusted/service_runtime/sel_util.h"
 #include "native_client/src/trusted/service_runtime/sel_ldr.h"
 #include "native_client/src/trusted/service_runtime/sel_memory.h"
@@ -643,4 +644,11 @@ int32_t NaClSysSysconf(struct NaClAppThread *natp,
 cleanup:
   NaClSysCommonThreadSyscallLeave(natp);
   return retval;
+}
+
+int32_t NaClSysDyncode_Copy(struct NaClAppThread *natp,
+                            uint32_t             dest,
+                            uint32_t             src,
+                            uint32_t             size) {
+  return NaClTextSysDyncode_Copy(natp, dest, src, size);
 }

src/trusted/service_runtime/nacl_text.c
@@ -4,13 +4,20 @@
  * be found in the LICENSE file.
  */
 
+#include <string.h>
+
+#include "native_client/src/include/concurrency_ops.h"
 #include "native_client/src/include/portability.h"
+#include "native_client/src/shared/platform/nacl_check.h"
 #include "native_client/src/shared/platform/nacl_log.h"
 #include "native_client/src/trusted/desc/nacl_desc_base.h"
 #include "native_client/src/trusted/desc/nacl_desc_effector.h"
+#include "native_client/src/trusted/desc/nacl_desc_effector.h"
 #include "native_client/src/trusted/desc/nacl_desc_imc_shm.h"
+#include "native_client/src/trusted/service_runtime/arch/sel_ldr_arch.h"
 #include "native_client/src/trusted/service_runtime/include/sys/errno.h"
 #include "native_client/src/trusted/service_runtime/include/sys/mman.h"
+#include "native_client/src/trusted/service_runtime/nacl_app_thread.h"
 #include "native_client/src/trusted/service_runtime/nacl_error_code.h"
 #include "native_client/src/trusted/service_runtime/nacl_text.h"
 #include "native_client/src/trusted/service_runtime/sel_ldr.h"
@@ -236,6 +243,7 @@ NaClErrorCode NaClMakeDynamicTextShared(struct NaClApp *nap) {
     }
   }
 
+  nap->text_shm = &shm->base;
   retval = LOAD_OK;
 
 cleanup:
@@ -247,5 +255,206 @@ cleanup:
     free(shm);
   }
 
+  return retval;
+}
+
+static int CodeRangeIsUnused(uint8_t *data, uint32_t size) {
+  uint32_t *end = (uint32_t *) (data + size);
+  uint32_t *addr;
+  for (addr = (uint32_t *) data; addr < end; addr++) {
+    /* Check that the code range contains only HLT instructions. */
+    if (NACL_HALT_WORD != *addr)
+      return 0;
+  }
+  return 1;
+}
+
+static void CopyBundleTails(uint8_t *dest,
+                            uint8_t *src,
+                            int32_t size,
+                            int     bundle_size) {
+  /*
+   * The order in which these locations are written does not matter:
+   * none of the locations will be reachable, because the bundle heads
+   * still contains HLTs.
+   */
+  int bundle_mask = bundle_size - 1;
+  uint32_t *src_ptr;
+  uint32_t *dest_ptr;
+  uint32_t *end_ptr;
+
+  CHECK(0 == ((uintptr_t) dest & 3));
+
+  src_ptr = (uint32_t *) src;
+  dest_ptr = (uint32_t *) dest;
+  end_ptr = (uint32_t *) (dest + size);
+  while (dest_ptr < end_ptr) {
+    if ((((uintptr_t) dest_ptr) & bundle_mask) != 0) {
+      *dest_ptr = *src_ptr;
+    }
+    dest_ptr++;
+    src_ptr++;
+  }
+}
+
+static void CopyBundleHeads(uint8_t  *dest,
+                            uint8_t  *src,
+                            uint32_t size,
+                            int      bundle_size) {
+  /* Again, the order in which these locations are written does not matter. */
+  uint8_t *src_ptr;
+  uint8_t *dest_ptr;
+  uint8_t *end_ptr;
+
+  /* dest must be aligned for the writes to be atomic. */
+  CHECK(0 == ((uintptr_t) dest & 3));
+
+  src_ptr = src;
+  dest_ptr = dest;
+  end_ptr = dest + size;
+  while (dest_ptr < end_ptr) {
+    /*
+     * We assume that writing the 32-bit int here is atomic, which is
+     * the case on x86 and ARM as long as the address is word-aligned.
+     * The read does not have to be atomic.
+     */
+    *(uint32_t *) dest_ptr = *(uint32_t *) src_ptr;
+    dest_ptr += bundle_size;
+    src_ptr += bundle_size;
+  }
+}
+
+static void CopyCodeSafely(uint8_t  *dest,
+                           uint8_t  *src,
+                           uint32_t size,
+                           int      bundle_size) {
+  CopyBundleTails(dest, src, size, bundle_size);
+  NaClWriteMemoryBarrier();
+  CopyBundleHeads(dest, src, size, bundle_size);
+}
+
+int32_t NaClTextSysDyncode_Copy(struct NaClAppThread *natp,
+                                uint32_t             dest,
+                                uint32_t             src,
+                                uint32_t             size) {
+  struct NaClApp *nap = natp->nap;
+  uintptr_t dest_addr;
+  uintptr_t src_addr;
+  uint32_t shm_offset;
+  uint32_t shm_map_offset;
+  uint32_t within_page_offset;
+  uint32_t shm_map_offset_end;
+  uint32_t shm_map_size;
+  struct NaClDescEffectorShm shm_effector;
+  struct NaClDesc *shm = nap->text_shm;
+  uintptr_t mmap_ret;
+  uint8_t *mmap_result;
+  uint8_t *mapped_addr;
+  uint8_t *code_copy;
+  int validator_result;
+  int32_t retval = -NACL_ABI_EINVAL;
+
+  if (!shm) {
+    NaClLog(1, "NaClTextSysDyncode_Copy: Dynamic loading not enabled\n");
+    return -NACL_ABI_EINVAL;
+  }
+  if (0 != (dest & (nap->bundle_size - 1)) ||
+      0 != (size & (nap->bundle_size - 1))) {
+    NaClLog(1, "NaClTextSysDyncode_Copy: Non-bundle-aligned address or size\n");
+    return -NACL_ABI_EINVAL;
+  }
+  dest_addr = NaClUserToSysAddrRange(nap, dest, size);
+  src_addr = NaClUserToSysAddrRange(nap, src, size);
+  if (dest_addr == kNaClBadAddress ||
+      src_addr == kNaClBadAddress) {
+    NaClLog(1, "NaClTextSysDyncode_Copy: Address out of range\n");
+    return -NACL_ABI_EFAULT;
+  }
+  if (dest < nap->dynamic_text_start) {
+    NaClLog(1, "NaClTextSysDyncode_Copy: Below dynamic code area\n");
+    return -NACL_ABI_EFAULT;
+  }
+  if (dest + size > nap->dynamic_text_end) {
+    NaClLog(1, "NaClTextSysDyncode_Copy: Above dynamic code area\n");
+    return -NACL_ABI_EFAULT;
+  }
+  if (size == 0) {
+    /* Nothing to load.  Succeed trivially. */
+    return 0;
+  }
+
+  shm_offset = dest - (uint32_t) nap->dynamic_text_start;
+  shm_map_offset = shm_offset & ~(NACL_MAP_PAGESIZE - 1);
+  within_page_offset = shm_offset & (NACL_MAP_PAGESIZE - 1);
+  shm_map_offset_end =
+    (shm_offset + size + NACL_MAP_PAGESIZE - 1) & ~(NACL_MAP_PAGESIZE - 1);
+  shm_map_size = shm_map_offset_end - shm_map_offset;
+
+  if (!NaClDescEffectorShmCtor(&shm_effector)) {
+    NaClLog(LOG_FATAL,
+            "NaClTextSysDyncode_Copy: "
+            "shm effector initialization failed\n");
+  }
+
+  mmap_ret = (*shm->vtbl->Map)(shm, (struct NaClDescEffector *) &shm_effector,
+                               NULL, shm_map_size,
+                               NACL_ABI_PROT_READ | NACL_ABI_PROT_WRITE,
+                               NACL_ABI_MAP_SHARED, shm_map_offset);
+  if (NaClIsNegErrno(mmap_ret)) {
+    return (int32_t) mmap_ret;
+  }
+  mmap_result = (uint8_t *) mmap_ret;
+  mapped_addr = mmap_result + within_page_offset;
+  CHECK(mmap_result <= mapped_addr);
+  CHECK(mapped_addr + size <= mmap_result + shm_map_size);
+
+  /*
+   * Make a private copy of the code, so that we can validate it
+   * without a TOCTTOU race condition.
+   */
+  code_copy = malloc(size);
+  if (!code_copy) {
+    retval = -NACL_ABI_ENOMEM;
+  }
+  else {
+    memcpy(code_copy, (uint8_t *) src_addr, size);
+
+    NaClMutexLock(&nap->dynamic_load_mutex);
+
+    /*
+     * In principle, this can go outside the lock, but the validator
+     * has global mutable state which might not be used thread-safely.
+     * TODO(mseaborn): Check before moving this outside the lock.
+     */
+    validator_result = NaClValidateCode(nap, dest, code_copy, size);
+    if (validator_result != LOAD_OK) {
+      NaClLog(1, "NaClTextSysDyncode_Copy: "
+              "Validation of dynamic code failed\n");
+      retval = -NACL_ABI_EINVAL;
+    }
+    else {
+      if (!CodeRangeIsUnused(mapped_addr, size)) {
+        /*
+         * We cannot safely overwrite this memory because other
+         * threads could be executing code from it.
+         */
+        NaClLog(1, "NaClTextSysDyncode_Copy: Code range already allocated\n");
+        retval = -NACL_ABI_EINVAL;
+      }
+      else {
+        CopyCodeSafely(mapped_addr, code_copy, size, nap->bundle_size);
+        retval = 0;
+      }
+    }
+
+    NaClMutexUnlock(&nap->dynamic_load_mutex);
+    free(code_copy);
+  }
+
+  if (0 != (*shm->vtbl->UnmapUnsafe)(shm,
+                                     (struct NaClDescEffector *) &shm_effector,
+                                     mmap_result, shm_map_size)) {
+    NaClLog(LOG_FATAL, "NaClTextSysDyncode_Copy: Failed to unmap\n");
+  }
   return retval;
 }

src/trusted/service_runtime/nacl_text.h
@@ -13,6 +13,7 @@
 EXTERN_C_BEGIN
 
 struct NaClApp;
+struct NaClAppThread;
 
 /*
  * Create a shared memory descriptor and map it into the text region
@@ -20,6 +21,14 @@ struct NaClApp;
  * multiple of NACL_MAP_PAGESIZE.
  */
 NaClErrorCode NaClMakeDynamicTextShared(struct NaClApp *nap);
+
+struct NaClDescEffectorShm;
+int NaClDescEffectorShmCtor(struct NaClDescEffectorShm *self);
+
+int32_t NaClTextSysDyncode_Copy(struct NaClAppThread *natp,
+                                uint32_t             dest,
+                                uint32_t             src,
+                                uint32_t             size);
 
 EXTERN_C_END
 

commit 4f1efe3a3f6388265c69e633069b5b9f9dba6ca1

Fix shared memory to work again in the Chromium Mac sandbox
This fixes breakage introduced by r2097, which stopped shared memory
creation from working in the context of Chromium's outer sandbox on
Mac.  /tmp and open() are not available inside this sandbox.

We work around this by trying open() first and by falling back to
shm_open().  On the Mac, shm_open() is a special syscall which works
inside the sandbox.  (In contrast, on Linux, shm_open() is implemented
in terms of open().)

This means that dynamic loading will work in standalone NaCl or with
--no-sandbox.  We will need to change this to use RPC to get SHM FDs.
The Chromium code for RPC in the sandbox is currently Linux-specific.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=467

Review URL: http://codereview.chromium.org/1810007

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2160 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: c494fdc5195b4a2b53abcceb6bc64766daceb356

src/shared/imc/nacl_imc_unistd.cc
@@ -40,17 +40,10 @@ namespace nacl {
 
 namespace {
 
-// The pathname prefix for memory objects created by CreateMemoryObject().
-#if NACL_OSX
-// On Mac OS X, shm_open() gives us file descriptors that the OS won't
-// mmap() with PROT_EXEC, which is no good for the dynamic code
-// region, so use /tmp instead.
-const char kShmPrefix[] = "/tmp/google-nacl-shm-";
-# define SHM_OPEN open
-#else
-const char kShmPrefix[] = "/google-nacl-shm-";
-# define SHM_OPEN shm_open
-#endif
+// The pathname or SHM-namespace prefixes for memory objects created
+// by CreateMemoryObject().
+const char kShmTempPrefix[] = "/tmp/google-nacl-shm-";
+const char kShmOpenPrefix[] = "/google-nacl-shm-";
 
 }  // namespace
 
@@ -80,17 +73,23 @@ int GetLastErrorString(char* buffer, size_t length) {
 
 static AtomicWord memory_object_count = 0;
 
-Handle CreateMemoryObject(size_t length) {
+static int TryShmOrTempOpen(size_t length, bool use_temp) {
   if (0 == length) {
     return -1;
   }
 
   char name[PATH_MAX];
   for (;;) {
-    snprintf(name, sizeof name, "%s-%u.%u", kShmPrefix,
+    const char *prefix = use_temp ? kShmTempPrefix : kShmOpenPrefix;
+    snprintf(name, sizeof name, "%s-%u.%u", prefix,
              getpid(),
              static_cast<uint32_t>(AtomicIncrement(&memory_object_count, 1)));
-    int m = SHM_OPEN(name, O_RDWR | O_CREAT | O_EXCL, 0);
+    int m;
+    if (use_temp) {
+      m = open(name, O_RDWR | O_CREAT | O_EXCL, 0);
+    } else {
+      m = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0);
+    }
     if (0 <= m) {
       (void) shm_unlink(name);
       if (ftruncate(m, length) == -1) {
@@ -100,17 +99,53 @@ Handle CreateMemoryObject(size_t length) {
       return m;
     }
     if (errno != EEXIST) {
-#if NACL_LINUX && (defined(CHROMIUM_BUILD) || defined(GOOGLE_CHROME_BUILD))
-      // As a temporary measure, we try shm_open() as well as calling
-      // the unsandboxed browser process.  This code runs in the
-      // context of both the renderer and (Chromium's compiled-in)
-      // sel_ldr.  Currently sel_ldr is not sandboxed and doesn't have
-      // the appropriate socket FD set up for talking to the browser.
-      return renderer_sandbox_support::MakeSharedMemorySegmentViaIPC(length);
-#endif
       return -1;
     }
+    // Retry only if we got EEXIST.
   }
+}
+
+Handle CreateMemoryObject(size_t length) {
+  if (0 == length) {
+    return -1;
+  }
+  int fd;
+
+  // On Mac OS X, shm_open() gives us file descriptors that the OS
+  // won't mmap() with PROT_EXEC, which is no good for the dynamic
+  // code region.  Try open()ing a file in /tmp first, but fall back
+  // to using shm_open() if /tmp is not available, which will be the
+  // case inside the Chromium sandbox.  This means that dynamic
+  // loading will only work with --no-sandbox.
+  //
+  // TODO(mseaborn): We will probably need to do IPC to acquire SHM FDs
+  // inside the Chromium Mac sandbox, as on Linux.
+
+#if NACL_OSX
+  // Try /tmp first.  It would be OK to enable this for Linux, but
+  // there's no need because shm_open() (which uses /dev/shm rather
+  // than /tmp) is fine on Linux.
+  fd = TryShmOrTempOpen(length, true);
+  if (fd >= 0)
+    return fd;
+#endif
+  
+  // Try shm_open().
+  fd = TryShmOrTempOpen(length, false);
+  if (fd >= 0)
+    return fd;
+
+#if NACL_LINUX && (defined(CHROMIUM_BUILD) || defined(GOOGLE_CHROME_BUILD))
+  // As a temporary measure, we try shm_open() as well as calling
+  // the unsandboxed browser process.  This code runs in the
+  // context of both the renderer and (Chromium's compiled-in)
+  // sel_ldr.  Currently sel_ldr is not sandboxed and doesn't have
+  // the appropriate socket FD set up for talking to the browser.
+  // TODO(mseaborn): Move this to be the first method tried.
+  return renderer_sandbox_support::MakeSharedMemorySegmentViaIPC(length);
+#endif
+
+  return -1;
 }
 
 void* Map(void* start, size_t length, int prot, int flags,