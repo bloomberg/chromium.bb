ee3dc8b5ad8f05904864b8b5b34604bc804a800c
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-03 22:14:47 +0000

    Enable Core Animation for Flash by default
    Enabling this now on the Dev channel will give us better coverage to find any issues we don't know about yet
    
    BUG=38932
    TEST='gpu' and 'direct' wmode Flash instances should use CA on 10.6 (renderer CPU should be very low during video playback)
    
    Review URL: http://codereview.chromium.org/1877004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46285 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b2f872a12a12606065eabb59e4e338b122c6b372

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -255,7 +255,7 @@ extern const char kNoProcessSingletonDialog[];
 #endif
 
 #if defined(OS_MACOSX)
-extern const char kEnableFlashCoreAnimation[];
+extern const char kDisableFlashCoreAnimation[];
 extern const char kEnableSandboxLogging[];
 #else
 extern const char kKioskMode[];
