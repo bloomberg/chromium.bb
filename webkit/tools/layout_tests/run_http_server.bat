c4113adb8a8854453853c6a18a3083decabeaeec
michaeln@chromium.org <michaeln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-03 18:33:00 +0000

    Switch run_http_server.bat to using python from depot_tools instead of what's checked in to third_party.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/1774020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46250 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 66c652a9f38913528f6867cebc46141a15396b2f

--- webkit/tools/layout_tests/run_http_server.bat
+++ webkit/tools/layout_tests/run_http_server.bat
@@ -1,1 +1,1 @@
-@%~dp0\..\..\..\third_party\python_24\python.exe %~dp0\run_http_server.py %*
+@python %~dp0\run_http_server.py %*
--- webkit/tools/layout_tests/run_http_server.sh
+++ webkit/tools/layout_tests/run_http_server.sh
@@ -7,16 +7,12 @@
 exec_dir=$(dirname $0)
 
 if [ "$OSTYPE" = "cygwin" ]; then
-  system_root=`cygpath "$SYSTEMROOT"`
-  PATH="/usr/bin:$system_root/system32:$system_root:$system_root/system32/WBEM"
-  export PATH
-  unset PYTHONPATH
-  PYTHON_PROG="$exec_dir/../../../third_party/python_24/python.exe"
   SCRIPT=$(cygpath -wa "$exec_dir/run_http_server.py")
 else
-  PYTHON_PROG=python
-  unset PYTHONPATH
   SCRIPT="$exec_dir/run_http_server.py"
 fi
+
+PYTHON_PROG=python
+unset PYTHONPATH
 
 "$PYTHON_PROG" "$SCRIPT" "$@"
