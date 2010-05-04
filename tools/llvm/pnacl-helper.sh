aaf4f7586c78a0a2511666f3dae0245f1516d23e
adonovan@google.com <adonovan@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-05-04 14:48:15 +0000

    Tweaks to pnacl-helper.sh: improved docstrings, simpler dispatch.
    Review URL: http://codereview.chromium.org/1914003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2199 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 707b5f000a56b554143622e76a20dd60a318479e

--- tools/llvm/pnacl-helper.sh
+++ tools/llvm/pnacl-helper.sh
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can
 # be found in the LICENSE file.
 #
-#@ This script performs various helper task for pnacl.
+#@ This script performs various helper tasks for pnacl.
 #@ NOTE: It must be run from the native_client/ directory.
 ######################################################################
 # Config
@@ -19,9 +19,8 @@ readonly PNACL_X8632_ROOT=${PNACL_TOOLCHAIN_ROOT}/x8632
 readonly PNACL_X8664_ROOT=${PNACL_TOOLCHAIN_ROOT}/x8664
 readonly PNACL_BITCODE_ROOT=${PNACL_TOOLCHAIN_ROOT}/bitcode
 
-
 ######################################################################
-# Helper
+# Helpers
 ######################################################################
 
 Banner() {
@@ -30,17 +29,14 @@ Banner() {
   echo "######################################################################"
 }
 
+# Print the usage message to stdout.
 Usage() {
   egrep "^#@" $0 | cut --bytes=3-
 }
 
-######################################################################
-#
-######################################################################
-
 # Usage: VerifyObject <regexp> <filename>
 # Ensure that the ELF "file format" string for <filename> matches <regexp>.
-VerifyObject () {
+VerifyObject() {
   local pattern=$1 filename=$2
   echo -n "verify $filename [$pattern]: "
   local format=$(objdump -a "$filename" |
@@ -54,7 +50,10 @@ VerifyObject () {
   fi
 }
 
-VerifyLLVM () {
+# Usage: VerifyLLVM <filename>
+# Ensure that the specified file's format is unrecognized by objdump,
+# i.e. probably in LLVM format.
+VerifyLLVM() {
   echo -n "verify $1: "
   local n=$(objdump -a $1 2>&1 | grep -c "format")
   local good=$(objdump -a $1 2>&1 | grep -c "not recognized")
@@ -68,48 +67,36 @@ VerifyLLVM () {
 }
 
 ######################################################################
-# Main
+# Actions
 ######################################################################
-if [ $(basename $(pwd)) != "native_client" ]; then
-  echo "You must run this script from the 'native_client' directory." >&2
-  exit -1
-fi
-if [ $# -eq 0 ] ; then
-  echo
-  echo "ERROR: you must specify a mode on the command line:"
-  echo
-  Usage
-  exit -1
-fi
-
-MODE=$1
-shift
 
 #@
 #@ help
 #@
-#@   print help for all modes
-if [ $MODE} = 'help' ] ; then
+#@   Print help for all modes.
+help() {
   Usage
-  exit 0
-fi
+}
 
 #@
 #@ download-toolchains
 #@
-#@   down load all the toolchains needed by pnacl
-if [ ${MODE} = 'download-toolchains' ] ; then
+#@   Download all the toolchains + SDKs needed by PNaCl.
+download-toolchains() {
   ./scons platform=arm --download sdl=none
   ./scons platform=x86-64 --download sdl=none
-  exit 0
-fi
+}
 
 #@
 #@ organize-native-code
 #@
-#@   save and organize native code
-if [ ${MODE} = 'organize-native-code' ] ; then
-  rm -rf ${PNACL_TOOLCHAIN_ROOT}
+#@   Saves the native code libraries (e.g. crt*.o) for each architecture
+#@   into the toolchain/pnacl-untrusted/<arch> directories.
+#@
+#@   (You'll need to re-run this step after you modify the native code sources
+#@   and re-run the "nacl_extra_sdk" build.)
+organize-native-code() {
+  rm -rf ${PNACL_TOOLCHAIN_ROOT}  # (a bit aggressive when making mods?)
 
   readonly arm_src=toolchain/linux_arm-untrusted
   readonly x86_src=toolchain/linux_x86/sdk/nacl-sdk
@@ -125,30 +112,30 @@ if [ ${MODE} = 'organize-native-code' ] ; then
 
   Banner "x86-32 native code: ${PNACL_X8632_ROOT}"
   mkdir -p ${PNACL_X8632_ROOT}
-  cp ${x86_src}/lib/gcc/nacl64/4.4.3/32/libgcc.a ${PNACL_X8632_ROOT}
-  cp ${x86_src}/nacl64/lib/32/crt*.o ${PNACL_X8632_ROOT}
-  cp ${x86_src}/nacl64/lib/32/libcrt*.a ${PNACL_X8632_ROOT}
-  cp ${x86_src}/nacl64/lib/32/intrinsics.o ${PNACL_X8632_ROOT}
+  cp ${x86_src}/lib/gcc/nacl64/4.4.3/32/libgcc.a \
+     ${x86_src}/nacl64/lib/32/crt*.o \
+     ${x86_src}/nacl64/lib/32/libcrt*.a \
+     ${x86_src}/nacl64/lib/32/intrinsics.o \
+     ${PNACL_X8632_ROOT}
   ls -l ${PNACL_X8632_ROOT}
 
   Banner "x86-64 native code: ${PNACL_X8664_ROOT}"
   mkdir -p ${PNACL_X8664_ROOT}
-  cp ${x86_src}/lib/gcc/nacl64/4.4.3/libgcc.a ${PNACL_X8664_ROOT}
-  cp ${x86_src}/nacl64/lib/crt*.o ${PNACL_X8664_ROOT}
-  cp ${x86_src}/nacl64/lib/libcrt*.a ${PNACL_X8664_ROOT}
+  cp ${x86_src}/lib/gcc/nacl64/4.4.3/libgcc.a \
+     ${x86_src}/nacl64/lib/crt*.o \
+     ${x86_src}/nacl64/lib/libcrt*.a \
+     ${PNACL_X8664_ROOT}
   # NOTE: we do not yet have a this for x86-64
   #cp ${x86_src}/nacl64/lib/intrinsics.o ${X8664_ROOT}
   ls -l ${PNACL_X8664_ROOT}
-
-  exit 0
-fi
+}
 
 #@
 #@ verify
 #@
-#@   verify files in platform directories
-
-if [ ${MODE} = 'verify' ] ; then
+#@   Verifies that toolchain/pnacl-untrusted ELF files are of the correct
+#@   architecture.
+verify() {
   Banner "Verify ${PNACL_ARM_ROOT}"
   for i in ${PNACL_ARM_ROOT}/*.[oa] ; do
     VerifyObject 'elf32-little\(arm\)\?' $i  # objdumps vary in their output.
@@ -168,15 +155,13 @@ if [ ${MODE} = 'verify' ] ; then
   for i in ${PNACL_BITCODE_ROOT}/*.[oa] ; do
     VerifyLLVM $i
   done
-
-  exit 0
-fi
+}
 
 #@
 #@ build-bitcode
 #@
-#@   build bitcode libs (newlib and extra sdk)
-if [ ${MODE} = 'build-bitcode' ] ; then
+#@   Builds bitcode libraries (e.g. newlib and extra SDK).
+build-bitcode() {
   rm -rf ${PNACL_BITCODE_ROOT}
   mkdir -p ${PNACL_BITCODE_ROOT}
 
@@ -198,22 +183,35 @@ if [ ${MODE} = 'build-bitcode' ] ; then
   rm ${PNACL_BITCODE_ROOT}/libcrt_platform.a
 
   # TODO: cpp suppport is still broken so we fake the library
-  exit 0
-fi
+}
 
 #@
 #@ build-bitcode-cpp
 #@
-#@   build bitcode for cpp - currently broken
-if [ ${MODE} = 'build-bitcode-cpp' ] ; then
+#@   Build bitcode libraries for C++.  Currently broken.
+build-bitcode-cpp() {
   Banner "C++"
   tools/llvm/untrusted-toolchain-creator.sh libstdcpp-libonly \
       ${PNACL_BITCODE_ROOT}
 
   Banner "Summary  ${PNACL_BITCODE_ROOT}"
   ls -l ${PNACL_BITCODE_ROOT}
-  exit 0
+}
+
+#@
+
+######################################################################
+# Main
+
+if [ $(basename $(pwd)) != "native_client" ]; then
+  echo "You must run this script from the 'native_client' directory." >&2
+  exit -1
 fi
 
-echo "ERROR: unknown mode ${MODE}"
-exit -1
+if [ "$(type -t $1)" != "function" ]; then
+  Usage
+  echo "ERROR: unknown mode '$1'." >&2
+  exit 1
+fi
+
+eval "$@"
