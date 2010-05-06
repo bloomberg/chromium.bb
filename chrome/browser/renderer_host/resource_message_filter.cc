87b8339a07cfb1fcfaea6cd6d0b048a362d3c04e
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-06 23:11:30 +0000

    Handle CBF_SMBITMAP in the async version of the IPC.
    BUG=43307
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/1973002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46639 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7473b624aff7e1db5b22d7a856d1f21509fa04bc

--- chrome/browser/renderer_host/resource_message_filter.cc
+++ chrome/browser/renderer_host/resource_message_filter.cc
@@ -875,6 +875,10 @@ void ResourceMessageFilter::OnClipboardWriteObjectsAsync(
   // the write on the UI thread.
   Clipboard::ObjectMap* long_living_objects = new Clipboard::ObjectMap(objects);
 
+  // This async message doesn't support shared-memory based bitmaps; they must
+  // be removed otherwise we might dereference a rubbish pointer.
+  long_living_objects->erase(Clipboard::CBF_SMBITMAP);
+
   ChromeThread::PostTask(
       ChromeThread::UI,
       FROM_HERE,
