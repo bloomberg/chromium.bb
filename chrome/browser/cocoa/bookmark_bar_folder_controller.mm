65c7cb90e2246057516c628fa9ae87005419008b
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-06 15:42:11 +0000

    The button was being told to highlight but then it was not being told it needed to be redrawn.
    BUG=43225
    TEST=Drag a bookmark into a bar folder and then a subfolder and verify that the buttons in the path to the open subfolder are all highlighted.
    
    Review URL: http://codereview.chromium.org/2006001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46570 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8dd8df87354afec54b34c0fbdc96356e407568f6

--- chrome/browser/cocoa/bookmark_bar_folder_controller.mm
+++ chrome/browser/cocoa/bookmark_bar_folder_controller.mm
@@ -85,6 +85,7 @@ const CGFloat kScrollWindowVerticalMargin = 0.0;
 
 - (void)forceButtonBorderToStayOnAlways:(BOOL)forceOn {
   [self setShowsBorderOnlyWhileMouseInside:!forceOn];
+  [self setNeedsDisplay];
 }
 
 - (void)toggleButtonBorderingWhileMouseInside {
@@ -536,6 +537,9 @@ const CGFloat kScrollWindowVerticalMargin = 0.0;
   frame.size.height += bookmarks::kBookmarkBarHeight;
   frame.size.width = windowWidth;
   [window setFrame:frame display:YES];
+#if 1
+  [self showOrHideScrollArrows];
+#endif
 }
 
 - (void)moveButtonFromIndex:(NSInteger)fromIndex toIndex:(NSInteger)toIndex {
