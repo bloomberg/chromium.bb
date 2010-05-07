1fcb02182bc8eed5d837bd54e0142a45a7f81c7f
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-07 18:30:01 +0000

    Use a reasonable default pthread stack size on Mac OS X.
    Mac OS X uses a fixed default stack size of 512kB, which is too small for some
    purposes. glibc on Linux uses ulimit -s, which is both a more reasonable thing
    to do, and is generally set to a rational limit, such as 8MB.
    
    BUG=43422
    TEST=None really on the trunk. On the Mac 375 branch, using the .pac file on
         the bug, Chrome should not crash on startup.
    Review URL: http://codereview.chromium.org/2035007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@46717 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 831d29c2dfd23457aa54b9a4f73f030827500a65

--- base/platform_thread_posix.cc
+++ base/platform_thread_posix.cc
@@ -9,6 +9,8 @@
 
 #if defined(OS_MACOSX)
 #include <mach/mach.h>
+#include <sys/resource.h>
+#include <algorithm>
 #else
 #include <sys/syscall.h>
 #include <unistd.h>
@@ -89,6 +91,37 @@ bool CreateThread(size_t stack_size, bool joinable,
   if (!joinable) {
     pthread_attr_setdetachstate(&attributes, PTHREAD_CREATE_DETACHED);
   }
+
+#if defined(OS_MACOSX)
+  // The Mac OS X default for a pthread stack size is 512kB.
+  // Libc-594.1.4/pthreads/pthread.c's pthread_attr_init uses
+  // DEFAULT_STACK_SIZE for this purpose.
+  //
+  // 512kB isn't quite generous enough for some deeply recursive threads that
+  // otherwise request the default stack size by specifying 0. Here, adopt
+  // glibc's behavior as on Linux, which is to use the current stack size
+  // limit (ulimit -s) as the default stack size. See
+  // glibc-2.11.1/nptl/nptl-init.c's __pthread_initialize_minimal_internal. To
+  // avoid setting the limit below the Mac OS X default or the minimum usable
+  // stack size, these values are also considered. If any of these values
+  // can't be determined, or if stack size is unlimited (ulimit -s unlimited),
+  // stack_size is left at 0 to get the system default.
+  //
+  // Mac OS X normally only applies ulimit -s to the main thread stack. On
+  // contemporary OS X and Linux systems alike, this value is generally 8MB
+  // or in that neighborhood.
+  if (stack_size == 0) {
+    size_t default_stack_size;
+    struct rlimit stack_rlimit;
+    if (pthread_attr_getstacksize(&attributes, &default_stack_size) == 0 &&
+        getrlimit(RLIMIT_STACK, &stack_rlimit) == 0 &&
+        stack_rlimit.rlim_cur != RLIM_INFINITY) {
+      stack_size = std::max(std::max(default_stack_size,
+                                     static_cast<size_t>(PTHREAD_STACK_MIN)),
+                            static_cast<size_t>(stack_rlimit.rlim_cur));
+    }
+  }
+#endif  // OS_MACOSX
 
   if (stack_size > 0)
     pthread_attr_setstacksize(&attributes, stack_size);
