4977ba454dc4c328a0e679c9067a341e8f8bec6f
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-12 15:31:55 +0000

    Allow the gallery url passed through the command line to be referrer for actual gallery downloads.
    BUG=None
    TEST=Pass in a gallery url through the command line and make sure the dangerous download warning does not show when downloading from that gallery.
    
    Review URL: http://codereview.chromium.org/1997019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47033 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 473ff6e45d365bbf341efef2e36afbc39d26652a

--- chrome/browser/extensions/extensions_service.cc
+++ chrome/browser/extensions/extensions_service.cc
@@ -135,6 +135,18 @@ bool ExtensionsService::IsDownloadFromGallery(const GURL& download_url,
     return true;
   }
 
+  // Allow command line gallery url to be referrer for the gallery downloads.
+  std::string command_line_gallery_url =
+      CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+          switches::kAppLauncherGalleryURL);
+  if (!command_line_gallery_url.empty() &&
+      StartsWithASCII(download_url.spec(),
+                      extension_urls::kGalleryDownloadPrefix, false) &&
+      StartsWithASCII(referrer_url.spec(),
+                      command_line_gallery_url, false)) {
+    return true;
+  }
+
   return false;
 }
 
