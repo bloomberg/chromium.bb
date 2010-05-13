7f0a2fb92fd6f09708b40701fec2f48ada89a114
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-13 02:32:06 +0000

    Fix a ChromeFrame regression introduced by the fix for bug  http://code.google.com/p/chromium/issues/detail?id=43922. The active document url was not being updated when we receive the navigation state from chrome. This caused a couple of chrome frame tests to fail.
    Bug=43922
    TBR=amit
    
    Review URL: http://codereview.chromium.org/2052011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47113 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 38939def12bda00e73d96fcbb6f8b90f05bc5e24

--- chrome_frame/chrome_active_document.cc
+++ chrome_frame/chrome_active_document.cc
@@ -685,6 +685,10 @@ void ChromeActiveDocument::UpdateNavigationState(
        StartsWith(static_cast<BSTR>(url_), kChromeAttachExternalTabPrefix,
                   false);
 
+  if (new_navigation_info.url.is_valid()) {
+    url_.Allocate(UTF8ToWide(new_navigation_info.url.spec()).c_str());
+  }
+
   if (is_internal_navigation) {
     ScopedComPtr<IDocObjectService> doc_object_svc;
     ScopedComPtr<IWebBrowserEventsService> web_browser_events_svc;
