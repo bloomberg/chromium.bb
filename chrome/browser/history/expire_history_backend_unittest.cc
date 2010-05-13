1ba30434904578db69223092b3c8176817f9c4fd
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-13 06:13:02 +0000

    Flip ExpireHistoryTest.DeleteURLAndFavicon from FAILS back to FLAKY per James Hawkins' request from http://codereview.chromium.org/2055014/show
    The problem is that this test does not appear on the flakiness dashboard,
    but hopefully the new flakiness bot will help with that.
    
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/2012012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47123 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1964d12e176788db130c82ca2e312e04dc97a6fa

--- chrome/browser/history/expire_history_backend_unittest.cc
+++ chrome/browser/history/expire_history_backend_unittest.cc
@@ -382,7 +382,7 @@ bool ExpireHistoryTest::IsStringInFile(const FilePath& filename,
 // Deletes a URL with a favicon that it is the last referencer of, so that it
 // should also get deleted.
 // Fails near end of month. http://crbug.com/43586
-TEST_F(ExpireHistoryTest, FAILS_DeleteURLAndFavicon) {
+TEST_F(ExpireHistoryTest, FLAKY_DeleteURLAndFavicon) {
   URLID url_ids[3];
   Time visit_times[4];
   AddExampleData(url_ids, visit_times);
