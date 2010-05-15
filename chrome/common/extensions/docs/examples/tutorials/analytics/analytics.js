f41678d017b60b7b605e148f435313503a028d4e
kurrik@chromium.org <kurrik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-15 01:42:11 +0000

    Analytics tutorial updates Updated analytics tutorial with new tracking code as well as more information about debugging requests. TEST=None BUG=None
    Review URL: http://codereview.chromium.org/1880001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47353 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d797e5a0f114ebbd0b7962c09678fb2b7219ad1e

--- chrome/common/extensions/docs/examples/tutorials/analytics/analytics.js
+++ chrome/common/extensions/docs/examples/tutorials/analytics/analytics.js
@@ -28,5 +28,5 @@ _gaq.push(['_trackPageview']);
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = 'https://ssl.google-analytics.com/ga.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
