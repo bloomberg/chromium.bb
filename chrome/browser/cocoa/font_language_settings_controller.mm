134a8445ba326e83832c62318c5321395fd4a058
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-18 18:06:59 +0000

    [Mac] Make the size text field in the font settings editable.
    BUG=33406
    TEST=Chromium-->Preferences-->Under the Hood-->Change font settings-->Change. Type into the size field. The font size changes.
    
    Review URL: http://codereview.chromium.org/2136014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@47529 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 68f2eac80f154637c907c30b0b710b9603d25549

--- chrome/browser/cocoa/font_language_settings_controller.mm
+++ chrome/browser/cocoa/font_language_settings_controller.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -156,6 +156,7 @@ void ShowFontsLanguagesWindow(gfx::NativeWindow window,
   // Validate whatever editing is currently happening.
   if ([[self window] makeFirstResponder:nil]) {
     NSFontManager* manager = [NSFontManager sharedFontManager];
+    [manager setTarget:self];
     [manager setSelectedFont:currentFont_ isMultiple:NO];
     [manager orderFrontFontPanel:self];
   }
