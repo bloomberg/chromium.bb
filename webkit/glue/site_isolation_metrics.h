52a5ace295d2e5b285c31e81e0ae79cd0a1c2b9c
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-23 17:37:54 +0000

    Update the Site Isolation UMA metrics.
    Fixes a bug with tracking CORS headers and starts tracking all requests
    and all cross-site requests, for calculating the fraction we would block.
    Also explicitly counts the requests we would block in new UMA histograms.
    
    Currently has a problem with "304 Not Modified" responses, since no MIME
    type is provided...
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/2132002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48003 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f4b8f31dde49067a092303025d4baaf47e93f491

--- webkit/glue/site_isolation_metrics.h
+++ webkit/glue/site_isolation_metrics.h
@@ -15,7 +15,7 @@ class WebURLResponse;
 
 namespace webkit_glue {
 
-// Metrics to check the feasability of blocking cross-site requests
+// Metrics to check the feasability of blocking cross-site requests that
 // a renderer shouldn't be making (in case we try to move cross-site frames
 // into their own process someday). We're erring on the side of counting more
 // mime-types then we strictly need (we'd only consider blocking cross-site
@@ -24,6 +24,8 @@ class SiteIsolationMetrics {
  public:
   static void AddRequest(unsigned identifier,
                          WebKit::WebURLRequest::TargetType target_type);
+  static bool AllowedByAccessControlHeader(
+      WebKit::WebFrame* frame, const WebKit::WebURLResponse& response);
   static void LogMimeTypeForCrossOriginRequest(
       WebKit::WebFrame* frame,
       unsigned identifier,
