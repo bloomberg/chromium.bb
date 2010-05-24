d9bfee78271b59137911bbf444111670c60f1c49
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-24 12:21:52 +0000

    Set PIN_COMMAND env to the TSan/Win runner path. This is similar to the way Valgrind path is guessed in locate_valgrind.sh TBR=glider Review URL: http://codereview.chromium.org/2150002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48033 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 34ec463f418f3f12e9fe57000ace2a27f06c829e

--- tools/valgrind/chrome_tests.bat
+++ tools/valgrind/chrome_tests.bat
@@ -3,5 +3,17 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
+set TSAN_PATH=%~dp0..\..\third_party\tsan
+set TSAN_SFX=%TSAN_PATH%\tsan-x86-windows-sfx.exe
+if EXIST %TSAN_SFX% GOTO BINARY_OK
+echo "Can't find ThreadSanitizer executables."
+echo "See http://www.chromium.org/developers/how-tos/using-valgrind/threadsanitizer/threadsanitizer-on-windows"
+echo "for the instructions on how to get them."
+set %ERRORLEVEL% 1
+goto :EOF
+
+:BINARY_OK
+%TSAN_SFX% -o%TSAN_PATH%\unpacked -y
+set PIN_COMMAND=%TSAN_PATH%\unpacked\tsan-x86-windows\tsan.bat
 set PYTHONPATH=%~dp0../python/google
 python %~dp0/chrome_tests.py %*
