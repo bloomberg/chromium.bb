c84b06e51f8703cbc6ac2b840aa0018cae8d243e
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-24 04:40:53 +0000

    Make out-of-process tests output correct XML file (filepath fixed)
    Original issue was: http://codereview.chromium.org/2071001/show (reverted due to build break)
    
    - Disable XML output for children
    - Output XML file with failed, disabled and timing information in the
      parent launcher process.
    
    BUG=40473,42781
    TEST=run the browser_tests (or other tests that uses out-of-proc-test runner) with 1) --gtest_output=xml 2) --gtest_output=xml:test.xml 3) --gtest_output=xml:/tmp/ and see if the output XML file (test_detail.xml for 1), test.xml for 2), /tmp/browser_tests.xml for 3)) contains correct information.
    
    Review URL: http://codereview.chromium.org/2071001
    
    
    Review URL: http://codereview.chromium.org/2067022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48015 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 703f8d749f9cb925a83a531eda5f2a1fd897b230

--- chrome/test/test_launcher/out_of_proc_test_runner.cc
+++ chrome/test/test_launcher/out_of_proc_test_runner.cc
@@ -16,6 +16,7 @@
 namespace {
 
 const char kGTestListTestsFlag[] = "gtest_list_tests";
+const char kGTestOutputFlag[] = "gtest_output";
 const char kGTestHelpFlag[]   = "gtest_help";
 const char kSingleProcessTestsFlag[]   = "single_process";
 const char kSingleProcessTestsAndChromeFlag[]   = "single-process";
@@ -42,12 +43,17 @@ class OutOfProcTestRunner : public tests::TestRunner {
   // Returns true if the test succeeded, false if it failed.
   bool RunTest(const std::string& test_name) {
     const CommandLine* cmd_line = CommandLine::ForCurrentProcess();
-#if defined(OS_WIN)
-    CommandLine new_cmd_line =
-        CommandLine::FromString(cmd_line->command_line_string());
-#else
-    CommandLine new_cmd_line(cmd_line->argv());
-#endif
+    // Construct the new command line.  Strip out gtest_output flag if
+    // it has been given because otherwise each test outputs the same file
+    // over and over overriding the previous one every time.
+    // We will generate the final output file later in RunTests().
+    CommandLine new_cmd_line(cmd_line->GetProgram());
+    CommandLine::SwitchMap switches = cmd_line->GetSwitches();
+    switches.erase(kGTestOutputFlag);
+    for (CommandLine::SwitchMap::const_iterator iter = switches.begin();
+         iter != switches.end(); ++iter) {
+      new_cmd_line.AppendSwitchWithValue((*iter).first, (*iter).second);
+    }
 
     // Always enable disabled tests.  This method is not called with disabled
     // tests unless this flag was specified to the browser test executable.
