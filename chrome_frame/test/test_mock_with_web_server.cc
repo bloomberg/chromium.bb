e29cd3880d7131c14184b0ca61acbaf626dd5002
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-25 23:03:07 +0000

    Enabling the ChromeFrame unload event test which validates that unload handlers on the page are invoked correctly. To flag success or failure we now set the state in the localStorage object instead of setting this via cookies as the cookies are set in IE on the UI thread causing this test to fail.
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=40814
    
    Bug=40814
    
    Review URL: http://codereview.chromium.org/2215001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48218 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bb2c9a816bfa5709c5b43624f24713bc1a4c3d19

--- chrome_frame/test/test_mock_with_web_server.cc
+++ chrome_frame/test/test_mock_with_web_server.cc
@@ -804,8 +804,7 @@ const wchar_t kBeforeUnloadTest[] =
 const wchar_t kBeforeUnloadMain[] =
     L"http://localhost:1337/files/fulltab_before_unload_event_main.html";
 
-// http://code.google.com/p/chromium/issues/detail?id=40814
-TEST_F(ChromeFrameTestWithWebServer, DISABLED_FullTabModeIE_UnloadEventTest) {
+TEST_F(ChromeFrameTestWithWebServer, FullTabModeIE_UnloadEventTest) {
   CloseIeAtEndOfScope last_resort_close_ie;
   ComStackObjectWithUninitialize<MockWebBrowserEventSink> mock;
   chrome_frame_test::TimedMsgLoop loop;
