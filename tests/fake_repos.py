3eac705726cb7253781b2abc5e0960b942f23800
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-26 01:56:14 +0000

    Add real gclient_smoke tests now
    gclient sync, revert and status are tested.
    
    GClientSmokeGIT.testRevertAndStatus is known to fail since git-backed revert doesn't work.
    
    Review URL: http://codereview.chromium.org/2189001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@48239 0039d316-1c4b-4281-b951-d872f2087c98

--- tests/fake_repos.py
+++ tests/fake_repos.py
@@ -5,7 +5,9 @@
 
 """Generate fake repositories for testing."""
 
+import logging
 import os
+import re
 import shutil
 import subprocess
 import sys
@@ -39,10 +41,73 @@ def write(path, content):
   f.close()
 
 
+join = os.path.join
+
+
+def call(*args, **kwargs):
+  logging.debug(args[0])
+  subprocess.call(*args, **kwargs)
+
+
+def check_call(*args, **kwargs):
+  logging.debug(args[0])
+  subprocess.check_call(*args, **kwargs)
+
+
+def Popen(*args, **kwargs):
+  kwargs.setdefault('stdout', subprocess.PIPE)
+  kwargs.setdefault('stderr', subprocess.STDOUT)
+  logging.debug(args[0])
+  return subprocess.Popen(*args, **kwargs)
+
+
+def commit_svn(repo):
+  """Commits the changes and returns the new revision number."""
+  # Basic parsing.
+  to_add = []
+  to_remove = []
+  for item in Popen(['svn', 'status'],
+                    cwd=repo).communicate()[0].splitlines(False):
+    if item[0] == '?':
+      to_add.append(item[8:])
+    elif item[0] == '!':
+      to_remove.append(item[8:])
+  if to_add:
+    check_call(['svn', 'add', '--no-auto-props', '-q'] + to_add, cwd=repo)
+  if to_remove:
+    check_call(['svn', 'remove', '-q'] + to_remove, cwd=repo)
+  out = Popen(['svn', 'commit', repo, '-m', 'foo', '--non-interactive',
+                '--no-auth-cache', '--username', 'user1', '--password', 'foo'],
+              cwd=repo).communicate()[0]
+  rev = re.search(r'revision (\d+).', out).group(1)
+  st = Popen(['svn', 'status'], cwd=repo).communicate()[0]
+  assert len(st) == 0, st
+  logging.debug('At revision %s' % rev)
+  return rev
+
+
+def commit_git(repo):
+  """Commits the changes and returns the new hash."""
+  check_call(['git', 'add', '-A', '-f'], cwd=repo)
+  out = Popen(['git', 'commit', '-m', 'foo'], cwd=repo).communicate()[0]
+  rev = re.search(r'^\[.*? ([a-f\d]+)\] ', out).group(1)
+  logging.debug('At revision %s' % rev)
+  return rev
+
+
 class FakeRepos(object):
+  """Generate both svn and git repositories to test gclient functionality.
+
+  Many DEPS functionalities need to be tested: Var, File, From, deps_os, hooks,
+  use_relative_paths.
+
+  And types of dependencies: Relative urls, Full urls, both svn and git."""
+
   def __init__(self, trial_dir, leak, local_only):
     self.trial_dir = trial_dir
     self.repos_dir = os.path.join(self.trial_dir, 'repos')
+    self.git_root = join(self.repos_dir, 'git')
+    self.svn_root = join(self.repos_dir, 'svn_checkout')
     self.leak = leak
     self.local_only = local_only
     self.svnserve = []
@@ -51,6 +116,10 @@ class FakeRepos(object):
     rmtree(self.trial_dir)
     os.mkdir(self.trial_dir)
     os.mkdir(self.repos_dir)
+    # Format is [ None, tree, tree, ...]
+    self.svn_revs = [None]
+    # Format is { repo: [ (hash, tree), (hash, tree), ... ], ... }
+    self.git_hashes = {}
 
   def setUp(self):
     self.setUpSVN()
@@ -58,19 +127,37 @@ class FakeRepos(object):
 
   def tearDown(self):
     for i in self.svnserve:
+      logging.debug('Killing svnserve pid %s' % i.pid)
       i.kill()
     for i in self.gitdaemon:
+      logging.debug('Killing git-daemon pid %s' % i.pid)
       i.kill()
     if not self.leak:
+      logging.debug('Removing %s' % self.trial_dir)
       rmtree(self.trial_dir)
+
+  def _genTree(self, root, tree_dict):
+    """For a dictionary of file contents, generate a filesystem."""
+    if not os.path.isdir(root):
+      os.makedirs(root)
+    for (k, v) in tree_dict.iteritems():
+      k_os = k.replace('/', os.sep)
+      k_arr = k_os.split(os.sep)
+      if len(k_arr) > 1:
+        p = os.sep.join([root] + k_arr[:-1])
+        if not os.path.isdir(p):
+          os.makedirs(p)
+      if v is None:
+        os.remove(join(root, k))
+      else:
+        write(join(root, k), v)
 
   def setUpSVN(self):
     """Creates subversion repositories and start the servers."""
     assert not self.svnserve
-    join = os.path.join
     root = join(self.repos_dir, 'svn')
     rmtree(root)
-    subprocess.check_call(['svnadmin', 'create', root])
+    check_call(['svnadmin', 'create', root])
     write(join(root, 'conf', 'svnserve.conf'),
         '[general]\n'
         'anon-access = read\n'
@@ -81,123 +168,183 @@ class FakeRepos(object):
         'user1 = foo\n'
         'user2 = bar\n')
 
-    # Repos
-    repo = join(self.repos_dir, 'svn_import')
-    rmtree(repo)
-    os.mkdir(repo)
-    os.mkdir(join(repo, 'trunk'))
-    os.mkdir(join(repo, 'trunk', 'src'))
-    write(join(repo, 'trunk', 'src', 'DEPS'), """
-# Smoke test DEPS file.
-# Many DEPS functionalities need to be tested:
-#  Var
-#  File
-#  From
-#  deps_os
-#  hooks
-#  use_relative_paths
-#
-# Types of dependencies:
-#  Relative urls
-#  Full urls
-#  svn
-#  git
-
-deps = {
- 'src/other': 'svn://%(host)s/svn/trunk/other',
- 'src/third_party': '/trunk/third_party',
-}
-
-deps_os = {
- 'mac': 'repo_4'
-}
-""" % {
-    'host': 'localhost',
-})
-    write(join(repo, 'trunk', 'src', 'origin'), "svn/trunk/src")
-    os.mkdir(join(repo, 'trunk', 'other'))
-    write(join(repo, 'trunk', 'other', 'origin'), "svn/trunk/other")
-    os.mkdir(join(repo, 'trunk', 'third_party'))
-    write(join(repo, 'trunk', 'third_party', 'origin'), "svn/trunk/third_party")
-
     # Start the daemon.
     cmd = ['svnserve', '-d', '--foreground', '-r', self.repos_dir]
     if self.local_only:
       cmd.append('--listen-host=127.0.0.1')
-    self.svnserve.append(subprocess.Popen(cmd, cwd=root))
+    logging.debug(cmd)
+    self.svnserve.append(Popen(cmd, cwd=root))
+    self.populateSvn()
 
-    # Import the repo.
-    subprocess.check_call(['svn', 'import', repo,
-      'svn://127.0.0.1/svn', '-m', 'foo', '-q',
-      '--no-auto-props', '--non-interactive', '--no-auth-cache',
-      '--username', 'user1', '--password', 'foo'])
+  def populateSvn(self):
+    """Creates a few revisions of changes including DEPS files."""
+    # Repos
+    check_call(['svn', 'checkout', 'svn://127.0.0.1/svn', self.svn_root, '-q',
+                '--non-interactive', '--no-auth-cache',
+                '--username', 'user1', '--password', 'foo'])
+    assert os.path.isdir(join(self.svn_root, '.svn'))
+    def file_system(rev, DEPS):
+      fs = {
+        'origin': 'svn@%(rev)d\n',
+        'trunk/origin': 'svn/trunk@%(rev)d\n',
+        'trunk/src/origin': 'svn/trunk/src@%(rev)d\n',
+        'trunk/src/third_party/origin': 'svn/trunk/src/third_party@%(rev)d\n',
+        'trunk/other/origin': 'src/trunk/other@%(rev)d\n',
+        'trunk/third_party/origin': 'svn/trunk/third_party@%(rev)d\n',
+        'trunk/third_party/foo/origin': 'svn/trunk/third_party/foo@%(rev)d\n',
+        'trunk/third_party/prout/origin': 'svn/trunk/third_party/foo@%(rev)d\n',
+      }
+      for k in fs.iterkeys():
+        fs[k] = fs[k] % { 'rev': rev }
+      fs['trunk/src/DEPS'] = DEPS
+      return fs
+
+    # Testing:
+    # - dependency disapear
+    # - dependency renamed
+    # - versioned and unversioned reference
+    # - relative and full reference
+    # - deps_os
+    # TODO(maruel):
+    # - var
+    # - hooks
+    # - File
+    self._commit_svn(file_system(1, """
+deps = {
+  'src/other': 'svn://%(host)s/svn/trunk/other',
+  'src/third_party/fpp': '/trunk/third_party/foo',
+}
+deps_os = {
+  'mac': {
+    'src/third_party/prout': '/trunk/third_party/prout',
+  },
+}""" % { 'host': '127.0.0.1' }))
+
+    self._commit_svn(file_system(2, """
+deps = {
+  'src/other': 'svn://%(host)s/svn/trunk/other',
+  'src/third_party/foo': '/trunk/third_party/foo@1',
+}
+""" % { 'host': '127.0.0.1' }))
 
   def setUpGIT(self):
     """Creates git repositories and start the servers."""
     assert not self.gitdaemon
-    join = os.path.join
-    root = join(self.repos_dir, 'git')
-    rmtree(root)
-    os.mkdir(root)
-    # Repo 1
-    repo = join(root, 'repo_1')
-    subprocess.check_call(['git', 'init', '-q', repo])
-    write(join(repo, 'DEPS'), """
-# Smoke test DEPS file.
-# Many DEPS functionalities need to be tested:
-#  Var
-#  File
-#  From
-#  deps_os
-#  hooks
-#  use_relative_paths
-#
-# Types of dependencies:
-#  Relative urls
-#  Full urls
-#  svn
-#  git
+    rmtree(self.git_root)
+    for repo in ['repo_%d' % r for r in range(1, 5)]:
+      check_call(['git', 'init', '-q', join(self.git_root, repo)])
+      self.git_hashes[repo] = []
 
+    # Testing:
+    # - dependency disapear
+    # - dependency renamed
+    # - versioned and unversioned reference
+    # - relative and full reference
+    # - deps_os
+    # TODO(maruel):
+    # - var
+    # - hooks
+    # - File
+    self._commit_git('repo_1', {
+      'DEPS': """
 deps = {
- 'repo2': 'git://%(host)s/git/repo_2',
- 'repo2/repo3': '/repo_3',
+  'src/repo2': 'git://%(host)s/git/repo_2',
+  'src/repo2/repo3': '/repo_3',
 }
-
 deps_os = {
- 'mac': 'repo_4'
+  'mac': {
+    'src/repo4': '/repo_4',
+  },
+}""" % { 'host': '127.0.0.1' },
+      'origin': 'git/repo_1@1\n',
+    })
+
+    self._commit_git('repo_2', {
+      'origin': "git/repo_2@1\n"
+    })
+
+    self._commit_git('repo_2', {
+      'origin': "git/repo_2@2\n"
+    })
+
+    self._commit_git('repo_3', {
+      'origin': "git/repo_3@1\n"
+    })
+
+    self._commit_git('repo_3', {
+      'origin': "git/repo_3@2\n"
+    })
+
+    self._commit_git('repo_4', {
+      'origin': "git/repo_4@1\n"
+    })
+
+    self._commit_git('repo_4', {
+      'origin': "git/repo_4@2\n"
+    })
+
+    self._commit_git('repo_1', {
+      'DEPS': """
+deps = {
+  'src/repo2': 'git://%(host)s/git/repo_2@%(hash)s',
+  'src/repo2/repo_renamed': '/repo_3',
 }
-""" % {
-    'host': 'localhost',
-})
-    write(join(repo, 'origin'), "git/repo_1")
-    subprocess.check_call(['git', 'add', '-A', '-f'], cwd=repo)
-    subprocess.check_call(['git', 'commit', '-q', '-m', 'foo'], cwd=repo)
-
-    # Repo 2
-    repo = join(root, 'repo_2')
-    subprocess.check_call(['git', 'init', '-q', repo])
-    write(join(repo, 'origin'), "git/repo_2")
-    subprocess.check_call(['git', 'add', '-A', '-f'], cwd=repo)
-    subprocess.check_call(['git', 'commit', '-q', '-m', 'foo'], cwd=repo)
-
-    # Repo 3
-    repo = join(root, 'repo_3')
-    subprocess.check_call(['git', 'init', '-q', repo])
-    write(join(repo, 'origin'), "git/repo_3")
-    subprocess.check_call(['git', 'add', '-A', '-f'], cwd=repo)
-    subprocess.check_call(['git', 'commit', '-q', '-m', 'foo'], cwd=repo)
+""" % { 'host': '127.0.0.1', 'hash': self.git_hashes['repo_2'][0][0] },
+      'origin': "git/repo_1@2\n"
+    })
 
     # Start the daemon.
     cmd = ['git', 'daemon', '--export-all', '--base-path=' + self.repos_dir]
     if self.local_only:
       cmd.append('--listen=127.0.0.1')
-    self.gitdaemon.append(subprocess.Popen(cmd, cwd=self.repos_dir,
-                                           stderr=subprocess.PIPE))
+    logging.debug(cmd)
+    self.gitdaemon.append(Popen(cmd, cwd=self.repos_dir))
 
-if __name__ == '__main__':
-  fake = FakeRepos(os.path.dirname(os.path.abspath(__file__)), False)
+  def _commit_svn(self, tree):
+    self._genTree(self.svn_root, tree)
+    commit_svn(self.svn_root)
+    if self.svn_revs and self.svn_revs[-1]:
+      new_tree = self.svn_revs[-1].copy()
+      new_tree.update(tree)
+    else:
+      new_tree = tree.copy()
+    self.svn_revs.append(new_tree)
+
+  def _commit_git(self, repo, tree):
+    repo_root = join(self.git_root, repo)
+    self._genTree(repo_root, tree)
+    hash = commit_git(repo_root)
+    if self.git_hashes[repo]:
+      new_tree = self.git_hashes[repo][-1][1].copy()
+      new_tree.update(tree)
+    else:
+      new_tree = tree.copy()
+    self.git_hashes[repo].append((hash, new_tree))
+
+
+def main(argv):
+  leak = '-l' in argv
+  if leak:
+    argv.remove('-l')
+  verbose = '-v' in argv
+  if verbose:
+    logging.basicConfig(level=logging.DEBUG)
+    argv.remove('-v')
+  assert len(argv) == 1, argv
+  trial_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                           '_trial')
+  print 'Using %s' % trial_dir
+  fake = FakeRepos(trial_dir, leak, True)
   try:
     fake.setUp()
+    print('Fake setup, press enter to quit or Ctrl-C to keep the checkouts.')
     sys.stdin.readline()
+  except KeyboardInterrupt:
+    fake.leak = True
   finally:
     fake.tearDown()
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv))
--- tests/gclient_smoketest.py
+++ tests/gclient_smoketest.py
@@ -10,27 +10,74 @@ Shell out 'gclient' and run basic conformance tests.
 This test assumes GClientSmokeBase.URL_BASE is valid.
 """
 
+import logging
 import os
+import pprint
 import shutil
 import subprocess
 import sys
 import unittest
 
-from fake_repos import rmtree, FakeRepos
+from fake_repos import rmtree, write, FakeRepos
+
+join = os.path.join
 
 SHOULD_LEAK = False
 UNITTEST_DIR = os.path.abspath(os.path.dirname(__file__))
-GCLIENT_PATH = os.path.join(os.path.dirname(UNITTEST_DIR), 'gclient')
+GCLIENT_PATH = join(os.path.dirname(UNITTEST_DIR), 'gclient')
 # all tests outputs goes there.
-TRIAL_DIR = os.path.join(UNITTEST_DIR, '_trial')
+TRIAL_DIR = join(UNITTEST_DIR, '_trial')
 # In case you want to use another machine to create the fake repos, e.g.
 # not on Windows.
 HOST = '127.0.0.1'
+FAKE = None
+
+
+def read_tree(tree_root):
+  """Returns a dict of all the files in a tree."""
+  tree = {}
+  for root, dirs, files in os.walk(tree_root):
+    for d in filter(lambda x: x.startswith('.'), dirs):
+      dirs.remove(d)
+    for f in [join(root, f) for f in files if not f.startswith('.')]:
+      tree[f[len(tree_root) + 1:]] = open(join(root, f), 'rb').read()
+  return tree
+
+
+def dict_diff(dict1, dict2):
+  diff = {}
+  for k, v in dict1.iteritems():
+    if k not in dict2:
+      diff[k] = v
+    elif v != dict2[k]:
+      diff[k] = (v, dict2[k])
+  for k, v in dict2.iteritems():
+    if k not in dict1:
+      diff[k] = v
+  return diff
+
+
+def mangle_svn_tree(*args):
+  result = {}
+  for old_root, new_root, tree in args:
+    for k, v in tree.iteritems():
+      if not k.startswith(old_root):
+        continue
+      result[join(new_root, k[len(old_root) + 1:])] = v
+  return result
+
+
+def mangle_git_tree(*args):
+  result = {}
+  for new_root, tree in args:
+    for k, v in tree.iteritems():
+      result[join(new_root, k)] = v
+  return result
 
 
 class GClientSmokeBase(unittest.TestCase):
   # This subversion repository contains a test repository.
-  ROOT_DIR = os.path.join(TRIAL_DIR, 'smoke')
+  ROOT_DIR = join(TRIAL_DIR, 'smoke')
 
   def setUp(self):
     # Vaguely inspired by twisted.
@@ -38,7 +85,7 @@ class GClientSmokeBase(unittest.TestCase):
     self.env = os.environ.copy()
     self.env['DEPOT_TOOLS_UPDATE'] = '0'
     # Remove left overs
-    self.root_dir = os.path.join(self.ROOT_DIR, self.id())
+    self.root_dir = join(self.ROOT_DIR, self.id())
     rmtree(self.root_dir)
     if not os.path.exists(self.ROOT_DIR):
       os.mkdir(self.ROOT_DIR)
@@ -70,6 +117,15 @@ class GClientSmokeBase(unittest.TestCase):
     checkString(expected[1], results[1])
     self.assertEquals(expected[2], results[2])
 
+  def assertTree(self, tree):
+    actual = read_tree(self.root_dir)
+    diff = dict_diff(tree, actual)
+    if diff:
+      logging.debug('Actual %s\n%s' % (self.root_dir, pprint.pformat(actual)))
+      logging.debug('Expected\n%s' % pprint.pformat(tree))
+      logging.debug('Diff\n%s' % pprint.pformat(diff))
+    self.assertEquals(tree, actual)
+
 
 class GClientSmoke(GClientSmokeBase):
   def testCommands(self):
@@ -80,7 +136,7 @@ class GClientSmoke(GClientSmokeBase):
     self.assertEquals(0, result[2])
 
   def testNotConfigured(self):
-    res = ("", "Error: client not configured; see 'gclient config'\n", 1)
+    res = ('', 'Error: client not configured; see \'gclient config\'\n', 1)
     self.check(res, self.gclient(['cleanup']))
     self.check(res, self.gclient(['diff']))
     self.check(res, self.gclient(['export', 'foo']))
@@ -93,30 +149,169 @@ class GClientSmoke(GClientSmokeBase):
     self.check(res, self.gclient(['update']))
 
 
-class GClientSmokeSync(GClientSmokeBase):
+class GClientSmokeSVN(GClientSmokeBase):
   """sync is the most important command. Hence test it more."""
-  def testSyncSvn(self):
+  def testSync(self):
     """Test pure gclient svn checkout, example of Chromium checkout"""
     self.gclient(['config', self.svn_base + 'trunk/src/'])
-    results = self.gclient(['sync'])
+    # Test unversioned checkout.
+    results = self.gclient(['sync', '--deps', 'mac'])
+    logging.debug(results[0])
+    self.assertEquals('', results[1])
     self.assertEquals(0, results[2])
-    results = self.gclient(['sync', '--revision', 'a@32'])
+    tree = mangle_svn_tree(
+        (join('trunk', 'src'), 'src', FAKE.svn_revs[-1]),
+        (join('trunk', 'third_party', 'foo'), join('src', 'third_party', 'foo'),
+            FAKE.svn_revs[1]),
+        (join('trunk', 'other'), join('src', 'other'), FAKE.svn_revs[2]),
+        )
+    self.assertTree(tree)
+
+    # Test incremental versioned sync: sync backward.
+    results = self.gclient(['sync', '--revision', 'src@1', '--deps', 'mac',
+                            '--delete_unversioned_trees'])
+    logging.debug(results[0])
+    self.assertEquals('', results[1])
+    self.assertEquals(0, results[2])
+    tree = mangle_svn_tree(
+        (join('trunk', 'src'), 'src', FAKE.svn_revs[1]),
+        (join('trunk', 'third_party', 'foo'), join('src', 'third_party', 'fpp'),
+            FAKE.svn_revs[2]),
+        (join('trunk', 'other'), join('src', 'other'), FAKE.svn_revs[2]),
+        (join('trunk', 'third_party', 'foo'),
+            join('src', 'third_party', 'prout'),
+            FAKE.svn_revs[2]),
+        )
+    self.assertTree(tree)
+    # Test incremental sync: delete-unversioned_trees isn't there.
+    results = self.gclient(['sync', '--deps', 'mac'])
+    logging.debug(results[0])
+    self.assertEquals('', results[1])
+    self.assertEquals(0, results[2])
+    tree = mangle_svn_tree(
+        (join('trunk', 'src'), 'src', FAKE.svn_revs[-1]),
+        (join('trunk', 'third_party', 'foo'), join('src', 'third_party', 'fpp'),
+            FAKE.svn_revs[2]),
+        (join('trunk', 'third_party', 'foo'), join('src', 'third_party', 'foo'),
+            FAKE.svn_revs[1]),
+        (join('trunk', 'other'), join('src', 'other'), FAKE.svn_revs[2]),
+        (join('trunk', 'third_party', 'foo'),
+            join('src', 'third_party', 'prout'),
+            FAKE.svn_revs[2]),
+        )
+    self.assertTree(tree)
+
+  def testRevertAndStatus(self):
+    self.gclient(['config', self.svn_base + 'trunk/src/'])
+    results = self.gclient(['sync', '--deps', 'mac'])
+    write(join(self.root_dir, 'src', 'third_party', 'foo', 'hi'), 'Hey!')
+
+    results = self.gclient(['status'])
+    out = results[0].splitlines(False)
+    self.assertEquals(7, len(out))
+    self.assertEquals(out[0], '')
+    self.assertTrue(out[1].startswith('________ running \'svn status\' in \''))
+    self.assertEquals(out[2], '?       other')
+    self.assertEquals(out[3], '?       third_party/foo')
+    self.assertEquals(out[4], '')
+    self.assertTrue(out[5].startswith('________ running \'svn status\' in \''))
+    self.assertEquals(out[6], '?       hi')
+    self.assertEquals('', results[1])
     self.assertEquals(0, results[2])
 
+    results = self.gclient(['revert'])
+    self.assertEquals('', results[1])
+    self.assertEquals(0, results[2])
+    tree = mangle_svn_tree(
+        (join('trunk', 'src'), 'src', FAKE.svn_revs[-1]),
+        (join('trunk', 'third_party', 'foo'), join('src', 'third_party', 'foo'),
+            FAKE.svn_revs[1]),
+        (join('trunk', 'other'), join('src', 'other'), FAKE.svn_revs[2]),
+        )
+    self.assertTree(tree)
+
+    results = self.gclient(['status'])
+    out = results[0].splitlines(False)
+    self.assertEquals(4, len(out))
+    self.assertEquals(out[0], '')
+    self.assertTrue(out[1].startswith('________ running \'svn status\' in \''))
+    self.assertEquals(out[2], '?       other')
+    self.assertEquals(out[3], '?       third_party/foo')
+    self.assertEquals('', results[1])
+    self.assertEquals(0, results[2])
+
+
+class GClientSmokeGIT(GClientSmokeBase):
   def testSyncGit(self):
     """Test pure gclient git checkout, example of Chromium OS checkout"""
-    self.gclient(['config', self.git_base + 'repo_1'])
-    results = self.gclient(['sync'])
-    print results[0]
-    print results[1]
+    self.gclient(['config', self.git_base + 'repo_1', '--name', 'src'])
+    # Test unversioned checkout.
+    results = self.gclient(['sync', '--deps', 'mac'])
+    logging.debug(results[0])
+    self.assertTrue(results[1].startswith('Switched to a new branch \''))
     self.assertEquals(0, results[2])
+    tree = mangle_git_tree(
+        ('src', FAKE.git_hashes['repo_1'][1][1]),
+        (join('src', 'repo2'), FAKE.git_hashes['repo_2'][0][1]),
+        (join('src', 'repo2', 'repo_renamed'), FAKE.git_hashes['repo_3'][1][1]),
+        )
+    self.assertTree(tree)
 
+    # Test incremental versioned sync: sync backward.
+    results = self.gclient(['sync', '--revision',
+                            'src@' + FAKE.git_hashes['repo_1'][0][0],
+                            '--deps', 'mac', '--delete_unversioned_trees'])
+    logging.debug(results[0])
+    self.assertEquals('', results[1])
+    self.assertEquals(0, results[2])
+    tree = mangle_git_tree(
+        ('src', FAKE.git_hashes['repo_1'][0][1]),
+        (join('src', 'repo2'), FAKE.git_hashes['repo_2'][1][1]),
+        (join('src', 'repo2', 'repo3'), FAKE.git_hashes['repo_3'][1][1]),
+        (join('src', 'repo4'), FAKE.git_hashes['repo_4'][1][1]),
+        )
+    self.assertTree(tree)
+    # Test incremental sync: delete-unversioned_trees isn't there.
+    results = self.gclient(['sync', '--deps', 'mac'])
+    logging.debug(results[0])
+    self.assertEquals('', results[1])
+    self.assertEquals(0, results[2])
+    tree = mangle_git_tree(
+        ('src', FAKE.git_hashes['repo_1'][1][1]),
+        (join('src', 'repo2'), FAKE.git_hashes['repo_2'][1][1]),
+        (join('src', 'repo2', 'repo3'), FAKE.git_hashes['repo_3'][1][1]),
+        (join('src', 'repo2', 'repo_renamed'), FAKE.git_hashes['repo_3'][1][1]),
+        (join('src', 'repo4'), FAKE.git_hashes['repo_4'][1][1]),
+        )
+    self.assertTree(tree)
 
-class GClientSmokeRevert(GClientSmokeBase):
-  """revert is the second most important command. Hence test it more."""
-  def setUp(self):
-    GClientSmokeBase.setUp(self)
-    self.gclient(['config', self.URL_BASE])
+  def testRevertAndStatus(self):
+    """TODO(maruel): Remove this line once this test is fixed."""
+    self.gclient(['config', self.git_base + 'repo_1', '--name', 'src'])
+    results = self.gclient(['sync', '--deps', 'mac'])
+    write(join(self.root_dir, 'src', 'repo2', 'hi'), 'Hey!')
+
+    results = self.gclient(['status'])
+    out = results[0].splitlines(False)
+    # TODO(maruel): THIS IS WRONG.
+    self.assertEquals(0, len(out))
+
+    results = self.gclient(['revert'])
+    self.assertEquals('', results[1])
+    self.assertEquals(0, results[2])
+    tree = mangle_git_tree(
+        ('src', FAKE.git_hashes['repo_1'][1][1]),
+        (join('src', 'repo2'), FAKE.git_hashes['repo_2'][0][1]),
+        (join('src', 'repo2', 'repo_renamed'), FAKE.git_hashes['repo_3'][1][1]),
+        )
+    # TODO(maruel): THIS IS WRONG.
+    tree[join('src', 'repo2', 'hi')] = 'Hey!'
+    self.assertTree(tree)
+
+    results = self.gclient(['status'])
+    out = results[0].splitlines(False)
+    # TODO(maruel): THIS IS WRONG.
+    self.assertEquals(0, len(out))
 
 
 class GClientSmokeRevInfo(GClientSmokeBase):
@@ -127,9 +322,14 @@ class GClientSmokeRevInfo(GClientSmokeBase):
 
 
 if __name__ == '__main__':
-  fake = FakeRepos(TRIAL_DIR, SHOULD_LEAK, True)
+  if '-v' in sys.argv:
+    logging.basicConfig(level=logging.DEBUG)
+  if '-l' in sys.argv:
+    SHOULD_LEAK = True
+    sys.argv.remove('-l')
+  FAKE = FakeRepos(TRIAL_DIR, SHOULD_LEAK, True)
   try:
-    fake.setUp()
+    FAKE.setUp()
     unittest.main()
   finally:
-    fake.tearDown()
+    FAKE.tearDown()
