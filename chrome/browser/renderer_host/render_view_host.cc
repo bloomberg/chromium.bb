924cdc0c71c14937b7247955da3d5390f43792fe
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-05-27 06:21:28 +0000

    Attempt 2 at landing this as this broke ui_tests on linux.
    The desktop notification service infobar would not display in ChromeFrame rendered pages as the code assumed the existing of a BrowserList
    and associated objects like Browser etc. In ChromeFrame the tab contents is owned by the ExternalTabContainer. Fix is to pass the current tab
    contents from the RenderViewHost to the DesktopNotificationService::RequestPermission function and use that if we don't have a browser list.
    
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=44913
    
    Bug=44913
    
    
    Review URL: http://codereview.chromium.org/2266005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48367 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 64e41217726cda2866e54fe2c93207da496c134b

--- chrome/browser/renderer_host/render_view_host.cc
+++ chrome/browser/renderer_host/render_view_host.cc
@@ -15,6 +15,7 @@
 #include "base/string_util.h"
 #include "base/time.h"
 #include "base/waitable_event.h"
+#include "chrome/browser/browser_list.h"
 #include "chrome/browser/child_process_security_policy.h"
 #include "chrome/browser/cross_site_request_manager.h"
 #include "chrome/browser/debugger/devtools_manager.h"
@@ -1752,10 +1753,18 @@ void RenderViewHost::OnCancelDesktopNotification(int notification_id) {
 
 void RenderViewHost::OnRequestNotificationPermission(
     const GURL& source_origin, int callback_context) {
-  DesktopNotificationService* service =
-      process()->profile()->GetDesktopNotificationService();
-  service->RequestPermission(
-      source_origin, process()->id(), routing_id(), callback_context);
+  Browser* browser = BrowserList::GetLastActive();
+  // We may not have a BrowserList if the chrome browser process is launched as
+  // a ChromeFrame process in which case we attempt to use the TabContents
+  // provided by the RenderViewHostDelegate.
+  TabContents* tab = browser ? browser->GetSelectedTabContents() :
+      (delegate_ ? delegate_->GetAsTabContents() : NULL);
+  if (tab) {
+    DesktopNotificationService* service =
+        process()->profile()->GetDesktopNotificationService();
+    service->RequestPermission(
+        source_origin, process()->id(), routing_id(), callback_context, tab);
+  }
 }
 
 void RenderViewHost::OnExtensionRequest(const std::string& name,
