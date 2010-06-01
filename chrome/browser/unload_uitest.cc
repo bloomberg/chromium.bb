4f649a8991772ddf755f5cac785b879a1b88591c
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-01 20:23:38 +0000

    Only disable UnloadTest.BrowserCloseTabWhenOtherTabHasListener under valgrind
    (leave it enabled on non-valgrind Linux)
    
    BUG=45562
    TEST=green tree
    
    Review URL: http://codereview.chromium.org/2428002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48649 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3f94a9eedbb0bd352beeea9ae65591c69c9012f1

--- chrome/browser/unload_uitest.cc
+++ chrome/browser/unload_uitest.cc
@@ -406,10 +406,8 @@ TEST_F(UnloadTest, BrowserCloseTwoSecondBeforeUnloadAlert) {
 #define MAYBE_BrowserCloseTabWhenOtherTabHasListener \
     DISABLED_BrowserCloseTabWhenOtherTabHasListener
 #else
-// http://crbug.com/45562
-// (whoa momma!  That's all of them!)
 #define MAYBE_BrowserCloseTabWhenOtherTabHasListener \
-    DISABLED_BrowserCloseTabWhenOtherTabHasListener
+    BrowserCloseTabWhenOtherTabHasListener
 #endif
 
 // Tests that if there's a renderer process with two tabs, one of which has an
