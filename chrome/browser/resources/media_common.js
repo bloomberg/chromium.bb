6cf1e7377ee0a9f3391e0134a91ef7aa9d773387
dhg@chromium.org <dhg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-04 22:26:37 +0000

    Adding support for .mov.  We might have to remove this later if we find that too many are not h.264.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/2622002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@48985 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3b5df032cc3a382021316fd74acfe432813c0889

---
+++ chrome/browser/resources/media_common.js
@@ -0,0 +1,19 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function pathIsVideoFile(path) {
+  return /\.(mp4|ogg|mpg|avi|mov)$/i.test(path);
+}
+
+function pathIsAudioFile(path) {
+  return /\.(mp3|m4a)$/i.test(path);
+}
+
+function pathIsImageFile(path) {
+  return /\.(jpg|png|gif)$/i.test(path);
+}
+
+function pathIsHtmlFile(path) {
+  return /\.(htm|html)$/i.test(path);
+}
