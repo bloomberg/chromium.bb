a38558497588b371d70605ae6460f79448b6d25c
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-14 20:33:51 +0000

    Linux: Remove KWin from the list of window managers that default to custom frame on.
    BUG=15698
    TEST=none
    
    Review URL: http://codereview.chromium.org/2833003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@49722 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d49099c62c66e972dd4dee81aa0d7eb47096dc1c

--- chrome/browser/gtk/browser_window_gtk.cc
+++ chrome/browser/gtk/browser_window_gtk.cc
@@ -2127,7 +2127,6 @@ bool BrowserWindowGtk::GetCustomFramePrefDefault() {
   return (wm_name == "Blackbox" ||
           wm_name == "compiz" ||
           wm_name == "e16" ||  // Enlightenment DR16
-          wm_name == "KWin" ||
           wm_name == "Metacity" ||
           wm_name == "Mutter" ||
           wm_name == "Openbox" ||
