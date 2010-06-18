dd9326909beaf149fa5a0c393420d5a0adf80d97
jiesun@google.com <jiesun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-18 15:58:03 +0000

    remove omx from win and mac build.
    TEST=try-bot
    BUG=45601
    
    Review URL: http://codereview.chromium.org/2870006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50243 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0ec4fc67805ecdbb955aa05ae329636f1699d5e5

--- webkit/glue/webmediaplayer_impl.cc
+++ webkit/glue/webmediaplayer_impl.cc
@@ -13,7 +13,6 @@
 #include "media/filters/ffmpeg_demuxer.h"
 #include "media/filters/ffmpeg_video_decoder.h"
 #include "media/filters/null_audio_renderer.h"
-#include "media/filters/omx_video_decoder.h"
 #include "skia/ext/platform_canvas.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebRect.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebSize.h"
@@ -228,10 +227,6 @@ WebMediaPlayerImpl::WebMediaPlayerImpl(WebKit::WebMediaPlayerClient* client,
   // Add in the default filter factories.
   filter_factory_->AddFactory(media::FFmpegDemuxer::CreateFilterFactory());
   filter_factory_->AddFactory(media::FFmpegAudioDecoder::CreateFactory());
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableOpenMax)) {
-    filter_factory_->AddFactory(media::OmxVideoDecoder::CreateFactory());
-  }
   filter_factory_->AddFactory(media::FFmpegVideoDecoder::CreateFactory());
   filter_factory_->AddFactory(media::NullAudioRenderer::CreateFilterFactory());
   filter_factory_->AddFactory(video_renderer_factory->CreateFactory(proxy_));
