c5f7eaa4511d22d3df3790c4008e7f7fdc0eb832
hinoka@chromium.org <hinoka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-27 22:17:41 +0000

    Added an about:about page (re-commit)
    Added a new page, about:about (or chrome://about/about)
    
    continued from http://codereview.chromium.org/2799038/show fixed typo that screwed up ChromeOS compiling
    
    BUG=44699
    TEST=Go to about:about, and see if About page loads. Check if links work. 
    TBR=pkasting@chromium.org
    Review URL: http://codereview.chromium.org/2861028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@50956 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5455842888161a22e42dfcc6c7dba1df34600c5a

--- chrome/test/functional/special_tabs.py
+++ chrome/test/functional/special_tabs.py
@@ -18,6 +18,7 @@ class SpecialTabsTest(pyauto.PyUITest):
 
   special_url_tabs = {
     'about:': 'About Version',
+    'about:about': 'About Pages',
     'about:appcache-internals': 'AppCache Internals',
     'about:credits': 'Credits',
     'about:dns': 'About DNS',
