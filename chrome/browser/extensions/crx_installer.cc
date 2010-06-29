b7b2fce10a765e4bb42ba7b4952a4c6f249531a5
rafaelw@chromium.org <rafaelw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-06-29 18:14:10 +0000

    Remove download check for web store apps (temporarily).
    There's still discussion about the design for this, so to minimize breakage on the dev channel, I'm backing the check out temporarily.
    
    BUG=47675,45542
    
    Review URL: http://codereview.chromium.org/2815035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51146 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6415a9cbb518893b583300d14bcaf92d8d950dd1

--- chrome/browser/extensions/crx_installer.cc
+++ chrome/browser/extensions/crx_installer.cc
@@ -147,16 +147,6 @@ void CrxInstaller::OnUnpackSuccess(const FilePath& temp_dir,
   // the temp dir.
   unpacked_extension_root_ = extension_dir;
 
-  // Only allow extensions with a gallery update url to be installed after
-  // having been directly downloaded from the gallery.
-  if (extension->update_url() == GURL(extension_urls::kGalleryUpdateURL) &&
-      !ExtensionsService::IsGalleryDownloadURL(original_url_)) {
-    ReportFailureFromFileThread(l10n_util::GetStringFUTF8(
-        IDS_EXTENSION_DISALLOW_NON_DOWNLOADED_GALLERY_INSTALLS,
-        l10n_util::GetStringUTF16(IDS_EXTENSION_WEB_STORE_TITLE)));
-    return;
-  }
-
   // Determine whether to allow installation. We always allow themes and
   // external installs.
   if (!extensions_enabled_ && !extension->is_theme() &&
