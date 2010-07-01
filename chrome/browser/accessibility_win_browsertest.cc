50c7721cf597cd73f1170e4eedfa555fabc003b4
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-01 17:18:16 +0000

    Disable AccessibilityWinBrowserTest.TestRendererAccessibilityTree which is failing after WebKit roll
    TBR=levin
    
    Review URL: http://codereview.chromium.org/2806047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51377 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ab95871c32a470ddf3e260c44328e32bf129ebde

--- chrome/browser/accessibility_win_browsertest.cc
+++ chrome/browser/accessibility_win_browsertest.cc
@@ -205,8 +205,9 @@ void AccessibleChecker::CheckAccessibleChildren(IAccessible* parent) {
   }
 }
 
+// http://crbug.com/48079 started failing after WebKit roll  62240:62258 
 IN_PROC_BROWSER_TEST_F(AccessibilityWinBrowserTest,
-                       TestRendererAccessibilityTree) {
+                       DISABLED_TestRendererAccessibilityTree) {
   // By requesting an accessible chrome will believe a screen reader has been
   // detected.
   ScopedComPtr<IAccessible> document_accessible(
