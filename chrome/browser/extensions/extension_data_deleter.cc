5728cb477b7f0c5fcf0e9088729b7fa63abcce03
rdsmith@google.com <rdsmith@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-07 19:58:36 +0000

    Fix bug in DeleteAllForURL; deletes entire store instead of just cookies related to URL (found by inspection.)  Also changed name and semantics to more closely reflect usage of primary caller (extension data deleter), and added test for that set of semantics.
    BUG=none
    TEST=Linux CookieMonsterTest.*:ParsedCookieTest.* (especially new CookieMonsterTest.DeleteAllHost)
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=51544
    
    Review URL: http://codereview.chromium.org/2857029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51766 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c8c7d8a930c1cb7af9a13c60cab10cfece2d0645

--- chrome/browser/extensions/extension_data_deleter.cc
+++ chrome/browser/extensions/extension_data_deleter.cc
@@ -44,7 +44,7 @@ void ExtensionDataDeleter::DeleteCookiesOnIOThread() {
   net::CookieMonster* cookie_monster =
       extension_request_context_->GetCookieStore()->GetCookieMonster();
   if (cookie_monster)
-    cookie_monster->DeleteAllForURL(extension_url_, true);
+    cookie_monster->DeleteAllForHost(extension_url_);
 }
 
 void ExtensionDataDeleter::DeleteDatabaseOnFileThread() {
--- net/base/cookie_monster.cc
+++ net/base/cookie_monster.cc
@@ -901,17 +901,24 @@ int CookieMonster::DeleteAllCreatedAfter(const Time& delete_begin,
   return DeleteAllCreatedBetween(delete_begin, Time(), sync_to_store);
 }
 
-int CookieMonster::DeleteAllForURL(const GURL& url,
-                                   bool sync_to_store) {
+int CookieMonster::DeleteAllForHost(const GURL& url) {
   AutoLock autolock(lock_);
   InitIfNecessary();
 
-  CookieList cookies = InternalGetAllCookiesForURL(url);
+  if (!HasCookieableScheme(url))
+    return 0;
+
+  // We store host cookies in the store by their canonical host name;
+  // domain cookies are stored with a leading ".".  So this is a pretty
+  // simple lookup and per-cookie delete.
   int num_deleted = 0;
-  for (CookieMap::iterator it = cookies_.begin(); it != cookies_.end();) {
-    CookieMap::iterator curit = it;
-    ++it;
-    InternalDeleteCookie(curit, sync_to_store, kDeleteCookieExplicit);
+  for (CookieMapItPair its = cookies_.equal_range(url.host());
+       its.first != its.second;) {
+    CookieMap::iterator curit = its.first;
+    ++its.first;
+    num_deleted++;
+
+    InternalDeleteCookie(curit, true, kDeleteCookieExplicit);
   }
   return num_deleted;
 }
