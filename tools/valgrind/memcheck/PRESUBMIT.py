83fc23c95f0b4e77dd9f36426e40c70c72c7ee5e
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-07 15:36:01 +0000

    Presumbit check for 'Suppression:' in valgrind suppressions
    BUG=None
    TEST=Try to accidentally break the valgrind suppressions by pasting too much into the file; fail.
    
    Review URL: http://codereview.chromium.org/2847044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@51717 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2f4294538dea93c4b48b20b9b564d09bb7591301

--- tools/valgrind/memcheck/PRESUBMIT.py
+++ tools/valgrind/memcheck/PRESUBMIT.py
@@ -7,5 +7,22 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details on the presubmit API built into gcl.
 """
 
+def CheckChange(input_api, output_api):
+  """Checks that the user didn't paste 'Suppression:' into the file"""
+  keyword = 'Suppression:'
+  for f, line_num, line in input_api.RightHandSideLines(lambda x:
+      x.LocalPath().endswith('.txt')):
+    if keyword in line:
+      text = '"%s" must not be included; %s line %s' % (
+          keyword, f.LocalPath(), line_num)
+      return [output_api.PresubmitError(text)]
+  return []
+
+def CheckChangeOnUpload(input_api, output_api):
+  return CheckChange(input_api, output_api)
+
+def CheckChangeOnCommit(input_api, output_api):
+  return CheckChange(input_api, output_api)
+
 def GetPreferredTrySlaves():
   return ['linux_valgrind', 'mac_valgrind']
