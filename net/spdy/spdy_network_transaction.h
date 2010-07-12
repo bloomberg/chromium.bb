c627b8a1d4db08211e8242c5fbdd1f8d61ebceb5
erikchen@google.com <erikchen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-12 18:16:14 +0000

    Streams send a Rst frame upon being closed by client. Some minor editorial fixes.
    TEST=net_unittests
    BUG=46589
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=51007
    
    Review URL: http://codereview.chromium.org/2804008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52106 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9c6527c9d4d289ed4f62194cf116e071b9d49ac9

--- net/spdy/spdy_network_transaction.h
+++ net/spdy/spdy_network_transaction.h
@@ -52,6 +52,9 @@ class SpdyNetworkTransaction : public HttpTransaction {
   virtual LoadState GetLoadState() const;
   virtual uint64 GetUploadProgress() const;
 
+  // Provide access to the stream for testing.
+  friend class SpdyNetworkTransactionTest;
+
  private:
   enum State {
     STATE_INIT_CONNECTION,
