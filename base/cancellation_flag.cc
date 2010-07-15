baee597024f97d43b2187b9fa66a7a1176db330b
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 02:43:38 +0000

    Replace a DCHECK with DCHECK_EQ.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/2919013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52429 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 528e07b4e972d01abff8cab24e65c29a668fea23

--- base/cancellation_flag.cc
+++ base/cancellation_flag.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,7 +10,7 @@ namespace base {
 
 void CancellationFlag::Set() {
 #if !defined(NDEBUG)
-  DCHECK(set_on_ == PlatformThread::CurrentId());
+  DCHECK_EQ(set_on_, PlatformThread::CurrentId());
 #endif
   base::subtle::Release_Store(&flag_, 1);
 }
