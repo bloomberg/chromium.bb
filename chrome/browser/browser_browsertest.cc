e4cab47b110a9a0bdac7e4cbde60d97016e45c72
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 21:50:17 +0000

    Disabling crashing browser test "ThirtyFourTabs" on mac
    BUG=49193
    TEST=none
    
    Review URL: http://codereview.chromium.org/2812054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52549 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c4bfb4f11dba0d48ae808c2b3fd023ed9f7a029b

--- chrome/browser/browser_browsertest.cc
+++ chrome/browser/browser_browsertest.cc
@@ -221,13 +221,19 @@ IN_PROC_BROWSER_TEST_F(BrowserTest, MAYBE_JavascriptAlertActivatesTab) {
   EXPECT_EQ(1, browser()->selected_index());
 }
 
+#if defined(OS_MACOSX)
+// Test is crashing on Mac trybot, see http://crbug.com/49193
+#define MAYBE_ThirtyFourTabs DISABLED_ThirtyFourTabs
+#else
+#define MAYBE_ThirtyFourTabs ThirtyFourTabs
+#endif
 // Create 34 tabs and verify that a lot of processes have been created. The
 // exact number of processes depends on the amount of memory. Previously we
 // had a hard limit of 31 processes and this test is mainly directed at
 // verifying that we don't crash when we pass this limit.
 // Warning: this test can take >30 seconds when running on a slow (low
 // memory?) Mac builder.
-IN_PROC_BROWSER_TEST_F(BrowserTest, ThirtyFourTabs) {
+IN_PROC_BROWSER_TEST_F(BrowserTest, MAYBE_ThirtyFourTabs) {
   GURL url(ui_test_utils::GetTestUrl(FilePath(FilePath::kCurrentDirectory),
                                      FilePath(kTitle2File)));
 
