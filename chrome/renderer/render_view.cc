fba83fef05c5a975ec3d0b887f53324353051dea
jcivelli@chromium.org <jcivelli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-15 21:38:20 +0000

    Fix the password autofill. My previous 1st refactor step of the password autofill broke the feature by removing the call to fill the form.
    BUG=49173
    TEST=See bug.
    
    Review URL: http://codereview.chromium.org/2812053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52543 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ad24ccbe258a148002af7ccfb243951100cc7dc8

--- chrome/renderer/render_view.cc
+++ chrome/renderer/render_view.cc
@@ -3903,6 +3903,8 @@ void RenderView::OnFillPasswordForm(
 #if defined(WEBKIT_BUG_41283_IS_FIXED)
   password_autocomplete_manager_.ReceivedPasswordFormFillData(webview(),
                                                               form_data);
+#else
+  webkit_glue::FillPasswordForm(this->webview(), form_data);
 #endif
 }
 
