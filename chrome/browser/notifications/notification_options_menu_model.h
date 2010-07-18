5b9ada2630421ed641acb4e4008b9f5996d9a962
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-18 21:12:44 +0000

    Reapply r52859
    This reverts commit a3d08737344176477998ba74549b3a23a72cdc96.
    
    This fix the win trybot by adding a missing ALLOW_THIS_IN_INITIALIZER_LIST macro necessary to silent the VS compiler.
    
    BUG=None
    TEST=trybots
    TBR=johnnyg@chromium.org
    
    Review URL: http://codereview.chromium.org/3045003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52868 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 78904e96cfdbd62a905bd2d26c3e5c8e6e84cee7

---
+++ chrome/browser/notifications/notification_options_menu_model.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_NOTIFICATIONS_NOTIFICATION_OPTIONS_MENU_MODEL_H_
+#define CHROME_BROWSER_NOTIFICATIONS_NOTIFICATION_OPTIONS_MENU_MODEL_H_
+
+#include "app/menus/simple_menu_model.h"
+#include "chrome/browser/notifications/balloon.h"
+
+class NotificationOptionsMenuModel : public menus::SimpleMenuModel,
+                                     public menus::SimpleMenuModel::Delegate {
+ public:
+  explicit NotificationOptionsMenuModel(Balloon* balloon);
+  virtual ~NotificationOptionsMenuModel();
+
+  // Overridden from menus::SimpleMenuModel::Delegate:
+  virtual bool IsCommandIdChecked(int command_id) const;
+  virtual bool IsCommandIdEnabled(int command_id) const;
+  virtual bool GetAcceleratorForCommandId(int command_id,
+                                          menus::Accelerator* accelerator);
+  virtual void ExecuteCommand(int command_id);
+
+ private:
+  Balloon* balloon_; // Not owned.
+
+  DISALLOW_COPY_AND_ASSIGN(NotificationOptionsMenuModel);
+};
+
+#endif  // CHROME_BROWSER_NOTIFICATIONS_NOTIFICATION_OPTIONS_MENU_MODEL_H_
