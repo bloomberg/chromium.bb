46d672586aebf3530e585407032e1bb76169f18c
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-19 23:07:27 +0000

    I reversed the order of the EXPECT_EQ tests so expected value is first, actual value is second.
    BUG=None
    TEST=unit_tests --gtest_filter="*CreditCardTest*"
    
    Review URL: http://codereview.chromium.org/3014014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@52961 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5f3d5355f9bf3bc2b8f12bc5affa784e00792d39

--- chrome/browser/autofill/credit_card_unittest.cc
+++ chrome/browser/autofill/credit_card_unittest.cc
@@ -16,54 +16,54 @@ TEST(CreditCardTest, PreviewSummaryAndObfuscatedNumberStrings) {
   // Case 0: empty credit card.
   CreditCard credit_card0(string16(), 0);
   string16 summary0 = credit_card0.PreviewSummary();
-  EXPECT_EQ(summary0, string16());
+  EXPECT_EQ(string16(), summary0);
   string16 obfuscated0 = credit_card0.ObfuscatedNumber();
-  EXPECT_EQ(obfuscated0, string16());
+  EXPECT_EQ(string16(), obfuscated0);
 
   // Case 00: Empty credit card with empty strings.
   CreditCard credit_card00(string16(), 0);
   autofill_unittest::SetCreditCardInfo(&credit_card00, "Corporate",
       "John Dillinger", "Visa", "", "", "", "Chicago");
   string16 summary00 = credit_card00.PreviewSummary();
-  EXPECT_EQ(summary00, string16());
+  EXPECT_EQ(string16(), summary00);
   string16 obfuscated00 = credit_card00.ObfuscatedNumber();
-  EXPECT_EQ(obfuscated00, string16());
+  EXPECT_EQ(string16(), obfuscated00);
 
   // Case 1: No credit card number.
   CreditCard credit_card1(string16(), 0);
   autofill_unittest::SetCreditCardInfo(&credit_card1, "Corporate",
       "John Dillinger", "Visa", "", "01", "2010", "Chicago");
   string16 summary1 = credit_card1.PreviewSummary();
-  EXPECT_EQ(summary1, string16());
+  EXPECT_EQ(string16(), summary1);
   string16 obfuscated1 = credit_card1.ObfuscatedNumber();
-  EXPECT_EQ(obfuscated1, string16());
+  EXPECT_EQ(string16(), obfuscated1);
 
   // Case 2: No month.
   CreditCard credit_card2(string16(), 0);
   autofill_unittest::SetCreditCardInfo(&credit_card2, "Corporate",
       "John Dillinger", "Visa", "123456789012", "", "2010", "Chicago");
   string16 summary2 = credit_card2.PreviewSummary();
-  EXPECT_EQ(summary2, string16(ASCIIToUTF16("************9012")));
+  EXPECT_EQ(string16(ASCIIToUTF16("************9012")), summary2);
   string16 obfuscated2 = credit_card2.ObfuscatedNumber();
-  EXPECT_EQ(obfuscated2, string16(ASCIIToUTF16("************9012")));
+  EXPECT_EQ(string16(ASCIIToUTF16("************9012")), obfuscated2);
 
   // Case 3: No year.
   CreditCard credit_card3(string16(), 0);
   autofill_unittest::SetCreditCardInfo(&credit_card3, "Corporate",
       "John Dillinger", "Visa", "123456789012", "01", "", "Chicago");
   string16 summary3 = credit_card3.PreviewSummary();
-  EXPECT_EQ(summary3, string16(ASCIIToUTF16("************9012")));
+  EXPECT_EQ(string16(ASCIIToUTF16("************9012")), summary3);
   string16 obfuscated3 = credit_card3.ObfuscatedNumber();
-  EXPECT_EQ(obfuscated3, string16(ASCIIToUTF16("************9012")));
+  EXPECT_EQ(string16(ASCIIToUTF16("************9012")), obfuscated3);
 
   // Case 4: Have everything.
   CreditCard credit_card4(string16(), 0);
   autofill_unittest::SetCreditCardInfo(&credit_card4, "Corporate",
       "John Dillinger", "Visa", "123456789012", "01", "2010", "Chicago");
   string16 summary4 = credit_card4.PreviewSummary();
-  EXPECT_EQ(summary4, string16(ASCIIToUTF16("************9012, Exp: 01/2010")));
+  EXPECT_EQ(string16(ASCIIToUTF16("************9012, Exp: 01/2010")), summary4);
   string16 obfuscated4 = credit_card4.ObfuscatedNumber();
-  EXPECT_EQ(obfuscated4, string16(ASCIIToUTF16("************9012")));
+  EXPECT_EQ(string16(ASCIIToUTF16("************9012")), obfuscated4);
 }
 
 }  // namespace
