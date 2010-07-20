1bc49cace48f53efef796360a7a9864367626307
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-20 16:56:50 +0000

    Improve the notifications options menu.  If the notification came from an extension, show the option to disable the extension rather than revoke permission.  Also give a link to the notifications content settings panel.OB
    BUG=49019
    TEST=create notification from page & extension, open the menu
    
    Review URL: http://codereview.chromium.org/3034015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53054 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ab7ede9ffdcf2af8028e3f95bece44caa51765b3

--- chrome/browser/notifications/notification_options_menu_model.cc
+++ chrome/browser/notifications/notification_options_menu_model.cc
@@ -8,19 +8,41 @@
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "base/utf_string_conversions.h"
+#include "chrome/browser/browser_list.h"
+#include "chrome/browser/extensions/extensions_service.h"
 #include "chrome/browser/notifications/desktop_notification_service.h"
 #include "chrome/browser/profile.h"
+#include "chrome/common/content_settings_types.h"
+#include "chrome/common/extensions/extension.h"
+#include "chrome/common/url_constants.h"
 #include "grit/generated_resources.h"
 
-static const int kRevokePermissionCommand = 0;
+// Menu commands
+const int kRevokePermissionCommand = 0;
+const int kDisableExtensionCommand = 1;
+const int kOpenContentSettingsCommand = 2;
 
 NotificationOptionsMenuModel::NotificationOptionsMenuModel(Balloon* balloon)
     : ALLOW_THIS_IN_INITIALIZER_LIST(menus::SimpleMenuModel(this)),
       balloon_(balloon) {
-  const string16 label_text = WideToUTF16Hack(l10n_util::GetStringF(
-      IDS_NOTIFICATION_BALLOON_REVOKE_MESSAGE,
-      balloon->notification().display_source()));
-  AddItem(kRevokePermissionCommand, label_text);
+
+  const Notification& notification = balloon->notification();
+  const GURL& origin = notification.origin_url();
+
+  if (origin.SchemeIs(chrome::kExtensionScheme)) {
+    const string16 disable_label = l10n_util::GetStringUTF16(
+        IDS_EXTENSIONS_DISABLE);
+    AddItem(kDisableExtensionCommand, disable_label);
+  } else {
+    const string16 disable_label = l10n_util::GetStringFUTF16(
+        IDS_NOTIFICATION_BALLOON_REVOKE_MESSAGE,
+        WideToUTF16(notification.display_source()));
+    AddItem(kRevokePermissionCommand, disable_label);
+  }
+
+  const string16 settings_label = l10n_util::GetStringUTF16(
+      IDS_NOTIFICATIONS_SETTINGS_BUTTON);
+  AddItem(kOpenContentSettingsCommand, settings_label);
 }
 
 NotificationOptionsMenuModel::~NotificationOptionsMenuModel() {
@@ -40,17 +62,33 @@ bool NotificationOptionsMenuModel::IsCommandIdEnabled(int /* command_id */)
 
 bool NotificationOptionsMenuModel::GetAcceleratorForCommandId(
     int /* command_id */, menus::Accelerator* /* accelerator */) {
-  // Current no accelerators.
+  // Currently no accelerators.
   return false;
 }
 
 void NotificationOptionsMenuModel::ExecuteCommand(int command_id) {
   DesktopNotificationService* service =
-    balloon_->profile()->GetDesktopNotificationService();
+      balloon_->profile()->GetDesktopNotificationService();
+  ExtensionsService* ext_service =
+      balloon_->profile()->GetExtensionsService();
+  const GURL& origin = balloon_->notification().origin_url();
   switch (command_id) {
     case kRevokePermissionCommand:
-      service->DenyPermission(balloon_->notification().origin_url());
+      service->DenyPermission(origin);
       break;
+    case kDisableExtensionCommand: {
+      Extension* extension = ext_service->GetExtensionByURL(origin);
+      if (extension)
+        ext_service->DisableExtension(extension->id());
+      break;
+    }
+    case kOpenContentSettingsCommand: {
+      Browser* browser = BrowserList::GetLastActive();
+      if (browser)
+        static_cast<TabContentsDelegate*>(browser)->ShowContentSettingsWindow(
+            CONTENT_SETTINGS_TYPE_NOTIFICATIONS);
+      break;
+    }
     default:
       NOTREACHED();
       break;
