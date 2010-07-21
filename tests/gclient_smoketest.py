df2b31582f88cd0bc09c74079806ab53b1a445ff
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-21 17:35:24 +0000

    Reapply gclient.py refactor for the third time.
    Bring some OOP and sanity to gclient.py.
    
    - Changed the algorithm from breadth-first to depth-first.
    - Added infinite recursion support.
    - Fixed From() of From() dependency.
    - Fixed cross solution custom deps aliasing.
    - Removed support code for old .gclient_entries format.
    - Removed IsGitCheckout() in favor for a direct check for .git presence.
    
    TEST=all test pass
    
    The main difference with r51760 is that new smoke tests have been added since which replicates how webkit checkouts with gclient.
    
    Review URL: http://codereview.chromium.org/2917009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@53205 0039d316-1c4b-4281-b951-d872f2087c98

--- tests/gclient_smoketest.py
+++ tests/gclient_smoketest.py
@@ -602,7 +602,6 @@ class GClientSmokeGIT(GClientSmokeBase):
   def testRevInfo(self):
     if not self.enabled:
       return
-    # TODO(maruel): Test multiple solutions.
     self.gclient(['config', self.git_base + 'repo_1', '--name', 'src'])
     self.gclient(['sync', '--deps', 'mac'])
     results = self.gclient(['revinfo', '--deps', 'mac'])
@@ -641,7 +640,7 @@ class GClientSmokeBoth(GClientSmokeBase):
                      # file when File() is used in a DEPS file.
                      ('running', self.root_dir + '/src/file/other'),
                      'running', 'running', 'running', 'running', 'running',
-                     'running', 'running'])
+                     'running'])
     # TODO(maruel): Something's wrong here. git outputs to stderr 'Switched to
     # new branch \'hash\''.
     #self.checkString('', results[1])
@@ -658,7 +657,6 @@ class GClientSmokeBoth(GClientSmokeBase):
     tree['src/git_hooked1'] = 'git_hooked1'
     tree['src/git_hooked2'] = 'git_hooked2'
     tree['src/svn_hooked1'] = 'svn_hooked1'
-    tree['src/svn_hooked2'] = 'svn_hooked2'
     self.assertTree(tree)
 
   def testMultiSolutionsMultiRev(self):
