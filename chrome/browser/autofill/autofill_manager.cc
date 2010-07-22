a6df96bea6438b02fc57ce53dfd568e84249c0e1
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-22 21:53:30 +0000

    AutoFill memory exception when deleting text from a filled form
    Changes duplicate removal logic to avoid erasure of missing unique_id element.
    
    BUG=49872
    TEST=Manual test as per bug.
    
    Review URL: http://codereview.chromium.org/3031018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53396 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2f66099544951cddee4f58543a66fe41018e2c02

--- chrome/browser/autofill/autofill_manager.cc
+++ chrome/browser/autofill/autofill_manager.cc
@@ -43,26 +43,31 @@ const string16::value_type kLabelSeparator[] = {';',' ',0};
 // |unique_ids|.
 void RemoveDuplicateElements(
     std::vector<string16>* elements, std::vector<int>* unique_ids) {
-  std::vector<string16> copy;
+  DCHECK_EQ(elements->size(), unique_ids->size());
+
+  std::vector<string16> elements_copy;
+  std::vector<int> unique_ids_copy;
   for (size_t i = 0; i < elements->size(); ++i) {
     const string16& element = (*elements)[i];
 
     bool unique = true;
-    for (std::vector<string16>::const_iterator copy_iter = copy.begin();
-         copy_iter != copy.end(); ++copy_iter) {
+    for (std::vector<string16>::const_iterator copy_iter
+             = elements_copy.begin();
+         copy_iter != elements_copy.end(); ++copy_iter) {
       if (element == *copy_iter) {
         unique = false;
         break;
       }
     }
 
-    if (unique)
-      copy.push_back(element);
-    else
-      unique_ids->erase(unique_ids->begin() + i);
+    if (unique) {
+      elements_copy.push_back(element);
+      unique_ids_copy.push_back((*unique_ids)[i]);
+    }
   }
 
-  elements->assign(copy.begin(), copy.end());
+  elements->assign(elements_copy.begin(), elements_copy.end());
+  unique_ids->assign(unique_ids_copy.begin(), unique_ids_copy.end());
 }
 
 bool FormIsHTTPS(FormStructure* form) {
