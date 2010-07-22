3ef4eaf12779105131a5dd5069c2c68b7fb38556
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-22 18:21:02 +0000

    AutoFill: Initialize |succeeded_| in the AutoFillXmlParser constructor.
    CID=8457
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/3033019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53360 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8cd7fd05921738dab7a0d9645217f21bfc38fa90

--- chrome/browser/autofill/autofill_xml_parser.cc
+++ chrome/browser/autofill/autofill_xml_parser.cc
@@ -93,7 +93,8 @@ int AutoFillQueryXmlParser::GetIntValue(buzz::XmlParseContext* context,
 
 AutoFillUploadXmlParser::AutoFillUploadXmlParser(double* positive_upload_rate,
                                                  double* negative_upload_rate)
-    : positive_upload_rate_(positive_upload_rate),
+    : succeeded_(false),
+      positive_upload_rate_(positive_upload_rate),
       negative_upload_rate_(negative_upload_rate) {
   DCHECK(positive_upload_rate_);
   DCHECK(negative_upload_rate_);
