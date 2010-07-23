9036e9986f621c9a8f541aa0a570343106df0969
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-23 21:07:58 +0000

    Add pyauto tests for verifying some facts about chrome processes.
    testFlashLoadsAfterKill
    testBasics
    testSingleFlashPluginProcess
    
    Merge an old test into this.
    
    Review URL: http://codereview.chromium.org/3051010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53528 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f98391c1fac48105c5b503d18dee5c83a65aee60

--- chrome/test/functional/browser.py
+++ chrome/test/functional/browser.py
@@ -5,6 +5,7 @@
 
 import os
 import re
+import types
 
 import pyauto_functional  # Must be imported before pyauto
 import pyauto
@@ -25,10 +26,53 @@ class BrowserTest(pyauto.PyUITest):
       info = self.GetBrowserInfo()
       pp.pprint(info)
 
-  def testGotVersion(self):
-    """Verify there's a valid version string."""
-    version_string = self.GetBrowserInfo()['properties']['ChromeVersion']
+  def _VerifyUniqueRendererProcesses(self, browser_info):
+    """Verify that each tab has a unique renderer process.
+
+    This cannot be used for large number of renderers since there actually is
+    a cap, depending on memory on the machine.
+
+    Args:
+      browser_info: browser info data dictionary as returned by GetBrowserInfo()
+    """
+    seen_pids = {}  # lookup table of PIDs we've seen
+    for window in browser_info['windows']:
+      for tab in window['tabs']:
+        renderer_pid = tab['renderer_pid']
+        self.assertEqual(types.IntType, type(renderer_pid))
+        # Verify unique renderer pid
+        self.assertFalse(renderer_pid in seen_pids, 'renderer pid not unique')
+        seen_pids[renderer_pid] = True
+
+  def testBasics(self):
+    """Verify basic browser info at startup."""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title2.html'))
+    self.NavigateToURL(url)
+    info = self.GetBrowserInfo()
+    # Verify valid version string
+    version_string = info['properties']['ChromeVersion']
     self.assertTrue(re.match('\d+\.\d+\.\d+.\.\d+', version_string))
+    # Verify browser process
+    self.assertEqual(types.IntType, type(info['browser_pid']))
+    self.assertEqual(1, len(info['windows']))  # one window
+    self.assertEqual(1, len(info['windows'][0]['tabs']))  # one tab
+    self.assertEqual(0, info['windows'][0]['selected_tab'])  # 1st tab selected
+    self.assertEqual(url, info['windows'][0]['tabs'][0]['url'])
+    self.assertFalse(info['windows'][0]['fullscreen'])  # not fullscreen
+    self._VerifyUniqueRendererProcesses(info)
+
+  def testProcessesForMultipleWindowsAndTabs(self):
+    """Verify processes for multiple windows and tabs"""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title2.html'))
+    self.NavigateToURL(url)
+    for _ in range(2):
+      self.AppendTab(pyauto.GURL(url))
+    for windex in range(1, 3):  # 2 new windows
+      self.OpenNewBrowserWindow(True)
+      self.NavigateToURL(url, windex, 0)
+    self.RunCommand(pyauto.IDC_NEW_INCOGNITO_WINDOW)  # incognito window
+    self.NavigateToURL(url, 3, 0)
+    self._VerifyUniqueRendererProcesses(self.GetBrowserInfo())
 
   def testWindowResize(self):
     """Verify window resizing and persistence after restart."""
@@ -54,6 +98,39 @@ class BrowserTest(pyauto.PyUITest):
     child_processes = self.GetBrowserInfo()['child_processes']
     self.assertTrue([x for x in child_processes
         if x['type'] == 'Plug-in' and x['name'] == 'Shockwave Flash'])
+
+  def _GetFlashProcessesInfo(self):
+    """Get info about flash processes, if any."""
+    return [x for x in self.GetBrowserInfo()['child_processes']
+            if x['type'] == 'Plug-in' and x['name'] == 'Shockwave Flash']
+
+  def testSingleFlashPluginProcess(self):
+    """Verify there's only one flash plugin process shared across all uses."""
+    flash_url = self.GetFileURLForPath(os.path.join(self.DataDir(),
+                                                    'plugin', 'flash.swf'))
+    self.NavigateToURL(flash_url)
+    for _ in range(2):
+      self.AppendTab(pyauto.GURL(flash_url))
+    # Open flash in new window
+    self.OpenNewBrowserWindow(True)
+    self.NavigateToURL(flash_url, 1, 0)
+    # Open flash in new incognito window
+    self.RunCommand(pyauto.IDC_NEW_INCOGNITO_WINDOW)
+    self.NavigateToURL(flash_url, 1, 0)
+    # Verify there's only 1 flash process
+    self.assertEqual(1, len(self._GetFlashProcessesInfo()))
+
+  def testFlashLoadsAfterKill(self):
+    """Verify that Flash process reloads after crashing (or being killed)."""
+    flash_url = self.GetFileURLForPath(os.path.join(self.DataDir(),
+                                                    'plugin', 'flash.swf'))
+    self.NavigateToURL(flash_url)
+    flash_process_id1 = self._GetFlashProcessesInfo()[0]['pid']
+    self.Kill(flash_process_id1)
+    self.GetBrowserWindow(0).GetTab(0).Reload()  # Reload
+    flash_processes = self._GetFlashProcessesInfo()
+    self.assertEqual(1, len(flash_processes))
+    self.assertNotEqual(flash_process_id1, flash_processes[0]['pid'])
 
 
 if __name__ == '__main__':
