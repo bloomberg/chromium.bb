e23d00ef71784f5bbb6d8a6cc57ef3c004edcb83
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-07-23 17:33:19 +0000

    Fix missing #include in nacl_entry_points.h
    Attempt to fix the error that occurred on some of the Chromium Linux
    buildbots when updating DEPS to include r2744:
    
    ./native_client/src/trusted/plugin/nacl_entry_points.h:52: error: ‘string’ is not a member of ‘std’
    
    Review URL: http://codereview.chromium.org/3034022
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2776 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: f910369f9801bf9412a64fd6883afbfb3a951b29

--- src/trusted/plugin/nacl_entry_points.h
+++ src/trusted/plugin/nacl_entry_points.h
@@ -33,8 +33,12 @@
 #define NATIVE_CLIENT_SRC_TRUSTED_PLUGIN_NACL_ENTRY_POINTS_H_
 
 #include <stddef.h>
+
 #include <map>
+#include <string>
+
 #include "native_client/src/shared/imc/nacl_imc.h"
+
 
 typedef bool (*LaunchNaClProcessFunc)(const char* url,
                                       int imc_fd,
