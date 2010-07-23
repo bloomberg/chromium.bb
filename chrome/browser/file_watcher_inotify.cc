4a4e71a0a62f0b244455c8e3acab4821782fa460
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-23 12:13:32 +0000

    Coverity issue 9320: Uninitialized member in file_watcher_inotify.cc
    BUG=None (Coverity issue id listed above)
    TEST=None
    
    Review URL: http://codereview.chromium.org/3063004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@53463 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 34a1f2f580f5fd28de16abebbe80cbba274f30ad

--- chrome/browser/file_watcher_inotify.cc
+++ chrome/browser/file_watcher_inotify.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -276,7 +276,8 @@ void InotifyReader::OnInotifyEvent(const inotify_event* event) {
 }
 
 FileWatcherImpl::FileWatcherImpl()
-    : watch_(InotifyReader::kInvalidWatch) {
+    : delegate_(NULL),
+      watch_(InotifyReader::kInvalidWatch) {
 }
 
 FileWatcherImpl::~FileWatcherImpl() {
