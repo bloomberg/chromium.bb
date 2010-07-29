237ea77c473156351349e9472d38e70c98310ea0
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-29 20:55:00 +0000

    Changes to get npapi_pepper_test_plugin recognized on linux. Review URL: http://codereview.chromium.org/2819071
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54186 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dcaeb85d3d88fbfb40f1451f9a40fac19e9885f4

--- webkit/tools/npapi_pepper_test_plugin/main.cc
+++ webkit/tools/npapi_pepper_test_plugin/main.cc
@@ -11,15 +11,54 @@
 #include "webkit/glue/plugins/test/plugin_client.h"
 
 extern "C" {
-EXPORT NPError API_CALL NP_GetEntryPoints(NPPluginFuncs* pFuncs) {
-  return NPAPIClient::PluginClient::GetEntryPoints(pFuncs);
-}
-
-EXPORT NPError API_CALL NP_Initialize(NPNetscapeFuncs* pFuncs) {
-  return NPAPIClient::PluginClient::Initialize(pFuncs);
+EXPORT NPError API_CALL NP_GetEntryPoints(NPPluginFuncs* nppFuncs) {
+  return NPAPIClient::PluginClient::GetEntryPoints(nppFuncs);
 }
 
 EXPORT NPError API_CALL NP_Shutdown() {
   return NPAPIClient::PluginClient::Shutdown();
 }
+
+#if defined(OS_POSIX) && !defined(OS_MACOSX)
+EXPORT NPError API_CALL NP_Initialize(NPNetscapeFuncs* npnFuncs,
+                                      NPPluginFuncs* nppFuncs) {
+  NPError error = NPAPIClient::PluginClient::Initialize(npnFuncs);
+  if (error == NPERR_NO_ERROR) {
+    error = NP_GetEntryPoints(nppFuncs);
+  }
+  return error;
+}
+
+EXPORT NPError API_CALL NP_GetValue(NPP instance, NPPVariable variable,
+                                    void* value) {
+  NPError err = NPERR_NO_ERROR;
+
+  switch (variable) {
+    case NPPVpluginNameString:
+      *(static_cast<const char**>(value)) = "NPAPI Pepper Test Plugin";
+      break;
+    case NPPVpluginDescriptionString:
+      *(static_cast<const char**>(value)) = "NPAPI Pepper Test Plugin";
+      break;
+    case NPPVpluginNeedsXEmbed:
+      *(static_cast<NPBool*>(value)) = true;
+      break;
+    default:
+      err = NPERR_GENERIC_ERROR;
+      break;
+  }
+
+  return err;
+}
+
+EXPORT const char* API_CALL NP_GetMIMEDescription() {
+  return "pepper-application/x-pepper-test:pepper:NPAPI Pepper Test Plugin";
+}
+#else  // OS_POSIX
+EXPORT NPError API_CALL NP_Initialize(NPNetscapeFuncs* npnFuncs) {
+  return NPAPIClient::PluginClient::Initialize(npnFuncs);
+}
+#endif  // OS_POSIX
+
 } // extern "C"
+
