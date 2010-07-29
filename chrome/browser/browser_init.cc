5d9594aebfc7e97a3ad4fa4988ec55726bccf984
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-29 03:55:05 +0000

    Revert 54073 - Fix --kiosk to work regardless of the startup pref setting or supplied URLs.
    BUG=none
    TEST=Set Chrome to "open windows and tabs from last time", then run with --kiosk.  Should start in kiosk mode (fullscreen, no status bubble).
    Review URL: http://codereview.chromium.org/3061026
    
    TBR=pkasting@chromium.org
    Review URL: http://codereview.chromium.org/3012031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54095 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 49c474faa2fa4175bb00fb9a12ae5ee003ea1bae

--- chrome/browser/browser_init.cc
+++ chrome/browser/browser_init.cc
@@ -552,17 +552,13 @@ bool BrowserInit::LaunchWithProfile::Launch(Profile* profile,
     RecordLaunchModeHistogram(LM_AS_WEBAPP);
   }
 
-#if !defined(OS_MACOSX)
-  // In kiosk mode, we want to always be fullscreen, so switch to that now.
-  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kKioskMode))
-    BrowserList::GetLastActive()->ToggleFullscreenMode();
-#endif
-
 #if defined(OS_WIN)
   // Print the selected page if the command line switch exists. Note that the
   // current selected tab would be the page which will be printed.
-  if (command_line_.HasSwitch(switches::kPrint))
-    BrowserList::GetLastActive()->Print();
+  if (command_line_.HasSwitch(switches::kPrint)) {
+    Browser* browser = BrowserList::GetLastActive();
+    browser->Print();
+  }
 #endif
 
   // If we're recording or playing back, startup the EventRecorder now
@@ -745,6 +741,12 @@ Browser* BrowserInit::LaunchWithProfile::OpenTabsInBrowser(
     gtk_util::SetWMLastUserActionTime(browser->window()->GetNativeHandle());
 #endif
   }
+
+#if !defined(OS_MACOSX)
+  // In kiosk mode, we want to always be fullscreen, so switch to that now.
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kKioskMode))
+    browser->ToggleFullscreenMode();
+#endif
 
   bool first_tab = true;
   for (size_t i = 0; i < tabs.size(); ++i) {
