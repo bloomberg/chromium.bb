9d5f7f329762251b52d6301625e4d4d50909c13c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-07-31 23:10:07 +0000

    Fix bad license header in app/ and net/ directories.
    BUG=50266
    TEST=None
    
    Review URL: http://codereview.chromium.org/3013049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54482 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5461d9de823901b0b4413b175e970aa23763056f

--- app/app_paths.h
+++ app/app_paths.h
@@ -1,6 +1,6 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved. Use of this
-// source code is governed by a BSD-style license that can be found in the
-// LICENSE file.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef APP_APP_PATHS_H_
 #define APP_APP_PATHS_H_
--- net/tools/flip_server/other_defines.h
+++ net/tools/flip_server/other_defines.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef NET_TOOLS_FLIP_SERVER_OTHER_DEFINES
 #define NET_TOOLS_FLIP_SERVER_OTHER_DEFINES
 #pragma once
