fc411b80e2d81a173b8331856324ac9129334591
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-03 09:13:57 +0000

    DevTools: follow up to r54722, remove unused classes.
    Review URL: http://codereview.chromium.org/3037037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@54724 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3b4c852dbdb38a51453945619394ea0801542019

--- chrome/renderer/devtools_agent.cc
+++ chrome/renderer/devtools_agent.cc
@@ -15,7 +15,6 @@
 #include "third_party/WebKit/WebKit/chromium/public/WebPoint.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebString.h"
 #include "third_party/WebKit/WebKit/chromium/public/WebView.h"
-#include "webkit/glue/devtools_message_data.h"
 #include "webkit/glue/webkit_glue.h"
 
 using WebKit::WebDevToolsAgent;
