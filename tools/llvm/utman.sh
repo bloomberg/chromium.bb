6e9e50923bf90ae995e68872ce82c1ae5df07c07
jvoung@google.com <jvoung@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-08-03 22:46:47 +0000

    LLVM bump to enable http://codereview.chromium.org/3085008/show Review URL: http://codereview.chromium.org/3057032
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2893 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 571ed7826297c8c0b23727fe1b6f851313eef325

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -122,7 +122,7 @@ readonly PNACL_CLIENT_TC_X8632="${PNACL_CLIENT_TC_ROOT}/x8632"
 readonly PNACL_CLIENT_TC_X8664="${PNACL_CLIENT_TC_ROOT}/x8664"
 
 # Current milestones within each Hg repo:
-readonly LLVM_REV=d8bd7bd01fd1
+readonly LLVM_REV=d01784bb0fc4
 readonly LLVM_GCC_REV=3eec4354e37b
 readonly NEWLIB_REV=c74ed6d22b4f
 readonly BINUTILS_REV=e22514d846ae
