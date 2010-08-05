c6ae766207b364e8a53fba113a71a7917ae011b4
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-05 04:47:44 +0000

    Remove a couple of unneeded forwarded decls.
    BUG=none
    TEST=builds
    
    Review URL: http://codereview.chromium.org/3012046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55029 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7d7299a68bb7563ed4d7b17e80f80de6f0c48b24

--- chrome/browser/browser_theme_provider.h
+++ chrome/browser/browser_theme_provider.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -20,10 +20,8 @@ namespace color_utils {
 
 class BrowserThemePack;
 class BrowserThemeProviderTest;
-class DictionaryValue;
 class Extension;
 class FilePath;
-class PrefService;
 class Profile;
 class ResourceBundle;
 
