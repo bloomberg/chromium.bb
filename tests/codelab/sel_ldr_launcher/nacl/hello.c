be66d98ebb7be787109e1b8d4fd56014aee71578
krasin@google.com <krasin@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-08-06 15:52:29 +0000

    Added the code for codelab about using Native Client as a sandbox outside of the browser.
    Review URL: http://codereview.chromium.org/3096004
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2929 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: b230dc7d01befa5623c0824d3c45cd7e26166dbc

---
+++ tests/codelab/sel_ldr_launcher/nacl/hello.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2010 The Native Client Authors.  All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can
+ * be found in the LICENSE file.
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+
+#include <nacl/nacl_srpc.h>
+
+NaClSrpcError GetGreeting(NaClSrpcChannel *channel,
+    NaClSrpcArg **in_args, NaClSrpcArg **out_args) {
+  printf("[GetGreeting] GetGreeting is called\n");
+
+  char* name = in_args[0]->u.sval;
+  printf("[GetGreeting] name = %s\n", name);
+
+  char* greeting = (char*) calloc(100 + strlen(name), sizeof(char));
+  sprintf(greeting, "Hello, %s\n", name);
+
+  /* Note that SRPC layer frees strings passed to it. */
+  out_args[0]->u.sval = greeting;
+
+  return NACL_SRPC_RESULT_OK;
+}
+
+NACL_SRPC_METHOD("GetGreeting:s:s", GetGreeting);
---
+++ tests/codelab/sel_ldr_launcher/run.sh
@@ -0,0 +1,1 @@
+launcher/launch ../../../scons-out/opt-linux-x86-64/staging/ nacl/hello.nexe
