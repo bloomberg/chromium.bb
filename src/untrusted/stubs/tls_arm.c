b67ca5aba27fc935459e482fd75c7e548a0bd054
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-08-09 17:12:44 +0000

    Avoid sone platform dispatching cpp magic in untrusted code
    Review URL: http://codereview.chromium.org/3066045
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@2944 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 910c2569fe3180c366442268514570abbbbbc761

--- src/untrusted/stubs/tls_arm.c
+++ src/untrusted/stubs/tls_arm.c
@@ -10,58 +10,26 @@
  */
 
 #include "native_client/src/untrusted/nacl/tls.h"
-#include "native_client/src/untrusted/nacl/syscall_bindings_trampoline.h"
 
 /* See src/untrusted/nacl/tls.h */
 int __nacl_tls_aligment() {
-#ifdef __x86_32__
-  return 32;
-#elif NACL_ARCH(NACL_TARGET_ARCH) == NACL_arm
   return (1 << 12);
-#else
-  return 32;
-#endif
 }
 
 
 /* See src/untrusted/nacl/tls.h */
 size_t __nacl_tdb_offset_in_tls(size_t tls_data_and_bss_size) {
-#ifdef __x86_32__
-  return tls_data_and_bss_size; /* TDB after TLS */
-#elif NACL_ARCH(NACL_TARGET_ARCH) == NACL_arm
   return 0; /* TDB is the first symbol in the TLS. */
-#else
-  return tls_data_and_bss_size; /* TDB after TLS */
-#endif
 }
 
 
 /* See src/untrusted/nacl/tls.h */
 size_t __nacl_tdb_effective_payload_size(size_t tdb_size) {
-#ifdef __x86_32__
-  return tdb_size; /* TDB after TLS */
-#elif NACL_ARCH(NACL_TARGET_ARCH) == NACL_arm
   return 0; /* TDB is the first symbol in the TLS. */
-#else
-  return tdb_size; /* TDB after TLS */
-#endif
 }
 
 
 /* See src/untrusted/nacl/tls.h */
 size_t __nacl_return_address_size() {
-  /* We can't use sizeof(intptr_t) without relying stdint.h. */
-#ifdef __x86_64__
-  return 8;
-#else /* __arm__ or __i386__ */
   return 4;
-#endif
 }
-
-
-#ifdef __x86_64__
-/* Compiler intrinsic. */
-void *__tls_get_addr(int offset) {
-  return ((char*) NACL_SYSCALL(tls_get)()) + offset;
-}
-#endif
---
+++ src/untrusted/stubs/tls_x86_32.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2010 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can
+ * be found in the LICENSE file.
+ */
+
+/*
+ * Native Client support for TLS, platform-specific components.
+ * These routines are documented in the tls.h header included below.
+ */
+
+#include "native_client/src/untrusted/nacl/tls.h"
+
+/* See src/untrusted/nacl/tls.h */
+int __nacl_tls_aligment() {
+  return 32;
+}
+
+
+/* See src/untrusted/nacl/tls.h */
+size_t __nacl_tdb_offset_in_tls(size_t tls_data_and_bss_size) {
+  return tls_data_and_bss_size; /* TDB after TLS */
+}
+
+
+/* See src/untrusted/nacl/tls.h */
+size_t __nacl_tdb_effective_payload_size(size_t tdb_size) {
+  return tdb_size; /* TDB after TLS */
+}
+
+
+/* See src/untrusted/nacl/tls.h */
+size_t __nacl_return_address_size() {
+  return 4;
+}
