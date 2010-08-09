304c5c6fb4ba0d643eb19efc374cb9523aee8d5d
alyssad@chromium.org <alyssad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-09 16:29:31 +0000

    New autofill pyauto functional tests
    This adds some more tests to autofill.py that test invalid input and unicode input.
    
    Review URL: http://codereview.chromium.org/3054049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55407 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 92ae3884f0208cdf97e7bf3236379d85b810d8d5

--- chrome/test/functional/autofill.py
+++ chrome/test/functional/autofill.py
@@ -3,6 +3,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+
 import pyauto_functional  # Must be imported before pyauto
 import pyauto
 
@@ -11,7 +13,7 @@ class AutoFillTest(pyauto.PyUITest):
   """Tests that autofill works correctly"""
 
   def testFillProfile(self):
-    """Test filling profiles and overwriting with new profiles"""
+    """Test filling profiles and overwriting with new profiles."""
     profiles = [{'label': 'Profile 1', 'NAME_FIRST': 'Bob',
                  'NAME_LAST': 'Smith', 'ADDRESS_HOME_ZIP': '94043',},
                 {'label': 'Profile 2', 'EMAIL_ADDRESS': 'sue@example.com',
@@ -34,18 +36,53 @@ class AutoFillTest(pyauto.PyUITest):
     self.assertEqual(profiles, profile['profiles'])
     self.assertEqual(credit_cards, profile['credit_cards'])
 
-  def testFillProfileUnicode(self):
-    """Test filling profiles with unicode strings"""
-    profiles = [{'label': u'unic\u00F3de', 'NAME_FIRST': u'J\u00E4n',
-                'ADDRESS_HOME_LINE1': u'123 R\u00F6d'}]
-    self.FillAutoFillProfile(profiles)
+  def testFillProfileCrazyCharacters(self):
+    """Test filling profiles with unicode strings and crazy characters."""
+    # Adding autofill profiles.
+    file_path = os.path.join(self.DataDir(), 'autofill', 'crazy_autofill.txt')
+    profiles = self.EvalDataFrom(file_path)
+    self.FillAutoFillProfile(profiles=profiles)
     self.assertEqual(profiles, self.GetAutoFillProfile()['profiles'])
 
+    # Adding credit cards.
+    file_path = os.path.join(self.DataDir(), 'autofill',
+                             'crazy_creditcards.txt')
+    credit_cards = self.EvalDataFrom(file_path)
+    self.FillAutoFillProfile(credit_cards=credit_cards)
+    self.assertEqual(credit_cards,
+                     self.GetAutoFillProfile()['credit_cards'])
+
   def testGetProfilesEmpty(self):
-    """Test getting profiles when none have been filled"""
+    """Test getting profiles when none have been filled."""
     profile = self.GetAutoFillProfile()
     self.assertEqual([], profile['profiles'])
     self.assertEqual([], profile['credit_cards'])
+
+  def testAutofillInvalid(self):
+    """Test filling in invalid values for profiles and credit cards."""
+    # First try profiles with invalid input.
+    without_invalid = {'label': 'Invalid_Profile' , 'NAME_FIRST': u'Will',
+                       'ADDRESS_HOME_CITY': 'Sunnyvale',
+                       'ADDRESS_HOME_STATE': 'CA',
+                       'ADDRESS_HOME_ZIP': 'my_zip',
+                       'ADDRESS_HOME_COUNTRY': 'USA'}
+    # Add some invalid fields.
+    with_invalid = without_invalid.copy()
+    with_invalid['PHONE_HOME_NUMBER'] = 'Invalid_Phone_Number'
+    with_invalid['PHONE_FAX_NUMBER'] = 'Invalid_Fax_Number'
+    self.FillAutoFillProfile(profiles=[with_invalid])
+    self.assertEqual([without_invalid], self.GetAutoFillProfile()['profiles'])
+
+    # Then try credit cards with invalid input.
+    without_invalid = {'label': 'Invalid_CC',
+                       'CREDIT_CARD_NUMBER': 'Not_Checked'}
+    # Add some invalid fields.
+    with_invalid = without_invalid.copy()
+    with_invalid['CREDIT_CARD_EXP_MONTH'] = 'Not_A_Month'
+    with_invalid['CREDIT_CARD_EXP_4_DIGIT_YEAR'] = 'Not_A_Year'
+    self.FillAutoFillProfile(credit_cards=[with_invalid])
+    self.assertEqual([without_invalid],
+                     self.GetAutoFillProfile()['credit_cards'])
 
 
 if __name__ == '__main__':
