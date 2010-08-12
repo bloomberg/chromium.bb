b06e0b51ae218f0c7f0b5f02688e52aa05ba0fc9
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-12 23:21:01 +0000

    DOMUI: Add basic HTML for the AutoFill options page.
    BUG=49094
    TEST=none
    
    Review URL: http://codereview.chromium.org/3171003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@55957 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 182f85294d806edbda95708233815881d802b2df

--- chrome/browser/dom_ui/autofill_options_handler.cc
+++ chrome/browser/dom_ui/autofill_options_handler.cc
@@ -21,6 +21,22 @@ void AutoFillOptionsHandler::GetLocalizedValues(
 
   localized_strings->SetString("autoFillOptionsTitle",
       l10n_util::GetStringUTF16(IDS_AUTOFILL_OPTIONS_TITLE));
+  localized_strings->SetString(L"autoFillEnabled",
+      l10n_util::GetString(IDS_OPTIONS_AUTOFILL_ENABLE));
+  localized_strings->SetString(L"addressesHeader",
+      l10n_util::GetString(IDS_AUTOFILL_ADDRESSES_GROUP_NAME));
+  localized_strings->SetString(L"creditCardsHeader",
+      l10n_util::GetString(IDS_AUTOFILL_CREDITCARDS_GROUP_NAME));
+  localized_strings->SetString(L"addAddressButton",
+      l10n_util::GetString(IDS_AUTOFILL_ADD_ADDRESS_BUTTON));
+  localized_strings->SetString(L"addCreditCardButton",
+      l10n_util::GetString(IDS_AUTOFILL_ADD_CREDITCARD_BUTTON));
+  localized_strings->SetString(L"editButton",
+      l10n_util::GetString(IDS_AUTOFILL_EDIT_BUTTON));
+  localized_strings->SetString(L"deleteButton",
+      l10n_util::GetString(IDS_AUTOFILL_DELETE_BUTTON));
+  localized_strings->SetString(L"helpButton",
+      l10n_util::GetString(IDS_AUTOFILL_HELP_LABEL));
 }
 
 void AutoFillOptionsHandler::RegisterMessages() {
