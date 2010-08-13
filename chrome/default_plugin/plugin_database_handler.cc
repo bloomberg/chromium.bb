affa4ef36b9af09f44ef8cda7a89e86d6087c252
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-13 23:51:21 +0000

    Build fix.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56111 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 401cb18c451d41541e8baf444e093daf74e8f5df

--- chrome/default_plugin/plugin_database_handler.cc
+++ chrome/default_plugin/plugin_database_handler.cc
@@ -4,13 +4,8 @@
 
 #include "chrome/default_plugin/plugin_database_handler.h"
 
-#if defined(USE_SYSTEM_LIBXML)
-#include <parser.h>
-#include <xpath.h>
-#else
-#include "third_party/libxml/include/libxml/parser.h"
-#include "third_party/libxml/include/libxml/xpath.h"
-#endif
+#include "libxml/parser.h"
+#include "libxml/xpath.h"
 
 #include "base/file_util.h"
 #include "base/path_service.h"
