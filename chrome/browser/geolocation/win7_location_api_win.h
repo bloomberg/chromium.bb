b2e2016ab716d4b56a571285e391210c4aa3b087
allanwoj@chromium.org <allanwoj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-17 12:21:22 +0000

    TBR: I didn't upload the six files that I added last time round see here http://src.chromium.org/viewvc/chrome?view=rev&revision=56332 BUG=45535
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56334 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f9f23a78dc32653b41b716110d3cc7e6d0363607

---
+++ chrome/browser/geolocation/win7_location_api_win.h
@@ -0,0 +1,60 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_GEOLOCATION_WIN7_LOCATION_API_WIN_H_
+#define CHROME_BROWSER_GEOLOCATION_WIN7_LOCATION_API_WIN_H_
+
+#include <atlbase.h>
+#include <atlcom.h>
+#include <locationapi.h>
+#include <sensors.h>
+#include <vector>
+#include <Windows.h>
+
+#include "base/scoped_ptr.h"
+#include "base/time.h"
+#include "base/win_util.h"
+
+struct Geoposition;
+
+// PropVariantToDouble
+typedef HRESULT (WINAPI* PropVariantToDoubleFunction)
+    (REFPROPVARIANT propvarIn, DOUBLE *pdblRet);
+
+class Win7LocationApi {
+ public:
+  // Public for testing. See Create() below for normal usage.
+  Win7LocationApi(HINSTANCE prop_library,
+                  PropVariantToDoubleFunction PropVariantToDouble_function,
+                  ILocation* locator);
+  virtual ~Win7LocationApi();
+
+  // Attempts to load propsys.dll, initialise |location_| and requests the user
+  // for access to location information. Creates and returns ownership of an
+  // instance of Win7LocationApi if all succeed.
+  static Win7LocationApi* Create();
+  // Gives the best available position.
+  // Returns false if no valid position is available.
+  virtual void GetPosition(Geoposition* position);
+  // Changes the "accuracy" needed. Affects power levels of devices.
+  virtual bool SetHighAccuracy(bool acc);
+
+ private:
+  // Provides the best position fix if one is available.
+  // Does this by requesting a location report and querying it to obtain
+  // location information.
+  virtual bool GetPositionIfFixed(Geoposition* position);
+
+  // ILocation object that lets us communicate with the Location and
+  // Sensors platform.
+  CComPtr<ILocation> locator_;
+  // Holds the opened propsys.dll library that is passed on construction.
+  // This class is responsible for closing it.
+  HINSTANCE prop_lib_;
+  PropVariantToDoubleFunction PropVariantToDouble_function_;
+  
+  DISALLOW_COPY_AND_ASSIGN(Win7LocationApi);
+};
+
+#endif  // CHROME_BROWSER_GEOLOCATION_WIN7_LOCATION_API_WIN_H_
