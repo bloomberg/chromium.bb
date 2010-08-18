f460a2b3426555033a1ee43ae80976ce3f896c4c
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-18 16:25:52 +0000

    DOMUI prefs: Move inline header list class into its own file for reuse.
    Moves some corresponding CSS into the shared options CSS file as well; all of this will be re-used by other pref panes.
    Also splits the search engine list class into a separate file.
    
    BUG=None
    TEST=Search engine manager should continue to work.
    
    Review URL: http://codereview.chromium.org/3153019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56537 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8226fab91ba07240ba9b513606f8d3ffeed0ce09

---
+++ chrome/browser/resources/options/search_engine_manager_engine_list.js
@@ -0,0 +1,96 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+cr.define('options.search_engines', function() {
+  const List = cr.ui.List;
+  const ListInlineHeaderSelectionController =
+      options.ListInlineHeaderSelectionController;
+  const ListItem = cr.ui.ListItem;
+
+  /**
+   * Creates a new search engine list item.
+   * @param {Object} searchEnigne The search engine this represents.
+   * @constructor
+   * @extends {cr.ui.ListItem}
+   */
+  function SearchEngineListItem(searchEngine) {
+    var el = cr.doc.createElement('div');
+    el.searchEngine_ = searchEngine;
+    SearchEngineListItem.decorate(el);
+    return el;
+  }
+
+  /**
+   * Decorates an element as a search engine list item.
+   * @param {!HTMLElement} el The element to decorate.
+   */
+  SearchEngineListItem.decorate = function(el) {
+    el.__proto__ = SearchEngineListItem.prototype;
+    el.decorate();
+  };
+
+  SearchEngineListItem.prototype = {
+    __proto__: ListItem.prototype,
+
+    /** @inheritDoc */
+    decorate: function() {
+      ListItem.prototype.decorate.call(this);
+
+      var engine = this.searchEngine_;
+      if (engine['heading']) {
+        var titleEl = this.ownerDocument.createElement('div');
+        titleEl.textContent = engine['heading'];
+        this.classList.add('heading');
+        this.appendChild(titleEl);
+      } else {
+        var nameEl = this.ownerDocument.createElement('div');
+        nameEl.className = 'name';
+        nameEl.textContent = engine['name'];
+        nameEl.style.backgroundImage = url('chrome://favicon/iconurl/' +
+                                           engine['iconURL']);
+
+        var keywordEl = this.ownerDocument.createElement('div');
+        keywordEl.className = 'keyword';
+        keywordEl.textContent = engine['keyword'];
+
+        this.appendChild(nameEl);
+        this.appendChild(keywordEl);
+
+        if (engine['default'])
+          this.classList.add('default');
+      }
+    },
+  };
+
+  var SearchEngineList = cr.ui.define('list');
+
+  SearchEngineList.prototype = {
+    __proto__: List.prototype,
+
+    /** @inheritDoc */
+    createItem: function(searchEngine) {
+      return new SearchEngineListItem(searchEngine);
+    },
+
+    /** @inheritDoc */
+    createSelectionController: function(sm) {
+      return new ListInlineHeaderSelectionController(sm, this);
+    },
+
+    /**
+     * Returns true if the given item is selectable.
+     * @param {number} index The index to check.
+     */
+    canSelectIndex: function(index) {
+      return !this.dataModel.item(index).hasOwnProperty('heading');
+    }
+  };
+
+  // Export
+  return {
+    SearchEngineList: SearchEngineList
+  };
+
+});
+
