e4858a6a2b4e7df002e275858be4758af361c5cc
cmasone@google.com <cmasone@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-18 15:47:50 +0000

    [Chrome OS] Block the installation of extensions with NPAPI plugins
    BUG=chromium-os:5454
    TEST=try to install http://www.3djam.com/roozz/roozz-plugin.crx, see that it is rejected.  Try to install https://chrome.google.com/extensions/detail/gofhjkjmkpinhpoiabjplobcaignabnl, see that it works.  And...unit tests
    
    Review URL: http://codereview.chromium.org/3163013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@56529 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 270eb8f84ec765617bab57c3af4c86763c3fc5a0

--- chrome/common/extensions/extension_unittest.cc
+++ chrome/common/extensions/extension_unittest.cc
@@ -833,7 +833,11 @@ TEST(ExtensionTest, IsPrivilegeIncrease) {
     { "permissions4", false },  // plugin -> plugin,tabs
     { "plugin1", false },  // plugin -> plugin
     { "plugin2", false },  // plugin -> none
+#if defined(OS_CHROMEOS)
+    { "plugin3", false },  // none -> plugin (illegal on Chrome OS)
+#else
     { "plugin3", true },  // none -> plugin
+#endif
     { "storage", false },  // none -> storage
     { "notifications", false }  // none -> notifications
   };
