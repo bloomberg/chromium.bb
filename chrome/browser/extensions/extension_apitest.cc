a60105f024b4afd06bf26ab8763e574273f0e3cc
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-22 04:05:26 +0000

    Whitespace change to trigger the bots.
    BUG=things aren't as green as they should be
    TEST=things get greener
    
    Review URL: http://codereview.chromium.org/3125035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57011 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d11347da8113fe0fa7b3e00219f112e3700e172f

--- chrome/browser/extensions/extension_apitest.cc
+++ chrome/browser/extensions/extension_apitest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -82,7 +82,7 @@ bool ExtensionApiTest::RunExtensionTestImpl(const char* extension_name,
     return false;
   }
 
-  // If there is a subtest to load, navigate to teh subtest page.
+  // If there is a subtest to load, navigate to the subtest page.
   if (!subtest_page.empty()) {
     Extension* extension = GetSingleLoadedExtension();
     if (!extension)
