41a27c6d00f3f1f9347ed42e5a9b121e36840602
Christian Egli <christian.egli@alumni.ethz.ch>
2010-08-23 12:20:53 +0000

    Handle two tests more elegantly.

--- tests/lastworditalafter.c
+++ tests/lastworditalafter.c
@@ -6,19 +6,22 @@
 int
 main(int argc, char **argv)
 {
+
+  int result = 0;
+
   /* First check italics with the English table */
   const char *str      = "He said it wasn't always working as expected.";
   const char *typeform = "110000000000000000111111111111110000000000000";
   const char *expected = ".,he sd x wasn't .alw .\"w+ z expect$4";
 
-  int result1 = check_translation(TRANSLATION_TABLE, str, typeform, expected);
+  result |= check_translation(TRANSLATION_TABLE, str, typeform, expected);
 
-  /* Then check the German table */
+  /* Then check a test table that defines lastworditalafter */
   str      = "Er sagte es funktioniere nicht immer wie erwartet.";
   typeform = "11000000000011111111111111111100000000000000000000";
   expected = "_ER SAGTE ES __FUNKTION0RE NI4T', IMMER W0 ERWARTET.";
 
-  int result2 = check_translation("de-ch-g1.ctb", str, typeform, expected);
+  result |= check_translation("lastworditalafter.ctb", str, typeform, expected);
 
-  return result1 || result2;
+  return result;
 }
