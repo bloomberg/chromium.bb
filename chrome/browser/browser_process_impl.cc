e6bf676e511b4533f492eb0ab7f4c0897f047ede
prasadt@chromium.org <prasadt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-24 16:52:53 +0000

    Autorestart on update should happen when (among other things), the browser is in KeepAlive mode.  The condition to check that in CanAutorestartForUpdate() is reversed.
    BUG=none
    TEST=On Linux - Change BrowserProcessImpl::StartAutoupdateTimer() function to
    change the timer from 6hrs to 30 seconds.  Build and run chrome with
    --no-startup-window and --keep-alive-for-test flags.  Run "touch" on browser
    exe to fake an update.  Wait for 30 seconds and verify that the browser shuts
    down and restarts.
    
    Review URL: http://codereview.chromium.org/3206003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57197 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c10fa39515fee77141ba42b07056fecf2f07fd95

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -717,7 +717,7 @@ void BrowserProcessImpl::DoInspectorFilesCheck() {
 bool BrowserProcessImpl::CanAutorestartForUpdate() const {
   // Check if browser is in the background and if it needs to be restarted to
   // apply a pending update.
-  return BrowserList::size() == 0 && !BrowserList::WillKeepAlive() &&
+  return BrowserList::size() == 0 && BrowserList::WillKeepAlive() &&
          Upgrade::IsUpdatePendingRestart();
 }
 
