7e10d15275d845279814aba1886de935e68efa84
bsy@google.com <bsy@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-08-25 21:02:07 +0000

    changed osx platform semaphore implementation to unlink the semaphore after creation.  this reduces the timing window for semaphore leaks.
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=686
    
    Review URL: http://codereview.chromium.org/3115027
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3119 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 041a5dd49fb7773a2c701756e01031d4fbe986ed

--- src/shared/platform/osx/nacl_semaphore.c
+++ src/shared/platform/osx/nacl_semaphore.c
@@ -17,22 +17,35 @@
 #include "native_client/src/shared/platform/nacl_log.h"
 #include "native_client/src/shared/platform/nacl_sync.h"
 #include "native_client/src/shared/platform/osx/nacl_semaphore.h"
+/*
+* NOTE(gregoryd): following Gears in defining SEM_NAME_LEN:
+* Gears: The docs claim that SEM_NAME_LEN should be defined.  It is not.
+* However, by looking at the xnu source (bsd/kern/posix_sem.c),
+* it defines PSEMNAMLEN to be 31 characters.  We'll use that value.
+*/
+#define SEM_NAME_LEN 31
 
 int NaClSemCtor(struct NaClSemaphore *sem, int32_t value) {
+  /*
+   * There are 62^30 possible names - should be enough.
+   * 62 = 26*2 + 10 - the number of alphanumeric characters
+   * 30 = SEM_NAME_LEN - 1
+   */
+  char sem_name[SEM_NAME_LEN];
 
   do {
-    NaClGenerateRandomPath(&sem->sem_name[0], SEM_NAME_LEN);
-    sem->sem_descriptor = sem_open(sem->sem_name, O_CREAT | O_EXCL, 700, value);
+    NaClGenerateRandomPath(&sem_name[0], SEM_NAME_LEN);
+    sem->sem_descriptor = sem_open(sem_name, O_CREAT | O_EXCL, 700, value);
   } while ((SEM_FAILED == sem->sem_descriptor) && (EEXIST == errno));
   if (SEM_FAILED == sem->sem_descriptor) {
     return 0;
   }
+  sem_unlink(sem_name);
   return 1;
 }
 
 void NaClSemDtor(struct NaClSemaphore *sem) {
   sem_close(sem->sem_descriptor);
-  sem_unlink(sem->sem_name);
 }
 
 NaClSyncStatus NaClSemWait(struct NaClSemaphore *sem) {
