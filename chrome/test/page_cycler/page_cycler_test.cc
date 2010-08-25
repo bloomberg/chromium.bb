d25e3c54ca1797914c503bef035dd313f8f20c69
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-25 00:29:50 +0000

    Disable page_cycler_database on Mac. It looks like the perf bots are having some problems, and on Mac, those problems show up the most when running this test suite.
    TEST=none
    BUG=53140
    
    Review URL: http://codereview.chromium.org/3119042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57265 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0ecd8f0d151973e110cac36055a2c939a73fd5b2

--- chrome/test/page_cycler/page_cycler_test.cc
+++ chrome/test/page_cycler/page_cycler_test.cc
@@ -499,7 +499,9 @@ PAGE_CYCLER_HTTP_TESTS("bloat", BloatHttp);
 
 // HTML5 database tests
 // These tests are _really_ slow on XP/Vista.
-#if !defined(OS_WIN)
+// crbug.com/53140: The Mac perf bot is not happy with this test suite either,
+// even though it doesn't seem to be a problem related to this test suite.
+#if !defined(OS_WIN) && !defined(OS_MAC)
 PAGE_CYCLER_DATABASE_TESTS("select-transactions",
                            SelectTransactions);
 PAGE_CYCLER_DATABASE_TESTS("select-readtransactions",
