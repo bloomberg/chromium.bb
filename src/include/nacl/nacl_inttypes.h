756d29e6840306e9837784db262aecdbb562830c
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-08-25 16:33:59 +0000

    Tidy nacl_inttypes.h in preparation for making it work with nacl-glibc
    Add an #include guard.  Though there were apparently problems with
    using an #include guard before, it seems to work OK now.
    
    This means that the #undefs are not needed.
    
    Change comment to clarify the newlib problem.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=617
    TEST=python build.py -sb libnacl_headers-input libnacl_headers libnacl_x86_32-input libnacl_x86_32
    
    Review URL: http://codereview.chromium.org/3124043
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3110 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 6528bd05a310f32a4908faa44c4bd31c53b11e5d

--- src/include/nacl/nacl_inttypes.h
+++ src/include/nacl/nacl_inttypes.h
@@ -30,10 +30,8 @@
  */
 
 
-/*
- * We do not want an include guard on this file, as this file needs to define
- * NACL_PRI*32 the last time and may be included through multiple paths.
- */
+#ifndef NATIVE_CLIENT_SRC_INCLUDE_NACL_NACL_INTTYPES_H_
+#define NATIVE_CLIENT_SRC_INCLUDE_NACL_NACL_INTTYPES_H_
 
 /*
  * printf macros for size_t, in the style of inttypes.h.  This is
@@ -48,117 +46,38 @@
 # endif
 #endif
 
-#ifdef NACL_PRIdS
-#undef NACL_PRIdS
-#endif
 #define NACL_PRIdS NACL__PRIS_PREFIX "d"
-
-#ifdef NACL_PRIiS
-#undef NACL_PRIiS
-#endif
 #define NACL_PRIiS NACL__PRIS_PREFIX "i"
-
-#ifdef NACL_PRIoS
-#undef NACL_PRIoS
-#endif
 #define NACL_PRIoS NACL__PRIS_PREFIX "o"
-
-#ifdef NACL_PRIuS
-#undef NACL_PRIuS
-#endif
 #define NACL_PRIuS NACL__PRIS_PREFIX "u"
-
-#ifdef NACL_PRIxS
-#undef NACL_PRIxS
-#endif
 #define NACL_PRIxS NACL__PRIS_PREFIX "x"
-
-#ifdef NACL_PRIXS
-#undef NACL_PRIXS
-#endif
 #define NACL_PRIXS NACL__PRIS_PREFIX "X"
 
 /*
- * Newlib stdint.h defines int32_t as unsigned long int for nacl, while
- * newlib inttypes.h seems to think that int32_t is unsigned int.
+ * This works around a bug in nacl-newlib.  Newlib's stdint.h defines
+ * uint32_t as unsigned long int for NaCl, while newlib's inttypes.h
+ * seems to think that uint32_t is unsigned int.  This means that
+ * using newlib's PRIu32 causes a -Wformat warning from gcc.
  */
 
-#ifdef NACL_PRId32
-#undef NACL_PRId32
-#endif
 #define NACL_PRId32 "ld"
-
-#ifdef NACL_PRIi32
-#undef NACL_PRIi32
-#endif
 #define NACL_PRIi32 "li"
-
-#ifdef NACL_PRIo32
-#undef NACL_PRIo32
-#endif
 #define NACL_PRIo32 "lo"
-
-#ifdef NACL_PRIu32
-#undef NACL_PRIu32
-#endif
 #define NACL_PRIu32 "lu"
-
-#ifdef NACL_PRIx32
-#undef NACL_PRIx32
-#endif
 #define NACL_PRIx32 "lx"
-
-#ifdef NACL_PRIX32
-#undef NACL_PRIX32
-#endif
 #define NACL_PRIX32 "lX"
 
-#ifdef NACL_PRId64
-#undef NACL_PRId64
-#endif
 #define NACL_PRId64 "lld"
-
-#ifdef NACL_PRIu64
-#define NACL_PRIu64
-#endif
 #define NACL_PRIu64 "llu"
-
-#ifdef NACL_PRIx64
-#undef NACL_PRIx64
-#endif
 #define NACL_PRIx64 "llx"
-
-#ifdef NACL_PRIX64
-#undef NACL_PRIX64
-#endif
 #define NACL_PRIX64 "llX"
 
-#ifdef NACL_PRId16
-#undef NACL_PRId16
-#endif
 #define NACL_PRId16 "d"
-
-#ifdef NACL_PRIu16
-#undef NACL_PRIu16
-#endif
 #define NACL_PRIu16 "u"
-
-#ifdef NACL_PRIx16
-#undef NACL_PRIx16
-#endif
 #define NACL_PRIx16 "x"
 
-#ifdef NACL_PRId8
-#undef NACL_PRId8
-#endif
 #define NACL_PRId8 "d"
-
-#ifdef NACL_PRIu8
-#undef NACL_PRIu8
-#endif
 #define NACL_PRIu8 "u"
-
-#ifdef NACL_PRIx8
-#undef NACL_PRIx8
-#endif
 #define NACL_PRIx8 "x"
+
+#endif
