8b351acb947a5ff06baebc22439c784f9e82e675
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-25 21:20:08 +0000

    Fix browser hang when blocking hulu pop-out
    Flash while drawing the movie frames cannot handle being sized 0-by-0 and
    a storm of math exception happen which prevents flash from pumping messages
    
    BUG=52754
    TEST=see bug
    
    
    Review URL: http://codereview.chromium.org/3132043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57388 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1cf2d256335a6a4e56837b85de0e486337c2405b

--- chrome/browser/blocked_popup_container.cc
+++ chrome/browser/blocked_popup_container.cc
@@ -3,8 +3,9 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/blocked_popup_container.h"
-
 #include "chrome/browser/tab_contents/tab_contents.h"
+#include "chrome/browser/tab_contents/tab_contents_view.h"
+
 #include "gfx/rect.h"
 
 // static
@@ -32,6 +33,12 @@ void BlockedPopupContainer::AddTabContents(TabContents* tab_contents,
         "be possible. Renderer compromised?";
     return;
   }
+
+  // BUG:52754 Super hack time. If we supress a popup that has flash 10.1
+  // playing a movie, it cannot handle being zero-sized: an unending series
+  // of divide by zero exceptions are thrown (and ignored) inside the flash
+  // in the UI thread thereby hanging the plugin and in turn hanging us.
+  tab_contents->view()->SizeContents(gfx::Size(10, 10));
 
   blocked_popups_.push_back(BlockedPopup(tab_contents, bounds));
   tab_contents->set_delegate(this);
