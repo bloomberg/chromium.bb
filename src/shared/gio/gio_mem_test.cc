c7997f02574464c192dfe491dcb53cac2b59a0b0
jvoung@google.com <jvoung@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-08-25 20:25:05 +0000

    Test file-backed gio functions (not just memory-backed gio funcs). - Refactor testing to share test code/expectations between file and memory implementations of gio. - Exercise close and dtor while we are at it. - Not excercising flush. - Exercising write on file-backed version by using temp file. - Exercising read/seek on file-backed version with premade files.
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=758
    TEST=run_gio_test run_gio_mem_test
    
    Review URL: http://codereview.chromium.org/3113022
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3116 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 200475e631cfffd0a90da9b62bcdec8d69375043

--- src/shared/gio/gio_mem_test.cc
+++ src/shared/gio/gio_mem_test.cc
@@ -6,169 +6,62 @@
 
 
 #include "native_client/src/shared/gio/gio.h"
+#include "native_client/src/shared/gio/gio_test_base.h"
 #include "gtest/gtest.h"
 
 namespace {
 
 TEST(GioMemTest, ReadTest) {
   struct GioMemoryFile mf;
-  char *in_buffer;
-  int in_size = 32;
-  char *out_buffer;
-  int out_size = 16;
-  ssize_t ret_code;
+  char* in_buffer;
+  int in_size = expected_file_size;
+  int ret_code;
 
-  in_buffer = (char*) malloc(in_size);
-  for (int i = 0; i < in_size; ++i)
-    in_buffer[i]=i+'A';
+  in_buffer = reinterpret_cast<char*>(malloc(in_size));
+  GioInitTestMemFile(in_buffer, 'A', in_size);
+  ret_code = GioMemoryFileCtor(&mf, in_buffer, in_size);
+  EXPECT_EQ(1, ret_code);
 
-  GioMemoryFileCtor(&mf, in_buffer, in_size);
-
-  out_buffer = (char*) malloc(out_size);
-
-  // mf_curpos = 0, 32 left, read 16
-  ret_code = GioMemoryFileRead(&mf.base, out_buffer, 16);
-  EXPECT_EQ(16, ret_code);
-  for (int i = 0; i < 16; ++i)
-    EXPECT_EQ(i+'A', out_buffer[i]);
-
-  // mf_curpos = 16, 16 left, read 10
-  ret_code = GioMemoryFileRead(&mf.base, out_buffer, 10);
-  EXPECT_EQ(10, ret_code);
-  for (int i = 0; i < 10; ++i)
-    EXPECT_EQ(16+i+'A', out_buffer[i]);
-  // residual value after idx 10
-  EXPECT_EQ(10+'A', out_buffer[10]);
-
-  // mf_curpos = 26, 6 left, read 8
-  ret_code = GioMemoryFileRead(&mf.base, out_buffer, 8);
-  EXPECT_EQ(6, ret_code);
-  for (int i = 0; i < 6; ++i)
-    EXPECT_EQ(26+i+'A', out_buffer[i]);
-  // residual value after idx 6
-  EXPECT_EQ(22+'A', out_buffer[6]);
-
-  // mf_curpos = 32, 0 left, read 16
-  ret_code = GioMemoryFileRead(&mf.base, out_buffer, 16);
-  EXPECT_EQ(0, ret_code);
+  GioReadTestWithOffset(&mf.base, 'A');
+  GioCloseTest(&mf.base);
+  free(in_buffer);
 }
 
 TEST(GioMemTest, WriteTest) {
   struct GioMemoryFile mf;
-  char *mf_buffer;
+  char* mf_buffer;
   int mf_size = 64;
-  char *in_buffer;
-  int in_size = 32;
-  char out_char;
-  ssize_t ret_code;
+  int ret_code;
 
-  mf_buffer = (char *) malloc(mf_size);
-  EXPECT_NE((void*)NULL, mf_buffer);
+  mf_buffer = reinterpret_cast<char*>(malloc(mf_size));
+  EXPECT_NE(reinterpret_cast<char*>(NULL), mf_buffer);
 
-  GioMemoryFileCtor(&mf, mf_buffer, mf_size);
-
-  in_buffer = (char*) malloc(in_size);
-  for (int i = 0; i < in_size; ++i)
-    in_buffer[i] = i;
-
-  // mf_curpos = 0, 64 left, write 32
-  ret_code = GioMemoryFileWrite(&mf.base, in_buffer, 32);
-  EXPECT_EQ(32, ret_code);
-
-  GioMemoryFileSeek(&mf.base, -1, SEEK_CUR);
-  GioMemoryFileRead(&mf.base, &out_char, 1);
-  EXPECT_EQ(31, out_char);
-
-  // mf_curpos = 32, 32 left, write 20
-  ret_code = GioMemoryFileWrite(&mf.base, in_buffer, 20);
-  EXPECT_EQ(20, ret_code);
-
-  GioMemoryFileSeek(&mf.base, -1, SEEK_CUR);
-  GioMemoryFileRead(&mf.base, &out_char, 1);
-  EXPECT_EQ(19, out_char);
-
-  // mf_curpos = 52, 12 left, write 20
-  ret_code = GioMemoryFileWrite(&mf.base, in_buffer, 20);
-  EXPECT_EQ(12, ret_code);
-
-  GioMemoryFileSeek(&mf.base, -1, SEEK_CUR);
-  GioMemoryFileRead(&mf.base, &out_char, 1);
-  EXPECT_EQ(11, out_char);
-
-  // mf_curpos = 64, 0 left, write 20
-  ret_code = GioMemoryFileWrite(&mf.base, in_buffer, 20);
-  EXPECT_EQ(0, ret_code);
+  ret_code = GioMemoryFileCtor(&mf, mf_buffer, mf_size);
+  EXPECT_EQ(1, ret_code);
+  GioWriteTest(&mf.base, true);
+  GioCloseTest(&mf.base);
+  free(mf_buffer);
 }
 
 TEST(GioMemTest, SeekTest) {
   struct GioMemoryFile mf;
-  char *in_buffer;
-  int in_size = 32;
-  char out_char;
-  ssize_t ret_code;
+  char* in_buffer;
+  int in_size = expected_file_size;
+  int ret_code;
 
-  in_buffer = (char*) malloc(in_size);
-  for (int i = 0; i < in_size; ++i)
-    in_buffer[i] = i;
-
-  GioMemoryFileCtor(&mf, in_buffer, in_size);
-
-  // mf_curpos = 0
-  ret_code = GioMemoryFileSeek(&mf.base, 15, SEEK_SET);
-  EXPECT_EQ(15, ret_code);
-
-  ret_code = GioMemoryFileRead(&mf.base, &out_char, 1);
+  in_buffer = reinterpret_cast<char*>(malloc(in_size));
+  GioInitTestMemFile(in_buffer, 0, in_size);
+  ret_code = GioMemoryFileCtor(&mf, in_buffer, in_size);
   EXPECT_EQ(1, ret_code);
-  EXPECT_EQ(15, out_char);
 
-  // mf_curpos = 16
-  ret_code = GioMemoryFileSeek(&mf.base, 4, SEEK_CUR);
-  EXPECT_EQ(20, ret_code);
-
-  ret_code = GioMemoryFileRead(&mf.base, &out_char, 1);
-  EXPECT_EQ(1, ret_code);
-  EXPECT_EQ(20, out_char);
-
-  // mf_curpos = 21
-  ret_code = GioMemoryFileSeek(&mf.base, -4, SEEK_CUR);
-  EXPECT_EQ(17, ret_code);
-
-  ret_code = GioMemoryFileRead(&mf.base, &out_char, 1);
-  EXPECT_EQ(1, ret_code);
-  EXPECT_EQ(17, out_char);
-
-  // mf_curpos = 17
-  ret_code = GioMemoryFileSeek(&mf.base, -4, SEEK_END);
-  EXPECT_EQ(28, ret_code);
-
-  GioMemoryFileRead(&mf.base, &out_char, 1);
-  EXPECT_EQ(28, out_char);
-
-  // mf_curpos = 29
-  ret_code = GioMemoryFileSeek(&mf.base, 4, SEEK_END);
-  EXPECT_EQ(-1, ret_code);
-
-  GioMemoryFileRead(&mf.base, &out_char, 1);
-  EXPECT_EQ(29, out_char);
-
-  // mf_curpos = 30
-  ret_code = GioMemoryFileSeek(&mf.base, 4, SEEK_END+3);
-  EXPECT_EQ(-1, ret_code);
-
-  GioMemoryFileRead(&mf.base, &out_char, 1);
-  EXPECT_EQ(30, out_char);
-
-  // mf_curpos = 31
-  ret_code = GioMemoryFileSeek(&mf.base, -5, SEEK_SET);
-  EXPECT_EQ(-1, ret_code);
-
-  GioMemoryFileRead(&mf.base, &out_char, 1);
-  EXPECT_EQ(31, out_char);
+  GioSeekTestWithOffset(&mf.base, 0, true);
+  GioCloseTest(&mf.base);
+  free(in_buffer);
 }
 
 }  // namespace
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
