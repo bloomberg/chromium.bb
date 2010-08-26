70b1746dba539758947e55203afc573f53a4dcce
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-08-26 18:35:37 +0000

    Moved chrome specific autotests to chromium repository to avoid circular dependencies on autotest repo in chromeos builds.
    TEST=make sure chrome tests still run in autotest framework on chromeos
    BUG=none
    
    Review URL: http://codereview.chromium.org/3185029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@57556 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2b131972ac332e343d70dcf363f444fe3767e9c9

---
+++ chrome/test/chromeos/autotest/files/client/deps/chrome_test/setup_test_links.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# A script to setup symbolic links needed for Chrome's automated UI tests.
+
+ln -f -s /opt/google/chrome/chrome $(dirname $0)/chrome
+ln -f -s /opt/google/chrome/chrome.pak $(dirname $0)/chrome.pak
+ln -f -s /opt/google/chrome/locales $(dirname $0)/locales
+ln -f -s /opt/google/chrome/resources $(dirname $0)/resources
+
+mkdir -p $(dirname $0)/chromeos
+ln -f -s /opt/google/chrome/chromeos/libcros.so \
+    $(dirname $0)/chromeos/libcros.so
