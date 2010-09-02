f6a633936b8e4cba1838c839b616cdaecb8abdf5
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-02 20:45:55 +0000

    Spelling error: s/ADDRESSSES/ADDRESSES/g.
    Review URL: http://codereview.chromium.org/3330006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58398 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d3aaf3f407776ab62684a6362d8e175770ed43e2

--- chrome/browser/io_thread.cc
+++ chrome/browser/io_thread.cc
@@ -110,7 +110,7 @@ class LoggingNetworkChangeObserver
   virtual void OnIPAddressChanged() {
     LOG(INFO) << "Observed a change to the network IP addresses";
 
-    net_log_->AddEntry(net::NetLog::TYPE_NETWORK_IP_ADDRESSSES_CHANGED,
+    net_log_->AddEntry(net::NetLog::TYPE_NETWORK_IP_ADDRESSES_CHANGED,
                        base::TimeTicks::Now(),
                        net::NetLog::Source(),
                        net::NetLog::PHASE_NONE,
