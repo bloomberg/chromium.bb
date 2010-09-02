8e7e9c3338fb6ef748c09fa1710395db0cd3fa2d
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-02 19:07:46 +0000

    Revert "Work around download system breakpage by using WaitUntil on pyauto side."
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/3341008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58386 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5b3218d8ea9351c119474a4ef8eefeb8f488b2d3

--- chrome/test/functional/downloads.py
+++ chrome/test/functional/downloads.py
@@ -200,12 +200,8 @@ class DownloadsTest(pyauto.PyUITest):
       self.DownloadAndWaitForStart(file_url)
       # Waiting for big file to download might exceed automation timeout.
       # Temporarily increase the automation timeout.
-
-      # Temp workaround for crbug.com/54131
-      # self._CallFunctionWithNewTimeout(4 * 60 * 1000,  # 4 min.
-      #                                  self.WaitForAllDownloadsToComplete)
-      self.WaitForAllDownloadsToComplete(timeout=4*60*1000)
-      # -- End workaround
+      self._CallFunctionWithNewTimeout(4 * 60 * 1000,  # 4 min.
+                                       self.WaitForAllDownloadsToComplete)
       # Verify that the file was correctly downloaded
       self.assertTrue(os.path.exists(downloaded_pkg),
                       'Downloaded file %s missing.' % downloaded_pkg)
