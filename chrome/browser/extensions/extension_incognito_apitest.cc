06073ee01d292b24d475d347cbf70f82c6917ff8
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-03 23:49:39 +0000

    Re-disable IncognitoSplitMode. It still fails/hangs periodically (though more rare at least).
    BUG=53991
    TEST=no
    TBR=aa
    
    Review URL: http://codereview.chromium.org/3305011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@58561 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3b3dff737b90daa8c30b5584d204399bf6317515

--- chrome/browser/extensions/extension_incognito_apitest.cc
+++ chrome/browser/extensions/extension_incognito_apitest.cc
@@ -97,7 +97,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Incognito) {
 
 // Tests that the APIs in an incognito-enabled split-mode extension work
 // properly.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, IncognitoSplitMode) {
+// Hangy, http://crbug.com/53991.
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_IncognitoSplitMode) {
   host_resolver()->AddRule("*", "127.0.0.1");
   ASSERT_TRUE(test_server()->Start());
 
