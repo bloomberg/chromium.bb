b182839e8900e1416288d25d4381e9b62081c427
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-09-03 21:36:32 +0000

    Plugin test: Change spin.c to not rely on libsrpc's implicit __srpc_init()
    The next change will change __srpc_init() so that it does nothing when
    the executable contains no NACL_SRPC_METHOD declarations.  So change
    spin.c to accept the plugin's connection explicitly instead.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=863
    TEST=run_fake_browser_test
    
    Review URL: http://codereview.chromium.org/3326003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3202 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: dbec4ded8f061b974c8c60921cb47968f1ac33f8

--- tests/fake_browser/spin.c
+++ tests/fake_browser/spin.c
@@ -4,7 +4,21 @@
  * be found in the LICENSE file.
  */
 
+#include <assert.h>
+#include <sys/nacl_syscalls.h>
+#include <unistd.h>
+
+
 int main() {
+  /* Accept the plugin's connection so that the plugin does not block
+     forever. */
+  int fd;
+  int rc;
+  fd = imc_accept(3);
+  assert(fd >= 0);
+  rc = close(fd);
+  assert(rc == 0);
+
   /* Run forever.  This is useful for testing that the plugin can shut
      down this process OK.  If this process is left behind by the
      plugin, it will be more obvious if it is consuming CPU than if it
