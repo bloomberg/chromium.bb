8601676f52e4b1268799a23fa6b5a6eb56310e5f
espindola@google.com <espindola@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-09-03 18:59:34 +0000

    Enable
    http://code.google.com/p/nacl-llvm-branches/source/detail?r=1e92a9c6c7ac1fcdc8b67f4bc81f55945b4ebc57
    
    and
    
    http://code.google.com/p/nacl-llvm-branches/source/detail?r=d5028f4b16b55045ca591752b3d806047437eedd
    
    Tested:
    
    ./tools/llvm/utman.sh clean
    ./tools/llvm/utman.sh all
    ./tools/llvm/utman.sh test-all
    
    
    Review URL: http://codereview.chromium.org/3363008
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3199 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 7e0e0495a56e6e87dc5158bc62897d1868c6d1e6

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -120,7 +120,7 @@ readonly PNACL_CLIENT_TC_X8632="${PNACL_CLIENT_TC_ROOT}/x8632"
 readonly PNACL_CLIENT_TC_X8664="${PNACL_CLIENT_TC_ROOT}/x8664"
 
 # Current milestones in each repo
-readonly LLVM_REV=d4438bd19bd8
+readonly LLVM_REV=1e92a9c6c7ac
 readonly LLVM_GCC_REV=6da5d51ee4ac
 readonly NEWLIB_REV=03ddd92d699d
 readonly BINUTILS_REV=0d86e38ba767
