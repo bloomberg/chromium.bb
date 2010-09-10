f2a2a745778905ef561d5612f48978e658e01f83
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-10 09:34:23 +0000

    Tiny cleanup: remove all use of kFallbackInputMethodId from the Chrome tree.
    BUG=chromium-os:6563
    TEST=ran emerge, ran chrome on the device
    
    Review URL: http://codereview.chromium.org/3340021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59077 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b5412500aec5289b385ddab77e6eb51863eecd98

--- chrome/browser/chromeos/preferences.cc
+++ chrome/browser/chromeos/preferences.cc
@@ -36,8 +36,10 @@ void Preferences::RegisterUserPrefs(PrefService* prefs) {
                             language_prefs::kHotkeyPreviousEngine);
   prefs->RegisterStringPref(prefs::kLanguagePreferredLanguages,
                             kFallbackInputMethodLocale);
+
+  KeyboardLibrary* keyboard_library = CrosLibrary::Get()->GetKeyboardLibrary();
   prefs->RegisterStringPref(prefs::kLanguagePreloadEngines,
-                            kFallbackInputMethodId);  // EN layout
+                            keyboard_library->GetHardwareKeyboardLayoutName());
   for (size_t i = 0; i < language_prefs::kNumChewingBooleanPrefs; ++i) {
     prefs->RegisterBooleanPref(
         language_prefs::kChewingBooleanPrefs[i].pref_name,
