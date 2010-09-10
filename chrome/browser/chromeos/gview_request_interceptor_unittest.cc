2f664355715f2309897fa559fc50f149338ee292
scottbyer@google.com <scottbyer@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-10 20:38:32 +0000

    Fix unit test for GView interceptor.
    Fix GView interceptor unit tests to match what is supposed to happen in the presence of the PDF internal plug-in on ChromeOS.
    
    BUG=51242
    TEST=GViewRequestInterceptorTest.DoNotInterceptPdfWhenEnabled, GViewRequestInterceptorTest.InterceptPdfWhenDisabled
    
    Review URL: http://codereview.chromium.org/3308016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59144 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6ece06fd803d64f8fcc4edec55e090940b46e4c2

--- chrome/browser/chromeos/gview_request_interceptor_unittest.cc
+++ chrome/browser/chromeos/gview_request_interceptor_unittest.cc
@@ -5,12 +5,15 @@
 #include <string>
 #include "base/message_loop.h"
 #include "chrome/browser/chromeos/gview_request_interceptor.h"
+#include "chrome/browser/plugin_service.h"
+#include "chrome/common/chrome_paths.h"
 #include "net/base/load_flags.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_job.h"
 #include "net/url_request/url_request_test_job.h"
 #include "net/url_request/url_request_unittest.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "webkit/glue/plugins/plugin_list.h"
 
 namespace chromeos {
 
@@ -49,6 +52,7 @@ class GViewRequestInterceptorTest : public testing::Test {
     URLRequest::RegisterProtocolFactory("http",
                                         &GViewRequestInterceptorTest::Factory);
     interceptor_ = GViewRequestInterceptor::GetGViewRequestInterceptor();
+    ASSERT_TRUE(PathService::Get(chrome::FILE_PDF_PLUGIN, &pdf_path_));
   }
 
   virtual void TearDown() {
@@ -61,10 +65,44 @@ class GViewRequestInterceptorTest : public testing::Test {
     return new GViewURLRequestTestJob(request);
   }
 
+  void RegisterPDFPlugin() {
+    NPAPI::PluginVersionInfo info;
+    info.path = pdf_path_;
+    memset(&info.entry_points, 0, sizeof(info.entry_points));
+    NPAPI::PluginList::Singleton()->RegisterInternalPlugin(info);
+    NPAPI::PluginList::Singleton()->RefreshPlugins();
+  }
+
+  void UnregisterPDFPlugin() {
+    NPAPI::PluginList::Singleton()->UnregisterInternalPlugin(pdf_path_);
+    NPAPI::PluginList::Singleton()->RefreshPlugins();
+  }
+
+  void SetPDFPluginLoadedState(bool want_loaded, bool* out_is_enabled) {
+    WebPluginInfo info;
+    bool is_loaded =
+        NPAPI::PluginList::Singleton()->GetPluginInfoByPath(pdf_path_, &info);
+    if (is_loaded && !want_loaded) {
+      UnregisterPDFPlugin();
+      is_loaded =
+          NPAPI::PluginList::Singleton()->GetPluginInfoByPath(pdf_path_, &info);
+    } else if (!is_loaded && want_loaded) {
+      // This "loads" the plug-in even if it's not present on the
+      // system - which is OK since we don't actually use it, just
+      // need it to be "enabled" for the test.
+      RegisterPDFPlugin();
+      is_loaded =
+          NPAPI::PluginList::Singleton()->GetPluginInfoByPath(pdf_path_, &info);
+    }
+    EXPECT_EQ(want_loaded, is_loaded);
+    *out_is_enabled = info.enabled;
+  }
+
  protected:
   MessageLoopForIO message_loop_;
   TestDelegate test_delegate_;
   URLRequest::Interceptor* interceptor_;
+  FilePath pdf_path_;
 };
 
 TEST_F(GViewRequestInterceptorTest, DoNotInterceptHtml) {
@@ -84,7 +122,46 @@ TEST_F(GViewRequestInterceptorTest, DoNotInterceptDownload) {
   EXPECT_EQ(GURL("http://foo.com/file.pdf"), request.url());
 }
 
-TEST_F(GViewRequestInterceptorTest, FAILS_InterceptPdf) {
+TEST_F(GViewRequestInterceptorTest, DoNotInterceptPdfWhenEnabled) {
+  bool enabled;
+  SetPDFPluginLoadedState(true, &enabled);
+
+  if (!enabled) {
+    bool pdf_plugin_enabled =
+        NPAPI::PluginList::Singleton()->EnablePlugin(pdf_path_);
+    EXPECT_TRUE(pdf_plugin_enabled);
+  }
+
+  URLRequest request(GURL("http://foo.com/file.pdf"), &test_delegate_);
+  request.Start();
+  MessageLoop::current()->Run();
+  EXPECT_EQ(0, test_delegate_.received_redirect_count());
+  EXPECT_EQ(GURL("http://foo.com/file.pdf"), request.url());
+}
+
+TEST_F(GViewRequestInterceptorTest, InterceptPdfWhenDisabled) {
+  bool enabled;
+  SetPDFPluginLoadedState(true, &enabled);
+
+  if (enabled) {
+    bool pdf_plugin_disabled =
+        NPAPI::PluginList::Singleton()->DisablePlugin(pdf_path_);
+    EXPECT_TRUE(pdf_plugin_disabled);
+  }
+
+  URLRequest request(GURL("http://foo.com/file.pdf"), &test_delegate_);
+  request.Start();
+  MessageLoop::current()->Run();
+  EXPECT_EQ(1, test_delegate_.received_redirect_count());
+  EXPECT_EQ(
+      GURL("http://docs.google.com/gview?url=http%3A//foo.com/file.pdf"),
+      request.url());
+}
+
+TEST_F(GViewRequestInterceptorTest, InterceptPdfWithNoPlugin) {
+  bool enabled;
+  SetPDFPluginLoadedState(false, &enabled);
+
   URLRequest request(GURL("http://foo.com/file.pdf"), &test_delegate_);
   request.Start();
   MessageLoop::current()->Run();
