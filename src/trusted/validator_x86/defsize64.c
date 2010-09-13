dd42d2d39e402b1460eb7784e153401099cbe3aa
kschimpf@google.com <kschimpf@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-09-13 17:01:31 +0000

    Fix the table for instructions which assume effective operand size is 64 by default, as defined in the ADM manual.
    Review URL: http://codereview.chromium.org/3308024
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3274 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 5473ec7dca2b40a711a1f1bd19da567b26d20bbd

--- src/trusted/validator_x86/defsize64.c
+++ src/trusted/validator_x86/defsize64.c
@@ -9,8 +9,9 @@
  * instead of 32 bits. That is, effective operand size is 64 bits,
  * even without a rex prefix.
  *
- * TODO(karl) Find a manual reference for how entries are defined
- * in this table.
+ * The tables below are from Table B-5 of Appendex B.4 in AMD document
+ * 24594-Rev.3.14-September 2007, "AMD64 Architecture Programmer's manual
+ * Volume 3: General-Purpose and System Instructions".
  */
 
 #include "native_client/src/trusted/validator_x86/zero_extends.h"
@@ -19,7 +20,60 @@
 #include "native_client/src/trusted/validator_x86/ncdecode_forms.h"
 #include "native_client/src/trusted/validator_x86/ncdecode_tablegen.h"
 
-static const NaClNameOpcodeSeq kSizeDefaultIs64[] = {
+/* List of instruction mnemonics that assumes the default size is 64 bits,
+ * and the mnemonic is sufficient to disambiguate the case.
+ */
+static const NaClMnemonic kNameSizeDefaultIs64[] = {
+  InstEnter,          /* c8 */
+  InstJo,             /* 70, 0f 80 */
+  InstJno,            /* 71, 0f 81 */
+  InstJb,             /* 72, 0f 82 */
+  InstJnb,            /* 73, 0f 83 */
+  InstJz,             /* 74, 0f 84 */
+  InstJnz,            /* 75, 0f 85 */
+  InstJbe,            /* 76, 0f 86 */
+  InstJnbe,           /* 77, 0f 87 */
+  InstJs,             /* 78, 0f 88 */
+  InstJns,            /* 79, 0f 89 */
+  InstJp,             /* 7a, 0f 8a */
+  InstJnp,            /* 7b, 0f 8b */
+  InstJl,             /* 7c, 0f 8c */
+  InstJnl,            /* 7d, 0f 8d */
+  InstJle,            /* 7e, 0f 8e */
+  InstJnle,           /* 7f, 0f 8f */
+  InstJcxz,           /* e3 */
+  InstJecxz,          /* e3 */
+  InstJrcxz,          /* e3 */
+  InstLeave,          /* c9 */
+  InstLoop,           /* e2 */
+  InstLoopne,         /* e0 */
+  InstLoope,          /* e1 */
+  InstPopf,           /* 9d */
+  InstPopfd,          /* 9d */
+  InstPopfq,          /* 9d */
+  InstPushf,          /* 9c */
+  InstPushfd,         /* 9c */
+  InstPushfq          /* 9c */
+
+};
+
+static const NaClNameOpcodeSeq kNameSeqSizeDefaultIs64[] = {
+  { InstCall , { 0xe8 , END_OPCODE_SEQ } },
+  { InstCall , { 0xff , SL(2) , END_OPCODE_SEQ } },
+  { InstJmp , { 0xe9 , END_OPCODE_SEQ } },
+  { InstJmp , { 0xeb , END_OPCODE_SEQ } },
+  { InstJmp , { 0xff , SL(4) , END_OPCODE_SEQ } },
+  { InstPop , { 0x58 , END_OPCODE_SEQ } },
+  { InstPop , { 0x59 , END_OPCODE_SEQ } },
+  { InstPop , { 0x5a , END_OPCODE_SEQ } },
+  { InstPop , { 0x5b , END_OPCODE_SEQ } },
+  { InstPop , { 0x5c , END_OPCODE_SEQ } },
+  { InstPop , { 0x5d , END_OPCODE_SEQ } },
+  { InstPop , { 0x5e , END_OPCODE_SEQ } },
+  { InstPop , { 0x5f , END_OPCODE_SEQ } },
+  { InstPop , { 0x8f , SL(0) , END_OPCODE_SEQ } },
+  { InstPop , { 0x0f , 0xa1 , END_OPCODE_SEQ } } ,
+  { InstPop , { 0x0f , 0xa9 , END_OPCODE_SEQ } },
   { InstPush , { 0x50 , END_OPCODE_SEQ } },
   { InstPush , { 0x51 , END_OPCODE_SEQ } },
   { InstPush , { 0x52 , END_OPCODE_SEQ } },
@@ -28,25 +82,23 @@ static const NaClNameOpcodeSeq kSizeDefaultIs64[] = {
   { InstPush , { 0x55 , END_OPCODE_SEQ } },
   { InstPush , { 0x56 , END_OPCODE_SEQ } },
   { InstPush , { 0x57 , END_OPCODE_SEQ } },
-  { InstPop  , { 0x58 , END_OPCODE_SEQ } },
-  { InstPop  , { 0x59 , END_OPCODE_SEQ } },
-  { InstPop  , { 0x5a , END_OPCODE_SEQ } },
-  { InstPop  , { 0x5b , END_OPCODE_SEQ } },
-  { InstPop  , { 0x5c , END_OPCODE_SEQ } },
-  { InstPop  , { 0x5d , END_OPCODE_SEQ } },
-  { InstPop  , { 0x5e , END_OPCODE_SEQ } },
-  { InstPop  , { 0x5f , END_OPCODE_SEQ } },
-  { InstPushfq , { 0x9c , END_OPCODE_SEQ } },
-  { InstPush , { 0xff , SL(6), END_OPCODE_SEQ } },
+  { InstPush , { 0x6a , END_OPCODE_SEQ } },
+  { InstPush , { 0x68 , END_OPCODE_SEQ } },
+  { InstPush , { 0xff , SL(6) , END_OPCODE_SEQ } },
+  { InstPush , { 0x0f , 0xa0 , END_OPCODE_SEQ } },
+  { InstPush , { 0x0f , 0xa8 , END_OPCODE_SEQ } },
+  { InstRet , { 0xc2 , END_OPCODE_SEQ } },
+  { InstRet , { 0xc3 , END_OPCODE_SEQ } },
 };
 
 void NaClAddSizeDefaultIs64() {
   NaClInst* inst = NaClGetDefInst();
   if ((X86_64 == NACL_FLAGS_run_mode) &&
       (NaClOperandSizes(inst) & NACL_IFLAG(OperandSize_o)) &&
-      NaClInInstructionSet(NULL, 0,
-                           kSizeDefaultIs64,
-                           NACL_ARRAY_SIZE(kSizeDefaultIs64))) {
+      NaClInInstructionSet(kNameSizeDefaultIs64,
+                           NACL_ARRAY_SIZE(kNameSizeDefaultIs64),
+                           kNameSeqSizeDefaultIs64,
+                           NACL_ARRAY_SIZE(kNameSeqSizeDefaultIs64))) {
     NaClAddIFlags(NACL_IFLAG(OperandSizeDefaultIs64));
   }
 }
