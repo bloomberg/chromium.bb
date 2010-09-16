a446cfafa8ba82739d85c128203295288e5aca2b
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-16 00:25:17 +0000

    Simplified chrome-specific ChromeOS autotest by introducing a common wrapper class.
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/3408006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@59590 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f3c713000ecf4057ceef9a62202cabd7dab14550

--- chrome/test/chromeos/autotest/files/client/site_tests/desktopui_BrowserTest/desktopui_BrowserTest.py
+++ chrome/test/chromeos/autotest/files/client/site_tests/desktopui_BrowserTest/desktopui_BrowserTest.py
@@ -2,37 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, utils
-from autotest_lib.client.bin import test
-from autotest_lib.client.common_lib import error, site_ui
+from autotest_lib.client.bin import site_chrome_test
 
-class desktopui_BrowserTest(test.test):
+class desktopui_BrowserTest(site_chrome_test.ChromeTestBase):
     version = 1
 
-    def setup(self):
-        self.job.setup_dep(['chrome_test'])
-        # create a empty srcdir to prevent the error that checks .version file
-        if not os.path.exists(self.srcdir):
-            os.mkdir(self.srcdir)
-
-
     def run_once(self):
-        dep = 'chrome_test'
-        dep_dir = os.path.join(self.autodir, 'deps', dep)
-        self.job.install_pkg(dep, 'dep', dep_dir)
+        self.run_chrome_test('browser_tests', '')
 
-        chrome_dir = '/opt/google/chrome'
-        test_binary_dir = '%s/test_src/out/Release' % dep_dir
-
-        try:
-            setup_cmd = '%s/%s' % (test_binary_dir,
-                                  'setup_test_links.sh')
-            utils.system(setup_cmd)
-
-            cmd = '%s/%s' % (test_binary_dir, 'browser_tests')
-            cmd = site_ui.xcommand(cmd)
-            logging.info("Running %s" % cmd)
-            utils.system(cmd)
-        except error.CmdError, e:
-            logging.debug(e)
-            raise error.TestFail('browser_test failed.')
--- chrome/test/chromeos/autotest/files/client/site_tests/desktopui_SyncIntegrationTests/desktopui_SyncIntegrationTests.py
+++ chrome/test/chromeos/autotest/files/client/site_tests/desktopui_SyncIntegrationTests/desktopui_SyncIntegrationTests.py
@@ -2,38 +2,18 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import logging, os, utils
-from autotest_lib.client.bin import test
-from autotest_lib.client.common_lib import error, site_ui
+import os
+from autotest_lib.client.bin import site_chrome_test
 
-class desktopui_SyncIntegrationTests(test.test):
+class desktopui_SyncIntegrationTests(site_chrome_test.ChromeTestBase):
     version = 1
-
-    def setup(self):
-        self.job.setup_dep(['chrome_test'])
-        # create a empty srcdir to prevent the error that checks .version file
-        if not os.path.exists(self.srcdir):
-            os.mkdir(self.srcdir)
-
 
     def run_once(self):
         dep = 'chrome_test'
         dep_dir = os.path.join(self.autodir, 'deps', dep)
-        self.job.install_pkg(dep, 'dep', dep_dir)
-
-        chrome_dir = '/opt/google/chrome'
-        test_binary_dir = '%s/test_src/out/Release' % dep_dir
         password_file = '%s/sync_password.txt' % dep_dir
 
-        try:
-            setup_cmd = '%s/%s' % (test_binary_dir,
-                                  'setup_test_links.sh')
-            utils.system(setup_cmd)
+        self.run_chrome_test('sync_integration_tests',
+            ('--password-file-for-test=%s ' +
+             '--test-terminate-timeout=300000') % password_file)
 
-            cmd = '%s/sync_integration_tests --password-file-for-test=%s --test-terminate-timeout=300000' % (test_binary_dir, password_file)
-            cmd = site_ui.xcommand(cmd)
-            logging.info("Running %s" % cmd)
-            utils.system(cmd)
-        except error.CmdError, e:
-            logging.debug(e)
-            raise error.TestFail('sync_integration_tests failed.')
