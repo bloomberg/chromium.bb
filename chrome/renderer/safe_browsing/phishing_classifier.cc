f3d221dfeb270797cc50f5dd1d304b2bb1a55c1c
bryner@chromium.org <bryner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-22 21:01:19 +0000

    Add a PhishingClassifier implementation.
    The PhishingClassifier ties together the feature extractors and the scorer,
    and will be invoked by the RenderView when a page finishes loading.  This
    change also makes the feature extractors have a non-owned pointer to the clock
    so that it can be shared, and fixes a problem where the DOM feature extractor
    cleared out any already-present features.
    
    BUG=none
    TEST=PhishingClassifierTest
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=60082
    
    Review URL: http://codereview.chromium.org/3452010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60220 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 27f53c1e40c8078159d0ddc11a0ea02dba94c257

---
+++ chrome/renderer/safe_browsing/phishing_classifier.cc
@@ -0,0 +1,169 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/renderer/safe_browsing/phishing_classifier.h"
+
+#include <string>
+
+#include "base/callback.h"
+#include "base/compiler_specific.h"
+#include "base/logging.h"
+#include "base/sha2.h"
+#include "chrome/renderer/render_view.h"
+#include "chrome/renderer/safe_browsing/feature_extractor_clock.h"
+#include "chrome/renderer/safe_browsing/features.h"
+#include "chrome/renderer/safe_browsing/phishing_dom_feature_extractor.h"
+#include "chrome/renderer/safe_browsing/phishing_term_feature_extractor.h"
+#include "chrome/renderer/safe_browsing/phishing_url_feature_extractor.h"
+#include "chrome/renderer/safe_browsing/scorer.h"
+#include "googleurl/src/gurl.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebFrame.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebURL.h"
+#include "third_party/WebKit/WebKit/chromium/public/WebView.h"
+
+namespace safe_browsing {
+
+const double PhishingClassifier::kInvalidScore = -1.0;
+const double PhishingClassifier::kPhishyThreshold = 0.5;
+
+PhishingClassifier::PhishingClassifier(RenderView* render_view,
+                                       const Scorer* scorer,
+                                       FeatureExtractorClock* clock)
+    : render_view_(render_view),
+      scorer_(scorer),
+      clock_(clock),
+      ALLOW_THIS_IN_INITIALIZER_LIST(method_factory_(this)) {
+  url_extractor_.reset(new PhishingUrlFeatureExtractor);
+  dom_extractor_.reset(
+      new PhishingDOMFeatureExtractor(render_view_, clock_.get()));
+  term_extractor_.reset(new PhishingTermFeatureExtractor(
+      &scorer_->page_terms(),
+      &scorer_->page_words(),
+      scorer_->max_words_per_term(),
+      clock_.get()));
+
+  Clear();
+}
+
+PhishingClassifier::~PhishingClassifier() {
+  // The RenderView should have called CancelPendingClassification() before
+  // we are destroyed.
+  CheckNoPendingClassification();
+}
+
+void PhishingClassifier::BeginClassification(const string16* page_text,
+                                             DoneCallback* done_callback) {
+  // The RenderView should have called CancelPendingClassification() before
+  // starting a new classification, so DCHECK this.
+  CheckNoPendingClassification();
+  // However, in an opt build, we will go ahead and clean up the pending
+  // classification so that we can start in a known state.
+  CancelPendingClassification();
+
+  page_text_ = page_text;
+  done_callback_.reset(done_callback);
+
+  // For consistency, we always want to invoke the DoneCallback
+  // asynchronously, rather than directly from this method.  To ensure that
+  // this is the case, post a task to begin feature extraction on the next
+  // iteration of the message loop.
+  MessageLoop::current()->PostTask(
+      FROM_HERE,
+      method_factory_.NewRunnableMethod(
+          &PhishingClassifier::BeginFeatureExtraction));
+}
+
+void PhishingClassifier::BeginFeatureExtraction() {
+  WebKit::WebView* web_view = render_view_->webview();
+  if (!web_view) {
+    RunFailureCallback();
+    return;
+  }
+
+  WebKit::WebFrame* frame = web_view->mainFrame();
+  if (!frame) {
+    RunFailureCallback();
+    return;
+  }
+
+  features_.reset(new FeatureMap);
+  if (!url_extractor_->ExtractFeatures(GURL(frame->url()), features_.get())) {
+    RunFailureCallback();
+    return;
+  }
+
+  // DOM feature extraction can take awhile, so it runs asynchronously
+  // in several chunks of work and invokes the callback when finished.
+  dom_extractor_->ExtractFeatures(
+      features_.get(),
+      NewCallback(this, &PhishingClassifier::DOMExtractionFinished));
+}
+
+void PhishingClassifier::CancelPendingClassification() {
+  // Note that cancelling the feature extractors is simply a no-op if they
+  // were not running.
+  dom_extractor_->CancelPendingExtraction();
+  term_extractor_->CancelPendingExtraction();
+  method_factory_.RevokeAll();
+  Clear();
+}
+
+void PhishingClassifier::DOMExtractionFinished(bool success) {
+  if (success) {
+    // Term feature extraction can take awhile, so it runs asynchronously
+    // in several chunks of work and invokes the callback when finished.
+    term_extractor_->ExtractFeatures(
+        page_text_,
+        features_.get(),
+        NewCallback(this, &PhishingClassifier::TermExtractionFinished));
+  } else {
+    RunFailureCallback();
+  }
+}
+
+void PhishingClassifier::TermExtractionFinished(bool success) {
+  if (success) {
+    // Hash all of the features so that they match the model, then compute
+    // the score.
+    FeatureMap hashed_features;
+    for (base::hash_map<std::string, double>::const_iterator it =
+             features_->features().begin();
+         it != features_->features().end(); ++it) {
+      bool result = hashed_features.AddRealFeature(
+          base::SHA256HashString(it->first), it->second);
+      DCHECK(result);
+    }
+
+    double score = scorer_->ComputeScore(hashed_features);
+    RunCallback(score >= kPhishyThreshold, score);
+  } else {
+    RunFailureCallback();
+  }
+}
+
+void PhishingClassifier::CheckNoPendingClassification() {
+  DCHECK(!done_callback_.get());
+  DCHECK(!page_text_);
+  if (done_callback_.get() || page_text_) {
+    LOG(ERROR) << "Classification in progress, missing call to "
+               << "CancelPendingClassification";
+  }
+}
+
+void PhishingClassifier::RunCallback(bool phishy, double phishy_score) {
+  done_callback_->Run(phishy, phishy_score);
+  Clear();
+}
+
+void PhishingClassifier::RunFailureCallback() {
+  RunCallback(false /* not phishy */, kInvalidScore);
+}
+
+void PhishingClassifier::Clear() {
+  page_text_ = NULL;
+  done_callback_.reset(NULL);
+  features_.reset(NULL);
+}
+
+}  // namespace safe_browsing
---
+++ chrome/renderer/safe_browsing/phishing_classifier.h
@@ -0,0 +1,124 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// This class handles the process of extracting all of the features from a
+// page and computing a phishyness score.  For more details, see
+// phishing_*_feature_extractor.h, scorer.h, and client_model.proto.
+
+#ifndef CHROME_RENDERER_SAFE_BROWSING_PHISHING_CLASSIFIER_H_
+#define CHROME_RENDERER_SAFE_BROWSING_PHISHING_CLASSIFIER_H_
+
+#include "base/basictypes.h"
+#include "base/callback.h"
+#include "base/scoped_ptr.h"
+#include "base/string16.h"
+#include "base/task.h"
+
+class RenderView;
+
+namespace safe_browsing {
+class FeatureExtractorClock;
+class FeatureMap;
+class PhishingDOMFeatureExtractor;
+class PhishingTermFeatureExtractor;
+class PhishingUrlFeatureExtractor;
+class Scorer;
+
+class PhishingClassifier {
+ public:
+  // Callback to be run when phishing classification finishes.  If the first
+  // argument is true, the page is considered phishy by the client-side model,
+  // and the browser should ping back to get a final verdict.  The second
+  // argument gives the phishyness score which is used in the pingback,
+  // or kInvalidScore if classification failed.
+  typedef Callback2<bool /* phishy */, double /* phishy_score */>::Type
+    DoneCallback;
+
+  static const double kInvalidScore;
+
+  // Creates a new PhishingClassifier object that will operate on
+  // |render_view|.  |scorer| will be used for computing the final score, and
+  // must live at least as long as the PhishingClassifier.  |clock| is used to
+  // time feature extractor operations, and the PhishingClassifier takes
+  // ownership of this object.
+  PhishingClassifier(RenderView* render_view,
+                     const Scorer* scorer,
+                     FeatureExtractorClock* clock);
+  ~PhishingClassifier();
+
+  // Called by the RenderView when a page has finished loading.  This begins
+  // the feature extraction and scoring process. |page_text| should contain
+  // the plain text of a web page, including any subframes, as returned by
+  // RenderView::CaptureText().  |page_text| is owned by the caller, and must
+  // not be destroyed until either |done_callback| is run or
+  // CancelPendingClassification() is called.
+  //
+  // To avoid blocking the render thread for too long, phishing classification
+  // may run in several chunks of work, posting a task to the current
+  // MessageLoop to continue processing.  Once the scoring process is complete,
+  // |done_callback| is run on the current thread.  PhishingClassifier takes
+  // ownership of the callback.
+  void BeginClassification(const string16* page_text, DoneCallback* callback);
+
+  // Called by the RenderView (on the render thread) when a page is unloading
+  // or the RenderView is being destroyed.  This cancels any extraction that
+  // is in progress.
+  void CancelPendingClassification();
+
+ private:
+  // Any score equal to or above this value is considered phishy.
+  static const double kPhishyThreshold;
+
+  // Begins the feature extraction process, by extracting URL features and
+  // beginning DOM feature extraction.
+  void BeginFeatureExtraction();
+
+  // Callback to be run when DOM feature extraction is complete.
+  // If it was successful, begins term feature extraction, otherwise
+  // runs the DoneCallback with a non-phishy verdict.
+  void DOMExtractionFinished(bool success);
+
+  // Callback to be run when term feature extraction is complete.
+  // If it was successful, computes a score and runs the DoneCallback.
+  // If extraction was unsuccessful, runs the DoneCallback with a
+  // non-phishy verdict.
+  void TermExtractionFinished(bool success);
+
+  // Helper to verify that there is no pending phishing classification.  Dies
+  // in debug builds if the state is not as expected.  This is a no-op in
+  // release builds.
+  void CheckNoPendingClassification();
+
+  // Helper method to run the DoneCallback and clear the state.
+  void RunCallback(bool phishy, double phishy_score);
+
+  // Helper to run the DoneCallback when feature extraction has failed.
+  // This always signals a non-phishy verdict for the page, with kInvalidScore.
+  void RunFailureCallback();
+
+  // Clears the current state of the PhishingClassifier.
+  void Clear();
+
+  RenderView* render_view_;  // owns us
+  const Scorer* scorer_;  // owned by the caller
+  scoped_ptr<FeatureExtractorClock> clock_;
+  scoped_ptr<PhishingUrlFeatureExtractor> url_extractor_;
+  scoped_ptr<PhishingDOMFeatureExtractor> dom_extractor_;
+  scoped_ptr<PhishingTermFeatureExtractor> term_extractor_;
+
+  // State for any in-progress extraction.
+  scoped_ptr<FeatureMap> features_;
+  const string16* page_text_;  // owned by the caller
+  scoped_ptr<DoneCallback> done_callback_;
+
+  // Used to create BeginFeatureExtraction tasks.
+  // These tasks are revoked if classification is cancelled.
+  ScopedRunnableMethodFactory<PhishingClassifier> method_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(PhishingClassifier);
+};
+
+}  // namespace safe_browsing
+
+#endif  // CHROME_RENDERER_SAFE_BROWSING_PHISHING_CLASSIFIER_H_
