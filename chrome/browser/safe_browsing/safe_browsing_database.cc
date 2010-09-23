66c3a2d53808bfc3d37bc236d56721163c009e22
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-23 04:07:46 +0000

    Make SafeBrowsingStoreFile the default store for developers.
    Get some early coverage by opting developers into the experiment.
    
    BUG=28647
    TEST=Developers don't complain.
    
    Review URL: http://codereview.chromium.org/3389025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60257 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b7c2e29503c528f053cd8659e1fbe3cbff427e86

--- chrome/browser/safe_browsing/safe_browsing_database.cc
+++ chrome/browser/safe_browsing/safe_browsing_database.cc
@@ -176,7 +176,13 @@ SafeBrowsingDatabase* SafeBrowsingDatabase::Create() {
   } else {
     // Default.
     DCHECK(value.empty());
+#if defined(NDEBUG)
     return new SafeBrowsingDatabaseNew(new SafeBrowsingStoreSqlite);
+#else
+    // Turn on the file store for developers to get some early
+    // coverage.
+    return new SafeBrowsingDatabaseNew(new SafeBrowsingStoreFile);
+#endif
   }
 }
 
