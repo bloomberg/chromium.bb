3438c4d4d6c3bded8c792a5c23e576b0477a7b85
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-24 20:55:44 +0000

    Small cleanup - remove 2 unnecessary includes.
    BUG=None
    TEST=It compiles
    
    TBR=mpcomplete@chromium.org
    
    
    Review URL: http://codereview.chromium.org/3473018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60518 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 081607e911be0315e7af7b7c508cb1510595b683

--- chrome/browser/extensions/extension_context_menu_apitest.cc
+++ chrome/browser/extensions/extension_context_menu_apitest.cc
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/command_line.h"
 #include "chrome/browser/extensions/extension_apitest.h"
-#include "chrome/common/chrome_switches.h"
 
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, ContextMenus) {
   ASSERT_TRUE(RunExtensionTest("context_menus/basics")) << message_;
