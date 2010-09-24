c335396f702a6d3c9dbf602bab3f0d8bbb8a54eb
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-24 19:16:04 +0000

    ui_tests: disable EarlyReturnTest.
    It always times out.  It is only passing on the bots because of a bug in the UI
    test framework that doesn't detect the timeout.
    
    BUG=45115
    TEST=ui_tests are 30 seconds faster
    
    Review URL: http://codereview.chromium.org/3433022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60504 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 88ba0c0486edf4a0640ba0044bafc36b70a2b171

--- chrome/browser/sanity_uitest.cc
+++ chrome/browser/sanity_uitest.cc
@@ -48,17 +48,23 @@ TEST_F(ColumnLayout, Crash) {
   // UITest will check if this crashed.
 }
 
-// By passing kTryChromeAgain with a magic value > 10000 we cause chrome
-// to exit fairly early. This was the cause of crashes. See bug 34799.
+// By passing kTryChromeAgain with a magic value > 10000 we cause Chrome
+// to exit fairly early.
+// Quickly exiting Chrome (regardless of this particular flag -- it
+// doesn't do anything other than cause Chrome to quit on startup on
+// non-Windows) was a cause of crashes (see bug 34799 for example) so
+// this is a useful test of the startup/quick-shutdown cycle.
 class EarlyReturnTest : public UITest {
  public:
   EarlyReturnTest() {
-    wait_for_initial_loads_ = false;
+    wait_for_initial_loads_ = false;  // Don't wait for any pages to load.
     launch_arguments_.AppendSwitchASCII(switches::kTryChromeAgain, "10001");
   }
 };
 
-// Flaky: http://crbug.com/45115
-TEST_F(EarlyReturnTest, FLAKY_ToastCrasher) {
+// Disabled: http://crbug.com/45115
+// Due to limitations in our test infrastructure, this test currently doesn't
+// work.
+TEST_F(EarlyReturnTest, DISABLED_ToastCrasher) {
   // UITest will check if this crashed.
 }
