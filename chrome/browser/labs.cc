bloomberg/chromium.bb

commit aaf2762e1557440f99252125c6f3c1fddafd5b5c

Add remoting to about:labs.
BUG=55676
TEST=Go to about:labs and make sure you see a section for Remoting.

Review URL: http://codereview.chromium.org/3422010

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60578 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 52fa2d51ad0335f29e471ff4cafb9c1347e7ea14

chrome/browser/labs.cc
@@ -81,6 +81,22 @@ const Experiment kExperiments[] = {
     kOsWin,
     switches::kEnableMatchPreview
   },
+  {
+    "remoting",  // Do not change; see above.
+    IDS_LABS_REMOTING_NAME,
+#if defined(OS_WINDOWS)
+    // Windows only supports host functionality at the moment.
+    IDS_LABS_REMOTING_HOST_DESCRIPTION,
+#elif defined(OS_LINUX)
+    // Linux only supports client functionality at the moment.
+    IDS_LABS_REMOTING_CLIENT_DESCRIPTION,
+#else
+    // On other platforms, this lab isn't available at all
+    "",
+#endif
+    kOsWin | kOsLinux,
+    switches::kEnableRemoting
+  },
 };
 
 // Extracts the list of enabled lab experiments from a profile and stores them

chrome/app/generated_resources.grd
@@ -3864,6 +3864,15 @@ Keep your key file in a safe place. You will need it to create new versions of y
      <message name="IDS_LABS_INSTANT_DESCRIPTION" desc="Description of the 'Instant' lab.">
         Makes the address bar load urls as you type.
       </message>
+     <message name="IDS_LABS_REMOTING_NAME" desc="Description of the 'Remoting' lab.">
+        Remoting
+      </message>
+     <message name="IDS_LABS_REMOTING_CLIENT_DESCRIPTION" desc="Description of the 'Remoting' client lab.">
+        Enable Remoting Client support.
+      </message>
+     <message name="IDS_LABS_REMOTING_HOST_DESCRIPTION" desc="Description of the 'Remoting' host lab.">
+        Enable Remoting Host support.
+      </message>
 
       <!-- Click-to-load -->
       <message name="IDS_PLUGIN_LOAD" desc="The link for loading a blocked plug-in, displayed in the click-to-play UI.">

chrome/browser/chromeos/dom_ui/labs_handler.cc
@@ -5,18 +5,8 @@
 #include "chrome/browser/chromeos/dom_ui/labs_handler.h"
 
 #include "app/l10n_util.h"
-#include "base/basictypes.h"
-#include "base/callback.h"
-#include "base/stl_util-inl.h"
-#include "base/time.h"
-#include "base/utf_string_conversions.h"
 #include "base/values.h"
-#include "chrome/common/notification_service.h"
-#include "grit/browser_resources.h"
-#include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
-#include "grit/locale_settings.h"
-#include "grit/theme_resources.h"
 
 LabsHandler::LabsHandler() {
 }
@@ -30,6 +20,7 @@ void LabsHandler::GetLocalizedValues(
   // Labs page - ChromeOS
   localized_strings->SetString("labsPage",
       l10n_util::GetStringUTF16(IDS_OPTIONS_LABS_TAB_LABEL));
+
   localized_strings->SetString("mediaplayer_title",
      l10n_util::GetStringUTF16(IDS_OPTIONS_SETTINGS_SECTION_TITLE_MEDIAPLAYER));
   localized_strings->SetString("mediaplayer",

chrome/browser/chromeos/dom_ui/labs_handler.h
@@ -18,7 +18,6 @@ class LabsHandler : public OptionsPageUIHandler {
   virtual void GetLocalizedValues(DictionaryValue* localized_strings);
 
  private:
-
   DISALLOW_COPY_AND_ASSIGN(LabsHandler);
 };
 

commit 0bac8588dc3d2d72ba36caf1f2f95ba68fcc6563

Fix a typo introduced in r60578.
OS_WINDOWS doesn't exists, it should be OS_WIN.

TBR=thakis@chromium.org
BUG=55676
TEST=None

Review URL: http://codereview.chromium.org/3505002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60580 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 4fb2f11929213712d0059d74ba008c1964fc0932

chrome/browser/labs.cc
@@ -84,7 +84,7 @@ const Experiment kExperiments[] = {
   {
     "remoting",  // Do not change; see above.
     IDS_LABS_REMOTING_NAME,
-#if defined(OS_WINDOWS)
+#if defined(OS_WIN)
     // Windows only supports host functionality at the moment.
     IDS_LABS_REMOTING_HOST_DESCRIPTION,
 #elif defined(OS_LINUX)