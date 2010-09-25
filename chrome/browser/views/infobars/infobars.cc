d8711f5c1011b816511e51b45016731ca9d9949a
ctguil@chromium.org <ctguil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-25 07:42:47 +0000

    Call InfoBar::ViewHierarchyChangedViewHierarchyChanged after the ConfirmInfoBar is initialized.
    BUG=37360
    TEST=JAWS screen reader will read all items in the confirm infobar.
    
    Review URL: http://codereview.chromium.org/3441032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60572 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 60b9190738ca15a4595ecaaa91a9acc9e309c5a3

--- chrome/browser/views/infobars/infobars.cc
+++ chrome/browser/views/infobars/infobars.cc
@@ -537,11 +537,11 @@ void ConfirmInfoBar::Layout() {
 void ConfirmInfoBar::ViewHierarchyChanged(bool is_add,
                                           views::View* parent,
                                           views::View* child) {
-  InfoBar::ViewHierarchyChanged(is_add, parent, child);
   if (is_add && child == this && !initialized_) {
     Init();
     initialized_ = true;
   }
+  InfoBar::ViewHierarchyChanged(is_add, parent, child);
 }
 
 // ConfirmInfoBar, views::ButtonListener implementation: ---------------
