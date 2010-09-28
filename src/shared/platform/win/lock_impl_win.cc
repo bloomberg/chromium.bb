9d2e750b792cfc50586068fb8f84a128e21739be
noelallen@google.com <noelallen@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-09-28 20:15:39 +0000

    Add check for failure on allocation and lock of Mutex for Windows.  Since both these cases tend to only happen when the system is unstable or out of resources, we write to stderr directly.
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=572
    
    Review URL: http://codereview.chromium.org/3435026
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3367 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 4834da658444953c9530b68d88c64c29c696fb2f

--- src/shared/platform/win/lock_impl_win.cc
+++ src/shared/platform/win/lock_impl_win.cc
@@ -4,12 +4,29 @@
  * be found in the LICENSE file.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <io.h>
 
 #include "native_client/src/include/portability.h"
 #include "native_client/src/shared/platform/win/lock_impl.h"
 
+static const char *kMutexCreateFailed =
+                     "Unrecoverable failure on CreateMutex.\n";
+static const char *kMutexLockFailed =
+                     "Unrecoverable failure locking a Mutex.\n";
+
 NaCl::LockImpl::LockImpl() {
   mutex_ = CreateMutex(NULL, FALSE, NULL);
+
+  /*
+   * Since this is probably resource exhaustion, and/or this could be the
+   * log's lock, then logging is risky so we write directly to stderr.
+   */
+  if (NULL == mutex_) {
+    _write(2, kMutexCreateFailed, sizeof kMutexCreateFailed - 1);
+    abort();
+  }
 }
 
 NaCl::LockImpl::~LockImpl() {
@@ -29,7 +46,18 @@ bool NaCl::LockImpl::Try() {
 }
 
 void NaCl::LockImpl::Lock() {
-  WaitForSingleObject(mutex_, INFINITE);
+  DWORD dwWaitResult;
+  dwWaitResult = WaitForSingleObject(mutex_, INFINITE);
+
+  /*
+   * It is possible to fail on Windows, so we need to abort to prevent a
+   * security issue.  Since this could be the log's lock, and/or the system
+   * is seriously unstable, we write directly.
+   */
+  if (WAIT_OBJECT_0 != dwWaitResult) {
+    _write(2, kMutexLockFailed, sizeof kMutexLockFailed - 1);
+    abort();
+  }
 }
 
 void NaCl::LockImpl::Unlock() {
