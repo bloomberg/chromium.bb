af511f95bd815f8b2f9c30b43eb5e412b3574c1c
John Boyer <john.boyer@jjb-software.com>
2010-09-28 22:07:58 +0000

    Framework for wrappers

--- liblouis/wrappers.c
+++ liblouis/wrappers.c
@@ -23,7 +23,59 @@
    Maintained by John J. Boyer john.boyer@abilitiessoft.com
    */
 
+#include <stdio.h>
+#include <string.h>
 #include "louis.h"
+
+static int
+ignoreCaseComp (const char *str1, const char *str2, int length)
+{
+/* Replaces strncasecmp, which some compilers don't support */
+  int k;
+  for (k = 0; k < length; k++)
+    if ((str1[k] | 32) != (str2[k] | 32))
+      break;
+  if (k != length)
+    return 1;
+  return 0;
+}
+
+static int
+findAction (const char **actions, const char *action)
+{
+  int actionLength = strlen (action);
+  int k;
+  for (k = 0; actions[k]; k += 2)
+    if (actionLength == strlen (actions[k])
+	&& ignoreCaseComp (actions[k], action, actionLength) == 0)
+      break;
+  if (actions[k] == NULL)
+    return -1;
+  return atoi (actions[k + 1]);
+}
+
+static char *
+findColon (char *transSpec)
+{
+  int k;
+  for (k = 0; transSpec[k]; k++)
+    if (transSpec[k] == ':')
+      {
+	transSpec[k] = 0;
+	return &transSpec[k + 1];
+      }
+  return NULL;
+}
+
+static const char *translators[] = {
+  "korean",
+  "1",
+  "japanese",
+  "2",
+  "german",
+  "3",
+  NULL
+};
 
 int
 other_translate (const char *trantab, const widechar
@@ -32,6 +84,26 @@ other_translate (const char *trantab, const widechar
 		 char *typeform, char *spacing, int *outputPos, int
 		 *inputPos, int *cursorPos, int mode)
 {
+  char transSpec[MAXSTRING];
+  char *colonPos;
+  int action;
+  strcpy (transSpec, trantab);
+  colonPos = findColon (transSpec);
+  action = findAction (translators, transSpec);
+  switch (action)
+    {
+    case -1:
+      lou_logPrint ("There is no translator called '%s'", transSpec);
+      return 0;
+    case 1:
+      return 1;
+    case 2:
+      return 1;
+    case 3:
+      return 1;
+    default:
+      return 0;
+    }
   return 0;
 }
 
@@ -42,5 +114,25 @@ other_backTranslate (const char *trantab, const widechar
 		     char *typeform, char *spacing, int *outputPos, int
 		     *inputPos, int *cursorPos, int mode)
 {
+  char transSpec[MAXSTRING];
+  char *colonPos;
+  int action;
+  strcpy (transSpec, trantab);
+  colonPos = findColon (transSpec);
+  action = findAction (translators, transSpec);
+  switch (action)
+    {
+    case -1:
+      lou_logPrint ("There is no translator called '%s'", transSpec);
+      return 0;
+    case 1:
+      return 1;
+    case 2:
+      return 1;
+    case 3:
+      return 1;
+    default:
+      return 0;
+    }
   return 0;
 }
