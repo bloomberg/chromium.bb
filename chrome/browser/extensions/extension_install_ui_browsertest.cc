4f5c3f8ba3ce52425dffd42084739e181da3d58f
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-29 13:57:00 +0000

    Disable crashy ExtensionInstallUIBrowserTest.TestThemeInstallUndoResetsToDefault
    TBR=satish
    
    BUG=44548
    TEST=none
    
    Review URL: http://codereview.chromium.org/3587001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60939 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 52e821efe2253b4dd9f7c9319b45469c5b22d455

--- chrome/browser/extensions/extension_install_ui_browsertest.cc
+++ chrome/browser/extensions/extension_install_ui_browsertest.cc
@@ -33,8 +33,9 @@ class ExtensionInstallUIBrowserTest : public ExtensionBrowserTest {
   }
 };
 
+// Crashy, http://crbug.com/44548.
 IN_PROC_BROWSER_TEST_F(ExtensionInstallUIBrowserTest,
-                       TestThemeInstallUndoResetsToDefault) {
+                       DISABLED_TestThemeInstallUndoResetsToDefault) {
   // Install theme once and undo to verify we go back to default theme.
   FilePath theme_path = test_data_dir_.AppendASCII("theme.crx");
   ASSERT_TRUE(InstallExtensionWithUI(theme_path, 1));
