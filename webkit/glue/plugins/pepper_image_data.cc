496325655358a8dab6e30eca873ad59edb2c0553
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-29 04:16:26 +0000

    Re-land 60850 Implement PPB_ImageDataTrusted on the Chrome side. This just connects the call to the existing backend implementation.
    Review URL: http://codereview.chromium.org/3473021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@60908 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ea27c8684c2cf233dfa439a03bd4876947de26c4

--- webkit/glue/plugins/pepper_image_data.cc
+++ webkit/glue/plugins/pepper_image_data.cc
@@ -14,6 +14,7 @@
 #include "third_party/ppapi/c/pp_module.h"
 #include "third_party/ppapi/c/pp_resource.h"
 #include "third_party/ppapi/c/ppb_image_data.h"
+#include "third_party/ppapi/c/trusted/ppb_image_data_trusted.h"
 #include "webkit/glue/plugins/pepper_plugin_instance.h"
 #include "webkit/glue/plugins/pepper_plugin_module.h"
 
@@ -68,6 +69,13 @@ void Unmap(PP_Resource resource) {
     image_data->Unmap();
 }
 
+uint64_t GetNativeMemoryHandle2(PP_Resource resource) {
+  scoped_refptr<ImageData> image_data(Resource::GetAs<ImageData>(resource));
+  if (image_data)
+    return image_data->GetNativeMemoryHandle();
+  return 0;
+}
+
 const PPB_ImageData ppb_imagedata = {
   &GetNativeImageDataFormat,
   &Create,
@@ -75,6 +83,10 @@ const PPB_ImageData ppb_imagedata = {
   &Describe,
   &Map,
   &Unmap,
+};
+
+const PPB_ImageDataTrusted ppb_imagedata_trusted = {
+  &GetNativeMemoryHandle2,
 };
 
 }  // namespace
@@ -91,6 +103,11 @@ ImageData::~ImageData() {
 // static
 const PPB_ImageData* ImageData::GetInterface() {
   return &ppb_imagedata;
+}
+
+// static
+const PPB_ImageDataTrusted* ImageData::GetTrustedInterface() {
+  return &ppb_imagedata_trusted;
 }
 
 bool ImageData::Init(PP_ImageDataFormat format,
@@ -141,6 +158,10 @@ void ImageData::Unmap() {
   // be around once it's mapped. Chrome's TransportDIB isn't currently
   // unmappable without freeing it, but this may be something we want to support
   // in the future to save some memory.
+}
+
+uint64 ImageData::GetNativeMemoryHandle() const {
+  return platform_image_->GetSharedMemoryHandle();
 }
 
 const SkBitmap* ImageData::GetMappedBitmap() const {
