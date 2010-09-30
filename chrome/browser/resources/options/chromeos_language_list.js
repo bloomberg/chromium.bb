4b2b34e91100d8b0205c95223d77bc8afbb1f911
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-09-30 07:59:54 +0000

    Save intl.accept_languages in addition to settings.language.preferred_languages.
    intl.accept_languaes is used for Accept-Language header in HTTP requests.
    On other platforms, this setting is configured from "Under the hood" options,
    but on Chromium OS, we integrate this setting with "Languages and Input" options.
    
    BUG=chromium-os:6292
    TEST=confirmed that intl.accept_languages is updated in the Preferences file
    
    Review URL: http://codereview.chromium.org/3543004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61039 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b053489e3e02341414b5b0cac4d7ab8b0560c07c

--- chrome/browser/resources/options/chromeos_language_list.js
+++ chrome/browser/resources/options/chromeos_language_list.js
@@ -74,7 +74,14 @@ cr.define('options.language', function() {
     draggedItem: null,
     // The drop position information: "below" or "above".
     dropPos: null,
-    pref: 'settings.language.preferred_languages',
+    // The preference is a CSV string that describes preferred languages
+    // in Chrome OS. The language list is used for showing the language
+    // list in "Language and Input" options page.
+    preferredLanguagesPref: 'settings.language.preferred_languages',
+    // The preference is a CSV string that describes accept languages used
+    // for content negotiation. To be more precise, the list will be used
+    // in "Accept-Language" header in HTTP requests.
+    acceptLanguagesPref: 'intl.accept_languages',
 
     /** @inheritDoc */
     decorate: function() {
@@ -85,8 +92,8 @@ cr.define('options.language', function() {
       window.addEventListener('resize', this.redraw.bind(this));
 
       // Listen to pref change.
-      Preferences.getInstance().addEventListener(this.pref,
-          this.handlePrefChange_.bind(this));
+      Preferences.getInstance().addEventListener(this.preferredLanguagesPref,
+          this.handlePreferredLanguagesPrefChange_.bind(this));
 
       // Listen to drag and drop events.
       this.addEventListener('dragstart', this.handleDragStart_.bind(this));
@@ -223,11 +230,11 @@ cr.define('options.language', function() {
     },
 
     /**
-     * Handles pref change.
+     * Handles preferred languages pref change.
      * @param {Event} e The change event object.
      * @private
      */
-    handlePrefChange_: function(e) {
+    handlePreferredLanguagesPrefChange_: function(e) {
       var languageCodesInCsv = e.value.value;
       var languageCodes = this.filterBadLanguageCodes_(
           languageCodesInCsv.split(','));
@@ -265,7 +272,15 @@ cr.define('options.language', function() {
      */
     savePreference_: function() {
       // Encode the language codes into a CSV string.
-      Preferences.setStringPref(this.pref, this.dataModel.slice().join(','));
+      Preferences.setStringPref(this.preferredLanguagesPref,
+                                this.dataModel.slice().join(','));
+      // Save the same language list as accept languages preference. In
+      // theory, we don't need two separate preferences but we keep these
+      // separate, as these are conceptually different. In other words,
+      // using "intl.accept_languages" for preferred languages in Chrome
+      // OS is a bit awkward.
+      Preferences.setStringPref(this.acceptLanguagesPref,
+                                this.dataModel.slice().join(','));
       cr.dispatchSimpleEvent(this, 'save');
     },
 
