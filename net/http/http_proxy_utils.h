0f7bb7265ab3cee62665c2a65c9036ac172337c7
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-01 03:34:07 +0000

    Fixes the tests written for http://codereview.chromium.org/3432009/show to use DeterministicSocketData which should avoid the flaky test problem that 3432009 introduced.
    --
    
    Add a new class SpdyProxyClientSocket which implements ClientSocket
    by sending a CONNECT request via a SPDY SYN_STREAM frame to a SPDY proxy,
    and then reading/writing data to/from SPDY Data frames.
    
    BUG=29625
    TEST=none
    
    Review URL: http://codereview.chromium.org/3578002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61144 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fe2f62adb59d0ca3789312b55646e3d63ba8918f

---
+++ net/http/http_proxy_utils.h
@@ -0,0 +1,28 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_HTTP_HTTP_PROXY_UTILS_H_
+#define NET_HTTP_HTTP_PROXY_UTILS_H_
+#pragma once
+
+#include <string>
+
+namespace net {
+
+struct HttpRequestInfo;
+class HttpRequestHeaders;
+class HostPortPair;
+
+// The HTTP CONNECT method for establishing a tunnel connection is documented
+// in draft-luotonen-web-proxy-tunneling-01.txt and RFC 2817, Sections 5.2 and
+// 5.3.
+void BuildTunnelRequest(const HttpRequestInfo& request_info,
+                        const HttpRequestHeaders& auth_headers,
+                        const HostPortPair& endpoint,
+                        std::string* request_line,
+                        HttpRequestHeaders* request_headers);
+
+}  // namespace net
+
+#endif  // NET_HTTP_HTTP_PROXY_UTILS_H_
