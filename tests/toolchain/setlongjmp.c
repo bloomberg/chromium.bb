324210372affd6b560c355b1f50e7fb524442b8f
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-10-04 17:46:37 +0000

    Added setjmp/longjmp test. Some cleanup in the corresponding nacl.scons file.
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=516
    Review URL: http://codereview.chromium.org/3615002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3395 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: a229ba761f3fbff7f7e50ad7ab9c0d89b5c24349

---
+++ tests/toolchain/setlongjmp.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2010 The Native Client Authors.  All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can
+ * be found in the LICENSE file.
+ */
+
+#include <setjmp.h>
+#include <stdio.h>
+
+static jmp_buf buf;
+
+int main() {
+  volatile int result = -1;
+  if (!setjmp(buf) ) {
+    result = 55;
+    printf("setjmp was invoked\n");
+    longjmp(buf, 1);
+    printf("this print statement is not reached\n");
+    return -1;
+  } else {
+    printf("longjmp was invoked\n");
+    return result;
+  }
+}
