a116e7d373994f43370b7cceffb3da194493170f
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-05 19:58:02 +0000

    Workaround to disable Progress() on buildbot by disabling it on --verbose.
    sys.stdout.isatty() is True even on buildbot.
    Otherwise, logs can't be untangled when using --jobs.
    
    TEST=none
    BUG=none
    
    Review URL: http://codereview.chromium.org/3596006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@61552 0039d316-1c4b-4281-b951-d872f2087c98

--- gclient.py
+++ gclient.py
@@ -716,7 +716,8 @@ solutions = [
     revision_overrides = self._EnforceRevisions()
     pm = None
     # Disable progress for non-tty stdout.
-    if command in ('update', 'revert') and sys.stdout.isatty():
+    if (command in ('update', 'revert') and sys.stdout.isatty() and not
+        self._options.verbose):
       pm = Progress('Syncing projects', 1)
     work_queue = gclient_utils.ExecutionQueue(self._options.jobs, pm)
     for s in self.dependencies:
