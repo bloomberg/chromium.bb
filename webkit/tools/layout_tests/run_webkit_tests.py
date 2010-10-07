fa1f5f0a7357e76cbd89878cbc16d96fb27e12aa
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-07 02:24:04 +0000

    Allow cygwin users to run run_webkit_tests.py.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/3593006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61754 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fa34b5afdcfb5b287efe788da7b612d37c0c40cb

--- webkit/tools/layout_tests/run_webkit_tests.py
+++ webkit/tools/layout_tests/run_webkit_tests.py
@@ -10,18 +10,13 @@ import subprocess
 import sys
 
 def main():
-    if sys.platform == 'cygwin':
-        return(
-            'ERROR: you are running from cygwin python, which is not\n'
-            '       supported by "run_webkit_test.py". You should use python\n'
-            '       for Windows (2.5+) to launch this script. A python for\n'
-            '       Windows can be found at your depot_tools folder:\n'
-            '       [DEPOT_TOOLS_PATH]/python ' + '"' + sys.argv[0] + '" ' +
-            ' '.join(sys.argv[1:]))
-
     cmd = [sys.executable]
+    src_dir = sys.argv[0];
+    if sys.platform != 'cygwin':
+        src_dir = os.path.abspath(sys.argv[0])
+    print src_dir
     src_dir=os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(
-                         os.path.dirname(os.path.abspath(sys.argv[0]))))))
+                         os.path.dirname(src_dir)))))
     script_dir=os.path.join(src_dir, "third_party", "WebKit", "WebKitTools",
                             "Scripts")
     script = os.path.join(script_dir, 'new-run-webkit-tests')
