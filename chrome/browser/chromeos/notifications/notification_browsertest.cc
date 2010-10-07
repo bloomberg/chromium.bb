233a5c93f1929915069e0a8f5b437220c965ab68
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-07 21:44:57 +0000

    Marked another ChromeOS test as flaky since it was breaking autotest suite.
    BUG=chromium-os:7306
    TEST=none
    TBR=oshima
    
    Review URL: http://codereview.chromium.org/3595010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@61850 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d5f94a38d31a35e3327de49d9857a92f483c2a88

--- chrome/browser/chromeos/notifications/notification_browsertest.cc
+++ chrome/browser/chromeos/notifications/notification_browsertest.cc
@@ -307,7 +307,9 @@ IN_PROC_BROWSER_TEST_F(NotificationTest, TestStateTransition1) {
 // This test depends on the fact that the panel state change occurs
 // quicker than stale timeout, thus the stale timeout cannot be set to
 // 0. This test explicitly controls the stale state instead.
-IN_PROC_BROWSER_TEST_F(NotificationTest, TestStateTransition2) {
+
+// TODO(oshima): bug chromium-os:7306 Fix this flaky test on ChromeOS.
+IN_PROC_BROWSER_TEST_F(NotificationTest, FLAKY_TestStateTransition2) {
   // Register observer here as the registration does not work in SetUp().
   NotificationRegistrar registrar;
   registrar.Add(this,
