56530f1a71e803fde7cf5858f49ed326e35841a2
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-10-08 16:49:07 +0000

    Add test for various init/fini mechanisms.
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=968
    Review URL: http://codereview.chromium.org/3534014
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3425 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 80e2604939665d74e73a88f876772de64710ebc6

---
+++ tests/toolchain/initfini.c
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2010 The Native Client Authors.  All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can
+ * be found in the LICENSE file.
+ */
+
+#include <stdio.h>
+
+
+typedef void (*FUN_PTR) (void);
+#define ATTR_USED __attribute__((used))
+#define ATTR_SEC(sec)\
+  __attribute__ ((__used__, section(sec), aligned(sizeof(FUN_PTR))))
+#define ADD_FUN_PTR_TO_SEC(name, sec, ptr)       \
+  static FUN_PTR name ATTR_SEC(sec)  = { ptr }
+
+
+
+#define MAKE_FUN(name) void name() { printf(#name "\n");}
+
+/*
+MAKE_FUN(fun_ctor)
+ADD_FUN_PTR_TO_SEC(array_ctor[1], ".ctor_array", fun_ctor);
+
+MAKE_FUN(fun_dtor)
+ADD_FUN_PTR_TO_SEC(array_dtor[1], ".dtor_array", fun_dtor);
+*/
+
+MAKE_FUN(fun_preinit)
+ADD_FUN_PTR_TO_SEC(array_preinit[1], ".preinit_array", fun_preinit);
+
+MAKE_FUN(fun_init)
+ADD_FUN_PTR_TO_SEC(array_init[1], ".init_array", fun_init);
+
+MAKE_FUN(fun_fini)
+ADD_FUN_PTR_TO_SEC(array_fini[1], ".fini_array", fun_fini);
+
+/* NOTE: there is NO .prefini_array */
+
+int main() {
+    return 0;
+}
