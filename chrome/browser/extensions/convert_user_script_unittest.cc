701c7eff4be2a7c5f2414d221104d8e86d80fb01
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-14 22:18:51 +0000

    Delete temporary directories created by ExtensionFromUserScript tests.
    BUG=58369
    TEST=running unit_tests --gtest_filter=ExtensionFromUserScript.* does not leave extra files in ~/.config/chromium/Temp.
    Review URL: http://codereview.chromium.org/3708005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@62658 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bd9ca3554160c3e4cac9ff289d2713d216fcbd6f

--- chrome/browser/extensions/convert_user_script_unittest.cc
+++ chrome/browser/extensions/convert_user_script_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -49,6 +49,9 @@ TEST(ExtensionFromUserScript, Basic) {
       extension->path().Append(script.js_scripts()[0].relative_path())));
   EXPECT_TRUE(file_util::PathExists(
       extension->path().Append(Extension::kManifestFilename)));
+
+  // Cleanup
+  file_util::Delete(extension->path(), true);
 }
 
 TEST(ExtensionFromUserScript, NoMetdata) {
@@ -85,4 +88,7 @@ TEST(ExtensionFromUserScript, NoMetdata) {
       extension->path().Append(script.js_scripts()[0].relative_path())));
   EXPECT_TRUE(file_util::PathExists(
       extension->path().Append(Extension::kManifestFilename)));
+
+  // Cleanup
+  file_util::Delete(extension->path(), true);
 }
