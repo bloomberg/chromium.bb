35f14ff3594cf17082c95a2cd43d3797e88bf0ec
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-10-14 20:02:05 +0000

    Adding a few exception handling tests to the toolchain tests. Also add more members to the "toolchain" test category.
    These tests do not strive to be exhaustive but are intented to help with
    the pnacl eh work and prevent gross regressions.
    
    Review URL: http://codereview.chromium.org/3824003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3482 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 2402868925b2ea6368e78a21b1e7ab16cd8f35c8

---
+++ tests/toolchain/eh_helper.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2010 The Native Client Authors.  All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can
+ * be found in the LICENSE file.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int g_step = 0;
+
+void next_step(int n) {
+  printf("%d -> %d\n", g_step, n);
+  if (g_step != n - 1) {
+    printf("ERROR: bad step\n");
+    abort();
+  }
+  g_step = n;
+}
