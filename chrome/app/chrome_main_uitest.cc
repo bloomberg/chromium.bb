ea8ce7e7b63e38fc696f13b4cf9b4f27207eabb1
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-19 09:11:50 +0000

    GTTF: Remove flaky and/or useless UI tests.
    An example of a flake:
    
    ChromeMainTest.AppLaunch:
    c:\b\slave\win\build\src\chrome\app\chrome_main_uitest.cc(28):
    error: Value of: UITest::GetBrowserProcessCount()
      Actual: 3
    Expected: 2
    
    BUG=none
    TEST=ui_tests
    
    Review URL: http://codereview.chromium.org/3796009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63033 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5a0d44252b20556142f45205669035f5622e87ec

--- chrome/app/chrome_main_uitest.cc
+++ chrome/app/chrome_main_uitest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,35 +9,6 @@
 #include "net/base/net_util.h"
 
 typedef UITest ChromeMainTest;
-
-// Launch the app, then close the app.
-TEST_F(ChromeMainTest, AppLaunch) {
-  // If we make it here at all, we've succeeded in retrieving the app window
-  // in UITest::SetUp()--otherwise we'd fail with an exception in SetUp().
-
-  if (UITest::in_process_renderer()) {
-    EXPECT_EQ(1, UITest::GetBrowserProcessCount());
-  } else {
-#if defined(OS_LINUX)
-    // On Linux we'll have four processes: browser, renderer, zygote and
-    // sandbox helper.
-    EXPECT_EQ(4, UITest::GetBrowserProcessCount());
-#else
-    // We should have two instances of the browser process alive -
-    // one is the Browser and the other is the Renderer.
-    EXPECT_EQ(2, UITest::GetBrowserProcessCount());
-#endif
-  }
-}
-
-// Make sure that the testing interface is there and giving reasonable answers.
-TEST_F(ChromeMainTest, AppTestingInterface) {
-  int window_count = 0;
-  EXPECT_TRUE(automation()->GetBrowserWindowCount(&window_count));
-  EXPECT_EQ(1, window_count);
-
-  EXPECT_EQ(1, GetTabCount());
-}
 
 #if !defined(OS_MACOSX)
 // These tests don't apply to the Mac version; see
@@ -64,5 +35,4 @@ TEST_F(ChromeMainTest, ReuseBrowserInstanceWhenOpeningFile) {
 
   ASSERT_TRUE(automation()->IsURLDisplayed(net::FilePathToFileURL(test_file)));
 }
-
 #endif  // !OS_MACOSX
