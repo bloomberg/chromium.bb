1d70c226e6d47617a2147ad252f9d7691fd25f1b
dumi@chromium.org <dumi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-20 22:52:13 +0000

    Revert 63271 - Add the FileRef and FileIO tests to ppapi_uitest.cc
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/3883003
    
    TBR=dumi@chromium.org
    Review URL: http://codereview.chromium.org/4019001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63282 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b018fedfa6c358ae77a241d68a8b626763af2c73

--- chrome/test/ui/ppapi_uitest.cc
+++ chrome/test/ui/ppapi_uitest.cc
@@ -106,7 +106,7 @@ TEST_F(PPAPITest, FLAKY_ImageData) {
   RunTest("ImageData");
 }
 
-TEST_F(PPAPITest, FAILS_Buffer) {
+TEST_F(PPAPITest, Buffer) {
   RunTest("Buffer");
 }
 
@@ -135,12 +135,4 @@ TEST_F(PPAPITest, CharSet) {
 
 TEST_F(PPAPITest, Var) {
   RunTest("Var");
-}
-
-TEST_F(PPAPITest, FileRef) {
-  RunTestViaHTTP("FileRef");
-}
-
-TEST_F(PPAPITest, FileIO) {
-  RunTestViaHTTP("FileIO");
 }
