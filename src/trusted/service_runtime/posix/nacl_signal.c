099f993a0e17d0826b84a23bffda3b8470bdc1f0
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-10-20 09:24:15 +0000

    Work around sigaltstack() bug on Mac OS X when unregistering a signal stack
    This bug manifests itself when building with
    "-mmacosx-version-min=10.5", which is necessary in order to build
    sel_ldr for x86-64 but which causes a newer version of libc's
    sigaltstack() wrapper to be used.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1051
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1053
    TEST=run_hello_world_test when SConstruct is changed to use
      "-mmacosx-version-min=10.5" instead of 10.4
    
    Review URL: http://codereview.chromium.org/3857001
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3504 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 517d2fe7e24336767bf62a3ab866ced6389962e1

--- src/trusted/service_runtime/posix/nacl_signal.c
+++ src/trusted/service_runtime/posix/nacl_signal.c
@@ -146,7 +146,18 @@ void NaClSignalStackUnregister(void) {
    * fault, although that is unlikely.
    */
   stack_t st;
+#if NACL_OSX
+  /*
+   * This is a workaround for a bug in Mac OS X's libc, in which new
+   * versions of the sigaltstack() wrapper return ENOMEM if ss_size is
+   * less than MINSIGSTKSZ, even when ss_size should be ignored
+   * because we are unregistering the signal stack.
+   * See http://code.google.com/p/nativeclient/issues/detail?id=1053
+   */
+  st.ss_size = MINSIGSTKSZ;
+#else
   st.ss_size = 0;
+#endif
   st.ss_sp = NULL;
   st.ss_flags = SS_DISABLE;
   if (sigaltstack(&st, NULL) != 0) {
