165bfff2a8ab33e74ee455fa35d4ff9936484cb2
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-10-20 19:15:04 +0000

    Add a new test to reproduce a failure for pnacl-x86-64
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=1069
    
    Review URL: http://codereview.chromium.org/3924002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3513 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 1dd6d7ddb082df987c7f9f8eaf86f873ebb734e0

---
+++ tests/toolchain/return_address.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2010 The Native Client Authors.  All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can
+ * be found in the LICENSE file.
+ */
+
+/* NOTE: because of fun pointer casting we need to disable -padantic. */
+/* NOTE: because of aggressive inlining we need to disable -O2. */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]);
+
+#define ASSERT(cond, message) if (!(cond) ) { puts(message); abort(); }
+
+void recurse(int n, int is_first) {
+  void* ra = (void*) __builtin_return_address (0);
+  void* era = (void*) __builtin_extract_return_addr(ra);
+  printf("ra: %p %p\n", ra, era);
+  ASSERT(ra == era,
+         "ERROR: until we know better we assert that ra == era\n");
+
+  if (is_first) {
+    ASSERT((void*) main < ra,
+           "ERROR: ra to main is off\n");
+  } else {
+    ASSERT((void*) recurse < ra && ra < (void*) main,
+           "ERROR: ra to recurse is off\n");
+  }
+
+  if (n == 0) return;
+  recurse(n - 1, 0);
+}
+
+int main(int argc, char* argv[]) {
+    /* NOTE: confuse optimizer, argc is never 5555 */
+  if (argc != 5555 ) {
+    argc = 10;
+  }
+  printf("main %p recurse %p\n", (void*) main, (void*) recurse);
+  ASSERT((void*) recurse < (void*) main,
+         "ERROR: this test assumes that main() follows recurse()\n");
+
+  recurse(argc, 1);
+  return 55;
+}
