540193dfaefbe1acb40b914ed4b4a53480761a13
jvoung@google.com <jvoung@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-10-20 21:47:04 +0000

    Update LLVM stable rev to include: http://codereview.chromium.org/3967001/show Review URL: http://codereview.chromium.org/3994001
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3516 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 7183f65cd3083986d8e2987b926d82a112cb92e4

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -121,7 +121,7 @@ readonly PNACL_SB_X8664="${PNACL_SB_ROOT}/x8664"
 
 # Current milestones in each repo
 # hg-update-stable  uses these
-readonly LLVM_REV=cb097058866f
+readonly LLVM_REV=b49708a2d6a7
 readonly LLVM_GCC_REV=84f105f4ca85
 readonly NEWLIB_REV=96671ff78208
 readonly BINUTILS_REV=a5b54c0cc733
