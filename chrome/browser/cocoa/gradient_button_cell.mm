df4ca326b645d5a1b5db739e7af47ac413b7a168
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-21 11:52:48 +0000

    Mac: Fix corner case of fading out Bookmark bar buttons for no reason
    The bug report was that you could create bookmark bar items that weren't truncated but still got the fadeout effect we use for truncated bookmark bar items.
    
    The reason this was happening was because our calculation of when Cocoa was going to clip the text was off. Solution: Add a slop value to better match Cocoa.
    
    BUG=53868
    TEST=Repro steps in bug.
    
    Review URL: http://codereview.chromium.org/3872004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63354 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8ddb57924349567a94419878536c781cd7791847

--- chrome/browser/cocoa/gradient_button_cell.mm
+++ chrome/browser/cocoa/gradient_button_cell.mm
@@ -641,8 +641,11 @@ static const NSTimeInterval kAnimationContinuousCycleDuration = 0.4;
              inView:(NSView *)controlView {
   NSSize size = [title size];
 
-  // Don't complicate drawing unless we need to clip
-  if (floor(size.width) <= NSWidth(cellFrame)) {
+  // Empirically, Cocoa will draw an extra 2 pixels past NSWidth(cellFrame)
+  // before it clips the text.
+  const CGFloat kOverflowBeforeClip = 2;
+  // Don't complicate drawing unless we need to clip.
+  if (floor(size.width) <= (NSWidth(cellFrame) + kOverflowBeforeClip)) {
     return [super drawTitle:title withFrame:cellFrame inView:controlView];
   }
 
