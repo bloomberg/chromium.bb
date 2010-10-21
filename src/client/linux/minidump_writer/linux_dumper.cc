43378265bf2483278bc70b32c59c3e47ad5b7ef8
ted.mielczarek <ted.mielczarek@4c0a9323-5329-0410-9bdc-e9ce6186880e>
2010-10-21 13:55:07 +0000

    Add asm/ptrace.h include to linux_dumper.cc to fix the silly scratchbox toolchain we're using for Maemo builds. Also shuffle around the include order to comply with style guidelines, while I'm here.
    git-svn-id: http://google-breakpad.googlecode.com/svn/trunk@718 4c0a9323-5329-0410-9bdc-e9ce6186880e

--- src/client/linux/minidump_writer/linux_dumper.cc
+++ src/client/linux/minidump_writer/linux_dumper.cc
@@ -34,24 +34,23 @@
 
 #include "client/linux/minidump_writer/linux_dumper.h"
 
+#include <asm/ptrace.h>
 #include <assert.h>
+#include <elf.h>
+#include <errno.h>
+#include <fcntl.h>
 #include <limits.h>
+#if !defined(__ANDROID__)
+#include <link.h>
+#endif
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
-#include <unistd.h>
-#include <elf.h>
-#include <errno.h>
-#include <fcntl.h>
-#if !defined(__ANDROID__)
-#include <link.h>
-#endif
-
 #include <sys/types.h>
 #include <sys/ptrace.h>
 #include <sys/wait.h>
+#include <unistd.h>
 
 #include <algorithm>
 
