69dfa5219fe008fb30019d7c8c79b3844951bd46
vrk@google.com <vrk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-22 23:16:55 +0000

    Fixing valgrind error because of mem leak in BuffereDataSource unit tests
    Forgot to stop the DataSource at the end of the ReadHang test.
    
    BUG=NONE
    TEST=test_shell_tests
    
    Review URL: http://codereview.chromium.org/4059002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@63592 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9fe9ec2e360b0c61bbdb134fc63cc4fc5af36da1

--- webkit/glue/media/buffered_data_source_unittest.cc
+++ webkit/glue/media/buffered_data_source_unittest.cc
@@ -772,6 +772,10 @@ class BufferedDataSourceTest : public testing::Test {
     EXPECT_CALL(*loader_, Stop());
     data_source_->Abort();
     message_loop_->RunAllPending();
+
+    // The loader has now been stopped. Set this to null so that when the
+    // DataSource is stopped, it does not expect a call to stop the loader.
+    loader_ = NULL;
   }
 
   void ReadDataSourceMiss(int64 position, int size) {
@@ -966,6 +970,7 @@ TEST_F(BufferedDataSourceTest, ReadCacheMiss) {
 TEST_F(BufferedDataSourceTest, ReadHang) {
   InitializeDataSource(kHttpUrl, net::OK, true, 25, LOADING);
   ReadDataSourceHang(10, 10);
+  StopDataSource();
 }
 
 TEST_F(BufferedDataSourceTest, ReadFailed) {
