acc762d5035412fa812847c4186fd88aa19bd695
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-10-27 22:45:17 +0000

    Update PNaCl TC stable rev to include
    http://codereview.chromium.org/4141003
     enable pnacl's unwind api extensions for x86-64
    
    http://codereview.chromium.org/4190008
     work around pointer size issues in the pnacl-x86-64 frontend
    
    Review URL: http://codereview.chromium.org/4189004
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3577 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: fb971e8bbc12b4f190f3af44540307014810ff84

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -122,7 +122,7 @@ readonly PNACL_SB_X8664="${PNACL_SB_ROOT}/x8664"
 # Current milestones in each repo
 # hg-update-stable  uses these
 readonly LLVM_REV=65e9a2c9cb0f
-readonly LLVM_GCC_REV=a755d544d2c1
+readonly LLVM_GCC_REV=1755e61d212c
 readonly NEWLIB_REV=96671ff78208
 readonly BINUTILS_REV=a5b54c0cc733
 
