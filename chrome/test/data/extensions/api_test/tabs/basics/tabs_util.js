a1d1cb55c20deccadcdb39b310fd98e48b3846b3
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-10-28 00:30:15 +0000

    Revert "Split ExtensionApiTest.Tabs into several smaller tests and re-enable."
    This reverts commit b20544e07006eff741c598abd16d98b30f01f00e.
    
    TBR=ncarter@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64187 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4a3bc6ef32ccff0ac7b19893e5940ea65b735b15

--- chrome/test/data/extensions/api_test/tabs/basics/tabs_util.js
+++ chrome/test/data/extensions/api_test/tabs/basics/tabs_util.js
@@ -1,9 +1,26 @@
-var pass = chrome.test.callbackPass;
-var assertEq = chrome.test.assertEq;
-var assertTrue = chrome.test.assertTrue;
+// Utility functions to help with tabs/windows testing.
 
-function pageUrl(letter) {
-  return chrome.extension.getURL(letter + ".html");
+// Removes current windows and creates one window with tabs set to
+// the urls in the array |tabUrls|. At least one url must be specified.
+// The |callback| should look like function(windowId, tabIds) {...}.
+function setupWindow(tabUrls, callback) {
+  createWindow(tabUrls, {}, function(winId, tabIds) {
+    // Remove all other windows.
+    var removedCount = 0;
+    chrome.windows.getAll({}, function(windows) {
+      for (var i in windows) {
+        if (windows[i].id != winId) {
+          chrome.windows.remove(windows[i].id, function() {
+            removedCount++;
+            if (removedCount == windows.length - 1)
+              callback(winId, tabIds);
+          });
+        }
+      }
+      if (windows.length == 1)
+        callback(winId, tabIds);
+    });
+  });
 }
 
 // Creates one window with tabs set to the urls in the array |tabUrls|.
@@ -50,3 +67,4 @@ function waitForAllTabs(callback) {
   }
   waitForTabs();
 }
+
