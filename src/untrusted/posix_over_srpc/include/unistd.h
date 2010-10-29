e02b1065ac722fafcd1994b5b48a51724aa69bda
krasin@google.com <krasin@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-10-29 09:18:45 +0000

    Remove code of posix_over_srpc.
    Review URL: http://codereview.chromium.org/4189009
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3594 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 32f3d968bbc16e03093cb1a3c4d67d943957260d

--- src/untrusted/posix_over_srpc/include/unistd.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UNTRUSTED_POSIX_OVER_SRPC_INCLUDE_UNISTD_H_
-#define _UNTRUSTED_POSIX_OVER_SRPC_INCLUDE_UNISTD_H_
-
-#include <sys/unistd.h>
-
-char* getwd(char *__buf);
-
-#endif  /* _UNTRUSTED_POSIX_OVER_SRPC_INCLUDE_UNISTD_H_ */
