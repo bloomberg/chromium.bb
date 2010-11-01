ced66a5927a7caddab8cbc59e2d170c8595e12c0
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-01 22:17:24 +0000

    Disable TestHTTPSExpiredCertAndGoBackViaButton on win
    Times out:
    
    SSLUITest.TestHTTPSExpiredCertAndGoBackViaButton:
    Did not complete.
    
    BUG=43575
    
    TEST=
    TBR=wtc
    
    Review URL: http://codereview.chromium.org/4244001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64670 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: acaac4133683a7d5cacf6977b89d124dd2537ee6

--- chrome/browser/ssl/ssl_browser_tests.cc
+++ chrome/browser/ssl/ssl_browser_tests.cc
@@ -230,6 +230,11 @@ IN_PROC_BROWSER_TEST_F(SSLUITest, MAYBE_TestHTTPSExpiredCertAndDontProceed) {
 }
 
 // Visits a page with https error and then goes back using Browser::GoBack.
+#if defined(OS_WIN)
+// Disabled on win. Times out. crbug.com/43575
+#define TestHTTPSExpiredCertAndGoBackViaButton \
+    DISABLED_TestHTTPSExpiredCertAndGoBackViaButton
+#endif
 IN_PROC_BROWSER_TEST_F(SSLUITest, TestHTTPSExpiredCertAndGoBackViaButton) {
   ASSERT_TRUE(test_server()->Start());
   ASSERT_TRUE(https_server_expired_.Start());
