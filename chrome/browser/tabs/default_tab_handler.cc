17765c573ea0b115ce31287c555355f1a3e0db5c
mmortensen@google.com <mmortensen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-01 19:22:13 +0000

    Fix indentation of a continuation. BUG=none TEST=none
    Review URL: http://codereview.chromium.org/4148010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@64643 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8898ec205da3a100d12ff5ddb64eab993de3cf67

--- chrome/browser/tabs/default_tab_handler.cc
+++ chrome/browser/tabs/default_tab_handler.cc
@@ -13,7 +13,7 @@
 DefaultTabHandler::DefaultTabHandler(TabHandlerDelegate* delegate)
     : delegate_(delegate),
       ALLOW_THIS_IN_INITIALIZER_LIST(
-        model_(new TabStripModel(this, delegate->GetProfile()))) {
+          model_(new TabStripModel(this, delegate->GetProfile()))) {
   UmaNaclHistogramEnumeration(FIRST_TAB_NACL_BASELINE);
   model_->AddObserver(this);
 }
