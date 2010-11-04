c87b6b0dcbe8749f54d4e2fd148985b441e1bde9
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-04 16:21:33 +0000

    Support for using OS-native certificates for SSL client auth.
    Known Limitations:
    - Only SSL3/TLS1.0 handshakes are supported. It's unlikely
      SSLv2 will/should ever be implemented. NSS does not yet
      support TLS1.1/1.2.
    - On Windows, only CryptoAPI keys are supported. Keys that
      can only be accessed via CNG will fail.
    
    Technical Notes:
    Windows: 
    - Only the AT_KEYEXCHANGE key is used, per
    http://msdn.microsoft.com/en-us/library/aa387461(VS.85).aspx 
    - CryptSetHashParam is used to directly set the hash value.
      This *should* be supported by all CSPs that are compatible
      with RSA/SChannel, AFAICT, but testing is needed.
    
    NSS:
    - The define NSS_PLATFORM_CLIENT_AUTH is used to guard all
      of the new/patched code. The primary implementation
      details are in sslplatf.c.
    
    Patch author: Ryan Sleevi <rsleevi@chromium.org>
    Original review URL: http://codereview.chromium.org/2828002
    
    BUG=148,37560,45369
    TEST=Attempt to authenticate with a site that requires SSL
    client authentication (e.g., https://foaf.me/simpleLogin.php
    with a FOAF+SSL client certificate).
    Review URL: http://codereview.chromium.org/3455019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65064 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4a842345af02ac3ae84015683383539a84f66d8c

--- net/socket/ssl_client_socket_nss_factory.cc
+++ net/socket/ssl_client_socket_nss_factory.cc
@@ -4,12 +4,8 @@
 
 #include "net/socket/client_socket_factory.h"
 
-#include "build/build_config.h"
 #include "net/socket/ssl_client_socket_nss.h"
 #include "net/socket/ssl_host_info.h"
-#if defined(OS_WIN)
-#include "net/socket/ssl_client_socket_win.h"
-#endif
 
 // This file is only used on platforms where NSS is not the system SSL
 // library.  When compiled, this file is the only object module that pulls
@@ -24,14 +20,6 @@ SSLClientSocket* SSLClientSocketNSSFactory(
     const SSLConfig& ssl_config,
     SSLHostInfo* ssl_host_info) {
   scoped_ptr<SSLHostInfo> shi(ssl_host_info);
-  // TODO(wtc): SSLClientSocketNSS can't do SSL client authentication using
-  // CryptoAPI yet (http://crbug.com/37560), so we fall back on
-  // SSLClientSocketWin.
-#if defined(OS_WIN)
-  if (ssl_config.send_client_cert)
-    return new SSLClientSocketWin(transport_socket, hostname, ssl_config);
-#endif
-
   return new SSLClientSocketNSS(transport_socket, hostname, ssl_config,
                                 shi.release());
 }
