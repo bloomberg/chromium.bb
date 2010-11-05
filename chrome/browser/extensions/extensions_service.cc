da6ba6fefbd98c89c73575cc580315ef3de15d29
jstritar@chromium.org <jstritar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-05 15:27:34 +0000

    Don't record component extensions crash reports
    BUG=58072
    TEST=After crashing the browser on Windows (goto about:inducebrowsercrashforrealz), component extensions should not be in the crash report.
    
    Review URL: http://codereview.chromium.org/4050004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65205 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2a500c1b16d2bf03e3841287f6741869c47d48d2

--- chrome/browser/extensions/extensions_service.cc
+++ chrome/browser/extensions/extensions_service.cc
@@ -1568,7 +1568,8 @@ void ExtensionsService::OnExtensionLoaded(const Extension* extension,
 void ExtensionsService::UpdateActiveExtensionsInCrashReporter() {
   std::set<std::string> extension_ids;
   for (size_t i = 0; i < extensions_.size(); ++i) {
-    if (!extensions_[i]->is_theme())
+    if (!extensions_[i]->is_theme() &&
+        extensions_[i]->location() != Extension::COMPONENT)
       extension_ids.insert(extensions_[i]->id());
   }
 
