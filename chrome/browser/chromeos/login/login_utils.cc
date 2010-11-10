b085e2173f375765fc39323f690a012100821a83
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 20:49:55 +0000

    fix broken cros build.
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/4718002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65700 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 61020f3647770a9369a9342114c11e58fbd5d2f5

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -191,7 +191,7 @@ void LoginUtilsImpl::CompleteLogin(
 
   // Set the CrOS user by getting this constructor run with the
   // user's email on first retrieval.
-  profile->GetProfileSyncService(username)->SetPassphrase(password);
+  profile->GetProfileSyncService(username)->SetPassphrase(password, false);
   btl->AddLoginTimeMarker("SyncStarted", false);
 
   // Attempt to take ownership; this will fail if device is already owned.
