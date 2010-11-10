40fc0aa24a8ee2f04c93efd514de2fe8765c20b0
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-10 23:47:56 +0000

    Adding CHECKs to troubleshoot the crash issue intuple.h/browser_render_process_host.cc (see bug description)
     This is chromeos only and will be removed once I correct data.
    
    BUG=chromium-os:7327
    TEST=none
    
    Review URL: http://codereview.chromium.org/4752003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@65729 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d0b2afe6eeae8b04bc2b21b6503a2aaa5c9b38ab

--- chrome/browser/renderer_host/browser_render_process_host.cc
+++ chrome/browser/renderer_host/browser_render_process_host.cc
@@ -857,6 +857,12 @@ void BrowserRenderProcessHost::OnMessageReceived(const IPC::Message& msg) {
   if (deleting_soon_)
     return;
 
+#if defined(OS_CHROMEOS)
+  // To troubleshoot crosbug.com/7327.
+  CHECK(this);
+  CHECK(&msg);
+#endif
+
   mark_child_process_activity_time();
   if (msg.routing_id() == MSG_ROUTING_CONTROL) {
     // Dispatch control messages.
