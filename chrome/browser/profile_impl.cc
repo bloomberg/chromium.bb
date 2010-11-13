d15e14888cc65c5bfaa6146665cffc9d28451a38
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-13 19:15:23 +0000

    linux: fix cache corruption when using multiple --user-data-dir
    This regressed in r59021.  I think I can fix it in a cleaner way
    with some refactoring, but for now since I want to cherry-pick this
    onto older branches let's revert some of r59021 to fix this in
    the simplest way possible.
    
    BUG=59824
    TEST=manual (will write a test in my followup that refactors it)
    
    Review URL: http://codereview.chromium.org/4911002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66066 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 493edd0e0d05d5a20e43f22463a62c1ad8ffed64

--- chrome/browser/profile_impl.cc
+++ chrome/browser/profile_impl.cc
@@ -293,15 +293,21 @@ ProfileImpl::ProfileImpl(const FilePath& path)
   // for a spec on where cache files go.  The net effect for most systems is we
   // use ~/.cache/chromium/ for Chromium and ~/.cache/google-chrome/ for
   // official builds.
+  // If we're using a different user-data-dir, though, fall through
+  // to the "normal" cache directory (a subdirectory of that).
+  // TODO(evan): all of this logic belongs in path_service; refactor and remove
+  // this IsOverriden business.
+  if (!PathService::IsOverridden(chrome::DIR_USER_DATA)) {
 #if defined(GOOGLE_CHROME_BUILD)
-  const char kCacheDir[] = "google-chrome";
+    const char kCacheDir[] = "google-chrome";
 #else
-  const char kCacheDir[] = "chromium";
+    const char kCacheDir[] = "chromium";
 #endif
-  PathService::Get(base::DIR_USER_CACHE, &base_cache_path_);
-  base_cache_path_ = base_cache_path_.Append(kCacheDir);
-  if (!file_util::PathExists(base_cache_path_))
-    file_util::CreateDirectory(base_cache_path_);
+    PathService::Get(base::DIR_USER_CACHE, &base_cache_path_);
+    base_cache_path_ = base_cache_path_.Append(kCacheDir);
+    if (!file_util::PathExists(base_cache_path_))
+      file_util::CreateDirectory(base_cache_path_);
+  }
 #endif
   if (base_cache_path_.empty())
     base_cache_path_ = path_;
