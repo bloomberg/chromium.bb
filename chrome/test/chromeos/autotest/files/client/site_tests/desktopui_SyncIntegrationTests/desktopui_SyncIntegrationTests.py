f4424301a2d2623ce5390572b7c283a6159c9766
cmasone@chromium.org <cmasone@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-15 20:53:53 +0000

    [Chrome OS] Cleanup and modify Chrome-for-ChromeOS autotest wrappers
    Some deps in the chrome tree and support classes on the Chrome OS side
    have changed, so update our autotest wrappers to work again.  The new
    code also takes advantage of some new features in the ChromeTestBase class.
    
    I've also split the control files to each run 1/4 of the browser tests and ui tests.  I tried to come up with a way to divide them more cleanly, but this seems the best way to split them across multiple VMs with the infrastructure on the bots.  If we used autoserv to run smoke suite, we could be more clever here, but that's a big hammer for this problem.
    
    BUG=chromium-os:8544
    TEST=install chrome with tests, then run desktopui_BrowserTest and friends.  They should be able to run, though not necessarily succeed.
    
    Review URL: http://codereview.chromium.org/4698004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66168 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8d8f11d7abee837d6fd15ba9c7d87efdca5d4707

--- chrome/test/chromeos/autotest/files/client/site_tests/desktopui_SyncIntegrationTests/desktopui_SyncIntegrationTests.py
+++ chrome/test/chromeos/autotest/files/client/site_tests/desktopui_SyncIntegrationTests/desktopui_SyncIntegrationTests.py
@@ -1,19 +1,15 @@
-# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
 from autotest_lib.client.bin import site_chrome_test
 
 class desktopui_SyncIntegrationTests(site_chrome_test.ChromeTestBase):
     version = 1
 
     def run_once(self):
-        dep = 'chrome_test'
-        dep_dir = os.path.join(self.autodir, 'deps', dep)
-        password_file = '%s/sync_password.txt' % dep_dir
+        password_file = '%s/sync_password.txt' % self.dep_dir
 
         self.run_chrome_test('sync_integration_tests',
             ('--password-file-for-test=%s ' +
              '--test-terminate-timeout=300000') % password_file)
-
--- chrome/test/chromeos/autotest/files/client/site_tests/desktopui_UITest/desktopui_UITest.py
+++ chrome/test/chromeos/autotest/files/client/site_tests/desktopui_UITest/desktopui_UITest.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 The Chromium OS Authors. All rights reserved.
+# Copyright (c) 2010 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -7,6 +7,12 @@ from autotest_lib.client.bin import site_chrome_test
 class desktopui_UITest(site_chrome_test.ChromeTestBase):
     version = 1
 
-    def run_once(self):
-        self.run_test('ui_tests', '')
+    binary_to_run='ui_tests'
+    blacklist = []
 
+    def run_once(self, group=0, total_groups=4):
+        tests_to_run = self.filter_bad_tests(
+            self.generate_test_list(self.binary_to_run, group, total_groups))
+        tests_to_run.extend(map(lambda(x): '-'+x, self.blacklist))
+        self.run_chrome_test(self.binary_to_run,
+                             '--gtest_filter=%s' % ':'.join(tests_to_run))
