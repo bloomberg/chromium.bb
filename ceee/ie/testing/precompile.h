935958cb69628c07e1b0b713dc41ce06dbf48a49
siggi@chromium.org <siggi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-17 20:14:06 +0000

    Revert 66501 - Removing now-redundant precompile files.
    This broke the windows compile as I forgot to remove gyp file references to them.
    
    BUG=none
    TEST=it compiles
    
    Review URL: http://codereview.chromium.org/5171002
    
    TBR=siggi@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66509 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8da2a7eed4598ad578d6da528541ab8b7654f75e

---
+++ ceee/ie/testing/precompile.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Precompile header for IE CEEE unittests.
+
+#ifndef CEEE_IE_TESTING_PRECOMPILE_H_
+#define CEEE_IE_TESTING_PRECOMPILE_H_
+
+#include <atlbase.h>
+#include <atlcom.h>
+#include <atlstr.h>
+
+#include "ceee/testing/utils/mock_com.h"
+#include "ceee/testing/utils/test_utils.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#endif  // CEEE_IE_TESTING_PRECOMPILE_H_
