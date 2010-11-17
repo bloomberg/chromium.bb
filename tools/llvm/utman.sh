07879e7fdb935a6a4db16f75fc2598882b06b2e3
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-11-17 20:15:42 +0000

    Update utman stable to include: http://codereview.chromium.org/5025005/ http://codereview.chromium.org/5012003/ http://codereview.chromium.org/4999002/
    Review URL: http://codereview.chromium.org/5188001
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3711 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 620ddf74168c67ad6d794a0dbdc38e856394b9b4

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -121,8 +121,8 @@ readonly PNACL_SB_X8664="${PNACL_SB_ROOT}/x8664"
 
 # Current milestones in each repo
 # hg-update-stable  uses these
-readonly LLVM_REV=71a4ab27c90a
-readonly LLVM_GCC_REV=c3ee8449a78f
+readonly LLVM_REV=8a11e217a840
+readonly LLVM_GCC_REV=b61967c2a266
 readonly NEWLIB_REV=96671ff78208
 readonly BINUTILS_REV=a5b54c0cc733
 
