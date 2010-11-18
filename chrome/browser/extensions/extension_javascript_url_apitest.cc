dad1f9005958a8ff1e6959000207b6e881c5489b
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-18 09:17:53 +0000

    Disable ExtensionApiTest.JavaScriptURLPermissions, it flakily hits an assertion.
    TBR=aa
    
    BUG=63589
    TEST=browser_tests
    Review URL: http://codereview.chromium.org/5221001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66600 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a894250823ff98c8856bb71f7c8c6e05b521094e

--- chrome/browser/extensions/extension_javascript_url_apitest.cc
+++ chrome/browser/extensions/extension_javascript_url_apitest.cc
@@ -5,7 +5,8 @@
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "net/base/mock_host_resolver.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, JavaScriptURLPermissions) {
+// Disabled, http://crbug.com/63589.
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_JavaScriptURLPermissions) {
   host_resolver()->AddRule("a.com", "127.0.0.1");
   host_resolver()->AddRule("b.com", "127.0.0.1");
   ASSERT_TRUE(StartTestServer());
