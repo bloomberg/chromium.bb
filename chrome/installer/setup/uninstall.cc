bloomberg/chromium.bb

commit e762333a7007c42e3bba98a3192676cead897363

Renames the function CreateTemporaryFilename to CreateTemporaryFile and track down all callers, also removes the deprecated function that uses std::wstring.
BUG=3078 (http://crbug.com/3078)
TEST=run base_unittests, installer_util_unittests, net_unittests, setup_unittests, and unit_tests.

Review URL: http://codereview.chromium.org/164537
Patch from Thiago Farina.

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@23631 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 33edeab42b851a5617a77caa1d65a3782a01ee7b

chrome/installer/setup/uninstall.cc
@@ -158,22 +158,25 @@ bool DeleteFilesAndFolders(const std::wstring& exe_path, bool system_uninstall,
       install_path, installed_version.GetString()));
   file_util::AppendToPath(&setup_exe, file_util::GetFilenameFromPath(exe_path));
 
-  std::wstring temp_file;
-  if (!file_util::CreateTemporaryFileName(&temp_file))
+  FilePath temp_file;
+  if (!file_util::CreateTemporaryFile(&temp_file)) {
     LOG(ERROR) << "Failed to create temporary file for setup.exe.";
-  else
-    file_util::Move(setup_exe, temp_file);
+  } else {
+    FilePath setup_exe_path = FilePath::FromWStringHack(setup_exe);
+    file_util::Move(setup_exe_path, temp_file);
+  }
 
   // Move the browser's persisted local state
   FilePath user_local_state;
   if (chrome::GetDefaultUserDataDirectory(&user_local_state)) {
-    std::wstring user_local_file(
-        user_local_state.Append(chrome::kLocalStateFilename).value());
+    FilePath user_local_file(
+        user_local_state.Append(chrome::kLocalStateFilename));
 
-    if (!file_util::CreateTemporaryFileName(local_state_path))
+    FilePath path = FilePath::FromWStringHack(*local_state_path);
+    if (!file_util::CreateTemporaryFile(&path))
       LOG(ERROR) << "Failed to create temporary file for Local State.";
-    else 
-      file_util::CopyFile(user_local_file, *local_state_path);
+    else
+      file_util::CopyFile(user_local_file, path);
   }
 
   LOG(INFO) << "Deleting install path " << install_path;
@@ -317,7 +320,7 @@ bool installer_setup::DeleteChromeRegistrationKeys(HKEY root,
   file_util::AppendToPath(&app_path_key, installer_util::kChromeExe);
   DeleteRegistryKey(key, app_path_key);
 
-  //Cleanup OpenWithList
+  // Cleanup OpenWithList
   for (int i = 0; ShellUtil::kFileAssociations[i] != NULL; i++) {
     std::wstring open_with_key(ShellUtil::kRegClasses);
     file_util::AppendToPath(&open_with_key, ShellUtil::kFileAssociations[i]);

base/file_util.cc
@@ -343,13 +343,6 @@ bool CreateNewTempDirectory(const std::wstring& prefix,
   *new_temp_path = temp_path.ToWStringHack();
   return true;
 }
-bool CreateTemporaryFileName(std::wstring* temp_file) {
-  FilePath temp_file_path;
-  if (!CreateTemporaryFileName(&temp_file_path))
-    return false;
-  *temp_file = temp_file_path.ToWStringHack();
-  return true;
-}
 bool Delete(const std::wstring& path, bool recursive) {
   return Delete(FilePath::FromWStringHack(path), recursive);
 }

base/file_util.h
@@ -283,12 +283,7 @@ bool GetShmemTempDir(FilePath* path);
 // Creates a temporary file. The full path is placed in |path|, and the
 // function returns true if was successful in creating the file. The file will
 // be empty and all handles closed after this function returns.
-// TODO(erikkay): rename this function and track down all of the callers.
-// (Clarification of erik's comment: the intent is to rename the BlahFileName()
-//  calls into BlahFile(), since they create temp files (not temp filenames).)
-bool CreateTemporaryFileName(FilePath* path);
-// Deprecated temporary compatibility function.
-bool CreateTemporaryFileName(std::wstring* temp_file);
+bool CreateTemporaryFile(FilePath* path);
 
 // Create and open a temporary file.  File is opened for read/write.
 // The full path is placed in |path|, and the function returns true if
@@ -300,9 +295,9 @@ FILE* CreateAndOpenTemporaryShmemFile(FilePath* path);
 // Similar to CreateAndOpenTemporaryFile, but the file is created in |dir|.
 FILE* CreateAndOpenTemporaryFileInDir(const FilePath& dir, FilePath* path);
 
-// Same as CreateTemporaryFileName but the file is created in |dir|.
-bool CreateTemporaryFileNameInDir(const std::wstring& dir,
-                                  std::wstring* temp_file);
+// Same as CreateTemporaryFile but the file is created in |dir|.
+bool CreateTemporaryFileInDir(const FilePath& dir,
+                              FilePath* temp_file);
 
 // Create a new directory under TempPath. If prefix is provided, the new
 // directory name is in the format of prefixyyyy.

base/file_util_posix.cc
@@ -387,7 +387,7 @@ int CreateAndOpenFdForTemporaryFile(FilePath directory, FilePath* path) {
   return mkstemp(buffer);
 }
 
-bool CreateTemporaryFileName(FilePath* path) {
+bool CreateTemporaryFile(FilePath* path) {
   FilePath directory;
   if (!GetTempDir(&directory))
     return false;
@@ -413,10 +413,9 @@ FILE* CreateAndOpenTemporaryFileInDir(const FilePath& dir, FilePath* path) {
 
   return fdopen(fd, "a+");
 }
-
-bool CreateTemporaryFileNameInDir(const std::wstring& dir,
-                                  std::wstring* temp_file) {
-  // Not implemented yet.
+// TODO(port): implement me.
+bool CreateTemporaryFileInDir(const FilePath& dir,
+                              FilePath* temp_file) {
   NOTREACHED();
   return false;
 }

base/file_util_unittest.cc
@@ -777,10 +777,10 @@ TEST_F(FileUtilTest, CopyAndDeleteDirectoryTest) {
 }
 #endif
 
-TEST_F(FileUtilTest, CreateTemporaryFileNameTest) {
-  std::wstring temp_files[3];
+TEST_F(FileUtilTest, CreateTemporaryFileTest) {
+  FilePath temp_files[3];
   for (int i = 0; i < 3; i++) {
-    ASSERT_TRUE(file_util::CreateTemporaryFileName(&(temp_files[i])));
+    ASSERT_TRUE(file_util::CreateTemporaryFile(&(temp_files[i])));
     EXPECT_TRUE(file_util::PathExists(temp_files[i]));
     EXPECT_FALSE(file_util::DirectoryExists(temp_files[i]));
   }
@@ -790,7 +790,7 @@ TEST_F(FileUtilTest, CreateTemporaryFileNameTest) {
     EXPECT_TRUE(file_util::Delete(temp_files[i], false));
 }
 
-TEST_F(FileUtilTest, CreateAndOpenTemporaryFileNameTest) {
+TEST_F(FileUtilTest, CreateAndOpenTemporaryFileTest) {
   FilePath names[3];
   FILE *fps[3];
   int i;

base/file_util_win.cc
@@ -445,14 +445,14 @@ bool GetShmemTempDir(FilePath* path) {
   return GetTempDir(path);
 }
 
-bool CreateTemporaryFileName(FilePath* path) {
-  std::wstring temp_path, temp_file;
+bool CreateTemporaryFile(FilePath* path) {
+  FilePath temp_file;
 
-  if (!GetTempDir(&temp_path))
+  if (!GetTempDir(path))
     return false;
 
-  if (CreateTemporaryFileNameInDir(temp_path, &temp_file)) {
-    *path = FilePath(temp_file);
+  if (CreateTemporaryFileInDir(*path, &temp_file)) {
+    *path = temp_file;
     return true;
   }
 
@@ -468,29 +468,29 @@ FILE* CreateAndOpenTemporaryShmemFile(FilePath* path) {
 // TODO(jrg): is there equivalent call to use on Windows instead of
 // going 2-step?
 FILE* CreateAndOpenTemporaryFileInDir(const FilePath& dir, FilePath* path) {
-  std::wstring wstring_path;
-  if (!CreateTemporaryFileNameInDir(dir.value(), &wstring_path)) {
+  if (!CreateTemporaryFileInDir(dir, path)) {
     return NULL;
   }
-  *path = FilePath(wstring_path);
   // Open file in binary mode, to avoid problems with fwrite. On Windows
   // it replaces \n's with \r\n's, which may surprise you.
   // Reference: http://msdn.microsoft.com/en-us/library/h9t88zwz(VS.71).aspx
   return OpenFile(*path, "wb+");
 }
 
-bool CreateTemporaryFileNameInDir(const std::wstring& dir,
-                                  std::wstring* temp_file) {
+bool CreateTemporaryFileInDir(const FilePath& dir,
+                              FilePath* temp_file) {
   wchar_t temp_name[MAX_PATH + 1];
 
-  if (!GetTempFileName(dir.c_str(), L"", 0, temp_name))
+  if (!GetTempFileName(dir.value().c_str(), L"", 0, temp_name))
     return false;  // fail!
 
   DWORD path_len = GetLongPathName(temp_name, temp_name, MAX_PATH);
   if (path_len > MAX_PATH + 1 || path_len == 0)
     return false;  // fail!
 
-  temp_file->assign(temp_name, path_len);
+  std::wstring temp_file_str;
+  temp_file_str.assign(temp_name, path_len);
+  *temp_file = FilePath(temp_file_str);
   return true;
 }
 

chrome/browser/download/download_file.cc
@@ -73,7 +73,7 @@ DownloadFile::~DownloadFile() {
 }
 
 bool DownloadFile::Initialize() {
-  if (file_util::CreateTemporaryFileName(&full_path_))
+  if (file_util::CreateTemporaryFile(&full_path_))
     return Open("wb");
   return false;
 }

chrome/browser/download/save_file.cc
@@ -22,7 +22,7 @@ SaveFile::SaveFile(const SaveFileCreateInfo* info)
       in_progress_(true) {
   DCHECK(info);
   DCHECK(info->path.empty());
-  if (file_util::CreateTemporaryFileName(&full_path_))
+  if (file_util::CreateTemporaryFile(&full_path_))
     Open("wb");
 }
 

commit 0523bf01fe8d95eeed9d092878f1151b941b9f94

Fix the collection of Chrome uninstall survey results which regressed a year ago in r23631.
BUG=63904
TEST=Install Chrome, ensure metrics collection is enabled in the Privacy settings, visit a few pages, uninstall Chrome. Note that the URL in the survey page contains stats such as pageviews, install date etc.
Review URL: http://codereview.chromium.org/5137007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@66899 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 4d5e4ffba71554dc4828c00b207c63f42fe315fe

chrome/installer/setup/uninstall.cc
@@ -252,11 +252,13 @@ DeleteResult DeleteFilesAndFolders(const std::wstring& exe_path,
   if (got_local_state) {
     FilePath user_local_file(
         user_local_state.Append(chrome::kLocalStateFilename));
-    FilePath path = FilePath::FromWStringHack(*local_state_path);
-    if (!file_util::CreateTemporaryFile(&path))
+    FilePath path;
+    if (!file_util::CreateTemporaryFile(&path)) {
       LOG(ERROR) << "Failed to create temporary file for Local State.";
-    else
+    } else {
+      *local_state_path = path.value();
       file_util::CopyFile(user_local_file, path);
+    }
   } else {
     LOG(ERROR) << "Could not retrieve user's profile directory.";
   }