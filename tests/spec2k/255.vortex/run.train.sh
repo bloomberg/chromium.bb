7a26268568ca62cbee348a4d5ba0df220f3eed08
jvoung@google.com <jvoung@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-11-22 19:56:37 +0000

    Re-enable 255.vortex for ARM unoptimized. No longer segfaults w/ 64KB issue. Review URL: http://codereview.chromium.org/5206008
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3757 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 3384c87363bd212083a920dc864e191279f2b8df

--- tests/spec2k/255.vortex/run.train.sh
+++ tests/spec2k/255.vortex/run.train.sh
@@ -21,13 +21,6 @@ if [[ "${EMU_HACK}" != "no" ]] ; then
   touch  vortex.out
 fi
 
-# TODO(jvoung) enable when bug 877 is fixed
-if [[ "$1" == "./vortex.pnacl.arm" ]]; then
-  echo "Skipping Vortex on Arm Unopt (bug 877)"
-  echo "OK..."
-  exit 0;
-fi
-
 ${PREFIX} $1 ${DASHDASH} lendian.raw     > stdout.out 2> stderr.out
 
 if [[ "${VERIFY}" != "no" ]] ; then
