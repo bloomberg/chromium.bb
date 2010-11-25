149c224fd4bb1755c1d1d3c2384920531bb9601c
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-25 09:14:26 +0000

    Make it so that notifications only start resizing after the first paint is done.
    Also, only allow them to grow.
    
    BUG=56693
    TEST=Ran many tests manually. Automated test in progress (before closing the bug).
    
    Review URL: http://codereview.chromium.org/5331006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67396 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 99c1e2e11855ba759129418fa5752cebdadb033c

--- chrome/browser/notifications/balloon.cc
+++ chrome/browser/notifications/balloon.cc
@@ -9,6 +9,7 @@
 #include "chrome/browser/notifications/notification.h"
 #include "chrome/browser/renderer_host/site_instance.h"
 #include "gfx/rect.h"
+#include "gfx/size.h"
 
 Balloon::Balloon(const Notification& notification, Profile* profile,
                  BalloonCollection* collection)
@@ -27,7 +28,16 @@ void Balloon::SetPosition(const gfx::Point& upper_left, bool reposition) {
 }
 
 void Balloon::SetContentPreferredSize(const gfx::Size& size) {
-  collection_->ResizeBalloon(this, size);
+  gfx::Size new_size(size);
+  // Only allow the size of notifications to grow.  This stops the balloon
+  // from jumping between sizes due to dynamic content.  For example, the
+  // balloon's contents may adjust due to changes in
+  // document.body.clientHeight.
+  new_size.set_height(std::max(new_size.height(), content_size_.height()));
+
+  if (content_size_ == new_size)
+    return;
+  collection_->ResizeBalloon(this, new_size);
 }
 
 void Balloon::set_view(BalloonView* balloon_view) {
