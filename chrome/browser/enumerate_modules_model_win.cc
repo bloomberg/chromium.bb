97ee39015b339050fd269462531197295de26619
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-11-26 10:23:39 +0000

    Remove the test DLLs from the blacklist. Demo is done.
    TBR=huanr
    BUG=51105
    TEST=None
    
    Review URL: http://codereview.chromium.org/5307004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@67446 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d4c5533befa296defc1c360d505094dbd1450fdc

--- chrome/browser/enumerate_modules_model_win.cc
+++ chrome/browser/enumerate_modules_model_win.cc
@@ -84,14 +84,6 @@ struct FindModule {
 // Entries without any Description, Signer info, or Location will never be
 // marked as confirmed bad (only as suspicious).
 const ModuleEnumerator::BlacklistEntry ModuleEnumerator::kModuleBlacklist[] = {
-  // Test DLLs, to demonstrate the feature. Will be removed soon.
-  {     // apphelp.dll, "%systemroot%\\system32\\"
-    "f5fda581", "23d01d5b", "", "", "", NONE
-  }, {  // rsaenh.dll, "%systemroot%\\system32\\", "Microsoft Windows"
-    "6af212cb", "23d01d5b", "7b47bf79", "", "",
-        static_cast<RecommendedAction>(UPDATE | DISABLE | SEE_LINK)
-  },
-
   // NOTE: Please keep this list sorted by dll name, then location.
 
   // foldersizecolumn.dll.
