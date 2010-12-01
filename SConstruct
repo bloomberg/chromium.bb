bloomberg/chromium.bb

commit 97000d2f90624825d63db6c3709060c502d7cb0c

SCons makefile: sysinfo now defaults to off when --verbose is not specified.
This change maintains the verbosity of the bots while reducing noise durring manual builds.

NOTE: This change also eliminates the "sysinfo=" idiom, sysinfo must now be given an explicit value.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=1188
TEST=none

Review URL: http://codereview.chromium.org/5369009

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3814 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: bed04451612107e6963e534ed1451231f352e879

SConstruct
@@ -68,7 +68,7 @@ def VerboseConfigInfo(env):
   "Should we print verbose config information useful for bug reports"
   if '--help' in sys.argv: return False
   if env.Bit('prebuilt') or env.Bit('built_elsewhere'): return False
-  return ARGUMENTS.get('sysinfo', True)
+  return env.Bit('sysinfo')
 
 # ----------------------------------------------------------
 # SANITY CHECKS
@@ -117,8 +117,6 @@ ACCEPTABLE_ARGUMENTS = set([
     'scale_timeout',
     # enable use of SDL
     'sdl',
-    # disable printing of sys info with sysinfo=
-    'sysinfo',
     # set target platform
     'targetplatform',
     # activates buildbot-specific presets
@@ -148,12 +146,12 @@ def StringValueToBoolean(value):
   elif value.lower() in _FALSE_STRINGS:
     return False
   else:
-    raise Exception("Cannot convert '%s' to a boolean value" % value)
+    raise Exception("Cannot convert '%s' to a Boolean value" % value)
 
 
 def GetBinaryArgumentValue(arg_name, default):
   if not isinstance(default, bool):
-    raise Exception("Default value for '%s' must be a boolean" % bit_name)
+    raise Exception("Default value for '%s' must be a Boolean" % arg_name)
   if arg_name not in ARGUMENTS:
     return default
   return StringValueToBoolean(ARGUMENTS[arg_name])
@@ -192,7 +190,9 @@ def SetUpArgumentBits(env):
   BitFromArgument(env, 'built_elsewhere', default=False,
     desc="The programs have already been built by another system")
 
-  BitFromArgument(env, 'target_stats', default=False,
+  # Defaults on when --verbose is specified.
+  # --verbose sets 'brief_comstr' to False, so this looks a little strange
+  BitFromArgument(env, 'target_stats', default=not GetOption('brief_comstr'),
     desc="Collect and display information about which commands are executed "
       "during the build process")
 
@@ -229,10 +229,15 @@ def SetUpArgumentBits(env):
   BitFromArgument(env, 'dangerous_debug_disable_inner_sandbox',
     default=False, desc="Make sel_ldr less strict")
 
-  # By default SCons does not use the system's enviroment variables when
+  # By default SCons does not use the system's environment variables when
   # executing commands, to help isolate the build process.
   BitFromArgument(env, 'use_environ', arg_name='USE_ENVIRON',
-    default=False, desc="Expose existing evironment variables to the build")
+    default=False, desc="Expose existing environment variables to the build")
+
+  # Defaults on when --verbose is specified
+  # --verbose sets 'brief_comstr' to False, so this looks a little strange
+  BitFromArgument(env, 'sysinfo', default=not GetOption('brief_comstr'),
+    desc="Print verbose system information")
 
 
 def CheckArguments():
@@ -1435,7 +1440,7 @@ naclsdk_mode=<mode>   where <mode>:
 
 pp=1                Use command line pretty printing (more concise output)
 
-sysinfo=            Suppress verbose system info printing
+sysinfo=1           Verbose system info printing
 
 naclsdk_validate=0  Suppress presence check of sdk
 

commit 28a127e07d7a20bf530379dcf50859b071be15e9

SCons makefile: fixed breakage of --verbose introduced by r3814
When verbose was specified, target_stats defaulted to on which disabled command printing.
This CL allows target_stats to function without interfering with command printing.

Code that printed the size of each binary was also removed because it would cause SCons to
relink binaries if --verbose changed between builds.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=1207
TEST=./scons --verbose

Review URL: http://codereview.chromium.org/5484002

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3827 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: 56e591fd4efbc50ff7498bfb907de75ef914766a

SConstruct
@@ -1230,36 +1230,27 @@ CMD_COUNTER = {}
 ENV_COUNTER = {}
 
 
-if pre_base_env.Bit('target_stats'):
-  # target_stats and pp are incompatible as they both use the
-  # PRINT_CMD_LINE_FUNC hook
-  assert not pre_base_env.Bit('pp')
-
-  def CommandStats(cmd, targets, source, env):
+def CustomCommandPrinter(cmd, targets, source, env):
+  # Log the command
+  if env.Bit('target_stats'):
     cmd_name = GetPrintableCommandName(cmd)
+    env_name = GetPrintableEnvironmentName(env)
     CMD_COUNTER[cmd_name] = CMD_COUNTER.get(cmd_name, 0) + 1
-    env_name = GetPrintableEnvironmentName(env)
     ENV_COUNTER[env_name] = ENV_COUNTER.get(env_name, 0) + 1
-  # Abuse standard scons's hook for command pretty printing (c.f.
-  # http://www.scons.org/doc/HTML/scons-man.html) to gather stats
-  # about how often each command is executed during this scons invocation.
-  pre_base_env.Append(PRINT_CMD_LINE_FUNC=CommandStats)
 
+  # Print the command
+  if env.Bit('pp'):
+    if targets:
+      cmd_name = GetPrintableCommandName(cmd)
+      env_name = GetPrintableEnvironmentName(env)
+      sys.stdout.write('[%s] [%s] %s\n' % (cmd_name, env_name,
+                                           targets[0].get_path()))
+  else:
+    # The SCons default
+    sys.stdout.write(cmd)
+    sys.stdout.write('\n')
 
-if pre_base_env.Bit('pp'):
-  # target_stats and pp are incompatible as they both use the
-  # PRINT_CMD_LINE_FUNC hook
-  assert not pre_base_env.Bit('target_stats')
-  def CommandPrettyPrinter(cmd, targets, source, env):
-    if not targets:
-      return
-    target =  targets[0]
-    cmd_name = GetPrintableCommandName(cmd)
-    env_name = GetPrintableEnvironmentName(env)
-    print '[%s] [%s] %s' % (cmd_name, env_name, target.get_path())
-  # Use standard scons's hooks for command pretty printing, c.f.
-  # http://www.scons.org/doc/HTML/scons-man.html
-  pre_base_env.Append(PRINT_CMD_LINE_FUNC=CommandPrettyPrinter)
+pre_base_env.Append(PRINT_CMD_LINE_FUNC=CustomCommandPrinter)
 
 # ----------------------------------------------------------
 # for generation of a promiscuous sel_ldr
@@ -1788,12 +1779,6 @@ if nacl_env.Bit('bitcode'):
   # NOTE: we change the linker command line to make it possible to
   #       sneak in startup and cleanup code
   nacl_env.Prepend(EMULATOR=EMULATOR)
-
-if not GetOption('brief_comstr'):
-  nacl_env['LINKCOM'] += '&& $PYTHON -c "import os; import sys;\
-      print(sys.argv[1] + sys.argv[2] + \
-            str(os.stat(sys.argv[1])[6]) + sys.argv[3])" \
-      $TARGET " is " " bytes"'
 
 environment_list.append(nacl_env)
 