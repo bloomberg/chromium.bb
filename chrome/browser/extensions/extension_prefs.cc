945038cc21cc1e7934abceb0062008793e14ac6c
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-08 09:02:05 +0000

    Fix browser crash in ExtensionPrefs.
    I am unable to repro this, despite numerous attempts, but from looking
    at the crash dump it seems that we get a valid ExtensionPrefs pointer
    but a NULL DictionaryValue pointer. Looking at the other callers for
    GetExtensionPref shows that they all expect a potentially NULL result,
    for example in GetExtensionState (which even documents that it can be
    NULL for --load-extension).
    
    I think a NULL check here is therefore in order too.
    
    BUG=64853
    TEST=QA will need to test this manually because I can't get it to crash for me.
    
    Review URL: http://codereview.chromium.org/5601005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@68572 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 20f4534eb02566c6a1fa86988a6b968e51cb1fd2

--- chrome/browser/extensions/extension_prefs.cc
+++ chrome/browser/extensions/extension_prefs.cc
@@ -747,6 +747,8 @@ void ExtensionPrefs::SetExtensionState(const Extension* extension,
 
 bool ExtensionPrefs::GetBrowserActionVisibility(const Extension* extension) {
   DictionaryValue* extension_prefs = GetExtensionPref(extension->id());
+  if (!extension_prefs)
+    return true;
   bool visible = false;
   if (!extension_prefs->GetBoolean(kBrowserActionVisible, &visible) || visible)
     return true;
