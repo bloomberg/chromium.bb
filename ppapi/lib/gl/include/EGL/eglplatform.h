b1d63728d34909627102879f3f84d07d48b46fdf
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-15 20:57:09 +0000

    Added native EGL types for pepper platform. Review URL: http://codereview.chromium.org/5865002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69311 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 630dc91c5a4e97b1c3b33799978c6bd4c00678e7

--- ppapi/lib/gl/include/EGL/eglplatform.h
+++ ppapi/lib/gl/include/EGL/eglplatform.h
@@ -67,40 +67,11 @@
  * implementations.
  */
 
-#if defined(_WIN32) || defined(__VC32__) && !defined(__CYGWIN__) && !defined(__SCITECH_SNAP__) /* Win32 and WinCE */
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN 1
-#endif
-#include <windows.h>
+#include "ppapi/c/pp_instance.h"
 
-typedef HDC     EGLNativeDisplayType;
-typedef HBITMAP EGLNativePixmapType;
-typedef HWND    EGLNativeWindowType;
-
-#elif defined(__WINSCW__) || defined(__SYMBIAN32__)  /* Symbian */
-
-typedef int   EGLNativeDisplayType;
-typedef void *EGLNativeWindowType;
-typedef void *EGLNativePixmapType;
-
-#elif defined(__unix__)
-
-/* X11 (tentative)  */
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-
-typedef Display *EGLNativeDisplayType;
-typedef Pixmap   EGLNativePixmapType;
-typedef Window   EGLNativeWindowType;
-
-#else
-/* #error "Platform not recognized" */
-
-typedef int EGLNativeDisplayType;
-typedef int EGLNativePixmapType;
-typedef int EGLNativeWindowType;
-
-#endif
+typedef PP_Instance EGLNativeDisplayType;
+typedef PP_Instance EGLNativeWindowType;
+typedef khronos_int32_t EGLNativePixmapType;  // Not supported.
 
 /* EGL 1.2 types, renamed for consistency in EGL 1.3 */
 typedef EGLNativeDisplayType NativeDisplayType;
