8df94e26b078f6ceb1873f7734b7e21b33486154
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-15 09:10:43 +0000

    Reland 69237 - Fix raw_scoped_refptr_mismatch_checker.h.
    BUG=28083
    TEST=none
    
    Review URL: http://codereview.chromium.org/3549010
    
    TBR=willchan@chromium.org
    Review URL: http://codereview.chromium.org/5885001
    
    TBR=phajdan.jr@chromium.org
    Review URL: http://codereview.chromium.org/5887001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@69243 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 968ca7b38235bdcae6ec82173e1fe01bcf248eb1

--- base/task.h
+++ base/task.h
@@ -149,8 +149,9 @@ class ScopedRunnableMethodFactory {
         : obj_(obj),
           meth_(meth),
           params_(params) {
-      COMPILE_ASSERT((MethodUsesScopedRefptrCorrectly<Method, Params>::value),
-                     badscopedrunnablemethodparams);
+      COMPILE_ASSERT(
+          (base::internal::ParamsUseScopedRefptrCorrectly<Params>::value),
+          badscopedrunnablemethodparams);
     }
 
     virtual void Run() {
@@ -317,8 +318,9 @@ class RunnableMethod : public CancelableTask {
   RunnableMethod(T* obj, Method meth, const Params& params)
       : obj_(obj), meth_(meth), params_(params) {
     traits_.RetainCallee(obj_);
-    COMPILE_ASSERT((MethodUsesScopedRefptrCorrectly<Method, Params>::value),
-                   badrunnablemethodparams);
+    COMPILE_ASSERT(
+        (base::internal::ParamsUseScopedRefptrCorrectly<Params>::value),
+        badrunnablemethodparams);
   }
 
   ~RunnableMethod() {
@@ -429,8 +431,9 @@ class RunnableFunction : public CancelableTask {
  public:
   RunnableFunction(Function function, const Params& params)
       : function_(function), params_(params) {
-    COMPILE_ASSERT((FunctionUsesScopedRefptrCorrectly<Function, Params>::value),
-                   badrunnablefunctionparams);
+    COMPILE_ASSERT(
+        (base::internal::ParamsUseScopedRefptrCorrectly<Params>::value),
+        badrunnablefunctionparams);
   }
 
   ~RunnableFunction() {
