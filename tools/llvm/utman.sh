9d42eec9584eeabbbad3d163a97bfb4b446064eb
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-12-16 19:25:25 +0000

    Updating stable llvm and llvm-gcc revs to include:
    http://codereview.chromium.org/5692004/
    http://codereview.chromium.org/5861004/
    "Make a custom TLS_addr32 for Native Client" + fix
    
    Review URL: http://codereview.chromium.org/5922002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@3986 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: aab4e86b36ab1f36daf2a9b5541f5081a3b17437

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -125,8 +125,8 @@ readonly PNACL_AS_X8664="${INSTALL_BIN}/pnacl-as_x86_64"
 
 # Current milestones in each repo
 # hg-update-stable  uses these
-readonly LLVM_REV=c730e46029c8
-readonly LLVM_GCC_REV=afdd3571034f
+readonly LLVM_REV=2c4c6ddac1f5
+readonly LLVM_GCC_REV=6b6e4b13d296
 readonly NEWLIB_REV=d0ac50acf303
 readonly BINUTILS_REV=a5b54c0cc733
 
