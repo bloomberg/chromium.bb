86c8d4d7f460dfdf2e85194b957fade5aca2dd0e
polina@google.com <polina@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2010-12-22 00:26:54 +0000

    ppapi_proxy: Fix include guards to match the renamed files. Minor clean-up edits.
    Review URL: http://codereview.chromium.org/6086002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4029 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 67556103bec03a90f41409dbb4364840b252c194

--- src/shared/ppapi_proxy/plugin_ppb_url_request_info.h
+++ src/shared/ppapi_proxy/plugin_ppb_url_request_info.h
@@ -1,18 +1,16 @@
-/*
- * Copyright 2010 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
- */
+// Copyright 2010 The Native Client Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can
+// be found in the LICENSE file.
 
-#ifndef NATIVE_CLIENT_SRC_SHARED_PPAPI_PROXY_PLUGIN_URL_REQUEST_INFO_H_
-#define NATIVE_CLIENT_SRC_SHARED_PPAPI_PROXY_PLUGIN_URL_REQUEST_INFO_H_
+#ifndef NATIVE_CLIENT_SRC_SHARED_PPAPI_PROXY_PLUGIN_PPB_URL_REQUEST_INFO_H_
+#define NATIVE_CLIENT_SRC_SHARED_PPAPI_PROXY_PLUGIN_PPB_URL_REQUEST_INFO_H_
 
 #include "native_client/src/include/nacl_macros.h"
 #include "ppapi/c/ppb_url_request_info.h"
 
 namespace ppapi_proxy {
 
-// Implements the plugin (i.e., .nexe) side of the PPB_URLRequestInfo interface.
+// Implements the untrusted side of the PPB_URLRequestInfo interface.
 class PluginURLRequestInfo {
  public:
   static const PPB_URLRequestInfo* GetInterface();
@@ -23,4 +21,4 @@ class PluginURLRequestInfo {
 
 }  // namespace ppapi_proxy
 
-#endif  // NATIVE_CLIENT_SRC_SHARED_PPAPI_PROXY_PLUGIN_URL_REQUEST_INFO_H_
+#endif  // NATIVE_CLIENT_SRC_SHARED_PPAPI_PROXY_PLUGIN_PPB_URL_REQUEST_INFO_H_
