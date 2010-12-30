749307419b4494d89302f3e8f5447affeec4c18c
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2010-12-30 22:05:03 +0000

    Dr. Memory - suppress some printing/ memory leaks, increase the number of callstack frames to remember BUG=68307 TBR=glider Review URL: http://codereview.chromium.org/6069008
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70320 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 004f2cf5d5eb19764260b1f513738be15ae77d47

--- tools/valgrind/valgrind_test.py
+++ tools/valgrind/valgrind_test.py
@@ -754,6 +754,7 @@ class DrMemory(BaseTool):
 
     proc += ["-logdir", self.temp_dir]
     proc += ["-batch", "-quiet"]
+    proc += ["-callstack_max_frames", "30"]
     #proc += ["-no_check_leaks", "-no_count_leaks"]
 
     # Dr.Memory requires -- to separate tool flags from the executable name.
