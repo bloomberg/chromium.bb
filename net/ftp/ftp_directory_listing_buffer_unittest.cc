cdf545ee96bd10a766745b2445ec5fb1e5f064af
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-04 19:59:29 +0000

    FTP: correctly handle directory listings containing "Permission denied" errors.
    BUG=67797
    TEST=net_unittests
    
    Review URL: http://codereview.chromium.org/6096006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70435 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cd4857b64f3e3b10ebfa3f44c38af0dc7376d5bb

--- net/ftp/ftp_directory_listing_buffer_unittest.cc
+++ net/ftp/ftp_directory_listing_buffer_unittest.cc
@@ -42,6 +42,7 @@ TEST(FtpDirectoryListingBufferTest, Parse) {
     "dir-listing-ls-20",  // TODO(phajdan.jr): should use windows-1251 encoding.
     "dir-listing-ls-21",  // TODO(phajdan.jr): should use windows-1251 encoding.
     "dir-listing-ls-22",  // TODO(phajdan.jr): should use windows-1251 encoding.
+    "dir-listing-ls-23",
     "dir-listing-netware-1",
     "dir-listing-netware-2",
     "dir-listing-vms-1",
