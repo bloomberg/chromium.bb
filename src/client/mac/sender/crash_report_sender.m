816f25011c3f2519e75014b92f00d6c204dcb1ca
stuart.morgan <stuart.morgan@4c0a9323-5329-0410-9bdc-e9ce6186880e>
2011-01-05 05:34:47 +0000

    Make localization resizing of Mac uploader email line more robust. Review URL: http://breakpad.appspot.com/247001
    git-svn-id: http://google-breakpad.googlecode.com/svn/trunk@749 4c0a9323-5329-0410-9bdc-e9ce6186880e

--- src/client/mac/sender/crash_report_sender.m
+++ src/client/mac/sender/crash_report_sender.m
@@ -616,16 +616,29 @@ NSString *const kDefaultServerType = @"google";
   CGFloat emailLabelWidthDelta = [emailLabel_ breakpad_adjustWidthToFit];
   [emailEntryField_ breakpad_shiftHorizontally:emailLabelWidthDelta];
 
+  // Localize the privacy policy label, and keep it right-aligned to the arrow.
+  [privacyLinkLabel_ setStringValue:NSLocalizedString(@"privacyLabel", @"")];
+  CGFloat privacyLabelWidthDelta =
+      [privacyLinkLabel_ breakpad_adjustWidthToFit];
+  [privacyLinkLabel_ breakpad_shiftHorizontally:(-privacyLabelWidthDelta)];
+
+  // Ensure that the email field and the privacy policy link don't overlap.
+  CGFloat kMinControlPadding = 8;
+  CGFloat maxEmailFieldWidth = NSMinX([privacyLinkLabel_ frame]) -
+                               NSMinX([emailEntryField_ frame]) -
+                               kMinControlPadding;
+  if (NSWidth([emailEntryField_ bounds]) > maxEmailFieldWidth &&
+      maxEmailFieldWidth > 0) {
+    NSSize emailSize = [emailEntryField_ frame].size;
+    emailSize.width = maxEmailFieldWidth;
+    [emailEntryField_ setFrameSize:emailSize];
+  }
+
   // Localize the placeholder text.
   [[commentsEntryField_ cell]
       setPlaceholderString:NSLocalizedString(@"commentsPlaceholder", @"")];
   [[emailEntryField_ cell]
       setPlaceholderString:NSLocalizedString(@"emailPlaceholder", @"")];
-
-  // Localize the privacy policy label, and keep it right-aligned to the arrow.
-  [privacyLinkLabel_ setStringValue:NSLocalizedString(@"privacyLabel", @"")];
-  CGFloat privacyLabelWidthDelta = [privacyLinkLabel_ breakpad_adjustWidthToFit];
-  [privacyLinkLabel_ breakpad_shiftHorizontally:(-privacyLabelWidthDelta)];
 
   // Localize the buttons, and keep the cancel button at the right distance.
   [sendButton_ setTitle:NSLocalizedString(@"sendReportButton", @"")];
