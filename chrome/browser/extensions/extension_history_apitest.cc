e4bbb32fa96d9122e302bb358b5cd1d98d1ce855
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-08 08:21:39 +0000

    Disable crashy ExtensionApiTest.History
    TBR=skerner
    
    BUG=26296
    TEST=browser_tests
    Review URL: http://codereview.chromium.org/6106009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@70841 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 73f092fa32cb7cbd88c8b8a206daa09754d253c2

--- chrome/browser/extensions/extension_history_apitest.cc
+++ chrome/browser/extensions/extension_history_apitest.cc
@@ -7,8 +7,8 @@
 #include "chrome/common/chrome_switches.h"
 #include "net/base/mock_host_resolver.h"
 
-// Flaky, http://crbug.com/26296.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, FLAKY_History) {
+// Disabled, http://crbug.com/26296.
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_History) {
   host_resolver()->AddRule("www.a.com", "127.0.0.1");
   host_resolver()->AddRule("www.b.com", "127.0.0.1");
   ASSERT_TRUE(StartTestServer());
