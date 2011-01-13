4051ea48f25e177346659ab8c1adedc8b9e7dc4d
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-01-13 02:21:41 +0000

    Fix platform qualification DEP check to work on x86-64 on Mac OS X
    On this system, attempting to execute a non-executable page raises
    SIGBUS rather than SIGSEGV.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1284
    TEST="run_hello_world_test platform=x86-64" on Mac
    
    Review URL: http://codereview.chromium.org/6033009
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4130 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 4e158e6666ec6a7bcb2d7cea650618db56c679e0

--- src/trusted/platform_qualify/posix/nacl_dep_qualify.c
+++ src/trusted/platform_qualify/posix/nacl_dep_qualify.c
@@ -16,6 +16,12 @@
 #include "native_client/src/trusted/platform_qualify/nacl_dep_qualify.h"
 #include "native_client/src/trusted/platform_qualify/linux/sysv_shm_and_mmap.h"
 
+#if NACL_OSX && NACL_BUILD_ARCH == NACL_x86 && NACL_BUILD_SUBARCH == 64
+# define EXPECTED_SIGNAL SIGBUS
+#else
+# define EXPECTED_SIGNAL SIGSEGV
+#endif
+
 static struct sigaction previous_sigaction;
 static struct sigaction try_sigaction;
 static sigjmp_buf try_state;
@@ -29,11 +35,11 @@ static void setup_signals() {
   sigemptyset(&try_sigaction.sa_mask);
   try_sigaction.sa_flags = SA_RESETHAND;
 
-  (void) sigaction(SIGSEGV, &try_sigaction, &previous_sigaction);
+  (void) sigaction(EXPECTED_SIGNAL, &try_sigaction, &previous_sigaction);
 }
 
 static void restore_signals() {
-  (void) sigaction(SIGSEGV, &previous_sigaction, 0);
+  (void) sigaction(EXPECTED_SIGNAL, &previous_sigaction, 0);
 }
 
 /*
