3cedf25510c8e13db6f60700bf2d123fae2e7a20
dyu@chromium.org <dyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-14 21:36:46 +0000

    Adding pyAuto tests for Chrome Instant features for: 1. testInstantDisabledInIncognito - instant should be disabled in incognito mode 2. testInstantOverlayNotStoredInHistory - instant overlay page not stored in history 3. testInstantDisabledForJavaScript - instant disabled for javascript urls
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6325003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@71490 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e689f18beeaf018bfeef7458029905d21801c403

--- chrome/test/functional/instant.py
+++ chrome/test/functional/instant.py
@@ -4,6 +4,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+
 import pyauto_functional  # Must be imported before pyauto
 import pyauto
 
@@ -39,6 +41,27 @@ class InstantTest(pyauto.PyUITest):
     location = self.GetInstantInfo()['location']
     self.assertTrue('google.com' in location,
                     msg='No google.com in %s' % location)
+
+  def testInstantDisabledInIncognito(self):
+    """Test that instant is disabled in Incognito mode."""
+    self.RunCommand(pyauto.IDC_NEW_INCOGNITO_WINDOW)
+    self.SetOmniboxText('google.com', windex=1)
+    self.assertFalse(self.GetInstantInfo()['active'],
+                     'Instant enabled in Incognito mode.')
+
+  def testInstantOverlayNotStoredInHistory(self):
+    """Test that instant overlay page is not stored in history."""
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title2.html'))
+    self.SetOmniboxText(url)
+    self.assertTrue(self.WaitUntil(self._DoneLoading))
+    history = self.GetHistoryInfo().History()
+    self.assertEqual(0, len(history))
+
+  def testInstantDisabledForJavaScript(self):
+    """Test that instant is disabled for javascript URLs."""
+    self.SetOmniboxText('javascript:')
+    self.assertFalse(self.GetInstantInfo()['active'],
+                     'Instant enabled for javascript URL.')
 
 
 if __name__ == '__main__':
