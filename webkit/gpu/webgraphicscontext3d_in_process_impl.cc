f6703d28f69b482198d4482f22410743f06d7dcb
jnd@chromium.org <jnd@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-23 13:38:45 +0000

    Don't map internalFormat in renderbufferStorage() in embedded envrionment.
    BUG=none
    TEST=Run webkit layout test fast/canvas/webgl/framebuffer_test.html in embedded environment. 
    
    Review URL: http://codereview.chromium.org/6365006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72299 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5e9aca2ab199d6a93fd1f060c2dd84e769f26482

--- webkit/gpu/webgraphicscontext3d_in_process_impl.cc
+++ webkit/gpu/webgraphicscontext3d_in_process_impl.cc
@@ -1310,20 +1310,22 @@ void WebGraphicsContext3DInProcessImpl::renderbufferStorage(
     unsigned long width,
     unsigned long height) {
   makeContextCurrent();
-  switch (internalformat) {
-  case GL_DEPTH_STENCIL:
-    internalformat = GL_DEPTH24_STENCIL8_EXT;
-    break;
-  case GL_DEPTH_COMPONENT16:
-    internalformat = GL_DEPTH_COMPONENT;
-    break;
-  case GL_RGBA4:
-  case GL_RGB5_A1:
-    internalformat = GL_RGBA;
-    break;
-  case 0x8D62:  // GL_RGB565
-    internalformat = GL_RGB;
-    break;
+  if (!is_gles2_) {
+    switch (internalformat) {
+    case GL_DEPTH_STENCIL:
+      internalformat = GL_DEPTH24_STENCIL8_EXT;
+      break;
+    case GL_DEPTH_COMPONENT16:
+      internalformat = GL_DEPTH_COMPONENT;
+      break;
+    case GL_RGBA4:
+    case GL_RGB5_A1:
+      internalformat = GL_RGBA;
+      break;
+    case 0x8D62:  // GL_RGB565
+      internalformat = GL_RGB;
+      break;
+    }
   }
   glRenderbufferStorageEXT(target, internalformat, width, height);
 }
