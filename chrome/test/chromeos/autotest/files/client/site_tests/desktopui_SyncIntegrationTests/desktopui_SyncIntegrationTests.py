ebe2dff24fd8d137a287730541cee7feb70d3742
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-01-25 23:33:15 +0000

    Make the sync integration tests self-contained on autotest
    In the past, the sync integration tests used to require a password file
    stored on every test device in order to do a gaia sign in using
    production gaia servers. This caused the tests to be brittle.
    
    As of today, the sync integration tests no longer rely on a password
    file, with gaia sign in being stubbed out locally.
    
    This patch reconfigures the tests on autotest, so that it no longer
    looks for a local password file.
    
    In addition, the tests run much faster now, and therefore, we reduce the
    max timeout to a more reasonable 2 minutes (in the extreme case).
    
    BUG=chromium-os:11294, chromium-os:9262
    TEST=sync_integration_tests
    
    Review URL: http://codereview.chromium.org/6387004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@72561 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b597956cd427a3b830a498c69602753ce6117119

--- chrome/test/chromeos/autotest/files/client/site_tests/desktopui_SyncIntegrationTests/desktopui_SyncIntegrationTests.py
+++ chrome/test/chromeos/autotest/files/client/site_tests/desktopui_SyncIntegrationTests/desktopui_SyncIntegrationTests.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -7,9 +7,8 @@ from autotest_lib.client.cros import chrome_test
 class desktopui_SyncIntegrationTests(chrome_test.ChromeTestBase):
     version = 1
 
-    def run_once(self):
-        password_file = '%s/sync_password.txt' % self.bindir
+    binary_to_run = 'sync_integration_tests'
+    cmd_line_params = '--test-terminate-timeout=120000'
 
-        self.run_chrome_test('sync_integration_tests',
-            ('--password-file-for-test=%s ' +
-             '--test-terminate-timeout=300000') % password_file)
+    def run_once(self):
+        self.run_chrome_test(self.binary_to_run, self.cmd_line_params)
