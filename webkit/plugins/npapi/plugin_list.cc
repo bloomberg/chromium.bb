032f03fd6544965c1a841c9dea5ff61fac77927d
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-01 22:52:29 +0000

    Fix a typo I introduced (IceTea should be IcedTea) in my last commit here. Also bump minimum version to 1.9.4; both Fedora and Ubuntu have released 1.9.4 as a security update. (Note -- there may be another one imminent, in which case I'll use 1.9.5 as appropriate).
    BUG=NONE
    TEST=NONE
    
    Review URL: http://codereview.chromium.org/6386046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73367 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 51e07e946c9aeb2b527c20a1a7dfa542581aad15

--- webkit/plugins/npapi/plugin_list.cc
+++ webkit/plugins/npapi/plugin_list.cc
@@ -141,8 +141,8 @@ static const VersionRangeDefinition kJavaVersionRange[] = {
 };
 
 static const VersionRangeDefinition kRedhatIcedTeaVersionRange[] = {
-    { "0", "1.9", "1.8.3", true },
-    { "1.9", "1.10", "1.9.2", true },
+    { "0", "1.9", "1.8.5", true },
+    { "1.9", "1.10", "1.9.5", true },
 };
 
 static const PluginGroupDefinition kGroupDefinitions[] = {
@@ -152,7 +152,7 @@ static const PluginGroupDefinition kGroupDefinitions[] = {
   { "java-runtime-environment", PluginGroup::kJavaGroupName, "Java",
     kJavaVersionRange, arraysize(kJavaVersionRange),
     "http://www.java.com/en/download/manual.jsp" },
-  { "redhat-icetea-java", "IceTea", "IcedTea",
+  { "redhat-icetea-java", "IcedTea", "IcedTea",
     kRedhatIcedTeaVersionRange, arraysize(kRedhatIcedTeaVersionRange),
     "http://www.linuxsecurity.com/content/section/3/170/" },
 };
