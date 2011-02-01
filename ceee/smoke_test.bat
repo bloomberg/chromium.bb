b9a3a75cd0f41a66a0e026af2974f66bb4c9a9ed
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-01 16:32:03 +0000

    The CEEE project is now deprecated.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6386038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73296 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9a0cda001b1bf8fac23a9e2802745c84a182ef29

--- ceee/smoke_test.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Copyright (c) 2010 The Chromium Authors. All rights reserved.
-REM Use of this source code is governed by a BSD-style license that can be
-REM found in the LICENSE file.
-
-"%~p0..\third_party\python_26\python.exe" "%~p0tools\smoke_test.py" %*
