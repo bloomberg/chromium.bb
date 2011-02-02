d931a6ecc66920502f9738063c3e88759ea83660
matthewjheaney <matthewjheaney@google.com>
2011-02-01 18:49:47 -0800

    libwebm: set version to 1.0.0.16
    Change-Id: Id8f9d8a5d1aa50545c403eb9da04f62347b4604f

--- mkvparser.cpp
+++ mkvparser.cpp
@@ -21,7 +21,7 @@ void mkvparser::GetVersion(int& major, int& minor, int& build, int& revision)
     major = 1;
     minor = 0;
     build = 0;
-    revision = 15;
+    revision = 16;
 }
 
 long long mkvparser::ReadUInt(IMkvReader* pReader, long long pos, long& len)
