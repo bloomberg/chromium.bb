bloomberg/chromium.bb

commit 17e51de15d0d7cd1904dd8c24fd25b3cd21de850

Split flip_in_mem_edsm_server into a gazillion pieces.
For the most part, this is a straight refactor.  I'm sure I broke something.

BUG=monolithic code
TEST=none

Review URL: http://codereview.chromium.org/6392011

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73491 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 878984e6a994d308543f3ce93b2d5a511c81ba00

net/tools/flip_server/acceptor_thread.h
@@ -1,0 +1,95 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_TOOLS_FLIP_SERVER_ACCEPTOR_THREAD_H_
+#define NET_TOOLS_FLIP_SERVER_ACCEPTOR_THREAD_H_
+
+#include <list>
+#include <string>
+#include <vector>
+
+#include "base/threading/simple_thread.h"
+#include "net/tools/flip_server/epoll_server.h"
+#include "net/tools/flip_server/sm_interface.h"
+#include "openssl/ssl.h"
+
+struct sockaddr_in;
+
+namespace net {
+
+class FlipAcceptor;
+class MemoryCache;
+class SMConnection;
+class SSLState;
+
+// TODO(mbelshe):  Get rid of this class; we don't need a lock just to set
+//    a bool cross threads - especially one which only is set once...
+class Notification {
+ public:
+   explicit Notification(bool value) : value_(value) {}
+
+   void Notify() {
+     base::AutoLock al(lock_);
+     value_ = true;
+   }
+   bool HasBeenNotified() {
+     base::AutoLock al(lock_);
+     return value_;
+   }
+   bool value_;
+   base::Lock lock_;
+};
+
+class SMAcceptorThread : public base::SimpleThread,
+                         public EpollCallbackInterface,
+                         public SMConnectionPoolInterface {
+ public:
+  SMAcceptorThread(FlipAcceptor *acceptor, MemoryCache* memory_cache);
+  ~SMAcceptorThread();
+
+  // EpollCallbackInteface interface
+  virtual void OnRegistration(EpollServer* eps, int fd, int event_mask) {}
+  virtual void OnModification(int fd, int event_mask) {}
+  virtual void OnEvent(int fd, EpollEvent* event);
+  virtual void OnUnregistration(int fd, bool replaced) {}
+  virtual void OnShutdown(EpollServer* eps, int fd) {}
+
+  // SMConnectionPool interface
+  virtual void SMConnectionDone(SMConnection* sc);
+
+  // TODO(mbelshe): figure out if we can move these to private functions.
+  SMConnection* NewConnection();
+  SMConnection* FindOrMakeNewSMConnection();
+  void InitWorker();
+  void HandleConnection(int server_fd, struct sockaddr_in *remote_addr);
+  void AcceptFromListenFD();
+
+  // Notify the Accept thread that it is time to terminate.
+  void Quit() { quitting_.Notify(); }
+
+  // Iterates through a list of active connections expiring any that have been
+  // idle longer than the configured timeout.
+  void HandleConnectionIdleTimeout();
+
+  void Run();
+
+ private:
+  EpollServer epoll_server_;
+  FlipAcceptor* acceptor_;
+  SSLState* ssl_state_;
+  bool use_ssl_;
+  int idle_socket_timeout_s_;
+
+  std::vector<SMConnection*> unused_server_connections_;
+  std::vector<SMConnection*> tmp_unused_server_connections_;
+  std::vector<SMConnection*> allocated_server_connections_;
+  std::list<SMConnection*> active_server_connections_;
+  Notification quitting_;
+  MemoryCache* memory_cache_;
+};
+
+}  // namespace net
+
+#endif  // NET_TOOLS_FLIP_SERVER_ACCEPTOR_THREAD_H_
+

net/tools/flip_server/sm_connection.h
@@ -1,0 +1,167 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_TOOLS_FLIP_SERVER_SM_CONNECTION_H_
+#define NET_TOOLS_FLIP_SERVER_SM_CONNECTION_H_
+
+#include <arpa/inet.h>  // in_addr_t
+#include <time.h>
+
+#include <list>
+#include <string>
+
+#include "net/tools/flip_server/create_listener.h"
+#include "net/tools/flip_server/epoll_server.h"
+#include "net/tools/flip_server/mem_cache.h"
+#include "net/tools/flip_server/ring_buffer.h"
+#include "net/tools/flip_server/sm_interface.h"
+#include "openssl/ssl.h"
+
+namespace net {
+
+class FlipAcceptor;
+class MemoryCache;
+class SSLState;
+
+// A frame of data to be sent.
+class DataFrame {
+ public:
+  const char* data;
+  size_t size;
+  bool delete_when_done;
+  size_t index;
+  DataFrame() : data(NULL), size(0), delete_when_done(false), index(0) {}
+  virtual ~DataFrame() {
+    if (delete_when_done)
+      delete[] data;
+  }
+};
+
+typedef std::list<DataFrame*> OutputList;
+
+class SMConnection : public SMConnectionInterface,
+                     public EpollCallbackInterface,
+                     public NotifierInterface {
+ public:
+  virtual ~SMConnection();
+
+  static SMConnection* NewSMConnection(EpollServer* epoll_server,
+                                       SSLState *ssl_state,
+                                       MemoryCache* memory_cache,
+                                       FlipAcceptor *acceptor,
+                                       std::string log_prefix);
+
+  // TODO(mbelshe): Make these private.
+  time_t last_read_time_;
+  std::string server_ip_;
+  std::string server_port_;
+
+  EpollServer* epoll_server() { return epoll_server_; }
+  OutputList* output_list() { return &output_list_; }
+  MemoryCache* memory_cache() { return memory_cache_; }
+  void ReadyToSend();
+  void EnqueueDataFrame(DataFrame* df);
+
+  int fd() const { return fd_; }
+  bool initialized() const { return initialized_; }
+  std::string client_ip() const { return client_ip_; }
+
+  void InitSMConnection(SMConnectionPoolInterface* connection_pool,
+                        SMInterface* sm_interface,
+                        EpollServer* epoll_server,
+                        int fd,
+                        std::string server_ip,
+                        std::string server_port,
+                        std::string remote_ip,
+                        bool use_ssl);
+
+  void CorkSocket();
+  void UncorkSocket();
+
+  int Send(const char* data, int len, int flags);
+
+  // EpollCallbackInterface interface.
+  virtual void OnRegistration(EpollServer* eps, int fd, int event_mask) {
+    registered_in_epoll_server_ = true;
+  }
+  virtual void OnModification(int fd, int event_mask) {}
+  virtual void OnEvent(int fd, EpollEvent* event);
+  virtual void OnUnregistration(int fd, bool replaced) {
+    registered_in_epoll_server_ = false;
+  }
+  virtual void OnShutdown(EpollServer* eps, int fd);
+
+  // NotifierInterface interface.
+  virtual void Notify() {}
+
+  void Cleanup(const char* cleanup);
+
+  // Flag indicating if we should force spdy on all connections.
+  static bool force_spdy() { return force_spdy_; }
+  static void set_force_spdy(bool value) { force_spdy_ = value; }
+
+ private:
+  // Decide if SPDY was negotiated.
+  bool WasSpdyNegotiated();
+
+  // Initialize the protocol interfaces we'll need for this connection.
+  // Returns true if successful, false otherwise.
+  bool SetupProtocolInterfaces();
+
+  bool DoRead();
+  bool DoWrite();
+  bool DoConsumeReadData();
+  void Reset();
+
+  void HandleEvents();
+  void HandleResponseFullyRead();
+
+ protected:
+  friend std::ostream& operator<<(std::ostream& os, const SMConnection& c) {
+    os << &c << "\n";
+    return os;
+  }
+
+ private:
+  SMConnection(EpollServer* epoll_server,
+               SSLState* ssl_state,
+               MemoryCache* memory_cache,
+               FlipAcceptor* acceptor,
+               std::string log_prefix);
+  int fd_;
+  int events_;
+
+  bool registered_in_epoll_server_;
+  bool initialized_;
+  bool protocol_detected_;
+  bool connection_complete_;
+
+  SMConnectionPoolInterface* connection_pool_;
+
+  EpollServer *epoll_server_;
+  SSLState *ssl_state_;
+  MemoryCache* memory_cache_;
+  FlipAcceptor *acceptor_;
+  std::string client_ip_;
+
+  RingBuffer read_buffer_;
+
+  OutputList output_list_;
+  SMInterface* sm_spdy_interface_;
+  SMInterface* sm_http_interface_;
+  SMInterface* sm_streamer_interface_;
+  SMInterface* sm_interface_;
+  std::string log_prefix_;
+
+  size_t max_bytes_sent_per_dowrite_;
+
+  SSL* ssl_;
+
+  static bool force_spdy_;
+};
+
+}  // namespace net
+
+#endif  // NET_TOOLS_FLIP_SERVER_SM_CONNECTION_H_
+

net/net.gyp
@@ -1349,6 +1349,8 @@
              'tools/dump_cache/url_utilities.h',
              'tools/dump_cache/url_utilities.cc',
 
+             'tools/flip_server/acceptor_thread.h',
+             'tools/flip_server/acceptor_thread.cc',
              'tools/flip_server/balsa_enums.h',
              'tools/flip_server/balsa_frame.cc',
              'tools/flip_server/balsa_frame.h',
@@ -1358,21 +1360,39 @@
              'tools/flip_server/balsa_headers_token_utils.h',
              'tools/flip_server/balsa_visitor_interface.h',
              'tools/flip_server/buffer_interface.h',
+             'tools/flip_server/constants.h',
              'tools/flip_server/create_listener.cc',
              'tools/flip_server/create_listener.h',
              'tools/flip_server/epoll_server.cc',
              'tools/flip_server/epoll_server.h',
              'tools/flip_server/flip_in_mem_edsm_server.cc',
+             'tools/flip_server/http_interface.cc',
+             'tools/flip_server/http_interface.h',
              'tools/flip_server/http_message_constants.cc',
              'tools/flip_server/http_message_constants.h',
              'tools/flip_server/loadtime_measurement.h',
+             'tools/flip_server/mem_cache.h',
+             'tools/flip_server/mem_cache.cc',
              'tools/flip_server/porting.txt',
+             'tools/flip_server/output_ordering.cc',
+             'tools/flip_server/output_ordering.h',
              'tools/flip_server/ring_buffer.cc',
              'tools/flip_server/ring_buffer.h',
              'tools/flip_server/simple_buffer.cc',
              'tools/flip_server/simple_buffer.h',
+             'tools/flip_server/sm_connection.cc',
+             'tools/flip_server/sm_connection.h',
+             'tools/flip_server/sm_interface.h',
              'tools/flip_server/split.h',
              'tools/flip_server/split.cc',
+             'tools/flip_server/spdy_ssl.cc',
+             'tools/flip_server/spdy_ssl.h',
+             'tools/flip_server/spdy_interface.cc',
+             'tools/flip_server/spdy_interface.h',
+             'tools/flip_server/spdy_util.cc',
+             'tools/flip_server/spdy_util.h',
+             'tools/flip_server/streamer_interface.cc',
+             'tools/flip_server/streamer_interface.h',
              'tools/flip_server/string_piece_utils.h',
              'tools/flip_server/thread.h',
              'tools/flip_server/url_to_filename_encoder.h',

net/tools/flip_server/acceptor_thread.cc
@@ -1,0 +1,210 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/tools/flip_server/acceptor_thread.h"
+
+#include <netinet/in.h>
+#include <netinet/tcp.h>  // For TCP_NODELAY
+#include <sys/socket.h>
+#include <sys/types.h>
+
+#include <string>
+
+#include "net/tools/flip_server/constants.h"
+#include "net/tools/flip_server/flip_config.h"
+#include "net/tools/flip_server/sm_connection.h"
+#include "net/tools/flip_server/spdy_ssl.h"
+#include "openssl/err.h"
+#include "openssl/ssl.h"
+
+namespace net {
+
+SMAcceptorThread::SMAcceptorThread(FlipAcceptor *acceptor,
+                                   MemoryCache* memory_cache)
+    : SimpleThread("SMAcceptorThread"),
+      acceptor_(acceptor),
+      ssl_state_(NULL),
+      use_ssl_(false),
+      idle_socket_timeout_s_(acceptor->idle_socket_timeout_s_),
+      quitting_(false),
+      memory_cache_(memory_cache) {
+  if (!acceptor->ssl_cert_filename_.empty() &&
+      !acceptor->ssl_key_filename_.empty()) {
+    ssl_state_ = new SSLState;
+    bool use_npn = true;
+    if (acceptor_->flip_handler_type_ == FLIP_HANDLER_HTTP_SERVER) {
+      use_npn = false;
+    }
+    InitSSL(ssl_state_,
+            acceptor_->ssl_cert_filename_,
+            acceptor_->ssl_key_filename_,
+            use_npn,
+            acceptor_->ssl_session_expiry_,
+            acceptor_->ssl_disable_compression_);
+    use_ssl_ = true;
+  }
+}
+
+SMAcceptorThread::~SMAcceptorThread() {
+  for (std::vector<SMConnection*>::iterator i =
+       allocated_server_connections_.begin();
+       i != allocated_server_connections_.end();
+       ++i) {
+    delete *i;
+  }
+  delete ssl_state_;
+}
+
+SMConnection* SMAcceptorThread::NewConnection() {
+  SMConnection* server =
+    SMConnection::NewSMConnection(&epoll_server_, ssl_state_,
+                                  memory_cache_, acceptor_,
+                                  "client_conn: ");
+  allocated_server_connections_.push_back(server);
+  VLOG(2) << ACCEPTOR_CLIENT_IDENT << "Acceptor: Making new server.";
+  return server;
+}
+
+SMConnection* SMAcceptorThread::FindOrMakeNewSMConnection() {
+  if (unused_server_connections_.empty()) {
+    return NewConnection();
+  }
+  SMConnection* server = unused_server_connections_.back();
+  unused_server_connections_.pop_back();
+  VLOG(2) << ACCEPTOR_CLIENT_IDENT << "Acceptor: Reusing server.";
+  return server;
+}
+
+void SMAcceptorThread::InitWorker() {
+  epoll_server_.RegisterFD(acceptor_->listen_fd_, this, EPOLLIN | EPOLLET);
+}
+
+void SMAcceptorThread::HandleConnection(int server_fd,
+                                        struct sockaddr_in *remote_addr) {
+  int on = 1;
+  int rc;
+  if (acceptor_->disable_nagle_) {
+    rc = setsockopt(server_fd, IPPROTO_TCP,  TCP_NODELAY,
+                    reinterpret_cast<char*>(&on), sizeof(on));
+    if (rc < 0) {
+      close(server_fd);
+      LOG(ERROR) << "setsockopt() failed fd=" + server_fd;
+      return;
+    }
+  }
+
+  SMConnection* server_connection = FindOrMakeNewSMConnection();
+  if (server_connection == NULL) {
+    VLOG(1) << ACCEPTOR_CLIENT_IDENT << "Acceptor: Closing fd " << server_fd;
+    close(server_fd);
+    return;
+  }
+  std::string remote_ip = inet_ntoa(remote_addr->sin_addr);
+  server_connection->InitSMConnection(this,
+                                      NULL,
+                                      &epoll_server_,
+                                      server_fd,
+                                      "", "", remote_ip,
+                                      use_ssl_);
+  if (server_connection->initialized())
+    active_server_connections_.push_back(server_connection);
+}
+
+void SMAcceptorThread::AcceptFromListenFD() {
+  if (acceptor_->accepts_per_wake_ > 0) {
+    for (int i = 0; i < acceptor_->accepts_per_wake_; ++i) {
+      struct sockaddr address;
+      socklen_t socklen = sizeof(address);
+      int fd = accept(acceptor_->listen_fd_, &address, &socklen);
+      if (fd == -1) {
+        if (errno != 11) {
+          VLOG(1) << ACCEPTOR_CLIENT_IDENT << "Acceptor: accept fail("
+                  << acceptor_->listen_fd_ << "): " << errno << ": "
+                  << strerror(errno);
+        }
+        break;
+      }
+      VLOG(1) << ACCEPTOR_CLIENT_IDENT << " Accepted connection";
+      HandleConnection(fd, (struct sockaddr_in *)&address);
+    }
+  } else {
+    while (true) {
+      struct sockaddr address;
+      socklen_t socklen = sizeof(address);
+      int fd = accept(acceptor_->listen_fd_, &address, &socklen);
+      if (fd == -1) {
+        if (errno != 11) {
+          VLOG(1) << ACCEPTOR_CLIENT_IDENT << "Acceptor: accept fail("
+                  << acceptor_->listen_fd_ << "): " << errno << ": "
+                  << strerror(errno);
+        }
+        break;
+      }
+      VLOG(1) << ACCEPTOR_CLIENT_IDENT << "Accepted connection";
+      HandleConnection(fd, (struct sockaddr_in *)&address);
+    }
+  }
+}
+
+void SMAcceptorThread::HandleConnectionIdleTimeout() {
+  static time_t oldest_time = time(NULL);
+
+  int cur_time = time(NULL);
+  // Only iterate the list if we speculate that a connection is ready to be
+  // expired
+  if ((cur_time - oldest_time) < idle_socket_timeout_s_)
+    return;
+
+  // TODO(mbelshe): This code could be optimized, active_server_connections_
+  //                is already in-order.
+  std::list<SMConnection*>::iterator iter = active_server_connections_.begin();
+  while (iter != active_server_connections_.end()) {
+    SMConnection *conn = *iter;
+    int elapsed_time = (cur_time - conn->last_read_time_);
+    if (elapsed_time > idle_socket_timeout_s_) {
+      conn->Cleanup("Connection idle timeout reached.");
+      iter = active_server_connections_.erase(iter);
+      continue;
+    }
+    if (conn->last_read_time_ < oldest_time)
+      oldest_time = conn->last_read_time_;
+    iter++;
+  }
+  if ((cur_time - oldest_time) >= idle_socket_timeout_s_)
+    oldest_time = cur_time;
+}
+
+void SMAcceptorThread::Run() {
+  while (!quitting_.HasBeenNotified()) {
+    epoll_server_.set_timeout_in_us(10 * 1000);  // 10 ms
+    epoll_server_.WaitForEventsAndExecuteCallbacks();
+    if (tmp_unused_server_connections_.size()) {
+      VLOG(2) << "have " << tmp_unused_server_connections_.size()
+              << " additional unused connections.  Total = "
+              << unused_server_connections_.size();
+      unused_server_connections_.insert(unused_server_connections_.end(),
+                                        tmp_unused_server_connections_.begin(),
+                                        tmp_unused_server_connections_.end());
+      tmp_unused_server_connections_.clear();
+    }
+    HandleConnectionIdleTimeout();
+  }
+}
+
+void SMAcceptorThread::OnEvent(int fd, EpollEvent* event) {
+  if (event->in_events | EPOLLIN) {
+    VLOG(2) << ACCEPTOR_CLIENT_IDENT
+            << "Acceptor: Accepting based upon epoll events";
+    AcceptFromListenFD();
+  }
+}
+
+void SMAcceptorThread::SMConnectionDone(SMConnection* sc) {
+  VLOG(1) << ACCEPTOR_CLIENT_IDENT << "Done with connection.";
+  tmp_unused_server_connections_.push_back(sc);
+}
+
+}  // namespace net
+
+

net/tools/flip_server/constants.h
@@ -1,0 +1,31 @@
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_TOOLS_FLIP_SERVER_CONSTANTS_H_
+#define NET_TOOLS_FLIP_SERVER_CONSTANTS_H_
+
+#include "net/spdy/spdy_protocol.h"
+
+const int kMSS = 1460;
+const int kSSLOverhead = 25;
+const int kSpdyOverhead = spdy::SpdyFrame::size();
+const int kInitialDataSendersThreshold = (2 * kMSS) - kSpdyOverhead;
+const int kSSLSegmentSize = (1 * kMSS) - kSSLOverhead;
+const int kSpdySegmentSize = kSSLSegmentSize - kSpdyOverhead;
+
+#define ACCEPTOR_CLIENT_IDENT \
+    acceptor_->listen_ip_ << ":" \
+    << acceptor_->listen_port_ << " "
+
+#define NEXT_PROTO_STRING "\x06spdy/2\x08http/1.1\x08http/1.0"
+
+#define SSL_CIPHER_LIST "!aNULL:!ADH:!eNull:!LOW:!EXP:RC4+RSA:MEDIUM:HIGH"
+
+#define IPV4_PRINTABLE_FORMAT(IP) (((IP)>>0)&0xff), (((IP)>>8)&0xff), \
+                                  (((IP)>>16)&0xff), (((IP)>>24)&0xff)
+
+#define PIDFILE "/var/run/flip-server.pid"
+
+#endif  // NET_TOOLS_FLIP_SERVER_CONSTANTS_H_
+

net/tools/flip_server/create_listener.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "net/tools/flip_server/create_listener.h"
+
 #include <arpa/inet.h>   // for inet_ntop
 #include <errno.h>       // for strerror
 #include <netdb.h>       // for getaddrinfo and getnameinfo
@@ -13,8 +15,6 @@
 #include <fcntl.h>
 #include <unistd.h>      // for exit()
 #include <ostream>
-
-#include "net/tools/flip_server/create_listener.h"
 
 #include "base/logging.h"
 

commit 29ae31edbf61ee9ce1831ca0e3797294ec814fc7

Alter the forward declarations so they match the definition. Unbreaks clang, broken by r73491.
BUG=none
TEST=none

Review URL: http://codereview.chromium.org/6250117

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73522 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: d4d304a5532717d92098d66bf1063c687d764833

net/tools/flip_server/acceptor_thread.h
@@ -21,7 +21,7 @@ namespace net {
 class FlipAcceptor;
 class MemoryCache;
 class SMConnection;
-class SSLState;
+struct SSLState;
 
 // TODO(mbelshe):  Get rid of this class; we don't need a lock just to set
 //    a bool cross threads - especially one which only is set once...

net/tools/flip_server/sm_connection.h
@@ -22,7 +22,7 @@ namespace net {
 
 class FlipAcceptor;
 class MemoryCache;
-class SSLState;
+struct SSLState;
 
 // A frame of data to be sent.
 class DataFrame {