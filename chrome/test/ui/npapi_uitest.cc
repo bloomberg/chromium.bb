eb3366639d7f452ad00162cb5784306de6aa8e30
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-04 07:39:55 +0000

    Mark NPAPITesterBase.GetURLRedirectNotification test as flaky on linux.
    BUG=chromium-os:71904
    TEST=itself
    
    Review URL: http://codereview.chromium.org/6410078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73779 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0d5ffe63758209fbc0bb94464201eb79ab44c7af

--- chrome/test/ui/npapi_uitest.cc
+++ chrome/test/ui/npapi_uitest.cc
@@ -92,7 +92,13 @@ TEST_F(NPAPITesterBase, GetJavaScriptURL2) {
                 kTestCompleteSuccess, TestTimeouts::action_max_timeout_ms());
 }
 
-TEST_F(NPAPITesterBase, GetURLRedirectNotification) {
+// Test is flaky on linux/cros builders.  http://crbug.com/71904
+#if defined(OS_POSIX)
+#define MAYBE_GetURLRedirectNotification FLAKY_GetURLRedirectNotification
+#else
+#define MAYBE_GetURLRedirectNotification GetURLRedirectNotification
+#endif
+TEST(NPAPITesterBase, MAYBE_GetURLRedirectNotification) {
   const FilePath test_case(FILE_PATH_LITERAL("geturl_redirect_notify.html"));
   GURL url = ui_test_utils::GetTestUrl(FilePath(kTestDir), test_case);
   ASSERT_NO_FATAL_FAILURE(NavigateToURL(url));
