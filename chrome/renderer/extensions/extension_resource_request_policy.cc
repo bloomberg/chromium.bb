d6cbfa4417de8b741fb5d175c49a46855ea61a97
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-04 04:17:58 +0000

    Move most of chrome-extension:// request checks into renderer.
    One cannot be moved because we don't have the bit of state
    we need in the renderer.
    
    This should have fixed the bug 57263, but it doesn't. So
    maybe something else is going on?
    
    BUG=57263
    TEST=
    
    Review URL: http://codereview.chromium.org/6296025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@73760 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dd591bbb5a82cd8b27b648e39d2a869a7b95a52b

---
+++ chrome/renderer/extensions/extension_resource_request_policy.cc
@@ -0,0 +1,63 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/renderer/extensions/extension_resource_request_policy.h"
+
+#include "base/logging.h"
+#include "chrome/common/url_constants.h"
+#include "chrome/common/extensions/extension.h"
+#include "chrome/common/extensions/extension_set.h"
+#include "googleurl/src/gurl.h"
+
+// static
+bool ExtensionResourceRequestPolicy::CanRequestResource(
+    const GURL& resource_url,
+    const GURL& frame_url,
+    const ExtensionSet* loaded_extensions) {
+  CHECK(resource_url.SchemeIs(chrome::kExtensionScheme));
+
+  // chrome:// URLs are always allowed to load chrome-extension:// resources.
+  // The app launcher in the NTP uses this feature, as does dev tools.
+  if (frame_url.SchemeIs(chrome::kChromeDevToolsScheme) ||
+      frame_url.SchemeIs(chrome::kChromeUIScheme))
+    return true;
+
+  // Disallow loading of packaged resources for hosted apps. We don't allow
+  // hybrid hosted/packaged apps. The one exception is access to icons, since
+  // some extensions want to be able to do things like create their own
+  // launchers.
+  const Extension* extension = loaded_extensions->GetByURL(resource_url);
+  if (extension && extension->is_hosted_app() &&
+      !extension->icons().ContainsPath(resource_url.path())) {
+    LOG(ERROR) << "Denying load of " << resource_url.spec() << " from "
+               << "hosted app.";
+    return false;
+  }
+
+  // Otherwise, pages are allowed to load resources from extensions if the
+  // extension has host permissions to (and therefore could be running script
+  // in, which might need access to the extension resources).
+  //
+  // Exceptions are:
+  // - empty origin (needed for some edge cases when we have empty origins)
+  // - chrome-extension:// (for legacy reasons -- some extensions interop)
+  // - data: (basic HTML notifications use data URLs internally)
+  if (frame_url.is_empty() ||
+      frame_url.SchemeIs(chrome::kExtensionScheme) |
+      frame_url.SchemeIs(chrome::kDataScheme)) {
+    return true;
+  } else {
+    if (extension->GetEffectiveHostPermissions().ContainsURL(frame_url)) {
+      return true;
+    } else {
+      LOG(ERROR) << "Denying load of " << resource_url.spec() << " from "
+                 << frame_url.spec() << " because the extension does not have "
+                 << "access to the requesting page.";
+      return false;
+    }
+  }
+}
+
+ExtensionResourceRequestPolicy::ExtensionResourceRequestPolicy() {
+}
