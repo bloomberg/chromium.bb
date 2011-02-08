336820e69e9bc7f17d3ee0b43a4865e0e0fd3ef2
pdox@google.com <pdox@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-02-08 20:59:20 +0000

    Update LLVM_REV. Relevant CL:
    http://codereview.chromium.org/6438002/
    
    Review URL: http://codereview.chromium.org/6453010
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4282 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 42dbd0786445af910e088a15615b1a3f7eb9014b

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -128,7 +128,7 @@ readonly PNACL_AS_X8664="${INSTALL_BIN}/pnacl-as_x86_64"
 
 # Current milestones in each repo
 # hg-update-stable  uses these
-readonly LLVM_REV=2063d587d444
+readonly LLVM_REV=04eba3102bd0
 readonly LLVM_GCC_REV=a24f3ae473c8
 readonly NEWLIB_REV=d0ac50acf303
 readonly BINUTILS_REV=18098c353f3c
