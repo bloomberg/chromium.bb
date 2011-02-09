11deb55418b1e52648b146fbf6e2855b87866682
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-09 20:14:06 +0000

    They're no longer flaky according to flakiness dashboard.
    BUG=chromium-os:7306
    TEST=the test is no longer flaky
    
    Review URL: http://codereview.chromium.org/6465015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74327 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0e3c9572844ac820dc2f83ce6f6985afc836369b

--- chrome/browser/chromeos/login/screen_locker_browsertest.cc
+++ chrome/browser/chromeos/login/screen_locker_browsertest.cc
@@ -174,8 +174,7 @@ class ScreenLockerTest : public CrosInProcessBrowserTest {
   DISALLOW_COPY_AND_ASSIGN(ScreenLockerTest);
 };
 
-// PulseAudioMixer sometimes crashes at exit. See http://crosbug.om/9303
-IN_PROC_BROWSER_TEST_F(ScreenLockerTest, FLAKY_TestBasic) {
+IN_PROC_BROWSER_TEST_F(ScreenLockerTest, TestBasic) {
   EXPECT_CALL(*mock_input_method_library_, GetNumActiveInputMethods())
       .Times(1)
       .WillRepeatedly((testing::Return(0)))
--- chrome/browser/chromeos/notifications/notification_browsertest.cc
+++ chrome/browser/chromeos/notifications/notification_browsertest.cc
@@ -288,9 +288,7 @@ IN_PROC_BROWSER_TEST_F(NotificationTest, TestStateTransition1) {
 // This test depends on the fact that the panel state change occurs
 // quicker than stale timeout, thus the stale timeout cannot be set to
 // 0. This test explicitly controls the stale state instead.
-
-// TODO(oshima): bug chromium-os:7306 Fix this flaky test on ChromeOS.
-IN_PROC_BROWSER_TEST_F(NotificationTest, FLAKY_TestStateTransition2) {
+IN_PROC_BROWSER_TEST_F(NotificationTest, TestStateTransition2) {
   // Register observer here as the registration does not work in SetUp().
   NotificationRegistrar registrar;
   registrar.Add(this,
