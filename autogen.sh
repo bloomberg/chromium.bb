fd3ed34a2070fca3804baf54ece40d0bc2666226
Javier Jardón <javier.jardon@codethink.co.uk>
2011-02-09 13:28:20 +0000

    build: Update autotools configuration
    Replace some deprecated autoconf macros and use the new libtool
    syntax

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,6 @@
 #! /bin/sh
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-ORIGDIR=`pwd`
-cd $srcdir
-
-autoreconf -v --install || exit 1
-cd $ORIGDIR || exit $?
-
-$srcdir/configure --enable-maintainer-mode "$@"
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+autoreconf --force --install --verbose "$srcdir"
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
