a024cd606b92f7d4070a1110d04e87f0b2841ad5
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-10 19:58:57 +0000

    Revert 74398 - Revert 74315 to see if it fixes media_unittests - GPU blacklist reporting to about:gpu page.
    BUG=71879
    TEST=
    
    Review URL: http://codereview.chromium.org/6460016
    Patch from John Bates <jbates@chromiu.org>.
    
    TBR=nduca@chromium.org
    Review URL: http://codereview.chromium.org/6476019
    
    TBR=nduca@chromium.org
    Review URL: http://codereview.chromium.org/6474007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74465 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6881b9fa4ecd56c0eaac31c562df23348f707aa8

--- chrome/browser/gpu_process_host_ui_shim.cc
+++ chrome/browser/gpu_process_host_ui_shim.cc
@@ -275,6 +275,12 @@ const GPUInfo& GpuProcessHostUIShim::gpu_info() const {
   return gpu_info_;
 }
 
+void GpuProcessHostUIShim::AddCustomLogMessage(int level,
+    const std::string& header,
+    const std::string& message) {
+ OnLogMessage(level, header, message);
+}
+
 bool GpuProcessHostUIShim::OnControlMessageReceived(
     const IPC::Message& message) {
   DCHECK(CalledOnValidThread());
@@ -340,6 +346,7 @@ void GpuProcessHostUIShim::OnChannelEstablished(
   if (gpu_feature_flags_.flags() != 0) {
     Send(new GpuMsg_CloseChannel(channel_handle));
     EstablishChannelError(callback.release(), IPC::ChannelHandle(), gpu_info);
+    AddCustomLogMessage(logging::LOG_WARNING, "WARNING", "GPU is blacklisted.");
   } else {
     callback->Run(channel_handle, gpu_info);
   }
