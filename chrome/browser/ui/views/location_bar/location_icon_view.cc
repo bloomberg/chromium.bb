9213be151f2b512a7e91e27eb62e971ed529d226
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-11 00:55:50 +0000

    views: Show a tooltip for the location icon in the omnibox.
    BUG=41073
    TEST=launch chrome, hover the location icon in the left, a tooltip with "Click to view site information" should appears.
    
    Review URL: http://codereview.chromium.org/6482030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74537 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0f44f96056935ebe5c79920c48be70ada5d15166

--- chrome/browser/ui/views/location_bar/location_icon_view.cc
+++ chrome/browser/ui/views/location_bar/location_icon_view.cc
@@ -4,8 +4,14 @@
 
 #include "chrome/browser/ui/views/location_bar/location_icon_view.h"
 
+#include "base/utf_string_conversions.h"
+#include "grit/generated_resources.h"
+#include "ui/base/l10n/l10n_util.h"
+
 LocationIconView::LocationIconView(const LocationBarView* location_bar)
     : ALLOW_THIS_IN_INITIALIZER_LIST(click_handler_(this, location_bar)) {
+  SetTooltipText(UTF16ToWide(l10n_util::GetStringUTF16(
+      IDS_TOOLTIP_LOCATION_ICON)));
 }
 
 LocationIconView::~LocationIconView() {
