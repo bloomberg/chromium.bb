250ccdf4aaed3701fae6ea4fc54689f7a160d59f
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-14 13:26:43 +0000

    Attempt to see if this still crashes.
    I suspect it was crashing due to a problem unrelated to this test.
    
    Also remove some debugging code.
    
    BUG=64304
    TEST=AllUrlsApiTest.RegularExtensions
    
    Review URL: http://codereview.chromium.org/6469024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@74813 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 50431b97f7a1e41be35cd03f717fa73fdac9d9a7

--- chrome/browser/extensions/all_urls_apitest.cc
+++ chrome/browser/extensions/all_urls_apitest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -26,9 +26,6 @@ void Checkpoint(const char* message, const base::TimeTicks& start_time) {
 }  // namespace
 
 IN_PROC_BROWSER_TEST_F(AllUrlsApiTest, WhitelistedExtension) {
-  base::TimeTicks start_time = base::TimeTicks::Now();
-  Checkpoint("Setting up extensions", start_time);
-
   // First setup the two extensions.
   FilePath extension_dir1 = test_data_dir_.AppendASCII("all_urls")
                                           .AppendASCII("content_script");
@@ -36,98 +33,84 @@ IN_PROC_BROWSER_TEST_F(AllUrlsApiTest, WhitelistedExtension) {
                                           .AppendASCII("execute_script");
 
   // Then add the two extensions to the whitelist.
-  Checkpoint("Setting up whitelist", start_time);
   Extension::ScriptingWhitelist whitelist;
   whitelist.push_back(Extension::GenerateIdForPath(extension_dir1));
   whitelist.push_back(Extension::GenerateIdForPath(extension_dir2));
   Extension::SetScriptingWhitelist(whitelist);
 
   // Then load extensions.
-  Checkpoint("LoadExtension1", start_time);
   ExtensionService* service = browser()->profile()->GetExtensionService();
   const size_t size_before = service->extensions()->size();
   ASSERT_TRUE(LoadExtension(extension_dir1));
-  Checkpoint("LoadExtension2", start_time);
   ASSERT_TRUE(LoadExtension(extension_dir2));
   EXPECT_EQ(size_before + 2, service->extensions()->size());
 
   std::string url;
 
   // Now verify we run content scripts on chrome://newtab/.
-  Checkpoint("Verify content scripts", start_time);
   url = "chrome://newtab/";
   ExtensionTestMessageListener listener1a("content script: " + url, false);
   ExtensionTestMessageListener listener1b("execute: " + url, false);
   ui_test_utils::NavigateToURL(browser(), GURL(url));
-  Checkpoint("Wait for 1a", start_time);
   ASSERT_TRUE(listener1a.WaitUntilSatisfied());
-  Checkpoint("Wait for 1b", start_time);
   ASSERT_TRUE(listener1b.WaitUntilSatisfied());
 
   // Now verify data: urls.
-  Checkpoint("Verify data:urls", start_time);
   url = "data:text/html;charset=utf-8,<html>asdf</html>";
   ExtensionTestMessageListener listener2a("content script: " + url, false);
   ExtensionTestMessageListener listener2b("execute: " + url, false);
   ui_test_utils::NavigateToURL(browser(), GURL(url));
-  Checkpoint("Wait for 2a", start_time);
   ASSERT_TRUE(listener2a.WaitUntilSatisfied());
-  Checkpoint("Wait for 2b", start_time);
   ASSERT_TRUE(listener2b.WaitUntilSatisfied());
 
   // Now verify about:version.
-  Checkpoint("Verify about:version", start_time);
   url = "about:version";
   ExtensionTestMessageListener listener3a("content script: " + url, false);
   ExtensionTestMessageListener listener3b("execute: " + url, false);
   ui_test_utils::NavigateToURL(browser(), GURL(url));
-  Checkpoint("Wait for 3a", start_time);
   ASSERT_TRUE(listener3a.WaitUntilSatisfied());
-  Checkpoint("Wait for 3b", start_time);
   ASSERT_TRUE(listener3b.WaitUntilSatisfied());
 
   // Now verify about:blank.
-  Checkpoint("Verify about:blank", start_time);
   url = "about:blank";
   ExtensionTestMessageListener listener4a("content script: " + url, false);
   ExtensionTestMessageListener listener4b("execute: " + url, false);
   ui_test_utils::NavigateToURL(browser(), GURL(url));
-  Checkpoint("Wait for 4a", start_time);
   ASSERT_TRUE(listener4a.WaitUntilSatisfied());
-  Checkpoint("Wait for 4b", start_time);
   ASSERT_TRUE(listener4b.WaitUntilSatisfied());
 
   // Now verify we can script a regular http page.
-  Checkpoint("Verify regular http page", start_time);
   ASSERT_TRUE(test_server()->Start());
   GURL page_url = test_server()->GetURL(kAllUrlsTarget);
   ExtensionTestMessageListener listener5a("content script: " + page_url.spec(),
                                           false);
   ExtensionTestMessageListener listener5b("execute: " + page_url.spec(), false);
   ui_test_utils::NavigateToURL(browser(), page_url);
-  Checkpoint("Wait for 5a", start_time);
   ASSERT_TRUE(listener5a.WaitUntilSatisfied());
-  Checkpoint("Wait for 5ba", start_time);
   ASSERT_TRUE(listener5b.WaitUntilSatisfied());
-
-  Checkpoint("Test complete", start_time);
 }
 
 // Test that an extension NOT whitelisted for scripting can ask for <all_urls>
 // and run scripts on non-restricted all pages.
-// Disabled, http://crbug.com/64304.
-IN_PROC_BROWSER_TEST_F(AllUrlsApiTest, DISABLED_RegularExtensions) {
+IN_PROC_BROWSER_TEST_F(AllUrlsApiTest, RegularExtensions) {
+  base::TimeTicks start_time = base::TimeTicks::Now();
+  Checkpoint("Test starting", start_time);
+
   // First load the two extension.
   FilePath extension_dir1 = test_data_dir_.AppendASCII("all_urls")
                                           .AppendASCII("content_script");
   FilePath extension_dir2 = test_data_dir_.AppendASCII("all_urls")
                                           .AppendASCII("execute_script");
 
+  Checkpoint("Loading extensions", start_time);
+
   ExtensionService* service = browser()->profile()->GetExtensionService();
   const size_t size_before = service->extensions()->size();
   ASSERT_TRUE(LoadExtension(extension_dir1));
   ASSERT_TRUE(LoadExtension(extension_dir2));
   EXPECT_EQ(size_before + 2, service->extensions()->size());
+
+  Checkpoint("Starting server", start_time);
 
   // Now verify we can script a regular http page.
   ASSERT_TRUE(test_server()->Start());
@@ -135,7 +118,9 @@ IN_PROC_BROWSER_TEST_F(AllUrlsApiTest, DISABLED_RegularExtensions) {
   ExtensionTestMessageListener listener1a("content script: " + page_url.spec(),
                                           false);
   ExtensionTestMessageListener listener1b("execute: " + page_url.spec(), false);
+  Checkpoint("Navigate to URL", start_time);
   ui_test_utils::NavigateToURL(browser(), page_url);
   ASSERT_TRUE(listener1a.WaitUntilSatisfied());
   ASSERT_TRUE(listener1b.WaitUntilSatisfied());
+  Checkpoint("Test ending", start_time);
 }
