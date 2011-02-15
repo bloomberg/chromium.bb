fcfb4e30a47a4cd2416dac210521594b489c8bfd
Kristian Høgsberg <krh@bitplanet.net>
2011-02-14 22:17:41 -0500

    Split into a core repository that only holds the core Wayland libraries

--- wayland-scanner.mk
+++ wayland-scanner.mk
@@ -1,8 +1,8 @@
-%-protocol.c : $(protocoldir)/%.xml
+%-protocol.c : $(wayland_protocoldir)/%.xml
 	$(AM_V_GEN)$(wayland_scanner) code < $< > $@
 
-%-server-protocol.h : $(protocoldir)/%.xml
+%-server-protocol.h : $(wayland_protocoldir)/%.xml
 	$(AM_V_GEN)$(wayland_scanner) server-header < $< > $@
 
-%-client-protocol.h : $(protocoldir)/%.xml
+%-client-protocol.h : $(wayland_protocoldir)/%.xml
 	$(AM_V_GEN)$(wayland_scanner) client-header < $< > $@
