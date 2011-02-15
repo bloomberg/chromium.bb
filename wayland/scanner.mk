b2d71852fd2ace71722f73065dbccb09c8e9910e
Kristian Høgsberg <krh@bitplanet.net>
2011-02-14 22:13:33 -0500

    Split into a demo repository that holds the demo compositor and clients

--- wayland/scanner.mk
+++
@@ -1,8 +0,0 @@
-%-protocol.c : $(top_srcdir)/protocol/%.xml
-	$(AM_V_GEN)$(top_builddir)/wayland/scanner code < $< > $@
-
-%-server-protocol.h : $(top_srcdir)/protocol/%.xml
-	$(AM_V_GEN)$(top_builddir)/wayland/scanner server-header < $< > $@
-
-%-client-protocol.h : $(top_srcdir)/protocol/%.xml
-	$(AM_V_GEN)$(top_builddir)/wayland/scanner client-header < $< > $@
