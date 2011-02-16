4c3be52391ec77d2c2a543f249a6a27710ed5f6e
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-16 00:17:22 +0000

    Basic implementation of Pepper Transport API.
    BUG=None
    TEST=Unittests
    
    Review URL: http://codereview.chromium.org/6478018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75035 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9b4cb193328469789bf1408e653f218f2940a9fa

--- ppapi/cpp/dev/transport_dev.cc
+++ ppapi/cpp/dev/transport_dev.cc
@@ -1,13 +1,15 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "ppapi/cpp/dev/transport_dev.h"
 
+#include "ppapi/c/pp_errors.h"
 #include "ppapi/cpp/instance.h"
 #include "ppapi/cpp/resource.h"
 #include "ppapi/cpp/module.h"
 #include "ppapi/cpp/module_impl.h"
+#include "ppapi/cpp/var.h"
 
 namespace pp {
 
@@ -27,5 +29,58 @@ Transport_Dev::Transport_Dev(Instance* instance,
         instance->pp_instance(), name, proto));
 }
 
-}  // namespace pp
+bool Transport_Dev::IsWritable() {
+  if (!has_interface<PPB_Transport_Dev>())
+    return false;
+  return PPBoolToBool(
+      get_interface<PPB_Transport_Dev>()->IsWritable(pp_resource()));
+}
 
+int32_t Transport_Dev::Connect(const CompletionCallback& cc) {
+  if (!has_interface<PPB_Transport_Dev>())
+    return PP_ERROR_NOINTERFACE;
+  return get_interface<PPB_Transport_Dev>()->Connect(
+      pp_resource(), cc.pp_completion_callback());
+}
+
+int32_t Transport_Dev::GetNextAddress(Var* address,
+                                      const CompletionCallback& cc) {
+  if (!has_interface<PPB_Transport_Dev>())
+    return PP_ERROR_NOINTERFACE;
+  PP_Var temp_address = PP_MakeUndefined();
+  int32_t ret_val = get_interface<PPB_Transport_Dev>()->GetNextAddress(
+      pp_resource(), &temp_address, cc.pp_completion_callback());
+  *address = Var(Var::PassRef(), temp_address);
+  return ret_val;
+}
+
+int32_t Transport_Dev::ReceiveRemoteAddress(const pp::Var& address) {
+  if (!has_interface<PPB_Transport_Dev>())
+    return PP_ERROR_NOINTERFACE;
+  return get_interface<PPB_Transport_Dev>()->ReceiveRemoteAddress(
+      pp_resource(), address.pp_var());
+}
+
+int32_t Transport_Dev::Recv(void* data, uint32_t len,
+                            const CompletionCallback& cc) {
+  if (!has_interface<PPB_Transport_Dev>())
+    return PP_ERROR_NOINTERFACE;
+  return get_interface<PPB_Transport_Dev>()->Recv(
+      pp_resource(), data, len, cc.pp_completion_callback());
+}
+
+int32_t Transport_Dev::Send(const void* data, uint32_t len,
+                            const CompletionCallback& cc) {
+  if (!has_interface<PPB_Transport_Dev>())
+    return PP_ERROR_NOINTERFACE;
+  return get_interface<PPB_Transport_Dev>()->Send(
+      pp_resource(), data, len, cc.pp_completion_callback());
+}
+
+int32_t Transport_Dev::Close() {
+  if (!has_interface<PPB_Transport_Dev>())
+    return PP_ERROR_NOINTERFACE;
+  return get_interface<PPB_Transport_Dev>()->Close(pp_resource());
+}
+
+}  // namespace pp
