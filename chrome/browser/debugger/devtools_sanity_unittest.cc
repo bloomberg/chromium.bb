39973399627e54bd1dcf31c9e65deae1461c5a5a
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-17 15:51:23 +0000

    Mark DevToolsSanityTest.TestScriptsTabIsPopulatedOnInspectedPageRefresh as disabled, as it timeouts, not fails.
    BUG=73289
    TEST=DevToolsSanityTest.TestScriptsTabIsPopulatedOnInspectedPageRefresh
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75274 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e23d057719a0dc1c74370a251308cefda9e242f2

--- chrome/browser/debugger/devtools_sanity_unittest.cc
+++ chrome/browser/debugger/devtools_sanity_unittest.cc
@@ -259,9 +259,9 @@ IN_PROC_BROWSER_TEST_F(DevToolsSanityTest, TestShowScriptsTab) {
 // Tests that scripts tab is populated with inspected scripts even if it
 // hadn't been shown by the moment inspected paged refreshed.
 // @see http://crbug.com/26312
-// Marked as failing: crbug/73289.
+// Marked as disabled: crbug/73289.
 IN_PROC_BROWSER_TEST_F(DevToolsSanityTest,
-                       FAILS_TestScriptsTabIsPopulatedOnInspectedPageRefresh) {
+                       DISABLED_TestScriptsTabIsPopulatedOnInspectedPageRefresh) {
   // Clear inspector settings to ensure that Elements will be
   // current panel when DevTools window is open.
   GetInspectedTab()->render_view_host()->delegate()->ClearInspectorSettings();
