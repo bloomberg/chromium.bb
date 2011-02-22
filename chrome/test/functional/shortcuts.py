6f9603faf171e967dd20921ce3334a6e60721864
sunandt@chromium.org <sunandt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-22 18:23:47 +0000

    Adding shortcuts.py to verify browser shortcuts. Splitting the existing single shortcuts test into different tests so that even if one fails other shortcuts would be verified.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/6543022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75611 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 92d6d5267f0b908c6dab1496dc4f95cd4d70ee19

--- chrome/test/functional/shortcuts.py
+++ chrome/test/functional/shortcuts.py
@@ -3,86 +3,125 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+
 import pyauto_functional
 import pyauto
 
 
 class ShortcutsTest(pyauto.PyUITest):
-  """Test for browser shortcuts."""
+  """Test for browser shortcuts.
 
-  def testShortcuts(self):
-    """Verify shortcuts work."""
-    # Open a new tab.
+  No tests for print, save page as... shortcuts as they involve interaction
+  with OS native dialogs.
+  """
+
+  def testNewTabShortcut(self):
+    """Verify new tab shortcut."""
     self.RunCommand(pyauto.IDC_NEW_TAB)
-    self.assertEqual(2, self.GetTabCount())
+    self.assertEqual(2, self.GetTabCount(), msg='Can not open a new tab.')
 
-    # Close a tab.
+  def testCloseTabShortcut(self):
+    """Verify close tab shortcut."""
+    self.RunCommand(pyauto.IDC_NEW_TAB)
+    self.assertEqual(2, self.GetTabCount(), msg='Can not open a new tab.')
     self.RunCommand(pyauto.IDC_CLOSE_TAB)
-    self.assertEqual(1, self.GetTabCount())
+    self.assertEqual(1, self.GetTabCount(), msg='Can not close a tab.')
 
-    # Open a new window.
+  def testReopenClosedTabShortcut(self):
+    """Verify reopen closed tab shortcut opens recently closed tab."""
+    self.RunCommand(pyauto.IDC_NEW_TAB)
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title2.html'))
+    self.NavigateToURL(url)
+    title = self.GetActiveTabTitle()
+    self.GetBrowserWindow(0).GetTab(1).Close()
+    self.assertEqual(1, self.GetTabCount(), msg='Can not close a tab.')
+    # Verify shortcut reopens the correct tab.
+    self.RunCommand(pyauto.IDC_RESTORE_TAB)
+    self.assertEqual(2, self.GetTabCount(), msg='Can not restore a tab.')
+    self.assertEqual(title, self.GetActiveTabTitle())
+
+  def testNewWindowShortcut(self):
+    """Verify new window shortcut."""
     self.RunCommand(pyauto.IDC_NEW_WINDOW)
     self.assertEquals(2, self.GetBrowserWindowCount())
 
-    # Close a window.
-    self.RunCommand(pyauto.IDC_CLOSE_WINDOW, 1)
-    self.assertEquals(1, self.GetBrowserWindowCount())
-
-    # Open a new incognito window.
+  def testNewIncognitoWindowShortcut(self):
+    """Verify new incognito window shortcut launches incognito window."""
     self.RunCommand(pyauto.IDC_NEW_INCOGNITO_WINDOW)
     self.assertEqual(2, self.GetBrowserWindowCount())
-    self.CloseBrowserWindow(1)
+    # Check if it is incognito by checking history.
+    assert not self.GetHistoryInfo().History(), 'Expecting clean history.'
+    url = self.GetFileURLForPath(os.path.join(self.DataDir(), 'title2.html'))
+    self.NavigateToURL(url, 1, 0)
+    self.assertEqual(0, len(self.GetHistoryInfo().History()))
 
-    # Open find in box.
+  def testFindShortcut(self):
+    """Verify find in box shortcut."""
     self.ApplyAccelerator(pyauto.IDC_FIND)
     self.assertTrue(self.WaitUntil(lambda: self.IsFindInPageVisible()),
                     msg='Find in box is not visible.')
 
-    # Always show Bookmark bar.
+  def testAlwaysShowBookmarksBarShortcut(self):
+    """Verify always show bookmarks bar shortcut."""
+    # Show bookmark bar.
     self.ApplyAccelerator(pyauto.IDC_SHOW_BOOKMARK_BAR)
     self.assertTrue(self.WaitUntil(lambda: self.GetBookmarkBarVisibility()),
-                    msg='Bookmark bar not visible.')
+                    msg='Bookmarks bar is not visible.')
+    # Hide bookmark bar.
+    self.ApplyAccelerator(pyauto.IDC_SHOW_BOOKMARK_BAR)
+    self.assertTrue(self.WaitUntil(lambda:
+        self.GetBookmarkBarVisibility() is False),
+        msg='Bookmarks bar is visible.')
 
-    # View Source.
+  # TODO: Task Manager Shortcut. crbug.com/73454
+
+  def testViewSourceShortcut(self):
+    """Verify view source shortcut."""
     self.ApplyAccelerator(pyauto.IDC_VIEW_SOURCE)
-    self.assertEqual(2, self.GetTabCount(), msg='Can\'t View Source.')
+    self.assertEqual(2, self.GetTabCount(), msg='Cannot View Source.')
     self.assertEqual('view-source:about:blank', self.GetActiveTabURL().spec(),
                       msg='View Source URL is not correct.')
-    self.GetBrowserWindow(0).GetTab(1).Close()
 
-    # Open Developer Tools.
+  def testDeveloperToolsShortcut(self):
+    """Verify developer tools shortcut opens developer tools window.."""
     # Setting the pref to undock devtools so that it can be seen
     # as a separate window.
     self.SetPrefs(pyauto.kDevToolsOpenDocked, False)
     self.ApplyAccelerator(pyauto.IDC_DEV_TOOLS)
     self.assertEqual(2, self.GetBrowserWindowCount(),
                      msg='DevTools window is not open.')
-    self.CloseBrowserWindow(1)
 
-    # Open Javascript Console.
+  def testJavaScriptConsoleShortcut(self):
+    """Verify javascript console shortcut opens developer tools window.
+    We can not check if console is open or not.
+    We are making sure at least the shortcut launches developer tools window.
+    """
+    # Setting the pref to undock devtools so that it can be seen
+    # as a separate window.
+    self.SetPrefs(pyauto.kDevToolsOpenDocked, False)
     self.ApplyAccelerator(pyauto.IDC_DEV_TOOLS_CONSOLE)
     self.assertEqual(2, self.GetBrowserWindowCount(),
-                     msg='DevTools console is not open.')
-    self.CloseBrowserWindow(1)
+                     msg='DevTools window is not open.')
 
-    # Open History.
+  def testHistoryShortcut(self):
+    """Verify history shortcut opens history page."""
     self.RunCommand(pyauto.IDC_SHOW_HISTORY)
     self.assertEqual('History', self.GetActiveTabTitle(),
                      msg='History page was not opened.')
-    self.GetBrowserWindow(0).GetTab(1).Close()
 
-    # Open Downloads.
+  def testDownloadsShortcut(self):
+    """Verify downloads shortcut opens downloads page."""
     self.RunCommand(pyauto.IDC_SHOW_DOWNLOADS)
     self.assertEqual('Downloads', self.GetActiveTabTitle(),
                      msg='Downloads page was not opened.')
-    self.GetBrowserWindow(0).GetTab(1).Close()
 
-    # Open Help page.
+  def testHelpShortcut(self):
+    """Verify help shortcut opens help page."""
     self.ApplyAccelerator(pyauto.IDC_HELP_PAGE)
     self.assertTrue(self.WaitUntil(lambda: self.GetActiveTabTitle(),
                     expect_retval='Google Chrome Help'),
                     msg='Google Chrome help page has not opened.')
-    self.GetBrowserWindow(0).GetTab(1).Close()
 
 
 if __name__ == '__main__':
