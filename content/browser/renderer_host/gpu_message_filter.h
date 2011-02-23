a076bd095f59b2051996723d9c8acbe9f6084065
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-02-23 21:03:26 +0000

    Fix bad merge at r75645.
    TEST=try
    BUG=none
    
    Review URL: http://codereview.chromium.org/6580007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@75782 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0b4d43bc81aa786440e8e71de990fad93b32ca5d

--- content/browser/renderer_host/gpu_message_filter.h
+++ content/browser/renderer_host/gpu_message_filter.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_RENDERER_HOST_GPU_MESSAGE_FILTER_H_
-#define CHROME_BROWSER_RENDERER_HOST_GPU_MESSAGE_FILTER_H_
+#ifndef CONTENT_BROWSER_RENDERER_HOST_GPU_MESSAGE_FILTER_H_
+#define CONTENT_BROWSER_RENDERER_HOST_GPU_MESSAGE_FILTER_H_
 #pragma once
 
 #include "chrome/browser/browser_message_filter.h"
@@ -51,4 +51,4 @@ class GpuMessageFilter : public BrowserMessageFilter,
   DISALLOW_COPY_AND_ASSIGN(GpuMessageFilter);
 };
 
-#endif  // CHROME_BROWSER_RENDERER_HOST_GPU_MESSAGE_FILTER_H_
+#endif  // CONTENT_BROWSER_RENDERER_HOST_GPU_MESSAGE_FILTER_H_
