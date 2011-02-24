bloomberg/chromium.bb

commit 3fda4ccee33d5449796f75a0c972dce342ecbaab

Add explicit file rename/cp information to svn rietveld uploads
Right now if you svn cp or svn mv before uploading to rietveld,
there's no description in the patch at all of what was done; so you
get a diff, but it's for who knows what revision of what prior file
to some current version.

For code reviews this kinda works (you ask the guy what it was), but for
trying to apply patches (ala git patch) this fails badly.  This patch
tries to add some metadata to the start of a rietveld patch that
describes these changes: both to a human (who can read them)
and to a potential clever future git-cl (which I'll do next).

The metadata looks like this after checking out a test repo, and
svn cp -r 1 foo nitz; svn cp bar quux; echo be good>>quux

  ### BEGIN SVN COPY METADATA
  #$ svn cp -r 1 foo nitz ### WARNING: note non-trunk copy
  #$ cp bar quux
  ### END SVN COPY METADATA
  Index: nitz
  Index: quux
  ===================================================================
  --- quux	(revision 0)
  +++ quux	(working copy)
  @@ -1,2 +1,3 @@
   hi mom
   hi sister
  +be good

I did a test, and this looks like it works in svn 1.4.4 as well.

BUG=none
TEST=none
Review URL: http://codereview.chromium.org/2824035

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@51121 0039d316-1c4b-4281-b951-d872f2087c98

scm.py
@@ -800,6 +800,7 @@ class SVN(object):
       filenames = [RelativePath(f, root) for f in filenames]
       # Get information about the modified items (files and directories)
       data = dict([(f, SVN.CaptureInfo(f)) for f in filenames])
+      diffs = []
       if full_move:
         # Eliminate modified files inside moved/copied directory.
         for (filename, info) in data.iteritems():
@@ -811,8 +812,32 @@ class SVN(object):
           if not filename in filenames:
             # Remove filtered out items.
             del data[filename]
+      else:
+        metaheaders = []
+        for (filename, info) in data.iteritems():
+          if SVN.IsMovedInfo(info):
+            # for now, the most common case is a head copy,
+            # so let's just encode that as a straight up cp.
+            srcurl = info.get('Copied From URL')
+            root = info.get('Repository Root')
+            rev = int(info.get('Copied From Rev'))
+            assert srcurl.startswith(root)
+            src = srcurl[len(root)+1:]
+            srcinfo = SVN.CaptureInfo(srcurl)
+            if (srcinfo.get('Revision') != rev and
+                SVN.Capture(['diff', '-r', '%d:head' % rev, srcurl])):
+              metaheaders.append("#$ svn cp -r %d %s %s "
+                                 "### WARNING: note non-trunk copy\n" %
+                                 (rev, src, filename))
+            else:
+              metaheaders.append("#$ cp %s %s\n" % (src,
+                                                    filename))
+
+        if metaheaders:
+          diffs.append("### BEGIN SVN COPY METADATA\n")
+          diffs.extend(metaheaders)
+          diffs.append("### END SVN COPY METADATA\n")
       # Now ready to do the actual diff.
-      diffs = []
       for filename in sorted(data.iterkeys()):
         diffs.append(SVN._DiffItemInternal(filename, data[filename], bogus_dir,
                                            full_move=full_move,

commit 00fdcb33802167bedbd890479efc4cad769f2290

Fix bug introduced in r51121: gcl upload crashes on a 'revived' file.
To reproduce, svn delete a file, then svn merge -c -N the file to revive it.

Also fix a unit test that was broken for a while.

TEST=none
BUG=none

Review URL: http://codereview.chromium.org/6576005

git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@75835 0039d316-1c4b-4281-b951-d872f2087c98

scm.py
@@ -659,6 +659,7 @@ class SVN(object):
     used.
     The diff will always use relative paths.
     """
+    assert isinstance(filenames, (list, tuple))
     previous_cwd = os.getcwd()
     root = root or SVN.GetCheckoutRoot(previous_cwd)
     root = os.path.normcase(os.path.join(root, ''))
@@ -703,7 +704,14 @@ class SVN(object):
             rev = int(info.get('Copied From Rev'))
             assert srcurl.startswith(root)
             src = srcurl[len(root)+1:]
-            srcinfo = SVN.CaptureInfo(srcurl)
+            try:
+              srcinfo = SVN.CaptureInfo(srcurl)
+            except gclient_utils.CheckCallError, e:
+              if not 'Not a valid URL' in e.stderr:
+                raise
+              # Assume the file was deleted. No idea how to figure out at which
+              # revision the file was deleted.
+              srcinfo = {'Revision': rev}
             if (srcinfo.get('Revision') != rev and
                 SVN.Capture(['diff', '-r', '%d:head' % rev, srcurl])):
               metaheaders.append("#$ svn cp -r %d %s %s "

tests/presubmit_unittest.py
@@ -257,8 +257,8 @@ class PresubmitUnittest(PresubmitTestsBase):
     presubmit.scm.SVN.CaptureInfo(notfound).AndReturn({})
     presubmit.scm.SVN.CaptureInfo(flap).AndReturn(
             {'URL': 'svn:/foo/boo/flap.h'})
-    presubmit.scm.SVN.GenerateDiff(blat).AndReturn(self.presubmit_diffs)
-    presubmit.scm.SVN.GenerateDiff(notfound).AndReturn(self.presubmit_diffs)
+    presubmit.scm.SVN.GenerateDiff([blat]).AndReturn(self.presubmit_diffs)
+    presubmit.scm.SVN.GenerateDiff([notfound]).AndReturn(self.presubmit_diffs)
 
     self.mox.ReplayAll()
 
@@ -776,8 +776,8 @@ class InputApiUnittest(PresubmitTestsBase):
     presubmit.scm.SVN.GetFileProperty(another, 'svn:mime-type').AndReturn(None)
     presubmit.scm.SVN.GetFileProperty(third_party, 'svn:mime-type'
         ).AndReturn(None)
-    presubmit.scm.SVN.GenerateDiff(blat).AndReturn(self.presubmit_diffs)
-    presubmit.scm.SVN.GenerateDiff(another).AndReturn(self.presubmit_diffs)
+    presubmit.scm.SVN.GenerateDiff([blat]).AndReturn(self.presubmit_diffs)
+    presubmit.scm.SVN.GenerateDiff([another]).AndReturn(self.presubmit_diffs)
 
     self.mox.ReplayAll()
 