921080d4f74c6de2119bd3d185ba7908865a375b
phajdan.jr@chromium.org <phajdan.jr@chromium.org@4ff67af0-8c30-449e-8e8b-ad334ec8d88c>
2011-02-24 17:36:38 +0000

    Linux: make it possible to build with system FLAC
    This is upstreaming Gentoo Linux changes.
    
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6574024
    
    git-svn-id: http://src.chromium.org/svn/trunk/deps/third_party/flac@75901 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

--- flac.h
+++ flac.h
@@ -9,6 +9,10 @@
 // This is a shim header to include the right flac headers.
 // Use this instead of referencing the flac headers directly.
 
+#if defined(USE_SYSTEM_FLAC)
+#include <FLAC/stream_encoder.h>
+#else
 #include "third_party/flac/include/FLAC/stream_encoder.h"
+#endif
 
 #endif  // THIRD_PARTY_FLAC_FLAC_H_
