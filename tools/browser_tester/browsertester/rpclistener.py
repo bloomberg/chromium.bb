2587fbb471882a05a0a742e31d6cabf3c7a8833a
ncbray@google.com <ncbray@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-02-25 18:07:32 +0000

    Browser Tester: refactored internal RPC interface.
    The only RPC calls passing between the browser and the Python script
    are now "Log" and "Shutdown".
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1151
    TEST=./scons chrome_browser_tests
    
    Review URL: http://codereview.chromium.org/6576012
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4403 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 8e405bcb32f718296887c8281ac6c0213f454507

--- tools/browser_tester/browsertester/rpclistener.py
+++ tools/browser_tester/browsertester/rpclistener.py
@@ -4,103 +4,47 @@
 # be found in the LICENSE file.
 
 
+import sys
+
+
 class RPCListener(object):
 
   def __init__(self, shutdown_callback):
     self.shutdown_callback = shutdown_callback
     self.prefix = '**** '
     self.ever_failed = False
-    self.ResetStatus()
-
-  def ResetStatus(self):
-    # NOTE this does not reset ever_failed because it should never be reset
-    self.passed = 0
-    self.failed = 0
-    self.errors = 0
 
   def Log(self, message):
-    print '%s%s\n' % (self.prefix, message),
+    lines = [line.rstrip() for line in message.split('\n')]
+    text = ''.join(['%s%s\n' % (self.prefix, line) for line in lines])
+    sys.stdout.write(text)
 
-  def NewLine(self):
-    self.Log('')
-
-  def Startup(self):
-    self.ResetStatus()
-    self.Log('[STARTUP]')
-    self.NewLine()
-    return 'OK'
-
-  def TestBegin(self, test_name):
-    self.Log('[%s BEGIN]' % (test_name,))
-    return 'OK'
-
-  def TestLog(self, test_name, message):
-    self.Log('[%s LOG] %s' % (test_name, message))
-    return 'OK'
-
-  def TestPassed(self, test_name):
-    self.passed += 1
-    self.Log('[%s PASSED]' % (test_name,))
-    self.NewLine()
-    return 'OK'
-
-  # The test decided it failed
-  def TestFailed(self, test_name, message):
-    self._TestFailed()
-    self.Log('[%s FAILED] %s' % (test_name, message))
-    self.NewLine()
-    return 'OK'
-
-  # A recognizable exception
-  def TestException(self, test_name, message, stack):
-    self._TestFailed()
-    self.Log('[%s EXCEPTION] %s' % (test_name, message.rstrip()))
-    for line in stack.rstrip().split('\n'):
-      self.Log('    ' + line)
-    self.NewLine()
-    return 'OK'
-
-  # An unrecognizable exception
-  def TestError(self, test_name, message):
-    self._TestFailed()
-    self.Log('[%s ERROR] %s' % (test_name, message))
-    self.NewLine()
-    return 'OK'
-
-  # Something went wrong on the client side, the test is probably screwed up
-  def ClientError(self, message):
-    self._ExternalError()
-    self.Log('[CLIENT_ERROR] %s' % (message,))
-    self.NewLine()
-    # Keep running... see what else the client says.
+  def TestLog(self, message):
+    self.Log(message)
     return 'OK'
 
   # Something went very wrong on the server side, everything is horked?
+  # Only called locally.
   def ServerError(self, message):
-    self._ExternalError()
-    self.Log('[SERVER_ERROR] %s' % (message,))
+    self.Log('\n[SERVER_ERROR] %s' % (message,))
+    self.ever_failed = True
     self._TestingDone()
     return 'OK'
 
-  def Shutdown(self):
-    self.Log('[SHUTDOWN] %s' % (self.StatusString(),))
+  def Shutdown(self, message, passed):
+    self.Log(message)
+    # This check looks slightly backwards, but this is intentional.
+    # Everything but passed.lower() == 'true' is considered a failure.  This
+    # means that if the test runner sends garbage, it will be a failure.
+    # NOTE in interactive mode this function may be called multiple times.
+    # ever_failed is designed to be set and never reset - if any of the runs
+    # fail, the an error code will be returned to the command line.
+    # In summary, the tester is biased towards failure - it should scream "FAIL"
+    # if things are not 100% correct.  False positives must be avoided.
+    if passed.lower() != 'true':
+      self.ever_failed = True
     self._TestingDone()
     return 'OK'
-
-  def _TestFailed(self):
-    self.failed += 1
-    self.ever_failed = True
-
-  def _ExternalError(self):
-    self.errors += 1
-    self.ever_failed = True
-
-  def StatusString(self):
-    if self.errors:
-      return '%d passed, %d failed, %d errors' % (self.passed, self.failed,
-                                                  self.errors)
-    else:
-      return '%d passed, %d failed' % (self.passed, self.failed)
 
   def _TestingDone(self):
     self.shutdown_callback()
