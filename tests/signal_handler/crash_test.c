d12d4d6914ae66bde08808cdc734df94a1d33023
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-03-01 21:37:42 +0000

    Enable the signal handler test for ARM
    Handling a signal used to fail with our build of qemu-arm, but now
    works.  This may be a result of changes to our local patches (last
    changed in r3977).
    
    Also clean up test to use "is_broken" for disabling for Valgrind.  Use
    "osenv" instead of assigning to "ENV"; the former makes it easier to
    copy-and-paste commands.
    
    BUG=none
    TEST=run_signal_handler_test
    
    Review URL: http://codereview.chromium.org/6598059
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4431 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 426da31e40cbc16c7226252e970c373e899086bf

--- tests/signal_handler/crash_test.c
+++ tests/signal_handler/crash_test.c
@@ -8,7 +8,11 @@
 
 int main() {
   fprintf(stderr, "[CRASH_TEST] Causing crash in untrusted code...\n");
-  *(int *) 0 = 0;
+  /* We use "volatile" because otherwise LLVM gets too clever (at
+     least on ARM) and generates an illegal instruction, which
+     generates SIGILL instead of SIGSEGV, and LLVM also optimises away
+     the rest of the function. */
+  *(volatile int *) 0 = 0;
   fprintf(stderr, "[CRASH_TEST] FAIL: Survived crash attempt\n");
   return 1;
 }
