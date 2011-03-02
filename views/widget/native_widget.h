8ef4e1db36ab240e21965d7c7476c2852a79ce6f
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-02 06:07:00 +0000

    Reverting to fix builder redness. Please reland with the fixes.
    Revert 76485 - Move typedef per review comment. oops!
    
    BUG=none
    TEST=none
    
    
    TBR=ben@chromium.org
    Review URL: http://codereview.chromium.org/6597098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76507 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 99f5dfdbdf3b9d948750a4bbed07b0a2eef2337e

--- views/widget/native_widget.h
+++ views/widget/native_widget.h
@@ -20,8 +20,6 @@ class Widget;
 //
 class NativeWidget {
  public:
-  typedef std::set<NativeWidget*> NativeWidgets;
-
   virtual ~NativeWidget() {}
 
   // Retrieves the NativeWidget implementation associated with the given
@@ -38,6 +36,7 @@ class NativeWidget {
 
   // Returns all NativeWidgets in |native_view|'s hierarchy, including itself if
   // it is one.
+  typedef std::set<NativeWidget*> NativeWidgets;
   static void GetAllNativeWidgets(gfx::NativeView native_view,
                                   NativeWidgets* children);
 
