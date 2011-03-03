b5f01e4e4b17c47ae45490ab3028bc19b9589c80
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-03 23:38:51 +0000

    Modify ThreadChecker and NonThreadSafe so that their functionality is available in release builds if explicitly requested by using their Impl types. The default usage remains that they do nothing in release mode.
    Also, update unit tests to run in release mode, verifying that the release versions of NonThreadSafe and ThreadChecker do nothing in release builds.
    
    BUG=none
    TEST=base unit tests
    
    Review URL: http://codereview.chromium.org/6599004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76833 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4006448408b35ca6be5aef5db1be1e966d31eee4

--- base/threading/thread_checker_impl.cc
+++ base/threading/thread_checker_impl.cc
@@ -1,32 +1,30 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/threading/thread_checker.h"
-
-// This code is only done in debug builds.
-#ifndef NDEBUG
+#include "base/threading/thread_checker_impl.h"
 
 namespace base {
 
-ThreadChecker::ThreadChecker() : valid_thread_id_(kInvalidThreadId) {
+ThreadCheckerImpl::ThreadCheckerImpl()
+    : valid_thread_id_(kInvalidThreadId) {
   EnsureThreadIdAssigned();
 }
 
-ThreadChecker::~ThreadChecker() {}
+ThreadCheckerImpl::~ThreadCheckerImpl() {}
 
-bool ThreadChecker::CalledOnValidThread() const {
+bool ThreadCheckerImpl::CalledOnValidThread() const {
   EnsureThreadIdAssigned();
   AutoLock auto_lock(lock_);
   return valid_thread_id_ == PlatformThread::CurrentId();
 }
 
-void ThreadChecker::DetachFromThread() {
+void ThreadCheckerImpl::DetachFromThread() {
   AutoLock auto_lock(lock_);
   valid_thread_id_ = kInvalidThreadId;
 }
 
-void ThreadChecker::EnsureThreadIdAssigned() const {
+void ThreadCheckerImpl::EnsureThreadIdAssigned() const {
   AutoLock auto_lock(lock_);
   if (valid_thread_id_ != kInvalidThreadId)
     return;
@@ -34,5 +32,3 @@ void ThreadChecker::EnsureThreadIdAssigned() const {
 }
 
 }  // namespace base
-
-#endif  // NDEBUG
