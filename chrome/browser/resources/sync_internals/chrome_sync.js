66a51d89665f767a98164d0bf5951416254f0b2d
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-03 04:06:22 +0000

    [Sync] Fix minor issues with sync data dump page
    Add copyright headers.  Use textContent instead of innerHTML.
    
    BUG=69500
    TEST=
    
    Review URL: http://codereview.chromium.org/6594136
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76711 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 257f2c3867e73ba7cfc9cc61700b243c86ad68b4

--- chrome/browser/resources/sync_internals/chrome_sync.js
+++ chrome/browser/resources/sync_internals/chrome_sync.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 var chrome = chrome || {};
 // TODO(akalin): Add mocking code for e.g. chrome.send() so that we
 // can test this without rebuilding chrome.
