55108697a468974f7df1689628ed50ae3ef27ed0
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-03-03 20:50:15 +0000

    Signal handler: Add test for crashes that occur inside syscall handlers
    When looking at the signal handler code recently, I noticed that the
    crash-in-syscall case is no longer fully tested because this branch is
    hidden behind the conditions
      NACL_TLS_INDEX_INVALID == current_thread_index
    or
      NULL == g_SignalNAP
    which are not true for the existing crash-in-trusted-code test.
    
    This new test case also doubles up as a reproducer for the issue
    linked below.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=579
    TEST=run_signal_handler_test
    
    Review URL: http://codereview.chromium.org/6613002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4447 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 77029733561ef07649a129fcd3d77cfe5419efd6

---
+++ tests/signal_handler/crash_in_syscall.c
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2011 The Native Client Authors.  All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can
+ * be found in the LICENSE file.
+ */
+
+#include <stdio.h>
+#include <sys/nacl_syscalls.h>
+
+int main() {
+  /*
+   * There are two possible kinds of fault in trusted code:
+   *  1) Faults legitimately triggered by untrusted code.  These are
+   *     safe and do not indicate a bug.
+   *  2) Faults caused by bugs in trusted code.
+   *
+   * Currently the signal handler does not distinguish between the
+   * two.  Ultimately, we do want to distinguish between them in order
+   * to avoid misleading bug reports.
+   * See http://code.google.com/p/nativeclient/issues/detail?id=579
+   *
+   * Below, we trigger a memory access fault in trusted code, inside a
+   * syscall handler.  The imc_recvmsg() implementation does a
+   * memcpy() from the address we give it.  This is an instance of
+   * (1).  If we later extend the system to distinguish (1) and (2),
+   * we will want to add a separate test case for (2).
+   *
+   * We use 0x1000 because we know that is unmapped.  (If this changes
+   * and the call fails to fault, the test runner will catch that.)
+   * We don't use NULL because the syscall checks for NULL.
+   */
+  int rc = imc_recvmsg(0, (struct NaClImcMsgHdr *) 0x1000, 0);
+  if (rc < 0) {
+    perror("imc_recvmsg");
+  }
+  printf("We do not expect to reach here.\n");
+  return 1;
+}
