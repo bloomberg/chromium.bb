4413378d4ad7d460c26cb7ddadc2bcf3d900fcf8
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-04 01:22:45 +0000

    Use SetLibcrosTimeHistogramFunction to add cros timing histograms.
    BUG=chromium-os:12322
    TEST=Build Chrome. Go to about:histograms. There should be a number of entries starting with "Cros." with timings from libcros.
    
    Review URL: http://codereview.chromium.org/6544024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76856 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 24fc860b86a539f884b65ebe7e7be5133d23ce20

--- chrome/browser/chromeos/cros/cros_library_loader.cc
+++ chrome/browser/chromeos/cros/cros_library_loader.cc
@@ -7,6 +7,7 @@
 #include <dlfcn.h>
 
 #include "base/file_path.h"
+#include "base/metrics/histogram.h"
 #include "base/logging.h"
 #include "base/path_service.h"
 #include "chrome/common/chrome_paths.h"
@@ -14,11 +15,35 @@
 
 namespace chromeos {
 
+namespace {
+
+void addLibcrosTimeHistogram(const char* name, const base::TimeDelta& delta) {
+  static const base::TimeDelta min_time = base::TimeDelta::FromMilliseconds(1);
+  static const base::TimeDelta max_time = base::TimeDelta::FromSeconds(1);
+  const size_t bucket_count(10);
+  DCHECK(name);
+  scoped_refptr<base::Histogram> counter = base::Histogram::FactoryTimeGet(
+      std::string(name),
+      min_time,
+      max_time,
+      bucket_count,
+      base::Histogram::kNoFlags);
+  if (counter.get()) {
+    counter->AddTime(delta);
+    VLOG(1) << "Cros Time: " << name << ": " << delta.InMilliseconds() << "ms.";
+  }
+}
+
+}  // namespace
+
 bool CrosLibraryLoader::Load(std::string* load_error_string) {
   bool loaded = false;
   FilePath path;
-  if (PathService::Get(chrome::FILE_CHROMEOS_API, &path))
+  if (PathService::Get(chrome::FILE_CHROMEOS_API, &path)) {
     loaded = LoadLibcros(path.value().c_str(), *load_error_string);
+    if (loaded)
+      SetLibcrosTimeHistogramFunction(addLibcrosTimeHistogram);
+  }
 
   if (!loaded) {
     LOG(ERROR) << "Problem loading chromeos shared object: "
@@ -28,4 +53,3 @@ bool CrosLibraryLoader::Load(std::string* load_error_string) {
 }
 
 }   // namespace chromeos
-
