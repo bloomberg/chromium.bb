a085776d8d177b81c6215474801c5ee5ce9adcda
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-04 20:04:58 +0000

    [Sync] Add 'Events' tab to chrome://sync-internals
    The new 'Events' tab displays sync events as they occur.
    
    Tidied up sync_log.js and data.html.
    
    BUG=69500
    TEST=
    
    Review URL: http://codereview.chromium.org/6612034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@76955 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1a679f4a1a1cfbbd2877290da161c415d79a914a

--- chrome/browser/resources/sync_internals/sync_log.js
+++ chrome/browser/resources/sync_internals/sync_log.js
@@ -2,115 +2,149 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var chrome = chrome || {};
-chrome.sync = chrome.sync || {};
-(function () {
+// require: cr.js
+// require: cr/event_target.js
 
-function Log() {
-  this.entries = [];
+/**
+ * @fileoverview This creates a log object which listens to and
+ * records all sync events.
+ */
 
-  var self = this;
+cr.define('chrome.sync', function() {
+  /**
+   * Creates a new log object which then immediately starts recording
+   * sync events.  Recorded entries are available in the 'entries'
+   * property and there is an 'append' event which can be listened to.
+   * @constructor
+   * @extends {cr.EventTarget}
+   */
+  var Log = function() {
+    var self = this;
 
-  // Service
+    // Service
 
-  chrome.sync.onSyncServiceStateChanged.addListener(function () {
-    self.log_('service', 'onSyncServiceStateChanged', {});
-  });
+    chrome.sync.onSyncServiceStateChanged.addListener(function () {
+      self.log_('service', 'onSyncServiceStateChanged', {});
+    });
 
-  // Notifier
+    // Notifier
 
-  chrome.sync.onSyncNotificationStateChange.addListener(
-    function (notificationsEnabled) {
-      self.log_('notifier', 'onSyncNotificationStateChange', {
-                  notificationsEnabled: notificationsEnabled
+    chrome.sync.onSyncNotificationStateChange.addListener(
+      function (notificationsEnabled) {
+        self.log_('notifier', 'onSyncNotificationStateChange', {
+                    notificationsEnabled: notificationsEnabled
+                  });
+      });
+
+    chrome.sync.onSyncIncomingNotification.addListener(function (changedTypes) {
+      self.log_('notifier', 'onSyncIncomingNotification', {
+                  changedTypes: changedTypes
                 });
     });
 
-  chrome.sync.onSyncIncomingNotification.addListener(function (changedTypes) {
-    self.log_('notifier', 'onSyncIncomingNotification', {
-                changedTypes: changedTypes
-              });
-  });
+    // Manager
 
-  // Manager
+    chrome.sync.onChangesApplied.addListener(function (modelType, changes) {
+      self.log_('manager', 'onChangesApplied', {
+                  modelType: modelType,
+                  changes: changes
+                });
+    });
 
-  chrome.sync.onChangesApplied.addListener(function (modelType, changes) {
-    self.log_('manager', 'onChangesApplied', {
-                modelType: modelType,
-                changes: changes
-              });
-  });
+    chrome.sync.onChangesComplete.addListener(function (modelType) {
+      self.log_('manager', 'onChangesComplete', {
+                  modelType: modelType
+                });
+    });
 
-  chrome.sync.onChangesComplete.addListener(function (modelType) {
-    self.log_('manager', 'onChangesComplete', {
-                modelType: modelType
-              });
-  });
+    chrome.sync.onSyncCycleCompleted.addListener(function (snapshot) {
+      self.log_('manager', 'onSyncCycleCompleted', {
+                  snapshot: snapshot
+                });
+    });
 
-  chrome.sync.onSyncCycleCompleted.addListener(function (snapshot) {
-    self.log_('manager', 'onSyncCycleCompleted', {
-                snapshot: snapshot
-              });
-  });
+    chrome.sync.onAuthError.addListener(function (authError) {
+      self.log_('manager', 'onAuthError', {
+                  authError: authError
+                });
+    });
 
-  chrome.sync.onAuthError.addListener(function (authError) {
-    self.log_('manager', 'onAuthError', {
-                authError: authError
-              });
-  });
+    chrome.sync.onUpdatedToken.addListener(function (token) {
+      self.log_('manager', 'onUpdatedToken', {
+                  token: token
+                });
+    });
 
-  chrome.sync.onUpdatedToken.addListener(function (token) {
-    self.log_('manager', 'onUpdatedToken', {
-                token: token
-              });
-  });
+    chrome.sync.onPassphraseRequired.addListener(function (forDecryption) {
+      self.log_('manager', 'onPassphraseRequired', {
+                  forDecryption: forDecryption
+                });
+    });
 
-  chrome.sync.onPassphraseRequired.addListener(function (forDecryption) {
-    self.log_('manager', 'onPassphraseRequired', {
-                forDecryption: forDecryption
-              });
-  });
+    chrome.sync.onPassphraseAccepted.addListener(function (bootstrapToken) {
+      self.log_('manager', 'onPassphraseAccepted', {
+                  bootstrapToken: bootstrapToken
+                });
+    });
 
-  chrome.sync.onPassphraseAccepted.addListener(function (bootstrapToken) {
-    self.log_('manager', 'onPassphraseAccepted', {
-                bootstrapToken: bootstrapToken
-              });
-  });
+    chrome.sync.onInitializationComplete.addListener(function () {
+      self.log_('manager', 'onInitializationComplete', {});
+    });
 
-  chrome.sync.onInitializationComplete.addListener(function () {
-    self.log_('manager', 'onInitializationComplete', {});
-  });
+    chrome.sync.onPaused.addListener(function () {
+      self.log_('manager', 'onPaused', {});
+    });
 
-  chrome.sync.onPaused.addListener(function () {
-    self.log_('manager', 'onPaused', {});
-  });
+    chrome.sync.onResumed.addListener(function () {
+      self.log_('manager', 'onResumed', {});
+    });
 
-  chrome.sync.onResumed.addListener(function () {
-    self.log_('manager', 'onResumed', {});
-  });
+    chrome.sync.onStopSyncingPermanently.addListener(function () {
+      self.log_('manager', 'onStopSyncingPermanently', {});
+    });
 
-  chrome.sync.onStopSyncingPermanently.addListener(function () {
-    self.log_('manager', 'onStopSyncingPermanently', {});
-  });
+    chrome.sync.onClearServerDataSucceeded.addListener(function () {
+      self.log_('manager', 'onClearServerDataSucceeded', {});
+    });
 
-  chrome.sync.onClearServerDataSucceeded.addListener(function () {
-    self.log_('manager', 'onClearServerDataSucceeded', {});
-  });
-
-  chrome.sync.onClearServerDataFailed.addListener(function () {
-    self.log_('manager', 'onClearServerDataFailed', {});
-  });
-}
-
-Log.prototype.log_ = function(submodule, event, details) {
-  var entry = {
-    submodule: submodule,
-    event: event,
-    date: new Date(),
-    details: details
+    chrome.sync.onClearServerDataFailed.addListener(function () {
+      self.log_('manager', 'onClearServerDataFailed', {});
+    });
   };
-  this.entries.push(entry);
-}
 
-chrome.sync.log = new Log();
-})();
+  Log.prototype = {
+    __proto__: cr.EventTarget.prototype,
+
+    /**
+     * The recorded log entries.
+     * @type {array}
+     */
+    entries: [],
+
+    /**
+     * Records a single event with the given parameters and fires the
+     * 'append' event with the newly-created event as the 'detail'
+     * field of a custom event.
+     * @param {string} submodule The sync submodule for the event.
+     * @param {string} event The name of the event.
+     * @param {dictionary} details A dictionary of event-specific details.
+     */
+    log_: function(submodule, event, details) {
+      var entry = {
+        submodule: submodule,
+        event: event,
+        date: new Date(),
+        details: details
+      };
+      this.entries.push(entry);
+      // Fire append event.
+      var e = cr.doc.createEvent('CustomEvent');
+      e.initCustomEvent('append', false, false, entry);
+      this.dispatchEvent(e);
+    }
+  };
+
+  return {
+    log: new Log()
+  };
+});
