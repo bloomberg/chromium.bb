e6f580d1f0477d898ea7c0d788a0835d19ee6309
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-07 20:42:38 +0000

    Disable tap to click by default
    BUG=chromium-os:12789
    TEST=Confirm it's disabled for new users.
    
    Review URL: http://codereview.chromium.org/6624067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77175 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8b80d0dccdc8c4a055186cd274603f6f5b5af171

--- chrome/browser/chromeos/preferences.cc
+++ chrome/browser/chromeos/preferences.cc
@@ -32,7 +32,7 @@ Preferences::~Preferences() {}
 
 // static
 void Preferences::RegisterUserPrefs(PrefService* prefs) {
-  prefs->RegisterBooleanPref(prefs::kTapToClickEnabled, true);
+  prefs->RegisterBooleanPref(prefs::kTapToClickEnabled, false);
   prefs->RegisterBooleanPref(prefs::kLabsMediaplayerEnabled, false);
   prefs->RegisterBooleanPref(prefs::kLabsAdvancedFilesystemEnabled, false);
   // Check if the accessibility pref is already registered, which can happen
