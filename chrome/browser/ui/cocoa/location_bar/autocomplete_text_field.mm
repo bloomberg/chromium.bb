bcefc862e1ea58e2f30d18d3e188b6141e0bf077
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-07 21:07:19 +0000

    [Mac] Findbar doesn't draw entire surface, override -isOpaque.
    Also indicate that this code draws the focus ring.
    
    BUG=74955, 74052, 24745
    TEST=typing in findbar doesn't show bleed-through from content.  sad-tab page works well for testing this.
    
    Review URL: http://codereview.chromium.org/6625026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77179 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3900683cfc013217805ff828d8276056b10a5251

--- chrome/browser/ui/cocoa/location_bar/autocomplete_text_field.mm
+++ chrome/browser/ui/cocoa/location_bar/autocomplete_text_field.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -175,11 +175,6 @@
     observer_->OnFrameChanged();
   }
   [self updateCursorAndToolTipRects];
-}
-
-// Due to theming, parts of the field are transparent.
-- (BOOL)isOpaque {
-  return NO;
 }
 
 - (void)setAttributedStringValue:(NSAttributedString*)aString {
