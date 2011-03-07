0bf5285b44b0a8630a3b6a308e96fe7fd01572b0
Benjamin Franzke <benjaminfranzke@googlemail.com>
2011-03-07 15:05:27 +0100

    Add buffer.damage request to the protocol
    Thus remove the server-side used vfunc buffer::damage.

--- wayland/wayland-server.h
+++ wayland/wayland-server.h
@@ -125,9 +125,6 @@ struct wl_buffer {
 	struct wl_visual *visual;
 	int32_t width, height;
 	void (*attach)(struct wl_buffer *buffer, struct wl_surface *surface);
-	void (*damage)(struct wl_buffer *buffer,
-		       struct wl_surface *surface,
-		       int32_t x, int32_t y, int32_t width, int32_t height);
 };
 
 struct wl_listener {
