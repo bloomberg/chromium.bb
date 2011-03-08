1435e6b1e8d6e5ce5f1349f18ad497e045111d5b
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-08 22:53:46 +0000

    Fix handling of invalid certificates in JingleSessionManager.
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/6649005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77359 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2b644406bd6b66a0e7c05a64c181158704bb208a

--- remoting/protocol/jingle_session_manager.cc
+++ remoting/protocol/jingle_session_manager.cc
@@ -444,7 +444,10 @@ bool JingleSessionManager::ParseContent(
       std::string base64_cert = child->BodyText();
       std::string der_cert;
       bool ret = base::Base64Decode(base64_cert, &der_cert);
-      DCHECK(ret) << "Failed to decode certificate";
+      if (!ret) {
+        LOG(ERROR) << "Failed to decode certificate received from the peer.";
+        return false;
+      }
       certificate = net::X509Certificate::CreateFromBytes(der_cert.data(),
                                                           der_cert.length());
     }
