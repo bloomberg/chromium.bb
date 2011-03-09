bc1a269eec996d778ea37996692410cb23c8052b
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-09 00:49:20 +0000

    This change enables ExtensionApiTest.WebSocket on Mac again.
    BUG=56596
    TEST=none
    Review URL: http://codereview.chromium.org/6611007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77387 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 955b73964372469fe35b0162d37f4a357532d6f4

--- chrome/browser/extensions/extension_websocket_apitest.cc
+++ chrome/browser/extensions/extension_websocket_apitest.cc
@@ -8,14 +8,7 @@
 #include "chrome/test/ui_test_utils.h"
 #include "net/base/mock_host_resolver.h"
 
-#if defined(OS_MACOSX)
-// WebSocket test started timing out - suspect webkit roll from 67965->68051.
-// http://crbug.com/56596
-#define MAYBE_WebSocket DISABLED_WebSocket
-#else
-#define MAYBE_WebSocket WebSocket
-#endif
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_WebSocket) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WebSocket) {
   FilePath websocket_root_dir;
   PathService::Get(chrome::DIR_TEST_DATA, &websocket_root_dir);
   websocket_root_dir = websocket_root_dir.AppendASCII("layout_tests")
