40b59134e0d490018dd22a7a5280ae3f06dff73d
pasko@google.com <pasko@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-03-10 14:17:15 +0000

    Change GMP header check for version declarations
    Newer versions of GMP declare the version in architecture-specific headers.
    Review URL: http://codereview.chromium.org/6661020
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4494 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 95f15a4582bd8e5b1b949e0c47226bb8ed31abbf

--- tools/use_gcc_extras.sh
+++ tools/use_gcc_extras.sh
@@ -14,19 +14,27 @@ if [ ! -n "${GMP_VERSION}" ] || [ ! -n "${MPFR_VERSION}" ]; then
 fi
 
 GNU_MP_MAJOR=0
-if [ -f /usr/include/gmp.h ] ; then
-  GNU_MP_MAJOR="`grep '#[ 	]*define[ 	]*__GNU_MP_VERSION ' /usr/include/gmp.h | sed -e s'@[ 	]*#define[ 	]*__GNU_MP_VERSION[ 	]*@@'`" ;
-  GNU_MP_MINOR="`grep '#[ 	]*define[ 	]*__GNU_MP_VERSION_MINOR ' /usr/include/gmp.h | sed -e s'@[ 	]*#[ 	]*define[ 	]*__GNU_MP_VERSION_MINOR[ 	]*@@'`" ;
-  GNU_MP_PATCHLEVEL="`grep '#[ 	]*define[ 	]*__GNU_MP_VERSION_PATCHLEVEL ' /usr/include/gmp.h | sed -e s'@[ 	]*[ 	]*#define[ 	]*__GNU_MP_VERSION_PATCHLEVEL[ 	]*@@'`" ;
+GMP_HEADER=""
+for header in gmp-i386.h gmp-x86_64.h gmp.h ; do
+  if [[ -f /usr/include/${header} ]]; then
+    declare -r GMP_HEADER=/usr/include/${header}
+    break
+  fi
+done
+
+if [[ ! -z ${GMP_HEADER} ]] ; then
+  GNU_MP_MAJOR="`grep '#[ 	]*define[ 	]*__GNU_MP_VERSION ' ${GMP_HEADER} | sed -e s'@[ 	]*#define[ 	]*__GNU_MP_VERSION[ 	]*@@'`" ;
+  GNU_MP_MINOR="`grep '#[ 	]*define[ 	]*__GNU_MP_VERSION_MINOR ' ${GMP_HEADER} | sed -e s'@[ 	]*#[ 	]*define[ 	]*__GNU_MP_VERSION_MINOR[ 	]*@@'`" ;
+  GNU_MP_PATCHLEVEL="`grep '#[ 	]*define[ 	]*__GNU_MP_VERSION_PATCHLEVEL ' ${GMP_HEADER} | sed -e s'@[ 	]*[ 	]*#define[ 	]*__GNU_MP_VERSION_PATCHLEVEL[ 	]*@@'`" ;
 fi
 
 # If GMP Version < 4.3.1, use GMP from our sources.
-if [ "$GNU_MP_MAJOR" -lt 4 ] ||
-   ( [ "$GNU_MP_MAJOR" -eq 4 ] &&
-     [ "$GNU_MP_MINOR" -lt 3 ] ) ||
-   ( [ "$GNU_MP_MAJOR" -eq 4 ] &&
-     [ "$GNU_MP_MINOR" -eq 3 ] &&
-     [ "$GNU_MP_PATCHLEVEL" -lt 1 ] ) ; then
+if [[ "$GNU_MP_MAJOR" -lt 4 ]] ||
+   ( [[ "$GNU_MP_MAJOR" -eq 4 ]] &&
+     [[ "$GNU_MP_MINOR" -lt 3 ]] ) ||
+   ( [[ "$GNU_MP_MAJOR" -eq 4 ]] &&
+     [[ "$GNU_MP_MINOR" -eq 3 ]] &&
+     [[ "$GNU_MP_PATCHLEVEL" -lt 1 ]] ) ; then
   echo "symlinking GMP"
   ln -sf gmp-${GMP_VERSION} SRC/gcc/gmp
 else
@@ -34,21 +42,21 @@ else
   rm -f SRC/gcc/gmp
 fi
 
-MPFR_VERSION_MAJOR=0 ; \
-if [ -f /usr/include/mpfr.h ] ; then
+MPFR_VERSION_MAJOR=0
+if [[ -f /usr/include/mpfr.h ]] ; then
   MPFR_VERSION_MAJOR="`grep '#[ 	]*define[ 	]*MPFR_VERSION_MAJOR ' /usr/include/mpfr.h | sed -e s'@[ 	]*#[ 	]*define[ 	]*MPFR_VERSION_MAJOR[ 	]*@@'`" ;
   MPFR_VERSION_MINOR="`grep '#[ 	]*define[ 	]*MPFR_VERSION_MINOR ' /usr/include/mpfr.h | sed -e s'@[ 	]*#[ 	]*define[ 	]*MPFR_VERSION_MINOR[ 	]*@@'`" ;
   MPFR_VERSION_PATCHLEVEL="`grep '#[ 	]*define[ 	]*MPFR_VERSION_PATCHLEVEL ' /usr/include/mpfr.h | sed -e s'@[ 	]*#[ 	]*define[ 	]*MPFR_VERSION_PATCHLEVEL[ 	]*@@'`" ;
 fi
 
 # If MPFR Version < 2.4.1, use GMP from our sources.
-if [ ! -e /usr/include/mpfr.h ] ||
-   [ "$MPFR_VERSION_MAJOR" -lt 2 ] ||
-   ( [ "$MPFR_VERSION_MAJOR" -eq 2 ] &&
-     [ "$MPFR_VERSION_MINOR" -lt 4 ] ) ||
-   ( [ "$MPFR_VERSION_MAJOR" -eq 2 ] &&
-     [ "$MPFR_VERSION_MINOR" -eq 4 ] &&
-     [ "$MPFR_VERSION_PATCHLEVEL" -lt 1 ] ) ; then
+if [[ ! -e /usr/include/mpfr.h ]] ||
+   [[ "$MPFR_VERSION_MAJOR" -lt 2 ]] ||
+   ( [[ "$MPFR_VERSION_MAJOR" -eq 2 ]] &&
+     [[ "$MPFR_VERSION_MINOR" -lt 4 ]] ) ||
+   ( [[ "$MPFR_VERSION_MAJOR" -eq 2 ]] &&
+     [[ "$MPFR_VERSION_MINOR" -eq 4 ]] &&
+     [[ "$MPFR_VERSION_PATCHLEVEL" -lt 1 ]] ) ; then
   echo "symlinking MPFR"
   ln -sf mpfr-${MPFR_VERSION} SRC/gcc/mpfr
 else
