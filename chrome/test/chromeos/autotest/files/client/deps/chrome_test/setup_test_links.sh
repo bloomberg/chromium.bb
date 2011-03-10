b4d724df3381cf4188f0e767cec55936ea6b405a
cmasone@chromium.org <cmasone@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-10 22:10:10 +0000

    [ChromeOS] symlink to resources.pak in our Chrome Autotest setup.
    Many of the browser tests will not run without this symlink.
    
    BUG=chromium-os:8544
    TEST=Run desktopui_BrowserTest.control and see that tests no longer error out complaining they cant find resources.pak
    
    Review URL: http://codereview.chromium.org/6670007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77709 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 70e40e03cea8c01acf57d497f80817ae6b11eac1

--- chrome/test/chromeos/autotest/files/client/deps/chrome_test/setup_test_links.sh
+++ chrome/test/chromeos/autotest/files/client/deps/chrome_test/setup_test_links.sh
@@ -10,6 +10,7 @@ ln -f -s /opt/google/chrome/chrome $(dirname $0)/chrome
 ln -f -s /opt/google/chrome/chrome.pak $(dirname $0)/chrome.pak
 ln -f -s /opt/google/chrome/locales $(dirname $0)/locales
 ln -f -s /opt/google/chrome/resources $(dirname $0)/resources
+ln -f -s /opt/google/chrome/resources.pak $(dirname $0)/resources.pak
 
 mkdir -p $(dirname $0)/chromeos
 ln -f -s /opt/google/chrome/chromeos/libcros.so \
