aa39c0751a96f4f64da6e3afe27bda824d8b6d20
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-10 22:16:38 +0000

    Patch for http://codereview.chromium.org/6628023/
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@77711 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c7a4e0a7b7eed6932a3e27ab15f9d591eb29d150

--- chrome/browser/automation/testing_automation_provider_chromeos.cc
+++ chrome/browser/automation/testing_automation_provider_chromeos.cc
@@ -172,12 +172,21 @@ void TestingAutomationProvider::ConnectToWifiNetwork(
   }
 
   NetworkLibrary* network_library = CrosLibrary::Get()->GetNetworkLibrary();
-  if (!network_library->ConnectToWifiNetwork(
-      service_path, password, identity, certpath)) {
+  WifiNetwork* wifi = network_library->FindWifiNetworkByPath(service_path);
+  if (!wifi) {
     reply.SendError("Failed to connect");
     return;
   }
+  if (!password.empty())
+    wifi->SetPassphrase(password);
+  if (!identity.empty())
+    wifi->SetIdentity(identity);
+  if (!certpath.empty())
+    wifi->SetCertPath(certpath);
+  network_library->ConnectToWifiNetwork(service_path);
+
+  // TODO(stevenjb): Observe the network library and check for a successful
+  // connection.
 
   reply.SendSuccess(NULL);
 }
-
