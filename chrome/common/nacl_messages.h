7a2a467ecfb8d230886960f35c96ad336b7664e4
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-14 19:23:43 +0000

    Convert devtools messages, nacl messages to new message format.
    Review URL: http://codereview.chromium.org/6686054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78070 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5808f7d72453c05c7e2722c6f30d1326c8759b4f

--- chrome/common/nacl_messages.h
+++ chrome/common/nacl_messages.h
@@ -4,10 +4,35 @@
 
 // Defines messages between the browser and NaCl process.
 
+// Multiply-included message file, no traditional include guard.
+#include "base/process.h"
+#include "chrome/common/nacl_types.h"
+#include "ipc/ipc_message_macros.h"
+
 #ifndef CHROME_COMMON_NACL_MESSAGES_H_
 #define CHROME_COMMON_NACL_MESSAGES_H_
-#pragma once
-
-#include "chrome/common/nacl_messages_internal.h"
 
 #endif  // CHROME_COMMON_NACL_MESSAGES_H_
+
+#define IPC_MESSAGE_START NaClMsgStart
+
+//-----------------------------------------------------------------------------
+// NaClProcess messages
+// These are messages sent from the browser to the NaCl process.
+// Tells the NaCl process to start.
+IPC_MESSAGE_CONTROL1(NaClProcessMsg_Start,
+                     std::vector<nacl::FileDescriptor> /* sockets */)
+
+// Tells the NaCl broker to launch a NaCl loader process.
+IPC_MESSAGE_CONTROL1(NaClProcessMsg_LaunchLoaderThroughBroker,
+                     std::wstring /* channel ID for the loader */)
+
+// Notify the browser process that the loader was launched successfully.
+IPC_MESSAGE_CONTROL2(NaClProcessMsg_LoaderLaunched,
+                     std::wstring,  /* channel ID for the loader */
+                     base::ProcessHandle /* loader process handle */)
+
+// Notify the broker that all loader processes have been terminated and it
+// should shutdown.
+IPC_MESSAGE_CONTROL0(NaClProcessMsg_StopBroker)
+
