009c82bad86ccfd49bd0fb0a127fbe3a27d91e3a
Christian Egli <christian.egli@alumni.ethz.ch>
2011-03-14 09:48:53 +0000

    Remove some test code that is no longer needed now that we have the multi table path feature.

--- tests/check_endless_loop.pl
+++ tests/check_endless_loop.pl
@@ -11,11 +11,6 @@ $|++;
 # are permitted in any medium without royalty provided the copyright
 # notice and this notice are preserved.
 
-# look for the table that causes the endless loop in the tables dir
-# which is local to the tests dir. The Makefile passes this path via
-# an env var named LOUIS_TEST_TABLEPATH.
-$ENV{LOUIS_TABLEPATH} = $ENV{LOUIS_TEST_TABLEPATH};
-
 my $table="loop.ctb";
 
 $SIG{ALRM} = sub { die "lou_checktable on $table stuck in endless loop\n" };
