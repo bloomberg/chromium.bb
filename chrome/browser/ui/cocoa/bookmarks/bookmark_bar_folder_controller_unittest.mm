0db02fb99469fbbc3573329cea54b3bfb818afb0
maf@chromium.org <maf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-16 22:11:06 +0000

    Disable flaky test. Review URL: http://codereview.chromium.org/6676053
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78445 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 555ea0d44b04853050b418d2da526218bbd25deb

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_controller_unittest.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_controller_unittest.mm
@@ -449,7 +449,7 @@ TEST_F(BookmarkBarFolderControllerTest, ChildFolderWidth) {
 
 // Simple scrolling tests.
 // Currently flaky due to a changed definition of the correct menu boundaries.
-TEST_F(BookmarkBarFolderControllerTest, SimpleScroll) {
+TEST_F(BookmarkBarFolderControllerTest, DISABLED_SimpleScroll) {
   scoped_nsobject<BookmarkBarFolderController> bbfc;
   NSRect screenFrame = [[NSScreen mainScreen] visibleFrame];
   CGFloat screenHeight = NSHeight(screenFrame);
