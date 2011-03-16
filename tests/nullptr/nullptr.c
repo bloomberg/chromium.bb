5f2db563861410da33fc139a62ce97e05f4a241d
jvoung@google.com <jvoung@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-03-16 16:27:32 +0000

    Make nullptr test harder to optimized, otherwise the nullptr access gets optimized out by llvm...
    Try online: http://llvm.org/demo/index.cgi
    Description of behavior: http://llvm.org/bugs/show_bug.cgi?id=3828#c6
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1225
    TEST=remove SKIP_OPT from pnacl driver and run_nullptr_test with test-all
    
    Review URL: http://codereview.chromium.org/6670037
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4545 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 7e1776f2798165fdfdafdc3776ff3e4ddd152f74

--- tests/nullptr/nullptr.c
+++ tests/nullptr/nullptr.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/* NOTE: this is a global to keep a smart compiler (llvm)
- *       from partially evaluating it
+/* NOTE: this is a volatile global to keep a smart compiler (llvm)
+ *       from treating this program as having undefined behavior.
+ *       Try online: http://llvm.org/demo/index.cgi
+ *       Description of behavior: http://llvm.org/bugs/show_bug.cgi?id=3828#c6
  */
-
-unsigned char *byteptr = 0;
+volatile unsigned char *byteptr = 0;
 
 int main(void) {
   unsigned char byte = *byteptr;
