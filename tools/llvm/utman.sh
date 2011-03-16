7ab03bd0f5be99404390793f013076d749236216
kschimpf@google.com <kschimpf@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-03-16 22:32:30 +0000

    Update REV number so that change to gold to handle .so files works.
    Review URL: http://codereview.chromium.org/6673099
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4554 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: b3753bd1066e5c962802b94a380b98a7d7ebb1dd

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -134,7 +134,7 @@ readonly PNACL_AS_X8664="${INSTALL_BIN}/pnacl-x86_64-as"
 readonly LLVM_REV=8b90cf891e03
 readonly LLVM_GCC_REV=80ce86616af9
 readonly NEWLIB_REV=eb9c4bb9ccd7
-readonly BINUTILS_REV=5d388c57ed1b
+readonly BINUTILS_REV=499ffe65010c
 
 # Repositories
 readonly REPO_LLVM_GCC="llvm-gcc.nacl-llvm-branches"
