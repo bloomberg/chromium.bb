787270d87c99807e48c6acde5f5263016a457408
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-18 20:53:11 +0000

    Don't Paint when hidden.
    BUG=none
    TEST=brower_tests, ui_tests, interactive_tests
    TBR=oshima
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78750 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 84f56caf2f1c429693d66bdb88f1713051a2d3b6

--- chrome/browser/renderer_host/render_widget_host_view_gtk.cc
+++ chrome/browser/renderer_host/render_widget_host_view_gtk.cc
@@ -161,6 +161,8 @@ class RenderWidgetHostViewGtkWidget {
   static gboolean OnExposeEvent(GtkWidget* widget,
                                 GdkEventExpose* expose,
                                 RenderWidgetHostViewGtk* host_view) {
+    if (host_view->is_hidden_)
+      return FALSE;
     const gfx::Rect damage_rect(expose->area);
     host_view->Paint(damage_rect);
     return FALSE;
