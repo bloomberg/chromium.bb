083dc91e36f7872df1e999bc82a4d70b0b1b8269
pasko@google.com <pasko@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-03-18 18:10:43 +0000

    Add a dumb buildbot scripts for makefile toolchain builds
    ... for windows, mac and linux
    Review URL: http://codereview.chromium.org/6682008
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4569 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 844039cc22f1faa2b0f8c38fe7d96cfbe2c47b3d

---
+++ buildbot/buildbot_lucid64-glibc-makefile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Copyright 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can
+# be found in the LICENSE file.
+
+# TODO(pasko): convert this dummy script to do something useful once it is
+# invoked on the lucid64-glibc-makefile buildbot.
+echo @@@BUILD_STEP compile_toolchain@@@
+echo Log message
