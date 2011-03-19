498e51cd83915b19983022cc9eabfa9699170081
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-19 23:36:39 +0000

    sync: fix umr from earlier patch TBR=zea@chromium.org
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/6685111
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78840 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7f04cbd40a899a06c7db8c983e29d3da1041c5b6

--- chrome/browser/sync/glue/sync_backend_host.cc
+++ chrome/browser/sync/glue/sync_backend_host.cc
@@ -648,7 +648,9 @@ SyncBackendHost::Core::Core(SyncBackendHost* backend)
     : host_(backend),
       syncapi_(new sync_api::SyncManager()),
       sync_manager_observer_(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
-      parent_router_(NULL) {
+      parent_router_(NULL),
+      processing_passphrase_(false),
+      deferred_nudge_for_cleanup_requested_(false) {
 }
 
 // Helper to construct a user agent string (ASCII) suitable for use by
