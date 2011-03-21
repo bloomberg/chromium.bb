5d6cefcd53e7e86479a235df73d6e51534508bce
noelutz@google.com <noelutz@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-21 21:51:50 +0000

    Integrate the csd-whitelist with the rest of the client-side phishing detection code.
    BUG=
    TEST=ClientSideDetectionHostTest
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=78635
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=78818
    
    Review URL: http://codereview.chromium.org/6670053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78929 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1fd6dccfc0bd2383005181c52635ff5b90dbae78

--- chrome/browser/safe_browsing/safe_browsing_service.h
+++ chrome/browser/safe_browsing/safe_browsing_service.h
@@ -165,7 +165,7 @@ class SafeBrowsingService
   // match and false otherwise.  To make sure we are conservative we will return
   // true if an error occurs. This method is expected to be called on the IO
   // thread.
-  bool MatchCsdWhitelistUrl(const GURL& url);
+  virtual bool MatchCsdWhitelistUrl(const GURL& url);
 
   // Called on the IO thread to cancel a pending check if the result is no
   // longer needed.
