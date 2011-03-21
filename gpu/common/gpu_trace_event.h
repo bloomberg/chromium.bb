40eba8a9075aa71b099b21dc7d9eafa17ed14d8e
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-21 22:24:51 +0000

    Fix bug in GPU_TRACE_EVENT_END.
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/6716008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@78937 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 619a7f5773bcbd3075d88a72bf710ca50e16e64a

--- gpu/common/gpu_trace_event.h
+++ gpu/common/gpu_trace_event.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -149,8 +149,8 @@
         __FILE__, __LINE__, \
         GPU_TRACE_EVENT_UNIQUE_IDENTIFIER(catstatic), \
         name, \
-        arg1name, arg1val, \
-        arg2name, arg2val); \
+        NULL, NULL, \
+        NULL, NULL); \
   }
 
 
