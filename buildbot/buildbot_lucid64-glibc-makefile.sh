d31300451aa848b90ddb29ea7fdc8251ca7d6c42
pasko@google.com <pasko@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-03-22 13:51:16 +0000

    Uploading toolchain on lucid64 buildbot. Review URL: http://codereview.chromium.org/6715027
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4592 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 4099bf729d6bbde1a532021c6806e87029c16b24

--- buildbot/buildbot_lucid64-glibc-makefile.sh
+++ buildbot/buildbot_lucid64-glibc-makefile.sh
@@ -16,46 +16,52 @@ set -u
 
 echo @@@BUILD_STEP gclient_runhooks@@@
 gclient runhooks --force
-echo BUILDBOT_GOT_REVISION ${BUILDBOT_GOT_REVISION}
-echo BUILDBOT_REVISION ${BUILDBOT_REVISION}
 
 echo @@@BUILD_STEP clobber@@@
-rm -rf scons-out tools/SRC tools/BUILD tools/sdk tools/out \
-  tools/naclsdk.tgz toolchain || echo already_clean
+rm -rf scons-out tools/SRC tools/BUILD tools/out tools/toolchain \
+  tools/toolchain.tgz toolchain .tmp || echo already_clean
 
 echo @@@BUILD_STEP compile_toolchain@@@
-cd tools
-make -j12 buildbot-build-with-glibc TOOLCHAINLOC=sdk
+(
+  cd tools
+  make -j8 buildbot-build-with-glibc TOOLCHAINLOC=toolchain SDKNAME=linux_x86
+)
 
 echo @@@BUILD_STEP tar_toolchain@@@
-tar cvfz naclsdk.tgz sdk/ && chmod a+r naclsdk.tgz
+(
+  cd tools
+  tar cSvfz toolchain.tgz toolchain/ && chmod a+r toolchain.tgz
+)
 
 echo @@@BUILD_STEP untar_toolchain@@@
-mkdir -p ../toolchain/linux_x86/.tmp && \
-  cd ../toolchain/linux_x86/.tmp && \
-  tar xfz ../../../tools/naclsdk.tgz && \
-  mv sdk/nacl-sdk/* ../
-cd ../../../../
+(
+  mkdir -p .tmp
+  cd .tmp
+  tar zxf ../tools/toolchain.tgz
+  mv toolchain ..
+)
 
 echo @@@BUILD_STEP gyp_compile@@@
-make -k -j12 V=1 BUILDTYPE=Release
+(
+  cd ..
+  make -k -j8 V=1 BUILDTYPE=Release
+)
 
 RETCODE=0
 
 echo @@@BUILD_STEP gyp_tests@@@
-cd native_client
 python trusted_test.py --config Release || \
   (RETCODE=$? && echo @@@BUILD_FAILED@@@)
 
 echo @@@BUILD_STEP small_tests32@@@
-scons -k -j 8 \
+./scons -k -j 8 \
   naclsdk_mode=custom:"${PWD}"/toolchain/linux_x86 \
   --mode=dbg-host,nacl platform=x86-32 \
   --nacl_glibc --verbose small_tests || \
   (RETCODE=$? && echo @@@BUILD_FAILED@@@)
 
 echo @@@BUILD_STEP small_tests64@@@
-scons -k -j 8 \
+./scons -k -j 8 \
   naclsdk_mode=custom:"${PWD}"/toolchain/linux_x86 \
   --mode=dbg-host,nacl platform=x86-64 \
   --nacl_glibc --verbose small_tests || \
@@ -64,6 +70,10 @@ scons -k -j 8 \
 # TODO(pasko): add medium_tests, large_tests, {chrome_}browser_tests.
 
 echo @@@BUILD_STEP archive_build@@@
-# TODO(pasko): upload the toolchain tarball.
+if [[ ${RETCODE} == 0 ]]; then
+  /b/build/scripts/slave/gsutil -h Cache-Control:no-cache cp -a public-read \
+    tools/out/toolchain.tgz \
+    gs://nativeclient-archive2/x86_toolchain/r${BUILDBOT_GOT_REVISION}/toolchain_linux_x86.tar.gz
+fi
 
 exit ${RETCODE}
