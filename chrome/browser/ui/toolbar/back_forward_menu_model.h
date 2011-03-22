a068823181f85d93c3a6db28bd90b176583bc02c
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-22 18:24:54 +0000

    Moves BackForwardMenuModel destructor into .cc to appease clang bot.
    BUG=none
    TEST=none
    TBR=ben@chromium.org
    Review URL: http://codereview.chromium.org/6676092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79004 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 60efea02a191d61807d3d42377a9aa8f20baca5b

--- chrome/browser/ui/toolbar/back_forward_menu_model.h
+++ chrome/browser/ui/toolbar/back_forward_menu_model.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,7 +38,7 @@ class BackForwardMenuModel : public ui::MenuModel {
   };
 
   BackForwardMenuModel(Browser* browser, ModelType model_type);
-  virtual ~BackForwardMenuModel() { }
+  virtual ~BackForwardMenuModel();
 
   // MenuModel implementation.
   virtual bool HasIcons() const;
