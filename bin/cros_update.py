ecb055d04a16a7dd70f7ccda896a13b4d24d64fc
Darin Petkov <petkov@chromium.org>
2011-03-24 15:59:54 -0700

    Initial check in of a tool to make updating Portage package easier.
    Currently the tool just list package dependencies in pre-order (w/ or w/o full
    path) so that we can upgrade packages from leaves up.
    
    BUG=chromium-os:13448
    TEST=ran it with various options
    
    Change-Id: I9acb703b89d79ca86fd4733c0d8ea0ac48debf14
    
    Review URL: http://codereview.chromium.org/6713109

---
+++ bin/cros_update.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python2.6
+# Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Perform various tasks related to updating Portage packages."""
+
+import optparse
+import os
+import parallel_emerge
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+import chromite.lib.cros_build_lib as cros_lib
+
+
+class Updater(object):
+  """A class to perform various tasks related to updating Portage packages."""
+
+  def __init__(self, options, args):
+    self._options = options
+    self._args = args
+
+  @staticmethod
+  def _GetPreOrderDepGraphPackage(deps_graph, package, pkglist, visited):
+    """Collect packages from |deps_graph| into |pkglist| in pre-order."""
+    if package in visited:
+      return
+    visited.add(package)
+    for parent in deps_graph[package]['provides']:
+      Updater._GetPreOrderDepGraphPackage(deps_graph, parent, pkglist, visited)
+    pkglist.append(package)
+
+  @staticmethod
+  def _GetPreOrderDepGraph(deps_graph):
+    """Return packages from |deps_graph| in pre-order."""
+    pkglist = []
+    visited = set()
+    for package in deps_graph:
+      Updater._GetPreOrderDepGraphPackage(deps_graph, package, pkglist, visited)
+    return pkglist
+
+  def _PrintDependencies(self):
+    argv = ['--quiet', '--emptytree']
+    argv.append('--board=%s' % self._options.board)
+    if not self._options.deps:
+      argv.append('--root-deps=rdeps')
+    argv.extend(self._args)
+
+    deps = parallel_emerge.DepGraphGenerator()
+    deps.Initialize(argv)
+    deps_tree, deps_info = deps.GenDependencyTree({})
+    deps_graph = deps.GenDependencyGraph(deps_tree, deps_info, {})
+    for package in Updater._GetPreOrderDepGraph(deps_graph):
+      if self._options.full_path:
+        equery = ['equery-%s' % self._options.board, 'which', package]
+        package = cros_lib.RunCommand(equery, print_cmd=False,
+                                      redirect_stdout=True).output.strip()
+      print package
+
+  def Run(self):
+    """Runs the updater based on the supplied options and arguments.
+
+    Currently just lists all package dependencies in pre-order."""
+    self._PrintDependencies()
+
+
+def main():
+  usage = 'Usage: %prog [options] packages...'
+  parser = optparse.OptionParser(usage=usage)
+  parser.add_option('--board', dest='board', type='string', action='store',
+                    default=None, help="Target board [default: '%default']")
+  parser.add_option('--deps', dest='deps', action='store_true',
+                    default=False,
+                    help="Use build time dependencies rather than runtime")
+  parser.add_option('--no-full-path', dest='full_path', action='store_false',
+                    default=True,
+                    help="Don't print the full path to the package")
+
+  (options, args) = parser.parse_args()
+
+  if not options.board:
+    parser.print_help()
+    cros_lib.Die('board is required')
+
+  if not args:
+    parser.print_help()
+    cros_lib.Die('no packages provided')
+
+  updater = Updater(options, args)
+  updater.Run()
+
+
+if __name__ == '__main__':
+  main()
