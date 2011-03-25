2270d1dab3da4cf773dc96b76946561ce4c316fa
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-25 20:23:38 +0000

    importer: Use unnamed namespace instead of static keyword.
    "C++ standard, 7.3.1.1: The use of the static keyword is deprecated when
     declaring objects in a namespace scope, the unnamed-namespace provides a
     superior alternative."
    
    BUG=None
    TEST=None
    
    R=maruel@chromium.org
    
    Review URL: http://codereview.chromium.org/6749008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79446 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: edc1ba641c9e0ace8b05d306638379d22e5be8f1

--- chrome/browser/importer/firefox2_importer.cc
+++ chrome/browser/importer/firefox2_importer.cc
@@ -31,14 +31,16 @@
 #include "net/base/data_url.h"
 #include "webkit/glue/password_form.h"
 
-static const char kItemOpen[] = "<DT><A";
-static const char kItemClose[] = "</A>";
-static const char kFeedURLAttribute[] = "FEEDURL";
-static const char kHrefAttribute[] = "HREF";
-static const char kIconAttribute[] = "ICON";
-static const char kShortcutURLAttribute[] = "SHORTCUTURL";
-static const char kAddDateAttribute[] = "ADD_DATE";
-static const char kPostDataAttribute[] = "POST_DATA";
+namespace {
+const char kItemOpen[] = "<DT><A";
+const char kItemClose[] = "</A>";
+const char kFeedURLAttribute[] = "FEEDURL";
+const char kHrefAttribute[] = "HREF";
+const char kIconAttribute[] = "ICON";
+const char kShortcutURLAttribute[] = "SHORTCUTURL";
+const char kAddDateAttribute[] = "ADD_DATE";
+const char kPostDataAttribute[] = "POST_DATA";
+}
 
 Firefox2Importer::Firefox2Importer() : parsing_bookmarks_html_file_(false) {}
 
