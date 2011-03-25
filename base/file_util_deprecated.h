a66f28dc197efad9711b9544ede9117c15000a58
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-25 18:46:38 +0000

    Base: A few more files using BASE_API (for base.dll)
    BUG=76996
    TEST=none
    Review URL: http://codereview.chromium.org/6733031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79426 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 90509cbdde392aa218771f6673b9b5a6600c2b57

--- base/file_util_deprecated.h
+++ base/file_util_deprecated.h
@@ -14,6 +14,7 @@
 #define BASE_FILE_UTIL_DEPRECATED_H_
 #pragma once
 
+#include "base/base_api.h"
 #include "build/build_config.h"
 
 // We've successfully deprecated all of these functions on non-Windows
@@ -24,22 +25,24 @@
 namespace file_util {
 
 // Use the FilePath versions instead.
-FILE* OpenFile(const std::string& filename, const char* mode);
-FILE* OpenFile(const std::wstring& filename, const char* mode);
+BASE_API FILE* OpenFile(const std::string& filename, const char* mode);
+BASE_API FILE* OpenFile(const std::wstring& filename, const char* mode);
 
 // Appends new_ending to path, adding a separator between the two if necessary.
-void AppendToPath(std::wstring* path, const std::wstring& new_ending);
+BASE_API void AppendToPath(std::wstring* path, const std::wstring& new_ending);
 
 // Use FilePath::Extension instead.
-FilePath::StringType GetFileExtensionFromPath(const FilePath& path);
-std::wstring GetFileExtensionFromPath(const std::wstring& path);
+BASE_API FilePath::StringType GetFileExtensionFromPath(const FilePath& path);
+BASE_API std::wstring GetFileExtensionFromPath(const std::wstring& path);
 
 // Use version that takes a FilePath.
-bool Delete(const std::wstring& path, bool recursive);
-bool CopyDirectory(const std::wstring& from_path, const std::wstring& to_path,
-                   bool recursive);
-int ReadFile(const std::wstring& filename, char* data, int size);
-int WriteFile(const std::wstring& filename, const char* data, int size);
+BASE_API bool Delete(const std::wstring& path, bool recursive);
+BASE_API bool CopyDirectory(const std::wstring& from_path,
+                            const std::wstring& to_path,
+                            bool recursive);
+BASE_API int ReadFile(const std::wstring& filename, char* data, int size);
+BASE_API int WriteFile(const std::wstring& filename,
+                       const char* data, int size);
 
 }
 
