6ddb11d6a3f265ee8dee13ee72f4766d8e3d0ddd
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-25 22:21:28 +0000

    This fixes some issues with the window switcher icon.
    1) it adds a tooltip ("Switch windows")
    2) it hides the icon when there is only one window, or when we're not logged in
    3) it skips all non-"normal" browser windows in the browser list, so we don't focus panels.
    
    BUG=chromium-os:11866, chromium-os:13103
    TEST=Ran ChromeOS version of chrome and tested various scenarios.
    
    Review URL: http://codereview.chromium.org/6724009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79462 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0db55de32ca87075de20d170f0bf56dc8e5c5f1a

--- chrome/browser/chromeos/status/window_switcher_button.cc
+++ chrome/browser/chromeos/status/window_switcher_button.cc
@@ -5,17 +5,35 @@
 #include "chrome/browser/chromeos/status/window_switcher_button.h"
 
 #include "chrome/browser/chromeos/status/status_area_host.h"
+#include "chrome/browser/chromeos/wm_ipc.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas.h"
 
 namespace chromeos {
 
+namespace {
+int GetNormalBrowserCount() {
+  int count = 0;
+  BrowserList::const_iterator iter;
+  for (iter = BrowserList::begin(); iter != BrowserList::end(); ++iter) {
+    if ((*iter)->type() == Browser::TYPE_NORMAL)
+      count++;
+  }
+  return count;
+}
+}
+
 WindowSwitcherButton::WindowSwitcherButton(StatusAreaHost* host)
     : StatusAreaButton(this),
       host_(host) {
+  ResourceBundle& rb = ResourceBundle::GetSharedInstance();
+  SetIcon(*rb.GetBitmapNamed(IDR_STATUSBAR_WINDOW_SWITCHER));
+  SetEnabled(true);
+
   UpdateStatus();
   BrowserList::AddObserver(this);
 }
@@ -25,38 +43,39 @@ WindowSwitcherButton::~WindowSwitcherButton() {
 }
 
 void WindowSwitcherButton::UpdateStatus() {
-  ResourceBundle& rb = ResourceBundle::GetSharedInstance();
-  // If there's more than one window, we enable ourselves.
-  if (BrowserList::size() > 1) {
-    SetIcon(*rb.GetBitmapNamed(IDR_STATUSBAR_WINDOW_SWITCHER));
-    SetEnabled(true);
-  } else {
-    SetIcon(*rb.GetBitmapNamed(IDR_STATUSBAR_WINDOW_SWITCHER_DISABLED));
-    SetEnabled(false);
+  if (GetNormalBrowserCount() < 2) {
+    // There are less than two browsers.  This means we can't switch
+    // anywhere, so we disappear.
+    SetTooltipText(L"");
+    SetVisible(false);
+    PreferredSizeChanged();
+    return;
   }
+
+  SetTooltipText(UTF16ToWide(l10n_util::GetStringUTF16(
+      IDS_STATUSBAR_WINDOW_SWITCHER_TOOLTIP)));
+
+  // There are at least two browsers, so we show ourselves.
+  SetVisible(true);
+  PreferredSizeChanged();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // WindowSwitcherButton, views::ViewMenuDelegate implementation:
 
 void WindowSwitcherButton::RunMenu(views::View* source, const gfx::Point& pt) {
-  if (BrowserList::empty())
+  // Don't do anything if there aren't at least two normal browsers to
+  // switch between.
+  if (GetNormalBrowserCount() < 2)
     return;
-  Browser* last_active = BrowserList::GetLastActive();
-  if (last_active) {
-    BrowserList::const_iterator iter;
-    for (iter = BrowserList::begin(); iter != BrowserList::end(); ++iter) {
-      if (last_active == *iter) {
-        ++iter;
-        break;
-      }
-    }
-    if (iter == BrowserList::end())
-      iter = BrowserList::begin();
-    (*iter)->window()->Activate();
-  } else {
-    (*BrowserList::begin())->window()->Activate();
-  }
+
+  // Send a message to the ChromeOS window manager to switch to the
+  // next top level browser window.  Only the window manager knows
+  // what order they are in, so we can't use Chrome's browser list
+  // to decide.
+  WmIpc::Message message(chromeos::WM_IPC_MESSAGE_WM_CYCLE_WINDOWS);
+  message.set_param(0, true);
+  WmIpc::instance()->SendMessage(message);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
