e62904338cba4795cc875e5072ac7d436662aeed
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-28 07:37:37 +0000

    Change gen_keyboard_overlay_data.js to generate the power key as a highlithed key in the base state.
    BUG=none
    TEST=Ran gen_keyboard_overlay_data.js and checked the generated data.
    
    Review URL: http://codereview.chromium.org/6724030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79527 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d99231b15054269fba1907dc8ce68208e6ad9751

--- tools/gen_keyboard_overlay_data/gen_keyboard_overlay_data.py
+++ tools/gen_keyboard_overlay_data/gen_keyboard_overlay_data.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -121,7 +121,9 @@ def ToKeys(hotkey):
       continue
   modifiers.sort()
   keycode = value.lower().rstrip()
-  if not modifiers and keycode not in ['backspace']:
+  # The keys which are highlighted even without modifier keys.
+  base_keys = ['backspace', 'power']
+  if not modifiers and keycode not in base_keys:
     return None
   return ' '.join([keycode] + modifiers)
 
