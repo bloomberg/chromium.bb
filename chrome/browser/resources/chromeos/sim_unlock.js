6e2c71d5b4fe90485f1fb5379ff7d8e6f092c2a0
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-28 14:11:27 +0000

    [cros] Initial check-in of SIM unlock dialog HTML/JS/CSS.
    BUG=chromium-os:12007
    TEST=Manual. Emulated SIM locked state/entered PIN/emulated incorrect PIN error.
    
    Review URL: http://codereview.chromium.org/6732022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79553 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fc912f8f06849d845762c893465bd6c455ff9c8c

--- chrome/browser/resources/chromeos/sim_unlock.js
+++ chrome/browser/resources/chromeos/sim_unlock.js
@@ -2,10 +2,118 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * DOMContentLoaded handler, initializes the page.
- */
-function load() {
-}
+cr.define('mobile', function() {
 
-document.addEventListener('DOMContentLoaded', load);
+  function SimUnlock() {
+  }
+
+  cr.addSingletonGetter(SimUnlock);
+
+  SimUnlock.SIM_UNLOCK_LOADING           = -1;
+  SimUnlock.SIM_ABSENT_NOT_LOCKED        =  0,
+  SimUnlock.SIM_LOCKED_PIN               =  1;
+  SimUnlock.SIM_LOCKED_NO_PIN_TRIES_LEFT =  2;
+  SimUnlock.SIM_LOCKED_PUK               =  3;
+  SimUnlock.SIM_LOCKED_NO_PUK_TRIES_LEFT =  4;
+  SimUnlock.SIM_DISABLED                 =  5;
+
+  SimUnlock.ERROR_PIN = 'incorrectPin';
+  SimUnlock.ERROR_PUK = 'incorrectPuk';
+  SimUnlock.ERROR_OK = 'ok';
+
+  SimUnlock.localStrings_ = new LocalStrings();
+
+  SimUnlock.prototype = {
+    initialized_: false,
+    state_: -1,
+
+    changeState_: function(simInfo) {
+      var newState = simInfo.state;
+      var error = simInfo.error;
+      this.hideAll_();
+      switch(newState) {
+        case SimUnlock.SIM_UNLOCK_LOADING:
+          break;
+        case SimUnlock.SIM_ABSENT_NOT_LOCKED:
+          // TODO(nkostylev): Update status info, probably close dialog.
+          break;
+        case SimUnlock.SIM_LOCKED_PIN:
+          $('pin-input').value = '';
+          SimUnlock.enablePinDialog(true);
+          $('locked-pin-overlay').hidden = false;
+          var pinMessage;
+          if (error == SimUnlock.ERROR_OK) {
+            pinMessage = SimUnlock.localStrings_.getString('enterPinMessage');
+            $('pin-error-msg').classList.remove('error');
+          }
+          if (error == SimUnlock.ERROR_PIN) {
+            pinMessage =
+                SimUnlock.localStrings_.getString('incorrectPinMessage');
+            $('pin-error-msg').classList.add('error');
+          }
+          $('pin-error-msg').textContent = pinMessage;
+          // TODO(nkostylev): Show pin tries left info.
+          break;
+        case SimUnlock.SIM_LOCKED_NO_PIN_TRIES_LEFT:
+          $('locked-pin-no-tries-overlay').hidden = false;
+          break;
+        case SimUnlock.SIM_LOCKED_PUK:
+          $('locked-puk-overlay').hidden = false;
+          // TODO(nkostylev): Show PUK warning.
+          // TODO(nkostylev): Show PUK tries left info.
+          break;
+        case SimUnlock.SIM_LOCKED_NO_PUK_TRIES_LEFT:
+          $('locked-puk-no-tries-overlay').hidden = false;
+          break;
+        case SimUnlock.SimUnlock.SIM_DISABLED:
+          $('sim-disabled-overlay').hidden = false;
+          break;
+      }
+      this.state_ = newState;
+    },
+
+    hideAll_: function() {
+      $('locked-pin-overlay').hidden = true;
+      $('locked-pin-no-tries-overlay').hidden = true;
+      $('locked-puk-overlay').hidden = true;
+      $('locked-puk-no-tries-overlay').hidden = true;
+      $('sim-disabled-overlay').hidden = true;
+    },
+
+    updateSimStatus_: function(simInfo) {
+      this.changeState_(simInfo);
+    },
+  };
+
+  SimUnlock.close = function() {
+    // TODO(nkostylev): Close dialog.
+  };
+
+  SimUnlock.initialize = function() {
+    this.initialized_ = true;
+    $('enter-pin-confirm').addEventListener('click', function(event) {
+      SimUnlock.enablePinDialog(false);
+      chrome.send('enterPinCode', [$('pin-input').value]);
+    });
+    $('enter-pin-dismiss').addEventListener('click', function(event) {
+      SimUnlock.close();
+    });
+    chrome.send('simStatusInitialize');
+  };
+
+  SimUnlock.enablePinDialog = function(enabled) {
+    $('pin-input').disabled = !enabled;
+    $('enter-pin-confirm').disabled = !enabled;
+    $('enter-pin-dismiss').disabled = !enabled;
+  };
+
+  SimUnlock.simStateChanged = function(simInfo) {
+    SimUnlock.getInstance().updateSimStatus_(simInfo);
+  };
+
+  // Export
+  return {
+    SimUnlock: SimUnlock
+  };
+
+});
--- chrome/browser/ui/webui/chromeos/sim_unlock_ui.cc
+++ chrome/browser/ui/webui/chromeos/sim_unlock_ui.cc
@@ -9,10 +9,13 @@
 #include "base/memory/weak_ptr.h"
 #include "base/string_piece.h"
 #include "base/values.h"
+#include "chrome/browser/chromeos/cros/cros_library.h"
+#include "chrome/browser/chromeos/cros/network_library.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
 #include "chrome/common/jstemplate_builder.h"
 #include "chrome/common/url_constants.h"
+#include "content/browser/browser_thread.h"
 #include "content/browser/tab_contents/tab_contents.h"
 #include "grit/browser_resources.h"
 #include "grit/generated_resources.h"
@@ -21,9 +24,28 @@
 
 namespace {
 
-// Host page JS API function names.
+// JS API callbacks names.
 const char kJsApiEnterPinCode[] = "enterPinCode";
 const char kJsApiEnterPukCode[] = "enterPukCode";
+const char kJsApiSimStatusInitialize[] = "simStatusInitialize";
+
+// Page JS API function names.
+const char kJsApiSimStatusChanged[] = "mobile.SimUnlock.simStateChanged";
+
+// Error constants, passed to the page.
+const char kErrorPin[] = "incorrectPin";
+const char kErrorPuk[] = "incorrectPuk";
+const char kErrorOk[] = "ok";
+
+chromeos::CellularNetwork* GetCellularNetwork() {
+  chromeos::NetworkLibrary* lib = chromeos::CrosLibrary::Get()->
+      GetNetworkLibrary();
+  if (lib &&
+      lib->cellular_networks().begin() != lib->cellular_networks().end()) {
+    return *(lib->cellular_networks().begin());
+  }
+  return NULL;
+}
 
 }  // namespace
 
@@ -64,11 +86,67 @@ class SimUnlockHandler : public WebUIMessageHandler,
   virtual void RegisterMessages();
 
  private:
+  // Should keep this state enum in sync with similar one in JS code.
+  typedef enum SimUnlockState {
+    SIM_UNLOCK_LOADING           = -1,
+    SIM_ABSEND_NOT_LOCKED        =  0,
+    SIM_LOCKED_PIN               =  1,
+    SIM_LOCKED_NO_PIN_TRIES_LEFT =  2,
+    SIM_LOCKED_PUK               =  3,
+    SIM_LOCKED_NO_PUK_TRIES_LEFT =  4,
+    SIM_DISABLED                 =  5,
+  } SimUnlockState;
+
+  class TaskProxy : public base::RefCountedThreadSafe<TaskProxy> {
+   public:
+    explicit TaskProxy(const base::WeakPtr<SimUnlockHandler>& handler)
+        : handler_(handler) {
+    }
+    TaskProxy(const base::WeakPtr<SimUnlockHandler>& handler,
+              const std::string& code)
+        : handler_(handler),
+          code_(code) {
+    }
+    void HandleEnterPinCode() {
+      if (handler_)
+        handler_->EnterPinCode(code_);
+    }
+    void HandleInitialize() {
+      if (handler_)
+        handler_->InitializeSimStatus();
+    }
+   private:
+    base::WeakPtr<SimUnlockHandler> handler_;
+
+    // Pending code input (PIN/PUK).
+    std::string code_;
+
+    DISALLOW_COPY_AND_ASSIGN(TaskProxy);
+  };
+
+  // Pass PIN code to flimflam and check status.
+  void EnterPinCode(const std::string& code);
+
+  // Exports SIM card state.
+  void GetSimInfo(chromeos::CellularNetwork* network, DictionaryValue* value);
+
   // Handlers for JS WebUI messages.
   void HandleEnterPinCode(const ListValue* args);
   void HandleEnterPukCode(const ListValue* args);
+  void HandleSimStatusInitialize(const ListValue* args);
+
+  // Initialize current SIM card status, passes that to page.
+  void InitializeSimStatus();
+
+  // Processes current SIM card state and update internal state/page.
+  void ProcessSimCardState(chromeos::CellularNetwork* network);
+
+  // Updates page with the current state/SIM card info/error.
+  void UpdatePage(chromeos::CellularNetwork* network,
+                  const std::string& error_msg);
 
   TabContents* tab_contents_;
+  SimUnlockState state_;
 
   DISALLOW_COPY_AND_ASSIGN(SimUnlockHandler);
 };
@@ -84,15 +162,17 @@ void SimUnlockUIHTMLSource::StartDataRequest(const std::string& path,
                                              int request_id) {
   DictionaryValue strings;
   strings.SetString("title",
-                    l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_ENTER_PIN_TITLE));
+      l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_ENTER_PIN_TITLE));
+  strings.SetString("ok", l10n_util::GetStringUTF16(IDS_OK));
+  strings.SetString("cancel", l10n_util::GetStringUTF16(IDS_CANCEL));
   strings.SetString("enterPinTitle",
-                    l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_ENTER_PIN_TITLE));
+      l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_ENTER_PIN_TITLE));
+  strings.SetString("enterPinMessage",
+      l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_ENTER_PIN_MESSAGE));
   strings.SetString("incorrectPinMessage",
       l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_INCORRECT_PIN_MESSAGE));
-  // TODO(nkostylev): Initialize string with number of PIN tries left.
-  strings.SetString("pinTriesLeft",
-      l10n_util::GetStringFUTF16(IDS_SIM_UNLOCK_PIN_TRIES_LEFT_MESSAGE,
-                                 string16()));
+  strings.SetString("triesLeft",
+      l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_TRIES_LEFT_MESSAGE));
   strings.SetString("incorrectPinTitle",
       l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_INCORRECT_PIN_TITLE));
   // TODO(nkostylev): Pass carrier name if we know that.
@@ -103,20 +183,18 @@ void SimUnlockUIHTMLSource::StartDataRequest(const std::string& path,
       l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_ENTER_PUK_BUTTON));
   strings.SetString("enterPukTitle",
       l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_ENTER_PUK_TITLE));
-  // TODO(nkostylev): Initialize string with number of PIN tries left.
-  strings.SetString("pukTriesLeft",
-      l10n_util::GetStringFUTF16(IDS_SIM_UNLOCK_PUK_TRIES_LEFT_MESSAGE,
-                                 string16()));
+  strings.SetString("enterPukWarning",
+      l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_ENTER_PUK_WARNING));
   // TODO(nkostylev): Pass carrier name if we know that.
   strings.SetString("enterPukMessage", l10n_util::GetStringFUTF16(
       IDS_SIM_UNLOCK_ENTER_PUK_MESSAGE,
       l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_DEFAULT_CARRIER)));
   strings.SetString("noPukTriesLeft",
       l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_NO_PUK_TRIES_LEFT_MESSAGE));
-  strings.SetString("simBlockedTitle",
-      l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_SIM_BLOCKED_TITLE));
-  strings.SetString("simBlockedMessage",
-      l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_SIM_BLOCKED_MESSAGE));
+  strings.SetString("simDisabledTitle",
+      l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_SIM_DISABLED_TITLE));
+  strings.SetString("simDisabledMessage",
+      l10n_util::GetStringUTF16(IDS_SIM_UNLOCK_SIM_DISABLED_MESSAGE));
   SetFontAndTextDirection(&strings);
 
   static const base::StringPiece html(
@@ -136,7 +214,8 @@ void SimUnlockUIHTMLSource::StartDataRequest(const std::string& path,
 // SimUnlockHandler ------------------------------------------------------------
 
 SimUnlockHandler::SimUnlockHandler()
-    : tab_contents_(NULL) {
+    : tab_contents_(NULL),
+      state_(SIM_UNLOCK_LOADING) {
 }
 
 SimUnlockHandler::~SimUnlockHandler() {
@@ -156,14 +235,78 @@ void SimUnlockHandler::RegisterMessages() {
       NewCallback(this, &SimUnlockHandler::HandleEnterPinCode));
   web_ui_->RegisterMessageCallback(kJsApiEnterPukCode,
       NewCallback(this, &SimUnlockHandler::HandleEnterPukCode));
+  web_ui_->RegisterMessageCallback(kJsApiSimStatusInitialize,
+      NewCallback(this, &SimUnlockHandler::HandleSimStatusInitialize));
+}
+
+void SimUnlockHandler::EnterPinCode(const std::string& code) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  // TODO(nkostylev): Pass PIN code to flimflam.
+  ProcessSimCardState(GetCellularNetwork());
+}
+
+void SimUnlockHandler::GetSimInfo(chromeos::CellularNetwork* network,
+                                  DictionaryValue* value) {
+  if (network) {
+    // TODO(nkostylev): Extract real tries left information.
+  }
 }
 
 void SimUnlockHandler::HandleEnterPinCode(const ListValue* args) {
-  // TODO(nkostylev): Pass PIN code to flimflam.
+  const size_t kEnterPinCodeParamCount = 1;
+  if (args->GetSize() != kEnterPinCodeParamCount)
+    return;
+  std::string pin_code;
+  if (!args->GetString(0, &pin_code))
+    return;
+  scoped_refptr<TaskProxy> task = new TaskProxy(AsWeakPtr(), pin_code);
+  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
+      NewRunnableMethod(task.get(), &TaskProxy::HandleEnterPinCode));
 }
 
 void SimUnlockHandler::HandleEnterPukCode(const ListValue* args) {
   // TODO(nkostylev): Pass PUK code to flimflam.
+}
+
+void SimUnlockHandler::HandleSimStatusInitialize(const ListValue* args) {
+  scoped_refptr<TaskProxy> task = new TaskProxy(AsWeakPtr());
+  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
+      NewRunnableMethod(task.get(), &TaskProxy::HandleInitialize));
+}
+
+void SimUnlockHandler::InitializeSimStatus() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  ProcessSimCardState(GetCellularNetwork());
+}
+
+void SimUnlockHandler::ProcessSimCardState(chromeos::CellularNetwork* network) {
+  std::string error_msg;
+  // TODO(nkostylev): Get real SIM card status:
+  // Absent/Not Locked/PIN locked, n tries/PUK locked, n tries/Blocked.
+  switch (state_) {
+    case SIM_UNLOCK_LOADING:
+    case SIM_ABSEND_NOT_LOCKED:
+    case SIM_LOCKED_PIN:
+    case SIM_LOCKED_NO_PIN_TRIES_LEFT:
+    case SIM_LOCKED_PUK:
+    case SIM_LOCKED_NO_PUK_TRIES_LEFT:
+    case SIM_DISABLED:
+      break;
+  }
+  UpdatePage(network, error_msg);
+}
+
+void SimUnlockHandler::UpdatePage(chromeos::CellularNetwork* network,
+                                  const std::string& error_msg) {
+  DictionaryValue sim_dict;
+  if (network)
+    GetSimInfo(network, &sim_dict);
+  sim_dict.SetInteger("state", state_);
+  if (!error_msg.empty())
+    sim_dict.SetString("error", error_msg);
+  else
+    sim_dict.SetString("error", kErrorOk);
+  web_ui_->CallJavascriptFunction(kJsApiSimStatusChanged, sim_dict);
 }
 
 // SimUnlockUI -----------------------------------------------------------------
