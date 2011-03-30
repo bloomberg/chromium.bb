30775bcd82f10ec5a8ad6e3c8c9530e40bba4d6d
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-30 00:32:29 +0000

    Revert 79768 - Fixed Clang error to http://codereview.chromium.org/6770014/
    BUG=none
    TEST=none
    TBR=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/6749046
    
    TBR=dhollowa@chromium.org
    Review URL: http://codereview.chromium.org/6749047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79769 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1b9f7ab1f8abf238acf180a029b25cf41cc7ec07

--- views/controls/button/native_button.h
+++ views/controls/button/native_button.h
@@ -120,8 +120,8 @@ class NativeButton : public TextButton {
 
   DISALLOW_COPY_AND_ASSIGN(NativeButton);
 };
-#else
-// TODO(saintlou): Windows and Clang do not like typedef, it
+#elif defined(OS_WIN)
+// TODO(saintlou): Windows does not like typedef, in particluar it
 // chokes in other modules that have a forward declaration for
 // NativeButton
 class NativeButton : public NativeButtonBase {
@@ -133,6 +133,9 @@ class NativeButton : public NativeButtonBase {
  private:
   DISALLOW_COPY_AND_ASSIGN(NativeButton);
 };
+#else
+// Keep the same implementation as before for non-touch case
+typedef NativeButtonBase NativeButton;
 #endif
 
 }  // namespace views
