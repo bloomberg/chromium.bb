a614f1bd9a18ee1aa7d090393da2e86bd2e6255c
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-03-30 21:30:14 +0000

    Mac: Clicking on download items should activate the window only on mouse up.
    This uses the technique from http://www.cocoadev.com/index.pl?PreventWindowOrdering
    
    Since DownloadItemButton is derived from DraggableButton which already overrides -mouseDown to do something like on that page, and since NSButton already -acceptsFirstMouse, overriding -shouldDelayWindowOrderingForEvent is sufficient.
    
    BUG=77807
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/6780002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@79898 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 99d364a272e478b323e08798b68811185f7cb965

--- chrome/browser/ui/cocoa/download/download_item_button.mm
+++ chrome/browser/ui/cocoa/download/download_item_button.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -45,6 +45,10 @@
 
 - (void)menuDidClose:(NSMenu*)menu {
   [[self cell] setHighlighted:NO];
+}
+
+- (BOOL)shouldDelayWindowOrderingForEvent:(NSEvent*)event {
+  return YES;
 }
 
 @end
