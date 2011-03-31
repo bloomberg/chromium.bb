7a4796210a65c6a10c58700ec43fa1fdd62eaaae
bradnelson@google.com <bradnelson@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-03-31 04:33:26 +0000

    Some bots have checkouts on c:\ instead of e:\ Generalizing...
    BUG=None
    TEST=None
    TBR=noelallen@google.com
    Review URL: http://codereview.chromium.org/6737016
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4677 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: dab911c9fffb51559c9838479af4f4a0d8985e46

--- buildbot/msvs_env.bat
+++ buildbot/msvs_env.bat
@@ -8,9 +8,12 @@ set GYP_MSVS_VERSION=2008
 
 REM Blow away path for now if on the bots (to be more hermetic).
 if %BUILDBOT_SLAVENAME% equ "" goto SkipPathReset
-set PATH=e:\b\depot_tools;e:\b\depot_tools\python_bin
-set PATH=%PATH%;C:\WINDOWS\system32;C:\WINDOWS\system32\WBEM
+set PATH=
+set PATH=%PATH%;c:\b\depot_tools;c:\b\depot_tools\python_bin
+set PATH=%PATH%;c:\b\build_internal\tools
+set PATH=%PATH%;e:\b\depot_tools;e:\b\depot_tools\python_bin
 set PATH=%PATH%;e:\b\build_internal\tools
+set PATH=%PATH%;C:\WINDOWS\system32;C:\WINDOWS\system32\WBEM
 :SkipPathReset
 
 rem Adding in both the 32-bit on 64-bit program files install location for both
