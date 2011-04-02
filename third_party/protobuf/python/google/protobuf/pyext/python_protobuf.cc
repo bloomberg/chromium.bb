c4eedea50b8549bf1b3406dd0b6ad754a05af340
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-02 12:46:25 +0000

    third_party/protobuf: update to upstream r371
    (This is a reland of r80810.)
    
    1) Got an upstream diff 346:371 from svn
    2) Pruned files from the diff that we don't have, as well as
       descriptor.pb.*
    3) Applied and fixed the merge failures.
    4) Copied the code (less descriptor.pb.*) back into the svn source.
    5) Manually fixed descriptor.pb.* and plugin.pb.* there.
    6) Ran ./generate_descriptor_proto.sh and copied the updated files back.
    7) Added new source files to the protoc target in protobuf.gyp.
    
    BUG=none
    TEST=sync_unit_tests
    
    Review URL: http://codereview.chromium.org/6737030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80265 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5fc07eb1a6c22881419f906f087e798b746809c2

--- third_party/protobuf/python/google/protobuf/pyext/python_protobuf.cc
+++ third_party/protobuf/python/google/protobuf/pyext/python_protobuf.cc
@@ -28,14 +28,36 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// Author: kenton@google.com (Kenton Varda)
+// Author: qrczak@google.com (Marcin Kowalczyk)
 
-#include <google/protobuf/stubs/hash.h>
+#include <google/protobuf/pyext/python_protobuf.h>
 
 namespace google {
 namespace protobuf {
+namespace python {
 
-// Nothing needed here right now.
+static const Message* GetCProtoInsidePyProtoStub(PyObject* msg) {
+  return NULL;
+}
+static Message* MutableCProtoInsidePyProtoStub(PyObject* msg) {
+  return NULL;
+}
 
+// This is initialized with a default, stub implementation.
+// If python-google.protobuf.cc is loaded, the function pointer is overridden
+// with a full implementation.
+const Message* (*GetCProtoInsidePyProtoPtr)(PyObject* msg) =
+    GetCProtoInsidePyProtoStub;
+Message* (*MutableCProtoInsidePyProtoPtr)(PyObject* msg) =
+    MutableCProtoInsidePyProtoStub;
+
+const Message* GetCProtoInsidePyProto(PyObject* msg) {
+  return GetCProtoInsidePyProtoPtr(msg);
+}
+Message* MutableCProtoInsidePyProto(PyObject* msg) {
+  return MutableCProtoInsidePyProtoPtr(msg);
+}
+
+}  // namespace python
 }  // namespace protobuf
 }  // namespace google
