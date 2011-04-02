f56b85f996482fe9926d3a857ec07ef3cc721da1
khim@google.com <khim@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-04-02 07:05:50 +0000

    Borrow test setup from regular windows bot.
    Use configuration similar to regular bot but ignore
    errors for now: glibc tests don't pass on Windows yet thus
    it's useless to have "forever red" bot till we'll fix them.
    Review URL: http://codereview.chromium.org/6760036
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4738 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 376b789d1920342c84e3943563bc34f228cbfb6e

--- buildbot/buildbot_windows-glibc-makefile.bat
+++ buildbot/buildbot_windows-glibc-makefile.bat
@@ -20,25 +20,51 @@ endlocal
 :: gsutil does not work when called from cygwin - python versions conflict.
 
 echo @@@BUILD_STEP archive_build@@@
-call ..\..\..\..\scripts\slave\gsutil -h Cache-Control:no-cache cp -a public-read tools\toolchain.tgz gs://nativeclient-archive2/x86_toolchain/r%BUILDBOT_GOT_REVISION%/toolchain_win_x86.tar.gz
+call ..\..\..\..\scripts\slave\gsutil -h Cache-Control:no-cache cp -a public-read ^
+ tools\toolchain.tgz ^
+ gs://nativeclient-archive2/x86_toolchain/r%BUILDBOT_GOT_REVISION%/toolchain_win_x86.tar.gz
 echo @@@STEP_LINK@download@http://gsdview.appspot.com/nativeclient-archive2/x86_toolchain/r%BUILDBOT_GOT_REVISION%/@@@
 
+:: Run tests
+
+setlocal
+call "%~dp0msvs_env.bat"
+
+setlocal
+echo @@@BUILD_STEP gyp_compile@@@
+call vcvarsall.bat x86 && call devenv.com build\all.sln /build Release
+:: if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+
 echo @@@BUILD_STEP gyp_tests@@@
-call vcvarsall.bat x86   && call devenv.com build\all.sln /build Release || (
-  set RETCODE=0 || echo @@@STEP_FAILURE@@@)
+python_slave.exe trusted_test.py --config Release
+:: if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+endlocal
 
+setlocal
 echo @@@BUILD_STEP scons_compile32@@@
-call scons.bat -k -j 8 DOXYGEN=..\third_party\doxygen\win/doxygen --nacl_glibc --verbose --mode=opt-win,nacl,doc platform=x86-32 || (
-  set RETCODE=0 || echo @@@STEP_FAILURE@@@)
+call vcvarsall.bat x86 && call scons.bat -j 8 ^
+ DOXYGEN=..\third_party\doxygen\win\doxygen ^
+ -k --verbose --mode=opt-win,nacl,doc platform=x86-64
+:: if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+endlocal
 
+setlocal
 echo @@@BUILD_STEP scons_compile64@@@
-call scons.bat -k -j 8 DOXYGEN=..\third_party\doxygen\win\doxygen --nacl_glibc --verbose --mode=opt-win,nacl,doc platform=x86-64 || (
-  set RETCODE=0 || echo @@@STEP_FAILURE@@@)
+call vcvarsall.bat x64 && call scons.bat -j 8 ^
+ DOXYGEN=..\third_party\doxygen\win\doxygen ^
+ -k --verbose --mode=opt-win,nacl,doc platform=x86-32
+:: if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+endlocal
+
+setlocal
+set RETCODE=0
 
 echo @@@BUILD_STEP small_tests64@@@
-call scons.bat -k -j 8 --mode=dbg-host,nacl platform=x86-64 --nacl_glibc --verbose small_tests || (
-  set RETCODE=0 || echo @@@STEP_FAILURE@@@)
+call vcvarsall.bat x64 && call scons.bat ^
+ DOXYGEN=..\third_party\doxygen\win\doxygen ^
+ -k --verbose --mode=dbg-win,nacl,doc small_tests platform=x86-64
+:: if %ERRORLEVEL% neq 0 (set RETCODE=%ERRORLEVEL% & echo @@@STEP_FAILURE@@@)
 
-# TODO(khim): add small_tests, medium_tests, large_tests, chrome_browser_tests.
+# TODO(khim): add medium_tests, large_tests, chrome_browser_tests.
 
-exit %RETCODE%
+exit /b %RETCODE%
