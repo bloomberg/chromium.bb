9c8ed4f61668b15844513c27127d6ca0305a29ac
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-04 17:43:45 +0000

    Close the download shelf if it's showing, when showing the downloads tab.
    BUG=77716
    TEST=trybots, manual testing
    
    Review URL: http://codereview.chromium.org/6794011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80334 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3d037ce75a4ae3dc07f062bad8afc5079a07d282

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1866,6 +1866,11 @@ void Browser::ShowHistoryTab() {
 
 void Browser::ShowDownloadsTab() {
   UserMetrics::RecordAction(UserMetricsAction("ShowDownloads"), profile_);
+  if (window()) {
+    DownloadShelf* shelf = window()->GetDownloadShelf();
+    if (shelf->IsShowing())
+      shelf->Close();
+  }
   ShowSingletonTab(GURL(chrome::kChromeUIDownloadsURL));
 }
 
