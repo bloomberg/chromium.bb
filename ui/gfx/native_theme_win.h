07388f44e5bf790be7857ef88e264ad530890294
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-04 15:25:17 +0000

    Step 1 in refactoring NativeThemeWin so that it shares more with the native theme support in linux and chromeos.
    This first change introduces a new common base class for all plaforms called
    NativeTheme, and NativeThemeWin now derives from it.  NativeThemeWin will
    continue to support its "old" API while the transition to the new API is
    ongoing, so that each user of NativeThemeWin can be transitioned carefully.
    
    Once the windows support is complete, the linux and chromeos native themes
    will then derive from NativeTheme, and any references do the specific platform
    classes will be removed as needed.
    
    TEST=No user visible changes caused by this CL
    BUG=None
    R=xiyuan@chromium.org
    
    Review URL: http://codereview.chromium.org/6728029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80314 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2bd51de1a5ffc4514a11c3f18c7e2e5f1b92d728

--- ui/gfx/native_theme_win.h
+++ ui/gfx/native_theme_win.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -12,6 +12,7 @@
 #define UI_GFX_NATIVE_THEME_WIN_H_
 #pragma once
 
+#include "ui/gfx/native_theme.h"
 #include "ui/gfx/size.h"
 #include <windows.h>
 #include <uxtheme.h>
@@ -24,14 +25,13 @@ class PlatformCanvas;
 
 namespace gfx {
 
-// TODO: Define class member enums to replace part_id and state_id parameters
-// that are currently defined in <vssym32.h>. Afterward, classic_state should
-// be removed and class users wouldn't need to include <vssym32.h> anymore.
-// This would enable HOT state on non-themed UI (like when RDP'ing) and would
-// simplify usage.
-// TODO: This class should probably be changed to be platform independent at
-// the same time.
-class NativeThemeWin {
+// Windows implementation of native theme class.
+//
+// At the moment, this class in in transition from an older API that consists
+// of several PaintXXX methods to an API, inherited from the NativeTheme base
+// class, that consists of a single Paint() method with a argument to indicate
+// what kind of part to paint.
+class NativeThemeWin : public NativeTheme {
  public:
   enum ThemeName {
     BUTTON,
@@ -48,6 +48,63 @@ class NativeThemeWin {
     SPIN,
     LAST
   };
+
+  bool IsThemingActive() const;
+
+  HRESULT GetThemePartSize(ThemeName themeName,
+                           HDC hdc,
+                           int part_id,
+                           int state_id,
+                           RECT* rect,
+                           int ts,
+                           SIZE* size) const;
+
+  HRESULT GetThemeColor(ThemeName theme,
+                        int part_id,
+                        int state_id,
+                        int prop_id,
+                        SkColor* color) const;
+
+  // Get the theme color if theming is enabled.  If theming is unsupported
+  // for this part, use Win32's GetSysColor to find the color specified
+  // by default_sys_color.
+  SkColor GetThemeColorWithDefault(ThemeName theme,
+                                   int part_id,
+                                   int state_id,
+                                   int prop_id,
+                                   int default_sys_color) const;
+
+  HRESULT GetThemeInt(ThemeName theme,
+                      int part_id,
+                      int state_id,
+                      int prop_id,
+                      int *result) const;
+
+  // Get the thickness of the border associated with the specified theme,
+  // defaulting to GetSystemMetrics edge size if themes are disabled.
+  // In Classic Windows, borders are typically 2px; on XP+, they are 1px.
+  Size GetThemeBorderSize(ThemeName theme) const;
+
+  // Disables all theming for top-level windows in the entire process, from
+  // when this method is called until the process exits.  All the other
+  // methods in this class will continue to work, but their output will ignore
+  // the user's theme. This is meant for use when running tests that require
+  // consistent visual results.
+  void DisableTheming() const;
+
+  // Closes cached theme handles so we can unload the DLL or update our UI
+  // for a theme change.
+  void CloseHandles() const;
+
+  // Returns true if classic theme is in use.
+  bool IsClassicTheme(ThemeName name) const;
+
+  // Gets our singleton instance.
+  static const NativeThemeWin* instance();
+
+  // The PaintXXX methods below this point should be private or be deleted,
+  // but remain public while NativeThemeWin is transitioned over to use the
+  // single Paint() entry point.  Do not make new calls to these methods.
 
   // This enumeration is used within PaintMenuArrow in order to indicate the
   // direction the menu arrow should point to.
@@ -104,16 +161,20 @@ class NativeThemeWin {
                                            int prop_id,
                                            int *value);
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintButton(HDC hdc,
                       int part_id,
                       int state_id,
                       int classic_state,
                       RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintDialogBackground(HDC dc, bool active, RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintListBackground(HDC dc, bool enabled, RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   // |arrow_direction| determines whether the arrow is pointing to the left or
   // to the right. In RTL locales, sub-menus open from right to left and
   // therefore the menu arrow should point to the left and not to the right.
@@ -125,12 +186,14 @@ class NativeThemeWin {
                          MenuArrowDirection arrow_direction,
                          ControlState state) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintMenuBackground(ThemeName theme,
                               HDC hdc,
                               int part_id,
                               int state_id,
                               RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintMenuCheck(ThemeName theme,
                          HDC hdc,
                          int part_id,
@@ -138,17 +201,20 @@ class NativeThemeWin {
                          RECT* rect,
                          ControlState state) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintMenuCheckBackground(ThemeName theme,
                                    HDC hdc,
                                    int part_id,
                                    int state_id,
                                    RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintMenuGutter(HDC hdc,
                           int part_id,
                           int state_id,
                           RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintMenuItemBackground(ThemeName theme,
                                   HDC hdc,
                                   int part_id,
@@ -156,17 +222,20 @@ class NativeThemeWin {
                                   bool selected,
                                   RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintMenuList(HDC hdc,
                         int part_id,
                         int state_id,
                         int classic_state,
                         RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintMenuSeparator(HDC hdc,
                              int part_id,
                              int state_id,
                              RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   // Paints a scrollbar arrow.  |classic_state| should have the appropriate
   // classic part number ORed in already.
   HRESULT PaintScrollbarArrow(HDC hdc,
@@ -174,6 +243,7 @@ class NativeThemeWin {
                               int classic_state,
                               RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   // Paints a scrollbar track section.  |align_rect| is only used in classic
   // mode, and makes sure the checkerboard pattern in |target_rect| is aligned
   // with one presumed to be in |align_rect|.
@@ -185,6 +255,7 @@ class NativeThemeWin {
                               RECT* align_rect,
                               skia::PlatformCanvas* canvas) const;
 
+  // This method is deprecated and will be removed in the near future.
   // Paints a scrollbar thumb or gripper.
   HRESULT PaintScrollbarThumb(HDC hdc,
                               int part_id,
@@ -192,20 +263,24 @@ class NativeThemeWin {
                               int classic_state,
                               RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintSpinButton(HDC hdc,
                           int part_id,
                           int state_id,
                           int classic_state,
                           RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintStatusGripper(HDC hdc,
                              int part_id,
                              int state_id,
                              int classic_state,
                              RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintTabPanelBackground(HDC dc, RECT* rect) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintTextField(HDC hdc,
                          int part_id,
                          int state_id,
@@ -215,6 +290,7 @@ class NativeThemeWin {
                          bool fill_content_area,
                          bool draw_edges) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintTrackbar(HDC hdc,
                         int part_id,
                         int state_id,
@@ -222,6 +298,7 @@ class NativeThemeWin {
                         RECT* rect,
                         skia::PlatformCanvas* canvas) const;
 
+  // This method is deprecated and will be removed in the near future.
   HRESULT PaintProgressBar(HDC hdc,
                            RECT* bar_rect,
                            RECT* value_rect,
@@ -229,62 +306,53 @@ class NativeThemeWin {
                            double animated_seconds,
                            skia::PlatformCanvas* canvas) const;
 
-  bool IsThemingActive() const;
-
-  HRESULT GetThemePartSize(ThemeName themeName,
-                           HDC hdc,
-                           int part_id,
-                           int state_id,
-                           RECT* rect,
-                           int ts,
-                           SIZE* size) const;
-
-  HRESULT GetThemeColor(ThemeName theme,
-                        int part_id,
-                        int state_id,
-                        int prop_id,
-                        SkColor* color) const;
-
-  // Get the theme color if theming is enabled.  If theming is unsupported
-  // for this part, use Win32's GetSysColor to find the color specified
-  // by default_sys_color.
-  SkColor GetThemeColorWithDefault(ThemeName theme,
-                                   int part_id,
-                                   int state_id,
-                                   int prop_id,
-                                   int default_sys_color) const;
-
-  HRESULT GetThemeInt(ThemeName theme,
-                      int part_id,
-                      int state_id,
-                      int prop_id,
-                      int *result) const;
-
-  // Get the thickness of the border associated with the specified theme,
-  // defaulting to GetSystemMetrics edge size if themes are disabled.
-  // In Classic Windows, borders are typically 2px; on XP+, they are 1px.
-  Size GetThemeBorderSize(ThemeName theme) const;
-
-  // Disables all theming for top-level windows in the entire process, from
-  // when this method is called until the process exits.  All the other
-  // methods in this class will continue to work, but their output will ignore
-  // the user's theme. This is meant for use when running tests that require
-  // consistent visual results.
-  void DisableTheming() const;
-
-  // Closes cached theme handles so we can unload the DLL or update our UI
-  // for a theme change.
-  void CloseHandles() const;
-
-  // Returns true if classic theme is in use.
-  bool IsClassicTheme(ThemeName name) const;
-
-  // Gets our singleton instance.
-  static const NativeThemeWin* instance();
-
  private:
   NativeThemeWin();
   ~NativeThemeWin();
+
+  // NativeTheme Implementation:
+  virtual gfx::Size GetPartSize(Part part) const;
+  virtual void Paint(skia::PlatformCanvas* canvas,
+                     Part part,
+                     State state,
+                     const gfx::Rect& rect,
+                     const ExtraParams& extra) const;
+
+  // Paints a scrollbar arrow.  |classic_state| should have the appropriate
+  // classic part number ORed in already.
+  HRESULT PaintScrollbarArrow(HDC hdc,
+                              Part direction,
+                              State state,
+                              const gfx::Rect& rect) const;
+
+  HRESULT PaintScrollbarThumb(HDC hdc,
+                              Part direction,
+                              State state,
+                              const gfx::Rect& rect) const;
+
+  HRESULT PaintPushButton(HDC hdc,
+                          Part part,
+                          State state,
+                          const gfx::Rect& rect,
+                          const ButtonExtraParams& extra) const;
+
+  HRESULT PaintRadioButton(HDC hdc,
+                           Part part,
+                           State state,
+                           const gfx::Rect& rect,
+                           const ButtonExtraParams& extra) const;
+
+  HRESULT PaintCheckbox(HDC hdc,
+                        Part part,
+                        State state,
+                        const gfx::Rect& rect,
+                        const ButtonExtraParams& extra) const;
+
+  // Get the windows theme name that goes with the part.
+  static ThemeName GetThemeName(Part part);
+
+  // Get the windows theme part id that goes with the part.
+  static int GetWindowsPart(Part part);
 
   HRESULT PaintFrameControl(HDC hdc,
                             RECT* rect,
