be92bd48a190e6a6c9ca115fba732cdfb6da92f6
rhashimoto@chromium.org <rhashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-05 17:04:37 +0000

    Print a grit debugging hint to stdout when a parse exception occurs to help with locating the error in the .grd file.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/6760030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80470 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 206d87fa6fb6342be364b02874b4af0899164f0d

--- tools/grit/grit/grd_reader.py
+++ tools/grit/grit/grd_reader.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python2.4
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -134,6 +134,8 @@ def Parse(filename_or_stream, dir=None, flexible_root=False,
     assert stop_after
     pass
   except:
+    if not debug:
+      print "parse exception: run GRIT with the -x flag to debug .grd problems"
     raise
 
   if not flexible_root or hasattr(handler.root, 'SetOwnDir'):
