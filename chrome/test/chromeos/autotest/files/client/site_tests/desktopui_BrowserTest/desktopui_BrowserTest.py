8875bc1b6355d2a832deaf8478ad3d264fb57279
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-06 22:43:40 +0000

    Run browser tests while logged in and disable PDF tests. Several browser tests on Chrome OS fail when run while not logged in. Running tests as a logged in user is also a closer representation of a real testing scenario.
    R=nirnimesh@chromium.org
    BUG=cros:13857
    TEST=Ran the browser tests on a cr-48. The default user logged in and tests passed.
    
    Review URL: http://codereview.chromium.org/6693104
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80711 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 81f1877a0181f1beee508e15867fa2c3b5f9b19c

--- chrome/test/chromeos/autotest/files/client/site_tests/desktopui_BrowserTest/desktopui_BrowserTest.py
+++ chrome/test/chromeos/autotest/files/client/site_tests/desktopui_BrowserTest/desktopui_BrowserTest.py
@@ -1,14 +1,22 @@
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 from autotest_lib.client.cros import chrome_test
+from autotest_lib.client.cros import cros_ui_test
+from autotest_lib.client.cros import login
 
-class desktopui_BrowserTest(chrome_test.ChromeTestBase):
+class desktopui_BrowserTest(chrome_test.ChromeTestBase, cros_ui_test.UITest):
     version = 1
 
     binary_to_run='browser_tests'
-    blacklist = []
+    # PDF tests blacklisted for now: chromium: 78510
+    blacklist = ['PDFBrowserTest.Basic', 'PDFBrowserTest.Scroll',
+                 'PDFBrowserTest.FindAndCopy']
+
+    def initialize(self, creds='$default'):
+        chrome_test.ChromeTestBase.initialize(self, False)
+        cros_ui_test.UITest.initialize(self, creds)
 
     def run_once(self, group=0, total_groups=4):
         tests_to_run = self.filter_bad_tests(
