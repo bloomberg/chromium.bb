957641f75a1af233574960bd55ed0c31710eeb76
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-07 18:06:54 +0000

    ntp4: tile shuffle animation
    the tiling should:
    - offer 6-wide and 3-wide tile grids to the user
    - in the 6-wide format, icons will grow to use the available space (within reason)
    - animate between these two states
    - not animate when not switching between these two states (keeping mouse-drag window resizing smooth)
    - always be smooth; not flicker or jump around
    
    BUG=none
    TEST=manual
    
    Review URL: http://codereview.chromium.org/6793033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@80811 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 545b0bced2bba28b9c2639dad45a2d1972191ce8

--- chrome/browser/resources/ntp4/apps_page.js
+++ chrome/browser/resources/ntp4/apps_page.js
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 cr.define('ntp4', function() {
+  'use strict';
 
   /**
    * Creates a new App object.
@@ -25,8 +26,14 @@ cr.define('ntp4', function() {
     initialize: function() {
       assert(this.appData.id, 'Got an app without an ID');
 
-      this.className = 'app tile';
+      this.className = 'app';
       this.setAttribute('app-id', this.appData.id);
+
+      // The subcontainer wraps the icon and text. Its main purpose is to give
+      // us additional, animated left/right CSS values (see setBounds).
+      this.subcontainer_ = this.ownerDocument.createElement('div');
+      this.subcontainer_.className = 'app-subcontainer';
+      this.appendChild(this.subcontainer_);
 
       var appImg = this.ownerDocument.createElement('img');
       appImg.src = this.appData.icon_big;
@@ -36,13 +43,39 @@ cr.define('ntp4', function() {
       // We put a click handler just on the app image - so clicking on the
       // margins between apps doesn't do anything.
       appImg.addEventListener('click', this.onClick_.bind(this));
-      this.appendChild(appImg);
+      this.subcontainer_.appendChild(appImg);
+      this.appImg_ = appImg;
 
       var appSpan = this.ownerDocument.createElement('span');
       appSpan.textContent = this.appData.name;
-      this.appendChild(appSpan);
+      this.subcontainer_.appendChild(appSpan);
 
       /* TODO(estade): grabber */
+    },
+
+    /**
+     * Set the size and position of the app tile. Part of the location should
+     * animate (animatedX/Y) and the rest (realX/Y) should take effect
+     * immediately.
+     * @param {number} innerSize The size of the app icon.
+     * @param {number} outerSize The total size of |this|.
+     * @param {number} realX The actual x-position.
+     * @param {number} realY The actual y-position.
+     * @param {number} animatedX The portion of the x-position that should
+     *     animate.
+     * @param {number} animatedY The portion of the y-position that should
+     *     animate.
+     */
+    setBounds: function(innerSize, outerSize,
+                        realX, realY,
+                        animatedX, animatedY) {
+      this.appImg_.style.width = this.appImg_.style.height = innerSize + 'px';
+      this.subcontainer_.style.width = this.subcontainer_.style.height =
+          outerSize + 'px';
+      this.subcontainer_.style.left = animatedX + 'px';
+      this.subcontainer_.style.top = animatedY + 'px';
+      this.style.left = (realX - animatedX) + 'px';
+      this.style.top = (realY - animatedY) + 'px';
     },
 
     /**
@@ -61,6 +94,72 @@ cr.define('ntp4', function() {
   };
 
   /**
+   * Gives the proportion of the row width that is devoted to a single icon.
+   * @param {number} rowTileCount The number of tiles in a row.
+   * @return {number} The ratio between icon width and row width.
+   */
+  function iconWidthFraction(rowTileCount) {
+    return rowTileCount +
+        rowTileCount * 2 * ICON_PADDING_FRACTION +
+        (rowTileCount - 1) * TILE_SPACING_FRACTION;
+  }
+
+  /**
+   * Calculates an assortment of tile-related values for a grid with the
+   * given dimensions.
+   * @param {number} width The pixel width of the grid.
+   * @param {number} numRowTiles The number of tiles in a row.
+   * @return {Object} A mapping of pixel values.
+   */
+  function tileValuesForGrid(width, numRowTiles) {
+    var iconWidth = Math.min(MAX_ICON_WIDTH,
+        width / iconWidthFraction(numRowTiles));
+    var tileWidth = iconWidth + iconWidth * 2 * ICON_PADDING_FRACTION;
+    var offset = tileWidth + iconWidth * TILE_SPACING_FRACTION;
+
+    return {
+      iconWidth: iconWidth,
+      tileWidth: tileWidth,
+      offset: offset,
+    };
+  }
+
+  // The most tiles we will show in a row.
+  var MAX_ROW_TILE_COUNT = 6;
+  // The fewest tiles we will show in a row.
+  var MIN_ROW_TILE_COUNT = 3;
+
+  // The smallest an app icon will ever get.
+  var MIN_ICON_WIDTH = 96;
+  // The largest an app icon can be.
+  var MAX_ICON_WIDTH = 128;
+
+  // The proportion of the icon width which will be added to each side of the
+  // icon as padding (within the tile).
+  var ICON_PADDING_FRACTION = 1 / 12;
+  // The proportion of the icon width which will be used as spacing between
+  // tiles.
+  var TILE_SPACING_FRACTION = 1 / 6;
+
+  // The smallest amount of horizontal blank space to display on the sides when
+  // displaying a six-up arrangement.
+  var MIN_SIXUP_MARGIN = 100;
+
+  // The minimum amount of space we need to display a six-wide grid.
+  var MIN_SIXUP_WIDTH = MIN_ICON_WIDTH * iconWidthFraction(MAX_ROW_TILE_COUNT);
+  // The largest we will ever display a six-wide grid.
+  var MAX_SIXUP_WIDTH = MAX_ICON_WIDTH * iconWidthFraction(MAX_ROW_TILE_COUNT);
+  // The amount of space we need to display a three-wide grid (all
+  // three-wide grids are this size).
+  var THREEUP_WIDTH = MIN_ICON_WIDTH * iconWidthFraction(MIN_ROW_TILE_COUNT);
+
+  // Tile-related pixel values for the minimum six-wide display.
+  var LARGE_TILE_VALUES =
+      tileValuesForGrid(MIN_SIXUP_WIDTH, MAX_ROW_TILE_COUNT);
+  // Tile-related pixel values for the three-wide display.
+  var SMALL_TILE_VALUES = tileValuesForGrid(THREEUP_WIDTH, MIN_ROW_TILE_COUNT);
+
+  /**
    * Creates a new AppsPage object. This object contains apps and controls
    * their layout.
    * @constructor
@@ -74,16 +173,19 @@ cr.define('ntp4', function() {
     return el;
   }
 
-  var MAX_ROW_TILE_COUNT = 6;
-  var MIN_ROW_TILE_COUNT = 3;
-
   AppsPage.prototype = {
     __proto__: HTMLDivElement.prototype,
 
     initialize: function() {
       this.className = 'apps-page';
-      /* Ordered list of our apps. */
-      this.appElements = this.getElementsByClassName('app');
+
+      // Div that holds the apps.
+      this.tileGrid_ = this.ownerDocument.createElement('div');
+      this.tileGrid_.className = 'tile-grid';
+      this.appendChild(this.tileGrid_);
+
+      // Ordered list of our apps.
+      this.appElements = this.tileGrid_.getElementsByClassName('app');
 
       this.lastWidth_ = this.clientWidth;
 
@@ -102,12 +204,15 @@ cr.define('ntp4', function() {
     /**
      * Creates an app DOM element and places it at the last position on the
      * page.
+     * TODO(estade): don't animate initial apps.
      * @param {Object} appData The data object that describes the app.
      */
     appendApp: function(appData) {
       var appElement = new App(appData);
-      this.appendChild(appElement);
+      this.tileGrid_.appendChild(appElement);
+
       this.positionApp_(this.appElements.length - 1);
+      this.classList.remove('resizing-apps-page');
     },
 
     /**
@@ -115,45 +220,41 @@ cr.define('ntp4', function() {
      * @param {number} The index of the element to be positioned.
      */
     positionApp_: function(index) {
-      var width = this.clientWidth;
-      // Vertical and horizontal spacing between tiles.
-      var innerSpacing = 28;
+      var availableSpace = this.tileGrid_.clientWidth - 2 * MIN_SIXUP_MARGIN;
+      var numRowTiles = availableSpace < MIN_SIXUP_WIDTH ?
+          MIN_ROW_TILE_COUNT : MAX_ROW_TILE_COUNT;
 
-      // Try to show 6 elements per row. If there's not enough space, go for
-      // 3.
-      var numRowTiles = MAX_ROW_TILE_COUNT;
-      var minSixupTileSize = 128;
-      var tileSize = (width - (numRowTiles - 1) * innerSpacing) / numRowTiles;
-      if (tileSize < minSixupTileSize) {
-        numRowTiles = MIN_ROW_TILE_COUNT;
-        tileSize = minSixupTileSize;
-      }
-
-      var row = Math.floor(index / numRowTiles);
       var col = index % numRowTiles;
+      var row = Math.floor(index / numRowTiles);
 
-      var usedWidth = numRowTiles * tileSize + (numRowTiles - 1) * innerSpacing;
-      var leftSpacing = (width - usedWidth) / 2;
-      var x = col * (innerSpacing + tileSize) + leftSpacing;
-      var topSpacing = 50;
-      var y = row * (innerSpacing + tileSize) + topSpacing;
+      // Calculate the portion of the tile's position that should be animated.
+      var animatedTileValues = numRowTiles == MAX_ROW_TILE_COUNT ?
+          LARGE_TILE_VALUES : SMALL_TILE_VALUES;
+      // Animate the difference between three-wide and six-wide.
+      var animatedLeftMargin = numRowTiles == MAX_ROW_TILE_COUNT ?
+          0 : (MIN_SIXUP_WIDTH - MIN_SIXUP_MARGIN - THREEUP_WIDTH) / 2;
+      var animatedX = col * animatedTileValues.offset + animatedLeftMargin;
+      var animatedY = row * animatedTileValues.offset;
 
-      var tileElement = this.appElements[index];
-      tileElement.style.left = x + 'px';
-      tileElement.style.top = y + 'px';
-    },
-
-    /**
-     * Find the index within the page of the given application.
-     * returns {int} The index of the app.
-     */
-    indexOf_: function(appElement) {
-      for (var i = 0; i < this.appElements.length; i++) {
-        if (appElement == this.appElements[i])
-          return i;
+      // Calculate the final on-screen position for the tile.
+      var effectiveGridWidth = THREEUP_WIDTH;
+      if (numRowTiles == MAX_ROW_TILE_COUNT) {
+        effectiveGridWidth =
+            Math.min(Math.max(availableSpace, MIN_SIXUP_WIDTH),
+                     MAX_SIXUP_WIDTH);
       }
+      var realTileValues = tileValuesForGrid(effectiveGridWidth, numRowTiles);
+      // leftMargin centers the grid within the avaiable space.
+      var minMargin = numRowTiles == MAX_ROW_TILE_COUNT ? MIN_SIXUP_MARGIN : 0;
+      var leftMargin =
+          Math.max(minMargin,
+                   (this.tileGrid_.clientWidth - effectiveGridWidth) / 2);
+      var realX = col * realTileValues.offset + leftMargin;
+      var realY = row * realTileValues.offset;
 
-      return -1;
+      this.appElements[index].setBounds(
+          realTileValues.iconWidth, realTileValues.tileWidth,
+          realX, realY, animatedX, animatedY);
     },
 
     /**
@@ -166,6 +267,7 @@ cr.define('ntp4', function() {
         return;
 
       this.lastWidth_ = this.clientWidth;
+      this.classList.add('resizing-apps-page');
 
       for (var i = 0; i < this.appElements.length; i++) {
         this.positionApp_(i);
