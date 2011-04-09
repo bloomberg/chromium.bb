4ed96b094092b77e7bb781203c09e9d2d4cfa76e
khim@google.com <khim@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-04-09 17:44:00 +0000

    Fix lucid64.x86_64.toolchain.prep.sh.
    Mostly cosmetic changes.
    
    1. Add shebang to file.
    2. Don't try to download file which is already found.
    3. Actually create linker script.
    Review URL: http://codereview.chromium.org/6825027
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@4808 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: a495a7cb899c98357911d2c607cd1de8fdedc33c

--- tools/lucid64.x86_64.toolchain.prep.sh
+++ tools/lucid64.x86_64.toolchain.prep.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # Copyright (c) 2011 The Native Client Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -28,9 +29,13 @@ mkdir "$working_directory"
 
 cd "$working_directory"
 
+need_libstdcxx46=0
+
 if dpkg -s libppl0.11-dev ; then
   libraries[libppl0.11-dev]=${libraries[libppl0.10-dev]}
+  libraries[libpwl-dev]=p/ppl
   unset libraries[libppl0.10-dev]
+  need_libstdcxx46=1
 fi
 
 if ((EUID==0)); then
@@ -54,25 +59,57 @@ for library in "${!libraries[@]}"; do
       if wget "$mirror/${libraries[$library]}/${library}_${version}_i386.deb"
       then
         found=1
+        break
       fi
     done
+    if ((!found)) && [[ "$library" = "libgmp-dev" ]]; then
+      for mirror in "${mirrors[@]}"; do
+        if wget "$mirror/m/mpfr/${library}_${version}_i386.deb"; then
+          found=1
+          break
+        fi
+      done
+    fi
     if ((!found)); then
       exit 1
     fi
     ar x "${library}_${version}_i386.deb" data.tar.gz
     tar xSvpf data.tar.gz --wildcards './usr/lib/*.*a'
+    if [[ "$library" = "libgmp-dev" ]]; then
+      tar xSvpf data.tar.gz './usr/include/gmp-i386.h'
+    fi
     rm data.tar.gz
   }
 done
 perl -pi -e s'|/usr/lib|/usr/lib32|' usr/lib/*.la
 mv usr/lib/libgmpxx.a usr/lib/libgmpxx-orig.a
-cat <<END
+if ((need_libstdcxx46)); then
+  wget "${mirrors[1]}"/g/gcc-4.6/libstdc++6-4.6-dev_4.6.0-2_i386.deb
+  ar x libstdc++6-4.6-dev_4.6.0-2_i386.deb data.tar.gz
+  tar xSvpf data.tar.gz ./usr/lib/gcc/i486-linux-gnu/4.6/libstdc++.a
+  mv usr/lib/gcc/i486-linux-gnu/4.6/libstdc++.a usr/lib/libstdc++46.a
+  rm -rf usr/lib/gcc
+  rm data.tar.gz
+  cat >usr/lib/libgmpxx.a <<END
 OUTPUT_FORMAT(elf32-i386)
 GROUP ( /usr/lib32/libgmpxx-orig.a
-        AS_NEEDED ( /usr/lib32/libstdc++.so /usr/lib32/libm.so ) )
+        AS_NEEDED ( /usr/lib32/libstdc++46.a
+                    /usr/lib32/libpwl.a
+                    /usr/lib32/libm.so ) )
 END
+else
+  cat >usr/lib/libgmpxx.a <<END
+OUTPUT_FORMAT(elf32-i386)
+GROUP ( /usr/lib32/libgmpxx-orig.a
+        AS_NEEDED ( /usr/lib32/libstdc++.so.6 /usr/lib32/libm.so ) )
+END
+fi
+chmod a+r usr/lib/libgmpxx.a
 if ((install)); then
   mv -f usr/lib/*.*a /usr/lib32
+  mv -f usr/include/*.h /usr/include
+  rm -rf "$PWD"
 else
   echo "Move files from $PWD/usr/lib to /usr/lib32"
+  echo "Move files from $PWD/usr/include to /usr/include"
 fi
