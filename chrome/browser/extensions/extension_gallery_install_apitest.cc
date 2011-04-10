c968cacb19bacc11fa23deaa7b81260947f70f26
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-10 04:05:43 +0000

    Add some extra debugging info for a flaky test.
    BUG=55642
    TEST=(this is a test)
    
    TBR=aa@chromium.org
    Review URL: http://codereview.chromium.org/6826018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81053 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 978857644f798a06075130b211b1780edde7a601

--- chrome/browser/extensions/extension_gallery_install_apitest.cc
+++ chrome/browser/extensions/extension_gallery_install_apitest.cc
@@ -1,8 +1,12 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/stringprintf.h"
+#if defined (OS_WIN)
+#include "base/win/windows_version.h"
+#endif  // defined (OS_WIN)
+
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_webstore_private_api.h"
 #include "chrome/common/chrome_switches.h"
@@ -35,22 +39,47 @@ class ExtensionGalleryInstallApiTest : public ExtensionApiTest {
   }
 };
 
-// http://crbug.com/55642 - failing on XP.
+namespace {
+
+bool RunningOnXP() {
 #if defined (OS_WIN)
-#define MAYBE_InstallAndUninstall DISABLED_InstallAndUninstall
+  return GetVersion() < base::win::VERSION_VISTA;
+#else
+  return false;
+#endif  // defined (OS_WIN)
+}
+
+}  // namespace
+
+// TODO(asargent) - for some reason this test occasionally fails on XP,
+// but not other versions of windows. http://crbug.com/55642
+#if defined (OS_WIN)
+#define MAYBE_InstallAndUninstall FLAKY_InstallAndUninstall
 #else
 #define MAYBE_InstallAndUninstall InstallAndUninstall
 #endif
 IN_PROC_BROWSER_TEST_F(ExtensionGalleryInstallApiTest,
                        MAYBE_InstallAndUninstall) {
+  if (RunningOnXP()) {
+    LOG(INFO) << "Adding host resolver rule";
+  }
   host_resolver()->AddRule("www.example.com", "127.0.0.1");
+  if (RunningOnXP()) {
+    LOG(INFO) << "Starting test server";
+  }
   ASSERT_TRUE(test_server()->Start());
 
+  if (RunningOnXP()) {
+    LOG(INFO) << "Starting tests without user gesture checking";
+  }
   BeginInstallFunction::SetIgnoreUserGestureForTests(true);
   ASSERT_TRUE(RunInstallTest("test.html"));
   ASSERT_TRUE(RunInstallTest("complete_without_begin.html"));
   ASSERT_TRUE(RunInstallTest("invalid_begin.html"));
 
+  if (RunningOnXP()) {
+    LOG(INFO) << "Starting tests with user gesture checking";
+  }
   BeginInstallFunction::SetIgnoreUserGestureForTests(false);
   ASSERT_TRUE(RunInstallTest("no_user_gesture.html"));
 }
