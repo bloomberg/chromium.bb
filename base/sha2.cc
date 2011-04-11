da2c9a27b716af8d0db4b22ca145c8562e995feb
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-11 10:28:46 +0000

    Simplify SHA2 implementation Now portable, implemented as a wrapper over the SecureHash class.
    BUG=None
    TEST=base_unittests --gtest_filter=*Sha* --vmodule=*openssl*=3 
    
    
    Review URL: http://codereview.chromium.org/6812048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81087 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 087b8c8ecf6630ab31d30913148e0e613a0ab881

--- base/sha2.cc
+++ base/sha2.cc
@@ -1,23 +1,20 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/sha2.h"
 
+#include "base/crypto/secure_hash.h"
+#include "base/scoped_ptr.h"
 #include "base/stl_util-inl.h"
-#include "base/third_party/nss/blapi.h"
-#include "base/third_party/nss/sha256.h"
 
 namespace base {
 
 void SHA256HashString(const std::string& str, void* output, size_t len) {
-  SHA256Context ctx;
-
-  SHA256_Begin(&ctx);
-  SHA256_Update(&ctx, reinterpret_cast<const unsigned char*>(str.data()),
-                static_cast<unsigned int>(str.length()));
-  SHA256_End(&ctx, static_cast<unsigned char*>(output), NULL,
-             static_cast<unsigned int>(len));
+  scoped_ptr<base::SecureHash> ctx(base::SecureHash::Create(
+      base::SecureHash::SHA256));
+  ctx->Update(str.data(), str.length());
+  ctx->Finish(output, len);
 }
 
 std::string SHA256HashString(const std::string& str) {
