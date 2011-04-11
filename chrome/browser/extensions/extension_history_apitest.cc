1ce5885b48a6ce405a0698017bf9795635187b2e
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-11 18:46:48 +0000

    Mark ExtensionHistoryApiTest.GetVisits flaky.
    TEST=N/A
    BUG=79074
    TBR=dmichael@chromium.org
    Review URL: http://codereview.chromium.org/6823050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81131 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 724571232ff3281f572c8a38aa7c2b3108db7847

--- chrome/browser/extensions/extension_history_apitest.cc
+++ chrome/browser/extensions/extension_history_apitest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -33,7 +33,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionHistoryApiTest, Delete) {
   ASSERT_TRUE(RunExtensionSubtest("history", "delete.html")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionHistoryApiTest, GetVisits) {
+// See crbug.com/79074
+IN_PROC_BROWSER_TEST_F(ExtensionHistoryApiTest, FLAKY_GetVisits) {
   ASSERT_TRUE(RunExtensionSubtest("history", "get_visits.html")) << message_;
 }
 
