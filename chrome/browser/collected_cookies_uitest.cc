21651406a916b333fb7048e2a374570102560fb9
siggi@chromium.org <siggi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-13 13:36:53 +0000

    Disable crashing tests on Windows.
    BUG=79331
    TBR=jochen@chromium.org
    Review URL: http://codereview.chromium.org/6834024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81404 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: eb280f098c807da75ddd19c737beecbbcccb8782

--- chrome/browser/collected_cookies_uitest.cc
+++ chrome/browser/collected_cookies_uitest.cc
@@ -20,7 +20,13 @@ const FilePath::CharType kDocRoot[] = FILE_PATH_LITERAL("chrome/test/data");
 
 typedef UITest CollectedCookiesTest;
 
-TEST_F(CollectedCookiesTest, DoubleDisplay) {
+// Crashing on Windows, see http://crbug.com/79331
+#if defined(OS_WINDOWS)
+#define MAYBE_DoubleDisplay DISABLED_DoubleDisplay
+#else
+#define MAYBE_DoubleDisplay DoubleDisplay
+#endif
+TEST_F(CollectedCookiesTest, MAYBE_DoubleDisplay) {
   net::TestServer test_server(net::TestServer::TYPE_HTTP, FilePath(kDocRoot));
   ASSERT_TRUE(test_server.Start());
 
@@ -42,7 +48,13 @@ TEST_F(CollectedCookiesTest, DoubleDisplay) {
   ASSERT_TRUE(tab->ShowCollectedCookiesDialog());
 }
 
-TEST_F(CollectedCookiesTest, NavigateAway) {
+// Crashing on Windows, see http://crbug.com/79331
+#if defined(OS_WINDOWS)
+#define MAYBE_NavigateAway DISABLED_NavigateAway
+#else
+#define MAYBE_NavigateAway NavigateAway
+#endif
+TEST_F(CollectedCookiesTest, MAYBE_NavigateAway) {
   net::TestServer test_server(net::TestServer::TYPE_HTTP, FilePath(kDocRoot));
   ASSERT_TRUE(test_server.Start());
 
