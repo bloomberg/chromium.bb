7c52e5a21ebd72fbb8744b6a2264691f22710be2
bradnelson@google.com <bradnelson@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-13 01:29:23 +0000

    Disabling PPAPI hello world test in nacl_ui_tests. This mis-named test uses a now deprecated interface. It's making the nacl-chrome integration builder red.
    BUG=None
    TEST=None
    R=ncbray@google.com
    Review URL: http://codereview.chromium.org/6841001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81350 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6228ea1ec913dd1b559b9b38e60e30e3514992b6

--- chrome/test/nacl/nacl_ui_test.cc
+++ chrome/test/nacl/nacl_ui_test.cc
@@ -41,7 +41,8 @@ TEST_F(NaClUITest, ServerTest) {
   RunTest(test_file, TestTimeouts::action_max_timeout_ms());
 }
 
-TEST_F(NaClUITest, PPAPIHelloWorld) {
+// Uses obsolete srpc interface.
+TEST_F(NaClUITest, DISABLED_PPAPIHelloWorld) {
   FilePath test_file(kPPAPIHwHtmlFileName);
   RunTest(test_file, TestTimeouts::action_max_timeout_ms());
 }
