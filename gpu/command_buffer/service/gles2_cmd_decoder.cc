ee514b79bfefc36e132b02907e49482f4f828e67
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-15 18:03:35 +0000

    Command buffer: fix a wrong error for glDrawArrays/glDrawElements/glClear if the framebuffer is not complete.
    BUG=none
    TEST=none
    Review URL: http://codereview.chromium.org/6861008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81766 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: be45d7c3673ff290fdf3025316b87db96a1f8e51

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -2244,7 +2244,7 @@ void GLES2DecoderImpl::RestoreCurrentTexture2DBindings() {
 
 bool GLES2DecoderImpl::CheckFramebufferComplete(const char* func_name) {
   if (bound_draw_framebuffer_ && bound_draw_framebuffer_->IsNotComplete()) {
-    SetGLError(GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT,
+    SetGLError(GL_INVALID_FRAMEBUFFER_OPERATION,
                (std::string(func_name) + " framebuffer incomplete").c_str());
     return false;
   }
