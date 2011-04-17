997bb199ba085585f82b5d866628820babefa775
bradnelson@google.com <bradnelson@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-17 06:17:19 +0000

    Replacing misguided .sh/.bat files with a python shim.
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1295
    TEST=None
    R=ncbray@google.com
    Review URL: http://codereview.chromium.org/6874036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81897 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: df37c6b66ee155d84f8c07cbd5d9e0d745799f3f

---
+++ chrome/test/nacl_test_injection/buildbot_nacl_integration.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import subprocess
+import sys
+
+
+def Main():
+  script_dir = os.path.dirname(os.path.abspath(__file__))
+  test_dir = os.path.dirname(script_dir)
+  chrome_dir = os.path.dirname(test_dir)
+  src_dir = os.path.dirname(chrome_dir)
+  nacl_integration_script = os.path.join(
+      src_dir, 'native_client/build/buildbot_chrome_nacl_stage.py')
+  cmd = [sys.executable, nacl_integration_script] + sys.argv[1:]
+  print cmd
+  # TODO(bradnelson): turn on when test is green.
+  #subprocess.check_call(cmd)
+
+
+if __name__ == '__main__':
+  Main()
