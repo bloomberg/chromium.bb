025565369dcbd2b2c97ccbbdab61177328608b63
bradnelson@google.com <bradnelson@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-17 09:28:43 +0000

    Disabling integration tests on linux (as they'll failing on the full builder).
    BUG=None
    TEST=None
    TBR=ncbray@google.com
    Review URL: http://codereview.chromium.org/6870031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@81905 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 26d4f5402f58d851928b455f4cbb0699d98e1c20

--- chrome/test/nacl_test_injection/buildbot_nacl_integration.py
+++ chrome/test/nacl_test_injection/buildbot_nacl_integration.py
@@ -11,6 +11,8 @@ import sys
 def Main():
   # TODO(ncbray): figure out why this is failing on windows and enable.
   if sys.platform in ['win32', 'cygwin']: return
+  # TODO(ncbray): use xvfb on linux and re-enable.
+  if sys.platform in ['linux', 'linux2']: return
 
   script_dir = os.path.dirname(os.path.abspath(__file__))
   test_dir = os.path.dirname(script_dir)
