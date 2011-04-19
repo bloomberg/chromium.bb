aed6d960e2227aa197e43f143befd6bbc231c913
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-19 02:56:31 +0000

    Support "canary" as a version string modifier in Mac Chrome.
    BUG=79814
    TEST=none
    Review URL: http://codereview.chromium.org/6873062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82060 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9de3c757d5e18e568cb804e16ad39809eec5ebc2

--- chrome/browser/platform_util_mac.mm
+++ chrome/browser/platform_util_mac.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,7 +6,7 @@
 
 #include <Carbon/Carbon.h>
 #import <Cocoa/Cocoa.h>
-#import <CoreServices/CoreServices.h>
+#include <CoreServices/CoreServices.h>
 
 #include "base/file_path.h"
 #include "base/logging.h"
@@ -187,14 +187,17 @@ std::string GetVersionStringModifier() {
   NSBundle* bundle = [NSBundle mainBundle];
   NSString* channel = [bundle objectForInfoDictionaryKey:@"KSChannelID"];
 
-  // Only ever return "", "unknown", "beta" or "dev" in a branded build.
+  // Only ever return "", "unknown", "beta", "dev", or "canary" in a branded
+  // build.
   if (![bundle objectForInfoDictionaryKey:@"KSProductID"]) {
     // This build is not Keystone-enabled, it can't have a channel.
     channel = @"unknown";
   } else if (!channel) {
     // For the stable channel, KSChannelID is not set.
     channel = @"";
-  } else if ([channel isEqual:@"beta"] || [channel isEqual:@"dev"]) {
+  } else if ([channel isEqual:@"beta"] ||
+             [channel isEqual:@"dev"] ||
+             [channel isEqual:@"canary"]) {
     // do nothing.
   } else {
     channel = @"unknown";
