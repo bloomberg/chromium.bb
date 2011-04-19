bloomberg/chromium.bb

commit c4d1ac89542264a09509645beb2522f351e7e3f8

linux: don't always print dlopen errors from LoadNativeLibrary
Instead, return them to the caller and let the caller decide whether
the error is worth notifying the user about.

BUG=79068

Review URL: http://codereview.chromium.org/6864020

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82008 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 84479325c4ccdecd2bddc2f257d436b66595aa10

ui/gfx/gl/gl_implementation_linux.cc
@@ -27,6 +27,18 @@ void GL_BINDING_CALL MarshalDepthRangeToDepthRangef(GLclampd z_near,
   glDepthRangef(static_cast<GLclampf>(z_near), static_cast<GLclampf>(z_far));
 }
 
+// Load a library, printing an error message on failure.
+base::NativeLibrary LoadLibrary(const char* filename) {
+  std::string error;
+  base::NativeLibrary library = base::LoadNativeLibrary(FilePath(filename),
+                                                        &error);
+  if (!library) {
+    VLOG(1) << "Failed to load " << filename << ": " << error;
+    return NULL;
+  }
+  return library;
+}
+
 }  // namespace anonymous
 
 bool InitializeGLBindings(GLImplementation implementation) {
@@ -44,12 +56,9 @@ bool InitializeGLBindings(GLImplementation implementation) {
         return false;
       }
 
-      base::NativeLibrary library = base::LoadNativeLibrary(
-          module_path.Append("libosmesa.so"));
-      if (!library) {
-        VLOG(1) << "libosmesa.so not found";
+      base::NativeLibrary library = LoadLibrary("libosmesa.so");
+      if (!library)
         return false;
-      }
 
       GLGetProcAddressProc get_proc_address =
           reinterpret_cast<GLGetProcAddressProc>(
@@ -70,12 +79,9 @@ bool InitializeGLBindings(GLImplementation implementation) {
       break;
     }
     case kGLImplementationDesktopGL: {
-      base::NativeLibrary library = base::LoadNativeLibrary(
-          FilePath("libGL.so.1"));
-      if (!library) {
-        VLOG(1) << "libGL.so.1 not found.";
+      base::NativeLibrary library = LoadLibrary("libGL.so.1");
+      if (!library)
         return false;
-      }
 
       GLGetProcAddressProc get_proc_address =
           reinterpret_cast<GLGetProcAddressProc>(
@@ -96,20 +102,12 @@ bool InitializeGLBindings(GLImplementation implementation) {
       break;
     }
     case kGLImplementationEGLGLES2: {
-      base::NativeLibrary gles_library = base::LoadNativeLibrary(
-          FilePath("libGLESv2.so"));
-      if (!gles_library) {
-        VLOG(1) << "libGLESv2.so not found";
+      base::NativeLibrary gles_library = LoadLibrary("libGLESv2.so");
+      if (!gles_library)
         return false;
-      }
-
-      base::NativeLibrary egl_library = base::LoadNativeLibrary(
-          FilePath("libEGL.so"));
-      if (!egl_library) {
-        VLOG(1) << "libEGL.so not found";
-        base::UnloadNativeLibrary(gles_library);
+      base::NativeLibrary egl_library = LoadLibrary("libEGL.so");
+      if (!egl_library)
         return false;
-      }
 
       GLGetProcAddressProc get_proc_address =
           reinterpret_cast<GLGetProcAddressProc>(

app/win/shell.cc
@@ -81,7 +81,7 @@ void SetAppIdForWindow(const string16& app_id, HWND hwnd) {
   std::wstring shell32_filename(kShell32);
   FilePath shell32_filepath(shell32_filename);
   base::NativeLibrary shell32_library = base::LoadNativeLibrary(
-      shell32_filepath);
+      shell32_filepath, NULL);
 
   if (!shell32_library)
     return;

base/memory/scoped_native_library.cc
@@ -14,7 +14,7 @@ ScopedNativeLibrary::ScopedNativeLibrary(NativeLibrary library)
 }
 
 ScopedNativeLibrary::ScopedNativeLibrary(const FilePath& library_path) {
-  library_ = base::LoadNativeLibrary(library_path);
+  library_ = base::LoadNativeLibrary(library_path, NULL);
 }
 
 ScopedNativeLibrary::~ScopedNativeLibrary() {

base/native_library.h
@@ -52,8 +52,11 @@ typedef void* NativeLibrary;
 #endif  // OS_*
 
 // Loads a native library from disk.  Release it with UnloadNativeLibrary when
-// you're done.
-BASE_API NativeLibrary LoadNativeLibrary(const FilePath& library_path);
+// you're done.  Returns NULL on failure.
+// If |err| is not NULL, it may be filled in with an error message on
+// error.
+BASE_API NativeLibrary LoadNativeLibrary(const FilePath& library_path,
+                                         std::string* error);
 
 #if defined(OS_WIN)
 // Loads a native library from disk.  Release it with UnloadNativeLibrary when

base/native_library_linux.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,7 +14,8 @@
 namespace base {
 
 // static
-NativeLibrary LoadNativeLibrary(const FilePath& library_path) {
+NativeLibrary LoadNativeLibrary(const FilePath& library_path,
+                                std::string* error) {
   // dlopen() opens the file off disk.
   base::ThreadRestrictions::AssertIOAllowed();
 
@@ -23,16 +24,8 @@ NativeLibrary LoadNativeLibrary(const FilePath& library_path) {
   // http://crbug.com/17943, http://crbug.com/17557, http://crbug.com/36892,
   // and http://crbug.com/40794.
   void* dl = dlopen(library_path.value().c_str(), RTLD_LAZY);
-  if (!dl) {
-    std::string error_message = dlerror();
-    // Some obsolete plugins depend on libxul or libxpcom.
-    // Ignore the error messages when failing to load these.
-    if (error_message.find("libxul.so") == std::string::npos &&
-        error_message.find("libxpcom.so") == std::string::npos) {
-      LOG(ERROR) << "dlopen failed when trying to open " << library_path.value()
-                 << ": " << error_message;
-    }
-  }
+  if (!dl && error)
+    *error = dlerror();
 
   return dl;
 }

base/native_library_mac.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,7 +16,8 @@
 namespace base {
 
 // static
-NativeLibrary LoadNativeLibrary(const FilePath& library_path) {
+NativeLibrary LoadNativeLibrary(const FilePath& library_path,
+                                std::string* error) {
   // dlopen() etc. open the file off disk.
   if (library_path.Extension() == "dylib" ||
       !file_util::DirectoryExists(library_path)) {

base/native_library_win.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -39,7 +39,8 @@ NativeLibrary LoadNativeLibraryHelper(const FilePath& library_path,
 }
 
 // static
-NativeLibrary LoadNativeLibrary(const FilePath& library_path) {
+NativeLibrary LoadNativeLibrary(const FilePath& library_path,
+                                std::string* error) {
   return LoadNativeLibraryHelper(library_path, LoadLibraryW);
 }
 

chrome/nacl/nacl_main_platform_delegate_mac.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -47,7 +47,7 @@ void NaClMainPlatformDelegate::InitSandboxTests(bool no_sandbox) {
     FilePath test_dll_name =
       command_line.GetSwitchValuePath(switches::kTestNaClSandbox);
     if (!test_dll_name.empty()) {
-      sandbox_test_module_ = base::LoadNativeLibrary(test_dll_name);
+      sandbox_test_module_ = base::LoadNativeLibrary(test_dll_name, NULL);
       CHECK(sandbox_test_module_);
     }
   }

commit 24769797645735d7af3313d3b28a907ea944c4d8

Fix osmesa lib path regression in Linux introduced in r82008.
BUG=none
TEST=a few webgl tests in linux turns green
TBR=evan@chromium.org
Review URL: http://codereview.chromium.org/6873060

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82043 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: a246122751b09011f4bb8bc7983d81f8f6d16b98

ui/gfx/gl/gl_implementation_linux.cc
@@ -28,15 +28,19 @@ void GL_BINDING_CALL MarshalDepthRangeToDepthRangef(GLclampd z_near,
 }
 
 // Load a library, printing an error message on failure.
-base::NativeLibrary LoadLibrary(const char* filename) {
+base::NativeLibrary LoadLibrary(const FilePath& filename) {
   std::string error;
-  base::NativeLibrary library = base::LoadNativeLibrary(FilePath(filename),
+  base::NativeLibrary library = base::LoadNativeLibrary(filename,
                                                         &error);
   if (!library) {
-    VLOG(1) << "Failed to load " << filename << ": " << error;
+    VLOG(1) << "Failed to load " << filename.MaybeAsASCII() << ": " << error;
     return NULL;
   }
   return library;
+}
+
+base::NativeLibrary LoadLibrary(const char* filename) {
+  return LoadLibrary(FilePath(filename));
 }
 
 }  // namespace anonymous
@@ -56,7 +60,8 @@ bool InitializeGLBindings(GLImplementation implementation) {
         return false;
       }
 
-      base::NativeLibrary library = LoadLibrary("libosmesa.so");
+      base::NativeLibrary library = LoadLibrary(
+          module_path.Append("libosmesa.so"));
       if (!library)
         return false;
 