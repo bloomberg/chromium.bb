2d67cf0bf3dfe993713b1018365a5a54c49f9494
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-20 23:13:30 +0000

    [Mac] Fix "Statusbar does not move up when we get the download shelf"
    BUG=28736
    TEST=In a maximized window without the download shelf showing, click on a download
    link. The status bubble should be moved above the download shelf that appears instead
    of overlapping with it.
    
    Review URL: http://codereview.chromium.org/6882040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82398 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ff19e410afdd3ab6767afaa44afbac648b569641

--- chrome/browser/ui/cocoa/status_bubble_mac.mm
+++ chrome/browser/ui/cocoa/status_bubble_mac.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -344,6 +344,7 @@ void StatusBubbleMac::MouseMoved(
 }
 
 void StatusBubbleMac::UpdateDownloadShelfVisibility(bool visible) {
+  UpdateSizeAndPosition();
 }
 
 void StatusBubbleMac::Create() {
