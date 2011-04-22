987809e69a14061a987bd02663b89551ed410b63
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-22 08:25:21 +0000

    Change the presubmit script to parse the whole suppression files instead of diffs Review URL: http://codereview.chromium.org/6881101
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@82630 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 887b4c4c2c5181c88285915d370ab231a1ea2444

--- tools/valgrind/memcheck/PRESUBMIT.py
+++ tools/valgrind/memcheck/PRESUBMIT.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -16,39 +16,44 @@ def CheckChange(input_api, output_api):
   # - 'skip_suppression_name': the next line is a suppression name, skip.
   # - 'skip_param': the next line is a system call parameter error, skip.
   skip_next_line = False
-  func_re = input_api.re.compile('[a-z_.]+\(.+\)$')
-  for f, line_num, line in input_api.RightHandSideLines(lambda x:
-      x.LocalPath().endswith('.txt')):
-    line = line.lstrip()
-    if line.startswith('#') or not line:
-      continue
+  for f in filter(lambda x: x.LocalPath().endswith('.txt'),
+                  input_api.AffectedFiles()):
+    for line, line_num in zip(f.NewContents(),
+                              xrange(1, len(f.NewContents()) + 1)):
+      line = line.lstrip()
+      if line.startswith('#') or not line:
+        continue
 
-    if skip_next_line:
-      if skip_next_line == 'skip_suppression_name':
-        if suppressions.has_key(line):
-          errors.append('suppression with name "%s" at %s line %s has already '
-                        'been defined at line %s' % (line, f.LocalPath(),
-                                                     line_num,
-                                                     suppressions[line][1]))
-        else:
-          suppressions[line] = (f, line_num)
-      skip_next_line = False
-      continue
-    if line == '{':
-      skip_next_line = 'skip_suppression_name'
-      continue
-    if line == "Memcheck:Param":
-      skip_next_line = 'skip_param'
-      continue
+      if skip_next_line:
+        if skip_next_line == 'skip_suppression_name':
+          if suppressions.has_key(line):
+            if f.LocalPath() == suppressions[line][1]:
+              errors.append('suppression with name "%s" at %s line %s '
+                            'has already been defined at line %s' %
+                            (line, f.LocalPath(), line_num,
+                             suppressions[line][1]))
+            else:
+              errors.append('suppression with name "%s" at %s line %s '
+                            'has already been defined at %s line %s' %
+                            (line, f.LocalPath(), line_num,
+                             suppressions[line][0], suppressions[line][1]))
+          else:
+            suppressions[line] = (f, line_num)
+        skip_next_line = False
+        continue
+      if line == '{':
+        skip_next_line = 'skip_suppression_name'
+        continue
+      if line == "Memcheck:Param":
+        skip_next_line = 'skip_param'
+        continue
 
-    if (line.startswith('fun:') or line.startswith('obj:') or
-        line.startswith('Memcheck:') or line == '}' or
-        line == '...'):
-      continue
-    if func_re.match(line):
-      continue
-    errors.append('"%s" is probably wrong: %s line %s' % (line, f.LocalPath(),
-                                                          line_num))
+      if (line.startswith('fun:') or line.startswith('obj:') or
+          line.startswith('Memcheck:') or line == '}' or
+          line == '...'):
+        continue
+      errors.append('"%s" is probably wrong: %s line %s' % (line, f.LocalPath(),
+                                                            line_num))
   if errors:
     return [output_api.PresubmitError('\n'.join(errors))]
   return []
