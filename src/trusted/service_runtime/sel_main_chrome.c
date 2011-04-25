bloomberg/chromium.bb

commit f87778dfe7e1c1de4637789939c3737489992cdd

Add an assertion to check that Chrome/Breakpad does not install a signal handler
Breakpad's signal handler is typically not designed to handle faults
that occur in NaCl sandboxed code, especially on x86-32 Linux where
the handler must restore %gs in order to be safe.

Breakpad's signal handler is being turned off for NaCl in a
Chrome-side change, http://codereview.chromium.org/6677168/.

This change is intended to prevent Chrome from accidentally
regressing.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=1607
TEST=build Chrome with this using "-D target_arch=ia32 -D linux_breakpad=1";
  run with CHROME_HEADLESS=1 --enable-nacl --enable-crash-reporter

Review URL: http://codereview.chromium.org/6798008

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5004 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: 8bc6c7c8d5eb6046a11a41aede3a418f33ae8cff

src/trusted/service_runtime/sel_main_chrome.c
@@ -105,12 +105,14 @@ int NaClMainForChromium(int handle_count, const NaClHandle *handles,
     fprintf(stderr, "Error while loading in SelMain: %s\n",
             NaClErrorString(errcode));
   }
-
-  /*
-   * Remove the handler and let error pass to Chrome's handlers.
-   */
+  /* Remove the handler that was used for platform qualification tests. */
   NaClSignalHandlerFini();
 
+  /*
+   * Check that Chrome did not register any signal handlers, because
+   * these are not always safe.
+   */
+  NaClSignalAssertNoHandlers();
 
   /* Give debuggers a well known point at which xlate_base is known.  */
   NaClGdbHook(&state);

src/trusted/service_runtime/nacl_signal.h
@@ -100,6 +100,11 @@ void NaClSignalHandlerInit(void);
 void NaClSignalHandlerFini(void);
 
 /*
+ * Assert that no signal handlers are registered.
+ */
+void NaClSignalAssertNoHandlers(void);
+
+/*
  * Provides a signal safe method to write to stderr.
  */
 ssize_t NaClSignalErrorMessage(const char *str);

src/trusted/service_runtime/posix/nacl_signal.c
@@ -263,3 +263,29 @@ void NaClSignalHandlerFiniPlatform() {
     }
   }
 }
+
+/*
+ * Check that signal handlers are not registered.  We want to
+ * discourage Chrome or libraries from registering signal handlers
+ * themselves, because those signal handlers are often not safe when
+ * triggered from untrusted code.  For background, see:
+ * http://code.google.com/p/nativeclient/issues/detail?id=1607
+ */
+void NaClSignalAssertNoHandlers() {
+  int index;
+  for (index = 0; index < SIGNAL_COUNT; index++) {
+    int signum = s_Signals[index];
+    struct sigaction sa;
+    if (sigaction(signum, NULL, &sa) != 0) {
+      NaClLog(LOG_FATAL, "NaClSignalAssertNoHandlers: "
+              "sigaction() call failed\n");
+    }
+    if ((sa.sa_flags & SA_SIGINFO) != 0
+        ? sa.sa_sigaction != NULL
+        : (sa.sa_handler != SIG_DFL && sa.sa_handler != SIG_IGN)) {
+      NaClLog(LOG_FATAL, "NaClSignalAssertNoHandlers: "
+              "A signal handler is registered for signal %d.  "
+              "Did Breakpad register this?\n", signum);
+    }
+  }
+}

src/trusted/service_runtime/sel_main.c
@@ -503,7 +503,11 @@ int main(int  argc,
   }
 
   /* Remove the signal handler if we are not using it. */
-  if (!handle_signals) NaClSignalHandlerFini();
+  if (!handle_signals) {
+    NaClSignalHandlerFini();
+    /* Sanity check. */
+    NaClSignalAssertNoHandlers();
+  }
 
   /*
    * Open both files first because (on Mac OS X at least)

src/trusted/service_runtime/win/nacl_signal.c
@@ -131,3 +131,10 @@ void NaClSignalHandlerFiniPlatform(void) {
   /* Remove our old catch if there is one, this allows us to add again */
   if (NULL != s_VEH) RemoveVectoredExceptionHandler(s_VEH);
 }
+
+void NaClSignalAssertNoHandlers() {
+  /*
+   * Windows has no direct equivalent of Unix signal handlers.  There
+   * is no interface for checking that no fault handlers are registered.
+   */
+}

commit 34a1b6fe9ebea9a6cd45b9f6c20a9f90da74191b

Disable NaClSignalAssertNoHandlers() on Mac
This check fails when NaCl is built into Chromium on Mac, presumably
because Chromium is registering a signal handler.  This is not as
dangerous on Mac as on Linux, since Mac OS X restores %gs when calling
a signal handler.  Disable the check for Mac for now.

This should fix the following error on the NaCl/Chrome integration
bot, introduced by r5004:

NaClSignalAssertNoHandlers: A signal handler is registered for signal
4.  Did Breakpad register this?

BUG=http://code.google.com/p/nativeclient/issues/detail?id=1607
TEST=none

Review URL: http://codereview.chromium.org/6880179

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5015 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: e3a754150272ef0782c639c47f4a7064c1b9983b

src/trusted/service_runtime/sel_main_chrome.c
@@ -111,8 +111,17 @@ int NaClMainForChromium(int handle_count, const NaClHandle *handles,
   /*
    * Check that Chrome did not register any signal handlers, because
    * these are not always safe.
+   *
+   * This check is disabled for Mac OS X because it currently does not
+   * pass there in Chromium, which registers a handler for at least
+   * SIGILL (signal 4).  Luckily, OS X restores %gs when entering a
+   * signal handler, so there should not be a vulnerability.
+   * TODO(mseaborn): However, we should reinstate the check for Mac to
+   * be on the safe side.
    */
-  NaClSignalAssertNoHandlers();
+  if (!NACL_OSX) {
+    NaClSignalAssertNoHandlers();
+  }
 
   /* Give debuggers a well known point at which xlate_base is known.  */
   NaClGdbHook(&state);