ceebe2606b8cd42d285f3bd054cc8d0c4be3dc22
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-04-28 23:16:47 +0000

    Toolchain downloader: Check for errors from subprocesses
    Fix code which failed to check for failures from os.spawnve() calls.
    Use subprocess.check_call() instead.
    
    Note that this means we don't need strange double-quoting when passing
    commands to Bash, since 'subprocess' handles this correctly.
    
    Add a comment to the obscure symlink-munging code.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1745
    TEST=run on trybots with "continue" commented out in download_toolchains.py
      to force the downloads to run
    
    Review URL: http://codereview.chromium.org/6903122
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5093 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: c8237e374976fc1424836a6a5dc7e8aad45443cb

--- build/sync_tgz.py
+++ build/sync_tgz.py
@@ -1,32 +1,7 @@
 #!/usr/bin/python2.4
-# Copyright 2009, Google Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#     * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
-# copyright notice, this list of conditions and the following disclaimer
-# in the documentation and/or other materials provided with the
-# distribution.
-#     * Neither the name of Google Inc. nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 """Keep a local directory in sync with a website tar file.
 
@@ -36,8 +11,10 @@ It supports username and password with basic authentication.
 
 import os
 import shutil
+import subprocess
 import sys
 import tarfile
+
 import http_download
 
 
@@ -82,20 +59,26 @@ def SyncTgz(url, target, compress='gzip', maindir='sdk',
     saveddir = os.getcwd()
     os.chdir(target)
     env = os.environ.copy()
-    env['LC_ALL']='C'
-    os.spawnve(os.P_WAIT, os.path.join('tmptar', 'bash.exe'),
-      ['/tmptar/bash', '-c', '"/tmptar/mkgroup -l -c > /etc/group"'], env)
-    os.spawnve(os.P_WAIT, os.path.join('tmptar', 'bash.exe'),
-      ['/tmptar/bash', '-c', '"/tmptar/mkpasswd -l -c > /etc/passwd"'], env)
-    os.spawnve(os.P_WAIT, os.path.join('tmptar', 'tar.exe'),
-      ['/tmptar/tar', '--use-compress-program', '/tmptar/' + compress,
-       '-xS' + verbosechar + 'pf', '../.tgz'], env)
-    os.spawnve(os.P_WAIT, os.path.join('tmptar', 'bash.exe'),
-      ['/tmptar/bash', '-c',
-       '"/tmptar/find -L ' + maindir + ' -type f -xtype l -print0 | ' +
-       'while IFS=\\"\\" read -r -d \\"\\" name; do if [[ -L \\"$name\\" ]];' +
-       'then /tmptar/ln -Tf \\"$(/tmptar/readlink -f \\"$name\\")\\" ' +
-       '\\"$name\\" ; fi ; done"'], env)
+    env['LC_ALL'] = 'C'
+    subprocess.check_call(
+        [os.path.join('tmptar', 'bash.exe'),
+         '-c', '/tmptar/mkgroup -l -c > /etc/group'], env=env)
+    subprocess.check_call(
+        [os.path.join('tmptar', 'bash.exe'),
+         '-c', '/tmptar/mkpasswd -l -c > /etc/passwd'], env=env)
+    subprocess.check_call(
+        [os.path.join('tmptar', 'tar.exe'),
+         '--use-compress-program', '/tmptar/' + compress,
+         '-xS' + verbosechar + 'pf', '../.tgz'], env=env)
+    # Convert symlinks to hard links.
+    subprocess.check_call(
+        [os.path.join('tmptar', 'bash.exe'),
+         '-c',
+         '/tmptar/find -L ' + maindir + ' -type f -xtype l -print0 | ' +
+         'while IFS="" read -r -d "" name; do ' +
+         'if [[ -L "$name" ]];' +
+         'then /tmptar/ln -Tf "$(/tmptar/readlink -f "$name")" ' +
+         '"$name" ; fi ; done'], env=env)
     os.chdir(saveddir)
     # Some antivirus software can prevent the removal - print message, but
     # don't stop.
@@ -118,13 +101,15 @@ def SyncTgz(url, target, compress='gzip', maindir='sdk',
           print "Can not rmdir %s: %s" % (os.path.join(target, 'tmptar'),
                                           e.strerror)
   elif sys.platform == 'linux2':
-    os.spawnv(os.P_WAIT, '/bin/tar',
-      ['tar', '-xS' + verbosechar + 'pf', tgz_filename, '-C', target])
+    subprocess.check_call(['tar', '-xS' + verbosechar + 'pf', tgz_filename,
+                           '-C', target])
   elif sys.platform == 'darwin':
     # TODO(khim): Replace with --warning=no-unknown-keyword when gnutar 1.23+
     # will be available.
-    os.spawnv(os.P_WAIT, '/bin/bash',
-      ['bash', '-c', '/usr/bin/gnutar -xS' + verbosechar + 'pf ' + tgz_filename + ' -C ' + target + ' 2> /dev/null'])
+    subprocess.check_call(
+        ['bash', '-c',
+         '/usr/bin/gnutar -xS' + verbosechar + 'pf ' + tgz_filename +
+         ' -C ' + target + ' 2> /dev/null'])
   else:
     tgz = tarfile.open(tgz_filename, 'r')
     for m in tgz:
