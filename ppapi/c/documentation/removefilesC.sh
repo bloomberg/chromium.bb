0c550be29c3a5487553c5ebc5f4dabd6379c1963
jond@google.com <jond@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 16:22:21 +0000

    Adding file to delete unused Doxygen files. Review URL: http://codereview.chromium.org/6901123
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83525 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 52bb01d16fb6427ddad5c4656c704c4a376e8267

---
+++ ppapi/c/documentation/removefilesC.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+rm annotated.ezt
+rm bc_s.png
+rm classes.ezt
+rm closed.png
+rm doxygen.png
+rm footer_8dox.ezt
+rm functions.ezt
+rm functions_vars.ezt
+rm globals.ezt
+rm globals_enum.ezt
+rm globals_eval.ezt
+rm globals_func.ezt
+rm globals_type.ezt
+rm globals_vars.ezt
+rm graph_legend.ezt
+rm graph_legend.png
+rm header_8dox.ezt
+rm index_8dox.ezt
+rm modules.ezt
+rm modules_8dox.ezt
+rm nav_f.png
+rm nav_h.png
+rm open.png
+rm tab_a.png
+rm tab_b.png
+rm tab_h.png
+rm tab_s.png
+# all .map and all .md5 files
+rm *.md5
+rm *.map
+rm *.css
+rm *.py
+rm files.ezt
+rm index.ezt
+
+
