084633e3bf2b02346d7ae8bfc7e69473eccc94e3
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 02:46:34 +0000

    Don't show error message for empty password.
    BUG=chromium-os:11597
    TEST=lock screen and hit enter w/o password. It shouldn't show message bubble.
    
    Review URL: http://codereview.chromium.org/6883247
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83481 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 70728fcca800dc70a6cbef77ccd7cc7b3d025a73

--- chrome/browser/chromeos/login/screen_locker.cc
+++ chrome/browser/chromeos/login/screen_locker.cc
@@ -863,6 +863,9 @@ void ScreenLocker::OnCaptchaEntered(const std::string& captcha) {
 }
 
 void ScreenLocker::Authenticate(const string16& password) {
+  if (password.empty())
+    return;
+
   authentication_start_time_ = base::Time::Now();
   screen_lock_view_->SetEnabled(false);
   screen_lock_view_->SetSignoutEnabled(false);
