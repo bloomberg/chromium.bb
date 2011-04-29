82411507da1cfabdd1b6454757c83e5f678389f3
kushi.p@gmail.com <kushi.p@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 18:38:03 +0000

    Updating logging in src/media/. Using DCHECK_EQ/NE/LE() where possible.
    BUG=58409
    TEST=None
    
    Review URL: http://codereview.chromium.org/6893032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83557 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e9d197715a43a79644e1c2a2bc4a641a1d2ad8c3

--- media/tools/player_wtl/view.h
+++ media/tools/player_wtl/view.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -195,7 +195,7 @@ class WtlVideoWindow : public CScrollWindowImpl<WtlVideoWindow> {
 
     bool enable_draw = media::Movie::GetInstance()->GetDrawEnable();
     if (enable_draw) {
-      DCHECK(bm.bmBitsPixel == 32);
+      DCHECK_EQ(32, bm.bmBitsPixel);
       DrawYUV(video_frame,
               movie_dib_bits,
               dibrowbytes,
