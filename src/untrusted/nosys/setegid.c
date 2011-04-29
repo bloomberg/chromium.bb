3a6618230802d3c8a8c976026082ba9aa421b62f
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-04-29 19:55:45 +0000

    Fix typo.  seteuid -> setegid
    This caused a duplicte symbol error when trying to merge all the components
    of libnosys into a single bitcode file.
    
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=1754
    Review URL: http://codereview.chromium.org/6902158
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5109 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 4b6fbd981701c32df1668ecc7437f158fdca2ea4

--- src/untrusted/nosys/setegid.c
+++ src/untrusted/nosys/setegid.c
@@ -1,7 +1,7 @@
 /*
- * Copyright 2010 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 
@@ -13,7 +13,7 @@
 #include <sys/types.h>
 #include <errno.h>
 
-int seteuid(uid_t euid) {
+int setegid(uid_t euid) {
   errno = ENOSYS;
   return -1;
 }
