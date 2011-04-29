b9357f85c6f0744c1df0357062868608271b813a
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 21:45:51 +0000

    MAC Cookies (2 of N)
    This CL contains the algorithmic guts of MAC cookies, including generating
    the canonical represntation of the request and signing it using HMAC.  This
    CL does not include support for body_hash, which requires some more thought.
    Review URL: http://codereview.chromium.org/6901121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83600 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4eb4f8ba70cd40195461d4981c90e55bc95c07d3

---
+++ net/http/http_mac_signature.h
@@ -0,0 +1,64 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_HTTP_MAC_SIGNATURE_H_
+#define NET_HTTP_MAC_SIGNATURE_H_
+#pragma once
+
+#include <string>
+
+#include "base/basictypes.h"
+#include "base/gtest_prod_util.h"
+#include "crypto/hmac.h"
+
+namespace net {
+
+class HttpMacSignature {
+ public:
+  HttpMacSignature();
+  ~HttpMacSignature();
+
+  // Returns whether this information is valid.
+  bool AddStateInfo(const std::string& id,
+                    const std::string& mac_key,
+                    const std::string& mac_algorithm,
+                    const std::string& issuer);
+
+  // Returns whether this information is valid.
+  bool AddHttpInfo(const std::string& method,
+                   const std::string& request_uri,
+                   const std::string& host,
+                   int port);
+
+  std::string GenerateAuthorizationHeader();
+
+ private:
+  FRIEND_TEST_ALL_PREFIXES(HttpMacSignatureTest, GenerateHeaderString);
+  FRIEND_TEST_ALL_PREFIXES(HttpMacSignatureTest, GenerateNormalizedRequest);
+  FRIEND_TEST_ALL_PREFIXES(HttpMacSignatureTest, GenerateMAC);
+
+  std::string GenerateHeaderString(const std::string& timestamp,
+                                   const std::string& nonce);
+  std::string GenerateNormalizedRequest(const std::string& timestamp,
+                                        const std::string& nonce);
+  std::string GenerateMAC(const std::string& timestamp,
+                          const std::string& nonce);
+
+  std::string id_;
+  std::string mac_key_;
+  crypto::HMAC::HashAlgorithm mac_algorithm_;
+  std::string issuer_;
+
+  std::string method_;
+  std::string request_uri_;
+  std::string host_;
+  std::string port_;
+  // TODO(abarth): body_hash_
+
+  DISALLOW_COPY_AND_ASSIGN(HttpMacSignature);
+};
+
+}  // namespace net
+
+#endif  // NET_HTTP_MAC_SIGNATURE_H_
