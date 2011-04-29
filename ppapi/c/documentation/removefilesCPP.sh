da00c39375ab178049da6a239f5797d6fe700aed
jond@google.com <jond@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-04-29 20:57:51 +0000

    Added removefilesCPP.sh to remove all unneeded html files generated by Doxygen. Removing three files not used by CPP doxygen process. Review URL: http://codereview.chromium.org/6903110
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83587 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: deaa35b7bad1d06f28ed394fb850b4f3dd7bc871

---
+++ ppapi/c/documentation/removefilesCPP.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+rm annotated.ezt
+rm bc_s.png
+rm classes.ezt
+rm closed.png
+rm doxygen.png
+rm footer_8dox.ezt
+rm functions*.*
+rm globals*.*
+rm group___c_p_p.ezt
+rm header_8dox.ezt
+rm index_8dox.ezt
+rm modules.ezt
+rm modules_8h.ezt
+rm module_embedder_8h.ezt
+rm module_impl_8h.ezt
+rm namespace*.*
+rm nav_f.png
+rm nav_h.png
+rm open.png
+rm tab_a.png
+rm tab_b.png
+rm tab_h.png
+rm tab_s.png
+# all .map and all .md5 files
+rm *.md5
+rm *.map
+rm *.css
+rm *.py
+rm index.ezt
+
+
