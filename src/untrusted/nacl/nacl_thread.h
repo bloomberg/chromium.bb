b288a393ec6a765bae4a3ac939eb631a2d07fadf
mcgrathr@chromium.org <mcgrathr@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-05-02 19:24:41 +0000

    Remove trivial wrappers for NaCl thread-related calls
    The only uses of these wrappers was libpthread, which now just uses the IRT
    ABI calls directly instead.  Remove the useless wrappers.  The tls wrappers
    are left because they are still used by the IRT itself too.
    
    BUG=cleanup
    TEST=trybots
    
    R=bradchen@google.com,mseaborn@chromium.org
    
    Review URL: http://codereview.chromium.org/6893160
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5123 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: dd1e33d04b9346a545edb3dc3059a8a6d2540e9a

--- src/untrusted/nacl/nacl_thread.h
+++ src/untrusted/nacl/nacl_thread.h
@@ -14,43 +14,8 @@
 extern "C" {
 #endif
 
-struct timespec;  /* Forward declaration.  */
-
-/*
- * Consistent with the style of <pthread.h> functions, all these functions
- * return zero for success or an errno code for failure.
- */
-
-extern int nacl_thread_create(void *start_user_address, void *stack,
-                              void *tdb, size_t tdb_size);
-#ifdef __GNUC__
-extern void nacl_thread_exit(int32_t *stack_flag) __attribute__((__noreturn__));
-#else
-extern void nacl_thread_exit(int32_t *stack_flag);
-#endif
-/*
- * The argument should be a NICE_* constant from <sys/nacl_nice.h>.
- */
-extern int nacl_thread_nice(const int nice);
-
 extern void *nacl_tls_get(void);
 extern int nacl_tls_init(void *tdb, size_t size);
-
-extern int nacl_mutex_create(int *mutex_handle);
-extern int nacl_mutex_lock(int mutex_handle);
-extern int nacl_mutex_unlock(int mutex_handle);
-extern int nacl_mutex_trylock(int mutex_handle);
-
-extern int nacl_cond_create(int *cond_handle);
-extern int nacl_cond_signal(int cond_handle);
-extern int nacl_cond_broadcast(int cond_handle);
-extern int nacl_cond_wait(int cond_handle, int mutex_handle);
-extern int nacl_cond_timed_wait_abs(int cond_handle, int mutex_handle,
-                                    const struct timespec *abstime);
-
-extern int nacl_sem_create(int *sem_handle, int32_t value);
-extern int nacl_sem_wait(int sem_handle);
-extern int nacl_sem_post(int sem_handle);
 
 #ifdef __cplusplus
 }
