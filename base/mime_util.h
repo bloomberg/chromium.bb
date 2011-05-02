0848d4f1ecfe53fe185edbbbf5434c60ec7f1d29
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-02 17:54:14 +0000

    linux: components build
    Reapply r83630, r83629, r83583, and fix the one compile error.
    
    TBR=rvargas
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83740 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 68a008e82da08b0bf7d421049f0a292b99b88048

--- base/mime_util.h
+++ base/mime_util.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,9 @@
 
 #include <string>
 
+#include "base/base_api.h"
+#include "build/build_config.h"
+
 class FilePath;
 
 namespace mime_util {
@@ -16,23 +19,23 @@ namespace mime_util {
 // have to exist. Please note because it doesn't touch the disk, this does not
 // work for directories.
 // If the mime type is unknown, this will return application/octet-stream.
-std::string GetFileMimeType(const FilePath& filepath);
+BASE_API std::string GetFileMimeType(const FilePath& filepath);
 
 // Get the mime type for a byte vector.
-std::string GetDataMimeType(const std::string& data);
+BASE_API std::string GetDataMimeType(const std::string& data);
 
 #if defined(TOOLKIT_GTK)
 // This detects the current GTK theme by calling gtk_settings_get_default().
 // It should only be executed on the UI thread and must be called before
 // GetMimeIcon().
-void DetectGtkTheme();
+BASE_API void DetectGtkTheme();
 #endif
 
 // Gets the file name for an icon given the mime type and icon pixel size.
 // Where an icon is a square image of |size| x |size|.
 // This will try to find the closest matching icon. If that's not available,
 // then a generic icon, and finally an empty FilePath if all else fails.
-FilePath GetMimeIcon(const std::string& mime_type, size_t size);
+BASE_API FilePath GetMimeIcon(const std::string& mime_type, size_t size);
 
 }  // namespace mime_util
 
--- sandbox/linux/suid/process_util.h
+++ sandbox/linux/suid/process_util.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,11 +11,13 @@
 #include <stdbool.h>
 #include <sys/types.h>
 
+#include "base/base_api.h"
+
 static const char kAdjustOOMScoreSwitch[] = "--adjust-oom-score";
 
 // This adjusts /proc/process/oom_adj so the Linux OOM killer will prefer
 // certain process types over others. The range for the adjustment is
 // [-17,15], with [0,15] being user accessible.
-bool AdjustOOMScore(pid_t process, int score);
+BASE_API bool AdjustOOMScore(pid_t process, int score);
 
 #endif  // SANDBOX_LINUX_SUID_PROCESS_UTIL_H_
