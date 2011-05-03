53e98536809f8cfc71d2085970db9e0b271a6575
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-03 23:19:22 +0000

    Marking the ChromeFrame.Launch CF test as flaky as it times out at times on the CF builders. Posting a delayed quit task to the message loop to ensure that this loop does not run forever.
    BUG=81479
    TBR=amit
    Review URL: http://codereview.chromium.org/6920003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83998 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bf0c841fb64b6e129d241670301229ea5cb7f9fb

--- chrome_frame/test/chrome_frame_automation_mock.cc
+++ chrome_frame/test/chrome_frame_automation_mock.cc
@@ -3,14 +3,20 @@
 // found in the LICENSE file.
 
 #include "chrome_frame/test/chrome_frame_automation_mock.h"
+#include "chrome_frame/test/chrome_frame_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 const int kLongWaitTimeout = 25 * 1000;
 const int kShortWaitTimeout = 5 * 1000;
 
-TEST(ChromeFrame, Launch) {
+// This test has been marked as flaky as it randomly times out on the CF
+// builders
+// http://code.google.com/p/chromium/issues/detail?id=81479
+TEST(ChromeFrame, FLAKY_Launch) {
   MessageLoopForUI loop;
   AutomationMockLaunch mock_launch(&loop, kLongWaitTimeout);
+
+  loop.PostDelayedTask(FROM_HERE, new MessageLoop::QuitTask, kLongWaitTimeout);
 
   mock_launch.Navigate("about:blank");
   loop.Run(NULL);
@@ -21,6 +27,8 @@ TEST(ChromeFrame, Navigate) {
   MessageLoopForUI loop;
   AutomationMockNavigate mock_navigate(&loop, kLongWaitTimeout);
 
+  loop.PostDelayedTask(FROM_HERE, new MessageLoop::QuitTask, kLongWaitTimeout);
+
   mock_navigate.NavigateRelativeFile(L"postmessage_basic_frame.html");
   loop.Run(NULL);
   EXPECT_FALSE(mock_navigate.navigation_result());
@@ -29,6 +37,8 @@ TEST(ChromeFrame, Navigate) {
 TEST(ChromeFrame, PostMessage) {
   MessageLoopForUI loop;
   AutomationMockPostMessage mock_postmessage(&loop, kLongWaitTimeout);
+
+  loop.PostDelayedTask(FROM_HERE, new MessageLoop::QuitTask, kLongWaitTimeout);
 
   mock_postmessage.NavigateRelativeFile(L"postmessage_basic_frame.html");
   loop.Run(NULL);
@@ -39,6 +49,8 @@ TEST(ChromeFrame, RequestStart) {
   MessageLoopForUI loop;
   AutomationMockHostNetworkRequestStart mock_request_start(&loop,
                                                            kLongWaitTimeout);
+
+  loop.PostDelayedTask(FROM_HERE, new MessageLoop::QuitTask, kLongWaitTimeout);
 
   mock_request_start.NavigateRelative(L"postmessage_basic_frame.html");
   loop.Run(NULL);
