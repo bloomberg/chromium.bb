dac31ded6875fb7cd3dd5e506973e19d138fffdd
sfeuz@google.com <sfeuz@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-03 17:06:41 +0000

    Fix: Hide bookmark bar for interstitial pages.
    BUG=64568
    TEST=Manually verify that bookmark bar is gone when visiting a ssl-page with a self-signed certificate. 
    
    
    Review URL: http://codereview.chromium.org/6912020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@83903 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7d68375d05452e8d3a139e969b52d149ec773c7a

--- content/browser/tab_contents/interstitial_page.cc
+++ content/browser/tab_contents/interstitial_page.cc
@@ -357,6 +357,11 @@ void InterstitialPage::DidNavigate(
   render_view_host_->view()->Show();
   tab_->set_interstitial_page(this);
 
+  // Hide the bookmark bar. Note that this has to happen after the interstitial
+  // page was registered with |tab_|, since there will be a callback to |tab_|
+  // testing if an interstitial page is showing before hiding the bookmark bar.
+  tab_->NotifyNavigationStateChanged(TabContents::INVALIDATE_BOOKMARK_BAR);
+
   NotificationService::current()->Notify(
       NotificationType::INTERSTITIAL_ATTACHED,
       Source<TabContents>(tab_),
