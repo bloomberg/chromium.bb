27c45056369984bbb55e60fb84759b2bfcc075a0
kschimpf@google.com <kschimpf@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-05-04 18:48:39 +0000

    Make ncval_stubout generate validator errors if stubbed out code invalid.
    For tests, see native_client/tests/stubout_mode/nacl.scons.
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=1695
    Review URL: http://codereview.chromium.org/6883230
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5159 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 03ffc94de9afc07195d9b7d96149be9f4dbc78e6

--- src/tools/validator_tools/ncstubout.c
+++ src/tools/validator_tools/ncstubout.c
@@ -18,7 +18,9 @@
 #include <string.h>
 
 #include "native_client/src/include/elf.h"
+#include "native_client/src/shared/gio/gio.h"
 #include "native_client/src/shared/platform/nacl_check.h"
+#include "native_client/src/shared/utils/types.h"
 #include "native_client/src/trusted/validator_x86/nacl_cpuid.h"
 
 #if NACL_TARGET_SUBARCH == 32
@@ -26,14 +28,20 @@
 # include "native_client/src/trusted/validator_x86/ncdecode.h"
 # include "native_client/src/trusted/validator_x86/ncvalidate.h"
 
-static void FixUpSection(uintptr_t load_address,
+static Bool FixUpSection(uintptr_t load_address,
                          unsigned char *code,
                          size_t code_size) {
+  int return_code;
   struct NCValidatorState *vstate;
   int bundle_size = 32;
   CPUFeatures features;
+
+  /* Start by stubbing out the code. */
   vstate = NCValidateInit(load_address, load_address + code_size, bundle_size);
-  CHECK(vstate != NULL);
+  if (vstate == NULL) {
+    fprintf(stderr, "Unable to stubout code, not enough memory\n");
+    return FALSE;
+  }
 
   NCValidateSetStubOutMode(vstate, 1);
 
@@ -46,21 +54,42 @@ static void FixUpSection(uintptr_t load_address,
   NCValidateSegment(code, load_address, code_size, vstate);
   NCValidateFinish(vstate);
   NCValidateFreeState(&vstate);
+
+  /* Now run the validator again, so that we report any errors
+   * that were not fixed by stubbing out. This is done so that
+   * the user knows that stubout doesn't fix all errors.
+   */
+  vstate = NCValidateInit(load_address, load_address + code_size, bundle_size);
+  if (vstate == NULL) {
+    fprintf(stderr, "Unable to stubout code, not enough memory\n");
+    return FALSE;
+  }
+  NCValidatorStateSetCPUFeatures(vstate, &features);
+  NCValidateSegment(code, load_address, code_size, vstate);
+  return_code = NCValidateFinish(vstate);
+  NCValidateFreeState(&vstate);
+  return return_code ? FALSE : TRUE;
 }
 
 #else
 
 # include "native_client/src/trusted/validator_x86/ncvalidate_iter.h"
 
-static void FixUpSection(uintptr_t load_address,
+static Bool FixUpSection(uintptr_t load_address,
                          unsigned char *code,
                          size_t code_size) {
   struct NaClValidatorState *vstate;
   int bundle_size = 32;
   CPUFeatures features;
+  Bool return_code;
+
+  /* Start by stubbing out the code. */
   vstate = NaClValidatorStateCreate(load_address, code_size, bundle_size,
                                     RegR15);
-  CHECK(vstate != NULL);
+  if (vstate == NULL) {
+    fprintf(stderr, "Unable to stubout code, not enough memory\n");
+    return FALSE;
+  }
   NaClValidatorStateSetDoStubOut(vstate, TRUE);
 
   /*
@@ -70,7 +99,24 @@ static void FixUpSection(uintptr_t load_address,
   NaClSetAllCPUFeatures(&features);
   NaClValidatorStateSetCPUFeatures(vstate, &features);
   NaClValidateSegment(code, load_address, code_size, vstate);
+  return_code = NaClValidatesOk(vstate);
   NaClValidatorStateDestroy(vstate);
+
+  /* Now run the validator again, so that we report any errors
+   * that were not fixed by stubbing out. This is done so that
+   * the user knows that stubout doesn't fix all errors.
+   */
+  vstate = NaClValidatorStateCreate(load_address, code_size, bundle_size,
+                                    RegR15);
+  if (vstate == NULL) {
+    fprintf(stderr, "Unable to stubout code, not enough memory\n");
+    return FALSE;
+  }
+  NaClValidatorStateSetCPUFeatures(vstate, &features);
+  NaClValidateSegment(code, load_address, code_size, vstate);
+  return_code = NaClValidatesOk(vstate);
+  NaClValidatorStateDestroy(vstate);
+  return return_code;
 }
 
 #endif
@@ -81,9 +127,10 @@ static void CheckBounds(unsigned char *data, size_t data_size,
   CHECK((unsigned char *) ptr + inside_size <= data + data_size);
 }
 
-static void FixUpELF(unsigned char *data, size_t data_size) {
+static Bool FixUpELF(unsigned char *data, size_t data_size) {
   Elf_Ehdr *header;
   int index;
+  Bool fixed = TRUE;  /* until proven otherwise. */
 
   header = (Elf_Ehdr *) data;
   CheckBounds(data, data_size, header, sizeof(*header));
@@ -97,13 +144,16 @@ static void FixUpELF(unsigned char *data, size_t data_size) {
     if ((section->sh_flags & SHF_EXECINSTR) != 0) {
       CheckBounds(data, data_size,
                   data + section->sh_offset, section->sh_size);
-      FixUpSection(section->sh_addr,
-                   data + section->sh_offset, section->sh_size);
+      if (!FixUpSection(section->sh_addr,
+                        data + section->sh_offset, section->sh_size)) {
+        fixed = FALSE;
+      }
     }
   }
+  return fixed;
 }
 
-static void FixUpELFFile(const char *input_file, const char *output_file) {
+static Bool FixUpELFFile(const char *input_file, const char *output_file) {
   FILE *fp;
   size_t file_size;
   unsigned char *data;
@@ -114,39 +164,57 @@ static void FixUpELFFile(const char *input_file, const char *output_file) {
   fp = fopen(input_file, "rb");
   if (fp == NULL) {
     fprintf(stderr, "Failed to open input file: %s\n", input_file);
-    exit(1);
+    return FALSE;
   }
   /* Find the file size. */
   fseek(fp, 0, SEEK_END);
   file_size = ftell(fp);
   data = malloc(file_size);
-  CHECK(data != NULL);
+  if (data == NULL) {
+    fprintf(stderr, "Unable to create memory imate of input file: %s\n",
+            input_file);
+    return FALSE;
+  }
   fseek(fp, 0, SEEK_SET);
   got = fread(data, 1, file_size, fp);
-  CHECK(got == file_size);
+  if (got != file_size) {
+    fprintf(stderr, "Unable to read data from input file: %s\n",
+            input_file);
+    return FALSE;
+  }
   fclose(fp);
 
-  FixUpELF(data, file_size);
+  if (!FixUpELF(data, file_size)) return FALSE;
 
   fp = fopen(output_file, "wb");
   if (fp == NULL) {
     fprintf(stderr, "Failed to open output file: %s\n", output_file);
-    exit(1);
+    return FALSE;
   }
   written = fwrite(data, 1, file_size, fp);
-  CHECK(written == file_size);
+  if (written != file_size) {
+    fprintf(stderr, "Unable to write data to output file: %s\n",
+            output_file);
+    return FALSE;
+  }
   fclose(fp);
+  return TRUE;
 }
 
 int main(int argc, const char *argv[]) {
+  /* Be sure to redirect validator error messages to stderr. */
+  struct GioFile err;
+  GioFileRefCtor(&err, stderr);
+  NaClLogPreInitSetGio((struct Gio*) &err);
   NaClLogModuleInit();
   if (argc != 4 || strcmp(argv[2], "-o") != 0) {
     fprintf(stderr, "Usage: %s <input-file> -o <output-file>\n\n", argv[0]);
     fprintf(stderr,
             "This tool rewrites ELF objects to replace instructions that are\n"
             "rejected by the NaCl validator with safe HLT instructions.\n");
+    GioFileDtor((struct Gio*) &err);
     return 1;
   }
-  FixUpELFFile(argv[1], argv[3]);
-  return 0;
+  GioFileDtor((struct Gio*) &err);
+  return FixUpELFFile(argv[1], argv[3]) ? 0 : 1;
 }
