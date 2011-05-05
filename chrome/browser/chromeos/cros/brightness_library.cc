403b62c4eb1735e8c4a4e8b4f578c94e2940f52a
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-05 01:23:51 +0000

    chromeos: Add functions to modify screen brightness.
    This adds BrightnessLibrary::IncreaseScreenBrightness()
    and DecreaseScreenBrightness().
    
    Contributed by Chris Wolfe <cwolfe@chromium.org>.  I am
    checking this in on his behalf from
    http://codereview.chromium.org/6928011/.
    
    BUG=chromium-os:13281
    TEST=cwolfe tested manually in conjunction with other changes that use these functions; i tested that it builds at ToT
    TBR=cwolfe
    
    Review URL: http://codereview.chromium.org/6935017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84183 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 66dea45f5b18a1352b0092e853d0e457dd1ab57b

--- chrome/browser/chromeos/cros/brightness_library.cc
+++ chrome/browser/chromeos/cros/brightness_library.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -25,6 +25,16 @@ class BrightnessLibraryImpl : public BrightnessLibrary {
       chromeos::DisconnectBrightness(brightness_connection_);
       brightness_connection_ = NULL;
     }
+  }
+
+  void DecreaseScreenBrightness(bool allow_off) {
+    if (chromeos::DecreaseScreenBrightness)
+      chromeos::DecreaseScreenBrightness(allow_off);
+  }
+
+  void IncreaseScreenBrightness() {
+    if (chromeos::IncreaseScreenBrightness)
+      chromeos::IncreaseScreenBrightness();
   }
 
   void AddObserver(Observer* observer) {
@@ -77,6 +87,8 @@ class BrightnessLibraryStubImpl : public BrightnessLibrary {
  public:
   BrightnessLibraryStubImpl() {}
   ~BrightnessLibraryStubImpl() {}
+  void DecreaseScreenBrightness(bool allow_off) {}
+  void IncreaseScreenBrightness() {}
   void AddObserver(Observer* observer) {}
   void RemoveObserver(Observer* observer) {}
 };
