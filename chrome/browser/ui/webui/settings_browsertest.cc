c1d424048e6a3c3d1dd2c8c1646e592869afaaa8
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-05 20:23:01 +0000

    Disable SettingsWebUITest.TestRefreshStaysOnCurrentPage on windows.
    BUG=81723
    TBR=sail@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84296 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2a8ca2c37f86064af5c07aab588fa8d2b30d2819

--- chrome/browser/ui/webui/settings_browsertest.cc
+++ chrome/browser/ui/webui/settings_browsertest.cc
@@ -111,7 +111,8 @@ IN_PROC_BROWSER_TEST_F(SettingsWebUITest, MAYBE_TestSetBooleanPrefTriggers) {
 }
 
 // Not meant to run on ChromeOS at this time.
-#if defined(OS_CHROMEOS) || defined(OS_MACOSX)
+// Not finishing in windows. http://crbug.com/81723
+#if defined(OS_CHROMEOS) || defined(OS_MACOSX) || defined (OS_WIN)
 #define MAYBE_TestRefreshStaysOnCurrentPage\
     DISABLED_TestRefreshStaysOnCurrentPage
 #else
