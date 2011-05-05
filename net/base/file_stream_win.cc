68ba288e30bf2e7a2faf1272e91a5bd766b3e4c7
kushi.p@gmail.com <kushi.p@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-05 09:59:28 +0000

    Using DCHECK_NE/EQ/LE/GE/GT/LT() where possible. Updating logging in src/net/base/
    Contributor: kushi.p@gmail.com
    
    BUG=58409
    
    
    Review URL: http://codereview.chromium.org/6905059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84228 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 52af5d521ef5d821672f7aaabdf9fd432e48a8cc

--- net/base/file_stream_win.cc
+++ net/base/file_stream_win.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -96,7 +96,7 @@ void FileStream::AsyncContext::IOCompletionIsPending(
 
 void FileStream::AsyncContext::OnIOCompleted(
     MessageLoopForIO::IOContext* context, DWORD bytes_read, DWORD error) {
-  DCHECK(&context_ == context);
+  DCHECK_EQ(&context_, context);
   DCHECK(callback_);
 
   if (is_closing_) {
