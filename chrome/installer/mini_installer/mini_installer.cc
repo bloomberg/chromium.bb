63b25854c6df1d35a6c85d0413012c5d220ea650
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-10 21:04:59 +0000

    Remove unnecessary include.
    TEST=n/a
    BUG=0
    
    Review URL: http://codereview.chromium.org/6981011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84850 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c383567127b0325fed41487d997c7391d7d8bb22

--- chrome/installer/mini_installer/mini_installer.cc
+++ chrome/installer/mini_installer/mini_installer.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2006-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -22,7 +22,6 @@
 #pragma comment(linker, "/MERGE:.rdata=.text")
 
 #include <windows.h>
-#include <setupapi.h>
 #include <shellapi.h>
 
 #include "chrome/installer/mini_installer/appid.h"
