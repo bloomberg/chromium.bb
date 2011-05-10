544d8a3ea557b2eb561693f40abe11804271ab54
dmaclach@chromium.org <dmaclach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-10 20:46:06 +0000

    Build a "real" webapp deliverable and add plugin to it.
    BUG=NONE
    TEST=BUILD
    
    Review URL: http://codereview.chromium.org/6998009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84843 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 03415f19c918a122cbb4aa696317570598cb4617

---
+++ remoting/webapp/build-webapp.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Copies the remoting webapp resources and host plugin into a single directory.
+
+import os
+import shutil
+import sys
+
+if len(sys.argv) != 4:
+  print 'Usage: build-webapp.py <webapp-resource-dir> <host-plugin> <dst>'
+  sys.exit(1)
+
+resources = sys.argv[1]
+plugin = sys.argv[2]
+destination = sys.argv[3]
+
+try:
+  shutil.rmtree(destination)
+except OSError:
+  if os.path.exists(destination):
+    raise
+  else:
+    pass
+
+shutil.copytree(resources, destination)
+pluginName = os.path.basename(plugin)
+newPluginPath = os.path.join(destination, pluginName)
+
+# On Mac we have a directory
+if os.path.isdir(plugin):
+  shutil.copytree(plugin, newPluginPath)
+else:
+  shutil.copy2(plugin, newPluginPath)
+
+# Now massage the manifest to the right plugin name
+templatePath = os.path.join(destination, 'manifest.json.template')
+manifestPath = os.path.join(destination, 'manifest.json')
+input = open(templatePath)
+output = open(manifestPath, 'w')
+for s in input:
+  output.write(s.replace('HOST_PLUGIN_NAME', pluginName))
+input.close()
+output.close()
+os.remove(templatePath)
