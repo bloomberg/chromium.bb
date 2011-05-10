177958f1f6412e153125a23141bfa27d812ab938
mcgrathr@chromium.org <mcgrathr@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-05-10 17:25:46 +0000

    Build core-only IRT blob
    This builds a second IRT image called irt_core.nexe, which does not
    include PPAPI.
    
    This might be part of a future plan or a transitional stage, but the
    related future choices are still undecided.  Regardless of how those
    come out, this provides another option for experimentation.
    
    BUG=none
    TEST=trybots
    
    R=bradchen@google.com,mseaborn@chromium.org,sehr@google.com
    
    Review URL: http://codereview.chromium.org/6976005
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5242 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 940add2fd56c01219ece26e731515f9d8dac805a

--- src/untrusted/irt/irt_interfaces.c
+++ src/untrusted/irt/irt_interfaces.c
@@ -27,7 +27,9 @@ static const struct nacl_interface_table irt_interfaces[] = {
   { NACL_IRT_SEM_v0_1, &nacl_irt_sem, sizeof(nacl_irt_sem) },
   { NACL_IRT_TLS_v0_1, &nacl_irt_tls, sizeof(nacl_irt_tls) },
   { NACL_IRT_BLOCKHOOK_v0_1, &nacl_irt_blockhook, sizeof(nacl_irt_blockhook) },
+#ifdef IRT_PPAPI
   { NACL_IRT_PPAPIHOOK_v0_1, &nacl_irt_ppapihook, sizeof(nacl_irt_ppapihook) },
+#endif
 };
 
 size_t nacl_irt_interface(const char *interface_ident,
---
+++ src/untrusted/irt/irt_interfaces_ppapi.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#define IRT_PPAPI 1
+#include "native_client/src/untrusted/irt/irt_interfaces.c"
