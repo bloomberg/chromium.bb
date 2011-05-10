f1ba6b9b43e9a8580ffaeb03082a4cac3325625e
wjia@google.com <wjia@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-10 21:03:22 +0000

    Adding a new file for media stream types that will be used by future Chrome changes to support video capture devices and the media stream api. Review URL: http://codereview.chromium.org/6980019
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84847 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 493b4afd5563f63d86ba8c5531c2ce8de8542559

---
+++ content/common/media_stream/media_stream_types.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_COMMON_MEDIA_STREAM_MEDIA_STREAM_TYPES_H_
+#define CONTENT_COMMON_MEDIA_STREAM_MEDIA_STREAM_TYPES_H_
+
+namespace media_stream {
+
+typedef int MediaCaptureSessionId;
+
+}  // namespace media_stream
+
+#endif  // CONTENT_COMMON_MEDIA_STREAM_MEDIA_STREAM_TYPES_H_
