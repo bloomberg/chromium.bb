1a3f2ced37f88fd634708bc75c559327998a09a9
khim@google.com <khim@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-05-11 12:51:01 +0000

    Fix newlib toolchain trybots.
    There are no boto keys on trybots and we don't really
    want to upload toolchain from trybots.
    Review URL: http://codereview.chromium.org/6995047
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5256 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 83018cb7210a6d77d8f8a0b24e74cc0656a80364

--- buildbot/buildbot_toolchain.sh
+++ buildbot/buildbot_toolchain.sh
@@ -43,36 +43,38 @@ echo @@@BUILD_STEP tar_toolchain@@@
 tar cvfz naclsdk.tgz sdk/
 chmod a+r naclsdk.tgz
 
-# Upload the toolchain before running the tests, in case the tests
-# fail.  We do not want a flaky test or a non-toolchain-related bug to
-# cause us to lose the toolchain snapshot, especially since this takes
-# so long to build on Windows.  We can always re-test a toolchain
-# snapshot on the trybots.
-echo @@@BUILD_STEP archive_build@@@
-( # Use the batch file as an entry point if on cygwin.
-  if [ "x${OSTYPE}" = "xcygwin" ]; then
-    # Use extended globbing (cygwin should always have it).
-    shopt -s extglob
-    # Filter out cygwin python (everything under /usr or /bin, or *cygwin*).
-    export PATH=${PATH/#\/bin*([^:])/}
-    export PATH=${PATH//:\/bin*([^:])/}
-    export PATH=${PATH/#\/usr*([^:])/}
-    export PATH=${PATH//:\/usr*([^:])/}
-    export PATH=${PATH/#*([^:])cygwin*([^:])/}
-    export PATH=${PATH//:*([^:])cygwin*([^:])/}
-    gsutil="${SCRIPT_DIR_ABS}/../../../../../scripts/slave/gsutil.bat"
-  else
-    gsutil=/b/build/scripts/slave/gsutil
-  fi
-  GS_BASE=gs://nativeclient-archive2/toolchain
-  "$gsutil" -h Cache-Control:no-cache cp -a public-read \
-    naclsdk.tgz \
-    ${GS_BASE}/${BUILDBOT_GOT_REVISION}/naclsdk_${PLATFORM}_x86.tgz
-  "$gsutil" -h Cache-Control:no-cache cp -a public-read \
-    naclsdk.tgz \
-    ${GS_BASE}/latest/naclsdk_${PLATFORM}_x86.tgz
-)
-echo @@@STEP_LINK@download@http://gsdview.appspot.com/nativeclient-archive2/toolchain/${BUILDBOT_GOT_REVISION}/@@@
+if [[ "${BUILDBOT_SLAVE_TYPE:-Trybot}" != "Trybot" ]]; then
+  # Upload the toolchain before running the tests, in case the tests
+  # fail.  We do not want a flaky test or a non-toolchain-related bug to
+  # cause us to lose the toolchain snapshot, especially since this takes
+  # so long to build on Windows.  We can always re-test a toolchain
+  # snapshot on the trybots.
+  echo @@@BUILD_STEP archive_build@@@
+  ( # Use the batch file as an entry point if on cygwin.
+    if [ "x${OSTYPE}" = "xcygwin" ]; then
+      # Use extended globbing (cygwin should always have it).
+      shopt -s extglob
+      # Filter out cygwin python (everything under /usr or /bin, or *cygwin*).
+      export PATH=${PATH/#\/bin*([^:])/}
+      export PATH=${PATH//:\/bin*([^:])/}
+      export PATH=${PATH/#\/usr*([^:])/}
+      export PATH=${PATH//:\/usr*([^:])/}
+      export PATH=${PATH/#*([^:])cygwin*([^:])/}
+      export PATH=${PATH//:*([^:])cygwin*([^:])/}
+      gsutil="${SCRIPT_DIR_ABS}/../../../../../scripts/slave/gsutil.bat"
+    else
+      gsutil=/b/build/scripts/slave/gsutil
+    fi
+    GS_BASE=gs://nativeclient-archive2/toolchain
+    "$gsutil" -h Cache-Control:no-cache cp -a public-read \
+      naclsdk.tgz \
+      ${GS_BASE}/${BUILDBOT_GOT_REVISION}/naclsdk_${PLATFORM}_x86.tgz
+    "$gsutil" -h Cache-Control:no-cache cp -a public-read \
+      naclsdk.tgz \
+      ${GS_BASE}/latest/naclsdk_${PLATFORM}_x86.tgz
+  )
+  echo @@@STEP_LINK@download@http://gsdview.appspot.com/nativeclient-archive2/toolchain/${BUILDBOT_GOT_REVISION}/@@@
+fi
 
 echo @@@BUILD_STEP untar_toolchain@@@
 mkdir -p ../toolchain/${PLATFORM}_x86_newlib/.tmp
