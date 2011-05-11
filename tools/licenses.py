c8105b38967e1f566f55cb5d73730d2ce18f1e65
ofri@google.com <ofri@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 19:50:21 +0000

    Suppressing license warnings for bidi checker
    BUG=NONE
    TEST=NONE
    
    
    Review URL: http://codereview.chromium.org/6982020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85020 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 78de2e4874658de56d48d5a4fcb9f6d22f011d89

--- tools/licenses.py
+++ tools/licenses.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -61,6 +61,7 @@ PRUNE_PATHS = set([
     os.path.join('v8','src','third_party','valgrind'),
 
     # Used for development and test, not in the shipping product.
+    os.path.join('third_party','bidichecker'),
     os.path.join('third_party','cygwin'),
     os.path.join('third_party','lighttpd'),
     os.path.join('third_party','mingw-w64'),
