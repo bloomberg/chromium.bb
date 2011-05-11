a626d6f47fec2d7fe7cc6ec965c1c87504ffbdf6
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 23:21:58 +0000

    If background thread gets XIOError, don't call shutdown
    BUG=82194
    TEST=None
    
    Review URL: http://codereview.chromium.org/7015013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85072 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 485c9bdda68c31147de3ceaf2996c51020a8f3bd

--- chrome/browser/browser_main_gtk.cc
+++ chrome/browser/browser_main_gtk.cc
@@ -39,6 +39,10 @@ namespace {
 // Indicates that we're currently responding to an IO error (by shutting down).
 bool g_in_x11_io_error_handler = false;
 
+// Number of seconds to wait for UI thread to get an IO error if we get it on
+// the background thread.
+const int kWaitForUIThreadSeconds = 10;
+
 int BrowserX11ErrorHandler(Display* d, XErrorEvent* error) {
   if (!g_in_x11_io_error_handler)
     MessageLoop::current()->PostTask(
@@ -47,7 +51,24 @@ int BrowserX11ErrorHandler(Display* d, XErrorEvent* error) {
   return 0;
 }
 
+
+// This function is used to help us diagnose crash dumps that happen
+// during the shutdown process.
+NOINLINE void WaitingForUIThreadToHandleIOError() {
+  // Ensure function isn't optimized away.
+  asm("");
+  sleep(kWaitForUIThreadSeconds);
+}
+
 int BrowserX11IOErrorHandler(Display* d) {
+  if (!BrowserThread::CurrentlyOn(BrowserThread::UI)) {
+    // Wait for the UI thread (which has a different connection to the X server)
+    // to get the error. We can't call shutdown from this thread without
+    // tripping an error. Doing it through a function so that we'll be able
+    // to see it in any crash dumps.
+    WaitingForUIThreadToHandleIOError();
+    return 0;
+  }
   // If there's an IO error it likely means the X server has gone away
   if (!g_in_x11_io_error_handler) {
     g_in_x11_io_error_handler = true;
