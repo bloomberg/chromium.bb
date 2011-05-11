3442f780a5146be632003031214ccbaff16a9050
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-11 16:19:43 +0000

    Fix windows browser_test failing due to UTF16 issue.
    BUG=82278,23581
    TEST=no
    TBR=evan
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@84987 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2143216d0f3858e34d8057473002df126c11c133

--- chrome/browser/ui/views/shell_dialogs_win.cc
+++ chrome/browser/ui/views/shell_dialogs_win.cc
@@ -143,9 +143,10 @@ std::wstring FormatFilterForExtensions(
         // based on the unknown extension type (i.e. if the extension is .qqq,
         // the we create a description "QQQ File (.qqq)").
         include_all_files = true;
-        desc = l10n_util::GetStringFUTF16(IDS_APP_SAVEAS_EXTENSION_FORMAT,
-                                          base::i18n::ToUpper(ext_name),
-                                          ext_name);
+        desc = l10n_util::GetStringFUTF16(
+            IDS_APP_SAVEAS_EXTENSION_FORMAT,
+            base::i18n::ToUpper(WideToUTF16(ext_name)),
+            ext_name);
       }
       if (desc.empty())
         desc = L"*." + ext_name;
