a8b8af8262c9a6291f2a78640dd129c88334dab1
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-13 00:57:20 +0000

    Disable AutofillAfterReload test for now.
    BUG=81451
    TEST=greener tree
    
    Review URL: http://codereview.chromium.org/6969052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85231 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 01f07bf53b3ae7af8511cfb5437837a031f49487

--- chrome/browser/autofill/autofill_browsertest.cc
+++ chrome/browser/autofill/autofill_browsertest.cc
@@ -382,9 +382,13 @@ IN_PROC_BROWSER_TEST_F(AutofillTest, AutofillFormsDistinguishedById) {
 
 // Test that form filling works after reloading the current page.
 // This test brought to you by http://crbug.com/69204
+#if defined(OS_MACOSX)
 // Sometimes times out on Mac: http://crbug.com/81451
-// Currently enabled to try to debug timeouts.
-IN_PROC_BROWSER_TEST_F(AutofillTest, AutofillAfterReload) {
+#define MAYBE_AutofillAfterReload DISABLED_AutofillAfterReload
+#else
+#define MAYBE_AutofillAfterReload AutofillAfterReload
+#endif
+IN_PROC_BROWSER_TEST_F(AutofillTest, MAYBE_AutofillAfterReload) {
   LOG(WARNING) << "Creating test profile.";
   CreateTestProfile();
 
