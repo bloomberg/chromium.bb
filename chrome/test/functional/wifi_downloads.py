e75e49b2d8210a3fc2afb2bcae34f57ff8565895
krisr@chromium.org <krisr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-13 20:42:21 +0000

    Add writing out the download time to a file Review URL: http://codereview.chromium.org/6973048
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85315 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7609182571ac521d6585a0cee5ed8d00af76f2b2

--- chrome/test/functional/wifi_downloads.py
+++ chrome/test/functional/wifi_downloads.py
@@ -22,6 +22,10 @@ class WifiDownloadsTest(chromeos_network.PyNetworkUITest):
      to the server 172.22.12.98:8080.  If the server is passed a filname in the
      format <integer>.lf, it will generate a file of size <integer> in KB.  In
      addition the name of the file returned is the md5 checksum of the file.
+
+     In addition the download times are written to a file in
+     /tmp/wifi_download_time.csv.  All times are appended to the file if it
+     already exists.
   """
 
   def setUp(self):
@@ -32,6 +36,50 @@ class WifiDownloadsTest(chromeos_network.PyNetworkUITest):
     self.PowerDownAllRouters()
     # Downloading files of a large size, can take a while, bump the timeout
     self.changer = pyauto.PyUITest.ActionTimeoutChanger(self, 4 * 1000 * 60)
+    self.log_file_path = '/tmp/wifi_download_time.csv'
+
+  def _WriteTimeToFile(self, output_file, router_name, file_size, dl_time):
+    """Write or append a time into a csv file.
+
+    This method will create or append the amount of time a download took for a
+    given filesize and router to a file at the given path.
+
+    The format of the output file is as follows:
+    <router name A>,<file size A>,time,time,time,time,time,time,time,time
+    <router name A>,<file size B>,time,time,time,time,time,time,time,time
+    <router name B>,<file size C>,time,time,time,time,time,time,time,time
+
+    Args:
+      output_file: the complete path of the file to write to
+      file_size: the size of the file, this is the row header
+      dl_time: the amount of time in seconds
+    """
+    file_data = []
+    if os.path.exists(output_file):
+      file_handle = open(output_file)
+      lines = file_handle.readlines()
+      for line in lines:
+        values = line.strip().split(',')
+        file_data.append(values)
+        found_existing_time = False
+        if values[0] == router_name and values[1] == file_size:
+          values.append('%2.2f' % dl_time)
+          found_existing_time = True
+          break
+      if not found_existing_time:
+        new_line = [router_name, file_size, ('%2.2f' % dl_time)]
+        file_data.append(new_line)
+      file_handle.close()
+    else:
+      file_data = [[router_name, file_size, ('%2.2f' % dl_time)]]
+    # Write the data back out
+    file_handle = open(output_file, 'w')
+    for line in file_data:
+      if len(line) > 2:
+        file_handle.write(','.join(line))
+        file_handle.write('\n')
+    file_handle.close()
+
 
   def _Md5Checksum(self, file_path):
     """Returns the md5 checksum of a file at a given path.
@@ -80,6 +128,9 @@ class WifiDownloadsTest(chromeos_network.PyNetworkUITest):
 
     Args:
       download_url: URL of the file to download.
+
+    Returns:
+      The download time in seconds.
     """
     start = time.time()
     # Make a copy of the download directory now to work around segfault
@@ -99,26 +150,36 @@ class WifiDownloadsTest(chromeos_network.PyNetworkUITest):
     md5_sum = self._Md5Checksum(file_path)
     self.assertEqual(filename, md5_sum, 'The checksums do not match.  The '
                      'download is incomplete.')
+    return end - start
 
   def testDownload1MBFile(self):
     """Test downloading a 1MB file from a wireless router."""
     download_url = 'http://172.22.12.98:8080/1024.lf'
-    self._ConnectToRouterAndVerify('Nfiniti')
-    self._DownloadAndVerifyFile(download_url)
+    router_name = 'Nfiniti'
+    self._ConnectToRouterAndVerify(router_name)
+    download_time = self._DownloadAndVerifyFile(download_url)
+    self._WriteTimeToFile(self.log_file_path, router_name, '1MB',
+                          download_time)
     self.DisconnectFromWifiNetwork()
 
   def testDownload10MBFile(self):
     """Test downloading a 10MB file from a wireless router."""
     download_url = 'http://172.22.12.98:8080/10240.lf'
-    self._ConnectToRouterAndVerify('Belkin_N+')
-    self._DownloadAndVerifyFile(download_url)
+    router_name = 'Belkin_N+'
+    self._ConnectToRouterAndVerify(router_name)
+    download_time = self._DownloadAndVerifyFile(download_url)
+    self._WriteTimeToFile(self.log_file_path, router_name, '10MB',
+                          download_time)
     self.DisconnectFromWifiNetwork()
 
   def testDownload100MBFile(self):
     """Test downloading a 100MB file from a wireless router."""
     download_url = 'http://172.22.12.98:8080/102400.lf'
-    self._ConnectToRouterAndVerify('Trendnet_639gr')
-    self._DownloadAndVerifyFile(download_url)
+    router_name = 'Trendnet_639gr'
+    self._ConnectToRouterAndVerify(router_name)
+    download_time = self._DownloadAndVerifyFile(download_url)
+    self._WriteTimeToFile(self.log_file_path, router_name, '100MB',
+                          download_time)
     self.DisconnectFromWifiNetwork()
 
 
