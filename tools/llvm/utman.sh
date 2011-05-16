b03c50b11232e6a1ff0a3b5bd017757db1e46300
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-05-16 22:26:18 +0000

    Update llvm stable rev to pull in:
    http://codereview.chromium.org/7035010/
    Review URL: http://codereview.chromium.org/7032011
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5326 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: de6ee68022f6679ef4258be926ab3428a0f772a2

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -174,7 +174,7 @@ SBTC_BUILD_WITH_PNACL="arm x8632 x8664"
 
 # Current milestones in each repo
 # hg-update-all uses these
-readonly LLVM_REV=bbaa8bbe8e9e
+readonly LLVM_REV=7726f48f8a4d
 readonly LLVM_GCC_REV=38c54bef2849
 readonly NEWLIB_REV=9bef47f82918
 readonly BINUTILS_REV=09eb0704d27f
