b9e3bd926fdedb9975ba2eb52ebc511ce3850ed1
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-05-16 22:07:26 +0000

    Remove two more refs to libnosys. One was not needed and the other one could be avoided.
    TEST= trybots
    Review URL: http://codereview.chromium.org/6910028
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5325 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 7ab4087606e94b3684481c583a19dadf5c929eb5

--- tests/file/file.cc
+++ tests/file/file.cc
@@ -1,7 +1,7 @@
 /*
- * Copyright 2008 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 
@@ -685,8 +685,10 @@ bool testSuite()
 
 void remove_testfiles()
 {
+#if !defined(__native_client__)
   remove("testdata256");
   remove("testdata.txt");
+#endif
 }
 
 
