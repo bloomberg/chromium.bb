64b49a3debc93ab14141d127f234f5a3cd7e7ada
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-16 00:53:29 +0000

    moar build fix TBR=dpolukhin
    Review URL: http://codereview.chromium.org/7029005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85449 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 75b96d83d7e4cca7def2343294078a96b249d366

--- chrome/browser/chromeos/login/mock_ownership_service.cc
+++ chrome/browser/chromeos/login/mock_ownership_service.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/chromeos/login/mock_ownership_service.h"
+
 namespace chromeos {
 
 MockOwnershipService::MockOwnershipService() {}
