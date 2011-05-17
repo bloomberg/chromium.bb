78b8a8636ae236c853747b8866078d303d1edb32
mcgrathr@chromium.org <mcgrathr@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-05-17 00:32:21 +0000

    Make pyauto failures log tab contents
    This makes the NaCl pyauto failure-detectors slurp the contents of the
    DOM as left by the JavaScript output and dump it into the build log.
    Without this, it is very hard to discern what went wrong in a test failure.
    
    BUG=none
    TEST=none
    
    R=ncbray@google.com
    
    Review URL: http://codereview.chromium.org/7029020
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5327 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 1d6a00691e9535e59cee889833c5b471921e9223

--- tests/pyauto_nacl/nacl_utils.py
+++ tests/pyauto_nacl/nacl_utils.py
@@ -6,6 +6,31 @@
 import pyauto_nacl  # Must be imported before pyauto
 import pyauto
 
+def AssertTrueOrLogTab(browser, ok, msg, tab_index=0):
+  """If passed False, scrapes the content of the tab and prints it before
+  throwing an axception.
+  """
+  try:
+    browser.assertTrue(ok, msg)
+  except AssertionError:
+    # NOTE!  A \n inside """ is a newline, not the text \n!
+    # Hence \\n to get \n into the JavaScript source.
+    # TODO(mcgrathr): Use getElementById after making the
+    # tests emit a DIV tag with an id attribute, diagnose failure to find it.
+    js = """\
+var bodyelt = document.documentElement.getElementsByTagName("div")[0];
+var elts = bodyelt.childNodes;
+var text = "*** " + elts.length + " lines:\\n";
+for (var i = 0; i < elts.length; ++i) {
+  text = text + "*** " + elts[i].innerHTML + "\\n";
+}
+window.domAutomationController.send(text);
+"""
+    print ('*** FAILED TEST ON TAB %u (%s)!  Log follows. ***' %
+           (tab_index, msg))
+    print browser.ExecuteJavascript(js, 0, tab_index),
+    print '*** END OF FAILED TAB LOG ***'
+    raise
 
 def WaitForNexeLoad(browser, tab_index=0):
   """Waits until a nexe has been fully loaded by the browser in the tab
@@ -13,11 +38,12 @@ def WaitForNexeLoad(browser, tab_index=0):
   calling this method. Also assumes that the nexe displays the string
   '[SHUTDOWN]' after fully loading.
   """
-  browser.assertTrue(browser.WaitUntil(
+  AssertTrueOrLogTab(browser, browser.WaitUntil(
       lambda:
-      browser.FindInPage('[SHUTDOWN]', tab_index=tab_index)['match_count'],
-      expect_retval=1))
-
+        browser.FindInPage('[SHUTDOWN]', tab_index=tab_index)['match_count'],
+      expect_retval=1),
+                     'nexe did not load',
+                     tab_index)
 
 def VerifyAllTestsPassed(browser, tab_index=0):
   """Returns true if all tests run by a nexe that was loaded by the browser in
@@ -25,5 +51,7 @@ def VerifyAllTestsPassed(browser, tab_index=0):
   assumes that the nexe displays the string '0 failed, 0 errors' when all tests
   have passed.
   """
-  browser.assertTrue(browser.FindInPage(
-      '0 failed, 0 errors', tab_index=tab_index)['match_count'] == 1)
+  AssertTrueOrLogTab(browser, browser.FindInPage(
+      '0 failed, 0 errors', tab_index=tab_index)['match_count'] == 1,
+                     'nexe test did not report success',
+                     tab_index)
