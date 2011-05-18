95ea46608a8e083ad48f65b4fb0e6ef8330580bb
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-18 21:23:39 +0000

    Marking the ContextMenuTest.CFSavePageAs and ContextMenuTest.CFSaveLinkAs tests as flaky.
    Increasing the timeout of these tests to see if that fixes the flakiness.
    
    BUG=83114
    TBR=amit
    Review URL: http://codereview.chromium.org/7039052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@85816 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bc53197d9cb1db97998efbb345a5fb2ec0d5c42e

--- chrome_frame/test/ui_test.cc
+++ chrome_frame/test/ui_test.cc
@@ -428,7 +428,8 @@ class ContextMenuTest : public MockIEEventSinkTest, public testing::Test {
     EXPECT_CALL(win_observer_mock, OnWindowClose(_))
         .WillOnce(CloseWhenFileSaved(&ie_mock_, temp_file_path, 5000));
 
-    LaunchIEAndNavigate(GetTestUrl(L"save_as_context_menu.html"));
+    LaunchIENavigateAndLoop(GetTestUrl(L"save_as_context_menu.html"),
+                            kChromeFrameVeryLongNavigationTimeoutInSeconds);
     ASSERT_TRUE(file_util::DieFileDie(temp_file_path, false));
   }
 
@@ -551,7 +552,8 @@ TEST_F(ContextMenuTest, CFInspector) {
                           kChromeFrameVeryLongNavigationTimeoutInSeconds);
 }
 
-TEST_F(ContextMenuTest, CFSavePageAs) {
+// http://code.google.com/p/chromium/issues/detail?id=83114
+TEST_F(ContextMenuTest, FLAKY_CFSavePageAs) {
   // Please see http://code.google.com/p/chromium/issues/detail?id=60987
   // for more information on why this test is disabled for Vista with IE7.
   if (base::win::GetVersion() == base::win::VERSION_VISTA &&
@@ -562,7 +564,8 @@ TEST_F(ContextMenuTest, CFSavePageAs) {
   ASSERT_NO_FATAL_FAILURE(DoSaveAsTest(L"", L"Save as...", L".html"));
 }
 
-TEST_F(ContextMenuTest, CFSaveLinkAs) {
+// http://code.google.com/p/chromium/issues/detail?id=83114
+TEST_F(ContextMenuTest, FLAKY_CFSaveLinkAs) {
   // Please see http://code.google.com/p/chromium/issues/detail?id=60987
   // for more information on why this test is disabled for Vista with IE7.
   if (base::win::GetVersion() == base::win::VERSION_VISTA &&
