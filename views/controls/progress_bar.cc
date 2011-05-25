327a5ceab2bf3df0f3c1f9b608e2a6bbc765aa21
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-25 09:23:43 +0000

    Fix SkIntToScalar conversion.
    This broke the Chrome OS bot http://build.chromium.org/p/chromium/builders/Linux%20Tests%20%28ChromiumOS%20dbg%29%281%29/builds/607/steps/browser_tests/logs/stdio
    
    BUG=no
    TEST=no
    TBR=ben@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86589 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f2d1ed493c380b16a4196e131fa4969a27a9148d

--- views/controls/progress_bar.cc
+++ views/controls/progress_bar.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -42,8 +42,8 @@ static void AddRoundRectPathWithPadding(int x, int y,
       SkIntToScalar(x + w) - padding, SkIntToScalar(y + h) - padding);
   path->addRoundRect(
       rect,
-      SkIntToScalar(corner_radius - padding),
-      SkIntToScalar(corner_radius - padding));
+      SkIntToScalar(corner_radius) - padding,
+      SkIntToScalar(corner_radius) - padding);
 }
 
 static void AddRoundRectPath(int x, int y,
