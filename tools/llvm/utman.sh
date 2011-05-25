2404aa4ed2b388312dc7bca5268d841ad0d5ff43
dschuff@google.com <dschuff@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-05-25 16:46:05 +0000

    Fix typo which borked utman.sh
    BUG=
    TEST=
    
    committing
    TBR=sehr
    
    Review URL: http://codereview.chromium.org/7068018
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5436 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 89b1c29045b5ca4d3c1a86fbc7b4aefb4dbef4ae

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -1,4 +1,4 @@
-fg#!/bin/bash
+#!/bin/bash
 # Copyright (c) 2011 The Native Client Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
