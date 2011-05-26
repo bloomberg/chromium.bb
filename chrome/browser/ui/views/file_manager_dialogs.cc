01668a09e559d4bf29cd47c4b5f1fb5cd4e00cd2
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 20:36:35 +0000

    CrOS - Fix crash on opening/saving file with file browser
    Users of SelectFileDialog may delete the object before the window is done closing, so we need to clean things up early.
    
    BUG=chromium-os:15758
    TEST=Use bookmarks manager to export and import a set of bookmarks.
    
    Review URL: http://codereview.chromium.org/6962020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86889 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bc1abde37db0a4fe6d4c7d6e3b50b34c969280c1

--- chrome/browser/ui/views/file_manager_dialogs.cc
+++ chrome/browser/ui/views/file_manager_dialogs.cc
@@ -3,9 +3,9 @@
 // found in the LICENSE file.
 
 #include "base/logging.h"
+#include "base/memory/ref_counted.h"
 #include "chrome/browser/extensions/extension_file_browser_private_api.h"
 #include "chrome/browser/extensions/file_manager_util.h"
-#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -13,7 +13,6 @@
 #include "chrome/browser/ui/views/window.h"
 #include "content/browser/browser_thread.h"
 #include "content/browser/tab_contents/tab_contents.h"
-#include "content/browser/user_metrics.h"
 #include "views/window/window.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/size.h"
@@ -52,8 +51,12 @@ class FileManagerDialog
                               void* params) OVERRIDE;
 
  private:
-  virtual ~FileManagerDialog() {}
+  virtual ~FileManagerDialog();
 
+  // Host for the extension that implements this dialog.
+  scoped_refptr<ExtensionDialog> extension_dialog_;
+
+  // ID of the tab that spawned this dialog, used to route callbacks.
   int32 tab_id_;
 
   gfx::NativeWindow owner_window_;
@@ -73,6 +76,11 @@ FileManagerDialog::FileManagerDialog(Listener* listener)
     : SelectFileDialog(listener),
       tab_id_(0),
       owner_window_(0) {
+}
+
+FileManagerDialog::~FileManagerDialog() {
+  extension_dialog_->ObserverDestroyed();
+  FileDialogFunction::Callback::Remove(tab_id_);
 }
 
 bool FileManagerDialog::IsRunning(gfx::NativeWindow owner_window) const {
@@ -109,12 +117,12 @@ void FileManagerDialog::SelectFileImpl(
   GURL file_browser_url = FileManagerUtil::GetFileBrowserUrlWithParams(
       type, title, default_path, file_types, file_type_index,
       default_extension);
-  ExtensionDialog* dialog = ExtensionDialog::Show(file_browser_url,
+  extension_dialog_ = ExtensionDialog::Show(file_browser_url,
       active_browser, kFileManagerWidth, kFileManagerHeight,
       this /* ExtensionDialog::Observer */);
 
   // Connect our listener to FileDialogFunction's per-tab callbacks.
-  Browser* extension_browser = dialog->host()->view()->browser();
+  Browser* extension_browser = extension_dialog_->host()->view()->browser();
   TabContents* contents = extension_browser->GetSelectedTabContents();
   int32 tab_id = (contents ? contents->controller().session_id().id() : 0);
   FileDialogFunction::Callback::Add(tab_id, listener_, params);
