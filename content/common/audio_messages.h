29fc9c40e26afdc073555c91ed994e3c4b90d196
annacc@chromium.org <annacc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 17:52:17 +0000

    Surround sound swizzling for Mac based on device channel layout preferences.
    This patch gets the surround sound channel layout from the user's device using CoreAudio.
    It then swizzles the channel layout from the source to match the user's prefered channel layout (if needed).
    
    BUG=none.
    TEST=none.
    
    
    Review URL: http://codereview.chromium.org/7047020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86851 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ca393c92c74b62915504e717a57e4169d27fdcd5

--- content/common/audio_messages.h
+++ content/common/audio_messages.h
@@ -16,6 +16,7 @@
 
 IPC_ENUM_TRAITS(AudioStreamState)
 IPC_ENUM_TRAITS(AudioParameters::Format)
+IPC_ENUM_TRAITS(ChannelLayout)
 
 IPC_STRUCT_TRAITS_BEGIN(AudioBuffersState)
   IPC_STRUCT_TRAITS_MEMBER(pending_bytes)
@@ -25,10 +26,11 @@ IPC_STRUCT_TRAITS_END()
 
 IPC_STRUCT_TRAITS_BEGIN(AudioParameters)
   IPC_STRUCT_TRAITS_MEMBER(format)
-  IPC_STRUCT_TRAITS_MEMBER(channels)
+  IPC_STRUCT_TRAITS_MEMBER(channel_layout)
   IPC_STRUCT_TRAITS_MEMBER(sample_rate)
   IPC_STRUCT_TRAITS_MEMBER(bits_per_sample)
   IPC_STRUCT_TRAITS_MEMBER(samples_per_packet)
+  IPC_STRUCT_TRAITS_MEMBER(channels)
 IPC_STRUCT_TRAITS_END()
 
 // Messages sent from the browser to the renderer.
