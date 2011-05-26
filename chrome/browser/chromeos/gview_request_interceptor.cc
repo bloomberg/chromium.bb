90b789dc20b6a4cb063cb5772ae37268641bbd34
willchan@chromium.org <willchan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 10:18:52 +0000

    Remove ProtocolFactory/Interceptor uses in GViewRequestInterceptor.
    Gets rid of more use of net/ globals, replacing with URLRequestJobFactory uses. Helps make net/ more thread-compatible.
    
    BUG=81979
    TEST=none
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=86802
    
    Review URL: http://codereview.chromium.org/7019030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86804 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a1f7bba412d50a3939f495011bdd3bdf30fb3bf5

--- chrome/browser/chromeos/gview_request_interceptor.cc
+++ chrome/browser/chromeos/gview_request_interceptor.cc
@@ -5,14 +5,12 @@
 #include "chrome/browser/chromeos/gview_request_interceptor.h"
 
 #include "base/file_path.h"
-#include "base/memory/singleton.h"
 #include "base/path_service.h"
 #include "chrome/common/chrome_paths.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/escape.h"
 #include "net/base/load_flags.h"
 #include "net/url_request/url_request.h"
-#include "net/url_request/url_request_job.h"
 #include "net/url_request/url_request_redirect_job.h"
 #include "webkit/glue/plugins/plugin_list.h"
 
@@ -21,37 +19,41 @@ namespace chromeos {
 // The PDF mime type is treated special if the browser has a built-in
 // PDF viewer plug-in installed - we want to intercept only if we're
 // told to.
-static const char* const kPdfMimeType = "application/pdf";
+static const char kPdfMimeType[] = "application/pdf";
 
 // This is the list of mime types currently supported by the Google
 // Document Viewer.
-static const char* const supported_mime_type_list[] = {
+static const char* const kSupportedMimeTypeList[] = {
   kPdfMimeType,
   "application/vnd.ms-powerpoint"
 };
 
-static const char* const kGViewUrlPrefix = "http://docs.google.com/gview?url=";
+static const char kGViewUrlPrefix[] = "http://docs.google.com/gview?url=";
 
 GViewRequestInterceptor::GViewRequestInterceptor() {
-  net::URLRequest::RegisterRequestInterceptor(this);
-  for (size_t i = 0; i < arraysize(supported_mime_type_list); ++i) {
-    supported_mime_types_.insert(supported_mime_type_list[i]);
+  for (size_t i = 0; i < arraysize(kSupportedMimeTypeList); ++i) {
+    supported_mime_types_.insert(kSupportedMimeTypeList[i]);
   }
 }
 
 GViewRequestInterceptor::~GViewRequestInterceptor() {
-  net::URLRequest::UnregisterRequestInterceptor(this);
 }
 
 net::URLRequestJob* GViewRequestInterceptor::MaybeIntercept(
-    net::URLRequest* request) {
+    net::URLRequest* request) const {
   // Don't attempt to intercept here as we want to wait until the mime
   // type is fully determined.
   return NULL;
 }
 
+net::URLRequestJob* GViewRequestInterceptor::MaybeInterceptRedirect(
+    const GURL& location,
+    net::URLRequest* request) const {
+  return NULL;
+}
+
 net::URLRequestJob* GViewRequestInterceptor::MaybeInterceptResponse(
-    net::URLRequest* request) {
+    net::URLRequest* request) const {
   // Do not intercept this request if it is a download.
   if (request->load_flags() & net::LOAD_IS_DOWNLOAD) {
     return NULL;
@@ -79,10 +81,6 @@ net::URLRequestJob* GViewRequestInterceptor::MaybeInterceptResponse(
     return new net::URLRequestRedirectJob(request, GURL(url));
   }
   return NULL;
-}
-
-GViewRequestInterceptor* GViewRequestInterceptor::GetInstance() {
-  return Singleton<GViewRequestInterceptor>::get();
 }
 
 }  // namespace chromeos
