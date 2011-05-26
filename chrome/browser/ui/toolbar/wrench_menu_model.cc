6fc25dd0b28505e8fc835b5941d1b06257d08e3a
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-05-26 22:08:29 +0000

    linux: move bookmark-related menus into bookmark menu
    Catching up with other platforms; removes some ifdefs.
    
    Review URL: http://codereview.chromium.org/7074021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@86907 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cf896f95430d7c72895e8ebd80fa25aa09ad2eac

--- chrome/browser/ui/toolbar/wrench_menu_model.cc
+++ chrome/browser/ui/toolbar/wrench_menu_model.cc
@@ -149,11 +149,6 @@ ToolsMenuModel::ToolsMenuModel(ui::SimpleMenuModel::Delegate* delegate,
 ToolsMenuModel::~ToolsMenuModel() {}
 
 void ToolsMenuModel::Build(Browser* browser) {
-#if !defined(OS_MACOSX) && !defined(TOOLKIT_VIEWS)
-  AddCheckItemWithStringId(IDC_SHOW_BOOKMARK_BAR, IDS_SHOW_BOOKMARK_BAR);
-  AddSeparator();
-#endif
-
 #if !defined(OS_CHROMEOS)
 #if defined(OS_MACOSX)
   AddItemWithStringId(IDC_CREATE_SHORTCUTS, IDS_CREATE_APPLICATION_MAC);
@@ -206,7 +201,11 @@ void BookmarkSubMenuModel::Build(Browser* browser) {
   AddCheckItemWithStringId(IDC_SHOW_BOOKMARK_BAR, IDS_SHOW_BOOKMARK_BAR);
   AddItemWithStringId(IDC_SHOW_BOOKMARK_MANAGER, IDS_BOOKMARK_MANAGER);
   AddItemWithStringId(IDC_IMPORT_SETTINGS, IDS_IMPORT_SETTINGS_TITLE);
+#if defined(OS_MACOSX) || defined(TOOLKIT_VIEWS)
   AddSeparator();
+#else
+  // TODO: add submenu for bookmarks themselves, restore separator.
+#endif
 }
 
 
@@ -452,13 +451,9 @@ void WrenchMenuModel::Build() {
 
   AddSeparator();
 
-#if defined(OS_MACOSX) || defined(TOOLKIT_VIEWS)
   bookmark_sub_menu_model_.reset(new BookmarkSubMenuModel(this, browser_));
   AddSubMenuWithStringId(IDC_BOOKMARKS_MENU, IDS_BOOKMARKS_MENU,
                          bookmark_sub_menu_model_.get());
-#else
-  AddItemWithStringId(IDC_SHOW_BOOKMARK_MANAGER, IDS_BOOKMARK_MANAGER);
-#endif
   AddItemWithStringId(IDC_SHOW_HISTORY, IDS_SHOW_HISTORY);
   AddItemWithStringId(IDC_SHOW_DOWNLOADS, IDS_SHOW_DOWNLOADS);
   AddSeparator();
