223ea6d959d912fda56b51552e92948a6f565d5f
halyavin@google.com <halyavin@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-05-26 11:26:49 +0000

    Use join with main thread to make test reliable.
    Join with main thread doesn't work in newlib, so I disable the test there.
    Review URL: http://codereview.chromium.org/7074006
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5462 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 58f760681b85a5d4902c5a5fe14f4b8a3445db39

--- tests/threads/main_thread_pthread_exit_test2.c
+++ tests/threads/main_thread_pthread_exit_test2.c
@@ -6,29 +6,32 @@
 
 /*
  * This test checks exiting main thread before other threads are finished.
- * Unfortunately, there is no way to join to main thread. So this test
- * sometimes fails to cover this scenario.
+ * Also this checks whether joining with main thread is working.
  */
 
 #include <errno.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/nacl_syscalls.h>
 #include <time.h>
 
+volatile pthread_t tmain;
+
 void* ThreadFunction(void* arg) {
-  struct timespec time;
   int rv;
-  time.tv_sec = 0;
-  time.tv_nsec = 500000000;
-  /* Sleep half a second taking early wakeups into account. */
-  while (-1 == (rv = nanosleep(&time, &time)) && EINTR == errno) ;
+  rv = pthread_join(tmain, NULL);
+  if (rv != 0) {
+    fprintf(stderr, "Error: can't join with main thread %d", rv);
+    exit(3);
+  }
   return NULL;
 }
 
 int main(void) {
   pthread_t tid;
   int rv;
+  tmain = pthread_self();
   rv = pthread_create(&tid, NULL, ThreadFunction, NULL);
   if (rv != 0) {
     fprintf(stderr, "Error: in thread creation %d\n", rv);
