02e1ffdefef7b3e3dd82198a0f2f7503e7883e9b
Samuel Rødal <samuel.rodal@nokia.com>
2011-05-30 17:23:40 +0200

    Fall back to accept() on systems where accept4() is not implemented.

--- wayland/wayland-server.c
+++ wayland/wayland-server.c
@@ -703,8 +703,14 @@ socket_data(int fd, uint32_t mask, void *data)
 	length = sizeof name;
 	client_fd =
 		accept4(fd, (struct sockaddr *) &name, &length, SOCK_CLOEXEC);
+	if (client_fd < 0 && errno == ENOSYS) {
+		client_fd = accept(fd, (struct sockaddr *) &name, &length);
+		if (client_fd >= 0 && fcntl(client_fd, F_SETFD, FD_CLOEXEC) == -1)
+			fprintf(stderr, "failed to set FD_CLOEXEC flag on client fd, errno: %d\n", errno);
+	}
+
 	if (client_fd < 0)
-		fprintf(stderr, "failed to accept\n");
+		fprintf(stderr, "failed to accept, errno: %d\n", errno);
 
 	wl_client_create(display, client_fd);
 
