418cd1edd11f1617f1d65a4f03ec258fbbbe3a9f
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-02 08:32:45 +0000

    Re-enable in-browser thumbnailing for Linux and Chrome OS.
    The last valgrind error was fixed by r87590.
    
    BUG=65936
    TEST=confirm that thumbnails in the new tab page are updated properly
    
    Review URL: http://codereview.chromium.org/6969091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87592 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d3a673263f3c3ced99d8b947a41c28a61c87c047

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1179,8 +1179,13 @@ bool IsPrintPreviewEnabled() {
 #endif
 
 bool IsInBrowserThumbnailingEnabled() {
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+  // The in-browser-thumbnailing is enabled for Linux and Chrome OS.
+  return true;
+#else
   return CommandLine::ForCurrentProcess()->HasSwitch(
       kEnableInBrowserThumbnailing);
+#endif
 }
 
 // -----------------------------------------------------------------------------
