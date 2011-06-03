9c450c3883d29c7fe6ebd1befb01b7206a2559a2
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-03 11:59:20 +0000

    Re-adds platform guard for OS_MACOSX, needed for ImageOperations.ResizeShouldAverageColors test.
    BUG=84837
    TEST=ImageOperations.ResizeShouldAverageColors
    TBR=tony@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87773 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e2b5abe9abaa29ad93523770d27215b1e3bcebdf

--- skia/ext/image_operations_unittest.cc
+++ skia/ext/image_operations_unittest.cc
@@ -506,7 +506,7 @@ TEST(ImageOperations, ResizeShouldAverageColors) {
     { skia::ImageOperations::RESIZE_HAMMING1, "HAMMING1", 0.0f },
     { skia::ImageOperations::RESIZE_LANCZOS2, "LANCZOS2", 0.0f },
     { skia::ImageOperations::RESIZE_LANCZOS3, "LANCZOS3", 0.0f },
-#if defined(OS_POSIX) && !defined(GTV)
+#if defined(OS_POSIX) && !defined(GTV) && !defined(OS_MACOSX)
     // SUBPIXEL has slightly worse performance than the other filters:
     //   6.324  Bottom left/right corners
     //   5.099  Top left/right corners
