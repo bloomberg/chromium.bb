6866c6e684d029e2e11b9c628bc8cd208c6296d1
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-04 00:14:25 +0000

    Fix a lock re-entry bug in GpuDataManager::UpdateGpuInfo.
    The issue is that the registered callbacks could request GPUInfo, so they could re-enter the lock.  Therefore, we should release the lock before we run through callbacks.
    
    BUG=84805
    TEST=the issue in 84805 is gone.
    Review URL: http://codereview.chromium.org/7054063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87898 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9400c64565586091f67d4131850fd0836b18b511

--- content/browser/gpu/gpu_data_manager.cc
+++ content/browser/gpu/gpu_data_manager.cc
@@ -49,8 +49,12 @@ void GpuDataManager::UpdateGpuInfo(const GPUInfo& gpu_info) {
     base::AutoLock auto_lock(gpu_info_lock_);
     if (!gpu_info_.Merge(gpu_info))
       return;
+  }
 
-    RunGpuInfoUpdateCallbacks();
+  RunGpuInfoUpdateCallbacks();
+
+  {
+    base::AutoLock auto_lock(gpu_info_lock_);
     content::GetContentClient()->SetGpuInfo(gpu_info_);
   }
 
