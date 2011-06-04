339599b4fca800b205d0aa331e48906a6bd2c866
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-04 08:04:22 +0000

    Disable strict mode for now until V8 bug 1423 is fixed.
    BUG=http://code.google.com/p/v8/issues/detail?id=1423
    TEST=Click the OAuth and email buttons on a 64-bit Linux machine. Chrome should not crash.
    
    
    Review URL: http://codereview.chromium.org/7112012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@87920 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5922825bc4daaf912aae54d6856c324a47ba8cb5

--- remoting/webapp/me2mom/remoting.js
+++ remoting/webapp/me2mom/remoting.js
@@ -2,7 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-"use strict";
+// TODO(jamiewalch): strict mode causes the page to crash so it's disabled for
+// now. Reinstate this when the associated bug is fixed.
+// http://code.google.com/p/v8/issues/detail?id=1423
+//"use strict";
 
 // TODO(ajwong): This seems like a bad idea to share the exact same object
 // with the background page.  Why are we doing it like this?
