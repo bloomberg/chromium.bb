3abc2be5ccbc273899ddf9add8fe6156589dccc4
bsy@google.com <bsy@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-06-06 20:50:01 +0000

    now that NaClApp objects no longer spawn service threads at NaClApp ctor time, races due to service threads are gone.  this change reverts the change to heap allocate NaClApp objects and leak memory -- we stack allocate again, since delayed thread launches means the tests no longer have threads holding improper references to deallocated objects. Review URL: http://codereview.chromium.org/7024050
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5566 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: be2affecd4a1da7b9ce5aab829a1f5de7402103a

--- src/trusted/service_runtime/sel_ldr_test.cc
+++ src/trusted/service_runtime/sel_ldr_test.cc
@@ -15,36 +15,35 @@
 #include "gtest/gtest.h"
 
 //
-// There are several bugs in how these tests are set up.
+// There are several problems in how these tests are set up.
 //
 // 1. NaCl modules such as the Log module are supposed to be
 // initialized at process startup and finalized at shutdown.  In
 // particular, there should not be any threads other than the main
 // thread running when the Log module initializes, since the verbosity
 // level is set then -- and thereafter it is assumed to be invariant
-// and read without acquring locks.
+// and read without acquring locks.  If any threads are left running
+// (e.g., NaClApp internal service threads), then race detectors would
+// legitimately report an error which is inappropriate because the
+// test is ignoring the API contract.
 //
 // 2. NaClApp objects, while they don't have a Dtor, are expected to
 // have a lifetime equal to that of the process that contain them.  In
 // particular, when the untrusted thread invokes the exit syscall, it
 // expects to be able to use _exit to exit, killing all other
 // untrusted threads as a side effect.  Furthermore, once a NaClApp
-// object is initialized, system service threads may be running holdin
-// references to the NaClApp object.  If the NaClApp object goes out
-// of scope or is otherwise destroyed and its memory freed, then these
-// system thread will probably access memory that is no longer valid.
+// object is initialized and NaClAppLaunchServiceThreads invoked,
+// system service threads are running holding references to the
+// NaClApp object.  If the NaClApp object goes out of scope or is
+// otherwise destroyed and its memory freed, then these system thread
+// may access memory that is no longer valid.  Tests cannot readily be
+// written to cleanly exercise the state space of a NaClApp after
+// NaClAppLaunchServiceThreads unless the test process exits---thereby
+// killing the service threads as a side-effect---when each individual
+// test is complete.
 //
-// These tests used to create NaClApp objects on the stack.  This
-// means that random system service threads have pointers into the
-// stack.  When a test function exits, the service threads now have a
-// pointer into memory that will be overwritten by the next test
-// function -- possibly to hold a different NaClApp object.  This
-// makes the test fail in a non-deterministic manner: if the system
-// service threads becomes quiescent before the main test thread
-// returns, then things will be fine.
-//
-// These tests now dynamically allocate the NaClApp objects.  This
-// means we leak memory.
+// These tests do not invoke NaClAppLaunchServiceThreads, so there
+// should be no service threads left running between tests.
 
 class SelLdrTest : public testing::Test {
  protected:
@@ -62,15 +61,13 @@ void SelLdrTest::TearDown() {
 
 // set, get, setavail operations on the descriptor table
 TEST_F(SelLdrTest, DescTable) {
-  struct NaClApp *app = (struct NaClApp *) malloc(sizeof *app);
+  struct NaClApp app;
   struct NaClHostDesc *host_desc;
   struct NaClDesc* io_desc;
   struct NaClDesc* ret_desc;
   int ret_code;
 
-  ASSERT_TRUE(NULL != app);
-
-  ret_code = NaClAppCtor(app);
+  ret_code = NaClAppCtor(&app);
   ASSERT_EQ(1, ret_code);
 
   host_desc = (struct NaClHostDesc *) malloc(sizeof *host_desc);
@@ -82,88 +79,84 @@ TEST_F(SelLdrTest, DescTable) {
   io_desc = (struct NaClDesc *) NaClDescIoDescMake(host_desc);
 
   // 1st pos available is 0
-  ret_code = NaClSetAvail(app, io_desc);
+  ret_code = NaClSetAvail(&app, io_desc);
   ASSERT_EQ(0, ret_code);
   // valid desc at pos 0
-  ret_desc = NaClGetDesc(app, 0);
+  ret_desc = NaClGetDesc(&app, 0);
   ASSERT_TRUE(NULL != ret_desc);
 
   // next pos available is 1
-  ret_code = NaClSetAvail(app, NULL);
+  ret_code = NaClSetAvail(&app, NULL);
   ASSERT_EQ(1, ret_code);
   // no desc at pos 1
-  ret_desc = NaClGetDesc(app, 1);
+  ret_desc = NaClGetDesc(&app, 1);
   ASSERT_TRUE(NULL == ret_desc);
 
   // no desc at pos 1 -> pos 1 is available
-  ret_code = NaClSetAvail(app, io_desc);
+  ret_code = NaClSetAvail(&app, io_desc);
   ASSERT_EQ(1, ret_code);
 
   // valid desc at pos 1
-  ret_desc = NaClGetDesc(app, 1);
+  ret_desc = NaClGetDesc(&app, 1);
   ASSERT_TRUE(NULL != ret_desc);
 
   // set no desc at pos 3
-  NaClSetDesc(app, 3, NULL);
+  NaClSetDesc(&app, 3, NULL);
 
   // valid desc at pos 4
-  NaClSetDesc(app, 4, io_desc);
-  ret_desc = NaClGetDesc(app, 4);
+  NaClSetDesc(&app, 4, io_desc);
+  ret_desc = NaClGetDesc(&app, 4);
   ASSERT_TRUE(NULL != ret_desc);
 
   // never set a desc at pos 10
-  ret_desc = NaClGetDesc(app, 10);
+  ret_desc = NaClGetDesc(&app, 10);
   ASSERT_TRUE(NULL == ret_desc);
 }
 
 // create service socket
 TEST_F(SelLdrTest, CreateServiceSocket) {
-  struct NaClApp *app = (struct NaClApp *) malloc(sizeof *app);
+  struct NaClApp app;
   int ret_code;
 
-  ASSERT_TRUE(NULL != app);
-
-  ret_code = NaClAppCtor(app);
+  ret_code = NaClAppCtor(&app);
   ASSERT_EQ(1, ret_code);
 
   // CreateServiceSocket sets the app service_port to a service port
   // desc and service_address to a service
-  ASSERT_TRUE(NULL == app->service_port);
-  ASSERT_TRUE(NULL == app->service_address);
-  NaClCreateServiceSocket(app);
-  ASSERT_TRUE(NULL != app->service_port);
-  ASSERT_TRUE(NULL != app->service_address);
+  ASSERT_TRUE(NULL == app.service_port);
+  ASSERT_TRUE(NULL == app.service_address);
+  NaClCreateServiceSocket(&app);
+  ASSERT_TRUE(NULL != app.service_port);
+  ASSERT_TRUE(NULL != app.service_address);
 }
 
 // add and remove operations on the threads table
 // Remove thread from an empty table is tested in a death test.
 // TODO(tuduce): specify the death test name when checking in.
 TEST_F(SelLdrTest, ThreadTableTest) {
-  struct NaClApp *app = (struct NaClApp *) malloc(sizeof *app);
+  struct NaClApp app;
   struct NaClAppThread nat, *appt=&nat;
   int ret_code;
 
-  ASSERT_TRUE(NULL != app);
-
-  ret_code = NaClAppCtor(app);
+  ret_code = NaClAppCtor(&app);
   ASSERT_EQ(1, ret_code);
 
   // 1st pos available is 0
-  ASSERT_EQ(0, app->num_threads);
-  ret_code = NaClAddThread(app, appt);
+  ASSERT_EQ(0, app.num_threads);
+  ret_code = NaClAddThread(&app, appt);
   ASSERT_EQ(0, ret_code);
-  ASSERT_EQ(1, app->num_threads);
+  ASSERT_EQ(1, app.num_threads);
 
   // next pos available is 1
-  ret_code = NaClAddThread(app, NULL);
+  ret_code = NaClAddThread(&app, NULL);
   ASSERT_EQ(1, ret_code);
-  ASSERT_EQ(2, app->num_threads);
+  ASSERT_EQ(2, app.num_threads);
 
   // no thread at pos 1 -> pos 1 is available
-  ret_code = NaClAddThread(app, appt);
+  ret_code = NaClAddThread(&app, appt);
   ASSERT_EQ(1, ret_code);
-  ASSERT_EQ(3, app->num_threads);
+  ASSERT_EQ(3, app.num_threads);
 
-  NaClRemoveThread(app, 0);
-  ASSERT_EQ(2, app->num_threads);
+  NaClRemoveThread(&app, 0);
+  ASSERT_EQ(2, app.num_threads);
 }
