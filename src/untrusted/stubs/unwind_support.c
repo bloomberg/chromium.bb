155c80839edafaad11296e4aeaa08c449c4544e5
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-06-07 14:15:37 +0000

    Abstracted another unwind primitive used by libgcc_eh into unwind_support.c
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=1520
    Review URL: http://codereview.chromium.org/7112037
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5571 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: ccbbdb413de14dda0f4bae72ad3521107ae27ace

--- src/untrusted/stubs/unwind_support.c
+++ src/untrusted/stubs/unwind_support.c
@@ -71,3 +71,32 @@ int pnacl_unwind_dwarf_frame_registers() {
   return -1;
 #endif
 }
+
+/* This corresponds to: __builtin_init_dwarf_reg_size_table() */
+/* TODO(robertm): this needs to be better documented */
+void pnacl_unwind_init_dwarf_reg_size_table(unsigned char* table) {
+  int i;
+  int n = pnacl_unwind_dwarf_frame_registers();
+#if defined(__x86_64__)
+  /* All regs are 64bit = 8 bytes */
+  for (i = 0; i < n; ++i ){
+    table[i] = 8;
+  }
+#elif defined(__i386__)
+  /* TODO(robetrm): find better documentation for these values */
+  for (i = 0; i <= 9; ++i ){
+    table[i] = 4;
+  }
+  table[10] = 0;   /* not a typo! register not used? */
+  for (i = 11; i < n; ++i ){
+    table[i] = 12;  /* fp registers ? */
+  }
+#elif defined(__arm__)
+    /* All regs are 32bit = 4 bytes */
+    for (i = 0; i < n; ++i ){
+    table[i] = 4;
+  }
+#else
+  #error "unknown platform"
+#endif
+}
