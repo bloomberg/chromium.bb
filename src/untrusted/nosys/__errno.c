92875eec6939544cbcf89760ef795d46456f5522
mcgrathr@chromium.org <mcgrathr@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-06-07 16:35:49 +0000

    Fix libnosys __errno linker warning
    The linker warning about using __errno should be attached to the __errno
    symbol, not to the errno symbol.  This makes the warning appear for
    references to the problematic function as intended.  It also avoids
    tickling an arcane BFD linker bug in unusual circumstances.
    
    BUG=none
    TEST=none
    
    R=robertm@google.com,khim@google.com
    
    Review URL: http://codereview.chromium.org/7024057
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5572 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: e99e63133c692fb2974ee53270eb4ca536b3c881

--- src/untrusted/nosys/__errno.c
+++ src/untrusted/nosys/__errno.c
@@ -1,9 +1,8 @@
 /*
- * Copyright 2010 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
-
 
 /*
  * Stub simple non-thread safe routine for `__errno' for porting support.
@@ -16,5 +15,5 @@ int *__errno() {
 }
 
 #include "native_client/src/untrusted/nosys/warning.h"
-link_warning(errno,
+link_warning(__errno,
   "the stub `__errno\' is not thread-safe, don\'t use it in multi-threaded environment");
