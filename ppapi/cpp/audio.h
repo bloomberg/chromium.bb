e73c0879a26301071e60d934c0494fa6de85fd66
jond@google.com <jond@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 20:00:44 +0000

    New documentation for audio.h and audio_config.h Review URL: http://codereview.chromium.org/6993025
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88186 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3be46e92b636f6d63884073ab48e6b10015b0d65

--- ppapi/cpp/audio.h
+++ ppapi/cpp/audio.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -11,20 +11,57 @@
 #include "ppapi/cpp/instance.h"
 #include "ppapi/cpp/resource.h"
 
+/// @file
+/// This file defines the API to create realtime stereo audio streaming
+/// capabilities.
+
 namespace pp {
 
+/// An audio resource. Refer to the
+/// <a href="chrome/nativeclient/docs/audio.html">Pepper
+/// Audio API Code Walkthrough</a> for information on using this interface.
 class Audio : public Resource {
  public:
+
+  /// An empty constructor for an Audio resource.
   Audio() {}
+
+  /// A constructor that creates an Audio resource. No sound will be heard
+  /// until StartPlayback() is called. The callback is called with the buffer
+  /// address and given user data whenever the buffer needs to be filled.
+  /// From within the callback, you should not call PPB_Audio functions.
+  /// The callback will be called on a different thread than the one which
+  /// created the interface. For performance-critical applications (i.e.
+  /// low-latency audio), the callback should avoid blocking or calling
+  /// functions that can obtain locks, such as malloc. The layout and the size
+  /// of the buffer passed to the audio callback will be determined by
+  /// the device configuration and is specified in the AudioConfig
+  /// documentation.
+  ///
+  /// @param[in] instance A pointer to an Instance indentifying one instance of
+  /// a module.
+  /// @param[in] config An AudioConfig containing the audio config resource.
+  /// @param[in] callback A PPB_Audio_Callback callback function that the
+  /// browser calls when it needs more samples to play.
+  /// @param[in] user_data A pointer to user data used in the callback function.
   Audio(Instance* instance,
         const AudioConfig& config,
         PPB_Audio_Callback callback,
         void* user_data);
 
+
+  /// Getter function for returning the internal PPB_AudioConfig struct
+  /// @return A mutable reference to the PPB_AudioConfig struct.
   AudioConfig& config() { return config_; }
+
+  /// Getter function for returning the internal PPB_AudioConfig struct.
+  /// @return A const reference to the internal PPB_AudioConfig struct.
   const AudioConfig& config() const { return config_; }
 
+  /// A function to start playback of audio.
   bool StartPlayback();
+
+  /// A function to stop playback of audio.
   bool StopPlayback();
 
  private:
--- ppapi/cpp/audio_config.h
+++ ppapi/cpp/audio_config.h
@@ -9,45 +9,98 @@
 #include "ppapi/c/pp_stdint.h"
 #include "ppapi/cpp/resource.h"
 
+
+/// @file
+/// This file defines the interface for establishing an
+/// audio configuration resource within the browser.
+
 namespace pp {
 
 class Instance;
 
-// Typical usage:
-//
-//   // Create an audio config with a supported frame count.
-//   uint32_t sample_frame_count = AudioConfig::RecommendSampleFrameCount(
-//       PP_AUDIOSAMPLERATE_44100, 4096);
-//   AudioConfig config(PP_AUDIOSAMPLERATE_44100, sample_frame_count);
-//   if (config.is_null())
-//     return false;  // Couldn't configure audio.
-//
-//   // Then use the config to create your audio resource.
-//   Audio audio(..., config, ...);
-//   if (audio.is_null())
-//     return false;  // Couldn't create audio.
+/// A 16 bit stereo AudioConfig resource. Refer to the
+/// <a href="/chrome/nativeclient/docs/audio.html">Pepper
+/// Audio API Code Walkthrough</a> for information on using this interface.
+///
+/// A single sample frame on a stereo device means one value for the left
+/// channel and one value for the right channel.
+///
+/// Buffer layout for a stereo int16 configuration:
+///
+/// int16_t *buffer16;
+/// buffer16[0] is the first left channel sample.
+/// buffer16[1] is the first right channel sample.
+/// buffer16[2] is the second left channel sample.
+/// buffer16[3] is the second right channel sample.
+/// ...
+/// buffer16[2 * (sample_frame_count - 1)] is the last left channel sample.
+/// buffer16[2 * (sample_frame_count - 1) + 1] is the last right channel
+/// sample.
+/// Data will always be in the native endian format of the platform.
+///
+/// <strong>Example:</strong>
+/// @code
+/// // Create an audio config with a supported frame count.
+/// uint32_t sample_frame_count = AudioConfig::RecommendSampleFrameCount(
+///    PP_AUDIOSAMPLERATE_44100, 4096);
+///  AudioConfig config(PP_AUDIOSAMPLERATE_44100, sample_frame_count);
+///  if (config.is_null())
+///    return false;  // Couldn't configure audio.
+///
+///   // Then use the config to create your audio resource.
+///  Audio audio(instance, config, callback, user_data);
+///   if (audio.is_null())
+///     return false;  // Couldn't create audio.
+/// @endcode
 class AudioConfig : public Resource {
  public:
+  /// An empty constructor for an AudioConfig resource.
   AudioConfig();
 
-  // Creates an audio config based on the given sample rate and frame count.
-  // If the rate and frame count aren't supported, the resulting resource
-  // will be is_null(). Pass the result of RecommendSampleFrameCount as the
-  // sample frame count.
-  //
-  // See PPB_AudioConfig.CreateStereo16Bit for more.
+  /// A constructor that creates an audio config based on the given sample rate
+  /// and frame count. If the rate and frame count aren't supported, the
+  /// resulting resource will be is_null(). You can pass the result of
+  /// RecommendSampleFrameCount() as the sample frame count.
+  ///
+  /// @param[in] instance A pointer to an Instance indentifying one instance of
+  /// a module.
+  /// @param[in] sample_rate A PP_AudioSampleRate which is either
+  /// PP_AUDIOSAMPLERATE_44100 or PP_AUDIOSAMPLERATE_48000.
+  /// @param[in] sample_frame_count A uint32_t frame count returned from the
+  /// RecommendSampleFrameCount function.
   AudioConfig(Instance* instance,
               PP_AudioSampleRate sample_rate,
               uint32_t sample_frame_count);
 
-  // Returns a supported frame count for use in the constructor.
-  //
-  // See PPB_AudioConfig.RecommendSampleFrameCount.
+  /// This function returns a supported frame count closest to the requested
+  /// count. The sample frame count determines the overall latency of audio.
+  /// Smaller frame counts will yield lower latency, but higher CPU
+  /// utilization. Supported sample frame counts will vary by hardware and
+  /// system (consider that the local system might be anywhere from a cell
+  /// phone or a high-end audio workstation). Sample counts less than
+  /// PP_AUDIOMINSAMPLEFRAMECOUNT and greater than PP_AUDIOMAXSAMPLEFRAMECOUNT
+  /// are never supported on any system, but values in between aren't
+  /// necessarily valid. This function will return a supported count closest to
+  /// the requested value for use in the constructor.
+  ///
+  /// @param[in] sample_rate A PP_AudioSampleRate which is either
+  /// PP_AUDIOSAMPLERATE_44100 or PP_AUDIOSAMPLERATE_48000.
+  /// @param[in] requested_sample_frame_count A uint_32t requested frame count.
+  /// If you pass 0 as the requested sample count, the recommended sample for
+  /// the local system is returned.
+  /// @return A uint32_t containing the recommended sample frame count if
+  /// successful. If the sample frame count or bit rate is not supported,
+  /// this function will fail and 0.
   static uint32_t RecommendSampleFrameCount(
       PP_AudioSampleRate sample_rate,
       uint32_t requested_sample_frame_count);
 
+  /// Getter function for returning the internal PP_AudioSampleRate enum.
+  /// @return The PP_AudioSampleRate enum.
   PP_AudioSampleRate sample_rate() const { return sample_rate_; }
+
+  /// Getter function for returning the internal sample frame count.
+  /// @return A uint32_t containing the sample frame count.
   uint32_t sample_frame_count() { return sample_frame_count_; }
 
  private:
