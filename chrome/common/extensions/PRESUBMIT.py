8b0ca88f34c5697fcfe3f16d8e638853ec722165
tessamac@chromium.org <tessamac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-07 23:03:32 +0000

    Only warn about extensions docs changes if they look incomplete.
    BUG=79003
    
    Review URL: http://codereview.chromium.org/7024035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88231 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8e1e1ba6c1db04111844195c6eded89865be88d1

--- chrome/common/extensions/PRESUBMIT.py
+++ chrome/common/extensions/PRESUBMIT.py
@@ -1,44 +1,204 @@
-#!/usr/bin/python
-# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os.path
+import os.path  # for initializing constants
 
-# When files in these directories are changed, we display a warning.
-DEPENDENT_DIRS = set([
-  os.path.normpath("chrome/common/extensions/api"),
-  os.path.normpath("chrome/common/extensions/docs")
-])
+# Directories that we run presubmit checks on.
+PRESUBMIT_PATH = os.path.normpath('chrome/common/extensions/PRESUBMIT.py')
+API_DIR = os.path.normpath('chrome/common/extensions/api')
+DOC_DIR = os.path.normpath('chrome/common/extensions/docs')
+BUILD_DIR = os.path.join(DOC_DIR, 'build')
+TEMPLATE_DIR = os.path.join(DOC_DIR, 'template')
+JS_DIR = os.path.join(DOC_DIR, 'js')
+CSS_DIR = os.path.join(DOC_DIR, 'css')
+STATIC_DIR = os.path.join(DOC_DIR, 'static')
+SAMPLES_DIR = os.path.join(DOC_DIR, 'examples')
 
-# Except for these directories.
-BLACKLIST_DIRS = set([
-  os.path.normpath("chrome/common/extensions/docs/server")
-])
+EXCEPTIONS = ['README', 'README.txt', 'OWNERS']
 
-REBUILD_WARNING = """
-This change modifies file(s) which the extension docs depend on. You must
-rebuild the extension docs.
+# Presubmit messages.
+README = os.path.join(DOC_DIR, 'README.txt')
+REBUILD_WARNING = (
+    'This change modifies the extension docs but the generated docs have '
+    'not been updated properly. See %s for more info. '
+    '(Report problems to tessamac@chromium.org.)' % README)
+BUILD_SCRIPT = os.path.join(BUILD_DIR, 'build.py')
+REBUILD_INSTRUCTIONS = (
+    'First build DumpRenderTree, then update the docs by running:\n  %s' %
+    BUILD_SCRIPT)
 
-Build by running the build.py script in chrome/common/extensions/docs/build/.
-
-Be sure to include any modified resulting static files
-(/common/extension/docs/*.html) in your final changelist. 
-"""
-
-def CheckChange(input_api, output_api):  
-  for f in input_api.AffectedFiles():
-    dir = os.path.normpath(input_api.os_path.dirname(f.LocalPath()))
-    while len(dir):
-      if dir in BLACKLIST_DIRS:
-        return []
-      if dir in DEPENDENT_DIRS:
-        return [output_api.PresubmitPromptWarning(REBUILD_WARNING)]
-      dir = os.path.dirname(dir)
-  return []
 
 def CheckChangeOnUpload(input_api, output_api):
-  return CheckChange(input_api, output_api)
+  return (CheckPresubmitChanges(input_api, output_api) +
+          CheckDocChanges(input_api, output_api))
 
 def CheckChangeOnCommit(input_api, output_api):
-  return CheckChange(input_api, output_api)
+  return (CheckPresubmitChanges(input_api, output_api) +
+          CheckDocChanges(input_api, output_api, strict=False))
+
+def CheckPresubmitChanges(input_api, output_api):
+  for PRESUBMIT_PATH in input_api.LocalPaths():
+    return input_api.canned_checks.RunUnitTests(input_api, output_api,
+                                                ['./PRESUBMIT_test.py'])
+  return []
+
+def CheckDocChanges(input_api, output_api, strict=True):
+  warnings = []
+
+  for af in input_api.AffectedFiles():
+    path = af.LocalPath()
+    if IsSkippedFile(path, input_api):
+      continue
+
+    elif (IsApiFile(path, input_api) or
+          IsBuildFile(path, input_api) or
+          IsTemplateFile(path, input_api) or
+          IsJsFile(path, input_api) or
+          IsCssFile(path, input_api)):
+      # These files do not always cause changes to the generated docs
+      # so we can ignore them if not running strict checks.
+      if strict and not DocsGenerated(input_api):
+        warnings.append('Docs out of sync with %s changes.' % path)
+
+    elif IsStaticDoc(path, input_api):
+      if not StaticDocBuilt(af, input_api):
+        warnings.append('Changes to %s not reflected in generated doc.' % path)
+
+    elif IsSampleFile(path, input_api):
+      if not SampleZipped(af, input_api):
+        warnings.append('Changes to sample %s have not been re-zipped.' % path)
+
+    elif IsGeneratedDoc(path, input_api):
+      if not NonGeneratedFilesEdited(input_api):
+        warnings.append('Changes to generated doc %s not reflected in '
+                        'non-generated files.' % path)
+
+  if warnings:
+    warnings.sort()
+    warnings = [' - %s\n' % w for w in warnings]
+    # Prompt user if they want to continue.
+    return [output_api.PresubmitPromptWarning(REBUILD_WARNING + '\n' +
+                                              ''.join(warnings) +
+                                              REBUILD_INSTRUCTIONS)]
+  return []
+
+def IsSkippedFile(path, input_api):
+  return input_api.os_path.basename(path) in EXCEPTIONS
+
+def IsApiFile(path, input_api):
+  return (input_api.os_path.dirname(path) == API_DIR and
+          path.endswith('.json'))
+
+def IsBuildFile(path, input_api):
+  return input_api.os_path.dirname(path) == BUILD_DIR
+
+def IsTemplateFile(path, input_api):
+  return input_api.os_path.dirname(path) == TEMPLATE_DIR
+
+def IsJsFile(path, input_api):
+  return (input_api.os_path.dirname(path) == JS_DIR and
+          path.endswith('.js'))
+
+def IsCssFile(path, input_api):
+  return (input_api.os_path.dirname(path) == CSS_DIR and
+          path.endswith('.css'))
+
+def IsStaticDoc(path, input_api):
+  return (input_api.os_path.dirname(path) == STATIC_DIR and
+          path.endswith('.html'))
+
+def IsSampleFile(path, input_api):
+  return input_api.os_path.dirname(path).startswith(SAMPLES_DIR)
+
+def IsGeneratedDoc(path, input_api):
+  return (input_api.os_path.dirname(path) == DOC_DIR and
+          path.endswith('.html'))
+
+def DocsGenerated(input_api):
+  """Return True if there are any generated docs in this change.
+
+  Generated docs are the files that are the output of build.py. Typically
+  all docs changes will contain both generated docs and non-generated files.
+  """
+  return any(IsGeneratedDoc(path, input_api)
+             for path in input_api.LocalPaths())
+
+def NonGeneratedFilesEdited(input_api):
+  """Return True if there are any non-generated files in this change.
+
+  Non-generated files are those that are the input to build.py. Typically
+  all docs changes will contain both non-generated files and generated docs.
+  """
+  return any(IsApiFile(path, input_api) or
+             IsBuildFile(path, input_api) or
+             IsTemplateFile(path, input_api) or
+             IsJsFile(path, input_api) or
+             IsCssFile(path, input_api) or
+             IsStaticDoc(path, input_api) or
+             IsSampleFile(path, input_api)
+             for path in input_api.LocalPaths())
+
+def StaticDocBuilt(static_file, input_api):
+  """Return True if the generated doc that corresponds to the |static_file|
+  is also in this change. Both files must also contain matching changes.
+  """
+  generated_file = _FindFileInAlternateDir(static_file, DOC_DIR, input_api)
+  return _ChangesMatch(generated_file, static_file)
+
+def _FindFileInAlternateDir(affected_file, alt_dir, input_api):
+  """Return an AffectFile for the file in |alt_dir| that corresponds to
+  |affected_file|.
+
+  If the file does not exist in the is change, return None.
+  """
+  alt_path = _AlternateFilePath(affected_file.LocalPath(), alt_dir, input_api)
+  for f in input_api.AffectedFiles():
+    if f.LocalPath() == alt_path:
+      return f
+
+def _AlternateFilePath(path, alt_dir, input_api):
+  """Return a path with the same basename as |path| but in |alt_dir| directory.
+
+  This is useful for finding corresponding static and generated docs.
+
+  Example:
+    _AlternateFilePath('/foo/bar', '/alt/dir', ...) == '/alt/dir/bar')
+  """
+  base_name = input_api.os_path.basename(path)
+  return input_api.os_path.join(alt_dir, base_name)
+
+def _ChangesMatch(affected_file1, affected_file2):
+  """Return True if the two files contain the same textual changes.
+
+  Line numbers may differ.
+  """
+  if not affected_file1 and not affected_file2:
+    return True  # Neither file affected.
+
+  if not affected_file1 or not affected_file2:
+    return False  # One file missing.
+
+  changes1 = affected_file1.ChangedContents()
+  changes2 = affected_file2.ChangedContents()
+  # ChangedContents() is a list of (line number, text) for all new lines.
+  # Ignore the line number, but check that the text for each new line matches.
+  if len(changes1) != len(changes2):
+    return False
+  for new_line in range(len(changes1)):
+    _, text1 = changes1[new_line]
+    _, text2 = changes2[new_line]
+    if text1 != text2:
+      return False
+  return True
+
+def SampleZipped(sample_file, input_api):
+  """Return True if the zipfile that should contain |sample_file| is in
+  this change.
+  """
+  sample_path = sample_file.LocalPath()
+  for af in input_api.AffectedFiles():
+    root, ext = input_api.os_path.splitext(af.LocalPath())
+    if ext == '.zip' and sample_path.startswith(root):
+      return True
+  return False
---
+++ chrome/common/extensions/PRESUBMIT_test.py
@@ -0,0 +1,386 @@
+#!/usr/bin/env python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+import os.path
+import PRESUBMIT
+
+
+class FakeInputApi(object):
+  os_path = os.path
+  def __init__(self, affected_files=[]):
+    self.affected_files = affected_files
+  def AffectedFiles(self):
+    return self.affected_files
+  def LocalPaths(self):
+    return [af.LocalPath() for af in self.AffectedFiles()]
+
+
+class FakeAffectedFile(object):
+  def __init__(self, local_path='', changed_contents=None):
+    self._local_path = os.path.normpath(local_path)
+    self._changed_contents = changed_contents
+  def LocalPath(self):
+    return self._local_path
+  def ChangedContents(self):
+    return self._changed_contents
+
+
+class FakeOutputApi(object):
+  def PresubmitPromptWarning(self, warning_msg):
+    return warning_msg
+
+
+class TestPresubmit(unittest.TestCase):
+  def setUp(self):
+    self.input_api = FakeInputApi()
+    self.output_api = FakeOutputApi()
+
+  def assertPathsEqual(self, path1, path2):
+    self.assertEqual(os.path.normpath(path1), os.path.normpath(path2))
+
+  def testCheckDocsChanges_NoFiles(self):
+    input_api = FakeInputApi(affected_files=[])
+    self.assertEqual([], PRESUBMIT.CheckDocChanges(input_api, self.output_api))
+
+  def testCheckDocsChanges_OnlyExcpetions(self):
+    input_api = FakeInputApi(affected_files=[
+        FakeAffectedFile(local_path='chrome/common/extensions/docs/README'),
+        FakeAffectedFile(local_path='chrome/common/extensions/docs/OWNERS')])
+    self.assertEqual([], PRESUBMIT.CheckDocChanges(input_api, self.output_api))
+
+  def testCheckDocsChanges_NoGeneratedFiles(self):
+    input_api = FakeInputApi(affected_files=[
+        FakeAffectedFile(
+            local_path='chrome/common/extensions/api/foo.json'),
+        FakeAffectedFile(
+            local_path='chrome/common/extensions/docs/examples/foo/b/bz.js'),
+        FakeAffectedFile(
+            local_path='chrome/common/extensions/docs/static/foo.html')])
+    expected_warning = (
+        'This change modifies the extension docs but the generated docs '
+        'have not been updated properly. See %s for more info. '
+        '(Report problems to tessamac@chromium.org.)\n'
+        ' - Changes to %s not reflected in generated doc.\n'
+        ' - Changes to sample %s have not been re-zipped.\n'
+        ' - Docs out of sync with %s changes.\n'
+        'First build DumpRenderTree, then update the docs by running:\n  %s' %
+        (os.path.normpath('chrome/common/extensions/docs/README.txt'),
+         os.path.normpath('chrome/common/extensions/docs/static/foo.html'),
+         os.path.normpath('chrome/common/extensions/docs/examples/foo/b/bz.js'),
+         os.path.normpath('chrome/common/extensions/api/foo.json'),
+         os.path.normpath('chrome/common/extensions/docs/build/build.py')))
+    self.assertEqual([expected_warning],
+                     PRESUBMIT.CheckDocChanges(input_api, self.output_api))
+
+  def testCheckDocsChanges_OnlyGeneratedDocs(self):
+    input_api = FakeInputApi(affected_files=[
+        FakeAffectedFile(local_path='chrome/common/extensions/docs/foo.html'),
+        FakeAffectedFile(local_path='chrome/common/extensions/docs/bar.html')])
+    expected_warning = (
+        'This change modifies the extension docs but the generated docs '
+        'have not been updated properly. See %s for more info. '
+        '(Report problems to tessamac@chromium.org.)\n'
+        ' - Changes to generated doc %s not reflected in non-generated files.\n'
+        ' - Changes to generated doc %s not reflected in non-generated files.\n'
+        'First build DumpRenderTree, then update the docs by running:\n  %s' %
+        (os.path.normpath('chrome/common/extensions/docs/README.txt'),
+         os.path.normpath('chrome/common/extensions/docs/bar.html'),
+         os.path.normpath('chrome/common/extensions/docs/foo.html'),
+         os.path.normpath('chrome/common/extensions/docs/build/build.py')))
+    self.assertEqual([expected_warning],
+                     PRESUBMIT.CheckDocChanges(input_api, self.output_api))
+
+  def testIsSkippedFile(self):
+    self.assertTrue(PRESUBMIT.IsSkippedFile(
+        os.path.normpath('chrome/common/extensions/docs/README.txt'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsSkippedFile(
+        os.path.normpath('chrome/common/extensions/OWNERS'), self.input_api))
+    self.assertTrue(PRESUBMIT.IsSkippedFile(
+        os.path.normpath('chrome/common/extensions/api/README'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsSkippedFile(
+        os.path.normpath('foo/bar/README.txt'), self.input_api))
+    self.assertFalse(PRESUBMIT.IsSkippedFile(
+        os.path.normpath('chrome/common/extensions/README/foo'),
+        self.input_api))
+
+  def testIsApiFile(self):
+    self.assertFalse(PRESUBMIT.IsApiFile(
+        os.path.normpath('chrome/common/extensions/docs/foo.html'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsApiFile(
+        os.path.normpath('chrome/common/extensions/api/foo.json'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsApiFile(
+        os.path.normpath('chrome/common/extensions/api/foo.html'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsApiFile(
+        os.path.normpath('chrome/common/extensions/docs/api/foo.html'),
+        self.input_api))
+
+  def testIsBuildFile(self):
+    self.assertTrue(PRESUBMIT.IsBuildFile(
+        os.path.normpath('chrome/common/extensions/docs/build/foo.py'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsBuildFile(
+        os.path.normpath('chrome/common/extensions/docs/build/bar.html'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsBuildFile(
+        os.path.normpath('chrome/common/extensions/docs/build.py'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsBuildFile(
+        os.path.normpath('chrome/common/extensions/build/foo.py'),
+        self.input_api))
+
+  def testIsTemplateFile(self):
+    self.assertTrue(PRESUBMIT.IsTemplateFile(
+        os.path.normpath('chrome/common/extensions/docs/template/foo.html'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsTemplateFile(
+        os.path.normpath('chrome/common/extensions/docs/template/bar.js'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsTemplateFile(
+        os.path.normpath('chrome/common/extensions/docs/template.html'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsTemplateFile(
+        os.path.normpath('chrome/common/extensions/template/foo.html'),
+        self.input_api))
+
+  def testIsStaticDoc(self):
+    self.assertFalse(PRESUBMIT.IsStaticDoc(
+        os.path.normpath('chrome/common/extensions/docs/foo.html'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsStaticDoc(
+        os.path.normpath('chrome/common/extensions/docs/static/foo.html'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsStaticDoc(
+        os.path.normpath('chrome/common/extensions/docs/api/foo.html'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsStaticDoc(
+        os.path.normpath('chrome/common/extensions/docs/static/foo.css'),
+        self.input_api))
+
+  def testIsSampleFile(self):
+    self.assertTrue(PRESUBMIT.IsSampleFile(
+        os.path.normpath(
+            'chrome/common/extensions/docs/examples/ex1/manifest.json'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsSampleFile(
+        os.path.normpath(
+            'chrome/common/extensions/docs/examples/ex1/images/icon.png'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsSampleFile(
+        os.path.normpath('chrome/common/extensions/docs/examples/dir'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsSampleFile(
+        os.path.normpath('chrome/common/extensions/docs/examples/dir/'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsSampleFile(
+        os.path.normpath(
+            'chrome/common/extensions/docs/samples/ex1/manifest.json'),
+        self.input_api))
+
+  def testIsJsFile(self):
+    self.assertFalse(PRESUBMIT.IsJsFile(
+        os.path.normpath('chrome/common/extensions/docs/foo.js'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsJsFile(
+        os.path.normpath('chrome/common/extensions/docs/js/foo.js'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsJsFile(
+        os.path.normpath('chrome/common/extensions/docs/js/foo.html'),
+        self.input_api))
+
+  def testIsCssFile(self):
+    self.assertFalse(PRESUBMIT.IsCssFile(
+        os.path.normpath('chrome/common/extensions/docs/foo.css'),
+        self.input_api))
+    self.assertTrue(PRESUBMIT.IsCssFile(
+        os.path.normpath('chrome/common/extensions/docs/css/foo.css'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsCssFile(
+        os.path.normpath('chrome/common/extensions/docs/css/foo.html'),
+        self.input_api))
+
+  def testIsGeneratedDoc(self):
+    self.assertTrue(PRESUBMIT.IsGeneratedDoc(
+        os.path.normpath('chrome/common/extensions/docs/foo.html'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsGeneratedDoc(
+        os.path.normpath('chrome/common/extensions/docs/static/foo.html'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsGeneratedDoc(
+        os.path.normpath('chrome/common/extensions/docs/api/foo.html'),
+        self.input_api))
+    self.assertFalse(PRESUBMIT.IsGeneratedDoc(
+        os.path.normpath('chrome/common/extensions/docs/foo.css'),
+        self.input_api))
+
+  def testDocsGenerated_SomeGeneratedDocs(self):
+    api_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/api/foo.json')
+    input_api = FakeInputApi(affected_files=[
+        FakeAffectedFile(local_path='chrome/common/extensions/docs/foo.html'),
+        FakeAffectedFile(local_path='chrome/common/extensions/docs/bar.html')])
+    self.assertTrue(PRESUBMIT.DocsGenerated(input_api))
+
+  def testDocsGenerated_NoGeneratedDocs(self):
+    api_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/api/foo.json')
+    input_api = FakeInputApi(affected_files=[api_file])
+    self.assertFalse(PRESUBMIT.DocsGenerated(input_api))
+
+  def testNonGeneratedFilesEdited_SomeNonGeneratedFiles(self):
+    input_api = FakeInputApi(affected_files=[
+        FakeAffectedFile(local_path='chrome/common/extensions/api/foo.json'),
+        FakeAffectedFile(
+            local_path='chrome/common/extensions/docs/static/index.html')])
+    self.assertTrue(PRESUBMIT.NonGeneratedFilesEdited(input_api))
+
+  def testNonGeneratedFilesEdited_ZeroNonGeneratedFiles(self):
+    input_api = FakeInputApi(affected_files=[
+        FakeAffectedFile(local_path='chrome/common/extensions/docs/one.html'),
+        FakeAffectedFile(local_path='chrome/common/extensions/docs/two.html')])
+    self.assertFalse(PRESUBMIT.NonGeneratedFilesEdited(input_api))
+
+  def testStaticDocBuilt_ChangesMatch(self):
+    static_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/static/index.html',
+        changed_contents=[(3, 'foo!'), (4, 'bar!')])
+    generated_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/index.html',
+        changed_contents=[(13, 'foo!'), (14, 'bar!')])
+    input_api = FakeInputApi(affected_files=[generated_file, static_file])
+
+    self.assertTrue(PRESUBMIT.StaticDocBuilt(static_file, input_api))
+
+  def testStaticDocBuilt_OnlyStaticChanged(self):
+    static_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/static/index.html',
+        changed_contents=[(3, 'foo!'), (4, 'bar!')])
+    other_static_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/static/next.html',
+        changed_contents=[(3, 'double foo!'), (4, 'double bar!')])
+    input_api = FakeInputApi(affected_files=[static_file, other_static_file])
+
+    self.assertFalse(PRESUBMIT.StaticDocBuilt(static_file, input_api))
+    self.assertFalse(PRESUBMIT.StaticDocBuilt(other_static_file, input_api))
+
+  def testStaticDocBuilt_ChangesDoNotMatch(self):
+    static_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/static/index.html',
+        changed_contents=[(3, 'double foo!'), (4, 'double bar!')])
+    generated_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/index.html',
+        changed_contents=[(13, 'foo!'), (14, 'bar!')])
+    input_api = FakeInputApi(affected_files=[generated_file, static_file])
+
+    self.assertFalse(PRESUBMIT.StaticDocBuilt(static_file, input_api))
+
+  def testAlternateFilePath(self):
+    path = os.path.normpath('foo/bar/baz.html')
+    alt_dir = os.path.normpath('woop/dee/doo')
+    self.assertPathsEqual('woop/dee/doo/baz.html',
+                          PRESUBMIT._AlternateFilePath(path, alt_dir,
+                                                       self.input_api))
+    alt_dir = 'foo'
+    self.assertPathsEqual('foo/baz.html',
+                          PRESUBMIT._AlternateFilePath(path, alt_dir,
+                                                       self.input_api))
+    alt_dir = ''
+    self.assertPathsEqual('baz.html',
+                          PRESUBMIT._AlternateFilePath(path, alt_dir,
+                                                       self.input_api))
+
+  def testFindFileInAlternateDir(self):
+    input_api = FakeInputApi(affected_files=[
+        FakeAffectedFile(local_path='dir/1.html'),
+        FakeAffectedFile(local_path='dir/2.html'),
+        FakeAffectedFile(local_path='dir/alt/1.html'),
+        FakeAffectedFile(local_path='dir/alt/3.html')])
+    alt_dir = os.path.normpath('dir/alt')
+
+    # Both files in affected files.
+    alt_file = PRESUBMIT._FindFileInAlternateDir(
+        FakeAffectedFile(local_path='dir/1.html'), alt_dir, input_api)
+    self.assertTrue(alt_file)
+    self.assertPathsEqual('dir/alt/1.html', alt_file.LocalPath())
+
+    # Given file not in affected files.
+    alt_file = PRESUBMIT._FindFileInAlternateDir(
+        FakeAffectedFile(local_path='dir/3.html'), alt_dir, input_api)
+    self.assertTrue(alt_file)
+    self.assertPathsEqual('dir/alt/3.html', alt_file.LocalPath())
+
+    # Alternate file not found.
+    alt_file = PRESUBMIT._FindFileInAlternateDir(
+        FakeAffectedFile(local_path='dir/2.html'), alt_dir, input_api)
+    self.assertFalse(alt_file)
+
+  def testChangesMatch_Same(self):
+    af1 = FakeAffectedFile(changed_contents=[(1, 'foo'), (14, 'bar')])
+    af2 = FakeAffectedFile(changed_contents=[(1, 'foo'), (14, 'bar')])
+    self.assertTrue(PRESUBMIT._ChangesMatch(af1, af2))
+    self.assertTrue(PRESUBMIT._ChangesMatch(af2, af1))
+
+  def testChangesMatch_OneFileNone(self):
+    af1 = FakeAffectedFile(changed_contents=[(1, 'foo'), (14, 'bar')])
+    af2 = None
+    self.assertFalse(PRESUBMIT._ChangesMatch(af1, af2))
+    self.assertFalse(PRESUBMIT._ChangesMatch(af2, af1))
+
+  def testChangesMatch_BothFilesNone(self):
+    af1 = None
+    af2 = None
+    self.assertTrue(PRESUBMIT._ChangesMatch(af1, af2))
+    self.assertTrue(PRESUBMIT._ChangesMatch(af2, af1))
+
+  def testChangesMatch_DifferentLineNumbers(self):
+    af1 = FakeAffectedFile(changed_contents=[(1, 'foo'), (14, 'bar')])
+    af2 = FakeAffectedFile(changed_contents=[(1, 'foo'), (15, 'bar')])
+    self.assertTrue(PRESUBMIT._ChangesMatch(af1, af2))
+    self.assertTrue(PRESUBMIT._ChangesMatch(af2, af1))
+
+  def testChangesMatch_DifferentLength(self):
+    af1 = FakeAffectedFile(changed_contents=[(1, 'foo'), (14, 'bar')])
+    af2 = FakeAffectedFile(changed_contents=[(14, 'bar')])
+    self.assertFalse(PRESUBMIT._ChangesMatch(af1, af2))
+    self.assertFalse(PRESUBMIT._ChangesMatch(af2, af1))
+
+  def testChangesMatch_SameLengthButDifferentText(self):
+    af1 = FakeAffectedFile(changed_contents=[(1, 'foo'), (14, 'bar')])
+    af2 = FakeAffectedFile(changed_contents=[(1, 'foo'), (14, 'barbaz')])
+    self.assertFalse(PRESUBMIT._ChangesMatch(af1, af2))
+    self.assertFalse(PRESUBMIT._ChangesMatch(af2, af1))
+
+  def testSampleZipped_ZipInAffectedFiles(self):
+    sample_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/examples/foo/bar/baz.jpg')
+    zip_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/examples/foo/bar.zip')
+    input_api = FakeInputApi(affected_files=[sample_file, zip_file])
+    self.assertTrue(PRESUBMIT.SampleZipped(sample_file, input_api))
+
+  def testSampleZipped_NoZip(self):
+    sample_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/examples/foo/bar.jpg')
+    input_api = FakeInputApi(affected_files=[sample_file])
+
+    self.assertFalse(PRESUBMIT.SampleZipped(sample_file, input_api))
+
+  def testSampleZipped_WrongZip(self):
+    sample_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/examples/foo/bar.jpg')
+    zip_file = FakeAffectedFile(
+        local_path='chrome/common/extensions/docs/examples/baz.zip')
+    input_api = FakeInputApi(affected_files=[sample_file, zip_file])
+
+    self.assertFalse(PRESUBMIT.SampleZipped(sample_file, input_api))
+
+
+if __name__ == '__main__':
+  unittest.main()
