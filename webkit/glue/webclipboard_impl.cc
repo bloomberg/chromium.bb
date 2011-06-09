bbf13395fb7b0cb78db2bcd1a4159fcfc30cb0ce
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-09 19:28:31 +0000

    Don't write image markup to the clipboard when copying images on OS X.
    If image markup is present on the clipboard, Mail.app will always prefer it over the actual image even if we declare the image types before the HTML type in -[NSPasteboard types]. With this change, we also match the behavior of other Mac browser when invoking the "Copy Image" action.
    
    BUG=33016
    TEST=Manual test with 'copy image' context menu item and then pasting it into a Mail.app draft.
    
    Review URL: http://codereview.chromium.org/7044074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88569 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0930323026fd1ffe2b18a33c2df87d04d9a3a68f

--- webkit/glue/webclipboard_impl.cc
+++ webkit/glue/webclipboard_impl.cc
@@ -179,13 +179,18 @@ void WebClipboardImpl::writeImage(
     scw.WriteBitmapFromPixels(bitmap.getPixels(), image.size());
   }
 
-  // When writing the image, we also write the image markup so that pasting
-  // into rich text editors, such as Gmail, reveals the image. We also don't
-  // want to call writeText(), since some applications (WordPad) don't pick the
-  // image if there is also a text format on the clipboard.
   if (!url.isEmpty()) {
     scw.WriteBookmark(title, url.spec());
+#if !defined(OS_MACOSX)
+    // When writing the image, we also write the image markup so that pasting
+    // into rich text editors, such as Gmail, reveals the image. We also don't
+    // want to call writeText(), since some applications (WordPad) don't pick
+    // the image if there is also a text format on the clipboard.
+    // We also don't want to write HTML on a Mac, since Mail.app prefers to use
+    // the image markup over attaching the actual image. See
+    // http://crbug.com/33016 for details.
     scw.WriteHTML(UTF8ToUTF16(URLToImageMarkup(url, title)), "");
+#endif
   }
 }
 
