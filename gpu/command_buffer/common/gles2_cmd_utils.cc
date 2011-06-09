7c1e31161cb03314f644d9a1934ebebcc3636546
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-09 00:58:30 +0000

    Fix LUMANINCE_ALPHA issue
    TEST=unit tests
    BUG=85046
    
    Review URL: http://codereview.chromium.org/7046057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88452 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 010791dcdbfb46d31846a486790650f8dc38aa5a

--- gpu/command_buffer/common/gles2_cmd_utils.cc
+++ gpu/command_buffer/common/gles2_cmd_utils.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -323,6 +323,7 @@ int ElementsPerGroup(int format, int type) {
     case GL_RGB:
        return 3;
     case GL_LUMINANCE_ALPHA:
+       return 2;
     case GL_RGBA:
     case GL_BGRA_EXT:
        return 4;
