166c183f9daebafc87b76ea422c8a4aadb146075
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-10 15:28:53 +0000

    Get URLRequestContext from the profile attached to the automation provider, not the default. Use System request context if no profile context available.
    BUG=64339
    TEST=automation provider tests continue to work correctly.
    
    Review URL: http://codereview.chromium.org/7134020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88668 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4066a1d5744eda720ea8e013710c13c2e58d6b26

--- chrome/browser/automation/automation_provider.cc
+++ chrome/browser/automation/automation_provider.cc
@@ -579,16 +579,7 @@ class SetProxyConfigTask : public Task {
 
 void AutomationProvider::SetProxyConfig(const std::string& new_proxy_config) {
   net::URLRequestContextGetter* context_getter =
-      Profile::GetDefaultRequestContext();
-  if (!context_getter) {
-    FilePath user_data_dir;
-    PathService::Get(chrome::DIR_USER_DATA, &user_data_dir);
-    ProfileManager* profile_manager = g_browser_process->profile_manager();
-    DCHECK(profile_manager);
-    Profile* profile = profile_manager->GetDefaultProfile(user_data_dir);
-    DCHECK(profile);
-    context_getter = profile->GetRequestContext();
-  }
+      profile_->GetRequestContext();
   DCHECK(context_getter);
 
   BrowserThread::PostTask(
