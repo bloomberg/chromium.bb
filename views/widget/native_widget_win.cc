ba328200eadd4fc93196d22f27ee569410620680
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 23:04:13 +0000

    Revert 89076 - Make sure custom frame windows paint as active when they're activated.
    Rationale for fix:
    
    The code in NativeWidgetWin::OnNCActivate schedules a paint for the entire non-client view if the window is visible because its activation state has changed and it'll doubtless require a repaint.
    
    A subsequent hack introduced by me to recover from the "other windows bleed through the UI" immediately forces a redraw, before the default WM_NCACTIVATE handler is called for the window.
    
    It seems that the window's active state is only set by or after the default WM_NCACTIVATE handler, so this hack redraw causes the window to just repaint itself as inactive since the state hasn't updated yet (and my earlier refactorings have made the frame ask Windows if the window is active rather than relying on custom cached state).
    
    So, to address this I moved the hack redraw from NativeWidgetWin::OnNCActivate to NativeWidgetWin::OnActivate, after the state and redraws should have happened. This corrects the issue for me.
    
    http://crbug.com/85791
    TEST=see bug
    Review URL: http://codereview.chromium.org/7165003
    
    TBR=ben@chromium.org
    Review URL: http://codereview.chromium.org/7168002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89089 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 46c597bcf6a7be69fd9068db19c36ddc19fa0475

--- views/widget/native_widget_win.cc
+++ views/widget/native_widget_win.cc
@@ -1117,17 +1117,6 @@ LRESULT NativeWidgetWin::OnWndProc(UINT message, WPARAM w_param,
 // Message handlers ------------------------------------------------------------
 
 void NativeWidgetWin::OnActivate(UINT action, BOOL minimized, HWND window) {
-  if (!GetWidget()->ShouldUseNativeFrame()) {
-    // TODO(beng, et al): Hack to redraw this window and child windows
-    //     synchronously upon activation. Not all child windows are redrawing
-    //     themselves leading to issues like http://crbug.com/74604
-    //     We redraw out-of-process HWNDs asynchronously to avoid hanging the
-    //     whole app if a child HWND belonging to a hung plugin is encountered.
-    RedrawWindow(GetNativeView(), NULL, NULL,
-                 RDW_NOCHILDREN | RDW_INVALIDATE | RDW_UPDATENOW);
-    EnumChildWindows(GetNativeView(), EnumChildWindowsForRedraw, NULL);
-  }
-
   SetMsgHandled(FALSE);
 }
 
@@ -1579,6 +1568,17 @@ LRESULT NativeWidgetWin::OnNCActivate(BOOL active) {
   // visible, no need to paint.
   if (IsVisible())
     GetWidget()->non_client_view()->SchedulePaint();
+
+  if (!GetWidget()->ShouldUseNativeFrame()) {
+    // TODO(beng, et al): Hack to redraw this window and child windows
+    //     synchronously upon activation. Not all child windows are redrawing
+    //     themselves leading to issues like http://crbug.com/74604
+    //     We redraw out-of-process HWNDs asynchronously to avoid hanging the
+    //     whole app if a child HWND belonging to a hung plugin is encountered.
+    RedrawWindow(GetNativeView(), NULL, NULL,
+                 RDW_NOCHILDREN | RDW_INVALIDATE | RDW_UPDATENOW);
+    EnumChildWindows(GetNativeView(), EnumChildWindowsForRedraw, NULL);
+  }
 
   // If we're active again, we should be allowed to render as inactive, so
   // tell the non-client view.
