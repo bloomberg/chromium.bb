cdac4c2403cd172abd0f5401f58cfe3d15c90c91
rhashimoto@chromium.org <rhashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 18:33:19 +0000

    Convert LanguageSwitchMenu from Menu2 to MenuItemView.
    This conversion is part of GTK removal required.  
    
    BUG=chromium-os:13887
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/6982054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89033 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 740379aebc5c3368b63af4ef8f8d3214aa0eb1cb

--- chrome/browser/chromeos/login/views_network_screen_actor.cc
+++ chrome/browser/chromeos/login/views_network_screen_actor.cc
@@ -44,7 +44,6 @@ ViewsNetworkScreenActor::ViewsNetworkScreenActor(
         kWelcomeScreenHeight),
       bubble_(NULL),
       screen_(NULL) {
-  language_switch_menu_.set_menu_alignment(views::Menu2::ALIGN_TOPLEFT);
 }
 
 ViewsNetworkScreenActor::~ViewsNetworkScreenActor() {
