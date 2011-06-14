68d89afe2496b904c71ea3645bc0c808f91178f9
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 01:13:52 +0000

    Fix mac build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@88937 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8b71c08456fe0dc7a420aa05c609ab433d08181d

--- content/browser/plugin_service_browsertest.cc
+++ content/browser/plugin_service_browsertest.cc
@@ -77,7 +77,7 @@ class PluginServiceTest : public InProcessBrowserTest {
   virtual void SetUpCommandLine(CommandLine* command_line) {
 #ifdef OS_MACOSX
     FilePath browser_directory;
-    PathService::Get(chrome::DIR_APP, &browser_directory);
+    PathService::Get(base::DIR_MODULE, &browser_directory);
     command_line->AppendSwitchPath(switches::kExtraPluginDir,
                                    browser_directory.AppendASCII("plugins"));
 #endif
