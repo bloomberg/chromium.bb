c00441b8368e30da22e1ea8164b651601cc6628e
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-14 22:09:22 +0000

    Marking 2 ExtensionInstallUIBrowserTest's as flaky
    BUG=86105
    TEST=none
    
    TBR=tessamac@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89080 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6be09e62da1d4efc9baf2f7636b57281decee1bd

--- chrome/browser/extensions/extension_install_ui_browsertest.cc
+++ chrome/browser/extensions/extension_install_ui_browsertest.cc
@@ -37,8 +37,16 @@ class ExtensionInstallUIBrowserTest : public ExtensionBrowserTest {
   }
 };
 
+// Flaky on linux. See http://crbug.com/86105
+#if defined(OS_LINUX)
+#define MAYBE_TestThemeInstallUndoResetsToDefault \
+FLAKY_TestThemeInstallUndoResetsToDefault
+#else
+#define MAYBE_TestThemeInstallUndoResetsToDefault \
+TestThemeInstallUndoResetsToDefault
+#endif
 IN_PROC_BROWSER_TEST_F(ExtensionInstallUIBrowserTest,
-                       TestThemeInstallUndoResetsToDefault) {
+                       MAYBE_TestThemeInstallUndoResetsToDefault) {
   // Install theme once and undo to verify we go back to default theme.
   FilePath theme_crx = PackExtension(test_data_dir_.AppendASCII("theme"));
   ASSERT_TRUE(InstallExtensionWithUI(theme_crx, 1));
@@ -64,8 +72,16 @@ IN_PROC_BROWSER_TEST_F(ExtensionInstallUIBrowserTest,
   ASSERT_EQ(NULL, GetTheme());
 }
 
+// Flaky on linux. See http://crbug.com/86105
+#if defined(OS_LINUX)
+#define MAYBE_TestThemeInstallUndoResetsToPreviousTheme \
+FLAKY_TestThemeInstallUndoResetsToPreviousTheme
+#else
+#define MAYBE_TestThemeInstallUndoResetsToPreviousTheme \
+TestThemeInstallUndoResetsToPreviousTheme
+#endif
 IN_PROC_BROWSER_TEST_F(ExtensionInstallUIBrowserTest,
-                       TestThemeInstallUndoResetsToPreviousTheme) {
+                       MAYBE_TestThemeInstallUndoResetsToPreviousTheme) {
   // Install first theme.
   FilePath theme_path = test_data_dir_.AppendASCII("theme");
   ASSERT_TRUE(InstallExtensionWithUI(theme_path, 1));
