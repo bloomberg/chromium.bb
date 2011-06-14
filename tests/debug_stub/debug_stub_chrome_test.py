9699970d4b3574748c755b5e3fa45778c0ff59c4
mmortensen@google.com <mmortensen@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-06-14 19:03:44 +0000

    Re-enable browser_debug test.
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1920
    TEST=Test manually, validate on bots.
    
    The problem was that the 'new' chrome (from DEPS roll) has my fix that 'breaks on entry' with --enable-nacl-debug.
    We need to guarantee we issue a 'c' before we call WaitForNexeLoad.  I switched listen() to no longer be a thread, but to call it directly.
    
    I modified WaitForNexeLoad to be able to specify a longer timeout.  It turned out this was not needed, but may be useful for other tests.
    Review URL: http://codereview.chromium.org/7150011
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5643 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 74247d7f034a80e97176971b7027a82f3c3b1176

--- tests/debug_stub/debug_stub_chrome_test.py
+++ tests/debug_stub/debug_stub_chrome_test.py
@@ -26,16 +26,23 @@ class NaClTest(pyauto.PyUITest):
   def chromeDebug(self, page, title_word):
     self.debug_socket_opened = False
 
+    # This function will open port 4014 and send some RSP commands.
+    # Last of all, it must send a 'c' command (continue) so that
+    # the nexe will continue (past the initial breakpoint) and
+    # load.
     def listen():
       print 'About to open GdbRsp connection'
       connection = debug_stub_test.GdbRspConnection()
+      print 'Opened debug connection'
       self.debug_socket_opened = True
       # Query the debug_stub for architecture string
       reply = connection.RspRequest('qXfer:features:read:target.xml:0,fff')
+      print 'reply to features is %s' % reply
       # Make sure string returned includes the word 'architecture'
       self.assertTrue(re.search('architecture', reply), reply)
       # Query for reason halted -- should return S0 and a number
       reply = connection.RspRequest('?')
+      print 'reply to ? is %s' % reply
       # Validate we stopped for some kind of exception
       self.assertTrue(re.match('^S0', reply))
       # Just send a "c" so that the nexe continues. No more breakpoints
@@ -43,13 +50,13 @@ class NaClTest(pyauto.PyUITest):
       reply = connection.RspSendOnly('c')
       print 'Sent a "c"'
 
-    # Start a thread that will send RSP debug commands on port 4014.
-    # The main thread will open a url in chrome, which was launched with
-    # --enable-nacl-debug, which will open port 4014.
-    thread.start_new_thread(listen, ())
-
+    # Navigate to the page that has the nexe
     url = self.GetHttpURLForDataPath(page)
     self.NavigateToURL(url)
+
+    # Because sel_ldr will break on entry, we need to call the listen function
+    # so that it will tell sel_ldr to continue.
+    listen()
     nacl_utils.WaitForNexeLoad(self)
     nacl_utils.VerifyAllTestsPassed(self)
     # The debug socket must have opened for this test to pass, even if
