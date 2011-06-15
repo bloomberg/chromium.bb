73499961c94f044c27002cc1ff38f3c27b378e4f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-15 19:57:38 +0000

    ui/base: Get rid of UiBaseTestSuite class.
    This class is overriding some methods from base::TestSuite but not adding anything useful, so just get rid of it entirely.
    
    BUG=None
    TEST=ui_unittests
    
    R=rsesek@chromium.org,phajdan.jr@chromium.org
    
    Review URL: http://codereview.chromium.org/7170024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89236 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9783fac6b4e00309fb643bd3a9c5435c939a04eb

--- ui/base/run_all_unittests.cc
+++ ui/base/run_all_unittests.cc
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/base/test_suite.h"
+#include "base/test/test_suite.h"
 
 int main(int argc, char** argv) {
-  return UiBaseTestSuite(argc, argv).Run();
+  return base::TestSuite(argc, argv).Run();
 }
