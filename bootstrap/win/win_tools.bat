8a4fb64c90223990753f28ce1db1002a2b48a008
cmp@chromium.org <cmp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-15 22:40:16 +0000

    In Windows Git, set autocrlf and filemode by default. Review URL: http://codereview.chromium.org/7172033
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@89261 0039d316-1c4b-4281-b951-d872f2087c98

--- bootstrap/win/win_tools.bat
+++ bootstrap/win/win_tools.bat
@@ -48,6 +48,9 @@ call copy /y "%WIN_TOOLS_ROOT_DIR%\git_bin\git.bat" "%WIN_TOOLS_ROOT_DIR%\git.ba
 call copy /y "%WIN_TOOLS_ROOT_DIR%\git_bin\gitk.bat" "%WIN_TOOLS_ROOT_DIR%\gitk.bat" 1>nul
 call copy /y "%WIN_TOOLS_ROOT_DIR%\git_bin\ssh.bat" "%WIN_TOOLS_ROOT_DIR%\ssh.bat" 1>nul
 call copy /y "%WIN_TOOLS_ROOT_DIR%\git_bin\ssh-keygen.bat" "%WIN_TOOLS_ROOT_DIR%\ssh-keygen.bat" 1>nul
+:: Ensure autocrlf and filemode are set correctly.
+call %WIN_TOOLS_ROOT_DIR%\git.bat config --global core.autocrlf false
+call %WIN_TOOLS_ROOT_DIR%\git.bat config --global core.filemode false
 goto :SVN_CHECK
 
 
