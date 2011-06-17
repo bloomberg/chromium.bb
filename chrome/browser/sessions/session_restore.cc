b5f2f80e973a7ed60b913f7678616b7c4df78fba
mirandac@chromium.org <mirandac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-17 18:50:58 +0000

    Change BrowserList::GetLastActive to select the window with the correct profile.
    BUG=86361
    TEST=session restore tests remain correct.
    
    Review URL: http://codereview.chromium.org/7206002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89529 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 841e679e8d73ddb58fa2a3938aed99d323451065

--- chrome/browser/sessions/session_restore.cc
+++ chrome/browser/sessions/session_restore.cc
@@ -481,7 +481,7 @@ class SessionRestoreImpl : public NotificationObserver {
   void RestoreForeignTab(const SessionTab& tab) {
     StartTabCreation();
     Browser* current_browser =
-        browser_ ? browser_ : BrowserList::GetLastActive();
+        browser_ ? browser_ : BrowserList::GetLastActiveWithProfile(profile_);
     RestoreTab(tab, current_browser->tab_count(), current_browser, true);
     NotifySessionServiceOfRestoredTabs(current_browser,
                                        current_browser->tab_count());
