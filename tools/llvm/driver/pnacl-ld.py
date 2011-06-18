6f4de795ffaf04735a35ebfab2486092da454c93
bradchen@google.com <bradchen@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-06-18 15:46:28 +0000

    Revert changes 5708 and 5709 which broke the PNaCl toolchain builder. Review URL: http://codereview.chromium.org/7204018
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5711 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 9eb16e864ec082034b2cf67658b26a4f1f5fa249

--- tools/llvm/driver/pnacl-ld.py
+++ tools/llvm/driver/pnacl-ld.py
@@ -41,8 +41,8 @@ EXTRA_ENV = {
 
   'PNACL_TRANSLATE_FLAGS': '${PIC ? -fPIC}',
 
-  'OPT_FLAGS': '-O${OPT_LEVEL} ${OPT_STRIP_%STRIP_MODE%}',
-  'OPT_LEVEL': '0',
+  'OPT_FLAGS': '-std-compile-opts ${OPT_LEVEL} ${OPT_STRIP_%STRIP_MODE%}',
+  'OPT_LEVEL': '-O3',
   'OPT_STRIP_none': '',
   'OPT_STRIP_all': '-disable-opt --strip',
   'OPT_STRIP_debug': '-disable-opt --strip-debug',
@@ -152,9 +152,7 @@ LDPatterns = [
   ( '-fPIC',               "env.set('PIC', '1')"),
 
   # This controls LTO optimization.
-  # opt does not support -Os but internally it is identical to -O2
-  ( '-Os',                 "env.set('OPT_LEVEL', '2')"),
-  ( '-O([0-3])',           "env.set('OPT_LEVEL', $0)"),
+  ( '(-O.+)',              "env.set('OPT_LEVEL', $0)"),
 
   ( '-s',                  "env.append('STRIP_MODE', 'all')"),
   ( '--strip-all',         "env.append('STRIP_MODE', 'all')"),
@@ -226,7 +224,7 @@ def main(argv):
       chain.add(WrapDIS, 'before_wrap.ll')
       chain.add(WrapLL, 'after_wrap.ll')
       chain.add(WrapAS, 'after_wrap.' + bitcode_type)
-    if env.getone('OPT_LEVEL') != '0' and not env.getbool('SKIP_OPT'):
+    if not env.getbool('SKIP_OPT'):
       chain.add(DoOPT, 'opt.' + bitcode_type)
     elif env.getone('STRIP_MODE') != 'none':
       chain.add(DoStrip, 'stripped.' + bitcode_type)
