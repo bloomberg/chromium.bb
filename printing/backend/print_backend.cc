5b3fd645cdc75e6613a8a16e759622a5c15ef847
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-22 06:18:18 +0000

    Coverity: Initialize a variable in PrinterBasicInfo.
    BUG=none
    TEST=none
    CID=16140
    
    Review URL: http://codereview.chromium.org/7218029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@89992 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3fe291b761880a19cba9a680cd26f35cc3f19494

--- printing/backend/print_backend.cc
+++ printing/backend/print_backend.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,7 +6,9 @@
 
 namespace printing {
 
-PrinterBasicInfo::PrinterBasicInfo() : printer_status(0) {}
+PrinterBasicInfo::PrinterBasicInfo()
+    : printer_status(0),
+      is_default(false) {}
 
 PrinterBasicInfo::~PrinterBasicInfo() {}
 
