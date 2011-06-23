4175d9703ec41001d418ad92bc58a304a94b0f93
gfeher@chromium.org <gfeher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 14:44:28 +0000

    Test cases for the cloud policy subsystem
    BUG=chromium-os:16637
    TEST=compiles and passes
    
    
    Review URL: http://codereview.chromium.org/7216012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90211 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bb08efcb9e09b7dc83ffd1e211086d7c225aeb6b

--- chrome/browser/policy/cloud_policy_subsystem_unittest.cc
+++ chrome/browser/policy/cloud_policy_subsystem_unittest.cc
@@ -105,128 +105,6 @@ class TestingIdentityStrategy : public CloudPolicyIdentityStrategy {
   DISALLOW_COPY_AND_ASSIGN(TestingIdentityStrategy);
 };
 
-// Tests CloudPolicySubsystem by intercepting its network requests.
-// The requests are intercepted by PolicyRequestInterceptor and they are
-// logged by LoggingWorkScheduler for further examination.
-class CloudPolicySubsystemTest : public testing::Test {
- public:
-  CloudPolicySubsystemTest()
-      : ui_thread_(BrowserThread::UI, &loop_),
-        io_thread_(BrowserThread::IO, &loop_) {
-  }
-
- protected:
-  void StopMessageLoop() {
-    loop_.QuitNow();
-  }
-
-  virtual void SetUp() {
-    prefs_.reset(new TestingPrefService);
-    CloudPolicySubsystem::RegisterPrefs(prefs_.get());
-    ((TestingBrowserProcess*) g_browser_process)->SetLocalState(prefs_.get());
-
-    logger_.reset(new EventLogger);
-    factory_.reset(new TestingPolicyURLFetcherFactory(logger_.get()));
-    URLFetcher::set_factory(factory_.get());
-    identity_strategy_.reset(new TestingIdentityStrategy);
-    ASSERT_TRUE(temp_user_data_dir_.CreateUniqueTempDir());
-    cache_ = new UserPolicyCache(
-        temp_user_data_dir_.path().AppendASCII("CloudPolicyControllerTest"));
-    cloud_policy_subsystem_.reset(new TestingCloudPolicySubsystem(
-        identity_strategy_.get(), cache_, kDeviceManagementUrl,
-        logger_.get()));
-    cloud_policy_subsystem_->CompleteInitialization(
-        prefs::kDevicePolicyRefreshRate, 0);
-  }
-
-  virtual void TearDown() {
-    ((TestingBrowserProcess*) g_browser_process)->SetLocalState(NULL);
-    cloud_policy_subsystem_->Shutdown();
-    cloud_policy_subsystem_.reset();
-    factory_.reset();
-    logger_.reset();
-    prefs_.reset();
-  }
-
-  // Verifies for a given policy that it is provided by the subsystem.
-  void VerifyPolicy(enum ConfigurationPolicyType type, Value* expected) {
-    MockConfigurationPolicyStore store;
-    EXPECT_CALL(store, Apply(_, _)).Times(AtLeast(1));
-    cache_->GetManagedPolicyProvider()->Provide(&store);
-    ASSERT_TRUE(Value::Equals(expected, store.Get(type)));
-  }
-
-  // Verifies that the last recorded run of the subsystem did not issue
-  // too frequent requests:
-  // - no more than 10 requests in the first 10 minutes
-  // - no more then 12 requests per hour in the next 10 hours
-  // TODO(gfeher): Thighten these conditions further. This will require
-  // fine-tuning of the subsystem. See: http://crosbug.com/15195
-  void VerifyServerLoad() {
-    std::vector<int64> events;
-    logger_->Swap(&events);
-
-    ASSERT_FALSE(events.empty());
-
-    int64 cur = 0;
-    int count = 0;
-
-    // Length and max number of requests for the first interval.
-    int64 length = 10*60*1000; // 10 minutes
-    int64 limit = 10; // maximum nr of requests in the first 10 minutes
-
-    while (cur <= events.back()) {
-      EXPECT_LE(EventLogger::CountEvents(events, cur, length), limit);
-      count++;
-
-      cur += length;
-
-      // Length and max number of requests for the subsequent intervals.
-      length = 60*60*1000; // 60 minutes
-      limit = 12; // maxminum nr of requests in the next 60 minutes
-    }
-
-    EXPECT_GE(count, 11)
-        << "No enough requests were fired during the test run.";
-  }
-
-  void ExecuteTest(const std::string& homepage_location) {
-    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-    // The first unexptected request of the policy subsystem will stop the
-    // message loop.
-    // This code relies on the fact that an InSequence object is active.
-    EXPECT_CALL(*(factory_.get()), Intercept(_, _, _)).
-       Times(AtMost(1)).
-       WillOnce(InvokeWithoutArgs(
-           this,
-           &CloudPolicySubsystemTest::StopMessageLoop));
-    loop_.RunAllPending();
-
-    // Test conditions.
-    EXPECT_EQ(CloudPolicySubsystem::SUCCESS, cloud_policy_subsystem_->state());
-    StringValue homepage_value(homepage_location);
-    VerifyPolicy(kPolicyHomepageLocation, &homepage_value);
-    VerifyServerLoad();
-  }
-
-  scoped_ptr<TestingPolicyURLFetcherFactory> factory_;
-
- private:
-  ScopedTempDir temp_user_data_dir_;
-
-  MessageLoop loop_;
-  BrowserThread ui_thread_;
-  BrowserThread io_thread_;
-
-  scoped_ptr<EventLogger> logger_;
-  scoped_ptr<TestingIdentityStrategy> identity_strategy_;
-  scoped_ptr<CloudPolicySubsystem> cloud_policy_subsystem_;
-  scoped_ptr<PrefService> prefs_;
-  CloudPolicyCacheBase* cache_;
-
-  DISALLOW_COPY_AND_ASSIGN(CloudPolicySubsystemTest);
-};
-
 // An action that returns an URLRequestJob with an HTTP error code.
 ACTION_P(CreateFailedResponse, http_error_code) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
@@ -267,38 +145,301 @@ ACTION_P(CreateSuccessfulPolicyResponse, homepage_location) {
   arg2->response_code = 200;
 }
 
-TEST_F(CloudPolicySubsystemTest, SuccessAfterSingleErrors) {
-  InSequence c;
+// Tests CloudPolicySubsystem by intercepting its network requests.
+// The requests are intercepted by PolicyRequestInterceptor and they are
+// logged by LoggingWorkScheduler for further examination.
+template<typename TESTBASE>
+class CloudPolicySubsystemTestBase : public TESTBASE {
+ public:
+  CloudPolicySubsystemTestBase()
+      : ui_thread_(BrowserThread::UI, &loop_),
+        io_thread_(BrowserThread::IO, &loop_) {
+  }
 
-  EXPECT_CALL(*(factory_.get()), Intercept(kGaiaAuthHeader, "register", _)).
-      WillOnce(CreateFailedResponse(901));
-  EXPECT_CALL(*(factory_.get()), Intercept(kGaiaAuthHeader, "register", _)).
-      WillOnce(CreateSuccessfulRegisterResponse(kDMToken));
+  virtual ~CloudPolicySubsystemTestBase() {
+  }
 
-  EXPECT_CALL(*(factory_.get()), Intercept(kDMAuthHeader, "policy", _)).
-      WillOnce(CreateFailedResponse(501));
-  EXPECT_CALL(*(factory_.get()), Intercept(kDMAuthHeader, "policy", _)).
-      Times(200).
-      WillRepeatedly(CreateSuccessfulPolicyResponse("http://www.google.com"));
-  EXPECT_CALL(*(factory_.get()), Intercept(kDMAuthHeader, "policy", _)).
-      WillOnce(CreateSuccessfulPolicyResponse("http://www.chromium.org"));
+ protected:
+  void StopMessageLoop() {
+    loop_.QuitNow();
+  }
 
-  ExecuteTest("http://www.chromium.org");
+  virtual void SetUp() {
+    prefs_.reset(new TestingPrefService);
+    CloudPolicySubsystem::RegisterPrefs(prefs_.get());
+    ((TestingBrowserProcess*) g_browser_process)->SetLocalState(prefs_.get());
+
+    logger_.reset(new EventLogger);
+    factory_.reset(new TestingPolicyURLFetcherFactory(logger_.get()));
+    URLFetcher::set_factory(factory_.get());
+    identity_strategy_.reset(new TestingIdentityStrategy);
+    ASSERT_TRUE(temp_user_data_dir_.CreateUniqueTempDir());
+    cache_ = new UserPolicyCache(
+        temp_user_data_dir_.path().AppendASCII("CloudPolicyControllerTest"));
+    cloud_policy_subsystem_.reset(new TestingCloudPolicySubsystem(
+        identity_strategy_.get(), cache_, kDeviceManagementUrl,
+        logger_.get()));
+    cloud_policy_subsystem_->CompleteInitialization(
+        prefs::kDevicePolicyRefreshRate, 0);
+  }
+
+  virtual void TearDown() {
+    ((TestingBrowserProcess*) g_browser_process)->SetLocalState(NULL);
+    cloud_policy_subsystem_->Shutdown();
+    cloud_policy_subsystem_.reset();
+    factory_.reset();
+    logger_.reset();
+    prefs_.reset();
+  }
+
+  void ExecuteTest() {
+    DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+    // The first unexpected request of the policy subsystem will stop the
+    // message loop.
+    // This code relies on the fact that an InSequence object is active.
+    EXPECT_CALL(*(factory_.get()), Intercept(_, _, _))
+       .Times(AtMost(1))
+       .WillOnce(InvokeWithoutArgs(
+           this,
+           &CloudPolicySubsystemTestBase::StopMessageLoop));
+    loop_.RunAllPending();
+  }
+
+  void VerifyTest(const std::string& homepage_location) {
+    // Test conditions.
+    EXPECT_EQ(CloudPolicySubsystem::SUCCESS, cloud_policy_subsystem_->state());
+    StringValue homepage_value(homepage_location);
+    VerifyPolicy(kPolicyHomepageLocation, &homepage_value);
+    VerifyServerLoad();
+  }
+
+  void VerifyState(CloudPolicySubsystem::PolicySubsystemState state) {
+    EXPECT_EQ(state, cloud_policy_subsystem_->state());
+  }
+
+  void ExpectSuccessfulRegistration() {
+    EXPECT_CALL(*(factory_.get()), Intercept(kGaiaAuthHeader, "register", _))
+        .WillOnce(CreateSuccessfulRegisterResponse(kDMToken));
+  }
+
+  void ExpectFailedRegistration(int n, int code) {
+    EXPECT_CALL(*(factory_.get()), Intercept(kGaiaAuthHeader, "register", _))
+        .Times(n)
+        .WillRepeatedly(CreateFailedResponse(code));
+  }
+
+  void ExpectFailedPolicy(int n, int code) {
+    EXPECT_CALL(*(factory_.get()), Intercept(kDMAuthHeader, "policy", _))
+        .Times(n)
+        .WillRepeatedly(CreateFailedResponse(code));
+  }
+
+  void ExpectSuccessfulPolicy(int n, const std::string& homepage) {
+    EXPECT_CALL(*(factory_.get()), Intercept(kDMAuthHeader, "policy", _))
+        .Times(n)
+        .WillRepeatedly(CreateSuccessfulPolicyResponse(homepage));
+  }
+
+ private:
+  // Verifies for a given policy that it is provided by the subsystem.
+  void VerifyPolicy(enum ConfigurationPolicyType type, Value* expected) {
+    MockConfigurationPolicyStore store;
+    EXPECT_CALL(store, Apply(_, _)).Times(AtLeast(1));
+    cache_->GetManagedPolicyProvider()->Provide(&store);
+    ASSERT_TRUE(Value::Equals(expected, store.Get(type)));
+  }
+
+  // Verifies that the last recorded run of the subsystem did not issue
+  // too frequent requests:
+  // - no more than 10 requests in the first 10 minutes
+  // - no more then 12 requests per hour in the next 10 hours
+  // TODO(gfeher): Thighten these conditions further. This will require
+  // fine-tuning of the subsystem. See: http://crosbug.com/16637
+  void VerifyServerLoad() {
+    std::vector<int64> events;
+    logger_->Swap(&events);
+
+    ASSERT_FALSE(events.empty());
+
+    int64 cur = 0;
+    int count = 0;
+
+    // Length and max number of requests for the first interval.
+    int64 length = 10*60*1000; // 10 minutes
+    int64 limit = 10; // maximum nr of requests in the first 10 minutes
+
+    while (cur <= events.back()) {
+      EXPECT_LE(EventLogger::CountEvents(events, cur, length), limit);
+      count++;
+
+      cur += length;
+
+      // Length and max number of requests for the subsequent intervals.
+      length = 60*60*1000; // 60 minutes
+      limit = 12; // maxminum nr of requests in the next 60 minutes
+    }
+
+    EXPECT_GE(count, 11)
+        << "No enough requests were fired during the test run.";
+  }
+
+  ScopedTempDir temp_user_data_dir_;
+
+  MessageLoop loop_;
+  BrowserThread ui_thread_;
+  BrowserThread io_thread_;
+
+  scoped_ptr<EventLogger> logger_;
+  scoped_ptr<TestingIdentityStrategy> identity_strategy_;
+  scoped_ptr<CloudPolicySubsystem> cloud_policy_subsystem_;
+  scoped_ptr<PrefService> prefs_;
+  CloudPolicyCacheBase* cache_;
+
+  scoped_ptr<TestingPolicyURLFetcherFactory> factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(CloudPolicySubsystemTestBase);
+};
+
+// A parameterized test case that simulates 100 failed registration attempts,
+// then a successful one, then 100 failed policy fetch attempts and then 100
+// successful policy fetches. The two parameters are the error codes for the
+// failed registration and policy responses.
+
+class CombinedTestDesc {
+ public:
+  CombinedTestDesc(int registration_error_code, int policy_error_code)
+      : registration_error_code_(registration_error_code),
+        policy_error_code_(policy_error_code) {
+  }
+
+  ~CombinedTestDesc() {}
+
+  int registration_error_code() const { return registration_error_code_; }
+  int policy_error_code() const { return policy_error_code_; }
+
+ private:
+  int registration_error_code_;
+  int policy_error_code_;
+};
+
+class CloudPolicySubsystemCombinedTest
+    : public CloudPolicySubsystemTestBase<
+                 testing::TestWithParam<CombinedTestDesc> > {
+};
+
+TEST_P(CloudPolicySubsystemCombinedTest, Combined) {
+  InSequence s;
+  ExpectFailedRegistration(100, GetParam().registration_error_code());
+  ExpectSuccessfulRegistration();
+  ExpectFailedPolicy(100, GetParam().policy_error_code());
+  ExpectSuccessfulPolicy(100, "http://www.google.com");
+  ExpectSuccessfulPolicy(1, "http://www.chromium.org");
+  ExecuteTest();
+  VerifyTest("http://www.chromium.org");
 }
 
-TEST_F(CloudPolicySubsystemTest, SuccessAfterRegistrationErrors) {
-  InSequence c;
+// A random sample of error code pairs. Note that the following policy error
+// codes (401, 403, 410) make the policy subsystem to try and reregister, and
+// that is not expected in these tests.
+INSTANTIATE_TEST_CASE_P(
+    CloudPolicySubsystemCombinedTestInstance,
+    CloudPolicySubsystemCombinedTest,
+    testing::Values(
+        CombinedTestDesc(403, 400),
+        CombinedTestDesc(403, 404),
+        CombinedTestDesc(403, 412),
+        CombinedTestDesc(403, 500),
+        CombinedTestDesc(403, 503),
+        CombinedTestDesc(403, 902),
+        CombinedTestDesc(902, 400),
+        CombinedTestDesc(503, 404),
+        CombinedTestDesc(500, 412),
+        CombinedTestDesc(412, 500),
+        CombinedTestDesc(404, 503),
+        CombinedTestDesc(400, 902)));
 
-  EXPECT_CALL(*(factory_.get()), Intercept(kGaiaAuthHeader, "register", _)).
-      Times(200).
-      WillRepeatedly(CreateFailedResponse(901));
-  EXPECT_CALL(*(factory_.get()), Intercept(kGaiaAuthHeader, "register", _)).
-      WillOnce(CreateSuccessfulRegisterResponse(kDMToken));
+// A parameterized test case that simulates 100 failed registration attempts,
+// then a successful one, and then a succesful policy fetch. The parameter is
+// the error code returned for registration attempts.
 
-  EXPECT_CALL(*(factory_.get()), Intercept(kDMAuthHeader, "policy", _)).
-      WillOnce(CreateSuccessfulPolicyResponse("http://www.example.com"));
+class CloudPolicySubsystemRegistrationTest
+    : public CloudPolicySubsystemTestBase<testing::TestWithParam<int> > {
+};
 
-  ExecuteTest("http://www.example.com");
+TEST_P(CloudPolicySubsystemRegistrationTest, Registration) {
+  InSequence s;
+  ExpectFailedRegistration(100, GetParam());
+  ExpectSuccessfulRegistration();
+  ExpectSuccessfulPolicy(1, "http://www.youtube.com");
+  ExecuteTest();
+  VerifyTest("http://www.youtube.com");
 }
+
+INSTANTIATE_TEST_CASE_P(
+    CloudPolicySubsystemRegistrationTestInstance,
+    CloudPolicySubsystemRegistrationTest,
+    // For the case of 401 see CloudPolicySubsystemRegistrationFailureTest
+    testing::Values(400, 403, 404, 410, 412, 500, 503, 902));
+
+// A test case that verifies that the subsystem understands the "not managed"
+// response from the server.
+
+class CloudPolicySubsystemRegistrationFailureTest
+    : public CloudPolicySubsystemTestBase<testing::Test> {
+};
+
+TEST_F(CloudPolicySubsystemRegistrationFailureTest, RegistrationFailure) {
+  InSequence s;
+  ExpectFailedRegistration(1, 401);
+  ExecuteTest();
+  VerifyState(CloudPolicySubsystem::BAD_GAIA_TOKEN);
+}
+
+// A parameterized test case that simulates a successful registration, then 100
+// failed policy fetch attempts and then a successful one. The parameter is
+// the error code returned for failed policy attempts.
+
+class CloudPolicySubsystemPolicyTest
+    : public CloudPolicySubsystemTestBase<testing::TestWithParam<int> > {
+};
+
+TEST_P(CloudPolicySubsystemPolicyTest, Policy) {
+  InSequence s;
+  ExpectSuccessfulRegistration();
+  ExpectFailedPolicy(100, GetParam());
+  ExpectSuccessfulPolicy(1, "http://www.youtube.com");
+  ExecuteTest();
+  VerifyTest("http://www.youtube.com");
+}
+
+INSTANTIATE_TEST_CASE_P(
+    CloudPolicySubsystemPolicyTestInstance,
+    CloudPolicySubsystemPolicyTest,
+    testing::Values(400, 404, 412, 500, 503, 902));
+
+// A parameterized test case that simulates a successful registration, then 40
+// failed policy fetch attempts and a successful registration after each of
+// them. The parameter is the error code returned for registration attempts.
+
+class CloudPolicySubsystemPolicyReregisterTest
+    : public CloudPolicySubsystemTestBase<testing::TestWithParam<int> > {
+};
+
+TEST_P(CloudPolicySubsystemPolicyReregisterTest, Policy) {
+  InSequence s;
+  for (int i = 0; i < 40; i++) {
+    ExpectSuccessfulRegistration();
+    ExpectFailedPolicy(1, GetParam());
+  }
+  ExpectSuccessfulRegistration();
+  ExpectSuccessfulPolicy(1, "http://www.youtube.com");
+  ExecuteTest();
+  VerifyTest("http://www.youtube.com");
+}
+
+INSTANTIATE_TEST_CASE_P(
+    CloudPolicySubsystemPolicyReregisterTestInstance,
+    CloudPolicySubsystemPolicyReregisterTest,
+    testing::Values(401, 403, 410));
 
 }  // policy
