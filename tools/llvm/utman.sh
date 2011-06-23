442737315368ce1e691cf51358363fbdd90aebf2
pdox@google.com <pdox@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-06-23 22:12:36 +0000

    Whitespace change to trigger rebuild
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5771 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: bd5e6fa4dc7c3071d5e729fa5abd246ba2e80bff

--- tools/llvm/utman.sh
+++ tools/llvm/utman.sh
@@ -8,6 +8,7 @@
 #@ This script builds the ARM and PNaCl untrusted toolchains.
 #@ It MUST be run from the native_client/ directory.
 #
+
 ######################################################################
 # Directory Layout Description
 ######################################################################
