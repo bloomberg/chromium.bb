9f41fc94c86407a9b206fbee9972a9d49b39c002
annapop@chromium.org <annapop@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 19:47:35 +0000

    New sync pyAuto test and an update.
    As the new sync UI stabilizes, this is the first patch of sync UI automation work for sync UI areas that are highly visible and where regressions have a large impact to users.
    
    BUG=none
    TEST=run the tests themselves
    
    
    Review URL: http://codereview.chromium.org/7150026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90250 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a3f1ca04983128b41992f76c06d1f343e0edf20f

--- chrome/test/functional/sync.py
+++ chrome/test/functional/sync.py
@@ -23,8 +23,8 @@ class SyncTest(pyauto.PyUITest):
     self.assertTrue(self.GetSyncInfo()['summary'] == 'READY')
     self.assertTrue(self.GetSyncInfo()['last synced'] == 'Just now')
 
-  def testDisableAndEnableDatatype(self):
-    """Sign in, disable and then enable sync for a datatype."""
+  def testDisableAndEnableDatatypes(self):
+    """Sign in, disable and then enable sync for multiple sync datatypes."""
     creds = self.GetPrivateInfo()['test_google_account']
     username = creds['username']
     password = creds['password']
@@ -33,10 +33,24 @@ class SyncTest(pyauto.PyUITest):
     self.assertTrue(self.SignInToSync(username, password))
     self.assertTrue(self.GetSyncInfo()['summary'] == 'READY')
     self.assertTrue(self.GetSyncInfo()['last synced'] == 'Just now')
-    self.assertTrue(self.DisableSyncForDatatypes(['Bookmarks']))
+    self.assertTrue(self.DisableSyncForDatatypes(['Apps', 'Autofill',
+        'Bookmarks', 'Extensions', 'Preferences', 'Themes']))
+    self.assertFalse('Apps' in self.GetSyncInfo()['synced datatypes'])
+    self.assertFalse('Autofill' in self.GetSyncInfo()['synced datatypes'])
     self.assertFalse('Bookmarks' in self.GetSyncInfo()['synced datatypes'])
-    self.assertTrue(self.EnableSyncForDatatypes(['Bookmarks']))
+    self.assertFalse('Extensions' in self.GetSyncInfo()['synced datatypes'])
+    self.assertFalse('Preferences' in self.GetSyncInfo()['synced datatypes'])
+    self.assertFalse('Themes' in self.GetSyncInfo()['synced datatypes'])
+    self.assertTrue(self.EnableSyncForDatatypes(['Apps', 'Autofill',
+        'Bookmarks', 'Extensions', 'Preferences','Themes']))
+    self.assertTrue(self.DisableSyncForDatatypes(['Passwords']))
+    self.assertTrue('Apps' in self.GetSyncInfo()['synced datatypes'])
+    self.assertTrue('Autofill' in self.GetSyncInfo()['synced datatypes'])
     self.assertTrue('Bookmarks' in self.GetSyncInfo()['synced datatypes'])
+    self.assertTrue('Extensions' in self.GetSyncInfo()['synced datatypes'])
+    self.assertTrue('Preferences' in self.GetSyncInfo()['synced datatypes'])
+    self.assertTrue('Themes' in self.GetSyncInfo()['synced datatypes'])
+    self.assertFalse('Passwords' in self.GetSyncInfo()['synced datatypes'])
 
   def testRestartBrowser(self):
     """Sign in to sync and restart the browser."""
@@ -53,6 +67,48 @@ class SyncTest(pyauto.PyUITest):
     self.assertTrue(self.GetSyncInfo()['summary'] == 'READY')
     self.assertTrue(self.GetSyncInfo()['last synced'] == 'Just now')
     self.assertTrue(self.GetSyncInfo()['updates received'] == 0)
+
+  def testPersonalStuffSyncSection(self):
+    """Verify the Sync section in Preferences before and after sync."""
+    creds = self.GetPrivateInfo()['test_google_account']
+    username = creds['username']
+    password = creds['password']
+    default_text = 'Keep everything synced or choose what data to sync'
+    set_up_button = 'Set Up Sync'
+    customize_button = 'Customize'
+    stop_button = 'Stop Sync'
+    signed_in_text = 'Google Dashboard'
+    chrome_personal_stuff_url = 'chrome://settings/personal'
+    new_timeout = pyauto.PyUITest.ActionTimeoutChanger(self,
+                                                       2 * 60 * 1000)  # 2 min.
+    self.AppendTab(pyauto.GURL(chrome_personal_stuff_url))
+    self.assertTrue(self.WaitUntil(
+        lambda: self.FindInPage(default_text, tab_index=1)['match_count'],
+                expect_retval=1),
+        'No default sync text.')
+    self.assertTrue(self.WaitUntil(
+        lambda: self.FindInPage(set_up_button, tab_index=1)['match_count'],
+                expect_retval=1),
+        'No set up sync button.')
+
+    self.assertTrue(self.SignInToSync(username, password))
+    self.GetBrowserWindow(0).GetTab(1).Reload()
+    self.assertTrue(self.WaitUntil(
+        lambda: self.FindInPage(username, tab_index=1)['match_count'],
+                expect_retval=1),
+        'No sync user account information.')
+    self.assertTrue(self.WaitUntil(
+        lambda: self.FindInPage(signed_in_text, tab_index=1)['match_count'],
+                expect_retval=1),
+        'No Google Dashboard information after signing in.')
+    self.assertTrue(self.WaitUntil(
+        lambda: self.FindInPage(stop_button, tab_index=1)['match_count'],
+                expect_retval=1),
+        'No stop sync button.')
+    self.assertTrue(self.WaitUntil(
+        lambda: self.FindInPage(customize_button, tab_index=1)['match_count'],
+                expect_retval=1),
+        'No customize sync button.')
 
 
 if __name__ == '__main__':
