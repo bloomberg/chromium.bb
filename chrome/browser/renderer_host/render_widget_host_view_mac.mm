0d44218b57b81ffb4c1aca130d1613908f9382cf
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-23 03:16:01 +0000

    Mac: Fix crash on live resize did end event
    Looks like we were missing a NULL pointer check.
    
    BUG=87195
    TEST=Testing.
    
    
    Review URL: http://codereview.chromium.org/7242008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90162 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2907bd6938108dd247cf3fa4eee5a8b49c7a99fc

--- chrome/browser/renderer_host/render_widget_host_view_mac.mm
+++ chrome/browser/renderer_host/render_widget_host_view_mac.mm
@@ -2562,13 +2562,15 @@ extern NSString *NSTextInputReplacementRangeAttributeName;
 - (void)viewWillStartLiveResize {
   [super viewWillStartLiveResize];
   RenderWidgetHost* widget = renderWidgetHostView_->render_widget_host_;
-  widget->Send(new ViewMsg_SetInLiveResize(widget->routing_id(), true));
+  if (widget)
+    widget->Send(new ViewMsg_SetInLiveResize(widget->routing_id(), true));
 }
 
 - (void)viewDidEndLiveResize {
   [super viewDidEndLiveResize];
   RenderWidgetHost* widget = renderWidgetHostView_->render_widget_host_;
-  widget->Send(new ViewMsg_SetInLiveResize(widget->routing_id(), false));
+  if (widget)
+    widget->Send(new ViewMsg_SetInLiveResize(widget->routing_id(), false));
 }
 
 @end
