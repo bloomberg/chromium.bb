f68d3f196a6fedef909937acd6e5e955cdd12158
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-25 04:41:35 +0000

    Prepare test shell for animate changes.
    R=jamesr@chromium.org
    
    
    Review URL: http://codereview.chromium.org/7261006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90493 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9a039aead5c77431a21db52692970a0d3e63f1fc

--- webkit/tools/test_shell/mac/webwidget_host.mm
+++ webkit/tools/test_shell/mac/webwidget_host.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2008-2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -184,7 +184,11 @@ void WebWidgetHost::Paint() {
       [NSGraphicsContext graphicsContextWithGraphicsPort:bitmap_context
                                                  flipped:YES]];
 
+#ifdef WEBWIDGET_HAS_ANIMATE_CHANGES
+  webwidget_->animate(0.0);
+#else
   webwidget_->animate();
+#endif
 
   // This may result in more invalidation
   webwidget_->layout();
