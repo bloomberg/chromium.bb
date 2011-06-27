a0715b0dc0e6ff63997a461444137e863b426c5a
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-27 22:07:53 +0000

    Fix bug that would cause short trace events to disappear when zoomed in.
    BUG=87184
    
    Review URL: http://codereview.chromium.org/7242003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90657 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 82584e985fe31201b7691631e61aa88b6ac849c6

--- chrome/browser/resources/gpu_internals/timeline_track.js
+++ chrome/browser/resources/gpu_internals/timeline_track.js
@@ -283,7 +283,8 @@ cr.define('gpu', function() {
       for (var i = 0; i < slices.length; ++i) {
         var slice = slices[i];
         var x = slice.start;
-        var w = slice.duration;
+        // Less than 0.001 causes short events to disappear when zoomed in.
+        var w = Math.max(slice.duration, 0.001);
         var colorId;
         colorId = slice.selected ?
             slice.colorId + selectedIdBoost :
