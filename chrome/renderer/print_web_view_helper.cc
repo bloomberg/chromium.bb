bloomberg/chromium.bb

commit 44a571f03d7d551f0c8b308cd14b7cd83f18b8d0

PrintPreview: Regenerate print preview data only when required.
Do not regenerate print preview data if the new printer settings matches previously selected printer settings.

BUG=82211
TEST=Preview a long webpage. Wait till the preview is loaded. Change the destination printer. If the new printer settings matches the old printer settings, you can observe that the preview data is loaded very quickly. 


Review URL: http://codereview.chromium.org/7204043

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90320 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: d1f9090e19aa5223eece8b11b8d680991e261953

chrome/renderer/print_web_view_helper.cc
@@ -70,6 +70,25 @@ bool PrintMsg_Print_Params_IsEmpty(const PrintMsg_Print_Params& params) {
          !params.supports_alpha_blend;
 }
 
+bool PrintMsg_Print_Params_IsEqual(
+    const PrintMsg_PrintPages_Params& oldParams,
+    const PrintMsg_PrintPages_Params& newParams) {
+  return oldParams.params.desired_dpi == newParams.params.desired_dpi &&
+         oldParams.params.max_shrink == newParams.params.max_shrink &&
+         oldParams.params.min_shrink == newParams.params.min_shrink &&
+         oldParams.params.dpi == newParams.params.dpi &&
+         oldParams.params.printable_size == newParams.params.printable_size &&
+         oldParams.params.selection_only == newParams.params.selection_only &&
+         oldParams.params.page_size == newParams.params.page_size &&
+         oldParams.params.margin_top == newParams.params.margin_top &&
+         oldParams.params.margin_left == newParams.params.margin_left &&
+         oldParams.params.supports_alpha_blend ==
+             newParams.params.supports_alpha_blend &&
+         oldParams.pages.size() == newParams.pages.size() &&
+         std::equal(oldParams.pages.begin(), oldParams.pages.end(),
+             newParams.pages.begin());
+}
+
 }  // namespace
 
 PrepareFrameAndViewForPrint::PrepareFrameAndViewForPrint(
@@ -135,7 +154,8 @@ PrintWebViewHelper::PrintWebViewHelper(RenderView* render_view)
       script_initiated_preview_frame_(NULL),
       context_menu_preview_node_(NULL),
       user_cancelled_scripted_print_count_(0),
-      notify_browser_of_print_failure_(true) {
+      notify_browser_of_print_failure_(true),
+      preview_page_count_(0) {
   is_preview_ = switches::IsPrintPreviewEnabled();
 }
 
@@ -353,7 +373,22 @@ void PrintWebViewHelper::PrintPreview(WebKit::WebFrame* frame,
   if (!settings.GetBoolean(printing::kSettingDraftDocument, &draft))
     draft = false;
 
-  // Render Pages for printing.
+  if (draft && old_print_pages_params_.get() &&
+      PrintMsg_Print_Params_IsEqual(*old_print_pages_params_,
+                                    *print_pages_params_)) {
+    PrintHostMsg_DidPreviewDocument_Params preview_params;
+    preview_params.reuse_existing_data = true;
+    preview_params.data_size = 0;
+    preview_params.document_cookie =
+        print_pages_params_->params.document_cookie;
+    preview_params.expected_pages_count = preview_page_count_;
+    preview_params.modifiable = IsModifiable(frame, node);
+
+    Send(new PrintHostMsg_PagesReadyForPreview(routing_id(), preview_params));
+    return;
+  }
+
+  // Render Pages for preview.
   if (!RenderPagesForPreview(frame, node, draft))
     DidFinishPrinting(FAIL_PREVIEW);
 }
@@ -375,7 +410,9 @@ void PrintWebViewHelper::DidFinishPrinting(PrintingResult result) {
     print_web_view_->close();
     print_web_view_ = NULL;
   }
-  print_pages_params_.reset();
+
+  old_print_pages_params_ .reset(print_pages_params_.release());
+
   notify_browser_of_print_failure_ = true;
 }
 

chrome/renderer/print_web_view_helper.h
@@ -278,6 +278,9 @@ class PrintWebViewHelper : public RenderViewObserver ,
   // the failure came from the browser in the first place.
   bool notify_browser_of_print_failure_;
 
+  int preview_page_count_;
+  scoped_ptr<PrintMsg_PrintPages_Params> old_print_pages_params_;
+
   DISALLOW_COPY_AND_ASSIGN(PrintWebViewHelper);
 };
 

chrome/browser/printing/print_preview_message_handler.cc
@@ -75,6 +75,20 @@ void PrintPreviewMessageHandler::OnPagesReadyForPreview(
   if (!print_preview_tab)
     return;
 
+  PrintPreviewUI* print_preview_ui =
+      static_cast<PrintPreviewUI*>(print_preview_tab->web_ui());
+
+  TabContentsWrapper* wrapper =
+      TabContentsWrapper::GetCurrentWrapperForContents(print_preview_tab);
+
+  if (params.reuse_existing_data) {
+    print_preview_ui->OnPreviewDataIsAvailable(
+        params.expected_pages_count,
+        wrapper->print_view_manager()->RenderSourceName(),
+        params.modifiable);
+    return;
+  }
+
   base::SharedMemory* shared_buf =
       new base::SharedMemory(params.metafile_data_handle, true);
   if (!shared_buf->Map(params.data_size)) {
@@ -83,12 +97,7 @@ void PrintPreviewMessageHandler::OnPagesReadyForPreview(
     return;
   }
 
-  TabContentsWrapper* wrapper =
-      TabContentsWrapper::GetCurrentWrapperForContents(print_preview_tab);
   wrapper->print_view_manager()->OverrideTitle(tab_contents());
-
-  PrintPreviewUI* print_preview_ui =
-      static_cast<PrintPreviewUI*>(print_preview_tab->web_ui());
 
   char* preview_data = static_cast<char*>(shared_buf->memory());
   uint32 preview_data_size = params.data_size;

chrome/common/print_messages.h
@@ -71,6 +71,10 @@ IPC_STRUCT_END()
 
 // Parameters to describe a rendered document.
 IPC_STRUCT_BEGIN(PrintHostMsg_DidPreviewDocument_Params)
+  // True when we can reuse existing preview data. |metafile_data_handle| and
+  // |data_size| should not be used when this is true.
+  IPC_STRUCT_MEMBER(bool, reuse_existing_data)
+
   // A shared memory handle to metafile data.
   IPC_STRUCT_MEMBER(base::SharedMemoryHandle, metafile_data_handle)
 

chrome/renderer/print_web_view_helper_linux.cc
@@ -28,24 +28,26 @@ using WebKit::WebNode;
 bool PrintWebViewHelper::CreatePreviewDocument(
     const PrintMsg_PrintPages_Params& params, WebKit::WebFrame* frame,
     WebKit::WebNode* node, bool draft) {
-  int page_count = 0;
+  preview_page_count_ = 0;
   printing::PreviewMetafile metafile;
   metafile.set_draft(draft);
   if (!metafile.Init())
     return false;
 
-  if (!RenderPages(params, frame, node, false, &page_count, &metafile, true))
+  if (!RenderPages(params, frame, node, false, &preview_page_count_,
+                   &metafile, true)) {
     return false;
-
+  }
 
   // Get the size of the resulting metafile.
   uint32 buf_size = metafile.GetDataSize();
   DCHECK_GT(buf_size, 0u);
 
   PrintHostMsg_DidPreviewDocument_Params preview_params;
+  preview_params.reuse_existing_data = false;
   preview_params.data_size = buf_size;
   preview_params.document_cookie = params.params.document_cookie;
-  preview_params.expected_pages_count = page_count;
+  preview_params.expected_pages_count = preview_page_count_;
   preview_params.modifiable = IsModifiable(frame, node);
 
   if (!CopyMetafileDataToSharedMem(&metafile,

chrome/renderer/print_web_view_helper_mac.mm
@@ -67,9 +67,9 @@ bool PrintWebViewHelper::CreatePreviewDocument(
 
   PrepareFrameAndViewForPrint prep_frame_view(printParams,
                                               frame, node, frame->view());
-  int page_count = prep_frame_view.GetExpectedPageCount();
+  preview_page_count_ = prep_frame_view.GetExpectedPageCount();
 
-  if (!page_count)
+  if (!preview_page_count_)
     return false;
 
   printing::PreviewMetafile metafile;
@@ -86,14 +86,14 @@ bool PrintWebViewHelper::CreatePreviewDocument(
   base::TimeTicks page_begin_time = begin_time;
 
   if (params.pages.empty()) {
-    for (int i = 0; i < page_count; ++i) {
+    for (int i = 0; i < preview_page_count_; ++i) {
       RenderPage(printParams.page_size, content_area, scale_factor, i, frame,
                  &metafile);
       page_begin_time = ReportPreviewPageRenderTime(page_begin_time);
     }
   } else {
     for (size_t i = 0; i < params.pages.size(); ++i) {
-      if (params.pages[i] >= page_count)
+      if (params.pages[i] >= preview_page_count_)
         break;
       RenderPage(printParams.page_size, content_area, scale_factor,
                  static_cast<int>(params.pages[i]), frame, &metafile);
@@ -106,14 +106,16 @@ bool PrintWebViewHelper::CreatePreviewDocument(
   prep_frame_view.FinishPrinting();
   metafile.FinishDocument();
 
-  ReportTotalPreviewGenerationTime(params.pages.size(), page_count,
+  ReportTotalPreviewGenerationTime(params.pages.size(),
+                                   preview_page_count_,
                                    render_time,
                                    base::TimeTicks::Now() - begin_time);
 
   PrintHostMsg_DidPreviewDocument_Params preview_params;
+  preview_params.reuse_existing_data = false;
   preview_params.data_size = metafile.GetDataSize();
   preview_params.document_cookie = params.params.document_cookie;
-  preview_params.expected_pages_count = page_count;
+  preview_params.expected_pages_count = preview_page_count_;
   preview_params.modifiable = IsModifiable(frame, node);
 
   // Ask the browser to create the shared memory for us.

chrome/renderer/print_web_view_helper_win.cc
@@ -126,13 +126,12 @@ void PrintWebViewHelper::PrintPageInternal(
 bool PrintWebViewHelper::CreatePreviewDocument(
     const PrintMsg_PrintPages_Params& params, WebKit::WebFrame* frame,
     WebKit::WebNode* node, bool draft) {
-  int page_count = 0;
   PrintMsg_Print_Params print_params = params.params;
   UpdatePrintableSizeInPrintParameters(frame, node, &print_params);
   PrepareFrameAndViewForPrint prep_frame_view(print_params, frame, node,
                                               frame->view());
-  page_count = prep_frame_view.GetExpectedPageCount();
-  if (!page_count)
+  preview_page_count_ = prep_frame_view.GetExpectedPageCount();
+  if (!preview_page_count_)
     return false;
 
   scoped_ptr<Metafile> metafile(new printing::PreviewMetafile);
@@ -147,14 +146,14 @@ bool PrintWebViewHelper::CreatePreviewDocument(
   base::TimeTicks page_begin_time = begin_time;
 
   if (params.pages.empty()) {
-    for (int i = 0; i < page_count; ++i) {
+    for (int i = 0; i < preview_page_count_; ++i) {
       float scale_factor = shrink;
       RenderPage(print_params, &scale_factor, i, true, frame, &metafile);
       page_begin_time = ReportPreviewPageRenderTime(page_begin_time);
     }
   } else {
     for (size_t i = 0; i < params.pages.size(); ++i) {
-      if (params.pages[i] >= page_count)
+      if (params.pages[i] >= preview_page_count_)
         break;
       float scale_factor = shrink;
       RenderPage(print_params, &scale_factor,
@@ -174,7 +173,8 @@ bool PrintWebViewHelper::CreatePreviewDocument(
   if (!metafile->FinishDocument())
     NOTREACHED();
 
-  ReportTotalPreviewGenerationTime(params.pages.size(), page_count,
+  ReportTotalPreviewGenerationTime(params.pages.size(),
+                                   preview_page_count_,
                                    render_time,
                                    base::TimeTicks::Now() - begin_time);
 
@@ -183,9 +183,10 @@ bool PrintWebViewHelper::CreatePreviewDocument(
   DCHECK_GT(buf_size, 128u);
 
   PrintHostMsg_DidPreviewDocument_Params preview_params;
+  preview_params.reuse_existing_data = false;
   preview_params.data_size = buf_size;
   preview_params.document_cookie = params.params.document_cookie;
-  preview_params.expected_pages_count = page_count;
+  preview_params.expected_pages_count = preview_page_count_;
   preview_params.modifiable = IsModifiable(frame, node);
 
   if (!CopyMetafileDataToSharedMem(metafile.get(),

commit 3214a67bae959de57f343fbee8487b56841a3b26

PrintPreview: Fix the regression caused by r90320.
Clear |old_print_pages_params_| before creating a print preview tab.

BUG=none
TEST=Open a webpage. Press Ctrl+P to display a preview tab. Once the preview tab is loaded, close the preview tab. Press Ctrl+P again on Initiator tab. A new preview tab is created and the associated preview data is displayed to the user.


Review URL: http://codereview.chromium.org/7273007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90663 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 76de16e7c78d65c1a4e4f48968f92f7350c37ebc

chrome/renderer/print_web_view_helper.cc
@@ -178,7 +178,7 @@ void PrintWebViewHelper::PrintPage(WebKit::WebFrame* frame) {
   if (is_preview_) {
     script_initiated_preview_frame_ = frame;
     context_menu_preview_node_.reset();
-    Send(new PrintHostMsg_RequestPrintPreview(routing_id()));
+    RequestPrintPreview();
   } else {
     Print(frame, NULL);
   }
@@ -299,7 +299,7 @@ void PrintWebViewHelper::OnPrintNodeUnderContextMenu() {
   if (is_preview_) {
     context_menu_preview_node_.reset(new WebNode(context_menu_node));
     script_initiated_preview_frame_ = NULL;
-    Send(new PrintHostMsg_RequestPrintPreview(routing_id()));
+    RequestPrintPreview();
   } else {
     WebNode duplicate_node(context_menu_node);
     Print(duplicate_node.document().frame(), &duplicate_node);
@@ -310,7 +310,7 @@ void PrintWebViewHelper::OnInitiatePrintPreview() {
   DCHECK(is_preview_);
   script_initiated_preview_frame_ = NULL;
   context_menu_preview_node_.reset();
-  Send(new PrintHostMsg_RequestPrintPreview(routing_id()));
+  RequestPrintPreview();
 }
 
 void PrintWebViewHelper::Print(WebKit::WebFrame* frame, WebKit::WebNode* node) {
@@ -798,4 +798,9 @@ void PrintWebViewHelper::DisplayPrintJobError() {
   render_view()->runModalAlertDialog(
       web_view->mainFrame(),
       l10n_util::GetStringUTF16(IDS_PRINT_SPOOL_FAILED_ERROR_TEXT));
+}
+
+void PrintWebViewHelper::RequestPrintPreview() {
+  old_print_pages_params_.reset();
+  Send(new PrintHostMsg_RequestPrintPreview(routing_id()));
 }

chrome/renderer/print_web_view_helper.h
@@ -259,6 +259,8 @@ class PrintWebViewHelper : public RenderViewObserver ,
   // Displays the print job error message to the user.
   void DisplayPrintJobError();
 
+  void RequestPrintPreview();
+
   WebKit::WebView* print_web_view_;
 
   // The frame to print for script initiated print preview.