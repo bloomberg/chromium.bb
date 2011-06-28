a8725dd65f6dfb9bfe956e23cb2e85e3387fbb4e
dpapad@chromium.org <dpapad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-28 22:53:21 +0000

    Print Preview: Fixing showing/hiding of printing options.
    Also
    -> Fixing showing/hiding of error messages underneath the copies and pages
       textfields.
    -> Cleaning up after animations so that we dont end up polluting the DOM with
       new <style> elements.
    
    BUG=87414
    TEST=Enter an invalid value in the copies textfield. Observe the animation.
    Erase it and observe again. Switch selected printer and observe the options
    fade in and out.
    
    
    Review URL: http://codereview.chromium.org/7253042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@90860 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ad5956c54d1dfea2af30795bbd60779a45b4382e

--- chrome/browser/resources/print_preview/print_preview_animations.js
+++ chrome/browser/resources/print_preview/print_preview_animations.js
@@ -13,11 +13,17 @@ function addAnimation(code) {
   var el = document.createElement('style');
   el.type = 'text/css';
   el.appendChild(rules);
+  el.setAttribute('id', name);
   document.body.appendChild(el);
 
   return name;
 }
 
+/**
+ * Fades in an element. Used for both printing options and error messages
+ * appearing underneath the textfields.
+ * @param {HTMLElement} el The element to be faded in.
+ */
 function fadeInElement(el) {
   if (el.classList.contains('visible'))
     return;
@@ -34,15 +40,42 @@ function fadeInElement(el) {
   el.addEventListener('webkitAnimationEnd', function() {
     el.style.height = '';
     el.style.webkitAnimationName = '';
+    fadeInOutCleanup(animName);
+    el.removeEventListener('webkitAnimationEnd', arguments.callee, false);
   }, false );
 }
 
+/**
+ * Fades out an element. Used for both printing options and error messages
+ * appearing underneath the textfields.
+ * @param {HTMLElement} el The element to be faded out.
+ */
 function fadeOutElement(el) {
   if (!el.classList.contains('visible'))
     return;
-  el.style.webkitAnimationName = '';
+  el.style.height = 'auto';
+  var height = el.offsetHeight;
+  el.style.height = height + 'px';
+  var animName = addAnimation(
+    '0% { opacity: 1; height: ' + height + 'px; }' +
+    '100% { opacity: 1; height: 0; }');
+  el.style.webkitAnimationName = animName;
   el.classList.add('closing');
   el.classList.remove('visible');
+  el.addEventListener('webkitAnimationEnd', function() {
+    fadeInOutCleanup(animName);
+    el.removeEventListener('webkitAnimationEnd', arguments.callee, false);
+  }, false );
+}
+
+/**
+ * Removes the <style> element corrsponding to |animationName| from the DOM.
+ * @param {string} animationName The name of the animation to be removed.
+ */
+function fadeInOutCleanup(animationName) {
+  animEl = document.getElementById(animationName);
+  if (animEl)
+    animEl.parentNode.removeChild(animEl);
 }
 
 function showLoadingAnimation() {
