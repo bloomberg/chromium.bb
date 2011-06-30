b8f2f2a55c2d46781967ef74b98059baf49c8213
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-06-30 05:23:17 +0000

    Fix to make the behavior of TextfieldWithMargin consistent with Textfield
    Add call to views::Textfield::OnKeyPressed in TextfieldWithMargin::OnKeyPressed
    Without this call, HandleKeyEvent of controller will not be called by NativeTextfieldViews.
    
    BUG=None
    TEST=None
    
    Review URL: http://codereview.chromium.org/7284001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91090 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2ae23e799a4f85edff8eae296b9e6eb663e2010c

--- chrome/browser/chromeos/login/textfield_with_margin.cc
+++ chrome/browser/chromeos/login/textfield_with_margin.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -40,7 +40,7 @@ bool TextfieldWithMargin::OnKeyPressed(const views::KeyEvent& e) {
     SetText(string16());
     return true;
   }
-  return false;
+  return views::Textfield::OnKeyPressed(e);
 }
 
 }  // namespace chromeos
