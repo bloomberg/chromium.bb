32814dedf213018aeca05ccfe2099bd4c1a7905e
Peter Mayo <petermayo@chromium.org>
2011-07-01 01:48:45 -0400

    Disable prebuilts on the tangent config.
    BUG=none
    TEST=unittests
    
    Change-Id: I21b0de332b209f1b7f540e37cfdd16668225ab1f
    Reviewed-on: http://gerrit.chromium.org/gerrit/3543
    Reviewed-by: David James <davidjames@chromium.org>
    Tested-by: Peter Mayo <petermayo@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -444,12 +444,14 @@ add_config('arm-tegra2_seaboard-toolchain', [arm, internal, full, official, {
 add_config('arm-tegra2_seaboard-tangent-private-full', [internal, arm, full, {
   'board' : 'tegra2_seaboard',
   'profile' : 'cosine',
+  'prebuilts' : False,
 }])
 
 add_config('arm-tegra2_seaboard-tangent-private-bin',
   [internal, arm, binary, {
     'board' : 'tegra2_seaboard',
     'profile' : 'cosine',
+    'prebuilts' : False,
 }])
 
 add_config('x86-mario-release', [internal, full, official, release, {
