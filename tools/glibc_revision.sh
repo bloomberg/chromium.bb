bloomberg/chromium.bb

commit 3bb2e42098f5f231e3b3d24974b44e5b1a9fc146

Fix typo in r5890
http://codereview.chromium.org/7301001/ got committed as rev 5890
with a typo that completely defeated its effect.  D'oh!

BUG=http://code.google.com/p/nativeclient/issues/detail?id=1096
TEST=none

R=eaeltsin@google.com
TBR=http://codereview.chromium.org/7289042/

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5894 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: e4011d7cfcbb30b55794add664d513654df20b74

tools/Makefile
@@ -510,7 +510,7 @@ BUILD/stamp-$(CROSSARCH)-newlib: | SRC/newlib BUILD newlib-libc-script
 		      --enable-newlib-io-long-double \
 		      --enable-newlib-io-c99-formats \
 		      --enable-newlib-mb \
-		      libc_cv_initfinit_array=yes \
+		      libc_cv_initfini_array=yes \
 	    --prefix=$(SDKROOT) \
 	    CFLAGS="-O2 $(CFLAGS)" \
 	    CFLAGS_FOR_TARGET='$(NEWLIB_CFLAGS)' \

commit 5284d15472ff3eab2ca90cc5204797ba0f2ea183

Restore necessary typo broken in r5894
It wasn't my typo after all, the typo was in newlib.
Put it back the way it needs to be.

Also temporarily defuse the glibc revision detector so we don't
have to wait two more cycles to get a toolchain built.

BUG=none
TEST=none

R=sehr@google.com
TBR=http://codereview.chromium.org/7282049/

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5897 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: 8ee2b33254ca36cfb2fb05f0c7cd490054285217

tools/Makefile
@@ -510,7 +510,7 @@ BUILD/stamp-$(CROSSARCH)-newlib: | SRC/newlib BUILD newlib-libc-script
 		      --enable-newlib-io-long-double \
 		      --enable-newlib-io-c99-formats \
 		      --enable-newlib-mb \
-		      libc_cv_initfini_array=yes \
+		      libc_cv_initfinit_array=yes \
 	    --prefix=$(SDKROOT) \
 	    CFLAGS="-O2 $(CFLAGS)" \
 	    CFLAGS_FOR_TARGET='$(NEWLIB_CFLAGS)' \

tools/glibc_revision.sh
@@ -10,7 +10,8 @@ if [[ "$(uname -s)" = Darwin ]] || [[ "$(uname -o)" != "Cygwin" ]] ; then
 else
   SVN="svn.bat"
 fi
-for i in glibc_revision.sh REVISIONS Makefile ; do
+# glibc_revision.sh Makefile
+for i in REVISIONS ; do
   NREV="$("$SVN" log "$i" 2>/dev/null | head -n 2 | tail -n 1 |
                                                 sed -e s'+r\([0-9]*\) .*+\1+')"
   if [[ "$NREV" -gt "$REVISION" ]]; then