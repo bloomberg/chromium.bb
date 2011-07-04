288009887efd0818ea3e1fcc3bec0dc61f9939cd
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-04 21:28:58 +0000

    Rename range.mm to range_mac.mm.
    For consistency, move the windows-specific code from range.cc into
    range_win.cc.
    
    Do the same for unit tests.
    
    In addition to this being nicer in general, the make build also gets
    confused if two files have the same path and basename, but different
    extensions (range.cc, range.mm), which is fixed by this CL.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7248068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91480 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c44d8d39ba494047175928e4f83fa2d1ffbe7f60

--- ui/base/range/range.cc
+++ ui/base/range/range.cc
@@ -75,40 +75,6 @@ Range Range::Intersect(const Range& range) const {
   return Range(min, max);
 }
 
-#if defined(OS_WIN)
-Range::Range(const CHARRANGE& range, LONG total_length) {
-  // Check if this is an invalid range.
-  if (range.cpMin == -1 && range.cpMax == -1) {
-    *this = InvalidRange();
-  } else {
-    DCHECK_GE(range.cpMin, 0);
-    set_start(range.cpMin);
-    // {0,-1} is the "whole range" but that doesn't mean much out of context,
-    // so use the |total_length| parameter.
-    if (range.cpMax == -1) {
-      DCHECK_EQ(0, range.cpMin);
-      DCHECK_NE(-1, total_length);
-      set_end(total_length);
-    } else {
-      set_end(range.cpMax);
-    }
-  }
-}
-
-CHARRANGE Range::ToCHARRANGE() const {
-  CHARRANGE r = { -1, -1 };
-  if (!IsValid())
-    return r;
-
-  const LONG kLONGMax = std::numeric_limits<LONG>::max();
-  DCHECK_LE(static_cast<LONG>(start()), kLONGMax);
-  DCHECK_LE(static_cast<LONG>(end()), kLONGMax);
-  r.cpMin = start();
-  r.cpMax = end();
-  return r;
-}
-#endif  // defined(OS_WIN)
-
 std::ostream& operator<<(std::ostream& out, const ui::Range& range) {
   return out << "{" << range.start() << "," << range.end() << "}";
 }
