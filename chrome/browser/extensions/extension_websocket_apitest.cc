bf875c7f85326643c5a0a17c48caadaa101b172f
yutak@chromium.org <yutak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-06 09:46:05 +0000

    Disable ExtensionApiTest.WebSocket temporarily.
    This test can be reenabled after WebKit r90445 is merged into trunk.
    
    BUG=88445
    TEST=none
    TBR=caseq
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91538 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 79b9304993c14cf6f94c88f9ffe0afdb44218ca5

--- chrome/browser/extensions/extension_websocket_apitest.cc
+++ chrome/browser/extensions/extension_websocket_apitest.cc
@@ -8,7 +8,9 @@
 #include "chrome/test/ui_test_utils.h"
 #include "net/base/mock_host_resolver.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WebSocket) {
+// WebSocket layout tests have moved to a new directory.
+// See http://crbug.com/88445.
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_WebSocket) {
   FilePath websocket_root_dir;
   PathService::Get(chrome::DIR_TEST_DATA, &websocket_root_dir);
   websocket_root_dir = websocket_root_dir.AppendASCII("layout_tests")
