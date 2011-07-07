cedb09779b7641d625c1ab68ec3c25b438941570
jond@google.com <jond@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-07 21:15:40 +0000

    New documentation for one of the file io headers. Review URL: http://codereview.chromium.org/7227011
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91754 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d0fb7b40556a8cccf0047020518e5b00022959d0

--- ppapi/c/pp_file_info.h
+++ ppapi/c/pp_file_info.h
@@ -9,29 +9,89 @@
 #include "ppapi/c/pp_stdint.h"
 #include "ppapi/c/pp_time.h"
 
+/**
+ * @file
+ * This file defines three enumerations for use in the PPAPI C file IO APIs.
+ */
+
+/**
+ * @addtogroup Enums
+ * @{
+ */
+
+/**
+ * The <code>PP_FileType</code> enum contains file type constants.
+ */
 typedef enum {
+  /** A regular file type */
   PP_FILETYPE_REGULAR,
+  /** A directory */
   PP_FILETYPE_DIRECTORY,
-  PP_FILETYPE_OTHER  /* A catch-all for unidentified types. */
+  /** A catch-all for unidentified types */
+  PP_FILETYPE_OTHER
 } PP_FileType;
 PP_COMPILE_ASSERT_SIZE_IN_BYTES(PP_FileType, 4);
 
+/**
+ * The <code>PP_FileSystemType</code> enum contains file system type constants.
+ */
 typedef enum {
-  PP_FILESYSTEMTYPE_INVALID = 0,  /* For identifying invalid return values. */
+  /** For identified invalid return values */
+  PP_FILESYSTEMTYPE_INVALID = 0,
+  /** For external file system types */
   PP_FILESYSTEMTYPE_EXTERNAL,
+  /** For local persistant file system types */
   PP_FILESYSTEMTYPE_LOCALPERSISTENT,
+  /** For local temporary file system types */
   PP_FILESYSTEMTYPE_LOCALTEMPORARY
 } PP_FileSystemType;
 PP_COMPILE_ASSERT_SIZE_IN_BYTES(PP_FileSystemType, 4);
+/**
+ * @}
+ */
 
+/**
+ * @addtogroup Structs
+ * @{
+ */
+/**
+ * The <code>PP_FileInfo</code> struct represents all information about a file,
+ * such as size, type, and creation time.
+ */
 struct PP_FileInfo {
+  /** This value represents the size of the file. */
   int64_t size;  /* Measured in bytes */
+
+  /**
+   * This value represents the type of file as defined by the
+   * <code>PP_FileType</code> enum
+   */
   PP_FileType type;
+
+  /**
+   * This value represents the file system type of the file as defined by the
+   * <code>PP_FileSystemType</code> enum.
+   */
   PP_FileSystemType system_type;
+
+  /**
+   * This value represents the creation time of the file.
+   */
   PP_Time creation_time;
+
+  /**
+   * This value represents the last time the file was accessed.
+   */
   PP_Time last_access_time;
+
+  /**
+   * This value represents the last time the file was modified.
+   */
   PP_Time last_modified_time;
 };
 PP_COMPILE_ASSERT_STRUCT_SIZE_IN_BYTES(PP_FileInfo, 40);
+/**
+ * @}
+ */
 
 #endif  /* PPAPI_C_PP_FILE_INFO_H_ */
