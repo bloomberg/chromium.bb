3da4aecb8a65ed4281178cf9b18def6b1334e48b
polina@google.com <polina@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-07-08 20:57:51 +0000

    Upgrade gyp_extract to handle variable target_names like ppapi_cpp<(nacl_ppapi_library_suffix).
    BUG=none
    TEST=manually in a chrome client
    Review URL: http://codereview.chromium.org/7329017
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@5960 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 8388a548083eac464c744fae3fba2dcc939f7e78

--- site_scons/gyp_extract.py
+++ site_scons/gyp_extract.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-# Copyright 2011 The Native Client Authors.  All rights reserved.
-# Use of this source code is governed by a BSD-style license that can
-# be found in the LICENSE file.
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 import re
 
@@ -16,6 +16,9 @@ def LoadGypFile(gyp_filename):
   """
   return eval(open(gyp_filename).read(), {}, {})
 
+def IgnoreVariables(name):
+  """E.g. map ppapi_cpp<(nacl_ppapi_library_suffix) to ppapi_cpp."""
+  return re.sub('\<\([^)]*\)$', '', name)
 
 def GypTargetSources(gyp_data, target_name, pattern):
   """Extract a sources from a target matching a given pattern.
@@ -28,7 +31,7 @@ def GypTargetSources(gyp_data, target_name, pattern):
     A list of strings containing source filenames.
   """
   targets = [target for target in gyp_data['targets']
-             if target['target_name'] == target_name]
+             if IgnoreVariables(target['target_name']) == target_name]
   # Only one target should have this name.
   assert len(targets) == 1
   desired_target = targets[0]
