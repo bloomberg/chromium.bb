7b1c938045ab6bc22cd9dfe5b8c73d1be189571d
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-10 01:38:51 +0000

    Re-enable SafeBrowsingBlockingPageTest.PhishingLearnMore
    This test might have failed due to bug 87970. thakis just rebooted the test machine so I'm re-enabling the test to see if it now passes.
    
    BUG=88831
    TEST=None
    Review URL: http://codereview.chromium.org/7331031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@91965 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7c914fbc0b2c29c5ddf66caa600215308a9a5df8

--- chrome/browser/safe_browsing/safe_browsing_blocking_page_test.cc
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page_test.cc
@@ -468,7 +468,7 @@ IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest, PhishingReportError) {
 }
 
 IN_PROC_BROWSER_TEST_F(SafeBrowsingBlockingPageTest,
-                       FLAKY_PhishingLearnMore) {
+                       PhishingLearnMore) {
   GURL url = test_server()->GetURL(kEmptyPage);
   AddURLResult(url, SafeBrowsingService::URL_PHISHING);
 
