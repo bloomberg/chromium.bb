efae5444bcf15a50f0dfb3f0c1b57d654dbea75c
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-12 19:21:14 +0000

    sync-webkit-git: work around From() in DEPS
    We don't need From() to do anything, we just need it to be
    available.
    
    Review URL: http://codereview.chromium.org/7349013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92213 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 78f87ba172e524823776903a63be5958641ef421

--- tools/sync-webkit-git.py
+++ tools/sync-webkit-git.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -57,7 +57,8 @@ def GetGClientBranchName():
 
 def GetWebKitRev():
   """Extract the 'webkit_revision' variable out of DEPS."""
-  locals = {'Var': lambda _: locals["vars"][_]}
+  locals = {'Var': lambda _: locals["vars"][_],
+            'From': lambda *args: None}
   execfile('DEPS', {}, locals)
   return locals['vars']['webkit_revision']
 
