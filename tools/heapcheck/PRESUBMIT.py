f3bde1600b3a38e37aeb954a76aad3327b74af68
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-14 07:40:20 +0000

    Fix the heapcheck presubmit script to parse the whole suppression file instead of diffs.
    BUG=84772
    Review URL: http://codereview.chromium.org/7354018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92494 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 95c26346db7dcfc103b5a3d36be7b3d1a771875f

--- tools/heapcheck/PRESUBMIT.py
+++ tools/heapcheck/PRESUBMIT.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -8,38 +8,38 @@ for more details on the presubmit API built into gcl.
 """
 
 def CheckChange(input_api, output_api):
-  """Checks the memcheck suppressions files for bad data."""
+  """Checks the heapcheck suppressions files for bad data."""
   suppressions = {}
   errors = []
   skip_next_line = False
-  func_re = input_api.re.compile('[a-z_.]+\(.+\)$')
-  for f, line_num, line in input_api.RightHandSideLines(lambda x:
-      x.LocalPath().endswith('.txt')):
-    line = line.lstrip()
-    if line.startswith('#') or not line:
-      continue
+  for f in filter(lambda x: x.LocalPath().endswith('.txt'),
+                  input_api.AffectedFiles()):
+    for line, line_num in zip(f.NewContents(),
+                              xrange(1, len(f.NewContents()) + 1)):
 
-    if skip_next_line:
-      if suppressions.has_key(line):
-        errors.append('suppression with name "%s" at %s line %s has already '
-                      'been defined at line %s' % (line, f.LocalPath(),
-                                                   line_num,
-                                                   suppressions[line][1]))
-      else:
-        suppressions[line] = (f, line_num)
-      skip_next_line = False
-      continue
-    if line == '{':
-      skip_next_line = True
-      continue
-    if (line.startswith('fun:') or line.startswith('obj:') or
-        line == 'Heapcheck:Leak' or line == '}' or
-        line == '...'):
-      continue
-    if func_re.match(line):
-      continue
-    errors.append('"%s" is probably wrong: %s line %s' % (line, f.LocalPath(),
-                                                          line_num))
+      line = line.lstrip()
+      if line.startswith('#') or not line:
+        continue
+
+      if skip_next_line:
+        if suppressions.has_key(line):
+          errors.append('suppression with name "%s" at %s line %s has already '
+                        'been defined at line %s' % (line, f.LocalPath(),
+                                                     line_num,
+                                                     suppressions[line][1]))
+        else:
+          suppressions[line] = (f, line_num)
+        skip_next_line = False
+        continue
+      if line == '{':
+        skip_next_line = True
+        continue
+      if (line.startswith('fun:') or line.startswith('obj:') or
+          line == 'Heapcheck:Leak' or line == '}' or
+          line == '...'):
+        continue
+      errors.append('"%s" is probably wrong: %s line %s' % (line, f.LocalPath(),
+                                                            line_num))
   if errors:
     return [output_api.PresubmitError('\n'.join(errors))]
   return []
