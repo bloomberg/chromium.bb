a0060ac6dba71f694c6e22ec320a30db3f1353d8
yutak@chromium.org <yutak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-14 05:15:38 +0000

    Reenable ExtensionApiTest.WebSocket.
    In WebKit, WebSocket tests have moved from http/tests/websocket/tests/ to
    http/tests/websocket/tests/hixie76/. Hardcoded URL in "websocket" extension
    needs to be updated.
    
    BUG=88445
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7351009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92482 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 61abf81a22c02e5f79cff6975e1edea83b55ec44

--- chrome/browser/extensions/extension_websocket_apitest.cc
+++ chrome/browser/extensions/extension_websocket_apitest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,9 +8,7 @@
 #include "chrome/test/ui_test_utils.h"
 #include "net/base/mock_host_resolver.h"
 
-// WebSocket layout tests have moved to a new directory.
-// See http://crbug.com/88445.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_WebSocket) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WebSocket) {
   FilePath websocket_root_dir;
   PathService::Get(chrome::DIR_TEST_DATA, &websocket_root_dir);
   websocket_root_dir = websocket_root_dir.AppendASCII("layout_tests")
