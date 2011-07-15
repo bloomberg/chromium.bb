8d35081d7c32458cc530e61fafc55e7b49af7c9c
ahendrickson@chromium.org <ahendrickson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-15 15:49:19 +0000

    Revert 91769 - Revert 89600 - Treat ERR_CONNECTION_CLOSED as a succesful download.
    Some servers will send a Content-Length which exceeds the number of bytes sent
    in the message body.
    
    Although this violates the RFC spec, other major browsers permit it
    and treat a download as complete in that case. Chrome did as well
    prior to M12, and now some users are unable to access the item that
    they downloaded.
    
    We ran an experiment that caused an error in this case, to gather UMA statistics.  This reverts that experiment.
    
    BUG=85549
    TEST=Go to server which advertises larger Content-Length than number of bytes
    sent, confirm that the download is again reported as interrupted.
    
    
    Review URL: http://codereview.chromium.org/7353023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92697 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1822a424381f493372697dfb260d7a760363f430

--- chrome/browser/download/download_manager.cc
+++ chrome/browser/download/download_manager.cc
@@ -643,9 +643,7 @@ void DownloadManager::OnResponseCompleted(int32 download_id,
   // body, and then close the connection. Other browsers - IE8, Firefox 4.0.1,
   // and Safari 5.0.4 - treat the download as complete in this case, so we
   // follow their lead.
-  // EXPERIMENT(ahendrickson) -- Treat ERR_CONNECTION_CLOSED as an error to
-  // gather statistics.  To be reverted in a few days.
-  if (os_error == 0) {
+  if (os_error == 0 || os_error == net::ERR_CONNECTION_CLOSED) {
     OnAllDataSaved(download_id, size, hash);
   } else {
     OnDownloadError(download_id, size, os_error);
