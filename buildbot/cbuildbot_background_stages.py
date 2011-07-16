3193f161f05bcdfca99a96b013b3896e25e9b63e
David James <davidjames@chromium.org>
2011-07-15 17:49:26 -0700

    Show incremental progress in background stages.
    Showing incremental progress in background stages makes the output nicer and
    makes it easier to see what the buildbot is doing.
    
    BUG=chromium-os:17670
    TEST=Run cbuildbot run and test incremental output. It looks nice!
    
    Change-Id: Iaaaffbdc4df5d37b57fc1bff05e876683c3c72dd
    Reviewed-on: http://gerrit.chromium.org/gerrit/4218
    Reviewed-by: Ryan Cui <rcui@chromium.org>
    Tested-by: David James <davidjames@chromium.org>

--- buildbot/cbuildbot_background_stages.py
+++ buildbot/cbuildbot_background_stages.py
@@ -6,19 +6,13 @@
 
 import multiprocessing
 import os
+import Queue
 import sys
 import tempfile
 
 from chromite.buildbot import cbuildbot_stages as stages
 
-def _PrintFile(filename):
-  """Print the contents of the specified file to stdout."""
-  output = file(filename)
-  for line in output:
-    sys.stdout.write(line)
-
-  output.close()
-
+_PRINT_INTERVAL = 1
 
 class BackgroundStages(multiprocessing.Process):
   """Run a list of stages in sequence in the background."""
@@ -26,21 +20,38 @@ class BackgroundStages(multiprocessing.Process):
   def __init__(self):
     multiprocessing.Process.__init__(self)
     self._stages = []
+    self._files = []
     self._queue = multiprocessing.Queue()
 
   def AddStage(self, stage):
     """Add a stage to the list of stages to run in the background."""
-    self._stages.append(stage)
+    output = tempfile.NamedTemporaryFile(delete=False)
+    self._stages.append((stage, output))
 
   def WaitForStage(self):
     """Wait for the first stage to complete.
 
     Return whether an exception was caught."""
     assert not self.Empty()
-    error, results, filename = self._queue.get()
-    _PrintFile(filename)
-    os.unlink(filename)
-    self._stages.pop(0)
+    stage, output = self._stages.pop(0)
+    pos = 0
+    more_output = True
+    while more_output:
+      # Check whether the process is finished.
+      try:
+        error, results = self._queue.get(True, _PRINT_INTERVAL)
+        more_output = False
+      except Queue.Empty:
+        more_output = True
+
+      # Print output so far.
+      output.seek(pos)
+      for line in output:
+        sys.stdout.write(line)
+      pos = output.tell()
+
+    output.close()
+    os.unlink(output.name)
     for result in results:
       stages.Results.Record(*result)
 
@@ -58,9 +69,8 @@ class BackgroundStages(multiprocessing.Process):
 
     stdout_fileno = sys.stdout.fileno()
     stderr_fileno = sys.stderr.fileno()
-    for stage in self._stages:
+    for stage, output in self._stages:
       # Send all output to a named temporary file.
-      output = tempfile.NamedTemporaryFile(delete=False)
       os.dup2(output.fileno(), stdout_fileno)
       os.dup2(output.fileno(), stderr_fileno)
       error = True
@@ -73,4 +83,4 @@ class BackgroundStages(multiprocessing.Process):
 
       output.close()
       results = stages.Results.Get()
-      self._queue.put((error, results, output.name))
+      self._queue.put((error, results))
