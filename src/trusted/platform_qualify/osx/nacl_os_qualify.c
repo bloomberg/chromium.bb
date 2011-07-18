b4961ead440c33819a30b72ac160dbe37150785d
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-07-18 20:50:59 +0000

    Disable x86-32 NaCl on 64-bit Mac OS X kernels
    This will prevent NaCl from causing kernel panics when run on buggy
    kernels which cannot handle %cs being changed.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=1712
    TEST=on a Mac running a 64-bit kernel:
     * "run_hello_world_test platform=x86-32" exits with an error
     * "run_hello_world_test platform=x86-64" still works
    
    Review URL: http://codereview.chromium.org/7381008
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6105 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: ae71d64fc110b7006dd0d1c8a3ec0102a82c8c34

--- src/trusted/platform_qualify/osx/nacl_os_qualify.c
+++ src/trusted/platform_qualify/osx/nacl_os_qualify.c
@@ -1,16 +1,42 @@
 /*
- * Copyright 2008 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
+#include <string.h>
+#include <sys/utsname.h>
 
+#include "native_client/src/shared/platform/nacl_log.h"
 #include "native_client/src/trusted/platform_qualify/nacl_os_qualify.h"
 
 /*
  * Returns 1 if the operating system can run Native Client modules.
  */
 int NaClOsIsSupported() {
+#if NACL_ARCH(NACL_BUILD_ARCH) == NACL_x86 && NACL_BUILD_SUBARCH == 32
+  struct utsname info;
+  if (uname(&info) != 0) {
+    NaClLog(LOG_ERROR, "NaClOsIsSupported: uname() failed\n");
+    return 0;
+  }
+  if (strcmp(info.machine, "x86_64") == 0) {
+    /*
+     * TODO(mseaborn): We believe the kernel bug is fixed in Mac OS X
+     * 10.7 (Lion).  We could check for the new kernel version or OS
+     * version and enable NaCl on new systems.
+     */
+    NaClLog(LOG_ERROR,
+            "NaClOsIsSupported: "
+            "This system is running a 64-bit Mac OS X kernel.  "
+            "64-bit Mac kernels are buggy and cannot handle "
+            "Native Client's x86-32 sandbox.  "
+            "A workaround is to switch to using a 32-bit kernel, which will "
+            "be capable of running Native Client.  For more information, see "
+            "http://code.google.com/p/nativeclient/issues/detail?id=1712\n");
+    return 0;
+  }
+#endif
   return 1;
 }
 
