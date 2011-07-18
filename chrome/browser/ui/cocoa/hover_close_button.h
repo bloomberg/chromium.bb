91745d09eb5964c2bcc1f5ef4a7beb00e3e3528a
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-18 21:46:01 +0000

    HoverCloseButton 10.6 SDK compatibility.
    In the 10.6 SDK, -[NSAnimation setDelegate:] accepts an
    id<NSAnimationDelegate> parameter.
    
    BUG=none
    TEST=10.6 SDK build
    Review URL: http://codereview.chromium.org/7400029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@92900 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7364282aa314f209fa8e7a51903c6e6447a23f6e

--- chrome/browser/ui/cocoa/hover_close_button.h
+++ chrome/browser/ui/cocoa/hover_close_button.h
@@ -4,12 +4,14 @@
 
 #include "chrome/browser/ui/cocoa/hover_button.h"
 
+#include "base/mac/cocoa_protocols.h"
+
 @class GTMKeyValueAnimation;
 
 // The standard close button for our Mac UI which is the "x" that changes to a
 // dark circle with the "x" when you hover over it. At this time it is used by
 // the download bar, info bar and tabs.
-@interface HoverCloseButton : HoverButton {
+@interface HoverCloseButton : HoverButton<NSAnimationDelegate> {
  @private
   GTMKeyValueAnimation* fadeOutAnimation_;
   HoverState previousState_;
