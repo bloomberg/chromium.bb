680633578b743119517b4ddec69dadcbb8d6a995
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-19 11:58:29 +0000

    Enable browser compositor on all TOUCH_UI builds.
    Even Chrome OS builds of chromium.
    
    BUG=none
    TEST=build with GYP_DEFINES="chromeos=1 touchui=1 use_ibus=0" on the desktop
    
    Review URL: http://codereview.chromium.org/7396028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93001 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9f3b3b4ca41f5df2924b9cc36d1d91082a9102c7

--- views/view.cc
+++ views/view.cc
@@ -42,7 +42,7 @@ namespace {
 
 // Whether to use accelerated compositing when necessary (e.g. when a view has a
 // transformation).
-#if !defined(OS_CHROMEOS)
+#if !defined(OS_CHROMEOS) || defined(TOUCH_UI)
 bool use_acceleration_when_possible = true;
 #else
 bool use_acceleration_when_possible = false;
