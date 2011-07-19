065fed02d413f5716ddd0a3a8eccd7c38a588f1b
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-19 19:28:01 +0000

    Add support for text/html to bookmarks gtk drag/drop.
    R=estade@chromium.org
    BUG=none
    TEST=BookmarkUtilsGtkTest.WriteBookmarkToSelectionHTML
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=92744
    
    Review URL: http://codereview.chromium.org/7377003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93075 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: afa3c18d08b411bda2975a5ae6f33a4ed939875f

--- chrome/browser/ui/gtk/bookmarks/bookmark_utils_gtk_unittest.cc
+++ chrome/browser/ui/gtk/bookmarks/bookmark_utils_gtk_unittest.cc
@@ -2,7 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+#include "base/utf_string_conversions.h"
+#include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/ui/gtk/bookmarks/bookmark_utils_gtk.h"
+#include "googleurl/src/gurl.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/dragdrop/gtk_dnd_util.h"
 
@@ -35,4 +39,18 @@ TEST(BookmarkUtilsGtkTest, GetNodesFromSelectionInvalid) {
   nodes = bookmark_utils::GetNodesFromSelection(NULL, &data,
       ui::CHROME_BOOKMARK_ITEM, NULL, NULL, NULL);
   EXPECT_EQ(0u, nodes.size());
+}
+
+TEST(BookmarkUtilsGtkTest, WriteBookmarkToSelectionHTML) {
+  BookmarkNode x(GURL("http://www.google.com"));
+  x.set_title(string16(ASCIIToUTF16("Google")));
+  GtkSelectionData data;
+  data.data = NULL;
+  data.length = 0;
+  bookmark_utils::WriteBookmarkToSelection(&x, &data, ui::TEXT_HTML, NULL);
+  std::string selection(reinterpret_cast<char*>(data.data), data.length);
+  EXPECT_EQ("<a href=\"http://www.google.com/\">Google</a>", selection);
+
+  // Free the copied data in GtkSelectionData
+  gtk_selection_data_set(&data, data.type, data.format, NULL, -1);
 }
