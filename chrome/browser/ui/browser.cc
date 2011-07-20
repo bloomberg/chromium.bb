2c06587fffe75e311a39441fadc7f9b83e778ff6
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-20 23:23:16 +0000

    [Mac] Fix crash opening prefs with no windows open.
    ShowSingletonTabOverwritingNTP() checks for contents != NULL _after_
    accessing contents.  Sigh.
    
    BUG=89901
    TEST=Close all windows.  Chose Chrome/Clear Browsing Data... menu item.  No crash.
    
    Review URL: http://codereview.chromium.org/7465008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93280 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 842a648cb58cd12f6ac04531a2e3e26943a87467

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1248,12 +1248,13 @@ void Browser::ShowSingletonTabOverwritingNTP(
     const browser::NavigateParams& params) {
   browser::NavigateParams local_params(params);
   TabContents* contents = GetSelectedTabContents();
-  const GURL& contents_url = contents->GetURL();
-  if (contents != NULL &&
-      (contents_url == GURL(chrome::kChromeUINewTabURL) ||
-       contents_url == GURL(chrome::kAboutBlankURL)) &&
-      browser::GetIndexOfSingletonTab(&local_params) < 0) {
-    local_params.disposition = CURRENT_TAB;
+  if (contents) {
+    const GURL& contents_url = contents->GetURL();
+    if ((contents_url == GURL(chrome::kChromeUINewTabURL) ||
+         contents_url == GURL(chrome::kAboutBlankURL)) &&
+        browser::GetIndexOfSingletonTab(&local_params) < 0) {
+      local_params.disposition = CURRENT_TAB;
+    }
   }
 
   browser::Navigate(&local_params);
