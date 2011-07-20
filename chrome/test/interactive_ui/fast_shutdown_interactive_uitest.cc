38e95098a367d7028ccd8f1ae5dcc9020162ff73
sreeram@chromium.org <sreeram@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-20 22:08:14 +0000

    Add debugging messages to diagnose flakiness on Chrome OS.
    FastShutdown.SlowTermination seems to be flaky (timing out) on Chrome OS. I've
    been trying to reproduce the failures on the try bot (linux_chromeos) for an
    entire week now, but without success. This might be because the try bot runs
    debug, and not release. I don't have access to any other CrOS devices. So this
    CL adds temporary debugging to the code; once the root cause of the timeouts
    has been isolated, I'll remove the debug messages.
    
    BUG=89173
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/7465003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93266 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 57c498ceb484f5a517982818721db22851b80861

--- chrome/test/interactive_ui/fast_shutdown_interactive_uitest.cc
+++ chrome/test/interactive_ui/fast_shutdown_interactive_uitest.cc
@@ -34,6 +34,14 @@ class FastShutdown : public UITest {
 #define MAYBE_SlowTermination SlowTermination
 #endif
 
+// Enable some temporary debugging to diagnose flakiness on Chrome OS.
+// http://crbug.com/89173. TODO(sreeram): Delete this soon!
+#if defined(OS_CHROMEOS)
+#define HERE(x) LOG(ERROR) << "Here " << (x)
+#else
+#define HERE(x)
+#endif
+
 // Loads the cookie store from |user_data_dir|. If the given |cookie| exists,
 // puts the cookie's value in |cookie_value| and sets |has_cookie| to true.
 // Sets |has_cookie| to false if the |cookie| wasn't found.
@@ -47,8 +55,10 @@ static void GetCookie(
       new SQLitePersistentCookieStore(
           user_data_dir.AppendASCII(chrome::kNotSignedInProfile)
                        .Append(chrome::kCookieFilename)));
+  HERE(1);
   std::vector<net::CookieMonster::CanonicalCookie*> cookies;
   ASSERT_TRUE(cookie_store->Load(&cookies));
+  HERE(2);
   *has_cookie = false;
   for (size_t i = 0; i < cookies.size(); ++i) {
     if (cookies[i]->IsEquivalent(cookie)) {
@@ -58,7 +68,9 @@ static void GetCookie(
   }
   cookie_store = NULL;
   ASSERT_TRUE(thread_helper->Run());
+  HERE(3);
   STLDeleteElements(&cookies);
+  HERE(4);
 }
 
 // This tests for a previous error where uninstalling an onbeforeunload
@@ -67,10 +79,13 @@ static void GetCookie(
 TEST_F(FastShutdown, MAYBE_SlowTermination) {
   // Set up the DB thread (used by the cookie store during its clean up).
   BrowserThread db_thread(BrowserThread::DB);
+  HERE(10);
   db_thread.Start();
+  HERE(11);
   scoped_refptr<base::ThreadTestHelper> thread_helper(
       new base::ThreadTestHelper(
           BrowserThread::GetMessageLoopProxyForThread(BrowserThread::DB)));
+  HERE(12);
 
   // Only the name, domain and path are used in IsEquivalent(), so we don't
   // care what the other fields have.
@@ -88,45 +103,58 @@ TEST_F(FastShutdown, MAYBE_SlowTermination) {
       false,         // secure
       false,         // httponly
       false);        // has_expires
+  HERE(13);
 
   bool has_cookie = false;
   std::string cookie_value;
 
   // Check that the cookie (to be set during unload) doesn't exist already.
   GetCookie(user_data_dir(), cookie, &has_cookie, &cookie_value, thread_helper);
+  HERE(14);
   EXPECT_FALSE(has_cookie);
   EXPECT_EQ("", cookie_value);
 
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
+  HERE(15);
   ASSERT_TRUE(browser.get());
   scoped_refptr<WindowProxy> window(browser->GetWindow());
+  HERE(16);
   ASSERT_TRUE(window.get());
 
   // This page has an unload handler.
   const FilePath dir(FILE_PATH_LITERAL("fast_shutdown"));
   const FilePath file(FILE_PATH_LITERAL("on_unloader.html"));
   NavigateToURL(ui_test_utils::GetTestUrl(dir, file));
+  HERE(17);
   gfx::Rect bounds;
   ASSERT_TRUE(window->GetViewBounds(VIEW_ID_TAB_CONTAINER, &bounds, true));
+  HERE(18);
   // This click will launch a popup which has a before unload handler.
   ASSERT_TRUE(window->SimulateOSClick(bounds.CenterPoint(),
                                       ui::EF_LEFT_BUTTON_DOWN));
+  HERE(19);
   ASSERT_TRUE(browser->WaitForTabCountToBecome(2));
+  HERE(20);
   // Close the tab, removing the one and only before unload handler.
   scoped_refptr<TabProxy> tab(browser->GetTab(1));
+  HERE(21);
   ASSERT_TRUE(tab->Close(true));
+  HERE(22);
 
   // Close the browser. We should launch the unload handler, which sets a
   // cookie that's stored to disk.
   ASSERT_TRUE(browser->ApplyAccelerator(IDC_CLOSE_WINDOW));
+  HERE(23);
 
   int exit_code = -1;
   ASSERT_TRUE(launcher_->WaitForBrowserProcessToQuit(
                   TestTimeouts::action_max_timeout_ms(), &exit_code));
+  HERE(24);
   EXPECT_EQ(0, exit_code);  // Expect a clean shutdown.
 
   // Read the cookie and check that it has the expected value.
   GetCookie(user_data_dir(), cookie, &has_cookie, &cookie_value, thread_helper);
+  HERE(25);
   EXPECT_TRUE(has_cookie);
   EXPECT_EQ("ohyeah", cookie_value);
 }
