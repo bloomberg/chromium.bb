48e325bbf3fbfc4f05d371df2f8e86ddac491023
Ryan Cui <rcui@chromium.org>
2011-07-20 14:46:37 -0700

    Fix cbuildbot_unittest.py failures
    BUG=chromium-os:17929
    TEST=ran unittest.
    
    Change-Id: I4380826d84da04e573b0518b8e53482a741ef895
    Reviewed-on: http://gerrit.chromium.org/gerrit/4439
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Tested-by: Ryan Cui <rcui@chromium.org>

--- buildbot/cbuildbot_unittest.py
+++ buildbot/cbuildbot_unittest.py
@@ -215,8 +215,7 @@ class FullInterfaceTest(unittest.TestCase):
     self.mox.StubOutWithMock(sys, 'exit')
     self.mox.StubOutWithMock(cbuildbot, '_GetInput')
     self.mox.StubOutWithMock(cros_lib, 'FindRepoDir')
-    self.mox.StubOutWithMock(cbuildbot, '_SetupRedirectOutputToFile')
-    self.mox.StubOutWithMock(cbuildbot, '_RunBuildStagesWithCheck')
+    self.mox.StubOutWithMock(cbuildbot, '_RunBuildStagesWrapper')
     self.mox.StubOutWithMock(cbuildbot, '_RunBuildStagesWithSudoProcess')
     self.mox.StubOutWithMock(cbuildbot.os.path, 'realpath')
 
@@ -226,8 +225,7 @@ class FullInterfaceTest(unittest.TestCase):
     cros_lib.FindRepoDir().InAnyOrder().AndReturn('/b/test_build1/.repo')
     (cbuildbot.os.path.realpath('/b/test_build1').InAnyOrder().
         AndReturn('/b/test_build1'))
-    cbuildbot._SetupRedirectOutputToFile().InAnyOrder()
-    cbuildbot._RunBuildStagesWithCheck(
+    cbuildbot._RunBuildStagesWrapper(
         mox.IgnoreArg(),
         mox.IgnoreArg(),
         mox.IgnoreArg()).InAnyOrder().AndReturn(True)
