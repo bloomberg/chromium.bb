ebd9e12bc246ceb31ae649fc6a5cadcb4f0f74f3
jasonwkim@google.com <jasonwkim@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-07-21 19:42:42 +0000

    fix typo
    TEST=manual
    
    Review URL: http://codereview.chromium.org/7479003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6152 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 7db42d62904fe64e041989029155c2b605c3b0c3

--- tests/spec2k/254.gap/run.train.sh
+++ tests/spec2k/254.gap/run.train.sh
@@ -7,7 +7,7 @@ PREFIX=${PREFIX:-}
 VERIFY=${VERIFY:-yes}
 EMU_HACK=${EMU_HACK:-yes}
 
-if [[ $1 =~ .*pnacl.* ]]; the
+if [[ $1 =~ .*pnacl.* ]]; then
   echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
   echo "@@@BUILD_STEP skip 254.gap due to BUG2036 in PNaCl@@@"
   echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
