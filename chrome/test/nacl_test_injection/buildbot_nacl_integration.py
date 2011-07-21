f3b2201889ca0702d80114290f33423fb040cfe7
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-21 19:54:51 +0000

    NaCl: Partially enable tests on Mac OS X
    Enable running the NaCl tests, but without making failures turn
    Buildbot red, because of some known flakiness issues on Mac.  This
    will give us more data about the flakiness and give us more confidence
    in enabling the tests fully when the failures are fixed.
    
    BUG=none
    TEST=nacl_integration
    
    Review URL: http://codereview.chromium.org/7464036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93454 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a91087dc51eac42a3df62615a03a3c5dcb4f21fd

--- chrome/test/nacl_test_injection/buildbot_nacl_integration.py
+++ chrome/test/nacl_test_injection/buildbot_nacl_integration.py
@@ -12,20 +12,12 @@ def Main():
   pwd = os.environ.get('PWD', '')
   is_integration_bot = 'nacl-chrome' in pwd
 
-  if not is_integration_bot:
-    # On the main Chrome waterfall, we may need to control where the tests are
-    # run.
-
-    # TODO(mseaborn): Enable these tests on Mac OS X.  We believe that
-    # NaCl's startup within Chromium may be flaky on Mac and
-    # occasionally fail due to a kernel bug that we need to work
-    # around.
-    # See http://code.google.com/p/nativeclient/issues/detail?id=1835
-    if sys.platform == 'darwin': return
-
-    # Uncomment the following line if there is skew in the PPAPI interface
-    # and the tests are failing.  Comment out once the issues are resolved.
-    #return
+  # On the main Chrome waterfall, we may need to control where the tests are
+  # run.
+  # Uncomment the following if there is skew in the PPAPI interface
+  # and the tests are failing.  Comment out once the issues are resolved.
+  #if not is_integration_bot:
+  #  return
 
   script_dir = os.path.dirname(os.path.abspath(__file__))
   test_dir = os.path.dirname(script_dir)
@@ -43,8 +35,23 @@ def Main():
     cmd.append('--disable_tests=ppapi_ppb_scrollbar_browser_test')
 
   print cmd
-  subprocess.check_call(cmd)
+  result = subprocess.call(cmd)
+
+  if sys.platform == 'darwin' and result != 0:
+    # TODO(mseaborn): Remove this when the issues below are fixed.
+    print """
+WARNING: Ignoring test failure on Mac because of known flakiness issues.
+
+We believe that NaCl's startup within Chromium may be flaky on Mac and
+occasionally fail due to a kernel bug that we need to work around.
+See http://code.google.com/p/nativeclient/issues/detail?id=1835
+Also, ASLR is breaking address space allocation.
+See http://code.google.com/p/nativeclient/issues/detail?id=2043
+"""
+    result = 0
+
+  return result
 
 
 if __name__ == '__main__':
-  Main()
+  sys.exit(Main())
