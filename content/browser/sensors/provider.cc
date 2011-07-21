c503dfbbf3b2fed3d2d630634544712955f0e025
cwolfe@chromium.org <cwolfe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-21 21:44:00 +0000

    Initial sensor event provider for testing views screen rotation
    The sensors API will unify various types of sensor data into a set of channels, each of which provides change events and periodic updates.
    
    This version of the API is intended only to support the experimental screen rotation code and is not for general use. In particular, the final listener will declare generic |OnSensorChange| and |OnSensorUpdate| methods, rather than the special-purpose |OnScreenOrientationChange|.
    
    BUG=none
    TEST=manual with http://codereview.chromium.org/7273073/
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=93094
    
    Review URL: http://codereview.chromium.org/7366011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93480 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 05dabf01dc00942787750525b64df393787b326a

---
+++ content/browser/sensors/provider.cc
@@ -0,0 +1,20 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/sensors/provider.h"
+#include "content/browser/sensors/provider_impl.h"
+
+namespace sensors {
+
+Provider* Provider::GetInstance() {
+  return ProviderImpl::GetInstance();
+}
+
+Provider::Provider() {
+}
+
+Provider::~Provider() {
+}
+
+}  // sensors
