f8a7a7829c5de2bef84464d6595403b2897a33a0
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-22 06:18:20 +0000

    Added prefs.PrefsTest.testDefaultUserImage to pyauto.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7479012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93562 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 32fb8bc8d240e54fe9f044baebf49600be4e2899

--- chrome/test/functional/prefs.py
+++ chrome/test/functional/prefs.py
@@ -190,6 +190,28 @@ class PrefsTest(pyauto.PyUITest):
     self.NavigateToURL(url)
     self.assertEqual(title, self.GetActiveTabTitle())
 
+  def testDefaultUserImage(self):
+    """Verify changing default user image prefs work."""
+
+    # Defined in src/chrome/browser/chromeos/login/user_manager.cc
+    k_logged_in_users = 'LoggedInUsers'
+    k_user_images = 'UserImages'
+    # Defined in src/chrome/browser/chromeos/login/default_user_images.cc
+    image1 = u'default:4'
+    image2 = u'default:5'
+
+    for image in image1, image2:
+      logged_in_user = \
+          self.GetLocalStatePrefsInfo().Prefs(k_logged_in_users)[0]
+      user_images = {}
+      user_images[logged_in_user] = image
+      self.SetLocalStatePrefs(k_user_images, user_images)
+      self.RestartBrowser(clear_profile=False)
+      current_user_images = self.GetLocalStatePrefsInfo().Prefs(k_user_images)
+      current_image = current_user_images.get(logged_in_user)
+      self.assertEqual(image, current_image)
+
+
 if __name__ == '__main__':
   pyauto_functional.Main()
 
