37ada27d9761dc8c63ad280207690fa1bd88a81f
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-22 22:42:49 +0000

    NaCl: Enable tests on Mac OS X
    The tests were partly enabled on Mac before, but with the return code
    ignored.  Now that we've seen the tests mostly work on the bots,
    enable them properly, but with two disabled.
    
    Update the comment to explain how to disable tests.
    
    Remove the disabling of "ppapi_ppb_scrollbar_browser_test", which I
    did erroneously: The test started passing in the change where I added
    this (because I updated DEPS to a revision where it passed), and
    anyway I got the test name wrong (omitting the "run_" suffix).
    
    Add a flush() call because otherwise the log message does not appear
    until after the tests on Windows.
    
    BUG=none
    TEST=nacl_integration
    
    Review URL: http://codereview.chromium.org/7484046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93719 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7b7b68ff19e6e56fa5f7f908ab7a6ef4ae55d043

--- chrome/test/nacl_test_injection/buildbot_nacl_integration.py
+++ chrome/test/nacl_test_injection/buildbot_nacl_integration.py
@@ -8,16 +8,31 @@ import subprocess
 import sys
 
 
-def Main():
+def Main(args):
   pwd = os.environ.get('PWD', '')
   is_integration_bot = 'nacl-chrome' in pwd
 
   # On the main Chrome waterfall, we may need to control where the tests are
   # run.
-  # Uncomment the following if there is skew in the PPAPI interface
-  # and the tests are failing.  Comment out once the issues are resolved.
+  # If there is serious skew in the PPAPI interface that causes all of
+  # the NaCl integration tests to fail, you can uncomment the
+  # following block.  (Make sure you comment it out when the issues
+  # are resolved.)  *However*, it is much preferred to add tests to
+  # the 'tests_to_disable' list below.
   #if not is_integration_bot:
   #  return
+
+  tests_to_disable = []
+  if sys.platform == 'darwin':
+    # The following test is failing on Mac OS X 10.5.  This may be
+    # because of a kernel bug that we might need to work around.
+    # See http://code.google.com/p/nativeclient/issues/detail?id=1835
+    # TODO(mseaborn): Remove this when the issue is resolved.
+    tests_to_disable.append('run_async_messaging_test')
+    # The following test fails on debug builds of Chromium.
+    # See http://code.google.com/p/nativeclient/issues/detail?id=2077
+    # TODO(mseaborn): Remove this when the issue is resolved.
+    tests_to_disable.append('run_ppapi_example_font_test')
 
   script_dir = os.path.dirname(os.path.abspath(__file__))
   test_dir = os.path.dirname(script_dir)
@@ -25,33 +40,13 @@ def Main():
   src_dir = os.path.dirname(chrome_dir)
   nacl_integration_script = os.path.join(
       src_dir, 'native_client/build/buildbot_chrome_nacl_stage.py')
-  cmd = [sys.executable, nacl_integration_script] + sys.argv[1:]
-
-  if sys.platform in ('win32', 'cygwin'):
-    # TODO(mseaborn): Re-enable this test when it passes.
-    # See http://code.google.com/p/nativeclient/issues/detail?id=2038
-    # It is disabled here because it fails in the debug build of Chromium
-    # but not in the release build that the NaCl buildbots test against.
-    cmd.append('--disable_tests=ppapi_ppb_scrollbar_browser_test')
-
-  print cmd
-  result = subprocess.call(cmd)
-
-  if sys.platform == 'darwin' and result != 0:
-    # TODO(mseaborn): Remove this when the issues below are fixed.
-    print """
-WARNING: Ignoring test failure on Mac because of known flakiness issues.
-
-We believe that NaCl's startup within Chromium may be flaky on Mac and
-occasionally fail due to a kernel bug that we need to work around.
-See http://code.google.com/p/nativeclient/issues/detail?id=1835
-Also, ASLR is breaking address space allocation.
-See http://code.google.com/p/nativeclient/issues/detail?id=2043
-"""
-    result = 0
-
-  return result
+  cmd = [sys.executable,
+         nacl_integration_script,
+         '--disable_tests=%s' % ','.join(tests_to_disable)] + args
+  sys.stdout.write('Running %s\n' % ' '.join(cmd))
+  sys.stdout.flush()
+  return subprocess.call(cmd)
 
 
 if __name__ == '__main__':
-  sys.exit(Main())
+  sys.exit(Main(sys.argv[1:]))
