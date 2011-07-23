8d9b7f71f00e237638ca8f6730c1a5a745f12041
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-23 02:39:58 +0000

    Add details tab to WebUI certificate viewer.
    The details view of the certificate viewer allows the user to explore all certificate fields in the hierarchy of certificates.
    
    BUG=None
    TEST=Tested that the certificate details can be navigated for all certificates in the hierarchy of a few https sites.
    
    
    Review URL: http://codereview.chromium.org/7480001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93766 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ccb067e65dfc79d5e336c84736fe66e76f5f585a

--- chrome/browser/resources/certificate_viewer.js
+++ chrome/browser/resources/certificate_viewer.js
@@ -13,9 +13,51 @@ cr.define('cert_viewer', function() {
     $('close').onclick = function() {
       window.close();
     }
-    var args = JSON.parse(chrome.dialogArguments);
+    cr.ui.decorate('tabbox', cr.ui.TabBox);
+
+    initializeTree($('hierarchy'), showCertificateFields);
+    initializeTree($('certfields'), showCertificateFieldValue);
+
     i18nTemplate.process(document, templateData);
+    stripGtkAccessorKeys();
+    var args = JSON.parse(chrome.dialogArguments);
     chrome.send('requestCertificateInfo', [args.cert]);
+  }
+
+  /**
+   * Initialize a cr.ui.Tree object from a given element using the specified
+   * change handler.
+   * @param {HTMLElement} tree The HTMLElement to style as a tree.
+   * @param {function()} handler Function to call when a node is selected.
+   */
+  function initializeTree(tree, handler) {
+    cr.ui.decorate(tree, cr.ui.Tree);
+    tree.detail = {payload: {}, children: {}};
+    tree.addEventListener('change', handler);
+  }
+
+  /**
+   * The tab name strings in the languages file have accessor keys indicated
+   * by a preceding & sign. Strip these out for now.
+   * @TODO(flackr) These accessor keys could be implemented with Javascript or
+   *     translated strings could be added / modified to remove the & sign.
+   */
+  function stripGtkAccessorKeys() {
+    var tabs = $('tabs').childNodes;
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].textContent = tabs[i].textContent.replace('&','');
+    }
+  }
+
+  /**
+   * Expand all nodes of the given tree object.
+   * @param {cr.ui.Tree} tree The tree object to expand all nodes on.
+   */
+  function revealTree(tree) {
+    tree.expanded = true;
+    for (var key in tree.detail.children) {
+      revealTree(tree.detail.children[key]);
+    }
   }
 
   /**
@@ -24,9 +66,70 @@ cr.define('cert_viewer', function() {
    * @param {Object} certInfo Certificate information in named fields.
    */
   function getCertificateInfo(certInfo) {
-    for (var key in certInfo) {
-      $(key).textContent = certInfo[key];
+    for (var key in certInfo.general) {
+      $(key).textContent = certInfo.general[key];
     }
+    createCertificateHierarchy(certInfo.hierarchy);
+  }
+
+  /**
+   * This function populates the certificate hierarchy.
+   * @param {Object} hierarchy A dictionary containing the hierarchy.
+   */
+  function createCertificateHierarchy(hierarchy) {
+    var treeItem = $('hierarchy');
+    treeItem.add(treeItem.detail.children['root'] =
+        constructTree(hierarchy[0]));
+    revealTree(treeItem);
+  }
+
+  /**
+   * Constructs a cr.ui.TreeItem corresponding to the passed in tree
+   * @param {Object} tree Dictionary describing the tree structure.
+   * @return {cr.ui.TreeItem} Tree node corresponding to the input dictionary.
+   */
+  function constructTree(tree)
+  {
+    var treeItem = new cr.ui.TreeItem({
+        label: tree.label,
+        detail: {payload: tree.payload ? tree.payload : {},
+            children: {}
+        }});
+    if (tree.children) {
+      for (var i = 0; i < tree.children.length; i++) {
+        treeItem.add(treeItem.detail.children[i] =
+            constructTree(tree.children[i]));
+      }
+    }
+    return treeItem;
+  }
+
+  /**
+   * Show certificate fields for the selected certificate in the hierarchy.
+   */
+  function showCertificateFields() {
+    var treeItem = $('certfields');
+    for (var key in treeItem.detail.children) {
+      treeItem.remove(treeItem.detail.children[key]);
+      delete treeItem.detail.children[key];
+    }
+    var item = $('hierarchy').selectedItem;
+    if (item && item.detail.payload.fields) {
+      treeItem.add(treeItem.detail.children['root'] =
+          constructTree(item.detail.payload.fields[0]));
+      revealTree(treeItem);
+    }
+  }
+
+  /**
+   * Show certificate field value for a selected certificate field.
+   */
+  function showCertificateFieldValue() {
+    var item = $('certfields').selectedItem;
+    if (item && item.detail.payload.val)
+      $('certfieldvalue').textContent = item.detail.payload.val;
+    else
+      $('certfieldvalue').textContent = '';
   }
 
   return {
--- chrome/browser/ui/webui/certificate_viewer_ui.cc
+++ chrome/browser/ui/webui/certificate_viewer_ui.cc
@@ -30,6 +30,8 @@ CertificateViewerUI::CertificateViewerUI(TabContents* contents)
       NewCallback(this, &CertificateViewerUI::RequestCertificateInfo));
 
   // Localized strings.
+  html_source->AddLocalizedString("general", IDS_CERT_INFO_GENERAL_TAB_LABEL);
+  html_source->AddLocalizedString("details", IDS_CERT_INFO_DETAILS_TAB_LABEL);
   html_source->AddLocalizedString("close", IDS_CLOSE);
   html_source->AddLocalizedString("usages",
       IDS_CERT_INFO_VERIFIED_USAGES_GROUP);
@@ -48,6 +50,12 @@ CertificateViewerUI::CertificateViewerUI(TabContents* contents)
   html_source->AddLocalizedString("sha256",
       IDS_CERT_INFO_SHA256_FINGERPRINT_LABEL);
   html_source->AddLocalizedString("sha1", IDS_CERT_INFO_SHA1_FINGERPRINT_LABEL);
+  html_source->AddLocalizedString("hierarchy",
+      IDS_CERT_DETAILS_CERTIFICATE_HIERARCHY_LABEL);
+  html_source->AddLocalizedString("certfields",
+      IDS_CERT_DETAILS_CERTIFICATE_FIELDS_LABEL);
+  html_source->AddLocalizedString("certfieldval",
+      IDS_CERT_DETAILS_CERTIFICATE_FIELD_VALUE_LABEL);
   html_source->set_json_path("strings.js");
 
   // Add required resources.
@@ -87,8 +95,8 @@ void CertificateViewerUI::RequestCertificateInfo(const ListValue* args) {
   net::X509Certificate* cert = static_cast<net::X509Certificate*>(
       HexStringToPointer(val));
 
-  // Certificate information. The keys in this dictionary correspond to the IDs
-  // in the Html page.
+  // Certificate information. The keys in this dictionary's general key
+  // correspond to the IDs in the Html page.
   DictionaryValue cert_info;
   net::X509Certificate::OSCertHandle cert_hnd = cert->os_cert_handle();
 
@@ -107,33 +115,34 @@ void CertificateViewerUI::RequestCertificateInfo(const ListValue* args) {
     }
     usagestr += *it;
   }
-  cert_info.SetString("usages", usagestr);
+  cert_info.SetString("general.usages", usagestr);
 
   // Standard certificate details.
   const std::string alternative_text =
       l10n_util::GetStringUTF8(IDS_CERT_INFO_FIELD_NOT_PRESENT);
-  cert_info.SetString("title", l10n_util::GetStringFUTF8(
+  cert_info.SetString("general.title", l10n_util::GetStringFUTF8(
       IDS_CERT_INFO_DIALOG_TITLE, UTF8ToUTF16(x509_certificate_model::GetTitle(
           cert_chain.front()))));
 
   // Issued to information.
-  cert_info.SetString("issued-cn", x509_certificate_model::GetSubjectCommonName(
-      cert_hnd, alternative_text));
-  cert_info.SetString("issued-o", x509_certificate_model::GetSubjectOrgName(
-      cert_hnd, alternative_text));
-  cert_info.SetString("issued-ou", x509_certificate_model::GetSubjectOrgUnitName
-      (cert_hnd, alternative_text));
-  cert_info.SetString("issued-sn",
-      x509_certificate_model::GetSerialNumberHexified(
-          cert_hnd, alternative_text));
+  cert_info.SetString("general.issued-cn",
+      x509_certificate_model::GetSubjectCommonName(cert_hnd, alternative_text));
+  cert_info.SetString("general.issued-o",
+      x509_certificate_model::GetSubjectOrgName(cert_hnd, alternative_text));
+  cert_info.SetString("general.issued-ou",
+      x509_certificate_model::GetSubjectOrgUnitName(cert_hnd,
+                                                    alternative_text));
+  cert_info.SetString("general.issued-sn",
+      x509_certificate_model::GetSerialNumberHexified(cert_hnd,
+                                                      alternative_text));
 
   // Issuer information.
-  cert_info.SetString("issuer-cn", x509_certificate_model::GetIssuerCommonName(
-      cert_hnd, alternative_text));
-  cert_info.SetString("issuer-o", x509_certificate_model::GetIssuerOrgName(
-      cert_hnd, alternative_text));
-  cert_info.SetString("issuer-ou", x509_certificate_model::GetIssuerOrgUnitName
-      (cert_hnd, alternative_text));
+  cert_info.SetString("general.issuer-cn",
+      x509_certificate_model::GetIssuerCommonName(cert_hnd, alternative_text));
+  cert_info.SetString("general.issuer-o",
+      x509_certificate_model::GetIssuerOrgName(cert_hnd, alternative_text));
+  cert_info.SetString("general.issuer-ou",
+      x509_certificate_model::GetIssuerOrgUnitName(cert_hnd, alternative_text));
 
   // Validity period.
   base::Time issued, expires;
@@ -147,15 +156,174 @@ void CertificateViewerUI::RequestCertificateInfo(const ListValue* args) {
     issued_str = alternative_text;
     expires_str = alternative_text;
   }
-  cert_info.SetString("issuedate", issued_str);
-  cert_info.SetString("expirydate", expires_str);
+  cert_info.SetString("general.issuedate", issued_str);
+  cert_info.SetString("general.expirydate", expires_str);
 
-  cert_info.SetString("sha256",
+  cert_info.SetString("general.sha256",
       x509_certificate_model::HashCertSHA256(cert_hnd));
-  cert_info.SetString("sha1",
+  cert_info.SetString("general.sha1",
       x509_certificate_model::HashCertSHA1(cert_hnd));
+
+  // Certificate hierarchy is constructed from bottom up.
+  ListValue* children = NULL;
+  for (net::X509Certificate::OSCertHandles::const_iterator i =
+      cert_chain.begin(); i != cert_chain.end(); ++i) {
+    DictionaryValue* cert_node = new DictionaryValue();
+    ListValue cert_details;
+    cert_node->SetString("label", x509_certificate_model::GetTitle(*i).c_str());
+    cert_node->Set("payload.fields", GetCertificateFields(*i));
+    // Add the child from the previous iteration.
+    if (children)
+      cert_node->Set("children", children);
+
+    // Add this node to the children list for the next iteration.
+    children = new ListValue();
+    children->Append(cert_node);
+  }
+  // Set the last node as the top of the certificate hierarchy.
+  cert_info.Set("hierarchy", children);
 
   // Send certificate information to javascript.
   CallJavascriptFunction("cert_viewer.getCertificateInfo", cert_info);
+}
+
+ListValue* CertificateViewerUI::GetCertificateFields(
+    net::X509Certificate::OSCertHandle cert) {
+  ListValue* root_list = new ListValue();
+  DictionaryValue* node_details;
+  DictionaryValue* alt_node_details;
+  ListValue* cert_sub_fields;
+  root_list->Append(node_details = new DictionaryValue());
+  node_details->SetString("label", x509_certificate_model::GetTitle(cert));
+
+  ListValue* cert_fields;
+  node_details->Set("children", cert_fields = new ListValue());
+  cert_fields->Append(node_details = new DictionaryValue());
+
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_CERTIFICATE));
+  node_details->Set("children", cert_fields = new ListValue());
+
+  // Main certificate fields.
+  cert_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_VERSION));
+  std::string version = x509_certificate_model::GetVersion(cert);
+  if (!version.empty())
+    node_details->SetString("payload.val",
+        l10n_util::GetStringFUTF8(IDS_CERT_DETAILS_VERSION_FORMAT,
+                                  UTF8ToUTF16(version)));
+
+  cert_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_SERIAL_NUMBER));
+  node_details->SetString("payload.val",
+      x509_certificate_model::GetSerialNumberHexified(cert,
+          l10n_util::GetStringUTF8(IDS_CERT_INFO_FIELD_NOT_PRESENT)));
+
+  cert_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_CERTIFICATE_SIG_ALG));
+  node_details->SetString("payload.val",
+      x509_certificate_model::ProcessSecAlgorithmSignature(cert));
+
+  cert_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_ISSUER));
+  node_details->SetString("payload.val",
+      x509_certificate_model::GetIssuerName(cert));
+
+  // Validity period.
+  cert_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_VALIDITY));
+
+  node_details->Set("children", cert_sub_fields = new ListValue());
+  cert_sub_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_NOT_BEFORE));
+  cert_sub_fields->Append(alt_node_details = new DictionaryValue());
+  alt_node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_NOT_AFTER));
+  base::Time issued, expires;
+  if (x509_certificate_model::GetTimes(cert, &issued, &expires)) {
+    node_details->SetString("payload.val",
+        UTF16ToUTF8(base::TimeFormatShortDateAndTime(issued)));
+    alt_node_details->SetString("payload.val",
+        UTF16ToUTF8(base::TimeFormatShortDateAndTime(expires)));
+  }
+
+  cert_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_SUBJECT));
+  node_details->SetString("payload.val",
+      x509_certificate_model::GetSubjectName(cert));
+
+  // Subject key information.
+  cert_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_SUBJECT_KEY_INFO));
+
+  node_details->Set("children", cert_sub_fields = new ListValue());
+  cert_sub_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_SUBJECT_KEY_ALG));
+  node_details->SetString("payload.val",
+      x509_certificate_model::ProcessSecAlgorithmSubjectPublicKey(cert));
+  cert_sub_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_SUBJECT_KEY));
+  node_details->SetString("payload.val",
+      x509_certificate_model::ProcessSubjectPublicKeyInfo(cert));
+
+  // Extensions.
+  x509_certificate_model::Extensions extensions;
+  x509_certificate_model::GetExtensions(
+      l10n_util::GetStringUTF8(IDS_CERT_EXTENSION_CRITICAL),
+      l10n_util::GetStringUTF8(IDS_CERT_EXTENSION_NON_CRITICAL),
+      cert, &extensions);
+
+  if (!extensions.empty()) {
+    cert_fields->Append(node_details = new DictionaryValue());
+    node_details->SetString("label",
+        l10n_util::GetStringUTF8(IDS_CERT_DETAILS_EXTENSIONS));
+
+    node_details->Set("children", cert_sub_fields = new ListValue());
+    for (x509_certificate_model::Extensions::const_iterator i =
+         extensions.begin(); i != extensions.end(); ++i) {
+      cert_sub_fields->Append(node_details = new DictionaryValue());
+      node_details->SetString("label", i->name);
+      node_details->SetString("payload.val", i->value);
+    }
+  }
+
+  cert_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_CERTIFICATE_SIG_ALG));
+  node_details->SetString("payload.val",
+      x509_certificate_model::ProcessSecAlgorithmSignatureWrap(cert));
+
+  cert_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_DETAILS_CERTIFICATE_SIG_VALUE));
+  node_details->SetString("payload.val",
+      x509_certificate_model::ProcessRawBitsSignatureWrap(cert));
+
+  cert_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_INFO_FINGERPRINTS_GROUP));
+  node_details->Set("children", cert_sub_fields = new ListValue());
+
+  cert_sub_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_INFO_SHA256_FINGERPRINT_LABEL));
+  node_details->SetString("payload.val",
+      x509_certificate_model::HashCertSHA256(cert));
+  cert_sub_fields->Append(node_details = new DictionaryValue());
+  node_details->SetString("label",
+      l10n_util::GetStringUTF8(IDS_CERT_INFO_SHA1_FINGERPRINT_LABEL));
+  node_details->SetString("payload.val",
+      x509_certificate_model::HashCertSHA1(cert));
+  return root_list;
 }
 
