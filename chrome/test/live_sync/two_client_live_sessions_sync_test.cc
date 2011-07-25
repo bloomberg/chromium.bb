82ecb4a085fa09d2dac0df65b8cc05f61e89f89f
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-25 18:47:10 +0000

    [Sync] Mark SecondChangesAfterEncrAndPassphraseChange flaky
    TBR=rsimha@chromium.org
    BUG=85294
    TEST=sync_integration_tests
    
    Review URL: http://codereview.chromium.org/7501003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@93924 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 42efbf9f2dad4acc57a42de56aa3918d66170ba4

--- chrome/test/live_sync/two_client_live_sessions_sync_test.cc
+++ chrome/test/live_sync/two_client_live_sessions_sync_test.cc
@@ -201,8 +201,9 @@ IN_PROC_BROWSER_TEST_F(TwoClientLiveSessionsSyncTest,
   ASSERT_TRUE(WindowsMatch(sessions1[0]->windows, *client0_windows));
 }
 
+// Flaky. http://crbug.com/85294
 IN_PROC_BROWSER_TEST_F(TwoClientLiveSessionsSyncTest,
-                       SecondChangesAfterEncrAndPassphraseChange) {
+                       FLAKY_SecondChangesAfterEncrAndPassphraseChange) {
   ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
 
   ASSERT_TRUE(CheckInitialState(0));
