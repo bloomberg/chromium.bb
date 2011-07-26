c6c62f12da445e73cd29ce8cf8de125d14081808
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-26 15:59:47 +0000

    Rewire the metrics pref to the signed settings store on chromeos.
    It used to check the consent file "Consent To Send Stats" which is insecure
    and does not respect cloud policies. The file is still used but only to store
    the install GUID.
    
    BUG=chromium-os:15188
    TEST=Manually - set the policy on the server and verify that the UI reacts correctly. Also the owner should be able to change it.
    
    
    Review URL: http://codereview.chromium.org/7355019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94104 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 33ca366d226f980c5a8513f09258ca84addc8f47

--- chrome/browser/chromeos/user_cros_settings_provider.h
+++ chrome/browser/chromeos/user_cros_settings_provider.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -42,6 +42,7 @@ class UserCrosSettingsProvider : public CrosSettingsProvider {
   bool RequestTrustedDataRoamingEnabled(Task* callback);
   bool RequestTrustedShowUsersOnSignin(Task* callback);
   bool RequestTrustedOwner(Task* callback);
+  bool RequestTrustedReportingEnabled(Task* callback);
 
   // Reloads values from device settings.
   void Reload();
@@ -51,6 +52,7 @@ class UserCrosSettingsProvider : public CrosSettingsProvider {
   static bool cached_allow_new_user();
   static bool cached_data_roaming_enabled();
   static bool cached_show_users_on_signin();
+  static bool cached_reporting_enabled();
   static const base::ListValue* cached_whitelist();
   static std::string cached_owner();
 
