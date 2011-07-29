59062e92b74af14c23a20f3af10750b73d33fceb
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-07-29 21:41:58 +0000

    Re-enable TaskManagerBrowserTest.ReloadExtension
    In the past this test had been crashy, and I was able to reproduce the crash
    occasionally in vmware and on the try bots. I'm no longer able to repro, so
    I'm speculatively re-enabling it to see if the underlying cause is actually
    fixed.
    
    BUG=42315
    TEST=none
    
    TBR=mpcomplete@chromium.org
    
    Review URL: http://codereview.chromium.org/7537006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94761 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d8d8f1b3e1d10a4e846cef3a3d49419a8cd7d085

--- chrome/browser/task_manager/task_manager_browsertest.cc
+++ chrome/browser/task_manager/task_manager_browsertest.cc
@@ -435,9 +435,7 @@ IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest,
 }
 
 // Regression test for http://crbug.com/18693.
-//
-// This test is crashy. See http://crbug.com/42315.
-IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest, DISABLED_ReloadExtension) {
+IN_PROC_BROWSER_TEST_F(TaskManagerBrowserTest, ReloadExtension) {
   // Show the task manager. This populates the model, and helps with debugging
   // (you see the task manager).
   browser()->window()->ShowTaskManager();
