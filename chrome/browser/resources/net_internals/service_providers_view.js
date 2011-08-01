f37f948aec7cac965898e7a2ad145172da48ba0a
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-01 16:06:08 +0000

    Refactor ServiceProvidersView to be defined inside an anonymous namespace.
    BUG=90857
    Review URL: http://codereview.chromium.org/7541017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@94911 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5fa749b6af60c8b4e360ac267b836a19463277f1

--- chrome/browser/resources/net_internals/service_providers_view.js
+++ chrome/browser/resources/net_internals/service_providers_view.js
@@ -9,146 +9,158 @@
  * For each layered service provider, shows the name, dll, and type
  * information.  For each namespace provider, shows the name and
  * whether or not it's active.
- *
- * @constructor
  */
-function ServiceProvidersView() {
-  const tabId = 'tab-handle-service-providers';
-  const mainBoxId = 'service-providers-view-tab-content';
-  const serviceProvidersTbodyId = 'service-providers-view-tbody';
-  const namespaceProvidersTbodyId =
+
+var ServiceProvidersView = (function() {
+  const TAB_ID = 'tab-handle-service-providers';
+
+  // IDs for special HTML elements in service_providers_view.html
+  const MAIN_BOX_ID = 'service-providers-view-tab-content';
+  const SERVICE_PROVIDERS_TBODY_ID = 'service-providers-view-tbody';
+  const NAMESPACE_PROVIDERS_TBODY_ID =
       'service-providers-view-namespace-providers-tbody';
 
-  DivView.call(this, mainBoxId);
+  // We inherit from DivView.
+  var superClass = DivView;
 
-  var tab = $(tabId);
-  setNodeDisplay(tab, true);
+  /**
+   * @constructor
+   */
+  function ServiceProvidersView() {
+    // Call superclass's constructor.
+    superClass.call(this, MAIN_BOX_ID);
 
-  this.serviceProvidersTbody_ = $(serviceProvidersTbodyId);
-  this.namespaceProvidersTbody_ = $(namespaceProvidersTbodyId);
+    var tab = $(TAB_ID);
+    setNodeDisplay(tab, true);
 
-  g_browser.addServiceProvidersObserver(this);
-}
+    this.serviceProvidersTbody_ = $(SERVICE_PROVIDERS_TBODY_ID);
+    this.namespaceProvidersTbody_ = $(NAMESPACE_PROVIDERS_TBODY_ID);
 
-inherits(ServiceProvidersView, DivView);
-
-ServiceProvidersView.prototype.onLoadLogFinish = function(data) {
-  return this.onServiceProvidersChanged(data.serviceProviders);
-};
-
-ServiceProvidersView.prototype.onServiceProvidersChanged =
-function(serviceProviders) {
-  return serviceProviders &&
-      this.updateServiceProviders_(serviceProviders['service_providers']) &&
-      this.updateNamespaceProviders_(serviceProviders['namespace_providers']);
-};
-
-/**
- * Returns type of a layered service provider.
- */
-ServiceProvidersView.getLayeredServiceProviderType =
-function(serviceProvider) {
-  if (serviceProvider.chain_length == 0)
-    return 'Layer';
-  if (serviceProvider.chain_length == 1)
-    return 'Base';
-  return 'Chain';
-};
-
-ServiceProvidersView.namespaceProviderType_ = {
-  '12': 'NS_DNS',
-  '15': 'NS_NLA',
-  '16': 'NS_BTH',
-  '32': 'NS_NTDS',
-  '37': 'NS_EMAIL',
-  '38': 'NS_PNRPNAME',
-  '39': 'NS_PNRPCLOUD'
-};
-
-/**
- * Returns the type of a namespace provider as a string.
- */
-ServiceProvidersView.getNamespaceProviderType = function(namespaceProvider) {
-  return tryGetValueWithKey(ServiceProvidersView.namespaceProviderType_,
-                            namespaceProvider.type);
-};
-
-ServiceProvidersView.socketType_ = {
-  '1': 'SOCK_STREAM',
-  '2': 'SOCK_DGRAM',
-  '3': 'SOCK_RAW',
-  '4': 'SOCK_RDM',
-  '5': 'SOCK_SEQPACKET'
-};
-
-/**
- * Returns socket type of a layered service provider as a string.
- */
-ServiceProvidersView.getSocketType = function(layeredServiceProvider) {
-  return tryGetValueWithKey(ServiceProvidersView.socketType_,
-                            layeredServiceProvider.socket_type);
-};
-
-ServiceProvidersView.protocolType_ = {
-  '1': 'IPPROTO_ICMP',
-  '6': 'IPPROTO_TCP',
-  '17': 'IPPROTO_UDP',
-  '58': 'IPPROTO_ICMPV6'
-};
-
-/**
- * Returns protocol type of a layered service provider as a string.
- */
-ServiceProvidersView.getProtocolType = function(layeredServiceProvider) {
-  return tryGetValueWithKey(ServiceProvidersView.protocolType_,
-                            layeredServiceProvider.socket_protocol);
-};
-
- /**
- * Updates the table of layered service providers.
- */
-ServiceProvidersView.prototype.updateServiceProviders_ =
-function(serviceProviders) {
-  this.serviceProvidersTbody_.innerHTML = '';
-
-  if (!serviceProviders)
-    return false;
-
-  // Add a table row for each service provider.
-  for (var i = 0; i < serviceProviders.length; ++i) {
-    var tr = addNode(this.serviceProvidersTbody_, 'tr');
-    var entry = serviceProviders[i];
-
-    addNodeWithText(tr, 'td', entry.name);
-    addNodeWithText(tr, 'td', entry.version);
-    addNodeWithText(tr, 'td',
-                    ServiceProvidersView.getLayeredServiceProviderType(entry));
-    addNodeWithText(tr, 'td', ServiceProvidersView.getSocketType(entry));
-    addNodeWithText(tr, 'td', ServiceProvidersView.getProtocolType(entry));
-    addNodeWithText(tr, 'td', entry.path);
+    g_browser.addServiceProvidersObserver(this);
   }
-  return true;
-};
 
-/**
- * Updates the lable of namespace providers.
- */
-ServiceProvidersView.prototype.updateNamespaceProviders_ =
-function(namespaceProviders) {
-  this.namespaceProvidersTbody_.innerHTML = '';
+  cr.addSingletonGetter(ServiceProvidersView);
 
-  if (!namespaceProviders)
-    return false;
+  ServiceProvidersView.prototype = {
+    // Inherit the superclass's methods.
+    __proto__: superClass.prototype,
 
-  // Add a table row for each namespace provider.
-  for (var i = 0; i < namespaceProviders.length; ++i) {
-    var tr = addNode(this.namespaceProvidersTbody_, 'tr');
-    var entry = namespaceProviders[i];
-    addNodeWithText(tr, 'td', entry.name);
-    addNodeWithText(tr, 'td', entry.version);
-    addNodeWithText(tr, 'td',
-                    ServiceProvidersView.getNamespaceProviderType(entry));
-    addNodeWithText(tr, 'td', entry.active);
+    onLoadLogFinish: function(data) {
+      return this.onServiceProvidersChanged(data.serviceProviders);
+    },
+
+    onServiceProvidersChanged: function(serviceProviders) {
+      return serviceProviders &&
+          this.updateServiceProviders_(serviceProviders['service_providers']) &&
+          this.updateNamespaceProviders_(
+              serviceProviders['namespace_providers']);
+    },
+
+     /**
+     * Updates the table of layered service providers.
+     */
+    updateServiceProviders_: function(serviceProviders) {
+      this.serviceProvidersTbody_.innerHTML = '';
+
+      if (!serviceProviders)
+        return false;
+
+      // Add a table row for each service provider.
+      for (var i = 0; i < serviceProviders.length; ++i) {
+        var tr = addNode(this.serviceProvidersTbody_, 'tr');
+        var entry = serviceProviders[i];
+
+        addNodeWithText(tr, 'td', entry.name);
+        addNodeWithText(tr, 'td', entry.version);
+        addNodeWithText(tr, 'td', getLayeredServiceProviderType(entry));
+        addNodeWithText(tr, 'td', getSocketType(entry));
+        addNodeWithText(tr, 'td', getProtocolType(entry));
+        addNodeWithText(tr, 'td', entry.path);
+      }
+      return true;
+    },
+
+    /**
+     * Updates the lable of namespace providers.
+     */
+    updateNamespaceProviders_: function(namespaceProviders) {
+      this.namespaceProvidersTbody_.innerHTML = '';
+
+      if (!namespaceProviders)
+        return false;
+
+      // Add a table row for each namespace provider.
+      for (var i = 0; i < namespaceProviders.length; ++i) {
+        var tr = addNode(this.namespaceProvidersTbody_, 'tr');
+        var entry = namespaceProviders[i];
+        addNodeWithText(tr, 'td', entry.name);
+        addNodeWithText(tr, 'td', entry.version);
+        addNodeWithText(tr, 'td', getNamespaceProviderType(entry));
+        addNodeWithText(tr, 'td', entry.active);
+      }
+      return true;
+    }
+  };
+
+  /**
+   * Returns type of a layered service provider.
+   */
+  function getLayeredServiceProviderType(serviceProvider) {
+    if (serviceProvider.chain_length == 0)
+      return 'Layer';
+    if (serviceProvider.chain_length == 1)
+      return 'Base';
+    return 'Chain';
   }
-  return true;
-};
+
+  var NAMESPACE_PROVIDER_PTYPE = {
+    '12': 'NS_DNS',
+    '15': 'NS_NLA',
+    '16': 'NS_BTH',
+    '32': 'NS_NTDS',
+    '37': 'NS_EMAIL',
+    '38': 'NS_PNRPNAME',
+    '39': 'NS_PNRPCLOUD'
+  }
+
+  /**
+   * Returns the type of a namespace provider as a string.
+   */
+  function getNamespaceProviderType(namespaceProvider) {
+    return tryGetValueWithKey(NAMESPACE_PROVIDER_PTYPE,
+                              namespaceProvider.type);
+  };
+
+  var SOCKET_TYPE = {
+    '1': 'SOCK_STREAM',
+    '2': 'SOCK_DGRAM',
+    '3': 'SOCK_RAW',
+    '4': 'SOCK_RDM',
+    '5': 'SOCK_SEQPACKET'
+  };
+
+  /**
+   * Returns socket type of a layered service provider as a string.
+   */
+  function getSocketType(layeredServiceProvider) {
+    return tryGetValueWithKey(SOCKET_TYPE,
+                              layeredServiceProvider.socket_type);
+  }
+
+  var PROTOCOL_TYPE = {
+    '1': 'IPPROTO_ICMP',
+    '6': 'IPPROTO_TCP',
+    '17': 'IPPROTO_UDP',
+    '58': 'IPPROTO_ICMPV6'
+  };
+
+  /**
+   * Returns protocol type of a layered service provider as a string.
+   */
+  function getProtocolType(layeredServiceProvider) {
+    return tryGetValueWithKey(PROTOCOL_TYPE,
+                              layeredServiceProvider.socket_protocol);
+  }
+
+  return ServiceProvidersView;
+})();
