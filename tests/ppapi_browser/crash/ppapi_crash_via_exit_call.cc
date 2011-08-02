369d211fb38338977674a899fb9a30e4603e7282
jvoung@google.com <jvoung@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-08-02 00:27:20 +0000

    Add include for unistd.h for _exit. Otherwise, the exit test won't build w/ pnacl.
    R= polina@google.com,dspringer@google.com
    BUG=
    TEST= ./scons platform=x86-32 bitcode=1 run_ppapi_crash_browser_test
    
    Review URL: http://codereview.chromium.org/7453063
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6282 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 6e98efd1f3787a80ff2ad381ff3837a6221e4105

--- tests/ppapi_browser/crash/ppapi_crash_via_exit_call.cc
+++ tests/ppapi_browser/crash/ppapi_crash_via_exit_call.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <unistd.h>
+
 #include "native_client/src/shared/platform/nacl_check.h"
 #include "native_client/tests/ppapi_test_lib/test_interface.h"
 
