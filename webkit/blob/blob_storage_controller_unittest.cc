71e06702aac1705081ef7bbf2e25a805bfae1801
shinyak@google.com <shinyak@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-03 05:28:10 +0000

    Removed wchat_t from Time::FromString. Also, some of the test case are moved for pr_time_unittests to time_unittests.
    BUG=77962
    TEST=base_unittests:TimeTest.*
    
    
    Review URL: http://codereview.chromium.org/7492063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95207 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 46470aab29e72565ff358331094402b68d053e30

--- webkit/blob/blob_storage_controller_unittest.cc
+++ webkit/blob/blob_storage_controller_unittest.cc
@@ -18,8 +18,8 @@ namespace webkit_blob {
 TEST(BlobStorageControllerTest, RegisterBlobUrl) {
   // Setup a set of blob data for testing.
   base::Time time1, time2;
-  base::Time::FromString(L"Tue, 15 Nov 1994, 12:45:26 GMT", &time1);
-  base::Time::FromString(L"Mon, 14 Nov 1994, 11:30:49 GMT", &time2);
+  base::Time::FromString("Tue, 15 Nov 1994, 12:45:26 GMT", &time1);
+  base::Time::FromString("Mon, 14 Nov 1994, 11:30:49 GMT", &time2);
 
   scoped_refptr<BlobData> blob_data1(new BlobData());
   blob_data1->AppendData("Data1");
@@ -80,8 +80,8 @@ TEST(BlobStorageControllerTest, RegisterBlobUrl) {
 TEST(BlobStorageControllerTest, ResolveBlobReferencesInUploadData) {
   // Setup blob data for testing.
   base::Time time1, time2;
-  base::Time::FromString(L"Tue, 15 Nov 1994, 12:45:26 GMT", &time1);
-  base::Time::FromString(L"Mon, 14 Nov 1994, 11:30:49 GMT", &time2);
+  base::Time::FromString("Tue, 15 Nov 1994, 12:45:26 GMT", &time1);
+  base::Time::FromString("Mon, 14 Nov 1994, 11:30:49 GMT", &time2);
 
   scoped_ptr<BlobStorageController> blob_storage_controller(
       new BlobStorageController());
