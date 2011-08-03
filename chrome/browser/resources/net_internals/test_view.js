dbba8cd9d348964de2ff0928d0ba4fef688d3aa1
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-03 05:03:45 +0000

    Refactor TestView to be defined inside an anonymous namespace.
    Also display error strings in addition to numeric ids.
    
    BUG=90857
    TEST=none
    
    Review URL: http://codereview.chromium.org/7551023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95200 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dc7ed18c56e28a81db0c6bdcc6650db7416a896c

--- chrome/browser/resources/net_internals/test_view.js
+++ chrome/browser/resources/net_internals/test_view.js
@@ -9,130 +9,145 @@
  *   - Has a button to start running the tests.
  *   - Shows the set of experiments that have been run so far, and their
  *     result.
- *
- *  @constructor
  */
-function TestView() {
-  const mainBoxId = 'test-view-tab-content';
-  const urlInputId = 'test-view-url-input';
-  const formId = 'test-view-connection-tests-form';
-  const summaryDivId = 'test-view-summary';
 
-  DivView.call(this, mainBoxId);
+var TestView = (function() {
+  // IDs for special HTML elements in test_view.html
+  const MAIN_BOX_ID = 'test-view-tab-content';
+  const URL_INPUT_ID = 'test-view-url-input';
+  const FORM_ID = 'test-view-connection-tests-form';
+  const SUMMARY_DIV_ID = 'test-view-summary';
 
-  this.urlInput_ = $(urlInputId);
-  this.summaryDiv_ = $(summaryDivId);
+  // We inherit from DivView.
+  var superClass = DivView;
 
-  var form = $(formId);
-  form.addEventListener('submit', this.onSubmitForm_.bind(this), false);
+  /**
+   * @constructor
+   */
+  function TestView() {
+    // Call superclass's constructor.
+    superClass.call(this, MAIN_BOX_ID);
 
-  g_browser.addConnectionTestsObserver(this);
-}
+    this.urlInput_ = $(URL_INPUT_ID);
+    this.summaryDiv_ = $(SUMMARY_DIV_ID);
 
-inherits(TestView, DivView);
+    var form = $(FORM_ID);
+    form.addEventListener('submit', this.onSubmitForm_.bind(this), false);
 
-TestView.prototype.onSubmitForm_ = function(event) {
-  g_browser.sendStartConnectionTests(this.urlInput_.value);
-  event.preventDefault();
-};
+    // Register to test information as it's received.
+    g_browser.addConnectionTestsObserver(this);
+  }
 
-/**
- * Callback for when the connection tests have begun.
- */
-TestView.prototype.onStartedConnectionTestSuite = function() {
-  this.summaryDiv_.innerHTML = '';
+  cr.addSingletonGetter(TestView);
 
-  var p = addNode(this.summaryDiv_, 'p');
-  addTextNode(p, 'Started connection test suite suite on ' +
-                 (new Date()).toLocaleString());
+  TestView.prototype = {
+    // Inherit the superclass's methods.
+    __proto__: superClass.prototype,
 
-  // Add a table that will hold the individual test results.
-  var table = addNode(this.summaryDiv_, 'table');
-  table.className = 'styledTable';
-  var thead = addNode(table, 'thead');
-  thead.innerHTML = '<tr><th>Result</th><th>Experiment</th>' +
-                    '<th>Error</th><th>Time (ms)</th></tr>';
+    onSubmitForm_: function(event) {
+      g_browser.sendStartConnectionTests(this.urlInput_.value);
+      event.preventDefault();
+    },
 
-  this.tbody_ = addNode(table, 'tbody');
-};
+    /**
+     * Callback for when the connection tests have begun.
+     */
+    onStartedConnectionTestSuite: function() {
+      this.summaryDiv_.innerHTML = '';
 
-/**
- * Callback for when an individual test in the suite has begun.
- */
-TestView.prototype.onStartedConnectionTestExperiment = function(experiment) {
-  var tr = addNode(this.tbody_, 'tr');
+      var p = addNode(this.summaryDiv_, 'p');
+      addTextNode(p, 'Started connection test suite suite on ' +
+                     (new Date()).toLocaleString());
 
-  var passFailCell = addNode(tr, 'td');
+      // Add a table that will hold the individual test results.
+      var table = addNode(this.summaryDiv_, 'table');
+      table.className = 'styledTable';
+      var thead = addNode(table, 'thead');
+      thead.innerHTML = '<tr><th>Result</th><th>Experiment</th>' +
+                        '<th>Error</th><th>Time (ms)</th></tr>';
 
-  var experimentCell = addNode(tr, 'td');
+      this.tbody_ = addNode(table, 'tbody');
+    },
 
-  var resultCell = addNode(tr, 'td');
-  addTextNode(resultCell, '?');
+    /**
+     * Callback for when an individual test in the suite has begun.
+     */
+    onStartedConnectionTestExperiment: function(experiment) {
+      var tr = addNode(this.tbody_, 'tr');
 
-  var dtCell = addNode(tr, 'td');
-  addTextNode(dtCell, '?');
+      var passFailCell = addNode(tr, 'td');
 
-  // We will fill in result cells with actual values (to replace the
-  // placeholder '?') once the test has completed. For now we just
-  // save references to these cells.
-  this.currentExperimentRow_ = {
-    experimentCell: experimentCell,
-    dtCell: dtCell,
-    resultCell: resultCell,
-    passFailCell: passFailCell,
-    startTime: (new Date()).getTime()
+      var experimentCell = addNode(tr, 'td');
+
+      var resultCell = addNode(tr, 'td');
+      addTextNode(resultCell, '?');
+
+      var dtCell = addNode(tr, 'td');
+      addTextNode(dtCell, '?');
+
+      // We will fill in result cells with actual values (to replace the
+      // placeholder '?') once the test has completed. For now we just
+      // save references to these cells.
+      this.currentExperimentRow_ = {
+        experimentCell: experimentCell,
+        dtCell: dtCell,
+        resultCell: resultCell,
+        passFailCell: passFailCell,
+        startTime: (new Date()).getTime()
+      };
+
+      addTextNode(experimentCell, 'Fetch ' + experiment.url);
+
+      if (experiment.proxy_settings_experiment ||
+          experiment.host_resolver_experiment) {
+        var ul = addNode(experimentCell, 'ul');
+
+        if (experiment.proxy_settings_experiment) {
+          var li = addNode(ul, 'li');
+          addTextNode(li, experiment.proxy_settings_experiment);
+        }
+
+        if (experiment.host_resolver_experiment) {
+          var li = addNode(ul, 'li');
+          addTextNode(li, experiment.host_resolver_experiment);
+        }
+      }
+    },
+
+    /**
+     * Callback for when an individual test in the suite has finished.
+     */
+    onCompletedConnectionTestExperiment: function(experiment, result) {
+      var r = this.currentExperimentRow_;
+
+      var endTime = (new Date()).getTime();
+
+      r.dtCell.innerHTML = '';
+      addTextNode(r.dtCell, (endTime - r.startTime));
+
+      r.resultCell.innerHTML = '';
+
+      if (result == 0) {
+        r.passFailCell.style.color = 'green';
+        addTextNode(r.passFailCell, 'PASS');
+      } else {
+        addTextNode(r.resultCell,
+                    getKeyWithValue(NetError, result) + ' (' + result + ')');
+        r.passFailCell.style.color = 'red';
+        addTextNode(r.passFailCell, 'FAIL');
+      }
+
+      this.currentExperimentRow_ = null;
+    },
+
+    /**
+     * Callback for when the last test in the suite has finished.
+     */
+    onCompletedConnectionTestSuite: function() {
+      var p = addNode(this.summaryDiv_, 'p');
+      addTextNode(p, 'Completed connection test suite suite');
+    }
   };
 
-  addTextNode(experimentCell, 'Fetch ' + experiment.url);
-
-  if (experiment.proxy_settings_experiment ||
-      experiment.host_resolver_experiment) {
-    var ul = addNode(experimentCell, 'ul');
-
-    if (experiment.proxy_settings_experiment) {
-      var li = addNode(ul, 'li');
-      addTextNode(li, experiment.proxy_settings_experiment);
-    }
-
-    if (experiment.host_resolver_experiment) {
-      var li = addNode(ul, 'li');
-      addTextNode(li, experiment.host_resolver_experiment);
-    }
-  }
-};
-
-/**
- * Callback for when an individual test in the suite has finished.
- */
-TestView.prototype.onCompletedConnectionTestExperiment = function(
-    experiment, result) {
-  var r = this.currentExperimentRow_;
-
-  var endTime = (new Date()).getTime();
-
-  r.dtCell.innerHTML = '';
-  addTextNode(r.dtCell, (endTime - r.startTime));
-
-  r.resultCell.innerHTML = '';
-
-  if (result == 0) {
-    r.passFailCell.style.color = 'green';
-    addTextNode(r.passFailCell, 'PASS');
-  } else {
-    // TODO(eroman): stringize the error code.
-    addTextNode(r.resultCell, result);
-    r.passFailCell.style.color = 'red';
-    addTextNode(r.passFailCell, 'FAIL');
-  }
-
-  this.currentExperimentRow_ = null;
-};
-
-/**
- * Callback for when the last test in the suite has finished.
- */
-TestView.prototype.onCompletedConnectionTestSuite = function() {
-  var p = addNode(this.summaryDiv_, 'p');
-  addTextNode(p, 'Completed connection test suite suite');
-};
-
+  return TestView;
+})();
