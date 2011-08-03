11c49e38df0becb6fe218017a850a8028f7caa5d
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-08-03 16:25:17 +0000

    Fix Chromium build breakage introduced by r6280
    r6280 removed nacl_elf.h but did not update a file that #included it
    that is only used in the Chromium build.  We remove this unneeded #include,
    plus a couple of others.
    
    BUG=http://code.google.com/p/chromium/issues/detail?id=91482
    TEST=compile this file on its own
    
    Review URL: http://codereview.chromium.org/7564012
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6302 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: e457b8020d685b95731cc1f7b3c8f2f9d04e545e

--- src/trusted/plugin/browser_utils.cc
+++ src/trusted/plugin/browser_utils.cc
@@ -1,7 +1,7 @@
 /*
- * Copyright 2008 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 
@@ -9,9 +9,6 @@
 
 #include "base/rand_util_c.h"
 
-#include "native_client/src/include/nacl_string.h"
-#include "native_client/src/include/nacl_elf.h"
-#include "native_client/src/include/portability_io.h"
 #include "native_client/src/trusted/plugin/nacl_entry_points.h"
 
 
