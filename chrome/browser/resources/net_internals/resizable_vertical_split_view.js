0c0c4b4c47110328e7d9de32c0186461ed695019
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-03 19:55:55 +0000

    Refactor ResizableVerticalSplitView to be defined inside an anonymous namespace.
    BUG=90857
    Review URL: http://codereview.chromium.org/7564018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95291 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4fee20d1672a52e59657120dc398e6aa89256670

--- chrome/browser/resources/net_internals/resizable_vertical_split_view.js
+++ chrome/browser/resources/net_internals/resizable_vertical_split_view.js
@@ -23,115 +23,125 @@
  * @param {!View} leftView The widget to position on the left.
  * @param {!View} rightView The widget to position on the right.
  * @param {!DivView} sizerView The widget that will serve as draggable divider.
- * @constructor
  */
-function ResizableVerticalSplitView(leftView, rightView, sizerView) {
-  View.call(this);
+var ResizableVerticalSplitView = (function() {
+  // Minimum width to size panels to, in pixels.
+  const MIN_PANEL_WIDTH = 50;
 
-  this.leftView_ = leftView;
-  this.rightView_ = rightView;
-  this.sizerView_ = sizerView;
+  // We inherit from View.
+  var superClass = View;
 
-  // Setup the "sizer" so it can be dragged left/right to reposition the
-  // vertical split.
-  sizerView.getNode().addEventListener(
-      'mousedown', this.onDragSizerStart_.bind(this), true);
-}
+  /**
+   * @constructor
+   */
+  function ResizableVerticalSplitView(leftView, rightView, sizerView) {
+    // Call superclass's constructor.
+    superClass.call(this);
 
-inherits(ResizableVerticalSplitView, View);
+    this.leftView_ = leftView;
+    this.rightView_ = rightView;
+    this.sizerView_ = sizerView;
 
-// Minimum width to size panels to, in pixels.
-ResizableVerticalSplitView.MIN_PANEL_WIDTH = 50;
-
-/**
- * Sets the width of the left view.
- * @param {Integer} px The number of pixels
- */
-ResizableVerticalSplitView.prototype.setLeftSplit = function(px) {
-  this.leftSplit_ = px;
-};
-
-/**
- * Repositions all of the elements to fit the window.
- */
-ResizableVerticalSplitView.prototype.setGeometry = function(
-    left, top, width, height) {
-  ResizableVerticalSplitView.superClass_.setGeometry.call(
-      this, left, top, width, height);
-
-  // If this is the first setGeometry(), initialize the split point at 50%.
-  if (!this.leftSplit_)
-    this.leftSplit_ = parseInt((width / 2).toFixed(0));
-
-  // Calculate the horizontal split points.
-  var leftboxWidth = this.leftSplit_;
-  var sizerWidth = this.sizerView_.getWidth();
-  var rightboxWidth = width - (leftboxWidth + sizerWidth);
-
-  // Don't let the right pane get too small.
-  if (rightboxWidth < ResizableVerticalSplitView.MIN_PANEL_WIDTH) {
-    rightboxWidth = ResizableVerticalSplitView.MIN_PANEL_WIDTH;
-    leftboxWidth = width - (sizerWidth + rightboxWidth);
+    // Setup the "sizer" so it can be dragged left/right to reposition the
+    // vertical split.
+    sizerView.getNode().addEventListener(
+        'mousedown', this.onDragSizerStart_.bind(this), true);
   }
 
-  // Position the boxes using calculated split points.
-  this.leftView_.setGeometry(left, top, leftboxWidth, height);
-  this.sizerView_.setGeometry(this.leftView_.getRight(), top,
-                              sizerWidth, height);
-  this.rightView_.setGeometry(this.sizerView_.getRight(), top,
-                              rightboxWidth, height);
-};
+  ResizableVerticalSplitView.prototype = {
+    // Inherit the superclass's methods.
+    __proto__: superClass.prototype,
 
-ResizableVerticalSplitView.prototype.show = function(isVisible) {
-  ResizableVerticalSplitView.superClass_.show.call(this, isVisible);
-  this.leftView_.show(isVisible);
-  this.sizerView_.show(isVisible);
-  this.rightView_.show(isVisible);
-};
+    /**
+     * Sets the width of the left view.
+     * @param {Integer} px The number of pixels
+     */
+    setLeftSplit: function(px) {
+      this.leftSplit_ = px;
+    },
 
-/**
- * Called once we have clicked into the sizer. Starts capturing the mouse
- * position to implement dragging.
- */
-ResizableVerticalSplitView.prototype.onDragSizerStart_ = function(event) {
-  this.sizerMouseMoveListener_ = this.onDragSizer.bind(this);
-  this.sizerMouseUpListener_ = this.onDragSizerEnd.bind(this);
+    /**
+     * Repositions all of the elements to fit the window.
+     */
+    setGeometry: function(left, top, width, height) {
+      superClass.prototype.setGeometry.call(this, left, top, width, height);
 
-  window.addEventListener('mousemove', this.sizerMouseMoveListener_, true);
-  window.addEventListener('mouseup', this.sizerMouseUpListener_, true);
+      // If this is the first setGeometry(), initialize the split point at 50%.
+      if (!this.leftSplit_)
+        this.leftSplit_ = parseInt((width / 2).toFixed(0));
 
-  event.preventDefault();
-};
+      // Calculate the horizontal split points.
+      var leftboxWidth = this.leftSplit_;
+      var sizerWidth = this.sizerView_.getWidth();
+      var rightboxWidth = width - (leftboxWidth + sizerWidth);
 
-/**
- * Called when the mouse has moved after dragging started.
- */
-ResizableVerticalSplitView.prototype.onDragSizer = function(event) {
-  // Convert from page coordinates, to view coordinates.
-  this.leftSplit_ = (event.pageX - this.getLeft());
+      // Don't let the right pane get too small.
+      if (rightboxWidth < MIN_PANEL_WIDTH) {
+        rightboxWidth = MIN_PANEL_WIDTH;
+        leftboxWidth = width - (sizerWidth + rightboxWidth);
+      }
 
-  // Avoid shrinking the left box too much.
-  this.leftSplit_ = Math.max(
-      this.leftSplit_, ResizableVerticalSplitView.MIN_PANEL_WIDTH);
-  // Avoid shrinking the right box too much.
-  this.leftSplit_ = Math.min(
-      this.leftSplit_,
-      this.getWidth() - ResizableVerticalSplitView.MIN_PANEL_WIDTH);
+      // Position the boxes using calculated split points.
+      this.leftView_.setGeometry(left, top, leftboxWidth, height);
+      this.sizerView_.setGeometry(this.leftView_.getRight(), top,
+                                  sizerWidth, height);
+      this.rightView_.setGeometry(this.sizerView_.getRight(), top,
+                                  rightboxWidth, height);
+    },
 
-  // Force a layout with the new |leftSplit_|.
-  this.setGeometry(
-      this.getLeft(), this.getTop(), this.getWidth(), this.getHeight());
-};
+    show: function(isVisible) {
+      superClass.prototype.show.call(this, isVisible);
+      this.leftView_.show(isVisible);
+      this.sizerView_.show(isVisible);
+      this.rightView_.show(isVisible);
+    },
 
-/**
- * Called once the mouse has been released, and the dragging is over.
- */
-ResizableVerticalSplitView.prototype.onDragSizerEnd = function(event) {
-  window.removeEventListener('mousemove', this.sizerMouseMoveListener_, true);
-  window.removeEventListener('mouseup', this.sizerMouseUpListener_, true);
+    /**
+     * Called once we have clicked into the sizer. Starts capturing the mouse
+     * position to implement dragging.
+     */
+    onDragSizerStart_: function(event) {
+      this.sizerMouseMoveListener_ = this.onDragSizer_.bind(this);
+      this.sizerMouseUpListener_ = this.onDragSizerEnd_.bind(this);
 
-  this.sizerMouseMoveListener_ = null;
-  this.sizerMouseUpListener_ = null;
+      window.addEventListener('mousemove', this.sizerMouseMoveListener_, true);
+      window.addEventListener('mouseup', this.sizerMouseUpListener_, true);
 
-  event.preventDefault();
-};
+      event.preventDefault();
+    },
+
+    /**
+     * Called when the mouse has moved after dragging started.
+     */
+    onDragSizer_: function(event) {
+      // Convert from page coordinates, to view coordinates.
+      this.leftSplit_ = (event.pageX - this.getLeft());
+
+      // Avoid shrinking the left box too much.
+      this.leftSplit_ = Math.max(this.leftSplit_, MIN_PANEL_WIDTH);
+      // Avoid shrinking the right box too much.
+      this.leftSplit_ = Math.min(
+          this.leftSplit_, this.getWidth() - MIN_PANEL_WIDTH);
+
+      // Force a layout with the new |leftSplit_|.
+      this.setGeometry(
+          this.getLeft(), this.getTop(), this.getWidth(), this.getHeight());
+    },
+
+    /**
+     * Called once the mouse has been released, and the dragging is over.
+     */
+    onDragSizerEnd_: function(event) {
+      window.removeEventListener(
+          'mousemove', this.sizerMouseMoveListener_, true);
+      window.removeEventListener('mouseup', this.sizerMouseUpListener_, true);
+
+      this.sizerMouseMoveListener_ = null;
+      this.sizerMouseUpListener_ = null;
+
+      event.preventDefault();
+    }
+  };
+
+  return ResizableVerticalSplitView;
+})();
