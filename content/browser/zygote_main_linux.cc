7987f3edb45fbc95f065b3f048f08d4da225bef4
evan@chromium.org <evan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-04 18:08:09 +0000

    linux: undo the seccomp warning I recently added
    I had expected this to help track down failing bots,
    but miraculuously no bots are failing.  I have restored the
    comment to its former form.
    
    Review URL: http://codereview.chromium.org/7574012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95444 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0b4610a933e9c0bba5ee448b96c1289c04863869

--- content/browser/zygote_main_linux.cc
+++ content/browser/zygote_main_linux.cc
@@ -799,10 +799,7 @@ bool ZygoteMain(const MainFunctionParams& params,
                     "Seccomp sandbox. Running renderers with Seccomp "
                     "sandboxing disabled.";
     } else {
-      // TODO(evan): switch to VLOG once we shake out all the sandbox bugs.
-      LOG(WARNING) << "Enabling experimental Seccomp sandbox; if you encounter "
-                   << "any problems, please try running with "
-                   << "--disable-seccomp-sandbox and file bugs.";
+      VLOG(1) << "Enabling experimental Seccomp sandbox.";
       sandbox_flags |= ZygoteHost::kSandboxSeccomp;
     }
   }
