49760c2237f278279d02fbda6f8114428004d7ac
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-05 15:49:50 +0000

    Fixup bad include and usage of BASE_API, which should be BASE_EXPORT.
    TBR=rvargas@chromium.org
    Review URL: http://codereview.chromium.org/7582007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@95619 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ce8a26c784c60ba9278038db1c100e8687e709e0

--- sandbox/linux/suid/process_util.h
+++ sandbox/linux/suid/process_util.h
@@ -11,13 +11,13 @@
 #include <stdbool.h>
 #include <sys/types.h>
 
-#include "base/base_api.h"
+#include "base/base_export.h"
 
 static const char kAdjustOOMScoreSwitch[] = "--adjust-oom-score";
 
 // This adjusts /proc/process/oom_adj so the Linux OOM killer will prefer
 // certain process types over others. The range for the adjustment is
 // [-17,15], with [0,15] being user accessible.
-BASE_API bool AdjustOOMScore(pid_t process, int score);
+BASE_EXPORT bool AdjustOOMScore(pid_t process, int score);
 
 #endif  // SANDBOX_LINUX_SUID_PROCESS_UTIL_H_
