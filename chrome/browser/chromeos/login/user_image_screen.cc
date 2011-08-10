2d4f62c2389918a0fe0ad35663bef5070f0dbd0e
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-10 09:18:12 +0000

    Revert 96157 - Speak page name when arriving at picture selection screen
    R=hashimoto@chromium.org
    BUG=chromium-os:18955
    TEST=Manual
    
    
    Review URL: http://codereview.chromium.org/7607028
    
    TBR=zork@chromium.org
    Review URL: http://codereview.chromium.org/7497059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96158 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 36ae85801ef391088be39b770228e4aafa7dbd74

--- chrome/browser/chromeos/login/user_image_screen.cc
+++ chrome/browser/chromeos/login/user_image_screen.cc
@@ -9,7 +9,6 @@
 #include "chrome/browser/chromeos/login/login_utils.h"
 #include "chrome/browser/chromeos/login/screen_observer.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
-#include "chrome/browser/chromeos/login/wizard_accessibility_helper.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "content/common/notification_service.h"
 #include "grit/theme_resources.h"
@@ -65,10 +64,6 @@ void UserImageScreen::Show() {
   if (selected_image_index == -1)
     selected_image_index = 0;
   actor_->SelectImage(selected_image_index);
-
-  WizardAccessibilityHelper::GetInstance()->MaybeSpeak(
-      l10n_util::GetStringUTF8(IDS_OPTIONS_CHANGE_PICTURE_DIALOG_TEXT).c_str(),
-      false, true);
 }
 
 void UserImageScreen::Hide() {
@@ -155,3 +150,4 @@ void UserImageScreen::Observe(int type,
 }
 
 }  // namespace chromeos
+
