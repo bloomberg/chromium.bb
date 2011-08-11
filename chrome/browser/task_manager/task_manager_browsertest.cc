791d477371e9c87c2a52ae33e58a87db69b80f40
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-11 10:13:34 +0000

    Disable TaskManagerBrowserTest.ShutdownWhileOpen on ChromeOS, it actually crashes.
    BUG=92297
    TEST=
    TBR=mnissler@chromium.org
    
    Review URL: http://codereview.chromium.org/7621009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96352 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4c0be9fe84bab49a68fd95bd09591affc84d5886

--- chrome/browser/task_manager/task_manager_browsertest.cc
+++ chrome/browser/task_manager/task_manager_browsertest.cc
@@ -56,10 +56,10 @@ class TaskManagerBrowserTest : public ExtensionBrowserTest {
   }
 };
 
-// Flaky on ChromeOS (triggers pure virtual function call), see
+// Flaky crashes on ChromeOS (triggers pure virtual function call), see
 // http://crbug.com/92297 for details
 #if defined(OS_CHROMEOS)
-#define MAYBE_ShutdownWhileOpen FLAKY_ShutdownWhileOpen
+#define MAYBE_ShutdownWhileOpen DISABLED_ShutdownWhileOpen
 #else
 #define MAYBE_ShutdownWhileOpen ShutdownWhileOpen
 #endif
