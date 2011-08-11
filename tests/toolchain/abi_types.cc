a808da1f4b321eaeca18987a8b280be61f99a723
polina@google.com <polina@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-08-11 18:18:57 +0000

    tests/toolchain: Add a new test to verify that various standard types have the same size, alignment and in certain cases type equivalency on all platforms. The test is self-contained (no golden files) and must be run separately for each platform.
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=1246
    TEST=scons run_abi_types_test
    Review URL: http://codereview.chromium.org/7617011
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6399 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 89cd2265f8344da7c55b902317972c0a6b516f96

---
+++ tests/toolchain/abi_types.cc
@@ -0,0 +1,64 @@
+// Copyright (c) 2011 The Native Client Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// This test confirms the actual data types of types defined by various
+// C/C++ standards. We must ensure that the actual types match on all
+// platforms.
+
+#include <setjmp.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <sys/types.h>
+
+#include <typeinfo>
+
+#include "native_client/tests/toolchain/utils.h"
+
+// Checks size and alignment.
+template<typename T>
+void IsSizeAndAlignOf(const char* test_type, int size, unsigned int align) {
+  char error[256];
+  snprintf(error, sizeof(error), "sizeof(%s)=%d", test_type, sizeof(T));
+  ASSERT(sizeof(T) == size, error);
+  snprintf(error, sizeof(error), "alignof(%s)=%u", test_type, __alignof__(T));
+  ASSERT(__alignof__(T) == align, error);
+}
+
+// Checks type equivalency.
+template<typename T, typename A>
+void IsType(const char* test_type) {
+  char error[256];
+  snprintf(error, sizeof(error), "typeid(%s)=%s", test_type, typeid(T).name());
+  ASSERT(typeid(T) == typeid(A), error);
+}
+
+// Checks type equivalency and size and alignment.
+template<typename T, typename A>
+void IsTypeAndSizeAndAlignOf(const char* test_type,
+                             int size,
+                             unsigned int align) {
+  IsType<T, A>(test_type);
+  IsSizeAndAlignOf<T>(test_type, size, align);
+}
+
+int main() {
+  // setjmp.h
+  // TODO(eaeltsin): jmp_buf must be the same on all platforms.
+  // IsSizeAndAlignOf<jmp_buf>("jmp_buf", 48, 8);  // x86-64
+  // IsSizeAndAlignOf<jmp_buf>("jmp_buf", 36, 4);  // x86-32
+
+  // stddef.h
+  IsTypeAndSizeAndAlignOf<size_t, unsigned int>("size_t", 4, 4);
+  IsTypeAndSizeAndAlignOf<ptrdiff_t, int>("ptrdiff_t", 4, 4);
+  IsSizeAndAlignOf<wchar_t>("wchar_t", 4, 4);
+
+  // stdarg.h
+  IsSizeAndAlignOf<va_list>("va_list", 16, 4);
+
+  // sys/types.h
+  IsTypeAndSizeAndAlignOf<ssize_t, int>("ssize_t", 4, 4);
+  // TODO(eaeltsin): add more types
+
+  return 0;
+}
