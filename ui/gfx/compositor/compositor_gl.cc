1766c805a7fb718b01781738c6b90df5c03b50fc
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-12 14:48:50 +0000

    Make texture mapping OpenGL ES 2.0 compliant.
    According to 3.8.2 of the spec, we need to CLAMP_TO_EDGE and use either NEAREST or LINEAR. Otherwise, the fragment shader is supposed to draw black when texture mapping.
    
    BUG=none
    TEST=run TOUCH_UI build on compliant OpenGL ES driver --> screen should display
    
    Review URL: http://codereview.chromium.org/7637011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@96555 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e1419c1b3099898d388f05fb52d3908cfb978051

--- ui/gfx/compositor/compositor_gl.cc
+++ ui/gfx/compositor/compositor_gl.cc
@@ -316,6 +316,8 @@ void TextureGL::SetCanvas(const SkCanvas& canvas,
 
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   } else if (size_ != overall_size) {  // Size has changed.
     size_ = overall_size;
     glBindTexture(GL_TEXTURE_2D, texture_id_);
