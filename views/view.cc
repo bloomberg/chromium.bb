bec9c9cdba3385ea29aa76a0d54f955bc0fbc4ae
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-19 02:27:39 +0000

    Adds some debugging code to track crash. It seems as though ClearNativeFocus in View::OnFocus is triggering 'this' to be deleted. I couldn't readily find any views that do anything interesting when the native focus changes, but this should help us find out if that's true.
    BUG=89439
    TEST=none
    R=ben@chromium.org
    
    Review URL: http://codereview.chromium.org/7670077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97412 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fb0c3927221612b0d43e88c22c47491a5ea5a8cc

--- views/view.cc
+++ views/view.cc
@@ -82,6 +82,9 @@ ViewsDelegate* ViewsDelegate::views_delegate = NULL;
 // static
 char View::kViewClassName[] = "views/View";
 
+// TODO(sky): remove this when we figure out 89439.
+static View* view_being_focused_ = NULL;
+
 ////////////////////////////////////////////////////////////////////////////////
 // View, public:
 
@@ -120,6 +123,8 @@ View::View()
 }
 
 View::~View() {
+  CHECK_NE(view_being_focused_, this);
+
   if (parent_)
     parent_->RemoveChildView(this);
 
@@ -979,8 +984,11 @@ FocusManager* View::GetFocusManager() {
 
 void View::RequestFocus() {
   FocusManager* focus_manager = GetFocusManager();
-  if (focus_manager && IsFocusableInRootView())
+  if (focus_manager && IsFocusableInRootView()) {
+    view_being_focused_ = this;
     focus_manager->SetFocusedView(this);
+    view_being_focused_ = NULL;
+  }
 }
 
 bool View::SkipDefaultKeyEventProcessing(const KeyEvent& event) {
