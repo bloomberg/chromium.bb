2b55dc794e124fdd0a716828bc98191908d18f30
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-19 17:18:26 +0000

    Implemented a View based version of ShowErrorBox and ShowYesNoBox
    rhashimoto figured out the correct loop in the modal Yes/No box.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7677006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97473 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f125146993f5b26d7aaf9264cdff891b0b45c681

---
+++ chrome/browser/ui/views/simple_message_box_views.cc
@@ -0,0 +1,173 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/simple_message_box_views.h"
+
+#include "base/utf_string_conversions.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/simple_message_box.h"
+#include "chrome/browser/ui/views/window.h"
+#include "grit/generated_resources.h"
+#include "ui/base/l10n/l10n_util.h"
+#include "views/controls/message_box_view.h"
+#include "views/widget/widget.h"
+
+#if defined(TOUCH_UI)
+#include "views/focus/accelerator_handler.h"
+#endif
+
+namespace browser {
+
+void ShowErrorBox(gfx::NativeWindow parent,
+                  const string16& title,
+                  const string16& message) {
+  SimpleMessageBoxViews::ShowErrorBox(parent, title, message);
+}
+
+bool ShowYesNoBox(gfx::NativeWindow parent,
+                  const string16& title,
+                  const string16& message) {
+  return SimpleMessageBoxViews::ShowYesNoBox(parent, title, message);
+}
+
+}  // namespace browser
+
+////////////////////////////////////////////////////////////////////////////////
+// SimpleMessageBoxViews, public:
+
+// static
+void SimpleMessageBoxViews::ShowErrorBox(gfx::NativeWindow parent_window,
+                                         const string16& title,
+                                         const string16& message) {
+  int dialog_flags = ui::MessageBoxFlags::kFlagHasMessage |
+      ui::MessageBoxFlags::kFlagHasOKButton;
+
+  // This is a reference counted object so it is given an initial increment
+  // in the constructor with a corresponding decrement in DeleteDelegate().
+  new SimpleMessageBoxViews(parent_window, dialog_flags, title, message);
+}
+
+bool SimpleMessageBoxViews::ShowYesNoBox(gfx::NativeWindow parent_window,
+                                         const string16& title,
+                                         const string16& message) {
+  int dialog_flags = ui::MessageBoxFlags::kIsConfirmMessageBox;
+
+  // This is a reference counted object so it is given an initial increment
+  // in the constructor plus an extra one below to ensure the dialog persists
+  // until we retrieve the user response..
+  scoped_refptr<SimpleMessageBoxViews> dialog =
+      new SimpleMessageBoxViews(parent_window, dialog_flags, title, message);
+
+  // Make sure Chrome doesn't attempt to shut down with the dialog up.
+  g_browser_process->AddRefModule();
+
+  bool old_state = MessageLoopForUI::current()->NestableTasksAllowed();
+  MessageLoopForUI::current()->SetNestableTasksAllowed(true);
+  MessageLoopForUI::current()->Run(dialog);
+  MessageLoopForUI::current()->SetNestableTasksAllowed(old_state);
+
+  g_browser_process->ReleaseModule();
+
+  return dialog->Accepted();
+}
+
+bool SimpleMessageBoxViews::Cancel() {
+  disposition_ = DISPOSITION_CANCEL;
+  return true;
+}
+
+bool SimpleMessageBoxViews::Accept() {
+  disposition_ = DISPOSITION_OK;
+  return true;
+}
+
+int SimpleMessageBoxViews::GetDialogButtons() const {
+  // NOTE: It seems unsafe to assume that the flags for OK/cancel will always
+  // have the same value as the button ids.
+  return (dialog_flags_ & ui::MessageBoxFlags::kFlagHasOKButton
+      ? ui::MessageBoxFlags::DIALOGBUTTON_OK : 0) |
+      (dialog_flags_ & ui::MessageBoxFlags::kFlagHasCancelButton
+          ? ui::MessageBoxFlags::DIALOGBUTTON_CANCEL : 0);
+}
+
+std::wstring SimpleMessageBoxViews::GetDialogButtonLabel(
+    ui::MessageBoxFlags::DialogButton button) const {
+  return button == ui::MessageBoxFlags::DIALOGBUTTON_OK ?
+      UTF16ToWide(l10n_util::GetStringUTF16(IDS_OK)) :
+      UTF16ToWide(l10n_util::GetStringUTF16(IDS_CLOSE));
+}
+
+bool SimpleMessageBoxViews::ShouldShowWindowTitle() const {
+  return true;
+}
+
+std::wstring SimpleMessageBoxViews::GetWindowTitle() const {
+  return message_box_title_;
+}
+
+void SimpleMessageBoxViews::DeleteDelegate() {
+  Release();
+}
+
+bool SimpleMessageBoxViews::IsModal() const {
+  return true;
+}
+
+views::View* SimpleMessageBoxViews::GetContentsView() {
+  return message_box_view_;
+}
+
+views::Widget* SimpleMessageBoxViews::GetWidget() {
+  return message_box_view_->GetWidget();
+}
+
+const views::Widget* SimpleMessageBoxViews::GetWidget() const {
+  return message_box_view_->GetWidget();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// SimpleMessageBoxViews, private:
+
+SimpleMessageBoxViews::SimpleMessageBoxViews(gfx::NativeWindow parent_window,
+                                             int dialog_flags,
+                                             const string16& title,
+                                             const string16& message)
+    : dialog_flags_(dialog_flags),
+      disposition_(DISPOSITION_UNKNOWN) {
+  message_box_title_ = UTF16ToWide(title);
+  message_box_view_ = new views::MessageBoxView(
+      dialog_flags,
+      UTF16ToWide(message),
+      std::wstring());
+  browser::CreateViewsWindow(parent_window, this)->Show();
+
+  // Add reference to be released in DeleteDelegate().
+  AddRef();
+}
+
+SimpleMessageBoxViews::~SimpleMessageBoxViews() {
+}
+
+#if defined(OS_WIN)
+bool SimpleMessageBoxViews::Dispatch(const MSG& msg) {
+  TranslateMessage(&msg);
+  DispatchMessage(&msg);
+  return disposition_ == DISPOSITION_UNKNOWN;
+}
+#elif defined(TOUCH_UI)
+base::MessagePumpDispatcher::DispatchStatus
+    SimpleMessageBoxViews::Dispatch(XEvent* xev) {
+  if (!views::DispatchXEvent(xev))
+    return EVENT_IGNORED;
+
+  if (disposition_ == DISPOSITION_UNKNOWN)
+    return base::MessagePumpDispatcher::EVENT_PROCESSED;
+  return base::MessagePumpDispatcher::EVENT_QUIT;
+}
+#else
+bool SimpleMessageBoxViews::Dispatch(GdkEvent* event) {
+  gtk_main_do_event(event);
+  return disposition_ == DISPOSITION_UNKNOWN;
+}
+#endif
---
+++ chrome/browser/ui/views/simple_message_box_views.h
@@ -0,0 +1,92 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_SIMPLE_MESSAGE_BOX_VIEWS_H_
+#define CHROME_BROWSER_UI_VIEWS_SIMPLE_MESSAGE_BOX_VIEWS_H_
+#pragma once
+
+#include "base/basictypes.h"
+#include "base/memory/ref_counted.h"
+#include "base/message_loop.h"
+#include "ui/gfx/native_widget_types.h"
+#include "views/window/dialog_delegate.h"
+
+namespace views {
+class MessageBoxView;
+}
+
+// Simple message box implemented in Views.
+class SimpleMessageBoxViews : public views::DialogDelegate,
+                              public MessageLoop::Dispatcher,
+                              public base::RefCounted<SimpleMessageBoxViews> {
+ public:
+  friend class base::RefCounted<SimpleMessageBoxViews>;
+
+  // The state of the dialog when closing.
+  enum DispositionType {
+    DISPOSITION_UNKNOWN,
+    DISPOSITION_CANCEL,
+    DISPOSITION_OK
+  };
+
+  // Message box is modal to |parent_window|.
+  static void ShowErrorBox(gfx::NativeWindow parent_window,
+                           const string16& title,
+                           const string16& message);
+  static bool ShowYesNoBox(gfx::NativeWindow parent_window,
+                           const string16& title,
+                           const string16& message);
+
+  // Overridden from views::DialogDelegate:
+  virtual bool Cancel() OVERRIDE;
+  virtual bool Accept() OVERRIDE;
+
+  // Returns true if the Accept button was clicked.
+  const bool Accepted() {
+    return disposition_ == DISPOSITION_OK;
+  }
+
+ protected:
+  // Overridden from views::DialogDelegate:
+  virtual int GetDialogButtons() const OVERRIDE;
+  virtual std::wstring GetDialogButtonLabel(
+      MessageBoxFlags::DialogButton button) const OVERRIDE;
+
+  // Overridden from views::WindowDelegate:
+  virtual bool ShouldShowWindowTitle() const OVERRIDE;
+  virtual std::wstring GetWindowTitle() const OVERRIDE;
+  virtual void DeleteDelegate() OVERRIDE;
+  virtual bool IsModal() const OVERRIDE;
+  virtual views::View* GetContentsView() OVERRIDE;
+  virtual views::Widget* GetWidget() OVERRIDE;
+  virtual const views::Widget* GetWidget() const OVERRIDE;
+
+ private:
+  SimpleMessageBoxViews(gfx::NativeWindow parent_window,
+                        int dialog_flags,
+                        const string16& title,
+                        const string16& message);
+  virtual ~SimpleMessageBoxViews();
+
+  // MessageLoop::Dispatcher implementation.
+#if defined(OS_WIN)
+  // Dispatcher method. This returns true if the menu was canceled, or
+  // if the message is such that the menu should be closed.
+  virtual bool Dispatch(const MSG& msg) OVERRIDE;
+#elif defined(TOUCH_UI)
+  virtual base::MessagePumpDispatcher::DispatchStatus Dispatch(
+      XEvent* xevent) OVERRIDE;
+#else
+  virtual bool Dispatch(GdkEvent* event) OVERRIDE;
+#endif
+
+  int dialog_flags_;
+  std::wstring message_box_title_;
+  views::MessageBoxView* message_box_view_;
+  DispositionType disposition_;
+
+  DISALLOW_COPY_AND_ASSIGN(SimpleMessageBoxViews);
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_SIMPLE_MESSAGE_BOX_VIEWS_H_
