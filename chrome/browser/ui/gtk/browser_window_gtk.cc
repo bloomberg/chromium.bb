4c21869a38218cda48d539a1d34b9cab1ea7ad05
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-20 00:11:38 +0000

    gtk: Don't use static variable of user-defined type (in this case a NineBox).
    Instead make it a pointer and initialize it lazily.
    
    BUG=83856
    TEST=None
    
    R=tony@chromium.org
    
    Review URL: http://codereview.chromium.org/7695006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97538 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 74f649e83fb1b6f144ff20bde6abd7ca752689f9

--- chrome/browser/ui/gtk/browser_window_gtk.cc
+++ chrome/browser/ui/gtk/browser_window_gtk.cc
@@ -424,18 +424,19 @@ gboolean BrowserWindowGtk::OnCustomFrameExpose(GtkWidget* widget,
   cairo_destroy(cr);
 
   if (UseCustomFrame() && !IsMaximized()) {
-    static NineBox custom_frame_border(
-        IDR_WINDOW_TOP_LEFT_CORNER,
-        IDR_WINDOW_TOP_CENTER,
-        IDR_WINDOW_TOP_RIGHT_CORNER,
-        IDR_WINDOW_LEFT_SIDE,
-        0,
-        IDR_WINDOW_RIGHT_SIDE,
-        IDR_WINDOW_BOTTOM_LEFT_CORNER,
-        IDR_WINDOW_BOTTOM_CENTER,
-        IDR_WINDOW_BOTTOM_RIGHT_CORNER);
-
-    custom_frame_border.RenderToWidget(widget);
+    static NineBox* custom_frame_border = NULL;
+    if (!custom_frame_border) {
+      custom_frame_border = new NineBox(IDR_WINDOW_TOP_LEFT_CORNER,
+                                        IDR_WINDOW_TOP_CENTER,
+                                        IDR_WINDOW_TOP_RIGHT_CORNER,
+                                        IDR_WINDOW_LEFT_SIDE,
+                                        0,
+                                        IDR_WINDOW_RIGHT_SIDE,
+                                        IDR_WINDOW_BOTTOM_LEFT_CORNER,
+                                        IDR_WINDOW_BOTTOM_CENTER,
+                                        IDR_WINDOW_BOTTOM_RIGHT_CORNER);
+    }
+    custom_frame_border->RenderToWidget(widget);
   }
 
   return FALSE;  // Allow subwidgets to paint.
