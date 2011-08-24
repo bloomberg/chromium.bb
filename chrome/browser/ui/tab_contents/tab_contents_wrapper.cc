da07601311bb226f90abd841ef4fff4162f39b4a
mark@chromium.org <mark@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 05:27:21 +0000

    Revert trunk r97990. It broke some Linux builds:
    http://build.chromium.org/p/chromium/builders/Linux%20Clang%20%28ChromiumOS%20dbg%29/builds/9822/steps/compile/logs/stdio
    http://build.chromium.org/p/chromium/builders/Linux%20Builder%20%28Views%20dbg%29/builds/12722/steps/compile/logs/stdio
    http://build.chromium.org/p/chromium/builders/Linux%20Touch/builds/4948/steps/compile/logs/stdio
    http://build.chromium.org/p/chromium/builders/Arm/builds/16148/steps/compile/logs/stdio
    
    You can use the linux_view, linux_chromeos, linux_clang_chromeos, and other
    try bots to test further iterations of this change.
    
    --
    
    Web Intent Picker UI (implemented as a constrained dialog, linux only)
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7648061
    
    TBR=binji@chromium.org
    Review URL: http://codereview.chromium.org/7701022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@97994 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1158d69dfd5c0a320ba237abe3044566d98786ab

--- chrome/browser/ui/tab_contents/tab_contents_wrapper.cc
+++ chrome/browser/ui/tab_contents/tab_contents_wrapper.cc
@@ -54,8 +54,6 @@
 #include "chrome/browser/ui/blocked_content/blocked_content_tab_helper.h"
 #include "chrome/browser/ui/bookmarks/bookmark_tab_helper.h"
 #include "chrome/browser/ui/find_bar/find_tab_helper.h"
-#include "chrome/browser/ui/intents/web_intent_constrained_dialog_factory.h"
-#include "chrome/browser/ui/intents/web_intent_picker_controller.h"
 #include "chrome/browser/ui/search_engines/search_engine_tab_helper.h"
 #include "chrome/browser/ui/tab_contents/tab_contents_wrapper_delegate.h"
 #include "chrome/common/chrome_notification_types.h"
@@ -158,9 +156,6 @@ TabContentsWrapper::TabContentsWrapper(TabContents* contents)
   content_settings_.reset(new TabSpecificContentSettings(contents));
   translate_tab_helper_.reset(new TranslateTabHelper(contents));
   print_view_manager_.reset(new printing::PrintViewManager(this));
-  web_intent_picker_controller_.reset(new WebIntentPickerController(
-        contents,
-        new WebIntentConstrainedDialogFactory()));
 
   // Create the per-tab observers.
   external_protocol_observer_.reset(new ExternalProtocolObserver(contents));
@@ -650,8 +645,6 @@ void TabContentsWrapper::OnWebIntentDispatch(const IPC::Message& message,
              << "\ntype=" << UTF16ToASCII(type)
              << "\nrenderer_id=" << message.routing_id()
              << "\nid=" << intent_id;
-
-  web_intent_picker_controller_->ShowDialog(action, type);
 }
 
 void TabContentsWrapper::OnSnapshot(const SkBitmap& bitmap) {
