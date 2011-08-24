8b9938b3aa7b08ccbb5cac4ed134f4fae3dd0a91
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-24 20:13:06 +0000

    [Mac] Return an empty StringPiece from GetDataResource() when |g_resource_data_pack| is NULL.
    When running unit tests, |g_resource_data_pack| is never loaded. As a result,
    if a resource is needed by code exercised by a test, the dereference of
    |g_resource_data_pack| would previously crash, unless that resource id was
    special cased.
    
    This CL changes it to instead return an empty StringPiece, rather than
    crashing. This allows PlatformBridge::loadPlatformImageResource() to
    return a Image::nullImage() in this case, which is fine for some resources
    loaded during unit tests.
    
    In particular, this fixes a crash in webkit_unit_tests that would otherwise
    happen with: https://bugs.webkit.org/show_bug.cgi?id=66707.
    
    BUG=93397
    TEST=Run webkit_unit_tests with patch from https://bugs.webkit.org/show_bug.cgi?id=66707. They shouldn't crash.
    
    Review URL: http://codereview.chromium.org/7714036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98101 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f6c682a5133e568ad5d6cab27206c20c7d923a90

--- webkit/support/platform_support_mac.mm
+++ webkit/support/platform_support_mac.mm
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -211,7 +211,8 @@ base::StringPiece GetDataResource(int resource_id) {
   }
   }
   base::StringPiece res;
-  g_resource_data_pack->GetStringPiece(resource_id, &res);
+  if (g_resource_data_pack)
+    g_resource_data_pack->GetStringPiece(resource_id, &res);
   return res;
 }
 
