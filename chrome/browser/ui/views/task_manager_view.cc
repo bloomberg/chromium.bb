00bd21b55d868a7e1e4c09dec129cf30c446b33f
gideonwald@chromium.org <gideonwald@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-26 23:14:52 +0000

    Make the first character of the Task Manager kill button be underlined.
    BUG=92510
    TEST=Make sure that the text "End Process" isn't preceded by an ampersand on Windows and has the E underlined.
    
    
    Review URL: http://codereview.chromium.org/7723001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98515 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1c50df7614fb90a3685569667f89d668d4d5bee9

--- chrome/browser/ui/views/task_manager_view.cc
+++ chrome/browser/ui/views/task_manager_view.cc
@@ -458,6 +458,7 @@ void TaskManagerView::Init() {
   kill_button_->AddAccelerator(views::Accelerator(ui::VKEY_E,
                                                   false, false, false));
   kill_button_->SetAccessibleKeyboardShortcut(L"E");
+  kill_button_->set_prefix_type(views::TextButtonBase::PREFIX_SHOW);
   about_memory_link_ = new views::Link(UTF16ToWide(
       l10n_util::GetStringUTF16(IDS_TASK_MANAGER_ABOUT_MEMORY_LINK)));
   about_memory_link_->set_listener(this);
