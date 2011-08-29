6842f80edb6f7247860294ea41598b6b35e79735
krasin@chromium.org <krasin@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-08-29 20:09:53 +0000

    pnacl abi test: add checks for short/long long/long double sizeof and align. Review URL: http://codereview.chromium.org/7795003
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6579 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: f3bd53ac9114a2273f5a1aab673ea1ba48564ff1

--- tests/pnacl_abi/data_type.c
+++ tests/pnacl_abi/data_type.c
@@ -8,14 +8,17 @@
 #include <stddef.h>
 #include <unwind.h>
 
-int size_c = sizeof(char);
-int size_i = sizeof(int);
-int size_l = sizeof(long);
+int size_c  = sizeof(char);
+int size_s  = sizeof(short);
+int size_i  = sizeof(int);
+int size_l  = sizeof(long);
+int size_ll = sizeof(long long);
 
 int size_p = sizeof(void*);
 
-int size_f = sizeof(float);
-int size_d = sizeof(double);
+int size_f  = sizeof(float);
+int size_d  = sizeof(double);
+int size_ld = sizeof(long double);
 
 /* A 16 byte buffer to accomodate all three architectures */
 int size_vl = sizeof(va_list);
@@ -28,13 +31,16 @@ int size_jb = sizeof(jmp_buf);
   int align_ ## name = offsetof(AlignStruct_ ## name, x)
 
 SET_ALIGNMENT(char, c);
+SET_ALIGNMENT(short, s);
 SET_ALIGNMENT(int, i);
 SET_ALIGNMENT(long, l);
+SET_ALIGNMENT(long long, ll);
 
 SET_ALIGNMENT(void*, p);
 
 SET_ALIGNMENT(float, f);
 SET_ALIGNMENT(double, d);
+SET_ALIGNMENT(long double, ld);
 
 SET_ALIGNMENT(va_list, vl);
 
