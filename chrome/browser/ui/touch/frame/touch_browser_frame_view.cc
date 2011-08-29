19c18472b97971df12d1db7171662b244f5d3193
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-29 16:49:24 +0000

    touchui: Some cleanup for managing keyboard visibility.
    Views IME keeps track of the focus changes, and notifies the keyboard manager if
    the keyboard should be visible or not. So doing the same from
    TouchBrowserFrameView isn't necessary anymore.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98646 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f6ab52acb195a42c1e2cfb4482fb40a65cf98209

--- chrome/browser/ui/touch/frame/touch_browser_frame_view.cc
+++ chrome/browser/ui/touch/frame/touch_browser_frame_view.cc
@@ -5,11 +5,7 @@
 #include "chrome/browser/ui/touch/frame/touch_browser_frame_view.h"
 
 #include "chrome/browser/ui/touch/keyboard/keyboard_manager.h"
-#include "ui/base/ime/text_input_type.h"
 #include "views/controls/button/image_button.h"
-#include "views/controls/textfield/textfield.h"
-#include "views/focus/focus_manager.h"
-#include "views/ime/text_input_client.h"
 
 // static
 const char TouchBrowserFrameView::kViewClassName[] =
@@ -20,8 +16,7 @@ const char TouchBrowserFrameView::kViewClassName[] =
 
 TouchBrowserFrameView::TouchBrowserFrameView(BrowserFrame* frame,
                                              BrowserView* browser_view)
-    : OpaqueBrowserFrameView(frame, browser_view),
-      focus_listener_added_(false) {
+    : OpaqueBrowserFrameView(frame, browser_view) {
   // Make sure the singleton KeyboardManager object is initialized.
   KeyboardManager::GetInstance();
 }
@@ -29,45 +24,8 @@ TouchBrowserFrameView::TouchBrowserFrameView(BrowserFrame* frame,
 TouchBrowserFrameView::~TouchBrowserFrameView() {
 }
 
-///////////////////////////////////////////////////////////////////////////////
-// TouchBrowserFrameView, private:
-
-void TouchBrowserFrameView::FocusWillChange(views::View* focused_before,
-                                            views::View* focused_now) {
-  views::Widget* widget = focused_now ? focused_now->GetWidget() : NULL;
-  if (!widget || !widget->IsActive())
-    return;
-
-  views::TextInputClient* input =
-      focused_now ? focused_now->GetTextInputClient() : NULL;
-  // Show the keyboard if the focused view supports text-input.
-  KeyboardManager* keyboard = KeyboardManager::GetInstance();
-  if (input && input->GetTextInputType() != ui::TEXT_INPUT_TYPE_NONE)
-    keyboard->ShowKeyboardForWidget(focused_now->GetWidget());
-  else
-    keyboard->Hide();
-}
-
 std::string TouchBrowserFrameView::GetClassName() const {
   return kViewClassName;
-}
-
-void TouchBrowserFrameView::ViewHierarchyChanged(bool is_add,
-                                                 View* parent,
-                                                 View* child) {
-  OpaqueBrowserFrameView::ViewHierarchyChanged(is_add, parent, child);
-  if (!GetFocusManager())
-    return;
-
-  if (is_add && !focus_listener_added_) {
-    // Add focus listener when this view is added to the hierarchy.
-    GetFocusManager()->AddFocusChangeListener(this);
-    focus_listener_added_ = true;
-  } else if (!is_add && focus_listener_added_) {
-    // Remove focus listener when this view is removed from the hierarchy.
-    GetFocusManager()->RemoveFocusChangeListener(this);
-    focus_listener_added_ = false;
-  }
 }
 
 bool TouchBrowserFrameView::HitTest(const gfx::Point& point) const {
--- chrome/browser/ui/touch/frame/touch_browser_frame_view.h
+++ chrome/browser/ui/touch/frame/touch_browser_frame_view.h
@@ -6,24 +6,13 @@
 #define CHROME_BROWSER_UI_TOUCH_FRAME_TOUCH_BROWSER_FRAME_VIEW_H_
 #pragma once
 
-#include "chrome/browser/tabs/tab_strip_model_observer.h"
 #include "chrome/browser/ui/views/frame/opaque_browser_frame_view.h"
-#include "content/common/notification_observer.h"
-#include "content/common/notification_registrar.h"
-#include "googleurl/src/gurl.h"
-#include "ui/base/animation/animation_delegate.h"
-#include "views/focus/focus_manager.h"
-
-#if defined(OS_CHROMEOS)
-#include "chrome/browser/chromeos/input_method/input_method_manager.h"
-#endif
 
 class BrowserFrame;
 class BrowserView;
 
 class TouchBrowserFrameView
-    : public OpaqueBrowserFrameView,
-      public views::FocusChangeListener {
+    : public OpaqueBrowserFrameView {
  public:
   // Internal class name.
   static const char kViewClassName[];
@@ -33,16 +22,9 @@ class TouchBrowserFrameView
   virtual ~TouchBrowserFrameView();
 
  private:
-  // views::FocusChangeListener implementation
-  virtual void FocusWillChange(views::View* focused_before,
-                               views::View* focused_now);
-
   // Overridden from views::View
   virtual std::string GetClassName() const OVERRIDE;
-  virtual void ViewHierarchyChanged(bool is_add, View* parent, View* child);
   virtual bool HitTest(const gfx::Point& point) const OVERRIDE;
-
-  bool focus_listener_added_;
 
   DISALLOW_COPY_AND_ASSIGN(TouchBrowserFrameView);
 };
