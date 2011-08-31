8a8c24aa5ff8679e76d486684d6a00e82659caad
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 16:13:39 +0000

    DevTool: [OS_CHROMEOS] window doesn't open after detaching it for the current tab.
    Review URL: http://codereview.chromium.org/7740047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98979 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4befe2ec782e9237aff67935c6ae16ce7a96674d

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -466,7 +466,11 @@ Browser* Browser::CreateForApp(Type type,
 
 // static
 Browser* Browser::CreateForDevTools(Profile* profile) {
+#if defined(OS_CHROMEOS)
+  CreateParams params(TYPE_TABBED, profile);
+#else
   CreateParams params(TYPE_POPUP, profile);
+#endif
   params.app_name = DevToolsWindow::kDevToolsApp;
   return CreateWithParams(params);
 }
