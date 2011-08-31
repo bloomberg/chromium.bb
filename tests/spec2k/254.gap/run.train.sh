ecebd003fcadbfc74b646d9ad35034641724b0fc
pdox@google.com <pdox@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-08-31 14:52:18 +0000

    Add -fwrapv to Spec2K 254.gap Makefile. gap relies on signed arithmetic overflow being well-defined. Re-enable Spec2K 254.gap for PNaCl.
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2036
    Review URL: http://codereview.chromium.org/7776032
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6597 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 35202709ba0f2097f8c5669c84a9d50ec9162346

--- tests/spec2k/254.gap/run.train.sh
+++ tests/spec2k/254.gap/run.train.sh
@@ -7,14 +7,6 @@ PREFIX=${PREFIX:-}
 VERIFY=${VERIFY:-yes}
 EMU_HACK=${EMU_HACK:-yes}
 
-if [[ $1 =~ .*pnacl.* ]]; then
-  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-  echo "@@@BUILD_STEP skip 254.gap due to BUG2036 in PNaCl@@@"
-  echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-  echo OK
-  exit 0
-fi
-
 DASHDASH=
 if [[ "${PREFIX}" =~ sel_ldr ]] ; then
  DASHDASH="--"
