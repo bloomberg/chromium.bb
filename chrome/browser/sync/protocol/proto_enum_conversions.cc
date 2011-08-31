16c31e6253b29739c7c58f314f0723e17d576df4
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-08-31 16:34:35 +0000

    Break TYPE_PC into TYPE_WIN, TYPE_MAC, TYPE_LINUX.
    This can give us flexibility down the line with ui changes. No clients use the protocol yet so it's safe to move CrOs from 2 to 4.
    
    BUG=94693
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7811001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@98984 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 38fdd650c9ccec20cca84b3138ee5eb49913dc44

--- chrome/browser/sync/protocol/proto_enum_conversions.cc
+++ chrome/browser/sync/protocol/proto_enum_conversions.cc
@@ -91,10 +91,13 @@ const char* GetUpdatesSourceString(
 
 const char* GetDeviceTypeString(
     sync_pb::SessionHeader::DeviceType device_type) {
-  ASSERT_ENUM_BOUNDS(sync_pb::SessionHeader, DeviceType, TYPE_PC, TYPE_CROS);
+  ASSERT_ENUM_BOUNDS(sync_pb::SessionHeader, DeviceType, TYPE_WIN, TYPE_OTHER);
   switch (device_type) {
-    ENUM_CASE(sync_pb::SessionHeader, TYPE_PC);
+    ENUM_CASE(sync_pb::SessionHeader, TYPE_WIN);
+    ENUM_CASE(sync_pb::SessionHeader, TYPE_MAC);
+    ENUM_CASE(sync_pb::SessionHeader, TYPE_LINUX);
     ENUM_CASE(sync_pb::SessionHeader, TYPE_CROS);
+    ENUM_CASE(sync_pb::SessionHeader, TYPE_OTHER);
   }
   NOTREACHED();
   return "";
