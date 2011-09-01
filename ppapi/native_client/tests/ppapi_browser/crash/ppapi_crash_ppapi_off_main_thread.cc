2bfd857ccd0fd468172141a0a8331b09bbedb013
sehr@google.com <sehr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-01 19:20:46 +0000

    Add missing #include in ppapi_crash_ppapi_off_main_thread test (Copy of http://codereview.chromium.org/7740085/ for mcgrathr@google.com) BUG=none TEST=none Review URL: http://codereview.chromium.org/7828012
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99225 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b7e8121b1d32a02d394c9efdbea42e64e9efb1f0

--- ppapi/native_client/tests/ppapi_browser/crash/ppapi_crash_ppapi_off_main_thread.cc
+++ ppapi/native_client/tests/ppapi_browser/crash/ppapi_crash_ppapi_off_main_thread.cc
@@ -1,10 +1,12 @@
-// Copyright (c) 2011 The Native Client Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
 // Test that we kill the nexe on a CHECK and handle it gracefully on the
 // trusted side when untrusted code makes unsupported PPAPI calls
 // on other than the main thread.
+
+#include <pthread.h>
 
 #include "native_client/src/shared/platform/nacl_check.h"
 #include "native_client/tests/ppapi_test_lib/get_browser_interface.h"
