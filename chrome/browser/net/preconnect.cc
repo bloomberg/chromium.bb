1a024f30c4618933c9701156a13b21f859420ca0
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 17:10:14 +0000

    Change HttpNetworkTransaction and HttpStreamFactoryImpl::Job to keep different SSLConfig objects for HTTPS Proxies vs HTTPS Servers.
    Add an is_proxy field to SSLCertRequest indicating if the
    request came from a proxy or from an origin server.
    
    BUG=95071
    
    Review URL: http://codereview.chromium.org/7768002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99392 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 102957f8a81d2aeb4e0069603977221a3b93a0bd

--- chrome/browser/net/preconnect.cc
+++ chrome/browser/net/preconnect.cc
@@ -102,7 +102,7 @@ void PreconnectOnIOThread(
 
   net::HttpStreamFactory* http_stream_factory = session->http_stream_factory();
   http_stream_factory->PreconnectStreams(
-      count, request_info, ssl_config, net::BoundNetLog());
+      count, request_info, ssl_config, ssl_config, net::BoundNetLog());
 }
 
 }  // namespace chrome_browser_net
