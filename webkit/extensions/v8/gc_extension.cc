1b2bccbe8a0adb7b4572e41ca1c9b439f08505ff
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-02 18:15:50 +0000

    webkit/extensions/v8: Forward declare v8::Extension in GCExtension header.
    BUG=None
    TEST=None
    
    R=asargent@chromium.org
    
    Review URL: http://codereview.chromium.org/7828032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99405 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5407c3827e3177c6c505d39250502579529d6a83

--- webkit/extensions/v8/gc_extension.cc
+++ webkit/extensions/v8/gc_extension.cc
@@ -1,23 +1,26 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "webkit/extensions/v8/gc_extension.h"
 
+#include "v8/include/v8.h"
+
+const char kGCExtensionName[] = "v8/GCController";
+
 namespace extensions_v8 {
 
-const char* kGCExtensionName = "v8/GCController";
-
+// static
 v8::Extension* GCExtension::Get() {
   v8::Extension* extension = new v8::Extension(
-    kGCExtensionName,
-    "(function () {"
-    "   var v8_gc;"
-    "   if (gc) v8_gc = gc;"
-    "   GCController = new Object();"
-    "   GCController.collect ="
-    "     function() {if (v8_gc) v8_gc(); };"
-    " })();");
+      kGCExtensionName,
+      "(function () {"
+      "   var v8_gc;"
+      "   if (gc) v8_gc = gc;"
+      "   GCController = new Object();"
+      "   GCController.collect ="
+      "     function() {if (v8_gc) v8_gc(); };"
+      " })();");
   return extension;
 }
 
--- webkit/extensions/v8/gc_extension.h
+++ webkit/extensions/v8/gc_extension.h
@@ -1,17 +1,19 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// The GCExtension is a v8 extension to expose a method into JS for triggering
-// garbage collection.  This should only be used for debugging.
-
 #ifndef WEBKIT_EXTENSIONS_V8_GC_EXTENSION_H_
 #define WEBKIT_EXTENSIONS_V8_GC_EXTENSION_H_
+#pragma once
 
-#include "v8/include/v8.h"
+namespace v8 {
+class Extension;
+}
 
 namespace extensions_v8 {
 
+// GCExtension is a v8 extension to expose a method into JS for triggering
+// garbage collection. This should only be used for debugging.
 class GCExtension {
  public:
   static v8::Extension* Get();
