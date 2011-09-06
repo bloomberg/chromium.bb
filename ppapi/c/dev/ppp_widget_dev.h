d4c64050d428a9a294e4aece780060e04cc6b72a
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-06 21:20:31 +0000

    Convert c/dev ppp_zoom_dev and ppp_widget_dev to IDL
    TEST= ./generator.py & try
    BUG= http://code.google.com/p/chromium/issues/detail?id=89968
    Review URL: http://codereview.chromium.org/7833031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99823 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0924a206ca821e8537e432c42293c71ef1f1b8fa

--- ppapi/c/dev/ppp_widget_dev.h
+++ ppapi/c/dev/ppp_widget_dev.h
@@ -1,23 +1,48 @@
-/* Copyright (c) 2010 The Chromium Authors. All rights reserved.
+/* Copyright (c) 2011 The Chromium Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+
+/* From dev/ppp_widget_dev.idl modified Fri Aug 26 17:59:12 2011. */
+
 #ifndef PPAPI_C_DEV_PPP_WIDGET_DEV_H_
 #define PPAPI_C_DEV_PPP_WIDGET_DEV_H_
 
 #include "ppapi/c/pp_instance.h"
-#include "ppapi/c/pp_resource.h"
+#include "ppapi/c/pp_macros.h"
+#include "ppapi/c/pp_point.h"
 #include "ppapi/c/pp_rect.h"
+#include "ppapi/c/pp_resource.h"
+#include "ppapi/c/pp_size.h"
+#include "ppapi/c/pp_stdint.h"
 
-// Interface for the plugin to implement when using a widget.
-#define PPP_WIDGET_DEV_INTERFACE "PPP_Widget(Dev);0.2"
+#define PPP_WIDGET_DEV_INTERFACE_0_2 "PPP_Widget(Dev);0.2"
+#define PPP_WIDGET_DEV_INTERFACE PPP_WIDGET_DEV_INTERFACE_0_2
 
+/**
+ * @file
+ * Implementation of the Widget interface.
+ */
+
+
+/**
+ * @addtogroup Interfaces
+ * @{
+ */
+/**
+ * The interface for reusing browser widgets.
+ */
 struct PPP_Widget_Dev {
-  // Informs the instance that the given rectangle needs to be repainted.
+  /**
+   * Informs the instance that the given rectangle needs to be repainted.
+   */
   void (*Invalidate)(PP_Instance instance,
                      PP_Resource widget,
                      const struct PP_Rect* dirty_rect);
 };
+/**
+ * @}
+ */
 
 #endif  /* PPAPI_C_DEV_PPP_WIDGET_DEV_H_ */
 
