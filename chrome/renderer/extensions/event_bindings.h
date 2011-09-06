5264b228bd6465cc01c4863ea7f611becfef1114
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-06 03:23:56 +0000

    Refactor the ContextInfo struct from bindings_utils into a real class. Pull functionality from bindings_utils and event_bindings in as methods. Simplify lifetime management.
    Review URL: http://codereview.chromium.org/7792090
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99689 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2a55391236948b261e9603653ddf8e29e975ebb5

--- chrome/renderer/extensions/event_bindings.h
+++ chrome/renderer/extensions/event_bindings.h
@@ -6,57 +6,21 @@
 #define CHROME_RENDERER_EXTENSIONS_EVENT_BINDINGS_H_
 #pragma once
 
-#include <string>
+#include "v8/include/v8.h"
 
 class ExtensionDispatcher;
-class GURL;
 class RenderThreadBase;
-class RenderView;
-
-namespace base {
-class ListValue;
-}
-
-namespace v8 {
-class Context;
-class Extension;
-template <class T> class Handle;
-}
-
-namespace WebKit {
-class WebFrame;
-}
 
 // This class deals with the javascript bindings related to Event objects.
 class EventBindings {
  public:
   static const char* kName;  // The v8::Extension name, for dependencies.
-  static const char* kTestingExtensionId;
 
   static v8::Extension* Get(ExtensionDispatcher* dispatcher);
 
   // Allow RenderThread to be mocked out.
   static void SetRenderThread(RenderThreadBase* thread);
   static RenderThreadBase* GetRenderThread();
-
-  // Handle a script context coming / going away.
-  static void HandleContextCreated(WebKit::WebFrame* frame,
-                                   v8::Handle<v8::Context> context,
-                                   ExtensionDispatcher* extension_dispatcher,
-                                   int isolated_world_id);
-  static void HandleContextDestroyed(WebKit::WebFrame* frame);
-
-  // Calls the given function in each registered context which is listening for
-  // events.  If render_view is non-NULL, only call the function in contexts
-  // belonging to that view.  See comments on
-  // bindings_utils::CallFunctionInContext for more details.
-  // The called javascript function should not return a value other than
-  // v8::Undefined(). A DCHECK is setup to break if it is otherwise.
-  static void CallFunction(const std::string& extension_id,
-                           const std::string& function_name,
-                           const base::ListValue& arguments,
-                           RenderView* render_view,
-                           const GURL& event_url);
 };
 
 #endif  // CHROME_RENDERER_EXTENSIONS_EVENT_BINDINGS_H_
