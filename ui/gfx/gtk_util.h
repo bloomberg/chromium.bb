ba9897dc5eba762b1ebf3d6616a98a5d7f199e0b
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-07 01:21:55 +0000

    Really fix Linux shared build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@99887 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 14256d3c80984be35df633521ce66427eda654ba

--- ui/gfx/gtk_util.h
+++ ui/gfx/gtk_util.h
@@ -64,13 +64,13 @@ UI_EXPORT std::string RemoveWindowsStyleAccelerators(const std::string& label);
 // Makes a copy of |pixels| with the ordering changed from BGRA to RGBA.
 // The caller is responsible for free()ing the data. If |stride| is 0, it's
 // assumed to be 4 * |width|.
-UI_EXPORT uint8_t* BGRAToRGBA(const uint8_t* pixels,
+uint8_t* BGRAToRGBA(const uint8_t* pixels,
                               int width,
                               int height,
                               int stride);
 
 // Initialize some GTK settings so that our dialogs are consistent.
-void InitRCStyles();
+UI_EXPORT void InitRCStyles();
 
 }  // namespace gfx
 
