3765524cfed5072fabcd77bcc40c37caaf822052
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 13:00:17 +0000

    Revert the chrome_tests.sh changes until the Bash support in the buildsystem is fixed.
    TBR=timurrrr
    Review URL: http://codereview.chromium.org/7857034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100396 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 81e0a0d3472499b29be60d6a5c098c144c028904

--- tools/valgrind/chrome_tests.sh
+++ tools/valgrind/chrome_tests.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2009 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -8,60 +8,18 @@
 
 export THISDIR=`dirname $0`
 
-TOOL_OPTION=0
-NEEDS_VALGRIND=0
-
-# We need to set CHROME_VALGRIND iff using Memcheck or TSan-Valgrind:
-#   tools/valgrind/chrome_tests.sh --tool memcheck
-# or
-#   tools/valgrind/chrome_tests.sh --tool=memcheck
-# (same for "--tool=tsan")
-for flag in $@
-do
-  if [ "$flag" == "--tool" ]
-  then
-    TOOL_OPTION=1
-    continue
-  elif [ "$flag" == "--tool=tsan" ]
-  then
-    NEEDS_VALGRIND=1
-    break
-  elif [ "$flag" == "--tool=memcheck" ]
-  then
-    NEEDS_VALGRIND=1
-    break
-  fi
-  if [ "$TOOL_OPTION" == "1" ]
-  then
-    if [ "$flag" == "memcheck" ]
-    then
-      NEEDS_VALGRIND=1
-      break
-    elif [ "$flag" == "tsan" ]
-    then
-      NEEDS_VALGRIND=1
-      break
-    else
-      TOOL_OPTION=0
-    fi
-  fi
-done
-
-if [ "$NEEDS_VALGRIND" == "1" ]
+CHROME_VALGRIND=`sh $THISDIR/locate_valgrind.sh`
+if [ "$CHROME_VALGRIND" = "" ]
 then
-  CHROME_VALGRIND=`sh $THISDIR/locate_valgrind.sh`
-  if [ "$CHROME_VALGRIND" = "" ]
-  then
-    # locate_valgrind.sh failed
-    exit 1
-  fi
-  echo "Using valgrind binaries from ${CHROME_VALGRIND}"
-
-  PATH="${CHROME_VALGRIND}/bin:$PATH"
-  # We need to set these variables to override default lib paths hard-coded into
-  # Valgrind binary.
-  export VALGRIND_LIB="$CHROME_VALGRIND/lib/valgrind"
-  export VALGRIND_LIB_INNER="$CHROME_VALGRIND/lib/valgrind"
+  # locate_valgrind.sh failed
+  exit 1
 fi
+echo "Using valgrind binaries from ${CHROME_VALGRIND}"
+
+PATH="${CHROME_VALGRIND}/bin:$PATH"
+# We need to set these variables to override default lib paths hard-coded into
+# Valgrind binary.
+export VALGRIND_LIB="$CHROME_VALGRIND/lib/valgrind"
+export VALGRIND_LIB_INNER="$CHROME_VALGRIND/lib/valgrind"
 
 PYTHONPATH=$THISDIR/../python/google "$THISDIR/chrome_tests.py" "$@"
