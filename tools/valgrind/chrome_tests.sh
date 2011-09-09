6a77a975e3221911c109a08a75c052babb119b86
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 08:11:35 +0000

    Make chrome_tests.sh detect Valgrind only for --tool=memcheck and --tool=tsan
    TBR=timurrrr
    Review URL: http://codereview.chromium.org/7847022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100368 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3d5b5f00cbb15bc929e4b5bbc3255c24bb6b6512

--- tools/valgrind/chrome_tests.sh
+++ tools/valgrind/chrome_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (c) 2009 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -8,18 +8,37 @@
 
 export THISDIR=`dirname $0`
 
-CHROME_VALGRIND=`sh $THISDIR/locate_valgrind.sh`
-if [ "$CHROME_VALGRIND" = "" ]
-then
-  # locate_valgrind.sh failed
-  exit 1
-fi
-echo "Using valgrind binaries from ${CHROME_VALGRIND}"
+NEEDS_VALGRIND=0
 
-PATH="${CHROME_VALGRIND}/bin:$PATH"
-# We need to set these variables to override default lib paths hard-coded into
-# Valgrind binary.
-export VALGRIND_LIB="$CHROME_VALGRIND/lib/valgrind"
-export VALGRIND_LIB_INNER="$CHROME_VALGRIND/lib/valgrind"
+# We need to set CHROME_VALGRIND iff using Memcheck or TSan-Valgrind.
+for flag in $@
+do
+  if [ "$flag" == "--tool=tsan" ]
+  then
+    NEEDS_VALGRIND=1
+    break
+  fi
+  if [ "$flag" == "--tool=memcheck" ]
+  then
+    NEEDS_VALGRIND=1
+  fi
+done
+
+if [ "$NEEDS_VALGRIND" == "1" ]
+then
+  CHROME_VALGRIND=`sh $THISDIR/locate_valgrind.sh`
+  if [ "$CHROME_VALGRIND" = "" ]
+  then
+    # locate_valgrind.sh failed
+    exit 1
+  fi
+  echo "Using valgrind binaries from ${CHROME_VALGRIND}"
+
+  PATH="${CHROME_VALGRIND}/bin:$PATH"
+  # We need to set these variables to override default lib paths hard-coded into
+  # Valgrind binary.
+  export VALGRIND_LIB="$CHROME_VALGRIND/lib/valgrind"
+  export VALGRIND_LIB_INNER="$CHROME_VALGRIND/lib/valgrind"
+fi
 
 PYTHONPATH=$THISDIR/../python/google "$THISDIR/chrome_tests.py" "$@"
