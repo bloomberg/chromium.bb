593ab206a665a577bebbca2f7fd32d9ea0026ccf
bxx@chromium.org <bxx@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-09 23:04:17 +0000

    tcmalloc doubly-linked free-lists for thread caches
    Added the ability for free lists to be built out of doubly-linked lists in tcalloc. TCMALLOC_USE_DOUBLYLINKED_FREELIST flag must be set in order for doubly-linked lists to be used. By default flag is only set in Debug builds.
    
    BUG=None
    TEST=None
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=99515
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=100074
    
    Review URL: http://codereview.chromium.org/7671034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100525 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8fdf9f5214e385b650c1a708a5c7e015b8fbde5e

--- third_party/tcmalloc/chromium/src/thread_cache.cc
+++ third_party/tcmalloc/chromium/src/thread_cache.cc
@@ -153,7 +153,10 @@ void* ThreadCache::FetchFromCentralCache(size_t cl, size_t byte_size) {
   ASSERT((start == NULL) == (fetch_count == 0));
   if (--fetch_count >= 0) {
     size_ += byte_size * fetch_count;
-    list->PushRange(fetch_count, SLL_Next(start), end);
+    // Pop the top of the list and add the rest to the freelist.
+    void *second = start;
+    start = FL_Pop(&second);
+    list->PushRange(fetch_count, second, end);
   }
 
   // Increase max length slowly up to batch_size.  After that,
