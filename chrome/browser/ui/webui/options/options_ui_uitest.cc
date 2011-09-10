a32fad9f51335c1b9f873dba0470f138dbde8ec3
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-10 01:38:39 +0000

    Cleanups for options_ui_uitest.cc. * Minor changes to the implementation of LoadOptionsByURL to make it more reliable. * Remove extra UI tests that added little benefit vs cost in time and effort. * Removed Valgrind supressions related to the above tests.
    BUG=48521,60036,64619,83209
    TEST=Green trybots and green valgrind runs.
    Review URL: http://codereview.chromium.org/7865011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100556 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a6ad58b709de2eb5b512e76bf91077f26fd8e7ac

--- chrome/browser/ui/webui/options/options_ui_uitest.cc
+++ chrome/browser/ui/webui/options/options_ui_uitest.cc
@@ -2,19 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/command_line.h"
 #include "base/string16.h"
-#include "base/test/test_timeouts.h"
 #include "base/utf_string_conversions.h"
-#include "chrome/app/chrome_command_ids.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/automation/automation_proxy.h"
 #include "chrome/test/automation/browser_proxy.h"
 #include "chrome/test/automation/tab_proxy.h"
-#include "chrome/test/automation/window_proxy.h"
 #include "chrome/test/ui/ui_test.h"
-#include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -24,34 +18,6 @@ class OptionsUITest : public UITest {
  public:
   OptionsUITest() {
     dom_automation_enabled_ = true;
-  }
-
-  bool WaitForOptionsUI(TabProxy* tab) {
-    return WaitUntilJavaScriptCondition(tab, L"",
-        L"domAutomationController.send("
-        L"    location.protocol == 'chrome:' && "
-        L"    document.readyState == 'complete')",
-        TestTimeouts::large_test_timeout_ms());
-  }
-
-  scoped_refptr<TabProxy> GetOptionsUITab() {
-    scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
-    EXPECT_TRUE(browser.get());
-    if (!browser.get())
-      return NULL;
-    scoped_refptr<TabProxy> tab = browser->GetActiveTab();
-    EXPECT_TRUE(tab.get());
-    if (!tab.get())
-      return NULL;
-    bool success = tab->NavigateToURL(GURL(chrome::kChromeUISettingsURL));
-    EXPECT_TRUE(success);
-    if (!success)
-      return NULL;
-    success = WaitForOptionsUI(tab);
-    EXPECT_TRUE(success);
-    if (!success)
-      return NULL;
-    return tab;
   }
 
   void AssertIsOptionsPage(TabProxy* tab) {
@@ -65,80 +31,13 @@ class OptionsUITest : public UITest {
 };
 
 TEST_F(OptionsUITest, LoadOptionsByURL) {
-  scoped_refptr<TabProxy> tab = GetOptionsUITab();
-  ASSERT_TRUE(tab.get());
-  AssertIsOptionsPage(tab);
-}
-
-// Flaky, and takes very long to fail. http://crbug.com/64619.
-TEST_F(OptionsUITest, DISABLED_CommandOpensOptionsTab) {
   scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
   ASSERT_TRUE(browser.get());
-
-  int tab_count = -1;
-  ASSERT_TRUE(browser->GetTabCount(&tab_count));
-  ASSERT_EQ(1, tab_count);
-
-  // Bring up the options tab via command.
-  ASSERT_TRUE(browser->RunCommand(IDC_OPTIONS));
-  ASSERT_TRUE(browser->GetTabCount(&tab_count));
-  ASSERT_EQ(2, tab_count);
 
   scoped_refptr<TabProxy> tab = browser->GetActiveTab();
   ASSERT_TRUE(tab.get());
-  AssertIsOptionsPage(tab);
-}
 
-// Flaky, and takes very long to fail. http://crbug.com/48521
-TEST_F(OptionsUITest, DISABLED_CommandAgainGoesBackToOptionsTab) {
-  scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
-  ASSERT_TRUE(browser.get());
-
-  int tab_count = -1;
-  ASSERT_TRUE(browser->GetTabCount(&tab_count));
-  ASSERT_EQ(1, tab_count);
-
-  // Bring up the options tab via command.
-  ASSERT_TRUE(browser->RunCommand(IDC_OPTIONS));
-  ASSERT_TRUE(browser->GetTabCount(&tab_count));
-  ASSERT_EQ(2, tab_count);
-
-  scoped_refptr<TabProxy> tab = browser->GetActiveTab();
-  ASSERT_TRUE(tab.get());
-  AssertIsOptionsPage(tab);
-
-  // Switch to first tab and run command again.
-  ASSERT_TRUE(browser->ActivateTab(0));
-  ASSERT_TRUE(browser->WaitForTabToBecomeActive(
-      0, TestTimeouts::action_max_timeout_ms()));
-  ASSERT_TRUE(browser->RunCommand(IDC_OPTIONS));
-
-  // Ensure the options ui tab is active.
-  ASSERT_TRUE(browser->WaitForTabToBecomeActive(
-      1, TestTimeouts::action_max_timeout_ms()));
-  ASSERT_TRUE(browser->GetTabCount(&tab_count));
-  ASSERT_EQ(2, tab_count);
-}
-
-// Flaky, and takes very long to fail. http://crbug.com/48521
-TEST_F(OptionsUITest, DISABLED_TwoCommandsOneTab) {
-  scoped_refptr<BrowserProxy> browser(automation()->GetBrowserWindow(0));
-  ASSERT_TRUE(browser.get());
-
-  int tab_count = -1;
-  ASSERT_TRUE(browser->GetTabCount(&tab_count));
-  ASSERT_EQ(1, tab_count);
-
-  ASSERT_TRUE(browser->RunCommand(IDC_OPTIONS));
-  ASSERT_TRUE(browser->RunCommand(IDC_OPTIONS));
-  ASSERT_TRUE(browser->GetTabCount(&tab_count));
-  ASSERT_EQ(2, tab_count);
-}
-
-// Navigates to settings page and do sanity check on settings sections.
-TEST_F(OptionsUITest, NavBarCheck) {
-  scoped_refptr<TabProxy> tab = GetOptionsUITab();
-  ASSERT_TRUE(tab.get());
+  NavigateToURL(GURL(chrome::kChromeUISettingsURL));
   AssertIsOptionsPage(tab);
 
   // Check navbar's existence.
