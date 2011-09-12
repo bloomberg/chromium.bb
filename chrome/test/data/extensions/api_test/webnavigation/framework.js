f405beaf7da4d45b1e8a1c1d470231a8c8d9c56d
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-12 12:13:39 +0000

    Refactor ExtensionApiTest.WebNavigationEvents* to not rely on window.setTimeout
    BUG=70382,94932
    TEST=tests still work
    
    
    Review URL: http://codereview.chromium.org/7859014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100675 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 89b1769644f8a140ef663db69156a651422f50db

--- chrome/test/data/extensions/api_test/webnavigation/framework.js
+++ chrome/test/data/extensions/api_test/webnavigation/framework.js
@@ -2,7 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+var deepEq = chrome.test.checkDeepEq;
 var expectedEventData;
+var expectedEventOrder;
 var capturedEventData;
 var nextFrameId;
 var frameIds;
@@ -10,9 +12,18 @@ var nextTabId;
 var tabIds;
 var initialized = false;
 
-function expect(data) {
+// data: array of expected events, each one is a dictionary:
+//     { label: "<unique identifier>",
+//       event: "<webnavigation event type>",
+//       details: { <expected details of the event> }
+//     }
+// order: an array of sequences, e.g. [ ["a", "b", "c"], ["d", "e"] ] means that
+//     event with label "a" needs to occur before event with label "b". The
+//     relative order of "a" and "d" does not matter.
+function expect(data, order) {
   expectedEventData = data;
   capturedEventData = [];
+  expectedEventOrder = order;
   nextFrameId = 1;
   frameIds = {};
   nextTabId = 0;
@@ -24,8 +35,35 @@ function checkExpectations() {
   if (capturedEventData.length < expectedEventData.length) {
     return;
   }
-  chrome.test.assertEq(JSON.stringify(expectedEventData),
-      JSON.stringify(capturedEventData));
+  if (capturedEventData.length > expectedEventData.length) {
+    chrome.test.fail("Recorded too many events. " +
+        JSON.stringify(capturedEventData));
+  }
+  // We have ensured that capturedEventData contains exactly the same elements
+  // as expectedEventData. Now we need to verify the ordering.
+  // Step 1: build positions such that
+  //     position[<event-label>]=<position of this event in capturedEventData>
+  var curPos = 0;
+  var positions = {};
+  capturedEventData.forEach(function (event) {
+    chrome.test.assertTrue(event.hasOwnProperty("label"));
+    positions[event.label] = curPos;
+    curPos++;
+  });
+  // Step 2: check that elements arrived in correct order
+  expectedEventOrder.forEach(function (order) {
+    var previousLabel = undefined;
+    order.forEach(function (label) {
+      if (previousLabel === undefined) {
+        previousLabel = label;
+        return;
+      }
+      chrome.test.assertTrue(positions[previousLabel] < positions[label],
+          "Event " + previousLabel + " is supposed to arrive before " +
+          label + ".");
+      previousLabel = label;
+    });
+  });
   chrome.test.succeed();
 }
 
@@ -62,7 +100,24 @@ function captureEvent(name, details) {
     }
     details.sourceTabId = tabIds[details.sourceTabId];
   }
-  capturedEventData.push([name, details]);
+
+  // find |details| in expectedEventData
+  var found = false;
+  var label = undefined;
+  expectedEventData.forEach(function (exp) {
+    if (deepEq(exp.event, name) && deepEq(exp.details, details)) {
+      if (!found) {
+        found = true;
+        label = exp.label;
+        exp.event = undefined;
+      }
+    }
+  });
+  if (!found) {
+    chrome.test.fail("Received unexpected event '" + name + "':" +
+        JSON.stringify(details));
+  }
+  capturedEventData.push({label: label, event: name, details: details});
   checkExpectations();
 }
 
@@ -98,4 +153,27 @@ function initListeners() {
       function(details) {
     captureEvent("onErrorOccurred", details);
   });
+}
+
+// Returns the usual order of navigation events.
+function navigationOrder(prefix) {
+  return [ prefix + "onBeforeNavigate",
+           prefix + "onCommitted",
+           prefix + "onDOMContentLoaded",
+           prefix + "onCompleted" ];
+}
+
+// Returns the constraints expressing that a frame is an iframe of another
+// frame.
+function isIFrameOf(iframe, main_frame) {
+  return [ main_frame + "onCommitted",
+           iframe + "onBeforeNavigate",
+           main_frame + "onDOMContentLoaded",
+           iframe + "onCompleted",
+           main_frame + "onCompleted" ];
+}
+
+// Returns the constraint expressing that a frame was loaded by another.
+function isLoadedBy(target, source) {
+  return [ source + "onDOMContentLoaded", target + "onBeforeNavigate"];
 }
--- chrome/test/data/extensions/api_test/webnavigation/test_serverRedirect.js
+++ chrome/test/data/extensions/api_test/webnavigation/test_serverRedirect.js
@@ -4,7 +4,8 @@
 
 function runTests() {
   var getURL = chrome.extension.getURL;
-  var URL_LOAD = "http://www.a.com:PORT/files/extensions/api_test/webnavigation/serverRedirect/a.html";
+  var URL_LOAD =
+      "http://www.a.com:PORT/files/extensions/api_test/webnavigation/serverRedirect/a.html";
   var URL_LOAD_REDIRECT = "http://www.a.com:PORT/server-redirect?" + URL_LOAD;
   chrome.tabs.create({"url": "about:blank"}, function(tab) {
     var tabId = tab.id;
@@ -18,28 +19,33 @@ function runTests() {
         // Navigates to a page that redirects (on the server side) to a.html.
         function serverRedirect() {
           expect([
-            [ "onBeforeNavigate",
-              { frameId: 0,
-                tabId: 0,
-                timeStamp: 0,
-                url: URL_LOAD_REDIRECT }],
-            [ "onCommitted",
-              { frameId: 0,
-                tabId: 0,
-                timeStamp: 0,
-                transitionQualifiers: [],
-                transitionType: "link",
-                url: URL_LOAD }],
-            [ "onDOMContentLoaded",
-              { frameId: 0,
-                tabId: 0,
-                timeStamp: 0,
-                url: URL_LOAD }],
-            [ "onCompleted",
-              { frameId: 0,
-                tabId: 0,
-                timeStamp: 0,
-                url: URL_LOAD }]]);
+            { label: "a-onBeforeNavigate",
+              event: "onBeforeNavigate",
+              details: { frameId: 0,
+                         tabId: 0,
+                         timeStamp: 0,
+                         url: URL_LOAD_REDIRECT }},
+            { label: "a-onCommitted",
+              event: "onCommitted",
+              details: { frameId: 0,
+                         tabId: 0,
+                         timeStamp: 0,
+                         transitionQualifiers: [],
+                         transitionType: "link",
+                         url: URL_LOAD }},
+            { label: "a-onDOMContentLoaded",
+              event: "onDOMContentLoaded",
+              details: { frameId: 0,
+                         tabId: 0,
+                         timeStamp: 0,
+                         url: URL_LOAD }},
+            { label: "a-onCompleted",
+              event: "onCompleted",
+              details: { frameId: 0,
+                         tabId: 0,
+                         timeStamp: 0,
+                         url: URL_LOAD }}],
+            [ navigationOrder("a-") ]);
           chrome.tabs.update(tabId, { url: URL_LOAD_REDIRECT });
         },
       ]);
