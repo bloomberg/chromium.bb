63e8fc8af88c8fd8fb25cde53ad716552ce138e4
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-12 17:29:19 +0000

    Revert 100708 - Unit test for adding global errors to the wrench menu
    BUG=95146
    TEST=
    
    
    Review URL: http://codereview.chromium.org/7865017
    
    TBR=sail@chromium.org
    Review URL: http://codereview.chromium.org/7873003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100713 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: eb93c336d5e616b9c5ab0ded7bf4b63fe005ebbe

--- chrome/browser/ui/toolbar/wrench_menu_model_unittest.cc
+++ chrome/browser/ui/toolbar/wrench_menu_model_unittest.cc
@@ -6,72 +6,11 @@
 
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/global_error.h"
-#include "chrome/browser/ui/global_error_service.h"
-#include "chrome/browser/ui/global_error_service_factory.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/menu_model_test.h"
 #include "chrome/test/base/testing_profile.h"
 #include "grit/generated_resources.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-namespace {
-
-// Error class has a menu item.
-class MenuError : public GlobalError {
- public:
-  explicit MenuError(int command_id)
-      : command_id_(command_id),
-        execute_count_(0) {
-  }
-
-  int execute_count() { return execute_count_; }
-
-  bool HasBadge() OVERRIDE { return false; }
-  virtual int GetBadgeResourceID() OVERRIDE {
-    ADD_FAILURE();
-    return 0;
-  }
-
-  virtual bool HasMenuItem() OVERRIDE { return true; }
-  virtual int MenuItemCommandID() OVERRIDE { return command_id_; }
-  virtual string16 MenuItemLabel() OVERRIDE { return string16(); }
-  virtual void ExecuteMenuItem(Browser* browser) OVERRIDE { execute_count_++; }
-
-  virtual bool HasBubbleView() OVERRIDE { return false; }
-  virtual void ShowBubbleView(Browser* browser) OVERRIDE { ADD_FAILURE(); }
-  virtual int GetBubbleViewIconResourceID() OVERRIDE {
-    ADD_FAILURE();
-    return 0;
-  }
-  virtual string16 GetBubbleViewTitle() OVERRIDE {
-    ADD_FAILURE();
-    return string16();
-  }
-  virtual string16 GetBubbleViewMessage() OVERRIDE {
-    ADD_FAILURE();
-    return string16();
-  }
-  virtual string16 GetBubbleViewAcceptButtonLabel() OVERRIDE {
-    ADD_FAILURE();
-    return string16();
-  }
-  virtual string16 GetBubbleViewCancelButtonLabel() OVERRIDE {
-    ADD_FAILURE();
-    return string16();
-  }
-  virtual void BubbleViewDidClose() OVERRIDE { ADD_FAILURE(); }
-  virtual void BubbleViewAcceptButtonPressed() OVERRIDE { ADD_FAILURE(); }
-  virtual void BubbleViewCancelButtonPressed() OVERRIDE { ADD_FAILURE(); }
-
- private:
-  int command_id_;
-  int execute_count_;
-
-  DISALLOW_COPY_AND_ASSIGN(MenuError);
-};
-
-} // namespace
 
 class WrenchMenuModelTest : public BrowserWithTestWindowTest,
                             public ui::AcceleratorProvider {
@@ -154,36 +93,6 @@ TEST_F(WrenchMenuModelTest, Basics) {
   EXPECT_TRUE(bookmarksModel->IsEnabledAt(1));
   EXPECT_EQ(model.execute_count_, 1);
   EXPECT_EQ(model.enable_count_, 1);
-}
-
-// Tests global error menu items in the wrench menu.
-TEST_F(WrenchMenuModelTest, GlobalError) {
-  GlobalErrorService* service =
-      GlobalErrorServiceFactory::GetForProfile(browser()->profile());
-  const int command1 = 1234567;
-  // AddGlobalError takes ownership of error1.
-  MenuError* error1 = new MenuError(command1);
-  service->AddGlobalError(error1);
-  const int command2 = 1234568;
-  // AddGlobalError takes ownership of error2.
-  MenuError* error2 = new MenuError(command2);
-  service->AddGlobalError(error2);
-
-  WrenchMenuModel model(this, browser());
-  int index1 = model.GetIndexOfCommandId(command1);
-  EXPECT_GT(index1, -1);
-  int index2 = model.GetIndexOfCommandId(command2);
-  EXPECT_GT(index2, -1);
-
-  EXPECT_TRUE(model.IsEnabledAt(index1));
-  EXPECT_EQ(0, error1->execute_count());
-  model.ActivatedAt(index1);
-  EXPECT_EQ(1, error1->execute_count());
-
-  EXPECT_TRUE(model.IsEnabledAt(index2));
-  EXPECT_EQ(0, error2->execute_count());
-  model.ActivatedAt(index2);
-  EXPECT_EQ(1, error1->execute_count());
 }
 
 class EncodingMenuModelTest : public BrowserWithTestWindowTest,
