bae2ebaa731d16476a171533db25d6f50afd575a
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-13 14:28:21 +0000

    touchui: Activate the menu window after displaying.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/7870013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100913 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d1d08c1037c4151af2ee28d1b743b501f59c6eed

--- views/controls/menu/menu_host.cc
+++ views/controls/menu/menu_host.cc
@@ -48,6 +48,9 @@ void MenuHost::ShowMenuHost(bool do_capture) {
   // process of showing.
   ignore_capture_lost_ = true;
   Show();
+#if defined(TOUCH_UI)
+  Activate();
+#endif
   if (do_capture)
     native_widget_private()->SetMouseCapture();
   ignore_capture_lost_ = false;
