d0ae31df837a58ccb97c2e1d0681c765fac0f793
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-13 02:57:12 +0000

    ntp4: make footer look snappier
    don't show the navigation dots during startup (otherwise the most visited and bookmarks dots show up visibly faster than apps)
    
    also fix even more fallout of the .hidden css rule change (Recently Closed was getting display: none when we just wanted visibility: hidden)
    
    BUG=92989
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/7862012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@100847 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d70bbed71daef079f54094a7cb99c27aa176503b

--- chrome/browser/resources/ntp4/recently_closed.js
+++ chrome/browser/resources/ntp4/recently_closed.js
@@ -38,7 +38,7 @@ cr.define('ntp4', function() {
       document.body.appendChild(this.menu);
 
       this.needsRebuild_ = true;
-      this.hidden = true;
+      this.classList.add('invisible');
       this.anchorType = cr.ui.AnchorType.ABOVE;
       this.invertLeftRight = true;
     },
@@ -66,7 +66,10 @@ cr.define('ntp4', function() {
     set dataItems(dataItems) {
       this.dataItems_ = dataItems;
       this.needsRebuild_ = true;
-      this.hidden = dataItems.length == 0;
+      if (dataItems.length)
+        this.classList.remove('invisible');
+      else
+        this.classList.add('invisible');
     },
 
     /**
