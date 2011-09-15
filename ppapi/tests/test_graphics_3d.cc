91afe6fa29972bad2e664bec1037d4c544f7fd4e
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 17:29:20 +0000

    Included ppb_opengles2.h instead of ppb_opengles.h in test_graphics_3d.cc. TBR=brettw@chromium.org Review URL: http://codereview.chromium.org/7909008
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101321 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 016807781fc6476894cf599dd37afaa730784157

--- ppapi/tests/test_graphics_3d.cc
+++ ppapi/tests/test_graphics_3d.cc
@@ -7,7 +7,7 @@
 #include <GLES2/gl2.h>
 
 #include "ppapi/c/dev/ppb_testing_dev.h"
-#include "ppapi/c/ppb_opengles.h"
+#include "ppapi/c/ppb_opengles2.h"
 #include "ppapi/cpp/graphics_3d.h"
 #include "ppapi/cpp/module.h"
 #include "ppapi/tests/test_utils.h"
