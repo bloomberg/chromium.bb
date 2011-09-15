1d228329d74dc5d9db8d0dd946b7e7f953df5406
tturchetto@google.com <tturchetto@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-15 21:57:26 +0000

    Test Incognito window with tabs
    BUG=
    TEST=This is a test.
    
    Review URL: http://codereview.chromium.org/7887026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101389 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 560349690af4547709233e8f6b72efe450bc4a19

--- chrome/test/functional/chromeos_browser.py
+++ chrome/test/functional/chromeos_browser.py
@@ -23,6 +23,22 @@ class ChromeosBrowserTest(pyauto.PyUITest):
     self.assertEqual('chrome://newtab/', url,
                      msg='Unexpected URL: %s' % url)
 
+  def testCannotCloseLastIncognito(self):
+    """Verify that last incognito window cannot be closed if it's the
+    last window"""
+    self.RunCommand(pyauto.IDC_NEW_INCOGNITO_WINDOW)
+    self.assertTrue(self.GetBrowserInfo()['windows'][1]['incognito'],
+                    msg='Incognito window is not displayed')
+
+    self.CloseBrowserWindow(0)
+    info = self.GetBrowserInfo()['windows']
+    self.assertEqual(1, len(info))
+    url = info[0]['tabs'][0]['url']
+    self.assertEqual('chrome://newtab/', url,
+                     msg='Unexpected URL: %s' % url)
+    self.assertTrue(info[0]['incognito'],
+                    msg='Incognito window is not displayed.')
+
   def testFullScreen(self):
     """Verify that a browser window can enter and exit full screen mode."""
     self.ApplyAccelerator(pyauto.IDC_FULLSCREEN)
