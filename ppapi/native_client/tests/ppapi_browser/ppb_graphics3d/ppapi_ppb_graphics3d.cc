637eb2a53e241f175caa67bfa9cfa4ca3fee3312
nfullagar@google.com <nfullagar@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-16 20:10:10 +0000

    More testing for Graphics3D BUG= none TEST= ppapi_ppb_graphics3d
    Review URL: http://codereview.chromium.org/7888028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101555 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c96312d46205ea82764aba6255ecbb8dd5f57d11

--- ppapi/native_client/tests/ppapi_browser/ppb_graphics3d/ppapi_ppb_graphics3d.cc
+++ ppapi/native_client/tests/ppapi_browser/ppb_graphics3d/ppapi_ppb_graphics3d.cc
@@ -26,7 +26,7 @@
 #include "ppapi/c/ppb_graphics_3d.h"
 #include "ppapi/c/ppb_image_data.h"
 #include "ppapi/c/ppb_instance.h"
-#include "ppapi/c/ppb_opengles.h"
+#include "ppapi/c/ppb_opengles2.h"
 #include "ppapi/c/ppb_url_loader.h"
 #include "ppapi/lib/gl/gles2/gl2ext_ppapi.h"
 
@@ -90,10 +90,42 @@ void TestIsGraphics3D() {
   TEST_PASSED;
 }
 
-// Tests glInitializePPAPI.
+// Tests glInitializePPAPI().
 void Test_glInitializePPAPI() {
   GLboolean init_ppapi = glInitializePPAPI(ppb_get_interface());
   EXPECT(init_ppapi == true);
+  TEST_PASSED;
+}
+
+// Test basic setup:
+//   glSetCurrentContextPPAPI(), glGetIntegerv(), glGetString() and glGetError()
+void TestBasicSetup() {
+  int32_t attribs[] = {
+      PP_GRAPHICS3DATTRIB_WIDTH, kWidth,
+      PP_GRAPHICS3DATTRIB_HEIGHT, kHeight,
+      PP_GRAPHICS3DATTRIB_DEPTH_SIZE, 32,
+      PP_GRAPHICS3DATTRIB_NONE};
+  PP_Resource graphics3d_id = PPBGraphics3D()->
+      Create(pp_instance(), kInvalidResource, attribs);
+  EXPECT(graphics3d_id != kInvalidResource);
+  glSetCurrentContextPPAPI(graphics3d_id);
+  EXPECT(glGetString(GL_VERSION) != NULL);
+  glGetString(GL_EXTENSIONS);
+  int gl_max_texture_size;
+  const int expected_lower_bound_max_texture_size = 256;
+  glGetIntegerv(GL_MAX_TEXTURE_SIZE, &gl_max_texture_size);
+  EXPECT(gl_max_texture_size >= expected_lower_bound_max_texture_size);
+  int gl_depth_size;
+  const int expected_lower_bound_depth_size = 16;
+  glGetIntegerv(GL_DEPTH_BITS, &gl_depth_size);
+  EXPECT(gl_depth_size >= expected_lower_bound_depth_size);
+  GLenum lastError = glGetError();
+  EXPECT(lastError == GL_NO_ERROR);
+  glEnable(GL_LINE_LOOP);
+  lastError = glGetError();
+  EXPECT(lastError == GL_INVALID_ENUM);
+  glSetCurrentContextPPAPI(0);
+  PPBCore()->ReleaseResource(graphics3d_id);
   TEST_PASSED;
 }
 
@@ -151,21 +183,20 @@ void TestSwapBuffers() {
 }
 
 void TestResizeAndSwapCallback(void* user_data, int32_t result) {
+  const int kMinSize = 1;
   EXPECT(result == PP_OK);
   RenderInfo* info = static_cast<RenderInfo *>(user_data);
   int32_t new_width = kWidth - info->frame_counter * 2;
   int32_t new_height = kHeight - info->frame_counter * 2;
-  if (new_width < 0) new_width = 0;
-  if (new_height < 0) new_height = 0;
+  if (new_width < kMinSize) new_width = kMinSize;
+  if (new_height < kMinSize) new_height = kMinSize;
+  // Normally ResizeBuffers() would be in response to DidChangeView(...),
+  // This test calls it to see if any unexpected crashes or return values occur.
   int32_t resize_result = PPBGraphics3D()->ResizeBuffers(info->graphics3d_id,
       new_width, new_height);
   EXPECT(resize_result == PP_OK);
-  // Set graphics3d_id to the main context, so we can use normal gl style calls
-  // instead of going through the PPAPI PPBOpenGLES2 interface.
   glSetCurrentContextPPAPI(info->graphics3d_id);
-  // Note: still use original width & height in glViewport; visual inspection
-  // should show a clipped window that shrinks to new_width, new_height size.
-  glViewport(0, 0, new_width, new_height);
+  glViewport(0, 0, kWidth, kHeight);
   float green = float(info->frame_counter) / float(info->frame_end);
   glClearColor(0.0f, green, 0.0f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
@@ -184,6 +215,7 @@ void TestResizeAndSwapCallback(void* user_data, int32_t result) {
 }
 
 // Tests PPB_Graphics3D::ResizeBuffers().
+// The resize tests will render as a flashing green filled rectangle.
 void TestResizeBuffers(int32_t* attribs) {
   PP_Resource graphics3d_id = PPBGraphics3D()->
       Create(pp_instance(), kInvalidResource, attribs);
@@ -208,7 +240,6 @@ void TestResizeBuffers(int32_t* attribs) {
   PPBCore()->CallOnMainThread(0, cc, PP_OK);
 }
 
-
 // Tests PPB_Graphics3D::ResizeBuffers() w/o depth buffer attached.
 void TestResizeBuffersWithoutDepthBuffer() {
   int32_t attribs[] = {
@@ -218,17 +249,15 @@ void TestResizeBuffersWithoutDepthBuffer() {
   TestResizeBuffers(attribs);
 }
 
-
 // Tests PPB_Graphics3D::ResizeBuffers() w/ depth buffer attached.
 void TestResizeBuffersWithDepthBuffer() {
   int32_t attribs[] = {
       PP_GRAPHICS3DATTRIB_WIDTH, kWidth,
       PP_GRAPHICS3DATTRIB_HEIGHT, kHeight,
-      PP_GRAPHICS3DATTRIB_DEPTH_SIZE, 32,
+      PP_GRAPHICS3DATTRIB_DEPTH_SIZE, 16,
       PP_GRAPHICS3DATTRIB_NONE};
   TestResizeBuffers(attribs);
 }
-
 
 // Tests glTerminatePPAPI.
 void Test_glTerminatePPAPI() {
@@ -245,6 +274,7 @@ void SetupTests() {
   RegisterTest("TestCreate", TestCreate);
   RegisterTest("TestIsGraphics3D", TestIsGraphics3D);
   RegisterTest("Test_glInitializePPAPI", Test_glInitializePPAPI);
+  RegisterTest("TestBasicSetup", TestBasicSetup);
   RegisterTest("TestSwapBuffers", TestSwapBuffers);
   RegisterTest("TestResizeBuffersWithoutDepthBuffer",
       TestResizeBuffersWithoutDepthBuffer);
