d2b1642d0150e725a34081f5da50ead01e55a27c
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-16 23:29:36 +0000

    Second attempt at fixing deadlock. My earlier attempt failed because I added the call to BookmarkModel::Cleanup after the call to HistoryService::Cleanup. BookmarkModel::Cleanup needs to be first.
    BUG=96100
    TEST=none
    R=brettw@chromium.org
    
    Review URL: http://codereview.chromium.org/7918021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101599 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a076cfa2217b425cad700b1b6c847bfc6bbb7ffb

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -801,6 +801,18 @@ ProfileImpl::~ProfileImpl() {
   if (top_sites_.get())
     top_sites_->Shutdown();
 
+  if (bookmark_bar_model_.get()) {
+    // It's possible that bookmarks haven't loaded and history is waiting for
+    // bookmarks to complete loading. In such a situation history can't shutdown
+    // (meaning if we invoked history_service_->Cleanup now, we would
+    // deadlock). To break the deadlock we tell BookmarkModel it's about to be
+    // deleted so that it can release the signal history is waiting on, allowing
+    // history to shutdown (history_service_->Cleanup to complete). In such a
+    // scenario history sees an incorrect view of bookmarks, but it's better
+    // than a deadlock.
+    bookmark_bar_model_->Cleanup();
+  }
+
   if (history_service_.get())
     history_service_->Cleanup();
 
@@ -809,15 +821,6 @@ ProfileImpl::~ProfileImpl() {
     default_request_context_ = NULL;
   }
 
-  if (bookmark_bar_model_.get()) {
-    // It's possible that bookmarks haven't loaded and history is waiting for
-    // bookmarks to complete loading. In such a situation history can't
-    // shutdown. To break the deadlock we tell BookmarkModel it's about to be
-    // deleted so that it can release the signal history is waiting on, allowing
-    // history to shutdown. In such a scenario history sees an incorrect view of
-    // bookmarks, but it's better than a deadlock.
-    bookmark_bar_model_->Cleanup();
-  }
   // HistoryService may call into the BookmarkModel, as such we need to
   // delete HistoryService before the BookmarkModel. The destructor for
   // HistoryService will join with HistoryService's backend thread so that
