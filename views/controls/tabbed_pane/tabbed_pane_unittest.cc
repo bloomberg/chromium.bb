40f67cb91c64212ab065feda73a58dc677e3fe46
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-20 18:12:59 +0000

    views: Add unittests for the TabbedPane operations.
    R=sky@chromium.org
    TEST=views_unittests --gtest_filter=TabbedPaneTest.*
    
    Review URL: http://codereview.chromium.org/7934010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@101984 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a4c62c538416d8cf5401c857b44c0a6f8cb3bf9c

--- views/controls/tabbed_pane/tabbed_pane_unittest.cc
+++ views/controls/tabbed_pane/tabbed_pane_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -90,6 +90,48 @@ TEST_F(TabbedPaneTest, SizeAndLayout) {
   // If we switch to the other tab, it should get assigned the same bounds.
   tabbed_pane_->SelectTabAt(1);
   EXPECT_EQ(bounds, child2->bounds());
+}
+
+TEST_F(TabbedPaneTest, AddRemove) {
+  View* tab0 = new View;
+  tabbed_pane_->AddTab(L"tab0", tab0);
+  EXPECT_EQ(tab0, tabbed_pane_->GetSelectedTab());
+  EXPECT_EQ(0, tabbed_pane_->GetSelectedTabIndex());
+
+  // Add more 3 tabs.
+  tabbed_pane_->AddTab(L"tab1", new View);
+  tabbed_pane_->AddTab(L"tab2", new View);
+  tabbed_pane_->AddTab(L"tab3", new View);
+  EXPECT_EQ(4, tabbed_pane_->GetTabCount());
+
+  // Note: AddTab() doesn't select a tab if the tabbed pane isn't empty.
+
+  // Select the last one.
+  tabbed_pane_->SelectTabAt(tabbed_pane_->GetTabCount() - 1);
+  EXPECT_EQ(3, tabbed_pane_->GetSelectedTabIndex());
+
+  // Remove the last one.
+  delete tabbed_pane_->RemoveTabAtIndex(3);
+  EXPECT_EQ(3, tabbed_pane_->GetTabCount());
+
+  // After removing the last tab, check if the tabbed pane selected the previous
+  // tab.
+  EXPECT_EQ(2, tabbed_pane_->GetSelectedTabIndex());
+
+  tabbed_pane_->AddTabAtIndex(0, L"tab4", new View, true);
+
+  // Assert that even adding a new tab, the tabbed pane doesn't change the
+  // selection, i.e., it doesn't select the new one.
+  // The last tab should remains selected, instead of the tab added at index 0.
+  EXPECT_EQ(3, tabbed_pane_->GetSelectedTabIndex());
+
+  // Now change the selected tab.
+  tabbed_pane_->SelectTabAt(1);
+  EXPECT_EQ(1, tabbed_pane_->GetSelectedTabIndex());
+
+  // Remove the first one.
+  delete tabbed_pane_->RemoveTabAtIndex(0);
+  EXPECT_EQ(0, tabbed_pane_->GetSelectedTabIndex());
 }
 
 } // namespace views
