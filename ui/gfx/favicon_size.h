77e9ea6f663c555ec56fd4f9dd91838a03e8bf2f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-26 18:41:55 +0000

    ui/gfx: Fix Linux shared build by adding missing UI_EXPORT annotations.
    TBR=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/8028029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102765 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e2170f8cc32db962f0c67dc7862cca7c59e828d3

--- ui/gfx/favicon_size.h
+++ ui/gfx/favicon_size.h
@@ -6,15 +6,17 @@
 #define UI_GFX_FAVICON_SIZE_H_
 #pragma once
 
+#include "ui/base/ui_export.h"
+
 namespace gfx {
 
 // Size (along each axis) of the favicon.
-extern const int kFaviconSize;
+UI_EXPORT extern const int kFaviconSize;
 
 // If the width or height is bigger than the favicon size, a new width/height
 // is calculated and returned in width/height that maintains the aspect
 // ratio of the supplied values.
-void CalculateFaviconTargetSize(int* width, int* height);
+UI_EXPORT void CalculateFaviconTargetSize(int* width, int* height);
 
 }  // namespace gfx
 
