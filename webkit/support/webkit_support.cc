bloomberg/chromium.bb

commit b6eafe037cc13226dd8981061ae6a43010d671c8

Re-land r102336 (remove webkit_glue::BuildUserAgent) w/ fix.
Remove webkit_glue::BuildUserAgent(), change the contract in webkit_glue so
that SetUserAgent() must be called before GetUserAgent().

This was causing a dependency inversion between webkit_support and its clients,
and was needed for the content component build.

For content users, calling SetContentClient() will automatically initialize the
user agent (retrieved from client->GetUserAgent()).

As a bonus, fixing this allowed me to re-test the "mimic_windows" code path and
it looks like we no longer need it.

R=jam@chromium.org,rsesek@chromium.org
BUG=11136, 90442
TEST=visit yahoo! mail using Chromium on Linux, ensure that we don't get an "unsupported browser" warning.

Review URL: http://codereview.chromium.org/8045005

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102763 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 79bd6e8691a7ff0b1d2b66a54cd09c9b874da8f9

webkit/support/webkit_support.cc
@@ -264,6 +264,7 @@ static void SetUpTestEnvironmentImpl(bool unit_test_mode) {
     // because on Linux, we need base::AtExitManager.
     icu_util::Initialize();
   }
+  webkit_glue::SetUserAgent("DumpRenderTree", false);
 }
 
 void SetUpTestEnvironment() {

webkit/tools/test_shell/test_shell.cc
@@ -146,6 +146,7 @@ TestShell::TestShell()
     filter->AddHostnameHandler("test-shell-resource", "inspector",
                                &URLRequestTestShellFileJob::InspectorFactory);
     url_util::AddStandardScheme("test-shell-resource");
+    webkit_glue::SetUserAgent("TestShell", false);
 }
 
 TestShell::~TestShell() {
@@ -633,10 +634,6 @@ bool IsProtocolSupportedForMedia(const GURL& url) {
       url.SchemeIs("data"))
     return true;
   return false;
-}
-
-std::string BuildUserAgent(bool mimic_windows) {
-  return webkit_glue::BuildUserAgentHelper(mimic_windows, "Chrome/0.0.0.0");
 }
 
 #if defined(OS_LINUX)

chrome/browser/ui/browser_init.cc
@@ -78,7 +78,6 @@
 #include "net/base/net_util.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
-#include "webkit/glue/webkit_glue.h"
 
 #if defined(OS_MACOSX)
 #include "base/mac/mac_util.h"
@@ -688,11 +687,6 @@ bool BrowserInit::LaunchWithProfile::Launch(
     } else {
       DLOG(WARNING) << "Invalid http debugger port number " << port;
     }
-  }
-
-  if (command_line_.HasSwitch(switches::kUserAgent)) {
-    webkit_glue::SetUserAgent(command_line_.GetSwitchValueASCII(
-        switches::kUserAgent));
   }
 
   // Open the required browser windows and tabs. First, see if

chrome/browser/ui/cocoa/nsimage_cache_unittest.mm
@@ -19,16 +19,6 @@ namespace {
 
 class NSImageCacheTest : public PlatformTest {
  public:
-  NSImageCacheTest() {
-    // Look in the framework bundle for resources.
-    FilePath path;
-    PathService::Get(base::DIR_EXE, &path);
-    path = path.Append(chrome::kFrameworkName);
-    base::mac::SetOverrideAppBundlePath(path);
-  }
-  virtual ~NSImageCacheTest() {
-    base::mac::SetOverrideAppBundle(nil);
-  }
 };
 
 TEST_F(NSImageCacheTest, LookupFound) {

chrome/common/chrome_content_client.cc
@@ -300,12 +300,18 @@ bool ChromeContentClient::CanHandleWhileSwappedOut(
   return false;
 }
 
-std::string ChromeContentClient::GetUserAgent(bool mimic_windows) const {
-  chrome::VersionInfo version_info;
-  std::string product("Chrome/");
-  product += version_info.is_valid() ? version_info.Version() : "0.0.0.0";
-
-  return webkit_glue::BuildUserAgentHelper(mimic_windows, product);
+std::string ChromeContentClient::GetUserAgent(bool* overriding) const {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kUserAgent)) {
+    *overriding = true;
+    return CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+        switches::kUserAgent);
+  } else {
+    *overriding = false;
+    chrome::VersionInfo version_info;
+    std::string product("Chrome/");
+    product += version_info.is_valid() ? version_info.Version() : "0.0.0.0";
+    return webkit_glue::BuildUserAgentFromProduct(product);
+  }
 }
 
 string16 ChromeContentClient::GetLocalizedString(int message_id) const {

chrome/common/chrome_content_client.h
@@ -6,6 +6,7 @@
 #define CHROME_COMMON_CHROME_CONTENT_CLIENT_H_
 #pragma once
 
+#include "base/compiler_specific.h"
 #include "content/common/content_client.h"
 
 namespace chrome {
@@ -16,18 +17,19 @@ class ChromeContentClient : public content::ContentClient {
   static const char* const kNaClPluginName;
   static const char* const kNaClOldPluginName;
 
-  virtual void SetActiveURL(const GURL& url);
-  virtual void SetGpuInfo(const GPUInfo& gpu_info);
-  virtual void AddPepperPlugins(std::vector<PepperPluginInfo>* plugins);
-  virtual bool CanSendWhileSwappedOut(const IPC::Message* msg);
-  virtual bool CanHandleWhileSwappedOut(const IPC::Message& msg);
-  virtual std::string GetUserAgent(bool mimic_windows) const;
-  virtual string16 GetLocalizedString(int message_id) const;
-  virtual base::StringPiece GetDataResource(int resource_id) const;
+  virtual void SetActiveURL(const GURL& url) OVERRIDE;
+  virtual void SetGpuInfo(const GPUInfo& gpu_info) OVERRIDE;
+  virtual void AddPepperPlugins(
+      std::vector<PepperPluginInfo>* plugins) OVERRIDE;
+  virtual bool CanSendWhileSwappedOut(const IPC::Message* msg) OVERRIDE;
+  virtual bool CanHandleWhileSwappedOut(const IPC::Message& msg) OVERRIDE;
+  virtual std::string GetUserAgent(bool* overriding) const OVERRIDE;
+  virtual string16 GetLocalizedString(int message_id) const OVERRIDE;
+  virtual base::StringPiece GetDataResource(int resource_id) const OVERRIDE;
 
 #if defined(OS_WIN)
   virtual bool SandboxPlugin(CommandLine* command_line,
-                             sandbox::TargetPolicy* policy);
+                             sandbox::TargetPolicy* policy) OVERRIDE;
 #endif
 };
 

chrome_frame/html_utils.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -385,7 +385,7 @@ const char* GetChromeUserAgent() {
       product += version_info.is_valid() ? version_info.Version()
                                          : "0.0.0.0";
 
-      ua = webkit_glue::BuildUserAgentHelper(false, product);
+      ua = webkit_glue::BuildUserAgentFromProduct(product);
 
       DCHECK(ua.length() < arraysize(g_chrome_user_agent));
       lstrcpynA(g_chrome_user_agent, ua.c_str(),

chrome_frame/renderer_glue.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,15 +12,3 @@ class GURL;
 bool IsPluginProcess() {
   return false;
 }
-
-namespace webkit_glue {
-
-std::string BuildUserAgent(bool mimic_windows) {
-  chrome::VersionInfo version_info;
-  std::string product("Chrome/");
-  product += version_info.is_valid() ? version_info.Version()
-                                     : "0.0.0.0";
-  return webkit_glue::BuildUserAgentHelper(mimic_windows, product);
-}
-
-}  // end namespace webkit_glue

commit 4fde8893f3c52d4617b8b0954870616efddc642e

fix DRT and TestShell to have a full user agent string again.
I regressed this in r102763. Should've run the layout try jobs :(.

R=mihaip@chromium.org
BUG=90442
TEST=layout bots pass
fast/dom/navigator-userAgent.html,http/tests/misc/redirect.php,http/tests/navigation/useragent.php

Review URL: http://codereview.chromium.org/8046017

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102798 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: fb0902a919bb22f7c2002611abeeae37e0ac94ac

webkit/support/webkit_support.cc
@@ -46,6 +46,7 @@
 #include "webkit/appcache/web_application_cache_host_impl.h"
 #include "webkit/glue/media/video_renderer_impl.h"
 #include "webkit/glue/webkit_constants.h"
+#include "webkit/glue/user_agent.h"
 #include "webkit/glue/webkit_glue.h"
 #include "webkit/glue/webkitplatformsupport_impl.h"
 #include "webkit/glue/webmediaplayer_impl.h"
@@ -264,7 +265,8 @@ static void SetUpTestEnvironmentImpl(bool unit_test_mode) {
     // because on Linux, we need base::AtExitManager.
     icu_util::Initialize();
   }
-  webkit_glue::SetUserAgent("DumpRenderTree", false);
+  webkit_glue::SetUserAgent(webkit_glue::BuildUserAgentFromProduct(
+      "DumpRenderTree/0.0.0.0"), false);
 }
 
 void SetUpTestEnvironment() {

webkit/tools/test_shell/test_shell.cc
@@ -146,7 +146,8 @@ TestShell::TestShell()
     filter->AddHostnameHandler("test-shell-resource", "inspector",
                                &URLRequestTestShellFileJob::InspectorFactory);
     url_util::AddStandardScheme("test-shell-resource");
-    webkit_glue::SetUserAgent("TestShell", false);
+    webkit_glue::SetUserAgent(webkit_glue::BuildUserAgentFromProduct(
+        "TestShell/0.0.0.0"), false);
 }
 
 TestShell::~TestShell() {