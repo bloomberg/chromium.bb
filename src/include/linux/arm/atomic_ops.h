5bc493bb34debb87d6aca737d0b4b53d9d906a74
mcgrathr@chromium.org <mcgrathr@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-09-26 22:01:19 +0000

    Make ARM atomic_ops compatible with -mthumb
    It's possible to write the code so that it assembles either as ARM or as
    Thumb correctly.
    
    BUG= none
    TEST= trybots + hand-built with a -mthumb compiler
    
    R=sehr@google.com,jasonwkim@google.com
    
    Review URL: http://codereview.chromium.org/8028036
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6787 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: d54c20671791de357131fe8efbce29f018973aa4

--- src/include/linux/arm/atomic_ops.h
+++ src/include/linux/arm/atomic_ops.h
@@ -1,34 +1,8 @@
 /*
- * Copyright 2009, Google Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *     * Redistributions of source code must retain the above copyright
- * notice, this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above
- * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or other materials provided with the
- * distribution.
- *     * Neither the name of Google Inc. nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
-
 
 #ifndef NATIVE_CLIENT_SRC_INCLUDE_LINUX_ARM_ATOMIC_OPS_LINUX_ARM_H_
 #define NATIVE_CLIENT_SRC_INCLUDE_LINUX_ARM_ATOMIC_OPS_LINUX_ARM_H_ 1
@@ -43,23 +17,29 @@ typedef int32_t Atomic32;
 static INLINE Atomic32 CompareAndSwap(volatile Atomic32* ptr,
                                       Atomic32 old_value,
                                       Atomic32 new_value) {
-  Atomic32 old, tmp;
-
-  __asm__ __volatile__(
-      "1:\n"
-      "ldrex %1, [%2]\n"
-      "cmp %1, %3\n"
-      "strexne %0, %1, [%2]\n"  /* Dummy strex to clear exclusive bit */
-      "bne 2f\n"
-      "strex %0, %4, [%2]\n"
-      "cmp %0, #0\n"
-      "bne 1b\n"
-      "2:\n"
-      : "=&r" (tmp), "=&r" (old)
-      : "r" (ptr), "Ir" (old_value),
-        "r" (new_value)
-      : "cc", "memory");
-  return old;
+  Atomic32 oldval, res;
+  do {
+    __asm__ __volatile__(
+        "ldrex   %1, [%3]\n"
+        "mov     %0, #0\n"
+        "teq     %1, %4\n"
+        /*
+         * The if-then block affects the following four instructions.
+         * But we only have one we want to predicate.  So we have to
+         * add three nops when compiling for Thumb.
+         */
+        "itt     eq\n"
+        "strexeq %0, %5, [%3]\n"
+#ifdef __thumb__
+        "nop\n"
+        "nop\n"
+        "nop\n"
+#endif
+        : "=&r" (res), "=&r" (oldval), "+Qo" (*ptr)
+        : "r" (ptr), "Ir" (old_value), "r" (new_value)
+        : "cc", "memory");
+  } while (res);
+  return oldval;
 }
 
 static INLINE Atomic32 AtomicExchange(volatile Atomic32* ptr,
