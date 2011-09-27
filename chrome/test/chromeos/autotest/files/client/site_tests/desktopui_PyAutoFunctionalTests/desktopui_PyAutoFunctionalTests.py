f80af4a53dccc1dc1b9ab5f377a2204b3171d094
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-09-27 08:07:58 +0000

    Create a single autotest launcher for different variants of pyauto functional tests
    Usage:
    
    ./run_remote_tests.sh desktopui_PyAutoFunctionalTests -a suite=FULL
    
    ./run_remote_tests.sh desktopui_PyAutoFunctionalTests -a tests=test_basic
    
    ./run_remote_tests.sh desktopui_PyAutoFunctionalTests \
        -a auto_login=False,as_chronos=False,suite=CHROMEOS_LOGIN
    
    BUG=chromium-os:20253
    TEST=desktopui_PyAutoFunctionalTests
    
    R=dennisjeffrey@chromium.org
    
    Review URL: http://codereview.chromium.org/8045009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@102896 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bec2695b1a4faad59f6c71d777032af5f04042bc

--- chrome/test/chromeos/autotest/files/client/site_tests/desktopui_PyAutoFunctionalTests/desktopui_PyAutoFunctionalTests.py
+++ chrome/test/chromeos/autotest/files/client/site_tests/desktopui_PyAutoFunctionalTests/desktopui_PyAutoFunctionalTests.py
@@ -59,20 +59,45 @@ class desktopui_PyAutoFunctionalTests(chrome_test.ChromeTestBase):
             open(constants.DISABLE_BROWSER_RESTART_MAGIC_FILE, 'w').close()
         assert os.path.exists(constants.DISABLE_BROWSER_RESTART_MAGIC_FILE)
 
-    def run_once(self, suite='FULL'):
-        """Run pyauto functional tests."""
-        # Enable chrome testing interface and Login
-        deps_dir = os.path.join(self.autodir, 'deps')
-        pyautolib_dir = os.path.join(self.cr_source_dir,
-                                     'chrome', 'test', 'pyautolib')
-        login_cmd = cros_ui.xcommand_as(
-            'python %s chromeos_utils.ChromeosUtils.LoginToDefaultAccount '
-            '-v --no-http-server' %
-                os.path.join(pyautolib_dir, 'chromeos', 'chromeos_utils.py'))
-        utils.system(login_cmd)
+    def run_once(self, suite=None, tests=None,
+                 as_chronos=True, auto_login=True):
+        """Run pyauto functional tests.
 
-        # Run pyauto tests
-        functional_cmd = cros_ui.xcommand_as(
-            '%s/chrome_test/test_src/chrome/test/functional/'
-            'pyauto_functional.py --suite=%s -v' % (deps_dir, suite))
-        utils.system(functional_cmd)
+        Args:
+            suite: the pyauto functional suite to run.
+            tests: the test modules to run.
+            as_chronos: if True, run tests as chronos.
+            auto_login: if True, login to default account before firing off.
+
+        Either suite or tests should be specified, not both.
+        """
+        assert suite or tests, 'Should specify suite or tests'
+        assert not (suite and tests), \
+            'Should specify either suite or tests, not both'
+
+        deps_dir = os.path.join(self.autodir, 'deps')
+        if auto_login:
+            # Enable chrome testing interface and Login.
+            pyautolib_dir = os.path.join(self.cr_source_dir,
+                                         'chrome', 'test', 'pyautolib')
+            login_cmd = cros_ui.xcommand_as(
+                'python %s chromeos_utils.ChromeosUtils.LoginToDefaultAccount '
+                '-v --no-http-server' % os.path.join(
+                    pyautolib_dir, 'chromeos', 'chromeos_utils.py'))
+            print 'Login cmd', login_cmd
+            utils.system(login_cmd)
+
+        # Run tests.
+        functional_cmd = 'python %s/chrome_test/test_src/' \
+            'chrome/test/functional/pyauto_functional.py -v ' % deps_dir
+        if suite:
+            functional_cmd += ' --suite=%s' % suite
+        elif tests:
+            functional_cmd += tests
+
+        if as_chronos:
+            launch_cmd = cros_ui.xcommand_as(functional_cmd)
+        else:
+            launch_cmd = cros_ui.xcommand(functional_cmd)
+        print 'Test launch cmd', launch_cmd
+        utils.system(launch_cmd)
