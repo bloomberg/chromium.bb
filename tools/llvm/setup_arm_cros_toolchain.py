783952d514d911a41f34e5d6565963890e86e590
jasonwkim@google.com <jasonwkim@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-09-27 20:41:53 +0000

    fix more nits
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6807 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 7cd4983972f7fc497f37c9b913d2e1665b401f96

--- tools/llvm/setup_arm_cros_toolchain.py
+++ tools/llvm/setup_arm_cros_toolchain.py
@@ -7,8 +7,10 @@
 #########################################################################
 # EXPERIMENTAL
 # this code is NOT active in the main NaCl yet!
-# this is only for testing within an artificial CrOS
-# CrOS hook for ARM/thumb2
+# this is only for testing within an artificial CrOS hook for ARM/thumb2
+# BUG=http://code.google.com/p/chromium/issues/detail?id=61695
+# BUG=http://code.google.com/p/chromium/issues/detail?id=38909
+# BUG=http://code.google.com/p/nativeclient/issues/detail?id=135
 #########################################################################
 
 import os
@@ -26,27 +28,29 @@ BASE_DIR = J(NACL_ROOT, 'toolchain', 'linux_arm-trusted')
 ARM_CROSS_TARGET = 'armv7a-cros-linux-gnueabi'
 
 # do we need this stuff at all?
-CODE_SOURCERY_PREFIX = J(BASE_DIR, 'arm-2009q3', 'bin', ARM_CROSS_TARGET)
-CODE_SOURCERY_PREFIX=""
+# CODE_SOURCERY_PREFIX = J(BASE_DIR, 'arm-2009q3', 'bin', ARM_CROSS_TARGET)
+CODE_SOURCERY_PREFIX = ""
 CODE_SOURCERY_JAIL = J(BASE_DIR, 'arm-2009q3', ARM_CROSS_TARGET, 'libc')
 LD_SCRIPT_TRUSTED = J(BASE_DIR, 'ld_script_arm_trusted')
 
 # so do we need to explicitly use the shell scripts in this directory?
 # "/usr/x86_64-pc-linux-gnu/armv7a-cros-linux-gnueabi/gcc-bin/4.4.3/"
-DEFAULT_CROS_DIR=""
-CROS_TOOLCHAIN="%s%s" % (DEFAULT_CROS_DIR, ARM_CROSS_TARGET)
+DEFAULT_CROS_DIR = ""
+CROS_TOOLCHAIN = "%s%s" % (DEFAULT_CROS_DIR, ARM_CROSS_TARGET)
 
-BASE_CC = ("%s-%%s -Werror -O2 %%s "
+##
+BASE_CC = ("%s-%%s %%s "
+           "-Werror -O2 "
            "-fdiagnostics-show-option "
            ) % (CROS_TOOLCHAIN)
 
 # Shell exports
-ARM_CC  = BASE_CC % ('gcc', '-std=gnu99 -pedantic')
-ARM_CXX = BASE_CC % ('g++', '')
-ARM_LD = '%s-ld' % CROS_TOOLCHAIN
-ARM_LINKFLAGS = '' # '-static'
-ARM_LIB_DIR = J(CODE_SOURCERY_JAIL, 'usr', 'lib')
-ARM_EMU = J(BASE_DIR, 'run_under_qemu_arm')
+ARM_CC =         BASE_CC % ('gcc', '-std=gnu99 -pedantic')
+ARM_CXX =        BASE_CC % ('g++', '')
+ARM_LD =         '%s-ld' % CROS_TOOLCHAIN
+ARM_LINKFLAGS =  '' # '-static'
+ARM_LIB_DIR =    J(CODE_SOURCERY_JAIL, 'usr', 'lib')
+ARM_EMU =        J(BASE_DIR, 'run_under_qemu_arm')
 
 # Don't actually emit BASE_CC
 del BASE_CC
@@ -70,9 +74,9 @@ for k,v in dict(globals()).iteritems():
 
 if __name__ == '__main__':
   # Print the values out, for use in a shell script
-  for k,v in arm_env.iteritems():
+  for k, v in arm_env.iteritems():
     if k in shell_exports:
       prefix = 'export '
     else:
       prefix = ''
-    print '%s%s="%s";' % (prefix,k,v)
+    print '%s%s="%s";' % (prefix, k, v)
