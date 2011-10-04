53d41c62a9951d0b2905a0668a11d46f4cd047f7
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-04 15:34:49 +0000

    Convert font, console, buffer, and find to IDL.
    TEST=none
    BUG=none
    Review URL: http://codereview.chromium.org/8124015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@103905 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3fc769ef74a35fcf38aa4d78b7747d18139a7645

--- ppapi/c/dev/ppb_find_dev.h
+++ ppapi/c/dev/ppb_find_dev.h
@@ -1,31 +1,49 @@
-/* Copyright (c) 2010 The Chromium Authors. All rights reserved.
+/* Copyright (c) 2011 The Chromium Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
+
+/* From dev/ppb_find_dev.idl modified Tue Oct  4 08:34:10 2011. */
+
 #ifndef PPAPI_C_DEV_PPB_FIND_DEV_H_
 #define PPAPI_C_DEV_PPB_FIND_DEV_H_
 
 #include "ppapi/c/pp_bool.h"
 #include "ppapi/c/pp_instance.h"
+#include "ppapi/c/pp_macros.h"
 #include "ppapi/c/pp_stdint.h"
 
 #define PPB_FIND_DEV_INTERFACE_0_3 "PPB_Find(Dev);0.3"
 #define PPB_FIND_DEV_INTERFACE PPB_FIND_DEV_INTERFACE_0_3
 
+/**
+ * @file
+ * This file defines the <code>PPB_Find_Dev</code> interface.
+ */
+
+
+/**
+ * @addtogroup Interfaces
+ * @{
+ */
 struct PPB_Find_Dev {
-  // Updates the number of find results for the current search term.  If
-  // there are no matches 0 should be passed in.  Only when the plugin has
-  // finished searching should it pass in the final count with finalResult set
-  // to PP_TRUE.
+  /**
+   * Updates the number of find results for the current search term.  If
+   * there are no matches 0 should be passed in.  Only when the plugin has
+   * finished searching should it pass in the final count with final_result set
+   * to PP_TRUE.
+   */
   void (*NumberOfFindResultsChanged)(PP_Instance instance,
                                      int32_t total,
                                      PP_Bool final_result);
-
-  // Updates the index of the currently selected search item.
-  void (*SelectedFindResultChanged)(PP_Instance instance,
-                                    int32_t index);
-
+  /**
+   * Updates the index of the currently selected search item.
+   */
+  void (*SelectedFindResultChanged)(PP_Instance instance, int32_t index);
 };
+/**
+ * @}
+ */
 
 #endif  /* PPAPI_C_DEV_PPB_FIND_DEV_H_ */
 
