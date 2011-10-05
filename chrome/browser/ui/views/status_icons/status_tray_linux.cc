9eb143989eea65d42d70c443fe621b3f222b9101
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-05 21:44:17 +0000

    There is no StatusTray for Aura/Linux (just like Views/Linux).
    BUG=97131
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8133030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104177 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a22a04852c9e9260b193e9a08469d95eb2092417

--- chrome/browser/ui/views/status_icons/status_tray_linux.cc
+++ chrome/browser/ui/views/status_icons/status_tray_linux.cc
@@ -1,10 +1,10 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "chrome/browser/status_icons/status_tray.h"
 
-// Status icons are not currently supported on linux/views.
+// Status icons are not currently supported on linux/views or Aura.
 StatusTray* StatusTray::Create() {
   return NULL;
 }
