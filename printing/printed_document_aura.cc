88c98902a47dd6b98cee6e8d84367597b51708a2
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-06 04:32:56 +0000

    We will need to support printing under Aura + ChromeOS (and we will have to remove Gtk too).
    BUG=97131
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8165004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104244 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8ad5f1d684fa801c35c7227e84b256056cc2e558

---
+++ printing/printed_document_aura.cc
@@ -0,0 +1,18 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "printing/printed_document.h"
+
+#include "base/logging.h"
+
+namespace printing {
+
+void PrintedDocument::RenderPrintedPage(
+    const PrintedPage& page, PrintingContext* context) const {
+  // TODO(saintlou): This a stub to allow us to build under Aura.
+  // See issue: http://crbug.com/99282
+  NOTIMPLEMENTED();
+}
+
+}  // namespace printing
