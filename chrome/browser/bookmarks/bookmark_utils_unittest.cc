3c0636bbd57fe1fa9f9f6ac0f5814cddb31f0bcd
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-10 16:26:02 +0000

    bookmarks: Change some assertions to expectations in bookmark_utils unittests.
    Also wrap it in bookmark_utils namespace.
    
    TEST=unit_tests --gtest_filter=BookmarkUtilsTest*
    R=sky@chromium.org
    
    Review URL: http://codereview.chromium.org/8208001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@104728 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5eaef1abd8d2d416f94c17093a3befe2bcccb9a6

--- chrome/browser/bookmarks/bookmark_utils_unittest.cc
+++ chrome/browser/bookmarks/bookmark_utils_unittest.cc
@@ -2,127 +2,123 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "testing/gtest/include/gtest/gtest.h"
+#include "chrome/browser/bookmarks/bookmark_utils.h"
 
 #include "base/message_loop.h"
-#include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
-#include "chrome/browser/bookmarks/bookmark_utils.h"
+#include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
 
 #if !defined(OS_MACOSX)
 #include "chrome/browser/browser_process.h"
 #endif
 
-typedef testing::Test BookmarkUtilsTest;
+using std::string;
 
-TEST_F(BookmarkUtilsTest, GetBookmarksContainingText) {
+namespace bookmark_utils {
+namespace {
+
+TEST(BookmarkUtilsTest, GetBookmarksContainingText) {
   BookmarkModel model(NULL);
-  const BookmarkNode* n1 =
-      model.AddURL(model.other_node(), 0, ASCIIToUTF16("foo bar"),
-                   GURL("http://www.google.com"));
-  const BookmarkNode* n2 =
-      model.AddURL(model.other_node(), 0, ASCIIToUTF16("baz buz"),
-                   GURL("http://www.cnn.com"));
+  const BookmarkNode* n1 = model.AddURL(model.other_node(),
+                                        0,
+                                        ASCIIToUTF16("foo bar"),
+                                        GURL("http://www.google.com"));
+  const BookmarkNode* n2 = model.AddURL(model.other_node(),
+                                        0,
+                                        ASCIIToUTF16("baz buz"),
+                                        GURL("http://www.cnn.com"));
 
   model.AddFolder(model.other_node(), 0, ASCIIToUTF16("foo"));
 
   std::vector<const BookmarkNode*> nodes;
-  bookmark_utils::GetBookmarksContainingText(
-      &model, ASCIIToUTF16("foo"), 100, std::string(), &nodes);
+  GetBookmarksContainingText(
+      &model, ASCIIToUTF16("foo"), 100, string(), &nodes);
   ASSERT_EQ(1U, nodes.size());
   EXPECT_TRUE(nodes[0] == n1);
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      n1, ASCIIToUTF16("foo"), std::string()));
+  EXPECT_TRUE(DoesBookmarkContainText(n1, ASCIIToUTF16("foo"), string()));
   nodes.clear();
 
-  bookmark_utils::GetBookmarksContainingText(
-      &model, ASCIIToUTF16("cnn"), 100, std::string(), &nodes);
+  GetBookmarksContainingText(
+      &model, ASCIIToUTF16("cnn"), 100, string(), &nodes);
   ASSERT_EQ(1U, nodes.size());
   EXPECT_TRUE(nodes[0] == n2);
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      n2, ASCIIToUTF16("cnn"), std::string()));
+  EXPECT_TRUE(DoesBookmarkContainText(n2, ASCIIToUTF16("cnn"), string()));
   nodes.clear();
 
-  bookmark_utils::GetBookmarksContainingText(
-      &model, ASCIIToUTF16("foo bar"), 100, std::string(), &nodes);
+  GetBookmarksContainingText(
+      &model, ASCIIToUTF16("foo bar"), 100, string(), &nodes);
   ASSERT_EQ(1U, nodes.size());
   EXPECT_TRUE(nodes[0] == n1);
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      n1, ASCIIToUTF16("foo bar"), std::string()));
+  EXPECT_TRUE(DoesBookmarkContainText(n1, ASCIIToUTF16("foo bar"), string()));
   nodes.clear();
 }
 
-TEST_F(BookmarkUtilsTest, DoesBookmarkContainText) {
+TEST(BookmarkUtilsTest, DoesBookmarkContainText) {
   BookmarkModel model(NULL);
-  const BookmarkNode* node = model.AddURL(model.other_node(), 0,
+  const BookmarkNode* node = model.AddURL(model.other_node(),
+                                          0,
                                           ASCIIToUTF16("foo bar"),
                                           GURL("http://www.google.com"));
   // Matches to the title.
-  ASSERT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("ar"), std::string()));
-  // Matches to the URL
-  ASSERT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("www"), std::string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("ar"), string()));
+  // Matches to the URL.
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("www"), string()));
   // No match.
-  ASSERT_FALSE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("cnn"), std::string()));
+  EXPECT_FALSE(DoesBookmarkContainText(node, ASCIIToUTF16("cnn"), string()));
 
   // Tests for a Japanese IDN.
   const string16 kDecodedIdn = WideToUTF16(L"\x30B0\x30FC\x30B0\x30EB");
-  node = model.AddURL(model.other_node(), 0, ASCIIToUTF16("foo bar"),
+  node = model.AddURL(model.other_node(),
+                      0,
+                      ASCIIToUTF16("foo bar"),
                       GURL("http://xn--qcka1pmc.jp"));
   // Unicode query doesn't match if languages have no "ja".
-  ASSERT_FALSE(bookmark_utils::DoesBookmarkContainText(
-      node, kDecodedIdn, "en"));
+  EXPECT_FALSE(DoesBookmarkContainText(node, kDecodedIdn, "en"));
   // Unicode query matches if languages have "ja".
-  ASSERT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, kDecodedIdn, "ja"));
+  EXPECT_TRUE(DoesBookmarkContainText(node, kDecodedIdn, "ja"));
   // Punycode query also matches as ever.
-  ASSERT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("qcka1pmc"), "ja"));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("qcka1pmc"), "ja"));
 
   // Tests with various lower/upper case characters.
-  node = model.AddURL(model.other_node(), 0, ASCIIToUTF16("FOO bar"),
+  node = model.AddURL(model.other_node(),
+                      0,
+                      ASCIIToUTF16("FOO bar"),
                       GURL("http://www.google.com/search?q=ABC"));
-  ASSERT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("foo"), std::string()));
-  ASSERT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("Foo"), std::string()));
-  ASSERT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("FOO"), std::string()));
-  ASSERT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("google abc"), std::string()));
-  ASSERT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("google ABC"), std::string()));
-  ASSERT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("http://www.google.com/search?q=A"), std::string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("foo"), string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("Foo"), string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("FOO"), string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("google abc"),
+                                      string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("google ABC"),
+                                      string()));
+  EXPECT_TRUE(DoesBookmarkContainText(
+      node, ASCIIToUTF16("http://www.google.com/search?q=A"), string()));
 
   // Test with accents.
-  node = model.AddURL(model.other_node(), 0,
-      WideToUTF16(L"fr\u00E4n\u00E7\u00F3s\u00EA"),
-      GURL("http://www.google.com/search?q=FBA"));
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("francose"), std::string()));
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("FrAnCoSe"), std::string()));
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, WideToUTF16(L"fr\u00E4ncose"), std::string()));
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, WideToUTF16(L"fran\u00E7ose"), std::string()));
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, WideToUTF16(L"franc\u00F3se"), std::string()));
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, WideToUTF16(L"francos\u00EA"), std::string()));
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, WideToUTF16(L"Fr\u00C4n\u00C7\u00F3S\u00EA"), std::string()));
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, WideToUTF16(L"fr\u00C4n\u00C7\u00D3s\u00CA"), std::string()));
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("fba"), std::string()));
-  EXPECT_TRUE(bookmark_utils::DoesBookmarkContainText(
-      node, ASCIIToUTF16("FBA"), std::string()));
+  node = model.AddURL(model.other_node(),
+                      0,
+                      WideToUTF16(L"fr\u00E4n\u00E7\u00F3s\u00EA"),
+                      GURL("http://www.google.com/search?q=FBA"));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("francose"),
+                                      string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("FrAnCoSe"),
+                                      string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, WideToUTF16(L"fr\u00E4ncose"),
+                                      string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, WideToUTF16(L"fran\u00E7ose"),
+                                      string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, WideToUTF16(L"franc\u00F3se"),
+                                      string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, WideToUTF16(L"francos\u00EA"),
+                                      string()));
+  EXPECT_TRUE(DoesBookmarkContainText(
+      node, WideToUTF16(L"Fr\u00C4n\u00C7\u00F3S\u00EA"), string()));
+  EXPECT_TRUE(DoesBookmarkContainText(
+      node, WideToUTF16(L"fr\u00C4n\u00C7\u00D3s\u00CA"), string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("fba"), string()));
+  EXPECT_TRUE(DoesBookmarkContainText(node, ASCIIToUTF16("FBA"), string()));
 }
 
 #if !defined(OS_MACOSX)
@@ -133,22 +129,23 @@ TEST_F(BookmarkUtilsTest, DoesBookmarkContainText) {
 #else
 #define MAYBE_CopyPaste CopyPaste
 #endif
-TEST_F(BookmarkUtilsTest, MAYBE_CopyPaste) {
+TEST(BookmarkUtilsTest, MAYBE_CopyPaste) {
   // Clipboard requires a message loop.
   MessageLoopForUI loop;
 
   BookmarkModel model(NULL);
-  const BookmarkNode* node = model.AddURL(model.other_node(), 0,
+  const BookmarkNode* node = model.AddURL(model.other_node(),
+                                          0,
                                           ASCIIToUTF16("foo bar"),
                                           GURL("http://www.google.com"));
 
   // Copy a node to the clipboard.
   std::vector<const BookmarkNode*> nodes;
   nodes.push_back(node);
-  bookmark_utils::CopyToClipboard(&model, nodes, false);
+  CopyToClipboard(&model, nodes, false);
 
   // And make sure we can paste a bookmark from the clipboard.
-  EXPECT_TRUE(bookmark_utils::CanPasteFromClipboard(model.bookmark_bar_node()));
+  EXPECT_TRUE(CanPasteFromClipboard(model.bookmark_bar_node()));
 
   // Write some text to the clipboard.
   {
@@ -157,7 +154,9 @@ TEST_F(BookmarkUtilsTest, MAYBE_CopyPaste) {
   }
 
   // Now we shouldn't be able to paste from the clipboard.
-  EXPECT_FALSE(
-      bookmark_utils::CanPasteFromClipboard(model.bookmark_bar_node()));
+  EXPECT_FALSE(CanPasteFromClipboard(model.bookmark_bar_node()));
 }
 #endif
+
+}  // namespace
+}  // namespace bookmark_utils
