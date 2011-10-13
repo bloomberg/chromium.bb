87d135becc47eb992a13934c0e528557a6f32282
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 10:48:16 +0000

    Introducing the basic functionality to the speech input experimental API.
    BUG=97388
    TEST=none
    
    Review URL: http://codereview.chromium.org/8201030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105291 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7c216711dc47d02fd87d453f81d1f495cd94ab94

--- chrome/browser/extensions/speech_input/extension_speech_input_api.h
+++ chrome/browser/extensions/speech_input/extension_speech_input_api.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_EXTENSION_SPEECH_INPUT_API_H_
-#define CHROME_BROWSER_EXTENSIONS_EXTENSION_SPEECH_INPUT_API_H_
+#ifndef CHROME_BROWSER_EXTENSIONS_SPEECH_INPUT_EXTENSION_SPEECH_INPUT_API_H_
+#define CHROME_BROWSER_EXTENSIONS_SPEECH_INPUT_EXTENSION_SPEECH_INPUT_API_H_
 #pragma once
 
 #include "chrome/browser/extensions/extension_function.h"
@@ -16,7 +16,9 @@ class ExtensionSpeechInputManager;
 class SpeechInputAsyncFunction : public AsyncExtensionFunction,
                                  public NotificationObserver {
  protected:
-  SpeechInputAsyncFunction();
+  SpeechInputAsyncFunction(int start_state, int transition_state,
+                           int end_state, int transition_notification);
+  virtual ~SpeechInputAsyncFunction();
 
   virtual void Run() OVERRIDE;
   virtual bool RunImpl() = 0;
@@ -27,11 +29,23 @@ class SpeechInputAsyncFunction : public AsyncExtensionFunction,
                        const NotificationSource& source,
                        const NotificationDetails& details) OVERRIDE;
 
+  // To be defined on construction by derived classes.
+  int start_state_;
+  int transition_state_;
+  int end_state_;
+  int transition_notification_;
+
   NotificationRegistrar registrar_;
+  bool expecting_transition_;
+  bool failed_;
 };
 
 // Implements experimental.speechInput.start.
 class StartSpeechInputFunction : public SpeechInputAsyncFunction {
+ public:
+  StartSpeechInputFunction();
+  virtual ~StartSpeechInputFunction() {}
+
  protected:
   virtual bool RunImpl() OVERRIDE;
   DECLARE_EXTENSION_FUNCTION_NAME("experimental.speechInput.start");
@@ -39,16 +53,20 @@ class StartSpeechInputFunction : public SpeechInputAsyncFunction {
 
 // Implements experimental.speechInput.stop.
 class StopSpeechInputFunction : public SpeechInputAsyncFunction {
+ public:
+  StopSpeechInputFunction();
+  virtual ~StopSpeechInputFunction() {}
+
  protected:
   virtual bool RunImpl() OVERRIDE;
   DECLARE_EXTENSION_FUNCTION_NAME("experimental.speechInput.stop");
 };
 
 // Implements experimental.speechInput.isRecording.
-class IsRecordingSpeechInputFunction : public SyncIOThreadExtensionFunction {
+class IsRecordingSpeechInputFunction : public SyncExtensionFunction {
  protected:
   virtual bool RunImpl() OVERRIDE;
   DECLARE_EXTENSION_FUNCTION_NAME("experimental.speechInput.isRecording");
 };
 
-#endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_SPEECH_INPUT_API_H_
+#endif  // CHROME_BROWSER_EXTENSIONS_SPEECH_INPUT_EXTENSION_SPEECH_INPUT_API_H_
