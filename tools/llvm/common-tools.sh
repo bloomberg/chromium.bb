36fa9e9f1f8f1254110a2b7d9a054431c5e63272
jasonwkim@google.com <jasonwkim@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-10-13 22:03:30 +0000

    fix typo
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8274018
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6924 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: d62ee00a02a61f26616ae33b870e32b9ddffd1dc

--- tools/llvm/common-tools.sh
+++ tools/llvm/common-tools.sh
@@ -37,7 +37,7 @@ SetLogDirectory() {
 ######################################################################
 # Detect if we are in a ChromiumOS chroot
 ######################################################################
-if [ -e /etc/debian_chroot ] then
+if [ -e /etc/debian_chroot ]; then
   readonly UTMAN_IN_CROS_CHROOT=true
 else
   readonly UTMAN_IN_CROS_CHROOT=false
