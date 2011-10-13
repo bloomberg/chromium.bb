8f6be359e128f180a0c93535c1dbf8fa3939b1aa
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 19:18:02 +0000

    [Sync UI] Fix Mac main menu sync strings.
    BUG=29358, 99264
    TEST=Load chrome for mac. Under apple menu item should say "Sign in to Chrome...". When signed in, it should say "Signed in as foo@example.com...".
    
    
    Review URL: http://codereview.chromium.org/8242015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105349 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 93a8181cb08504db9767899abb0b7f8f0900c878

--- chrome/browser/sync/sync_ui_util_mac.h
+++ chrome/browser/sync/sync_ui_util_mac.h
@@ -1,10 +1,12 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef CHROME_BROWSER_SYNC_SYNC_UI_UTIL_MAC_H_
 #define CHROME_BROWSER_SYNC_SYNC_UI_UTIL_MAC_H_
 #pragma once
+
+#include <string>
 
 #include "chrome/browser/sync/sync_ui_util.h"
 
@@ -21,7 +23,8 @@ void UpdateSyncItem(id syncItem, BOOL syncEnabled, Profile* profile);
 // This function (used by UpdateSyncItem) is only exposed for testing.
 // Just use UpdateSyncItem() instead.
 void UpdateSyncItemForStatus(id syncItem, BOOL syncEnabled,
-                             sync_ui_util::MessageType status);
+                             sync_ui_util::MessageType status,
+                             const std::string& userName);
 
 }  // namespace sync_ui_util
 
--- chrome/browser/sync/sync_ui_util_mac.mm
+++ chrome/browser/sync/sync_ui_util_mac.mm
@@ -6,23 +6,36 @@
 
 #import <Cocoa/Cocoa.h>
 
+#include "base/utf_string_conversions.h"
 #include "base/logging.h"
+#include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/sync_ui_util.h"
+#include "chrome/common/pref_names.h"
+#include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/l10n/l10n_util_mac.h"
+
+using l10n_util::GetStringUTF16;
+using l10n_util::GetNSStringWithFixup;
+using l10n_util::GetNSStringFWithFixup;
 
 namespace sync_ui_util {
 
 void UpdateSyncItem(id syncItem, BOOL syncEnabled, Profile* profile) {
   ProfileSyncService* syncService =
     profile->GetOriginalProfile()->GetProfileSyncService();
-  UpdateSyncItemForStatus(syncItem, syncEnabled,
-                          sync_ui_util::GetStatus(syncService));
+  UpdateSyncItemForStatus(
+      syncItem,
+      syncEnabled,
+      sync_ui_util::GetStatus(syncService),
+      profile->GetPrefs()->GetString(prefs::kGoogleServicesUsername));
 }
 
 void UpdateSyncItemForStatus(id syncItem, BOOL syncEnabled,
-                             sync_ui_util::MessageType status) {
+                             sync_ui_util::MessageType status,
+                             const std::string& userName) {
   DCHECK([syncItem isKindOfClass:[NSMenuItem class]]);
   NSMenuItem* syncMenuItem = static_cast<NSMenuItem*>(syncItem);
   // Look for a separator immediately after the menu item.
@@ -41,24 +54,22 @@ void UpdateSyncItemForStatus(id syncItem, BOOL syncEnabled,
 
   // TODO(akalin): consolidate this code with the equivalent Windows code in
   // chrome/browser/ui/views/toolbar_view.cc.
-  int titleId;
+  NSString* title = NULL;
   switch (status) {
     case sync_ui_util::SYNCED:
-      titleId = IDS_SYNC_MENU_SYNCED_LABEL;
+      title = GetNSStringFWithFixup(IDS_SYNC_MENU_SYNCED_LABEL,
+                                    UTF8ToUTF16(userName));
       break;
     case sync_ui_util::SYNC_ERROR:
-      titleId = IDS_SYNC_MENU_SYNC_ERROR_LABEL;
+      title = GetNSStringWithFixup(IDS_SYNC_MENU_SYNC_ERROR_LABEL);
       break;
     case sync_ui_util::PRE_SYNCED:
-      titleId = IDS_SYNC_START_SYNC_BUTTON_LABEL;
-      break;
+    case sync_ui_util::SYNC_PROMO:
     default:
-      NOTREACHED();
-      // Needed to prevent release-mode warnings.
-      titleId = IDS_SYNC_START_SYNC_BUTTON_LABEL;
+      title = GetNSStringFWithFixup(IDS_SYNC_MENU_PRE_SYNCED_LABEL,
+                                    GetStringUTF16(IDS_SHORT_PRODUCT_NAME));
       break;
   }
-  NSString* title = l10n_util::GetNSStringWithFixup(titleId);
   [syncMenuItem setTitle:title];
 
   // If we don't have a sync service, hide any sync-related menu
