570810986032817fa2d46a7dc8657327de8eee76
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-13 21:41:01 +0000

    Change the default favicon to 16x16, except for tabstrip (32x32), for touchui builds.
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8251001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105380 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2cc6a929e97b99af96696983549c3cf0e18e9ba5

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -520,7 +520,14 @@ std::string ChromeContentBrowserClient::GetAcceptLangs(const TabContents* tab) {
 
 SkBitmap* ChromeContentBrowserClient::GetDefaultFavicon() {
   ResourceBundle &rb = ResourceBundle::GetSharedInstance();
+#if defined(TOUCH_UI)
+  // In touch builds, we want large default favicons for the tabstrip, but in
+  // other places (such as bookmark, manage search engines, homepage) we assume
+  // default favicons are 16x16.
+  return rb.GetBitmapNamed(IDR_DEFAULT_LARGE_FAVICON);
+#else
   return rb.GetBitmapNamed(IDR_DEFAULT_FAVICON);
+#endif
 }
 
 bool ChromeContentBrowserClient::AllowAppCache(
