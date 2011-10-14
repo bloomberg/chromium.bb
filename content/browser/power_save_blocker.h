5db542362f22cfcb32b95039bc1d13183b32c0ab
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 17:09:54 +0000

    Add screen power save block level.
    BUG=100054
    TEST=no change
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=105401
    
    Reverted: http://src.chromium.org/viewvc/chrome?view=rev&revision=105415
    
    Review URL: http://codereview.chromium.org/8251008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105507 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b48a93fae23ed15b5535c41694629da589757c4a

--- content/browser/power_save_blocker.h
+++ content/browser/power_save_blocker.h
@@ -6,35 +6,48 @@
 #define CONTENT_BROWSER_POWER_SAVE_BLOCKER_H_
 #pragma once
 
+#include <vector>
+
 #include "base/basictypes.h"
 
 // A RAII-style class to block the system from entering low-power (sleep) mode.
 class PowerSaveBlocker {
  public:
-  explicit PowerSaveBlocker(bool enabled);
+  enum PowerSaveBlockerType {
+    kPowerSaveBlockPreventNone = -1,
+
+    // Prevent the system from going to sleep; allow display sleep.
+    kPowerSaveBlockPreventSystemSleep,
+
+    // Prevent the system or display from going to sleep.
+    kPowerSaveBlockPreventDisplaySleep,
+
+    // Count of the values; not valid as a parameter.
+    kPowerSaveBlockPreventStateCount
+  };
+
+  // Pass in the level of sleep prevention desired. kPowerSaveBlockPreventNone
+  // is not a valid option.
+  explicit PowerSaveBlocker(PowerSaveBlockerType type);
   ~PowerSaveBlocker();
-
-  bool enabled() const { return enabled_; }
-
-  // Puts the sleep mode block into effect.
-  void Enable();
-  // Disables the sleep block.
-  void Disable();
 
  private:
   // Platform-specific function called when enable state is changed.
   // Guaranteed to be called only from the UI thread.
-  static void ApplyBlock(bool blocked);
+  static void ApplyBlock(PowerSaveBlockerType type);
 
   // Called only from UI thread.
-  static void AdjustBlockCount(int delta);
+  static void AdjustBlockCount(const std::vector<int>& deltas);
 
   // Invokes AdjustBlockCount on the UI thread.
-  static void PostAdjustBlockCount(int delta);
+  static void PostAdjustBlockCount(const std::vector<int>& deltas);
 
-  bool enabled_;
+  // Returns the highest-severity block type in use.
+  static PowerSaveBlockerType HighestBlockType();
 
-  static int blocker_count_;
+  PowerSaveBlockerType type_;
+
+  static int blocker_count_[];
 
   DISALLOW_COPY_AND_ASSIGN(PowerSaveBlocker);
 };
