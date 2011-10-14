aafb6d8e41f9e94db9a22c26f80fc45c2a8f75f8
enal@chromium.org <enal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-14 03:08:32 +0000

    Urgent workaround for M16 ChromeOS bug.
    I suspect it is caused by low latency audeo on the slow hardware.
    While attempting proper investigation I just turn off low latency
    audio on ChromeOS for now.
    If it is really the case I se several ways to properly fix the bug,
    but they require much more testing and experimenting.
    
    BUG=chromium-os:21491
    
    Review URL: http://codereview.chromium.org/8275016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105448 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4dc86f9547ec8942ddb4e52ae4c4cf21dc305969

--- content/renderer/media/audio_renderer_impl.cc
+++ content/renderer/media/audio_renderer_impl.cc
@@ -40,12 +40,19 @@ AudioRendererImpl::AudioRendererImpl()
   // We are initializing only one variable and double initialization is Ok,
   // so there would not be any issues caused by CPU memory model.
   if (latency_type_ == kUninitializedLatency) {
+    // Urgent workaround for
+    // http://code.google.com/p/chromium-os/issues/detail?id=21491
+    // TODO(enal): Fix it properly.
+#if defined(OS_CHROMEOS)
+    latency_type_ = kHighLatency;
+#else
     if (!CommandLine::ForCurrentProcess()->HasSwitch(
         switches::kHighLatencyAudio)) {
       latency_type_ = kLowLatency;
     } else {
       latency_type_ = kHighLatency;
     }
+#endif
   }
 }
 
