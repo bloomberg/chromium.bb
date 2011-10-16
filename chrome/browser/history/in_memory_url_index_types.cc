98f91d72919cb52d63a0d7e308dec83ecf9778c7
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-16 00:41:37 +0000

    Revert 105681 - Caused flakiness described in r100479
    Fix MSVC 2010 compile following http://crrev.com/105678
    
    
    R=mrossetti@chromium.org
    
    BUG=96731, 92718
    
    TEST=none
    
    
    
    Review URL: http://codereview.chromium.org/8310004
    
    
    
    TBR=rsleevi@chromium.org
    Review URL: http://codereview.chromium.org/8311004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105696 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0a121ca5705403021cf2e54ae6d66f68d1c9952e

--- chrome/browser/history/in_memory_url_index_types.cc
+++ chrome/browser/history/in_memory_url_index_types.cc
@@ -5,7 +5,6 @@
 #include "chrome/browser/history/in_memory_url_index_types.h"
 
 #include <algorithm>
-#include <iterator>
 
 #include "base/i18n/break_iterator.h"
 #include "base/i18n/case_conversion.h"
