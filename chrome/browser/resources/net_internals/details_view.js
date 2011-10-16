12d8c39178319b000327059210f661ee0d17a4b7
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-16 03:59:58 +0000

    Fixup style in details_view.js
    BUG=90857
    
    Review URL: http://codereview.chromium.org/8267016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105715 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8488e5f6538ae31c58452fa48e6a23e2fc6ebe41

--- chrome/browser/resources/net_internals/details_view.js
+++ chrome/browser/resources/net_internals/details_view.js
@@ -2,132 +2,171 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * The DetailsView handles the tabbed view that displays either the "log" or
- * "timeline" view. This class keeps track of what the current view is, and
- * invalidates the specific view each time the selected data has changed.
- *
- * @constructor
- */
-function DetailsView(tabHandlesContainerId,
-                     logTabId,
-                     timelineTabId,
-                     logBoxId,
-                     timelineBoxId) {
-  var tabSwitcher = new TabSwitcherView();
+var DetailsView = (function() {
+  'use strict';
 
-  VerticalSplitView.call(this,
-                         new DivView(tabHandlesContainerId),
-                         tabSwitcher);
+  // We inherit from VerticalSplitView.
+  var superClass = VerticalSplitView;
 
-  this.tabSwitcher_ = tabSwitcher;
+  /**
+   * The DetailsView handles the tabbed view that displays either the "log" or
+   * "timeline" view. This class keeps track of what the current view is, and
+   * invalidates the specific view each time the selected data has changed.
+   *
+   * @constructor
+   */
+  function DetailsView(tabHandlesContainerId,
+                       logTabId,
+                       timelineTabId,
+                       logBoxId,
+                       timelineBoxId) {
+    var tabSwitcher = new TabSwitcherView();
 
-  this.logView_ = new DetailsLogView(logBoxId);
-  this.timelineView_ = new DetailsTimelineView(timelineBoxId);
+    superClass.call(this, new DivView(tabHandlesContainerId), tabSwitcher);
 
-  this.tabSwitcher_.addTab(logTabId, this.logView_, true, true);
-  this.tabSwitcher_.addTab(timelineTabId, this.timelineView_, true, true);
+    this.tabSwitcher_ = tabSwitcher;
 
-  // Default to the log view.
-  this.tabSwitcher_.switchToTab(logTabId, null);
-};
+    this.logView_ = new DetailsLogView(logBoxId);
+    this.timelineView_ = new DetailsTimelineView(timelineBoxId);
 
-inherits(DetailsView, VerticalSplitView);
+    this.tabSwitcher_.addTab(logTabId, this.logView_, true, true);
+    this.tabSwitcher_.addTab(timelineTabId, this.timelineView_, true, true);
 
-// The delay between updates to repaint.
-DetailsView.REPAINT_TIMEOUT_MS = 50;
-
-/**
- * Updates the data this view is using.
- */
-DetailsView.prototype.setData = function(currentData) {
-  // Make a copy of the array (in case the caller mutates it), and sort it
-  // by the source ID.
-  var sortedCurrentData = DetailsView.createSortedCopy_(currentData);
-
-  // TODO(eroman): Should not access private members of TabSwitcherView.
-  for (var i = 0; i < this.tabSwitcher_.tabs_.length; ++i)
-    this.tabSwitcher_.tabs_[i].contentView.setData(sortedCurrentData);
-};
-
-DetailsView.createSortedCopy_ = function(origArray) {
-  var sortedArray = origArray.slice(0);
-  sortedArray.sort(function(a, b) {
-    return a.getSourceId() - b.getSourceId();
-  });
-  return sortedArray;
-};
-
-//-----------------------------------------------------------------------------
-
-/**
- * Base class for the Log view and Timeline view.
- *
- * @constructor
- */
-function DetailsSubView(boxId) {
-  DivView.call(this, boxId);
-  this.sourceEntries_ = [];
-}
-
-inherits(DetailsSubView, DivView);
-
-DetailsSubView.prototype.setData = function(sourceEntries) {
-  this.sourceEntries_ = sourceEntries;
-
-  // Repaint the view.
-  if (this.isVisible() && !this.outstandingRepaint_) {
-    this.outstandingRepaint_ = true;
-    window.setTimeout(this.repaint.bind(this),
-                      DetailsView.REPAINT_TIMEOUT_MS);
+    // Default to the log view.
+    this.tabSwitcher_.switchToTab(logTabId, null);
   }
-};
 
-DetailsSubView.prototype.repaint = function() {
-  this.outstandingRepaint_ = false;
-  this.getNode().innerHTML = '';
-};
+  // The delay between updates to repaint.
+  var REPAINT_TIMEOUT_MS = 50;
 
-DetailsSubView.prototype.show = function(isVisible) {
-  DetailsSubView.superClass_.show.call(this, isVisible);
-  if (isVisible) {
-    this.repaint();
-  } else {
-    this.getNode().innerHTML = '';
+  DetailsView.prototype = {
+    // Inherit the superclass's methods.
+    __proto__: superClass.prototype,
+
+    /**
+     * Updates the data this view is using.
+     */
+    setData: function(currentData) {
+      // Make a copy of the array (in case the caller mutates it), and sort it
+      // by the source ID.
+      var sortedCurrentData = createSortedCopy(currentData);
+
+      // TODO(eroman): Should not access private members of TabSwitcherView.
+      for (var i = 0; i < this.tabSwitcher_.tabs_.length; ++i)
+        this.tabSwitcher_.tabs_[i].contentView.setData(sortedCurrentData);
+    }
+  };
+
+  function createSortedCopy(origArray) {
+    var sortedArray = origArray.slice(0);
+    sortedArray.sort(function(a, b) {
+      return a.getSourceId() - b.getSourceId();
+    });
+    return sortedArray;
   }
-};
 
-//-----------------------------------------------------------------------------
+  //---------------------------------------------------------------------------
 
+  var DetailsSubView = (function() {
+    // We inherit from DivView.
+    var superClass = DivView;
 
-/**
- * Subview that is displayed in the log tab.
- * @constructor
- */
-function DetailsLogView(boxId) {
-  DetailsSubView.call(this, boxId);
-}
+    /**
+     * Base class for the Log view and Timeline view.
+     *
+     * @constructor
+     */
+    function DetailsSubView(boxId) {
+      superClass.call(this, boxId);
+      this.sourceEntries_ = [];
+    }
 
-inherits(DetailsLogView, DetailsSubView);
+    DetailsSubView.prototype = {
+      // Inherit the superclass's methods.
+      __proto__: superClass.prototype,
 
-DetailsLogView.prototype.repaint = function() {
-  DetailsLogView.superClass_.repaint.call(this);
-  PaintLogView(this.sourceEntries_, this.getNode());
-};
+      setData: function(sourceEntries) {
+        this.sourceEntries_ = sourceEntries;
 
-//-----------------------------------------------------------------------------
+        // Repaint the view.
+        if (this.isVisible() && !this.outstandingRepaint_) {
+          this.outstandingRepaint_ = true;
+          window.setTimeout(this.repaint.bind(this),
+                            REPAINT_TIMEOUT_MS);
+        }
+      },
 
-/**
- * Subview that is displayed in the timeline tab.
- * @constructor
- */
-function DetailsTimelineView(boxId) {
-  DetailsSubView.call(this, boxId);
-}
+      repaint: function() {
+        this.outstandingRepaint_ = false;
+        this.getNode().innerHTML = '';
+      },
 
-inherits(DetailsTimelineView, DetailsSubView);
+      show: function(isVisible) {
+        superClass.prototype.show.call(this, isVisible);
+        if (isVisible) {
+          this.repaint();
+        } else {
+          this.getNode().innerHTML = '';
+        }
+      }
+    };
 
-DetailsTimelineView.prototype.repaint = function() {
-  DetailsTimelineView.superClass_.repaint.call(this);
-  PaintTimelineView(this.sourceEntries_, this.getNode());
-};
+    return DetailsSubView;
+  })();
+
+  //---------------------------------------------------------------------------
+
+  var DetailsLogView = (function() {
+    // We inherit from DetailsSubView.
+    var superClass = DetailsSubView;
+
+    /**
+     * Subview that is displayed in the log tab.
+     * @constructor
+     */
+    function DetailsLogView(boxId) {
+      superClass.call(this, boxId);
+    }
+
+    DetailsLogView.prototype = {
+      // Inherit the superclass's methods.
+      __proto__: superClass.prototype,
+
+      repaint: function() {
+        superClass.prototype.repaint.call(this);
+        PaintLogView(this.sourceEntries_, this.getNode());
+      }
+    };
+
+    return DetailsLogView;
+  })();
+
+  //---------------------------------------------------------------------------
+
+  var DetailsTimelineView = (function() {
+    // We inherit from DetailsSubView.
+    var superClass = DetailsSubView;
+
+    /**
+     * Subview that is displayed in the timeline tab.
+     * @constructor
+     */
+    function DetailsTimelineView(boxId) {
+      superClass.call(this, boxId);
+    }
+
+    DetailsTimelineView.prototype = {
+      // Inherit the superclass's methods.
+      __proto__: superClass.prototype,
+
+      repaint: function() {
+        superClass.prototype.repaint.call(this);
+        PaintTimelineView(this.sourceEntries_, this.getNode());
+      }
+    };
+
+    return DetailsTimelineView;
+  })();
+
+  return DetailsView;
+})();
