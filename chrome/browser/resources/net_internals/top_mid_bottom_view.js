e28f68bcf4c9ef5bd596f1cd62ab5c322307fb80
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-16 01:53:11 +0000

    Fix up style for TopMidBottomView.
    BUG=90857
    
    Review URL: http://codereview.chromium.org/8289011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105703 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 157e8df19bb6bf85ecee8ad6a5c09e9f7c7a8046

--- chrome/browser/resources/net_internals/top_mid_bottom_view.js
+++ chrome/browser/resources/net_internals/top_mid_bottom_view.js
@@ -1,59 +1,71 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * This view stacks three boxes -- one at the top, one at the bottom, and
- * one that fills the remaining space between those two.
- *
- *  +----------------------+
- *  |      topbar          |
- *  +----------------------+
- *  |                      |
- *  |                      |
- *  |                      |
- *  |                      |
- *  |      middlebox       |
- *  |                      |
- *  |                      |
- *  |                      |
- *  |                      |
- *  |                      |
- *  +----------------------+
- *  |     bottombar        |
- *  +----------------------+
- *
- *  @constructor
- */
-function TopMidBottomView(topView, midView, bottomView) {
-  View.call(this);
+var TopMidBottomView = (function() {
+  'use strict';
 
-  this.topView_ = topView;
-  this.midView_ = midView;
-  this.bottomView_ = bottomView;
-}
+  // We inherit from View.
+  var superClass = View;
 
-inherits(TopMidBottomView, View);
+  /**
+   * This view stacks three boxes -- one at the top, one at the bottom, and
+   * one that fills the remaining space between those two.
+   *
+   *  +----------------------+
+   *  |      topbar          |
+   *  +----------------------+
+   *  |                      |
+   *  |                      |
+   *  |                      |
+   *  |                      |
+   *  |      middlebox       |
+   *  |                      |
+   *  |                      |
+   *  |                      |
+   *  |                      |
+   *  |                      |
+   *  +----------------------+
+   *  |     bottombar        |
+   *  +----------------------+
+   *
+   *  @constructor
+   */
+  function TopMidBottomView(topView, midView, bottomView) {
+    superClass.call(this);
 
-TopMidBottomView.prototype.setGeometry = function(left, top, width, height) {
-  TopMidBottomView.superClass_.setGeometry.call(this, left, top, width, height);
+    this.topView_ = topView;
+    this.midView_ = midView;
+    this.bottomView_ = bottomView;
+  }
 
-  // Calculate the vertical split points.
-  var topbarHeight = this.topView_.getHeight();
-  var bottombarHeight = this.bottomView_.getHeight();
-  var middleboxHeight = height - (topbarHeight + bottombarHeight);
+  TopMidBottomView.prototype = {
+    // Inherit the superclass's methods.
+    __proto__: superClass.prototype,
 
-  // Position the boxes using calculated split points.
-  this.topView_.setGeometry(left, top, width, topbarHeight);
-  this.midView_.setGeometry(left, this.topView_.getBottom(),
-                            width, middleboxHeight);
-  this.bottomView_.setGeometry(left, this.midView_.getBottom(),
-                               width, bottombarHeight);
-};
+    setGeometry: function(left, top, width, height) {
+      superClass.prototype.setGeometry.call(this, left, top, width, height);
 
-TopMidBottomView.prototype.show = function(isVisible) {
-  TopMidBottomView.superClass_.show.call(this, isVisible);
-  this.topView_.show(isVisible);
-  this.midView_.show(isVisible);
-  this.bottomView_.show(isVisible);
-};
+      // Calculate the vertical split points.
+      var topbarHeight = this.topView_.getHeight();
+      var bottombarHeight = this.bottomView_.getHeight();
+      var middleboxHeight = height - (topbarHeight + bottombarHeight);
+
+      // Position the boxes using calculated split points.
+      this.topView_.setGeometry(left, top, width, topbarHeight);
+      this.midView_.setGeometry(left, this.topView_.getBottom(),
+                                width, middleboxHeight);
+      this.bottomView_.setGeometry(left, this.midView_.getBottom(),
+                                   width, bottombarHeight);
+    },
+
+    show: function(isVisible) {
+      superClass.prototype.show.call(this, isVisible);
+      this.topView_.show(isVisible);
+      this.midView_.show(isVisible);
+      this.bottomView_.show(isVisible);
+    }
+  };
+
+  return TopMidBottomView;
+})();
