cfef573824e69172ab3d48ff2785c365fd885995
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-17 18:28:16 +0000

    Revert 105759 - Revert 105755 - Fails on XP and Vista
    Fixed win tests.
    
    Adding experimental extension clipboard api.
    
    The api will give extensions that use it access to system clipboard.
    
    design doc:
    https://docs.google.com/a/google.com/document/d/1my6Ifb86y0M9WMNNUqkE1LSXe8WLwfRtY3ZUetqNKt0/edit?hl=en_US
    
    TEST=ExtensionClipboardApiTest.*
    BUG=chromium-os:20492
    
    Review URL: http://codereview.chromium.org/8258007
    
    TBR=tbarzic@chromium.org
    Review URL: http://codereview.chromium.org/8306020
    
    TBR=tbarzic@chromium.org
    Review URL: http://codereview.chromium.org/8294014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105855 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6a16dc9bb9f21da5215029d73f28b670f6b64473

--- chrome/browser/extensions/extension_function_dispatcher.cc
+++ chrome/browser/extensions/extension_function_dispatcher.cc
@@ -18,6 +18,7 @@
 #include "chrome/browser/extensions/extension_bookmarks_module.h"
 #include "chrome/browser/extensions/extension_browser_actions_api.h"
 #include "chrome/browser/extensions/extension_clear_api.h"
+#include "chrome/browser/extensions/extension_clipboard_api.h"
 #include "chrome/browser/extensions/extension_chrome_auth_private_api.h"
 #include "chrome/browser/extensions/extension_content_settings_api.h"
 #include "chrome/browser/extensions/extension_context_menu_api.h"
@@ -461,6 +462,11 @@ void FactoryRegistry::ResetFunctions() {
 
   // SavePage
   RegisterFunction<SavePageAsMHTMLFunction>();
+
+  // Clipboard
+  RegisterFunction<WriteDataClipboardFunction>();
+  RegisterFunction<ReadDataClipboardFunction>();
+  RegisterFunction<GetAvailableMimeTypesClipboardFunction>();
 }
 
 void FactoryRegistry::GetAllNames(std::vector<std::string>* names) {
--- chrome/renderer/resources/extensions/renderer_process_bindings.js
+++ chrome/renderer/resources/extensions/renderer_process_bindings.js
@@ -326,6 +326,7 @@ var chrome = chrome || {};
       "experimental.app",
       "experimental.bookmarkManager",
       "experimental.clear",
+      "experimental.clipboard",
       "experimental.debugger",
       "experimental.downloads",
       "experimental.extension",
