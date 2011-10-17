e49e5bdf7192dc781f5d58257f112ccd891766d1
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-17 20:59:23 +0000

    Remove testSetReleaseTrack test
    Test images now have 'testimage-channel' channel by default. Setting the
    channel to this string is not supported (or expected), and we obviously
    cannot just switch channels and leave it.
    
    R=dtu@chromium.org
    BUG=
    TEST=
    
    Review URL: http://codereview.chromium.org/8316018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105915 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4fcfd34cd445497b495381301083a11ec5e45cb6

--- chrome/test/functional/chromeos_update.py
+++ chrome/test/functional/chromeos_update.py
@@ -12,13 +12,6 @@ import pyauto
 class ChromeosUpdate(pyauto.PyUITest):
   """Tests for ChromeOS updater and channel changer."""
 
-  def testSetReleaseTrack(self):
-    """Ensure we can set the device's release track (channel)."""
-    release_track = self.GetUpdateInfo()['release_track']
-    for track in ('dev-channel', 'beta-channel', release_track):
-      self.SetReleaseTrack(track)
-      self.assertEqual(self.GetUpdateInfo()['release_track'], track)
-
   def testGetUpdateInfo(self):
     """Get some status info about the updater and release track."""
     result = self.GetUpdateInfo()
