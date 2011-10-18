f699aab6ca5a88b2bc1a2a5ebe4b97b89da4d6eb
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 08:42:56 +0000

    Adding `content_security_policy` to the "Mappy" sample.
    This involved pretty much rewriting the popup code to avoid a script injected
    via `document.write` in the Maps API code.
    
    BUG=92644
    TEST=
    
    Review URL: http://codereview.chromium.org/8311007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106043 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1c806dff522222f36dfc1345c7f1c20a2c54dfe1

--- chrome/common/extensions/docs/examples/extensions/mappy/mappy_content_script.js
+++ chrome/common/extensions/docs/examples/extensions/mappy/mappy_content_script.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // The background page is asking us to find an address on the page.
 if (window == top) {
   chrome.extension.onRequest.addListener(function(req, sender, sendResponse) {
