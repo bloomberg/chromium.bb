c7c8cfe3a71e25b9531a2eb2b05b0c2b7d682ed7
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-10-18 14:22:39 +0000

    Enable another test for pnacl by using the standard entry point name and using -nostdlib instead of  -nostartfiles Review URL: http://codereview.chromium.org/8316005
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6963 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: da144aae13b03280150b17216a90d4231996f194

--- tests/minnacl/minimal_test_guest.c
+++ tests/minnacl/minimal_test_guest.c
@@ -1,7 +1,7 @@
 /*
- * Copyright 2011 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 #include "native_client/src/trusted/service_runtime/nacl_config.h"
@@ -11,14 +11,14 @@
 #define UNTYPED_SYSCALL(s) ((int (*)()) NACL_SYSCALL_ADDR(s))
 
 
-void SimpleAbort() {
+static void SimpleAbort() {
   while (1) {
     /* Exit by causing a crash. */
     *(volatile int *) 0 = 0;
   }
 }
 
-void EntryPoint() {
+void _start() {
   int retval = UNTYPED_SYSCALL(TEST_SYSCALL_INVOKE)();
   if (retval != 123) {
     /* This sandbox is so simple that we have no way of printing a
