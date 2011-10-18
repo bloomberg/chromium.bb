64e54b6a4717791d8819addbb260cb3d223651e1
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 01:19:09 +0000

    [Sync UI] Update "Disconnect your Google Account" dialog to reference Google Dashboard.
    BUG=100643
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8294031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@105987 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1bc6fb677d89be38ce18d09282a7372fc62cf709

--- chrome/browser/ui/webui/options/stop_syncing_handler.cc
+++ chrome/browser/ui/webui/options/stop_syncing_handler.cc
@@ -6,10 +6,13 @@
 
 #include "base/basictypes.h"
 #include "base/bind.h"
+#include "base/utf_string_conversions.h"
 #include "base/values.h"
+#include "chrome/browser/google/google_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/sync/profile_sync_service.h"
+#include "chrome/common/url_constants.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -24,8 +27,11 @@ void StopSyncingHandler::GetLocalizedValues(
     DictionaryValue* localized_strings) {
   DCHECK(localized_strings);
   localized_strings->SetString("stopSyncingExplanation",
-      l10n_util::GetStringFUTF16(IDS_SYNC_STOP_SYNCING_EXPLANATION_LABEL,
-                                 l10n_util::GetStringUTF16(IDS_PRODUCT_NAME)));
+      l10n_util::GetStringFUTF16(
+          IDS_SYNC_STOP_SYNCING_EXPLANATION_LABEL,
+          l10n_util::GetStringUTF16(IDS_PRODUCT_NAME),
+          ASCIIToUTF16(google_util::StringAppendGoogleLocaleParam(
+              chrome::kSyncGoogleDashboardURL))));
   localized_strings->SetString("stopSyncingTitle",
       l10n_util::GetStringUTF16(IDS_SYNC_STOP_SYNCING_DIALOG_TITLE));
   localized_strings->SetString("stopSyncingConfirm",
