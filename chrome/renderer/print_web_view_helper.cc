674f1bfc484dacb4d37a0871b1f5d45a16f4cc62
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 23:06:35 +0000

    Fix print media page size by using the value we compute.
    BUG=82472
    TEST=NONE (in bug)
    
    
    Review URL: http://codereview.chromium.org/8344016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106160 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f1a142d29ad1dfaecd3b609051b476440289ec72

--- chrome/renderer/print_web_view_helper.cc
+++ chrome/renderer/print_web_view_helper.cc
@@ -1382,8 +1382,6 @@ bool PrintWebViewHelper::PrintPreviewContext::CreatePreviewDocument(
   DCHECK_EQ(INITIALIZED, state_);
   state_ = RENDERING;
 
-  print_params_.reset(new PrintMsg_Print_Params(*print_params));
-
   metafile_.reset(new printing::PreviewMetafile);
   if (!metafile_->Init()) {
     set_error(PREVIEW_ERROR_METAFILE_INIT_FAILED);
@@ -1396,6 +1394,8 @@ bool PrintWebViewHelper::PrintPreviewContext::CreatePreviewDocument(
                                                          node()));
   UpdatePrintableSizeInPrintParameters(frame_, node_,
                                        prep_frame_view_.get(), print_params);
+
+  print_params_.reset(new PrintMsg_Print_Params(*print_params));
 
   total_page_count_ = prep_frame_view_->GetExpectedPageCount();
   if (total_page_count_ == 0) {
