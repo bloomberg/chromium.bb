93f5137e08b806fac2e5b02a35cb55109e41b91e
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-10-18 21:21:35 +0000

    Document progress with building libstdc++ from gcc 4.6.1 using pnacl-clang
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2327
    Review URL: http://codereview.chromium.org/8348014
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@6969 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: d1dda5f9c356d0899105f80ca635faa635dffd44

---
+++ tools/llvm/unsupported/build-libstdc++.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+# This is a temporary hack to see whether we can build libstdc++ from
+# gcc 4.6.1 with pnacl-clang
+#  Upon success this should be folded into utman
+# NOTE: run this in native_client/hg/ like so
+#
+# ../tools/llvm/unsupported/build-libstdc++.sh download
+# ../tools/llvm/unsupported/build-libstdc++.sh patch
+# ../tools/llvm/unsupported/build-libstdc++.sh libstdcpp-configure
+# ../tools/llvm/unsupported/build-libstdc++.sh libstdcpp-make -k
+#
+set -o nounset
+set -o errexit
+
+readonly INSTALL_BIN="$(pwd)/../toolchain/pnacl_linux_x86_64_newlib/bin/"
+
+CC=gcc
+TARBALL_URL=http://ftp.gnu.org/gnu/gcc/gcc-4.6.1/gcc-g++-4.6.1.tar.bz2
+
+
+
+# Location of PNaCl gcc/g++/as
+readonly PNACL_GCC="${INSTALL_BIN}/pnacl-gcc"
+readonly PNACL_GXX="${INSTALL_BIN}/pnacl-g++"
+readonly PNACL_CLANG="${INSTALL_BIN}/pnacl-clang"
+readonly PNACL_CLANGXX="${INSTALL_BIN}/pnacl-clang++"
+readonly PNACL_AR="${INSTALL_BIN}/pnacl-ar"
+readonly PNACL_RANLIB="${INSTALL_BIN}/pnacl-ranlib"
+readonly PNACL_AS="${INSTALL_BIN}/pnacl-as"
+readonly PNACL_LD="${INSTALL_BIN}/pnacl-ld"
+readonly PNACL_NM="${INSTALL_BIN}/pnacl-nm"
+readonly PNACL_TRANSLATE="${INSTALL_BIN}/pnacl-translate"
+readonly PNACL_READELF="${INSTALL_BIN}/readelf"
+readonly PNACL_SIZE="${INSTALL_BIN}/size"
+readonly PNACL_STRIP="${INSTALL_BIN}/pnacl-strip"
+
+readonly PNACL_AS_ARM="${INSTALL_BIN}/pnacl-arm-as"
+readonly PNACL_AS_X8632="${INSTALL_BIN}/pnacl-i686-as"
+readonly PNACL_AS_X8664="${INSTALL_BIN}/pnacl-x86_64-as"
+
+
+readonly ILLEGAL_TOOL=${INSTALL_BIN}/pnacl-illegal
+
+STD_ENV_FOR_LIBSTDCPP_CLANG=(
+  CC_FOR_BUILD="${CC}"
+  CC="${PNACL_CLANG}"
+  CXX="${PNACL_CLANGXX}"
+  RAW_CXX_FOR_TARGET="${PNACL_CLANGXX}"
+  LD="${ILLEGAL_TOOL}"
+  CFLAGS="--pnacl-arm-bias"
+  CPPFLAGS="--pnacl-arm-bias"
+  CXXFLAGS="--pnacl-arm-bias "
+  CFLAGS_FOR_TARGET="--pnacl-arm-bias"
+  CPPFLAGS_FOR_TARGET="--pnacl-arm-bias"
+  CC_FOR_TARGET="${PNACL_CLANG}"
+  GCC_FOR_TARGET="${PNACL_CLANG}"
+  CXX_FOR_TARGET="${PNACL_CLANGXX}"
+  AR="${PNACL_AR}"
+  AR_FOR_TARGET="${PNACL_AR}"
+  NM_FOR_TARGET="${PNACL_NM}"
+  RANLIB="${PNACL_RANLIB}"
+  RANLIB_FOR_TARGET="${PNACL_RANLIB}"
+  AS_FOR_TARGET="${ILLEGAL_TOOL}"
+  LD_FOR_TARGET="${ILLEGAL_TOOL}"
+  OBJDUMP_FOR_TARGET="${ILLEGAL_TOOL}" )
+
+
+mkdir -p build
+
+
+download() {
+  wget ${TARBALL_URL} -O gcc-g++-4.6.1.tar.bz2
+  tar jxf gcc-g++-4.6.1.tar.bz2
+}
+
+
+patch() {
+  pushd gcc-4.6.1
+  patch -p1 < ../../tools/llvm/unsupported/patch-libstdc++
+  popd
+}
+
+
+libstdcpp-configure() {
+  mkdir -p build
+  pushd build
+  env -i PATH=/usr/bin/:/bin  "${STD_ENV_FOR_LIBSTDCPP_CLANG[@]}" \
+    ../gcc-4.6.1/libstdc++-v3/configure \
+    --host=arm-none-linux-gnueabi \
+    --disable-shared \
+    --disable-rpath \
+    --enable-shared=no \
+    --disable-libstdcxx-pch \
+    --enable-linux-futex=no \
+    --with-newlib \
+    --enable-sjlj-exceptions=no \
+    "$@"
+  popd
+}
+
+
+libstdcpp-make() {
+  pushd  build
+  env -i PATH=/usr/bin/:/bin "${STD_ENV_FOR_LIBSTDCPP_CLANG[@]}" \
+        make \
+        "$@"
+  popd
+}
+
+
+"$@"
