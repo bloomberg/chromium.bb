8e91e2996382ef25eea66fb44ad1828214b7352c
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 18:55:13 +0000

    Disable NoInfobarForAllowedOrigin on Win.
    BUG=100763
    TEST=
    TBR=bulach@chromium.org
    
    Review URL: http://codereview.chromium.org/8342006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106092 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c6e6617d80159e7c1dbf00ebf44f82b52f89f4ff

--- chrome/browser/geolocation/geolocation_browsertest.cc
+++ chrome/browser/geolocation/geolocation_browsertest.cc
@@ -444,7 +444,13 @@ IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest, MAYBE_NoInfobarForDeniedOrigin) {
   CheckStringValueFromJavascript("1", "geoGetLastError()");
 }
 
-IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest, NoInfobarForAllowedOrigin) {
+// http://crbug.com/100763. Crashes occasionally on XP.
+#if defined(OS_WIN)
+#define MAYBE_NoInfobarForAllowedOrigin DISABLED_NoInfobarForAllowedOrigin
+#else
+#define MAYBE_NoInfobarForAllowedOrigin NoInfobarForAllowedOrigin
+#endif
+IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest, MAYBE_NoInfobarForAllowedOrigin) {
   ASSERT_TRUE(Initialize(INITIALIZATION_NONE));
   current_browser_->profile()->GetHostContentSettingsMap()->
       SetContentSetting(ContentSettingsPattern::FromURLNoWildcard(current_url_),
