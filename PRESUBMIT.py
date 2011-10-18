f13120624cf87327debe370c688c2115ed32d434
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-18 02:39:11 +0000

    Make whitelist for files starting with test_ or mock_ treat path before filename correctly.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8341004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106008 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2aefa8627e19ede126281162e4376639085e9ab0

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -49,7 +49,7 @@ def _CheckNoProductionCodeUsingTestOnlyFunctions(input_api, output_api):
   source_extensions = r'\.(cc|cpp|cxx|mm)$'
   file_inclusion_pattern = r'.+%s' % source_extensions
   file_exclusion_pattern = (
-    r'((test_|mock_).+|.+(_test_support|profile_sync_service_harness|'
+    r'(.*/(test_|mock_).+|.+(_test_support|profile_sync_service_harness|'
     r'_(api|browser|perf|unit|ui)?test))%s' % source_extensions)
   path_exclusion_pattern = r'.*[/\\](test|tool(s)?)[/\\].*'
 
