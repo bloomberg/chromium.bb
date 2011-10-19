e7b3d731639f3175d2f7c0b66a204f6a65a6bbef
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-19 03:01:06 +0000

    Fix build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106202 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fccf2fb4583e78321e5da59dfba0c8b2021f914e

--- chrome/browser/automation/automation_provider_observers.cc
+++ chrome/browser/automation/automation_provider_observers.cc
@@ -2502,8 +2502,8 @@ NewNotificationBalloonObserver::~NewNotificationBalloonObserver() { }
 
 void NewNotificationBalloonObserver::Observe(
     int type,
-    const NotificationSource& source,
-    const NotificationDetails& details) {
+    const content::NotificationSource& source,
+    const content::NotificationDetails& details) {
   if (automation_) {
     AutomationJSONReply(automation_,
                         reply_message_.release()).SendSuccess(NULL);
