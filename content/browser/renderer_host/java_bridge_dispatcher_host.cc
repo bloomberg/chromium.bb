0070c239df836663214cab77389a2eca0472437a
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-21 03:06:58 +0000

    Adds the browser component of the Java Bridge for a single RenderView
    This patch adds JavaBridgeDispatcherHost, which handles injecting Java objects
    into a single RenderView.
    
    BUG=96703
    
    
    Review URL: http://codereview.chromium.org/8361018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106668 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9a56c756a72f003cb126d7497d9123e19179636d

---
+++ content/browser/renderer_host/java_bridge_dispatcher_host.cc
@@ -0,0 +1,89 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/browser/renderer_host/java_bridge_dispatcher_host.h"
+
+#include "content/browser/browser_thread.h"
+#include "content/browser/renderer_host/java_bridge_channel_host.h"
+#include "content/browser/renderer_host/browser_render_process_host.h"
+#include "content/browser/renderer_host/render_view_host.h"
+#include "content/common/child_process.h"
+#include "content/common/java_bridge_messages.h"
+#include "content/common/npobject_stub.h"
+#include "content/common/npobject_util.h"  // For CreateNPVariantParam()
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebBindings.h"
+
+JavaBridgeDispatcherHost::JavaBridgeDispatcherHost(
+    RenderViewHost* render_view_host)
+    : render_view_host_(render_view_host) {
+  DCHECK(render_view_host_);
+}
+
+JavaBridgeDispatcherHost::~JavaBridgeDispatcherHost() {
+}
+
+void JavaBridgeDispatcherHost::AddNamedObject(const string16& name,
+                                              NPObject* object) {
+  // Post to the WEBKIT thread, as that's where we want injected objects to
+  // live.
+  WebKit::WebBindings::retainObject(object);
+  BrowserThread::PostTask(
+      BrowserThread::WEBKIT,
+      FROM_HERE,
+      NewRunnableMethod(this,
+                        &JavaBridgeDispatcherHost::DoAddNamedObject,
+                        name,
+                        object));
+}
+
+void JavaBridgeDispatcherHost::RemoveNamedObject(const string16& name) {
+  // On receipt of this message, the JavaBridgeDispatcher will drop its
+  // reference to the corresponding proxy object. When the last reference is
+  // removed, the proxy object will delete its NPObjectProxy, which will cause
+  // the NPObjectStub to be deleted, which will drop its reference to the
+  // original NPObject.
+  render_view_host_->Send(new JavaBridgeMsg_RemoveNamedObject(
+      render_view_host_->routing_id(), name));
+}
+
+void JavaBridgeDispatcherHost::DoAddNamedObject(const string16& name,
+                                                NPObject* object) {
+  EnsureChannelIsSetUp();
+
+  NPVariant variant;
+  OBJECT_TO_NPVARIANT(object, variant);
+  DCHECK_EQ(variant.type, NPVariantType_Object);
+
+  // Create an NPVariantParam suitable for serialization over IPC from our
+  // NPVariant. Internally, this will create an NPObjectStub, which takes a
+  // reference to the underlying NPObject. We pass release = true to release
+  // the ref added in AddNamedObject(). We don't need the containing window or
+  // the page URL, as we don't do re-entrant sync IPC.
+  NPVariant_Param variant_param;
+  CreateNPVariantParam(variant, channel_, &variant_param, true, NULL, GURL());
+  DCHECK_EQ(variant_param.type, NPVARIANT_PARAM_SENDER_OBJECT_ROUTING_ID);
+
+  render_view_host_->Send(new JavaBridgeMsg_AddNamedObject(
+      render_view_host_->routing_id(), name, variant_param));
+}
+
+void JavaBridgeDispatcherHost::EnsureChannelIsSetUp() {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::WEBKIT));
+  if (channel_) {
+    return;
+  }
+
+  channel_ = JavaBridgeChannelHost::GetJavaBridgeChannelHost(
+      render_view_host_->process()->id(),
+      BrowserThread::GetMessageLoopProxyForThread(BrowserThread::IO));
+
+  // The channel creates the channel handle based on the renderer ID we passed
+  // to GetJavaBridgeChannelHost() and, on POSIX, the file descriptor used by
+  // the underlying channel.
+  IPC::ChannelHandle channel_handle = channel_->channel_handle();
+
+  bool sent = render_view_host_->Send(new JavaBridgeMsg_Init(
+      render_view_host_->routing_id(), channel_handle));
+  DCHECK(sent);
+}
---
+++ content/browser/renderer_host/java_bridge_dispatcher_host.h
@@ -0,0 +1,51 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_BROWSER_RENDERER_JAVA_BRIDGE_DISPATCHER_HOST_H_
+#define CONTENT_BROWSER_RENDERER_JAVA_BRIDGE_DISPATCHER_HOST_H_
+#pragma once
+
+#include "base/memory/ref_counted.h"
+#include "base/string16.h"
+
+class JavaBridgeChannelHost;
+class RenderViewHost;
+struct NPObject;
+
+// This class handles injecting Java objects into a single RenderView. The Java
+// object itself lives on the browser's WEBKIT thread, while a proxy object is
+// created in the renderer. An instance of this class exists for each
+// RenderViewHost.
+class JavaBridgeDispatcherHost :
+    public base::RefCountedThreadSafe<JavaBridgeDispatcherHost> {
+ public:
+  // We hold a weak pointer to the RenderViewhost. It must outlive this object.
+  JavaBridgeDispatcherHost(RenderViewHost* render_view_host);
+
+  // Injects |object| into the main frame of the corresponding RenderView. A
+  // proxy object is created in the renderer and when the main frame's window
+  // object is next cleared, this proxy object is bound to the window object
+  // using |name|. The proxy object remains bound until the next time the
+  // window object is cleared after a call to RemoveNamedObject() or
+  // AddNamedObject() with the same name. The proxy object proxies calls back
+  // to |object|, which is manipulated on the WEBKIT thread. This class holds a
+  // reference to |object| for the time that the proxy object is bound to the
+  // window object.
+  void AddNamedObject(const string16& name, NPObject* object);
+  void RemoveNamedObject(const string16& name);
+
+ private:
+  friend class base::RefCountedThreadSafe<JavaBridgeDispatcherHost>;
+  ~JavaBridgeDispatcherHost();
+
+  void DoAddNamedObject(const string16& name, NPObject* object);
+  void EnsureChannelIsSetUp();
+
+  RenderViewHost* render_view_host_;
+  scoped_refptr<JavaBridgeChannelHost> channel_;
+
+  DISALLOW_COPY_AND_ASSIGN(JavaBridgeDispatcherHost);
+};
+
+#endif  // CONTENT_BROWSER_RENDERER_JAVA_BRIDGE_DISPATCHER_HOST_H_
