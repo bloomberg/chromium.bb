ef358be6118b5c9c9f837efab4abe036b31aa703
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-21 18:28:15 +0000

    Get rid of ChildProcessHost usage from chrome/common. The service process code didn't use any of the logic in ChildProcessHost to get the path of the current binary, so it can go directly to PathService.
    BUG=98716
    Review URL: http://codereview.chromium.org/8364029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@106744 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: da4216b5f0aaa247a24df6e1ac6aa3480cc037ff

--- chrome/common/service_process_util.cc
+++ chrome/common/service_process_util.cc
@@ -22,7 +22,7 @@
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/chrome_version_info.h"
 #include "chrome/common/service_process_util.h"
-#include "content/common/child_process_host.h"
+#include "content/common/content_paths.h"
 
 #if !defined(OS_MACOSX)
 
@@ -252,11 +252,9 @@ IPC::ChannelHandle ServiceProcessState::GetServiceProcessChannel() {
 #endif  // !OS_MACOSX
 
 void ServiceProcessState::CreateAutoRunCommandLine() {
-  FilePath exe_path =
-      ChildProcessHost::GetChildPath(ChildProcessHost::CHILD_NORMAL);
-  if (exe_path.empty()) {
-    NOTREACHED() << "Unable to get service process binary name.";
-  }
+  FilePath exe_path;
+  PathService::Get(content::CHILD_PROCESS_EXE, &exe_path);
+  DCHECK(!exe_path.empty()) << "Unable to get service process binary name.";
   autorun_command_line_.reset(new CommandLine(exe_path));
   autorun_command_line_->AppendSwitchASCII(switches::kProcessType,
                                            switches::kServiceProcess);
