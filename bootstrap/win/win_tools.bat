b3528b0a4566de602f5b895ac4cec1f585e297f0
cmp@chromium.org <cmp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-25 21:50:37 +0000

    Don't auto-install on bots for now.
    BUG=79356
    Review URL: http://codereview.chromium.org/8394016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@107212 0039d316-1c4b-4281-b951-d872f2087c98

--- bootstrap/win/win_tools.bat
+++ bootstrap/win/win_tools.bat
@@ -25,6 +25,7 @@ if "%1" == "force" (
 :GIT_CHECK
 :: If the batch file exists, skip the git check.
 if exist "%WIN_TOOLS_ROOT_DIR%\git.bat" goto :SVN_CHECK
+if "%CHROME_HEADLESS%" == "1" goto :SVN_CHECK
 if "%WIN_TOOLS_FORCE%" == "1" goto :GIT_INSTALL
 call git --version 2>nul 1>nul
 if errorlevel 1 goto :GIT_INSTALL
