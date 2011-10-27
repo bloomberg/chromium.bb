bloomberg/chromium.bb

commit e3b3a46b0ee8df5642ebeff747f8f69b8599a090

aura: Add touch-event support for RWHVA.
The way a WebTouchEvent is prepared in RWHVAura is the same as it is in
RWHVViews: the RWHVA maintains a WebTouchEvent, and each aura::TouchEvent
adds/removes/updates a WebTouchPoint in the WTE.

BUG=99757
TEST=manually, with the --touch-devices flag on linux.

Review URL: http://codereview.chromium.org/8377001

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107569 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 0446db079ba108e034d7b79da20c694b6eee935b

content/browser/renderer_host/web_input_event_aurawin.cc
@@ -36,4 +36,10 @@ WebKit::WebKeyboardEvent MakeWebKeyboardEventFromNativeEvent(
                                                      native_event.lParam);
 }
 
+WebKit::WebTouchPoint* UpdateWebTouchEventFromNativeEvent(
+    base::NativeEvent native_event, WebKit::WebTouchEvent* web_event) {
+  NOTIMPLEMENTED();
+  return NULL;
+}
+
 }  // namespace content

content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -25,6 +25,8 @@
 #include "ui/gfx/gl/gl_bindings.h"
 #endif
 
+using WebKit::WebTouchEvent;
+
 namespace {
 
 #if defined(UI_COMPOSITOR_IMAGE_TRANSPORT)
@@ -35,6 +37,37 @@ void AcknowledgeSwapBuffers(int32 route_id, int gpu_host_id) {
       new AcceleratedSurfaceMsg_BuffersSwappedACK(route_id));
 }
 #endif
+
+ui::TouchStatus DecideTouchStatus(const WebKit::WebTouchEvent& event,
+                                  WebKit::WebTouchPoint* point) {
+  if (event.type == WebKit::WebInputEvent::TouchStart &&
+      event.touchesLength == 1)
+    return ui::TOUCH_STATUS_START;
+
+  if (event.type == WebKit::WebInputEvent::TouchMove && point == NULL)
+    return ui::TOUCH_STATUS_CONTINUE;
+
+  if (event.type == WebKit::WebInputEvent::TouchEnd &&
+      event.touchesLength == 0)
+    return ui::TOUCH_STATUS_END;
+
+  if (event.type == WebKit::WebInputEvent::TouchCancel)
+    return ui::TOUCH_STATUS_CANCEL;
+
+  return point ? ui::TOUCH_STATUS_CONTINUE : ui::TOUCH_STATUS_UNKNOWN;
+}
+
+void UpdateWebTouchEventAfterDispatch(WebKit::WebTouchEvent* event,
+                                      WebKit::WebTouchPoint* point) {
+  if (point->state != WebKit::WebTouchPoint::StateReleased)
+    return;
+  --event->touchesLength;
+  for (unsigned i = point - event->touches;
+       i < event->touchesLength;
+       ++i) {
+    event->touches[i] = event->touches[i + 1];
+  }
+}
 
 }  // namespace
 
@@ -361,8 +394,17 @@ bool RenderWidgetHostViewAura::OnMouseEvent(aura::MouseEvent* event) {
 
 ui::TouchStatus RenderWidgetHostViewAura::OnTouchEvent(
     aura::TouchEvent* event) {
-  NOTIMPLEMENTED();
-  return ui::TOUCH_STATUS_UNKNOWN;
+  // Update the touch event first.
+  WebKit::WebTouchPoint* point = content::UpdateWebTouchEvent(event,
+      &touch_event_);
+
+  // Forward the touch event only if a touch point was updated.
+  if (point) {
+    host_->ForwardTouchEvent(touch_event_);
+    UpdateWebTouchEventAfterDispatch(&touch_event_, point);
+  }
+
+  return DecideTouchStatus(touch_event_, point);
 }
 
 bool RenderWidgetHostViewAura::ShouldActivate(aura::Event* event) {

content/browser/renderer_host/render_widget_host_view_aura.h
@@ -13,6 +13,10 @@
 #include "ui/gfx/compositor/compositor_observer.h"
 #include "webkit/glue/webcursor.h"
 
+namespace WebKit {
+class WebTouchEvent;
+}
+
 #if defined(UI_COMPOSITOR_IMAGE_TRANSPORT)
 class AcceleratedSurfaceContainerLinux;
 #endif
@@ -133,6 +137,11 @@ class RenderWidgetHostViewAura : public RenderWidgetHostView,
 
   // The cursor for the page. This is passed up from the renderer.
   WebCursor current_cursor_;
+
+  // The touch-event. Its touch-points are updated as necessary. A new
+  // touch-point is added from an ET_TOUCH_PRESSED event, and a touch-point is
+  // removed from the list on an ET_TOUCH_RELEASED event.
+  WebKit::WebTouchEvent touch_event_;
 
 #if defined(UI_COMPOSITOR_IMAGE_TRANSPORT)
   std::vector< base::Callback<void(void)> > on_compositing_ended_callbacks_;

content/browser/renderer_host/web_input_event_aura.cc
@@ -15,12 +15,16 @@ WebKit::WebMouseWheelEvent MakeUntranslatedWebMouseWheelEventFromNativeEvent(
     base::NativeEvent native_event);
 WebKit::WebKeyboardEvent MakeWebKeyboardEventFromNativeEvent(
     base::NativeEvent native_event);
+WebKit::WebTouchPoint* UpdateWebTouchEventFromNativeEvent(
+    base::NativeEvent native_event, WebKit::WebTouchEvent* web_event);
 #else
 WebKit::WebMouseEvent MakeWebMouseEventFromAuraEvent(aura::MouseEvent* event);
 WebKit::WebMouseWheelEvent MakeWebMouseWheelEventFromAuraEvent(
     aura::MouseEvent* event);
 WebKit::WebKeyboardEvent MakeWebKeyboardEventFromAuraEvent(
     aura::KeyEvent* event);
+WebKit::WebTouchPoint* UpdateWebTouchEventFromAuraEvent(
+    aura::TouchEvent* event, WebKit::WebTouchEvent* web_event);
 #endif
 
 // General approach:
@@ -101,6 +105,15 @@ WebKit::WebKeyboardEvent MakeWebKeyboardEvent(aura::KeyEvent* event) {
   return MakeWebKeyboardEventFromNativeEvent(event->native_event());
 #else
   return MakeWebKeyboardEventFromAuraEvent(event);
+#endif
+}
+
+WebKit::WebTouchPoint* UpdateWebTouchEvent(aura::TouchEvent* event,
+                                           WebKit::WebTouchEvent* web_event) {
+#if defined(OS_WIN)
+  return UpdateWebTouchEventFromNativeEvent(event->native_event(), web_event);
+#else
+  return UpdateWebTouchEventFromAuraEvent(event, web_event);
 #endif
 }
 

content/browser/renderer_host/web_input_event_aura.h
@@ -11,6 +11,7 @@
 namespace aura {
 class KeyEvent;
 class MouseEvent;
+class TouchEvent;
 }
 
 namespace content {
@@ -19,7 +20,12 @@ WebKit::WebMouseEvent MakeWebMouseEvent(aura::MouseEvent* event);
 WebKit::WebMouseWheelEvent MakeWebMouseWheelEvent(aura::MouseEvent* event);
 WebKit::WebKeyboardEvent MakeWebKeyboardEvent(aura::KeyEvent* event);
 
-// TODO(beng): WebKit::WebMouseWheelEvent
+// Updates the WebTouchEvent based on the TouchEvent. It returns the updated
+// WebTouchPoint contained in the WebTouchEvent, or NULL if no point was
+// updated.
+WebKit::WebTouchPoint* UpdateWebTouchEvent(aura::TouchEvent* event,
+                                           WebKit::WebTouchEvent* web_event);
+
 
 }
 

content/browser/renderer_host/web_input_event_aurax11.cc
@@ -199,6 +199,38 @@ void ResetClickCountState() {
   g_last_click_button = WebKit::WebMouseEvent::ButtonNone;
 }
 
+WebKit::WebTouchPoint::State TouchPointStateFromEvent(
+    const aura::TouchEvent* event) {
+  switch (event->type()) {
+    case ui::ET_TOUCH_PRESSED:
+      return WebKit::WebTouchPoint::StatePressed;
+    case ui::ET_TOUCH_RELEASED:
+      return WebKit::WebTouchPoint::StateReleased;
+    case ui::ET_TOUCH_MOVED:
+      return WebKit::WebTouchPoint::StateMoved;
+    case ui::ET_TOUCH_CANCELLED:
+      return WebKit::WebTouchPoint::StateCancelled;
+    default:
+      return WebKit::WebTouchPoint::StateUndefined;
+  }
+}
+
+WebKit::WebInputEvent::Type TouchEventTypeFromEvent(
+    const aura::TouchEvent* event) {
+  switch (event->type()) {
+    case ui::ET_TOUCH_PRESSED:
+      return WebKit::WebInputEvent::TouchStart;
+    case ui::ET_TOUCH_RELEASED:
+      return WebKit::WebInputEvent::TouchEnd;
+    case ui::ET_TOUCH_MOVED:
+      return WebKit::WebInputEvent::TouchMove;
+    case ui::ET_TOUCH_CANCELLED:
+      return WebKit::WebInputEvent::TouchCancel;
+    default:
+      return WebKit::WebInputEvent::Undefined;
+  }
+}
+
 }  // namespace
 
 WebKit::WebMouseEvent MakeWebMouseEventFromAuraEvent(aura::MouseEvent* event) {
@@ -315,6 +347,75 @@ WebKit::WebKeyboardEvent MakeWebKeyboardEventFromAuraEvent(
   // TODO: IsAutoRepeat/IsKeyPad?
 
   return webkit_event;
+}
+
+WebKit::WebTouchPoint* UpdateWebTouchEventFromAuraEvent(
+    aura::TouchEvent* event, WebKit::WebTouchEvent* web_event) {
+  WebKit::WebTouchPoint* point = NULL;
+  switch (event->type()) {
+    case ui::ET_TOUCH_PRESSED:
+      // Add a new touch point.
+      if (web_event->touchesLength < WebKit::WebTouchEvent::touchesLengthCap) {
+        point = &web_event->touches[web_event->touchesLength++];
+        point->id = event->touch_id();
+      }
+      break;
+    case ui::ET_TOUCH_RELEASED:
+    case ui::ET_TOUCH_CANCELLED:
+    case ui::ET_TOUCH_MOVED: {
+      // The touch point should have been added to the event from an earlier
+      // _PRESSED event. So find that.
+      // At the moment, only a maximum of 4 touch-points are allowed. So a
+      // simple loop should be sufficient.
+      for (unsigned i = 0; i < web_event->touchesLength; ++i) {
+        point = web_event->touches + i;
+        if (point->id == event->touch_id())
+          break;
+        point = NULL;
+      }
+      break;
+    }
+    default:
+      DLOG(WARNING) << "Unknown touch event " << event->type();
+      break;
+  }
+
+  if (!point)
+    return NULL;
+
+  point->radiusX = event->radius_x();
+  point->radiusY = event->radius_y();
+  point->rotationAngle = event->rotation_angle();
+  point->force = event->force();
+
+  // Update the location and state of the point.
+  point->state = TouchPointStateFromEvent(event);
+  if (point->state == WebKit::WebTouchPoint::StateMoved) {
+    // It is possible for badly written touch drivers to emit Move events even
+    // when the touch location hasn't changed. In such cases, consume the event
+    // and pretend nothing happened.
+    if (point->position.x == event->x() && point->position.y == event->y())
+      return NULL;
+  }
+  point->position.x = event->x();
+  point->position.y = event->y();
+
+  // TODO(sad): Convert to screen coordinates.
+  point->screenPosition.x = point->position.x;
+  point->screenPosition.y = point->position.y;
+
+  // Mark the rest of the points as stationary.
+  for (unsigned i = 0; i < web_event->touchesLength; ++i) {
+    WebKit::WebTouchPoint* iter = web_event->touches + i;
+    if (iter != point)
+      iter->state = WebKit::WebTouchPoint::StateStationary;
+  }
+
+  // Update the type of the touch event.
+  web_event->type = TouchEventTypeFromEvent(event);
+  web_event->timeStampSeconds = event->time_stamp().ToDoubleT();
+
+  return point;
 }
 
 }  // namespace content

commit 0f5406e2dfc76f4ac541c2887dd171553d8e544f

aura: Fix win compile, broken in r107569.
TBR=ben@chromium.org
BUG=none
TEST=none

Review URL: http://codereview.chromium.org/8403042

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107573 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: ab30b683cd22fd3310e80bac168e2346a0fde276

content/browser/renderer_host/web_input_event_aurawin.cc
@@ -5,6 +5,7 @@
 #include "content/browser/renderer_host/web_input_event_aura.h"
 
 #include "base/event_types.h"
+#include "base/logging.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/win/WebInputEventFactory.h"
 
 namespace content {