ea2102390c0c4ec984f0643e5c7dbf49a9c1fc61
benjhayden@chromium.org <benjhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-28 15:53:29 +0000

    DownloadsApiTest uses a ScopedTempDir to clean up after itself. BUG=101162
    Review URL: http://codereview.chromium.org/8418009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107739 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bb6434a07ca57d2293e7db65b16c6f3229befeeb

--- chrome/browser/extensions/extension_downloads_apitest.cc
+++ chrome/browser/extensions/extension_downloads_apitest.cc
@@ -3,7 +3,11 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/extensions/extension_apitest.h"
+#include "chrome/browser/prefs/pref_service.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/pref_names.h"
 
 class DownloadsApiTest : public ExtensionApiTest {
  public:
@@ -11,10 +15,19 @@ class DownloadsApiTest : public ExtensionApiTest {
     ExtensionApiTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(switches::kEnableExperimentalExtensionApis);
   }
+
+  void SetUpTempDownloadsDir() {
+    ASSERT_TRUE(tmpdir.CreateUniqueTempDir());
+    browser()->profile()->GetPrefs()->SetFilePath(
+        prefs::kDownloadDefaultDirectory, tmpdir.path());
+  }
+
+ private:
+  ScopedTempDir tmpdir;
 };
 
-// Disabled: see http://crbug.com/101170
-IN_PROC_BROWSER_TEST_F(DownloadsApiTest, DISABLED_Downloads) {
+IN_PROC_BROWSER_TEST_F(DownloadsApiTest, Downloads) {
+  SetUpTempDownloadsDir();
   ASSERT_TRUE(StartTestServer());
   ASSERT_TRUE(RunExtensionTest("downloads")) << message_;
 }
