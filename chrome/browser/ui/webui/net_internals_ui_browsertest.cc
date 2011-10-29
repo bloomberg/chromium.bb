e4263484ecb37ed5a36b2b8e60ca8b8dc4fec08f
johnnyg@chromium.org <johnnyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-10-29 03:36:26 +0000

    DISABLED, not DISABLE
    TBR=eroman
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@107862 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8cc9738eff9c169487e8356947d88640659fc497

--- chrome/browser/ui/webui/net_internals_ui_browsertest.cc
+++ chrome/browser/ui/webui/net_internals_ui_browsertest.cc
@@ -277,22 +277,22 @@ IN_PROC_BROWSER_TEST_F(NetInternalsTest, NetInternalsExportImportDump) {
 // TODO http://crbug.com/102227.  These tests timeout around webkit r98738.
 
 // Adds a successful lookup to the DNS cache, then clears the cache.
-IN_PROC_BROWSER_TEST_F(NetInternalsTest, DISABLE_NetInternalsDnsViewSuccess) {
+IN_PROC_BROWSER_TEST_F(NetInternalsTest, DISABLED_NetInternalsDnsViewSuccess) {
   EXPECT_TRUE(RunJavascriptAsyncTest("netInternalsDnsViewSuccess"));
 }
 
 // Adds a failed lookup to the DNS cache, then clears the cache.
-IN_PROC_BROWSER_TEST_F(NetInternalsTest, DISABLE_NetInternalsDnsViewFail) {
+IN_PROC_BROWSER_TEST_F(NetInternalsTest, DISABLED_NetInternalsDnsViewFail) {
   EXPECT_TRUE(RunJavascriptAsyncTest("netInternalsDnsViewFail"));
 }
 
 // Adds an expired successful lookup to the DNS cache, then clears the cache.
-IN_PROC_BROWSER_TEST_F(NetInternalsTest, DISABLE_NetInternalsDnsViewExpired) {
+IN_PROC_BROWSER_TEST_F(NetInternalsTest, DISABLED_NetInternalsDnsViewExpired) {
   EXPECT_TRUE(RunJavascriptAsyncTest("netInternalsDnsViewExpired"));
 }
 
 // Adds two entries to the DNS cache, clears the cache, and then repeats.
-IN_PROC_BROWSER_TEST_F(NetInternalsTest, DISABLE_NetInternalsDnsViewAddTwoTwice) {
+IN_PROC_BROWSER_TEST_F(NetInternalsTest, DISABLED_NetInternalsDnsViewAddTwoTwice) {
   EXPECT_TRUE(RunJavascriptAsyncTest("netInternalsDnsViewAddTwoTwice"));
 }
 
