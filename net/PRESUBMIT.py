a16591be746ca3ed3389a618bf61e1a5848a10f4
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-02 23:53:17 +0000

    Automatically send try jobs in linux_redux if NSS files are touched
    BUG=None
    TEST=git cl try on this CL goes to linux_redux too
    
    
    Review URL: http://codereview.chromium.org/8430030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108365 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8c2a2d5be02d9c2720441011256b0267cd8f323b

--- net/PRESUBMIT.py
+++ net/PRESUBMIT.py
@@ -9,5 +9,10 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details on the presubmit API built into gcl.
 """
 
-def GetPreferredTrySlaves():
-  return ['win', 'linux', 'mac', 'win_sync', 'linux_sync', 'mac_sync']
+def GetPreferredTrySlaves(project, change):
+  slaves = ['win', 'linux', 'mac', 'win_sync', 'linux_sync', 'mac_sync']
+  # Changes that touch NSS files will likely need a corresponding OpenSSL edit.
+  # Conveniently, this one glob also matches _openssl.* changes too.
+  if any('nss' in f.LocalPath() for f in change.AffectedFiles()):
+    slaves.append('linux_redux')
+  return slaves
