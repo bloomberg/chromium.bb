3a2c3a132f13f83e2eb7df26887d4c438e23b496
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-02 21:25:00 +0000

    Revert r107440 because this causes a crash on touchui on page load.
    The CL being reverted:
    This patch lets the browser window punch a hole in the DesktopBackgroundView,
    thus eliminating overdraw.
    To do this, the hole punching logic was changed to look at the entire tree.
    Drawing the screen is a preorder traversal through all the layers. Thus, to
    compute the hole, we have to look at all of the layers after a particular layer
    in the preorder traversal.
    This is not an efficient algorithm, but it is simplest I could come up with.
    Plus we will be moving to the WebKit compositor soon.
    
    Hopefully this will improve the frame rate for the Aura demos
    
    Note: I will put the changes to Transform into another patch.
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=107360
    
    Review URL: http://codereview.chromium.org/8368013
    
    TBR=pkotwicz@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108347 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 02876f5fdafb3b6e500ba322715c3a848170763a

--- views/view_unittest.cc
+++ views/view_unittest.cc
@@ -2835,15 +2835,11 @@ TEST_F(ViewLayerTest, ToggleOpacityWithLayer) {
   ASSERT_TRUE(child_view->layer() == NULL);
   child_view->SetPaintToLayer(true);
   child_view->SetFillsBoundsOpaquely(true);
-
-  widget()->GetCompositor()->Draw(false);
   ASSERT_TRUE(child_view->layer());
   EXPECT_EQ(
       gfx::Rect(50, 50, 100, 100), parent_view->layer()->hole_rect());
 
   child_view->SetFillsBoundsOpaquely(false);
-
-  widget()->GetCompositor()->Draw(false);
   EXPECT_TRUE(parent_view->layer()->hole_rect().IsEmpty());
 }
 
@@ -2870,14 +2866,12 @@ TEST_F(ViewLayerTest, MultipleOpaqueLayers) {
   child_view2->SetBounds(150, 150, 200, 200);
   parent_view->AddChildView(child_view2);
 
-  widget()->GetCompositor()->Draw(false);
   // Only child_view1 is opaque
   EXPECT_EQ(
       gfx::Rect(50, 50, 100, 100), parent_view->layer()->hole_rect());
 
   // Both child views are opaque
   child_view2->SetFillsBoundsOpaquely(true);
-  widget()->GetCompositor()->Draw(false);
   EXPECT_TRUE(
       gfx::Rect(50, 50, 100, 100) == parent_view->layer()->hole_rect() ||
       gfx::Rect(150, 150, 200, 200) == parent_view->layer()->hole_rect());
@@ -2906,17 +2900,13 @@ TEST_F(ViewLayerTest, ToggleVisibilityWithOpaqueLayer) {
   child_view->SetPaintToLayer(true);
   child_view->SetFillsBoundsOpaquely(true);
   parent_view->AddChildView(child_view);
-
-  widget()->GetCompositor()->Draw(false);
   EXPECT_EQ(
        gfx::Rect(50, 50, 100, 100), parent_view->layer()->hole_rect());
 
   child_view->SetVisible(false);
-  widget()->GetCompositor()->Draw(false);
   EXPECT_TRUE(parent_view->layer()->hole_rect().IsEmpty());
 
   child_view->SetVisible(true);
-  widget()->GetCompositor()->Draw(false);
   EXPECT_EQ(
       gfx::Rect(50, 50, 100, 100), parent_view->layer()->hole_rect());
 }
@@ -2933,7 +2923,6 @@ TEST_F(ViewLayerTest, OrphanLayerAfterViewRemove) {
   View* v2 = new View;
   v1->AddChildView(v2);
   v2->SetPaintToLayer(true);
-
   EXPECT_TRUE(LayerIsAncestor(widget()->GetCompositor()->root_layer(),
                               v2->layer()));
   EXPECT_TRUE(v2->layer()->IsDrawn());
