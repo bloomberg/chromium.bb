97689d978f6e98d2a09bcc8a406e1c2ef455a369
scr@chromium.org <scr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-03 01:25:29 +0000

    Allow javascript unit tests using webui test_api framework.
    I moved javascript2webui.js over to chrome/test/base/js2gtest.js and shared it
    across webui and unit tests with an extra parameter for test type, which governs
    the few differences, such as what to include/subclass from and the flavor of
    gtest (TEST_F v. IN_PROC_BROWSER_TEST_F).
    
    v8_unit_test implemented 2 main methods to make it work with the generated C++
    - AddLibrary - loads the file in the test context
    - RunJavascriptF - launches the runTest with the testFixture and testName,
    coordinating with Error and ChromeSend to report results.
    
    Helper functions:
    - Error - watches for console.error, noting failure if seen.
    - ChromeSend - pulls apart the result from the test_api's call to
    chrome.send('testResult', [ok, msg])
    
    R=arv@chromium.org
    BUG=87820
    TEST=unit_tests --gtest_filter=FrameworkUnitTest.*
    
    
    Review URL: http://codereview.chromium.org/8418015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108391 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 369d25ca02211727361c11af0b8e8db2b234d421

--- chrome/test/base/js2gtest.js
+++ chrome/test/base/js2gtest.js
@@ -1,21 +1,33 @@
 // Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-if (arguments.length < 3) {
+if (arguments.length < 4) {
   print('usage: ' +
-        arguments[0] + ' path-to-testfile.js testfile.js [output.cc]');
+        arguments[0] + ' path-to-testfile.js testfile.js output.cc test-type');
   quit(-1);
 }
 var jsFile = arguments[1];
 var jsFileBase = arguments[2];
 var outputFile = arguments[3];
+var testType = arguments[4];
 
 // Generate the file to stdout.
 print('// GENERATED FILE');
 print('// ' + arguments.join(' '));
 print('// PLEASE DO NOT HAND EDIT!');
 print();
-print('#include "chrome/browser/ui/webui/web_ui_browsertest.h"');
+
+var testF;
+
+if (testType === 'unit') {
+  print('#include "chrome/test/base/v8_unit_test.h"');
+  testing.Test.prototype.typedefCppFixture = 'V8UnitTest';
+  testF = 'TEST_F';
+} else {
+  print('#include "chrome/browser/ui/webui/web_ui_browsertest.h"');
+  testing.Test.prototype.typedefCppFixture = 'WebUIBrowserTest';
+  testF = 'IN_PROC_BROWSER_TEST_F';
+}
 print('#include "googleurl/src/gurl.h"');
 print('#include "testing/gtest/include/gtest/gtest.h"');
 print();
@@ -32,7 +44,8 @@ function TEST_F(testFixture, testFunction, testBody) {
   var testGenPreamble = this[testFixture].prototype.testGenPreamble;
   var testGenPostamble = this[testFixture].prototype.testGenPostamble;
   var typedefCppFixture = this[testFixture].prototype.typedefCppFixture;
-  var isAsync = this[testFixture].prototype.isAsync;
+  var isAsyncParam = testType === 'unit' ? '' :
+      this[testFixture].prototype.isAsync + ', ';
   var testShouldFail = this[testFixture].prototype.testShouldFail;
   var testPredicate = testShouldFail ? 'ASSERT_FALSE' : 'ASSERT_TRUE';
 
@@ -41,7 +54,7 @@ function TEST_F(testFixture, testFunction, testBody) {
     typedeffedCppFixtures[testFixture] = typedefCppFixture;
   }
 
-  print('IN_PROC_BROWSER_TEST_F(' + testFixture + ', ' + testFunction + ') {');
+  print(testF + '(' + testFixture + ', ' + testFunction + ') {');
   if (testGenPreamble)
     testGenPreamble(testFixture, testFunction);
   print('  AddLibrary(FilePath(FILE_PATH_LITERAL("' +
@@ -55,7 +68,7 @@ function TEST_F(testFixture, testFunction, testBody) {
           '      FILE_PATH_LITERAL("' + browsePrintPreload + '"))),\n' +
           '      "' + testFixture + '", "' + testFunction + '");');
   }
-  print('  ' + testPredicate + '(RunJavascriptTestF(' + isAsync + ', ' +
+  print('  ' + testPredicate + '(RunJavascriptTestF(' + isAsyncParam +
         '"' + testFixture + '", ' +
         '"' + testFunction + '"));');
   if (testGenPostamble)
