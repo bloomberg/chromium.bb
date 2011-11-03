bloomberg/chromium.bb

commit 3e57376d829dc6c8523f8d02cbd6a89030d7a4f7

Refactor loading out of ExtensionService.
- Make ExtensionServiceBackend go away, as  it's only used for installing unpacked extensions. Tear this out into UnpackedInstaller task.
- Pull out LoadAllExtensions / LoadInstalledExtension into InstalledExtensionLoader task.
- Break out component extensions to be handled by ComponentLoader.

Coming soon: a unit test for InstalledExtensionLoader.

BUG=97883
TEST=existing tests + InstalledExtensionLoaderUnittest


Review URL: http://codereview.chromium.org/8417012

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108319 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: d8c8f25f706a2f565ced667ddd90ff3f1c831a6d

chrome/browser/extensions/component_loader.cc
@@ -1,0 +1,224 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/component_loader.h"
+
+#include "base/command_line.h"
+#include "base/path_service.h"
+#include "base/json/json_value_serializer.h"
+#include "chrome/browser/browser_process.h"
+#include "chrome/browser/extensions/extension_service.h"
+#include "chrome/common/chrome_paths.h"
+#include "chrome/common/chrome_switches.h"
+#include "chrome/common/extensions/extension.h"
+#include "chrome/common/pref_names.h"
+#include "grit/browser_resources.h"
+#include "ui/base/resource/resource_bundle.h"
+
+namespace {
+
+typedef std::list<std::pair<FilePath::StringType, int> >
+    ComponentExtensionList;
+
+#if defined(FILE_MANAGER_EXTENSION)
+void AddFileManagerExtension(ComponentExtensionList* component_extensions) {
+#ifndef NDEBUG
+  const CommandLine* command_line = CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kFileManagerExtensionPath)) {
+    FilePath filemgr_extension_path =
+        command_line->GetSwitchValuePath(switches::kFileManagerExtensionPath);
+    component_extensions->push_back(std::make_pair(
+        filemgr_extension_path.value(),
+        IDR_FILEMANAGER_MANIFEST));
+    return;
+  }
+#endif  // NDEBUG
+  component_extensions->push_back(std::make_pair(
+      FILE_PATH_LITERAL("file_manager"),
+      IDR_FILEMANAGER_MANIFEST));
+}
+#endif  // defined(FILE_MANAGER_EXTENSION)
+
+}  // namespace
+
+namespace extensions {
+
+bool ComponentLoader::ComponentExtensionInfo::Equals(
+    const ComponentExtensionInfo& other) const {
+  return other.manifest == manifest && other.root_directory == root_directory;
+}
+
+ComponentLoader::ComponentLoader(ExtensionService* extension_service)
+    : extension_service_(extension_service) {
+}
+
+ComponentLoader::~ComponentLoader() {
+}
+
+void ComponentLoader::LoadAll() {
+  for (RegisteredComponentExtensions::iterator it =
+           component_extensions_.begin();
+       it != component_extensions_.end(); ++it) {
+    Load(*it);
+  }
+}
+
+const Extension* ComponentLoader::Add(
+    const std::string& manifest, const FilePath& root_directory) {
+  ComponentExtensionInfo info(manifest, root_directory);
+  Register(info);
+  if (extension_service_->is_ready())
+    return Load(info);
+  return NULL;
+}
+
+const Extension* ComponentLoader::Load(const ComponentExtensionInfo& info) {
+  JSONStringValueSerializer serializer(info.manifest);
+  scoped_ptr<Value> manifest(serializer.Deserialize(NULL, NULL));
+  if (!manifest.get()) {
+    LOG(ERROR) << "Failed to parse manifest for extension";
+    return NULL;
+  }
+
+  int flags = Extension::REQUIRE_KEY;
+  if (Extension::ShouldDoStrictErrorChecking(Extension::COMPONENT))
+    flags |= Extension::STRICT_ERROR_CHECKS;
+  std::string error;
+  scoped_refptr<const Extension> extension(Extension::Create(
+      info.root_directory,
+      Extension::COMPONENT,
+      *static_cast<DictionaryValue*>(manifest.get()),
+      flags,
+      &error));
+  if (!extension.get()) {
+    LOG(ERROR) << error;
+    return NULL;
+  }
+  extension_service_->AddExtension(extension);
+  return extension;
+}
+
+void ComponentLoader::Remove(const std::string& manifest_str) {
+  // Unload the extension.
+  JSONStringValueSerializer serializer(manifest_str);
+  scoped_ptr<Value> manifest(serializer.Deserialize(NULL, NULL));
+  if (!manifest.get()) {
+    LOG(ERROR) << "Failed to parse manifest for extension";
+    return;
+  }
+  std::string public_key;
+  std::string public_key_bytes;
+  std::string id;
+  if (!static_cast<DictionaryValue*>(manifest.get())->
+      GetString(extension_manifest_keys::kPublicKey, &public_key) ||
+      !Extension::ParsePEMKeyBytes(public_key, &public_key_bytes) ||
+      !Extension::GenerateId(public_key_bytes, &id)) {
+    LOG(ERROR) << "Failed to get extension id";
+    return;
+  }
+  extension_service_->
+      UnloadExtension(id, extension_misc::UNLOAD_REASON_DISABLE);
+
+  // Unregister the extension.
+  RegisteredComponentExtensions new_component_extensions;
+  for (RegisteredComponentExtensions::iterator it =
+           component_extensions_.begin();
+       it != component_extensions_.end(); ++it) {
+    if (it->manifest != manifest_str)
+      new_component_extensions.push_back(*it);
+  }
+  component_extensions_.swap(new_component_extensions);
+}
+
+// We take ComponentExtensionList:
+// path, manifest ID => full manifest, absolute path
+void ComponentLoader::AddDefaultComponentExtensions() {
+  ComponentExtensionList component_extensions;
+
+  // Bookmark manager.
+  component_extensions.push_back(std::make_pair(
+      FILE_PATH_LITERAL("bookmark_manager"),
+      IDR_BOOKMARKS_MANIFEST));
+
+#if defined(FILE_MANAGER_EXTENSION)
+  AddFileManagerExtension(&component_extensions);
+#endif
+
+#if defined(TOUCH_UI)
+  component_extensions.push_back(std::make_pair(
+      FILE_PATH_LITERAL("keyboard"),
+      IDR_KEYBOARD_MANIFEST));
+#endif
+
+#if defined(OS_CHROMEOS)
+    component_extensions.push_back(std::make_pair(
+        FILE_PATH_LITERAL("/usr/share/chromeos-assets/mobile"),
+        IDR_MOBILE_MANIFEST));
+
+    const CommandLine* command_line = CommandLine::ForCurrentProcess();
+    if (command_line->HasSwitch(switches::kAuthExtensionPath)) {
+      FilePath auth_extension_path =
+          command_line->GetSwitchValuePath(switches::kAuthExtensionPath);
+      component_extensions.push_back(std::make_pair(
+          auth_extension_path.value(),
+          IDR_GAIA_TEST_AUTH_MANIFEST));
+    } else {
+      component_extensions.push_back(std::make_pair(
+          FILE_PATH_LITERAL("/usr/share/chromeos-assets/gaia_auth"),
+          IDR_GAIA_AUTH_MANIFEST));
+    }
+
+#if defined(OFFICIAL_BUILD)
+    if (browser_defaults::enable_help_app) {
+      component_extensions.push_back(std::make_pair(
+          FILE_PATH_LITERAL("/usr/share/chromeos-assets/helpapp"),
+          IDR_HELP_MANIFEST));
+    }
+#endif
+#endif
+
+  // Web Store.
+  component_extensions.push_back(std::make_pair(
+      FILE_PATH_LITERAL("web_store"),
+      IDR_WEBSTORE_MANIFEST));
+
+#if !defined(OS_CHROMEOS)
+  // Cloud Print component app. Not required on Chrome OS.
+  component_extensions.push_back(std::make_pair(
+      FILE_PATH_LITERAL("cloud_print"),
+      IDR_CLOUDPRINT_MANIFEST));
+#endif  // !defined(OS_CHROMEOS)
+
+  for (ComponentExtensionList::iterator iter = component_extensions.begin();
+    iter != component_extensions.end(); ++iter) {
+    FilePath path(iter->first);
+    if (!path.IsAbsolute()) {
+      if (PathService::Get(chrome::DIR_RESOURCES, &path)) {
+        path = path.Append(iter->first);
+      } else {
+        NOTREACHED();
+      }
+    }
+
+    std::string manifest =
+        ResourceBundle::GetSharedInstance().GetRawDataResource(
+            iter->second).as_string();
+    Add(manifest, path);
+  }
+
+#if defined(OS_CHROMEOS)
+  // Register access extensions only if accessibility is enabled.
+  if (g_browser_process->local_state()->
+      GetBoolean(prefs::kAccessibilityEnabled)) {
+    FilePath path = FilePath(extension_misc::kAccessExtensionPath)
+        .AppendASCII(extension_misc::kChromeVoxDirectoryName);
+    std::string manifest =
+        ResourceBundle::GetSharedInstance().GetRawDataResource(
+            IDR_CHROMEVOX_MANIFEST).as_string();
+    Add(manifest, path);
+  }
+#endif
+}
+
+}  // namespace extensions

chrome/browser/autocomplete/autocomplete_browsertest.cc
@@ -12,6 +12,7 @@
 #include "chrome/browser/autocomplete/autocomplete_popup_model.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/extensions/unpacked_installer.h"
 #include "chrome/browser/history/history.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/tabs/tab_strip_model.h"
@@ -264,12 +265,12 @@ IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, ExtensionAppProvider) {
   FilePath test_dir;
   ASSERT_TRUE(PathService::Get(chrome::DIR_TEST_DATA, &test_dir));
   // Load a packaged app.
-  service->LoadExtension(test_dir.AppendASCII("extensions")
-                                 .AppendASCII("packaged_app"));
+  extensions::UnpackedInstaller::Create(service)->Load(
+      test_dir.AppendASCII("extensions").AppendASCII("packaged_app"));
   WaitForExtensionLoad();
   // Load a hosted app.
-  service->LoadExtension(test_dir.AppendASCII("extensions")
-                                 .AppendASCII("app"));
+  extensions::UnpackedInstaller::Create(service)->Load(
+      test_dir.AppendASCII("extensions").AppendASCII("app"));
   WaitForExtensionLoad();
   ASSERT_EQ(extension_count + 2U, service->extensions()->size());
 

chrome/browser/automation/automation_provider.cc
@@ -50,6 +50,7 @@
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_tab_util.h"
 #include "chrome/browser/extensions/extension_toolbar_model.h"
+#include "chrome/browser/extensions/unpacked_installer.h"
 #include "chrome/browser/extensions/user_script_master.h"
 #include "chrome/browser/net/url_request_mock_util.h"
 #include "chrome/browser/prefs/pref_service.h"
@@ -858,13 +859,17 @@ void AutomationProvider::InstallExtension(
     if (extension_path.MatchesExtension(FILE_PATH_LITERAL(".crx"))) {
       ExtensionInstallUI* client =
           (with_ui ? new ExtensionInstallUI(profile_) : NULL);
-      scoped_refptr<CrxInstaller> installer(service->MakeCrxInstaller(client));
+      scoped_refptr<CrxInstaller> installer(
+          CrxInstaller::Create(service, client));
       if (!with_ui)
         installer->set_allow_silent_install(true);
       installer->set_install_cause(extension_misc::INSTALL_CAUSE_AUTOMATION);
       installer->InstallCrx(extension_path);
     } else {
-      service->LoadExtension(extension_path, with_ui);
+      scoped_refptr<extensions::UnpackedInstaller> installer(
+          extensions::UnpackedInstaller::Create(service));
+      installer->set_prompt_for_plugins(with_ui);
+      installer->Load(extension_path);
     }
   } else {
     AutomationMsg_InstallExtension::WriteReplyParams(reply_message, 0);

chrome/browser/chromeos/accessibility_util.cc
@@ -10,6 +10,7 @@
 #include "chrome/browser/chromeos/dbus/dbus_thread_manager.h"
 #include "chrome/browser/chromeos/dbus/speech_synthesizer_client.h"
 #include "chrome/browser/extensions/extension_accessibility_api.h"
+#include "chrome/browser/extensions/component_loader.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/file_reader.h"
 #include "chrome/browser/profiles/profile.h"
@@ -110,11 +111,9 @@ void EnableAccessibility(bool enabled, WebUI* login_web_ui) {
       GetRawDataResource(IDR_CHROMEVOX_MANIFEST).as_string();
   FilePath path = FilePath(extension_misc::kAccessExtensionPath)
       .AppendASCII(extension_misc::kChromeVoxDirectoryName);
-  ExtensionService::ComponentExtensionInfo info(manifest, path);
   if (enabled) { // Load ChromeVox
-    extension_service->register_component_extension(info);
     const Extension* extension =
-        extension_service->LoadComponentExtension(info);
+        extension_service->component_loader()->Add(manifest, path);
 
     if (login_web_ui) {
       RenderViewHost* render_view_host =
@@ -136,8 +135,7 @@ void EnableAccessibility(bool enabled, WebUI* login_web_ui) {
 
     LOG(INFO) << "ChromeVox was Loaded.";
   } else { // Unload ChromeVox
-    extension_service->UnloadComponentExtension(info);
-    extension_service->UnregisterComponentExtension(info);
+    extension_service->component_loader()->Remove(manifest);
     LOG(INFO) << "ChromeVox was Unloaded.";
   }
 }

chrome/browser/debugger/devtools_sanity_unittest.cc
@@ -12,6 +12,7 @@
 #include "chrome/browser/debugger/devtools_window.h"
 #include "chrome/browser/extensions/extension_host.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/extensions/unpacked_installer.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/common/chrome_notification_types.h"
@@ -211,7 +212,7 @@ class DevToolsExtensionDebugTest : public DevToolsSanityTest,
           new CancelableQuitTask("Extension load timed out.");
       MessageLoop::current()->PostDelayedTask(FROM_HERE, delayed_quit,
           4*1000);
-      service->LoadExtension(path);
+      extensions::UnpackedInstaller::Create(service)->Load(path);
       ui_test_utils::RunMessageLoop();
       delayed_quit->cancel();
     }

chrome/browser/download/download_crx_util.cc
@@ -57,7 +57,7 @@ scoped_refptr<CrxInstaller> OpenChromeExtension(
   CHECK(service);
 
   scoped_refptr<CrxInstaller> installer(
-      service->MakeCrxInstaller(CreateExtensionInstallUI(profile)));
+      CrxInstaller::Create(service, CreateExtensionInstallUI(profile)));
   installer->set_delete_source(true);
 
   if (UserScript::IsURLUserScript(download_item.GetURL(),

chrome/browser/extensions/component_loader.h
@@ -1,0 +1,83 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EXTENSIONS_COMPONENT_LOADER_H_
+#define CHROME_BROWSER_EXTENSIONS_COMPONENT_LOADER_H_
+#pragma once
+
+#include <string>
+
+#include "base/file_path.h"
+
+class Extension;
+class ExtensionService;
+
+namespace extensions {
+
+// For registering, loading, and unloading component extensions.
+class ComponentLoader {
+ public:
+  explicit ComponentLoader(ExtensionService* extension_service);
+  virtual ~ComponentLoader();
+
+  // Loads any registered component extensions.
+  void LoadAll();
+
+  // Loads and registers a component extension. If ExtensionService has been
+  // initialized, the extension is loaded; otherwise, the load is deferred
+  // until LoadAll is called.
+  const Extension* Add(const std::string& manifest,
+                       const FilePath& root_directory);
+
+  // Unloads a component extension and removes it from the list of component
+  // extensions to be loaded.
+  void Remove(const std::string& manifest_str);
+
+  // Adds the default component extensions.
+  //
+  // Component extension manifests must contain a 'key' property with a unique
+  // public key, serialized in base64. You can create a suitable value with the
+  // following commands on a unixy system:
+  //
+  //   ssh-keygen -t rsa -b 1024 -N '' -f /tmp/key.pem
+  //   openssl rsa -pubout -outform DER < /tmp/key.pem 2>/dev/null | base64 -w 0
+  void AddDefaultComponentExtensions();
+
+ private:
+  // Information about a registered component extension.
+  struct ComponentExtensionInfo {
+    ComponentExtensionInfo(const std::string& manifest,
+                           const FilePath& root_directory)
+        : manifest(manifest),
+          root_directory(root_directory) {
+    }
+
+    bool Equals(const ComponentExtensionInfo& other) const;
+
+    // The extension's manifest. This is required for component extensions so
+    // that ExtensionService doesn't need to go to disk to load them.
+    std::string manifest;
+
+    // Directory where the extension is stored.
+    FilePath root_directory;
+  };
+
+  // Loads a registered component extension.
+  const Extension* Load(const ComponentExtensionInfo& info);
+
+  // Registers an extension to be loaded as a component extension.
+  void Register(const ComponentExtensionInfo& info) {
+    component_extensions_.push_back(info);
+  }
+
+  // List of registered component extensions (see Extension::Location).
+  typedef std::vector<ComponentExtensionInfo> RegisteredComponentExtensions;
+  RegisteredComponentExtensions component_extensions_;
+
+  ExtensionService* extension_service_;
+};
+
+}  // namespace extensions
+
+#endif  // CHROME_BROWSER_EXTENSIONS_COMPONENT_LOADER_H_

chrome/browser/extensions/crx_installer.cc
@@ -62,6 +62,13 @@ CrxInstaller::WhitelistEntry::WhitelistEntry()
 CrxInstaller::WhitelistEntry::~WhitelistEntry() {}
 
 // static
+scoped_refptr<CrxInstaller> CrxInstaller::Create(
+    ExtensionService* frontend,
+    ExtensionInstallUI* client) {
+  return new CrxInstaller(frontend->AsWeakPtr(), client);
+}
+
+// static
 void CrxInstaller::SetWhitelistedInstallId(const std::string& id) {
   CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   g_whitelisted_install_data.Get().ids.insert(id);

commit 054ebba256a16158c385ccd04b5fa7ef233cd129

Add a missing header for official build to fix compile error.
Regression happened in r108319

TBR=aa@chromium.org
BUG=none
TEST=none

Review URL: http://codereview.chromium.org/8437087

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108460 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: a9f9f31a70c92f82ab804e4e652eaf482bbe75d9

chrome/browser/extensions/component_loader.cc
@@ -16,6 +16,10 @@
 #include "grit/browser_resources.h"
 #include "ui/base/resource/resource_bundle.h"
 
+#if defined(OFFICIAL_BUILD)
+#include "chrome/browser/defaults.h"
+#endif
+
 namespace {
 
 typedef std::list<std::pair<FilePath::StringType, int> >