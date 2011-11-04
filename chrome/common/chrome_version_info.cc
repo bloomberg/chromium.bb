20efe10c0133badb522f9ebc58effd0d0c7cb666
jrg@chromium.org <jrg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-04 02:28:51 +0000

    Use UTF16ToUTF8 instead of UTF16ToASCII for version strings.
    BUG=102478
    
    TEST=about:version still works
    
    
    Review URL: http://codereview.chromium.org/8351082
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108617 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e5fdc57cf8c8892e1d73bb1ecd28b49bc9829964

--- chrome/common/chrome_version_info.cc
+++ chrome/common/chrome_version_info.cc
@@ -8,6 +8,7 @@
 #include "base/file_version_info.h"
 #include "base/string_util.h"
 #include "base/threading/thread_restrictions.h"
+#include "base/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
@@ -35,19 +36,19 @@ bool VersionInfo::is_valid() const {
 std::string VersionInfo::Name() const {
   if (!is_valid())
     return std::string();
-  return UTF16ToASCII(version_info_->product_name());
+  return UTF16ToUTF8(version_info_->product_name());
 }
 
 std::string VersionInfo::Version() const {
   if (!is_valid())
     return std::string();
-  return UTF16ToASCII(version_info_->product_version());
+  return UTF16ToUTF8(version_info_->product_version());
 }
 
 std::string VersionInfo::LastChange() const {
   if (!is_valid())
     return std::string();
-  return UTF16ToASCII(version_info_->last_change());
+  return UTF16ToUTF8(version_info_->last_change());
 }
 
 bool VersionInfo::IsOfficialBuild() const {
