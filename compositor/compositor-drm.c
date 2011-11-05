63ff706c0e00f9e583f5c8a4b220981772a62658
David Herrmann <dh.herrmann@googlemail.com>
2011-11-05 18:46:01 +0100

    compositor-drm: Fix setting O_CLOEXEC
    The third parameter of open() is for file-creation modes. File flags are passed
    in the second paramater.
    
    Signed-off-by: David Herrmann <dh.herrmann@googlemail.com>

--- compositor/compositor-drm.c
+++ compositor/compositor-drm.c
@@ -322,7 +322,7 @@ init_egl(struct drm_compositor *ec, struct udev_device *device)
 	};
 
 	filename = udev_device_get_devnode(device);
-	fd = open(filename, O_RDWR, O_CLOEXEC);
+	fd = open(filename, O_RDWR | O_CLOEXEC);
 	if (fd < 0) {
 		/* Probably permissions error */
 		fprintf(stderr, "couldn't open %s, skipping\n",
