d5a85742f5f4b6109eaffc0d4e4428e7c9e56430
yurys@chromium.org <yurys@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-08 09:12:51 +0000

    Mark WorkersUITest.SharedWorkersList as flaky on Linux and Win, not disable it
    BUG=89583
    TEST=WorkersUITest.SharedWorkersList
    TBR=glotov
    
    Review URL: http://codereview.chromium.org/8494010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@108992 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 615ac9472d36d55a24fdf5ab2467ac8f9e5e689e

--- chrome/browser/ui/webui/workers_ui_browsertest.cc
+++ chrome/browser/ui/webui/workers_ui_browsertest.cc
@@ -27,9 +27,13 @@ class WorkersUITest : public InProcessBrowserTest {
   DISALLOW_COPY_AND_ASSIGN(WorkersUITest);
 };
 
-// The test fails on Win Interactive Tests (dbg),
-// Linux Tests (dbg)(2) and Mac OS X, see crbug.com/89583
-IN_PROC_BROWSER_TEST_F(WorkersUITest, DISABLED_SharedWorkersList) {
+// The test fails on Mac OS X, see crbug.com/89583
+#if defined(OS_MACOSX)
+#define MAYBE_SharedWorkersList DISABLED_SharedWorkersList
+#else
+#define MAYBE_SharedWorkersList FLAKY_SharedWorkersList
+#endif
+IN_PROC_BROWSER_TEST_F(WorkersUITest, MAYBE_SharedWorkersList) {
   ASSERT_TRUE(test_server()->Start());
   GURL url = test_server()->GetURL(kSharedWorkerTestPage);
   ui_test_utils::NavigateToURL(browser(), url);
