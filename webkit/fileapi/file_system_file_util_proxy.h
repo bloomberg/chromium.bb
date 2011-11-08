2ec3f15e4487ed9a4fea1c31c16417a6e64bc494
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-08 11:25:34 +0000

    Merge FileUtilProxy and FileSystemFileUtilProxy using PostTaskAndReply: Delete/Touch/Truncate/Copy/Move
    BUG=none
    TEST=test_shell_tests:FileSystem*
    
    Review URL: http://codereview.chromium.org/8424007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109010 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 162d1e42a7856e7707f850fca8a4561c20ab6930

--- webkit/fileapi/file_system_file_util_proxy.h
+++ webkit/fileapi/file_system_file_util_proxy.h
@@ -35,7 +35,6 @@ class FileSystemFileUtilProxy {
  public:
   typedef base::FileUtilProxy::Entry Entry;
 
-  typedef base::FileUtilProxy::StatusCallback StatusCallback;
   typedef base::Callback<void(PlatformFileError,
                               bool /* created */
                               )> EnsureFileExistsCallback;
@@ -46,10 +45,6 @@ class FileSystemFileUtilProxy {
   typedef base::Callback<void(PlatformFileError,
                               const std::vector<Entry>&
                               )> ReadDirectoryCallback;
-
-  typedef Callback2<PlatformFileError /* error code */,
-                    const FilePath& /* local_path, where possible */
-                    >::Type GetLocalPathCallback;
 
   // Ensures that the given |file_path| exist.  This creates a empty new file
   // at |file_path| if the |file_path| does not exist.
@@ -66,13 +61,6 @@ class FileSystemFileUtilProxy {
       const FilePath& file_path,
       const EnsureFileExistsCallback& callback);
 
-  // Maps virtual file patch to its local physical location.
-  static bool GetLocalPath(
-      const FileSystemOperationContext& context,
-      scoped_refptr<MessageLoopProxy> message_loop_proxy,
-      const FilePath& virtual_path,
-      GetLocalPathCallback* callback);
-
   // Retrieves the information about a file. It is invalid to pass NULL for the
   // callback.
   static bool GetFileInfo(
@@ -85,66 +73,6 @@ class FileSystemFileUtilProxy {
                             scoped_refptr<MessageLoopProxy> message_loop_proxy,
                             const FilePath& file_path,
                             const ReadDirectoryCallback& callback);
-
-  // Creates directory at given path. It's an error to create
-  // if |exclusive| is true and dir already exists.
-  static bool CreateDirectory(
-      const FileSystemOperationContext& context,
-      scoped_refptr<MessageLoopProxy> message_loop_proxy,
-      const FilePath& file_path,
-      bool exclusive,
-      bool recursive,
-      const StatusCallback& callback);
-
-  // Copies a file or a directory from |src_file_path| to |dest_file_path|
-  // Error cases:
-  // If destination file doesn't exist or destination's parent
-  // doesn't exists.
-  // If source dir exists but destination path is an existing file.
-  // If source file exists but destination path is an existing directory.
-  // If source is a parent of destination.
-  // If source doesn't exists.
-  static bool Copy(const FileSystemOperationContext& context,
-                   scoped_refptr<MessageLoopProxy> message_loop_proxy,
-                   const FilePath& src_file_path,
-                   const FilePath& dest_file_path,
-                   const StatusCallback& callback);
-
-  // Moves a file or a directory from src_file_path to dest_file_path.
-  // Error cases are similar to Copy method's error cases.
-  static bool Move(
-      const FileSystemOperationContext& context,
-      scoped_refptr<MessageLoopProxy> message_loop_proxy,
-      const FilePath& src_file_path,
-      const FilePath& dest_file_path,
-      const StatusCallback& callback);
-
-  // Deletes a file or a directory.
-  // It is an error to delete a non-empty directory with recursive=false.
-  static bool Delete(const FileSystemOperationContext& context,
-                     scoped_refptr<MessageLoopProxy> message_loop_proxy,
-                     const FilePath& file_path,
-                     bool recursive,
-                     const StatusCallback& callback);
-
-  // Touches a file. The callback can be NULL.
-  static bool Touch(
-      const FileSystemOperationContext& context,
-      scoped_refptr<MessageLoopProxy> message_loop_proxy,
-      const FilePath& file_path,
-      const base::Time& last_access_time,
-      const base::Time& last_modified_time,
-      const StatusCallback& callback);
-
-  // Truncates a file to the given length. If |length| is greater than the
-  // current length of the file, the file will be extended with zeroes.
-  // The callback can be NULL.
-  static bool Truncate(
-      const FileSystemOperationContext& context,
-      scoped_refptr<MessageLoopProxy> message_loop_proxy,
-      const FilePath& path,
-      int64 length,
-      const StatusCallback& callback);
 
  private:
   DISALLOW_IMPLICIT_CONSTRUCTORS(FileSystemFileUtilProxy);
