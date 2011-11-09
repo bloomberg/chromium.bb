6a7e6bc61ba52c5a88612e623f89aa771e7c6f80
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 01:06:36 +0000

    Revert 109086 - Add private Pepper API for dealing with PP_Flash_NetAddress.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/8357030
    
    TBR=viettrungluu@chromium.org
    Review URL: http://codereview.chromium.org/8499045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109153 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3a1ba433272ed041e4b0d26e262272e3da600ede

--- ppapi/c/private/ppb_flash_udp_socket.h
+++ ppapi/c/private/ppb_flash_udp_socket.h
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-/* From private/ppb_flash_udp_socket.idl modified Mon Oct 17 15:55:01 2011. */
+/* From ppb_flash_udp_socket.idl modified Mon Sep 26 09:04:41 2011. */
 
 #ifndef PPAPI_C_PRIVATE_PPB_FLASH_UDP_SOCKET_H_
 #define PPAPI_C_PRIVATE_PPB_FLASH_UDP_SOCKET_H_
@@ -14,7 +14,7 @@
 #include "ppapi/c/pp_macros.h"
 #include "ppapi/c/pp_resource.h"
 #include "ppapi/c/pp_stdint.h"
-#include "ppapi/c/private/ppb_flash_net_address.h"
+#include "ppapi/c/private/ppb_flash_tcp_socket.h"
 
 #define PPB_FLASH_UDPSOCKET_INTERFACE_0_1 "PPB_Flash_UDPSocket;0.1"
 #define PPB_FLASH_UDPSOCKET_INTERFACE PPB_FLASH_UDPSOCKET_INTERFACE_0_1
