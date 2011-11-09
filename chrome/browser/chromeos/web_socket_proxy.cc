97c26cf8f174eb65d7a31e222b7d5db3e4c7f6c7
dilmah@chromium.org <dilmah@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-09 14:17:37 +0000

    Take care about lifetime of an object.
    BUG=chromium-os:22713
    TEST=Manual
    
    Review URL: http://codereview.chromium.org/8511009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109240 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 309c6a22275e7bad341507adae56ba18c2d59b05

--- chrome/browser/chromeos/web_socket_proxy.cc
+++ chrome/browser/chromeos/web_socket_proxy.cc
@@ -629,7 +629,7 @@ class SSLChan : public MessageLoopForIO::Watcher {
         socket_->Disconnect();
         socket_.reset();
       }
-      delete this;
+      MessageLoop::current()->DeleteSoon(FROM_HERE, this);
     }
   }
 
@@ -663,8 +663,10 @@ class SSLChan : public MessageLoopForIO::Watcher {
   }
 
   void OnSSLHandshakeCompleted(int result) {
-    if (result)
+    if (result) {
       Shut(result);
+      return;
+    }
     is_socket_read_pending_ = false;
     is_socket_write_pending_ = false;
     is_read_pipe_blocked_ = false;
@@ -823,7 +825,7 @@ class SSLChan : public MessageLoopForIO::Watcher {
   MessageLoopForIO::FileDescriptorWatcher read_pipe_controller_;
   MessageLoopForIO::FileDescriptorWatcher write_pipe_controller_;
 
-  friend class base::RefCountedThreadSafe<SSLChan>;
+  friend class DeleteTask<SSLChan>;
   DISALLOW_COPY_AND_ASSIGN(SSLChan);
 };
 
