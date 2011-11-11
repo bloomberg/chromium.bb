bloomberg/chromium.bb

commit 540e006c0cb0c569c246d7aadc47deb8fa2c7253

[chromium] Make the layout test script's kill timeout proportional to --time-out-ms https://bugs.webkit.org/show_bug.cgi?id=68026
Patch by Lei Zhang <thestig@chromium.org> on 2011-09-23
Reviewed by Dirk Pranke.

* Scripts/webkitpy/layout_tests/port/chromium.py:

git-svn-id: svn://svn.chromium.org/blink/trunk@95875 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 4aaa006367cf72adb677e3b295fc3dd5b068351a

third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium.py
@@ -394,8 +394,6 @@ class ChromiumPort(Port):
 
 # FIXME: This should inherit from WebKitDriver now that Chromium has a DumpRenderTree process like the rest of WebKit.
 class ChromiumDriver(Driver):
-    KILL_TIMEOUT = 3.0
-
     def __init__(self, port, worker_number):
         Driver.__init__(self, port, worker_number)
         self._proc = None
@@ -595,12 +593,21 @@ class ChromiumDriver(Driver):
         self._proc.stdout.close()
         if self._proc.stderr:
             self._proc.stderr.close()
+        # The kill timeout was hard coded to 3 seconds given a default
+        # --time-out-ms of 35 seconds. We divide by 12 to get a close
+        # approximation.
+        time_out_ms = self._port.get_option('time_out_ms')
+        if time_out_ms:
+          kill_timeout_seconds = int(time_out_ms) / 12000
+        else:
+          kill_timeout_seconds = 3.0
+
         # Closing stdin/stdout/stderr hangs sometimes on OS X,
         # (see __init__(), above), and anyway we don't want to hang
         # the harness if DRT is buggy, so we wait a couple
         # seconds to give DRT a chance to clean up, but then
         # force-kill the process if necessary.
-        timeout = time.time() + self.KILL_TIMEOUT
+        timeout = time.time() + kill_timeout_seconds
         while self._proc.poll() is None and time.time() < timeout:
             time.sleep(0.1)
         if self._proc.poll() is None:

commit 09188bbb44234c91a4ba8be3be9d6d8468ebf46c

allow two chromium drivers to run at the same time in NRWT https://bugs.webkit.org/show_bug.cgi?id=72067
Reviewed by Dirk Pranke.

* Scripts/webkitpy/layout_tests/port/chromium.py: close_fds=True on
    posix prevents the subprocesses from sharing the same fd for stdin.
    Sharing the same fd prevents us from closing the files unless all references
    to the fd are closed.
* Scripts/webkitpy/layout_tests/port/chromium_unittest.py: In addition
    to adding a test for this, speed up a test (from 3sec to 0) by fixing
    the timeout override. It looks like it regressed in r95875.

git-svn-id: svn://svn.chromium.org/blink/trunk@99989 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 5de581719353de80e6025472ab7d8682e47e613b

third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium.py
@@ -452,7 +452,8 @@ class ChromiumDriver(Driver):
         assert not self._proc
         # FIXME: This should use ServerProcess like WebKitDriver does.
         # FIXME: We should be reading stderr and stdout separately like how WebKitDriver does.
-        self._proc = subprocess.Popen(self.cmd_line(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        close_fds = sys.platform not in ('win32', 'cygwin')
+        self._proc = subprocess.Popen(self.cmd_line(), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, close_fds=close_fds)
 
     def poll(self):
         return self._proc.poll()

third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/chromium_unittest.py
@@ -26,8 +26,9 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import unittest
 import StringIO
+import time
+import unittest
 
 from webkitpy.common.system import logtesting
 from webkitpy.common.system.executive_mock import MockExecutive, MockExecutive2
@@ -110,10 +111,33 @@ class ChromiumDriverTest(unittest.TestCase):
                 self.driver._proc.poll = lambda: 2
 
         self.driver._port._executive = FakeExecutive()
-        self.driver.KILL_TIMEOUT = 0.01
+        # Override the kill timeout (ms) so the test runs faster.
+        self.driver._port.get_option = lambda name: 1
         self.driver.stop()
         self.assertTrue(self.wait_called)
         self.assertEquals(self.pid, 1)
+
+    def test_two_drivers(self):
+        mock_port = Mock()
+
+        class MockDriver(chromium.ChromiumDriver):
+            def __init__(self):
+                chromium.ChromiumDriver.__init__(self, mock_port, worker_number=0)
+
+            def cmd_line(self):
+                return 'python'
+
+        # get_option is used to get the timeout (ms) for a process before we kill it.
+        mock_port.get_option = lambda name: 60 * 1000
+        driver1 = MockDriver()
+        driver1._start()
+        driver2 = MockDriver()
+        driver2._start()
+        # It's possible for driver1 to timeout when stopping if it's sharing stdin with driver2.
+        start_time = time.time()
+        driver1.stop()
+        driver2.stop()
+        self.assertTrue(time.time() - start_time < 20)
 
 
 class ChromiumPortTest(port_testcase.PortTestCase):