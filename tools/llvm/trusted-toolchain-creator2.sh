27ae843c0604d81fb52a51f755d2419160e1984a
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-11-14 15:42:11 +0000

    Some improvements found while setting the TC up with sehr. Review URL: http://codereview.chromium.org/8515009
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@7150 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 6160451da93018aee2be0218127bf73d6973e934

--- tools/llvm/trusted-toolchain-creator2.sh
+++ tools/llvm/trusted-toolchain-creator2.sh
@@ -10,8 +10,10 @@
 #@
 #@ This Toolchain was tested with Ubuntu Lucid
 #@
-#@ Usage of this TC for compilation:
-#@  TODO(robertm)
+#@ Usage of this TC:
+#@  compile: arm-linux-gnueabi-gcc -march=armv7-a -isystem ${JAIL}/usr/include
+#@  link:    arm-linux-gnueabi-gcc -L${JAIL}/usr/lib -L${JAIL}/usr/lib/arm-linux-gnueabi
+#@                                 -L${JAIL}/lib -L${JAIL}/lib/arm-linux-gnueabi
 #@
 #@ Usage of this TC with qemu
 #@  TODO(robertm)
@@ -54,8 +56,8 @@ CROSS_ARM_TC_DEP_FILES="\
   universe/g/gcc-4.5-armel-cross/libmudflap0-armel-cross_4.5.2-8ubuntu3cross1.47_all.deb \
   universe/g/gcc-4.5-armel-cross/libstdc++6-4.5-dev-armel-cross_4.5.2-8ubuntu3cross1.47_all.deb \
   universe/g/gcc-4.5-armel-cross/libstdc++6-armel-cross_4.5.2-8ubuntu3cross1.47_all.deb \
-  universe/g/gcc-4.5-armel-cross/libgcc1-armel-cross_4.5.2-8ubuntu3cross1.62_all.deb \
   universe/a/armel-cross-toolchain-base/binutils-arm-linux-gnueabi_2.21.0.20110327-2ubuntu2cross1.62_amd64.deb \
+  universe/a/armel-cross-toolchain-base/libgcc1-armel-cross_4.5.2-8ubuntu3cross1.62_all.deb \
   universe/a/armel-cross-toolchain-base/libc6-armel-cross_2.13-0ubuntu13cross1.62_all.deb \
   universe/a/armel-cross-toolchain-base/libc6-dev-armel-cross_2.13-0ubuntu13cross1.62_all.deb \
   universe/a/armel-cross-toolchain-base/linux-libc-dev-armel-cross_2.6.38-8.42cross1.62_all.deb \
@@ -63,7 +65,7 @@ CROSS_ARM_TC_DEP_FILES="\
   main/m/mpfr4/libmpfr4_3.0.0-7_amd64.deb \
   main/c/cloog-ppl/libcloog-ppl0_0.15.9-2_amd64.deb"
 
-readonly ARMEL_PACKAGES="\
+readonly ARMEL_BASE_PACKAGES="\
   libssl-dev \
   libssl0.9.8 \
   libgcc1 \
@@ -76,7 +78,7 @@ readonly ARMEL_PACKAGES="\
   libxt-dev \
   libxt6"
 
-readonly ARMEL_DEP_FILES="\
+readonly ARMEL_BASE_DEP_FILES="\
   main/o/openssl/libssl-dev_0.9.8g-16ubuntu3_armel.deb \
   main/o/openssl/libssl0.9.8_0.9.8g-16ubuntu3_armel.deb \
   main/g/gcc-4.5/libgcc1_4.5.2-8ubuntu4_armel.deb \
@@ -88,6 +90,35 @@ readonly ARMEL_DEP_FILES="\
   main/x/x11proto-core/x11proto-core-dev_7.0.20-1_all.deb \
   main/libx/libxt/libxt-dev_1.0.9-1ubuntu1_armel.deb \
   main/libx/libxt/libxt6_1.0.9-1ubuntu1_armel.deb"
+
+
+readonly ARMEL_EXTRA_PACKAGES="\
+  libcups2-dev \
+  libx11-dev \
+  libx11-6 \
+  x11proto-core-dev \
+  libxt-dev \
+  libxt6 \
+  libxext-dev \
+  libxext6 \
+  libxau-dev \
+  libxau6 \
+  zlib1g \
+  zlib1g-dev"
+
+readonly ARMEL_EXTRA_DEP_FILES="
+  main/z/zlib/zlib1g_1.2.3.4.dfsg-3ubuntu3_armel.deb \
+  main/c/cups/libcups2-dev_1.4.6-5ubuntu1_armel.deb \
+  main/libx/libx11/libx11-dev_1.4.2-1ubuntu3_armel.deb \
+  main/libx/libx11/libx11-6_1.4.2-1ubuntu3_armel.deb \
+  main/x/x11proto-core/x11proto-core-dev_7.0.20-1_all.deb \
+  main/libx/libxt/libxt-dev_1.0.9-1ubuntu1_armel.deb \
+  main/libx/libxt/libxt6_1.0.9-1ubuntu1_armel.deb \
+  main/libx/libxext/libxext-dev_1.2.0-2ubuntu1_armel.deb \
+  main/libx/libxext/libxext6_1.2.0-2ubuntu1_armel.deb \
+  main/libx/libxau/libxau-dev_1.0.6-1ubuntu1_armel.deb \
+  main/libx/libxau/libxau6_1.0.6-1ubuntu1_armel.deb \
+  main/z/zlib/zlib1g-dev_1.2.3.4.dfsg-3ubuntu3_armel.deb"
 
 # this where we get the cross toolchain from for the manual install:
 readonly CROSS_ARM_TC_REPO=http://mirror.pnl.gov/ubuntu/pool
@@ -102,7 +133,7 @@ readonly TMP=/tmp/arm-crosstool-natty
 
 readonly REQUIRED_TOOLS="wget"
 
-readonly  MAKE_OPTS="-j8"
+readonly MAKE_OPTS="-j8"
 
 ######################################################################
 # Helper
@@ -185,34 +216,60 @@ CreateTarBall() {
 # One of these has to be run ONCE per machine
 ######################################################################
 
-# This has been tested on 64bit ubuntu natty
+#@
+#@ InstallCrossArmBasePackages
+#@
+#@ This has been tested on 64bit ubuntu natty
+#@ For oneiric additional adjustments are necessary
 InstallCrossArmBasePackages() {
   sudo apt-get install ${CROSS_ARM_TC_PACKAGES}
 }
 
-# This should work even for 64bit ubuntu lucid machine
-# The download part is more or less idem-potent, run it until
-# all files have been downloaded
+#@
+#@ InstallCrossArmBasePackagesManual
+#@
+#@ This should work even for 64bit ubuntu lucid machine
+#@ The download part is more or less idem-potent, run it until
+#@ all files have been downloaded
 InstallCrossArmBasePackagesManual() {
-  mkdir -p ${TMP}
+  Banner "Install arm cross TC semi-automatically"
 
+  local dest=${TMP}/manual-tc-packages
+  mkdir -p ${dest}
+
+  SubBanner "Download packages"
   for i in ${CROSS_ARM_TC_DEP_FILES} ; do
     echo $i
     url=${CROSS_ARM_TC_REPO}/$i
-    dst=${TMP}/$(basename $i)
-    DownloadOrCopy ${url} ${dst}
+    file=${dest}/$(basename $i)
+    DownloadOrCopy ${url} ${file}
   done
 
-  sudo dpkg -i ${TMP}/*.deb
-}
+  SubBanner "Package Sanity Check"
 
+  for i in ${dest}/*.deb ; do
+    ls -l $i
+    if [[ ! -s $i ]] ; then
+      echo
+      echo "ERROR: bad package $i"
+      exit -1
+    fi
+  done
+
+  SubBanner "Possibly install additional standard packages"
+  # these are needed for the TC packages we are about to install
+  sudo apt-get install libelfg0 libgmpxx4ldbl libmpc2 libppl7 libppl-c2
+
+  SubBanner "Install cross arm TC packages"
+  sudo dpkg -i ${dest}/*.deb
+}
 
 ######################################################################
 #
 ######################################################################
 
 #@
-#@
+#@ InstallTrustedLinkerScript
 InstallTrustedLinkerScript() {
   local trusted_ld_script=${INSTALL_ROOT}/ld_script_arm_trusted
   # We are using the output of "ld --verbose" which contains
@@ -235,6 +292,7 @@ HacksAndPatches() {
   Banner "Misc Hacks & Patches"
   # these are linker scripts with absolute pathnames in them
   # which we rewrite here
+  SubBanner "Rewriting Linker Scripts"
   sed -i -e 's|/usr/lib/arm-linux-gnueabi/||g' \
     toolchain/linux_arm_natty_jail/usr/lib/arm-linux-gnueabi/libpthread.so \
     toolchain/linux_arm_natty_jail/usr/lib/arm-linux-gnueabi/libc.so
@@ -246,6 +304,7 @@ HacksAndPatches() {
   # a dependency on libz.so. We don't download libz, although
   # we probably should. For now, remove the .so and let the linker
   # use the .a instead.
+  SubBanner "Deleting some .sos"
   rm -fv toolchain/linux_arm_natty_jail/usr/lib/libcrypto.so
   rm -fv toolchain/linux_arm_natty_jail/usr/lib/libssl.so.0.9.8
   rm -fv toolchain/linux_arm_natty_jail/usr/lib/libssl.so
@@ -254,38 +313,53 @@ HacksAndPatches() {
 
 
 InstallMissingArmLibrariesAndHeadersIntoJail() {
+  Banner "Install Libs And Headers Into Jail"
+
   mkdir -p ${TMP}
   mkdir -p ${INSTALL_ROOT}
-  for file in ${ARMEL_DEP_FILES} ; do
+  for file in $@ ; do
     local package="${TMP}/${file##*/}"
     Banner "installing ${file}"
     DownloadOrCopy ${ARMEL_REPO}/${file} ${package}
     SubBanner "extracting to ${INSTALL_ROOT}"
+    if [[ ! -s ${package} ]] ; then
+      echo
+      echo "ERROR: bad package ${package}"
+      exit -1
+    fi
     dpkg --fsys-tarfile ${package}\
       | tar -xvf - --exclude=./usr/share -C ${INSTALL_ROOT}
   done
+}
 
-  Banner "symlink cleanup"
-  (
-    cd ${INSTALL_ROOT}
-    find usr/lib -type l -printf '%p %l\n' | while read link target; do
-      case "${target}" in
-        /*)
-          # Relativize the symlink.
-          ln -snfv "../..${target}" "${link}"
-          ;;
-      esac
-      # Just in case there are dangling links other than ones
-      # that just needed to be relativized, remove them.
-      if [ ! -r "${link}" ]; then
-        rm -fv "${link}"
-      fi
-    done
-  )
+
+CleanupJailSymlinks() {
+  Banner "jail symlink cleanup"
+
+  pushd ${INSTALL_ROOT}
+  find usr/lib -type l -printf '%p %l\n' | while read link target; do
+    # skip links with non-absolute paths
+    if [[ ${target} != /* ]] ; then
+      continue
+    fi
+    echo "${link}: ${target}"
+    case "${link}" in
+      usr/lib/arm-linux-gnueabi/*)
+        # Relativize the symlink.
+        ln -snfv "../../..${target}" "${link}"
+        ;;
+    esac
+    # make sure we catch new bad links
+    if [ ! -r "${link}" ]; then
+      echo "ERROR: FOUND BAD LINK ${link}"
+      exit -1
+    fi
+  done
+  popd
 }
 
 #@
-#@
+#@ BuildAndInstallQemu
 BuildAndInstallQemu() {
   local saved_dir=$(pwd)
   local tmpdir="${TMP}/qemu.nacl"
@@ -311,6 +385,7 @@ BuildAndInstallQemu() {
     --disable-darwin-user \
     --disable-bsd-user \
     --target-list=arm-linux-user \
+    --disable-smartcard-nss \
     --disable-sdl\
     --static
 
@@ -329,28 +404,47 @@ BuildAndInstallQemu() {
 }
 
 #@
+#@ BuildJail
+BuildJail() {
+  ClearInstallDir
+  InstallMissingArmLibrariesAndHeadersIntoJail ${ARMEL_BASE_DEP_FILES}
+  CleanupJailSymlinks
+  InstallTrustedLinkerScript
+  HacksAndPatches
+  BuildAndInstallQemu
+  CreateTarBall $1
+}
+
+#@
+#@ ExperimentalBuildBigJail
+ExperimentalBuildBigJail() {
+  InstallMissingArmLibrariesAndHeadersIntoJail ${ARMEL_BASE_DEP_FILES}
+  InstallMissingArmLibrariesAndHeadersIntoJail ${ARMEL_EXTRA_DEP_FILES}
+  CleanupJailSymlinks
+}
+
+#@
 #@ Regenerat Package List
+#@
 #@ This will need some manual intervention, e.g.
 #@ pool/ needs to be stripped and special characters like "+" cause problems
 GeneratePackageList() {
   DownloadOrCopy ${PACKAGE_LIST} ${TMP}/Packages.bz2
   bzcat ${TMP}/Packages.bz2 | egrep '^(Package:|Filename:)' > ${TMP}/Packages
-  echo "# BEGIN: update for ARMEL_DEP_FILES"
-  for pkg in ${ARMEL_PACKAGES} ; do
+  echo  ${ARMEL_EXTRA_PACKAGES}
+  echo "# BEGIN:"
+  for pkg in $@ ; do
     egrep -A 1 "${pkg}\$" ${TMP}/Packages | egrep -o "pool/.*"
   done
-  echo "# END: update for ARMEL_DEP_FILES"
+  echo "# END:"
 }
 
-#@
-#@
-BuildJail() {
-  ClearInstallDir
-  InstallMissingArmLibrariesAndHeadersIntoJail
-  InstallTrustedLinkerScript
-  HacksAndPatches
-  BuildAndInstallQemu
-  CreateTarBall $1
+GeneratePackageListBase() {
+  GeneratePackageList ${ARMEL_BASE_PACKAGES}
+}
+
+GeneratePackageListExtra() {
+  GeneratePackageList "${ARMEL_EXTRA_PACKAGES}"
 }
 
 
