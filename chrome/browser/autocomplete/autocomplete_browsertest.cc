e684f7cc72e76c308bc4ca336ddcbca8b5011843
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-14 13:38:12 +0000

    Mark AutocompleteBrowserTest.Basic flaky on Win and Linux.
    TBR=pkasting@chromium.org
    BUG=104124
    
    Review URL: http://codereview.chromium.org/8566004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109873 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 335631552435289113e764413830f44ebf1c3385

--- chrome/browser/autocomplete/autocomplete_browsertest.cc
+++ chrome/browser/autocomplete/autocomplete_browsertest.cc
@@ -66,7 +66,12 @@ class AutocompleteBrowserTest : public ExtensionBrowserTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, Basic) {
+#if defined(OS_WIN) || defined(OS_LINUX)
+#define MAYBE_Basic FLAKY_Basic
+#else
+#define MAYBE_Basic Basic
+#endif
+IN_PROC_BROWSER_TEST_F(AutocompleteBrowserTest, MAYBE_Basic) {
   LocationBar* location_bar = GetLocationBar();
 
   EXPECT_TRUE(location_bar->GetInputString().empty());
