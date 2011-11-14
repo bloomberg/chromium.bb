64c8fa0dcdc8cdd6b1d98be0f1ec5ea5e299786c
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-14 19:41:43 +0000

    NTP4: Fix crash when re-ordering apps.
    R=csilv@chromium.org
    BUG=104059
    TEST=Re-order apps, Chrome doesn't crash when a CHECK() fails.
    
    
    Review URL: http://codereview.chromium.org/8533014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@109938 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c5463cd20034c3a486c012c7436195fccab09b60

--- chrome/browser/resources/ntp4/new_tab.js
+++ chrome/browser/resources/ntp4/new_tab.js
@@ -400,7 +400,7 @@ cr.define('ntp4', function() {
   }
 
   function getAppsPageIndex(page) {
-    newTabView.getAppsPageIndex(page);
+    return newTabView.getAppsPageIndex(page);
   }
 
   function getCardSlider() {
