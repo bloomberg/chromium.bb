c757a198c6fef3e6deba51d94c8437afc96b9346
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 04:25:37 +0000

    Use USE_VIRTUAL_KEYBOARD instead of TOUCH_UI when loading virtual keyboard component extension
    Use correct definition introduced in http://codereview.chromium.org/8051010
    
    BUG=None
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/8566002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110042 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cf99f3b5cdc644528028cd472dc80fe1e3de33f7

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -149,7 +149,7 @@ void ComponentLoader::AddDefaultComponentExtensions() {
   AddFileManagerExtension(&component_extensions);
 #endif
 
-#if defined(TOUCH_UI)
+#if defined(USE_VIRTUAL_KEYBOARD)
   component_extensions.push_back(std::make_pair(
       FILE_PATH_LITERAL("keyboard"),
       IDR_KEYBOARD_MANIFEST));
