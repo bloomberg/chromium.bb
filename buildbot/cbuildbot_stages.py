30ce18b047500ed94198bfd61a03c1e1ddd4df2b
Chris Sosa <sosa@chromium.org>
2011-11-14 22:11:21 -0800

    Revert "Uprev packages even if manifest_manager isn't set."
    This reverts commit cfd1279bd6b6f1863222c6c9367ac3b4b4ccbb03
    
    Change-Id: Ib127f26c561d4d6920c3ab6093ca36d6b7dcfc99
    Reviewed-on: https://gerrit.chromium.org/gerrit/11692
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Tested-by: Chris Sosa <sosa@chromium.org>

--- buildbot/cbuildbot_stages.py
+++ buildbot/cbuildbot_stages.py
@@ -345,8 +345,7 @@ class LKGMCandidateSyncCompletionStage(ManifestVersionedSyncCompletionStage):
   def HandleSuccess(self):
     # We only promote for the pfq, not chrome pfq.
     if (self._build_config['build_type'] == constants.PFQ_TYPE and
-        self._build_config['master'] and self._tracking_branch == 'master' and
-        ManifestVersionedSyncStage.manifest_manager != None):
+        self._build_config['master'] and self._tracking_branch == 'master'):
       ManifestVersionedSyncStage.manifest_manager.PromoteCandidate()
 
   def _PerformStage(self):
