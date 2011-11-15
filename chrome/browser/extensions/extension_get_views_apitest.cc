631cce53dafed73b2949f80b3931aa4ebe09ed62
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-15 16:40:44 +0000

    Enable ExtensionApiTest.GetViews on all platforms.
    BUG=none
    TEST=passes everywhere
    
    
    Review URL: http://codereview.chromium.org/8520013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110104 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0b3e0bd60faca7602bf19a9eba78479ff1cceffb

--- chrome/browser/extensions/extension_get_views_apitest.cc
+++ chrome/browser/extensions/extension_get_views_apitest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,15 +6,7 @@
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_switches.h"
 
-#if defined(TOOLKIT_VIEWS)
-// Need to port ExtensionInfoBarDelegate::CreateInfoBar() to other platforms.
-// See http://crbug.com/39916 for details.
-#define MAYBE_GetViews GetViews
-#else
-#define MAYBE_GetViews DISABLED_GetViews
-#endif
-
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_GetViews) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, GetViews) {
   // TODO(finnur): Remove once infobars are no longer experimental (bug 39511).
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
