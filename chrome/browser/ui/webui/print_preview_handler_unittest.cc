d3f0996b8ff0226d633c9a199623ae1390c4977d
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-16 00:18:29 +0000

    Disable PrintPreviewHandlerTests on CrOS. They are crashing.
    BUG=104403
    TEST=none
    Review URL: http://codereview.chromium.org/8574028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110220 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 53c9a2bd0d3db088e3ed8566f40ad31001c200ac

--- chrome/browser/ui/webui/print_preview_handler_unittest.cc
+++ chrome/browser/ui/webui/print_preview_handler_unittest.cc
@@ -149,7 +149,8 @@ class PrintPreviewHandlerTest : public BrowserWithTestWindowTest {
 
 // Test crashs on TouchUI due to initiator tab's native view having no parent.
 // http://crbug.com/104284
-#if defined(TOUCH_UI)
+// Test crashes on CrOS as well. http://crbug.com/104403
+#if defined(TOUCH_UI) || defined(OS_CHROMEOS)
 #define MAYBE_StickyMarginsCustom DISABLED_StickyMarginsCustom
 #else
 #define MAYBE_StickyMarginsCustom StickyMarginsCustom
@@ -173,8 +174,8 @@ TEST_F(PrintPreviewHandlerTest, MAYBE_StickyMarginsCustom) {
   CheckCustomMargins(kMarginTop, kMarginRight, kMarginBottom, kMarginLeft);
 }
 
-// http://crbug.com/104284
-#if defined(TOUCH_UI)
+// http://crbug.com/104284, http://crbug.com/104403
+#if defined(TOUCH_UI) || defined(OS_CHROMEOS)
 #define MAYBE_StickyMarginsDefault DISABLED_StickyMarginsDefault
 #else
 #define MAYBE_StickyMarginsDefault StickyMarginsDefault
@@ -192,8 +193,8 @@ TEST_F(PrintPreviewHandlerTest, MAYBE_StickyMarginsDefault) {
   ASSERT_FALSE(PrintPreviewHandler::last_used_page_size_margins_);
 }
 
-// http://crbug.com/104284
-#if defined(TOUCH_UI)
+// http://crbug.com/104284, http://crbug.com/104403
+#if defined(TOUCH_UI) || defined(OS_CHROMEOS)
 #define MAYBE_StickyMarginsCustomThenDefault \
     DISABLED_StickyMarginsCustomThenDefault
 #else
@@ -226,8 +227,8 @@ TEST_F(PrintPreviewHandlerTest, MAYBE_StickyMarginsCustomThenDefault) {
   CheckCustomMargins(kMarginTop, kMarginRight, kMarginBottom, kMarginLeft);
 }
 
-// http://crbug.com/104284
-#if defined(TOUCH_UI)
+// http://crbug.com/104284, http://crbug.com/104403
+#if defined(TOUCH_UI) || defined(OS_CHROMEOS)
 #define MAYBE_GetLastUsedMarginSettingsCustom \
     DISABLED_GetLastUsedMarginSettingsCustom
 #else
@@ -263,8 +264,8 @@ TEST_F(PrintPreviewHandlerTest, MAYBE_GetLastUsedMarginSettingsCustom) {
   EXPECT_EQ(kMarginLeft, margin_value);
 }
 
-// http://crbug.com/104284
-#if defined(TOUCH_UI)
+// http://crbug.com/104284, http://crbug.com/104403
+#if defined(TOUCH_UI) || defined(OS_CHROMEOS)
 #define MAYBE_GetLastUsedMarginSettingsDefault \
     DISABLED_GetLastUsedMarginSettingsDefault
 #else
