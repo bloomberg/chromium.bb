b5bff294178d0a826ff8c3355b8ced08828e9c7d
levin@chromium.org <levin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-17 09:20:59 +0000

    Ensure that WebKit is initialized before making calls to it.
    BUG=104496
    TEST=Opening a Panel in debug mode with Youtube frame embedded right after launching chromium.
    
    
    Review URL: http://codereview.chromium.org/8581004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110475 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0654cc8ef8cd3fa4af7980a899c6ec1e8791d6f2

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -631,8 +631,10 @@ void RenderThreadImpl::OnSetZoomLevelForCurrentURL(const GURL& url,
 
 void RenderThreadImpl::OnDOMStorageEvent(
     const DOMStorageMsg_Event_Params& params) {
-  if (!dom_storage_event_dispatcher_.get())
+  if (!dom_storage_event_dispatcher_.get()) {
+    EnsureWebKitInitialized();
     dom_storage_event_dispatcher_.reset(WebStorageEventDispatcher::create());
+  }
   dom_storage_event_dispatcher_->dispatchStorageEvent(params.key,
       params.old_value, params.new_value, params.origin, params.url,
       params.storage_type == DOM_STORAGE_LOCAL);
