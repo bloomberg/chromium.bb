137743d49b7fdef3ee768b6a444bf25aa387f1e6
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-11-17 15:04:14 +0000

    Disable arm adhoc test which has problems with the new arm trusted TC, which are not readily reproducible locally. Review URL: http://codereview.chromium.org/8586006
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@7211 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 5361813d3d5c666494045ee9027b8c582b40e093

--- buildbot/buildbot_pnacl.sh
+++ buildbot/buildbot_pnacl.sh
@@ -281,7 +281,9 @@ mode-buildbot-arm() {
   scons-tests "arm" "${mode} -k" "medium_tests"
   scons-tests "arm" "${mode} -k" "large_tests"
   browser-tests "arm" "${mode}"
-  ad-hoc-shared-lib-tests "arm"
+  # TODO(robertm): this test has problems with the new trusted TC
+  #                which require some more investigation
+  # ad-hoc-shared-lib-tests "arm"
 }
 
 mode-buildbot-arm-dbg() {
