928ea75c3c4b1a6668a7ba722c3175b0afa4a1ee
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-11-18 19:55:45 +0000

    x86-64 Windows: Make stack unwinding test stricter
    If I insert "sub rsp, 16" at the start of NaClSwitchSavingStackPtr,
    the backtrace at the time of the fault includes a couple of bogus
    stack frames, which the existing test case does not catch.  Such stack
    frames would come from reading return addresses from uninitialised
    memory, which is bad.
    
    This change makes the test stricter to catch such problems.  We walk
    the stack and look for too-small frames which are not valid in the
    Windows x86-64 ABI but which the Windows runtime lets through.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2414
    TEST=run_windows_crash_in_syscall_test
    
    Review URL: http://codereview.chromium.org/8595001
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@7243 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: f9cfb7279797950e86a8b9d5190cd777f154e286

--- tests/windows_crash_in_syscall/test_host.c
+++ tests/windows_crash_in_syscall/test_host.c
@@ -4,6 +4,9 @@
  * found in the LICENSE file.
  */
 
+#include <windows.h>
+#include <dbghelp.h>
+
 #include "native_client/src/shared/gio/gio.h"
 #include "native_client/src/shared/platform/nacl_check.h"
 #include "native_client/src/shared/platform/nacl_log.h"
@@ -44,9 +47,140 @@ static int32_t CrashyTestSyscall(struct NaClAppThread *natp) {
   _exit(1);
 }
 
+#define MAX_SYMBOL_NAME_LENGTH 100
+
+static void PrintSymbolForAddress(DWORD64 addr) {
+  /*
+   * Code adapted from Chromium's stack_trace_win.cc, in turn adapted
+   * from an MSDN example:
+   * http://msdn.microsoft.com/en-us/library/ms680578(VS.85).aspx
+   */
+  ULONG64 buffer[(sizeof(SYMBOL_INFO) +
+                  MAX_SYMBOL_NAME_LENGTH * sizeof(wchar_t) +
+                  sizeof(ULONG64) - 1) /
+                 sizeof(ULONG64)];
+  DWORD64 sym_displacement = 0;
+  PSYMBOL_INFO symbol = (PSYMBOL_INFO) buffer;
+  BOOL has_symbol;
+  memset(buffer, 0, sizeof(buffer));
+
+  SymInitialize(GetCurrentProcess(), NULL, TRUE);
+
+  symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+  symbol->MaxNameLen = MAX_SYMBOL_NAME_LENGTH - 1;
+  has_symbol = SymFromAddr(GetCurrentProcess(),
+                           addr, &sym_displacement, symbol);
+  if (has_symbol) {
+    fprintf(stderr, "%s + 0x%x\n", symbol->Name, sym_displacement);
+  } else {
+    fprintf(stderr, "<no symbol>\n");
+  }
+}
+
+/*
+ * On x86-64 Windows, we expect the backtrace to contain the
+ * following, with no gaps:
+ *
+ *   NaClSyscallCSegHook
+ *   NaClSwitchSavingStackPtr
+ *   NaClStartThreadInApp
+ *
+ * We could check for those names, but symbols are not always
+ * available.  Instead we check for bogus stack frames below, which
+ * the stack unwinder lets through.
+ */
+static void Backtrace(CONTEXT *initial_context) {
+  int machine_type;
+  CONTEXT context_for_frame = *initial_context;
+  STACKFRAME64 frame = { 0 };
+  int frame_number = 0;
+  int failed = 0;
+
+  fprintf(stderr, "Stack backtrace:\n");
+#if NACL_ARCH(NACL_BUILD_ARCH) == NACL_x86 && NACL_BUILD_SUBARCH == 64
+  machine_type = IMAGE_FILE_MACHINE_AMD64;
+  frame.AddrPC.Offset = initial_context->Rip;
+  frame.AddrFrame.Offset = initial_context->Rbp;
+  frame.AddrStack.Offset = initial_context->Rsp;
+#elif NACL_ARCH(NACL_BUILD_ARCH) == NACL_x86 && NACL_BUILD_SUBARCH == 32
+  machine_type = IMAGE_FILE_MACHINE_I386;
+  frame.AddrPC.Offset = initial_context->Eip;
+  frame.AddrFrame.Offset = initial_context->Ebp;
+  frame.AddrStack.Offset = initial_context->Esp;
+#else
+# error Unknown architecture
+#endif
+  frame.AddrPC.Mode = AddrModeFlat;
+  frame.AddrFrame.Mode = AddrModeFlat;
+  frame.AddrStack.Mode = AddrModeFlat;
+  frame.Virtual = 0;
+
+  while (1) {
+    STACKFRAME64 previous_frame = frame;
+    if (!StackWalk64(machine_type, GetCurrentProcess(), GetCurrentThread(),
+                     &frame, &context_for_frame,
+                     NULL, /* use ReadMemory() */
+                     SymFunctionTableAccess64, SymGetModuleBase64, NULL)) {
+      break;
+    }
+    fprintf(stderr, "#%i: ip=%p stack=%llx frame=%llx ",
+            frame_number,
+            frame.AddrPC.Offset,
+            frame.AddrStack.Offset,
+            frame.AddrFrame.Offset);
+    PrintSymbolForAddress(frame.AddrPC.Offset);
+    /*
+     * Perform some sanity checks.  Windows' x86-64 stack unwinder
+     * applies a fallback rule when it sees return addresses without
+     * unwind info, but the fallback rule is for leaf functions.  This
+     * causes the stack unwinder to report stack frames that cannot
+     * possibly be valid in the Windows x86-64 ABI.  We check for such
+     * frames here.  An error here would suggest that the unwind info
+     * for NaClSwitchSavingStackPtr is wrong.
+     *
+     * The frame for a non-leaf function Foo() looks like this:
+     *     32 bytes   shadow space (scratch space for Foo())
+     *   ---- Foo()'s caller's AddrStack points here
+     *      8 bytes   return address (points into Foo()'s caller)
+     *      8 bytes   scratch space for Foo()
+     *   ---- Foo()'s AddrFrame points here (unless the hardware exception
+     *        occurred inside Foo(), in which case this is less well-defined)
+     *   16*n bytes   scratch space for Foo() (for some n >= 0)
+     *     32 bytes   shadow space (scratch space for Foo()'s callees)
+     *   ---- Foo()'s rsp and AddrStack point here
+     *
+     * The frame for a leaf function Bar() that never adjusts rsp
+     * looks like this:
+     *     32 bytes   shadow space (scratch space for Bar())
+     *      8 bytes   return address (points into Bar()'s caller)
+     *   ---- Bar()'s rsp and AddrStack points here
+     *      8 bytes   not usable by Bar() at all!
+     *   ---- Bar()'s AddrFrame points here
+     */
+    if (NACL_ARCH(NACL_BUILD_ARCH) == NACL_x86 && NACL_BUILD_SUBARCH == 64) {
+      DWORD64 frame_size = frame.AddrFrame.Offset - frame.AddrStack.Offset;
+      if (frame_number > 0 && frame_size < 32) {
+        fprintf(stderr, "Error: frame_size=%i, which is too small\n",
+                frame_size);
+        failed = 1;
+      }
+      if (frame_number > 1 &&
+          frame.AddrStack.Offset != previous_frame.AddrFrame.Offset + 16) {
+        fprintf(stderr, "Error: stack does not fit with previous frame\n");
+        failed = 1;
+      }
+    }
+    frame_number++;
+  }
+  if (failed) {
+    _exit(1);
+  }
+}
+
 static LONG WINAPI ExceptionHandler(EXCEPTION_POINTERS *exc_info) {
   printf("Inside exception handler, as expected\n");
-  fflush(stdout);
+  Backtrace(exc_info->ContextRecord);
+  printf("Stack backtrace passed sanity checks\n");
   /*
    * Continuing is what Breakpad does, but this should cause the
    * process to exit with an exit status that is appropriate for the
@@ -68,6 +202,10 @@ int main(int argc, char **argv) {
     syscall_table[index].handler = NotImplementedDecoder;
   }
   syscall_table[CRASHY_TEST_SYSCALL].handler = CrashyTestSyscall;
+
+  /* Turn off buffering to aid debugging. */
+  setvbuf(stdout, NULL, _IONBF, 0);
+  setvbuf(stderr, NULL, _IONBF, 0);
 
   if (argc != 2) {
     NaClLog(LOG_FATAL, "Expected 1 argument: executable filename\n");
