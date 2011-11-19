a9c70f51cb19d420181a928e678cf92f67f17f09
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-19 00:50:08 +0000

    Fix compile error introduced on r110794 TBR=wez@chromium.org
    Review URL: http://codereview.chromium.org/8604002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110798 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ab21f2c226f357d92789d7d5a59b6902cb477f18

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -4722,7 +4722,7 @@ void RenderViewImpl::OnMouseLockLost() {
 }
 
 bool RenderViewImpl::WebWidgetHandlesCompositorScheduling() const {
-  return RenderThreadImpl::current()->compositor_thread();
+  return !!RenderThreadImpl::current()->compositor_thread();
 }
 
 void RenderViewImpl::OnJavaBridgeInit(
