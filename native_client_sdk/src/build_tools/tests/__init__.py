536e910d777f2f0d6540a84b2df085ef05405cd8
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-19 04:05:02 +0000

    Revert 110822 - Breaks check_deps and check_perms
    Adding reduced version of sdk at 1387 to chrome tree.
    
    BUG=None
    TEST=None
    R=noelallen@google.com
    
    TBR
    
    BUG=None
    TBR=bradnelson@google.com
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110830 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6e1d728ba695c4aed797b57d94537378b7e12623

--- native_client_sdk/src/build_tools/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-#! -*- python -*-
-#
-# Copyright (c) 2011 The Native Client Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-"""Package for build_tools/tests"""
--- native_client_sdk/src/build_tools/tests/fake_gsutil.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem Simple Wrapper function to allow running fake_gsutil on Windows
-python "%~dp0fake_gsutil.py" %*
