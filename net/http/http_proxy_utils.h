97fea9c5b5fe3dcec96a927a8eac6a7ddf6b4bbf
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-21 18:32:01 +0000

    Allow chrome to handle 407 auth challenges to CONNECT requests through HTTPS Proxies.  This also changes the mechanism used to restart HttpProxyClientSocket requests with auth.  Previously the transport socket would be Disconnected, and then re-Connected (which was not implemented for SSLClientSockets).  However, the approach was problematic in the face of, for example, ipv6.  The new approach is to close the HttpProxyClientSocket, and request a new socket from the pool.
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=110529
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=110879
    
    Review URL: http://codereview.chromium.org/8502024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@110965 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 15a3c20866a1de64fa731b03b8662ad0e89436dd

--- net/http/http_proxy_utils.h
+++ net/http/http_proxy_utils.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -10,6 +10,9 @@
 
 namespace net {
 
+class BoundNetLog;
+class HttpAuthController;
+class HttpResponseInfo;
 struct HttpRequestInfo;
 class HttpRequestHeaders;
 class HostPortPair;
@@ -22,6 +25,12 @@ void BuildTunnelRequest(const HttpRequestInfo& request_info,
                         const HostPortPair& endpoint,
                         std::string* request_line,
                         HttpRequestHeaders* request_headers);
+
+// When an auth challenge (407 response) is received during tunnel construction
+// this method should be called.
+int HandleAuthChallenge(HttpAuthController* auth,
+                        HttpResponseInfo* response,
+                        const BoundNetLog& net_log);
 
 }  // namespace net
 
