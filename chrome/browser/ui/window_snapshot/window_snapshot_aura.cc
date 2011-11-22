9a1f9576366df353ada82884490f227676562dd9
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-22 16:36:42 +0000

    Implementation of window_snapshot_aura
    Review URL: http://codereview.chromium.org/8588058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111166 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 386e02f42203d17b1256373fff0fc015deac75f0

--- chrome/browser/ui/window_snapshot/window_snapshot_aura.cc
+++ chrome/browser/ui/window_snapshot/window_snapshot_aura.cc
@@ -5,16 +5,38 @@
 #include "chrome/browser/ui/window_snapshot/window_snapshot.h"
 
 #include "base/logging.h"
+#include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/aura/window.h"
+#include "ui/gfx/codec/png_codec.h"
+#include "ui/gfx/compositor/compositor.h"
 #include "ui/gfx/rect.h"
 
 namespace browser {
 
-bool GrabWindowSnapshot(gfx::NativeWindow window_handle,
-                             std::vector<unsigned char>* png_representation,
-                             const gfx::Rect& snapshot_bounds) {
-  // TODO(saintlou): Stub for Aura.
-  NOTIMPLEMENTED();
-  return false;
+bool GrabWindowSnapshot(gfx::NativeWindow window,
+                        std::vector<unsigned char>* png_representation,
+                        const gfx::Rect& snapshot_bounds) {
+  ui::Compositor* compositor = window->layer()->GetCompositor();
+
+  gfx::Rect desktop_snapshot_bounds = gfx::Rect(
+      snapshot_bounds.origin().Add(window->bounds().origin()),
+      snapshot_bounds.size());
+
+  DCHECK_LE(desktop_snapshot_bounds.right() , compositor->size().width());
+  DCHECK_LE(desktop_snapshot_bounds.bottom(), compositor->size().height());
+
+  SkBitmap bitmap;
+  if (!compositor->ReadPixels(&bitmap, desktop_snapshot_bounds))
+    return false;
+
+  unsigned char* pixels = reinterpret_cast<unsigned char*>(bitmap.getPixels());
+
+  gfx::PNGCodec::Encode(pixels, gfx::PNGCodec::FORMAT_BGRA,
+                        snapshot_bounds.size(),
+                        bitmap.rowBytes(), true,
+                        std::vector<gfx::PNGCodec::Comment>(),
+                        png_representation);
+  return true;
 }
 
 }  // namespace browser
