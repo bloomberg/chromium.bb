99f499b8d25486af5e6782333aaa214ec2317b3d
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-23 07:20:56 +0000

    Disable DeviceOrientationproviderTest.ObserverNotRemoved on Linux
    BUG=104950
    TBR=hans@chromium.org
    
    Review URL: http://codereview.chromium.org/8682001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111326 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 393b5d3527605f5c808a077f7bd095ce365ed151

--- content/browser/device_orientation/provider_unittest.cc
+++ content/browser/device_orientation/provider_unittest.cc
@@ -248,8 +248,13 @@ TEST_F(DeviceOrientationProviderTest, MultipleObserversPushTest) {
   provider_->RemoveObserver(checker_c.get());
 }
 
-// Flaky. See crbug.com/104950.
-TEST_F(DeviceOrientationProviderTest, FLAKY_ObserverNotRemoved) {
+#if defined(OS_LINUX)
+// Flakily DCHECKs on Linux. See crbug.com/104950.
+#define MAYBE_ObserverNotRemoved DISABLED_ObserverNotRemoved
+#else
+#define MAYBE_ObserverNotRemoved ObserverNotRemoved
+#endif
+TEST_F(DeviceOrientationProviderTest, MAYBE_ObserverNotRemoved) {
   scoped_refptr<MockOrientationFactory> orientation_factory(
       new MockOrientationFactory());
   Init(MockOrientationFactory::CreateDataFetcher);
