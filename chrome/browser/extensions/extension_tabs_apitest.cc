9b07b77c4b55ec9f8d2a376467193fe637069cb1
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-24 07:19:04 +0000

    FAILS_ win browser_test ExtensionApiTest.UpdateWindowShowState
    Is failing consistently on Windows, looks like a specific check-in
    r111480, suspect author is out for the holiday.
    
    TBR=dmich@chromium.org
    BUG=105356
    
    Review URL: http://codereview.chromium.org/8681028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111501 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 388c420f566b4f62f6cf619c1008ae8860105dd3

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -31,7 +31,14 @@
 #else
 #define MAYBE_FocusWindowDoesNotExitFullscreen FocusWindowDoesNotExitFullscreen
 #define MAYBE_UpdateWindowSizeExitsFullscreen UpdateWindowSizeExitsFullscreen
+
+// http://crbug.com/105356 , this test is failing browser_tests on Windows.
+#if defined(OS_WIN)
+#define MAYBE_UpdateWindowShowState FAILS_UpdateWindowShowState
+#else
 #define MAYBE_UpdateWindowShowState UpdateWindowShowState
+#endif
+
 #endif
 
 // In the touch build, this fails reliably. http://crbug.com/85191
