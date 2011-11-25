02674ec232d4910e48ffa2681a197813b5ef370f
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-25 14:50:58 +0000

    WebSocket Pepper API: in process API implementation
    This change enable in process Websocket Pepper API.
    For now, some unit tests are disabled because they need external a
    WebSocket server.
    These disabled tests run manually with external pywebsocket and
    --gtest_also_run_disabled_tests flag as follows.
    These tests will enabled in a few days.
    
    TBR=dmichael@chromium.org
    BUG=87310
    TEST=ui_tests --gtest_filter='PPAPITest.WebSocket*'
    
    Review URL: http://codereview.chromium.org/8558017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111596 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3138146004b13a2f1a249da15531d96a53f3e2d2

--- ppapi/tests/test_websocket.h
+++ ppapi/tests/test_websocket.h
@@ -9,6 +9,8 @@
 
 #include "ppapi/tests/test_case.h"
 
+struct PPB_Core;
+struct PPB_Var;
 struct PPB_WebSocket_Dev;
 
 class TestWebSocket : public TestCase {
@@ -20,11 +22,21 @@ class TestWebSocket : public TestCase {
   virtual void RunTests(const std::string& filter);
 
  private:
-  std::string TestCreate();
+  PP_Var CreateVar(const char* string);
+  void ReleaseVar(const PP_Var& var);
+  bool AreEqual(const PP_Var& var, const char* string);
+
+  PP_Resource Connect();
+
   std::string TestIsWebSocket();
+  std::string TestInvalidConnect();
+  std::string TestValidConnect();
+  std::string TestTextSendReceive();
 
   // Used by the tests that access the C API directly.
   const PPB_WebSocket_Dev* websocket_interface_;
+  const PPB_Var* var_interface_;
+  const PPB_Core* core_interface_;
 };
 
 #endif  // PAPPI_TESTS_TEST_WEBSOCKET_H_
