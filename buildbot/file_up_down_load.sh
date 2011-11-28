ae2a75f9ba7b9f54fc3927fb17049caf25f291dd
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-11-28 19:49:37 +0000

    Concentrate knowledge about gs_util. Review URL: http://codereview.chromium.org/8565029
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@7290 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: bae563061cf73dd4272caa9689a477e50da1cacf

---
+++ buildbot/file_up_down_load.sh
@@ -0,0 +1,203 @@
+#!/bin/bash
+# Copyright (c) 2011 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+#@ This script for up/downloading native client toolchains.
+
+#set -o xtrace
+set -o nounset
+set -o errexit
+
+######################################################################
+# Helper
+######################################################################
+
+Banner() {
+  echo "######################################################################"
+  echo $*
+  echo "######################################################################"
+}
+
+
+Usage() {
+  egrep "^#@" $0 | cut --bytes=3-
+}
+
+SanityCheck() {
+  Banner "Sanity Checks"
+  if [[ $(basename $(pwd)) != "native_client" ]] ; then
+    echo "ERROR: run this script from the native_client/ dir"
+    exit -1
+  fi
+}
+
+######################################################################
+# Config
+######################################################################
+
+readonly GS_UTIL=${GS_UTIL:-buildbot/gsutil.sh}
+
+readonly URL_PREFIX=https://commondatastorage.googleapis.com
+
+readonly BASE_ARM_TOOLCHAIN=nativeclient-archive2/toolchain
+
+readonly BASE_BETWEEN_BOTS_TRY=nativeclient-trybot/between_builders
+readonly BASE_BETWEEN_BOTS=nativeclient-archive2/between_builders
+
+
+######################################################################
+# UTIL
+######################################################################
+Upload() {
+  echo  "uploading: $2"
+  ${GS_UTIL} -h Cache-Control:no-cache cp -a public-read $1 $2
+}
+
+
+UploadToolchain() {
+  rev=$1
+  name=$2
+  tarball=$3
+
+  Upload ${tarball} gs://${BASE_ARM_TOOLCHAIN}/${rev}/${name}
+  Upload ${tarball} gs://${BASE_ARM_TOOLCHAIN}/latest/${name}
+}
+
+######################################################################
+# ARM TRUSTED
+######################################################################
+
+UploadArmTrustedToolchain() {
+  rev=$1
+  tarball=$2
+
+  UploadToolchain ${rev} naclsdk_linux_arm-trusted.tgz ${tarball}
+}
+
+DownloadArmTrustedToolchain() {
+  local rev=$1
+  local tarball=$2
+  curl -L \
+     ${URL_PREFIX}/${BASE_ARM_TOOLCHAIN}/${rev}/naclsdk_linux_arm-trusted.tgz
+     -o $2
+}
+
+ShowRecentArmTrustedToolchains() {
+   local url=gs://${BASE_ARM_TOOLCHAIN}/*/naclsdk_linux_arm-trusted.tgz
+   local recent=$(${GS_UTIL} ls ${url} | tail -5)
+   for url in ${recent} ; do
+     if ${GS_UTIL} ls -L "${url}" ; then
+       echo "====="
+     fi
+   done
+}
+
+######################################################################
+# ARM UN-TRUSTED
+######################################################################
+
+#@ label should be in :
+#@
+#@ pnacl_linux_i686_newlib
+#@ pnacl_linux_i686_glibc
+#@ pnacl_linux_x86_64_newlib
+#@ pnacl_linux_x86_64_glibc
+#@ pnacl_darwin_i386_newlib
+#@ pnacl_darwin_i386_glibc
+#@ pnacl_windows_i686_newlib
+#@ pnacl_windows_i686_newlib
+
+UploadArmUntrustedToolchains() {
+  local rev=$1
+  local label=$2
+  local tarball=$3
+
+  UploadToolchain ${rev} naclsdk_${label}.tgz ${tarball}
+}
+
+DownloadArmUntrustedToolchains() {
+  local rev=$1
+  local label=$2
+  local tarball=$3
+
+   curl -L \
+     ${URL_PREFIX}/${BASE_ARM_TOOLCHAIN}/${rev}/naclsdk_${label}.tgz
+     -o $2
+}
+
+ShowRecentArmUntrustedToolchains() {
+  local label=$1
+  local url="gs://${BASE_ARM_TOOLCHAIN}/*/naclsdk_${label}.tgz"
+
+  local recent=$(${GS_UTIL} ls ${url} | tail -5)
+  for url in ${recent} ; do
+    if ${GS_UTIL} ls -L "${url}" ; then
+      echo "====="
+    fi
+  done
+}
+
+######################################################################
+# ARM BETWEEN BOTS
+######################################################################
+
+UploadArmBinariesForHWBots() {
+  rev=$1
+  name=$2
+  tarball=$3
+
+  Upload ${tarball}  ${BASE_BETWEEN_BOTS}/${name}/${rev}/build.tgz
+}
+
+
+DownloadArmBinariesForHWBots() {
+  rev=$1
+  name=$2
+  tarball=$3
+
+  curl -L \
+     ${URL_PREFIX}/${BASE_BETWEEN_BOTS}/${name}/${rev}/build.tgz \
+     -o $3
+}
+
+######################################################################
+# ARM BETWEEN BOTS TRY
+######################################################################
+
+UploadArmBinariesForHWBotsTry() {
+  rev=$1
+  name=$2
+  tarball=$3
+
+  Download ${tarball} ${BASE_BETWEEN_BOTS_TRY}/${name}/${rev}/build.tgz
+}
+
+
+DownloadArmBinariesForHWBotsTry() {
+  rev=$1
+  name=$2
+  tarball=$3
+  curl -L \
+     ${URL_PREFIX}/${BASE_BETWEEN_BOTS_TRY}/${name}/${rev}/build.tgz \
+     -o $2
+}
+
+######################################################################
+# DISPATCH
+######################################################################
+SanityCheck
+
+if [[ $# -eq 0 ]] ; then
+  echo "you must specify a mode on the commandline:"
+  echo
+  Usage
+  exit -1
+elif [[ "$(type -t $1)" != "function" ]]; then
+  echo "ERROR: unknown function '$1'." >&2
+  echo "For help, try:"
+  echo "    $0 help"
+  exit 1
+else
+  "$@"
+fi
