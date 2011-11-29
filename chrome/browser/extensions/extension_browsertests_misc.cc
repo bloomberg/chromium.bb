1d3257f3830fd3977288eb135a69198cdb4e855e
oshima@google.com <oshima@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-29 22:04:54 +0000

    Disable ExtensionBrowserTest.PluginPrivate on aura
    BUG=105627
    TEST=browser_tests on aura bot turns green
    
    Review URL: http://codereview.chromium.org/8659044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112046 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b54991fbcff1b3c2f047c3a2a378fac7c1e525a1

--- chrome/browser/extensions/extension_browsertests_misc.cc
+++ chrome/browser/extensions/extension_browsertests_misc.cc
@@ -768,10 +768,11 @@ IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, MAYBE_PluginLoadUnload) {
   EXPECT_TRUE(result);
 }
 
-#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS)
+#if !defined(USE_AURA) && (defined(OS_WIN) || defined(OS_LINUX) || defined(OS_CHROMEOS))
 #define MAYBE_PluginPrivate PluginPrivate
 #else
 // TODO(mpcomplete): http://crbug.com/29900 need cross platform plugin support.
+// crbug.com/105627 for AURA.
 #define MAYBE_PluginPrivate DISABLED_PluginPrivate
 #endif
 
