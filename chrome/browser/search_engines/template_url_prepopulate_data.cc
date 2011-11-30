15ef3d718c5559b66d8f2ffa4fbc00db58eb4f3f
sreeram@chromium.org <sreeram@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-30 00:53:50 +0000

    Remove nord=1 from the Instant template for Google.
    This was originally added to prevent the homepage from redirecting to
    iGoogle, which doesn't support Instant. However, this param also
    prevents redirecting to SSL, which redirect we actually want.
    
    With this change, iGoogle users will see no Instant previews (so they'll
    have to press <Enter> to search), but it is being fixed transparently on
    the server side, so that Chrome Instant homepage requests will never go
    to iGoogle. That fix should be live by the end of the next week.
    
    BUG=105669
    TEST=Sign in to Google. With Instant enabled, do a query. Observe that you are on SSL (https).
    
    Review URL: http://codereview.chromium.org/8733003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112088 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5a36a60251c8a509cf3b4e09711ccea063adba2d

--- chrome/browser/search_engines/template_url_prepopulate_data.cc
+++ chrome/browser/search_engines/template_url_prepopulate_data.cc
@@ -1193,7 +1193,7 @@ const PrepopulatedEngine google = {
       L"client=chrome&hl={language}&q={searchTerms}",
   L"{google:baseURL}webhp?{google:RLZ}sourceid=chrome-instant&"
       L"{google:instantFieldTrialGroupParameter}"
-      L"ie={inputEncoding}&ion=1{searchTerms}&nord=1",
+      L"ie={inputEncoding}&ion=1{searchTerms}",
   SEARCH_ENGINE_GOOGLE,
   IDR_SEARCH_ENGINE_LOGO_GOOGLE,
   1,
@@ -3329,7 +3329,7 @@ void RegisterUserPrefs(PrefService* prefs) {
 int GetDataVersion(PrefService* prefs) {
   // Increment this if you change the above data in ways that mean users with
   // existing data should get a new version.
-  const int kCurrentDataVersion = 36;
+  const int kCurrentDataVersion = 37;
   if (!prefs)
     return kCurrentDataVersion;
   // If a version number exist in the preferences file, it overrides the
