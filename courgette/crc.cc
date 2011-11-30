59283137ad98e59da5fea51232fd67451ab76754
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-30 02:16:29 +0000

    Revert 112083 - Try a different library for Crc32.
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8569018
    
    TBR=dgarrett@chromium.org
    Review URL: http://codereview.chromium.org/8742002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112104 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4ae589487233c076abd03a0b1a962d4c2b8af935

--- courgette/crc.cc
+++ courgette/crc.cc
@@ -1,34 +1,22 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2009 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Calculate Crc by calling CRC method in LZMA SDK
+
 #include "courgette/crc.h"
 
-#ifdef OS_CHROMEOS
-#  include "zlib.h"
-#else
 extern "C" {
-#  include "third_party/lzma_sdk/7zCrc.h"
+#include "third_party/lzma_sdk/7zCrc.h"
 }
-#endif
-
-#include "base/basictypes.h"
 
 namespace courgette {
 
 uint32 CalculateCrc(const uint8* buffer, size_t size) {
-  uint32 crc;
-
-#ifdef OS_CHROMEOS
-  // Calculate Crc by calling CRC method in zlib
-  crc = crc32(0, buffer, size);
-#else
-  // Calculate Crc by calling CRC method in LZMA SDK
   CrcGenerateTable();
-  crc = CrcCalc(buffer, size);
-#endif
-
-  return ~crc;
+  uint32 crc = 0xffffffffL;
+  crc = ~CrcCalc(buffer, size);
+  return crc;
 }
 
 }  // namespace
