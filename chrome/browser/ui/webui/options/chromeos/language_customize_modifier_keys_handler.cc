ea3f4ceaca629671b0d00bfea38ea104eabb95d8
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-11-30 08:22:16 +0000

    Remove unused IME/keyboard string resources.
    BUG=None
    TEST=ran try
    
    
    Review URL: http://codereview.chromium.org/8742005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112164 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: af725c0ee77c505ddb1703d2632c6b50e1a01030

--- chrome/browser/ui/webui/options/chromeos/language_customize_modifier_keys_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/language_customize_modifier_keys_handler.cc
@@ -14,7 +14,7 @@ const struct ModifierKeysSelectItem {
   int message_id;
   chromeos::input_method::ModifierKey value;
 } kModifierKeysSelectItems[] = {
-  { IDS_OPTIONS_SETTINGS_LANGUAGES_XKB_KEY_SEARCH_LABEL,
+  { IDS_OPTIONS_SETTINGS_LANGUAGES_XKB_KEY_SEARCH,
     chromeos::input_method::kSearchKey },
   { IDS_OPTIONS_SETTINGS_LANGUAGES_XKB_KEY_LEFT_CTRL,
     chromeos::input_method::kLeftControlKey },
