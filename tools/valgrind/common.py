7b3e3214994660884799d665105e9b12a87efc2f
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-01 10:53:48 +0000

    Add one more boring caller TBR=thestig Review URL: http://codereview.chromium.org/8776002
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@112448 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8beb2ed9575ddcbc83b22c45657966a82094950a

--- tools/valgrind/common.py
+++ tools/valgrind/common.py
@@ -173,6 +173,8 @@ def BoringCallers(mangled, use_re_wildcards):
     ("DispatchToMethod*",    "_Z*16DispatchToMethod*"),
     ("base::internal::Invoker*::DoInvoke*",
      "_ZN4base8internal8Invoker*DoInvoke*"),  # Invoker{1,2,3}
+    ("base::internal::RunnableAdapter*::Run*",
+     "_ZN4base8internal15RunnableAdapter*Run*"),
   ]
 
   ret = []
