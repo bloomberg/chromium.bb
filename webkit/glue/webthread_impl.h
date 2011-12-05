d8c61b2cb8b00437c16b0aaa9eec7481d6daf9f4
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-05 20:48:24 +0000

    Revert 113010 - Implement WebThread::{add,remove}TaskObserver.
    Depends on http://trac.webkit.org/changeset/101418
    
    Review URL: http://codereview.chromium.org/8586038
    
    TBR=adamk@chromium.org
    Review URL: http://codereview.chromium.org/8805026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113014 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 17788c00f5aae81798d365bdfa86054ed021ed2d

--- webkit/glue/webthread_impl.h
+++ webkit/glue/webthread_impl.h
@@ -4,8 +4,6 @@
 #ifndef WEBKIT_GLUE_WEBTHREAD_IMPL_H_
 #define WEBKIT_GLUE_WEBTHREAD_IMPL_H_
 
-#include <map>
-
 #include "base/threading/thread.h"
 #include "base/memory/scoped_ptr.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebThread.h"
@@ -13,23 +11,9 @@
 
 namespace webkit_glue {
 
-class WebThreadBase : public WebKit::WebThread {
+class WebThreadImpl : public WebKit::WebThread {
  public:
-  virtual void addTaskObserver(TaskObserver* observer);
-  virtual void removeTaskObserver(TaskObserver* observer);
-
- protected:
-  virtual bool IsCurrentThread() const = 0;
-
- private:
-  class TaskObserverAdapter;
-  typedef std::map<TaskObserver*, TaskObserverAdapter*> TaskObserverMap;
-  TaskObserverMap task_observer_map_;
-};
-
-class WebThreadImpl : public WebThreadBase {
- public:
-  WEBKIT_GLUE_EXPORT explicit WebThreadImpl(const char* name);
+  WEBKIT_GLUE_EXPORT WebThreadImpl(const char* name);
   WEBKIT_GLUE_EXPORT virtual ~WebThreadImpl();
 
   virtual void postTask(Task* task) OVERRIDE;
@@ -38,24 +22,18 @@ class WebThreadImpl : public WebThreadBase {
   MessageLoop* message_loop() const { return thread_->message_loop(); }
 
  protected:
-  virtual bool IsCurrentThread() const;
-
- private:
   scoped_ptr<base::Thread> thread_;
 };
 
-class WebThreadImplForMessageLoop : public WebThreadBase {
+class WebThreadImplForMessageLoop : public WebKit::WebThread {
  public:
-  explicit WebThreadImplForMessageLoop(base::MessageLoopProxy* message_loop);
+  WebThreadImplForMessageLoop(base::MessageLoopProxy* message_loop);
   virtual ~WebThreadImplForMessageLoop();
 
   virtual void postTask(Task* task) OVERRIDE;
   virtual void postDelayedTask(Task* task, long long delay_ms) OVERRIDE;
 
  protected:
-  virtual bool IsCurrentThread() const;
-
- private:
   scoped_refptr<base::MessageLoopProxy> message_loop_;
 };
 
