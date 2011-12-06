49ed47f037b82dc1479e54bd447db4406f0f4c7e
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-06 12:23:48 +0000

    Revert 113183 - Revert 113177 - process all ui events before posting closure because menu depends on ui events to quit.  Add new RunClosureAfterAllPendingUIEvents for toolkit views.
    BUG=104359
    TEST=BookmarkBarViewTest in interactive_ui_tests will pass
    
    
    Review URL: http://codereview.chromium.org/8799020
    
    Reason for revert: http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromeOS%20Aura/builds/1514
    Reason for un-revert: The test succeeded before the original revert, looks like it's just flaky.
    
    TBR=oshima@chromium.org
    Review URL: http://codereview.chromium.org/8821012
    
    TBR=joi@chromium.org
    Review URL: http://codereview.chromium.org/8820010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113189 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ccf2231b71685fedbd0a9effdce6875a77a27ccc

--- chrome/browser/automation/ui_controls_aurawin.cc
+++ chrome/browser/automation/ui_controls_aurawin.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/automation/ui_controls.h"
 
 #include "base/logging.h"
+#include "base/message_loop.h"
 #include "chrome/browser/automation/ui_controls_internal.h"
 #include "ui/aura/desktop.h"
 #include "ui/views/view.h"
@@ -67,6 +68,11 @@ void MoveMouseToCenterAndPress(views::View* view,
   views::View::ConvertPointToScreen(view, &view_center);
   SendMouseMove(view_center.x(), view_center.y());
   SendMouseEventsNotifyWhenDone(button, state, task);
+}
+
+void RunClosureAfterAllPendingUIEvents(const base::Closure& task) {
+  // On windows, posting UI events is synchronous so just post the closure.
+  MessageLoopForUI::current()->PostTask(FROM_HERE, task);
 }
 
 }  // namespace ui_controls
