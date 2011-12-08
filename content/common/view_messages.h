1325732cc04c760131822e92760f201921d151aa
apavlov@chromium.org <apavlov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-08 15:11:12 +0000

    Revert r112160 ("Send one WebKeyboardEvent to the RenderWidget at a time.")
    This change has resulted in severe regressions in keyboard event dispatching -
    the keydown and keypress events are no longer dispatched synchronously. Reverting as suggested by Darin Fisher.
    
    BUG=106224
    TEST=manual
    TBR=darin
    
    
    Review URL: http://codereview.chromium.org/8885009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113603 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b68a0e500b8067b65af4cf13e417b63cc2f794b8

--- content/common/view_messages.h
+++ content/common/view_messages.h
@@ -747,7 +747,10 @@ IPC_MESSAGE_ROUTED4(ViewMsg_PaintAtSize,
 // This signals the render view that it can send another UpdateRect message.
 IPC_MESSAGE_ROUTED0(ViewMsg_UpdateRect_ACK)
 
-// Message payload includes a blob that should be cast to WebInputEvent
+// Message payload includes:
+// 1. A blob that should be cast to WebInputEvent
+// 2. An optional boolean value indicating if a RawKeyDown event is associated
+//    to a keyboard shortcut of the browser.
 IPC_MESSAGE_ROUTED0(ViewMsg_HandleInputEvent)
 
 // This message notifies the renderer that the next key event is bound to one
