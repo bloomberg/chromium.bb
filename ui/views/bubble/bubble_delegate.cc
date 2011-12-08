9d0011e9fb741a52aee7e21c0ef5785bd08bcee2
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-08 18:56:42 +0000

    Add support for high contrast mode to bubbles
    BUG=105023
    TEST=
    
    
    Review URL: http://codereview.chromium.org/8887003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113634 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 717da72a338c8cef0ca51132dcd492baba5d08a4

--- ui/views/bubble/bubble_delegate.cc
+++ ui/views/bubble/bubble_delegate.cc
@@ -5,6 +5,7 @@
 #include "ui/views/bubble/bubble_delegate.h"
 
 #include "ui/base/animation/slide_animation.h"
+#include "ui/gfx/color_utils.h"
 #include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/widget/widget.h"
 
@@ -71,8 +72,13 @@ Widget* CreateBorderWidget(BubbleDelegateView* bubble, Widget* parent) {
 
 }  // namespace
 
-// TODO(msw): Use NativeTheme/color_helper (crbug.com/105023).
-const SkColor BubbleDelegateView::kBackgroundColor = SK_ColorWHITE;
+#if defined(OS_WIN) && !defined(USE_AURA)
+const SkColor BubbleDelegateView::kBackgroundColor =
+    color_utils::GetSysSkColor(COLOR_WINDOW);
+#else
+// TODO(beng): source from theme provider.
+const SkColor Bubble::kBackgroundColor = SK_ColorWHITE;
+#endif
 
 BubbleDelegateView::BubbleDelegateView()
     : close_on_esc_(true),
