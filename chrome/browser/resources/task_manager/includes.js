8420cb80f4bb66459cdf14cf7c01814cbd9bc564
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-09 07:15:13 +0000

    WebUI TaskManager: Optimize initialization taskmanager and loading scripts.
    BUG=104136
    TEST=manual on linux
    
    
    Review URL: http://codereview.chromium.org/8713016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@113772 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 56f66a49be4e89803a70a0577c6ab8f8c3243c50

--- chrome/browser/resources/task_manager/includes.js
+++ chrome/browser/resources/task_manager/includes.js
@@ -2,44 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This script includes additional resources via document.write(). Hence, it
-// must be a separate script file loaded before other scripts which would
-// reference the resources.
-
-var css = [
-  'chrome_shared.css',
-  'list.css',
-  'table.css',
-  'menu.css',
-  'button.css',
-];
+// This script loads additional scripts after initialization of task manager.
 
 var script = [
-  'local_strings.js',
-  'i18n_template.js',
-
-  'util.js',
-  'cr.js',
-  'cr/ui.js',
-  'cr/event_target.js',
-  'cr/ui/array_data_model.js',
-  'cr/ui/list_item.js',
-  'cr/ui/list_selection_model.js',
-  'cr/ui/list_single_selection_model.js',
-  'cr/ui/list_selection_controller.js',
-  'cr/ui/list.js',
-
-  'cr/ui/splitter.js',
-  'cr/ui/table/table_splitter.js',
-
-  'cr/ui/table/table_column.js',
-  'cr/ui/table/table_column_model.js',
-  'cr/ui/table/table_header.js',
-  'cr/ui/table/table_list.js',
-  'cr/ui/table.js',
-
-  'cr/ui/grid.js',
-
   'cr/ui/command.js',
   'cr/ui/position_util.js',
   'cr/ui/menu_item.js',
@@ -47,20 +12,30 @@ var script = [
   'cr/ui/context_menu_handler.js',
 ];
 
-(function() {
+/**
+ * Loads delayed scripts.
+ * This function is called by TaskManager::initalize() in main.js.
+ */
+function loadDelayedIncludes(taskmanager) {
   // Switch to 'test harness' mode when loading from a file url.
   var isHarness = document.location.protocol == 'file:';
 
   // In test harness mode we load resources from relative dirs.
   var prefix = isHarness ? './shared/' : 'chrome://resources/';
 
-  for (var i = 0; i < css.length; ++i) {
-    document.write('<link href="' + prefix + 'css/' + css[i] +
-                   '" rel="stylesheet"></link>');
-  }
+  // Number of remaining scripts to load.
+  var remain = script.length;
 
+  // Waits for initialization of task manager.
   for (var i = 0; i < script.length; ++i) {
-    document.write('<script src="' + prefix + 'js/' + script[i] +
-                   '"><\/script>');
+    var s = document.createElement('script');
+    s.onload = function(e) {
+      if (!--remain)
+        taskmanager.delayedInitialize();
+    };
+    s.type = 'text/javascript';
+    s.src = prefix + 'js/' + script[i];
+    s.defer = 'defer';
+    document.body.appendChild(s);
   }
-})();
+}
