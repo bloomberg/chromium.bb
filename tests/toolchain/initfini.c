decb3f7a89f657cb6f06e3055264622725a5d916
pdox@google.com <pdox@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-12-10 00:04:11 +0000

    Augment initfini test by adding 3 function pointers to each array. This will catch bugs in which preinit/init/fini_array elements are skipped due to pointer alignment/size issues.
    Also augment initfini test by adding a shared library test. The dynamic loader is responsible for running init_array when loading a shared object, so this exercises a different code path.
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2482
    Review URL: http://codereview.chromium.org/8892010
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@7402 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 8901571073795e0e513664624b299468e096261f

--- tests/toolchain/initfini.c
+++ tests/toolchain/initfini.c
@@ -1,7 +1,7 @@
 /*
- * Copyright 2010 The Native Client Authors.  All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+ * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 #include <stdio.h>
@@ -12,31 +12,26 @@ typedef void (*FUN_PTR) (void);
 #define ATTR_SEC(sec)\
   __attribute__ ((__used__, section(sec), aligned(sizeof(FUN_PTR))))
 
-#define ADD_FUN_PTR_TO_SEC(name, sec, ptr) \
-  static FUN_PTR name ATTR_SEC(sec)  = { ptr }
-
-
-
 #define MAKE_FUN(name) void name() { printf(#name "\n");}
 
+MAKE_FUN(main_preinit1)
+MAKE_FUN(main_preinit2)
+MAKE_FUN(main_preinit3)
+static const FUN_PTR array_preinit[] ATTR_SEC(".preinit_array") =
+    { main_preinit1, main_preinit2, main_preinit3 };
 
-#if 0
-/* .ctors and .dtors are not supported in PNaCl. */
-MAKE_FUN(fun_ctor)
-ADD_FUN_PTR_TO_SEC(array_ctor[1], ".ctors", fun_ctor);
 
-MAKE_FUN(fun_dtor)
-ADD_FUN_PTR_TO_SEC(array_dtor[1], ".dtors", fun_dtor);
-#endif  /* 0 */
+MAKE_FUN(main_init1)
+MAKE_FUN(main_init2)
+MAKE_FUN(main_init3)
+static const FUN_PTR array_init[] ATTR_SEC(".init_array") =
+    { main_init1, main_init2, main_init3 };
 
-MAKE_FUN(fun_preinit)
-ADD_FUN_PTR_TO_SEC(array_preinit[1], ".preinit_array", fun_preinit);
-
-MAKE_FUN(fun_init)
-ADD_FUN_PTR_TO_SEC(array_init[1], ".init_array", fun_init);
-
-MAKE_FUN(fun_fini)
-ADD_FUN_PTR_TO_SEC(array_fini[1], ".fini_array", fun_fini);
+MAKE_FUN(main_fini1)
+MAKE_FUN(main_fini2)
+MAKE_FUN(main_fini3)
+static const FUN_PTR array_fini[] ATTR_SEC(".fini_array") =
+    { main_fini1, main_fini2, main_fini3 };
 
 /* NOTE: there is NO .prefini_array */
 
