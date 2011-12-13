ca6b1c34f092c5384b79da1f133c6d5b2192457c
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-13 08:18:51 +0000

    Track webcore worker message loops in chromium.
    BUG=106265
    TEST=
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=114157
    
    Review URL: http://codereview.chromium.org/8785013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114192 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0213480a48c9d02f15ad80e5bc670303c65ea11b

---
+++ webkit/glue/worker_task_runner_unittest.cc
@@ -0,0 +1,56 @@
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/logging.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "webkit/glue/worker_task_runner.h"
+
+namespace webkit_glue {
+
+class WorkerTaskRunnerTest : public testing::Test {
+ public:
+  void FakeStart() {
+    task_runner_.OnWorkerRunLoopStarted(WebKit::WebWorkerRunLoop());
+  }
+  void FakeStop() {
+    task_runner_.OnWorkerRunLoopStopped(WebKit::WebWorkerRunLoop());
+  }
+  WorkerTaskRunner task_runner_;
+};
+
+class MockObserver : public WorkerTaskRunner::Observer {
+ public:
+  MOCK_METHOD0(OnWorkerRunLoopStopped, void());
+  void RemoveSelfOnNotify() {
+    ON_CALL(*this, OnWorkerRunLoopStopped()).WillByDefault(
+        testing::Invoke(this, &MockObserver::RemoveSelf));
+  }
+  void RemoveSelf() {
+    runner_->RemoveStopObserver(this);
+  }
+  WorkerTaskRunner* runner_;
+};
+
+TEST_F(WorkerTaskRunnerTest, BasicObservingAndWorkerId) {
+  ASSERT_EQ(0, task_runner_.CurrentWorkerId());
+  MockObserver o;
+  EXPECT_CALL(o, OnWorkerRunLoopStopped()).Times(1);
+  FakeStart();
+  task_runner_.AddStopObserver(&o);
+  ASSERT_LT(0, task_runner_.CurrentWorkerId());
+  FakeStop();
+}
+
+TEST_F(WorkerTaskRunnerTest, CanRemoveSelfDuringNotification) {
+  MockObserver o;
+  o.RemoveSelfOnNotify();
+  o.runner_ = &task_runner_;
+  EXPECT_CALL(o, OnWorkerRunLoopStopped()).Times(1);
+  FakeStart();
+  task_runner_.AddStopObserver(&o);
+  FakeStop();
+}
+
+}
