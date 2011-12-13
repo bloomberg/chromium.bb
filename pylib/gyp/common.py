b7b7f31931e287d42e767840249194570d07f975
bradnelson@google.com <bradnelson@google.com@78cadc50-ecff-11dd-a971-7dbc132099af>
2011-12-13 22:28:05 +0000

    Correctly handle canonicalizing dependencies.
    BUG=https://bugs.webkit.org/show_bug.cgi?id=74323
    TEST=relative
    
    R=dpranke@chromium.org
    Review URL: http://codereview.chromium.org/8930003
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1105 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/common.py
+++ pylib/gyp/common.py
@@ -76,6 +76,8 @@ def ResolveTarget(build_file, target, toolset):
       # and os.path.join will return it as-is.
       build_file = os.path.normpath(os.path.join(os.path.dirname(build_file),
                                                  parsed_build_file))
+      # Further (to handle cases like ../cwd), make it relative to cwd)
+      build_file = RelativePath(build_file, '.')
     else:
       build_file = parsed_build_file
 
