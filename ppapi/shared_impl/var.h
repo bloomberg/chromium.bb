8dcd5a67ffc001df1841b27f2f4478d9e1f73823
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-14 06:32:31 +0000

    Var keeps invalid var_id if VarTracker release it and there is another reference.
    When VarTracker remove PP_Var from VarMap, it release its Var object
    if needed, but never reset var_id stored in Var object. Then, if Var's
    reference count is not 1, Var continue to exist with invalid var_id until
    the last reference is released.
    
    BUG=87310
    TEST=ui_tests --gtest_filter='PPAPITest.WebSocket_*'
    
    Review URL: http://codereview.chromium.org/8872065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114384 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9384b0088557335e5ff12049d2053835fceeae3e

--- ppapi/shared_impl/var.h
+++ ppapi/shared_impl/var.h
@@ -17,6 +17,7 @@ namespace ppapi {
 class NPObjectVar;
 class ProxyObjectVar;
 class StringVar;
+class VarTracker;
 
 // Var -------------------------------------------------------------------------
 
@@ -48,6 +49,8 @@ class PPAPI_SHARED_EXPORT Var : public base::RefCounted<Var> {
   int32 GetExistingVarID() const;
 
  protected:
+  friend class VarTracker;
+
   Var();
 
   // Returns the unique ID associated with this string or object, creating it
@@ -61,6 +64,9 @@ class PPAPI_SHARED_EXPORT Var : public base::RefCounted<Var> {
   // Sets the internal object ID. This assumes that the ID hasn't been set
   // before. This is used in cases where the ID is generated externally.
   void AssignVarID(int32 id);
+
+  // Reset the assigned object ID.
+  void ResetVarID() { AssignVarID(0); };
 
  private:
   // This will be 0 if no ID has been assigned (this happens lazily).
