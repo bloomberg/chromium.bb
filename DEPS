a52ca14e77f5e44e7513c41cd62d5f18b8547d38
pdox@google.com <pdox@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2011-12-14 05:37:58 +0000

    Roll toolchain DEPS and commit test changes for CL:
    http://codereview.chromium.org/8934018
    
    TBR= jvoung
    Review URL: http://codereview.chromium.org/8898037
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@7428 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 1bcf3c39d7ee70522c5be20220570ff5b58ea261

--- DEPS
+++ DEPS
@@ -2,8 +2,8 @@
 
 vars = {
   # These two should be updated to the same revision whenever possible.
-  "x86_toolchain_version": "7399",
-  "pnacl_toolchain_version": "7399",
+  "x86_toolchain_version": "7427",
+  "pnacl_toolchain_version": "7427",
 
   # ARM trusted toolchain version cannot be changed for now.
   "arm_trusted_toolchain_version": "7000",
--- tests/toolchain/llvm_bitmanip_intrinsics.c
+++ tests/toolchain/llvm_bitmanip_intrinsics.c
@@ -18,12 +18,12 @@ uint32_t llvm_intrinsic_ctpop(uint32_t) __asm__("llvm.ctpop.i32");
 uint64_t llvm_intrinsic_ctpopll(uint64_t) __asm__("llvm.ctpop.i64");
 
 /* __builtin_clz(x) */
-uint32_t llvm_intrinsic_ctlz(uint32_t) __asm__("llvm.ctlz.i32");
-uint64_t llvm_intrinsic_ctlzll(uint64_t) __asm__("llvm.ctlz.i64");
+uint32_t llvm_intrinsic_ctlz(uint32_t, _Bool) __asm__("llvm.ctlz.i32");
+uint64_t llvm_intrinsic_ctlzll(uint64_t, _Bool) __asm__("llvm.ctlz.i64");
 
 /* __builtin_ctz(x) */
-uint32_t llvm_intrinsic_cttz(uint32_t) __asm__("llvm.cttz.i32");
-uint64_t llvm_intrinsic_cttzll(uint64_t) __asm__("llvm.cttz.i64");
+uint32_t llvm_intrinsic_cttz(uint32_t, _Bool) __asm__("llvm.cttz.i32");
+uint64_t llvm_intrinsic_cttzll(uint64_t, _Bool) __asm__("llvm.cttz.i64");
 
 /* __builtin_bswap32(x) */
 uint32_t llvm_intrinsic_bswap(uint32_t) __asm__("llvm.bswap.i32");
@@ -36,21 +36,24 @@ volatile uint64_t i64[] = {0xaa000000ll, 0xaa00000000ll};
 #define print_op(op, x) \
   printf("%s: %u\n", #op, (unsigned) llvm_intrinsic_ ## op(x))
 
+#define print_op2(op, x, y) \
+  printf("%s: %u\n", #op, (unsigned) llvm_intrinsic_ ## op(x, y))
+
 int main(int argc, char* argv[]) {
   int i;
   for (i = 0; i < ARRAY_SIZE_UNSAFE(i32); ++i) {
     printf("\ni32 value is: %08x\n", (unsigned) i32[i]);
     print_op(ctpop, i32[i]);
-    print_op(ctlz, i32[i]);
-    print_op(cttz, i32[i]);
+    print_op2(ctlz, i32[i], 1);
+    print_op2(cttz, i32[i], 1);
     printf("%s: %08x\n", "bswap", (unsigned) llvm_intrinsic_bswap(i32[i]));
   }
 
   for (i = 0; i < ARRAY_SIZE_UNSAFE(i64); ++i) {
     printf("\ni64 value is: %016llx\n",  i64[i]);
     print_op(ctpopll, i64[i]);
-    print_op(ctlzll, i64[i]);
-    print_op(cttzll, i64[i]);
+    print_op2(ctlzll, i64[i], 1);
+    print_op2(cttzll, i64[i], 1);
     printf("%s: %016llx\n", "bswapll", llvm_intrinsic_bswapll(i64[i]));
   }
 
