1429cf274a2c8d5a0afb05b27e9112b4a13756f0
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 00:48:12 +0000

    Print preview: Prevent a crash when zooming with the scroll wheel.
    BUG=107222
    TEST=see bug
    
    Review URL: http://codereview.chromium.org/8918022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114550 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 54b7fdb52ddeeeda32d2a5e16376883b09707c3f

--- chrome/browser/tab_contents/tab_contents_view_gtk.cc
+++ chrome/browser/tab_contents/tab_contents_view_gtk.cc
@@ -54,18 +54,22 @@ gboolean OnMouseMove(GtkWidget* widget, GdkEventMotion* event,
 // See tab_contents_view_views.cc for discussion of mouse scroll zooming.
 gboolean OnMouseScroll(GtkWidget* widget, GdkEventScroll* event,
                        TabContents* tab_contents) {
-  if ((event->state & gtk_accelerator_get_default_mod_mask()) ==
+  if ((event->state & gtk_accelerator_get_default_mod_mask()) !=
       GDK_CONTROL_MASK) {
-    if (event->direction == GDK_SCROLL_DOWN) {
-      tab_contents->delegate()->ContentsZoomChange(false);
-      return TRUE;
-    } else if (event->direction == GDK_SCROLL_UP) {
-      tab_contents->delegate()->ContentsZoomChange(true);
-      return TRUE;
-    }
+    return FALSE;
   }
 
-  return FALSE;
+  TabContentsDelegate* delegate = tab_contents->delegate();
+  if (!delegate)
+    return FALSE;
+
+  if (!(event->direction == GDK_SCROLL_DOWN ||
+        event->direction == GDK_SCROLL_UP)) {
+    return FALSE;
+  }
+
+  delegate->ContentsZoomChange(event->direction == GDK_SCROLL_UP);
+  return TRUE;
 }
 
 }  // namespace
