3ddcd0018ef97838064c0452e9a20cca31aeb264
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-15 18:06:04 +0000

    Restore kTraceStartup flag propogation to gpu process
    This was accidentally removed in http://codereview.chromium.org/8491043/diff/19001/content/browser/gpu/gpu_process_host.cc
    
    BUG=None
    TEST=start browser with --trace-startup
    
    
    Review URL: http://codereview.chromium.org/8912005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114662 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 25fba3fde8096d254d7823b19d0ecdcf717aac05

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -570,6 +570,7 @@ bool GpuProcessHost::LaunchGpuProcess(const std::string& channel_id) {
     switches::kGpuStartupDialog,
     switches::kLoggingLevel,
     switches::kNoSandbox,
+    switches::kTraceStartup,
   };
   cmd_line->CopySwitchesFrom(browser_command_line, kSwitchNames,
                              arraysize(kSwitchNames));
