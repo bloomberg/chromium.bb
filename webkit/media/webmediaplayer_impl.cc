99ce6b34f25f1fe0e01a0b6f8b59f348579d3ffa
vrk@chromium.org <vrk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 21:33:13 +0000

    Revert effects of r110733 (again) for autoplay breakage.
    The delayed firing of the canplaythrough event for video broke autoplay in
    certain situations. Since firing canplaythrough "properly" isn't a high-priority
    feature, disable it for now until a proper fix can be had.
    
    BUG=106480
    TEST=media_unittests,video with autoplay attribute begins playback immediately
    
    
    Review URL: http://codereview.chromium.org/8966015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114859 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9820f6181d50ef5328dd5780d05ff838270c3e34

--- webkit/media/webmediaplayer_impl.cc
+++ webkit/media/webmediaplayer_impl.cc
@@ -761,7 +761,10 @@ void WebMediaPlayerImpl::OnPipelineInitialize(PipelineStatus status) {
       SetNetworkState(WebKit::WebMediaPlayer::Loaded);
 
     SetReadyState(WebKit::WebMediaPlayer::HaveMetadata);
-    SetReadyState(WebKit::WebMediaPlayer::HaveFutureData);
+    // Fire canplaythrough immediately after playback begins because of
+    // crbug.com/106480.
+    // TODO(vrk): set ready state to HaveFutureData when bug above is fixed.
+    SetReadyState(WebKit::WebMediaPlayer::HaveEnoughData);
   } else {
     // TODO(hclam): should use |status| to determine the state
     // properly and reports error using MediaError.
@@ -850,7 +853,10 @@ void WebMediaPlayerImpl::OnNetworkEvent(NetworkEvent type) {
       SetNetworkState(WebKit::WebMediaPlayer::Idle);
       break;
     case media::CAN_PLAY_THROUGH:
-      SetReadyState(WebKit::WebMediaPlayer::HaveEnoughData);
+      // Temporarily disable delayed firing of CAN_PLAY_THROUGH due to
+      // crbug.com/106480.
+      // TODO(vrk): uncomment code below when bug above is fixed.
+      // SetReadyState(WebKit::WebMediaPlayer::HaveEnoughData);
       break;
     default:
       NOTREACHED();
