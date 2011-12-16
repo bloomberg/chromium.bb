ae2002300071a9b06a6703baaf9bbbe9ccfd066d
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-16 23:17:32 +0000

    Add linux_shared to the default try set!
    BUG=105750
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/8969030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114877 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7838696019d2c39f30f8cfcbe8479f9cd52d06b3

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -338,7 +338,7 @@ def GetPreferredTrySlaves(project, change):
       f.LocalPath().endswith(('.mm', '.m')) for f in change.AffectedFiles())
   if only_objc_files:
     return ['mac_rel']
-  preferred = ['win_rel', 'linux_rel', 'mac_rel']
+  preferred = ['win_rel', 'linux_rel', 'mac_rel', 'linux_shared']
   aura_re = '_aura[^/]*[.][^/]*'
   if any(re.search(aura_re, f.LocalPath()) for f in change.AffectedFiles()):
     preferred.append('linux_chromeos_aura:compile')
