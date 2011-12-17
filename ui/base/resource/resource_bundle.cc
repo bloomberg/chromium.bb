a776ec3c9731157c4f9e97cd31fb052431657b6f
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-17 02:27:04 +0000

    Made wallpaper smaller.
    BUG=105508
    TEST=none
    
    Review URL: http://codereview.chromium.org/8983005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@114902 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 196fd8a8240ffa5750867f4253475a1f440ca2a6

--- ui/base/resource/resource_bundle.cc
+++ ui/base/resource/resource_bundle.cc
@@ -19,7 +19,6 @@
 #include "ui/base/resource/data_pack.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/base/ui_base_switches.h"
-#include "ui/gfx/codec/jpeg_codec.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/image/image.h"
@@ -370,17 +369,12 @@ SkBitmap* ResourceBundle::LoadBitmap(DataHandle data_handle, int resource_id) {
     return NULL;
 
   SkBitmap bitmap;
-  if (gfx::PNGCodec::Decode(memory->front(), memory->size(), &bitmap))
-    return new SkBitmap(bitmap);
+  if (!gfx::PNGCodec::Decode(memory->front(), memory->size(), &bitmap)) {
+    NOTREACHED() << "Unable to decode theme image resource " << resource_id;
+    return NULL;
+  }
 
-  // 99% of our assets are PNGs, however fallback to JPEG.
-  SkBitmap* allocated_bitmap =
-      gfx::JPEGCodec::Decode(memory->front(), memory->size());
-  if (allocated_bitmap)
-    return allocated_bitmap;
-
-  NOTREACHED() << "Unable to decode theme image resource " << resource_id;
-  return NULL;
+  return new SkBitmap(bitmap);
 }
 
 gfx::Image* ResourceBundle::GetEmptyImage() {
