20a6f3731be2d4575c647b4734ce3efaf9131fb5
David James <davidjames@google.com>
2011-12-18 10:48:52 -0800

    Increment branch build number on stabilize branch.
    BUG=chromium-os:24375
    TEST=Unit tests.
    
    Change-Id: Iab1886bfd672d24a01c64f47541e0cffc3e81ce6
    Reviewed-on: https://gerrit.chromium.org/gerrit/13128
    Reviewed-by: David James <davidjames@chromium.org>
    Tested-by: David James <davidjames@chromium.org>
    (cherry picked from commit 042a86d9c2b7965208559ed68225deaac3b51d7e)

--- buildbot/cbuildbot_stages.py
+++ buildbot/cbuildbot_stages.py
@@ -158,7 +158,11 @@ class ManifestVersionedSyncStage(SyncStage):
 
   def InitializeManifestManager(self):
     """Initializes a manager that manages manifests for associated stages."""
-    increment = 'build' if self._tracking_branch == 'master' else 'branch'
+    # On the master branch, increment the build number. On the stabilize
+    # branch, increment the patch number. For other branches, increment
+    # the branch build number.
+    _BRANCH_INCREMENT_TYPE = {'master': 'build', 'stabilize': 'patch'}
+    increment = _BRANCH_INCREMENT_TYPE.get(self._tracking_branch, 'branch')
 
     dry_run = self._options.debug
     source_repo = repository.RepoRepository(
