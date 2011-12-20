892f36e5fcf03d129252daf56ef675473fb57685
amit@chromium.org <amit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-20 22:53:03 +0000

    Cleanup JingleInfoRequest on JingleSessionManager::Close
    If NAT traversal is turned on then JingleInfoRequest adds itself as a
    listener to the signal strategy.  Delete the request in
    SessionManager::Close so that it removes itself as a listener and
    signal strategy can be cleaned up.
    
    BUG=108212
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9004024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115210 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: becdff12426f86bf21894822b6780bc80fee4f8f

--- remoting/protocol/jingle_session_manager.cc
+++ remoting/protocol/jingle_session_manager.cc
@@ -111,6 +111,7 @@ void JingleSessionManager::Close() {
   DCHECK(CalledOnValidThread());
 
   if (!closed_) {
+    jingle_info_request_.reset();
     cricket_session_manager_->RemoveClient(kChromotingXmlNamespace);
     jingle_signaling_connector_.reset();
     closed_ = true;
