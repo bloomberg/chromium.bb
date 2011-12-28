bloomberg/chromium.bb

commit a54857949f847cb11f4584effd9c250beb275287

Fix many* python scripts in src/
Make sure that:
- shebang is only present for executable files
- shebang is #!/usr/bin/env python
- __main__ is only present for executable files
- file's executable bit is coherent

Also fix EOF LF to be only one.

* Do not fix them all at once otherwise the CL would be too large.

TBR=jamiewalch@chromium.org
BUG=105108
TEST=

Review URL: http://codereview.chromium.org/8665013

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@111427 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: aa539ba4c9dc3e99a4e55f21a10fb79b9402637c

remoting/tools/register_host.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-#
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-#
-# register_host.py registers new hosts in chromoting directory. It
-# asks for username/password and then writes these settings to config file.
+
+"""Registers new hosts in chromoting directory.
+
+It asks for username/password and then writes these settings to config file.
+"""
 
 import getpass
 import os
@@ -22,67 +23,74 @@ def random_uuid():
   return ("%04x%04x-%04x-%04x-%04x-%04x%04x%04x" %
     tuple(map(lambda x: random.randrange(0,65536), range(8))))
 
-server = 'www.googleapis.com'
-url = 'https://' + server + '/chromoting/v1/@me/hosts'
 
-settings_filepath = os.path.join(os.path.expanduser('~'),
-                                 '.ChromotingConfig.json')
+def main():
+  server = 'www.googleapis.com'
+  url = 'https://' + server + '/chromoting/v1/@me/hosts'
 
-print "Email:",
-email = raw_input()
-password = getpass.getpass("Password: ")
+  settings_filepath = os.path.join(os.path.expanduser('~'),
+                                  '.ChromotingConfig.json')
 
-chromoting_auth = gaia_auth.GaiaAuthenticator('chromoting')
-auth_token = chromoting_auth.authenticate(email, password)
+  print "Email:",
+  email = raw_input()
+  password = getpass.getpass("Password: ")
 
-host_id = random_uuid()
-print "HostId:", host_id
-host_name = socket.gethostname()
-print "HostName:", host_name
+  chromoting_auth = gaia_auth.GaiaAuthenticator('chromoting')
+  auth_token = chromoting_auth.authenticate(email, password)
 
-print "Generating RSA key pair...",
-(private_key, public_key) = keygen.generateRSAKeyPair()
-print "Done"
+  host_id = random_uuid()
+  print "HostId:", host_id
+  host_name = socket.gethostname()
+  print "HostName:", host_name
 
-params = ('{"data":{' + \
-          '"hostId": "%(hostId)s",' + \
-          '"hostName": "%(hostName)s",' + \
-          '"publicKey": "%(publicKey)s"}}') % \
-          {'hostId': host_id, 'hostName': host_name,
-           'publicKey': public_key}
-headers = {"Authorization": "GoogleLogin auth=" + auth_token,
-           "Content-Type": "application/json" }
-request = urllib2.Request(url, params, headers)
+  print "Generating RSA key pair...",
+  (private_key, public_key) = keygen.generateRSAKeyPair()
+  print "Done"
 
-opener = urllib2.OpenerDirector()
-opener.add_handler(urllib2.HTTPDefaultErrorHandler())
+  params = ('{"data":{' +
+            '"hostId": "%(hostId)s",' +
+            '"hostName": "%(hostName)s",' +
+            '"publicKey": "%(publicKey)s"}}') %
+            {'hostId': host_id, 'hostName': host_name,
+            'publicKey': public_key}
+  headers = {"Authorization": "GoogleLogin auth=" + auth_token,
+            "Content-Type": "application/json" }
+  request = urllib2.Request(url, params, headers)
 
-print
-print "Registering host with directory service..."
-try:
-  res = urllib2.urlopen(request)
-  data = res.read()
-except urllib2.HTTPError, err:
-  print >> sys.stderr, "Directory returned error:", err
-  print >> sys.stderr, err.fp.read()
-  sys.exit(1)
+  opener = urllib2.OpenerDirector()
+  opener.add_handler(urllib2.HTTPDefaultErrorHandler())
 
-print "Done"
+  print
+  print "Registering host with directory service..."
+  try:
+    res = urllib2.urlopen(request)
+    data = res.read()
+  except urllib2.HTTPError, err:
+    print >> sys.stderr, "Directory returned error:", err
+    print >> sys.stderr, err.fp.read()
+    return 1
 
-# Get token that the host will use to athenticate in talk network.
-authenticator = gaia_auth.GaiaAuthenticator('chromiumsync');
-auth_token = authenticator.authenticate(email, password)
+  print "Done"
 
-# Write settings file.
-os.umask(0066) # Set permission mask for created file.
-settings_file = open(settings_filepath, 'w')
-settings_file.write('{\n');
-settings_file.write('  "xmpp_login" : "' + email + '",\n')
-settings_file.write('  "xmpp_auth_token" : "' + auth_token + '",\n')
-settings_file.write('  "host_id" : "' + host_id + '",\n')
-settings_file.write('  "host_name" : "' + host_name + '",\n')
-settings_file.write('  "private_key" : "' + private_key + '",\n')
-settings_file.write('}\n')
-settings_file.close()
+  # Get token that the host will use to athenticate in talk network.
+  authenticator = gaia_auth.GaiaAuthenticator('chromiumsync');
+  auth_token = authenticator.authenticate(email, password)
 
-print 'Configuration saved in', settings_filepath
+  # Write settings file.
+  os.umask(0066) # Set permission mask for created file.
+  settings_file = open(settings_filepath, 'w')
+  settings_file.write('{\n');
+  settings_file.write('  "xmpp_login" : "' + email + '",\n')
+  settings_file.write('  "xmpp_auth_token" : "' + auth_token + '",\n')
+  settings_file.write('  "host_id" : "' + host_id + '",\n')
+  settings_file.write('  "host_name" : "' + host_name + '",\n')
+  settings_file.write('  "private_key" : "' + private_key + '",\n')
+  settings_file.write('}\n')
+  settings_file.close()
+
+  print 'Configuration saved in', settings_filepath
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main())

base/PRESUBMIT.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.

chrome_frame/combine_libs.py
@@ -1,4 +1,5 @@
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+#!/usr/bin/env python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -108,7 +109,8 @@ def Main():
     del os.environ['VS_UNICODE_OUTPUT']
 
   CombineLibraries(output, remove_re, args)
+  return 0
 
 
 if __name__ == '__main__':
-  Main()
+  sys.exit(Main())

chrome_frame/tools/helper_shutdown.py
@@ -1,13 +1,17 @@
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+#!/usr/bin/env python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-'''This is a simple helper script to shut down the Chrome Frame helper process.
-It needs the Python Win32 extensions.'''
+"""This is a simple helper script to shut down the Chrome Frame helper process.
+
+Requires Python Win32 extensions.
+"""
 
 import pywintypes
 import sys
 import win32gui
 import win32con
+
 
 def main():
   exit_code = 0
@@ -23,8 +27,8 @@ def main():
       print 'Failed to shutdown Chrome Frame helper process: '
       print ex
       exit_code = 1
-
   return exit_code
+
 
 if __name__ == '__main__':
   sys.exit(main())

chrome_frame/tools/smoke_test.py
@@ -1,9 +1,11 @@
-# Copyright (c) 2010 The Chromium Authors. All rights reserved.
+#!/usr/bin/env python
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-'''This script builds and runs the Chrome Frame unit and integration tests,
+"""Builds and runs the Chrome Frame unit and integration tests,
 the exit status of the scrip is the number of failed tests.
-'''
+"""
+
 import os.path
 import sys
 import win32com.client

gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-#
+#!/usr/bin/env python
 # Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -5992,7 +5991,9 @@ def main(argv):
 
   if gen.errors > 0:
     print "%d errors" % gen.errors
-    sys.exit(1)
+    return 1
+  return 0
+
 
 if __name__ == '__main__':
-  main(sys.argv[1:])
+  sys.exit(main(sys.argv[1:]))

jingle/PRESUBMIT.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.

printing/cups_config_helper.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -20,39 +20,48 @@ import subprocess
 import sys
 
 def usage():
-    print 'usage: %s {--cflags|--ldflags|--libs}' % sys.argv[0]
-    sys.exit(1)
+  print 'usage: %s {--cflags|--ldflags|--libs}' % sys.argv[0]
+
 
 def run_cups_config(mode):
-    """Run cups-config with all --cflags etc modes, parse out the mode we want,
-    and return those flags as a list."""
+  """Run cups-config with all --cflags etc modes, parse out the mode we want,
+  and return those flags as a list."""
 
-    cups = subprocess.Popen(['cups-config', '--cflags', '--ldflags', '--libs'],
-                            stdout=subprocess.PIPE)
-    flags = cups.communicate()[0].strip()
+  cups = subprocess.Popen(['cups-config', '--cflags', '--ldflags', '--libs'],
+                          stdout=subprocess.PIPE)
+  flags = cups.communicate()[0].strip()
 
-    flags_subset = []
-    for flag in flags.split():
-        flag_mode = None
-        if flag.startswith('-l'):
-            flag_mode = '--libs'
-        elif (flag.startswith('-L') or flag.startswith('-Wl,')):
-            flag_mode = '--ldflags'
-        elif (flag.startswith('-I') or flag.startswith('-D')):
-            flag_mode = '--cflags'
+  flags_subset = []
+  for flag in flags.split():
+    flag_mode = None
+    if flag.startswith('-l'):
+      flag_mode = '--libs'
+    elif (flag.startswith('-L') or flag.startswith('-Wl,')):
+      flag_mode = '--ldflags'
+    elif (flag.startswith('-I') or flag.startswith('-D')):
+      flag_mode = '--cflags'
 
-        # Be conservative: for flags where we don't know which mode they
-        # belong in, always include them.
-        if flag_mode is None or flag_mode == mode:
-            flags_subset.append(flag)
+    # Be conservative: for flags where we don't know which mode they
+    # belong in, always include them.
+    if flag_mode is None or flag_mode == mode:
+      flags_subset.append(flag)
 
-    return flags_subset
+  return flags_subset
 
-if len(sys.argv) != 2:
+
+def main():
+  if len(sys.argv) != 2:
     usage()
+    return 1
 
-mode = sys.argv[1]
-if mode not in ('--cflags', '--libs', '--ldflags'):
+  mode = sys.argv[1]
+  if mode not in ('--cflags', '--libs', '--ldflags'):
     usage()
-flags = run_cups_config(mode)
-print ' '.join(flags)
+    return 1
+  flags = run_cups_config(mode)
+  print ' '.join(flags)
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main())

commit 17553bd80238fbaaed89b34052a50ee34182290d

Fix python syntax error in register_host.py introduced by r111427.
TBR=maruel@chromium.org
TEST=register_host.py works as expected.

Review URL: http://codereview.chromium.org/8956058

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@115903 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 39bd290893ffe32a463231c19445d1de7744986f

remoting/tools/register_host.py
@@ -50,7 +50,7 @@ def main():
   params = ('{"data":{' +
             '"hostId": "%(hostId)s",' +
             '"hostName": "%(hostName)s",' +
-            '"publicKey": "%(publicKey)s"}}') %
+            '"publicKey": "%(publicKey)s"}}') % \
             {'hostId': host_id, 'hostName': host_name,
             'publicKey': public_key}
   headers = {"Authorization": "GoogleLogin auth=" + auth_token,