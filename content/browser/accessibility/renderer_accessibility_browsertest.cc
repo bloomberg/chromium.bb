c4d9966c3cb806f8537dded3f8ac5e412eb057b2
dtseng@chromium.org <dtseng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2011-12-29 23:45:37 +0000

    Re-enable these tests as they are no longer failing.
    BUG=107985
    TEST=Green try bots.
    
    
    Review URL: http://codereview.chromium.org/9053002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116006 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0370485992fa455ca06c9cb7305e5ca0f8a010c0

--- content/browser/accessibility/renderer_accessibility_browsertest.cc
+++ content/browser/accessibility/renderer_accessibility_browsertest.cc
@@ -279,10 +279,9 @@ IN_PROC_BROWSER_TEST_F(RendererAccessibilityBrowserTest,
   EXPECT_EQ(cell2.id, column2.indirect_child_ids[0]);
 }
 
-// Test crashes/times out, see http://crbug.com/107985
 IN_PROC_BROWSER_TEST_F(
     RendererAccessibilityBrowserTest,
-    DISABLED_CrossPlatformMultipleInheritanceAccessibility2) {
+    CrossPlatformMultipleInheritanceAccessibility2) {
   // Here's another html snippet where WebKit puts the same node as a child
   // of two different parents. Instead of checking the exact output, just
   // make sure that no id is reused in the resulting tree.
@@ -372,9 +371,8 @@ IN_PROC_BROWSER_TEST_F(RendererAccessibilityBrowserTest,
   RecursiveAssertUniqueIds(tree, &ids);
 }
 
-// Test crashes/times out, see http://crbug.com/107985
 IN_PROC_BROWSER_TEST_F(RendererAccessibilityBrowserTest,
-                       DISABLED_CrossPlatformTableSpan) {
+                       CrossPlatformTableSpan) {
   // +---+---+---+
   // |   1   | 2 |
   // +---+---+---+
