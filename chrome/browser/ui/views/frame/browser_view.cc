f9acf61e0d805669cc3bbff9da81dfd1ce676b41
cem.kocagil@gmail.com <cem.kocagil@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-04 21:25:53 +0000

    Popup windows' state should always be SHOW_STATE_NORMAL, not depend on the saved window state.
    BUG=106967
    TEST=Windows opened by window.open should be unmaximized by default
    
    Review URL: http://codereview.chromium.org/9004046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116389 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0f3851cecbe7c7ba5234f4e5f508bf38bbd4c838

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -1643,6 +1643,7 @@ bool BrowserView::GetSavedWindowPlacement(
     }
 
     *bounds = window_rect;
+    *show_state = ui::SHOW_STATE_NORMAL;
   }
 
   // We return true because we can _always_ locate reasonable bounds using the
