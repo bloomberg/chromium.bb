baecfd584f0103a8b628e735c54ec0f009ff97d9
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 01:22:58 +0000

    [uber] make the uber page itself nice
    i18n strings, among other things, are TODO
    
    BUG=109183
    TEST=manual
    
    Review URL: http://codereview.chromium.org/9030033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116434 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d9f9821c2ddacdff061a74862391f6013277f88e

--- chrome/browser/resources/uber/uber.js
+++ chrome/browser/resources/uber/uber.js
@@ -1,6 +1,42 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-window.onload = function() {
-}
+cr.define('uber', function() {
+
+  /**
+   * Handles page initialization.
+   */
+  function onLoad() {
+    var navigationItems = document.querySelectorAll('#navigation li');
+    var iframes = document.querySelectorAll('.iframe-container');
+
+    for (var i = 0; i < navigationItems.length; ++i) {
+      var navItem = navigationItems[i];
+      navItem.associatedIframe = iframes[i];
+      navItem.addEventListener('click', onNavItemClicked);
+    }
+  }
+
+  function onNavItemClicked(e) {
+    var iframe = e.currentTarget.associatedIframe;
+    var currentIframe =
+        document.querySelector('.iframe-container.selected');
+    if (currentIframe != iframe) {
+      currentIframe.classList.remove('selected');
+      iframe.classList.add('selected');
+    }
+
+    var currentNavItem = document.querySelector('li.selected');
+    if (currentNavItem != e.currentTarget) {
+      currentNavItem.classList.remove('selected');
+      e.currentTarget.classList.add('selected');
+    }
+  }
+
+  return {
+    onLoad: onLoad
+  };
+});
+
+document.addEventListener('DOMContentLoaded', uber.onLoad);
