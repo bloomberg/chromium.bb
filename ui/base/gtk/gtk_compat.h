cc963067d0a5fe1fbbf0a32380c03d4b94103a71
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-05 20:04:39 +0000

    Linux: Make the minimal required GTK version in Google Chrome builds 2.18.
    BUG=106689
    TEST=Official builders go green.
    TBR=erg,cmp
    Review URL: http://codereview.chromium.org/9108017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116534 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 963e5d08986f5b9ca65d38304c33ce399ada12cb

--- ui/base/gtk/gtk_compat.h
+++ ui/base/gtk/gtk_compat.h
@@ -16,12 +16,12 @@
 // This file is organized first by version, and then with each version,
 // alphabetically by method.
 //
-// For official builds, we want to support RHEL 6, which uses GTK 2.18, but the
-// official builder is Ubuntu Lucid with GTK 2.20. Thus for official builds, we
-// define the GTK 2.20.0 compatibility functions even though the system GTK
-// provides the functions.
+// For Google Chrome builds, we want to support RHEL 6, which uses GTK 2.18,
+// but the official builder is Ubuntu Lucid with GTK 2.20. Thus for Google
+// Chrome builds, we define the GTK 2.20.0 compatibility functions even though
+// the system GTK provides the functions.
 
-#if !GTK_CHECK_VERSION(2, 20, 0) || defined(OFFICIAL_BUILD)
+#if !GTK_CHECK_VERSION(2, 20, 0) || defined(GOOGLE_CHROME_BUILD)
 inline gboolean gtk_widget_get_mapped(GtkWidget* widget) {
   return GTK_WIDGET_MAPPED(widget);
 }
@@ -49,7 +49,7 @@ inline void gtk_widget_set_realized(GtkWidget* widget,
   else
     GTK_WIDGET_UNSET_FLAGS(widget, GTK_REALIZED);
 }
-#endif  // !GTK_CHECK_VERSION(2, 20, 0) || defined(OFFICIAL_BUILD)
+#endif  // !GTK_CHECK_VERSION(2, 20, 0) || defined(GOOGLE_CHROME_BUILD)
 
 #if !GTK_CHECK_VERSION(2, 22, 0)
 inline gint gdk_visual_get_depth(GdkVisual* visual) {
