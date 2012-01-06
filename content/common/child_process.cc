8def136f69e4279231342d67ddb8448195acd54f
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 22:19:45 +0000

    Crash if the render process fails to create its IO thread.
    We are seeing some latent crashes that I believe are a consequence of this earlier failure. So this change should help clarify what is happening.
    
    A next step would be to determine the particular platform error that caused thread creation to fail.
    (However base::PlatformThread doesn't currently expose that).
    
    BUG=109302
    
    
    Review URL: http://codereview.chromium.org/9122010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116750 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1882d05858db4de45c6427b26b9b3c7189a17606

--- content/common/child_process.cc
+++ content/common/child_process.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,7 +28,9 @@ ChildProcess::ChildProcess()
   DCHECK(!child_process_);
   child_process_ = this;
 
-  io_thread_.StartWithOptions(base::Thread::Options(MessageLoop::TYPE_IO, 0));
+  // We can't recover from failing to start the IO thread.
+  CHECK(io_thread_.StartWithOptions(
+            base::Thread::Options(MessageLoop::TYPE_IO, 0)));
 }
 
 ChildProcess::~ChildProcess() {
