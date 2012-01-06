d0a3f4a733ad447bf599ab08d88b9738634dcf07
kgr@chromium.org <kgr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 23:24:20 +0000

    Increased width of Certificate Viewer WebUI to avoid need for scrollbar.
    BUG= 109433
    TEST= manually
    
    
    Review URL: http://codereview.chromium.org/9124015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116770 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e470bb14bc164db220f49b2aa4bd4547536dfafe

--- chrome/browser/ui/webui/certificate_viewer_webui.cc
+++ chrome/browser/ui/webui/certificate_viewer_webui.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -35,7 +35,7 @@ using content::WebUIMessageHandler;
 namespace {
 
 // Default width/height of the dialog.
-const int kDefaultWidth = 530;
+const int kDefaultWidth = 580;
 const int kDefaultHeight = 600;
 
 }  // namespace
