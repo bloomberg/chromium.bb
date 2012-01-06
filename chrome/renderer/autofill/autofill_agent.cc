1e8dc54a75f43f888cf4e1439983acf95a5d5060
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-06 00:07:36 +0000

    Remove Hardcoded Autofill Popup Location.
    Remove the old hardcoded values and replace it with a call to the webkit code to get the correct value.
    
    BUG=51644
    
    
    Review URL: http://codereview.chromium.org/9109027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116591 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 66f9052c53fe2831641bcf89012e60f166aeb630

--- chrome/renderer/autofill/autofill_agent.cc
+++ chrome/renderer/autofill/autofill_agent.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -418,10 +418,7 @@ void AutofillAgent::QueryAutofillSuggestions(const WebInputElement& element,
     WebFormControlElementToFormField(element, EXTRACT_VALUE, &field);
   }
 
-  // TODO(csharp): Stop using the hardcoded value once the WebKit change to
-  // expose the position lands.
-  // gfx::Rect bounding_box(autofill_query_element_.boundsInRootViewSpace());
-  gfx::Rect bounding_box(26, 51, 155, 22);
+  gfx::Rect bounding_box(autofill_query_element_.boundsInViewportSpace());
 
   Send(new AutofillHostMsg_QueryFormFieldAutofill(routing_id(),
                                                   autofill_query_id_,
