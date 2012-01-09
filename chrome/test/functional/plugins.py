2534ac6f09b72d2a864b0625233f9b50b86f464e
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-09 22:10:24 +0000

    Revert 116892 - Add pyauto tests for about plugins page, including PDF viewer enable disable and details info. Patch from vivianz@chromium.org. BUG=95586 TEST=none
    Review URL: http://codereview.chromium.org/8686013
    Patch from Vivian Zhi <vivianz@chromium.org>.
    
    TBR=kkania@chromium.org
    Review URL: http://codereview.chromium.org/9146012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116922 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1029de9f77581e660d5bc114c73c77e6461e91b7

--- chrome/test/functional/plugins.py
+++ chrome/test/functional/plugins.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -232,118 +232,6 @@ class PluginsTest(pyauto.PyUITest):
     self.GetBrowserWindow(0).GetTab(0).Reload()
     self.assertFalse(self._GetPluginPID('Shockwave Flash'),
                      msg='Shockwave Flash Plug-in not blocked.')
-
-  def testAboutPluginDetailInfo(self):
-    """Verify about:plugin page shows plugin details."""
-    self.NavigateToURL('chrome://plugins/')
-    driver = self.NewWebDriver()
-    detail_link = driver.find_element_by_id('details-link')
-    detail_link.click()
-    # Verify the detail info for Remote Viewer plugin show up.
-    self.assertTrue(self.WaitUntil(lambda: len(driver.find_elements_by_xpath(
-        '//*[@jscontent="name"][text()="Remoting Viewer"]' +
-        '//ancestor::*[@class="plugin-text"]//a[text()="Disable"]')) == 1))
-
-  def _ClearLocalDownloadState(self, path):
-    """Prepare for downloading the given path.
-
-    Clears the given path and the corresponding .crdownload, to prepare it to
-    be downloaded.
-    """
-    if os.path.exists(path): os.remove(path)
-    crdownload = path + '.crdownload'
-    if os.path.exists(crdownload): os.remove(crdownload)
-
-  def _GoPDFSiteAndVerify(self, pdf_name, tab_index=0, windex=0,
-                          is_pdf_enabled=True):
-    """Navigate to PDF file site and verify PDF viewer plugin.
-
-    When PDF Viewer plugin is enabled, navigate the site should not trigger
-    PDF file downloade and if PDF viewer plugin is disabled, PDF file should
-    be viewable in browser.
-
-    Args:
-      pdf_name: the name of the PDF file.
-                This should exist in the 'dangerous' directory.
-      tab_index: tab index. Default 0.
-      windex: window index. Default 0.
-      is_pdf_enabled: the boolean flag check if PDF viewer plugin is enabled.
-                      Default True.
-    """
-    file_url = self.GetHttpURLForDataPath('downloads', 'dangerous',
-        'download-dangerous.html' + '?' + pdf_name)
-    num_downloads = len(self.GetDownloadsInfo().Downloads())
-    self.NavigateToURL(file_url, windex, tab_index)
-    # It might take a while for the download to kick in, hold on until then.
-    if is_pdf_enabled == False:
-      self.assertTrue(self.WaitUntil(lambda:
-          len(self.GetDownloadsInfo().Downloads()) == num_downloads + 1))
-    else:
-      driver = self.NewWebDriver()
-      self.assertTrue(self.WaitUntil(lambda: len(
-          driver.find_elements_by_name('plugin')) == 1))
-
-  def testAboutPluginEnableAndDisablePDFPlugin(self):
-    """Verify enable and disable plugins from about:plugins page."""
-    self.NavigateToURL('chrome://plugins/')
-    driver = self.NewWebDriver()
-    # Override the animation for expanding detail info to make sure element
-    # remain at the same location where web driver found it.
-    override_animation_style_js = """
-        style = document.createElement('style');
-        style.innerHTML = "* { -webkit-transition: all 0s ease-in !important}";
-        document.head.appendChild(style);
-        """
-    driver.execute_script(override_animation_style_js)
-
-    # Click Details link to expand details.
-    detail_link = driver.find_element_by_id('details-link')
-    detail_link.click()
-    pdf_disable_path = '//*[@class="plugin-name"][text()="Chrome PDF Viewer"' \
-        ']//ancestor::*[@class="plugin-text"]//a[text()="Disable"]'
-    # Confirm Chrome PDF Viewer plugin is found.
-    self.assertTrue(self.WaitUntil(lambda: len(driver.find_elements_by_xpath(
-        pdf_disable_path)) == 1),
-        msg='Failed to find Chrome PDF Viewer plugin')
-
-    # Disable PDF viewer plugin in about:plugins.
-    pdf_disable_link = driver.find_element_by_xpath(pdf_disable_path)
-
-    # Need to sleep off 100ms for the detail-info expansion webkit transition.
-    pdf_disable_link.click()
-    self.assertFalse(self.WaitUntil(lambda:
-        self._IsEnabled('Chrome PDF Viewer')))
-
-    # Navigate to a PDF file and verify the pdf file is downloaded.
-    self.OpenNewBrowserWindow(True)
-    self._GoPDFSiteAndVerify('fw4.pdf', tab_index=0, windex=1,
-                             is_pdf_enabled=False)
-    id = self.GetDownloadsInfo().Downloads()[0]['id']
-    self.PerformActionOnDownload(id,
-                                 'save_dangerous_download',
-                                 window_index=1)
-    self.WaitForAllDownloadsToComplete(windex=1)
-
-    pdf_download = self.GetDownloadsInfo(1).Downloads()
-    # Verify that download info exists in the correct profile.
-    self.assertEqual(len(pdf_download), 1)
-    download_pkg = os.path.join(self.GetDownloadDirectory().value(),
-                                'fw4.pdf')
-    self._ClearLocalDownloadState(download_pkg)
-
-    # Navigate to plugins settings again and re-enable PDF viewer plugin.
-    self.NavigateToURL('chrome://plugins/')
-    driver = self.NewWebDriver()
-    pdf_enable_path = '//*[@class="plugin-name"][text()="Chrome PDF Viewer"]' \
-        '//ancestor::*[@class="plugin-text"]//a[text()="Enable"]'
-    self.assertTrue(self.WaitUntil(lambda: len(driver.find_elements_by_xpath(
-         pdf_enable_path)) == 1))
-    pdf_enable_link = driver.find_element_by_xpath(pdf_enable_path)
-    pdf_enable_link.click()
-    self.CloseBrowserWindow(0)
-    self.OpenNewBrowserWindow(True)
-    self._GoPDFSiteAndVerify('fw4.pdf', tab_index=0, windex=0,
-                             is_pdf_enabled=True)
 
 
 if __name__ == '__main__':
