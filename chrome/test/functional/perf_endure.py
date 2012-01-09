c1aae449a9c5f4f16dd1c5168376f0eaa1ae85de
dennisjeffrey@chromium.org <dennisjeffrey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-09 19:50:59 +0000

    Adding 2 new long-running pyauto tests for Chrome Endure: Docs and Plus.
    The Docs test takes v8 heap snapshots while repeatedly clicking on the
    "Owned by me" and "Home" buttons in Google Docs.  The Plus test takes
    snapshots while repeatedly clicking on the "What's hot" and "Following"
    streams of a test Google account.
    
    BUG=chromium-os:24774
    TEST=None
    
    Review URL: http://codereview.chromium.org/9114025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116900 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dc08829586171f0d33750de889ff3ec88b63cf06

--- chrome/test/functional/perf_endure.py
+++ chrome/test/functional/perf_endure.py
@@ -15,8 +15,11 @@ import pyauto_functional  # Must be imported before pyauto.
 import pyauto
 
 
-class ChromeEndureGmailTest(perf.BasePerfTest):
-  """Long-running performance tests for Chrome."""
+class ChromeEndureBaseTest(perf.BasePerfTest):
+  """Implements common functionality for all Chrome Endure tests.
+
+  All Chrome Endure test classes should inherit from this class.
+  """
 
   def setUp(self):
     perf.BasePerfTest.setUp(self)
@@ -24,7 +27,10 @@ class ChromeEndureGmailTest(perf.BasePerfTest):
     # Set up an object that takes v8 heap snapshots of the first opened tab
     # (index 0).
     self._snapshotter = perf_snapshot.PerformanceSnapshotter()
-    self._snapshot_results = []
+    self._full_snapshot_results = []
+    self._snapshot_iteration = 0
+    self._heap_size_results = []
+    self._node_count_results = []
 
   def ExtraChromeFlags(self):
     """Ensures Chrome is launched with custom flags.
@@ -38,19 +44,82 @@ class ChromeEndureGmailTest(perf.BasePerfTest):
             ['--remote-debugging-port=9222'])
 
   def _TakeHeapSnapshot(self):
-    """Takes a v8 heap snapshot using |self._snapshotter| and stores the result.
+    """Takes a v8 heap snapshot and outputs/stores the results.
 
     This function will fail the current test if no snapshot can be taken.
-
-    Returns:
-      The number of seconds it took to take the heap snapshot.
     """
+    # Take the snapshot and store the associated information.
+    logging.info('Taking heap snapshot...')
     start_time = time.time()
     snapshot = self._snapshotter.HeapSnapshot()
     elapsed_time = time.time() - start_time
     self.assertTrue(snapshot, msg='Failed to take a v8 heap snapshot.')
-    self._snapshot_results.append(snapshot[0])
-    return elapsed_time
+    snapshot_info = snapshot[0]
+    self._full_snapshot_results.append(snapshot_info)
+    logging.info('Snapshot taken (%.2f sec).' % elapsed_time)
+
+    base_timestamp = self._full_snapshot_results[0]['timestamp']
+
+    logging.info('Snapshot time: %.2f sec' % (
+                 snapshot_info['timestamp'] - base_timestamp))
+    heap_size = snapshot_info['total_heap_size'] / (1024.0 * 1024.0)
+    logging.info('  Total heap size: %.2f MB' % heap_size)
+    self._heap_size_results.append((self._snapshot_iteration, heap_size))
+
+    node_count = snapshot_info['total_node_count']
+    logging.info('  Total node count: %d nodes' % node_count)
+    self._node_count_results.append((self._snapshot_iteration, node_count))
+    self._snapshot_iteration += 1
+
+    # Output the results seen so far, to be graphed.
+    self._OutputPerfGraphValue(
+        'HeapSize', self._heap_size_results, 'MB', graph_name='Gmail-Heap',
+        units_x='iteration')
+    self._OutputPerfGraphValue(
+        'TotalNodeCount', self._node_count_results, 'nodes',
+        graph_name='Gmail-Nodes', units_x='iteration')
+
+  def _OutputFinalHeapSnapshotResults(self):
+    """Outputs final snapshot results to be graphed at the end of a test.
+
+    Assumes that at least one snapshot was previously taken by the current test.
+    """
+    assert len(self._full_snapshot_results) >= 1
+    max_heap_size = 0
+    max_node_count = 0
+    for index, snapshot_info in enumerate(self._full_snapshot_results):
+      heap_size = snapshot_info['total_heap_size'] / (1024.0 * 1024.0)
+      if heap_size > max_heap_size:
+        max_heap_size = heap_size
+      node_count = snapshot_info['total_node_count']
+      if node_count > max_node_count:
+        max_node_count = node_count
+    self._OutputPerfGraphValue(
+        'HeapSize', max_heap_size, 'MB', graph_name='Gmail-Heap-Max')
+    self._OutputPerfGraphValue(
+        'TotalNodeCount', max_node_count, 'nodes', graph_name='Gmail-Nodes-Max')
+
+  def _GetElement(self, find_by, value):
+    """Gets a WebDriver element object from the webpage DOM.
+
+    Args:
+      find_by: A callable that queries WebDriver for an element from the DOM.
+      value: A string value that can be passed to the |find_by| callable.
+
+    Returns:
+      The identified WebDriver element object, if found in the DOM, or
+      None, otherwise.
+    """
+    # The following cannot yet be imported on ChromeOS.
+    import selenium.common.exceptions
+    try:
+      return find_by(value)
+    except selenium.common.exceptions.NoSuchElementException:
+      return None
+
+
+class ChromeEndureGmailTest(ChromeEndureBaseTest):
+  """Long-running performance tests for Chrome using Gmail."""
 
   def testGmailComposeDiscard(self):
     """Interact with Gmail while periodically taking v8 heap snapshots.
@@ -90,107 +159,181 @@ class ChromeEndureGmailTest(perf.BasePerfTest):
       except selenium.common.exceptions.NoSuchFrameException:
         return False
 
-    def _GetElement(find_by, value):
-      """Gets a WebDriver element object from the webpage DOM.
-
-      Args:
-        find_by: A callable that queries WebDriver for an element from the DOM.
-        value: A string value that can be passed to the |find_by| callable.
-
-      Returns:
-        The identified WebDriver element object, if found in the DOM, or
-        None, otherwise.
-      """
-      try:
-        return find_by(value)
-      except selenium.common.exceptions.NoSuchElementException:
-        return None
-
     # Wait until Gmail's 'canvas_frame' loads and the 'Inbox' link is present.
     # TODO(dennisjeffrey): Check with the Gmail team to see if there's a better
     # way to tell when the webpage is ready for user interaction.
     wait.until(_SwitchToCanvasFrame)  # Raises exception if the timeout is hit.
     # Wait for the inbox to appear.
-    wait.until(lambda _: _GetElement(
+    wait.until(lambda _: self._GetElement(
                    driver.find_element_by_partial_link_text, 'Inbox'))
 
-    # Interact with Gmail for awhile.  Here, we repeat the following sequence of
-    # interactions: click the "Compose" button, enter some text into the "To"
+    # Interact with Gmail for a while.  Here, we repeat the following sequence
+    # of interactions: click the "Compose" button, enter some text into the "To"
     # field, enter some text into the "Subject" field, then click the "Discard"
     # button to discard the message.
-    heap_size_results = []
-    node_count_results = []
-    snapshot_iteration = 0
     num_iterations = 501
     for i in xrange(num_iterations):
       logging.info('Chrome interaction iteration %d of %d.' % (
                    i + 1, num_iterations))
 
-      compose_button = wait.until(lambda _: _GetElement(
+      compose_button = wait.until(lambda _: self._GetElement(
                                       driver.find_element_by_xpath,
                                       '//div[text()="COMPOSE"]'))
       compose_button.click()
 
-      to_field = wait.until(lambda _: _GetElement(
+      to_field = wait.until(lambda _: self._GetElement(
                                 driver.find_element_by_name, 'to'))
       to_field.send_keys('nobody@nowhere.com')
 
-      subject_field = wait.until(lambda _: _GetElement(
+      subject_field = wait.until(lambda _: self._GetElement(
                                      driver.find_element_by_name, 'subject'))
       subject_field.send_keys('This message is about to be discarded')
 
-      discard_button = wait.until(lambda _: _GetElement(
+      discard_button = wait.until(lambda _: self._GetElement(
                                       driver.find_element_by_xpath,
                                       '//div[text()="Discard"]'))
       discard_button.click()
 
       # Wait for the message to be discarded, assumed to be true after the
       # "To" field is removed from the webpage DOM.
-      wait.until(lambda _: not _GetElement(
+      wait.until(lambda _: not self._GetElement(
                      driver.find_element_by_name, 'to'))
 
       # Snapshot after the first iteration, then every 50 iterations after that.
       if i % 50 == 0:
-        logging.info('Taking heap snapshot...')
-        sec_to_snapshot = self._TakeHeapSnapshot()
-        logging.info('Snapshot taken (%.2f sec).' % sec_to_snapshot)
+        self._TakeHeapSnapshot()
 
-        assert len(self._snapshot_results) >= 1
-        base_timestamp = self._snapshot_results[0]['timestamp']
+    self._OutputFinalHeapSnapshotResults()
 
-        snapshot_info = self._snapshot_results[-1]  # Get the last snapshot.
-        logging.info('Snapshot time: %.2f sec' % (
-                     snapshot_info['timestamp'] - base_timestamp))
-        heap_size = snapshot_info['total_heap_size'] / (1024.0 * 1024.0)
-        logging.info('  Total heap size: %.2f MB' % heap_size)
-        heap_size_results.append((snapshot_iteration, heap_size))
 
-        node_count = snapshot_info['total_node_count']
-        logging.info('  Total node count: %d nodes' % node_count)
-        node_count_results.append((snapshot_iteration, node_count))
-        snapshot_iteration += 1
-        self._OutputPerfGraphValue(
-            'HeapSize', heap_size_results, 'MB', graph_name='Gmail-Heap',
-            units_x='iteration')
-        self._OutputPerfGraphValue(
-            'TotalNodeCount', node_count_results, 'nodes',
-            graph_name='Gmail-Nodes', units_x='iteration')
+class ChromeEndureDocsTest(ChromeEndureBaseTest):
+  """Long-running performance tests for Chrome using Google Docs."""
 
-    # Output other snapshot results.
-    assert len(self._snapshot_results) >= 1
-    max_heap_size = 0
-    max_node_count = 0
-    for index, snapshot_info in enumerate(self._snapshot_results):
-      heap_size = snapshot_info['total_heap_size'] / (1024.0 * 1024.0)
-      if heap_size > max_heap_size:
-        max_heap_size = heap_size
-      node_count = snapshot_info['total_node_count']
-      if node_count > max_node_count:
-        max_node_count = node_count
-    self._OutputPerfGraphValue(
-        'HeapSize', max_heap_size, 'MB', graph_name='Gmail-Heap-Max')
-    self._OutputPerfGraphValue(
-        'TotalNodeCount', max_node_count, 'nodes', graph_name='Gmail-Nodes-Max')
+  def testDocsAlternatelyClickLists(self):
+    """Interact with Google Docs while periodically taking v8 heap snapshots.
+
+    This test alternately clicks the "Owned by me" and "Home" buttons using
+    Google Docs, and periodically takes v8 heap snapshots that may reveal memory
+    bloat.
+    """
+    # The following cannot yet be imported on ChromeOS.
+    import selenium.common.exceptions
+    from selenium.webdriver.support.ui import WebDriverWait
+
+    # Log into a test Google account and open up Google Docs.
+    self._LoginToGoogleAccount()
+    self.NavigateToURL('http://docs.google.com')
+    loaded_tab_title = self.GetActiveTabTitle()
+    self.assertTrue('Docs' in loaded_tab_title,
+                    msg='Loaded tab title does not contain "Docs": "%s"' %
+                        loaded_tab_title)
+
+    driver = self.NewWebDriver()
+    # Any call to wait.until() will raise an exception if the timeout is hit.
+    wait = WebDriverWait(driver, timeout=60)
+
+    # Interact with Google Docs for a while.  Here, we repeat the following
+    # sequence of interactions: click the "Owned by me" button, then click the
+    # "Home" button.
+    num_iterations = 2001
+    for i in xrange(num_iterations):
+      if i % 5 == 0:
+        logging.info('Chrome interaction iteration %d of %d.' % (
+                     i + 1, num_iterations))
+
+      # Click the "Owned by me" button and wait for a resulting div to appear.
+      owned_by_me_button = wait.until(lambda _: self._GetElement(
+                                          driver.find_element_by_xpath,
+                                          '//div[text()="Owned by me"]'))
+      owned_by_me_button.click()
+      wait.until(
+          lambda _: self._GetElement(
+              driver.find_element_by_xpath,
+              '//div[@title="Owned by me filter.  Use backspace or delete to '
+              'remove"]'))
+      time.sleep(0.1)
+
+      # Click the "Home" button and wait for a resulting div to appear.
+      home_button = wait.until(lambda _: self._GetElement(
+                                   driver.find_element_by_xpath,
+                                   '//div[text()="Home"]'))
+      home_button.click()
+      wait.until(
+          lambda _: self._GetElement(
+              driver.find_element_by_xpath,
+              '//div[@title="Home filter.  Use backspace or delete to '
+              'remove"]'))
+      time.sleep(0.1)
+
+      # Snapshot after the first iteration, then every 100 iterations after
+      # that.
+      if i % 100 == 0:
+        self._TakeHeapSnapshot()
+
+    self._OutputFinalHeapSnapshotResults()
+
+
+class ChromeEndurePlusTest(ChromeEndureBaseTest):
+  """Long-running performance tests for Chrome using Google Plus."""
+
+  def testPlusAlternatelyClickStreams(self):
+    """Interact with Google Plus while periodically taking v8 heap snapshots.
+
+    This test alternately clicks the "Friends" and "Acquaintances" buttons using
+    Google Plus, and periodically takes v8 heap snapshots that may reveal memory
+    bloat.
+    """
+    # The following cannot yet be imported on ChromeOS.
+    import selenium.common.exceptions
+    from selenium.webdriver.support.ui import WebDriverWait
+
+    # Log into a test Google account and open up Google Plus.
+    self._LoginToGoogleAccount()
+    self.NavigateToURL('http://plus.google.com')
+    loaded_tab_title = self.GetActiveTabTitle()
+    self.assertTrue('Google+' in loaded_tab_title,
+                    msg='Loaded tab title does not contain "Google+": "%s"' %
+                        loaded_tab_title)
+
+    driver = self.NewWebDriver()
+    # Any call to wait.until() will raise an exception if the timeout is hit.
+    wait = WebDriverWait(driver, timeout=60)
+
+    # Interact with Google Plus for a while.  Here, we repeat the following
+    # sequence of interactions: click the "Friends" button, then click the
+    # "Acquaintances" button.
+    num_iterations = 1001
+    for i in xrange(num_iterations):
+      if i % 5 == 0:
+        logging.info('Chrome interaction iteration %d of %d.' % (
+                     i + 1, num_iterations))
+
+      # Click the "Friends" button and wait for a resulting div to appear.
+      friends_button = wait.until(lambda _: self._GetElement(
+                                      driver.find_element_by_xpath,
+                                      '//a[text()="Friends"]'))
+      friends_button.click()
+      wait.until(lambda _: self._GetElement(
+                     driver.find_element_by_xpath,
+                     '//div[text()="Friends"]'))
+      time.sleep(0.1)
+
+      # Click the "Acquaintances" button and wait for a resulting div to appear.
+      acquaintances_button = wait.until(lambda _: self._GetElement(
+                                            driver.find_element_by_xpath,
+                                            '//a[text()="Acquaintances"]'))
+      acquaintances_button.click()
+      wait.until(lambda _: self._GetElement(
+                     driver.find_element_by_xpath,
+                     '//div[text()="Acquaintances"]'))
+      time.sleep(0.1)
+
+      # Snapshot after the first iteration, then every 100 iterations after
+      # that.
+      if i % 100 == 0:
+        self._TakeHeapSnapshot()
+
+    self._OutputFinalHeapSnapshotResults()
 
 
 if __name__ == '__main__':
