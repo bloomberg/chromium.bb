bloomberg/chromium.bb

commit 911a4dae5160b68bca8c389947612d17fe876a16

[GTK] Rounding errors on 32-bit machines causes tests to fail https://bugs.webkit.org/show_bug.cgi?id=72254
Initial patch by vanuan@gmail.com.
Reviewed by Martin Robinson.

* Scripts/webkitdirs.pm:
(runAutogenForAutotoolsProjectIfNecessary): Pass additional
CXXFLAGS to autogen in an attempt to harmonize floating point
values between 32-bit and 64-bit architectures.

git-svn-id: svn://svn.chromium.org/blink/trunk@103040 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: d45b70d27b7e5731c11a2671f65a6a69f07f0e06

third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -1621,6 +1621,14 @@ sub runAutogenForAutotoolsProjectIfNecessary($@)
     # Long argument lists cause bizarre slowdowns in libtool.
     my $relSourceDir = File::Spec->abs2rel($sourceDir) || ".";
 
+    # Compiler options to keep floating point values consistent
+    # between 32-bit and 64-bit architectures. The options are also
+    # used on Chromium build.
+    determineArchitecture();
+    if ($architecture ne "x86_64") {
+        $ENV{'CXXFLAGS'} = "-march=pentium4 -msse2 -mfpmath=sse";
+    }
+
     # Prefix the command with jhbuild run.
     unshift(@buildArgs, "$relSourceDir/autogen.sh");
     unshift(@buildArgs, "$sourceDir/Tools/gtk/run-with-jhbuild");

commit 13eb1ba035b5c9a3bbd7650388077cd22301861d

[Gtk][ARM] Cross compiler buildbot is failing after r103040 https://bugs.webkit.org/show_bug.cgi?id=75846
Reviewed by Philippe Normand.

* Scripts/webkitdirs.pm:
(runAutogenForAutotoolsProjectIfNecessary):


git-svn-id: svn://svn.chromium.org/blink/trunk@104461 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 2f762f39bf3d82b859b4724677f68d18fc9b1310

third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -1770,7 +1770,7 @@ sub runAutogenForAutotoolsProjectIfNecessary($@)
     # between 32-bit and 64-bit architectures. The options are also
     # used on Chromium build.
     determineArchitecture();
-    if ($architecture ne "x86_64") {
+    if ($architecture ne "x86_64" && !isARM()) {
         $ENV{'CXXFLAGS'} = "-march=pentium4 -msse2 -mfpmath=sse";
     }
 