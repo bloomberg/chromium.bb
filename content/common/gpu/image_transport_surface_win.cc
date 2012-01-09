f8cc3e35677899df51ebd7fad3b1678c47d814a2
arthurhsu@chromium.org <arthurhsu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-09 20:09:05 +0000

    Coverity fix: Unused value
    BUG=none
    CID=102785
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9007035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@116905 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 03f70d7d9045311c88a40c8e4ea50da6eb906b68

--- content/common/gpu/image_transport_surface_win.cc
+++ content/common/gpu/image_transport_surface_win.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -191,7 +191,6 @@ scoped_refptr<gfx::GLSurface> ImageTransportSurface::CreateSurface(
     int32 command_buffer_id,
     gfx::PluginWindowHandle handle) {
   scoped_refptr<gfx::GLSurface> surface;
-  base::win::OSInfo* os_info = base::win::OSInfo::GetInstance();
 
   if (gfx::GetGLImplementation() == gfx::kGLImplementationEGLGLES2 &&
       !CommandLine::ForCurrentProcess()->HasSwitch(
