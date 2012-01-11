1e69173d1a2b74b4d46bd5822daa343c01591225
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 17:26:34 +0000

    Update tools/git/update-copyrights.sh with the current year.
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9148027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117237 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: af5c6ad6b08f3130fba12b931f3d6475471580b2

--- tools/git/update-copyrights.sh
+++ tools/git/update-copyrights.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -9,7 +9,7 @@ git diff --name-only $(git cl upstream)... | while read file; do
     cp "$file" "$tmp"
     # Rather than editing the temporary file, edit the original file in-place
     # so that we preserve file modes.
-    sed -i -e '1,4s/Copyright .c. .* The Chromium Authors/Copyright (c) 2011 The Chromium Authors/' "$file"
+    sed -i -e '1,4s/Copyright .c. .* The Chromium Authors/Copyright (c) 2012 The Chromium Authors/' "$file"
     if ! diff -q "$file" "$tmp" > /dev/null; then
         echo "updated $file"
     fi
