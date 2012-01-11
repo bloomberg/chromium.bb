f8c6ed04b189d8471c6644cd06f4d41867b7fb38
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 11:34:04 +0000

    Revert 117201 - Adding support for MediaStream and PeerConnection functionality.
    Reason for revert: libyuv breakage on the chromium os arm bots.
    
    BUG=
    TEST=content_unittests
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=117187
    
    Review URL: http://codereview.chromium.org/8060055
    
    TBR=grunell@chromium.org
    Review URL: http://codereview.chromium.org/9186004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117202 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bcd1df77729e966a225de98957dbaf0c687a89d0

--- content/renderer/p2p/socket_dispatcher.h
+++ content/renderer/p2p/socket_dispatcher.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -26,7 +26,6 @@
 #include "base/id_map.h"
 #include "base/observer_list_threadsafe.h"
 #include "base/synchronization/lock.h"
-#include "content/common/content_export.h"
 #include "content/common/p2p_sockets.h"
 #include "content/public/renderer/render_view_observer.h"
 #include "net/base/net_util.h"
@@ -49,7 +48,7 @@ class P2PSocketClient;
 // P2PSocketDispatcher works on the renderer thread. It dispatches all
 // messages on that thread, and all its methods must be called on the
 // same thread.
-class CONTENT_EXPORT P2PSocketDispatcher : public content::RenderViewObserver {
+class P2PSocketDispatcher : public content::RenderViewObserver {
  public:
   class NetworkListObserver {
    public:
