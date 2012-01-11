463d510b2e1dc59c877460bc7c1f4a5a903de449
jasonwkim@google.com <jasonwkim@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-01-11 16:04:56 +0000

    Make driver support arbitrary digit arguments to -O There are some folks who insist on passing -O20 to the compile flag.
    Clamp strange -O args into -O3
    (clang dies with a cryptic '4' when fed -O20)
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2507
    BUG=http://code.google.com/p/naclports/issues/detail?id=27
    TEST=manual
    
    Review URL: http://codereview.chromium.org/9166029
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@7558 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: f2fe689b39ef4441d0f32845916c109eb1e803c1

--- pnacl/driver/pnacl-driver.py
+++ pnacl/driver/pnacl-driver.py
@@ -216,7 +216,9 @@ GCCPatterns = [
   ( '(-Bstatic)',         "env.append('INPUTS', $0)"),
   ( '(-Bdynamic)',        "env.append('INPUTS', $0)"),
 
-  ( '-O(s|[0-9]+)',       "env.set('OPT_LEVEL', $0)\n"),
+  ( '-O(s)',              "env.set('OPT_LEVEL', $0)\n"),
+  ( '-O([0-3])',          "env.set('OPT_LEVEL', $0)\n"),
+  ( '-O([0-9]+)',         "env.set('OPT_LEVEL', '3')\n"),
   ( '-O',                 "env.set('OPT_LEVEL', '1')\n"),
 
   ( ('-isystem', '(.*)'),
