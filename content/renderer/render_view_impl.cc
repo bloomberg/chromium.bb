ee2bbe06c77cb947530446f9bd6abeb423e26343
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-11 07:44:23 +0000

    Coverity: Add NULL checks in RenderViewImpl::createApplicationCacheHost.
    CID=101766
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9014001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117186 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cdeeeb29d7a3a2a0988061bf63aad184060b4c14

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -1991,6 +1991,8 @@ WebMediaPlayer* RenderViewImpl::createMediaPlayer(
 
 WebApplicationCacheHost* RenderViewImpl::createApplicationCacheHost(
     WebFrame* frame, WebApplicationCacheHostClient* client) {
+  if (!frame || !frame->view())
+    return NULL;
   return new RendererWebApplicationCacheHostImpl(
       FromWebView(frame->view()), client,
       RenderThreadImpl::current()->appcache_dispatcher()->backend_proxy());
