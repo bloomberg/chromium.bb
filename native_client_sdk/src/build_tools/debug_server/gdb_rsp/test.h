75c71e443c5131e375229d94fe9a7c948d5f787f
bradnelson@google.com <bradnelson@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-12 18:58:52 +0000

    Switching license notice from nacl to chromium as the naclsdk has been merged into chromium.
    BUG=None
    TEST=None
    R=noelallen@chromium.org,steveblock@chromium.org
    Review URL: http://codereview.chromium.org/9051006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117462 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: aeaed3b6b3528d636ce4b22f92f072bd91542727

--- native_client_sdk/src/build_tools/debug_server/gdb_rsp/test.h
+++ native_client_sdk/src/build_tools/debug_server/gdb_rsp/test.h
@@ -1,7 +1,6 @@
-/*
- * Copyright 2010 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+/* Copyright (c) 2012 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 #ifndef NATIVE_CLIENT_GDB_RSP_TEST_H_
 #define NATIVE_CLIENT_GDB_RSP_TEST_H_ 1
