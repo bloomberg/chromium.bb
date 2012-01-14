ac2f6e8b1a1bbf521e997ad1f6cce37fc28075b3
pdox@google.com <pdox@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-01-14 00:00:18 +0000

    Change buildbot names in buildbot selector.
    R= bradn, jvoung
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2452
    Review URL: http://codereview.chromium.org/9117005
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@7597 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 447b48c295ce382d2f3fc1c5447461d4c9c556b9

--- buildbot/buildbot_selector.py
+++ buildbot/buildbot_selector.py
@@ -219,13 +219,13 @@ BOT_ASSIGNMENT = {
     # Pnacl toolchain builders.
     'linux-armtools-x86_32':
         'bash buildbot/buildbot_toolchain_arm_trusted.sh',
-    'linux-pnacl-x86_32-newlib':
+    'linux-pnacl-x86_32':
         'bash buildbot/buildbot_toolchain_arm_untrusted.sh linux 32',
-    'linux-pnacl-x86_64-newlib':
+    'linux-pnacl-x86_64':
         'bash buildbot/buildbot_toolchain_arm_untrusted.sh linux 64',
-    'mac-pnacl-x86_32-newlib':
+    'mac-pnacl-x86_32':
         'bash buildbot/buildbot_toolchain_arm_untrusted.sh mac 32',
-    'linux-pnacl-x86_64-newlib-tests':
+    'linux-pnacl-x86_64-tests':
         'bash buildbot/buildbot_pnacl_toolchain_tests.sh',
     # TODO(pdox): Rename this bot to x86_64
     'win-pnacl-x86_32-newlib':
