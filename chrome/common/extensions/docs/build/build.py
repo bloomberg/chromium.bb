6604cd370a940a5b3056a513a7fbdb34af706816
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 20:00:00 +0000

    Documentation builder now looks for DumpRenderTree under `out` on Mac.
    The `make` build puts `DumpRenderTree` under `out/{Release,Debug}/...`. We
    should search for it there.
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9242012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117940 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3c330fb5b5e658519dfeb76c17deda05da6c020b

--- chrome/common/extensions/docs/build/build.py
+++ chrome/common/extensions/docs/build/build.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -142,6 +142,10 @@ def FindDumpRenderTree():
         "/xcodebuild/Release/DumpRenderTree.app/Contents/MacOS/DumpRenderTree")
     search_locations.append(src_dir +
         "/xcodebuild/Debug/DumpRenderTree.app/Contents/MacOS/DumpRenderTree")
+    search_locations.append(src_dir +
+        "/out/Release/DumpRenderTree.app/Contents/MacOS/DumpRenderTree")
+    search_locations.append(src_dir +
+        "/out/Debug/DumpRenderTree.app/Contents/MacOS/DumpRenderTree")
     search_locations.append(os.getenv("HOME") + "/bin/DumpRenderTree/" +
                             "DumpRenderTree.app/Contents/MacOS/DumpRenderTree")
 
