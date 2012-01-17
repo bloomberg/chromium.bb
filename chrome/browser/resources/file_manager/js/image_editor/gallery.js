527675ab10e5fb374abf631fc627cacfb127f32a
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 13:29:42 +0000

    Changed the edited file name prefix in the Photo Editor.
    "Copy of" -> "Edited"
    
    BUG=chromium-os:24998
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9245001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117892 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 361cbc42e1fc587025c40ebf333cd6ba080d52da

--- chrome/browser/resources/file_manager/js/image_editor/gallery.js
+++ chrome/browser/resources/file_manager/js/image_editor/gallery.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -913,7 +913,7 @@ Ribbon.Item.prototype.save = function(
 };
 
 // TODO: Localize?
-Ribbon.Item.COPY_SIGNATURE = 'Copy of ';
+Ribbon.Item.COPY_SIGNATURE = 'Edited - ';
 
 Ribbon.Item.REGEXP_COPY_N =
     new RegExp('^' + Ribbon.Item.COPY_SIGNATURE + '(.+) \\((\\d+)\\)$');
