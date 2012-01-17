223c3dd2437654e10150bc19b3c9c2fc96a82ca2
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 17:50:17 +0000

    Fix disabling of controlled settings input elements on the internet detail dialog.
    BUG=chromium-os:24615
    TEST=Open "Options" for a network connection, check that all the controls are enabled as expected.
    
    
    Review URL: http://codereview.chromium.org/9221012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117913 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 63b26dc290e3d547bccac36d40fb6ea570a26c2c

--- chrome/browser/resources/options/chromeos/internet_detail.js
+++ chrome/browser/resources/options/chromeos/internet_detail.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -75,7 +75,7 @@ cr.define('options.internet', function() {
       indicator.controlledBy = controlledBy;
 
       if (forElement) {
-        forElement.disabled = !recommended;
+        forElement.disabled = (controlledBy != null) && !recommended;
 
         // Special handling for radio buttons:
         //  - If the setting is recommended, show the recommended indicator
