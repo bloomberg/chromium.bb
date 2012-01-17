da8bb36f51493ea38491a0806ebba8820e8b761c
scottbyer@chromium.org <scottbyer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-17 20:04:10 +0000

    fix up locate script for Lion
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9212022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@117943 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a4c0eaadcfc2f86eeb1ea46ff72e4b9f10a688b0

--- tools/valgrind/locate_valgrind.sh
+++ tools/valgrind/locate_valgrind.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -32,6 +32,9 @@ then
   *Darwin*10.[0-9].[0-9]*x86_64*)
     PLATFORM="mac_10.6"
     ;;
+  *Darwin*11.[0-9].[0-9]*x86_64*)
+    PLATFORM="mac_10.7"
+    ;;
   *)
     echo "Unknown platform:" >&2
     uname -a >&2
@@ -46,7 +49,9 @@ then
     CHROME_VALGRIND="$THISDIR/../../third_party/valgrind/$PLATFORM"
 
     # TODO(timurrrr): readlink -f is not present on Mac...
-    if [ "$PLATFORM" != "mac" ] && [ "$PLATFORM" != "mac_10.6" ]
+    if [ "$PLATFORM" != "mac" ] && \
+      [ "$PLATFORM" != "mac_10.6" ] && \
+      [ "$PLATFORM" != "mac_10.7" ]
     then
       # Get rid of all "../" dirs
       CHROME_VALGRIND=`readlink -f $CHROME_VALGRIND`
