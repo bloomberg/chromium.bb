596b9cd2167464ee34b67d35c754e320a6e0341d
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 02:19:34 +0000

    Revert 117964 - UILayoutTest: Use DOMAutomation to retrieve document.body.innerText
    Also enable some tests that can run now.
    
    We currently store innerText in a cookie and then read the cookie.  So we can
    only run tests whose output is < 4096 bytes.  This change removes that
    limitation.
    
    BUG=33472
    TEST=
    
    
    Trying to diagnose this test failure on windows. Will reland if this patch is not the cause.
    
    http://chromegw.corp.google.com/i/chromium/builders/XP%20Tests%20%28dbg%29%282%29/builds/13488
    
    WorkerFileSystemTest.FileWriterSyncWriteOverlapped: 
    [2960:2576:0117/155221:2999875:INFO:ui_layout_test.cc(57)] Assume LayoutTests in e:\b\build\slave\XP_Tests__dbg__2_\build\src\chrome\test\data\layout_tests\LayoutTests
    [2960:2576:0117/155221:2999890:INFO:ui_layout_test.cc(143)] Assume LayoutTests in fast\filesystem
    [2960:2576:0117/155221:3000093:INFO:ui_layout_test.cc(143)] Assume LayoutTests in fast\js
    .\test\ui\ui_test.cc(720): error: Failed
    Timeout reached in WaitUntilCookieNonEmpty
    Google Test trace:
    .\test\ui\ui_layout_test.cc(174): file-writer-sync-write-overlapped.html
    .\test\ui\ui_layout_test.cc(254): error: Value of: inner_text.c_str()
      Actual: "[Worker] Test using FileWriterSync.seek to write overlapping existing data in a file.
    
    On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
    
    Starting worker: ../resources/file-writer-sync-write-overlapped.js
    
    "
    Expected: expected_result_value.c_str()
    Which is: "[Worker] Test using FileWriterSync.seek to write overlapping existing data in a file.
    
    On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
    
    Starting worker: ../resources/file-writer-sync-write-overlapped.js
    PASS [Worker] Overlapped write 1 verified.
    PASS [Worker] Overlapped write 2 verified.
    PASS [Worker] Overlapped write 3 verified.
    PASS successfullyParsed is true
    
    TEST COMPLETE
    
    "
    Google Test trace:
    .\test\ui\ui_layout_test.cc(174): file-writer-sync-write-overlapped.html
    
    The theory is that the test is timing out waiting for a cookie and this patch modified how cookies and tests work.
    
    Review URL: http://codereview.chromium.org/9127021
    
    TBR=dgrogan@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118015 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1becd39dfff23f3eb469b639382f8ecd97c39cc0

--- chrome/renderer/resources/dom_automation.js
+++ chrome/renderer/resources/dom_automation.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2010 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -399,8 +399,6 @@ var domAutomation = domAutomation || {};
       return element.id;
     } else if (type == "contentdocument") {
       return element.contentDocument;
-    } else if (type == "innertext") {
-      return element.innerText;
     }
   }
 
