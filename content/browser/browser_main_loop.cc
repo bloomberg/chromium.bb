0bdc2d40012b4016dc3c85048996fae0b706baab
simon.hong81@gmail.com <simon.hong81@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 03:37:34 +0000

    In the BrowserMainLoop::InitializeToolkit(),    Change gfx::GtkInitFromCommandLine's parameter    from parameters_.command_line to parsed_command_line_.
    Contributed by simon.hong81@gmail.com
    
    BUG=none
    TEST=compiles
    
    Review URL: http://codereview.chromium.org/9241021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118030 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 38d0b2d49f614e30ee9d6c9ce3c1b1c66fab3dfa

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -539,7 +539,7 @@ void BrowserMainLoop::InitializeToolkit() {
 #endif
 
 #if !defined(USE_AURA)
-  gfx::GtkInitFromCommandLine(parameters_.command_line);
+  gfx::GtkInitFromCommandLine(parsed_command_line_);
 #endif
 
   SetUpGLibLogHandler();
