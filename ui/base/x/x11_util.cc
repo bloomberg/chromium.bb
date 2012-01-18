31020285e984578ef939bf8a2008c12431d43045
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-18 04:16:46 +0000

    ui::EF_ALT_DOWN is missing from XKeyEventState
    BUG=None
    TEST=try
    
    
    Review URL: http://codereview.chromium.org/9243004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118040 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0971733b42f2870a50106f6f4addb9215bf7b9d7

--- ui/base/x/x11_util.cc
+++ ui/base/x/x11_util.cc
@@ -123,6 +123,7 @@ unsigned int XKeyEventState(int flags) {
   return
       ((flags & ui::EF_SHIFT_DOWN) ? ShiftMask : 0) |
       ((flags & ui::EF_CONTROL_DOWN) ? ControlMask : 0) |
+      ((flags & ui::EF_ALT_DOWN) ? Mod1Mask : 0) |
       ((flags & ui::EF_CAPS_LOCK_DOWN) ? LockMask : 0);
 }
 
