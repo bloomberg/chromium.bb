e5a44664f1acf4fe0381fcadb17c3fd91e654dca
dominich@chromium.org <dominich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-19 00:35:24 +0000

    Add filtering to chrome://network-action-predictor
    BUG=110091
    TEST=Navigate to chrome://network-action-predictor. Notice that checking the 'filter' checkbox causes the table to be redrawn with only entries where Confidence is > 0
    
    
    Review URL: http://codereview.chromium.org/9186055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118196 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 151d7ef47c58555ddf9c2937be37d38101b61026

--- chrome/browser/resources/network_action_predictor/network_action_predictor.js
+++ chrome/browser/resources/network_action_predictor/network_action_predictor.js
@@ -10,40 +10,65 @@ function requestNetworkActionPredictorDb() {
   chrome.send('requestNetworkActionPredictorDb', [])
 }
 
+
 /**
- * Callback from backend with the database contents. Builds the UI.
+ * Callback from backend with the database contents. Sets up some globals and
+ * calls to create the UI.
  * @param {boolean} enabled Whether or not NetworkActionPredictor is enabled.
- * @param {array} database The database as a flattened list.
+ * @param {Array} database The database as a flattened list.
  */
 function updateDatabaseTable(enabled, database) {
   console.debug('Updating Table NAP DB');
-  $('countBanner').textContent = 'Entries: ' + database.length;
 
-  $('enabledMode').hidden = !enabled;
-  $('disabledMode').hidden = enabled;
+  var filter = $('filter');
+  filter.disabled = false;
+  filter.onchange = function() {
+    updateDatabaseView(enabled, database);
+  };
 
-  if (!enabled)
+  updateDatabaseView(enabled, database);
+}
+
+/**
+ * Updates the table from the database.
+ * @param {boolean} enabled Whether or not NetworkActionPredictor is enabled.
+ * @param {Array} database The database as a flattened list.
+ */
+function updateDatabaseView(enabled, database) {
+  var databaseSection = $('databaseTableBody');
+  var showEnabled = database && enabled;
+
+  $('enabledMode').hidden = !showEnabled;
+  $('disabledMode').hidden = showEnabled;
+
+  if (!showEnabled)
     return;
 
-  var databaseSection = $('databaseTableBody');
+  var filter = $('filter');
 
   // Clear any previous list.
   databaseSection.textContent = '';
 
   for (var i = 0; i < database.length; ++i) {
     var entry = database[i];
-    var row = document.createElement('tr');
 
-    row.appendChild(document.createElement('td')).textContent = entry.user_text;
-    row.appendChild(document.createElement('td')).textContent = entry.url;
-    row.appendChild(document.createElement('td')).textContent = entry.hit_count;
-    row.appendChild(document.createElement('td')).textContent =
-        entry.miss_count;
-    row.appendChild(document.createElement('td')).textContent =
-        entry.confidence;
+    if (!filter.checked || entry.confidence > 0) {
+      var row = document.createElement('tr');
 
-    databaseSection.appendChild(row);
+      row.appendChild(document.createElement('td')).textContent =
+          entry.user_text;
+      row.appendChild(document.createElement('td')).textContent = entry.url;
+      row.appendChild(document.createElement('td')).textContent =
+          entry.hit_count;
+      row.appendChild(document.createElement('td')).textContent =
+          entry.miss_count;
+      row.appendChild(document.createElement('td')).textContent =
+          entry.confidence;
+
+      databaseSection.appendChild(row);
+    }
   }
+  $('countBanner').textContent = 'Entries: ' + databaseSection.children.length;
 }
 
 document.addEventListener('DOMContentLoaded', requestNetworkActionPredictorDb);
