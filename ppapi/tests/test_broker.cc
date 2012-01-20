6036a8d8fd43eb1d202dc05da2464c91fd8ee3ca
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 23:00:39 +0000

    Enable Pepper broker test for windows
    The sync_socket creation failure has been fixed so the test should now pass. See bug for details
    
    BUG=103975
    TEST=this is a test dammit!
    
    Review URL: http://codereview.chromium.org/9254042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118537 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9c1a22dc3194f2c7c11c408b2b26c2c21adc03b3

--- ppapi/tests/test_broker.cc
+++ ppapi/tests/test_broker.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -212,9 +212,7 @@ void TestBroker::RunTests(const std::string& filter) {
   RUN_TEST(Create, filter);
   RUN_TEST(GetHandleFailure, filter);
   RUN_TEST(ConnectFailure, filter);
-#if !defined(OS_WIN)  // This is broken on Windows. See http://crbug.com/103975.
   RUN_TEST(ConnectAndPipe, filter);
-#endif
 }
 
 std::string TestBroker::TestCreate() {
