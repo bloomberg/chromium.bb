08afdbd816af947e52f5780ca991cf9dc8cb33dc
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-20 06:33:27 +0000

    Move operator<< for StringPiece into string_piece.{h,cc}
    BUG=110651
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9159005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118416 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 34a90773493e1a272ae21b27616fd80bb2d7300b

--- chrome/renderer/extensions/chrome_v8_extension.h
+++ chrome/renderer/extensions/chrome_v8_extension.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,7 @@
 
 #include "base/logging.h"
 #include "base/memory/linked_ptr.h"
+#include "base/string_piece.h"
 #include "chrome/renderer/extensions/chrome_v8_extension_handler.h"
 #include "v8/include/v8.h"
 
