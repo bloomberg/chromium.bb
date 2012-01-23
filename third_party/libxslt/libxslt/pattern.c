86775264c813a342b19d6858db44df93f4499c82
aarya@google.com <aarya@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-23 05:06:58 +0000

    Fix issue with pattern parsing in xsltCompilePatternInternal. Import pattern parsing fix for commit: http://git.gnome.org/browse/libxslt/commit/?id=fe5a4fa33eb85bce3253ed3742b1ea6c4b59b41b
    BUG=110277
    Review URL: https://chromiumcodereview.appspot.com/9275001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118654 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 094c773bb6c144f07b004ff3d1886100f157f4f6

--- third_party/libxslt/libxslt/pattern.c
+++ third_party/libxslt/libxslt/pattern.c
@@ -1864,6 +1864,8 @@ xsltCompilePatternInternal(const xmlChar *pattern, xmlDocPtr doc,
 		while ((pattern[end] != 0) && (pattern[end] != '"'))
 		    end++;
 	    }
+      if (pattern[end] == 0)
+          break;
 	    end++;
 	}
 	if (current == end) {
