c8358a2d7e44c2fb0ce4c4a6d26cb4bc16453e86
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-24 23:31:19 +0000

    [Coverity] Pass GURL by const-ref
    CID=100578
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/9117039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118937 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 00aee6381c929229af9c0b1cce55a08e5eac84ed

--- webkit/appcache/view_appcache_internals_job.cc
+++ webkit/appcache/view_appcache_internals_job.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -433,7 +433,7 @@ class ViewAppCacheJob : public BaseInternalsJob,
  public:
   ViewAppCacheJob(
       net::URLRequest* request, AppCacheService* service,
-      const GURL manifest_url)
+      const GURL& manifest_url)
       : BaseInternalsJob(request, service),
         manifest_url_(manifest_url) {}
 
