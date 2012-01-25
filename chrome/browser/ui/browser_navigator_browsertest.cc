fb0cbc9c168023321524424882e15bb9026c6c9e
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 23:09:45 +0000

    Fix bug 107792: 'learn more' link after an app in a panel crashes should open in a regular tab
    BUG=107792
    TEST=New browser test
    
    
    Review URL: http://codereview.chromium.org/9187069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119136 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 675e779b5fd78f0c818d0ac5dce073ab6ec8f40a

--- chrome/browser/ui/browser_navigator_browsertest.cc
+++ chrome/browser/ui/browser_navigator_browsertest.cc
@@ -1274,4 +1274,58 @@ IN_PROC_BROWSER_TEST_F(BrowserNavigatorTest,
             browser()->GetSelectedWebContents()->GetURL());
 }
 
+// This test makes sure any link in a crashed panel page navigates to a tabbed
+// window.
+class PanelBrowserNavigatorTest : public BrowserNavigatorTest {
+ protected:
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    command_line->AppendSwitch(switches::kEnablePanels);
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(PanelBrowserNavigatorTest, NavigateFromCrashedPanel) {
+  GURL url("http://maps.google.com/#a");
+  GURL url2("http://maps.google.com/#b");
+
+  // Create a panel.
+  Browser* panel_browser = Browser::CreateForApp(Browser::TYPE_PANEL,
+      "Test", gfx::Rect(100, 100), browser()->profile());
+
+  // Navigate to the page.
+  browser::NavigateParams p(MakeNavigateParams(panel_browser));
+  p.url = url;
+  p.disposition = CURRENT_TAB;
+  browser::Navigate(&p);
+
+  // Navigate() should have navigated in the existing panel window.
+  EXPECT_EQ(panel_browser, p.browser);
+
+  // We should now have two windows, the browser() provided by the framework and
+  // the panel window we opened earlier. The tabbed browser window has 1 tab.
+  EXPECT_EQ(2u, BrowserList::size());
+  EXPECT_EQ(1, browser()->tab_count());
+  EXPECT_EQ(1, panel_browser->tab_count());
+
+  // Kill the panel page.
+  WebContents* web_contents = panel_browser->GetSelectedWebContents();
+  web_contents->SetIsCrashed(base::TERMINATION_STATUS_PROCESS_CRASHED, -1);
+  EXPECT_TRUE(web_contents->IsCrashed());
+
+  // Navigate to the page.
+  browser::NavigateParams p2(MakeNavigateParams(panel_browser));
+  p2.source_contents = panel_browser->GetSelectedTabContentsWrapper();
+  p2.url = url2;
+  p2.disposition = CURRENT_TAB;
+  browser::Navigate(&p2);
+
+  // Navigate() should have opened a new tab in the existing tabbed window.
+  EXPECT_EQ(browser(), p2.browser);
+
+  // We should now have two windows, the browser() provided by the framework and
+  // the panel window we opened earlier. The tabbed browser window has 2 tabs.
+  EXPECT_EQ(2u, BrowserList::size());
+  EXPECT_EQ(2, browser()->tab_count());
+  EXPECT_EQ(1, panel_browser->tab_count());
+}
+
 } // namespace
