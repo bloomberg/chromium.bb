cf96a94bdb18ca8f303c117f8265a2d817039ba9
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 00:50:54 +0000

    Highlight changes in about:sync's about tab
    Highlight rows in about:sync About tab as their values change.  It uses
    -webkit-animation CSS properties to highlight and fade recently changed
    values.  Change detection is triggered on the 'jseval' hook provided by
    jstemplate.js.
    
    BUG=108100
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9019027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@118960 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 242f58b8f831a12f65a361c4ff1a737d19064471

--- chrome/browser/resources/sync_internals/about.js
+++ chrome/browser/resources/sync_internals/about.js
@@ -1,6 +1,22 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
+function highlightIfChanged(node, oldVal, newVal) {
+  function clearHighlight() {
+    this.removeAttribute('highlighted');
+  }
+
+  var oldStr = oldVal.toString();
+  var newStr = newVal.toString();
+  if (oldStr != '' && oldStr != newStr) {
+    // Note the addListener function does not end up creating duplicate
+    // listeners.  There can be only one listener per event at a time.
+    // Reference: https://developer.mozilla.org/en/DOM/element.addEventListener
+    node.addEventListener('webkitAnimationEnd', clearHighlight, false);
+    node.setAttribute('highlighted');
+  }
+}
 
 (function () {
 // Contains the latest snapshot of sync about info.
@@ -16,6 +32,7 @@ function refreshAboutInfo(aboutInfo) {
 
 function onLoad() {
   chrome.sync.getAboutInfo(refreshAboutInfo);
+
   chrome.sync.onServiceStateChanged.addListener(function() {
     chrome.sync.getAboutInfo(refreshAboutInfo);
   });
