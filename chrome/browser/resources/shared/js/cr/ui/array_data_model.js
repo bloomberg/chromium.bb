c2f554e07e247da179bc5a5503d499bc1addc1f5
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 05:09:41 +0000

    ui/list.js: prevent from sorting with the previous status.
    Clicking columns doesn't change the sort status if the click is just before updating list. This is because the delayed process saves the previous sort status and uses them.
    
    To avoid it, this cl prevents to sort on updating if the sort status has been changed.
    
    BUG=110652
    TEST=manual
    
    Review URL: http://codereview.chromium.org/9255013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119009 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 77bd542c6e5e223bcd7e76189235634ad8f30e48

--- chrome/browser/resources/shared/js/cr/ui/array_data_model.js
+++ chrome/browser/resources/shared/js/cr/ui/array_data_model.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -181,9 +181,9 @@ cr.define('cr.ui', function() {
       // change), and then sort again.
       // Still need to finish the sorting above (including events), so
       // list will not go to inconsistent state.
-      if (status.field) {
-        setTimeout(this.sort.bind(this, status.field, status.direction), 0);
-      }
+      if (status.field)
+        this.delayedSort_(status.field, status.direction);
+
       return rv;
     },
 
@@ -227,7 +227,7 @@ cr.define('cr.ui', function() {
         // We should first call prepareSort (data may change), and then sort.
         // Still need to finish the sorting above (including events), so
         // list will not go to inconsistent state.
-        setTimeout(this.sort.bind(this, status.field, status.direction), 0);
+        this.delayedSort_(status.field, status.direction);
       }
     },
 
@@ -258,6 +258,25 @@ cr.define('cr.ui', function() {
 
     /**
      * Sorts data model according to given field and direction and dispathes
+     * sorted event with delay. If no need to delay, use sort() instead.
+     * @param {string} field Sort field.
+     * @param {string} direction Sort direction.
+     * @private
+     */
+    delayedSort_: function(field, direction) {
+      var self = this;
+      setTimeout(function() {
+        // If the sort status has been changed, sorting has already done
+        // on the change event.
+        if (field == self.sortStatus.field &&
+            direction == self.sortStatus.direction) {
+          self.sort(field, direction);
+        }
+      }, 0);
+    },
+
+    /**
+     * Sorts data model according to given field and direction and dispathes
      * sorted event.
      * @param {string} field Sort field.
      * @param {string} direction Sort direction.
@@ -277,6 +296,7 @@ cr.define('cr.ui', function() {
      * Sorts data model according to given field and direction.
      * @param {string} field Sort field.
      * @param {string} direction Sort direction.
+     * @private
      */
     doSort_: function(field, direction) {
       var compareFunction = this.sortFunction_(field, direction);
@@ -316,6 +336,7 @@ cr.define('cr.ui', function() {
      * or default compare function
      * @param {string} field Sort field.
      * @param {function(*, *): number} Compare function.
+     * @private
      */
     createCompareFunction_: function(field) {
       var compareFunction =
@@ -336,6 +357,7 @@ cr.define('cr.ui', function() {
      * @param {string} field Sort field.
      * @param {string} direction Sort direction.
      * @param {function(*, *): number} Compare function.
+     * @private
      */
     sortFunction_: function(field, direction) {
       var compareFunction = null;
