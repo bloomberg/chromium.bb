af6123d34a35647bdb09c5af8776d7cafc8383c2
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-25 05:14:22 +0000

    ui/list.js: prevent to set removed item to pinnedItem.
    ensureAllItemSizesInCache() creates temporary list items and caches them, but temporary list items are removed from the list. Removed item is not belong to the list, so that it cannot be a pinnedItem.
    
    BUG=none
    TEST=manual
    
    Review URL: http://codereview.chromium.org/9159016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119011 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 81737d203d40abaa318f612527db2b18a4e267e0

--- chrome/browser/resources/shared/js/cr/ui/list.js
+++ chrome/browser/resources/shared/js/cr/ui/list.js
@@ -972,6 +972,12 @@ cr.define('cr.ui', function() {
 
       while (currentIndex < lastIndex)
         insert(this);
+
+
+      for (var i in newCachedItems) {
+        if (!newCachedItems[i].parentElement)
+          console.log("I'm hogehgoe! " + i);
+      }
     },
 
     /**
@@ -1083,7 +1089,10 @@ cr.define('cr.ui', function() {
           this.removeChild(this.pinnedItem_);
         this.pinnedItem_ = undefined;
       }
-      this.pinnedItem_ = this.pinnedItem_ || cachedItems[leadIndex];
+      if (!this.pinnedItem_ && cachedItems[leadIndex] &&
+          cachedItems[leadIndex].parentNode == this) {
+        this.pinnedItem_ = cachedItems[leadIndex];
+      }
 
       this.mergeItems(firstIndex, lastIndex, cachedItems, newCachedItems);
 
