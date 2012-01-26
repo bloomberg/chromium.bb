dc2c8ef3952024e3f0cecac816478ba9ec573a43
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-26 19:10:58 +0000

    Flakify PluginReferrerTest on Mac OS X
    TBR=jam@chromium.org
    BUG=111508
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9235066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119266 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d6d971b26a2a174c4142560999c3f0ad5c33d6d9

--- chrome/test/ui/npapi_uitest.cc
+++ chrome/test/ui/npapi_uitest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -382,6 +382,10 @@ TEST_F(NPAPITesterBase, NoHangIfInitCrashes) {
 
 #endif
 
+#if defined(OS_MACOSX)
+// http://crbug.com/111508
+#define PluginReferrerTest FLAKY_PluginReferrerTest
+#endif
 TEST_F(NPAPIVisiblePluginTester, PluginReferrerTest) {
   GURL url(URLRequestMockHTTPJob::GetMockUrl(
                FilePath(FILE_PATH_LITERAL(
