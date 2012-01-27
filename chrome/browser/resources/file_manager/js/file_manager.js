c44fe0922c5ae3413b4ba6bf9c8a223fb638207e
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 21:05:07 +0000

    Revert "Display File shelf instead of Chromebook in breadcrumb."
    This reverts commit 960228a5d2a476fa41f1d7c6fbdaed225eb9616e.
    Codereview site is http://codereview.chromium.org/9227009/
    We decide to use Chromebook in breadcrumb for simplicity in R18.
    
    
    BUG=chromium-os:25007
    TEST=Open file manager and select Chromebook in the left panel. It should say
    "Chromebook" in the breadcrumbs.
    
    
    Review URL: http://codereview.chromium.org/9146047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119494 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f11020d4254753acac7141c4f6da3f344119cf46

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -2336,15 +2336,7 @@ FileManager.prototype = {
       var div = doc.createElement('div');
 
       div.className = 'breadcrumb-path';
-      if (i == 0) {
-        // Need to say "File shelf" instead of "Chromebook" in the breadcrumb
-        // after select Chromebook in the left panel.
-        div.textContent = pathName == DirectoryModel.DOWNLOADS_DIRECTORY ?
-            str('CHROMEBOOK_DIRECTORY_BREADCRUMB_LABEL') :
-            this.getRootLabel_(path);
-      } else {
-        div.textContent = pathName;
-      }
+      div.textContent = i == 0 ? this.getRootLabel_(path) : pathName;
 
       path = path + '/';
       div.path = path;
