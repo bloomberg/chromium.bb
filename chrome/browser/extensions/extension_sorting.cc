f966322a985d88616101294045976d35ee2f692c
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-27 23:16:50 +0000

    Clean up GetMinOrMaxAppLaunchOrdinalsOnPage
    Clean up GetMinOrMaxAppLaunchOrdinalsOnPage to use the  generated map to quickly find the the min or max app launch ordinal on a page, instead of checking all of the apps.
    
    BUG=110762
    TEST=unit_tests:ExtensionSorting*
    
    
    Review URL: http://codereview.chromium.org/9289055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119524 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2ac208a649211db35ba84922d77fe7b4041325f5

--- chrome/browser/extensions/extension_sorting.cc
+++ chrome/browser/extensions/extension_sorting.cc
@@ -376,28 +376,22 @@ StringOrdinal ExtensionSorting::PageIntegerAsStringOrdinal(size_t page_index)
 StringOrdinal ExtensionSorting::GetMinOrMaxAppLaunchOrdinalsOnPage(
     const StringOrdinal& target_page_ordinal,
     AppLaunchOrdinalReturn return_type) const {
-  const DictionaryValue* extensions = pref_service_->GetDictionary(
-          ExtensionPrefs::kExtensionsPref);
-  CHECK(extensions);
   CHECK(target_page_ordinal.IsValid());
 
   StringOrdinal return_value;
-  for (DictionaryValue::key_iterator ext_it = extensions->begin_keys();
-       ext_it != extensions->end_keys(); ++ext_it) {
-    const StringOrdinal& page_ordinal = GetPageOrdinal(*ext_it);
-    if (page_ordinal.IsValid() && page_ordinal.Equal(target_page_ordinal)) {
-      const StringOrdinal& app_launch_ordinal = GetAppLaunchOrdinal(*ext_it);
-      if (app_launch_ordinal.IsValid()) {
-        if (!return_value.IsValid())
-          return_value = app_launch_ordinal;
-        else if (return_type == ExtensionSorting::MIN_ORDINAL &&
-                 app_launch_ordinal.LessThan(return_value))
-          return_value = app_launch_ordinal;
-        else if (return_type == ExtensionSorting::MAX_ORDINAL &&
-                 app_launch_ordinal.GreaterThan(return_value))
-          return_value = app_launch_ordinal;
-      }
-    }
+
+  PageOrdinalMap::const_iterator page =
+      ntp_ordinal_map_.find(target_page_ordinal);
+  if (page != ntp_ordinal_map_.end()) {
+    const AppLaunchOrdinalMap& app_list = page->second;
+
+    if (app_list.empty())
+      return StringOrdinal();
+
+    if (return_type == ExtensionSorting::MAX_ORDINAL)
+      return_value = app_list.rbegin()->first;
+    else if (return_type == ExtensionSorting::MIN_ORDINAL)
+      return_value = app_list.begin()->first;
   }
 
   return return_value;
