5f199e6b5e0dba3164e3ff85a10305354e45ab4a
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-28 02:16:20 +0000

    Convert use of int ms to TimeDelta in content/plugin.
    BUG=108171
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9223020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119556 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6189cd94d214d936e14587b250095aa1a7f7ab6a

--- content/plugin/plugin_channel.cc
+++ content/plugin/plugin_channel.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -31,7 +31,7 @@ void PluginReleaseCallback() {
 }
 
 // How long we wait before releasing the plugin process.
-const int kPluginReleaseTimeMs = 5 * 60 * 1000;  // 5 minutes
+const int kPluginReleaseTimeMinutes = 5;
 
 }  // namespace
 
@@ -177,7 +177,9 @@ PluginChannel::~PluginChannel() {
     base::CloseProcessHandle(renderer_handle_);
 
   MessageLoop::current()->PostDelayedTask(
-      FROM_HERE, base::Bind(&PluginReleaseCallback), kPluginReleaseTimeMs);
+      FROM_HERE,
+      base::Bind(&PluginReleaseCallback),
+      base::TimeDelta::FromMinutes(kPluginReleaseTimeMinutes));
 }
 
 bool PluginChannel::Send(IPC::Message* msg) {
