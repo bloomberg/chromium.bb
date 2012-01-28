bf9ea20b8423963a8e4080a3b47bbf46cbf8b618
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-28 03:14:28 +0000

    Added missing member initialization
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9295008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119575 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ee00488b9cee9309306c316806f25ce190680609

--- content/test/test_url_fetcher_factory.cc
+++ content/test/test_url_fetcher_factory.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -271,7 +271,8 @@ class FakeURLFetcher : public TestURLFetcher {
 };
 
 FakeURLFetcherFactory::FakeURLFetcherFactory()
-    : ScopedURLFetcherFactory(ALLOW_THIS_IN_INITIALIZER_LIST(this)) {
+    : ScopedURLFetcherFactory(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
+      default_factory_(NULL) {
 }
 
 FakeURLFetcherFactory::FakeURLFetcherFactory(
