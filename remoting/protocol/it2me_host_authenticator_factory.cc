5410255fdb0eee7c3ad1dd2d10874af17ba43312
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 02:16:45 +0000

    Enable NegotiatingAuthentication in It2Me host.
    BUG=105214
    
    Review URL: https://chromiumcodereview.appspot.com/9113047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119819 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8db04987509e981472144182ec0e96c2a3abc218

--- remoting/protocol/it2me_host_authenticator_factory.cc
+++ remoting/protocol/it2me_host_authenticator_factory.cc
@@ -7,7 +7,7 @@
 #include "base/logging.h"
 #include "crypto/rsa_private_key.h"
 #include "remoting/protocol/v1_authenticator.h"
-#include "remoting/protocol/v2_authenticator.h"
+#include "remoting/protocol/negotiating_authenticator.h"
 
 namespace remoting {
 namespace protocol {
@@ -27,13 +27,11 @@ It2MeHostAuthenticatorFactory::~It2MeHostAuthenticatorFactory() {
 scoped_ptr<Authenticator> It2MeHostAuthenticatorFactory::CreateAuthenticator(
     const std::string& remote_jid,
     const buzz::XmlElement* first_message) {
-  // TODO(sergeyu): V2 authenticator is not finished yet. Enable it
-  // here when it is finished. crbug.com/105214
-  //
-  // if (V2Authenticator::IsEkeMessage(first_message)) {
-  //   return V2Authenticator::CreateForHost(
-  //       local_cert_, *local_private_key_, shared_secret_);
-  // }
+  if (NegotiatingAuthenticator::IsNegotiableMessage(first_message)) {
+    return NegotiatingAuthenticator::CreateForHost(
+        local_cert_, *local_private_key_, shared_secret_,
+        AuthenticationMethod::NONE);
+  }
 
   return scoped_ptr<Authenticator>(new V1HostAuthenticator(
       local_cert_, *local_private_key_, shared_secret_, remote_jid));
