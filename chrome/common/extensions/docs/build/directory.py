129c19fc63efd71b59225d75cdfa2ab736311cc3
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 23:47:33 +0000

    Honor 'nodoc' in samples.html of API documentation
    BUG=112153
    TEST=no
    
    
    Review URL: http://codereview.chromium.org/9315008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119988 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fe7fd6d8ac7c8d480944034505f636404b4a0fc9

--- chrome/common/extensions/docs/build/directory.py
+++ chrome/common/extensions/docs/build/directory.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -122,6 +122,8 @@ class ApiManifest(object):
     if module.has_key(key):
       methods.extend(module[key])
     for method in methods:
+      if method.has_key('nodoc'):
+        continue
       method_name = 'chrome.%s.%s' % (namespace, method['name'])
       hashprefix = 'method'
       if key == 'events':
