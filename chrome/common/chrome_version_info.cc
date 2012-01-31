ff5cf38972d5fd99a2d934185d70fb841953f5b9
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 14:19:03 +0000

    Fix duplicate OFFICIAL_BUILD macros.
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/9212049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119893 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9e0cdb821b66e00389142e175dfbc15ab9535d42

--- chrome/common/chrome_version_info.cc
+++ chrome/common/chrome_version_info.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -86,7 +86,7 @@ std::string VersionInfo::LastChange() const {
 }
 
 bool VersionInfo::IsOfficialBuild() const {
-  return OFFICIAL_BUILD;
+  return IS_OFFICIAL_BUILD;
 }
 
 #endif
