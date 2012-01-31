bf76678dbf04c7ab8f903290ff0a1a0c8f7cb174
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 02:30:04 +0000

    aura: Fix login crash in UserImageScreen.
    Let UserImageScreen know UserImageScreenHandler is destroyed.
    
    BUG=111940
    TEST=Verify crash in issue 111940 no longer happens.
    
    
    Review URL: http://codereview.chromium.org/9159046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119824 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8a34f4bfece0167e819066016b26a84ee2cc5a2b

--- chrome/browser/ui/webui/chromeos/login/user_image_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/user_image_screen_handler.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -42,6 +42,8 @@ UserImageScreenHandler::UserImageScreenHandler()
 }
 
 UserImageScreenHandler::~UserImageScreenHandler() {
+  if (screen_)
+    screen_->OnActorDestroyed(this);
 }
 
 void UserImageScreenHandler::GetLocalizedStrings(
