d82927f68aa30ed23d496bff3a710373e94f3de9
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-01-31 23:17:07 +0000

    Bug fixes for basic playback test.
    Does the following:
    - Removes race between autoplay and explicit .play().
    - Removes race between seeked and .play().
    - Adds logging for several other failure type events.
    - Moves the events.join() code into Python so better errors can be
      reported.
    - Instead of opening a new tab for each video in the matrix, reuses the
      same tab for each playback.  Ensures abort and emptied events fire on
      src switch.
    
    Reusing the same tab was necessary to prevent the video from hanging on
    the Linux bot, as a nice bonus it shaves seconds off the runtime.
    
    BUG=111603
    TEST=Ran on Linux bot.
    
    Review URL: https://chromiumcodereview.appspot.com/9233046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@119985 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a7b35ed103e8fb38f6eff3ccbf3cda2282c38874

--- chrome/test/functional/media/media_basic_playback.py
+++ chrome/test/functional/media/media_basic_playback.py
@@ -25,32 +25,51 @@ _TEST_VIDEOS = [
     'bear.mp4', 'bear.ogv', 'bear.webm', 'bear_silent.mp4', 'bear_silent.ogv',
     'bear_silent.webm']
 
+# Expected events for the first iteration and every iteration thereafter.
+_EXPECTED_EVENTS_0 = [('ended', 2), ('playing', 2), ('seeked', 1)]
+_EXPECTED_EVENTS_n = [('abort', 1), ('emptied', 1)] + _EXPECTED_EVENTS_0
+
 
 class MediaConstrainedNetworkPerfTest(pyauto.PyUITest):
   """PyAuto test container.  See file doc string for more information."""
 
   def testBasicPlaybackMatrix(self):
-    """Launches HTML test which plays a video until end, seeks, and replays.
+    """Launches HTML test which plays each video until end, seeks, and replays.
 
     Specifically ensures that after the above sequence of events, the following
-    is true:
+    are true:
 
-        1. 2x playing, 2x ended, 1x seeked, 0x error, and 0x abort events.
-        2. video.currentTime == video.duration.
+        1. The first video has only 2x playing, 2x ended, and 1x seeked events.
+        2. Each subsequent video additionally has 1x abort and 1x emptied due to
+           switching of the src attribute.
+        3. video.currentTime == video.duration for each video.
+
+    See the HTML file at _TEST_HTML_PATH for more information.
     """
-    for media in _TEST_VIDEOS:
-      logging.debug('Running basic playback test for %s', media)
+    self.NavigateToURL(self.GetFileURLForDataPath(_TEST_HTML_PATH))
 
-      self.NavigateToURL('%s?media=%s' % (
-          self.GetFileURLForDataPath(_TEST_HTML_PATH), media))
+    for i, media in enumerate(_TEST_VIDEOS):
+      logging.debug('Running basic playback test for %s', media)
 
       # Block until the test finishes and notifies us.  Upon return the value of
       # video.currentTime == video.duration is provided.
-      self.assertTrue(self.ExecuteJavascript('true;'))
+      try:
+        self.assertTrue(self.ExecuteJavascript("startTest('%s');" % media))
 
-      events = self.GetDOMValue('events').split(',')
-      counts = [(item, events.count(item)) for item in sorted(set(events))]
-      self.assertEqual(counts, [('ended', 2), ('playing', 2), ('seeked', 1)])
+        # PyAuto has trouble with arrays, so convert to string prior to request.
+        events = self.GetDOMValue("events.join(',')").split(',')
+        counts = [(item, events.count(item)) for item in sorted(set(events))]
+
+        # The first loop will not have the abort and emptied events triggered by
+        # changing the video src.
+        if (i == 0):
+          self.assertEqual(counts, _EXPECTED_EVENTS_0)
+        else:
+          self.assertEqual(counts, _EXPECTED_EVENTS_n)
+      except:
+        logging.debug(
+            'Test failed with events: %s', self.GetDOMValue("events.join(',')"))
+        raise
 
 
 if __name__ == '__main__':
