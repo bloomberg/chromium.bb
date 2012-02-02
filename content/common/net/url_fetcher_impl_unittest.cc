e6a2ac62b6cd49147a1c0ace26a10fb869f8f949
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-02 19:00:54 +0000

    Disable URLFetcherTempFileTest.SmallGet
    BUG=112482
    TEST=tree is greener!
    TBR=saintlou@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9323010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120227 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3de01d600a6ac2a3eb1cf2504ed52ee3bd10ec2d

--- content/common/net/url_fetcher_impl_unittest.cc
+++ content/common/net/url_fetcher_impl_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -279,7 +279,7 @@ void URLFetcherTempFileTest::CreateFetcher(const GURL& url) {
   fetcher_->Start();
 }
 
-TEST_F(URLFetcherTempFileTest, SmallGet) {
+TEST_F(URLFetcherTempFileTest, DISABLED_SmallGet) {
   net::TestServer test_server(net::TestServer::TYPE_HTTP, FilePath(kDocRoot));
   ASSERT_TRUE(test_server.Start());
 
