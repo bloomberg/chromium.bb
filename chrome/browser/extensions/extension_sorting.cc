deaade71e6e1095ffd0c3015586237fb04d224bd
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 15:36:39 +0000

    Fix Idle NTPs to Update
    Fix the extension code so that everytime a icon is moved it sends a notification. It was only sending notifications when the icon was moved and it had to get its ordinal value (which only occured if it was moved to a page that had at least 1 other icon).
    
    BUG=110269
    TEST=When two NTP are open, users should be able to move one icon to a new page and have the change reflected in both views
    
    
    Review URL: http://codereview.chromium.org/9307066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120332 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 23211fbcc81a80f4221d2b37a18c646a6730fa4b

--- chrome/browser/extensions/extension_sorting.cc
+++ chrome/browser/extensions/extension_sorting.cc
@@ -194,25 +194,27 @@ void ExtensionSorting::OnExtensionMoved(
     const std::string& moved_extension_id,
     const std::string& predecessor_extension_id,
     const std::string& successor_extension_id) {
-  // If there are no neighbors then no changes are required, so we can return.
-  if (predecessor_extension_id.empty() && successor_extension_id.empty())
-    return;
-
-  if (predecessor_extension_id.empty()) {
-    SetAppLaunchOrdinal(
-        moved_extension_id,
-        GetAppLaunchOrdinal(successor_extension_id).CreateBefore());
-  } else if (successor_extension_id.empty()) {
-    SetAppLaunchOrdinal(
-        moved_extension_id,
-        GetAppLaunchOrdinal(predecessor_extension_id).CreateAfter());
-  } else {
-    const StringOrdinal& predecessor_ordinal =
-        GetAppLaunchOrdinal(predecessor_extension_id);
-    const StringOrdinal& successor_ordinal =
-        GetAppLaunchOrdinal(successor_extension_id);
-    SetAppLaunchOrdinal(moved_extension_id,
-                        predecessor_ordinal.CreateBetween(successor_ordinal));
+  // We only need to change the StringOrdinal if there are neighbours.
+  if (!predecessor_extension_id.empty() || !successor_extension_id.empty()) {
+    if (predecessor_extension_id.empty()) {
+      // Only a successor.
+      SetAppLaunchOrdinal(
+          moved_extension_id,
+          GetAppLaunchOrdinal(successor_extension_id).CreateBefore());
+    } else if (successor_extension_id.empty()) {
+      // Only a predecessor.
+      SetAppLaunchOrdinal(
+          moved_extension_id,
+          GetAppLaunchOrdinal(predecessor_extension_id).CreateAfter());
+    } else {
+      // Both a successor and predecessor
+      const StringOrdinal& predecessor_ordinal =
+          GetAppLaunchOrdinal(predecessor_extension_id);
+      const StringOrdinal& successor_ordinal =
+          GetAppLaunchOrdinal(successor_extension_id);
+      SetAppLaunchOrdinal(moved_extension_id,
+                          predecessor_ordinal.CreateBetween(successor_ordinal));
+    }
   }
 
   content::NotificationService::current()->Notify(
