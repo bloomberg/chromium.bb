14ca9df9d9f3a83a7c1da644c14b12092aa7f291
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 08:28:54 +0000

    Allow verbatim codec to be negotiated for a Chromoting session since having another codec helps testing the coder/decoder pipeline.
    BUG=109938
    
    
    Review URL: http://codereview.chromium.org/9315046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120297 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0a364972ead03ee3560d473d5324fd93e3d55672

--- remoting/protocol/content_description.cc
+++ remoting/protocol/content_description.cc
@@ -40,6 +40,7 @@ const char kDatagramTransport[] = "datagram";
 const char kSrtpTransport[] = "srtp";
 const char kRtpDtlsTransport[] = "rtp-dtls";
 
+const char kVerbatimCodec[] = "verbatim";
 const char kVp8Codec[] = "vp8";
 const char kZipCodec[] = "zip";
 
@@ -60,6 +61,8 @@ const char* GetTransportName(ChannelConfig::TransportType type) {
 
 const char* GetCodecName(ChannelConfig::Codec type) {
   switch (type) {
+    case ChannelConfig::CODEC_VERBATIM:
+      return kVerbatimCodec;
     case ChannelConfig::CODEC_VP8:
       return kVp8Codec;
     case ChannelConfig::CODEC_ZIP:
@@ -111,7 +114,9 @@ bool ParseTransportName(const std::string& value,
 }
 
 bool ParseCodecName(const std::string& value, ChannelConfig::Codec* codec) {
-  if (value == kVp8Codec) {
+  if (value == kVerbatimCodec) {
+    *codec = ChannelConfig::CODEC_VERBATIM;
+  } else if (value == kVp8Codec) {
     *codec = ChannelConfig::CODEC_VP8;
   } else if (value == kZipCodec) {
     *codec = ChannelConfig::CODEC_ZIP;
