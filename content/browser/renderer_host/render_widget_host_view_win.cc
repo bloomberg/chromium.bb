c5424fb971c380dfc09a4f44b56042d6a8211a7a
bashi@chromium.org <bashi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-03 08:23:11 +0000

    Enable/Disable IME based on |text_input_type_| when a view is selected
    This prevents IME consuming keyboard events.
    
    BUG=80772
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/9307020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120293 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5079886571581fbab1f97e82c7f80a5068f73151

--- content/browser/renderer_host/render_widget_host_view_win.cc
+++ content/browser/renderer_host/render_widget_host_view_win.cc
@@ -298,6 +298,11 @@ inline void SetTouchType(TOUCHINPUT* point, int type) {
   point->dwFlags = (point->dwFlags & kTouchMask) | type;
 }
 
+bool ShouldEnableIME(ui::TextInputType type) {
+  return type != ui::TEXT_INPUT_TYPE_NONE &&
+      type != ui::TEXT_INPUT_TYPE_PASSWORD;
+}
+
 }  // namespace
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -402,6 +407,11 @@ void RenderWidgetHostViewWin::DidBecomeSelected() {
     tab_switch_paint_time_ = TimeTicks::Now();
   is_hidden_ = false;
   EnsureTooltip();
+
+  if (ShouldEnableIME(text_input_type_))
+    ime_input_.EnableIME(m_hWnd);
+  else
+    ime_input_.DisableIME(m_hWnd);
 
   // |render_widget_host_| may be NULL if the TabContents is in the process of
   // closing.
@@ -744,8 +754,7 @@ void RenderWidgetHostViewWin::TextInputStateChanged(
   // as true. We need to support "can_compose_inline=false" for PPAPI plugins
   // that may want to avoid drawing composition-text by themselves and pass
   // the responsibility to the browser.
-  bool is_enabled = (type != ui::TEXT_INPUT_TYPE_NONE &&
-      type != ui::TEXT_INPUT_TYPE_PASSWORD);
+  bool is_enabled = ShouldEnableIME(type);
   if (text_input_type_ != type) {
     text_input_type_ = type;
     if (is_enabled)
