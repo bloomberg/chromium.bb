6ebc6f783c4548be64b5379b205f39dae8599c0a
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-04 01:58:10 +0000

    MakeContextCurrent before querying for swapbuffers_complete GL extension
    RenderViewImpl::SupportsAsynchronousSwapBuffers() makes GL calls without calling makeContextCurrent(). This used to work,
    but with http://trac.webkit.org/changeset/106700 single-threaded context creation no longer does an implicit makeContextCurrent()
    so this fails DCHECKs in the command buffer code.
    
    BUG=none
    TEST=can instantiate compositor in debug mode at WebKit rev >= 106700
    
    Review URL: https://chromiumcodereview.appspot.com/9317105
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120455 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 274343e3988c995f574765b25d84b567f602219f

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -4518,6 +4518,8 @@ bool RenderViewImpl::SupportsAsynchronousSwapBuffers() {
   WebKit::WebGraphicsContext3D* context = webview()->graphicsContext3D();
   if (!context)
     return false;
+  if (!context->makeContextCurrent())
+    return false;
   std::string extensions(context->getRequestableExtensionsCHROMIUM().utf8());
   return extensions.find("GL_CHROMIUM_swapbuffers_complete_callback") !=
       std::string::npos;
