2f8e4d546540c139d315ef6714771c550bbfd04a
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-06 22:29:27 +0000

    Implement required tracing API for UnitTestWebKitPlatformSupport.
    Review URL: https://chromiumcodereview.appspot.com/9325048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120632 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d0edde8e09990240abc94ec7f54eea21dd9f042f

--- content/test/unittest_test_suite.cc
+++ content/test/unittest_test_suite.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,6 +18,12 @@ class UnitTestTestSuite::UnitTestWebKitPlatformSupport
   virtual void cryptographicallyRandomValues(unsigned char* buffer,
                                              size_t length) OVERRIDE {
     memset(buffer, 0, length);
+  }
+  virtual const unsigned char* getTraceCategoryEnabledFlag(
+      const char* categoryName) {
+    // Causes tracing macros to be disabled.
+    static const unsigned char kEnabled = 0;
+    return &kEnabled;
   }
 };
 
