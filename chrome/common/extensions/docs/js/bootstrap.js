825f72b2f2e9c4c7d542e856d65c86f9f48404eb
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 20:21:08 +0000

    Adding a trailing '/' to the doc switcher URL.
    BUG=112999
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/9350019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120815 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fc8a66eca756ef502d67aab36c444d88a6dac5b2

--- chrome/common/extensions/docs/js/bootstrap.js
+++ chrome/common/extensions/docs/js/bootstrap.js
@@ -1,3 +1,7 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 var fileXHREnabled = function() {
   var xhr = new XMLHttpRequest();
   try {
@@ -76,7 +80,7 @@ if (window.location.search == "?regenerate" ||
           currentBranch.toUpperCase();
       document.getElementById('branchWarning').style.display = 'block';
       document.getElementById('branchChooser').onchange = function() {
-        location.href = officialURL + this.value;
+        location.href = officialURL + this.value + "/";
       };
     }
   }
