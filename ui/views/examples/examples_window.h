6c77e866bb3691c6be166be8b1becb5334d12d9c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-07 00:38:24 +0000

    views: Fix header guards comment of examples_window.h
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9337001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@120656 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8513a6db065f003c2e83ac4c2d542150b9967f73

--- ui/views/examples/examples_window.h
+++ ui/views/examples/examples_window.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,9 +8,11 @@
 
 namespace views {
 namespace examples {
+
 // Shows a window with the views examples in it.
 void ShowExamplesWindow(bool quit_on_close);
+
 }  // namespace examples
 }  // namespace views
 
-#endif  // VIEWS_EXAMPLES_EXAMPLE_MAIN_H_
+#endif  // UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_H_
