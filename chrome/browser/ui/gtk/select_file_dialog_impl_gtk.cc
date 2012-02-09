16d560cabf06c90f1184497894f3b2f37daaf1f1
primiano@chromium.org <primiano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-09 10:40:00 +0000

    Replaced base::nix::GetFileMimeType with net::GetMimeTypeFromExtension for consistent reverse lookup of mime types in file dialogs under GTK
    BUG=111321
    TEST=Click browse on a <input type="file" accept="image/jpeg" /> under GTK. The file dialog shown should only let the user peek jpeg images rather then apply a catch-all filter.
    
    
    Review URL: http://codereview.chromium.org/9288065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121222 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4703610dc7c8dceeace4d1a916eadb95b634c69a

--- chrome/browser/ui/gtk/select_file_dialog_impl_gtk.cc
+++ chrome/browser/ui/gtk/select_file_dialog_impl_gtk.cc
@@ -1,15 +1,16 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include <gtk/gtk.h>
 #include <map>
 #include <set>
+#include <vector>
 
 #include "base/file_util.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
-#include "base/nix/mime_util_xdg.h"
+#include "base/string_util.h"
 #include "base/sys_string_conversions.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_restrictions.h"
@@ -18,6 +19,7 @@
 #include "chrome/browser/ui/select_file_dialog.h"
 #include "content/public/browser/browser_thread.h"
 #include "grit/generated_resources.h"
+#include "net/base/mime_util.h"
 #include "ui/base/gtk/gtk_signal.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -221,16 +223,32 @@ void SelectFileDialogImplGTK::SelectFileImpl(
 void SelectFileDialogImplGTK::AddFilters(GtkFileChooser* chooser) {
   for (size_t i = 0; i < file_types_.extensions.size(); ++i) {
     GtkFileFilter* filter = NULL;
+    std::set<std::string> fallback_labels;
+
     for (size_t j = 0; j < file_types_.extensions[i].size(); ++j) {
-      if (!file_types_.extensions[i][j].empty()) {
+      const std::string& current_extension = file_types_.extensions[i][j];
+      if (!current_extension.empty()) {
+        std::string mime_type;
+        bool found_valid_mime_type;
+        {
+          // Allow IO in the file dialog. (http://crbug.com/72637)
+          base::ThreadRestrictions::ScopedAllowIO allow_io;
+          found_valid_mime_type = net::GetMimeTypeFromExtension(
+            current_extension, &mime_type);
+        }
         if (!filter)
           filter = gtk_file_filter_new();
 
-        // Allow IO in the file dialog. http://crbug.com/72637
-        base::ThreadRestrictions::ScopedAllowIO allow_io;
-        std::string mime_type = base::nix::GetFileMimeType(
-            FilePath("name").ReplaceExtension(file_types_.extensions[i][j]));
-        gtk_file_filter_add_mime_type(filter, mime_type.c_str());
+        // Try to add a mime-type filter instead of an extension filter if
+        // possible. (http://crbug.com/12347)
+        if (found_valid_mime_type) {
+          gtk_file_filter_add_mime_type(filter, mime_type.c_str());
+          fallback_labels.insert(mime_type);
+        } else {
+          std::string pattern = "*." + current_extension;
+          gtk_file_filter_add_pattern(filter, pattern.c_str());
+          fallback_labels.insert(pattern);
+        }
       }
     }
     // We didn't find any non-empty extensions to filter on.
@@ -243,13 +261,12 @@ void SelectFileDialogImplGTK::AddFilters(GtkFileChooser* chooser) {
       gtk_file_filter_set_name(filter, UTF16ToUTF8(
           file_types_.extension_description_overrides[i]).c_str());
     } else {
-      // Allow IO in the file dialog. http://crbug.com/72637
-      base::ThreadRestrictions::ScopedAllowIO allow_io;
       // There is no system default filter description so we use
-      // the MIME type itself if the description is blank.
-      std::string mime_type = base::nix::GetFileMimeType(
-          FilePath("name").ReplaceExtension(file_types_.extensions[i][0]));
-      gtk_file_filter_set_name(filter, mime_type.c_str());
+      // the MIME types themselves if the description is blank.
+      std::vector<std::string> fallback_labels_vector(fallback_labels.begin(),
+                                                      fallback_labels.end());
+      std::string fallback_label = JoinString(fallback_labels_vector, ',');
+      gtk_file_filter_set_name(filter, fallback_label.c_str());
     }
 
     gtk_file_chooser_add_filter(chooser, filter);
