9b65b7d7f15959c1d48dbc259d5ef16a0c92d386
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-10 14:41:38 +0000

    net: SSL blacklisting for Nordea Bank
    They will be updating their serving infrastructure in the coming months,
    so this is acceptably short term. It's not clear whether they have
    issues with False Start or record splitting.
    
    BUG=113625
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121450 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d479727aebc869a1feeb7f8d341efcca997cb269

--- net/base/ssl_false_start_blacklist_unittest.cc
+++ net/base/ssl_false_start_blacklist_unittest.cc
@@ -30,6 +30,10 @@ TEST(SSLFalseStartBlacklistTest, IsMember) {
   EXPECT_TRUE(SSLFalseStartBlacklist::IsMember("www.toto-dream.com"));
   EXPECT_TRUE(SSLFalseStartBlacklist::IsMember("billing.ntt-east.co.jp"));
   EXPECT_TRUE(SSLFalseStartBlacklist::IsMember("thepayplace.com"));
+
+  EXPECT_TRUE(SSLFalseStartBlacklist::IsMember("nordea.se"));
+  EXPECT_TRUE(SSLFalseStartBlacklist::IsMember("nordea.com"));
+  EXPECT_TRUE(SSLFalseStartBlacklist::IsMember("nordeanetbank.dk"));
 }
 
 }  // namespace net
