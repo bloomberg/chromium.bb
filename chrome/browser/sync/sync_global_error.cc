38b2e3bc512ef6f63355f27b64162210cfbf552f
altimofeev@chromium.org <altimofeev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 09:06:36 +0000

    Revert 121858 - [cros] Suggest sign-out/sign-in when sync login error appears.
    Also, OAuth token is invalidated when sync error appears. This is done
    to force Gaia login flow after user's sign-out.
    
    BUG=chroimum-os:24883
    TEST=manual
    
    
    Review URL: http://codereview.chromium.org/9320039
    
    TBR=altimofeev@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9361055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121859 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b1601b9bd5a145011d0b8d06b64cf150107ed87b

--- chrome/browser/sync/sync_global_error.cc
+++ chrome/browser/sync/sync_global_error.cc
@@ -8,7 +8,6 @@
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/profile_sync_service_observer.h"
 #include "chrome/browser/sync/sync_ui_util.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/global_error_service.h"
 #include "chrome/browser/ui/global_error_service_factory.h"
 #include "chrome/common/net/gaia/google_service_auth_error.h"
@@ -50,13 +49,6 @@ string16 SyncGlobalError::MenuItemLabel() {
 }
 
 void SyncGlobalError::ExecuteMenuItem(Browser* browser) {
-#if defined(OS_CHROMEOS)
-  if (service_->GetAuthError().state() != AuthError::NONE) {
-    DLOG(INFO) << "Signing out the user to fix a sync error.";
-    browser->ExecuteCommand(IDC_EXIT);
-    return;
-  }
-#endif
   service_->ShowErrorUI();
 }
 
@@ -84,7 +76,7 @@ void SyncGlobalError::OnBubbleViewDidClose(Browser* browser) {
 }
 
 void SyncGlobalError::BubbleViewAcceptButtonPressed(Browser* browser) {
-  ExecuteMenuItem(browser);
+  service_->ShowErrorUI();
 }
 
 void SyncGlobalError::BubbleViewCancelButtonPressed(Browser* browser) {
