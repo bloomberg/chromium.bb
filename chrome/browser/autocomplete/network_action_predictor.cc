149fff079e6644366a6d304ae9add6d965111125
dominich@chromium.org <dominich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-14 20:35:08 +0000

    Removing dead code from NetworkActionPredictor.
    BUG=none
    
    
    Review URL: http://codereview.chromium.org/9358062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@121926 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dbcfe72cb16222c9f7e7907fcc5f35b27cc25331

--- chrome/browser/autocomplete/network_action_predictor.cc
+++ chrome/browser/autocomplete/network_action_predictor.cc
@@ -45,20 +45,6 @@ COMPILE_ASSERT(arraysize(kConfidenceCutoff) ==
                NetworkActionPredictor::LAST_PREDICT_ACTION,
                ConfidenceCutoff_count_mismatch);
 
-bool GetURLRowForAutocompleteMatch(Profile* profile,
-                                   const AutocompleteMatch& match,
-                                   history::URLRow* url_row) {
-  DCHECK(url_row);
-
-  HistoryService* history_service =
-      profile->GetHistoryService(Profile::EXPLICIT_ACCESS);
-  if (!history_service)
-    return false;
-
-  history::URLDatabase* url_db = history_service->InMemoryDatabase();
-  return url_db && (url_db->GetRowForURL(match.destination_url, url_row) != 0);
-}
-
 }
 
 const int NetworkActionPredictor::kMaximumDaysToKeepEntry = 14;
