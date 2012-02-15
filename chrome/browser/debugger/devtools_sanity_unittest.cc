116d3a1827213996c81b24ce587befa5a49bb76a
vsevik@chromium.org <vsevik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 12:18:12 +0000

    Unskip DevTools test
    BUG=114104
    TBR=pfeldman
    
    Review URL: https://chromiumcodereview.appspot.com/9360044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122078 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8d6781f6980eb1f53f9a831828ac3a7e47ea78ed

--- chrome/browser/debugger/devtools_sanity_unittest.cc
+++ chrome/browser/debugger/devtools_sanity_unittest.cc
@@ -444,9 +444,8 @@ IN_PROC_BROWSER_TEST_F(DevToolsExperimentalExtensionTest,
 }
 
 // Tests that a content script is in the scripts list.
-// http://crbug.com/114104
 IN_PROC_BROWSER_TEST_F(DevToolsExtensionTest,
-                       DISABLED_TestContentScriptIsPresent) {
+                       TestContentScriptIsPresent) {
   LoadExtension("simple_content_script");
   RunTest("testContentScriptIsPresent", kPageWithContentScript);
 }
