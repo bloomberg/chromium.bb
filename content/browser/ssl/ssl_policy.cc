56c2e9ba4c928de6cfe1abc3057d493d80c84d97
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-15 22:11:00 +0000

    Display an interstitial for weak key errors.
    Without this, the request is canceled, but no interstitial is shown.
    
    I have checked that all other ERR_CERT_ errors are handled and only
    ERR_CERT_NON_UNIQUE_NAME is missing. However, I don't see that
    ERR_CERT_NON_UNIQUE_NAME can actually be returned, so that's probably
    fine.
    
    (There is also ERR_CERT_NOT_IN_DNS, but that should be removed.)
    
    BUG=113645
    TEST=Import a 512-bit CA (see bug) and try to navigate to a site which uses it. Verify that an interstitial is shown.
    
    https://chromiumcodereview.appspot.com/9358060/
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122166 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f7354208762318c870c51e50196a9276ff422afb

--- content/browser/ssl/ssl_policy.cc
+++ content/browser/ssl/ssl_policy.cc
@@ -63,6 +63,7 @@ void SSLPolicy::OnCertError(SSLCertErrorHandler* handler) {
     case net::ERR_CERT_DATE_INVALID:
     case net::ERR_CERT_AUTHORITY_INVALID:
     case net::ERR_CERT_WEAK_SIGNATURE_ALGORITHM:
+    case net::ERR_CERT_WEAK_KEY:
       OnCertErrorInternal(handler, !handler->fatal());
       break;
     case net::ERR_CERT_NO_REVOCATION_MECHANISM:
