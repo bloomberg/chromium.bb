8720530eaab2f69add8d414aa9ef7dac47f12c75
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-16 09:33:10 +0000

    chromeos: dbus: add Bluetooth properties support
    BlueZ predates the standard D-Bus Properties API, so we can't use the
    new dbus::Property<> and dbus::PropertySet for it. Fortunately it did
    at least inspire the standard API, so derive a BluetoothProperty<> and
    BluetoothPropertySet respectively that generate the correct D-Bus
    methods, connect the correct signals and parse them properly.
    
    BUG=chromium-os:22086
    TEST=verified with dbus-send
    
    Change-Id: I71c871175c98a007c547792af8765baf550e4498
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=122199
    
    Review URL: http://codereview.chromium.org/9379044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122254 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ee979cbecb0ed4d8af5c179876fdf9f537dc00d0

---
+++ chrome/browser/chromeos/dbus/bluetooth_property.cc
@@ -0,0 +1,47 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/dbus/bluetooth_property.h"
+
+#include "base/bind.h"
+#include "dbus/message.h"
+#include "dbus/object_proxy.h"
+#include "third_party/cros_system_api/dbus/service_constants.h"
+
+namespace chromeos {
+
+// dbus::PropertySet override.
+void BluetoothPropertySet::ConnectSignals() {
+  dbus::ObjectProxy* object_proxy = this->object_proxy();
+  DCHECK(object_proxy);
+  object_proxy->ConnectToSignal(
+      interface(),
+      bluetooth_common::kPropertyChangedSignal,
+      base::Bind(&dbus::PropertySet::ChangedReceived, GetWeakPtr()),
+      base::Bind(&dbus::PropertySet::ChangedConnected, GetWeakPtr()));
+}
+
+// dbus::PropertySet override.
+void BluetoothPropertySet::ChangedReceived(dbus::Signal* signal) {
+  DVLOG(1) << "ChangedReceived";
+  DCHECK(signal);
+
+  dbus::MessageReader reader(signal);
+  UpdatePropertyFromReader(&reader);
+}
+
+// dbus::PropertySet override.
+void BluetoothPropertySet::GetAll() {
+  dbus::MethodCall method_call(interface(),
+                               bluetooth_common::kGetProperties);
+
+  dbus::ObjectProxy* object_proxy = this->object_proxy();
+  DCHECK(object_proxy);
+  object_proxy->CallMethod(&method_call,
+                           dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+                           base::Bind(&dbus::PropertySet::OnGetAll,
+                                      GetWeakPtr()));
+}
+
+}
