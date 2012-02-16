c705829f36c09445c1154f722563db54df9c3bc8
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-16 00:27:55 +0000

    chromeos: dbus: add Bluetooth properties support
    BlueZ predates the standard D-Bus Properties API, so we can't use the
    new dbus::Property<> and dbus::PropertySet for it. Fortunately it did
    at least inspire the standard API, so derive a BluetoothProperty<> and
    BluetoothPropertySet respectively that generate the correct D-Bus
    methods, connect the correct signals and parse them properly.
    
    BUG=chromium-os:22086
    TEST=verified with dbus-send
    
    Change-Id: I71c871175c98a007c547792af8765baf550e4498
    
    
    Review URL: http://codereview.chromium.org/9379044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122199 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a528abfdaf56b35ccd381cbce2807b5d0da2710e

---
+++ chrome/browser/chromeos/dbus/bluetooth_property.h
@@ -0,0 +1,91 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_DBUS_BLUETOOTH_PROPERTY_H_
+#define CHROME_BROWSER_CHROMEOS_DBUS_BLUETOOTH_PROPERTY_H_
+#pragma once
+
+#include "dbus/message.h"
+#include "dbus/object_proxy.h"
+#include "dbus/property.h"
+#include "third_party/cros_system_api/dbus/service_constants.h"
+
+namespace chromeos {
+
+// BlueZ predates the common D-Bus Properties API (though it inspired it),
+// override dbus::PropertySet to generate the correct method call to get
+// all properties, conenect to the correct signal and parse it correctly.
+//
+// BluetoothPropertySet should be used with BluetoothProperty<>.
+class BluetoothPropertySet : public dbus::PropertySet {
+ public:
+  BluetoothPropertySet(dbus::ObjectProxy* object_proxy,
+                       const std::string& interface,
+                       PropertyChangedCallback callback)
+      : dbus::PropertySet(object_proxy, interface, callback) {}
+
+  // Call after construction to connect property change notification
+  // signals. Sub-classes may override to use different D-Bus signals.
+  virtual void ConnectSignals();
+
+  // Queries the remote object for values of all properties and updates
+  // initial values.
+  virtual void GetAll();
+
+  // Method connected by ConnectSignals() and called by dbus:: when
+  // a property is changed.
+  virtual void ChangedReceived(dbus::Signal* signal);
+};
+
+// BlueZ predates the common D-Bus Properties API (though it inspired it),
+// override dbus::Property<> to generate the correct method call to set a
+// new property value.
+template <class T>
+class BluetoothProperty : public dbus::Property<T> {
+ public:
+  // Import the callbacks into our namespace (this is a template derived from
+  // a template, the C++ standard gets a bit wibbly and doesn't do it for us).
+  //
+  // |success| indicates whether or not the value could be retrived, or new
+  // value set. For Get, if true the new value can be obtained by calling
+  // value() on the property; for Set() a Get() call may be necessary.
+  typedef typename dbus::Property<T>::GetCallback GetCallback;
+  typedef typename dbus::Property<T>::SetCallback SetCallback;
+
+  // Requests an updated value from the remote object incurring a
+  // round-trip. |callback| will be called when the new value is available.
+  //
+  // dbus::Property<> override.
+  void Get(GetCallback callback) {
+    NOTREACHED() << "BlueZ does not implement Get for properties";
+  }
+
+  // Requests that the remote object change the property value to |value|,
+  // |callback| will be called to indicate the success or failure of the
+  // request, however the new value may not be available depending on the
+  // remote object.
+  //
+  // dbus::Property<> override.
+  void Set(const T& value, SetCallback callback) {
+    DVLOG(1) << "Set: " << this->name();
+
+    dbus::MethodCall method_call(this->GetInterface(),
+                                 bluetooth_common::kSetProperty);
+    dbus::MessageWriter writer(&method_call);
+    writer.AppendString(this->name());
+    AppendToWriter(&writer, value);
+
+    dbus::ObjectProxy *object_proxy = this->property_set()->object_proxy();
+    DCHECK(object_proxy);
+    object_proxy->CallMethod(&method_call,
+                             dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
+                             base::Bind(&dbus::Property<T>::OnSet,
+                                        this->GetWeakPtr(),
+                                        callback));
+  }
+};
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_DBUS_BLUETOOTH_PROPERTY_H_
