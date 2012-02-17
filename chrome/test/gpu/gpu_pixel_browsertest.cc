82828bf1f38a3e27de9693897b1936586adfd0d7
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 23:59:44 +0000

    Rebaseline gpu pixel test ref images after resizing window size to 400x300
    BUG=
    TEST=gpu bots
    TBR=jbates
    Review URL: https://chromiumcodereview.appspot.com/9425022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122632 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0cc79d43b03d3266498de36f7412b8048c921071

--- chrome/test/gpu/gpu_pixel_browsertest.cc
+++ chrome/test/gpu/gpu_pixel_browsertest.cc
@@ -351,7 +351,7 @@ class GpuPixelBrowserTest : public InProcessBrowserTest {
 IN_PROC_BROWSER_TEST_F(GpuPixelBrowserTest, MAYBE_WebGLGreenTriangle) {
   // If test baseline needs to be updated after a given revision, update the
   // following number. If no revision requirement, then 0.
-  const int64 ref_img_revision_update = 118461;
+  const int64 ref_img_revision_update = 122628;
 
   gfx::Size container_size(400, 300);
   FilePath url =
@@ -367,7 +367,7 @@ IN_PROC_BROWSER_TEST_F(GpuPixelBrowserTest, MAYBE_WebGLGreenTriangle) {
 IN_PROC_BROWSER_TEST_F(GpuPixelBrowserTest, MAYBE_CSS3DBlueBox) {
   // If test baseline needs to be updated after a given revision, update the
   // following number. If no revision requirement, then 0.
-  const int64 ref_img_revision_update = 118461;
+  const int64 ref_img_revision_update = 122628;
 
   gfx::Size container_size(400, 300);
   FilePath url =
@@ -391,7 +391,7 @@ class Canvas2DPixelTestHD : public GpuPixelBrowserTest {
 IN_PROC_BROWSER_TEST_F(Canvas2DPixelTestHD, MAYBE_Canvas2DRedBoxHD) {
   // If test baseline needs to be updated after a given revision, update the
   // following number. If no revision requirement, then 0.
-  const int64 ref_img_revision_update = 121581;
+  const int64 ref_img_revision_update = 122628;
 
   gfx::Size container_size(400, 300);
   FilePath url =
@@ -415,7 +415,7 @@ class Canvas2DPixelTestSD : public GpuPixelBrowserTest {
 IN_PROC_BROWSER_TEST_F(Canvas2DPixelTestSD, MAYBE_Canvas2DRedBoxSD) {
   // If test baseline needs to be updated after a given revision, update the
   // following number. If no revision requirement, then 0.
-  const int64 ref_img_revision_update = 121581;
+  const int64 ref_img_revision_update = 122628;
 
   gfx::Size container_size(400, 300);
   FilePath url =
