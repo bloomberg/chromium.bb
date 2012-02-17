0d0c9b1038a79b8b2ae409fd4c507e6337f33a55
Andrew de los Reyes <adlr@chromium.org>
2012-02-16 18:20:09 -0800

    util: DistSq()
    A utility function for calculating the distance squared between two
    contacts. Future CLs will use these functions.
    
    BUG=None
    TEST=unittest
    
    Change-Id: I742f3da8ac1f44812763cff84bcb32ded661e01b

--- include/util.h
+++ include/util.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium OS Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium OS Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,6 +6,8 @@
 #define GESTURES_UTIL_H_
 
 #include <math.h>
+
+#include "gestures/include/gestures.h"
 
 namespace gestures {
 
@@ -15,6 +17,21 @@ inline bool FloatEq(float a, float b) {
 
 inline bool DoubleEq(float a, float b) {
   return fabsf(a - b) <= 1e-8;
+}
+
+// Returns the square of the distance between the two contacts.
+template<typename ContactType>  // UnmergedContact or FingerState
+static float DistSq(const ContactType& finger_a,
+                    const FingerState& finger_b) {
+  float dx = finger_a.position_x - finger_b.position_x;
+  float dy = finger_a.position_y - finger_b.position_y;
+  return dx * dx + dy * dy;
+}
+template<typename ContactType>  // UnmergedContact or FingerState
+static float DistSqXY(const ContactType& finger_a, float pos_x, float pos_y) {
+  float dx = finger_a.position_x - pos_x;
+  float dy = finger_a.position_y - pos_y;
+  return dx * dx + dy * dy;
 }
 
 }  // namespace gestures
