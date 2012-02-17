845c0b321a0bff84badf6ebc4713c215f7ea86e6
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-17 16:16:54 +0000

    Removing code that is never compiled under any config.
    Class NativeBrowserFrameDelegate is abstract, with no derivation. 
    browser_frame_gtk.cc has 
    NativeBrowserFrameDelegate* delegate_ 
    which is never set.
    
    BUG=none
    TEST=none
    
    Review URL: http://codereview.chromium.org/9418034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@122518 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 30c55372215faa0d58b6f52896b2dadb0f6b1f0c

--- chrome/browser/ui/views/frame/browser_frame.h
+++ chrome/browser/ui/views/frame/browser_frame.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,7 +9,6 @@
 #include "base/compiler_specific.h"
 #include "base/logging.h"
 #include "build/build_config.h"
-#include "chrome/browser/ui/views/frame/native_browser_frame_delegate.h"
 #include "ui/views/widget/widget.h"
 
 class AvatarMenuButton;
--- chrome/browser/ui/views/frame/browser_frame_gtk.h
+++ chrome/browser/ui/views/frame/browser_frame_gtk.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -33,8 +33,6 @@ class BrowserFrameGtk : public views::NativeWidgetGtk,
                                     GdkEventConfigure* event) OVERRIDE;
 
  private:
-  NativeBrowserFrameDelegate* delegate_;
-
   // The BrowserView is our ClientView. This is a pointer to it.
   BrowserView* browser_view_;
 
