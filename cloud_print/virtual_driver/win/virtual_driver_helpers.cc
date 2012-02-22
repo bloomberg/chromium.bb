e3666d764b29915558f494079f4d9dbc66fd08f5
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 23:03:31 +0000

    Fix typo in 32-bit port monitor name.
    BUG=115304
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9432036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123119 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ff75391b75d96c4862d3e24d3186178dba0b32a6

--- cloud_print/virtual_driver/win/virtual_driver_helpers.cc
+++ cloud_print/virtual_driver/win/virtual_driver_helpers.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -38,7 +38,7 @@ string16 GetPortMonitorDllName() {
   if (IsSystem64Bit()) {
     return string16(L"gcp_portmon64.dll");
   } else {
-    return string16(L"gcp_portmon32.dll");
+    return string16(L"gcp_portmon.dll");
   }
 }
 
