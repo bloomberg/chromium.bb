e9a54a403792131bcc398686e68cbc41292d30a4
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 23:06:45 +0000

    Fix media code to work with new ffmpeg.
    Once ffmpeg git, svn are updated to new ffmpeg, will update DEPS
    in this CL.  Which when committed, should seal the deal for the
    ffmpeg roll.
    
    API Changes:
    avutil: SampleFormat->AVSampleFormat
    avutil: av_get_bits_per_sample_fmt -> av_get_bytes_per_sample
    avcodec: avcodec_open -> avcodec_open2(..., NULL)
    avcodec: avcodec_decode_video2(... AVPacket ...) -> const AVPacket.
    avformat: av_open_input_file -> avformat_open_input
    avformat: av_register_protocol2 -> ffurl_register_protocol
    avformat: av_close_input_file -> avformat_close_input(&...)
    avformat: av_find_stream_info -> avformat_find_stream_info(..., NULL)
    
    URLContext now has a url_open2 method as well, for now I've set this
    to NULL.
    
    Also fixes:
    - ffmpeg_unittests change threading to mirror ffmpeg_video_decoder. There's
    an issue where threading causes the last frames of a no-audio video to be
    clipped. It existed before this ffmpeg roll, but because threading was
    disabled by default in ffmpeg, we never noticed it.
    
    - ffmpeg_demuxer_tests: GetBitrate_UnsetInContainer_NoFileSize now passes.
    
    - New ffmpeg_unittests passes:
    sync0_ogv/FFmpegTest.Seek_Video/0, where GetParam() = "sync0.ogv"
    sync1_ogv/FFmpegTest.Seek_Video/0, where GetParam() = "sync1.ogv"
    sync2_ogv/FFmpegTest.Seek_Video/0, where GetParam() = "sync2.ogv"
    
    FFmpeg fixups here, https://chromiumcodereview.appspot.com/9325049/
    New git repo here: http://git.chromium.org/gitweb/?p=chromium/third_party/ffmpeg.git;a=summary
    Merge+Patches diff: https://chromiumcodereview.appspot.com/9317107
    
    BUG=110776
    TEST=unittests, layouttests, etc. Trybots.
    
    Review URL: https://chromiumcodereview.appspot.com/9317096
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123123 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7a032766439f88961cf02aa8b06e31c865c30692

--- DEPS
+++ DEPS
@@ -41,7 +41,12 @@ vars = {
   "libphonenumber_revision": "425",
   "libvpx_revision": "122996",
   "lss_revision": "9",
-  "ffmpeg_revision": "122092",
+
+  # These two FFmpeg variables must be updated together.  One is used for SVN
+  # checkouts and the other for Git checkouts.
+  "ffmpeg_revision": "123079",
+  "ffmpeg_hash": "8a05b9314610683d837eec3b03306ca7a894bef7",
+
   "sfntly_revision": "118",
   "skia_revision": "3226",
   # Three lines of non-changing comments so that
@@ -252,7 +257,7 @@ deps = {
     Var("libvpx_revision"),
 
   "src/third_party/ffmpeg":
-    "/trunk/deps/third_party/ffmpeg/source@" +
+    "/trunk/deps/third_party/ffmpeg@" +
     Var("ffmpeg_revision"),
 
   "src/third_party/libjingle/source":
@@ -338,9 +343,6 @@ deps_os = {
 
     "src/third_party/lighttpd":
       "/trunk/deps/third_party/lighttpd@33727",
-
-    "src/third_party/ffmpeg/binaries/chromium/win/ia32":
-      "/trunk/deps/third_party/ffmpeg/binaries/win@" + Var("ffmpeg_revision"),
 
     # Chrome Frame related deps
     "src/third_party/xulrunner-sdk":
--- media/tools/media_bench/media_bench.cc
+++ media/tools/media_bench/media_bench.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -228,12 +228,11 @@ int main(int argc, const char** argv) {
 #endif
 
   // Register FFmpeg and attempt to open file.
-  avcodec_init();
   av_log_set_level(verbose_level);
   av_register_all();
   av_register_protocol2(&kFFmpegFileProtocol, sizeof(kFFmpegFileProtocol));
   AVFormatContext* format_context = NULL;
-  // av_open_input_file wants a char*, which can't work with wide paths.
+  // avformat_open_input() wants a char*, which can't work with wide paths.
   // So we assume ASCII on Windows.  On other platforms we can pass the
   // path bytes through verbatim.
 #if defined(OS_WIN)
@@ -241,8 +240,8 @@ int main(int argc, const char** argv) {
 #else
   const std::string& string_path = in_path.value();
 #endif
-  int result = av_open_input_file(&format_context, string_path.c_str(),
-                                  NULL, 0, NULL);
+  int result = avformat_open_input(&format_context, string_path.c_str(),
+                                   NULL, NULL);
   if (result < 0) {
     switch (result) {
       case AVERROR(EINVAL):
@@ -279,7 +278,7 @@ int main(int argc, const char** argv) {
   }
 
   // Parse a little bit of the stream to fill out the format context.
-  if (av_find_stream_info(format_context) < 0) {
+  if (avformat_find_stream_info(format_context, NULL) < 0) {
     std::cerr << "Error: Could not find stream info for "
               << in_path.value() << std::endl;
     return 1;
@@ -341,7 +340,7 @@ int main(int argc, const char** argv) {
   }
   if (error_correction) {
     codec_context->error_concealment = FF_EC_GUESS_MVS | FF_EC_DEBLOCK;
-    codec_context->error_recognition = FF_ER_CAREFUL;
+    codec_context->err_recognition = AV_EF_CAREFUL;
   }
 
   // Initialize threaded decode.
@@ -350,7 +349,7 @@ int main(int argc, const char** argv) {
   }
 
   // Initialize our codec.
-  if (avcodec_open(codec_context, codec) < 0) {
+  if (avcodec_open2(codec_context, codec, NULL) < 0) {
     std::cerr << "Error: Could not open codec "
               << (codec_context->codec ? codec_context->codec->name : "(NULL)")
               << " for " << in_path.value() << std::endl;
@@ -533,7 +532,7 @@ int main(int argc, const char** argv) {
   if (codec_context)
     avcodec_close(codec_context);
   if (format_context)
-    av_close_input_file(format_context);
+    avformat_close_input(&format_context);
 
   // Calculate the sum of times.  Note that some of these may be zero.
   double sum = 0;
