347b8b9d9b97807efa5bfe2ef94e3ca1f4d50d3b
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 18:48:00 +0000

    Mark ExtensionDevToolsBrowserTest.TimelineApi flaky for debugging.
    BUG=69369,52544
    TEST=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/9430059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123071 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b37db0786972fdd10c2977576a5db3c625215ceb

--- chrome/browser/extensions/extension_devtools_browsertests.cc
+++ chrome/browser/extensions/extension_devtools_browsertests.cc
@@ -50,7 +50,8 @@ static ExtensionHost* FindHostWithPath(ExtensionProcessManager* manager,
 }
 
 // Tests for the experimental timeline extensions API.
-IN_PROC_BROWSER_TEST_F(ExtensionDevToolsBrowserTest, TimelineApi) {
+// http://crbug.com/69369  http://crbug.com/52544
+IN_PROC_BROWSER_TEST_F(ExtensionDevToolsBrowserTest, FLAKY_TimelineApi) {
   ASSERT_TRUE(LoadExtension(
       test_data_dir_.AppendASCII("devtools").AppendASCII("timeline_api")));
 
