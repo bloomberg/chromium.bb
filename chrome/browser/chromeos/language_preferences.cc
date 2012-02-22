6948758e679ebd49254709cdddcddfb46fd46acf
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 15:59:54 +0000

    Revert "Change the initial keyboard repeat delay from 500ms to 750ms."
    This reverts commit c048ac37837c99166d86a2e7f1128b918c8391f4 [formerly 0dbccc984bc1625556e5a85ad0ce8dc77e7afab2].
    
    The change is no longer necessary since it turned out that the duplicated character issue (see crosbug.com/p/6372) was not triggered by the repeat delay setting.
    
    BUG=chromium-os:26716
    TEST=None
    
    
    Review URL: http://codereview.chromium.org/9372100
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123037 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 954bed0c1c347073fe65b6a8cbf4374fc87ae6b5

--- chrome/browser/chromeos/language_preferences.cc
+++ chrome/browser/chromeos/language_preferences.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -390,7 +390,7 @@ COMPILE_ASSERT(kNumMozcIntegerPrefs == arraysize(kMozcIntegerPrefs),
 // ---------------------------------------------------------------------------
 // For keyboard stuff
 // ---------------------------------------------------------------------------
-const int kXkbAutoRepeatDelayInMs = 750;
+const int kXkbAutoRepeatDelayInMs = 500;
 const int kXkbAutoRepeatIntervalInMs = 50;
 const char kPreferredKeyboardLayout[] = "PreferredKeyboardLayout";
 
