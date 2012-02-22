8198bddc4112bfaf48bd4ff2f7098898a054cfb1
dennisjeffrey@chromium.org <dennisjeffrey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-22 19:40:39 +0000

    Flush stdout when pyauto perf tests print perf results on non-ChromeOS platforms.
    When the tests in perf.py run through buildbot on non-ChromeOS platforms such
    as Windows, sometimes the specially-formatted perf result output is corrupted
    by interleaved output due to buffering.  This problem was previously fixed
    on ChromeOS:
    
    http://codereview.chromium.org/8772022/
    
    The current CL makes the same fix for non-ChromeOS platforms.
    
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/9428038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123081 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5cb809e3f196356ec7cc059e6e786ced8b0991a6

--- chrome/test/pyautolib/pyauto_utils.py
+++ chrome/test/pyautolib/pyauto_utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -158,3 +158,4 @@ def PrintPerfResult(graph_name, series_name, data_point, units,
   print '%sRESULT %s: %s= %s %s' % (
       waterfall_indicator, graph_name, series_name,
       str(data_point).replace(' ', ''), units)
+  sys.stdout.flush()
