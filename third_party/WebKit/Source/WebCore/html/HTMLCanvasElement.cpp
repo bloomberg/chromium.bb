bloomberg/chromium.bb

commit 59ab69abeeaaf82d533b8753cd18dcff572d3a1e

Remove GraphicsContext3D::paintsIntoCanvasBuffer and unify WebGL and canvas 2d logic https://bugs.webkit.org/show_bug.cgi?id=79317
Reviewed by Kenneth Russell.

Source/WebCore:

HTMLCanvasElement::paint needs to know whether to attempt to paint the canvas buffer for its context. In the
case of an accelerated canvas or WebGL context, unless we are printing it is typically the composited layer's
responsibility to present the contents and the element itself is only responsible for backgrounds, borders and
the like.  This removes a rather indirect path that WebGL content used to take to accomplish this and unifies
the logic with CanvasRenderingContext2D's, which has the exact same needs.

Covered by existing canvas layout tests.

* html/HTMLCanvasElement.cpp:
(WebCore::HTMLCanvasElement::paintsIntoCanvasBuffer):
(WebCore):
(WebCore::HTMLCanvasElement::paint):
* html/HTMLCanvasElement.h:
(HTMLCanvasElement):
* html/canvas/CanvasRenderingContext.h:
* html/canvas/CanvasRenderingContext2D.cpp:
* html/canvas/CanvasRenderingContext2D.h:
* html/canvas/WebGLRenderingContext.cpp:
(WebCore):
* html/canvas/WebGLRenderingContext.h:
* platform/graphics/GraphicsContext3D.h:

Source/WebKit/chromium:

Remove paintsIntoCanvasBuffer() implementations.

* src/GraphicsContext3DChromium.cpp:
(WebCore):
* src/GraphicsContext3DPrivate.h:

git-svn-id: svn://svn.chromium.org/blink/trunk@108597 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 444d662f0dfb16fa731a2e820ec74b3459816469

third_party/WebKit/Source/WebCore/html/HTMLCanvasElement.cpp
@@ -45,6 +45,7 @@
 #include "MIMETypeRegistry.h"
 #include "Page.h"
 #include "RenderHTMLCanvas.h"
+#include "RenderLayer.h"
 #include "Settings.h"
 #include <math.h>
 #include <stdio.h>
@@ -269,6 +270,20 @@ void HTMLCanvasElement::reset()
         (*it)->canvasResized(this);
 }
 
+bool HTMLCanvasElement::paintsIntoCanvasBuffer() const
+{
+    ASSERT(m_context);
+#if USE(ACCELERATED_COMPOSITING)
+    if (!m_context->isAccelerated())
+        return true;
+
+    if (renderBox() && renderBox()->hasLayer() && renderBox()->layer()->hasAcceleratedCompositing())
+        return false;
+#endif
+    return true;
+}
+
+
 void HTMLCanvasElement::paint(GraphicsContext* context, const LayoutRect& r, bool useLowQualityScale)
 {
     // Clear the dirty rect
@@ -278,7 +293,7 @@ void HTMLCanvasElement::paint(GraphicsContext* context, const LayoutRect& r, boo
         return;
     
     if (m_context) {
-        if (!m_context->paintsIntoCanvasBuffer() && !document()->printing())
+        if (!paintsIntoCanvasBuffer() && !document()->printing())
             return;
         m_context->paintRenderingResultsToCanvas();
     }

third_party/WebKit/Source/WebCore/html/HTMLCanvasElement.h
@@ -148,6 +148,8 @@ private:
 
     bool shouldDefer() const;
 
+    bool paintsIntoCanvasBuffer() const;
+
     HashSet<CanvasObserver*> m_observers;
 
     IntSize m_size;

third_party/WebKit/Source/WebCore/html/canvas/CanvasRenderingContext.h
@@ -55,7 +55,6 @@ public:
     virtual bool isAccelerated() const { return false; }
 
     virtual void paintRenderingResultsToCanvas() {}
-    virtual bool paintsIntoCanvasBuffer() const { return true; }
 
 #if USE(ACCELERATED_COMPOSITING)
     virtual PlatformLayer* platformLayer() const { return 0; }

third_party/WebKit/Source/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -155,20 +155,6 @@ bool CanvasRenderingContext2D::isAccelerated() const
 #endif
 }
 
-bool CanvasRenderingContext2D::paintsIntoCanvasBuffer() const
-{
-#if ENABLE(ACCELERATED_2D_CANVAS) && USE(ACCELERATED_COMPOSITING)
-    if (!isAccelerated())
-        return true;
-
-    RenderBox* renderBox = canvas()->renderBox();
-    if (renderBox && renderBox->hasLayer() && renderBox->layer()->hasAcceleratedCompositing())
-        return false;
-#endif
-    return true;
-}
-
-
 void CanvasRenderingContext2D::reset()
 {
     unwindStateStack();

third_party/WebKit/Source/WebCore/html/canvas/CanvasRenderingContext2D.h
@@ -67,7 +67,6 @@ public:
 
     virtual bool is2d() const { return true; }
     virtual bool isAccelerated() const;
-    virtual bool paintsIntoCanvasBuffer() const;
 
     CanvasStyle* strokeStyle() const;
     void setStrokeStyle(PassRefPtr<CanvasStyle>);

third_party/WebKit/Source/WebCore/html/canvas/WebGLRenderingContext.cpp
@@ -719,11 +719,6 @@ PassRefPtr<ImageData> WebGLRenderingContext::paintRenderingResultsToImageData()
     return imageData;
 }
 
-bool WebGLRenderingContext::paintsIntoCanvasBuffer() const
-{
-    return m_context->paintsIntoCanvasBuffer();
-}
-
 void WebGLRenderingContext::reshape(int width, int height)
 {
     // This is an approximation because at WebGLRenderingContext level we don't

third_party/WebKit/Source/WebCore/html/canvas/WebGLRenderingContext.h
@@ -77,7 +77,6 @@ public:
 
     virtual bool is3d() const { return true; }
     virtual bool isAccelerated() const { return true; }
-    virtual bool paintsIntoCanvasBuffer() const;
 
     int drawingBufferWidth() const;
     int drawingBufferHeight() const;

third_party/WebKit/Source/WebCore/platform/graphics/GraphicsContext3D.h
@@ -814,18 +814,6 @@ public:
     PassRefPtr<ImageData> paintRenderingResultsToImageData(DrawingBuffer*);
     bool paintCompositedResultsToCanvas(CanvasRenderingContext*);
 
-#if PLATFORM(QT)
-    bool paintsIntoCanvasBuffer() const { return true; }
-#elif PLATFORM(CHROMIUM)
-    bool paintsIntoCanvasBuffer() const;
-#elif PLATFORM(GTK)
-    bool paintsIntoCanvasBuffer() const { return true; }
-#elif PLATFORM(EFL)
-    bool paintsIntoCanvasBuffer() const { return true; }
-#else
-    bool paintsIntoCanvasBuffer() const { return false; }
-#endif
-
     // Support for buffer creation and deletion
     Platform3DObject createBuffer();
     Platform3DObject createFramebuffer();

commit 0f1709c0bb2ed66f84a7ad0a045ffdac2ca1aa0e

Fix for canvas breakage caused by r108597 from the following: https://bugs.webkit.org/show_bug.cgi?id=79317
Reviewed by Oliver Hunt.

* html/HTMLCanvasElement.cpp:
(WebCore::HTMLCanvasElement::paintsIntoCanvasBuffer):


git-svn-id: svn://svn.chromium.org/blink/trunk@108657 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 9cb0de9f0ddaa404cb755cfe5348a40d287079ca

third_party/WebKit/Source/WebCore/html/HTMLCanvasElement.cpp
@@ -273,6 +273,10 @@ void HTMLCanvasElement::reset()
 bool HTMLCanvasElement::paintsIntoCanvasBuffer() const
 {
     ASSERT(m_context);
+#if USE(IOSURFACE_CANVAS_BACKING_STORE)
+    return true;
+#endif
+
 #if USE(ACCELERATED_COMPOSITING)
     if (!m_context->isAccelerated())
         return true;