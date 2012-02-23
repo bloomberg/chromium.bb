35ad4e4ac06cd46905c31f77405506c8bdbccfcd
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 01:35:57 +0000

    Revert 122849 - Update state on creating NativeComboboxWrapper.
    Replace inconsistent and incomplete state initialization code with
    explicit Update calls to the wrapper after initiliazation. This fixes
    the issue with views comboboxes not being initialized correctly in case
    the state was set up before the combobox was added to the view
    hierarchy.
    
    BUG=chromium-os:26633
    TEST=EAP method dropdown is initialized correctly for policy-pushed networks on Chrome OS.
    
    
    Review URL: http://codereview.chromium.org/9395079
    
    TBR=mnissler@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9455001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123170 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 729897a909bf672aeaaad7fdf5fad883542653bd

--- ui/views/controls/combobox/native_combobox_gtk.cc
+++ ui/views/controls/combobox/native_combobox_gtk.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -144,6 +144,14 @@ void NativeComboboxGtk::CreateNativeControl() {
   }
 
   NativeControlCreated(widget);
+}
+
+void NativeComboboxGtk::NativeControlCreated(GtkWidget* native_control) {
+  NativeControlGtk::NativeControlCreated(native_control);
+  // Set the initial state of the combobox.
+  UpdateFromModel();
+  UpdateEnabled();
+  UpdateSelectedItem();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
