ddaedbab3d03d64eefb0ab5684a2c49bccf2d48a
benjhayden@chromium.org <benjhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-23 23:15:16 +0000

    Increase maximum value for Download.HistorySize UMA.
    12% of samples are greater than the current maximum value of 1024.
    
    Review URL: http://codereview.chromium.org/9432039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123364 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3cb15d956523bba041fdaae1e1c5cdc94ec5cb3b

--- content/browser/download/download_stats.cc
+++ content/browser/download/download_stats.cc
@@ -280,11 +280,11 @@ void RecordOpen(const base::Time& end, bool first) {
 }
 
 void RecordHistorySize(int size) {
-  UMA_HISTOGRAM_CUSTOM_COUNTS("Download.HistorySize",
+  UMA_HISTOGRAM_CUSTOM_COUNTS("Download.HistorySize2",
                               size,
                               0/*min*/,
-                              (1 << 10)/*max*/,
-                              32/*num_buckets*/);
+                              (1 << 23)/*max*/,
+                              (1 << 7)/*num_buckets*/);
 }
 
 void RecordClearAllSize(int size) {
