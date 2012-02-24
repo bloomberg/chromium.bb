f5f5632c6b087425db9347aed28370f272d75cb4
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-24 23:09:40 +0000

    ChildThread::current()->Send() should DCHECK that it's only used on the main thread
    BUG=115657
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9465009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123577 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4de895b129163ad0ab036d8f0a4d248d0aedc922

--- content/common/child_thread.cc
+++ content/common/child_thread.cc
@@ -90,6 +90,7 @@ void ChildThread::OnChannelError() {
 }
 
 bool ChildThread::Send(IPC::Message* msg) {
+  DCHECK(MessageLoop::current() == message_loop());
   if (!channel_.get()) {
     delete msg;
     return false;
