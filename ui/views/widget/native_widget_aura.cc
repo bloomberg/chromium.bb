f578a905858848c3bb0ce0e171915f67b8cf76ce
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-24 23:53:23 +0000

    Remove a bunch more uses of RootWindow::GetInstance()
    http://crbug.com/112131
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9460025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123592 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 36ae6a190c547974c6594de43cfd9e38c669af18

--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -106,10 +106,10 @@ void SetRestoreBounds(aura::Window* window, const gfx::Rect& bounds) {
 class NativeWidgetAura::ActiveWindowObserver : public aura::WindowObserver {
  public:
   explicit ActiveWindowObserver(NativeWidgetAura* host) : host_(host) {
-    aura::RootWindow::GetInstance()->AddObserver(this);
+    host_->GetNativeView()->GetRootWindow()->AddObserver(this);
   }
   virtual ~ActiveWindowObserver() {
-    aura::RootWindow::GetInstance()->RemoveObserver(this);
+    host_->GetNativeView()->GetRootWindow()->RemoveObserver(this);
   }
 
   // Overridden from aura::WindowObserver:
@@ -320,7 +320,7 @@ bool NativeWidgetAura::HasMouseCapture() const {
 }
 
 InputMethod* NativeWidgetAura::CreateInputMethod() {
-  aura::RootWindow* root_window = aura::RootWindow::GetInstance();
+  aura::RootWindow* root_window = window_->GetRootWindow();
   ui::InputMethod* host =
       root_window->GetProperty(aura::client::kRootWindowInputMethodKey);
   InputMethod* input_method = new InputMethodBridge(this, host);
@@ -587,7 +587,7 @@ void NativeWidgetAura::SchedulePaintInRect(const gfx::Rect& rect) {
 
 void NativeWidgetAura::SetCursor(gfx::NativeCursor cursor) {
   cursor_ = cursor;
-  aura::RootWindow::GetInstance()->SetCursor(cursor);
+  window_->GetRootWindow()->SetCursor(cursor);
 }
 
 void NativeWidgetAura::ClearNativeFocus() {
