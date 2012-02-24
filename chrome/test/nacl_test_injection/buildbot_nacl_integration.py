6377efedacf6dc2f002ccd19dcf2e2f0b6498f09
ncbray@chromium.org <ncbray@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-24 23:14:42 +0000

    Revert "Disable NaCl glibc tests on Mac due to flake." (revision 123254)
    The flake should have been resolved by revision 123540.
    
    BUG= http://code.google.com/p/chromium/issues/detail?id=115561
    TEST= nacl_integration
    
    
    Review URL: http://codereview.chromium.org/9467015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123580 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e49221987f30d61b7b696de183a6a416f502ffbf

--- chrome/test/nacl_test_injection/buildbot_nacl_integration.py
+++ chrome/test/nacl_test_injection/buildbot_nacl_integration.py
@@ -90,12 +90,7 @@ def Main(args):
          '--build-dir=',
          '--',
          nacl_integration_script,
-         '--disable_tests=%s' % ','.join(tests_to_disable)]
-  # TODO(ncbray) reenable glibc on Mac
-  # http://code.google.com/p/chromium/issues/detail?id=115325
-  if sys.platform == 'darwin':
-    cmd.append('--disable_glibc')
-  cmd += args
+         '--disable_tests=%s' % ','.join(tests_to_disable)] + args
   sys.stdout.write('Running %s\n' % ' '.join(cmd))
   sys.stdout.flush()
   return subprocess.call(cmd)
