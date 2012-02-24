4d2da2d6d7cb6ff2b80ce423e2fe643770beb9ed
Scott Zawalski <scottz@google.com>
2012-02-24 09:45:31 -0500

    Modify autotest-experimental cbuildbot profile.
    Add the hw_tests back in to the autotest-experimental cbuildbot_config.
    Only specify bvt as the suite to run.
    Set chrome_test and unittests to false.
    
    TEST=Unittests
    BUG=None
    
    Change-Id: I6c8c950b99fb2ba93db4e031e7b31b269c37ca51
    Reviewed-on: https://gerrit.chromium.org/gerrit/16716
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Commit-Ready: Scott Zawalski <scottz@chromium.org>
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Tested-by: Scott Zawalski <scottz@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -785,11 +785,16 @@ _release.add_config('autotest-experimental',
   boards=['x86-alex'],
   prebuilts=False,
   push_image=False,
+  chrome_tests=False,
+  unittests=False,
 
   # Make this experimental bot build/test incrementally.
   usepkg_setup_board=True,
   usepkg_build_packages=True,
   chroot_replace=False,
+
+  platform='netbook_ALEX',
+  hw_tests=['bvt']
 )
 
 _arm_release = _release.derive(arm)
