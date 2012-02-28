f9f94c2ed1adc508a394d444f3b55601bab7b8dc
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-28 15:58:23 +0000

    chromeos: Change DrawRectWithBorder() to take gfx::Size instead of a pair of ints.
    BUG=100898
    R=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9466038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@123981 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ae2c06f73ff881a6889b693b0a4b1ea7ab58e6ed

--- chrome/browser/chromeos/login/rounded_rect_painter.cc
+++ chrome/browser/chromeos/login/rounded_rect_painter.cc
@@ -9,6 +9,7 @@
 #include "third_party/skia/include/effects/SkBlurMaskFilter.h"
 #include "third_party/skia/include/effects/SkGradientShader.h"
 #include "ui/gfx/canvas_skia.h"
+#include "ui/gfx/skia_util.h"
 #include "ui/views/border.h"
 #include "ui/views/painter.h"
 
@@ -19,34 +20,26 @@ namespace {
 const SkColor kScreenTopColor = SkColorSetRGB(250, 251, 251);
 const SkColor kScreenBottomColor = SkColorSetRGB(204, 209, 212);
 const SkColor kScreenShadowColor = SkColorSetARGB(64, 34, 54, 115);
-const SkColor kShadowStrokeColor = 0;
 const int kScreenShadow = 10;
 
-static void DrawRoundedRect(
-      gfx::Canvas* canvas,
-      int x, int y,
-      int w, int h,
-      int corner_radius,
-      SkColor top_color, SkColor bottom_color,
-      SkColor stroke_color) {
-  SkRect rect;
-  rect.set(
-      SkIntToScalar(x), SkIntToScalar(y),
-      SkIntToScalar(x + w), SkIntToScalar(y + h));
+void DrawRoundedRect(gfx::Canvas* canvas,
+                     const SkRect& rect,
+                     int corner_radius,
+                     SkColor top_color,
+                     SkColor bottom_color) {
   SkPath path;
-  path.addRoundRect(
-      rect, SkIntToScalar(corner_radius), SkIntToScalar(corner_radius));
+  path.addRoundRect(rect,
+                    SkIntToScalar(corner_radius), SkIntToScalar(corner_radius));
   SkPaint paint;
   paint.setStyle(SkPaint::kFill_Style);
   paint.setFlags(SkPaint::kAntiAlias_Flag);
   if (top_color != bottom_color) {
     SkPoint p[2];
-    p[0].set(SkIntToScalar(x), SkIntToScalar(y));
-    p[1].set(SkIntToScalar(x), SkIntToScalar(y + h));
+    p[0].set(SkIntToScalar(rect.left()), SkIntToScalar(rect.top()));
+    p[1].set(SkIntToScalar(rect.left()), SkIntToScalar(rect.bottom()));
     SkColor colors[2] = { top_color, bottom_color };
-    SkShader* s =
-        SkGradientShader::CreateLinear(p, colors, NULL, 2,
-                                       SkShader::kClamp_TileMode, NULL);
+    SkShader* s = SkGradientShader::CreateLinear(p, colors, NULL, 2,
+        SkShader::kClamp_TileMode, NULL);
     paint.setShader(s);
     // Need to unref shader, otherwise never deleted.
     s->unref();
@@ -54,32 +47,13 @@ static void DrawRoundedRect(
     paint.setColor(top_color);
   }
   canvas->GetSkCanvas()->drawPath(path, paint);
-
-  if (stroke_color != 0) {
-    // Expand rect by 0.5px so resulting stroke will take the whole pixel.
-    rect.set(
-        SkIntToScalar(x) - SK_ScalarHalf,
-        SkIntToScalar(y) - SK_ScalarHalf,
-        SkIntToScalar(x + w) + SK_ScalarHalf,
-        SkIntToScalar(y + h) + SK_ScalarHalf);
-    paint.setShader(NULL);
-    paint.setStyle(SkPaint::kStroke_Style);
-    paint.setStrokeWidth(SkIntToScalar(SK_Scalar1));
-    paint.setColor(stroke_color);
-    canvas->GetSkCanvas()->drawRoundRect(
-      rect,
-      SkIntToScalar(corner_radius), SkIntToScalar(corner_radius),
-      paint);
-  }
 }
 
-static void DrawRoundedRectShadow(
-    gfx::Canvas* canvas,
-    int x, int y,
-    int w, int h,
-    int corner_radius,
-    int shadow,
-    SkColor color) {
+void DrawRoundedRectShadow(gfx::Canvas* canvas,
+                           const SkRect& rect,
+                           int corner_radius,
+                           int shadow,
+                           SkColor color) {
   SkPaint paint;
   paint.setFlags(SkPaint::kAntiAlias_Flag);
   paint.setStyle(SkPaint::kFill_Style);
@@ -87,52 +61,33 @@ static void DrawRoundedRectShadow(
   SkMaskFilter* filter = SkBlurMaskFilter::Create(
       shadow / 2, SkBlurMaskFilter::kNormal_BlurStyle);
   paint.setMaskFilter(filter)->unref();
-  SkRect rect;
-  rect.set(
-      SkIntToScalar(x + shadow / 2), SkIntToScalar(y + shadow / 2),
-      SkIntToScalar(x + w - shadow / 2), SkIntToScalar(y + h - shadow / 2));
+  SkRect inset_rect(rect);
+  inset_rect.inset(SkIntToScalar(shadow / 2), SkIntToScalar(shadow / 2));
   canvas->GetSkCanvas()->drawRoundRect(
-      rect,
+      inset_rect,
       SkIntToScalar(corner_radius), SkIntToScalar(corner_radius),
       paint);
   paint.setMaskFilter(NULL);
 }
 
-static void DrawRectWithBorder(int w,
-                               int h,
-                               const BorderDefinition* const border,
-                               gfx::Canvas* canvas) {
-  int padding = border->padding;
-  SkColor padding_color = border->padding_color;
-  int shadow = border->shadow;
-  SkColor shadow_color = border->shadow_color;
-  int corner_radius = border->corner_radius;
-  SkColor top_color = border->top_color;
-  SkColor bottom_color = border->bottom_color;
-  if (padding > 0) {
+void DrawRectWithBorder(gfx::Canvas* canvas,
+                        const gfx::Size& size,
+                        const BorderDefinition* const border) {
+  SkRect rect(gfx::RectToSkRect(gfx::Rect(size)));
+  if (border->padding > 0) {
     SkPaint paint;
-    paint.setColor(padding_color);
-    canvas->GetSkCanvas()->drawRectCoords(
-        SkIntToScalar(0), SkIntToScalar(0), SkIntToScalar(w), SkIntToScalar(h),
-        paint);
+    paint.setColor(border->padding_color);
+    canvas->GetSkCanvas()->drawRect(rect, paint);
+    rect.inset(SkIntToScalar(border->padding), SkIntToScalar(border->padding));
   }
   if (border->shadow > 0) {
-    DrawRoundedRectShadow(
-        canvas,
-        padding, padding,
-        w - 2 * padding, h - 2 * padding,
-        corner_radius,
-        shadow, shadow_color);
+    DrawRoundedRectShadow(canvas, rect, border->corner_radius, border->shadow,
+                          border->shadow_color);
+    rect.inset(SkIntToScalar(border->shadow), SkIntToScalar(border->shadow));
+    rect.offset(0, SkIntToScalar(-border->shadow / 3));
   }
-  DrawRoundedRect(
-        canvas,
-        padding + shadow,
-        padding + shadow - shadow / 3,
-        w - 2 * padding - 2 * shadow,
-        h - 2 * padding - 2 * shadow,
-        corner_radius,
-        top_color, bottom_color,
-        shadow ? kShadowStrokeColor : 0);
+  DrawRoundedRect(canvas, rect, border->corner_radius, border->top_color,
+                  border->bottom_color);
 }
 
 // This Painter can be used to draw a background consistent cross all login
@@ -145,7 +100,7 @@ class RoundedRectPainter : public views::Painter {
 
   // Overridden from views::Painter:
   virtual void Paint(gfx::Canvas* canvas, const gfx::Size& size) OVERRIDE {
-    DrawRectWithBorder(size.width(), size.height(), border_, canvas);
+    DrawRectWithBorder(canvas, size, border_);
   }
 
  private:
