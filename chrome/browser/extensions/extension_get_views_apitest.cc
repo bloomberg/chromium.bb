f1d7c1877ed7306747de2309f01024654a9da4c7
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-02-29 21:06:36 +0000

    Revert 124235 - Re-enable tests that were broken due to WebKit rename of DOMWindow to Window
    The extension code was fixed in r123884
    
    This is a revert of r123590.
    
    BUG=None
    TEST=ExtensionApiTest.GetViews:ExtensionApiTest.TabRelativeURLs:ExtensionApiTest.TabGetCurrent:ExtensionApiTest.TabReload:ExtensionApiTest.GetViewsOfCreatedPopup:ExtensionApiTest.GetViewsOfCreatedWindow:ExtensionApiTest.WebSocket:ExtensionApiTest.WindowOpener:ExtensionApiTest.MAYBE_Infobars
    
    
    Review URL: http://codereview.chromium.org/9517002
    
    TBR=arv@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9538018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124251 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 24c6bcfc207917c542fafba61dc8ed5dda85d86c

--- chrome/browser/extensions/extension_get_views_apitest.cc
+++ chrome/browser/extensions/extension_get_views_apitest.cc
@@ -6,7 +6,7 @@
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_switches.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, GetViews) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_GetViews) {
   // TODO(finnur): Remove once infobars are no longer experimental (bug 39511).
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
