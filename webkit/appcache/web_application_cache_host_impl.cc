dfd94a68106d16bdd5d2dd5bfc93b7a2b8a5852a
mmaxfield@google.com <mmaxfield@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 01:52:53 +0000

    DCHECK sets a variable, instead of comparing
    Review URL: http://codereview.chromium.org/9534003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124327 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 93e9303e8b6655df475aed1dd969ea0a5e712810

--- webkit/appcache/web_application_cache_host_impl.cc
+++ webkit/appcache/web_application_cache_host_impl.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -226,7 +226,7 @@ bool WebApplicationCacheHostImpl::selectCacheWithManifest(
     return true;
   }
 
-  DCHECK(is_new_master_entry_ = NO);
+  DCHECK_EQ(NO, is_new_master_entry_);
 
   // 6.9.6 The application cache selection algorithm
   // Check for 'foreign' entries.
