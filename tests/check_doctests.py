66cfe50c32af02920913bfac89a843f27582fe3f
Christian Egli <christian.egli@alumni.ethz.ch>
2012-03-01 14:50:42 +0000

    Add python based doctests.
    Check for Python in configure and if there is a Python interpreter
    enable some Python based doctests.

---
+++ tests/check_doctests.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import doctest
+import glob
+import sys
+
+exit_value = 0
+for test in glob.iglob('doctests/*_test.txt'):
+    failure_count, ignore = doctest.testfile(test, report=True)
+    if failure_count > 0: 
+        exit_value = 1
+
+sys.exit(exit_value)
