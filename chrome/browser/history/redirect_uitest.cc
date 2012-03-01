31e25db35d4bfdf6c91f69457fa7360a2b7b9fe2
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-01 00:52:48 +0000

    fix RedirectTest.ClientCancelled
    BUG=113958,53091,104396,45162
    TEST=RedirectTest.ClientCancelled
    
    Review URL: https://chromiumcodereview.appspot.com/9419018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124314 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 05632b7b83dcccbd536d1506f0b115ac19ad12e9

--- chrome/browser/history/redirect_uitest.cc
+++ chrome/browser/history/redirect_uitest.cc
@@ -133,13 +133,7 @@ TEST_F(RedirectTest, DISABLED_ClientEmptyReferer) {
 
 // Tests to make sure a location change when a pending redirect exists isn't
 // flagged as a redirect.
-// SimulateOSClick is broken on Mac: http://crbug.com/45162
-// SimulateOSClick is broken on Linux: http://crbug.com/113958
-// SimulateOSClick is broken on Windows: http://crbug.com/53091
-// Probably never worked on Aura.
-// When it fails, it takes 45 seconds to fail, slowing down all the test bots.
-// Needs to be rewritten as an interactive_test. http://crbug.com/115673
-TEST_F(RedirectTest, DISABLED_ClientCancelled) {
+TEST_F(RedirectTest, ClientCancelled) {
   FilePath first_path(test_data_directory_);
   first_path = first_path.AppendASCII("cancelled_redirect_test.html");
   ASSERT_TRUE(file_util::AbsolutePath(&first_path));
