5e5a77207a31d1d607735c925a85387e54d837eb
Brian Harring <ferringb@chromium.org>
2012-03-01 10:38:46 -0800

    Fix braindead interpolation in I6fa35ce98ce
    The wordwrapping that was done lead to basically recursive interpolation.
    
    BUG=None
    TEST=cbuildbot x86-generic-full # look at the bash process
    
    Change-Id: Iac82ca1dee9de1b5f2cfabc28501c52746eb4a20
    Reviewed-on: https://gerrit.chromium.org/gerrit/17151
    Reviewed-by: Brian Harring <ferringb@chromium.org>
    Tested-by: Brian Harring <ferringb@chromium.org>

--- lib/sudo.py
+++ lib/sudo.py
@@ -91,8 +91,8 @@ class SudoKeepAlive(cros_build_lib.MasterPidContextManager):
     repeat_interval = self._repeat_interval * 60
     cmd = 'sudo -n true && sudo -n true < /dev/null > /dev/null 2>&1'
     # Anything other than a timeout results in us shutting down.
-    cmd = 'while :; do read -t %i; [ $? -le 128 ] && exit; %s; done'
-    cmd %= (repeat_interval, cmd)
+    cmd = ('while :; do read -t %i; [ $? -le 128 ] && exit; %s; done' %
+           (repeat_interval, cmd))
     self._proc = subprocess.Popen(['bash', '-c', cmd], shell=False,
                                   close_fds=True,
                                   stdin=subprocess.PIPE)
