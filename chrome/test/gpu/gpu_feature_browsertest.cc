d3ba427498acca29a1eb0762f328b352ffaf07d4
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 09:24:34 +0000

    Revert 124562 - Add basic threaded compositor test to gpu_feature_browsertest.cc
    BUG=113159,116438
    
    Reverting to unbreak the tree. See http://crbug.com/113159 for details.
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=124346
    
    Review URL: http://codereview.chromium.org/9509001
    
    TBR=jbates@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9581006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124621 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 53f4bfbb703ad41e43ae320ed35f2448cd7526f2

--- chrome/test/gpu/gpu_feature_browsertest.cc
+++ chrome/test/gpu/gpu_feature_browsertest.cc
@@ -323,24 +323,5 @@ IN_PROC_BROWSER_TEST_F(GpuFeatureTest, CanOpenPopupAndRenderWith2DCanvas) {
   RunTest(url, "\"SUCCESS\"", false);
 }
 
-class ThreadedCompositorTest : public GpuFeatureTest {
- public:
-  virtual void SetUpCommandLine(CommandLine* command_line) {
-    GpuFeatureTest::SetUpCommandLine(command_line);
-    command_line->AppendSwitch(switches::kEnableThreadedCompositing);
-  }
-};
-
-#if defined(OS_CHROMEOS)
-// crbug.com/116438
-#define MAYBE_ThreadedCompositor DISABLED_ThreadedCompositor
-#else
-#define MAYBE_ThreadedCompositor ThreadedCompositor
-#endif
-IN_PROC_BROWSER_TEST_F(ThreadedCompositorTest, MAYBE_ThreadedCompositor) {
-  const FilePath url(FILE_PATH_LITERAL("feature_compositing.html"));
-  RunTest(url, EXPECT_GPU_SWAP_BUFFERS);
-}
-
 }  // namespace anonymous
 
