9f3fc78d3e1cc8ed448639c7bc7dee7f75df8285
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-02 21:16:55 +0000

    Convert the XXX_sync builders to using directly sync_integration_tests.
    TBR=nsylvain@chromium.org
    BUG=109517
    TEST=sync_integration_tests is properly run.
    NOTRY=true
    
    Review URL: http://codereview.chromium.org/9513008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@124732 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2c92353880e6ab7d7d8517fca03273e5052556c1

--- net/PRESUBMIT.py
+++ net/PRESUBMIT.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -9,7 +9,11 @@ for more details on the presubmit API built into gcl.
 """
 
 def GetPreferredTrySlaves(project, change):
-  slaves = ['win_sync', 'linux_sync', 'mac_sync']
+  slaves = [
+    'linux_rel:sync_integration_tests',
+    'mac_rel:sync_integration_tests',
+    'win_rel:sync_integration_tests',
+  ]
   # Changes that touch NSS files will likely need a corresponding OpenSSL edit.
   # Conveniently, this one glob also matches _openssl.* changes too.
   if any('nss' in f.LocalPath() for f in change.AffectedFiles()):
