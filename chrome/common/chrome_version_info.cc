6ab129ff8b50a4773313997e77d3eef6b3b8e3cd
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-06 14:51:18 +0000

    Fix usage of --log-net-log on chromeos.
    BUG=chromium-os:25696
    TEST=Using --log-net-log on chromeos works and doesn't crash
    
    
    Review URL: http://codereview.chromium.org/9592005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125160 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 731fadcdf3c51d166137e115782ae3e1bb56b544

--- chrome/common/chrome_version_info.cc
+++ chrome/common/chrome_version_info.cc
@@ -13,6 +13,7 @@
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/base/resource/resource_bundle.h"
 
 namespace chrome {
 
@@ -117,7 +118,10 @@ std::string VersionInfo::OSType() const {
 #elif defined(OS_MACOSX)
   return "Mac OS X";
 #elif defined(OS_CHROMEOS)
-  return UTF16ToASCII(l10n_util::GetStringUTF16(IDS_PRODUCT_OS_NAME));
+  if (ui::ResourceBundle::HasSharedInstance())
+    return UTF16ToASCII(l10n_util::GetStringUTF16(IDS_PRODUCT_OS_NAME));
+  else
+    return "Chromium OS";
 #elif defined(OS_LINUX)
   return "Linux";
 #elif defined(OS_FREEBSD)
