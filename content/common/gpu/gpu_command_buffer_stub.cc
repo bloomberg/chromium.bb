0cfcb0264d18559c19f8950e3e66dcbc30f23b90
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-07 03:55:59 +0000

    Make context current before doing PollUnscheduleFences
    BUG=117080
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9546038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125320 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4c5f7f18427ffea952b6737a3a591510df68c2cf

--- content/common/gpu/gpu_command_buffer_stub.cc
+++ content/common/gpu/gpu_command_buffer_stub.cc
@@ -143,6 +143,16 @@ bool GpuCommandBufferStub::HasMoreWork() {
 }
 
 void GpuCommandBufferStub::PollWork() {
+  if (decoder_.get()) {
+    if (!decoder_->MakeCurrent()) {
+      DLOG(ERROR) << "Context lost because MakeCurrent failed.";
+      command_buffer_->SetContextLostReason(decoder_->GetContextLostReason());
+      command_buffer_->SetParseError(gpu::error::kLostContext);
+      if (gfx::GLContext::LosesAllContextsOnContextLost())
+        channel_->LoseAllContexts();
+      return;
+    }
+  }
   if (scheduler_.get())
     scheduler_->PollUnscheduleFences();
 }
