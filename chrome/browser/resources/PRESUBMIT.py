b0081155ccfa415d0b3b09fd6303473ae5560d10
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-10 04:04:31 +0000

    [WebUI] Add uber and extensions to chrome/browser/resources/PRESUBMIT.py
    R=estade@chromium.org
    BUG=113202
    TEST=uber/ and extensions/ give off presubmit errors if not in line with style
    guide.
    NOTRY=true
    
    Review URL: http://codereview.chromium.org/9632010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126002 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6fbbe3c9cf77b3f4792850b59a0be81dd3c0f8fa

--- chrome/browser/resources/PRESUBMIT.py
+++ chrome/browser/resources/PRESUBMIT.py
@@ -38,9 +38,14 @@ def _CommonChecks(input_api, output_api):
     sys.path.insert(0, resources)
     from web_dev_style import css_checker, js_checker
 
-    # TODO(dbeam): Remove this filter eventually when ready.
+    # TODO(dbeam): Remove this directory filter eventually when ready.
+    dirs = (
+        path.join(resources, 'extensions'),
+        path.join(resources, 'ntp4'),
+        path.join(resources, 'options2'),
+        path.join(resources, 'uber'),
+    )
     def file_filter(affected_file):
-      dirs = (path.join(resources, 'ntp4'), path.join(resources, 'options2'))
       f = affected_file.AbsoluteLocalPath()
       return (f.startswith(dirs) and f.endswith(('.css', '.html', '.js')))
 
