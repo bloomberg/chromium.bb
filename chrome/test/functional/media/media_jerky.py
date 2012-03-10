8c8370ccdf36fe959a74835ae53590873420b8a5
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-10 00:28:25 +0000

    Add PyAuto test for Jerky tool.
    Using Jerky tool we can now record Jerkiness metrics for videos on
    Linux.  The test allows us to plot this value over time using the
    Chromium perf Buildbots.
    
    In order to keep the jerkiness metric stable, we discard results from
    invalid runs and take three samples.  With the included video this
    yields results of around 2% jerkiness.
    
    I've hand built the jerky tool binary for Linux with some custom
    fixes at the moment.  Longer term we'll move these sources into
    their own project or somewhere under the Chromium tree.
    
    BUG=110779
    TEST=Ran test.
    
    Review URL: https://chromiumcodereview.appspot.com/9562052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@125957 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c9a7428436a7528c76fd03f4bf324197589b3a58

--- chrome/test/functional/media/media_jerky.py
+++ chrome/test/functional/media/media_jerky.py
@@ -1,123 +1,223 @@
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Simple test for HTML5 media tag to measure playback time.
+"""Jerkiness performance test for video playback.
 
-This PyAuto powered script plays media (video or audio) files using the HTML5
-tag embedded in an HTML file (specified in the GetPlayerHTMLFileName() method)
-for a test that uses a jerky tool that is being developed. The tool will
-measure display Frame-per-second (FPS) (instead of decode FPS, which is
-measured in media_fps.py). The tool requires predefined pattern to show before
-video plays to determine the area to be measured (it is done in
-data/media/html/media_jerky.html).
+Uses jerky tool, (http://go/jerky), to record a jerkiness metric for videos
+sensitive to jerkiness.
 
-The parameters needed to run this test are passed in the form of environment
-variables (such as the number of runs). Media_perf_runner.py is used for
-generating these variables (PyAuto does not support direct parameters).
+Jerkiness is defined as a percentage of the average on screen frame time by the
+formula below.  Where smoothed_frame_time[i] represents a frame's on screen time
+plus amortized measurement gap error (time taken to capture each frame).
+
+sqrt(average((avg_frame_time - smoothed_frame_time[i])^2, i=m..n))
+------------------------------------------------------------------
+                          avg_frame_time
+
+Currently, only the Linux binaries are checked in for this test since we only
+have a Linux performance bot.  The current binary is a custom build with some
+fixes from veganjerky (http://go/veganjerky) for timing, waiting, and stdout
+flushing.
+
+TODO(dalecurtis): Move Jerky tool sources into the Chromium tree.
+
+TODO(dalecurtis): Jerky tool uses a questionable method for determining torn
+frames, determine if it is actually worth recording.
 """
+
+import glob
 import logging
 import os
+import re
 import subprocess
-import time
+import tempfile
 
-from media_test_base import MediaTestBase
-from media_test_env_names import MediaTestEnvNames
 import pyauto_media
+import pyauto
+import pyauto_utils
+
+# HTML test path; relative to src/chrome/test/data.
+_TEST_HTML_PATH = os.path.join('media', 'html', 'media_jerky.html')
+
+# Path under data path for test files.
+_TEST_MEDIA_PATH = os.path.join('pyauto_private', 'media', 'birds')
+
+# Path to Jerky tool executable.
+_JERKY_PATH = os.path.join('pyauto_private', 'media', 'tools', 'jerky')
+
+# Regular expression for extracting jerkiness percentage.  Sample line:
+#   using 1:9 title 'test.log (35.36% jerky, 0 teared frames)' lw 2 with lines
+_JERKY_LOG_REGEX = re.compile(
+    r'\((\d{0,3}\.?\d{0,2})% jerky, (\d+) teared frames\)')
+
+# Regular expression for extracting computed fps.  Sample line:
+# INFO: 33797 us per frame => 29.6 fps.
+_JERKY_LOG_FPS_REGEX = re.compile(r' => (\d+\.\d+) fps')
+
+# Minimum and maximum number of iterations for each test.  Due to timing issues
+# the jerky tool may not always calculate the fps correctly.  When invalid
+# results are detected, the test is rerun up to the maxium # of times set below.
+_JERKY_ITERATIONS_MIN = 3
+_JERKY_ITERATIONS_MAX = 10
+
+# The media files used for testing.  Each entry represents a tuple of (filename,
+# width, height, fps).  The width and height are used to create a calibration
+# pattern for jerky tool.  The fps is used to ensure Jerky tool computed a valid
+# result.
+_TEST_VIDEOS = [('birds540.webm', 960, 540, 29.9)]
 
 
-class MediaJerkyTest(MediaTestBase):
-  """Test class to use the jerky tool."""
-  # A name list comes from the jerky tool.
-  # JERKY_NAME_LIST, JERKY_UNIT_LIST and JERKY_TRACE_LIST correspond to each
-  # other.
-  JERKY_NAME_LIST = ['jerkiness', 'frame drawing rate',
-                     'torn frames', 'skipped frames']
-  JERKY_UNIT_LIST = ['msec', 'count', 'count', 'count']
-  # A trace list is used for legends in graph. One letter is
-  # preferable for trace and can be any one character letter. Here
-  # I use the first letter of each unit except for 'sec'.
-  JERKY_TRACE_LIST = ['t', 'c', 'c', 'c']
+def GetTempFilename():
+  """Returns an absolute path to an empty temp file."""
+  f, path = tempfile.mkstemp(prefix='jerky_tmp')
+  os.close(f)
+  return path
 
-  jerky_tool_output_filename_base = ''
 
-  def testHTML5MediaTag(self):
-    """Test the HTML5 media tag."""
-    MediaTestBase.ExecuteTest(self)
+class MediaJerkyPerfTest(pyauto.PyUITest):
+  """PyAuto test container.  See file doc string for more information."""
 
-  def PreAllRunsProcess(self):
-    """A method to be executed before all runs."""
-    os.environ[MediaTestEnvNames.JERKY_TEST_ENV_NAME] = 'True'
-    MediaTestBase.PreAllRunsProcess(self)
+  def StartJerkyCapture(self):
+    """Starts jerky tool in capture mode and waits until its ready to capture.
 
-  def GetPlayerHTMLFileName(self):
-    """A method to get the player HTML file name."""
-    return 'media_jerky.html'
+    Returns:
+      A tuple of the jerky process and an absolute path to the capture log.
+    """
+    jerky_log = GetTempFilename()
+    logging.debug('Logging data to %s', jerky_log)
+    process = subprocess.Popen(
+        [os.path.join(self.DataDir(), _JERKY_PATH),
+         'capture', '--log', jerky_log],
+        stdout=subprocess.PIPE)
 
-  def PreEachRunProcess(self, run_counter):
-    """Starts the jerky tool.  Executes before each test run.
+    # Set the jerky tool process to soft-realtime w/ round-robin scheduling.
+    subprocess.check_call(['sudo', 'chrt', '-r', '-p', str(process.pid)])
+
+    # Wait for server to start up.
+    line = True
+    while line:
+      line = process.stdout.readline()
+      if 'Waiting for calibration pattern to disappear' in line:
+        return process, jerky_log
+    self.fail('Failed to launch Jerky tool.')
+
+  def AnalyzeJerkyCapture(self, jerky_log):
+    """Run jerky analyze on the specified log and return various metrics.
+
+    Once analyze has completed, the jerky_log and associated outputs will be
+    removed.
 
     Args:
-      run_counter: counter for each run.
+      jerky_log: Absolute path to the capture log.
+
+    Returns:
+      Tuple of fps, jerkiness, and torn frames.
     """
-    MediaTestBase.PreEachRunProcess(self, run_counter)
+    results_log_base = GetTempFilename()
+    process = subprocess.Popen(
+        [os.path.join(self.DataDir(), _JERKY_PATH),
+         'analyze', '--ref', jerky_log, '--out', results_log_base],
+        stdout=subprocess.PIPE)
 
-    jerky_tool_binary_loc = os.getenv(
-        MediaTestEnvNames.JERKY_TOOL_BINARY_LOCATION_ENV_NAME, '')
-    jerky_tool_output_dir = os.getenv(
-        MediaTestEnvNames.JERKY_TOOL_OUTPUT_DIR_ENV_NAME, '')
-    filename = self.media_filename_nickname + '_' + str(run_counter)
-    self.jerky_tool_output_filename_base = os.path.join(
-        jerky_tool_output_dir, self.media_filename_nickname)
-    jerky_tool_output_filename = (
-        os.path.join(jerky_tool_output_dir, filename))
-    jerky_tool_baseline_dir = os.getenv(
-        MediaTestEnvNames.JERKY_TOOL_BASELINE_DIR_ENV_NAME, '')
-    jerky_tool_baseline_filename = (
-        os.path.join(jerky_tool_baseline_dir, filename))
-    if jerky_tool_binary_loc:
-      # TODO(imasaki@): change the following when jerky tool is implemented.
-      cmd = '%s -o %s -r %s' % (jerky_tool_binary_loc,
-                                jerky_tool_output_filename,
-                                jerky_tool_baseline_filename)
-      proc = subprocess.Popen(cmd, shell=True)
-      proc.communicate()
+    # Wait for process to end w/o risking deadlock.
+    stdout = process.communicate()[0]
+    self.assertEquals(process.returncode, 0)
 
-  def PostAllRunsProcess(self):
-    """Collects the jerky tool results.  Executes after all test runs.
+    # Scrape out the calculated FPS.
+    fps_match = None
+    for line in stdout.splitlines():
+      fps_match = _JERKY_LOG_FPS_REGEX.search(line)
+      if fps_match:
+        break
 
-    Jerky tool writes the output for each run in the following format
-    v1('jerkiness'), v2('frame drawing rate'), v3('torn frames'),
-    v4('skipped frames') to the file specified by the commandline
-    argument.
+    # Open *.error.gnuplot and scrape out jerkiness.
+    jerky_match = None
+    with open('%s.error.gnuplot' % results_log_base) as results:
+      for line in results:
+        jerky_match = _JERKY_LOG_REGEX.search(line)
+        if jerky_match:
+          break
+
+    # Cleanup all the temp and results files jerky spits out.
+    for log in glob.glob('%s*' % results_log_base) + [jerky_log]:
+      os.unlink(log)
+
+    if fps_match and jerky_match:
+      return (float(fps_match.group(1)), float(jerky_match.group(1)),
+              int(jerky_match.group(2)))
+    return None, None, None
+
+  def testMediaJerkyPerformance(self):
+    """Launches Jerky tool and records jerkiness for HTML5 videos.
+
+    For each video, the test starts up jerky tool then plays until the Jerky
+    tool collects enough information.  Next the capture log is analyzed using
+    Jerky's analyze command.  If the computed fps matches the expected fps the
+    jerkiness metric is recorded.
+
+    The test will run up to _JERKY_ITERATIONS_MAX times in an attempt to get at
+    least _JERKY_ITERATIONS_MIN valid values.  The results are recorded under
+    the 'jerkiness' variable for graphing on the bots.
     """
-    # Storing inserted data that is necessary for perf graphs.
-    data_for_perf_graphs = []
-    # Initialize |data_for_perf_graphs|, which is a 2-D array.
-    for i in xrange(len(self.JERKY_NAME_LIST)):
-      data_for_perf_graphs.append([])
-    for i in xrange(self.number_of_runs):
-      jerky_output_filename = (
-          self.jerky_tool_output_filename_base + '_' + str(i))
-      jerky_output_file = open(jerky_output_filename, 'r')
-      # Jerky output data is delimited by commas.
-      jerky_output_data = jerky_output_file.read().split(',')
-      if len(jerky_output_data) != len(self.JERKY_NAME_LIST):
-        logging.warning(
-            ('Jerky output file (%s) only has %d data elements. '
-             'It should have %d elements') % (jerky_output_filename,
-                                              len(jerky_output_data),
-                                              len(self.JERKY_NAME_LIST)))
-        continue
-      for j in xrange(len(jerky_output_data)):
-        data_for_perf_graphs[j].append(jerky_output_data[j])
-      jerky_output_file.close()
-    for i in xrange(len(data_for_perf_graphs)):
-      print 'RESULT %s: %s= %s %s' % (self.JERKY_NAME_LIST[i],
-                                      self.JERKY_TRACE_LIST[i],
-                                      data_for_perf_graphs[i],
-                                      self.JERKY_UNIT_LIST[i])
+    self.NavigateToURL(self.GetFileURLForDataPath(_TEST_HTML_PATH))
+
+    # Xvfb on the bots is restricted to 1024x768 at present.  Ensure we're using
+    # all of the real estate we can.  Jerky tool needs a clear picture of every
+    # frame, so we can't clip the video in any way.
+    self.SetWindowDimensions(0, 0, 1024, 768)
+
+    for name, width, height, expected_fps in _TEST_VIDEOS:
+      jerkiness = []
+      torn_frames = []
+      file_url = self.GetFileURLForDataPath(
+          os.path.join(_TEST_MEDIA_PATH, name))
+
+      # Initialize the calibration area for Jerky tool.
+      self.assertTrue(self.ExecuteJavascript(
+          'initializeTest(%d, %d);' % (width, height)))
+
+      runs_left = _JERKY_ITERATIONS_MIN
+      runs_total = 0
+      while runs_left > 0 and runs_total < _JERKY_ITERATIONS_MAX:
+        runs_total += 1
+        logging.info('Running Jerky perf test #%d for %s.', runs_total, name)
+
+        # Startup Jerky tool in capture mode.
+        jerky_process, jerky_log = self.StartJerkyCapture()
+
+        # Start playback of the test video.
+        self.assertTrue(self.ExecuteJavascript("startTest('%s');" % file_url))
+
+        # Wait for jerky tool to finish if it hasn't already.
+        self.assertTrue(jerky_process.wait() == 0)
+
+        # Stop playback of the test video so the next run can cleanly find the
+        # calibration zone.
+        self.assertTrue(self.ExecuteJavascript('stopTest();'))
+
+        # Analyze the results.
+        jerky_fps, jerky_percent, jerky_torn_frames = self.AnalyzeJerkyCapture(
+            jerky_log)
+        if (jerky_fps is None or jerky_percent is None or
+            jerky_torn_frames is None):
+          logging.error('No metrics recorded for this run.')
+          continue
+
+        # Ensure the results for this run are valid.
+        if jerky_fps != expected_fps:
+          logging.error(
+              'Invalid fps detected (actual: %f, expected: %f, jerkiness: %f). '
+              'Discarding results for this run.', jerky_fps, expected_fps,
+              jerky_percent)
+          continue
+
+        jerkiness.append(jerky_percent)
+        torn_frames.append(jerky_torn_frames)
+        runs_left -= 1
+
+      pyauto_utils.PrintPerfResult('jerkiness', name, jerkiness, '%')
 
 
 if __name__ == '__main__':
