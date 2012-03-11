bbbf7080cfeda12b685a599f5bc8b56c308dc886
David James <davidjames@chromium.org>
2012-03-11 11:06:57 -0700

    Clean up the archive directory on failure.
    Right now, if the archive stage fails, old images aren't ever cleaned
    up. This can lead to the builder running out of disk space. Example:
    http://chromegw/i/chromeos/builders/x86-zgb%20canary/builds/1563
    
    This CL fixes this problem by moving the archive directory cleanup
    into a finally block.
    
    BUG=chromium-os:27634
    TEST=Remote trybot run.
    
    Change-Id: I465a115d686df581ba1ef1a3e459a8f69d347fd4
    Reviewed-on: https://gerrit.chromium.org/gerrit/17801
    Tested-by: David James <davidjames@chromium.org>
    Reviewed-by: Brian Harring <ferringb@chromium.org>

--- buildbot/cbuildbot_stages.py
+++ buildbot/cbuildbot_stages.py
@@ -1226,30 +1226,34 @@ class ArchiveStage(BoardSpecificBuilderStage):
 
         background.RunParallelSteps(steps)
 
-    BuildAndArchiveArtifacts()
+    def PushImage():
+      # Now that all data has been generated, we can upload the final result to
+      # the image server.
+      # TODO: When we support branches fully, the friendly name of the branch
+      # needs to be used with PushImages
+      if not debug and config['push_image']:
+        commands.PushImages(buildroot,
+                            board=board,
+                            branch_name='master',
+                            archive_url=upload_url,
+                            profile=self._options.profile or config['profile'])
 
-    # Now that all data has been generated, we can upload the final result to
-    # the image server.
-    # TODO: When we support branches fully, the friendly name of the branch
-    # needs to be used with PushImages
-    if not debug and config['push_image']:
-      commands.PushImages(buildroot,
-                          board=board,
-                          branch_name='master',
-                          archive_url=upload_url,
-                          profile=self._options.profile or
-                            self._build_config['profile'])
+    def MarkAsLatest():
+      # Update and upload LATEST file.
+      version = self.GetVersion()
+      if version:
+        commands.UpdateLatestFile(self._bot_archive_root, version)
+        commands.UploadArchivedFile(self._bot_archive_root,
+                                    self._GetGSUtilArchiveDir(), 'LATEST',
+                                    debug)
 
-    # Update and upload LATEST file.
-    version = self.GetVersion()
-    if version:
-      commands.UpdateLatestFile(self._bot_archive_root, version)
-
-    commands.UploadArchivedFile(self._bot_archive_root,
-                                self._GetGSUtilArchiveDir(), 'LATEST', debug)
-
-    commands.RemoveOldArchives(self._bot_archive_root,
-                               self._options.max_archive_builds)
+    try:
+      BuildAndArchiveArtifacts()
+      PushImage()
+      MarkAsLatest()
+    finally:
+      commands.RemoveOldArchives(self._bot_archive_root,
+                                 self._options.max_archive_builds)
 
   def _HandleStageException(self, exception):
     # Tell the HWTestStage not to wait for artifacts to be uploaded
