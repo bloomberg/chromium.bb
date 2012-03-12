9df6ee5bded26bff2692ba6e68d60dac4efa7eb9
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-12 21:13:54 +0000

    NaCl: Disable hardware exception test in advance
    This test for exception handling in the browser is being added on the
    NaCl side, but it won't work yet until support is added on the
    Chromium side.  I'm adding a line to disable it so that I can later
    enable the test in the same change that adds exception handling
    support in Chromium.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2602
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9688017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126222 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1aba7969677f375e6deef365cda3d788f084a546

--- chrome/test/nacl_test_injection/buildbot_nacl_integration.py
+++ chrome/test/nacl_test_injection/buildbot_nacl_integration.py
@@ -80,6 +80,10 @@ def Main(args):
   if sys.platform in ('win32', 'cygwin'):
     tests_to_disable.append('run_ppapi_ppp_input_event_browser_test')
 
+  # TODO(mseaborn): Remove this when exception handling works inside
+  # Chromium.  See http://code.google.com/p/nativeclient/issues/detail?id=2602
+  tests_to_disable.append('run_inbrowser_exception_test')
+
   script_dir = os.path.dirname(os.path.abspath(__file__))
   test_dir = os.path.dirname(script_dir)
   chrome_dir = os.path.dirname(test_dir)
