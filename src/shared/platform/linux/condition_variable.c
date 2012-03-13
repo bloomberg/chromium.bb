7749ebfb7cc6e54d23eadb6de9c48fad700709b6
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-03-13 18:53:05 +0000

    Undo hacks for qemu which worked around problems with sync primitives.
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=1987
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2421
    Review URL: https://chromiumcodereview.appspot.com/9693003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8011 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 55e99b597e0497349e2a6849e7af472ac93db590

--- src/shared/platform/linux/condition_variable.c
+++ src/shared/platform/linux/condition_variable.c
@@ -7,7 +7,6 @@
 #include <pthread.h>
 #include <sys/time.h>
 #include "native_client/src/shared/platform/nacl_sync.h"
-#include "native_client/src/include/atomic_ops.h"
 #include "native_client/src/include/nacl_base.h"
 #include "native_client/src/include/nacl_macros.h"
 #include "native_client/src/trusted/service_runtime/include/sys/time.h"
@@ -15,20 +14,12 @@
 static const uint64_t kMicrosecondsPerSecond = 1000 * 1000;
 static const uint64_t kNanosecondsPerMicrosecond = 1000;
 
-#if NACL_COND_BROADCAST_HACK
-#define ATOMIC_INC(_var)  (AtomicIncrement((_var), (Atomic32)1))
-#define ATOMIC_DEC(_var)  (AtomicIncrement((_var), (Atomic32)-1))
-#endif
-
 /* Condition variable C API */
 
 int NaClCondVarCtor(struct NaClCondVar  *cvp) {
   if (0 != pthread_cond_init(&cvp->cv, (pthread_condattr_t *) 0)) {
     return 0;
   }
-#if NACL_COND_BROADCAST_HACK
-  cvp->waiting = 0;
-#endif
   return 1;
 }
 
@@ -41,47 +32,14 @@ NaClSyncStatus NaClCondVarSignal(struct NaClCondVar *cvp) {
   return NACL_SYNC_OK;
 }
 
-#if NACL_COND_BROADCAST_HACK
-/* Strange linking bug:
- * If pthread_cond_broadcast is not referenced in this file, then
- * the function is never linked in, but libstdc++ tries to use it
- * anyway, resulting in an exception being thrown.
- * BUG= http://code.google.com/p/nativeclient/issues/detail?id=1987
- */
-int (*fake_reference_to_pthread_cond_broadcast)(pthread_cond_t*) =
-    pthread_cond_broadcast;
-#endif
-
 NaClSyncStatus NaClCondVarBroadcast(struct NaClCondVar *cvp) {
-#if NACL_COND_BROADCAST_HACK
-  /* There is a race condition where cvp->waiting may be
-   * incremented before a thread actually calls pthread_cond_(timed)wait.
-   * In that case, we'll send an extra signal and it'll go nowhere.
-   * This is not a problem, because broadcasting always
-   * has this race condition. If both the broadcast and
-   * wait are protected by the same mutex, then the waiting counter
-   * will be accurate
-   */
-  uint32_t waiting = cvp->waiting;
-  while (waiting > 0) {
-    pthread_cond_signal(&cvp->cv);
-    waiting--;
-  }
-#else
   pthread_cond_broadcast(&cvp->cv);
-#endif
   return NACL_SYNC_OK;
 }
 
 NaClSyncStatus NaClCondVarWait(struct NaClCondVar *cvp,
                                struct NaClMutex   *mp) {
-#if NACL_COND_BROADCAST_HACK
-  ATOMIC_INC(&cvp->waiting);
-#endif
   pthread_cond_wait(&cvp->cv, &mp->mu);
-#if NACL_COND_BROADCAST_HACK
-  ATOMIC_DEC(&cvp->waiting);
-#endif
   return NACL_SYNC_OK;
 }
 
@@ -108,13 +66,7 @@ NaClSyncStatus NaClCondVarTimedWaitRelative(
   ts.tv_nsec = (wakeup_time_us - ts.tv_sec * kMicrosecondsPerSecond) *
                kNanosecondsPerMicrosecond;
 
-#if NACL_COND_BROADCAST_HACK
-  ATOMIC_INC(&cvp->waiting);
-#endif
   result = pthread_cond_timedwait(&cvp->cv, &mp->mu, &ts);
-#if NACL_COND_BROADCAST_HACK
-  ATOMIC_DEC(&cvp->waiting);
-#endif
   if (0 == result) {
     return NACL_SYNC_OK;
   }
@@ -129,13 +81,7 @@ NaClSyncStatus NaClCondVarTimedWaitAbsolute(
   int result;
   ts.tv_sec = abstime->tv_sec;
   ts.tv_nsec = abstime->tv_nsec;
-#if NACL_COND_BROADCAST_HACK
-  ATOMIC_INC(&cvp->waiting);
-#endif
   result = pthread_cond_timedwait(&cvp->cv, &mp->mu, &ts);
-#if NACL_COND_BROADCAST_HACK
-  ATOMIC_DEC(&cvp->waiting);
-#endif
   if (0 == result) {
     return NACL_SYNC_OK;
   }
