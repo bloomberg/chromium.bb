8bbe6bb093cea99492e5c39d733eb9c3c86b413e
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-13 07:58:52 +0000

    Add initial stub for AutofillExternalDelegate.
    The actual implementation will be upstreamed once we can build/run
    tests.
    
    BUG=117407
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9691004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126351 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0d0c7fd8b845c7bd35ff93ff3556888c40e21688

---
+++ chrome/browser/autofill/autofill_external_delegate_android.cc
@@ -0,0 +1,15 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/autofill/autofill_external_delegate.h"
+
+#include "chrome/browser/autofill/autofill_manager.h"
+#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
+
+AutofillExternalDelegate* AutofillExternalDelegate::Create(
+    TabContentsWrapper* tab_contents,
+    AutofillManager* manager) {
+  NOTIMPLEMENTED() << "TODO(jrg): Upstream AutofillExternalDelegateAndroid";
+  return NULL;
+}
