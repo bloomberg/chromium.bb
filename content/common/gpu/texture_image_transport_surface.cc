55a55d43bbd92aab6c7fa2ff36da354dc4324e30
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-13 03:32:21 +0000

    Aura: sanity check for --enable-partial-swap
    We're getting bug reports from canaries that are hard to repro. This CL will hopefully give us some crash reports if the params from the compositor aren't sane.
    
    TEST=none
    BUG=99527
    
    
    Review URL: http://codereview.chromium.org/9677002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126318 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 012c52ee3753362e29dd49f38e3cfa816d144ae3

--- content/common/gpu/texture_image_transport_surface.cc
+++ content/common/gpu/texture_image_transport_surface.cc
@@ -219,6 +219,13 @@ bool TextureImageTransportSurface::PostSubBuffer(
 
   const gfx::Rect new_damage_rect(x, y, width, height);
   if (surfaces_same_size) {
+    // TODO(backer): Remove temporary sanity checking once we verified that this
+    // is not causing the black rects with --enable-partial-swap.
+    CHECK_GE(x, 0);
+    CHECK_GE(y, 0);
+    CHECK_LE(x + width, expected_size.width());
+    CHECK_LE(y + height, expected_size.height());
+
     std::vector<gfx::Rect> regions_to_copy;
     GetRegionsToCopy(previous_damage_rect_, new_damage_rect, &regions_to_copy);
 
@@ -239,7 +246,9 @@ bool TextureImageTransportSurface::PostSubBuffer(
       }
     }
   } else {
-    DCHECK(new_damage_rect == gfx::Rect(expected_size));
+    // TODO(backer): Remove temporary sanity checking once we verified that this
+    // is not causing the black rects with --enable-partial-swap.
+    CHECK(new_damage_rect == gfx::Rect(expected_size));
   }
 
   glFlush();
