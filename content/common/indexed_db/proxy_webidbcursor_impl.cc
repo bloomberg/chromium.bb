ab73544523415247b42eef3810492fa7da56be37
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-14 21:56:54 +0000

    Revert 126719 - IndexedDB: Fix prefetching; cache was always being bypassed.
    Regression introduced in r119183 when "Null" and "Invalid" were disambiguated.
    
    Tests will require refactoring; tracked as crbug.com/118033
    
    BUG=117920
    
    Review URL: http://codereview.chromium.org/9691021
    
    TBR=jsbell@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9704039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126752 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b8233a139f5ca4cf5ec9cd2e6121c591cba9385f

--- content/common/indexed_db/proxy_webidbcursor_impl.cc
+++ content/common/indexed_db/proxy_webidbcursor_impl.cc
@@ -67,7 +67,7 @@ void RendererWebIDBCursorImpl::continueFunction(const WebIDBKey& key,
   IndexedDBDispatcher* dispatcher =
       IndexedDBDispatcher::ThreadSpecificInstance();
 
-  if (key.type() == WebIDBKey::NullType) {
+  if (key.type() == WebIDBKey::InvalidType) {
     // No key, so this would qualify for a prefetch.
     ++continue_count_;
 
