fd112253e666c8feeb6be8e83f144819ab8d83c3
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-15 19:42:09 +0000

    ui: Export a function that's used by a test.
    BUG=90078
    TEST=none
    TBR=sky
    
    Review URL: http://codereview.chromium.org/9699080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126969 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2abce6ad0fb732f98bd530406758e0b7185f5d96

--- ui/gfx/mac/nsimage_cache.h
+++ ui/gfx/mac/nsimage_cache.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,7 +28,7 @@ namespace gfx {
 UI_EXPORT NSImage* GetCachedImageWithName(NSString* name);
 
 // Clears the image cache.
-void ClearCachedImages(void);
+UI_EXPORT void ClearCachedImages(void);
 
 }  // namespace gfx
 
