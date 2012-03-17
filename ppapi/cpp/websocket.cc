f4d0ea637847da4b23c940b672a333e10dd5a227
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-17 12:14:36 +0000

    Check explicitly for PPB_WebSocket v1.0 in pp::WebSocket wrapper.
    BUG=107398
    
    
    Review URL: http://codereview.chromium.org/9702107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127357 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 236790f7ae982eb6dda025960964a88d4fc538f8

--- ppapi/cpp/websocket.cc
+++ ppapi/cpp/websocket.cc
@@ -16,16 +16,16 @@ namespace pp {
 
 namespace {
 
-template <> const char* interface_name<PPB_WebSocket>() {
-  return PPB_WEBSOCKET_INTERFACE;
+template <> const char* interface_name<PPB_WebSocket_1_0>() {
+  return PPB_WEBSOCKET_INTERFACE_1_0;
 }
 
 }  // namespace
 
 WebSocket::WebSocket(const InstanceHandle& instance) {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return;
-  PassRefFromConstructor(get_interface<PPB_WebSocket>()->Create(
+  PassRefFromConstructor(get_interface<PPB_WebSocket_1_0>()->Create(
     instance.pp_instance()));
 }
 
@@ -34,7 +34,7 @@ WebSocket::~WebSocket() {
 
 int32_t WebSocket::Connect(const Var& url, const Var protocols[],
     uint32_t protocol_count, const CompletionCallback& callback) {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return PP_ERROR_BADRESOURCE;
 
   // Convert protocols to C interface.
@@ -47,7 +47,7 @@ int32_t WebSocket::Connect(const Var& url, const Var protocols[],
   for (uint32_t i = 0; i < protocol_count; ++i)
     c_protocols[i] = protocols[i].pp_var();
 
-  int32_t result = get_interface<PPB_WebSocket>()->Connect(
+  int32_t result = get_interface<PPB_WebSocket_1_0>()->Connect(
       pp_resource(), url.pp_var(), c_protocols, protocol_count,
       callback.pp_completion_callback());
   if (c_protocols)
@@ -57,92 +57,92 @@ int32_t WebSocket::Connect(const Var& url, const Var protocols[],
 
 int32_t WebSocket::Close(uint16_t code, const Var& reason,
     const CompletionCallback& callback) {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return PP_ERROR_BADRESOURCE;
 
-  return get_interface<PPB_WebSocket>()->Close(
+  return get_interface<PPB_WebSocket_1_0>()->Close(
       pp_resource(), code, reason.pp_var(),
       callback.pp_completion_callback());
 }
 
 int32_t WebSocket::ReceiveMessage(Var* message,
     const CompletionCallback& callback) {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return PP_ERROR_BADRESOURCE;
 
-  return get_interface<PPB_WebSocket>()->ReceiveMessage(
+  return get_interface<PPB_WebSocket_1_0>()->ReceiveMessage(
       pp_resource(), const_cast<PP_Var*>(&message->pp_var()),
       callback.pp_completion_callback());
 }
 
 int32_t WebSocket::SendMessage(const Var& message) {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return PP_ERROR_BADRESOURCE;
 
-  return get_interface<PPB_WebSocket>()->SendMessage(
+  return get_interface<PPB_WebSocket_1_0>()->SendMessage(
       pp_resource(), message.pp_var());
 }
 
 uint64_t WebSocket::GetBufferedAmount() {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return 0;
 
-  return get_interface<PPB_WebSocket>()->GetBufferedAmount(pp_resource());
+  return get_interface<PPB_WebSocket_1_0>()->GetBufferedAmount(pp_resource());
 }
 
 uint16_t WebSocket::GetCloseCode() {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return 0;
 
-  return get_interface<PPB_WebSocket>()->GetCloseCode(pp_resource());
+  return get_interface<PPB_WebSocket_1_0>()->GetCloseCode(pp_resource());
 }
 
 Var WebSocket::GetCloseReason() {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return 0;
 
   return Var(PASS_REF,
-      get_interface<PPB_WebSocket>()->GetCloseReason(pp_resource()));
+      get_interface<PPB_WebSocket_1_0>()->GetCloseReason(pp_resource()));
 }
 
 bool WebSocket::GetCloseWasClean() {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return false;
 
   PP_Bool result =
-      get_interface<PPB_WebSocket>()->GetCloseWasClean(pp_resource());
+      get_interface<PPB_WebSocket_1_0>()->GetCloseWasClean(pp_resource());
   return PP_ToBool(result);
 }
 
 Var WebSocket::GetExtensions() {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return Var();
 
   return Var(PASS_REF,
-             get_interface<PPB_WebSocket>()->GetExtensions(pp_resource()));
+             get_interface<PPB_WebSocket_1_0>()->GetExtensions(pp_resource()));
 }
 
 Var WebSocket::GetProtocol() {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return Var();
 
   return Var(PASS_REF,
-             get_interface<PPB_WebSocket>()->GetProtocol(pp_resource()));
+             get_interface<PPB_WebSocket_1_0>()->GetProtocol(pp_resource()));
 }
 
 PP_WebSocketReadyState WebSocket::GetReadyState() {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return PP_WEBSOCKETREADYSTATE_INVALID;
 
-  return get_interface<PPB_WebSocket>()->GetReadyState(pp_resource());
+  return get_interface<PPB_WebSocket_1_0>()->GetReadyState(pp_resource());
 }
 
 Var WebSocket::GetURL() {
-  if (!has_interface<PPB_WebSocket>())
+  if (!has_interface<PPB_WebSocket_1_0>())
     return Var();
 
   return Var(PASS_REF,
-             get_interface<PPB_WebSocket>()->GetURL(pp_resource()));
+             get_interface<PPB_WebSocket_1_0>()->GetURL(pp_resource()));
 }
 
 }  // namespace pp
