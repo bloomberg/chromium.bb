a7990922f5426b8b29fa28aa991c5eeaf97e180d
Christian Egli <christian.egli@alumni.ethz.ch>
2012-03-19 14:48:43 +0000

    Doctests should be encoded with utf-8

--- tests/check_doctests.py
+++ tests/check_doctests.py
@@ -5,7 +5,7 @@ import sys
 
 exit_value = 0
 for test in glob.iglob('doctests/*_test.txt'):
-    failure_count, ignore = doctest.testfile(test, report=True)
+    failure_count, ignore = doctest.testfile(test, report=True, encoding='utf-8')
     if failure_count > 0: 
         exit_value = 1
 
