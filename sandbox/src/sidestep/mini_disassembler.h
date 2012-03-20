3367cfadefaa9f0279a269fba4313e1a0fd0e185
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 10:36:57 +0000

    Fix sidestep licenses
    Review URL: http://codereview.chromium.org/9733004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127665 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 71415d221d69a258b1d55ed76d8ebc32c673240e

--- sandbox/src/sidestep/mini_disassembler.h
+++ sandbox/src/sidestep/mini_disassembler.h
@@ -1,6 +1,7 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 // Definition of MiniDisassembler.
 
 #ifndef SANDBOX_SRC_SIDESTEP_MINI_DISASSEMBLER_H__
--- sandbox/src/sidestep/preamble_patcher.h
+++ sandbox/src/sidestep/preamble_patcher.h
@@ -1,6 +1,7 @@
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 // Definition of PreamblePatcher
 
 #ifndef SANDBOX_SRC_SIDESTEP_PREAMBLE_PATCHER_H__
