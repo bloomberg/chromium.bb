6b3da89754ec60a23c631b7e27a2bd527370beac
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-20 21:24:00 +0000

    Re-land: Adjust window timings to match Cole's video.
    http://crbug.com/118653
    TEST=none
    TBR=sadrul
    
    Original review: https://chromiumcodereview.appspot.com/9733003/
    Review URL: https://chromiumcodereview.appspot.com/9732030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@127790 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d3d46bfb5e1b1e87c4b1dfb84ff468592d6acdb5

---
+++ ash/animation_timings.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_ANIMATION_TIMINGS_H_
+#define ASH_ANIMATION_TIMINGS_H_
+#pragma once
+
+namespace ash {
+
+// All timings in ms.
+
+const int kStandardTransitionDuration = 218;
+
+// Roughly 0.5x standard.
+const int kFastTransitionDuration = 130;
+
+// Roughly 2x standard.
+const int kSlowTransitionDuration = 414;
+
+}  // namespace ash
+
+#endif  // ASH_ANIMATION_TIMINGS_H_
