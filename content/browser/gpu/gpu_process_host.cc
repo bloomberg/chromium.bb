bloomberg/chromium.bb

commit f84b577f738b5b55ba2551f47de0140284f57424

Add an explicit single-process check to the GPU process initialization.
This avoids a race between the GPU process and the renderer process that can cause the GPU code to create the first ChildProcess instance, causing the renderer to dcheck.

BUG=NONE
TEST=NONE


Review URL: http://codereview.chromium.org/9647001

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@126140 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 84004c5e3965a13beb46c7052bbd200cfa3707ac

content/browser/gpu/gpu_process_host.cc
@@ -159,10 +159,8 @@ class GpuMainThread : public base::Thread {
 
  protected:
   virtual void Init() {
-    // TODO: Currently, ChildProcess supports only a single static instance,
-    // which is a problem in --single-process mode, where both gpu and renderer
-    // should be able to create separate instances.
-    if (GpuProcess::current()) {
+    if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kSingleProcess) ||
+        CommandLine::ForCurrentProcess()->HasSwitch(switches::kInProcessGPU)) {
       child_thread_ = new GpuChildThread(channel_id_);
     } else {
       gpu_process_ = new GpuProcess();

commit 089f1610fc2827577a87a9cb0c50a84cf350a90f

Removing --in-process-gpu from the set of flags that prevents creation of a GpuChildThread.
The Android build uses this flag, which assumes that the GPU process host creates the ChildProcess instance. This regressed in r126140.

BUG=NONE
TEST=NONE


Review URL: http://codereview.chromium.org/9815011

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128079 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 606fac0c97ffc8c60801ce7dbdc2d8e1bd07cdb4

content/browser/gpu/gpu_process_host.cc
@@ -163,8 +163,7 @@ class GpuMainThread : public base::Thread {
 
  protected:
   virtual void Init() {
-    if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kSingleProcess) ||
-        CommandLine::ForCurrentProcess()->HasSwitch(switches::kInProcessGPU)) {
+    if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kSingleProcess)) {
       child_thread_ = new GpuChildThread(channel_id_);
     } else {
       gpu_process_ = new GpuProcess();