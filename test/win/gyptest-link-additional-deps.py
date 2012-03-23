d34fcb776c73fe11e18699eccb9c0c409379c7ac
scottmg@chromium.org <scottmg@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2012-03-23 21:59:43 +0000

    ninja windows: test for additional link dependencies
    R=thakis
    
    Review URL: https://chromiumcodereview.appspot.com/9808084
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1252 78cadc50-ecff-11dd-a971-7dbc132099af

---
+++ test/win/gyptest-link-additional-deps.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2012 Google Inc. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+Make sure additional library dependencies are handled.
+"""
+
+import TestGyp
+
+import subprocess
+import sys
+
+if sys.platform == 'win32':
+  test = TestGyp.TestGyp(formats=['ninja', 'msvs'])
+
+  CHDIR = 'linker-flags'
+  test.run_gyp('additional-deps.gyp', chdir=CHDIR)
+  test.build('additional-deps.gyp', test.ALL, chdir=CHDIR)
+
+  test.pass_test()
