6365be0ef7796c02c3f278b5defbb8ca6d6de744
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-23 23:11:24 +0000

    Add shortcut image to the secondary monitor screen Moved the message to the top per glen's request.
    BUG=115510
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/9834055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128602 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 196cbc99e92e29c99a053e85d9fb93bf4fc8e232

--- ash/monitor/secondary_monitor_view.cc
+++ ash/monitor/secondary_monitor_view.cc
@@ -5,11 +5,13 @@
 #include "ash/monitor/secondary_monitor_view.h"
 
 #include "grit/ash_strings.h"
+#include "grit/ui_resources.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/aura/window.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/views/background.h"
+#include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/view.h"
 #include "ui/views/widget/widget.h"
@@ -39,37 +41,54 @@ class SecondaryMonitorView : public views::WidgetDelegateView {
     message_ = new views::Label(
         l10n_util::GetStringUTF16(IDS_ASH_SECONDARY_MONITOR));
     message_->SetAutoColorReadabilityEnabled(false);
-    message_->SetFont(rb.GetFont(ui::ResourceBundle::MediumFont));
+    message_->SetFont(rb.GetFont(ui::ResourceBundle::LargeFont));
     message_->SetHorizontalAlignment(views::Label::ALIGN_CENTER);
     message_->SetEnabledColor(kMessageColor);
     AddChildView(message_);
 
-    // TODO(oshima): Add image for fullscreen shortcut.
-    shortcut_ = new views::Label(
+    shortcut_text_ = new views::Label(
         l10n_util::GetStringUTF16(IDS_ASH_SECONDARY_MONITOR_SHORTCUT));
-    shortcut_->SetAutoColorReadabilityEnabled(false);
-    shortcut_->SetFont(rb.GetFont(ui::ResourceBundle::SmallFont));
-    shortcut_->SetHorizontalAlignment(views::Label::ALIGN_CENTER);
-    shortcut_->SetEnabledColor(kShortcutColor);
-    AddChildView(shortcut_);
+    shortcut_text_->SetAutoColorReadabilityEnabled(false);
+    shortcut_text_->SetFont(rb.GetFont(ui::ResourceBundle::MediumFont));
+    shortcut_text_->SetHorizontalAlignment(views::Label::ALIGN_CENTER);
+    shortcut_text_->SetEnabledColor(kShortcutColor);
+    AddChildView(shortcut_text_);
+
+    shortcut_image_ = new views::ImageView();
+    shortcut_image_->SetImage(rb.GetBitmapNamed(IDR_AURA_SWITCH_MONITOR));
+    AddChildView(shortcut_image_);
   }
 
   virtual void Layout() {
-    const int kMessageMargin = 20;
-    const float kShortcutPositionFromBottom = 50;
+    const int kMessagePositionTopMargin = 40;
+    const int kShortcutPositionBottomMargin = 40;
+    const int kShortcutMargin = 4;  // margin between text and image.
     gfx::Rect b = bounds();
-    int bottom = bounds().height() - kShortcutPositionFromBottom;
-    int shortcut_height = shortcut_->GetHeightForWidth(b.width());
-    shortcut_->SetBounds(0, bottom, b.width(), shortcut_height);
 
     int msg_height = message_->GetHeightForWidth(b.width());
-    bottom -= msg_height + kMessageMargin;
-    message_->SetBounds(0, bottom, bounds().width(), msg_height);
+    message_->SetBounds(
+        0, kMessagePositionTopMargin, bounds().width(), msg_height);
+
+    // TODO(oshima): Figure out what to do for RTL.
+    // Align the shortcut text & image to the center.
+    gfx::Size text_size = shortcut_text_->GetPreferredSize();
+    gfx::Size image_size = shortcut_image_->GetPreferredSize();
+    int height = std::max(text_size.height(), image_size.height());
+    int y = b.height() - kShortcutPositionBottomMargin - height;
+    int x = (b.width() -
+             (text_size.width() + kShortcutMargin + image_size.width())) / 2;
+    shortcut_text_->SetBounds(x, y + (height - text_size.height()) / 2,
+                              text_size.width(), text_size.height());
+    shortcut_image_->SetBounds(
+        x + text_size.width() + kShortcutMargin,
+        y + (height - image_size.height()) / 2,
+        image_size.width(), image_size.height());
   }
 
  private:
   views::Label* message_;
-  views::Label* shortcut_;
+  views::Label* shortcut_text_;
+  views::ImageView* shortcut_image_;
 
   DISALLOW_COPY_AND_ASSIGN(SecondaryMonitorView);
 };
