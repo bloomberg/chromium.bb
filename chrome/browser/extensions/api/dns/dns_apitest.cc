9caf41d0a67f34ac1dbf87ee22e9f451b83cbdba
nduca@google.com <nduca@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 00:40:34 +0000

    Revert 128610 - Expand usage of platform-apps flag and permission features.
    Reapplication of http://codereview.chromium.org/9834022/ with the
    VerifyPermissions test #ifdefed out for Windows.
    
    BUG=119758
    TEST=added
    
    Review URL: https://chromiumcodereview.appspot.com/9837045
    
    TBR=miket@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/9808096
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128620 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 457e4e38a186b1d5bb84228b331970efd4242b7a

--- chrome/browser/extensions/api/dns/dns_apitest.cc
+++ chrome/browser/extensions/api/dns/dns_apitest.cc
@@ -2,11 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/command_line.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/synchronization/waitable_event.h"
 #include "chrome/browser/extensions/api/dns/dns_api.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_function_test_utils.h"
+#include "chrome/common/chrome_switches.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/host_resolver.h"
 #include "net/base/mock_host_resolver.h"
@@ -19,13 +21,19 @@ using extension_function_test_utils::RunFunctionAndReturnResult;
 
 namespace {
 
-class DnsApiTest : public PlatformAppApiTest {
+class DnsApiTest : public ExtensionApiTest {
  public:
   static const std::string kHostname;
   static const std::string kAddress;
 
   DnsApiTest() : resolver_event_(true, false),
                  mock_host_resolver_(NULL) {
+  }
+
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    ExtensionApiTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitch(switches::kEnableExperimentalExtensionApis);
+    command_line->AppendSwitch(switches::kEnablePlatformApps);
   }
 
   virtual void SetUpOnMainThread() OVERRIDE {
@@ -96,10 +104,6 @@ const std::string DnsApiTest::kHostname = "www.sowbug.org";
 const std::string DnsApiTest::kAddress = "9.8.7.6";
 
 }  // namespace
-
-IN_PROC_BROWSER_TEST_F(DnsApiTest, VerifyPermissions) {
-  VerifyPermissions(test_data_dir_.AppendASCII("dns/api"));
-}
 
 IN_PROC_BROWSER_TEST_F(DnsApiTest, DnsResolveIPLiteral) {
   scoped_refptr<extensions::DnsResolveFunction> resolve_function(
--- chrome/browser/extensions/api/serial/serial_apitest.cc
+++ chrome/browser/extensions/api/serial/serial_apitest.cc
@@ -2,28 +2,32 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/command_line.h"
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/extensions/api/serial/serial_api.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_function_test_utils.h"
 #include "chrome/browser/extensions/extension_test_message_listener.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/common/chrome_switches.h"
 #include "content/public/browser/browser_thread.h"
 
 using content::BrowserThread;
 
 namespace {
 
-class SerialApiTest : public PlatformAppApiTest {
+class SerialApiTest : public ExtensionApiTest {
  public:
   SerialApiTest() {}
+
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    ExtensionApiTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitch(switches::kEnableExperimentalExtensionApis);
+    command_line->AppendSwitch(switches::kEnablePlatformApps);
+  }
 };
 
 }  // namespace
-
-IN_PROC_BROWSER_TEST_F(SerialApiTest, VerifyPermissions) {
-  VerifyPermissions(test_data_dir_.AppendASCII("serial/api"));
-}
 
 IN_PROC_BROWSER_TEST_F(SerialApiTest, SerialExtension) {
   ResultCatcher catcher;
