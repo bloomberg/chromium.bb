dc4876d64bce2642b20e4e1c5c7ef996a6849ae6
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-24 02:03:11 +0000

    Ash: Fix toolbar bg painting with themes
    I was using the wrong size to compute how far down to paint the toolbar background color for themes that use a solid color.
    
    BUG=119836
    TEST=visual, install "Grass" theme and verify green toolbar background extends all the way to the bottom edge
    
    Review URL: https://chromiumcodereview.appspot.com/9838082
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128655 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f48150c2db81d9b0f700326d4042927b792942da

--- chrome/browser/ui/views/frame/browser_non_client_frame_view_aura.cc
+++ chrome/browser/ui/views/frame/browser_non_client_frame_view_aura.cc
@@ -326,8 +326,7 @@ void BrowserNonClientFrameViewAura::PaintToolbarBackground(
   int split_point = kFrameShadowThickness * 2;
   int bottom_y = y + split_point;
   ui::ThemeProvider* tp = GetThemeProvider();
-  SkBitmap* theme_toolbar = tp->GetBitmapNamed(IDR_THEME_TOOLBAR);
-  int bottom_edge_height = std::min(theme_toolbar->height(), h) - split_point;
+  int bottom_edge_height = h - split_point;
 
   canvas->FillRect(gfx::Rect(x, bottom_y, w, bottom_edge_height),
                    tp->GetColor(ThemeService::COLOR_TOOLBAR));
@@ -337,6 +336,7 @@ void BrowserNonClientFrameViewAura::PaintToolbarBackground(
   // source y position.  If you have to debug this code use an image editor
   // to paint a diagonal line through the toolbar image and ensure it lines up
   // across the tab and toolbar.
+  SkBitmap* theme_toolbar = tp->GetBitmapNamed(IDR_THEME_TOOLBAR);
   canvas->TileImageInt(
       *theme_toolbar,
       x, bottom_y - GetHorizontalTabStripVerticalOffset(false),
