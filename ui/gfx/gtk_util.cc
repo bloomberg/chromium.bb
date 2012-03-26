2142e7707b40033e1d9e4e80604267cf7fd703aa
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-26 14:34:22 +0000

    ui/gfx: Add a typedef of std::map to improve the readability of GdkCursorCache implementation.
    R=asvitkine@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/9804003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@128908 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bc00d9ca499e3c51f00c01e51ed9c17b43d8b594

--- ui/gfx/gtk_util.cc
+++ ui/gfx/gtk_util.cc
@@ -10,7 +10,6 @@
 
 #include "base/basictypes.h"
 #include "base/command_line.h"
-#include "base/linux_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkUnPreMultiply.h"
@@ -18,26 +17,25 @@
 
 namespace {
 
-// A process wide singleton that manages our usage of gdk
-// cursors. gdk_cursor_new() hits the disk in several places and GdkCursor
-// instances can be reused throughout the process.
+// A process wide singleton that manages our usage of gdk cursors.
+// gdk_cursor_new() hits the disk in several places and GdkCursor instances can
+// be reused throughout the process.
 class GdkCursorCache {
  public:
-   GdkCursorCache() {}
+  GdkCursorCache() {}
   ~GdkCursorCache() {
-    for (std::map<GdkCursorType, GdkCursor*>::iterator it =
-        cursor_cache_.begin(); it != cursor_cache_.end(); ++it) {
-      gdk_cursor_unref(it->second);
+    for (GdkCursorMap::iterator i(cursors_.begin()); i != cursors_.end(); ++i) {
+      gdk_cursor_unref(i->second);
     }
-    cursor_cache_.clear();
+    cursors_.clear();
   }
 
   GdkCursor* GetCursorImpl(GdkCursorType type) {
-    std::map<GdkCursorType, GdkCursor*>::iterator it = cursor_cache_.find(type);
+    GdkCursorMap::iterator it = cursors_.find(type);
     GdkCursor* cursor = NULL;
-    if (it == cursor_cache_.end()) {
+    if (it == cursors_.end()) {
       cursor = gdk_cursor_new(type);
-      cursor_cache_.insert(std::make_pair(type, cursor));
+      cursors_.insert(std::make_pair(type, cursor));
     } else {
       cursor = it->second;
     }
@@ -47,7 +45,9 @@ class GdkCursorCache {
     return cursor;
   }
 
-  std::map<GdkCursorType, GdkCursor*> cursor_cache_;
+ private:
+  typedef std::map<GdkCursorType, GdkCursor*> GdkCursorMap;
+  GdkCursorMap cursors_;
 
   DISALLOW_COPY_AND_ASSIGN(GdkCursorCache);
 };
