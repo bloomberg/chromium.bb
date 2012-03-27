0e8a4d392f96a35a154f63a1ad5e3da067ace3a7
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 04:50:20 +0000

    Make the "Stylizr" storage API example extension use the "local" storage area.
    Review URL: https://chromiumcodereview.appspot.com/9861013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129133 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: afbd2477a523c4715e274a948a9c4ae1753a6fb5

--- chrome/common/extensions/docs/examples/api/storage/stylizr/options.js
+++ chrome/common/extensions/docs/examples/api/storage/stylizr/options.js
@@ -2,8 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Store settings in the synchronized repository.
-var storage = chrome.storage.sync;
+// Store CSS data in the "local" storage area.
+//
+// Usually we try to store settings in the "sync" area since a lot of the time
+// it will be a better user experience for settings to automatically sync
+// between browsers.
+//
+// However, "sync" is expensive with a strict quota (both in storage space and
+// bandwidth) so data that may be as large and updated as frequently as the CSS
+// may not be suitable.
+var storage = chrome.storage.local;
 
 // Get at the DOM controls used in the sample.
 var resetButton = document.querySelector('button.reset');
--- chrome/common/extensions/docs/examples/api/storage/stylizr/popup.js
+++ chrome/common/extensions/docs/examples/api/storage/stylizr/popup.js
@@ -2,8 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// Store settings in the synchronized repository.
-var storage = chrome.storage.sync;
+// Store CSS data in the "local" storage area.
+//
+// See note in options.js for rationale on why not to use "sync".
+var storage = chrome.storage.local;
+
 var message = document.querySelector('#message');
 
 // Check if there is CSS specified.
