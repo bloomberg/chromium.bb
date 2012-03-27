ac40a76cf6db936d991037c9fee3071bfcf2e332
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 19:36:22 +0000

    Make PrintPreviewHandler::SendCloudPrintJob properly handle being called with no data.
    BUG=119919
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9854028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129242 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a704ebf656966a0dd3ed4170625d1386c783df2c

--- chrome/browser/ui/webui/print_preview/print_preview_handler.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.cc
@@ -711,54 +711,56 @@ void PrintPreviewHandler::SendCloudPrintJob(const DictionaryValue& settings,
       web_ui()->GetController());
   print_preview_ui->GetPrintPreviewDataForIndex(
       printing::COMPLETE_PREVIEW_DOCUMENT_INDEX, &data);
-  DCHECK_GT(data->size(), 0U);
+  if (data->size() > 0U && data->front()) {
+    string16 print_job_title_utf16 =
+        preview_tab_wrapper()->print_view_manager()->RenderSourceName();
+    std::string print_job_title = UTF16ToUTF8(print_job_title_utf16);
+    std::string printer_id;
+    settings.GetString(printing::kSettingCloudPrintId, &printer_id);
+    // BASE64 encode the job data.
+    std::string raw_data(reinterpret_cast<const char*>(data->front()),
+                         data->size());
+    std::string base64_data;
+    if (!base::Base64Encode(raw_data, &base64_data)) {
+      NOTREACHED() << "Base64 encoding PDF data.";
+    }
 
-  string16 print_job_title_utf16 =
-      preview_tab_wrapper()->print_view_manager()->RenderSourceName();
-  std::string print_job_title = UTF16ToUTF8(print_job_title_utf16);
-  std::string printer_id;
-  settings.GetString(printing::kSettingCloudPrintId, &printer_id);
-  // BASE64 encode the job data.
-  std::string raw_data(reinterpret_cast<const char*>(data->front()),
-                       data->size());
-  std::string base64_data;
-  if (!base::Base64Encode(raw_data, &base64_data)) {
-    NOTREACHED() << "Base64 encoding PDF data.";
+    const char boundary[] = "----CloudPrintFormBoundaryjc9wuprokl8i";
+    const char prolog[] = "--%s\r\n"
+      "Content-Disposition: form-data; name=\"capabilities\"\r\n\r\n%s\r\n"
+      "--%s\r\n"
+      "Content-Disposition: form-data; name=\"contentType\"\r\n\r\ndataUrl\r\n"
+      "--%s\r\n"
+      "Content-Disposition: form-data; name=\"title\"\r\n\r\n%s\r\n"
+      "--%s\r\n"
+      "Content-Disposition: form-data; name=\"printerid\"\r\n\r\n%s\r\n"
+      "--%s\r\n"
+      "Content-Disposition: form-data; name=\"content\"\r\n\r\n"
+      "data:application/pdf;base64,%s\r\n"
+      "--%s\r\n";
+
+    // TODO(abodenha@chromium.org) This implies a large copy operation.
+    // Profile this and optimize if necessary.
+    std::string final_data;
+    base::SStringPrintf(&final_data,
+                        prolog,
+                        boundary,
+                        print_ticket.c_str(),
+                        boundary,
+                        boundary,
+                        print_job_title.c_str(),
+                        boundary,
+                        printer_id.c_str(),
+                        boundary,
+                        base64_data.c_str(),
+                        boundary);
+
+    StringValue data_value(final_data);
+
+    web_ui()->CallJavascriptFunction("printToCloud", data_value);
+  } else {
+    NOTREACHED();
   }
-
-  const char boundary[] = "----CloudPrintFormBoundaryjc9wuprokl8i";
-  const char prolog[] = "--%s\r\n"
-    "Content-Disposition: form-data; name=\"capabilities\"\r\n\r\n%s\r\n"
-    "--%s\r\n"
-    "Content-Disposition: form-data; name=\"contentType\"\r\n\r\ndataUrl\r\n"
-    "--%s\r\n"
-    "Content-Disposition: form-data; name=\"title\"\r\n\r\n%s\r\n"
-    "--%s\r\n"
-    "Content-Disposition: form-data; name=\"printerid\"\r\n\r\n%s\r\n"
-    "--%s\r\n"
-    "Content-Disposition: form-data; name=\"content\"\r\n\r\n"
-    "data:application/pdf;base64,%s\r\n"
-    "--%s\r\n";
-
-  // TODO(abodenha@chromium.org) This implies a large copy operation.
-  // Profile this and optimize if necessary.
-  std::string final_data;
-  base::SStringPrintf(&final_data,
-                      prolog,
-                      boundary,
-                      print_ticket.c_str(),
-                      boundary,
-                      boundary,
-                      print_job_title.c_str(),
-                      boundary,
-                      printer_id.c_str(),
-                      boundary,
-                      base64_data.c_str(),
-                      boundary);
-
-  StringValue data_value(final_data);
-
-  web_ui()->CallJavascriptFunction("printToCloud", data_value);
 }
 
 TabContentsWrapper* PrintPreviewHandler::GetInitiatorTab() const {
