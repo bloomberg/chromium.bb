95270581ddacc9539ddfa7b0ab1b1625d0a932e8
csilv@chromium.org <csilv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-27 05:55:26 +0000

    Disallow uber URLs in incognito windows.
    BUG=111243
    TEST=Select settings, extensions, or about from wrench menu in OTR, verify that tab is opened in non-OTR window.
    Review URL: https://chromiumcodereview.appspot.com/9861010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129151 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b0f989d179736b72487547ffff37083b5a95e1c6

--- chrome/browser/ui/browser_navigator.cc
+++ chrome/browser/ui/browser_navigator.cc
@@ -678,7 +678,8 @@ bool IsURLAllowedInIncognito(const GURL& url) {
        url.host() == chrome::kChromeUISettingsFrameHost ||
        url.host() == chrome::kChromeUIExtensionsHost ||
        url.host() == chrome::kChromeUIBookmarksHost ||
-       url.host() == chrome::kChromeUISyncPromoHost));
+       url.host() == chrome::kChromeUISyncPromoHost ||
+       url.host() == chrome::kChromeUIUberHost));
 }
 
 #if defined(OS_CHROMEOS) || defined(USE_AURA)
