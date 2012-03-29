4e3a516eecb9529bad6a0be33183a1fd303f000f
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-29 03:24:11 +0000

    Run indexeddb layout tests as browser tests.
    BUG=118499
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9834072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129566 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 943ecf651c4e9af455d059e52be62c17b32e8e31

--- content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
+++ content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
@@ -10,28 +10,99 @@ class IndexedDBLayoutTest : public InProcessBrowserLayoutTest {
   IndexedDBLayoutTest() : InProcessBrowserLayoutTest(
       FilePath().AppendASCII("storage").AppendASCII("indexeddb")) {
   }
+
   virtual void SetUpInProcessBrowserTestFixture() OVERRIDE {
     InProcessBrowserLayoutTest::SetUpInProcessBrowserTestFixture();
     AddResourceForLayoutTest(
         FilePath().AppendASCII("fast").AppendASCII("js"),
         FilePath().AppendASCII("resources"));
   }
+
+  void RunLayoutTests(const char* file_names[]) {
+    for (size_t i = 0; file_names[i]; i++)
+      RunLayoutTest(file_names[i]);
+  }
 };
 
 namespace {
 
-static const char* kLayoutTestFileNames[] = {
+static const char* kBasicTests[] = {
   "basics.html",
   "basics-shared-workers.html",
   "basics-workers.html",
+  "database-basics.html",
+  "factory-basics.html",
   "index-basics.html",
   "objectstore-basics.html",
+  NULL
+};
+
+static const char* kComplexTests[] = {
   "prefetch-bugfix-108071.html",
+  NULL
+};
+
+static const char* kIndexTests[] = {
+  "deleteIndex.html",
+  "index-basics-workers.html",
+  "index-count.html",
+  "index-cursor.html",  // Locally takes ~6s compared to <1 for the others.
+  "index-get-key-argument-required.html",
+  "index-multientry.html",
+  "index-population.html",
+  "index-unique.html",
+  NULL
+};
+
+static const char* kKeyTests[] = {
+  "key-generator.html",
+  "keypath-basics.html",
+  "keypath-edges.html",
+  "keypath-fetch-key.html",
+  "keyrange.html",
+  "keyrange-required-arguments.html",
+  "key-sort-order-across-types.html",
+  "key-sort-order-date.html",
+  "key-type-array.html",
+  "key-type-infinity.html",
+  "invalid-keys.html",
+  NULL
+};
+
+static const char* kTransactionTests[] = {
+//  "transaction-abort.html", // Flaky, http://crbug.com/83226
+  "transaction-abort-with-js-recursion-cross-frame.html",
+  "transaction-abort-with-js-recursion.html",
+  "transaction-abort-workers.html",
+  "transaction-after-close.html",
+  "transaction-and-objectstore-calls.html",
+  "transaction-basics.html",
+  "transaction-crash-on-abort.html",
+  "transaction-event-propagation.html",
+  "transaction-read-only.html",
+  "transaction-rollback.html",
+  "transaction-storeNames-required.html",
+  NULL
 };
 
 }
 
-IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, FirstTest) {
-  for (size_t i = 0; i < arraysize(kLayoutTestFileNames); ++i)
-    RunLayoutTest(kLayoutTestFileNames[i]);
+IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, BasicTests) {
+  RunLayoutTests(kBasicTests);
+}
+
+IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, ComplexTests) {
+  RunLayoutTests(kComplexTests);
+}
+
+IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, IndexTests) {
+  RunLayoutTests(kIndexTests);
+}
+
+IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, KeyTests) {
+  RunLayoutTests(kKeyTests);
+}
+
+IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, TransactionTests) {
+  RunLayoutTests(kTransactionTests);
 }
