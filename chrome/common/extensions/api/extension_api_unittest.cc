507ac6bc3b3c2c9e05049430ea798b76de8f574c
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-30 23:48:49 +0000

    When input is "" (or " " with trim_whitespace true), SplitString() should return an empty vector, not a vector of one empty string.
    Brett and I discussed this for a while and felt this would be wise, whereas dropping all empty segments entirely (e.g. converting "a,,b" to a vector of two elements instead of three) was probably unwise. 
    
    This also simplifies the code some. 
    
    Fixing this also required changing the code in mime_util.cc to handle empty vectors to "are codecs valid" oracle functions (in which case we return false). I also fixed some style issues there. It also required avoiding passing the empty string in a test in extension_api_unittest.cc; Aaron assures me that this code is not expected to be defensive against such inputs, but at his suggestion I also added some CHECK()s to the API.
    
    BUG=none
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/9960004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129998 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 657cab96e02528adfe513c90c4f07a036d92bfe6

--- chrome/common/extensions/api/extension_api_unittest.cc
+++ chrome/common/extensions/api/extension_api_unittest.cc
@@ -26,7 +26,6 @@ TEST(ExtensionAPI, IsPrivileged) {
   EXPECT_TRUE(extension_api.IsPrivileged("extension.lastError"));
 
   // Default unknown names to privileged for paranoia's sake.
-  EXPECT_TRUE(extension_api.IsPrivileged(""));
   EXPECT_TRUE(extension_api.IsPrivileged("<unknown-namespace>"));
   EXPECT_TRUE(extension_api.IsPrivileged("extension.<unknown-member>"));
 
