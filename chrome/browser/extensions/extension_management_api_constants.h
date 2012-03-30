63de4fec06970d3d76a31097fc1089bb3a442bd8
pam@chromium.org <pam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-03-30 06:08:39 +0000

    Remove leftover constant declarations accidentally left behind by a previous patch.
    BUG=119692
    TEST=compile succeeds
    Review URL: https://chromiumcodereview.appspot.com/9903017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@129799 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 64286c63308db83935b5112b4adc2524f7c7f28f

--- chrome/browser/extensions/extension_management_api_constants.h
+++ chrome/browser/extensions/extension_management_api_constants.h
@@ -10,23 +10,14 @@ namespace extension_management_api_constants {
 
 // Keys used for incoming arguments and outgoing JSON data.
 extern const char kAppLaunchUrlKey[];
-extern const char kDescriptionKey[];
-extern const char kEnabledKey[];
 extern const char kDisabledReasonKey[];
-extern const char kHomepageUrlKey[];
 extern const char kHostPermissionsKey[];
 extern const char kIconsKey[];
-extern const char kIdKey[];
 extern const char kIsAppKey[];
-extern const char kNameKey[];
-extern const char kOfflineEnabledKey[];
-extern const char kOptionsUrlKey[];
 extern const char kPermissionsKey[];
-extern const char kMayDisableKey[];
 extern const char kSizeKey[];
 extern const char kUpdateUrlKey[];
 extern const char kUrlKey[];
-extern const char kVersionKey[];
 
 // Values for outgoing JSON data.
 extern const char kDisabledReasonPermissionsIncrease[];
