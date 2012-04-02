68ce07d10edb2ea0de93744b578cfaddddf68c83
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-02 16:22:07 +0000

    Fix forward declaraion of content::RenderView.
    BUG=none
    
    Review URL: http://codereview.chromium.org/9963020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130137 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5a327b594dd1f2842c3a8b4a797a96166ac8bc93

--- content/public/renderer/render_view_observer_tracker.h
+++ content/public/renderer/render_view_observer_tracker.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -31,9 +31,9 @@
 
 #include "base/lazy_instance.h"
 
-class RenderView;
-
 namespace content {
+
+class RenderView;
 
 template <class T>
 class RenderViewObserverTracker {
