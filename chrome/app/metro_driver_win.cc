69af4a682bc6b1d692d83fa701772965d6f848c4
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-03 03:12:28 +0000

    Change the metro driver to use the driver from the version directory.
    So upgrades are less of a PITA
    
    BUG=118641
    TEST=chrome starts.
    Review URL: https://chromiumcodereview.appspot.com/9963074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130297 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8ecd3cdd1876ff6b6079385ba05c666fc9580ec2

--- chrome/app/metro_driver_win.cc
+++ chrome/app/metro_driver_win.cc
@@ -1,12 +1,16 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "metro_driver_win.h"
 
+#include <string.h>
+
+#include "chrome/app/client_util.h"
+
 namespace {
 // The windows 8 metro driver dll name and entry point.
-const char kMetroDriverDll[] = "metro_driver.dll";
+const wchar_t kMetroDriverDll[] = L"metro_driver.dll";
 // This environment variable controls the loading of the metro driver DLL.
 const char* kMetroModeEnvVar = "CHROME_METRO_DLL";
 
@@ -29,15 +33,35 @@ DWORD WINAPI MainThread(void* param) {
 MetroDriver::MetroDriver() : init_metro_fn_(NULL) {
   if (0 != ::GetEnvironmentVariableA(kMetroModeEnvVar, NULL, 0))
     return;
+  // The metro activation always has the |ServerName| parameter. If we dont
+  // see it, we are being launched in desktop mode.
+  if (!wcsstr(::GetCommandLineW(), L" -ServerName:DefaultBrowserServer")) {
+    ::SetEnvironmentVariableA(kMetroModeEnvVar, "0");
+    return;
+  }
   // We haven't tried to load the metro driver, this probably means we are the
   // browser. Find it or not we set the environment variable because we don't
   // want to keep trying in the child processes.
-  HMODULE metro_dll = ::LoadLibraryA(kMetroDriverDll);
+  HMODULE metro_dll = ::LoadLibraryW(kMetroDriverDll);
+  if (!metro_dll) {
+    // It is not next to the build output, so this must be an actual deployment
+    // and in that case we need the mainloader to find the current version
+    // directory.
+    MainDllLoader* loader = MakeMainDllLoader();
+    std::wstring version = loader->GetVersion();
+    delete loader;
+    if (!version.empty()) {
+      std::wstring exe_path(GetExecutablePath());
+      exe_path.append(version).append(L"\\").append(kMetroDriverDll);
+      metro_dll = ::LoadLibraryW(exe_path.c_str());
+    }
+  }
+  // We set the environment variable always, so we don't keep trying in
+  // the child processes.
   ::SetEnvironmentVariableA(kMetroModeEnvVar, metro_dll ? "1" : "0");
   if (!metro_dll)
     return;
-  init_metro_fn_ =
-      ::GetProcAddress(::GetModuleHandleA(kMetroDriverDll), "InitMetro");
+  init_metro_fn_ = ::GetProcAddress(metro_dll, "InitMetro");
 }
 
 int MetroDriver::RunInMetro(HINSTANCE instance, MainFn main_fn) {
