41bf9f0d970111051792f3e20e95133a6a337a1b
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-03 20:30:13 +0000

    Change SingletonHwnd to be based on WindowImpl.
    This removes the need for some Windows plumbing in
    SingletonHwnd and fixes an issue where a WndProc
    message would be getting sent after the Singleton
    destruction, which was being triggered in ui_tests.
    
    WindowImpl is robust against the above issue because
    it stores a pointer to itself in the window user data
    and clears it when it is destroyed.
    
    BUG=105550
    TEST=Running ui_tests with use_canvas_skia=1 on Windows shouldn't crash.
    Review URL: https://chromiumcodereview.appspot.com/9965082
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130448 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: eb2328754b0ebbb73a53624ffd1461c66b7655e2

--- ui/base/win/singleton_hwnd.cc
+++ ui/base/win/singleton_hwnd.cc
@@ -5,39 +5,6 @@
 #include "ui/base/win/singleton_hwnd.h"
 
 #include "base/memory/singleton.h"
-#include "base/process_util.h"
-#include "base/win/wrapped_window_proc.h"
-#include "ui/base/win/hwnd_util.h"
-
-namespace {
-
-// Windows class name to use for the listener window.
-const wchar_t kWindowClassName[] = L"Chrome_SingletonHwnd";
-
-// Windows callback for listening for WM_* messages.
-LRESULT CALLBACK ListenerWindowProc(HWND hwnd,
-                                    UINT message,
-                                    WPARAM wparam,
-                                    LPARAM lparam) {
-  ui::SingletonHwnd::GetInstance()->OnWndProc(hwnd, message, wparam, lparam);
-  return ::DefWindowProc(hwnd, message, wparam, lparam);
-}
-
-// Creates a listener window to receive WM_* messages.
-HWND CreateListenerWindow() {
-  WNDCLASSEX wc = {0};
-  wc.cbSize = sizeof(wc);
-  wc.lpfnWndProc = base::win::WrappedWindowProc<ListenerWindowProc>;
-  wc.hInstance = base::GetModuleFromAddress(&ListenerWindowProc);
-  wc.lpszClassName = kWindowClassName;
-  ATOM window_class = ::RegisterClassEx(&wc);
-  DCHECK(window_class);
-
-  return ::CreateWindow(MAKEINTATOM(window_class), 0, 0, 0, 0, 0, 0, 0, 0,
-                        wc.hInstance, 0);
-}
-
-}  // namespace
 
 namespace ui {
 
@@ -47,10 +14,8 @@ SingletonHwnd* SingletonHwnd::GetInstance() {
 }
 
 void SingletonHwnd::AddObserver(Observer* observer) {
-  if (!listener_window_) {
-    listener_window_ = CreateListenerWindow();
-    ui::CheckWindowCreated(listener_window_);
-  }
+  if (!hwnd())
+    WindowImpl::Init(NULL, gfx::Rect());
   observer_list_.AddObserver(observer);
 }
 
@@ -58,24 +23,22 @@ void SingletonHwnd::RemoveObserver(Observer* observer) {
   observer_list_.RemoveObserver(observer);
 }
 
-void SingletonHwnd::OnWndProc(HWND hwnd,
-                              UINT message,
-                              WPARAM wparam,
-                              LPARAM lparam) {
+BOOL SingletonHwnd::ProcessWindowMessage(HWND window,
+                                         UINT message,
+                                         WPARAM wparam,
+                                         LPARAM lparam,
+                                         LRESULT& result,
+                                         DWORD msg_map_id) {
   FOR_EACH_OBSERVER(Observer,
                     observer_list_,
-                    OnWndProc(hwnd, message, wparam, lparam));
+                    OnWndProc(window, message, wparam, lparam));
+  return false;
 }
 
-SingletonHwnd::SingletonHwnd()
-    : listener_window_(NULL) {
+SingletonHwnd::SingletonHwnd() {
 }
 
 SingletonHwnd::~SingletonHwnd() {
-  if (listener_window_) {
-    ::DestroyWindow(listener_window_);
-    ::UnregisterClass(kWindowClassName, GetModuleHandle(NULL));
-  }
 }
 
 }  // namespace ui
--- ui/base/win/singleton_hwnd.h
+++ ui/base/win/singleton_hwnd.h
@@ -12,6 +12,7 @@
 #include "base/basictypes.h"
 #include "base/callback_forward.h"
 #include "base/observer_list.h"
+#include "ui/base/win/window_impl.h"
 
 template<typename T> struct DefaultSingletonTraits;
 
@@ -19,7 +20,7 @@ namespace ui {
 
 // Singleton message-only HWND that allows interested clients to receive WM_*
 // notifications.
-class SingletonHwnd {
+class SingletonHwnd : public WindowImpl {
  public:
   static SingletonHwnd* GetInstance();
 
@@ -37,16 +38,18 @@ class SingletonHwnd {
   void RemoveObserver(Observer* observer);
 
   // Windows callback for WM_* notifications.
-  void OnWndProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
+  virtual BOOL ProcessWindowMessage(HWND window,
+                                    UINT message,
+                                    WPARAM wparam,
+                                    LPARAM lparam,
+                                    LRESULT& result,
+                                    DWORD msg_map_id) OVERRIDE;
 
  private:
   friend struct DefaultSingletonTraits<SingletonHwnd>;
 
   SingletonHwnd();
   ~SingletonHwnd();
-
-  // Listener HWND for WM_* notifications.
-  HWND listener_window_;
 
   // List of registered observers.
   ObserverList<Observer> observer_list_;
