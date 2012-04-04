b904f9da378320e9d83e3cb322e19427fa070def
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-04 23:20:14 +0000

    DomStorageBrowserTest sanity check Review URL: https://chromiumcodereview.appspot.com/9956117
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130762 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 23b93682d8e6f1380d90ebdb32ecfd8a28bdc0f3

---
+++ content/browser/dom_storage/dom_storage_browsertest.cc
@@ -0,0 +1,56 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/path_service.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/browser/tab_contents/tab_contents.h"
+#include "content/public/common/content_paths.h"
+#include "net/base/net_util.h"
+#include "webkit/dom_storage/dom_storage_types.h"
+
+// This browser test is aimed towards exercising the DomStorage system
+// from end-to-end.
+class DomStorageBrowserTest : public InProcessBrowserTest {
+ public:
+  DomStorageBrowserTest() {
+    EnableDOMAutomation();
+  }
+
+  GURL GetTestURL(const char* name) {
+    FilePath file_name = FilePath().AppendASCII(name);
+    FilePath dir;
+    PathService::Get(content::DIR_TEST_DATA, &dir);
+    return net::FilePathToFileURL(
+        dir.Append(FILE_PATH_LITERAL("dom_storage")).Append(file_name));
+  }
+
+  void SimpleTest(const GURL& test_url, bool incognito) {
+    // The test page will perform tests then navigate to either
+    // a #pass or #fail ref.
+    Browser* the_browser = incognito ? CreateIncognitoBrowser() : browser();
+    ui_test_utils::NavigateToURLBlockUntilNavigationsComplete(
+        the_browser, test_url, 2);
+    std::string result = the_browser->GetSelectedWebContents()->GetURL().ref();
+    if (result != "pass") {
+      std::string js_result;
+      ASSERT_TRUE(ui_test_utils::ExecuteJavaScriptAndExtractString(
+          the_browser->GetSelectedWebContents()->GetRenderViewHost(), L"",
+          L"window.domAutomationController.send(getLog())", &js_result));
+      FAIL() << "Failed: " << js_result;
+    }
+  }
+};
+
+static const bool kIncognito = true;
+static const bool kNotIncognito = false;
+
+IN_PROC_BROWSER_TEST_F(DomStorageBrowserTest, SanityCheck) {
+  SimpleTest(GetTestURL("sanity_check.html"), kNotIncognito);
+}
+
+IN_PROC_BROWSER_TEST_F(DomStorageBrowserTest, SanityCheckIncognito) {
+  SimpleTest(GetTestURL("sanity_check.html"), kIncognito);
+}
