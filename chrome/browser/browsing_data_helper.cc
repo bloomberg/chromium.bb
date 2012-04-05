2c1b96d5135c9ac463a9b2f725305a534ea36bd4
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 11:39:17 +0000

    BrowsingData::IsValidScheme should respect --enable-file-cookies CLI flag.
    BUG=121998
    TEST=pyauto:databases.DatabasesTest.testDeleteAndUpdateDatabase
    
    Review URL: https://chromiumcodereview.appspot.com/9949006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130895 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6b9f9f4d1f4c4a3a80174793103cca2995395bd5

--- chrome/browser/browsing_data_helper.cc
+++ chrome/browser/browsing_data_helper.cc
@@ -4,7 +4,9 @@
 
 #include "chrome/browser/browsing_data_helper.h"
 
+#include "base/command_line.h"
 #include "base/utf_string_conversions.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/child_process_security_policy.h"
 #include "googleurl/src/gurl.h"
@@ -12,11 +14,21 @@
 
 // Static
 bool BrowsingDataHelper::IsValidScheme(const std::string& scheme) {
-  content::ChildProcessSecurityPolicy* policy =
-      content::ChildProcessSecurityPolicy::GetInstance();
-  return (policy->IsWebSafeScheme(scheme) &&
-          scheme != chrome::kChromeDevToolsScheme &&
-          scheme != chrome::kExtensionScheme);
+  // Special-case `file://` scheme iff cookies and site data are enabled via
+  // the `--allow-file-cookies` CLI flag.
+  if (scheme == chrome::kFileScheme) {
+    return CommandLine::ForCurrentProcess()->HasSwitch(
+        switches::kEnableFileCookies);
+
+  // Otherwise, all "web safe" schemes are valid, except `chrome-extension://`
+  // and `chrome-devtools://`.
+  } else {
+    content::ChildProcessSecurityPolicy* policy =
+        content::ChildProcessSecurityPolicy::GetInstance();
+    return (policy->IsWebSafeScheme(scheme) &&
+            scheme != chrome::kChromeDevToolsScheme &&
+            scheme != chrome::kExtensionScheme);
+  }
 }
 
 // Static
