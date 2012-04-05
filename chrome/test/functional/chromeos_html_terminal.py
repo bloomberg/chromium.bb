b101ef2a754b4d498956fa5d87dfb1d82aaa8c95
tturchetto@google.com <tturchetto@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-05 01:09:38 +0000

    Adding install hterm_terminal test
    BUG=
    TEST=This is a test for installing hterm terminal
    
    Review URL: https://chromiumcodereview.appspot.com/9950003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@130799 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e2557dfcf24d59cfab0fca0580926b708122e180

---
+++ chrome/test/functional/chromeos_html_terminal.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+import pyauto_functional  # must be imported before pyauto
+import pyauto
+
+
+class ChromeosHTMLTerminalTest(pyauto.PyUITest):
+  """Basic tests for ChromeOS HTML Terminal.
+
+  Requires ChromeOS to be logged in.
+  """
+
+  def _GetExtensionInfoById(self, extensions, id):
+    for x in extensions:
+      if x['id'] == id:
+        return x
+    return None
+
+  def testInstallHTMLTerminal(self):
+    """Basic installation test for HTML Terminal on ChromeOS."""
+    crx_file_path = os.path.abspath(
+        os.path.join(self.DataDir(), 'pyauto_private', 'apps',
+                     'hterm-dev-0.7.6.3.crx'))
+    ext_id = self.InstallExtension(crx_file_path)
+    self.assertTrue(ext_id, 'Failed to install extension.')
+    extension = self._GetExtensionInfoById(self.GetExtensionsInfo(), ext_id)
+    self.assertTrue(extension['is_enabled'],
+                    msg='Extension was not enabled on installation.')
+    self.assertFalse(extension['allowed_in_incognito'],
+                     msg='Extension was allowed in incognito on installation.')
+
+
+if __name__ == '__main__':
+  pyauto_functional.Main()
