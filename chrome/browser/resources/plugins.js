1e13ed55bc95a62d8a8b59927d64567d49d7f8f6
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-06 20:50:46 +0000

    JS style nits: plugins.js
    BUG=122002
    TEST=presubmit
    
    Review URL: https://chromiumcodereview.appspot.com/10008060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131182 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0786b97254c7e8ad4760cb98f1539c9cf4979e31

--- chrome/browser/resources/plugins.js
+++ chrome/browser/resources/plugins.js
@@ -7,65 +7,65 @@
  * expects to correctly populate the page.
  */
 var pluginDataFormat = {
-  'plugins': [
+  plugins: [
     {
-      'name': 'Group Name',
-      'description': 'description',
-      'version': 'version',
-      'update_url': 'http://update/',
-      'critical': true,
-      'enabled': true,
-      'plugin_files': [
+      name: 'Group Name',
+      description: 'description',
+      version: 'version',
+      update_url: 'http://update/',
+      critical: true,
+      enabled: true,
+      plugin_files: [
         {
-          'path': '/blahblah/blahblah/MyCrappyPlugin.plugin',
-          'name': 'MyCrappyPlugin',
-          'version': '1.2.3',
-          'description': 'My crappy plugin',
-          'mimeTypes': [
-            { 'description': 'Foo Media',
-              'fileExtensions': ['foo'],
-              'mimeType': 'application/x-my-foo' },
-            { 'description': 'Bar Stuff',
-              'fileExtensions': ['bar', 'baz'],
-              'mimeType': 'application/my-bar' }
+          path: '/blahblah/blahblah/MyCrappyPlugin.plugin',
+          name: 'MyCrappyPlugin',
+          version: '1.2.3',
+          description: 'My crappy plugin',
+          mimeTypes: [
+            { description: 'Foo Media',
+              fileExtensions: ['foo'],
+              mimeType: 'application/x-my-foo' },
+            { description: 'Bar Stuff',
+              fileExtensions: ['bar', 'baz'],
+              mimeType: 'application/my-bar' }
           ],
-          'enabledMode': 'enabledByUser'
+          enabledMode: 'enabledByUser'
         },
         {
-          'path': '/tmp/MyFirst.plugin',
-          'name': 'MyFirstPlugin',
-          'version': '3.14r15926',
-          'description': 'My first plugin',
-          'mimeTypes': [
-            { 'description': 'New Guy Media',
-              'fileExtensions': ['mfp'],
-              'mimeType': 'application/x-my-first' }
+          path: '/tmp/MyFirst.plugin',
+          name: 'MyFirstPlugin',
+          version: '3.14r15926',
+          description: 'My first plugin',
+          mimeTypes: [
+            { description: 'New Guy Media',
+              fileExtensions: ['mfp'],
+              mimeType: 'application/x-my-first' }
           ],
-          'enabledMode': 'enabledByPolicy'
+          enabledMode: 'enabledByPolicy'
         },
         {
-          'path': '/foobar/baz/YourGreatPlugin.plugin',
-          'name': 'YourGreatPlugin',
-          'version': '4.5',
-          'description': 'Your great plugin',
-          'mimeTypes': [
-            { 'description': 'Baz Stuff',
-              'fileExtensions': ['baz'],
-              'mimeType': 'application/x-your-baz' }
+          path: '/foobar/baz/YourGreatPlugin.plugin',
+          name: 'YourGreatPlugin',
+          version: '4.5',
+          description: 'Your great plugin',
+          mimeTypes: [
+            { description: 'Baz Stuff',
+              fileExtensions: ['baz'],
+              mimeType: 'application/x-your-baz' }
           ],
-          'enabledMode': 'disabledByUser'
+          enabledMode: 'disabledByUser'
         },
         {
-          'path': '/foobiz/bar/HisGreatPlugin.plugin',
-          'name': 'HisGreatPlugin',
-          'version': '1.2',
-          'description': 'His great plugin',
-          'mimeTypes': [
-            { 'description': 'More baz Stuff',
-              'fileExtensions': ['bor'],
-              'mimeType': 'application/x-his-bor' }
+          path: '/foobiz/bar/HisGreatPlugin.plugin',
+          name: 'HisGreatPlugin',
+          version: '1.2',
+          description: 'His great plugin',
+          mimeTypes: [
+            { description: 'More baz Stuff',
+              fileExtensions: ['bor'],
+              mimeType: 'application/x-his-bor' }
           ],
-          'enabledMode': 'disabledByPolicy'
+          enabledMode: 'disabledByPolicy'
         }
       ]
     }
@@ -76,12 +76,13 @@ var pluginDataFormat = {
  * Takes the |pluginsData| input argument which represents data about the
  * currently installed/running plugins and populates the html jstemplate with
  * that data. It expects an object structure like the above.
- * @param {Object} pluginsData Detailed info about installed plugins
+ * @param {Object} pluginsData Detailed info about installed plugins. See
+ *     'var pluginDataFormat' above.
  */
 function renderTemplate(pluginsData) {
   // This is the javascript code that processes the template:
   var input = new JsEvalContext(pluginsData);
-  var output = document.getElementById('pluginTemplate');
+  var output = $('pluginTemplate');
   jstProcess(input, output);
 }
 
@@ -97,9 +98,9 @@ function requestPluginsData() {
 
 function loadShowDetailsFromPrefs(show_details) {
   tmiModeExpanded = show_details;
-  document.getElementById('collapse').style.display =
+  $('collapse').style.display =
       show_details ? 'inline' : 'none';
-  document.getElementById('expand').style.display =
+  $('expand').style.display =
       show_details ? 'none' : 'inline';
 
   document.body.className = show_details ? 'show-tmi-mode' : 'hide-tmi-mode';
@@ -108,9 +109,11 @@ function loadShowDetailsFromPrefs(show_details) {
 /**
  * Called by the web_ui_ to re-populate the page with data representing the
  * current state of installed plugins.
+ * @param {Object} pluginsData Detailed info about installed plugins. See
+ *     'var pluginDataFormat' above.
  */
 function returnPluginsData(pluginsData) {
-  var bodyContainer = document.getElementById('body-container');
+  var bodyContainer = $('body-container');
   var body = document.body;
 
   // Set all page content to be visible so we can measure heights.
@@ -176,9 +179,9 @@ function returnPluginsData(pluginsData) {
     slidables[i].style.height = slidables[i].offsetHeight + 'px';
 
   // Reset visibility of page based on the current tmi mode.
-  document.getElementById('collapse').style.display =
+  $('collapse').style.display =
      tmiModeExpanded ? 'inline' : 'none';
-  document.getElementById('expand').style.display =
+  $('expand').style.display =
      tmiModeExpanded ? 'none' : 'inline';
   bodyContainer.style.visibility = 'visible';
   body.className = tmiModeExpanded ?
@@ -187,6 +190,10 @@ function returnPluginsData(pluginsData) {
 
 /**
  * Handles a 'enable' or 'disable' button getting clicked.
+ * @param {HTMLElement} node The HTML element for the plugin being changed.
+ * @param {boolean} enable Whether to enable or disable the plugin.
+ * @param {boolean} isGroup True if we're enabling/disabling a plugin group,
+ *     rather than a single plugin.
  */
 function handleEnablePlugin(node, enable, isGroup) {
   // Tell the C++ PluginsDOMHandler to enable/disable the plugin.
@@ -203,9 +210,9 @@ var tmiModeExpanded = false;
 function toggleTmiMode() {
   tmiModeExpanded = !tmiModeExpanded;
 
-  document.getElementById('collapse').style.display =
+  $('collapse').style.display =
       tmiModeExpanded ? 'inline' : 'none';
-  document.getElementById('expand').style.display =
+  $('expand').style.display =
       tmiModeExpanded ? 'none' : 'inline';
 
   document.body.className =
@@ -219,14 +226,18 @@ function handleSetPluginAlwaysAllowed(el) {
 }
 
 /**
- * Determines whether a plugin's version should be displayed.
+ * @param {Object} plugin An object containing the information about a plugin.
+ *     See 'var pluginDataFormat' above.
+ * @return {boolean} Whether the plugin's version should be displayed.
  */
 function shouldDisplayPluginVersion(plugin) {
   return !!plugin.version && plugin.version != '0';
 }
 
 /**
- * Determines whether a plugin's description should be displayed.
+ * @param {Object} plugin An object containing the information about a plugin.
+ *     See 'var pluginDataFormat' above.
+ * @return {boolean} Whether the plugin's description should be displayed.
  */
 function shouldDisplayPluginDescription(plugin) {
   // Only display the description if it's not blank and if it's not just the
@@ -239,7 +250,9 @@ function shouldDisplayPluginDescription(plugin) {
 }
 
 /**
- * Determines whether a plugin is enabled or not.
+ * @param {Object} plugin An object containing the information about a plugin.
+ *     See 'var pluginDataFormat' above.
+ * @return {boolean} Whether the plugin is enabled.
  */
 function isPluginEnabled(plugin) {
   return plugin.enabledMode == 'enabledByUser' ||
@@ -254,6 +267,6 @@ setInterval(requestPluginsData, 30000);
 document.addEventListener('DOMContentLoaded', requestPluginsData);
 
 // Add handlers to static HTML elements.
-document.getElementById('collapse').onclick = toggleTmiMode;
-document.getElementById('expand').onclick = toggleTmiMode;
-document.getElementById('details-link').onclick = toggleTmiMode;
+$('collapse').onclick = toggleTmiMode;
+$('expand').onclick = toggleTmiMode;
+$('details-link').onclick = toggleTmiMode;
