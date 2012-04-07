73afb58aace2f9d723ad5e2123f4bea171b67526
simon.hong81@gmail.com <simon.hong81@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-07 05:27:16 +0000

    Remove unused forward declaration
    class Task is not used in main_function_params.h
    
    BUG=none
    TEST=compile
    
    Review URL: http://codereview.chromium.org/9956074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131258 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 42070e6f4154aed2dba5fb13d5da925e6864aaa4

--- content/public/common/main_function_params.h
+++ content/public/common/main_function_params.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -24,8 +24,6 @@ class ScopedNSAutoreleasePool;
 }
 }
 #endif
-
-class Task;
 
 namespace content {
 
