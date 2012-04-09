cd3700606b96082a040196a4ff22c7630a6c59db
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-09 18:57:39 +0000

    Fade in the demo login UI's logo and text. This CL fades in the Chrome logo on the demo login UI after 1s and the text fades in, in the 1s after that.
    R=xiyuan@chromium.org
    BUG=chromium-os:28634
    TEST=Tested the fade in screen with the --enable-kiosk-mode flag.
    
    Review URL: https://chromiumcodereview.appspot.com/10024023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131400 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6e575772033fb8ad816b805fbaa95bde2ce18985

--- chrome/browser/resources/chromeos/login/demo_user_login.js
+++ chrome/browser/resources/chromeos/login/demo_user_login.js
@@ -9,6 +9,7 @@
 /**
  * Handles a user clicking anywhere on the screen. This will log the demo user
  * in. Yes, this actually _is the intention.
+ * @param {Event} e The click event that triggered this function.
  */
 onClick = function(e) {
   document.removeEventListener('click', onClick);
@@ -20,6 +21,8 @@ onClick = function(e) {
  * Initializes the click handler.
  */
 initialize = function() {
+  $('logo').style.opacity = 1;
+  $('demo-login-text').style.opacity = 1;
   document.addEventListener('click', onClick);
 };
 
