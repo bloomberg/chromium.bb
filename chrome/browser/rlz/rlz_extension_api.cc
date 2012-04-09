f45ab8ee60bdbd1ee80769a1bfa6a5229098655f
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-09 23:58:38 +0000

    [Coverity] Fix uninitialized member
    CID=103683
    
    
    BUG=
    TEST=
    
    
    Review URL: http://codereview.chromium.org/9959135
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131491 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 42349b2265f38c7495903f2bd0f21895768155eb

--- chrome/browser/rlz/rlz_extension_api.cc
+++ chrome/browser/rlz/rlz_extension_api.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -117,7 +117,9 @@ bool RlzGetAccessPointRlzFunction::RunImpl() {
   return true;
 }
 
-RlzSendFinancialPingFunction::RlzSendFinancialPingFunction() {
+RlzSendFinancialPingFunction::RlzSendFinancialPingFunction()
+    : product_(rlz_lib::CHROME),
+      exclude_machine_id_(true) {
 }
 
 RlzSendFinancialPingFunction::~RlzSendFinancialPingFunction() {
