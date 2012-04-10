c39bfa9b88f6a5060807329b7a7e9efdcc2084b5
dschuff@google.com <dschuff@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-04-10 22:11:26 +0000

    Update UPSTREAM_REV to bring in merge and pushf/popf fix
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2711
    TEST=toolchain bot
    
    Review URL: https://chromiumcodereview.appspot.com/10048007
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8220 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 08c8a2958cffbb59c2b979d92b1f5283c918dab9

--- pnacl/build.sh
+++ pnacl/build.sh
@@ -250,7 +250,7 @@ if ${PNACL_IN_CROS_CHROOT}; then
 fi
 
 # Current milestones in each repo
-readonly UPSTREAM_REV=${UPSTREAM_REV:-4dd3419f60be}
+readonly UPSTREAM_REV=${UPSTREAM_REV:-792304e4f227}
 
 readonly NEWLIB_REV=346ea38d142f
 readonly BINUTILS_REV=f44074cff866
