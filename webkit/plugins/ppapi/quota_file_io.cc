724611368b0dc24908a305a56ec65a8836e9c491
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-11 04:55:27 +0000

    Fix Local Storage Issues. Changes in GURL file system URL parsing cause quota queries to fail. Fix URL handling to work with the new GURL code. BUG=122776 TEST=manual (Native Client Example app from Chrome Webstore)
    Review URL: http://codereview.chromium.org/10033041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131715 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 685415ff649df1ca1f06f2677c9c8fbc7d2bcc97

--- webkit/plugins/ppapi/quota_file_io.cc
+++ webkit/plugins/ppapi/quota_file_io.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -301,7 +301,7 @@ bool QuotaFileIO::RegisterOperationForQuotaChecks(
     // Query the current available space.
     ++outstanding_quota_queries_;
     plugin_delegate->QueryAvailableSpace(
-        GURL(file_url_.path()).GetOrigin(), storage_type_,
+        file_url_.GetOrigin(), storage_type_,
         base::Bind(&QuotaFileIO::DidQueryAvailableSpace,
                    weak_factory_.GetWeakPtr()));
   }
