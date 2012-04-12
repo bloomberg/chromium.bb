67b7b6d63f97dfdbaa4adb92973a2421af8baf60
nduca@google.com <nduca@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-12 20:21:29 +0000

    Fix about:tracing again, due to yet more fallout from adding profiling_view tests.
    TBR=jbates@chromium.org
    BUG=123914
    
    Review URL: https://chromiumcodereview.appspot.com/10069027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132041 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 378f29cc2d3f381a0304bfe176c007a40454a491

--- chrome/browser/resources/tracing.js
+++ chrome/browser/resources/tracing.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,6 +28,7 @@ function onLoad() {
   tracingController = new tracing.TracingController();
 
   profilingView = $('profiling-view');
+  profilingView.tracingController = tracingController;
   cr.ui.decorate(profilingView, tracing.ProfilingView);
 }
 
