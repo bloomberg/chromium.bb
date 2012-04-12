8acbcdeb375fbc668127d2e482be014ea62b7692
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-12 19:14:20 +0000

    Convert chrome_switches ui_test to a browser_test.
    BUG=121574
    Review URL: https://chromiumcodereview.appspot.com/10071017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132029 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3f36ad405b50affcad06ac039b5546524176f585

---
+++ chrome/browser/chrome_switches_browsertest.cc
@@ -0,0 +1,49 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/command_line.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/common/chrome_switches.h"
+#include "chrome/test/base/in_process_browser_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/public/browser/web_contents.h"
+#include "net/test/test_server.h"
+
+class HostRulesTest : public InProcessBrowserTest {
+ protected:
+  HostRulesTest() {
+    EnableDOMAutomation();
+  }
+
+  virtual void SetUpCommandLine(CommandLine* command_line) {
+    ASSERT_TRUE(test_server()->Start());
+
+    // Map all hosts to our local server.
+    std::string host_rule(
+        "MAP * " + test_server()->host_port_pair().ToString());
+    command_line->AppendSwitchASCII(switches::kHostRules, host_rule);
+    // Use no proxy or otherwise this test will fail on a machine that has a
+    // proxy configured.
+    command_line->AppendSwitch(switches::kNoProxyServer);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(HostRulesTest);
+};
+
+IN_PROC_BROWSER_TEST_F(HostRulesTest, TestMap) {
+  // Go to the empty page using www.google.com as the host.
+  GURL local_url = test_server()->GetURL("files/empty.html");
+  GURL test_url(std::string("http://www.google.com") + local_url.path());
+  ui_test_utils::NavigateToURL(browser(), test_url);
+
+  std::string html;
+  EXPECT_TRUE(ui_test_utils::ExecuteJavaScriptAndExtractString(
+      browser()->GetSelectedWebContents()->GetRenderViewHost(),
+      L"",
+      L"window.domAutomationController.send(document.body.outerHTML);",
+      &html));
+
+  EXPECT_STREQ("<body></body>", html.c_str());
+}
