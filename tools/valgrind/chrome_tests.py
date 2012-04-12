3aa74c28c44bc361e7670278c37588acfecbcae6
eugenis@google.com <eugenis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-12 08:15:42 +0000

    Increase tiny test timeout under Valgrind.
    TEST=may or may not fix flaky failures of WorkerPoolTest.PostTaskAndReply test.
    BUG=None
    Review URL: https://chromiumcodereview.appspot.com/10053010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131950 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: db4b5bed67c0fcd0d6085363838512cba12e2854

--- tools/valgrind/chrome_tests.py
+++ tools/valgrind/chrome_tests.py
@@ -210,6 +210,7 @@ class ChromeTests:
     tool = valgrind_test.CreateTool(self._options.valgrind_tool)
     cmd = self._DefaultCommand(tool, name, valgrind_test_args)
     self._AppendGtestFilter(tool, name, cmd)
+    cmd.extend(['--test-tiny-timeout=1000'])
     if cmd_args:
       cmd.extend(cmd_args)
 
