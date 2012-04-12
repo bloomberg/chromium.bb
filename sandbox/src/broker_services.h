08f222f1559b65af6337e9133a5f0b1ffb197799
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-12 06:40:06 +0000

    Revert 131940 - Add sandbox support for associating peer processes
    TEST=HandlePolicyTest.DuplicatePeerHandle
    
    Review URL: http://codereview.chromium.org/9960045
    
    TBR=jschuh@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10065007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@131944 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5562b3fb0808890238ac69d6885ed9717813072c

--- sandbox/src/broker_services.h
+++ sandbox/src/broker_services.h
@@ -6,23 +6,14 @@
 #define SANDBOX_SRC_BROKER_SERVICES_H__
 
 #include <list>
-#include <map>
 #include <set>
 #include "base/basictypes.h"
-#include "base/win/scoped_handle.h"
 #include "sandbox/src/crosscall_server.h"
 #include "sandbox/src/job.h"
 #include "sandbox/src/sandbox.h"
 #include "sandbox/src/sharedmem_ipc_server.h"
 #include "sandbox/src/win2k_threadpool.h"
 #include "sandbox/src/win_utils.h"
-
-namespace {
-
-struct JobTracker;
-struct PeerTracker;
-
-}  // namespace
 
 namespace sandbox {
 
@@ -54,8 +45,6 @@ class BrokerServicesBase : public BrokerServices,
 
   virtual ResultCode WaitForAllTargets();
 
-  virtual ResultCode AddTargetPeer(HANDLE peer_process);
-
   // Checks if the supplied process ID matches one of the broker's active
   // target processes
   // Returns:
@@ -63,6 +52,16 @@ class BrokerServicesBase : public BrokerServices,
   bool IsActiveTarget(DWORD process_id);
 
  private:
+  // Helper structure that allows the Broker to associate a job notification
+  // with a job object and with a policy.
+  struct JobTracker {
+    HANDLE job;
+    PolicyBase* policy;
+    JobTracker(HANDLE cjob, PolicyBase* cpolicy)
+        : job(cjob), policy(cpolicy) {
+     }
+  };
+
   // Releases the Job and notifies the associated Policy object to its
   // resources as well.
   static void FreeResources(JobTracker* tracker);
@@ -70,9 +69,6 @@ class BrokerServicesBase : public BrokerServices,
   // The routine that the worker thread executes. It is in charge of
   // notifications and cleanup-related tasks.
   static DWORD WINAPI TargetEventsThread(PVOID param);
-
-  // Removes a target peer from the process list if it expires.
-  static VOID CALLBACK RemovePeerData(PVOID parameter, BOOLEAN);
 
   // The completion port used by the job objects to communicate events to
   // the worker thread.
@@ -95,11 +91,6 @@ class BrokerServicesBase : public BrokerServices,
   // List of the trackers for closing and cleanup purposes.
   typedef std::list<JobTracker*> JobTrackerList;
   JobTrackerList tracker_list_;
-
-  // Maps peer process IDs to the saved handle and wait event.
-  // Prevents peer callbacks from accessing the broker after destruction.
-  typedef std::map<DWORD, PeerTracker*> PeerTrackerMap;
-  PeerTrackerMap peer_map_;
 
   // Provides a fast lookup to identify sandboxed processes.
   std::set<DWORD> child_process_ids_;
--- sandbox/tests/integration_tests/integration_tests.cc
+++ sandbox/tests/integration_tests/integration_tests.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,8 +7,7 @@
 
 int wmain(int argc, wchar_t **argv) {
   if (argc >= 2) {
-    if (0 == _wcsicmp(argv[1], L"-child") ||
-        0 == _wcsicmp(argv[1], L"-child-no-sandbox"))
+    if (0 == _wcsicmp(argv[1], L"-child"))
       // This instance is a child, not the test.
       return sandbox::DispatchCall(argc, argv);
   }
