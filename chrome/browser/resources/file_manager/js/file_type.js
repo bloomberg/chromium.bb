d2b88e17e850db41799e58d12676a0b16be8e134
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-12 17:00:23 +0000

    Fxing sort by file type.
    BUG=chromium-os:29118
    TEST=Manual test.
    
    
    Review URL: http://codereview.chromium.org/10069013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132000 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 83a072aa7f45bd4fc5c4ecbfa0fd565b3fe5f648

--- chrome/browser/resources/file_manager/js/file_type.js
+++ chrome/browser/resources/file_manager/js/file_type.js
@@ -7,6 +7,10 @@
  */
 var FileType = {};
 
+/**
+ * Description of known file types.
+ * Pair type-subtype defines order when sorted by file type.
+ */
 FileType.types = [
   // Images
   {type: 'image', name: 'IMAGE_FILE_TYPE', subtype: 'JPEG',
@@ -59,42 +63,45 @@ FileType.types = [
    pattern: /\.log$/i},
 
   // Archive
-  {type: 'archive', name: 'ZIP_ARCHIVE_FILE_TYPE', subtype:'ZIP',
+  {type: 'archive', name: 'ZIP_ARCHIVE_FILE_TYPE', subtype: 'ZIP',
    pattern: /\.zip$/i},
-  {type: 'archive', name: 'RAR_ARCHIVE_FILE_TYPE', subtype:'RAR',
+  {type: 'archive', name: 'RAR_ARCHIVE_FILE_TYPE', subtype: 'RAR',
    pattern: /\.rar$/i},
-  {type: 'archive', name: 'TAR_ARCHIVE_FILE_TYPE', subtype:'TAR',
+  {type: 'archive', name: 'TAR_ARCHIVE_FILE_TYPE', subtype: 'TAR',
    pattern: /\.tar$/i},
-  {type: 'archive', name: 'TAR_BZIP2_ARCHIVE_FILE_TYPE', subtype:'TBZ2',
+  {type: 'archive', name: 'TAR_BZIP2_ARCHIVE_FILE_TYPE', subtype: 'TBZ2',
    pattern: /\.(tar.bz2|tbz|tbz2)$/i},
-  {type: 'archive', name: 'TAR_GZIP_ARCHIVE_FILE_TYPE', subtype:'TGZ',
+  {type: 'archive', name: 'TAR_GZIP_ARCHIVE_FILE_TYPE', subtype: 'TGZ',
    pattern: /\.(tar.|t)gz$/i},
 
   // Hosted docs.
   {type: 'hosted', icon: 'gdoc', name: 'GDOC_DOCUMENT_FILE_TYPE',
-   pattern: /\.gdoc$/i},
+   subtype: 'doc', pattern: /\.gdoc$/i},
   {type: 'hosted', icon: 'gsheet', name: 'GSHEET_DOCUMENT_FILE_TYPE',
-   pattern: /\.gsheet$/i},
+   subtype: 'sheet', pattern: /\.gsheet$/i},
   {type: 'hosted', icon: 'gslides', name: 'GSLIDES_DOCUMENT_FILE_TYPE',
-   pattern: /\.gslides$/i},
+   subtype: 'slides', pattern: /\.gslides$/i},
   {type: 'hosted', icon: 'gdraw', name: 'GDRAW_DOCUMENT_FILE_TYPE',
-   pattern: /\.gdraw$/i},
+   subtype: 'draw', pattern: /\.gdraw$/i},
   {type: 'hosted', icon: 'gtable', name: 'GTABLE_DOCUMENT_FILE_TYPE',
-   pattern: /\.gtable$/i},
+   subtype: 'table', pattern: /\.gtable$/i},
 
   // Others
   {type: 'document', icon: 'pdf', name: 'PDF_DOCUMENT_FILE_TYPE',
-   pattern: /\.pdf$/i},
+   subtype: 'PDF', pattern: /\.pdf$/i},
   {type: 'document', icon: 'html', name: 'HTML_DOCUMENT_FILE_TYPE',
-   pattern: /\.(html?|mht|mhtml)$/i},
+   subtype: 'HTML', pattern: /\.(html?|mht|mhtml)$/i},
   {type: 'document', icon: 'word', name: 'WORD_DOCUMENT_FILE_TYPE',
-   pattern: /\.(doc|docx)$/i},
+   subtype: 'Word', pattern: /\.(doc|docx)$/i},
   {type: 'document', icon: 'ppt', name: 'POWERPOINT_PRESENTATION_FILE_TYPE',
-   pattern: /\.(ppt|pptx)$/i},
+   subtype: 'PPT', pattern: /\.(ppt|pptx)$/i},
   {type: 'document', icon: 'excel', name: 'EXCEL_FILE_TYPE',
-   pattern: /\.(xls|xlsx)$/i}
+   subtype: 'Excel', pattern: /\.(xls|xlsx)$/i}
 ];
 
+/**
+ * Map from file type to preview art.
+ */
 FileType.previewArt = {
   'audio': 'images/filetype_large_audio.png',
   'folder': 'images/filetype_large_folder.png',
@@ -106,7 +113,7 @@ FileType.previewArt = {
 /**
  * Get the file type object that matches a given url.
  *
- * @param {string} url
+ * @param {string} url Reference to the file.
  * @return {Object} The matching file type object or an empty object.
  */
 FileType.getType = function(url) {
@@ -122,7 +129,7 @@ FileType.getType = function(url) {
 /**
  * Get the media type for a given url.
  *
- * @param {string} url
+ * @param {string} url Reference to the file.
  * @return {string} The value of 'type' property from one of the elements in
  *   FileType.types or undefined.
  */
@@ -130,21 +137,39 @@ FileType.getMediaType = function(url) {
   return FileType.getType(url).type;
 };
 
-/*
- * Helper functions useful with Array.filter().
+/**
+ * True if the url refers an audio file.
+ * @param {string} url Reference to the file.
+ * @return {boolean} True if audio.
  */
 FileType.isAudio = function(url) {
   return FileType.getMediaType(url) == 'audio';
 };
 
+/**
+ * True if the url refers an image file.
+ * @param {string} url Reference to the file.
+ * @return {boolean} True if image.
+ */
 FileType.isImage = function(url) {
   return FileType.getMediaType(url) == 'image';
 };
 
+/**
+ * True if the url refers a video file.
+ * @param {string} url Reference to the file.
+ * @return {boolean} True if video.
+ */
 FileType.isVideo = function(url) {
   return FileType.getMediaType(url) == 'video';
 };
 
+
+/**
+ * Files with more pixels won't have preview.
+ * @param {string} url Reference to the file.
+ * @return {boolean} True if image or video.
+ */
 FileType.isImageOrVideo = function(url) {
   var type = FileType.getMediaType(url);
   return type == 'image' || type == 'video';
@@ -153,14 +178,21 @@ FileType.isImageOrVideo = function(url) {
 /**
  * Get the preview url for a given type.
  *
- * @param {string} type
- * @return {string}
+ * @param {string} type File type from FileType.types.
+ * @return {string} Path to preview art.
  */
 FileType.getPreviewArt = function(type) {
   return FileType.previewArt[type] || FileType.previewArt['unknown'];
 };
 
+/**
+ * Files with more pixels won't have preview.
+ */
 FileType.MAX_PREVIEW_PIXEL_COUNT = 1 << 21; // 2 MPix
+
+/**
+ * Files of bigger size won't have preview.
+ */
 FileType.MAX_PREVIEW_FILE_SIZE = 1 << 20; // 1 Mb
 
 /**
@@ -168,14 +200,14 @@ FileType.MAX_PREVIEW_FILE_SIZE = 1 << 20; // 1 Mb
  * the image itself as a thumbnail. If the image is too large it hurts
  * the performance very much so we allow it only for moderately sized files.
  *
- * @param {Object} metadata
+ * @param {Object} metadata From MetadataProvider.
  * @param {number} opt_size The file size to be used if the metadata does not
  *   contain fileSize.
  * @return {boolean} Whether it is OK to use the image url for a preview.
  */
 FileType.canUseImageUrlForPreview = function(metadata, opt_size) {
   var fileSize = metadata.fileSize || opt_size;
-  return ((fileSize && fileSize <= FileType.MAX_PREVIEW_FILE_SIZE)  ||
+  return ((fileSize && fileSize <= FileType.MAX_PREVIEW_FILE_SIZE) ||
       (metadata.width && metadata.height &&
       (metadata.width * metadata.height <= FileType.MAX_PREVIEW_PIXEL_COUNT)));
 };
