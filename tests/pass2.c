ed730bd592678c9562e25e5933ba7dbf157b96e5
Christian Egli <christian.egli@alumni.ethz.ch>
2012-04-13 12:33:51 +0000

    Simple change to factor the table name into a variable

--- tests/pass2.c
+++ tests/pass2.c
@@ -7,13 +7,14 @@ int
 main(int argc, char **argv)
 {
 
+  const char* table = "pass2.ctb";
   int result = 0;
 
   /* First check a plain word to see if the table works */
-  result = check_translation("pass2.ctb", "Rene", NULL, "rene");
+  result = check_translation(table, "Rene", NULL, "rene");
 
   /* then try a word which uses pass2 */
-  result |= check_translation("pass2.ctb", "Reno", NULL, "ren'o");
+  result |= check_translation(table, "Reno", NULL, "ren'o");
 
   return result;
 }
