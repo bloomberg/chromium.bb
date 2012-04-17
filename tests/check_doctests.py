997f044533b8ae9e8c7eb202222f858a902935ce
Christian Egli <christian.egli@alumni.ethz.ch>
2012-04-17 09:35:31 +0000

    Add a test helper to simplify the writing of doctests.

--- tests/check_doctests.py
+++ tests/check_doctests.py
@@ -2,10 +2,27 @@
 import doctest
 import glob
 import sys
+import louis
+
+class TestHelper():
+
+    def __init__(self, tables):
+        self.tables = tables
+
+    def braille(self, txt):
+        return louis.translateString(self.tables, txt)
+
+    def cursor(self, txt, cursorPos):
+        return louis.translate(self.tables, txt, 
+                               cursorPos=cursorPos, 
+                               mode=louis.compbrlAtCursor)[0:4:3]
+
 
 exit_value = 0
 for test in glob.iglob('doctests/*_test.txt'):
-    failure_count, ignore = doctest.testfile(test, report=True, encoding='utf-8')
+    failure_count, ignore = doctest.testfile(
+        test, report=True, encoding='utf-8',
+        extraglobs={'TestHelper': TestHelper})
     if failure_count > 0: 
         exit_value = 1
 
