638676d87dd811a005ef10be4ba38922403c682e
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-18 02:14:42 +0000

    SPDY - replaced SpdySettings (list) with SettingsMap
    CreateSettings accepts SettingsMap which enforces
    that there are no duplicate settings.
    
    Deleted unused class SpdySettingsStorage.
    
    In net-internals, log setting's id, flags and value for
    SpdySettings.
    
    BUG=119752
    R=rch@chromium.org,hkhalil@google.com
    TEST=network unit tests
    
    Review URL: http://codereview.chromium.org/10054034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132722 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 18b28abfc0a6afc32d44bafeff5caf4db80d0524

--- net/tools/flip_server/spdy_interface.cc
+++ net/tools/flip_server/spdy_interface.cc
@@ -293,9 +293,9 @@ void SpdySM::ResetForNewConnection() {
 
 // Send a settings frame
 int SpdySM::PostAcceptHook() {
-  SpdySettings settings;
-  SettingsFlagsAndId settings_id(0, SETTINGS_MAX_CONCURRENT_STREAMS);
-  settings.push_back(SpdySetting(settings_id, 100));
+  SettingsMap settings;
+  settings[SETTINGS_MAX_CONCURRENT_STREAMS] =
+      SettingsFlagsAndValue(SETTINGS_FLAG_NONE, 100);
   SpdySettingsControlFrame* settings_frame =
       buffered_spdy_framer_->CreateSettings(settings);
 
