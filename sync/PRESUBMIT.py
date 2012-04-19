02733ebc4ea6e52fc33441df396d43c8414afe78
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-19 04:22:55 +0000

    Add PRESUBMIT script for src/sync directory
    BUG=123912
    TEST=
    
    
    Review URL: http://codereview.chromium.org/10041037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@132946 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ec87ae3af6aa280b7906644c3b0616e6cdb8f8f5

---
+++ sync/PRESUBMIT.py
@@ -0,0 +1,16 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Chromium presubmit script for src/sync.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details on the presubmit API built into gcl.
+"""
+
+def GetPreferredTrySlaves():
+  return [
+    'linux_rel:sync_integration_tests',
+    'mac_rel:sync_integration_tests',
+    'win_rel:sync_integration_tests',
+  ]
