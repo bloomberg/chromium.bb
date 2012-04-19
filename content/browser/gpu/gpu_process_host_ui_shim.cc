80ec8740a837a988bdf5814e0e370e60c50349be
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-19 19:23:48 +0000

    Add a message to about:gpu when the gpu process crashes.
    R=apatrick@chromium.org
    CC=davemoore@chromium.org
    
    
    Review URL: http://codereview.chromium.org/10116036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133035 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 92f2286d1323d0c4ae2f1bc97ef263c062368d69

--- content/browser/gpu/gpu_process_host_ui_shim.cc
+++ content/browser/gpu/gpu_process_host_ui_shim.cc
@@ -179,6 +179,12 @@ void GpuProcessHostUIShim::SimulateHang() {
 GpuProcessHostUIShim::~GpuProcessHostUIShim() {
   DCHECK(CalledOnValidThread());
   g_hosts_by_id.Pointer()->Remove(host_id_);
+
+  DictionaryValue* dict = new DictionaryValue();
+  dict->SetInteger("level", logging::LOG_ERROR);
+  dict->SetString("header", "GpuProcessHostUIShim");
+  dict->SetString("message", "GPU Process Crashed.");
+  GpuDataManagerImpl::GetInstance()->AddLogMessage(dict);
 }
 
 bool GpuProcessHostUIShim::OnControlMessageReceived(
