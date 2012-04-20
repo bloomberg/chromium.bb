61b885a16469d1410255a03ee4caf8c2f3c966ed
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 23:10:22 +0000

    Fix bug 124426: Attached panels lost the ability to be resized on Windows
    The fix is to set full size when the system resizing is ended.
    
    BUG=124426
    TEST=Manual test by resizing docked panels to verify
    
    
    Review URL: http://codereview.chromium.org/10166010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133297 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2fdceb2533b8139dc1c52448691504bd9c1999bf

--- chrome/browser/ui/panels/panel_browser_view.cc
+++ chrome/browser/ui/panels/panel_browser_view.cc
@@ -290,6 +290,7 @@ void PanelBrowserView::OnWindowBeginUserBoundsChange() {
 
 void PanelBrowserView::OnWindowEndUserBoundsChange() {
   bounds_ = GetBounds();
+  panel_->set_full_size(bounds_.size());
   panel_->SetPreviewMode(false);
   panel_->panel_strip()->RefreshLayout();
 }
