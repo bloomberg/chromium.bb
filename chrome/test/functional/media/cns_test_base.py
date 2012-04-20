05ef0dc44d7d434acd9a10b7fda49f330f8779cd
shadi@chromium.org <shadi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 03:07:08 +0000

    Create a cns_test_base to be used as base for cns perf tests.
    BUG=122460
    TEST=ran media_constrained_network_perf.py
    
    
    Review URL: http://codereview.chromium.org/9949066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133137 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9bbb5c279cb33ee4ce45c8fe0c03e022f158a159

---
+++ chrome/test/functional/media/cns_test_base.py
@@ -0,0 +1,141 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Constrained network server (CNS) test base."""
+
+import logging
+import os
+import Queue
+import subprocess
+import sys
+import threading
+import urllib2
+
+import pyauto
+import pyauto_paths
+
+# List of commonly used network constraints settings.
+LowHighMedium = ('LowHighMedium', [256, 180, 2])
+LowMediumNone = ('LowMediumNone', [256, 105, 0])
+MediumMediumNone = ('MediumMediumNone', [2000, 105, 0])
+HighMediumNone = ('HighMediumNone', [5000, 105, 0])
+HighLowNone = ('HighLowNone', [5000, 43, 0])
+Wifi = ('Wifi_1Mbps_60ms', [1024, 60, 0])
+DSL = ('DSL_1.5Mbps_50ms', [1541, 50, 0])
+Cable = ('Cable_5Mbps_28ms', [5120, 28, 0])
+NoConstraints = ('NoConstraints', [0, 0, 0])
+
+# Path to CNS executable relative to source root.
+_CNS_PATH = os.path.join(
+    'media', 'tools', 'constrained_network_server', 'cns.py')
+
+# Port to start the CNS on.
+_CNS_PORT = 9000
+
+# Base CNS URL, only requires & separated parameter names appended.
+CNS_BASE_URL = 'http://127.0.0.1:%d/ServeConstrained?' % _CNS_PORT
+
+# Used for server sanity check.
+_TEST_VIDEO = 'roller.webm'
+
+
+class CNSTestBase(pyauto.PyUITest):
+  """CNS test base hadles startup and teardown of CNS server."""
+
+  def setUp(self):
+    """Starts the Constrained Network Server (CNS)."""
+    cmd = [sys.executable, os.path.join(pyauto_paths.GetSourceDir(), _CNS_PATH),
+           '--port', str(_CNS_PORT),
+           '--interface', 'lo',
+           '--www-root', os.path.join(
+               self.DataDir(), 'pyauto_private', 'media'),
+           '-v',
+           '--expiry-time', '0']
+
+    self._cns_process = subprocess.Popen(cmd, stderr=subprocess.PIPE)
+    ProcessLogger(self._cns_process)
+
+    if self.WaitUntil(self._CanAccessServer, retry_sleep=3, timeout=30,
+                      debug=False):
+      pyauto.PyUITest.setUp(self)
+    else:
+      self.tearDown()
+      self.fail('Failed to start CNS.')
+
+  def _CanAccessServer(self):
+    """Checks if the CNS server can serve a file with no network constraints."""
+    test_url = ''.join([CNS_BASE_URL, 'f=', _TEST_VIDEO])
+    try:
+      return urllib2.urlopen(test_url) is not None
+    except Exception:
+      return False
+
+  def tearDown(self):
+    """Stops the Constrained Network Server (CNS)."""
+    pyauto.PyUITest.tearDown(self)
+    logging.debug('Stopping CNS server.')
+    # Do not use process.kill(), it will not clean up cns.
+    self.Kill(self._cns_process.pid)
+    # Need to wait since the process logger has a lock on the process stderr.
+    self._cns_process.wait()
+    self.assertFalse(self._cns_process.returncode is None)
+    logging.debug('CNS server stopped.')
+
+
+class ProcessLogger(threading.Thread):
+  """A thread to log a process's stderr output."""
+
+  def __init__(self, process):
+    """Starts the process logger thread.
+
+    Args:
+      process: The process to log.
+    """
+    threading.Thread.__init__(self)
+    self._process = process
+    self.start()
+
+  def run(self):
+    """Adds debug statements for the process's stderr output."""
+    line = True
+    while line:
+      line = self._process.stderr.readline()
+      logging.debug(line.strip())
+
+
+def GetFileURL(file_name, bandwidth=0, latency=0, loss=0, new_port=False):
+  """Returns CNS URL for the file with specified constraints.
+
+  Args:
+    Check cns.ServeConstrained() args for more details.
+  """
+  video_url = [CNS_BASE_URL, 'f=' + file_name]
+  if bandwidth > 0:
+    video_url.append('bandwidth=%d' % bandwidth)
+  if latency > 0:
+    video_url.append('latency=%d' % latency)
+  if loss > 0:
+    video_url.append('loss=%d' % loss)
+  if new_port:
+    video_url.append('new_port=%s' % new_port)
+  return '&'.join(video_url)
+
+
+def CreateCNSPerfTasks(network_constraints_settings, test_media_files):
+  """Returns a queue of tasks combinining network constrains with media files.
+
+  Args:
+    network_constraints_settings: List of (setting_name, setting_values)
+        tupples.
+    test_media_files: List of media files to run the tests on.
+  """
+  # Convert relative test path into an absolute path.
+  tasks = Queue.Queue()
+  for file_name in test_media_files:
+    for series_name, settings in network_constraints_settings:
+      logging.debug('Add test: %s\tSettings: %s\tMedia: %s', series_name,
+                    settings, file_name)
+      tasks.put((series_name, settings, file_name))
+
+  return tasks
--- chrome/test/functional/media/media_constrained_network_perf.py
+++ chrome/test/functional/media/media_constrained_network_perf.py
@@ -17,19 +17,15 @@ into PyAuto are necessary.  The number of threads can be set by _TEST_THREADS.
 The CNS code is located under: <root>/src/media/tools/constrained_network_server
 """
 
-import itertools
 import logging
 import os
 import Queue
-import subprocess
-import sys
-import threading
-import urllib2
 
 import pyauto_media
-import pyauto
-import pyauto_paths
 import pyauto_utils
+
+import cns_test_base
+import worker_thread
 
 # The list of tests to run. Each entry is of the form:
 #    'TEST_NAME' : [BANDWIDTH_Kbps, LATENCY_ms, PACKET_LOSS_%]
@@ -54,17 +50,12 @@ import pyauto_utils
 #
 # TODO(shadi): After recording metrics and getting stable results, remove the
 # first 5 settings if the last five are enough for regression testing.
-_TESTS_TO_RUN = {
-    'LowHighMedium': [256, 180, 2],
-    'LowMediumNone': [256, 105, 0],
-    'MediumMediumNone': [2000, 105, 0],
-    'HighMediumNone': [5000, 105, 0],
-    'HighLowNone': [5000, 43, 0],
-    'Wifi_1Mbps_60ms': [1024, 60, 0],
-    'DSL_1.5Mbps_50ms': [1541, 50, 0],
-    'Cable_5Mbps_28ms': [5120, 28, 0],
-    'NoConstraints': [0, 0, 0]
-}
+_TESTS_TO_RUN = [cns_test_base.Cable,
+                 cns_test_base.Wifi,
+                 cns_test_base.DSL,
+                 cns_test_base.LowHighMedium,
+                 cns_test_base.LowMediumNone,
+                 cns_test_base.NoConstraints]
 
 # HTML test path; relative to src/chrome/test/data.  Loads a test video and
 # records metrics in JavaScript.
@@ -79,19 +70,8 @@ _TEST_ITERATIONS = 3
 
 # File name of video to collect metrics for and its duration (used for timeout).
 # TODO(dalecurtis): Should be set on the command line.
-_TEST_VIDEO = 'roller.webm'
+_TEST_VIDEOS = ['roller.webm']
 _TEST_VIDEO_DURATION_SEC = 28.53
-
-
-# Path to CNS executable relative to source root.
-_CNS_PATH = os.path.join(
-    'media', 'tools', 'constrained_network_server', 'cns.py')
-
-# Port to start the CNS on.
-_CNS_PORT = 9000
-
-# Base CNS URL, only requires & separated parameter names appended.
-_CNS_BASE_URL = 'http://127.0.0.1:%d/ServeConstrained?' % _CNS_PORT
 
 
 def Median(values):
@@ -107,38 +87,15 @@ def Median(values):
     return (float(lower + upper)) / 2
 
 
-class TestWorker(threading.Thread):
-  """Worker thread.  For each queue entry: opens tab, runs test, closes tab."""
+class CNSWorkerThread(worker_thread.WorkerThread):
+  """Worker thread.  Runs a test for each task in the queue."""
 
-  # Atomic, monotonically increasing task identifier.  Used to ID tabs.
-  _task_id = itertools.count()
-
-  def __init__(self, pyauto_test, tasks, automation_lock, url):
-    """Sets up TestWorker class variables.
-
-    Args:
-      pyauto_test: Reference to a pyauto.PyUITest instance.
-      tasks: Queue containing (settings, name) tuples.
-      automation_lock: Global automation lock for pyauto calls.
-      url: File URL to HTML/JavaScript test code.
-    """
-    threading.Thread.__init__(self)
-    self._tasks = tasks
-    self._automation_lock = automation_lock
-    self._pyauto = pyauto_test
-    self._url = url
+  def __init__(self, *args, **kwargs):
+    """Sets up CNSWorkerThread class variables."""
+    # Allocate local vars before WorkerThread.__init__ runs the thread.
     self._metrics = {}
-    self.fail_count = 0
-    self.start()
-
-  def _FindTabLocked(self, url):
-    """Returns the tab index for the tab belonging to this url.
-
-    self._automation_lock must be owned by caller.
-    """
-    for tab in self._pyauto.GetBrowserInfo()['windows'][0]['tabs']:
-      if tab['url'] == url:
-        return tab['index']
+    self._test_iterations = _TEST_ITERATIONS
+    worker_thread.WorkerThread.__init__(self, *args, **kwargs)
 
   def _HaveMetricOrError(self, var_name, unique_url):
     """Checks if the page has variable value ready or if an error has occured.
@@ -152,11 +109,8 @@ class TestWorker(threading.Thread):
     Returns:
       True is the var_name value is >=0 or if an error_msg exists.
     """
-    with self._automation_lock:
-      tab = self._FindTabLocked(unique_url)
-      self._metrics[var_name] = int(self._pyauto.GetDOMValue(var_name,
-                                                             tab_index=tab))
-      end_test = self._pyauto.GetDOMValue('endTest', tab_index=tab)
+    self._metrics[var_name] = int(self.GetDOMValue(var_name, url=unique_url))
+    end_test = self.GetDOMValue('endTest', url=unique_url)
 
     return self._metrics[var_name] >= 0 or end_test
 
@@ -166,9 +120,7 @@ class TestWorker(threading.Thread):
     Args:
       unique_url: The url of the page identifying the test.
     """
-    with self._automation_lock:
-      tab = self._FindTabLocked(unique_url)
-      return self._pyauto.GetDOMValue('eventsMsg', tab_index=tab)
+    return self.GetDOMValue('eventsMsg', url=unique_url)
 
   def _GetVideoProgress(self, unique_url):
     """Gets the video's current play progress percentage.
@@ -176,209 +128,94 @@ class TestWorker(threading.Thread):
     Args:
       unique_url: The url of the page to check for video play progress.
     """
-    with self._automation_lock:
-      return int(self._pyauto.CallJavascriptFunc(
-          'calculateProgress', tab_index=self._FindTabLocked(unique_url)))
+    return int(self.CallJavascriptFunc('calculateProgress', url=unique_url))
 
-  def run(self):
-    """Opens tab, starts HTML test, and records metrics for each queue entry.
+  def RunTask(self, unique_url, task):
+    """Runs the specific task on the url given.
 
-    No exception handling is done to make sure the main thread exits properly
-    during Chrome crashes or other failures.  Doing otherwise has the potential
-    to leave the CNS server running in the background.
-
-    For a clean shutdown, put the magic exit value (None, None) in the queue.
+    It is assumed that a tab with the unique_url is already loaded.
+    Args:
+      unique_url: A unique identifier of the test page.
+      task: A (series_name, settings, file_name) tuple to run the test on.
+    Returns:
+      True if the tests run as expected.
     """
-    while True:
-      series_name, settings = self._tasks.get()
+    ttp_results = []
+    epp_results = []
+    # Build video source URL.  Values <= 0 mean the setting is disabled.
+    series_name, settings, file_name = task
+    video_url = cns_test_base.GetFileURL(
+        file_name, bandwidth=settings[0], latency=settings[1],
+        loss=settings[2], new_port=True)
 
-      # Check for magic exit values.
-      if (series_name, settings) == (None, None):
-        break
+    for iter_num in xrange(self._test_iterations):
+      # Start the test!
+      self.CallJavascriptFunc('startTest', [video_url], url=unique_url)
 
-      # Build video source URL.  Values <= 0 mean the setting is disabled.
-      video_url = [_CNS_BASE_URL, 'f=' + _TEST_VIDEO]
-      if settings[0] > 0:
-        video_url.append('bandwidth=%d' % settings[0])
-      if settings[1] > 0:
-        video_url.append('latency=%d' % settings[1])
-      if settings[2] > 0:
-        video_url.append('loss=%d' % settings[2])
-      video_url.append('new_port=true')
-      video_url = '&'.join(video_url)
+      # Wait until the necessary metrics have been collected.
+      self._metrics['epp'] = self._metrics['ttp'] = -1
+      self.WaitUntil(self._HaveMetricOrError, args=['ttp', unique_url],
+                     retry_sleep=1, timeout=10, debug=False)
+      # Do not wait for epp if ttp is not available.
+      if self._metrics['ttp'] >= 0:
+        ttp_results.append(self._metrics['ttp'])
+        self.WaitUntil(
+            self._HaveMetricOrError, args=['epp', unique_url], retry_sleep=2,
+            timeout=_TEST_VIDEO_DURATION_SEC * 10, debug=False)
 
-      ttp_results = []
-      epp_results = []
-      for iter_num in xrange(_TEST_ITERATIONS):
-        # Make the test URL unique so we can figure out our tab index later.
-        unique_url = '%s?%d' % (self._url, TestWorker._task_id.next())
-        # Start the test!
-        with self._automation_lock:
-          self._pyauto.AppendTab(pyauto.GURL(unique_url))
-          self._pyauto.CallJavascriptFunc(
-              'startTest', [video_url],
-              tab_index=self._FindTabLocked(unique_url))
+        if self._metrics['epp'] >= 0:
+          epp_results.append(self._metrics['epp'])
 
-        # Wait until the necessary metrics have been collected. Okay to not lock
-        # here since pyauto.WaitUntil doesn't call into Chrome.
-        self._metrics['epp'] = self._metrics['ttp'] = -1
-        self._pyauto.WaitUntil(
-            self._HaveMetricOrError, args=['ttp', unique_url], retry_sleep=1,
-            timeout=10, debug=False)
-
-        # Do not wait for epp if ttp is not available.
-        if self._metrics['ttp'] >= 0:
-          ttp_results.append(self._metrics['ttp'])
-          self._pyauto.WaitUntil(
-              self._HaveMetricOrError, args=['epp', unique_url], retry_sleep=2,
-              timeout=_TEST_VIDEO_DURATION_SEC * 10, debug=False)
-
-          if self._metrics['epp'] >= 0:
-            epp_results.append(self._metrics['epp'])
-
-          logging.debug('Iteration:%d - Test %s ended with %d%% of the video '
-                        'played.', iter_num, series_name,
-                        self._GetVideoProgress(unique_url),)
+        logging.debug('Iteration:%d - Test %s ended with %d%% of the video '
+                      'played.', iter_num, series_name,
+                      self._GetVideoProgress(unique_url),)
 
         if self._metrics['ttp'] < 0 or self._metrics['epp'] < 0:
           logging.error('Iteration:%d - Test %s failed to end gracefully due '
                         'to time-out or error.\nVideo events fired:\n%s',
                         iter_num, series_name, self._GetEventsLog(unique_url))
 
-        # Close the tab.
-        with self._automation_lock:
-          self._pyauto.GetBrowserWindow(0).GetTab(
-              self._FindTabLocked(unique_url)).Close(True)
+    # End of iterations, print results,
+    logging.debug('TTP results: %s', ttp_results)
+    logging.debug('EPP results: %s', epp_results)
+    pyauto_utils.PrintPerfResult('epp', series_name, epp_results, '%')
+    pyauto_utils.PrintPerfResult('ttp', series_name, ttp_results, 'ms')
 
-      # Check if any of the tests failed to report the metrics.
-      if not len(ttp_results) == len(epp_results) == _TEST_ITERATIONS:
-        self.fail_count += 1
-      # End of iterations, print results,
-      logging.debug('TTP results: %s', ttp_results)
-      logging.debug('EPP results: %s', epp_results)
-      pyauto_utils.PrintPerfResult('epp', series_name, max(epp_results), '%')
-      pyauto_utils.PrintPerfResult('ttp', series_name,
-                                   Median(ttp_results), 'ms')
-
-      # TODO(dalecurtis): Check results for regressions.
-      self._tasks.task_done()
+    # Check if any of the tests failed to report the metrics.
+    return len(ttp_results) == len(epp_results) == self._test_iterations
 
 
-class ProcessLogger(threading.Thread):
-  """A thread to log a process's stderr output."""
-
-  def __init__(self, process):
-    """Starts the process logger thread.
-
-    Args:
-      process: The process to log.
-    """
-    threading.Thread.__init__(self)
-    self._process = process
-    self.start()
-
-  def run(self):
-    """Adds debug statements for the process's stderr output."""
-    line = True
-    while line:
-      line = self._process.stderr.readline()
-      logging.debug(line.strip())
-
-
-class MediaConstrainedNetworkPerfTest(pyauto.PyUITest):
+class MediaConstrainedNetworkPerfTest(cns_test_base.CNSTestBase):
   """PyAuto test container.  See file doc string for more information."""
 
-  def setUp(self):
-    """Starts the Constrained Network Server (CNS)."""
-    cmd = [sys.executable, os.path.join(pyauto_paths.GetSourceDir(), _CNS_PATH),
-           '--port', str(_CNS_PORT),
-           '--interface', 'lo',
-           '--www-root', os.path.join(
-               self.DataDir(), 'pyauto_private', 'media'),
-           '-v',
-           '--expiry-time', '0']
-
-    process = subprocess.Popen(cmd, stderr=subprocess.PIPE)
-
-    # Wait for server to start up.
-    line = True
-    while line:
-      line = process.stderr.readline()
-      logging.debug(line.strip())
-      if 'STARTED' in line:
-        self._server_pid = process.pid
-        pyauto.PyUITest.setUp(self)
-        ProcessLogger(process)
-        if self._CanAccessServer():
-          return
-        # Need to call teardown since the server has already started.
-        self.tearDown()
-    self.fail('Failed to start CNS.')
-
-  def _CanAccessServer(self):
-    """Checks if the CNS server can serve a file with no network constraints."""
-    test_url = ''.join([_CNS_BASE_URL, 'f=', _TEST_VIDEO])
-    try:
-      return urllib2.urlopen(test_url) is not None
-    except Exception, e:
-      logging.exception(e)
-      return False
-
-  def tearDown(self):
-    """Stops the Constrained Network Server (CNS)."""
-    pyauto.PyUITest.tearDown(self)
-    self.Kill(self._server_pid)
-
-  def _RunDummyTest(self, automation_lock, test_url):
+  def _RunDummyTest(self, test_path):
     """Runs a dummy test with high bandwidth and no latency or packet loss.
 
     Fails the unit test if the dummy test does not end.
 
     Args:
-      automation_lock: Global automation lock for pyauto calls.
-      test_url: File URL to HTML/JavaScript test code.
+      test_path: Path to HTML/JavaScript test code.
     """
     tasks = Queue.Queue()
-    tasks.put(('Dummy Test', [5000, 0, 0]))
-    tasks.put((None, None))
-    dummy_test = TestWorker(self, tasks, automation_lock, test_url)
-    dummy_test.join()
-    # Dummy test should successfully finish by storing epp results.
-    if dummy_test.fail_count:
+    tasks.put(('Dummy Test', [5000, 0, 0], _TEST_VIDEOS[0]))
+    # Dummy test should successfully finish by passing all the tests.
+    if worker_thread.RunWorkerThreads(self, CNSWorkerThread, tasks, 1,
+                                      test_path) > 0:
       self.fail('Failed to run dummy test.')
 
   def testConstrainedNetworkPerf(self):
+
     """Starts CNS, spins up worker threads to run through _TEST_CONSTRAINTS."""
-    # Convert relative test path into an absolute path.
-    test_url = self.GetFileURLForDataPath(_TEST_HTML_PATH)
-
-    # PyAuto doesn't support threads, so we synchronize all automation calls.
-    automation_lock = threading.Lock()
-
     # Run a dummy test to avoid Chrome/CNS startup overhead.
     logging.debug('Starting a dummy test to avoid Chrome/CNS startup overhead.')
-    self._RunDummyTest(automation_lock, test_url)
+    self._RunDummyTest(_TEST_HTML_PATH)
     logging.debug('Dummy test has finished. Starting real perf tests.')
 
-    # Spin up worker threads.
-    tasks = Queue.Queue()
-    threads = []
-    for _ in xrange(_TEST_THREADS):
-      threads.append(TestWorker(self, tasks, automation_lock, test_url))
-
-    for series_name, settings in _TESTS_TO_RUN.iteritems():
-      logging.debug('Add test: %s\tSettings: %s', series_name, settings)
-      tasks.put((series_name, settings))
-
-    # Add shutdown magic to end of queue.
-    for thread in threads:
-      tasks.put((None, None))
-
-    # Wait for threads to exit, gracefully or otherwise.
-    for thread in threads:
-      thread.join()
+    # Spin up CNS worker threads.
+    tasks = cns_test_base.CreateCNSPerfTasks(_TESTS_TO_RUN, _TEST_VIDEOS)
+    worker_thread.RunWorkerThreads(self, CNSWorkerThread, tasks, _TEST_THREADS,
+                                   _TEST_HTML_PATH)
 
 
 if __name__ == '__main__':
-  # TODO(dalecurtis): Process command line parameters here.
   pyauto_media.Main()
