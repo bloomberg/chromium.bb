30fb41e9d163cc4a932cb1e64fb61c30a860798a
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 20:46:51 +0000

    Remove obsolete test: testCloseAllTabs
    Test is invalid on Aura.
    
    R=krisr@chromium.org
    BUG=None
    TEST=pyauto functional tests.
    
    Review URL: https://chromiumcodereview.appspot.com/10160004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133269 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e2def0e6c57e2c08f9e0d422fb7554c446bf6a6e

--- chrome/test/functional/chromeos_browser.py
+++ chrome/test/functional/chromeos_browser.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -8,20 +8,6 @@ import pyauto
 
 
 class ChromeosBrowserTest(pyauto.PyUITest):
-
-  def testCloseAllTabs(self):
-    """Close all tabs and verify 1 tab is still open on Chrome OS."""
-    tab_count = self.GetTabCount()
-
-    for tab_index in xrange(tab_count - 1, -1, -1):
-      self.GetBrowserWindow(0).GetTab(tab_index).Close(True)
-
-    info = self.GetBrowserInfo()
-    self.assertEqual(1, len(info['windows']))
-    self.assertEqual(1, len(info['windows'][0]['tabs']))
-    url = info['windows'][0]['tabs'][0]['url']
-    self.assertEqual('chrome://newtab/', url,
-                     msg='Unexpected URL: %s' % url)
 
   def testCannotCloseLastIncognito(self):
     """Verify that last incognito window cannot be closed if it's the
