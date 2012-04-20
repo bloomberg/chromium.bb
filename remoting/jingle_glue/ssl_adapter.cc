e32f8410a807b789f3054cbef2be4496761bf8fb
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-20 20:07:30 +0000

    Use NSS for SSL encryption of XMPP connection on Windows.
    Review URL: http://codereview.chromium.org/10169012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133247 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c78aebb5b07c7e1c6f50b2c460658b8202306fbf

--- remoting/jingle_glue/ssl_adapter.cc
+++ remoting/jingle_glue/ssl_adapter.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -14,11 +14,7 @@ namespace remoting {
 
 talk_base::SSLAdapter* CreateSSLAdapter(talk_base::AsyncSocket* socket) {
   talk_base::SSLAdapter* ssl_adapter =
-#if defined(OS_WIN)
-      talk_base::SSLAdapter::Create(socket);
-#else
       remoting::SSLSocketAdapter::Create(socket);
-#endif
   DCHECK(ssl_adapter);
   return ssl_adapter;
 }
