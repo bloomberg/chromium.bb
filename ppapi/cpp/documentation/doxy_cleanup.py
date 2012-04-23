94c6c235e2324a2a4a03e2eb4316293567af642c
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-23 18:07:54 +0000

    Set svn:executable bit on scripts that are executable.
    R=viettrungluu@chromium.org
    TEST=
    BUG=
    
    
    Review URL: http://codereview.chromium.org/10162022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133474 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3e5eb1076860d9b951a0882dfd31227bd32d732c

--- ppapi/cpp/documentation/doxy_cleanup.py
+++ ppapi/cpp/documentation/doxy_cleanup.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python
-
+#!/usr/bin/env python
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -137,6 +136,7 @@ def main():
       raise
 
   return 0
+
 
 if __name__ == '__main__':
   sys.exit(main())
