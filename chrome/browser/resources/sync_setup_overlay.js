507f383c8433af553ac9cc74984e3685d9e70e3b
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-24 20:56:40 +0000

    Fix build failure from a bad merge.
    BUG=none
    TEST=none
    TBR=dpapad
    
    Review URL: https://chromiumcodereview.appspot.com/10211009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133761 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a54ba12fbf02b0cea138311e94bb14361681b57d

--- chrome/browser/resources/sync_setup_overlay.js
+++ chrome/browser/resources/sync_setup_overlay.js
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-<include src="../shared/js/util.js"></include>
-
 cr.define('options', function() {
   /** @const */ var OptionsPage = options.OptionsPage;
 
