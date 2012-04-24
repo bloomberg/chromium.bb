ffac19a63b569035bdcc7947de7a880f04bf8a02
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-24 14:36:44 +0000

    Failing SelectFileDialogExtensionBrowserTest.SelectFileAndSave on chromiumos browser_tests.
    Revert 133663 - 1. Fix JSDocs in directory_model.js.
    2. Replace all getters and setters in directory_model.js with get*() and set*() methods.
    Review URL: https://chromiumcodereview.appspot.com/10206015
    
    TBR=oleg@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10201018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@133679 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d43eccfc3ee273bfbfea602f961b39c9bd191f47

--- chrome/browser/resources/file_manager/js/file_manager_pyauto.js
+++ chrome/browser/resources/file_manager/js/file_manager_pyauto.js
@@ -8,8 +8,6 @@
  *
  * Refer to chrome/test/functional/chromeos_file_browser.py for examples
  * of how this API is used.
- *
- * TODO(olege): Fix style warnings.
  */
 var pyautoAPI = {
   /**
@@ -20,7 +18,7 @@ var pyautoAPI = {
    */
   addItemToSelection: function(name) {
     var entryExists = false;
-    var dm = fileManager.directoryModel_.getFileList();
+    var dm = fileManager.directoryModel_.fileList;
     for (var i = 0; i < dm.length; i++) {
       if (dm.item(i).name == name) {
         fileManager.currentList_.selectionModel.setIndexSelected(i, true);
@@ -41,7 +39,7 @@ var pyautoAPI = {
    */
   listDirectory: function() {
     var list = [];
-    var dm = fileManager.directoryModel_.getFileList();
+    var dm = fileManager.directoryModel_.fileList;
     for (var i = 0; i < dm.length; i++) {
       list.push(dm.item(i).name);
     }
