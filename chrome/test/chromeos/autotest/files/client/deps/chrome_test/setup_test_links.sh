eabb01d6f2c075e81cd63e51126a01c03f4cd810
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-26 00:22:31 +0000

    Create symlinks to all .pak files during tests on chromeos
    ui_resources_standard.pak and theme_resources_standard.pak should be available
    along with test binaries for some tests to work properly.
    
    BUG=None
    TEST=None
    
    R=zelidrag@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10161039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134022 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fd01bda38591a60d78469c9e0790caf3c861a228

--- chrome/test/chromeos/autotest/files/client/deps/chrome_test/setup_test_links.sh
+++ chrome/test/chromeos/autotest/files/client/deps/chrome_test/setup_test_links.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
@@ -9,9 +9,15 @@
 # TODO(rkc): Figure out why does the sym linking break tests, eventually
 cp /opt/google/chrome/chrome $(dirname $0)/chrome
 ln -f -s /opt/google/chrome/chrome.pak $(dirname $0)/chrome.pak
-ln -f -s /opt/google/chrome/locales $(dirname $0)/locales
-ln -f -s /opt/google/chrome/resources $(dirname $0)/resources
+[ -L $(dirname $0)/locales ] || ln -f -s /opt/google/chrome/locales \
+    $(dirname $0)/locales
+[ -L $(dirname $0)/resources ] || ln -f -s /opt/google/chrome/resources \
+    $(dirname $0)/resources
 ln -f -s /opt/google/chrome/resources.pak $(dirname $0)/resources.pak
+ln -f -s /opt/google/chrome/ui_resources_standard.pak \
+    $(dirname $0)/ui_resources_standard.pak
+ln -f -s /opt/google/chrome/theme_resources_standard.pak \
+    $(dirname $0)/theme_resources_standard.pak
 
 mkdir -p $(dirname $0)/chromeos
 ln -f -s /opt/google/chrome/chromeos/libcros.so \
