8a79d2c5bfc7f6babf0ced06716272f077e62213
jvoung@google.com <jvoung@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-04-26 22:25:59 +0000

    Bump llvm rev for:
    http://codereview.chromium.org/10222018/
    
    "llvm: ensure intrinsic prototypes are available before
    generating metadata stubs."
    
    This ensures prototypes for functions like "pow" are
    available when we generate stubs for them, even though the
    only function calls are to "llvm.pow".
    
    and
    
    http://codereview.chromium.org/10239014/
    "Fix bit vs byte in GetElfSize (mistake from last minute change)"
    
    BUG=
    TEST= run_float_math_noerrno_test
    
    Review URL: https://chromiumcodereview.appspot.com/10238015
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8421 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: d9e5b445fcc13e479a375edef8e9cff419c5b362

--- pnacl/build.sh
+++ pnacl/build.sh
@@ -250,7 +250,7 @@ if ${PNACL_IN_CROS_CHROOT}; then
 fi
 
 # Current milestones in each repo
-readonly UPSTREAM_REV=${UPSTREAM_REV:-369a33ba460a}
+readonly UPSTREAM_REV=${UPSTREAM_REV:-e38521d9a0ea}
 
 readonly NEWLIB_REV=346ea38d142f
 readonly BINUTILS_REV=f44074cff866
