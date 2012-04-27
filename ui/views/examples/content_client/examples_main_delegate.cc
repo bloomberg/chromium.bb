647c15bc97e88741cf2a9f0355c5153aea2e9ed0
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-04-27 22:21:40 +0000

    Enable logging in Views Examples.
    BUG=none
    TEST=Check that Sawbuck shows logs output by views_examples.
    
    Review URL: http://codereview.chromium.org/10255013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134372 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 66a7c0cd84eb5303ba95f4be9f633b31b1cf4722

--- ui/views/examples/content_client/examples_main_delegate.cc
+++ ui/views/examples/content_client/examples_main_delegate.cc
@@ -6,6 +6,7 @@
 
 #include "base/command_line.h"
 #include "base/file_path.h"
+#include "base/logging.h"
 #include "base/path_service.h"
 #include "content/public/browser/browser_main_runner.h"
 #include "content/public/common/content_switches.h"
@@ -16,9 +17,20 @@
 #include "ui/base/ui_base_paths.h"
 #include "ui/views/examples/content_client/examples_content_browser_client.h"
 
+#if defined(OS_WIN)
+#include "base/logging_win.h"
+#endif
+
 namespace views {
 namespace examples {
 namespace {
+
+#if defined(OS_WIN)
+// {83FAC8EE-7A0E-4dbb-A3F6-6F500D7CAB1A}
+const GUID kViewsExamplesProviderName =
+    { 0x83fac8ee, 0x7a0e, 0x4dbb,
+        { 0xa3, 0xf6, 0x6f, 0x50, 0xd, 0x7c, 0xab, 0x1a } };
+#endif
 
 int ExamplesBrowserMain(
     const content::MainFunctionParams& main_function_params) {
@@ -47,6 +59,17 @@ bool ExamplesMainDelegate::BasicStartupComplete(int* exit_code) {
 
   content::SetContentClient(&content_client_);
   InitializeShellContentClient(process_type);
+
+  bool success = logging::InitLogging(NULL,
+      logging::LOG_ONLY_TO_SYSTEM_DEBUG_LOG,
+      logging::LOCK_LOG_FILE,
+      logging::DELETE_OLD_LOG_FILE,
+      logging::DISABLE_DCHECK_FOR_NON_OFFICIAL_RELEASE_BUILDS);
+  CHECK(success);
+#if defined(OS_WIN)
+  logging::LogEventProvider::Initialize(kViewsExamplesProviderName);
+#endif
+
   return false;
 }
 
