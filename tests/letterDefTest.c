3fd75145a8914971adc1166c0792ec93353f48d8
Mesar Hameed <mesar.hameed@gmail.com>
2012-04-30 08:21:56 +0000

    moved tables/letterDefTest.ctb to tests/tables/letterDefTest.ctb. Corrected tests/letterDefTest.c to test for unicode problems.

--- tests/letterDefTest.c
+++ tests/letterDefTest.c
@@ -1,14 +1,38 @@
+#include <locale.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "brl_checks.h"
+#include <wchar.h>
+#include "liblouis.h"
 
-int
-main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+  wchar_t *text = L"⠍⠎";
+  wchar_t *expected = L"⠎⠍";
+  const char* tableList = "./tables/letterDefTest.ctb";
+  setlocale(LC_ALL, "en_GB.UTF-8");
 
-  int result = 0;
-
-  result = check_translation("letterDefTest.ctb", "ms", NULL, "\x280e\x280d");
-  return result;
+  wchar_t *inbuf;
+  wchar_t *outbuf;
+  int inlen;
+  int outlen;
+  int i = 0;
+  int expectedlen = strlen(expected);
+  inlen = strlen(text) * 2;
+  outlen = inlen;
+  inbuf = malloc(sizeof(wchar_t) * inlen);
+  outbuf = malloc(sizeof(wchar_t) * outlen);
+  printf("%ls\n", text);
+  for (i = 0; i < inlen; i++) {
+      inbuf[i] = text[i];
+  }
+  if (!lou_translate(tableList, inbuf, &inlen, outbuf, &outlen,
+                     NULL, NULL, NULL, NULL, NULL, 0)) {
+      printf("Translation failed.\n");
+      return 1;
+    }
+  printf("%ls\n", outbuf);
+  free(inbuf);
+  free(outbuf);
+  lou_free();
+  return 0;
 }
