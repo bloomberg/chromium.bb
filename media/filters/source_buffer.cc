86ce8621f497d465692980b565def0685de4ff8e
annacc@chromium.org <annacc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-02 20:04:53 +0000

    This patch introduces a simplified SourceBuffer that takes ownership of ChunkDemuxer's StreamParser and simply forwards the callbacks back to ChunkDemuxer.  Existing functionality should be maintained.
    BUG=125770
    TEST=chunk_demuxer_unittest.cc should still succeed.  source_buffer_unittest.cc coming soon.
    
    
    Review URL: http://codereview.chromium.org/10261028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134975 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3af58c07fc124ced9d709820a02d1f884466c4ae

---
+++ media/filters/source_buffer.cc
@@ -0,0 +1,78 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/filters/source_buffer.h"
+
+#include "base/bind.h"
+#include "base/logging.h"
+#include "media/webm/webm_stream_parser.h"
+
+namespace media {
+
+SourceBuffer::SourceBuffer() {}
+
+SourceBuffer::~SourceBuffer() {}
+
+void SourceBuffer::Init(const InitCB& init_cb,
+                        const NewConfigCB& config_cb,
+                        const NewBuffersCB& audio_cb,
+                        const NewBuffersCB& video_cb,
+                        const KeyNeededCB& key_needed_cb) {
+  DCHECK(init_cb_.is_null());
+  DCHECK(!init_cb.is_null());
+  DCHECK(!config_cb.is_null());
+  DCHECK(!audio_cb.is_null() || !video_cb.is_null());
+  DCHECK(!key_needed_cb.is_null());
+
+  init_cb_ = init_cb;
+  config_cb_ = config_cb;
+  audio_cb_ = audio_cb;
+  video_cb_ = video_cb;
+  key_needed_cb_ = key_needed_cb;
+
+  stream_parser_.reset(new WebMStreamParser());
+
+  stream_parser_->Init(
+      base::Bind(&SourceBuffer::OnStreamParserInitDone, base::Unretained(this)),
+      base::Bind(&SourceBuffer::OnNewConfigs, base::Unretained(this)),
+      base::Bind(&SourceBuffer::OnAudioBuffers, base::Unretained(this)),
+      base::Bind(&SourceBuffer::OnVideoBuffers, base::Unretained(this)),
+      base::Bind(&SourceBuffer::OnKeyNeeded, base::Unretained(this)));
+}
+
+bool SourceBuffer::AppendData(const uint8* data, size_t length) {
+  return stream_parser_->Parse(data, length);
+}
+
+void SourceBuffer::Flush() {
+  stream_parser_->Flush();
+}
+
+void SourceBuffer::OnStreamParserInitDone(bool success,
+                                          base::TimeDelta duration) {
+  init_cb_.Run(success, duration);
+  init_cb_.Reset();
+}
+
+bool SourceBuffer::OnNewConfigs(const AudioDecoderConfig& audio_config,
+                    const VideoDecoderConfig& video_config) {
+  CHECK(audio_config.IsValidConfig() || video_config.IsValidConfig());
+
+  return config_cb_.Run(audio_config, video_config);
+}
+
+bool SourceBuffer::OnAudioBuffers(const StreamParser::BufferQueue& buffer) {
+  return audio_cb_.Run(buffer);
+}
+
+bool SourceBuffer::OnVideoBuffers(const StreamParser::BufferQueue& buffer) {
+  return video_cb_.Run(buffer);
+}
+
+bool SourceBuffer::OnKeyNeeded(scoped_array<uint8> init_data,
+                               int init_data_size) {
+  return key_needed_cb_.Run(init_data.Pass(), init_data_size);
+}
+
+}  // namespace media
---
+++ media/filters/source_buffer.h
@@ -0,0 +1,65 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_FILTERS_SOURCE_BUFFER_H_
+#define MEDIA_FILTERS_SOURCE_BUFFER_H_
+
+#include "media/base/audio_decoder_config.h"
+#include "media/base/stream_parser.h"
+#include "media/base/video_decoder_config.h"
+
+namespace media {
+
+// SourceBuffer will store media segments and initialization segments that are
+// dynamically appended to a particular source ID, hand them to a StreamParser
+// to be parsed, and provide buffers to ChunkDemuxerStreams during playback.
+class MEDIA_EXPORT SourceBuffer {
+ public:
+  SourceBuffer();
+  ~SourceBuffer();
+
+  typedef base::Callback<void(bool, base::TimeDelta)> InitCB;
+  typedef base::Callback<bool(const AudioDecoderConfig&,
+                              const VideoDecoderConfig&)> NewConfigCB;
+  typedef base::Callback<bool(const StreamParser::BufferQueue&)> NewBuffersCB;
+  typedef base::Callback<bool(scoped_array<uint8>, int)> KeyNeededCB;
+
+  void Init(const InitCB& init_cb,
+            const NewConfigCB& config_cb,
+            const NewBuffersCB& audio_cb,
+            const NewBuffersCB& video_cb,
+            const KeyNeededCB& key_needed_cb);
+
+  // Add buffers to this source.  Incoming data will be parsed and then stored
+  // in SourceBufferStreams, which will handle ordering and overlap resolution.
+  // Returns true if data was successfully appended, false if the parse failed.
+  bool AppendData(const uint8* data, size_t length);
+
+  // Clears the data from this buffer.  Used during a seek to ensure the next
+  // buffers provided during a read are the buffers appended after the seek.
+  void Flush();
+
+ private:
+  // StreamParser callbacks.
+  void OnStreamParserInitDone(bool success, base::TimeDelta duration);
+  bool OnNewConfigs(const AudioDecoderConfig& audio_config,
+                    const VideoDecoderConfig& video_config);
+  bool OnAudioBuffers(const StreamParser::BufferQueue& buffer);
+  bool OnVideoBuffers(const StreamParser::BufferQueue& buffer);
+  bool OnKeyNeeded(scoped_array<uint8> init_data, int init_data_size);
+
+  scoped_ptr<StreamParser> stream_parser_;
+
+  InitCB init_cb_;
+  NewConfigCB config_cb_;
+  NewBuffersCB audio_cb_;
+  NewBuffersCB video_cb_;
+  KeyNeededCB key_needed_cb_;
+
+  DISALLOW_COPY_AND_ASSIGN(SourceBuffer);
+};
+
+}  // namespace media
+
+#endif  // MEDIA_FILTERS_SOURCE_BUFFER_H_
