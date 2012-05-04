1276da429de94817a6b4640f1ff4eb0b28770724
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 21:59:58 +0000

    Fix svn:executable on new NaCl scripts
    TBR=noelallen@chromium.org
    BUG=
    TEST=checkperms.py is not regressing
    
    
    Review URL: http://codereview.chromium.org/10384013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135453 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1dedf4941edb0d7f58bcc4bc636411f8a805415b

--- native_client_sdk/src/build_tools/json/upload.sh
+++ native_client_sdk/src/build_tools/json/upload.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 echo DEPRECATED gsutil -h "Cache-Control: no-cache" cp -a public-read naclsdk_manifest.json gs://nativeclient-mirror/nacl/nacl_sdk/naclsdk_manifest.json
 gsutil -h "Cache-Control: no-cache" cp -a public-read naclsdk_manifest0.json gs://nativeclient-mirror/nacl/nacl_sdk/naclsdk_manifest0.json
 gsutil -h "Cache-Control: no-cache" cp -a public-read naclsdk_manifest2.json gs://nativeclient-mirror/nacl/nacl_sdk/naclsdk_manifest2.json
