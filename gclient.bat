5255b2d1167796e2ec44d429d8fdd3a2b4981ddb
johnw@google.com <johnw@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-04 18:24:21 +0000

    Attempting to fix a C:\cygwin\bin\python.exe stack trace Review URL: https://chromiumcodereview.appspot.com/10206030
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@135377 0039d316-1c4b-4281-b951-d872f2087c98

--- gclient.bat
+++ gclient.bat
@@ -3,8 +3,11 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
+:: This is required with cygwin only.
+PATH=%~dp0;%PATH%
+
 :: Synchronize the root directory before deferring control back to gclient.py.
 call "%~dp0\update_depot_tools.bat"
 
 :: Defer control.
-python "%~dp0\gclient.py" %*
+%~dp0python "%~dp0\gclient.py" %*
