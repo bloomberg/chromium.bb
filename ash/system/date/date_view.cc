bloomberg/chromium.bb

commit c5c70ae80f7ebc27c29a6f151efbad5030e5f120

ash: Add vertical spacing to multi-line text in tray.
This breaks the multi-line date/day-of-week and battery text
in the uber system tray across multiple labels so we can add
a bit more spacing between the lines.

A side effect (maybe a slight improvement?) is that some of
the longer strings (e.g.  "Calculating remaining time") now
get elided instead of getting wrapped and clipped.

Since I needed to change the battery-related strings anyway,
I'm also splitting the "2h 23m"-style string into
"2h 23m remaining" and "2h 23m until full".

BUG=124664,124074
TEST=manual

Review URL: https://chromiumcodereview.appspot.com/10209038

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@134585 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 1529417c20010031996353af807b4cd34086516f

ash/system/date/date_view.cc
@@ -6,6 +6,7 @@
 
 #include "ash/shell.h"
 #include "ash/system/tray/system_tray_delegate.h"
+#include "ash/system/tray/tray_constants.h"
 #include "base/time.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
@@ -17,83 +18,55 @@ namespace internal {
 namespace tray {
 
 namespace {
+
 // Amount of slop to add into the timer to make sure we're into the next minute
 // when the timer goes off.
 const int kTimerSlopSeconds = 1;
 
-string16 FormatNicely(const base::Time& time) {
+string16 FormatDate(const base::Time& time) {
   icu::UnicodeString date_string;
-
   scoped_ptr<icu::DateFormat> formatter(
-      icu::DateFormat::createDateInstance(icu::DateFormat::kFull));
-  icu::FieldPosition position;
-  position.setField(UDAT_DAY_OF_WEEK_FIELD);
-  formatter->format(static_cast<UDate>(time.ToDoubleT() * 1000),
-                    date_string,
-                    position);
-  icu::UnicodeString day = date_string.retainBetween(position.getBeginIndex(),
-                                                     position.getEndIndex());
-
-  date_string.remove();
-  formatter.reset(
       icu::DateFormat::createDateInstance(icu::DateFormat::kMedium));
   formatter->format(static_cast<UDate>(time.ToDoubleT() * 1000), date_string);
-
-  date_string += "\n";
-  date_string += day;
-
   return string16(date_string.getBuffer(),
                   static_cast<size_t>(date_string.length()));
 }
 
+string16 FormatDayOfWeek(const base::Time& time) {
+  scoped_ptr<icu::DateFormat> formatter(
+      icu::DateFormat::createDateInstance(icu::DateFormat::kFull));
+  icu::UnicodeString date_string;
+  icu::FieldPosition position;
+  position.setField(UDAT_DAY_OF_WEEK_FIELD);
+  formatter->format(
+      static_cast<UDate>(time.ToDoubleT() * 1000), date_string, position);
+  icu::UnicodeString day = date_string.retainBetween(position.getBeginIndex(),
+                                                     position.getEndIndex());
+  return string16(day.getBuffer(), static_cast<size_t>(day.length()));
 }
 
-DateView::DateView(TimeType type)
-    : hour_type_(ash::Shell::GetInstance()->tray_delegate()->
-          GetHourClockType()),
-      type_(type),
-      actionable_(false) {
-  // TODO(flackr): Investigate respecting the view's border in FillLayout.
-  SetLayoutManager(new views::BoxLayout(
-      views::BoxLayout::kHorizontal, 0, 0, 0));
-  label_ = new views::Label;
-  label_->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
-  label_->SetBackgroundColor(SkColorSetARGB(0, 255, 255, 255));
-  UpdateText();
-  AddChildView(label_);
-  set_focusable(actionable_);
+views::Label* CreateLabel() {
+  views::Label* label = new views::Label;
+  label->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
+  label->SetBackgroundColor(SkColorSetARGB(0, 255, 255, 255));
+  return label;
 }
 
-DateView::~DateView() {
+}  // namespace
+
+BaseDateTimeView::~BaseDateTimeView() {
   timer_.Stop();
 }
 
-void DateView::UpdateTimeFormat() {
-  hour_type_ = ash::Shell::GetInstance()->tray_delegate()->GetHourClockType();
-  UpdateText();
-}
-
-void DateView::SetActionable(bool actionable) {
-  actionable_ = actionable;
-  set_focusable(actionable_);
-}
-
-void DateView::UpdateText() {
+void BaseDateTimeView::UpdateText() {
   base::Time now = base::Time::Now();
-  gfx::Size old_size = label_->GetPreferredSize();
-  if (type_ == DATE) {
-    label_->SetText(FormatNicely(now));
-  } else {
-    label_->SetText(base::TimeFormatTimeOfDayWithHourClockType(
-        now, hour_type_, base::kDropAmPm));
+  gfx::Size old_size = GetPreferredSize();
+  UpdateTextInternal(now);
+  if (GetWidget() && GetPreferredSize() != old_size) {
+    // Forcing the widget to the new size is sufficient. The positioning is
+    // taken care of by the layout manager (ShelfLayoutManager).
+    GetWidget()->SetSize(GetPreferredSize());
   }
-  if (label_->GetPreferredSize() != old_size && GetWidget()) {
-    // Forcing the widget to the new size is sufficient. The positing is taken
-    // care of by the layout manager (ShelfLayoutManager).
-    GetWidget()->SetSize(GetWidget()->GetContentsView()->GetPreferredSize());
-  }
-
-  label_->SetTooltipText(base::TimeFormatFriendlyDate(now));
   SchedulePaint();
 
   // Try to set the timer to go off at the next change of the minute. We don't
@@ -113,8 +86,41 @@ void DateView::UpdateText() {
   seconds_left += kTimerSlopSeconds;
 
   timer_.Stop();
-  timer_.Start(FROM_HERE, base::TimeDelta::FromSeconds(seconds_left), this,
-      &DateView::UpdateText);
+  timer_.Start(
+      FROM_HERE, base::TimeDelta::FromSeconds(seconds_left),
+      this, &BaseDateTimeView::UpdateText);
+}
+
+BaseDateTimeView::BaseDateTimeView() {
+}
+
+void BaseDateTimeView::OnLocaleChanged() {
+  UpdateText();
+}
+
+DateView::DateView() : actionable_(false) {
+  SetLayoutManager(
+      new views::BoxLayout(
+          views::BoxLayout::kVertical, 0, 0, kTrayPopupTextSpacingVertical));
+  date_label_ = CreateLabel();
+  day_of_week_label_ = CreateLabel();
+  UpdateTextInternal(base::Time::Now());
+  AddChildView(date_label_);
+  AddChildView(day_of_week_label_);
+  set_focusable(actionable_);
+}
+
+DateView::~DateView() {
+}
+
+void DateView::SetActionable(bool actionable) {
+  actionable_ = actionable;
+  set_focusable(actionable_);
+}
+
+void DateView::UpdateTextInternal(const base::Time& now) {
+  date_label_->SetText(FormatDate(now));
+  day_of_week_label_->SetText(FormatDayOfWeek(now));
 }
 
 bool DateView::PerformAction(const views::Event& event) {
@@ -125,9 +131,36 @@ bool DateView::PerformAction(const views::Event& event) {
   return true;
 }
 
-void DateView::OnLocaleChanged() {
+TimeView::TimeView()
+    : hour_type_(
+          ash::Shell::GetInstance()->tray_delegate()->GetHourClockType()) {
+  SetLayoutManager(
+      new views::BoxLayout(views::BoxLayout::kHorizontal, 0, 0, 0));
+  label_ = CreateLabel();
+  UpdateTextInternal(base::Time::Now());
+  AddChildView(label_);
+  set_focusable(true);
+}
+
+TimeView::~TimeView() {
+}
+
+void TimeView::UpdateTimeFormat() {
+  hour_type_ = ash::Shell::GetInstance()->tray_delegate()->GetHourClockType();
   UpdateText();
 }
+
+void TimeView::UpdateTextInternal(const base::Time& now) {
+  label_->SetText(
+      base::TimeFormatTimeOfDayWithHourClockType(
+          now, hour_type_, base::kDropAmPm));
+  label_->SetTooltipText(base::TimeFormatFriendlyDate(now));
+}
+
+bool TimeView::PerformAction(const views::Event& event) {
+  return false;
+}
+
 
 }  // namespace tray
 }  // namespace internal

ash/system/date/date_view.h
@@ -19,40 +19,79 @@ namespace ash {
 namespace internal {
 namespace tray {
 
-// This view is used for both the TrayDate tray icon and the TrayPower popup.
-class DateView : public ActionableView {
+// Abstract base class containing common updating and layout code for the
+// DateView popup and the TimeView tray icon.
+class BaseDateTimeView : public ActionableView {
  public:
-  enum TimeType {
-    TIME,
-    DATE
-  };
+  virtual ~BaseDateTimeView();
 
-  explicit DateView(TimeType type);
+  // Updates the displayed text for the current time.
+  void UpdateText();
+
+ protected:
+  BaseDateTimeView();
+
+ private:
+  // Updates labels to display the current time.
+  virtual void UpdateTextInternal(const base::Time& now) = 0;
+
+  // Overridden from views::View.
+  virtual void OnLocaleChanged() OVERRIDE;
+
+  base::OneShotTimer<BaseDateTimeView> timer_;
+
+  DISALLOW_COPY_AND_ASSIGN(BaseDateTimeView);
+};
+
+// Popup view used to display the date and day of week.
+class DateView : public BaseDateTimeView {
+ public:
+  DateView();
   virtual ~DateView();
-  void UpdateTimeFormat();
-  views::Label* label() const { return label_; }
 
   // Sets whether the view is actionable. An actionable date view gives visual
   // feedback on hover, can be focused by keyboard, and clicking/pressing space
   // or enter on the view shows date-related settings.
   void SetActionable(bool actionable);
 
-  void UpdateText();
-
  private:
+  // Overridden from BaseDateTimeView.
+  virtual void UpdateTextInternal(const base::Time& now) OVERRIDE;
+
   // Overridden from ActionableView.
   virtual bool PerformAction(const views::Event& event) OVERRIDE;
 
-  // Overridden from views::View.
-  virtual void OnLocaleChanged() OVERRIDE;
+  views::Label* date_label_;
+  views::Label* day_of_week_label_;
 
-  base::OneShotTimer<DateView> timer_;
-  base::HourClockType hour_type_;
-  TimeType type_;
   bool actionable_;
-  views::Label* label_;
 
   DISALLOW_COPY_AND_ASSIGN(DateView);
+};
+
+// Tray view used to display the current time.
+class TimeView : public BaseDateTimeView {
+ public:
+  TimeView();
+  virtual ~TimeView();
+
+  views::Label* label() const { return label_; }
+
+  // Updates the format of the displayed time.
+  void UpdateTimeFormat();
+
+ private:
+  // Overridden from BaseDateTimeView.
+  virtual void UpdateTextInternal(const base::Time& now) OVERRIDE;
+
+  // Overridden from ActionableView.
+  virtual bool PerformAction(const views::Event& event) OVERRIDE;
+
+  views::Label* label_;
+
+  base::HourClockType hour_type_;
+
+  DISALLOW_COPY_AND_ASSIGN(TimeView);
 };
 
 }  // namespace tray

ash/ash_strings.grd
@@ -311,20 +311,20 @@ Press Search key to cancel.
       <message name="IDS_ASH_STATUS_TRAY_BATTERY_FULL" desc="The label in the tray dialog to indicate that the battery is full.">
         Battery full
       </message>
-      <message name="IDS_ASH_STATUS_TRAY_BATTERY_LOW" desc="The label in the tray dialog to indicate that the battery is low.">
-        Battery low
+      <message name="IDS_ASH_STATUS_TRAY_BATTERY_PERCENT" desc="The label in the tray dialog to show the remaining battery power as a percent.">
+        Battery: <ph name="percentage">$1<ex>56</ex></ph>%
       </message>
-      <message name="IDS_ASH_STATUS_TRAY_BATTERY_STATUS" desc="The label in the tray dialog to show the status of the battery power.">
-Battery: <ph name="percentage">$1<ex>56</ex></ph>%
-<ph name="hour">$2</ph>h <ph name="minute">$3</ph>m
+      <message name="IDS_ASH_STATUS_TRAY_BATTERY_TIME_UNTIL_FULL" desc="The label in the tray dialog to show a time estimate until the battery is fully charged.">
+        <ph name="hour">$1<ex>2</ex></ph>h <ph name="minute">$2<ex>53</ex></ph>m until full
+      </message>
+      <message name="IDS_ASH_STATUS_TRAY_BATTERY_TIME_UNTIL_EMPTY" desc="The label in the tray dialog to show a time estimate until the battery is empty.">
+        <ph name="hour">$1<ex>2</ex></ph>h <ph name="minute">$2<ex>53</ex></ph>m remaining
       </message>
       <message name="IDS_ASH_STATUS_TRAY_BATTERY_CALCULATING_ON" desc="The label in the tray dialog indicating that the time to completely charge the battery is being calculated.">
-Battery: <ph name="percentage">$1<ex>56</ex></ph>%
-Calculating time until full
+        Calculating time until full
       </message>
       <message name="IDS_ASH_STATUS_TRAY_BATTERY_CALCULATING_OFF" desc="The label in the tray dialog indicating that the time to completely drain the battery is being calculated.">
-Battery: <ph name="percentage">$1<ex>56</ex></ph>%
-Calculating remaining time
+        Calculating remaining time
       </message>
       <message name="IDS_ASH_STATUS_TRAY_IP" desc="The label for the IP address of the network:">
         IP Address

ash/system/date/tray_date.cc
@@ -42,16 +42,16 @@ TrayDate::~TrayDate() {
 }
 
 views::View* TrayDate::CreateTrayView(user::LoginStatus status) {
-  date_tray_.reset(new tray::DateView(tray::DateView::TIME));
-  date_tray_->set_border(
+  time_tray_.reset(new tray::TimeView());
+  time_tray_->set_border(
       views::Border::CreateEmptyBorder(0, 10, 0, 7));
-  SetupLabelForTray(date_tray_->label());
-  gfx::Font font = date_tray_->label()->font();
-  date_tray_->label()->SetFont(
+  SetupLabelForTray(time_tray_->label());
+  gfx::Font font = time_tray_->label()->font();
+  time_tray_->label()->SetFont(
       font.DeriveFont(0, font.GetStyle() & ~gfx::Font::BOLD));
 
   views::View* view = new TrayItemView;
-  view->AddChildView(date_tray_.get());
+  view->AddChildView(time_tray_.get());
   return view;
 }
 
@@ -64,7 +64,7 @@ views::View* TrayDate::CreateDetailedView(user::LoginStatus status) {
 }
 
 void TrayDate::DestroyTrayView() {
-  date_tray_.reset();
+  time_tray_.reset();
 }
 
 void TrayDate::DestroyDefaultView() {
@@ -77,11 +77,11 @@ void TrayDate::UpdateAfterLoginStatusChange(user::LoginStatus status) {
 }
 
 void TrayDate::OnDateFormatChanged() {
-  date_tray_->UpdateTimeFormat();
+  time_tray_->UpdateTimeFormat();
 }
 
 void TrayDate::Refresh() {
-  date_tray_->UpdateText();
+  time_tray_->UpdateText();
 }
 
 }  // namespace internal

ash/system/date/tray_date.h
@@ -13,7 +13,7 @@ namespace ash {
 namespace internal {
 
 namespace tray {
-class DateView;
+class TimeView;
 }
 
 class TrayDate : public SystemTrayItem,
@@ -36,7 +36,7 @@ class TrayDate : public SystemTrayItem,
   virtual void OnDateFormatChanged() OVERRIDE;
   virtual void Refresh() OVERRIDE;
 
-  scoped_ptr<tray::DateView> date_tray_;
+  scoped_ptr<tray::TimeView> time_tray_;
 
   DISALLOW_COPY_AND_ASSIGN(TrayDate);
 };

ash/system/power/tray_power.cc
@@ -103,12 +103,20 @@ class PowerTrayView : public views::ImageView {
 };
 
 // This view is used only for the popup.
-class PowerPopupView : public views::Label {
+class PowerPopupView : public views::View {
  public:
   PowerPopupView() {
-    SetHorizontalAlignment(ALIGN_RIGHT);
-    SetMultiLine(true);
+    status_label_ = new views::Label;
+    status_label_->SetHorizontalAlignment(views::Label::ALIGN_RIGHT);
+    time_label_ = new views::Label;
+    time_label_->SetHorizontalAlignment(views::Label::ALIGN_RIGHT);
     UpdateText();
+
+    SetLayoutManager(
+        new views::BoxLayout(
+            views::BoxLayout::kVertical, 0, 0, kTrayPopupTextSpacingVertical));
+    AddChildView(status_label_);
+    AddChildView(time_label_);
   }
 
   virtual ~PowerPopupView() {
@@ -126,11 +134,16 @@ class PowerPopupView : public views::Label {
  private:
   void UpdateText() {
     if (supply_status_.is_calculating_battery_time) {
-      SetText(l10n_util::GetStringFUTF16(supply_status_.line_power_on ?
-          IDS_ASH_STATUS_TRAY_BATTERY_CALCULATING_ON :
-          IDS_ASH_STATUS_TRAY_BATTERY_CALCULATING_OFF,
-          base::IntToString16(
-              static_cast<int>(supply_status_.battery_percentage))));
+      status_label_->SetText(
+          l10n_util::GetStringFUTF16(
+              IDS_ASH_STATUS_TRAY_BATTERY_PERCENT,
+              base::IntToString16(
+                  static_cast<int>(supply_status_.battery_percentage))));
+      time_label_->SetText(
+          ui::ResourceBundle::GetSharedInstance().GetLocalizedString(
+              supply_status_.line_power_on ?
+                  IDS_ASH_STATUS_TRAY_BATTERY_CALCULATING_ON :
+                  IDS_ASH_STATUS_TRAY_BATTERY_CALCULATING_OFF));
       return;
     }
 
@@ -141,21 +154,33 @@ class PowerPopupView : public views::Label {
     int hour = time.InHours();
     int min = (time - base::TimeDelta::FromHours(hour)).InMinutes();
     if (hour || min) {
-      SetText(l10n_util::GetStringFUTF16(IDS_ASH_STATUS_TRAY_BATTERY_STATUS,
-          base::IntToString16(
-              static_cast<int>(supply_status_.battery_percentage)),
-          base::IntToString16(hour),
-          base::IntToString16(min)));
+      status_label_->SetText(
+          l10n_util::GetStringFUTF16(
+              IDS_ASH_STATUS_TRAY_BATTERY_PERCENT,
+              base::IntToString16(
+                  static_cast<int>(supply_status_.battery_percentage))));
+      time_label_->SetText(
+          l10n_util::GetStringFUTF16(
+              supply_status_.line_power_on ?
+                  IDS_ASH_STATUS_TRAY_BATTERY_TIME_UNTIL_FULL :
+                  IDS_ASH_STATUS_TRAY_BATTERY_TIME_UNTIL_EMPTY,
+              base::IntToString16(hour),
+              base::IntToString16(min)));
     } else {
       if (supply_status_.line_power_on) {
-        SetText(ui::ResourceBundle::GetSharedInstance().GetLocalizedString(
-            IDS_ASH_STATUS_TRAY_BATTERY_FULL));
+        status_label_->SetText(
+            ui::ResourceBundle::GetSharedInstance().GetLocalizedString(
+                IDS_ASH_STATUS_TRAY_BATTERY_FULL));
       } else {
         // Completely discharged? ... ha?
-        SetText(string16());
+        status_label_->SetText(string16());
       }
+      time_label_->SetText(string16());
     }
   }
+
+  views::Label* status_label_;
+  views::Label* time_label_;
 
   PowerSupplyStatus supply_status_;
 
@@ -184,7 +209,7 @@ views::View* TrayPower::CreateTrayView(user::LoginStatus status) {
 }
 
 views::View* TrayPower::CreateDefaultView(user::LoginStatus status) {
-  date_.reset(new tray::DateView(tray::DateView::DATE));
+  date_.reset(new tray::DateView());
 
   views::View* container = new views::View;
   views::BoxLayout* layout = new views::BoxLayout(views::BoxLayout::kHorizontal,

ash/system/tray/tray_constants.cc
@@ -12,6 +12,7 @@ const int kTrayPopupAutoCloseDelayInSeconds = 2;
 const int kTrayPopupAutoCloseDelayForTextInSeconds = 5;
 const int kTrayPopupPaddingHorizontal = 18;
 const int kTrayPopupPaddingBetweenItems = 10;
+const int kTrayPopupTextSpacingVertical = 4;
 
 const int kTrayPopupItemHeight = 48;
 const int kTrayPopupDetailsIconWidth = 27;

ash/system/tray/tray_constants.h
@@ -14,6 +14,7 @@ extern const int kTrayPopupAutoCloseDelayInSeconds;
 extern const int kTrayPopupAutoCloseDelayForTextInSeconds;
 extern const int kTrayPopupPaddingHorizontal;
 extern const int kTrayPopupPaddingBetweenItems;
+extern const int kTrayPopupTextSpacingVertical;
 
 extern const int kTrayPopupItemHeight;
 extern const int kTrayPopupDetailsIconWidth;

commit 3f3d01861bbbc401646653c4a0c8d4785458bcbb

ash: Fix regression where system tray clock wasn't updated.
I suspect that I broke this in r134585.

BUG=126155
TEST=manual: clock is updated after UI job starts


Review URL: http://codereview.chromium.org/10352022

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135265 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 7c14d2a127881484c5e7e801a8d3384172cb796e

ash/system/date/date_view.cc
@@ -60,10 +60,16 @@ BaseDateTimeView::~BaseDateTimeView() {
 
 void BaseDateTimeView::UpdateText() {
   base::Time now = base::Time::Now();
-  gfx::Size old_size = GetPreferredSize();
   UpdateTextInternal(now);
   SchedulePaint();
+  SetTimer(now);
+}
 
+BaseDateTimeView::BaseDateTimeView() {
+  SetTimer(base::Time::Now());
+}
+
+void BaseDateTimeView::SetTimer(const base::Time& now) {
   // Try to set the timer to go off at the next change of the minute. We don't
   // want to have the timer go off more than necessary since that will cause
   // the CPU to wake up and consume power.
@@ -84,9 +90,6 @@ void BaseDateTimeView::UpdateText() {
   timer_.Start(
       FROM_HERE, base::TimeDelta::FromSeconds(seconds_left),
       this, &BaseDateTimeView::UpdateText);
-}
-
-BaseDateTimeView::BaseDateTimeView() {
 }
 
 void BaseDateTimeView::ChildPreferredSizeChanged(views::View* child) {

ash/system/date/date_view.h
@@ -25,13 +25,16 @@ class BaseDateTimeView : public ActionableView {
  public:
   virtual ~BaseDateTimeView();
 
-  // Updates the displayed text for the current time.
+  // Updates the displayed text for the current time and calls SetTimer().
   void UpdateText();
 
  protected:
   BaseDateTimeView();
 
  private:
+  // Starts |timer_| to schedule the next update.
+  void SetTimer(const base::Time& now);
+
   // Updates labels to display the current time.
   virtual void UpdateTextInternal(const base::Time& now) = 0;
 
@@ -39,6 +42,7 @@ class BaseDateTimeView : public ActionableView {
   virtual void ChildPreferredSizeChanged(views::View* child) OVERRIDE;
   virtual void OnLocaleChanged() OVERRIDE;
 
+  // Invokes UpdateText() when the displayed time should change.
   base::OneShotTimer<BaseDateTimeView> timer_;
 
   DISALLOW_COPY_AND_ASSIGN(BaseDateTimeView);