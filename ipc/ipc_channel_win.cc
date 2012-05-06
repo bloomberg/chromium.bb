2df2f6a2c0e23c026372fde0ddc5883aebacc89c
bryner@chromium.org <bryner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-06 23:21:55 +0000

    Fix ipc_channel_win logging for failed pipe creation.
    BUG=none
    TEST=none
    
    
    Review URL: http://codereview.chromium.org/10378022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135607 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 34088fbeb4382bd1c65241403de99699dbabd022

--- ipc/ipc_channel_win.cc
+++ ipc/ipc_channel_win.cc
@@ -251,7 +251,7 @@ bool Channel::ChannelImpl::CreatePipe(const IPC::ChannelHandle &channel_handle,
   if (pipe_ == INVALID_HANDLE_VALUE) {
     // If this process is being closed, the pipe may be gone already.
     LOG(WARNING) << "Unable to create pipe \"" << pipe_name <<
-                    "\" in " << (mode == 0 ? "server" : "client")
+                    "\" in " << (mode & MODE_SERVER_FLAG ? "server" : "client")
                     << " mode. Error :" << GetLastError();
     return false;
   }
