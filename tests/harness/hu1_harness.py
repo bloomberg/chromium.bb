be3d90c6bf247bae5035b6bf368bb5f071ccbb01
Mesar Hameed <mesar.hameed@gmail.com>
2012-05-07 11:18:48 +0000

    harnesses: Add a Hungarian test harness for hu1.ctb

---
+++ tests/harness/hu1_harness.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+"""
+Liblouis test harness for the Hungarian grade 1 table.
+Please see the liblouis documentationfor more information.
+"""
+
+import sys
+import louis
+
+table = 'hu1.ctb'
+
+tests = [
+    {
+        'txt':  u'kerekesszék', 
+        'brl':  u'kerekes:*k'
+    }, 
+}
