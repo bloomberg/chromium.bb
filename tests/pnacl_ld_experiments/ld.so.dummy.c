ed1e395574941a7c90bf07468c2ff96f715ccffe
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-05-08 21:18:59 +0000

    Add some more infrastructure for x86-64 builds to pnacl shared newlib experiment.
    x8664 support is not quite working yet.
    code duplication is intentional for now until we add arm support.
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2745
    TEST= manual
    Review URL: https://chromiumcodereview.appspot.com/10356057
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8527 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: d58b511e97803e919468a633ef073a9aeeab0ecd

---
+++ tests/pnacl_ld_experiments/ld.so.dummy.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2012 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+/*
+ * These dummy functions document the ld.so interface we are currently using.
+ * We do not make any attempt to get the typing right as  this is not modeled
+ * by elf anyway.
+ */
+
+void _dl_debug_state() {
+}
+
+void _dl_get_tls_static_info() {
+}
