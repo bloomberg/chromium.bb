3f4bea3bba88ac0dc9231957f665483e923d71be
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 06:39:48 +0000

    Extract ModuleSystemTest into its own file.
    BUG=121479
    TEST=existing tests
    
    Review URL: https://chromiumcodereview.appspot.com/10349005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@135995 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 635e2c12b9c2fbfbbb86d18454690dc39aedc607

---
+++ chrome/test/base/module_system_test.cc
@@ -0,0 +1,116 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/base/module_system_test.h"
+
+#include "base/callback.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/string_piece.h"
+#include "chrome/renderer/native_handler.h"
+
+#include <map>
+#include <string>
+
+// Native JS functions for doing asserts.
+class AssertNatives : public NativeHandler {
+ public:
+  AssertNatives()
+      : assertion_made_(false),
+        failed_(false) {
+    RouteFunction("AssertTrue", base::Bind(&AssertNatives::AssertTrue,
+        base::Unretained(this)));
+    RouteFunction("AssertFalse", base::Bind(&AssertNatives::AssertFalse,
+        base::Unretained(this)));
+  }
+
+  bool assertion_made() { return assertion_made_; }
+  bool failed() { return failed_; }
+
+  v8::Handle<v8::Value> AssertTrue(const v8::Arguments& args) {
+    CHECK_EQ(1, args.Length());
+    assertion_made_ = true;
+    failed_ = failed_ || !args[0]->ToBoolean()->Value();
+    return v8::Undefined();
+  }
+
+  v8::Handle<v8::Value> AssertFalse(const v8::Arguments& args) {
+    CHECK_EQ(1, args.Length());
+    assertion_made_ = true;
+    failed_ = failed_ || args[0]->ToBoolean()->Value();
+    return v8::Undefined();
+  }
+
+ private:
+  bool assertion_made_;
+  bool failed_;
+};
+
+// Source map that operates on std::strings.
+class StringSourceMap : public ModuleSystem::SourceMap {
+ public:
+  StringSourceMap() {}
+  virtual ~StringSourceMap() {}
+
+  v8::Handle<v8::Value> GetSource(const std::string& name) OVERRIDE {
+    if (source_map_.count(name) == 0)
+      return v8::Undefined();
+    return v8::String::New(source_map_[name].c_str());
+  }
+
+  bool Contains(const std::string& name) OVERRIDE {
+    return source_map_.count(name);
+  }
+
+  void RegisterModule(const std::string& name, const std::string& source) {
+    CHECK_EQ(0u, source_map_.count(name));
+    source_map_[name] = source;
+  }
+
+ private:
+  std::map<std::string, std::string> source_map_;
+};
+
+ModuleSystemTest::ModuleSystemTest()
+    : context_(v8::Context::New()),
+      source_map_(new StringSourceMap()),
+      should_assertions_be_made_(true) {
+  context_->Enter();
+  assert_natives_ = new AssertNatives();
+  module_system_.reset(new ModuleSystem(context_, source_map_.get()));
+  module_system_->RegisterNativeHandler("assert", scoped_ptr<NativeHandler>(
+      assert_natives_));
+  try_catch_.SetCaptureMessage(true);
+}
+
+ModuleSystemTest::~ModuleSystemTest() {
+  module_system_.reset();
+  context_->Exit();
+  context_.Dispose();
+}
+
+void ModuleSystemTest::RegisterModule(const std::string& name,
+                                      const std::string& code) {
+  source_map_->RegisterModule(name, code);
+}
+
+void ModuleSystemTest::TearDown() {
+  EXPECT_FALSE(try_catch_.HasCaught())
+      << *v8::String::Utf8Value(try_catch_.Message()->Get());
+  // All tests must assert at least once unless otherwise specified.
+  EXPECT_EQ(should_assertions_be_made_,
+            assert_natives_->assertion_made());
+  EXPECT_FALSE(assert_natives_->failed());
+}
+
+void ModuleSystemTest::ExpectNoAssertionsMade() {
+  should_assertions_be_made_ = false;
+}
+
+v8::Handle<v8::Object> ModuleSystemTest::CreateGlobal(const std::string& name) {
+  v8::HandleScope handle_scope;
+  v8::Handle<v8::Object> object = v8::Object::New();
+  v8::Context::GetCurrent()->Global()->Set(v8::String::New(name.c_str()),
+                                           object);
+  return handle_scope.Close(object);
+}
---
+++ chrome/test/base/module_system_test.h
@@ -0,0 +1,54 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_TEST_BASE_MODULE_SYSTEM_TEST_H_
+#define CHROME_TEST_BASE_MODULE_SYSTEM_TEST_H_
+#pragma once
+
+#include "chrome/renderer/module_system.h"
+#include "v8/include/v8.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class AssertNatives;
+class StringSourceMap;
+
+// Test fixture for testing JS that makes use of the module system.
+//
+// Typically tests will look like:
+//
+// TEST_F(MyModuleSystemTest, TestStuff) {
+//   RegisterModule("test", "requireNative('assert').AssertTrue(true);");
+//   module_system_->Require("test");
+// }
+//
+// By default a test will fail if no method in the native module 'assert' is
+// called. This behaviour can be overridden by calling ExpectNoAssertionsMade().
+class ModuleSystemTest : public testing::Test {
+ public:
+  ModuleSystemTest();
+  virtual ~ModuleSystemTest();
+
+  virtual void TearDown() OVERRIDE;
+
+  // Register a named JS module in the module system.
+  void RegisterModule(const std::string& name, const std::string& code);
+
+  // Make the test fail if any asserts are called. By default a test will fail
+  // if no asserts are called.
+  void ExpectNoAssertionsMade();
+
+  // Create an empty object in the global scope with name |name|.
+  v8::Handle<v8::Object> CreateGlobal(const std::string& name);
+
+ protected:
+  v8::Persistent<v8::Context> context_;
+  v8::HandleScope handle_scope_;
+  v8::TryCatch try_catch_;
+  AssertNatives* assert_natives_;
+  scoped_ptr<StringSourceMap> source_map_;
+  scoped_ptr<ModuleSystem> module_system_;
+  bool should_assertions_be_made_;
+};
+
+#endif  // CHROME_TEST_BASE_MODULE_SYSTEM_TEST_H_
