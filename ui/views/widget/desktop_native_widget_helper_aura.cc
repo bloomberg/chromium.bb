471f66ccbe72173ec56ccc891fdaf6b1fbb0d263
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-09 23:40:36 +0000

    Aura desktop: Don't move the window to the origin when we get an external size change.
    BUG=125106
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10377068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136169 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e551093f590069ef822177fc694ab51131406f98

--- ui/views/widget/desktop_native_widget_helper_aura.cc
+++ ui/views/widget/desktop_native_widget_helper_aura.cc
@@ -118,7 +118,8 @@ void DesktopNativeWidgetHelperAura::OnRootWindowResized(
     const aura::RootWindow* root,
     const gfx::Size& old_size) {
   DCHECK_EQ(root, root_window_.get());
-  widget_->SetBounds(gfx::Rect(root->GetHostSize()));
+  widget_->SetBounds(gfx::Rect(root->GetHostOrigin(),
+                               root->GetHostSize()));
 }
 
 void DesktopNativeWidgetHelperAura::OnRootWindowHostClosed(
