bloomberg/chromium.bb

commit a507e4d04cd2d79eb137053460f387a9b4f5a11f

Revert "linux: Fix grabs for popups belonging to inactive tabs."
This reverts commit 33dec4faa7a508b77157875bca595a4ebaa6cda4 [formerly a1da28cad77e028c38f54c6355cfcd1b96a705df].
Combo boxes don't work properly in Chrome DevTools.
Scenario:
1) Open a page
2) open DevTools
3) open Profiler tab
4) take Heap Snapshot
5) try to change the view type from Summary to Comparison (combo box at the bottom of the DevTools window)

BUG=none
TEST=none
TBR=estade@google.com,derat@google.com,sky@google.com

Review URL: https://chromiumcodereview.appspot.com/10378088

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136293 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 1a3a98614c89db39832dcbb1620775002f5ef12b

content/browser/renderer_host/render_widget_host_view_gtk.cc
@@ -596,37 +596,35 @@ void RenderWidgetHostViewGtk::InitAsPopup(
   gtk_container_add(GTK_CONTAINER(window), view_.get());
   DoPopupOrFullscreenInit(window, pos);
 
-  // Grab all input for the app. If a click lands outside the bounds of the
-  // popup, WebKit will notice and destroy us. The underlying X window needs to
-  // be created and mapped by the above code before we can grab the input
-  // devices.
+  // The underlying X window needs to be created and mapped by the above code
+  // before we can grab the input devices.
   if (NeedsInputGrab()) {
-    // Install an application-level GTK grab to make sure that we receive all of
-    // the app's input.
+    // Grab all input for the app. If a click lands outside the bounds of the
+    // popup, WebKit will notice and destroy us. Before doing this we need
+    // to ensure that the the popup is added to the browser's window group,
+    // to allow for the grabs to work correctly.
+    gtk_window_group_add_window(gtk_window_get_group(
+        GTK_WINDOW(gtk_widget_get_toplevel(parent_))), window);
     gtk_grab_add(view_.get());
 
-    // We need to install an X grab as well. However if the app already has an X
-    // grab (as in the case of extension popup), an app grab will suffice.
+    // We need for the application to do an X grab as well. However if the app
+    // already has an X grab (as in the case of extension popup), an app grab
+    // will suffice.
     do_x_grab_ = !gdk_pointer_is_grabbed();
-    if (do_x_grab_) {
-      // Install the grab on behalf our parent window if it and all of its
-      // ancestors are mapped; otherwise, just use ourselves (maybe we're being
-      // shown on behalf of an inactive tab).
-      GdkWindow* grab_window = gtk_widget_get_window(parent_);
-      if (!grab_window || !gdk_window_is_viewable(grab_window))
-        grab_window = gtk_widget_get_window(view_.get());
 
+    // Now grab all of X's input.
+    if (do_x_grab_) {
       gdk_pointer_grab(
-          grab_window,
-          TRUE,  // Only events outside of the window are reported with
-                 // respect to |parent_->window|.
+          gtk_widget_get_window(parent_),
+          TRUE,  // Only events outside of the window are reported with respect
+                 // to |parent_->window|.
           static_cast<GdkEventMask>(GDK_BUTTON_PRESS_MASK |
               GDK_BUTTON_RELEASE_MASK | GDK_POINTER_MOTION_MASK),
           NULL,
           NULL,
           GDK_CURRENT_TIME);
       // We grab keyboard events too so things like alt+tab are eaten.
-      gdk_keyboard_grab(grab_window, TRUE, GDK_CURRENT_TIME);
+      gdk_keyboard_grab(gtk_widget_get_window(parent_), TRUE, GDK_CURRENT_TIME);
     }
   }
 }

commit 3007f8a25e8377ccf1098b36aee1a683b120275e

Reland "linux: Fix grabs for popups belonging to ..."
This relands r135966, which was reverted by r136293.  By
adding the popup to its parent's window group, the DevTools
profiler heap snapshot combobox receives mouse input again.
I'm still not sure why this is necessary.

Original description:

When displaying a popup for an inactive tab, grab input on
behalf of the popup's window rather than its parent window.

[snip obsolete, now-removed window group change]

BUG=126296
TEST=manual: original test case is still fine; dev tool regression described in r136293 is gone


Review URL: https://chromiumcodereview.appspot.com/10389080

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136451 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: a6fd68e5303ecb4700de8775ca1ed09dcfb00948

content/browser/renderer_host/render_widget_host_view_gtk.cc
@@ -596,35 +596,48 @@ void RenderWidgetHostViewGtk::InitAsPopup(
   gtk_container_add(GTK_CONTAINER(window), view_.get());
   DoPopupOrFullscreenInit(window, pos);
 
-  // The underlying X window needs to be created and mapped by the above code
-  // before we can grab the input devices.
+  // Grab all input for the app. If a click lands outside the bounds of the
+  // popup, WebKit will notice and destroy us. The underlying X window needs to
+  // be created and mapped by the above code before we can grab the input
+  // devices.
   if (NeedsInputGrab()) {
-    // Grab all input for the app. If a click lands outside the bounds of the
-    // popup, WebKit will notice and destroy us. Before doing this we need
-    // to ensure that the the popup is added to the browser's window group,
-    // to allow for the grabs to work correctly.
-    gtk_window_group_add_window(gtk_window_get_group(
-        GTK_WINDOW(gtk_widget_get_toplevel(parent_))), window);
+    // If our parent is in a widget hierarchy that ends with a window, add
+    // ourselves to the same window group to make sure that our GTK grab
+    // covers it.
+    GtkWidget* toplevel = gtk_widget_get_toplevel(parent_);
+    if (toplevel &&
+        GTK_WIDGET_TOPLEVEL(toplevel) &&
+        GTK_IS_WINDOW(toplevel)) {
+      gtk_window_group_add_window(
+          gtk_window_get_group(GTK_WINDOW(toplevel)), window);
+    }
+
+    // Install an application-level GTK grab to make sure that we receive all of
+    // the app's input.
     gtk_grab_add(view_.get());
 
-    // We need for the application to do an X grab as well. However if the app
-    // already has an X grab (as in the case of extension popup), an app grab
-    // will suffice.
+    // We need to install an X grab as well. However if the app already has an X
+    // grab (as in the case of extension popup), an app grab will suffice.
     do_x_grab_ = !gdk_pointer_is_grabbed();
-
-    // Now grab all of X's input.
     if (do_x_grab_) {
+      // Install the grab on behalf our parent window if it and all of its
+      // ancestors are mapped; otherwise, just use ourselves (maybe we're being
+      // shown on behalf of an inactive tab).
+      GdkWindow* grab_window = gtk_widget_get_window(parent_);
+      if (!grab_window || !gdk_window_is_viewable(grab_window))
+        grab_window = gtk_widget_get_window(view_.get());
+
       gdk_pointer_grab(
-          gtk_widget_get_window(parent_),
-          TRUE,  // Only events outside of the window are reported with respect
-                 // to |parent_->window|.
+          grab_window,
+          TRUE,  // Only events outside of the window are reported with
+                 // respect to |parent_->window|.
           static_cast<GdkEventMask>(GDK_BUTTON_PRESS_MASK |
               GDK_BUTTON_RELEASE_MASK | GDK_POINTER_MOTION_MASK),
           NULL,
           NULL,
           GDK_CURRENT_TIME);
       // We grab keyboard events too so things like alt+tab are eaten.
-      gdk_keyboard_grab(gtk_widget_get_window(parent_), TRUE, GDK_CURRENT_TIME);
+      gdk_keyboard_grab(grab_window, TRUE, GDK_CURRENT_TIME);
     }
   }
 }