c33507f78d193563e1324d3745a2d39aba3a4116
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-11 03:22:42 +0000

    Revert 136414 - Fixes taking a screenshot of a page in high DPI Also tagged read_pixel_bounds with _in_pixel to make it more clear what coordinate system it is in.
    BUG=127550
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/10381093
    
    TBR=pkotwicz@chromium.org
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=136416
    
    Review URL: https://chromiumcodereview.appspot.com/10332102
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136495 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a7aa47d5f7c39378db55c6af18d1ea7c791886eb

--- chrome/browser/ui/window_snapshot/window_snapshot_aura.cc
+++ chrome/browser/ui/window_snapshot/window_snapshot_aura.cc
@@ -26,22 +26,22 @@ bool GrabWindowSnapshot(gfx::NativeWindow window,
   // the desktop.
   read_pixels_bounds.set_origin(
       snapshot_bounds.origin().Add(window->bounds().origin()));
-  read_pixels_bounds =
+  gfx::Rect read_pixels_bounds_in_pixel =
       ui::ConvertRectToPixel(window->layer(), read_pixels_bounds);
 
-  DCHECK_GE(compositor->size().width(), read_pixels_bounds.right());
-  DCHECK_GE(compositor->size().height(), read_pixels_bounds.bottom());
+  DCHECK_GE(compositor->size().width(), read_pixels_bounds_in_pixel.right());
+  DCHECK_GE(compositor->size().height(), read_pixels_bounds_in_pixel.bottom());
   DCHECK_LE(0, read_pixels_bounds.x());
   DCHECK_LE(0, read_pixels_bounds.y());
 
   SkBitmap bitmap;
-  if (!compositor->ReadPixels(&bitmap, read_pixels_bounds))
+  if (!compositor->ReadPixels(&bitmap, read_pixels_bounds_in_pixel))
     return false;
 
   unsigned char* pixels = reinterpret_cast<unsigned char*>(bitmap.getPixels());
 
   gfx::PNGCodec::Encode(pixels, gfx::PNGCodec::FORMAT_BGRA,
-                        snapshot_bounds.size(),
+                        read_pixels_bounds_in_pixel.size(),
                         bitmap.rowBytes(), true,
                         std::vector<gfx::PNGCodec::Comment>(),
                         png_representation);
