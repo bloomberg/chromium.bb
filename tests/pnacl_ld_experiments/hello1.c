0cf0d11c73738258b45ea14b0960c7ad07736c3e
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-05-11 17:59:50 +0000

    Some restructuring for 64bit debugging and intial pthreads hookup.
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2745
    TEST= make run1.x8632
    Review URL: https://chromiumcodereview.appspot.com/10381091
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8575 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: d49137480e3048a34a863a7404ca661c5ebedd08

--- tests/pnacl_ld_experiments/hello1.c
+++ tests/pnacl_ld_experiments/hello1.c
@@ -16,6 +16,13 @@
 
 #include <bits/nacl_syscalls.h>
 
+
+/* ====================================================================== */
+#define USE_TLS 1
+#define USE_LIBC 1
+#define CALL_LDSO 1
+#define CALL_SIMPLESO 1
+#define USE_PTHREAD 1
 /* ====================================================================== */
 #define NACL_INSTR_BLOCK_SHIFT         5
 #define NACL_PAGESHIFT                12
@@ -26,25 +33,37 @@
 #define NACL_SYSCALL(s) ((TYPE_nacl_ ## s) NACL_SYSCALL_ADDR(NACL_sys_ ## s))
 
 /* ====================================================================== */
-/* proto-types for nacl syscalls */
+/* prototypes for nacl syscalls */
 
 typedef int (*TYPE_nacl_write) (int desc, void const *buf, int count);
 typedef void (*TYPE_nacl_null) (void);
 typedef void (*TYPE_nacl_exit) (int status);
 
 /* ====================================================================== */
-/* proto-types for functions  inside ld.so */
+/* prototypes for functions inside ld.so */
 #define REGPARM(n) __attribute__((regparm(n)))
 
 extern REGPARM(2) void _dl_get_tls_static_info(int *static_tls_size,
                                                int *static_tls_align);
-
 extern void _dl_debug_state();
+extern void *_dl_allocate_tls (void *mem);
 
+/* this may be x86 specific but we do not have ld.so for arm anyway */
+extern char *__nacl_read_tp();
+/* from src/untrusted/nacl/tls_params.h */
+ptrdiff_t __nacl_tp_tdb_offset(size_t tdb_size) asm("llvm.nacl.tp.tdb.offset");
+
+/* prototypes for functions inside libsimple.so */
 extern int fortytwo();
 
-/* This does not seem to have any effect - left for reference
- */
+/* prototypes for functions inside mylibpthread.so */
+extern int __pthread_initialize(void);
+
+/* prototypes for functions inside mylibc.so */
+extern void __newlib_thread_init(void);
+
+
+/* This does not seem to have any effect - left for reference */
 #define EXPORT __attribute__ ((visibility("default"),externally_visible))
 /* ====================================================================== */
 /* poor man's io */
@@ -74,97 +93,171 @@ ssize_t mywrite(int fd, const void* buf, size_t n)  {
   return NACL_SYSCALL(write)(fd, buf, n);
 }
 
-#define myprint(s) mywrite(1, s, mystrlen(s))
+void myprint(const char *s) {
+  mywrite(1, s, mystrlen(s));
+}
 
+void myprinthex(int n) {
+  char buffer[9];
+  myhextochar(n, buffer);
+  myprint(buffer);
+}
 /* ====================================================================== */
 /* stub functions */
 /* ====================================================================== */
 void __deregister_frame_info(const void *begin) {
-  myprint("STUB __deregister_frame_info\n");
+   myprint("STUB __deregister_frame_info\n");
 }
 
 void __register_frame_info(void *begin, void *ob) {
   myprint("STUB __register_frame_info\n");
 }
 
+/* This is usually provided by
+ * src/untrusted/nacl/pthread_initialize_minimal.c
+ */
+int __pthread_initialize_minimal(size_t tdb_size) {
+  /* ld.so does some of the work for us, e.g.
+   *  allocating and initialzing the tls area
+   * but we still need put it in a
+   * state which our libpthread expects.
+   * We also do not call  __newlib_thread_init here
+   */
+   char *tp = __nacl_read_tp();
+   void *tdb = tp + __nacl_tp_tdb_offset(tdb_size);
+   /*
+    * This should only be done for x86 - but this is all we support now anyway
+    */
+   *(void **) tdb = tdb;
+
+   /* I do not think we need this as it has been setup already: confirm */
+   /* nacl_tls_init(tdb); */
+   myprint("tdb_size: ");
+   myprinthex(tdb_size);
+   myprint("\n");
+
+   myprint("tp: ");
+   myprinthex((int)tp);
+   myprint("\n");
+
+   myprint("tdb: ");
+   myprinthex((int)tdb);
+   myprint("\n");
+
+   return 0;
+}
+
 /* ====================================================================== */
-/* dummy functions */
-/* ====================================================================== */
+/*
+ * __local_lock_acquire, etc.  only work when pthread has been initialized.
+ * We provide stubs here which will be called by newlib if the
+ * pthread implementations are not available
+ */
+
+#if USE_PTHREAD == 0
 void __local_lock_acquire() {
+  myprint("STUB __local_lock_acquire\n");
 }
 
 void __local_lock_acquire_recursive() {
+  myprint("STUB __local_lock_acquire_recursive\n");
 }
 
 void __local_lock_close_recursive() {
+  myprint("STUB __local_lock_close_recursive\n");
 }
 
 void __local_lock_init_recursive() {
+  myprint("STUB __local_lock_init_recursive\n");
 }
 
 void __local_lock_release() {
+  myprint("STUB __local_lock_release\n");
 }
 
 void __local_lock_release_recursive() {
+  myprint("STUB __local_lock_release_recursive\n");
 }
+#endif
 
 /* ====================================================================== */
-__thread int tdata1 = 1;
-__thread int tdata2 = 3;
-
+#if USE_LIBC == 1
+__thread int tdata1 = 0x73537353;
+__thread int tdata2 = 0xfaceface;
+#endif
 
 int main(int argc, char** argv, char** envp) {
-  myprint("hello world\n");
-  char buffer[9];
+  myprint("raw printing: hello world\n");
 
+  /* ======================================== */
+  /* tls initialization test
+   * NOTE: tls access works even without pthread init
+   */
+#if USE_TLS == 1
+  myprinthex(tdata1);
+  myprint(" expecting  0x73537353\n");
+
+  myprinthex(tdata2);
+  myprint(" expecting 0xfaceface\n");
+#endif
+
+  /* ======================================== */
+  /*
+   * libc calls  various pthread function which we have
+   * conditionally stubbed out above.
+   * they can only be used after __pthread_initialize
+   * has been called which enables certain irt functions.
+   */
+#if USE_PTHREAD == 1
+  /* Note: this calls __pthread_initialize_minimal */
+  __pthread_initialize();
+#endif
   /* ======================================== */
   /* this is usually called by either
    * src/untrusted/nacl/pthread_initialize_minimal.c
    * but we do not want any of the tls setup which goes with it
    */
+#if USE_LIBC == 1
   __newlib_thread_init();
   puts("libc call\n");
-  printf("another libc call %p\n", buffer);
-  printf("another libc call %p\n", &tdata1);
+  void *addr = (void *) &main;
+  double d = 3.1415;
+  printf("another libc call %p\n", addr);
+  printf("another libc call %e\n", d);
 
   time_t rawtime;
   time(&rawtime);
   printf("The current local time is: %s\n", ctime(&rawtime));
-
-
-  /* ======================================== */
-  /* tls initialization test */
-  myhextochar(tdata1, buffer);
-  myprint(buffer);
-  myprint(" expecting 1\n");
-
-  myhextochar(tdata2, buffer);
-  myprint(buffer);
-  myprint(" expecting 3\n");
+#endif
 
   /* ======================================== */
   /* call into another .so (and back) */
+#if CALL_SIMPLESO == 1
   int x = fortytwo();
-  myhextochar(x, buffer);
-  myprint(buffer);
+  myprinthex(x);
   myprint(" expecting 42\n");
+#endif
 
   /* ======================================== */
   /* trivial call into ld.so */
+#if CALL_LDSO == 1
   _dl_debug_state();
+#endif
 
   /* ======================================== */
   /* non-trivial call into ld.so */
+#if CALL_LDSO == 1
   int static_tls_size;
   int static_tls_align;
   _dl_get_tls_static_info (&static_tls_size, &static_tls_align);
-  myprint("tls size and alignment\n");
-  myhextochar(static_tls_size, buffer);
-  myprint(buffer);
+  myprint("tls size: ");
+  myprinthex(static_tls_size);
   myprint("\n");
-  myhextochar(static_tls_align, buffer);
-  myprint(buffer);
+
+  myprint("tls align: ");
+  myprinthex(static_tls_align);
   myprint("\n");
+#endif
 
   myprint("exit\n");
   return 0;
