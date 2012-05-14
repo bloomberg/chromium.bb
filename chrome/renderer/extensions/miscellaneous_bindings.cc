98a7b42696ab6b8e9899ba8d3fa852a102b57af6
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-14 23:50:23 +0000

    Don't crash the renderer if v8 throws an exception when dispatching an extension port message. It looks like this can happen when V8 hits a stack overflow.
    BUG=127932
    TEST=no
    
    Review URL: https://chromiumcodereview.appspot.com/10399012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137016 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ca68ee18f2bd69890689b70cc14b2324ef39f29f

--- chrome/renderer/extensions/miscellaneous_bindings.cc
+++ chrome/renderer/extensions/miscellaneous_bindings.cc
@@ -208,9 +208,15 @@ void MiscellaneousBindings::DispatchOnConnect(
     arguments.push_back(v8::String::New(target_extension_id.c_str(),
                                         target_extension_id.size()));
     v8::Handle<v8::Value> retval;
+    v8::TryCatch try_catch;
     if (!(*it)->CallChromeHiddenMethod("Port.dispatchOnConnect",
                                       arguments.size(), &arguments[0],
                                       &retval)) {
+      continue;
+    }
+
+    if (try_catch.HasCaught()) {
+      LOG(ERROR) << "Exception caught when calling Port.dispatchOnConnect.";
       continue;
     }
 
@@ -246,8 +252,14 @@ void MiscellaneousBindings::DeliverMessage(
     // the message.
     v8::Handle<v8::Value> port_id_handle = v8::Integer::New(target_port_id);
     v8::Handle<v8::Value> has_port;
+    v8::TryCatch try_catch;
     if (!(*it)->CallChromeHiddenMethod("Port.hasPort", 1, &port_id_handle,
                                        &has_port)) {
+      continue;
+    }
+
+    if (try_catch.HasCaught()) {
+      LOG(ERROR) << "Exception caught when calling Port.hasPort.";
       continue;
     }
 
