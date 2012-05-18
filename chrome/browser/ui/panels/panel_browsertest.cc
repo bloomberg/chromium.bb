147d1daf6fdde54919fb494c813c067388b1dd17
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 04:24:42 +0000

    Add FAILS_ to PanelAndNotificationTest.NoOverlapping.
    TBR=jianli
    BUG=118168
    TEST=Tree is green.
    
    Review URL: https://chromiumcodereview.appspot.com/10412002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137823 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6796ef404c298526294b2536b99fcc98a1fa379b

--- chrome/browser/ui/panels/panel_browsertest.cc
+++ chrome/browser/ui/panels/panel_browsertest.cc
@@ -1817,13 +1817,7 @@ class PanelAndNotificationTest : public PanelBrowserTest {
 };
 
 // crbug.com/118168
-#if defined(OS_MACOSX)
-#define MAYBE_NoOverlapping FAILS_NoOverlapping
-#else
-#define MAYBE_NoOverlapping NoOverlapping
-#endif
-
-IN_PROC_BROWSER_TEST_F(PanelAndNotificationTest, MAYBE_NoOverlapping) {
+IN_PROC_BROWSER_TEST_F(PanelAndNotificationTest, FAILS_NoOverlapping) {
   const int kPanelWidth = 200;
   const int kShortPanelHeight = 150;
   const int kTallPanelHeight = 200;
