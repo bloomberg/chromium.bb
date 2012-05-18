a6b35b550e504b7d71cf4c2a0600603de54dd8d8
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 11:02:57 +0000

    Disable PhishingClassifierDelegateTest.NoScorer
    BUG=128705
    
    Review URL: https://chromiumcodereview.appspot.com/10413003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137842 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dbbc6865d93c51f17c2791af1a27412a58f3cdf6

--- chrome/renderer/safe_browsing/phishing_classifier_delegate_browsertest.cc
+++ chrome/renderer/safe_browsing/phishing_classifier_delegate_browsertest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
@@ -222,7 +222,7 @@ TEST_F(PhishingClassifierDelegateTest, Navigation) {
   EXPECT_CALL(*classifier, CancelPendingClassification());
 }
 
-TEST_F(PhishingClassifierDelegateTest, NoScorer) {
+TEST_F(PhishingClassifierDelegateTest, DISABLED_NoScorer) {
   // For this test, we'll create the delegate with no scorer available yet.
   MockPhishingClassifier* classifier =
       new StrictMock<MockPhishingClassifier>(view());
