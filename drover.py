66bc1a513143867e724fa18536c397b4a4c2892e
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 19:34:04 +0000

    Fix typo in help text.
    No space between -- and merge.
    
    Review URL: https://chromiumcodereview.appspot.com/10428002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@138562 0039d316-1c4b-4281-b951-d872f2087c98

--- drover.py
+++ drover.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -29,7 +29,7 @@ Example: %(app)s --merge 12345 --branch 187
 
 [Merge from trunk to milestone]
 --merge <revision> --milestone <milestone_num>
-Example: %(app)s -- merge 12345 --milestone 16
+Example: %(app)s --merge 12345 --milestone 16
 
 [Merge from trunk to local copy]
 --merge <revision> --local
