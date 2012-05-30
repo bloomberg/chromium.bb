8c6cbcc62c99d93fc18f8dd7ea52400054118cdc
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 09:00:56 +0000

    Revert 139472 - Revert 139404 - ash: Allow pinch gestures to resize the window.
    139398 is suspected to windows browser_builds.
    
    BUG=121140
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10454045
    
    TBR=sadrul@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10456023
    
    TBR=kinuko@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10443078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139505 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 78666b9220ad679c8e758188a0797a6362a5f3b6

--- ash/wm/system_gesture_event_filter.cc
+++ ash/wm/system_gesture_event_filter.cc
@@ -8,8 +8,10 @@
 #include "ash/shell.h"
 #include "ash/system/brightness/brightness_control_delegate.h"
 #include "ash/volume_control_delegate.h"
+#include "ash/wm/property_util.h"
 #include "ash/wm/window_resizer.h"
 #include "ash/wm/window_util.h"
+#include "ash/wm/workspace/phantom_window_controller.h"
 #include "ash/wm/workspace/snap_sizer.h"
 #include "ui/aura/event.h"
 #include "ui/aura/root_window.h"
@@ -44,7 +46,9 @@ namespace internal {
 class SystemPinchHandler {
  public:
   explicit SystemPinchHandler(aura::Window* target)
-      : target_(target) {
+      : target_(target),
+        phantom_(target),
+        pinch_factor_(1.) {
     widget_ = views::Widget::GetWidgetForNativeWindow(target_);
   }
 
@@ -53,35 +57,77 @@ class SystemPinchHandler {
 
   SystemGestureStatus ProcessGestureEvent(const aura::GestureEvent& event) {
     // The target has changed, somehow. Let's bale.
-    if (!widget_)
+    if (!widget_ || !widget_->widget_delegate()->CanResize())
       return SYSTEM_GESTURE_END;
 
     switch (event.type()) {
-      case ui::ET_GESTURE_TAP_UP:
-        if (event.delta_x() == kSystemPinchPoints) {
-          gfx::Rect bounds = target_->bounds();
-          int grid = Shell::GetInstance()->GetGridSize();
-          bounds.set_x(WindowResizer::AlignToGridRoundUp(bounds.x(), grid));
-          bounds.set_y(WindowResizer::AlignToGridRoundUp(bounds.y(), grid));
-          target_->SetBounds(bounds);
-          return SYSTEM_GESTURE_END;
-        }
-        break;
-
-      case ui::ET_GESTURE_SCROLL_UPDATE: {
-        if (wm::IsWindowFullscreen(target_) ||
-            !widget_->widget_delegate()->CanResize())
+      case ui::ET_GESTURE_TAP_UP: {
+        if (event.delta_x() > kSystemPinchPoints)
           break;
 
-        gfx::Rect bounds = target_->bounds();
-        bounds.set_x(static_cast<int>(bounds.x() + event.delta_x()));
-        bounds.set_y(static_cast<int>(bounds.y() + event.delta_y()));
-        target_->SetBounds(bounds);
+        gfx::Rect bounds = phantom_.IsShowing() ?  phantom_.bounds() :
+                                                   target_->bounds();
+        int grid = Shell::GetInstance()->GetGridSize();
+        bounds.set_x(WindowResizer::AlignToGridRoundUp(bounds.x(), grid));
+        bounds.set_y(WindowResizer::AlignToGridRoundUp(bounds.y(), grid));
+        if (wm::IsWindowFullscreen(target_) || wm::IsWindowMaximized(target_)) {
+          SetRestoreBounds(target_, bounds);
+          wm::RestoreWindow(target_);
+        } else {
+          target_->SetBounds(bounds);
+        }
+        return SYSTEM_GESTURE_END;
+      }
+
+      case ui::ET_GESTURE_SCROLL_UPDATE: {
+        if (wm::IsWindowFullscreen(target_) || wm::IsWindowMaximized(target_)) {
+          if (!phantom_.IsShowing())
+            break;
+        } else {
+          gfx::Rect bounds = target_->bounds();
+          bounds.set_x(static_cast<int>(bounds.x() + event.delta_x()));
+          bounds.set_y(static_cast<int>(bounds.y() + event.delta_y()));
+          target_->SetBounds(bounds);
+        }
+
+        if (phantom_.IsShowing()) {
+          gfx::Rect bounds = phantom_.bounds();
+          bounds.set_x(static_cast<int>(bounds.x() + event.delta_x()));
+          bounds.set_y(static_cast<int>(bounds.y() + event.delta_y()));
+          phantom_.SetBounds(bounds);
+        }
         break;
       }
 
       case ui::ET_GESTURE_PINCH_UPDATE: {
-        // TODO(sad): Process PINCH_UPDATEs too.
+        // The PINCH_UPDATE events contain incremental scaling updates.
+        pinch_factor_ *= event.delta_x();
+        gfx::Rect bounds = target_->bounds();
+
+        if (wm::IsWindowFullscreen(target_) || wm::IsWindowMaximized(target_)) {
+          // For a fullscreen/maximized window, if you start pinching in, and
+          // you pinch enough, then it shows the phantom window with the
+          // restore-bounds. The subsequent pinch updates then work on the
+          // restore bounds instead of the fullscreen/maximized bounds.
+          const gfx::Rect* restore = NULL;
+          if (phantom_.IsShowing()) {
+            restore = GetRestoreBounds(target_);
+          } else if (pinch_factor_ < 0.8) {
+            restore = GetRestoreBounds(target_);
+            // Reset the pinch factor.
+            pinch_factor_ = 1.0;
+          }
+
+          if (restore)
+            bounds = *restore;
+          else
+            break;
+        }
+
+        gfx::Rect new_bounds = bounds.Scale(pinch_factor_);
+        new_bounds.set_x(bounds.x() + (event.x() - event.x() * pinch_factor_));
+        new_bounds.set_y(bounds.y() + (event.y() - event.y() * pinch_factor_));
+        phantom_.Show(new_bounds);
         break;
       }
 
@@ -95,6 +141,9 @@ class SystemPinchHandler {
  private:
   aura::Window* target_;
   views::Widget* widget_;
+
+  PhantomWindowController phantom_;
+  double pinch_factor_;
 
   DISALLOW_COPY_AND_ASSIGN(SystemPinchHandler);
 };
