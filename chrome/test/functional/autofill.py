683f62c08999e7425c9199c38b8a9bd093778d1a
dyu@chromium.org <dyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 18:17:54 +0000

    Add a test to verify correct tab order in autofill.
    This test verifies the correct tab ordering through the fields when adding an address.
    
    TEST=none
    BUG=125570
    NOTRY=true
    Review URL: https://chromiumcodereview.appspot.com/10332223
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139579 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1c7c4788096927bdeab055681c4d7853b9cbe9fd

--- chrome/test/functional/autofill.py
+++ chrome/test/functional/autofill.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -7,16 +7,24 @@ import logging
 import os
 import pickle
 import re
+import simplejson
 
 import autofill_dataset_converter
 import autofill_dataset_generator
 import pyauto_functional  # Must be imported before pyauto
 import pyauto
 import test_utils
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_pages import settings
 
 
 class AutofillTest(pyauto.PyUITest):
   """Tests that autofill works correctly"""
+
+  def setUp(self):
+    pyauto.PyUITest.setUp(self)
+    self._driver = self.NewWebDriver()
 
   def Debug(self):
     """Test method for experimentation.
@@ -731,12 +739,9 @@ class AutofillTest(pyauto.PyUITest):
     """Verify postal code and state labels based on selected country."""
     data_file = os.path.join(self.DataDir(), 'autofill', 'functional',
                              'state_zip_labels.txt')
-    import simplejson
-    from webdriver_pages import settings
     test_data = simplejson.loads(open(data_file).read())
 
-    driver = self.NewWebDriver()
-    page = settings.AutofillEditAddressDialog.FromNavigation(driver)
+    page = settings.AutofillEditAddressDialog.FromNavigation(self._driver)
     # Initial check of State and ZIP labels.
     self.assertEqual('State', page.GetStateLabel())
     self.assertEqual('ZIP code', page.GetPostalCodeLabel())
@@ -762,9 +767,7 @@ class AutofillTest(pyauto.PyUITest):
 
   def testNoDuplicatePhoneNumsInPrefs(self):
     """Test duplicate phone numbers entered in prefs are removed."""
-    from webdriver_pages import settings
-    driver = self.NewWebDriver()
-    page = settings.AutofillEditAddressDialog.FromNavigation(driver)
+    page = settings.AutofillEditAddressDialog.FromNavigation(self._driver)
     non_duplicates = ['111-1111', '222-2222']
     duplicates = ['111-1111']
     page.Fill(phones=non_duplicates + duplicates)
@@ -773,16 +776,15 @@ class AutofillTest(pyauto.PyUITest):
 
   def testDisplayLineItemForEntriesWithNoCCNum(self):
     """Verify Autofill creates a line item for CC entries with no CC number."""
-    driver = self.NewWebDriver()
     self.NavigateToURL('chrome://settings-frame/autofillEditCreditCard')
-    driver.find_element_by_id('name-on-card').send_keys('Jane Doe')
+    self._driver.find_element_by_id('name-on-card').send_keys('Jane Doe')
     query_month = self._SelectOptionXpath('12')
     query_year = self._SelectOptionXpath('2014')
-    driver.find_element_by_id('expiration-month').find_element_by_xpath(
+    self._driver.find_element_by_id('expiration-month').find_element_by_xpath(
         query_month).click()
-    driver.find_element_by_id('expiration-year').find_element_by_xpath(
+    self._driver.find_element_by_id('expiration-year').find_element_by_xpath(
         query_year).click()
-    driver.find_element_by_id(
+    self._driver.find_element_by_id(
         'autofill-edit-credit-card-apply-button').click()
     # Refresh the page to ensure the UI is up-to-date.
     driver.refresh()
@@ -790,6 +792,109 @@ class AutofillTest(pyauto.PyUITest):
     self.assertTrue(list_entry.is_displayed)
     self.assertEqual('Jane Doe', list_entry.text,
                      msg='Saved CC line item not same as what was entered.')
+
+  def _GetElementList(self, container_elem, fields_to_select):
+    """Returns all sub elements of specific characteristics.
+
+    Args:
+      container_elem: An element that contains other elements.
+      fields_to_select: A list of fields to select with strings that
+                        help create an xpath string, which in turn identifies
+                        the elements needed.
+                        For example: ['input', 'button']
+                        ['div[@id]', 'button[@disabled]']
+                        ['*[class="example"]']
+
+    Returns:
+      List of all subelements found in the container element.
+    """
+    self.assertTrue(fields_to_select, msg='No fields specified for selection.')
+    fields_to_select = ['.//' + field for field in fields_to_select]
+    xpath_arg = ' | '.join(fields_to_select)
+    field_elems = container_elem.find_elements_by_xpath(xpath_arg)
+    return field_elems
+
+  def _GetElementInfo(self, element):
+    """Returns visual comprehensive info about an element.
+
+    This function identifies the text of the correspoinding label when tab
+    ordering fails.
+    This info consists of:
+      The labels, buttons, ids, placeholder attribute values, or the element id.
+
+    Args:
+      element: The target element.
+
+    Returns:
+      A string that identifies the element in the page.
+    """
+    element_info = ''
+    if element.tag_name == 'button':
+      element_info = element.text
+    element_info = (element_info or element.get_attribute('id') or
+        element.get_attribute('placeholder') or
+        element.get_attribute('class') or element.id)
+    return '%s: %s' % (element.tag_name, element_info)
+
+  def _LoadPageAndGetFieldList(self):
+    """Navigate to autofillEditAddress page and finds the elements with focus.
+
+    These elements are of input, select, and button types.
+
+    Returns:
+      A list with all elements that can receive focus.
+    """
+    url = 'chrome://settings-frame/autofillEditAddress'
+    self._driver.get(url)
+    container_elem = self._driver.find_element_by_id(
+        'autofill-edit-address-overlay')
+    # The container element contains input, select and button fields. Some of
+    # the buttons are disabled so they are ignored.
+    field_list = self._GetElementList(container_elem,
+                                      ['input', 'select',
+                                       'button[not(@disabled)]'])
+    self.assertTrue(field_list, 'No fields found in "%s".' % url)
+    return field_list
+
+  def testTabOrderForEditAddress(self):
+    """Verify the TAB ordering for Edit Address page is correct."""
+    tab_press = ActionChains(self._driver).send_keys(Keys.TAB)
+    field_list = self._LoadPageAndGetFieldList()
+
+    # Creates a dictionary where a field key returns the value of the next field
+    # in the field list. The last field of the field list is mapped to the first
+    # field of the field list.
+    field_nextfield_dict = dict(
+        zip(field_list, field_list[1:] + field_list[:1]))
+
+    # Tab pressed for the first time.
+    tab_press.perform()
+    # Wait until a field of |field_list| has received the focus.
+    self.WaitUntil(lambda:
+                   self._driver.switch_to_active_element().id in
+                   [f.id for f in field_list])
+    # The first field is expected to receive the focus.
+    self.assertEqual(self._driver.switch_to_active_element().id,
+                     field_list[0].id,
+                     msg='The first field did not receive tab focus.')
+    for field in field_list:
+      tab_press.perform()
+      # Wait until a field of |field_list|, other than the current field, has
+      # received the focus.
+      self.WaitUntil(lambda:
+                     self._driver.switch_to_active_element().id != field.id and
+                     self._driver.switch_to_active_element().id in
+                     [f.id for f in field_list])
+
+      self.assertEqual(self._driver.switch_to_active_element().id,
+                       field_nextfield_dict[field].id,
+                       msg=('The TAB ordering is broken. Previous field: "%s"\n'
+                            'Field expected to receive focus: "%s"\n'
+                            'Field that received focus instead: "%s"')
+                       % (self._GetElementInfo(field),
+                          self._GetElementInfo(field_nextfield_dict[field]),
+                          self._GetElementInfo(
+                              self._driver.switch_to_active_element())))
 
 
 if __name__ == '__main__':
