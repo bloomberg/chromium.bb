7b9073802c8205f6a4608f14bedd644ab4cec41c
Christian Egli <christian.egli@alumni.ethz.ch>
2012-05-30 15:16:08 +0000

    Convert the letterdeftest test so that it returns the result of the test. Also use the \xhhhh notation for unicode so that it works with the c based test framework.

--- tests/letterDefTest.c
+++ tests/letterDefTest.c
@@ -1,38 +1,21 @@
-#include <locale.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <wchar.h>
 #include "liblouis.h"
+#include "brl_checks.h"
 
-int main(int argc, char **argv) {
-  wchar_t *text = L"⠍⠎";
-  wchar_t *expected = L"⠎⠍";
-  const char* tableList = "./tables/letterDefTest.ctb";
-  setlocale(LC_ALL, "en_GB.UTF-8");
+int main(int argc, char **argv)
+{
 
-  wchar_t *inbuf;
-  wchar_t *outbuf;
-  int inlen;
-  int outlen;
-  int i = 0;
-  int expectedlen = strlen(expected);
-  inlen = strlen(text) * 2;
-  outlen = inlen;
-  inbuf = malloc(sizeof(wchar_t) * inlen);
-  outbuf = malloc(sizeof(wchar_t) * outlen);
-  printf("%ls\n", text);
-  for (i = 0; i < inlen; i++) {
-      inbuf[i] = text[i];
-  }
-  if (!lou_translate(tableList, inbuf, &inlen, outbuf, &outlen,
-                     NULL, NULL, NULL, NULL, NULL, 0)) {
-      printf("Translation failed.\n");
-      return 1;
-    }
-  printf("%ls\n", outbuf);
-  free(inbuf);
-  free(outbuf);
-  lou_free();
-  return 0;
+  int result = 0;
+
+  const char *text = "\\x280d\\x280e";  // "⠍⠎"
+  const char *expected = "sm";  // "⠎⠍"
+
+  result |= check_translation("letterDefTest_letter.ctb", text, NULL, expected);
+  result |= check_translation("letterDefTest_lowercase.ctb", text, NULL, expected);
+  result |= check_translation("letterDefTest_uplow.ctb", text, NULL, expected);
+  result |= check_translation("letterDefTest_uppercase.ctb", text, NULL, expected);
+
+  return result;
 }
