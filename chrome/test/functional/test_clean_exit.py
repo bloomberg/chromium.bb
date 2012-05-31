55cadde33d5ffae84de8b9d58ea1e7403dbb1d87
asharif@chromium.org <asharif@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-31 19:34:33 +0000

    Added WaitUntil to remove race between reading/writing file.
    This test is flaky because we read the output of strace without waiting for it
    to finish writing to it. This is eliminated by adding a WaitUtil() statement
    which waits until lsof returns an emtpy string.
    
    BUG=none
    TEST=python test_clean_exit.py
    
    
    Review URL: https://chromiumcodereview.appspot.com/10446067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139839 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6eed4094d08609af962197b99d2348d442d00d72

--- chrome/test/functional/test_clean_exit.py
+++ chrome/test/functional/test_clean_exit.py
@@ -3,7 +3,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 import os
+import signal
+import subprocess
 import tempfile
 import unittest
 
@@ -20,12 +23,12 @@ class SimpleTest(pyauto.PyUITest):
     Returns:
       A list of extra flags to pass to Chrome when it is launched.
     """
-    # Extra flag needed by scroll performance tests.
     fd, self._strace_log = tempfile.mkstemp()
     os.close(fd)
     extra_flags = ['--no-sandbox', '--renderer-clean-exit',
                    '--renderer-cmd-prefix=/usr/bin/strace -o %s' %
                     self._strace_log]
+    logging.debug('Strace file is: %s' % self._strace_log)
     return pyauto.PyUITest.ExtraChromeFlags(self) + extra_flags
 
 
@@ -33,10 +36,15 @@ class SimpleTest(pyauto.PyUITest):
     """Ensures the renderer process cleanly exits."""
     url = self.GetHttpURLForDataPath('title2.html')
     self.NavigateToURL(url)
-    test_utils.CrashBrowser(self)
+    os.kill(self.GetBrowserInfo()['browser_pid'], signal.SIGINT)
+    self.WaitUntil(lambda: self._IsFileOpen(self._strace_log))
     strace_contents = open(self._strace_log).read()
     self.assertTrue('exit_group' in strace_contents)
     os.remove(self._strace_log)
+
+  def _IsFileOpen(self, filename):
+    p = subprocess.Popen(['lsof', filename])
+    return p.communicate()[0] == ''
 
 
 if __name__ == '__main__':
