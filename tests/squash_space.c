3c6dc9a2a4e279849ce3cf74719f495b3bbfcc94
Christian Egli <christian.egli@alumni.ethz.ch>
2012-06-01 21:06:45 +0000

    Simplify the code

--- tests/squash_space.c
+++ tests/squash_space.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv)
 {
 
   int result = 0;
-  const char *tests_using_repeated[] = {
+  const char *tests[] = {
     /* first column is the text, the second is the expected value */
     " ", " ",
     /* the usual case */
@@ -46,23 +46,25 @@ int main(int argc, char **argv)
     "\n    ",     "\n "
   };
 
-  int tests_len = sizeof(tests_using_repeated)/sizeof(char*);
+  int tests_len = sizeof(tests)/sizeof(char*);
 
   for (int i = 0; i < tests_len; i += 2)
-    result |= check_translation("squash_space_with_repeated.utb", tests_using_repeated[i], NULL, tests_using_repeated[i+1]);
+    result |= check_translation("squash_space_with_repeated.utb", tests[i], NULL, tests[i+1]);
 
-  const char *tests[] = {
-    /* first column is the text, the second is the expected value */
-    " ", " ",
-    /* the usual case */
-    "                                                                                ", " ",
+  /* A number of strings that we want to squash, i.e. they should all
+     result in an output of one space */
+  const char *strings[] = {
+    /* a simple space */
+    " ",
+    /* a long string */
+    "                                                                                ",
     /* a very long string */
     "                                                                                " \
     "                                                                                " \
     "                                                                                " \
     "                                                                                " \
     "                                                                                " \
-    "                                                                                ", " ",
+    "                                                                                ",
     /* an even longer string */
     "                                                                                " \
     "                                                                                " \
@@ -79,29 +81,30 @@ int main(int argc, char **argv)
     "                                                                                " \
     "                                                                                " \
     "                                                                                " \
-    "                                                                                ", " ",
-    /* a string containing tabs */
-    "    \t",     " ",
-    "    \t    ", " ",
-    "\t    ",     " ",
+    "                                                                                ",
+    /* a couple of strings containing tabs */
+    "    \t",
+    "    \t    ",
+    "\t    ",
     /* Strings containing newlines */
-    "    \n",     " ",
-    "    \n    ", " ",
-    "\n    ",     " ",
+    "    \n",
+    "    \n    ",
+    "\n    ",
     /* All mixed */
-    "    \n    \t    \n    \t\t    \n\n\n\t\t    ",     " ",
+    "    \n    \t    \n    \t\t    \n\n\n\t\t    ",
   };
+  const char *expected = " ";
 
-  tests_len = sizeof(tests)/sizeof(char*);
+  tests_len = sizeof(strings)/sizeof(char*);
 
-  for (int i = 0; i < tests_len; i += 2)
-    result |= check_translation("squash_space_with_correct.utb", tests[i], NULL, tests[i+1]);
+  for (int i = 0; i < tests_len; i++)
+    result |= check_translation("squash_space_with_correct.utb", strings[i], NULL, expected);
 
-  for (int i = 0; i < tests_len; i += 2)
-    result |= check_translation("squash_space_with_context_1.utb", tests[i], NULL, tests[i+1]);
+  for (int i = 0; i < tests_len; i++)
+    result |= check_translation("squash_space_with_context_1.utb", strings[i], NULL, expected);
 
-  for (int i = 0; i < tests_len; i += 2)
-    result |= check_translation("squash_space_with_context_2.utb", tests[i], NULL, tests[i+1]);
+  for (int i = 0; i < tests_len; i++)
+    result |= check_translation("squash_space_with_context_2.utb", strings[i], NULL, expected);
 
 
   return result;
