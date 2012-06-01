4b080045baa52831bfeabbd58d315dafe6c289b4
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-01 07:04:06 +0000

    Add unit tests for crbug.com/130519.
    BUG=130519
    TEST=ran the test locally
    
    
    Review URL: https://chromiumcodereview.appspot.com/10443106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139984 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: abc9329e46f22b9dbd7ff6c5ac8c8f8e090dfbb6

---
+++ chrome/browser/ui/views/ash/ime_controller_chromeos_unittest.cc
@@ -0,0 +1,78 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/basictypes.h"
+#include "chrome/browser/chromeos/input_method/mock_input_method_manager.h"
+#include "chrome/browser/ui/views/ash/ime_controller_chromeos.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/accelerators/accelerator.h"
+#include "ui/base/events.h"
+#include "ui/base/keycodes/keyboard_codes.h"
+
+class ImeControllerTest : public testing::Test {
+ public:
+  ImeControllerTest() : mock_input_method_manager_(NULL) {}
+  virtual ~ImeControllerTest() {}
+
+  virtual void SetUp() OVERRIDE {
+    mock_input_method_manager_ =
+        new chromeos::input_method::MockInputMethodManager;
+    chromeos::input_method::InputMethodManager::InitializeForTesting(
+        mock_input_method_manager_);
+  }
+
+  virtual void TearDown() OVERRIDE {
+    chromeos::input_method::InputMethodManager::Shutdown();
+    mock_input_method_manager_ = NULL;
+  }
+
+ protected:
+  ImeController controller_;
+  chromeos::input_method::MockInputMethodManager* mock_input_method_manager_;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ImeControllerTest);
+};
+
+TEST_F(ImeControllerTest, TestRemapAccelerator) {
+  mock_input_method_manager_->SetCurrentInputMethodId("xkb:us::eng");
+  {
+    ui::Accelerator accel(ui::VKEY_A, ui::EF_CONTROL_DOWN);
+    ui::Accelerator remapped = controller_.RemapAccelerator(accel);
+    EXPECT_EQ(accel, remapped);
+    accel.set_type(ui::ET_KEY_RELEASED);
+    remapped = controller_.RemapAccelerator(accel);
+    EXPECT_EQ(accel, remapped);  // crbug.com/130519
+  }
+  mock_input_method_manager_->SetCurrentInputMethodId("xkb:fr::fra");
+  {
+    // Control+A shouldn't be remapped even when the current layout is FR.
+    ui::Accelerator accel(ui::VKEY_A, ui::EF_CONTROL_DOWN);
+    ui::Accelerator remapped = controller_.RemapAccelerator(accel);
+    EXPECT_EQ(accel, remapped);
+    accel.set_type(ui::ET_KEY_RELEASED);
+    remapped = controller_.RemapAccelerator(accel);
+    EXPECT_EQ(accel, remapped);
+  }
+  {
+    // Shift+A shouldn't be remapped even when the current layout is FR.
+    ui::Accelerator accel(ui::VKEY_A, ui::EF_SHIFT_DOWN);
+    ui::Accelerator remapped = controller_.RemapAccelerator(accel);
+    EXPECT_EQ(accel, remapped);
+    accel.set_type(ui::ET_KEY_RELEASED);
+    remapped = controller_.RemapAccelerator(accel);
+    EXPECT_EQ(accel, remapped);
+  }
+  {
+    // Shift+1 should be remapped when the current layout is FR.
+    ui::Accelerator accel(ui::VKEY_1, ui::EF_SHIFT_DOWN);
+    ui::Accelerator remapped = controller_.RemapAccelerator(accel);
+    ui::Accelerator expected(ui::VKEY_1, 0);
+    EXPECT_EQ(expected, remapped);
+    accel.set_type(ui::ET_KEY_RELEASED);
+    remapped = controller_.RemapAccelerator(accel);
+    expected.set_type(ui::ET_KEY_RELEASED);
+    EXPECT_EQ(expected, remapped);
+  }
+}
