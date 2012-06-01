70cccb5cb73314759bc7845c608c1b93223ec02b
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-06-01 15:05:23 +0000

    Fix pnacl's windows tc builder script invocation. Review URL: https://chromiumcodereview.appspot.com/10443126
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8779 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: ca6713fb80b69dff7e8a6b2f87e2bd2d14b7be00

--- buildbot/buildbot_pnacl.bat
+++ buildbot/buildbot_pnacl.bat
@@ -1,9 +1,9 @@
-:: Copyright (c) 2011 The Native Client Authors. All rights reserved.
+:: Copyright (c) 2012 The Native Client Authors. All rights reserved.
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
 setlocal
 set "PATH=c:\cygwin\bin;%PATH%"
-bash -c "buildbot/buildbot_toolchain_arm_untrusted.sh %*"
+bash -c "buildbot/buildbot_pnacl.sh %*"
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 endlocal
