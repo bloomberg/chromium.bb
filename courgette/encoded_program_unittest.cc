c99eadc89b0262549bb4cc9e5d1341147d9bb723
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-01 19:24:53 +0000

    [Leak fixlet] Fix leak in courgette_unittests
    BUG=46649
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10453106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140077 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 408cdd584c8f309bf3f036feffbadf1943c8f09c

--- courgette/encoded_program_unittest.cc
+++ courgette/encoded_program_unittest.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2009 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,6 +29,7 @@ TEST(EncodedProgramTest, Test) {
 
   courgette::SinkStreamSet sinks;
   EXPECT_TRUE(program->WriteTo(&sinks));
+  delete program;
 
   courgette::SinkStream sink;
   bool can_collect = sinks.CopyTo(&sink);
@@ -49,6 +50,7 @@ TEST(EncodedProgramTest, Test) {
   courgette::SinkStream assembled;
   bool can_assemble = encoded2->AssembleTo(&assembled);
   EXPECT_TRUE(can_assemble);
+  delete encoded2;
 
   const void* assembled_buffer = assembled.Buffer();
   size_t assembled_length = assembled.Length();
