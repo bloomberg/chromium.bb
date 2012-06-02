971d44a25acb3747857c6656580688bacffc802b
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-02 17:31:20 +0000

    add missing file from previous checkin
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140198 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 92ca91e6a9ba0b98f179d7a199d94c9042816f87

--- content/test/accessibility_test_utils_win.cc
+++ content/test/accessibility_test_utils_win.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/test/accessibility_test_utils_win.h"
+#include "content/public/test/accessibility_test_utils_win.h"
 
 #include <oleacc.h>
 
