01f64e951395a917601480ced4a137b6c9113961
mdm@chromium.org <mdm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-05 01:16:39 +0000

    Add a new version of the power save blocker class, to be implemented for all platforms in subsequent CLs. Review URL: https://chromiumcodereview.appspot.com/10511021
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140460 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d582b82df6f471b58feaa4167d7e448ade9ea9a9

--- content/browser/power_save_blocker.h
+++ content/browser/power_save_blocker.h
@@ -6,9 +6,11 @@
 #define CONTENT_BROWSER_POWER_SAVE_BLOCKER_H_
 #pragma once
 
+#include <string>
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/memory/ref_counted.h"
 #include "content/common/content_export.h"
 
 // A RAII-style class to block the system from entering low-power (sleep) mode.
@@ -52,5 +54,50 @@ class CONTENT_EXPORT PowerSaveBlocker {
 
   DISALLOW_COPY_AND_ASSIGN(PowerSaveBlocker);
 };
+
+namespace content {
+
+// NOT READY YET. PowerSaveBlocker above is soon to be replaced by this class,
+// but it's not done yet so client code should use the one above for now.
+// A RAII-style class to block the system from entering low-power (sleep) mode.
+class CONTENT_EXPORT PowerSaveBlocker2 {
+ public:
+  enum PowerSaveBlockerType {
+    // Prevent the application from being suspended. On some platforms, apps may
+    // be suspended when they are not visible to the user. This type of block
+    // requests that the app continue to run in that case, and on all platforms
+    // prevents the system from sleeping.
+    // Example use cases: downloading a file, playing audio.
+    kPowerSaveBlockPreventAppSuspension,
+
+    // Prevent the display from going to sleep. This also has the side effect of
+    // preventing the system from sleeping, but does not necessarily prevent the
+    // app from being suspended on some platforms if the user hides it.
+    // Example use case: playing video.
+    kPowerSaveBlockPreventDisplaySleep,
+  };
+
+  // Pass in the type of power save blocking desired. If multiple types of
+  // blocking are desired, instantiate one PowerSaveBlocker for each type.
+  // |reason| may be provided to the underlying system APIs on some platforms.
+  PowerSaveBlocker2(PowerSaveBlockerType type, const std::string& reason);
+  ~PowerSaveBlocker2();
+
+ private:
+  class Delegate;
+
+  // Implementations of this class may need a second object with different
+  // lifetime than the RAII container, or additional storage. This member is
+  // here for that purpose. If not used, just define the class as an empty
+  // RefCounted like so to make it compile:
+  // class PowerSaveBlocker2::Delegate
+  //     : public RefCounted<PowerSaveBlocker2::Delegate> {
+  // };
+  scoped_refptr<Delegate> delegate_;
+
+  DISALLOW_COPY_AND_ASSIGN(PowerSaveBlocker2);
+};
+
+}  // namespace content
 
 #endif  // CONTENT_BROWSER_POWER_SAVE_BLOCKER_H_
