c9e7a224a5b956f8fc0f4a46da6c2c52b020da71
Mesar Hameed <mesar.hameed@gmail.com>
2012-06-05 09:03:11 +0000

    runHarnessV2.py, docs/liblouis.texi 'type' is a python reserved keyword, use testmode instead. (Thanks Attila for patch).

--- tests/runHarnessV2.py
+++ tests/runHarnessV2.py
@@ -61,7 +61,7 @@ def showCurPos(length, pos1, marker1="^", pos2=None, marker2="*"):
     return "".join(display)
 
 class BrailleTest():
-    def __init__(self, harnessName, table, txt, brl, mode=0, cursorPos=None, brlCursorPos=None, comment=None, test_type=None):
+    def __init__(self, harnessName, table, txt, brl, mode=0, cursorPos=None, brlCursorPos=None, comment=None, testmode=None):
         self.harnessName = harnessName
         self.table = table
         self.txt = txt
@@ -70,13 +70,13 @@ class BrailleTest():
         self.cursorPos = cursorPos
         self.expectedBrlCursorPos = brlCursorPos
         self.comment = comment
-        self.test_type = 'translate' if not test_type else test_type
+        self.testmode = 'translate' if not testmode else testmode
 
     def __str__(self):
         return "%s: %s" % (self.harnessName, self.txt)
 
     def check_translate(self):
-        if self.test_type != 'translate': return
+        if self.testmode != 'translate': return
         if self.cursorPos is not None:
             tBrl, temp1, temp2, tBrlCurPos = translate(self.table, self.txt, mode=self.mode, cursorPos=self.cursorPos)
         else:
@@ -93,19 +93,20 @@ class BrailleTest():
         assert tBrl == self.expectedBrl, u("\n".join(report))
 
     def check_backtranslate(self):
-        if self.test_type != 'backtranslate': return
+        if self.testmode != 'backtranslate': return
         backtranslate_output = backTranslateString(self.table, self.expectedBrl, None, mode=self.mode)
         template = "%-25s '%s'"
         report = [
             self.__str__(),
             "--- Backtranslate failure: ---",
-            template % ("expected word:", self.txt),
-            template % ("actual backtranslated word:", backtranslate_output),
+            template % ("expected text:", self.txt),
+            template % ("actual backtranslated text:", backtranslate_output),
             "--- end ---",
         ]
         assert backtranslate_output == self.txt, u("\n".join(report))
 
     def check_cursor(self):
+        if self.testmode != 'translate': return
         if self.cursorPos == None: return
         tBrl, temp1, temp2, tBrlCurPos = translate(self.table, self.txt, mode=self.mode, cursorPos=self.cursorPos)
         template = "%-25s '%s'"
