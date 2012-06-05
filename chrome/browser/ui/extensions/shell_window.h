8df825777b8b1618e7a09a79f31940443308b447
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-05 03:47:26 +0000

    Revert r140472 and r140475.
    r140472 broke Chromium OS Tests 2: http://build.chromium.org/p/chromium.chromiumos/buildstatus?builder=Linux%20ChromiumOS%20Tests%20%282%29&number=3037
    
    Revert "Fix linux_chromeos_clang build break."
    
    This reverts commit 84a24efa0812903945c14899578039358e85ffa6 [formerly 3e2d2fc688f3b53aad07d1823659de1e72874e95].
    
    Revert "[win] Make the HTML5 fullscreen API work in system apps."
    
    This reverts commit 9c366922b594f15d0175a2519e685d42473c7e94 [formerly 84aa895cec5487b872e954ddb6702f448501cddd].
    
    BUG=None
    TEST=./out/Debug/browser_tests --gtest_filter='*LaunchPinned*' pass
    TBR=jeremya@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10531003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140486 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 915344d9e1057eaa4bb8aa62bfdbaafb94234bf0

--- chrome/browser/ui/extensions/shell_window.h
+++ chrome/browser/ui/extensions/shell_window.h
@@ -76,9 +76,6 @@ class ShellWindow : public content::NotificationObserver,
   // startup and from within UpdateWindowTitle().
   virtual string16 GetTitle() const;
 
-  virtual void SetFullscreen(bool fullscreen) {}
-  virtual bool IsFullscreenOrPending() const;
-
  private:
   // PlatformAppBrowserTest needs access to web_contents()
   friend class PlatformAppBrowserTest;
@@ -108,10 +105,6 @@ class ShellWindow : public content::NotificationObserver,
       content::WebContents* source, const gfx::Rect& pos) OVERRIDE;
   virtual void NavigationStateChanged(const content::WebContents* source,
                                       unsigned changed_flags) OVERRIDE;
-  virtual void ToggleFullscreenModeForTab(content::WebContents* source,
-                                          bool enter_fullscreen) OVERRIDE;
-  virtual bool IsFullscreenForTabOrPending(
-      const content::WebContents* source) const OVERRIDE;
 
   // content::NotificationObserver implementation.
   virtual void Observe(int type,
