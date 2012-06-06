c669474d7204073b1ce5aedfb3f71c8c1e7ef38e
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 12:57:44 +0000

    Upstream Android proxy config service.
    This change adds in an Android-specific system proxy provider called
    ProxyConfigServiceAndroid. This is created as required from C++. In
    turn ProxyConfigServiceAndroid creates a java object which provides
    the link to Android's proxy changed broadcast, as well as the
    functionality to read the Java system properties where the current
    proxy settings are located.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10206014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140748 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 45ab7abc0b8ae4d3a78637c158d98cd06b0a8f7a

---
+++ net/android/java/org/chromium/net/ProxyChangeListener.java
@@ -0,0 +1,91 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.net;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Proxy;
+import org.chromium.base.CalledByNative;
+
+// This class partners with native ProxyConfigServiceAndroid to listen for
+// proxy change notifications from Android.
+class ProxyChangeListener {
+    private static final String TAG = "ProxyChangeListener";
+
+    private int mNativePtr;
+    private Context mContext;
+    private ProxyReceiver mProxyReceiver;
+
+    private ProxyChangeListener(Context context) {
+        mContext = context;
+    }
+
+    @CalledByNative
+    static public ProxyChangeListener create(Context context) {
+        return new ProxyChangeListener(context);
+    }
+
+    @CalledByNative
+    static public String getProperty(String property) {
+        return System.getProperty(property);
+    }
+
+    @CalledByNative
+    public void start(int nativePtr) {
+        assert mNativePtr == 0;
+        mNativePtr = nativePtr;
+        registerReceiver();
+    }
+
+    @CalledByNative
+    public void stop() {
+        mNativePtr = 0;
+        unregisterReceiver();
+    }
+
+    private class ProxyReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(Proxy.PROXY_CHANGE_ACTION)) {
+                proxySettingsChanged();
+            }
+        }
+    }
+
+    private void proxySettingsChanged() {
+        if (mNativePtr == 0)
+            return;
+
+        // Note that this code currently runs on a MESSAGE_LOOP_UI thread, but
+        // the C++ code must run the callbacks on the network thread.
+        nativeProxySettingsChanged(mNativePtr);
+    }
+
+    private void registerReceiver() {
+        if (mProxyReceiver != null) {
+            return;
+        }
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Proxy.PROXY_CHANGE_ACTION);
+        mProxyReceiver = new ProxyReceiver();
+        mContext.getApplicationContext().registerReceiver(mProxyReceiver, filter);
+    }
+
+    private void unregisterReceiver() {
+        if (mProxyReceiver == null) {
+            return;
+        }
+        mContext.unregisterReceiver(mProxyReceiver);
+        mProxyReceiver = null;
+    }
+
+    /**
+     * See net/proxy/proxy_config_service_android.cc
+     */
+    private native void nativeProxySettingsChanged(
+            int nativePtr /* net::ProxyConfigServiceAndroid::JNIDelegate */);
+}
