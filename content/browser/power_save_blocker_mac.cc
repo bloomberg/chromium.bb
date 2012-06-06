2578415d537220e731f134c6f9ea5933fdc2867f
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 00:16:52 +0000

    Content: Implement PowerSaveBlocker2 for the Mac.
    BUG=126591
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10543009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140664 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 64db870b49a1951603625d7e1ea4792e45c6c010

--- content/browser/power_save_blocker_mac.cc
+++ content/browser/power_save_blocker_mac.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,6 +7,7 @@
 #include <IOKit/pwr_mgt/IOPMLib.h>
 
 #include "base/bind.h"
+#include "base/lazy_instance.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread.h"
 #include "content/public/browser/browser_thread.h"
@@ -71,3 +72,104 @@ void PowerSaveBlocker::ApplyBlock(PowerSaveBlockerType type) {
   g_power_thread->message_loop()->
       PostTask(FROM_HERE, base::Bind(CreateSleepAssertion, type));
 }
+
+// TODO(avi): Remove ^^^ this code in favor of vvv this code.
+// http://crbug.com/126591
+
+namespace {
+
+// Power management cannot be done on the UI thread. IOPMAssertionCreate does a
+// synchronous MIG call to configd, so if it is called on the main thread the UI
+// is at the mercy of another process. See http://crbug.com/79559 and
+// http://www.opensource.apple.com/source/IOKitUser/IOKitUser-514.16.31/pwr_mgt.subproj/IOPMLibPrivate.c .
+struct PowerSaveBlockerLazyInstanceTraits {
+  static const bool kRegisterOnExit = false;
+  static const bool kAllowedToAccessOnNonjoinableThread = true;
+
+  static base::Thread* New(void* instance) {
+    base::Thread* thread = new (instance) base::Thread("PowerSaveBlocker");
+    thread->Start();
+    return thread;
+  }
+  static void Delete(base::Thread* instance) { }
+};
+base::LazyInstance<base::Thread, PowerSaveBlockerLazyInstanceTraits>
+    g_power_thread2 = LAZY_INSTANCE_INITIALIZER;
+
+}  // namespace
+
+namespace content {
+
+class PowerSaveBlocker2::Delegate
+    : public base::RefCountedThreadSafe<PowerSaveBlocker2::Delegate> {
+ public:
+  Delegate(PowerSaveBlockerType type, const std::string& reason)
+      : type_(type), reason_(reason), assertion_(kIOPMNullAssertionID) {}
+
+  // Does the actual work to apply or remove the desired power save block.
+  void ApplyBlock();
+  void RemoveBlock();
+
+ private:
+  friend class base::RefCountedThreadSafe<PowerSaveBlocker2::Delegate>;
+  ~Delegate() {}
+  PowerSaveBlockerType type_;
+  std::string reason_;
+  IOPMAssertionID assertion_;
+};
+
+void PowerSaveBlocker2::Delegate::ApplyBlock() {
+  DCHECK_EQ(base::PlatformThread::CurrentId(),
+            g_power_thread2.Pointer()->thread_id());
+
+  CFStringRef level = NULL;
+  // See QA1340 <http://developer.apple.com/library/mac/#qa/qa1340/> for more
+  // details.
+  switch (type_) {
+    case PowerSaveBlocker2::kPowerSaveBlockPreventAppSuspension:
+      level = kIOPMAssertionTypeNoIdleSleep;
+      break;
+    case PowerSaveBlocker2::kPowerSaveBlockPreventDisplaySleep:
+      level = kIOPMAssertionTypeNoDisplaySleep;
+      break;
+    default:
+      NOTREACHED();
+      break;
+  }
+  if (level) {
+    // TODO(avi): Switch to IOPMAssertionCreateWithName when 10.6 is the minimum
+    // system supported by Chromium.
+    IOReturn result = IOPMAssertionCreate(level,
+                                          kIOPMAssertionLevelOn,
+                                          &assertion_);
+    LOG_IF(ERROR, result != kIOReturnSuccess)
+        << "IOPMAssertionCreate: " << result;
+  }
+}
+
+void PowerSaveBlocker2::Delegate::RemoveBlock() {
+  DCHECK_EQ(base::PlatformThread::CurrentId(),
+            g_power_thread2.Pointer()->thread_id());
+
+  if (assertion_ != kIOPMNullAssertionID) {
+    IOReturn result = IOPMAssertionRelease(assertion_);
+    LOG_IF(ERROR, result != kIOReturnSuccess)
+        << "IOPMAssertionRelease: " << result;
+  }
+}
+
+PowerSaveBlocker2::PowerSaveBlocker2(PowerSaveBlockerType type,
+                                     const std::string& reason)
+    : delegate_(new PowerSaveBlocker2::Delegate(type, reason)) {
+  g_power_thread2.Pointer()->message_loop()->PostTask(
+      FROM_HERE,
+      base::Bind(&PowerSaveBlocker2::Delegate::ApplyBlock, delegate_));
+}
+
+PowerSaveBlocker2::~PowerSaveBlocker2() {
+  g_power_thread2.Pointer()->message_loop()->PostTask(
+      FROM_HERE,
+      base::Bind(&PowerSaveBlocker2::Delegate::RemoveBlock, delegate_));
+}
+
+}  // namespace content
