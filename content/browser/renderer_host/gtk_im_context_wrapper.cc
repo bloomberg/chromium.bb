ac602a701a9dd78b6e22310ada22cb5c627c2ca5
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 07:25:40 +0000

    Always update |context_| in UpdateInputMethodState() regardless of the current value of |is_focused_| to support corner cases described in crbug.com/127731#c10. This change is safe because Chrome for GTK+ does not share input context between other RenderWidgetHosts.
    We should add test case to prevent the regression for crbug.com/127731, but it requires some changes for IME state testing.
    So please let me split CL.
    
    BUG=127731
    TEST=try bots.
    
    Review URL: https://chromiumcodereview.appspot.com/10501009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141391 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fa7244d42bf5a6506efb4fe0e6eb45c007096b37

--- content/browser/renderer_host/gtk_im_context_wrapper.cc
+++ content/browser/renderer_host/gtk_im_context_wrapper.cc
@@ -228,7 +228,7 @@ void GtkIMContextWrapper::UpdateInputMethodState(
 
   // The renderer has updated its IME status.
   // Control the GtkIMContext object according to this status.
-  if (!context_ || !is_focused_)
+  if (!context_)
     return;
 
   DCHECK(!is_in_key_event_handler_);
@@ -237,7 +237,7 @@ void GtkIMContextWrapper::UpdateInputMethodState(
       type != ui::TEXT_INPUT_TYPE_PASSWORD);
   if (is_enabled_ != is_enabled) {
     is_enabled_ = is_enabled;
-    if (is_enabled)
+    if (is_enabled && is_focused_)
       gtk_im_context_focus_in(context_);
     else
       gtk_im_context_focus_out(context_);
