106365a086d65b20fc0191ffe3348e2adbdba510
mcgrathr@chromium.org <mcgrathr@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-06-11 23:24:24 +0000

    Use stack space rather than data space for execute_data test
    Using a link-time-constant .data address for a call leads to a link-time
    error with arm-nacl-gcc because the address is too far away for a direct
    branch.  This was only using .data space rather than stack for a reason
    that was fixed long ago, so just use stack instead.
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=244
    TEST= trybots
    
    R=mseaborn@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10535114
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8891 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: d4a49ab500cb2f7b4b4ca83482574518a22a6ba5

--- tests/data_not_executable/execute_data.c
+++ tests/data_not_executable/execute_data.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Copyright (c) 2012 The Native Client Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -8,27 +8,19 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-/*
- * We put the buffer in the data segment (low address) rather than on
- * the stack (high address) so that the address is not affected by the
- * ARM sandbox's 0xf000000f address masking.  However, ARM's mask
- * should be relaxed -- see
- * http://code.google.com/p/nativeclient/issues/detail?id=462.
- * -- and then this comment can be removed (TODO(mseaborn)).
- */
+int main(void) {
 #if defined(__i386__) || defined(__x86_64__)
-uint8_t code[] __attribute__((aligned(32)))= { 0xc3 /* ret */ };
+  uint8_t code[] __attribute__((aligned(32))) = { 0xc3 /* ret */ };
 #elif defined(__arm__)
-uint32_t code[] __attribute__((aligned(32)))= { 0xe12fff1e /* BX LR */ };
+  uint32_t code[] __attribute__((aligned(32))) = { 0xe12fff1e /* BX LR */ };
 #else
 # error Unknown architecture
 #endif
 
-int main() {
-  void (*func)();
+  void (*func)(void);
 
   /* Double cast required to stop gcc complaining. */
-  func = (void (*)()) (uintptr_t) code;
+  func = (void (*)(void)) (uintptr_t) code;
 
   fprintf(stderr, "** intended_exit_status=untrusted_sigsegv_or_equivalent\n");
   /* This should fault because the data segment should not be executable. */
