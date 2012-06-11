41174abb2788d98a6f6b5c4c40cd2d0065a42318
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 15:46:52 +0000

    TabContentsWrapper -> TabContents, part 27.
    Speech.
    
    BUG=131026
    TEST=no change
    
    
    Review URL: https://chromiumcodereview.appspot.com/10537083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141416 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 49ce31487d7380754923106d5c1a73aeb9aacbc5

--- chrome/browser/speech/speech_recognition_bubble_browsertest.cc
+++ chrome/browser/speech/speech_recognition_bubble_browsertest.cc
@@ -5,7 +5,7 @@
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/speech/speech_recognition_bubble.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
+#include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/rect.h"
@@ -24,14 +24,14 @@ class SpeechRecognitionBubbleTest : public SpeechRecognitionBubbleDelegate,
 IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, CreateAndDestroy) {
   gfx::Rect element_rect(100, 100, 100, 100);
   scoped_ptr<SpeechRecognitionBubble> bubble(SpeechRecognitionBubble::Create(
-      browser()->GetSelectedWebContents(), this, element_rect));
+      browser()->GetActiveWebContents(), this, element_rect));
   EXPECT_TRUE(bubble.get());
 }
 
 IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, ShowAndDestroy) {
   gfx::Rect element_rect(100, 100, 100, 100);
   scoped_ptr<SpeechRecognitionBubble> bubble(SpeechRecognitionBubble::Create(
-      browser()->GetSelectedWebContents(), this, element_rect));
+      browser()->GetActiveWebContents(), this, element_rect));
   EXPECT_TRUE(bubble.get());
   bubble->Show();
 }
@@ -39,7 +39,7 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, ShowAndDestroy) {
 IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, ShowAndHide) {
   gfx::Rect element_rect(100, 100, 100, 100);
   scoped_ptr<SpeechRecognitionBubble> bubble(SpeechRecognitionBubble::Create(
-      browser()->GetSelectedWebContents(), this, element_rect));
+      browser()->GetActiveWebContents(), this, element_rect));
   EXPECT_TRUE(bubble.get());
   bubble->Show();
   bubble->Hide();
@@ -48,7 +48,7 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, ShowAndHide) {
 IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, ShowAndHideTwice) {
   gfx::Rect element_rect(100, 100, 100, 100);
   scoped_ptr<SpeechRecognitionBubble> bubble(SpeechRecognitionBubble::Create(
-      browser()->GetSelectedWebContents(), this, element_rect));
+      browser()->GetActiveWebContents(), this, element_rect));
   EXPECT_TRUE(bubble.get());
   bubble->Show();
   bubble->Hide();
--- chrome/browser/speech/speech_recognition_bubble_controller_unittest.cc
+++ chrome/browser/speech/speech_recognition_bubble_controller_unittest.cc
@@ -6,7 +6,7 @@
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/speech/speech_recognition_bubble_controller.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
+#include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/test_browser_thread.h"
@@ -152,10 +152,10 @@ class SpeechRecognitionBubbleControllerTest
     // real WebContents pointer from the test fixture and pass that, because
     // the bubble controller registers for tab close notifications which need
     // a valid WebContents.
-    TabContentsWrapper* wrapper =
-        test_fixture_->browser()->GetSelectedTabContentsWrapper();
-    if (wrapper)
-      web_contents = wrapper->web_contents();
+    TabContents* tab_contents =
+        test_fixture_->browser()->GetActiveTabContents();
+    if (tab_contents)
+      web_contents = tab_contents->web_contents();
     return new MockSpeechRecognitionBubble(web_contents, delegate,
                                            element_rect);
   }
