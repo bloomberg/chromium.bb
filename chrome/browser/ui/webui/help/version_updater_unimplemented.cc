e53ccad63b86a22b36eb328e2163df5c6aa8530b
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 18:44:36 +0000

    Add relaunch prompt to linux help page.
    This doesn't allow you to check for updates or show progress, it just notifies you if a new version is ready. As such, I didn't see a reason to limit it to Google Chrome builds.
    
    This also fixes a bug where a 'disabled' state permanently hides the status text.
    
    BUG=117403
    TEST=manual with --check-for-update-interval=1 and manually editing the version
    
    
    Review URL: https://chromiumcodereview.appspot.com/10541013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141693 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a092a066a017c352118438967f6d80f2790a81fa

--- chrome/browser/ui/webui/help/version_updater_unimplemented.cc
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#include "chrome/browser/ui/webui/help/version_updater_unimplemented.h"
-
-VersionUpdater* VersionUpdater::Create() {
-  return new VersionUpdaterUnimplemented;
-}
