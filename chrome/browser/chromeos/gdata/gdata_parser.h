d1632f82cf25ad2a946ae0f8d765d1613098a1ba
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 18:01:55 +0000

    Revert 141680 - Adds parsing for the app_id field from an "open-with-" link in Drive.  Later CLs will use app_id field to look up an associated app.
    Also adjusted unit test so that it would assert less and expect more.
    I expect more from my unit tests. :-)
    
    BUG=chromium:126895
    TEST=Ran updated unit test.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10546034
    
    TBR=gspencer@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10539112
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141685 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1ac8e82e49ee45081109d8a43c3d79b5112fabbc

--- chrome/browser/chromeos/gdata/gdata_parser.h
+++ chrome/browser/chromeos/gdata/gdata_parser.h
@@ -62,7 +62,6 @@ class Link {
     EMBED,
     PRODUCT,
     ICON,
-    OPEN_WITH,
   };
   Link();
 
@@ -82,30 +81,19 @@ class Link {
   // Title of the link.
   const string16& title() const { return title_; }
 
-  // For OPEN_WITH links, this contains the application ID. For all other link
-  // types, it is the empty string.
-  const std::string& app_id() const { return app_id_; }
-
   // Link MIME type.
   const std::string& mime_type() const { return mime_type_; }
 
  private:
   friend class DocumentEntry;
-  // Converts value of link.rel into LinkType. Outputs to |type| and returns
-  // true when |rel| has a valid value. Otherwise does nothing and returns
-  // false.
-  static bool GetLinkType(const base::StringPiece& rel, LinkType* type);
-
-  // Converts value of link.rel to application ID, if there is one embedded in
-  // the link.rel field. Outputs to |app_id| and returns true when |rel| has a
-  // valid value. Otherwise does nothing and returns false.
-  static bool GetAppID(const base::StringPiece& rel, std::string* app_id);
-
+  // Converts value of link.rel into LinkType. Outputs to |result| and
+  // returns true when |rel| has a valid value. Otherwise does nothing
+  // and returns false.
+  static bool GetLinkType(const base::StringPiece& rel, LinkType* result);
 
   LinkType type_;
   GURL href_;
   string16 title_;
-  std::string app_id_;
   std::string mime_type_;
 
   DISALLOW_COPY_AND_ASSIGN(Link);
