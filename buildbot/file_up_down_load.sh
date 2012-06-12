cde0e7d14491a8cbcc600e6375934685586950db
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-06-12 20:00:53 +0000

    Make size of bot-uploaded tarballs easily noticable.
    This is in preparation for an effort to reduce TC size(s).
    
    TEST= manual
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=392
    Review URL: https://chromiumcodereview.appspot.com/10533111
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8904 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 0e8552a34436aaca407d1dfd27d5af9a141f9179

--- buildbot/file_up_down_load.sh
+++ buildbot/file_up_down_load.sh
@@ -53,8 +53,10 @@ readonly BASE_BETWEEN_BOTS=nativeclient-archive2/between_builders
 # UTIL
 ######################################################################
 Upload() {
-  echo  "uploading: $2"
-  echo  @@@STEP_LINK@download@${URL_PREFIX_UI}/$2@@@
+  local size_kb="$(stat -c '%s' $1)"
+  size_kb=$((${size_kb} / 1024))
+  echo  "uploading: $2 (${size_kb}kB)"
+  echo  "@@@STEP_LINK@download (${size_kb}kB)@${URL_PREFIX_UI}/$2@@@"
   ${GS_UTIL} cp -a public-read $1 gs://$2
 }
 
