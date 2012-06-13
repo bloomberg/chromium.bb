bec6ac656f9952cf23c88bf33e72d7923e749663
bradnelson@google.com <bradnelson@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-06-13 21:10:09 +0000

    Checking that content-length matches file size for downloads.
    We have been seeing truncation on download of large files.
    Checking that the final file size matches what the headers say.
    
    BUG=None
    TEST=None
    R=maruel@chromium.org,ncbray@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10541149
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8923 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 0347d2665bc35398c424106ed6b568acc23f8847

--- build/http_download.py
+++ build/http_download.py
@@ -1,32 +1,7 @@
-#!/usr/bin/python2.4
-# Copyright 2009, Google Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-#     * Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#     * Redistributions in binary form must reproduce the above
-# copyright notice, this list of conditions and the following disclaimer
-# in the documentation and/or other materials provided with the
-# distribution.
-#     * Neither the name of Google Inc. nor the names of its
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#!/usr/bin/python
+# Copyright (c) 2012 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 """Download a file from a URL to a file on disk.
 
@@ -80,6 +55,14 @@ def HttpDownload(url, target, username=None, password=None, verbose=True):
       try:
         download_utils.WriteDataFromStream(target, src, chunk_size=2**20,
                                            verbose=verbose)
+        content_len = src.headers.get('Content-Length')
+        if content_len:
+          content_len = int(content_len)
+          file_size = os.path.getsize(target)
+          if content_len != file_size:
+            sys.stdout.write('Filesize:%d does not match Content-Length:%d' % (
+                file_size, content_length))
+            continue
       finally:
         src.close()
       break
