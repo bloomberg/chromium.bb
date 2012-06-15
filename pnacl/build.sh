3d026e9c431fb89d34b7f75ea8c63a0b52a735d5
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-06-15 20:39:44 +0000

    Update llvm stable rev
    This pulls in:
    http://codereview.chromium.org/10562009/
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2545
    TEST= manual
    Review URL: https://chromiumcodereview.appspot.com/10566008
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8948 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 55fd1846c4f294c2893dc0ce4bb6b3a335159c9d

--- pnacl/build.sh
+++ pnacl/build.sh
@@ -247,7 +247,7 @@ SBTC_PRODUCTION=${SBTC_PRODUCTION:-false}
 SBTC_BUILD_WITH_PNACL="armv7 i686 x86_64"
 
 # Current milestones in each repo
-readonly UPSTREAM_REV=${UPSTREAM_REV:-4796bf673eff}
+readonly UPSTREAM_REV=${UPSTREAM_REV:-952f768aec31}
 
 readonly NEWLIB_REV=346ea38d142f
 readonly BINUTILS_REV=5ccab9d0bb73
