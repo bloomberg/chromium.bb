197be91c4512b63674249aad9abeabf09a09b427
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-16 09:05:28 +0000

    Predefined allowed socket origins must have chrome-extension scheme
    BUG=132806
    TEST=browser_tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10556002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142589 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 924449497521c6d2988b632d290964bf9c244fc9

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1499,7 +1499,7 @@ bool ChromeContentBrowserClient::AllowPepperSocketAPI(
     return false;
 
   std::string host = url.host();
-  if (allowed_socket_origins_.count(host))
+  if (url.SchemeIs(kExtensionScheme) && allowed_socket_origins_.count(host))
     return true;
 
   Profile* profile = Profile::FromBrowserContext(browser_context);
