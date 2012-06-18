88d6b68b211f5e81c5e97102dcee48c3fa9fddc3
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 13:52:45 +0000

    chromeos: Send 'keypress' for the content when composing a character with dead keys
    This change leaves characters outside BMP unable to be typed on docs.google.com, but surely fixes the problem for most use cases.
    
    BUG=132668
    TEST=Create a document on docs.google.com and try typing a character with dead keys (e.g. type '^'+'a' with keyboard layout "English - US international")
    
    Review URL: https://chromiumcodereview.appspot.com/10565032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142705 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 41a7e42ef575c10375f574145e5d023118fbd149

--- ui/base/ime/input_method_ibus.cc
+++ ui/base/ime/input_method_ibus.cc
@@ -587,8 +587,15 @@ void InputMethodIBus::ProcessUnfilteredKeyPressEvent(
     string16 composed = character_composer_.composed_character();
     if (!composed.empty()) {
       client = GetTextInputClient();
-      if (client)
-        client->InsertText(composed);
+      if (client) {
+        // TODO(hashimoto): Send correct DOM event for characters which cannot
+        // be inserted with InsertChar(). Without sending those events, composed
+        // character will not be shown on docs.google.com. crbug.com/133269
+        if (composed.size() == 1)
+          client->InsertChar(composed[0], state);
+        else
+          client->InsertText(composed);
+      }
     }
     return;
   }
@@ -625,8 +632,15 @@ void InputMethodIBus::ProcessUnfilteredFabricatedKeyPressEvent(
     string16 composed = character_composer_.composed_character();
     if (!composed.empty()) {
       client = GetTextInputClient();
-      if (client)
-        client->InsertText(composed);
+      if (client) {
+        // TODO(hashimoto): Send correct DOM event for characters which cannot
+        // be inserted with InsertChar(). Without sending those events, composed
+        // character will not be shown on docs.google.com. crbug.com/133269
+        if (composed.size() == 1)
+          client->InsertChar(composed[0], flags);
+        else
+          client->InsertText(composed);
+      }
     }
     return;
   }
