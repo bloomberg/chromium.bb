bd0c67fb5d79b9c723a3ceaadfa959d1fce33d9a
Hung-Te Lin <hungte@chromium.org>
2012-06-19 12:11:25 +0800

    cbuildbot: Change firmware_from_source file name pattern to include *image*.bin
    legacy_image.bin may change file name to "nv_image.bin", so let's change the
    selection pattern to "*image*.bin".
    
    BUG=chrome-os-partner:10025
    TEST=trybot
         http://chromegw/p/tryserver.chromiumos/builders/link-release/builds/58
    
    Change-Id: I40e308ee520014089ae601621bc845b5405e2269
    Reviewed-on: https://gerrit.chromium.org/gerrit/25600
    Tested-by: Hung-Te Lin <hungte@chromium.org>
    Reviewed-by: David James <davidjames@chromium.org>
    Commit-Ready: Hung-Te Lin <hungte@chromium.org>

--- buildbot/cbuildbot_commands.py
+++ buildbot/cbuildbot_commands.py
@@ -985,7 +985,7 @@ def BuildFirmwareArchive(buildroot, board, archive_dir):
   Returns the basename of the archived file, or None if the target board does
   not have firmware from source.
   """
-  patterns = ['image*.bin', 'ec.bin', 'legacy_image.bin']
+  patterns = ['*image*.bin', 'ec.bin']
   firmware_root = os.path.join(buildroot, 'chroot', 'build', board, 'firmware')
   source_list = []
   for pattern in patterns:
