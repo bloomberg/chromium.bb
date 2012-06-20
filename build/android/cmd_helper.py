3c48835adf6ffab7b0723186d9d13d358d6b701b
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 21:05:29 +0000

    Upstreaming android build tools
    - These are used to monitor and debug devices when running tests
    
    Hooks will be added in a separate CL.
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10578032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143261 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e9a57b2de7a89ade8859b00ac0c3ff9051e6e357

--- build/android/cmd_helper.py
+++ build/android/cmd_helper.py
@@ -1,6 +1,10 @@
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+#!/usr/bin/python
+#
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+"""A wrapper for subprocess to make calling shell commands easier."""
 
 
 import logging
@@ -15,13 +19,16 @@ def RunCmd(args, cwd=None):
       the string or the first item in the args sequence.
     cwd: If not None, the subprocess's current directory will be changed to
       |cwd| before it's executed.
+
+  Returns:
+    Return code from the command execution.
   """
   logging.info(str(args) + ' ' + (cwd or ''))
   p = subprocess.Popen(args=args, cwd=cwd)
   return p.wait()
 
 
-def GetCmdOutput(args, cwd=None):
+def GetCmdOutput(args, cwd=None, shell=False):
   """Open a subprocess to execute a program and returns its output.
 
   Args:
@@ -29,10 +36,15 @@ def GetCmdOutput(args, cwd=None):
       the string or the first item in the args sequence.
     cwd: If not None, the subprocess's current directory will be changed to
       |cwd| before it's executed.
+    shell: Whether to execute args as a shell command.
+
+  Returns:
+    Captures and returns the command's stdout.
+    Prints the command's stderr to logger (which defaults to stdout).
   """
   logging.info(str(args) + ' ' + (cwd or ''))
   p = subprocess.Popen(args=args, cwd=cwd, stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
+                       stderr=subprocess.PIPE, shell=shell)
   stdout, stderr = p.communicate()
   if stderr:
     logging.critical(stderr)
