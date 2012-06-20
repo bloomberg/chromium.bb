477754131168c1f6d54a18c5f08209abc160c774
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 21:45:04 +0000

    Revert 143261 - Upstreaming android build tools
    - These are used to monitor and debug devices when running tests
    
    Hooks will be added in a separate CL.
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10578032
    
    TBR=ilevy@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10583044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143268 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1ad94575470ccb61f9a64915638aafac3269a28f

--- build/android/cmd_helper.py
+++ build/android/cmd_helper.py
@@ -1,10 +1,6 @@
-#!/usr/bin/python
-#
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Copyright (c) 2011 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
-"""A wrapper for subprocess to make calling shell commands easier."""
 
 
 import logging
@@ -19,16 +15,13 @@ def RunCmd(args, cwd=None):
       the string or the first item in the args sequence.
     cwd: If not None, the subprocess's current directory will be changed to
       |cwd| before it's executed.
-
-  Returns:
-    Return code from the command execution.
   """
   logging.info(str(args) + ' ' + (cwd or ''))
   p = subprocess.Popen(args=args, cwd=cwd)
   return p.wait()
 
 
-def GetCmdOutput(args, cwd=None, shell=False):
+def GetCmdOutput(args, cwd=None):
   """Open a subprocess to execute a program and returns its output.
 
   Args:
@@ -36,15 +29,10 @@ def GetCmdOutput(args, cwd=None, shell=False):
       the string or the first item in the args sequence.
     cwd: If not None, the subprocess's current directory will be changed to
       |cwd| before it's executed.
-    shell: Whether to execute args as a shell command.
-
-  Returns:
-    Captures and returns the command's stdout.
-    Prints the command's stderr to logger (which defaults to stdout).
   """
   logging.info(str(args) + ' ' + (cwd or ''))
   p = subprocess.Popen(args=args, cwd=cwd, stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE, shell=shell)
+                       stderr=subprocess.PIPE)
   stdout, stderr = p.communicate()
   if stderr:
     logging.critical(stderr)
