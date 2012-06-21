e05c95c538038580765ce08820aadaadbac1a699
aelias@chromium.org <aelias@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 06:00:14 +0000

    Remove setting of fixed layout fallback width.
    This code is being implemented instead in
    https://bugs.webkit.org/show_bug.cgi?id=86819
    
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10382232
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143348 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9e2c5763127d64bce27bfcb1e46c5ea438cd103a

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -4927,12 +4927,6 @@ void RenderViewImpl::OnResize(const gfx::Size& new_size,
                               const gfx::Rect& resizer_rect,
                               bool is_fullscreen) {
   if (webview()) {
-    // This setting has no effect if fixed layout is not enabled.
-    DCHECK(device_scale_factor_);
-    if (device_scale_factor_) {
-      webview()->settings()->setLayoutFallbackWidth(
-          new_size.width() / device_scale_factor_);
-    }
     webview()->hidePopups();
     if (send_preferred_size_changes_) {
       webview()->mainFrame()->setCanHaveScrollbars(
