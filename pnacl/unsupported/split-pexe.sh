655f361bec91d1201b8e57150c992ee172dcdd84
robertm@google.com <robertm@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-06-21 23:10:21 +0000

    Add simple script for splitting pexes
    This works in conjunction with 
    https://chromiumcodereview.appspot.com/10581042/
    on a very large pexe
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2858 
    TEST= manual
    Review URL: https://chromiumcodereview.appspot.com/10572045
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@9007 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 3a2f0de2e7b97fa05ec00e5ef841b54d11dc1564

---
+++ pnacl/unsupported/split-pexe.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Copyright (c) 2012 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+set -o nounset
+set -o errexit
+
+readonly EXTRACT=toolchain/pnacl_linux_x86_64/pkg/llvm/bin/llvm-extract
+readonly TRANSLATE=toolchain/pnacl_linux_x86_64/newlib/bin/pnacl-translate
+
+split-pexe() {
+  local out=$1
+  local in=$2
+  local rem=$3
+  local div=$4
+
+  ${EXTRACT} -delete  -remainder ${rem} -divisor ${div} ${in} -o ${out}
+}
+
+
+translate-split-pexe() {
+  local arch=$1
+  local out=$2
+  local in=$3
+
+  ${TRANSLATE} -c -arch ${arch} ${in} -o ${out} --pnacl-driver-verbose
+}
+
+
+link-split-objects() {
+    local arch=$1
+    local out=$2
+    shift 2
+    ${TRANSLATE} -arch ${arch} -o ${out} "$@" --pnacl-driver-verbose
+}
+
+
+split-translate-link() {
+  local arch=$1
+  local out=$2
+  local in=$3
+  local n=$4
+
+  for i in $(seq 0 $(($n - 1))) ; do
+    echo "splitting $i/$n"
+    split-pexe  ${in}.$i ${in} $i $n
+  done
+
+  # TODO(robertm): these could be parallized
+  local objs=""
+  for i in $(seq 0 $(($n - 1))) ; do
+    echo "translating  $i/$n"
+    objs+=" ${out}.$i"
+    translate-split-pexe ${arch} ${out}.$i  ${in}.$i
+  done
+
+  echo "linking ${objs}"
+  link-split-objects ${arch} ${out} ${objs}
+}
+
+"$@"
