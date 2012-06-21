2016bb0bbc67e2734892e43d9f3d21e995accfe8
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 14:48:28 +0000

    add missing license
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143383 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 23b6f99ad70fa5a5bd762f576e356ee1db31ab35

--- rlz/lib/machine_id.cc
+++ rlz/lib/machine_id.cc
@@ -1,3 +1,7 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #include "rlz/lib/machine_id.h"
 
 #include "base/sha1.h"
