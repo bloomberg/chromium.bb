056720d6f97f0321645afe0b80e5b396adfeb277
bradchen@google.com <bradchen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-23 20:49:57 +0000

    Disable Linux SUID sandbox for heapcheck bots.
    The trybot seems to be wedged so I haven't been able to try this. Couldn't hurt; the linux_heapcheck bot has been red for days.
    
    TBR=thestig
    
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10662008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143818 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3f70ec29839a95928316488e73f409420687c1bb

--- tools/heapcheck/heapcheck_test.py
+++ tools/heapcheck/heapcheck_test.py
@@ -56,6 +56,8 @@ class HeapcheckWrapper(object):
         '/../../third_party/tcmalloc/chromium/src/pprof')
     self.PutEnvAndLog('LD_LIBRARY_PATH',
                       '/usr/lib/debug/:/usr/lib32/debug/')
+    # CHROME_DEVEL_SANDBOX causes problems with heapcheck
+    self.PutEnvAndLog('CHROME_DEVEL_SANDBOX', '');
 
     return common.RunSubprocess(proc, self._timeout)
 
