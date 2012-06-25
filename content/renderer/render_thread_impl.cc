1b5d731ce5e80dcb55be3e2d7249ae45dd4d603e
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-25 23:57:07 +0000

    Revert 144012 - Revert 143931 - Enable Pointer Lock by default, change about:flags option to multi-select.
    Pyauto test failures were collateral damage from other tests
    failing to restore registry settings impacting content settings.
    
    BUG=72754
    
    Review URL: https://chromiumcodereview.appspot.com/10661030
    
    TBR=scheib@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10667023
    
    TBR=scheib@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10660018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144062 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e4a81663978eae613f2c7c1ae1d7f93198b8cfc2

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -567,7 +567,7 @@ void RenderThreadImpl::EnsureWebKitInitialized() {
       !command_line.HasSwitch(switches::kDisableFullScreen));
 
   WebKit::WebRuntimeFeatures::enablePointerLock(
-      command_line.HasSwitch(switches::kEnablePointerLock));
+      !command_line.HasSwitch(switches::kDisablePointerLock));
 
   WebKit::WebRuntimeFeatures::enableVideoTrack(
       command_line.HasSwitch(switches::kEnableVideoTrack));
