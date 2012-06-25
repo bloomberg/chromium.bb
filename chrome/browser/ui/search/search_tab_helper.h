1bc68cbb4f28b69963a0393a11c509caa5bb883b
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-25 17:56:06 +0000

    Fixes bug in SearchTabHelper not correctly updating state. Also removes NTP_LOADING since we don't need it anymore.
    BUG=none
    TEST=none
    R=dhollowa@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10657007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143946 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 900ab4233e24712fc62d621c59de8ca72d3d8828

--- chrome/browser/ui/search/search_tab_helper.h
+++ chrome/browser/ui/search/search_tab_helper.h
@@ -15,10 +15,6 @@
 class OmniboxEditModel;
 class TabContents;
 
-namespace content {
-class RenderWidgetHost;
-}
-
 namespace chrome {
 namespace search {
 
@@ -44,13 +40,6 @@ class SearchTabHelper : public content::WebContentsObserver,
   virtual void NavigateToPendingEntry(
       const GURL& url,
       content::NavigationController::ReloadType reload_type) OVERRIDE;
-  virtual void DidStartProvisionalLoadForFrame(
-      int64 frame_id,
-      bool is_main_frame,
-      const GURL& validated_url,
-      bool is_error_page,
-      content::RenderViewHost* render_view_host) OVERRIDE;
-  virtual void DocumentLoadedInFrame(int64 frame_id) OVERRIDE;
 
   // Overridden from content::NotificationObserver:
   virtual void Observe(int type,
@@ -58,34 +47,8 @@ class SearchTabHelper : public content::WebContentsObserver,
                        const content::NotificationDetails& details) OVERRIDE;
 
  private:
-  // Enum of the load states for the NTP.
-  //
-  // Once the user loads the NTP the |ntp_load_state_| changes to
-  // WAITING_FOR_FRAME_ID and the chrome::search::mode::Type changes to
-  // MODE_NTP_LOADING. The |ntp_load_state_| progresses through the remaining
-  // states and when done chrome::search::mode::Type is changed to MODE_NTP.
-  //
-  // This code is intended to avoid a flash between white (default background
-  // color) and the background the pages wants (gray). We know the CSS has been
-  // applied once we get DocumentLoadedInFrame() and we know the backing store
-  // has been updated once we get a paint.
-  enum NTPLoadState {
-    // The default initial state.
-    DEFAULT,
-
-    // The user loaded the NTP and we're waiting for the id of the main frame.
-    WAITING_FOR_FRAME_ID,
-
-    // We got the frame id (in |main_frame_id_|) and are waiting for the frame
-    // to complete loading.
-    WAITING_FOR_FRAME_LOAD,
-
-    // The document finished loading. We're now waiting for a paint.
-    WAITING_FOR_PAINT,
-  };
-
-  // Returns the current RenderWidgetHost of the |web_contents()|.
-  content::RenderWidgetHost* GetRenderWidgetHost();
+  // Sets the mode of the model based on |url|.
+  void UpdateModel(const GURL& url);
 
   const bool is_search_enabled_;
 
@@ -93,12 +56,6 @@ class SearchTabHelper : public content::WebContentsObserver,
   scoped_ptr<SearchModel> model_;
 
   content::NotificationRegistrar registrar_;
-
-  // See description above NTPLoadState.
-  NTPLoadState ntp_load_state_;
-
-  // See description above NTPLoadState.
-  int64 main_frame_id_;
 
   DISALLOW_COPY_AND_ASSIGN(SearchTabHelper);
 };
