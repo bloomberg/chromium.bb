88f7901ec000c2ffefa109c3d7b130810416fd92
gdk@chromium.org <gdk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 01:39:25 +0000

    Disable the special behaviour of document.cookie in platform apps.
    Previously, if code within a platform app attempted to access (or
    override) the document.cookie field then a DOM security exception would
    have been raised.
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10636039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144091 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2f446d0e7e759e13297313c278266c48604bde2e

--- chrome/browser/extensions/extension_webkit_preferences.cc
+++ chrome/browser/extensions/extension_webkit_preferences.cc
@@ -39,6 +39,7 @@ void SetPreferences(const extensions::Extension* extension,
     webkit_prefs->databases_enabled = false;
     webkit_prefs->local_storage_enabled = false;
     webkit_prefs->sync_xhr_in_documents_enabled = false;
+    webkit_prefs->cookie_enabled = false;
   }
 
   // Enable WebGL features that regular pages can't access, since they add
