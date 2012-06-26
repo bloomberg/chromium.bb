5233bef8e5e0bef46fdb645b6c6d48a951be40e5
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 05:05:18 +0000

    Manage IsolatedContext with reference counts
    to make it possible to be shared by multiple children.
    
    This patch itself should have no side effect.
    
    BUG=none
    TEST=existing tests should pass
    
    Review URL: https://chromiumcodereview.appspot.com/10536200
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144115 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d16ef66b6fac08e893265ba581bb0db270dbe5ef

--- content/browser/child_process_security_policy_impl.cc
+++ content/browser/child_process_security_policy_impl.cc
@@ -58,7 +58,7 @@ class ChildProcessSecurityPolicyImpl::SecurityState {
     for (FileSystemMap::iterator iter = filesystem_permissions_.begin();
          iter != filesystem_permissions_.end();
          ++iter) {
-      isolated_context->RevokeIsolatedFileSystem(iter->first);
+      isolated_context->RemoveReference(iter->first);
     }
     UMA_HISTOGRAM_COUNTS("ChildProcessSecurityPolicy.PerChildFilePermissions",
                          file_permissions_.size());
@@ -97,6 +97,9 @@ class ChildProcessSecurityPolicyImpl::SecurityState {
   // Grant certain permissions to a file.
   void GrantPermissionsForFileSystem(const std::string& filesystem_id,
                                      int permissions) {
+    if (filesystem_permissions_.find(filesystem_id) ==
+        filesystem_permissions_.end())
+      fileapi::IsolatedContext::GetInstance()->AddReference(filesystem_id);
     filesystem_permissions_[filesystem_id] = permissions;
   }
 
