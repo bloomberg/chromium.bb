f8cfba3571a9aa868d3557ba35a4d34ab9d0aa7c
primiano@chromium.org <primiano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 17:47:38 +0000

    Hiding tray icon before destroying it on GTK.
    The current status_icon_gtk.cc implementation has a small esthetic bug: if a tray icon is shown and destroyed repeatedly under GTK, a small whitespace accumulates on the Linux tray area on every destruction.
    
    BUG=116954
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10668040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144486 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2115e1e021f8b7146e1fe00b11e05a89d7372128

--- chrome/browser/ui/gtk/status_icons/status_icon_gtk.cc
+++ chrome/browser/ui/gtk/status_icons/status_icon_gtk.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,6 +21,7 @@ StatusIconGtk::StatusIconGtk() {
 }
 
 StatusIconGtk::~StatusIconGtk() {
+  gtk_status_icon_set_visible(icon_, FALSE);
   g_object_unref(icon_);
 }
 
