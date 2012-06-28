3755f150de5d7aee89f8f416b2d002a1c0e7577e
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-28 22:56:20 +0000

    Enable crosh tests on chromeos.
    Rename chromeos_html_terminal.py to chromeos_crosh.py. Remove secure shell
    install test from it, since I'm putting a similar one in secure_shell.py
    
    BUG=None
    TEST=None
    
    R=craigdh@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10677019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144823 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ed50d3ee0ed2e26da0ff805dc52ee0c03f80df2e

--- chrome/test/functional/chromeos_crosh.py
+++ chrome/test/functional/chromeos_crosh.py
@@ -9,35 +9,6 @@ import pyauto_functional  # must be imported before pyauto
 import pyauto
 
 
-class ChromeosHTMLTerminalTest(pyauto.PyUITest):
-  """Basic tests for ChromeOS HTML Terminal.
-
-  Requires ChromeOS to be logged in.
-  """
-
-  def _GetExtensionInfoById(self, extensions, id):
-    for x in extensions:
-      if x['id'] == id:
-        return x
-    return None
-
-  def testInstallAndUninstallSecureShellExt(self):
-    """Basic installation test for HTML Terminal on ChromeOS."""
-    crx_file_path = os.path.abspath(
-        os.path.join(self.DataDir(), 'pyauto_private', 'apps',
-                     'SecureShell-dev-0.7.9.3.crx'))
-    ext_id = self.InstallExtension(crx_file_path)
-    self.assertTrue(ext_id, 'Failed to install extension.')
-    extension = self._GetExtensionInfoById(self.GetExtensionsInfo(), ext_id)
-    self.assertTrue(extension['is_enabled'],
-                    msg='Extension was not enabled on installation.')
-    self.assertFalse(extension['allowed_in_incognito'],
-                     msg='Extension was allowed in incognito on installation.')
-    # Uninstall HTML Terminal extension
-    self.assertTrue(self.UninstallExtensionById(ext_id),
-                    msg='Failed to uninstall extension.')
-
-
 class CroshTest(pyauto.PyUITest):
   """Tests for crosh."""
 
