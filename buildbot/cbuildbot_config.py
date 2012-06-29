e024dcecd8a14b38f75c4a528fe2456e9e04692e
Scott Zawalski <scottz@google.com>
2012-06-29 14:41:14 -0400

    Temporarily disable HWTest step for Autotest migration.
    Remove all hw_tests=['XXX'] from target configs.
    
    BUG=None
    TEST=Unittests
    
    Change-Id: I023f984edd61695861f79faf6840f517ea09414e
    Reviewed-on: https://gerrit.chromium.org/gerrit/26465
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Commit-Ready: Scott Zawalski <scottz@chromium.org>
    Reviewed-by: Scott Zawalski <scottz@chromium.org>
    Tested-by: Scott Zawalski <scottz@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -567,7 +567,6 @@ chrome_pfq = internal_chromium_pfq.derive(
   overlays=constants.BOTH_OVERLAYS,
   prebuilts=False,
   useflags=official['useflags'] + ['pgo_generate'],
-  hw_tests=['PGO_record'],
   upload_hw_test_artifacts=True,
   hw_tests_pool=constants.HWTEST_CHROME_PFQ_POOL,
 )
@@ -757,7 +756,6 @@ internal_paladin.add_config('alex-paladin',
   boards=['x86-alex'],
   paladin_builder_name='alex paladin',
   hw_tests_pool='cq-bvt',
-  hw_tests=['bvt'],
   upload_hw_test_artifacts=True,
 )
 
@@ -770,7 +768,6 @@ internal_paladin.add_config('lumpy-paladin',
   boards=['lumpy'],
   paladin_builder_name='lumpy paladin',
   hw_tests_pool='cq-bvt',
-  hw_tests=['bvt'],
   upload_hw_test_artifacts=True,
 )
 
@@ -813,7 +810,6 @@ internal_paladin.add_config('unified-lumpy-paladin',
   vm_tests=None,
   prebuilts=None,
   hw_tests_pool='cq-bvt',
-  hw_tests=['bvt'],
   upload_hw_test_artifacts=True,
   important=False,
 )
@@ -883,13 +879,11 @@ _release = full.derive(official, internal,
 
 _release.add_config('x86-mario-release',
   boards=['x86-mario'],
-  hw_tests=['bvt'],
 )
 
 _config.add_group('x86-alex-release-group',
   _release.add_config('x86-alex-release',
     boards=['x86-alex'],
-    hw_tests=['bvt'],
   ),
   _release.add_config('x86-alex_he-release',
     boards=['x86-alex_he'],
@@ -914,7 +908,6 @@ _config.add_group('x86-alex32-release-group',
 _config.add_group('x86-zgb-release-group',
   _release.add_config('x86-zgb-release',
     boards=['x86-zgb'],
-    hw_tests=['bvt'],
   ),
   _release.add_config('x86-zgb_he-release',
     boards=['x86-zgb_he'],
@@ -938,12 +931,10 @@ _config.add_group('x86-zgb32-release-group',
 
 _release.add_config('stumpy-release',
   boards=['stumpy'],
-  hw_tests=['bvt'],
 )
 
 _release.add_config('lumpy-release',
   boards=['lumpy'],
-  hw_tests=['bvt'],
 )
 
 _release.add_config('link-release',
@@ -975,7 +966,6 @@ _arm_release.add_config('arm-tegra2_seaboard-release',
 
 _arm_release.add_config('arm-tegra2_kaen-release',
   boards=['tegra2_kaen'],
-  hw_tests=['kaen_bvt'],
 )
 
 _arm_release.add_config('arm-daisy-release',
