3e72f1289f678e17c917be2e118e6abb5ff016be
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-30 19:39:10 +0000

    Disable TwoClientPasswords.SyncTestSetPassphraseAndThenSetupSync on mac.
    It fails on new bots.
    
    BUG=135336
    TBR=rsimha
    Review URL: https://chromiumcodereview.appspot.com/10696064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145087 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bc95cf08c50f2db6600de9b65491ae195b204639

--- chrome/browser/sync/test/integration/two_client_passwords_sync_test.cc
+++ chrome/browser/sync/test/integration/two_client_passwords_sync_test.cc
@@ -229,8 +229,15 @@ IN_PROC_BROWSER_TEST_F(TwoClientPasswordsSyncTest, Merge) {
   ASSERT_TRUE(AllProfilesContainSamePasswordForms());
 }
 
+// This test is disabled on MAC. See bug http://crbug.com/135336
+#if defined(OS_MACOSX)
+#define MAYBE_SetPassphraseAndThenSetupSync \
+    DISABLED_SetPassphraseAndThenSetupSync
+#else
+#define MAYBE_SetPassphraseAndThenSetupSync SetPassphraseAndThenSetupSync
+#endif
 IN_PROC_BROWSER_TEST_F(TwoClientPasswordsSyncTest,
-                       SetPassphraseAndThenSetupSync) {
+                       MAYBE_SetPassphraseAndThenSetupSync) {
   ASSERT_TRUE(SetupClients()) << "SetupClients() failed.";
 
   ASSERT_TRUE(GetClient(0)->SetupSync());
