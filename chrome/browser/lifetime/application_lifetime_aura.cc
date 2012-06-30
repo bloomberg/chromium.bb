2e78a43b6dacf3debe6929ffe0e14297108341cc
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-30 01:40:57 +0000

    ash: Avoid hang on signout when desktop menu is open.
    Explicitly release any captures on the root window when
    we're exiting.
    
    BUG=134472
    TEST=manual: close all browser windows on chrome os, open desktop context menu, hit ctrl-shift-q, no longer see SIGABRT getting sent to chrome in /var/log/messages
    
    
    Review URL: https://chromiumcodereview.appspot.com/10698059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145048 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 90fa92875677ab043895ee753102d66005d8e29a

--- chrome/browser/lifetime/application_lifetime_aura.cc
+++ chrome/browser/lifetime/application_lifetime_aura.cc
@@ -9,14 +9,28 @@
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/notifications/notification_ui_manager.h"
 
+#if defined(USE_ASH)
+#include "ash/shell.h"
+#include "ui/aura/client/capture_client.h"
+#endif
+
 namespace browser {
 
 void HandleAppExitingForPlatform() {
-  // Close All non browser windows now. Those includes notifications
+  // Close all non browser windows now. Those includes notifications
   // and windows created by Ash (launcher, background, etc).
   g_browser_process->notification_ui_manager()->CancelAll();
+
+#if defined(USE_ASH)
+  // Releasing the capture will close any menus that might be open:
+  // http://crbug.com/134472
+  aura::client::GetCaptureClient(ash::Shell::GetPrimaryRootWindow())->
+      SetCapture(NULL);
+#endif
+
   // TODO(oshima): Close all non browser windows here while
   // the message loop is still alive.
+
 #if defined(OS_CHROMEOS)
   if (!CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kDisableZeroBrowsersOpenForTests)) {
