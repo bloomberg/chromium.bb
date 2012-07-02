d71fc772be59c221ec862302f0f626ed80105ecb
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-02 06:31:32 +0000

    Mark flaky PPAPITtest.ImeInputEvent as DISABLED on Linux and Win.
    Temporarily disabling it and start investigation.
    
    BUG=135403
    TEST=browser_tests
    TBR=brettw@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10695054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145121 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 48af5360104b6322b6657220df86ff62c1bf82c5

--- chrome/test/ui/ppapi_uitest.cc
+++ chrome/test/ui/ppapi_uitest.cc
@@ -524,13 +524,20 @@ TEST_PPAPI_OUT_OF_PROCESS(Core)
 #define MAYBE_InputEvent InputEvent
 #endif
 
+// Flaky on Linux and Windows. http://crbug.com/135403
+#if defined(OS_LINUX) || defined(OS_WIN)
+#define MAYBE_ImeInputEvent DISABLED_ImeInputEvent
+#else
+#define MAYBE_ImeInputEvent ImeInputEvent
+#endif
+
 TEST_PPAPI_IN_PROCESS(MAYBE_InputEvent)
 TEST_PPAPI_OUT_OF_PROCESS(MAYBE_InputEvent)
 // TODO(bbudge) Enable when input events are proxied correctly for NaCl.
 TEST_PPAPI_NACL_VIA_HTTP(DISABLED_InputEvent)
 
-TEST_PPAPI_IN_PROCESS(ImeInputEvent)
-TEST_PPAPI_OUT_OF_PROCESS(ImeInputEvent)
+TEST_PPAPI_IN_PROCESS(MAYBE_ImeInputEvent)
+TEST_PPAPI_OUT_OF_PROCESS(MAYBE_ImeInputEvent)
 // TODO(kinaba) Enable when IME events are proxied correctly for NaCl.
 TEST_PPAPI_NACL_VIA_HTTP(DISABLED_ImeInputEvent)
 
