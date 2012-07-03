1bb77e63a69bf7404c7e09707f640c2c422c77a8
bradchen@google.com <bradchen@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-07-03 21:34:22 +0000

    Fix a few license issues.
    Fix a few missing copyright headers and licensecheck issues.
    
    BUG=crbug.com/98099
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10695086
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@9086 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: cde60f53924e43ac6dfdecbea7343a2ef2c82ce7

--- tests/bundle_size/bundle_size.c
+++ tests/bundle_size/bundle_size.c
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2012 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
