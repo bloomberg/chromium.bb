0d4b977ccd2fa098a4d9baa88102ea844917ce7e
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-03 21:18:00 +0000

    gtk: Make screen.avail{Width,Height} use _NET_WORKAREA.
    This makes us take the window-manager-reported work area
    (the area not obscured by docks or panels) into account when
    computing window.screen.
    
    BUG=64796
    TEST=manual: status bar is subtracted from availHeight with ion3; panels are subtracted with gnome-plus-metacity
    
    
    Review URL: https://chromiumcodereview.appspot.com/10704072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145382 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9afe7f462a02dbe07cc0a27d9c3a571d4b30ff93

--- content/browser/renderer_host/gtk_window_utils.cc
+++ content/browser/renderer_host/gtk_window_utils.cc
@@ -1,17 +1,50 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "content/browser/renderer_host/gtk_window_utils.h"
 
+#include <vector>
+
 #include <gdk/gdkx.h>
 #include <gtk/gtk.h>
 
 #include "ui/base/gtk/gtk_compat.h"
+#include "ui/base/x/x11_util.h"
+#include "ui/gfx/rect.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebScreenInfo.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/x11/WebScreenInfoFactory.h"
 
 namespace content {
+
+namespace {
+
+// Returns the region of |window|'s desktop that isn't occupied by docks or
+// panels.  Returns an empty rect on failure.
+gfx::Rect GetWorkArea(Window window) {
+  Window root = ui::GetX11RootWindow();
+  int desktop = -1;
+  if (!ui::GetIntProperty(window, "_NET_WM_DESKTOP", &desktop)) {
+    // Hack for ion3: _NET_WM_DESKTOP doesn't get set on individual windows, but
+    // _NET_CURRENT_DESKTOP and _NET_WORKAREA do get set.
+    ui::GetIntProperty(root, "_NET_CURRENT_DESKTOP", &desktop);
+  }
+  if (desktop < 0)
+    return gfx::Rect();
+
+  std::vector<int> property;
+  if (!ui::GetIntArrayProperty(root, "_NET_WORKAREA", &property))
+    return gfx::Rect();
+
+  size_t start_index = 4 * desktop;
+  if (property.size() < start_index + 4)
+    return gfx::Rect();
+
+  return gfx::Rect(property[start_index], property[start_index + 1],
+                   property[start_index + 2], property[start_index + 3]);
+}
+
+}  // namespace
 
 void GetScreenInfoFromNativeWindow(
     GdkWindow* gdk_window, WebKit::WebScreenInfo* results) {
@@ -26,8 +59,12 @@ void GetScreenInfoFromNativeWindow(
   gdk_screen_get_monitor_geometry(screen, monitor_number, &monitor_rect);
   results->rect = WebKit::WebRect(monitor_rect.x, monitor_rect.y,
                                   monitor_rect.width, monitor_rect.height);
-  // TODO(tony): Should we query _NET_WORKAREA to get the workarea?
-  results->availableRect = results->rect;
+
+  gfx::Rect available_rect = results->rect;
+  gfx::Rect work_area = GetWorkArea(GDK_WINDOW_XID(gdk_window));
+  if (!work_area.IsEmpty())
+    available_rect = available_rect.Intersect(work_area);
+  results->availableRect = available_rect;
 }
 
 }  // namespace content
