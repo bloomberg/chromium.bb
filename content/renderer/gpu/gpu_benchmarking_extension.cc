2500031654af3e0551e691e552cf9c3135cba16c
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-03 04:28:04 +0000

    Plumb rendering statistics to benchmarking extension.
    BUG=chromium:130130
    TEST=Start Chrome with --enable-benchmarking --enable-stats-table. Open a page that uses hardware compositing, like poster circle. Open Javascript console and type in chrome.benchmarking.renderingStats(). The numbers should be reasonable. Do the same with a page that uses software compositing. It should return an empty Object, and it should not crash.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10536080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145253 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8af3f2c6032b00e7a4d1a81b37f5b6751fd6f9a9

--- content/renderer/gpu/gpu_benchmarking_extension.cc
+++ content/renderer/gpu/gpu_benchmarking_extension.cc
@@ -4,7 +4,14 @@
 
 #include "content/renderer/gpu/gpu_benchmarking_extension.h"
 
+#include "third_party/WebKit/Source/Platform/chromium/public/WebRenderingStats.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
 #include "v8/include/v8.h"
+
+using WebKit::WebFrame;
+using WebKit::WebRenderingStats;
+using WebKit::WebView;
 
 const char kGpuBenchmarkingExtensionName[] = "v8/GpuBenchmarking";
 
@@ -19,11 +26,42 @@ class GpuBenchmarkingWrapper : public v8::Extension {
           "};"
           "if (typeof(chrome.gpuBenchmarking) == 'undefined') {"
           "  chrome.gpuBenchmarking = {};"
+          "};"
+          "chrome.gpuBenchmarking.renderingStats = function() {"
+          "  native function GetRenderingStats();"
+          "  return GetRenderingStats();"
           "};") {}
 
   virtual v8::Handle<v8::FunctionTemplate> GetNativeFunction(
       v8::Handle<v8::String> name) {
+    if (name->Equals(v8::String::New("GetRenderingStats")))
+      return v8::FunctionTemplate::New(GetRenderingStats);
+
     return v8::Handle<v8::FunctionTemplate>();
+  }
+
+  static v8::Handle<v8::Value> GetRenderingStats(const v8::Arguments& args) {
+    WebFrame* web_frame = WebFrame::frameForEnteredContext();
+    if (!web_frame)
+      return v8::Undefined();
+
+    WebView* web_view = web_frame->view();
+    if (!web_view)
+      return v8::Undefined();
+
+    WebRenderingStats stats;
+    web_view->renderingStats(stats);
+
+    v8::Handle<v8::Object> stats_object = v8::Object::New();
+    if (stats.numAnimationFrames)
+      stats_object->Set(v8::String::New("numAnimationFrames"),
+                        v8::Integer::New(stats.numAnimationFrames),
+                        v8::ReadOnly);
+    if (stats.numFramesSentToScreen)
+      stats_object->Set(v8::String::New("numFramesSentToScreen"),
+                        v8::Integer::New(stats.numFramesSentToScreen),
+                        v8::ReadOnly);
+    return stats_object;
   }
 };
 
