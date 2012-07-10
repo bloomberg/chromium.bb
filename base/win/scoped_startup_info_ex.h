e837955e67cb82703b18dfc8a63236d8ebc24b94
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-10 17:35:52 +0000

    Add scoped StartupInfoEx to base.
    Review URL: https://chromiumcodereview.appspot.com/10765008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145901 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a8c782f25bb3af639b326c506b8971f6ee33ff9c

---
+++ base/win/scoped_startup_info_ex.h
@@ -0,0 +1,43 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_WIN_SCOPED_STARTUP_INFO_EX_H_
+#define BASE_WIN_SCOPED_STARTUP_INFO_EX_H_
+#pragma once
+
+#include <windows.h>
+
+#include "base/base_export.h"
+#include "base/basictypes.h"
+
+namespace base {
+namespace win {
+
+// Manages the lifetime of additional attributes in STARTUPINFOEX.
+class BASE_EXPORT ScopedStartupInfoEx {
+ public:
+  ScopedStartupInfoEx();
+
+  ~ScopedStartupInfoEx();
+
+  // Initialize the attribute list for the specified number of entries.
+  bool InitializeProcThreadAttributeList(DWORD attribute_count);
+
+  // Sets one entry in the initialized attribute list.
+  bool UpdateProcThreadAttribute(DWORD_PTR attribute,
+                                 void* value,
+                                 size_t size);
+
+  STARTUPINFO* startup_info() { return &startup_info_.StartupInfo; }
+
+ private:
+  STARTUPINFOEX startup_info_;
+  DISALLOW_COPY_AND_ASSIGN(ScopedStartupInfoEx);
+};
+
+}  // namespace win
+}  // namespace base
+
+#endif  // BASE_WIN_SCOPED_STARTUP_INFO_EX_H_
+
