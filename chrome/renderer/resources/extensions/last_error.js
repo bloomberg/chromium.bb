46e4d4acc38d9855ef2883b389ae1dcdfd3beffa
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-10 22:03:55 +0000

    Fix to problem of chrome.runtime sometimes not being defined.
    There appears to be some non-determinism in the order that the chrome.runtime
    namespace gets set up relative to when the last_error.js file is loaded. This adds a require statement that should hopefully resolve this.
    
    BUG=136352
    TEST=Chrome webstore should function normally
    
    Review URL: https://chromiumcodereview.appspot.com/10748011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145964 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b9c34c87422070f5064490522ed60a92250e1d32

--- chrome/renderer/resources/extensions/last_error.js
+++ chrome/renderer/resources/extensions/last_error.js
@@ -2,8 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+requireNative('runtime');
+
 function set(message) {
-  var errorObject = { "message": message };
+  var errorObject = { 'message': message };
   if (chrome.extension)
     chrome.extension.lastError = errorObject;
   chrome.runtime.lastError = errorObject;
