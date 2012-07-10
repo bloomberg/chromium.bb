8744097a6b2cb10d73b98650d89850e57107c1a4
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-10 12:58:47 +0000

    Disable use of ShowBubbleView() on Android.
    This is part of Chrome for Android upstreaming (and also needed to link
    unit_tests).
    
    
    Review URL: https://chromiumcodereview.appspot.com/10689107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145864 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8361c93f3aa8d3445bdf5ce414a37ded3b44e62b

--- chrome/browser/ui/global_error/global_error.cc
+++ chrome/browser/ui/global_error/global_error.cc
@@ -31,8 +31,13 @@ bool GlobalError::HasShownBubbleView() {
 
 void GlobalError::ShowBubbleView(Browser* browser) {
   has_shown_bubble_view_ = true;
+#if defined(OS_ANDROID)
+  // http://crbug.com/136506
+  NOTIMPLEMENTED() << "Chrome for Android doesn't support global errors";
+#else
   bubble_view_ =
       GlobalErrorBubbleViewBase::ShowBubbleView(browser, AsWeakPtr());
+#endif
 }
 
 GlobalErrorBubbleViewBase* GlobalError::GetBubbleView() {
