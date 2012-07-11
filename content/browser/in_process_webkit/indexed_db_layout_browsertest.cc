5d116c4c61dc86cc87fe711dc50a3c9946fa44bd
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-11 19:54:26 +0000

    IndexedDB: Re-enable browser layout tests for transactions following WK patches
    The transaction-abort test was flaky in Chromium, fixed by WK90412/r122291. The others were
    disabled to allow landing WK89379/r121492 which renamed the tests.
    
    BUG=83226
    TEST=browser_tests --gtest_filter='IndexedDBLayoutTest.TransactionTests'
    
    
    Review URL: https://chromiumcodereview.appspot.com/10736036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146178 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: edb83fcf25b042b4369ebbbbadc9a45347706a50

--- content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
+++ content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
@@ -73,11 +73,10 @@ static const char* kKeyTests[] = {
 };
 
 static const char* kTransactionTests[] = {
-//  "transaction-abort.html", // Flaky, http://crbug.com/83226
-// TODO(jsbell): Re-enable the following 3 tests after http://webkit.org/b/89379 lands.
-// "transaction-complete-with-js-recursion-cross-frame.html",
-// "transaction-complete-with-js-recursion.html",
-// "transaction-complete-workers.html",
+  "transaction-abort.html",
+  "transaction-complete-with-js-recursion-cross-frame.html",
+  "transaction-complete-with-js-recursion.html",
+  "transaction-complete-workers.html",
   "transaction-after-close.html",
   "transaction-and-objectstore-calls.html",
   "transaction-basics.html",
