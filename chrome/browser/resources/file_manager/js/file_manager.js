fa803077a8fdb543b782285224ec7b7b78c4928b
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-12 14:24:04 +0000

    Adding "File type" combo box to open/save dialogs.
    File list filters according to the selected type.
    
    BUG=133866,135262
    TEST=Manual test
    
    
    Review URL: https://chromiumcodereview.appspot.com/10690135
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146338 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 444a27b2fa90b9a367a559e40bb67a995bc95e80

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -36,6 +36,8 @@ function FileManager(dialogDom) {
   this.document_ = dialogDom.ownerDocument;
   this.dialogType_ = this.params_.type || FileManager.DialogType.FULL_PAGE;
 
+  // Optional list of file types.
+  this.fileTypes_ = this.params_.typeList || [];
   metrics.recordEnum('Create', this.dialogType_,
       [FileManager.DialogType.SELECT_FOLDER,
       FileManager.DialogType.SELECT_SAVEAS_FILE,
@@ -469,9 +471,6 @@ FileManager.prototype = {
 
     this.collator_ = v8Intl.Collator([], {numeric: true, sensitivity: 'base'});
 
-    // Optional list of file types.
-    this.fileTypes_ = this.params_.typeList;
-
     this.showCheckboxes_ =
         (this.dialogType_ == FileManager.DialogType.FULL_PAGE ||
          this.dialogType_ == FileManager.DialogType.SELECT_OPEN_MULTI_FILE);
@@ -531,6 +530,8 @@ FileManager.prototype = {
 
     this.table_.endBatchUpdates();
     this.grid_.endBatchUpdates();
+
+    this.updateFileTypeFilter_();
 
     // Show the page now unless it's already delayed.
     this.delayShow_(0);
@@ -706,6 +707,8 @@ FileManager.prototype = {
     this.dialogDom_.querySelector('#search-box').addEventListener(
         'input', this.onSearchBoxUpdate_.bind(this));
 
+    this.fileTypeSelector_ = this.dialogDom_.querySelector('#file-type');
+    this.initFileTypeFilter_();
     // Populate the static localized strings.
     i18nTemplate.process(this.document_, loadTimeData);
   };
@@ -1039,26 +1042,13 @@ FileManager.prototype = {
   };
 
   /**
-   * "Save a file" dialog is supposed to have a combo box with available
-   * file types. Selecting an item filters files by extension and specifies how
-   * file should be saved.
+   * Index of selected item in the typeList of the dialog params.
    * @return {intener} Index of selected type from this.fileTypes_ + 1. 0
    *                   means value is not specified.
    */
-  FileManager.prototype.getSelectedFilterIndex_ = function(fileName) {
-    // TODO(serya): Implement the combo box
-    // For now try to guess choice by file extension.
-    if (!this.fileTypes_ || this.fileTypes_.length == 0) return 0;
-
-    var extension = /\.[^\.]+$/.exec(fileName);
-    extension = extension ? extension[0].substring(1).toLowerCase() : '';
-    var result = 0;  // Use first type by default.
-    for (var i = 0; i < this.fileTypes_.length; i++) {
-      if (this.fileTypes_[i].extensions.indexOf(extension)) {
-        result = i;
-      }
-    }
-    return result + 1;  // 1-based index.
+  FileManager.prototype.getSelectedFilterIndex_ = function() {
+    // 0 is the 'All files' item.
+    return Math.min(0, this.fileTypeSelector_.selectedIndex);
   };
 
   /**
@@ -1388,6 +1378,61 @@ FileManager.prototype = {
         (this.isOnGData() && this.gdataColumnModel_) ?
             this.gdataColumnModel_ :
             this.regularColumnModel_;
+  };
+
+  /**
+   * Fills the file type list or hides it.
+   */
+  FileManager.prototype.initFileTypeFilter_ = function() {
+    if (this.fileTypes_.length == 0) {
+      this.fileTypeSelector_.hidden = true;
+      return;
+    }
+
+    var option = this.document_.createElement('option');
+    option.innerText = str('ALL_FILES_FILTER');
+    this.fileTypeSelector_.appendChild(option);
+    option.value = 0;
+
+    for (var i = 0; i < this.fileTypes_.length; i++) {
+       var option = this.document_.createElement('option');
+       var description = this.fileTypes_[i].description;
+       if (!description) {
+         if (this.fileTypes_[i].extensions.length == 1) {
+           description = this.getFileTypeString_('.' +
+               this.fileTypes_[i].extensions[0]);
+         } else {
+           description = this.fileTypes_[i].extensions.join(', ');
+         }
+       }
+       option.innerText = description;
+
+       option.value = i + 1;
+
+       if (this.fileTypes_[i].selected)
+         option.selected = true;
+
+       this.fileTypeSelector_.appendChild(option);
+    }
+
+    this.fileTypeSelector_.addEventListener('change',
+        this.updateFileTypeFilter_.bind(this));
+  };
+
+  /**
+   * Filters file according to the selected file type.
+   */
+  FileManager.prototype.updateFileTypeFilter_ = function() {
+    this.directoryModel_.removeFilter('fileType');
+    var selectedIndex = Number(this.fileTypeSelector_.selectedIndex);
+    if (selectedIndex < 1)  // 'All files' or nothing selected.
+      return;
+    var regexp = new RegExp('.*(' +
+        this.fileTypes_[selectedIndex - 1].extensions.join('|') + ')$', 'i');
+    function filter(entry) {
+      return entry.isDirectory || regexp.test(entry.name);
+    }
+    this.directoryModel_.addFilter('fileType', filter);
   };
 
   /**
@@ -4271,7 +4316,7 @@ FileManager.prototype = {
       var singleSelection = {
         urls: [url],
         multiple: false,
-        filterIndex: this.getSelectedFilterIndex_(url)
+        filterIndex: this.getSelectedFilterIndex_()
       };
       this.selectFilesAndClose_(singleSelection);
       return;
@@ -4321,7 +4366,7 @@ FileManager.prototype = {
     var singleSelection = {
       urls: [files[0]],
       multiple: false,
-      filterIndex: this.getSelectedFilterIndex_(files[0])
+      filterIndex: this.getSelectedFilterIndex_()
     };
     this.selectFilesAndClose_(singleSelection);
   };
