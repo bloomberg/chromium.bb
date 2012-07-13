06e4cc4d6362a0356cf45af663d92ea7fdffb514
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-13 04:45:04 +0000

    Set active URL for GPU process when processing delayed work.
    BUG=131466
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10696216
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146521 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a5b2abb91410f82f2e1dcc525e97cc7fa8bac0dc

--- content/common/gpu/gpu_command_buffer_stub.cc
+++ content/common/gpu/gpu_command_buffer_stub.cc
@@ -205,6 +205,7 @@ bool GpuCommandBufferStub::HasMoreWork() {
 void GpuCommandBufferStub::PollWork() {
   TRACE_EVENT0("gpu", "GpuCommandBufferStub::PollWork");
   delayed_work_scheduled_ = false;
+  FastSetActiveURL(active_url_, active_url_hash_);
   if (decoder_.get() && !MakeCurrent())
     return;
   if (scheduler_.get())
