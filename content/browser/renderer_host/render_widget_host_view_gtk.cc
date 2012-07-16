10e86b11d99c8740877f7317255fa1814d95801a
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 19:08:19 +0000

    Fix multi monitor fullscreen issue on PPAPI for Linux.
    Old logic does not consider dual monitors.
    With this CL, Chrome makes monitor-wide full screen window instead of screen-wide full screen window.
    
    BUG=136564
    TEST=Try bot and manually done.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10693149
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146850 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ceed30fd5a106cc325f414d302574b469a378d65

--- content/browser/renderer_host/render_widget_host_view_gtk.cc
+++ content/browser/renderer_host/render_widget_host_view_gtk.cc
@@ -657,13 +657,13 @@ void RenderWidgetHostViewGtk::InitAsPopup(
 }
 
 void RenderWidgetHostViewGtk::InitAsFullscreen(
-    RenderWidgetHostView* /*reference_host_view*/) {
+    RenderWidgetHostView* reference_host_view) {
+  DCHECK(reference_host_view);
   DoSharedInit();
 
   is_fullscreen_ = true;
   GtkWindow* window = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
   gtk_window_set_decorated(window, FALSE);
-  gtk_window_fullscreen(window);
   destroy_handler_id_ = g_signal_connect(GTK_WIDGET(window),
                                          "destroy",
                                          G_CALLBACK(OnDestroyThunk),
@@ -673,8 +673,23 @@ void RenderWidgetHostViewGtk::InitAsFullscreen(
   // Try to move and resize the window to cover the screen in case the window
   // manager doesn't support _NET_WM_STATE_FULLSCREEN.
   GdkScreen* screen = gtk_window_get_screen(window);
-  gfx::Rect bounds(
-      0, 0, gdk_screen_get_width(screen), gdk_screen_get_height(screen));
+  GdkWindow* ref_gdk_window = gtk_widget_get_window(
+      reference_host_view->GetNativeView());
+
+  gfx::Rect bounds;
+  if (ref_gdk_window) {
+    const int monitor_id = gdk_screen_get_monitor_at_window(screen,
+                                                            ref_gdk_window);
+    GdkRectangle monitor_rect;
+    gdk_screen_get_monitor_geometry(screen, monitor_id, &monitor_rect);
+    bounds = gfx::Rect(monitor_rect);
+  } else {
+    bounds = gfx::Rect(
+        0, 0, gdk_screen_get_width(screen), gdk_screen_get_height(screen));
+  }
+  gtk_window_move(window, bounds.x(), bounds.y());
+  gtk_window_resize(window, bounds.width(), bounds.height());
+  gtk_window_fullscreen(window);
   DoPopupOrFullscreenInit(window, bounds);
 }
 
