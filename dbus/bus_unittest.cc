37a4f25a161adfd27e4b8b294b4e965ccc478f78
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-18 14:20:13 +0000

    Marking BusTest.UnregisterExportedObject as flaky
    BUG=137846
    TEST=none
    TBR=keybuk@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10797009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147234 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 09862fad65af12890bf2039fe3d7172b0c1c8f6e

--- dbus/bus_unittest.cc
+++ dbus/bus_unittest.cc
@@ -108,7 +108,8 @@ TEST(BusTest, GetExportedObject) {
   bus->ShutdownAndBlock();
 }
 
-TEST(BusTest, UnregisterExportedObject) {
+// http://crbug.com/137846
+TEST(BusTest, FLAKY_UnregisterExportedObject) {
   // Start the D-Bus thread.
   base::Thread::Options thread_options;
   thread_options.message_loop_type = MessageLoop::TYPE_IO;
