4d14c3181cde794b2b24ad5b6d5ece6ed2801f1f
cduvall@chromium.org <cduvall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 23:39:23 +0000

    Extensions Docs Server: PRESUBMIT.py fix
    Now the presubmit checks will pass even if you haven't run build_server.py.
    We can also use the try-bots now for patches to server2.
    
    BUG=131095, 138206
    
    Review URL: https://chromiumcodereview.appspot.com/10805044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147741 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8e991c1448aa5853da90e8294a4539e488c1b549

--- chrome/common/extensions/docs/server2/PRESUBMIT.py
+++ chrome/common/extensions/docs/server2/PRESUBMIT.py
@@ -8,12 +8,19 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into gcl.
 """
 
+# Run build_server so that files needed by tests are copied to the local
+# third_party directory.
+import sys
+sys.path.insert(0, '.')
+import build_server
+build_server.main()
+sys.path.pop(0)
+
 WHITELIST = [ r'.+_test.py$' ]
 
 def CheckChangeOnUpload(input_api, output_api):
   return input_api.canned_checks.RunUnitTestsInDirectory(
       input_api, output_api, '.', whitelist=WHITELIST)
-
 
 def CheckChangeOnCommit(input_api, output_api):
   return input_api.canned_checks.RunUnitTestsInDirectory(
