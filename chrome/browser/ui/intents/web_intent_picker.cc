00ce85d05554ac3ef897bb26689718b1cab3b680
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 00:54:25 +0000

    Removed unused constants/functions
    BUG=134557
    TEST=none
    
    R=jhawkins@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10810008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147580 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4ceddd5525785f4e6b0a152272c8a49f67619459

--- chrome/browser/ui/intents/web_intent_picker.cc
+++ chrome/browser/ui/intents/web_intent_picker.cc
@@ -13,12 +13,6 @@
 
 namespace {
 
-// The minimum width of the inline disposition tab contents.
-const int kMinInlineDispositionWidth = 0;
-
-// The minimum height of the inline disposition tab contents.
-const int kMinInlineDispositionHeight = 0;
-
 // Maximum inline disposition container sizes.
 const int kMaxInlineDispositionWidth = 900;
 const int kMaxInlineDispositionHeight = 900;
@@ -26,19 +20,8 @@ const int kMaxInlineDispositionHeight = 900;
 }  // namespace
 
 // static
-gfx::Size WebIntentPicker::GetDefaultInlineDispositionSize(
-    content::WebContents* web_contents) {
-  // TODO(gbillock): This size calculation needs more thought.
-  gfx::Size tab_size = web_contents->GetView()->GetContainerSize();
-  int width = std::max(tab_size.width()/2, kMinInlineDispositionWidth);
-  int height = std::max(tab_size.height()/2, kMinInlineDispositionHeight);
-
-  return gfx::Size(width, height);
-}
-
-// static
 gfx::Size WebIntentPicker::GetMinInlineDispositionSize() {
-  return gfx::Size(kMinInlineDispositionWidth, kMinInlineDispositionHeight);
+  return gfx::Size(0, 0);
 }
 
 // static
