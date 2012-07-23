69f5a6ff87619db529844aa3ff11f83fa063263b
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-23 15:48:39 +0000

    Use find_runtime_symbols in dmprof.
    BUG=123763
    TEST=use dmprof.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10795043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147856 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 082e4ae7d486fb1abd5ba7f165285018d0f0ed0d

--- tools/deep_memory_profiler/download.sh
+++ tools/deep_memory_profiler/download.sh
@@ -5,8 +5,3 @@
 svn checkout --force \
     http://src.chromium.org/chrome/trunk/src/tools/deep_memory_profiler \
     deep_memory_profiler
-
-curl -o deep_memory_profiler/pprof \
-    http://src.chromium.org/chrome/trunk/src/third_party/tcmalloc/chromium/src/pprof
-
-chmod a+x deep_memory_profiler/pprof
