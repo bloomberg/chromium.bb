09e94792e9ea3716c05968b7a8d579095cc484ec
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 20:53:33 +0000

    This is a straight file move for SVN history preserving purposes.
    A future patch will modify and adapt these files.
    
    BUG=134529
    TEST=none
    TBR=ben
    Review URL: https://chromiumcodereview.appspot.com/10802094
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148194 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3ae3b5b8d5c75f69b8ca49cadd1dc660ac3be66d

---
+++ chrome/browser/ui/libgtk2ui/select_file_dialog_impl.cc
@@ -0,0 +1,90 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// This file implements common select dialog functionality between GTK and KDE.
+
+#include "chrome/browser/ui/gtk/select_file_dialog_impl.h"
+
+#include "base/environment.h"
+#include "base/file_util.h"
+#include "base/nix/xdg_util.h"
+#include "base/threading/thread_restrictions.h"
+#include "content/public/browser/browser_thread.h"
+
+using content::BrowserThread;
+
+namespace {
+
+enum UseKdeFileDialogStatus {
+  UNKNOWN,
+  NO_KDE,
+  YES_KDE
+};
+
+UseKdeFileDialogStatus use_kde_ = UNKNOWN;
+
+}  // namespace
+
+FilePath* SelectFileDialogImpl::last_saved_path_ = NULL;
+FilePath* SelectFileDialogImpl::last_opened_path_ = NULL;
+
+// static
+SelectFileDialog* SelectFileDialog::Create(Listener* listener,
+                                           ui::SelectFilePolicy* policy) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  if (use_kde_ == UNKNOWN) {
+    // Start out assumimg we are not going to use KDE.
+    use_kde_ = NO_KDE;
+
+    // Check to see if KDE is the desktop environment.
+    scoped_ptr<base::Environment> env(base::Environment::Create());
+    base::nix::DesktopEnvironment desktop =
+        base::nix::GetDesktopEnvironment(env.get());
+    if (desktop == base::nix::DESKTOP_ENVIRONMENT_KDE3 ||
+        desktop == base::nix::DESKTOP_ENVIRONMENT_KDE4) {
+      // Check to see if the user dislikes the KDE file dialog.
+      if (!env->HasVar("NO_CHROME_KDE_FILE_DIALOG")) {
+        // Check to see if the KDE dialog works.
+        if (SelectFileDialogImpl::CheckKDEDialogWorksOnUIThread()) {
+          use_kde_ = YES_KDE;
+        }
+      }
+    }
+  }
+
+  if (use_kde_ == NO_KDE)
+    return SelectFileDialogImpl::NewSelectFileDialogImplGTK(listener, policy);
+
+  scoped_ptr<base::Environment> env(base::Environment::Create());
+  base::nix::DesktopEnvironment desktop =
+      base::nix::GetDesktopEnvironment(env.get());
+  return SelectFileDialogImpl::NewSelectFileDialogImplKDE(
+      listener, policy, desktop);
+}
+
+SelectFileDialogImpl::SelectFileDialogImpl(Listener* listener,
+                                           ui::SelectFilePolicy* policy)
+    : SelectFileDialog(listener, policy),
+      file_type_index_(0),
+      type_(SELECT_NONE) {
+  if (!last_saved_path_) {
+    last_saved_path_ = new FilePath();
+    last_opened_path_ = new FilePath();
+  }
+}
+
+SelectFileDialogImpl::~SelectFileDialogImpl() { }
+
+void SelectFileDialogImpl::ListenerDestroyed() {
+  listener_ = NULL;
+}
+
+bool SelectFileDialogImpl::IsRunning(gfx::NativeWindow parent_window) const {
+  return parents_.find(parent_window) != parents_.end();
+}
+
+bool SelectFileDialogImpl::CallDirectoryExistsOnUIThread(const FilePath& path) {
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
+  return file_util::DirectoryExists(path);
+}
