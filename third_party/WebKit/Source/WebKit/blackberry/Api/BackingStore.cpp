bloomberg/chromium.bb

commit c53ab92806ea8f0c9516df6a01d0c990b33d2e25

Source/WebCore: [Soup] Unnecessary RefPtr usage and null check https://bugs.webkit.org/show_bug.cgi?id=91636
Patch by Gustavo Noronha Silva <gustavo.noronha@collabora.com> on 2012-07-18
Reviewed by Martin Robinson.

Small clean up of code that is not necessary; no change in functionality.

* platform/network/soup/SocketStreamHandleSoup.cpp:
(WebCore::SocketStreamHandle::connected):

Source/WebKit/blackberry: [BlackBerry] clock_gettime() in BackingStore.cpp should use CLOCK_MONOTONIC
https://bugs.webkit.org/show_bug.cgi?id=91898

Patch by Jacky Jiang <zhajiang@rim.com> on 2012-07-20
Reviewed by Yong Li.

PR: 181043
Use CLOCK_MONOTONIC when we do expect a monotonic timer.

* Api/BackingStore.cpp:
(BlackBerry::WebKit::BackingStorePrivate::blitContents):

LayoutTests: [Qt] svg/text rebaseline after new test fonts (part 1)
https://bugs.webkit.org/show_bug.cgi?id=91762

Patch by Caio Marcelo de Oliveira Filho <caio.oliveira@openbossa.org> on 2012-07-20
Reviewed by Andreas Kling.

* platform/qt-5.0-wk2/svg/text/kerning-expected.png: Removed.
* platform/qt-5.0-wk2/svg/text/kerning-expected.txt: Removed.
* platform/qt-5.0/Skipped:
* platform/qt-5.0/svg/text/text-text-01-b-expected.png: Removed.
* platform/qt-5.0/svg/text/text-text-01-b-expected.txt: Removed.
* platform/qt-5.0/svg/text/text-text-04-t-expected.png: Removed.
* platform/qt-5.0/svg/text/text-text-04-t-expected.txt: Removed.
* platform/qt/svg/text/kerning-expected.png:
* platform/qt/svg/text/kerning-expected.txt:
* platform/qt/svg/text/small-fonts-2-expected.png:
* platform/qt/svg/text/small-fonts-2-expected.txt:
* platform/qt/svg/text/small-fonts-3-expected.png:
* platform/qt/svg/text/small-fonts-3-expected.txt:
* platform/qt/svg/text/small-fonts-expected.png:
* platform/qt/svg/text/small-fonts-expected.txt: Added.
* platform/qt/svg/text/small-fonts-in-html5-expected.png:
* platform/qt/svg/text/small-fonts-in-html5-expected.txt:
* platform/qt/svg/text/text-text-01-b-expected.png:

third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
@@ -1602,7 +1602,7 @@ void BackingStorePrivate::blitContents(const Platform::IntRect& dstRect,
         for (unsigned int i = 0; i < blittedTiles.size(); ++i)
             blittedTiles[i]->setBlitGeneration(m_blitGeneration);
 
-        clock_gettime(CLOCK_REALTIME, &m_currentBlitEnd);
+        clock_gettime(CLOCK_MONOTONIC, &m_currentBlitEnd);
         m_currentBlitEnd.tv_nsec += 30 * 1000 * 1000;
         if (m_currentBlitEnd.tv_nsec >= 1000000000L) {
             m_currentBlitEnd.tv_sec  += 1;

commit 1ef47aabf580407a241c70d9cfe13d5e0eeab0a2

[BlackBerry] clock_gettime() in BackingStore.cpp should use CLOCK_MONOTONIC https://bugs.webkit.org/show_bug.cgi?id=91898
Reviewed by Yong Li.
Patch by Jacky Jiang  <zhajiang@rim.com>

Let pthread condition variable in BackingStore.cpp use monotonic clock
to fix a regression caused by r123266.

* Api/BackingStore.cpp:
(BlackBerry::WebKit::BackingStorePrivate::BackingStorePrivate):

git-svn-id: svn://svn.chromium.org/blink/trunk@123629 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 49b412c25f1292a5a13adcdb8d28411363162655

third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
@@ -228,7 +228,12 @@ BackingStorePrivate::BackingStorePrivate()
     pthread_mutexattr_destroy(&attr);
 
     pthread_mutex_init(&m_blitGenerationLock, 0);
-    pthread_cond_init(&m_blitGenerationCond, 0);
+
+    pthread_condattr_t condattr;
+    pthread_condattr_init(&condattr);
+    pthread_condattr_setclock(&condattr, CLOCK_MONOTONIC);
+    pthread_cond_init(&m_blitGenerationCond, &condattr);
+    pthread_condattr_destroy(&condattr);
 }
 
 BackingStorePrivate::~BackingStorePrivate()