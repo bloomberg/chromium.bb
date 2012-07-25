d376f8dfe93b2a867b0b9ad3f04639346710917f
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-25 02:31:12 +0000

    Revert 148272 - Refactor the wallpaper encoding to another file/class. Encoding the wallpaper to png from a skia is something we'll need to do from at least one more place (the screensaver images source) so it makes sense to refactor this code to pull out the encoder so it's usable from other places too.
    R=bshe@chromium.org,csilv@chromium.org
    BUG=None.
    TEST=Tested with the lock screen to verify that the background set for the user is pulled and displayed.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10790084
    
    TBR=rkc@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10824006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148278 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e06110d9deb9b0e6301adeb90a8ca5d3ef25c0ac

--- chrome/browser/ui/webui/options2/chromeos/wallpaper_source.h
+++ chrome/browser/ui/webui/options2/chromeos/wallpaper_source.h
@@ -14,9 +14,6 @@
 #include "third_party/skia/include/core/SkBitmap.h"
 
 namespace chromeos {
-
-class SimplePngEncoder;
-
 namespace options2 {
 
 // A DataSource for chrome://wallpaper/ URL, provides current user's wallpaper.
@@ -46,15 +43,12 @@ class WallpaperImageSource : public ChromeURLDataManager::DataSource {
   // Cancel current image encoding operation.
   void CancelPendingEncodingOperation();
 
-  // Callback to call if we cancel the wallpaper encode operation.
-  void CancelCallback(int request_id);
-
-  // Send image stored in |data| as a reply to request
+    // Send image stored in |data| as a reply to request
   // identifed by |request_id|.
   void SendCurrentUserWallpaper(int request_id,
                                 scoped_refptr<base::RefCountedBytes> data);
 
-  scoped_refptr<SimplePngEncoder> png_encoder_;
+  scoped_refptr<WallpaperEncodingOperation> wallpaper_encoding_op_;
 
   base::WeakPtrFactory<WallpaperImageSource> weak_ptr_factory_;
 
