bloomberg/chromium.bb

commit dd263e546bc20a2a7f424374afc9c81e5f59f2de

window.c: Only set input region when we have a decoration
https://bugs.freedesktop.org/show_bug.cgi?id=50490

clients/window.c
@@ -1260,6 +1260,13 @@ frame_resize_handler(struct widget *widget,
 		allocation.width = width - decoration_width;
 		allocation.height = height - decoration_height;
 
+		widget->window->input_region =
+			wl_compositor_create_region(display->compositor);
+		wl_region_add(widget->window->input_region,
+			      t->margin, t->margin,
+			      width - 2 * t->margin,
+			      height - 2 * t->margin);
+
 		opaque_margin = t->margin + t->frame_radius;
 
 		wl_list_for_each(button, &frame->buttons_list, link)
@@ -1289,13 +1296,6 @@ frame_resize_handler(struct widget *widget,
 
 	width = child->allocation.width + decoration_width;
 	height = child->allocation.height + decoration_height;
-
-	widget->window->input_region =
-		      wl_compositor_create_region(display->compositor);
-	wl_region_add(widget->window->input_region,
-		      t->margin, t->margin,
-		      width - 2 * t->margin,
-		      height - 2 * t->margin);
 
 	widget_set_allocation(widget, 0, 0, width, height);
 

commit 023be102d7255118d6d7022a93823110a3a0a4c0

window.c: Set input region after (potentially) adjusting the surface size
Regressed in dd263e546bc20a2a7f42, spotted by Scott Moreau <oreaus@gmail.com>

clients/window.c
@@ -1252,13 +1252,6 @@ frame_resize_handler(struct widget *widget,
 		allocation.width = width - decoration_width;
 		allocation.height = height - decoration_height;
 
-		widget->window->input_region =
-			wl_compositor_create_region(display->compositor);
-		wl_region_add(widget->window->input_region,
-			      t->margin, t->margin,
-			      width - 2 * t->margin,
-			      height - 2 * t->margin);
-
 		opaque_margin = t->margin + t->frame_radius;
 
 		wl_list_for_each(button, &frame->buttons_list, link)
@@ -1288,6 +1281,15 @@ frame_resize_handler(struct widget *widget,
 
 	width = child->allocation.width + decoration_width;
 	height = child->allocation.height + decoration_height;
+
+	if (widget->window->type != TYPE_FULLSCREEN) {
+		widget->window->input_region =
+			wl_compositor_create_region(display->compositor);
+		wl_region_add(widget->window->input_region,
+			      t->margin, t->margin,
+			      width - 2 * t->margin,
+			      height - 2 * t->margin);
+	}
 
 	widget_set_allocation(widget, 0, 0, width, height);
 