db09a4db5228a5e0d7b443debe1520570faf0b0a
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-31 08:30:40 +0000

    Fix clip_span() to use memmove() instead of memcpy(). This is actually a merge of upstream commit a44d715d2b19dc2f8f48b01144cc38e4e2c5015a
    BUG=139587,132211
    Review URL: https://chromiumcodereview.appspot.com/10833067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149148 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3f869667a8206993904b53faf62325dd333ec14e

--- third_party/mesa/MesaLib/src/mesa/swrast/s_span.c
+++ third_party/mesa/MesaLib/src/mesa/swrast/s_span.c
@@ -762,7 +762,7 @@ clip_span( GLcontext *ctx, SWspan *span )
          span->intTex[1] += leftClip * span->intTexStep[1];
 
 #define SHIFT_ARRAY(ARRAY, SHIFT, LEN) \
-         memcpy(ARRAY, ARRAY + (SHIFT), (LEN) * sizeof(ARRAY[0]))
+         memmove(ARRAY, ARRAY + (SHIFT), (LEN) * sizeof(ARRAY[0]))
 
          for (i = 0; i < FRAG_ATTRIB_MAX; i++) {
             if (span->arrayAttribs & (1 << i)) {
