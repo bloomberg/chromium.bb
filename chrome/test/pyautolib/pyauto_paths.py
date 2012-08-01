48aafaca89d1c8da491528e11da130fc72faf99a
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-01 01:03:08 +0000

    Add out dir to win32 for winja
    R=nirnimesh@chromium.org
    BUG=134540
    
    
    Review URL: https://chromiumcodereview.appspot.com/10832091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149340 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ff97e16416ff894fc13c66ea23c8d98bbc249949

--- chrome/test/pyautolib/pyauto_paths.py
+++ chrome/test/pyautolib/pyauto_paths.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -26,7 +26,7 @@ def GetBuildDirs():
       'linux2': ['out', 'sconsbuild'],
       'linux3': ['out', 'sconsbuild'],
       'darwin': ['out', 'xcodebuild'],
-      'win32':  ['chrome', 'build'],
+      'win32':  ['chrome', 'build', 'out'],
       'cygwin': ['chrome'],
   }.get(sys.platform, [])
   src_dir = GetSourceDir()
