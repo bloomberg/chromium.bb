91f49a4a4dbd816ca7c952ce1487e7f303a3a2e1
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 23:14:21 +0000

    Create a LinuxSandbox class.
    The LinuxSandbox class aims to become the central place for Linux
    sandboxing inside content/.
    
    For now, this refactors mostly code from the Zygote.
    
    (Note: this is a re-land of https://chromiumcodereview.appspot.com/10826093/
     with a trivial fix for ARM architectures).
    
    BUG=
    TBR=piman@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/10843059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149734 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3426ce1981d7ace455c51c5cc74fe80e5d2d5b4d

--- content/common/child_process_sandbox_support_impl_linux.cc
+++ content/common/child_process_sandbox_support_impl_linux.cc
@@ -10,7 +10,7 @@
 #include "base/memory/scoped_ptr.h"
 #include "base/pickle.h"
 #include "base/posix/unix_domain_socket.h"
-#include "content/common/sandbox_methods_linux.h"
+#include "content/common/sandbox_linux.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/linux/WebFontFamily.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/linux/WebFontRenderStyle.h"
 
