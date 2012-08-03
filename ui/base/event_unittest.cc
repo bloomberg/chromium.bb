471d76b2266ad0c2152df09957cf6b22ac35622e
beng@google.com <beng@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-03 20:46:51 +0000

    Copy aura::Event into ui::Event.
    http://crbug.com/125937
    TEST=existing
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=149888
    Review URL: https://chromiumcodereview.appspot.com/10831137
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149916 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2c51dbe6f490dd907389581b360b4f84c8621283

---
+++ ui/base/event_unittest.cc
@@ -0,0 +1,102 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/base/event.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#if defined(USE_X11)
+#include <X11/Xlib.h>
+#include "ui/base/x/x11_util.h"
+#endif
+
+namespace ui {
+
+TEST(EventTest, NoNativeEvent) {
+  KeyEvent keyev(ET_KEY_PRESSED, VKEY_SPACE, 0);
+  EXPECT_FALSE(keyev.HasNativeEvent());
+}
+
+TEST(EventTest, NativeEvent) {
+#if defined(OS_WIN)
+  MSG native_event = { NULL, WM_KEYUP, VKEY_A, 0 };
+  KeyEvent keyev(native_event, false);
+  EXPECT_TRUE(keyev.HasNativeEvent());
+#elif defined(USE_X11)
+  scoped_ptr<XEvent> native_event(new XEvent);
+  InitXKeyEventForTesting(ET_KEY_RELEASED, VKEY_A, 0, native_event.get());
+  KeyEvent keyev(native_event.get(), false);
+  EXPECT_TRUE(keyev.HasNativeEvent());
+#endif
+}
+
+TEST(EventTest, GetCharacter) {
+  // Check if Control+Enter returns 10.
+  KeyEvent keyev1(ET_KEY_PRESSED, VKEY_RETURN, EF_CONTROL_DOWN);
+  EXPECT_EQ(10, keyev1.GetCharacter());
+  EXPECT_EQ(13, keyev1.GetUnmodifiedCharacter());
+  // Check if Enter returns 13.
+  KeyEvent keyev2(ET_KEY_PRESSED, VKEY_RETURN, 0);
+  EXPECT_EQ(13, keyev2.GetCharacter());
+  EXPECT_EQ(13, keyev2.GetUnmodifiedCharacter());
+
+#if defined(USE_X11)
+  // For X11, test the functions with native_event() as well. crbug.com/107837
+  scoped_ptr<XEvent> native_event(new XEvent);
+
+  InitXKeyEventForTesting(ET_KEY_PRESSED, VKEY_RETURN, EF_CONTROL_DOWN,
+                          native_event.get());
+  KeyEvent keyev3(native_event.get(), false);
+  EXPECT_EQ(10, keyev3.GetCharacter());
+  EXPECT_EQ(13, keyev3.GetUnmodifiedCharacter());
+
+  InitXKeyEventForTesting(ET_KEY_PRESSED, VKEY_RETURN, 0, native_event.get());
+  KeyEvent keyev4(native_event.get(), false);
+  EXPECT_EQ(13, keyev4.GetCharacter());
+  EXPECT_EQ(13, keyev4.GetUnmodifiedCharacter());
+#endif
+}
+
+TEST(EventTest, ClickCount) {
+  const gfx::Point origin(0, 0);
+  MouseEvent mouseev(ET_MOUSE_PRESSED, origin, origin, 0);
+  for (int i = 1; i <=3 ; ++i) {
+    mouseev.SetClickCount(i);
+    EXPECT_EQ(i, mouseev.GetClickCount());
+  }
+}
+
+TEST(EventTest, Repeated) {
+  const gfx::Point origin(0, 0);
+  MouseEvent mouse_ev1(ET_MOUSE_PRESSED, origin, origin, 0);
+  MouseEvent mouse_ev2(ET_MOUSE_PRESSED, origin, origin, 0);
+  MouseEvent::TestApi test_ev1(&mouse_ev1);
+  MouseEvent::TestApi test_ev2(&mouse_ev2);
+
+  base::TimeDelta start = base::TimeDelta::FromMilliseconds(0);
+  base::TimeDelta soon = start + base::TimeDelta::FromMilliseconds(1);
+  base::TimeDelta later = start + base::TimeDelta::FromMilliseconds(1000);
+
+  // Close point.
+  test_ev1.set_location(gfx::Point(0, 0));
+  test_ev2.set_location(gfx::Point(1, 0));
+  test_ev1.set_time_stamp(start);
+  test_ev2.set_time_stamp(soon);
+  EXPECT_TRUE(MouseEvent::IsRepeatedClickEvent(mouse_ev1, mouse_ev2));
+
+  // Too far.
+  test_ev1.set_location(gfx::Point(0, 0));
+  test_ev2.set_location(gfx::Point(10, 0));
+  test_ev1.set_time_stamp(start);
+  test_ev2.set_time_stamp(soon);
+  EXPECT_FALSE(MouseEvent::IsRepeatedClickEvent(mouse_ev1, mouse_ev2));
+
+  // Too long a time between clicks.
+  test_ev1.set_location(gfx::Point(0, 0));
+  test_ev2.set_location(gfx::Point(0, 0));
+  test_ev1.set_time_stamp(start);
+  test_ev2.set_time_stamp(later);
+  EXPECT_FALSE(MouseEvent::IsRepeatedClickEvent(mouse_ev1, mouse_ev2));
+}
+
+}  // namespace ui
--- ui/ui_unittests.gypi
+++ ui/ui_unittests.gypi
@@ -67,6 +67,7 @@
         'base/cocoa/events_mac_unittest.mm',
         'base/cocoa/focus_tracker_unittest.mm',
         'base/dialogs/select_file_dialog_win_unittest.cc',
+        'base/event_unittest.cc',
         'base/gtk/gtk_expanded_container_unittest.cc',
         'base/gtk/gtk_im_context_util_unittest.cc',
         'base/gtk/menu_label_accelerator_util_unittest.cc',
@@ -207,17 +208,21 @@
             'base/view_prop_unittest.cc',
           ],
         }],
+        ['use_aura==1 or toolkit_views==1',  {
+          'sources': [
+            'base/gestures/velocity_calculator_unittest.cc',
+          ],
+        }, {
+          'sources!': [
+            'base/event_unittest.cc',
+          ],
+        }],
         ['use_aura==1', {
           'sources!': [
             'base/dialogs/select_file_dialog_win_unittest.cc',
             'base/dragdrop/os_exchange_data_win_unittest.cc',
             'base/native_theme/native_theme_win_unittest.cc',
             'gfx/screen_unittest.cc',
-          ],
-        }],
-        ['use_aura==1 or toolkit_views==1', {
-          'sources': [
-            'base/gestures/velocity_calculator_unittest.cc',
           ],
         }],
       ],
