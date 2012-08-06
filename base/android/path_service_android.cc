7c7e1dec334387c4a3aa58ae37b533949da13e8b
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-06 17:56:04 +0000

    Move base/android/path_service.cc|h to path_service_android.cc|h
    Gyp targets shouldn't contain source with identical basenames.
    
    Fallout from http://src.chromium.org/viewvc/chrome?view=rev&revision=149842
    
    
    Review URL: https://chromiumcodereview.appspot.com/10828152
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150110 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 61be42da3bb094cb9b5ebff6bf8d61c55479b0bf

--- base/android/path_service_android.cc
+++ base/android/path_service_android.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/android/path_service.h"
+#include "base/android/path_service_android.h"
 
 #include "base/file_path.h"
 #include "base/path_service.h"
--- base/android/path_service_android.h
+++ base/android/path_service_android.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BASE_ANDROID_PATH_SERVICE_H_
-#define BASE_ANDROID_PATH_SERVICE_H_
+#ifndef BASE_ANDROID_PATH_SERVICE_ANDROID_H_
+#define BASE_ANDROID_PATH_SERVICE_ANDROID_H_
 
 #include <jni.h>
 
@@ -15,4 +15,4 @@ bool RegisterPathService(JNIEnv* env);
 }  // namespace android
 }  // namespace base
 
-#endif  // BASE_ANDROID_PATH_SERVICE_H_
+#endif  // BASE_ANDROID_PATH_SERVICE_ANDROID_H_
