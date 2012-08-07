6372cb69ee2b5fc9a1ca535114683ff19486ec61
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 16:01:45 +0000

    extension_utils: Fix include path and header guards.
    BUG=125846
    TBR=ben@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10834206
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150356 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d4360f0783698ed6e6d2e0dee0eadb78a6f14896

--- chrome/browser/ui/ash/extension_utils.cc
+++ chrome/browser/ui/ash/extension_utils.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/views/ash/extension_utils.h"
+#include "chrome/browser/ui/ash/extension_utils.h"
 
 #include "chrome/browser/event_disposition.h"
 #include "chrome/browser/extensions/extension_prefs.h"
