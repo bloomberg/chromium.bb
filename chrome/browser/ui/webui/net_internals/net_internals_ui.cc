4db92b787f82c61705782d953a89b7c980532207
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 18:14:34 +0000

    Implement SHA-256 fingerprint support
    The HTTP-based Public Key Pinning Internet Draft
    (tools.ietf.org/html/draft-ietf-websec-key-pinning) requires this.
    
    Per wtc, give the *Fingeprint* types more meaningful *HashValue* names.
    
    Cleaning up lint along the way.
    
    BUG=117914
    TEST=net_unittests, unit_tests TransportSecurityPersisterTest
    Review URL: https://chromiumcodereview.appspot.com/10825211
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150375 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2c6349934c1e8e8199431d79ade127c18700fc1f

--- chrome/browser/ui/webui/net_internals/net_internals_ui.cc
+++ chrome/browser/ui/webui/net_internals/net_internals_ui.cc
@@ -1084,18 +1084,32 @@ void NetInternalsMessageHandler::IOThreadImpl::OnStartConnectionTests(
   connection_tester_->RunAllTests(url);
 }
 
-void SPKIHashesToString(const net::FingerprintVector& hashes,
+void SPKIHashesToString(const net::HashValueVector& hashes,
                         std::string* string) {
-  for (net::FingerprintVector::const_iterator
+  for (net::HashValueVector::const_iterator
        i = hashes.begin(); i != hashes.end(); ++i) {
-    base::StringPiece hash_str(reinterpret_cast<const char*>(i->data),
-                               arraysize(i->data));
+    std::string label;
+    switch (i->tag) {
+      case net::HASH_VALUE_SHA1:
+        label = "sha1/";
+        break;
+      case net::HASH_VALUE_SHA256:
+        label = "sha256/";
+        break;
+      default:
+        NOTREACHED();
+        LOG(WARNING) << "Invalid fingerprint of unknown type " << i->tag;
+        label = "unknown/";
+    }
+
+    base::StringPiece hash_str(reinterpret_cast<const char*>(i->data()),
+                               i->size());
     std::string encoded;
     base::Base64Encode(hash_str, &encoded);
 
     if (i != hashes.begin())
       *string += ",";
-    *string += "sha1/" + encoded;
+    *string += label + encoded;
   }
 }
 
@@ -1171,7 +1185,7 @@ void NetInternalsMessageHandler::IOThreadImpl::OnHSTSAdd(
          i = type_and_b64s.begin(); i != type_and_b64s.end(); ++i) {
       std::string type_and_b64;
       RemoveChars(*i, " \t\r\n", &type_and_b64);
-      net::SHA1Fingerprint hash;
+      net::HashValue hash;
       if (!net::TransportSecurityState::ParsePin(type_and_b64, &hash))
         continue;
 
