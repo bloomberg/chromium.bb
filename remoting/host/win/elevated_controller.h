74b529654d88fa11ad9ca4e5c7651a151a848a6e
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-08 04:25:07 +0000

    Reverting r148671, r149866 (broke VS2008 builder) and r150361 (broke ninja).
    Rolling GYP to r1458 to undo (r1457, r1452, r1450, r1449).
    
    BUG=141132, 137755
    TBR=alexeypa@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10828203
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150494 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3387e48402e3f2cd4e322ae1bd21d7ece3ce55f1

--- remoting/host/win/elevated_controller.h
+++ remoting/host/win/elevated_controller.h
@@ -16,10 +16,9 @@
 
 namespace remoting {
 
-class ATL_NO_VTABLE __declspec(uuid(DAEMON_CONTROLLER_CLSID)) ElevatedController
+class ATL_NO_VTABLE ElevatedController
     : public ATL::CComObjectRootEx<ATL::CComSingleThreadModel>,
-      public ATL::CComCoClass<ElevatedController,
-                              &__uuidof(ElevatedController)>,
+      public ATL::CComCoClass<ElevatedController, &CLSID_ElevatedController>,
       public ATL::IDispatchImpl<IDaemonControl2, &IID_IDaemonControl2,
                                 &LIBID_ChromotingElevatedControllerLib, 1, 1> {
  public:
@@ -58,7 +57,7 @@ class ATL_NO_VTABLE __declspec(uuid(DAEMON_CONTROLLER_CLSID)) ElevatedController
   DECLARE_PROTECT_FINAL_CONSTRUCT()
 };
 
-OBJECT_ENTRY_AUTO(__uuidof(ElevatedController), ElevatedController)
+OBJECT_ENTRY_AUTO(CLSID_ElevatedController, ElevatedController)
 
 } // namespace remoting
 
