257d8dcb3f00d2aa0010555932974be6fe8ee392
harrym@chromium.org <harrym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 03:35:25 +0000

    A unit test that creates a situation that crashed (bug 130306) verifying that ash_shell terminates successfully when a window watcher is attached.
    BUG=130332
    
    
    Review URL: https://chromiumcodereview.appspot.com/10834185
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150709 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 246c2e8819ea0c1666ced28de846c749a495c364

---
+++ ash/shell/window_watcher_unittest.cc
@@ -0,0 +1,35 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/shell/window_watcher.h"
+
+#include "ash/shell.h"
+#include "ash/shell_delegate.h"
+#include "ash/shell/shell_delegate_impl.h"
+#include "ash/test/ash_test_base.h"
+#include "ui/aura/root_window.h"
+
+namespace ash {
+
+typedef test::AshTestBase WindowWatcherTest;
+
+// This test verifies that shell can be torn down without causing failures
+// bug http://code.google.com/p/chromium/issues/detail?id=130332
+TEST_F(WindowWatcherTest, ShellDeleteInstance) {
+  scoped_ptr<ash::shell::WindowWatcher> window_watcher;
+  Shell::DeleteInstance();
+
+  shell::ShellDelegateImpl* delegate = new ash::shell::ShellDelegateImpl;
+  Shell::CreateInstance(delegate);
+
+  Shell::GetPrimaryRootWindow()->ShowRootWindow();
+  window_watcher.reset(new ash::shell::WindowWatcher);
+
+  delegate->SetWatcher(window_watcher.get());
+  Shell::GetPrimaryRootWindow()->Hide();
+  window_watcher.reset();
+  delegate->SetWatcher(NULL);
+}
+
+}  // namespace ash
