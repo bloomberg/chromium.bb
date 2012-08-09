0c46e715e6b0ac45348164dce66d3e3bab94a349
iannucci@chromium.org <iannucci@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2012-08-09 19:36:02 +0000

    Fix usage of shlex in unit tests on windows.
    When shlex was getting an arg like:
      r'"some\interesting\path.file" other_file'
    
    shlex was turning it into:
      ['someinterestingpath.file', 'other_file']
    
    Which _happened_ to work, putting 'someinterestingpath.file' in the working
    directory.
    
    This patch doubles up all backslashes prior to splitting, to make the incoming
    string conform to shlex's expectations
    
    R=scottmg
    Review URL: https://chromiumcodereview.appspot.com/10829252
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1465 78cadc50-ecff-11dd-a971-7dbc132099af

--- test/same-rule-output-file-name/src/touch.py
+++ test/same-rule-output-file-name/src/touch.py
@@ -4,11 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import shlex
 import sys
-
-# Trim quotes
-sys.argv[1], = shlex.split(sys.argv[1])
 
 f = open(sys.argv[1], 'w+')
 f.write('Hello from touch.py\n')
