dc61669f7d685bc168f2b633349f1ac321ef094d
mcgrathr@chromium.org <mcgrathr@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-08-10 22:09:40 +0000

    Extend fpu_control_word test for ARM
    The analogous issue affects ARM's fpscr register.  Make the test cover
    that.  The test is expected to fail pending a TCB fix.
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2955
    TEST= trybots
    
    Review URL: https://chromiumcodereview.appspot.com/10828240
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@9442 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: f5630b72d504c8263b080d0adb3c586869de38cb

--- tests/infoleak/test_fpu_control_word.c
+++ tests/infoleak/test_fpu_control_word.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Copyright (c) 2012 The Native Client Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -8,6 +8,10 @@
 #include <sched.h>
 #include <stdint.h>
 #include <stdio.h>
+
+#if defined(__i386__) || defined(__x86_64__)
+
+typedef uint16_t fpcw_t;
 
 #define X86_FPCW_PREC_MASK      0x300
 #define X86_FPCW_PREC_DOUBLE    0x200
@@ -23,16 +27,46 @@ static void set_fpcw(uint16_t fpcw) {
   __asm__  __volatile__("fldcw %0": : "m" (fpcw));
 }
 
+static fpcw_t modify_fpcw_bits(fpcw_t orig_fpcw) {
+  assert(orig_fpcw & X86_FPCW_PREC_EXTENDED);
+  return (orig_fpcw & ~X86_FPCW_PREC_MASK) | X86_FPCW_PREC_DOUBLE;
+}
+
+#elif defined(__arm__)
+
+typedef uint32_t fpcw_t;
+
+#define ARM_FPSCR_ROUND_MASK            0xc00000
+#define ARM_FPSCR_ROUND_TONEAREST       0x000000
+#define ARM_FPSCR_ROUND_UPWARD          0x800000
+#define ARM_FPSCR_ROUND_DOWNWARD        0x400000
+#define ARM_FPSCR_ROUND_TOWARDZERO      0xc00000
+
+/*
+ * These are defined in test_fpu_control_word_arm.S because
+ * PNaCl/LLVM cannot handle the inline assembly for these instructions.
+ */
+fpcw_t get_fpcw(void);
+void set_fpcw(fpcw_t);
+
+static fpcw_t modify_fpcw_bits(fpcw_t orig_fpcw) {
+  assert((orig_fpcw & ARM_FPSCR_ROUND_MASK) == ARM_FPSCR_ROUND_TONEAREST);
+  return (orig_fpcw & ~ARM_FPSCR_ROUND_MASK) | ARM_FPSCR_ROUND_TOWARDZERO;
+}
+
+#else
+#error unsupported architecture
+#endif
+
 int main(void) {
-  uint16_t fpcw1 = 0;
-  uint16_t fpcw2 = 0;
+  fpcw_t fpcw1;
+  fpcw_t fpcw2;
 
   /*
    * Change the FPU control word to a non-default setting.
    */
   fpcw1 = get_fpcw();
-  assert(fpcw1 & X86_FPCW_PREC_EXTENDED);
-  fpcw1 = (fpcw1 & ~X86_FPCW_PREC_MASK) | X86_FPCW_PREC_DOUBLE;
+  fpcw1 = modify_fpcw_bits(fpcw1);
   set_fpcw(fpcw1);
 
   /*
@@ -46,7 +80,8 @@ int main(void) {
    */
   fpcw2 = get_fpcw();
   if (fpcw2 != fpcw1) {
-    fprintf(stderr, "Set FPCW to %hx and got back %hx\n", fpcw1, fpcw2);
+    fprintf(stderr, "Set FPCW to %#x and got back %#x\n",
+            (unsigned int) fpcw1, (unsigned int) fpcw2);
     return 1;
   }
 
