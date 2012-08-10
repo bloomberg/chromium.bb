efe726da0b4a12718b2b28d795706151f80b6087
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-10 22:13:00 +0000

    Fix static initialization in CPM Metric Details
    BUG=130212
    
    
    Review URL: https://chromiumcodereview.appspot.com/10855109
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151136 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f78087aa2c43bb7298d027078b7c353b77ad6e98

--- chrome/browser/performance_monitor/constants.h
+++ chrome/browser/performance_monitor/constants.h
@@ -19,37 +19,6 @@ extern const int kGatherIntervalInMinutes;
 extern const char kStateChromeVersion[];
 extern const char kStateProfilePrefix[];
 
-// Metric details
-extern const char kMetricCPUUsageName[];
-extern const char kMetricCPUUsageDescription[];
-extern const char kMetricCPUUsageUnits[];
-extern const double kMetricCPUUsageTickSize;
-
-extern const char kMetricPrivateMemoryUsageName[];
-extern const char kMetricPrivateMemoryUsageDescription[];
-extern const char kMetricPrivateMemoryUsageUnits[];
-extern const double kMetricPrivateMemoryUsageTickSize;
-
-extern const char kMetricSharedMemoryUsageName[];
-extern const char kMetricSharedMemoryUsageDescription[];
-extern const char kMetricSharedMemoryUsageUnits[];
-extern const double kMetricSharedMemoryUsageTickSize;
-
-extern const char kMetricStartupTimeName[];
-extern const char kMetricStartupTimeDescription[];
-extern const char kMetricStartupTimeUnits[];
-extern const double kMetricStartupTimeTickSize;
-
-extern const char kMetricTestStartupTimeName[];
-extern const char kMetricTestStartupTimeDescription[];
-extern const char kMetricTestStartupTimeUnits[];
-extern const double kMetricTestStartupTimeTickSize;
-
-extern const char kMetricSessionRestoreTimeName[];
-extern const char kMetricSessionRestoreTimeDescription[];
-extern const char kMetricSessionRestoreTimeUnits[];
-extern const double kMetricSessionRestoreTimeTickSize;
-
 }  // namespace performance_monitor
 
 #endif  // CHROME_BROWSER_PERFORMANCE_MONITOR_CONSTANTS_H_
--- chrome/browser/performance_monitor/metric_details.cc
+++ chrome/browser/performance_monitor/metric_details.cc
@@ -10,6 +10,54 @@
 namespace performance_monitor {
 namespace {
 
+// Metric details follow.
+// All metric details have the following constants:
+// - Name
+// - Description
+// - Units
+// - TickSize (the smallest possible maximum which will be viewed in the ui.)
+
+// CPU Usage
+const char kMetricCPUUsageName[] = "CPU Usage";
+const char kMetricCPUUsageDescription[] = "The CPU usage measured in percent.";
+const char kMetricCPUUsageUnits[] = "percent";
+const double kMetricCPUUsageTickSize = 100.0;
+
+// Private Memory Usage
+const char kMetricPrivateMemoryUsageName[] = "Private Memory Usage";
+const char kMetricPrivateMemoryUsageDescription[] =
+    "The total private memory usage of all chrome processes measured in bytes.";
+const char kMetricPrivateMemoryUsageUnits[] = "bytes";
+const double kMetricPrivateMemoryUsageTickSize = 10000000.0;
+
+// Shared Memory Usage
+const char kMetricSharedMemoryUsageName[] = "Shared Memory Usage";
+const char kMetricSharedMemoryUsageDescription[] =
+    "The total shared memory usage of all chrome processes measured in bytes.";
+const char kMetricSharedMemoryUsageUnits[] = "bytes";
+const double kMetricSharedMemoryUsageTickSize = 10000000.0;
+
+// Startup Time
+const char kMetricStartupTimeName[] = "Startup Time";
+const char kMetricStartupTimeDescription[] =
+    "The startup time measured in microseconds";
+const char kMetricStartupTimeUnits[] = "microseconds";
+const double kMetricStartupTimeTickSize = 5000000;
+
+// Test Startup Time
+const char kMetricTestStartupTimeName[] = "Test Startup Time";
+const char kMetricTestStartupTimeDescription[] =
+    "The startup time of test startups measured in microseconds";
+const char kMetricTestStartupTimeUnits[] = "microseconds";
+const double kMetricTestStartupTimeTickSize = 5000000;
+
+// Session Restore Time
+const char kMetricSessionRestoreTimeName[] = "Session Restore Time";
+const char kMetricSessionRestoreTimeDescription[] =
+    "The session restore time measured in microseconds";
+const char kMetricSessionRestoreTimeUnits[] = "microseconds";
+const double kMetricSessionRestoreTimeTickSize = 5000000;
+
 // Keep this array synced with MetricTypes in the header file.
 // TODO(mtytel): i18n.
 const MetricDetails kMetricDetailsList[] = {
