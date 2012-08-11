b57694ca0e9dc7d2845ce9fb0ac9c4b0ebc940ce
Christopher Michael <cpmichael1@comcast.net>
2012-08-11 15:09:02 +0100

    Add support for X cursor themes.
    This patch adds a few more directories to search for xcursor themes.
    
    Along with the weston patch, this adds the ability to configure weston
    to use an X11 cursor theme. Previously, wayland cursor would just look
    in the icons and pixmaps directories for cursor images to load. This
    adds the ability to also search in the x cursors directory.

--- cursor/xcursor.c
+++ cursor/xcursor.c
@@ -606,7 +606,7 @@ XcursorFileLoadImages (FILE *file, int size)
 #endif
 
 #ifndef XCURSORPATH
-#define XCURSORPATH "~/.icons:/usr/share/icons:/usr/share/pixmaps:"ICONDIR
+#define XCURSORPATH "~/.icons:/usr/share/icons:/usr/share/pixmaps:~/.cursors:/usr/share/cursors/xorg-x11:"ICONDIR
 #endif
 
 static const char *
