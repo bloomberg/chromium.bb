8b721e821744d9b83ce5bf4fbd84af157abc258f
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 12:53:02 +0000

    Move AutofillExternalDelegateGtk to chrome/browser/ui/gtk/autofill, where it belongs.
    The class implementation is all to do with UI, and has dependencies on
    GTK UI code, so this seems a more appropriate home for it.  Moving it
    to its rightful home also removes a couple of unwanted includes from
    chrome/browser/autofill/DEPS.
    
    TBR=ben@chromium.org
    BUG=140037
    
    Review URL: https://chromiumcodereview.appspot.com/10828259
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151467 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2279a33481b362478863c62d60ff84e1c7544e78

--- chrome/browser/ui/gtk/autofill/autofill_external_delegate_gtk.cc
+++ chrome/browser/ui/gtk/autofill/autofill_external_delegate_gtk.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/autofill/autofill_external_delegate_gtk.h"
+#include "chrome/browser/ui/gtk/autofill/autofill_external_delegate_gtk.h"
 
 #include "chrome/browser/ui/gtk/autofill/autofill_popup_view_gtk.h"
 #include "chrome/browser/ui/gtk/gtk_theme_service.h"
--- chrome/browser/ui/gtk/autofill/autofill_external_delegate_gtk.h
+++ chrome/browser/ui/gtk/autofill/autofill_external_delegate_gtk.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_AUTOFILL_AUTOFILL_EXTERNAL_DELEGATE_GTK_H_
-#define CHROME_BROWSER_AUTOFILL_AUTOFILL_EXTERNAL_DELEGATE_GTK_H_
+#ifndef CHROME_BROWSER_UI_GTK_AUTOFILL_AUTOFILL_EXTERNAL_DELEGATE_GTK_H_
+#define CHROME_BROWSER_UI_GTK_AUTOFILL_AUTOFILL_EXTERNAL_DELEGATE_GTK_H_
 
 #include <gtk/gtk.h>
 
@@ -60,4 +60,4 @@ class AutofillExternalDelegateGtk : public AutofillExternalDelegate {
   DISALLOW_COPY_AND_ASSIGN(AutofillExternalDelegateGtk);
 };
 
-#endif  // CHROME_BROWSER_AUTOFILL_AUTOFILL_EXTERNAL_DELEGATE_GTK_H_
+#endif  // CHROME_BROWSER_UI_GTK_AUTOFILL_AUTOFILL_EXTERNAL_DELEGATE_GTK_H_
