1472ceed603dd792bb4d418cc0ad4335baa7d5e5
jhorwich@chromium.org <jhorwich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-15 19:09:40 +0000

    Revert 151717 - Initial checkin of the me2me pyauto automation - Modified chromoting.py to enable me2me automation - Added chromoting_helper.py to handle install, uninstall, enable, disable, changepin which requires admin privilege - Added chromoting.base.py as the base for all chromoting test cases - Modified chromoting_basic, renamed it it2me_basic and moved it under chromoting dir - Added auth, me2me_enable, me2me_connect test - Added a cert and a private key for signing host on mac - Added mock_pref_pane.* files to mock the pref pane for different scenarios
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/10821015
    
    TBR=yihongg@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10837266
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151722 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9c621c92cb46f0d9bd56e0bca9102a8a46994fd8

---
+++ chrome/test/functional/chromoting_basic.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+
+import pyauto_functional  # Must come before chromoting and pyauto.
+import chromoting
+import pyauto
+
+
+class ChromotingBasic(chromoting.ChromotingMixIn, pyauto.PyUITest):
+  """Basic tests for Chromoting."""
+
+  _EXTRA_CHROME_FLAGS = [
+    '--allow-nacl-socket-api=*',
+  ]
+
+  def ExtraChromeFlags(self):
+    """Ensures Chrome is launched with some custom flags.
+
+    Overrides the default list of extra flags passed to Chrome.  See
+    ExtraChromeFlags() in pyauto.py.
+    """
+    return pyauto.PyUITest.ExtraChromeFlags(self) + self._EXTRA_CHROME_FLAGS
+
+  def setUp(self):
+    """Set up test for Chromoting on both local and remote machines.
+
+    Installs the Chromoting app, launches it, and authenticates
+    using the default Chromoting test account.
+    """
+    super(ChromotingBasic, self).setUp()
+    self._app = self.InstallExtension(self.GetWebappPath())
+    self.LaunchApp(self._app)
+    account = self.GetPrivateInfo()['test_chromoting_account']
+    self.Authenticate(account['username'], account['password'])
+
+  def testChromoting(self):
+    """Verify that we can start and disconnect from a Chromoting session."""
+    client_local = (self.remote == None)
+    host = self
+    client = self if client_local else self.remote
+    client_tab_index = 2 if client_local else 1
+
+    access_code = host.Share()
+    self.assertTrue(access_code,
+                    msg='Host attempted to share, but it failed. '
+                        'No access code was found.')
+
+    if client_local:
+      client.LaunchApp(self._app)
+
+    self.assertTrue(client.Connect(access_code, True, client_tab_index),
+                    msg='The client attempted to connect to the host, '
+                        'but the chromoting session did not start.')
+
+    host.CancelShare()
+    client.Disconnect(client_tab_index)
+
+
+if __name__ == '__main__':
+  pyauto_functional.Main()
