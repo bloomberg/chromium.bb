3faebd62534c02780eb41106c9a87573c3cfa6e4
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-15 23:38:08 +0000

    [Sync] Re-enable error uploading for search engines.
    r151391 hopefully fixed most sources of errors, so we're re-enabling to better
    track any remaining issues.
    
    BUG=135196,130448
    
    
    Review URL: https://chromiumcodereview.appspot.com/10824288
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151788 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ddeea47bab6bb72dfe155fb508a6b8d3d98c0b88

--- chrome/browser/sync/glue/data_type_controller.cc
+++ chrome/browser/sync/glue/data_type_controller.cc
@@ -17,10 +17,7 @@ syncer::SyncError DataTypeController::CreateAndUploadError(
     const tracked_objects::Location& location,
     const std::string& message,
     syncer::ModelType type) {
-  // TODO(sync): remove this once search engines triggers less errors, such as
-  // due to crbug.com/130448.
-  if (type != syncer::SEARCH_ENGINES)
-    ChromeReportUnrecoverableError();
+  ChromeReportUnrecoverableError();
   return syncer::SyncError(location, message, type);
 }
 
