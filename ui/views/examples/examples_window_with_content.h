3acef897d22dd9ed9b2ddb39766b4e4c2793b6ef
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-16 01:19:14 +0000

    views/examples: Copy examples_window.* into examples_window_with_content.*
    They will be used in views_examples_with_content_lib, so later we can have
    a views_examples_lib with examples_window.* that does not include anything from
    content/ module.
    
    BUG=141187
    R=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10827309
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151817 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9cd76c4b0d1b843275b2d098c1b6f1126be1817d

---
+++ ui/views/examples/examples_window_with_content.h
@@ -0,0 +1,27 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_H_
+#define UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_H_
+
+namespace content {
+class BrowserContext;
+}
+
+namespace views {
+namespace examples {
+
+enum Operation {
+  DO_NOTHING_ON_CLOSE = 0,
+  QUIT_ON_CLOSE,
+};
+
+// Shows a window with the views examples in it.
+void ShowExamplesWindow(Operation operation,
+                        content::BrowserContext* browser_context);
+
+}  // namespace examples
+}  // namespace views
+
+#endif  // UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_H_
