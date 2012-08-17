10442b528f97ba9bb2bc282f9610dc0fe6c79adc
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-17 20:41:36 +0000

    Disable flaky OffscreenTabsApiTest.OffscreenTabMouseEvents for Win and Linux
    BUG=142993
    
    
    Review URL: https://chromiumcodereview.appspot.com/10836277
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152147 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 48280439bdbf6ab359ad38c45c94924ab9a9785f

--- chrome/browser/extensions/api/offscreen_tabs/offscreen_tabs_apitest.cc
+++ chrome/browser/extensions/api/offscreen_tabs/offscreen_tabs_apitest.cc
@@ -22,7 +22,13 @@ IN_PROC_BROWSER_TEST_F(OffscreenTabsApiTest, DISABLED_OffscreenTabBasics) {
 }
 
 
-IN_PROC_BROWSER_TEST_F(OffscreenTabsApiTest, OffscreenTabMouseEvents) {
+#if defined (OS_WIN) || defined (OS_LINUX)
+// http://crbug.com/142993 - this times out flakily on Win/Linux
+#define MAYBE_OffscreenTabMouseEvents DISABLED_OffscreenTabMouseEvents
+#else
+#define MAYBE_OffscreenTabMouseEvents OffscreenTabMouseEvents
+#endif
+IN_PROC_BROWSER_TEST_F(OffscreenTabsApiTest, MAYBE_OffscreenTabMouseEvents) {
   ASSERT_TRUE(RunExtensionSubtest("offscreen_tabs",
                                   "mouse_events.html")) << message_;
 }
