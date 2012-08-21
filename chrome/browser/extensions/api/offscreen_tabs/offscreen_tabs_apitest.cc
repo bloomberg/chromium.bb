79fddb171c535845527b815734f2a7d49da43169
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 02:49:36 +0000

    Disable flaky OffscreenTabsApiTest.OffscreenTabKeyEvents for Win and Linux
    BUG=142993
    TBR=jstritar
    
    
    Review URL: https://chromiumcodereview.appspot.com/10861021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152493 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e8c4612dd7454464fac243645d1f6d01ca9596ff

--- chrome/browser/extensions/api/offscreen_tabs/offscreen_tabs_apitest.cc
+++ chrome/browser/extensions/api/offscreen_tabs/offscreen_tabs_apitest.cc
@@ -25,15 +25,17 @@ IN_PROC_BROWSER_TEST_F(OffscreenTabsApiTest, DISABLED_OffscreenTabBasics) {
 #if defined (OS_WIN) || defined (OS_LINUX)
 // http://crbug.com/142993 - this times out flakily on Win/Linux
 #define MAYBE_OffscreenTabMouseEvents DISABLED_OffscreenTabMouseEvents
+#define MAYBE_OffscreenTabKeyboardEvents DISABLED_OffscreenTabKeyboardEvents
 #else
 #define MAYBE_OffscreenTabMouseEvents OffscreenTabMouseEvents
+#define MAYBE_OffscreenTabKeyboardEvents OffscreenTabKeyboardEvents
 #endif
 IN_PROC_BROWSER_TEST_F(OffscreenTabsApiTest, MAYBE_OffscreenTabMouseEvents) {
   ASSERT_TRUE(RunExtensionSubtest("offscreen_tabs",
                                   "mouse_events.html")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(OffscreenTabsApiTest, OffscreenTabKeyboardEvents) {
+IN_PROC_BROWSER_TEST_F(OffscreenTabsApiTest, MAYBE_OffscreenTabKeyboardEvents) {
   ASSERT_TRUE(RunExtensionSubtest("offscreen_tabs",
                                   "keyboard_events.html")) << message_;
 }
