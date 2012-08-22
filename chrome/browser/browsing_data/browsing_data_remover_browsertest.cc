e7f17843e02ca75f09392479eb47daa1f39b08c0
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-22 20:52:07 +0000

    Disable the right test
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152814 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ac96f4d24b68152a2918178b38d7945cc6ffda51

--- chrome/browser/browsing_data/browsing_data_remover_browsertest.cc
+++ chrome/browser/browsing_data/browsing_data_remover_browsertest.cc
@@ -42,7 +42,7 @@ class BrowsingDataRemoverBrowserTest : public InProcessBrowserTest {
     signal.Wait();
   }
 };
-
+#if !defined(OS_LINUX)
 // Test BrowsingDataRemover for downloads.
 IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest, Download) {
   // Start a download.
@@ -69,7 +69,7 @@ IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest, Download) {
   download_manager->GetAllDownloads(FilePath(), &downloads);
   EXPECT_TRUE(downloads.empty());
 }
-#if !defined(OS_LINUX)
+#endif
 // Verify can modify database after deleting it.
 IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest, Database) {
   ASSERT_TRUE(test_server()->Start());
@@ -87,4 +87,3 @@ IN_PROC_BROWSER_TEST_F(BrowsingDataRemoverBrowserTest, Database) {
   RunScriptAndCheckResult(L"insertRecord('text2')", "done");
   RunScriptAndCheckResult(L"getRecords()", "text2");
 }
-#endif
