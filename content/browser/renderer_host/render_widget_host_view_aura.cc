cb6189a1285752af7ff75a52029d141cd46c1799
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 17:21:04 +0000

    Fix RenderWidgetHostViewAura::AcceleratedSurfacePostSubBuffer() so that it computes |rect_to_paint| correctly.
    BUG=139208
    TEST=manually tested on Link
    
    Review URL: https://chromiumcodereview.appspot.com/10872027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153021 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6dbc8adea5745cca2c94e343db1831c20423e1fc

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -720,7 +720,7 @@ void RenderWidgetHostViewAura::AcceleratedSurfacePostSubBuffer(
     // Damage may not have been DIP aligned, so inflate damage to compensate
     // for any round-off error.
     rect_to_paint.Inset(-1, -1);
-    rect_to_paint.Intersect(window_->bounds());
+    rect_to_paint = rect_to_paint.Intersect(window_->bounds());
 
     window_->SchedulePaintInRect(rect_to_paint);
 
