d1a87cd880d3f8b1a0d676b0db956c1ffe060bdf
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-27 22:27:23 +0000

    Remove DownloadFileManager in favor of direct ownership of DownloadFiles.
    This CL is equivalent to CLs http://codereview.chromium.org/10799005, 
    http://codereview.chromium.org/10836293, and
    https://chromiumcodereview.appspot.com/10823406, which were previous attempts 
    to land the same functionality.  
    
    TBR=jam@chromium.org
    
    BUG=123998
    BUG=144751
    
    
    Review URL: https://chromiumcodereview.appspot.com/10867065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153563 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7da0c2b783a69aa9ee4b0c914a1ccfb2965adcad

--- chrome/browser/download/save_page_browsertest.cc
+++ chrome/browser/download/save_page_browsertest.cc
@@ -64,6 +64,9 @@ static const char* kAppendedExtension =
     ".html";
 #endif
 
+void NullFunction() {
+}
+
 }  // namespace
 
 // Loosely based on logic in DownloadTestObserver.
@@ -101,7 +104,8 @@ class DownloadItemCreatedObserver : public DownloadManager::Observer {
  private:
 
   // DownloadManager::Observer
-  void OnDownloadCreated(DownloadManager* manager, DownloadItem* item) {
+  virtual void OnDownloadCreated(
+      DownloadManager* manager, DownloadItem* item) OVERRIDE {
     DCHECK_EQ(manager, manager_);
     if (!created_item_)
       created_item_ = item;
@@ -110,7 +114,7 @@ class DownloadItemCreatedObserver : public DownloadManager::Observer {
       MessageLoopForUI::current()->Quit();
   }
 
-  void ManagerGoingDownload(DownloadManager* manager) {
+  virtual void ManagerGoingDown(DownloadManager* manager) OVERRIDE {
     manager_->RemoveObserver(this);
     manager_ = NULL;
     if (waiting_)
@@ -122,6 +126,60 @@ class DownloadItemCreatedObserver : public DownloadManager::Observer {
   DownloadItem* created_item_;
 
   DISALLOW_COPY_AND_ASSIGN(DownloadItemCreatedObserver);
+};
+
+class DownloadPersistedObserver : public DownloadItem::Observer {
+ public:
+  explicit DownloadPersistedObserver(DownloadItem* item)
+      : waiting_(false), item_(item) {
+    item->AddObserver(this);
+  }
+
+  ~DownloadPersistedObserver() {
+    if (item_)
+      item_->RemoveObserver(this);
+  }
+
+  // Wait for download item to get the persisted bit set.
+  // Note that this class provides no protection against the download
+  // being destroyed between creation and return of WaitForPersisted();
+  // the caller must guarantee that in some other fashion.
+  void WaitForPersisted() {
+    // In combination with OnDownloadDestroyed() below, verify the
+    // above interface contract.
+    DCHECK(item_);
+
+    if (item_->IsPersisted())
+      return;
+
+    waiting_ = true;
+    content::RunMessageLoop();
+    waiting_ = false;
+
+    return;
+  }
+
+ private:
+  // DownloadItem::Observer
+  virtual void OnDownloadUpdated(DownloadItem* item) OVERRIDE {
+    DCHECK_EQ(item, item_);
+
+    if (waiting_ && item->IsPersisted())
+      MessageLoopForUI::current()->Quit();
+  }
+
+  virtual void OnDownloadDestroyed(DownloadItem* item) OVERRIDE {
+    if (item != item_)
+      return;
+
+    item_->RemoveObserver(this);
+    item_ = NULL;
+  }
+
+  bool waiting_;
+  DownloadItem* item_;
+
+  DISALLOW_COPY_AND_ASSIGN(DownloadPersistedObserver);
 };
 
 class SavePageBrowserTest : public InProcessBrowserTest {
@@ -165,12 +223,12 @@ class SavePageBrowserTest : public InProcessBrowserTest {
     return current_tab;
   }
 
-
   GURL WaitForSavePackageToFinish() const {
     content::WindowedNotificationObserver observer(
         content::NOTIFICATION_SAVE_PACKAGE_SUCCESSFULLY_FINISHED,
         content::NotificationService::AllSources());
     observer.Wait();
+
     return content::Details<DownloadItem>(observer.details()).ptr()->
         GetOriginalUrl();
   }
@@ -209,11 +267,12 @@ class SavePageBrowserTest : public InProcessBrowserTest {
 
     DownloadPersistentStoreInfoMatch(const GURL& url,
                                      const FilePath& path,
-                                     int64 num_files)
+                                     int64 num_files,
+                                     DownloadItem::DownloadState state)
       : url_(url),
         path_(path),
-        num_files_(num_files) {
-    }
+        num_files_(num_files),
+        state_(state) {}
 
     bool operator() (const DownloadPersistentStoreInfo& info) const {
       return info.url == url_ &&
@@ -223,22 +282,37 @@ class SavePageBrowserTest : public InProcessBrowserTest {
              ((num_files_ < 0) ||
               (info.received_bytes == num_files_)) &&
              info.total_bytes == 0 &&
-             info.state == DownloadItem::COMPLETE;
+             info.state == state_;
     }
 
     GURL url_;
     FilePath path_;
     int64 num_files_;
+    DownloadItem::DownloadState state_;
   };
 
   void CheckDownloadHistory(const GURL& url,
                             const FilePath& path,
-                            int64 num_files) {
+                            int64 num_files,
+                            DownloadItem::DownloadState state) {
+    // Make sure the relevant download item made it into the history.
+    std::vector<DownloadItem*> downloads;
+    GetDownloadManager()->SearchDownloads(string16(), &downloads);
+    ASSERT_EQ(1u, downloads.size());
+    DownloadPersistedObserver(downloads[0]).WaitForPersisted();
+
+    // Make sure any final updates have made it to the history DB.
+    BrowserThread::PostTaskAndReply(
+        BrowserThread::DB, FROM_HERE, base::Bind(&NullFunction),
+        MessageLoop::QuitClosure());
+    content::RunMessageLoop();
+
     QueryDownloadHistory();
 
     std::vector<DownloadPersistentStoreInfo>::iterator found =
       std::find_if(history_entries_.begin(), history_entries_.end(),
-                   DownloadPersistentStoreInfoMatch(url, path, num_files));
+                   DownloadPersistentStoreInfoMatch(url, path, num_files,
+                                                    state));
 
     if (found == history_entries_.end()) {
       LOG(ERROR) << "Missing url=" << url.spec()
@@ -282,7 +356,8 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveHTMLOnly) {
   EXPECT_EQ(url, WaitForSavePackageToFinish());
 
   EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
-  CheckDownloadHistory(url, full_file_name, 1);  // a.htm is 1 file.
+  // a.htm is 1 file.
+  CheckDownloadHistory(url, full_file_name, 1, DownloadItem::COMPLETE);
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   EXPECT_FALSE(file_util::PathExists(dir));
@@ -310,8 +385,12 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveHTMLOnlyCancel) {
   // Currently it's ignored.
   EXPECT_EQ(url, WaitForSavePackageToFinish());
 
+  // -1 to disable number of files check; we don't update after cancel, and
+  // we don't know when the single file completed in relationship to
+  // the cancel.
+  CheckDownloadHistory(url, full_file_name, -1, DownloadItem::CANCELLED);
+
   EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
-  CheckDownloadHistory(url, full_file_name, 1);  // a.htm is 1 file.
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   EXPECT_FALSE(file_util::PathExists(dir));
@@ -360,7 +439,9 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveViewSourceHTMLOnly) {
   EXPECT_EQ(actual_page_url, WaitForSavePackageToFinish());
 
   EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
-  CheckDownloadHistory(actual_page_url, full_file_name, 1);  // a.htm is 1 file.
+  // a.htm is 1 file.
+  CheckDownloadHistory(actual_page_url, full_file_name, 1,
+                       DownloadItem::COMPLETE);
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   EXPECT_FALSE(file_util::PathExists(dir));
@@ -380,7 +461,8 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, SaveCompleteHTML) {
   EXPECT_EQ(url, WaitForSavePackageToFinish());
 
   EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
-  CheckDownloadHistory(url, full_file_name, 3);  // b.htm is 3 files.
+  // b.htm is 3 files.
+  CheckDownloadHistory(url, full_file_name, 3, DownloadItem::COMPLETE);
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   EXPECT_TRUE(file_util::PathExists(dir));
@@ -413,7 +495,8 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, FileNameFromPageTitle) {
   EXPECT_EQ(url, WaitForSavePackageToFinish());
 
   EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
-  CheckDownloadHistory(url, full_file_name, 3);  // b.htm is 3 files.
+  // b.htm is 3 files.
+  CheckDownloadHistory(url, full_file_name, 3, DownloadItem::COMPLETE);
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   EXPECT_TRUE(file_util::PathExists(dir));
@@ -439,14 +522,16 @@ IN_PROC_BROWSER_TEST_F(SavePageBrowserTest, RemoveFromList) {
   EXPECT_EQ(url, WaitForSavePackageToFinish());
 
   EXPECT_TRUE(browser()->window()->IsDownloadShelfVisible());
-  CheckDownloadHistory(url, full_file_name, 1);  // a.htm is 1 file.
+  // a.htm is 1 file.
+  CheckDownloadHistory(url, full_file_name, 1, DownloadItem::COMPLETE);
 
   EXPECT_EQ(GetDownloadManager()->RemoveAllDownloads(), 1);
 
   // Should not be in history.
   QueryDownloadHistory();
   EXPECT_EQ(std::find_if(history_entries_.begin(), history_entries_.end(),
-      DownloadPersistentStoreInfoMatch(url, full_file_name, 1)),
+                         DownloadPersistentStoreInfoMatch(
+                             url, full_file_name, 1, DownloadItem::COMPLETE)),
       history_entries_.end());
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
@@ -520,7 +605,7 @@ IN_PROC_BROWSER_TEST_F(SavePageAsMHTMLBrowserTest, SavePageAsMHTML) {
         content::NotificationService::AllSources());
   chrome::SavePage(browser());
   observer.Wait();
-  CheckDownloadHistory(url, full_file_name, -1);
+  CheckDownloadHistory(url, full_file_name, -1, DownloadItem::COMPLETE);
 
   EXPECT_TRUE(file_util::PathExists(full_file_name));
   int64 actual_file_size = -1;
--- content/browser/download/download_file_impl.h
+++ content/browser/download/download_file_impl.h
@@ -14,13 +14,14 @@
 #include "base/timer.h"
 #include "content/browser/download/base_file.h"
 #include "content/browser/download/byte_stream.h"
-#include "content/browser/download/download_request_handle.h"
+#include "content/public/browser/download_save_info.h"
 #include "net/base/net_log.h"
 
 struct DownloadCreateInfo;
 
 namespace content {
 class ByteStreamReader;
+class DownloadDestinationObserver;
 class DownloadManager;
 class PowerSaveBlocker;
 }
@@ -29,17 +30,27 @@ class CONTENT_EXPORT DownloadFileImpl : virtual public content::DownloadFile {
  public:
   // Takes ownership of the object pointed to by |request_handle|.
   // |bound_net_log| will be used for logging the download file's events.
-  DownloadFileImpl(const DownloadCreateInfo* info,
-                   scoped_ptr<content::ByteStreamReader> stream,
-                   DownloadRequestHandleInterface* request_handle,
-                   scoped_refptr<content::DownloadManager> download_manager,
-                   bool calculate_hash,
-                   scoped_ptr<content::PowerSaveBlocker> power_save_blocker,
-                   const net::BoundNetLog& bound_net_log);
+  // May be constructed on any thread.  All methods besides the constructor
+  // (including destruction) must occur on the FILE thread.
+  //
+  // Note that the DownloadFileImpl automatically reads from the passed in
+  // stream, and sends updates and status of those reads to the
+  // DownloadDestinationObserver.
+  DownloadFileImpl(
+    const content::DownloadSaveInfo& save_info,
+    const GURL& url,
+    const GURL& referrer_url,
+    int64 received_bytes,
+    bool calculate_hash,
+    scoped_ptr<content::ByteStreamReader> stream,
+    const net::BoundNetLog& bound_net_log,
+    scoped_ptr<content::PowerSaveBlocker> power_save_blocker,
+    base::WeakPtr<content::DownloadDestinationObserver> observer);
+
   virtual ~DownloadFileImpl();
 
   // DownloadFile functions.
-  virtual content::DownloadInterruptReason Initialize() OVERRIDE;
+  virtual void Initialize(const InitializeCallback& callback) OVERRIDE;
   virtual void Rename(const FilePath& full_path,
                       bool overwrite_existing_file,
                       const RenameCompletionCallback& callback) OVERRIDE;
@@ -52,11 +63,6 @@ class CONTENT_EXPORT DownloadFileImpl : virtual public content::DownloadFile {
   virtual int64 CurrentSpeed() const OVERRIDE;
   virtual bool GetHash(std::string* hash) OVERRIDE;
   virtual std::string GetHashState() OVERRIDE;
-  virtual void CancelDownloadRequest() OVERRIDE;
-  virtual int Id() const OVERRIDE;
-  virtual content::DownloadManager* GetDownloadManager() OVERRIDE;
-  virtual const content::DownloadId& GlobalId() const OVERRIDE;
-  virtual std::string DebugString() const OVERRIDE;
 
  protected:
   // For test class overrides.
@@ -80,19 +86,8 @@ class CONTENT_EXPORT DownloadFileImpl : virtual public content::DownloadFile {
   // with DownloadFile and get rid of BaseFile.
   scoped_ptr<content::ByteStreamReader> stream_reader_;
 
-  // The unique identifier for this download, assigned at creation by
-  // the DownloadFileManager for its internal record keeping.
-  content::DownloadId id_;
-
   // Used to trigger progress updates.
   scoped_ptr<base::RepeatingTimer<DownloadFileImpl> > update_timer_;
-
-  // The handle to the request information.  Used for operations outside the
-  // download system, specifically canceling a download.
-  scoped_ptr<DownloadRequestHandleInterface> request_handle_;
-
-  // DownloadManager this download belongs to.
-  scoped_refptr<content::DownloadManager> download_manager_;
 
   // Statistics
   size_t bytes_seen_;
@@ -100,6 +95,8 @@ class CONTENT_EXPORT DownloadFileImpl : virtual public content::DownloadFile {
   base::TimeTicks download_start_;
 
   net::BoundNetLog bound_net_log_;
+
+  base::WeakPtr<content::DownloadDestinationObserver> observer_;
 
   base::WeakPtrFactory<DownloadFileImpl> weak_factory_;
 
