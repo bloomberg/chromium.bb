6706f97443083379a444e8a17ea2ec11410888be
hongbo.min@intel.com <hongbo.min@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 07:00:22 +0000

    Add an example for showing how to use systemInfo API.
    BUG=136519
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/10882059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153637 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3b8a1974aa8cfd5745b20657f867b5e712c1b3c4

---
+++ chrome/common/extensions/docs/examples/api/systemInfo/main.js
@@ -0,0 +1,62 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var systemInfo = chrome.experimental.systemInfo;
+
+var indicator = {}
+var isStarted = false;
+
+function onStorageChanged(info) {
+  var elem = document.getElementById(info.id);
+  if (indicator[info.id]++ % 2)
+    elem.bgColor = "green";
+  else
+    elem.bgColor = "white";
+  elem.innerHTML = info.availableCapacity;
+}
+
+function startMonitor() {
+  if (isStarted) return;
+  systemInfo.storage.onAvailableCapacityChanged.addListener(onStorageChanged);
+  isStarted = true;
+}
+
+function stopMonitor() {
+  if (!isStarted) return;
+  systemInfo.storage.onAvailableCapacityChanged.removeListener(
+      onStorageChanged);
+  isStarted = false;
+}
+
+function init() {
+  document.getElementById("start-btn").onclick = startMonitor;
+  document.getElementById("stop-btn").onclick = stopMonitor;
+
+  chrome.experimental.systemInfo.storage.get(function(info) {
+    var table = "<table width=65% border=\"1\">\n" +
+      "<tr><td><b>ID</b></td>" +
+      "<td>Type</td>" +
+      "<td>Capacity (bytes)</td>" +
+      "<td>Available (bytes)</td>" +
+      "</tr>\n";
+    for (var i = 0; i < info.units.length; i++) {
+      indicator[info.units[i].id] = 0;
+      table += showStorageInfo(info.units[i]);
+    }
+    table += "</table>\n";
+    var div = document.getElementById("storage-list");
+    div.innerHTML = table;
+  });
+}
+
+function showStorageInfo(unit) {
+  table = "<tr><td>" + unit.id + "</td>" +
+    "<td>" + unit.type + "</td>" +
+    "<td>" + unit.capacity + "</td>" +
+    "<td id=" + "\"" + unit.id + "\">" + unit.availableCapacity + "</td>" +
+    "</tr>\n";
+  return table;
+}
+
+document.addEventListener('DOMContentLoaded', init);
