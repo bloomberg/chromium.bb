cff90b909695e12ebe4767f9ee150d413badd0e6
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 01:30:40 +0000

    No more extension install infobars (on Mac).
    BUG=109940
    TEST=On mac, install https://chrome.google.com/webstore/detail/dmgcpfplbobdchnijhbmogkbibagiiam and see a bubble pointing to the wrench menu instead of an infobar.
    
    Review URL: https://chromiumcodereview.appspot.com/10872067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153602 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4106828279eaf917ff6d4d5f8545227615e62b72

--- chrome/browser/ui/cocoa/extensions/extension_installed_bubble_controller.mm
+++ chrome/browser/ui/cocoa/extensions/extension_installed_bubble_controller.mm
@@ -113,7 +113,7 @@ class ExtensionLoadedNotificationObserver
                !extension->page_action()->default_icon_path().empty()) {
       type_ = extension_installed_bubble::kPageAction;
     } else {
-      NOTREACHED();  // kGeneric installs handled in extension_install_prompt.
+      type_ = extension_installed_bubble::kGeneric;
     }
 
     if (type_ == extension_installed_bubble::kBundle) {
@@ -207,16 +207,18 @@ class ExtensionLoadedNotificationObserver
           locationBarView->GetPageActionBubblePoint(extension_->page_action());
       break;
     }
-    case extension_installed_bubble::kBundle: {
+    case extension_installed_bubble::kBundle:
+    case extension_installed_bubble::kGeneric: {
+      // Point at the bottom of the wrench menu.
       NSView* wrenchButton =
           [[window->cocoa_controller() toolbarController] wrenchButton];
       const NSRect bounds = [wrenchButton bounds];
-      NSPoint anchor = NSMakePoint(NSMidX(bounds), NSMidY(bounds));
+      NSPoint anchor = NSMakePoint(NSMidX(bounds), NSMaxY(bounds));
       arrowPoint = [wrenchButton convertPoint:anchor toView:nil];
       break;
     }
     default: {
-      NOTREACHED() << "Generic extension type not allowed in install bubble.";
+      NOTREACHED();
     }
   }
   return arrowPoint;
@@ -225,8 +227,6 @@ class ExtensionLoadedNotificationObserver
 // Override -[BaseBubbleController showWindow:] to tweak bubble location and
 // set up UI elements.
 - (void)showWindow:(id)sender {
-  // Generic extensions get an infobar rather than a bubble.
-  DCHECK(type_ != extension_installed_bubble::kGeneric);
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
   // Load nib and calculate height based on messages to be shown.
