187760e48008b265cd451528e32260f881205b33
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-29 03:59:58 +0000

    Revert 153843 - Fix and expand upon BubbleDelegateTests.
    This was causing BubbleDelegateTest.CloseAnchorWidget to crash on the Win Aura build bot:
    http://build.chromium.org/p/chromium/builders/Win%20Aura/builds/20590/steps/views_unittests/logs/stdio
    
    BubbleDelegateTest.ResetAnchorWidget was previously failing  and crashing because of it's widget ownership pattern and lack of close_on_deactivate.
    
    Add TestWidgetObserver to observe bubbles closing.
    Ensure the bubble widget closes as expected in each test.
    
    BUG=none
    TEST=unit tests pass
    
    Review URL: https://chromiumcodereview.appspot.com/10889014
    
    TBR=msw@chromium.org,ben@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10885026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153848 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6c56fde5bf676be3cee74fc3587163f067a28e04

--- ui/views/bubble/bubble_delegate_unittest.cc
+++ ui/views/bubble/bubble_delegate_unittest.cc
@@ -9,40 +9,6 @@
 
 namespace views {
 
-namespace {
-
-// A Widget observer class used in the tests below to observe bubbles closing.
-class TestWidgetObserver : public WidgetObserver {
- public:
-  explicit TestWidgetObserver(Widget* widget);
-  virtual ~TestWidgetObserver();
-
-  // WidgetObserver overrides:
-  virtual void OnWidgetClosing(Widget* widget) OVERRIDE;
-
-  bool widget_closed() const { return widget_ == NULL; }
-
- private:
-  Widget* widget_;
-};
-
-TestWidgetObserver::TestWidgetObserver(Widget* widget)
-    : widget_(widget) {
-  widget_->AddObserver(this);
-}
-
-TestWidgetObserver::~TestWidgetObserver() {
-  if (widget_)
-    widget_->RemoveObserver(this);
-}
-
-void TestWidgetObserver::OnWidgetClosing(Widget* widget) {
-  DCHECK_EQ(widget_, widget);
-  widget_ = NULL;
-}
-
-}  // namespace
-
 typedef ViewsTestBase BubbleDelegateTest;
 
 TEST_F(BubbleDelegateTest, CreateDelegate) {
@@ -53,8 +19,6 @@ TEST_F(BubbleDelegateTest, CreateDelegate) {
       BubbleDelegateView::CreateBubble(bubble_delegate));
   EXPECT_EQ(bubble_delegate, bubble_widget->widget_delegate());
   EXPECT_EQ(bubble_widget, bubble_delegate->GetWidget());
-  TestWidgetObserver bubble_observer(bubble_widget);
-  EXPECT_FALSE(bubble_observer.widget_closed());
 
   BubbleBorder* border =
       bubble_delegate->GetBubbleFrameView()->bubble_border();
@@ -63,83 +27,52 @@ TEST_F(BubbleDelegateTest, CreateDelegate) {
 
   bubble_widget->CloseNow();
   RunPendingMessages();
-  EXPECT_TRUE(bubble_observer.widget_closed());
-}
-
-TEST_F(BubbleDelegateTest, CloseAnchorWidget) {
-  // Create the anchor widget.
-  Widget::InitParams params(Widget::InitParams::TYPE_WINDOW);
-  params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
-  scoped_ptr<Widget> anchor_widget(new Widget);
-  anchor_widget->Init(params);
-  anchor_widget->Show();
-
-  BubbleDelegateView* bubble_delegate = new BubbleDelegateView(
-      anchor_widget->GetContentsView(), BubbleBorder::NONE);
-  // Preventing close on deactivate should not prevent closing with the anchor.
-  bubble_delegate->set_close_on_deactivate(false);
-  Widget* bubble_widget = BubbleDelegateView::CreateBubble(bubble_delegate);
-  EXPECT_EQ(bubble_delegate, bubble_widget->widget_delegate());
-  EXPECT_EQ(bubble_widget, bubble_delegate->GetWidget());
-  EXPECT_EQ(anchor_widget.get(), bubble_delegate->anchor_widget());
-  TestWidgetObserver bubble_observer(bubble_widget);
-  EXPECT_FALSE(bubble_observer.widget_closed());
-
-  bubble_widget->Show();
-  RunPendingMessages();
-  EXPECT_EQ(anchor_widget.get(), bubble_delegate->anchor_widget());
-  EXPECT_FALSE(bubble_observer.widget_closed());
-
-  // Ensure that closing the anchor widget also closes the bubble itself.
-  anchor_widget->CloseNow();
-  RunPendingMessages();
-  EXPECT_TRUE(bubble_observer.widget_closed());
 }
 
 TEST_F(BubbleDelegateTest, ResetAnchorWidget) {
-  // Create the anchor and parent widgets.
+  // Create the anchor widget first.
+  Widget* widget = new Widget;
+  View* contents = new View;
+
   Widget::InitParams params(Widget::InitParams::TYPE_WINDOW);
-  params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
-  scoped_ptr<Widget> anchor_widget(new Widget);
-  anchor_widget->Init(params);
-  anchor_widget->Show();
-  scoped_ptr<Widget> parent_widget(new Widget);
-  parent_widget->Init(params);
-  parent_widget->Show();
+  widget->Init(params);
+  widget->SetContentsView(contents);
+  widget->Show();
+
+  Widget* parent = new Widget;
+  parent->Init(params);
+  parent->SetContentsView(new View);
+  parent->Show();
 
   // Make sure the bubble widget is parented to a widget other than the anchor
   // widget so that closing the anchor widget does not close the bubble widget.
-  BubbleDelegateView* bubble_delegate = new BubbleDelegateView(
-      anchor_widget->GetContentsView(), BubbleBorder::NONE);
-  bubble_delegate->set_parent_window(parent_widget->GetNativeView());
-  // Preventing close on deactivate should not prevent closing with the parent.
-  bubble_delegate->set_close_on_deactivate(false);
-  Widget* bubble_widget = BubbleDelegateView::CreateBubble(bubble_delegate);
+  BubbleDelegateView* bubble_delegate =
+      new BubbleDelegateView(contents, BubbleBorder::NONE);
+  bubble_delegate->set_parent_window(parent->GetNativeView());
+  bubble_delegate->set_color(SK_ColorGREEN);
+  Widget* bubble_widget(
+      BubbleDelegateView::CreateBubble(bubble_delegate));
   EXPECT_EQ(bubble_delegate, bubble_widget->widget_delegate());
   EXPECT_EQ(bubble_widget, bubble_delegate->GetWidget());
-  EXPECT_EQ(anchor_widget.get(), bubble_delegate->anchor_widget());
-  TestWidgetObserver bubble_observer(bubble_widget);
-  EXPECT_FALSE(bubble_observer.widget_closed());
+  EXPECT_EQ(widget, bubble_delegate->anchor_widget());
 
-  // Showing and hiding the bubble widget should have no effect on its anchor.
   bubble_widget->Show();
   RunPendingMessages();
-  EXPECT_EQ(anchor_widget.get(), bubble_delegate->anchor_widget());
+  EXPECT_EQ(widget, bubble_delegate->anchor_widget());
+
   bubble_widget->Hide();
   RunPendingMessages();
-  EXPECT_EQ(anchor_widget.get(), bubble_delegate->anchor_widget());
+  EXPECT_EQ(widget, bubble_delegate->anchor_widget());
 
-  // Ensure that closing the anchor widget clears the bubble's reference to that
-  // anchor widget, but the bubble itself does not close.
-  anchor_widget->CloseNow();
+  // Closing the anchor widget should unset the reference to the anchor widget
+  // for the bubble.
+  widget->CloseNow();
   RunPendingMessages();
-  EXPECT_NE(anchor_widget.get(), bubble_delegate->anchor_widget());
-  EXPECT_FALSE(bubble_observer.widget_closed());
+  EXPECT_NE(bubble_delegate->anchor_widget(), widget);
 
-  // Ensure that closing the parent widget also closes the bubble itself.
-  parent_widget->CloseNow();
+  bubble_widget->CloseNow();
+  parent->CloseNow();
   RunPendingMessages();
-  EXPECT_TRUE(bubble_observer.widget_closed());
 }
 
 }  // namespace views
