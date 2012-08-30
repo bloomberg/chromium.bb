0d309fa36080da1635369b5ccf9a2615d3c28469
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-30 23:38:20 +0000

    Add UMA counts for Metro snap mode toggle
    TBR=pkasting@chromium.org
    BUG=143675
    TEST=Make sure we properly count the Metro Snap mode toggles.
    
    Review URL: https://chromiumcodereview.appspot.com/10919015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154313 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d1ce78beb9d7a54f25e4e581c9cfc57c4d704c5d

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -782,6 +782,7 @@ void BrowserView::FullScreenStateChanged() {
 
 #if defined(OS_WIN)
 void BrowserView::SetMetroSnapMode(bool enable) {
+  HISTOGRAM_COUNTS("Metro.SnapModeToggle", enable);
   ProcessFullscreen(enable, FOR_METRO, GURL(), FEB_TYPE_NONE);
 }
 
