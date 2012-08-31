d2549f7ce2bc0627bb6e31d0ebae606607c63d37
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-31 01:58:12 +0000

    Remove erroneous DCHECK for flapper.
    GetBundledPepperFlash() DCHECK for file existence in a sandboxed context
    which always fails on Linux.
    
    BUG=145833
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/10913019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154347 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 68827548e8177daf50f6a6a62fa399d00311d7c5

--- chrome/common/chrome_content_client.cc
+++ chrome/common/chrome_content_client.cc
@@ -292,10 +292,6 @@ bool GetBundledPepperFlash(content::PepperPluginInfo* plugin,
   FilePath flash_path;
   if (!PathService::Get(chrome::FILE_PEPPER_FLASH_PLUGIN, &flash_path))
     return false;
-  // It is an error to have FLAPPER_AVAILABLE defined but then not having the
-  // plugin file in place, but this happens in Chrome OS builds.
-  // Use --disable-bundled-ppapi-flash to skip this.
-  DCHECK(file_util::PathExists(flash_path));
 
   bool force_enable = CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kEnableBundledPpapiFlash);
