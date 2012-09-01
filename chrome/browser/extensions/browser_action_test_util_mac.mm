8012352c9f131932cee21c9067f9ba153f9efef9
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-01 16:17:25 +0000

    extensions: Fix the include path to browser_action_test_util.h
    TBR=aa@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10916073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154619 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3920fc361cdcc9c18b5c1873210df26e37977918

--- chrome/browser/extensions/browser_action_test_util_mac.mm
+++ chrome/browser/extensions/browser_action_test_util_mac.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "browser_action_test_util.h"
+#include "chrome/browser/extensions/browser_action_test_util.h"
 
 #include "base/sys_string_conversions.h"
 #include "chrome/browser/ui/browser.h"
