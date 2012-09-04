3f84695104b169fe25742ba3b91d04467f5debc4
Akira TAGOH <akira@tagoh.org>
2012-09-04 12:39:48 +0900

    Bug 50497 - RFE: Add OpenType feature tags support
    Add FC_FONT_FEATURES to store the feature tags to be enabled.

--- src/fcobjs.h
+++ src/fcobjs.h
@@ -41,4 +41,5 @@ FC_OBJECT (EMBEDDED_BITMAP,	FcTypeBool)
 FC_OBJECT (DECORATIVE,		FcTypeBool)
 FC_OBJECT (LCD_FILTER,		FcTypeInteger)
 FC_OBJECT (NAMELANG,		FcTypeString)
+FC_OBJECT (FONT_FEATURES,	FcTypeString)
 /* ^-------------- Add new objects here. */
