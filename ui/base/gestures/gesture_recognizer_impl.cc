be9d4ad05aa4b8a7522acd339e559b2b74fed47a
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-05 16:10:45 +0000

    gesture recognizer: Plug a leak.
    BUG=146465
    
    Review URL: https://chromiumcodereview.appspot.com/10912091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154963 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dbd59dd7637b90f1288b645c8352ed137cc38f64

--- ui/base/gestures/gesture_recognizer_impl.cc
+++ ui/base/gestures/gesture_recognizer_impl.cc
@@ -324,16 +324,17 @@ GestureSequence::Gestures* GestureRecognizerImpl::AdvanceTouchQueue(
     if (event->status() == TOUCH_STATUS_QUEUED)
       break;
 
-    GestureSequence::Gestures* current_gestures = AdvanceTouchQueueByOne(
-        consumer, event->status());
-    if (current_gestures) {
+    scoped_ptr<GestureSequence::Gestures> current_gestures(
+        AdvanceTouchQueueByOne(consumer, event->status()));
+    if (current_gestures.get()) {
       if (!gestures.get()) {
-        gestures.reset(current_gestures);
+        gestures.reset(current_gestures.release());
       } else {
         gestures->insert(gestures->end(), current_gestures->begin(),
                                           current_gestures->end());
-        // current_gestures leaks here, but deleting it causes
-        // double free on some memory. crbug.com/146465.
+        // The gestures in |current_gestures| are now owned by |gestures|. Make
+        // sure they don't get freed with |current_gestures|.
+        current_gestures->weak_clear();
       }
     }
   }
