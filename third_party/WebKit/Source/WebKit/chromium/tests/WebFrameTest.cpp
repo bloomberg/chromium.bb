bloomberg/chromium.bb

commit 113ee7b1eede75569b427f24cae3cd51a21c69bb

Source/WebCore: REGRESSION (r115654): Sometimes does not replace content for multipart/x-mixed-replace https://bugs.webkit.org/show_bug.cgi?id=88436
Reviewed by Brady Eidson.

Test: http/tests/multipart/multipart-replace-non-html-content.php

* loader/DocumentLoader.cpp:
(WebCore::DocumentLoader::commitData): We should only send receivedFirstData() once per main resource load,
    rather than multiple times in a multipart load.
(WebCore::DocumentLoader::setupForReplaceByMIMEType): m_gotFirstByte isn't set to true until data is
    actually committed, and multipart data is often not committed until the part is finished. Check
    whether the SharedBuffer is non-null instead.
* testing/js/WebCoreTestSupport.cpp:
(WebCoreTestSupport::resetInternalsObject): The JSInternals object my have already been cleared if the window shell
    was cleared as part of creation of a new Document. Check it before using it.

LayoutTests: Test for https://bugs.webkit.org/show_bug.cgi?id=88436.

Reviewed by Brady Eidson.

* http/tests/multipart/multipart-replace-non-html-content-expected.txt: Added.
* http/tests/multipart/multipart-replace-non-html-content.php: Added.


git-svn-id: svn://svn.chromium.org/blink/trunk@121912 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: e73832895eb378b25d5ad9f5452dd602e75c4142

third_party/WebKit/Source/WebCore/loader/DocumentLoader.cpp
@@ -335,7 +335,8 @@ void DocumentLoader::commitData(const char* bytes, size_t length)
             m_frame->document()->setBaseURLOverride(m_archive->mainResource()->url());
 #endif
 
-        frameLoader()->receivedFirstData();
+        if (!frameLoader()->isReplacing())
+            frameLoader()->receivedFirstData();
 
         bool userChosen = true;
         String encoding = overrideEncoding();
@@ -366,7 +367,7 @@ void DocumentLoader::receivedData(const char* data, int length)
 
 void DocumentLoader::setupForReplaceByMIMEType(const String& newMIMEType)
 {
-    if (!m_gotFirstByte)
+    if (!mainResourceData())
         return;
     
     String oldMIMEType = m_response.mimeType();

third_party/WebKit/LayoutTests/http/tests/multipart/multipart-replace-non-html-content-expected.txt
@@ -1,0 +1,3 @@
+This text should only appear once 10                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+
+

third_party/WebKit/LayoutTests/http/tests/multipart/multipart-replace-non-html-content.php
@@ -1,0 +1,26 @@
+<?php
+header('Content-type: multipart/x-mixed-replace; boundary=boundary');
+header('Connection: keep-alive');
+echo "--boundary\r\n";
+echo "Content-Type: text/html\r\n\r\n";
+echo str_pad('', 5000);
+?>
+
+<script>
+if (window.layoutTestController)
+    layoutTestController.dumpAsText();
+</script>
+
+<?php
+for ($i = 0; $i <= 10; $i++) {
+    echo "--boundary\r\n";
+    echo "Content-Type: text/plain\r\n\r\n";
+    echo "This text should only appear once ";
+    echo $i;
+    echo str_pad('', 5000);
+    echo "\r\n\r\n";
+    flush();
+    usleep(100000);
+    $i++;
+}
+?>

third_party/WebKit/Source/WebCore/testing/js/WebCoreTestSupport.cpp
@@ -52,8 +52,10 @@ void resetInternalsObject(JSContextRef context)
     ExecState* exec = toJS(context);
     JSLockHolder lock(exec);
     JSDOMGlobalObject* globalObject = jsCast<JSDOMGlobalObject*>(exec->lexicalGlobalObject());
-    Internals * internals = toInternals(globalObject->getDirect(exec->globalData(), Identifier(exec, Internals::internalsId)));
-    if (internals) {
+    JSValue internalsJS = globalObject->getDirect(exec->globalData(), Identifier(exec, Internals::internalsId));
+    if (internalsJS.isNull() || internalsJS.isEmpty())
+        return;
+    if (Internals* internals = toInternals(internalsJS)) {
         ScriptExecutionContext* scriptContext = globalObject->scriptExecutionContext();
         if (scriptContext->isDocument())
             internals->reset(static_cast<Document*>(scriptContext));

commit 08dc0c29b93bbd129c4a47b0c6f2ad582e374c30

Source/WebCore: [chromium] Some SubstituteData loads broken after r121912 https://bugs.webkit.org/show_bug.cgi?id=91685
Reviewed by Adam Barth.

Test: WebFrameTest.ReplaceNavigationAfterHistoryNavigation in chromium's webkit_unit_tests.

* loader/DocumentLoader.cpp:
(WebCore::DocumentLoader::commitData): receivedFirstData() should be called exactly once per load,
    on the first commit. I had mistakely assumed in r121912 that isReplacing() was true only for multipart
    loads, and only after the first commit (chromium uses it for some SubstituteData loads to ensure the error page
    replaces the failed load). We need to check whether we are loading multipart content before assuming isReplacing()
    will tell us what we need to know.

Source/WebKit/chromium: Test for https://bugs.webkit.org/show_bug.cgi?id=91685

Reviewed by Adam Barth.

* tests/WebFrameTest.cpp:


git-svn-id: svn://svn.chromium.org/blink/trunk@127690 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 303f5fdb3bacbb3c0078bec3d54e74a32e14f662

third_party/WebKit/Source/WebCore/loader/DocumentLoader.cpp
@@ -336,7 +336,9 @@ void DocumentLoader::commitData(const char* bytes, size_t length)
             m_frame->document()->setBaseURLOverride(m_archive->mainResource()->url());
 #endif
 
-        if (!frameLoader()->isReplacing())
+        // Call receivedFirstData() exactly once per load. We should only reach this point multiple times
+        // for multipart loads, and isReplacing() will be true after the first time.
+        if (!m_mainResourceLoader || !m_mainResourceLoader->isLoadingMultipartContent() || !frameLoader()->isReplacing())
             frameLoader()->receivedFirstData();
 
         bool userChosen = true;

third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
@@ -45,6 +45,7 @@
 #include "WebFormElement.h"
 #include "WebFrameClient.h"
 #include "WebFrameImpl.h"
+#include "WebHistoryItem.h"
 #include "WebRange.h"
 #include "WebScriptSource.h"
 #include "WebSearchableFormData.h"
@@ -54,6 +55,7 @@
 #include "WebViewClient.h"
 #include "WebViewImpl.h"
 #include "platform/WebFloatRect.h"
+#include "platform/WebURLResponse.h"
 #include "v8.h"
 #include <gtest/gtest.h>
 #include <webkit/support/webkit_support.h>
@@ -1144,6 +1146,65 @@ TEST_F(WebFrameTest, DisambiguationPopupTest)
             EXPECT_FALSE(client.triggered());
     }
 
+}
+
+class TestSubstituteDataWebFrameClient : public WebFrameClient {
+public:
+    TestSubstituteDataWebFrameClient()
+        : m_commitCalled(false)
+    {
+    }
+
+    virtual void didFailProvisionalLoad(WebFrame* frame, const WebURLError& error)
+    {
+        frame->loadHTMLString("This should appear", toKURL("data:text/html,chromewebdata"), error.unreachableURL, true);
+        webkit_support::RunAllPendingMessages();
+    }
+
+    virtual void didCommitProvisionalLoad(WebFrame* frame, bool)
+    {
+        if (frame->dataSource()->response().url() != WebURL(URLTestHelpers::toKURL("about:blank")))
+            m_commitCalled = true;
+    }
+
+    bool commitCalled() const { return m_commitCalled; }
+
+private:
+    bool m_commitCalled;
+};
+
+TEST_F(WebFrameTest, ReplaceNavigationAfterHistoryNavigation)
+{
+    TestSubstituteDataWebFrameClient webFrameClient;
+
+    WebView* webView = FrameTestHelpers::createWebViewAndLoad("about:blank", true, &webFrameClient);
+    webkit_support::RunAllPendingMessages();
+    WebFrame* frame = webView->mainFrame();
+
+    // Load a url as a history navigation that will return an error. TestSubstituteDataWebFrameClient
+    // will start a SubstituteData load in response to the load failure, which should get fully committed.
+    // Due to https://bugs.webkit.org/show_bug.cgi?id=91685, FrameLoader::didReceiveData() wasn't getting
+    // called in this case, which resulted in the SubstituteData document not getting displayed.
+    WebURLError error;
+    error.reason = 1337;
+    error.domain = "WebFrameTest";
+    std::string errorURL = "http://0.0.0.0";
+    WebURLResponse response;
+    response.initialize();
+    response.setURL(URLTestHelpers::toKURL(errorURL));
+    response.setMIMEType("text/html");
+    response.setHTTPStatusCode(500);
+    WebHistoryItem errorHistoryItem;
+    errorHistoryItem.initialize();
+    errorHistoryItem.setURLString(WebString::fromUTF8(errorURL.c_str(), errorURL.length()));
+    errorHistoryItem.setOriginalURLString(WebString::fromUTF8(errorURL.c_str(), errorURL.length()));
+    webkit_support::RegisterMockedErrorURL(URLTestHelpers::toKURL(errorURL), response, error);
+    frame->loadHistoryItem(errorHistoryItem);
+    webkit_support::ServeAsynchronousMockedRequests();
+
+    WebString text = frame->contentAsText(std::numeric_limits<size_t>::max());
+    EXPECT_EQ("This should appear", std::string(text.utf8().data()));
+    EXPECT_TRUE(webFrameClient.commitCalled());
 }
 
 } // namespace