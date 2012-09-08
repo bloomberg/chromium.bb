e37324af418b40c90a5e10cc08e432a16ab0677a
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-08 17:41:52 +0000

    Fix include guard style for stale_cache_files_remover.h
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10916168
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155586 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e6ff9116ad6375e83aac9c0d1447cf7122caee28

--- chrome/browser/chromeos/gdata/stale_cache_files_remover.h
+++ chrome/browser/chromeos/gdata/stale_cache_files_remover.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_GDATA_STALE_CACHE_FILE_REMOVER_H_
-#define CHROME_BROWSER_CHROMEOS_GDATA_STALE_CACHE_FILE_REMOVER_H_
+#ifndef CHROME_BROWSER_CHROMEOS_GDATA_STALE_CACHE_FILES_REMOVER_H_
+#define CHROME_BROWSER_CHROMEOS_GDATA_STALE_CACHE_FILES_REMOVER_H_
 
 #include <string>
 #include <vector>
@@ -62,4 +62,4 @@ class StaleCacheFilesRemover : public DriveFileSystemInterface::Observer {
 
 }  // namespace gdata
 
-#endif  // CHROME_BROWSER_CHROMEOS_GDATA_STALE_CACHE_FILE_REMOVER_H_
+#endif  // CHROME_BROWSER_CHROMEOS_GDATA_STALE_CACHE_FILES_REMOVER_H_
