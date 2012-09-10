6878c2a2f555c382ccf5152d7b8cf2ed772984d1
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 07:45:00 +0000

    Scope ManifestTest.RestrictedKeys to DEV channel for the commands API.
    TBR=finnur@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10928078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155678 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 38d71208373bc7f7eb51bb01ff517bc8a5ad498a

--- chrome/common/extensions/manifest_unittest.cc
+++ chrome/common/extensions/manifest_unittest.cc
@@ -191,15 +191,21 @@ TEST_F(ManifestTest, RestrictedKeys) {
       &manifest, keys::kPlatformAppBackground, NULL);
 
   // "commands" is restricted to manifest_version >= 2.
-  MutateManifest(
-      &manifest, keys::kCommands, new DictionaryValue());
-  EXPECT_FALSE(manifest->HasKey(keys::kCommands));
-  EXPECT_FALSE(manifest->Get(keys::kCommands, &output));
+  {
+    // ... and dev channel, for now.
+    Feature::ScopedCurrentChannel dev_channel_scope(
+        chrome::VersionInfo::CHANNEL_DEV);
 
-  MutateManifest(
-      &manifest, keys::kManifestVersion, Value::CreateIntegerValue(2));
-  EXPECT_TRUE(manifest->HasKey(keys::kCommands));
-  EXPECT_TRUE(manifest->Get(keys::kCommands, &output));
+    MutateManifest(
+        &manifest, keys::kCommands, new DictionaryValue());
+    EXPECT_FALSE(manifest->HasKey(keys::kCommands));
+    EXPECT_FALSE(manifest->Get(keys::kCommands, &output));
+
+    MutateManifest(
+        &manifest, keys::kManifestVersion, Value::CreateIntegerValue(2));
+    EXPECT_TRUE(manifest->HasKey(keys::kCommands));
+    EXPECT_TRUE(manifest->Get(keys::kCommands, &output));
+  }
 };
 
 }  // namespace extensions
