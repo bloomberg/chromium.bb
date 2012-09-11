bbbf8d85c68ff09e1d112a85dcc05a762e32bb6f
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 16:48:35 +0000

    Remove UA spoofing for Mac Silverlight 4
    Silverlight 4 no longer loads (it uses the Carbon event model), so this
    spoofing code will never be used.
    
    BUG=127555
    
    
    Review URL: https://chromiumcodereview.appspot.com/10916202
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156028 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b5bd8166cb4bea1d94ad1696beb3c4ac85735f6f

--- webkit/plugins/npapi/plugin_host.cc
+++ webkit/plugins/npapi/plugin_host.cc
@@ -600,20 +600,6 @@ const char* NPN_UserAgent(NPP id) {
   if (use_mozilla_user_agent)
     return "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9a1) "
         "Gecko/20061103 Firefox/2.0a1";
-#elif defined(OS_MACOSX)
-  // Silverlight 4 doesn't handle events correctly unless we claim to be Safari.
-  scoped_refptr<PluginInstance> plugin;
-  if (id)
-    plugin = FindInstance(id);
-  if (plugin.get()) {
-    webkit::WebPluginInfo plugin_info =
-        plugin->plugin_lib()->plugin_info();
-    if (plugin_info.name == ASCIIToUTF16("Silverlight Plug-In") &&
-        StartsWith(plugin_info.version, ASCIIToUTF16("4."), false)) {
-      return "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-us) "
-          "AppleWebKit/534.1+ (KHTML, like Gecko) Version/5.0 Safari/533.16";
-    }
-  }
 #endif
 
   return webkit_glue::GetUserAgent(GURL()).c_str();
