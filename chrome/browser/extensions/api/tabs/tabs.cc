f93f1a15f22717a811ed67899733b4942c86f5e9
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 18:58:41 +0000

    Fix a typo in extensions/api/tabs/tabs.cc
    TBR=mihaip
    R=jennb@chromium.org
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/10905217
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156083 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e9b6ac9b5cfb2881778fadb656a586c0f13ac576

--- chrome/browser/extensions/api/tabs/tabs.cc
+++ chrome/browser/extensions/api/tabs/tabs.cc
@@ -500,7 +500,7 @@ bool CreateWindowFunction::RunImpl() {
 #endif
         if (use_panels) {
           window_type = Browser::TYPE_PANEL;
-#if !defined(US_ASH)
+#if !defined(USE_ASH)
           // Non-Ash has both docked and detached panel types.
           if (type_str == keys::kWindowTypeValueDetachedPanel)
             panel_create_mode = PanelManager::CREATE_AS_DETACHED;
