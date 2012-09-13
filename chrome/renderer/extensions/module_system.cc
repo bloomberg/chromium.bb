e0848fdf7d4c92e0876bef2fc9bfe7ea2e3caaee
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 03:03:23 +0000

    Add IsEmpty() check to ModuleSystem::IsPresentInCurrentContext().
    BUG=134977
    
    
    Review URL: https://chromiumcodereview.appspot.com/10928170
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156483 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e6a1b75cefbd01bb03fa6913cf652ec823bacee7

--- chrome/renderer/extensions/module_system.cc
+++ chrome/renderer/extensions/module_system.cc
@@ -56,6 +56,8 @@ ModuleSystem::NativesEnabledScope::~NativesEnabledScope() {
 // static
 bool ModuleSystem::IsPresentInCurrentContext() {
   v8::Handle<v8::Object> global(v8::Context::GetCurrent()->Global());
+  if (global.IsEmpty())
+    return false;
   v8::Handle<v8::Value> module_system =
       global->GetHiddenValue(v8::String::New(kModuleSystem));
   return !module_system.IsEmpty() && !module_system->IsUndefined();
