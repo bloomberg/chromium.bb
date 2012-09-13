ce275277eb81ad646ff8760cc813259c4bfb4ad0
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 13:24:28 +0000

    Adjust tsan suppressions for media::Pipeline due to r156011.
    BUG=137973
    TBR=timurrrr
    
    Review URL: https://codereview.chromium.org/10907213
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156540 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7f3875751ae2619503cf3433408ba27ca872e6e1

--- media/base/pipeline.cc
+++ media/base/pipeline.cc
@@ -360,6 +360,8 @@ void Pipeline::OnAudioTimeUpdate(TimeDelta time, TimeDelta max_time) {
   if (waiting_for_clock_update_ && time < clock_->Elapsed())
     return;
 
+  // TODO(scherkus): |state_| should only be accessed on pipeline thread, see
+  // http://crbug.com/137973
   if (state_ == kSeeking)
     return;
 
@@ -374,6 +376,8 @@ void Pipeline::OnVideoTimeUpdate(TimeDelta max_time) {
   if (has_audio_)
     return;
 
+  // TODO(scherkus): |state_| should only be accessed on pipeline thread, see
+  // http://crbug.com/137973
   if (state_ == kSeeking)
     return;
 
