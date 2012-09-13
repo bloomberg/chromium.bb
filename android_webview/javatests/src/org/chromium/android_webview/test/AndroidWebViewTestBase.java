f73b7b520c7f5e7f2a462c39b6c2ce5766bc7b28
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 16:41:14 +0000

    Revert 155208 - Revert 154435 - [Android] Port simple ContentSettings tests to android_webview.
    This CL depends on https://chromiumcodereview.appspot.com/10908014/
    
    Review URL: https://chromiumcodereview.appspot.com/10919010
    
    TBR=mnaganov@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10907095
    
    TBR=torne@chromium.org
    Review URL: https://codereview.chromium.org/10917238
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156564 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3206e4198b8655b5666c014d483a15b0f09ed926

--- android_webview/javatests/src/org/chromium/android_webview/test/AndroidWebViewTestBase.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AndroidWebViewTestBase.java
@@ -174,4 +174,14 @@ public class AndroidWebViewTestBase
             }
         });
     }
+
+    protected ContentSettings getContentSettingsOnUiThread(
+            final ContentViewCore contentViewCore) throws Throwable {
+        return runTestOnUiThreadAndGetResult(new Callable<ContentSettings>() {
+            @Override
+            public ContentSettings call() throws Exception {
+                return contentViewCore.getContentSettings();
+            }
+        });
+    }
 }
