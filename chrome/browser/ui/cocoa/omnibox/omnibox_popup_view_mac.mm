8750aee93f2e50d85a1ea372cf1efb8826916dc7
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 17:56:39 +0000

    mac: Don't call userSpaceScaleFactor on 10.7+
    BUG=none
    TEST=no more log messages about userSpaceScaleFactor being deprecated.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10910273
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156836 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 14b41b56962e22e7b19266ead0c28412ea5e0b5f

--- chrome/browser/ui/cocoa/omnibox/omnibox_popup_view_mac.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_popup_view_mac.mm
@@ -22,7 +22,14 @@
 #include "ui/gfx/rect.h"
 #include "ui/gfx/scoped_ns_graphics_context_save_gstate_mac.h"
 
-namespace {
+
+#if !defined(MAC_OS_X_VERSION_10_7) || \
+    MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_7
+@interface NSWindow (LionAPI)
+- (CGFloat)backingScaleFactor;
+@end
+#endif  // 10.7
+
 
 // The size delta between the font used for the edit and the result
 // rows.
@@ -69,6 +76,8 @@ const CGFloat kFieldVisualInset = 1.0;
 // borders.
 const CGFloat kWindowBorderWidth = 1.0;
 
+namespace {
+
 // Background colors for different states of the popup elements.
 NSColor* BackgroundColor() {
   return [[NSColor controlBackgroundColor] colorWithAlphaComponent:kPopupAlpha];
@@ -80,15 +89,22 @@ NSColor* HoveredBackgroundColor() {
   return [[NSColor controlHighlightColor] colorWithAlphaComponent:kPopupAlpha];
 }
 
-static NSColor* ContentTextColor() {
+NSColor* ContentTextColor() {
   return [NSColor blackColor];
 }
-static NSColor* DimContentTextColor() {
+NSColor* DimContentTextColor() {
   return [NSColor darkGrayColor];
 }
-static NSColor* URLTextColor() {
+NSColor* URLTextColor() {
   return [NSColor colorWithCalibratedRed:0.0 green:0.55 blue:0.0 alpha:1.0];
 }
+
+CGFloat ScaleFactor(NSWindow* window) {
+  if ([window respondsToSelector:@selector(backingScaleFactor)])
+    return [window backingScaleFactor];
+  return [window userSpaceScaleFactor];
+}
+
 }  // namespace
 
 // Helper for MatchText() to allow sharing code between the contents
@@ -344,14 +360,14 @@ void OmniboxPopupViewMac::PositionPopup(const CGFloat matrixHeight) {
   // window border.  Would prefer -convertSize:fromView: to
   // -userSpaceScaleFactor for the scale conversion, but until the
   // window is on-screen that doesn't work right (bug?).
-  popupFrame.size.height = matrixHeight * [popup_ userSpaceScaleFactor];
+  popupFrame.size.height = matrixHeight * ScaleFactor(popup_);
   popupFrame.origin.y -= NSHeight(popupFrame) + kWindowBorderWidth;
 
   // Inset to account for the horizontal border drawn by the window.
   popupFrame = NSInsetRect(popupFrame, kWindowBorderWidth, 0.0);
 
   // Leave a gap between the popup and the field.
-  popupFrame.origin.y -= kPopupFieldGap * [popup_ userSpaceScaleFactor];
+  popupFrame.origin.y -= kPopupFieldGap * ScaleFactor(popup_);
 
   // Do nothing if the popup is already animating to the given |frame|.
   if (NSEqualRects(popupFrame, targetPopupFrame_))
@@ -444,7 +460,7 @@ void OmniboxPopupViewMac::UpdatePopupAppearance() {
   const NSRect fieldRectBase = [field_ convertRect:[field_ bounds] toView:nil];
   const CGFloat popupWidth = NSWidth(fieldRectBase) - 2 * kWindowBorderWidth;
   DCHECK_GT(popupWidth, 0.0);
-  const CGFloat matrixWidth = popupWidth / [popup_ userSpaceScaleFactor];
+  const CGFloat matrixWidth = popupWidth / ScaleFactor(popup_);
 
   // Load the results into the popup's matrix.
   const size_t rows = model_->result().size();
