6e193dbefb341f63a53c4ed62a0594f31e155855
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 20:37:18 +0000

    Revert 156872 - Per request load time stats. R=cbentzel@chromium.org, rvargas@chromium.org BUG=149682 Review URL: https://codereview.chromium.org/10908252
    TBR=tburkard@chromium.org
    Review URL: https://codereview.chromium.org/10910289
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156876 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 63d968cde9110740d50faa13f1d1da4dc06eb775

--- chrome/browser/net/chrome_network_delegate.cc
+++ chrome/browser/net/chrome_network_delegate.cc
@@ -297,8 +297,6 @@ void ChromeNetworkDelegate::OnCompleted(net::URLRequest* request,
 void ChromeNetworkDelegate::OnURLRequestDestroyed(net::URLRequest* request) {
   ExtensionWebRequestEventRouter::GetInstance()->OnURLRequestDestroyed(
       profile_, request);
-  if (load_time_stats_)
-    load_time_stats_->OnURLRequestDestroyed(*request);
 }
 
 void ChromeNetworkDelegate::OnPACScriptError(int line_number,
