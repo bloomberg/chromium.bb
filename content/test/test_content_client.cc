99aaec9373729465f69968224b34179c0b5370b2
msarda@chromium.org <msarda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 10:45:59 +0000

    Do not load content_resources.pak on iOS.
    content_resources.pak is not built on iOS as it is not required. Do not
    load it for tests to avoid confusion.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10907198
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156779 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: af738a3fec0fa10f5a44c951ddc3cb1e4e42ef9d

--- content/test/test_content_client.cc
+++ content/test/test_content_client.cc
@@ -12,11 +12,14 @@
 
 TestContentClient::TestContentClient()
     : data_pack_(ui::SCALE_FACTOR_100P) {
+  // content_resources.pak is not built on iOS as it is not required.
+#if !defined(OS_IOS)
   FilePath content_resources_pack_path;
   PathService::Get(base::DIR_MODULE, &content_resources_pack_path);
   content_resources_pack_path = content_resources_pack_path.Append(
       FILE_PATH_LITERAL("content_resources.pak"));
   data_pack_.LoadFromPath(content_resources_pack_path);
+#endif
 }
 
 TestContentClient::~TestContentClient() {
