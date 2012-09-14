4ad96f489be101d18515d31dc1305373ba24e7af
yongsheng.zhu@intel.com <yongsheng.zhu@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 03:21:39 +0000

    Enable the history quick provider for android
    All 12 unit tests cases of the history quick provider could pass.
    
    BUG=139411
    TEST=run_tests.py -s unit_tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10928081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156731 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a25f064cc53ce6345bc7237ada9b432c08ec5b77

--- chrome/browser/history/history.cc
+++ chrome/browser/history/history.cc
@@ -768,10 +768,6 @@ bool HistoryService::Init(const FilePath& history_dir,
   bookmark_service_ = bookmark_service;
   no_db_ = no_db;
 
-#if !defined(OS_ANDROID)
-  // History quick provider is enabled on all platforms other than Android.
-  // TODO(jcivelli): Enable the History Quick Provider on Android and figure out
-  // why it reports the wrong results for some pages.
   if (profile_) {
     std::string languages =
         profile_->GetPrefs()->GetString(prefs::kAcceptLanguages);
@@ -779,7 +775,6 @@ bool HistoryService::Init(const FilePath& history_dir,
         new history::InMemoryURLIndex(profile_, history_dir_, languages));
     in_memory_url_index_->Init();
   }
-#endif  // !OS_ANDROID
 
   // Create the history backend.
   LoadBackendIfNecessary();
