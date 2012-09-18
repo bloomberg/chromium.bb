74630fe302c81b03b58aedfe513128201cd96fd5
reed@google.com <reed@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-18 15:37:23 +0000

    Now that we can handle NULL from BitmapPlatformDevice::Create, remove the crashers that were used to try to diagnose... before crashing. With this change we can safely run over-allocating sites like http://ie.microsoft.com/testdrive/Performance/Chalkboard/
    BUG=134626
    Review URL: https://codereview.chromium.org/10918270
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157363 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 75d9d6327a7773af0ae733cfd876fe14f355426b

--- skia/ext/bitmap_platform_device_win.cc
+++ skia/ext/bitmap_platform_device_win.cc
@@ -15,65 +15,6 @@
 
 namespace skia {
 
-// Disable optimizations during crash analysis.
-#pragma optimize("", off)
-
-// Crash On Failure. |address| should be a number less than 4000.
-#define COF(address, condition) if (!(condition)) *((int*) address) = 0
-
-// This is called when a bitmap allocation fails, and this function tries to
-// determine why it might have failed, and crash on different
-// lines. This allows us to see in crash dumps the most likely reason for the
-// failure. It takes the size of the bitmap we were trying to allocate as its
-// arguments so we can check that as well.
-//
-// Note that in a sandboxed renderer this function crashes when trying to
-// call GetProcessMemoryInfo() because it tries to load psapi.dll, which
-// is fine but gives you a very hard to read crash dump.
-void CrashForBitmapAllocationFailure(int w, int h, unsigned int error) {
-  // Store the extended error info in a place easy to find at debug time.
-  unsigned int diag_error = 0;
-  // If the bitmap is ginormous, then we probably can't allocate it.
-  // We use 32M pixels = 128MB @ 4 bytes per pixel.
-  const LONG_PTR kGinormousBitmapPxl = 32000000;
-  COF(1, LONG_PTR(w) * LONG_PTR(h) < kGinormousBitmapPxl);
-
-  // The maximum number of GDI objects per process is 10K. If we're very close
-  // to that, it's probably the problem.
-  const unsigned int kLotsOfGDIObjects = 9990;
-  unsigned int num_gdi_objects = GetGuiResources(GetCurrentProcess(),
-                                                 GR_GDIOBJECTS);
-  if (num_gdi_objects == 0) {
-    diag_error = GetLastError();
-    COF(2, false);
-  }
-  COF(3, num_gdi_objects < kLotsOfGDIObjects);
-
-  // If we're using a crazy amount of virtual address space, then maybe there
-  // isn't enough for our bitmap.
-  const SIZE_T kLotsOfMem = 1500000000;  // 1.5GB.
-  PROCESS_MEMORY_COUNTERS_EX pmc;
-  pmc.cb = sizeof(pmc);
-  if (!GetProcessMemoryInfo(GetCurrentProcess(),
-                            reinterpret_cast<PROCESS_MEMORY_COUNTERS*>(&pmc),
-                            sizeof(pmc))) {
-    diag_error = GetLastError();
-    COF(4, false);
-  }
-  COF(5, pmc.PagefileUsage < kLotsOfMem);
-  COF(6, pmc.PrivateUsage < kLotsOfMem);
-  // Ok but we are somehow out of memory?
-  COF(7, error != ERROR_NOT_ENOUGH_MEMORY);
-}
-
-// Crashes the process. This is called when a bitmap allocation fails but
-// unlike its cousin CrashForBitmapAllocationFailure() it tries to detect if
-// the issue was a non-valid shared bitmap handle.
-void CrashIfInvalidSection(HANDLE shared_section) {
-  DWORD handle_info = 0;
-  COF(8, ::GetHandleInformation(shared_section, &handle_info) == TRUE);
-}
-
 BitmapPlatformDevice::BitmapPlatformDeviceData::BitmapPlatformDeviceData(
     HBITMAP hbitmap)
     : bitmap_context_(hbitmap),
@@ -178,14 +119,8 @@ BitmapPlatformDevice* BitmapPlatformDevice::Create(
                                      reinterpret_cast<BITMAPINFO*>(&hdr), 0,
                                      &data,
                                      shared_section, 0);
-  if (!hbitmap) {
-    // Investigate we failed. If we know the reason, crash in a specific place.
-    unsigned int error = GetLastError();
-    if (shared_section)
-      CrashIfInvalidSection(shared_section);
-    CrashForBitmapAllocationFailure(width, height, error);
+  if (!hbitmap)
     return NULL;
-  }
 
   bitmap.setConfig(SkBitmap::kARGB_8888_Config, width, height);
   bitmap.setPixels(data);
