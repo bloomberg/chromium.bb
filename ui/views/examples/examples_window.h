2bf83d1255dcca4559fea7dd3ed8665403ec734e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-19 01:51:28 +0000

    views/examples: Export ShowExamplesWindow() function.
    We need to export this function because views_examples_lib is now a component
    target, i.e, it can be built either as static_library or shared_library, and
    in shared_library this function should be exported.
    
    BUG=141187
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/10944004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157474 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 235a7b26de5be3f1d9b37dc8b84675771c37b21d

--- ui/views/examples/examples_window.h
+++ ui/views/examples/examples_window.h
@@ -5,6 +5,8 @@
 #ifndef UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_H_
 #define UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_H_
 
+#include "ui/views/examples/views_examples_export.h"
+
 namespace views {
 namespace examples {
 
@@ -14,7 +16,7 @@ enum Operation {
 };
 
 // Shows a window with the views examples in it.
-void ShowExamplesWindow(Operation operation);
+VIEWS_EXAMPLES_EXPORT void ShowExamplesWindow(Operation operation);
 
 }  // namespace examples
 }  // namespace views
