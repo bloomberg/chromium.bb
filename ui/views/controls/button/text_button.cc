9f7aab977d6fa915f71e57f94bcd2abe3011603a
sheu@chromium.org <sheu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-19 23:22:35 +0000

    Conservative update button/label/textfield content
    Round 2, minus changes to Textfield (since it does some funky text
    postproceesing),
    
    Saves a repaint when no change actually occurs.
    
    BUG=chrome-os-partner:13141
    TEST=build, run on lumpy, lucas
    
    Change-Id: I8db2384954b2bf2c77ae1685f5db737f417fe312
    
    
    Review URL: https://chromiumcodereview.appspot.com/10928134
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157647 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7c4cf308ed4fdd84e5bc17dec1619ae2e9acb60d

--- ui/views/controls/button/text_button.cc
+++ ui/views/controls/button/text_button.cc
@@ -291,6 +291,8 @@ void TextButtonBase::SetIsDefault(bool is_default) {
 }
 
 void TextButtonBase::SetText(const string16& text) {
+  if (text == text_)
+    return;
   text_ = text;
   SetAccessibleName(text);
   UpdateTextSize();
