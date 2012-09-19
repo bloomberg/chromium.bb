944647a846975b2f4306ccd286548e221ea9dd90
bradnelson@google.com <bradnelson@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-09-19 19:02:07 +0000

    Adding known bad nexes to the corpus list.
    We have bad nexes in the corpus with sha1:
    903d3e5f29b909db162f2679363685144f61e3cc
    fac308d79f5c2d3a7f1f27112cfe19bf8431acfa
    
    Adding to the known bad list.
    
    BUG=None
    TEST=None
    R=khim@chromium.org
    
    Review URL: https://codereview.chromium.org/10941038
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@9780 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 513124d99730259d8d6cd70c1595e3f446f8183d

--- tests/abi_corpus/corpus_errors.py
+++ tests/abi_corpus/corpus_errors.py
@@ -13,6 +13,8 @@ import corpus_utils
 # Sha1s of known non-validating NEXEs in the corpus.
 BAD_NEXES = set([
     '1c2b051cb60367cf103128c9cd76769ffa1cf356',
+    '903d3e5f29b909db162f2679363685144f61e3cc',
+    'fac308d79f5c2d3a7f1f27112cfe19bf8431acfa',
 ])
 
 
