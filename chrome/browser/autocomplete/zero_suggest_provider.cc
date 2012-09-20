c00c90f918befd57345b900fb97c33173552302a
jered@chromium.org <jered@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-20 19:17:11 +0000

    Zero suggest: Do not notify listener if no results.
    This fixes a bug where, when the user first focuses in the omnibox on an
    HTTP URL with no suggestions, we erase the URL.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10947045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157807 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1aedaaa08397171253d1aa0a99951d96a52fe2a9

--- chrome/browser/autocomplete/zero_suggest_provider.cc
+++ chrome/browser/autocomplete/zero_suggest_provider.cc
@@ -101,13 +101,15 @@ void ZeroSuggestProvider::OnURLFetchComplete(const net::URLFetcher* source) {
     JSONStringValueSerializer deserializer(json_data);
     deserializer.set_allow_trailing_comma(true);
     scoped_ptr<Value> data(deserializer.Deserialize(NULL, NULL));
-    results_updated = data.get() && ParseSuggestResults(data.get());
+    results_updated =
+        data.get() && ParseSuggestResults(data.get()) && !results_.empty();
   }
   done_ = true;
 
-  ConvertResultsToAutocompleteMatches();
-  if (results_updated)
+  if (results_updated) {
+    ConvertResultsToAutocompleteMatches();
     listener_->OnProviderUpdate(true);
+  }
 }
 
 ZeroSuggestProvider::~ZeroSuggestProvider() {
