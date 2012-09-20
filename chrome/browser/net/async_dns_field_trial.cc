fc664994ec8519ec95e378ae4b05044b43eb5d65
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-20 11:22:19 +0000

    [net] Enable async DNS field trial on CrOS (canary+dev channels).
    BUG=143454
    
    
    Review URL: https://chromiumcodereview.appspot.com/10945041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157744 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1a5493cce5e80bdf4d00d4edecc059df0a972cd3

--- chrome/browser/net/async_dns_field_trial.cc
+++ chrome/browser/net/async_dns_field_trial.cc
@@ -11,20 +11,21 @@
 namespace chrome_browser_net {
 
 bool ConfigureAsyncDnsFieldTrial() {
-#if defined(OS_ANDROID) || defined(OS_IOS) || defined(OS_CHROMEOS)
+#if defined(OS_ANDROID) || defined(OS_IOS)
   // There is no DnsConfigService on those platforms so disable the field trial.
   return false;
 #endif
   const base::FieldTrial::Probability kAsyncDnsDivisor = 100;
   base::FieldTrial::Probability enabled_probability = 0;
 
-  // TODO(szym): expand to DEV channel after fixing http://crbug.com/121085
-  if (chrome::VersionInfo::GetChannel() <= chrome::VersionInfo::CHANNEL_CANARY)
-    enabled_probability = 0;
+#if defined(OS_CHROMEOS)
+  if (chrome::VersionInfo::GetChannel() <= chrome::VersionInfo::CHANNEL_DEV)
+    enabled_probability = 50;
+#endif
 
   scoped_refptr<base::FieldTrial> trial(
       base::FieldTrialList::FactoryGetFieldTrial(
-          "AsyncDns", kAsyncDnsDivisor, "disabled", 2012, 9, 30, NULL));
+          "AsyncDns", kAsyncDnsDivisor, "disabled", 2012, 10, 30, NULL));
 
   int enabled_group = trial->AppendGroup("enabled", enabled_probability);
   return trial->group() == enabled_group;
