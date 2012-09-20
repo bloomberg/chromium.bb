f7d8c6bf354d4c0717bf1c0040e5feca99163895
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-20 23:38:00 +0000

    Better warning on missing browser argument
    R=dtu@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10968005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157870 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7bc56fa9dbcbae3476312045db95b170ab3f1d77

--- tools/chrome_remote_control/chrome_remote_control/browser_options.py
+++ tools/chrome_remote_control/chrome_remote_control/browser_options.py
@@ -100,7 +100,8 @@ class BrowserOptions(optparse.Values):
       if self.browser_executable and not self.browser_type:
         self.browser_type = 'exact'
       if not self.browser_executable and not self.browser_type:
-        sys.stderr.write('Must provide --browser=<type>\n')
+        sys.stderr.write('Must provide --browser=<type>. ' +
+                         'Use --browser=list for valid options.\n')
         sys.exit(1)
       if self.browser_type == 'list':
         types = browser_finder.GetAllAvailableBrowserTypes(self)
