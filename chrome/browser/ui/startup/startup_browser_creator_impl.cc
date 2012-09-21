59858645a103975003d83d8b961bad2f1c02b883
motek@chromium.org <motek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-21 13:56:11 +0000

    A crash fix: removed the entire implicated section as obsolete.
    Note that the TODO points to a crbug which is declared obsolete / irrelevant.
    
    BUG=149729
    
    Review URL: https://chromiumcodereview.appspot.com/10948021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157974 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c28a567cb7ab2382e23ee642afe0262b064a83cb

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -849,12 +849,8 @@ Browser* StartupBrowserCreatorImpl::OpenTabsInBrowser(Browser* browser,
   // value of StartupBrowserCreated::show_main_browser_window_. If this was set
   // to true ahead of this place, it means another task must have been spawned
   // to take care of that.
-  if (!browser_creator_ || browser_creator_->show_main_browser_window()) {
+  if (!browser_creator_ || browser_creator_->show_main_browser_window())
     browser->window()->Show();
-    // TODO(jcampan): http://crbug.com/8123 we should not need to set the
-    //                initial focus explicitly.
-    chrome::GetActiveWebContents(browser)->GetView()->SetInitialFocus();
-  }
 
   return browser;
 }
