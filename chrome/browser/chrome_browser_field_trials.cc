c662201f22582c13e8ca9611f25cf236e3c74df4
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-21 03:35:53 +0000

    Add field trial for enabling Stage3D in XP
    Adds an experiment that we can set to enable Stage3D on XP (where not completely blacklisted). It will be disabled by default.
    
    
    BUG=136447
    
    
    Review URL: https://chromiumcodereview.appspot.com/10944037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157910 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 118d754ded87e8eaf3cb214f0ce58e8ed69078cc

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -124,6 +124,7 @@ void ChromeBrowserFieldTrials::SetupFieldTrials(bool proxy_policy_is_set) {
   DefaultAppsFieldTrial();
   AutoLaunchChromeFieldTrial();
   gpu_util::InitializeCompositingFieldTrial();
+  gpu_util::InitializeStage3DFieldTrial();
   SetupUniformityFieldTrials();
   AutocompleteFieldTrial::Activate();
   DisableNewTabFieldTrialIfNecesssary();
