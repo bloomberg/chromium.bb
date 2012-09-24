f9d460178b4cbea8d2bc9dcf956bb2df6b26f9eb
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-24 20:22:32 +0000

    New page set with fewer more focused pages
    R=dtu@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10958035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158367 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c136126223da5c9a5295b0faf9946667a906865f

--- tools/chrome_remote_control/chrome_remote_control/page_set.py
+++ tools/chrome_remote_control/chrome_remote_control/page_set.py
@@ -2,10 +2,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 import json
+import urlparse
 
 class Page(object):
   def __init__(self, url, attributes={}):
-    self.url = url
+    parsed_url = urlparse.urlparse(url)
+    if parse_url.scheme == None:
+      raise Exception("urls must be fully qualified: %s" % url)
     self.interactions = 'scroll'
     self.credentials = None
     self.wait_time_after_navigate = 2
