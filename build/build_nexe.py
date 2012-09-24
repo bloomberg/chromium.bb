94104b502ce6e846d5d7bd2a8c4332576dd5cbe7
thakis@chromium.org <thakis@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-09-24 04:57:39 +0000

    Add -Werror to the flags used by build_nexe.py.
    BUG=chromium:148648
    Review URL: https://codereview.chromium.org/10968057/
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@9816 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: e445fc28c69e9276340128dfd8d12be0eaef2741

--- build/build_nexe.py
+++ build/build_nexe.py
@@ -207,6 +207,7 @@ class Builder(object):
                            'WINDOWS' in define or
                            'WINVER' in define)]
     options += ['-D' + define for define in define_list]
+    options.append('-Werror')
     self.compile_options = options + ['-I' + name for name in self.inc_paths]
 
   def BuildLinkOptions(self, options):
