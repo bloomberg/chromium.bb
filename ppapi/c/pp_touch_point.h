5939658c74b350a65673e7b0475c5e4d268db1bb
jond@chromium.org <jond@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-25 15:47:11 +0000

    Updated pp_touch_point.h docs in preparation for fixing the C++ docs (which are a bit lean). Review URL: https://codereview.chromium.org/10956037
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158574 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: af0bb0644f726c9442d6b141ef9c126d40385348

--- ppapi/c/pp_touch_point.h
+++ ppapi/c/pp_touch_point.h
@@ -3,7 +3,7 @@
  * found in the LICENSE file.
  */
 
-/* From pp_touch_point.idl modified Thu Jun 21 16:46:17 2012. */
+/* From pp_touch_point.idl modified Fri Sep 21 10:18:48 2012. */
 
 #ifndef PPAPI_C_PP_TOUCH_POINT_H_
 #define PPAPI_C_PP_TOUCH_POINT_H_
@@ -14,7 +14,8 @@
 
 /**
  * @file
- * This file defines the API to create a touch-point.
+ * This file defines the API to create a touch point or position where fingers
+ * makes contact with touch screen device.
  */
 
 
@@ -23,39 +24,39 @@
  * @{
  */
 /**
- * The <code>PP_TouchPoint</code> represents all information about a single
- * touch point, such ase position, id, rotation angle, and pressure.
+ * The <code>PP_TouchPoint</code> struct represents all information about a
+ * single touch point, such ase position, id, rotation angle, and pressure.
  */
 struct PP_TouchPoint {
   /**
-   * The identifier for this TouchPoint. This corresponds to the order
-   * in which the points were pressed. For example, the first point to be
-   * pressed has an id of 0, the second has an id of 1, and so on. An id can be
-   * reused when a touch point is released.  For example, if two fingers are
-   * down, with id 0 and 1, and finger 0 releases, the next finger to be
-   * pressed can be assigned to id 0.
+   * This value represents the identifier for this TouchPoint. The id
+   * corresponds to the order in which the points were pressed. For example,
+   * the first point to be pressed has an id of 0, the second has an id of 1,
+   * and so on. An id can be reused when a touch point is released.  For
+   * example, if two fingers are down, with id 0 and 1, and finger 0 releases,
+   * the next finger to be pressed can be assigned to id 0.
    */
   uint32_t id;
   /**
-   * The x-y pixel position of this TouchPoint, relative to the upper-left of
-   * the instance receiving the event.
+   * This value represents the x and y pixel position of this TouchPoint
+   * relative to the upper-left of the module instance receiving the event.
    */
   struct PP_FloatPoint position;
   /**
-   * The elliptical radii, in screen pixels, in the x and y direction of this
-   * TouchPoint.
+   * This value represents the elliptical radii, in screen pixels, in the x
+   * and y direction of this TouchPoint.
    */
   struct PP_FloatPoint radius;
   /**
-   * The angle of rotation in degrees of the elliptical model of this TouchPoint
-   * clockwise from "up."
+   * This value represents the angle of rotation in degrees of the elliptical
+   * model of this TouchPoint clockwise from "up."
    */
   float rotation_angle;
   /**
-   * The pressure applied to this TouchPoint.  This is typically a
-   * value between 0 and 1, with 0 indicating no pressure and 1 indicating
-   * some maximum pressure, but scaling differs depending on the hardware and
-   * the value is not guaranteed to stay within that range.
+   * This value represents the pressure applied to this TouchPoint.  This value
+   * is typically between 0 and 1, with 0 indicating no pressure and 1
+   * indicating some maximum pressure. Scaling differs depending on the
+   * hardware and the value is not guaranteed to stay within that range.
    */
   float pressure;
 };
