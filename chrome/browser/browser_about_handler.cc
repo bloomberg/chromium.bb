5b05b07cb52bc5d0b0df8d417c05b335b88e7e49
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-25 14:44:54 +0000

    Revert r158555 "[Android] Exclude unsupported URLs from chrome://chrome-urls."
    This broke unit_tests on android_dbg_triggered_tests.
    
    TBR=newt@chromium.org
    NOTRY=true
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10993005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158568 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 20cd74f9317b37e8faee699dfdf7dcf2a7eea12c

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -23,40 +23,41 @@ namespace {
 const char* const kPaths[] = {
   chrome::kChromeUIAppCacheInternalsHost,
   chrome::kChromeUIBlobInternalsHost,
+  chrome::kChromeUIBookmarksHost,
   chrome::kChromeUICacheHost,
   chrome::kChromeUIChromeURLsHost,
   chrome::kChromeUICrashesHost,
   chrome::kChromeUICreditsHost,
   chrome::kChromeUIDNSHost,
+  chrome::kChromeUIDownloadsHost,
+  chrome::kChromeUIExtensionsHost,
+  chrome::kChromeUIFlagsHost,
+  chrome::kChromeUIFlashHost,
   chrome::kChromeUIGpuInternalsHost,
   chrome::kChromeUIHistoryHost,
   chrome::kChromeUIIPCHost,
+  chrome::kChromeUIInspectHost,
   chrome::kChromeUIMediaInternalsHost,
   chrome::kChromeUIMemoryHost,
+  chrome::kChromeUINaClHost,
   chrome::kChromeUINetInternalsHost,
   chrome::kChromeUINetworkViewCacheHost,
   chrome::kChromeUINewTabHost,
   chrome::kChromeUIOmniboxHost,
+  chrome::kChromeUIPluginsHost,
+  chrome::kChromeUIPolicyHost,
   chrome::kChromeUIPredictorsHost,
   chrome::kChromeUIProfilerHost,
   chrome::kChromeUIQuotaInternalsHost,
+  chrome::kChromeUISettingsHost,
   chrome::kChromeUIStatsHost,
   chrome::kChromeUISyncInternalsHost,
-  chrome::kChromeUITermsHost,
-  chrome::kChromeUIVersionHost,
-#if defined(OS_ANDROID)
-  chrome::kChromeUIWelcomeHost,
-#else
-  chrome::kChromeUIBookmarksHost,
-  chrome::kChromeUIDownloadsHost,
-  // TODO(dfalcantara): Enable after http://crbug.com/143146 is fixed.
-  chrome::kChromeUIFlagsHost,
-  chrome::kChromeUIFlashHost,
-  chrome::kChromeUIInspectHost,
-  chrome::kChromeUIPluginsHost,
-  chrome::kChromeUISettingsHost,
-  chrome::kChromeUITracingHost,
+#if defined(OS_CHROMEOS)
+  chrome::kChromeUITaskManagerHost,
 #endif
+  chrome::kChromeUITermsHost,
+  chrome::kChromeUITracingHost,
+  chrome::kChromeUIVersionHost,
 #if defined(OS_WIN)
   chrome::kChromeUIConflictsHost,
 #endif
@@ -78,17 +79,7 @@ const char* const kPaths[] = {
   chrome::kChromeUIOSCreditsHost,
   chrome::kChromeUIProxySettingsHost,
   chrome::kChromeUISystemInfoHost,
-  chrome::kChromeUITaskManagerHost,
   chrome::kChromeUIWallpaperHost,
-#endif
-#if !defined(DISABLE_NACL)
-  chrome::kChromeUINaClHost,
-#endif
-#if defined(ENABLE_CONFIGURATION_POLICY)
-  chrome::kChromeUIPolicyHost,
-#endif
-#if defined(ENABLE_EXTENSIONS)
-  chrome::kChromeUIExtensionsHost,
 #endif
 #if defined(ENABLE_PRINTING)
   chrome::kChromeUIPrintHost,
