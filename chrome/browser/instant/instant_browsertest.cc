74443bc286a9073c64554e7973eea2b1bb74fafb
sreeram@chromium.org <sreeram@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-25 03:19:20 +0000

    Maybe fix InstantTest.NewWindowDismissesInstant on Mac.
    This test times out sporadically on mac_rel (see review thread for links
    to test logs). Local testing seems to suggest that this is because the
    omnibox in the old browser window never loses focus, and that adding a
    RunAllPending() call may fix it.
    
    I've not been able to reproduce the original failure locally, so this is
    a prospective fix, which will be confirmed by this test not timing out
    on the bots anymore.
    
    BUG=none
    R=jennb@chromium.org
    TEST=Watch the bots, as described above.
    
    Review URL: https://chromiumcodereview.appspot.com/10981003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158513 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 50d9dd812a6238daab5624f6512553c8d81918e8

--- chrome/browser/instant/instant_browsertest.cc
+++ chrome/browser/instant/instant_browsertest.cc
@@ -828,6 +828,7 @@ IN_PROC_BROWSER_TEST_F(InstantTest, History) {
 IN_PROC_BROWSER_TEST_F(InstantTest, MAYBE_NewWindowDismissesInstant) {
   ASSERT_NO_FATAL_FAILURE(SetupInstant("instant.html"));
   EXPECT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
+  base::RunLoop().RunUntilIdle();
   SetOmniboxTextAndWaitForInstantToShow("search");
 
   Browser* previous_window = browser();
