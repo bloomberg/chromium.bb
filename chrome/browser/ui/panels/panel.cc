558889814973e432d7abcf6246dfa2a5c0aa1f4d
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-26 03:45:58 +0000

    Fix bug 152277: [Panels] Panels are still showing taskbar icons in smaller resolution on Windows
    The test is added at http://codereview.chromium.org/10987037/ that contains all the test cases for more than this bug. We want to separate out the fix and the remaining complicated test such that we could merge the fix to the branch if needed.
    
    BUG=152277
    TEST=Test will be added in next patch
    
    Review URL: https://chromiumcodereview.appspot.com/10979023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158739 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 63a8954dd703a1ecd37e2b9c90b98dfd43f0a3a6

--- chrome/browser/ui/panels/panel.cc
+++ chrome/browser/ui/panels/panel.cc
@@ -819,10 +819,10 @@ void Panel::UpdateAppIcon() {
   app_icon_loader_.reset(new ImageLoadingTracker(this));
   app_icon_loader_->LoadImage(
       extension,
-      extension->GetIconResource(extension_misc::EXTENSION_ICON_SMALLISH,
+      extension->GetIconResource(extension_misc::EXTENSION_ICON_SMALL,
                                  ExtensionIconSet::MATCH_BIGGER),
-      gfx::Size(extension_misc::EXTENSION_ICON_SMALLISH,
-                extension_misc::EXTENSION_ICON_SMALLISH),
+      gfx::Size(extension_misc::EXTENSION_ICON_SMALL,
+                extension_misc::EXTENSION_ICON_SMALL),
       ImageLoadingTracker::CACHE);
 }
 
