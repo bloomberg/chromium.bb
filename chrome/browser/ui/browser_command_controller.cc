f2a84c1d7067917fe62ee7f4d82d7d2aba210ec2
simon.hong81@gmail.com <simon.hong81@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-26 16:21:23 +0000

    Disable history menu in incognito or guest mode
    BUG=127627
    TEST=manual test
    
    
    Review URL: https://chromiumcodereview.appspot.com/10990026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158808 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0b083cdfe51ed571d697f96ca5ba7756e9af5ed2

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -834,7 +834,8 @@ void BrowserCommandController::InitCommandState() {
   command_updater_.UpdateCommandEnabled(IDC_CREATE_SHORTCUTS, false);
   UpdateCommandsForDevTools();
   command_updater_.UpdateCommandEnabled(IDC_TASK_MANAGER, CanOpenTaskManager());
-  command_updater_.UpdateCommandEnabled(IDC_SHOW_HISTORY, true);
+  command_updater_.UpdateCommandEnabled(IDC_SHOW_HISTORY,
+                                        !Profile::IsGuestSession());
   command_updater_.UpdateCommandEnabled(IDC_SHOW_DOWNLOADS, true);
   command_updater_.UpdateCommandEnabled(IDC_HELP_PAGE_VIA_KEYBOARD, true);
   command_updater_.UpdateCommandEnabled(IDC_HELP_PAGE_VIA_MENU, true);
