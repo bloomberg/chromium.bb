8c3e8aeec7375958e729e90fd755e90e24c73d51
cduvall@chromium.org <cduvall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-27 23:48:26 +0000

    Extensions Docs Server: Fix headings with no IDs
    PRESUBMIT.py will check for headings with no IDs, and all headings that didn't
    have IDs now have IDs.
    
    BUG=149146
    
    Review URL: https://codereview.chromium.org/10993029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159156 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b854830650109bfa77961f50830f6f516562f13a

--- chrome/common/extensions/PRESUBMIT.py
+++ chrome/common/extensions/PRESUBMIT.py
@@ -73,8 +73,23 @@ def _CreateIntegrationTestArgs(affected_files):
                                          _ListFilesInPublic()))
   return args
 
+def _CheckHeadingIDs(input_api):
+  ids_re = re.compile('<h[23].*id=.*?>')
+  headings_re = re.compile('<h[23].*?>')
+  bad_files = []
+  for name in input_api.AbsoluteLocalPaths():
+    if (fnmatch.fnmatch(name, '*%s*' % INTROS_PATH) or
+        fnmatch.fnmatch(name, '*%s*' % ARTICLES_PATH)):
+      contents = input_api.ReadFile(name)
+      if (len(re.findall(headings_re, contents)) !=
+          len(re.findall(ids_re, contents))):
+        bad_files.append(name)
+  return bad_files
+
 def _CheckChange(input_api, output_api):
-  results = []
+  results = [
+      output_api.PresubmitError('File %s needs an id for each heading.' % name)
+      for name in _CheckHeadingIDs(input_api)]
   try:
     integration_test = []
     # From depot_tools/presubmit_canned_checks.py:529
