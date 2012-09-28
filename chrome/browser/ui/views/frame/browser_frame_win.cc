b292521f0cd8d3533d678f9dd9b5e4b687e337e0
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-28 01:54:12 +0000

    Ensure that the browser window is correctly activated in metro mode. In the BrowserFrameWin::Activate override, in metro we call Show because we can have only one window active at any given point. However the default Show internally calls the ShowWindow API with SW_SHOWNOACTIVATE flag which results in it losing keyboard focus at times.
    Fix is to call the ShowWithWindowState function instead.
    
    BUG=151136
    R=sky
    Review URL: https://codereview.chromium.org/10977067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159175 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7e472d653565a05731a623b35f17f6656723eb41

--- chrome/browser/ui/views/frame/browser_frame_win.cc
+++ chrome/browser/ui/views/frame/browser_frame_win.cc
@@ -354,9 +354,9 @@ void BrowserFrameWin::Activate() {
   // The Activate code path is typically called when a new browser window is
   // being activated. In metro we need to ensure that the window currently
   // being displayed is hidden and the new window being activated becomes
-  // visible. This is achieved by calling Show.
+  // visible. This is achieved by calling ShowWithWindowState.
   if (base::win::IsMetroProcess()) {
-    Show();
+    ShowWithWindowState(ui::SHOW_STATE_NORMAL);
   } else {
     views::NativeWidgetWin::Activate();
   }
