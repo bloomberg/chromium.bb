a71fd48c7c06e99ea2643ac3ea21cc6fe2f5e8db
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-02 20:34:43 +0000

    Land 10991060 for Charles.
    Text of original CL (fixed one typo):
    
    == BEGIN ==
    Import of github calculator example to start fork.
    
    Forking the chrome-app-samples calculator from
    https://github.com/GoogleChrome/chrome-app-samples/tree/master/calculator
    to the chromium source tree so browser tests can be written for it and
    bug fixes are protected by these tests. This is an initial verbatim
    import of the calculator example as it is on GitHub now (GitHub commit
    6b441f2d423c1ec23b39afa5bdc974328068e1c5).
    
    BUG=150880
    == END ==
    
    TBR=mihaip@chromium.org
    TBR=aa@chromium.org
    
    Review URL: https://codereview.chromium.org/11046005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159754 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 03c956ae04e3802df6afc9ac5fe1dad8737da2db

---
+++ chrome/common/extensions/docs/examples/apps/calculator/README.md
@@ -0,0 +1,13 @@
+# Calculator
+
+A sample application that provides a simple calculator. Supports basic operations
+such as addition, multiplication, subtraction and division.
+
+This sample also incorporates an MVC-style structure and requires jQuery for
+DOM manipulation.
+
+## APIs
+
+* [Runtime](http://developer.chrome.com/trunk/apps/app.runtime.html)
+* [Window](http://developer.chrome.com/trunk/apps/app.window.html)
+
---
+++ chrome/common/extensions/docs/examples/apps/calculator/main.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2012 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ **/
+
+
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/trunk/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('calculator.html', {
+    width: 244,
+    height: 380,
+    maxWidth: 244,
+    minWidth: 244,
+    minHeight: 380,
+    maxHeight: 380,
+  });
+});
