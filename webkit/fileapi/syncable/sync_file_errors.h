f1dc522706b22bd3834055312861da11c5115994
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 10:30:13 +0000

    Adding error code for SyncFileSystem feature
    BUG=153526
    TEST=n/a
    
    Review URL: https://codereview.chromium.org/11051007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159878 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 574a0791a2325fc034a8c5f220e67195ab7d881b

---
+++ webkit/fileapi/syncable/sync_file_errors.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBKIT_FILEAPI_SYNCABLE_SYNC_FILE_ERRORS_H_
+#define WEBKIT_FILEAPI_SYNCABLE_SYNC_FILE_ERRORS_H_
+
+namespace fileapi {
+
+enum SyncFileError {
+  // Basic ones that could be directly mapped to PlatformFileError.
+  SYNC_FILE_OK = 0,
+  SYNC_FILE_ERROR_FAILED = -1,
+  SYNC_FILE_ERROR_IN_USE = -2,
+  SYNC_FILE_ERROR_EXISTS = -3,
+  SYNC_FILE_ERROR_NOT_FOUND = -4,
+  SYNC_FILE_ERROR_ACCESS_DENIED = -5,
+  SYNC_FILE_ERROR_TOO_MANY_OPENED = -6,
+  SYNC_FILE_ERROR_NO_MEMORY = -7,
+  SYNC_FILE_ERROR_NO_SPACE = -8,
+  SYNC_FILE_ERROR_NOT_A_DIRECTORY = -9,
+  SYNC_FILE_ERROR_INVALID_OPERATION = -10,
+  SYNC_FILE_ERROR_SECURITY = -11,
+  SYNC_FILE_ERROR_ABORT = -12,
+  SYNC_FILE_ERROR_NOT_A_FILE = -13,
+  SYNC_FILE_ERROR_NOT_EMPTY = -14,
+  SYNC_FILE_ERROR_INVALID_URL = -15,
+
+  // Database related errors.
+  SYNC_FILE_ERROR_DATABASE_NOT_FOUND = -16,
+  SYNC_FILE_ERROR_DATABASE_CORRUPTION = -17,
+  SYNC_FILE_ERROR_DATABASE_IO_ERROR = -18,
+};
+
+}  // namespace fileapi
+
+#endif  // WEBKIT_FILEAPI_SYNCABLE_SYNC_FILE_ERRORS_H_
