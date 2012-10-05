bc92e900d7382b694dc5ec9d1ab4e168742e8dc0
torne@chromium.org <torne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-05 14:29:10 +0000

    Android: Add list of files with licenses not wanted in webview.
    The script which merges Chromium code into the Android tree needs to
    know which directories to remove for licensing reasons; it's much easier
    if this is just a static list in the tree which can subsequently be
    validated with the webview_licences tool. The list should change very
    rarely.
    
    R=mnaganov@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/11040056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160369 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 31fff75ff499604453cd3cd07ad75496f5e0d222

---
+++ android_webview/tools/known_incompatible.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""List of known-incompatibly-licensed directories for Android WebView.
+
+This is not used by the webview_licenses tool itself; it is effectively a
+"cache" of the output of webview_licenses.GetIncompatibleDirectories() for the
+subset of repositories that WebView needs.
+
+We store a copy here because GetIncompatibleDirectories() doesn't work properly
+after things have been removed from the tree - it can no longer see the
+README.chromium files for previously-removed directories, but they may have
+newly added files in them. As long as this list is up to date, we can remove the
+things listed first, and then just run the tool afterwards to validate that it
+was sufficient. If the tool returns any extra directories then the snapshotting
+process will stop and this list must be updated.
+
+"""
+
+KNOWN_INCOMPATIBLE = [
+    'base/third_party/xdg_mime',
+    'breakpad',
+    'chrome/installer/mac/third_party/xz',
+    'chrome/test/data',
+    'third_party/active_doc',
+    'third_party/apple_apsl',
+    'third_party/apple_sample_code',
+    'third_party/bsdiff',
+    'third_party/bspatch',
+    'third_party/sudden_motion_sensor',
+    'third_party/swiftshader',
+    'third_party/talloc',
+    'third_party/webdriver',
+    'third_party/wtl',
+    'tools/chrome_remote_control/third_party/websocket-client',
+]
