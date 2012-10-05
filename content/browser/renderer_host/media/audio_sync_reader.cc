e90f9c9ea896a1f3b7a6336e533c848b1dad6fdf
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-05 11:52:55 +0000

    Add WASAPI-based unified audio I/O back-end for Windows 7.
    Implementation of AudioOuputStream for Windows using the WASAPI API.
    Both input and output device must use the same sample rate.
    
    Corresponding implementation for Mac OS X is given by http://codereview.chromium.org/10916105/.
    
    Additional test using full Chrome:
    http://webaudiodemos.appspot.com/input/index.html
    
    Local tests have been performed using a wide range of audio devices and sample rates. It has also been verified that the QoS is good between two different devices as well as long as they both run at the same sample rate.
    
    12 hour test of http://webaudiodemos.appspot.com/input/index.html worked well.
    
    Verified that we log a warning "Unified audio I/O is not supported." if the user has selected different sample rates for in and out.
    
    BUG=145092
    TEST=media_unittests.exe --gtest_filter=WASAPIUnified* --enable-webaudio-input
    
    Review URL: https://codereview.chromium.org/10959068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160346 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e75fe3ded56552f45e19c058e03794bc488429f6

--- content/browser/renderer_host/media/audio_sync_reader.cc
+++ content/browser/renderer_host/media/audio_sync_reader.cc
@@ -25,17 +25,18 @@ AudioSyncReader::AudioSyncReader(base::SharedMemory* shared_memory,
     : shared_memory_(shared_memory),
       input_channels_(input_channels) {
   packet_size_ = media::PacketSizeInBytes(shared_memory_->created_size());
-  DCHECK_EQ(packet_size_, AudioBus::CalculateMemorySize(params));
-  output_bus_ = AudioBus::WrapMemory(params, shared_memory->memory());
-
+  int input_memory_size = 0;
+  int output_memory_size = AudioBus::CalculateMemorySize(params);
   if (input_channels_ > 0) {
     // The input storage is after the output storage.
-    int output_memory_size = AudioBus::CalculateMemorySize(params);
     int frames = params.frames_per_buffer();
+    input_memory_size = AudioBus::CalculateMemorySize(input_channels_, frames);
     char* input_data =
         static_cast<char*>(shared_memory_->memory()) + output_memory_size;
     input_bus_ = AudioBus::WrapMemory(input_channels_, frames, input_data);
   }
+  DCHECK_EQ(packet_size_, output_memory_size + input_memory_size);
+  output_bus_ = AudioBus::WrapMemory(params, shared_memory->memory());
 }
 
 AudioSyncReader::~AudioSyncReader() {
