5bbb9be90610c7d03ff7f16946a6f1c95a605215
jorgelo@chromium.org <jorgelo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-08 20:46:44 +0000

    Allow mremap() for the Seccomp-BPF sandbox in renderers.
    WebKit string handling uses realloc(), which for some glibc implementations
    may call mremap().
    
    BUG=149834
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/11073003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160697 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c3e8fec7303b52d9a07d153fafaa7849b4dcb041

--- content/common/sandbox_seccomp_bpf_linux.cc
+++ content/common/sandbox_seccomp_bpf_linux.cc
@@ -1248,6 +1248,7 @@ ErrorCode RendererOrWorkerProcessPolicy_x86_64(int sysno) {
 #if defined(__i386__) || defined(__x86_64__)
     case __NR_getrlimit:
 #endif
+    case __NR_mremap:   // See crbug.com/149834.
     case __NR_pread64:
     case __NR_pwrite64:
     case __NR_sched_get_priority_max:
