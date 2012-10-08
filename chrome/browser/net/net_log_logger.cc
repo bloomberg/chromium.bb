aa39ffe24808427386b8d9213e66a82630af4e4b
gauravsh@chromium.org <gauravsh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-08 22:01:24 +0000

    Do not crash if the file specified for net logging can not be opened for writing
    Instead, log an error and bail out.
    
    BUG=chromium:154382
    TEST=chrome --log-net-log=/invalid/path no longer crashes
    
    
    Review URL: https://chromiumcodereview.appspot.com/11096003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160718 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4f8db9ae5dccb00bbfa02929683b9447c2b4a9af

--- chrome/browser/net/net_log_logger.cc
+++ chrome/browser/net/net_log_logger.cc
@@ -17,7 +17,13 @@
 NetLogLogger::NetLogLogger(const FilePath &log_path) {
   if (!log_path.empty()) {
     base::ThreadRestrictions::ScopedAllowIO allow_io;
-    file_.Set(file_util::OpenFile(log_path, "w"));
+    FILE* fp = file_util::OpenFile(log_path, "w");
+    if (!fp) {
+      LOG(ERROR) << "Could not open file " << log_path.value()
+                 << " for net logging";
+      return;
+    }
+    file_.Set(fp);
 
     // Write constants to the output file.  This allows loading files that have
     // different source and event types, as they may be added and removed
