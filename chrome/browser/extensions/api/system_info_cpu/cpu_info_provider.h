85b294c5d4500d09f2132bf23789192078b85827
hongbo.min@intel.com <hongbo.min@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 04:15:43 +0000

    Add systemInfo.cpu.onUpdated event implementation.
    BUG=136519
    TEST=browser_tests --gtest_filter=SystemInfoCpuApiTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/10905171
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161499 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7a62b93e02290354e100d048c177014e500247e6

--- chrome/browser/extensions/api/system_info_cpu/cpu_info_provider.h
+++ chrome/browser/extensions/api/system_info_cpu/cpu_info_provider.h
@@ -8,16 +8,22 @@
 
 #include <vector>
 
+#include "base/timer.h"
 #include "chrome/common/extensions/api/experimental_system_info_cpu.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 
 namespace extensions {
 
 class CpuInfoProvider
-    : public SystemInfoProvider<api::experimental_system_info_cpu::CpuInfo> {
+    : public content::NotificationObserver,
+      public SystemInfoProvider<api::experimental_system_info_cpu::CpuInfo> {
  public:
-  typedef base::Callback<api::experimental_system_info_cpu::CpuUpdateInfo>
-      QueryCpuTimeCallback;
-  virtual ~CpuInfoProvider() {}
+  typedef base::Callback<
+      void(scoped_ptr<api::experimental_system_info_cpu::CpuUpdateInfo>)>
+          SamplingCallback;
+
+  virtual ~CpuInfoProvider();
 
   // Overriden from SystemInfoProvider<CpuInfo>.
   virtual bool QueryInfo(
@@ -25,28 +31,64 @@ class CpuInfoProvider
 
   // Start sampling the CPU usage. The callback gets called when one sampling
   // cycle is completed periodically with the CPU updated usage info for each
-  // processors. Return true if it succeeds to start, otherwise, false is
-  // returned.
-  bool StartSampling(const QueryCpuTimeCallback& callback);
+  // processors. It gets called on UI thread, the |callback| gets called on the
+  // FILE thread.
+  void StartSampling(const SamplingCallback& callback);
 
-  // Stop the sampling cycle. Return true if it succeeds to stop.
-  bool StopSampling();
+  // Stop the sampling cycle. Called on the FILE thread.
+  void StopSampling();
 
   // Return the single shared instance of CpuInfoProvider.
   static CpuInfoProvider* Get();
 
  private:
+  friend class SystemInfoProvider<api::experimental_system_info_cpu::CpuInfo>;
+  friend class MockCpuInfoProviderImpl;
+
   // The amount of time that CPU spent on performing different kinds of work.
   // It is used to calculate the usage percent for processors.
   struct CpuTime {
+    CpuTime() : user(0), kernel(0), idle(0) {}
     int64 user;   // user mode.
     int64 kernel; // kernel mode.
     int64 idle;   // twiddling thumbs.
   };
 
+  CpuInfoProvider();
+
+  // content::NotificationObserver implementation.
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
+
   // Platform specific implementation for querying the CPU time information
   // for each processor.
-  bool QueryCpuTimePerProcessor(std::vector<CpuTime>* times);
+  virtual bool QueryCpuTimePerProcessor(std::vector<CpuTime>* times);
+
+  // Start and stop sampling on the FILE thread.
+  void StartSamplingOnFileThread(const SamplingCallback& callback);
+  void StopSamplingOnFileThread();
+
+  // Called when the sampling timer is triggered.
+  void DoSample();
+
+  content::NotificationRegistrar registrar_;
+
+  // Indicates whether the CPU sampling is started.
+  bool is_sampling_started_;
+
+  // The sampling value returned from the most recent QueryCpuTimePerProcessor
+  // call.
+  std::vector<CpuTime> baseline_cpu_time_;
+
+  // The callback which will be called when one sampling cycle is completed.
+  SamplingCallback callback_;
+
+  // The timer used for polling CPU time periodically. Lives on FILE thread.
+  base::RepeatingTimer<CpuInfoProvider>* sampling_timer_;
+
+  // The time interval for sampling, in milliseconds.
+  int sampling_interval_;
 };
 
 }  // namespace extensions
---
+++ chrome/browser/extensions/api/system_info_cpu/cpu_info_provider_android.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/api/system_info_cpu/cpu_info_provider.h"
+
+namespace extensions {
+
+bool CpuInfoProvider::QueryCpuTimePerProcessor(std::vector<CpuTime>* times) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+}  // namespace extensions
