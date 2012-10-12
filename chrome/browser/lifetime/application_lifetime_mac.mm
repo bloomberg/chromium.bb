49925f3ef35191cb3be651a9dda6b1fc8ce1c65b
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 15:26:51 +0000

    [Mac] Do not post NSApplicationWillTerminateNotification via a task, do it immediately.
    BUG=155318,153283
    TEST=Crash server numbers.
    
    Review URL: https://codereview.chromium.org/11120002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161578 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dad42977db2d6e24d2da436e818266134466d27e

--- chrome/browser/lifetime/application_lifetime_mac.mm
+++ chrome/browser/lifetime/application_lifetime_mac.mm
@@ -8,37 +8,24 @@
 
 #include "chrome/browser/lifetime/application_lifetime.h"
 
-#include "base/bind.h"
-#include "base/message_loop.h"
+#include "base/logging.h"
 #include "chrome/browser/browser_shutdown.h"
 #import "chrome/browser/chrome_browser_application_mac.h"
 
 namespace browser {
 
-namespace {
-
-// A closure that is used to finalize application shutdown.
-void PerformTerminate() {
-  [[NSNotificationCenter defaultCenter]
-      postNotificationName:NSApplicationWillTerminateNotification
-                    object:NSApp];
-}
-
-}  // namespace
-
 // At this point, the user is trying to quit (or the system is forcing the
 // application to quit) and all browsers have been successfully closed. The
 // final step in shutdown is to post the NSApplicationWillTerminateNotification
-// to end the -[NSApplication run] event loop. That is performed via the
-// MessageLoop, since this can be called via the destruction path of the last
-// Browser object.
+// to end the -[NSApplication run] event loop.
 void HandleAppExitingForPlatform() {
   static bool kill_me_now = false;
   CHECK(!kill_me_now);
   kill_me_now = true;
 
-  MessageLoop::current()->PostNonNestableTask(FROM_HERE,
-      base::Bind(&PerformTerminate));
+  [[NSNotificationCenter defaultCenter]
+      postNotificationName:NSApplicationWillTerminateNotification
+                    object:NSApp];
 }
 
 }  // namespace browser
