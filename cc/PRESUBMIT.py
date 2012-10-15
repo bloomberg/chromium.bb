51bf0d496e9b8564535730f517232475f5a4a211
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-15 16:41:58 +0000

    Add a PRESUBMIT.py for cc/ that specifies default try bots.
    The default bots build and run on linux debug in order to
    verify assertions are working as intended.
    
    We just run the unittests that test the cc/ code
    specifically. And we also run linux_layout_rel in order to
    get equal coverage of LayoutTests as we had from the WebKit
    EWS bots (though with more noise).
    
    Review URL: https://chromiumcodereview.appspot.com/11091047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161871 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 69085bc591f70cad8c7c8662c678f07602aa976c

---
+++ cc/PRESUBMIT.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Top-level presubmit script for cc.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts for
+details on the presubmit API built into gcl.
+"""
+
+def GetPreferredTrySlaves(project, change):
+  return [
+    'linux_layout_rel',
+    ]
