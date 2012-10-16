263f8d8b76d1fb91380574b9683fa63418face82
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-16 08:14:44 +0000

    Re-land 162026: White-list the two ssh client webstore ids besides quickoffice.
    Re-land with TBR because the patch was merged into M23 and was not reverted there.
    
    BUG= http://code.google.com/p/chromium-os/issues/detail?id=35298
    TBR=robertm@gogole.com
    
    Review URL: https://codereview.chromium.org/11166004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162102 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8d2f148e025c7ed9265561151a6f3a17a1fa197c

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -579,6 +579,19 @@ GURL ChromeContentRendererClient::GetNaClContentHandlerURL(
   return GURL();
 }
 
+// TODO(dschuff): remove this when ARM ABI is stable
+#if defined(__arm__) && defined(OS_CHROMEOS)
+static bool IsWhiteListedARM(const GURL& url) {
+  return
+    // QuickOffice
+    url.host() == "gbkeegbaiigmenfmjfclcdgdpimamgkj" ||
+    // ssh dev
+    url.host() == "okddffdblfhhnmhodogpojmfkjmhinfp" ||
+    // ssh stable
+    url.host() == "pnhechapfaindjhompbnflcldabbghjo";
+}
+#endif
+
 //  static
 bool ChromeContentRendererClient::IsNaClAllowed(
     const GURL& manifest_url,
@@ -599,15 +612,15 @@ bool ChromeContentRendererClient::IsNaClAllowed(
   // under development, invocations from whitelisted URLs, and all invocations
   // if --enable-nacl is set.
   bool is_nacl_allowed =
-#if defined(__arm__)
+#if defined(__arm__) && defined(OS_CHROMEOS)
     // The ARM ABI is not quite stable, so only allow NaCl for
     // unrestricted extensions (i.e. built-in and under development),
-    // and for the QuickOffice webstore app.
+    // and for certain whitelisted webstore apps.
     // See http://crbug.com/145694
     // TODO(dschuff): remove this when the ABI is stable
     (is_extension_from_webstore &&
      manifest_url.SchemeIs("chrome-extension") &&
-     manifest_url.host() == "gbkeegbaiigmenfmjfclcdgdpimamgkj") ||
+     IsWhiteListedARM(manifest_url)) ||
 #else
     is_extension_from_webstore ||
     is_whitelisted_url ||
