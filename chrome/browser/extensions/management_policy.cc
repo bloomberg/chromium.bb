d3c3c56607610599204a2ee7038639ebd610f863
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-16 09:50:13 +0000

    Cleanup: Fix a formatting issue in extensions::ManagementPolicy and combine some common code.
    Review URL: https://chromiumcodereview.appspot.com/11093060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162112 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1397c1bbb9e365fd97234b41f57a99cf89afd56a

--- chrome/browser/extensions/management_policy.cc
+++ chrome/browser/extensions/management_policy.cc
@@ -5,6 +5,19 @@
 #include "chrome/browser/extensions/management_policy.h"
 
 namespace extensions {
+
+namespace {
+
+void GetExtensionNameAndId(const Extension* extension,
+                           std::string* name,
+                           std::string* id) {
+  // The extension may be NULL in testing.
+  *id = extension ? extension->id() : "[test]";
+  *name = extension ? extension->name() : "test";
+}
+
+}  // namespace
+
 ManagementPolicy::ManagementPolicy() {
 }
 
@@ -37,13 +50,13 @@ void ManagementPolicy::UnregisterProvider(Provider* provider) {
 bool ManagementPolicy::UserMayLoad(const Extension* extension,
                                    string16* error) const {
   for (ProviderList::const_iterator it = providers_.begin();
-      it != providers_.end(); ++it) {
+       it != providers_.end(); ++it) {
     if (!(*it)->UserMayLoad(extension, error)) {
-      // The extension may be NULL in testing.
-      std::string id = extension ? extension->id() : "[test]";
-      std::string name = extension ? extension->name() : "test";
+      std::string id;
+      std::string name;
+      GetExtensionNameAndId(extension, &name, &id);
       DLOG(WARNING) << "Installation of extension " << name
-                    << "( " << id << ")"
+                    << " (" << id << ")"
                     << " prohibited by " << (*it)->GetDebugPolicyProviderName();
       return false;
     }
@@ -54,13 +67,13 @@ bool ManagementPolicy::UserMayLoad(const Extension* extension,
 bool ManagementPolicy::UserMayModifySettings(const Extension* extension,
                                              string16* error) const {
   for (ProviderList::const_iterator it = providers_.begin();
-      it != providers_.end(); ++it) {
+       it != providers_.end(); ++it) {
     if (!(*it)->UserMayModifySettings(extension, error)) {
-      // The extension may be NULL in testing.
-      std::string id = extension ? extension->id() : "[test]";
-      std::string name = extension ? extension->name() : "test";
+      std::string id;
+      std::string name;
+      GetExtensionNameAndId(extension, &name, &id);
       DLOG(WARNING) << "Modification of extension " << name
-                    << "( " << id << ")"
+                    << " (" << id << ")"
                     << " prohibited by " << (*it)->GetDebugPolicyProviderName();
       return false;
     }
@@ -71,13 +84,13 @@ bool ManagementPolicy::UserMayModifySettings(const Extension* extension,
 bool ManagementPolicy::MustRemainEnabled(const Extension* extension,
                                          string16* error) const {
   for (ProviderList::const_iterator it = providers_.begin();
-      it != providers_.end(); ++it) {
+       it != providers_.end(); ++it) {
     if ((*it)->MustRemainEnabled(extension, error)) {
-      // The extension may be NULL in testing.
-      std::string id = extension ? extension->id() : "[test]";
-      std::string name = extension ? extension->name() : "test";
+      std::string id;
+      std::string name;
+      GetExtensionNameAndId(extension, &name, &id);
       DLOG(WARNING) << "Extension " << name
-                    << "( " << id << ")"
+                    << " (" << id << ")"
                     << " required to remain enabled by "
                     << (*it)->GetDebugPolicyProviderName();
       return true;
@@ -93,4 +106,5 @@ void ManagementPolicy::UnregisterAllProviders() {
 int ManagementPolicy::GetNumProviders() const {
   return providers_.size();
 }
-}  // namespace
+
+}  // namespace extensions
