b55e1627d15caa005ca8fab810d8303d5df48eb1
torne@chromium.org <torne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 14:27:51 +0000

    Android WebView: create a java module.
    The WebView needs a bunch of Java code from the Chromium tree
    (contentview and its dependencies) in order to build. Introduce an
    Android build system makefile into android_webview which builds a
    suitable static java library.
    
    R=benm@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/11185025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162404 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 979f7a8ef2b439c4e032ae6d777178f847d63e75

--- Android.mk
+++ Android.mk
@@ -3,11 +3,12 @@
 # found in the LICENSE file.
 
 # This Android makefile is used to build WebView in the Android build system.
-# gyp autogenerates the real makefiles, which we just include here if we are
-# doing a WebView build. For other builds, this makefile does nothing, which
-# prevents the Android build system from mistakenly loading any other
+# gyp autogenerates most of the real makefiles, which we just include here if
+# we are doing a WebView build. For other builds, this makefile does nothing,
+# which prevents the Android build system from mistakenly loading any other
 # Android.mk that may exist in the Chromium tree.
 
 ifdef CHROME_ANDROID_BUILD_WEBVIEW
 include $(call my-dir)/GypAndroid.mk
+include $(call my-dir)/android_webview/Android.mk
 endif
