bloomberg/chromium.bb

commit 02d1b9d51392cd76c23755195e9a58a594b8ac0c

Fix drift while scrolling.
Scrolling with one finger (particularly slow scrolling) would exhibit
drift between the scrolled amount and the finger position because
of the use of the badly-defined bounding box center as the starting
position of the scroll. Use the touch down as start position for
single-finger scroll gestures. Tested via unit-test.

BUG=150573


Review URL: https://chromiumcodereview.appspot.com/10958053

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158592 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 48a5d57a35761c6592bfa68902f83ee173adc1bc

ui/base/gestures/gesture_sequence.cc
@@ -38,7 +38,10 @@ enum TouchStatusInternal {
                       // been processed.
 
   TSI_PROCESSED,      // The touch-event should affect gesture-recognition only
-                      // if the touch-event has been processed.
+                      // if the touch-event has been processed. For example,,
+                      // this means that a JavaScript touch handler called
+                      // |preventDefault| on the associated touch event
+                      // or was processed by an aura-window or views-view.
 
   TSI_ALWAYS          // The touch-event should always affect gesture
                       // recognition.
@@ -88,6 +91,9 @@ enum EdgeStateSignatureType {
   // Ignore processed touch-move events until gesture-scroll starts.
   GST_PENDING_SYNTHETIC_CLICK_FIRST_MOVED =
       G(GS_PENDING_SYNTHETIC_CLICK, 0, TS_MOVED, TSI_NOT_PROCESSED),
+
+  GST_PENDING_SYNTHETIC_CLICK_FIRST_MOVED_PROCESSED =
+      G(GS_PENDING_SYNTHETIC_CLICK, 0, TS_MOVED, TSI_PROCESSED),
 
   GST_PENDING_SYNTHETIC_CLICK_FIRST_STATIONARY =
       G(GS_PENDING_SYNTHETIC_CLICK, 0, TS_STATIONARY, TSI_NOT_PROCESSED),
@@ -214,6 +220,7 @@ EdgeStateSignatureType Signature(GestureState gesture_state,
     case GST_PENDING_SYNTHETIC_CLICK_FIRST_RELEASED:
     case GST_PENDING_SYNTHETIC_CLICK_FIRST_RELEASED_HANDLED:
     case GST_PENDING_SYNTHETIC_CLICK_FIRST_MOVED:
+    case GST_PENDING_SYNTHETIC_CLICK_FIRST_MOVED_PROCESSED:
     case GST_PENDING_SYNTHETIC_CLICK_FIRST_STATIONARY:
     case GST_PENDING_SYNTHETIC_CLICK_FIRST_CANCELLED:
     case GST_PENDING_SYNTHETIC_CLICK_SECOND_PRESSED:
@@ -274,10 +281,6 @@ unsigned int ComputeTouchBitmask(const GesturePoint* points) {
 }
 
 float CalibrateFlingVelocity(float velocity) {
-  // TODO(sad|rjkroege): fling-curve is currently configured to work well with
-  // touchpad scroll-events. This curve needs to be adjusted to work correctly
-  // with both touchpad and touchscreen. Until then, scale quadratically.
-  // http://crbug.com/120154
   const float velocity_scaling  =
       GestureConfiguration::touchscreen_fling_acceleration_adjustment();
   return velocity_scaling * velocity;
@@ -380,6 +383,9 @@ GestureSequence::Gestures* GestureSequence::ProcessTouchEventForGesture(
         if (ScrollUpdate(event, point, gestures.get()))
           point.UpdateForScroll();
       }
+      break;
+    case GST_PENDING_SYNTHETIC_CLICK_FIRST_MOVED_PROCESSED:
+      point.UpdateForScroll();
       break;
     case GST_PENDING_SYNTHETIC_CLICK_FIRST_RELEASED_HANDLED:
     case GST_PENDING_SYNTHETIC_CLICK_FIRST_CANCELLED:
@@ -703,9 +709,15 @@ void GestureSequence::AppendScrollGestureEnd(const GesturePoint& point,
 void GestureSequence::AppendScrollGestureUpdate(GesturePoint& point,
                                                 const gfx::Point& location,
                                                 Gestures* gestures) {
-  gfx::Point current_center = bounding_box_.CenterPoint();
-  int dx = current_center.x() - bounding_box_last_center_.x();
-  int dy = current_center.y() - bounding_box_last_center_.y();
+  float dx, dy;
+  if (point_count_ == 1) {
+    dx = point.x_delta();
+    dy = point.y_delta();
+  } else {
+    gfx::Point current_center = bounding_box_.CenterPoint();
+    dx = current_center.x() - bounding_box_last_center_.x();
+    dy = current_center.y() - bounding_box_last_center_.y();
+  }
   if (scroll_type_ == ST_HORIZONTAL)
     dy = 0;
   else if (scroll_type_ == ST_VERTICAL)

ui/aura/gestures/gesture_recognizer_unittest.cc
@@ -2621,12 +2621,12 @@ TEST_F(GestureRecognizerTest, GestureEventScrollTouchMoveConsumed) {
   EXPECT_FALSE(delegate->scroll_end());
   EXPECT_EQ(29, delegate->scroll_x());
   EXPECT_EQ(29, delegate->scroll_y());
-  EXPECT_EQ(gfx::Point(1, 1).ToString(),
+  EXPECT_EQ(gfx::Point(30, 30).ToString(),
             delegate->scroll_begin_position().ToString());
 
   // Start consuming touch-move events again. However, since gesture-scroll has
   // already started, the touch-move events should still result in scroll-update
-  // gesturs.
+  // gestures.
   delegate->set_consume_touch_move(true);
 
   // Move some more to generate a few more scroll updates.
@@ -2852,6 +2852,60 @@ TEST_F(GestureRecognizerTest, BoundingBoxRadiusChange) {
   EXPECT_FALSE(delegate->tap_cancel());
   EXPECT_FALSE(delegate->scroll_update());
   EXPECT_FALSE(delegate->pinch_update());
+
+  delegate->Reset();
+}
+
+// Checks that slow scrolls deliver the correct deltas.
+// In particular, fix for http;//crbug.com/150573.
+TEST_F(GestureRecognizerTest, NoDriftInScroll) {
+  ui::GestureConfiguration::set_max_touch_move_in_pixels_for_click(3);
+  ui::GestureConfiguration::set_min_scroll_delta_squared(9);
+  scoped_ptr<GestureEventConsumeDelegate> delegate(
+      new GestureEventConsumeDelegate());
+  const int kWindowWidth = 234;
+  const int kWindowHeight = 345;
+  const int kTouchId = 5;
+  gfx::Rect bounds(100, 200, kWindowWidth, kWindowHeight);
+  scoped_ptr<aura::Window> window(CreateTestWindowWithDelegate(
+      delegate.get(), -1234, bounds, NULL));
+
+  ui::TouchEvent press1(
+      ui::ET_TOUCH_PRESSED, gfx::Point(101, 208), kTouchId, GetTime());
+  root_window()->AsRootWindowHostDelegate()->OnHostTouchEvent(&press1);
+  EXPECT_TRUE(delegate->begin());
+
+  delegate->Reset();
+
+  ui::TouchEvent move1(ui::ET_TOUCH_MOVED, gfx::Point(101, 206), kTouchId,
+      press1.time_stamp() + base::TimeDelta::FromMilliseconds(40));
+  root_window()->AsRootWindowHostDelegate()->OnHostTouchEvent(&move1);
+  EXPECT_FALSE(delegate->scroll_begin());
+
+  delegate->Reset();
+
+  ui::TouchEvent move2(ui::ET_TOUCH_MOVED, gfx::Point(101, 204), kTouchId,
+      press1.time_stamp() + base::TimeDelta::FromMilliseconds(40));
+  root_window()->AsRootWindowHostDelegate()->OnHostTouchEvent(&move2);
+  EXPECT_TRUE(delegate->tap_cancel());
+  EXPECT_TRUE(delegate->scroll_begin());
+  EXPECT_TRUE(delegate->scroll_update());
+  EXPECT_EQ(-4, delegate->scroll_y());
+
+  delegate->Reset();
+
+  ui::TouchEvent move3(ui::ET_TOUCH_MOVED, gfx::Point(101, 204), kTouchId,
+      press1.time_stamp() + base::TimeDelta::FromMilliseconds(40));
+  root_window()->AsRootWindowHostDelegate()->OnHostTouchEvent(&move3);
+  EXPECT_FALSE(delegate->scroll_update());
+
+  delegate->Reset();
+
+  ui::TouchEvent move4(ui::ET_TOUCH_MOVED, gfx::Point(101, 203), kTouchId,
+      press1.time_stamp() + base::TimeDelta::FromMilliseconds(40));
+  root_window()->AsRootWindowHostDelegate()->OnHostTouchEvent(&move4);
+  EXPECT_TRUE(delegate->scroll_update());
+  EXPECT_EQ(-1, delegate->scroll_y());
 
   delegate->Reset();
 }

commit 0daf91fd5f6311c3e891c26b2e94ffaaf708ab62

Fix getting tap events on scroll gestures when touchmove is consumed
Regression introduced in r158592.  The GesturePoint::UpdateForScroll call was
having the side-effect of making the InInsideManhattanSquare check for a click
always return true.  This is a quick fix designed to be easily merged back
to M23 - just don't update the notion of the starting position of a scroll
on consumed touchmove events.

BUG=154844


Review URL: https://chromiumcodereview.appspot.com/11147015

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162521 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 41f79692680de91ba6e9e43c2ddd2184ffd3619c

ui/base/gestures/gesture_point.h
@@ -101,8 +101,11 @@ class GesturePoint {
   void UpdateEnclosingRectangle(const TouchEvent& event);
   void clear_enclosing_rectangle() { enclosing_rect_ = gfx::Rect(); }
 
+  // The position of touchdown event, or of the last scroll gesture event.
+  // We may want to consider tracking these two things in separate variables.
   gfx::Point first_touch_position_;
   double first_touch_time_;
+
   gfx::Point last_touch_position_;
   double last_touch_time_;
 

ui/base/gestures/gesture_sequence.cc
@@ -384,7 +384,8 @@ GestureSequence::Gestures* GestureSequence::ProcessTouchEventForGesture(
       }
       break;
     case GST_PENDING_SYNTHETIC_CLICK_FIRST_MOVED_PROCESSED:
-      point.UpdateForScroll();
+      // TODO(rbyers): This should be able to trigger a TapCancel
+      // if we moved far enough. crbug.com/146397
       break;
     case GST_PENDING_SYNTHETIC_CLICK_FIRST_RELEASED_HANDLED:
     case GST_PENDING_SYNTHETIC_CLICK_FIRST_CANCELLED:

ui/aura/gestures/gesture_recognizer_unittest.cc
@@ -2530,8 +2530,8 @@ class ConsumesTouchMovesDelegate : public GestureEventConsumeDelegate {
   DISALLOW_COPY_AND_ASSIGN(ConsumesTouchMovesDelegate);
 };
 
-// Same as GestureEventScroll, but tests the different behaviour for consumed
-// touch-move events before and after scroll has started.
+// Same as GestureEventScroll, but tests that the behavior is the same
+// even if all the touch-move events are consumed.
 TEST_F(GestureRecognizerTest, GestureEventScrollTouchMoveConsumed) {
   scoped_ptr<ConsumesTouchMovesDelegate> delegate(
       new ConsumesTouchMovesDelegate());
@@ -2557,8 +2557,66 @@ TEST_F(GestureRecognizerTest, GestureEventScrollTouchMoveConsumed) {
 
   // Move the touch-point enough so that it would normally be considered a
   // scroll. But since the touch-moves will be consumed, the scroll should not
-  // start.  We should, however, get the TAP_CANCEL event now (since it's now
-  // impossible for the gesture to become a tap).
+  // start.
+  SendScrollEvent(root_window(), 130, 230, kTouchId, delegate.get());
+  EXPECT_FALSE(delegate->tap());
+  EXPECT_FALSE(delegate->tap_down());
+  // TODO(rbyers): Really we should get the TapCancel here instead of below,
+  // but this is a symptom of a larger issue: crbug.com/146397.
+  EXPECT_FALSE(delegate->tap_cancel());
+  EXPECT_FALSE(delegate->begin());
+  EXPECT_FALSE(delegate->double_tap());
+  EXPECT_FALSE(delegate->scroll_begin());
+  EXPECT_FALSE(delegate->scroll_update());
+  EXPECT_FALSE(delegate->scroll_end());
+
+  // Release the touch back at the start point. This should end without causing
+  // a tap.
+  delegate->Reset();
+  ui::TouchEvent release(ui::ET_TOUCH_RELEASED, gfx::Point(130, 230),
+                             kTouchId, press.time_stamp() +
+                                 base::TimeDelta::FromMilliseconds(50));
+  root_window()->AsRootWindowHostDelegate()->OnHostTouchEvent(&release);
+  EXPECT_FALSE(delegate->tap());
+  EXPECT_FALSE(delegate->tap_down());
+  EXPECT_TRUE(delegate->tap_cancel());
+  EXPECT_FALSE(delegate->begin());
+  EXPECT_TRUE(delegate->end());
+  EXPECT_FALSE(delegate->double_tap());
+  EXPECT_FALSE(delegate->scroll_begin());
+  EXPECT_FALSE(delegate->scroll_update());
+  EXPECT_FALSE(delegate->scroll_end());
+}
+
+// Like as GestureEventTouchMoveConsumed but tests the different behavior
+// depending on whether the events were consumed before or after the scroll
+// started.
+TEST_F(GestureRecognizerTest, GestureEventScrollTouchMovePartialConsumed) {
+  scoped_ptr<ConsumesTouchMovesDelegate> delegate(
+      new ConsumesTouchMovesDelegate());
+  const int kWindowWidth = 123;
+  const int kWindowHeight = 45;
+  const int kTouchId = 5;
+  gfx::Rect bounds(100, 200, kWindowWidth, kWindowHeight);
+  scoped_ptr<aura::Window> window(CreateTestWindowWithDelegate(
+      delegate.get(), -1234, bounds, NULL));
+
+  delegate->Reset();
+  ui::TouchEvent press(ui::ET_TOUCH_PRESSED, gfx::Point(101, 201),
+                           kTouchId, GetTime());
+  root_window()->AsRootWindowHostDelegate()->OnHostTouchEvent(&press);
+  EXPECT_FALSE(delegate->tap());
+  EXPECT_TRUE(delegate->tap_down());
+  EXPECT_FALSE(delegate->tap_cancel());
+  EXPECT_TRUE(delegate->begin());
+  EXPECT_FALSE(delegate->double_tap());
+  EXPECT_FALSE(delegate->scroll_begin());
+  EXPECT_FALSE(delegate->scroll_update());
+  EXPECT_FALSE(delegate->scroll_end());
+
+  // Move the touch-point enough so that it would normally be considered a
+  // scroll. But since the touch-moves will be consumed, the scroll should not
+  // start.
   SendScrollEvent(root_window(), 130, 230, kTouchId, delegate.get());
   EXPECT_FALSE(delegate->tap());
   EXPECT_FALSE(delegate->tap_down());
@@ -2582,9 +2640,11 @@ TEST_F(GestureRecognizerTest, GestureEventScrollTouchMoveConsumed) {
   EXPECT_TRUE(delegate->scroll_begin());
   EXPECT_TRUE(delegate->scroll_update());
   EXPECT_FALSE(delegate->scroll_end());
-  EXPECT_EQ(29, delegate->scroll_x());
-  EXPECT_EQ(29, delegate->scroll_y());
-  EXPECT_EQ(gfx::Point(30, 30).ToString(),
+  // Consuming move events doesn't effect what the ultimate scroll position
+  // will be if scrolling is later allowed to happen.
+  EXPECT_EQ(58, delegate->scroll_x());
+  EXPECT_EQ(58, delegate->scroll_y());
+  EXPECT_EQ(gfx::Point(1, 1).ToString(),
             delegate->scroll_begin_position().ToString());
 
   // Start consuming touch-move events again. However, since gesture-scroll has