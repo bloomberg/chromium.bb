53eca61ee7580cd1cfd877844f3fb4655be27055
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 21:36:33 +0000

    Make browser-executable error messages more accurate.
    BUG=None
    TEST=None
    
    Review URL: https://codereview.chromium.org/11193028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162534 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 32cc84cc565196157c6e98e60c9c6787f54ec371

--- tools/chrome_remote_control/chrome_remote_control/browser_finder.py
+++ tools/chrome_remote_control/chrome_remote_control/browser_finder.py
@@ -23,10 +23,10 @@ def FindBrowser(options):
   connect to and control the located browser.
   """
   if options.browser_type == 'exact' and options.browser_executable == None:
-    raise Exception('browser_type=exact requires browser_executable be set.')
+    raise Exception('--browser=exact requires --browser-executable to be set.')
 
   if options.browser_type != 'exact' and options.browser_executable != None:
-    raise Exception('browser_executable requires browser_executable=exact.')
+    raise Exception('--browser-executable requires --browser=exact.')
 
   if options.browser_type == None:
     raise BrowserTypeRequiredException('browser_type must be specified')
