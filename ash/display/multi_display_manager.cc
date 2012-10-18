48a27ffdb6dedf5a3791264e2d9621abd4eb49e7
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 11:25:21 +0000

    Fix for out-of-sync scale factor issue - Set active root window early when creating browsers on startup. - Send screen info when device scale factor chagned. - gfx::Screen::GetDisplayNearestWindow should return Primary when the window's root doesn't exist. I missed this condition in  https://chromiumcodereview.appspot.com/10961021
    BUG=155201, 152728
    TEST=manual: see bug for repro steps
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=161758
    
    Review URL: https://chromiumcodereview.appspot.com/11066130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162691 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5ef3331bbfc3c1b03801409e023f395177530ef0

--- ash/display/multi_display_manager.cc
+++ ash/display/multi_display_manager.cc
@@ -313,7 +313,9 @@ const gfx::Display& MultiDisplayManager::GetDisplayNearestWindow(
     return DisplayController::GetPrimaryDisplay();
   const RootWindow* root = window->GetRootWindow();
   MultiDisplayManager* manager = const_cast<MultiDisplayManager*>(this);
-  return root ? manager->FindDisplayForRootWindow(root) : GetInvalidDisplay();
+  return root ?
+      manager->FindDisplayForRootWindow(root) :
+      DisplayController::GetPrimaryDisplay();
 }
 
 const gfx::Display& MultiDisplayManager::GetDisplayNearestPoint(
--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1109,6 +1109,13 @@ void RenderWidgetHostImpl::NotifyScreenInfoChanged() {
 }
 
 void RenderWidgetHostImpl::SetDeviceScaleFactor(float scale) {
+#if defined(USE_AURA)
+  // Send secreen info as well because JavaScript API |window.open|
+  // uses screen info to determine the scale factor (crbug.com/155201).
+  // TODO(oshima|thakis): Consolidate SetDeviceScaleFactor and
+  // ScreenInfoChanged. crbug.com/155213.
+  NotifyScreenInfoChanged();
+#endif
   Send(new ViewMsg_SetDeviceScaleFactor(GetRoutingID(), scale));
 }
 
