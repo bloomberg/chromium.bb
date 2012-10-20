842a59a476ff6f326d7f9171778c4d5bbb201685
brianderson@chromium.org <brianderson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-20 02:08:00 +0000

    Revert "Use WebRenderingStats::Enumerate to populate benchmarking extension renderingStats() fields."
    BUG=156851
    
    
    Review URL: https://chromiumcodereview.appspot.com/11229007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163128 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 15060bd349133c72c44cd4a1fb745d9e71d08444

--- content/common/gpu/gpu_rendering_stats.h
+++ content/common/gpu/gpu_rendering_stats.h
@@ -20,30 +20,6 @@ struct CONTENT_EXPORT GpuRenderingStats {
   base::TimeDelta total_texture_upload_time;
   base::TimeDelta global_total_processing_commands_time;
   base::TimeDelta total_processing_commands_time;
-
-  // In conjunction with enumerateFields, this allows the embedder to
-  // enumerate the values in this structure without
-  // having to embed references to its specific member variables. This
-  // simplifies the addition of new fields to this type.
-  class Enumerator {
-   public:
-    virtual void addInt(const char* name, int value) = 0;
-    virtual void addTimeDelta(const char* name, base::TimeDelta value) = 0;
-   protected:
-    virtual ~Enumerator() { }
-  };
-
-  // Outputs the fields in this structure to the provided enumerator.
-  void enumerateFields(Enumerator* enumerator) const {
-    enumerator->addInt("globalTextureUploadCount", global_texture_upload_count);
-    enumerator->addTimeDelta("totalTextureUploadTime",
-                             total_texture_upload_time);
-    enumerator->addInt("textureUploadCount", texture_upload_count);
-    enumerator->addTimeDelta("globalTotalProcessingCommandsTime",
-                             global_total_processing_commands_time);
-    enumerator->addTimeDelta("totalProcessingCommandsTime",
-                             total_processing_commands_time);
-  }
 };
 
 }  // namespace content
