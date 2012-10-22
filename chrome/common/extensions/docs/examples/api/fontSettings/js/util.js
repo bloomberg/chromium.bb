c1bec71942c751c57ce871a0c304877809294f1e
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-22 08:36:28 +0000

    Font Settings API: UI overhaul of example extension.
    Based on suggestions from UI review and Marcin Wichary.
    This change copies several files required to use the list and overlay/popup
    widgets of Chrome.
    
    BUG=114805
    TBR=mpcomplete
    
    Review URL: https://codereview.chromium.org/11227018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163241 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fb944b59b806cbd88e477b0ca65f0a00692f39eb

---
+++ chrome/common/extensions/docs/examples/api/fontSettings/js/util.js
@@ -0,0 +1,240 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * The global object.
+ * @type {!Object}
+ * @const
+ */
+var global = this;
+
+/**
+ * Alias for document.getElementById.
+ * @param {string} id The ID of the element to find.
+ * @return {HTMLElement} The found element or null if not found.
+ */
+function $(id) {
+  return document.getElementById(id);
+}
+
+/**
+ * Calls chrome.send with a callback and restores the original afterwards.
+ * @param {string} name The name of the message to send.
+ * @param {!Array} params The parameters to send.
+ * @param {string} callbackName The name of the function that the backend calls.
+ * @param {!Function} callback The function to call.
+ */
+function chromeSend(name, params, callbackName, callback) {
+  var old = global[callbackName];
+  global[callbackName] = function() {
+    // restore
+    global[callbackName] = old;
+
+    var args = Array.prototype.slice.call(arguments);
+    return callback.apply(global, args);
+  };
+  chrome.send(name, params);
+}
+
+/**
+ * Generates a CSS url string.
+ * @param {string} s The URL to generate the CSS url for.
+ * @return {string} The CSS url string.
+ */
+function url(s) {
+  // http://www.w3.org/TR/css3-values/#uris
+  // Parentheses, commas, whitespace characters, single quotes (') and double
+  // quotes (") appearing in a URI must be escaped with a backslash
+  var s2 = s.replace(/(\(|\)|\,|\s|\'|\"|\\)/g, '\\$1');
+  // WebKit has a bug when it comes to URLs that end with \
+  // https://bugs.webkit.org/show_bug.cgi?id=28885
+  if (/\\\\$/.test(s2)) {
+    // Add a space to work around the WebKit bug.
+    s2 += ' ';
+  }
+  return 'url("' + s2 + '")';
+}
+
+/**
+ * Parses query parameters from Location.
+ * @param {string} location The URL to generate the CSS url for.
+ * @return {object} Dictionary containing name value pairs for URL
+ */
+function parseQueryParams(location) {
+  var params = {};
+  var query = unescape(location.search.substring(1));
+  var vars = query.split('&');
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split('=');
+    params[pair[0]] = pair[1];
+  }
+  return params;
+}
+
+function findAncestorByClass(el, className) {
+  return findAncestor(el, function(el) {
+    if (el.classList)
+      return el.classList.contains(className);
+    return null;
+  });
+}
+
+/**
+ * Return the first ancestor for which the {@code predicate} returns true.
+ * @param {Node} node The node to check.
+ * @param {function(Node) : boolean} predicate The function that tests the
+ *     nodes.
+ * @return {Node} The found ancestor or null if not found.
+ */
+function findAncestor(node, predicate) {
+  var last = false;
+  while (node != null && !(last = predicate(node))) {
+    node = node.parentNode;
+  }
+  return last ? node : null;
+}
+
+function swapDomNodes(a, b) {
+  var afterA = a.nextSibling;
+  if (afterA == b) {
+    swapDomNodes(b, a);
+    return;
+  }
+  var aParent = a.parentNode;
+  b.parentNode.replaceChild(a, b);
+  aParent.insertBefore(b, afterA);
+}
+
+/**
+ * Disables text selection and dragging, with optional whitelist callbacks.
+ * @param {function(Event):boolean=} opt_allowSelectStart Unless this function
+ *    is defined and returns true, the onselectionstart event will be
+ *    surpressed.
+ * @param {function(Event):boolean=} opt_allowDragStart Unless this function
+ *    is defined and returns true, the ondragstart event will be surpressed.
+ */
+function disableTextSelectAndDrag(opt_allowSelectStart, opt_allowDragStart) {
+  // Disable text selection.
+  document.onselectstart = function(e) {
+    if (!(opt_allowSelectStart && opt_allowSelectStart.call(this, e)))
+      e.preventDefault();
+  };
+
+  // Disable dragging.
+  document.ondragstart = function(e) {
+    if (!(opt_allowDragStart && opt_allowDragStart.call(this, e)))
+      e.preventDefault();
+  };
+}
+
+/**
+ * Call this to stop clicks on <a href="#"> links from scrolling to the top of
+ * the page (and possibly showing a # in the link).
+ */
+function preventDefaultOnPoundLinkClicks() {
+  document.addEventListener('click', function(e) {
+    var anchor = findAncestor(e.target, function(el) {
+      return el.tagName == 'A';
+    });
+    // Use getAttribute() to prevent URL normalization.
+    if (anchor && anchor.getAttribute('href') == '#')
+      e.preventDefault();
+  });
+}
+
+/**
+ * Check the directionality of the page.
+ * @return {boolean} True if Chrome is running an RTL UI.
+ */
+function isRTL() {
+  return document.documentElement.dir == 'rtl';
+}
+
+/**
+ * Simple common assertion API
+ * @param {*} condition The condition to test.  Note that this may be used to
+ *     test whether a value is defined or not, and we don't want to force a
+ *     cast to Boolean.
+ * @param {string=} opt_message A message to use in any error.
+ */
+function assert(condition, opt_message) {
+  'use strict';
+  if (!condition) {
+    var msg = 'Assertion failed';
+    if (opt_message)
+      msg = msg + ': ' + opt_message;
+    throw new Error(msg);
+  }
+}
+
+/**
+ * Get an element that's known to exist by its ID. We use this instead of just
+ * calling getElementById and not checking the result because this lets us
+ * satisfy the JSCompiler type system.
+ * @param {string} id The identifier name.
+ * @return {!Element} the Element.
+ */
+function getRequiredElement(id) {
+  var element = $(id);
+  assert(element, 'Missing required element: ' + id);
+  return element;
+}
+
+// Handle click on a link. If the link points to a chrome: or file: url, then
+// call into the browser to do the navigation.
+document.addEventListener('click', function(e) {
+  // Allow preventDefault to work.
+  if (!e.returnValue)
+    return;
+
+  var el = e.target;
+  if (el.nodeType == Node.ELEMENT_NODE &&
+      el.webkitMatchesSelector('A, A *')) {
+    while (el.tagName != 'A') {
+      el = el.parentElement;
+    }
+
+    if ((el.protocol == 'file:' || el.protocol == 'about:') &&
+        (e.button == 0 || e.button == 1)) {
+      chrome.send('navigateToUrl', [
+        el.href,
+        el.target,
+        e.button,
+        e.altKey,
+        e.ctrlKey,
+        e.metaKey,
+        e.shiftKey
+      ]);
+      e.preventDefault();
+    }
+  }
+});
+
+/**
+ * Creates a new URL which is the old URL with a GET param of key=value.
+ * @param {string} url The base URL. There is not sanity checking on the URL so
+ *     it must be passed in a proper format.
+ * @param {string} key The key of the param.
+ * @param {string} value The value of the param.
+ * @return {string} The new URL.
+ */
+function appendParam(url, key, value) {
+  var param = encodeURIComponent(key) + '=' + encodeURIComponent(value);
+
+  if (url.indexOf('?') == -1)
+    return url + '?' + param;
+  return url + '&' + param;
+}
+
+/**
+ * Creates a new URL for a favicon request.
+ * @param {string} url The url for the favicon.
+ * @param {number=} opt_size Optional preferred size of the favicon.
+ * @return {string} Updated URL for the favicon.
+ */
+function getFaviconURL(url, opt_size) {
+  var size = opt_size || 16;
+  return 'chrome://favicon/size/' + size + '@' +
+      window.devicePixelRatio + 'x/' + url;
+}
--- chrome/common/extensions/docs/examples/api/fontSettings/options.js
+++ chrome/common/extensions/docs/examples/api/fontSettings/options.js
@@ -2,16 +2,56 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// The scripts supported by the Font Settings Extension API.
+var scripts = [
+  { scriptCode: 'Zyyy', scriptName: 'Default' },
+  { scriptCode: 'Arab', scriptName: 'Arabic' },
+  { scriptCode: 'Armn', scriptName: 'Armenian' },
+  { scriptCode: "Beng", scriptName: 'Bengali' },
+  { scriptCode: "Cher", scriptName: 'Cherokee' },
+  { scriptCode: "Cyrl", scriptName: 'Cyrillic' },
+  { scriptCode: "Deva", scriptName: 'Devanagari' },
+  { scriptCode: "Ethi", scriptName: 'Ethiopic' },
+  { scriptCode: "Geor", scriptName: 'Georgian' },
+  { scriptCode: "Grek", scriptName: 'Greek' },
+  { scriptCode: "Gujr", scriptName: 'Gujarati' },
+  { scriptCode: "Guru", scriptName: 'Gurmukhi' },
+  { scriptCode: "Hebr", scriptName: 'Hebrew' },
+  { scriptCode: "Jpan", scriptName: 'Japanese' },
+  { scriptCode: "Knda", scriptName: 'Kannada' },
+  { scriptCode: "Khmr", scriptName: 'Khmer' },
+  { scriptCode: "Hang", scriptName: 'Korean' },
+  { scriptCode: "Laoo", scriptName: 'Lao' },
+  { scriptCode: "Mlym", scriptName: 'Malayalam' },
+  { scriptCode: "Mong", scriptName: 'Mongolian' },
+  { scriptCode: "Mymr", scriptName: 'Myanmar' },
+  { scriptCode: "Orya", scriptName: 'Oriya' },
+  { scriptCode: "Hans", scriptName: 'Simplified Chinese' },
+  { scriptCode: "Sinh", scriptName: 'Sinhala' },
+  { scriptCode: "Taml", scriptName: 'Tamil' },
+  { scriptCode: "Telu", scriptName: 'Telugu' },
+  { scriptCode: "Thaa", scriptName: 'Thaana' },
+  { scriptCode: "Thai", scriptName: 'Thai' },
+  { scriptCode: "Tibt", scriptName: 'Tibetan' },
+  { scriptCode: "Hant", scriptName: 'Traditional Chinese' },
+  { scriptCode: "Cans", scriptName: 'Unified Canadian Aboriginal Syllabics' },
+  { scriptCode: "Yiii", scriptName: 'Yi' }
+];
+
+// The generic font families supported by the Font Settings Extension API.
+var families =
+    ["standard", "sansserif", "serif", "fixed", "cursive", "fantasy"];
+
 // Mapping between font list ids and the generic family setting they
 // represent.
-var genericFamilies = [
+var fontPickers = [
   { fontList: 'standardFontList', name: 'standard' },
   { fontList: 'serifFontList', name: 'serif' },
   { fontList: 'sansSerifFontList', name: 'sansserif' },
   { fontList: 'fixedFontList', name: 'fixed' }
 ];
 
-// Ids of elements to contain the "Lorem ipsum" sample text.
+// Ids of elements to contain the sample text.
 var sampleTextDivIds = [
   'standardFontSample',
   'serifFontSample',
@@ -20,7 +60,8 @@ var sampleTextDivIds = [
   'minFontSample'
 ];
 
-var defaultSampleText = 'Lorem ipsum dolor sit amat.';
+// Sample texts.
+var defaultSampleText = 'The quick brown fox jumps over the lazy dog.';
 var scriptSpecificSampleText = {
   // "Cyrllic script".
   'Cyrl': 'Кириллица',
@@ -32,9 +73,62 @@ var scriptSpecificSampleText = {
   'Khmr': '\u1797\u17B6\u179F\u17B6\u1781\u17D2\u1798\u17C2\u179A',
 };
 
+// Definition for ScriptList.
+cr.define('fontSettings.ui', function() {
+  const List = cr.ui.List;
+  const ListItem = cr.ui.ListItem;
+  const ListSingleSelectionModel = cr.ui.ListSingleSelectionModel;
+
+  function ScriptListItem(info) {
+    var el = cr.doc.createElement('li');
+    el.__proto__ = ScriptListItem.prototype;
+    el.info_ = info;
+    el.decorate();
+    return el;
+  };
+
+  ScriptListItem.prototype = {
+    __proto__: ListItem.prototype,
+
+    decorate: function() {
+      this.textContent = this.info_.scriptName;
+      if (this.info_.scriptCode == 'Zyyy') {
+        this.style.marginBottom = '1em';
+      }
+    }
+  };
+
+  var ScriptList = cr.ui.define('list');
+  ScriptList.prototype = {
+    __proto__: List.prototype,
+
+    decorate: function() {
+      List.prototype.decorate.call(this);
+      var sm = new ListSingleSelectionModel();
+      this.selectionModel = sm;
+      this.autoExpands = true;
+      this.dataModel = new cr.ui.ArrayDataModel(scripts);
+
+      // The list auto expands but is still just barely short enough to require
+      // a scroll bar. This is a hack to resize it to not require the scroll
+      // bar.
+      this.style.height = (this.clientHeight + 4) + 'px';
+    },
+
+    createItem: function(info) {
+      return new ScriptListItem(info);
+    }
+  };
+
+  return {
+    ScriptList: ScriptList,
+    ScriptListItem: ScriptListItem
+  };
+});
+
 function getSelectedScript() {
   var scriptList = document.getElementById('scriptList');
-  return scriptList.options[scriptList.selectedIndex].value;
+  return scriptList.selectedItem.scriptCode;
 }
 
 function getSelectedFont(fontList) {
@@ -43,16 +137,16 @@ function getSelectedFont(fontList) {
 
 // Populates the font lists with the list of system fonts from |fonts|.
 function populateLists(fonts) {
-  for (var i = 0; i < genericFamilies.length; i++) {
-    var list = document.getElementById(genericFamilies[i].fontList);
+  for (var i = 0; i < fontPickers.length; i++) {
+    var list = document.getElementById(fontPickers[i].fontList);
 
-    // Add special "(none)" item to indicate fallback to the non-per-script
+    // Add special item to indicate fallback to the non-per-script
     // font setting. The Font Settings API uses the empty string to indicate
     // fallback.
-    var noneItem = document.createElement('option');
-    noneItem.value = '';
-    noneItem.text = '(none)';
-    list.add(noneItem);
+    var defaultItem = document.createElement('option');
+    defaultItem.value = '';
+    defaultItem.text = '(Use default)';
+    list.add(defaultItem);
 
     for (var j = 0; j < fonts.length; j++) {
       var item = document.createElement('option');
@@ -108,22 +202,18 @@ function getFontHandler(list) {
 }
 
 // Called when the script list selection changes. Sets the selected value of
-// each font list to the current font setting, and updates the document's lang
-// so that the samples are shown in the current font setting.
+// each font list to the current font setting, and updates the samples' lang
+// so that they are shown in the current font setting.
 function updateFontListsForScript() {
   var script = getSelectedScript();
 
-  for (var i = 0; i < genericFamilies.length; i++) {
-    var list = document.getElementById(genericFamilies[i].fontList);
-    var family = genericFamilies[i].name;
+  for (var i = 0; i < fontPickers.length; i++) {
+    var list = document.getElementById(fontPickers[i].fontList);
+    var family = fontPickers[i].name;
 
     var details = {};
     details.genericFamily = family;
     details.script = script;
-    // For font selection it's the script code that matters, not language, so
-    // just use en for lang.
-    document.body.lang = 'en-' + script;
-
     chrome.fontSettings.getFont(details, getFontHandler(list));
   }
 
@@ -132,7 +222,11 @@ function updateFontListsForScript() {
   else
     sample = defaultSampleText;
   for (var i = 0; i < sampleTextDivIds.length; i++) {
-    document.getElementById(sampleTextDivIds[i]).innerText = sample;
+    var sampleTextDiv = document.getElementById(sampleTextDivIds[i]);
+    // For font selection it's the script code that matters, not language, so
+    // just use en for lang.
+    sampleTextDiv.lang = 'en-' + script;
+    sampleTextDiv.innerText = sample;
   }
 }
 
@@ -182,53 +276,76 @@ function initFontSizePref(id, sampleTexts, getter, setter, apiEvent) {
   apiEvent.addListener(getFontSizeChangedOnBrowserFunc(elem, sampleTexts));
 }
 
-function clearAllSettings() {
-  var scripts =
-      ["Afak", "Arab", "Armi", "Armn", "Avst", "Bali", "Bamu", "Bass", "Batk",
-       "Beng", "Blis", "Bopo", "Brah", "Brai", "Bugi", "Buhd", "Cakm", "Cans",
-       "Cari", "Cham", "Cher", "Cirt", "Copt", "Cprt", "Cyrl", "Cyrs", "Deva",
-       "Dsrt", "Dupl", "Egyd", "Egyh", "Egyp", "Elba", "Ethi", "Geor", "Geok",
-       "Glag", "Goth", "Gran", "Grek", "Gujr", "Guru", "Hang", "Hani", "Hano",
-       "Hans", "Hant", "Hebr", "Hluw", "Hmng", "Hung", "Inds", "Ital", "Java",
-       "Jpan", "Jurc", "Kali", "Khar", "Khmr", "Khoj", "Knda", "Kpel", "Kthi",
-       "Lana", "Laoo", "Latf", "Latg", "Latn", "Lepc", "Limb", "Lina", "Linb",
-       "Lisu", "Loma", "Lyci", "Lydi", "Mand", "Mani", "Maya", "Mend", "Merc",
-       "Mero", "Mlym", "Moon", "Mong", "Mroo", "Mtei", "Mymr", "Narb", "Nbat",
-       "Nkgb", "Nkoo", "Nshu", "Ogam", "Olck", "Orkh", "Orya", "Osma", "Palm",
-       "Perm", "Phag", "Phli", "Phlp", "Phlv", "Phnx", "Plrd", "Prti", "Rjng",
-       "Roro", "Runr", "Samr", "Sara", "Sarb", "Saur", "Sgnw", "Shaw", "Shrd",
-       "Sind", "Sinh", "Sora", "Sund", "Sylo", "Syrc", "Syre", "Syrj", "Syrn",
-       "Tagb", "Takr", "Tale", "Talu", "Taml", "Tang", "Tavt", "Telu", "Teng",
-       "Tfng", "Tglg", "Thaa", "Thai", "Tibt", "Tirh", "Ugar", "Vaii", "Visp",
-       "Wara", "Wole", "Xpeo", "Xsux", "Yiii", "Zmth", "Zsym", "Zyyy"];
-  var families =
-      ["standard", "sansserif", "serif", "fixed", "cursive", "fantasy"];
-  for (var i = 0; i < scripts.length; i++) {
-    for (var j = 0; j < families.length; j++) {
-      chrome.fontSettings.clearFont({
-        script: scripts[i],
-        genericFamily: families[j]
-      });
-    }
+function clearSettingsForScript(script) {
+  for (var i = 0; i < families.length; i++) {
+    chrome.fontSettings.clearFont({
+      script: script,
+      genericFamily: families[i]
+    });
   }
+}
+
+function clearAllSettings() {
+  for (var i = 0; i < scripts.length; i++)
+    clearSettingsForScript(scripts[i].scriptCode);
 
   chrome.fontSettings.clearDefaultFixedFontSize();
   chrome.fontSettings.clearDefaultFontSize();
   chrome.fontSettings.clearMinimumFontSize();
 }
 
+function closeOverlay() {
+  $('overlay-container').hidden = true;
+}
+
+function initResetButtons() {
+  var overlay = $('overlay-container');
+  cr.ui.overlay.globalInitialization();
+  cr.ui.overlay.setupOverlay(overlay);
+  overlay.addEventListener('cancelOverlay', closeOverlay);
+
+  $('reset-this-script-button').onclick = function(event) {
+    var scriptName = $('scriptList').selectedItem.scriptName;
+    $('reset-this-script-overlay-dialog-content').innerText =
+        'Are you sure you want to reset settings for ' + scriptName +
+        ' script?';
+
+    $('overlay-container').hidden = false;
+    $('reset-this-script-overlay-dialog').hidden = false;
+    $('reset-all-scripts-overlay-dialog').hidden = true;
+  }
+  $('reset-this-script-ok').onclick = function(event) {
+    clearSettingsForScript(getSelectedScript());
+    closeOverlay();
+  };
+  $('reset-this-script-cancel').onclick = closeOverlay;
+
+  $('reset-all-button').onclick = function(event) {
+    $('overlay-container').hidden = false;
+    $('reset-all-scripts-overlay-dialog').hidden = false;
+    $('reset-this-script-overlay-dialog').hidden = true;
+  }
+  $('reset-all-ok').onclick = function(event) {
+    clearAllSettings();
+    closeOverlay();
+  }
+  $('reset-all-cancel').onclick = closeOverlay;
+}
+
 function init() {
-  scriptList = document.getElementById('scriptList');
-  scriptList.addEventListener('change',
-                              updateFontListsForScript);
+  var scriptList = document.getElementById('scriptList');
+  fontSettings.ui.ScriptList.decorate(scriptList);
+  scriptList.selectionModel.selectedIndex = 0;
+  scriptList.selectionModel.addEventListener('change',
+                                             updateFontListsForScript);
 
   // Populate the font lists.
   chrome.fontSettings.getFontList(populateLists);
 
   // Add change handlers to the font lists.
-  for (var i = 0; i < genericFamilies.length; i++) {
-    var list = document.getElementById(genericFamilies[i].fontList);
-    var handler = getFontChangeHandler(list, genericFamilies[i].name);
+  for (var i = 0; i < fontPickers.length; i++) {
+    var list = document.getElementById(fontPickers[i].fontList);
+    var handler = getFontChangeHandler(list, fontPickers[i].name);
     list.addEventListener('change', handler);
   }
 
@@ -236,26 +353,43 @@ function init() {
       updateFontListsForScript);
 
   initFontSizePref(
-      'defaultFontSize',
+      'defaultFontSizeRocker',
       ['standardFontSample', 'serifFontSample', 'sansSerifFontSample'],
       chrome.fontSettings.getDefaultFontSize,
       chrome.fontSettings.setDefaultFontSize,
       chrome.fontSettings.onDefaultFontSizeChanged);
   initFontSizePref(
-      'defaultFixedFontSize',
+      'defaultFontSizeRange',
+      ['standardFontSample', 'serifFontSample', 'sansSerifFontSample'],
+      chrome.fontSettings.getDefaultFontSize,
+      chrome.fontSettings.setDefaultFontSize,
+      chrome.fontSettings.onDefaultFontSizeChanged);
+  initFontSizePref(
+      'defaultFixedFontSizeRocker',
       ['fixedFontSample'],
       chrome.fontSettings.getDefaultFixedFontSize,
       chrome.fontSettings.setDefaultFixedFontSize,
       chrome.fontSettings.onDefaultFixedFontSizeChanged);
   initFontSizePref(
-      'minFontSize',
+      'defaultFixedFontSizeRange',
+      ['fixedFontSample'],
+      chrome.fontSettings.getDefaultFixedFontSize,
+      chrome.fontSettings.setDefaultFixedFontSize,
+      chrome.fontSettings.onDefaultFixedFontSizeChanged);
+  initFontSizePref(
+      'minFontSizeRocker',
+      ['minFontSample'],
+      chrome.fontSettings.getMinimumFontSize,
+      chrome.fontSettings.setMinimumFontSize,
+      chrome.fontSettings.onMinimumFontSizeChanged);
+  initFontSizePref(
+      'minFontSizeRange',
       ['minFontSample'],
       chrome.fontSettings.getMinimumFontSize,
       chrome.fontSettings.setMinimumFontSize,
       chrome.fontSettings.onMinimumFontSizeChanged);
 
-  var clearButton = document.getElementById('clearButton');
-  clearButton.addEventListener('click', clearAllSettings);
+  initResetButtons();
 }
 
 document.addEventListener('DOMContentLoaded', init);
