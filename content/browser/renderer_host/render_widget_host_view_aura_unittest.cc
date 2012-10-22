45cda84f6d6064f33b7d8dbb3cb317a12780acf0
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-22 23:57:09 +0000

    touch: Do not run a test on windows since a number of the related functions aren't implemented yet.
    TBR=sky@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/11231061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163447 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bf297d5d9aceebaeedc1dde595def241b50a8a0d

--- content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
+++ content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
@@ -145,7 +145,10 @@ TEST_F(RenderWidgetHostViewAuraTest, DestroyFullscreenOnBlur) {
   view_ = NULL;
 }
 
-// Checks that touch-event state is maintained correctly.
+#if !defined(OS_WIN)
+// Checks that touch-event state is maintained correctly. A lot of the
+// touch-event related functions are not implemented on Windows. So run this
+// test only in chromeos. http://crbug.com/157268
 TEST_F(RenderWidgetHostViewAuraTest, TouchEventState) {
   view_->InitAsChild(NULL);
   view_->Show();
@@ -219,5 +222,6 @@ TEST_F(RenderWidgetHostViewAuraTest, TouchEventState) {
   EXPECT_EQ(WebKit::WebInputEvent::TouchEnd, view_->touch_event_.type);
   EXPECT_EQ(0U, view_->touch_event_.touchesLength);
 }
+#endif // !defined(OS_WIN)
 
 }  // namespace content
