d1003e296d99d7dd3650f861cddf0ba8188aea43
nileshagrawal@chromium.org <nileshagrawal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 21:08:06 +0000

    Initialize the TabHelpers needed for Android.
    This helper method exists to remove the need to create a TabContents.
    BUG=153587
    
    
    Review URL: https://chromiumcodereview.appspot.com/11235036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163673 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 79a27460eca0f96b7566d826db4615f2da19fc86

--- chrome/browser/android/tab_android.cc
+++ chrome/browser/android/tab_android.cc
@@ -3,7 +3,24 @@
 // found in the LICENSE file.
 
 #include "chrome/browser/android/tab_android.h"
+#include "chrome/browser/autofill/autofill_external_delegate.h"
+#include "chrome/browser/autofill/autofill_manager.h"
+#include "chrome/browser/content_settings/tab_specific_content_settings.h"
+#include "chrome/browser/favicon/favicon_tab_helper.h"
+#include "chrome/browser/history/history_tab_helper.h"
+#include "chrome/browser/infobars/infobar_tab_helper.h"
+#include "chrome/browser/password_manager/password_manager.h"
+#include "chrome/browser/password_manager/password_manager_delegate_impl.h"
+#include "chrome/browser/sessions/session_tab_helper.h"
+#include "chrome/browser/ssl/ssl_tab_helper.h"
 #include "chrome/browser/ui/android/window_android_helper.h"
+#include "chrome/browser/ui/autofill/tab_autofill_manager_delegate.h"
+#include "chrome/browser/ui/blocked_content/blocked_content_tab_helper.h"
+#include "chrome/browser/ui/bookmarks/bookmark_tab_helper.h"
+#include "chrome/browser/ui/find_bar/find_tab_helper.h"
+#include "chrome/browser/ui/prefs/prefs_tab_helper.h"
+#include "chrome/browser/ui/sync/tab_contents_synced_tab_delegate.h"
+#include "chrome/browser/ui/tab_contents/core_tab_helper.h"
 #include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "content/public/browser/android/content_view_core.h"
 #include "content/public/browser/web_contents.h"
@@ -18,8 +35,37 @@ TabContents* TabAndroid::GetOrCreateTabContents(
   return tab_contents;
 }
 
-void TabAndroid::InitTabHelpers(content::WebContents* web_contents) {
-  WindowAndroidHelper::CreateForWebContents(web_contents);
+void TabAndroid::InitTabHelpers(content::WebContents* contents) {
+  // TODO(nileshagrawal): Currently this is not used by Chrome for Android,
+  // as it uses TabContents. When TabContents is not created for Android,
+  // this will be used to initialize all the tab helpers.
+
+  // SessionTabHelper comes first because it sets up the tab ID, and other
+  // helpers may rely on that.
+  SessionTabHelper::CreateForWebContents(contents);
+
+  TabAutofillManagerDelegate::CreateForWebContents(contents);
+  AutofillManager::CreateForWebContentsAndDelegate(
+      contents, TabAutofillManagerDelegate::FromWebContents(contents));
+  AutofillExternalDelegate::CreateForWebContentsAndManager(
+      contents, AutofillManager::FromWebContents(contents));
+  AutofillManager::FromWebContents(contents)->SetExternalDelegate(
+      AutofillExternalDelegate::FromWebContents(contents));
+  BlockedContentTabHelper::CreateForWebContents(contents);
+  BookmarkTabHelper::CreateForWebContents(contents);
+  CoreTabHelper::CreateForWebContents(contents);
+  FaviconTabHelper::CreateForWebContents(contents);
+  FindTabHelper::CreateForWebContents(contents);
+  HistoryTabHelper::CreateForWebContents(contents);
+  InfoBarTabHelper::CreateForWebContents(contents);
+  PasswordManagerDelegateImpl::CreateForWebContents(contents);
+  PasswordManager::CreateForWebContentsAndDelegate(
+      contents, PasswordManagerDelegateImpl::FromWebContents(contents));
+  PrefsTabHelper::CreateForWebContents(contents);
+  SSLTabHelper::CreateForWebContents(contents);
+  TabContentsSyncedTabDelegate::CreateForWebContents(contents);
+  TabSpecificContentSettings::CreateForWebContents(contents);
+  WindowAndroidHelper::CreateForWebContents(contents);
 }
 
 TabContents* TabAndroid::InitTabContentsFromView(JNIEnv* env,
