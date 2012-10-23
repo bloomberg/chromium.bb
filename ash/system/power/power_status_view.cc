0171f1af51cbe15dfd0011f88d622f6c7fde145f
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 19:35:58 +0000

    ash: Make sure the power-status string is updated correctly.
    BUG=157144
    
    Review URL: https://codereview.chromium.org/11225063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163643 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5f3eb5e2dff13534aec09222c2cf0cca2b074cf6

--- ash/system/power/power_status_view.cc
+++ ash/system/power/power_status_view.cc
@@ -122,7 +122,9 @@ void PowerStatusView::UpdateTextForDefaultView() {
     int hour = 0;
     int min = 0;
     base::TimeDelta time = base::TimeDelta::FromSeconds(
-        supply_status_.averaged_battery_time_to_empty);
+        supply_status_.line_power_on ?
+          supply_status_.averaged_battery_time_to_full :
+          supply_status_.averaged_battery_time_to_empty);
     hour = time.InHours();
     min = (time - base::TimeDelta::FromHours(hour)).InMinutes();
     if (hour || min) {
