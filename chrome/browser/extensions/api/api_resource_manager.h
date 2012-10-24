3491f89aa32c1b5a82478b489a791f20f25bebfd
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-24 22:32:15 +0000

    Fixed unsafe deletion of ApiResourceManager<T> that exposed crashes in http://codereview.chromium.org/11027070/.
    BUG=157501
    TEST=existing browser tests should not crash
    
    Review URL: https://codereview.chromium.org/11229072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163930 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2bd7d9ec294434bc9819ee7cd553545c6eb765a3

--- chrome/browser/extensions/api/api_resource_manager.h
+++ chrome/browser/extensions/api/api_resource_manager.h
@@ -28,14 +28,9 @@ class ApiResourceManager : public ProfileKeyedService,
                            public base::NonThreadSafe,
                            public content::NotificationObserver {
  public:
-  typedef std::map<int, linked_ptr<T> > ApiResourceMap;
-  // Lookup map from extension id's to allocated resource id's.
-  typedef std::map<std::string, base::hash_set<int> > ExtensionToResourceMap;
-
   explicit ApiResourceManager(const BrowserThread::ID thread_id)
-      : next_id_(1),
-        thread_id_(thread_id),
-        api_resource_map_(new ApiResourceMap()) {
+      : thread_id_(thread_id),
+        data_(new ApiResourceData(thread_id)) {
     registrar_.Add(this,
       chrome::NOTIFICATION_EXTENSION_UNLOADED,
       content::NotificationService::AllSources());
@@ -43,47 +38,25 @@ class ApiResourceManager : public ProfileKeyedService,
 
   virtual ~ApiResourceManager() {
     DCHECK(CalledOnValidThread());
-
     DCHECK(BrowserThread::IsMessageLoopValid(thread_id_)) <<
         "A unit test is using an ApiResourceManager but didn't provide "
         "the thread message loop needed for that kind of resource. "
         "Please ensure that the appropriate message loop is operational.";
 
-    BrowserThread::DeleteSoon(thread_id_, FROM_HERE, api_resource_map_);
+    BrowserThread::DeleteSoon(thread_id_, FROM_HERE, data_.release());
   }
 
   // Takes ownership.
   int Add(T* api_resource) {
-    DCHECK(BrowserThread::CurrentlyOn(thread_id_));
-    int id = GenerateId();
-    if (id > 0) {
-      linked_ptr<T> resource_ptr(api_resource);
-      (*api_resource_map_)[id] = resource_ptr;
-
-      const std::string& extension_id = api_resource->owner_extension_id();
-      if (extension_resource_map_.find(extension_id) ==
-          extension_resource_map_.end())
-        extension_resource_map_[extension_id] = base::hash_set<int>();
-      extension_resource_map_[extension_id].insert(id);
-
-      return id;
-    }
-    return 0;
+    return data_->Add(api_resource);
   }
 
   void Remove(const std::string& extension_id, int api_resource_id) {
-    DCHECK(BrowserThread::CurrentlyOn(thread_id_));
-    if (GetOwnedResource(extension_id, api_resource_id) != NULL) {
-      DCHECK(extension_resource_map_.find(extension_id) !=
-             extension_resource_map_.end());
-      extension_resource_map_[extension_id].erase(api_resource_id);
-      api_resource_map_->erase(api_resource_id);
-    }
+    data_->Remove(extension_id, api_resource_id);
   }
 
   T* Get(const std::string& extension_id, int api_resource_id) {
-    DCHECK(BrowserThread::CurrentlyOn(thread_id_));
-    return GetOwnedResource(extension_id, api_resource_id);
+    return data_->Get(extension_id, api_resource_id);
   }
 
  protected:
@@ -96,48 +69,104 @@ class ApiResourceManager : public ProfileKeyedService,
         std::string id =
             content::Details<extensions::UnloadedExtensionInfo>(details)->
                 extension->id();
-        BrowserThread::PostTask(thread_id_, FROM_HERE,
-            base::Bind(&ApiResourceManager::CleanupResourcesFromExtension,
-                       base::Unretained(this), id));
+        data_->InitiateCleanup(id);
         break;
       }
     }
   }
 
  private:
-  int GenerateId() {
-    return next_id_++;
-  }
 
-  T* GetOwnedResource(const std::string& extension_id,
-                      int api_resource_id) {
-    linked_ptr<T> ptr = (*api_resource_map_)[api_resource_id];
-    T* resource = ptr.get();
-    if (resource && extension_id == resource->owner_extension_id())
-      return resource;
-    return NULL;
-  }
+  // ApiResourceData class handles resource bookkeeping on a thread
+  // where resource lifetime is handled.
+  class ApiResourceData {
+   public:
+    typedef std::map<int, linked_ptr<T> > ApiResourceMap;
+    // Lookup map from extension id's to allocated resource id's.
+    typedef std::map<std::string, base::hash_set<int> > ExtensionToResourceMap;
 
-  void CleanupResourcesFromExtension(const std::string& extension_id) {
-    if (extension_resource_map_.find(extension_id) !=
-        extension_resource_map_.end()) {
-      base::hash_set<int>& resource_ids = extension_resource_map_[extension_id];
-      for (base::hash_set<int>::iterator it = resource_ids.begin();
-           it != resource_ids.end(); ++it) {
-        api_resource_map_->erase(*it);
-      }
-      extension_resource_map_.erase(extension_id);
+    explicit ApiResourceData(const BrowserThread::ID thread_id)
+        : next_id_(1),
+          thread_id_(thread_id) {
     }
-  }
 
-  int next_id_;
+    int Add(T* api_resource) {
+      DCHECK(BrowserThread::CurrentlyOn(thread_id_));
+      int id = GenerateId();
+      if (id > 0) {
+        linked_ptr<T> resource_ptr(api_resource);
+        api_resource_map_[id] = resource_ptr;
+
+        const std::string& extension_id = api_resource->owner_extension_id();
+        if (extension_resource_map_.find(extension_id) ==
+            extension_resource_map_.end()) {
+          extension_resource_map_[extension_id] = base::hash_set<int>();
+        }
+        extension_resource_map_[extension_id].insert(id);
+
+       return id;
+     }
+     return 0;
+    }
+
+    void Remove(const std::string& extension_id, int api_resource_id) {
+      DCHECK(BrowserThread::CurrentlyOn(thread_id_));
+      if (GetOwnedResource(extension_id, api_resource_id) != NULL) {
+        DCHECK(extension_resource_map_.find(extension_id) !=
+               extension_resource_map_.end());
+        extension_resource_map_[extension_id].erase(api_resource_id);
+        api_resource_map_.erase(api_resource_id);
+      }
+    }
+
+    T* Get(const std::string& extension_id, int api_resource_id) {
+      DCHECK(BrowserThread::CurrentlyOn(thread_id_));
+      return GetOwnedResource(extension_id, api_resource_id);
+    }
+
+    void InitiateCleanup(const std::string& extension_id) {
+      BrowserThread::PostTask(thread_id_, FROM_HERE,
+          base::Bind(&ApiResourceData::CleanupResourcesFromExtension,
+                     base::Unretained(this), extension_id));
+    }
+
+   private:
+    T* GetOwnedResource(const std::string& extension_id,
+                        int api_resource_id) {
+      linked_ptr<T> ptr = api_resource_map_[api_resource_id];
+      T* resource = ptr.get();
+      if (resource && extension_id == resource->owner_extension_id())
+        return resource;
+      return NULL;
+    }
+
+    void CleanupResourcesFromExtension(const std::string& extension_id) {
+      DCHECK(BrowserThread::CurrentlyOn(thread_id_));
+      if (extension_resource_map_.find(extension_id) !=
+          extension_resource_map_.end()) {
+        base::hash_set<int>& resource_ids =
+            extension_resource_map_[extension_id];
+        for (base::hash_set<int>::iterator it = resource_ids.begin();
+             it != resource_ids.end(); ++it) {
+          api_resource_map_.erase(*it);
+        }
+        extension_resource_map_.erase(extension_id);
+      }
+    }
+
+    int GenerateId() {
+      return next_id_++;
+    }
+
+    int next_id_;
+    const BrowserThread::ID thread_id_;
+    ApiResourceMap api_resource_map_;
+    ExtensionToResourceMap extension_resource_map_;
+  };
+
   const BrowserThread::ID thread_id_;
   content::NotificationRegistrar registrar_;
-
-  // We need finer-grained control over the lifetime of this instance than RAII
-  // can give us.
-  ApiResourceMap* api_resource_map_;
-  ExtensionToResourceMap extension_resource_map_;
+  scoped_ptr<ApiResourceData> data_;
 };
 
 }  // namespace extensions
