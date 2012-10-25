9c7a8bf36373a0b3900d380d4cc5988f8304c849
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 01:17:58 +0000

    Revert 163972 - IndexedDB: Ensure pending callbacks are fired before dispatcher is torn down
    Worker termination was marking the dispatcher as "deleted" before pending
    callbacks were destructed, and some of those pending callbacks were trying
    to use the dispatcher. Swap the order by explicitly clearing out callback
    maps rather than relying on implicit destruction.
    
    R=dgrogan@chromium.org,michaeln@chromium.org
    BUG=153652
    
    
    Review URL: https://chromiumcodereview.appspot.com/11287002
    
    TBR=jsbell@chromium.org
    Review URL: https://codereview.chromium.org/11265017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163977 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d65165bee9d7da2dfba9a6a8958cd9e39de73c9c

--- content/common/indexed_db/indexed_db_dispatcher.cc
+++ content/common/indexed_db/indexed_db_dispatcher.cc
@@ -54,26 +54,6 @@ IndexedDBDispatcher::IndexedDBDispatcher() {
 }
 
 IndexedDBDispatcher::~IndexedDBDispatcher() {
-
-  // Clear any pending callbacks - which may result in dispatch requests -
-  // before marking the dispatcher as deleted.
-  for (IDMap<WebKit::WebIDBCallbacks, IDMapOwnPointer>::iterator
-           it(&pending_callbacks_); !it.IsAtEnd(); it.Advance()) {
-    pending_callbacks_.Remove(it.GetCurrentKey());
-  }
-  for (IDMap<WebKit::WebIDBDatabaseCallbacks, IDMapOwnPointer>::iterator
-           it(&pending_database_callbacks_); !it.IsAtEnd(); it.Advance()) {
-    pending_database_callbacks_.Remove(it.GetCurrentKey());
-  }
-  for (IDMap<WebKit::WebIDBTransactionCallbacks, IDMapOwnPointer>::iterator
-           it(&pending_transaction_callbacks_); !it.IsAtEnd(); it.Advance()) {
-    pending_transaction_callbacks_.Remove(it.GetCurrentKey());
-  }
-
-  DCHECK(pending_callbacks_.IsEmpty());
-  DCHECK(pending_database_callbacks_.IsEmpty());
-  DCHECK(pending_transaction_callbacks_.IsEmpty());
-
   g_idb_dispatcher_tls.Pointer()->Set(kHasBeenDeleted);
 }
 
