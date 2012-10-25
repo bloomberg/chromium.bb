3fb072174d1c26c86ec72e36f35e9be993b0d626
nkang@chromium.org <nkang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 17:00:14 +0000

    Moved Main class to a new file called run_install_tests.py and modified it by adding the ability to filter unittests. This will allow users to run individual unittests, a set of tests, or the entire suite.
    BUG=none
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11234068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164090 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3fdbcd44fbb0250bced1430ac8f730a6d2b0b2be

--- chrome/test/install_test/sample_updater.py
+++ chrome/test/install_test/sample_updater.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Runs sample updater and install tests using one or more Chrome builds.
-
-Test requires two arguments: build url and chrome versions to be used, rest of
-the arguments are optional. Builds must be specified in ascending order, with
-the lower version first and the higher version last, each separated by commas.
-Separate arguments are required for install and update test scenarios. The
-setUp method creates a ChromeDriver instance, which can be used to run tests.
-ChromeDriver is shutdown upon completion of the testcase.
-
-Example:
-    $ python sample_updater.py --url=<URL> --builds=19.0.1069.0,19.0.1070.0
-"""
+"""Tests that demonstrate use of install test framework."""
 
 import os
 import sys
 import unittest
 
 from install_test import InstallTest
-from install_test import Main
 
 
 class SampleUpdater(InstallTest):
@@ -49,7 +36,3 @@ class SampleUpdater(InstallTest):
     self.Install(self.GetInstallBuild())
     self.StartChrome()
     self._driver.get('http://www.google.org/')
-
-
-if __name__ == '__main__':
-  Main()
