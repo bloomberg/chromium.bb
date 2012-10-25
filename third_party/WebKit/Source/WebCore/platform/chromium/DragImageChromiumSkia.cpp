bloomberg/chromium.bb

commit a497a5c9601486aec857d87e24c6d72c6df3ec98

Honor image orientation in GraphicsContextSkia https://bugs.webkit.org/show_bug.cgi?id=100179
Reviewed by Stephen White.

Also fix a bug in ImageOrientation.h: Some of the values were
switched. They now match the description in the exif spec at
http://www.exif.org/Exif2-2.PDF page 18, and the notes at
http://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/EXIF.html and
the picture at http://www.80sidea.com/archives/2316.

They showed up correctly in Safari because the orientation transform
was done after flipping Y. Let the flipping happen later. I verified
that flipping transformation order and fixing the exif enum cancelled
each other out, so this has no effect on the apple port.

Not hooked up yet, so no observable difference.

* platform/graphics/BitmapImage.h:
* platform/graphics/ImageOrientation.h:
(WebCore::ImageOrientation::usesWidthAsHeight):
(WebCore::ImageOrientation::fromEXIFValue):
* platform/graphics/cg/GraphicsContextCG.cpp:
(WebCore::GraphicsContext::drawNativeImage):
* platform/graphics/skia/ImageSkia.cpp:
(WebCore::paintSkBitmap):
(WebCore::BitmapImage::draw):
(WebCore):


git-svn-id: svn://svn.chromium.org/blink/trunk@132384 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 8473e41ac90996e87ca8452f9516d1300d36e4c9

third_party/WebKit/Source/WebCore/platform/graphics/ImageOrientation.h
@@ -36,13 +36,13 @@ class FloatSize;
 enum ImageOrientationEnum {
     // "TopLeft" means that the 0 row starts at the Top, the 0 column starts at the Left.
     OriginTopLeft = 1, // default
-    OriginTopRight = 2, // mirror along y-axes
+    OriginTopRight = 2, // mirror along y-axis
     OriginBottomRight = 3, // 180 degree rotation
-    OriginBottomLeft = 4, // mirror along the x-axes
-    OriginLeftTop = 5, // -90 degree rotation + mirror along x-axes
-    OriginRightTop = 6, // 90 degree rotation
-    OriginRightBottom = 7, // 90 degree rotation + mirror along x-axes
-    OriginLeftBottom = 8, // -90 degree rotation
+    OriginBottomLeft = 4, // mirror along the x-axis
+    OriginRightBottom = 5, // mirror along x-axis + 270 degree CW rotation
+    OriginLeftBottom = 6, // 90 degree CW rotation
+    OriginLeftTop = 7, // mirror along x-axis + 90 degree CW rotation
+    OriginRightTop = 8, // 270 degree CW rotation
     // All other values are "reserved" as of EXIF 2.2
     DefaultImageOrientation = OriginTopLeft,
 };
@@ -62,7 +62,7 @@ public:
     bool usesWidthAsHeight() const
     {
         // Values 5 through 8 all flip the width/height.
-        return m_orientation >= OriginLeftTop;
+        return m_orientation >= OriginRightBottom;
     }
 
     // ImageOrientationEnum currently matches EXIF values, however code outside
@@ -70,7 +70,7 @@ public:
     static ImageOrientation fromEXIFValue(int exifValue)
     {
         // Values direct from images may be invalid, in which case we use the default.
-        if (exifValue < OriginTopLeft || exifValue > OriginLeftBottom)
+        if (exifValue < OriginTopLeft || exifValue > OriginRightTop)
             return DefaultImageOrientation;
         return static_cast<ImageOrientationEnum>(exifValue);
     }

third_party/WebKit/Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
@@ -326,11 +326,6 @@ void GraphicsContext::drawNativeImage(NativeImagePtr imagePtr, const FloatSize& 
 
     setPlatformCompositeOperation(op);
 
-    // Flip the coords.
-    CGContextTranslateCTM(context, adjustedDestRect.x(), adjustedDestRect.maxY());
-    CGContextScaleCTM(context, 1, -1);
-    adjustedDestRect.setLocation(FloatPoint());
-
     if (orientation != DefaultImageOrientation) {
         CGContextConcatCTM(context, orientation.transformFromDefault(adjustedDestRect.size()));
         if (orientation.usesWidthAsHeight()) {
@@ -340,6 +335,11 @@ void GraphicsContext::drawNativeImage(NativeImagePtr imagePtr, const FloatSize& 
         }
     }
     
+    // Flip the coords.
+    CGContextTranslateCTM(context, adjustedDestRect.x(), adjustedDestRect.maxY());
+    CGContextScaleCTM(context, 1, -1);
+    adjustedDestRect.setLocation(FloatPoint());
+
     // Adjust the color space.
     image = Image::imageWithColorSpace(image.get(), styleColorSpace);
 

third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageSkia.cpp
@@ -389,9 +389,9 @@ static void paintSkBitmap(PlatformContextSkia* platformContext, const NativeImag
     }
 
     if (resampling == RESAMPLE_NONE) {
-      // FIXME: This is to not break tests (it results in the filter bitmap flag
-      // being set to true). We need to decide if we respect RESAMPLE_NONE
-      // being returned from computeResamplingMode.
+        // FIXME: This is to not break tests (it results in the filter bitmap flag
+        // being set to true). We need to decide if we respect RESAMPLE_NONE
+        // being returned from computeResamplingMode.
         resampling = RESAMPLE_LINEAR;
     }
     resampling = limitResamplingMode(platformContext, resampling);
@@ -582,8 +582,12 @@ void BitmapImage::checkForSolidColor()
     }
 }
 
-void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& dstRect,
-                       const FloatRect& srcRect, ColorSpace colorSpace, CompositeOperator compositeOp)
+void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& dstRect, const FloatRect& srcRect, ColorSpace colorSpace, CompositeOperator compositeOp)
+{
+    draw(ctxt, dstRect, srcRect, colorSpace, compositeOp, DoNotRespectImageOrientation);
+}
+
+void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& dstRect, const FloatRect& srcRect, ColorSpace colorSpace, CompositeOperator compositeOp, RespectImageOrientationEnum shouldRespectImageOrientation)
 {
     if (!m_source.initialized())
         return;
@@ -602,6 +606,21 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& dstRect,
 
     if (normSrcRect.isEmpty() || normDstRect.isEmpty())
         return; // Nothing to draw.
+
+    ImageOrientation orientation = DefaultImageOrientation;
+    if (shouldRespectImageOrientation == RespectImageOrientation)
+        orientation = frameOrientationAtIndex(m_currentFrame);
+
+    GraphicsContextStateSaver saveContext(*ctxt, false);
+    if (orientation != DefaultImageOrientation) {
+        saveContext.save();
+        ctxt->concatCTM(orientation.transformFromDefault(normDstRect.size()));
+        if (orientation.usesWidthAsHeight()) {
+            // The destination rect will have it's width and height already reversed for the orientation of
+            // the image, as it was needed for page layout, so we need to reverse it back here.
+            normDstRect = FloatRect(normDstRect.x(), normDstRect.y(), normDstRect.height(), normDstRect.width());
+        }
+    }
 
     paintSkBitmap(ctxt->platformContext(),
         *bm,

third_party/WebKit/Source/WebCore/platform/graphics/BitmapImage.h
@@ -204,7 +204,7 @@ protected:
     virtual void drawFrameMatchingSourceSize(GraphicsContext*, const FloatRect& dstRect, const IntSize& srcSize, ColorSpace styleColorSpace, CompositeOperator);
 #endif
     virtual void draw(GraphicsContext*, const FloatRect& dstRect, const FloatRect& srcRect, ColorSpace styleColorSpace, CompositeOperator);
-#if USE(CG)
+#if USE(CG) || PLATFORM(CHROMIUM)
     virtual void draw(GraphicsContext*, const FloatRect& dstRect, const FloatRect& srcRect, ColorSpace styleColorSpace, CompositeOperator, RespectImageOrientationEnum) OVERRIDE;
 #endif
 

commit 2ce924b665edd5d6493c9ce9ac957b6ad04e8e84

Source/WebCore: Move ImageOrientation transform back in CG space (lower-left origin), adapt the skia code to work with that, turn on feature for chromium https://bugs.webkit.org/show_bug.cgi?id=100401
Reviewed by Eric Seidel.

This is an alternative fix for the regression caused by r132384 / bug
100179. The bug exists because different pieces of code didn't agree
which space the transformation returned by ImageOrientation was in.

r132384 moved it from CG coordinates to skia coordinates, but didn't
do it completetely. This CL reverts this part of r132384 so that the
transformation is in CG coordinates again, and adapts the skia code to
work in that space.

Alternatively, https://bugs.webkit.org/show_bug.cgi?id=100319 woudl
make the cg code work with the transform in skia coordinates.

Covered by fast/images/exif-orientation.html

* platform/chromium/DragImageChromiumSkia.cpp:
(WebCore::createDragImageFromImage):
* platform/graphics/ImageOrientation.cpp:
(WebCore::ImageOrientation::transformFromDefault):
* platform/graphics/ImageOrientation.h:
(WebCore):
(WebCore::ImageOrientation::usesWidthAsHeight):
(WebCore::ImageOrientation::fromEXIFValue):
(ImageOrientation):
* platform/graphics/cg/GraphicsContextCG.cpp:
(WebCore::GraphicsContext::drawNativeImage):
* platform/graphics/skia/ImageSkia.cpp:
(WebCore::BitmapImage::draw):
* platform/mac/DragImageMac.mm:
(WebCore::createDragImageFromImage):
* rendering/RenderObject.cpp:
(WebCore::RenderObject::shouldRespectImageOrientation):

LayoutTests: Move ImageOrientation transform back in CG space (lower-left origin), adapt the skia code to work with that, turn on feature for chromium
https://bugs.webkit.org/show_bug.cgi?id=100319

Reviewed by Eric Seidel.

* platform/chromium/TestExpectations:


git-svn-id: svn://svn.chromium.org/blink/trunk@132525 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: da5fc33d50bab0ce75f67d49c7094e777337759d

third_party/WebKit/Source/WebCore/platform/chromium/DragImageChromiumSkia.cpp
@@ -127,13 +127,20 @@ DragImageRef createDragImageFromImage(Image* image, RespectImageOrientationEnum 
                 SkBitmap::kARGB_8888_Config, sizeRespectingOrientation.width(), sizeRespectingOrientation.height());
             dragImageChromium->bitmap->allocPixels();
 
-            SkCanvas canvas(*dragImageChromium->bitmap);
-            SkMatrix transform = orientation.transformFromDefault(sizeRespectingOrientation);
-            canvas.concat(transform);
-
             FloatRect destRect(FloatPoint(), sizeRespectingOrientation);
+            SkCanvas canvas(*dragImageChromium->bitmap);
+
+            // ImageOrientation expects the origin in the lower left corner, so flip, transform, and then...
+            canvas.translate(0, destRect.height());
+            canvas.scale(1, -1);
+            canvas.concat(orientation.transformFromDefault(sizeRespectingOrientation));
+
             if (orientation.usesWidthAsHeight())
                 destRect = FloatRect(destRect.x(), destRect.y(), destRect.height(), destRect.width());
+
+            // ...flip back.
+            canvas.translate(0, destRect.height());
+            canvas.scale(1, -1);
 
             canvas.drawBitmapRect(bitmap->bitmap(), 0, destRect);
             return dragImageChromium;

third_party/WebKit/Source/WebCore/platform/graphics/ImageOrientation.h
@@ -39,10 +39,10 @@ enum ImageOrientationEnum {
     OriginTopRight = 2, // mirror along y-axis
     OriginBottomRight = 3, // 180 degree rotation
     OriginBottomLeft = 4, // mirror along the x-axis
-    OriginRightBottom = 5, // mirror along x-axis + 270 degree CW rotation
-    OriginLeftBottom = 6, // 90 degree CW rotation
-    OriginLeftTop = 7, // mirror along x-axis + 90 degree CW rotation
-    OriginRightTop = 8, // 270 degree CW rotation
+    OriginLeftTop = 5, // mirror along x-axis + 270 degree CW rotation
+    OriginRightTop = 6, // 90 degree CW rotation
+    OriginRightBottom = 7, // mirror along x-axis + 90 degree CW rotation
+    OriginLeftBottom = 8, // 270 degree CW rotation
     // All other values are "reserved" as of EXIF 2.2
     DefaultImageOrientation = OriginTopLeft,
 };
@@ -62,7 +62,7 @@ public:
     bool usesWidthAsHeight() const
     {
         // Values 5 through 8 all flip the width/height.
-        return m_orientation >= OriginRightBottom;
+        return m_orientation >= OriginLeftTop;
     }
 
     // ImageOrientationEnum currently matches EXIF values, however code outside
@@ -70,12 +70,13 @@ public:
     static ImageOrientation fromEXIFValue(int exifValue)
     {
         // Values direct from images may be invalid, in which case we use the default.
-        if (exifValue < OriginTopLeft || exifValue > OriginRightTop)
+        if (exifValue < OriginTopLeft || exifValue > OriginLeftBottom)
             return DefaultImageOrientation;
         return static_cast<ImageOrientationEnum>(exifValue);
     }
 
     // This transform can be used for drawing an image according to the orientation.
+    // It should be used in a left-handed coordinate system.
     AffineTransform transformFromDefault(const FloatSize& drawnSize) const;
 
     inline bool operator==(const ImageOrientation& other) const { return other.m_orientation == m_orientation; }

third_party/WebKit/Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
@@ -326,6 +326,11 @@ void GraphicsContext::drawNativeImage(NativeImagePtr imagePtr, const FloatSize& 
 
     setPlatformCompositeOperation(op);
 
+    // Flip the coords.
+    CGContextTranslateCTM(context, adjustedDestRect.x(), adjustedDestRect.maxY());
+    CGContextScaleCTM(context, 1, -1);
+    adjustedDestRect.setLocation(FloatPoint());
+
     if (orientation != DefaultImageOrientation) {
         CGContextConcatCTM(context, orientation.transformFromDefault(adjustedDestRect.size()));
         if (orientation.usesWidthAsHeight()) {
@@ -335,11 +340,6 @@ void GraphicsContext::drawNativeImage(NativeImagePtr imagePtr, const FloatSize& 
         }
     }
     
-    // Flip the coords.
-    CGContextTranslateCTM(context, adjustedDestRect.x(), adjustedDestRect.maxY());
-    CGContextScaleCTM(context, 1, -1);
-    adjustedDestRect.setLocation(FloatPoint());
-
     // Adjust the color space.
     image = Image::imageWithColorSpace(image.get(), styleColorSpace);
 

third_party/WebKit/Source/WebCore/platform/graphics/skia/ImageSkia.cpp
@@ -614,12 +614,25 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& dstRect, const Fl
     GraphicsContextStateSaver saveContext(*ctxt, false);
     if (orientation != DefaultImageOrientation) {
         saveContext.save();
+
+        // ImageOrientation expects the origin to be at (0, 0)
+        ctxt->translate(normDstRect.x(), normDstRect.y());
+        normDstRect.setLocation(FloatPoint());
+
+        // ImageOrientation also expects the origin in the lower left corner, so flip, transform, and then...
+        ctxt->translate(0, normDstRect.height());
+        ctxt->scale(FloatSize(1, -1));
         ctxt->concatCTM(orientation.transformFromDefault(normDstRect.size()));
+
         if (orientation.usesWidthAsHeight()) {
             // The destination rect will have it's width and height already reversed for the orientation of
             // the image, as it was needed for page layout, so we need to reverse it back here.
             normDstRect = FloatRect(normDstRect.x(), normDstRect.y(), normDstRect.height(), normDstRect.width());
         }
+
+        // ...flip back.
+        ctxt->translate(0, normDstRect.height());
+        ctxt->scale(FloatSize(1, -1));
     }
 
     paintSkBitmap(ctxt->platformContext(),

third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
@@ -2227,7 +2227,7 @@ RespectImageOrientationEnum RenderObject::shouldRespectImageOrientation() const
     // Respect the image's orientation if it's being used as a full-page image or it's
     // an <img> and the setting to respect it everywhere is set.
     return
-#if USE(CG)
+#if USE(CG) || PLATFORM(CHROMIUM)
         // This can only be enabled for ports which honor the orientation flag in their drawing code.
         document()->isImageDocument() ||
 #endif

third_party/WebKit/LayoutTests/platform/chromium/TestExpectations
@@ -3314,7 +3314,6 @@ webkit.org/b/83219 [ Lion ] fonts/cursive.html [ Failure Pass ]
 webkit.org/b/83324 [ Win Debug ] fast/js/cross-global-object-inline-global-var.html [ Failure Pass ]
 
 # EXIF orientation support has not yet been implemented for this platform
-webkit.org/b/19688 fast/images/exif-orientation.html
 webkit.org/b/19688 fast/images/exif-orientation-css.html
 
 crbug.com/122462 [ Linux Win ] http/tests/inspector/inspect-element.html [ Failure Slow ]
@@ -3984,6 +3983,9 @@ webkit.org/b/98192 [ Win Debug ] fast/js/dfg-cross-global-object-inline-new-arra
 webkit.org/b/98192 [ Win Debug ] fast/js/post-message-numeric-property.html [ Failure Pass ]
 
 webkit.org/b/98275 media/event-queue-crash.html [ Skip ]
+
+# BUGTHAKIS: Requires baselines
+webkit.org/b/19688 fast/images/exif-orientation.html [ ImageOnlyFailure ]
 
 # These are real failues due to 95121.
 # This is spilling caused by LANCZOS3 scaling algorithm that samples outside the source rect.