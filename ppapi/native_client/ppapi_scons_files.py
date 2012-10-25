4c60cd36e9ee14bd068bab5fd2bd25b5829136cd
ncbray@chromium.org <ncbray@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 19:14:07 +0000

    Remove more redundant PPAPI interface tests from NaCl's SCons build.
    BUG= http://code.google.com/p/chromium/issues/detail?id=154400
    
    
    Review URL: https://chromiumcodereview.appspot.com/11276017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164136 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 68661964609ba64b3215227b2dd122a099d56700

--- ppapi/native_client/ppapi_scons_files.py
+++ ppapi/native_client/ppapi_scons_files.py
@@ -25,18 +25,12 @@ nonvariant_test_scons_files = [
     'tests/ppapi_browser/crash/nacl.scons',
     'tests/ppapi_browser/extension_mime_handler/nacl.scons',
     'tests/ppapi_browser/manifest/nacl.scons',
-    'tests/ppapi_browser/ppb_audio/nacl.scons',
-    'tests/ppapi_browser/ppb_dev/nacl.scons',
     'tests/ppapi_browser/ppb_file_system/nacl.scons',
-    'tests/ppapi_browser/ppb_fullscreen/nacl.scons',
-    'tests/ppapi_browser/ppb_graphics3d/nacl.scons',
     'tests/ppapi_browser/ppb_image_data/nacl.scons',
     'tests/ppapi_browser/ppb_instance/nacl.scons',
-    'tests/ppapi_browser/ppb_var/nacl.scons',
     'tests/ppapi_browser/ppp_input_event/nacl.scons',
     'tests/ppapi_browser/ppp_instance/nacl.scons',
     'tests/ppapi_browser/progress_events/nacl.scons',
-    'tests/ppapi_messaging/nacl.scons',
     'tests/ppapi_test_lib/nacl.scons',
 ]
 
