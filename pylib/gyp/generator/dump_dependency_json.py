20568c84a371274c21a763a5680bad0b9bea081a
torne@chromium.org <torne@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2012-10-26 08:14:28 +0000

    Fix dump_dependency_json.
    dump_dependency_json needs to define CONFIGURATION_NAME like the other
    generators, otherwise it fails when run on real projects.
    Review URL: https://codereview.chromium.org/11271026
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1524 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/generator/dump_dependency_json.py
+++ pylib/gyp/generator/dump_dependency_json.py
@@ -24,7 +24,8 @@ for unused in ['RULE_INPUT_PATH', 'RULE_INPUT_ROOT', 'RULE_INPUT_NAME',
                'RULE_INPUT_DIRNAME', 'RULE_INPUT_EXT',
                'EXECUTABLE_PREFIX', 'EXECUTABLE_SUFFIX',
                'STATIC_LIB_PREFIX', 'STATIC_LIB_SUFFIX',
-               'SHARED_LIB_PREFIX', 'SHARED_LIB_SUFFIX']:
+               'SHARED_LIB_PREFIX', 'SHARED_LIB_SUFFIX',
+               'CONFIGURATION_NAME']:
   generator_default_variables[unused] = ''
 
 
