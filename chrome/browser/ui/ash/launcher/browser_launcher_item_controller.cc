0fec7ba0357fc72163f563846d4a21c30c48325a
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 09:27:53 +0000

    Add NULL check in BrowserLauncherItemController::GetTitle()
    BUG=157772
    R=davemore@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11262037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164303 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 542fdb95131927853de0203b46bb14caaf1a6f1f

--- chrome/browser/ui/ash/launcher/browser_launcher_item_controller.cc
+++ chrome/browser/ui/ash/launcher/browser_launcher_item_controller.cc
@@ -110,10 +110,12 @@ void BrowserLauncherItemController::BrowserActivationStateChanged() {
 
 string16 BrowserLauncherItemController::GetTitle() {
   if (type() == TYPE_TABBED || type() == TYPE_EXTENSION_PANEL) {
-    const content::WebContents* contents =
-        tab_model_->GetActiveTabContents()->web_contents();
-    if (contents)
-      return contents->GetTitle();
+    if (tab_model_->GetActiveTabContents()) {
+      const content::WebContents* contents =
+          tab_model_->GetActiveTabContents()->web_contents();
+      if (contents)
+        return contents->GetTitle();
+    }
   }
   return GetAppTitle();
 }
