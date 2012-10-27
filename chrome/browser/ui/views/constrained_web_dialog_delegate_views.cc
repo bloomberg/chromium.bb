015af48168e7cd078b97c9e41a50a6afc20d7c2c
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-27 00:43:15 +0000

    Revert 164433 - Automatically resizing constrained windows like the print preview with a browser resize
    Broke XP Tests (1).
    
    BUG=157598, 157227
    
    
    Review URL: https://chromiumcodereview.appspot.com/11264027
    
    TBR=skuhne@chromium.org
    Review URL: https://codereview.chromium.org/11335002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164457 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bcfa9233e2c4ec3135fa62f2ac3bbff539f0150b

--- chrome/browser/ui/views/constrained_web_dialog_delegate_views.cc
+++ chrome/browser/ui/views/constrained_web_dialog_delegate_views.cc
@@ -130,12 +130,6 @@ class ConstrainedWebDialogDelegateViewViews
     return this;
   }
 
-  virtual void OnWidgetMove() OVERRIDE {
-    GetWidget()->CenterWindow(
-        GetWidget()->non_client_view()->GetPreferredSize());
-    views::WidgetDelegate::OnWidgetMove();
-  }
-
   // views::WebView overrides.
   virtual bool AcceleratorPressed(
       const ui::Accelerator& accelerator) OVERRIDE {
