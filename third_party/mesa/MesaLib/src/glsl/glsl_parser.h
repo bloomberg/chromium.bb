21235b14ba594e1d38992b521ef03add7efaa93d
bungeman@chromium.org <bungeman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 21:06:13 +0000

    Revert 165006 - Revert 165005 until skia_webkit.gyp is present everywhere.
    TBR=bungeman@chromium.org
    Review URL: https://codereview.chromium.org/11344041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165007 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f85313a7d1d03c27abea107fd1b88b9fe50ae8cc

--- third_party/mesa/MesaLib/src/glsl/glsl_parser.h
+++ third_party/mesa/MesaLib/src/glsl/glsl_parser.h
@@ -1,8 +1,9 @@
-/* A Bison parser, made by GNU Bison 2.5.  */
+/* A Bison parser, made by GNU Bison 2.4.3.  */
 
-/* Bison interface for Yacc-like parsers in C
+/* Skeleton interface for Bison's Yacc-like parsers in C
    
-      Copyright (C) 1984, 1989-1990, 2000-2011 Free Software Foundation, Inc.
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
+   2009, 2010 Free Software Foundation, Inc.
    
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -234,7 +235,7 @@
 typedef union YYSTYPE
 {
 
-/* Line 2068 of yacc.c  */
+/* Line 1685 of yacc.c  */
 #line 52 "glsl_parser.ypp"
 
    int n;
@@ -270,8 +271,8 @@ typedef union YYSTYPE
 
 
 
-/* Line 2068 of yacc.c  */
-#line 275 "glsl_parser.h"
+/* Line 1685 of yacc.c  */
+#line 276 "glsl_parser.h"
 } YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define yystype YYSTYPE /* obsolescent; will be withdrawn */
