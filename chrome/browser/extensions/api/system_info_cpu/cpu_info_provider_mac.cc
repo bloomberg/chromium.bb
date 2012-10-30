d3ec50675a9ed53705cec410327fe44eb0f3d2ab
hongbo.min@intel.com <hongbo.min@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 05:47:07 +0000

    Add code to implement querying CPU time for each processor on Mac for systemInfo.cpu API.
    BUG=136519
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/11233037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164863 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 04b0cf4e1e16c3dc7f5ea9702ffe407586d4f9de

--- chrome/browser/extensions/api/system_info_cpu/cpu_info_provider_mac.cc
+++ chrome/browser/extensions/api/system_info_cpu/cpu_info_provider_mac.cc
@@ -4,10 +4,48 @@
 
 #include "chrome/browser/extensions/api/system_info_cpu/cpu_info_provider.h"
 
+#include <mach/mach_host.h>
+
+#include "base/mac/scoped_mach_port.h"
+
 namespace extensions {
 
 bool CpuInfoProvider::QueryCpuTimePerProcessor(std::vector<CpuTime>* times) {
-  // TODO(hongbo): Query the cpu time.
+  natural_t num_of_processors;
+  base::mac::ScopedMachPort host(mach_host_self());
+  mach_msg_type_number_t type;
+  processor_cpu_load_info_data_t* cpu_infos;
+
+  if (host_processor_info(host.get(),
+                          PROCESSOR_CPU_LOAD_INFO,
+                          &num_of_processors,
+                          reinterpret_cast<processor_info_array_t*>(&cpu_infos),
+                          &type) == KERN_SUCCESS) {
+    std::vector<CpuTime> results;
+    int64 user = 0, nice = 0, sys = 0, idle = 0;
+
+    for (natural_t i = 0; i < num_of_processors; ++i) {
+      CpuTime time;
+
+      user = static_cast<int64>(cpu_infos[i].cpu_ticks[CPU_STATE_USER]);
+      sys  = static_cast<int64>(cpu_infos[i].cpu_ticks[CPU_STATE_SYSTEM]);
+      nice = static_cast<int64>(cpu_infos[i].cpu_ticks[CPU_STATE_NICE]);
+      idle = static_cast<int64>(cpu_infos[i].cpu_ticks[CPU_STATE_IDLE]);
+
+      time.kernel = sys;
+      time.user = user + nice;
+      time.idle = idle;
+      results.push_back(time);
+    }
+
+    vm_deallocate(mach_task_self(),
+                  reinterpret_cast<vm_address_t>(cpu_infos),
+                  num_of_processors * sizeof(processor_cpu_load_info));
+
+    times->swap(results);
+    return true;
+  }
+
   return false;
 }
 
