78cdc6b430507e07c009de22c2e115d649a01bfd
mmontgomery@chromium.org <mmontgomery@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-31 00:37:32 +0000

    Add "dump to file" button to about:sync.
    In the Data tab, there's a "dump to text" button. This adds a
    "dump to file" button which triggers a download of a bunch of
    debugging data suitable for attaching to a bug.
    
    The format is fake CSV. There's a header with date/time plus
    useragent, followed by field labels, and then the data.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11232067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165073 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d7edbed02fab01f423aadebc960c1524296acaeb

--- chrome/browser/resources/sync_internals/data.js
+++ chrome/browser/resources/sync_internals/data.js
@@ -3,8 +3,8 @@
 // found in the LICENSE file.
 
 (function() {
-var dumpToTextButton = document.getElementById('dump-to-text');
-var dataDump = document.getElementById('data-dump');
+var dumpToTextButton = $('dump-to-text');
+var dataDump = $('data-dump');
 dumpToTextButton.addEventListener('click', function(event) {
   // TODO(akalin): Add info like Chrome version, OS, date dumped, etc.
 
@@ -30,5 +30,151 @@ dumpToTextButton.addEventListener('click', function(event) {
   data += '\n';
 
   dataDump.textContent = data;
+});
+
+// TODO(mmontgomery): add SPECIFICS as an opt-in.
+var allFields = [
+  'ID',
+  'IS_UNSYNCED',
+  'IS_UNAPPLIED_UPDATE',
+  'BASE_VERSION',
+  'BASE_VERSION_TIME',
+  'SERVER_VERSION',
+  'SERVER_VERSION_TIME',
+  'PARENT_ID',
+  'SERVER_PARENT_ID',
+  'IS_DEL',
+  'SERVER_IS_DEL',
+  'serverModelType',
+];
+
+function versionToDateString(version) {
+  // TODO(mmontgomery): ugly? Hacky? Is there a better way?
+  var epochLength = Date.now().toString().length;
+  var epochTime = parseInt(version.slice(0, epochLength));
+  var date = new Date(epochTime);
+  return date.toString();
+}
+
+function getFields(node) {
+  return allFields.map(function(field) {
+    var fieldVal;
+    if (field == 'SERVER_VERSION_TIME') {
+      var version = node['SERVER_VERSION'];
+      fieldVal = versionToDateString(version);
+    } if (field == 'BASE_VERSION_TIME') {
+      var version = node['BASE_VERSION'];
+      fieldVal = versionToDateString(version);
+    } else {
+      fieldVal = node[field];
+    }
+    return fieldVal;
+  });
+}
+
+function isSelectedDatatype(node) {
+  var type = node.serverModelType;
+  var typeCheckbox = $(type);
+  // Some types, such as 'Top level folder', appear in the list of nodes
+  // but not in the list of selectable items.
+  if (typeCheckbox == null) {
+    return false;
+  }
+  return typeCheckbox.checked;
+}
+
+function makeBlobUrl(data) {
+  var textBlob = new Blob([data], {type: 'octet/stream'});
+  var blobUrl = window.webkitURL.createObjectURL(textBlob);
+  return blobUrl;
+}
+
+function makeDownloadName() {
+  // Format is sync-data-dump-$epoch-$year-$month-$day-$OS.csv.
+  var now = new Date();
+  var friendlyDate = [now.getFullYear(),
+                      now.getMonth(),
+                      now.getDate()].join('-');
+  var name = ['sync-data-dump',
+              Date.now(),
+              friendlyDate,
+              navigator.platform].join('-');
+  return [name, 'csv'].join('.');
+}
+
+function makeDateUserAgentHeader() {
+  var now = new Date();
+  var userAgent = window.navigator.userAgent;
+  var dateUaHeader = [now.toISOString(), userAgent].join(',');
+  return dateUaHeader;
+}
+
+function triggerDataDownload(data) {
+  // Prepend a header with ISO date and useragent.
+  var output = [makeDateUserAgentHeader()];
+  output.push('=====');
+
+  var aboutInfo = JSON.stringify(chrome.sync.aboutInfo, null, 2);
+  output.push(aboutInfo);
+
+  if (data != null && data.length > 0) {
+    output.push('=====');
+    var fieldLabels = allFields.join(',');
+    output.push(fieldLabels);
+
+    var data = data.filter(isSelectedDatatype);
+    data = data.map(getFields);
+    var dataAsString = data.join('\n');
+    output.push(dataAsString);
+  }
+  output = output.join('\n');
+
+  var anchor = $('dump-to-file-anchor');
+  anchor.href = makeBlobUrl(output);
+  anchor.download = makeDownloadName();
+  anchor.click();
+}
+
+function createTypesCheckboxes(types) {
+  var containerElt = $('node-type-checkboxes');
+
+  types.map(function(type) {
+    var div = document.createElement('div');
+
+    var checkbox = document.createElement('input');
+    checkbox.id = type;
+    checkbox.type = 'checkbox';
+    checkbox.checked = 'yes';
+    div.appendChild(checkbox);
+
+    var label = document.createElement('label');
+    // Assigning to label.for doesn't work.
+    label.setAttribute('for', type);
+    label.innerText = type;
+    div.appendChild(label);
+
+    containerElt.appendChild(div);
+  });
+}
+
+function populateDatatypes(childNodeSummaries) {
+  var types = childNodeSummaries.map(function(n) {
+    return n.type;
+  });
+  types = types.sort();
+  createTypesCheckboxes(types);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  chrome.sync.getRootNodeDetails(function(rootNode) {
+    chrome.sync.getChildNodeIds(rootNode.id, function(childNodeIds) {
+      chrome.sync.getNodeSummariesById(childNodeIds, populateDatatypes);
+    });
+  });
+});
+
+var dumpToFileLink = $('dump-to-file');
+dumpToFileLink.addEventListener('click', function(event) {
+  chrome.sync.getAllNodes(triggerDataDownload);
 });
 })();
