1d2845bf1b4b6ead2af7adcd847f653bef5bcf3c
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-31 11:04:04 +0000

    Enable SafeBrowsingServiceShutdownTest.DontStartAfterShutdown but on ChromeOS.
    The failure is confirmed to be specific to ChromeOS, and not originated from
    the tested part.
    
    BUG=158285
    TBR=mattm@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11358004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165144 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a03d5e9c8fe0da48a12592085646e525f68c8e82

--- chrome/browser/safe_browsing/safe_browsing_service_browsertest.cc
+++ chrome/browser/safe_browsing/safe_browsing_service_browsertest.cc
@@ -815,9 +815,14 @@ class SafeBrowsingServiceShutdownTest : public SafeBrowsingServiceTest {
   Profile* profile2_;
 };
 
-// Crashes, see http://crbug.com/158285.
+// Fails on Chrome OS, see http://crbug.com/158285.
+#if defined(OS_CHROMEOS)
+#define MAYBE_DontStartAfterShutdown DISABLED_DontStartAfterShutdown
+#else
+#define MAYBE_DontStartAfterShutdown DontStartAfterShutdown
+#endif
 IN_PROC_BROWSER_TEST_F(SafeBrowsingServiceShutdownTest,
-                       DISABLED_DontStartAfterShutdown) {
+                       MAYBE_DontStartAfterShutdown) {
   CreateCSDService();
   SafeBrowsingService* sb_service = g_browser_process->safe_browsing_service();
   safe_browsing::ClientSideDetectionService* csd_service =
