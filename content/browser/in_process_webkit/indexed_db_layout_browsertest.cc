df100c6d7013eff928ca62a002ef48586f3ec113
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 03:25:00 +0000

    Reenable two IDB intversion tests
    BUG=150691,150947
    
    Review URL: https://chromiumcodereview.appspot.com/11364087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166115 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0213e6b712a942233d6fb18bf11cee0bd6769620

--- content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
+++ content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
@@ -94,8 +94,8 @@ const char* kIntVersionTests[] = {
   "intversion-abort-in-initial-upgradeneeded.html",
   "intversion-and-setversion.html",
   "intversion-blocked.html",
-  // "intversion-close-between-events.html", // crbug.com/150947
-  // "intversion-close-in-oncomplete.html", // crbug.com/150691
+  "intversion-close-between-events.html",
+  "intversion-close-in-oncomplete.html",
   "intversion-close-in-upgradeneeded.html",
   "intversion-delete-in-upgradeneeded.html",
   // "intversion-gated-on-delete.html", // behaves slightly differently in DRT
