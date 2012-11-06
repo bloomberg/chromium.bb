521dccc07f98369e9e99f03470b581ef9c846cae
rfevang@chromium.org <rfevang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 23:06:48 +0000

    Fix VisitFilterTest.GetVisitScore so it doesn't fail during DST transitions.
    Changed the test to use a fixed date instead of the current day.
    
    
    BUG=159384
    
    
    Review URL: https://chromiumcodereview.appspot.com/11369086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166292 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c473d914a87151e73ceb7212ba0ed076b73b4085

--- chrome/browser/history/visit_filter_unittest.cc
+++ chrome/browser/history/visit_filter_unittest.cc
@@ -239,9 +239,10 @@ TEST_F(VisitFilterTest, IntersectTimeVectors) {
   EXPECT_TRUE(result.empty());
 }
 
-// Disabled because this test is not DST-safe. http://crbug.com/159384
-TEST_F(VisitFilterTest, DISABLED_GetVisitScore) {
-  base::Time filter_time = GetClosestMidday();
+TEST_F(VisitFilterTest, GetVisitScore) {
+  base::Time filter_time;
+  ASSERT_TRUE(base::Time::FromString("Tue, 24 Apr 2012, 12:00:00",
+                                     &filter_time));
   VisitFilter filter;
   VisitRow visit;
 
