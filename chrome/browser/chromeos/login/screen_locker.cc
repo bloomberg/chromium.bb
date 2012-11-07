0dff83ce990bd9f1150c3a234afc20559dcdc7e9
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 15:36:39 +0000

    Remove BrowserList::GetLastActive usage in CrOS Screen Locker.
    BUG=129187
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11361139
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166437 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7a3a2ccf21d70ed5e6ab3140ee56273b6ee7b2ea

--- chrome/browser/chromeos/login/screen_locker.cc
+++ chrome/browser/chromeos/login/screen_locker.cc
@@ -33,7 +33,7 @@
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
-#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
@@ -318,7 +318,8 @@ void ScreenLocker::Show() {
   }
 
   // Exit fullscreen.
-  Browser* browser = BrowserList::GetLastActive();
+  Browser* browser = browser::FindLastActiveWithHostDesktopType(
+      chrome::HOST_DESKTOP_TYPE_ASH);
   // browser can be NULL if we receive a lock request before the first browser
   // window is shown.
   if (browser && browser->window()->IsFullscreen()) {
