09d07a04a2a509176130328a62193344369a3063
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 13:49:15 +0000

    Fix black screen if guest login and logout before first user login
    BUG=152964
    
    
    Review URL: https://chromiumcodereview.appspot.com/11362108
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166414 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ed129c631180f4866dfbc3d31a13d6cc2e1a3643

--- chrome/browser/chromeos/login/wallpaper_manager.cc
+++ chrome/browser/chromeos/login/wallpaper_manager.cc
@@ -312,21 +312,19 @@ void WallpaperManager::InitializeWallpaper() {
             kAccountsPrefShowUserNamesOnSignIn, &show_users);
         DCHECK(result) << "Unable to fetch setting "
                        << kAccountsPrefShowUserNamesOnSignIn;
-        if (!show_users) {
+        const chromeos::UserList& users = user_manager->GetUsers();
+        if (!show_users || users.empty()) {
           // Boot into sign in form, preload default wallpaper.
           ash::Shell::GetInstance()->desktop_background_controller()->
               SetDefaultWallpaper(kDefaultOOBEWallpaperIndex);
-        } else if (!disable_boot_animation) {
+          return;
+        }
+
+        if (!disable_boot_animation) {
           // Normal boot, load user wallpaper.
           // If normal boot animation is disabled wallpaper would be set
           // asynchronously once user pods are loaded.
-          const chromeos::UserList& users = user_manager->GetUsers();
-          if (!users.empty()) {
-            SetUserWallpaper(users[0]->email());
-          } else {
-            ash::Shell::GetInstance()->desktop_background_controller()->
-                SetDefaultWallpaper(kDefaultOOBEWallpaperIndex);
-          }
+          SetUserWallpaper(users[0]->email());
         }
       }
     }
