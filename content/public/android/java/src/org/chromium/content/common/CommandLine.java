1c049a8953649f29cf7ae2ff4010c6f9c793081f
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 23:25:25 +0000

    This adjusts the tile-size to 512x512 for the Nexus 10 quickly. This is so we can work on Manta's scrolling and texture upload performance and compare with m18 which used 512x512 tiles, and find any tile-size related bugs early.
    In upstream Chrome, there is no code to adjusts the tile
    size for high-res/dpi displays, so we should implement a
    generic solution which works across all platforms and
    devices. This bug tracks this feature:
    http://crbug.com/159524
    
    BUG=159524
    
    
    Review URL: https://chromiumcodereview.appspot.com/11358106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166545 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d87dbfd9ad2751ecdbb155459e55ac8b2cd9486f

--- content/public/android/java/src/org/chromium/content/common/CommandLine.java
+++ content/public/android/java/src/org/chromium/content/common/CommandLine.java
@@ -57,6 +57,11 @@ public abstract class CommandLine {
     // Sets the ISO country code that will be used for phone number detection.
     public static final String NETWORK_COUNTRY_ISO = "network-country-iso";
 
+    // Sets the default tile width/height. We use this for very high-res
+    // displays until Chrome can detect and do this correctly for all platforms.
+    public static final String DEFAULT_TILE_WIDTH = "default-tile-width";
+    public static final String DEFAULT_TILE_HEIGHT = "default-tile-height";
+
     // Public abstract interface, implemented in derived classes.
     // All these methods reflect their native-side counterparts.
     /**
