b916dda3d8a293807e3be44ed4b68c3ee0c0de9f
phosek@chromium.org <phosek@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2012-11-09 11:02:22 +0000

    Handle case when mmap_min_addr != CONFIG_LSM_MMAP_MIN_ADDR under SELinux.
    When SELinux is enabled and mmap_low_allowed is set to false, the system
    ignores mman_min_addr value and does not allow mapping addresses bellow
    CONFIG_LSM_MMAP_MIN_ADDR. While properly handled by nacl_bootstrap, it
    causes nacl_bootstrap_preservation_test to fail.
    
    BUG=none
    TESTS=small_tests
    
    Review URL: https://codereview.chromium.org/11187022
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@10221 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 59412d2c83605780770276e4a42b3f062812db50

--- src/trusted/service_runtime/linux/nacl_bootstrap_prereservation_test.c
+++ src/trusted/service_runtime/linux/nacl_bootstrap_prereservation_test.c
@@ -4,9 +4,11 @@
  * found in the LICENSE file.
  */
 
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 
 #include "native_client/src/include/nacl_assert.h"
 #include "native_client/src/shared/platform/nacl_check.h"
@@ -14,56 +16,74 @@
 #include "native_client/src/trusted/service_runtime/sel_ldr.h"
 #include "native_client/src/trusted/service_runtime/sel_util.h"
 
-size_t GetProcSysVmMmapMinAddr(void) {
-  FILE *fp;
-  char buf[32];
-  size_t min_addr = 0;
-  /*
-   * The first possible address is at vm.mmap_min_addr (which is usually at 64K
-   * but is sometimes set lower), which can be found in
-   * /proc/sys/vm/mmap_min_addr.
-   */
-  fp = fopen("/proc/sys/vm/mmap_min_addr", "r");
-  CHECK(fp != NULL);
-  CHECK(fgets(buf, sizeof(buf), fp) == buf);
-  NaClLog(LOG_INFO, "/proc/sys/vm/mmap_min_addr contains: %s", buf);
-  CHECK(sscanf(buf, "%zu", &min_addr) == 1);
-  /*
-   * Sometimes, mmap_min_addr is not page-aligned, but we can only map on page
-   * boundaries.  Thus, the minimum address is the first page boundary above
-   * vm.mmap_min_addr.
-   */
-  min_addr = NaClRoundPage(min_addr);
-  fclose(fp);
-  return min_addr;
+/*
+ * Duplicates the definition in the nacl_bootstrap.x linker script.
+ */
+#define TEXT_START 0x10000
+
+int ParseMapsLine(FILE *fp, uintptr_t *start, uintptr_t *end) {
+  char buf[256];
+
+  if (!fgets(buf, sizeof(buf), fp)) {
+    return 0;
+  }
+
+  if (sscanf(buf, "%"SCNxPTR"-%"SCNxPTR" %*4[-rwxp] %*8x %*2x:%*2x %*16x",
+             start, end) != 2) {
+    return 0;
+  }
+
+  return 1;
 }
 
-/*
- * FindMinUnreservedAddress returns the smallest unrereserved address
- * in the address space above vm.mmap_min_addr of the process running this
- * program by examining the file /proc/self/maps.
- */
-size_t FindMinUnreservedAddress(void) {
+void FindLowestMappedRange(uintptr_t *start, uintptr_t *end) {
   FILE *fp;
-  char buf[256];
-  int result;
-  size_t last_end = GetProcSysVmMmapMinAddr();
+  uintptr_t next_start;
+  uintptr_t next_end;
 
   fp = fopen("/proc/self/maps", "r");
   CHECK(fp != NULL);
-  /* Scan the maps file for the first address that is unreserved. */
-  while (fgets(buf, sizeof(buf), fp)) {
-    size_t start;
-    size_t end;
-    NaClLog(LOG_INFO, "Read line from /proc/self/maps: %s", buf);
-    result = sscanf(buf, "%zx-%zx %*4[-rwxp] %*8x %*2x:%*2x %*16x", &start,
-                    &end);
-    if (result != 2 || start != last_end)
+
+  CHECK(ParseMapsLine(fp, start, end));
+  while (ParseMapsLine(fp, &next_start, &next_end)) {
+    if (next_start != *end) {
       break;
-    last_end = end;
+    }
+    *end = next_end;
   }
+
   fclose(fp);
-  return last_end;
+}
+
+int IsPageMappable(uintptr_t addr) {
+  void *retval = mmap((void *) addr, NACL_PAGESIZE, PROT_NONE,
+                      MAP_PRIVATE | MAP_FIXED |
+                      MAP_ANONYMOUS | MAP_NORESERVE,
+                      -1, 0);
+  if (retval != (void *) addr)   {
+    CHECK(MAP_FAILED == retval &&
+          (EPERM == errno || EACCES == errno ||
+           (EINVAL == errno && 0 == addr)));
+    return 0;
+  }
+  return 1;
+}
+
+uintptr_t FindLowestMappableAddress() {
+  uintptr_t mmap_addr;
+  uintptr_t low_addr;
+
+  /* Find lowest mappable address */
+  for (mmap_addr = TEXT_START; mmap_addr > 0; mmap_addr -= NACL_PAGESIZE) {
+    if (!IsPageMappable(mmap_addr - NACL_PAGESIZE)) {
+      break;
+    }
+  }
+  /* Check that all lower addresses are unmappable */
+  for (low_addr = mmap_addr; low_addr > 0; low_addr -= NACL_PAGESIZE) {
+    CHECK(!IsPageMappable(low_addr - NACL_PAGESIZE));
+  }
+  return mmap_addr;
 }
 
 /*
@@ -73,6 +93,10 @@ size_t FindMinUnreservedAddress(void) {
  * command line option.
  */
 int main(int argc, char **argv) {
+  uintptr_t start;
+  uintptr_t end;
+  size_t lowest_mappable_addr;
+
   NaClHandleBootstrapArgs(&argc, &argv);
 
   /* On Linux x86-64, there is no prereserved memory. */
@@ -87,7 +111,11 @@ int main(int argc, char **argv) {
    * first address which is not reserved in the address space.  If this
    * is the case, then enough memory was reserved.
    */
-  ASSERT_LE(g_prereserved_sandbox_size, FindMinUnreservedAddress());
+  FindLowestMappedRange(&start, &end);
+  ASSERT_LE(g_prereserved_sandbox_size, end);
+  ASSERT_LE(start, TEXT_START);
+  lowest_mappable_addr = FindLowestMappableAddress();
+  ASSERT_EQ(start, lowest_mappable_addr);
   printf("Found the right amount of prereserved memory\n");
   return 0;
 }
