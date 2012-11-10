fedfbfe6f6d95705317736308eb3e0c1bed2830c
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-10 03:41:16 +0000

    PepperPlatformImage2DImpl: don't immediately map shared memory
    PepperPlatformImage2DImpl::Create calls TransportDIB::Map() when it doesn't actually need to access the shared memory.
    
    Changed this to TransportDIB::CreateWithHandle() which doesn't map the shared memory.
    
    This may improve performance for cases where the image is only ever used by the pepper plugin.
    
    BUG=
    TEST=Ran some flash sites in 2d mode. Verified that things still worked.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11377087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167047 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d50fa952d39eb1cdb420bab79e4521dc8f7d1a32

--- content/renderer/pepper/pepper_platform_image_2d_impl.cc
+++ content/renderer/pepper/pepper_platform_image_2d_impl.cc
@@ -52,7 +52,7 @@ PepperPlatformImage2DImpl* PepperPlatformImage2DImpl::Create(int width,
   if (!TransportDIB::is_valid_handle(dib_handle))
     return NULL;
 
-  TransportDIB* dib = TransportDIB::Map(dib_handle);
+  TransportDIB* dib = TransportDIB::CreateWithHandle(dib_handle);
 #else
   static int next_dib_id = 0;
   TransportDIB* dib = TransportDIB::Create(buffer_size, next_dib_id++);
