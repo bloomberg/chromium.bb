b74397d311f15a78f21b6b3dc5aeaf77e2413e2e
hongbo.min@intel.com <hongbo.min@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 10:48:41 +0000

    Update example code to show how to use systemInfo.cpu and systemInfo.memory API.
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11312191
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167368 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7b8c7314394718febeb4d3bd94a4417287e930b3

--- chrome/common/extensions/docs/examples/api/systemInfo/main.js
+++ chrome/common/extensions/docs/examples/api/systemInfo/main.js
@@ -29,16 +29,62 @@ function stopMonitor() {
   isStarted = false;
 }
 
+function showStorageInfo(unit) {
+  table = "<tr><td>" + unit.id + "</td>" +
+    "<td>" + unit.type + "</td>" +
+    "<td>" + Math.round(unit.capacity/1024) + "</td>" +
+    "<td id=" + "\"" + unit.id + "\">" +
+    Math.round(unit.availableCapacity/1024) +
+    "</td></tr>\n";
+  return table;
+}
+
 function init() {
   document.getElementById("start-btn").onclick = startMonitor;
   document.getElementById("stop-btn").onclick = stopMonitor;
 
+  // Get CPU information.
+  chrome.experimental.systemInfo.cpu.get(function(cpu) {
+    var cpuInfo = "<b>Architecture:</b> " + cpu.archName +
+      "<br><b>Model Name: </b>" + cpu.modelName +
+      "<br><b>Number of Processors: </b>" + cpu.numOfProcessors;
+    var div = document.getElementById("cpu-info");
+    div.innerHTML = cpuInfo;
+  });
+  chrome.experimental.systemInfo.cpu.onUpdated.addListener(function(info) {
+    var table = "<br><table border=\"1\">\n" +
+      "<tr><td width=\"80px\"><b>Index</b></td>";
+    for (var i = 0; i < info.usagePerProcessor.length; i++) {
+      table += "<td width=\"120px\"><b>" + i + "</b></td>";
+    }
+    table += "<td width=\"120px\"><b>Total</b></td></tr>\n";
+    table += "<tr><td><b>History Usage</b></td>";
+    for (var i = 0; i < info.usagePerProcessor.length; i++) {
+      table += "<td>" + Math.round(info.usagePerProcessor[i]) + "</td>";
+    }
+    table += "<td>" + Math.round(info.averageUsage) + "</td></tr>";
+    table += "</table>\n";
+    var div = document.getElementById("cpu-cores");
+    div.innerHTML = table;
+  });
+
+  // Get memory information.
+  chrome.experimental.systemInfo.memory.get(function(memory) {
+    var memoryInfo =
+    "<b>Total Capacity:</b> " + Math.round(memory.capacity / 1024) + "KB" +
+    "<br><b>Available Capacity: </b>" +
+    Math.round(memory.availableCapacity / 1024) + "KB"
+    var div = document.getElementById("memory-info");
+    div.innerHTML = memoryInfo;
+  });
+
+  // Get storage information.
   chrome.experimental.systemInfo.storage.get(function(units) {
     var table = "<table width=65% border=\"1\">\n" +
       "<tr><td><b>ID</b></td>" +
-      "<td>Type</td>" +
-      "<td>Capacity (bytes)</td>" +
-      "<td>Available (bytes)</td>" +
+      "<td><b>Type</b></td>" +
+      "<td><b>Total Capacity (KB)</b></td>" +
+      "<td><b>Available Capacity (KB)</b></td>" +
       "</tr>\n";
     for (var i = 0; i < units.length; i++) {
       indicator[units[i].id] = 0;
@@ -48,15 +94,6 @@ function init() {
     var div = document.getElementById("storage-list");
     div.innerHTML = table;
   });
-}
-
-function showStorageInfo(unit) {
-  table = "<tr><td>" + unit.id + "</td>" +
-    "<td>" + unit.type + "</td>" +
-    "<td>" + unit.capacity + "</td>" +
-    "<td id=" + "\"" + unit.id + "\">" + unit.availableCapacity + "</td>" +
-    "</tr>\n";
-  return table;
 }
 
 document.addEventListener('DOMContentLoaded', init);
