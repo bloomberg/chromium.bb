fab283f158cedc28266d3709ed6dfdb878e43c10
japhet@chromium.org <japhet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 23:31:16 +0000

    Reenable NullOpenerRedirectForksProcess, as the offending patch has been reverted
    BUG=161152
    TEST=none
    TBR=dglazkov
    Review URL: https://codereview.chromium.org/11348073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168073 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 14eba181af0099a979068beb456570104e0739b8

--- chrome/browser/ui/browser_browsertest.cc
+++ chrome/browser/ui/browser_browsertest.cc
@@ -512,9 +512,7 @@ IN_PROC_BROWSER_TEST_F(BeforeUnloadAtQuitWithTwoWindows,
 // The script must open a new tab, set its window.opener to null, and navigate
 // it to a cross-site URL.  It should also work for meta-refreshes.
 // See http://crbug.com/93517.
-// TODO(japhet): This is failing because of a WebKit patch's handling of
-// about:blank versus GURL(). See http://crbug.com/161152
-IN_PROC_BROWSER_TEST_F(BrowserTest, DISABLED_NullOpenerRedirectForksProcess) {
+IN_PROC_BROWSER_TEST_F(BrowserTest, NullOpenerRedirectForksProcess) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kDisablePopupBlocking);
 
