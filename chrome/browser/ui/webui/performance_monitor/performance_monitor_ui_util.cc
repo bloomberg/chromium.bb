2d0b59b131094a52b4a35a2f8195a3b3e6e44e18
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 06:25:27 +0000

    Revert 168135 - Reduce CPM DB Usage
    Pass off the separation into active interval series to the aggregation methods,
    which already run through all the data anyway.  Now, each metric only hits the
    DB once.
    
    If we assume that a user starts up a browser 10 times a day (not sure what
    others do, but I do at least that much), then over the course of a month, with
    8 metrics selected, this reduces the hits to the DB from 2400 to 8.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11293192
    
    TBR=rdevlin.cronin@chromium.org
    Review URL: https://codereview.chromium.org/11411029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168140 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 31e1c1fa33a728878bffba2739b1676af25db865

--- chrome/browser/ui/webui/performance_monitor/performance_monitor_ui_util.cc
+++ chrome/browser/ui/webui/performance_monitor/performance_monitor_ui_util.cc
@@ -27,132 +27,93 @@ double SortAndGetMedian(std::vector<double>* values) {
       values->at(size / 2);
 }
 
-}  // namespace
+scoped_ptr<Database::MetricVector> NoAggregation(
+    MetricType type,
+    const Database::MetricVector* metrics,
+    const base::Time& start) {
+  scoped_ptr<Database::MetricVector> results(new Database::MetricVector());
 
-Aggregator::Aggregator() {
+  Database::MetricVector::const_iterator iter = metrics->begin();
+  while (iter != metrics->end() && iter->time < start)
+    ++iter;
+
+  for (; iter != metrics->end(); ++iter)
+    results->push_back(*iter);
+
+  return results.Pass();
 }
 
-Aggregator::~Aggregator() {
-}
-
-scoped_ptr<VectorOfMetricVectors> Aggregator::AggregateMetrics(
-    MetricType metric_type,
+scoped_ptr<Database::MetricVector> AggregateByMedian(
+    MetricType type,
     const Database::MetricVector* metrics,
     const base::Time& start,
-    const std::vector<TimeRange>& intervals,
     const base::TimeDelta& resolution) {
-  scoped_ptr<VectorOfMetricVectors> results(new VectorOfMetricVectors());
+  scoped_ptr<Database::MetricVector> results(new Database::MetricVector());
 
-  Database::MetricVector::const_iterator metric = metrics->begin();
-  while (metric != metrics->end() && metric->time < start)
-    ++metric;
+  Database::MetricVector::const_iterator iter = metrics->begin();
+  while (iter != metrics->end() && iter->time < start)
+    ++iter;
 
-  // For each interval, advance the metric to the start of the interval, and
-  // append a metric vector for the aggregated data within that interval,
-  // according to the appropriate strategy.
-  for (std::vector<TimeRange>::const_iterator interval = intervals.begin();
-       interval != intervals.end(); ++interval) {
-    while (metric != metrics->end() && metric->time < interval->start)
-      ++metric;
+  base::Time window_start = start;
 
-    results->push_back(*AggregateInterval(
-        metric_type,
-      &metric,
-      metrics->end(),
-      interval == intervals.begin() ? start : interval->start,
-      interval->end,
-      resolution));
+  while (iter != metrics->end()) {
+    std::vector<double> values;
+    while (iter != metrics->end() && iter->time < window_start + resolution) {
+      values.push_back(iter->value);
+      ++iter;
+    }
+
+    if (!values.empty()) {
+      results->push_back(Metric(type,
+                                window_start + resolution,
+                                SortAndGetMedian(&values)));
+    }
+    window_start += resolution;
   }
 
   return results.Pass();
 }
 
-scoped_ptr<Database::MetricVector> NoAggregation::AggregateInterval(
-    MetricType metric_type,
-    Database::MetricVector::const_iterator* metric,
-    const Database::MetricVector::const_iterator& metric_end,
-    const base::Time& time_start,
-    const base::Time& time_end,
+scoped_ptr<Database::MetricVector> AggregateByMean(
+    MetricType type,
+    const Database::MetricVector* metrics,
+    const base::Time& start,
     const base::TimeDelta& resolution) {
-  scoped_ptr<Database::MetricVector> aggregated_series(
-      new Database::MetricVector());
+  scoped_ptr<Database::MetricVector> results(new Database::MetricVector());
 
-  for (; *metric != metric_end && (*metric)->time <= time_end; ++(*metric))
-    aggregated_series->push_back(**metric);
+  Database::MetricVector::const_iterator iter = metrics->begin();
+  while (iter != metrics->end() && iter->time < start)
+    ++iter;
 
-  return aggregated_series.Pass();
-}
-
-scoped_ptr<Database::MetricVector> MedianAggregation::AggregateInterval(
-    MetricType metric_type,
-    Database::MetricVector::const_iterator* metric,
-    const Database::MetricVector::const_iterator& metric_end,
-    const base::Time& time_start,
-    const base::Time& time_end,
-    const base::TimeDelta& resolution) {
-  scoped_ptr<Database::MetricVector> aggregated_series(
-      new Database::MetricVector());
-  base::Time window_start = time_start;
-
-  while (*metric != metric_end && (*metric)->time <= time_end) {
-    std::vector<double> values;
-    while (*metric != metric_end &&
-           (*metric)->time <= time_end &&
-           (*metric)->time < window_start + resolution) {
-      values.push_back((*metric)->value);
-      ++(*metric);
-    }
-
-    if (!values.empty()) {
-      aggregated_series->push_back(Metric(metric_type,
-                                          window_start + resolution,
-                                          SortAndGetMedian(&values)));
-    }
-    window_start += resolution;
-  }
-
-  return aggregated_series.Pass();
-}
-
-scoped_ptr<Database::MetricVector> MeanAggregation::AggregateInterval(
-    MetricType metric_type,
-    Database::MetricVector::const_iterator* metric,
-    const Database::MetricVector::const_iterator& metric_end,
-    const base::Time& time_start,
-    const base::Time& time_end,
-    const base::TimeDelta& resolution) {
-  scoped_ptr<Database::MetricVector> aggregated_series(
-      new Database::MetricVector());
-
-  while (*metric != metric_end && (*metric)->time <= time_end) {
+  while (iter != metrics->end()) {
     // Finds the beginning of the next aggregation window.
-    int64 window_offset = ((*metric)->time - time_start) / resolution;
-    base::Time window_start = time_start + (window_offset * resolution);
+    int64 window_offset = (iter->time - start) / resolution;
+    base::Time window_start = start + (window_offset * resolution);
     base::Time window_end = window_start + resolution;
     base::Time last_sample_time = window_start;
     double integrated = 0.0;
     double metric_value = 0.0;
 
     // Aggregate the step function defined by the Metrics in |metrics|.
-    while (*metric != metric_end && (*metric)->time <= window_end) {
-      metric_value = (*metric)->value;
-      integrated += metric_value *
-                    ((*metric)->time - last_sample_time).InSecondsF();
-      last_sample_time = (*metric)->time;
-      ++(*metric);
+    while (iter != metrics->end() && iter->time <= window_end) {
+      metric_value = iter->value;
+      integrated += metric_value * (iter->time - last_sample_time).InSecondsF();
+      last_sample_time = iter->time;
+      ++iter;
     }
-    if (*metric != metric_end)
-      metric_value = (*metric)->value;
+    if (iter != metrics->end())
+      metric_value = iter->value;
 
-    // If the window splits an area of the step function, split the
-    // aggregation at the end of the window.
+    // If the window splits an area of the step function, split the aggregation
+    // at the end of the window.
     integrated += metric_value * (window_end - last_sample_time).InSecondsF();
     double average = integrated / resolution.InSecondsF();
-    aggregated_series->push_back(Metric(metric_type, window_end, average));
+    results->push_back(Metric(type, window_end, average));
   }
-
-  return aggregated_series.Pass();
+  return results.Pass();
 }
+
+}  // namespace
 
 double GetConversionFactor(UnitDetails from, UnitDetails to) {
   if (from.measurement_type != to.measurement_type) {
@@ -164,31 +125,27 @@ double GetConversionFactor(UnitDetails from, UnitDetails to) {
       static_cast<double>(to.amount_in_base_units);
 }
 
-scoped_ptr<VectorOfMetricVectors> AggregateMetric(
+scoped_ptr<Database::MetricVector> AggregateMetric(
     MetricType type,
     const Database::MetricVector* metrics,
     const base::Time& start,
-    const std::vector<TimeRange>& intervals,
     const base::TimeDelta& resolution,
     AggregationMethod method) {
-  if (!metrics || intervals.empty())
-    return scoped_ptr<VectorOfMetricVectors>(NULL);
+  if (!metrics)
+    return scoped_ptr<Database::MetricVector>(NULL);
 
   CHECK(resolution > base::TimeDelta());
 
   switch (method) {
     case AGGREGATION_METHOD_NONE:
-      return NoAggregation().AggregateMetrics(
-          type, metrics, start, intervals, resolution);
+      return NoAggregation(type, metrics, start);
     case AGGREGATION_METHOD_MEDIAN:
-      return MedianAggregation().AggregateMetrics(
-          type, metrics, start, intervals, resolution);
+      return AggregateByMedian(type, metrics, start, resolution);
     case AGGREGATION_METHOD_MEAN:
-      return MeanAggregation().AggregateMetrics(
-          type, metrics, start, intervals, resolution);
+      return AggregateByMean(type, metrics, start, resolution);
     default:
       NOTREACHED();
-      return scoped_ptr<VectorOfMetricVectors>(NULL);
+      return scoped_ptr<Database::MetricVector>(NULL);
   }
 }
 
