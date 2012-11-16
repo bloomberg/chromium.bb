e25f5d93a2d0114d3fa2b4a41e7e78fc86317930
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 07:44:34 +0000

    Chromoting Debian package improvements
    * Update .gitignore.
    * Remove "lucid" from changelog - lucid will soon be no more!
    * Add revision number to changelog.
    * Don't strip binaries by default - the small increase in package size is
      worth the convenience of easier debugging.
    * Downgrade xvfb-randr from Depends: to Recommends:. This allows users to opt
      out of dynamic-resize, but new installs will still get xvfb-randr due to the
      default APT config (APT::Install-Recommends "true").
    
    
    Review URL: https://chromiumcodereview.appspot.com/11366258
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168157 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4b992eb3e2af0a41dfe7124e587fa8e57ddba08f

--- remoting/host/installer/linux/build-deb.sh
+++ remoting/host/installer/linux/build-deb.sh
@@ -27,15 +27,29 @@ if [[ ! "$version_full" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
   exit 1
 fi
 
-echo "Building version $version_full"
+# Include revision information in changelog when building from a local
+# git-based checkout.
+merge_head="$(git merge-base HEAD origin/git-svn 2>/dev/null)"
+if [[ -n "$merge_head" ]]; then
+  revision="$(git svn find-rev "$merge_head" 2>/dev/null)"
+  if [[ -n "$revision" ]]; then
+    revision_text="(r$revision)"
+  fi
+fi
+
+# Don't strip binaries unless build options are already specified. This doesn't
+# have much effect on package size (13MB versus 14MB), so it's worth keeping
+# the symbols for now.
+export DEB_BUILD_OPTIONS="${DEB_BUILD_OPTIONS:-nostrip}"
+
+echo "Building version $version_full $revision_text"
 
 # Create a fresh debian/changelog.
 export DEBEMAIL="The Chromium Authors <chromium-dev@chromium.org>"
 rm -f debian/changelog
 debchange --create \
   --package chrome-remote-desktop \
-  --distribution lucid \
   --newversion "$version_full" \
-  "New Debian package"
+  "New Debian package $revision_text"
 
 dpkg-buildpackage -b -us -uc
