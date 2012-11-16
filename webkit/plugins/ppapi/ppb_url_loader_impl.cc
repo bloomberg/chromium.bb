4440029105525dcba53483e7b016b74af08c3eb3
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 07:35:55 +0000

    Break path whereby AssociatedURLLoader::~AssociatedURLLoader() is re-entered on top of itself.
    BUG=159429
    
    Review URL: https://chromiumcodereview.appspot.com/11359222
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168150 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a75c45bf1cad925548a75bf88f828443bc8ee27d

--- webkit/plugins/ppapi/ppb_url_loader_impl.cc
+++ webkit/plugins/ppapi/ppb_url_loader_impl.cc
@@ -85,6 +85,12 @@ PPB_URLLoader_Impl::PPB_URLLoader_Impl(PP_Instance instance,
 }
 
 PPB_URLLoader_Impl::~PPB_URLLoader_Impl() {
+  // There is a path whereby the destructor for the loader_ member can
+  // invoke InstanceWasDeleted() upon this PPB_URLLoader_Impl, thereby
+  // re-entering the scoped_ptr destructor with the same scoped_ptr object
+  // via loader_.reset(). Be sure that loader_ is first NULL then destroy
+  // the scoped_ptr. See http://crbug.com/159429.
+  scoped_ptr<WebKit::WebURLLoader> for_destruction_only(loader_.release());
 }
 
 PPB_URLLoader_API* PPB_URLLoader_Impl::AsPPB_URLLoader_API() {
