6ad0ea0b4b807e783cab46b50b7a0314eeb3d90c
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 08:26:57 +0000

    Revert 168625 - Disable ServerRedirectSingleProcess browser test on Windows
    This test has been flaky on the Win7 Tests bot
    
    TBR=jochen@chromium.org
    BUG=161897
    
    Review URL: https://codereview.chromium.org/11413074
    
    TBR=benwells@chromium.org
    Review URL: https://codereview.chromium.org/11419088
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168761 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ccf17a5123367f21d6a6589048378d6952a825be

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -401,14 +401,7 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, ServerRedirect) {
           << message_;
 }
 
-#if defined(OS_WIN)
-#define MAYBE_ServerRedirectSingleProcess DISABLED_ServerRedirectSingleProcess
-#else
-#define MAYBE_ServerRedirectSingleProcess ServerRedirectSingleProcess
-#endif
-
-IN_PROC_BROWSER_TEST_F(WebNavigationApiTest,
-    MAYBE_ServerRedirectSingleProcess) {
+IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, ServerRedirectSingleProcess) {
   // Set max renderers to 1 to force running out of processes.
   content::RenderProcessHost::SetMaxRendererProcessCount(1);
 
