740629c8ae71413a85cc49f6d4aec7910f1cd160
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 13:47:09 +0000

    Don't add an extension tab twice when it has an associated tab contents
    This was deleted by accident in https://chromiumcodereview.appspot.com/10868116/
    
    BUG=161854
    R=finnur@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11348150
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168798 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a3421eb19e57ae294514ac37fe0bd7a8c2e38d98

--- chrome/browser/extensions/extension_host.cc
+++ chrome/browser/extensions/extension_host.cc
@@ -626,6 +626,7 @@ void ExtensionHost::AddNewContents(WebContents* source,
         delegate->AddNewContents(
             associated_contents, new_contents, disposition, initial_pos,
             user_gesture, was_blocked);
+        return;
       }
     }
   }
