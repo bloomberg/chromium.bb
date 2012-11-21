1d825ce6e164eed6def6e937a8fe6139f4375294
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 16:08:01 +0000

    Add regen_test_data.py to easily update the test data files.
    Everytime one is modified, the SHA-1s of every .isolated files change, and it's
    a pain to maintain manually. This tool automates updating the .isolated test
    data files.
    
    And reformat all the files accordingly to be standardized.
    
    R=csharp@chromium.org
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11412132
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/swarm_client@169056 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tests/run_isolated/regen_test_data.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Regenerates all the .isolated test data files.
+
+Keep in sync with ../run_isolated_smoke_test.py.
+"""
+
+import glob
+import hashlib
+import json
+import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# Ordering is important to keep this script simple.
+INCLUDES_TO_FIX = [
+    ('manifest2.isolated', ['manifest1.isolated']),
+    ('check_files.isolated', ['gtest_fake.isolated', 'manifest2.isolated']),
+]
+
+
+def sha1(filename):
+  with open(filename, 'rb') as f:
+    return hashlib.sha1(f.read()).hexdigest()
+
+
+def load(filename):
+  with open(filename, 'r') as f:
+    return json.load(f)
+
+
+def save(filename, data):
+  """Saves data as json properly formatted.
+
+  Strips spurious whitespace json.dump likes to add at the end of lines and add
+  a trailing \n.
+  """
+  out = ''.join(
+      '%s\n' % l.rstrip()
+      for l in json.dumps(data, indent=2, sort_keys=True).splitlines())
+  with open(filename, 'wb') as f:
+    f.write(out)
+
+
+def main():
+  # Simplify our life.
+  os.chdir(ROOT_DIR)
+
+  # First, reformat all the files.
+  for filename in glob.glob('*.isolated'):
+    save(filename, load(filename))
+
+  # Then update the SHA-1s.
+  for manifest, includes in INCLUDES_TO_FIX:
+    data = load(manifest)
+    data['includes'] = [sha1(f) for f in includes]
+    save(manifest, data)
+  return 0
+
+
+if __name__ == '__main__':
+  sys.exit(main())
