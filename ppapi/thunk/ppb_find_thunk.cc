59025cf39fc6162bd65e919589c2dce64a72bdca
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-26 22:19:51 +0000

    Autogenerate thunk .cc file for PPB_Find_Dev.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11299174
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169496 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 05bb365eb061163f73a6230e3a0fbe81893c94c3

--- ppapi/thunk/ppb_find_thunk.cc
+++ ppapi/thunk/ppb_find_thunk.cc
@@ -2,9 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// From dev/ppb_find_dev.idl modified Mon Nov 26 10:29:49 2012.
+
 #include "ppapi/c/dev/ppb_find_dev.h"
-#include "ppapi/thunk/thunk.h"
+#include "ppapi/c/pp_errors.h"
+#include "ppapi/shared_impl/tracked_callback.h"
 #include "ppapi/thunk/enter.h"
+#include "ppapi/thunk/ppb_instance_api.h"
+#include "ppapi/thunk/resource_creation_api.h"
+#include "ppapi/thunk/thunk.h"
 
 namespace ppapi {
 namespace thunk {
@@ -13,10 +19,12 @@ namespace {
 
 void NumberOfFindResultsChanged(PP_Instance instance,
                                 int32_t total,
-                                PP_Bool final) {
+                                PP_Bool final_result) {
   EnterInstance enter(instance);
   if (enter.succeeded())
-    enter.functions()->NumberOfFindResultsChanged(instance, total, final);
+    enter.functions()->NumberOfFindResultsChanged(instance,
+                                                  total,
+                                                  final_result);
 }
 
 void SelectedFindResultChanged(PP_Instance instance, int32_t index) {
@@ -25,15 +33,15 @@ void SelectedFindResultChanged(PP_Instance instance, int32_t index) {
     enter.functions()->SelectedFindResultChanged(instance, index);
 }
 
-const PPB_Find_Dev g_ppb_find_thunk = {
+const PPB_Find_Dev_0_3 g_ppb_find_dev_thunk_0_3 = {
   &NumberOfFindResultsChanged,
-  &SelectedFindResultChanged
+  &SelectedFindResultChanged,
 };
 
 }  // namespace
 
 const PPB_Find_Dev_0_3* GetPPB_Find_Dev_0_3_Thunk() {
-  return &g_ppb_find_thunk;
+  return &g_ppb_find_dev_thunk_0_3;
 }
 
 }  // namespace thunk
