bloomberg/chromium.bb

commit 3e41195c624df23d766e8094b525ddf7979e26eb

[Qt] Separate QWidget dependant code into separate WebKitWidgets static library https://bugs.webkit.org/show_bug.cgi?id=102800
Patch by Simon Hausmann  <simon.hausmann@digia.com>, Pierre Rossi <pierre.rossi@digia.com> on 2012-11-22
Reviewed by Tor Arne Vestbø.

This patch separates code that needs to use QWidget related APIs in
WebKit/qt/WebCoreSupport and Api from code that doesn't. This means for
example FrameLoaderClientQt.cpp remains in the WebKit1 static library,
while qwebpage.cpp and qwebframe.cpp become part of the WebKitWidgets
static library. WebKit1 is compiled without QT += widgets and therefore
any widget related dependency has been moved "up" and out of WebKit1 into
the WebKitWidgets library.

Between the code in WebKit.a and WebKitWidgets.a new adapters and
interfaces have been introduced, such as QWebPageAdapter and
QWebFrameAdapter. QWebPageAdapter, when used from WebKit1, is a way to
call out into the API layer, implemented by QWebPage (QWebPagePrivate).
The other way around if QWebPage wants to access WebCore or
WebCoreSupport related functionality, it will go through
QWebPageAdapater (as base class). The separation in the direction up
into the API layer is complete with this patch, no code in WebKit1
depends on QtWidgets. The separation the other way around, code in the
API layer not using any WebCore types, is not complete yet.

Some classes such as QWebSettings, QWebElement or
DumpRenderTreeSupportQt remain in WebKit1. While they are API layer,
they do not depend on widget related Qt APIs and they make much more
use of WebCore internal APIs and therefore are easier to keep in
WebKit1.

In the future we plan to place a real shared library boundary between
WebKit1 and WebKitWidgets, by keeping the WebKit1 static library as
part of the QtWebKit shared library and by turning the WebKitWidgets
static library into a shared one.

.:

* Source/api.pri:
* WebKit.pro:

Source/WebCore:

* platform/qt/QWebPageClient.h:
(QWebPageClient):

Source/WebKit:

third_party/WebKit/Source/WebKit/qt/WebCoreSupport/InitWebCoreQt.cpp
@@ -44,19 +44,31 @@
 
 #include "qwebelement_p.h"
 #include <JavaScriptCore/runtime/InitializeThreading.h>
-#include <QApplication>
-#include <QStyle>
 #include <wtf/MainThread.h>
 
 namespace WebKit {
 
-// Called also from WebKit2's WebProcess.
-Q_DECL_EXPORT void initializeWebKit2Theme()
+static QtStyleFactoryFunction initCallback = 0;
+
+void setWebKitWidgetsInitCallback(QtStyleFactoryFunction callback)
 {
-    if (qgetenv("QT_WEBKIT_THEME_NAME") == "qstyle") {
-        WebCore::RenderThemeQStyle::setStyleFactoryFunction(WebKit::QStyleFacadeImp::create);
+    initCallback = callback;
+}
+
+// Called also from WebKit2's WebProcess
+Q_DECL_EXPORT void initializeWebKitQt()
+{
+    if (initCallback) {
+        WebCore::RenderThemeQStyle::setStyleFactoryFunction(initCallback);
         WebCore::RenderThemeQt::setCustomTheme(WebCore::RenderThemeQStyle::create, new WebCore::ScrollbarThemeQStyle);
     }
+
+    WebCore::initializeWebCoreQt();
+}
+
+Q_DECL_EXPORT void setImagePlatformResource(const char* name, const QPixmap& pixmap)
+{
+    WebCore::Image::setPlatformResource(name, pixmap);
 }
 
 }
@@ -79,17 +91,9 @@ void initializeWebCoreQt()
     PlatformStrategiesQt::initialize();
     QtWebElementRuntime::initialize();
 
-    RenderThemeQStyle::setStyleFactoryFunction(WebKit::QStyleFacadeImp::create);
-    RenderThemeQt::setCustomTheme(RenderThemeQStyle::create, new ScrollbarThemeQStyle);
-
 #if USE(QTKIT)
     InitWebCoreSystemInterface();
 #endif
-
-    // QWebSettings::SearchCancelButtonGraphic
-    Image::setPlatformResource("searchCancelButton", QApplication::style()->standardPixmap(QStyle::SP_DialogCloseButton));
-    // QWebSettings::SearchCancelButtonPressedGraphic
-    Image::setPlatformResource("searchCancelButtonPressed", QApplication::style()->standardPixmap(QStyle::SP_DialogCloseButton));
 
     initialized = true;
 }

third_party/WebKit/Source/WebKit/qt/WebCoreSupport/InitWebCoreQt.h
@@ -30,6 +30,22 @@
 #ifndef InitWebCoreQt_h
 #define InitWebCoreQt_h
 
+#include "qimage.h"
+
+namespace WebCore {
+class Page;
+class QStyleFacade;
+}
+typedef WebCore::QStyleFacade* (*QtStyleFactoryFunction)(WebCore::Page*);
+
+namespace WebKit {
+
+void setWebKitWidgetsInitCallback(QtStyleFactoryFunction);
+void initializeWebKitQt();
+void setImagePlatformResource(const char* /* name */, const QPixmap&);
+
+}
+
 namespace WebCore {
 
 void initializeWebCoreQt();

third_party/WebKit/Source/WebKit2/qt/MainQt.cpp
@@ -42,9 +42,10 @@ typedef QApplication ApplicationType;
 
 namespace WebKit {
 Q_DECL_IMPORT int WebProcessMainQt(QGuiApplication*);
-#if defined(HAVE_WEBKIT1)
-Q_DECL_IMPORT void initializeWebKit2Theme();
+#if !defined(QT_NO_WIDGETS)
+Q_DECL_IMPORT void initializeWebKitWidgets();
 #endif
+Q_DECL_IMPORT void initializeWebKitQt();
 }
 
 #if !defined(NDEBUG) && defined(Q_OS_UNIX)
@@ -82,15 +83,23 @@ int main(int argc, char** argv)
     }
 #endif
 
-#if defined(HAVE_WEBKIT1)
-    WebKit::initializeWebKit2Theme();
-#endif
-
     // Has to be done before QApplication is constructed in case
     // QApplication itself produces debug output.
     QByteArray suppressOutput = qgetenv("QT_WEBKIT_SUPPRESS_WEB_PROCESS_OUTPUT");
     if (!suppressOutput.isEmpty() && suppressOutput != "0")
         qInstallMessageHandler(messageHandler);
 
-    return WebKit::WebProcessMainQt(new ApplicationType(argc, argv));
+    // QApplication must be created before we call initializeWebKitWidgets() so that
+    // the standard pixmaps can be fetched from the style.
+    ApplicationType* appInstance = new ApplicationType(argc, argv);
+
+#if !defined(QT_NO_WIDGETS)
+    if (qgetenv("QT_WEBKIT_THEME_NAME") == "qstyle")
+        WebKit::initializeWebKitWidgets();
+    else
+#endif
+        WebKit::initializeWebKitQt();
+
+
+    return WebKit::WebProcessMainQt(appInstance);
 }

third_party/WebKit/Tools/DumpRenderTree/qt/DumpRenderTreeQt.cpp
@@ -36,6 +36,7 @@
 #include "DumpRenderTreeSupportQt.h"
 #include "EventSenderQt.h"
 #include "GCControllerQt.h"
+#include "InitWebKitQt.h"
 #include "QtTestSupport.h"
 #include "TestRunnerQt.h"
 #include "TextInputControllerQt.h"
@@ -62,18 +63,13 @@
 #include <QProgressBar>
 #include <QUndoStack>
 #include <QUrl>
-
-#include <qwebsettings.h>
-#include <qwebsecurityorigin.h>
-
 #include <limits.h>
 #include <locale.h>
-
+#include <qwebsecurityorigin.h>
+#include <qwebsettings.h>
 #ifndef Q_OS_WIN
 #include <unistd.h>
 #endif
-
-#include <qdebug.h>
 
 namespace WebCore {
 
@@ -206,14 +202,14 @@ void WebPage::resetSettings()
     QWebSettings::setMaximumPagesInCache(0); // reset to default
     settings()->setUserStyleSheetUrl(QUrl()); // reset to default
 
-    DumpRenderTreeSupportQt::setMinimumTimerInterval(this, DumpRenderTreeSupportQt::defaultMinimumTimerInterval());
+    DumpRenderTreeSupportQt::setMinimumTimerInterval(handle(), DumpRenderTreeSupportQt::defaultMinimumTimerInterval());
 
-    DumpRenderTreeSupportQt::resetInternalsObject(mainFrame());
+    DumpRenderTreeSupportQt::resetInternalsObject(mainFrame()->handle());
 
     m_pendingGeolocationRequests.clear();
 }
 
-QWebPage *WebPage::createWindow(QWebPage::WebWindowType)
+QWebPage* WebPage::createWindow(QWebPage::WebWindowType)
 {
     return m_drt->createWindow();
 }
@@ -399,6 +395,7 @@ DumpRenderTree::DumpRenderTree()
     if (viewMode == "graphics")
         setGraphicsBased(true);
 
+    WebKit::initializeWebKitWidgets();
     DumpRenderTreeSupportQt::initialize();
 
     // Set running in DRT mode for qwebpage to create testable objects.
@@ -421,7 +418,7 @@ DumpRenderTree::DumpRenderTree()
     }
     // Use a frame group name for all pages created by DumpRenderTree to allow
     // testing of cross-page frame lookup.
-    DumpRenderTreeSupportQt::webPageSetGroupName(m_page, "org.webkit.qt.DumpRenderTree");
+    DumpRenderTreeSupportQt::webPageSetGroupName(pageAdapter(), "org.webkit.qt.DumpRenderTree");
 
     m_mainView->setContextMenuPolicy(Qt::NoContextMenu);
     m_mainView->resize(QSize(TestRunner::maxViewWidth, TestRunner::maxViewHeight));
@@ -470,7 +467,7 @@ DumpRenderTree::DumpRenderTree()
     QObject::connect(this, SIGNAL(quit()), qApp, SLOT(quit()), Qt::QueuedConnection);
 
     DumpRenderTreeSupportQt::setDumpRenderTreeModeEnabled(true);
-    DumpRenderTreeSupportQt::setInteractiveFormValidationEnabled(webPage(), true);
+    DumpRenderTreeSupportQt::setInteractiveFormValidationEnabled(pageAdapter(), true);
     DumpRenderTreeSupportQt::enableMockScrollbars();
 
     QFocusEvent event(QEvent::FocusIn, Qt::ActiveWindowFocusReason);
@@ -541,9 +538,9 @@ void DumpRenderTree::resetToConsistentStateBeforeTesting(const QUrl& url)
 #endif
 
     clearHistory(m_page);
-    DumpRenderTreeSupportQt::scalePageBy(m_page->mainFrame(), 1, QPoint(0, 0));
-    DumpRenderTreeSupportQt::clearFrameName(m_page->mainFrame());
-    DumpRenderTreeSupportQt::removeUserStyleSheets(m_page);
+    DumpRenderTreeSupportQt::scalePageBy(mainFrameAdapter(), 1, QPoint(0, 0));
+    DumpRenderTreeSupportQt::clearFrameName(mainFrameAdapter());
+    DumpRenderTreeSupportQt::removeUserStyleSheets(pageAdapter());
 
     m_page->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAsNeeded);
     m_page->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAsNeeded);
@@ -561,7 +558,7 @@ void DumpRenderTree::resetToConsistentStateBeforeTesting(const QUrl& url)
 
     DumpRenderTreeSupportQt::resetOriginAccessWhiteLists();
 
-    DumpRenderTreeSupportQt::setWindowsBehaviorAsEditingBehavior(m_page);
+    DumpRenderTreeSupportQt::setWindowsBehaviorAsEditingBehavior(pageAdapter());
 
     QLocale::setDefault(QLocale::c());
 
@@ -570,7 +567,7 @@ void DumpRenderTree::resetToConsistentStateBeforeTesting(const QUrl& url)
     setlocale(LC_ALL, "");
 #endif
 
-    DumpRenderTreeSupportQt::clearOpener(m_page->mainFrame());
+    DumpRenderTreeSupportQt::clearOpener(mainFrameAdapter());
 }
 
 static bool isGlobalHistoryTest(const QUrl& url)
@@ -739,7 +736,7 @@ void DumpRenderTree::initJSObjects()
     frame->addToJavaScriptWindowObject(QLatin1String("eventSender"), m_eventSender);
     frame->addToJavaScriptWindowObject(QLatin1String("textInputController"), m_textInputController);
     frame->addToJavaScriptWindowObject(QLatin1String("GCController"), m_gcController);
-    DumpRenderTreeSupportQt::injectInternalsObject(frame);
+    DumpRenderTreeSupportQt::injectInternalsObject(frame->handle());
 }
 
 void DumpRenderTree::showPage()
@@ -757,7 +754,7 @@ void DumpRenderTree::hidePage()
 
 QString DumpRenderTree::dumpFrameScrollPosition(QWebFrame* frame)
 {
-    if (!frame || !DumpRenderTreeSupportQt::hasDocumentElement(frame))
+    if (!frame || !DumpRenderTreeSupportQt::hasDocumentElement(frame->handle()))
         return QString();
 
     QString result;
@@ -779,7 +776,7 @@ QString DumpRenderTree::dumpFrameScrollPosition(QWebFrame* frame)
 
 QString DumpRenderTree::dumpFramesAsText(QWebFrame* frame)
 {
-    if (!frame || !DumpRenderTreeSupportQt::hasDocumentElement(frame))
+    if (!frame || !DumpRenderTreeSupportQt::hasDocumentElement(frame->handle()))
         return QString();
 
     QString result;
@@ -907,7 +904,7 @@ void DumpRenderTree::dump()
         fprintf(stdout, "Source:\n\n%s\n", markup.toUtf8().constData());
     }
 
-    QString mimeType = DumpRenderTreeSupportQt::responseMimeType(mainFrame);
+    QString mimeType = DumpRenderTreeSupportQt::responseMimeType(mainFrame->handle());
     if (mimeType == "text/plain")
         m_controller->dumpAsText();
 
@@ -947,11 +944,11 @@ void DumpRenderTree::dump()
             mainFrame->render(&painter);
             painter.end();
         } else
-            image = DumpRenderTreeSupportQt::paintPagesWithBoundaries(mainFrame);
+            image = DumpRenderTreeSupportQt::paintPagesWithBoundaries(mainFrame->handle());
 
-        if (DumpRenderTreeSupportQt::trackRepaintRects(m_page->mainFrame())) {
+        if (DumpRenderTreeSupportQt::trackRepaintRects(mainFrameAdapter())) {
             QVector<QRect> repaintRects;
-            DumpRenderTreeSupportQt::getTrackedRepaintRects(m_page->mainFrame(), repaintRects);
+            DumpRenderTreeSupportQt::getTrackedRepaintRects(mainFrameAdapter(), repaintRects);
             QImage mask(image.size(), image.format());
             mask.fill(QColor(0, 0, 0, 0.66 * 255));
 
@@ -963,7 +960,7 @@ void DumpRenderTree::dump()
             QPainter painter(&image);
             painter.drawImage(image.rect(), mask);
 
-            DumpRenderTreeSupportQt::setTrackRepaintRects(m_page->mainFrame(), false);
+            DumpRenderTreeSupportQt::setTrackRepaintRects(mainFrameAdapter(), false);
         }
 
         QCryptographicHash hash(QCryptographicHash::Md5);
@@ -1098,7 +1095,7 @@ QWebPage *DumpRenderTree::createWindow()
 
     // Use a frame group name for all pages created by DumpRenderTree to allow
     // testing of cross-page frame lookup.
-    DumpRenderTreeSupportQt::webPageSetGroupName(page, "org.webkit.qt.DumpRenderTree");
+    DumpRenderTreeSupportQt::webPageSetGroupName(page->handle(), "org.webkit.qt.DumpRenderTree");
 
     return page;
 }
@@ -1132,6 +1129,16 @@ void DumpRenderTree::switchFocus(bool focused)
             view->scene()->sendEvent(view->graphicsView(), &event);
     }
 
+}
+
+QWebPageAdapter* DumpRenderTree::pageAdapter() const
+{
+    return m_page->handle();
+}
+
+QWebFrameAdapter* DumpRenderTree::mainFrameAdapter() const
+{
+    return m_page->mainFrame()->handle();
 }
 
 QList<WebPage*> DumpRenderTree::getAllPages() const

commit 01f4aee0e4029eff5076ffb06eaeee66dbcd002d

[Qt] REGRESSION(r135575): It made all tests assert https://bugs.webkit.org/show_bug.cgi?id=103169
Patch by Pierre Rossi <pierre.rossi@gmail.com> on 2012-11-26
Reviewed by Simon Hausmann.

This fixes another regression introduced in r135515:
initializeWebKitQt shouldn't implicitely call initializeWebCoreQt
since it can be called from WebKit2 to initialize QStyle for testing.
This would then lead to things such as PlatformStrategies being
initialized twice.

Source/WebKit/qt:

* Api/qwebpage.cpp: Explicitely call initializeWebCoreQt().
(QWebPagePrivate::QWebPagePrivate):
* WebCoreSupport/InitWebCoreQt.cpp:
(WebKit::initializeWebKitQt):
(WebCore::initializeWebCoreQt):
* WebCoreSupport/InitWebCoreQt.h:
(WebCore):

Source/WebKit2:

* qt/MainQt.cpp: No need to initialize anything if we're not using QStyle.
(WebKit):
(main):

Tools:

* DumpRenderTree/qt/DumpRenderTreeQt.cpp: Also propagate the change to DRT this time.
(WebCore::DumpRenderTree::DumpRenderTree):

git-svn-id: svn://svn.chromium.org/blink/trunk@135701 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: c841fd1af433bc8a74a89e186baadfec9fcb8324

third_party/WebKit/Source/WebKit/qt/Api/qwebpage.cpp
@@ -68,6 +68,7 @@
 #include "HTMLNames.h"
 #include "HitTestResult.h"
 #include "Image.h"
+#include "InitWebCoreQt.h"
 #include "InitWebKitQt.h"
 #include "InspectorClientQt.h"
 #include "InspectorClientWebPage.h"
@@ -325,6 +326,7 @@ QWebPagePrivate::QWebPagePrivate(QWebPage *qq)
 #endif
 
     WebKit::initializeWebKitWidgets();
+    WebCore::initializeWebCoreQt();
 
     Page::PageClients pageClients;
     pageClients.chromeClient = new ChromeClientQt(this);

third_party/WebKit/Source/WebKit/qt/WebCoreSupport/InitWebCoreQt.cpp
@@ -62,8 +62,6 @@ Q_DECL_EXPORT void initializeWebKitQt()
         WebCore::RenderThemeQStyle::setStyleFactoryFunction(initCallback);
         WebCore::RenderThemeQt::setCustomTheme(WebCore::RenderThemeQStyle::create, new WebCore::ScrollbarThemeQStyle);
     }
-
-    WebCore::initializeWebCoreQt();
 }
 
 Q_DECL_EXPORT void setImagePlatformResource(const char* name, const QPixmap& pixmap)
@@ -75,7 +73,7 @@ Q_DECL_EXPORT void setImagePlatformResource(const char* name, const QPixmap& pix
 
 namespace WebCore {
 
-void initializeWebCoreQt()
+Q_DECL_EXPORT void initializeWebCoreQt()
 {
     static bool initialized = false;
     if (initialized)

third_party/WebKit/Source/WebKit/qt/WebCoreSupport/InitWebCoreQt.h
@@ -48,7 +48,7 @@ Q_DECL_EXPORT void setImagePlatformResource(const char* /* name */, const QPixma
 
 namespace WebCore {
 
-void initializeWebCoreQt();
+Q_DECL_EXPORT void initializeWebCoreQt();
 
 }
 

third_party/WebKit/Source/WebKit2/qt/MainQt.cpp
@@ -45,7 +45,6 @@ Q_DECL_IMPORT int WebProcessMainQt(QGuiApplication*);
 #if !defined(QT_NO_WIDGETS)
 Q_DECL_IMPORT void initializeWebKitWidgets();
 #endif
-Q_DECL_IMPORT void initializeWebKitQt();
 }
 
 #if !defined(NDEBUG) && defined(Q_OS_UNIX)
@@ -96,10 +95,7 @@ int main(int argc, char** argv)
 #if !defined(QT_NO_WIDGETS)
     if (qgetenv("QT_WEBKIT_THEME_NAME") == "qstyle")
         WebKit::initializeWebKitWidgets();
-    else
 #endif
-        WebKit::initializeWebKitQt();
-
 
     return WebKit::WebProcessMainQt(appInstance);
 }

third_party/WebKit/Tools/DumpRenderTree/qt/DumpRenderTreeQt.cpp
@@ -36,6 +36,7 @@
 #include "DumpRenderTreeSupportQt.h"
 #include "EventSenderQt.h"
 #include "GCControllerQt.h"
+#include "InitWebCoreQt.h"
 #include "InitWebKitQt.h"
 #include "QtTestSupport.h"
 #include "TestRunnerQt.h"
@@ -396,6 +397,7 @@ DumpRenderTree::DumpRenderTree()
         setGraphicsBased(true);
 
     WebKit::initializeWebKitWidgets();
+    WebCore::initializeWebCoreQt();
     DumpRenderTreeSupportQt::initialize();
 
     // Set running in DRT mode for qwebpage to create testable objects.