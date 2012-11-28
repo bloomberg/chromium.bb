19186647b12a81466fbf475f25e2ce7346237920
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 17:37:15 +0000

    Android Perf: upstream image_decoding_benchmark using Chrome Remote Control.
    We're migrating our bots to use Chrome Remote Control instead of the downstream
    harness. Start migrating some of the existing tests.
    
    BUG=160045
    TEST=tools/perf/run_multipage_benchmarks --show-stdout --browser=android-content-shell image_decoding_benchmark tools/perf/page_sets/image_decoding/image_decoding_png.json
    
    Review URL: https://chromiumcodereview.appspot.com/11293167
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169986 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cfb7e8ebb4f53f13b4b21469c71e90cef60e3bf2

---
+++ chrome/test/data/image_decoding/image_decoding.js
@@ -0,0 +1,87 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Run benchmark for at least this many milliseconds.
+var minTotalTimeMs = 10 * 1000;
+// Run benchmark at least this many times.
+var minIterations = 20;
+// Discard results for this many milliseconds after startup.
+var warmUpTimeMs = 5 * 1000;
+var benchmarkStartTime;
+var loadStartTimeMs;
+var decodingTimesMs = [];
+var isDone = false;
+var img;
+
+getImageFormat = function() {
+  if (document.location.search) {
+    return document.location.search.substr(1);
+  }
+  return "jpg";
+}
+
+prepareImageElement = function() {
+  img = document.createElement('img');
+  // Scale the image down to the display size to make sure the entire image is
+  // decoded. If the image is much larger than the display, some browsers might
+  // only decode the visible portion of the image, which would skew the results
+  // of this test.
+  img.setAttribute('width', '100%');
+  document.body.appendChild(img);
+  console.log("Running benchmark for at least " + minTotalTimeMs +
+              " ms and at least " + minIterations + " times.");
+  document.getElementById('status').innerHTML = "Benchmark running.";
+  setStatus(getImageFormat().toUpperCase() + " benchmark running.");
+  benchmarkStartTimeMs = window.performance.now();
+}
+
+setStatus = function(status) {
+  document.getElementById('status').innerHTML = status;
+}
+
+runBenchmark = function() {
+  setStatus("Preparing benchmark.");
+  prepareImageElement();
+  startLoadingImage();
+}
+
+var iteration = (new Date).getTime();
+
+startLoadingImage = function() {
+  img.setAttribute('onload', '');
+  img.setAttribute('src', '');
+  loadStartTimeMs = window.performance.now();
+  img.addEventListener('load', onImageLoaded);
+  // Use a unique URL for each test iteration to work around image caching.
+  img.setAttribute('src', 'droids.' + getImageFormat() + '?' + iteration);
+  iteration += 1;
+}
+
+var requestAnimationFrame = (function() {
+  return window.requestAnimationFrame       ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame    ||
+         window.oRequestAnimationFrame      ||
+         window.msRequestAnimationFrame     ||
+         function(callback) {
+           window.setTimeout(callback, 1000 / 60);
+         };
+})().bind(window);
+
+onImageLoaded = function(img) {
+  var now = window.performance.now();
+  var decodingTimeMs = now - loadStartTimeMs;
+  if (now - benchmarkStartTimeMs >= warmUpTimeMs) {
+    decodingTimesMs.push(decodingTimeMs);
+  }
+  if (now - benchmarkStartTimeMs < minTotalTimeMs ||
+      decodingTimesMs.length < minIterations) {
+    requestAnimationFrame(startLoadingImage);
+  } else {
+    isDone = true;
+    console.log("decodingTimes: " + decodingTimesMs);
+    setStatus(getImageFormat().toUpperCase() + " benchmark finished: " +
+              decodingTimesMs);
+  }
+}
