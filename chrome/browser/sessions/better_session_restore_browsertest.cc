3bc9d89df4b24b393f7918a7e6c1c30b8443c0b7
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 12:11:03 +0000

    Disable ContinueWhereILeftOffTest.LocalStorageClearedOnExit on Linux.
    BUG=163096
    TBR=marja@chromium.org
    
    Review URL: https://codereview.chromium.org/11316217
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169900 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 186b661f7383039cecfb392643349eb0c5fce2a8

--- chrome/browser/sessions/better_session_restore_browsertest.cc
+++ chrome/browser/sessions/better_session_restore_browsertest.cc
@@ -257,7 +257,14 @@ IN_PROC_BROWSER_TEST_F(ContinueWhereILeftOffTest,
       SetDefaultCookieSetting(CONTENT_SETTING_SESSION_ONLY);
 }
 
-IN_PROC_BROWSER_TEST_F(ContinueWhereILeftOffTest, LocalStorageClearedOnExit) {
+// Flaky on Linux: http://crbug.com/163096
+#if defined(OS_LINUX)
+#define MAYBE_LocalStorageClearedOnExit DISABLED_LocalStorageClearedOnExit
+#else
+#define MAYBE_LocalStorageClearedOnExit LocalStorageClearedOnExit
+#endif
+IN_PROC_BROWSER_TEST_F(ContinueWhereILeftOffTest,
+                       MAYBE_LocalStorageClearedOnExit) {
   CheckReloadedPageNotRestored();
 }
 
