1fc309b01f87b1f6a61679dd11e27331b2aecb3d
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-29 23:29:27 +0000

    Factor out common chromium stack init code
    This means we can share it between tests & glue layer, and also insulate
    the glue layer from some of the internal details in this.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11416226
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170286 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5585d8b55d75fd168a266dce4265393ef914db94

---
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
@@ -0,0 +1,58 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview;
+
+import android.content.Context;
+
+import org.chromium.base.PathUtils;
+import org.chromium.base.ThreadUtils;
+import org.chromium.content.app.LibraryLoader;
+import org.chromium.content.browser.AndroidBrowserProcess;
+import org.chromium.content.browser.ResourceExtractor;
+
+/**
+ * Wrapper for the steps needed to initialize the java and native sides of webview chromium.
+ */
+public abstract class AwBrowserProcess {
+    /**
+     * The name of the library to load.
+     */
+    private static final String NATIVE_LIBRARY = "webviewchromium";
+
+    private static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "webview";
+
+    /**
+     * Loads the native library, and performs basic static construction of objects needed
+     * to run webview in this process. Does not create threads; safe to call from zygote.
+     * Note: it is up to the caller to ensure this is only called once.
+     */
+    public static void loadLibrary() {
+        PathUtils.setPrivateDataDirectorySuffix(PRIVATE_DATA_DIRECTORY_SUFFIX);
+        // We don't need to extract any paks because for WebView, they are
+        // in the system image. TODO(joth): remove the paks and make ResourceExtractor
+        // a true no-op for this use-case (i.e. remove need to pass empty string here).
+        ResourceExtractor.setMandatoryPaksToExtract("");
+        LibraryLoader.setLibraryToLoad(NATIVE_LIBRARY);
+        LibraryLoader.ensureInitialized();
+    }
+
+    /**
+     * Starts the chromium browser process running within this process. Creates threads
+     * and performs other per-app resource allocations; must not be called from zygote.
+     * Note: it is up to the caller to ensure this is only called once.
+     */
+    public static void start(final Context context) {
+        // We must post to the UI thread to cover the case that the user
+        // has invoked Chromium startup by using the (thread-safe)
+        // CookieManager rather than creating a WebView.
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                AndroidBrowserProcess.initContentViewProcess(context,
+                        AndroidBrowserProcess.MAX_RENDERERS_SINGLE_PROCESS);
+            }
+        });
+    }
+}
--- android_webview/javatests/src/org/chromium/android_webview/test/AndroidWebViewTestRunnerApplication.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AndroidWebViewTestRunnerApplication.java
@@ -6,39 +6,17 @@ package org.chromium.android_webview.test;
 
 import android.app.Application;
 
-import org.chromium.base.PathUtils;
-import org.chromium.content.app.LibraryLoader;
-import org.chromium.content.browser.ResourceExtractor;
+import org.chromium.android_webview.AwBrowserProcess;
 import org.chromium.content.common.CommandLine;
 
 public class AndroidWebViewTestRunnerApplication extends Application {
 
-    /**
-     * The name of the library to load.
-     */
-    private static final String NATIVE_LIBRARY = "webviewchromium";
-
-    /** The minimum set of .pak files Chrome needs. */
-    private static final String[] CHROME_MANDATORY_PAKS = {
-        "chrome.pak", "chrome_100_percent.pak", "en-US.pak",
-    };
-
-    private static final String PRIVATE_DATA_DIRECTORY_SUFFIX = "webview";
-
     @Override
     public void onCreate() {
         super.onCreate();
-        initializeApplicationParameters();
-    }
 
-    /** Handles initializing the common application parameters. */
-    private static void initializeApplicationParameters() {
         CommandLine.initFromFile("/data/local/chrome-command-line");
-        PathUtils.setPrivateDataDirectorySuffix(PRIVATE_DATA_DIRECTORY_SUFFIX);
-        // We don't need to extract any paks because for WebView, they are
-        // in the system image.
-        ResourceExtractor.setMandatoryPaksToExtract(CHROME_MANDATORY_PAKS);
-        LibraryLoader.setLibraryToLoad(NATIVE_LIBRARY);
-        LibraryLoader.ensureInitialized();
+        AwBrowserProcess.loadLibrary();
+        AwBrowserProcess.start(this);
     }
 }
