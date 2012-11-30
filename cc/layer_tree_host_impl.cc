db416a5937d2b43a52f8298ce310e899cae9422e
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 05:55:02 +0000

    [cc] Track the rendering & painting cost of impl-side painting.
    We record how long the rendering thread takes to render each PicturePile,
    accumulate in TileManager, and report through LayerTreeHostImpl; telemetry
    queries this information and dumps the output.
    
    BUG=163324
    R=reveman
    TBR=enne
    
    Review URL: https://codereview.chromium.org/11434033
    
    Review URL: https://codereview.chromium.org/11418250
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170413 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4d3c5c915340092e54e40382b93d74661830074b

--- cc/layer_tree_host_impl.cc
+++ cc/layer_tree_host_impl.cc
@@ -1614,6 +1614,9 @@ void LayerTreeHostImpl::renderingStats(RenderingStats* stats) const
     stats->numImplThreadScrolls = m_numImplThreadScrolls;
     stats->numMainThreadScrolls = m_numMainThreadScrolls;
     stats->numLayersDrawn = m_cumulativeNumLayersDrawn;
+
+    if (m_tileManager)
+        m_tileManager->renderingStats(stats);
 }
 
 void LayerTreeHostImpl::animateScrollbars(base::TimeTicks time)
--- cc/tile_manager.h
+++ cc/tile_manager.h
@@ -20,6 +20,7 @@ namespace cc {
 
 class Tile;
 class TileVersion;
+struct RenderingStats;
 class ResourceProvider;
 
 class CC_EXPORT TileManagerClient {
@@ -75,6 +76,8 @@ class CC_EXPORT TileManager {
 
   void ManageTiles();
 
+  void renderingStats(RenderingStats* stats);
+
  protected:
   // Methods called by Tile
   friend class Tile;
@@ -91,7 +94,8 @@ class CC_EXPORT TileManager {
   void OnRasterTaskCompleted(
       scoped_refptr<Tile>,
       ResourceProvider::ResourceId,
-      scoped_refptr<PicturePileImpl>);
+      scoped_refptr<PicturePileImpl>,
+      RenderingStats*);
   void DidFinishTileInitialization(Tile*, ResourceProvider::ResourceId);
 
   TileManagerClient* client_;
@@ -105,6 +109,8 @@ class CC_EXPORT TileManager {
   typedef std::vector<Tile*> TileVector;
   TileVector tiles_;
   TileVector tiles_that_need_to_be_rasterized_;
+
+  RenderingStats rendering_stats_;
 };
 
 }  // namespace cc
