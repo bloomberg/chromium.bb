a83b044b83c82c5a758b0850af076c42c204e306
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 16:59:18 +0000

    Changes the visibility of message view cards.
    - Changes the background color lighter
    - Adds 'shadow' at the bottom of each card. Boder is no longer used.
    See the issue for the current screenshot.
    
    BUG=161052
    TEST=manually check on goobuntu
    
    
    Review URL: https://chromiumcodereview.appspot.com/11412240
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170465 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0016082d2d4638407297cf1c9f2d909d13971b01

--- ui/message_center/message_center_bubble.cc
+++ ui/message_center/message_center_bubble.cc
@@ -28,10 +28,11 @@ namespace {
 const int kMessageBubbleBaseMinHeight = 80;
 const int kMessageBubbleBaseMaxHeight = 400;
 const int kMarginBetweenItems = 10;
-const SkColor kMessageCenterBackgroundColor = SkColorSetRGB(0xd3, 0xd3, 0xd3);
+const int kItemShadowHeight = 4;
+const SkColor kMessageCenterBackgroundColor = SkColorSetRGB(0xe5, 0xe5, 0xe5);
 const SkColor kBorderDarkColor = SkColorSetRGB(0xaa, 0xaa, 0xaa);
-const SkColor kMessageItemBorderWidth = 1;
-const SkColor kMessageItemBorderColor = SkColorSetRGB(0xaa, 0xaa, 0xaa);
+const SkColor kMessageItemShadowColorBase = SkColorSetARGB(0.3 * 255, 0, 0, 0);
+const SkColor kTransparentColor = SkColorSetARGB(0, 0, 0, 0);
 
 // The view for the buttons at the bottom of the web notification tray.
 class WebNotificationButtonView : public views::View,
@@ -148,13 +149,22 @@ class FixedSizedScrollView : public views::ScrollView {
 class ScrollContentView : public views::View {
  public:
   ScrollContentView() {
+    // Set the margin to 0 for the layout. BoxLayout assumes the same margin
+    // for top and bottom, but the bottom margin here should be smaller
+    // because of the shadow of message view. Use an empty border instead
+    // to provide this margin.
     SetLayoutManager(
         new views::BoxLayout(views::BoxLayout::kVertical,
-                             kMarginBetweenItems,
-                             kMarginBetweenItems,
-                             kMarginBetweenItems));
+                             0,
+                             0,
+                             kMarginBetweenItems - kItemShadowHeight));
     set_background(views::Background::CreateSolidBackground(
         kMessageCenterBackgroundColor));
+    set_border(views::Border::CreateEmptyBorder(
+        kMarginBetweenItems, /* top */
+        kMarginBetweenItems, /* left */
+        kMarginBetweenItems - kItemShadowHeight,  /* bottom */
+        kMarginBetweenItems /* right */ ));
   }
 
   virtual ~ScrollContentView() {
@@ -171,6 +181,30 @@ class ScrollContentView : public views::View {
  private:
   gfx::Size preferred_size_;
   DISALLOW_COPY_AND_ASSIGN(ScrollContentView);
+};
+
+// A view to draw gradient shadow for each MessageView.
+class MessageViewShadow : public views::View {
+ public:
+  MessageViewShadow()
+      : painter_(views::Painter::CreateVerticalGradient(
+            kMessageItemShadowColorBase, kTransparentColor)) {
+  }
+
+ private:
+  // views::View overrides:
+  virtual gfx::Size GetPreferredSize() OVERRIDE {
+    // The preferred size must not be empty. Thus put an arbitrary non-zero
+    // width here. It will be just ignored by the vertical box layout.
+    return gfx::Size(1, kItemShadowHeight);
+  }
+
+  virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE {
+    painter_->Paint(canvas, bounds().size());
+  }
+
+  scoped_ptr<views::Painter> painter_;
+  DISALLOW_COPY_AND_ASSIGN(MessageViewShadow);
 };
 
 }  // namespace
@@ -209,10 +243,14 @@ class MessageCenterContentsView : public views::View {
       MessageView* view =
           MessageViewFactory::ViewForNotification(*iter, list_delegate_);
       view->set_scroller(scroller_);
-      view->set_border(views::Border::CreateSolidBorder(
-          kMessageItemBorderWidth, kMessageItemBorderColor));
       view->SetUpView();
-      scroll_content_->AddChildView(view);
+
+      views::View* container = new views::View();
+      container->SetLayoutManager(
+          new views::BoxLayout(views::BoxLayout::kVertical, 0, 0, 0));
+      container->AddChildView(view);
+      container->AddChildView(new MessageViewShadow());
+      scroll_content_->AddChildView(container);
       if (++num_children >=
           NotificationList::kMaxVisibleMessageCenterNotifications) {
         break;
