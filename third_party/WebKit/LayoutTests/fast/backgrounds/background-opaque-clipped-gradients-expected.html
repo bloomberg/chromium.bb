bloomberg/chromium.bb

commit d067ed64e38e0af46819953fdcc71797b51c1e1a

Adding occlusion detection to reduce overdraw in RenderBox background rendering https://bugs.webkit.org/show_bug.cgi?id=102557
Reviewed by Stephen White.

Source/WebCore:

Adding hasAlpha method to all classes dervived from CSSImageValue and
StyleImage, in order to support occlusion testing.
This allows the FillLayer recursion to stop early when an opaque layer
is encountered while applying a CSS background style. This also allows
the elimination of background color application when the background is
completely covered by an image.  The optimization also skips the
clearing of the page's root element when the page background is
entirely covered by an image (e.g a tiled bitmap or a gradient)

Test: fast/backgrounds/background-opaque-images-over-color.html

* css/CSSCrossfadeValue.cpp:
(WebCore::subimageHasAlpha):
(WebCore):
(WebCore::CSSCrossfadeValue::isPending):
(WebCore::CSSCrossfadeValue::hasAlpha):
* css/CSSCrossfadeValue.h:
(CSSCrossfadeValue):
* css/CSSGradientValue.cpp:
(WebCore::CSSGradientValue::hasAlpha):
(WebCore):
* css/CSSGradientValue.h:
(CSSGradientValue):
* css/CSSImageGeneratorValue.cpp:
(WebCore::CSSImageGeneratorValue::hasAlpha):
(WebCore):
* css/CSSImageGeneratorValue.h:
(CSSImageGeneratorValue):
* css/CSSImageValue.cpp:
(WebCore::CSSImageValue::hasAlpha):
(WebCore):
* css/CSSImageValue.h:
(WebCore):
(CSSImageValue):
* loader/cache/CachedImage.cpp:
(WebCore::CachedImage::currentFrameHasAlpha):
Test whether a cached image has alpha, with side effect of ensuring
the image is in cache. (new method)
(WebCore):
* loader/cache/CachedImage.h:
(CachedImage):
* rendering/RenderBox.cpp:
(WebCore::RenderBox::paintFillLayers):
Added layer recursion early exit when an opaque layer is encountered.
* rendering/RenderBoxModelObject.cpp:
(WebCore::RenderBoxModelObject::paintFillLayerExtended):
Eliminated background color fill when the layer contains an opaque
image that covers the entire RenderBox.
* rendering/style/FillLayer.cpp:

third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
@@ -1001,7 +1001,14 @@ void RenderBox::paintFillLayers(const PaintInfo& paintInfo, const Color& c, cons
     if (!fillLayer)
         return;
 
-    paintFillLayers(paintInfo, c, fillLayer->next(), rect, bleedAvoidance, op, backgroundObject);
+    // FIXME : It would be possible for the following occlusion culling test to be more aggressive 
+    // on layers with no repeat by testing whether the image covers the layout rect.
+    // Testing that here would imply duplicating a lot of calculations that are currently done in
+    // RenderBoxModelOBject::paintFillLayerExtended. A more efficient solution might be to move
+    // the layer recursion into paintFillLayerExtended, or to compute the layer geometry here
+    // and pass it down.
+    if (fillLayer->next() && (!fillLayer->hasOpaqueImage(this) || !fillLayer->image()->canRender(this, style()->effectiveZoom()) || !fillLayer->hasRepeatXY()))
+        paintFillLayers(paintInfo, c, fillLayer->next(), rect, bleedAvoidance, op, backgroundObject);
     paintFillLayer(paintInfo, c, fillLayer, rect, bleedAvoidance, op, backgroundObject);
 }
 

third_party/WebKit/Source/WebCore/rendering/style/FillLayer.cpp
@@ -280,4 +280,23 @@ bool FillLayer::imagesAreLoaded() const
     return true;
 }
 
+bool FillLayer::hasOpaqueImage(const RenderObject* renderer) const
+{
+    if (!m_image)
+        return false;
+
+    if (m_composite == CompositeClear || m_composite == CompositeCopy)
+        return true;
+
+    if (m_composite == CompositeSourceOver)
+        return !m_image->hasAlpha(renderer);
+
+    return false;
+}
+
+bool FillLayer::hasRepeatXY() const
+{
+    return m_repeatX == RepeatFill && m_repeatY == RepeatFill;
+}
+
 } // namespace WebCore

third_party/WebKit/Source/WebCore/rendering/style/FillLayer.h
@@ -144,6 +144,9 @@ public:
         return m_next ? m_next->hasFixedImage() : false;
     }
 
+    bool hasOpaqueImage(const RenderObject*) const;
+    bool hasRepeatXY() const;
+
     EFillLayerType type() const { return static_cast<EFillLayerType>(m_type); }
 
     void fillUnsetProperties();

third_party/WebKit/LayoutTests/fast/backgrounds/background-opaque-images-over-color-expected.txt
@@ -1,0 +1,32 @@
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x108
+  RenderBlock {HTML} at (0,0) size 800x108
+    RenderBody {BODY} at (8,8) size 784x0
+      RenderBlock {DIV} at (0,0) size 784x0
+        RenderBlock (floating) {DIV} at (0,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (20,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (40,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (60,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (80,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (100,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (120,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (140,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (160,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (180,0) size 20x100 [bgcolor=#FF0000]
+layer at (0,0) size 800x600
+  RenderView at (0,0) size 800x600
+layer at (0,0) size 800x108
+  RenderBlock {HTML} at (0,0) size 800x108
+    RenderBody {BODY} at (8,8) size 784x0
+      RenderBlock {DIV} at (0,0) size 784x0
+        RenderBlock (floating) {DIV} at (0,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (20,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (40,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (60,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (80,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (100,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (120,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (140,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (160,0) size 20x100 [bgcolor=#FF0000]
+        RenderBlock (floating) {DIV} at (180,0) size 20x100 [bgcolor=#FF0000]

third_party/WebKit/LayoutTests/fast/backgrounds/background-opaque-images-over-color.html
@@ -1,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+<!-- Test passes when a green rectangle is displayed.
+These are all drawing cases that can be optimized by occlusion culling
+The fix for bug 102557 optimizes cases 1, 2, 3, 6, 7, and 8-->
+<style type="text/css" media="screen">
+    .redBack {
+        width:20px;
+        height:100px;
+        display:block;
+        background-repeat: repeat;
+        background-color: red;
+        background-position:0px 0px, 0px 0px;
+        float: left;
+    }
+</style>
+</head>
+<body>
+<div>
+    <div id="case_1" class="redBack" style="background-image: url(../../http/tests/multipart/resources/green-100x100.png)"></div>
+    <div id="case_2" class="redBack" style="background-image: -webkit-linear-gradient(top, green, green)"></div>
+    <div id="case_3" class="redBack" style="background-image: -webkit-cross-fade(url(../../http/tests/multipart/resources/green-100x100.png), url(../../http/tests/multipart/resources/green-100x100.png), 50%)"></div>
+    <div id="case_4" class="redBack" style="background-repeat: no-repeat; background-image: url(../../http/tests/multipart/resources/green-100x100.png)"></div>
+    <div id="case_5" class="redBack" style="background-repeat: repeat-x; background-image: url(../../http/tests/multipart/resources/green-100x100.png)"></div>
+    <div id="case_6" class="redBack" style="background-image: url(../../http/tests/multipart/resources/green-100x100.png),url(resources/64x32-red.png)"></div>
+    <div id="case_7" class="redBack" style="background-image: -webkit-linear-gradient(top, green, green),url(resources/64x32-red.png)"></div>
+    <div id="case_8" class="redBack" style="background-image: -webkit-cross-fade(url(../../http/tests/multipart/resources/green-100x100.png),url(../../http/tests/multipart/resources/green-100x100.png), 50%),url(resources/64x32-red.png)"></div>
+    <div id="case_9" class="redBack" style="background-repeat: no-repeat; background-image: url(../../http/tests/multipart/resources/green-100x100.png),url(resources/64x32-red.png)"></div>
+    <div id="case_10" class="redBack" style="background-repeat: repeat-x; background-image: url(../../http/tests/multipart/resources/green-100x100.png),url(resources/64x32-red.png)"></div>
+</div>
+</body>
+</html>
+<!doctype html>
+<html>
+<head>
+<!-- Test passes when a green rectangle is displayed.
+These are all drawing cases that can be optimized by occlusion culling
+The fix for bug 102557 optimizes cases 1, 2, 3, 6, 7, and 8-->
+<style type="text/css" media="screen">
+    .redBack {
+        width:20px;
+        height:100px;
+        display:block;
+        background-repeat: repeat;
+        background-color: red;
+        background-position:0px 0px, 0px 0px;
+        float: left;
+    }
+</style>
+</head>
+<body>
+<div>
+    <div id="case_1" class="redBack" style="background-image: url(../../http/tests/multipart/resources/green-100x100.png)"></div>
+    <div id="case_2" class="redBack" style="background-image: -webkit-linear-gradient(top, green, green)"></div>
+    <div id="case_3" class="redBack" style="background-image: -webkit-cross-fade(url(../../http/tests/multipart/resources/green-100x100.png), url(../../http/tests/multipart/resources/green-100x100.png), 50%)"></div>
+    <div id="case_4" class="redBack" style="background-repeat: no-repeat; background-image: url(../../http/tests/multipart/resources/green-100x100.png)"></div>
+    <div id="case_5" class="redBack" style="background-repeat: repeat-x; background-image: url(../../http/tests/multipart/resources/green-100x100.png)"></div>
+    <div id="case_6" class="redBack" style="background-image: url(../../http/tests/multipart/resources/green-100x100.png),url(resources/64x32-red.png)"></div>
+    <div id="case_7" class="redBack" style="background-image: -webkit-linear-gradient(top, green, green),url(resources/64x32-red.png)"></div>
+    <div id="case_8" class="redBack" style="background-image: -webkit-cross-fade(url(../../http/tests/multipart/resources/green-100x100.png),url(../../http/tests/multipart/resources/green-100x100.png), 50%),url(resources/64x32-red.png)"></div>
+    <div id="case_9" class="redBack" style="background-repeat: no-repeat; background-image: url(../../http/tests/multipart/resources/green-100x100.png),url(resources/64x32-red.png)"></div>
+    <div id="case_10" class="redBack" style="background-repeat: repeat-x; background-image: url(../../http/tests/multipart/resources/green-100x100.png),url(resources/64x32-red.png)"></div>
+</div>
+</body>
+</html>

third_party/WebKit/LayoutTests/platform/chromium/TestExpectations
@@ -1263,6 +1263,9 @@ webkit.org/b/25817 svg/dom/SVGScriptElement/script-load-and-error-events.svg [ F
 
 webkit.org/b/60118 [ Mac ] svg/dom/SVGScriptElement/script-set-href.svg [ Failure Pass ]
 
+# New test in need of new baselines
+webkit.org/b/102557 fast/backgrounds/background-opaque-images-over-color.html [ Pass ImageOnlyFailure Failure Missing ]
+
 # Introduced due to BUGWK53512, fails under Skia right now
 webkit.org/b/65939 svg/custom/getBBox-path.svg [ Failure ]
 

third_party/WebKit/LayoutTests/platform/mac/TestExpectations
@@ -468,6 +468,9 @@ media/track/track-cue-rendering-snap-to-lines-not-set.html
 media/track/track-cue-rendering-vertical.html
 media/track/track-webvtt-tc028-unsupported-markup.html
 
+# Test needs new baselines https://bugs.webkit.org/show_bug.cgi?id=102557
+fast/backgrounds/background-opaque-images-over-color.html
+
 # Tests for MediaSource API. Feature is not yet functional.
 # https://bugs.webkit.org/show_bug.cgi?id=64731
 http/tests/media/media-source/

third_party/WebKit/Source/WebCore/css/CSSCrossfadeValue.cpp
@@ -52,6 +52,19 @@ static bool subimageIsPending(CSSValue* value)
     return false;
 }
 
+static bool subimageHasAlpha(CSSValue* value, const RenderObject* renderer)
+{
+    if (value->isImageValue())
+        return static_cast<CSSImageValue*>(value)->hasAlpha(renderer);
+
+    if (value->isImageGeneratorValue())
+        return static_cast<CSSImageGeneratorValue*>(value)->hasAlpha(renderer);
+
+    ASSERT_NOT_REACHED();
+
+    return true;
+}
+
 static CachedImage* cachedImageForCSSValue(CSSValue* value, CachedResourceLoader* cachedResourceLoader)
 {
     if (!value)
@@ -124,6 +137,11 @@ IntSize CSSCrossfadeValue::fixedSize(const RenderObject* renderer)
 bool CSSCrossfadeValue::isPending() const
 {
     return subimageIsPending(m_fromValue.get()) || subimageIsPending(m_toValue.get());
+}
+
+bool CSSCrossfadeValue::hasAlpha(const RenderObject* renderer) const
+{
+    return subimageHasAlpha(m_fromValue.get(), renderer) || subimageHasAlpha(m_toValue.get(), renderer);
 }
 
 void CSSCrossfadeValue::loadSubimages(CachedResourceLoader* cachedResourceLoader)

commit 1fd81bba3e6240e2120def6f7759471fe46d74de

Fix occlusion culling logic to handle css background layer clipping https://bugs.webkit.org/show_bug.cgi?id=103276
Reviewed by Simon Fraser.

Source/WebCore:

Bug was introduced by r135629
This patch fixes it by verifying layer clip.  A fill layer must
not be treated as if it occludes the layers under it unless it has
a larger or equal clip setting than the layers under it.

Test: fast/backgrounds/background-opaque-clipped-gradients.html

* rendering/RenderBox.cpp:
(WebCore::RenderBox::paintFillLayers):
* rendering/style/FillLayer.cpp:
(WebCore::clipMax):
(WebCore::FillLayer::computeClipMax):
(WebCore::FillLayer::clipOccludesNextLayers):
* rendering/style/FillLayer.h:
(FillLayer):

LayoutTests:

New ref test that verifies the superposition of background layers with
the layers having smaller clip regions than the layers beneath them.
This test ensures the bottom layers are not removed by culling
optimizations.

* fast/backgrounds/background-opaque-clipped-gradients-expected.html: Added.
* fast/backgrounds/background-opaque-clipped-gradients.html: Added.


git-svn-id: svn://svn.chromium.org/blink/trunk@136326 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: bf7696efbbd8833cca7ed8ea91b6e6d7d99779ea

third_party/WebKit/Source/WebCore/rendering/RenderBox.cpp
@@ -1048,18 +1048,25 @@ LayoutRect RenderBox::maskClipRect()
 void RenderBox::paintFillLayers(const PaintInfo& paintInfo, const Color& c, const FillLayer* fillLayer, const LayoutRect& rect,
     BackgroundBleedAvoidance bleedAvoidance, CompositeOperator op, RenderObject* backgroundObject)
 {
-    if (!fillLayer)
-        return;
+    Vector<const FillLayer*, 8> layers;
+    const FillLayer* curLayer = fillLayer;
+    while (curLayer) {
+        layers.append(curLayer);
+        // Stop traversal when an opaque layer is encountered.
+        // FIXME : It would be possible for the following occlusion culling test to be more aggressive 
+        // on layers with no repeat by testing whether the image covers the layout rect.
+        // Testing that here would imply duplicating a lot of calculations that are currently done in
+        // RenderBoxModelOBject::paintFillLayerExtended. A more efficient solution might be to move
+        // the layer recursion into paintFillLayerExtended, or to compute the layer geometry here
+        // and pass it down.
+        if (curLayer->hasOpaqueImage(this) && curLayer->clipOccludesNextLayers(curLayer == fillLayer) && curLayer->image()->canRender(this, style()->effectiveZoom()) && curLayer->hasRepeatXY())
+            break;
+        curLayer = curLayer->next();
+    }
 
-    // FIXME : It would be possible for the following occlusion culling test to be more aggressive 
-    // on layers with no repeat by testing whether the image covers the layout rect.
-    // Testing that here would imply duplicating a lot of calculations that are currently done in
-    // RenderBoxModelOBject::paintFillLayerExtended. A more efficient solution might be to move
-    // the layer recursion into paintFillLayerExtended, or to compute the layer geometry here
-    // and pass it down.
-    if (fillLayer->next() && (!fillLayer->hasOpaqueImage(this) || !fillLayer->image()->canRender(this, style()->effectiveZoom()) || !fillLayer->hasRepeatXY()))
-        paintFillLayers(paintInfo, c, fillLayer->next(), rect, bleedAvoidance, op, backgroundObject);
-    paintFillLayer(paintInfo, c, fillLayer, rect, bleedAvoidance, op, backgroundObject);
+    Vector<const FillLayer*>::const_reverse_iterator topLayer = layers.rend();
+    for (Vector<const FillLayer*>::const_reverse_iterator it = layers.rbegin(); it != topLayer; ++it)
+        paintFillLayer(paintInfo, c, *it, rect, bleedAvoidance, op, backgroundObject);
 }
 
 void RenderBox::paintFillLayer(const PaintInfo& paintInfo, const Color& c, const FillLayer* fillLayer, const LayoutRect& rect,

third_party/WebKit/Source/WebCore/rendering/style/FillLayer.cpp
@@ -258,6 +258,33 @@ void FillLayer::cullEmptyLayers()
     }
 }
 
+static EFillBox clipMax(EFillBox clipA, EFillBox clipB)
+{
+    if (clipA == BorderFillBox || clipB == BorderFillBox)
+        return BorderFillBox;
+    if (clipA == PaddingFillBox || clipB == PaddingFillBox)
+        return PaddingFillBox;
+    if (clipA == ContentFillBox || clipB == ContentFillBox)
+        return ContentFillBox;
+    return TextFillBox;
+}
+
+void FillLayer::computeClipMax() const
+{
+    if (m_next) {
+        m_next->computeClipMax();
+        m_clipMax = clipMax(clip(), m_next->clip());
+    } else
+        m_clipMax = m_clip;
+}
+
+bool FillLayer::clipOccludesNextLayers(bool firstLayer) const
+{
+    if (firstLayer)
+        computeClipMax();
+    return m_clip == m_clipMax;
+}
+
 bool FillLayer::containsImage(StyleImage* s) const
 {
     if (!s)

third_party/WebKit/Source/WebCore/rendering/style/FillLayer.h
@@ -146,6 +146,7 @@ public:
 
     bool hasOpaqueImage(const RenderObject*) const;
     bool hasRepeatXY() const;
+    bool clipOccludesNextLayers(bool firstLayer) const;
 
     EFillLayerType type() const { return static_cast<EFillLayerType>(m_type); }
 
@@ -167,6 +168,8 @@ public:
 
 private:
     friend class RenderStyle;
+
+    void computeClipMax() const;
 
     FillLayer() { }
 
@@ -198,6 +201,8 @@ private:
     unsigned m_compositeSet : 1;
     
     unsigned m_type : 1; // EFillLayerType
+
+    mutable unsigned m_clipMax : 2; // EFillBox, maximum m_clip value from this to bottom layer
 };
 
 } // namespace WebCore

third_party/WebKit/LayoutTests/fast/backgrounds/background-opaque-clipped-gradients-expected.html
@@ -1,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<style type="text/css" media="screen">
+#div1
+{
+width: 150px;
+height: 150px;
+border:25px dotted;
+background-color: green;
+}
+#div2
+{
+width: 100px;
+height: 100px;
+padding: 25px;
+background-color: blue;
+}
+#div3
+{
+width: 100px;
+height: 100px;
+background-color: yellow;
+}
+</style>
+</head>
+<body>
+Test passes if the image below shows nested green, blue and yellow squares with a dotted black border.
+<div id="div1"><div id="div2"><div id="div3"></div></div></div>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/backgrounds/background-opaque-clipped-gradients.html
@@ -1,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<style type="text/css" media="screen">
+    #div1 {
+        width: 100px;
+        height: 100px;
+        padding: 25px;
+        border: 25px dotted;
+        background-image: -webkit-linear-gradient(top, yellow, yellow), -webkit-linear-gradient(top, blue, blue), -webkit-linear-gradient(top, green, green);
+        background-clip: content-box, padding-box, border-box;
+}
+</style>
+</head>
+<body>
+Test passes if the image below shows nested green, blue and yellow squares with a dotted black border.
+<div id="div1"></div>
+</body>
+</html>