bloomberg/chromium.bb

commit 28362a735bca3f165a6557c2254d5f503005323c

IRT threading: Make pthread_create() work inside the IRT
This means we now have three kinds of thread inside the IRT:

 1) user threads, created by the IRT's public thread_create() interface
 2) IRT internal threads, created by the IRT's internal pthread_create()
 3) the initial thread

We now link nc_thread.c into the IRT.

We need to use the same TDB (thread info block) size for all kinds of
thread so that functions like pthread_self() don't get confused.
(This especially important given that ARM and x86 use different TLS
layouts.)  We introduce "struct nc_combined_tdb": this replaces
"struct irt_private_tdb", and we use it in nc_thread.c to clarify what
was already being allocated.

Remove the IRT-specific pthread_self(): we will use the version from
nc_thread.c.

As a cleanup, replace irt_initialized with the already-existing
__nc_thread_initialized field.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=2788
TEST=run_irt_thread_test

Review URL: https://chromiumcodereview.appspot.com/10412018

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@8685 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: 1061d09123c202630db056f018cbc137ced7ed83

src/untrusted/irt/irt_thread.c
@@ -1,11 +1,12 @@
 /*
- * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Copyright (c) 2012 The Native Client Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
 
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "native_client/src/untrusted/irt/irt.h"
 #include "native_client/src/untrusted/irt/irt_private.h"
@@ -13,41 +14,26 @@
 #include "native_client/src/untrusted/nacl/tls.h"
 #include "native_client/src/untrusted/nacl/tls_params.h"
 #include "native_client/src/untrusted/pthread/pthread_internal.h"
+#include "native_client/src/untrusted/pthread/pthread_types.h"
 
-/*
- * This remains zero until we complete __pthread_initialize, below.
- */
-int irt_initialized;
 
-/*
- * Each thread has an IRT-private TLS area.
- * This is the "thread descriptor" part of that.
- */
-struct irt_private_tdb {
-  /*
-   * We use a union to reuse the same space for user_start initially,
-   * and then for self after thread startup, keeping this struct as
-   * small as possible.
-   */
-  union {
-    void *self;
-    void (*user_start)(void);
-  };
-  void *combined_area;
-};
-
-static struct irt_private_tdb *get_irt_tdb(void *thread_ptr) {
-  struct irt_private_tdb *tdb = (void *) ((uintptr_t) thread_ptr +
+static struct nc_combined_tdb *get_irt_tdb(void *thread_ptr) {
+  struct nc_combined_tdb *tdb = (void *) ((uintptr_t) thread_ptr +
                                           __nacl_tp_tdb_offset(sizeof(*tdb)));
   return tdb;
 }
 
+/* This is the inverse of get_irt_tdb(). */
+static void *get_malloc_block_from_tdb(struct nc_combined_tdb *tdb) {
+  return (void *) ((uintptr_t) tdb - __nacl_tp_tdb_offset(sizeof(*tdb)));
+}
+
 /*
- * This replaces __pthread_initialize_minimal from libnacl.
- * We set up our private TLS area with our irt_private_tdb structure.
+ * This replaces __pthread_initialize_minimal() from libnacl and
+ * __pthread_initialize() from libpthread.
  */
 void __pthread_initialize(void) {
-  struct irt_private_tdb *tdb;
+  struct nc_combined_tdb *tdb;
 
   /*
    * Allocate the area.  If malloc fails here, we'll crash before it returns.
@@ -59,15 +45,8 @@ void __pthread_initialize(void) {
    * Initialize TLS proper (i.e., __thread variable initializers).
    */
   void *tp = __nacl_tls_initialize_memory(combined_area, sizeof(*tdb));
-
-  /*
-   * Set up our "private TDB".  Only the "self" pointer actually matters
-   * here (and that only on some machines, like x86).  We'll never actually
-   * free this combined_area, but no reason not to store the pointer.
-   */
   tdb = get_irt_tdb(tp);
-  tdb->combined_area = combined_area;
-  tdb->self = tdb;
+  __nc_initialize_unjoinable_thread(tdb);
 
   /*
    * Now install it for later fetching.
@@ -80,18 +59,15 @@ void __pthread_initialize(void) {
    */
   __newlib_thread_init();
 
-  /*
-   * Mark that we are fully initialized, so malloc can fail gracefully.
-   */
-  irt_initialized = 1;
+  __nc_initialize_globals();
 }
 
 static void nacl_irt_thread_exit(int32_t *stack_flag) {
-  struct irt_private_tdb *tdb = get_irt_tdb(NACL_SYSCALL(second_tls_get)());
+  struct nc_combined_tdb *tdb = get_irt_tdb(NACL_SYSCALL(second_tls_get)());
 
   __nc_tsd_exit();
 
-  free(tdb->combined_area);
+  free(get_malloc_block_from_tdb(tdb));
 
   NACL_SYSCALL(thread_exit)(stack_flag);
   while (1) *(volatile int *) 0 = 0;  /* Crash.  */
@@ -102,14 +78,12 @@ static void nacl_irt_thread_exit(int32_t *stack_flag) {
  */
 static void irt_start_thread(void) {
   void *thread_ptr = NACL_SYSCALL(second_tls_get)();
-  struct irt_private_tdb *tdb = get_irt_tdb(thread_ptr);
+  struct nc_combined_tdb *tdb = get_irt_tdb(thread_ptr);
 
   /*
    * Fetch the user's start routine.
-   * Then set the self pointer, which occupies the same word.
    */
-  void (*user_start)(void) = tdb->user_start;
-  tdb->self = thread_ptr;
+  void (*user_start)(void) = (void (*)(void)) tdb->tdb.start_func;
 
   /*
    * Now do per-thread initialization for the IRT-private C library state.
@@ -129,7 +103,7 @@ static void irt_start_thread(void) {
 
 static int nacl_irt_thread_create(void *start_user_address, void *stack,
                                   void *thread_ptr) {
-  struct irt_private_tdb *tdb;
+  struct nc_combined_tdb *tdb;
 
   /*
    * Before we start the thread, allocate the IRT-private TLS area for it.
@@ -141,13 +115,12 @@ static int nacl_irt_thread_create(void *start_user_address, void *stack,
 
   void *irt_tp = __nacl_tls_initialize_memory(combined_area, sizeof(*tdb));
   tdb = get_irt_tdb(irt_tp);
-
+  __nc_initialize_unjoinable_thread(tdb);
   /*
-   * Store the whole TLS area so we can free it at thread exit,
-   * and the user's routine to be run.
+   * We overload the libpthread start_func field to store a function
+   * of a different type.
    */
-  tdb->combined_area = combined_area;
-  tdb->user_start = (void (*)(void)) (uintptr_t) start_user_address;
+  tdb->tdb.start_func = (void *(*)(void *)) (uintptr_t) start_user_address;
 
   int error = -NACL_SYSCALL(thread_create)(
       (void *) (uintptr_t) &irt_start_thread, stack, thread_ptr, irt_tp);

src/untrusted/pthread/nc_thread.c
@@ -38,8 +38,7 @@ static struct nacl_irt_thread irt_thread;
 
 static const uint32_t kStackAlignment = 32;
 
-#define TDB_SIZE \
-  (sizeof(nc_thread_descriptor_t) + sizeof(nc_basic_thread_data_t))
+#define TDB_SIZE (sizeof(struct nc_combined_tdb))
 
 static inline char* align(uint32_t offset, uint32_t alignment) {
   return (char*) ((offset + alignment - 1) & ~(alignment - 1));
@@ -48,7 +47,7 @@ static inline char* align(uint32_t offset, uint32_t alignment) {
 /* Thread management global variables */
 const int __nc_kMaxCachedMemoryBlocks = 50;
 
-static int __nc_thread_initialized;
+int __nc_thread_initialized;
 
 /* mutex used to synchronize thread management code */
 pthread_mutex_t  __nc_thread_management_lock;
@@ -60,7 +59,7 @@ static pthread_cond_t __nc_last_thread_cond;
 static pthread_t __nc_initial_thread_id;
 
 /* number of threads currently running in this NaCl module */
-int __nc_running_threads_counter;
+int __nc_running_threads_counter = 1;
 
 /* we have two queues of memory blocks - one for each type */
 STAILQ_HEAD(tailhead, entry) __nc_thread_memory_blocks[2];
@@ -225,26 +224,14 @@ static int nc_tdb_init(nc_thread_descriptor_t *tdb,
   return 0;
 }
 
-/* Will be called from the library startup code,
- * which always happens on the application's main thread
- */
-int __pthread_initialize(void) {
+/* Initializes all globals except for the initial thread structure. */
+int __nc_initialize_globals(void) {
   int retval = 0;
-  nc_thread_descriptor_t *tdb;
-  nc_basic_thread_data_t *basic_data;
-  /* allocate TLS+TDB area */
-  /* We allocate the basic data immediately after TDB */
-  __pthread_initialize_minimal(TDB_SIZE);
 
   /*
    * Fetch the ABI tables from the IRT.  If we don't have these, all is lost.
    */
   __nc_initialize_interfaces(&irt_thread);
-
-  /* At this point GS is already initialized */
-  tdb = nc_get_tdb();
-  basic_data = (nc_basic_thread_data_t *)(tdb + 1);
-  __nc_initial_thread_id = basic_data;
 
   retval = pthread_mutex_init(&__nc_thread_management_lock, NULL);
   if (retval) {
@@ -270,13 +257,53 @@ int __pthread_initialize(void) {
   STAILQ_INIT(&__nc_thread_memory_blocks[0]);
   STAILQ_INIT(&__nc_thread_memory_blocks[1]);
 
-  __nc_running_threads_counter = 1; /* the main thread */
-
-  /* Initialize the main thread TDB */
-  nc_tdb_init(tdb, basic_data);
   __nc_thread_initialized = 1;
-  return retval;
+  return 0;
 }
+
+#if defined(NACL_IN_IRT)
+
+/*
+ * This is used by the IRT for user threads.  We initialize all fields
+ * so that we get predictable behaviour in case some IRT code does an
+ * unsupported pthread operation on a user thread.
+ *
+ * We avoid using nc_tdb_init() because it creates a condvar which we
+ * do not need.
+ */
+void __nc_initialize_unjoinable_thread(struct nc_combined_tdb *tdb) {
+  tdb->tdb.tls_base = tdb;
+  tdb->tdb.joinable = 0;
+  tdb->tdb.join_waiting = 0;
+  tdb->tdb.stack_node = NULL;
+  tdb->tdb.tls_node = NULL;
+  tdb->tdb.start_func = NULL;
+  tdb->tdb.state = NULL;
+  tdb->basic_data.retval = NULL;
+  tdb->basic_data.status = THREAD_RUNNING;
+  pthread_cond_t condvar_init = PTHREAD_COND_INITIALIZER;
+  tdb->basic_data.join_condvar = condvar_init;
+
+  tdb->tdb.basic_data = &tdb->basic_data;
+  tdb->basic_data.tdb = &tdb->tdb;
+}
+
+#else
+
+/* Will be called from the library startup code,
+ * which always happens on the application's main thread
+ */
+int __pthread_initialize(void) {
+  __pthread_initialize_minimal(TDB_SIZE);
+
+  struct nc_combined_tdb *tdb = (struct nc_combined_tdb *) nc_get_tdb();
+  nc_tdb_init(&tdb->tdb, &tdb->basic_data);
+  __nc_initial_thread_id = &tdb->basic_data;
+
+  return __nc_initialize_globals();
+}
+
+#endif
 
 
 /* pthread functions */

src/untrusted/pthread/pthread_types.h
@@ -73,6 +73,19 @@ typedef struct __nc_basic_thread_data {
   nc_thread_descriptor_t *tdb;
 } nc_basic_thread_data_t;
 
+/*
+ * This structure is allocated for all threads.  However, for threads
+ * created with pthread_create(), the basic_data field is not used,
+ * and a separate nc_basic_thread_data_t struct is allocated.  The
+ * initial thread, and threads created by the IRT's thread_create()
+ * interface, do use the basic_data field though.
+ * TODO(mseaborn): Clean this up to be more consistent.
+ */
+struct nc_combined_tdb {
+  nc_thread_descriptor_t tdb;
+  nc_basic_thread_data_t basic_data;
+};
+
 
 #define MEMORY_BLOCK_ALLOCATION_SIZE(real_size) \
   (sizeof(nc_thread_memory_block_t) + (real_size))

SConstruct
@@ -3155,8 +3155,8 @@ nacl_irt_env = nacl_env.Clone(
     NACL_BUILD_FAMILY = 'UNTRUSTED_IRT',
 )
 
-# Since we don't build a whole libpthread for the IRT and thus we can
-# omit the directory, we must tell it how to find the pthread.h header.
+# Since we don't build src/untrusted/pthread/nacl.scons in
+# nacl_irt_env, we must tell the IRT how to find the pthread.h header.
 nacl_irt_env.Append(CPPPATH='${MAIN_DIR}/src/untrusted/pthread')
 
 # Map certain flag bits to suffices on the build output.  This needs to

src/untrusted/irt/irt.gyp
@@ -28,10 +28,13 @@
       'irt_clock.c',
       'irt_dev_exception_handling.c',
 # support_srcs
+      # We also get nc_init_private.c, nc_thread.c and nc_tsd.c via
+      # #includes of .c files.
       '../pthread/nc_mutex.c',
       '../pthread/nc_condvar.c',
       '../pthread/nc_token.c',
       '../nacl/sys_private.c',
+      '../valgrind/dynamic_annotations.c',
     ],
     'irt_nonbrowser': [
       'irt_interfaces.c',

src/untrusted/irt/irt_malloc.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 The Native Client Authors. All rights reserved.
+ * Copyright (c) 2012 The Native Client Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file.
  */
@@ -20,6 +20,7 @@
 #include <unistd.h>
 
 #include "native_client/src/untrusted/irt/irt_private.h"
+#include "native_client/src/untrusted/pthread/pthread_internal.h"
 
 /*
  * These macros parameterize the dlmalloc code the way we want it.
@@ -44,7 +45,7 @@
  */
 #define MALLOC_FAILURE_ACTION   irt_malloc_failure()
 static void irt_malloc_failure(void) {
-  if (!irt_initialized) {
+  if (!__nc_thread_initialized) {
     static const char msg[] = "Memory allocation failure in IRT startup!\n";
     write(2, msg, sizeof msg - 1);
     _exit(-1);

src/untrusted/irt/irt_private.h
@@ -6,7 +6,6 @@
 #ifndef NATIVE_CLIENT_SRC_UNTRUSTED_IRT_IRT_PRIVATE_H_
 #define NATIVE_CLIENT_SRC_UNTRUSTED_IRT_IRT_PRIVATE_H_
 
-extern int irt_initialized;
 extern __thread int g_is_main_thread;
 
 int irt_nameservice_lookup(const char *name, int oflag, int *out_fd);

src/untrusted/irt/irt_private_pthread.c
@@ -11,15 +11,6 @@
 #include "native_client/src/untrusted/nacl/tls.h"
 
 /*
- * This is used by the pthread mutex implementation.  It really doesn't
- * need a pthread_t it can do anything with, just a unique identifier for
- * the thread.  So using our TLS pointer works just fine.
- */
-pthread_t pthread_self(void) {
-  return __nacl_read_tp();
-}
-
-/*
  * libstdc++ makes minimal use of pthread_key_t stuff.
  */
 
@@ -32,4 +23,7 @@ static void irt_tsd_no_more_keys(void) {
   write(2, msg, sizeof msg - 1);
 }
 
-#include "native_client/src/untrusted/pthread/nc_tsd.c"
+#define NACL_IN_IRT
+
+#include "native_client/src/untrusted/pthread/nc_init_private.c"
+#include "native_client/src/untrusted/pthread/nc_thread.c"

commit 25f18f7394a1586508a54b79f254422cd3fa4267

IRT: Fix free() of invalid address in thread_exit()
The calculation to get the malloc()'d block from the TDB pointer was
not correct, and this led to a free() of an invalid address.  This
mistake was introduced by r8685.

I discovered this when a futex-related change perturbed the memory
layout enough to cause free() to detect memory corruption and call
abort().

The bug is also detected by Valgrind, which I will enable for
run_irt_thread_test in a later change.

It is actually not possible to get the malloc()'d block pointer from
the TDB pointer using simple arithmetic, because
__nacl_tls_initialize_memory() applies some address alignment that is
not reversible.

So, to fix the bug, we go back to the strategy from before r8685 of
storing a pointer to the malloc()'d block in the TDB.

BUG=https://code.google.com/p/nativeclient/issues/detail?id=3184
TEST=run_irt_thread_test under valgrind (see above)

Review URL: https://codereview.chromium.org/11411309

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@10366 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: 5dd219f9522fedff013956569b7c005c3c9b8403

src/untrusted/irt/irt_thread.c
@@ -4,6 +4,7 @@
  * found in the LICENSE file.
  */
 
+#include <assert.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,11 +22,6 @@ static struct nc_combined_tdb *get_irt_tdb(void *thread_ptr) {
   struct nc_combined_tdb *tdb = (void *) ((uintptr_t) thread_ptr +
                                           __nacl_tp_tdb_offset(sizeof(*tdb)));
   return tdb;
-}
-
-/* This is the inverse of get_irt_tdb(). */
-static void *get_malloc_block_from_tdb(struct nc_combined_tdb *tdb) {
-  return (void *) ((uintptr_t) tdb - __nacl_tp_tdb_offset(sizeof(*tdb)));
 }
 
 /*
@@ -47,6 +43,7 @@ void __pthread_initialize(void) {
   void *tp = __nacl_tls_initialize_memory(combined_area, sizeof(*tdb));
   tdb = get_irt_tdb(tp);
   __nc_initialize_unjoinable_thread(tdb);
+  tdb->tdb.irt_thread_data = combined_area;
 
   /*
    * Now install it for later fetching.
@@ -67,7 +64,14 @@ static void nacl_irt_thread_exit(int32_t *stack_flag) {
 
   __nc_tsd_exit();
 
-  free(get_malloc_block_from_tdb(tdb));
+  /*
+   * Sanity check: Check that this function was not called on a thread
+   * created by the IRT's internal pthread_create().  For such
+   * threads, irt_thread_data == NULL.
+   */
+  assert(tdb->tdb.irt_thread_data != NULL);
+
+  free(tdb->tdb.irt_thread_data);
 
   NACL_SYSCALL(thread_exit)(stack_flag);
   while (1) *(volatile int *) 0 = 0;  /* Crash.  */
@@ -134,9 +138,17 @@ static int nacl_irt_thread_create(void *start_user_address, void *stack,
   if (combined_area == NULL)
     return EAGAIN;
 
+  /*
+   * Note that __nacl_tls_initialize_memory() is not reversible,
+   * because it takes a pointer that need not be aligned and can
+   * return a pointer that is aligned.  In order to
+   * free(combined_area) later, we must save the value of
+   * combined_area.
+   */
   void *irt_tp = __nacl_tls_initialize_memory(combined_area, sizeof(*tdb));
   tdb = get_irt_tdb(irt_tp);
   __nc_initialize_unjoinable_thread(tdb);
+  tdb->tdb.irt_thread_data = combined_area;
   /*
    * We overload the libpthread start_func field to store a function
    * of a different type.

src/untrusted/pthread/nc_thread.c
@@ -243,6 +243,8 @@ static int nc_tdb_init(nc_thread_descriptor_t *tdb,
   tdb->start_func = NULL;
   tdb->state = NULL;
 
+  tdb->irt_thread_data = NULL;
+
   /* Imitate PTHREAD_COND_INITIALIZER - we cannot use it directly here,
    * since this is not variable initialization.
    */
@@ -292,6 +294,7 @@ void __nc_initialize_unjoinable_thread(struct nc_combined_tdb *tdb) {
   tdb->tdb.tls_node = NULL;
   tdb->tdb.start_func = NULL;
   tdb->tdb.state = NULL;
+  tdb->tdb.irt_thread_data = NULL;
   tdb->basic_data.retval = NULL;
   tdb->basic_data.status = THREAD_RUNNING;
   pthread_cond_t condvar_init = PTHREAD_COND_INITIALIZER;

src/untrusted/pthread/pthread_types.h
@@ -62,6 +62,13 @@ typedef struct {
   nc_thread_memory_block_t *tls_node;
   nc_thread_function  start_func;
   void* state;
+  /*
+   * irt_thread_data is used when libpthread is linked into the IRT.
+   * It is used for free()ing the thread block.
+   * TODO(mseaborn): This plays a similar role to tls_node; the two
+   * could be unified in future.
+   */
+  void *irt_thread_data;
   struct __nc_basic_thread_data *basic_data;
 } nc_thread_descriptor_t;
 