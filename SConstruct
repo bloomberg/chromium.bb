bloomberg/chromium.bb

commit 82ed57c6cda7cd18b07575aa574cbfa5e6054e9b

Build chrome and nacl parts of the build separately.
Adding a --no_chrome_side flag to the scons build,
to allow the chrome side portion of the build to be gated out.

Test this portion of the build last and separately.

BUG=http://code.google.com/p/nativeclient/issues/detail?id=2957
TEST=many
R=ncbray@chromium.org

Review URL: https://chromiumcodereview.appspot.com/10828402

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@9589 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: 116e07e4de8f799e016c84709918ffafb1853151

SConstruct
@@ -329,6 +329,9 @@ def SetUpArgumentBits(env):
     desc='Use the zero-address-based x86-64 sandbox model instead of '
       'the r15-based model.')
 
+  BitFromArgument(env, 'enable_chrome_side', default=True,
+    desc='Exclude chrome-only components from build.')
+
   #########################################################################
   # EXPERIMENTAL
   # This is for generating a testing library for use within private test
@@ -1234,11 +1237,29 @@ def ExtractPublishedFiles(env, target_name):
 pre_base_env.AddMethod(ExtractPublishedFiles)
 
 
-# Pull in the Chrome side of the build.
-# As there are multiple contexts in which the Chrome build
-# will need to inject files, a method (AddChromeFilesFromGroup)
-# is added which gets called in each.
-SConscript('chrome_root.scons', exports=['pre_base_env'])
+# Optionally exclude the chrome side of the build.
+# As there are multiple contexts in which the chrome build
+# will need to inject files, call this method in each to grant
+# the chrome side an opportunity to inject them.
+# When the chrome side is absent, provide an empty stub.
+def AddChromeFilesFromGroup(env, file_group):
+  pass
+if not pre_base_env.Bit('enable_chrome_side'):
+  pre_base_env.AddMethod(AddChromeFilesFromGroup)
+# Currently chrome_root.scons in this directory is the entry point for chrome
+# build/test that will be moved to the chrome side. On the chrome side,
+# we will land a copy of this file renamed to chrome_main.scons.
+# If this file is present, we will assume we've landing the migrated files and
+# use chrome_main.scons as the entry point. Prior to that, we will fall back
+# to chrome_root.scons.
+elif os.path.exists(pre_base_env.File(
+    '#/../ppapi/native_client/chrome_main.scons').abspath):
+  SConscript('#/../ppapi/native_client/chrome_main.scons',
+      exports=['pre_base_env'])
+# TODO(bradnelson): drop once chrome_root.scons -> chrome_main.scons
+#     http://code.google.com/p/nativeclient/issues/detail?id=2957
+else:
+  SConscript('chrome_root.scons', exports=['pre_base_env'])
 
 
 def ProgramNameForNmf(env, basename):
@@ -2665,11 +2686,7 @@ nacl_env.Append(
     'src/trusted/validator_x86/nacl.scons',
     'src/trusted/weak_ref/nacl.scons',
     'src/untrusted/crash_dump/nacl.scons',
-    'src/untrusted/irt_stub/nacl.scons',
     'src/untrusted/nacl/nacl.scons',
-    'src/untrusted/nacl_ppapi_util/nacl.scons',
-    'src/untrusted/pnacl_irt_shim/nacl.scons',
-    'src/untrusted/pnacl_support_extension/nacl.scons',
     'src/untrusted/valgrind/nacl.scons',
     ####  ALPHABETICALLY SORTED ####
 ])
@@ -2691,8 +2708,6 @@ irt_variant_tests = [
     'tests/autoloader/nacl.scons',
     'tests/bigalloc/nacl.scons',
     'tests/blob_library_loading/nacl.scons',
-    'tests/browser_dynamic_library/nacl.scons',
-    'tests/browser_startup_time/nacl.scons',
     'tests/bundle_size/nacl.scons',
     'tests/callingconv/nacl.scons',
     'tests/callingconv_case_by_case/nacl.scons',
@@ -2707,8 +2722,6 @@ irt_variant_tests = [
     'tests/egyptian_cotton/nacl.scons',
     'tests/environment_variables/nacl.scons',
     'tests/exception_test/nacl.scons',
-# Disabled by Brad Chen 4 Sep to try to green Chromium nacl_integrationt tests
-#    'tests/fault_injection/nacl.scons',
     'tests/fib/nacl.scons',
     'tests/file/nacl.scons',
     'tests/fixedfeaturecpu/nacl.scons',
@@ -2726,14 +2739,12 @@ irt_variant_tests = [
     'tests/loop/nacl.scons',
     'tests/mandel/nacl.scons',
     'tests/manifest_file/nacl.scons',
-    'tests/manifest_file/nacl_chrome.scons',
     'tests/math/nacl.scons',
     'tests/memcheck_test/nacl.scons',
     'tests/mmap/nacl.scons',
     'tests/mmap_race_protect/nacl.scons',
     'tests/nacl_log/nacl.scons',
     'tests/nameservice/nacl.scons',
-    'tests/nameservice/nacl_chrome.scons',
     'tests/nanosleep/nacl.scons',
     'tests/noop/nacl.scons',
     'tests/nrd_xfer/nacl.scons',
@@ -2742,8 +2753,6 @@ irt_variant_tests = [
     'tests/nullptr/nacl.scons',
     'tests/pagesize/nacl.scons',
     'tests/pnacl_abi/nacl.scons',
-    'tests/pnacl_client_translator/nacl.scons',
-    'tests/postmessage_redir/nacl.scons',
     'tests/redir/nacl.scons',
     'tests/rodata_not_writable/nacl.scons',
     'tests/sbrk/nacl.scons',
@@ -2774,9 +2783,7 @@ irt_variant_tests = [
     # works in nacl_env but not in nacl_irt_test_env) while also
     # adding tests to nacl_irt_test_env.
     'tests/inbrowser_test_runner/nacl.scons',
-    'tests/inbrowser_test_runner/nacl_chrome.scons',
     'tests/untrusted_crash_dump/nacl.scons',
-    'tests/untrusted_crash_dump/nacl_chrome.scons',
 ]
 
 # These are tests that are NOT worthwhile to run in an IRT variant.
@@ -2788,12 +2795,10 @@ nonvariant_tests = [
     'tests/barebones/nacl.scons',
     'tests/chrome_extension/nacl.scons',
     'tests/custom_desc/nacl.scons',
-    'tests/exit_status/nacl.scons',
     'tests/faulted_thread_queue/nacl.scons',
     'tests/imc_sockets/nacl.scons',
     'tests/minnacl/nacl.scons',
     'tests/multiple_sandboxes/nacl.scons',
-    'tests/nacl.scons',
     # Potential issue with running them:
     # http://code.google.com/p/nativeclient/issues/detail?id=2092
     # See also the comment in "buildbot/buildbot_standard.py"
@@ -2812,6 +2817,7 @@ nonvariant_tests = [
 nacl_env.Append(BUILD_SCONSCRIPTS=nonvariant_tests)
 nacl_env.AddChromeFilesFromGroup('nonvariant_test_scons_files')
 nacl_env.Append(BUILD_SCONSCRIPTS=irt_variant_tests)
+nacl_env.AddChromeFilesFromGroup('irt_variant_test_scons_files')
 
 # Defines TESTS_TO_RUN_INBROWSER.
 SConscript('tests/inbrowser_test_runner/selection.scons',
@@ -3036,7 +3042,13 @@ def AddImplicitLibs(env):
     #       is not really needed.
     #       Note: the "precious" mechanism did not work in this case
     if not env.Bit('built_elsewhere'):
-      implicit_libs += ['libpnacl_irt_shim.a']
+      if env.Bit('enable_chrome_side'):
+        implicit_libs += ['libpnacl_irt_shim.a']
+      else:
+        if env.Bit('pnacl_generate_pexe'):
+          env.Append(TRANSLATEFLAGS=['--noirtshim'])
+        else:
+          env.Append(LINKFLAGS=['--pnacl-allow-native', '-Wt,--noirtshim'])
 
   if not env.Bit('nacl_glibc'):
     # These are automatically linked in by the compiler, either directly
@@ -3093,7 +3105,8 @@ nacl_irt_test_env = nacl_env.Clone(
     BUILD_SCONSCRIPTS = [],
     )
 nacl_irt_test_env.SetBits('tests_use_irt')
-nacl_irt_test_env.Replace(TESTRUNNER_LIBS=['testrunner_browser'])
+if nacl_irt_test_env.Bit('enable_chrome_side'):
+  nacl_irt_test_env.Replace(TESTRUNNER_LIBS=['testrunner_browser'])
 
 nacl_irt_test_env.Append(BUILD_SCONSCRIPTS=irt_variant_tests)
 nacl_irt_test_env.Append(BUILD_SCONSCRIPTS=irt_only_tests)

buildbot/buildbot_lib.py
@@ -242,7 +242,7 @@ def Command(context, cmd, cwd=None):
 
 # A specialized version of CommandStep.
 def SCons(context, mode=None, platform=None, parallel=False, browser_test=False,
-          args=(), cwd=None):
+          args=(), cwd=None, enable_chrome_side=False):
   python = sys.executable
   if mode is None: mode = context['default_scons_mode']
   if platform is None: platform = context['default_scons_platform']
@@ -268,6 +268,7 @@ def SCons(context, mode=None, platform=None, parallel=False, browser_test=False,
   if context['clang']: cmd.append('--clang')
   if context['asan']: cmd.append('--asan')
   if context['use_glibc']: cmd.append('--nacl_glibc')
+  cmd.append('enable_chrome_side=%s' % enable_chrome_side)
   # Append used-specified arguments.
   cmd.extend(args)
   Command(context, cmd, cwd)

buildbot/buildbot_pnacl.sh
@@ -29,7 +29,8 @@ readonly SCONS_S_M_L_IRT="small_tests_irt medium_tests_irt large_tests_irt"
 # subset of tests used on toolchain builders
 readonly SCONS_TC_TESTS="small_tests medium_tests large_tests"
 
-readonly SCONS_COMMON="./scons --verbose bitcode=1"
+readonly CHROME_SELECT="enable_chrome_side=${ENABLE_CHROME_SIDE:-False}"
+readonly SCONS_COMMON="./scons --verbose bitcode=1 ${CHROME_SELECT}"
 readonly UP_DOWN_LOAD="buildbot/file_up_down_load.sh"
 # This script is used by toolchain bots (i.e. tc-xxx functions)
 readonly PNACL_BUILD="pnacl/build.sh"
@@ -390,6 +391,7 @@ single-browser-test() {
 browser-tests() {
   local platform=$1
   local extra=$2
+  export ENABLE_CHROME_SIDE=True
   if [[ "${extra}" =~ --nacl_glibc ]]; then
     # For glibc, only non-pexe mode works for now.
     # We need to ensure psos are translated before running.

buildbot/buildbot_standard.py
@@ -171,12 +171,6 @@ def BuildScript(status, context):
                           not context['off_trunk'] and
                           not context['asan'])
 
-  # If we're running browser tests on a 64-bit Windows machine, build a 32-bit
-  # plugin.
-  need_plugin_32 = (context.Windows() and
-                    context['bits'] == '64' and
-                    not inside_toolchain)
-
   # Clean out build directories.
   with Step('clobber', status):
     RemoveDirectory(r'scons-out')
@@ -314,13 +308,6 @@ def BuildScript(status, context):
   with Step('scons_compile', status):
     SCons(context, parallel=True, args=[])
 
-  if need_plugin_32:
-    with Step('plugin_compile_32', status):
-      SCons(context, platform='x86-32', parallel=True, args=['plugin'])
-
-  with Step('compile IRT tests', status):
-    SCons(context, parallel=True, mode=['nacl_irt_test'])
-
   ### BEGIN tests ###
   with Step('small_tests', status, halt_on_fail=False):
     SCons(context, args=['small_tests'])
@@ -329,20 +316,12 @@ def BuildScript(status, context):
   with Step('large_tests', status, halt_on_fail=False):
     SCons(context, args=['large_tests'])
 
-  if do_integration_tests:
-    with Step('chrome_browser_tests', status, halt_on_fail=False):
-      # Note that we have to add nacl_irt_test to --mode in order to
-      # get inbrowser_test_runner to run.
-      # TODO(mseaborn): Change it so that inbrowser_test_runner is not
-      # a special case.
-      SCons(context, browser_test=True,
-            mode=context['default_scons_mode'] + ['nacl_irt_test'],
-            args=['SILENT=1', 'chrome_browser_tests'])
+  with Step('compile IRT tests', status):
+    SCons(context, parallel=True, mode=['nacl_irt_test'])
 
   with Step('small_tests under IRT', status, halt_on_fail=False):
     SCons(context, mode=context['default_scons_mode'] + ['nacl_irt_test'],
           args=['small_tests_irt'])
-
   with Step('medium_tests under IRT', status, halt_on_fail=False):
     SCons(context, mode=context['default_scons_mode'] + ['nacl_irt_test'],
           args=['medium_tests_irt'])
@@ -388,6 +367,32 @@ def BuildScript(status, context):
     SCons(context,
           mode=context['default_scons_mode'] + ['nacl_irt_test'],
           args=args)
+
+  # These tests will move to chrome side.
+  if do_integration_tests:
+    # If we're running browser tests on a 64-bit Windows machine, build a 32-bit
+    # plugin.
+    if (context.Windows() and
+        context['bits'] == '64' and
+        not inside_toolchain):
+      with Step('plugin_compile_32', status):
+        SCons(context, platform='x86-32', parallel=True, args=['plugin'],
+              enable_chrome_side=True)
+
+    with Step('scons_compile (chrome side)', status, halt_on_fail=False):
+      # Build chrome side of the build.
+      SCons(context, parallel=True, args=[],
+            enable_chrome_side=True)
+
+    with Step('chrome_browser_tests', status, halt_on_fail=False):
+      # Note that we have to add nacl_irt_test to --mode in order to
+      # get inbrowser_test_runner to run.
+      # TODO(mseaborn): Change it so that inbrowser_test_runner is not
+      # a special case.
+      SCons(context, browser_test=True,
+            mode=context['default_scons_mode'] + ['nacl_irt_test'],
+            args=['SILENT=1', 'chrome_browser_tests'],
+            enable_chrome_side=True)
 
 
 def Main():

chrome_root.scons
@@ -37,6 +37,35 @@ def ExtendFileList(existing, additional):
     combined.add(file_name)
   return sorted(combined)
 
+ppapi_scons_files['nonvariant_test_scons_files'] = ExtendFileList(
+    ppapi_scons_files.get('nonvariant_test_scons_files', []), [
+        'tests/browser_dynamic_library/nacl.scons',
+        'tests/browser_startup_time/nacl.scons',
+        'tests/exit_status/nacl.scons',
+        'tests/inbrowser_test_runner/nacl_chrome.scons',
+        'tests/manifest_file/nacl_chrome.scons',
+        'tests/nameservice/nacl_chrome.scons',
+        'tests/postmessage_redir/nacl.scons',
+        'tests/untrusted_crash_dump/nacl_chrome.scons',
+    ])
+
+ppapi_scons_files['irt_variant_test_scons_files'] = ExtendFileList(
+    ppapi_scons_files.get('irt_variant_test_scons_files', []), [
+        # Disabled by Brad Chen 4 Sep to try to green Chromium
+        # nacl_integration tests
+        #'tests/fault_injection/nacl.scons',
+        'tests/nacl.scons',
+        'tests/pnacl_client_translator/nacl.scons',
+    ])
+
+ppapi_scons_files['untrusted_scons_files'] = ExtendFileList(
+    ppapi_scons_files.get('untrusted_scons_files', []), [
+        'src/untrusted/irt_stub/nacl.scons',
+        'src/untrusted/nacl_ppapi_util/nacl.scons',
+        'src/untrusted/pnacl_irt_shim/nacl.scons',
+        'src/untrusted/pnacl_support_extension/nacl.scons',
+    ])
+
 EXTRA_ENV = ['XAUTHORITY', 'HOME', 'DISPLAY', 'SSH_TTY', 'KRB5CCNAME']
 
 def SetupBrowserEnv(env):

pnacl/build.sh
@@ -3050,6 +3050,7 @@ sdk-irt-shim() {
             naclsdk_validate=0 \
             pnacl_generate_pexe=0 \
             --verbose \
+            enable_chrome_side=1 \
             pnacl_irt_shim
   local out_dir_prefix="${SCONS_OUT}"/nacl-x86-64-pnacl-clang
   local outdir="${out_dir_prefix}"/obj/src/untrusted/pnacl_irt_shim

src/untrusted/irt/nacl.scons
@@ -118,10 +118,11 @@ def LinkIrt(output, files, libs):
                                    EXTRA_LIBS=libs + irt_libs)
 
 irt_core_library = LinkIrt('irt_core', irt_nonbrowser, [])
-irt_library = LinkIrt('irt', irt_browser, ['ppruntime'])
-
 env.SDKInstallBin('irt_core.nexe', irt_core_library)
-env.SDKInstallBin('irt.nexe', irt_library)
+
+if env.Bit('enable_chrome_side'):
+  irt_library = LinkIrt('irt', irt_browser, ['ppruntime'])
+  env.SDKInstallBin('irt.nexe', irt_library)
 
 # TODO(mcgrathr): these should be installed, but scons is a mystery
 #env.AddHeaderToSdk(['irt.h'])
@@ -144,15 +145,16 @@ if ((env.Bit('build_arm') or env.Bit('build_x86_32'))
       check_tls_arch = 'arm-pnacl'
     else:
       check_tls_arch = 'arm-gcc'
-  node = env.CommandTest('irt_tls_test.out',
-                         ['${PYTHON}', env.File('check_tls.py'),
-                          check_tls_arch, '${OBJDUMP}', irt_library],
-                         # don't run ${PYTHON} under the emulator.
-                         direct_emulation=False,
-                         # Holy cow it takes over 30 seconds to disassemble
-                         # the ARM irt.nexe. Set test size to 'large'.
-                         size='large')
-  env.AddNodeToTestSuite(node, ['small_tests'], 'run_irt_tls_test')
+  if env.Bit('enable_chrome_side'):
+    node = env.CommandTest('irt_tls_test.out',
+                           ['${PYTHON}', env.File('check_tls.py'),
+                            check_tls_arch, '${OBJDUMP}', irt_library],
+                           # don't run ${PYTHON} under the emulator.
+                           direct_emulation=False,
+                           # Holy cow it takes over 30 seconds to disassemble
+                           # the ARM irt.nexe. Set test size to 'large'.
+                           size='large')
+    env.AddNodeToTestSuite(node, ['small_tests'], 'run_irt_tls_test')
   node = env.CommandTest('irt_core_tls_test.out',
                          ['${PYTHON}', env.File('check_tls.py'),
                           check_tls_arch, '${OBJDUMP}', irt_core_library],

tests/random/nacl.scons
@@ -7,7 +7,7 @@ Import('env')
 
 nexe = env.ComponentProgram(
     env.ProgramNameForNmf('random_test'), 'random_test.c',
-    EXTRA_LIBS=['testrunner_browser'])
+    EXTRA_LIBS=['${TESTRUNNER_LIBS}'])
 
 node = env.CommandSelLdrTestNacl('random_test.out', nexe,
                                  sel_ldr_flags=['-E', 'OUTSIDE_BROWSER=1'])

commit 635a269999b4c57cfacad36922af61fd596abe46

Fix Chrome's irt_variant_test_scons_files to be added to nacl_irt_test_env
This fixes a regression introduced by r9589 that stops
inbrowser_test_runner from running inside Chrome.

irt_variant_tests are supposed to get added to both nacl_env and
nacl_irt_test_env, but in r9589 they were added only to nacl_env.

BUG=http://code.google.com/p/chromium/issues/detail?id=162037
TEST=tested manually along with a Chrome-side change

Review URL: https://codereview.chromium.org/11434092

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@10369 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: ced554ab11801e887fd748d907d8cd75b56bf3c5

SConstruct
@@ -3241,6 +3241,7 @@ if nacl_irt_test_env.Bit('enable_chrome_side'):
   nacl_irt_test_env.Replace(TESTRUNNER_LIBS=['testrunner_browser'])
 
 nacl_irt_test_env.Append(BUILD_SCONSCRIPTS=irt_variant_tests)
+nacl_irt_test_env.AddChromeFilesFromGroup('irt_variant_test_scons_files')
 nacl_irt_test_env.Append(BUILD_SCONSCRIPTS=irt_only_tests)
 TestsUsePublicLibs(nacl_irt_test_env)
 