f7247d7aec3ecae641afa1c13d8caebf67b0f326
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 22:27:52 +0000

    chrome: Use skia::RefPtr for skia ref-counted objects.
    BUG=163454
    R=sky
    Depends on: https://codereview.chromium.org/11299262
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11280251
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171333 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 14d1cae5089df247eec3f7a9ebb4b5fb0f34a1ba

--- chrome/browser/ui/omnibox/location_bar_util.cc
+++ chrome/browser/ui/omnibox/location_bar_util.cc
@@ -50,11 +50,11 @@ void PaintExtensionActionBackground(const ExtensionAction& action,
     color_utils::AlphaBlend(text_color, background_color, 0x13),
     color_utils::AlphaBlend(text_color, background_color, 0x1d)
   };
-  SkShader* gradient = SkGradientShader::CreateLinear(
-      gradient_bounds, gradient_colors, NULL, 2, SkShader::kClamp_TileMode);
+  skia::RefPtr<SkShader> gradient = skia::AdoptRef(
+      SkGradientShader::CreateLinear(gradient_bounds, gradient_colors,
+                                     NULL, 2, SkShader::kClamp_TileMode));
   SkPaint paint;
-  paint.setShader(gradient);
-  gradient->unref();
+  paint.setShader(gradient.get());
   canvas->DrawRect(bounds, paint);
 
   SkColor border_color =
