503b4753c5f9522556440bbf55db35caa223b400
apatrick@google.com <apatrick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 19:09:54 +0000

    Revert 171174 > Move initialization of D3D earlier in present thread lifetime. > > This improves startup time. > > Fixed bug where initial window size was treated as a resize and triggered the slow GDI path. > > > Review URL: https://chromiumcodereview.appspot.com/11418226
    BUG=163215,164398
    TBR=apatrick@chromium.org
    Review URL: https://codereview.chromium.org/11445021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171269 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 69f74c532814c667b26d190b2e0d58dddf1c9530

--- ui/surface/accelerated_surface_win.cc
+++ ui/surface/accelerated_surface_win.cc
@@ -189,7 +189,6 @@ class PresentThread : public base::Thread,
   void ResetDevice();
 
  protected:
-  virtual void Init();
   virtual void CleanUp();
 
  private:
@@ -352,11 +351,6 @@ void PresentThread::ResetDevice() {
   }
 
   device_->SetVertexDeclaration(vertex_declaration);
-}
-
-void PresentThread::Init() {
-  TRACE_EVENT0("gpu", "Initialize thread");
-  InitDevice();
 }
 
 void PresentThread::CleanUp() {
@@ -1024,7 +1018,7 @@ gfx::Size AcceleratedPresenter::GetWindowSize() {
 
 bool AcceleratedPresenter::CheckDirect3DWillWork() {
   gfx::Size window_size = GetWindowSize();
-  if (window_size != last_window_size_ && last_window_size_.GetArea() != 0) {
+  if (window_size != last_window_size_) {
     last_window_size_ = window_size;
     last_window_resize_time_ = base::Time::Now();
     return false;
