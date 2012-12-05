59fcba4cfb333f2399901c5e2848d0da1ef0feff
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 23:40:34 +0000

    Make the activeTab permission available to stable channel. Convert the Stylizr sample extension to use it rather than [tabs, <all_urls>].
    R=mpcomplete@chromium.org
    BUG=93903
    
    
    Review URL: https://chromiumcodereview.appspot.com/11348368
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171357 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0b53a370894631f31d1ef02bac9aaff270da4700

--- chrome/common/extensions/docs/examples/api/storage/stylizr/popup.js
+++ chrome/common/extensions/docs/examples/api/storage/stylizr/popup.js
@@ -14,7 +14,7 @@ storage.get('css', function(items) {
   console.log(items);
   // If there is CSS specified, inject it into the page.
   if (items.css) {
-    chrome.tabs.insertCSS(null, {code: items.css}, function() {
+    chrome.tabs.insertCSS({code: items.css}, function() {
       if (chrome.extension.lastError) {
         message.innerText = 'Not allowed to inject CSS into special page.';
       } else {
