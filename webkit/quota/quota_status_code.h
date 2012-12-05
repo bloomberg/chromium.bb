f190647829473e7acda003fb8e1b36f0804a2862
arv@chromium.org <arv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 02:45:30 +0000

    QuotaStatusCode: Use enum names instead of hard coding the values
    This is blocking https://bugs.webkit.org/show_bug.cgi?id=102726 since
    QuotaStatusCode hard codes the values which prevents us from changing those
    values.
    
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11434107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171133 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4267cb8f43839891a5ed3b050b94954ed8f00b2c

--- webkit/quota/quota_status_code.h
+++ webkit/quota/quota_status_code.h
@@ -5,18 +5,18 @@
 #ifndef WEBKIT_QUOTA_QUOTA_STATUS_CODE_H_
 #define WEBKIT_QUOTA_QUOTA_STATUS_CODE_H_
 
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebStorageQuotaError.h"
 #include "webkit/storage/webkit_storage_export.h"
 
 namespace quota {
 
-// The numbers should match with the error code defined in
-// third_party/WebKit/Source/WebCore/dom/ExceptionCode.h.
 enum QuotaStatusCode {
   kQuotaStatusOk = 0,
-  kQuotaErrorNotSupported = 9,          // NOT_SUPPORTED_ERR
-  kQuotaErrorInvalidModification = 13,  // INVALID_MODIFICATION_ERR
-  kQuotaErrorInvalidAccess = 15,        // INVALID_ACCESS_ERR
-  kQuotaErrorAbort = 20,                // ABORT_ERR
+  kQuotaErrorNotSupported = WebKit::WebStorageQuotaErrorNotSupported,
+  kQuotaErrorInvalidModification =
+      WebKit::WebStorageQuotaErrorInvalidModification,
+  kQuotaErrorInvalidAccess = WebKit::WebStorageQuotaErrorInvalidAccess,
+  kQuotaErrorAbort = WebKit::WebStorageQuotaErrorAbort,
   kQuotaStatusUnknown = -1,
 };
 
