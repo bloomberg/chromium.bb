441f4717b533daba2fa54a19420900bd93ed7ab3
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-06 00:40:47 +0000

    Use only a Queue for inter-thread communication in sharding_supervisor StdioBuffer.
    This should avoid some threading issues which lead to timeouts.
    
    BUG=164280
    
    Review URL: https://chromiumcodereview.appspot.com/11418313
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171367 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0838c04d3d3dac91f9a4259fc71cd68e17851794

--- tools/sharding_supervisor/stdio_buffer.py
+++ tools/sharding_supervisor/stdio_buffer.py
@@ -12,9 +12,7 @@ import Queue
 
 class StdioBuffer(object):
   def __init__(self, shard):
-    self.line_ready_event = threading.Event()
     self.queue = Queue.Queue()
-    self.lock = threading.Lock()
     self.completed = 0
     self.shard = shard
 
@@ -28,17 +26,13 @@ class StdioBuffer(object):
       char = program_pipe.read(1)
       if not char and self.shard.poll() is not None:
         pipe_running = False
-        self.line_ready_event.set()
       buffer.write(char)
       if char == '\n' or not pipe_running:
         line = buffer.getvalue()
-        if not line and not pipe_running:
-          with self.lock:
-            self.completed += 1
-            self.line_ready_event.set()
-          break
-        self.queue.put((system_pipe, line))
-        self.line_ready_event.set()
+        if line:
+          self.queue.put((system_pipe, line))
+        if not pipe_running:
+          self.queue.put((system_pipe, None))
         buffer.close()
         buffer = cStringIO.StringIO()
 
@@ -49,12 +43,12 @@ class StdioBuffer(object):
     return t
 
   def readline(self):
-    """Emits a tuple of (sys.stderr, line) or (sys.stdout, line).  This is a
-    blocking call."""
-    while self.completed < 2 and self.queue.empty():
-      self.line_ready_event.wait()
-      self.line_ready_event.clear()
-    if not self.queue.empty():
-      return self.queue.get_nowait()
-    else:
-      return (None, None)
+    """Emits a tuple of (sys.stderr, line), (sys.stdout, line), or (None, None)
+    if the process has finished.  This is a blocking call."""
+    while True:
+      (pipe, line) = self.queue.get(True)
+      if line:
+        return (pipe, line)
+      self.completed += 1
+      if self.completed >= 2:
+        return (None, None)
