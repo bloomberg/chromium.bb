3b2b12d862743a54da867225ac1c9cde41349b66
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 06:51:25 +0000

    Add some debugging info for PlatformAppBrowserTest.WindowsApiBounds test
    Also switch from using polling with setTimeout to listening on the onBoundsChanged event. 
    
    This has been intermittently failing on linux. 
    
    BUG=164735
    
    
    Review URL: https://chromiumcodereview.appspot.com/11475014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171709 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 35051a6acb1ed93cde969b8d4528e9972e0ccc38

--- chrome/test/data/extensions/platform_apps/windows_api_bounds/main.js
+++ chrome/test/data/extensions/platform_apps/windows_api_bounds/main.js
@@ -2,6 +2,12 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+function logOnLinux(msg) {
+  if (navigator.userAgent.search("X11; Linux") != -1) {
+    console.log(msg);
+  }
+}
+
 function closeEnough(actual, expected, maxAllowedDifference) {
   return Math.abs(actual - expected) <= maxAllowedDifference;
 }
@@ -21,32 +27,38 @@ function boundsEqual(b1, b2, slop) {
 }
 
 function waitForBounds(expectedBounds, callback) {
+  var currentWindow = chrome.app.window.current();
   function checkBounds() {
-    var bounds = chrome.app.window.current().getBounds();
+    var bounds = currentWindow.getBounds();
     if (!bounds) {
       console.error("getBounds returned null!");
       window.close();
       return;
     }
     if (boundsEqual(bounds, expectedBounds, slop)) {
+      logOnLinux("bounds equal - actual was " + JSON.stringify(bounds));
+      currentWindow.onBoundsChanged.removeListener(checkBounds);
       callback();
     } else {
-      // TODO(asargent) - change this to just listen for the bounds change event
-      // once that's supported.
-      setTimeout(checkBounds, 50);
+      logOnLinux("bounds unequal - actual was " + JSON.stringify(bounds));
     }
   }
+  currentWindow.onBoundsChanged.addListener(checkBounds);
   checkBounds();
 }
 
 var slop = 0;
 
+logOnLinux("sending ready");
 chrome.test.sendMessage("ready", function(response) {
   slop = parseInt(response);
+  logOnLinux("got slop of " + slop + ", waiting for first bounds");
   waitForBounds({left:100, top:200, width:300, height:400}, function() {
+    logOnLinux("saw first bounds, now resizing");
     var newBounds = {left:50, top:100, width:150, height:200};
     chrome.app.window.current().setBounds(newBounds);
     waitForBounds(newBounds, function(){
+      logOnLinux("saw second bounds, sending success message");
       chrome.test.sendMessage("success");
     });
   });
