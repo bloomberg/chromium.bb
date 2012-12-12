ee788600a0d97e88de1c308de47e99d912ff561f
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-12 22:41:18 +0000

    Include <dialog> in --enable-experimental-webkit-features flag
    BUG=140205
    
    Review URL: https://codereview.chromium.org/11545003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172676 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 23b0d21a340522b6416b39460dfeb9f444a115b5

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -698,6 +698,9 @@ void RenderThreadImpl::EnsureWebKitInitialized() {
   WebRuntimeFeatures::enableCSSRegions(
       command_line.HasSwitch(switches::kEnableExperimentalWebKitFeatures));
 
+  WebRuntimeFeatures::enableDialogElement(
+      command_line.HasSwitch(switches::kEnableExperimentalWebKitFeatures));
+
   FOR_EACH_OBSERVER(RenderProcessObserver, observers_, WebKitInitialized());
 
   devtools_agent_message_filter_ = new DevToolsAgentFilter();
