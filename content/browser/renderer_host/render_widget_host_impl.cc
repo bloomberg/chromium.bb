c8637674bf4725d9968e1b36e8e9fbe78ef59635
yusufo@chromium.org <yusufo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 03:31:27 +0000

    Don't call FlingHasBeenhalted from setIsLoading
    Calling FlingHasBeenHalted sets the fling_in_progress to false without checking
    whether we are flinging or not and if we managed to get a fling started while
    loading this drops flingCancel gestures  and makes us hit the DCHECK in scrollBegin
    that checks whether the currentScrollingLayer is null or not.
    
    BUG=162767
    
    
    Review URL: https://chromiumcodereview.appspot.com/11416361
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172804 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d65a27f1844fb3790a94bd44260a705995901e3d

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -548,7 +548,6 @@ void RenderWidgetHostImpl::ViewDestroyed() {
 
 void RenderWidgetHostImpl::SetIsLoading(bool is_loading) {
   is_loading_ = is_loading;
-  gesture_event_filter_->FlingHasBeenHalted();
   if (!view_)
     return;
   view_->SetIsLoading(is_loading);
