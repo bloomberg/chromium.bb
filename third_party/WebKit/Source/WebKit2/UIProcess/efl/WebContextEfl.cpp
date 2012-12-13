bloomberg/chromium.bb

commit ce9b1d909253aed0870aea2b848d69adef646417

[GTK] Cookies' storage path and policy are not set when the WebProcess is killed and relaunched https://bugs.webkit.org/show_bug.cgi?id=104370
Patch by Joaquim Rocha <jrocha@igalia.com> on 2012-12-12
Reviewed by Carlos Garcia Campos.

This issue happens on ports that use Soup and causes problems like not finding the stored cookies
nor their policy when the WebProcess is relaunched. As an example of a problem caused by this,
a previously logged in user will not be logged in again when the process is relaunched and the
policy used is always the same one.

* Shared/WebProcessCreationParameters.cpp:
(WebKit::WebProcessCreationParameters::encode): Encode cookies' persistent storage path,
storage type and acceptance policy.
(WebKit::WebProcessCreationParameters::decode): Decode cookies' persistent storage path,
storage type and acceptance policy.
* Shared/WebProcessCreationParameters.h: Create 3 variables to hold the values mentioned above.
(WebProcessCreationParameters):
* UIProcess/WebCookieManagerProxy.cpp:
(WebKit::WebCookieManagerProxy::WebCookieManagerProxy):
(WebKit::WebCookieManagerProxy::setHTTPCookieAcceptPolicy): Call WebCookieManagerProxySoup::setCookieAcceptPolicy.
* UIProcess/WebCookieManagerProxy.h: Declare new Soup's methods setCookieAcceptPolicy, cookieAcceptPolicy and
cookiePersistentStorage as well as the variables m_cookieAcceptPolicy and m_cookiePersistentStorage.
(WebCookieManagerProxy):
* UIProcess/efl/WebContextEfl.cpp: Assign the parameters' cookiePersistentStoragePath and
cookiePersistentStorageType with the respective values.
(WebKit::WebContext::platformInitializeWebProcess):
* UIProcess/gtk/WebContextGtk.cpp: Assign the parameters' cookiePersistentStoragePath and
cookiePersistentStorageType with the respective values.
(WebKit::WebContext::platformInitializeWebProcess):
* UIProcess/soup/WebCookieManagerProxySoup.cpp:
(WebKit::WebCookieManagerProxy::setCookiePersistentStorage): Assign the m_cookiePersistentStorage pair

third_party/WebKit/Source/WebKit2/UIProcess/efl/WebContextEfl.cpp
@@ -24,6 +24,7 @@
  */
 
 #include "config.h"
+#include "WebCookieManagerProxy.h"
 #include "WebContext.h"
 
 #include "Logging.h"
@@ -88,6 +89,7 @@ void WebContext::platformInitializeWebProcess(WebProcessCreationParameters& para
     initializeInspectorServer();
 
     parameters.urlSchemesRegistered = m_soupRequestManagerProxy->registeredURISchemes();
+    m_cookieManagerProxy->getCookiePersistentStorage(parameters.cookiePersistentStoragePath, parameters.cookieAcceptPolicy);
 }
 
 void WebContext::platformInvalidateContext()

third_party/WebKit/Source/WebKit2/Shared/WebProcessCreationParameters.cpp
@@ -78,6 +78,9 @@ void WebProcessCreationParameters::encode(CoreIPC::ArgumentEncoder& encoder) con
 #endif
 #if USE(SOUP)
     encoder << urlSchemesRegistered;
+    encoder << cookiePersistentStoragePath;
+    encoder << cookiePersistentStorageType;
+    encoder.encodeEnum(cookieAcceptPolicy);
 #endif
     encoder.encodeEnum(cacheModel);
     encoder << shouldTrackVisitedLinks;
@@ -171,6 +174,12 @@ bool WebProcessCreationParameters::decode(CoreIPC::ArgumentDecoder* decoder, Web
 #endif
 #if USE(SOUP)
     if (!decoder->decode(parameters.urlSchemesRegistered))
+        return false;
+    if (!decoder->decode(parameters.cookiePersistentStoragePath))
+        return false;
+    if (!decoder->decode(parameters.cookiePersistentStorageType))
+        return false;
+    if (!decoder->decode(parameters.cookieAcceptPolicy))
         return false;
 #endif
     if (!decoder->decodeEnum(parameters.cacheModel))

third_party/WebKit/Source/WebKit2/Shared/WebProcessCreationParameters.h
@@ -38,6 +38,10 @@
 #include "MachPort.h"
 #endif
 
+#if USE(SOUP)
+#include "HTTPCookieAcceptPolicy.h"
+#endif
+
 namespace CoreIPC {
     class ArgumentDecoder;
     class ArgumentEncoder;
@@ -77,6 +81,9 @@ struct WebProcessCreationParameters {
 #endif
 #if USE(SOUP)
     Vector<String> urlSchemesRegistered;
+    String cookiePersistentStoragePath;
+    uint32_t cookiePersistentStorageType;
+    HTTPCookieAcceptPolicy cookieAcceptPolicy;
 #endif
 
     CacheModel cacheModel;

third_party/WebKit/Source/WebKit2/UIProcess/WebContext.h
@@ -169,10 +169,10 @@ public:
 
 #if PLATFORM(WIN)
     void setShouldPaintNativeControls(bool);
-
+#endif
+#if PLATFORM(WIN) || USE(SOUP)
     void setInitialHTTPCookieAcceptPolicy(HTTPCookieAcceptPolicy policy) { m_initialHTTPCookieAcceptPolicy = policy; }
 #endif
-
     void setEnhancedAccessibility(bool);
     
     // Downloads.
@@ -398,6 +398,8 @@ private:
 
 #if PLATFORM(WIN)
     bool m_shouldPaintNativeControls;
+#endif
+#if PLATFORM(WIN) || USE(SOUP)
     HTTPCookieAcceptPolicy m_initialHTTPCookieAcceptPolicy;
 #endif
 

third_party/WebKit/Source/WebKit2/UIProcess/WebCookieManagerProxy.cpp
@@ -41,6 +41,9 @@ PassRefPtr<WebCookieManagerProxy> WebCookieManagerProxy::create(WebContext* cont
 
 WebCookieManagerProxy::WebCookieManagerProxy(WebContext* context)
     : m_webContext(context)
+#if USE(SOUP)
+    , m_cookiePersistentStorageType(SoupCookiePersistentStorageSQLite)
+#endif
 {
     m_webContext->addMessageReceiver(Messages::WebCookieManagerProxy::messageReceiverName(), this);
 }
@@ -133,6 +136,9 @@ void WebCookieManagerProxy::setHTTPCookieAcceptPolicy(HTTPCookieAcceptPolicy pol
     ASSERT(m_webContext);
 #if PLATFORM(MAC)
     persistHTTPCookieAcceptPolicy(policy);
+#endif
+#if USE(SOUP)
+    m_webContext->setInitialHTTPCookieAcceptPolicy(policy);
 #endif
 
     m_webContext->sendToAllProcessesRelaunchingThemIfNecessary(Messages::WebCookieManager::SetHTTPCookieAcceptPolicy(policy));

third_party/WebKit/Source/WebKit2/UIProcess/WebCookieManagerProxy.h
@@ -35,6 +35,10 @@
 #include <wtf/RefPtr.h>
 #include <wtf/Vector.h>
 
+#if USE(SOUP)
+#include "SoupCookiePersistentStorageType.h"
+#endif
+
 namespace CoreIPC {
     class Connection;
     class MessageID;
@@ -72,6 +76,7 @@ public:
 
 #if USE(SOUP)
     void setCookiePersistentStorage(const String& storagePath, uint32_t storageType);
+    void getCookiePersistentStorage(String& storagePath, uint32_t& storageType) const;
 #endif
 
     bool shouldTerminate(WebProcessProxy*) const;
@@ -99,6 +104,11 @@ private:
     HashMap<uint64_t, RefPtr<HTTPCookieAcceptPolicyCallback> > m_httpCookieAcceptPolicyCallbacks;
 
     WebCookieManagerProxyClient m_client;
+
+#if USE(SOUP)
+    String m_cookiePersistentStoragePath;
+    SoupCookiePersistentStorageType m_cookiePersistentStorageType;
+#endif
 };
 
 } // namespace WebKit

third_party/WebKit/Source/WebKit2/UIProcess/gtk/WebContextGtk.cpp
@@ -29,6 +29,7 @@
 #include "WebContext.h"
 
 #include "Logging.h"
+#include "WebCookieManagerProxy.h";
 #include "WebInspectorServer.h"
 #include "WebProcessCreationParameters.h"
 #include "WebSoupRequestManagerProxy.h"
@@ -87,6 +88,7 @@ void WebContext::platformInitializeWebProcess(WebProcessCreationParameters& para
     initInspectorServer();
 
     parameters.urlSchemesRegistered = m_soupRequestManagerProxy->registeredURISchemes();
+    m_cookieManagerProxy->getCookiePersistentStorage(parameters.cookiePersistentStoragePath, parameters.cookieAcceptPolicy);
 }
 
 void WebContext::platformInvalidateContext()

third_party/WebKit/Source/WebKit2/UIProcess/soup/WebCookieManagerProxySoup.cpp
@@ -34,6 +34,15 @@ namespace WebKit {
 void WebCookieManagerProxy::setCookiePersistentStorage(const String& storagePath, uint32_t storageType)
 {
     m_webContext->sendToAllProcessesRelaunchingThemIfNecessary(Messages::WebCookieManager::SetCookiePersistentStorage(storagePath, storageType));
+
+    m_cookiePersistentStoragePath = storagePath;
+    m_cookiePersistentStorageType = static_cast<SoupCookiePersistentStorageType>(storageType);
+}
+
+void WebCookieManagerProxy::getCookiePersistentStorage(String& storagePath, uint32_t& storageType) const
+{
+    storagePath = m_cookiePersistentStoragePath;
+    storageType = static_cast<uint32_t>(m_cookiePersistentStorageType);
 }
 
 }

commit 34712cd47abcf632d661e124680e0331ec4a481a

[EFL] Update the way we set the cookies policy https://bugs.webkit.org/show_bug.cgi?id=104915
Reviewed by Kenneth Rohde Christiansen.

We are mistakenly assigning the cookie policy to the persistent storage
type which is not correct. This was caused by r137432 and is making
the unit tests fail.

* UIProcess/efl/WebContextEfl.cpp:
(WebKit::WebContext::platformInitializeWebProcess):


git-svn-id: svn://svn.chromium.org/blink/trunk@137598 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 9a0b1c0b4b3de41030175aa996e55def22466992

third_party/WebKit/Source/WebKit2/UIProcess/efl/WebContextEfl.cpp
@@ -89,7 +89,8 @@ void WebContext::platformInitializeWebProcess(WebProcessCreationParameters& para
     initializeInspectorServer();
 
     parameters.urlSchemesRegistered = m_soupRequestManagerProxy->registeredURISchemes();
-    m_cookieManagerProxy->getCookiePersistentStorage(parameters.cookiePersistentStoragePath, parameters.cookieAcceptPolicy);
+    m_cookieManagerProxy->getCookiePersistentStorage(parameters.cookiePersistentStoragePath, parameters.cookiePersistentStorageType);
+    parameters.cookieAcceptPolicy = m_initialHTTPCookieAcceptPolicy;
 }
 
 void WebContext::platformInvalidateContext()