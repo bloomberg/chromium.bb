4619a6b87f8115af4975d8b79d5429c2b6c1168b
dcblack@chromium.org <dcblack@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-15 02:42:27 +0000

    Define Instant Extended field trials in the chrome/common/metrics/variations/variation_ids.h enum.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11516018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173264 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fae76fba25c599d3cc392b4098bd5cf73eef24fe

--- chrome/common/metrics/variations/variation_ids.h
+++ chrome/common/metrics/variations/variation_ids.h
@@ -129,11 +129,15 @@ enum VariationID {
   kSuggestTrialStarted2012Q4IDMin = 3310029,
   kSuggestTrialStarted2012Q4IDMax = 3310048,
 
-  // Name: Instant channel field trial.
+  // Name: Instant channel and extended field trials.
   // Range: 3310050 - 3310059
-  kChannelInstantIDBeta   = 3310050,
-  kChannelInstantIDDev    = 3310051,
-  kChannelInstantIDStable = 3310052,
+  kChannelInstantIDBeta            = 3310050,
+  kChannelInstantIDDev             = 3310051,
+  kChannelInstantIDStable          = 3310052,
+  kExtendedInstantIDCanaryGroupOne = 3310053,
+  kExtendedInstantIDCanaryControl  = 3310054,
+  kExtendedInstantIDDevGroupOne    = 3310055,
+  kExtendedInstantIDDevControl     = 3310056,
 
   // NEXT ID: When adding new IDs, please add them above this section, starting
   // with the value of kNextID, and updating kNextID to (end of your reserved
