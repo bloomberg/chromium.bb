a0df8bb40ef7d521983061be898b6ff66368f965
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-17 21:22:33 +0000

    Disable BrowserPluginHostTest.GuestUnresponsive on win and linux due to flakiness
    TBR=fsamuel@chromium.org
    BUG=164812
    
    Review URL: https://codereview.chromium.org/11615005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173534 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f1db54caa968a0af0c1292479cfc9d343cb9dad6

--- content/browser/browser_plugin/browser_plugin_host_browsertest.cc
+++ content/browser/browser_plugin/browser_plugin_host_browsertest.cc
@@ -343,7 +343,15 @@ class BrowserPluginHostTest : public ContentBrowserTest {
 
 // This test loads a guest that has a busy loop, and therefore it hangs the
 // guest.
-IN_PROC_BROWSER_TEST_F(BrowserPluginHostTest, GuestUnresponsive) {
+//
+// Disabled on Windows and Linux since it is flaky. crbug.com/164812
+#if defined(OS_WIN) || defined(OS_LINUX)
+#define MAYBE_GuestUnresponsive DISABLED_GuestUnresponsive
+#else
+#define MAYBE_GuestUnresponsive GuestUnresponsive
+#endif
+IN_PROC_BROWSER_TEST_F(BrowserPluginHostTest,
+                       MAYBE_GuestUnresponsive) {
   // Override the hang timeout for guest to be very small.
   content::BrowserPluginGuest::set_factory_for_testing(
       TestShortHangTimeoutGuestFactory::GetInstance());
