0ab23c33c5406ee36e6c64151741f6404e460732
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-18 00:01:51 +0000

    Beware of print-read inconsistency when serializing GURLs. BUG=165622
    Review URL: https://chromiumcodereview.appspot.com/11576038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173583 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 74aaa70032784e7cf00256821f29b2b53edb6589

--- content/public/common/common_param_traits.cc
+++ content/public/common/common_param_traits.cc
@@ -50,6 +50,23 @@ namespace IPC {
 
 void ParamTraits<GURL>::Write(Message* m, const GURL& p) {
   DCHECK(p.possibly_invalid_spec().length() <= content::kMaxURLChars);
+
+  // Beware of print-parse inconsistency which would change an invalid
+  // URL into a valid one. Ideally, the message would contain this flag
+  // so that the read side could make the check, but performing it here
+  // avoids changing the on-the-wire representation of such a fundamental
+  // type as GURL. See https://crbug.com/166486 for additional work in
+  // this area.
+  if (!p.is_valid()) {
+    GURL reconstructed_url(p.possibly_invalid_spec());
+    if (reconstructed_url.is_valid()) {
+      DLOG(WARNING) << "GURL string " << p.possibly_invalid_spec()
+                    << " (marked invalid) but parsed as valid.";
+      m->WriteString(std::string());
+      return;
+    }
+  }
+
   m->WriteString(p.possibly_invalid_spec());
   // TODO(brettw) bug 684583: Add encoding for query params.
 }
