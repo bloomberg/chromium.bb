e23351ad766566125af33f5f4a3ab98de3efe029
wangxianzhu@chromium.org <wangxianzhu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 01:19:19 +0000

    Add accelerated_compositing_for_scrollable_frames_enabled field in WebPreferences IPC
    The field was added in WebPreferences in https://codereview.chromium.org/11543002/.
    Now declare it for IPC.
    
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11573054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173819 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 16d42e6f5652b890b1ee4c7875711c85bec3f916

--- content/public/common/common_param_traits_macros.h
+++ content/public/common/common_param_traits_macros.h
@@ -145,6 +145,8 @@ IPC_STRUCT_TRAITS_BEGIN(webkit_glue::WebPreferences)
   IPC_STRUCT_TRAITS_MEMBER(show_composited_layer_tree)
   IPC_STRUCT_TRAITS_MEMBER(show_fps_counter)
   IPC_STRUCT_TRAITS_MEMBER(accelerated_compositing_for_overflow_scroll_enabled)
+  IPC_STRUCT_TRAITS_MEMBER(
+      accelerated_compositing_for_scrollable_frames_enabled)
   IPC_STRUCT_TRAITS_MEMBER(show_paint_rects)
   IPC_STRUCT_TRAITS_MEMBER(render_vsync_enabled)
   IPC_STRUCT_TRAITS_MEMBER(asynchronous_spell_checking_enabled)
