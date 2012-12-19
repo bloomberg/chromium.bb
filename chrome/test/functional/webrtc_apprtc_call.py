b7a3beb02a248c4cda160ce614a1c287223de33a
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 09:38:23 +0000

    Added a AppRTC integration test to PyAuto.
    This is going to be a FYI-style test which tells us if the apprtc.appspot.com reference web application is working.
    
    BUG=
    NOTRY=True
    
    
    Review URL: https://chromiumcodereview.appspot.com/11628005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173885 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ab5c17aa5528f92a3204d72dc6309c6b7e79a9c5

---
+++ chrome/test/functional/webrtc_apprtc_call.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Note: pyauto_functional must come before pyauto.
+import pyauto_functional
+import pyauto
+import webrtc_test_base
+
+
+class WebrtcApprtcCallTest(webrtc_test_base.WebrtcTestBase):
+  """Tests calling apprtc.appspot.com and setting up a call.
+
+  Prerequisites: This test case must run on a machine with a webcam, either
+  fake or real, and with some kind of audio device. The machine must have access
+  to the public Internet.
+
+  This should be considered an integration test: test failures could mean
+  that the AppRTC reference is broken, that WebRTC is broken, or both.
+  """
+
+  def tearDown(self):
+    pyauto.PyUITest.tearDown(self)
+    self.assertEquals('', self.CheckErrorsAndCrashes(),
+                      'Chrome crashed or hit a critical error during test.')
+
+  def testApprtcLoopbackVideoAudioCall(self):
+    self.NavigateToURL('http://apprtc.appspot.com/?debug=loopback')
+    self.WaitForInfobarCount(1, tab_index=0)
+    self.PerformActionOnInfobar('accept', infobar_index=0, tab_index=0)
+
+    self._WaitForCallEstablishment(tab_index=0)
+
+  def _WaitForCallEstablishment(self, tab_index):
+    # AppRTC will set opacity to 1 for remote video when the call is up.
+    video_playing = self.WaitUntil(
+        function=lambda: self.GetDOMValue('remoteVideo.style.opacity',
+                                          tab_index=tab_index),
+        expect_retval='1')
+    self.assertTrue(video_playing,
+                    msg=('Timed out while waiting for '
+                         'remoteVideo.style.opacity to return 1.'))
+
+
+if __name__ == '__main__':
+  pyauto_functional.Main()
