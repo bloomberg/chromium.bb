2b2ddd1ada121e3c531dc3c84e13d9879e7e0fc0
David James <davidjames@chromium.org>
2012-12-19 15:56:36 -0800

    Don't include branch number in manifest version directories.
    Right now, every time we bump the Chrome version, the build versions of the
    canaries and the paladin builders repeat. This is because of a bug in
    manifest-versions.
    
    Currently, builders check whether a given version has been built before
    by looking at the following directory:
      CHROME_BRANCH/CHROMEOS_VERSION
    
    For example, if CHROME_BRANCH is 25 and CHROMEOS_VERSION is 2266.0.0, we look
    at 25/2226.0.0 to see whether 2226.0.0 has been built before. Similarly, if
    CHROME_BRANCH is 26, we look at 26/2226.0.0 to see whether 2226.0.0 has been
    built before. This, obviously, doesn't work too well, since it means that we
    forget the fact that 2226.0.0 has been built before every time we bump the
    chrome branch.
    
    Fortunately, this is easily fixable, by just removing CHROME_BRANCH, which is
    an unnecessary part of the version string.
    
    BUG=chromium-os:30762
    TEST=Run example canary run.
    
    Change-Id: I69b1dba25beb5a626cdd291e5a421d33fbc75bb8
    Reviewed-on: https://gerrit.chromium.org/gerrit/39986
    Reviewed-by: Ryan Cui <rcui@chromium.org>
    Tested-by: David James <davidjames@chromium.org>

--- buildbot/manifest_version.py
+++ buildbot/manifest_version.py
@@ -441,15 +441,14 @@ class BuildSpecsManager(object):
       version_info: Info class for version information of cros.
     """
     working_dir = os.path.join(self.manifest_dir, self.rel_working_dir)
-    dir_pfx = version_info.chrome_branch
     self.specs_for_builder = os.path.join(working_dir, 'build-name',
                                           '%(builder)s')
     specs_for_build = self.specs_for_builder % {'builder': self.build_name}
-    self.all_specs_dir = os.path.join(working_dir, 'buildspecs', dir_pfx)
+    self.all_specs_dir = os.path.join(working_dir, 'buildspecs')
     self.pass_dir = os.path.join(specs_for_build,
-                                 BuilderStatus.STATUS_PASSED, dir_pfx)
+                                 BuilderStatus.STATUS_PASSED)
     self.fail_dir = os.path.join(specs_for_build,
-                                 BuilderStatus.STATUS_FAILED, dir_pfx)
+                                 BuilderStatus.STATUS_FAILED)
 
     # Calculate the status of the latest build, and whether the build was
     # processed. We consider a spec unprocessed if we have not started a build
