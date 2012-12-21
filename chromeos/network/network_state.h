4cd98cc5e3ab48e5be8222060056d7b18eac491e
gauravsh@chromium.org <gauravsh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 19:28:13 +0000

    Implement new network change notifier that uses NetworkStateHandler
    [This is a reland of 174127 which failed clang bots due to missing virtual
    keyword on OVERRIDEs.]
    
    This implements the new network change notifier that informs the Chrome
    network stack of connectivity changes.
    
    It's currently behind the --enable-new-network-handlers command line flag.
    
    BUG=chromium-os:159647
    TEST=unit tests, gmerged on a device and ensured offline->online and
         online->offline transitions are correctly detected.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11469044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174431 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d26930b2926aeb54b819662d888a8929c0bb0847

--- chromeos/network/network_state.h
+++ chromeos/network/network_state.h
@@ -43,6 +43,7 @@ class CHROMEOS_EXPORT NetworkState : public ManagedState {
 
  private:
   friend class NetworkStateHandler;
+  friend class NetworkChangeNotifierChromeosUpdateTest;
 
   // Called by NetworkStateHandler when the ip config changes.
   void set_ip_address(const std::string& ip_address) {
