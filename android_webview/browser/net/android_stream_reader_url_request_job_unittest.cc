bloomberg/chromium.bb

commit ef8b7e3c521132991175328671bb7e17b18256fb

Convert trivial URLRequest::Interceptors and URLRequestJobFactory::Interceptor into URLRequestJobFactory::ProtocolHandlers.
BUG=161524

Review URL: https://codereview.chromium.org/11293252

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174174 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: f53b48075849f4f7d7e13002176cb861c885ab52

android_webview/browser/net/android_stream_reader_url_request_job_unittest.cc
@@ -164,9 +164,10 @@ class AndroidStreamReaderURLRequestJobTest : public Test {
             stream_reader_delegate.Pass(),
             stream_reader.Pass());
     // The Interceptor is owned by the |factory_|.
-    TestJobInterceptor* interceptor = new TestJobInterceptor;
-    interceptor->set_main_intercept_job(test_stream_reader_job);
-    factory_.AddInterceptor(interceptor);
+    TestJobInterceptor* protocol_handler = new TestJobInterceptor;
+    protocol_handler->set_main_intercept_job(test_stream_reader_job);
+    bool set_protocol = factory_.SetProtocolHandler("http", protocol_handler);
+    DCHECK(set_protocol);
   }
 
   MessageLoop loop_;

android_webview/browser/net/aw_url_request_context_getter.cc
@@ -103,7 +103,6 @@ void AwURLRequestContextGetter::Init() {
       chrome::kDataScheme, new net::DataProtocolHandler());
   DCHECK(set_protocol);
   job_factory->AddInterceptor(new AwRequestInterceptor());
-  url_request_context_->set_job_factory(job_factory.get());
 
   // TODO(mnaganov): Fix URLRequestContextBuilder to use proper threads.
   net::HttpNetworkSession::Params network_session_params;
@@ -118,9 +117,9 @@ void AwURLRequestContextGetter::Init() {
   main_http_factory_.reset(main_cache);
   url_request_context_->set_http_transaction_factory(main_cache);
 
-  OnNetworkStackInitialized(url_request_context_.get(),
-                            job_factory.get());
-  job_factory_ = job_factory.Pass();
+  job_factory_ = CreateAndroidJobFactoryAndCookieMonster(
+      url_request_context_.get(), job_factory.Pass());
+  url_request_context_->set_job_factory(job_factory_.get());
 }
 
 void AwURLRequestContextGetter::PopulateNetworkSessionParams(

android_webview/browser/net/init_native_callback.h
@@ -5,8 +5,11 @@
 #ifndef ANDROID_WEBVIEW_BROWSER_NET_INIT_NATIVE_CALLBACK_H_
 #define ANDROID_WEBVIEW_BROWSER_NET_INIT_NATIVE_CALLBACK_H_
 
+#include "base/memory/scoped_ptr.h"
+
 namespace net {
 class URLRequestContext;
+class URLRequestJobFactory;
 }  // namespace net
 
 namespace android_webview {
@@ -15,8 +18,9 @@ class AwURLRequestJobFactory;
 // This is called on the IO thread when the network URLRequestContext has been
 // initialized but not used. Note that the UI thread is blocked during this
 // call.
-void OnNetworkStackInitialized(net::URLRequestContext* context,
-                               AwURLRequestJobFactory* job_factory);
+scoped_ptr<net::URLRequestJobFactory> CreateAndroidJobFactoryAndCookieMonster(
+    net::URLRequestContext* context,
+    scoped_ptr<AwURLRequestJobFactory> job_factory);
 
 }  // namespace android_webview
 

android_webview/native/android_protocol_handler.cc
@@ -20,6 +20,7 @@
 #include "net/base/net_errors.h"
 #include "net/base/net_util.h"
 #include "net/http/http_util.h"
+#include "net/url_request/protocol_intercept_job_factory.h"
 #include "net/url_request/url_request.h"
 
 using android_webview::InputStream;
@@ -66,22 +67,18 @@ class AndroidStreamReaderURLRequestJobDelegateImpl
 };
 
 class AssetFileProtocolInterceptor :
-      public net::URLRequestJobFactory::Interceptor {
+      public net::URLRequestJobFactory::ProtocolHandler {
  public:
-  AssetFileProtocolInterceptor();
   virtual ~AssetFileProtocolInterceptor() OVERRIDE;
-  virtual net::URLRequestJob* MaybeIntercept(
-      net::URLRequest* request,
-      net::NetworkDelegate* network_delegate) const OVERRIDE;
-  virtual net::URLRequestJob* MaybeInterceptRedirect(
-      const GURL& location,
-      net::URLRequest* request,
-      net::NetworkDelegate* network_delegate) const OVERRIDE;
-  virtual net::URLRequestJob* MaybeInterceptResponse(
+  static scoped_ptr<net::URLRequestJobFactory> CreateURLRequestJobFactory(
+      scoped_ptr<net::URLRequestJobFactory> base_job_factory);
+  virtual net::URLRequestJob* MaybeCreateJob(
       net::URLRequest* request,
       net::NetworkDelegate* network_delegate) const OVERRIDE;
 
  private:
+  AssetFileProtocolInterceptor();
+
   // file:///android_asset/
   const std::string asset_prefix_;
   // file:///android_res/
@@ -196,7 +193,19 @@ AssetFileProtocolInterceptor::AssetFileProtocolInterceptor()
 AssetFileProtocolInterceptor::~AssetFileProtocolInterceptor() {
 }
 
-net::URLRequestJob* AssetFileProtocolInterceptor::MaybeIntercept(
+// static
+scoped_ptr<net::URLRequestJobFactory>
+AssetFileProtocolInterceptor::CreateURLRequestJobFactory(
+    scoped_ptr<net::URLRequestJobFactory> base_job_factory) {
+  scoped_ptr<net::URLRequestJobFactory> top_job_factory(
+      new net::ProtocolInterceptJobFactory(
+          base_job_factory.Pass(),
+          scoped_ptr<net::URLRequestJobFactory::ProtocolHandler>(
+              new AssetFileProtocolInterceptor())));
+  return top_job_factory.Pass();
+}
+
+net::URLRequestJob* AssetFileProtocolInterceptor::MaybeCreateJob(
     net::URLRequest* request, net::NetworkDelegate* network_delegate) const {
   if (!request->url().SchemeIsFile()) return NULL;
 
@@ -213,19 +222,6 @@ net::URLRequestJob* AssetFileProtocolInterceptor::MaybeIntercept(
           new AndroidStreamReaderURLRequestJobDelegateImpl()));
 }
 
-net::URLRequestJob* AssetFileProtocolInterceptor::MaybeInterceptRedirect(
-    const GURL& location,
-    net::URLRequest* request,
-    net::NetworkDelegate* network_delegate) const {
-  return NULL;
-}
-
-net::URLRequestJob* AssetFileProtocolInterceptor::MaybeInterceptResponse(
-    net::URLRequest* request,
-    net::NetworkDelegate* network_delegate) const {
-  return NULL;
-}
-
 }  // namespace
 
 namespace android_webview {
@@ -235,18 +231,18 @@ bool RegisterAndroidProtocolHandler(JNIEnv* env) {
 }
 
 // static
-void RegisterAndroidProtocolsOnIOThread(
-    net::URLRequestContext* context,
-    AwURLRequestJobFactory* job_factory) {
+scoped_ptr<net::URLRequestJobFactory> CreateAndroidRequestJobFactory(
+    scoped_ptr<AwURLRequestJobFactory> job_factory) {
   // Register content://. Note that even though a scheme is
   // registered here, it cannot be used by child processes until access to it is
   // granted via ChildProcessSecurityPolicy::GrantScheme(). This is done in
   // AwContentBrowserClient.
   // The job factory takes ownership of the handler.
-  job_factory->SetProtocolHandler(android_webview::kContentScheme,
-                                  new ContentSchemeProtocolHandler());
-  // The job factory takes ownership of the interceptor.
-  job_factory->AddInterceptor(new AssetFileProtocolInterceptor());
+  bool set_protocol = job_factory->SetProtocolHandler(
+      android_webview::kContentScheme, new ContentSchemeProtocolHandler());
+  DCHECK(set_protocol);
+  return AssetFileProtocolInterceptor::CreateURLRequestJobFactory(
+      job_factory.PassAs<net::URLRequestJobFactory>());
 }
 
 // Set a context object to be used for resolving resource queries. This can

android_webview/native/android_protocol_handler.h
@@ -6,9 +6,11 @@
 #define ANDROID_WEBVIEW_NATIVE_ANDROID_PROTOCOL_HANDLER_H_
 
 #include "base/android/jni_android.h"
+#include "base/memory/scoped_ptr.h"
 
 namespace net {
 class URLRequestContext;
+class URLRequestJobFactory;
 }  // namespace net
 
 namespace android_webview {
@@ -24,11 +26,11 @@ class AwURLRequestJobFactory;
 //    (file:///android_asset/ and file:///android_res/), see
 //    http://developer.android.com/reference/android/webkit/
 //      WebSettings.html#setAllowFileAccess(boolean)
-void RegisterAndroidProtocolsOnIOThread(
-    net::URLRequestContext* context,
-    AwURLRequestJobFactory* job_factory);
+scoped_ptr<net::URLRequestJobFactory> CreateAndroidRequestJobFactory(
+    scoped_ptr<AwURLRequestJobFactory> job_factory);
 
 bool RegisterAndroidProtocolHandler(JNIEnv* env);
+
 }  // namespace android_webview
 
 #endif  // ANDROID_WEBVIEW_NATIVE_ANDROID_PROTOCOL_HANDLER_H_

android_webview/native/cookie_manager.cc
@@ -348,9 +348,7 @@ static void SetAcceptFileSchemeCookies(JNIEnv* env, jobject obj,
   return CookieManager::GetInstance()->SetAcceptFileSchemeCookies(accept);
 }
 
-void SetCookieMonsterOnNetworkStackInit(
-    net::URLRequestContext* context,
-    AwURLRequestJobFactory* job_factory) {
+void SetCookieMonsterOnNetworkStackInit(net::URLRequestContext* context) {
   CookieManager::GetInstance()->SetCookieMonster(context);
 }
 

android_webview/native/cookie_manager.h
@@ -14,11 +14,10 @@ class URLRequestContext;
 namespace android_webview {
 class AwURLRequestJobFactory;
 
-void SetCookieMonsterOnNetworkStackInit(net::URLRequestContext* context,
-                                        AwURLRequestJobFactory* job_factory);
+void SetCookieMonsterOnNetworkStackInit(net::URLRequestContext* context);
 
 bool RegisterCookieManager(JNIEnv* env);
 
-}  // namespace android_webview;
+}  // namespace android_webview
 
 #endif  // ANDROID_WEBVIEW_NATIVE_COOKIE_MANAGER_H_

android_webview/native/net_init_native_callback.cc
@@ -4,16 +4,19 @@
 
 #include "android_webview/browser/net/init_native_callback.h"
 
+#include "android_webview/browser/net/aw_url_request_job_factory.h"
 #include "android_webview/native/android_protocol_handler.h"
 #include "android_webview/native/cookie_manager.h"
+#include "net/url_request/url_request_job_factory.h"
 
 namespace android_webview {
 class AwURLRequestJobFactory;
 
-void OnNetworkStackInitialized(net::URLRequestContext* context,
-                               AwURLRequestJobFactory* job_factory) {
-  RegisterAndroidProtocolsOnIOThread(context, job_factory);
-  SetCookieMonsterOnNetworkStackInit(context, job_factory);
+scoped_ptr<net::URLRequestJobFactory> CreateAndroidJobFactoryAndCookieMonster(
+    net::URLRequestContext* context,
+    scoped_ptr<AwURLRequestJobFactory> job_factory) {
+  SetCookieMonsterOnNetworkStackInit(context);
+  return CreateAndroidRequestJobFactory(job_factory.Pass());
 }
 
-}
+}  // namespace android_webview

commit 22f347ba03ac47402596099eab240b4a219b0d91

Fix aw unittests
They were broken in r174174 but since these tests are not run on the cq
or main waterfall, they were broken and no one noticed for awhile.

R=joth@chromium.org,mkosiba@chromium.org

BUG=
Android only change. Ran through Android bots
NOTRY=true


Review URL: https://chromiumcodereview.appspot.com/11637052

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174308 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: f4298ef402a6d3de995cb75a84dcf67d50716f4b

android_webview/browser/net/android_stream_reader_url_request_job_unittest.cc
@@ -168,6 +168,11 @@ class AndroidStreamReaderURLRequestJobTest : public Test {
     protocol_handler->set_main_intercept_job(test_stream_reader_job);
     bool set_protocol = factory_.SetProtocolHandler("http", protocol_handler);
     DCHECK(set_protocol);
+
+    protocol_handler = new TestJobInterceptor;
+    protocol_handler->set_main_intercept_job(test_stream_reader_job);
+    set_protocol = factory_.SetProtocolHandler("content", protocol_handler);
+    DCHECK(set_protocol);
   }
 
   MessageLoop loop_;