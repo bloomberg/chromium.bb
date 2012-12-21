b1221ce611ece239ead2eea69584bf90084a084a
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 07:20:10 +0000

    Revert 174311
    Windows build is failing around libvpx after this patch:
    http://build.chromium.org/p/chromium/builders/Win/builds/14332
    [1220/203018 : error : decomposer.cc(2186)] Block collision for function at 46813200(1536) with ..\..\build\Release\obj\global_intermediate\third_party\libvpx\vp9_subpixel_mmx.obj
    
    > Add wrapper class to media for support of VP9 video, and add a command line flag to enable the support. 
    > 
    > This initial version of the wrapper provides support for decoding VP9 video in WebM container files, and is disabled by default. 
    > 
    > New flag added: --enable-vp9-playback 
    > 
    > BUG=166094
    > TEST=VP9 video in WebM containers plays back in <video> elements when --enable-vp9-playback is specified on the command line. 
    > 
    > 
    > Review URL: https://chromiumcodereview.appspot.com/11468018
    
    TBR=tomfinegan@chromium.org
    Review URL: https://codereview.chromium.org/11578046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174346 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6c9463c58d37922dbd8c47c535f76f72cf40dc41

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -801,7 +801,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableTouchAdjustment,
     switches::kEnableViewport,
     switches::kEnableOpusPlayback,
-    switches::kEnableVp9Playback,
     switches::kForceDeviceScaleFactor,
     switches::kFullMemoryCrashReport,
 #if !defined (GOOGLE_CHROME_BUILD)
