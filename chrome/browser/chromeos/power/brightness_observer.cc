71df52f193cebd4c27249c1aff15af49d3627d2b
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-03 19:26:22 +0000

    Public Accounts: Log out when the screen turns off on idle
    This CL makes Chrome terminate a Public Account session when the user is
    idle and the power manager turns off the screen. Without this CL, the
    Public Account session would also terminate on idle but only after a
    period of several minutes during which the screen is blank.
    
    BUG=chromium-os:37593
    TEST=Manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/11743018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174989 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 71808f7fc58bdd3babbf888928b3341d7c0811be

--- chrome/browser/chromeos/power/brightness_observer.cc
+++ chrome/browser/chromeos/power/brightness_observer.cc
@@ -6,7 +6,9 @@
 
 #include "ash/shell.h"
 #include "ash/wm/power_button_controller.h"
+#include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/extensions/system/system_api.h"
+#include "chrome/browser/lifetime/application_lifetime.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 
 namespace chromeos {
@@ -23,6 +25,17 @@ void BrightnessObserver::BrightnessChanged(int level, bool user_initiated) {
   extensions::DispatchBrightnessChangedEvent(level, user_initiated);
   ash::Shell::GetInstance()->power_button_controller()->
       OnScreenBrightnessChanged(static_cast<double>(level));
+
+  // When the user is idle, the power manager dims the screen, turns off the
+  // screen and eventually locks the screen (if screen lock on idle and suspend
+  // is enabled). For Public Accounts, the session should be terminated instead
+  // as soon as the screen turns off.
+  // This implementation will be superseded after a revamp of the power manager
+  // is complete, see crbug.com/161267.
+  if (UserManager::Get()->IsLoggedInAsPublicAccount() &&
+      !level && !user_initiated) {
+    browser::AttemptUserExit();
+  }
 }
 
 }  // namespace chromeos
