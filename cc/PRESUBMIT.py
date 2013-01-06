4bcd777a74caa81fd3e2c7854d7ab0262be584fa
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-06 05:08:07 +0000

    cc: Ban DLOG/LOG(INFO) from cc via presubmit
    Let's not contribute to spamming the console with messages.
    
    NOTRY=true
    R=danakj@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11778009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175294 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d936f9089ac76c693102352328d75fa8d88acefc

--- cc/PRESUBMIT.py
+++ cc/PRESUBMIT.py
@@ -38,9 +38,28 @@ def CheckAsserts(input_api, output_api, white_list=CC_SOURCE_FILES, black_list=N
       items=notreached_files)]
   return []
 
+def CheckSpamLogging(input_api, output_api, white_list=CC_SOURCE_FILES, black_list=None):
+  black_list = tuple(black_list or input_api.DEFAULT_BLACK_LIST)
+  source_file_filter = lambda x: input_api.FilterSourceFile(x, white_list, black_list)
+
+  bad_files = []
+
+  for f in input_api.AffectedSourceFiles(source_file_filter):
+    contents = input_api.ReadFile(f, 'rb')
+    if re.search(r"\bD?LOG\s*\(\s*INFO\s*\)", contents):
+      bad_files.append(f.LocalPath())
+
+  if bad_files:
+    return [output_api.PresubmitError(
+      'These files spam the console log with LOG(INFO):',
+      items=bad_files)]
+  return []
+
+
 def CheckChangeOnUpload(input_api, output_api):
   results = []
   results += CheckAsserts(input_api, output_api)
+  results += CheckSpamLogging(input_api, output_api)
   return results
 
 def GetPreferredTrySlaves(project, change):
