00f5931c3ab1c41929c5bd7d9cb6d281f5573ed0
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-07 22:38:54 +0000

    [telemetry] Clean up image decoding benchmark
    The image decoding benchmark still contained some logic for navigation/test
    control. The image_decoding_benchmark should measure image decodign times. Critically, the
    logic to make chrome/test/data/image_decoding/image_decoding.html do its work
    should be handled by the page runner, not the benchmark. To make this
    separation happen, three changes are made...
    
    First, page runner now notifies the test that naivgation will happen. This
    allows us to begin recording the timeline right before navigation. This is when timeline
    recording should begin for image decode tests.
    
    Second, this adds a post_navigate_javascript_to_execute method to the page. This allows us to
    call runBenchmark() on the image decoding benchmarks after we navigate.
    
    Finally, the benchmark uses the standard wait_for_javascript_expression to poll
    the isDone variable.The old code actually did this in python.
    
    R=tonyg
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/11753023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175404 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 18d8eef0e3c223361c60efabe1be0a7164aca024

--- tools/perf/perf_tools/image_decoding_benchmark.py
+++ tools/perf/perf_tools/image_decoding_benchmark.py
@@ -3,20 +3,30 @@
 # found in the LICENSE file.
 
 from telemetry import multi_page_benchmark
-from telemetry import util
 
 
 class ImageDecoding(multi_page_benchmark.MultiPageBenchmark):
-  def MeasurePage(self, _, tab, results):
+  def WillNavigateToPage(self, page, tab):
+    tab.timeline.Start()
+
+  def MeasurePage(self, page, tab, results):
+    tab.timeline.Stop()
     def _IsDone():
       return tab.runtime.Evaluate('isDone')
 
-    with tab.timeline.Recorder(tab.timeline):
-      tab.runtime.Execute('runBenchmark()')
-      util.WaitFor(_IsDone, 60)
-    iterations = tab.runtime.Evaluate('minIterations')
-    decode_image = tab.timeline.timeline_events.GetAllOfType('DecodeImage')
-    elapsed_times = [d.elapsed_time for d in decode_image[-iterations:]]
+    decode_image_events = \
+        tab.timeline.timeline_events.GetAllOfType('DecodeImage')
+
+    # If it is a real image benchmark, then store only the last-minIterations
+    # decode tasks.
+    if (hasattr(page,
+                'image_decoding_benchmark_limit_results_to_min_iterations') and
+        page.image_decoding_benchmark_limit_results_to_min_iterations):
+      assert _IsDone()
+      min_iterations = tab.runtime.Evaluate('minIterations')
+      decode_image_events = decode_image_events[-min_iterations:]
+
+    elapsed_times = [d.elapsed_time for d in decode_image_events]
     if not elapsed_times:
       results.Add('ImageDecoding_avg', 'ms', 'unsupported')
       return
