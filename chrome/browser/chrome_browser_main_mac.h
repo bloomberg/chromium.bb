afdc1d329dc64fd130ef80834dc189d3200ccadf
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-08 02:30:52 +0000

    Revert 175471 > [Media Galleries] Add an ImageCaptureCore listener for Mac. (part 2) > > This listener uses the ImageCapture API to watch for attach and detach > events of PTP devices and other devices which can be read through the > library. It forwards such notifications through the SystemMonitor. > > Also provides an API for clients to access such devices directly and > retrieve the media contents from them. > > R=thestig@chromium.org,sail@chromium.org > BUG=151681 > > > Review URL: https://chromiumcodereview.appspot.com/11442057
    TBR=gbillock@chromium.org
    Review URL: https://codereview.chromium.org/11779036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175475 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 67393f5ab2285a9025fe175636836a0647da61df

--- chrome/browser/chrome_browser_main_mac.h
+++ chrome/browser/chrome_browser_main_mac.h
@@ -9,7 +9,6 @@
 #include "base/memory/ref_counted.h"
 
 namespace chrome {
-class ImageCaptureDeviceManager;
 class RemovableDeviceNotificationsMac;
 }
 
@@ -31,8 +30,6 @@ class ChromeBrowserMainPartsMac : public ChromeBrowserMainPartsPosix {
  private:
   scoped_refptr<chrome::RemovableDeviceNotificationsMac>
       removable_device_notifications_mac_;
-
-  scoped_ptr<chrome::ImageCaptureDeviceManager> image_capture_device_manager_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeBrowserMainPartsMac);
 };
