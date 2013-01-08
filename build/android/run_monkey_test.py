1a38b95f47f73e1cabd93763a8108a418fe9f3e3
frankf@google.com <frankf@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-08 19:08:45 +0000

    [Android] Fix import of test_options_parser in run_monkey_test.py
    BUG=
    
    Review URL: https://codereview.chromium.org/11806004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175572 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4f54223dcfeade1bbc2055e942349073d130c497

--- build/android/run_monkey_test.py
+++ build/android/run_monkey_test.py
@@ -11,10 +11,10 @@ import sys
 import time
 
 from pylib import android_commands
-from pylib import test_options_parser
 from pylib import test_result
 from pylib.host_driven import python_test_base
 from pylib.host_driven import python_test_sharder
+from pylib.utils import test_options_parser
 
 
 class MonkeyTest(python_test_base.PythonTestBase):
