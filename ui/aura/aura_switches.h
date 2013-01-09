0145bb0111a7a0389329df250ef804f272a401af
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 19:27:16 +0000

    Add a comment with the format string for --aura-host-window-size
    I keep having to look this up from the implementation
    
    BUG=none
    TEST=none
    TBR=sky@chromium.org - it's just a comment
    
    Review URL: https://codereview.chromium.org/11820027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175853 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2680606a9f85945aa0766815e7d7e5d720d12efd

--- ui/aura/aura_switches.h
+++ ui/aura/aura_switches.h
@@ -10,7 +10,13 @@
 namespace switches {
 
 // Please keep alphabetized.
+
+// Sets a window size, optional position, and optional scale factor.
+// "1024x768" creates a window of size 1024x768.
+// "100+200-1024x768" positions the window at 100,200.
+// "1024x768*2" sets the scale factor to 2 for a high DPI display.
 AURA_EXPORT extern const char kAuraHostWindowSize[];
+
 AURA_EXPORT extern const char kAuraHostWindowUseFullscreen[];
 
 }  // namespace switches
