9a0465d8eb4c7c9a5231af1366ffa4cdb9b9c955
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 19:27:57 +0000

    NaCl: Fix '*' spacing style in nacl_helper_linux.cc
    Use the Chrome style rather than the NaCl style.
    
    Also remove "struct" keywords and use a C++-style cast.
    
    BUG=none
    TEST=build
    
    Review URL: https://codereview.chromium.org/11828010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175854 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a6808c939d2b9231ee7c73d382ff06d74a3b5345

--- chrome/nacl/nacl_helper_linux.cc
+++ chrome/nacl/nacl_helper_linux.cc
@@ -145,14 +145,14 @@ static const char kNaClHelperRDebug[] = "r_debug";
 // dynamic linker's structure into the address provided by the option.
 // Hereafter, if someone attaches a debugger (or examines a core dump),
 // the debugger will find all the symbols in the normal way.
-static void CheckRDebug(char *argv0) {
+static void CheckRDebug(char* argv0) {
   std::string r_debug_switch_value =
       CommandLine::ForCurrentProcess()->GetSwitchValueASCII(kNaClHelperRDebug);
   if (!r_debug_switch_value.empty()) {
-    char *endp;
+    char* endp;
     uintptr_t r_debug_addr = strtoul(r_debug_switch_value.c_str(), &endp, 0);
     if (r_debug_addr != 0 && *endp == '\0') {
-      struct r_debug *bootstrap_r_debug = (struct r_debug *) r_debug_addr;
+      r_debug* bootstrap_r_debug = reinterpret_cast<r_debug*>(r_debug_addr);
       *bootstrap_r_debug = _r_debug;
 
       // Since the main executable (the bootstrap program) does not
@@ -163,7 +163,7 @@ static void CheckRDebug(char *argv0) {
       // normal for the main executable.  So the debugger doesn't
       // know which file it is.  Fill in the actual file name, which
       // came in as our argv[0].
-      struct link_map *l = _r_debug.r_map;
+      link_map* l = _r_debug.r_map;
       if (l->l_name[0] == '\0')
         l->l_name = argv0;
     }
@@ -182,7 +182,7 @@ static size_t CheckReservedAtZero() {
       CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
           kNaClHelperReservedAtZero);
   if (!reserved_at_zero_switch_value.empty()) {
-    char *endp;
+    char* endp;
     prereserved_sandbox_size =
         strtoul(reserved_at_zero_switch_value.c_str(), &endp, 0);
     if (*endp != '\0')
@@ -202,12 +202,12 @@ static const char kAsanDefaultOptionsNaCl[] = "handle_segv=0";
 // before ASan is initialized.
 extern "C"
 __attribute__((no_address_safety_analysis))
-const char *__asan_default_options() {
+const char* __asan_default_options() {
   return kAsanDefaultOptionsNaCl;
 }
 #endif
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
   CommandLine::Init(argc, argv);
   base::AtExitManager exit_manager;
   base::RandUint64();  // acquire /dev/urandom fd before sandbox is raised
