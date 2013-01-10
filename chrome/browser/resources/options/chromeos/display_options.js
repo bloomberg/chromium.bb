dfb16cf32d5ed3b2b1b407d52605074a98bebf9a
hshi@google.com <hshi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 21:08:29 +0000

    Only show start/stop mirroring button when there are multiple displays.
    BUG=169124
    TBR=mukai@chromium.org
    TEST=manually on parrot
    
    Review URL: https://codereview.chromium.org/11786011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176153 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8f0e33c93d1ebea278ebca880e2e91db75f91446

--- chrome/browser/resources/options/chromeos/display_options.js
+++ chrome/browser/resources/options/chromeos/display_options.js
@@ -835,6 +835,7 @@ cr.define('options', function() {
         $('selected-display-data-container').hidden = true;
         $('display-configuration-arrow').hidden = true;
         $('display-options-set-primary').hidden = true;
+        $('display-options-toggle-mirroring').hidden = true;
         return;
       }
 
@@ -873,6 +874,8 @@ cr.define('options', function() {
 
       $('display-options-set-primary').hidden =
           this.displays_[this.focusedIndex_].isPrimary;
+      $('display-options-toggle-mirroring').hidden =
+          (this.displays_.length <= 1);
     },
 
     /**
