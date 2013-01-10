5f90e6b96a70d6f827831320dc9f7b410a5075f6
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 20:00:47 +0000

    Enable shared memory handle sharing in NaCl.
    The destructor for base::SharedMemory closes its handle on destruction.
    To keep a shared memory region alive after using it, we need to
    duplicate the handle. This uses dup(), which is available in native
    client.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11817020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176133 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 52d1b4e358427885ef7b5f0f4507016f65fe9309

--- base/shared_memory_nacl.cc
+++ base/shared_memory_nacl.cc
@@ -142,7 +142,18 @@ void SharedMemory::Unlock() {
 bool SharedMemory::ShareToProcessCommon(ProcessHandle process,
                                         SharedMemoryHandle *new_handle,
                                         bool close_self) {
-  return false;
+  const int new_fd = dup(mapped_file_);
+  if (new_fd < 0) {
+    DPLOG(ERROR) << "dup() failed.";
+    return false;
+  }
+
+  new_handle->fd = new_fd;
+  new_handle->auto_close = true;
+
+  if (close_self)
+    Close();
+  return true;
 }
 
 }  // namespace base
