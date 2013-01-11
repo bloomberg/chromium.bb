6653921ac73e18554827e12512554116bbda0547
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-11 12:08:01 +0000

    Telemetry: add unit tests for memory benchmark.
    BUG=169084
    R=nduca
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/11865008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176318 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4e35a5b8488db0a04fdc2eb40c6fb1b8e0a764b6

---
+++ tools/perf/perf_tools/memory_benchmark_unittest.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from telemetry import multi_page_benchmark_unittest_base
+from perf_tools import memory_benchmark
+
+class MemoryBenchmarkUnitTest(
+  multi_page_benchmark_unittest_base.MultiPageBenchmarkUnitTestBase):
+
+  def testMemoryBenchmark(self):
+    ps = self.CreatePageSetFromFileInUnittestDataDir('page_with_link.html')
+    ps.pages[0].stress_memory = {'action': 'click_element_interaction',
+                                 'text': 'Click me'}
+
+    benchmark = memory_benchmark.MemoryBenchmark()
+    all_results = self.RunBenchmark(benchmark, ps)
+
+    self.assertEqual(0, len(all_results.page_failures))
+    self.assertEqual(1, len(all_results.page_results))
+
+    results0 = all_results.page_results[0]
+    expected_measurements = ['V8_MemoryExternalFragmentationTotal',
+                             'V8_MemoryHeapSampleTotalCommitted',
+                             'V8_MemoryHeapSampleTotalUsed']
+
+    self.assertTrue(all(
+        [m in results0.measurement_names for m in expected_measurements]))
