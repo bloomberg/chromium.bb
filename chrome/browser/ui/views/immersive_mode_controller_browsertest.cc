965d7622ac5360c1018e08fd948161b0cd14c2d6
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-11 17:56:55 +0000

    Disable ImmersiveModeControllerTest.ImmersiveShelf on win aura because it's failing all the time there on try bots.
    BUG=169496
    TBR=jamescook
    Review URL: https://codereview.chromium.org/11863014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176382 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 127d3b3e05474d944d4a5575c8f6773fdeba0a89

--- chrome/browser/ui/views/immersive_mode_controller_browsertest.cc
+++ chrome/browser/ui/views/immersive_mode_controller_browsertest.cc
@@ -182,8 +182,16 @@ IN_PROC_BROWSER_TEST_F(ImmersiveModeControllerTest, MAYBE_ImmersiveMode) {
 }
 
 #if defined(USE_ASH)
+
+// Flakes on win aura trybots: http://crbug.com/169496
+#if defined(OS_WIN)
+#define MAYBE_ImmersiveShelf DISABLED_ImmersiveShelf
+#else
+#define MAYBE_ImmersiveShelf ImmersiveShelf
+#endif
+
 // Test shelf auto-hide toggling behavior.
-IN_PROC_BROWSER_TEST_F(ImmersiveModeControllerTest, ImmersiveShelf) {
+IN_PROC_BROWSER_TEST_F(ImmersiveModeControllerTest, MAYBE_ImmersiveShelf) {
   ui::LayerAnimator::set_disable_animations_for_test(true);
 
   BrowserView* browser_view = static_cast<BrowserView*>(browser()->window());
