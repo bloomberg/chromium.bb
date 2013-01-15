5301dde284cc1fe9489512ec965f90b80f900922
ericu@chromium.org <ericu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-15 19:37:29 +0000

    See https://code.google.com/p/chromium/issues/detail?id=170089 Revert 176846 > Explicitly initialize the IDB Factory after resetting webkit_platform_support, because upstream WebKit will soon require all embedders to do this. > > Review URL: https://chromiumcodereview.appspot.com/11878023
    TBR=pilgrim@chromium.org
    Review URL: https://codereview.chromium.org/11916007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176954 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 018c9908c69c8e5ab04a0f7878da276b27f6fc2d

--- webkit/support/webkit_support.cc
+++ webkit/support/webkit_support.cc
@@ -38,7 +38,6 @@
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebURLError.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebCache.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFileSystemCallbacks.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebIDBFactory.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebKit.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPluginParams.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebStorageNamespace.h"
@@ -171,8 +170,6 @@ class TestEnvironment {
     webkit_platform_support_.reset(
         new TestWebKitPlatformSupport(unit_test_mode,
                                       shadow_platform_delegate));
-    WebKit::setIDBFactory(
-        webkit_platform_support_.get()->idbFactory());
 
 #if defined(OS_ANDROID)
     // Make sure we have enough decoding resources for layout tests.
