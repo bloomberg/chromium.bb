bloomberg/chromium.bb

commit e6e774a29649174972442454870974badebc7ce0

NRWT lies about the number of tests that it's skipping when used with --repeat-each https://bugs.webkit.org/show_bug.cgi?id=95789
Reviewed by Ojan Vafai.

Fix miscalculation in log message.

* Scripts/webkitpy/layout_tests/views/printing.py:
(Printer.print_found):


git-svn-id: svn://svn.chromium.org/blink/trunk@127688 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: 204a18ee08092301119c4024be8b3d93c94de627

third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/views/printing.py
@@ -109,10 +109,11 @@ class Printer(object):
         self._print_default('')
 
     def print_found(self, num_all_test_files, num_to_run, repeat_each, iterations):
-        found_str = 'Found %s; running %d' % (grammar.pluralize('test', num_all_test_files), num_to_run)
+        num_unique_tests = num_to_run / (repeat_each * iterations)
+        found_str = 'Found %s; running %d' % (grammar.pluralize('test', num_all_test_files), num_unique_tests)
         if repeat_each * iterations > 1:
             found_str += ' (%d times each: --repeat-each=%d --iterations=%d)' % (repeat_each * iterations, repeat_each, iterations)
-        found_str += ', skipping %d' % (num_all_test_files - num_to_run)
+        found_str += ', skipping %d' % (num_all_test_files - num_unique_tests)
         self._print_default(found_str + '.')
 
     def print_expected(self, result_summary, tests_with_result_type_callback):

commit 0b5bf6a0f5ff93a45fcb0d8a358e29c81cad261c

NRWT still confused about test count with --repeat-each and --iterations https://bugs.webkit.org/show_bug.cgi?id=106562
Patch by Jussi Kukkonen <jussi.kukkonen@intel.com> on 2013-01-16
Reviewed by Dirk Pranke.

Fix the printed unique test count and skipped count. This
seems to have broken in r127688 ("NRWT lies about the number
of tests that it's skipping when used with --repeat-each").

Also add a test for print_found()

* Scripts/webkitpy/layout_tests/views/printing.py:
(Printer.print_found):
* Scripts/webkitpy/layout_tests/views/printing_unittest.py:
(Testprinter.test_details):
(Testprinter):
(Testprinter.test_found):

git-svn-id: svn://svn.chromium.org/blink/trunk@139841 bbb929c8-8fbe-4397-9dbb-9b2b20218538
Former-commit-id: f3e65e3e27c3c8bf2127b8bc717e206d7a8c1ab3

third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/views/printing.py
@@ -98,12 +98,12 @@ class Printer(object):
         self._print_default('')
 
     def print_found(self, num_all_test_files, num_to_run, repeat_each, iterations):
-        num_unique_tests = num_to_run / (repeat_each * iterations)
-        found_str = 'Found %s; running %d' % (grammar.pluralize('test', num_all_test_files), num_unique_tests)
+        found_str = 'Found %s; running %d' % (grammar.pluralize('test', num_all_test_files), num_to_run)
         if repeat_each * iterations > 1:
             found_str += ' (%d times each: --repeat-each=%d --iterations=%d)' % (repeat_each * iterations, repeat_each, iterations)
-        found_str += ', skipping %d' % (num_all_test_files - num_unique_tests)
+        found_str += ', skipping %d' % (num_all_test_files - num_to_run)
         self._print_default(found_str + '.')
+        print (found_str)
 
     def print_expected(self, run_results, tests_with_result_type_callback):
         self._print_expected_results_of_type(run_results, test_expectations.PASS, "passes", tests_with_result_type_callback)

third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/views/printing_unittest.py
@@ -175,3 +175,13 @@ class  Testprinter(unittest.TestCase):
         printer.print_started_test('passes/image.html')
         printer.print_finished_test(result, expected=False, exp_str='', got_str='')
         self.assertNotEmpty(err)
+
+    def test_print_found(self):
+        printer, err = self.get_printer()
+
+        printer.print_found(100, 10, 1, 1)
+        self.assertWritten(err, ["Found 100 tests; running 10, skipping 90.\n"])
+
+        self.reset(err)
+        printer.print_found(100, 10, 2, 3)
+        self.assertWritten(err, ["Found 100 tests; running 10 (6 times each: --repeat-each=2 --iterations=3), skipping 90.\n"])