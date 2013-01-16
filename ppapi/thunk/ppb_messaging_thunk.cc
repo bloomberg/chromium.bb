9c44e9dc8d7f2ca3fb6df4476ab99979e44545b1
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 20:03:25 +0000

    IDL: Autogenerate thunk .cc file for PPB_Messaging.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11886024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177201 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 64462f6f2229d94957e34e40829c4ddb2f544891

--- ppapi/thunk/ppb_messaging_thunk.cc
+++ ppapi/thunk/ppb_messaging_thunk.cc
@@ -2,30 +2,35 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// From ppb_messaging.idl modified Mon Jan 14 14:09:38 2013.
+
+#include "ppapi/c/pp_errors.h"
 #include "ppapi/c/ppb_messaging.h"
+#include "ppapi/shared_impl/tracked_callback.h"
 #include "ppapi/thunk/enter.h"
-#include "ppapi/thunk/thunk.h"
 #include "ppapi/thunk/ppb_instance_api.h"
+#include "ppapi/thunk/resource_creation_api.h"
+#include "ppapi/thunk/thunk.h"
 
 namespace ppapi {
 namespace thunk {
 
 namespace {
 
-void PostMessage(PP_Instance instance, PP_Var message) {
+void PostMessage(PP_Instance instance, struct PP_Var message) {
   EnterInstance enter(instance);
   if (enter.succeeded())
     enter.functions()->PostMessage(instance, message);
 }
 
-const PPB_Messaging g_ppb_messaging_thunk = {
+const PPB_Messaging_1_0 g_ppb_messaging_thunk_1_0 = {
   &PostMessage
 };
 
 }  // namespace
 
 const PPB_Messaging_1_0* GetPPB_Messaging_1_0_Thunk() {
-  return &g_ppb_messaging_thunk;
+  return &g_ppb_messaging_thunk_1_0;
 }
 
 }  // namespace thunk
