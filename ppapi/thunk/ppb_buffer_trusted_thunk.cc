1e3025396fdd74f62aa36ece43f8a3f49ac2d726
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 17:20:33 +0000

    IDL: Autogenerate thunk .cc file for PPB_BufferTrusted.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11875023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177160 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e921967a10108d9751709ff4f35abd67b49103f8

--- ppapi/thunk/ppb_buffer_trusted_thunk.cc
+++ ppapi/thunk/ppb_buffer_trusted_thunk.cc
@@ -2,32 +2,37 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// From trusted/ppb_buffer_trusted.idl modified Mon Jan 14 14:31:34 2013.
+
 #include "ppapi/c/pp_errors.h"
-#include "ppapi/thunk/thunk.h"
+#include "ppapi/c/trusted/ppb_buffer_trusted.h"
+#include "ppapi/shared_impl/tracked_callback.h"
 #include "ppapi/thunk/enter.h"
 #include "ppapi/thunk/ppb_buffer_trusted_api.h"
+#include "ppapi/thunk/ppb_instance_api.h"
 #include "ppapi/thunk/resource_creation_api.h"
+#include "ppapi/thunk/thunk.h"
 
 namespace ppapi {
 namespace thunk {
 
 namespace {
 
-int32_t GetSharedMemory(PP_Resource buffer_id, int* shm_handle) {
-  EnterResource<PPB_BufferTrusted_API> enter(buffer_id, true);
+int32_t GetSharedMemory(PP_Resource buffer, int* handle) {
+  EnterResource<PPB_BufferTrusted_API> enter(buffer, true);
   if (enter.failed())
     return enter.retval();
-  return enter.object()->GetSharedMemory(shm_handle);
+  return enter.object()->GetSharedMemory(handle);
 }
 
-const PPB_BufferTrusted g_ppb_buffer_trusted_thunk = {
-  &GetSharedMemory,
+const PPB_BufferTrusted_0_1 g_ppb_buffertrusted_thunk_0_1 = {
+  &GetSharedMemory
 };
 
 }  // namespace
 
 const PPB_BufferTrusted_0_1* GetPPB_BufferTrusted_0_1_Thunk() {
-  return &g_ppb_buffer_trusted_thunk;
+  return &g_ppb_buffertrusted_thunk_0_1;
 }
 
 }  // namespace thunk
