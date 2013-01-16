26aae8200a3764ba2b6dd86d7cf57896460271f7
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 11:40:07 +0000

    Telemetry: adds ImageDecodingBenchmarkUnitTest.
    Adds a quick unit test for image_decoding_benchmark.ImageDecoding.
    
    BUG=169079
    TEST=tools/perf/run_tests ImageDecodingBenchmarkUnitTest
    
    
    Review URL: https://chromiumcodereview.appspot.com/11881022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177131 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f42b1d3155df1a7928f7ece1399e99e1da22c488

---
+++ tools/perf/perf_tools/image_decoding_benchmark_unittest.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry import multi_page_benchmark_unittest_base
+from perf_tools import image_decoding_benchmark
+
+
+class ImageDecodingBenchmarkUnitTest(
+  multi_page_benchmark_unittest_base.MultiPageBenchmarkUnitTestBase):
+
+  def testImageDecodingMeasurement(self):
+    ps = self.CreatePageSetFromFileInUnittestDataDir('image_decoding.html')
+
+    benchmark = image_decoding_benchmark.ImageDecoding()
+    all_results = self.RunBenchmark(benchmark, ps)
+
+    self.assertEqual(0, len(all_results.page_failures))
+    self.assertEqual(1, len(all_results.page_results))
+
+    results0 = all_results.page_results[0]
+    self.assertTrue('ImageDecoding_avg' in results0)
+    self.assertGreater(results0['ImageDecoding_avg'], 0)
