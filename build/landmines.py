1d401bcf86e96655d90aa11e3f24445901bcac09
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 23:50:02 +0000

    Add help to landmines.py if it errors out with unexpected builder
    R=iannucci@chromium.org
    
    Review URL: https://codereview.chromium.org/12025005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177534 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 68d8069189de214a44a59095bd8f46e51217921b

--- build/landmines.py
+++ build/landmines.py
@@ -168,7 +168,7 @@ def get_target_build_dir(build_tool, target, is_iphone=False):
   elif build_tool == 'scons':
     ret = os.path.join(SRC_DIR, 'sconsbuild', target)
   else:
-    raise NotImplementedError()
+    raise NotImplementedError('Unexpected GYP_GENERATORS (%s)' % build_tool)
   return os.path.abspath(ret)
 
 
