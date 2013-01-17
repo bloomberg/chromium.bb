078490d16b838c67a7cf35784a610e49610f7a0f
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 21:05:51 +0000

    Fix resource.pak lookup for unit_test suite.
    It shouldn't be manually loading resources.pak. Instead it should be using chrome::FILE_RESOURCES_PACK
    which gets correctly redirected to the right place
    (see chrome_paths.cc where chrome::FILE_RESOURCES_PACK uses ui::DIR_RESOURCE_PAKS_ANDROID
    instead of just DIR_MODULE on android).
    
    BUG=170702
    
    Review URL: https://codereview.chromium.org/12006002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177477 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e20ca736e46b2f40d18799ca5a03577637d5e69a

--- chrome/test/base/chrome_test_suite.cc
+++ chrome/test/base/chrome_test_suite.cc
@@ -233,9 +233,7 @@ void ChromeTestSuite::Initialize() {
   // output, it'll pass regardless of the system language.
   ResourceBundle::InitSharedInstanceWithLocale("en-US", NULL);
   FilePath resources_pack_path;
-  PathService::Get(base::DIR_MODULE, &resources_pack_path);
-  resources_pack_path =
-      resources_pack_path.Append(FILE_PATH_LITERAL("resources.pak"));
+  PathService::Get(chrome::FILE_RESOURCES_PACK, &resources_pack_path);
   ResourceBundle::GetSharedInstance().AddDataPackFromPath(
       resources_pack_path, ui::SCALE_FACTOR_NONE);
 
