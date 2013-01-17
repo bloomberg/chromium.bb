068b455367f9db80707de5e7b8ece47c59995964
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 23:31:42 +0000

    Disable impl thread event handling on Mac
    This allows threaded compositing to be enabled without regressing
    functionality on the Mac (though also without adding most of its
    benefits).
    
    BUG=138003
    
    
    Review URL: https://chromiumcodereview.appspot.com/11988002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177527 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fa18ab77bacb3829a554ab67ae813c94f2989cbf

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2537,12 +2537,14 @@ bool RenderViewImpl::isPointerLocked() {
 }
 
 void RenderViewImpl::didActivateCompositor(int input_handler_identifier) {
+#if !defined(OS_MACOSX) // many events are unhandled - http://crbug.com/138003
 #if !defined(OS_WIN)  // http://crbug.com/160122
   CompositorThread* compositor_thread =
       RenderThreadImpl::current()->compositor_thread();
   if (compositor_thread)
     compositor_thread->AddInputHandler(
         routing_id_, input_handler_identifier, AsWeakPtr());
+#endif
 #endif
 
   RenderWidget::didActivateCompositor(input_handler_identifier);
