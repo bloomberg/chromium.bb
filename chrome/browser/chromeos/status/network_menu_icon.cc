5cf87bf81faef15518c40723cff99471062858d3
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 21:55:01 +0000

    Add 'dark' lte icons. Add 'dark' versions for the LTE/HSPA/GPRS icons so that we show them correctly on the network list.
    R=stevenjb@chromium.org
    BUG=169969
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=177323
    
    Review URL: https://codereview.chromium.org/11975029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177502 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 62f5d129ee3d4deff2784bba85d2bbb4c94ba4c4

--- chrome/browser/chromeos/status/network_menu_icon.cc
+++ chrome/browser/chromeos/status/network_menu_icon.cc
@@ -91,7 +91,9 @@ const gfx::ImageSkia* BadgeForNetworkTechnology(
       id = IDR_AURA_UBER_TRAY_NETWORK_1X;
       break;
     case NETWORK_TECHNOLOGY_GPRS:
-      id = IDR_AURA_UBER_TRAY_NETWORK_GPRS;
+      id = (color == NetworkMenuIcon::COLOR_DARK) ?
+          IDR_AURA_UBER_TRAY_NETWORK_GPRS_DARK :
+          IDR_AURA_UBER_TRAY_NETWORK_GPRS_LIGHT;
       break;
     case NETWORK_TECHNOLOGY_EDGE:
       id = (color == NetworkMenuIcon::COLOR_DARK) ?
@@ -99,24 +101,34 @@ const gfx::ImageSkia* BadgeForNetworkTechnology(
           IDR_AURA_UBER_TRAY_NETWORK_EDGE_LIGHT;
       break;
     case NETWORK_TECHNOLOGY_UMTS:
-      id =  (color == NetworkMenuIcon::COLOR_DARK) ?
+      id = (color == NetworkMenuIcon::COLOR_DARK) ?
           IDR_AURA_UBER_TRAY_NETWORK_3G_DARK :
           IDR_AURA_UBER_TRAY_NETWORK_3G_LIGHT;
       break;
     case NETWORK_TECHNOLOGY_HSPA:
-      id = IDR_AURA_UBER_TRAY_NETWORK_HSPA;
+      id = (color == NetworkMenuIcon::COLOR_DARK) ?
+          IDR_AURA_UBER_TRAY_NETWORK_HSPA_DARK :
+          IDR_AURA_UBER_TRAY_NETWORK_HSPA_LIGHT;
       break;
     case NETWORK_TECHNOLOGY_HSPA_PLUS:
-      id = IDR_AURA_UBER_TRAY_NETWORK_HSPA_PLUS;
+      id = (color == NetworkMenuIcon::COLOR_DARK) ?
+          IDR_AURA_UBER_TRAY_NETWORK_HSPA_PLUS_DARK :
+          IDR_AURA_UBER_TRAY_NETWORK_HSPA_PLUS_LIGHT;
       break;
     case NETWORK_TECHNOLOGY_LTE:
-      id = IDR_AURA_UBER_TRAY_NETWORK_LTE;
+      id = (color == NetworkMenuIcon::COLOR_DARK) ?
+          IDR_AURA_UBER_TRAY_NETWORK_LTE_DARK :
+          IDR_AURA_UBER_TRAY_NETWORK_LTE_LIGHT;
       break;
     case NETWORK_TECHNOLOGY_LTE_ADVANCED:
-      id = IDR_AURA_UBER_TRAY_NETWORK_LTE_ADVANCED;
+      id = (color == NetworkMenuIcon::COLOR_DARK) ?
+          IDR_AURA_UBER_TRAY_NETWORK_LTE_ADVANCED_DARK :
+          IDR_AURA_UBER_TRAY_NETWORK_LTE_ADVANCED_LIGHT;
       break;
     case NETWORK_TECHNOLOGY_GSM:
-      id = IDR_AURA_UBER_TRAY_NETWORK_GPRS;
+      id = (color == NetworkMenuIcon::COLOR_DARK) ?
+          IDR_AURA_UBER_TRAY_NETWORK_GPRS_DARK :
+          IDR_AURA_UBER_TRAY_NETWORK_GPRS_LIGHT;
       break;
     case NETWORK_TECHNOLOGY_UNKNOWN:
       break;
