7114e7545f21bd85f1048771e982aff5306a9e6b
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 19:50:06 +0000

    Move the jstemplate resources back to common_resources.grd to fix test issues.
    BUG=169170
    Review URL: https://codereview.chromium.org/12001003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177458 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3481985be0789d7bd5cff1933fe92c94a3874f56

--- chrome/browser/ui/webui/chrome_web_ui_data_source_unittest.cc
+++ chrome/browser/ui/webui/chrome_web_ui_data_source_unittest.cc
@@ -6,8 +6,8 @@
 #include "base/memory/ref_counted_memory.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_data_source.h"
+#include "grit/common_resources.h"
 #include "grit/generated_resources.h"
-#include "grit/webui_resources.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 class MockChromeWebUIDataSource : public ChromeWebUIDataSource {
@@ -76,14 +76,7 @@ TEST_F(ChromeWebUIDataSourceTest, SomeStrings) {
   EXPECT_NE(result.find("\"button\":\"OK\""), std::string::npos);
 }
 
-// TODO(jam): http://crbug.com/170702
-#if !defined(OS_ANDROID)
-#define MAYBE_DefaultResource DISABLED_DefaultResource
-#else
-#define MAYBE_DefaultResource DefaultResource
-#endif
-
-TEST_F(ChromeWebUIDataSourceTest, MAYBE_DefaultResource) {
+TEST_F(ChromeWebUIDataSourceTest, DefaultResource) {
   source()->set_default_resource(IDR_WEBUI_I18N_PROCESS_JS);
   source()->StartDataRequest("foobar" );
   std::string result(
@@ -98,8 +91,8 @@ TEST_F(ChromeWebUIDataSourceTest, MAYBE_DefaultResource) {
 }
 
 TEST_F(ChromeWebUIDataSourceTest, NamedResource) {
-  source()->set_default_resource(IDR_WEBUI_I18N_PROCESS_JS);
-  source()->add_resource_path("foobar", IDR_WEBUI_I18N_TEMPLATE_JS);
+  source()->set_default_resource(IDR_I18N_PROCESS_JS);
+  source()->add_resource_path("foobar", IDR_I18N_TEMPLATE_JS);
   source()->StartDataRequest("foobar");
   std::string result(
       reinterpret_cast<const char*>(source()->result_data_->front()),
