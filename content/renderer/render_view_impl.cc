201c20238b11646aeb862ade3b0a3fde208001aa
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 09:16:49 +0000

    Disable compositor thread input handling on windows
    BUG=160122
    
    
    Review URL: https://chromiumcodereview.appspot.com/11946019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177369 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 70cff275010b33dbab628f837d76364359065b79

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2537,11 +2537,13 @@ bool RenderViewImpl::isPointerLocked() {
 }
 
 void RenderViewImpl::didActivateCompositor(int input_handler_identifier) {
+#if !defined(OS_WIN)  // http://crbug.com/160122
   CompositorThread* compositor_thread =
       RenderThreadImpl::current()->compositor_thread();
   if (compositor_thread)
     compositor_thread->AddInputHandler(
         routing_id_, input_handler_identifier, AsWeakPtr());
+#endif
 
   RenderWidget::didActivateCompositor(input_handler_identifier);
 
