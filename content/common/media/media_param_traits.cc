9200ed654489ccd9251638d5d87b87aa3056c5f0
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 00:10:59 +0000

    Introduce IPC validation to the media::VideoCaptureParams type.
    BUG=167199
    
    R=jam@chromium.org,dalecurtis@chromium.org
    Review URL: https://codereview.chromium.org/11888015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177277 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b0ad814b4045d009581dc2b49f525bcd33357952

--- content/common/media/media_param_traits.cc
+++ content/common/media/media_param_traits.cc
@@ -2,13 +2,17 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/common/media/audio_param_traits.h"
+#include "content/common/media/media_param_traits.h"
 
 #include "base/stringprintf.h"
 #include "media/audio/audio_parameters.h"
+#include "media/base/limits.h"
+#include "media/video/capture/video_capture_types.h"
 
 using media::AudioParameters;
 using media::ChannelLayout;
+using media::VideoCaptureParams;
+using media::VideoCaptureSessionId;
 
 namespace IPC {
 
@@ -46,6 +50,44 @@ bool ParamTraits<AudioParameters>::Read(const Message* m,
 void ParamTraits<AudioParameters>::Log(const AudioParameters& p,
                                        std::string* l) {
   l->append(base::StringPrintf("<AudioParameters>"));
+}
+
+void ParamTraits<VideoCaptureParams>::Write(Message* m,
+                                            const VideoCaptureParams& p) {
+  m->WriteInt(p.width);
+  m->WriteInt(p.height);
+  m->WriteInt(p.frame_per_second);
+  m->WriteInt(static_cast<int>(p.session_id));
+}
+
+bool ParamTraits<VideoCaptureParams>::Read(const Message* m,
+                                           PickleIterator* iter,
+                                           VideoCaptureParams* r) {
+  int session_id;
+
+  if (!m->ReadInt(iter, &r->width) ||
+      !m->ReadInt(iter, &r->height) ||
+      !m->ReadInt(iter, &r->frame_per_second) ||
+      !m->ReadInt(iter, &session_id))
+    return false;
+
+  r->session_id = static_cast<VideoCaptureSessionId>(session_id);
+
+  // TODO(wjia): Replace with IsValid() method on VideoCaptureParams.
+  if (r->width <= 0 || r->height <= 0 || r->frame_per_second <= 0 ||
+      r->frame_per_second > media::limits::kMaxFramesPerSecond ||
+      r->width > media::limits::kMaxDimension ||
+      r->height > media::limits::kMaxDimension ||
+      r->width * r->height > media::limits::kMaxCanvas) {
+    return false;
+  }
+
+  return true;
+}
+
+void ParamTraits<VideoCaptureParams>::Log(const VideoCaptureParams& p,
+                                          std::string* l) {
+  l->append(base::StringPrintf("<VideoCaptureParams>"));
 }
 
 }
--- content/common/media/media_param_traits.h
+++ content/common/media/media_param_traits.h
@@ -2,14 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_COMMON_MEDIA_AUDIO_AUDIO_PARAM_TRAITS_H_
-#define CONTENT_COMMON_MEDIA_AUDIO_AUDIO_PARAM_TRAITS_H_
+#ifndef CONTENT_COMMON_MEDIA_MEDIA_PARAM_TRAITS_H_
+#define CONTENT_COMMON_MEDIA_MEDIA_PARAM_TRAITS_H_
 
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_param_traits.h"
 
 namespace media {
 class AudioParameters;
+struct VideoCaptureParams;
 }
 
 namespace IPC {
@@ -22,6 +23,15 @@ struct ParamTraits<media::AudioParameters> {
   static void Log(const param_type& p, std::string* l);
 };
 
+template <>
+struct ParamTraits<media::VideoCaptureParams> {
+  typedef media::VideoCaptureParams param_type;
+  static void Write(Message* m, const param_type& p);
+  static bool Read(const Message* m, PickleIterator* iter, param_type* r);
+  static void Log(const param_type& p, std::string* l);
+};
+
+
 } // namespace IPC
 
-#endif  // CONTENT_COMMON_MEDIA_AUDIO_AUDIO_PARAM_TRAITS_H_
+#endif  // CONTENT_COMMON_MEDIA_MEDIA_PARAM_TRAITS_H_
