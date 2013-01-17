200e7331229244b07a538e3c074a36de47b80be2
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 16:27:27 +0000

    Difficult to move corners fo box when cropping large image in Files.app's photo editor.
    This patch attaches onmousemove event handler to window instead of a container. As a result, the script can receive onmousemove events from outside of the Files.app's window which solves the issue.
    
    TEST=Crop a big image, while resizing the crop window move mouse outside of the window. The crop tool should be still being updated.
    BUG=155705
    
    
    Review URL: https://chromiumcodereview.appspot.com/11973026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177418 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 26ab9204abd815739c7b5731e218c9201c5aa483

--- chrome/browser/resources/file_manager/js/image_editor/image_editor.js
+++ chrome/browser/resources/file_manager/js/image_editor/image_editor.js
@@ -628,16 +628,18 @@ ImageEditor.MouseControl = function(rootContainer, container, buffer) {
     'touchend': this.onTouchEnd,
     'touchcancel': this.onTouchCancel,
     'touchmove': this.onTouchMove,
-
-     'mousedown': this.onMouseDown,
-     'mouseup': this.onMouseUp,
-     'mousemove': this.onMouseMove
+    'mousedown': this.onMouseDown,
+    'mouseup': this.onMouseUp,
   };
 
   for (var eventName in handlers) {
     container.addEventListener(
         eventName, handlers[eventName].bind(this), false);
   }
+
+  // Mouse move handler has to be attached to the window to receive events
+  // from outside of the window. See: http://crbug.com/155705
+  window.addEventListener('mousemove', this.onMouseMove.bind(this), false);
 };
 
 /**
