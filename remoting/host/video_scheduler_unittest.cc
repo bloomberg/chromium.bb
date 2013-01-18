c226e84e53abcee1e7770c6b9682c8b396910558
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-18 23:37:49 +0000

    Mocked VideoFrameCapturer should use the VideoFrameCapturer::Delegate pointer.
    BUG=170626
    
    
    Review URL: https://chromiumcodereview.appspot.com/12027002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177799 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e786cd109052f1a14d2108bebb7eeff87eb07f60

--- remoting/host/video_scheduler_unittest.cc
+++ remoting/host/video_scheduler_unittest.cc
@@ -70,30 +70,17 @@ MockVideoEncoder::~MockVideoEncoder() {}
 
 class VideoSchedulerTest : public testing::Test {
  public:
-  VideoSchedulerTest() {
-  }
+  VideoSchedulerTest();
 
-  virtual void SetUp() OVERRIDE {
-    task_runner_ = new AutoThreadTaskRunner(
-        message_loop_.message_loop_proxy(), run_loop_.QuitClosure());
+  virtual void SetUp() OVERRIDE;
 
-    encoder_ = new MockVideoEncoder();
-  }
-
-  void StartVideoScheduler(scoped_ptr<VideoFrameCapturer> capturer) {
-    scheduler_ = VideoScheduler::Create(
-        task_runner_, // Capture
-        task_runner_, // Encode
-        task_runner_, // Network
-        capturer.Pass(),
-        scoped_ptr<VideoEncoder>(encoder_),
-        &client_stub_,
-        &video_stub_);
-  }
-
-  void GenerateOnCaptureCompleted();
-
+  void StartVideoScheduler(scoped_ptr<VideoFrameCapturer> capturer);
   void StopVideoScheduler();
+
+  // VideoFrameCapturer mocks.
+  void OnCapturerStart(VideoFrameCapturer::Delegate* delegate);
+  void OnCapturerStop();
+  void OnCaptureFrame();
 
  protected:
   MessageLoop message_loop_;
@@ -109,20 +96,59 @@ class VideoSchedulerTest : public testing::Test {
 
   scoped_refptr<CaptureData> data_;
 
+  // Points to the delegate passed to VideoFrameCapturer::Start().
+  VideoFrameCapturer::Delegate* capturer_delegate_;
+
  private:
   DISALLOW_COPY_AND_ASSIGN(VideoSchedulerTest);
 };
 
-void VideoSchedulerTest::GenerateOnCaptureCompleted() {
-  SkRegion update_region(SkIRect::MakeXYWH(0, 0, 10, 10));
-  data_->mutable_dirty_region().op(update_region, SkRegion::kUnion_Op);
+VideoSchedulerTest::VideoSchedulerTest()
+    : encoder_(NULL),
+      capturer_delegate_(NULL) {
+}
 
-  scheduler_->OnCaptureCompleted(data_);
+void VideoSchedulerTest::SetUp() {
+  task_runner_ = new AutoThreadTaskRunner(
+      message_loop_.message_loop_proxy(), run_loop_.QuitClosure());
+
+  encoder_ = new MockVideoEncoder();
+}
+
+void VideoSchedulerTest::StartVideoScheduler(
+    scoped_ptr<VideoFrameCapturer> capturer) {
+  scheduler_ = VideoScheduler::Create(
+      task_runner_, // Capture
+      task_runner_, // Encode
+      task_runner_, // Network
+      capturer.Pass(),
+      scoped_ptr<VideoEncoder>(encoder_),
+      &client_stub_,
+      &video_stub_);
 }
 
 void VideoSchedulerTest::StopVideoScheduler() {
   scheduler_->Stop();
   scheduler_ = NULL;
+}
+
+void VideoSchedulerTest::OnCapturerStart(
+    VideoFrameCapturer::Delegate* delegate) {
+  EXPECT_FALSE(capturer_delegate_);
+  EXPECT_TRUE(delegate);
+
+  capturer_delegate_ = delegate;
+}
+
+void VideoSchedulerTest::OnCapturerStop() {
+  capturer_delegate_ = NULL;
+}
+
+void VideoSchedulerTest::OnCaptureFrame() {
+  SkRegion update_region(SkIRect::MakeXYWH(0, 0, 10, 10));
+  data_->mutable_dirty_region().op(update_region, SkRegion::kUnion_Op);
+
+  capturer_delegate_->OnCaptureCompleted(data_);
 }
 
 // This test mocks capturer, encoder and network layer to simulate one capture
@@ -131,7 +157,9 @@ void VideoSchedulerTest::StopVideoScheduler() {
 // sequence to be executed successfully.
 TEST_F(VideoSchedulerTest, StartAndStop) {
   scoped_ptr<MockVideoFrameCapturer> capturer_(new MockVideoFrameCapturer());
-  Expectation capturer_start = EXPECT_CALL(*capturer_, Start(_));
+  Expectation capturer_start =
+      EXPECT_CALL(*capturer_, Start(_))
+          .WillOnce(Invoke(this, &VideoSchedulerTest::OnCapturerStart));
 
   data_ = new CaptureData(NULL, kWidth * CaptureData::kBytesPerPixel,
                           SkISize::Make(kWidth, kHeight));
@@ -139,8 +167,7 @@ TEST_F(VideoSchedulerTest, StartAndStop) {
   // First the capturer is called.
   Expectation capturer_capture = EXPECT_CALL(*capturer_, CaptureFrame())
       .After(capturer_start)
-      .WillRepeatedly(InvokeWithoutArgs(
-          this, &VideoSchedulerTest::GenerateOnCaptureCompleted));
+      .WillRepeatedly(Invoke(this, &VideoSchedulerTest::OnCaptureFrame));
 
   // Expect the encoder be called.
   EXPECT_CALL(*encoder_, Encode(data_, false, _))
@@ -159,7 +186,8 @@ TEST_F(VideoSchedulerTest, StartAndStop) {
       .RetiresOnSaturation();
 
   EXPECT_CALL(*capturer_, Stop())
-      .After(capturer_capture);
+      .After(capturer_capture)
+      .WillOnce(Invoke(this, &VideoSchedulerTest::OnCapturerStop));
 
   // Start video frame capture.
   StartVideoScheduler(capturer_.PassAs<VideoFrameCapturer>());
