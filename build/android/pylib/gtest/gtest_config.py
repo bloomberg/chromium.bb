00c51318da0d4e9234d47cc89aa0d1e56f90f23e
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-18 01:17:05 +0000

    Add webkit tests to android FYI bots
    After assessing stability we can move these to CQ.
    
    R=yfriedman@chromium.org
    BUG=170787
    
    Review URL: https://codereview.chromium.org/11967035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177564 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3594b0d8935f3625ac4b151c5af33b9196ac47d2

--- build/android/pylib/gtest/gtest_config.py
+++ build/android/pylib/gtest/gtest_config.py
@@ -6,13 +6,16 @@
 
 # Add new suites here before upgrading them to the stable list below.
 EXPERIMENTAL_TEST_SUITES = [
+    'TestWebKitAPI',
     'sandbox_linux_unittests',
+    'webkit_unit_tests',
 ]
 
 # Do not modify this list without approval of an android owner.
 # This list determines which suites are run by default, both for local
 # testing and on android trybots running on commit-queue.
 STABLE_TEST_SUITES = [
+    'android_webview_unittests',
     'base_unittests',
     'cc_unittests',
     'content_unittests',
@@ -25,5 +28,4 @@ STABLE_TEST_SUITES = [
     'ui_unittests',
     'unit_tests',
     'webkit_compositor_bindings_unittests',
-    'android_webview_unittests',
 ]
