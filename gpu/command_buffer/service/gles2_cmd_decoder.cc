6874bf49d2c31f961e1f51eddce62568f2aaa8b7
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-19 04:05:59 +0000

    Make GPU process crash if the GLES2CmdDecode Resize callback changes the current context.
    I want to see how often this happens in practice.
    
    Review URL: https://chromiumcodereview.appspot.com/11929042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177826 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 126cbd0e0a5e5c02322a950fcce85956cfca2015

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -3340,7 +3340,7 @@ error::Error GLES2DecoderImpl::HandleResizeCHROMIUM(
 
   if (!resize_callback_.is_null()) {
     resize_callback_.Run(gfx::Size(width, height));
-    DCHECK(context_->IsCurrent(surface_.get()));
+    CHECK(context_->IsCurrent(surface_.get()));
     if (!context_->IsCurrent(surface_.get())) {
       LOG(ERROR) << "GLES2DecoderImpl: Context lost because context no longer "
                  << "current after resize callback.";
