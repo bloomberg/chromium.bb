f434e47bd37b4d660500612ed0803f418e185dd5
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-22 19:00:31 +0000

    Revert 178037
    It broke android.
    
    > Fix official builder compile error when building aura on windows
    > 
    > The error is in the chrome_frame_common project
    > chrome_frame_automation.cc (833) : error C2440: '=' cannot convert from HWND to gfx::NativeWindow
    > 
    > That is because in aura the gfx::NativeWindow is a aura::Window* instead of HWND
    > 
    > BUG=171018
    > TEST=see bug
    > Review URL: https://codereview.chromium.org/12022038
    
    TBR=cpu@chromium.org
    Review URL: https://codereview.chromium.org/12035027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178043 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8fda621a0b9574a72a60c38a71a77be78d26196f

--- content/common/content_message_generator.cc
+++ content/common/content_message_generator.cc
@@ -14,6 +14,30 @@
 #include "ipc/struct_destructor_macros.h"
 #include "content/common/content_message_generator.h"
 
+#if defined(USE_AURA) && defined(OS_WIN)
+#include "ui/gfx/native_widget_types.h"
+
+namespace IPC {
+// TODO(beng): Figure out why this is needed, fix that issue and remove
+//             this. Brett and I were unable to figure out why, but he
+//             thought this should be harmless.
+template <>
+struct ParamTraits<gfx::PluginWindowHandle> {
+  typedef gfx::PluginWindowHandle param_type;
+  static void Write(Message* m, const param_type& p) {
+    m->WriteUInt32(reinterpret_cast<uint32>(p));
+  }
+  static bool Read(const Message* m, PickleIterator* iter, param_type* r) {
+    DCHECK_EQ(sizeof(param_type), sizeof(uint32));
+    return m->ReadUInt32(iter, reinterpret_cast<uint32*>(r));
+  }
+  static void Log(const param_type& p, std::string* l) {
+    l->append(StringPrintf("0x%X", p));
+  }
+};
+}  // namespace IPC
+#endif
+
 // Generate param traits write methods.
 #include "ipc/param_traits_write_macros.h"
 namespace IPC {
