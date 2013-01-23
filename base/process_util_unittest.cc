72775e421d06202a411ca9591489457f99eb94dd
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-23 21:16:38 +0000

    [Memory Sheriff] Longer timeout for heapchecker test, too.
    Get occasional timeout for this test under heapchecker. Temp
    
    TBR=brettw@chromium.org
    BUG=171544
    
    Review URL: https://chromiumcodereview.appspot.com/12041030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178384 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4198ad3bda8a39f8faaac893586b49634ed3fa76

--- base/process_util_unittest.cc
+++ base/process_util_unittest.cc
@@ -613,7 +613,7 @@ int ProcessUtilTest::CountOpenFDsInChild() {
       HANDLE_EINTR(read(fds[0], &num_open_files, sizeof(num_open_files)));
   CHECK_EQ(bytes_read, static_cast<ssize_t>(sizeof(num_open_files)));
 
-#if defined(THREAD_SANITIZER)
+#if defined(THREAD_SANITIZER) || defined(USE_HEAPCHECKER)
   // Compiler-based ThreadSanitizer makes this test slow.
   CHECK(base::WaitForSingleProcess(handle, base::TimeDelta::FromSeconds(3)));
 #else
