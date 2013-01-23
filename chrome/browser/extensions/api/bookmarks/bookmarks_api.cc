4d734d45a394e2fd962f0fa3ee261f6672602756
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-23 23:10:51 +0000

    extensions/api/bookmarks: Attempt to fix Win Aura build breakage.
    FAILED: C:\b\depot_tools\python_bin\python.exe gyp-win-tool link-wrapper environment.x86 link.exe /nologo /IMPLIB:initial\chrome.dll.lib /DLL /OUT:initial\chrome.dll /PDB:initial\chrome.dll.pdb @initial\chrome.dll.rsp && C:\b\depot_tools\python_bin\python.exe gyp-win-tool manifest-wrapper environment.x86 mt.exe -nologo -manifest ..\..\chrome\app\chrome.dll.manifest obj\chrome\chrome_main_dll.chrome.dll.intermediate.manifest -out:initial\chrome.dll.manifest
    browser_extensions.extension_function_registry.obj : error LNK2005: "protected: virtual __thiscall extensions::BookmarksFunction::~BookmarksFunction(void)" (??1BookmarksFunction@extensions@@MAE@XZ) already defined in browser_extensions.bookmarks_api.obj
    
    browser_extensions.extension_function_registry.obj : error LNK2001: unresolved external symbol "private: virtual void __thiscall extensions::BookmarksFunction::Observe(int,class content::NotificationSource const &,class content::NotificationDetails const &)" (?Observe@BookmarksFunction@extensions@@EAEXHABVNotificationSource@content@@ABVNotificationDetails@4@@Z)
    
    This patch relands r178289 (Convert BookmarksFunction to listen to loaded event through BaseBookmarkModelObserver.)
    
    BUG=144783
    R=sky@chromium.org,asargent@chromium.org
    
    
    
    Review URL: https://chromiumcodereview.appspot.com/12039043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178411 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 22a4ef4489d4aba3e4d2d779940c62f45c538430

--- chrome/browser/extensions/api/bookmarks/bookmarks_api.cc
+++ chrome/browser/extensions/api/bookmarks/bookmarks_api.cc
@@ -87,13 +87,15 @@ FilePath GetDefaultFilepathForBookmarkExport() {
 
 }  // namespace
 
+BookmarksFunction::BookmarksFunction()
+    : bookmark_model_(NULL) {
+}
+
 void BookmarksFunction::Run() {
-  BookmarkModel* model = BookmarkModelFactory::GetForProfile(profile());
-  if (!model->IsLoaded()) {
+  bookmark_model_ = BookmarkModelFactory::GetForProfile(profile());
+  if (!bookmark_model_->IsLoaded()) {
     // Bookmarks are not ready yet.  We'll wait.
-    registrar_.Add(
-        this, chrome::NOTIFICATION_BOOKMARK_MODEL_LOADED,
-        content::NotificationService::AllBrowserContextsAndSources());
+    bookmark_model_->AddObserver(this);
     AddRef();  // Balanced in Observe().
     return;
   }
@@ -108,8 +110,8 @@ void BookmarksFunction::Run() {
   SendResponse(success);
 }
 
-bool BookmarksFunction::GetBookmarkIdAsInt64(
-    const std::string& id_string, int64* id) {
+bool BookmarksFunction::GetBookmarkIdAsInt64(const std::string& id_string,
+                                             int64* id) {
   if (base::StringToInt64(id_string, id))
     return true;
 
@@ -125,17 +127,17 @@ bool BookmarksFunction::EditBookmarksEnabled() {
   return false;
 }
 
-void BookmarksFunction::Observe(int type,
-                                const content::NotificationSource& source,
-                                const content::NotificationDetails& details) {
-  DCHECK(type == chrome::NOTIFICATION_BOOKMARK_MODEL_LOADED);
-  Profile* source_profile = content::Source<Profile>(source).ptr();
-  if (!source_profile || !source_profile->IsSameProfile(profile()))
-    return;
+void BookmarksFunction::BookmarkModelChanged() {
+}
 
-  DCHECK(BookmarkModelFactory::GetForProfile(profile())->IsLoaded());
+void BookmarksFunction::Loaded(BookmarkModel* model, bool ids_reassigned) {
   Run();
   Release();  // Balanced in Run().
+}
+
+void BookmarksFunction::BookmarkModelBeingDeleted(BookmarkModel* model) {
+  bookmark_model_->RemoveObserver(this);
+  bookmark_model_ = NULL;
 }
 
 BookmarkEventRouter::BookmarkEventRouter(BookmarkModel* model) : model_(model) {
