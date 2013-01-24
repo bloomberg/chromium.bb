c5969d9d9b60b4863f3b1152e664f02a3dacaab3
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 00:58:25 +0000

    Remove unused SRPC proxy code from gpu.
    Remove unused SRPC proxy code.
    
    
    BUG=169152
    
    
    Review URL: https://chromiumcodereview.appspot.com/11896068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178449 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6ab53f3f5686b970a96bbb3b9526167c75349cd5

--- gpu/command_buffer/client/ref_counted.h
+++ gpu/command_buffer/client/ref_counted.h
@@ -5,20 +5,9 @@
 #ifndef GPU_COMMAND_BUFFER_CLIENT_REF_COUNTED_H_
 #define GPU_COMMAND_BUFFER_CLIENT_REF_COUNTED_H_
 
-// TODO(bbudge) The NaCl SRPC proxy can't depend on base, so we define our own
-// ref_counted. This causes duplicate class warnings in the NaCl IPC proxy,
-// which uses base. Remove the custom ref_counted class after NaCl has switched
-// proxies.
-#if defined(NACL_PPAPI_SRPC_PROXY)
-#include "native_client/src/include/ref_counted.h"
-namespace gpu {
-using nacl::RefCountedThreadSafe;
-}
-#else
 #include "base/memory/ref_counted.h"
 namespace gpu {
 using base::RefCountedThreadSafe;
 }
-#endif
 
 #endif  // GPU_COMMAND_BUFFER_CLIENT_REF_COUNTED_H_
--- gpu/command_buffer/common/trace_event.h
+++ gpu/command_buffer/common/trace_event.h
@@ -5,28 +5,6 @@
 #ifndef GPU_COMMAND_BUFFER_COMMON_TRACE_EVENT_H_
 #define GPU_COMMAND_BUFFER_COMMON_TRACE_EVENT_H_
 
-#if !defined(NACL_PPAPI_SRPC_PROXY)
-
 #include "base/debug/trace_event.h"
-
-#else
-
-#define TRACE_EVENT0(x0, x1) { }
-#define TRACE_EVENT1(x0, x1, x2, x3) { }
-#define TRACE_EVENT2(x0, x1, x2, x3, x4, x5) { }
-#define TRACE_EVENT_INSTANT0(x0, x1) { }
-#define TRACE_EVENT_INSTANT1(x0, x1, x2, x3) { }
-#define TRACE_EVENT_INSTANT2(x0, x1, x2, x3, x4, x5) { }
-#define TRACE_BEGIN0(x0, x1) { }
-#define TRACE_BEGIN1(x0, x1, x2, x3) { }
-#define TRACE_BEGIN2(x0, x1, x2, x3, x4, x5) { }
-#define TRACE_END0(x0, x1) { }
-#define TRACE_END1(x0, x1, x2, x3) { }
-#define TRACE_END2(x0, x1, x2, x3, x4, x5) { }
-#define TRACE_EVENT_IF_LONGER_THAN0(x0, x1, x2) { }
-#define TRACE_EVENT_IF_LONGER_THAN1(x0, x1, x2, x3, x4) { }
-#define TRACE_EVENT_IF_LONGER_THAN2(x0, x1, x2, x3, x4, x5, x6) { }
-
-#endif  // NACL_PPAPI_SRPC_PROXY
 
 #endif  // GPU_COMMAND_BUFFER_COMMON_TRACE_EVENT_H_
