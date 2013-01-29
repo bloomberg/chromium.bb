b4a80d065a44b28803a63f6e4b071e56eec2c466
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-29 23:38:55 +0000

    [NaCl SDK] Fix off-by-one bug in http mount.
    BUG=172045
    R=sbc@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/12095044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179464 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 791416e841c75a60d7447e908fabb5cd2e54a239

--- native_client_sdk/src/libraries/nacl_mounts/mount_http.cc
+++ native_client_sdk/src/libraries/nacl_mounts/mount_http.cc
@@ -73,7 +73,7 @@ StringMap_t ParseHeaders(const char* headers, int32_t headers_length) {
         }
 
         // Found a non-whitespace, mark this as the start of the value.
-        start = &headers[i + 1];
+        start = &headers[i];
         state = FINDING_VALUE;
         // Fallthrough to start processing value without incrementing i.
 
