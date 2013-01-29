d2dcac76f2dc12c89fff0c6f525dea061f1f42d7
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-29 01:07:51 +0000

    Add MEDIA_EXPORT for MacDesktopConfiguration::GetCurrent() to fix build break after r179218.
    Review URL: https://codereview.chromium.org/12092031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179237 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f4473de40413416e09131f83a2fec11efc494659

--- media/video/capture/screen/mac/desktop_configuration.h
+++ media/video/capture/screen/mac/desktop_configuration.h
@@ -10,17 +10,19 @@
 #include <vector>
 
 #include "base/basictypes.h"
+#include "media/base/media_export.h"
 #include "third_party/skia/include/core/SkPoint.h"
 #include "third_party/skia/include/core/SkRect.h"
 
 namespace media {
 
 // Describes the configuration of a specific display.
-struct MacDisplayConfiguration {
+struct MEDIA_EXPORT MacDisplayConfiguration {
   MacDisplayConfiguration();
 
   // Returns the current configuration of the specified display.
-  static MacDisplayConfiguration ForDisplay(CGDirectDisplayID display_id);
+  MEDIA_EXPORT static MacDisplayConfiguration ForDisplay(
+      CGDirectDisplayID display_id);
 
   // Cocoa identifier for this display.
   CGDirectDisplayID id;
@@ -38,12 +40,12 @@ struct MacDisplayConfiguration {
 typedef std::vector<MacDisplayConfiguration> MacDisplayConfigurations;
 
 // Describes the configuration of the whole desktop.
-struct MacDesktopConfiguration {
+struct MEDIA_EXPORT MacDesktopConfiguration {
   MacDesktopConfiguration();
   ~MacDesktopConfiguration();
 
   // Returns the current configuration of the desktop.
-  static MacDesktopConfiguration GetCurrent();
+  MEDIA_EXPORT static MacDesktopConfiguration GetCurrent();
 
   // Bounds of the desktop in Density-Independent Pixels (DIPs).
   SkIRect bounds;
