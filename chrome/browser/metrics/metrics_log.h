cfb1cf13b68f5cc4b3e5854a0cfa291dfe4d2053
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-29 01:24:42 +0000

    Add NetworkChangeNotifier::ConnectionType to SystemProfile.
    SystemProfile::Network tracks the network environment. The flag
    |is_ambiguous| indicates if there was a change to that data since the
    time the previous log was closed.
    
    BUG=169161
    
    
    Review URL: https://chromiumcodereview.appspot.com/12086008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179240 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 74b299ec651904d35f8a0e3a8bfacb5c3f4b2c66

--- chrome/browser/metrics/metrics_log.h
+++ chrome/browser/metrics/metrics_log.h
@@ -141,6 +141,8 @@ class MetricsLog : public MetricsLogBase {
  private:
   FRIEND_TEST_ALL_PREFIXES(MetricsLogTest, ChromeOSStabilityData);
 
+  class NetworkObserver;
+
   // Writes application stability metrics (as part of the profile log).
   // NOTE: Has the side-effect of clearing those counts.
   void WriteStabilityElement(
@@ -183,6 +185,10 @@ class MetricsLog : public MetricsLogBase {
   // Writes info about the Google Update install that is managing this client.
   // This is a no-op if called on a non-Windows platform.
   void WriteGoogleUpdateProto(const GoogleUpdateMetrics& google_update_metrics);
+
+  // Registers as observer with net::NetworkChangeNotifier and keeps track of
+  // the network environment.
+  scoped_ptr<NetworkObserver> network_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(MetricsLog);
 };
