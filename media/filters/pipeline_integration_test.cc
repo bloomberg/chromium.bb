ab02ac48d6cbd0293ea0cc986cfd338181ed679a
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-31 11:37:22 +0000

    Disable a VP9 media test that fails under Valgrind on Mac and might fail on other bots too
    Also fix a nit in the heapcheck suppressions.
    BUG=173333
    TBR=thakis
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12095079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179860 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 55e01e939c2f7c32b811f4f3eb8d878de58133d3

--- media/filters/pipeline_integration_test.cc
+++ media/filters/pipeline_integration_test.cc
@@ -855,7 +855,8 @@ TEST_F(PipelineIntegrationTest, BasicPlayback_AudioOnly_Opus_WebM) {
 }
 
 // Verify that VP9 video in WebM containers can be played back.
-TEST_F(PipelineIntegrationTest, BasicPlayback_VideoOnly_VP9_WebM) {
+// Disabled since it might crash or corrupt heap, see http://crbug.com/173333
+TEST_F(PipelineIntegrationTest, DISABLED_BasicPlayback_VideoOnly_VP9_WebM) {
   ASSERT_TRUE(Start(GetTestDataFilePath("bear-vp9.webm"),
                     PIPELINE_OK));
   Play();
@@ -864,7 +865,8 @@ TEST_F(PipelineIntegrationTest, BasicPlayback_VideoOnly_VP9_WebM) {
 
 // Verify that VP9 video and Opus audio in the same WebM container can be played
 // back.
-TEST_F(PipelineIntegrationTest, BasicPlayback_VP9_Opus_WebM) {
+// Disabled since it might crash or corrupt heap, see http://crbug.com/173333
+TEST_F(PipelineIntegrationTest, DISABLED_BasicPlayback_VP9_Opus_WebM) {
   ASSERT_TRUE(Start(GetTestDataFilePath("bear-vp9-opus.webm"),
                     PIPELINE_OK));
   Play();
