97f81ccbb2368ffb8815f6808ab898417d2d677f
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-01 05:58:54 +0000

    As chrome on android starts to use deferred image decoding, images decoding may not be captured by InspectorTimelineAgent. In such case, we cannot rely on telemetry to get the decoding time. Disable this test for now while we working on a fix. Since this test is android only, just disable it
    BUG=173184
    
    
    Review URL: https://chromiumcodereview.appspot.com/12100007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180094 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: db556fe2511c54ab336b6f32221761bb0de993b3

--- tools/perf/perf_tools/image_decoding_benchmark_unittest.py
+++ tools/perf/perf_tools/image_decoding_benchmark_unittest.py
@@ -3,21 +3,24 @@
 # found in the LICENSE file.
 
 from telemetry import multi_page_benchmark_unittest_base
-from perf_tools import image_decoding_benchmark
+# from perf_tools import image_decoding_benchmark
 
 
 class ImageDecodingBenchmarkUnitTest(
   multi_page_benchmark_unittest_base.MultiPageBenchmarkUnitTestBase):
 
   def testImageDecodingMeasurement(self):
-    ps = self.CreatePageSetFromFileInUnittestDataDir('image_decoding.html')
+    # TODO(qinmin): uncomment this after we fix the image decoding benchmark
+    # for lazily decoded images
+    return
+    # ps = self.CreatePageSetFromFileInUnittestDataDir('image_decoding.html')
 
-    benchmark = image_decoding_benchmark.ImageDecoding()
-    all_results = self.RunBenchmark(benchmark, ps)
+    # benchmark = image_decoding_benchmark.ImageDecoding()
+    # all_results = self.RunBenchmark(benchmark, ps)
 
-    self.assertEqual(0, len(all_results.page_failures))
-    self.assertEqual(1, len(all_results.page_results))
+    # self.assertEqual(0, len(all_results.page_failures))
+    # self.assertEqual(1, len(all_results.page_results))
 
-    results0 = all_results.page_results[0]
-    self.assertTrue('ImageDecoding_avg' in results0)
-    self.assertTrue(results0['ImageDecoding_avg'] > 0)
+    # results0 = all_results.page_results[0]
+    # self.assertTrue('ImageDecoding_avg' in results0)
+    # self.assertTrue(results0['ImageDecoding_avg'] > 0)
