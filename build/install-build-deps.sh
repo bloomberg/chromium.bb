8c00ccb5a523e222ac1178bfe68de7a82e1d0fcf
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-04 21:02:20 +0000

    Adds metacity to the install-build-deps.sh list so it gets installed on bots.
    BUG=167114
    
    Review URL: https://codereview.chromium.org/12194021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180517 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d84ab4b5cb3921338e5b80711a9de37060521a72

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -75,9 +75,9 @@ dev_list="apache2.2-bin bison curl elfutils fakeroot flex g++ gperf
           libgnome-keyring-dev libgtk2.0-dev libkrb5-dev libnspr4-dev
           libnss3-dev libpam0g-dev libpci-dev libsctp-dev libspeechd-dev
           libsqlite3-dev libssl-dev libudev-dev libwww-perl libxslt1-dev
-          libxss-dev libxt-dev libxtst-dev mesa-common-dev patch perl php5-cgi
-          pkg-config python python-cherrypy3 python-dev python-psutil rpm ruby
-          subversion ttf-dejavu-core ttf-indic-fonts ttf-kochi-gothic
+          libxss-dev libxt-dev libxtst-dev mesa-common-dev metacity patch perl
+          php5-cgi pkg-config python python-cherrypy3 python-dev python-psutil
+          rpm ruby subversion ttf-dejavu-core ttf-indic-fonts ttf-kochi-gothic
           ttf-kochi-mincho ttf-thai-tlwg wdiff git-core
           $chromeos_dev_list"
 
