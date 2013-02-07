10d038c9a5117adf3877bd98605d7a8a211d2a34
vapier@chromium.org <vapier@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-07 00:32:49 +0000

    fix x32 handling of Atomic64
    The x32 logic for the size of Atomic64 handles NaCL, but misses
    the Linux case.  Check the standard __ILP32__ to handle that too.
    
    BUG=chromium-os:36866
    TEST=compiled the code for x86_64 (64bit) & x86_64 (x32)
    
    Review URL: https://chromiumcodereview.appspot.com/12186005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181114 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c163248dfa72284c77323c3ef36777fc2166aef0

--- base/atomicops.h
+++ base/atomicops.h
@@ -47,7 +47,7 @@ typedef int32 Atomic32;
 #ifdef ARCH_CPU_64_BITS
 // We need to be able to go between Atomic64 and AtomicWord implicitly.  This
 // means Atomic64 and AtomicWord should be the same type on 64-bit.
-#if defined(OS_NACL)
+#if defined(__ILP32__) || defined(OS_NACL)
 // NaCl's intptr_t is not actually 64-bits on 64-bit!
 // http://code.google.com/p/nativeclient/issues/detail?id=1162
 typedef int64_t Atomic64;
