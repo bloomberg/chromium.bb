9a6c8f2b5c613ac5be23de59852dd54b5ca64b68
Xiaohan Wang <xhwang@chromium.org>
2013-02-07 12:01:18 -0800

    Use the Netflix helper plugin on Daisy.
    BUG=chrome-os-partner:5255
    TEST=Remote trybot run of daisy-release; cbuildbot_config_unittest.py.
    
    Change-Id: If6d7ad905130d515177af90d9f1da55cb09fa0dd
    Reviewed-on: https://gerrit.chromium.org/gerrit/42880
    Reviewed-by: David Dorwin <ddorwin@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Tested-by: Xiaohan Wang <xhwang@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -1172,7 +1172,7 @@ _arm_release.add_config('arm-tegra2_kaen-release',
 
 _arm_release.add_config('daisy-release',
   boards=['daisy'],
-  useflags=official['useflags'] + ['widevine_cdm'],
+  useflags=official['useflags'] + ['widevine_cdm', 'netflix-helper'],
   hw_tests=['bvt'],
   hw_tests_num=4,
   critical_for_chrome=True,
@@ -1180,7 +1180,7 @@ _arm_release.add_config('daisy-release',
 
 _arm_release.add_config('spring-release',
   boards=['daisy_spring'],
-  useflags=official['useflags'] + ['widevine_cdm'],
+  useflags=official['useflags'] + ['widevine_cdm', 'netflix-helper'],
 )
 
 # Factory and Firmware releases much inherit from these classes.  Modifications
