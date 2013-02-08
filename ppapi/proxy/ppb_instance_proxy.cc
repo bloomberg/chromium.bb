2127e00abc8f4203108fb5e49adb7deac2104d10
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-08 01:10:42 +0000

    Remove DEV permission requirement on PPB_MouseCursor::SetCursor()
    The permission-check was accidentally added in r171921, because the API's proxy implementation is shared with PPB_CursorControl_Dev.
    
    BUG=174792
    
    Review URL: https://chromiumcodereview.appspot.com/12207054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181396 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e65bf5b4dbae7cc753405a0bace3f73c8a6bdec2

--- ppapi/proxy/ppb_instance_proxy.cc
+++ ppapi/proxy/ppb_instance_proxy.cc
@@ -1140,13 +1140,12 @@ void PPB_Instance_Proxy::OnHostMsgDeliverSamples(
     enter.functions()->DeliverSamples(instance, audio_frames, &block_info);
 }
 
-void  PPB_Instance_Proxy::OnHostMsgSetCursor(
+void PPB_Instance_Proxy::OnHostMsgSetCursor(
     PP_Instance instance,
     int32_t type,
     const ppapi::HostResource& custom_image,
     const PP_Point& hot_spot) {
-  if (!dispatcher()->permissions().HasPermission(PERMISSION_DEV))
-    return;
+  // This API serves PPB_CursorControl_Dev and PPB_MouseCursor, so is public.
   EnterInstanceNoLock enter(instance);
   if (enter.succeeded()) {
     enter.functions()->SetCursor(
