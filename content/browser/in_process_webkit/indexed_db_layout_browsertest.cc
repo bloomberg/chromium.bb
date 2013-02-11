ef7f8c5e0756435640bf4981ce57b3c9b1d29ef8
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-11 11:05:04 +0000

    Split IndexedDBLayoutTest.IntVersionTest in two as it times out on Win XP(dbg) a lot
    BUG=none
    R=hans@chromium.org
    
    Review URL: https://codereview.chromium.org/12218090
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181678 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7aa5e0837df7c81a9725aa659e92bd67d83dc823

--- content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
+++ content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
@@ -91,13 +91,17 @@ static const char* kRegressionTests[] = {
   NULL
 };
 
-const char* kIntVersionTests[] = {
+const char* kIntVersionTests1[] = {
   "intversion-abort-in-initial-upgradeneeded.html",
   "intversion-blocked.html",
   "intversion-close-between-events.html",
   "intversion-close-in-oncomplete.html",
   "intversion-close-in-upgradeneeded.html",
   "delete-in-upgradeneeded-close-in-open-success.html",
+  NULL
+};
+
+const char* kIntVersionTests2[] = {
   "delete-in-upgradeneeded-close-in-versionchange.html",
   "intversion-gated-on-delete.html",
   "intversion-long-queue.html",
@@ -131,8 +135,12 @@ IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, TransactionTests) {
   RunLayoutTests(kTransactionTests);
 }
 
-IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, IntVersionTests) {
-  RunLayoutTests(kIntVersionTests);
+IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, IntVersionTests1) {
+  RunLayoutTests(kIntVersionTests1);
+}
+
+IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, IntVersionTests2) {
+  RunLayoutTests(kIntVersionTests2);
 }
 
 IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, RegressionTests) {
