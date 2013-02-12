2b8e1894e8e8d5e8c358eb81cf0310262e1342d4
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 02:07:07 +0000

    Turn on threaded compositing in OSX for continuous builds.
    BUG=172015
    TBR=vangelis
    
    Review URL: https://codereview.chromium.org/12223055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181821 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3b22093c5ea7fd343be8d40f6e229ca7e4eaef8c

--- chrome/browser/gpu/chrome_gpu_util.cc
+++ chrome/browser/gpu/chrome_gpu_util.cc
@@ -156,16 +156,9 @@ void InitializeCompositingFieldTrial() {
   // http://crbug.com/133602 for mac
   // http://crbug.com/140866 for linux
 
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_MACOSX)
     // Enable threaded compositing on Windows.
   threaded_compositing_probability = kDivisor;
-#elif defined(OS_MACOSX)
-  chrome::VersionInfo::Channel channel = chrome::VersionInfo::GetChannel();
-  if (channel == chrome::VersionInfo::CHANNEL_CANARY ||
-      channel == chrome::VersionInfo::CHANNEL_DEV) {
-    // Enable force-compositing-mode on the Mac.
-    force_compositing_mode_probability = kDivisor;
-  }
 #endif
 
   int force_compositing_group = trial->AppendGroup(
