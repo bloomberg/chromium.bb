8090c2ee49c8cb646f7bf70509c8ea57c51c0d2c
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 22:03:42 +0000

    Revert 182017 -broke check_licenses > [cc] Add gl-matrix to cc-frame-viewer 3rd party > > R=vmpstr > > Review URL: https://codereview.chromium.org/12250012
    TBR=nduca@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182031 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3b01e8785dae09c2fae12d475b97c56710a44ae3

--- tools/cc-frame-viewer/third_party/gl-matrix/tasks/support/gl-matrix/version.rb
+++
@@ -1,8 +0,0 @@
-module GLMatrix
-  module Version
-    MAJOR, MINOR, PATCH, REL = *File.read(base_path.join 'VERSION').split(".")
-    STRING = [MAJOR, MINOR, PATCH, REL].compact.join '.'
-  end
-
-  VERSION = Version::STRING
-end
