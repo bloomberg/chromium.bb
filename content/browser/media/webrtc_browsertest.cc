61191b6258b17cd468c64fd35e329328ff9a9323
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 08:39:51 +0000

    Disable WebrtcBrowserTest.CallWithDataAndLaterAddMedia everywhere, also fails on Windows
    BUG=175683
    TBR=perkj@chromium.org
    
    Review URL: https://codereview.chromium.org/12220117
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181897 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1b8ca015a0cfba96022edde9bef6b020d848f648

--- content/browser/media/webrtc_browsertest.cc
+++ content/browser/media/webrtc_browsertest.cc
@@ -114,13 +114,9 @@ IN_PROC_BROWSER_TEST_F(WebrtcBrowserTest, CallWithDataAndMedia) {
 
 // This test will make a PeerConnection-based call and test an unreliable text
 // dataChannel and later add an audio and video track.
-#if defined(OS_LINUX)
-// Flaky on Linux. http://crbug.com/175683
-#define MAYBE_CallWithDataAndLaterAddMedia DISABLED_CallWithDataAndLaterAddMedia
-#else
-#define MAYBE_CallWithDataAndLaterAddMedia CallWithDataAndLaterAddMedia
-#endif
-IN_PROC_BROWSER_TEST_F(WebrtcBrowserTest, MAYBE_CallWithDataAndLaterAddMedia) {
+// Flaky. http://crbug.com/175683
+IN_PROC_BROWSER_TEST_F(WebrtcBrowserTest,
+                       DISABLED_CallWithDataAndLaterAddMedia) {
   GURL url(test_server()->GetURL("files/media/peerconnection-call.html"));
   NavigateToURL(shell(), url);
 
