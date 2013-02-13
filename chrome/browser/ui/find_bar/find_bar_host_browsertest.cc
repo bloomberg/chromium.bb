ff5d3c2b9d036624c7af3e8de7a752077a981a35
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-13 06:40:05 +0000

    find_bar: Convert a bunch of string constants to char array.
    And put them in unnamed namespace.
    
    TEST=browser_tests
    TBR=ben@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12220139
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182146 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3e2b860ca14e18504e98287644b313ed5187a644

--- chrome/browser/ui/find_bar/find_bar_host_browsertest.cc
+++ chrome/browser/ui/find_bar/find_bar_host_browsertest.cc
@@ -40,22 +40,24 @@
 using content::NavigationController;
 using content::WebContents;
 
-const std::string kAnchorPage = "anchor.html";
-const std::string kAnchor = "#chapter2";
-const std::string kFramePage = "frames.html";
-const std::string kFrameData = "framedata_general.html";
-const std::string kUserSelectPage = "user-select.html";
-const std::string kCrashPage = "crash_1341577.html";
-const std::string kTooFewMatchesPage = "bug_1155639.html";
-const std::string kLongTextareaPage = "large_textarea.html";
-const std::string kEndState = "end_state.html";
-const std::string kPrematureEnd = "premature_end.html";
-const std::string kMoveIfOver = "move_if_obscuring.html";
-const std::string kBitstackCrash = "crash_14491.html";
-const std::string kSelectChangesOrdinal = "select_changes_ordinal.html";
-const std::string kStartAfterSelection = "start_after_selection.html";
-const std::string kSimple = "simple.html";
-const std::string kLinkPage = "link.html";
+namespace {
+
+const char kAnchorPage[] = "anchor.html";
+const char kAnchor[] = "#chapter2";
+const char kFramePage[] = "frames.html";
+const char kFrameData[] = "framedata_general.html";
+const char kUserSelectPage[] = "user-select.html";
+const char kCrashPage[] = "crash_1341577.html";
+const char kTooFewMatchesPage[] = "bug_1155639.html";
+const char kLongTextareaPage[] = "large_textarea.html";
+const char kEndState[] = "end_state.html";
+const char kPrematureEnd[] = "premature_end.html";
+const char kMoveIfOver[] = "move_if_obscuring.html";
+const char kBitstackCrash[] = "crash_14491.html";
+const char kSelectChangesOrdinal[] = "select_changes_ordinal.html";
+const char kStartAfterSelection[] = "start_after_selection.html";
+const char kSimple[] = "simple.html";
+const char kLinkPage[] = "link.html";
 
 const bool kBack = false;
 const bool kFwd = true;
@@ -64,8 +66,6 @@ const bool kIgnoreCase = false;
 const bool kCaseSensitive = true;
 
 const int kMoveIterations = 30;
-
-namespace {
 
 void HistoryServiceQueried(int) {
   MessageLoop::current()->Quit();
