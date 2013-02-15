a23366867205fe47abb1ae5432b379a3eaf896a1
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-02-15 23:15:11 +0000

    Performance tests: Extend to measure cycle counts on x86 using RDTSC
    For the faster tests (such as TestNaClSyscall), this should give us
    more reproducible results than measuring the real time.
    
    I've changed the code to indent some of the existing results to make
    the output more readable.
    
    Example output:
    
    TestCatchingFault:
    Measuring real time:
      ...
      1.388 usec (1.38799e-06 sec) per iteration: 0.0893395 sec for 64366 iterations
      mean:   1.489966 usec
      stddev: 0.135989 usec
      relative stddev: 9.13%
    RESULT TestCatchingFault: linux_x86-64_newlib_nonirt= {1.489966, 0.135989} microseconds
    Measuring clock cycles:
      first runs (cycles):    6788 3659 3383 3365 3365 3365 3373 3362 3365 3365 ...
      slowest runs (cycles):  ... 3417 3417 3417 3417 3417 3418 3491 3597 3659 6788
      min:     3359 cycles
      q1:      3409 cycles
      median:  3409 cycles
      q3:      3414 cycles
      max:     6788 cycles
    RESULT TestCatchingFault_CycleCount: linux_x86-64_newlib_nonirt= {3409, 5} cycles
    Apparent clock speed: 2288 MHz
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3280
    TEST=run_performance_test
    
    Review URL: https://codereview.chromium.org/12287010
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@10829 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: eb659690531d5eb5ba308ca50c5f20584b6a6ac9

--- tests/performance/perf_test_runner.cc
+++ tests/performance/perf_test_runner.cc
@@ -10,6 +10,7 @@
 #include <time.h>
 
 #include "native_client/src/include/nacl_assert.h"
+#include "native_client/src/include/nacl_macros.h"
 #include "native_client/tests/performance/perf_test_runner.h"
 
 
@@ -25,7 +26,7 @@ double TimeIterations(PerfTest *test, int iterations) {
       (end_time.tv_sec - start_time.tv_sec
        + (double) (end_time.tv_nsec - start_time.tv_nsec) / 1e9);
   // Output the raw data.
-  printf("%.3f usec (%g sec) per iteration: %g sec for %i iterations\n",
+  printf("  %.3f usec (%g sec) per iteration: %g sec for %i iterations\n",
          total_time / iterations * 1e6,
          total_time / iterations,
          total_time, iterations);
@@ -77,20 +78,97 @@ void TimePerfTest(PerfTest *test, double *mean, double *stddev) {
   *stddev = sqrt(sum_of_squares / sample_count - *mean * *mean);
 }
 
-void RunPerfTest(const char *description_string, const char *test_name,
-                 PerfTest *test) {
+void PerfTestRealTime(const char *description_string, const char *test_name,
+                      PerfTest *test, double *result_mean) {
   double mean;
   double stddev;
-  printf("\n%s:\n", test_name);
+  printf("Measuring real time:\n");
   TimePerfTest(test, &mean, &stddev);
-  printf("mean:   %.6f usec\n", mean * 1e6);
-  printf("stddev: %.6f usec\n", stddev * 1e6);
-  printf("relative stddev: %.2f%%\n", stddev / mean * 100);
+  printf("  mean:   %.6f usec\n", mean * 1e6);
+  printf("  stddev: %.6f usec\n", stddev * 1e6);
+  printf("  relative stddev: %.2f%%\n", stddev / mean * 100);
   // Output the result in a format that Buildbot will recognise in the
   // logs and record, using the Chromium perf testing infrastructure.
   printf("RESULT %s: %s= {%.6f, %.6f} microseconds\n",
          test_name, description_string, mean * 1e6, stddev * 1e6);
+  *result_mean = mean;
+}
 
+#if defined(__i386__) || defined(__x86_64__)
+
+static INLINE uint64_t ReadTimestampCounter() {
+  uint32_t edx;  // Top 32 bits of timestamp
+  uint32_t eax;  // Bottom 32 bits of timestamp
+  // NaCl's x86 validators don't allow rdtscp, so we can't check
+  // whether the thread has been moved to a different core.
+  __asm__ volatile("rdtsc" : "=d"(edx), "=a"(eax));
+  return (((uint64_t) edx) << 32) | eax;
+}
+
+static int CompareUint64(const void *val1, const void *val2) {
+  uint64_t i1 = *(uint64_t *) val1;
+  uint64_t i2 = *(uint64_t *) val2;
+  if (i1 == i2)
+    return 0;
+  return i1 < i2 ? -1 : 1;
+}
+
+void PerfTestCycleCount(const char *description_string, const char *test_name,
+                        PerfTest *test, uint64_t *result_cycles) {
+  printf("Measuring clock cycles:\n");
+  uint64_t times[101];
+  for (size_t i = 0; i < NACL_ARRAY_SIZE(times); i++) {
+    uint64_t start_time = ReadTimestampCounter();
+    test->run();
+    uint64_t end_time = ReadTimestampCounter();
+    times[i] = end_time - start_time;
+  }
+
+  // We expect the first run to be slower because caches won't be
+  // warm.  We print the first and slowest runs so that we can verify
+  // this.
+  printf("  first runs (cycles):   ");
+  for (size_t i = 0; i < 10; i++)
+    printf(" %"PRId64, times[i]);
+  printf(" ...\n");
+
+  qsort(times, NACL_ARRAY_SIZE(times), sizeof(times[0]), CompareUint64);
+
+  printf("  slowest runs (cycles):  ...");
+  for (size_t i = NACL_ARRAY_SIZE(times) - 10; i < NACL_ARRAY_SIZE(times); i++)
+    printf(" %"PRId64, times[i]);
+  printf("\n");
+
+  int count = NACL_ARRAY_SIZE(times) - 1;
+  uint64_t q1 = times[count * 1 / 4];  // First quartile
+  uint64_t q2 = times[count * 1 / 2];  // Median
+  uint64_t q3 = times[count * 3 / 4];  // Third quartile
+  printf("  min:     %"PRId64" cycles\n", times[0]);
+  printf("  q1:      %"PRId64" cycles\n", q1);
+  printf("  median:  %"PRId64" cycles\n", q2);
+  printf("  q3:      %"PRId64" cycles\n", q3);
+  printf("  max:     %"PRId64" cycles\n", times[count]);
+  // The "{...}" RESULT syntax usually means standard deviation but
+  // here we report the interquartile range.
+  printf("RESULT %s_CycleCount: %s= {%"PRId64", %"PRId64"} cycles\n",
+         test_name, description_string, q2, q3 - q1);
+  *result_cycles = q2;
+}
+
+#endif
+
+void RunPerfTest(const char *description_string, const char *test_name,
+                 PerfTest *test) {
+  printf("\n%s:\n", test_name);
+  double mean_time;
+  PerfTestRealTime(description_string, test_name, test, &mean_time);
+#if defined(__i386__) || defined(__x86_64__)
+  uint64_t cycles;
+  PerfTestCycleCount(description_string, test_name, test, &cycles);
+  // The apparent clock speed can be used to sanity-check the results,
+  // e.g. to see whether the CPU is in power-saving mode.
+  printf("Apparent clock speed: %.0f MHz\n", cycles / mean_time / 1e6);
+#endif
   delete test;
 }
 
