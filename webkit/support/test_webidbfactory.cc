9caa9a63e1b170796664ddcab96b2f01562e22bb
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 21:19:28 +0000

    Make TestWebIDBFactory allocate a separate WebIDBFactory per thread.
    This allows the main WebKit thread and web worker threads to each get their own
    WebIDBFactory instance, which mimics the behavior when we let the WebIDBFactory
    be instantiated via the WebKitPlatformSupport::idbFactory() method.
    
    Avoid storing WebString on class that will be reached by multiple threads.
    
    This is a slight variation on the patch attempted here:
    https://chromiumcodereview.appspot.com/12181010
    
    R=alecflett@chromium.org
    
    Review URL: https://codereview.chromium.org/12230054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182828 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 467a1036c61793da05d1c18737876cdd57448251

---
+++ webkit/support/test_webidbfactory.cc
@@ -0,0 +1,67 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "webkit/support/test_webidbfactory.h"
+
+#include "base/logging.h"
+#include "webkit/base/file_path_string_conversions.h"
+#include "webkit/support/webkit_support.h"
+
+TestWebIDBFactory::TestWebIDBFactory() {
+  // Create a new temp directory for Indexed DB storage, specific to this
+  // factory. If this fails, WebKit uses in-memory storage.
+  if (!indexed_db_dir_.CreateUniqueTempDir()) {
+    LOG(WARNING) << "Failed to create a temp dir for Indexed DB, "
+        "using in-memory storage.";
+    DCHECK(indexed_db_dir_.path().empty());
+  }
+}
+
+TestWebIDBFactory::~TestWebIDBFactory() {
+}
+
+void TestWebIDBFactory::getDatabaseNames(
+    WebKit::WebIDBCallbacks* callbacks,
+    const WebKit::WebSecurityOrigin& origin,
+    WebKit::WebFrame* frame,
+    const WebKit::WebString& data_dir) {
+  GetFactory()->getDatabaseNames(callbacks, origin, frame,
+                                 data_dir.isEmpty() ? GetDataDir() : data_dir);
+}
+
+void TestWebIDBFactory::open(
+    const WebKit::WebString& name,
+    long long version,
+    long long transaction_id,
+    WebKit::WebIDBCallbacks* callbacks,
+    WebKit::WebIDBDatabaseCallbacks* database_callbacks,
+    const WebKit::WebSecurityOrigin& origin,
+    WebKit::WebFrame* frame,
+    const WebKit::WebString& data_dir) {
+  GetFactory()->open(name, version, transaction_id, callbacks,
+                     database_callbacks, origin, frame,
+                     data_dir.isEmpty() ? GetDataDir() : data_dir);
+}
+
+void TestWebIDBFactory::deleteDatabase(const WebKit::WebString& name,
+                                       WebKit::WebIDBCallbacks* callbacks,
+                                       const WebKit::WebSecurityOrigin& origin,
+                                       WebKit::WebFrame* frame,
+                                       const WebKit::WebString& data_dir) {
+  GetFactory()->deleteDatabase(name, callbacks, origin, frame,
+                               data_dir.isEmpty() ? GetDataDir() : data_dir);
+}
+
+WebKit::WebIDBFactory* TestWebIDBFactory::GetFactory() {
+  WebKit::WebIDBFactory* factory = factories_.Get();
+  if (!factory) {
+    factory = WebKit::WebIDBFactory::create();
+    factories_.Set(factory);
+  }
+  return factory;
+}
+
+WebKit::WebString TestWebIDBFactory::GetDataDir() const {
+  return webkit_base::FilePathToWebString(indexed_db_dir_.path());
+}
---
+++ webkit/support/test_webidbfactory.h
@@ -0,0 +1,55 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBKIT_SUPPORT_TEST_WEBIDBFACTORY_H_
+#define WEBKIT_SUPPORT_TEST_WEBIDBFACTORY_H_
+
+#include "base/files/scoped_temp_dir.h"
+#include "base/threading/thread_local.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebIDBFactory.h"
+
+// Wrap a WebKit::WebIDBFactory to rewrite the data directory to
+// a scoped temp directory. In multiprocess Chromium this is rewritten
+// to a real profile directory during IPC.
+class TestWebIDBFactory : public WebKit::WebIDBFactory {
+ public:
+  TestWebIDBFactory();
+  virtual ~TestWebIDBFactory();
+
+  // WebIDBFactory methods:
+  virtual void getDatabaseNames(WebKit::WebIDBCallbacks* callbacks,
+                                const WebKit::WebSecurityOrigin& origin,
+                                WebKit::WebFrame* frame,
+                                const WebKit::WebString& data_dir);
+  virtual void open(const WebKit::WebString& name,
+                    long long version,
+                    long long transaction_id,
+                    WebKit::WebIDBCallbacks* callbacks,
+                    WebKit::WebIDBDatabaseCallbacks* database_callbacks,
+                    const WebKit::WebSecurityOrigin& origin,
+                    WebKit::WebFrame* frame,
+                    const WebKit::WebString& data_dir);
+  virtual void deleteDatabase(const WebKit::WebString& name,
+                              WebKit::WebIDBCallbacks* callbacks,
+                              const WebKit::WebSecurityOrigin& origin,
+                              WebKit::WebFrame* frame,
+                              const WebKit::WebString& data_dir);
+
+ private:
+  // Returns the WebIDBFactory implementation to use for the current thread.
+  WebKit::WebIDBFactory* GetFactory();
+
+  // Returns the data directory to use.
+  WebKit::WebString GetDataDir() const;
+
+  // We allocate a separate WebIDBFactory instance per thread since the
+  // implementation is not thread-safe.  We also intentionally leak the
+  // factory instances to avoid shutdown races.  TODO(darin): Can we
+  // avoid leaking these?
+  base::ThreadLocalPointer<WebKit::WebIDBFactory> factories_;
+
+  base::ScopedTempDir indexed_db_dir_;
+};
+
+#endif  // WEBKIT_SUPPORT_TEST_WEBIDBFACTORY_H_
