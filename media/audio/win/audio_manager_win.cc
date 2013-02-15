db7cd1287b53df1c2e0c5e5c4e188ccc79629a12
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 13:12:21 +0000

    Fixes live-audio input on Windows.
    TBR=tommi
    BUG=12271022
    
    Review URL: https://codereview.chromium.org/12252066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182686 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 98be8b1efb2383bf7a15533b6a6b5b894ced8a16

--- media/audio/win/audio_manager_win.cc
+++ media/audio/win/audio_manager_win.cc
@@ -361,6 +361,7 @@ AudioParameters AudioManagerWin::GetPreferredLowLatencyOutputStreamParameters(
   int sample_rate = input_params.sample_rate();
   int bits_per_sample = input_params.bits_per_sample();
   ChannelLayout channel_layout = input_params.channel_layout();
+  int input_channels = input_params.input_channels();
   if (CoreAudioUtil::IsSupported()) {
     sample_rate = GetAudioHardwareSampleRate();
     bits_per_sample = 16;
@@ -369,7 +370,7 @@ AudioParameters AudioManagerWin::GetPreferredLowLatencyOutputStreamParameters(
 
   // TODO(dalecurtis): This should include hardware bits per channel eventually.
   return AudioParameters(
-      AudioParameters::AUDIO_PCM_LOW_LATENCY, channel_layout,
+      AudioParameters::AUDIO_PCM_LOW_LATENCY, channel_layout, input_channels,
       sample_rate, bits_per_sample, GetAudioHardwareBufferSize());
 }
 
