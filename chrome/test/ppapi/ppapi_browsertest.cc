6a8bdac4ffb89fe3edb5ea92172f121425007a39
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-16 18:24:47 +0000

    PPAPI/WS: enable WebSocket_AbortReceiveMessageCall on win_aura
    These tests are disabled because they are slow due to issue 175237.
    Now these tests are expected to run fast enough, so enable them again.
    
    BUG=165657
    TEST=browser_test --gtest_filter='*.WebSocket_AbortReceiveMessageCall' # on win_aura
    TBR=dmichael@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12256057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182988 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 547e4c53ce565712eb40cdbad5301bf53478189c

--- chrome/test/ppapi/ppapi_browsertest.cc
+++ chrome/test/ppapi/ppapi_browsertest.cc
@@ -295,14 +295,6 @@ TEST_PPAPI_NACL_VIA_HTTP(Core)
 #define MAYBE_ImeInputEvent ImeInputEvent
 #endif
 
-#if defined(OS_WIN) && defined(USE_AURA)
-#define MAYBE_WebSocket_AbortReceiveMessageCall \
-        DISABLED_WebSocket_AbortReceiveMessageCall
-#else
-#define MAYBE_WebSocket_AbortReceiveMessageCall \
-        WebSocket_AbortReceiveMessageCall
-#endif
-
 TEST_PPAPI_IN_PROCESS(MAYBE_InputEvent)
 TEST_PPAPI_OUT_OF_PROCESS(MAYBE_InputEvent)
 // TODO(bbudge) Enable when input events are proxied correctly for NaCl.
@@ -751,7 +743,7 @@ TEST_PPAPI_IN_PROCESS_WITH_WS(WebSocket_BufferedAmount)
 TEST_PPAPI_IN_PROCESS_WITH_WS(WebSocket_AbortCallsWithCallback)
 TEST_PPAPI_IN_PROCESS_WITH_WS(WebSocket_AbortSendMessageCall)
 TEST_PPAPI_IN_PROCESS_WITH_WS(WebSocket_AbortCloseCall)
-TEST_PPAPI_IN_PROCESS_WITH_WS(MAYBE_WebSocket_AbortReceiveMessageCall)
+TEST_PPAPI_IN_PROCESS_WITH_WS(WebSocket_AbortReceiveMessageCall)
 TEST_PPAPI_IN_PROCESS_WITH_WS(WebSocket_CcInterfaces)
 TEST_PPAPI_IN_PROCESS(WebSocket_UtilityInvalidConnect)
 TEST_PPAPI_IN_PROCESS(WebSocket_UtilityProtocols)
@@ -779,7 +771,7 @@ TEST_PPAPI_OUT_OF_PROCESS_WITH_WS(WebSocket_BufferedAmount)
 TEST_PPAPI_OUT_OF_PROCESS_WITH_WS(WebSocket_AbortCallsWithCallback)
 TEST_PPAPI_OUT_OF_PROCESS_WITH_WS(WebSocket_AbortSendMessageCall)
 TEST_PPAPI_OUT_OF_PROCESS_WITH_WS(WebSocket_AbortCloseCall)
-TEST_PPAPI_OUT_OF_PROCESS_WITH_WS(MAYBE_WebSocket_AbortReceiveMessageCall)
+TEST_PPAPI_OUT_OF_PROCESS_WITH_WS(WebSocket_AbortReceiveMessageCall)
 TEST_PPAPI_OUT_OF_PROCESS_WITH_WS(WebSocket_CcInterfaces)
 TEST_PPAPI_OUT_OF_PROCESS(WebSocket_UtilityInvalidConnect)
 TEST_PPAPI_OUT_OF_PROCESS(WebSocket_UtilityProtocols)
@@ -807,7 +799,7 @@ TEST_PPAPI_NACL_VIA_HTTP_WITH_WS(WebSocket_BufferedAmount)
 TEST_PPAPI_NACL_VIA_HTTP_WITH_WS(WebSocket_AbortCallsWithCallback)
 TEST_PPAPI_NACL_VIA_HTTP_WITH_WS(WebSocket_AbortSendMessageCall)
 TEST_PPAPI_NACL_VIA_HTTP_WITH_WS(WebSocket_AbortCloseCall)
-TEST_PPAPI_NACL_VIA_HTTP_WITH_WS(MAYBE_WebSocket_AbortReceiveMessageCall)
+TEST_PPAPI_NACL_VIA_HTTP_WITH_WS(WebSocket_AbortReceiveMessageCall)
 TEST_PPAPI_NACL_VIA_HTTP_WITH_WS(WebSocket_CcInterfaces)
 TEST_PPAPI_NACL_VIA_HTTP(WebSocket_UtilityInvalidConnect)
 TEST_PPAPI_NACL_VIA_HTTP(WebSocket_UtilityProtocols)
