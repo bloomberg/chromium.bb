ef73ac9c93066632e8290127545943aff28e83eb
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-18 22:21:32 +0000

    mac content shell drt: Register fonts, like regular DRT does.
    On Linux and Windows, regular DumpRenderTree registers fonts in
    DumpRenderTree/chromium/TestShellWin.cpp / TestShellX11.cpp.
    Mac on the other hand has this code in AfterInitialize() in
    webkit/support/platform_support_mac.mm (called from SetUpTestEnvironment(),
    which is called from DumpRenderTree's main() function).
    
    (It'd be nice to make the mac drt code look like windows and linux do, but
    since drt will be replaced by content shell drt eventually, that's probably
    not worth doing.)
    
    content shell drt must not depend on webkit/, copy the font initialization
    code into the designated setup function used by content shell -- this is also
    where content shell drt does font setup on linux.
    
    Since Content Shell.app uses the same app-in-app design as regular chrome
    (I'm guessing for sandboxing reasons?) and DRT.gyp injects the ttf files into
    its direct dependends (which is content_shell, not content_shell_helper), do
    a little dance to find the right resource directory.
    
    BUG= 111316
    TEST=Run `webkit/tools/layout_tests/run_webkit_tests.sh --driver-name 'Content Shell' --additional-drt-flag=--dump-render-tree fast/text/shadow-translucent-fill.html`, verify there are green boxes, not green "A" characters.
    
    Review URL: https://chromiumcodereview.appspot.com/12288033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183143 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cb05d47fc6cb7fe27b1400ddd0962fe2ade1f0bf

--- content/shell/webkit_test_platform_support_mac.mm
+++ content/shell/webkit_test_platform_support_mac.mm
@@ -2,11 +2,55 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/logging.h"
 #include "content/shell/webkit_test_platform_support.h"
+
+#include <AppKit/AppKit.h>
+#include <Foundation/Foundation.h>
 
 namespace content {
 
 bool WebKitTestPlatformInitialize() {
+
+  // Load font files in the resource folder.
+  static const char* const fontFileNames[] = {
+      "AHEM____.TTF",
+      "WebKitWeightWatcher100.ttf",
+      "WebKitWeightWatcher200.ttf",
+      "WebKitWeightWatcher300.ttf",
+      "WebKitWeightWatcher400.ttf",
+      "WebKitWeightWatcher500.ttf",
+      "WebKitWeightWatcher600.ttf",
+      "WebKitWeightWatcher700.ttf",
+      "WebKitWeightWatcher800.ttf",
+      "WebKitWeightWatcher900.ttf",
+  };
+
+  // mainBundle is Content Shell Helper.app.  Go two levels up to find
+  // Content Shell.app. Due to DumpRenderTree injecting the font files into
+  // its direct dependents, it's not easily possible to put the ttf files into
+  // the helper's resource directory instead of the outer bundle's resource
+  // directory.
+  NSString* bundle = [[NSBundle mainBundle] bundlePath];
+  bundle = [bundle stringByAppendingPathComponent:@"../.."];
+  NSURL* resources_directory = [[NSBundle bundleWithPath:bundle] resourceURL];
+
+  NSMutableArray* font_urls = [NSMutableArray array];
+  for (unsigned i = 0; i < arraysize(fontFileNames); ++i) {
+    NSURL* font_url = [resources_directory
+        URLByAppendingPathComponent:[NSString
+            stringWithUTF8String:fontFileNames[i]]];
+    [font_urls addObject:[font_url absoluteURL]];
+  }
+
+  CFArrayRef errors = 0;
+  if (!CTFontManagerRegisterFontsForURLs((CFArrayRef)font_urls,
+                                         kCTFontManagerScopeProcess,
+                                         &errors)) {
+    DLOG(FATAL) << "Fail to activate fonts.";
+    CFRelease(errors);
+  }
+
   return true;
 }
 
