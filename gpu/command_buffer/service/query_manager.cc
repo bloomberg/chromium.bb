b2cc8e0382dcc42ea673a5011af2be6104f95127
reveman@google.com <reveman@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-18 16:10:48 +0000

    gpu: Clear pending_transfer_queries_ in QueryManager::Destroy.
    Failing to do this can cause query_count_ to be non-zero at the time
    the destructor is called.
    
    BUG=176811
    TBR=gman
    
    Review URL: https://codereview.chromium.org/12301010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183127 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 99bb5410f37928816b9701bf3d35464123c3e481

--- gpu/command_buffer/service/query_manager.cc
+++ gpu/command_buffer/service/query_manager.cc
@@ -320,6 +320,7 @@ QueryManager::~QueryManager() {
 
 void QueryManager::Destroy(bool have_context) {
   pending_queries_.clear();
+  pending_transfer_queries_.clear();
   while (!queries_.empty()) {
     Query* query = queries_.begin()->second;
     query->Destroy(have_context);
