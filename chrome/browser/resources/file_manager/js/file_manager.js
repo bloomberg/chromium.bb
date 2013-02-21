57a298e4634843805cd45bd52883f293cf43231e
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-21 04:33:27 +0000

    Revert 183103
    Splitting this patch into changes-without-string-change and string-change to make this mergable.
    
    > File.app: Escape search string. 
    > 
    > BUG=176620
    > TEST=enter html to searchbox, and confirms the string is escaped in search result.
    > 
    > R=mtomasz@chromium.org
    > TBR=satorux@chromium.org
    > # TBRing for just renaming the name of string resource.
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12295006
    
    TBR=yoshiki@chromium.org
    Review URL: https://codereview.chromium.org/12328031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183739 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: adae1b4005a76128888c2a44385bd136ad23e629

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -2857,11 +2857,8 @@ DialogType.isModal = function(type) {
 
     var reportEmptySearchResults = function() {
       if (this.directoryModel_.getFileList().length === 0) {
-        // The string 'SEARCH_NO_MATCHING_FILES_HTML' may contain HTML tags,
-        // hence we escapes |searchString| here.
-        var html = strf('SEARCH_NO_MATCHING_FILES_HTML',
-                        util.htmlEscape(searchString));
-        noResultsDiv.innerHTML = html;
+        var text = strf('SEARCH_NO_MATCHING_FILES', searchString);
+        noResultsDiv.innerHTML = text;
         noResultsDiv.setAttribute('show', 'true');
       } else {
         noResultsDiv.removeAttribute('show');
