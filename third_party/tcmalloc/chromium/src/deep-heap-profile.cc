2c7f83252cb764c04cf6d79b6a45851d35600f5c
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 00:48:30 +0000

    Change the order of hooked and unhooked memory regions dumped in deep-heap-profile.
    BUG=174304
    
    Review URL: https://chromiumcodereview.appspot.com/12316004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183946 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 774628b3e3c6919199ee7f01e0b0f46b75f7641d

--- third_party/tcmalloc/chromium/src/deep-heap-profile.cc
+++ third_party/tcmalloc/chromium/src/deep-heap-profile.cc
@@ -702,24 +702,6 @@ void DeepHeapProfile::GlobalStats::SnapshotProcMaps(
         } else {
           last_address_of_unhooked =
               mmap_list[mmap_list_index].first_address - 1;
-          if (mmap_dump_buffer) {
-            bool trailing =
-              mmap_list[mmap_list_index].first_address < first_address;
-            bool continued =
-              mmap_list[mmap_list_index].last_address > last_address;
-            mmap_dump_buffer->AppendString(trailing ? " (" : "  ", 0);
-            mmap_dump_buffer->AppendPtr(
-                mmap_list[mmap_list_index].first_address, 0);
-            mmap_dump_buffer->AppendString(trailing ? ")" : " ", 0);
-            mmap_dump_buffer->AppendString("-", 0);
-            mmap_dump_buffer->AppendString(continued ? "(" : " ", 0);
-            mmap_dump_buffer->AppendPtr(
-                mmap_list[mmap_list_index].last_address + 1, 0);
-            mmap_dump_buffer->AppendString(continued ? ")" : " ", 0);
-            mmap_dump_buffer->AppendString(" hooked ", 0);
-            mmap_dump_buffer->AppendString(kMapsRegionTypeDict[type], 0);
-            mmap_dump_buffer->AppendString("\n", 0);
-          }
         }
 
         if (last_address_of_unhooked + 1 > cursor) {
@@ -739,6 +721,27 @@ void DeepHeapProfile::GlobalStats::SnapshotProcMaps(
             mmap_dump_buffer->AppendString("\n", 0);
           }
           cursor = last_address_of_unhooked + 1;
+        }
+
+        if (mmap_list_index < mmap_list_length &&
+            mmap_list[mmap_list_index].first_address <= last_address &&
+            mmap_dump_buffer) {
+          bool trailing =
+            mmap_list[mmap_list_index].first_address < first_address;
+          bool continued =
+            mmap_list[mmap_list_index].last_address > last_address;
+          mmap_dump_buffer->AppendString(trailing ? " (" : "  ", 0);
+          mmap_dump_buffer->AppendPtr(
+              mmap_list[mmap_list_index].first_address, 0);
+          mmap_dump_buffer->AppendString(trailing ? ")" : " ", 0);
+          mmap_dump_buffer->AppendString("-", 0);
+          mmap_dump_buffer->AppendString(continued ? "(" : " ", 0);
+          mmap_dump_buffer->AppendPtr(
+              mmap_list[mmap_list_index].last_address + 1, 0);
+          mmap_dump_buffer->AppendString(continued ? ")" : " ", 0);
+          mmap_dump_buffer->AppendString(" hooked ", 0);
+          mmap_dump_buffer->AppendString(kMapsRegionTypeDict[type], 0);
+          mmap_dump_buffer->AppendString("\n", 0);
         }
       } while (mmap_list_index < mmap_list_length &&
                mmap_list[mmap_list_index].last_address <= last_address);
