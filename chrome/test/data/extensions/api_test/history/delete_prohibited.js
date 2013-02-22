b22f46e0394a42f48b955cd0cca24d5941822f0a
pam@chromium.org <pam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 01:05:49 +0000

    Try a few times to let the initial added URLs show up in the history database.
    An unknown file-locking or asynchronous operation sometimes prevents the two added URLs from showing up immediately in the history database, leading to *more* history results after the (prohibited, no-op) deletion than before it. We'll try a few times, waiting a little in between each (10 tries, 100ms each try, still giving a total of only 1s delay), to see if that eliminates the flakiness.
    
    The test is, of course, fundamentally still flaky on Windows, as it must be if writing to the history database is itself unreliable. If the flakiness continues to show up in practice, the test will have to be disabled on Windows.
    
    BUG=176828
    TEST=HistoryApiTest.DeleteProhibited no longer flaky on Windows (XP, Win7, Vista)
    
    Review URL: https://chromiumcodereview.appspot.com/12319013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183952 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8ca70510eefac18d15b011ccc32b4cdaf3a20693

--- chrome/test/data/extensions/api_test/history/delete_prohibited.js
+++ chrome/test/data/extensions/api_test/history/delete_prohibited.js
@@ -7,33 +7,72 @@
 
 var PROHIBITED_ERR = "Browsing history is not allowed to be deleted.";
 
+// The maximum number of tries querying the history database to make sure that
+// the initial addUrl calls succeeded.
+var MAX_HISTORY_TRIES = 10;
+
 function deleteProhibitedTestVerification() {
   removeItemRemovedListener();
   chrome.test.fail("Delete was prohibited, but succeeded anyway.");
 }
 
-// Order history results by ID for reliable array comparison.
+// Orders history results by ID for reliable array comparison.
 function sortResults(a, b) {
   return (a.id - b.id);
 }
 
-// Run the provided function (which must be curried with arguments if needed)
-// and verify that it both returns an error and does not delete any items from
+// Returns true if both expected URLs are in the result list.
+function verifyHistory(resultList) {
+  var hasGoogle = false;
+  var hasPicasa = false;
+  for (var i = 0; i < resultList.length; i++) {
+    if (resultList[i].url == GOOGLE_URL)
+      hasGoogle = true;
+    else if (resultList[i].url == PICASA_URL)
+      hasPicasa = true;
+  }
+  return (hasGoogle && hasPicasa);
+}
+
+// Runs the provided function (which must be curried with arguments if needed)
+// and verifies that it both returns an error and does not delete any items from
 // history.
 function verifyNoDeletion(testFunction) {
   setItemRemovedListener(deleteProhibitedTestVerification);
 
+  // Add two URLs, wait if necessary to make sure they both show up in the
+  // history results, then run the provided test function. Re-query the history
+  // to make sure the test function had no effect.
   var query = { 'text': '' };
   chrome.history.addUrl({ url: GOOGLE_URL }, pass(function() {
     chrome.history.addUrl({ url: PICASA_URL }, pass(function() {
-      chrome.history.search(query, pass(function(resultsBefore) {
-        testFunction(fail(PROHIBITED_ERR, function() {
-          chrome.history.search(query, pass(function (resultsAfter) {
-            assertEq(resultsBefore.sort(sortResults),
-                     resultsAfter.sort(sortResults));
-            removeItemRemovedListener();
+      // Humans use 1-based counting.
+      var tries = 1;
+      chrome.history.search(query, pass(function lambda(resultsBefore) {
+        if (verifyHistory(resultsBefore)) {
+          // Success: proceed with the test.
+          if (tries > 1) {
+            console.log("Warning: Added URLs took " + tries + " tries to " +
+                        "show up in history. See http://crbug.com/176828.");
+          }
+          testFunction(fail(PROHIBITED_ERR, function() {
+            chrome.history.search(query, pass(function(resultsAfter) {
+              assertEq(resultsBefore.sort(sortResults),
+                      resultsAfter.sort(sortResults));
+              removeItemRemovedListener();
+            }));
           }));
-        }));
+        } else if (tries < MAX_HISTORY_TRIES) {
+          // Results not yet in history: try again. See http://crbug.com/176828.
+          ++tries;
+          waitAFewSeconds(0.1, pass(function() {
+            chrome.history.search(query, pass(lambda));
+          }));
+        } else {
+          // Too many tries: fail.
+          chrome.test.fail("Added URLs never showed up in the history. " +
+                           "See http://crbug.com/176828.");
+        }
       }));
     }));
   }));
