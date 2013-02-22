e2c123e510496b5e0abf2b7f55365b0480cebcd2
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 01:16:26 +0000

    Remove hover animation from app launcher grid.
    This makes the launcher feel sluggish.
    
    BUG=175560
    
    
    Review URL: https://chromiumcodereview.appspot.com/12320031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183956 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 780bbf148e36ff9562385cbc0803ad1bf406d59a

--- ui/app_list/views/app_list_item_view.cc
+++ ui/app_list/views/app_list_item_view.cc
@@ -92,6 +92,8 @@ AppListItemView::AppListItemView(AppsGridView* apps_grid_view,
 
   set_context_menu_controller(this);
   set_request_focus_on_press(false);
+
+  SetAnimationDuration(0);
 }
 
 AppListItemView::~AppListItemView() {
