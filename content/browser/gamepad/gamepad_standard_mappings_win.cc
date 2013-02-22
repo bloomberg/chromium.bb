15cdff7ff665dd1ff0f9331d60f4268724924447
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 00:37:44 +0000

    Gamepad: Windows support for Logitech Precision.
    Adds support for the Logitech Precision DirectInput gamepad
    (P/N 863251-0000) to Windows.
    
    Tested:
    Verified that all buttons work (buttons 10-11 and axes are not present).
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/12314033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183937 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8e4c16de88364ef8c797b740fd630a12c1ad0bf4

--- content/browser/gamepad/gamepad_standard_mappings_win.cc
+++ content/browser/gamepad/gamepad_standard_mappings_win.cc
@@ -15,6 +15,14 @@ float NormalizeDirectInputAxis(long value) {
   return (value * 1.f / 32767.5f) - 1.f;
 }
 
+float AxisNegativeAsButton(long value) {
+  return (value < 32767) ? 1.f : 0.f;
+}
+
+float AxisPositiveAsButton(long value) {
+  return (value > 32767) ? 1.f : 0.f;
+}
+
 void MapperDragonRiseGeneric(
     const WebKit::WebGamepad& input,
     WebKit::WebGamepad* mapped) {
@@ -53,6 +61,23 @@ void MapperLogitechDualAction(
   mapped->axesLength = 4;
 }
 
+void MapperLogitechPrecision(
+    const WebKit::WebGamepad& input,
+    WebKit::WebGamepad* mapped) {
+  *mapped = input;
+  mapped->buttons[0] = input.buttons[1];
+  mapped->buttons[1] = input.buttons[2];
+  mapped->buttons[2] = input.buttons[0];
+  mapped->buttons[kButtonLeftThumbstick] = 0;  // Not present
+  mapped->buttons[kButtonRightThumbstick] = 0;  // Not present
+  mapped->buttons[12] = AxisNegativeAsButton(input.axes[1]);
+  mapped->buttons[13] = AxisPositiveAsButton(input.axes[1]);
+  mapped->buttons[14] = AxisNegativeAsButton(input.axes[0]);
+  mapped->buttons[15] = AxisPositiveAsButton(input.axes[0]);
+  mapped->buttonsLength = 16;
+  mapped->axesLength = 0;
+}
+
 struct MappingData {
   const char* const vendor_id;
   const char* const product_id;
@@ -61,6 +86,7 @@ struct MappingData {
   // http://www.linux-usb.org/usb.ids
   { "0079", "0006", MapperDragonRiseGeneric },  // DragonRise Generic USB
   { "046d", "c216", MapperLogitechDualAction },  // Logitech DualAction
+  { "046d", "c21a", MapperLogitechPrecision },  // Logitech Precision
 };
 
 }  // namespace
