b17537e2c5e3358eff1f878d28242e4f607b33f4
Ander Conselvan de Oliveira <ander.conselvan.de.oliveira@intel.com>
2013-02-22 14:16:18 +0200

    shell: Use weston_compositor_sleep() instead of setting DPMS state
    This should have been changed when the weston_compositor_sleep() entry
    point was added.

--- src/shell.c
+++ src/shell.c
@@ -2154,14 +2154,11 @@ handle_screensaver_sigchld(struct weston_process *proc, int status)
 {
 	struct desktop_shell *shell =
 		container_of(proc, struct desktop_shell, screensaver.process);
-	struct weston_output *output;
 
 	proc->pid = 0;
 
 	if (shell->locked)
-		wl_list_for_each(output, &shell->compositor->output_list, link)
-			if (output->set_dpms)
-				output->set_dpms(output, WESTON_DPMS_STANDBY);
+		weston_compositor_sleep(shell->compositor);
 }
 
 static void
