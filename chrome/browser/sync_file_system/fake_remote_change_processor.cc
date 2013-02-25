c0188a5b889a047cceb295eb1090e3c1ace5faab
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-25 03:31:53 +0000

    [Retry] SyncFS: add FakeRemoteChangeProcessor for testing DriveFileSyncService
    FakeRemoteChangeProcessor records the given remote change and the local directory structure after the change is applied. ApplyRemoteChange() will be implemented in the next CL.
    
    Original review: https://codereview.chromium.org/12319009/
    
    BUG=177162
    TEST=n/a
    TBR=kinuko,tzik
    
    
    Review URL: https://chromiumcodereview.appspot.com/12316092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184369 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1d2d368ae185b8ca1a308608c643afa16db49b5b

---
+++ chrome/browser/sync_file_system/fake_remote_change_processor.cc
@@ -0,0 +1,58 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/sync_file_system/fake_remote_change_processor.h"
+
+#include "base/bind.h"
+#include "base/file_path.h"
+#include "base/location.h"
+#include "base/logging.h"
+#include "base/message_loop_proxy.h"
+#include "webkit/fileapi/file_system_url.h"
+#include "webkit/fileapi/syncable/file_change.h"
+#include "webkit/fileapi/syncable/sync_file_metadata.h"
+
+namespace sync_file_system {
+
+FakeRemoteChangeProcessor::FakeRemoteChangeProcessor() {
+}
+
+FakeRemoteChangeProcessor::~FakeRemoteChangeProcessor() {
+}
+
+void FakeRemoteChangeProcessor::PrepareForProcessRemoteChange(
+    const fileapi::FileSystemURL& url,
+    const std::string& service_name,
+    const PrepareChangeCallback& callback) {
+  base::MessageLoopProxy::current()->PostTask(
+      FROM_HERE,
+      base::Bind(callback, fileapi::SYNC_STATUS_OK,
+                 fileapi::SyncFileMetadata(), FileChangeList()));
+}
+
+void FakeRemoteChangeProcessor::ApplyRemoteChange(
+    const FileChange& change,
+    const base::FilePath& local_path,
+    const fileapi::FileSystemURL& url,
+    const fileapi::SyncStatusCallback& callback) {
+  // TODO(nhiroki): record the given change and the local directory structure
+  // after the change is processed (actually not processed) (crbug.com/177162).
+  NOTIMPLEMENTED();
+}
+
+void FakeRemoteChangeProcessor::ClearLocalChanges(
+    const fileapi::FileSystemURL& url,
+    const base::Closure& completion_callback) {
+  base::MessageLoopProxy::current()->PostTask(FROM_HERE, completion_callback);
+}
+
+void FakeRemoteChangeProcessor::RecordFakeLocalChange(
+    const fileapi::FileSystemURL& url,
+    const FileChange& change,
+    const fileapi::SyncStatusCallback& callback) {
+  base::MessageLoopProxy::current()->PostTask(
+      FROM_HERE, base::Bind(callback, fileapi::SYNC_STATUS_OK));
+}
+
+}  // namespace sync_file_system
