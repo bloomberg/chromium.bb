ff7a3b2fbac465021ceedd423381262c6ce78254
hshi@chromium.org <hshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 08:09:54 +0000

    Re-land 184821 with updated unit tests.
    Fix a minor typo in SnapSizer::GetSnapBounds().
    
    The second operand for the "%" operator should be the array size of usable_width_,
    not the size minus 1.
    
    BUG=178561
    TEST=manually tested
    
    
    Review URL: https://chromiumcodereview.appspot.com/12334113
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184906 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 03d14d0eae08f152a8e74fb180f442a12a4da691

--- ash/accelerators/accelerator_controller_unittest.cc
+++ ash/accelerators/accelerator_controller_unittest.cc
@@ -437,6 +437,8 @@ TEST_F(AcceleratorControllerTest, WindowSnap) {
     gfx::Rect snap_left = window->bounds();
     GetController()->PerformAction(WINDOW_SNAP_LEFT, dummy);
     EXPECT_NE(window->bounds().ToString(), snap_left.ToString());
+    GetController()->PerformAction(WINDOW_SNAP_LEFT, dummy);
+    EXPECT_NE(window->bounds().ToString(), snap_left.ToString());
 
     // It should cycle back to the first snapped position.
     GetController()->PerformAction(WINDOW_SNAP_LEFT, dummy);
@@ -445,6 +447,8 @@ TEST_F(AcceleratorControllerTest, WindowSnap) {
   {
     GetController()->PerformAction(WINDOW_SNAP_RIGHT, dummy);
     gfx::Rect snap_right = window->bounds();
+    GetController()->PerformAction(WINDOW_SNAP_RIGHT, dummy);
+    EXPECT_NE(window->bounds().ToString(), snap_right.ToString());
     GetController()->PerformAction(WINDOW_SNAP_RIGHT, dummy);
     EXPECT_NE(window->bounds().ToString(), snap_right.ToString());
 
