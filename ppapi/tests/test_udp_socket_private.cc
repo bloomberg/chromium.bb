13bcc40661135907b4911d77dd7e7595f69b9797
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 15:11:55 +0000

    Revert 184946 > Fixed message size and number of iterations. > > BUG=154338 > TBR=yzshen@chromium.org > > Review URL: https://codereview.chromium.org/12328139
    TBR=ygorshenin@chromium.org
    Review URL: https://codereview.chromium.org/12321161
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184953 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 75b37d607c94e3d307df69135ab65048773c7c8d

--- ppapi/tests/test_udp_socket_private.cc
+++ ppapi/tests/test_udp_socket_private.cc
@@ -22,11 +22,11 @@ const uint16_t kPortScanTo = 4096;
 
 const size_t kEnglishAlphabetSize = 'z' - 'a' + 1;
 
-// 1K datagrams for sendto/recvfrom benchmarks.
-const size_t kBenchmarkMessageSize = 1u << 10;
+// 4K datagrams for sendto/recvfrom benchmarks.
+const size_t kBenchmarkMessageSize = 1u << 12;
 
 // Maximum number of datagrams for sendto/recvfrom benchmarks.
-const size_t kBenchmarkMaxNumMessages = 1u << 10;
+const size_t kBenchmarkMaxNumMessages = 1u << 12;
 
 // Assuming that system buffer size for UDP sockets is at least 32K.
 const size_t kUDPBufferSize = 32768;
