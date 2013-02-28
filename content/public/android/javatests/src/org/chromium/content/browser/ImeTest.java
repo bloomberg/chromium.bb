6b6ae8813d9441117edca0ab5576d270a32b515d
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 14:55:20 +0000

    try to fix android bot after compile breakage in r185241/r185242
    BUG=none
    TBR=nona
    
    Review URL: https://codereview.chromium.org/12387015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185247 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d6883423c291ea495b64f91075586a432aa6df24

--- content/public/android/javatests/src/org/chromium/content/browser/ImeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ImeTest.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.view.inputmethod.EditorInfo;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content.browser.ImeAdapter.AdapterInputConnection;
