cec1ecb2fd0aa9c9df0f9f834627911c2b2a9ac7
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 08:55:04 +0000

    Unbreak check_grd_for_unused_strings
    BUG=none
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12316160
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185183 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 70fd3bb96a93c887c0adc08bcf6ce4635c5e17a8

--- chrome/tools/check_grd_for_unused_strings.py
+++ chrome/tools/check_grd_for_unused_strings.py
@@ -153,7 +153,7 @@ def main():
                    'renderer_resources.grd'),
       os.path.join(device_base_dir, 'device_bluetooth_strings.grd'),
       os.path.join(src_dir, 'ui', 'resources', 'ui_resources.grd'),
-      os.path.join(src_dir, 'ui', 'webui', 'resources', 'shared_resources.grd'),
+      os.path.join(src_dir, 'ui', 'webui', 'resources', 'webui_resources.grd'),
       os.path.join(ui_base_strings_dir, 'app_locale_settings.grd'),
       os.path.join(ui_base_strings_dir, 'ui_strings.grd'),
     ]
