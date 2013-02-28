253fcf244d77fbaa530a0526126a686a36008ea1
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 16:57:01 +0000

    Disable ClearHistoryTest#testClearHistory
    This test is failing on JB only bots. See bug for details.
    
    TBR=joth@chromium.org
    NOTRY=true
    BUG=178762
    
    
    Review URL: https://chromiumcodereview.appspot.com/12379016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185265 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 512d5055ec310ea99d1ffe78c79c63acfbaa2c58

--- android_webview/javatests/src/org/chromium/android_webview/test/ClearHistoryTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/ClearHistoryTest.java
@@ -7,6 +7,7 @@ package org.chromium.android_webview.test;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.android_webview.AwContents;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content.browser.ContentViewCore;
@@ -26,8 +27,13 @@ public class ClearHistoryTest extends AndroidWebViewTestBase {
         }
     }
 
+    /*
     @SmallTest
     @Feature({"History", "Main"})
+    This test is only failing on JellyBean bots.
+    See crbug.com/178762.
+    */
+    @DisabledTest
     public void testClearHistory() throws Throwable {
         final TestAwContentsClient contentsClient = new TestAwContentsClient();
         final AwTestContainerView testContainerView =
