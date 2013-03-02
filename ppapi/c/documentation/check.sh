7d03346db724644207e0f5f952127013b8c23f84
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-02 17:22:09 +0000

    Pepper: Add copyright info to doc shell scripts.
    This adds missing copyright info to some utility shell scripts under ppapi so
    that checklicenses.py passes.
    
    BUG=98107
    
    
    Review URL: https://chromiumcodereview.appspot.com/12374055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185717 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 504f1dd22013ca7ce82f25ddbaec1fe2c0409778

--- ppapi/c/documentation/check.sh
+++ ppapi/c/documentation/check.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 # simple script to check html via tidy. Either specify html files on
 # command line or rely on default which checks all html files in
