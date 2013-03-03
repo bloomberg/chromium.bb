cc4c80b97c9b4d171fefabdd14f995cdacc9f0ea
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-03 02:16:14 +0000

    Add comboboxes to DetailOutputMap filling.
    R=estade@chromium.org
    TBR=sky@chromium.org
    BUG=163505
    
    Review URL: https://chromiumcodereview.appspot.com/12387057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185791 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f746d62be499785f030ae3f0a5609336aef4cc76

--- chrome/browser/ui/views/autofill/autofill_dialog_views.cc
+++ chrome/browser/ui/views/autofill/autofill_dialog_views.cc
@@ -604,9 +604,14 @@ void AutofillDialogViews::UpdateSection(DialogSection section) {
 void AutofillDialogViews::GetUserInput(DialogSection section,
                                        DetailOutputMap* output) {
   DetailsGroup* group = GroupForSection(section);
-  for (TextfieldMap::iterator it = group->textfields.begin();
+  for (TextfieldMap::const_iterator it = group->textfields.begin();
        it != group->textfields.end(); ++it) {
     output->insert(std::make_pair(it->first, it->second->textfield()->text()));
+  }
+  for (ComboboxMap::const_iterator it = group->comboboxes.begin();
+       it != group->comboboxes.end(); ++it) {
+    output->insert(std::make_pair(it->first,
+        it->second->model()->GetItemAt(it->second->selected_index())));
   }
 }
 
