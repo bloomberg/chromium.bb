fa570efe2bc45f413881e2e633a17ee7f0d704b8
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-04 09:14:46 +0000

    Fixed leak in DeviceSettingsTestBase.
    TBR=dhollowa@chromium.org
    BUG=179231
    
    Review URL: https://codereview.chromium.org/12377074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185858 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 56da6de2f7bc37e80fceb20542089b4e5eb8b20d

--- chrome/browser/chromeos/settings/device_settings_test_helper.cc
+++ chrome/browser/chromeos/settings/device_settings_test_helper.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/chromeos/settings/device_settings_test_helper.h"
 
 #include "base/message_loop.h"
+#include "base/run_loop.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "chrome/browser/chromeos/settings/device_settings_service.h"
 #include "chrome/browser/chromeos/settings/mock_owner_key_util.h"
@@ -182,7 +183,9 @@ DeviceSettingsTestBase::DeviceSettingsTestBase()
       file_thread_(content::BrowserThread::FILE, &loop_),
       owner_key_util_(new MockOwnerKeyUtil()) {}
 
-DeviceSettingsTestBase::~DeviceSettingsTestBase() {}
+DeviceSettingsTestBase::~DeviceSettingsTestBase() {
+  base::RunLoop().RunUntilIdle();
+}
 
 void DeviceSettingsTestBase::SetUp() {
   device_policy_.payload().mutable_metrics_enabled()->set_metrics_enabled(
