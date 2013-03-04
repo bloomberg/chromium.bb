bloomberg/chromium.bb

commit e4e6419825859832ece6ab914d1eab6d4109a929

FileSystem mods: Changes to snapshot file creation to remove dependencies on blobs.
* Implementats of a new WebFileSystem::createSnapshotFileAndReadMetadata(...)
method that does not take a blobURL as input and that invokes a new
WebFileSystemCallbacks::didCreateSnapshotFile(...) in response instead of
overloading the didReadMetadata callback for that purpose.

* Leaves the existing handling in place since its still in use by WK.

This CL is the 2nd in a series of changes that straddle webkit vs chromium repositories.
1) WK: Declare new virtual createSnapshotFile/didCreateSnapshotFile public api methods.
2) CR: Implement the new create method such that the didCreate method is invoked in response.
3) WK: Use the new create method and implement the new didCreate method.
3) CR: Cleanup the obsolete/deprecated blocks of code.

BUG=174200

Review URL: https://chromiumcodereview.appspot.com/12084077

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182822 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 6df15b3fb7493528e01770a4ef66266e1229d5f9

content/common/fileapi/file_system_dispatcher.cc
@@ -251,12 +251,25 @@ bool FileSystemDispatcher::NotifyCloseFile(const GURL& file_path) {
 }
 
 bool FileSystemDispatcher::CreateSnapshotFile(
-    const GURL& blob_url,
     const GURL& file_path,
     fileapi::FileSystemCallbackDispatcher* dispatcher) {
   int request_id = dispatchers_.Add(dispatcher);
   if (!ChildThread::current()->Send(
           new FileSystemHostMsg_CreateSnapshotFile(
+              request_id, file_path))) {
+    dispatchers_.Remove(request_id);  // destroys |dispatcher|
+    return false;
+  }
+  return true;
+}
+
+bool FileSystemDispatcher::CreateSnapshotFile_Deprecated(
+    const GURL& blob_url,
+    const GURL& file_path,
+    fileapi::FileSystemCallbackDispatcher* dispatcher) {
+  int request_id = dispatchers_.Add(dispatcher);
+  if (!ChildThread::current()->Send(
+          new FileSystemHostMsg_CreateSnapshotFile_Deprecated(
               request_id, blob_url, file_path))) {
     dispatchers_.Remove(request_id); // destroys |dispatcher|
     return false;
@@ -291,6 +304,18 @@ void FileSystemDispatcher::OnDidReadMetadata(
   DCHECK(dispatcher);
   dispatcher->DidReadMetadata(file_info, platform_path);
   dispatchers_.Remove(request_id);
+}
+
+void FileSystemDispatcher::OnDidCreateSnapshotFile(
+    int request_id, const base::PlatformFileInfo& file_info,
+    const base::FilePath& platform_path) {
+  fileapi::FileSystemCallbackDispatcher* dispatcher =
+      dispatchers_.Lookup(request_id);
+  DCHECK(dispatcher);
+  dispatcher->DidCreateSnapshotFile(file_info, platform_path);
+  dispatchers_.Remove(request_id);
+  ChildThread::current()->Send(
+      new FileSystemHostMsg_DidReceiveSnapshotFile(request_id));
 }
 
 void FileSystemDispatcher::OnDidReadDirectory(

content/browser/fileapi/fileapi_message_filter.cc
@@ -106,6 +106,8 @@ void FileAPIMessageFilter::OnChannelClosing() {
     blob_storage_context_->controller()->RemoveBlob(GURL(*iter));
   }
 
+  in_transit_snapshot_files_.clear();
+
   // Close all files that are previously OpenFile()'ed in this process.
   if (!open_filesystem_urls_.empty()) {
     DLOG(INFO)
@@ -152,6 +154,10 @@ bool FileAPIMessageFilter::OnMessageReceived(
     IPC_MESSAGE_HANDLER(FileSystemHostMsg_NotifyCloseFile, OnNotifyCloseFile)
     IPC_MESSAGE_HANDLER(FileSystemHostMsg_CreateSnapshotFile,
                         OnCreateSnapshotFile)
+    IPC_MESSAGE_HANDLER(FileSystemHostMsg_DidReceiveSnapshotFile,
+                        OnDidReceiveSnapshotFile)
+    IPC_MESSAGE_HANDLER(FileSystemHostMsg_CreateSnapshotFile_Deprecated,
+                        OnCreateSnapshotFile_Deprecated)
     IPC_MESSAGE_HANDLER(FileSystemHostMsg_WillUpdate, OnWillUpdate)
     IPC_MESSAGE_HANDLER(FileSystemHostMsg_DidUpdate, OnDidUpdate)
     IPC_MESSAGE_HANDLER(FileSystemHostMsg_SyncGetPlatformPath,
@@ -539,6 +545,34 @@ void FileAPIMessageFilter::OnSyncGetPlatformPath(
 }
 
 void FileAPIMessageFilter::OnCreateSnapshotFile(
+    int request_id, const GURL& path) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  FileSystemURL url(context_->CrackURL(path));
+
+  // Make sure if this file can be read by the renderer as this is
+  // called when the renderer is about to create a new File object
+  // (for reading the file).
+  base::PlatformFileError error;
+  if (!HasPermissionsForFile(url, fileapi::kReadFilePermissions, &error)) {
+    Send(new FileSystemMsg_DidFail(request_id, error));
+    return;
+  }
+
+  FileSystemOperation* operation = GetNewOperation(url, request_id);
+  if (!operation)
+    return;
+  operation->CreateSnapshotFile(
+      url,
+      base::Bind(&FileAPIMessageFilter::DidCreateSnapshot,
+                 this, request_id, url));
+}
+
+void FileAPIMessageFilter::OnDidReceiveSnapshotFile(int request_id) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  in_transit_snapshot_files_.erase(request_id);
+}
+
+void FileAPIMessageFilter::OnCreateSnapshotFile_Deprecated(
     int request_id, const GURL& blob_url, const GURL& path) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   FileSystemURL url(context_->CrackURL(path));
@@ -560,7 +594,7 @@ void FileAPIMessageFilter::OnCreateSnapshotFile(
     return;
   operation->CreateSnapshotFile(
       url,
-      base::Bind(&FileAPIMessageFilter::DidCreateSnapshot,
+      base::Bind(&FileAPIMessageFilter::DidCreateSnapshot_Deprecated,
                  this, request_id, register_file_callback));
 }
 
@@ -735,6 +769,57 @@ void FileAPIMessageFilter::DidDeleteFileSystem(
 }
 
 void FileAPIMessageFilter::DidCreateSnapshot(
+    int request_id,
+    const fileapi::FileSystemURL& url,
+    base::PlatformFileError result,
+    const base::PlatformFileInfo& info,
+    const base::FilePath& platform_path,
+    const scoped_refptr<webkit_blob::ShareableFileReference>& snapshot_file) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  if (result != base::PLATFORM_FILE_OK) {
+    Send(new FileSystemMsg_DidFail(request_id, result));
+    return;
+  }
+
+  if (!ChildProcessSecurityPolicyImpl::GetInstance()->CanReadFile(
+          process_id_, platform_path)) {
+    // In order for the renderer to be able to read the file, it must be granted
+    // read permission for the file's platform path. By now, it has already been
+    // verified that the renderer has sufficient permissions to read the file.
+    // It is still possible that ChildProcessSecurityPolicyImpl doesn't reflect
+    // that the renderer can read the file's platform path. If this is the case
+    // the renderer should be granted read permission for the file's platform
+    // path. This can happen in the following situations:
+    // - the file comes from sandboxed filesystem. Reading sandboxed files is
+    //   always permitted, but only implicitly.
+    // - the underlying filesystem returned newly created snapshot file.
+    // - the file comes from an external drive filesystem. The renderer has
+    //   already been granted read permission for the file's nominal path, but
+    //   for drive files, platform paths differ from the nominal paths.
+    DCHECK(snapshot_file ||
+           fileapi::SandboxMountPointProvider::CanHandleType(url.type()) ||
+           url.type() == fileapi::kFileSystemTypeDrive);
+    ChildProcessSecurityPolicyImpl::GetInstance()->GrantReadFile(
+        process_id_, platform_path);
+    if (snapshot_file) {
+      // This will revoke all permissions for the file when the last ref
+      // of the file is dropped (assuming it's ok).
+      snapshot_file->AddFinalReleaseCallback(
+          base::Bind(&RevokeFilePermission, process_id_));
+    }
+  }
+
+  if (snapshot_file) {
+    // This ref is held until OnDidReceiveSnapshotFile is called.
+    in_transit_snapshot_files_[request_id] = snapshot_file;
+  }
+
+  // Return the file info and platform_path.
+  Send(new FileSystemMsg_DidCreateSnapshotFile(
+               request_id, info, platform_path));
+}
+
+void FileAPIMessageFilter::DidCreateSnapshot_Deprecated(
     int request_id,
     const base::Callback<void(const base::FilePath&)>& register_file_callback,
     base::PlatformFileError result,

content/browser/fileapi/fileapi_message_filter.h
@@ -5,6 +5,7 @@
 #ifndef CONTENT_BROWSER_FILEAPI_FILEAPI_MESSAGE_FILTER_H_
 #define CONTENT_BROWSER_FILEAPI_FILEAPI_MESSAGE_FILTER_H_
 
+#include <map>
 #include <set>
 #include <string>
 
@@ -115,8 +116,12 @@ class FileAPIMessageFilter : public BrowserMessageFilter {
   void OnSyncGetPlatformPath(const GURL& path,
                              base::FilePath* platform_path);
   void OnCreateSnapshotFile(int request_id,
-                            const GURL& blob_url,
                             const GURL& path);
+  void OnDidReceiveSnapshotFile(int request_id);
+
+  void OnCreateSnapshotFile_Deprecated(int request_id,
+                                       const GURL& blob_url,
+                                       const GURL& path);
 
   void OnStartBuildingBlob(const GURL& url);
   void OnAppendBlobDataItem(const GURL& url,
@@ -155,14 +160,21 @@ class FileAPIMessageFilter : public BrowserMessageFilter {
                            base::PlatformFileError result);
   void DidCreateSnapshot(
       int request_id,
-      const base::Callback<void(const base::FilePath&)>& register_file_callback,
+      const fileapi::FileSystemURL& url,
       base::PlatformFileError result,
       const base::PlatformFileInfo& info,
       const base::FilePath& platform_path,
       const scoped_refptr<webkit_blob::ShareableFileReference>& file_ref);
 
+  void DidCreateSnapshot_Deprecated(
+      int request_id,
+      const base::Callback<void(const base::FilePath&)>& register_file_callback,
+      base::PlatformFileError result,
+      const base::PlatformFileInfo& info,
+      const base::FilePath& platform_path,
+      const scoped_refptr<webkit_blob::ShareableFileReference>& file_ref);
   // Registers the given file pointed by |virtual_path| and backed by
-  // |platform_path| as the |blob_url|.  Called by DidCreateSnapshot.
+  // |platform_path| as the |blob_url|.  Called by DidCreateSnapshot_Deprecated.
   void RegisterFileAsBlob(const GURL& blob_url,
                           const fileapi::FileSystemURL& url,
                           const base::FilePath& platform_path);
@@ -195,6 +207,11 @@ class FileAPIMessageFilter : public BrowserMessageFilter {
   // Keep track of blob URLs registered in this process. Need to unregister
   // all of them when the renderer process dies.
   base::hash_set<std::string> blob_urls_;
+
+  // Used to keep snapshot files alive while a DidCreateSnapshot
+  // is being sent to the renderer.
+  std::map<int, scoped_refptr<webkit_blob::ShareableFileReference> >
+      in_transit_snapshot_files_;
 
   // Keep track of file system file URLs opened by OpenFile() in this process.
   // Need to close all of them when the renderer process dies.

content/common/fileapi/file_system_dispatcher.h
@@ -91,9 +91,14 @@ class FileSystemDispatcher : public IPC::Listener {
   // raw PlatformFile returned from OpenFile.
   bool NotifyCloseFile(const GURL& file_path);
 
-  bool CreateSnapshotFile(const GURL& blod_url,
-                          const GURL& file_path,
+  bool CreateSnapshotFile(const GURL& file_path,
                           fileapi::FileSystemCallbackDispatcher* dispatcher);
+
+  bool CreateSnapshotFile_Deprecated(
+      const GURL& blod_url,
+      const GURL& file_path,
+      fileapi::FileSystemCallbackDispatcher* dispatcher);
+
  private:
   // Message handlers.
   void OnDidOpenFileSystem(int request_id,
@@ -103,6 +108,9 @@ class FileSystemDispatcher : public IPC::Listener {
   void OnDidReadMetadata(int request_id,
                          const base::PlatformFileInfo& file_info,
                          const base::FilePath& platform_path);
+  void OnDidCreateSnapshotFile(int request_id,
+                               const base::PlatformFileInfo& file_info,
+                               const base::FilePath& platform_path);
   void OnDidReadDirectory(
       int request_id,
       const std::vector<base::FileUtilProxy::Entry>& entries,

content/common/fileapi/file_system_messages.h
@@ -35,6 +35,10 @@ IPC_MESSAGE_CONTROL3(FileSystemMsg_DidReadMetadata,
                      int /* request_id */,
                      base::PlatformFileInfo,
                      base::FilePath /* true platform path, where possible */)
+IPC_MESSAGE_CONTROL3(FileSystemMsg_DidCreateSnapshotFile,
+                     int /* request_id */,
+                     base::PlatformFileInfo,
+                     base::FilePath /* true platform path */)
 IPC_MESSAGE_CONTROL3(FileSystemMsg_DidReadDirectory,
                      int /* request_id */,
                      std::vector<base::FileUtilProxy::Entry> /* entries */,
@@ -143,11 +147,22 @@ IPC_MESSAGE_CONTROL3(FileSystemHostMsg_OpenFile,
 IPC_MESSAGE_CONTROL1(FileSystemHostMsg_NotifyCloseFile,
                      GURL /* file path */)
 
-// WebFileSystem::createSnapshotFileAndReadMetadata() message.
-IPC_MESSAGE_CONTROL3(FileSystemHostMsg_CreateSnapshotFile,
+// DEPRECATED
+IPC_MESSAGE_CONTROL3(FileSystemHostMsg_CreateSnapshotFile_Deprecated,
                      int /* request_id */,
                      GURL /* blob_url */,
                      GURL /* file_path */)
+
+// WebFileSystem::createSnapshotFileAndReadMetadata() message.
+IPC_MESSAGE_CONTROL2(FileSystemHostMsg_CreateSnapshotFile,
+                     int /* request_id */,
+                     GURL /* file_path */)
+
+// Renderers are expected to send this message after having processed
+// the FileSystemMsg_DidCreateSnapshotFile message. In particular,
+// after having created a BlobDataHandle backed by the snapshot file.
+IPC_MESSAGE_CONTROL1(FileSystemHostMsg_DidReceiveSnapshotFile,
+                     int /* request_id */)
 
 // For Pepper's URL loader.
 IPC_SYNC_MESSAGE_CONTROL1_1(FileSystemHostMsg_SyncGetPlatformPath,

content/common/fileapi/webfilesystem_callback_dispatcher.cc
@@ -46,6 +46,15 @@ void WebFileSystemCallbackDispatcher::DidReadMetadata(
   callbacks_->didReadMetadata(web_file_info);
 }
 
+void WebFileSystemCallbackDispatcher::DidCreateSnapshotFile(
+    const base::PlatformFileInfo& file_info,
+    const base::FilePath& platform_path) {
+  WebFileInfo web_file_info;
+  webkit_glue::PlatformFileInfoToWebFileInfo(file_info, &web_file_info);
+  web_file_info.platformPath = webkit_base::FilePathToWebString(platform_path);
+  callbacks_->didCreateSnapshotFile(web_file_info);
+}
+
 void WebFileSystemCallbackDispatcher::DidReadDirectory(
     const std::vector<base::FileUtilProxy::Entry>& entries, bool has_more) {
   WebVector<WebFileSystemEntry> file_system_entries(entries.size());

content/common/fileapi/webfilesystem_callback_dispatcher.h
@@ -28,6 +28,9 @@ class WebFileSystemCallbackDispatcher
   virtual void DidReadMetadata(
       const base::PlatformFileInfo& file_info,
       const base::FilePath& platform_path) OVERRIDE;
+  virtual void DidCreateSnapshotFile(
+      const base::PlatformFileInfo& file_info,
+      const base::FilePath& platform_path) OVERRIDE;
   virtual void DidReadDirectory(
       const std::vector<base::FileUtilProxy::Entry>& entries,
       bool has_more) OVERRIDE;

content/common/fileapi/webfilesystem_impl.cc
@@ -120,12 +120,22 @@ WebKit::WebFileWriter* WebFileSystemImpl::createFileWriter(
 }
 
 void WebFileSystemImpl::createSnapshotFileAndReadMetadata(
-    const WebKit::WebURL& blobURL,
     const WebKit::WebURL& path,
     WebKit::WebFileSystemCallbacks* callbacks) {
   FileSystemDispatcher* dispatcher =
       ChildThread::current()->file_system_dispatcher();
   dispatcher->CreateSnapshotFile(
+      GURL(path), new WebFileSystemCallbackDispatcher(callbacks));
+}
+
+// DEPRECATED
+void WebFileSystemImpl::createSnapshotFileAndReadMetadata(
+    const WebKit::WebURL& blobURL,
+    const WebKit::WebURL& path,
+    WebKit::WebFileSystemCallbacks* callbacks) {
+  FileSystemDispatcher* dispatcher =
+      ChildThread::current()->file_system_dispatcher();
+  dispatcher->CreateSnapshotFile_Deprecated(
       GURL(blobURL), GURL(path),
       new WebFileSystemCallbackDispatcher(callbacks));
 }

commit 3e59ad40633a1e74a0553ba97f8c1c665e171fe9

Fix a boneheaded problem introduced in r182822. Review URL: https://codereview.chromium.org/12388058
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185974 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: bec4c85601a5ca356daa320327425f915d142152

content/common/fileapi/file_system_dispatcher.cc
@@ -33,6 +33,8 @@ bool FileSystemDispatcher::OnMessageReceived(const IPC::Message& msg) {
     IPC_MESSAGE_HANDLER(FileSystemMsg_DidSucceed, OnDidSucceed)
     IPC_MESSAGE_HANDLER(FileSystemMsg_DidReadDirectory, OnDidReadDirectory)
     IPC_MESSAGE_HANDLER(FileSystemMsg_DidReadMetadata, OnDidReadMetadata)
+    IPC_MESSAGE_HANDLER(FileSystemMsg_DidCreateSnapshotFile,
+                        OnDidCreateSnapshotFile)
     IPC_MESSAGE_HANDLER(FileSystemMsg_DidFail, OnDidFail)
     IPC_MESSAGE_HANDLER(FileSystemMsg_DidWrite, OnDidWrite)
     IPC_MESSAGE_HANDLER(FileSystemMsg_DidOpenFile, OnDidOpenFile)