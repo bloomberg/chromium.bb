531cdf4aa92fd700175d36ca39140868a35f5350
glotov@chromium.org <glotov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-05 12:20:51 +0000

    Update asan_stub in accordance with asan API.
    BUG=none
    TEST=none
    TBR=dennisjeffrey@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12413006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186159 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 62a80cdd844c68d6021c10eea37ac376d6836f06

--- chrome/test/pyautolib/asan_stub.c
+++ chrome/test/pyautolib/asan_stub.c
@@ -60,18 +60,19 @@ void __asan_init() {
   }
 }
 
-void __asan_register_globals() { }
-void __asan_unregister_globals() { }
 void __asan_handle_no_return() { }
+void __asan_init_v1() { }
+void __asan_register_globals() { }
 void __asan_report_load1() { }
+void __asan_report_load16() { }
 void __asan_report_load2() { }
 void __asan_report_load4() { }
 void __asan_report_load8() { }
-void __asan_report_load16() { }
 void __asan_report_store1() { }
+void __asan_report_store16() { }
 void __asan_report_store2() { }
 void __asan_report_store4() { }
 void __asan_report_store8() { }
-void __asan_report_store16() { }
 void __asan_set_error_report_callback() { }
+void __asan_unregister_globals() { }
 void __sanitizer_sandbox_on_notify() { }
