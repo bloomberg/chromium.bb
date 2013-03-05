02c43ab9f2ddf2abffca9778d7ba520b40739dd0
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-05 22:36:07 +0000

    Revert 186264
    Caused failures in interactive_ui_tests on Mac 10.6 Tests (dbg) bots. 
    
    > Disable threaded compositing, but leave force compositing
    > enabled.
    > 
    > BUG=172015
    > TBR=tonyg
    > 
    > Review URL: https://codereview.chromium.org/12482002
    
    TBR=ccameron@chromium.org
    Review URL: https://codereview.chromium.org/12494002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186272 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d664bc3c11093e0eb5c7c4b4fe0d47d86baed958

--- chrome/browser/gpu/chrome_gpu_util.cc
+++ chrome/browser/gpu/chrome_gpu_util.cc
@@ -156,12 +156,9 @@ void InitializeCompositingFieldTrial() {
   // http://crbug.com/133602 for mac
   // http://crbug.com/140866 for linux
 
-#if defined(OS_WIN)
+#if defined(OS_WIN) || defined(OS_MACOSX)
   // Enable threaded compositing on Windows.
   threaded_compositing_probability = kDivisor;
-#elif defined(OS_MACOSX)
-  // Enable force compositing mode on Mac.
-  force_compositing_mode_probability = kDivisor;
 #endif
 
   int force_compositing_group = trial->AppendGroup(
