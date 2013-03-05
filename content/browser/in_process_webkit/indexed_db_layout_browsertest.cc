10875df0816b36fa5acc9a8d7699b17d51bc7b7c
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-05 17:02:44 +0000

    IndexedDB: Re-enable previously flaky tests
    Root cause of key-type-array (165671) believed fixed. Index tests
    not timing out marked as flaky (but maybe just not running?). In
    any case - let's give it a whirl.
    
    BUG=153064,165671
    
    Review URL: https://codereview.chromium.org/12387105
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186191 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6474dc7dcaa029dc7b8dd4d543e97d094bacaa8f

--- content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
+++ content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
@@ -63,8 +63,7 @@ static const char* kKeyTests[] = {
   "keyrange-required-arguments.html",
   "key-sort-order-across-types.html",
   "key-sort-order-date.html",
-  // Flaky: http://crbug.com/165671
-  //"key-type-array.html",
+  "key-type-array.html",
   "key-type-infinity.html",
   "invalid-keys.html",
   NULL
@@ -122,10 +121,7 @@ IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, ComplexTests) {
   RunLayoutTests(kComplexTests);
 }
 
-// TODO(dgrogan): times out flakily. http://crbug.com/153064
-// Marking FLAKY to diagnose if this is just a test duration
-// issue (and we need to split it up) or something else.
-IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, FLAKY_IndexTests) {
+IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, IndexTests) {
   RunLayoutTests(kIndexTests);
 }
 
@@ -147,11 +143,6 @@ IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, IntVersionTests2) {
 
 IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, RegressionTests) {
   RunLayoutTests(kRegressionTests);
-}
-
-// TODO(jsbell): Remove this when data has been gathered.
-IN_PROC_BROWSER_TEST_F(IndexedDBLayoutTest, FLAKY_FlakyTests) {
-  RunLayoutTest("key-type-array.html");
 }
 
 }  // namespace content
