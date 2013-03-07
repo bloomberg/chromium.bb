b1928519ec255c01bed043329699515ace4e9916
vsevik@chromium.org <vsevik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 22:33:41 +0000

    Rebind frontend host when frontend renderer navigates to devtools url.
    BUG=180887
    
    
    Review URL: https://chromiumcodereview.appspot.com/12528011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186802 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: df2a3057eb2c4a13deabefcad8c5a0c13f78aa67

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -498,20 +498,28 @@ void DevToolsWindow::AddDevToolsExtensionsToClient() {
 
 WebContents* DevToolsWindow::OpenURLFromTab(WebContents* source,
                                             const OpenURLParams& params) {
-  if (params.url.SchemeIs(chrome::kChromeDevToolsScheme)) {
-    chrome::NavigateParams nav_params(profile_, params.url, params.transition);
-    FillNavigateParamsFromOpenURLParams(&nav_params, params);
-    nav_params.source_contents = source;
-    nav_params.tabstrip_add_types = TabStripModel::ADD_NONE;
-    nav_params.window_action = chrome::NavigateParams::SHOW_WINDOW;
-    nav_params.user_gesture = true;
-    chrome::Navigate(&nav_params);
-    return nav_params.target_contents;
+  if (!params.url.SchemeIs(chrome::kChromeDevToolsScheme)) {
+    if (inspected_web_contents_)
+      return inspected_web_contents_->OpenURL(params);
+    return NULL;
   }
 
-  if (inspected_web_contents_)
-    return inspected_web_contents_->OpenURL(params);
-  return NULL;
+  DevToolsManager* manager = DevToolsManager::GetInstance();
+  scoped_refptr<DevToolsAgentHost> agent_host(
+      manager->GetDevToolsAgentHostFor(frontend_host_.get()));
+  if (!agent_host)
+    return NULL;
+  manager->ClientHostClosing(frontend_host_.get());
+  manager->RegisterDevToolsClientHostFor(agent_host, frontend_host_.get());
+
+  chrome::NavigateParams nav_params(profile_, params.url, params.transition);
+  FillNavigateParamsFromOpenURLParams(&nav_params, params);
+  nav_params.source_contents = source;
+  nav_params.tabstrip_add_types = TabStripModel::ADD_NONE;
+  nav_params.window_action = chrome::NavigateParams::SHOW_WINDOW;
+  nav_params.user_gesture = true;
+  chrome::Navigate(&nav_params);
+  return nav_params.target_contents;
 }
 
 void DevToolsWindow::CallClientFunction(const std::string& function_name,
