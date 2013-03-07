2753389cf3e55b3594a7311e5de64128bfd5cb4b
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 13:34:25 +0000

    [content shell] WebRuntimeFeatures are managed by TestRunner
    Remove the obsolete (and incomplete) code from content_shell
    
    BUG=111316
    R=marja@chromium.org
    TEST=no functional change
    
    Review URL: https://codereview.chromium.org/12459007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186706 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 558b2e4ba581c3c20b22520aeaf5705adaa442ee

--- content/shell/shell_render_process_observer.cc
+++ content/shell/shell_render_process_observer.cc
@@ -13,14 +13,12 @@
 #include "content/shell/shell_messages.h"
 #include "content/shell/shell_switches.h"
 #include "content/shell/webkit_test_runner.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebRuntimeFeatures.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
 #include "third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebTestInterfaces.h"
 #include "webkit/glue/webkit_glue.h"
 #include "webkit/support/gc_extension.h"
 
 using WebKit::WebFrame;
-using WebKit::WebRuntimeFeatures;
 using WebTestRunner::WebTestDelegate;
 using WebTestRunner::WebTestInterfaces;
 
@@ -43,12 +41,6 @@ ShellRenderProcessObserver::ShellRenderProcessObserver()
   RenderThread::Get()->AddObserver(this);
   if (!CommandLine::ForCurrentProcess()->HasSwitch(switches::kDumpRenderTree))
     return;
-  WebRuntimeFeatures::enableInputTypeDateTime(true);
-  WebRuntimeFeatures::enableInputTypeDateTimeLocal(true);
-  WebRuntimeFeatures::enableInputTypeMonth(true);
-  WebRuntimeFeatures::enableInputTypeTime(true);
-  WebRuntimeFeatures::enableInputTypeWeek(true);
-  WebRuntimeFeatures::enableCanvasPath(true);
   DisableAppCacheLogging();
   EnableDevToolsFrontendTesting();
   EnableShortCircuitSizeUpdates();
