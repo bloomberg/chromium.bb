092b1b803d11df3b834a9ca8bce440c0c9ff3b6a
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 19:33:37 +0000

    Take by_user out of experimental.
    TBR=dewittj@chromium.org
    
    BUG=178511
    
    Review URL: https://codereview.chromium.org/12634005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186759 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e5da0c2f1b2fcb6f68af10e53a07bfcbe7c23d09

--- chrome/test/data/extensions/api_test/notifications/api/by_user/background.js
+++ chrome/test/data/extensions/api_test/notifications/api/by_user/background.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-const notifications = chrome.experimental.notifications;
+const notifications = chrome.notifications;
 var theOnlyTestDone = null;
 
 var notificationData = {
