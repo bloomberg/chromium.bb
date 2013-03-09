a2422ef8951abb3082cdcbd00b340342412f5122
crogers@google.com <crogers@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-09 02:23:56 +0000

    Disable CanRunUnifiedAudioTests() which may be hanging other Windows tests.
    This test is now being run by default on the bots and is hanging them.
    So I'm disabling for now.
    
    BUG=none
    TEST=none
    TBR=henrika@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187113 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b0398c17744f357ea58c9e73fc69d2478c2109f6

--- media/audio/win/audio_unified_win_unittest.cc
+++ media/audio/win/audio_unified_win_unittest.cc
@@ -123,27 +123,9 @@ class UnifiedSourceCallback : public AudioOutputStream::AudioSourceCallback {
 // Convenience method which ensures that we fulfill all required conditions
 // to run unified audio tests on Windows.
 static bool CanRunUnifiedAudioTests(AudioManager* audio_man) {
-  if (!CoreAudioUtil::IsSupported()) {
-    LOG(WARNING) << "This tests requires Windows Vista or higher.";
-    return false;
-  }
-
-  if (!audio_man->HasAudioOutputDevices()) {
-    LOG(WARNING) << "No output devices detected.";
-    return false;
-  }
-
-  if (!audio_man->HasAudioInputDevices()) {
-    LOG(WARNING) << "No input devices detected.";
-    return false;
-  }
-
-  if (!WASAPIUnifiedStream::HasUnifiedDefaultIO()) {
-    LOG(WARNING) << "Audio IO is not supported.";
-    return false;
-  }
-
-  return true;
+  // TODO(crogers, henrika): figure out why enabling this test causes
+  // other tests to hang.
+  return false;
 }
 
 // Convenience class which simplifies creation of a unified AudioOutputStream
