d39526fae3aa915f869490ad21a85d7fdd80cf38
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-12 00:09:41 +0000

    Omnibox: Improve HistoryQuick Cache Restoring Test
    Check the version of the private data files before and after restoring.
    
    I have verified that this would've caught the problem of always
    getting an empty private_data back from the fileDB restoring thread.
    
    TBR=sky@chromium.org
    BUG=180098
    
    
    Review URL: https://chromiumcodereview.appspot.com/12719003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187419 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4a27ad7935cff70f2e2752046ee9a69f2cecc16b

--- chrome/browser/history/in_memory_url_index_unittest.cc
+++ chrome/browser/history/in_memory_url_index_unittest.cc
@@ -941,6 +941,10 @@ TEST_F(InMemoryURLIndexTest, CacheSaveRestore) {
   EXPECT_FALSE(private_data.history_info_map_.empty());
   EXPECT_FALSE(private_data.word_starts_map_.empty());
 
+  // Make sure the data we have was built from history.  (Version 0
+  // means rebuilt from history.)
+  EXPECT_EQ(0, private_data.restored_cache_version_);
+
   // Capture the current private data for later comparison to restored data.
   scoped_refptr<URLIndexPrivateData> old_data(private_data.Duplicate());
 
@@ -970,6 +974,11 @@ TEST_F(InMemoryURLIndexTest, CacheSaveRestore) {
   EXPECT_TRUE(restore_observer.succeeded());
 
   URLIndexPrivateData& new_data(*GetPrivateData());
+
+  // Make sure the data we have was reloaded from cache.  (Version 0
+  // means rebuilt from history; anything else means restored from
+  // a cache version.)
+  EXPECT_GT(new_data.restored_cache_version_, 0);
 
   // Compare the captured and restored for equality.
   ExpectPrivateDataEqual(*old_data, new_data);
