f4b1a4705a736ac18e780f095eb6c175162ce8cf
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-12 06:31:30 +0000

    Replace the implementation of RelayCallback by ComposedCallback.
    The new method CreateComposedCallback is not yet used, but CreateRelayCallback.
    It will be used to clean up boiler plate callbacks in test_util.
    
    BUG=180569
    TEST=Ran unit_tests.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12567003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187521 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: aacfff784b0e57d24a693ed29a9db2b4ce03109d

--- chrome/browser/google_apis/task_util.h
+++ chrome/browser/google_apis/task_util.h
@@ -19,101 +19,98 @@ void RunTaskOnUIThread(const base::Closure& task);
 
 namespace internal {
 
-// RelayCallback relays arguments for callback running on the given thread.
-template<typename CallbackType>
-struct RelayCallback;
+// Implementation of the composed callback, whose signature is |Sig|.
+template<typename Sig> struct ComposedCallback;
 
-// RelayCallback for callback with one argument.
+// ComposedCallback with one argument.
 template<typename T1>
-struct RelayCallback<base::Callback<void(T1)> > {
-  static void Run(scoped_refptr<base::MessageLoopProxy> relay_proxy,
-                  const base::Callback<void(T1)>& callback,
-                  T1 arg1) {
-    if (callback.is_null())
-      return;
-    RunTaskOnThread(relay_proxy, base::Bind(callback, arg1));
+struct ComposedCallback<void(T1)> {
+  static void Run(
+      const base::Callback<void(const base::Closure&)>& runner,
+      const base::Callback<void(T1)>& callback,
+      T1 arg1) {
+    runner.Run(base::Bind(callback, arg1));
   }
 };
 
-// RelayCallback for callback with two arguments.
+// ComposedCallback with two arguments.
 template<typename T1, typename T2>
-struct RelayCallback<base::Callback<void(T1, T2)> > {
-  static void Run(scoped_refptr<base::MessageLoopProxy> relay_proxy,
-                  const base::Callback<void(T1, T2)>& callback,
-                  T1 arg1,
-                  T2 arg2) {
-    if (callback.is_null())
-      return;
-    RunTaskOnThread(relay_proxy, base::Bind(callback, arg1, arg2));
+struct ComposedCallback<void(T1, T2)> {
+  static void Run(
+      const base::Callback<void(const base::Closure&)>& runner,
+      const base::Callback<void(T1, T2)>& callback,
+      T1 arg1, T2 arg2) {
+    runner.Run(base::Bind(callback, arg1, arg2));
   }
 };
 
-// RelayCallback for callback with two arguments, the last one is scoped_ptr.
-template<typename T1, typename T2>
-struct RelayCallback<base::Callback<void(T1, scoped_ptr<T2>)> > {
-  static void Run(scoped_refptr<base::MessageLoopProxy> relay_proxy,
-                  const base::Callback<void(T1, scoped_ptr<T2>)>& callback,
-                  T1 arg1,
-                  scoped_ptr<T2> arg2) {
-    if (callback.is_null())
-      return;
-    RunTaskOnThread(relay_proxy,
-                    base::Bind(callback, arg1, base::Passed(&arg2)));
+// ComposedCallback with two arguments, and the last one is scoped_ptr.
+template<typename T1, typename T2, typename D2>
+struct ComposedCallback<void(T1, scoped_ptr<T2, D2>)> {
+  static void Run(
+      const base::Callback<void(const base::Closure&)>& runner,
+      const base::Callback<void(T1, scoped_ptr<T2, D2>)>& callback,
+      T1 arg1, scoped_ptr<T2, D2> arg2) {
+    runner.Run(base::Bind(callback, arg1, base::Passed(&arg2)));
   }
 };
 
-// RelayCallback for callback with three arguments.
+// ComposedCallback with three arguments.
 template<typename T1, typename T2, typename T3>
-struct RelayCallback<base::Callback<void(T1, T2, T3)> > {
-  static void Run(scoped_refptr<base::MessageLoopProxy> relay_proxy,
-                  const base::Callback<void(T1, T2, T3)>& callback,
-                  T1 arg1,
-                  T2 arg2,
-                  T3 arg3) {
-    if (callback.is_null())
-      return;
-    RunTaskOnThread(relay_proxy, base::Bind(callback, arg1, arg2, arg3));
+struct ComposedCallback<void(T1, T2, T3)> {
+  static void Run(
+      const base::Callback<void(const base::Closure&)>& runner,
+      const base::Callback<void(T1, T2, T3)>& callback,
+      T1 arg1, T2 arg2, T3 arg3) {
+    runner.Run(base::Bind(callback, arg1, arg2, arg3));
   }
 };
 
-// RelayCallback for callback with three arguments, the last one is scoped_ptr.
-template<typename T1, typename T2, typename T3>
-struct RelayCallback<base::Callback<void(T1, T2, scoped_ptr<T3>)> > {
-  static void Run(scoped_refptr<base::MessageLoopProxy> relay_proxy,
-                  const base::Callback<void(T1, T2, scoped_ptr<T3>)>& callback,
-                  T1 arg1,
-                  T2 arg2,
-                  scoped_ptr<T3> arg3) {
-    if (callback.is_null())
-      return;
-    RunTaskOnThread(relay_proxy,
-                    base::Bind(callback, arg1, arg2, base::Passed(&arg3)));
+// ComposedCallback with three arguments, and the last one is scoped_ptr.
+template<typename T1, typename T2, typename T3, typename D3>
+struct ComposedCallback<void(T1, T2, scoped_ptr<T3, D3>)> {
+  static void Run(
+      const base::Callback<void(const base::Closure&)>& runner,
+      const base::Callback<void(T1, T2, scoped_ptr<T3, D3>)>& callback,
+      T1 arg1, T2 arg2, scoped_ptr<T3, D3> arg3) {
+    runner.Run(base::Bind(callback, arg1, arg2, base::Passed(&arg3)));
   }
 };
 
-// RelayCallback for callback with four arguments.
+// ComposedCallback with four arguments.
 template<typename T1, typename T2, typename T3, typename T4>
-struct RelayCallback<base::Callback<void(T1, T2, T3, T4)> > {
-  static void Run(scoped_refptr<base::MessageLoopProxy> relay_proxy,
-                  const base::Callback<void(T1, T2, T3, T4)>& callback,
-                  T1 arg1,
-                  T2 arg2,
-                  T3 arg3,
-                  T4 arg4) {
-    if (callback.is_null())
-      return;
-    RunTaskOnThread(relay_proxy, base::Bind(callback, arg1, arg2, arg3, arg4));
+struct ComposedCallback<void(T1, T2, T3, T4)> {
+  static void Run(
+      const base::Callback<void(const base::Closure&)>& runner,
+      const base::Callback<void(T1, T2, T3, T4)>& callback,
+      T1 arg1, T2 arg2, T3 arg3, T4 arg4) {
+    runner.Run(base::Bind(callback, arg1, arg2, arg3, arg4));
   }
 };
 
 }  // namespace internal
 
+// Returns callback that takes arguments (arg1, arg2, ...), create a closure
+// by binding them to |callback|, and runs |runner| with the closure.
+// I.e. the returned callback works as follows:
+//   runner.Run(Bind(callback, arg1, arg2, ...))
+template<typename CallbackType>
+CallbackType CreateComposedCallback(
+    const base::Callback<void(const base::Closure&)>& runner,
+    const CallbackType& callback) {
+  DCHECK(!runner.is_null());
+  DCHECK(!callback.is_null());
+  return base::Bind(
+      &internal::ComposedCallback<typename CallbackType::RunType>::Run,
+      runner, callback);
+}
+
 // Returns callback which runs the given |callback| on the current thread.
 template<typename CallbackType>
 CallbackType CreateRelayCallback(const CallbackType& callback) {
-  return base::Bind(&internal::RelayCallback<CallbackType>::Run,
-                    base::MessageLoopProxy::current(),
-                    callback);
+  return CreateComposedCallback(
+      base::Bind(&RunTaskOnThread, base::MessageLoopProxy::current()),
+      callback);
 }
 
 }  // namespace google_apis
