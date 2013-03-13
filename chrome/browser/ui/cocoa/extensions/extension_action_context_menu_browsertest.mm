0c880e1b63fe85cb3785f56b9917bc4fac951ac9
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 10:10:00 +0000

    Disable flaky test ExtensionActionContextMenuTest.RunInspectPopup
    BUG=180759
    TBR=yoz
    
    Review URL: https://chromiumcodereview.appspot.com/12408027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187824 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d19a7ee4660baa79ed3e4faf1628b319066778c7

--- chrome/browser/ui/cocoa/extensions/extension_action_context_menu_browsertest.mm
+++ chrome/browser/ui/cocoa/extensions/extension_action_context_menu_browsertest.mm
@@ -124,7 +124,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionActionContextMenuTest, BrowserAction) {
   [wc destroyBrowser];
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionActionContextMenuTest, RunInspectPopup) {
+IN_PROC_BROWSER_TEST_F(
+    ExtensionActionContextMenuTest, DISABLED_RunInspectPopup) {
   SetupPageAction();
   scoped_nsobject<ExtensionActionContextMenu> menu;
   menu.reset([[ExtensionActionContextMenu alloc] initWithExtension:extension_
