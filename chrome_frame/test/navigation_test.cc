53e16e56dad1a09bb3b3cab96d04611ce20ac472
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 19:30:27 +0000

    Disabling FullTabDownloadTest.CF_DownloadFileFromPost due to flaky timeouts.
    BUG=189099
    TBR=ananta
    
    Review URL: https://chromiumcodereview.appspot.com/12490011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187915 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8cf4350ee12a563f4f78f9820fba10a16760240e

--- chrome_frame/test/navigation_test.cc
+++ chrome_frame/test/navigation_test.cc
@@ -749,7 +749,7 @@ void CloseWindow(HWND* window) {
 // IE brings up a file open dialog in this context.
 // We bring up the Save dialog via accessibility and save the file
 // and validate that all is well.
-TEST_F(FullTabDownloadTest, CF_DownloadFileFromPost) {
+TEST_F(FullTabDownloadTest, DISABLED_CF_DownloadFileFromPost) {
   // Please see http://code.google.com/p/chromium/issues/detail?id=60987
   // for more information on why this test is disabled for Vista with IE7.
   if (base::win::GetVersion() >= base::win::VERSION_VISTA) {
