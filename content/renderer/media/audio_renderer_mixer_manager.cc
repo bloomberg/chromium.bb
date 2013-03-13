db166fae26308f998563da44310bb5b88332701b
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 01:17:40 +0000

    Only skip resampling on ChromeOS.
    Pulse requires the native sample rate to be used for perfect playback.
    
    BUG=178101
    TEST=pulse playback works fine.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12564005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187733 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c8031f8f8dfb16e7dd4ef374f39fbaaf9d450b3d

--- content/renderer/media/audio_renderer_mixer_manager.cc
+++ content/renderer/media/audio_renderer_mixer_manager.cc
@@ -52,9 +52,9 @@ media::AudioRendererMixer* AudioRendererMixerManager::GetMixer(
     return it->second.mixer;
   }
 
-  // On Linux and ChromeOS we can rely on the playback device to handle
-  // resampling, so don't waste cycles on it here.
-#if defined(OS_LINUX)
+  // On ChromeOS we can rely on the playback device to handle resampling, so
+  // don't waste cycles on it here.
+#if defined(OS_CHROMEOS)
   int sample_rate = params.sample_rate();
 #else
   int sample_rate = hardware_config_->GetOutputSampleRate();
