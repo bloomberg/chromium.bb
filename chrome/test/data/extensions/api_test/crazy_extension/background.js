933aeae998420668627dea9f7d9497f8c22bafe0
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 01:11:34 +0000

    Prevent ObjectBackedNativeHandler from calling itself once invalid. Recommitting: added pesky HandleScopes.
    BUG=181668
    
    Review URL: https://codereview.chromium.org/12693008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187981 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ae1096c8d0715dbfb3febcd6513f53933a300c28

---
+++ chrome/test/data/extensions/api_test/crazy_extension/background.js
@@ -0,0 +1,54 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This test is for bizarre things that extensions probably will do. We just
+// need to ensure that behaviour is predictable.
+
+chrome.test.runTests([
+  function accessNonexistentIframe() {
+    var iframe = document.createElement("iframe");
+    iframe.src = document.location;
+    var iteration = 0;
+    iframe.addEventListener('load', function() {
+      switch (iteration) {
+        case 0: {
+          // First test is that chrome.webstore doesn't even get defined if it
+          // was never accessed.
+          var iframeChrome = iframe.contentWindow.chrome;
+          document.body.removeChild(iframe);
+          chrome.test.assertEq(undefined, iframeChrome.webstore);
+          document.body.appendChild(iframe);
+          break;
+        }
+        case 1: {
+          // Second test is that it does if accessed before removal.
+          var iframeChrome = iframe.contentWindow.chrome;
+          var iframeChromeWebstore = iframeChrome.webstore;
+          chrome.test.assertTrue(typeof(iframeChromeWebstore) == 'object');
+          document.body.removeChild(iframe);
+          chrome.test.assertTrue(typeof(iframeChrome.webstore) == 'object');
+          document.body.appendChild(iframe);
+          break;
+        }
+        case 2: {
+          // Third test is that calling frame-dependent methods doesn't crash
+          // the renderer if the frame doesn't exist anymore.
+          var iframeChromeApp = iframe.contentWindow.chrome.app;
+          document.body.removeChild(iframe);
+          var exception = null;
+          try {
+            iframeChromeApp.getDetails();
+          } catch (e) {
+            exception = e;
+          }
+          chrome.test.assertEq("Extension view no longer exists", exception);
+          chrome.test.succeed();
+          break;
+        }
+      }
+      iteration++;
+    });
+    document.body.appendChild(iframe);
+  }
+]);
