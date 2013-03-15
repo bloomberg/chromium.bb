610807c6b842c26f2963a942d2e61482ba66a6b2
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 21:55:14 +0000

    [Telemetry] Re-enable image decoding benchmark.
    It was fixed by http://trac.webkit.org/changeset/145889.
    
    BUG=173184
    TEST=tools/perf/run_multipage_benchmarks --browser=system image_decoding_benchmark tools/perf/page_sets/image_decoding_benchmark.json
    
    Review URL: https://codereview.chromium.org/12767012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188483 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7080b0bb01560e830262697886e14dd43be42c96

--- tools/perf/perf_tools/image_decoding_benchmark.py
+++ tools/perf/perf_tools/image_decoding_benchmark.py
@@ -6,37 +6,29 @@ from telemetry.page import page_benchmark
 
 
 class ImageDecoding(page_benchmark.PageBenchmark):
-  # TODO(qinmin): uncomment this after we fix the image decoding benchmark
-  # for lazily decoded images
-  # def WillNavigateToPage(self, page, tab):
-  #   tab.StartTimelineRecording()
+  def WillNavigateToPage(self, page, tab):
+    tab.StartTimelineRecording()
 
   def MeasurePage(self, page, tab, results):
-    # TODO(qinmin): This android only test may fail after we switch to
-    # deferred image decoding and impl-side painting. Before we fix the test,
-    # temporarily disable calculation for lazily decoded images.
-    # Uncommented the following lines after we fix the timeline for lazily
-    # decoded images.
-    return
-    # tab.StopTimelineRecording()
-    # def _IsDone():
-    #   return tab.EvaluateJavaScript('isDone')
+    tab.StopTimelineRecording()
+    def _IsDone():
+      return tab.EvaluateJavaScript('isDone')
 
-    # decode_image_events = \
-    #     tab.timeline_model.GetAllOfName('DecodeImage')
+    decode_image_events = \
+        tab.timeline_model.GetAllOfName('DecodeImage')
 
     # If it is a real image benchmark, then store only the last-minIterations
     # decode tasks.
-    # if (hasattr(page,
-    #            'image_decoding_benchmark_limit_results_to_min_iterations') and
-    #     page.image_decoding_benchmark_limit_results_to_min_iterations):
-    #   assert _IsDone()
-    #   min_iterations = tab.EvaluateJavaScript('minIterations')
-    #   decode_image_events = decode_image_events[-min_iterations:]
+    if (hasattr(page,
+               'image_decoding_benchmark_limit_results_to_min_iterations') and
+        page.image_decoding_benchmark_limit_results_to_min_iterations):
+      assert _IsDone()
+      min_iterations = tab.EvaluateJavaScript('minIterations')
+      decode_image_events = decode_image_events[-min_iterations:]
 
-    # durations = [d.duration_ms for d in decode_image_events]
-    # if not durations:
-    #   results.Add('ImageDecoding_avg', 'ms', 'unsupported')
-    #   return
-    # image_decoding_avg = sum(durations) / len(durations)
-    # results.Add('ImageDecoding_avg', 'ms', image_decoding_avg)
+    durations = [d.duration_ms for d in decode_image_events]
+    if not durations:
+      results.Add('ImageDecoding_avg', 'ms', 'unsupported')
+      return
+    image_decoding_avg = sum(durations) / len(durations)
+    results.Add('ImageDecoding_avg', 'ms', image_decoding_avg)
--- tools/perf/perf_tools/image_decoding_benchmark_unittest.py
+++ tools/perf/perf_tools/image_decoding_benchmark_unittest.py
@@ -3,24 +3,21 @@
 # found in the LICENSE file.
 
 from telemetry.page import page_benchmark_unittest_base
-# from perf_tools import image_decoding_benchmark
+from perf_tools import image_decoding_benchmark
 
 
 class ImageDecodingBenchmarkUnitTest(
   page_benchmark_unittest_base.PageBenchmarkUnitTestBase):
 
   def testImageDecodingMeasurement(self):
-    # TODO(qinmin): uncomment this after we fix the image decoding benchmark
-    # for lazily decoded images
-    return
-    # ps = self.CreatePageSetFromFileInUnittestDataDir('image_decoding.html')
+    ps = self.CreatePageSetFromFileInUnittestDataDir('image_decoding.html')
 
-    # benchmark = image_decoding_benchmark.ImageDecoding()
-    # all_results = self.RunBenchmark(benchmark, ps)
+    benchmark = image_decoding_benchmark.ImageDecoding()
+    all_results = self.RunBenchmark(benchmark, ps)
 
-    # self.assertEqual(0, len(all_results.page_failures))
-    # self.assertEqual(1, len(all_results.page_results))
+    self.assertEqual(0, len(all_results.page_failures))
+    self.assertEqual(1, len(all_results.page_results))
 
-    # results0 = all_results.page_results[0]
-    # self.assertTrue('ImageDecoding_avg' in results0)
-    # self.assertTrue(results0['ImageDecoding_avg'] > 0)
+    results0 = all_results.page_results[0]
+    self.assertTrue('ImageDecoding_avg' in results0)
+    self.assertTrue(results0['ImageDecoding_avg'] > 0)
