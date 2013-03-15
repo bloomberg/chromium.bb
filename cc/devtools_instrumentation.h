b458dace8ac04c37ee3a59631a848be5b93cced4
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 12:20:55 +0000

    cc: Fix style issues in devtools_instrumentation.h
    R=danakj@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12788005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188359 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fcb7a77ab923510021e883ab5193b8911ece20b9

--- cc/devtools_instrumentation.h
+++ cc/devtools_instrumentation.h
@@ -17,8 +17,7 @@ const char kPaintLayer[] = "PaintLayer";
 const char kRasterTask[] = "RasterTask";
 }
 
-struct ScopedPaintLayer
-{
+struct ScopedPaintLayer {
   ScopedPaintLayer(int layer_id) {
     TRACE_EVENT_BEGIN1(internal::kCategory, internal::kPaintLayer,
         internal::kLayerId, layer_id);
@@ -26,10 +25,11 @@ struct ScopedPaintLayer
   ~ScopedPaintLayer() {
     TRACE_EVENT_END0(internal::kCategory, internal::kPaintLayer);
   }
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedPaintLayer);
 };
 
-struct ScopedRasterTask
-{
+struct ScopedRasterTask {
   ScopedRasterTask(int layer_id) {
     TRACE_EVENT_BEGIN1(internal::kCategory, internal::kRasterTask,
         internal::kLayerId, layer_id);
@@ -37,21 +37,24 @@ struct ScopedRasterTask
   ~ScopedRasterTask() {
     TRACE_EVENT_END0(internal::kCategory, internal::kRasterTask);
   }
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedRasterTask);
 };
 
 struct ScopedLayerObjectTracker
-    : public base::debug::TraceScopedTrackableObject<int>
-{
+    : public base::debug::TraceScopedTrackableObject<int> {
   ScopedLayerObjectTracker(int layer_id)
       : base::debug::TraceScopedTrackableObject<int>(
             internal::kCategory,
             internal::kLayerId,
             layer_id) {
   }
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedLayerObjectTracker);
 };
 
-}
-}
+}  // namespace devtools_instrumentation
+}  // namespace cc
 
 #endif
 
