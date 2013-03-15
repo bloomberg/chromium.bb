a6218782331abc9e2f64369d88faa251e70a31fb
chrisgao@chromium.org <chrisgao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 19:35:09 +0000

    [chromedriver] Add adb_commands.py to PATH. Moving adb_commands.py into subdirectory "chrome" caused error in android bot.
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/12893002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188453 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 38459808e53106cca57acfea28c00803d4da3be9

--- chrome/test/chromedriver/run_py_tests.py
+++ chrome/test/chromedriver/run_py_tests.py
@@ -27,6 +27,7 @@ from common import unittest_util
 from common import util
 
 if util.IsLinux():
+  sys.path.insert(0, os.path.join(_THIS_DIR, 'chrome'))
   sys.path.insert(0, os.path.join(_THIS_DIR, os.pardir, os.pardir, os.pardir,
                                   'build', 'android'))
   from pylib import android_commands
