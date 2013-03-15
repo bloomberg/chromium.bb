4c3fa79fd382dafeef4f5d63573a916cd38c2aa7
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 19:21:15 +0000

    [content shell] skip gpu data loading entirely instead of just ignoring the blacklist
    During layout tests, we use osmesa and not the gpu hardware
    
    BUG=190942,111316
    R=zmo@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/12891002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188446 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9aec60629ceb5924528606a851b157d0cc9f0e76

--- content/shell/shell_main_delegate.cc
+++ content/shell/shell_main_delegate.cc
@@ -108,7 +108,7 @@ bool ShellMainDelegate::BasicStartupComplete(int* exit_code) {
     command_line.AppendSwitchASCII(
         switches::kUseGL, gfx::kGLImplementationOSMesaName);
     SetAllowOSMesaImageTransportForTesting();
-    command_line.AppendSwitch(switches::kIgnoreGpuBlacklist);
+    command_line.AppendSwitch(switches::kSkipGpuDataLoading);
     command_line.AppendSwitch(switches::kEnableExperimentalWebKitFeatures);
     command_line.AppendSwitch(switches::kEnableCssShaders);
     command_line.AppendSwitchASCII(switches::kTouchEvents,
