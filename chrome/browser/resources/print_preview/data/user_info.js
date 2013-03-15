74025c77f050abb5e9c73d3cd0ad92fe38f5225b
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 16:08:11 +0000

    Removed unnecessary reference to interface.
    Review URL: https://chromiumcodereview.appspot.com/12842010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188408 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f7357d0b6cddbc164c9a1633ce54f308de422558

--- chrome/browser/resources/print_preview/data/user_info.js
+++ chrome/browser/resources/print_preview/data/user_info.js
@@ -22,14 +22,6 @@ cr.define('print_preview', function() {
     this.tracker_ = new EventTracker();
 
     /**
-     * Google Cloud Print interface to listen to for events. Currently, through
-     * Google Cloud Print is how we determine the info of the logged in user.
-     * @type {cloudprint.CloudPrintInterface}
-     * @private
-     */
-    this.cloudPrintInterface_ = null;
-
-    /**
      * Email address of the logged in user or {@code null} if no user is logged
      * in.
      * @type {?string}
@@ -62,9 +54,8 @@ cr.define('print_preview', function() {
      *     to Google Cloud Print that the print preview uses.
      */
     setCloudPrintInterface: function(cloudPrintInterface) {
-      this.cloudPrintInterface_ = cloudPrintInterface;
       this.tracker_.add(
-          this.cloudPrintInterface_,
+          cloudPrintInterface,
           cloudprint.CloudPrintInterface.EventType.SEARCH_DONE,
           this.onCloudPrintSearchDone_.bind(this));
     },
