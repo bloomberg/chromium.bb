d8aaa469f937a48c025f03d3c7dd70b98ff36beb
jinsukkim@google.com <jinsukkim@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-16 15:53:22 +0000

    Split cache dir between main and web-app activity.
    For the context in which the chrome runs in application
    mode, PathUtils.getCacheDirectory() returns the cache
    directory different from that of main chrome activity.
    
    BUG=181470
    
    Review URL: https://chromiumcodereview.appspot.com/12419005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188579 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 325a2027024bec2b967a9572d3136145a766c71c

--- base/android/java/src/org/chromium/base/PathUtils.java
+++ base/android/java/src/org/chromium/base/PathUtils.java
@@ -8,12 +8,16 @@ import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.os.Environment;
 
+import java.io.File;
+
 /**
  * This class provides the path related methods for the native library.
  */
 public abstract class PathUtils {
 
     private static String sDataDirectorySuffix;
+    private static String sWebappDirectorySuffix;
+    private static String sWebappCacheDirectory;
 
     // Prevent instantiation.
     private PathUtils() {}
@@ -26,6 +30,17 @@ public abstract class PathUtils {
      */
     public static void setPrivateDataDirectorySuffix(String suffix) {
         sDataDirectorySuffix = suffix;
+    }
+
+    /**
+     * Sets the directory info used for chrome process running in application mode.
+     *
+     * @param webappSuffix The suffix of the directory used for storing webapp-specific profile
+     * @param cacheDir Cache directory name for web apps.
+     */
+    public static void setWebappDirectoryInfo(String webappSuffix, String cacheDir) {
+        sWebappDirectorySuffix = webappSuffix;
+        sWebappCacheDirectory = cacheDir;
     }
 
     /**
@@ -46,7 +61,15 @@ public abstract class PathUtils {
     @SuppressWarnings("unused")
     @CalledByNative
     private static String getCacheDirectory(Context appContext) {
-        return appContext.getCacheDir().getPath();
+        if (ContextTypes.getInstance().getType(appContext) == ContextTypes.CONTEXT_TYPE_NORMAL) {
+            return appContext.getCacheDir().getPath();
+        }
+        if (sWebappDirectorySuffix == null || sWebappCacheDirectory == null) {
+            throw new IllegalStateException(
+                    "setWebappDirectoryInfo must be called before getCacheDirectory");
+        }
+        return new File(appContext.getDir(sWebappDirectorySuffix, appContext.MODE_PRIVATE),
+                sWebappCacheDirectory).getPath();
     }
 
     /**
