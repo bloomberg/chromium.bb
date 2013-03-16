ca9484b1457a4a46ad5c6192d4b194fb94822aca
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-16 05:15:37 +0000

    Give correct advice about the use of unsigned ints.
    Previous incorrect advice led people to write unsafe code.
    
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/12601015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188541 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3ec0d40b8bf06e1faa49a58733f6d3fd01ebbdbe

--- base/basictypes.h
+++ base/basictypes.h
@@ -32,11 +32,21 @@ typedef long                int64;
 typedef long long           int64;
 #endif
 
-// NOTE: unsigned types are DANGEROUS in loops and other arithmetical
-// places.  Use the signed types unless your variable represents a bit
-// pattern (eg a hash value) or you really need the extra bit.  Do NOT
-// use 'unsigned' to express "this value should always be positive";
+// NOTE: It is DANGEROUS to compare signed with unsigned types in loop
+// conditions and other conditional expressions, and it is DANGEROUS to
+// compute object/allocation sizes, indices, and offsets with signed types.
+// Integer overflow behavior for signed types is UNDEFINED in the C/C++
+// standards, but is defined for unsigned types.
+//
+// Use the unsigned types if your variable represents a bit pattern (e.g. a
+// hash value), object or allocation size, object count, offset,
+// array/vector index, etc.
+//
+// Do NOT use 'unsigned' to express "this value should always be positive";
 // use assertions for this.
+//
+// See the Chromium style guide for more information.
+// https://sites.google.com/a/chromium.org/dev/developers/coding-style
 
 typedef unsigned char      uint8;
 typedef unsigned short     uint16;
