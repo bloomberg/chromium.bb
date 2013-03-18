84d022727fcfb2e3a7c2d3dabda187ff14b35156
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-18 23:00:49 +0000

    cc chromification: render_pass_unittest.cc
    BUG=144577
    
    
    Review URL: https://chromiumcodereview.appspot.com/12655009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188866 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1b2f402028a151b2007769268673c3fe057aa9d1

--- cc/quads/render_pass_unittest.cc
+++ cc/quads/render_pass_unittest.cc
@@ -19,55 +19,59 @@ namespace cc {
 namespace {
 
 struct RenderPassSize {
-    // If you add a new field to this class, make sure to add it to the copy() tests.
-    RenderPass::Id m_id;
-    QuadList m_quadList;
-    SharedQuadStateList m_sharedQuadStateList;
-    gfx::Transform m_transformToRootTarget;
-    gfx::Rect m_outputRect;
-    gfx::RectF m_damageRect;
-    bool m_hasTransparentBackground;
-    bool m_hasOcclusionFromOutsideTargetSurface;
+  // If you add a new field to this class, make sure to add it to the copy() tests.
+  RenderPass::Id id;
+  QuadList quad_list;
+  SharedQuadStateList shared_quad_state_list;
+  gfx::Transform transform_to_root_target;
+  gfx::Rect output_rect;
+  gfx::RectF damage_rect;
+  bool has_transparent_background;
+  bool has_occlusion_from_outside_target_surface;
 };
 
-TEST(RenderPassTest, copyShouldBeIdenticalExceptIdAndQuads)
-{
-    RenderPass::Id id(3, 2);
-    gfx::Rect outputRect(45, 22, 120, 13);
-    gfx::Transform transformToRoot = gfx::Transform(1.0, 0.5, 0.5, -0.5, -1.0, 0.0);
-    gfx::Rect damageRect(56, 123, 19, 43);
-    bool hasTransparentBackground = true;
-    bool hasOcclusionFromOutsideTargetSurface = true;
+TEST(RenderPassTest, CopyShouldBeIdenticalExceptIdAndQuads) {
+  RenderPass::Id id(3, 2);
+  gfx::Rect output_rect(45, 22, 120, 13);
+  gfx::Transform transform_to_root =
+      gfx::Transform(1.0, 0.5, 0.5, -0.5, -1.0, 0.0);
+  gfx::Rect damage_rect(56, 123, 19, 43);
+  bool has_transparent_background = true;
+  bool has_occlusion_from_outside_target_surface = true;
 
-    scoped_ptr<TestRenderPass> pass = TestRenderPass::Create();
-    pass->SetAll(id,
-                 outputRect,
-                 damageRect,
-                 transformToRoot,
-                 hasTransparentBackground,
-                 hasOcclusionFromOutsideTargetSurface);
+  scoped_ptr<TestRenderPass> pass = TestRenderPass::Create();
+  pass->SetAll(id,
+               output_rect,
+               damage_rect,
+               transform_to_root,
+               has_transparent_background,
+               has_occlusion_from_outside_target_surface);
 
-    // Stick a quad in the pass, this should not get copied.
-    scoped_ptr<SharedQuadState> sharedState = SharedQuadState::Create();
-    sharedState->SetAll(gfx::Transform(), gfx::Size(), gfx::Rect(), gfx::Rect(), false, 1);
-    pass->AppendSharedQuadState(sharedState.Pass());
+  // Stick a quad in the pass, this should not get copied.
+  scoped_ptr<SharedQuadState> shared_state = SharedQuadState::Create();
+  shared_state->SetAll(
+      gfx::Transform(), gfx::Size(), gfx::Rect(), gfx::Rect(), false, 1);
+  pass->AppendSharedQuadState(shared_state.Pass());
 
-    scoped_ptr<CheckerboardDrawQuad> checkerboardQuad = CheckerboardDrawQuad::Create();
-    checkerboardQuad->SetNew(pass->shared_quad_state_list.back(), gfx::Rect(), SkColor());
-    pass->quad_list.push_back(checkerboardQuad.PassAs<DrawQuad>());
+  scoped_ptr<CheckerboardDrawQuad> checkerboard_quad =
+      CheckerboardDrawQuad::Create();
+  checkerboard_quad->SetNew(
+      pass->shared_quad_state_list.back(), gfx::Rect(), SkColor());
+  pass->quad_list.push_back(checkerboard_quad.PassAs<DrawQuad>());
 
-    RenderPass::Id newId(63, 4);
+  RenderPass::Id new_id(63, 4);
 
-    scoped_ptr<RenderPass> copy = pass->Copy(newId);
-    EXPECT_EQ(newId, copy->id);
-    EXPECT_RECT_EQ(pass->output_rect, copy->output_rect);
-    EXPECT_EQ(pass->transform_to_root_target, copy->transform_to_root_target);
-    EXPECT_RECT_EQ(pass->damage_rect, copy->damage_rect);
-    EXPECT_EQ(pass->has_transparent_background, copy->has_transparent_background);
-    EXPECT_EQ(pass->has_occlusion_from_outside_target_surface, copy->has_occlusion_from_outside_target_surface);
-    EXPECT_EQ(0u, copy->quad_list.size());
+  scoped_ptr<RenderPass> copy = pass->Copy(new_id);
+  EXPECT_EQ(new_id, copy->id);
+  EXPECT_RECT_EQ(pass->output_rect, copy->output_rect);
+  EXPECT_EQ(pass->transform_to_root_target, copy->transform_to_root_target);
+  EXPECT_RECT_EQ(pass->damage_rect, copy->damage_rect);
+  EXPECT_EQ(pass->has_transparent_background, copy->has_transparent_background);
+  EXPECT_EQ(pass->has_occlusion_from_outside_target_surface,
+            copy->has_occlusion_from_outside_target_surface);
+  EXPECT_EQ(0u, copy->quad_list.size());
 
-    EXPECT_EQ(sizeof(RenderPassSize), sizeof(RenderPass));
+  EXPECT_EQ(sizeof(RenderPassSize), sizeof(RenderPass));
 }
 
 }  // namespace
