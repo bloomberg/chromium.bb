b4f50e6f88f39750f323cef57b18847d36efdf69
kjyoun@google.com <kjyoun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 01:57:26 +0000

    Added missing header file
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/12779014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188915 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7fbd4a80183050fa570ba069965328063639a171

--- content/browser/renderer_host/pepper/pepper_truetype_font_list_android.cc
+++ content/browser/renderer_host/pepper/pepper_truetype_font_list_android.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/logging.h"
 #include "content/browser/renderer_host/pepper/pepper_truetype_font_list.h"
 
 namespace content {
