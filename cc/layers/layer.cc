77092c80a7f88b0772f34cb86e346a0f39888ef2
ajuma@chromium.org <ajuma@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 23:21:27 +0000

    Get UMA data for animations added to orphan layers
    We currently accelerate animations on orphan layers, though in the past
    we have had to disable this due to rendering glitches on some websites.
    Knowing how frequently animations are added to orphan layers will help
    us better evaluate the cost of disabling the acceleration of these
    animations, should we need to consider disabling them again.
    
    BUG=129683
    
    
    Review URL: https://chromiumcodereview.appspot.com/12965002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189456 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d1cddac8127c2888f24952e94b826330ad47efef

--- cc/layers/layer.cc
+++ cc/layers/layer.cc
@@ -4,6 +4,7 @@
 
 #include "cc/layers/layer.h"
 
+#include "base/metrics/histogram.h"
 #include "cc/animation/animation.h"
 #include "cc/animation/animation_events.h"
 #include "cc/animation/layer_animation_controller.h"
@@ -721,6 +722,8 @@ bool Layer::AddAnimation(scoped_ptr <Animation> animation) {
   if (!layer_animation_controller_->animation_registrar())
     return false;
 
+  UMA_HISTOGRAM_BOOLEAN("Renderer.AnimationAddedToOrphanLayer",
+                        !layer_tree_host_);
   layer_animation_controller_->AddAnimation(animation.Pass());
   SetNeedsCommit();
   return true;
