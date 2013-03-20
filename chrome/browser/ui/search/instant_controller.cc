4360e9d1adb358033f7f49629b759b95ced14325
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 00:57:03 +0000

    Revert 189142 "13.0% chromium-rel-win7-webkit/startup/shutdown/s..."
    Reverting speculative "fix".  The bots indicate a performance gain.  So I'm
    concluding that this code is indeed the culprit.  See:
    
      http://chromium-perf.appspot.com/?tab=chromium-rel-win7-webkit&graph=shutdown&trace=simple-user-quit&rev=189142&history=110&master=ChromiumWebkit&testSuite=startup_test&details=true
    
    Backing this out and will pursue a proper fix.
    
    
    > 13.0% chromium-rel-win7-webkit/startup/shutdown/simple-user-quit
    > 
    > Speculative "fix" for perf regression with Instant on Win7.  Will
    > revert once analysis is complete.
    > 
    > BUG=189163
    > TEST=performance_ui_tests --gtest_filter=ShutdownTest.SimpleUserQuit
    > TBR=sreeram@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/12594005
    
    TBR=dhollowa@chromium.org
    Review URL: https://codereview.chromium.org/12935005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189183 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 88519f51a5033f0d1118e451a6d21b70ba47a432

--- chrome/browser/ui/search/instant_controller.cc
+++ chrome/browser/ui/search/instant_controller.cc
@@ -239,9 +239,7 @@ InstantController::InstantController(chrome::BrowserInstantController* browser,
   // When the InstantController lives, the InstantService should live.
   // InstantService sets up profile-level facilities such as the ThemeSource for
   // the NTP.
-  // TODO(dhollowa): Temporarily disabling to diagnose perf regression.
-  // http://crbug.com/189163.
-  //   InstantServiceFactory::GetForProfile(browser_->profile());
+  InstantServiceFactory::GetForProfile(browser_->profile());
 }
 
 InstantController::~InstantController() {
