cdec2d1abc328c26d5b48ee10e1bd593453f84dd
vasilii@chromium.org <vasilii@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-21 11:39:33 +0000

    Fixed flaky FindDialogTest.CopyTextInIncognito. Original failure was caused by unreliable sending Ctrl-A command to the text box.
    BUG=196592
    
    
    Review URL: https://chromiumcodereview.appspot.com/12494017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189564 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dfa666153ff29108a29124a7a639ce7396638fec

--- chrome/browser/ui/find_bar/find_bar_interactive_uitest.cc
+++ chrome/browser/ui/find_bar/find_bar_interactive_uitest.cc
@@ -10,11 +10,13 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/find_bar/find_bar_controller.h"
+#include "chrome/browser/ui/find_bar/find_bar_state.h"
+#include "chrome/browser/ui/find_bar/find_bar_state_factory.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
 #include "ui/base/clipboard/clipboard.h"
-#include "ui/base/events/event_constants.h"
 
 #if defined(OS_MACOSX)
 #include "base/mac/scoped_nsautorelease_pool.h"
@@ -22,24 +24,22 @@
 
 namespace {
 
-#if defined(OS_MACOSX)
-const int kCtrlOrCmdMask = ui::EF_COMMAND_DOWN;
-#else
-const int kCtrlOrCmdMask = ui::EF_CONTROL_DOWN;
-#endif
-
-void SendKeyForBrowser(const Browser* browser,
-                       ui::KeyboardCode key,
-                       int modifiers) {
-  ASSERT_TRUE(ui_test_utils::SendKeyPressSync(
-      browser, key,
-      (modifiers & ui::EF_CONTROL_DOWN) != 0,
-      (modifiers & ui::EF_SHIFT_DOWN) != 0,
-      (modifiers & ui::EF_ALT_DOWN) != 0,
-      (modifiers & ui::EF_COMMAND_DOWN) != 0));
+int FindInPage(Browser* browser,
+               const string16& search_str16,
+               bool forward,
+               bool case_sensitive,
+               int* ordinal) {
+  browser->GetFindBarController()->find_bar()->SetFindText(search_str16);
+  return ui_test_utils::FindInPage(
+      browser->tab_strip_model()->GetActiveWebContents(),
+      search_str16,
+      forward,
+      case_sensitive,
+      ordinal,
+      NULL);
 }
 
-}  // namespace
+}
 
 class FindDialogTest : public InProcessBrowserTest {
  protected:
@@ -62,30 +62,34 @@ class FindDialogTest : public InProcessBrowserTest {
 
 // Copy text in Incognito window, close window, check the test disappeared from
 // the clipboard.
-// http://crbug.com/196592
-#if defined(OS_WIN)
-#define MAYBE_CopyTextInIncognito DISABLED_CopyTextInIncognito
-#else
-#define MAYBE_CopyTextInIncognito CopyTextInIncognito
-#endif
-IN_PROC_BROWSER_TEST_F(FindDialogTest, MAYBE_CopyTextInIncognito) {
+IN_PROC_BROWSER_TEST_F(FindDialogTest, CopyTextInIncognito) {
   EXPECT_FALSE(browser()->profile()->IsOffTheRecord());
   Browser* browser_incognito = CreateIncognitoBrowser();
   ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser_incognito));
+  const string16 find_text(ASCIIToUTF16("foo"));
+
+#if !defined(OS_MACOSX)
+  // Set prepopulated text, find bar will appear with this text being selected.
+  // Mac ignores this and reads Find pasteboard instead.
+  FindBarState* find_bar_state =
+      FindBarStateFactory::GetForProfile(browser_incognito->profile());
+  find_bar_state->set_last_prepopulate_text(find_text);
+#endif
+
   chrome::ShowFindBar(browser_incognito);
+
+#if defined(OS_MACOSX)
+  FindInPage(browser_incognito, find_text, true, true, NULL);
+#endif
+
   ASSERT_TRUE(ui_test_utils::IsViewFocused(browser_incognito,
                                            VIEW_ID_FIND_IN_PAGE_TEXT_FIELD));
-  string16 text(ASCIIToUTF16("foo"));
-  browser_incognito->GetFindBarController()->find_bar()->SetFindText(text);
-
-  // Select and copy.
-  ASSERT_NO_FATAL_FAILURE(SendKeyForBrowser(browser_incognito,
-                                            ui::VKEY_A,
-                                            kCtrlOrCmdMask));
   EXPECT_TRUE(chrome::ExecuteCommand(browser_incognito, IDC_COPY));
   ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
-  EXPECT_TRUE(clipboard->IsFormatAvailable(
-      ui::Clipboard::GetPlainTextFormatType(), ui::Clipboard::BUFFER_STANDARD));
+  string16 clipboard_str;
+  clipboard->ReadText(ui::Clipboard::BUFFER_STANDARD, &clipboard_str);
+  // Make sure the text is copied successfully.
+  EXPECT_EQ(find_text, clipboard_str);
 
   CloseBrowser(browser_incognito);
   EXPECT_FALSE(clipboard->IsFormatAvailable(
