5b7824ec930a8372660b0f7594936e130984e4d0
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-21 16:52:59 +0000

    Remove DCHECK() for being current in GLSurface::GetExtensions().
    The DCHECK() does not play nicely with virtual contexts, and since there is no need for the context/surface to be current in the current code (the derived classes just add static extensions strings), I'm just removing it. 
    
    BUG=180575
    NOTRY=True
    
    
    Review URL: https://chromiumcodereview.appspot.com/12621005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189631 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 367ca6e556eb8322426f2f51e4a12513f9a9230d

--- ui/gl/gl_surface.cc
+++ ui/gl/gl_surface.cc
@@ -89,10 +89,6 @@ bool GLSurface::DeferDraws() {
 }
 
 std::string GLSurface::GetExtensions() {
-  // Use of GLSurfaceAdapter class means that we can't compare
-  // GetCurrent() and this directly.
-  DCHECK(GetCurrent()->GetHandle() == GetHandle() ||
-         GetBackingFrameBufferObject());
   return std::string("");
 }
 
