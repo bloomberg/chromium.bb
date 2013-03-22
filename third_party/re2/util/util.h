b22a998e617840b391adabf19f14096f71cf0861
eugenis@chromium.org <eugenis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 09:26:14 +0000

    Fix re2 build with libcxx.
    BUG=178409
    
    R=battre@chromium.org,thakis@chromium.org
    TBR=cpu@chromium.org
    
    Review URL: https://codereview.chromium.org/12951002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189776 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1f24d9cc531ddecebe035ef1f90321cfe81903a4

--- third_party/re2/util/util.h
+++ third_party/re2/util/util.h
@@ -46,7 +46,8 @@ using std::sort;
 using std::swap;
 using std::make_pair;
 
-#if defined(__GNUC__) && !defined(USE_CXX0X) && !defined(OS_ANDROID)
+#if defined(__GNUC__) && !defined(USE_CXX0X) && !defined(OS_ANDROID) && \
+    !defined(_LIBCPP_ABI_VERSION)
 
 #include <tr1/unordered_set>
 using std::tr1::unordered_set;
