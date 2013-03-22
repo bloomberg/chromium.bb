3f4b6fa62f304e6491556f36e42cc75e52c2d997
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 17:46:42 +0000

    Disable MalwareRedirectInIFrameCanceled for win aura.
    Repeatedly failing on trybots:
    http://build.chromium.org/p/tryserver.chromium/builders/win7_aura/builds/22730
    http://build.chromium.org/p/tryserver.chromium/builders/win7_aura/builds/22731
    http://build.chromium.org/p/tryserver.chromium/builders/win7_aura/builds/22732
    
    BUG=154081
    R=maruel@chromium.org
    TBR=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/12578025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189846 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0963b398e5c365c8fd3c4249efec2cd409a43abf

--- chrome/browser/safe_browsing/safe_browsing_blocking_page_test.cc
+++ chrome/browser/safe_browsing/safe_browsing_blocking_page_test.cc
@@ -591,7 +591,8 @@ class SafeBrowsingBlockingPageTest : public InProcessBrowserTest {
 };
 
 // TODO(linux_aura) http://crbug.com/163931
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(USE_AURA)
+// TODO(win_aura) http://crbug.com/154081
+#if defined(USE_AURA) && !defined(OS_CHROMEOS)
 #define MAYBE_MalwareRedirectInIFrameCanceled DISABLED_MalwareRedirectInIFrameCanceled
 #else
 #define MAYBE_MalwareRedirectInIFrameCanceled MalwareRedirectInIFrameCanceled
