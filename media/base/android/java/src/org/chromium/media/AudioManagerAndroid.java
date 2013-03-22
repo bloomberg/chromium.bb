6d48f0d149ffe207152c3ce7874d6c34fb262a2e
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 18:46:39 +0000

    Add speaker on/off control on Android for WebRTC
    Description: 
    Application listens to the headset plug intent, turns on or off speaker when 
    webrtc is running. It will help devices with dual mics to improve audio 
    quality. 
    
    BUG=222394
    
    Contributed by leozwang@chromium.org (https://chromiumcodereview.appspot.com/12974004/)
    Review URL: https://codereview.chromium.org/12702031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189870 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9790156eba13917f6b68f90fdc749c5958ee3f30

--- media/base/android/java/src/org/chromium/media/AudioManagerAndroid.java
+++ media/base/android/java/src/org/chromium/media/AudioManagerAndroid.java
@@ -4,7 +4,10 @@
 
 package org.chromium.media;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioManager;
 
 import org.chromium.base.CalledByNative;
@@ -12,12 +15,54 @@ import org.chromium.base.JNINamespace;
 
 @JNINamespace("media")
 class AudioManagerAndroid {
+    private static final String TAG = AudioManagerAndroid.class.getSimpleName();
+
+    private final AudioManager mAudioManager;
+    private final Context mContext;
+
+    private BroadcastReceiver mReceiver;
+    private boolean mOriginalSpeakerStatus;
+
     @CalledByNative
-    public static void setMode(Context context, int mode) {
-        AudioManager audioManager =
-                (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
-        if (null != audioManager) {
-            audioManager.setMode(mode);
+    public void setMode(int mode) {
+        mAudioManager.setMode(mode);
+    }
+
+    @CalledByNative
+    private static AudioManagerAndroid createAudioManagerAndroid(Context context) {
+        return new AudioManagerAndroid(context);
+    }
+
+    private AudioManagerAndroid(Context context) {
+        mContext = context;
+        mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
+    }
+
+    @CalledByNative
+    public void registerHeadsetReceiver() {
+        if (mReceiver != null) {
+            return;
         }
+
+        mOriginalSpeakerStatus = mAudioManager.isSpeakerphoneOn();
+        IntentFilter filter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
+
+        mReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (Intent.ACTION_HEADSET_PLUG.equals(intent.getAction())) {
+                    mAudioManager.setSpeakerphoneOn(
+                            intent.getIntExtra("state", 0) == 0);
+                }
+            }
+        };
+        mContext.registerReceiver(mReceiver, filter);
+    }
+
+    @CalledByNative
+    public void unregisterHeadsetReceiver() {
+        mContext.unregisterReceiver(mReceiver);
+        mReceiver = null;
+        mAudioManager.setSpeakerphoneOn(mOriginalSpeakerStatus);
     }
 }
