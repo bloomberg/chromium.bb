6abf847a16c552af089316646975c67af5632db9
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 10:11:28 +0000

    libxml: Clean up two redundant comparisons.
    This was already fixed upstream.
    
    Found by experimenting with a new Clang warning.
    
    BUG=none
    TBR=cpu
    
    Review URL: https://codereview.chromium.org/12952004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189789 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ddb63118e3379bc4a5237db217843c624267af2d

--- third_party/libxml/src/xmlreader.c
+++ third_party/libxml/src/xmlreader.c
@@ -1392,8 +1392,7 @@ get_next_node:
 #endif
 	    (reader->entNr == 0) &&
 	    (reader->node->prev != NULL) &&
-            (reader->node->prev->type != XML_DTD_NODE) &&
-	    (reader->entNr == 0)) {
+	    (reader->node->prev->type != XML_DTD_NODE)) {
 	    xmlNodePtr tmp = reader->node->prev;
 	    if ((tmp->extra & NODE_IS_PRESERVED) == 0) {
 		xmlUnlinkNode(tmp);
@@ -1442,8 +1441,7 @@ get_next_node:
 #endif
 	    (reader->entNr == 0) &&
 	    (oldnode->type != XML_DTD_NODE) &&
-	    ((oldnode->extra & NODE_IS_PRESERVED) == 0) &&
-	    (reader->entNr == 0)) {
+	    ((oldnode->extra & NODE_IS_PRESERVED) == 0)) {
 	    xmlUnlinkNode(oldnode);
 	    xmlTextReaderFreeNode(reader, oldnode);
 	}
