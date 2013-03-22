f44dc26efadd362a362ccbfdf91e78a3c704db9b
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-03-22 23:55:28 +0000

    PNaCl: Enable the ExpandVarArgs pass for ABI simplification
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3338
    TEST=PNaCl toolchain trybots
    
    Review URL: https://codereview.chromium.org/12674041
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11044 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 2297d492390045383041c1bbb1bab7f77258f7fe

--- pnacl/driver/pnacl-ld.py
+++ pnacl/driver/pnacl-ld.py
@@ -336,13 +336,15 @@ def main(argv):
     chain = DriverChain(inputs, output, tng)
     chain.add(LinkBC, 'pre_opt.' + bitcode_type)
 
-    if env.getbool('STATIC') and len(native_objects) == 0:
-      # ABI simplification passes.  These two passes assume the whole
-      # program is available and should not be used if we are linking
-      # .o files, otherwise they will drop constructors and leave TLS
-      # variables unconverted.
-      chain.add(DoLLVMPasses(['-nacl-expand-ctors', '-nacl-expand-tls']),
-                'expand_features.' + bitcode_type)
+    if env.getbool('STATIC'):
+      # ABI simplification passes.
+      passes = ['-expand-varargs']
+      # These two passes assume the whole program is available and
+      # should not be used if we are linking .o files, otherwise they
+      # will drop constructors and leave TLS variables unconverted.
+      if len(native_objects) == 0:
+        passes.extend(['-nacl-expand-ctors', '-nacl-expand-tls'])
+      chain.add(DoLLVMPasses(passes), 'expand_features.' + bitcode_type)
 
     if env.getone('OPT_LEVEL') != '' and env.getone('OPT_LEVEL') != '0':
       chain.add(DoOPT, 'opt.' + bitcode_type)
