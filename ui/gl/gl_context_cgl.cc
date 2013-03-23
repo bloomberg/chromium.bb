426f80b9dde3f53ff2e5e5459ff101fee8c74aef
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-23 22:12:02 +0000

    Wait some time before switching to integrated GPU.
    Switching between the discrete GPU and integrated GPU is expensive (and probably buggy), so delay switching back from the discrete GPU to the integrated GPU for 10 seconds after a context is deleted in case a new context will soon request the discrete GPU.
    
    BUG=156986
    
    
    Review URL: https://chromiumcodereview.appspot.com/12619009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190096 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ac64d6fe6fcd98b877f2dc87469ef530968c2964

--- ui/gl/gl_context_cgl.cc
+++ ui/gl/gl_context_cgl.cc
@@ -109,7 +109,12 @@ bool GLContextCGL::Initialize(GLSurface* compatible_surface,
 
 void GLContextCGL::Destroy() {
   if (discrete_pixelformat_) {
-    CGLReleasePixelFormat(discrete_pixelformat_);
+    // Delay releasing the pixel format for 10 seconds to reduce the number of
+    // unnecessary GPU switches.
+    MessageLoop::current()->PostDelayedTask(FROM_HERE,
+                                            base::Bind(&CGLReleasePixelFormat,
+                                                       discrete_pixelformat_),
+                                            base::TimeDelta::FromSeconds(10));
     discrete_pixelformat_ = NULL;
   }
   if (context_) {
