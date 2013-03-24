4565c063062c18494d72a49f7e54ede909b012a7
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-24 10:01:06 +0000

    Improve logging of interactions with the RDP ActiveX control.
    This CL makes the following improvements:
      - Added "RDP" prefix to all log messages around the RDP ActiveX control
      - The server endpoint is printed out for connect and disconnect messages.
      - The error message describing the disconnection reason is printed out if the connection was interrupted by an error.
    
    BUG=137696
    
    
    Review URL: https://chromiumcodereview.appspot.com/12998002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190252 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 919f9458ca72f009295873ec735caebcfe33a56f

--- remoting/host/win/rdp_client_window.cc
+++ remoting/host/win/rdp_client_window.cc
@@ -10,6 +10,13 @@
 #include "base/utf_string_conversions.h"
 #include "base/win/scoped_bstr.h"
 
+// RDP connection disconnect reasons codes that should not be interpreted as
+// errors.
+const long kDisconnectReasonNoInfo = 0;
+const long kDisconnectReasonLocalNotError = 1;
+const long kDisconnectReasonRemoteByUser = 2;
+const long kDisconnectReasonByServer = 3;
+
 namespace remoting {
 
 RdpClientWindow::RdpClientWindow(const net::IPEndPoint& server_endpoint,
@@ -177,8 +184,9 @@ LRESULT RdpClientWindow::OnCreate(CREATESTRUCT* create_struct) {
 
 done:
   if (FAILED(result)) {
-    LOG(ERROR) << "Failed to start an RDP connection: error=" << std::hex
-               << result << std::dec;
+    LOG(ERROR) << "RDP: failed to initiate a connection to "
+               << server_endpoint_.ToString() << ": error="
+               << std::hex << result << std::dec;
     client_.Release();
     client_settings_.Release();
     return -1;
@@ -193,42 +201,50 @@ void RdpClientWindow::OnDestroy() {
 }
 
 HRESULT RdpClientWindow::OnConnected() {
-  VLOG(3) << "The RDP client control has established a connection";
+  VLOG(1) << "RDP: successfully connected to " << server_endpoint_.ToString();
 
   NotifyConnected();
   return S_OK;
 }
 
 HRESULT RdpClientWindow::OnDisconnected(long reason) {
-  // Log the disconnect reason and extended code.
+  if (reason == kDisconnectReasonNoInfo ||
+      reason == kDisconnectReasonLocalNotError ||
+      reason == kDisconnectReasonRemoteByUser ||
+      reason == kDisconnectReasonByServer) {
+    VLOG(1) << "RDP: disconnected from " << server_endpoint_.ToString()
+            << ", reason=" << reason;
+    NotifyDisconnected();
+    return S_OK;
+  }
+
+  // Get the extended disconnect reason code.
   mstsc::ExtendedDisconnectReasonCode extended_code;
   HRESULT result = client_->get_ExtendedDisconnectReason(&extended_code);
   if (FAILED(result))
     extended_code = mstsc::exDiscReasonNoInfo;
 
-  VLOG(1) << "The RDP client control has been disconnected: reason=" << reason
-          << ", extended_code=" << extended_code;
-
-  // Try to log the error message as well.
-  if (extended_code != mstsc::exDiscReasonNoInfo) {
-    base::win::ScopedComPtr<mstsc::IMsRdpClient5> client5;
-    result = client_.QueryInterface(client5.Receive());
-    if (SUCCEEDED(result)) {
-      base::win::ScopedBstr error_message;
-      reason = client5->GetErrorDescription(reason, extended_code,
-                                            error_message.Receive());
-      if (SUCCEEDED(result)) {
-        VLOG(1) << "  error_message=" << error_message;
-      }
-    }
+  // Get the error message as well.
+  base::win::ScopedBstr error_message;
+  base::win::ScopedComPtr<mstsc::IMsRdpClient5> client5;
+  result = client_.QueryInterface(client5.Receive());
+  if (SUCCEEDED(result)) {
+    result = client5->GetErrorDescription(reason, extended_code,
+                                          error_message.Receive());
+    if (FAILED(result))
+      error_message.Reset();
   }
+
+  LOG(ERROR) << "RDP: disconnected from " << server_endpoint_.ToString()
+             << ": " << error_message << " (reason=" << reason
+             << ", extended_code=" << extended_code << ")";
 
   NotifyDisconnected();
   return S_OK;
 }
 
 HRESULT RdpClientWindow::OnFatalError(long error_code) {
-  LOG(ERROR) << "An error occured in the RDP client control: error_code="
+  LOG(ERROR) << "RDP: an error occured: error_code="
              << error_code;
 
   NotifyDisconnected();
