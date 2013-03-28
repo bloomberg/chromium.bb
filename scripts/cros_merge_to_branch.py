da60bbd0fa65ae2101c6d7861ed2380e78a4c2be
Mike Frysinger <vapier@chromium.org>
2013-03-27 23:57:11 -0400

    cros_merge_to_branch: tighten up help output
    No functional changes.
    
    BUG=None
    TEST=`cros_merge_to_branch -h` looks nice
    
    Change-Id: Idf2109b91033d957f4c907fb661e7de015eeb09c
    Reviewed-on: https://gerrit.chromium.org/gerrit/46743
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- scripts/cros_merge_to_branch.py
+++ scripts/cros_merge_to_branch.py
@@ -4,7 +4,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-""" This simple program takes changes from gerrit/gerrit-int and creates new
+"""This simple program takes changes from gerrit/gerrit-int and creates new
 changes for them on the desired branch using your gerrit/ssh credentials. To
 specify a change on gerrit-int, you must prefix the change with a *.
 
@@ -31,8 +31,7 @@ from gerrit-int, you must prefix the gerrit change number with a * e.g.
 
 For more information on how to do this yourself you can go here:
 http://dev.chromium.org/chromium-os/how-tos-and-troubleshooting/working-on-a-br\
-anch
-"""
+anch"""
 
 import errno
 import logging
@@ -52,7 +51,7 @@ from chromite.lib import patch as cros_patch
 
 
 _USAGE = """
-cros_merge_to_branch [*]change_number1 [[*]change_number2 ...] branch\n\n %s
+cros_merge_to_branch [*]change_number1 [[*]change_number2 ...] branch\n\n%s\
 """ % __doc__
 
 
@@ -60,19 +59,19 @@ def _GetParser():
   """Returns the parser to use for this module."""
   parser = commandline.OptionParser(usage=_USAGE)
   parser.add_option('-d', '--draft', default=False, action='store_true',
-                    help='Upload a draft to Gerrit rather than a change.')
+                    help='upload a draft to Gerrit rather than a change')
   parser.add_option('-n', '--dry-run', default=False, action='store_true',
                     dest='dryrun',
-                    help='Apply changes locally but do not upload them.')
+                    help='apply changes locally but do not upload them')
   parser.add_option('-e', '--email',
-                    help='If specified, use this email instead of '
-                    'the email you would upload changes as. Must be set if '
-                    'nomirror is set.')
+                    help='if specified, use this email instead of '
+                    'the email you would upload changes as; must be set w/'
+                    '--nomirror')
   parser.add_option('--nomirror', default=True, dest='mirror',
-                    action='store_false', help='Disable mirroring -- requires '
-                    'email to be set.')
+                    action='store_false', help='checkout git repo directly; '
+                    'requires --email')
   parser.add_option('--nowipe', default=True, dest='wipe', action='store_false',
-                    help='Do not wipe the work directory after finishing.')
+                    help='do not wipe the work directory after finishing')
   return parser
 
 
