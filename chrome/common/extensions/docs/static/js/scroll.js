1965a947fd219ae59df8a9db3da97e5bb18497d8
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 21:56:58 +0000

    Devsite: only make the sidebar float if it's big enough to fit in the window. If not, show a scroll-to-top button.
    BUG=225685
    
    Review URL: https://codereview.chromium.org/13465005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191905 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 04e5f79340f005331f9edb4fed18e5fd2fbfada7

--- chrome/common/extensions/docs/static/js/scroll.js
+++ chrome/common/extensions/docs/static/js/scroll.js
@@ -5,24 +5,37 @@
 // Scroll handling.
 //
 // Switches the sidebar between floating on the left and position:fixed
-// depending on whether it's scrolled into view.
+// depending on whether it's scrolled into view, and manages the scroll-to-top
+// button: click logic, and when to show it.
 (function() {
 
 var sidebar = document.getElementById('gc-sidebar');
+var scrollToTop = document.getElementById('scroll-to-top');
 var offsetTop = sidebar.offsetTop;
 
 window.addEventListener('scroll', function() {
   // Obviously, this code executes every time the window scrolls, so avoid
   // putting things in here.
-  if (sidebar.classList.contains('floating')) {
-    if (window.scrollY < offsetTop)
-      sidebar.classList.remove('floating');
-  } else {
-    if (window.scrollY > offsetTop) {
-      sidebar.classList.add('floating');
-      sidebar.scrollTop = 0;
+  var floatSidebar = false;
+  var showScrollToTop = false;
+
+  if (window.scrollY > offsetTop) {
+    // Scrolled past the top of the sidebar.
+    if (window.innerHeight >= sidebar.offsetHeight) {
+      // The whole sidebar fits in the window. Make it always visible.
+      floatSidebar = true;
+    } else {
+      // Whole sidebar doesn't fit, so show the scroll-to-top button instead.
+      showScrollToTop = true;
     }
   }
+
+  sidebar.classList.toggle('floating', floatSidebar);
+  scrollToTop.classList.toggle('hidden', !showScrollToTop);
+});
+
+scrollToTop.addEventListener('click', function() {
+  window.scrollTo(0, 0);
 });
 
 }());
