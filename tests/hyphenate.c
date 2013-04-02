4d30562e944bfb6b88b3d7a8650856350c64b683
Mesar Hameed <mesar.hameed@gmail.com>
2013-04-02 19:25:39 +0000

    tests/hyphenate.c: update table name.

--- tests/hyphenate.c
+++ tests/hyphenate.c
@@ -2,5 +2,5 @@
 
 int main(int argc, char **argv)
 {
-  return check_hyphenation("en-us-g1.utb,hyph_en_US.dic", "straightforward", "010000001001000");
+  return check_hyphenation("en-us-g1.ctb,hyph_en_US.dic", "straightforward", "010000001001000");
 }
