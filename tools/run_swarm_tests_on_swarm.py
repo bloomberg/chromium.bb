eb7d0d36a42c70fd7d44a6ccac22d12bad966f7e
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-03 21:35:35 +0000

    Make run_swarm_tests_on_swarm.py run everything in parallel.
    Use output similar to run_test_cases.py. It gives numbers against which we can
    optimize. Some tests take over 2m30s so there is room for optimization (or
    increase the Swarm slave pool). Note that running the presubmit checks locally
    take less than 60s total so there is room for improvement.
    
    R=csharp@chromium.org
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/13587003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/swarm_client@192166 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/run_swarm_tests_on_swarm.py
+++ tools/run_swarm_tests_on_swarm.py
@@ -9,16 +9,168 @@ import datetime
 import glob
 import getpass
 import hashlib
+import logging
 import optparse
 import os
 import shutil
 import subprocess
 import sys
 import tempfile
+import time
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
+sys.path.insert(0, ROOT_DIR)
+
+import run_test_cases
+
+
+# Mapping of the sys.platform value into Swarm OS value.
 OSES = {'win32': 'win', 'linux2': 'linux', 'darwin': 'mac'}
+
+
+class Runner(object):
+  def __init__(self, isolate_server, swarm_server, add_task, progress, tempdir):
+    self.isolate_server = isolate_server
+    self.swarm_server = swarm_server
+    self.add_task = add_task
+    self.progress = progress
+    self.tempdir = tempdir
+    self.prefix = (
+        getpass.getuser() + '-' + datetime.datetime.now().isoformat() + '-')
+
+  @staticmethod
+  def _call(args):
+    start = time.time()
+    proc = subprocess.Popen(
+        [sys.executable] + args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        cwd=ROOT_DIR)
+    stdout = proc.communicate()[0]
+    return proc.returncode, stdout, time.time() - start
+
+  def archive(self, test, platform):
+    # Put the .isolated files in a temporary directory. This is simply done so
+    # the current directory doesn't have the following files created:
+    # - swarm_client_tests.isolated
+    # - swarm_client_tests.isolated.state
+    test_name = os.path.basename(test)
+    handle, isolated = tempfile.mkstemp(
+        dir=self.tempdir, prefix='run_swarm_tests_on_swarm_',
+        suffix='.isolated')
+    os.close(handle)
+    try:
+      returncode, stdout, duration = self._call(
+          [
+            'isolate.py',
+            'hashtable',
+            '--isolate', os.path.join(BASE_DIR, 'run_a_test.isolate'),
+            '--isolated', isolated,
+            '--outdir', self.isolate_server,
+            '--variable', 'TEST_EXECUTABLE', test,
+            '--variable', 'OS', OSES[platform],
+          ])
+      step_name = '%s/%s (%3.2fs)' % (platform, test_name, duration)
+      if returncode:
+        self.progress.update_item(
+            'Failed to archive %s\n%s' % (step_name, stdout), True, False)
+      else:
+        hash_value = hashlib.sha1(open(isolated, 'rb').read()).hexdigest()
+        logging.info('%s: %s', step_name, hash_value)
+        self.progress.update_item('Archived %s' % step_name, True, False)
+        self.add_task(0, self.trigger, test, platform, hash_value)
+    finally:
+      try:
+        os.remove(isolated)
+      except OSError:
+        logging.debug('%s was already deleted', isolated)
+    return None
+
+  def trigger(self, test, platform, hash_value):
+    test_name = os.path.basename(test)
+    returncode, stdout, duration = self._call(
+        [
+          'swarm_trigger_step.py',
+          '--os_image', platform,
+          '--swarm-url', self.swarm_server,
+          '--test-name-prefix', self.prefix,
+          '--data-server', self.isolate_server,
+          '--run_from_hash',
+          # Isolated hash.
+          hash_value,
+          # Test name.
+          'swarm_client_tests_%s_%s' % (platform, test_name),
+          # Number of shards.
+          '1',
+          # test filter.
+          '*',
+        ])
+    step_name = '%s/%s (%3.2fs)' % (platform, test_name, duration)
+    if returncode:
+      self.progress.update_item(
+          'Failed to trigger %s\n%s' % (step_name, stdout), True, False)
+    else:
+      self.progress.update_item('Triggered %s' % step_name, True, False)
+      self.add_task(0, self.get_result, test, platform)
+    return None
+
+  def get_result(self, test, platform):
+    test_name = os.path.basename(test)
+    name = '%s_%s' % (platform, test_name)
+    returncode, stdout, duration = self._call(
+        [
+          'swarm_get_results.py',
+          '--url', self.swarm_server,
+          self.prefix + 'swarm_client_tests_' + name,
+        ])
+    step_name = '%s/%s (%3.2fs)' % (platform, test_name, duration)
+    # Only print the output for failures, successes are unexciting.
+    if returncode:
+      self.progress.update_item(
+          'Failed %s:\n%s' % (step_name, stdout), True, False)
+      return (test_name, platform, stdout)
+    self.progress.update_item('Passed %s' % step_name, True, False)
+    return None
+
+
+def run_swarm_tests_on_swarm(oses, tests, logs, isolate_server, swarm_server):
+  runs = len(tests) * len(oses)
+  total = 3 * runs
+  progress = run_test_cases.Progress(total)
+  progress.use_cr_only = False
+  tempdir = tempfile.mkdtemp(prefix='swarm_client_tests')
+  try:
+    with run_test_cases.ThreadPool(progress, runs, runs, total) as pool:
+      start = time.time()
+      runner = Runner(
+          isolate_server, swarm_server, pool.add_task, progress, tempdir)
+      for test in tests:
+        for platform in oses:
+          pool.add_task(0, runner.archive, test, platform)
+
+      failed_tests = pool.join()
+      duration = time.time() - start
+      print('')
+  finally:
+    shutil.rmtree(tempdir)
+
+  if logs:
+    for test, platform, stdout in failed_tests:
+      name = '%s_%s' % (platform, os.path.basename(test))
+      with open(os.path.join(logs, name + '.log'), 'wb') as f:
+        f.write(stdout)
+
+  print('Completed in %3.2fs' % duration)
+  if failed_tests:
+    failed_tests_per_os = {}
+    for test, platform, _ in failed_tests:
+      failed_tests_per_os.setdefault(test, []).append(platform)
+    print('Detected the following failures:')
+    for test, platforms in failed_tests_per_os.iteritems():
+      print('  %s on %s' % (test, ', '.join(sorted(platforms))))
+  return bool(failed_tests)
 
 
 def main():
@@ -43,14 +195,14 @@ def main():
   if options.verbose:
     os.environ['ISOLATE_DEBUG'] = '1'
 
-  prefix = getpass.getuser() + '-' + datetime.datetime.now().isoformat() + '-'
+  logging.basicConfig(level=logging.DEBUG if options.verbose else logging.ERROR)
 
   # Note that the swarm and the isolate code use different strings for the
   # different oses.
-  oses = OSES.copy()
+  oses = OSES.keys()
   tests = [
-    os.path.relpath(i, ROOT_DIR)
-    for i in glob.iglob(os.path.join(ROOT_DIR, '..', 'tests', '*_test.py'))
+    os.path.relpath(i, BASE_DIR)
+    for i in glob.iglob(os.path.join(ROOT_DIR, 'tests', '*_test.py'))
   ]
 
   if options.test:
@@ -61,114 +213,26 @@ def main():
             options.test, '\n'.join('  ' + i for i in valid_tests)))
     tests = [t for t in tests if t.endswith(os.path.sep + options.test)]
 
-  on_windows = sys.platform in ('win32', 'cygwin')
-  if on_windows:
-    # If we are on Windows, don't generate the tests for Linux and Mac since
-    # they use symlinks and we can't create symlinks on windows.
-    oses = {'win32': 'win'}
-
   if options.os:
     if options.os not in oses:
       parser.error(
           '--os %s is unknown. Valid values are %s' % (
             options.os, ', '.join(sorted(oses))))
-    oses = dict((k, v) for k, v in oses.iteritems() if options.os == k)
+    oses = [options.os]
 
-  result = 0
-  tempdir = tempfile.mkdtemp(prefix='swarm_client_tests')
-  try:
-    # Put the .isolated files in a temporary directory. This is simply done so
-    # the current directory doesn't have the following files created:
-    # - swarm_client_tests.isolate
-    # - swarm_client_tests.isolated
-    # - swarm_client_tests.isolated.state
-    isolated = os.path.join(tempdir, 'swarm_client_tests.isolated')
+  if sys.platform in ('win32', 'cygwin'):
+    # If we are on Windows, don't generate the tests for Linux and Mac since
+    # they use symlinks and we can't create symlinks on windows.
+    oses = ['win32']
+    if options.os != 'win32':
+      print('Linux and Mac tests skipped since running on Windows.')
 
-    print('Archiving')
-    hashvals = []
-    for i, test in enumerate(tests):
-      hashvals.append([])
-      for platform in oses.itervalues():
-        subprocess.check_call(
-            [
-                sys.executable,
-                'isolate.py',
-                'hashtable',
-                '--isolate', os.path.join(ROOT_DIR, 'run_a_test.isolate'),
-                '--isolated', isolated,
-                '--outdir', options.isolate_server,
-                '--variable', 'TEST_EXECUTABLE', test,
-                '--variable', 'OS', platform,
-          ],
-          cwd=os.path.dirname(ROOT_DIR))
-        hashvals[i].append(
-            hashlib.sha1(open(isolated, 'rb').read()).hexdigest())
-
-    print('\nTriggering')
-    for i, test in enumerate(tests):
-      sys.stdout.write('  %s: ' % os.path.basename(test))
-      for j, platform in enumerate(oses):
-        sys.stdout.write(platform)
-        if j != len(oses) - 1:
-          sys.stdout.write(', ')
-        subprocess.check_call(
-            [
-              sys.executable,
-              'swarm_trigger_step.py',
-              '--os_image', platform,
-              '--swarm-url', options.swarm_server,
-              '--test-name-prefix', prefix,
-              '--data-server', options.isolate_server,
-              '--run_from_hash', hashvals[i][j],
-              'swarm_client_tests_%s_%s' % (platform, os.path.basename(test)),
-              # Number of shards.
-              '1',
-              '',
-            ],
-            cwd=os.path.dirname(ROOT_DIR))
-      sys.stdout.write('\n')
-
-    print('\nGetting results')
-    failed_tests = {}
-    for i, test in enumerate(tests):
-      print('  %s' % os.path.basename(test))
-      for platform in oses:
-        print('    Retrieving results for %s' % platform)
-        name = '%s_%s' % (platform, os.path.basename(test))
-        process = subprocess.Popen(
-            [
-              sys.executable,
-              'swarm_get_results.py',
-              '--url', options.swarm_server,
-              prefix + 'swarm_client_tests_' + name,
-            ],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            cwd=os.path.dirname(ROOT_DIR))
-        stdout, _ = process.communicate()
-
-        # Only print the output for failures, successes are unexciting.
-        if process.returncode:
-          print stdout
-          failed_tests.setdefault(test, []).append(platform)
-          if options.logs:
-            with open(os.path.join(options.logs, name + '.log'), 'wb') as f:
-              f.write(stdout)
-        result = result or process.returncode
-  finally:
-    shutil.rmtree(tempdir)
-
-  if on_windows:
-    print 'Linux and Mac tests skipped since running on Windows.'
-
-  if result:
-    print 'Detected the following failures:'
-    for test, failed_oses in failed_tests.iteritems():
-      print '  %s on %s' % (test, ','.join(failed_oses))
-  else:
-    print 'No Swarm errors detected :)'
-
-  return result
+  return run_swarm_tests_on_swarm(
+      oses,
+      tests,
+      options.logs,
+      options.isolate_server,
+      options.swarm_server)
 
 
 if __name__ == '__main__':
