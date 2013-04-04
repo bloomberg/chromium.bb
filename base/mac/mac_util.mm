1679f58c5a3c97fd83d7a8ee76a6bf2db6bee07f
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-04 11:39:02 +0000

    Work around bug coming back from hiding menu bar with fullscreen.
    BUG=170189,225659
    TEST=as in bug 225659
    
    
    Review URL: https://chromiumcodereview.appspot.com/13583005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192301 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6c4a32d6bde80481b35b246b7fd5ba1e47e21c4e

--- base/mac/mac_util.mm
+++ base/mac/mac_util.mm
@@ -27,6 +27,16 @@
 namespace base {
 namespace mac {
 
+// Replicate specific 10.7 SDK declarations for building with prior SDKs.
+#if !defined(MAC_OS_X_VERSION_10_7) || \
+    MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_7
+
+enum {
+  NSApplicationPresentationFullScreen = 1 << 10
+};
+
+#endif  // MAC_OS_X_VERSION_10_7
+
 namespace {
 
 // The current count of outstanding requests for full screen mode from browser
@@ -58,6 +68,17 @@ void SetUIMode() {
   } else if (g_full_screen_requests[kFullScreenModeHideAll] > 0) {
     desired_options = NSApplicationPresentationHideDock |
                       NSApplicationPresentationHideMenuBar;
+  }
+
+  // Bug-fix: if the window is fullscreened (Lion-style) and
+  // NSApplicationPresentationDefault is requested, the result is that the menu
+  // bar doesn't auto-hide. In that case, explicitly set the presentation
+  // options to the ones that are set by the system as it fullscreens a window.
+  if (desired_options == NSApplicationPresentationDefault &&
+      current_options & NSApplicationPresentationFullScreen) {
+    desired_options |= NSApplicationPresentationFullScreen |
+                       NSApplicationPresentationAutoHideMenuBar |
+                       NSApplicationPresentationAutoHideDock;
   }
 
   if (current_options != desired_options)
