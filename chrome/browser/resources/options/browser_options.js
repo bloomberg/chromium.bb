983697306ae67050465595a320134406983b5f3f
haruki@chromium.org <haruki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-05 16:04:28 +0000

    Update options UI to display "/special/drive/root" as "Google Drive".
    We are introducing a new namespace "root". The "My Drive" root will be represented as
    "/special/drive/root".
    
    BUG=174233,218221
    TEST=Open options UI
    
    
    Review URL: https://chromiumcodereview.appspot.com/12790013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192572 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 22ce2735c6f2a71aaee3d27f086a0336791c8835

--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -869,7 +869,7 @@ cr.define('options', function() {
         // /home/chronos/user/Downloads with Downloads for local files.
         // Also replace '/' with ' \u203a ' (angled quote sign) everywhere.
         var path = $('downloadLocationPath').value;
-        path = path.replace(/^\/special\/drive/, 'Google Drive');
+        path = path.replace(/^\/special\/drive\/root/, 'Google Drive');
         path = path.replace(/^\/home\/chronos\/user\//, '');
         path = path.replace(/\//g, ' \u203a ');
         $('downloadLocationPath').value = path;
