ba35daa406ea53f8630f8412d34563cb22732e5f
commit-bot@chromium.org <commit-bot@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-10 17:54:37 +0000

    Simplify the patch to malloc() on systems using the system allocator.
    This is a no-op on Mac but simplifies things on Win / Linux:
    - Don't decorate classes with "operator new" etc. for WTF_MAKE_FAST_ALLOCATED.
    - Don't check for NULL in fastMalloc() because tcmalloc has already done that.
    
    I was expecting this to be noticeably perf positive but it comes out a wash on
    DOM/*
    If anyone knows of any tests particularly sensitive to malloc performance then
    I'm happy to run them.
    
    The patch does reduce binary bloat by a tiny amount, presumably because the
    fastMalloc() NULL checks were inlined previously.
    Before: 66894040 bytes (DRT)
    After:  66889944 bytes
    For a whopping saving of 4k!
    
    R=eseidel@chromium.org,abarth@chromiumlorg
    BUG=229735
    TEST=wktry
    
    
    Review URL: https://chromiumcodereview.appspot.com/14006002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148068 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 1d86ce91f4f2996c3d40adcbb997f6d7e5d77dd6

--- third_party/WebKit/Source/WTF/wtf/FastAllocBase.h
+++ third_party/WebKit/Source/WTF/wtf/FastAllocBase.h
@@ -93,6 +93,8 @@
 #include <wtf/StdLibExtras.h>
 #include <wtf/TypeTraits.h>
 
+#if !USE(SYSTEM_MALLOC)
+
 #define WTF_MAKE_FAST_ALLOCATED \
 public: \
     void* operator new(size_t, void* p) { return p; } \
@@ -130,6 +132,12 @@ public: \
     } \
 private: \
 typedef int __thisIsHereToForceASemicolonAfterThisMacro
+
+#else
+
+#define WTF_MAKE_FAST_ALLOCATED
+
+#endif
 
 namespace WTF {
 
