5d32d5bd3fab448fc34410ca57ccf71b4b31aed5
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 15:50:58 +0000

    keyboard: Add a window delegate to the keyboard window container.
    The delegate makes sure that the keyboard window does not get focus,
    and that events are not consumed by the container window itself.
    
    BUG=226986
    
    Review URL: https://codereview.chromium.org/13726015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193392 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a1f53c7bf2f989105b612aa4bbebf401afec1b7e

--- ui/keyboard/keyboard_controller.cc
+++ ui/keyboard/keyboard_controller.cc
@@ -6,10 +6,23 @@
 
 #include "ui/aura/layout_manager.h"
 #include "ui/aura/window.h"
+#include "ui/aura/window_delegate.h"
+#include "ui/base/hit_test.h"
+#include "ui/gfx/path.h"
 #include "ui/gfx/rect.h"
+#include "ui/gfx/skia_util.h"
 #include "ui/keyboard/keyboard_controller_proxy.h"
 
 namespace {
+
+gfx::Rect KeyboardBoundsFromWindowBounds(const gfx::Rect& window_bounds) {
+  const float kKeyboardHeightRatio = 0.3f;
+  return gfx::Rect(
+      window_bounds.x(),
+      window_bounds.y() + window_bounds.height() * (1 - kKeyboardHeightRatio),
+      window_bounds.width(),
+      window_bounds.height() * kKeyboardHeightRatio);
+}
 
 // LayoutManager for the virtual keyboard container.  Manages a single window
 // (the virtual keyboard) and keeps it positioned at the bottom of the
@@ -21,13 +34,8 @@ class KeyboardLayoutManager : public aura::LayoutManager {
 
   // Overridden from aura::LayoutManager
   virtual void OnWindowResized() OVERRIDE {
-    gfx::Rect owner_bounds = owner_->bounds();
-    gfx::Rect keyboard_bounds = gfx::Rect(
-        owner_bounds.x(),
-        owner_bounds.y() + owner_bounds.height() * 0.7,
-        owner_bounds.width(),
-        owner_bounds.height() * 0.3);
-    SetChildBoundsDirect(keyboard_, keyboard_bounds);
+    SetChildBoundsDirect(keyboard_,
+                         KeyboardBoundsFromWindowBounds(owner_->bounds()));
   }
   virtual void OnWindowAddedToLayout(aura::Window* child) OVERRIDE {
     CHECK(child == keyboard_);
@@ -48,6 +56,52 @@ class KeyboardLayoutManager : public aura::LayoutManager {
   DISALLOW_COPY_AND_ASSIGN(KeyboardLayoutManager);
 };
 
+// The KeyboardWindowDelegate makes sure the keyboard-window does not get focus.
+// This is necessary to make sure that the synthetic key-events reach the target
+// window.
+// The delegate deletes itself when the window is destroyed.
+class KeyboardWindowDelegate : public aura::WindowDelegate {
+ public:
+  KeyboardWindowDelegate() {}
+  virtual ~KeyboardWindowDelegate() {}
+
+ private:
+  // Overridden from aura::WindowDelegate:
+  virtual gfx::Size GetMinimumSize() const OVERRIDE { return gfx::Size(); }
+  virtual gfx::Size GetMaximumSize() const OVERRIDE { return gfx::Size(); }
+  virtual void OnBoundsChanged(const gfx::Rect& old_bounds,
+                               const gfx::Rect& new_bounds) OVERRIDE {
+    bounds_ = new_bounds;
+  }
+  virtual gfx::NativeCursor GetCursor(const gfx::Point& point) OVERRIDE {
+    return gfx::kNullCursor;
+  }
+  virtual int GetNonClientComponent(const gfx::Point& point) const OVERRIDE {
+    return HTNOWHERE;
+  }
+  virtual bool ShouldDescendIntoChildForEventHandling(
+      aura::Window* child,
+      const gfx::Point& location) OVERRIDE {
+    return true;
+  }
+  virtual bool CanFocus() OVERRIDE { return false; }
+  virtual void OnCaptureLost() OVERRIDE {}
+  virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE {}
+  virtual void OnDeviceScaleFactorChanged(float device_scale_factor) OVERRIDE {}
+  virtual void OnWindowDestroying() OVERRIDE {}
+  virtual void OnWindowDestroyed() OVERRIDE { delete this; }
+  virtual void OnWindowTargetVisibilityChanged(bool visible) OVERRIDE {}
+  virtual bool HasHitTestMask() const OVERRIDE { return true; }
+  virtual void GetHitTestMask(gfx::Path* mask) const OVERRIDE {
+    gfx::Rect keyboard_bounds = KeyboardBoundsFromWindowBounds(bounds_);
+    mask->addRect(RectToSkRect(keyboard_bounds));
+  }
+  virtual scoped_refptr<ui::Texture> CopyTexture() OVERRIDE { return NULL; }
+
+  gfx::Rect bounds_;
+  DISALLOW_COPY_AND_ASSIGN(KeyboardWindowDelegate);
+};
+
 }  // namespace
 
 namespace keyboard {
@@ -61,7 +115,7 @@ KeyboardController::~KeyboardController() {}
 
 aura::Window* KeyboardController::GetContainerWindow() {
   if (!container_) {
-    container_ = new aura::Window(NULL);
+    container_ = new aura::Window(new KeyboardWindowDelegate());
     container_->SetName("KeyboardContainer");
     container_->Init(ui::LAYER_NOT_DRAWN);
 
--- ui/keyboard/keyboard_controller_unittest.cc
+++ ui/keyboard/keyboard_controller_unittest.cc
@@ -5,7 +5,11 @@
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/aura/client/focus_client.h"
+#include "ui/aura/root_window.h"
 #include "ui/aura/test/aura_test_helper.h"
+#include "ui/aura/test/event_generator.h"
+#include "ui/aura/test/test_window_delegate.h"
 #include "ui/aura/window.h"
 #include "ui/compositor/layer_type.h"
 #include "ui/gfx/rect.h"
@@ -15,6 +19,33 @@
 namespace keyboard {
 namespace {
 
+// An event handler that focuses a window when it is clicked/touched on. This is
+// used to match the focus manger behaviour in ash and views.
+class TestFocusController : public ui::EventHandler {
+ public:
+  explicit TestFocusController(aura::RootWindow* root)
+      : root_(root) {
+    root_->AddPreTargetHandler(this);
+  }
+
+  virtual ~TestFocusController() {
+    root_->RemovePreTargetHandler(this);
+  }
+
+ private:
+  // Overridden from ui::EventHandler:
+  virtual void OnEvent(ui::Event* event) OVERRIDE {
+    aura::Window* target = static_cast<aura::Window*>(event->target());
+    if (event->type() == ui::ET_MOUSE_PRESSED ||
+        event->type() == ui::ET_TOUCH_PRESSED) {
+      aura::client::GetFocusClient(target)->FocusWindow(target);
+    }
+  }
+
+  aura::Window* root_;
+  DISALLOW_COPY_AND_ASSIGN(TestFocusController);
+};
+
 class KeyboardControllerTest : public testing::Test {
  public:
   KeyboardControllerTest() {}
@@ -23,15 +54,19 @@ class KeyboardControllerTest : public testing::Test {
   virtual void SetUp() OVERRIDE {
     aura_test_helper_.reset(new aura::test::AuraTestHelper(&message_loop_));
     aura_test_helper_->SetUp();
+    focus_controller_.reset(new TestFocusController(root_window()));
   }
 
   virtual void TearDown() OVERRIDE {
     aura_test_helper_->TearDown();
   }
 
+  aura::RootWindow* root_window() { return aura_test_helper_->root_window(); }
+
  protected:
   base::MessageLoopForUI message_loop_;
   scoped_ptr<aura::test::AuraTestHelper> aura_test_helper_;
+  scoped_ptr<TestFocusController> focus_controller_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(KeyboardControllerTest);
@@ -39,16 +74,45 @@ class KeyboardControllerTest : public testing::Test {
 
 class TestKeyboardControllerProxy : public KeyboardControllerProxy {
  public:
-  TestKeyboardControllerProxy() : window_(new aura::Window(NULL)) {
+  TestKeyboardControllerProxy() : window_(new aura::Window(&delegate_)) {
     window_->Init(ui::LAYER_NOT_DRAWN);
     window_->set_owned_by_parent(false);
   }
-  virtual ~TestKeyboardControllerProxy() {}
+
+  virtual ~TestKeyboardControllerProxy() {
+    // Destroy the window before the delegate.
+    window_.reset();
+  }
+
+  // Overridden from KeyboardControllerProxy:
   virtual aura::Window* GetKeyboardWindow() OVERRIDE { return window_.get(); }
 
  private:
   scoped_ptr<aura::Window> window_;
+  aura::test::TestWindowDelegate delegate_;
+
   DISALLOW_COPY_AND_ASSIGN(TestKeyboardControllerProxy);
+};
+
+// Keeps a count of all the events a window receives.
+class EventObserver : public ui::EventHandler {
+ public:
+  EventObserver() {}
+  virtual ~EventObserver() {}
+
+  int GetEventCount(ui::EventType type) {
+    return event_counts_[type];
+  }
+
+ private:
+  // Overridden from ui::EventHandler:
+  virtual void OnEvent(ui::Event* event) OVERRIDE {
+    ui::EventHandler::OnEvent(event);
+    event_counts_[event->type()]++;
+  }
+
+  std::map<ui::EventType, int> event_counts_;
+  DISALLOW_COPY_AND_ASSIGN(EventObserver);
 };
 
 }  // namespace
@@ -69,6 +133,52 @@ TEST_F(KeyboardControllerTest, KeyboardSize) {
   // The KeyboardController's LayoutManager shouldn't let this happen
   proxy->GetKeyboardWindow()->SetBounds(new_bounds);
   ASSERT_EQ(before_bounds, proxy->GetKeyboardWindow()->bounds());
+}
+
+// Tests that tapping/clicking inside the keyboard does not give it focus.
+TEST_F(KeyboardControllerTest, ClickDoesNotFocusKeyboard) {
+  const gfx::Rect& root_bounds = root_window()->bounds();
+  aura::test::EventCountDelegate delegate;
+  scoped_ptr<aura::Window> window(new aura::Window(&delegate));
+  window->Init(ui::LAYER_NOT_DRAWN);
+  window->SetBounds(root_bounds);
+  root_window()->AddChild(window.get());
+  window->Show();
+  window->Focus();
+
+  KeyboardControllerProxy* proxy = new TestKeyboardControllerProxy();
+  KeyboardController controller(proxy);
+
+  scoped_ptr<aura::Window> keyboard_container(controller.GetContainerWindow());
+  keyboard_container->SetBounds(root_bounds);
+
+  root_window()->AddChild(keyboard_container.get());
+  keyboard_container->Show();
+
+  root_window()->StackChildAtTop(keyboard_container.get());
+  EXPECT_TRUE(window->IsVisible());
+  EXPECT_TRUE(keyboard_container->IsVisible());
+  EXPECT_TRUE(window->HasFocus());
+  EXPECT_FALSE(keyboard_container->HasFocus());
+
+  // Click on the keyboard. Make sure the keyboard receives the event, but does
+  // not get focus.
+  EventObserver observer;
+  keyboard_container->AddPreTargetHandler(&observer);
+
+  aura::test::EventGenerator generator(root_window());
+  generator.MoveMouseTo(proxy->GetKeyboardWindow()->bounds().CenterPoint());
+  generator.ClickLeftButton();
+  EXPECT_TRUE(window->HasFocus());
+  EXPECT_FALSE(keyboard_container->HasFocus());
+  EXPECT_EQ("0 0", delegate.GetMouseButtonCountsAndReset());
+  EXPECT_EQ(1, observer.GetEventCount(ui::ET_MOUSE_PRESSED));
+  EXPECT_EQ(1, observer.GetEventCount(ui::ET_MOUSE_RELEASED));
+
+  // Click outside of the keyboard. It should reach the window behind.
+  generator.MoveMouseTo(gfx::Point());
+  generator.ClickLeftButton();
+  EXPECT_EQ("1 1", delegate.GetMouseButtonCountsAndReset());
 }
 
 }  // namespace keyboard
