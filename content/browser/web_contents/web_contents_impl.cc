a5df1a4c5eb3ce514f6b5c441662d9a42e88500e
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 20:28:39 +0000

    Disable Threaded HTML Parsing for Android
    BUG=230542
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/14159004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193731 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dab5ad36c163c14f95b7375e66715bc390152c92

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -501,8 +501,16 @@ WebPreferences WebContentsImpl::GetWebkitPrefs(RenderViewHost* rvh,
       command_line.HasSwitch(switches::kEnableExperimentalWebKitFeatures);
   prefs.css_grid_layout_enabled =
       command_line.HasSwitch(switches::kEnableExperimentalWebKitFeatures);
+
+  // TODO(abarth, eseidel): Enable threaded_html_parser by default on Android
+  // once crbug 230542 is resolved.
+#if defined(OS_ANDROID)
+  prefs.threaded_html_parser = false;
+#else
   prefs.threaded_html_parser =
       !command_line.HasSwitch(switches::kDisableThreadedHTMLParser);
+#endif
+
 #if defined(OS_ANDROID)
   prefs.user_gesture_required_for_media_playback = !command_line.HasSwitch(
       switches::kDisableGestureRequirementForMediaPlayback);
