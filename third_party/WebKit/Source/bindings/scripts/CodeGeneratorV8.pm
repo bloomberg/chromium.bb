0d6420083279059db8ce38b30b9d4860d2aa2b69
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-11 13:44:20 +0000

    Revert 148183 "Support for selective DOM activity logging, based..."
    > Support for selective DOM activity logging, based on IDL attributes.
    > 
    > Add support for activity logging, which logs access to selected DOM
    > attributes and methods, based on IDL attribute markings.  Logging adds code
    > to getter, setter, and method callbacks in the V8 bindings, either in all
    > of the callbacks, or just the callbacks for the isolated worlds.
    > 
    > R=haraken@chromium.org
    > NOTRY=true
    > BUG=160989
    > 
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=148146
    > 
    > Review URL: https://chromiumcodereview.appspot.com/13799007
    
    TBR=ulfar@google.com
    Review URL: https://codereview.chromium.org/14137008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148184 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c5aec3dafd7489a9e31cf22874ffed2a6fc2f4f2

--- third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
@@ -775,26 +775,6 @@ sub GenerateHeaderCustomCall
     }
 }
 
-sub HasActivityLogging
-{
-    my $forMainWorldSuffix = shift;
-    my $attrExt = shift;
-    my $access = shift;
-
-    if (!$attrExt->{"ActivityLog"}) {
-        return 0;
-    }
-    my $logAllAccess = ($attrExt->{"ActivityLog"} =~ /^Access/);
-    my $logGetter = ($attrExt->{"ActivityLog"} =~ /^Getter/);
-    my $logSetter = ($attrExt->{"ActivityLog"} =~ /^Setter/);
-    my $logOnlyIsolatedWorlds = ($attrExt->{"ActivityLog"} =~ /ForIsolatedWorlds$/);
-
-    if ($logOnlyIsolatedWorlds && $forMainWorldSuffix eq "ForMainWorld") {
-        return 0;
-    }
-    return $logAllAccess || ($logGetter && $access eq "Getter") || ($logSetter && $access eq "Setter");
-}
-
 sub IsConstructable
 {
     my $interface = shift;
@@ -938,45 +918,6 @@ sub GenerateFeatureObservation
     return "";
 }
 
-sub GenerateActivityLogging
-{
-    my $accessType = shift;
-    my $interface = shift;
-    my $propertyName = shift;
-
-    my $visibleInterfaceName = $codeGenerator->GetVisibleInterfaceName($interface);
-
-    AddToImplIncludes("V8Binding.h");
-    AddToImplIncludes("V8DOMActivityLogger.h");
-    AddToImplIncludes("wtf/Vector.h");
-
-    if ($accessType eq "Method") {
-        push(@implContentInternals, <<END);
-    V8PerContextData* contextData = V8PerContextData::from(v8::Context::GetCurrent());
-    if (contextData && contextData->activityLogger()) {
-        Vector<v8::Handle<v8::Value> > loggerArgs = toVectorOfArguments(args);
-        contextData->activityLogger()->log("${visibleInterfaceName}.${propertyName}", args.Length(), loggerArgs.data(), "${accessType}");
-    }
-END
-    } elsif ($accessType eq "Setter") {
-        push(@implContentInternals, <<END);
-    V8PerContextData* contextData = V8PerContextData::from(v8::Context::GetCurrent());
-    if (contextData && contextData->activityLogger()) {
-        v8::Handle<v8::Value> loggerArg[] = { value };
-        contextData->activityLogger()->log("${visibleInterfaceName}.${propertyName}", 1, &loggerArg[0], "${accessType}");
-    }
-END
-    } elsif ($accessType eq "Getter") {
-        push(@implContentInternals, <<END);
-    V8PerContextData* contextData = V8PerContextData::from(v8::Context::GetCurrent());
-    if (contextData && contextData->activityLogger())
-        contextData->activityLogger()->log("${visibleInterfaceName}.${propertyName}", 0, 0, "${accessType}");
-END
-    } else {
-        die "Unrecognized activity logging access type";
-    }
-}
-
 sub GenerateNormalAttrGetterCallback
 {
     my $attribute = shift;
@@ -994,9 +935,6 @@ sub GenerateNormalAttrGetterCallback
     push(@implContentInternals, "static v8::Handle<v8::Value> ${attrName}AttrGetterCallback${forMainWorldSuffix}(v8::Local<v8::String> name, const v8::AccessorInfo& info)\n");
     push(@implContentInternals, "{\n");
     push(@implContentInternals, GenerateFeatureObservation($attrExt->{"MeasureAs"}));
-    if (HasActivityLogging($forMainWorldSuffix, $attrExt, "Getter")) {
-        GenerateActivityLogging("Getter", $interface, "${attrName}");
-    }
     if (HasCustomGetter($attrExt)) {
         push(@implContentInternals, "    return ${v8InterfaceName}::${attrName}AttrGetterCustom(name, info);\n");
     } else {
@@ -1287,9 +1225,6 @@ sub GenerateReplaceableAttrSetterCallback
     push(@implContentInternals, "static void ${interfaceName}ReplaceableAttrSetterCallback(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::AccessorInfo& info)\n");
     push(@implContentInternals, "{\n");
     push(@implContentInternals, GenerateFeatureObservation($interface->extendedAttributes->{"MeasureAs"}));
-    if (HasActivityLogging("", $interface->extendedAttributes, "Setter")) {
-         die "IDL error: ActivityLog attribute cannot exist on a ReplacableAttrSetterCallback";
-    }
     push(@implContentInternals, "    return ${interfaceName}V8Internal::${interfaceName}ReplaceableAttrSetter(name, value, info);\n");
     push(@implContentInternals, "}\n\n");
 }
@@ -1355,9 +1290,6 @@ sub GenerateNormalAttrSetterCallback
     push(@implContentInternals, "static void ${attrName}AttrSetterCallback${forMainWorldSuffix}(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::AccessorInfo& info)\n");
     push(@implContentInternals, "{\n");
     push(@implContentInternals, GenerateFeatureObservation($attrExt->{"MeasureAs"}));
-    if (HasActivityLogging($forMainWorldSuffix, $attrExt, "Setter")) {
-        GenerateActivityLogging("Setter", $interface, "${attrName}");
-    }
     if (HasCustomSetter($attrExt)) {
         push(@implContentInternals, "    ${v8InterfaceName}::${attrName}AttrSetterCustom(name, value, info);\n");
     } else {
@@ -1698,9 +1630,6 @@ static v8::Handle<v8::Value> ${name}MethodCallback${forMainWorldSuffix}(const v8
 {
 END
     push(@implContentInternals, GenerateFeatureObservation($function->signature->extendedAttributes->{"MeasureAs"}));
-    if (HasActivityLogging($forMainWorldSuffix, $function->signature->extendedAttributes, "Access")) {
-        GenerateActivityLogging("Method", $interface, "${name}");
-    }
     if (HasCustomMethod($function->signature->extendedAttributes)) {
         push(@implContentInternals, "    return ${v8InterfaceName}::${name}MethodCustom(args);\n");
     } else {
