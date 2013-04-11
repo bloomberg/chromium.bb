28c103f7f36acc2400630fe78602a3d9a0c1fb38
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 00:14:33 +0000

    Disable ExtensionManagementApiTest.LaunchTabApp on windows
    BUG=230165
    TBR=mpcomplete@chromium.org
    
    Review URL: https://codereview.chromium.org/14095006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193512 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5ea712290906d29201f443194b4f3715a8c36592

--- chrome/browser/extensions/api/management/management_apitest.cc
+++ chrome/browser/extensions/api/management/management_apitest.cc
@@ -218,7 +218,13 @@ IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, DISABLED_LaunchPanelApp) {
   ASSERT_TRUE(app_browser->is_app());
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, LaunchTabApp) {
+// Disabled: http://crbug.com/230165
+#if defined(OS_WIN)
+#define MAYBE_LaunchTabApp DISABLED_LaunchTabApp
+#else
+#define MAYBE_LaunchTabApp LaunchTabApp
+#endif
+IN_PROC_BROWSER_TEST_F(ExtensionManagementApiTest, MAYBE_LaunchTabApp) {
   ExtensionService* service = extensions::ExtensionSystem::Get(
       browser()->profile())->extension_service();
 
