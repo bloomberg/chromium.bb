41ac1a28336c19a9ad10401cdc06545a41d0bf62
ajuma@chromium.org <ajuma@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 13:04:00 +0000

    Fix main-thread event handling for impl-only animations
    Since impl-only animations have no main-thread counterpart, the
    main thread (more specifically, LayerTreeHost::SetAnimationEvents,
    LayerAnimationController::NotifyAnimationStarted, and
    LayerAnimationController::NotfiyAnimationFinished) should not
    assume that incoming events will be for animations that are
    running on the main thread. 
    
    BUG=196284
    
    Review URL: https://chromiumcodereview.appspot.com/13820019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193629 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: de39b124d63b6f7f16980a1434ccb0a7ad29f4b4

--- cc/animation/animation_events.h
+++ cc/animation/animation_events.h
@@ -8,30 +8,26 @@
 #include <vector>
 
 #include "cc/animation/animation.h"
+#include "cc/base/cc_export.h"
 #include "ui/gfx/transform.h"
 
 namespace cc {
 
-struct AnimationEvent {
+struct CC_EXPORT AnimationEvent {
   enum Type { Started, Finished, PropertyUpdate };
 
   AnimationEvent(Type type,
                  int layer_id,
                  int group_id,
                  Animation::TargetProperty target_property,
-                 double monotonic_time)
-      : type(type),
-        layer_id(layer_id),
-        group_id(group_id),
-        target_property(target_property),
-        monotonic_time(monotonic_time),
-        opacity(0.f) {}
+                 double monotonic_time);
 
   Type type;
   int layer_id;
   int group_id;
   Animation::TargetProperty target_property;
   double monotonic_time;
+  bool is_impl_only;
   float opacity;
   gfx::Transform transform;
 };
