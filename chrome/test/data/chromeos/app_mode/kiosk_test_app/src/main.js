c5da9376fc90cb867e56ba78f0b024aad4ad3936
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 21:35:22 +0000

    The second part of tests assets from https://codereview.chromium.org/13861037/, should have listen xiyuan@ the first time...
    BUG=none
    TEST=none
    TBR=xiyuan,sky
    
    Review URL: https://codereview.chromium.org/14070009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193757 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c63f127f3c545d14dad558ba8ad79879860ac1a6

---
+++ chrome/test/data/chromeos/app_mode/kiosk_test_app/src/main.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.app.runtime.onLaunched.addListener(function() {
+  if (chrome.power)
+    chrome.power.requestKeepAwake('display');
+  else
+    chrome.experimental.power.requestKeepAwake(function() {});
+
+  chrome.app.window.create('app_main.html',
+      { 'width': 1920,
+        'height': 1080 },
+      function(window) {
+        window.close();
+  });
+});
+
