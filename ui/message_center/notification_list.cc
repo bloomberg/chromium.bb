bloomberg/chromium.bb

commit 02fd9411383fac503e9e649cacf45679d1c68010

Copies on-memory notification states for updates.
is_read, is_expanded, shown_as_popup, and never_timeout should be
inherited from the original notification for update or add with
the same id. Otherwise the notification will re-appear as a popup,
or expand/collapse state can be reset, or never-timeout notifications
can timeout.

BUG=223338
TEST=message_center_unittests passed

Review URL: https://chromiumcodereview.appspot.com/13912008

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193780 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 35e39ce95d2e92efb891f69f1416e6b91aca9e96

ui/message_center/notification_list.cc
@@ -105,8 +105,11 @@ void NotificationList::UpdateNotificationMessage(
                        (*iter)->display_source(),
                        (*iter)->extension_id(),
                        optional_fields));
-  EraseNotification(iter);
-  PushNotification(notification.Pass());
+  notification->CopyState(*iter);
+  // Do not use EraseNotification and PushNotification, since we don't want to
+  // change unread counts nor to update is_read/shown_as_popup states.
+  notifications_.erase(iter);
+  notifications_.insert(notification.release());
 }
 
 void NotificationList::RemoveNotification(const std::string& id) {
@@ -319,10 +322,14 @@ void NotificationList::PushNotification(scoped_ptr<Notification> notification) {
   // Ensure that notification.id is unique by erasing any existing
   // notification with the same id (shouldn't normally happen).
   Notifications::iterator iter = GetNotification(notification->id());
-  if (iter != notifications_.end())
+  bool state_inherited = false;
+  if (iter != notifications_.end()) {
+    notification->CopyState(*iter);
+    state_inherited = true;
     EraseNotification(iter);
+  }
   // Add the notification to the the list and mark it unread and unshown.
-  if (!message_center_visible_) {
+  if (!message_center_visible_ && !state_inherited) {
     // TODO(mukai): needs to distinguish if a notification is dismissed by
     // the quiet mode or user operation.
     notification->set_is_read(quiet_mode_);

ui/message_center/notification.cc
@@ -50,6 +50,13 @@ Notification::Notification(NotificationType type,
 Notification::~Notification() {
 }
 
+void Notification::CopyState(Notification* base) {
+  shown_as_popup_ = base->shown_as_popup();
+  is_read_ = base->is_read();
+  is_expanded_ = base->is_expanded();
+  never_timeout_ = base->never_timeout();
+}
+
 bool Notification::SetButtonIcon(size_t index, const gfx::Image& icon) {
   if (index >= buttons_.size())
     return false;

ui/message_center/notification.h
@@ -42,6 +42,10 @@ class MESSAGE_CENTER_EXPORT Notification {
                const DictionaryValue* optional_fields);  // May be NULL.
   virtual ~Notification();
 
+  // Copies the internal on-memory state from |base|, i.e. shown_as_popup,
+  // is_read, is_expanded, and never_timeout.
+  void CopyState(Notification* base);
+
   NotificationType type() const { return type_; }
   const std::string& id() const { return id_; }
   const string16& title() const { return title_; }

ui/message_center/notification_list.h
@@ -22,6 +22,9 @@ class DictionaryValue;
 }
 
 namespace message_center {
+namespace test {
+class NotificationListTest;
+}
 
 // Comparers used to auto-sort the lists of Notifications.
 struct MESSAGE_CENTER_EXPORT ComparePriorityTimestampSerial {
@@ -139,6 +142,8 @@ class MESSAGE_CENTER_EXPORT NotificationList {
   static const size_t kMaxVisibleMessageCenterNotifications;
 
  private:
+  friend class test::NotificationListTest;
+
   // Iterates through the list and returns the first notification matching |id|.
   Notifications::iterator GetNotification(const std::string& id);
 

ui/message_center/notification_list_unittest.cc
@@ -13,7 +13,7 @@
 #include "ui/message_center/notification_types.h"
 
 namespace message_center {
-namespace {
+namespace test {
 
 class MockNotificationListDelegate : public NotificationList::Delegate {
  public:
@@ -83,6 +83,14 @@ class NotificationListTest : public testing::Test {
     return notification_list()->GetPopupNotifications().size();
   }
 
+  Notification* GetNotification(const std::string& id) {
+    NotificationList::Notifications::iterator iter =
+        notification_list()->GetNotification(id);
+    if (iter == notification_list()->GetNotifications().end())
+      return NULL;
+    return *iter;
+  }
+
   MockNotificationListDelegate* delegate() { return delegate_.get(); }
   NotificationList* notification_list() { return notification_list_.get(); }
 
@@ -105,8 +113,6 @@ const char NotificationListTest::kTitleFormat[] = "id%ld";
 const char NotificationListTest::kMessageFormat[] = "message%ld";
 const char NotificationListTest::kDisplaySource[] = "source";
 const char NotificationListTest::kExtensionId[] = "ext";
-
-}  // namespace
 
 TEST_F(NotificationListTest, Basic) {
   ASSERT_EQ(0u, notification_list()->NotificationCount());
@@ -396,6 +402,32 @@ TEST_F(NotificationListTest, MarkSinglePopupAsShown) {
   EXPECT_EQ(id1, (*iter)->id());
 }
 
+TEST_F(NotificationListTest, UpdateAfterMarkedAsShown) {
+  std::string id1 = AddNotification(NULL);
+  std::string id2 = AddNotification(NULL);
+
+  EXPECT_EQ(2u, GetPopupCounts());
+
+  const Notification* n1 = GetNotification(id1);
+  EXPECT_FALSE(n1->shown_as_popup());
+  EXPECT_FALSE(n1->is_read());
+
+  notification_list()->MarkSinglePopupAsShown(id1, true);
+
+  n1 = GetNotification(id1);
+  EXPECT_TRUE(n1->shown_as_popup());
+  EXPECT_TRUE(n1->is_read());
+
+  const std::string replaced("test-replaced-id");
+  notification_list()->UpdateNotificationMessage(
+      id1, replaced, UTF8ToUTF16("newtitle"), UTF8ToUTF16("newbody"), NULL);
+  n1 = GetNotification(id1);
+  EXPECT_TRUE(n1 == NULL);
+  const Notification* nr = GetNotification(replaced);
+  EXPECT_TRUE(nr->shown_as_popup());
+  EXPECT_TRUE(nr->is_read());
+}
+
 TEST_F(NotificationListTest, QuietMode) {
   notification_list()->SetQuietMode(true);
   AddNotification(NULL);
@@ -415,4 +447,5 @@ TEST_F(NotificationListTest, QuietMode) {
   // TODO(mukai): Add test of quiet mode with expiration.
 }
 
+}  // namespace test
 }  // namespace message_center

commit 237e535a3692416d1d38d82cee2026f9b0d1fdd0

Try to fix notification list leak introduced in r193780
TBR=mukai@chromium.org
NOTRY=true

Review URL: https://codereview.chromium.org/13903008

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193931 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 5274fbb5225d18d902d712a95591d72b8ad91ba1

ui/message_center/notification_list.cc
@@ -108,7 +108,9 @@ void NotificationList::UpdateNotificationMessage(
   notification->CopyState(*iter);
   // Do not use EraseNotification and PushNotification, since we don't want to
   // change unread counts nor to update is_read/shown_as_popup states.
+  Notification *old = *iter;
   notifications_.erase(iter);
+  delete old;
   notifications_.insert(notification.release());
 }
 