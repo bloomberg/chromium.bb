252bd24950b752d416a50c7145f546eb090a970e
Frank Galligan <fgalligan@google.com>
2013-04-15 08:45:31 -0700

    libwebm: Update Android build
    - Change to libwebm.
    - Add muxer support.
    - Remove sample app.
    
    Change-Id: I39c31ecbff573f86214e85b3ac08e0dcdfe22ae2

--- Android.mk
+++ Android.mk
@@ -1,13 +1,10 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE:= libmkvparser
+LOCAL_MODULE:= libwebm
 LOCAL_SRC_FILES:= mkvparser.cpp \
-                  mkvreader.cpp
+                  mkvreader.cpp \
+                  mkvmuxer.cpp \
+                  mkvmuxerutil.cpp \
+                  mkvwriter.cpp
 include $(BUILD_STATIC_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE:= mkvparser
-LOCAL_SRC_FILES:= sample.cpp
-LOCAL_STATIC_LIBRARIES:= libmkvparser
-include $(BUILD_EXECUTABLE)
