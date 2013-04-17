b1f6a52d9e559f9d8ed74c1e96dd19dcfa27bbf9
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 11:13:14 +0000

    Implement ComposedCallback with three arguments and the second one is scoped_ptr.
    Just add new pattern. It will be used to implement DriveFileStreamReader.
    
    BUG=127129
    TEST=Ran unit_tests.
    
    Review URL: https://codereview.chromium.org/14235017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194569 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b5a8b7c894ce50bfd369096d2b709b35f018a6f3

--- chrome/browser/google_apis/task_util.h
+++ chrome/browser/google_apis/task_util.h
@@ -66,6 +66,17 @@ struct ComposedCallback<void(T1, T2, T3)> {
   }
 };
 
+// ComposedCallback with three arguments, and the second one is scoped_ptr.
+template<typename T1, typename T2, typename D2, typename T3>
+struct ComposedCallback<void(T1, scoped_ptr<T2, D2>, T3)> {
+  static void Run(
+      const base::Callback<void(const base::Closure&)>& runner,
+      const base::Callback<void(T1, scoped_ptr<T2, D2>, T3)>& callback,
+      T1 arg1, scoped_ptr<T2, D2> arg2, T3 arg3) {
+    runner.Run(base::Bind(callback, arg1, base::Passed(&arg2), arg3));
+  }
+};
+
 // ComposedCallback with three arguments, and the last one is scoped_ptr.
 template<typename T1, typename T2, typename T3, typename D3>
 struct ComposedCallback<void(T1, T2, scoped_ptr<T3, D3>)> {
