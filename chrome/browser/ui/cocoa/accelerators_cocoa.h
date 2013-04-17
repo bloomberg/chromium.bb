16bcdabf8727b07fa6a4e400d21f8a95aea8ec80
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 15:46:17 +0000

    Revert 194594 "Make sure manifest specified shortcut for Extensi..."
    Got linker error not caught by try servers.
    
    > Make sure manifest specified shortcut for Extension Command can not override the built-in shortcuts.
    > 
    > User is still free to override manually.
    > 
    > BUG=226994
    > TEST=Automated test included.
    > 
    > Review URL: https://codereview.chromium.org/13044014
    
    TBR=finnur@chromium.org
    
    Review URL: https://codereview.chromium.org/14096026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194597 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: af4e286ebd6cc5fd1c3a2987da03875e1c0a53d8

--- chrome/browser/ui/cocoa/accelerators_cocoa.h
+++ chrome/browser/ui/cocoa/accelerators_cocoa.h
@@ -26,10 +26,6 @@ template <typename T> struct DefaultSingletonTraits;
 class AcceleratorsCocoa {
  public:
   typedef std::map<int, ui::Accelerator> AcceleratorMap;
-  typedef AcceleratorMap::const_iterator const_iterator;
-
-  const_iterator const begin() { return accelerators_.begin(); }
-  const_iterator const end() { return accelerators_.end(); }
 
   // Returns NULL if there is no accelerator for the command.
   const ui::Accelerator* GetAcceleratorForCommand(int command_id);
