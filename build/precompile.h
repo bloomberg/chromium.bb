b95324061ebe8428850d4ba058d1c6b06ed01c88
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 01:37:20 +0000

    Revert 194506 "intsafe workaround to separate header"
    Broke x64 compile.
    
    > intsafe workaround to separate header
    > 
    > Allows workaround to work without precompiled headers (as in
    > buildtype=Official)
    > 
    > R=cpu@chromium.org
    > BUG=225822
    > 
    > Review URL: https://codereview.chromium.org/14201003
    
    TBR=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/13977005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194507 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8e2f90cf7d4267949321a6e8f3ebd7ef675b374b

--- build/precompile.h
+++ build/precompile.h
@@ -107,4 +107,21 @@
 #include <utility>
 #include <vector>
 
-#include "build/intsafe_workaround.h"
+// Workaround for:
+// http://connect.microsoft.com/VisualStudio/feedback/details/621653/
+// http://crbug.com/225822
+// Note that we can't actually include <stdint.h> here because there's other
+// code in third_party that has partial versions of stdint types that conflict.
+#include <intsafe.h>
+#undef INT8_MIN
+#undef INT16_MIN
+#undef INT32_MIN
+#undef INT64_MIN
+#undef INT8_MAX
+#undef UINT8_MAX
+#undef INT16_MAX
+#undef UINT16_MAX
+#undef INT32_MAX
+#undef UINT32_MAX
+#undef INT64_MAX
+#undef UINT64_MAX
