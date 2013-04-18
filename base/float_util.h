54b9d2123c45be0aa4fcb3fa3dcdcfccc58fc7e7
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 22:41:16 +0000

    Use std::isfinite().
    More standards-comformant, also needed for C++11 on Linux.
    
    BUG=233330
    
    Review URL: https://codereview.chromium.org/14188028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195020 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7fe73a7231945d4e5f6f4b27ad3b6dfa3a019336

--- base/float_util.h
+++ base/float_util.h
@@ -8,16 +8,14 @@
 #include "build/build_config.h"
 
 #include <float.h>
-#include <math.h>
+
+#include <cmath>
 
 namespace base {
 
 inline bool IsFinite(const double& number) {
-#if defined(OS_ANDROID)
-  // isfinite isn't available on Android: http://b.android.com/34793
-  return finite(number) != 0;
-#elif defined(OS_POSIX)
-  return isfinite(number) != 0;
+#if defined(OS_POSIX)
+  return std::isfinite(number) != 0;
 #elif defined(OS_WIN)
   return _finite(number) != 0;
 #endif
