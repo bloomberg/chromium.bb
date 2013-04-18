748909e39a35231846a80314296f3e3d1bcde594
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 02:10:25 +0000

    ui: Use RectF::x/y/right/bottom for rect conversions.
    R=danakj
    BUG=
    
    Review URL: https://codereview.chromium.org/14195014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194750 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c2c83d1fa0a3eda5c60da7fb71cb56bffb764fc1

--- ui/gfx/rect_conversions.cc
+++ ui/gfx/rect_conversions.cc
@@ -12,30 +12,30 @@
 namespace gfx {
 
 Rect ToEnclosingRect(const RectF& rect) {
-  int min_x = ToFlooredInt(rect.origin().x());
-  int min_y = ToFlooredInt(rect.origin().y());
-  float max_x = rect.origin().x() + rect.size().width();
-  float max_y = rect.origin().y() + rect.size().height();
+  int min_x = ToFlooredInt(rect.x());
+  int min_y = ToFlooredInt(rect.y());
+  float max_x = rect.right();
+  float max_y = rect.bottom();
   int width = std::max(ToCeiledInt(max_x) - min_x, 0);
   int height = std::max(ToCeiledInt(max_y) - min_y, 0);
   return Rect(min_x, min_y, width, height);
 }
 
 Rect ToEnclosedRect(const RectF& rect) {
-  int min_x = ToCeiledInt(rect.origin().x());
-  int min_y = ToCeiledInt(rect.origin().y());
-  float max_x = rect.origin().x() + rect.size().width();
-  float max_y = rect.origin().y() + rect.size().height();
+  int min_x = ToCeiledInt(rect.x());
+  int min_y = ToCeiledInt(rect.y());
+  float max_x = rect.right();
+  float max_y = rect.bottom();
   int width = std::max(ToFlooredInt(max_x) - min_x, 0);
   int height = std::max(ToFlooredInt(max_y) - min_y, 0);
   return Rect(min_x, min_y, width, height);
 }
 
 Rect ToNearestRect(const RectF& rect) {
-  float float_min_x = rect.origin().x();
-  float float_min_y = rect.origin().y();
-  float float_max_x = float_min_x + rect.size().width();
-  float float_max_y = float_min_y + rect.size().height();
+  float float_min_x = rect.x();
+  float float_min_y = rect.y();
+  float float_max_x = rect.right();
+  float float_max_y = rect.bottom();
 
   int min_x = ToRoundedInt(float_min_x);
   int min_y = ToRoundedInt(float_min_y);
@@ -53,10 +53,10 @@ Rect ToNearestRect(const RectF& rect) {
 }
 
 Rect ToFlooredRectDeprecated(const RectF& rect) {
-  return Rect(ToFlooredInt(rect.origin().x()),
-              ToFlooredInt(rect.origin().y()),
-              ToFlooredInt(rect.size().width()),
-              ToFlooredInt(rect.size().height()));
+  return Rect(ToFlooredInt(rect.x()),
+              ToFlooredInt(rect.y()),
+              ToFlooredInt(rect.width()),
+              ToFlooredInt(rect.height()));
 }
 
 }  // namespace gfx
