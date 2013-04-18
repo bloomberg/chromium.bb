d160a309e620094e188ed2476e9a7a14fffc08df
Jan Arne Petersen <jpetersen@openismus.com>
2013-04-18 16:47:21 +0200

    view: g_type_init() is deprecated in glib >= 2.35
    Signed-off-by: Jan Arne Petersen <jpetersen@openismus.com>

--- clients/view.c
+++ clients/view.c
@@ -293,7 +293,9 @@ main(int argc, char *argv[])
 	int i;
 	int view_counter = 0;
 
+#if !GLIB_CHECK_VERSION(2, 35, 0)
 	g_type_init();
+#endif
 
 	parse_options(view_options, ARRAY_LENGTH(view_options), &argc, argv);
 
