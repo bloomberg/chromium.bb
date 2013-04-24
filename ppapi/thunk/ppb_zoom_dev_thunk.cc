e29281ff8a29657cc05a05c93d41c792f190364b
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 23:53:32 +0000

    Pepper: Update PPB_Zoom thunk.
    I forgot to rebase the patch for generating the thunk for PPB_Zoom_Dev. This
    updates the thunk.cc file to match the current implementation of idl_thunk.py.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14447012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196266 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: decc67aa8357e276c63080170c047335f49e6164

--- ppapi/thunk/ppb_zoom_dev_thunk.cc
+++ ppapi/thunk/ppb_zoom_dev_thunk.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// From dev/ppb_zoom_dev.idl modified Thu Apr 18 10:24:21 2013.
+// From dev/ppb_zoom_dev.idl modified Wed Apr 24 13:31:08 2013.
 
 #include "ppapi/c/dev/ppb_zoom_dev.h"
 #include "ppapi/c/pp_errors.h"
@@ -20,8 +20,9 @@ namespace {
 void ZoomChanged(PP_Instance instance, double factor) {
   VLOG(4) << "PPB_Zoom_Dev::ZoomChanged()";
   EnterInstance enter(instance);
-  if (enter.succeeded())
-    enter.functions()->ZoomChanged(instance, factor);
+  if (enter.failed())
+    return;
+  enter.functions()->ZoomChanged(instance, factor);
 }
 
 void ZoomLimitsChanged(PP_Instance instance,
@@ -29,10 +30,11 @@ void ZoomLimitsChanged(PP_Instance instance,
                        double maximum_factor) {
   VLOG(4) << "PPB_Zoom_Dev::ZoomLimitsChanged()";
   EnterInstance enter(instance);
-  if (enter.succeeded())
-    enter.functions()->ZoomLimitsChanged(instance,
-                                         minimum_factor,
-                                         maximum_factor);
+  if (enter.failed())
+    return;
+  enter.functions()->ZoomLimitsChanged(instance,
+                                       minimum_factor,
+                                       maximum_factor);
 }
 
 const PPB_Zoom_Dev_0_2 g_ppb_zoom_dev_thunk_0_2 = {
