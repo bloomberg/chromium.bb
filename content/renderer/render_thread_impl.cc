b9a738f886571d3f1dd133fc283fdacc6cf7e5a0
hayato@chromium.org <hayato@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-25 15:43:54 +0000

    Remove Shadow DOM runtime flag.
    This is the chromium side patch.
    The blink side patch will follow.
    
    BUG=230744.
    
    Review URL: https://chromiumcodereview.appspot.com/14167011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196408 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fef7a7647de7fd1d29a17df81c27de69e4423233

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -772,8 +772,6 @@ void RenderThreadImpl::EnsureWebKitInitialized() {
 
   WebRuntimeFeatures::enableQuota(true);
 
-  WebRuntimeFeatures::enableShadowDOM(true);
-
   if (command_line.HasSwitch(switches::kEnableExperimentalWebKitFeatures)) {
     WebRuntimeFeatures::enableStyleScoped(true);
     WebRuntimeFeatures::enableCustomDOMElements(true);
