525a57542e7c62cc2e5334a952489b882aef85be
jinsukkim@chromium.org <jinsukkim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 07:20:37 +0000

    Insert a keyword 'GoogleTV' to system device name
    When built against GoogleTV platform, this ensures Chrome for GoogleTV
    has system device name cotaining a keyword 'GoogleTV'. GoogleTV being
    based on Android, this needs to be done in sys_info_android.cc.
    
    Chrome for GoogleTV has a separate binary. The change takes effect
    only for the corresponding binary, not those for mobile/tablet.
    
    BUG=230242
    
    Review URL: https://chromiumcodereview.appspot.com/13863013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196643 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 30d06382b2e09eefe82f5f45ec63018ff47092b4

--- webkit/user_agent/user_agent_util.cc
+++ webkit/user_agent/user_agent_util.cc
@@ -33,6 +33,16 @@ std::string GetWebKitRevision() {
   return WEBKIT_SVN_REVISION;
 }
 
+#if defined(OS_ANDROID)
+std::string GetAndroidDeviceName() {
+  std::string android_device_name = base::SysInfo::GetDeviceName();
+#if defined(GOOGLE_TV)
+  android_device_name += " TV";
+#endif
+  return android_device_name;
+}
+#endif
+
 std::string BuildOSCpuInfo() {
   std::string os_cpu;
 
@@ -88,7 +98,7 @@ std::string BuildOSCpuInfo() {
   // Send information about the device.
   bool semicolon_inserted = false;
   std::string android_build_codename = base::SysInfo::GetAndroidBuildCodename();
-  std::string android_device_name = base::SysInfo::GetDeviceName();
+  std::string android_device_name = GetAndroidDeviceName();
   if ("REL" == android_build_codename && android_device_name.size() > 0) {
     android_info_str += "; " + android_device_name;
     semicolon_inserted = true;
