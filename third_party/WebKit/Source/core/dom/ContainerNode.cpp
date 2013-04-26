a0f3f5e11969c5e49a793c1d5b46670f1a7e6d1d
andrei.bucur@gmail.com <andrei.bucur@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-26 09:51:37 +0000

    Make ContainerNode::removeChildren detach before removing from the tree.
    Similar to http://trac.webkit.org/changeset/148754 by Andrei Bucur.
    
    Currently, ContainerNode::removeChildren initially removes the nodes from the
    DOM tree  and then calls detach() on each of them. This is anti-intuitive and can
    lead to subtle bugs because the detached renderers are not backed by a valid DOM
    tree any more. With the patch, the nodes are first detached and then removed from
    the DOM.
    
    The patch also lets the tree in a consistent state after each node removal by using
    removeBetween which clears dangling pointers (this might have a very, very small
    performance impact because NoEventDispatchAssertion is called for every removed
    node even though it's called for the ContainerNode as well). I haven't found any proof
    the performance will get worse if the detachment happens when the children are still
    in the DOM tree.
    
    BUG=235447
    
    Review URL: https://chromiumcodereview.appspot.com/14392016
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149189 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0cc2a99e0f64e44e5c405ad31b536674816870d7

--- third_party/WebKit/Source/core/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/core/dom/ContainerNode.cpp
@@ -587,39 +587,15 @@ void ContainerNode::removeChildren()
     // and remove... e.g. stop loading frames, fire unload events.
     willRemoveChildren(protect.get());
 
-    Vector<RefPtr<Node>, 10> removedChildren;
+    NodeVector removedChildren;
     {
         WidgetHierarchyUpdatesSuspensionScope suspendWidgetHierarchyUpdates;
         {
             NoEventDispatchAssertion assertNoEventDispatch;
             removedChildren.reserveInitialCapacity(childNodeCount());
-            while (RefPtr<Node> n = m_firstChild) {
-                Node* next = n->nextSibling();
-
-                // Remove the node from the tree before calling detach or removedFromDocument (4427024, 4129744).
-                // removeChild() does this after calling detach(). There is no explanation for
-                // this discrepancy between removeChild() and its optimized version removeChildren().
-                n->setPreviousSibling(0);
-                n->setNextSibling(0);
-                n->setParentOrShadowHostNode(0);
-                document()->adoptIfNeeded(n.get());
-
-                m_firstChild = next;
-                if (n == m_lastChild)
-                    m_lastChild = 0;
-                removedChildren.append(n.release());
-            }
-
-            // Detach the nodes only after properly removed from the tree because
-            // a. detaching requires a proper DOM tree (for counters and quotes for
-            // example) and during the previous loop the next sibling still points to
-            // the node being removed while the node being removed does not point back
-            // and does not point to the same parent as its next sibling.
-            // b. destroying Renderers of standalone nodes is sometimes faster.
-            for (size_t i = 0; i < removedChildren.size(); ++i) {
-                Node* removedChild = removedChildren[i].get();
-                if (removedChild->attached())
-                    removedChild->detach();
+            while (m_firstChild) {
+                removedChildren.append(m_firstChild);
+                removeBetween(0, m_firstChild->nextSibling(), m_firstChild);
             }
         }
 
