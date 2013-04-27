6f674536892e8bd7ef41d743c8ec906d03ba7d54
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-27 02:36:27 +0000

    Reland https://chromiumcodereview.appspot.com/14286004 which was reverted fue to linux_chromeos_asan build break. The cause of the break has since been fixed in https://codereview.chromium.org/14113034/ There are no additional changes from the original CL.
    BUG=115237,168162
    
    TBR=sky@chromium.org,avi@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/14283013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196927 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1400e6dc12c13f62aa300dd4468019a08388daec

--- ui/views/touchui/touch_selection_controller_impl.cc
+++ ui/views/touchui/touch_selection_controller_impl.cc
@@ -4,10 +4,9 @@
 
 #include "ui/views/touchui/touch_selection_controller_impl.h"
 
-#include "base/command_line.h"
 #include "base/time.h"
 #include "grit/ui_strings.h"
-#include "ui/base/ui_base_switches.h"
+#include "ui/base/ui_base_switches_util.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/screen.h"
@@ -420,11 +419,8 @@ ViewsTouchSelectionControllerFactory::ViewsTouchSelectionControllerFactory() {
 
 ui::TouchSelectionController* ViewsTouchSelectionControllerFactory::create(
     ui::TouchEditable* client_view) {
-#if defined(OS_CHROMEOS)
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableTouchEditing))
+  if (switches::IsTouchEditingEnabled())
     return new views::TouchSelectionControllerImpl(client_view);
-#endif
   return NULL;
 }
 
