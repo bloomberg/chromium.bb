9d59a339aee0d603407725d0a234649c5d974762
noelallen@chromium.org <noelallen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-29 16:11:14 +0000

    Add Core example
    As part of the example reorg, we are adding examples for all APIs.
    This CL shows how to use Core to get the time and call on main.
    
    BUG=234855
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/14049038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197054 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 804e103508e5f0c47ead50119ec9ee84e2e2959a

---
+++ native_client_sdk/src/examples/api/core/example.dsc
@@ -0,0 +1,19 @@
+{
+  'TOOLS': ['newlib', 'glibc', 'pnacl', 'win', 'linux'],
+  'TARGETS': [
+    {
+      'NAME' : 'core',
+      'TYPE' : 'main',
+      'SOURCES' : ['core.cc'],
+      'LIBS' : ['ppapi_cpp', 'ppapi', 'pthread']
+    }
+  ],
+  'DATA': [
+    'example.js',
+  ],
+  'DEST': 'examples/api',
+  'NAME': 'core',
+  'TITLE': 'Core',
+  'GROUP': 'API'
+}
+
---
+++ native_client_sdk/src/examples/api/core/example.js
@@ -0,0 +1,68 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var itrMax = 1000;
+var itrCount = 0;
+var itrSend = new Float64Array(itr_max);
+var itrNaCl = new Float64Array(itr_max);
+var itrRecv = new Float64Array(itr_max);
+var delay = 0;
+
+function attachListeners() {
+  document.getElementById('start').addEventListener('click',
+      startTest);
+  count_pp = document.getElementById('count')
+  count_pp.textContent = itrMax;
+}
+
+function startTest() {
+  if (common.naclModule) {
+    var startButton = document.getElementById('start');
+    startButton.disabled = true;
+
+    var delayControl = document.getElementById('delay');
+    delay = parseInt(delayControl.value, 10);
+
+    common.updateStatus('Running Test');
+    itrCount = 0;
+    itrSend[0] = (new Date()).getTime();
+    common.naclModule.postMessage(delay);
+  }
+}
+
+function setStats(nacl, compute, total) {
+  var statNaCl = document.getElementById('NaCl');
+  var statRound = document.getElementById('Round');
+  var statAll = document.getElementById('Total');
+
+  statNaCl.textContent = (nacl / itrMax) + ' ms';
+  statRound.textContent = (compute / itrMax) + ' ms';
+  statAll.textContent = (total / itrMax) + ' ms';
+}
+
+// Called by the common.js module.
+function handleMessage(message_event) {
+  // Convert NaCl Seconds elapsed to MS
+  itrNaCl[itrCount] = message_event.data * 1000.0;
+  itrRecv[itrCount] = (new Date()).getTime();
+  itrCount++;
+
+  if (itrCount == itrMax) {
+    common.updateStatus('Test Finished');
+    var startButton = document.getElementById('start');
+    startButton.disabled = false;
+
+    var naclMS = 0.0;
+    var computeMS = 0.0;
+    for (var i=0; i < itrMax; i++) {
+      naclMS += itrNaCl[i];
+      computeMS += itrRecv[i] - itrSend[i];
+    }
+
+    SetStats(naclMS, computeMS, itrRecv[itrMax - 1] - itrSend[0]);
+  } else {
+    itrSend[itrCount] = (new Date()).getTime();
+    common.naclModule.postMessage(delay);
+  }
+}
