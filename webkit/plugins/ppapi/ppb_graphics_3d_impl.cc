691508a26ef3a3aa7eb9598c3bb02a89d77aa61d
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-29 16:55:18 +0000

    PPAPI: Make Graphics3D impl not use |this| after delete
    BUG=232816
    
    Review URL: https://chromiumcodereview.appspot.com/14061016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197062 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 965666ad3cec772aaa0906b0e055f7e15cc676d7

--- webkit/plugins/ppapi/ppb_graphics_3d_impl.cc
+++ webkit/plugins/ppapi/ppb_graphics_3d_impl.cc
@@ -331,12 +331,17 @@ void PPB_Graphics3D_Impl::SendContextLost() {
   if (!instance || !instance->container())
     return;
 
+  // This PPB_Graphics3D_Impl could be deleted during the call to
+  // GetPluginInterface (which sends a sync message in some cases). We still
+  // send the Graphics3DContextLost to the plugin; the instance may care about
+  // that event even though this context has been destroyed.
+  PP_Instance this_pp_instance = pp_instance();
   const PPP_Graphics3D* ppp_graphics_3d =
       static_cast<const PPP_Graphics3D*>(
           instance->module()->GetPluginInterface(
               PPP_GRAPHICS_3D_INTERFACE));
   if (ppp_graphics_3d)
-    ppp_graphics_3d->Graphics3DContextLost(pp_instance());
+    ppp_graphics_3d->Graphics3DContextLost(this_pp_instance);
 }
 
 }  // namespace ppapi
