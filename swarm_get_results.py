baf9be591660e436b342973430961eee08924fc7
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-01 01:10:23 +0000

    Make swarm_get_results usable as a library.
    This is done so build/scripts/slave/get_swarm_results.py can reuse the same
    code. It will be useful once Swarm enforces tokens.
    
    Make it fetch results in parallel to return results as soon as they arrives,
    instead of going through in order.
    
    R=csharp@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14577002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/swarm_client@197535 0039d316-1c4b-4281-b951-d872f2087c98

--- swarm_get_results.py
+++ swarm_get_results.py
@@ -8,8 +8,13 @@ that name.
 """
 
 import json
+import logging
 import optparse
+import Queue
+import re
 import sys
+import threading
+import time
 import urllib
 
 #from common import find_depot_tools  # pylint: disable=W0611
@@ -19,51 +24,137 @@ import urllib
 
 import run_isolated
 
+SHARD_LINE = re.compile(
+    r'^Note: This is test shard ([0-9]+) of ([0-9]+)\.$', re.MULTILINE)
 
-def get_test_keys(swarm_base_url, test_name):
+
+class Failure(Exception):
+  """Generic failure."""
+  pass
+
+
+def get_test_keys(swarm_base_url, test_name, timeout):
+  """Returns the Swarm test key for each shards of test_name."""
+  assert isinstance(timeout, float)
   key_data = urllib.urlencode([('name', test_name)])
-  test_keys_url = '%s/get_matching_test_cases?%s' % (swarm_base_url, key_data)
-  result = run_isolated.url_open(test_keys_url)
-  if result is None:
-    return []
+  url = '%s/get_matching_test_cases?%s' % (swarm_base_url, key_data)
+  response = run_isolated.url_open(url, retry_404=True, timeout=timeout)
+  if response is None:
+    raise Failure(
+        'Error: Unable to find any tests with the name, %s, on swarm server'
+          % test_name)
+  result = response.read()
+  # TODO(maruel): Compare exact string.
+  if 'No matching' in result:
+    raise Failure(
+        'Error: Unable to find any tests with the name, %s, on swarm server'
+          % test_name)
+  return json.loads(result) or []
 
-  result_str = result.read()
-  if 'No matching' in result_str:
-    print ('Error: Unable to find any tests with the name, %s, on swarm server'
-           % test_name)
-    return []
 
-  # TODO(csharp): return in a proper format (like json)
-  return result_str.split()
+class Bit(object):
+  """Thread safe setable bit."""
+  _lock = threading.Lock()
+  _value = False
+
+  def get(self):
+    with self._lock:
+      return self._value
+
+  def set(self):
+    with self._lock:
+      self._value = True
 
 
-def swarm_get_results(swarm_base_url, test_keys, wait):
-  """Retrieves the given swarm test results from the swarm server and print it
-  to stdout.
+def now():
+  """Exists so it can be mocked easily."""
+  return time.time()
+
+
+def retrieve_results(base_url, test_key, timeout, should_stop):
+  """Retrieves results for a single test_key."""
+  assert isinstance(timeout, float)
+  params = [('r', test_key)]
+  result_url = '%s/get_result?%s' % (base_url, urllib.urlencode(params))
+  start = now()
+  while True:
+    if timeout and (now() - start) >= timeout:
+      logging.warning('retrieve_results(%s) timed out', base_url)
+      return None
+    # Do retries ourselve.
+    response = run_isolated.url_open(
+        result_url, retry_404=False, retry_50x=False)
+    if response is None:
+      # Aggressively poll for results. Do not use retry_404 so
+      # should_stop is polled more often.
+      remaining = min(5, timeout - (now() - start)) if timeout else 5
+      if remaining > 0:
+        run_isolated.HttpService.sleep_before_retry(1, remaining)
+    else:
+      try:
+        data = json.load(response)
+      except (ValueError, TypeError):
+        logging.warning(
+            'Received corrupted data for test_key %s. Retrying.', test_key)
+      else:
+        if data['output']:
+          # Ignore the result code.
+          run_isolated.url_open('%s/cleanup_results' % base_url, data=params)
+          return data
+    if should_stop.get():
+      return None
+
+
+def yield_results(swarm_base_url, shard_count, test_keys, timeout):
+  """Yields swarm test results from the swarm server as (index, result).
+
+  If shard_count < len(test_keys), it will return as soon as all the shards
+  under shard_count are retrieved. It may return duplicate if they are returned
+  before the slowest shard.
   """
-  outputs = []
-  for test in test_keys:
-    result_url = '%s/get_result?r=%s' % (swarm_base_url, test)
-    while True:
-      result = run_isolated.url_open(result_url)
-      if result is None:
-        continue
-      data = json.load(result)
-      if data['output']:
-        outputs.append(data)
-        break
-      if not wait:
-        break
-  return outputs
+  shards_remaining = range(shard_count)
+  results = Queue.Queue()
+  threads = []
+  should_stop = Bit()
+  def append(k):
+    """Swallows exceptions."""
+    try:
+      r = retrieve_results(swarm_base_url, k, timeout, should_stop)
+    except Exception as e:
+      logging.exception('Dang')
+      r = e
+    results.put(r)
+  for test_key in test_keys:
+    t = threading.Thread(target=append, args=(test_key,))
+    t.daemon = True
+    t.start()
+    threads.append(t)
+
+  results_remaining = len(test_keys)
+  try:
+    while shards_remaining and results_remaining:
+      # Retrieve the number of shard count, even if lower than test_keys.
+      result = results.get()
+      results_remaining -= 1
+      if isinstance(result, Exception):
+        raise result
+      if result:
+        # TODO(maruel): Quite adhoc.
+        match = SHARD_LINE.search(result['output'])
+        shard_index = int(match.group(1)) - 1 if match else 0
+        if shard_index in shards_remaining:
+          shards_remaining.remove(shard_index)
+          yield shard_index, result
+        else:
+          logging.warning('Ignoring duplicate shard index %d', shard_index)
+  finally:
+    # Done, kill the remaining threads.
+    should_stop.set()
+    for t in threads:
+      t.join()
 
 
-def print_results(outputs):
-  for output in outputs:
-    print '%s: %s' % (output['machine_id'], output['exit_codes'])
-    print ''.join('  %s\n' % l for l in output['output'].splitlines())
-
-
-def main():
+def parse_args():
   run_isolated.disable_buffering()
   parser = optparse.OptionParser(
       usage='%prog [options] test_name',
@@ -75,26 +166,50 @@ def main():
       '-v', '--verbose', action='store_true',
       help='Print verbose logging')
   parser.add_option(
-      '-n', '--no-wait', action='store_true',
-      help='Do not wait for completion')
+      '-t', '--timeout',
+      type='float',
+      default=run_isolated.URL_OPEN_TIMEOUT,
+      help='Timeout to wait for result, set to 0 for no timeout; default: '
+           '%default s')
+  # TODO(csharp): Change default to 0 once all callers have been updated to
+  # pass in --shards.
+  parser.add_option(
+      '-s', '--shards',
+      default=-1,
+      type=int,
+      help='Specify the number of shards that the test was split into '
+           '(i.e. how many shards should be retrieved from swarm.')
+
   (options, args) = parser.parse_args()
   if not args:
     parser.error('Must specify one test name.')
   elif len(args) > 1:
     parser.error('Must specify only one test name.')
+  options.url = options.url.rstrip('/')
+  logging.basicConfig(level=logging.DEBUG if options.verbose else logging.ERROR)
+  return parser, options, args[0]
 
-  url = options.url.rstrip('/')
-  test_name = args[0]
-  test_keys = get_test_keys(url, test_name)
 
-  outputs = swarm_get_results(url, test_keys, not options.no_wait)
-  print_results(outputs)
+def main():
+  parser, options, test_name = parse_args()
+  try:
+    test_keys = get_test_keys(options.url, test_name, options.timeout)
+  except Failure as e:
+    parser.error(e.args[0])
+  if not test_keys:
+    parser.error('No test keys to get results with.')
 
-  exit_codes = []
-  for output in outputs:
-    exit_codes.extend(map(int, output['exit_codes'].split(',')))
+  options.shards = len(test_keys) if options.shards == -1 else options.shards
+  exit_code = None
+  for _index, output in yield_results(
+      options.url, options.shards, test_keys, options.timeout):
+    print(
+        '%s/%s: %s' % (
+            output['machine_id'], output['machine_tag'], output['exit_codes']))
+    print(''.join('  %s\n' % l for l in output['output'].splitlines()))
+    exit_code = max(exit_code, max(map(int, output['exit_codes'].split(','))))
 
-  return max(exit_codes)
+  return exit_code
 
 
 if __name__ == '__main__':
---
+++ tests/swarm_get_results_test.py
@@ -0,0 +1,384 @@
+#!/usr/bin/env python
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import json
+import logging
+import os
+import StringIO
+import sys
+import threading
+import unittest
+import urllib2
+
+import auto_stub
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, ROOT_DIR)
+
+import swarm_get_results
+
+
+TEST_CASE_SUCCESS = (
+  '[----------] 2 tests from StaticCookiePolicyTest\n'
+  '[ RUN      ] StaticCookiePolicyTest.AllowAllCookiesTest\n'
+  '[       OK ] StaticCookiePolicyTest.AllowAllCookiesTest (0 ms)\n'
+  '[ RUN      ] StaticCookiePolicyTest.BlockAllCookiesTest\n'
+  '[       OK ] StaticCookiePolicyTest.BlockAllCookiesTest (0 ms)\n'
+  '[----------] 2 tests from StaticCookiePolicyTest (0 ms total)\n'
+  '\n'
+  '[----------] 1 test from TCPListenSocketTest\n'
+  '[ RUN      ] TCPListenSocketTest.ServerSend\n'
+  '[       OK ] TCPListenSocketTest.ServerSend (1 ms)\n'
+  '[----------] 1 test from TCPListenSocketTest (1 ms total)\n')
+
+
+TEST_CASE_FAILURE = (
+  '[----------] 2 tests from StaticCookiePolicyTest\n'
+  '[ RUN      ] StaticCookiePolicyTest.AllowAllCookiesTest\n'
+  '[       OK ] StaticCookiePolicyTest.AllowAllCookiesTest (0 ms)\n'
+  '[ RUN      ] StaticCookiePolicyTest.BlockAllCookiesTest\n'
+  'C:\\win\\build\\src\\chrome\\test.cc: error: Value of: result()\n'
+  '  Actual: false\n'
+  'Expected: true\n'
+  '[  FAILED  ] StaticCookiePolicyTest.BlockAllCookiesTest (0 ms)\n'
+  '[----------] 2 tests from StaticCookiePolicyTest (0 ms total)\n'
+  '\n'
+  '[----------] 1 test from TCPListenSocketTest\n'
+  '[ RUN      ] TCPListenSocketTest.ServerSend\n'
+  '[       OK ] TCPListenSocketTest.ServerSend (1 ms)\n'
+  '[----------] 1 test from TCPListenSocketTest (1 ms total)\n')
+
+
+SWARM_OUTPUT_SUCCESS = (
+  '[ RUN      ] unittests.Run Test\n' +
+  TEST_CASE_SUCCESS +
+  '[       OK ] unittests.Run Test (2549 ms)\n'
+  '[ RUN      ] unittests.Clean Up\n'
+  'No output!\n'
+  '[       OK ] unittests.Clean Up (6 ms)\n'
+  '\n'
+  '[----------] unittests summary\n'
+  '[==========] 2 tests ran. (2556 ms total)\n')
+
+
+SWARM_OUTPUT_FAILURE = (
+  '[ RUN      ] unittests.Run Test\n' +
+  TEST_CASE_FAILURE +
+  '[       OK ] unittests.Run Test (2549 ms)\n'
+  '[ RUN      ] unittests.Clean Up\n'
+  'No output!\n'
+  '[       OK ] unittests.Clean Up (6 ms)\n'
+  '\n'
+  '[----------] unittests summary\n'
+  '[==========] 2 tests ran. (2556 ms total)\n')
+
+
+SWARM_OUTPUT_WITH_NO_TEST_OUTPUT = (
+  '\n'
+  'Unable to connection to swarm machine.\n')
+
+
+TEST_SHARD_1 = 'Note: This is test shard 1 of 3.'
+TEST_SHARD_2 = 'Note: This is test shard 2 of 3.'
+TEST_SHARD_3 = 'Note: This is test shard 3 of 3.'
+
+
+SWARM_SHARD_OUTPUT = (
+  '[ RUN      ] unittests.Run Test\n'
+  '%s\n'
+  '[       OK ] unittests.Run Test (2549 ms)\n'
+  '[ RUN      ] unittests.Clean Up\n'
+  'No output!\n'
+  '[       OK ] unittests.Clean Up (6 ms)\n'
+  '\n'
+  '[----------] unittests summary\n'
+  '[==========] 2 tests ran. (2556 ms total)\n')
+
+
+TEST_SHARD_OUTPUT_1 = SWARM_SHARD_OUTPUT % TEST_SHARD_1
+TEST_SHARD_OUTPUT_2 = SWARM_SHARD_OUTPUT % TEST_SHARD_2
+TEST_SHARD_OUTPUT_3 = SWARM_SHARD_OUTPUT % TEST_SHARD_3
+
+
+def gen_data(shard_output, exit_codes):
+  return {
+    u'exit_codes': unicode(exit_codes),
+    u'machine_id': u'host',
+    u'machine_tag': u'localhost',
+    u'output': unicode(shard_output),
+  }
+
+
+def generate_url_response(shard_output, exit_codes):
+  url_response = urllib2.addinfourl(
+      StringIO.StringIO(json.dumps(gen_data(shard_output, exit_codes))),
+      'mock message',
+      'host')
+  url_response.code = 200
+  url_response.msg = 'OK'
+  return url_response
+
+
+def get_swarm_results(shard_count, keys):
+  """Simplifies the call to yield_results().
+
+  The timeout is hard-coded to 10 seconds.
+  """
+  return list(
+      swarm_get_results.yield_results(
+          'http://host:9001', shard_count, keys, 10.))
+
+
+class TestCase(auto_stub.TestCase):
+  """Base class that defines the url_open mock."""
+  def setUp(self):
+    super(TestCase, self).setUp()
+    self._lock = threading.Lock()
+    self.requests = []
+    self.mock(
+        swarm_get_results.run_isolated, 'url_open',
+        self._url_open)
+
+  def tearDown(self):
+    try:
+      if not self.has_failed():
+        self.assertEqual([], self.requests)
+    finally:
+      super(TestCase, self).tearDown()
+
+  def _url_open(self, url, **kwargs):
+    logging.info('url_open(%s)', url)
+    with self._lock:
+      # Since the client is multi-threaded, requests can be processed out of
+      # order.
+      for index, r in enumerate(self.requests):
+        if r[0] == url and r[1] == kwargs:
+          _, _, returned = self.requests.pop(index)
+          break
+      else:
+        self.fail('Failed to find url %s' % url)
+    return returned
+
+
+class TestGetTestKeys(TestCase):
+  def test_no_keys(self):
+    self.requests = [
+      (
+        'http://host:9001/get_matching_test_cases?name=my_test',
+        {'retry_404': True, 'timeout': 0.0},
+        StringIO.StringIO('No matching Test Cases'),
+      ),
+    ]
+    try:
+      swarm_get_results.get_test_keys('http://host:9001', 'my_test', 0.)
+      self.fail()
+    except swarm_get_results.Failure as e:
+      msg = (
+          'Error: Unable to find any tests with the name, my_test, on swarm '
+          'server')
+      self.assertEqual(msg, e.args[0])
+
+  def test_find_keys(self):
+    keys = ['key_1', 'key_2']
+    self.requests = [
+      (
+        'http://host:9001/get_matching_test_cases?name=my_test',
+        {'retry_404': True, 'timeout': 0.0},
+        StringIO.StringIO(json.dumps(keys)),
+      ),
+    ]
+    actual = swarm_get_results.get_test_keys('http://host:9001', 'my_test', 0.)
+    self.assertEqual(keys, actual)
+
+
+class TestGetSwarmResults(TestCase):
+  def test_success(self):
+    self.requests = [
+      (
+        'http://host:9001/get_result?r=key1',
+        {'retry_404': False, 'retry_50x': False},
+        generate_url_response(SWARM_OUTPUT_SUCCESS, '0, 0'),
+      ),
+      (
+        'http://host:9001/cleanup_results',
+        {'data': [('r', 'key1')]},
+        StringIO.StringIO(''),
+      ),
+    ]
+    expected = [(0, gen_data(SWARM_OUTPUT_SUCCESS, '0, 0'))]
+    actual = get_swarm_results(1, ['key1'])
+    self.assertEqual(expected, actual)
+
+  def test_failure(self):
+    self.requests = [
+      (
+        'http://host:9001/get_result?r=key1',
+        {'retry_404': False, 'retry_50x': False},
+        generate_url_response(SWARM_OUTPUT_FAILURE, '0, 1'),
+      ),
+      (
+        'http://host:9001/cleanup_results',
+        {'data': [('r', 'key1')]},
+        StringIO.StringIO(''),
+      ),
+    ]
+    expected = [(0, gen_data(SWARM_OUTPUT_FAILURE, '0, 1'))]
+    actual = get_swarm_results(1, ['key1'])
+    self.assertEqual(expected, actual)
+
+  def test_no_test_output(self):
+    self.requests = [
+      (
+        'http://host:9001/get_result?r=key1',
+        {'retry_404': False, 'retry_50x': False},
+        generate_url_response(SWARM_OUTPUT_WITH_NO_TEST_OUTPUT, '0, 0'),
+      ),
+      (
+        'http://host:9001/cleanup_results',
+        {'data': [('r', 'key1')]},
+        StringIO.StringIO(''),
+      ),
+    ]
+    expected = [(0, gen_data(SWARM_OUTPUT_WITH_NO_TEST_OUTPUT, '0, 0'))]
+    actual = get_swarm_results(1, ['key1'])
+    self.assertEqual(expected, actual)
+
+  def test_no_keys(self):
+    actual = get_swarm_results(1, [])
+    self.assertEqual([], actual)
+
+  def test_url_errors(self):
+    # NOTE: get_swarm_results() hardcodes timeout=10. range(12) is because of an
+    # additional time.time() call deep in run_isolated.url_open().
+    now = {}
+    lock = threading.Lock()
+    def get_now():
+      t = threading.current_thread()
+      with lock:
+        return now.setdefault(t, range(12)).pop(0)
+    self.mock(
+        swarm_get_results.run_isolated.HttpService,
+        'sleep_before_retry',
+        staticmethod(lambda _x, _y: None))
+    self.mock(swarm_get_results, 'now', get_now)
+    # The actual number of requests here depends on 'now' progressing to 10
+    # seconds. It's called twice per loop.
+    self.requests = [
+      (
+        'http://host:9001/get_result?r=key1',
+        {'retry_404': False, 'retry_50x': False},
+        None,
+      ),
+      (
+        'http://host:9001/get_result?r=key1',
+        {'retry_404': False, 'retry_50x': False},
+        None,
+      ),
+      (
+        'http://host:9001/get_result?r=key1',
+        {'retry_404': False, 'retry_50x': False},
+        None,
+      ),
+      (
+        'http://host:9001/get_result?r=key1',
+        {'retry_404': False, 'retry_50x': False},
+        None,
+      ),
+      (
+        'http://host:9001/get_result?r=key1',
+        {'retry_404': False, 'retry_50x': False},
+        None,
+      ),
+    ]
+    actual = get_swarm_results(1, ['key1'])
+    self.assertEqual([], actual)
+    self.assertTrue(all(not v for v in now.itervalues()), now)
+
+  def test_shard_repeated(self):
+    self.requests = [
+      (
+        'http://host:9001/get_result?r=key1',
+        {'retry_404': False, 'retry_50x': False},
+        generate_url_response(SWARM_OUTPUT_SUCCESS, '0, 0'),
+      ),
+      (
+        'http://host:9001/cleanup_results',
+        {'data': [('r', 'key1')]},
+        StringIO.StringIO(''),
+      ),
+      (
+        'http://host:9001/get_result?r=key1-repeat',
+        {'retry_404': False, 'retry_50x': False},
+        generate_url_response(SWARM_OUTPUT_SUCCESS, '0, 0'),
+      ),
+      (
+        'http://host:9001/cleanup_results',
+        {'data': [('r', 'key1-repeat')]},
+        StringIO.StringIO(''),
+      ),
+    ]
+    expected = [(0, gen_data(SWARM_OUTPUT_SUCCESS, '0, 0'))]
+    actual = get_swarm_results(1, ['key1', 'key1-repeat'])
+    self.assertEqual(expected, actual)
+
+  def test_one_shard_repeated(self):
+    """Have shard 1 repeated twice, then shard 2 and 3."""
+    self.requests = [
+      (
+        'http://host:9001/get_result?r=key1',
+        {'retry_404': False, 'retry_50x': False},
+        generate_url_response(TEST_SHARD_OUTPUT_1, '0, 0'),
+      ),
+      (
+        'http://host:9001/cleanup_results',
+        {'data': [('r', 'key1')]},
+        StringIO.StringIO(''),
+      ),
+      (
+        'http://host:9001/get_result?r=key1-repeat',
+        {'retry_404': False, 'retry_50x': False},
+        generate_url_response(TEST_SHARD_OUTPUT_1, '0, 0'),
+      ),
+      (
+        'http://host:9001/cleanup_results',
+        {'data': [('r', 'key1-repeat')]},
+        StringIO.StringIO(''),
+      ),
+      (
+        'http://host:9001/get_result?r=key2',
+        {'retry_404': False, 'retry_50x': False},
+        generate_url_response(TEST_SHARD_OUTPUT_2, '0, 0'),
+      ),
+      (
+        'http://host:9001/cleanup_results',
+        {'data': [('r', 'key2')]},
+        StringIO.StringIO(''),
+      ),
+      (
+        'http://host:9001/get_result?r=key3',
+        {'retry_404': False, 'retry_50x': False},
+        generate_url_response(TEST_SHARD_OUTPUT_3, '0, 0'),
+      ),
+      (
+        'http://host:9001/cleanup_results',
+        {'data': [('r', 'key3')]},
+        StringIO.StringIO(''),
+      ),
+    ]
+    expected = [
+      (0, gen_data(TEST_SHARD_OUTPUT_1, '0, 0')),
+      (1, gen_data(TEST_SHARD_OUTPUT_2, '0, 0')),
+      (2, gen_data(TEST_SHARD_OUTPUT_3, '0, 0')),
+    ]
+    actual = get_swarm_results(3, ['key1', 'key1-repeat', 'key2', 'key3'])
+    self.assertEqual(expected, sorted(actual))
+
+
+if __name__ == '__main__':
+  logging.basicConfig(
+      level=logging.DEBUG if '-v' in sys.argv else logging.ERROR)
+  if '-v' in sys.argv:
+    unittest.TestCase.maxDiff = None
+  unittest.main()
