df672ab5849d8c024b404920e56330b2b2f6c387
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-01 22:16:30 +0000

    Revert 197717 "Create a frame tree in WebContents on the browser..." for DCHECK fail on mac browser_tests.
    > Create a frame tree in WebContents on the browser side.
    > 
    > The first step to supporting out-of-process iframes is to replicate the frame tree for each page to the browser process. This CL adds a FrameTreeNode class and forms a tree of such nodes in WebContents. Follow up changes will utilize this class to hold frame-specific functionality.
    > 
    > BUG=235879
    > 
    > Review URL: https://chromiumcodereview.appspot.com/14374002
    
    TBR=nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/14598013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197727 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 108f60905372f2e85e3036cda066593dbbf52fbc

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2788,8 +2788,6 @@ WebCookieJar* RenderViewImpl::cookieJar(WebFrame* frame) {
 }
 
 void RenderViewImpl::didCreateFrame(WebFrame* parent, WebFrame* child) {
-  Send(new ViewHostMsg_FrameAttached(routing_id_, parent->identifier(),
-      child->identifier(), UTF16ToUTF8(child->assignedName()).c_str()));
 }
 
 void RenderViewImpl::didDisownOpener(WebKit::WebFrame* frame) {
@@ -2805,12 +2803,7 @@ void RenderViewImpl::didDisownOpener(WebKit::WebFrame* frame) {
 }
 
 void RenderViewImpl::frameDetached(WebFrame* frame) {
-  int64 parent_frame_id = -1;
-  if (frame->parent())
-    parent_frame_id = frame->parent()->identifier();
-
-  Send(new ViewHostMsg_FrameDetached(routing_id_, parent_frame_id,
-      frame->identifier()));
+  Send(new ViewHostMsg_FrameDetached(routing_id_, frame->identifier()));
 
   FOR_EACH_OBSERVER(RenderViewObserver, observers_, FrameDetached(frame));
 }
