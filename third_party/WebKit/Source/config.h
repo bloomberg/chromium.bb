2f2a73eae5152b534f3d94a1532ab7f14ea5380c
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-01 20:12:45 +0000

    Chop off more unused bits from config.h.
    build/common.gypi already defines _WIN32_WINNT and WINVER via a /D flag.
    It also defines WIN32_LEAN_AND_MEAN which is a superset of _WINSOCKAPI_.
    
    No intended functionality change.
    
    BUG=none
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/14598005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149535 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 9c6d7a316e66ba44188a12568c33e65415740d85

--- third_party/WebKit/Source/config.h
+++ third_party/WebKit/Source/config.h
@@ -27,14 +27,6 @@
 
 #if OS(WINDOWS)
 
-#ifndef _WIN32_WINNT
-#define _WIN32_WINNT 0x0502
-#endif
-
-#ifndef WINVER
-#define WINVER 0x0502
-#endif
-
 // If we don't define these, they get defined in windef.h.
 // We want to use std::min and std::max.
 #ifndef max
@@ -42,10 +34,6 @@
 #endif
 #ifndef min
 #define min min
-#endif
-
-#ifndef _WINSOCKAPI_
-#define _WINSOCKAPI_ // Prevent inclusion of winsock.h in windows.h
 #endif
 
 #endif /* OS(WINDOWS) */
