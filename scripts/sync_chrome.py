21aa0be4e326392865fc295a8432050ef289da6e
Ryan Cui <rcui@chromium.org>
2013-05-02 10:34:21 -0700

    sync_chrome: Sync chrome twice when run with --reset.
    There's a bug in 'gclient sync -D' that gets hit when the location of a
    dependency checkout (in the DEPS file) is moved to a path that contains
    (in a directory fashion) its old path.  I.e., when Blink is moved from
    Webkit/Source/ to Webkit/.  When this happens, a 'gclient sync -D' will
    blow away Webkit/Source/ after the sync, since it is no longer in the
    DEPS file, leaving the Blink checkout missing a Source/ subdirectory.
    
    This bug gets hit the other way around - i.e., if Blink moves from
    Webkit/ to Webkit/Source/.
    
    To work around this, we sync twice, so that any directories deleted by
    the first sync will be restored in the second.
    
    BUG=chromium:237234
    TEST=Trybots
    
    Change-Id: Iff632aa642737f4b55d6072fabec29d101be48b4
    Reviewed-on: https://gerrit.chromium.org/gerrit/49819
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: David James <davidjames@chromium.org>
    Tested-by: David James <davidjames@chromium.org>
    Commit-Queue: Ryan Cui <rcui@chromium.org>

--- scripts/sync_chrome.py
+++ scripts/sync_chrome.py
@@ -4,6 +4,8 @@
 
 """Sync the Chrome source code used by Chrome OS to the specified directory."""
 
+import functools
+
 from chromite.lib import commandline
 from chromite.lib import cros_build_lib
 from chromite.lib import gclient
@@ -48,5 +50,28 @@ def main(args):
   # Sync new Chrome.
   gclient.WriteConfigFile(options.gclient, options.chrome_root,
                           options.internal, options.pdf, options.version)
-  gclient.Sync(options.gclient, options.chrome_root, options.reset)
+  sync_fn = functools.partial(
+      gclient.Sync, options.gclient, options.chrome_root, reset=options.reset)
+
+  # Sync twice when run with --reset, which implies 'gclient sync -D'.
+  #
+  # There's a bug with 'gclient sync -D' that gets hit when the location of a
+  # dependency checkout (in the DEPS file) is moved to a path that contains
+  # (in a directory fashion) its old path.  E.g., when Blink is moved from
+  # Webkit/Source/ to Webkit/.  When this happens, a 'gclient sync -D' will
+  # blow away Webkit/Source/ after the sync, since it is no longer in the
+  # DEPS file, leaving the Blink checkout missing a Source/ subdirectory.
+  #
+  # This bug also gets hit the other way around - E.g., if Blink moves from
+  # Webkit/ to Webkit/Source/.
+  #
+  # To work around this, we sync twice, so that any directories deleted by
+  # the first sync will be restored in the second.
+  #
+  # TODO(rcui): Remove this workaround when the bug is fixed in gclient, or
+  # replace with a more sophisticated solution that syncs twice only when any
+  # paths in the DEPS file cannot be found after initial sync.
+  if options.reset:
+    sync_fn()
+  sync_fn()
   return 0
