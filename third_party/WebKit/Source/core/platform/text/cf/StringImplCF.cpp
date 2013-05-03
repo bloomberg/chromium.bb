fe0548b17654fb45b9665db8db24fd59d0273aa2
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-03 02:30:08 +0000

    Remove garbageCollectionEnabled
    Chromium never compiles with Cocoa GC turned on and this function was left
    from Webkit which had a real check for GC, now this function just returns false
    so we can remove it.
    
    Review URL: https://chromiumcodereview.appspot.com/14829002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149629 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 31c061ec65696ed2ce8fe745321abb70f46f2ed3

--- third_party/WebKit/Source/core/platform/text/cf/StringImplCF.cpp
+++ third_party/WebKit/Source/core/platform/text/cf/StringImplCF.cpp
@@ -29,11 +29,6 @@
 #include <wtf/RetainPtr.h>
 #include <wtf/Threading.h>
 
-static inline bool garbageCollectionEnabled()
-{
-    return false;
-}
-
 namespace WTF {
 
 namespace StringWrapperCFAllocator {
@@ -117,7 +112,6 @@ namespace StringWrapperCFAllocator {
 
     static CFAllocatorRef create()
     {
-        ASSERT(!garbageCollectionEnabled());
         CFAllocatorContext context = { 0, 0, retain, release, copyDescription, allocate, reallocate, deallocate, preferredSize };
         return CFAllocatorCreate(0, &context);
     }
@@ -134,7 +128,7 @@ RetainPtr<CFStringRef> StringImpl::createCFString()
 {
     // Since garbage collection isn't compatible with custom allocators, we
     // can't use the NoCopy variants of CFStringCreate*() when GC is enabled.
-    if (!m_length || !isMainThread() || garbageCollectionEnabled()) {
+    if (!m_length || !isMainThread()) {
         if (is8Bit())
             return adoptCF(CFStringCreateWithBytes(0, reinterpret_cast<const UInt8*>(characters8()), m_length, kCFStringEncodingISOLatin1, false));
         return adoptCF(CFStringCreateWithCharacters(0, reinterpret_cast<const UniChar*>(characters16()), m_length));
