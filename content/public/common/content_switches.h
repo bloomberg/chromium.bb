38746eb06e61a9e5beae82fc532a306c3b7df7c0
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-03 00:13:28 +0000

    Export new switch to fix component build
    Review URL: https://chromiumcodereview.appspot.com/14784008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198013 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3d0ffb67f2864c823b27ee1890ab2891e5a387a0

--- content/public/common/content_switches.h
+++ content/public/common/content_switches.h
@@ -129,7 +129,7 @@ CONTENT_EXPORT extern const char kEnableSoftwareCompositingGLAdapter[];
 CONTENT_EXPORT extern const char kEnableSmoothScrolling[];
 CONTENT_EXPORT extern const char kEnableStatsTable[];
 extern const char kEnableStrictSiteIsolation[];
-extern const char kEnableSynchronousRendererCompositor[];
+CONTENT_EXPORT extern const char kEnableSynchronousRendererCompositor[];
 CONTENT_EXPORT extern const char kEnableThreadedCompositing[];
 CONTENT_EXPORT extern const char kDisableThreadedCompositing[];
 extern const char kEnableVirtualGLContexts[];
