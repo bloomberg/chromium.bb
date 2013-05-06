e141a743786e08dbf829fa49ae9f70ba844693b1
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-06 17:12:25 +0000

    Improve mitigation from r109406 to reset layout root in cases when document is getting destroyed.
    BUG=209604
    TEST=No test since it is flaky and will be tracked in a seperate functional bug to analyze why RenderSVGRoot is left as a layout root. Fix tested under ASAN.
    R=schenney@chromium.org
    
    Review URL: https://codereview.chromium.org/14846011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149745 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 423da3318b236e4d4144c84a641b275a67b4467f

--- third_party/WebKit/Source/core/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/core/rendering/RenderObject.cpp
@@ -2367,7 +2367,7 @@ bool RenderObject::isSelectionBorder() const
 
 inline void RenderObject::clearLayoutRootIfNeeded() const
 {
-    if (!documentBeingDestroyed() && frame()) {
+    if (frame()) {
         if (FrameView* view = frame()->view()) {
             if (view->layoutRoot() == this) {
                 ASSERT_NOT_REACHED();
