bbb27c665f7ecf214e628b0d1d1112d4dfff4656
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-07 21:52:15 +0000

    Delete earlywarningsystem.py.
    We use trybots now.
    
    BUG=none
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/14876004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149895 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 7bd4299fff2a89805eaa17b4a5871bb693b7601d

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/__init__.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/__init__.py
@@ -7,13 +7,11 @@ from webkitpy.tool.commands.bugfortest import BugForTest
 from webkitpy.tool.commands.bugsearch import BugSearch
 from webkitpy.tool.commands.chromechannels import ChromeChannels
 from webkitpy.tool.commands.download import *
-from webkitpy.tool.commands.earlywarningsystem import *
 from webkitpy.tool.commands.findusers import FindUsers
 from webkitpy.tool.commands.gardenomatic import GardenOMatic
 from webkitpy.tool.commands.openbugs import OpenBugs
 from webkitpy.tool.commands.prettydiff import PrettyDiff
 from webkitpy.tool.commands.queries import *
-from webkitpy.tool.commands.queues import *
 from webkitpy.tool.commands.rebaseline import Rebaseline
 from webkitpy.tool.commands.rebaselineserver import RebaselineServer
 from webkitpy.tool.commands.roll import *
