bce9a12925190c92354ed771ad93575ce2851f25
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-07 14:15:57 +0000

    Disable ExtensionContextMenuBrowserTest.Separators on Windows
    TBR=asargent@chromium.org
    NOTRY=True
    BUG=238636
    
    Review URL: https://chromiumcodereview.appspot.com/14581009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198720 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b7cc2e784ad4c7c261595bf1766e307c043708b2

--- chrome/browser/extensions/extension_context_menu_browsertest.cc
+++ chrome/browser/extensions/extension_context_menu_browsertest.cc
@@ -372,6 +372,12 @@ static void VerifyMenuForSeparatorsTest(const MenuModel& menu) {
   ExpectLabelAndType("normal3", MenuModel::TYPE_COMMAND, menu, index++);
 }
 
+#if defined(OS_WIN)
+#define MAYBE_Separators DISABLED_Separators
+#else
+#define MAYBE_Separators Separators
+#endif
+
 // Tests a number of cases for auto-generated and explicitly added separators.
 IN_PROC_BROWSER_TEST_F(ExtensionContextMenuBrowserTest, Separators) {
   // Load the extension.
