b4f5e9e756c24234ef25d01f7a08be40b5811e55
vabr@chromium.org <vabr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-08 13:16:44 +0000

    Disabling ExtensionApiTest.TabHighlight for being flaky.
    BUG=239022
    TBR=jstritar
    
    Review URL: https://codereview.chromium.org/15059007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198886 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7c4f1ad9c50f9cd3c3b4b776abcb213dffa2ef0c

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -129,7 +129,13 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, TabQuery) {
   ASSERT_TRUE(RunExtensionSubtest("tabs/basics", "query.html")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, TabHighlight) {
+// Flaky on windows: http://crbug.com/239022
+#if defined(OS_WIN)
+#define MAYBE_TabHighlight DISABLED_TabHighlight
+#else
+#define MAYBE_TabHighlight TabHighlight
+#endif
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_TabHighlight) {
   ASSERT_TRUE(RunExtensionSubtest("tabs/basics", "highlight.html")) << message_;
 }
 
