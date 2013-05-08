7f462bccc072e0d83b2e7c827775076cc1b88ef2
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-08 20:34:16 +0000

    Fix typographical errors in src/chromeos/ime/ibus_keymap.cc.
    BUG=234949
    
    Review URL: https://chromiumcodereview.appspot.com/14802006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198984 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 983be7dcb50da2f6ffc6be258026521feec7a759

--- chromeos/ime/ibus_keymap.cc
+++ chromeos/ime/ibus_keymap.cc
@@ -147,7 +147,7 @@ std::string GetIBusKeyCode(uint16 keycode) {
     case 0x0020: return "KeyO";
     case 0x0021: return "KeyP";
     case 0x0022: return "BracketLeft";
-    case 0x0023: return "BlacketRight";
+    case 0x0023: return "BracketRight";
     case 0x0024: return "Enter";
     case 0x0025: return "ControlLeft";
     case 0x0026: return "KeyA";
@@ -232,8 +232,8 @@ std::string GetIBusKeyCode(uint16 keycode) {
     case 0x005b: return "NumpadDecimal";
     case 0x0068: return "NumpadEnter";
     case 0x006a: return "NumpadDivide";
-    case 0x00bb: return "NumpadParentLeft";
-    case 0x00bc: return "NumpadParentRight";
+    case 0x00bb: return "NumpadParenLeft";
+    case 0x00bc: return "NumpadParenRight";
 
     // Unsupported keys
 
