e6abdf2ab619e5462aa91e8d0fed02a9a6b88a0e
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-08 22:30:36 +0000

    Consider mixed-content iframes to be mixed script instead of mixed display.
    BUG=238484
    TEST=http/tests/security/mixedContent/*
    R=abarth@chromium.org, tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/15018007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149986 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ffe1e0fdf9dbf34ce8b8e94e0347fbe0b9584da9

--- third_party/WebKit/Source/core/loader/DocumentLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentLoader.cpp
@@ -479,7 +479,7 @@ void DocumentLoader::willSendRequest(ResourceRequest& newRequest, const Resource
 
     Frame* top = m_frame->tree()->top();
     if (top != m_frame) {
-        if (!frameLoader()->mixedContentChecker()->canDisplayInsecureContent(top->document()->securityOrigin(), newRequest.url())) {
+        if (!frameLoader()->mixedContentChecker()->canRunInsecureContent(top->document()->securityOrigin(), newRequest.url())) {
             cancelMainResourceLoad(frameLoader()->cancelledError(newRequest));
             return;
         }
