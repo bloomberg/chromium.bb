dfe74ffd38b6f9c6c19f38987593159fb0676a83
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-09 08:31:44 +0000

    Update the volume and audio tray view when TrayAudio is updated.
    BUG=238853
    
    Review URL: https://chromiumcodereview.appspot.com/14630015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199163 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d9c7755e6c948dcd57cd6777b22b32602741d0ea

--- ash/system/chromeos/audio/tray_audio.cc
+++ ash/system/chromeos/audio/tray_audio.cc
@@ -615,10 +615,14 @@ void TrayAudio::OnActiveInputNodeChanged() {
 }
 
 void TrayAudio::Update() {
+  if (tray_view())
+      tray_view()->SetVisible(GetInitialVisibility());
   if (audio_detail_)
     audio_detail_->Update();
-  if (volume_view_)
+  if (volume_view_) {
+    volume_view_->SetVolumeLevel(GetVolumeLevel());
     volume_view_->Update();
+  }
 }
 
 }  // namespace internal
