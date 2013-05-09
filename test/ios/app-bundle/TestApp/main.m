c0d279c53da37dfdb2984a13a802c622e13af728
thakis@chromium.org <thakis@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2013-05-09 00:10:52 +0000

    Support iOS application bundle structure.
    Ninja was defaulting to building the mac desktop bundle structure. Added a way
    to build the iOS structure. This is triggered by a target having an
    IPHONEOS_DEPLOYMENT_TARGET entry in their xcode_settings.
    
    BUG=chromium:236517
    Review URL: https://codereview.chromium.org/14367052/
    
    patch from Justin Cohen <justincohen@chromium.org>!
    
    
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1630 78cadc50-ecff-11dd-a971-7dbc132099af

---
+++ test/ios/app-bundle/TestApp/main.m
@@ -0,0 +1,12 @@
+// Copyright (c) 2011 Google Inc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, nil);
+  }
+}
