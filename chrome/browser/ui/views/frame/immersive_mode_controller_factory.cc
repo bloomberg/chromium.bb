6ad0d4558dd0ca3180123aab1d53dbf17a47fa0e
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-09 19:01:59 +0000

    cros: Enable immersive fullscreen by default
    This is basically a revert of 198085 "cros: Disable immersive fullscreen by default for..."
    
    BUG=239481
    TEST=browser_tests
    TBR=pkotwicz@chromium.org
    
    Review URL: https://codereview.chromium.org/14969009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199251 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5e3a67e47ed269a7aed466e580a4c4c2cba629a7

--- chrome/browser/ui/views/frame/immersive_mode_controller_factory.cc
+++ chrome/browser/ui/views/frame/immersive_mode_controller_factory.cc
@@ -21,21 +21,18 @@ bool UseImmersiveFullscreen() {
   // Kiosk mode needs the whole screen.
   if (command->HasSwitch(switches::kKioskMode))
     return false;
-  // Immersive fullscreen is off by default. If you change the default you must
+  // Immersive fullscreen is on by default. If you change the default you must
   // change the enable function below and BrowserTest FullscreenBookmarkBar
   // (which cannot depend on this function due to DEPS).
-  return command->HasSwitch(ash::switches::kAshEnableImmersiveFullscreen);
+  return !command->HasSwitch(ash::switches::kAshDisableImmersiveFullscreen);
 #endif
   return false;
 }
 
 // Implemented here so all the code dealing with flags lives in one place.
 void EnableImmersiveFullscreenForTest() {
-#if defined(OS_CHROMEOS)
-  // Immersive fullscreen is off by default.
-  CommandLine::ForCurrentProcess()->AppendSwitch(
-      ash::switches::kAshEnableImmersiveFullscreen);
-#endif
+  // Immersive fullscreen is on by default. If we turn it off, this function
+  // will need to add kAshEnableImmersiveFullscreen to the command line.
 }
 
 ImmersiveModeController* CreateImmersiveModeController() {
