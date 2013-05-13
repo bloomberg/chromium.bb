ebe547b5e804f1d5a160733681576af145e3f4f3
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-13 17:29:52 +0000

    Error event was fired synchronously blowing away the input element from underneath. Remove the FIXME and fire it asynchronously using errorEventSender().
    BUG=240124
    
    Review URL: https://chromiumcodereview.appspot.com/14741011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150232 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 7d085fbb43b21e959900b94f191588fd10546a94

--- third_party/WebKit/Source/core/loader/ImageLoader.cpp
+++ third_party/WebKit/Source/core/loader/ImageLoader.cpp
@@ -211,8 +211,8 @@ void ImageLoader::updateFromElement()
             clearFailedLoadURL();
     } else if (!attr.isNull()) {
         // Fire an error event if the url is empty.
-        // FIXME: Should we fire this event asynchronoulsy via errorEventSender()?
-        m_element->dispatchEvent(Event::create(eventNames().errorEvent, false, false));
+        m_hasPendingErrorEvent = true;
+        errorEventSender().dispatchEventSoon(this);
     }
     
     CachedImage* oldImage = m_image.get();
