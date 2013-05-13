0171ae25f808d2cc2ed6488f9443dde9114ba05a
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-13 17:32:10 +0000

    Fix conflicting #define for WebaudioMediaCodecBridge
    This conflicts with the #define in MediaCodecBridge
    
    R=dalecurtis@chromium.org
    
    Review URL: https://codereview.chromium.org/14796017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199756 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bfa6ecc67b157bc3f54cbce4e37f20e9b3915cf2

--- media/base/android/webaudio_media_codec_bridge.h
+++ media/base/android/webaudio_media_codec_bridge.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef MEDIA_BASE_ANDROID_MEDIA_CODEC_BRIDGE_H_
-#define MEDIA_BASE_ANDROID_MEDIA_CODEC_BRIDGE_H_
+#ifndef MEDIA_BASE_ANDROID_WEBAUDIO_MEDIA_CODEC_BRIDGE_H_
+#define MEDIA_BASE_ANDROID_WEBAUDIO_MEDIA_CODEC_BRIDGE_H_
 
 #include <jni.h>
 
@@ -70,4 +70,4 @@ class MEDIA_EXPORT WebAudioMediaCodecBridge {
 };
 
 }  // namespace media
-#endif  // MEDIA_BASE_ANDROID_MEDIA_CODEC_BRIDGE_H_
+#endif  // MEDIA_BASE_ANDROID_WEBAUDIO_MEDIA_CODEC_BRIDGE_H_
