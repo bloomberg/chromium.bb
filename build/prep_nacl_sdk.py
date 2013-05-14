12741c1a6f8f99203f3493d21a0d4732bc401bdd
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-05-14 22:25:13 +0000

    Minidumper: Add minidump_generator library to the untrusted-code Gyp build
    This requires fixing some header install paths, otherwise we get the
    old version of pthread.h from the toolchain tarball (from before a
    recent libpthread change) and the newer version is installed in the
    wrong place.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3424
    TEST=gyp build
    
    Review URL: https://codereview.chromium.org/14657027
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11347 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: e079095f5f68cf1c77c3d1ab02aa2c5e79e335f0

--- build/prep_nacl_sdk.py
+++ build/prep_nacl_sdk.py
@@ -58,23 +58,23 @@ HEADER_MAP = {
           'src/untrusted/nacl/nacl_startup.h',
       'arm-nacl/include/nacl/nacl_thread.h':
           'src/untrusted/nacl/nacl_thread.h',
-      'arm-nacl/include/include/irt.h': 'src/untrusted/irt/irt.h',
-      'arm-nacl/include/include/irt_ppapi.h': 'src/untrusted/irt/irt_ppapi.h',
+      'arm-nacl/include/irt.h': 'src/untrusted/irt/irt.h',
+      'arm-nacl/include/irt_ppapi.h': 'src/untrusted/irt/irt_ppapi.h',
       'arm-nacl/lib/crt1.o': 'src/untrusted/stubs/crt1.x',
   },
   'x86_pnacl': {
-      'newlib/sdk/include/include/pthread.h': 'src/untrusted/pthread/pthread.h',
-      'newlib/sdk/include/include/semaphore.h': 'src/untrusted/pthread/semaphore.h',
-      'newlib/sdk/include/include/nacl/dynamic_annotations.h':
+      'newlib/sdk/include/pthread.h': 'src/untrusted/pthread/pthread.h',
+      'newlib/sdk/include/semaphore.h': 'src/untrusted/pthread/semaphore.h',
+      'newlib/sdk/include/nacl/dynamic_annotations.h':
           'src/untrusted/valgrind/dynamic_annotations.h',
-      'newlib/sdk/include/include/nacl/nacl_dyncode.h':
+      'newlib/sdk/include/nacl/nacl_dyncode.h':
           'src/untrusted/nacl/nacl_dyncode.h',
-      'newlib/sdk/include/include/nacl/nacl_startup.h':
+      'newlib/sdk/include/nacl/nacl_startup.h':
           'src/untrusted/nacl/nacl_startup.h',
-      'newlib/sdk/include/include/nacl/nacl_thread.h':
+      'newlib/sdk/include/nacl/nacl_thread.h':
           'src/untrusted/nacl/nacl_thread.h',
-      'newlib/sdk/include/include/irt.h': 'src/untrusted/irt/irt.h',
-      'newlib/sdk/include/include/irt_ppapi.h': 'src/untrusted/irt/irt_ppapi.h',
+      'newlib/sdk/include/irt.h': 'src/untrusted/irt/irt.h',
+      'newlib/sdk/include/irt_ppapi.h': 'src/untrusted/irt/irt_ppapi.h',
   }
 }
 
