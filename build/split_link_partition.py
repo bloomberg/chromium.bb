eef4cc298a16e2d2b374bcf3cc8aba84ddc17691
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-14 20:51:37 +0000

    fix license
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200067 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f8a60d4c7351405378a92e7bbb42175e2ba79683

--- build/split_link_partition.py
+++ build/split_link_partition.py
@@ -1,3 +1,7 @@
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 # This dict determines how chrome.dll is split into multiple parts.
 {
   'parts': [
