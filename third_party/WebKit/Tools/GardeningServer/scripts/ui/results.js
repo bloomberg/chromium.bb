d88621c5ec23d374f60752ae7e99454f85915af1
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-14 00:50:50 +0000

    Garden-o-matic should open flakiness dashboard in new window
    In the "examine" view, we forgot to set the target to _blank when we link to
    the flakiness dashboard. As requested by Eric Seidel.
    
    BUG=240525
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/15089004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150275 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 62d0c71cd63d2c110add48c9e3bb4896ea432789

--- third_party/WebKit/Tools/GardeningServer/scripts/ui/results.js
+++ third_party/WebKit/Tools/GardeningServer/scripts/ui/results.js
@@ -241,6 +241,7 @@ ui.results.TestSelector = base.extends('div', {
             var linkTitle = document.createElement('a');
             linkTitle.classList.add('link-title');
             linkTitle.setAttribute('href', ui.urlForFlakinessDashboard([testName]))
+            linkTitle.target = '_blank';
             linkTitle.textContent = testName;
 
             var header = document.createElement('h3');
