1d9cdb7cf75d9013a36b2cae4f65252afa5cf68c
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-16 03:43:01 +0000

    Add minorGC() to js-test-pre.js, which triggers one minor GC cycle
    This enables us to write a test case that depends on minor GC behavior.
    
    R=abarth,morrita
    
    Review URL: https://chromiumcodereview.appspot.com/15216002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150475 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: bbd587367c2070133685d73d8ad0dc5e6cb83dbc

--- third_party/WebKit/LayoutTests/fast/js/resources/js-test-pre.js
+++ third_party/WebKit/LayoutTests/fast/js/resources/js-test-pre.js
@@ -564,6 +564,13 @@ function gc() {
     }
 }
 
+function minorGC() {
+    if (typeof GCController !== "undefined")
+        GCController.minorCollect();
+    else
+        testFailed("Minor GC is available only when you enable the --expose-gc option in V8.");
+}
+
 function isSuccessfullyParsed()
 {
     // FIXME: Remove this and only report unexpected syntax errors.
