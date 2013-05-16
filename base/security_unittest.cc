3659938d6583b8c80e1084e21e2f43ed043c0a0b
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 15:32:33 +0000

    Enable SecurityTest.CallocOverflow under ASan on Mac.
    BUG=175554
    R=jln@chromium.org
    
    Review URL: https://codereview.chromium.org/15108007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200543 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6bc8b768412e242eabab85f74ebcb71a7d5a0e62

--- base/security_unittest.cc
+++ base/security_unittest.cc
@@ -150,12 +150,6 @@ TEST(SecurityTest, TCMALLOC_TEST(MemoryAllocationRestrictionsNewArray)) {
   #define DISABLE_ON_IOS_AND_WIN(function) function
 #endif
 
-#if defined(ADDRESS_SANITIZER) && defined(OS_MACOSX)
-  #define DISABLE_ON_MAC_WITH_ASAN(function) DISABLED_##function
-#else
-  #define DISABLE_ON_MAC_WITH_ASAN(function) function
-#endif
-
 // There are platforms where these tests are known to fail. We would like to
 // be able to easily check the status on the bots, but marking tests as
 // FAILS_ is too clunky.
@@ -216,7 +210,7 @@ bool CallocReturnsNull(size_t nmemb, size_t size) {
 }
 
 // Test if calloc() can overflow.
-TEST(SecurityTest, DISABLE_ON_MAC_WITH_ASAN(CallocOverflow)) {
+TEST(SecurityTest, CallocOverflow) {
   const size_t kArraySize = 4096;
   const size_t kMaxSizeT = numeric_limits<size_t>::max();
   const size_t kArraySize2 = kMaxSizeT / kArraySize + 10;
