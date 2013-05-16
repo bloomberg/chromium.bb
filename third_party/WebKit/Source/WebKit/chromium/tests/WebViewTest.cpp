c977484adf61e73c1d391f4a158918192f0dacc6
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-16 20:36:15 +0000

    Remove WebViewTest.VisitedLinkCrash
    This test didn't actually test the issue I wanted it to test and it's causing
    trouble for the Android buildbot.
    
    R=akalin@chromium.org
    
    Review URL: https://codereview.chromium.org/15179009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150535 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: be12556bf307f4137ce3772d32d1051696348b0d

--- third_party/WebKit/Source/WebKit/chromium/tests/WebViewTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/WebViewTest.cpp
@@ -806,14 +806,6 @@ TEST_F(WebViewTest, ConfirmCompositionTriggersAutofillTextChange)
     webView->close();
 }
 
-TEST_F(WebViewTest, VisitedLinkCrash)
-{
-    const char* testURL = "data:text/html,<svg><a></a></svg>";
-    WebView* webView = FrameTestHelpers::createWebViewAndLoad(testURL);
-    webView->updateVisitedLinkState(0);
-    webView->close();
-}
-
 TEST_F(WebViewTest, ShadowRoot)
 {
     URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(m_baseURL.c_str()), WebString::fromUTF8("shadow_dom_test.html"));
