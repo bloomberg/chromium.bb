d33f2b065c57729f6eac98df71a4157da8f965d3
Rob Bradford <rob@linux.intel.com>
2013-05-20 16:55:10 +0100

    weston-launch: Print explanation of why we failed to open the device

--- src/weston-launch.c
+++ src/weston-launch.c
@@ -320,12 +320,17 @@ handle_open(struct weston_launch *wl, struct msghdr *msg, ssize_t len)
 		goto err0;
 
 	fd = open(message->path, message->flags);
-	if (fd < 0)
+	if (fd < 0) {
+		fprintf(stderr, "Error opening device %s: %m\n",
+			message->path);
 		goto err0;
+	}
 
 	if (major(s.st_rdev) != INPUT_MAJOR) {
 		close(fd);
 		fd = -1;
+		fprintf(stderr, "Device %s is not an input device\n",
+			message->path);
 		goto err0;
 	}
 
