9be3306b4b2cf7fa5d5f2a718ff17f41ae23ab4b
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 16:57:02 +0000

    [Android] Remove deprected CVC.onFocusChanged.
    The multiple paramter version of this function is no longer used,
    so remove it in favor of the single parameter version.
    
    Review URL: https://chromiumcodereview.appspot.com/15485004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201320 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 986e70dd409c2d249a256ca7aaa3a3e4c8022104

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1574,18 +1574,6 @@ public class ContentViewCore implements MotionEventDelegate, NavigationClient {
         mScrolledAndZoomedFocusedEditableNode = false;
     }
 
-    /**
-     * @see View#onFocusedChanged(boolean, int, Rect)
-     * TODO(benm): Remove once downstream usages have been updated to use single
-     * parameter version
-     */
-    @Deprecated
-    @SuppressWarnings("javadoc")
-    public void onFocusChanged(boolean gainFocus, int direction, Rect previouslyFocusedRect) {
-        onFocusChanged(gainFocus);
-    }
-
-
     public void onFocusChanged(boolean gainFocus) {
         if (!gainFocus) getContentViewClient().onImeStateChangeRequested(false);
         if (mNativeContentViewCore != 0) nativeSetFocus(mNativeContentViewCore, gainFocus);
