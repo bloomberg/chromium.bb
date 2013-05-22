6746a1ca8fbea251b908e93210ca84b429d8ceb6
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-22 12:07:17 +0000

    Remove bad FIXME after r150840 removed ENABLE(SVG)
    FeTile.cpp had a FIXME for wkbug.com/47315 which added ENABLE(SVG) to fix a
    compile failure. This fixme was dubious to begin with and is now not needed
    as ENABLE(SVG) is gone.
    
    Landing TBR as this change was pointed out by Stephen Chenney in
    https://codereview.chromium.org/15627003.
    
    B=240006
    TBR=schenney@google.com
    
    Review URL: https://chromiumcodereview.appspot.com/15646002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150888 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 72c5505cd8ce83927da152c6d5b4c094c47e0fb9

--- third_party/WebKit/Source/core/platform/graphics/filters/FETile.cpp
+++ third_party/WebKit/Source/core/platform/graphics/filters/FETile.cpp
@@ -45,7 +45,6 @@ PassRefPtr<FETile> FETile::create(Filter* filter)
 
 void FETile::applySoftware()
 {
-    // FIXME: See bug 47315. This is a hack to work around a compile failure, but is incorrect behavior otherwise.
     FilterEffect* in = inputEffect(0);
 
     ImageBuffer* resultImage = createImageBufferResult();
