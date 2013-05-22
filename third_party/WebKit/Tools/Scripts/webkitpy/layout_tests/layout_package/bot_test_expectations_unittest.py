b4943f42d459c3641d6b2e30a13a3ddc26e17925
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-22 04:46:41 +0000

    Add more tests of BotTestExpectations, including some which we fail.
    BUG=242366
    R=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/15536003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150863 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: eb852c3dc69a6cf9bf27f4c8b7b714bb26400912

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/bot_test_expectations_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/bot_test_expectations_unittest.py
@@ -32,7 +32,29 @@ from webkitpy.layout_tests.layout_package import bot_test_expectations
 
 
 class BotTestExpectationsTest(unittest.TestCase):
-    # Expects newest result on left of string "PFF", means it just passed after 2 failures.
+    # All result_string's in this file expect newest result
+    # on left: "PFF", means it just passed after 2 failures.
+
+    def _assert_is_flaky(self, results_string, should_be_flaky):
+        expectations = bot_test_expectations.BotTestExpectations(only_ignore_very_flaky=True)
+        length_encoded = self._results_from_string(results_string)['results']
+        num_actual_results = len(expectations._actual_results_for_test(length_encoded))
+        if should_be_flaky:
+            self.assertGreater(num_actual_results, 1)
+        else:
+            self.assertEqual(num_actual_results, 1)
+
+    def test_basic_flaky(self):
+        self._assert_is_flaky('PFF', False)  # Used to fail, but now passes.
+        self._assert_is_flaky('FFP', False)  # Just started failing.
+        self._assert_is_flaky('PFPF', True)  # Seen both failures and passes.
+        # self._assert_is_flaky('PPPF', True)  # Should be counted as flaky but isn't yet.
+        self._assert_is_flaky('FPPP', False)  # Just started failing, not flaky.
+        self._assert_is_flaky('PFFP', True)  # Failed twice in a row, still flaky.
+        # Failing 3+ times in a row is unlikely to be flaky, but rather a transient failure on trunk.
+        # self._assert_is_flaky('PFFFP', False)
+        # self._assert_is_flaky('PFFFFP', False)
+
     def _results_from_string(self, results_string):
         results_list = []
         last_char = None
