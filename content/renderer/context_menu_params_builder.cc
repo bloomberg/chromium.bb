4e908ed61bef8e79f6ab89260e1137c5a18e8dfe
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 00:47:57 +0000

    Pass the misspelling hash identifier to the spelling menu observer
    This CL passes the hash identifier of the misspelling under the cursor to the
    spelling menu observer. The spelling menu observer will use this identifier to
    collect feedback about user actions on misspellings. For example, the user could
    ignore spelling suggestions, add a word to their custom dictionary, or select
    one of the spellcheck suggestions. This feedback will be used to improve
    spellcheck suggestions.
    
    BUG=170514
    
    Review URL: https://chromiumcodereview.appspot.com/15616002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201654 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a48b6295fdea8636bb77805c478f2408e68fd350

--- content/renderer/context_menu_params_builder.cc
+++ content/renderer/context_menu_params_builder.cc
@@ -30,6 +30,7 @@ ContextMenuParams ContextMenuParamsBuilder::Build(
   params.media_flags = data.mediaFlags;
   params.selection_text = data.selectedText;
   params.misspelled_word = data.misspelledWord;
+  params.misspelling_hash = data.misspellingHash;
   params.speech_input_enabled = data.isSpeechInputEnabled;
   params.spellcheck_enabled = data.isSpellCheckingEnabled;
   params.is_editable = data.isEditable;
