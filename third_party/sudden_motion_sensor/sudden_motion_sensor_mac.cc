a30da5461954840463bb71da7552c3bbe2b56058
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 11:16:16 +0000

    Update accelerometer table for 13" MacBookPro with Retina display.
    BUG=none
    TEST=no visible change
    TBR=brettw
    
    Review URL: https://chromiumcodereview.appspot.com/15737015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201751 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 95eb31e3c2501a5bb92dbe9db7e8e4c5df0d3a0e

--- third_party/sudden_motion_sensor/sudden_motion_sensor_mac.cc
+++ third_party/sudden_motion_sensor/sudden_motion_sensor_mac.cc
@@ -172,23 +172,23 @@ const SuddenMotionSensor::SensorDescriptor
   // Tested by Eric Shapiro (via avi) on a 17" MacBook Pro.
   { "MacBookPro4,1", NULL, { { 0, true  }, { 2, true  }, { 4, false } } },
 
+  // MacBookPro5,1 handled by the generic case below.
   // Tested by leandrogracia on a 15" MacBook Pro.
-  { "MacBookPro5,1", NULL, { { 0, false }, { 2, false }, { 4, false } } },
 
+  // MacBookPro5,2 handled by the generic case below.
   // Tested by S.Selz. (via avi) on a 17" MacBook Pro.
-  { "MacBookPro5,2", NULL, { { 0, false }, { 2, false }, { 4, false } } },
 
   // Tested by dmaclach on a 15" MacBook Pro.
   { "MacBookPro5,3", NULL, { { 2, false }, { 0, false }, { 4, true  } } },
 
+  // MacBookPro5,4 handled by the generic case below.
   // Tested by leandrogracia on a 15" MacBook Pro.
-  { "MacBookPro5,4", NULL, { { 0, false }, { 2, false }, { 4, false } } },
 
   // Tested by leandrogracia on a 13" MacBook Pro.
   { "MacBookPro5,5", NULL, { { 0, true  }, { 2, true  }, { 4, false } } },
 
+  // MacBookPro6,1 handled by the generic case below.
   // Tested by khom, leadpipe on a 17" MacBook Pro.
-  { "MacBookPro6,1", NULL, { { 0, false }, { 2, false }, { 4, false } } },
 
   // Tested by leandrogracia on a 15" MacBook Pro.
   { "MacBookPro6,2", NULL, { { 0, true  }, { 2, false }, { 4, true  } } },
@@ -196,28 +196,32 @@ const SuddenMotionSensor::SensorDescriptor
   // Tested by leandrogracia on a 13" MacBook Pro.
   { "MacBookPro7,1", NULL, { { 0, true  }, { 2, true  }, { 4, false } } },
 
+  // MacBookPro8,1 handled by the generic case below.
   // Tested by avi on a 13" MacBook Pro.
-  { "MacBookPro8,1", NULL, { { 0, false }, { 2, false }, { 4, false } } },
 
+  // MacBookPro8,2 handled by the generic case below.
   // Tested by avi on a 15" MacBook Pro.
-  { "MacBookPro8,2", NULL, { { 0, false }, { 2, false }, { 4, false } } },
 
+  // MacBookPro8,3 handled by the generic case below.
   // Tested by avi on a 17" MacBook Pro.
-  { "MacBookPro8,3", NULL, { { 0, false }, { 2, false }, { 4, false } } },
 
+  // MacBookPro9,1 handled by the generic case below.
   // Tested by avi on a 15" MacBook Pro.
-  { "MacBookPro9,1", NULL, { { 0, false }, { 2, false }, { 4, false } } },
 
+  // MacBookPro9,2 handled by the generic case below.
   // Tested by avi on a 13" MacBook Pro.
-  { "MacBookPro9,2", NULL, { { 0, false }, { 2, false }, { 4, false } } },
 
   // Note:
-  // - MacBookPro10,1 (15" MacBook Pro with Retina display, mid 2012)
+  // - MacBookPro10,1 (15" MacBook Pro with Retina display)
   // has no accelerometer sensors.
 
+  // MacBookPro10,2 handled by the generic case below.
+  // Tested by avi on a 13" MacBook Pro with Retina display.
+
   // Generic MacBook accelerometer sensor data, to be used for future models
-  // until they can be tested and their data entered. Note that this generic
-  // configuration may well have problems with inverted axes.
+  // as well as models for which it is verified to be correct. Note that this
+  // configuration may have problems with inverted axes when used generically
+  // for untested models.
   { "",              NULL, { { 0, false }, { 2, false }, { 4, false } } }
 };
 
