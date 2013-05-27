c46c935296f2be02c7c3278c0ddbf71037d5b846
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-27 03:17:12 +0000

    Track NPObject ownership by the originating plugins' NPP identifier. [2/3] (Chrome)
    This CL updates Chrome to return plugin NPP identifiers for NPAPI, PPAPI and browser plugins, and to make the necessary calls into Blink to support object ownership tracking.
    
    This CL requires Blink CL crrev.com/14989014, and is itself required by Blink CL crrev.com/14019005.
    
    BUG=152006
    
    Review URL: https://chromiumcodereview.appspot.com/15007012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202364 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8c18303fa5717c900180d7ce10dc7919d0b1781d

--- webkit/plugins/ppapi/ppapi_plugin_instance.cc
+++ webkit/plugins/ppapi/ppapi_plugin_instance.cc
@@ -437,17 +437,16 @@ PluginInstance::PluginInstance(
       selection_anchor_(0),
       pending_user_gesture_(0.0),
       document_loader_(NULL),
-      nacl_document_load_(false) {
+      nacl_document_load_(false),
+      npp_(new NPP_t) {
   pp_instance_ = HostGlobals::Get()->AddInstance(this);
 
   memset(&current_print_settings_, 0, sizeof(current_print_settings_));
   DCHECK(delegate);
   module_->InstanceCreated(this);
   delegate_->InstanceCreated(this);
-  message_channel_.reset(new MessageChannel(this));
 
   view_data_.is_page_visible = delegate->IsPageVisible();
-
   resource_creation_ = delegate_->CreateResourceCreationAPI(this);
 
   // TODO(bbudge) remove this when the trusted NaCl plugin has been removed.
@@ -634,6 +633,8 @@ static void SetGPUHistogram(const ::ppapi::Preferences& prefs,
 bool PluginInstance::Initialize(const std::vector<std::string>& arg_names,
                                 const std::vector<std::string>& arg_values,
                                 bool full_frame) {
+  message_channel_.reset(new MessageChannel(this));
+
   full_frame_ = full_frame;
 
   UpdateTouchEventRequest();
@@ -2543,6 +2544,10 @@ bool PluginInstance::IsValidInstanceOf(PluginModule* module) {
   DCHECK(module);
   return module == module_.get() ||
          module == original_module_.get();
+}
+
+NPP PluginInstance::instanceNPP() {
+  return npp_.get();
 }
 
 void PluginInstance::DoSetCursor(WebCursorInfo* cursor) {
