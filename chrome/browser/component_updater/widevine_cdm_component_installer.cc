bloomberg/chromium.bb

commit 8f2136ae14b91dde009694cb143aca22dae77325

Conditionally build support for Pepper-based CDMs.
Adds enable_pepper_cdms and ENABLE_PEPPER_CDMS to control building of this logic.
Previously, it was built for all platforms, but not all platforms use Pepper.

TEST=content_browsertests on platforms with and without Pepper CDM support

Review URL: https://chromiumcodereview.appspot.com/15028015

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201738 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 280755cad298b37cd6ead4d16337c4e1ed79f1a0

chrome/common/chrome_paths.cc
@@ -354,7 +354,7 @@ bool PathProvider(int key, base::FilePath* result) {
       cur = cur.Append(kGTalkPluginFileName);
       break;
 #endif
-#if defined(WIDEVINE_CDM_AVAILABLE)
+#if defined(WIDEVINE_CDM_AVAILABLE) && defined(ENABLE_PEPPER_CDMS)
 #if defined(WIDEVINE_CDM_IS_COMPONENT)
     case chrome::DIR_COMPONENT_WIDEVINE_CDM:
       if (!PathService::Get(chrome::DIR_USER_DATA, &cur))
@@ -370,7 +370,7 @@ bool PathProvider(int key, base::FilePath* result) {
         return false;
       cur = cur.Append(kWidevineCdmAdapterFileName);
       break;
-#endif  // defined(WIDEVINE_CDM_AVAILABLE)
+#endif  // defined(WIDEVINE_CDM_AVAILABLE) && defined(ENABLE_PEPPER_CDMS)
     case chrome::FILE_RESOURCES_PACK:
 #if defined(OS_MACOSX) && !defined(OS_IOS)
       if (base::mac::AmIBundled()) {

third_party/widevine/cdm/widevine_cdm_common.h
@@ -19,9 +19,11 @@ const char kWidevineCdmDisplayName[] = "Widevine Content Decryption Module";
 // Will be parsed as HTML.
 const char kWidevineCdmDescription[] =
     "Enables Widevine licenses for playback of HTML audio/video content.";
-const char kWidevineCdmPluginMimeType[] ="application/x-ppapi-widevine-cdm";
+#if defined(ENABLE_PEPPER_CDMS)
+const char kWidevineCdmPluginMimeType[] = "application/x-ppapi-widevine-cdm";
 const char kWidevineCdmPluginMimeTypeDescription[] =
     "Widevine Content Decryption Module";
+#endif
 
 // File name of the CDM on different platforms.
 const base::FilePath::CharType kWidevineCdmFileName[] =
@@ -33,6 +35,7 @@ const base::FilePath::CharType kWidevineCdmFileName[] =
     FILE_PATH_LITERAL("libwidevinecdm.so");
 #endif
 
+#if defined(ENABLE_PEPPER_CDMS)
 // File name of the adapter on different platforms.
 const base::FilePath::CharType kWidevineCdmAdapterFileName[] =
 #if defined(OS_MACOSX)
@@ -42,8 +45,10 @@ const base::FilePath::CharType kWidevineCdmAdapterFileName[] =
 #else  // OS_LINUX, etc.
     FILE_PATH_LITERAL("libwidevinecdmadapter.so");
 #endif
+#endif  // defined(ENABLE_PEPPER_CDMS)
 
-#if defined(OS_MACOSX) || defined(OS_WIN)
+
+#if defined(ENABLE_PEPPER_CDMS) && (defined(OS_MACOSX) || defined(OS_WIN))
 // CDM is installed by the component installer instead of the Chrome installer.
 #define WIDEVINE_CDM_IS_COMPONENT
 #endif

build/common.gypi
@@ -1412,6 +1412,12 @@
         'use_cups%': 0,
       }],
 
+      ['enable_plugins==1 and (OS=="linux" or OS=="mac" or OS=="win" or google_tv==1)', {
+        'enable_pepper_cdms%': 1,
+      }, {
+        'enable_pepper_cdms%': 0,
+      }],
+
       # Native Client glibc toolchain is enabled
       # by default except on arm and mips.
       ['target_arch=="arm" or target_arch=="mipsel"', {
@@ -1911,6 +1917,9 @@
       }],
       ['enable_viewport==1', {
         'defines': ['ENABLE_VIEWPORT'],
+      }],
+      ['enable_pepper_cdms==1', {
+        'defines': ['ENABLE_PEPPER_CDMS'],
       }],
       ['configuration_policy==1', {
         'defines': ['ENABLE_CONFIGURATION_POLICY'],

chrome/browser/content_settings/content_settings_browsertest.cc
@@ -471,11 +471,12 @@ class PepperContentSettingsTest : public ContentSettingsTest {
 
   // Registers any CDM plugins not registered by default.
   virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+#if defined(ENABLE_PEPPER_CDMS)
     // Platform-specific filename relative to the chrome executable.
 #if defined(OS_WIN)
-    const wchar_t kLibraryName[] = L"clearkeycdmadapter.dll";
     const std::wstring external_clear_key_mime_type =
         ASCIIToWide(kExternalClearKeyMimeType);
+    const char kLibraryName[] = "clearkeycdmadapter.dll";
 #else  // !defined(OS_WIN)
     const char* external_clear_key_mime_type = kExternalClearKeyMimeType;
 #if defined(OS_MACOSX)
@@ -488,7 +489,7 @@ class PepperContentSettingsTest : public ContentSettingsTest {
     // Append the switch to register the External Clear Key CDM.
     base::FilePath plugin_dir;
     EXPECT_TRUE(PathService::Get(base::DIR_MODULE, &plugin_dir));
-    base::FilePath plugin_lib = plugin_dir.Append(kLibraryName);
+    base::FilePath plugin_lib = plugin_dir.AppendASCII(kLibraryName);
     EXPECT_TRUE(file_util::PathExists(plugin_lib));
     base::FilePath::StringType pepper_plugin = plugin_lib.value();
     pepper_plugin.append(FILE_PATH_LITERAL(
@@ -496,6 +497,8 @@ class PepperContentSettingsTest : public ContentSettingsTest {
     pepper_plugin.append(external_clear_key_mime_type);
     command_line->AppendSwitchNative(switches::kRegisterPepperPlugins,
                                      pepper_plugin);
+#endif  // defined(ENABLE_PEPPER_CDMS)
+
 #if !defined(DISABLE_NACL)
     // Ensure NaCl can run.
     command_line->AppendSwitch(switches::kEnableNaCl);
@@ -560,18 +563,22 @@ IN_PROC_BROWSER_TEST_F(PepperContentSettingsTest, PluginSpecialCases) {
   content_settings->SetDefaultContentSetting(
       CONTENT_SETTINGS_TYPE_PLUGINS, CONTENT_SETTING_ALLOW);
 
+#if defined(ENABLE_PEPPER_CDMS)
   RunLoadPepperPluginTest(kExternalClearKeyMimeType, true);
+#endif
 
   // Next, test behavior when plug-ins are blocked.
   content_settings->SetDefaultContentSetting(
       CONTENT_SETTINGS_TYPE_PLUGINS, CONTENT_SETTING_BLOCK);
 
+#if defined(ENABLE_PEPPER_CDMS)
   // The plugin we loaded above does not load now.
   RunLoadPepperPluginTest(kExternalClearKeyMimeType, false);
 
 #if defined(WIDEVINE_CDM_AVAILABLE)
   RunLoadPepperPluginTest(kWidevineCdmPluginMimeType, true);
 #endif
+#endif  // defined(ENABLE_PEPPER_CDMS)
 
 #if !defined(DISABLE_NACL)
   RunLoadPepperPluginTest("application/x-nacl", true);
@@ -583,12 +590,14 @@ IN_PROC_BROWSER_TEST_F(PepperContentSettingsTest, PluginSpecialCases) {
   content_settings->SetDefaultContentSetting(
       CONTENT_SETTINGS_TYPE_JAVASCRIPT, CONTENT_SETTING_BLOCK);
 
+#if defined(ENABLE_PEPPER_CDMS)
   // This plugin has no special behavior and does not require JavaScript.
   RunJavaScriptBlockedTest("load_clearkey_no_js.html", false);
 
 #if defined(WIDEVINE_CDM_AVAILABLE)
   RunJavaScriptBlockedTest("load_widevine_no_js.html", true);
 #endif
+#endif  // defined(ENABLE_PEPPER_CDMS)
 
 #if !defined(DISABLE_NACL)
   RunJavaScriptBlockedTest("load_nacl_no_js.html", true);

chrome/common/chrome_content_client.cc
@@ -49,7 +49,8 @@
 #include "chrome/common/chrome_sandbox_type_mac.h"
 #endif
 
-#if defined(WIDEVINE_CDM_AVAILABLE) && !defined(WIDEVINE_CDM_IS_COMPONENT)
+#if defined(WIDEVINE_CDM_AVAILABLE) && defined(ENABLE_PEPPER_CDMS) && \
+    !defined(WIDEVINE_CDM_IS_COMPONENT)
 #include "chrome/common/widevine_cdm_constants.h"
 #endif
 
@@ -230,7 +231,8 @@ void ComputeBuiltInPlugins(std::vector<content::PepperPluginInfo>* plugins) {
     }
   }
 
-#if defined(WIDEVINE_CDM_AVAILABLE) && !defined(WIDEVINE_CDM_IS_COMPONENT)
+#if defined(WIDEVINE_CDM_AVAILABLE) && defined(ENABLE_PEPPER_CDMS) && \
+    !defined(WIDEVINE_CDM_IS_COMPONENT)
   static bool skip_widevine_cdm_file_check = false;
   if (PathService::Get(chrome::FILE_WIDEVINE_CDM_ADAPTER, &path)) {
     if (skip_widevine_cdm_file_check || file_util::PathExists(path)) {
@@ -251,7 +253,7 @@ void ComputeBuiltInPlugins(std::vector<content::PepperPluginInfo>* plugins) {
       skip_widevine_cdm_file_check = true;
     }
   }
-#endif  // defined(WIDEVINE_CDM_AVAILABLE) &&
+#endif  // defined(WIDEVINE_CDM_AVAILABLE) && defined(ENABLE_PEPPER_CDMS) &&
         // !defined(WIDEVINE_CDM_IS_COMPONENT)
 
   // The Remoting Viewer plugin is built-in.

chrome/renderer/plugins/plugin_uma.cc
@@ -197,8 +197,10 @@ PluginUMAReporter::PluginType PluginUMAReporter::MimeTypeToPluginType(
     return SHOCKWAVE_FLASH;
   }
 
+#if defined(ENABLE_PEPPER_CDMS)
   if (mime_type == kWidevineCdmPluginMimeType)
     return WIDEVINE_CDM;
+#endif
 
   return UNSUPPORTED_MIMETYPE;
 }

chrome/renderer/plugins/plugin_uma_unittest.cc
@@ -106,7 +106,11 @@ TEST_F(PluginUMATest, ShockwaveFlash) {
 }
 
 TEST_F(PluginUMATest, WidevineCdm) {
+#if defined(ENABLE_PEPPER_CDMS)
   ExpectPluginType(PluginUMAReporter::WIDEVINE_CDM,
+#else
+  ExpectPluginType(PluginUMAReporter::UNSUPPORTED_MIMETYPE,
+#endif
                    "application/x-ppapi-widevine-cdm",
                    GURL("some url"));
   ExpectPluginType(PluginUMAReporter::UNSUPPORTED_MIMETYPE,

content/browser/media/encrypted_media_browsertest.cc
@@ -27,17 +27,17 @@
 #include <gnu/libc-version.h>
 #endif  // defined(WIDEVINE_CDM_AVAILABLE) && defined(OS_LINUX)
 
+#if defined(ENABLE_PEPPER_CDMS)
 // Platform-specific filename relative to the chrome executable.
-#if defined(OS_WIN)
-static const char kClearKeyLibraryName[] = "clearkeycdmadapter.dll";
-static const char kWidevineLibraryName[] = "widevinecdmadapter.dll";
-#elif defined(OS_MACOSX)
-static const char kClearKeyLibraryName[] = "clearkeycdmadapter.plugin";
-static const char kWidevineLibraryName[] = "widevinecdmadapter.plugin";
+static const char kClearKeyCdmAdapterFileName[] =
+#if defined(OS_MACOSX)
+    "clearkeycdmadapter.plugin";
+#elif defined(OS_WIN)
+    "clearkeycdmadapter.dll";
 #elif defined(OS_POSIX)
-static const char kClearKeyLibraryName[] = "libclearkeycdmadapter.so";
-static const char kWidevineLibraryName[] = "libwidevinecdmadapter.so";
+    "libclearkeycdmadapter.so";
 #endif
+#endif  // defined(ENABLE_PEPPER_CDMS)
 
 // Available key systems.
 static const char kClearKeyKeySystem[] = "webkit-org.w3.clearkey";
@@ -104,34 +104,31 @@ class EncryptedMediaTest : public testing::WithParamInterface<const char*>,
   }
 
  protected:
-  // Registers any CDM plugins not registered by default.
+#if defined(ENABLE_PEPPER_CDMS)
   virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
-    RegisterPepperPlugin(command_line, kClearKeyLibraryName,
-                         kExternalClearKeyKeySystem);
+    RegisterPepperCdm(command_line, kClearKeyCdmAdapterFileName,
+                      kExternalClearKeyKeySystem);
   }
 
-  virtual void RegisterPepperPlugin(CommandLine* command_line,
-                                    const std::string& library_name,
-                                    const std::string& key_system) {
+  virtual void RegisterPepperCdm(CommandLine* command_line,
+                                 const std::string& adapter_name,
+                                 const std::string& key_system) {
     // Append the switch to register the Clear Key CDM Adapter.
     base::FilePath plugin_dir;
     EXPECT_TRUE(PathService::Get(base::DIR_MODULE, &plugin_dir));
-#if defined(OS_WIN)
-    base::FilePath plugin_lib = plugin_dir.Append(ASCIIToWide(library_name));
-#else
-    base::FilePath plugin_lib = plugin_dir.Append(library_name);
-#endif
+    base::FilePath plugin_lib = plugin_dir.AppendASCII(adapter_name);
     EXPECT_TRUE(file_util::PathExists(plugin_lib));
     base::FilePath::StringType pepper_plugin = plugin_lib.value();
     pepper_plugin.append(FILE_PATH_LITERAL("#CDM#0.1.0.0;"));
 #if defined(OS_WIN)
-    pepper_plugin.append(ASCIIToWide(webkit_media::GetPluginType(key_system)));
+    pepper_plugin.append(ASCIIToWide(webkit_media::GetPepperType(key_system)));
 #else
-    pepper_plugin.append(webkit_media::GetPluginType(key_system));
+    pepper_plugin.append(webkit_media::GetPepperType(key_system));
 #endif
     command_line->AppendSwitchNative(switches::kRegisterPepperPlugins,
                                      pepper_plugin);
   }
+#endif  // defined(ENABLE_PEPPER_CDMS)
 };
 
 #if defined(WIDEVINE_CDM_AVAILABLE)
@@ -164,20 +161,23 @@ class WVEncryptedMediaTest : public EncryptedMediaTest {
   }
 
  protected:
-  // Registers any CDM plugins not registered by default.
+#if defined(ENABLE_PEPPER_CDMS)
   virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
-    command_line->AppendSwitch(switches::kAllowFileAccessFromFiles);
-    RegisterPepperPlugin(command_line, kWidevineLibraryName,
-                         kWidevineKeySystem);
+    RegisterPepperCdm(command_line, kWidevineCdmAdapterFileName,
+                      kWidevineKeySystem);
   }
+#endif  // defined(ENABLE_PEPPER_CDMS)
 };
 #endif  // defined(WIDEVINE_CDM_AVAILABLE)
 
 INSTANTIATE_TEST_CASE_P(ClearKey, EncryptedMediaTest,
                         ::testing::Values(kClearKeyKeySystem));
 
+// External Clear Key is currently only used on platforms that use Pepper CDMs.
+#if defined(ENABLE_PEPPER_CDMS)
 INSTANTIATE_TEST_CASE_P(ExternalClearKey, EncryptedMediaTest,
                         ::testing::Values(kExternalClearKeyKeySystem));
+#endif
 
 IN_PROC_BROWSER_TEST_F(EncryptedMediaTest, InvalidKeySystem) {
   const string16 kExpected = ASCIIToUTF16(

commit 07818732f74f787f3d919a87cbf66b0b747baae8

Fix a type mismatch on Windows caused by r201738.
The problem was at encrypted_media_browsertest.cc line 167, which is a call to RegisterPepperCdm(), but the right fix is to use ASCII for the constants.

TBR=thestig@chromium.org

Review URL: https://codereview.chromium.org/15658009

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202598 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 0f253da52ef4fdb2dde29f3cf5f8ca660e5067ea

chrome/browser/component_updater/widevine_cdm_component_installer.cc
@@ -217,7 +217,7 @@ bool WidevineCdmComponentInstaller::Install(
   if (current_version_.CompareTo(version) > 0)
     return false;
 
-  if (!file_util::PathExists(unpack_path.Append(kWidevineCdmFileName)))
+  if (!file_util::PathExists(unpack_path.AppendASCII(kWidevineCdmFileName)))
     return false;
 
   base::FilePath adapter_source_path;
@@ -234,7 +234,7 @@ bool WidevineCdmComponentInstaller::Install(
     return false;
 
   base::FilePath adapter_install_path =
-      install_path.Append(kWidevineCdmAdapterFileName);
+      install_path.AppendASCII(kWidevineCdmAdapterFileName);
   if (!file_util::CopyFile(adapter_source_path, adapter_install_path))
     return false;
 
@@ -270,8 +270,8 @@ void StartWidevineCdmUpdateRegistration(ComponentUpdateService* cus) {
   base::Version version(kNullVersion);
   std::vector<base::FilePath> older_dirs;
   if (GetWidevineCdmDirectory(&path, &version, &older_dirs)) {
-    if (file_util::PathExists(path.Append(kWidevineCdmAdapterFileName)) &&
-        file_util::PathExists(path.Append(kWidevineCdmFileName))) {
+    if (file_util::PathExists(path.AppendASCII(kWidevineCdmAdapterFileName)) &&
+        file_util::PathExists(path.AppendASCII(kWidevineCdmFileName))) {
       BrowserThread::PostTask(
           BrowserThread::UI, FROM_HERE,
           base::Bind(&RegisterWidevineCdmWithChrome, path, version));

chrome/common/chrome_paths.cc
@@ -368,7 +368,7 @@ bool PathProvider(int key, base::FilePath* result) {
     case chrome::FILE_WIDEVINE_CDM_ADAPTER:
       if (!GetInternalPluginsDirectory(&cur))
         return false;
-      cur = cur.Append(kWidevineCdmAdapterFileName);
+      cur = cur.AppendASCII(kWidevineCdmAdapterFileName);
       break;
 #endif  // defined(WIDEVINE_CDM_AVAILABLE) && defined(ENABLE_PEPPER_CDMS)
     case chrome::FILE_RESOURCES_PACK:

third_party/widevine/cdm/widevine_cdm_common.h
@@ -26,24 +26,24 @@ const char kWidevineCdmPluginMimeTypeDescription[] =
 #endif
 
 // File name of the CDM on different platforms.
-const base::FilePath::CharType kWidevineCdmFileName[] =
+const char kWidevineCdmFileName[] =
 #if defined(OS_MACOSX)
-    FILE_PATH_LITERAL("widevinecdm.dylib");
+    "widevinecdm.dylib";
 #elif defined(OS_WIN)
-    FILE_PATH_LITERAL("widevinecdm.dll");
+    "widevinecdm.dll";
 #else  // OS_LINUX, etc.
-    FILE_PATH_LITERAL("libwidevinecdm.so");
+    "libwidevinecdm.so";
 #endif
 
 #if defined(ENABLE_PEPPER_CDMS)
 // File name of the adapter on different platforms.
-const base::FilePath::CharType kWidevineCdmAdapterFileName[] =
+const char kWidevineCdmAdapterFileName[] =
 #if defined(OS_MACOSX)
-    FILE_PATH_LITERAL("widevinecdmadapter.plugin");
+    "widevinecdmadapter.plugin";
 #elif defined(OS_WIN)
-    FILE_PATH_LITERAL("widevinecdmadapter.dll");
+    "widevinecdmadapter.dll";
 #else  // OS_LINUX, etc.
-    FILE_PATH_LITERAL("libwidevinecdmadapter.so");
+    "libwidevinecdmadapter.so";
 #endif
 #endif  // defined(ENABLE_PEPPER_CDMS)
 