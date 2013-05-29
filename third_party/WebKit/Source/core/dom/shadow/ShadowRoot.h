6aeaac0dd710d3939dd58a2a4598e7b79290c559
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-29 10:21:57 +0000

    applyAuthorStyles and resetStyleInheritance should use shadow root's setNeedsStyleRecalc, not shadow host's.
    - ShadowRoot::recalcStyle should see its styleChangeType. If FullStyleChange, use Force for its children's style recalc.
    - Changed applyAuthorStyles and resetStyleInheritance to check whether the modified shadow root is inert or not. If inert, we don't need style recalc.
    
    BUG=242151
    
    Review URL: https://chromiumcodereview.appspot.com/15536002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151373 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ae1c14cb886c916b9569b0edc3aee1114150bad3

--- third_party/WebKit/Source/core/dom/shadow/ShadowRoot.h
+++ third_party/WebKit/Source/core/dom/shadow/ShadowRoot.h
@@ -109,6 +109,7 @@ private:
 
     // FIXME: This shouldn't happen. https://bugs.webkit.org/show_bug.cgi?id=88834
     bool isOrphan() const { return !host(); }
+    bool isActive() const;
 
     ShadowRoot* m_prev;
     ShadowRoot* m_next;
