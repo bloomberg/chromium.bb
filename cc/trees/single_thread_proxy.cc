8d4584504d963c7e228d7031f70624d86f9194ed
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-29 19:45:33 +0000

    Shouldn't access LayerTreeTest::layer_tree_host on impl thread
    If the main thread is unblocked and we access this function on the impl thread,
    we're in a racy situation. This should be an error.
    
    BUG=235188
    
    Review URL: https://chromiumcodereview.appspot.com/14179022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202942 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1675fd684187bea60094441e2d0406057f6adbee

--- cc/trees/single_thread_proxy.cc
+++ cc/trees/single_thread_proxy.cc
@@ -97,7 +97,7 @@ void SingleThreadProxy::SetLayerTreeHostClientReady() {
 }
 
 void SingleThreadProxy::SetVisible(bool visible) {
-  DebugScopedSetImplThread impl(this);
+  DebugScopedSetImplThreadAndMainThreadBlocked impl(this);
   layer_tree_host_impl_->SetVisible(visible);
 }
 
@@ -176,9 +176,7 @@ void SingleThreadProxy::DoCommit(scoped_ptr<ResourceUpdateQueue> queue) {
   DCHECK(Proxy::IsMainThread());
   // Commit immediately.
   {
-    DebugScopedSetMainThreadBlocked mainThreadBlocked(this);
-    DebugScopedSetImplThread impl(this);
-
+    DebugScopedSetImplThreadAndMainThreadBlocked impl_with_main_blocked(this);
     RenderingStatsInstrumentation* stats_instrumentation =
         layer_tree_host_->rendering_stats_instrumentation();
     base::TimeTicks start_time = stats_instrumentation->StartRecording();
