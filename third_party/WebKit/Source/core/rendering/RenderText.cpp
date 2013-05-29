b3ee97645af48213189500ced1816dc536493d6d
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-29 07:25:33 +0000

    Optimize RenderText::offsetNext for 8 bit strings
    Previously, this function was always upconverting strings to 16 bits in order
    to create a cursorMovementIterator. This CL adds a fast path for ASCII and
    Latin-1 strings, which don't need this complicated cursor movement logic.
    
    After this CL, upconverting falls off my profile of Mobile Gmail completely.
    
    R=eseidel,ojan
    
    Review URL: https://chromiumcodereview.appspot.com/15883006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151360 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 822e024bd7a7b750c8a3e2cd4051518f39468061

--- third_party/WebKit/Source/core/rendering/RenderText.cpp
+++ third_party/WebKit/Source/core/rendering/RenderText.cpp
@@ -1715,8 +1715,11 @@ unsigned RenderText::renderedTextLength() const
 
 int RenderText::previousOffset(int current) const
 {
-    StringImpl* si = m_text.impl();
-    TextBreakIterator* iterator = cursorMovementIterator(si->characters(), si->length());
+    if (isAllASCII() || m_text.is8Bit())
+        return current - 1;
+
+    StringImpl* textImpl = m_text.impl();
+    TextBreakIterator* iterator = cursorMovementIterator(textImpl->characters16(), textImpl->length());
     if (!iterator)
         return current - 1;
 
@@ -1868,15 +1871,17 @@ int RenderText::previousOffsetForBackwardDeletion(int current) const
 
 int RenderText::nextOffset(int current) const
 {
-    StringImpl* si = m_text.impl();
-    TextBreakIterator* iterator = cursorMovementIterator(si->characters(), si->length());
+    if (isAllASCII() || m_text.is8Bit())
+        return current + 1;
+
+    StringImpl* textImpl = m_text.impl();
+    TextBreakIterator* iterator = cursorMovementIterator(textImpl->characters16(), textImpl->length());
     if (!iterator)
         return current + 1;
 
     long result = textBreakFollowing(iterator, current);
     if (result == TextBreakDone)
         result = current + 1;
-
 
     return result;
 }
