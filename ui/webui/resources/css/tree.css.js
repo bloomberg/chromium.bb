1cb9adac80946e2ae07b96fc0ecd24e7a13b99a6
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-31 03:37:40 +0000

    tree.js: Triangle for expanding supports HiDPI.
    With this CL, a triangle is drawn to an internal canvas by doubling size, and is scaled back down with CSS.
    
    BUG=237411
    TEST=manual
    R=dbeam@chromium.org
    
    Review URL: https://codereview.chromium.org/15915011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203321 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 49d69ba3a43aa6da00f672d6178dad11d197e94a

--- ui/webui/resources/css/tree.css.js
+++ ui/webui/resources/css/tree.css.js
@@ -3,19 +3,23 @@
 // found in the LICENSE file.
 
 (function() {
-  var a = 7;
-  var a2 = a / 2;
-  var ctx = document.getCSSCanvasContext('2d', 'tree-triangle', a + 1, a2 + 2);
+  /** @const */ var WIDTH = 14;
+  /** @const */ var HEIGHT = WIDTH / 2 + 2;
+  /** @const */ var MARGIN = 1;
+  var ctx = document.getCSSCanvasContext('2d',
+                                         'tree-triangle',
+                                         WIDTH + MARGIN * 2,
+                                         HEIGHT + MARGIN * 2);
 
   ctx.fillStyle = '#000';
-  ctx.translate(.5, .5);
+  ctx.translate(MARGIN, MARGIN);
 
   ctx.beginPath();
   ctx.moveTo(0, 0);
-  ctx.lineTo(0, 1);
-  ctx.lineTo(a2, 1 + a2);
-  ctx.lineTo(a, 1);
-  ctx.lineTo(a, 0);
+  ctx.lineTo(0, 2);
+  ctx.lineTo(WIDTH / 2, HEIGHT);
+  ctx.lineTo(WIDTH, 2);
+  ctx.lineTo(WIDTH, 0);
   ctx.closePath();
   ctx.fill();
   ctx.stroke();
