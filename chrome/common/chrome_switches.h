eedf1e8c7e8eec1019a76cf9c35996a0934954b8
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-31 01:49:40 +0000

    Flag to enable experimental device discovery.
    BUG=245379
    
    Review URL: https://chromiumcodereview.appspot.com/16125014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203302 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 28970406de84c79a0040360461ec199201aaa70c

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -151,6 +151,7 @@ extern const char kEnableCloudPrintProxy[];
 extern const char kEnableComponentCloudPolicy[];
 extern const char kEnableContacts[];
 extern const char kEnableDesktopGuestMode[];
+extern const char kEnableDeviceDiscovery[];
 extern const char kEnableDevToolsExperiments[];
 extern const char kEnableDraggableMenuButton[];
 extern const char kEnableExperimentalExtensionApis[];
