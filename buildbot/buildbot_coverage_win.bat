dfc79804e0064d01cae01c823be0e9a65d25a019
bradnelson@google.com <bradnelson@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-05-31 00:26:29 +0000

    Fixing windows coverage archive to actually archive.
    The last fix did not actually work on the bots
    (it merely suppressed the error code).
    
    Using new recursive copy option in gsutil in lieu of separate script.
    This solution has been tried on the bots.
    
    BUG=None
    TEST=local test on bots
    R=jfb@chromium.org
    
    Review URL: https://codereview.chromium.org/15705007
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11432 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: a469a9e4d24ad1d566a6b7e8b168a05600261df1

--- buildbot/buildbot_coverage_win.bat
+++ buildbot/buildbot_coverage_win.bat
@@ -23,15 +23,19 @@ if %BUILDBOT_SLAVE_TYPE% equ "" exit /b 0
 if %BUILDBOT_SLAVE_TYPE% equ "Trybot" exit /b 0
 
 echo @@@BUILD_STEP archive_coverage@@@
-set GSUTIL="\b\build\scripts\slave\gsutil.bat"
-set GSD_URL="http://gsdview.appspot.com/nativeclient-coverage2/revs"
-set VARIANT_NAME="coverage-win-x86-%BITS%"
-set COVERAGE_PATH="%VARIANT_NAME%/html/index.html"
-if %BUILDBOT_REVISION% equ "" set BUILDBOT_REVISION=None
-set LINK_URL="%GSD_URL%/%BUILDBOT_REVISION%/%COVERAGE_PATH%"
-set GSD_BASE="gs://nativeclient-coverage2/revs"
-set GS_PATH="%GSD_BASE%/%BUILDBOT_REVISION%/%VARIANT_NAME%"
-call python.bat /b/build/scripts/slave/gsutil_cp_dir.py -a public-read ^
- scons-out/%VARIANT_NAME%/coverage %GS_PATH%
+set GSUTIL=\b\build\third_party\gsutil\gsutil
+set GSD_URL=http://gsdview.appspot.com/nativeclient-coverage2/revs
+set VARIANT_NAME=coverage-win-x86-%BITS%
+set COVERAGE_PATH=%VARIANT_NAME%/html/index.html
+if "%BUILDBOT_REVISION%" equ "" set BUILDBOT_REVISION=None
+set LINK_URL=%GSD_URL%/%BUILDBOT_REVISION%/%COVERAGE_PATH%
+set GSD_BASE=gs://nativeclient-coverage2/revs
+set GS_PATH=%GSD_BASE%/%BUILDBOT_REVISION%/%VARIANT_NAME%
+set COV_DIR=scons-out/%VARIANT_NAME%/coverage
+call python.bat %GSUTIL% cp -a public-read ^
+ %COV_DIR%/coverage.lcov %GS_PATH%/coverage.lcov
+if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
+cd %COV_DIR%
+call python.bat %GSUTIL% cp -a public-read html %GS_PATH%
 if %ERRORLEVEL% neq 0 exit /b %ERRORLEVEL%
 echo @@@STEP_LINK@view@%LINK_URL%@@@
