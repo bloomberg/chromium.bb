8fffb7f363c487216b87967de167b131edea7e26
aurimas@chromium.org <aurimas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-03 18:21:00 +0000

    [Android] Delaying previous selection check.
    editable.replace(..) call has side-effects on selection therefore,
    we have to delay the previous selection check until after that.
    
    BUG=232426
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/16305012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203735 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4a44454beac82dab215deeeb8e811d711c9fe3ff

--- content/public/android/java/src/org/chromium/content/browser/input/AdapterInputConnection.java
+++ content/public/android/java/src/org/chromium/content/browser/input/AdapterInputConnection.java
@@ -127,24 +127,23 @@ public class AdapterInputConnection extends BaseInputConnection {
         // Non-breaking spaces can cause the IME to get confused. Replace with normal spaces.
         text = text.replace('\u00A0', ' ');
 
-        Editable editable = getEditable();
-
-        int prevSelectionStart = Selection.getSelectionStart(editable);
-        int prevSelectionEnd = Selection.getSelectionEnd(editable);
-        int prevCompositionStart = getComposingSpanStart(editable);
-        int prevCompositionEnd = getComposingSpanEnd(editable);
-        String prevText = editable.toString();
-
         selectionStart = Math.min(selectionStart, text.length());
         selectionEnd = Math.min(selectionEnd, text.length());
         compositionStart = Math.min(compositionStart, text.length());
         compositionEnd = Math.min(compositionEnd, text.length());
 
+        Editable editable = getEditable();
+        String prevText = editable.toString();
         boolean textUnchanged = prevText.equals(text);
 
         if (!textUnchanged) {
             editable.replace(0, editable.length(), text);
         }
+
+        int prevSelectionStart = Selection.getSelectionStart(editable);
+        int prevSelectionEnd = Selection.getSelectionEnd(editable);
+        int prevCompositionStart = getComposingSpanStart(editable);
+        int prevCompositionEnd = getComposingSpanEnd(editable);
 
         if (prevSelectionStart == selectionStart && prevSelectionEnd == selectionEnd
                 && prevCompositionStart == compositionStart
