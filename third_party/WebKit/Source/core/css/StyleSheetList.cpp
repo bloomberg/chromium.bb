abdc7dff8189fbdcb271beb5cd2df3de345e4999
kojih@chromium.org <kojih@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-03 16:48:40 +0000

    Auto-generate V8StyleSheetList::namedPropertyGetter
    This patch is once landed (150663)
    but I noticed my mistake and reverted.
    Now it's OK because Blink checks own property
    if [OverrideBuiltins] not specified.
    
    R=haraken@chromium.org 
    BUG=229740
    TEST=No tests. refactoring.
    
    Review URL: https://chromiumcodereview.appspot.com/16321004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151669 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 9078dbf4440e70fabd25d523031a87539cec4a52

--- third_party/WebKit/Source/core/css/StyleSheetList.cpp
+++ third_party/WebKit/Source/core/css/StyleSheetList.cpp
@@ -80,4 +80,12 @@ HTMLStyleElement* StyleSheetList::getNamedItem(const String& name) const
     return 0;
 }
 
+CSSStyleSheet* StyleSheetList::anonymousNamedGetter(const AtomicString& name)
+{
+    HTMLStyleElement* item = getNamedItem(name);
+    if (!item)
+        return 0;
+    return item->sheet();
+}
+
 } // namespace WebCore
--- third_party/WebKit/Source/core/css/StyleSheetList.h
+++ third_party/WebKit/Source/core/css/StyleSheetList.h
@@ -21,6 +21,7 @@
 #ifndef StyleSheetList_h
 #define StyleSheetList_h
 
+#include "core/css/CSSStyleSheet.h"
 #include <wtf/Forward.h>
 #include <wtf/RefCounted.h>
 #include <wtf/PassRefPtr.h>
@@ -45,6 +46,7 @@ public:
     Document* document() { return m_document; }
 
     void detachFromDocument();
+    CSSStyleSheet* anonymousNamedGetter(const AtomicString&);
 
 private:
     StyleSheetList(Document*);
