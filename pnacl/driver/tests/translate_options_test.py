356a9a745d71880214024f4c473b21b0d205f45a
jvoung@google.com <jvoung@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-06-04 16:39:45 +0000

    Revert ARM pnacl-translate driver setting for -global-merge=0.
    I'm assuming that the LLVM change fixes the problem:
    6aea8b4: Fix FlattenGlobals to correctly handle implicitly-aligned variables
    
    If so, we can make the commandline tools be back in sync
    with the in-browser translator settings.
    
    This reverts nacl revs 11363 and 11365.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3437
    R=mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/16335019
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11464 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 35faaaf43317dea28de834de29d107c22daf6689

--- pnacl/driver/tests/translate_options_test.py
+++ pnacl/driver/tests/translate_options_test.py
@@ -115,21 +115,11 @@ define i32 @main() {
           self.platform,
           [],
           expected_triple_cpu)
-      # Test that the default StreamInit is used, when no flags are given.
-      # Temporarily not true for ARM, due to
-      # https://code.google.com/p/nativeclient/issues/detail?id=3437
-      if self.platform == 'arm':
-        self.checkLLCTranslateFlags(
-            pexe,
-            self.platform,
-            ['--pnacl-sb'],
-            ['StreamInitWithOverrides.*global-merge=0'])
-      else:
-        self.checkLLCTranslateFlags(
-            pexe,
-            self.platform,
-            ['--pnacl-sb'],
-            ['StreamInit h'])
+      self.checkLLCTranslateFlags(
+          pexe,
+          self.platform,
+          ['--pnacl-sb'],
+          ['StreamInit h'])
 
   def test_overrideO0(self):
     if driver_test_utils.CanRunHost():
