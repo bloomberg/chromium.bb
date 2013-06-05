8121113c5eeaf877cce03d7fd59f9bd2d06b95f0
sergiu@chromium.org <sergiu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 18:47:27 +0000

    Add histograms for OAuth token request for web history.
    The UMA dashboard is reporting a large number of failures for the web history
    queries. Since it is not reproducing locally this patch adds more UMAs to the
    code that obtains the OAuth token for those queries in order to track failure
    and network status there.
    
    BUG=246657
    R=asvitkine@chromium.org, jar@chromium.org, sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16360016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204306 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4e639686276353c007d50331988a058391020a6f

--- chrome/browser/history/web_history_service.cc
+++ chrome/browser/history/web_history_service.cc
@@ -7,6 +7,7 @@
 #include "base/bind.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
+#include "base/metrics/histogram.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
@@ -19,6 +20,7 @@
 #include "net/base/load_flags.h"
 #include "net/base/url_util.h"
 #include "net/http/http_status_code.h"
+#include "net/http/http_util.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_fetcher_delegate.h"
 
@@ -88,6 +90,10 @@ class RequestImpl : public WebHistoryService::Request,
     DCHECK_EQ(source, url_fetcher_.get());
     response_code_ = url_fetcher_->GetResponseCode();
 
+    UMA_HISTOGRAM_CUSTOM_ENUMERATION("WebHistory.OAuthTokenResponseCode",
+        net::HttpUtil::MapStatusCodeForHistogram(response_code_),
+        net::HttpUtil::GetStatusCodesForHistogram());
+
     // If the response code indicates that the token might not be valid,
     // invalidate the token and try again.
     if (response_code_ == net::HTTP_UNAUTHORIZED && ++auth_retry_count_ <= 1) {
@@ -117,6 +123,8 @@ class RequestImpl : public WebHistoryService::Request,
     DCHECK(!access_token.empty());
     access_token_ = access_token;
 
+    UMA_HISTOGRAM_BOOLEAN("WebHistory.OAuthTokenCompletion", true);
+
     // Got an access token -- start the actual API request.
     url_fetcher_.reset(CreateUrlFetcher(access_token));
     url_fetcher_->Start();
@@ -127,6 +135,9 @@ class RequestImpl : public WebHistoryService::Request,
       const GoogleServiceAuthError& error) OVERRIDE {
     token_request_.reset();
     is_pending_ = false;
+
+    UMA_HISTOGRAM_BOOLEAN("WebHistory.OAuthTokenCompletion", false);
+
     callback_.Run(this, false);
     // It is valid for the callback to delete |this|, so do not access any
     // members below here.
