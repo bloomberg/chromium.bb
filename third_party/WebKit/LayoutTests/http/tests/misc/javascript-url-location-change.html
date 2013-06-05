bloomberg/chromium.bb

commit 55db21457255ef4af04ce86f31633bcd834eed32

Remove ShouldReplaceDocumentIfJavaScriptURL enum and always replace document.
This is a long-standing FIXME.

BUG=none
TEST=none

Review URL: https://chromiumcodereview.appspot.com/14995011

git-svn-id: svn://svn.chromium.org/blink/trunk@149881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: fefb9eff0e65bd8f14b7d385967eaf95d0f35416

third_party/WebKit/Source/bindings/v8/ScriptController.cpp
@@ -710,7 +710,7 @@ ScriptValue ScriptController::executeScript(const ScriptSourceCode& sourceCode)
     return evaluate(sourceCode);
 }
 
-bool ScriptController::executeIfJavaScriptURL(const KURL& url, ShouldReplaceDocumentIfJavaScriptURL shouldReplaceDocumentIfJavaScriptURL)
+bool ScriptController::executeIfJavaScriptURL(const KURL& url)
 {
     if (!protocolIsJavaScript(url))
         return false;
@@ -738,18 +738,13 @@ bool ScriptController::executeIfJavaScriptURL(const KURL& url, ShouldReplaceDocu
     if (!result.getString(scriptResult))
         return true;
 
-    // FIXME: We should always replace the document, but doing so
-    //        synchronously can cause crashes:
-    //        http://bugs.webkit.org/show_bug.cgi?id=16782
-    if (shouldReplaceDocumentIfJavaScriptURL == ReplaceDocumentIfJavaScriptURL) {
-        // We're still in a frame, so there should be a DocumentLoader.
-        ASSERT(m_frame->document()->loader());
+    // We're still in a frame, so there should be a DocumentLoader.
+    ASSERT(m_frame->document()->loader());
         
-        // DocumentWriter::replaceDocument can cause the DocumentLoader to get deref'ed and possible destroyed,
-        // so protect it with a RefPtr.
-        if (RefPtr<DocumentLoader> loader = m_frame->document()->loader())
-            loader->writer()->replaceDocument(scriptResult, ownerDocument.get());
-    }
+    // DocumentWriter::replaceDocument can cause the DocumentLoader to get deref'ed and possible destroyed,
+    // so protect it with a RefPtr.
+    if (RefPtr<DocumentLoader> loader = m_frame->document()->loader())
+        loader->writer()->replaceDocument(scriptResult, ownerDocument.get());
     return true;
 }
 

third_party/WebKit/Source/bindings/v8/ScriptController.h
@@ -33,7 +33,6 @@
 
 #include "bindings/v8/ScriptInstance.h"
 #include "bindings/v8/ScriptValue.h"
-#include "core/loader/FrameLoaderTypes.h"
 
 #include <v8.h>
 #include "wtf/Forward.h"
@@ -86,7 +85,7 @@ public:
     ScriptValue callFunctionEvenIfScriptDisabled(v8::Handle<v8::Function>, v8::Handle<v8::Object>, int argc, v8::Handle<v8::Value> argv[]);
 
     // Returns true if argument is a JavaScript URL.
-    bool executeIfJavaScriptURL(const KURL&, ShouldReplaceDocumentIfJavaScriptURL shouldReplaceDocumentIfJavaScriptURL = ReplaceDocumentIfJavaScriptURL);
+    bool executeIfJavaScriptURL(const KURL&);
 
     // This function must be called from the main thread. It is safe to call it repeatedly.
     static void initializeThreading();

third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -271,25 +271,23 @@ void FrameLoader::setDefersLoading(bool defers)
 void FrameLoader::changeLocation(SecurityOrigin* securityOrigin, const KURL& url, const String& referrer, bool lockBackForwardList, bool refresh)
 {
     urlSelected(FrameLoadRequest(securityOrigin, ResourceRequest(url, referrer, refresh ? ReloadIgnoringCacheData : UseProtocolCachePolicy), "_self"),
-        0, lockBackForwardList, MaybeSendReferrer, ReplaceDocumentIfJavaScriptURL);
+        0, lockBackForwardList, MaybeSendReferrer);
 }
 
 void FrameLoader::urlSelected(const KURL& url, const String& passedTarget, PassRefPtr<Event> triggeringEvent, bool lockBackForwardList, ShouldSendReferrer shouldSendReferrer)
 {
     urlSelected(FrameLoadRequest(m_frame->document()->securityOrigin(), ResourceRequest(url), passedTarget),
-        triggeringEvent, lockBackForwardList, shouldSendReferrer, DoNotReplaceDocumentIfJavaScriptURL);
+        triggeringEvent, lockBackForwardList, shouldSendReferrer);
 }
 
-// The shouldReplaceDocumentIfJavaScriptURL parameter will go away when the FIXME to eliminate the
-// corresponding parameter from ScriptController::executeIfJavaScriptURL() is addressed.
-void FrameLoader::urlSelected(const FrameLoadRequest& passedRequest, PassRefPtr<Event> triggeringEvent, bool lockBackForwardList, ShouldSendReferrer shouldSendReferrer, ShouldReplaceDocumentIfJavaScriptURL shouldReplaceDocumentIfJavaScriptURL)
+void FrameLoader::urlSelected(const FrameLoadRequest& passedRequest, PassRefPtr<Event> triggeringEvent, bool lockBackForwardList, ShouldSendReferrer shouldSendReferrer)
 {
     ASSERT(!m_suppressOpenerInNewFrame);
 
     RefPtr<Frame> protect(m_frame);
     FrameLoadRequest frameRequest(passedRequest);
 
-    if (m_frame->script()->executeIfJavaScriptURL(frameRequest.resourceRequest().url(), shouldReplaceDocumentIfJavaScriptURL))
+    if (m_frame->script()->executeIfJavaScriptURL(frameRequest.resourceRequest().url()))
         return;
 
     if (frameRequest.frameName().isEmpty())
@@ -329,7 +327,7 @@ void FrameLoader::submitForm(PassRefPtr<FormSubmission> submission)
         if (!m_frame->document()->contentSecurityPolicy()->allowFormAction(KURL(submission->action())))
             return;
         m_isExecutingJavaScriptFormAction = true;
-        m_frame->script()->executeIfJavaScriptURL(submission->action(), DoNotReplaceDocumentIfJavaScriptURL);
+        m_frame->script()->executeIfJavaScriptURL(submission->action());
         m_isExecutingJavaScriptFormAction = false;
         return;
     }

third_party/WebKit/Source/core/loader/FrameLoader.h
@@ -313,7 +313,7 @@ private:
 
     void dispatchDidCommitLoad();
 
-    void urlSelected(const FrameLoadRequest&, PassRefPtr<Event>, bool lockBackForwardList, ShouldSendReferrer, ShouldReplaceDocumentIfJavaScriptURL);
+    void urlSelected(const FrameLoadRequest&, PassRefPtr<Event>, bool lockBackForwardList, ShouldSendReferrer);
 
     void loadWithDocumentLoader(DocumentLoader*, FrameLoadType, PassRefPtr<FormState>); // Calls continueLoadAfterNavigationPolicy
     void load(DocumentLoader*);                                                         // Calls loadWithDocumentLoader   

third_party/WebKit/Source/core/loader/FrameLoaderTypes.h
@@ -92,15 +92,6 @@ namespace WebCore {
         NeverSendReferrer
     };
 
-    // Passed to FrameLoader::urlSelected() and ScriptController::executeIfJavaScriptURL()
-    // to control whether, in the case of a JavaScript URL, executeIfJavaScriptURL() should
-    // replace the document.  It is a FIXME to eliminate this extra parameter from
-    // executeIfJavaScriptURL(), in which case this enum can go away.
-    enum ShouldReplaceDocumentIfJavaScriptURL {
-        ReplaceDocumentIfJavaScriptURL,
-        DoNotReplaceDocumentIfJavaScriptURL
-    };
-
     enum ReasonForCallingAllowPlugins {
         AboutToInstantiatePlugin,
         NotAboutToInstantiatePlugin

commit 29922182e1b267ec03191837cd334fdf7d8c6949

Don't replace document for javascript urls that trigger a new navigation.
This was regressed in r149881. If a javascript url does something like
"document.location.href=foo", we will conclude that the script had a result
that should be used to replace the current document. Instead, we should let
the location.href navigation occur.

BUG=246502
TEST=http/tests/misc/javascript-url-location-change.html

Review URL: https://chromiumcodereview.appspot.com/16474003

git-svn-id: svn://svn.chromium.org/blink/trunk@151870 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 6f823fdc776675c719ad9cede77a24757cf97668

third_party/WebKit/Source/bindings/v8/ScriptController.cpp
@@ -641,6 +641,8 @@ bool ScriptController::executeScriptIfJavaScriptURL(const KURL& url)
 
     const int javascriptSchemeLength = sizeof("javascript:") - 1;
 
+    bool locationChangeBefore = m_frame->navigationScheduler()->locationChangePending();
+
     String decodedURL = decodeURLEscapeSequences(url.string());
     ScriptValue result = executeScript(decodedURL.substring(javascriptSchemeLength));
 
@@ -655,6 +657,9 @@ bool ScriptController::executeScriptIfJavaScriptURL(const KURL& url)
 
     // We're still in a frame, so there should be a DocumentLoader.
     ASSERT(m_frame->document()->loader());
+
+    if (!locationChangeBefore && m_frame->navigationScheduler()->locationChangePending())
+        return true;
         
     // DocumentWriter::replaceDocument can cause the DocumentLoader to get deref'ed and possible destroyed,
     // so protect it with a RefPtr.

third_party/WebKit/LayoutTests/http/tests/misc/javascript-url-location-change-expected.txt
@@ -1,0 +1,1 @@
+SUCCESS

third_party/WebKit/LayoutTests/http/tests/misc/javascript-url-location-change.html
@@ -1,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<body onload="click()">
+<script>
+if (window.testRunner) {
+    testRunner.dumpAsText();
+    testRunner.waitUntilDone();
+}
+
+function click() {
+    if (window.eventSender) {
+        var a = document.getElementById("a");
+        eventSender.mouseMoveTo(a.offsetLeft + 2, a.offsetTop + 2);
+        eventSender.mouseDown();
+        eventSender.mouseUp();
+    }
+}
+</script>
+<a id="a" href="javascript:document.location.href='resources/success-notify-done.html'">Click me</a>
+</body>
+</html>