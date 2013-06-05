76e3f76fea612bccd6d9f42cdd6dcd2ebcc581bf
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 21:05:13 +0000

    Revert 204313 "net_watcher does not build in Ubuntu 13.04"
    broke compile on Google Chrome Linux bot:
    ../../net/tools/net_watcher/net_watcher.cc:133:error: expected [error|warning|ignored] after '#pragma GCC diagnostic'
    ../../net/tools/net_watcher/net_watcher.cc:207:error: expected [error|warning|ignored] after '#pragma GCC diagnostic'
    
    > net_watcher does not build in Ubuntu 13.04
    > 
    > This is due to a deprecation warning in glib, which affects glib 2.36 onward. Added a pragma to suppress deprecation warnings.
    > BUG=245658
    > 
    > Review URL: https://chromiumcodereview.appspot.com/15994010
    
    TBR=yael.aharon@intel.com
    
    Review URL: https://codereview.chromium.org/16404004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204338 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 19c5072ca6d1776ae1d03caaed67dc6dccc0bfe5

--- net/tools/net_watcher/net_watcher.cc
+++ net/tools/net_watcher/net_watcher.cc
@@ -127,13 +127,6 @@ class NetWatcher :
 
 }  // namespace
 
-// From glib version 2.36 onwards, g_type_init is implicitly called and it is
-// deprecated.
-#if (defined(OS_LINUX) || defined(OS_OPENBSD)) && !defined(OS_CHROMEOS)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#endif
-
 int main(int argc, char* argv[]) {
 #if defined(OS_MACOSX)
   base::mac::ScopedNSAutoreleasePool pool;
@@ -202,7 +195,3 @@ int main(int argc, char* argv[]) {
 
   return 0;
 }
-
-#if (defined(OS_LINUX) || defined(OS_OPENBSD)) && !defined(OS_CHROMEOS)
-#pragma GCC diagnostic pop
-#endif
