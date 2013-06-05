dbcab765c06ce98899cc50224cd4426a17e4c0a5
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 18:01:23 +0000

    Remove WeakPtr<T>'s operator T*()
    BUG=245942
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/16267003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204293 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 16e8015331419fcdb1cd6ea192e14e4b87d10e94

--- base/memory/weak_ptr.h
+++ base/memory/weak_ptr.h
@@ -210,7 +210,6 @@ class WeakPtr : public internal::WeakPtrBase {
   }
 
   T* get() const { return ref_.is_valid() ? ptr_ : NULL; }
-  operator T*() const { return get(); }
 
   T& operator*() const {
     DCHECK(get() != NULL);
