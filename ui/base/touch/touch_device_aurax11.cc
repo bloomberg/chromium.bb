50ba413c52dd3e8d14caa69d3768761ebb1a8884
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 06:35:09 +0000

    Remove unnecessary include
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/16387002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204179 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c00cd0453269925c7952d8ee4fa8c02b45b7326b

--- ui/base/touch/touch_device_aurax11.cc
+++ ui/base/touch/touch_device_aurax11.cc
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/aura/window.h"
 #include "ui/base/touch/touch_device.h"
 #include "ui/base/touch/touch_factory_x11.h"
 
