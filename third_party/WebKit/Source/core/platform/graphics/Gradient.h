4744ebd7dc131e148056d5062d4a7cc2db3db206
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-07 07:37:31 +0000

    Reformat Gradient.h to conform to Blink coding style.
    This CL removes the extra indentation and corrects the // namespace comment.
    
    Incidentally, another forward declaration was added and some vertical whitespace
    was removed.
    
    BUG=247144
    
    Review URL: https://chromiumcodereview.appspot.com/16563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152003 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2f62c1878d550b3f8626fabe194b11666ce76bb1

--- third_party/WebKit/Source/core/platform/graphics/Gradient.h
+++ third_party/WebKit/Source/core/platform/graphics/Gradient.h
@@ -41,128 +41,123 @@ class SkShader;
 
 namespace WebCore {
 
-    class Color;
-    class FloatRect;
+class Color;
+class FloatRect;
+class IntSize;
 
-    class Gradient : public RefCounted<Gradient> {
-    public:
-        static PassRefPtr<Gradient> create(const FloatPoint& p0, const FloatPoint& p1)
-        {
-            return adoptRef(new Gradient(p0, p1));
-        }
-        static PassRefPtr<Gradient> create(const FloatPoint& p0, float r0, const FloatPoint& p1, float r1, float aspectRatio = 1)
-        {
-            return adoptRef(new Gradient(p0, r0, p1, r1, aspectRatio));
-        }
-        ~Gradient();
+class Gradient : public RefCounted<Gradient> {
+public:
+    static PassRefPtr<Gradient> create(const FloatPoint& p0, const FloatPoint& p1)
+    {
+        return adoptRef(new Gradient(p0, p1));
+    }
+    static PassRefPtr<Gradient> create(const FloatPoint& p0, float r0, const FloatPoint& p1, float r1, float aspectRatio = 1)
+    {
+        return adoptRef(new Gradient(p0, r0, p1, r1, aspectRatio));
+    }
+    ~Gradient();
 
-        struct ColorStop;
-        void addColorStop(const ColorStop&);
-        void addColorStop(float, const Color&);
+    struct ColorStop {
+        float stop;
+        float red;
+        float green;
+        float blue;
+        float alpha;
 
-        bool hasAlpha() const;
-
-        bool isRadial() const { return m_radial; }
-        bool isZeroSize() const { return m_p0.x() == m_p1.x() && m_p0.y() == m_p1.y() && (!m_radial || m_r0 == m_r1); }
-
-        const FloatPoint& p0() const { return m_p0; }
-        const FloatPoint& p1() const { return m_p1; }
-
-        void setP0(const FloatPoint& p)
-        {
-            if (m_p0 == p)
-                return;
-
-            m_p0 = p;
-
-            invalidateHash();
-        }
-
-        void setP1(const FloatPoint& p)
-        {
-            if (m_p1 == p)
-                return;
-
-            m_p1 = p;
-
-            invalidateHash();
-        }
-
-        float startRadius() const { return m_r0; }
-        float endRadius() const { return m_r1; }
-
-        void setStartRadius(float r)
-        {
-            if (m_r0 == r)
-                return;
-
-            m_r0 = r;
-
-            invalidateHash();
-        }
-
-        void setEndRadius(float r)
-        {
-            if (m_r1 == r)
-                return;
-
-            m_r1 = r;
-
-            invalidateHash();
-        }
-
-        float aspectRatio() const { return m_aspectRatio; }
-
-        SkShader* shader();
-
-        struct ColorStop {
-            float stop;
-            float red;
-            float green;
-            float blue;
-            float alpha;
-
-            ColorStop() : stop(0), red(0), green(0), blue(0), alpha(0) { }
-            ColorStop(float s, float r, float g, float b, float a) : stop(s), red(r), green(g), blue(b), alpha(a) { }
-        };
-
-        void setStopsSorted(bool s) { m_stopsSorted = s; }
-
-        void setSpreadMethod(GradientSpreadMethod);
-        GradientSpreadMethod spreadMethod() { return m_spreadMethod; }
-        void setGradientSpaceTransform(const AffineTransform& gradientSpaceTransformation);
-        AffineTransform gradientSpaceTransform() { return m_gradientSpaceTransformation; }
-
-        void adjustParametersForTiledDrawing(IntSize&, FloatRect&);
-
-        unsigned hash() const;
-        void invalidateHash() { m_cachedHash = 0; }
-
-    private:
-        Gradient(const FloatPoint& p0, const FloatPoint& p1);
-        Gradient(const FloatPoint& p0, float r0, const FloatPoint& p1, float r1, float aspectRatio);
-
-        void destroyShader();
-
-        void sortStopsIfNecessary();
-
-        // Keep any parameters relevant to rendering in sync with the structure in Gradient::hash().
-        bool m_radial;
-        FloatPoint m_p0;
-        FloatPoint m_p1;
-        float m_r0;
-        float m_r1;
-        float m_aspectRatio; // For elliptical gradient, width / height.
-        mutable Vector<ColorStop, 2> m_stops;
-        mutable bool m_stopsSorted;
-        GradientSpreadMethod m_spreadMethod;
-        AffineTransform m_gradientSpaceTransformation;
-
-        mutable unsigned m_cachedHash;
-
-        SkShader* m_gradient;
+        ColorStop() : stop(0), red(0), green(0), blue(0), alpha(0) { }
+        ColorStop(float s, float r, float g, float b, float a) : stop(s), red(r), green(g), blue(b), alpha(a) { }
     };
+    void addColorStop(const ColorStop&);
+    void addColorStop(float, const Color&);
 
-} //namespace
+    bool hasAlpha() const;
+
+    bool isRadial() const { return m_radial; }
+    bool isZeroSize() const { return m_p0.x() == m_p1.x() && m_p0.y() == m_p1.y() && (!m_radial || m_r0 == m_r1); }
+
+    const FloatPoint& p0() const { return m_p0; }
+    const FloatPoint& p1() const { return m_p1; }
+
+    void setP0(const FloatPoint& p)
+    {
+        if (m_p0 == p)
+            return;
+
+        m_p0 = p;
+        invalidateHash();
+    }
+
+    void setP1(const FloatPoint& p)
+    {
+        if (m_p1 == p)
+            return;
+
+        m_p1 = p;
+        invalidateHash();
+    }
+
+    float startRadius() const { return m_r0; }
+    float endRadius() const { return m_r1; }
+
+    void setStartRadius(float r)
+    {
+        if (m_r0 == r)
+            return;
+
+        m_r0 = r;
+        invalidateHash();
+    }
+
+    void setEndRadius(float r)
+    {
+        if (m_r1 == r)
+            return;
+
+        m_r1 = r;
+        invalidateHash();
+    }
+
+    float aspectRatio() const { return m_aspectRatio; }
+
+    SkShader* shader();
+
+    void setStopsSorted(bool s) { m_stopsSorted = s; }
+
+    void setSpreadMethod(GradientSpreadMethod);
+    GradientSpreadMethod spreadMethod() { return m_spreadMethod; }
+    void setGradientSpaceTransform(const AffineTransform& gradientSpaceTransformation);
+    AffineTransform gradientSpaceTransform() { return m_gradientSpaceTransformation; }
+
+    void adjustParametersForTiledDrawing(IntSize&, FloatRect&);
+
+    unsigned hash() const;
+    void invalidateHash() { m_cachedHash = 0; }
+
+private:
+    Gradient(const FloatPoint& p0, const FloatPoint& p1);
+    Gradient(const FloatPoint& p0, float r0, const FloatPoint& p1, float r1, float aspectRatio);
+
+    void destroyShader();
+
+    void sortStopsIfNecessary();
+
+    // Keep any parameters relevant to rendering in sync with the structure in Gradient::hash().
+    bool m_radial;
+    FloatPoint m_p0;
+    FloatPoint m_p1;
+    float m_r0;
+    float m_r1;
+    float m_aspectRatio; // For elliptical gradient, width / height.
+    mutable Vector<ColorStop, 2> m_stops;
+    mutable bool m_stopsSorted;
+    GradientSpreadMethod m_spreadMethod;
+    AffineTransform m_gradientSpaceTransformation;
+
+    mutable unsigned m_cachedHash;
+
+    SkShader* m_gradient;
+};
+
+} // namespace WebCore
 
 #endif
