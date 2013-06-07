d7292175e61e4a292541d85475e58f35dc1ac9d5
jrummell@chromium.org <jrummell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-07 21:01:29 +0000

    Reland "Add UMA stats for MSE playback".
    BUG=245418
    
    Review URL: https://chromiumcodereview.appspot.com/15984012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204926 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0814ac6ba9c09942dcdebed59d18a88e6f2882b0

--- media/filters/stream_parser_factory.cc
+++ media/filters/stream_parser_factory.cc
@@ -5,6 +5,7 @@
 #include "media/filters/stream_parser_factory.h"
 
 #include "base/command_line.h"
+#include "base/metrics/histogram.h"
 #include "base/string_number_conversions.h"
 #include "base/string_util.h"
 #include "media/base/media_log.h"
@@ -27,10 +28,22 @@ struct CodecInfo {
     AUDIO,
     VIDEO
   };
+  enum HistogramTag {
+    HISTOGRAM_UNKNOWN,
+    HISTOGRAM_VP8,
+    HISTOGRAM_VP9,
+    HISTOGRAM_VORBIS,
+    HISTOGRAM_H264,
+    HISTOGRAM_MPEG2AAC,
+    HISTOGRAM_MPEG4AAC,
+    HISTOGRAM_EAC3,
+    HISTOGRAM_MAX  // Must be the last entry.
+  };
 
   const char* pattern;
   Type type;
   CodecIDValidatorFunction validator;
+  HistogramTag tag;
 };
 
 typedef media::StreamParser* (*ParserFactoryFunction)(
@@ -43,9 +56,12 @@ struct SupportedTypeInfo {
   const CodecInfo** codecs;
 };
 
-static const CodecInfo kVP8CodecInfo = { "vp8", CodecInfo::VIDEO, NULL };
-static const CodecInfo kVP9CodecInfo = { "vp9", CodecInfo::VIDEO, NULL };
-static const CodecInfo kVorbisCodecInfo = { "vorbis", CodecInfo::AUDIO, NULL };
+static const CodecInfo kVP8CodecInfo = { "vp8", CodecInfo::VIDEO, NULL,
+                                         CodecInfo::HISTOGRAM_VP8 };
+static const CodecInfo kVP9CodecInfo = { "vp9", CodecInfo::VIDEO, NULL,
+                                         CodecInfo::HISTOGRAM_VP9 };
+static const CodecInfo kVorbisCodecInfo = { "vorbis", CodecInfo::AUDIO, NULL,
+                                            CodecInfo::HISTOGRAM_VORBIS };
 
 static const CodecInfo* kVideoWebMCodecs[] = {
   &kVP8CodecInfo,
@@ -99,19 +115,18 @@ bool ValidateMP4ACodecID(const std::string& codec_id,
   return false;
 }
 
-static const CodecInfo kH264CodecInfo = { "avc1.*", CodecInfo::VIDEO, NULL };
-static const CodecInfo kMPEG4AACCodecInfo = {
-  "mp4a.40.*", CodecInfo::AUDIO, &ValidateMP4ACodecID
-};
-
-static const CodecInfo kMPEG2AACLCCodecInfo = {
-  "mp4a.67", CodecInfo::AUDIO, NULL
-};
+static const CodecInfo kH264CodecInfo = { "avc1.*", CodecInfo::VIDEO, NULL,
+                                          CodecInfo::HISTOGRAM_H264 };
+static const CodecInfo kMPEG4AACCodecInfo = { "mp4a.40.*", CodecInfo::AUDIO,
+                                              &ValidateMP4ACodecID,
+                                              CodecInfo::HISTOGRAM_MPEG4AAC };
+static const CodecInfo kMPEG2AACLCCodecInfo = { "mp4a.67", CodecInfo::AUDIO,
+                                                NULL,
+                                                CodecInfo::HISTOGRAM_MPEG2AAC };
 
 #if defined(ENABLE_EAC3_PLAYBACK)
-static const CodecInfo kEAC3CodecInfo = {
-  "mp4a.a6", CodecInfo::AUDIO, NULL
-};
+static const CodecInfo kEAC3CodecInfo = { "mp4a.a6", CodecInfo::AUDIO, NULL,
+                                          CodecInfo::HISTOGRAM_EAC3 };
 #endif
 
 static const CodecInfo* kVideoMP4Codecs[] = {
@@ -172,24 +187,71 @@ static const SupportedTypeInfo kSupportedTypeInfo[] = {
 #endif
 };
 
+// Verify that |codec_info| is supported on this platform.
+//
+// Returns true if |codec_info| is a valid audio/video codec and is allowed.
+// |audio_codecs| has |codec_info|.tag added to its list if |codec_info| is an
+// audio codec. |audio_codecs| may be NULL, in which case it is not updated.
+// |video_codecs| has |codec_info|.tag added to its list if |codec_info| is a
+// video codec. |video_codecs| may be NULL, in which case it is not updated.
+//
+// Returns false otherwise, and |audio_codecs| and |video_codecs| not touched.
+static bool VerifyCodec(
+    const CodecInfo* codec_info,
+    std::vector<CodecInfo::HistogramTag>* audio_codecs,
+    std::vector<CodecInfo::HistogramTag>* video_codecs) {
+  const CommandLine* cmd_line = CommandLine::ForCurrentProcess();
+  switch (codec_info->type) {
+    case CodecInfo::AUDIO:
+#if defined(ENABLE_EAC3_PLAYBACK)
+      if (codec_info->tag == CodecInfo::HISTOGRAM_EAC3 &&
+          !cmd_line->HasSwitch(switches::kEnableEac3Playback)) {
+        return false;
+      }
+#endif
+      if (audio_codecs)
+        audio_codecs->push_back(codec_info->tag);
+      return true;
+    case CodecInfo::VIDEO:
+      // TODO(tomfinegan): Remove this check (or negate it, if we just
+      // negate the flag) when VP9 is enabled by default.
+      if (codec_info->tag == CodecInfo::HISTOGRAM_VP9 &&
+          !cmd_line->HasSwitch(switches::kEnableVp9Playback)) {
+        return false;
+      }
+      if (video_codecs)
+        video_codecs->push_back(codec_info->tag);
+      return true;
+    default:
+      // Not audio or video, so skip it.
+      DVLOG(1) << "CodecInfo type of " << codec_info->type
+               << " should not be specified in a SupportedTypes list";
+      return false;
+  }
+}
+
 // Checks to see if the specified |type| and |codecs| list are supported.
+//
 // Returns true if |type| and all codecs listed in |codecs| are supported.
-//         |factory_function| contains a function that can build a StreamParser
-//                            for this type.
-//         |has_audio| is true if an audio codec was specified.
-//         |has_video| is true if a video codec was specified.
-// Returns false otherwise. The values of |factory_function|, |has_audio|,
-//         and |has_video| are undefined.
-static bool IsSupported(const std::string& type,
-                        const std::vector<std::string>& codecs,
-                        const media::LogCB& log_cb,
-                        ParserFactoryFunction* factory_function,
-                        bool* has_audio,
-                        bool* has_video) {
+// |factory_function| contains a function that can build a StreamParser for this
+// type. Value may be NULL, in which case it is not touched.
+// |audio_codecs| is updated with the appropriate HistogramTags for matching
+// audio codecs specified in |codecs|. Value may be NULL, in which case it is
+// not touched.
+// |video_codecs| is updated with the appropriate HistogramTags for matching
+// video codecs specified in |codecs|. Value may be NULL, in which case it is
+// not touched.
+//
+// Returns false otherwise. The values of |factory_function|, |audio_codecs|,
+// and |video_codecs| are undefined.
+static bool CheckTypeAndCodecs(
+    const std::string& type,
+    const std::vector<std::string>& codecs,
+    const media::LogCB& log_cb,
+    ParserFactoryFunction* factory_function,
+    std::vector<CodecInfo::HistogramTag>* audio_codecs,
+    std::vector<CodecInfo::HistogramTag>* video_codecs) {
   DCHECK_GT(codecs.size(), 0u);
-  *factory_function = NULL;
-  *has_audio = false;
-  *has_video = false;
 
   // Search for the SupportedTypeInfo for |type|.
   for (size_t i = 0; i < arraysize(kSupportedTypeInfo); ++i) {
@@ -200,53 +262,25 @@ static bool IsSupported(const std::string& type,
       for (size_t j = 0; j < codecs.size(); ++j) {
         // Search the type info for a match.
         bool found_codec = false;
-        CodecInfo::Type codec_type = CodecInfo::UNKNOWN;
         std::string codec_id = codecs[j];
         for (int k = 0; type_info.codecs[k]; ++k) {
           if (MatchPattern(codec_id, type_info.codecs[k]->pattern) &&
               (!type_info.codecs[k]->validator ||
                type_info.codecs[k]->validator(codec_id, log_cb))) {
-            found_codec = true;
-            codec_type = type_info.codecs[k]->type;
-            break;
+            found_codec =
+                VerifyCodec(type_info.codecs[k], audio_codecs, video_codecs);
+            break;  // Since only 1 pattern will match, no need to check others.
           }
         }
-
         if (!found_codec) {
           MEDIA_LOG(log_cb) << "Codec '" << codec_id
-                            <<"' is not supported for '" << type << "'";
+                            << "' is not supported for '" << type << "'";
           return false;
-        }
-
-        const CommandLine* cmd_line = CommandLine::ForCurrentProcess();
-        switch (codec_type) {
-          case CodecInfo::AUDIO:
-#if defined(ENABLE_EAC3_PLAYBACK)
-            if (MatchPattern(codec_id, kEAC3CodecInfo.pattern) &&
-                !cmd_line->HasSwitch(switches::kEnableEac3Playback)) {
-              return false;
-            }
-#endif
-            *has_audio = true;
-            break;
-          case CodecInfo::VIDEO:
-            // TODO(tomfinegan): Remove this check (or negate it, if we just
-            // negate the flag) when VP9 is enabled by default.
-            if (MatchPattern(codec_id, kVP9CodecInfo.pattern) &&
-                !cmd_line->HasSwitch(switches::kEnableVp9Playback)) {
-              return false;
-            }
-
-            *has_video = true;
-            break;
-          default:
-            MEDIA_LOG(log_cb) << "Unsupported codec type '"<< codec_type
-                              << "' for " << codec_id;
-            return false;
         }
       }
 
-      *factory_function = type_info.factory_function;
+      if (factory_function)
+        *factory_function = type_info.factory_function;
 
       // All codecs were supported by this |type|.
       return true;
@@ -259,25 +293,46 @@ static bool IsSupported(const std::string& type,
 
 bool StreamParserFactory::IsTypeSupported(
     const std::string& type, const std::vector<std::string>& codecs) {
-  bool has_audio;
-  bool has_video;
-  ParserFactoryFunction factory_function;
-  return IsSupported(type, codecs, media::LogCB(), &factory_function,
-                     &has_audio, &has_video);
+  return CheckTypeAndCodecs(type, codecs, media::LogCB(), NULL, NULL, NULL);
 }
 
 scoped_ptr<media::StreamParser> StreamParserFactory::Create(
-    const std::string& type, const std::vector<std::string>& codecs,
-      const media::LogCB& log_cb, bool* has_audio, bool* has_video) {
+    const std::string& type,
+    const std::vector<std::string>& codecs,
+    const media::LogCB& log_cb,
+    bool* has_audio,
+    bool* has_video) {
   scoped_ptr<media::StreamParser> stream_parser;
   ParserFactoryFunction factory_function;
-  if (IsSupported(type, codecs, log_cb, &factory_function,
-                  has_audio, has_video)) {
+  std::vector<CodecInfo::HistogramTag> audio_codecs;
+  std::vector<CodecInfo::HistogramTag> video_codecs;
+  *has_audio = false;
+  *has_video = false;
+
+  if (CheckTypeAndCodecs(type,
+                         codecs,
+                         log_cb,
+                         &factory_function,
+                         &audio_codecs,
+                         &video_codecs)) {
+    *has_audio = !audio_codecs.empty();
+    *has_video = !video_codecs.empty();
+
+    // Log the number of codecs specified, as well as the details on each one.
+    UMA_HISTOGRAM_COUNTS_100("Media.MSE.NumberOfTracks", codecs.size());
+    for (size_t i = 0; i < audio_codecs.size(); ++i) {
+      UMA_HISTOGRAM_ENUMERATION(
+          "Media.MSE.AudioCodec", audio_codecs[i], CodecInfo::HISTOGRAM_MAX);
+    }
+    for (size_t i = 0; i < video_codecs.size(); ++i) {
+      UMA_HISTOGRAM_ENUMERATION(
+          "Media.MSE.VideoCodec", video_codecs[i], CodecInfo::HISTOGRAM_MAX);
+    }
+
     stream_parser.reset(factory_function(codecs, log_cb));
   }
 
   return stream_parser.Pass();
 }
-
 
 }  // namespace media
