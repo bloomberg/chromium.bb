0bdc2650d40bf11c024dd78a3a7a817d3d699129
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-07 23:47:05 +0000

    Revert 204671 "Default to using --3way when using `git cl patch`."
    > Default to using --3way when using `git cl patch`.
    > 
    > Resolving conflicts is much more fun this way.
    > 
    > BUG=None
    > TEST=Ran `git cl patch <issue>` with both clean and unclean patches, also checked behaviour of --reject is preserved.
    > R=maruel@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/16211005
    
    TBR=tapted@chromium.org
    
    Review URL: https://codereview.chromium.org/15786008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@204978 0039d316-1c4b-4281-b951-d872f2087c98

--- git_cl.py
+++ git_cl.py
@@ -1692,8 +1692,7 @@ def CMDpatch(parser, args):
   parser.add_option('-f', action='store_true', dest='force',
                     help='with -b, clobber any existing branch')
   parser.add_option('--reject', action='store_true', dest='reject',
-                    help='failed patches spew .rej files rather than '
-                        'attempting a 3-way merge')
+                    help='allow failed patches and spew .rej files')
   parser.add_option('-n', '--no-commit', action='store_true', dest='nocommit',
                     help="don't commit after patch applies")
   (options, args) = parser.parse_args(args)
@@ -1751,8 +1750,6 @@ def CMDpatch(parser, args):
   cmd = ['git', '--no-pager', 'apply', '--index', '-p0']
   if options.reject:
     cmd.append('--reject')
-  else:
-    cmd.append('--3way')
   try:
     subprocess2.check_call(cmd, stdin=patch_data, stdout=subprocess2.VOID)
   except subprocess2.CalledProcessError:
