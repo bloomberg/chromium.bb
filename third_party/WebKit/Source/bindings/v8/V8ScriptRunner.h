9731df4757ff7cfdb286a8a6997ed55f794545c1
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-10 06:21:02 +0000

    Build fix after r152101
    TBR=dcarney
    
    Review URL: https://codereview.chromium.org/15819006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152102 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0d400eb6dc39b467bab67da3177ea1cc1f3e7c31

--- third_party/WebKit/Source/bindings/v8/V8ScriptRunner.h
+++ third_party/WebKit/Source/bindings/v8/V8ScriptRunner.h
@@ -41,7 +41,7 @@ public:
     // For the following methods, the caller sites have to hold
     // a HandleScope and a ContextScope.
     static PassOwnPtr<v8::ScriptData> precompileScript(v8::Handle<v8::String>, CachedScript*);
-    static v8::Local<v8::Script> compileScript(v8::Handle<v8::String>, const String&, const TextPosition&, v8::Isolate*);
+    static v8::Local<v8::Script> compileScript(v8::Handle<v8::String>, const String&, const TextPosition&, v8::ScriptData*, v8::Isolate*);
     static v8::Local<v8::Value> runCompiledScript(v8::Handle<v8::Script>, ScriptExecutionContext*);
     static v8::Local<v8::Value> compileAndRunInternalScript(v8::Handle<v8::String>, v8::Isolate*, const String& = String(), const TextPosition& = TextPosition(), v8::ScriptData* = 0);
     static v8::Local<v8::Value> callInternalFunction(v8::Handle<v8::Function>, v8::Handle<v8::Object>, int argc, v8::Handle<v8::Value> args[], v8::Isolate*);
