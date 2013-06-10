40ad35c5b047bc28f0208dda45113023911ab7fd
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-10 18:56:43 +0000

    [content shell] don't synchronously destroy the main window if a plugin crashed
    WebContentsImpl doesn't expect to be deleted in the call to its
    delegate, so we shouldn't.
    
    BUG=247965
    R=marja@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16738003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205257 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3085128ee0b77d67afc1c4f51c4fda569033d82c

--- content/shell/webkit_test_controller.cc
+++ content/shell/webkit_test_controller.cc
@@ -338,7 +338,10 @@ void WebKitTestController::PluginCrashed(const base::FilePath& plugin_path,
   DCHECK(CalledOnValidThread());
   printer_->AddErrorMessage(
       base::StringPrintf("#CRASHED - plugin (pid %d)", plugin_pid));
-  DiscardMainWindow();
+  base::MessageLoop::current()->PostTask(
+      FROM_HERE,
+      base::Bind(base::IgnoreResult(&WebKitTestController::DiscardMainWindow),
+                 base::Unretained(this)));
 }
 
 void WebKitTestController::RenderViewCreated(RenderViewHost* render_view_host) {
