ed94b3442a55aa2d7dd9e6b308089c88d507952f
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-10 08:36:59 +0000

    Disable AppApiTest.AppProcessInstances on Windows
    BUG=248047
    TBR=kjellander@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/16625005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205165 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4ef849e6d1d7617986754b96eee95042c4021033

--- chrome/browser/extensions/app_process_apitest.cc
+++ chrome/browser/extensions/app_process_apitest.cc
@@ -258,7 +258,13 @@ IN_PROC_BROWSER_TEST_F(AppApiTest, MAYBE_AppProcess) {
 
 // Test that hosted apps without the background permission use a process per app
 // instance model, such that separate instances are in separate processes.
-IN_PROC_BROWSER_TEST_F(AppApiTest, AppProcessInstances) {
+// Flaky on Windows. http://crbug.com/248047
+#if defined(OS_WIN)
+#define MAYBE_AppProcessInstances DISABLED_AppProcessInstances
+#else
+#define MAYBE_AppProcessInstances AppProcessInstances
+#endif
+IN_PROC_BROWSER_TEST_F(AppApiTest, MAYBE_AppProcessInstances) {
   TestAppInstancesHelper("app_process_instances");
 }
 
