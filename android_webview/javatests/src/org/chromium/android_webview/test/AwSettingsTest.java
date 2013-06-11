92bc48d2e7b717f95bb94ce00822f109a03a24b4
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 15:50:59 +0000

    [Android WebView] Actually disaled @DisabledTests.
    @DisabledTest must be the only annotation on a test.
    
    BUG=248249
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/16777002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205554 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2f2954d1e96f3c9f48762907efc3d13caa1c7ef9

--- android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
@@ -1369,9 +1369,11 @@ public class AwSettingsTest extends AwTestBase {
     // setLoadsImagesAutomatically to false doesn't prevent images already
     // contained in the memory cache to be displayed.  The cache is shared among
     // all views, so the image can be put there by another view.
-    // Fixme (crbug.com/248249): Needs investigation after crbug.com/224317
-    @Feature({"AndroidWebView", "Preferences"})
-    @SmallTest
+    /*
+     * Fixme (crbug.com/248249): Needs investigation after crbug.com/224317
+     * @Feature({"AndroidWebView", "Preferences"})
+     * @SmallTest
+     */
     @DisabledTest
     public void testLoadsImagesAutomaticallyWithCachedImage() throws Throwable {
         ViewPair views = createViews();
@@ -1441,10 +1443,13 @@ public class AwSettingsTest extends AwTestBase {
     // The test verifies that after changing the LoadsImagesAutomatically
     // setting value from false to true previously skipped images are
     // automatically loaded.
-    // Fixme (crbug.com/248249): Needs investigation after crbug.com/224317
-    @SmallTest
+
+    /*
+     * Fixme (crbug.com/248249): Needs investigation after crbug.com/224317
+     * @SmallTest
+     * @Feature({"AndroidWebView", "Preferences"})
+     */
     @DisabledTest
-    @Feature({"AndroidWebView", "Preferences"})
     public void testLoadsImagesAutomaticallyNoPageReload() throws Throwable {
         final TestAwContentsClient contentClient = new TestAwContentsClient();
         final AwTestContainerView testContainerView =
@@ -1477,10 +1482,13 @@ public class AwSettingsTest extends AwTestBase {
         assertEquals(ImagePageGenerator.IMAGE_LOADED_STRING, getTitleOnUiThread(awContents));
     }
 
-    // Fixme (crbug.com/248249): Needs investigation after crbug.com/224317
-    @SmallTest
+
+    /*
+     * Fixme (crbug.com/248249): Needs investigation after crbug.com/224317
+     * @SmallTest
+     * @Feature({"AndroidWebView", "Preferences"})
+     */
     @DisabledTest
-    @Feature({"AndroidWebView", "Preferences"})
     public void testLoadsImagesAutomaticallyWithTwoViews() throws Throwable {
         ViewPair views = createViews();
         runPerViewSettingsTest(
