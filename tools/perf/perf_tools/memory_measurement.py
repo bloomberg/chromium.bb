6b79b2f8a0d012f4d2e4643177e82cac10f2bef9
abarth@chromium.org <abarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 10:36:13 +0000

    memory_measurement fails on android-content-shell
    We seem to be missing the --enable-memory-benchmarking flag.
    
    R=bulach
    BUG=247543
    
    Review URL: https://chromiumcodereview.appspot.com/16012013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205485 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 34a32ef2fdc9cd730b133705d455c7beea62222d

--- tools/perf/perf_tools/memory_measurement.py
+++ tools/perf/perf_tools/memory_measurement.py
@@ -30,6 +30,7 @@ class MemoryMeasurement(page_measurement.PageMeasurement):
 
   def CustomizeBrowserOptions(self, options):
     options.AppendExtraBrowserArg('--enable-stats-collection-bindings')
+    options.AppendExtraBrowserArg('--enable-memory-benchmarking')
     # For a hard-coded set of Google pages (such as GMail), we produce custom
     # memory histograms (V8.Something_gmail) instead of the generic histograms
     # (V8.Something), if we detect that a renderer is only rendering this page
