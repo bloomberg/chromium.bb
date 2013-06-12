20e2978d0fd46e9f8044a55e14e78e19e7347c73
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 14:34:40 +0000

    Revert 205772 "Revert "Revert 205443 "headless windows content_s..."
    > Revert "Revert 205443 "headless windows content_shell""
    > 
    > Also, add more verbose debug output to LOGFONT pickle code
    > 
    > The NOTREACHED() is reached on WebKit Win7 (dbg) bots, but I cannot reproduce
    > locally, so I need more data from the bots
    > 
    > I'll revert the CL once the Webkit Win Debug builder cycled once.
    > 
    > BUG=248520
    > TBR=jam@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/16309013
    
    TBR=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/16034019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205775 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 702e3a6a7e1a9c335e3aee534f690ea7a3cf61e1

--- content/shell/shell_win.cc
+++ content/shell/shell_win.cc
@@ -57,8 +57,6 @@ void Shell::PlatformCleanUp() {
 }
 
 void Shell::PlatformEnableUIControl(UIControl control, bool is_enabled) {
-  if (headless_)
-    return;
   int id;
   switch (control) {
     case BACK_BUTTON:
@@ -78,8 +76,6 @@ void Shell::PlatformEnableUIControl(UIControl control, bool is_enabled) {
 }
 
 void Shell::PlatformSetAddressBarURL(const GURL& url) {
-  if (headless_)
-    return;
   std::wstring url_string = UTF8ToWide(url.spec());
   SendMessage(url_edit_view_, WM_SETTEXT, 0,
               reinterpret_cast<LPARAM>(url_string.c_str()));
@@ -89,8 +85,6 @@ void Shell::PlatformSetIsLoading(bool loading) {
 }
 
 void Shell::PlatformCreateWindow(int width, int height) {
-  if (headless_)
-    return;
   window_ = CreateWindow(kWindowClass, kWindowTitle,
                          WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN,
                          CW_USEDEFAULT, 0, CW_USEDEFAULT, 0,
@@ -140,14 +134,10 @@ void Shell::PlatformCreateWindow(int width, int height) {
 }
 
 void Shell::PlatformSetContents() {
-  if (headless_)
-    return;
   SetParent(web_contents_->GetView()->GetNativeView(), window_);
 }
 
 void Shell::SizeTo(int width, int height) {
-  if (headless_)
-    return;
   RECT rc, rw;
   GetClientRect(window_, &rc);
   GetWindowRect(window_, &rw);
@@ -168,8 +158,6 @@ void Shell::SizeTo(int width, int height) {
 }
 
 void Shell::PlatformResizeSubViews() {
-  if (headless_)
-    return;
   RECT rc;
   GetClientRect(window_, &rc);
 
@@ -181,10 +169,6 @@ void Shell::PlatformResizeSubViews() {
 }
 
 void Shell::Close() {
-  if (headless_) {
-    delete this;
-    return;
-  }
   DestroyWindow(window_);
 }
 
@@ -215,8 +199,6 @@ LRESULT CALLBACK Shell::WndProc(HWND hwnd, UINT message, WPARAM wParam,
       int id = LOWORD(wParam);
       switch (id) {
         case IDM_NEW_WINDOW:
-          if (shell->headless_)
-            return 0;
           CreateNewWindow(
               shell->web_contents()->GetBrowserContext(),
               GURL(), NULL, MSG_ROUTING_NONE, gfx::Size());
@@ -297,8 +279,6 @@ LRESULT CALLBACK Shell::EditWndProc(HWND hwnd, UINT message,
 }
 
 void Shell::PlatformSetTitle(const string16& text) {
-  if (headless_)
-    return;
   ::SetWindowText(window_, text.c_str());
 }
 
