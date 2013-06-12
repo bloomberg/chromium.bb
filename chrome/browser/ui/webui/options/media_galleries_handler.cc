8a493642f08f007a9aa0957c31b2e21fbf9d92f2
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 14:48:41 +0000

    [MediaGalleries] Use the standard controller method to retrieve gallery names to show for WebUI control list.
    R=vandebo@chromium.org
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/16123027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205780 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 220e025910f6cf98f329d85fcdd94ef7f45e0f4c

--- chrome/browser/ui/webui/options/media_galleries_handler.cc
+++ chrome/browser/ui/webui/options/media_galleries_handler.cc
@@ -9,6 +9,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/media_galleries/media_file_system_registry.h"
+#include "chrome/browser/media_galleries/media_galleries_dialog_controller.h"
 #include "chrome/browser/media_galleries/media_galleries_preferences.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/storage_monitor/storage_monitor.h"
@@ -23,6 +24,7 @@
 
 namespace options {
 
+using chrome::MediaGalleriesDialogController;
 using chrome::MediaGalleriesPreferences;
 using chrome::MediaGalleriesPrefInfoMap;
 using chrome::MediaGalleryPrefInfo;
@@ -104,7 +106,9 @@ void MediaGalleriesHandler::OnGalleriesChanged() {
       continue;
 
     DictionaryValue* dict = new DictionaryValue();
-    dict->SetString("displayName", gallery.display_name);
+    dict->SetString("displayName",
+        MediaGalleriesDialogController::GetGalleryDisplayNameNoAttachment(
+            gallery));
     dict->SetString("path", gallery.AbsolutePath().LossyDisplayName());
     dict->SetString("id", base::Uint64ToString(gallery.pref_id));
     list.Append(dict);
