505bc5c9fa60c4bc838bb22f5b3a1f30c089d97b
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 16:46:17 +0000

    Revert 205585 "Disabling flaky browser_tests item BrowserNavigat..."
    > Disabling flaky browser_tests item BrowserNavigatorTest.CloseSingletonTab in Windows.
    > 
    > BUG=248584
    > TBR=ben
    > 
    > Review URL: https://codereview.chromium.org/16276011
    
    TBR=huangs@chromium.org
    
    Review URL: https://codereview.chromium.org/16747009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205824 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4e78b2d1171eb59d02fde937d7d1765f93c1ff6f

--- chrome/browser/ui/browser_navigator_browsertest.cc
+++ chrome/browser/ui/browser_navigator_browsertest.cc
@@ -1196,14 +1196,7 @@ IN_PROC_BROWSER_TEST_F(BrowserNavigatorTest,
                 GetActiveWebContents()->GetURL()));
 }
 
-// See crbug.com/248584.
-#if defined(OS_WIN)
-#define MAYBE_CloseSingletonTab DISABLED_CloseSingletonTab
-#else
-#define MAYBE_CloseSingletonTab CloseSingletonTab
-#endif
-
-IN_PROC_BROWSER_TEST_F(BrowserNavigatorTest, MAYBE_CloseSingletonTab) {
+IN_PROC_BROWSER_TEST_F(BrowserNavigatorTest, CloseSingletonTab) {
   for (int i = 0; i < 2; ++i) {
     content::WindowedNotificationObserver observer(
         content::NOTIFICATION_LOAD_STOP,
