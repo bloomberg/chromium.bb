b80e03fd7170bab69f32494261e0887d65c3c8e0
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 09:38:18 +0000

    Disable WebViewTest.DOMStorageIsolation on Windows
    BUG=248873
    TBR=nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/16606006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205739 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7bcb6ecde0254a97a297c9781158fd14c209920d

--- chrome/browser/extensions/web_view_browsertest.cc
+++ chrome/browser/extensions/web_view_browsertest.cc
@@ -680,11 +680,18 @@ IN_PROC_BROWSER_TEST_F(WebViewTest, DISABLED_StoragePersistence) {
   EXPECT_EQ("persist2=true", cookie_value);
 }
 
+#if defined(OS_WIN)
+// This test is very flaky on Win Aura and XP. http://crbug.com/248873
+#define MAYBE_DOMStorageIsolation DISABLED_DOMStorageIsolation
+#else
+#define MAYBE_DOMStorageIsolation DOMStorageIsolation
+#endif
+
 // This tests DOM storage isolation for packaged apps with webview tags. It
 // loads an app with multiple webview tags and each tag sets DOM storage
 // entries, which the test checks to ensure proper storage isolation is
 // enforced.
-IN_PROC_BROWSER_TEST_F(WebViewTest, DOMStorageIsolation) {
+IN_PROC_BROWSER_TEST_F(WebViewTest, MAYBE_DOMStorageIsolation) {
   ASSERT_TRUE(StartTestServer());
   GURL regular_url = test_server()->GetURL("files/title1.html");
 
