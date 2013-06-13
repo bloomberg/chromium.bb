37398b68496c85f2038d6b4fed72403c05c4ebfc
jamesr@chromium.org <jamesr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-13 19:46:07 +0000

    Add more tests for hostname encoding in database identifiers
    This tests the encoding of hostnames with various characters into database
    identifiers. Many of these inputs are not valid hostnames, but we want to
    have consistent behavior for all inputs as this is run on untrusted input.
    
    We could also have the database identifier code more aggressively reject
    invalid or non-canonicalized input data, but that would have to be done
    with care to make sure that we don't accidentally orphan user data.
    
    BUG=243095
    
    Review URL: https://chromiumcodereview.appspot.com/16934002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152372 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 5d20a48a23c39893f1d90a760f0c751a8d430057

--- third_party/WebKit/Source/weborigin/DatabaseIdentifierTest.cpp
+++ third_party/WebKit/Source/weborigin/DatabaseIdentifierTest.cpp
@@ -40,7 +40,6 @@
 using WebCore::SecurityOrigin;
 using WebCore::createDatabaseIdentifierFromSecurityOrigin;
 using WebCore::createSecurityOriginFromDatabaseIdentifier;
-using WebCore::encodeWithURLEscapeSequences;
 
 namespace {
 
@@ -64,13 +63,141 @@ TEST(DatabaseIdentifierTest, CreateIdentifierFromSecurityOrigin)
         {"file", "", 0, "file__0"},
         {"data", "", 0, "__0"},
         {"about", "blank", 0, "__0"},
+        {"non-standard", "foobar.com", 0, "non-standard__0"},
     };
 
     for (size_t i = 0; i < ARRAYSIZE_UNSAFE(cases); ++i) {
         RefPtr<SecurityOrigin> origin = SecurityOrigin::create(cases[i].protocol, cases[i].host, cases[i].port);
         String identifier = createDatabaseIdentifierFromSecurityOrigin(origin.get());
-        EXPECT_EQ(cases[i].expectedIdentifier, identifier) << "test case " << i;
+        EXPECT_EQ(cases[i].expectedIdentifier, identifier) << "test case " << origin->toString();
     }
+}
+
+// This tests the encoding of a hostname including every character in the range [\x1f, \x80].
+TEST(DatabaseIdentifierTest, CreateIdentifierAllHostChars)
+{
+    struct Case {
+        String hostname;
+        String expected;
+        bool shouldRoundTrip;
+    } cases[] = {
+        {"x\x1Fx", "__0", false},
+
+        {"x\x20x", "http_x%2520x_0", false},
+        {"x\x21x", "http_x%2521x_0", false},
+        {"x\x22x", "http_x%2522x_0", false},
+        {"x\x23x", "http_x_0"}, // 'x#x', the # and following are ignored.
+        {"x\x24x", "http_x%2524x_0", false},
+        {"x\x25x", "__0", false},
+        {"x\x26x", "http_x%2526x_0", false},
+        {"x\x27x", "http_x%2527x_0", false},
+        {"x\x28x", "http_x%2528x_0", false},
+        {"x\x29x", "http_x%2529x_0", false},
+        {"x\x2ax", "http_x%252ax_0", false},
+        {"x\x2bx", "http_x+x_0"},
+        {"x\x2cx", "http_x%252cx_0", false},
+        {"x\x2dx", "http_x-x_0", true},
+        {"x\x2ex", "http_x.x_0", true},
+        {"x\x2fx", "http_x_0", false}, // 'x/x', the / and following are ignored.
+
+        {"x\x30x", "http_x0x_0", true},
+        {"x\x31x", "http_x1x_0", true},
+        {"x\x32x", "http_x2x_0", true},
+        {"x\x33x", "http_x3x_0", true},
+        {"x\x34x", "http_x4x_0", true},
+        {"x\x35x", "http_x5x_0", true},
+        {"x\x36x", "http_x6x_0", true},
+        {"x\x37x", "http_x7x_0", true},
+        {"x\x38x", "http_x8x_0", true},
+        {"x\x39x", "http_x9x_0", true},
+        {"x\x3ax", "__0", false},
+        {"x\x3bx", "__0", false},
+        {"x\x3cx", "http_x%253cx_0", false},
+        {"x\x3dx", "http_x%253dx_0", false},
+        {"x\x3ex", "http_x%253ex_0", false},
+        {"x\x3fx", "http_x_0", false}, // 'x?x', the ? and following are ignored.
+
+        {"x\x40x", "http_x_0", false}, // 'x@x', the @ and following are ignored.
+        {"x\x41x", "http_xax_0", true},
+        {"x\x42x", "http_xbx_0", true},
+        {"x\x43x", "http_xcx_0", true},
+        {"x\x44x", "http_xdx_0", true},
+        {"x\x45x", "http_xex_0", true},
+        {"x\x46x", "http_xfx_0", true},
+        {"x\x47x", "http_xgx_0", true},
+        {"x\x48x", "http_xhx_0", true},
+        {"x\x49x", "http_xix_0", true},
+        {"x\x4ax", "http_xjx_0", true},
+        {"x\x4bx", "http_xkx_0", true},
+        {"x\x4cx", "http_xlx_0", true},
+        {"x\x4dx", "http_xmx_0", true},
+        {"x\x4ex", "http_xnx_0", true},
+        {"x\x4fx", "http_xox_0", true},
+
+        {"x\x50x", "http_xpx_0", true},
+        {"x\x51x", "http_xqx_0", true},
+        {"x\x52x", "http_xrx_0", true},
+        {"x\x53x", "http_xsx_0", true},
+        {"x\x54x", "http_xtx_0", true},
+        {"x\x55x", "http_xux_0", true},
+        {"x\x56x", "http_xvx_0", true},
+        {"x\x57x", "http_xwx_0", true},
+        {"x\x58x", "http_xxx_0", true},
+        {"x\x59x", "http_xyx_0", true},
+        {"x\x5ax", "http_xzx_0", true},
+        {"x\x5bx", "__0", false},
+        {"x\x5cx", "http_x_0", false}, // "x\x", the \ and following are ignored.
+        {"x\x5dx", "__0", false},
+        {"x\x5ex", "__0", false},
+        {"x\x5fx", "http_x_x_0", true},
+
+        {"x\x60x", "http_x%2560x_0", false},
+        {"x\x61x", "http_xax_0", true},
+        {"x\x62x", "http_xbx_0", true},
+        {"x\x63x", "http_xcx_0", true},
+        {"x\x64x", "http_xdx_0", true},
+        {"x\x65x", "http_xex_0", true},
+        {"x\x66x", "http_xfx_0", true},
+        {"x\x67x", "http_xgx_0", true},
+        {"x\x68x", "http_xhx_0", true},
+        {"x\x69x", "http_xix_0", true},
+        {"x\x6ax", "http_xjx_0", true},
+        {"x\x6bx", "http_xkx_0", true},
+        {"x\x6cx", "http_xlx_0", true},
+        {"x\x6dx", "http_xmx_0", true},
+        {"x\x6ex", "http_xnx_0", true},
+        {"x\x6fx", "http_xox_0", true},
+
+        {"x\x70x", "http_xpx_0", true},
+        {"x\x71x", "http_xqx_0", true},
+        {"x\x72x", "http_xrx_0", true},
+        {"x\x73x", "http_xsx_0", true},
+        {"x\x74x", "http_xtx_0", true},
+        {"x\x75x", "http_xux_0", true},
+        {"x\x76x", "http_xvx_0", true},
+        {"x\x77x", "http_xwx_0", true},
+        {"x\x78x", "http_xxx_0", true},
+        {"x\x79x", "http_xyx_0", true},
+        {"x\x7ax", "http_xzx_0", true},
+        {"x\x7bx", "http_x%257bx_0", false},
+        {"x\x7cx", "http_x%257cx_0", false},
+        {"x\x7dx", "http_x%257dx_0", false},
+        {"x\x7ex", "__0", false},
+        {"x\x7fx", "__0", false},
+
+        {"x\x80x", "__0", false},
+    };
+
+    for (size_t i = 0; i < ARRAYSIZE_UNSAFE(cases); ++i) {
+        RefPtr<SecurityOrigin> origin = SecurityOrigin::create("http", cases[i].hostname, 80);
+        String identifier = createDatabaseIdentifierFromSecurityOrigin(origin.get());
+        EXPECT_EQ(cases[i].expected, identifier) << "test case " << i << ": \"" << cases[i].hostname << "\"";
+        if (cases[i].shouldRoundTrip) {
+            RefPtr<SecurityOrigin> parsedOrigin = createSecurityOriginFromDatabaseIdentifier(identifier);
+            EXPECT_EQ(cases[i].hostname.lower(), parsedOrigin->host()) << "test case " << i;
+        }
+    }
+
 }
 
 TEST(DatabaseIdentifierTest, CreateSecurityOriginFromIdentifier)
@@ -94,6 +221,7 @@ TEST(DatabaseIdentifierTest, CreateSecurityOriginFromIdentifier)
         {"__0", "", "", 0, "null", true},
         {"http_foo_bar_baz.org_0", "http", "foo_bar_baz.org", 0, "http://foo_bar_baz.org", false},
         {"http_xn--n3h.unicode.com_0", "http", "xn--n3h.unicode.com", 0, "http://xn--n3h.unicode.com", false},
+        {"http_dot.com_0", "http", "dot.com", 0, "http://dot.com", false},
     };
 
     for (size_t i = 0; i < ARRAYSIZE_UNSAFE(validCases); ++i) {
@@ -116,6 +244,7 @@ TEST(DatabaseIdentifierTest, CreateSecurityOriginFromIdentifier)
         "http_shouldbeescaped\x7c.com_0",
         "http_latin1\x8a.org_8001",
         String::fromUTF8("http_\xe2\x98\x83.unicode.com_0"),
+        "http_dot%252ecom_0",
     };
 
     for (size_t i = 0; i < ARRAYSIZE_UNSAFE(bogusIdentifiers); ++i) {
