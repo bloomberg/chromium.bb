8bb3bfb44c803c229ce7b86a93b3cf2ca1e89d81
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 22:44:52 +0000

    Linux: fix compatibility with nss-3.15 .
    See https://groups.google.com/a/chromium.org/d/msg/chromium-packagers/Jwz20yLB_7E/acOh0BYai30J
    for more info.
    
    BUG=none
    R=wtc@chromium.org
    
    Review URL: https://codereview.chromium.org/16982002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206184 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: abf7641841f0ade77b8339e3f1368c9f3ff711bd

--- net/third_party/nss/ssl/bodge/secitem_array.c
+++ net/third_party/nss/ssl/bodge/secitem_array.c
@@ -6,6 +6,7 @@
  * Support routines for SECItemArray data structure.
  */
 
+#include "nssutil.h"
 #include "seccomon.h"
 #include "secitem.h"
 #include "secerr.h"
@@ -13,10 +14,15 @@
 
 typedef struct SECItemArrayStr SECItemArray;
 
+#define NSSUTIL_VERSION_NUM \
+    (NSSUTIL_VMAJOR * 10000 + NSSUTIL_VMINOR * 100 + NSSUTIL_VPATCH)
+#if NSSUTIL_VERSION_NUM < 31500
+// Added in NSS 3.15.
 struct SECItemArrayStr {
     SECItem *items;
     unsigned int len;
 };
+#endif
 
 SECItemArray *
 SECITEM_AllocArray(PLArenaPool *arena, SECItemArray *array, unsigned int len)
