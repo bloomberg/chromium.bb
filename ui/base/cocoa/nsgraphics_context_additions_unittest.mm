3265bddd02c5bef592c8175c257e756d5f223d42
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 06:12:35 +0000

    Disabling NSGraphicsContextCrAdditionsTest.PatternPhase
    Test is failing on bots. See bug.
    
    BUG=249241
    
    Review URL: https://codereview.chromium.org/16923003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206006 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 43e7d21f745fce3d399c947ce765efe30f7a7fab

--- ui/base/cocoa/nsgraphics_context_additions_unittest.mm
+++ ui/base/cocoa/nsgraphics_context_additions_unittest.mm
@@ -9,7 +9,8 @@
 
 typedef ui::CocoaTest NSGraphicsContextCrAdditionsTest;
 
-TEST_F(NSGraphicsContextCrAdditionsTest, PatternPhase) {
+// Test fails, see http://crbug.com/249241
+TEST_F(NSGraphicsContextCrAdditionsTest, DISABLED_PatternPhase) {
   NSRect frame = NSMakeRect(50, 50, 100, 100);
   scoped_nsobject<NSView> view([[NSView alloc] initWithFrame:frame]);
   [[test_window() contentView] addSubview:view];
