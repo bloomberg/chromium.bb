38d9435549a34d8f4701de536925aaaff7297a0c
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-14 03:00:57 +0000

    HTMLObjectElement does not need to manually reattach in most cases
    HTMLObjectElement::renderFallbackContent can use lazyAttach instead of reattach
    or calling attach and detach manually. This changes failed object loads to fire
    their onerror handler with slightly different timing in some cases, but it shouldn't
    be any different than if the load had come back from the network with different
    timing.
    
    This is a part of the change from https://codereview.chromium.org/15159008 which
    was reverted. Instead of attempting to land the larger change, we're going to do it
    in parts.
    
    Review URL: https://chromiumcodereview.appspot.com/17028004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152403 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 3f666755bbc1358d3ee0c5ba84a8a0cc396898fc

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -885,6 +885,16 @@ void Node::setNeedsStyleRecalc(StyleChangeType changeType)
 
 void Node::lazyAttach(ShouldSetAttached shouldSetAttached)
 {
+    // It's safe to synchronously attach here because we're in the middle of style recalc
+    // while it's not safe to mark nodes as needing style recalc except in the loop in
+    // Element::recalcStyle because we may mark an ancestor as not needing recalc and
+    // then the node would never get updated. One place this currently happens is
+    // HTMLObjectElement::renderFallbackContent which may call lazyAttach from inside
+    // attach which was triggered by a recalcStyle.
+    if (document()->inStyleRecalc()) {
+        attach();
+        return;
+    }
     for (Node* n = this; n; n = NodeTraversal::next(n, this)) {
         if (n->hasChildNodes())
             n->setChildNeedsStyleRecalc();
--- third_party/WebKit/Source/core/html/HTMLObjectElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLObjectElement.cpp
@@ -370,7 +370,7 @@ void HTMLObjectElement::renderFallbackContent()
         if (!isImageType()) {
             // If we don't think we have an image type anymore, then clear the image from the loader.
             m_imageLoader->setImage(0);
-            reattach();
+            lazyReattach();
             return;
         }
     }
@@ -378,8 +378,7 @@ void HTMLObjectElement::renderFallbackContent()
     m_useFallbackContent = true;
 
     // FIXME: Style gets recalculated which is suboptimal.
-    detach();
-    attach();
+    lazyReattach();
 }
 
 // FIXME: This should be removed, all callers are almost certainly wrong.
