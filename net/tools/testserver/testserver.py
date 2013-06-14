781ecdfabd15f6f03dc849bccab72301b0ae819c
senorblanco@chromium.org <senorblanco@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 06:57:50 +0000

    Remove MultipartSlowHandler, since the extension test referencing multipart-slow has been removed.
    Review URL: https://chromiumcodereview.appspot.com/16860011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206342 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 78c3af65054040d7104165a06e4e1af40733e773

--- net/tools/testserver/testserver.py
+++ net/tools/testserver/testserver.py
@@ -267,7 +267,6 @@ class TestPageHandler(testserver_base.BasePageHandler):
       self.ServerRedirectHandler,
       self.ClientRedirectHandler,
       self.MultipartHandler,
-      self.MultipartSlowHandler,
       self.GetSSLSessionCacheHandler,
       self.SSLManySmallRecords,
       self.GetChannelID,
@@ -1363,35 +1362,6 @@ class TestPageHandler(testserver_base.BasePageHandler):
       self.wfile.write('Content-Type: text/html\r\n\r\n')
       self.wfile.write('<title>page ' + str(i) + '</title>')
       self.wfile.write('page ' + str(i))
-
-    self.wfile.write('--' + bound + '--')
-    return True
-
-  def MultipartSlowHandler(self):
-    """Send a multipart response (3 text/html pages) with a slight delay
-    between each page.  This is similar to how some pages show status using
-    multipart."""
-
-    test_name = '/multipart-slow'
-    if not self._ShouldHandleRequest(test_name):
-      return False
-
-    num_frames = 3
-    bound = '12345'
-    self.send_response(200)
-    self.send_header('Content-Type',
-                     'multipart/x-mixed-replace;boundary=' + bound)
-    self.end_headers()
-
-    for i in xrange(num_frames):
-      self.wfile.write('--' + bound + '\r\n')
-      self.wfile.write('Content-Type: text/html\r\n\r\n')
-      time.sleep(0.25)
-      if i == 2:
-        self.wfile.write('<title>PASS</title>')
-      else:
-        self.wfile.write('<title>page ' + str(i) + '</title>')
-      self.wfile.write('page ' + str(i) + '<!-- ' + ('x' * 2048) + '-->')
 
     self.wfile.write('--' + bound + '--')
     return True
