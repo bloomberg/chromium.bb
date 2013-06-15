52d2c299d802666b6d9565179d63f2f626e0c700
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-15 14:44:00 +0000

    aura: Reenable window-slider unittests on win-aura.
    The crash in win-aura was likely happening because of invalid memory reads,
    fixed by crrev.com/206472. So re-enable the tests now.
    
    BUG=249634
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/17146002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206582 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8baab2cf657ef6dbefe43b42bf4f2d045421ae5b

--- content/browser/web_contents/aura/window_slider_unittest.cc
+++ content/browser/web_contents/aura/window_slider_unittest.cc
@@ -123,14 +123,7 @@ class WindowSliderDelegateTest : public WindowSlider::Delegate {
 
 typedef aura::test::AuraTestBase WindowSliderTest;
 
-#if defined(OS_WIN)
-// This test currently fails on the Win Aura (2) bot. http://crbug.com/249634
-#define MAYBE_WindowSlideUsingGesture DISABLED_WindowSlideUsingGesture
-#else
-#define MAYBE_WindowSlideUsingGesture WindowSlideUsingGesture
-#endif
-
-TEST_F(WindowSliderTest, MAYBE_WindowSlideUsingGesture) {
+TEST_F(WindowSliderTest, WindowSlideUsingGesture) {
   scoped_ptr<aura::Window> window(CreateNormalWindow(0, root_window(), NULL));
   window->SetBounds(gfx::Rect(0, 0, 400, 400));
   WindowSliderDelegateTest slider_delegate;
@@ -198,17 +191,9 @@ TEST_F(WindowSliderTest, MAYBE_WindowSlideUsingGesture) {
   EXPECT_TRUE(slider_delegate.slider_destroyed());
 }
 
-#if defined(OS_WIN)
-// This test currently fails on the Win Aura (2) bot. http://crbug.com/249634
-#define MAYBE_WindowSlideIsCancelledOnEvent \
-    DISABLED_WindowSlideIsCancelledOnEvent
-#else
-#define MAYBE_WindowSlideIsCancelledOnEvent WindowSlideIsCancelledOnEvent
-#endif
-
 // Tests that the window slide is cancelled when a different type of event
 // happens.
-TEST_F(WindowSliderTest, MAYBE_WindowSlideIsCancelledOnEvent) {
+TEST_F(WindowSliderTest, WindowSlideIsCancelledOnEvent) {
   scoped_ptr<aura::Window> window(CreateNormalWindow(0, root_window(), NULL));
   WindowSliderDelegateTest slider_delegate;
 
