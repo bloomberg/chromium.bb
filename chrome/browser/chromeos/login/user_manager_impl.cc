b1a4cf57851519a1153b95a3ca92c8f13545a158
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 19:42:41 +0000

    [sync] Fix mechanism by which chromeos::UserManagerImpl observes ProfileSyncService
    When a user signs in to Chrome OS, and NOTIFICATION_PROFILE_ADDED is
    fired, chromeos::UserManagerImpl::Observe() does not register itself as
    an observer of ProfileSyncService because the profile is not yet fully
    constructed, and therefore, the logged in profile is not identified as
    the default profile. This is due to a regression that was introduced in
    r176083.
    
    This is bad because UserManagerImpl fails to observe notifications from
    ProfileSyncService that are sent out as a result of auth errors. In
    fact, this was the root cause of bug http://crbug.com/234164.
    
    This patch fixes the mechanism by which UserManagerImpl registers itself
    as an observer of ProfileSyncService, by responding to
    NOTIFICATION_LOGIN_USER_PROFILE_PREPARED, which is fired after the
    Profile is fully constructed, and the logged in profile is now the
    default profile, resulting in UserManagerImpl correctly registering
    itself as an observer of ProfileSyncService.
    
    This patch also undoes the earlier fix landed with r206203.
    
    This is the true fix for http://crbug.com/234164.
    
    TBR=xiyuan@chromium.org
    BUG=234164, 249918
    TEST=Sign in, change password, sign out and back in; Gaia-reauth must be forced.
    
    Review URL: https://codereview.chromium.org/17022005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206775 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2175cc3705b076b03e46feb06c3e23d1135ba7f3

--- chrome/browser/chromeos/login/user_manager_impl.cc
+++ chrome/browser/chromeos/login/user_manager_impl.cc
@@ -212,7 +212,7 @@ UserManagerImpl::UserManagerImpl()
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   registrar_.Add(this, chrome::NOTIFICATION_OWNERSHIP_STATUS_CHANGED,
       content::NotificationService::AllSources());
-  registrar_.Add(this, chrome::NOTIFICATION_PROFILE_ADDED,
+  registrar_.Add(this, chrome::NOTIFICATION_LOGIN_USER_PROFILE_PREPARED,
       content::NotificationService::AllSources());
   RetrieveTrustedDevicePolicies();
   UpdateLoginState();
@@ -689,12 +689,12 @@ void UserManagerImpl::Observe(int type,
       CheckOwnership();
       RetrieveTrustedDevicePolicies();
       break;
-    case chrome::NOTIFICATION_PROFILE_ADDED:
+    case chrome::NOTIFICATION_LOGIN_USER_PROFILE_PREPARED:
       if (IsUserLoggedIn() &&
           !IsLoggedInAsGuest() &&
           !IsLoggedInAsLocallyManagedUser() &&
           !IsLoggedInAsKioskApp()) {
-        Profile* profile = content::Source<Profile>(source).ptr();
+        Profile* profile = content::Details<Profile>(details).ptr();
         if (!profile->IsOffTheRecord() &&
             profile == ProfileManager::GetDefaultProfile()) {
           // TODO(nkostylev): We should observe all logged in user's profiles.
