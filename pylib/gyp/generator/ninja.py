878ee285239e5777028c31d6b9d10fbc930ccda2
iannucci@chromium.org <iannucci@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2013-06-17 23:28:43 +0000

    Bump memory per linker to 4 to account for system overhead.
    R=scottmg@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/17121012
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1651 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/generator/ninja.py
+++ pylib/gyp/generator/ninja.py
@@ -1361,7 +1361,7 @@ def GetDefaultConcurrentLinks():
     stat.dwLength = ctypes.sizeof(stat)
     ctypes.windll.kernel32.GlobalMemoryStatusEx(ctypes.byref(stat))
 
-    return max(1, stat.ullTotalPhys / (2 ** 31))
+    return max(1, stat.ullTotalPhys / (4 ** 31))
   else:
     # TODO(scottmg): Implement this for other platforms.
     return 1
