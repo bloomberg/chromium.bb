d9ecc47cca6426e5fb6a33917ad9d327b0236b92
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 12:49:52 +0000

    Strip out \r characters from pexpect's log capture before adding the results.
    BUG=250002
    
    Review URL: https://chromiumcodereview.appspot.com/16854020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206960 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ab75a743507ae71f18a7861da6108dc9718843b0

--- build/android/pylib/gtest/test_package.py
+++ build/android/pylib/gtest/test_package.py
@@ -155,20 +155,21 @@ class TestPackage(object):
           break
         else:  # re_run
           full_test_name = p.match.group(1).replace('\r', '')
+          log = p.before.replace('\r', '')
           found = p.expect([re_ok, re_fail, re_crash], timeout=self.timeout)
           if found == 0:  # re_ok
             if full_test_name == p.match.group(1).replace('\r', ''):
               results.AddResult(base_test_result.BaseTestResult(
                   full_test_name, base_test_result.ResultType.PASS,
-                  log=p.before))
+                  log=log))
           elif found == 2:  # re_crash
             results.AddResult(base_test_result.BaseTestResult(
                 full_test_name, base_test_result.ResultType.CRASH,
-                log=p.before))
+                log=log))
             break
           else:  # re_fail
             results.AddResult(base_test_result.BaseTestResult(
-                full_test_name, base_test_result.ResultType.FAIL, log=p.before))
+                full_test_name, base_test_result.ResultType.FAIL, log=log))
     except pexpect.EOF:
       logging.error('Test terminated - EOF')
       # We're here because either the device went offline, or the test harness
@@ -178,13 +179,13 @@ class TestPackage(object):
                                              self.device)
       if full_test_name:
         results.AddResult(base_test_result.BaseTestResult(
-            full_test_name, base_test_result.ResultType.CRASH, log=p.before))
+            full_test_name, base_test_result.ResultType.CRASH, log=log))
     except pexpect.TIMEOUT:
       logging.error('Test terminated after %d second timeout.',
                     self.timeout)
       if full_test_name:
         results.AddResult(base_test_result.BaseTestResult(
-            full_test_name, base_test_result.ResultType.TIMEOUT, log=p.before))
+            full_test_name, base_test_result.ResultType.TIMEOUT, log=log))
     finally:
       p.close()
 
