c2f3fdf3bdc9f195a80d687ec8cd077f1ec81d18
fgalligan@chromium.org <fgalligan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 02:37:53 +0000

    Libvpx roll to r206753
    From r197977
    
    This roll adds support for the finalized VP9 bitstream.
    
    See:
    https://codereview.chromium.org/16109020/
    https://codereview.chromium.org/16863002/
    https://codereview.chromium.org/16970002/
    https://codereview.chromium.org/17028002/
    
    https://codereview.chromium.org/17286003/
    
    for the actual changes.
    
    Local Syzygy build passed.
    
    BUG=165504
    
    Review URL: https://chromiumcodereview.appspot.com/17300002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206883 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5b4773f72b2143f9d594ef638c15973aaf70c4bd

--- DEPS
+++ DEPS
@@ -20,7 +20,7 @@ vars = {
 
   "libjingle_revision": "342",
   "libphonenumber_revision": "456",
-  "libvpx_revision": "197977",
+  "libvpx_revision": "206753",
   "lss_revision": "20",
 
   # These two FFmpeg variables must be updated together.  One is used for SVN
--- media/filters/pipeline_integration_test.cc
+++ media/filters/pipeline_integration_test.cc
@@ -480,7 +480,9 @@ TEST_F(PipelineIntegrationTest, BasicPlayback_MediaSource) {
   Stop();
 }
 
-TEST_F(PipelineIntegrationTest, BasicPlayback_MediaSource_VideoOnly_VP9_WebM) {
+// TODO(fgalligan): Enable after new vp9 files are landed. crbug.com/180280
+TEST_F(PipelineIntegrationTest,
+       DISABLED_BasicPlayback_MediaSource_VideoOnly_VP9_WebM) {
   MockMediaSource source("bear-vp9.webm", kWebMVP9, 19678);
   StartPipelineWithMediaSource(&source);
   source.EndOfStream();
@@ -939,7 +941,8 @@ TEST_F(PipelineIntegrationTest, BasicPlayback_AudioOnly_Opus_WebM) {
 }
 
 // Verify that VP9 video in WebM containers can be played back.
-TEST_F(PipelineIntegrationTest, BasicPlayback_VideoOnly_VP9_WebM) {
+// TODO(fgalligan): Enable after new vp9 files are landed. crbug.com/180280
+TEST_F(PipelineIntegrationTest, DISABLED_BasicPlayback_VideoOnly_VP9_WebM) {
   ASSERT_TRUE(Start(GetTestDataFilePath("bear-vp9.webm"),
                     PIPELINE_OK));
   Play();
@@ -948,7 +951,8 @@ TEST_F(PipelineIntegrationTest, BasicPlayback_VideoOnly_VP9_WebM) {
 
 // Verify that VP9 video and Opus audio in the same WebM container can be played
 // back.
-TEST_F(PipelineIntegrationTest, BasicPlayback_VP9_Opus_WebM) {
+// TODO(fgalligan): Enable after new vp9 files are landed. crbug.com/180280
+TEST_F(PipelineIntegrationTest, DISABLED_BasicPlayback_VP9_Opus_WebM) {
   ASSERT_TRUE(Start(GetTestDataFilePath("bear-vp9-opus.webm"),
                     PIPELINE_OK));
   Play();
