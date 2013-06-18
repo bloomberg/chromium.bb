bloomberg/chromium.bb

commit e24a75d6777bbf33000e3ca07881caefd61fa526

Reland "Centralize setting a ResourceRequest's cache policy."
This reverts commit 66c335c892cf280323f0ba4a865a3fa068d8b32f.

BUG=none
TEST=none

Review URL: https://chromiumcodereview.appspot.com/14883010

git-svn-id: svn://svn.chromium.org/blink/trunk@151429 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: fbced548646a3a12a8ee2117835f376107d659b8

third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -1028,9 +1028,6 @@ void FrameLoader::loadFrameRequest(const FrameLoadRequest& request, bool lockBac
     else
         loadType = FrameLoadTypeStandard;
 
-    if (loadType == FrameLoadTypeReload || loadType == FrameLoadTypeReloadFromOrigin)
-        resourceRequest.setCachePolicy(ReloadIgnoringCacheData);
-
     loadURL(resourceRequest, request.frameName(), loadType, event, formState.get());
 
     // FIXME: It's possible this targetFrame will not be the same frame that was targeted by the actual
@@ -1113,10 +1110,9 @@ void FrameLoader::load(const FrameLoadRequest& passedRequest)
     const KURL& unreachableURL = request.substituteData().failingURL();
 
     FrameLoadType type;
-    if (shouldTreatURLAsSameAsCurrent(r.url())) {
-        r.setCachePolicy(ReloadIgnoringCacheData);
+    if (shouldTreatURLAsSameAsCurrent(r.url()))
         type = FrameLoadTypeSame;
-    } else if (shouldTreatURLAsSameAsCurrent(unreachableURL) && m_loadType == FrameLoadTypeReload)
+    else if (shouldTreatURLAsSameAsCurrent(unreachableURL) && m_loadType == FrameLoadTypeReload)
         type = FrameLoadTypeReload;
     else
         type = FrameLoadTypeStandard;
@@ -1192,16 +1188,8 @@ void FrameLoader::reload(bool endToEndReload, const KURL& overrideURL, const Str
     else if (!documentLoader->unreachableURL().isEmpty())
         request.setURL(documentLoader->unreachableURL());
 
-    bool isFormSubmission = request.httpMethod() == "POST";
-    if (overrideEncoding.isEmpty())
-        request.setCachePolicy(ReloadIgnoringCacheData);
-    else if (isFormSubmission)
-        request.setCachePolicy(ReturnCacheDataDontLoad);
-    else
-        request.setCachePolicy(ReturnCacheDataElseLoad);
-
     FrameLoadType type = endToEndReload ? FrameLoadTypeReloadFromOrigin : FrameLoadTypeReload;
-    NavigationAction action(request, type, isFormSubmission);
+    NavigationAction action(request, type, request.httpMethod() == "POST");
     loadWithNavigationAction(request, action, type, 0, defaultSubstituteDataForURL(request.url()), overrideEncoding);
 }
 
@@ -1846,30 +1834,6 @@ void FrameLoader::addExtraFieldsToRequest(ResourceRequest& request)
 
     applyUserAgent(request);
 
-    if (!isMainResource) {
-        if (request.isConditional())
-            request.setCachePolicy(ReloadIgnoringCacheData);
-        else if (documentLoader()->isLoadingInAPISense()) {
-            // If we inherit cache policy from a main resource, we use the DocumentLoader's
-            // original request cache policy for two reasons:
-            // 1. For POST requests, we mutate the cache policy for the main resource,
-            //    but we do not want this to apply to subresources
-            // 2. Delegates that modify the cache policy using willSendRequest: should
-            //    not affect any other resources. Such changes need to be done
-            //    per request.
-            ResourceRequestCachePolicy mainDocumentOriginalCachePolicy = documentLoader()->originalRequest().cachePolicy();
-            // Back-forward navigations try to load main resource from cache only to avoid re-submitting form data, and start over (with a warning dialog) if that fails.
-            // This policy is set on initial request too, but should not be inherited.
-            ResourceRequestCachePolicy subresourceCachePolicy = (mainDocumentOriginalCachePolicy == ReturnCacheDataDontLoad) ? ReturnCacheDataElseLoad : mainDocumentOriginalCachePolicy;
-            request.setCachePolicy(subresourceCachePolicy);
-        } else
-            request.setCachePolicy(UseProtocolCachePolicy);
-
-    // FIXME: Other FrameLoader functions have duplicated code for setting cache policy of main request when reloading.
-    // It seems better to manage it explicitly than to hide the logic inside addExtraFieldsToRequest().
-    } else if (m_loadType == FrameLoadTypeReload || m_loadType == FrameLoadTypeReloadFromOrigin || request.isConditional())
-        request.setCachePolicy(ReloadIgnoringCacheData);
-
     if (request.cachePolicy() == ReloadIgnoringCacheData) {
         if (m_loadType == FrameLoadTypeReload)
             request.setHTTPHeaderField("Cache-Control", "max-age=0");
@@ -2387,17 +2351,13 @@ void FrameLoader::loadDifferentDocumentItem(HistoryItem* item)
     RefPtr<FormData> formData = item->formData();
     ResourceRequest request(item->url());
     request.setHTTPReferrer(item->referrer());
-
-    NavigationAction action;
     if (formData) {
         request.setHTTPMethod("POST");
         request.setHTTPBody(formData);
         request.setHTTPContentType(item->formContentType());
         RefPtr<SecurityOrigin> securityOrigin = SecurityOrigin::createFromString(item->referrer());
         addHTTPOriginIfNeeded(request, securityOrigin->toString());
-        request.setCachePolicy(ReturnCacheDataDontLoad);
-    } else
-        request.setCachePolicy(ReturnCacheDataElseLoad);
+    }
 
     loadWithNavigationAction(request, NavigationAction(request, FrameLoadTypeBackForward, false), FrameLoadTypeBackForward, 0, defaultSubstituteDataForURL(request.url()));
 }

third_party/WebKit/LayoutTests/TestExpectations
@@ -588,13 +588,6 @@ crbug.com/9797 crbug.com/9798 [ Win ] http/tests/xmlhttprequest/state-after-netw
 # This test definitely times out (i.e. is not just slow).
 crbug.com/9798 [ Win Release ] http/tests/xmlhttprequest/small-chunks-response-text.html [ Pass Timeout ]
 
-# New failures from WebKit merge 42725:42805
-# WebKit change http://trac.webkit.org/changeset/42785
-# added a test which submits a form and then uses history.back()
-# to go back. in test_shell history.back() does not go back,
-# so the test timeouts.
-crbug.com/11001 http/tests/history/back-to-post.php [ Timeout ]
-
 # Flaky timeouts first appearing at r27764 (webkit 48947)
 crbug.com/9798 [ Win ] http/tests/cookies/multiple-cookies.html [ Pass Timeout ]
 

third_party/WebKit/LayoutTests/platform/chromium-mac/http/tests/history/back-to-post-expected.txt
@@ -1,5 +1,0 @@
-
-Test for rdar://problem/6791439 Getting an error page instead of login page navigating back in gmail.
-
-FAIL: Script did not run
- 

third_party/WebKit/Source/WebKit/chromium/src/FrameLoaderClientImpl.cpp
@@ -288,14 +288,6 @@ void FrameLoaderClientImpl::dispatchWillSendRequest(
     DocumentLoader* loader, unsigned long identifier, ResourceRequest& request,
     const ResourceResponse& redirectResponse)
 {
-    if (loader) {
-        // Avoid repeating a form submission when navigating back or forward.
-        if (loader == loader->frameLoader()->provisionalDocumentLoader()
-            && request.httpMethod() == "POST"
-            && isBackForwardLoadType(loader->frameLoader()->loadType()))
-            request.setCachePolicy(ReturnCacheDataDontLoad);
-    }
-
     // FrameLoader::loadEmptyDocumentSynchronously() creates an empty document
     // with no URL.  We don't like that, so we'll rename it to about:blank.
     if (request.url().isEmpty())

third_party/WebKit/Source/core/loader/cache/CachedResourceLoader.cpp
@@ -476,6 +476,34 @@ void CachedResourceLoader::determineTargetType(ResourceRequest& request, CachedR
     request.setTargetType(targetType);
 }
 
+ResourceRequestCachePolicy CachedResourceLoader::resourceRequestCachePolicy(const ResourceRequest& request, CachedResource::Type type)
+{
+    if (type == CachedResource::MainResource) {
+        FrameLoadType frameLoadType = frame()->loader()->loadType();
+        bool isReload = frameLoadType == FrameLoadTypeReload || frameLoadType == FrameLoadTypeReloadFromOrigin;
+        if (request.httpMethod() == "POST" && (isReload || frameLoadType == FrameLoadTypeBackForward))
+            return ReturnCacheDataDontLoad;
+        if (!m_documentLoader->overrideEncoding().isEmpty() || frameLoadType == FrameLoadTypeBackForward)
+            return ReturnCacheDataElseLoad;
+        if (isReload || frameLoadType == FrameLoadTypeSame || request.isConditional())
+            return ReloadIgnoringCacheData;
+        return UseProtocolCachePolicy;
+    }
+
+    if (request.isConditional())
+        return ReloadIgnoringCacheData;
+
+    if (m_documentLoader->isLoadingInAPISense()) {
+        // For POST requests, we mutate the main resource's cache policy to avoid form resubmission.
+        // This policy should not be inherited by subresources.
+        ResourceRequestCachePolicy mainResourceCachePolicy = m_documentLoader->request().cachePolicy();
+        if (mainResourceCachePolicy == ReturnCacheDataDontLoad)
+            return ReturnCacheDataElseLoad;
+        return mainResourceCachePolicy;
+    }
+    return UseProtocolCachePolicy;
+}
+
 void CachedResourceLoader::addAdditionalRequestHeaders(ResourceRequest& request, CachedResource::Type type)
 {
     if (!frame())
@@ -505,6 +533,8 @@ void CachedResourceLoader::addAdditionalRequestHeaders(ResourceRequest& request,
         FrameLoader::addHTTPOriginIfNeeded(request, outgoingOrigin);
     }
 
+    if (request.cachePolicy() == UseProtocolCachePolicy)
+        request.setCachePolicy(resourceRequestCachePolicy(request, type));
     if (request.targetType() == ResourceRequest::TargetIsUnspecified)
         determineTargetType(request, type);
     frameLoader->addExtraFieldsToRequest(request);

third_party/WebKit/Source/core/loader/cache/CachedResourceLoader.h
@@ -143,6 +143,7 @@ private:
     RevalidationPolicy determineRevalidationPolicy(CachedResource::Type, ResourceRequest&, bool forPreload, CachedResource* existingResource, CachedResourceRequest::DeferOption) const;
 
     void determineTargetType(ResourceRequest&, CachedResource::Type);
+    ResourceRequestCachePolicy resourceRequestCachePolicy(const ResourceRequest&, CachedResource::Type);
     void addAdditionalRequestHeaders(ResourceRequest&, CachedResource::Type);
 
     void notifyLoadedFromMemoryCache(CachedResource*);

commit 0c918eaf4e9218490a64c46efb175b92d90b78a6

Fix reload after back/forward navigation setting the wrong cache policy.
This was regressed in r151429.

BUG=248145
TEST=WebFrameTest.BackToReload

Review URL: https://chromiumcodereview.appspot.com/17220005

git-svn-id: svn://svn.chromium.org/blink/trunk@152592 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: f814cb1ad78556f5ace3e3b96438e3abd5152364

third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -1170,6 +1170,9 @@ void FrameLoader::reload(bool endToEndReload, const KURL& overrideURL, const Str
     frame()->loader()->history()->saveDocumentAndScrollState();
 
     ResourceRequest request = documentLoader->request();
+    // FIXME: We need to reset cache policy to prevent it from being incorrectly propagted to the reload.
+    // Do we need to propagate anything other than the url?
+    request.setCachePolicy(UseProtocolCachePolicy);
     if (!overrideURL.isEmpty())
         request.setURL(overrideURL);
     else if (!documentLoader->unreachableURL().isEmpty())

third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
@@ -3382,5 +3382,32 @@ TEST_F(WebFrameTest, SimulateFragmentAnchorMiddleClick)
     m_webView = 0;
 }
 
+TEST_F(WebFrameTest, BackToReload)
+{
+    registerMockedHttpURLLoad("fragment_middle_click.html");
+    m_webView = FrameTestHelpers::createWebViewAndLoad(m_baseURL + "fragment_middle_click.html", true);
+    WebFrame* frame = m_webView->mainFrame();
+    WebHistoryItem firstItem = frame->currentHistoryItem();
+    EXPECT_FALSE(firstItem.isNull());
+
+    registerMockedHttpURLLoad("white-1x1.png");
+    FrameTestHelpers::loadFrame(frame, m_baseURL + "white-1x1.png");
+    Platform::current()->unitTestSupport()->serveAsynchronousMockedRequests();
+    EXPECT_FALSE(frame->previousHistoryItem().isNull());
+    EXPECT_EQ(firstItem.urlString(), frame->previousHistoryItem().urlString());
+
+    registerMockedHttpURLLoad("fragment_middle_click.html");
+    frame->loadHistoryItem(frame->previousHistoryItem());
+    Platform::current()->unitTestSupport()->serveAsynchronousMockedRequests();
+    EXPECT_EQ(firstItem.urlString(), frame->currentHistoryItem().urlString());
+
+    frame->reload();
+    Platform::current()->unitTestSupport()->serveAsynchronousMockedRequests();
+    EXPECT_EQ(WebURLRequest::ReloadIgnoringCacheData, frame->dataSource()->request().cachePolicy());
+
+    m_webView->close();
+    m_webView = 0;
+}
+
 
 } // namespace