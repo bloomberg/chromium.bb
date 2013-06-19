d54cb8b6694fc61764a0b8df054586f9b621c8cd
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-19 04:05:00 +0000

    Reduce false-positives from media/PRESUBMIT.py in detecting use of the wrong system clock.
    Added exclude regexp to prevent warning for certain reasonable cases: 1) a 'using base::Time' declaration; and 2) references to base::Time::kXXX constants (a bug fix).
    
    BUG=247881
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/17426008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207173 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 49d2c70e4f60aa7943ed8d9cfaeeb67331ed2e81

--- media/PRESUBMIT.py
+++ media/PRESUBMIT.py
@@ -20,21 +20,37 @@ def _CheckForUseOfWrongClock(input_api, output_api):
   # Regular expression that should detect any explicit references to the
   # base::Time type (or base::Clock/DefaultClock), whether in using decls,
   # typedefs, or to call static methods.
-  base_time_type_pattern = r'base::(Time|Clock|DefaultClock)(\W|$)'
+  base_time_type_pattern = r'(^|\W)base::(Time|Clock|DefaultClock)(\W|$)'
 
   # Regular expression that should detect references to the base::Time class
-  # members, such as a call to base::Time::Now.  Exceptions: References to the
-  # kXXX constants are ignored.
-  base_time_member_pattern = r'(^|\W)(Time|Clock|DefaultClock)::[^k]'
+  # members, such as a call to base::Time::Now.
+  base_time_member_pattern = r'(^|\W)(Time|Clock|DefaultClock)::'
+
+  # Regular expression to detect "using base::Time" declarations.  We want to
+  # prevent these from triggerring a warning.  For example, it's perfectly
+  # reasonable for code to be written like this:
+  #
+  #   using base::Time;
+  #   ...
+  #   int64 foo_us = foo_s * Time::kMicrosecondsPerSecond;
+  using_base_time_decl_pattern = r'^\s*using\s+(::)?base::Time\s*;'
+
+  # Regular expression to detect references to the kXXX constants in the
+  # base::Time class.  We want to prevent these from triggerring a warning.
+  base_time_konstant_pattern = r'(^|\W)Time::k\w+'
 
   problem_re = input_api.re.compile(
       r'(' + base_time_type_pattern + r')|(' + base_time_member_pattern + r')')
+  exception_re = input_api.re.compile(
+      r'(' + using_base_time_decl_pattern + r')|(' +
+      base_time_konstant_pattern + r')')
   problems = []
   for f in input_api.AffectedSourceFiles(FilterFile):
     for line_number, line in f.ChangedContents():
       if problem_re.search(line):
-        problems.append(
-            '  %s:%d\n    %s' % (f.LocalPath(), line_number, line.strip()))
+        if not exception_re.search(line):
+          problems.append(
+              '  %s:%d\n    %s' % (f.LocalPath(), line_number, line.strip()))
 
   if problems:
     return [output_api.PresubmitPromptOrNotify(
