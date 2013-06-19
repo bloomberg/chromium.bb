3b37d342ab8c7276704d9d5c9148e24437838a96
cmp@chromium.org <cmp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-19 19:14:25 +0000

    Possible workaround to lower bot load.
    TBR=iannucci@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/17274009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@207253 0039d316-1c4b-4281-b951-d872f2087c98

--- gclient.py
+++ gclient.py
@@ -1695,6 +1695,10 @@ def Parser():
     jobs = 1
   else:
     jobs = max(8, gclient_utils.NumLocalCpus())
+  # cmp: 2013/06/19
+  # Temporary workaround to lower bot-load on SVN server.
+  if os.environ.get('CHROME_HEADLESS') == '1':
+    jobs = 1
   gclientfile_default = os.environ.get('GCLIENT_FILE', '.gclient')
   parser.add_option('-j', '--jobs', default=jobs, type='int',
                     help='Specify how many SCM commands can run in parallel; '
