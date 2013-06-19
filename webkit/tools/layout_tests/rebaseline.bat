8e869a253ad420a4af3872c3d1c5cdba31924eb6
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-19 06:08:42 +0000

    Remove obsolete rebaseline, test-webkitpy wrappers, stop passing --chromium
    R=ojan@chromium.org, abarth@chromium.org
    BUG=245504
    
    Review URL: https://chromiumcodereview.appspot.com/17239011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207190 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d97b7cdc2018d4a7997ee299ade8350a59ce71c8

--- webkit/tools/layout_tests/rebaseline.bat
+++
@@ -1,1 +0,0 @@
-%~dp0..\..\..\third_party\python_24\python.exe %~dp0\..\..\..\third_party\WebKit\Tools\Scripts\rebaseline-chromium-webkit-tests %*
--- webkit/tools/layout_tests/run_webkit_tests.py
+++ webkit/tools/layout_tests/run_webkit_tests.py
@@ -10,15 +10,11 @@ import subprocess
 import sys
 
 def main():
-    cmd = [sys.executable]
     src_dir = os.path.abspath(os.path.join(sys.path[0], '..', '..', '..'))
     script_dir=os.path.join(src_dir, "third_party", "WebKit", "Tools",
                             "Scripts")
     script = os.path.join(script_dir, 'run-webkit-tests')
-    cmd.append(script)
-    if '--chromium' not in sys.argv:
-        cmd.append('--chromium')
-    cmd.extend(sys.argv[1:])
+    cmd = [sys.executable, script] + sys.argv[1:]
     return subprocess.call(cmd)
 
 if __name__ == '__main__':
