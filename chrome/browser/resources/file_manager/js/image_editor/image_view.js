c05fb8b223f0e269391e27a9d80d666ca46dc2e6
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 06:42:30 +0000

    Fix playing video in Files.app's gallery.
    When loading more than one videos at once, only audio was played, without video. This was caused by not inserting the video tag to the dom structure. This patch resolves this issue.
    Along the way, the video playback starts automatically when switching to a video, without need to click 'Play' every time.
    
    TEST=Follow the bug's repro steps.
    BUG=177371
    R=hirono@chromium.org
    
    Review URL: https://codereview.chromium.org/17099011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207337 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c577b2cde316c01c422fbfdfb23cbdd8e20e2ac4

--- chrome/browser/resources/file_manager/js/image_editor/image_view.js
+++ chrome/browser/resources/file_manager/js/image_editor/image_view.js
@@ -429,9 +429,8 @@ ImageView.prototype.load = function(url, metadata, effect,
   }
 
   function displayMainImage(loadType, previewShown, content, opt_error) {
-    if (opt_error) {
+    if (opt_error)
       loadType = ImageView.LOAD_TYPE_ERROR;
-    }
 
     // If we already displayed the preview we should not replace the content if:
     //   1. The full content failed to load.
@@ -550,6 +549,7 @@ ImageView.prototype.replaceContent_ = function(
     this.videoElement_ = content;
     this.screenImage_ = content;
     this.screenImage_.className = 'image';
+    this.videoElement_.play();
     return;
   }
 
@@ -638,6 +638,7 @@ ImageView.prototype.replace = function(
   if (!opt_effect) {
     if (oldScreenImage)
       oldScreenImage.parentNode.removeChild(oldScreenImage);
+    this.container_.appendChild(content);
     return;
   }
 
