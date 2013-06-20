56ca4ebec9967b7568132cb18c4253fbd3d375c6
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 18:35:43 +0000

    Adding network interface info to diagnostic WebRTC log.
    Review URL: https://chromiumcodereview.appspot.com/17121006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207497 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3fc84827e9b332b799b09e40650a5af041d0c663

--- chrome/browser/media/webrtc_logging_handler_host.cc
+++ chrome/browser/media/webrtc_logging_handler_host.cc
@@ -4,6 +4,8 @@
 
 #include "chrome/browser/media/webrtc_logging_handler_host.h"
 
+#include <string>
+
 #include "base/bind.h"
 #include "base/cpu.h"
 #include "base/logging.h"
@@ -22,6 +24,8 @@
 #include "content/public/browser/render_process_host.h"
 #include "gpu/config/gpu_info.h"
 #include "gpu/config/gpu_info_collector.h"
+#include "net/base/address_family.h"
+#include "net/base/net_util.h"
 #include "net/url_request/url_request_context_getter.h"
 
 #if defined(OS_LINUX)
@@ -41,6 +45,47 @@ const size_t kWebRtcLogSize = 1 * 1024 * 1024;  // 1 MB
 #else
 const size_t kWebRtcLogSize = 6 * 1024 * 1024;  // 6 MB
 #endif
+
+namespace {
+
+// For privacy reasons when logging IP addresses. The returned "sensitive
+// string" is for release builds a string with the end stripped away. Last
+// octet for IPv4 and last 80 bits (5 groups) for IPv6. String will be
+// "1.2.3.x" and "1.2.3::" respectively. For debug builds, the string is
+// not stripped.
+std::string IPAddressToSensitiveString(const net::IPAddressNumber& address) {
+#if defined(NDEBUG)
+  std::string sensitive_address;
+  switch (net::GetAddressFamily(address)) {
+    case net::ADDRESS_FAMILY_IPV4: {
+      sensitive_address = net::IPAddressToString(address);
+      size_t find_pos = sensitive_address.rfind('.');
+      if (find_pos == std::string::npos)
+        return std::string();
+      sensitive_address.resize(find_pos);
+      sensitive_address += ".x";
+      break;
+    }
+    case net::ADDRESS_FAMILY_IPV6: {
+      // TODO(grunell): Create a string of format "1:2:3:x:x:x:x:x" to clarify
+      // that the end has been stripped out.
+      net::IPAddressNumber sensitive_address_number = address;
+      sensitive_address_number.resize(net::kIPv6AddressSize - 10);
+      sensitive_address_number.resize(net::kIPv6AddressSize, 0);
+      sensitive_address = net::IPAddressToString(sensitive_address_number);
+      break;
+    }
+    case net::ADDRESS_FAMILY_UNSPECIFIED: {
+      break;
+    }
+  }
+  return sensitive_address;
+#else
+  return net::IPAddressToString(address);
+#endif
+}
+
+}  // namespace
 
 WebRtcLoggingHandlerHost::WebRtcLoggingHandlerHost() {}
 
@@ -160,6 +205,7 @@ void WebRtcLoggingHandlerHost::LogMachineInfo() {
   pcb.Write(info.c_str(), info.length());
   std::string cpu_brand = cpu.cpu_brand();
   // Workaround for crbug.com/249713.
+  // TODO(grunell): Remove workaround when bug is fixed.
   size_t null_pos = cpu_brand.find('\0');
   if (null_pos != std::string::npos)
     cpu_brand.erase(null_pos);
@@ -183,6 +229,19 @@ void WebRtcLoggingHandlerHost::LogMachineInfo() {
          ", driver-vendor='" + gpu_info.driver_vendor +
          "', driver-version=" + gpu_info.driver_version + '\n';
   pcb.Write(info.c_str(), info.length());
+
+  // Network interfaces
+  net::NetworkInterfaceList network_list;
+  net::GetNetworkList(&network_list);
+  info  = "Discovered " + IntToString(network_list.size()) +
+          " network interfaces:" + '\n';
+  pcb.Write(info.c_str(), info.length());
+  for (net::NetworkInterfaceList::iterator it = network_list.begin();
+       it != network_list.end(); ++it) {
+    info = "Name: " + it->name +
+           ", Address: " + IPAddressToSensitiveString(it->address) + '\n';
+    pcb.Write(info.c_str(), info.length());
+  }
 
   BrowserThread::PostTask(BrowserThread::IO, FROM_HERE, base::Bind(
       &WebRtcLoggingHandlerHost::NotifyLogOpened, this));
