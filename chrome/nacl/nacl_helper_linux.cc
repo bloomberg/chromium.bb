eb5ee2d02900675c4a72001917e3cd44e25caaf2
bsy@google.com <bsy@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 08:11:37 +0000

    Init NaClChromeMainArgs number_of_cores member so apps can size threadpools
    The outer sandbox on Linux and OSX was preventing
    sysconf(_SC_NPROCESSORS_ONLN) from succeeding, so that NaCl
    applications that need the number of processors/cores to, e.g., figure
    out the appropriate size for threadpools were getting a bogus value
    (-1 when using newlib and 1 when using glibc).
    
    This is a reland of r204389
    (https://chromiumcodereview.appspot.com/16047003) which was reverted
    because of test failures on Win7 and Aura build bots -- even though
    the test passed on a desktop Win7 machine. Consultation with bradn and
    ncbray still indicate bot issues, but who knows?
    
    TEST= browser_tests --gtest_filter=*Sysconf*
    BUG= 176522
    
    Review URL: https://chromiumcodereview.appspot.com/16761002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207350 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ccf4747ae4366a25f45452524acafae2e6b16739

--- chrome/nacl/nacl_helper_linux.cc
+++ chrome/nacl/nacl_helper_linux.cc
@@ -40,7 +40,8 @@ namespace {
 // side of the fork. See zygote_main_linux.cc:HandleForkRequest from
 //   if (!child) {
 void BecomeNaClLoader(const std::vector<int>& child_fds,
-                      size_t prereserved_sandbox_size) {
+                      size_t prereserved_sandbox_size,
+                      int number_of_cores) {
   VLOG(1) << "NaCl loader: setting up IPC descriptor";
   // don't need zygote FD any more
   if (HANDLE_EINTR(close(kNaClZygoteDescriptor)) != 0)
@@ -56,6 +57,7 @@ void BecomeNaClLoader(const std::vector<int>& child_fds,
   base::MessageLoopForIO main_message_loop;
   NaClListener listener;
   listener.set_prereserved_sandbox_size(prereserved_sandbox_size);
+  listener.set_number_of_cores(number_of_cores);
   listener.Listen();
   _exit(0);
 }
@@ -63,7 +65,8 @@ void BecomeNaClLoader(const std::vector<int>& child_fds,
 // Some of this code was lifted from
 // content/browser/zygote_main_linux.cc:ForkWithRealPid()
 void HandleForkRequest(const std::vector<int>& child_fds,
-                       size_t prereserved_sandbox_size) {
+                       size_t prereserved_sandbox_size,
+                       int number_of_cores) {
   VLOG(1) << "nacl_helper: forking";
   pid_t childpid = fork();
   if (childpid < 0) {
@@ -101,7 +104,7 @@ void HandleForkRequest(const std::vector<int>& child_fds,
     if (HANDLE_EINTR(close(child_fds[kNaClParentFDIndex])) != 0)
       LOG(ERROR) << "close(child_fds[kNaClParentFDIndex]) failed";
     if (validack) {
-      BecomeNaClLoader(child_fds, prereserved_sandbox_size);
+      BecomeNaClLoader(child_fds, prereserved_sandbox_size, number_of_cores);
     } else {
       LOG(ERROR) << "Failed to synch with zygote";
     }
@@ -239,6 +242,7 @@ int main(int argc, char* argv[]) {
 #endif
   std::vector<int> empty; // for SendMsg() calls
   size_t prereserved_sandbox_size = CheckReservedAtZero();
+  int number_of_cores = sysconf(_SC_NPROCESSORS_ONLN);
 
   CheckRDebug(argv[0]);
 
@@ -275,7 +279,7 @@ int main(int argc, char* argv[]) {
     } else if (msglen == sizeof(kNaClForkRequest) - 1 &&
                memcmp(buf, kNaClForkRequest, msglen) == 0) {
       if (kNaClParentFDIndex + 1 == fds.size()) {
-        HandleForkRequest(fds, prereserved_sandbox_size);
+        HandleForkRequest(fds, prereserved_sandbox_size, number_of_cores);
         continue;  // fork succeeded. Note: child does not return
       } else {
         LOG(ERROR) << "nacl_helper: unexpected number of fds, got "
