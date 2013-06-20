06de8efdea19f25d12a29e58765ed3029feee8d7
rmsousa@chromium.org <rmsousa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 21:15:13 +0000

    Revert 207511 "ash: Add flags for specifying default wallpaper."
    Drover forgot a file somehow. sigh.
    
    > ash: Add flags for specifying default wallpaper.
    > 
    > This adds new command-line flags for specifying image files
    > that will be used as default wallpaper:
    > 
    >   --ash-default-guest-wallpaper-large
    >   --ash-default-guest-wallpaper-small
    >   --ash-default-wallpaper-large
    >   --ash-default-wallpaper-small
    > 
    > This makes it easier for Chrome OS to have per-device custom
    > wallpaper. (We previously loaded the default wallpaper from
    > a resource file, but that prevents the same build of Chrome
    > from being used on different devices.)
    > 
    > BUG=248764
    > 
    > Review URL: https://chromiumcodereview.appspot.com/16982006
    
    TBR=derat@chromium.org
    
    Review URL: https://codereview.chromium.org/17191005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207566 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 955cf1fc7b1d9bdb0e8173e518dbb959e01ea7d5

--- ash/desktop_background/wallpaper_resizer.cc
+++ ash/desktop_background/wallpaper_resizer.cc
@@ -18,95 +18,91 @@ using content::BrowserThread;
 namespace ash {
 namespace {
 
+// Callback used to indicate that wallpaper has been resized.
+typedef base::Callback<void(const SkBitmap&)> ResizedCallback;
+
 // For our scaling ratios we need to round positive numbers.
 int RoundPositive(double x) {
   return static_cast<int>(floor(x + 0.5));
 }
 
-// Resizes |orig_bitmap| to |target_size| using |layout| and stores the
-// resulting bitmap at |resized_bitmap_out|.
-void Resize(SkBitmap orig_bitmap,
-            const gfx::Size& target_size,
+// Resizes |wallpaper| to |target_size| and calls the callback.
+void Resize(const SkBitmap& wallpaper,
             WallpaperLayout layout,
-            SkBitmap* resized_bitmap_out) {
-  DCHECK(BrowserThread::GetBlockingPool()->RunsTasksOnCurrentThread());
-  SkBitmap new_bitmap = orig_bitmap;
-
-  const int orig_width = orig_bitmap.width();
-  const int orig_height = orig_bitmap.height();
-  const int new_width = target_size.width();
-  const int new_height = target_size.height();
-
-  if (orig_width > new_width || orig_height > new_height) {
-    gfx::Rect wallpaper_rect(0, 0, orig_width, orig_height);
-    gfx::Size cropped_size = gfx::Size(std::min(new_width, orig_width),
-                                       std::min(new_height, orig_height));
+            const gfx::Size& target_size,
+            base::MessageLoop* origin_loop,
+            const ResizedCallback& callback) {
+  SkBitmap resized_wallpaper = wallpaper;
+  int width = target_size.width();
+  int height = target_size.height();
+  if (wallpaper.width() > width || wallpaper.height() > height) {
+    gfx::Rect wallpaper_rect(0, 0, wallpaper.width(), wallpaper.height());
+    gfx::Size cropped_size = gfx::Size(std::min(width, wallpaper.width()),
+                                       std::min(height, wallpaper.height()));
     switch (layout) {
       case WALLPAPER_LAYOUT_CENTER:
         wallpaper_rect.ClampToCenteredSize(cropped_size);
-        orig_bitmap.extractSubset(&new_bitmap,
-                                  gfx::RectToSkIRect(wallpaper_rect));
+        wallpaper.extractSubset(&resized_wallpaper,
+                                gfx::RectToSkIRect(wallpaper_rect));
         break;
       case WALLPAPER_LAYOUT_TILE:
         wallpaper_rect.set_size(cropped_size);
-        orig_bitmap.extractSubset(&new_bitmap,
-                                  gfx::RectToSkIRect(wallpaper_rect));
+        wallpaper.extractSubset(&resized_wallpaper,
+                                gfx::RectToSkIRect(wallpaper_rect));
         break;
       case WALLPAPER_LAYOUT_STRETCH:
-        new_bitmap = skia::ImageOperations::Resize(
-            orig_bitmap, skia::ImageOperations::RESIZE_LANCZOS3,
-            new_width, new_height);
+        resized_wallpaper = skia::ImageOperations::Resize(
+            wallpaper, skia::ImageOperations::RESIZE_LANCZOS3,
+            width, height);
         break;
       case WALLPAPER_LAYOUT_CENTER_CROPPED:
-        if (orig_width > new_width && orig_height > new_height) {
+        if (wallpaper.width() > width && wallpaper.height() > height) {
           // The dimension with the smallest ratio must be cropped, the other
           // one is preserved. Both are set in gfx::Size cropped_size.
-          double horizontal_ratio = static_cast<double>(new_width) /
-              static_cast<double>(orig_width);
-          double vertical_ratio = static_cast<double>(new_height) /
-              static_cast<double>(orig_height);
+          double horizontal_ratio = static_cast<double>(width) /
+              static_cast<double>(wallpaper.width());
+          double vertical_ratio = static_cast<double>(height) /
+              static_cast<double>(wallpaper.height());
 
           if (vertical_ratio > horizontal_ratio) {
             cropped_size = gfx::Size(
-                RoundPositive(static_cast<double>(new_width) / vertical_ratio),
-                orig_height);
+                RoundPositive(static_cast<double>(width) / vertical_ratio),
+                wallpaper.height());
           } else {
-            cropped_size = gfx::Size(orig_width, RoundPositive(
-                static_cast<double>(new_height) / horizontal_ratio));
+            cropped_size = gfx::Size(wallpaper.width(),
+                RoundPositive(static_cast<double>(height) / horizontal_ratio));
           }
           wallpaper_rect.ClampToCenteredSize(cropped_size);
           SkBitmap sub_image;
-          orig_bitmap.extractSubset(&sub_image,
-                                    gfx::RectToSkIRect(wallpaper_rect));
-          new_bitmap = skia::ImageOperations::Resize(
+          wallpaper.extractSubset(&sub_image,
+                                  gfx::RectToSkIRect(wallpaper_rect));
+          resized_wallpaper = skia::ImageOperations::Resize(
               sub_image, skia::ImageOperations::RESIZE_LANCZOS3,
-              new_width, new_height);
+              width, height);
         }
     }
   }
-
-  *resized_bitmap_out = new_bitmap;
-  resized_bitmap_out->setImmutable();
+  resized_wallpaper.setImmutable();
+  origin_loop->PostTask(FROM_HERE, base::Bind(callback, resized_wallpaper));
 }
 
 }  // namespace
 
-WallpaperResizer::WallpaperResizer(int image_resource_id,
-                                   const gfx::Size& target_size,
-                                   WallpaperLayout layout)
-    : wallpaper_image_(*(ui::ResourceBundle::GetSharedInstance().
-          GetImageNamed(image_resource_id).ToImageSkia())),
+WallpaperResizer::WallpaperResizer(const WallpaperInfo& info,
+                                   const gfx::Size& target_size)
+    : wallpaper_info_(info),
       target_size_(target_size),
-      layout_(layout),
+      wallpaper_image_(*(ui::ResourceBundle::GetSharedInstance().
+          GetImageNamed(info.idr).ToImageSkia())),
       weak_ptr_factory_(this) {
 }
 
-WallpaperResizer::WallpaperResizer(const gfx::ImageSkia& image,
+WallpaperResizer::WallpaperResizer(const WallpaperInfo& info,
                                    const gfx::Size& target_size,
-                                   WallpaperLayout layout)
-    : wallpaper_image_(image),
+                                   const gfx::ImageSkia& image)
+    : wallpaper_info_(info),
       target_size_(target_size),
-      layout_(layout),
+      wallpaper_image_(image),
       weak_ptr_factory_(this) {
 }
 
@@ -114,17 +110,18 @@ WallpaperResizer::~WallpaperResizer() {
 }
 
 void WallpaperResizer::StartResize() {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  SkBitmap* resized_bitmap = new SkBitmap;
-  if (!content::BrowserThread::PostBlockingPoolTaskAndReply(
+  if (!BrowserThread::GetBlockingPool()->PostWorkerTaskWithShutdownBehavior(
           FROM_HERE,
-          base::Bind(&Resize, *wallpaper_image_.bitmap(), target_size_,
-                     layout_, resized_bitmap),
-          base::Bind(&WallpaperResizer::OnResizeFinished,
-                     weak_ptr_factory_.GetWeakPtr(),
-                     base::Owned(resized_bitmap)))) {
-    LOG(WARNING) << "PostSequencedWorkerTask failed. "
-                 << "Wallpaper may not be resized.";
+          base::Bind(&Resize,
+                     *wallpaper_image_.bitmap(),
+                     wallpaper_info_.layout,
+                     target_size_,
+                     base::MessageLoop::current(),
+                     base::Bind(&WallpaperResizer::OnResizeFinished,
+                                weak_ptr_factory_.GetWeakPtr())),
+          base::SequencedWorkerPool::CONTINUE_ON_SHUTDOWN)) {
+    LOG(WARNING) << "PostSequencedWorkerTask failed. " <<
+                    "Wallpaper may not be resized.";
   }
 }
 
@@ -136,9 +133,8 @@ void WallpaperResizer::RemoveObserver(WallpaperResizerObserver* observer) {
   observers_.RemoveObserver(observer);
 }
 
-void WallpaperResizer::OnResizeFinished(SkBitmap* resized_bitmap) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  wallpaper_image_ = gfx::ImageSkia::CreateFrom1xBitmap(*resized_bitmap);
+void WallpaperResizer::OnResizeFinished(const SkBitmap& resized_wallpaper) {
+  wallpaper_image_ = gfx::ImageSkia::CreateFrom1xBitmap(resized_wallpaper);
   FOR_EACH_OBSERVER(WallpaperResizerObserver, observers_,
                     OnWallpaperResized());
 }
