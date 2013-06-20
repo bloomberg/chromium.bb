c035f305c9f1511867279c2c7d8ab4d93df27a62
hartmanng@chromium.org <hartmanng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 19:49:53 +0000

    Migrate CompositingReasons to uint64.
    We've already used up the 32-bit enum, and planning to continue adding more 
    compositing reasons, so we need to expand to 64-bit values.
    
    Blink side is here: https://codereview.chromium.org/16994010/.
    
    BUG=240946
    
    Review URL: https://chromiumcodereview.appspot.com/17138004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207515 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d096e537605c143477745e9b4321f9cf1fa420d9

--- cc/layers/compositing_reasons.h
+++ cc/layers/compositing_reasons.h
@@ -5,46 +5,55 @@
 #ifndef CC_LAYERS_COMPOSITING_REASONS_H_
 #define CC_LAYERS_COMPOSITING_REASONS_H_
 
+#include "base/port.h"
+
 namespace cc {
 
 // This is a clone of CompositingReasons and WebCompositingReasons from Blink.
-enum {
-  kCompositingReasonUnknown                                = 0,
-  kCompositingReason3DTransform                            = 1 << 0,
-  kCompositingReasonVideo                                  = 1 << 1,
-  kCompositingReasonCanvas                                 = 1 << 2,
-  kCompositingReasonPlugin                                 = 1 << 3,
-  kCompositingReasonIFrame                                 = 1 << 4,
-  kCompositingReasonBackfaceVisibilityHidden               = 1 << 5,
-  kCompositingReasonAnimation                              = 1 << 6,
-  kCompositingReasonFilters                                = 1 << 7,
-  kCompositingReasonPositionFixed                          = 1 << 8,
-  kCompositingReasonPositionSticky                         = 1 << 9,
-  kCompositingReasonOverflowScrollingTouch                 = 1 << 10,
-  kCompositingReasonBlending                               = 1 << 11,
-  kCompositingReasonAssumedOverlap                         = 1 << 12,
-  kCompositingReasonOverlap                                = 1 << 13,
-  kCompositingReasonNegativeZIndexChildren                 = 1 << 14,
-  kCompositingReasonTransformWithCompositedDescendants     = 1 << 15,
-  kCompositingReasonOpacityWithCompositedDescendants       = 1 << 16,
-  kCompositingReasonMaskWithCompositedDescendants          = 1 << 17,
-  kCompositingReasonReflectionWithCompositedDescendants    = 1 << 18,
-  kCompositingReasonFilterWithCompositedDescendants        = 1 << 19,
-  kCompositingReasonBlendingWithCompositedDescendants      = 1 << 20,
-  kCompositingReasonClipsCompositingDescendants            = 1 << 21,
-  kCompositingReasonPerspective                            = 1 << 22,
-  kCompositingReasonPreserve3D                             = 1 << 23,
-  kCompositingReasonReflectionOfCompositedParent           = 1 << 24,
-  kCompositingReasonRoot                                   = 1 << 25,
-  kCompositingReasonLayerForClip                           = 1 << 26,
-  kCompositingReasonLayerForScrollbar                      = 1 << 27,
-  kCompositingReasonLayerForScrollingContainer             = 1 << 28,
-  kCompositingReasonLayerForForeground                     = 1 << 29,
-  kCompositingReasonLayerForBackground                     = 1 << 30,
-  kCompositingReasonLayerForMask                           = 1 << 31,
-};
+const uint64 kCompositingReasonUnknown = 0;
+const uint64 kCompositingReason3DTransform = GG_UINT64_C(1) << 0;
+const uint64 kCompositingReasonVideo = GG_UINT64_C(1) << 1;
+const uint64 kCompositingReasonCanvas = GG_UINT64_C(1) << 2;
+const uint64 kCompositingReasonPlugin = GG_UINT64_C(1) << 3;
+const uint64 kCompositingReasonIFrame = GG_UINT64_C(1) << 4;
+const uint64 kCompositingReasonBackfaceVisibilityHidden = GG_UINT64_C(1) << 5;
+const uint64 kCompositingReasonAnimation = GG_UINT64_C(1) << 6;
+const uint64 kCompositingReasonFilters = GG_UINT64_C(1) << 7;
+const uint64 kCompositingReasonPositionFixed = GG_UINT64_C(1) << 8;
+const uint64 kCompositingReasonPositionSticky = GG_UINT64_C(1) << 9;
+const uint64 kCompositingReasonOverflowScrollingTouch = GG_UINT64_C(1) << 10;
+const uint64 kCompositingReasonBlending = GG_UINT64_C(1) << 11;
+const uint64 kCompositingReasonAssumedOverlap = GG_UINT64_C(1) << 12;
+const uint64 kCompositingReasonOverlap = GG_UINT64_C(1) << 13;
+const uint64 kCompositingReasonNegativeZIndexChildren = GG_UINT64_C(1) << 14;
+const uint64 kCompositingReasonTransformWithCompositedDescendants =
+    GG_UINT64_C(1) << 15;
+const uint64 kCompositingReasonOpacityWithCompositedDescendants =
+    GG_UINT64_C(1) << 16;
+const uint64 kCompositingReasonMaskWithCompositedDescendants =
+    GG_UINT64_C(1) << 17;
+const uint64 kCompositingReasonReflectionWithCompositedDescendants =
+    GG_UINT64_C(1) << 18;
+const uint64 kCompositingReasonFilterWithCompositedDescendants =
+    GG_UINT64_C(1) << 19;
+const uint64 kCompositingReasonBlendingWithCompositedDescendants =
+    GG_UINT64_C(1) << 20;
+const uint64 kCompositingReasonClipsCompositingDescendants =
+    GG_UINT64_C(1) << 21;
+const uint64 kCompositingReasonPerspective = GG_UINT64_C(1) << 22;
+const uint64 kCompositingReasonPreserve3D = GG_UINT64_C(1) << 23;
+const uint64 kCompositingReasonReflectionOfCompositedParent =
+    GG_UINT64_C(1) << 24;
+const uint64 kCompositingReasonRoot = GG_UINT64_C(1) << 25;
+const uint64 kCompositingReasonLayerForClip = GG_UINT64_C(1) << 26;
+const uint64 kCompositingReasonLayerForScrollbar = GG_UINT64_C(1) << 27;
+const uint64 kCompositingReasonLayerForScrollingContainer =
+    GG_UINT64_C(1) << 28;
+const uint64 kCompositingReasonLayerForForeground = GG_UINT64_C(1) << 29;
+const uint64 kCompositingReasonLayerForBackground = GG_UINT64_C(1) << 30;
+const uint64 kCompositingReasonLayerForMask = GG_UINT64_C(1) << 31;
 
-typedef uint32 CompositingReasons;
+typedef uint64 CompositingReasons;
 
 }  // namespace cc
 
--- webkit/renderer/compositor_bindings/web_compositing_reasons.cc
+++ webkit/renderer/compositor_bindings/web_compositing_reasons.cc
@@ -3,142 +3,144 @@
 // found in the LICENSE file.
 
 #include "base/basictypes.h"
+#include "base/port.h"
 #include "cc/layers/compositing_reasons.h"
 #include "third_party/WebKit/public/platform/WebCompositingReasons.h"
 
-#define COMPILE_ASSERT_MATCHING_ENUMS(cc_name, webkit_name)             \
-    COMPILE_ASSERT(                                                     \
-        static_cast<int>(cc_name) == static_cast<int>(webkit_name),     \
-        mismatching_enums)
+#define COMPILE_ASSERT_MATCHING_UINT64(cc_name, webkit_name)                  \
+    COMPILE_ASSERT(                                                           \
+        static_cast<uint64>(cc_name) ==                                       \
+            (static_cast<uint64>(webkit_name) & GG_UINT64_C(0xffffffff)),     \
+        mismatching_uint64)
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonUnknown,
     WebKit::CompositingReasonUnknown);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReason3DTransform,
     WebKit::CompositingReason3DTransform);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonVideo,
     WebKit::CompositingReasonVideo);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonCanvas,
     WebKit::CompositingReasonCanvas);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonPlugin,
     WebKit::CompositingReasonPlugin);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonIFrame,
     WebKit::CompositingReasonIFrame);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonBackfaceVisibilityHidden,
     WebKit::CompositingReasonBackfaceVisibilityHidden);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonAnimation,
     WebKit::CompositingReasonAnimation);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonFilters,
     WebKit::CompositingReasonFilters);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonPositionFixed,
     WebKit::CompositingReasonPositionFixed);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonPositionSticky,
     WebKit::CompositingReasonPositionSticky);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonOverflowScrollingTouch,
     WebKit::CompositingReasonOverflowScrollingTouch);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonBlending,
     WebKit::CompositingReasonBlending);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonAssumedOverlap,
     WebKit::CompositingReasonAssumedOverlap);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonOverlap,
     WebKit::CompositingReasonOverlap);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonNegativeZIndexChildren,
     WebKit::CompositingReasonNegativeZIndexChildren);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonTransformWithCompositedDescendants,
     WebKit::CompositingReasonTransformWithCompositedDescendants);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonOpacityWithCompositedDescendants,
     WebKit::CompositingReasonOpacityWithCompositedDescendants);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonMaskWithCompositedDescendants,
     WebKit::CompositingReasonMaskWithCompositedDescendants);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonReflectionWithCompositedDescendants,
     WebKit::CompositingReasonReflectionWithCompositedDescendants);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonFilterWithCompositedDescendants,
     WebKit::CompositingReasonFilterWithCompositedDescendants);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonBlendingWithCompositedDescendants,
     WebKit::CompositingReasonBlendingWithCompositedDescendants);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonClipsCompositingDescendants,
     WebKit::CompositingReasonClipsCompositingDescendants);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonPerspective,
     WebKit::CompositingReasonPerspective);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonPreserve3D,
     WebKit::CompositingReasonPreserve3D);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonReflectionOfCompositedParent,
     WebKit::CompositingReasonReflectionOfCompositedParent);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonRoot,
     WebKit::CompositingReasonRoot);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonLayerForClip,
     WebKit::CompositingReasonLayerForClip);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonLayerForScrollbar,
     WebKit::CompositingReasonLayerForScrollbar);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonLayerForScrollingContainer,
     WebKit::CompositingReasonLayerForScrollingContainer);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonLayerForForeground,
     WebKit::CompositingReasonLayerForForeground);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonLayerForBackground,
     WebKit::CompositingReasonLayerForBackground);
 
-COMPILE_ASSERT_MATCHING_ENUMS(
+COMPILE_ASSERT_MATCHING_UINT64(
     cc::kCompositingReasonLayerForMask,
     WebKit::CompositingReasonLayerForMask);
