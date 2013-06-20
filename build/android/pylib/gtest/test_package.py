0e434f2b39cb441a54eed98dc2146ffcee50a7ff
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 10:17:32 +0000

    [Android] Fix _WatchTestOutput after r207099
    BUG=
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/17288008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207387 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6edd61cce39b3778765af46627ef235551370077

--- build/android/pylib/gtest/test_package.py
+++ build/android/pylib/gtest/test_package.py
@@ -179,13 +179,15 @@ class TestPackage(object):
                                              self.device)
       if full_test_name:
         results.AddResult(base_test_result.BaseTestResult(
-            full_test_name, base_test_result.ResultType.CRASH, log=log))
+            full_test_name, base_test_result.ResultType.CRASH,
+            log=p.before.replace('\r', '')))
     except pexpect.TIMEOUT:
       logging.error('Test terminated after %d second timeout.',
                     self.timeout)
       if full_test_name:
         results.AddResult(base_test_result.BaseTestResult(
-            full_test_name, base_test_result.ResultType.TIMEOUT, log=log))
+            full_test_name, base_test_result.ResultType.TIMEOUT,
+            log=p.before.replace('\r', '')))
     finally:
       p.close()
 
