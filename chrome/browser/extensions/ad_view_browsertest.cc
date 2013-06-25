3c219996c9cbfdd9b05da4655bd8c8170e6e52a2
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-25 04:23:00 +0000

    Disable AdviewTest.ChangeAdNetworkValue browsertest (flaky on Mac10.8).
    TBR=rpaquay@chromium.org
    BUG=253644
    
    Review URL: https://chromiumcodereview.appspot.com/17600004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208402 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 87c4336a39c090a36314ab0eeb9cb037a185dd1b

--- chrome/browser/extensions/ad_view_browsertest.cc
+++ chrome/browser/extensions/ad_view_browsertest.cc
@@ -90,6 +90,12 @@ IN_PROC_BROWSER_TEST_F(AdViewTest, AdViewPermissionIsRequired) {
 // This test checks that 1) it is possible change the value of the "ad-network"
 // attribute of an <adview> element and 2) changing the value will reset the
 // "src" attribute.
+#if defined(OS_MACOSX)
+// Very flaky on MacOS 10.8 - crbug.com/253644.
+#define MAYBE_ChangeAdNetworkValue DISABLED_ChangeAdNetworkValue
+#else
+#define MAYBE_ChangeAdNetworkValue ChangeAdNetworkValue
+#endif
 IN_PROC_BROWSER_TEST_F(AdViewTest, ChangeAdNetworkValue) {
   ASSERT_TRUE(StartTestServer());
 
