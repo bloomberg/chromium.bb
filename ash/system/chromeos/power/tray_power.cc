4044927fb286861b22cd4a6b436bf25ea77b78a1
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-25 04:10:30 +0000

    Don't show low battery notification bubble if battery state is "Neither Charging or Discharging". This is issue is a regression of crrev.com/203952.
    BUG=252026
    
    Review URL: https://chromiumcodereview.appspot.com/17569020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208392 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2a090cedc85e6ddc989ce51f6a9f57b04efa8b97

--- ash/system/chromeos/power/tray_power.cc
+++ ash/system/chromeos/power/tray_power.cc
@@ -458,7 +458,9 @@ bool TrayPower::UpdateNotificationState(
     const chromeos::PowerSupplyStatus& status) {
   if (!status.battery_is_present ||
       status.is_calculating_battery_time ||
-      status.battery_state == PowerSupplyStatus::CHARGING) {
+      status.battery_state == PowerSupplyStatus::CHARGING ||
+      status.battery_state ==
+          PowerSupplyStatus::NEITHER_CHARGING_NOR_DISCHARGING) {
     notification_state_ = NOTIFICATION_NONE;
     return false;
   }
