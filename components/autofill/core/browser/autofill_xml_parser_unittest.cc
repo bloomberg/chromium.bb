e86409488780cb450a40a8a50ad1ab4cd83b5546
eugenis@chromium.org <eugenis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-25 10:54:53 +0000

    Revert 208429 "Disable AutofillQueryXmlParserTest.ParseAutofillF..."
    Failing on Valgrind bots.
    
    > Disable AutofillQueryXmlParserTest.ParseAutofillFlow when defined(ADDRESS_SANITIZER), instead of defined(OS_LINUX)
    > 
    > ADDRESS_SANITIZER seems better than OS_LINUX in this case.
    > 
    > BUG=253797
    > TBR=blundell@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/17608008
    
    TBR=hashimoto@chromium.org
    
    Review URL: https://codereview.chromium.org/17653006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208473 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0d83c2cda2848eccd3b1e128443cc844b0fe2801

--- components/autofill/core/browser/autofill_xml_parser_unittest.cc
+++ components/autofill/core/browser/autofill_xml_parser_unittest.cc
@@ -165,8 +165,8 @@ TEST_F(AutofillQueryXmlParserTest, ParseExperimentId) {
   EXPECT_EQ("ServerSmartyPants", experiment_id_);
 }
 
-// Fails on ASAN bot. http://crbug.com/253797
-#if defined(ADDRESS_SANITIZER)
+// Fails on Linux ASAN bot. http://crbug.com/253797
+#if defined(OS_LINUX)
 #define MAYBE_ParseAutofillFlow DISABLED_ParseAutofillFlow
 #else
 #define MAYBE_ParseAutofillFlow ParseAutofillFlow
