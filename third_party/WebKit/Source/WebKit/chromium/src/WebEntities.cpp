92743f2c2155bdf134f2b93cd4cdb14c0bcb2ec3
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-27 23:03:03 +0000

    WebEntities shouldn't use bloated characters
    R=dglazkov
    BUG=254708
    
    Review URL: https://chromiumcodereview.appspot.com/18141003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153182 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 1492dc65f606e78bc3893b4023e5ca543cf4a922

--- third_party/WebKit/Source/WebKit/chromium/src/WebEntities.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebEntities.cpp
@@ -54,6 +54,7 @@ WebEntities::WebEntities(bool xmlEntities)
 
 String WebEntities::entityNameByCode(int code) const
 {
+    // FIXME: We should use find so we only do one hash lookup.
     if (m_entitiesMap.contains(code))
         return m_entitiesMap.get(code);
     return "";
@@ -61,27 +62,23 @@ String WebEntities::entityNameByCode(int code) const
 
 String WebEntities::convertEntitiesInString(const String& value) const
 {
-    unsigned len = value.length();
-    const UChar* startPos = value.bloatedCharacters();
-    const UChar* curPos = startPos;
-
-    // FIXME: Optimize - create StringBuilder only if value has any entities.
     StringBuilder result;
-    while (len--) {
-        if (m_entitiesMap.contains(*curPos)) {
-            // Append content before entity code.
-            if (curPos > startPos)
-                result.append(String(startPos, curPos - startPos));
+    bool didConvertEntity = false;
+    unsigned length = value.length();
+    for (unsigned i = 0; i < length; ++i) {
+        UChar c = value[i];
+        // FIXME: We should use find so we only do one hash lookup.
+        if (m_entitiesMap.contains(c)) {
+            didConvertEntity = true;
             result.append('&');
-            result.append(m_entitiesMap.get(*curPos));
+            result.append(m_entitiesMap.get(c));
             result.append(';');
-            startPos = ++curPos;
-        } else
-            curPos++;
+        }
+        result.append(c);
     }
-    // Append the remaining content.
-    if (curPos > startPos)
-        result.append(String(startPos, curPos - startPos));
+
+    if (!didConvertEntity)
+        return value;
 
     return result.toString();
 }
