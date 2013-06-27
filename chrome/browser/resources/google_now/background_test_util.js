dcb24349ace1ed2c1db240bcf3b4e3de4f1f389d
vadimt@chromium.org <vadimt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-27 22:43:39 +0000

    First unit test for Google Now extension. The test is based upon gtestjs technology.
    The test check only one function in one script of Google Now extension. Other tests will follow. 
    
    The test doesn't verify (and doesn't want to verify) how the extension' js code behaves in the context of a real extension. It's just a unit test that checks function outputs based on inputs, and future tests will also mock callees and check how they are called.
    
    The main goal of this CL is to get feedback from the reviewers regarding the general direction chosen for unit-testing scripts in component extensions. 
    
    
    BUG=164227
    
    Review URL: https://chromiumcodereview.appspot.com/17125003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209016 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2b62518f9a9480e3708b85c95bd65eda7c1b2d5b

---
+++ chrome/browser/resources/google_now/background_test_util.js
@@ -0,0 +1,25 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Mocks for globals needed for loading background.js.
+
+function emptyMock() {}
+function buildTaskManager() {
+  return {instrumentApiFunction: emptyMock};
+}
+var instrumentApiFunction = emptyMock;
+var buildAttemptManager = emptyMock;
+var emptyListener = {addListener: emptyMock};
+chrome['location'] = {onLocationUpdate: emptyListener};
+chrome['notifications'] = {
+  onButtonClicked: emptyListener,
+  onClicked: emptyListener,
+  onClosed: emptyListener
+};
+chrome['omnibox'] = {onInputEntered: emptyListener};
+chrome['runtime'] = {
+  onInstalled: emptyListener,
+  onStartup: emptyListener
+};
+var storage = {};
