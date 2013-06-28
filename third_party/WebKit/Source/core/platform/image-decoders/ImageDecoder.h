055ba4852d5810cf41529c8a0b7ec6cf21ada32a
b.kelemen@sisa.samsung.com <b.kelemen@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-28 22:50:38 +0000

    Fix mess in ImageDecoder::setRGB* methods after r153004
    I was acidentally changed the order of alpha tests (0, 255).
    Also my asserts are adding more noise than value so remove them.
    
    R=noel@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/17906002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153264 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ec71e8ffe28e59d3a749d7c2d57a690c6b92ee7f

--- third_party/WebKit/Source/core/platform/image-decoders/ImageDecoder.h
+++ third_party/WebKit/Source/core/platform/image-decoders/ImageDecoder.h
@@ -190,13 +190,11 @@ namespace WebCore {
 
         inline void setRGBAPremultiply(PixelData* dest, unsigned r, unsigned g, unsigned b, unsigned a)
         {
-            ASSERT(m_premultiplyAlpha);
-
-            if (!a) {
-                *dest = 0;
-                return;
-            }
             if (a < 255) {
+                if (!a) {
+                    *dest = 0;
+                    return;
+                }
                 unsigned alphaMult = a * fixPointMult;
                 r = fixPointUnsignedMultiply(r, alphaMult);
                 g = fixPointUnsignedMultiply(g, alphaMult);
@@ -210,8 +208,6 @@ namespace WebCore {
 
         inline void setRGBARaw(PixelData* dest, unsigned r, unsigned g, unsigned b, unsigned a)
         {
-            ASSERT(!m_premultiplyAlpha);
-
             *dest = SkPackARGB32NoCheck(a, r, g, b);
         }
 
