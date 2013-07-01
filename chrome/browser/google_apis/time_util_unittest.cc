c20067e14c448eb7342ea212ce2fc93715c74fcd
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-01 07:59:21 +0000

    google_apis: Remove use of TimezoneSettings from time_util_unittest.cc
    GetTimeFromStringLocalTimezoneForChromeOs test was unnecessary as
    the local timezone case was covered in GetTimeFromStringLocalTimezone.
    
    BUG=256115
    TEST=none
    R=kinaba@chromium.org
    
    Review URL: https://codereview.chromium.org/18326002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209396 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 484a36386d9dbb239e49a8e7c9f77a30441a5806

--- chrome/browser/google_apis/time_util_unittest.cc
+++ chrome/browser/google_apis/time_util_unittest.cc
@@ -9,49 +9,24 @@
 #include "base/time/time.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#if defined(OS_CHROMEOS)
-#include "chrome/browser/chromeos/system/timezone_settings.h"
-#endif  // OS_CHROMEOS
-
 namespace google_apis {
 namespace util {
 namespace {
 
 std::string FormatTime(const base::Time& time) {
-  return UTF16ToUTF8(TimeFormatShortDateAndTime(time));
+  return base::UTF16ToUTF8(base::TimeFormatShortDateAndTime(time));
 }
 
 }  // namespace
-
-#if defined(OS_CHROMEOS)
-// ChromeOS can test utilities using ICU library.
-TEST(TimeUtilTest, GetTimeFromStringLocalTimezoneForChromeOs) {
-  // Creates time object GMT.
-  base::Time::Exploded exploded = {2012, 7, 0, 14, 1, 3, 21, 151};
-  base::Time target_time = base::Time::FromUTCExploded(exploded);
-
-  // Creates time object as the local time.
-  base::Time test_time;
-  ASSERT_TRUE(GetTimeFromString("2012-07-14T01:03:21.151", &test_time));
-
-  // Gets the offset between the local time and GMT.
-  const icu::TimeZone& tz =
-      chromeos::system::TimezoneSettings::GetInstance()->GetTimezone();
-  UErrorCode status = U_ZERO_ERROR;
-  int millisecond_in_day = ((1 * 60 + 3) * 60 + 21) * 1000 + 151;
-  int offset = tz.getOffset(1, 2012, 7, 14, 1, millisecond_in_day, status);
-  ASSERT_TRUE(U_SUCCESS(status));
-
-  EXPECT_EQ((target_time - test_time).InMilliseconds(), offset);
-}
-#endif  // OS_CHROMEOS
 
 TEST(TimeUtilTest, GetTimeFromStringLocalTimezone) {
   // Creates local time objects from exploded structure.
   base::Time::Exploded exploded = {2013, 1, 0, 15, 17, 11, 35, 374};
   base::Time local_time = base::Time::FromLocalExploded(exploded);
 
-  // Creates local time object, parsing time string.
+  // Creates local time object, parsing time string. Note that if there is
+  // not timezone suffix, GetTimeFromString() will handle this as local time
+  // with FromLocalExploded().
   base::Time test_time;
   ASSERT_TRUE(GetTimeFromString("2013-01-15T17:11:35.374", &test_time));
 
@@ -59,7 +34,7 @@ TEST(TimeUtilTest, GetTimeFromStringLocalTimezone) {
   EXPECT_EQ(local_time, test_time);
 }
 
-TEST(TimeUtilTest, GetTimeFromStringTimezone) {
+TEST(TimeUtilTest, GetTimeFromStringNonTrivialTimezones) {
   base::Time target_time;
   base::Time test_time;
   // Creates the target time.
@@ -78,19 +53,23 @@ TEST(TimeUtilTest, GetTimeFromStringTimezone) {
   EXPECT_EQ(FormatTime(target_time), FormatTime(test_time));
 }
 
-TEST(TimeUtilTest, GetTimeFromString) {
+TEST(TimeUtilTest, GetTimeFromStringBasic) {
   base::Time test_time;
 
+  // Test that the special timezone "Z" (UTC) is handled.
   base::Time::Exploded target_time1 = {2005, 1, 0, 7, 8, 2, 0, 0};
   EXPECT_TRUE(GetTimeFromString("2005-01-07T08:02:00Z", &test_time));
   EXPECT_EQ(FormatTime(base::Time::FromUTCExploded(target_time1)),
             FormatTime(test_time));
 
+  // Test that a simple timezone "-08:00" is handled
+  // 17:57 - 8 hours = 09:57
   base::Time::Exploded target_time2 = {2005, 8, 0, 9, 17, 57, 0, 0};
   EXPECT_TRUE(GetTimeFromString("2005-08-09T09:57:00-08:00", &test_time));
   EXPECT_EQ(FormatTime(base::Time::FromUTCExploded(target_time2)),
             FormatTime(test_time));
 
+  // Test that milliseconds (.123) are handled.
   base::Time::Exploded target_time3 = {2005, 1, 0, 7, 8, 2, 0, 123};
   EXPECT_TRUE(GetTimeFromString("2005-01-07T08:02:00.123Z", &test_time));
   EXPECT_EQ(FormatTime(base::Time::FromUTCExploded(target_time3)),
