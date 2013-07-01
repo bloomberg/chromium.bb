f550fa860e06b49a389816efebd3977b98fdb85f
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-01 22:49:12 +0000

    [Telemetry] Add a netsim_top25 benchmark.
    This runs the top 25 page set under a simulated cable connection. It
    will be used to replace the old pyauto netsim benchmark.
    
    BUG=224049
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/18346009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209527 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b5167a3a9773bd348e1ab49e12545d6a6bdc2205

---
+++ tools/perf/benchmarks/netsim_top25.py
@@ -0,0 +1,20 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from telemetry import test
+
+from perf_tools import page_cycler
+
+
+class NetsimTop25(test.Test):
+  """Measures load time of the top 25 sites under simulated cable network."""
+  test = page_cycler.PageCycler
+  test.clear_cache_before_each_run = True
+  page_set = 'tools/perf/page_sets/top_25.json'
+  options = {
+    'extra_wpr_args': [
+      '--shaping_type=proxy',
+      '--net=cable'
+      ],
+    'pageset_repeat': '5',
+    }
