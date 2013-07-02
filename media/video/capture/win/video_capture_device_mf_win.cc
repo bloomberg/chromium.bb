ba553d3c3431e16db589f885b5e2be138e5a4ea6
mcasas@chromium.org <mcasas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 14:50:57 +0000

    Add frame rate setting to the video capture infrastructure in Windows, Media Foundation.
    BUG=https://code.google.com/p/chromium/issues/detail?id=246928
    
    Review URL: https://chromiumcodereview.appspot.com/15792020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209702 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 477296ef770110c2fc07a8038ef1e31bf157e8f4

--- media/video/capture/win/video_capture_device_mf_win.cc
+++ media/video/capture/win/video_capture_device_mf_win.cc
@@ -335,13 +335,41 @@ void VideoCaptureDeviceMFWin::Allocate(
   ScopedComPtr<IMFMediaType> type;
   if (FAILED(hr = reader_->GetNativeMediaType(
           MF_SOURCE_READER_FIRST_VIDEO_STREAM, found_capability.stream_index,
-          type.Receive())) ||
-      FAILED(hr = reader_->SetCurrentMediaType(
-          MF_SOURCE_READER_FIRST_VIDEO_STREAM, NULL, type))) {
+          type.Receive()))) {
     OnError(hr);
     return;
   }
+  // Set the framerate in the configuration struct retrieved before. This call
+  // cannot fail since it's just writing a number in the IMFMediaType*.
+  MFSetAttributeRatio(
+      type, MF_MT_FRAME_RATE, static_cast<UINT32>(frame_rate), 1);
 
+  if (FAILED(hr = reader_->SetCurrentMediaType(
+                 MF_SOURCE_READER_FIRST_VIDEO_STREAM, NULL, type))) {
+    OnError(hr);
+    return;
+  }
+  // Usually the driver supports a discrete set of capture frame rates, and will
+  // set the actual framerate as close as possible to the requested one; in the
+  // future (TODO(mcasas)) we'll need to retrieve it for later comparisons with
+  // user requested mandatory constraints.
+#if !defined(NDEBUG)
+  ScopedComPtr<IMFMediaType> type_read;
+  if (SUCCEEDED(
+          hr = reader_->GetNativeMediaType(MF_SOURCE_READER_FIRST_VIDEO_STREAM,
+                                           found_capability.stream_index,
+                                           type_read.Receive()))) {
+    UINT32 temp_frame_rate_num = 0;
+    UINT32 temp_frame_rate_denom = 0;
+    if (SUCCEEDED(MFGetAttributeRatio(type_read,
+                                      MF_MT_FRAME_RATE,
+                                      &temp_frame_rate_num,
+                                      &temp_frame_rate_denom))) {
+      DLOG(WARNING) << "Actual camera framerate: " << temp_frame_rate_num << "/"
+                    << temp_frame_rate_denom;
+    }
+  }
+#endif  // if defined(NDEBUG)
   observer_->OnFrameInfo(found_capability);
 }
 
