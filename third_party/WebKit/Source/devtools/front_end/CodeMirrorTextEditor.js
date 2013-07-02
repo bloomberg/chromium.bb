bloomberg/chromium.bb

commit 902902a1434d3afa86d1a3918b0338c5d73f5cbb

DevTools: [CodeMirror] highlight js and css embedded in html
This patch revisites the way of specifing highlight for the
language tokens. The previous approach was about using themes,
but this turned out to be not flexible enough as we can set only
one theme, but in html mixed modes we might want to specify couple of
them. Creation of one generic theme is not possible as well as
token names produced by different highlighters often clash, e.g.
both javascript and css modes generate "property" token.

To avoid token clashing, we override modes in CodeMirror to
make them return prefixed token values. This way javascript mode
will return "js-*" tokens and css mode will end up returning
"css-*" tokens.

BUG=
R=apavlov@chromium.org, vsevik@chromium.org

Review URL: https://codereview.chromium.org/18089015

git-svn-id: svn://svn.chromium.org/blink/trunk@153329 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: d71a8c70563d1f10bc7c6b2fd493089924189f56

third_party/WebKit/Source/devtools/front_end/CodeMirrorTextEditor.js
@@ -163,6 +163,11 @@ WebInspector.CodeMirrorTextEditor = function(url, delegate)
     this.element.addEventListener("focus", this._handleElementFocus.bind(this), false);
     this.element.addEventListener("keydown", this._handleKeyDown.bind(this), false);
     this.element.tabIndex = 0;
+
+    this._overrideModeWithPrefixedTokens("css-base", "css-");
+    this._overrideModeWithPrefixedTokens("javascript", "js-");
+    this._overrideModeWithPrefixedTokens("xml", "xml-");
+
     this._setupSelectionColor();
     this._setupWhitespaceHighlight();
 }
@@ -424,9 +429,13 @@ WebInspector.CodeMirrorTextEditor.prototype = {
         return hasLongLines;
     },
 
+    /**
+     * @param {string} mimeType
+     * @return {string}
+     */
     _whitespaceOverlayMode: function(mimeType)
     {
-        var modeName = mimeType + "+whitespaces";
+        var modeName = CodeMirror.mimeModes[mimeType] + "+whitespaces";
         if (CodeMirror.modes[modeName])
             return modeName;
 
@@ -453,6 +462,38 @@ WebInspector.CodeMirrorTextEditor.prototype = {
         }
         CodeMirror.defineMode(modeName, modeConstructor);
         return modeName;
+    },
+
+    /**
+     * @param {string} modeName
+     * @param {string} tokenPrefix
+     */
+    _overrideModeWithPrefixedTokens: function(modeName, tokenPrefix)
+    {
+        var oldModeName = modeName + "-old";
+        if (CodeMirror.modes[oldModeName])
+            return;
+
+        CodeMirror.defineMode(oldModeName, CodeMirror.modes[modeName]);
+        CodeMirror.defineMode(modeName, modeConstructor);
+
+        function modeConstructor(config, parserConfig)
+        {
+            var innerConfig = {};
+            for (var i in parserConfig)
+                innerConfig[i] = parserConfig[i];
+            innerConfig.name = oldModeName;
+            var codeMirrorMode = CodeMirror.getMode(config, innerConfig);
+            codeMirrorMode.name = modeName;
+            codeMirrorMode.token = tokenOverride.bind(this, codeMirrorMode.token);
+            return codeMirrorMode;
+        }
+
+        function tokenOverride(superToken, stream, state)
+        {
+            var token = superToken(stream, state);
+            return tokenPrefix + token;
+        }
     },
 
     _enableLongLinesMode: function()

third_party/WebKit/Source/devtools/front_end/cm/cmdevtools.css
@@ -171,31 +171,31 @@
     z-index: -1;
 }
 
-.cm-s-web-inspector-js span.cm-keyword {color: rgb(170, 13, 145);}
-.cm-s-web-inspector-js span.cm-number {color: rgb(28, 0, 207);}
-.cm-s-web-inspector-js span.cm-comment {color: rgb(0, 116, 0);}
-.cm-s-web-inspector-js span.cm-string {color: rgb(196, 26, 22);}
-.cm-s-web-inspector-js span.cm-string-2 {color: rgb(196, 26, 22);}
+span.cm-js-keyword {color: rgb(170, 13, 145);}
+span.cm-js-number {color: rgb(28, 0, 207);}
+span.cm-js-comment {color: rgb(0, 116, 0);}
+span.cm-js-string {color: rgb(196, 26, 22);}
+span.cm-js-string-2 {color: rgb(196, 26, 22);}
 
-.cm-s-web-inspector-css span.cm-keyword { color: rgb(7, 144, 154);}
-.cm-s-web-inspector-css span.cm-number {color: rgb(50, 0, 255);}
-.cm-s-web-inspector-css span.cm-comment {color: rgb(0, 116, 0);}
-.cm-s-web-inspector-css span.cm-def {color: rgb(200, 0, 0);}
-.cm-s-web-inspector-css span.cm-meta {color: rgb(200, 0, 0);}
-.cm-s-web-inspector-css span.cm-atom {color: rgb(7, 144, 154);}
-.cm-s-web-inspector-css span.cm-string {color: rgb(7, 144, 154);}
-.cm-s-web-inspector-css span.cm-string-2 {color: rgb(7, 144, 154);}
-.cm-s-web-inspector-css span.cm-link {color: rgb(7, 144, 154);}
-.cm-s-web-inspector-css span.cm-variable {color: rgb(200, 0, 0);}
-.cm-s-web-inspector-css span.cm-variable-2 {color: rgb(0, 0, 128);}
-.cm-s-web-inspector-css span.cm-property {color: rgb(200, 0, 0);}
+span.cm-css-keyword { color: rgb(7, 144, 154);}
+span.cm-css-number {color: rgb(50, 0, 255);}
+span.cm-css-comment {color: rgb(0, 116, 0);}
+span.cm-css-def {color: rgb(200, 0, 0);}
+span.cm-css-meta {color: rgb(200, 0, 0);}
+span.cm-css-atom {color: rgb(7, 144, 154);}
+span.cm-css-string {color: rgb(7, 144, 154);}
+span.cm-css-string-2 {color: rgb(7, 144, 154);}
+span.cm-css-link {color: rgb(7, 144, 154);}
+span.cm-css-variable {color: rgb(200, 0, 0);}
+span.cm-css-variable-2 {color: rgb(0, 0, 128);}
+span.cm-css-property {color: rgb(200, 0, 0);}
 
-.cm-s-web-inspector-html span.cm-meta {color: rgb(192, 192, 192);}
-.cm-s-web-inspector-html span.cm-comment {color: rgb(35, 110, 37);}
-.cm-s-web-inspector-html span.cm-string {color: rgb(26, 26, 166);}
-.cm-s-web-inspector-html span.cm-tag {color: rgb(136, 18, 128);}
-.cm-s-web-inspector-html span.cm-attribute {color: rgb(153, 69, 0);}
-.cm-s-web-inspector-html span.cm-link {color: #00e;}
+span.cm-xml-meta {color: rgb(192, 192, 192);}
+span.cm-xml-comment {color: rgb(35, 110, 37);}
+span.cm-xml-string {color: rgb(26, 26, 166);}
+span.cm-xml-tag {color: rgb(136, 18, 128);}
+span.cm-xml-attribute {color: rgb(153, 69, 0);}
+span.cm-xml-link {color: #00e;}
 
 .CodeMirror .webkit-html-message-bubble {
     box-shadow: black 0px 2px 5px;

third_party/WebKit/Source/devtools/front_end/externs.js
@@ -434,6 +434,7 @@ CodeMirror.Pass;
 CodeMirror.showHint = function(codeMirror, hintintFunction) { };
 CodeMirror.commands = {};
 CodeMirror.modes = {};
+CodeMirror.mimeModes = {};
 CodeMirror.getMode = function(options, spec) { };
 CodeMirror.overlayMode = function(mode1, mode2, squashSpans) { };
 CodeMirror.defineMode = function(modeName, modeConstructor) { };

commit 239e806e91670f56e46ceb21950c66f777231ada

DevTools: [CodeMirror] fix popover
This patch fixes the regression which completely broke popover so that
it doesn't show up anymore. The regression was caused by r153329.

BUG=
R=apavlov@chromium.org

Review URL: https://codereview.chromium.org/18504002

git-svn-id: svn://svn.chromium.org/blink/trunk@153410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 2730df20de09b9c09cae29526693a301b557120b

third_party/WebKit/Source/devtools/front_end/CodeMirrorTextEditor.js
@@ -406,12 +406,12 @@ WebInspector.CodeMirrorTextEditor.prototype = {
 
     _convertTokenType: function(tokenType)
     {
-        if (tokenType.startsWith("variable") || tokenType.startsWith("property") || tokenType === "def")
+        if (tokenType.startsWith("js-variable") || tokenType.startsWith("js-property") || tokenType === "js-def")
             return "javascript-ident";
-        if (tokenType === "string-2")
+        if (tokenType === "js-string-2")
             return "javascript-regexp";
-        if (tokenType === "number" || tokenType === "comment" || tokenType === "string" || tokenType === "keyword")
-            return "javascript-" + tokenType;
+        if (tokenType === "js-number" || tokenType === "js-comment" || tokenType === "js-string" || tokenType === "js-keyword")
+            return "javascript-" + tokenType.substring("js-".length);
         return null;
     },
 
@@ -536,7 +536,7 @@ WebInspector.CodeMirrorTextEditor.prototype = {
         function tokenOverride(superToken, stream, state)
         {
             var token = superToken(stream, state);
-            return tokenPrefix + token;
+            return token ? tokenPrefix + token : token;
         }
     },
 