69a00b917fa8c2f4719f7eac3c377ba470be0388
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 21:46:28 +0000

    Revert 210031 "Fix and reenable WindowOpenPanelTest.WindowOpenPa..."
    > Fix and reenable WindowOpenPanelTest.WindowOpenPanelDetached
    > 
    > The fix is to pass chrome.test.callbackPass() to wait for the remove callback get called.
    > 
    > BUG=179251
    > TEST=test enabled
    > 
    > Review URL: https://chromiumcodereview.appspot.com/18383004
    
    TBR=jianli@chromium.org
    
    Review URL: https://codereview.chromium.org/18348024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210046 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a0b6cccefcd49ddb5a8232baf716425050759f1e

--- chrome/browser/extensions/window_open_apitest.cc
+++ chrome/browser/extensions/window_open_apitest.cc
@@ -241,14 +241,10 @@ IN_PROC_BROWSER_TEST_F(WindowOpenPanelTest, MAYBE_WindowOpenPanel) {
   ASSERT_TRUE(RunExtensionTest("window_open/panel")) << message_;
 }
 
-#if defined(USE_ASH_PANELS)
 // On Ash, this currently fails because we're currently opening new panel
 // windows as popup windows instead.
-#define MAYBE_WindowOpenPanelDetached DISABLED_WindowOpenPanelDetached
-#else
-#define MAYBE_WindowOpenPanelDetached WindowOpenPanelDetached
-#endif
-IN_PROC_BROWSER_TEST_F(WindowOpenPanelTest, MAYBE_WindowOpenPanelDetached) {
+// This is also flakey on Windows, ASan and Mac builds: crbug.com/179251
+IN_PROC_BROWSER_TEST_F(WindowOpenPanelTest, DISABLED_WindowOpenPanelDetached) {
   ASSERT_TRUE(RunExtensionTest("window_open/panel_detached")) << message_;
 }
 
