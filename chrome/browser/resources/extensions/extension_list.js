10294ff794941aaed4b07e1e3df4bdcaf35f096a
DHNishi@gmail.com <DHNishi@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 04:55:26 +0000

    Fix a bug where extension's icon was not updated on reload.
    BUG=159302
    
    Review URL: https://chromiumcodereview.appspot.com/18369003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209895 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: df87559411fe06079453d3e3f038f5044dcb2e0d

--- chrome/browser/resources/extensions/extension_list.js
+++ chrome/browser/resources/extensions/extension_list.js
@@ -97,7 +97,10 @@ cr.define('options', function() {
         node.classList.add('extension-highlight');
 
       var item = node.querySelector('.extension-list-item');
-      item.style.backgroundImage = 'url(' + extension.icon + ')';
+      var extensionIconUrl = extension.icon;
+      if (extension.allow_reload)
+        extensionIconUrl = extension.icon + '?' + Date.now();
+      item.style.backgroundImage = 'url(' + extensionIconUrl + ')';
 
       var title = node.querySelector('.extension-title');
       title.textContent = extension.name;
