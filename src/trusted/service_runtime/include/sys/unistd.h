994d9b35b469eef0f3247c9da5355bb06d94d264
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-07-04 01:25:40 +0000

    Fix declaration of getpagesize() to use standard return type (int)
    The standard says getpagesize() returns int, not size_t:
    http://pubs.opengroup.org/onlinepubs/007908775/xsh/getpagesize.html
    
    NaCl has a forked copy of some of newlib's headers.  (I've added a
    comment explaining that.)
    
    This bug was fixed upstream some time ago:
    
    http://git.chromium.org/gitweb/?p=native_client/nacl-newlib.git;a=commit;h=b534873c396d64f9cef7d6fc4b9ba3d1c360a5ea
    Author: Christopher Faylor <cgf@redhat.com>
    Date:   Sat Sep 19 02:30:51 2009 +0000
    * libc/include/sys/unistd.h: Declare getpagesize as returning POSIX-mandated
    int.
    
    Also update PNaCl's binutils rev to pull in:
    dd7ce97: Undo LOCALMOD working around non-standard getpagesize() return value
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3545
    TEST=normal trybots + PNaCl toolchain trybots
    
    Review URL: https://codereview.chromium.org/18341010
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11681 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 09c403ed124153dce94ba37a157975342018cf0a

--- src/trusted/service_runtime/include/sys/unistd.h
+++ src/trusted/service_runtime/include/sys/unistd.h
@@ -4,7 +4,10 @@
  * found in the LICENSE file.
  */
 
-
+/*
+ * This file is based on the file "newlib/libc/include/sys/unistd.h"
+ * from newlib.
+ */
 
 #ifndef NATIVE_CLIENT_SRC_TRUSTED_SERVICE_RUNTIME_INCLUDE_SYS_UNISTD_H_
 #define NATIVE_CLIENT_SRC_TRUSTED_SERVICE_RUNTIME_INCLUDE_SYS_UNISTD_H_
@@ -88,7 +91,7 @@ char    _EXFUN(*getlogin, (void ));
 int _EXFUN(getlogin_r, (char *name, size_t namesize) );
 #endif
 char _EXFUN(*getpass, (const char *__prompt));
-size_t  _EXFUN(getpagesize, (void));
+int     _EXFUN(getpagesize, (void));
 #if defined(__CYGWIN__)
 int    _EXFUN(getpeereid, (int, uid_t *, gid_t *));
 #endif
