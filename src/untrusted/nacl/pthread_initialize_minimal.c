ece3b69b175776d3df15b698728beab92c619a35
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-07-08 23:31:50 +0000

    Avoid using sbrk()/sysbrk() for the initial thread's TLS setup
    This should remove the only case where an sbrk() allocation is
    required to work.
    
    Use malloc() to do the allocation instead, as the IRT does.  (The
    comment about malloc() not working at this point was not correct.)  To
    avoid pulling in malloc() for trivial executables, make this a weak
    reference and fall back to using mmap().
    
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3542
    TEST= run_tls_test is an example of a trivial test that doesn't link in malloc()
    
    Review URL: https://codereview.chromium.org/18572004
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11699 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: e78a4ebd61aefb5b8cd55fee95c62b75d86754bd

--- src/untrusted/nacl/pthread_initialize_minimal.c
+++ src/untrusted/nacl/pthread_initialize_minimal.c
@@ -4,11 +4,22 @@
  * found in the LICENSE file.
  */
 
+/* <sys/types.h> must come before <sys/mman.h> due to a bug in the headers. */
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/mman.h>
 #include <unistd.h>
 
 #include "native_client/src/untrusted/nacl/nacl_irt.h"
 #include "native_client/src/untrusted/nacl/nacl_thread.h"
 #include "native_client/src/untrusted/nacl/tls.h"
+
+/*
+ * Use a weak reference to malloc() so that we only use it if it is
+ * going to be linked in anyway.  This reduces executable sizes for
+ * very simple programs that don't use malloc().
+ */
+void *malloc(size_t size) __attribute__((weak));
 
 /*
  * This initialization happens early in startup with or without libpthread.
@@ -17,10 +28,22 @@
 void __pthread_initialize_minimal(size_t tdb_size) {
   size_t combined_size = __nacl_tls_combined_size(tdb_size);
 
-  /*
-   * Use sbrk not malloc here since the library is not initialized yet.
-   */
-  void *combined_area = sbrk(combined_size);
+  void *combined_area;
+  if (malloc != NULL) {
+    combined_area = malloc(combined_size);
+  } else {
+    /*
+     * Fall back to mmap() when malloc() is not linked in.  This will
+     * likely waste most of a 64k page, but that probably doesn't
+     * matter.  If the program doesn't use malloc() it probably won't
+     * need to allocate much else.
+     *
+     * Note that if this allocation fails, it might crash setting
+     * errno, because TLS is not set up yet.
+     */
+    combined_area = mmap(NULL, combined_size, PROT_READ | PROT_WRITE,
+                         MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  }
 
   /*
    * Fill in that memory with its initializer data.
