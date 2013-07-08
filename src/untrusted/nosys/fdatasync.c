d82f4be10524c505f025d1e1af174e7d377265a1
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-07-08 17:52:34 +0000

    Add stubs for fsync() and fdatasync() to libnosys
    Without these, Python fails to link when building against nacl-newlib.
    
    Python has autoconf checks for fsync() and fdatasync() which check
    whether these functions are declared at compile time, but they don't
    check link time.  These checks pass because these two functions are
    declared in nacl-newlib's <unistd.h>.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=1229
    TEST=none
    
    Review URL: https://codereview.chromium.org/18817002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11694 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: fdd7fc6bb7b73905e6330f0d30aedaa117ba94f5

---
+++ src/untrusted/nosys/fdatasync.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2013 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
+#include <errno.h>
+#include <unistd.h>
+
+int fdatasync(int fd) {
+  errno = ENOSYS;
+  return -1;
+}
+
+#include "native_client/src/untrusted/nosys/warning.h"
+stub_warning(fdatasync);
