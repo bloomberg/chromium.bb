c3bea29de642c7e23b7ef19de5bbadc294b77311
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-09 17:08:49 +0000

    DevTools: [CodeMirror] test suite for CodeMirrorTextEditor
    This patch revisits LayoutTests/inspector/editor/ folder
    to migrate tests from DefaultTextEditor into CodeMirrorTextEditor.
    - Those tests which were DTE-specific were purged
    - Highlighter tests are kept for now
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/18889002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153791 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: af1430813b9c4dc9579dd838817cef9651d664db

--- third_party/WebKit/LayoutTests/inspector/editor/editor-test.js
+++ third_party/WebKit/LayoutTests/inspector/editor/editor-test.js
@@ -3,9 +3,9 @@ function initialize_EditorTests()
 
 InspectorTest.createTestEditor = function(clientHeight, chunkSize, textEditorDelegate)
 {
+    loadScript("CodeMirrorTextEditor.js");
     WebInspector.debugDefaultTextEditor = true;
-    var textEditor = new WebInspector.DefaultTextEditor("", textEditorDelegate || new WebInspector.TextEditorDelegate());
-    textEditor.overrideViewportForTest(0, clientHeight || 100, chunkSize || 10);
+    var textEditor = new WebInspector.CodeMirrorTextEditor("", textEditorDelegate || new WebInspector.TextEditorDelegate());
     textEditor.show(WebInspector.inspectorView.element);
     return textEditor;
 };
@@ -39,67 +39,26 @@ InspectorTest.textWithSelection = function(text, selection)
     return lines.join("\n");
 }
 
-InspectorTest.insertTextLine = function(line)
+InspectorTest.typeIn = function(typeText)
 {
-    function enter()
-    {
-        eventSender.keyDown("\n");
+    for(var charIndex = 0; charIndex < typeText.length; ++charIndex) {
+        switch (typeText[charIndex]) {
+        case "L":
+            eventSender.keyDown("leftArrow");
+            break;
+        case "R":
+            eventSender.keyDown("rightArrow");
+            break;
+        case "U":
+            eventSender.keyDown("upArrow");
+            break;
+        case "D":
+            eventSender.keyDown("downArrow");
+            break;
+        default:
+            eventSender.keyDown(typeText[charIndex]);
+        }
     }
-
-    function innerInsertTextLine()
-    {
-        textInputController.insertText(line);
-    }
-    setTimeout(innerInsertTextLine);
-    setTimeout(enter);
 }
-
-InspectorTest.dumpEditorChunks = function(textEditor)
-{
-    InspectorTest.addResult("Chunk model");
-    var chunks = textEditor._mainPanel._textChunks;
-    for (var i = 0; i < chunks.length; ++i)
-        InspectorTest.addResult("Chunk [" + i + "] " + chunks[i].startLine + ":" + chunks[i].endLine + " (" + (chunks[i]._expanded ? "expanded" : "collapsed") + ")");
-};
-
-InspectorTest.dumpEditorModel = function(textEditor)
-{
-    InspectorTest.addResult("Text model");
-    var textModel = textEditor._textModel;
-    for (var i = 0; i < textModel.linesCount; ++i) {
-        var prefix = "[" + i + "]";
-        while (prefix.length < 10)
-            prefix += " ";
-        InspectorTest.addResult(prefix + textModel.line(i));
-    }
-};
-
-InspectorTest.dumpEditorDOM = function(textEditor)
-{
-    InspectorTest.addResult("Editor DOM");
-    var element = textEditor._mainPanel._container;
-    for (var node = element.firstChild; node; node = node.nextSibling) {
-        if (node._chunk)
-            var prefix = "[" + node._chunk.startLine + ":" + node._chunk.endLine + "]";
-        else
-            var prefix = "[" + node.lineNumber + "]";
-        while (prefix.length < 10)
-            prefix += " ";
-        InspectorTest.addResult(prefix + node.outerHTML);
-    }
-};
-
-InspectorTest.dumpEditorHTML = function(textEditor, mainPanelOnly)
-{
-    var element = mainPanelOnly ? textEditor._mainPanel.element : textEditor.element;
-    var dumpedHTML = element.innerHTML.replace(/<div/g, "\n<div");
-    var dumpedHTML = dumpedHTML.replace(/height: [0-9]+/g, "height: <number>");
-    InspectorTest.addResult(dumpedHTML);
-};
-
-InspectorTest.getLineElement = function(textEditor, lineNumber)
-{
-    return textEditor._mainPanel.chunkForLine(lineNumber).expandedLineRow(lineNumber);
-};
 
 }
