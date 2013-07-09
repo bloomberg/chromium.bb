e1ad1d2498e49badd1de7cee21531e18a10a8e72
Rob Bradford <rob@linux.intel.com>
2013-07-09 12:12:50 +0100

    tablet-shell: Avoid infinite loop when unlocking
    weston_compositor_wake will fire the signal that the unlock handler is
    setup as the listener for. Instead lets change the state to HOME which
    unlocks.

--- src/tablet-shell.c
+++ src/tablet-shell.c
@@ -417,7 +417,7 @@ tablet_shell_unlock(struct wl_listener *listener, void *data)
 	struct tablet_shell *shell =
 		container_of(listener, struct tablet_shell, unlock_listener);
 
-	weston_compositor_wake(shell->compositor);
+	tablet_shell_set_state(shell, STATE_HOME);
 }
 
 static void
