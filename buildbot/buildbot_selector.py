bloomberg/chromium.bb

commit 77c67f6259f16f5f38cc0dcb553477d149d8487e

Fixing clashing names in buildbot selector.
Recent changes to the selector introduced overlapping conventions.
Biasing towards the meaning we want.

BUG=None
TEST=None
TBR=krasin@chromium.org

Review URL: https://codereview.chromium.org/12441005

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@10959 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: 1ca6112b59f573219ac18649e7319c550bdee940

buildbot/buildbot_selector.py
@@ -92,17 +92,6 @@ BOT_ASSIGNMENT = {
     ######################################################################
     # Trybots.
     ######################################################################
-    'nacl-win32_newlib_opt':
-        python + ' buildbot\\buildbot_standard.py opt 32 newlib --no-gyp',
-    'nacl-win32_glibc_opt':
-        python + ' buildbot\\buildbot_standard.py opt 32 glibc --no-gyp',
-    'nacl-win64_newlib_dbg':
-        python + ' buildbot\\buildbot_standard.py dbg 64 newlib',
-    'nacl-win64_newlib_opt':
-        python + ' buildbot\\buildbot_standard.py opt 64 newlib',
-    'nacl-win64_glibc_opt':
-        python + ' buildbot\\buildbot_standard.py opt 64 glibc',
-
     'nacl-lucid64_validator_opt':
         python + ' buildbot/buildbot_standard.py opt 64 glibc --validator',
     'nacl-lucid64_newlib_dbg_valgrind':
@@ -229,16 +218,20 @@ BOT_ASSIGNMENT = {
 
 }
 
+special_for_arm = ['win7_64', 'win7-64', 'lucid-64', 'lucid64']
 for platform in [
-    'vista', 'win7', 'win8', 'win32', 'win64',
-    'win7_64', 'win7-64', 'lucid-64',  # For win7-64-arm etc.
+    'vista', 'win7', 'win8', 'win',
     'mac10.6', 'mac10.7', 'mac10.8',
-    'lucid', 'precise']:
-  for arch in ['', '32', '64', 'arm']:
+    'lucid', 'precise'] + special_for_arm:
+  if platform in special_for_arm:
+    arch_variants = ['arm']
+  else:
+    arch_variants = ['', '32', '64', 'arm']
+  for arch in arch_variants:
     arch_flags = ''
     real_arch = arch
     arch_part = '-' + arch
-    if arch == 'arm':
+    if arch == 'arm' or platform == 'win32':
       arch_flags += ' --no-gyp'
     if arch == '':
       arch_part = ''
@@ -249,18 +242,18 @@ for platform in [
       for libc in ['newlib', 'glibc']:
         # Buildbots.
         for bare in ['', '-bare']:
-          BOT_ASSIGNMENT.update({
-            platform + arch_part + bare + '-' + libc + '-' + mode:
-            python + ' buildbot/buildbot_standard.py ' +
-            mode + ' ' + real_arch + ' ' + libc + arch_flags
-          })
+          name = platform + arch_part + bare + '-' + libc + '-' + mode
+          assert name not in BOT_ASSIGNMENT, name
+          BOT_ASSIGNMENT[name] = (
+              python + ' buildbot/buildbot_standard.py ' +
+              mode + ' ' + real_arch + ' ' + libc + arch_flags)
         # Trybots
         for arch_sep in ['', '-', '_']:
-          BOT_ASSIGNMENT.update({
-            'nacl-' + platform + arch_sep + arch + '_' + libc + '_' + mode:
-            python + ' buildbot/buildbot_standard.py ' +
-            mode + ' ' + real_arch + ' ' + libc + arch_flags
-          })
+          name = 'nacl-' + platform + arch_sep + arch + '_' + libc + '_' + mode
+          assert name not in BOT_ASSIGNMENT, name
+          BOT_ASSIGNMENT[name] = (
+              python + ' buildbot/buildbot_standard.py ' +
+              mode + ' ' + real_arch + ' ' + libc + arch_flags)
 
 
 IRT_ARCHIVE_BUILDERS = [

commit 433de79d19fb3a633054153a8eb105aef5341550

Disable building with GYP on win32 bots. This was broken in r10959.
Building via GYP always builds all toolchains (i.e. newlib, glibc, pnacl) by
default, but the win32 XP pnacl builds are pathologically slow (~38 seconds per compile on
the nacl-win32_glibc_opt trybot).

We could just disable pnacl, but we have Windows GYP testing on other bots --
disabling it for win32 is not reducing test coverage much.

BUG=none
R=jvoung@chromium.org

Review URL: https://codereview.chromium.org/18901005

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11743 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: 67412179f5c8edc74971ef035dd2ad13de50dda5

buildbot/buildbot_selector.py
@@ -239,7 +239,14 @@ for platform in [
     arch_flags = ''
     real_arch = arch
     arch_part = '-' + arch
-    if arch == 'arm' or platform == 'win32':
+    # Disable GYP build for win32 bots and arm cross-builders. In this case
+    # "win" means Windows XP, not Vista, Windows 7, etc.
+    #
+    # Building via GYP always builds all toolchains by default, but the win32
+    # XP pnacl builds are pathologically slow (e.g. ~38 seconds per compile on
+    # the nacl-win32_glibc_opt trybot). There are other builders that test
+    # Windows builds via gyp, so the reduced test coverage should be slight.
+    if arch == 'arm' or (platform == 'win' and arch == '32'):
       arch_flags += ' --no-gyp'
     if arch == '':
       arch_part = ''