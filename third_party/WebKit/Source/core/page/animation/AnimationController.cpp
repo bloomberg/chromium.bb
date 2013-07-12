547a669555cc5f5286e12e13ef445f755dabcc7a
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-12 23:06:02 +0000

    Dont try to schedule animation when the frame has been detached.
    No new tests. Depends on a race which is difficult to test. Will monitor canary.
    
    TBR=steveblock
    BUG=259447
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/18683005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154147 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 8de94cd8cef041693bd1f51819638bfdb2e1ddfe

--- third_party/WebKit/Source/core/page/animation/AnimationController.cpp
+++ third_party/WebKit/Source/core/page/animation/AnimationController.cpp
@@ -144,6 +144,9 @@ void AnimationControllerPrivate::scheduleService()
 
 void AnimationControllerPrivate::scheduleService(double timeToNextService, double timeToNextEvent)
 {
+    if (!m_frame->page())
+        return;
+
     bool visible = m_frame->page()->visibilityState() == WebCore::PageVisibilityStateVisible;
 
     if (!visible)
