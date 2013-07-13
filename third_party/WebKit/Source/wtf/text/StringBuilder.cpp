858a611121a68294e10afc5fd72ec7796e06cb8e
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-13 00:28:08 +0000

    Fix name in StringBuilder
    We merged a slightly out-of-date version of
    http://trac.webkit.org/changeset/152306 that missed a last-minute name change.
    
    R=esprehn
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/19116002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154151 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: bc22e9880d883f13b0278cfa8b429ae3de422f50

--- third_party/WebKit/Source/wtf/text/StringBuilder.cpp
+++ third_party/WebKit/Source/wtf/text/StringBuilder.cpp
@@ -32,7 +32,7 @@
 
 namespace WTF {
 
-static size_t expandCapacity(size_t capacity, size_t newLength)
+static size_t expandedCapacity(size_t capacity, size_t newLength)
 {
     static const size_t minimumCapacity = 16;
     return std::max(capacity, std::max(minimumCapacity, newLength * 2));
@@ -227,10 +227,10 @@ CharType* StringBuilder::appendUninitializedSlow(unsigned requiredLength)
         // If the buffer is valid it must be at least as long as the current builder contents!
         ASSERT(m_buffer->length() >= m_length);
         
-        reallocateBuffer<CharType>(expandCapacity(capacity(), requiredLength));
+        reallocateBuffer<CharType>(expandedCapacity(capacity(), requiredLength));
     } else {
         ASSERT(m_string.length() == m_length);
-        allocateBuffer(m_length ? m_string.getCharacters<CharType>() : 0, expandCapacity(capacity(), requiredLength));
+        allocateBuffer(m_length ? m_string.getCharacters<CharType>() : 0, expandedCapacity(capacity(), requiredLength));
     }
     
     CharType* result = getBufferCharacters<CharType>() + m_length;
@@ -261,10 +261,10 @@ void StringBuilder::append(const UChar* characters, unsigned length)
             // If the buffer is valid it must be at least as long as the current builder contents!
             ASSERT(m_buffer->length() >= m_length);
             
-            allocateBufferUpConvert(m_buffer->characters8(), expandCapacity(capacity(), requiredLength));
+            allocateBufferUpConvert(m_buffer->characters8(), expandedCapacity(capacity(), requiredLength));
         } else {
             ASSERT(m_string.length() == m_length);
-            allocateBufferUpConvert(m_string.isNull() ? 0 : m_string.characters8(), expandCapacity(capacity(), requiredLength));
+            allocateBufferUpConvert(m_string.isNull() ? 0 : m_string.characters8(), expandedCapacity(capacity(), requiredLength));
         }
 
         memcpy(m_bufferCharacters16 + m_length, characters, static_cast<size_t>(length) * sizeof(UChar));        
