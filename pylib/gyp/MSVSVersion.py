bbefcd36c9c4f5158d93cb47b0e712f2f4ca940f
scottmg@chromium.org <scottmg@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2013-07-15 21:47:02 +0000

    Add Visual Studio 2013 support
    Copy of VS 2012. This code is increasingly icky.
    
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/19283003
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1676 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/MSVSVersion.py
+++ pylib/gyp/MSVSVersion.py
@@ -197,6 +197,24 @@ def _CreateVersion(name, path, sdk_based=False):
   if path:
     path = os.path.normpath(path)
   versions = {
+      '2013': VisualStudioVersion('2013',
+                                  'Visual Studio 2013',
+                                  solution_version='13.00',
+                                  project_version='4.0',
+                                  flat_sln=False,
+                                  uses_vcxproj=True,
+                                  path=path,
+                                  sdk_based=sdk_based,
+                                  default_toolset='v110'),
+      '2013e': VisualStudioVersion('2013e',
+                                   'Visual Studio 2013',
+                                   solution_version='13.00',
+                                   project_version='4.0',
+                                   flat_sln=True,
+                                   uses_vcxproj=True,
+                                   path=path,
+                                   sdk_based=sdk_based,
+                                   default_toolset='v110'),
       '2012': VisualStudioVersion('2012',
                                   'Visual Studio 2012',
                                   solution_version='12.00',
@@ -288,10 +306,16 @@ def _DetectVisualStudioVersions(versions_to_check, force_express):
       2008(e) - Visual Studio 2008 (9)
       2010(e) - Visual Studio 2010 (10)
       2012(e) - Visual Studio 2012 (11)
+      2013(e) - Visual Studio 2013 (11)
     Where (e) is e for express editions of MSVS and blank otherwise.
   """
   version_to_year = {
-      '8.0': '2005', '9.0': '2008', '10.0': '2010', '11.0': '2012'}
+      '8.0': '2005',
+      '9.0': '2008',
+      '10.0': '2010',
+      '11.0': '2012',
+      '12.0': '2013',
+  }
   versions = []
   for version in versions_to_check:
     # Old method of searching for which VS version is installed
@@ -354,6 +378,8 @@ def SelectVisualStudioVersion(version='auto'):
     '2010e': ('10.0',),
     '2012': ('11.0',),
     '2012e': ('11.0',),
+    '2013': ('12.0',),
+    '2013e': ('12.0',),
   }
   override_path = os.environ.get('GYP_MSVS_OVERRIDE_PATH')
   if override_path:
