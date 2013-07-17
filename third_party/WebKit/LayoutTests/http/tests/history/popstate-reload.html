bloomberg/chromium.bb

commit 3718b48df4d70a784033e747b46fd03de1664cc3

Simply FrameLoaderStateMachine and FrameLoader::transitionToCommitted().
This removes 2 cases from FrameLoaderStateMachine. FirstLayoutDone is
closely approximated by FrameView, and DisplayingInitialEmptyDocumentPostCommit
appears to be a workaround for declaring the first real load committed a bit
too early.

BUG=none
TEST=none

Review URL: https://chromiumcodereview.appspot.com/14787005

git-svn-id: svn://svn.chromium.org/blink/trunk@149766 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: f29de250732ff24c1fe0a79f5a2ea8f16bccbae1

third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -501,7 +501,7 @@ void FrameLoader::didExplicitOpen()
 
     // Calling document.open counts as committing the first real document load.
     if (!m_stateMachine.committedFirstRealDocumentLoad())
-        m_stateMachine.advanceTo(FrameLoaderStateMachine::DisplayingInitialEmptyDocumentPostCommit);
+        m_stateMachine.advanceTo(FrameLoaderStateMachine::CommittedFirstRealLoad);
     
     // Prevent window.open(url) -- eg window.open("about:blank") -- from blowing away results
     // from a subsequent window.document.open / window.document.write call. 
@@ -564,9 +564,6 @@ void FrameLoader::clear(bool clearWindowProperties, bool clearScriptObjects, boo
     m_checkTimer.stop();
     m_shouldCallCheckCompleted = false;
     m_shouldCallCheckLoadComplete = false;
-
-    if (m_stateMachine.isDisplayingInitialEmptyDocument() && m_stateMachine.committedFirstRealDocumentLoad())
-        m_stateMachine.advanceTo(FrameLoaderStateMachine::CommittedFirstRealLoad);
 }
 
 void FrameLoader::receivedFirstData()
@@ -610,10 +607,8 @@ void FrameLoader::didBeginDocument(bool dispatch)
     m_didCallImplicitClose = false;
     m_frame->document()->setReadyState(Document::Loading);
 
-    if (m_pendingStateObject) {
-        m_frame->document()->statePopped(m_pendingStateObject.get());
-        m_pendingStateObject.clear();
-    }
+    if (history()->currentItem())
+        m_frame->document()->statePopped(history()->currentItem()->stateObject());
 
     if (dispatch)
         dispatchDidClearWindowObjectsInAllWorlds();
@@ -918,8 +913,6 @@ void FrameLoader::handleFallbackContent()
 
 void FrameLoader::provisionalLoadStarted()
 {
-    if (m_stateMachine.firstLayoutDone())
-        m_stateMachine.advanceTo(FrameLoaderStateMachine::CommittedFirstRealLoad);
     m_frame->navigationScheduler()->cancel();
     m_quickRedirectComing = false;
 }
@@ -1047,7 +1040,6 @@ void FrameLoader::prepareForHistoryNavigation()
         frame()->page()->backForward()->setCurrentItem(currentItem.get());
 
         ASSERT(stateMachine()->isDisplayingInitialEmptyDocument());
-        stateMachine()->advanceTo(FrameLoaderStateMachine::DisplayingInitialEmptyDocumentPostCommit);
         stateMachine()->advanceTo(FrameLoaderStateMachine::CommittedFirstRealLoad);
     }
 }
@@ -1563,8 +1555,6 @@ void FrameLoader::transitionToCommitted()
             scrollAnimator->cancelAnimations();
     }
 
-    history()->updateForCommit();
-
     // The call to closeURL() invokes the unload event handler, which can execute arbitrary
     // JavaScript. If the script initiates a new load, we need to abandon the current load,
     // or the two will stomp each other.
@@ -1591,66 +1581,8 @@ void FrameLoader::transitionToCommitted()
     if (isLoadingMainFrame())
         m_frame->page()->chrome()->client()->needTouchEvents(false);
 
-    // Handle adding the URL to the back/forward list.
-    DocumentLoader* dl = m_documentLoader.get();
-
-    switch (m_loadType) {
-        case FrameLoadTypeForward:
-        case FrameLoadTypeBack:
-        case FrameLoadTypeIndexedBackForward:
-            if (m_frame->page()) {
-                // If the first load within a frame is a navigation within a back/forward list that was attached
-                // without any of the items being loaded then we need to update the history in a similar manner as
-                // for a standard load with the exception of updating the back/forward list (<rdar://problem/8091103>).
-                if (!m_stateMachine.committedFirstRealDocumentLoad() && isLoadingMainFrame())
-                    history()->updateForStandardLoad(HistoryController::UpdateAllExceptBackForwardList);
-
-                history()->updateForBackForwardNavigation();
-
-                if (history()->currentItem())
-                    m_pendingStateObject = history()->currentItem()->stateObject();
-
-                // Create a document view for this document.
-                m_client->transitionToCommittedForNewPage();
-            }
-            break;
-
-        case FrameLoadTypeReload:
-        case FrameLoadTypeReloadFromOrigin:
-        case FrameLoadTypeSame:
-        case FrameLoadTypeReplace:
-            history()->updateForReload();
-            m_client->transitionToCommittedForNewPage();
-            break;
-
-        case FrameLoadTypeStandard:
-            history()->updateForStandardLoad();
-            if (m_frame->view())
-                m_frame->view()->setScrollbarsSuppressed(true);
-            m_client->transitionToCommittedForNewPage();
-            break;
-
-        case FrameLoadTypeRedirectWithLockedBackForwardList:
-            history()->updateForRedirectWithLockedBackForwardList();
-            m_client->transitionToCommittedForNewPage();
-            break;
-
-        // FIXME Remove this check when dummy ds is removed (whatever "dummy ds" is).
-        // An exception should be thrown if we're in the FrameLoadTypeUninitialized state.
-        default:
-            ASSERT_NOT_REACHED();
-    }
-
-    m_documentLoader->writer()->setMIMEType(dl->responseMIMEType());
-
-    // Tell the client we've committed this URL.
-    ASSERT(m_frame->view());
-
-    if (m_stateMachine.creatingInitialEmptyDocument())
-        return;
-
-    if (!m_stateMachine.committedFirstRealDocumentLoad())
-        m_stateMachine.advanceTo(FrameLoaderStateMachine::DisplayingInitialEmptyDocumentPostCommit);
+    history()->updateForCommit();
+    m_client->transitionToCommittedForNewPage();
 }
 
 void FrameLoader::clientRedirectCancelledOrFinished()
@@ -1931,20 +1863,12 @@ void FrameLoader::didFirstLayout()
 {
     if (m_frame->page() && isBackForwardLoadType(m_loadType))
         history()->restoreScrollPositionAndViewState();
-
-    if (m_stateMachine.committedFirstRealDocumentLoad() && !m_stateMachine.isDisplayingInitialEmptyDocument() && !m_stateMachine.firstLayoutDone())
-        m_stateMachine.advanceTo(FrameLoaderStateMachine::FirstLayoutDone);
 }
 
 void FrameLoader::frameLoadCompleted()
 {
     // Note: Can be called multiple times.
     history()->updateForFrameLoadCompleted();
-
-    // After a canceled provisional load, firstLayoutDone is false.
-    // Reset it to true if we're displaying a page.
-    if (m_documentLoader && m_stateMachine.committedFirstRealDocumentLoad() && !m_stateMachine.isDisplayingInitialEmptyDocument() && !m_stateMachine.firstLayoutDone())
-        m_stateMachine.advanceTo(FrameLoaderStateMachine::FirstLayoutDone);
 }
 
 void FrameLoader::detachChildren()
@@ -2861,7 +2785,6 @@ void FrameLoader::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
     info.addMember(m_documentLoader, "documentLoader");
     info.addMember(m_provisionalDocumentLoader, "provisionalDocumentLoader");
     info.addMember(m_policyDocumentLoader, "policyDocumentLoader");
-    info.addMember(m_pendingStateObject, "pendingStateObject");
     info.addMember(m_submittedFormURL, "submittedFormURL");
     info.addMember(m_checkTimer, "checkTimer");
     info.addMember(m_opener, "opener");

third_party/WebKit/Source/core/loader/DocumentLoader.cpp
@@ -292,6 +292,7 @@ void DocumentLoader::commitIfReady()
     if (!m_committed) {
         m_committed = true;
         frameLoader()->commitProvisionalLoad();
+        m_writer.setMIMEType(m_response.mimeType());
     }
 }
 
@@ -646,6 +647,8 @@ void DocumentLoader::commitData(const char* bytes, size_t length)
 
         if (frameLoader()->stateMachine()->creatingInitialEmptyDocument())
             return;
+        if (frameLoader()->stateMachine()->isDisplayingInitialEmptyDocument())
+            frameLoader()->stateMachine()->advanceTo(FrameLoaderStateMachine::CommittedFirstRealLoad);
         
         // The origin is the MHTML file, we need to set the base URL to the document encoded in the MHTML so
         // relative URLs are resolved properly.

third_party/WebKit/Source/core/loader/FrameLoader.h
@@ -389,8 +389,6 @@ private:
     PageDismissalType m_pageDismissalEventBeingDispatched;
     bool m_isComplete;
 
-    RefPtr<SerializedScriptValue> m_pendingStateObject;
-
     bool m_needsClear;
 
     KURL m_submittedFormURL;

third_party/WebKit/Source/core/loader/FrameLoaderStateMachine.cpp
@@ -46,7 +46,7 @@ bool FrameLoaderStateMachine::committingFirstRealLoad() const
 
 bool FrameLoaderStateMachine::committedFirstRealDocumentLoad() const 
 {
-    return m_state >= DisplayingInitialEmptyDocumentPostCommit;
+    return m_state >= CommittedFirstRealLoad;
 }
 
 bool FrameLoaderStateMachine::creatingInitialEmptyDocument() const 
@@ -56,17 +56,12 @@ bool FrameLoaderStateMachine::creatingInitialEmptyDocument() const
 
 bool FrameLoaderStateMachine::isDisplayingInitialEmptyDocument() const 
 {
-    return m_state == DisplayingInitialEmptyDocument || m_state == DisplayingInitialEmptyDocumentPostCommit;
-}
-
-bool FrameLoaderStateMachine::firstLayoutDone() const
-{
-    return m_state == FirstLayoutDone;
+    return m_state == DisplayingInitialEmptyDocument;
 }
 
 void FrameLoaderStateMachine::advanceTo(State state)
 {
-    ASSERT(State(m_state + 1) == state || (firstLayoutDone() && state == CommittedFirstRealLoad));
+    ASSERT(State(m_state + 1) == state);
     m_state = state;
 }
 

third_party/WebKit/Source/core/loader/FrameLoaderStateMachine.h
@@ -46,16 +46,13 @@ public:
     enum State {
         CreatingInitialEmptyDocument,
         DisplayingInitialEmptyDocument,
-        DisplayingInitialEmptyDocumentPostCommit,
-        CommittedFirstRealLoad,
-        FirstLayoutDone
+        CommittedFirstRealLoad
     };
 
     bool committingFirstRealLoad() const;
     bool committedFirstRealDocumentLoad() const;
     bool creatingInitialEmptyDocument() const;
     bool isDisplayingInitialEmptyDocument() const;
-    bool firstLayoutDone() const;
     void advanceTo(State);
 
 private:

third_party/WebKit/Source/core/loader/HistoryController.cpp
@@ -308,7 +308,7 @@ void HistoryController::updateForReload()
 //     1) Back/forward: The m_currentItem is part of this mechanism.
 //     2) Global history: Handled by the client.
 //
-void HistoryController::updateForStandardLoad(HistoryUpdateType updateType)
+void HistoryController::updateForStandardLoad()
 {
     LOG(History, "WebCoreHistory: Updating History for Standard Load in frame %s", m_frame->loader()->documentLoader()->url().string().ascii().data());
 
@@ -317,10 +317,8 @@ void HistoryController::updateForStandardLoad(HistoryUpdateType updateType)
     const KURL& historyURL = frameLoader->documentLoader()->urlForHistory();
 
     if (!frameLoader->documentLoader()->isClientRedirect()) {
-        if (!historyURL.isEmpty()) {
-            if (updateType != UpdateAllExceptBackForwardList)
-                updateBackForwardListClippedAtTarget(true);
-        }
+        if (!historyURL.isEmpty())
+            updateBackForwardListClippedAtTarget(true);
     } else {
         // The client redirect replaces the current history item.
         updateCurrentItem();
@@ -375,7 +373,7 @@ void HistoryController::updateForCommit()
     FrameLoadType type = frameLoader->loadType();
     if (isBackForwardLoadType(type)
         || isReplaceLoadTypeWithProvisionalItem(type)
-        || (isReloadTypeWithProvisionalItem(type) && !frameLoader->provisionalDocumentLoader()->unreachableURL().isEmpty())) {
+        || (isReloadTypeWithProvisionalItem(type) && !frameLoader->documentLoader()->unreachableURL().isEmpty())) {
         // Once committed, we want to use current item for saving DocState, and
         // the provisional item for restoring state.
         // Note previousItem must be set before we close the URL, which will
@@ -392,6 +390,28 @@ void HistoryController::updateForCommit()
         Page* page = m_frame->page();
         ASSERT(page);
         page->mainFrame()->loader()->history()->recursiveUpdateForCommit();
+    }
+
+    switch (type) {
+        case FrameLoadTypeForward:
+        case FrameLoadTypeBack:
+        case FrameLoadTypeIndexedBackForward:
+            updateForBackForwardNavigation();
+            return;
+        case FrameLoadTypeReload:
+        case FrameLoadTypeReloadFromOrigin:
+        case FrameLoadTypeSame:
+        case FrameLoadTypeReplace:
+            updateForReload();
+            return;
+        case FrameLoadTypeStandard:
+            updateForStandardLoad();
+            return;
+        case FrameLoadTypeRedirectWithLockedBackForwardList:
+            updateForRedirectWithLockedBackForwardList();
+            return;
+        default:
+            ASSERT_NOT_REACHED();
     }
 }
 

third_party/WebKit/Source/core/loader/HistoryController.h
@@ -46,8 +46,6 @@ class StringWithDirection;
 class HistoryController {
     WTF_MAKE_NONCOPYABLE(HistoryController);
 public:
-    enum HistoryUpdateType { UpdateAll, UpdateAllExceptBackForwardList };
-
     explicit HistoryController(Frame*);
     ~HistoryController();
 
@@ -61,10 +59,6 @@ public:
     void saveDocumentAndScrollState();
     void restoreDocumentState();
 
-    void updateForBackForwardNavigation();
-    void updateForReload();
-    void updateForStandardLoad(HistoryUpdateType updateType = UpdateAll);
-    void updateForRedirectWithLockedBackForwardList();
     void updateForClientRedirect();
     void updateForCommit();
     void updateForSameDocumentNavigation();
@@ -93,6 +87,11 @@ private:
     void initializeItem(HistoryItem*);
     PassRefPtr<HistoryItem> createItem();
     PassRefPtr<HistoryItem> createItemTree(Frame* targetFrame, bool clipAtTarget);
+
+    void updateForBackForwardNavigation();
+    void updateForReload();
+    void updateForStandardLoad();
+    void updateForRedirectWithLockedBackForwardList();
 
     void recursiveSetProvisionalItem(HistoryItem*, HistoryItem*, FrameLoadType);
     void recursiveGoToItem(HistoryItem*, HistoryItem*, FrameLoadType);

third_party/WebKit/Source/core/loader/ProgressTracker.cpp
@@ -30,8 +30,8 @@
 #include "core/loader/DocumentLoader.h"
 #include "core/loader/FrameLoader.h"
 #include "core/loader/FrameLoaderClient.h"
-#include "core/loader/FrameLoaderStateMachine.h"
 #include "core/page/Frame.h"
+#include "core/page/FrameView.h"
 #include "core/platform/Logging.h"
 #include "core/platform/network/ResourceResponse.h"
 #include <wtf/CurrentTime.h>
@@ -196,7 +196,7 @@ void ProgressTracker::incrementProgress(unsigned long identifier, const char*, i
         percentOfRemainingBytes = 1.0;
     
     // For documents that use WebCore's layout system, treat first layout as the half-way point.
-    bool useClampedMaxProgress = !frame->loader()->stateMachine()->firstLayoutDone();
+    bool useClampedMaxProgress = !frame->view()->didFirstLayout();
     double maxProgressValue = useClampedMaxProgress ? 0.5 : finalProgressValue;
     increment = (maxProgressValue - m_progressValue) * percentOfRemainingBytes;
     m_progressValue += increment;

commit 4227fd17204b247cbe11cccdda6d0076e15fc61f

Don't provide a state object to initial popstate events unless the load was back/forward.
This was regressed in r149766, when we started providing a state object if one
existed for all loads.

BUG=257995
TEST=http/tests/history/popstate-reload.html

Review URL: https://chromiumcodereview.appspot.com/19429004

git-svn-id: svn://svn.chromium.org/blink/trunk@154356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: e480b2a18acb4faf003c22d464db0e70909bf64a

third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -526,7 +526,7 @@ void FrameLoader::didBeginDocument(bool dispatch)
     m_didCallImplicitClose = false;
     m_frame->document()->setReadyState(Document::Loading);
 
-    if (history()->currentItem())
+    if (history()->currentItem() && m_loadType == FrameLoadTypeBackForward)
         m_frame->document()->statePopped(history()->currentItem()->stateObject());
 
     if (dispatch)

third_party/WebKit/LayoutTests/http/tests/history/popstate-reload-expected.txt
@@ -1,0 +1,2 @@
+Tests that popstate events do not fire when reloading.
+PASS

third_party/WebKit/LayoutTests/http/tests/history/popstate-reload.html
@@ -1,0 +1,34 @@
+<html>
+<body>
+Tests that popstate events do not fire when reloading.<br>
+<div id="console"></div>
+<script>
+if (window.testRunner) {
+    testRunner.waitUntilDone();
+    testRunner.dumpAsText();
+}
+
+popStateCalled = false;
+onpopstate = function(event)
+{
+    if (event.state)
+        popStateCalled = true;
+}
+
+function finish() {
+    document.getElementById("console").appendChild(document.createTextNode(popStateCalled ? "FAIL" : "PASS"));
+    if (window.testRunner)
+        testRunner.notifyDone();
+}
+
+onload = function() {
+    if (location.hash != "") {
+        setTimeout(finish, 0);
+        return;
+    }
+    history.pushState('newState1', null, '#newState1');
+    location.reload();
+}
+</script>
+</body>
+</html>