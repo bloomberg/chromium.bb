a8001f5721a194fa721b74121298b052cb8249fc
cgrimm@chromium.org <cgrimm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 21:37:00 +0000

    [i-spy] Added the rendering_test_manager utility, which provides convenience methods for interacting with i-spy Tests and Results in Google Cloud Storage.
    BUG=260418
    
    Review URL: https://chromiumcodereview.appspot.com/18371006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212436 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 909f5c42b086a8388abbee04049e0f44091fef6d

---
+++ chrome/test/functional/ispy/ispy_core/tools/rendering_test_manager.py
@@ -0,0 +1,134 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Utilities for managing files in Google Cloud Storage."""
+
+from tools import image_tools
+import collections
+import posixpath
+
+
+class RenderingTestManager(object):
+  """The manager from which GCS utility functions are called."""
+
+  def __init__(self, cloud_bucket):
+    """Initialize with a cloud_bucket subclass to supply GCS functionality.
+
+    Args:
+      cloud_bucket: A subclass of cloud_bucket.CloudBucket.
+
+    Returns:
+      An instance of Manager.
+    """
+    self.cloud_bucket = cloud_bucket
+
+  def UploadImage(self, full_path, image):
+    """Uploads an image to a location in GCS.
+
+    Args:
+      full_path: the path to the file in GCS including the file extension.
+      image: a RGB PIL.Image to be uploaded.
+    """
+    self.cloud_bucket.UploadFile(
+        full_path, image_tools.SerializeImage(image), 'image/png')
+
+  def DownloadImage(self, full_path):
+    """Downloads an image from a location in GCS.
+
+    Args:
+      full_path: the path to the file in GCS including the file extension.
+
+    Returns:
+      The downloaded RGB PIL.Image.
+
+    Raises:
+      cloud_bucket.NotFoundError: if the path to the image is not valid.
+    """
+    return image_tools.DeserializeImage(
+        self.cloud_bucket.DownloadFile(full_path))
+
+  def UploadTest(self, test_name, images):
+    """Creates and uploads a test to GCS from a set of images and name.
+
+    This method generates a mask from the uploaded images, then
+      uploads the mask and first of the images to GCS as a test.
+
+    Args:
+      test_name: the name of the test.
+      images: a list of RGB encoded PIL.Images
+    """
+    path = posixpath.join('tests', test_name)
+    mask = image_tools.CreateMask(images)
+    self.UploadImage(posixpath.join(path, 'expected.png'), images[0])
+    self.UploadImage(posixpath.join(path, 'mask.png'), mask)
+
+  def RunTest(self, test_name, run_name, actual):
+    """Runs an image comparison, and uploads discrepancies to GCS.
+
+    Args:
+      test_name: the name of the test to run.
+      run_name: the name of this run of the test.
+      actual: an RGB-encoded PIL.Image that is the actual result of the
+        test.
+
+    Raises:
+      cloud_bucket.NotFoundError: if the given test_name is not found.
+    """
+    path = posixpath.join('failures', test_name, run_name)
+    test = self.GetTest(test_name)
+    if not image_tools.SameImage(actual, test['expected']):
+      self.UploadImage(posixpath.join(path, 'actual.png'), actual)
+
+  def GetTest(self, test_name):
+    """Returns given test from GCS.
+
+    Args:
+      test_name: the name of the test to get from GCS.
+
+    Returns:
+      A named tuple: 'Test', containing two images: expected and mask.
+
+    Raises:
+      cloud_bucket.NotFoundError: if the test is not found in GCS.
+    """
+    path = posixpath.join('tests', test_name)
+    Test = collections.namedtuple('Test', ['expected', 'mask'])
+    return Test(self.DownloadImage(posixpath.join(path, 'expected.png')),
+                self.DownloadImage(posixpath.join(path, 'mask.png')))
+
+  def GetFailure(self, test_name, run_name):
+    """Returns a given test failure's expected, diff, and actual images.
+
+    Args:
+      test_name: the name of the test the result corresponds to.
+      run_name: the name of the result on the given test.
+
+    Returns:
+      A named tuple: Failure containing three images: expected, diff, and
+        actual.
+
+    Raises:
+      cloud_bucket.NotFoundError: if the result is not found in GCS.
+    """
+    test_path = posixpath.join('tests', test_name)
+    failure_path = posixpath.join('failures', test_name, run_name)
+    expected = self.DownloadImage(posixpath.join(test_path, 'expected.png'))
+    mask = self.DownloadImage(posixpath.join(test_path, 'mask.png'))
+    actual = self.DownloadImage(posixpath.join(failure_path, 'actual.png'))
+    diff = image_tools.VisualizeImageDifferences(
+        expected, actual, mask=mask)
+    Failure = collections.namedtuple('Failure', ['expected', 'diff', 'actual'])
+    return Failure(expected, diff, actual)
+
+  def GetAllPaths(self, prefix):
+    """Gets urls to all files in GCS whose path starts with a given prefix.
+
+    Args:
+      prefix: the prefix to filter files in GCS by.
+
+    Returns:
+      a list containing urls to all objects that started with
+         the prefix.
+    """
+    return self.cloud_bucket.GetAllPaths(prefix)
---
+++ chrome/test/functional/ispy/ispy_core/tools/rendering_test_manager_unittest.py
@@ -0,0 +1,154 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from PIL import Image
+import unittest
+import sets
+
+from tests.rendering_test_manager import mock_cloud_bucket
+from tests.rendering_test_manager import cloud_bucket
+from tools import rendering_test_manager
+from tools import image_tools
+
+
+class BucketRenderingTestManagerUnitTest(unittest.TestCase):
+
+  def setUp(self):
+    # Set up structures that will be reused throughout testing.
+    self.bucket = mock_cloud_bucket.MockCloudBucket()
+    self.manager = rendering_test_manager.RenderingTestManager(self.bucket)
+    self.white = Image.new('RGB', (25, 25), (255, 255, 255))
+    self.red = Image.new('RGB', (25, 25), (255, 0, 0))
+    self.black = Image.new('RGB', (25, 25), (0, 0, 0))
+
+  def testUploadImage(self):
+    self.bucket.Reset()
+    # Upload some images to the datastore.
+    self.manager.UploadImage('path/to/white.png', self.white)
+    self.manager.UploadImage('path/to/black.png', self.black)
+    self.manager.UploadImage('path/to/red.png', self.red)
+    # Confirm that the images actually got uploaded.
+    self.assertEquals(self.bucket.datastore['path/to/white.png'],
+                      image_tools.SerializeImage(self.white))
+    self.assertEquals(self.bucket.datastore['path/to/black.png'],
+                      image_tools.SerializeImage(self.black))
+    self.assertEquals(self.bucket.datastore['path/to/red.png'],
+                      image_tools.SerializeImage(self.red))
+
+  def testDownloadImage(self):
+    self.bucket.Reset()
+    # Upload some images to the datastore.
+    self.manager.UploadImage('path/to/white.png', self.white)
+    self.manager.UploadImage('path/to/black.png', self.black)
+    self.manager.UploadImage('path/to/red.png', self.red)
+    # Check that the DownloadImage function gets the correct images.
+    self.assertEquals(
+        image_tools.SerializeImage(
+            self.manager.DownloadImage('path/to/white.png')),
+        image_tools.SerializeImage(self.white))
+    self.assertEquals(
+        image_tools.SerializeImage(
+            self.manager.DownloadImage('path/to/black.png')),
+        image_tools.SerializeImage(self.black))
+    self.assertEquals(
+        image_tools.SerializeImage(
+            self.manager.DownloadImage('path/to/red.png')),
+        image_tools.SerializeImage(self.red))
+    # Check that the DownloadImage function throws an error for a
+    #  nonexistant image.
+    self.assertRaises(cloud_bucket.FileNotFoundError,
+                      self.manager.DownloadImage,
+                      'path/to/yellow.png')
+
+  def testUploadTest(self):
+    self.bucket.Reset()
+    # Upload some tests to the datastore.
+    self.manager.UploadTest('test1', [self.white, self.black])
+    self.manager.UploadTest('test2', [self.black, self.black])
+    # Confirm that the tests were successfully uploaded.
+    self.assertEquals(self.bucket.datastore['tests/test1/expected.png'],
+                      image_tools.SerializeImage(self.white))
+    self.assertEquals(self.bucket.datastore['tests/test1/mask.png'],
+                      image_tools.SerializeImage(self.white))
+    self.assertEquals(self.bucket.datastore['tests/test2/expected.png'],
+                      image_tools.SerializeImage(self.black))
+    self.assertEquals(self.bucket.datastore['tests/test2/mask.png'],
+                      image_tools.SerializeImage(self.black))
+
+  def testRunTest(self):
+    self.bucket.Reset()
+
+    self.manager.UploadTest('test1', [self.red, self.red])
+    self.manager.RunTest('test1', 'run1', self.black)
+    self.assertEquals(
+        self.bucket.datastore['failures/test1/run1/actual.png'],
+        image_tools.SerializeImage(self.black))
+    self.manager.RunTest('test1', 'run2', self.red)
+    self.assertFalse(
+        self.bucket.datastore.has_key('failures/test1/run2/actual.png'))
+    self.assertRaises(cloud_bucket.FileNotFoundError, self.manager.RunTest,
+                      'test2', 'run1', self.black)
+
+  def testGetTest(self):
+    self.bucket.Reset()
+    # Upload some tests to the datastore
+    self.manager.UploadTest('test1', [self.white, self.black])
+    self.manager.UploadTest('test2', [self.red, self.white])
+    test1 = self.manager.GetTest('test1')
+    test2 = self.manager.GetTest('test2')
+    # Check that GetTest gets the appropriate tests.
+    self.assertEquals(image_tools.SerializeImage(test1.expected),
+                      image_tools.SerializeImage(self.white))
+    self.assertEquals(image_tools.SerializeImage(test1.mask),
+                      image_tools.SerializeImage(self.white))
+    self.assertEquals(image_tools.SerializeImage(test2.expected),
+                      image_tools.SerializeImage(self.red))
+    self.assertEquals(image_tools.SerializeImage(test2.mask),
+                      image_tools.SerializeImage(self.white))
+    # Check that GetTest throws an error for a nonexistant test.
+    self.assertRaises(
+        cloud_bucket.FileNotFoundError, self.manager.GetTest, 'test3')
+
+  def testGetFailure(self):
+    self.bucket.Reset()
+    # Upload a result
+    self.manager.UploadTest('test1', [self.red, self.red])
+    self.manager.RunTest('test1', 'run1', self.black)
+    res = self.manager.GetFailure('test1', 'run1')
+    # Check that the function correctly got the result.
+    self.assertEquals(image_tools.SerializeImage(res.expected),
+                      image_tools.SerializeImage(self.red))
+    self.assertEquals(image_tools.SerializeImage(res.diff),
+                      image_tools.SerializeImage(self.white))
+    self.assertEquals(image_tools.SerializeImage(res.actual),
+                      image_tools.SerializeImage(self.black))
+    # Check that the function raises an error when given non-existant results.
+    self.assertRaises(cloud_bucket.FileNotFoundError,
+                      self.manager.GetFailure,
+                      'test1', 'run2')
+    self.assertRaises(cloud_bucket.FileNotFoundError,
+                      self.manager.GetFailure,
+                      'test2', 'run1')
+
+  def testGetAllPaths(self):
+    self.bucket.Reset()
+    # Upload some tests.
+    self.manager.UploadTest('test1', [self.white, self.black])
+    self.manager.UploadTest('test2', [self.red, self.white])
+    # Check that the function gets all urls matching the prefix.
+    self.assertEquals(
+        sets.Set(self.manager.GetAllPaths('tests/test')),
+        sets.Set(['tests/test1/expected.png',
+                  'tests/test1/mask.png',
+                  'tests/test2/expected.png',
+                  'tests/test2/mask.png']))
+    self.assertEquals(sets.Set(self.manager.GetAllPaths('tests/test1')),
+                      sets.Set(['tests/test1/expected.png',
+                                'tests/test1/mask.png']))
+    self.assertEquals(sets.Set(self.manager.GetAllPaths('tests/test3')),
+                      sets.Set())
+
+
+if __name__ == '__main__':
+  unittest.main()
