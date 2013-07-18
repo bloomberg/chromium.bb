b656a34b789af5d95a612ff6ba9102fb67d36d4f
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-18 02:45:46 +0000

    fix broken webkitpy unittest introduced in r154456.
    (missed a reference to the --replay option).
    
    TBR=tonyg@chromium.org, eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/19726002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154460 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0005df44cf8c144b121b8044baca5763f22c9862

--- third_party/WebKit/Tools/Scripts/webkitpy/performance_tests/perftestsrunner_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/performance_tests/perftestsrunner_unittest.py
@@ -118,7 +118,6 @@ class MainTest(unittest.TestCase):
         self.assertEqual(options.time_out_ms, 600 * 1000)
         self.assertTrue(options.generate_results)
         self.assertTrue(options.show_results)
-        self.assertFalse(options.replay)
         self.assertTrue(options.use_skipped_list)
         self.assertEqual(options.repeat, 1)
         self.assertEqual(options.test_runner_count, DEFAULT_TEST_RUNNER_COUNT)
