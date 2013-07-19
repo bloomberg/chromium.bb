ef7511b6e1666f4becd61e47c0d112a9153d1e37
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-07-19 18:00:49 +0000

    Don't abort on startup if the "filename" or "fdio" IRT interfaces are absent
    These interfaces don't do anything useful under Chromium, so their
    presence should not be mandatory.
    
    Tweak the query ordering: Query for "fdio" first so that, if "fdio" is
    available, later query failures can produce useful error messages via
    write().
    
    This is partially tested via tests/irt_compatibility by disabling the
    "filename" interface within this test.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3574
    TEST=run_irt_compatibility_test_irt
    
    Review URL: https://codereview.chromium.org/19487002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11815 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 632e5f7c9659ef215e51957c5f24925d9ba5f316

--- src/untrusted/nacl/nacl_irt.c
+++ src/untrusted/nacl/nacl_irt.c
@@ -12,13 +12,69 @@
 #include "native_client/src/untrusted/irt/irt_interfaces.h"
 #include "native_client/src/untrusted/nacl/nacl_irt.h"
 
+static int stub_open(const char *pathname, int oflag, mode_t cmode,
+                     int *newfd) {
+  return ENOSYS;
+}
+
+static int stub_stat(const char *pathname, struct stat *st) {
+  return ENOSYS;
+}
+
+static int stub_close(int fd) {
+  return ENOSYS;
+}
+
+static int stub_dup(int fd, int *newfd) {
+  return ENOSYS;
+}
+
+static int stub_dup2(int fd, int newfd) {
+  return ENOSYS;
+}
+
+static int stub_read(int fd, void *buf, size_t count, size_t *nread) {
+  return ENOSYS;
+}
+
+static int stub_write(int fd, const void *buf, size_t count, size_t *nwrote) {
+  return ENOSYS;
+}
+
+static int stub_seek(int fd, off_t offset, int whence, off_t *new_offset) {
+  return ENOSYS;
+}
+
+static int stub_fstat(int fd, struct stat *st) {
+  return ENOSYS;
+}
+
+static int stub_getdents(int fd, struct dirent *dirent, size_t count,
+                         size_t *nread) {
+  return ENOSYS;
+}
+
 struct nacl_irt_basic __libnacl_irt_basic;
-struct nacl_irt_fdio __libnacl_irt_fdio;
-struct nacl_irt_filename __libnacl_irt_filename;
 struct nacl_irt_memory __libnacl_irt_memory;
 struct nacl_irt_tls __libnacl_irt_tls;
 struct nacl_irt_clock __libnacl_irt_clock;
 struct nacl_irt_dev_getpid __libnacl_irt_dev_getpid;
+
+struct nacl_irt_filename __libnacl_irt_filename = {
+  stub_open,
+  stub_stat,
+};
+
+struct nacl_irt_fdio __libnacl_irt_fdio = {
+  stub_close,
+  stub_dup,
+  stub_dup2,
+  stub_read,
+  stub_write,
+  stub_seek,
+  stub_fstat,
+  stub_getdents,
+};
 
 TYPE_nacl_irt_query __nacl_irt_query;
 
@@ -108,9 +164,22 @@ void __libnacl_mandatory_irt_query(const char *interface,
 void __libnacl_irt_init(Elf32_auxv_t *auxv) {
   grok_auxv(auxv);
 
+  /*
+   * The "fdio" and "filename" interfaces don't do anything useful in
+   * Chromium (with the exception that write() sometimes produces
+   * useful debugging output for stdout/stderr), so don't abort if
+   * they are not available.
+   *
+   * We query for "fdio" early on so that write() can produce a useful
+   * debugging message if any other IRT queries fail.
+   */
+  __libnacl_irt_query(NACL_IRT_FDIO_v0_1, &__libnacl_irt_fdio,
+                      sizeof(__libnacl_irt_fdio));
+  __libnacl_irt_query(NACL_IRT_FILENAME_v0_1, &__libnacl_irt_filename,
+                      sizeof(__libnacl_irt_filename));
+
   DO_QUERY(NACL_IRT_BASIC_v0_1, basic);
-  DO_QUERY(NACL_IRT_FDIO_v0_1, fdio);
-  DO_QUERY(NACL_IRT_FILENAME_v0_1, filename);
+
   if (!__libnacl_irt_query(NACL_IRT_MEMORY_v0_3,
                            &__libnacl_irt_memory,
                            sizeof(__libnacl_irt_memory))) {
@@ -128,5 +197,6 @@ void __libnacl_irt_init(Elf32_auxv_t *auxv) {
     __libnacl_irt_memory.mmap = old_irt_memory.mmap;
     __libnacl_irt_memory.munmap = old_irt_memory.munmap;
   }
+
   DO_QUERY(NACL_IRT_TLS_v0_1, tls);
 }
