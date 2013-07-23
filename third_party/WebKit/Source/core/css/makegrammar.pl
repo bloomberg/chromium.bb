e05e0078dfefe09377495288ddd7c9a2fdf2e056
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-23 00:22:32 +0000

    Simplify makegrammar.pl
    This change removes the preprocessor and defines parameters from
    makegrammar.pl that were passed by gyp but didn't change the output of
    the script. This is because we don't have ENABLE in CSSGrammar.y.in.
        
    While touching the code, also greatly simplified the script based on
    the fact that only one code path was ever used (we only call the
    script on CSSGrammar.y.in).
        
    CSSGrammar.y is the same before and after this change.
    
    Review URL: https://chromiumcodereview.appspot.com/19792008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154700 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ff1dabd792f55f62ced87155f2d2f404f44749e8

--- third_party/WebKit/Source/core/css/makegrammar.pl
+++ third_party/WebKit/Source/core/css/makegrammar.pl
@@ -26,70 +26,33 @@ use File::Spec;
 use Getopt::Long;
 
 my $outputDir = ".";
-my $extraDefines = "";
-my $symbolsPrefix = "";
-my $preprocessor = "";
-my $preprocessOnly = 0;
-my $bison = "bison";
 
 GetOptions(
     'outputDir=s' => \$outputDir,
-    'extraDefines=s' => \$extraDefines,
-    'bison=s' => \$bison,
-    'preprocessor=s' => \$preprocessor,
-    'preprocessOnly' => \$preprocessOnly,
-    'symbolsPrefix=s' => \$symbolsPrefix
 );
 
 my $grammarFilePath = $ARGV[0];
 my $grammarIncludesFilePath = @ARGV > 0 ? $ARGV[1] : "";
 
-if (!length($symbolsPrefix) && !$preprocessOnly) {
-    die "Need a symbols prefix to give to bison (e.g. cssyy, xpathyy)";
-}
-
 my ($filename, $basePath, $suffix) = fileparse($grammarFilePath, (".y", ".y.in"));
 
-if ($suffix eq ".y.in") {
-    my $grammarFileOutPath = File::Spec->join($outputDir, "$filename.y");
-    if (!$grammarIncludesFilePath) {
-        $grammarIncludesFilePath = "${basePath}${filename}.y.includes";
-    }
-
-    open GRAMMAR, ">$grammarFileOutPath" or die;
-    open INCLUDES, "<$grammarIncludesFilePath" or die;
-
-    require preprocessor;
-
-    while (<INCLUDES>) {
-        print GRAMMAR;
-    }
-    print GRAMMAR join("", applyPreprocessor($grammarFilePath, $extraDefines, $preprocessor));
-    close GRAMMAR;
-
-    $grammarFilePath = $grammarFileOutPath;
-
-    exit if $preprocessOnly;
+my $grammarFileOutPath = File::Spec->join($outputDir, "$filename.y");
+if (!$grammarIncludesFilePath) {
+    $grammarIncludesFilePath = "${basePath}${filename}.y.includes";
 }
 
-my $fileBase = File::Spec->join($outputDir, $filename);
-system("$bison -d -p $symbolsPrefix $grammarFilePath -o $fileBase.cpp");
+open GRAMMAR, ">$grammarFileOutPath" or die;
+open INCLUDES, "<$grammarIncludesFilePath" or die;
 
-open HEADER, ">$fileBase.h" or die;
-print HEADER << "EOF";
-#ifndef CSSGRAMMAR_H
-#define CSSGRAMMAR_H
-EOF
-
-open HPP, "<$fileBase.cpp.h" or open HPP, "<$fileBase.hpp" or die;
-while (<HPP>) {
-    print HEADER;
+while (<INCLUDES>) {
+    print GRAMMAR;
 }
-close HPP;
 
-print HEADER "#endif\n";
-close HEADER;
+open GRAMMARFILE, "<$grammarFilePath" or die;
+while (<GRAMMARFILE>) {
+    print GRAMMAR;
+}
 
-unlink("$fileBase.cpp.h");
-unlink("$fileBase.hpp");
+close GRAMMAR;
 
+$grammarFilePath = $grammarFileOutPath;
