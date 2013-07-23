c3d3e63e3860f8d5700591c7acf62b7d0d0ddf04
pavan.e@samsung.com <pavan.e@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-23 20:05:57 +0000

    [SVG] Handle animation freeze when 'repeatDur' is not a multiple of 'dur'
    This logic originally existed in WebKit as 'if (fmod(repeatingDuration.value(), simpleDuration.value() == 0.))'.
    It was supposed to read `if (fmod(...) == 0)`, but the == slipped into the fmod call and unfortunately got removed later as it looked meaningless.
    This logic is meant for handling the scenario where 'repeatingDuration' is not a multiple of 'simpleDuration'.
    Modified this logic in a proper way.
    
    R=pdr@chromium.org, schenney@chromium.org, dschulze@chromium.org
    BUG=262917
    
    Review URL: https://chromiumcodereview.appspot.com/19946002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154777 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6d9a0a17f83926d5e41530b4a80119c210062329

--- third_party/WebKit/Source/core/svg/animation/SVGSMILElement.cpp
+++ third_party/WebKit/Source/core/svg/animation/SVGSMILElement.cpp
@@ -1001,7 +1001,9 @@ float SVGSMILElement::calculateAnimationPercentAndRepeat(SMILTime elapsed, unsig
     SMILTime activeTime = elapsed - m_intervalBegin;
     SMILTime repeatingDuration = this->repeatingDuration();
     if (elapsed >= m_intervalEnd || activeTime > repeatingDuration) {
-        repeat = static_cast<unsigned>(repeatingDuration.value() / simpleDuration.value()) - 1;
+        repeat = static_cast<unsigned>(repeatingDuration.value() / simpleDuration.value());
+        if (!fmod(repeatingDuration.value(), simpleDuration.value()))
+            repeat--;
 
         double percent = (m_intervalEnd.value() - m_intervalBegin.value()) / simpleDuration.value();
         percent = percent - floor(percent);
