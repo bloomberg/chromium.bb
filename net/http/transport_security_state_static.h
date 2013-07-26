c363b6e1b759411692e0eaad7679be4b75919d9b
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 18:31:26 +0000

    Correct HSTS list entry: surkatty.com -> .org.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213957 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7fcbd84572308ebb16848275756b486c19556686

--- net/http/transport_security_state_static.h
+++ net/http/transport_security_state_static.h
@@ -829,7 +829,7 @@ static const struct HSTSPreload kPreloadedSTS[] = {
   {26, true, "\005forum\016quantifiedself\003com", true, kNoPins, DOMAIN_NOT_PINNED },
   {11, true, "\006shodan\002io", true, kNoPins, DOMAIN_NOT_PINNED },
   {18, true, "\015rapidresearch\002me", true, kNoPins, DOMAIN_NOT_PINNED },
-  {14, true, "\010surkatty\003com", true, kNoPins, DOMAIN_NOT_PINNED },
+  {14, true, "\010surkatty\003org", true, kNoPins, DOMAIN_NOT_PINNED },
   {21, true, "\017securityheaders\003com", true, kNoPins, DOMAIN_NOT_PINNED },
 };
 static const size_t kNumPreloadedSTS = ARRAYSIZE_UNSAFE(kPreloadedSTS);
