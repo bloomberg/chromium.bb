c40ad40f49723c1d3b3a7e85a153cadcf2b27f2d
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-27 12:34:06 +0000

    Remove old tests that have been migrated to the v8 repo.
    R=adamk@chromium.org, machenbach@chromium.org
    BUG=237872
    
    Review URL: https://chromiumcodereview.appspot.com/20867002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155053 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 5519218336463a1dcd9a864044ba571a980175b1

--- third_party/WebKit/LayoutTests/fast/js/script-tests/function-constructor-error.js
+++
@@ -1,6 +0,0 @@
-description(
-"This test checks that the Function constructor detects some syntax errors correctly (bug#59795)."
-);
-
-shouldThrow("Function('(i + (j)')", '"SyntaxError: Expected token \')\'"');
-shouldThrow("Function('return (i + (j)')", '"SyntaxError: Expected token \')\'"');
