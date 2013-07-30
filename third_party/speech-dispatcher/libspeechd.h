9ee587517a31eaa6e3706a91cae074f043751ddb
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-30 19:11:16 +0000

    c++11 ud suffix cleanup for third_party/speech-dispatcher
    This merges http://git.freebsoft.org/?p=speechd.git;a=commitdiff;h=c73bfee2b6ef683874b3fe7463239073f703c8b7
    to fix
    
    ../../third_party/speech-dispatcher/libspeechd.h:42:68: error: invalid suffix on literal; C++11 requires a space between literal and identifier [-Wreserved-user-defined-literal]
    
    This macro moved to a .c file in version 0.8 of this library.
    
    BUG=263960
    R=dmazzoni@chromium.org
    
    Review URL: https://codereview.chromium.org/20986006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214410 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 71316604afb68ed5926ff25dc952c4d14000d2c1

--- third_party/speech-dispatcher/libspeechd.h
+++ third_party/speech-dispatcher/libspeechd.h
@@ -33,14 +33,6 @@
 extern "C" {
 #endif
     
-#ifdef LIBSPEECHD_DEBUG
-/* Debugging */
-FILE* spd_debug;
-#endif
-
-/* Unless there is an fatal error, it doesn't print anything */
-#define SPD_FATAL(msg) { printf("Fatal error (libspeechd) [%s:%d]:"msg, __FILE__, __LINE__); fflush(stdout); exit(EXIT_FAILURE); }
-
 /* Arguments for spd_send_data() */
 #define SPD_WAIT_REPLY 1              /* Wait for reply */
 #define SPD_NO_REPLY 0               /* No reply requested */
