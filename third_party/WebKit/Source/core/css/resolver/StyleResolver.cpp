6b605f51bde34a2b9ed0944135fec9cfcc18199b
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-30 09:22:23 +0000

    Style resolution: Move display property to animation pass
    In r154056 a pass for applying properties which generate animations was
    introduced so that animations could be started at the appropriate point.
    However, starting an animation depends on the resolved value of display not
    being 'none'. This patch moves the resolution of display to be a part of the
    animation pass.
    
    Covered by existing tests. No change in behavior.
    
    BUG=232273
    
    Review URL: https://chromiumcodereview.appspot.com/21003002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155153 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 93eba23f1e7766c3675a15c6d0e2ed730cd323a7

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -1096,13 +1096,13 @@ template <StyleResolver::StyleApplicationPass pass>
 bool StyleResolver::isPropertyForPass(CSSPropertyID property)
 {
     COMPILE_ASSERT(CSSPropertyVariable < firstCSSProperty, CSS_variable_is_before_first_property);
-    const CSSPropertyID firstAnimationProperty = CSSPropertyWebkitAnimation;
+    const CSSPropertyID firstAnimationProperty = CSSPropertyDisplay;
     const CSSPropertyID lastAnimationProperty = CSSPropertyTransitionTimingFunction;
     COMPILE_ASSERT(firstCSSProperty == firstAnimationProperty, CSS_first_animation_property_should_be_first_property);
     const CSSPropertyID firstHighPriorityProperty = CSSPropertyColor;
     const CSSPropertyID lastHighPriorityProperty = CSSPropertyLineHeight;
     COMPILE_ASSERT(lastAnimationProperty + 1 == firstHighPriorityProperty, CSS_color_is_first_high_priority_property);
-    COMPILE_ASSERT(CSSPropertyLineHeight == firstHighPriorityProperty + 18, CSS_line_height_is_end_of_high_prioity_property_range);
+    COMPILE_ASSERT(CSSPropertyLineHeight == firstHighPriorityProperty + 17, CSS_line_height_is_end_of_high_prioity_property_range);
     COMPILE_ASSERT(CSSPropertyZoom == lastHighPriorityProperty - 1, CSS_zoom_is_before_line_height);
     switch (pass) {
     case VariableDefinitions:
