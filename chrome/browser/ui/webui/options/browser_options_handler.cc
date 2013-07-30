94f39d69ad1ff03acb07dcb96c67591437be5ebb
vasilii@chromium.org <vasilii@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-30 17:11:48 +0000

    Profile Reset: Get rid of the flag in about:flags.
    BUG=235037
    TBR=battre@chromium.org, dbeam@chromium.org
    
    Review URL: https://codereview.chromium.org/20686002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214376 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: aae0645d9f7957c355b8cf2c5c5778443320acb0

--- chrome/browser/ui/webui/options/browser_options_handler.cc
+++ chrome/browser/ui/webui/options/browser_options_handler.cc
@@ -12,6 +12,7 @@
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
 #include "base/memory/singleton.h"
+#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
 #include "base/path_service.h"
 #include "base/prefs/pref_service.h"
@@ -124,6 +125,10 @@ using content::UserMetricsAction;
 namespace options {
 
 namespace {
+
+// Constants for the new tab button field trial.
+ const char kProfileResetTrialName[] = "ManualResetProfile";
+ const char kProfileResetTrialEnableGroupName[] = "Enable";
 
 bool ShouldShowMultiProfilesUserList(chrome::HostDesktopType desktop_type) {
 #if defined(OS_CHROMEOS)
@@ -528,7 +533,11 @@ void BrowserOptionsHandler::GetLocalizedValues(DictionaryValue* values) {
       g_browser_process->gpu_mode_manager()->initial_gpu_mode_pref());
 #endif
 
+  bool finch_allows_button =
+      base::FieldTrialList::FindFullName(kProfileResetTrialName) ==
+      kProfileResetTrialEnableGroupName;
   values->SetBoolean("enableResetProfileSettingsSection",
+                     finch_allows_button ||
                      CommandLine::ForCurrentProcess()->HasSwitch(
                          switches::kEnableResetProfileSettings));
 }
