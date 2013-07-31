55480fd12cf0f0e5d4f8979f093cec64285e396a
simon.hong81@gmail.com <simon.hong81@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 14:13:19 +0000

    Create ScopedObserverWithDuplicatedSources to add duplicated source support in ScopedObserver
    When we need to handle duplicated source, ScopedObserverWithDuplicatedSources
    adds |observer| once for a particular source, no matter how many times Add() is invoked.
    Additionally |observer| is only removed once Remove() is invoked the same number of times as Add().
    
    Motivation of this change: 
    When RootWindow is created I want to add ActivationClientObserver to its ActivationClient
    in another CL(https://codereview.chromium.org/11513005/). 
    To do this, I always need to check ActivationClient of newly created RootWindow already has
    this observer because RootWindow can share same ActivationClient.
    When RootWindow is destroyed, I need to remove observer from its ActivationClient. To do
    this, ActivationaClient of all other RootWindow should be checked because of same
    reason.
    To handle this inconvenience, I want to check duplicated sources by ScopedObserverWithDuplicatedSources.
    
    R=sky@chromium.org
    BUG=NONE
    TEST=ash_unittests --gtest_filter=ScopedObserverWithDuplicatedSourcesTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/21185004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214724 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b0091d3c70607a5b0b80a51a82ae423e540f5b42

---
+++ ash/launcher/scoped_observer_with_duplicated_sources.h
@@ -0,0 +1,70 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_LAUNCHER_SCOPED_OBSERVER_WITH_DUPLICATED_SOURCES_H_
+#define ASH_LAUNCHER_SCOPED_OBSERVER_WITH_DUPLICATED_SOURCES_H_
+
+#include <map>
+
+#include "base/basictypes.h"
+#include "base/logging.h"
+
+// ScopedObserverWithDuplicatedSources is used to keep track of the set of
+// sources an object has attached itself to as an observer. When
+// ScopedObserverWithDuplicatedSources is destroyed it removes the object as an
+// observer from all sources it has been added to.
+// ScopedObserverWithDuplicatedSources adds |observer| once for a particular
+// source, no matter how many times Add() is invoked. Additionaly |observer| is
+// only removed once Remove() is invoked the same number of times as Add().
+
+template <class Source, class Observer>
+class ScopedObserverWithDuplicatedSources {
+ public:
+  explicit ScopedObserverWithDuplicatedSources(Observer* observer)
+      : observer_(observer) {}
+
+  ~ScopedObserverWithDuplicatedSources() {
+    typename SourceToAddCountMap::const_iterator iter =
+        counted_sources_.begin();
+    for (; iter != counted_sources_.end(); ++iter)
+      iter->first->RemoveObserver(observer_);
+  }
+
+  // Adds the object passed to the constructor only once as an observer on
+  // |source|.
+  void Add(Source* source) {
+    if (counted_sources_.find(source) == counted_sources_.end())
+      source->AddObserver(observer_);
+    counted_sources_[source]++;
+  }
+
+  // Only remove the object passed to the constructor as an observer from
+  // |source| when Remove() is invoked the same number of times as Add().
+  void Remove(Source* source) {
+    typename SourceToAddCountMap::iterator iter =
+        counted_sources_.find(source);
+    DCHECK(iter != counted_sources_.end() && iter->second > 0);
+
+    if (--iter->second == 0) {
+      counted_sources_.erase(source);
+      source->RemoveObserver(observer_);
+    }
+  }
+
+  bool IsObserving(Source* source) const {
+    return counted_sources_.find(source) != counted_sources_.end();
+  }
+
+ private:
+  typedef std::map<Source*, int> SourceToAddCountMap;
+
+  Observer* observer_;
+
+  // Map Source and its adding count.
+  std::map<Source*, int> counted_sources_;
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedObserverWithDuplicatedSources);
+};
+
+#endif  // ASH_LAUNCHER_SCOPED_OBSERVER_WITH_DUPLICATED_SOURCES_H_
---
+++ ash/launcher/scoped_observer_with_duplicated_sources_unittest.cc
@@ -0,0 +1,82 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/launcher/scoped_observer_with_duplicated_sources.h"
+
+#include "base/memory/scoped_ptr.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+class TestObserver {
+ public:
+  TestObserver() {}
+  ~TestObserver() {}
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(TestObserver);
+};
+
+class TestSource {
+ public:
+  TestSource() : observer_count_(0) {}
+  ~TestSource() {}
+
+  void AddObserver(TestObserver* observer) {
+    observer_count_++;
+  }
+  void RemoveObserver(TestObserver* observer) {
+    observer_count_--;
+  }
+
+  int GetObserverCount() {
+    return observer_count_;
+  }
+
+ private:
+  int observer_count_;
+
+  DISALLOW_COPY_AND_ASSIGN(TestSource);
+};
+
+TEST(ScopedObserverWithDuplicatedSourcesTest, DuplicatedSource) {
+  TestObserver observer;
+  TestSource source1;
+  TestSource source2;
+
+  ScopedObserverWithDuplicatedSources<TestSource, TestObserver>
+      observers(&observer);
+  EXPECT_EQ(0, source1.GetObserverCount());
+  EXPECT_FALSE(observers.IsObserving(&source1));
+  EXPECT_EQ(0, source2.GetObserverCount());
+  EXPECT_FALSE(observers.IsObserving(&source2));
+
+  // Add |source1|.
+  observers.Add(&source1);
+  EXPECT_EQ(1, source1.GetObserverCount());
+  EXPECT_TRUE(observers.IsObserving(&source1));
+  // AddObserver of TestSource is called only once.
+  observers.Add(&source1);
+  EXPECT_EQ(1, source1.GetObserverCount());
+  EXPECT_TRUE(observers.IsObserving(&source1));
+
+  // Add |source2|.
+  observers.Add(&source2);
+  EXPECT_EQ(1, source2.GetObserverCount());
+  EXPECT_TRUE(observers.IsObserving(&source2));
+
+  // Remove |source1|.
+  observers.Remove(&source1);
+  EXPECT_EQ(1, source1.GetObserverCount());
+  EXPECT_TRUE(observers.IsObserving(&source1));
+
+  // Remove |source2|.
+  observers.Remove(&source2);
+  EXPECT_EQ(0, source2.GetObserverCount());
+  EXPECT_FALSE(observers.IsObserving(&source2));
+
+  // Remove |source1| again.
+  observers.Remove(&source1);
+  // In this time, |observer| is removed from |source1|.
+  EXPECT_EQ(0, source1.GetObserverCount());
+  EXPECT_FALSE(observers.IsObserving(&source1));
+}
