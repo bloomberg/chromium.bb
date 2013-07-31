ed23760fa4ac986fb9acaa38ec977af083de075b
shcherbina@google.com <shcherbina@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-07-31 12:51:26 +0000

    Cleanup comments in validator text-based spec.
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3453
    TEST= none, only comments were changed
    R=khim@chromium.org
    
    Review URL: https://codereview.chromium.org/21074003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@11881 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: e2dfd95c2de51e744bad9171559657fe5ca6554a

--- src/trusted/validator_ragel/spec.py
+++ src/trusted/validator_ragel/spec.py
@@ -138,7 +138,7 @@ def _SplitOps(insn, args):
   ops = []
   i = 0
   while True:
-    # We do not use mere re.compile(_OperandRE(), args, i) here because
+    # We do not use mere re.match(_OperandRE(), args, i) here because
     # python backtracking regexes do not guarantee to find longest match.
     m = re.compile(r'(%s)($|,)' % _OperandRE()).match(args, i)
     assert m is not None, (args, i)
@@ -1049,7 +1049,8 @@ def ValidateRegularInstruction(instruction, bitness):
   except DoNotMatchError:
     pass
 
-  # Report error on duplicate prefixes (note that they are allowed in nops).
+  # Report error on duplicate prefixes (note that they are allowed in
+  # long nops).
   _GetLegacyPrefixes(instruction)
 
   if bitness == 32:
@@ -1173,7 +1174,7 @@ def ValidateRegularInstruction(instruction, bitness):
     # zero-extend regular register, or can modify protected registers (r15,
     # rbp, rsp).
     # This means that we don't have to worry about implicit operands (for
-    # example it does not matter to us that mul writes to edx and eax).
+    # example it does not matter to us that mul writes to rdx and rax).
 
     if (_InstructionNameIn(
           name, [
@@ -1182,8 +1183,8 @@ def ValidateRegularInstruction(instruction, bitness):
             'movd',
             'add', 'sub', 'and', 'or', 'xor']) or
         name in ['movd', 'vmovd', 'vmovq']):
-      # Technically, movabs is not allowed, it's ok to accept it here, because
-      # it will later be rejected because of improper memory access.
+      # Technically, movabs is not allowed, but it's ok to accept it here,
+      # because it will later be rejected because of improper memory access.
       # On the other hand, because of objdump quirk it prints regular
       # mov with 64-bit immediate as movabs:
       #   48 b8 00 00 00 00 00 00 00 00
@@ -1245,12 +1246,6 @@ def ValidateRegularInstruction(instruction, bitness):
          'lzcnt', 'tzcnt', 'popcnt', 'crc32', 'cmpxchg',
          'movbe',
          'movmskpd', 'movmskps', 'movnti']):
-      # Note: some versions of objdump (including one that is currently used
-      # in targeted tests) decode 'tzcnt' as 'repz bsf'
-      # (see validator_ragel/testdata/32/tzcnt.test)
-      # From sandboxing point of view bsf and tzcnt are the same, so
-      # we ignore this bug here.
-      # Same applies to 32-bit version.
       assert len(ops) == 2
       write_ops = [ops[1]]
 
@@ -1367,8 +1362,7 @@ def ValidateDirectJump(instruction, bitness):
       raise SandboxingError(
           'branch hints are not allowed with loop instruction', instruction)
     # Unfortunately we can't rely on presence of 'data16' prefix in disassembly,
-    # because nacl-objdump prints it, but objdump we base our decoder on
-    # does not.
+    # because neither nacl-objdump nor objdump we base our decoder print it.
     # So we look at bytes.
     if 0x66 in _GetLegacyPrefixes(instruction):
       raise SandboxingError(
