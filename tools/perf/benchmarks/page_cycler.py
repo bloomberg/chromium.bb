6d19b43c1350aaf7dc581347d52efbcc27850643
edmundyan@chromium.org <edmundyan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 04:15:54 +0000

    Fix for page_cycler on benchmarks
    BUG=263921
    
    Review URL: https://chromiumcodereview.appspot.com/20106003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214556 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 729e4e304e50a28e29eada7599684b7456fe2c1e

--- tools/perf/benchmarks/page_cycler.py
+++ tools/perf/benchmarks/page_cycler.py
@@ -9,61 +9,61 @@ from measurements import page_cycler
 class PageCyclerBloat(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/page_cycler/bloat.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
 
 
 class PageCyclerDhtml(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/page_cycler/dhtml.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
 
 
 class PageCyclerIndexeddb(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/page_cycler/indexed_db/basic_insert.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
 
 
 class PageCyclerIntlArFaHe(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/intl_ar_fa_he.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
 
 
 class PageCyclerIntlEsFrPtBr(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/intel_es_fr_pt-BR.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
 
 
 class PageCyclerIntlHiRu(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/intl_hi_ru.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
 
 
 class PageCyclerIntlJaZh(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/intl_ja_zh.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
 
 
 class PageCyclerIntlKoThVi(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/intl_ko_th_vi.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
 
 
 class PageCyclerMorejs(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/morejs.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
 
 
 class PageCyclerMoz(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/page_cycler/moz.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
 
 
 class PageCyclerNetsimTop25(test.Test):
@@ -75,7 +75,7 @@ class PageCyclerNetsimTop25(test.Test):
       '--shaping_type=proxy',
       '--net=cable'
       ],
-    'pageset_repeat': '5',
+    'pageset_repeat_iters': 5,
     }
 
   def __init__(self):
@@ -88,4 +88,4 @@ class PageCyclerNetsimTop25(test.Test):
 class PageCyclerTypical25(test.Test):
   test = page_cycler.PageCycler
   page_set = 'page_sets/typical_25.json'
-  options = {'pageset_repeat': 10}
+  options = {'pageset_repeat_iters': 10}
