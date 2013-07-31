6a3edfb2479d31f2526465414df81a4cb6cb6748
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 23:16:33 +0000

    Don't add ImageTransportFactory observer if the shared surface handle is NULL.
    If the handle is NULL, then it can't be lost, so don't register as an observer of that.
    
    BUG=
    R=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/21134003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214857 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 96fd77b1b0b9d6c0ea3c06268e27d98571cf9eff

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -2004,7 +2004,8 @@ gfx::GLSurfaceHandle RenderWidgetHostViewAura::GetCompositingSurface() {
   if (shared_surface_handle_.is_null()) {
     ImageTransportFactory* factory = ImageTransportFactory::GetInstance();
     shared_surface_handle_ = factory->CreateSharedSurfaceHandle();
-    factory->AddObserver(this);
+    if (!shared_surface_handle_.is_null())
+      factory->AddObserver(this);
   }
   return shared_surface_handle_;
 }
