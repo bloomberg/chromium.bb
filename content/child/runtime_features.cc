212c52e73e054f96f811b414b2816fe18e659e85
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-03 00:10:00 +0000

    Don't enable MSE/EME for ICS devices
    ICS devices doesn't have MSE capabilities, remove all devices whose API level < 16
    
    BUG=266231
    
    Review URL: https://chromiumcodereview.appspot.com/20824009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215413 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a885fb3eb3923eb83e0f635211a98ba4bde5ec89

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -10,6 +10,7 @@
 
 #if defined(OS_ANDROID)
 #include <cpu-features.h>
+#include "base/android/build_info.h"
 #endif
 
 using WebKit::WebRuntimeFeatures;
@@ -18,11 +19,19 @@ namespace content {
 
 static void SetRuntimeFeatureDefaultsForPlatform() {
 #if defined(OS_ANDROID)
+#if !defined(GOOGLE_TV)
+  // MSE/EME implementation needs Android MediaCodec API that was introduced
+  // in JellyBrean.
+  if (base::android::BuildInfo::GetInstance()->sdk_int() < 16) {
+    WebRuntimeFeatures::enableWebKitMediaSource(false);
+    WebRuntimeFeatures::enableLegacyEncryptedMedia(false);
+  }
+#endif  // !defined(GOOGLE_TV)
   bool enable_webaudio = false;
 #if defined(ARCH_CPU_ARMEL)
   enable_webaudio =
       ((android_getCpuFeatures() & ANDROID_CPU_ARM_FEATURE_NEON) != 0);
-#endif
+#endif  // defined(ARCH_CPU_ARMEL)
   WebRuntimeFeatures::enableWebAudio(enable_webaudio);
   // Android does not support the Gamepad API.
   WebRuntimeFeatures::enableGamepad(false);
@@ -30,7 +39,7 @@ static void SetRuntimeFeatureDefaultsForPlatform() {
   WebRuntimeFeatures::enablePagePopup(false);
   // datalist on Android is not enabled
   WebRuntimeFeatures::enableDataListElement(false);
-#endif
+#endif  // defined(OS_ANDROID)
 }
 
 void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
