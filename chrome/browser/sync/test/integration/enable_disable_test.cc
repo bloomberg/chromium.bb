7fc4d0aa0f47298eb17325b0c395a45d98334bb9
petewil@chromium.org <petewil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-03 02:59:04 +0000

    Enable Synced Notifications in Dev and Canary
    This is a resubmit of CL 19678018.  It was reverted after it was found
    to cause a crash in incognito mode.  The crash is fixed, and this has been
    tested in incognito mode.  The code has not changed since CL 19678018
    
    BUG=248331
    
    Review URL: https://chromiumcodereview.appspot.com/21952006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215467 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: eaa966b5847ab0d226ed323ff0609722ec6f0dd4

--- chrome/browser/sync/test/integration/enable_disable_test.cc
+++ chrome/browser/sync/test/integration/enable_disable_test.cc
@@ -111,7 +111,8 @@ IN_PROC_BROWSER_TEST_F(EnableDisableSingleClientTest, DisableOneAtATime) {
   for (syncer::ModelTypeSet::Iterator it = registered_types.First();
        it.Good(); it.Inc()) {
     // MANAGED_USERS is always synced.
-    if (it.Get() == syncer::MANAGED_USERS)
+    if (it.Get() == syncer::MANAGED_USERS ||
+        it.Get() == syncer::SYNCED_NOTIFICATIONS)
       continue;
 
     ASSERT_TRUE(GetClient(0)->DisableSyncForDatatype(it.Get()));
