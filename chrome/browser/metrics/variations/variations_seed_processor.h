853799d67ac8c645541ecb77bc24bd23f11c12d8
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-05 23:46:12 +0000

    Prioritise non-expired studies if both exist in a variations seed.
    Re-land of: https://codereview.chromium.org/21824003/
    
    The original CL accidentally changed the probability of a field
    trial group from 0 to 50%, making the test flaky, which is
    fixed in this CL.
    
    BUG=267651
    TEST=New unit test.
    TBR=jwd@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/22259002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215729 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5bd00cec389519e3fce0bbb4ad26145cb6718242

--- chrome/browser/metrics/variations/variations_seed_processor.h
+++ chrome/browser/metrics/variations/variations_seed_processor.h
@@ -68,9 +68,8 @@ class VariationsSeedProcessor {
                          const std::string& version_string);
 
   // Creates and registers a field trial from the |study| data. Disables the
-  // trial if IsStudyExpired(study, reference_date) is true.
-  void CreateTrialFromStudy(const Study& study,
-                            const base::Time& reference_date);
+  // trial if |is_expired| is true.
+  void CreateTrialFromStudy(const Study& study, bool is_expired);
 
   // Checks whether |study| is expired using the given date/time.
   bool IsStudyExpired(const Study& study, const base::Time& date_time);
