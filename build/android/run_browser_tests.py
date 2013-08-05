011ba5f7549b01fa24668795dc23bb92ade81275
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-05 17:34:00 +0000

    [Android] Deprecate old test scripts.
    Just print the new command and exit. This is in
    preparation for completely removing these scripts.
    
    BUG=
    R=craigdh@chromium.org
    
    Review URL: https://codereview.chromium.org/21979006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215634 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 27692510a2abab53929e2cd027827b272f74c0d2

--- build/android/run_browser_tests.py
+++ build/android/run_browser_tests.py
@@ -14,11 +14,10 @@ from pylib import cmd_helper
 
 
 if __name__ == '__main__':
-  args = ['python',
-          os.path.join(os.path.dirname(__file__), 'test_runner.py'),
+  args = [os.path.join(os.path.dirname(__file__), 'test_runner.py'),
           'gtest', '-s', 'content_browsertests'] + sys.argv[1:]
   logging.warning('*' * 80)
   logging.warning('This script is deprecated and will be removed soon.')
   logging.warning('Use the following instead: %s', ' '.join(args))
   logging.warning('*' * 80)
-  sys.exit(cmd_helper.RunCmd(args))
+  sys.exit(1)
