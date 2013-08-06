20d964240d736253232e624d44c5e29d2cd778a4
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 08:42:58 +0000

    Made --apps-use-native-frame flag available on Mac.
    This flag has been meaningful on Mac as of r213834.
    
    BUG=268789
    
    Review URL: https://chromiumcodereview.appspot.com/22365002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215857 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fa39ebdae953a67645cabc2ca987f4ea8d76bc17

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1457,7 +1457,7 @@ const Experiment kExperiments[] = {
     "apps-use-native-frame",
     IDS_FLAGS_ENABLE_NATIVE_FRAMES_FOR_APPS_NAME,
     IDS_FLAGS_ENABLE_NATIVE_FRAMES_FOR_APPS_DESCRIPTION,
-    kOsWin,
+    kOsMac | kOsWin,
     SINGLE_VALUE_TYPE(switches::kAppsUseNativeFrame)
   },
   {
