bloomberg/chromium.bb

commit 912679c5521dc22026390a9fadc633600eb1e200

Add 'error' parameter to 'window.onerror' and 'ErrorEvent'.
The HTML5 spec recently added the error object to both the
'window.onerror' callback and the 'ErrorEvent' interface in order to
improve developers' ability to debug issues that cause unexpected
exceptions in the wild[1]. This patch brings Blink's behavior into line
with that update.

Currently, we generate an ErrorEvent object after doing sanitization.
This patch refactors that code such that we generate an ErrorEvent when
V8 calls back into the bindings (V8Initializer::messageHandlerInWorker
and V8Initializer::messageHandlerInMainThread). Before we hand that
event off to core for sanitization and dispatch, we grab the wrapper
for the world in which the exception was thrown, and set the exception
object as a hidden value (V8HiddenPropertyName::error()).

On the other end, we grab that exception object in the custom 'error'
getter of 'V8ErrorEvent', and when triggering the 'onerror' handler
via 'V8ErrorHandler::callListenerFunction'. If the value exists, we're
still in the same isolated world, so we can safely return the exception
without leakage. If the value is empty, we've crossed worlds, and
return 'null' instead.

BUG=147127

Review URL: https://chromiumcodereview.appspot.com/20351002

git-svn-id: svn://svn.chromium.org/blink/trunk@155454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: acbdeb3cfbde4e90fb0a4ae427b6c66c87edf97c

third_party/WebKit/Source/bindings/v8/V8Initializer.cpp
@@ -26,6 +26,7 @@
 #include "config.h"
 #include "bindings/v8/V8Initializer.h"
 
+#include "V8ErrorEvent.h"
 #include "V8History.h"
 #include "V8Location.h"
 #include "V8Window.h"
@@ -33,6 +34,7 @@
 #include "bindings/v8/ScriptProfiler.h"
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8GCController.h"
+#include "bindings/v8/V8HiddenPropertyName.h"
 #include "bindings/v8/V8PerContextData.h"
 #include "core/dom/Document.h"
 #include "core/dom/ExceptionCode.h"
@@ -94,7 +96,13 @@ static void messageHandlerInMainThread(v8::Handle<v8::Message> message, v8::Hand
     v8::Handle<v8::Value> resourceName = message->GetScriptResourceName();
     bool shouldUseDocumentURL = resourceName.IsEmpty() || !resourceName->IsString();
     String resource = shouldUseDocumentURL ? firstWindow->document()->url() : toWebCoreString(resourceName);
-    firstWindow->document()->reportException(errorMessage, message->GetLineNumber(), message->GetStartColumn(), resource, callStack);
+    RefPtr<ErrorEvent> event = ErrorEvent::create(errorMessage, resource, message->GetLineNumber(), message->GetStartColumn());
+    v8::Local<v8::Value> wrappedEvent = toV8(event.get(), v8::Handle<v8::Object>(), v8::Isolate::GetCurrent());
+    if (!wrappedEvent.IsEmpty()) {
+        ASSERT(wrappedEvent->IsObject());
+        v8::Local<v8::Object>::Cast(wrappedEvent)->SetHiddenValue(V8HiddenPropertyName::error(), data);
+    }
+    firstWindow->document()->reportException(event.release(), callStack);
 }
 
 static void failedAccessCheckCallbackInMainThread(v8::Local<v8::Object> host, v8::AccessType type, v8::Local<v8::Value> data)
@@ -162,10 +170,14 @@ static void messageHandlerInWorker(v8::Handle<v8::Message> message, v8::Handle<v
     // During the frame teardown, there may not be a valid context.
     if (ScriptExecutionContext* context = getScriptExecutionContext()) {
         String errorMessage = toWebCoreString(message->Get());
-        int lineNumber = message->GetLineNumber();
-        int columnNumber = message->GetStartColumn();
         String sourceURL = toWebCoreString(message->GetScriptResourceName());
-        context->reportException(errorMessage, lineNumber, columnNumber, sourceURL, 0);
+        RefPtr<ErrorEvent> event = ErrorEvent::create(errorMessage, sourceURL, message->GetLineNumber(), message->GetStartColumn());
+        v8::Local<v8::Value> wrappedEvent = toV8(event.get(), v8::Handle<v8::Object>(), v8::Isolate::GetCurrent());
+        if (!wrappedEvent.IsEmpty()) {
+            ASSERT(wrappedEvent->IsObject());
+            v8::Local<v8::Object>::Cast(wrappedEvent)->SetHiddenValue(V8HiddenPropertyName::error(), data);
+        }
+        context->reportException(event.release(), 0);
     }
 
     isReportingException = false;

third_party/WebKit/LayoutTests/fast/events/constructors/error-event-constructor-expected.txt
@@ -82,12 +82,15 @@ PASS new ErrorEvent('eventType', { colno: [12345, 67890] }).colno is 0
 PASS new ErrorEvent('eventType', { colno: {} }).colno is 0
 PASS new ErrorEvent('eventType', { colno: {moemoe: 12345} }).colno is 0
 PASS new ErrorEvent('eventType', { colno: {valueOf: function () { return 12345; }} }).colno is 12345
-PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).bubbles is true
-PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).cancelable is true
-PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).message is "sakuranbo"
-PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).filename is "amaenbo"
-PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).lineno is 12345
-PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).colno is 23456
+PASS new ErrorEvent('eventType', { error: { moemoe: 12345 } }).error.moemoe is 12345
+PASS new ErrorEvent('eventType', { error: { message: 'Message' } }).error.message is "Message"
+PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).bubbles is true
+PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).cancelable is true
+PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).message is "sakuranbo"
+PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).filename is "amaenbo"
+PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).lineno is 12345
+PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).colno is 23456
+PASS new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).error.message is "hi"
 PASS successfullyParsed is true
 
 TEST COMPLETE

third_party/WebKit/LayoutTests/fast/events/constructors/error-event-constructor.html
@@ -105,13 +105,18 @@ shouldBe("new ErrorEvent('eventType', { colno: {} }).colno", "0");
 shouldBe("new ErrorEvent('eventType', { colno: {moemoe: 12345} }).colno", "0");
 shouldBe("new ErrorEvent('eventType', { colno: {valueOf: function () { return 12345; }} }).colno", "12345");
 
+// error is passed.
+shouldBe("new ErrorEvent('eventType', { error: { moemoe: 12345 } }).error.moemoe", "12345");
+shouldBeEqualToString("new ErrorEvent('eventType', { error: { message: 'Message' } }).error.message", "Message");
+
 // All initializers are passed.
-shouldBe("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).bubbles", "true");
-shouldBe("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).cancelable", "true");
-shouldBeEqualToString("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).message", "sakuranbo");
-shouldBeEqualToString("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).filename", "amaenbo");
-shouldBe("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).lineno", "12345");
-shouldBe("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456 }).colno", "23456");
+shouldBe("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).bubbles", "true");
+shouldBe("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).cancelable", "true");
+shouldBeEqualToString("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).message", "sakuranbo");
+shouldBeEqualToString("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).filename", "amaenbo");
+shouldBe("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).lineno", "12345");
+shouldBe("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).colno", "23456");
+shouldBeEqualToString("new ErrorEvent('eventType', { bubbles: true, cancelable: true, message: 'sakuranbo', filename: 'amaenbo', lineno: 12345, colno: 23456, error: { message: 'hi' } }).error.message", "hi");
 </script>
 <script src="../../js/resources/js-test-post.js"></script>
 </body>

third_party/WebKit/LayoutTests/fast/events/resources/onerror-test.js
@@ -8,15 +8,19 @@ function throwException(message) {
 
 var errorsSeen = 0;
 function dumpOnErrorArgumentValuesAndReturn(returnValue, callback) {
-    window.onerror = function (message, url, line, column) {
+    window.onerror = function (message, url, line, column, error) {
         debug("window.onerror: \"" + message + "\" at " + stripURL(url) + " (Line: " + line + ", Column: " + column + ")");
+        if (error)
+            debug(stripStackURLs(error.stack));
+        else
+            debug("No stack trace.");
 
         if (callback)
             callback(++errorsSeen);
         if (returnValue)
-            debug("Returning 'true': the error should not be reported in the console as an unhandled exception.");
+            debug("Returning 'true': the error should not be reported in the console as an unhandled exception.\n\n\n");
         else
-            debug("Returning 'false': the error should be reported in the console as an unhandled exception.");
+            debug("Returning 'false': the error should be reported in the console as an unhandled exception.\n\n\n");
         return returnValue;
     };
 }
@@ -24,7 +28,7 @@ function dumpOnErrorArgumentValuesAndReturn(returnValue, callback) {
 function dumpErrorEventAndAllowDefault(callback) {
     window.addEventListener('error', function (e) {
         dumpErrorEvent(e)
-        debug("Not calling e.preventDefault(): the error should be reported in the console as an unhandled exception.");
+        debug("Not calling e.preventDefault(): the error should be reported in the console as an unhandled exception.\n\n\n");
         if (callback)
             callback(++errorsSeen);
     });
@@ -33,7 +37,7 @@ function dumpErrorEventAndAllowDefault(callback) {
 function dumpErrorEventAndPreventDefault(callback) {
     window.addEventListener('error', function (e) {
         dumpErrorEvent(e);
-        debug("Calling e.preventDefault(): the error should not be reported in the console as an unhandled exception.");
+        debug("Calling e.preventDefault(): the error should not be reported in the console as an unhandled exception.\n\n\n");
         e.preventDefault();
         if (callback)
             callback(++errorsSeen);
@@ -44,6 +48,10 @@ var eventPassedToTheErrorListener = null;
 var eventCurrentTarget = null;
 function dumpErrorEvent(e) {
     debug("Handling '" + e.type + "' event (phase " + e.eventPhase + "): \"" + e.message + "\" at " + stripURL(e.filename) + ":" + e.lineno);
+    if (e.error)
+        debug(stripStackURLs(e.error.stack));
+    else
+        debug("No stack trace.");
 
     eventPassedToTheErrorListener = e;
     eventCurrentTarget = e.currentTarget;
@@ -51,4 +59,14 @@ function dumpErrorEvent(e) {
     shouldBe('eventCurrentTarget', 'window');
     eventPassedToTheErrorListener = null;
     eventCurrentTarget = null;
+}
+
+function stripStackURLs(stackTrace) {
+    stackTrace = stackTrace.split("\n");
+    var length = Math.min(stackTrace.length, 100);
+    var text = "Stack Trace:\n";
+    for (var i = 0; i < length; i++) {
+        text += stackTrace[i].replace(/at ((?:eval at \()?[a-zA-Z\.]+ )?\(?.+\/([^\/]+):(\d+):(\d+)\)?/, "at $1$2:$3:$4") + "\n";
+    }
+    return text;
 }

third_party/WebKit/LayoutTests/fast/events/window-onerror-01-expected.txt
@@ -4,7 +4,14 @@ On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE
 
 
 window.onerror: "Uncaught ReferenceError: hahaha_good_luck_finding_me is not defined" at window-onerror-01.html (Line: 14, Column: 8)
+Stack Trace:
+ReferenceError: hahaha_good_luck_finding_me is not defined
+    at window-onerror-01.html:14:9
+
 Returning 'true': the error should not be reported in the console as an unhandled exception.
+
+
+
 PASS successfullyParsed is true
 
 TEST COMPLETE

third_party/WebKit/LayoutTests/fast/events/window-onerror-02-expected.txt
@@ -4,7 +4,14 @@ On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE
 
 
 window.onerror: "Uncaught ReferenceError: unknownObject is not defined" at window-onerror-02.html (Line: 14, Column: 8)
+Stack Trace:
+ReferenceError: unknownObject is not defined
+    at window-onerror-02.html:14:9
+
 Returning 'true': the error should not be reported in the console as an unhandled exception.
+
+
+
 PASS successfullyParsed is true
 
 TEST COMPLETE

third_party/WebKit/LayoutTests/fast/events/window-onerror-03-expected.txt
@@ -4,7 +4,15 @@ On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE
 
 
 window.onerror: "Uncaught ReferenceError: Invalid left-hand side in assignment" at window-onerror-03.html (Line: 1, Column: 0)
+Stack Trace:
+ReferenceError: Invalid left-hand side in assignment
+    at eval window-onerror-03.html:14:9), :1:1
+    at window-onerror-03.html:14:9
+
 Returning 'true': the error should not be reported in the console as an unhandled exception.
+
+
+
 PASS successfullyParsed is true
 
 TEST COMPLETE

third_party/WebKit/LayoutTests/fast/events/window-onerror-04-expected.txt
@@ -4,7 +4,14 @@ On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE
 
 
 window.onerror: "Uncaught SyntaxError: Unexpected end of input" at window-onerror-04.html (Line: 1, Column: 2)
+Stack Trace:
+SyntaxError: Unexpected end of input
+    at window-onerror-04.html:14:9
+
 Returning 'true': the error should not be reported in the console as an unhandled exception.
+
+
+
 PASS successfullyParsed is true
 
 TEST COMPLETE

commit 142e0e76e602660f0bda2bdcd1502e900f2d59e5

messageHandlerInMainThread shouldn't create a wrapper during context initialization
This is a regression caused by r155454.

We need to skip creating a wrapper of an error event object
if messageHandlerInMainThread is called during context initialization.
It's not allowed to create a wrapper during context initialization.

I couldn't reproduce the crash in layout tests because
the crash disappears when I use testRunner.waitUntilDone()...

TEST=ManualTests/error-event-during-context-initialization-crash.html
BUG=269709
R=mkwst@chromium.org

Review URL: https://codereview.chromium.org/22273008

git-svn-id: svn://svn.chromium.org/blink/trunk@155745 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: d1140f9d4776fd272be4b873516dcae879146af8

third_party/WebKit/ManualTests/error-event-during-context-initialization-crash.html
@@ -1,0 +1,9 @@
+<script>
+function main()
+{
+    document.body.insertAdjacentHTML("beforeBegin", "<iframe></iframe>");
+}
+
+document.addEventListener("DOMNodeInserted", main);
+setTimeout(main, 0);
+</script>

third_party/WebKit/Source/bindings/v8/V8Initializer.cpp
@@ -30,7 +30,9 @@
 #include "V8History.h"
 #include "V8Location.h"
 #include "V8Window.h"
+#include "bindings/v8/DOMWrapperWorld.h"
 #include "bindings/v8/ScriptCallStackFactory.h"
+#include "bindings/v8/ScriptController.h"
 #include "bindings/v8/ScriptProfiler.h"
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8GCController.h"
@@ -97,10 +99,17 @@ static void messageHandlerInMainThread(v8::Handle<v8::Message> message, v8::Hand
     bool shouldUseDocumentURL = resourceName.IsEmpty() || !resourceName->IsString();
     String resource = shouldUseDocumentURL ? firstWindow->document()->url() : toWebCoreString(resourceName);
     RefPtr<ErrorEvent> event = ErrorEvent::create(errorMessage, resource, message->GetLineNumber(), message->GetStartColumn());
-    v8::Local<v8::Value> wrappedEvent = toV8(event.get(), v8::Handle<v8::Object>(), v8::Isolate::GetCurrent());
-    if (!wrappedEvent.IsEmpty()) {
-        ASSERT(wrappedEvent->IsObject());
-        v8::Local<v8::Object>::Cast(wrappedEvent)->SetHiddenValue(V8HiddenPropertyName::error(), data);
+
+    // messageHandlerInMainThread can be called while we're creating a new context.
+    // Since we cannot create a wrapper in the intermediate timing, we need to skip
+    // creating a wrapper for |event|.
+    DOMWrapperWorld* world = DOMWrapperWorld::current();
+    if (world && firstWindow->document()->frame()->script()->existingWindowShell(world)) {
+        v8::Local<v8::Value> wrappedEvent = toV8(event.get(), v8::Handle<v8::Object>(), v8::Isolate::GetCurrent());
+        if (!wrappedEvent.IsEmpty()) {
+            ASSERT(wrappedEvent->IsObject());
+            v8::Local<v8::Object>::Cast(wrappedEvent)->SetHiddenValue(V8HiddenPropertyName::error(), data);
+        }
     }
     AccessControlStatus corsStatus = message->IsSharedCrossOrigin() ? SharableCrossOrigin : NotSharableCrossOrigin;
     firstWindow->document()->reportException(event.release(), callStack, corsStatus);