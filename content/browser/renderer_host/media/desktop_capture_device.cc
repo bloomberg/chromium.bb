187494901a8a6b6bd0eb1f670077a34620fcf227
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 12:55:49 +0000

    Update ScreenCaptureDevice to support window capture.
    Renamed ScreenCaptureDevice to DesktopCaptureDevice and refactored it
    to support both screen and window capturers.
    
    BUG=237907
    TBR=piman@chromium.org (trivial changes in .gypi)
    
    Review URL: https://chromiumcodereview.appspot.com/22425004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216676 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 609e6d3c752c3eb1382be9485a4f3a49227d8e1a

--- content/browser/renderer_host/media/desktop_capture_device.cc
+++ content/browser/renderer_host/media/desktop_capture_device.cc
@@ -1,19 +1,23 @@
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/renderer_host/media/screen_capture_device.h"
+#include "content/browser/renderer_host/media/desktop_capture_device.h"
 
 #include "base/bind.h"
 #include "base/location.h"
 #include "base/logging.h"
 #include "base/sequenced_task_runner.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/synchronization/lock.h"
+#include "content/public/browser/browser_thread.h"
+#include "content/public/common/desktop_media_id.h"
 #include "media/base/video_util.h"
 #include "third_party/libyuv/include/libyuv/scale_argb.h"
+#include "third_party/webrtc/modules/desktop_capture/desktop_capturer.h"
 #include "third_party/webrtc/modules/desktop_capture/desktop_frame.h"
-#include "third_party/webrtc/modules/desktop_capture/mouse_cursor_shape.h"
 #include "third_party/webrtc/modules/desktop_capture/screen_capturer.h"
+#include "third_party/webrtc/modules/desktop_capture/window_capturer.h"
 
 namespace content {
 
@@ -21,16 +25,12 @@ namespace {
 const int kBytesPerPixel = 4;
 }  // namespace
 
-class ScreenCaptureDevice::Core
+class DesktopCaptureDevice::Core
     : public base::RefCountedThreadSafe<Core>,
       public webrtc::DesktopCapturer::Callback {
  public:
-  explicit Core(scoped_refptr<base::SequencedTaskRunner> task_runner);
-
-  // Helper used in tests to supply a fake capturer.
-  void SetScreenCapturerForTest(scoped_ptr<webrtc::ScreenCapturer> capturer) {
-    screen_capturer_ = capturer.Pass();
-  }
+  Core(scoped_refptr<base::SequencedTaskRunner> task_runner,
+       scoped_ptr<webrtc::DesktopCapturer> capturer);
 
   // Implementation of VideoCaptureDevice methods.
   void Allocate(int width, int height,
@@ -59,14 +59,17 @@ class ScreenCaptureDevice::Core
   void ScheduleCaptureTimer();
 
   // Method that is scheduled on |task_runner_| to be called on regular interval
-  // to capture the screen.
+  // to capture a frame.
   void OnCaptureTimer();
 
   // Captures a single frame.
   void DoCapture();
 
-  // Task runner used for screen capturing operations.
+  // Task runner used for capturing operations.
   scoped_refptr<base::SequencedTaskRunner> task_runner_;
+
+  // The underlying DesktopCapturer instance used to capture frames.
+  scoped_ptr<webrtc::DesktopCapturer> desktop_capturer_;
 
   // |event_handler_lock_| must be locked whenever |event_handler_| is used.
   // It's necessary because DeAllocate() needs to reset it on the calling thread
@@ -79,9 +82,6 @@ class ScreenCaptureDevice::Core
 
   // Frame rate specified to Allocate().
   int frame_rate_;
-
-  // The underlying ScreenCapturer instance used to capture frames.
-  scoped_ptr<webrtc::ScreenCapturer> screen_capturer_;
 
   // Empty until the first frame has been captured, and the output dimensions
   // chosen based on the capture frame's size, and any caller-supplied
@@ -104,25 +104,27 @@ class ScreenCaptureDevice::Core
   // |task_runner_|.
   bool capture_task_posted_;
 
-  // True when waiting for |screen_capturer_| to capture current frame.
+  // True when waiting for |desktop_capturer_| to capture current frame.
   bool capture_in_progress_;
 
   DISALLOW_COPY_AND_ASSIGN(Core);
 };
 
-ScreenCaptureDevice::Core::Core(
-    scoped_refptr<base::SequencedTaskRunner> task_runner)
+DesktopCaptureDevice::Core::Core(
+    scoped_refptr<base::SequencedTaskRunner> task_runner,
+    scoped_ptr<webrtc::DesktopCapturer> capturer)
     : task_runner_(task_runner),
+      desktop_capturer_(capturer.Pass()),
       event_handler_(NULL),
       started_(false),
       capture_task_posted_(false),
       capture_in_progress_(false) {
 }
 
-ScreenCaptureDevice::Core::~Core() {
+DesktopCaptureDevice::Core::~Core() {
 }
 
-void ScreenCaptureDevice::Core::Allocate(int width, int height,
+void DesktopCaptureDevice::Core::Allocate(int width, int height,
                                          int frame_rate,
                                          EventHandler* event_handler) {
   DCHECK_GT(width, 0);
@@ -139,17 +141,17 @@ void ScreenCaptureDevice::Core::Allocate(int width, int height,
       base::Bind(&Core::DoAllocate, this, width, height, frame_rate));
 }
 
-void ScreenCaptureDevice::Core::Start() {
+void DesktopCaptureDevice::Core::Start() {
   task_runner_->PostTask(
       FROM_HERE, base::Bind(&Core::DoStart, this));
 }
 
-void ScreenCaptureDevice::Core::Stop() {
+void DesktopCaptureDevice::Core::Stop() {
   task_runner_->PostTask(
       FROM_HERE, base::Bind(&Core::DoStop, this));
 }
 
-void ScreenCaptureDevice::Core::DeAllocate() {
+void DesktopCaptureDevice::Core::DeAllocate() {
   {
     base::AutoLock auto_lock(event_handler_lock_);
     event_handler_ = NULL;
@@ -158,11 +160,11 @@ void ScreenCaptureDevice::Core::DeAllocate() {
 }
 
 webrtc::SharedMemory*
-ScreenCaptureDevice::Core::CreateSharedMemory(size_t size) {
+DesktopCaptureDevice::Core::CreateSharedMemory(size_t size) {
   return NULL;
 }
 
-void ScreenCaptureDevice::Core::OnCaptureCompleted(
+void DesktopCaptureDevice::Core::OnCaptureCompleted(
     webrtc::DesktopFrame* frame) {
   DCHECK(task_runner_->RunsTasksOnCurrentThread());
   DCHECK(capture_in_progress_);
@@ -257,41 +259,23 @@ void ScreenCaptureDevice::Core::OnCaptureCompleted(
   }
 }
 
-void ScreenCaptureDevice::Core::DoAllocate(int width,
-                                           int height,
-                                           int frame_rate) {
+void DesktopCaptureDevice::Core::DoAllocate(int width,
+                                            int height,
+                                            int frame_rate) {
   DCHECK(task_runner_->RunsTasksOnCurrentThread());
+  DCHECK(desktop_capturer_);
 
   requested_size_.set(width, height);
   output_size_.set(0, 0);
   frame_rate_ = frame_rate;
 
-  // Create and start frame capturer.
-  if (!screen_capturer_) {
-#if defined(OS_CHROMEOS) && !defined(ARCH_CPU_ARMEL) && defined(USE_X11)
-    // ScreenCapturerX11 polls by default, due to poor driver support for
-    // DAMAGE. ChromeOS' drivers [can be patched to] support DAMAGE properly, so
-    // use it. However ARM driver seems to not support this properly, so disable
-    // it for ARM. See http://crbug.com/230105.
-    screen_capturer_.reset(webrtc::ScreenCapturer::CreateWithXDamage(true));
-#elif defined(OS_WIN)
-    // ScreenCapturerWin disables Aero by default. We don't want it disabled for
-    // WebRTC screen capture, though.
-    screen_capturer_.reset(
-        webrtc::ScreenCapturer::CreateWithDisableAero(false));
-#else
-    screen_capturer_.reset(webrtc::ScreenCapturer::Create());
-#endif
-  }
-
-  if (screen_capturer_)
-    screen_capturer_->Start(this);
+  desktop_capturer_->Start(this);
 
   // Capture first frame, so that we can call OnFrameInfo() callback.
   DoCapture();
 }
 
-void ScreenCaptureDevice::Core::DoStart() {
+void DesktopCaptureDevice::Core::DoStart() {
   DCHECK(task_runner_->RunsTasksOnCurrentThread());
   started_ = true;
   if (!capture_task_posted_) {
@@ -300,20 +284,20 @@ void ScreenCaptureDevice::Core::DoStart() {
   }
 }
 
-void ScreenCaptureDevice::Core::DoStop() {
+void DesktopCaptureDevice::Core::DoStop() {
   DCHECK(task_runner_->RunsTasksOnCurrentThread());
   started_ = false;
   scaled_frame_.reset();
 }
 
-void ScreenCaptureDevice::Core::DoDeAllocate() {
+void DesktopCaptureDevice::Core::DoDeAllocate() {
   DCHECK(task_runner_->RunsTasksOnCurrentThread());
   DoStop();
-  screen_capturer_.reset();
+  desktop_capturer_.reset();
   output_size_.set(0, 0);
 }
 
-void ScreenCaptureDevice::Core::ScheduleCaptureTimer() {
+void DesktopCaptureDevice::Core::ScheduleCaptureTimer() {
   DCHECK(!capture_task_posted_);
   capture_task_posted_ = true;
   task_runner_->PostDelayedTask(
@@ -321,7 +305,7 @@ void ScreenCaptureDevice::Core::ScheduleCaptureTimer() {
       base::TimeDelta::FromSeconds(1) / frame_rate_);
 }
 
-void ScreenCaptureDevice::Core::OnCaptureTimer() {
+void DesktopCaptureDevice::Core::OnCaptureTimer() {
   DCHECK(capture_task_posted_);
   capture_task_posted_ = false;
 
@@ -333,33 +317,81 @@ void ScreenCaptureDevice::Core::OnCaptureTimer() {
   DoCapture();
 }
 
-void ScreenCaptureDevice::Core::DoCapture() {
+void DesktopCaptureDevice::Core::DoCapture() {
   DCHECK(task_runner_->RunsTasksOnCurrentThread());
   DCHECK(!capture_in_progress_);
 
   capture_in_progress_ = true;
-  screen_capturer_->Capture(webrtc::DesktopRegion());
+  desktop_capturer_->Capture(webrtc::DesktopRegion());
 
   // Currently only synchronous implementations of DesktopCapturer are
   // supported.
   DCHECK(!capture_in_progress_);
 }
 
-ScreenCaptureDevice::ScreenCaptureDevice(
-    scoped_refptr<base::SequencedTaskRunner> task_runner)
-    : core_(new Core(task_runner)), name_("Screen", "Screen") {
+// static
+scoped_ptr<media::VideoCaptureDevice> DesktopCaptureDevice::Create(
+    const DesktopMediaID& source) {
+  scoped_refptr<base::SequencedWorkerPool> blocking_pool =
+      BrowserThread::GetBlockingPool();
+  scoped_refptr<base::SequencedTaskRunner> task_runner =
+      blocking_pool->GetSequencedTaskRunner(
+          blocking_pool->GetSequenceToken());
+
+  switch (source.type) {
+    case DesktopMediaID::TYPE_SCREEN: {
+      scoped_ptr<webrtc::DesktopCapturer> capturer;
+
+#if defined(OS_CHROMEOS) && !defined(ARCH_CPU_ARMEL) && defined(USE_X11)
+      // ScreenCapturerX11 polls by default, due to poor driver support for
+      // DAMAGE. ChromeOS' drivers [can be patched to] support DAMAGE properly,
+      // so use it. However ARM driver seems to not support this properly, so
+      // disable it for ARM. See http://crbug.com/230105 .
+      capturer.reset(webrtc::ScreenCapturer::CreateWithXDamage(true));
+#elif defined(OS_WIN)
+      // ScreenCapturerWin disables Aero by default. We don't want it disabled
+      // for WebRTC screen capture, though.
+      capturer.reset(
+          webrtc::ScreenCapturer::CreateWithDisableAero(false));
+#else
+      capturer.reset(webrtc::ScreenCapturer::Create());
+#endif
+
+      return scoped_ptr<media::VideoCaptureDevice>(new DesktopCaptureDevice(
+          task_runner, capturer.Pass()));
+    }
+
+    case DesktopMediaID::TYPE_WINDOW: {
+      scoped_ptr<webrtc::WindowCapturer> capturer(
+          webrtc::WindowCapturer::Create());
+
+      if (!capturer || !capturer->SelectWindow(source.id)) {
+        return scoped_ptr<media::VideoCaptureDevice>();
+      }
+
+      return scoped_ptr<media::VideoCaptureDevice>(new DesktopCaptureDevice(
+          task_runner, capturer.PassAs<webrtc::DesktopCapturer>()));
+    }
+
+    default: {
+      NOTREACHED();
+      return scoped_ptr<media::VideoCaptureDevice>();
+    }
+  }
 }
 
-ScreenCaptureDevice::~ScreenCaptureDevice() {
+DesktopCaptureDevice::DesktopCaptureDevice(
+    scoped_refptr<base::SequencedTaskRunner> task_runner,
+    scoped_ptr<webrtc::DesktopCapturer> capturer)
+    : core_(new Core(task_runner, capturer.Pass())),
+      name_("", "") {
+}
+
+DesktopCaptureDevice::~DesktopCaptureDevice() {
   DeAllocate();
 }
 
-void ScreenCaptureDevice::SetScreenCapturerForTest(
-    scoped_ptr<webrtc::ScreenCapturer> capturer) {
-  core_->SetScreenCapturerForTest(capturer.Pass());
-}
-
-void ScreenCaptureDevice::Allocate(
+void DesktopCaptureDevice::Allocate(
     const media::VideoCaptureCapability& capture_format,
     EventHandler* observer) {
   core_->Allocate(capture_format.width,
@@ -368,19 +400,19 @@ void ScreenCaptureDevice::Allocate(
                   observer);
 }
 
-void ScreenCaptureDevice::Start() {
+void DesktopCaptureDevice::Start() {
   core_->Start();
 }
 
-void ScreenCaptureDevice::Stop() {
+void DesktopCaptureDevice::Stop() {
   core_->Stop();
 }
 
-void ScreenCaptureDevice::DeAllocate() {
+void DesktopCaptureDevice::DeAllocate() {
   core_->DeAllocate();
 }
 
-const media::VideoCaptureDevice::Name& ScreenCaptureDevice::device_name() {
+const media::VideoCaptureDevice::Name& DesktopCaptureDevice::device_name() {
   return name_;
 }
 
