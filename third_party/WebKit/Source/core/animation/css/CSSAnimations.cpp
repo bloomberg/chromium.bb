05862cbe4db7df15715ac278acf881d0c9fce238
alancutter@chromium.org <alancutter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-09 18:27:47 +0000

    Web Animations CSS: Support 'none' keyword in -webkit-animation-name
    CSS animations update did not support "null" animations, instead crashes.
    This patch fixes this issue.
    
    Adds test for crash:
    virtual/web-animations-css/animations/animation-name-none.html
    
    Fixes crashing tests:
    virtual/web-animations-css/animations/animation-shorthand-name-order.html
    virtual/web-animations-css/animations/animation-shorthand-removed.html
    
    Review URL: https://chromiumcodereview.appspot.com/22573006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155853 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 8c212ec909a9d22df684de34ed5d005bbb6d2ecb

--- third_party/WebKit/Source/core/animation/css/CSSAnimations.cpp
+++ third_party/WebKit/Source/core/animation/css/CSSAnimations.cpp
@@ -124,6 +124,8 @@ PassOwnPtr<CSSAnimationUpdate> CSSAnimations::calculateUpdate(const Element* ele
     if (display != NONE) {
         for (size_t i = 0; animationDataList && i < animationDataList->size(); ++i) {
             const CSSAnimationData* animationData = animationDataList->animation(i);
+            if (animationData->isNoneAnimation())
+                continue;
             ASSERT(animationData->isValidAnimation());
             AtomicString animationName(animationData->name());
 
@@ -168,6 +170,8 @@ void CSSAnimations::update(Element* element, const RenderStyle* style)
     if (style->display() != NONE) {
         for (size_t i = 0; animationDataList && i < animationDataList->size(); ++i) {
             const CSSAnimationData* animationData = animationDataList->animation(i);
+            if (animationData->isNoneAnimation())
+                continue;
             ASSERT(animationData->isValidAnimation());
             AtomicString animationName(animationData->name());
 
