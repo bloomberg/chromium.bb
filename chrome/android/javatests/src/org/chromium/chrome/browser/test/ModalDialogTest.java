21903ac0fd57939e3c9e6e484a6cb9698845c445
andrewhayden@chromium.org <andrewhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 15:48:14 +0000

    Disable broken test ModalDialogTest#testBeforeUnloadDialog
    BUG=270593
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/22746003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216698 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f719864b6353bdaed88fd5bec8cf473b4e356c58

--- chrome/android/javatests/src/org/chromium/chrome/browser/test/ModalDialogTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/test/ModalDialogTest.java
@@ -14,6 +14,7 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.browser.JavascriptAppModalDialog;
@@ -170,8 +171,9 @@ public class ModalDialogTest extends ChromiumTestShellTestBase {
      * Verifies beforeunload dialogs are shown and they block/allow navigation
      * as appropriate.
      */
-    @MediumTest
-    @Feature({"Browser", "Main"})
+    //@MediumTest
+    //@Feature({"Browser", "Main"})
+    @DisabledTest //crbug/270593
     public void testBeforeUnloadDialog()
             throws InterruptedException, TimeoutException, ExecutionException {
         loadUrlWithSanitization(BEFORE_UNLOAD_URL);
