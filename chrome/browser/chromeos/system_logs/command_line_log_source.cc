6fbe5a91dd69380204f5aaf24f4d7d6064d33727
alhli@chromium.org <alhli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 02:25:50 +0000

    Add RMS value from cras to feedback.
    Add the RMS value from the loopback stream in cras to the feedback
    report and chrome://system. This value indicates whether cras is
    actually producing audio or not at that instant (as opposed to what is
    coming out from hardware).
    
    BUG=chromium:257154, chromium:215328
    TEST=Built Chrome and deployed to target device. Checked
    chrome://system and verified that cras_rms entry exists and is
    consistent with what is being played through cras.
    
    Review URL: https://chromiumcodereview.appspot.com/22614002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216542 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5eaec7807beae8183d4df85d9e2590af8558ea22

--- chrome/browser/chromeos/system_logs/command_line_log_source.cc
+++ chrome/browser/chromeos/system_logs/command_line_log_source.cc
@@ -33,6 +33,16 @@ void ExecuteCommandLines(chromeos::SystemLogsResponse* response) {
   command.AppendArg("--dump_server_info");
   commands.push_back(std::make_pair("cras", command));
 
+  command = CommandLine((base::FilePath("/usr/bin/cras_test_client")));
+  command.AppendArg("--loopback_file");
+  command.AppendArg("/dev/null");
+  command.AppendArg("--rate");
+  command.AppendArg("44100");
+  command.AppendArg("--duration_seconds");
+  command.AppendArg("0.01");
+  command.AppendArg("--show_total_rms");
+  commands.push_back(std::make_pair("cras_rms", command));
+
   command = CommandLine((base::FilePath("/usr/bin/printenv")));
   commands.push_back(std::make_pair("env", command));
 
