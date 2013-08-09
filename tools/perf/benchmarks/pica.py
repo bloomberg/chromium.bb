52b35fc267d391292fabac1768f5635f7dc7d933
adamk@chromium.org <adamk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 20:02:13 +0000

    Add a Pica page_set and a tap_element action
    This allows using any measurement on Pica without writing Python code.
    
    R=tonyg@chromium.org
    
    Review URL: https://codereview.chromium.org/22309003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216734 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ebbacddd52ca3698e33b0a4b134d9eca942ed068

--- tools/perf/benchmarks/pica.py
+++ tools/perf/benchmarks/pica.py
@@ -2,45 +2,15 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
-
 from telemetry import test
-from telemetry.core import util
 from telemetry.page import page_measurement
-from telemetry.page import page_set
-
 
 class PicaMeasurement(page_measurement.PageMeasurement):
-  def WillNavigateToPage(self, page, tab):
-    page.script_to_evaluate_on_commit = """
-      document.addEventListener('WebComponentsReady', function() {
-        var unused = document.body.offsetHeight; // force layout
-        window.__pica_load_time = performance.now();
-      });
-    """
-
   def MeasurePage(self, _, tab, results):
-    def _IsDone():
-      return tab.EvaluateJavaScript('window.__pica_load_time != undefined')
-    util.WaitFor(_IsDone, 60)
-
     result = int(tab.EvaluateJavaScript('__pica_load_time'))
     results.Add('Total', 'ms', result)
 
 
 class Pica(test.Test):
   test = PicaMeasurement
-
-  def CreatePageSet(self, options):
-    return page_set.PageSet.FromDict({
-        'archive_data_file': '../data/pica.json',
-        # Pica requires non-deterministic Date and Math.random calls
-        # See http://crbug.com/255641
-        'make_javascript_deterministic': False,
-        'pages': [
-          {
-            'url': 'http://www.polymer-project.org/'
-                   'polymer-all/projects/pica/index.html'
-          }
-        ]
-      }, os.path.abspath(__file__))
+  page_set = 'page_sets/pica.json'
