a0fd5577a8da2199c056ab0b9e375ad826c634f3
kkimlabs@chromium.org <kkimlabs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-10 01:49:12 +0000

    [Android] Added UMA for using popup menu (hw/sw button, dragging/tapping)
    TBR=isherman@chromium.org
    NOTRY=true
    
    BUG=267638
    
    Review URL: https://chromiumcodereview.appspot.com/22602004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216799 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 35342d00fedaf05b028401957d84b2a66a5d3704

--- tools/metrics/actions/extract_actions.py
+++ tools/metrics/actions/extract_actions.py
@@ -271,6 +271,10 @@ def AddAndroidActions(actions):
   actions.add('MobileToolbarShowStackView')
   actions.add('MobileToolbarStackViewNewTab')
   actions.add('MobileToolbarToggleBookmark')
+  actions.add('MobileUsingMenuByHwButtonDragging')
+  actions.add('MobileUsingMenuByHwButtonTap')
+  actions.add('MobileUsingMenuBySwButtonDragging')
+  actions.add('MobileUsingMenuBySwButtonTap')
   actions.add('SystemBack')
   actions.add('SystemBackForNavigation')
 
