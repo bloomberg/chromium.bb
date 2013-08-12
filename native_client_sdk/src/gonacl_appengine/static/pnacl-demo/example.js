a7e2600a3a4d56719aa9c80d30188faa91a83a02
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-12 18:07:52 +0000

    [NaCl SDK] Earth demo modifications for gonacl PNaCl.
    NOTE: this change modifies only files that are manually tested, and therefore do not need to be try'd against Chromium or NaCl SDK.
    
    BUG=none
    R=eliben@chromium.org
    
    Review URL: https://codereview.chromium.org/22503002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217039 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b7652c4bbc773cf20e42081324d97e6da2646c97

--- native_client_sdk/src/gonacl_appengine/static/pnacl-demo/example.js
+++ native_client_sdk/src/gonacl_appengine/static/pnacl-demo/example.js
@@ -7,6 +7,16 @@
 var naclModule = null;
 
 /**
+ * A helper function to abbreviate getElementById.
+ *
+ * @param {string} elementId The id to get.
+ * @return {Element}
+ */
+function $(elementId) {
+  return document.getElementById(elementId);
+}
+
+/**
  * MIME type for PNaCl
  *
  * @return {string} MIME type
@@ -26,6 +36,18 @@ function browserSupportsPNaCl() {
 }
 
 /**
+ * Get the URL for Google Cloud Storage.
+ *
+ * @param {string} name The relative path to the file.
+ * @return {string}
+ */
+function getDataURL(name) {
+  var baseUrl =
+    'http://commondatastorage.googleapis.com/gonacl/pnacl-earth-demo/';
+  return baseUrl + name;
+}
+
+/**
  * Create the Native Client <embed> element as a child of the DOM element
  * named "listener".
  *
@@ -41,7 +63,7 @@ function createNaClModule(name, width, height, attrs) {
   moduleEl.setAttribute('width', width);
   moduleEl.setAttribute('height', height);
   moduleEl.setAttribute('path', '');
-  moduleEl.setAttribute('src', name + '.nmf');
+  moduleEl.setAttribute('src', getDataURL(name + '.nmf'));
   moduleEl.setAttribute('type', PNaClmimeType());
 
   // Add any optional arguments
@@ -56,7 +78,7 @@ function createNaClModule(name, width, height, attrs) {
   // instead of attaching the event listeners directly to the <EMBED> element
   // to ensure that the listeners are active before the NaCl module 'load'
   // event fires.
-  var listenerDiv = document.getElementById('listener');
+  var listenerDiv = $('listener');
   listenerDiv.appendChild(moduleEl);
 }
 
@@ -64,14 +86,13 @@ function createNaClModule(name, width, height, attrs) {
  * Add the default event listeners to the element with id "listener".
  */
 function attachDefaultListeners() {
-  var listenerDiv = document.getElementById('listener');
+  var listenerDiv = $('listener');
   listenerDiv.addEventListener('load', moduleDidLoad, true);
+  listenerDiv.addEventListener('error', moduleLoadError, true);
   listenerDiv.addEventListener('progress', moduleLoadProgress, true);
   listenerDiv.addEventListener('message', handleMessage, true);
   listenerDiv.addEventListener('crash', handleCrash, true);
-  if (typeof window.attachListeners !== 'undefined') {
-    window.attachListeners();
-  }
+  attachListeners();
 }
 
 /**
@@ -87,9 +108,6 @@ function handleCrash(event) {
   } else {
     updateStatus('EXITED [' + naclModule.exitStatus + ']');
   }
-  if (typeof window.handleCrash !== 'undefined') {
-    window.handleCrash(naclModule.lastError);
-  }
 }
 
 /**
@@ -98,13 +116,29 @@ function handleCrash(event) {
  * This event listener is registered in attachDefaultListeners above.
  */
 function moduleDidLoad() {
-  var bar = document.getElementById('progress');
+  var bar = $('progress');
   bar.value = 100;
   bar.max = 100;
-  naclModule = document.getElementById('nacl_module');
-  updateStatus('RUNNING');
+  naclModule = $('nacl_module');
+  hideStatus();
+  setThreadCount();
+}
 
-  setAuxElementsVisibility(true);
+/**
+ * Hide the status field and progress bar.
+ */
+function hideStatus() {
+  $('statusField').style.display = 'none';
+  $('progress').style.display = 'none';
+}
+
+/**
+ * Called when the plugin fails to load.
+ *
+ * @param {Object} event
+ */
+function moduleLoadError(event) {
+  updateStatus('Load failed.');
 }
 
 /**
@@ -113,8 +147,10 @@ function moduleDidLoad() {
  * @param {Object} event
  */
 function moduleLoadProgress(event) {
+  $('progress').style.display = 'block';
+
   var loadPercent = 0.0;
-  var bar = document.getElementById('progress');
+  var bar = $('progress');
   bar.max = 100;
   if (event.lengthComputable && event.total > 0) {
     loadPercent = event.loaded / event.total * 100.0;
@@ -126,136 +162,43 @@ function moduleLoadProgress(event) {
 }
 
 /**
- * Return true when |s| starts with the string |prefix|.
- *
- * @param {string} s The string to search.
- * @param {string} prefix The prefix to search for in |s|.
- * @return {bool}
- */
-function startsWith(s, prefix) {
-  // indexOf would search the entire string, lastIndexOf(p, 0) only checks at
-  // the first index. See: http://stackoverflow.com/a/4579228
-  return s.lastIndexOf(prefix, 0) === 0;
-}
-
-/**
- * Add a message to an element with id "log".
- *
- * This function is used by the default "log:" message handler.
- *
- * @param {string} message The message to log.
- */
-function logMessage(message) {
-  document.getElementById('log').textContent = logMessageArray.join('');
-  console.log(message);
-}
-
-var defaultMessageTypes = {
-  'alert': alert,
-  'log': logMessage
-};
-
-/**
- * Called when the NaCl module sends a message to JavaScript (via
- * PPB_Messaging.PostMessage())
- *
- * This event listener is registered in createNaClModule above.
- *
- * @param {Event} message_event A message event. message_event.data contains
- *     the data sent from the NaCl module.
- */
-function handleMessage(message_event) {
-  if (typeof message_event.data === 'string') {
-    for (var type in defaultMessageTypes) {
-      if (defaultMessageTypes.hasOwnProperty(type)) {
-        if (startsWith(message_event.data, type + ':')) {
-          func = defaultMessageTypes[type];
-          func(message_event.data.slice(type.length + 1));
-          return;
-        }
-      }
-    }
-  }
-
-  if (typeof window.handleMessage !== 'undefined') {
-    window.handleMessage(message_event);
-    return;
-  }
-
-  logMessage('Unhandled message: ' + message_event.data + '\n');
-}
-
-/**
- * Called when the DOM content has loaded; i.e. the page's document is fully
- * parsed. At this point, we can safely query any elements in the document via
- * document.querySelector, document.getElementById, etc.
- *
- * @param {string} name The name of the example.
- * @param {number} width The width to create the plugin.
- * @param {number} height The height to create the plugin.
- * @param {Object} attrs Optional dictionary of additional attributes.
- */
-function domContentLoaded(name, width, height, attrs) {
-  // If the page loads before the Native Client module loads, then set the
-  // status message indicating that the module is still loading.  Otherwise,
-  // do not change the status message.
-  setAuxElementsVisibility(false);
-  updateStatus('Page loaded.');
-  if (!browserSupportsPNaCl()) {
-    updateStatus('Browser does not support PNaCl or PNaCl is disabled');
-  } else if (naclModule == null) {
-    // We use a non-zero sized embed to give Chrome space to place the bad
-    // plug-in graphic, if there is a problem.
-    width = typeof width !== 'undefined' ? width : 200;
-    height = typeof height !== 'undefined' ? height : 200;
-    createNaClModule(name, width, height, attrs);
-    attachDefaultListeners();
-  } else {
-    // It's possible that the Native Client module onload event fired
-    // before the page's onload event.  In this case, the status message
-    // will reflect 'SUCCESS', but won't be displayed.  This call will
-    // display the current message.
-    updateStatus('Waiting.');
-  }
-}
-
-/**
  * If the element with id 'statusField' exists, then set its HTML to the status
  * message as well.
  *
  * @param {string} opt_message The message to set.
  */
 function updateStatus(opt_message) {
-  var statusField = document.getElementById('statusField');
+  var statusField = $('statusField');
   if (statusField) {
-    statusField.innerHTML = opt_message;
+    statusField.style.display = 'block';
+    statusField.textContent = opt_message;
   }
 }
 
-function postThreadFunc(numThreads) {
-  return function() {
-    naclModule.postMessage({'message' : 'set_threads',
-                            'value' : numThreads});
-  }
+/**
+ * Send the current value of the element threadCount to the NaCl module.
+ *
+ * @param {number} threads The number of threads to use to render.
+ */
+function setThreadCount(threads) {
+  var value = parseInt($('threadCount').value);
+  naclModule.postMessage({'message': 'set_threads',
+                          'value': value});
 }
 
 // Add event listeners after the NaCl module has loaded.  These listeners will
 // forward messages to the NaCl module via postMessage()
 function attachListeners() {
-  var threads = [0, 1, 2, 4, 6, 8, 12, 16, 24, 32];
-  for (var i = 0; i < threads.length; i++) {
-    document.getElementById('radio' + i).addEventListener('click',
-        postThreadFunc(threads[i]));
-  }
-  document.getElementById('zoomRange').addEventListener('change',
+  $('threadCount').addEventListener('change', setThreadCount);
+  $('zoomRange').addEventListener('change',
     function() {
-      var value = parseFloat(document.getElementById('zoomRange').value);
+      var value = parseFloat($('zoomRange').value);
       naclModule.postMessage({'message' : 'set_zoom',
                               'value' : value});
     });
-  document.getElementById('lightRange').addEventListener('change',
+  $('lightRange').addEventListener('change',
     function() {
-      var value = parseFloat(document.getElementById('lightRange').value);
+      var value = parseFloat($('lightRange').value);
       naclModule.postMessage({'message' : 'set_light',
                               'value' : value});
     });
@@ -279,27 +222,23 @@ function loadTexture(name) {
                             'height' : img.height,
                             'data' : imageData.data.buffer});
   };
-  img.src = name;
+  // A cross-origin request to an image is "tainted", and cannot be read into a
+  // canvas without specifying this. See
+  // https://developer.mozilla.org/en-US/docs/HTML/CORS_Enabled_Image
+  img.crossOrigin = 'Anonymous';
+  img.src = getDataURL(name);
 }
 
 // Handle a message coming from the NaCl module.
 function handleMessage(message_event) {
-  if (message_event.data['message'] == 'benchmark_result') {
-    // benchmark result
-    var result = message_event.data['value'];
-    console.log('Benchmark result:' + result);
-    result = (Math.round(result * 1000) / 1000).toFixed(3);
-    document.getElementById('result').textContent =
-      'Result: ' + result + ' seconds';
-    updateStatus('SUCCESS');
-  } else if (message_event.data['message'] == 'set_zoom') {
+  if (message_event.data['message'] == 'set_zoom') {
     // zoom slider
     var zoom = message_event.data['value'];
-    document.getElementById('zoomRange').value = zoom;
+    $('zoomRange').value = zoom;
   } else if (message_event.data['message'] == 'set_light') {
     // light slider
     var light = message_event.data['value'];
-    document.getElementById('lightRange').value = light;
+    $('lightRange').value = light;
   } else if (message_event.data['message'] == 'request_textures') {
     // NaCl module is requesting a set of textures.
     var names = message_event.data['names'];
@@ -308,42 +247,20 @@ function handleMessage(message_event) {
   }
 }
 
-/**
- * Some elements are "auxiliary" to the NaCl embed area. They should only be
- * shown when the module actually loads. This function allows toggling their
- * visibility.
- *
- * @param {bool} isVisible The requested visibility.
- */
-function setAuxElementsVisibility(isVisible) {
-  var elems = ['config-demo', 'auxnotes'];
-  var visibility = isVisible ? 'block' : 'none';
-
-  for (var i = 0; i < elems.length; ++i) {
-    var elem = document.getElementById(elems[i]);
-    elem.style.display = visibility;
-  }
-}
-
 // Listen for the DOM content to be loaded. This event is fired when parsing of
 // the page's document has finished.
 document.addEventListener('DOMContentLoaded', function() {
-  var body = document.body;
-  // The data-* attributes on the body can be referenced via body.dataset.
-  if (body.dataset) {
-    var attrs = {};
-    if (body.dataset.attrs) {
-      var attr_list = body.dataset.attrs.split(' ');
-      for (var key in attr_list) {
-        var attr = attr_list[key].split('=');
-        var key = attr[0];
-        var value = attr[1];
-        attrs[key] = value;
-      }
-    }
-
-    domContentLoaded(body.dataset.name,
-                     body.dataset.width,
-                     body.dataset.height, attrs);
+  updateStatus('Loading...');
+  if (!browserSupportsPNaCl()) {
+    updateStatus('Browser does not support PNaCl or PNaCl is disabled');
+  } else if (naclModule == null) {
+    createNaClModule('earth', 480, 480);
+    attachDefaultListeners();
+  } else {
+    // It's possible that the Native Client module onload event fired
+    // before the page's onload event.  In this case, the status message
+    // will reflect 'SUCCESS', but won't be displayed.  This call will
+    // display the current message.
+    updateStatus('Waiting.');
   }
 });
