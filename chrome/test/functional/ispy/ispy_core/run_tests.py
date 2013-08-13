37d88f2ef38fad56fed2f19f2eabeaf244aeebef
cgrimm@chromium.org <cgrimm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-13 22:55:40 +0000

    [i-spy] Checked in some essential app-engine files.
    checked out general stuff
    
    BUG=260418
    
    Review URL: https://chromiumcodereview.appspot.com/22810003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217365 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 96f829b48bdb65982002b4356561d2477a8aa73e

---
+++ chrome/test/functional/ispy/ispy_core/run_tests.py
@@ -0,0 +1,10 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+if __name__ == '__main__':
+  suite = unittest.TestLoader().discover('.', pattern='*test.py')
+  unittest.TextTestRunner(verbosity=2).run(suite)
+
