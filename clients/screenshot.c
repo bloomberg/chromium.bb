473f248d7ef31a81d526257f1b309e549abb195c
Kristian Høgsberg <krh@bitplanet.net>
2013-08-12 22:15:38 -0700

    screenshooter: Add missing newline and use program_invocation_short_name

--- clients/screenshot.c
+++ clients/screenshot.c
@@ -268,8 +268,8 @@ int main(int argc, char *argv[])
 
 	if (getenv("WAYLAND_SOCKET") == NULL) {
 		fprintf(stderr, "%s must be launched by weston.\n"
-			"Use the MOD+S shortcut to take a screenshot.",
-			argv[0]);
+			"Use the MOD+S shortcut to take a screenshot.\n",
+			program_invocation_short_name);
 		return -1;
 	}
 
