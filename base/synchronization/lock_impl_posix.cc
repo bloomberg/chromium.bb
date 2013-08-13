26c37429d9c50ed951aa36e370541be76731fa63
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-13 12:04:02 +0000

    Pretty-print the return value from the pthread API.
    New DCHECK messages look like:
    
     Check failed: rv == 0 (35 vs. 0). Resource deadlock avoided
    
    This is less annoying than having to look up the number in /usr/include/asm-generic/errno-base.h
    or the platform equiv. We have strerror() so let's use it.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/22977003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217242 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0b012517e328ea0078bcccd6112f0574cc4c977c

--- base/synchronization/lock_impl_posix.cc
+++ base/synchronization/lock_impl_posix.cc
@@ -5,6 +5,7 @@
 #include "base/synchronization/lock_impl.h"
 
 #include <errno.h>
+#include <string.h>
 
 #include "base/logging.h"
 
@@ -16,13 +17,13 @@ LockImpl::LockImpl() {
   // In debug, setup attributes for lock error checking.
   pthread_mutexattr_t mta;
   int rv = pthread_mutexattr_init(&mta);
-  DCHECK_EQ(rv, 0);
+  DCHECK_EQ(rv, 0) << ". " << strerror(rv);
   rv = pthread_mutexattr_settype(&mta, PTHREAD_MUTEX_ERRORCHECK);
-  DCHECK_EQ(rv, 0);
+  DCHECK_EQ(rv, 0) << ". " << strerror(rv);
   rv = pthread_mutex_init(&os_lock_, &mta);
-  DCHECK_EQ(rv, 0);
+  DCHECK_EQ(rv, 0) << ". " << strerror(rv);
   rv = pthread_mutexattr_destroy(&mta);
-  DCHECK_EQ(rv, 0);
+  DCHECK_EQ(rv, 0) << ". " << strerror(rv);
 #else
   // In release, go with the default lock attributes.
   pthread_mutex_init(&os_lock_, NULL);
@@ -31,23 +32,23 @@ LockImpl::LockImpl() {
 
 LockImpl::~LockImpl() {
   int rv = pthread_mutex_destroy(&os_lock_);
-  DCHECK_EQ(rv, 0);
+  DCHECK_EQ(rv, 0) << ". " << strerror(rv);
 }
 
 bool LockImpl::Try() {
   int rv = pthread_mutex_trylock(&os_lock_);
-  DCHECK(rv == 0 || rv == EBUSY);
+  DCHECK(rv == 0 || rv == EBUSY) << ". " << strerror(rv);
   return rv == 0;
 }
 
 void LockImpl::Lock() {
   int rv = pthread_mutex_lock(&os_lock_);
-  DCHECK_EQ(rv, 0);
+  DCHECK_EQ(rv, 0) << ". " << strerror(rv);
 }
 
 void LockImpl::Unlock() {
   int rv = pthread_mutex_unlock(&os_lock_);
-  DCHECK_EQ(rv, 0);
+  DCHECK_EQ(rv, 0) << ". " << strerror(rv);
 }
 
 }  // namespace internal
