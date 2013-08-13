a4535e258f3093b77386d70e80acac9d5403c739
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-13 02:08:56 +0000

    Expand classList test to cover exception in toString
    When add and remove is called and there is an exception being thrown
    in one of the arguments we need to ensure that we are not calling the
    implementation of add and remove.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22880005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155973 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c30aad83ef3c8b4e9b41375a65c324d87de028df

--- third_party/WebKit/LayoutTests/fast/dom/HTMLElement/script-tests/class-list.js
+++ third_party/WebKit/LayoutTests/fast/dom/HTMLElement/script-tests/class-list.js
@@ -312,6 +312,9 @@ shouldThrowDOMException(function() {
 }, DOMException.INVALID_CHARACTER_ERR);
 shouldBeEqualToString('element.className', '');
 
+shouldThrow('element.classList.add("a", {toString: function() { throw new Error("user error"); }}, "b")', '"Error: user error"');
+shouldBeEqualToString('element.className', '');
+
 createElement('');
 shouldNotThrow('element.classList.add()');
 
@@ -342,6 +345,9 @@ shouldBeEqualToString('element.className', 'a b');
 shouldThrowDOMException(function() {
     element.classList.remove('a', 'b', 'c d');
 }, DOMException.INVALID_CHARACTER_ERR);
+shouldBeEqualToString('element.className', 'a b');
+
+shouldThrow('element.classList.remove("a", {toString: function() { throw new Error("user error"); }}, "b")', '"Error: user error"');
 shouldBeEqualToString('element.className', 'a b');
 
 shouldNotThrow('element.classList.remove()');
