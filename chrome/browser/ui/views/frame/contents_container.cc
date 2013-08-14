e1f791baa1965622d41a526f41095c631566775c
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-14 00:48:55 +0000

    remove dead code in ContentsContainer
    BUG=254670
    TEST=nothing.
    
    Review URL: https://chromiumcodereview.appspot.com/22851002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217412 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cab63ad5003e0e54778d810a240718523f7a61a1

--- chrome/browser/ui/views/frame/contents_container.cc
+++ chrome/browser/ui/views/frame/contents_container.cc
@@ -4,25 +4,13 @@
 
 #include "chrome/browser/ui/views/frame/contents_container.h"
 
-#include "content/public/browser/web_contents.h"
-#include "ui/views/controls/webview/webview.h"
-
-// static
-const char ContentsContainer::kViewClassName[] = "ContentsContainer";
-
 ContentsContainer::ContentsContainer(views::View* active_web_view)
-    : active_(active_web_view),
+    : active_web_view_(active_web_view),
       active_top_margin_(0) {
-  AddChildView(active_);
+  AddChildView(active_web_view_);
 }
 
 ContentsContainer::~ContentsContainer() {
-}
-
-void ContentsContainer::SetActive(views::WebView* overlay) {
-  DCHECK(overlay);
-  active_ = overlay;
-  Layout();
 }
 
 bool ContentsContainer::SetActiveTopMargin(int margin) {
@@ -40,7 +28,7 @@ void ContentsContainer::Layout() {
   int content_y = active_top_margin_;
   int content_height = std::max(0, height() - content_y);
 
-  active_->SetBounds(0, content_y, width(), content_height);
+  active_web_view_->SetBounds(0, content_y, width(), content_height);
 
   // Need to invoke views::View in case any views whose bounds didn't change
   // still need a layout.
@@ -48,5 +36,5 @@ void ContentsContainer::Layout() {
 }
 
 const char* ContentsContainer::GetClassName() const {
-  return kViewClassName;
+  return "ContentsContainer";
 }
--- chrome/browser/ui/views/frame/contents_container.h
+++ chrome/browser/ui/views/frame/contents_container.h
@@ -11,34 +11,12 @@
 #include "base/compiler_specific.h"
 #include "ui/views/view.h"
 
-namespace content {
-class WebContents;
-}
-
-namespace gfx {
-class Rect;
-}
-
-namespace views {
-class WebView;
-};
-
 // ContentsContainer is responsible for managing the active WebContents view.
 // ContentsContainer has one child: the currently active WebContents.
-// TODO(kuan): Remove ContentsContainer since it has only one child now -
-// http://crbug.com/236587.
 class ContentsContainer : public views::View {
  public:
-  // Internal class name
-  static const char kViewClassName[];
-
-  explicit ContentsContainer(views::View* active);
+  explicit ContentsContainer(views::View* active_web_view);
   virtual ~ContentsContainer();
-
-  // Makes the overlay view the active view and nulls out the old active view.
-  // The caller must delete or remove the old active view separately.
-  // Called after |overlay| has been reparented to |ContentsContainer|.
-  void SetActive(views::WebView* overlay);
 
   // Sets the active top margin; the active WebView's y origin would be
   // positioned at this |margin|, causing the active WebView to be pushed down
@@ -50,14 +28,11 @@ class ContentsContainer : public views::View {
   virtual void Layout() OVERRIDE;
   virtual const char* GetClassName() const OVERRIDE;
 
-  // Testing interface:
-  views::View* GetActiveWebViewForTest() { return active_; }
-
  private:
-  views::View* active_;
+  views::View* active_web_view_;
 
   // The margin between the top and the active view. This is used to make the
-  // overlay overlap the bookmark bar on the new tab page.
+  // find bar overlap the detached bookmark bar on the new tab page.
   int active_top_margin_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentsContainer);
