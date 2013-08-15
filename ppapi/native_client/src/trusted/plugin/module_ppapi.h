8834ea9d51252066f160db53ec9aebcd7a4ded15
sehr@google.com <sehr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-15 22:35:32 +0000

    Implement per-browser crash throttling for NaCl processes.
    This CL replicates the crash logic used to throttle plugins.
    
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=359
    R=bbudge@chromium.org, jvoung@chromium.org, mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/23135005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217860 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b9816aa0cbbffb12823238840c2fef73988469ca

--- ppapi/native_client/src/trusted/plugin/module_ppapi.h
+++ ppapi/native_client/src/trusted/plugin/module_ppapi.h
@@ -4,8 +4,6 @@
  * found in the LICENSE file.
  */
 
-#include <deque>
-
 #include "ppapi/c/private/ppb_nacl_private.h"
 #include "ppapi/cpp/module.h"
 
@@ -21,18 +19,9 @@ class ModulePpapi : public pp::Module {
 
   virtual pp::Instance* CreateInstance(PP_Instance pp_instance);
 
-  // NaCl crash throttling.  If RegisterPluginCrash is called too many times
-  // within a time period, IsPluginUnstable reports true.  As long as
-  // IsPluginUnstable returns true, NaCl modules will fail to load.
-  void RegisterPluginCrash();
-  bool IsPluginUnstable();
-
  private:
   bool init_was_successful_;
   const PPB_NaCl_Private* private_interface_;
-
-  // Crash throttling support.
-  std::deque<int64_t> crash_times_;
 };
 
 }  // namespace plugin
