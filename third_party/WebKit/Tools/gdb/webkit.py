aad197614661a4f5212fdf4edeed6a5e54f40cc4
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-15 20:03:29 +0000

    Fix pretty printer for WebCore::KURL.
    From what I can tell, this hasn't worked since abarth cleaned up KURL in
    https://src.chromium.org/viewvc/blink?view=rev&revision=150064.
    
    R=tony@chromium.org
    
    Review URL: https://codereview.chromium.org/22999015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156165 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 8b90e6fbf27ad0ef4ad83576f00d03eab427170b

--- third_party/WebKit/Tools/gdb/webkit.py
+++ third_party/WebKit/Tools/gdb/webkit.py
@@ -170,10 +170,10 @@ class JSCJSStringPrinter(StringPrinter):
         return WTFStringImplPrinter(self.val['m_value']).to_string()
 
 
-class WebCoreKURLGooglePrivatePrinter(StringPrinter):
-    "Print a WebCore::KURLGooglePrivate"
+class WebCoreKURLPrinter(StringPrinter):
+    "Print a WebCore::KURL"
     def to_string(self):
-        return WTFCStringPrinter(self.val['m_utf8']).to_string()
+        return WTFStringPrinter(self.val['m_string']).to_string()
 
 
 class WebCoreQualifiedNamePrinter(StringPrinter):
@@ -281,7 +281,7 @@ def add_pretty_printers():
         (re.compile("^WTF::CString$"), WTFCStringPrinter),
         (re.compile("^WTF::String$"), WTFStringPrinter),
         (re.compile("^WTF::StringImpl$"), WTFStringImplPrinter),
-        (re.compile("^WebCore::KURLGooglePrivate$"), WebCoreKURLGooglePrivatePrinter),
+        (re.compile("^WebCore::KURL$"), WebCoreKURLPrinter),
         (re.compile("^WebCore::QualifiedName$"), WebCoreQualifiedNamePrinter),
         (re.compile("^JSC::Identifier$"), JSCIdentifierPrinter),
         (re.compile("^JSC::JSString$"), JSCJSStringPrinter),
