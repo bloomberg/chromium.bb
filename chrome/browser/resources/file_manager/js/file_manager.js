5024193e3612df61bb064b89b041ca9b460239fd
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 13:51:40 +0000

    Not to show the middle bar during the drive is being mounted.
    This patch make the middle bar hidden during the drive is being mounted. So that the spinner and the middle bar doesn't mix since the spinner is shown only when the drive is being mounted. 
    
    BUG=272380
    TEST=Middle tree works fine.
    
    Review URL: https://chromiumcodereview.appspot.com/23104006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218011 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8870e8dc75308fa665b62a32db00dfe6163b7e31

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1227,7 +1227,10 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
    */
   FileManager.prototype.updateMiddleBarVisibility_ = function() {
     var currentPath = this.directoryModel_.getCurrentDirPath();
-    var visible = DirectoryTreeUtil.isEligiblePathForDirectoryTree(currentPath);
+    var driveStatus = this.volumeManager_.getDriveStatus();
+    var visible =
+        DirectoryTreeUtil.isEligiblePathForDirectoryTree(currentPath) &&
+        driveStatus == VolumeManager.DriveStatus.MOUNTED;
     this.dialogDom_.
         querySelector('.dialog-middlebar-contents').hidden = !visible;
     this.dialogDom_.querySelector('#middlebar-splitter').hidden = !visible;
