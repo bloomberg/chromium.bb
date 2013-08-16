4579e0a0ef208340587768c7e6e5506429119bd4
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 20:10:45 +0000

    [Telemetry] Fix page cycler reliability on mac.
    The page cyclers tend to fail on mac due to hitting the maximum number of
    open file descriptor limit. This was the reason typical 25 is disabled, the
    reason several page cyclers are failing on the waterfall now, and the reason
    for the connection closing hack.
    
    This patch raises the max fd limit rather than trying to stay below the limit.
    It removes the old hack, and re-enables typical 25.
    
    This problem is very hard to reproduce locally, so I'm not 100% confident in
    this patch. I'd like to land it, keep and eye on it, and revert if the failures
    don't get significantly better.
    
    BUG=269727,268646
    
    Review URL: https://chromiumcodereview.appspot.com/23273002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218073 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c947202f15f1df3d1cc2224c10a4d981cd35e017

--- tools/perf/measurements/page_cycler.py
+++ tools/perf/measurements/page_cycler.py
@@ -16,7 +16,6 @@ cycling all pages.
 """
 
 import os
-import sys
 
 from metrics import io
 from metrics import memory
@@ -64,11 +63,6 @@ class PageCycler(page_measurement.PageMeasurement):
 
     # Old commandline flags used for reference builds.
     options.AppendExtraBrowserArg('--dom-automation')
-
-    # Temporarily disable typical_25 page set on mac.
-    if sys.platform == 'darwin' and sys.argv[-1].endswith('/typical_25.json'):
-      print 'typical_25 is currently disabled on mac. Skipping test.'
-      sys.exit(0)
 
   def MeasurePage(self, page, tab, results):
     def _IsDone():
