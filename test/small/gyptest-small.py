13d918c00b53a1bcd97e338cfa4b00085a8206a9
mark@chromium.org <mark@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2013-08-19 19:57:42 +0000

    GTTF: Print much better error message for dependency cycles.
    Example:
    
    gyp: Cycles in .gyp file dependency graph detected:
    Cycle: build/linux/system.gyp -> third_party/zlib/zlib.gyp -> base/base.gyp -> tools/xdisplaycheck/xdisplaycheck.gyp -> build/linux/system.gyp
    Cycle: build/linux/system.gyp -> third_party/zlib/zlib.gyp -> base/base.gyp -> build/linux/system.gyp
    Cycle: base/base.gyp -> tools/xdisplaycheck/xdisplaycheck.gyp -> build/linux/system.gyp -> base/base.gyp
    Cycle: build/linux/system.gyp -> base/base.gyp -> tools/xdisplaycheck/xdisplaycheck.gyp -> build/linux/system.gyp
    Cycle: base/base.gyp -> build/linux/system.gyp -> base/base.gyp
    Cycle: base/base.gyp -> tools/xdisplaycheck/xdisplaycheck.gyp -> build/linux/system.gyp -> third_party/zlib/zlib.gyp -> base/base.gyp
    Cycle: base/base.gyp -> build/linux/system.gyp -> third_party/zlib/zlib.gyp -> base/base.gyp
    Cycle: build/linux/system.gyp -> base/base.gyp -> build/linux/system.gyp
    
    BUG=chromium:35878
    
    Patch by Paweł Hajdan Jr. <phajdan.jr@chromium.org>
    
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1695 78cadc50-ecff-11dd-a971-7dbc132099af

--- test/small/gyptest-small.py
+++ test/small/gyptest-small.py
@@ -30,6 +30,7 @@ files_to_test = [
     'pylib/gyp/generator/ninja_test.py',
     'pylib/gyp/generator/xcode_test.py',
     'pylib/gyp/common_test.py',
+    'pylib/gyp/input_test.py',
 ]
 
 # Collect all the suites from the above files.
