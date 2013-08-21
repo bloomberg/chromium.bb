abcd1a133eaf8cb86cd00fdd09a9bcc072e7efbe
bajones@chromium.org <bajones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 04:59:06 +0000

    Added support for GPU-based expectations
    BUG=245741
    
    Review URL: https://chromiumcodereview.appspot.com/22909011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218650 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f29d2e27e89e20c9bd844bd8fb857f6970e87023

--- content/test/gpu/gpu_tests/webgl_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl_conformance_expectations.py
@@ -9,9 +9,13 @@ from telemetry.page import test_expectations
 # mac leopard snowleopard lion mountainlion
 # linux chromeos android
 # nvidia amd intel
+# Specific gpu's can be listed as a tuple with vendor name and device ID.
+# Example: ('nvidia', 0x1234)
+# Device ID's must be paired with a gpu vendor.
 
 class WebGLConformanceExpectations(test_expectations.TestExpectations):
   def SetExpectations(self):
     # Sample Usage:
-    # self.Fail("gl-enable-vertex-attrib.html", ["mac", "win"], bug=1234)
+    # self.Fail("gl-enable-vertex-attrib.html",
+    #     ['mac', 'amd', ('nvidia', 0x1234)], bug=123)
     self.Fail('uniform-samplers-test.html', ['android'], bug=272080)
