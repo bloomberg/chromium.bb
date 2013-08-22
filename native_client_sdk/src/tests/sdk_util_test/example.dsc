63528f37c00787f72ef0e70a2a5dda53449f48b5
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-22 08:28:50 +0000

    [NaCl SDK] Added sdk_util_test test suite directory.
    Currently has no actual tests. This is a preliminary CL for adding a new module
    to sdk_util with tests.
    
    BUG=266678
    
    Review URL: https://chromiumcodereview.appspot.com/23202007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218953 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f27ccd96eb9fb09a3ca4530c9169eb8a78ad2950

---
+++ native_client_sdk/src/tests/sdk_util_test/example.dsc
@@ -0,0 +1,29 @@
+{
+  'TOOLS': ['newlib', 'glibc', 'pnacl'],
+  'SEL_LDR': True,
+
+  # Need to add ../../examples for common.js
+  'SEARCH': ['.', '../../examples'],
+  'TARGETS': [
+    {
+      'NAME' : 'sdk_util_test',
+      'TYPE' : 'main',
+      'SOURCES' : [
+        'main.cc',
+      ],
+      'DEPS': ['ppapi_simple', 'sdk_util', 'nacl_io'],
+      # Order matters here: gtest has a "main" function that will be used if
+      # referenced before ppapi.
+      'LIBS': ['gmock', 'ppapi_cpp', 'ppapi', 'gtest', 'pthread'],
+      'INCLUDES': ['$(NACL_SDK_ROOT)/include/gtest/internal'],
+      'CXXFLAGS': ['-Wno-sign-compare', '-Wno-unused-private-field'],
+      'CFLAGS_GCC': ['-Wno-unused-local-typedefs'],
+    }
+  ],
+  'DATA': [
+    'example.js'
+  ],
+  'DEST': 'tests',
+  'NAME': 'sdk_util_test',
+  'TITLE': 'SDK Util test',
+}
---
+++ native_client_sdk/src/tests/sdk_util_test/main.cc
@@ -0,0 +1,66 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <string>
+
+#include "gtest/gtest.h"
+
+#if defined(SEL_LDR)
+
+int main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
+#else
+
+#include "ppapi/cpp/instance.h"
+#include "ppapi/cpp/var.h"
+#include "ppapi_simple/ps_main.h"
+
+#if defined(WIN32)
+#include <Windows.h>
+#undef PostMessage
+#endif
+
+class GTestEventListener : public ::testing::EmptyTestEventListener {
+ public:
+  // TestEventListener overrides.
+  virtual void OnTestStart(const ::testing::TestInfo& test_info) {
+    std::stringstream msg;
+    msg << "start:" << test_info.test_case_name() << "." << test_info.name();
+    pp::Instance(PSGetInstanceId()).PostMessage(msg.str());
+  }
+
+  virtual void OnTestPartResult(
+      const ::testing::TestPartResult& test_part_result) {
+    if (test_part_result.failed()) {
+      std::stringstream msg;
+      msg << "fail:" << test_part_result.file_name() << ","
+          << test_part_result.line_number() << ","
+          << test_part_result.summary();
+      pp::Instance(PSGetInstanceId()).PostMessage(msg.str());
+    }
+  }
+
+  virtual void OnTestEnd(const ::testing::TestInfo& test_info) {
+    std::stringstream msg;
+    msg << "end:" << test_info.test_case_name() << "." << test_info.name()
+        << "," << (test_info.result()->Failed() ? "failed" : "ok");
+    pp::Instance(PSGetInstanceId()).PostMessage(msg.str());
+  }
+};
+
+int example_main(int argc, char* argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  ::testing::UnitTest::GetInstance()->listeners()
+      .Append(new GTestEventListener());
+  return RUN_ALL_TESTS();
+}
+
+// Register the function to call once the Instance Object is initialized.
+// see: pappi_simple/ps_main.h
+PPAPI_SIMPLE_REGISTER_MAIN(example_main);
+
+#endif
