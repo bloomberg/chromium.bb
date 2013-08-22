8e3b23f2e26415c1c429b8af141f0b7b3646019d
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-22 22:47:35 +0000

    RemoveNodeCommand should attach lazily.
    Review URL: https://chromiumcodereview.appspot.com/22909042
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156588 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 05cff975e35b9db0e82f33b668f4714ea6e02e77

--- third_party/WebKit/Source/core/editing/RemoveNodeCommand.cpp
+++ third_party/WebKit/Source/core/editing/RemoveNodeCommand.cpp
@@ -62,7 +62,7 @@ void RemoveNodeCommand::doUnapply()
     if (!parent || !parent->rendererIsEditable())
         return;
 
-    parent->insertBefore(m_node.get(), refChild.get(), IGNORE_EXCEPTION, DeprecatedAttachNow);
+    parent->insertBefore(m_node.get(), refChild.get(), IGNORE_EXCEPTION);
 }
 
 #ifndef NDEBUG
