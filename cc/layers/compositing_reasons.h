42851a5e4c6bb61f1e40ac117b56c3844c0413c8
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-22 15:49:36 +0000

    Add plumbing for scroll parent.
    Plumbs the scroll parent information into the cc layer trees. These relationships are not yet used. (That task is accomplished here https://codereview.chromium.org/18191009/).
    
    R=shawnsingh@chromium.org
    BUG=254435
    
    Review URL: https://chromiumcodereview.appspot.com/18133004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219026 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 167c71249ca6de270b3dba75ca94f5de2deb3ea4

--- cc/layers/compositing_reasons.h
+++ cc/layers/compositing_reasons.h
@@ -52,6 +52,8 @@ const uint64 kCompositingReasonLayerForScrollingContainer =
 const uint64 kCompositingReasonLayerForForeground = GG_UINT64_C(1) << 29;
 const uint64 kCompositingReasonLayerForBackground = GG_UINT64_C(1) << 30;
 const uint64 kCompositingReasonLayerForMask = GG_UINT64_C(1) << 31;
+const uint64 kCompositingReasonOverflowScrollingParent = GG_UINT64_C(1) << 32;
+const uint64 kCompositingReasonOutOfFlowClipping = GG_UINT64_C(1) << 33;
 
 typedef uint64 CompositingReasons;
 
