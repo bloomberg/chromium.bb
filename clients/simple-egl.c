777874350ba5f28c7d30310e278a0893313daf15
Kristian Høgsberg <krh@bitplanet.net>
2013-08-22 12:16:51 -0700

    simple-egl: Handle missing EGL_EXT_buffer_age better
    Including src/weston-egl-ext.h breaks distcheck, so lets just copy the
    one EGL_EXT_buffer_age into simple-egl.c.

--- clients/simple-egl.c
+++ clients/simple-egl.c
@@ -38,11 +38,14 @@
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 
-#include <src/weston-egl-ext.h>
-
 #ifndef EGL_EXT_swap_buffers_with_damage
 #define EGL_EXT_swap_buffers_with_damage 1
 typedef EGLBoolean (EGLAPIENTRYP PFNEGLSWAPBUFFERSWITHDAMAGEEXTPROC)(EGLDisplay dpy, EGLSurface surface, EGLint *rects, EGLint n_rects);
+#endif
+
+#ifndef EGL_EXT_buffer_age
+#define EGL_EXT_buffer_age 1
+#define EGL_BUFFER_AGE_EXT			0x313D
 #endif
 
 struct window;
