8f5e093549fb9830eab988340950fc930b5f7d4d
falken@chromium.org <falken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-26 14:55:37 +0000

    Set <dialog> width and height to 'fit-content'.
    As per the spec: http://html5.org/r/8151
    
    BUG=140205
    TBR=tkent
    
    Review URL: https://chromiumcodereview.appspot.com/23060031
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156704 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d77f1a9d143a0449647182d0d6ba42fd3672c1b8

--- third_party/WebKit/Source/core/rendering/RenderTheme.cpp
+++ third_party/WebKit/Source/core/rendering/RenderTheme.cpp
@@ -497,7 +497,7 @@ String RenderTheme::extraDefaultStyleSheet()
     }
     if (RuntimeEnabledFeatures::dialogElementEnabled()) {
         runtimeCSS.appendLiteral("dialog:not([open]) { display: none; }");
-        runtimeCSS.appendLiteral("dialog { position: absolute; left: 0; right: 0; margin: auto; border: solid; padding: 1em; background: white; color: black;}");
+        runtimeCSS.appendLiteral("dialog { position: absolute; left: 0; right: 0; width: -webkit-fit-content; height: -webkit-fit-content; margin: auto; border: solid; padding: 1em; background: white; color: black;}");
         runtimeCSS.appendLiteral("dialog::backdrop { background: rgba(0,0,0,0.1); }");
     }
 
