2e040e7cf4c6590b8217888708018aec4ae13e57
spang@chromium.org <spang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-27 06:55:46 +0000

    Add svg mime type to WebUIDataSourceImpl::GetMimeType
    This is used by the onscreen keyboard.
    
    BUG=279533
    
    Review URL: https://chromiumcodereview.appspot.com/23469005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219722 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4f80a9d8a0a9878916cbe6e7277c0414bc91668c

--- content/browser/webui/web_ui_data_source_impl.cc
+++ content/browser/webui/web_ui_data_source_impl.cc
@@ -176,6 +176,9 @@ std::string WebUIDataSourceImpl::GetMimeType(const std::string& path) const {
   if (EndsWith(path, ".pdf", false))
     return "application/pdf";
 
+  if (EndsWith(path, ".svg", false))
+    return "image/svg+xml";
+
   return "text/html";
 }
 
