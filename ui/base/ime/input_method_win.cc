b5d08a6f6dc94af110852b773ce8d0fa7d32f4f3
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-27 22:05:14 +0000

    This change regressed Chrome ASH on Windows 8. DispatchFabricatedKeyEvent is used by Chrome ASH on Windows to route keyboard messages from the viewer process to the browser.
    Revert 219492 "Drop DispatchFabrictedKeyEvent on Windows."
    
    > Drop DispatchFabrictedKeyEvent on Windows.
    > 
    > DispatchFabricatedKeyEvent should not be used on Windows.
    > The virtual keyboard on Windows works as normal keyboard event or works on TSF
    > which is independent from keyboard event.
    > 
    > BUG=None
    > TEST=Manually confirmed on Windows.
    > TBR=yukawa
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23304016
    
    TBR=nona@chromium.org
    
    Review URL: https://codereview.chromium.org/23614003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219840 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 21bdd6da9c62e152f92242292e34ce7a7be1aa51

--- ui/base/ime/input_method_win.cc
+++ ui/base/ime/input_method_win.cc
@@ -73,8 +73,20 @@ bool InputMethodWin::DispatchKeyEvent(
 }
 
 bool InputMethodWin::DispatchFabricatedKeyEvent(const ui::KeyEvent& event) {
-  NOTREACHED() << "Fabricated Key Event should not be received on Windows.";
-  return false;
+  // TODO(ananta)
+  // Support IMEs and RTL layout in Windows 8 metro Ash. The code below won't
+  // work with IMEs.
+  // Bug: https://code.google.com/p/chromium/issues/detail?id=164964
+  if (event.is_char()) {
+    if (GetTextInputClient()) {
+      GetTextInputClient()->InsertChar(event.key_code(),
+                                       ui::GetModifiersFromKeyState());
+      return true;
+    }
+  }
+  return DispatchFabricatedKeyEventPostIME(event.type(),
+                                           event.key_code(),
+                                           event.flags());
 }
 
 void InputMethodWin::OnInputLocaleChanged() {
