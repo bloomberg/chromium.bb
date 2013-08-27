7d216e6999df46fecfd5359662627c4a761437d0
rvargas@chromium.org <rvargas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-27 05:33:57 +0000

    App: Make sure unexpected exit paths generate crash dumps for any process.
    Right now it is possible for some random code to directly terminate some
    process (most notably the browser) without generating a crash dump.
    
    This CL moves r217049 to a place where it covers any process type while
    at the same type is Chrome-specific (not part of content). It is a 'minimal
    patch' designed to be applied to and older branch.
    
    BUG=275818
    TEST=none
    R=thestig@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23190042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219702 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 36a2f0837ffaad36ed9673c05ef2306d006168f0

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -7,6 +7,8 @@
 #include "content/public/app/content_main.h"
 
 #if defined(OS_WIN)
+#include "base/win/win_util.h"
+
 #define DLLEXPORT __declspec(dllexport)
 
 // We use extern C for the prototype DLLEXPORT to avoid C++ name mangling.
@@ -24,8 +26,13 @@ int ChromeMain(int argc, const char** argv);
 #if defined(OS_WIN)
 DLLEXPORT int __cdecl ChromeMain(HINSTANCE instance,
                                  sandbox::SandboxInterfaceInfo* sandbox_info) {
+  // The process should crash when going through abnormal termination.
+  base::win::SetShouldCrashOnProcessDetach(true);
+  base::win::SetAbortBehaviorForCrashReporting();
   ChromeMainDelegate chrome_main_delegate;
-  return content::ContentMain(instance, sandbox_info, &chrome_main_delegate);
+  int rv = content::ContentMain(instance, sandbox_info, &chrome_main_delegate);
+  base::win::SetShouldCrashOnProcessDetach(false);
+  return rv;
 #elif defined(OS_POSIX)
 int ChromeMain(int argc, const char** argv) {
   ChromeMainDelegate chrome_main_delegate;
