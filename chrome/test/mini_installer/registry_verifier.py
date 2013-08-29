c3955d6b9eef7f61bca63e9a9817fe2fbd656cc0
sukolsak@chromium.org <sukolsak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 21:48:37 +0000

    Verify registry values in the registry verifier.
    NOTRY=True 
    BUG=264859
    TEST= 
    1) Uninstall Chrome. 
    2) Put mini_installer.exe in the same folder as test_installer.py. 
    3) Run "python test_installer.py config\config.config". 
    4) The script will install Chrome and then uninstall Chrome. At each state, it will check that chrome.exe is running (or not running). You should see output similar to the following: 
    
    "Test: clean -> install chrome at user level -> chrome_installed_not_inuse -> launch chrome at user level -> chrome_installed_inuse -> quit chrome -> chrome_installed_not_inuse -> uninstall chrome -> clean ... ok
    
    ---------------------------------------------------------------------- 
    Ran 1 test in 12.345s"
    
    Review URL: https://chromiumcodereview.appspot.com/23583005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220384 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 79af97d6819ff75dc1580f4ec50e347d0c5d9b03

--- chrome/test/mini_installer/registry_verifier.py
+++ chrome/test/mini_installer/registry_verifier.py
@@ -17,43 +17,91 @@ def VerifyRegistryEntries(entries):
     VerifyRegistryEntry(key, expectation)
 
 
-def RootKeyConstant(key):
+def RootKeyConstant(root_key):
   """Converts a root registry key string into a _winreg.HKEY_* constant."""
-  if key == 'HKEY_CLASSES_ROOT':
-    return _winreg.HKEY_CLASSES_ROOT
-  if key == 'HKEY_CURRENT_USER':
-    return _winreg.HKEY_CURRENT_USER
-  if key == 'HKEY_LOCAL_MACHINE':
-    return _winreg.HKEY_LOCAL_MACHINE
-  if key == 'HKEY_USERS':
-    return _winreg.HKEY_USERS
-  raise KeyError("Unknown root registry key '%s'" % key)
+  root_key_mapping = {
+      'HKEY_CLASSES_ROOT': _winreg.HKEY_CLASSES_ROOT,
+      'HKEY_CURRENT_USER': _winreg.HKEY_CURRENT_USER,
+      'HKEY_LOCAL_MACHINE': _winreg.HKEY_LOCAL_MACHINE,
+      'HKEY_USERS': _winreg.HKEY_USERS,
+  }
+  if root_key not in root_key_mapping:
+    raise KeyError("Unknown root registry key '%s'" % root_key)
+  return root_key_mapping[root_key]
+
+
+def ValueTypeConstant(value_type):
+  """Converts a registry value type string into a _winreg.REG_* constant."""
+  value_type_mapping = {
+      'BINARY': _winreg.REG_BINARY,
+      'DWORD': _winreg.REG_DWORD,
+      'DWORD_LITTLE_ENDIAN': _winreg.REG_DWORD_LITTLE_ENDIAN,
+      'DWORD_BIG_ENDIAN': _winreg.REG_DWORD_BIG_ENDIAN,
+      'EXPAND_SZ': _winreg.REG_EXPAND_SZ,
+      'LINK': _winreg.REG_LINK,
+      'MULTI_SZ': _winreg.REG_MULTI_SZ,
+      'NONE': _winreg.REG_NONE,
+      'SZ': _winreg.REG_SZ,
+  }
+  if value_type not in value_type_mapping:
+    raise KeyError("Unknown registry value type '%s'" % value_type)
+  return value_type_mapping[value_type]
 
 
 def VerifyRegistryEntry(key, expectation):
   """Verifies a registry key according to the |expectation|.
 
   The |expectation| specifies whether or not the registry key should exist
-  (under 'exists') and optionally specifies an expected 'value' for the key.
+  (under 'exists') and optionally specifies expected 'values' for the key.
 
   Args:
-    key: Name of the registry key.
+    key: Name of the registry key. It is expanded using ResolvePath.
     expectation: A dictionary with the following keys and values:
-        'exists' a boolean indicating whether the registry entry should exist.
-        'value' (optional) a string representing the expected value for
-            the key.
+        'exists' a boolean indicating whether the registry key should exist.
+        'values' (optional) a dictionary where each key is a registry value and
+            its associated value is a dictionary with the following key and
+            values:
+                'type' a string indicating the type of the registry value.
+                'data' the associated data of the registry value. If it is a
+                    string, it is expanded using ResolvePath.
   """
-  root_key, sub_key = key.split('\\', 1)
-  resolved_sub_key = path_resolver.ResolvePath(sub_key)
+  resolved_key = path_resolver.ResolvePath(key)
+  root_key, sub_key = resolved_key.split('\\', 1)
   try:
     # Query the Windows registry for the registry key. It will throw a
     # WindowsError if the key doesn't exist.
-    _ = _winreg.OpenKey(RootKeyConstant(root_key), resolved_sub_key, 0,
-                        _winreg.KEY_READ)
+    key_handle = _winreg.OpenKey(RootKeyConstant(root_key), sub_key, 0,
+                                 _winreg.KEY_QUERY_VALUE)
   except WindowsError:
     # Key doesn't exist. See that it matches the expectation.
-    assert not expectation['exists'], 'Registry entry %s is missing' % key
+    assert not expectation['exists'], ('Registry key %s is missing' %
+                                       resolved_key)
     return
   # The key exists, see that it matches the expectation.
-  assert expectation['exists'], 'Registry entry %s exists' % key
-  # TODO(sukolsak): Verify the expected value.
+  assert expectation['exists'], ('Registry key %s exists' % resolved_key)
+
+  # Verify the expected values.
+  if 'values' not in expectation:
+    return
+  for value, value_expectation in expectation['values'].iteritems():
+    # Query the value. It will throw a WindowsError if the value doesn't exist.
+    try:
+      data, value_type = _winreg.QueryValueEx(key_handle, value)
+    except WindowsError:
+      raise KeyError("Value '%s' of registry key %s is missing" % (
+          value, resolved_key))
+
+    # Verify the type of the value.
+    expected_value_type = value_expectation['type']
+    assert ValueTypeConstant(expected_value_type) == value_type, \
+        "Value '%s' of registry key %s has unexpected type '%s'" % (
+            value, resolved_key, expected_value_type)
+
+    # Verify the associated data of the value.
+    expected_data = value_expectation['data']
+    if isinstance(expected_data, basestring):
+      expected_data = path_resolver.ResolvePath(expected_data)
+    assert expected_data == data, \
+        ("Value '%s' of registry key %s has unexpected data.\n"
+         "  Expected: %s\n"
+         "  Actual: %s" % (value, resolved_key, expected_data, data))
