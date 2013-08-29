e8cd2fc0fcb260087b239e30e9b8338cac1ec749
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 09:10:13 +0000

    Add --cold-load-percent option to page cycler to allow running tests with some cold and some warm cache loads.
    Review URL: https://chromiumcodereview.appspot.com/23113002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220231 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 86fe06c11ec22a81d667b328d04efc8d86b294ec

--- tools/perf/measurements/page_cycler.py
+++ tools/perf/measurements/page_cycler.py
@@ -15,6 +15,7 @@ Finally, various memory and IO statistics are gathered at the very end of
 cycling all pages.
 """
 
+import collections
 import os
 import sys
 
@@ -32,6 +33,9 @@ class PageCycler(page_measurement.PageMeasurement):
       self._page_cycler_js = f.read()
 
     self._memory_metric = None
+    self._number_warm_runs = None
+    self._cold_runs_requested = False
+    self._has_loaded_page = collections.defaultdict(int)
 
   def AddCommandLineOptions(self, parser):
     # The page cyclers should default to 10 iterations. In order to change the
@@ -41,6 +45,9 @@ class PageCycler(page_measurement.PageMeasurement):
     pageset_repeat_option.default = 10
     parser.remove_option('--pageset-repeat')
     parser.add_option(pageset_repeat_option)
+
+    parser.add_option('--cold-load-percent', type='int', default=0,
+                      help='%d of page visits for which a cold load is forced')
 
   def DidStartBrowser(self, browser):
     """Initialize metrics once right after the browser has been launched."""
@@ -53,6 +60,8 @@ class PageCycler(page_measurement.PageMeasurement):
 
   def WillNavigateToPage(self, page, tab):
     page.script_to_evaluate_on_commit = self._page_cycler_js
+    if self.ShouldRunCold(page.url):
+      tab.ClearCache()
 
   def DidNavigateToPage(self, page, tab):
     self._memory_metric.Start(page, tab)
@@ -71,13 +80,44 @@ class PageCycler(page_measurement.PageMeasurement):
       print '%s is currently disabled on mac. Skipping test.' % sys.argv[-1]
       sys.exit(0)
 
+    # Handle requests for cold cache runs
+    if (options.cold_load_percent and
+        (options.repeat_options.page_repeat_secs or
+         options.repeat_options.pageset_repeat_secs)):
+      raise Exception('--cold-load-percent is incompatible with timed repeat')
+
+    if (options.cold_load_percent and
+        (options.cold_load_percent < 0 or options.cold_load_percent > 100)):
+      raise Exception('--cold-load-percent must be in the range [0-100]')
+
+    # TODO(rdsmith): Properly handle interaction of page_repeat with
+    # dropping the first run.
+    number_warm_pageset_runs = int(
+        (int(options.repeat_options.pageset_repeat_iters) - 1) *
+        (100 - options.cold_load_percent) / 100)
+
+    # Make sure _number_cold_runs is an integer multiple of page_repeat.
+    # Without this, --pageset_shuffle + --page_repeat could lead to
+    # assertion failures on _started_warm in WillNavigateToPage.
+    self._number_warm_runs = (number_warm_pageset_runs *
+                              options.repeat_options.page_repeat_iters)
+    self._cold_runs_requested = bool(options.cold_load_percent)
+    self.discard_first_result = (bool(options.cold_load_percent) or
+                                 self.discard_first_result)
+
   def MeasurePage(self, page, tab, results):
     def _IsDone():
       return bool(tab.EvaluateJavaScript('__pc_load_time'))
     util.WaitFor(_IsDone, 60)
+    chart_name = ('times' if not self._cold_runs_requested else
+                  'cold_times' if self.ShouldRunCold(page.url) else
+                  'warm_times')
+
     results.Add('page_load_time', 'ms',
                 int(float(tab.EvaluateJavaScript('__pc_load_time'))),
-                chart_name='times')
+                chart_name=chart_name)
+
+    self._has_loaded_page[page.url] += 1
 
     self._memory_metric.Stop(page, tab)
     self._memory_metric.AddResults(tab, results)
@@ -86,3 +126,15 @@ class PageCycler(page_measurement.PageMeasurement):
     self._memory_metric.AddSummaryResults(results)
     io.IOMetric().AddSummaryResults(tab, results)
 
+  def ShouldRunCold(self, url):
+    # We do the warm runs first for two reasons.  The first is so we can
+    # preserve any initial profile cache for as long as possible.
+    # The second is that, if we did cold runs first, we'd have a transition
+    # page set during which we wanted the run for each URL to both
+    # contribute to the cold data and warm the catch for the following
+    # warm run, and clearing the cache before the load of the following
+    # URL would eliminate the intended warmup for the previous URL.
+    return self._has_loaded_page[url] >= self._number_warm_runs + 1
+
+  def results_are_the_same_on_every_page(self):
+    return not self._cold_runs_requested
---
+++ tools/perf/measurements/page_cycler_unittest.py
@@ -0,0 +1,133 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import unittest
+
+from measurements import page_cycler
+from telemetry.core import browser_options
+from telemetry.page import page_measurement_results
+from telemetry.unittest import simple_mock
+
+# Allow testing protected members in the unit test.
+# pylint: disable=W0212
+
+# Used instead of simple_mock.MockObject so that the precise order and
+# number of calls need not be specified.
+class MockMemoryMetric(object):
+  def __init__(self):
+    pass
+
+  def Start(self, page, tab):
+    pass
+
+  def Stop(self, page, tab):
+    pass
+
+  def AddResults(self, tab, results):
+    pass
+
+  def AddSummaryResults(self, tab, results):
+    pass
+
+class PageCyclerUnitTest(unittest.TestCase):
+
+  # TODO(tonyg): Remove this backfill when we can assume python 2.7 everywhere.
+  def assertIn(self, first, second, msg=None):
+    self.assertTrue(first in second,
+                    msg="'%s' not found in '%s'" % (first, second))
+
+  def setupCycler(self, args):
+    cycler = page_cycler.PageCycler()
+    options = browser_options.BrowserOptions()
+    parser = options.CreateParser()
+    cycler.AddCommandLineOptions(parser)
+    parser.parse_args(args)
+    cycler.CustomizeBrowserOptions(options)
+
+    return cycler
+
+  def testOptionsColdLoadNoArgs(self):
+    cycler = self.setupCycler([])
+
+    self.assertFalse(cycler._cold_runs_requested)
+    self.assertEqual(cycler._number_warm_runs, 9)
+
+  def testOptionsColdLoadPagesetRepeat(self):
+    cycler = self.setupCycler(['--pageset-repeat=20', '--page-repeat=2'])
+
+    self.assertFalse(cycler._cold_runs_requested)
+    self.assertEqual(cycler._number_warm_runs, 38)
+
+  def testOptionsColdLoadRequested(self):
+    cycler = self.setupCycler(['--pageset-repeat=21', '--page-repeat=2',
+                               '--cold-load-percent=40'])
+
+    self.assertTrue(cycler._cold_runs_requested)
+    self.assertEqual(cycler._number_warm_runs, 24)
+
+  def testIncompatibleOptions(self):
+    exception_seen = False
+    try:
+      self.setupCycler(['--pageset-repeat=20s', '--page-repeat=2s',
+                        '--cold-load-percent=40'])
+    except Exception as e:
+      exception_seen = True
+      self.assertEqual('--cold-load-percent is incompatible with '
+                       'timed repeat', e.args[0])
+
+    self.assertTrue(exception_seen)
+
+  def testCacheHandled(self):
+    cycler = self.setupCycler(['--pageset-repeat=5',
+                               '--cold-load-percent=50'])
+
+    # Mock out memory metrics; the real ones require a real browser.
+    mock_memory_metric = MockMemoryMetric()
+
+    mock_memory_module = simple_mock.MockObject()
+    mock_memory_module.ExpectCall(
+      'MemoryMetric').WithArgs(simple_mock.DONT_CARE).WillReturn(
+      mock_memory_metric)
+
+    real_memory_module = page_cycler.memory
+    try:
+      page_cycler.memory = mock_memory_module
+      cycler.DidStartBrowser(None)
+    finally:
+      page_cycler.memory = real_memory_module
+
+    class FakePage(object):
+      def __init__(self, url):
+        self.url = url
+
+    class FakeTab(object):
+      def __init__(self):
+        self.clear_cache_calls = 0
+      def ClearCache(self):
+        self.clear_cache_calls += 1
+      def EvaluateJavaScript(self, _):
+        return 1
+
+    url_name = "http://fakepage.com"
+    page = FakePage(url_name)
+    tab = FakeTab()
+    results = page_measurement_results.PageMeasurementResults()
+
+    for i in range(5):
+      cycler.WillNavigateToPage(page, tab)
+      self.assertEqual(max(0, i - 2), tab.clear_cache_calls,
+                       "Iteration %d tab.clear_cache_calls %d" %
+                       (i, tab.clear_cache_calls))
+      num_results = len(results.page_results)
+      results.WillMeasurePage(page)
+      cycler.MeasurePage(page, tab, results)
+      results.DidMeasurePage()
+      self.assertEqual(num_results+1, len(results.page_results))
+      result = results[-1]
+      self.assertEqual(result.page, page)
+      self.assertEqual(1, len(result.values))
+      self.assertEqual(result.values[0].trace_name, 'page_load_time')
+      self.assertEqual(result.values[0].units, 'ms')
+      self.assertEqual(result.values[0].chart_name,
+                       'warm_times' if i < 3 else 'cold_times')
+      cycler.DidNavigateToPage(page, tab)
