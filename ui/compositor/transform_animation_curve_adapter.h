d55cbbdfa0beafc8f7a323e0d35569d46122ed72
wittman@chromium.org <wittman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 00:39:26 +0000

    Revert 220124 "Add support for inverse transform animations."
    > Add support for inverse transform animations.
    > 
    > This allows creation of a transform and its inverse that would be used
    > to counter-animate some child layer.
    > 
    > BUG=270857
    > 
    > Review URL: https://chromiumcodereview.appspot.com/22861008
    
    TBR=avallee@chromium.org
    
    Review URL: https://codereview.chromium.org/23592016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220141 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 45643dc6cd3f9358427802e707ddde35d2745e34

--- ui/compositor/transform_animation_curve_adapter.h
+++ ui/compositor/transform_animation_curve_adapter.h
@@ -8,14 +8,12 @@
 #include "base/time/time.h"
 #include "cc/animation/animation_curve.h"
 #include "ui/base/animation/tween.h"
-#include "ui/compositor/compositor_export.h"
 #include "ui/gfx/transform.h"
 #include "ui/gfx/transform_util.h"
 
 namespace ui {
 
-class COMPOSITOR_EXPORT TransformAnimationCurveAdapter
-    : public cc::TransformAnimationCurve {
+class TransformAnimationCurveAdapter :public cc::TransformAnimationCurve {
  public:
   TransformAnimationCurveAdapter(Tween::Type tween_type,
                                  gfx::Transform intial_value,
@@ -38,32 +36,6 @@ class COMPOSITOR_EXPORT TransformAnimationCurveAdapter
   gfx::DecomposedTransform decomposed_initial_value_;
   gfx::DecomposedTransform decomposed_target_value_;
   base::TimeDelta duration_;
-
-  DISALLOW_ASSIGN(TransformAnimationCurveAdapter);
-};
-
-class COMPOSITOR_EXPORT InverseTransformCurveAdapter
-    : public cc::TransformAnimationCurve {
- public:
-  InverseTransformCurveAdapter(TransformAnimationCurveAdapter base_curve,
-                               gfx::Transform initial_value,
-                               base::TimeDelta duration);
-
-  virtual ~InverseTransformCurveAdapter();
-
-  virtual double Duration() const OVERRIDE;
-  virtual scoped_ptr<AnimationCurve> Clone() const OVERRIDE;
-  virtual gfx::Transform GetValue(double t) const OVERRIDE;
-  virtual bool AnimatedBoundsForBox(const gfx::BoxF& box,
-                                    gfx::BoxF* bounds) const OVERRIDE;
-
- private:
-  TransformAnimationCurveAdapter base_curve_;
-  gfx::Transform initial_value_;
-  gfx::Transform effective_initial_value_;
-  base::TimeDelta duration_;
-
-  DISALLOW_ASSIGN(InverseTransformCurveAdapter);
 };
 
 }  // namespace ui
