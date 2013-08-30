dbf5d14bfa67e5196230d4a83b9905514199fe51
jansson@chromium.org <jansson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-30 11:19:07 +0000

    Swapped force OPUS to iSAC
    Chrome uses OPUS by default hence it makes sense to force ISAC instead.
    
    BUG=279101
    NOTRY=TRUE
    TEST=Manual test verifying that iSAC is used instead of OPUS in chrome://webrtc-internals and that the sound is played back OK.
    
    Review URL: https://chromiumcodereview.appspot.com/23460010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220550 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9b06b2f03f9ca34356591db67b27bd19fd6a6bf3

--- chrome/test/data/webrtc/manual/peerconnection.js
+++ chrome/test/data/webrtc/manual/peerconnection.js
@@ -117,10 +117,10 @@ function insertDtmfFromHere() {
   insertDtmfOnSender(tones, duration, gap);
 }
 
-function forceOpusChanged() {
-  var forceOpus = $('force-opus').checked;
-  if (forceOpus) {
-    forceOpus_();
+function forceIsacChanged() {
+  var forceIsac = $('force-isac').checked;
+  if (forceIsac) {
+    forceIsac_();
   } else {
     dontTouchSdp_();
   }
@@ -332,12 +332,13 @@ function preferOpus_() {
 }
 
 /** @private */
-function forceOpus_() {
+function forceIsac_() {
   setOutgoingSdpTransform(function(sdp) {
     // Remove all other codecs (not the video codecs though).
     sdp = sdp.replace(/m=audio (\d+) RTP\/SAVPF.*\r\n/g,
-                      'm=audio $1 RTP/SAVPF 111\r\n');
-    sdp = sdp.replace(/a=rtpmap:(?!111)\d{1,3} (?!VP8|red|ulpfec).*\r\n/g, '');
+                      'm=audio $1 RTP/SAVPF 104\r\n');
+    sdp = sdp.replace('a=fmtp:111 minptime=10', 'a=fmtp:104 minptime=10');
+    sdp = sdp.replace(/a=rtpmap:(?!104)\d{1,3} (?!VP8|red|ulpfec).*\r\n/g, '');
     return sdp;
   });
 }
