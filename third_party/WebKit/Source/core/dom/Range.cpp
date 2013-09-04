92174e687672c16cc3a6f2c7b31e98f814b3c8a0
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-04 04:51:23 +0000

    Make Range::nodeWillBeRemoved() to accept removed node
    This patch changes Range::nodeWillBeRemoved() to accept remove node by replacing ASSERT to if-statement. Range::nodeWillBeRemoved() might be called with removed node, Node::parentNode() == nullptr, when DOMNodeRemovedFromDocument event handler calls removeChild(), directly or indirectly, for node being removed.
    
    This patch is a part of fixing issue 240594 for reproducing the reported situation.
    
    BUG=240594
    TEST=LayoutTests/fast/dom/Range/remove-twice-crash.html
    R=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/23709004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157193 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: eca23dc4a8680f26109068c0b0b0331bb79737c0

--- third_party/WebKit/Source/core/dom/Range.cpp
+++ third_party/WebKit/Source/core/dom/Range.cpp
@@ -1705,7 +1705,11 @@ void Range::nodeWillBeRemoved(Node* node)
     ASSERT(node);
     ASSERT(&node->document() == m_ownerDocument);
     ASSERT(node != m_ownerDocument);
-    ASSERT(node->parentNode());
+
+    // FIXME: Once DOMNodeRemovedFromDocument mutation event removed, we
+    // should change following if-statement to ASSERT(!node->parentNode).
+    if (!node->parentNode())
+        return;
     boundaryNodeWillBeRemoved(m_start, node);
     boundaryNodeWillBeRemoved(m_end, node);
 }
