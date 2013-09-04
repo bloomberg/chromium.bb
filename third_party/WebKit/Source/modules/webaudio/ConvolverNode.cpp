4794678ecf8ff51185288581e5d33f779ffda851
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-04 21:23:14 +0000

    Fix threading races on ConvolverNode::m_reverb in ConvolverNode::latencyFrames()
    According to the crash report (https://cluster-fuzz.appspot.com/testcase?key=6515787040817152),
    ConvolverNode::m_reverb races between ConvolverNode::latencyFrames() and ConvolverNode::setBuffer().
    This CL adds a proper lock for ConvolverNode::m_reverb.
    
    BUG=223962
    No tests because the crash depends on threading races and thus not reproducible.
    
    Review URL: https://chromiumcodereview.appspot.com/23514037
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157245 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 94bb8861ec61b4ebcce8a4489be2cf7e2a055d90

--- third_party/WebKit/Source/modules/webaudio/ConvolverNode.cpp
+++ third_party/WebKit/Source/modules/webaudio/ConvolverNode.cpp
@@ -168,7 +168,12 @@ double ConvolverNode::tailTime() const
 
 double ConvolverNode::latencyTime() const
 {
-    return m_reverb ? m_reverb->latencyFrames() / static_cast<double>(sampleRate()) : 0;
+    MutexTryLocker tryLocker(m_processLock);
+    if (tryLocker.locked())
+        return m_reverb ? m_reverb->latencyFrames() / static_cast<double>(sampleRate()) : 0;
+    // Since we don't want to block the Audio Device thread, we return a large value
+    // instead of trying to acquire the lock.
+    return std::numeric_limits<double>::infinity();
 }
 
 } // namespace WebCore
