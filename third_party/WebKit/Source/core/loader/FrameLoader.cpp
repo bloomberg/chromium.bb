bloomberg/chromium.bb

commit 30715e97c3a1c510a21f59d22926b3c0debbcf00

Move the provisional case in FrameLoader::checkLoadCompleteForThisFrame to receivedMainResourceError
BUG=none

Review URL: https://chromiumcodereview.appspot.com/21261005

git-svn-id: svn://svn.chromium.org/blink/trunk@155359 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 1e967fd004e19b869f7ba7d6ca76614fed1f1f18

third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -159,7 +159,6 @@ FrameLoader::FrameLoader(Frame* frame, FrameLoaderClient* client)
     , m_mixedContentChecker(frame)
     , m_state(FrameStateProvisional)
     , m_loadType(FrameLoadTypeStandard)
-    , m_delegateIsHandlingProvisionalLoadError(false)
     , m_inStopAllLoaders(false)
     , m_didCallImplicitClose(true)
     , m_wasUnloadEventEmitted(false)
@@ -1198,13 +1197,6 @@ void FrameLoader::setState(FrameState newState)
         frameLoadCompleted();
 }
 
-void FrameLoader::clearProvisionalLoad()
-{
-    setProvisionalDocumentLoader(0);
-    m_progressTracker->progressCompleted();
-    setState(FrameStateComplete);
-}
-
 void FrameLoader::commitProvisionalLoad()
 {
     ASSERT(m_client->hasWebView());
@@ -1344,50 +1336,8 @@ void FrameLoader::checkLoadCompleteForThisFrame()
 
     switch (m_state) {
         case FrameStateProvisional: {
-            if (m_delegateIsHandlingProvisionalLoadError)
-                return;
-
-            RefPtr<DocumentLoader> pdl = m_provisionalDocumentLoader;
-            if (!pdl)
-                return;
-
-            // If we've received any errors we may be stuck in the provisional state and actually complete.
-            const ResourceError& error = pdl->mainDocumentError();
-            if (error.isNull())
-                return;
-
-            // Check all children first.
-            RefPtr<HistoryItem> item;
-            if (Page* page = m_frame->page())
-                if (isBackForwardLoadType(loadType()))
-                    // Reset the back forward list to the last committed history item at the top level.
-                    item = page->mainFrame()->loader()->history()->currentItem();
-
-            // Only reset if we aren't already going to a new provisional item.
-            bool shouldReset = !history()->provisionalItem();
-            if (!pdl->isLoadingInAPISense() || pdl->isStopping()) {
-                m_delegateIsHandlingProvisionalLoadError = true;
-                m_client->dispatchDidFailProvisionalLoad(error);
-                m_delegateIsHandlingProvisionalLoadError = false;
-
-                ASSERT(!pdl->isLoading());
-
-                // Finish resetting the load state, but only if another load hasn't been started by the
-                // delegate callback.
-                if (pdl == m_provisionalDocumentLoader)
-                    clearProvisionalLoad();
-                else if (activeDocumentLoader()) {
-                    KURL unreachableURL = activeDocumentLoader()->unreachableURL();
-                    if (!unreachableURL.isEmpty() && unreachableURL == pdl->request().url())
-                        shouldReset = false;
-                }
-            }
-            if (shouldReset && item)
-                if (Page* page = m_frame->page())
-                    page->backForward()->setCurrentItem(item.get());
             return;
         }
-
         case FrameStateCommittedPage: {
             DocumentLoader* dl = m_documentLoader.get();
             if (!dl || (dl->isLoadingInAPISense() && !dl->isStopping()))
@@ -1672,6 +1622,18 @@ void FrameLoader::receivedMainResourceError(const ResourceError& error)
     if (m_state == FrameStateProvisional && m_provisionalDocumentLoader) {
         if (m_submittedFormURL == m_provisionalDocumentLoader->originalRequestCopy().url())
             m_submittedFormURL = KURL();
+
+        m_client->dispatchDidFailProvisionalLoad(error);
+        if (loader != m_provisionalDocumentLoader)
+            return;
+        setProvisionalDocumentLoader(0);
+        m_progressTracker->progressCompleted();
+        setState(FrameStateComplete);
+
+        // Reset the back forward list to the last committed history item at the top level.
+        RefPtr<HistoryItem> item = m_frame->page()->mainFrame()->loader()->history()->currentItem();
+        if (isBackForwardLoadType(loadType()) && !history()->provisionalItem() && item)
+            m_frame->page()->backForward()->setCurrentItem(item.get());
     }
 
     checkCompleted();

third_party/WebKit/LayoutTests/http/tests/loading/bad-scheme-subframe-expected.txt
@@ -2,8 +2,8 @@ main frame - didStartProvisionalLoadForFrame
 main frame - didCommitLoadForFrame
 frame "f1" - didStartProvisionalLoadForFrame
 main frame - didFinishDocumentLoadForFrame
-main frame - didHandleOnloadEventsForFrame
 frame "f1" - didFailProvisionalLoadWithError
+main frame - didHandleOnloadEventsForFrame
 main frame - didFinishLoadForFrame
 This is a test of load callbacks. It is only useful inside the regression test tool.
 

third_party/WebKit/LayoutTests/http/tests/loading/bad-server-subframe-expected.txt
@@ -2,8 +2,8 @@ main frame - didStartProvisionalLoadForFrame
 main frame - didCommitLoadForFrame
 frame "f1" - didStartProvisionalLoadForFrame
 main frame - didFinishDocumentLoadForFrame
-main frame - didHandleOnloadEventsForFrame
 frame "f1" - didFailProvisionalLoadWithError
+main frame - didHandleOnloadEventsForFrame
 main frame - didFinishLoadForFrame
 This is a test of load callbacks. It is only useful inside the regression test tool.
 It also assumes there is no web server running locally on port 54321.

third_party/WebKit/LayoutTests/http/tests/loading/pdf-commit-load-callbacks-expected.txt
@@ -2,7 +2,7 @@ main frame - didStartProvisionalLoadForFrame
 main frame - didCommitLoadForFrame
 frame "<!--framePath //<!--frame0-->-->" - didStartProvisionalLoadForFrame
 main frame - didFinishDocumentLoadForFrame
-main frame - didHandleOnloadEventsForFrame
 frame "<!--framePath //<!--frame0-->-->" - didFailProvisionalLoadWithError
+main frame - didHandleOnloadEventsForFrame
 main frame - didFinishLoadForFrame
 

third_party/WebKit/LayoutTests/http/tests/loading/text-content-type-with-binary-extension-expected.txt
@@ -2,8 +2,8 @@ main frame - didStartProvisionalLoadForFrame
 main frame - didCommitLoadForFrame
 frame "iframe" - didStartProvisionalLoadForFrame
 main frame - didFinishDocumentLoadForFrame
-main frame - didHandleOnloadEventsForFrame
 frame "iframe" - didFailProvisionalLoadWithError
+main frame - didHandleOnloadEventsForFrame
 main frame - didFinishLoadForFrame
 Test for https://bugs.webkit.org/show_bug.cgi?id=17360 REGRESSION: mp4 file downloaded from server is downloaded as html.
 

third_party/WebKit/LayoutTests/platform/linux/http/tests/loading/pdf-commit-load-callbacks-expected.txt
@@ -1,8 +1,0 @@
-main frame - didStartProvisionalLoadForFrame
-main frame - didCommitLoadForFrame
-frame "<!--framePath //<!--frame0-->-->" - didStartProvisionalLoadForFrame
-main frame - didFinishDocumentLoadForFrame
-main frame - didHandleOnloadEventsForFrame
-frame "<!--framePath //<!--frame0-->-->" - didFailProvisionalLoadWithError
-main frame - didFinishLoadForFrame
-

third_party/WebKit/Source/core/loader/FrameLoader.h
@@ -253,7 +253,6 @@ private:
     void setReferrerForFrameRequest(ResourceRequest&, ShouldSendReferrer);
     FrameLoadType determineFrameLoadType(const FrameLoadRequest&);
 
-    void clearProvisionalLoad();
     void frameLoadCompleted();
 
     SubstituteData defaultSubstituteDataForURL(const KURL&);
@@ -324,8 +323,6 @@ private:
     RefPtr<DocumentLoader> m_documentLoader;
     RefPtr<DocumentLoader> m_provisionalDocumentLoader;
     RefPtr<DocumentLoader> m_policyDocumentLoader;
-
-    bool m_delegateIsHandlingProvisionalLoadError;
 
     bool m_inStopAllLoaders;
 

commit 264a33e8d194e626b0091c9fadab0cb4b9355e3e

Fix perf regrssion caused by r155359
Move the logic for ending a failed provisional load back to
FrameLoader::checkLoadCompleteForThisFrame. I believe having it in 
receivedMainResourceError is slower because it allows the failed provisional
load callback to happen before the load event instead of after.

BUG=268777

Review URL: https://chromiumcodereview.appspot.com/23736005

git-svn-id: svn://svn.chromium.org/blink/trunk@157265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 56d394337666de78ea6002dbc903a6432e18f3a8

third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -1026,6 +1026,26 @@ CachePolicy FrameLoader::subresourceCachePolicy() const
 void FrameLoader::checkLoadCompleteForThisFrame()
 {
     ASSERT(m_client->hasWebView());
+
+    if (m_state == FrameStateProvisional && m_provisionalDocumentLoader) {
+        const ResourceError& error = m_provisionalDocumentLoader->mainDocumentError();
+        if (error.isNull())
+            return;
+        RefPtr<DocumentLoader> loader = m_provisionalDocumentLoader;
+        m_client->dispatchDidFailProvisionalLoad(error);
+        if (loader != m_provisionalDocumentLoader)
+            return;
+        m_provisionalDocumentLoader->detachFromFrame();
+        m_provisionalDocumentLoader = 0;
+        m_progressTracker->progressCompleted();
+        m_state = FrameStateComplete;
+
+        // Reset the back forward list to the last committed history item at the top level.
+        RefPtr<HistoryItem> item = m_frame->page()->mainFrame()->loader()->history()->currentItem();
+        if (isBackForwardLoadType(loadType()) && !history()->provisionalItem() && item)
+            m_frame->page()->backForward().setCurrentItem(item.get());
+    }
+
     if (m_state != FrameStateCommittedPage)
         return;
 
@@ -1285,21 +1305,6 @@ void FrameLoader::receivedMainResourceError(const ResourceError& error)
     ResourceError c(ResourceError::cancelledError(KURL()));
     if (error.errorCode() != c.errorCode() || error.domain() != c.domain())
         handleFallbackContent();
-
-    if (m_state == FrameStateProvisional && m_provisionalDocumentLoader) {
-        m_client->dispatchDidFailProvisionalLoad(error);
-        if (loader != m_provisionalDocumentLoader)
-            return;
-        m_provisionalDocumentLoader->detachFromFrame();
-        m_provisionalDocumentLoader = 0;
-        m_progressTracker->progressCompleted();
-        m_state = FrameStateComplete;
-
-        // Reset the back forward list to the last committed history item at the top level.
-        RefPtr<HistoryItem> item = m_frame->page()->mainFrame()->loader()->history()->currentItem();
-        if (isBackForwardLoadType(loadType()) && !history()->provisionalItem() && item)
-            m_frame->page()->backForward().setCurrentItem(item.get());
-    }
 
     checkCompleted();
     if (m_frame->page())

third_party/WebKit/LayoutTests/http/tests/loading/bad-scheme-subframe-expected.txt
@@ -2,8 +2,8 @@ main frame - didStartProvisionalLoadForFrame
 main frame - didCommitLoadForFrame
 frame "f1" - didStartProvisionalLoadForFrame
 main frame - didFinishDocumentLoadForFrame
-frame "f1" - didFailProvisionalLoadWithError
 main frame - didHandleOnloadEventsForFrame
+frame "f1" - didFailProvisionalLoadWithError
 main frame - didFinishLoadForFrame
 This is a test of load callbacks. It is only useful inside the regression test tool.
 

third_party/WebKit/LayoutTests/http/tests/loading/bad-server-subframe-expected.txt
@@ -2,8 +2,8 @@ main frame - didStartProvisionalLoadForFrame
 main frame - didCommitLoadForFrame
 frame "f1" - didStartProvisionalLoadForFrame
 main frame - didFinishDocumentLoadForFrame
-frame "f1" - didFailProvisionalLoadWithError
 main frame - didHandleOnloadEventsForFrame
+frame "f1" - didFailProvisionalLoadWithError
 main frame - didFinishLoadForFrame
 This is a test of load callbacks. It is only useful inside the regression test tool.
 It also assumes there is no web server running locally on port 54321.

third_party/WebKit/LayoutTests/http/tests/loading/pdf-commit-load-callbacks-expected.txt
@@ -2,7 +2,7 @@ main frame - didStartProvisionalLoadForFrame
 main frame - didCommitLoadForFrame
 frame "<!--framePath //<!--frame0-->-->" - didStartProvisionalLoadForFrame
 main frame - didFinishDocumentLoadForFrame
-frame "<!--framePath //<!--frame0-->-->" - didFailProvisionalLoadWithError
 main frame - didHandleOnloadEventsForFrame
+frame "<!--framePath //<!--frame0-->-->" - didFailProvisionalLoadWithError
 main frame - didFinishLoadForFrame
 

third_party/WebKit/LayoutTests/http/tests/loading/text-content-type-with-binary-extension-expected.txt
@@ -2,8 +2,8 @@ main frame - didStartProvisionalLoadForFrame
 main frame - didCommitLoadForFrame
 frame "iframe" - didStartProvisionalLoadForFrame
 main frame - didFinishDocumentLoadForFrame
-frame "iframe" - didFailProvisionalLoadWithError
 main frame - didHandleOnloadEventsForFrame
+frame "iframe" - didFailProvisionalLoadWithError
 main frame - didFinishLoadForFrame
 Test for https://bugs.webkit.org/show_bug.cgi?id=17360 REGRESSION: mp4 file downloaded from server is downloaded as html.
 