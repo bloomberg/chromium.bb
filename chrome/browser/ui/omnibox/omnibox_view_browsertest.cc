cd5b716500a8094878756f967a61d8de8f0066b7
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-05 13:15:31 +0000

    views: Move views specific portions out of omnibox_view_browsertest.cc
    This moves the views parts out of cross-platform omnibox_view_browsertest.cc into a views specific file (c/b/ui/views/omnibox_view_views_browsertest.cc).
    
    BUG=125846
    TEST=interactive_ui_tests --gtest_filter=OmniboxViewViewsTest.SelectAllOnClick
    R=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23494016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221400 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4bae2392754012c68369ccd7407dd00ba7d34cd8

--- chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
+++ chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
@@ -50,10 +50,6 @@
 #include <gtk/gtk.h>
 #endif
 
-#if defined(TOOLKIT_VIEWS)
-#include "chrome/browser/ui/views/frame/browser_view.h"
-#endif
-
 using base::Time;
 using base::TimeDelta;
 
@@ -1256,40 +1252,6 @@ class OmniboxViewTest : public InProcessBrowserTest,
     // Inline autocomplete should still be there.
     EXPECT_EQ(old_text, omnibox_view->GetText());
   }
-
-#if defined(TOOLKIT_VIEWS)
-  const BrowserView* GetBrowserView() const {
-    return BrowserView::GetBrowserViewForBrowser(browser());
-  }
-
-  // Move the mouse to the center of the browser window and left-click.
-  void ClickBrowserWindowCenter() {
-    ASSERT_TRUE(ui_test_utils::SendMouseMoveSync(
-                    GetBrowserView()->GetBoundsInScreen().CenterPoint()));
-    ASSERT_TRUE(ui_test_utils::SendMouseEventsSync(
-                    ui_controls::LEFT, ui_controls::DOWN));
-    ASSERT_TRUE(ui_test_utils::SendMouseEventsSync(
-                    ui_controls::LEFT, ui_controls::UP));
-  }
-
-  // Press and release the mouse in the omnibox at an offset from its origin.
-  // If |release_offset| differs from |press_offset|, the mouse will be moved
-  // between the press and release.
-  void ClickOmnibox(ui_controls::MouseButton button,
-                    const gfx::Vector2d& press_offset,
-                    const gfx::Vector2d& release_offset) {
-    const views::View* omnibox = GetBrowserView()->GetViewByID(VIEW_ID_OMNIBOX);
-    gfx::Point omnibox_origin = omnibox->GetBoundsInScreen().origin();
-    gfx::Point press_point = omnibox_origin + press_offset;
-    ASSERT_TRUE(ui_test_utils::SendMouseMoveSync(press_point));
-    ASSERT_TRUE(ui_test_utils::SendMouseEventsSync(button, ui_controls::DOWN));
-
-    gfx::Point release_point = omnibox_origin + release_offset;
-    if (release_point != press_point)
-      ASSERT_TRUE(ui_test_utils::SendMouseMoveSync(release_point));
-    ASSERT_TRUE(ui_test_utils::SendMouseEventsSync(button, ui_controls::UP));
-  }
-#endif  // defined(TOOLKIT_VIEWS)
 };
 
 // Test if ctrl-* accelerators are workable in omnibox.
@@ -1621,56 +1583,6 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, Paste) {
   EXPECT_EQ(ASCIIToUTF16("ab123cd"), omnibox_view->GetText());
   EXPECT_TRUE(popup_model->IsOpen());
 }
-
-#if defined(TOOLKIT_VIEWS)
-IN_PROC_BROWSER_TEST_F(OmniboxViewTest, SelectAllOnClick) {
-  OmniboxView* omnibox_view = NULL;
-  ASSERT_NO_FATAL_FAILURE(GetOmniboxView(&omnibox_view));
-  omnibox_view->SetUserText(ASCIIToUTF16("http://www.google.com/"));
-  const gfx::Vector2d click(40, 10);
-
-  // Take the focus away from the omnibox.
-  ASSERT_NO_FATAL_FAILURE(ClickBrowserWindowCenter());
-  EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
-  EXPECT_FALSE(omnibox_view->IsSelectAll());
-
-  // Clicking in the omnibox should take focus and select all text.
-  ASSERT_NO_FATAL_FAILURE(ClickOmnibox(ui_controls::LEFT, click, click));
-  EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
-  EXPECT_TRUE(omnibox_view->IsSelectAll());
-
-  // Clicking in another view should clear focus and the selection.
-  ASSERT_NO_FATAL_FAILURE(ClickBrowserWindowCenter());
-  EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
-  EXPECT_FALSE(omnibox_view->IsSelectAll());
-
-  // Clicking in the omnibox again should take focus and select all text again.
-  ASSERT_NO_FATAL_FAILURE(ClickOmnibox(ui_controls::LEFT, click, click));
-  EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
-  EXPECT_TRUE(omnibox_view->IsSelectAll());
-
-  // Clicking another omnibox spot should keep focus but clear the selection.
-  omnibox_view->SelectAll(false);
-  const gfx::Vector2d click_2(click.x() + 10, click.y());
-  ASSERT_NO_FATAL_FAILURE(ClickOmnibox(ui_controls::LEFT, click_2, click_2));
-  EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
-  EXPECT_FALSE(omnibox_view->IsSelectAll());
-
-  // Take the focus away and click in the omnibox again, but drag a bit before
-  // releasing.  We should focus the omnibox but not select all of its text.
-  ASSERT_NO_FATAL_FAILURE(ClickBrowserWindowCenter());
-  const gfx::Vector2d release(click.x() + 10, click.y());
-  ASSERT_NO_FATAL_FAILURE(ClickOmnibox(ui_controls::LEFT, click, release));
-  EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
-  EXPECT_FALSE(omnibox_view->IsSelectAll());
-
-  // Middle-clicking should not be handled by the omnibox.
-  ASSERT_NO_FATAL_FAILURE(ClickBrowserWindowCenter());
-  ASSERT_NO_FATAL_FAILURE(ClickOmnibox(ui_controls::MIDDLE, click, click));
-  EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
-  EXPECT_FALSE(omnibox_view->IsSelectAll());
-}
-#endif  // defined(TOOLKIT_VIEWS)
 
 IN_PROC_BROWSER_TEST_F(OmniboxViewTest, CopyURLToClipboard) {
   // Set permanent text thus making sure that omnibox treats 'google.com'
--- chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
@@ -4,24 +4,76 @@
 
 #include "chrome/browser/ui/views/omnibox/omnibox_view_views.h"
 
-#include "chrome/app/chrome_command_ids.h"
-#include "chrome/browser/command_updater.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/omnibox/location_bar.h"
 #include "chrome/browser/ui/omnibox/omnibox_popup_model.h"
 #include "chrome/browser/ui/view_ids.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_views.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/interactive_test_utils.h"
-#include "chrome/test/base/ui_test_utils.h"
 #include "grit/generated_resources.h"
 #include "ui/base/clipboard/clipboard.h"
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
+#include "ui/base/test/ui_controls.h"
 #include "ui/views/controls/textfield/native_textfield_wrapper.h"
 
-typedef InProcessBrowserTest OmniboxViewViewsTest;
+class OmniboxViewViewsTest : public InProcessBrowserTest {
+ protected:
+  OmniboxViewViewsTest() {}
+
+  static void GetOmniboxViewForBrowser(const Browser* browser,
+                                       OmniboxView** omnibox_view) {
+    BrowserWindow* window = browser->window();
+    ASSERT_TRUE(window);
+    LocationBar* location_bar = window->GetLocationBar();
+    ASSERT_TRUE(location_bar);
+    *omnibox_view = location_bar->GetLocationEntry();
+    ASSERT_TRUE(*omnibox_view);
+  }
+
+  // Move the mouse to the center of the browser window and left-click.
+  void ClickBrowserWindowCenter() {
+    ASSERT_TRUE(ui_test_utils::SendMouseMoveSync(
+        BrowserView::GetBrowserViewForBrowser(
+            browser())->GetBoundsInScreen().CenterPoint()));
+    ASSERT_TRUE(ui_test_utils::SendMouseEventsSync(ui_controls::LEFT,
+                                                   ui_controls::DOWN));
+    ASSERT_TRUE(
+        ui_test_utils::SendMouseEventsSync(ui_controls::LEFT, ui_controls::UP));
+  }
+
+  // Press and release the mouse in the omnibox at an offset from its origin.
+  // If |release_offset| differs from |press_offset|, the mouse will be moved
+  // between the press and release.
+  void ClickOmnibox(ui_controls::MouseButton button,
+                    const gfx::Vector2d& press_offset,
+                    const gfx::Vector2d& release_offset) {
+    const views::View* omnibox = BrowserView::GetBrowserViewForBrowser(
+        browser())->GetViewByID(VIEW_ID_OMNIBOX);
+    gfx::Point omnibox_origin = omnibox->GetBoundsInScreen().origin();
+    gfx::Point press_point = omnibox_origin + press_offset;
+    ASSERT_TRUE(ui_test_utils::SendMouseMoveSync(press_point));
+    ASSERT_TRUE(ui_test_utils::SendMouseEventsSync(button, ui_controls::DOWN));
+
+    gfx::Point release_point = omnibox_origin + release_offset;
+    if (release_point != press_point)
+      ASSERT_TRUE(ui_test_utils::SendMouseMoveSync(release_point));
+    ASSERT_TRUE(ui_test_utils::SendMouseEventsSync(button, ui_controls::UP));
+  }
+
+ private:
+  // InProcessBrowserTest:
+  virtual void SetUpOnMainThread() OVERRIDE {
+    ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
+    chrome::FocusLocationBar(browser());
+    ASSERT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(OmniboxViewViewsTest);
+};
 
 IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, PasteAndGoDoesNotLeavePopupOpen) {
   OmniboxView* view = browser()->window()->GetLocationBar()->GetLocationEntry();
@@ -48,4 +100,52 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, PasteAndGoDoesNotLeavePopupOpen) {
 
   // The popup should not be open.
   EXPECT_FALSE(view->model()->popup_model()->IsOpen());
+}
+
+IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, SelectAllOnClick) {
+  OmniboxView* omnibox_view = NULL;
+  ASSERT_NO_FATAL_FAILURE(GetOmniboxViewForBrowser(browser(), &omnibox_view));
+  omnibox_view->SetUserText(ASCIIToUTF16("http://www.google.com/"));
+  const gfx::Vector2d click(40, 10);
+
+  // Take the focus away from the omnibox.
+  ASSERT_NO_FATAL_FAILURE(ClickBrowserWindowCenter());
+  EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_FALSE(omnibox_view->IsSelectAll());
+
+  // Clicking in the omnibox should take focus and select all text.
+  ASSERT_NO_FATAL_FAILURE(ClickOmnibox(ui_controls::LEFT, click, click));
+  EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_TRUE(omnibox_view->IsSelectAll());
+
+  // Clicking in another view should clear focus and the selection.
+  ASSERT_NO_FATAL_FAILURE(ClickBrowserWindowCenter());
+  EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_FALSE(omnibox_view->IsSelectAll());
+
+  // Clicking in the omnibox again should take focus and select all text again.
+  ASSERT_NO_FATAL_FAILURE(ClickOmnibox(ui_controls::LEFT, click, click));
+  EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_TRUE(omnibox_view->IsSelectAll());
+
+  // Clicking another omnibox spot should keep focus but clear the selection.
+  omnibox_view->SelectAll(false);
+  const gfx::Vector2d click_2(click.x() + 10, click.y());
+  ASSERT_NO_FATAL_FAILURE(ClickOmnibox(ui_controls::LEFT, click_2, click_2));
+  EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_FALSE(omnibox_view->IsSelectAll());
+
+  // Take the focus away and click in the omnibox again, but drag a bit before
+  // releasing.  We should focus the omnibox but not select all of its text.
+  ASSERT_NO_FATAL_FAILURE(ClickBrowserWindowCenter());
+  const gfx::Vector2d release(click.x() + 10, click.y());
+  ASSERT_NO_FATAL_FAILURE(ClickOmnibox(ui_controls::LEFT, click, release));
+  EXPECT_TRUE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_FALSE(omnibox_view->IsSelectAll());
+
+  // Middle-clicking should not be handled by the omnibox.
+  ASSERT_NO_FATAL_FAILURE(ClickBrowserWindowCenter());
+  ASSERT_NO_FATAL_FAILURE(ClickOmnibox(ui_controls::MIDDLE, click, click));
+  EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
+  EXPECT_FALSE(omnibox_view->IsSelectAll());
 }
