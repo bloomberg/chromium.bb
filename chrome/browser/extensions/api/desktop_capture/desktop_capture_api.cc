b585d891daae120a214bf2ae8f1f5348e8d7a616
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-07 06:03:08 +0000

    Revert 221874 "Implement Desktop Media Picker (Mac version) for ..."
    > Implement Desktop Media Picker (Mac version) for the Desktop Capture API
    > 
    > This CL implements DesktopMediaPicker for the Mac used by the Desktop Capture API to present the user with a list of sources (including live thumbnails) to use.
    > 
    > BUG=237907
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23944003
    
    TBR=dcaiafa@chromium.org
    
    Review URL: https://codereview.chromium.org/23493028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@221901 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cc75a2a0d16076c0875d7a35ccd79abd5dab6cbf

--- chrome/browser/extensions/api/desktop_capture/desktop_capture_api.cc
+++ chrome/browser/extensions/api/desktop_capture/desktop_capture_api.cc
@@ -4,7 +4,6 @@
 
 #include "chrome/browser/extensions/api/desktop_capture/desktop_capture_api.h"
 
-#include "base/compiler_specific.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/media/desktop_streams_registry.h"
 #include "chrome/browser/media/media_capture_devices_dispatcher.h"
@@ -88,8 +87,9 @@ bool DesktopCaptureChooseDesktopMediaFunction::RunImpl() {
         screen_capturer.Pass(), window_capturer.Pass());
     picker_ = g_picker_factory->CreatePicker();
   } else {
-    // DesktopMediaPicker is not implented for all platforms yet.
-#if defined(TOOLKIT_VIEWS) || defined(OS_MACOSX)
+    // Currently DesktopMediaPicker is implemented only for platforms that
+    // use Views.
+#if defined(TOOLKIT_VIEWS)
     model.reset(new DesktopMediaPickerModelImpl(
         screen_capturer.Pass(), window_capturer.Pass()));
     picker_ = DesktopMediaPicker::Create();
