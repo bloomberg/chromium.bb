a34638c5d40fe818ab9bae4427bb567808dd471f
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-09 15:07:56 +0000

    Add use counter for insertAdjacentText and insertAdjacentElement
    These two are non standard IE extensions.
    
    BUG=286321
    
    Review URL: https://chromiumcodereview.appspot.com/23638009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157455 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: a706ad3cd488d6827cfcc99cd67cc30625296928

--- third_party/WebKit/Source/core/page/UseCounter.h
+++ third_party/WebKit/Source/core/page/UseCounter.h
@@ -199,6 +199,8 @@ public:
         EventReturnValue, // Legacy IE extension.
         SVGSVGElement,
         SVGAnimateColorElement,
+        InsertAdjacentText,
+        InsertAdjacentElement,
         // Add new features immediately above this line. Don't change assigned
         // numbers of each items, and don't reuse unused slots.
         NumberOfFeatures, // This enum value must be last.
