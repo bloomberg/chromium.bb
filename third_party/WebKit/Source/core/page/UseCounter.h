3d1a73dbb446537e8ec2f5d3ae9e9e41a86d022b
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-09 17:31:57 +0000

    Add use counter for hasAttributes
    hasAttributes was removed from DOM4.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23561008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157461 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 709823c231eb406d6928938c745effb16d6b3b3c

--- third_party/WebKit/Source/core/page/UseCounter.h
+++ third_party/WebKit/Source/core/page/UseCounter.h
@@ -201,6 +201,7 @@ public:
         SVGAnimateColorElement,
         InsertAdjacentText,
         InsertAdjacentElement,
+        HasAttributes, // Removed from DOM4.
         // Add new features immediately above this line. Don't change assigned
         // numbers of each items, and don't reuse unused slots.
         NumberOfFeatures, // This enum value must be last.
