73134278ddb0eafd393a2180a2cf094037139a3b
meacer@chromium.org <meacer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-10 01:59:19 +0000

    Add Pepper flag to UMA plugin records.
    BUG=285440
    
    Review URL: https://chromiumcodereview.appspot.com/23534051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222179 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4e3ca1c297de5183ba0d7a7226a165839df13a76

--- chrome/browser/metrics/metrics_log.cc
+++ chrome/browser/metrics/metrics_log.cc
@@ -228,6 +228,7 @@ void SetPluginInfo(const content::WebPluginInfo& plugin_info,
   plugin->set_name(UTF16ToUTF8(plugin_info.name));
   plugin->set_filename(plugin_info.path.BaseName().AsUTF8Unsafe());
   plugin->set_version(UTF16ToUTF8(plugin_info.version));
+  plugin->set_is_pepper(plugin_info.is_pepper_plugin());
   if (plugin_prefs)
     plugin->set_is_disabled(!plugin_prefs->IsPluginEnabled(plugin_info));
 }
