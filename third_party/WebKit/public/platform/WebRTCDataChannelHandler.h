4c5e78d4a19e9cd0b35c6bca44995b11b3b94ac3
tommyw@chromium.org <tommyw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-10 14:02:13 +0000

    MediaStream API: Making WebRTCDataChannelHandler pure virtual again Chromium has implemented these new functions so I'm cleaning up the API.
    Review URL: https://chromiumcodereview.appspot.com/23600022
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157519 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0800616107de498daa0c64126e90b57d131641cd

--- third_party/WebKit/public/platform/WebRTCDataChannelHandler.h
+++ third_party/WebKit/public/platform/WebRTCDataChannelHandler.h
@@ -43,12 +43,12 @@ public:
     // DEPRECATED
     virtual bool isReliable() { return true; }
 
-    virtual bool ordered() const { return false; }
-    virtual unsigned short maxRetransmitTime() const { return 0; }
-    virtual unsigned short maxRetransmits() const { return 0; }
-    virtual WebString protocol() const { return WebString(); }
-    virtual bool negotiated() const { return true; }
-    virtual unsigned short id() const { return 0; }
+    virtual bool ordered() const = 0;
+    virtual unsigned short maxRetransmitTime() const = 0;
+    virtual unsigned short maxRetransmits() const = 0;
+    virtual WebString protocol() const = 0;
+    virtual bool negotiated() const = 0;
+    virtual unsigned short id() const = 0;
 
     virtual unsigned long bufferedAmount() = 0;
     virtual bool sendStringData(const WebString&) = 0;
