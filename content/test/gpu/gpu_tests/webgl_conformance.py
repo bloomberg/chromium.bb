356b5eee5ec8f9157a443ef2fd270bfa3e5f63d4
bajones@chromium.org <bajones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 17:46:52 +0000

    Added ability for Telemetry tests to specify their own command line args
    This will be critical to allowing the bots to communicate configuration
    details to the tests when using Recipies.
    
    BUG=278398
    R=dtu@chromium.org, kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/23698006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222587 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d64d4c8eae596c5827f022d016a25c34379369f6

--- content/test/gpu/gpu_tests/webgl_conformance.py
+++ content/test/gpu/gpu_tests/webgl_conformance.py
@@ -2,6 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 import json
+import optparse
 import os
 import sys
 
@@ -75,8 +76,17 @@ class WebglConformance(test_module.Test):
   enabled = False
   test = WebglConformanceValidator
 
+  @staticmethod
+  def AddTestCommandLineOptions(parser):
+    group = optparse.OptionGroup(parser, 'WebGL conformance options')
+    group.add_option('--webgl-conformance-version',
+        help='Version of the WebGL conformance tests to run.',
+        default='1.0.1')
+    parser.add_option_group(group)
+
   def CreatePageSet(self, options):
-    tests = self._ParseTests('00_test_list.txt', '1.0.1')
+    tests = self._ParseTests('00_test_list.txt',
+        options.webgl_conformance_version)
 
     page_set_dict = {
       'description': 'Executes WebGL conformance tests',
