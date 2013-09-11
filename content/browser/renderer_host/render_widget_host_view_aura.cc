0b1deaba21118875904e3aa1ac98036f07abed2e
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-11 15:37:42 +0000

    Avoid crash on content_shell.
    GetScreenPositionClient can be NULL mostly on content shell.
    
    BUG=None
    TEST=content_browser_tests
    
    Review URL: https://chromiumcodereview.appspot.com/24083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222567 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 18262f4feb15c98c58a9b26bd57d4d8e8e2f9577

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -2199,18 +2199,18 @@ gfx::Rect RenderWidgetHostViewAura::ConvertRectToScreen(const gfx::Rect& rect) {
   gfx::Point end = gfx::Point(rect.right(), rect.bottom());
 
   aura::RootWindow* root_window = window_->GetRootWindow();
-  if (root_window) {
-    aura::client::ScreenPositionClient* screen_position_client =
-        aura::client::GetScreenPositionClient(root_window);
-    screen_position_client->ConvertPointToScreen(window_, &origin);
-    screen_position_client->ConvertPointToScreen(window_, &end);
-    return gfx::Rect(origin.x(),
-                     origin.y(),
-                     end.x() - origin.x(),
-                     end.y() - origin.y());
-  }
-
-  return rect;
+  if (!root_window)
+    return rect;
+  aura::client::ScreenPositionClient* screen_position_client =
+      aura::client::GetScreenPositionClient(root_window);
+  if (!screen_position_client)
+    return rect;
+  screen_position_client->ConvertPointToScreen(window_, &origin);
+  screen_position_client->ConvertPointToScreen(window_, &end);
+  return gfx::Rect(origin.x(),
+                   origin.y(),
+                   end.x() - origin.x(),
+                   end.y() - origin.y());
 }
 
 gfx::Rect RenderWidgetHostViewAura::ConvertRectFromScreen(
