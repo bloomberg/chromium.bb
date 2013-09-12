5921e7043da6951d0717275a19e237d410be8a8d
Yukawa@chromium.org <Yukawa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 17:40:26 +0000

    Revert r216992 and r221221 because they are no longer necessary
    It turned out that r216992 and r221221 did not use
    views::InputMethod::On[Focus|Blur] in a documented way.
    According to the comment in ui/views/ime/input_method.h,
    views::InputMethod::On[Focus|Blur] are designed to be called
    only when the top-level widget gains or loses keyboard focus.
    (Probably it should be called as "activated"/"deactivated")
    
    To fix this complicated situation, I submitted r222304 as
    yet another fix for  Issue 265337 and Issue 276720.
    With that change, r216992 and r221221 are no longer
    necessary as far as I've tested.
    
    BUG=265337,276720,287620
    TEST=Manually done at Omnibox, Find Bar, Bookmark Bubble, and Edit Bookmark Dialog on Windows 7 Aura and non-Aura build. Also test with --enable-text-services-framework.
    
    Review URL: https://chromiumcodereview.appspot.com/23707029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222805 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5105efdffd09027231ed8fed3dddd2e606905b7e

--- ui/views/controls/combobox/combobox.cc
+++ ui/views/controls/combobox/combobox.cc
@@ -141,7 +141,6 @@ bool Combobox::OnKeyReleased(const ui::KeyEvent& e) {
 }
 
 void Combobox::OnFocus() {
-  GetInputMethod()->OnFocus();
   // Forward the focus to the wrapper.
   if (native_wrapper_) {
     native_wrapper_->SetFocus();
@@ -153,7 +152,6 @@ void Combobox::OnFocus() {
 }
 
 void Combobox::OnBlur() {
-  GetInputMethod()->OnBlur();
   if (selector_)
     selector_->OnViewBlur();
   if (native_wrapper_)
