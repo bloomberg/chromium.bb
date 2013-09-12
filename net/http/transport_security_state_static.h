28c082192225036d5ed39685a5e607e719d87b8e
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 16:22:33 +0000

    net: add (*.)appseccalifornia.org to HSTS preloaded.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222793 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8c1c99c0b059f48c3c3be0e8fed118884cf62b95

--- net/http/transport_security_state_static.h
+++ net/http/transport_security_state_static.h
@@ -862,6 +862,7 @@ static const struct HSTSPreload kPreloadedSTS[] = {
   {12, false, "\006bcrook\003com", true, kNoPins, DOMAIN_NOT_PINNED },
   {17, true, "\004wiki\006python\003org", true, kNoPins, DOMAIN_NOT_PINNED },
   {9, false, "\004lumi\002do", true, kNoPins, DOMAIN_NOT_PINNED },
+  {22, true, "\020appseccalifornia\003org", true, kNoPins, DOMAIN_NOT_PINNED },
 };
 static const size_t kNumPreloadedSTS = ARRAYSIZE_UNSAFE(kPreloadedSTS);
 
