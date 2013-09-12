ad047c900b48de2468282115176c190fde5e8395
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-12 18:04:52 +0000

    Telemetry: Open more than 1 tab in small_profile_creator
    In order to make this type of generated profile useful for a session restore benchmark, open the last 5 links in their own tabs.
    
    Just opening each link in it's own tab would be simpler, but that's not possible yet due to crbug.com/258133 .
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23883006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@222811 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 084b18e4fb71f5e626f266e78b73f5536ce6908e

--- tools/perf/profile_creators/small_profile_creator.py
+++ tools/perf/profile_creators/small_profile_creator.py
@@ -6,6 +6,7 @@ import os
 
 from telemetry.core import util
 from telemetry.page import page_set
+from telemetry.page import page_runner
 from telemetry.page import profile_creator
 
 class SmallProfileCreator(profile_creator.ProfileCreator):
@@ -18,10 +19,31 @@ class SmallProfileCreator(profile_creator.ProfileCreator):
     top_25 = os.path.join(util.GetBaseDir(), 'page_sets', 'top_25.json')
     self._page_set = page_set.PageSet.FromFile(top_25)
 
+    # Open all links in the same tab save for the last N_NUM_TABS links which
+    # are each opened in a new tab.
+    self._NUM_TABS = 5
+
+  def CanRunForPage(self, page):
+    idx = page.page_set.pages.index(page)
+    return idx <= (len(page.page_set.pages) - self._NUM_TABS)
+
+  def DidNavigateToPage(self, page, tab):
+    num_pages_in_pageset = len(page.page_set.pages)
+    last_tab_loaded_automatically = num_pages_in_pageset - self._NUM_TABS
+    page_index = page.page_set.pages.index(page)
+    if (page_index == last_tab_loaded_automatically):
+      for i in xrange(last_tab_loaded_automatically + 1, num_pages_in_pageset):
+        # Load the last _NUM_TABS pages, each in a new tab.
+        t = tab.browser.tabs.New()
+
+        page_state = page_runner.PageState()
+        page_state.PreparePage(page.page_set.pages[i], t)
+        page_state.ImplicitPageNavigation(page.page_set.pages[i], t)
+
+        t.WaitForDocumentReadyStateToBeInteractiveOrBetter()
+
+
   def MeasurePage(self, _, tab, results):
-    # Multiple tabs would help make this faster, but that can't be done until
-    # crbug.com/258113 is fixed.
-    
     # Can't use WaitForDocumentReadyStateToBeComplete() here due to
     # crbug.com/280750 .
     tab.WaitForDocumentReadyStateToBeInteractiveOrBetter()
