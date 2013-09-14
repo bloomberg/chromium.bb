111ea46c76c9afd7b7b79f0e9de319cc37350b3f
sivachandra@chromium.org <sivachandra@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-14 01:02:25 +0000

    Helper script which can source a bash script and dump environment as JSON.
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/23618049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223193 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bbc811f9e1d03af2bbd3efad5fd31d324f702945

---
+++ build/env_dump.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This script can either source a file and dump the enironment changes done by
+# it, or just simply dump the current environment as JSON into a file.
+
+import json
+import optparse
+import os
+import subprocess
+import sys
+
+
+def main():
+  parser = optparse.OptionParser()
+  parser.add_option('-f', '--output-json',
+                    help='File to dump the environment as JSON into.')
+  parser.add_option(
+      '-d', '--dump-mode', action='store_true',
+      help='Dump the environment to the file and exit immediately.')
+
+  options, args = parser.parse_args()
+  if not options.output_json:
+    parser.error('Requires --output-json option.')
+
+  if options.dump_mode:
+    if args:
+      parser.error("Cannot specify args with --dump-mode")
+    with open(options.output_json, 'w') as f:
+      json.dump(dict(os.environ), f)
+  else:
+    envsetup_cmd = ' '.join(args)
+    full_cmd = [
+        'bash', '-c',
+        '. %s; ./%s -d -f %s' % (envsetup_cmd, __file__, options.output_json)]
+    ret = subprocess.call(full_cmd)
+    if ret:
+      sys.exit('Error running %s and dumping env', envsetup_cmd)
+
+
+if __name__ == '__main__':
+  sys.exit(main())
