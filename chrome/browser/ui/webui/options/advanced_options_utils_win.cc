6a06037bbb4e3149f3aa77e75cf2326e275e1f9e
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-16 20:19:49 +0000

    Parent SSL certs window properly in win aura
    Otherwise, parent window can be interacted with which causes
    redraw problems.
    
    R=estade@chromium.org
    BUG=284368
    
    Review URL: https://chromiumcodereview.appspot.com/23889021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223395 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 571daf4c30aa73d7557de5c068d36215582ea888

--- chrome/browser/ui/webui/options/advanced_options_utils_win.cc
+++ chrome/browser/ui/webui/options/advanced_options_utils_win.cc
@@ -16,6 +16,7 @@
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_view.h"
+#include "ui/views/win/hwnd_util.h"
 
 using content::BrowserThread;
 using content::WebContents;
@@ -60,12 +61,8 @@ void AdvancedOptionsUtilities::ShowManageSSLCertificates(
       WebContents* web_contents) {
   CRYPTUI_CERT_MGR_STRUCT cert_mgr = { 0 };
   cert_mgr.dwSize = sizeof(CRYPTUI_CERT_MGR_STRUCT);
-  cert_mgr.hwndParent =
-#if defined(USE_AURA)
-      NULL;
-#else
-      web_contents->GetView()->GetTopLevelNativeWindow();
-#endif
+  cert_mgr.hwndParent = views::HWNDForNativeWindow(
+      web_contents->GetView()->GetTopLevelNativeWindow());
   ::CryptUIDlgCertMgr(&cert_mgr);
 }
 
