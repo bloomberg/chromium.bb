e115691846399753eafb683ffb75f310a38f5067
nasko@chromium.org <nasko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-16 23:03:28 +0000

    Revert 223397 "Enable GPU blacklist in tests."
    > Enable GPU blacklist in tests.
    > 
    > Partial re-land of https://codereview.chromium.org/22198004/) -- Always enable FCM on Windows -- Part 1/3.
    > 
    > On top of fixing some of the issues there; only lands the part that enables the GPU blacklist in tests as the former CL is too hard to land all at once.
    > 
    > Also keeping --skip-gpu-data-loading around for now to be able to deal with failures caused in layout_tests by this on their own later (this needs to land ASAP and layout_tests don't need this ASAP).
    > 
    > Another CL will follow to always enable FCM on non-blacklisted Windows machines.
    > 
    > This part re-enables loading the blacklist in tests (and adds a content_browsertest to make sure that the configuration we expect to be testing is indeed the one we are testing -- this uncertainty is basically the only reason the blacklist was explicitly disabled before).
    > 
    > This CL also cleans up compositor_util.cc which was enforcing the blacklist twice.
    > 
    > The original plan was to do this only for Windows as Mac/Linux was causing trouble, but it turns out to be harder to do it only on Windows; so taking care of http://crbug.com/277242 in this CL too after all... 
    > 
    > BUG=233830, 267038, 190942, 277242
    > TBR=jcivelli, piman 
    > 
    > Originally Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=219132 
    > Then Reverted: https://src.chromium.org/viewvc/chrome?view=rev&revision=219159
    > 
    > Then re-committed (part 1/3): https://src.chromium.org/viewvc/chrome?view=rev&revision=221114
    > Reverted in: https://src.chromium.org/viewvc/chrome?view=rev&revision=221145
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=222105
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23534006
    
    TBR=gab@chromium.org
    
    Review URL: https://codereview.chromium.org/24074005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223456 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fd2726b63cdcbc379c5bdc9378a6b0fa1d291735

--- gpu/config/gpu_control_list.h
+++ gpu/config/gpu_control_list.h
@@ -96,9 +96,6 @@ class GPU_EXPORT GpuControlList {
   // Register whether "all" is recognized as all features.
   void set_supports_feature_type_all(bool supported);
 
-  // Enables logging of control list decisions.
-  void enable_control_list_logging() { control_list_logging_enabled_ = true; }
-
  private:
   friend class GpuControlListEntryTest;
   friend class MachineModelInfoTest;
@@ -294,9 +291,6 @@ class GPU_EXPORT GpuControlList {
         const base::DictionaryValue* value, bool top_level,
         const FeatureMap& feature_map,
         bool supports_feature_type_all);
-
-    // Logs a control list match for this rule.
-    void LogControlListMatch() const;
 
     // Determines if a given os/gc/machine_model/driver is included in the
     // Entry set.
@@ -501,8 +495,6 @@ class GPU_EXPORT GpuControlList {
   // The features a GpuControlList recognizes and handles.
   FeatureMap feature_map_;
   bool supports_feature_type_all_;
-
-  bool control_list_logging_enabled_;
 };
 
 }  // namespace gpu
--- gpu/config/software_rendering_list_json.cc
+++ gpu/config/software_rendering_list_json.cc
@@ -67,7 +67,7 @@
 // "number" is used for all "op" values except "any". "number" and "number2"
 // are in the format of x, x.x, x.x.x, etc.
 // Only "driver_version" supports lexical style if the format is major.minor;
-// in that case, major is still numerical, but minor is lexical.
+// in that case, major is still numerical, but minor is lexical. 
 //
 // STRING includes "op" and "value".  "op" can be any of the following values:
 // "contains", "beginwith", "endwith", "=".  "value" is a string.
@@ -89,7 +89,7 @@ const char kSoftwareRenderingListJson[] = LONG_STRING_CONST(
 {
   "name": "software rendering list",
   // Please update the version number whenever you change this file.
-  "version": "6.10",
+  "version": "6.9",
   "entries": [
     {
       "id": 1,
@@ -1177,21 +1177,6 @@ LONG_STRING_CONST(
       "features": [
         "multisampling"
       ]
-    },
-    {
-      "id": 78,
-      "description": "Disable force compositing mode on all Windows versions prior to Vista.",
-      "cr_bugs": [273920],
-      "os": {
-        "type": "win",
-        "version": {
-          "op": "<",
-          "number": "6.0"
-        }
-      },
-      "features": [
-        "force_compositing_mode"
-      ]
     }
   ]
 }
@@ -1199,3 +1184,4 @@ LONG_STRING_CONST(
 );  // LONG_STRING_CONST macro
 
 }  // namespace gpu
+
