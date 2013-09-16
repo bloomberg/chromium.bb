11f541148b4d885dfab3d9edfe2cf324439c1d68
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-16 23:16:42 +0000

    Remove smoke test that access remote services from PRESUBMIT.py
    It is problematic, as these services can be under load or god knows what could
    happen. In practice we loose a bit of coverage but it is still there with the
    canary master so it is not a big loss, and it will speed up significantly the
    CQ.
    
    R=vadimsh@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/24147003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/swarm_client@223461 0039d316-1c4b-4281-b951-d872f2087c98

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -25,21 +25,20 @@ def CommonChecks(input_api, output_api):
   finally:
     sys.path = sys_path_backup
 
-  integation_tests = []
-  if not input_api.is_committing:
-    # These tests are touching the live infrastructure. It's a pain if your IP
-    # is not whitelisted so only run these on commit.
-    integation_tests = [
-      r'.*isolateserver_smoke_test\.py$',
-      r'.*swarming_smoke_test\.py$',
-    ]
+  # These tests are touching the live infrastructure. It's a pain if your IP
+  # is not whitelisted so do not run them for now. They should use a local fake
+  # web service instead.
+  blacklist = [
+    r'.*isolateserver_smoke_test\.py$',
+    r'.*swarming_smoke_test\.py$',
+  ]
 
   output.extend(
       input_api.canned_checks.RunUnitTestsInDirectory(
           input_api, output_api,
           input_api.os_path.join(input_api.PresubmitLocalPath(), 'tests'),
           whitelist=[r'.+_test\.py$'],
-          blacklist=integation_tests))
+          blacklist=blacklist))
   output.extend(
       input_api.canned_checks.RunUnitTestsInDirectory(
           input_api, output_api,
