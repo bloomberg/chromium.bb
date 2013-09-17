bloomberg/chromium.bb

commit 6d6f6b5e76082a6b3295b67834fa29b5275e5da7

Switch LiveAudio to a source provider solution.
The current LiveAudio has some problems since it creates its own input stream instead of using the stream from gUM.
This patch will make the WebAudio uses the data from gUM through a source provider.


BUG=257963
TEST=content_unittests && http://webrtc.googlecode.com/svn/trunk/samples/js/demos/html/webaudio-and-webrtc.html

Review URL: https://chromiumcodereview.appspot.com/23691038

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223648 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 8f66e2064599b2afd1014d94147fe6895fea9feb

content/renderer/media/webrtc_local_audio_track.cc
@@ -4,12 +4,16 @@
 
 #include "content/renderer/media/webrtc_local_audio_track.h"
 
+#include "content/renderer/media/webaudio_capturer_source.h"
 #include "content/renderer/media/webrtc_audio_capturer.h"
 #include "content/renderer/media/webrtc_audio_capturer_sink_owner.h"
+#include "content/renderer/media/webrtc_local_audio_source_provider.h"
+#include "media/base/audio_fifo.h"
 #include "third_party/libjingle/source/talk/media/base/audiorenderer.h"
 
 namespace content {
 
+static const size_t kMaxNumberOfBuffersInFifo = 2;
 static const char kAudioTrackKind[] = "audio";
 
 namespace {
@@ -47,28 +51,86 @@ bool NeedsAudioProcessing(
 
 }  // namespace.
 
+// This is a temporary audio buffer with parameters used to send data to
+// callbacks.
+class WebRtcLocalAudioTrack::ConfiguredBuffer :
+    public base::RefCounted<WebRtcLocalAudioTrack::ConfiguredBuffer> {
+ public:
+  ConfiguredBuffer() : sink_buffer_size_(0) {}
+
+  void Initialize(const media::AudioParameters& params) {
+    DCHECK(params.IsValid());
+    params_ = params;
+
+    // Use 10ms as the sink buffer size since that is the native packet size
+    // WebRtc is running on.
+    sink_buffer_size_ = params.sample_rate() / 100;
+    audio_wrapper_ =
+        media::AudioBus::Create(params.channels(), sink_buffer_size_);
+    buffer_.reset(new int16[params.frames_per_buffer() * params.channels()]);
+
+    // The size of the FIFO should be at least twice of the source buffer size
+    // or twice of the sink buffer size.
+    int buffer_size = std::max(
+        kMaxNumberOfBuffersInFifo * params.frames_per_buffer(),
+        kMaxNumberOfBuffersInFifo * sink_buffer_size_);
+    fifo_.reset(new media::AudioFifo(params.channels(), buffer_size));
+  }
+
+  void Push(media::AudioBus* audio_source) {
+    DCHECK(fifo_->frames() + audio_source->frames() <= fifo_->max_frames());
+    fifo_->Push(audio_source);
+  }
+
+  bool Consume() {
+    if (fifo_->frames() < audio_wrapper_->frames())
+      return false;
+
+    fifo_->Consume(audio_wrapper_.get(), 0, audio_wrapper_->frames());
+    audio_wrapper_->ToInterleaved(audio_wrapper_->frames(),
+                                  params_.bits_per_sample() / 8,
+                                  buffer());
+    return true;
+  }
+
+  int16* buffer() const { return buffer_.get(); }
+  const media::AudioParameters& params() const { return params_; }
+  int sink_buffer_size() const { return sink_buffer_size_; }
+
+ private:
+  ~ConfiguredBuffer() {}
+  friend class base::RefCounted<WebRtcLocalAudioTrack::ConfiguredBuffer>;
+
+  media::AudioParameters params_;
+  scoped_ptr<media::AudioBus> audio_wrapper_;
+  scoped_ptr<media::AudioFifo> fifo_;
+  scoped_ptr<int16[]> buffer_;
+  int sink_buffer_size_;
+};
+
 scoped_refptr<WebRtcLocalAudioTrack> WebRtcLocalAudioTrack::Create(
     const std::string& id,
     const scoped_refptr<WebRtcAudioCapturer>& capturer,
+    WebAudioCapturerSource* webaudio_source,
     webrtc::AudioSourceInterface* track_source,
     const webrtc::MediaConstraintsInterface* constraints) {
   talk_base::RefCountedObject<WebRtcLocalAudioTrack>* track =
       new talk_base::RefCountedObject<WebRtcLocalAudioTrack>(
-          id, capturer, track_source, constraints);
+          id, capturer, webaudio_source, track_source, constraints);
   return track;
 }
 
 WebRtcLocalAudioTrack::WebRtcLocalAudioTrack(
     const std::string& label,
     const scoped_refptr<WebRtcAudioCapturer>& capturer,
+    WebAudioCapturerSource* webaudio_source,
     webrtc::AudioSourceInterface* track_source,
     const webrtc::MediaConstraintsInterface* constraints)
     : webrtc::MediaStreamTrack<webrtc::AudioTrackInterface>(label),
       capturer_(capturer),
+      webaudio_source_(webaudio_source),
       track_source_(track_source),
       need_audio_processing_(NeedsAudioProcessing(constraints)) {
-  // The capturer with a valid device id is using microphone as source,
-  // and APM (AudioProcessingModule) is turned on only for microphone data.
   DCHECK(capturer.get());
   DVLOG(1) << "WebRtcLocalAudioTrack::WebRtcLocalAudioTrack()";
 }
@@ -80,19 +142,20 @@ WebRtcLocalAudioTrack::~WebRtcLocalAudioTrack() {
   Stop();
 }
 
-void WebRtcLocalAudioTrack::CaptureData(const int16* audio_data,
-                                        int number_of_channels,
-                                        int number_of_frames,
-                                        int audio_delay_milliseconds,
-                                        int volume,
-                                        bool key_pressed) {
+void WebRtcLocalAudioTrack::Capture(media::AudioBus* audio_source,
+                                    int audio_delay_milliseconds,
+                                    int volume,
+                                    bool key_pressed) {
   scoped_refptr<WebRtcAudioCapturer> capturer;
   std::vector<int> voe_channels;
   int sample_rate = 0;
+  int number_of_channels = 0;
+  int number_of_frames = 0;
   SinkList sinks;
+  scoped_refptr<ConfiguredBuffer> current_buffer;
   {
     base::AutoLock auto_lock(lock_);
-    // When the track is diabled, we simply return here.
+    // When the track is disabled, we simply return here.
     // TODO(xians): Figure out if we should feed zero to sinks instead, in
     // order to inject VAD data in such case.
     if (!enabled())
@@ -100,35 +163,62 @@ void WebRtcLocalAudioTrack::CaptureData(const int16* audio_data,
 
     capturer = capturer_;
     voe_channels = voe_channels_;
-    sample_rate = params_.sample_rate(),
+    current_buffer = buffer_;
+    sample_rate = current_buffer->params().sample_rate();
+    number_of_channels = current_buffer->params().channels();
+    number_of_frames = current_buffer->sink_buffer_size();
     sinks = sinks_;
   }
 
-  // Feed the data to the sinks.
-  for (SinkList::const_iterator it = sinks.begin(); it != sinks.end(); ++it) {
-    int new_volume = (*it)->CaptureData(voe_channels,
-                                        audio_data,
-                                        sample_rate,
-                                        number_of_channels,
-                                        number_of_frames,
-                                        audio_delay_milliseconds,
-                                        volume,
-                                        need_audio_processing_,
-                                        key_pressed);
-    if (new_volume != 0 && capturer.get())
-      capturer->SetVolume(new_volume);
+  // Push the data to the fifo.
+  current_buffer->Push(audio_source);
+  // Only turn off the audio processing when the constrain is set to false as
+  // well as there is no correct delay value.
+  bool need_audio_processing = need_audio_processing_ ?
+      need_audio_processing_ : (audio_delay_milliseconds != 0);
+  int current_volume = volume;
+  while (current_buffer->Consume()) {
+    // Feed the data to the sinks.
+    for (SinkList::const_iterator it = sinks.begin(); it != sinks.end(); ++it) {
+      int new_volume = (*it)->CaptureData(voe_channels,
+                                          current_buffer->buffer(),
+                                          sample_rate,
+                                          number_of_channels,
+                                          number_of_frames,
+                                          audio_delay_milliseconds,
+                                          current_volume,
+                                          need_audio_processing,
+                                          key_pressed);
+      if (new_volume != 0 && capturer.get()) {
+        // Feed the new volume to WebRtc while changing the volume on the
+        // browser.
+        capturer->SetVolume(new_volume);
+        current_volume = new_volume;
+      }
+    }
   }
 }
 
 void WebRtcLocalAudioTrack::SetCaptureFormat(
     const media::AudioParameters& params) {
-  base::AutoLock auto_lock(lock_);
-  params_ = params;
+  if (!params.IsValid())
+    return;
+
+  scoped_refptr<ConfiguredBuffer> new_buffer(new ConfiguredBuffer());
+  new_buffer->Initialize(params);
+
+  SinkList sinks;
+  {
+    base::AutoLock auto_lock(lock_);
+    buffer_ = new_buffer;
+    sinks = sinks_;
+  }
 
   // Update all the existing sinks with the new format.
-  for (SinkList::const_iterator it = sinks_.begin();
-       it != sinks_.end(); ++it)
+  for (SinkList::const_iterator it = sinks.begin();
+       it != sinks.end(); ++it) {
     (*it)->SetCaptureFormat(params);
+  }
 }
 
 void WebRtcLocalAudioTrack::AddChannel(int channel_id) {
@@ -172,7 +262,8 @@ void WebRtcLocalAudioTrack::AddSink(WebRtcAudioCapturerSink* sink) {
   DCHECK(thread_checker_.CalledOnValidThread());
   DVLOG(1) << "WebRtcLocalAudioTrack::AddSink()";
   base::AutoLock auto_lock(lock_);
-  sink->SetCaptureFormat(params_);
+  if (buffer_.get())
+    sink->SetCaptureFormat(buffer_->params());
 
   // Verify that |sink| is not already added to the list.
   DCHECK(std::find_if(
@@ -207,8 +298,19 @@ void WebRtcLocalAudioTrack::RemoveSink(
 void WebRtcLocalAudioTrack::Start() {
   DCHECK(thread_checker_.CalledOnValidThread());
   DVLOG(1) << "WebRtcLocalAudioTrack::Start()";
-  if (capturer_.get())
-    capturer_->AddTrack(this);
+  DCHECK(capturer_.get());
+  if (webaudio_source_.get()) {
+    // If the track is hooking up with WebAudio, do NOT add the track to the
+    // capturer as its sink otherwise two streams in different clock will be
+    // pushed through the same track.
+    WebRtcLocalAudioSourceProvider* source_provider =
+        static_cast<WebRtcLocalAudioSourceProvider*>(
+            capturer_->audio_source_provider());
+    webaudio_source_->Start(this, source_provider);
+    return;
+  }
+
+  capturer_->AddTrack(this);
 }
 
 void WebRtcLocalAudioTrack::Stop() {
@@ -217,7 +319,15 @@ void WebRtcLocalAudioTrack::Stop() {
   if (!capturer_.get())
     return;
 
-  capturer_->RemoveTrack(this);
+  if (webaudio_source_.get()) {
+    // Called Stop() on the |webaudio_source_| explicitly so that
+    // |webaudio_source_| won't push more data to the track anymore.
+    // Also note that the track is not registered as a sink to the |capturer_|
+    // in such case and no need to call RemoveTrack().
+    webaudio_source_->Stop();
+  } else {
+    capturer_->RemoveTrack(this);
+  }
 
   // Protect the pointers using the lock when accessing |sinks_| and
   // setting the |capturer_| to NULL.
@@ -225,6 +335,7 @@ void WebRtcLocalAudioTrack::Stop() {
   {
     base::AutoLock auto_lock(lock_);
     sinks = sinks_;
+    webaudio_source_ = NULL;
     capturer_ = NULL;
   }
 

content/content_renderer.gypi
@@ -635,6 +635,8 @@
         'renderer/media/webrtc_identity_service.h',
         'renderer/media/webrtc_local_audio_renderer.cc',
         'renderer/media/webrtc_local_audio_renderer.h',
+        'renderer/media/webrtc_local_audio_source_provider.cc',
+        'renderer/media/webrtc_local_audio_source_provider.h',
         'renderer/media/webrtc_local_audio_track.cc',
         'renderer/media/webrtc_local_audio_track.h',
         'renderer/media/webrtc_logging_initializer.cc',

content/content_tests.gypi
@@ -670,6 +670,7 @@
             'renderer/media/video_source_handler_unittest.cc',
             'renderer/media/webrtc_audio_device_unittest.cc',
             'renderer/media/webrtc_identity_service_unittest.cc',
+            'renderer/media/webrtc_local_audio_source_provider_unittest.cc',
             'renderer/media/webrtc_local_audio_track_unittest.cc',
           ],
           'dependencies': [

content/renderer/media/media_stream_dependency_factory.cc
@@ -381,8 +381,8 @@ void MediaStreamDependencyFactory::CreateNativeLocalMediaStream(
 }
 
 bool MediaStreamDependencyFactory::AddNativeMediaStreamTrack(
-      const WebKit::WebMediaStream& stream,
-      const WebKit::WebMediaStreamTrack& track) {
+    const WebKit::WebMediaStream& stream,
+    const WebKit::WebMediaStreamTrack& track) {
   MediaStreamExtraData* extra_data =
      static_cast<MediaStreamExtraData*>(stream.extraData());
   webrtc::MediaStreamInterface* native_stream = extra_data->stream().get();
@@ -396,12 +396,12 @@ bool MediaStreamDependencyFactory::AddNativeMediaStreamTrack(
   // right now they're on the source, so we fetch them from there.
   RTCMediaConstraints track_constraints(source.constraints());
 
-  scoped_refptr<WebRtcAudioCapturer> capturer;
+  scoped_refptr<WebAudioCapturerSource> webaudio_source;
   if (!source_data) {
     if (source.requiresAudioConsumer()) {
       // We're adding a WebAudio MediaStream.
       // Create a specific capturer for each WebAudio consumer.
-      capturer = CreateWebAudioSource(&source, &track_constraints);
+      webaudio_source = CreateWebAudioSource(&source, &track_constraints);
       source_data =
           static_cast<MediaStreamSourceExtraData*>(source.extraData());
     } else {
@@ -418,15 +418,21 @@ bool MediaStreamDependencyFactory::AddNativeMediaStreamTrack(
 
   std::string track_id = UTF16ToUTF8(track.id());
   if (source.type() == WebKit::WebMediaStreamSource::TypeAudio) {
-    if (!capturer.get() && GetWebRtcAudioDevice())
+    scoped_refptr<WebRtcAudioCapturer> capturer;
+    if (GetWebRtcAudioDevice())
       capturer = GetWebRtcAudioDevice()->GetDefaultCapturer();
 
     scoped_refptr<webrtc::AudioTrackInterface> audio_track(
         CreateLocalAudioTrack(track_id,
                               capturer,
+                              webaudio_source.get(),
                               source_data->local_audio_source(),
                               &track_constraints));
     audio_track->set_enabled(track.isEnabled());
+    if (capturer.get()) {
+      WebKit::WebMediaStreamTrack writable_track = track;
+      writable_track.setSourceProvider(capturer->audio_source_provider());
+    }
     return native_stream->AddTrack(audio_track.get());
   } else {
     DCHECK(source.type() == WebKit::WebMediaStreamSource::TypeVideo);
@@ -484,9 +490,15 @@ bool MediaStreamDependencyFactory::RemoveNativeMediaStreamTrack(
          type == WebKit::WebMediaStreamSource::TypeVideo);
 
   std::string track_id = UTF16ToUTF8(track.id());
-  return type == WebKit::WebMediaStreamSource::TypeAudio ?
-      native_stream->RemoveTrack(native_stream->FindAudioTrack(track_id)) :
-      native_stream->RemoveTrack(native_stream->FindVideoTrack(track_id));
+  if (type == WebKit::WebMediaStreamSource::TypeAudio) {
+    // Remove the source provider as the track is going away.
+    WebKit::WebMediaStreamTrack writable_track = track;
+    writable_track.setSourceProvider(NULL);
+    return native_stream->RemoveTrack(native_stream->FindAudioTrack(track_id));
+  }
+
+  CHECK_EQ(type, WebKit::WebMediaStreamSource::TypeVideo);
+  return native_stream->RemoveTrack(native_stream->FindVideoTrack(track_id));
 }
 
 bool MediaStreamDependencyFactory::CreatePeerConnectionFactory() {
@@ -605,26 +617,17 @@ MediaStreamDependencyFactory::CreateLocalVideoSource(
   return source;
 }
 
-scoped_refptr<WebRtcAudioCapturer>
+scoped_refptr<WebAudioCapturerSource>
 MediaStreamDependencyFactory::CreateWebAudioSource(
     WebKit::WebMediaStreamSource* source,
     RTCMediaConstraints* constraints) {
   DVLOG(1) << "MediaStreamDependencyFactory::CreateWebAudioSource()";
   DCHECK(GetWebRtcAudioDevice());
 
-  // Set up the source and ensure that WebAudio is driving things instead of
-  // a microphone. For WebAudio, we always create a new capturer without
-  // calling initialize(), WebAudio will re-configure the capturer later on.
-  // Pass -1 as the |render_view_id| and an empty device struct to tell the
-  // capturer not to start the default source.
-  scoped_refptr<WebRtcAudioCapturer> capturer(
-      MaybeCreateAudioCapturer(-1, StreamDeviceInfo()));
-  DCHECK(capturer.get());
-
   scoped_refptr<WebAudioCapturerSource>
-      webaudio_capturer_source(new WebAudioCapturerSource(capturer.get()));
+      webaudio_capturer_source(new WebAudioCapturerSource());
   MediaStreamSourceExtraData* source_data =
-      new content::MediaStreamSourceExtraData(webaudio_capturer_source.get());
+      new content::MediaStreamSourceExtraData();
 
   // Create a LocalAudioSource object which holds audio options.
   // Use audio constraints where all values are true, i.e., enable
@@ -638,7 +641,7 @@ MediaStreamDependencyFactory::CreateWebAudioSource(
   // Replace the default source with WebAudio as source instead.
   source->addAudioConsumer(webaudio_capturer_source.get());
 
-  return capturer;
+  return webaudio_capturer_source;
 }
 
 scoped_refptr<webrtc::VideoTrackInterface>
@@ -668,13 +671,16 @@ scoped_refptr<webrtc::AudioTrackInterface>
 MediaStreamDependencyFactory::CreateLocalAudioTrack(
     const std::string& id,
     const scoped_refptr<WebRtcAudioCapturer>& capturer,
+    WebAudioCapturerSource* webaudio_source,
     webrtc::AudioSourceInterface* source,
     const webrtc::MediaConstraintsInterface* constraints) {
   // TODO(xians): Merge |source| to the capturer(). We can't do this today
   // because only one capturer() is supported while one |source| is created
   // for each audio track.
   scoped_refptr<WebRtcLocalAudioTrack> audio_track(
-      WebRtcLocalAudioTrack::Create(id, capturer, source, constraints));
+      WebRtcLocalAudioTrack::Create(id, capturer, webaudio_source,
+                                    source, constraints));
+
   // Add the WebRtcAudioDevice as the sink to the local audio track.
   audio_track->AddSink(GetWebRtcAudioDevice());
   // Start the audio track. This will hook the |audio_track| to the capturer
@@ -811,11 +817,13 @@ scoped_refptr<WebRtcAudioCapturer>
 MediaStreamDependencyFactory::MaybeCreateAudioCapturer(
     int render_view_id,
     const StreamDeviceInfo& device_info) {
-  scoped_refptr<WebRtcAudioCapturer> capturer;
-  if (render_view_id != -1) {
-    // From a normal getUserMedia, re-use the existing default capturer.
-    capturer = GetWebRtcAudioDevice()->GetDefaultCapturer();
-  }
+  // TODO(xians): Handle the cases when gUM is called without a proper render
+  // view, for example, by an extension.
+  DCHECK_GE(render_view_id, 0);
+
+  scoped_refptr<WebRtcAudioCapturer> capturer =
+      GetWebRtcAudioDevice()->GetDefaultCapturer();
+
   // If the default capturer does not exist or |render_view_id| == -1, create
   // a new capturer.
   bool is_new_capturer = false;
@@ -829,6 +837,7 @@ MediaStreamDependencyFactory::MaybeCreateAudioCapturer(
           static_cast<media::ChannelLayout>(
               device_info.device.input.channel_layout),
           device_info.device.input.sample_rate,
+          device_info.device.input.frames_per_buffer,
           device_info.session_id,
           device_info.device.id)) {
     return NULL;

content/renderer/media/media_stream_dependency_factory.h
@@ -44,6 +44,7 @@ class IpcNetworkManager;
 class IpcPacketSocketFactory;
 class RTCMediaConstraints;
 class VideoCaptureImplManager;
+class WebAudioCapturerSource;
 class WebRtcAudioCapturer;
 class WebRtcAudioDeviceImpl;
 class WebRtcLoggingHandlerImpl;
@@ -164,15 +165,15 @@ class CONTENT_EXPORT MediaStreamDependencyFactory
   // WebRtcAudioCapturer.
   // The |constraints| will be modified to include the default, mandatory
   // WebAudio constraints.
-  virtual scoped_refptr<WebRtcAudioCapturer> CreateWebAudioSource(
-      WebKit::WebMediaStreamSource* source,
-      RTCMediaConstraints* constraints);
+  virtual scoped_refptr<WebAudioCapturerSource> CreateWebAudioSource(
+      WebKit::WebMediaStreamSource* source, RTCMediaConstraints* constraints);
 
   // Asks the PeerConnection factory to create a Local AudioTrack object.
   virtual scoped_refptr<webrtc::AudioTrackInterface>
       CreateLocalAudioTrack(
           const std::string& id,
           const scoped_refptr<WebRtcAudioCapturer>& capturer,
+          WebAudioCapturerSource* webaudio_source,
           webrtc::AudioSourceInterface* source,
           const webrtc::MediaConstraintsInterface* constraints);
 

content/renderer/media/media_stream_impl.cc
@@ -727,9 +727,7 @@ MediaStreamSourceExtraData::MediaStreamSourceExtraData(
       webkit_source_(webkit_source) {
 }
 
-MediaStreamSourceExtraData::MediaStreamSourceExtraData(
-    media::AudioCapturerSource* source)
-    : audio_source_(source)  {
+MediaStreamSourceExtraData::MediaStreamSourceExtraData() {
 }
 
 MediaStreamSourceExtraData::~MediaStreamSourceExtraData() {}

content/renderer/media/media_stream_source_extra_data.h
@@ -9,7 +9,6 @@
 #include "content/common/content_export.h"
 #include "content/common/media/media_stream_options.h"
 #include "content/renderer/media/media_stream_source_observer.h"
-#include "media/base/audio_capturer_source.h"
 #include "third_party/libjingle/source/talk/app/webrtc/videosourceinterface.h"
 #include "third_party/WebKit/public/platform/WebMediaStreamSource.h"
 
@@ -21,8 +20,7 @@ class CONTENT_EXPORT MediaStreamSourceExtraData
   MediaStreamSourceExtraData(
       const StreamDeviceInfo& device_info,
       const WebKit::WebMediaStreamSource& webkit_source);
-  explicit MediaStreamSourceExtraData(
-      media::AudioCapturerSource* source);
+  MediaStreamSourceExtraData();
   virtual ~MediaStreamSourceExtraData();
 
   // Returns the WebMediaStreamSource object that owns this object.
@@ -47,7 +45,6 @@ class CONTENT_EXPORT MediaStreamSourceExtraData
   }
 
   webrtc::VideoSourceInterface* video_source() { return video_source_.get(); }
-  media::AudioCapturerSource* audio_source() { return audio_source_.get(); }
   webrtc::AudioSourceInterface* local_audio_source() {
     return local_audio_source_.get();
   }
@@ -65,7 +62,6 @@ class CONTENT_EXPORT MediaStreamSourceExtraData
   // MediaStreamImpl::~UserMediaRequestInfo() does.
   WebKit::WebMediaStreamSource webkit_source_;
   scoped_refptr<webrtc::VideoSourceInterface> video_source_;
-  scoped_refptr<media::AudioCapturerSource> audio_source_;
 
   // This member holds an instance of webrtc::LocalAudioSource. This is used
   // as a container for audio options.

content/renderer/media/mock_media_stream_dependency_factory.cc
@@ -7,6 +7,7 @@
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/renderer/media/mock_peer_connection_impl.h"
+#include "content/renderer/media/webaudio_capturer_source.h"
 #include "content/renderer/media/webrtc_audio_capturer.h"
 #include "content/renderer/media/webrtc_local_audio_track.h"
 #include "third_party/libjingle/source/talk/app/webrtc/mediastreaminterface.h"
@@ -406,7 +407,7 @@ MockMediaStreamDependencyFactory::CreateLocalVideoSource(
   return last_video_source_;
 }
 
-scoped_refptr<WebRtcAudioCapturer>
+scoped_refptr<WebAudioCapturerSource>
 MockMediaStreamDependencyFactory::CreateWebAudioSource(
     WebKit::WebMediaStreamSource* source,
     RTCMediaConstraints* constraints) {
@@ -448,12 +449,14 @@ scoped_refptr<webrtc::AudioTrackInterface>
 MockMediaStreamDependencyFactory::CreateLocalAudioTrack(
     const std::string& id,
     const scoped_refptr<WebRtcAudioCapturer>& capturer,
+    WebAudioCapturerSource* webaudio_source,
     webrtc::AudioSourceInterface* source,
     const webrtc::MediaConstraintsInterface* constraints) {
   DCHECK(mock_pc_factory_created_);
   DCHECK(!capturer.get());
   return WebRtcLocalAudioTrack::Create(
-      id, WebRtcAudioCapturer::CreateCapturer(), source, constraints);
+      id, WebRtcAudioCapturer::CreateCapturer(), webaudio_source,
+      source, constraints);
 }
 
 SessionDescriptionInterface*

commit 0a8b5d155bfa43e91fd2cc948d407acd9d90178f

Fixed the crash introduced by r223648.
The buffer size used by the buffer_ should be the same as the sink buffer size.

TBR=tommi@chromium.org

Review URL: https://chromiumcodereview.appspot.com/23724041

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223733 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 81a419fa09a4694ce7ff72461455fe21041e9906

content/renderer/media/webrtc_local_audio_track.cc
@@ -67,7 +67,7 @@ class WebRtcLocalAudioTrack::ConfiguredBuffer :
     sink_buffer_size_ = params.sample_rate() / 100;
     audio_wrapper_ =
         media::AudioBus::Create(params.channels(), sink_buffer_size_);
-    buffer_.reset(new int16[params.frames_per_buffer() * params.channels()]);
+    buffer_.reset(new int16[sink_buffer_size_ * params.channels()]);
 
     // The size of the FIFO should be at least twice of the source buffer size
     // or twice of the sink buffer size.