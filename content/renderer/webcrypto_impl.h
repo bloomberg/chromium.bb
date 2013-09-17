225fc93590a5b5e7b11b33b204217f8d8f0461de
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-17 14:29:42 +0000

    Revert 223506 "WebCrypto: Implement importKey() and sign() for H..."
    Believe it caused a compile failure on Linux. See:
    http://build.chromium.org/p/chromium.chrome/builders/Google%20Chrome%20Linux%20x64
    
    > WebCrypto: Implement importKey() and sign() for HMAC in NSS
    > 
    > BUG=245025
    > R=eroman@chromium.org,ellyjones@chromium.org
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23569007
    
    TBR=bryaneyler@google.com
    
    Review URL: https://codereview.chromium.org/24205003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223612 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2c84baa742b7610eff54d26c83eaf45e724ecdbd

--- content/renderer/webcrypto_impl.h
+++ content/renderer/webcrypto_impl.h
@@ -8,7 +8,6 @@
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
-#include "base/memory/scoped_ptr.h"
 #include "content/common/content_export.h"
 #include "third_party/WebKit/public/platform/WebCrypto.h"
 
@@ -24,43 +23,14 @@ class CONTENT_EXPORT WebCryptoImpl
       const unsigned char* data,
       unsigned data_size,
       WebKit::WebCryptoResult result);
-  virtual void importKey(
-      WebKit::WebCryptoKeyFormat format,
-      const unsigned char* key_data,
-      unsigned key_data_size,
-      const WebKit::WebCryptoAlgorithm& algorithm,
-      bool extractable,
-      WebKit::WebCryptoKeyUsageMask usage_mask,
-      WebKit::WebCryptoResult result);
-  virtual void sign(
-      const WebKit::WebCryptoAlgorithm& algorithm,
-      const WebKit::WebCryptoKey& key,
-      const unsigned char* data,
-      unsigned data_size,
-      WebKit::WebCryptoResult result);
 
  protected:
   FRIEND_TEST_ALL_PREFIXES(WebCryptoImplTest, DigestSampleSets);
-  FRIEND_TEST_ALL_PREFIXES(WebCryptoImplTest, HMACSampleSets);
 
   void Init();
 
   bool DigestInternal(
       const WebKit::WebCryptoAlgorithm& algorithm,
-      const unsigned char* data,
-      unsigned data_size,
-      WebKit::WebArrayBuffer* buffer);
-  bool ImportKeyInternal(
-      WebKit::WebCryptoKeyFormat format,
-      const unsigned char* key_data,
-      unsigned key_data_size,
-      const WebKit::WebCryptoAlgorithm& algorithm,
-      WebKit::WebCryptoKeyUsageMask usage_mask,
-      scoped_ptr<WebKit::WebCryptoKeyHandle>* handle,
-      WebKit::WebCryptoKeyType* type);
-  bool SignInternal(
-      const WebKit::WebCryptoAlgorithm& algorithm,
-      const WebKit::WebCryptoKey& key,
       const unsigned char* data,
       unsigned data_size,
       WebKit::WebArrayBuffer* buffer);
