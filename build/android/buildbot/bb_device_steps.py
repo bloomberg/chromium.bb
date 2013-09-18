e70c79b236cfd595218e437096a532100fa34603
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-18 19:24:43 +0000

    Sigh. Lower-case the "k" in ChromiumWebkit for the layout tests upload for Android.
    One more change after r223770. Hopefully this one's the ticket.
    
    (Build fix).
    
    TBR=ojan@chromium.org, ilevy@chromium.org
    BUG=293928
    
    Review URL: https://codereview.chromium.org/23452044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@223912 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 627cf5b1b9cbdfca1dabe14f43d12effeaf2c7e4

--- build/android/buildbot/bb_device_steps.py
+++ build/android/buildbot/bb_device_steps.py
@@ -228,7 +228,7 @@ def RunWebkitLayoutTests(options):
         '--target', options.target,
         '--builder-name', options.build_properties.get('buildername', ''),
         '--build-number', str(options.build_properties.get('buildnumber', '')),
-        '--master-name', 'ChromiumWebKit', # TODO: Get this from the cfg.
+        '--master-name', 'ChromiumWebkit', # TODO: Get this from the cfg.
         '--build-name', options.build_properties.get('buildername', ''),
         '--platform=android']
 
