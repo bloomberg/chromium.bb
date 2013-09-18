b025cf906effa128ac049626b9e480f4ff38bdd3
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-18 00:50:50 +0000

    Move margin handling and layoutBlockChild to RenderBlockFlow
    Cribbed from webkit.org/b/121049
    
    Continuing the work to move Block Flow layout out of RenderBlock
    (the root for all kinds of block layout) and into RenderBlockFlow.
    This change moves margin collapsing, which only applies to block
    flow layout. I also moved MarginInfo's definintion from being
    in the header to RenderBlockFlow.cpp, as it's only used by
    internal functions.
    
    All functions that return MarginValues or MarginInfo were moved
    as part of this patch, and layoutBlockChild, which handles
    margin collapse. Margin code was also tightened where possible
    to look for RenderBlockFlow instead of RenderBlock.
    
    This patch also created a RenderBlockFlowRareData internal class
    that currently only holds Margin data. The goal is to move all
    rare data from RenderBlock to RenderBlockFlow (or force them
    into a hash), but during this transition both classes will
    have rare data structures.
    
    Review URL: https://chromiumcodereview.appspot.com/24079007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157928 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6b1091d2581624bcea72348bc647e3897894a00d

--- third_party/WebKit/Source/core/rendering/RenderBlockFlow.h
+++ third_party/WebKit/Source/core/rendering/RenderBlockFlow.h
@@ -40,6 +40,8 @@
 
 namespace WebCore {
 
+class MarginInfo;
+
 class RenderBlockFlow : public RenderBlock {
 public:
     explicit RenderBlockFlow(ContainerNode*);
@@ -62,7 +64,125 @@ private:
     void layoutBlockChildren(bool relayoutChildren, LayoutUnit& maxFloatLogicalBottom, SubtreeLayoutScope&);
     void layoutInlineChildren(bool relayoutChildren, LayoutUnit& repaintLogicalTop, LayoutUnit& repaintLogicalBottom);
 
+    void layoutBlockChild(RenderBox* child, MarginInfo&, LayoutUnit& previousFloatLogicalBottom, LayoutUnit& maxFloatLogicalBottom);
+    void adjustPositionedBlock(RenderBox* child, const MarginInfo&);
+    void adjustFloatingBlock(const MarginInfo&);
+
     void clearFloats();
+
+public:
+    class MarginValues {
+    public:
+        MarginValues(LayoutUnit beforePos, LayoutUnit beforeNeg, LayoutUnit afterPos, LayoutUnit afterNeg)
+            : m_positiveMarginBefore(beforePos)
+            , m_negativeMarginBefore(beforeNeg)
+            , m_positiveMarginAfter(afterPos)
+            , m_negativeMarginAfter(afterNeg)
+        { }
+
+        LayoutUnit positiveMarginBefore() const { return m_positiveMarginBefore; }
+        LayoutUnit negativeMarginBefore() const { return m_negativeMarginBefore; }
+        LayoutUnit positiveMarginAfter() const { return m_positiveMarginAfter; }
+        LayoutUnit negativeMarginAfter() const { return m_negativeMarginAfter; }
+
+        void setPositiveMarginBefore(LayoutUnit pos) { m_positiveMarginBefore = pos; }
+        void setNegativeMarginBefore(LayoutUnit neg) { m_negativeMarginBefore = neg; }
+        void setPositiveMarginAfter(LayoutUnit pos) { m_positiveMarginAfter = pos; }
+        void setNegativeMarginAfter(LayoutUnit neg) { m_negativeMarginAfter = neg; }
+
+    private:
+        LayoutUnit m_positiveMarginBefore;
+        LayoutUnit m_negativeMarginBefore;
+        LayoutUnit m_positiveMarginAfter;
+        LayoutUnit m_negativeMarginAfter;
+    };
+    MarginValues marginValuesForChild(RenderBox* child) const;
+
+    // Allocated only when some of these fields have non-default values
+    struct RenderBlockFlowRareData {
+        WTF_MAKE_NONCOPYABLE(RenderBlockFlowRareData); WTF_MAKE_FAST_ALLOCATED;
+    public:
+        RenderBlockFlowRareData(const RenderBlockFlow* block)
+            : m_margins(positiveMarginBeforeDefault(block), negativeMarginBeforeDefault(block), positiveMarginAfterDefault(block), negativeMarginAfterDefault(block))
+            , m_discardMarginBefore(false)
+            , m_discardMarginAfter(false)
+        {
+        }
+
+        static LayoutUnit positiveMarginBeforeDefault(const RenderBlockFlow* block)
+        {
+            return std::max<LayoutUnit>(block->marginBefore(), 0);
+        }
+        static LayoutUnit negativeMarginBeforeDefault(const RenderBlockFlow* block)
+        {
+            return std::max<LayoutUnit>(-block->marginBefore(), 0);
+        }
+        static LayoutUnit positiveMarginAfterDefault(const RenderBlockFlow* block)
+        {
+            return std::max<LayoutUnit>(block->marginAfter(), 0);
+        }
+        static LayoutUnit negativeMarginAfterDefault(const RenderBlockFlow* block)
+        {
+            return std::max<LayoutUnit>(-block->marginAfter(), 0);
+        }
+
+        MarginValues m_margins;
+        bool m_discardMarginBefore : 1;
+        bool m_discardMarginAfter : 1;
+    };
+
+protected:
+    LayoutUnit maxPositiveMarginBefore() const { return m_rareData ? m_rareData->m_margins.positiveMarginBefore() : RenderBlockFlowRareData::positiveMarginBeforeDefault(this); }
+    LayoutUnit maxNegativeMarginBefore() const { return m_rareData ? m_rareData->m_margins.negativeMarginBefore() : RenderBlockFlowRareData::negativeMarginBeforeDefault(this); }
+    LayoutUnit maxPositiveMarginAfter() const { return m_rareData ? m_rareData->m_margins.positiveMarginAfter() : RenderBlockFlowRareData::positiveMarginAfterDefault(this); }
+    LayoutUnit maxNegativeMarginAfter() const { return m_rareData ? m_rareData->m_margins.negativeMarginAfter() : RenderBlockFlowRareData::negativeMarginAfterDefault(this); }
+
+    void setMaxMarginBeforeValues(LayoutUnit pos, LayoutUnit neg);
+    void setMaxMarginAfterValues(LayoutUnit pos, LayoutUnit neg);
+
+    void setMustDiscardMarginBefore(bool = true);
+    void setMustDiscardMarginAfter(bool = true);
+
+    bool mustDiscardMarginBefore() const;
+    bool mustDiscardMarginAfter() const;
+
+    bool mustDiscardMarginBeforeForChild(const RenderBox*) const;
+    bool mustDiscardMarginAfterForChild(const RenderBox*) const;
+
+    bool mustSeparateMarginBeforeForChild(const RenderBox*) const;
+    bool mustSeparateMarginAfterForChild(const RenderBox*) const;
+
+    void initMaxMarginValues()
+    {
+        if (m_rareData) {
+            m_rareData->m_margins = MarginValues(RenderBlockFlowRareData::positiveMarginBeforeDefault(this) , RenderBlockFlowRareData::negativeMarginBeforeDefault(this),
+                RenderBlockFlowRareData::positiveMarginAfterDefault(this), RenderBlockFlowRareData::negativeMarginAfterDefault(this));
+
+            m_rareData->m_discardMarginBefore = false;
+            m_rareData->m_discardMarginAfter = false;
+        }
+    }
+
+private:
+    virtual LayoutUnit collapsedMarginBefore() const OVERRIDE FINAL { return maxPositiveMarginBefore() - maxNegativeMarginBefore(); }
+    virtual LayoutUnit collapsedMarginAfter() const OVERRIDE FINAL { return maxPositiveMarginAfter() - maxNegativeMarginAfter(); }
+
+    LayoutUnit collapseMargins(RenderBox* child, MarginInfo&);
+    LayoutUnit clearFloatsIfNeeded(RenderBox* child, MarginInfo&, LayoutUnit oldTopPosMargin, LayoutUnit oldTopNegMargin, LayoutUnit yPos);
+    LayoutUnit estimateLogicalTopPosition(RenderBox* child, const MarginInfo&, LayoutUnit& estimateWithoutPagination);
+    void marginBeforeEstimateForChild(RenderBox*, LayoutUnit&, LayoutUnit&, bool&) const;
+    void handleAfterSideOfBlock(LayoutUnit top, LayoutUnit bottom, MarginInfo&);
+    void setCollapsedBottomMargin(const MarginInfo&);
+
+    LayoutUnit applyBeforeBreak(RenderBox* child, LayoutUnit logicalOffset); // If the child has a before break, then return a new yPos that shifts to the top of the next page/column.
+    LayoutUnit applyAfterBreak(RenderBox* child, LayoutUnit logicalOffset, MarginInfo&); // If the child has an after break, then return a new offset that shifts to the top of the next page/column.
+
+    LayoutUnit adjustBlockChildForPagination(LayoutUnit logicalTopAfterClear, LayoutUnit estimateWithoutPagination, RenderBox* child, bool atBeforeSideOfBlock);
+
+protected:
+    OwnPtr<RenderBlockFlowRareData> m_rareData;
+
+    friend class MarginInfo;
 };
 
 inline RenderBlockFlow& toRenderBlockFlow(RenderObject& object)
