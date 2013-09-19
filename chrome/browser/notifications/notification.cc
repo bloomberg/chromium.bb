40974c9b6d09de2062047b17149bfb1ff64c1066
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 18:18:37 +0000

    Allow chrome.notifications to have icons on Linux.
    Due to a typo, balloons don't actually contain the image content they need
    when created via chrome.notifications.
    
    BUG=294436
    
    Review URL: https://chromiumcodereview.appspot.com/23796013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224169 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 58e0adb3eb29f4a7371ae95e9e50d6a85cc5ad81

--- chrome/browser/notifications/notification.cc
+++ chrome/browser/notifications/notification.cc
@@ -81,14 +81,15 @@ Notification::Notification(
       is_html_(false),
       replace_id_(replace_id),
       delegate_(delegate) {
+  // It's important to leave |icon_url_| empty with rich notifications enabled,
+  // to prevent "Downloading" the data url and overwriting the existing |icon|.
   if (!message_center::IsRichNotificationEnabled()) {
     // "Upconvert" the string parameters to a data: URL.  Some balloon views
     // require content URL to render anything, so this serves as a backup.
-    GURL icon_url;
     if (!icon.IsEmpty())
       icon_url_ = GURL(webui::GetBitmapDataUrl(*icon.ToSkBitmap()));
     content_url_ = GURL(
-        DesktopNotificationService::CreateDataUrl(icon_url, title, body, dir));
+        DesktopNotificationService::CreateDataUrl(icon_url_, title, body, dir));
   }
 }
 
