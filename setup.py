201d1ecdbd93be5aa53d0576dba0389fd15ec92f
sbaig1@bloomberg.net <sbaig1@bloomberg.net@78cadc50-ecff-11dd-a971-7dbc132099af>
2013-09-19 18:55:25 +0000

    remove ez_setup import from setup.py
    During the review for commit r1713 it was determined that ez_setup.py
    was not going to be part of the commit.  While ez_setup.py was removed
    as requested, the import and usage of ez_setup in setup.py was not
    removed appropriately.  This change removes the import and usage so that
    installing gyp via setuptools works as expected.
    
    R=mark@chromium.org, scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/24282003
    
    Patch from Eric N. Vander Weele <ericvw@gmail.com>.
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1736 78cadc50-ecff-11dd-a971-7dbc132099af

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from ez_setup import use_setuptools
-use_setuptools()
 from setuptools import setup
 
 setup(
