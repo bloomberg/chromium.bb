e6cde5343511d0269b0645efc847c172d0ba7620
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 02:18:14 +0000

    Fix a static initializer regression.
    BUG=94925
    TBR=gcasto
    
    Review URL: https://codereview.chromium.org/23891006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224020 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fc78ef6ba17e2384ae5a7647794ee727d9a7d203

--- components/autofill/core/common/password_form.cc
+++ components/autofill/core/common/password_form.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <iostream>
+#include <ostream>
 
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
