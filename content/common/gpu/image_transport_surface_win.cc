bd6e9ad4a8ecd10d0c12f544b62f28f089baee3c
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-19 23:35:46 +0000

    Don't send a 0 Vsync interval to compositor.
    These fluke events can cause the compositor to crash.
    
    BUG=293265
    TBR=kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/23726044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224233 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 29bad18d8e69dfb563e1bed0a015136be4ee86b9

--- content/common/gpu/image_transport_surface_win.cc
+++ content/common/gpu/image_transport_surface_win.cc
@@ -208,8 +208,11 @@ void PbufferImageTransportSurface::SendBuffersSwapped() {
 
 void PbufferImageTransportSurface::OnBufferPresented(
     const AcceleratedSurfaceMsg_BufferPresented_Params& params) {
-  helper_->SendUpdateVSyncParameters(params.vsync_timebase,
-                                     params.vsync_interval);
+  if (!params.vsync_timebase.is_null() &&
+      params.vsync_interval != base::TimeDelta()) {
+    helper_->SendUpdateVSyncParameters(params.vsync_timebase,
+                                       params.vsync_interval);
+  }
   is_swap_buffers_pending_ = false;
   if (did_unschedule_) {
     did_unschedule_ = false;
