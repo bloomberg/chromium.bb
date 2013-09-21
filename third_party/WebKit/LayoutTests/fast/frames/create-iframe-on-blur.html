bloomberg/chromium.bb

commit 0bbee627af398c18b48f8903904eb76665ea2ab4

Fix crash by focusing multiple-fields input types during a unload event.
ContainerNode::removeChildren:
We should remove focus after dispatching unload events. Document::
m_focusedNode could have an element not in the document tree.

Document::implicitOpen:
Confirm that removeChildren clears m_focusedNode.

BaseMultipleFieldsDateAndTimeInputType::didBlurFromControl:
Protect element() because it might loose the last reference by
setFocus(false).

BUG=249640

Review URL: https://chromiumcodereview.appspot.com/17577013

git-svn-id: svn://svn.chromium.org/blink/trunk@152990 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 1221c8e17686f6105afe0212ccc3710050d28df7

third_party/WebKit/Source/core/dom/ContainerNode.cpp
@@ -556,15 +556,18 @@ void ContainerNode::removeChildren()
     // The container node can be removed from event handlers.
     RefPtr<ContainerNode> protect(this);
 
-    // exclude this node when looking for removed focusedNode since only children will be removed
-    document()->removeFocusedNodeOfSubtree(this, true);
-
     if (FullscreenController* fullscreen = FullscreenController::fromIfExists(document()))
         fullscreen->removeFullScreenElementOfSubtree(this, true);
 
     // Do any prep work needed before actually starting to detach
     // and remove... e.g. stop loading frames, fire unload events.
     willRemoveChildren(protect.get());
+
+    // Exclude this node when looking for removed focusedNode since only
+    // children will be removed.
+    // This must be later than willRemvoeChildren, which might change focus
+    // state of a child.
+    document()->removeFocusedNodeOfSubtree(this, true);
 
     NodeVector removedChildren;
     {

third_party/WebKit/LayoutTests/fast/forms/time-multiple-fields/time-multiple-fields-crash-by-focus-on-unload-expected.txt
@@ -1,0 +1,1 @@
+PASS if not crashed.

third_party/WebKit/LayoutTests/fast/forms/time-multiple-fields/time-multiple-fields-crash-by-focus-on-unload.html
@@ -1,0 +1,21 @@
+<script>
+// This test requires ASAN to detect a regression.
+function runTest() {
+    document.querySelector('iframe').contentWindow.addEventListener('unload', function() {
+        document.querySelector('input').focus();
+    }, true);
+
+    if (window.testRunner)
+        testRunner.dumpAsText();
+    document.writeln('');
+    if (window.GCController)
+        GCController.collect();
+    else if (window.gc)
+        gc();
+    document.writeln('PASS if not crashed.');
+}
+</script>
+<body onload="runTest()">
+<input type="time">
+<iframe></iframe>
+</body>

third_party/WebKit/Source/core/dom/Document.cpp
@@ -2146,6 +2146,7 @@ void Document::implicitOpen()
     cancelParsing();
 
     removeChildren();
+    ASSERT(!m_focusedNode);
 
     setCompatibilityMode(NoQuirksMode);
 

third_party/WebKit/Source/core/html/BaseMultipleFieldsDateAndTimeInputType.cpp
@@ -153,6 +153,7 @@ void BaseMultipleFieldsDateAndTimeInputType::didBlurFromControl()
     // We don't need to call blur(). This function is called when control
     // lost focus.
 
+    RefPtr<HTMLInputElement> protector(element());
     // Remove focus ring by CSS "focus" pseudo class.
     element()->setFocus(false);
 }
@@ -163,6 +164,7 @@ void BaseMultipleFieldsDateAndTimeInputType::didFocusOnControl()
     // got focus.
 
     // Add focus ring by CSS "focus" pseudo class.
+    // FIXME: Setting the focus flag to non-focused element is too tricky.
     element()->setFocus(true);
 }
 

commit 5944fabab126eafbb220602793f3097501cac01b

Prevent subframe loads during focus-removal in ContainerNode::removeChildren
This fixes a regression from r152990, which re-ordered the calls to
willRemoveChildren() and removeFocusedElementOfSubtree() to fix a different
crash.

R=esprehn
BUG=294202

Review URL: https://chromiumcodereview.appspot.com/24227007

git-svn-id: svn://svn.chromium.org/blink/trunk@158145 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: d02da4c95dead1c95cce3276e3612de9fbe9921e

third_party/WebKit/Source/core/dom/ContainerNode.cpp
@@ -553,11 +553,17 @@ void ContainerNode::removeChildren()
     // and remove... e.g. stop loading frames, fire unload events.
     willRemoveChildren(protect.get());
 
-    // Exclude this node when looking for removed focusedElement since only
-    // children will be removed.
-    // This must be later than willRemvoeChildren, which might change focus
-    // state of a child.
-    document().removeFocusedElementOfSubtree(this, true);
+    {
+        // Removing focus can cause frames to load, either via events (focusout, blur)
+        // or widget updates (e.g., for <embed>).
+        SubframeLoadingDisabler disabler(this);
+
+        // Exclude this node when looking for removed focusedElement since only
+        // children will be removed.
+        // This must be later than willRemoveChildren, which might change focus
+        // state of a child.
+        document().removeFocusedElementOfSubtree(this, true);
+    }
 
     NodeVector removedChildren;
     {

third_party/WebKit/LayoutTests/fast/frames/create-iframe-on-blur-expected.txt
@@ -1,0 +1,10 @@
+iframes added to a subtree that's being removed should not load
+
+On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE".
+
+
+PASS iframe.contentWindow is null
+PASS successfullyParsed is true
+
+TEST COMPLETE
+

third_party/WebKit/LayoutTests/fast/frames/create-iframe-on-blur.html
@@ -1,0 +1,20 @@
+<!DOCTYPE html>
+<body>
+<div id=container>
+    <input>
+</div>
+<script src="../js/resources/js-test-pre.js"></script>
+<script>
+description("iframes added to a subtree that's being removed should not load");
+var container = document.querySelector('#container');
+var input = document.querySelector('input');
+input.focus();
+input.addEventListener('blur', function() {
+    iframe = document.createElement('iframe');
+    input.parentNode.appendChild(iframe);
+});
+container.innerHTML = '';
+shouldBeNull("iframe.contentWindow");
+</script>
+<script src="../js/resources/js-test-post.js"></script>
+</body>