0c3344285132a6faf665656b22613fdda554761c
felt@chromium.org <felt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-23 17:35:20 +0000

    Widening the disable list for DenyingCameraDoesNotCauseStickyDenyForMics
    Seeing numerous CQ failures on linux_chromeos for DenyingCameraDoesNotCauseStickyDenyForMics.
    
    BUG=263333
    TBR=ddorwin@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/23606039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224740 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ebc4bd6c0849cf639c9b36056c75efd0fbf7c05a

--- chrome/browser/media/chrome_media_stream_infobar_browsertest.cc
+++ chrome/browser/media/chrome_media_stream_infobar_browsertest.cc
@@ -175,7 +175,7 @@ IN_PROC_BROWSER_TEST_F(MediaStreamInfoBarTest,
                                                kVideoOnlyCallConstraints);
 }
 
-#if defined(OS_CHROMEOS) && !defined(NDEBUG)
+#if defined(OS_CHROMEOS)
 #define MAYBE_DenyingCameraDoesNotCauseStickyDenyForMics \
   DISABLED_DenyingCameraDoesNotCauseStickyDenyForMics
 #else
