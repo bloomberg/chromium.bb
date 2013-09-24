07cf06fffab4a53254f4b8bb5f22e37169a58c09
brianderson@chromium.org <brianderson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-24 05:00:28 +0000

    android: Enable deadline scheduling by default.
    BUG=243461
    NOTRY=TRUE
    
    Review URL: https://chromiumcodereview.appspot.com/24135004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@224907 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 23170015821aa14f45cc64b6b674d552cda18d80

--- content/browser/android/content_startup_flags.cc
+++ content/browser/android/content_startup_flags.cc
@@ -58,6 +58,7 @@ void SetContentCommandLineFlags(int max_render_process_count,
   parsed_command_line->AppendSwitch(
       switches::kEnableCompositedScrollingForFrames);
   parsed_command_line->AppendSwitch(switches::kEnableBeginFrameScheduling);
+  parsed_command_line->AppendSwitch(switches::kEnableDeadlineScheduling);
 
   parsed_command_line->AppendSwitch(switches::kEnableGestureTapHighlight);
   parsed_command_line->AppendSwitch(switches::kEnablePinch);
