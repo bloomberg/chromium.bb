eeef7225721f43f25002ca2a4ed8ef51cd1a6689
petarj@mips.com <petarj@mips.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-09-24 06:51:26 +0000

    [MIPS] Move the upper guard line to 0x40008000
    Correct width of guard region for MIPS is 32K because the offset that is
    added to the contents of GPR base to form an effective address in load and
    store instructions is a 16-bit signed value.
    This change moves the guard line to 0x40008000.
    
    BUG=
    TEST= run smoke tests
    
    Review URL: https://chromiumcodereview.appspot.com/24379002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12196 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 4b836d1c2c635250bd38663afc66b54518a53272

--- src/trusted/service_runtime/arch/mips/sel_ldr_mips.h
+++ src/trusted/service_runtime/arch/mips/sel_ldr_mips.h
@@ -15,8 +15,22 @@
 
 #define NACL_MAX_ADDR_BITS      30
 
+/*
+ * Guard region is 32KB wide for MIPS32. This comes from the fact that load
+ * and store instructions can use signed 16-bit offset from a register, so
+ * the guard region is [0x40000000-0x40007FFF]. The guard size of 32KB is
+ * sufficient only in case this memory is mapped as PROT_NONE. Otherwise,
+ * we need to extend the guard region.
+ *
+ * Example of lw/sw instruction on MIPS: lw $a0, 0x7FFF($a0)
+ *
+ * Lower guard size is zero, as there is no need to have guard region in the
+ * address range [0xFFFF8000-0xFFFFFFFF] since this address space belongs to
+ * kernel virtual space, and user space already does not have privilege to
+ * access it.
+ */
 #define NACL_ADDRSPACE_LOWER_GUARD_SIZE 0
-#define NACL_ADDRSPACE_UPPER_GUARD_SIZE 0x2000
+#define NACL_ADDRSPACE_UPPER_GUARD_SIZE 0x8000
 
 /* Must be synced with irt_compatible_rodata_addr in SConstruct */
 #define NACL_DATA_SEGMENT_START 0x10000000
