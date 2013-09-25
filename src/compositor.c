840a421a5e4525898eb51ea9e1a908bfb4212d92
Alexandru DAMIAN <alexandru.damian@intel.com>
2013-09-25 14:47:47 +0100

    compositor: check if seteuid worked
    Checking the return value from seteuid in
    order to not launch clients with the wrong effective uid.
    
    Signed-off-by: Alexandru DAMIAN <alexandru.damian@intel.com>

--- src/compositor.c
+++ src/compositor.c
@@ -247,8 +247,11 @@ child_client_exec(int sockfd, const char *path)
 	sigfillset(&allsigs);
 	sigprocmask(SIG_UNBLOCK, &allsigs, NULL);
 
-	/* Launch clients as the user. */
-	seteuid(getuid());
+	/* Launch clients as the user. Do not lauch clients with wrong euid.*/
+	if (seteuid(getuid()) == -1) {
+		weston_log("compositor: failed seteuid\n");
+		return;
+	}
 
 	/* SOCK_CLOEXEC closes both ends, so we dup the fd to get a
 	 * non-CLOEXEC fd to pass through exec. */
