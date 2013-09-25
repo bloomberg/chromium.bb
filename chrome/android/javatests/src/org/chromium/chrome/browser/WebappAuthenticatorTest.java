0c771c98796857a7fec7446ed471696ac1ae41aa
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-25 19:43:10 +0000

    Move WebappAuthenticator to its new package.
    R=dtrainor@chromium.org
    TBR=dfalcantara@chromium.org, klobag@chromium.org
    BUG=285924
    
    Review URL: https://codereview.chromium.org/23710085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225230 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5d4c422a0fbbc4393b4e816d4f0f3b6472cd0377

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/WebappAuthenticatorTest.java
@@ -0,0 +1,23 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser;
+
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+
+public class WebappAuthenticatorTest extends InstrumentationTestCase {
+    @SmallTest
+    public void testAuthentication() {
+        Context context = getInstrumentation().getTargetContext();
+        String url = "http://www.example.org/hello.html";
+        byte[] mac = WebappAuthenticator.getMacForUrl(context, url);
+        assertNotNull(mac);
+        assertTrue(WebappAuthenticator.isUrlValid(context, url, mac));
+        assertFalse(WebappAuthenticator.isUrlValid(context, url + "?goats=true", mac));
+        mac[4] += 1;
+        assertFalse(WebappAuthenticator.isUrlValid(context, url, mac));
+    }
+}
