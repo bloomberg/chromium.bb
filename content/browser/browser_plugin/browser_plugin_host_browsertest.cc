7c4fc46588a91accb9815e19db78ac5446d3d7f7
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-25 16:48:34 +0000

    Disable BrowserPluginHostTest.AcceptDragEvents under ASan on Mac
    BUG=297576
    TBR=sadrul@chromium.org
    
    Review URL: https://codereview.chromium.org/23534072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225198 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 357036250288694b1eafef588d9e7936a9c7f30b

--- content/browser/browser_plugin/browser_plugin_host_browsertest.cc
+++ content/browser/browser_plugin/browser_plugin_host_browsertest.cc
@@ -596,8 +596,10 @@ IN_PROC_BROWSER_TEST_F(BrowserPluginHostTest, ReloadEmbedder) {
   }
 }
 
-// Always failing in the win7_aura try bot.  See http://crbug.com/181107.
-#if defined(OS_WIN) && defined(USE_AURA)
+// Always failing in the win7_aura try bot. See http://crbug.com/181107.
+// Times out under AddressSanitizer on Mac. See http://crbug.com/297576.
+#if (defined(OS_WIN) && defined(USE_AURA)) || \
+    (defined(OS_MACOSX) && defined(ADDRESS_SANITIZER))
 #define MAYBE_AcceptDragEvents DISABLED_AcceptDragEvents
 #else
 #define MAYBE_AcceptDragEvents AcceptDragEvents
