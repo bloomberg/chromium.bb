d4d4fdbd2e4677fec7437750724fd15e68d744ba
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 17:36:13 +0000

    [Android] When toggling AX script inject, also set native AX state.
    When we enable/disable script injection, also update the flag that
    says whether we should use native accessibility.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/24559002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225491 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5fd6d733a27ff1b6fb2607831853e827514eb889

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -2957,15 +2957,12 @@ import java.util.Map;
     public void setAccessibilityState(boolean state) {
         if (!state) {
             setInjectedAccessibility(false);
-            return;
+            mNativeAccessibilityAllowed = false;
+        } else {
+            boolean useScriptInjection = isDeviceAccessibilityScriptInjectionEnabled();
+            setInjectedAccessibility(useScriptInjection);
+            mNativeAccessibilityAllowed = !useScriptInjection;
         }
-
-        if (isDeviceAccessibilityScriptInjectionEnabled()) {
-            setInjectedAccessibility(true);
-            return;
-        }
-
-        mNativeAccessibilityAllowed = true;
     }
 
     /**
