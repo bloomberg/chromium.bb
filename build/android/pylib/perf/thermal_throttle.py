38b434774cd87395e3018f0008ad51888bedfcc5
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-26 21:58:02 +0000

    android: Add detection for Exynos 5 thermal throttling
    Add logic to detect thermal throttling on Exynos 5 based SoCs. Thermal
    throttling can affect benchmark results, and for that reason we print a
    warning if it happens while a benchmark was running.
    
    BUG=294358
    
    Review URL: https://codereview.chromium.org/24748002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225584 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8f7c1f76f82d5469869417b01b0b21989b926826

--- build/android/pylib/perf/thermal_throttle.py
+++ build/android/pylib/perf/thermal_throttle.py
@@ -5,6 +5,55 @@
 import logging
 
 
+class OmapThrottlingDetector(object):
+  """Class to detect and track thermal throttling on an OMAP 4."""
+  OMAP_TEMP_FILE = ('/sys/devices/platform/omap/omap_temp_sensor.0/'
+                    'temperature')
+
+  @staticmethod
+  def IsSupported(adb):
+    return adb.FileExistsOnDevice(OmapThrottlingDetector.OMAP_TEMP_FILE)
+
+  def __init__(self, adb):
+    self._adb = adb
+
+  def BecameThrottled(self, log_line):
+    return 'omap_thermal_throttle' in log_line
+
+  def BecameUnthrottled(self, log_line):
+    return 'omap_thermal_unthrottle' in log_line
+
+  def GetThrottlingTemperature(self, log_line):
+    if 'throttle_delayed_work_fn' in log_line:
+      return float([s for s in log_line.split() if s.isdigit()][0]) / 1000.0
+
+  def GetCurrentTemperature(self):
+    tempdata = self._adb.GetFileContents(OmapThrottlingDetector.OMAP_TEMP_FILE)
+    return float(tempdata[0]) / 1000.0
+
+
+class ExynosThrottlingDetector(object):
+  """Class to detect and track thermal throttling on an Exynos 5."""
+  @staticmethod
+  def IsSupported(adb):
+    return adb.FileExistsOnDevice('/sys/bus/exynos5-core')
+
+  def __init__(self, adb):
+    pass
+
+  def BecameThrottled(self, log_line):
+    return 'exynos_tmu: Throttling interrupt' in log_line
+
+  def BecameUnthrottled(self, log_line):
+    return 'exynos_thermal_unthrottle: not throttling' in log_line
+
+  def GetThrottlingTemperature(self, log_line):
+    return None
+
+  def GetCurrentTemperature(self):
+    return None
+
+
 class ThermalThrottle(object):
   """Class to detect and track thermal throttling.
 
@@ -12,14 +61,16 @@ class ThermalThrottle(object):
     Wait for IsThrottled() to be False before running test
     After running test call HasBeenThrottled() to find out if the
     test run was affected by thermal throttling.
-
-    Currently assumes an OMap device.
   """
 
   def __init__(self, adb):
     self._adb = adb
     self._throttled = False
-
+    self._detector = None
+    if OmapThrottlingDetector.IsSupported(adb):
+      self._detector = OmapThrottlingDetector(adb)
+    elif ExynosThrottlingDetector.IsSupported(adb):
+      self._detector = ExynosThrottlingDetector(adb)
 
   def HasBeenThrottled(self):
     """True if there has been any throttling since the last call to
@@ -33,35 +84,34 @@ class ThermalThrottle(object):
     return self._throttled
 
   def _ReadLog(self):
+    if not self._detector:
+      return False
     has_been_throttled = False
     serial_number = self._adb.Adb().GetSerialNumber()
     log = self._adb.RunShellCommand('dmesg -c')
     degree_symbol = unichr(0x00B0)
     for line in log:
-      if 'omap_thermal_throttle' in line:
+      if self._detector.BecameThrottled(line):
         if not self._throttled:
-          logging.warning('>>> Device %s Thermally Throttled', serial_number)
+          logging.warning('>>> Device %s thermally throttled', serial_number)
         self._throttled = True
         has_been_throttled = True
-      if 'omap_thermal_unthrottle' in line:
+      elif self._detector.BecameUnthrottled(line):
         if self._throttled:
-          logging.warning('>>> Device %s Thermally Unthrottled', serial_number)
+          logging.warning('>>> Device %s thermally unthrottled', serial_number)
         self._throttled = False
         has_been_throttled = True
-      if 'throttle_delayed_work_fn' in line:
-        temp = float([s for s in line.split() if s.isdigit()][0]) / 1000.0
-        logging.info(u' Device %s Thermally Thottled at %3.1f%sC',
-                     serial_number, temp, degree_symbol)
+      temperature = self._detector.GetThrottlingTemperature(line)
+      if temperature is not None:
+        logging.info(u'Device %s thermally throttled at %3.1f%sC',
+                     serial_number, temperature, degree_symbol)
 
     if logging.getLogger().isEnabledFor(logging.DEBUG):
-      # Print temperature of CPU SoC.
-      omap_temp_file = ('/sys/devices/platform/omap/omap_temp_sensor.0/'
-                        'temperature')
-      if self._adb.FileExistsOnDevice(omap_temp_file):
-        tempdata = self._adb.GetFileContents(omap_temp_file)
-        temp = float(tempdata[0]) / 1000.0
-        logging.debug(u'Current OMAP Temperature of %s = %3.1f%sC',
-                      serial_number, temp, degree_symbol)
+      # Print current temperature of CPU SoC.
+      temperature = self._detector.GetCurrentTemperature()
+      if temperature is not None:
+        logging.debug(u'Current SoC temperature of %s = %3.1f%sC',
+                      serial_number, temperature, degree_symbol)
 
       # Print temperature of battery, to give a system temperature
       dumpsys_log = self._adb.RunShellCommand('dumpsys battery')
