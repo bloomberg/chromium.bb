6c1785621702d0412590da7aadd055b4403a1bc2
trchen@chromium.org <trchen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-26 07:07:41 +0000

    Fix a logic mistake calculating video compositing requirement
    With overlay fullscreen video enabled, video must be composited when
    fullscreen. However the opposite does not apply.
    
    R=esprehn
    BUG=262945
    
    Review URL: https://chromiumcodereview.appspot.com/24647004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158359 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ee26b3bd2d0b107adfcabf11140d3cf15f04c39f

--- third_party/WebKit/Source/core/rendering/RenderLayerCompositor.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerCompositor.cpp
@@ -1887,7 +1887,8 @@ bool RenderLayerCompositor::requiresCompositingForVideo(RenderObject* renderer) 
 {
     if (RuntimeEnabledFeatures::overlayFullscreenVideoEnabled() && renderer->isVideo()) {
         HTMLMediaElement* media = toHTMLMediaElement(renderer->node());
-        return media->isFullscreen();
+        if (media->isFullscreen())
+            return true;
     }
 
     if (!(m_compositingTriggers & ChromeClient::VideoTrigger))
