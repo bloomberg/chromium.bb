f9c576a578a2836b4efee65cd91ed40661c6b095
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-30 11:33:39 +0000

    Disabled flaky EncryptedMediaIsTypeSupportedPepperCDMRegisteredWithWrongPathTest.PepperCDMsRegisteredButAdapterNotPresent.
    TBR=acolwell@chromium.org
    BUG=301571
    
    Review URL: https://codereview.chromium.org/25132006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225961 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 470240cd0265d2792543b47092473d730d9ec9f1

--- chrome/browser/media/encrypted_media_istypesupported_browsertest.cc
+++ chrome/browser/media/encrypted_media_istypesupported_browsertest.cc
@@ -896,9 +896,16 @@ IN_PROC_BROWSER_TEST_F(EncryptedMediaIsTypeSupportedTest,
 // Since this test fixture does not register the CDMs on the command line, the
 // check for the CDMs in chrome_key_systems.cc should fail, and they should not
 // be registered with KeySystems.
+#if defined(OS_WIN)
+// Flaky on Windows: crbug.com/301571.
+#define MAYBE_PepperCDMsRegisteredButAdapterNotPresent DISABLED_PepperCDMsRegisteredButAdapterNotPresent
+#else
+#define MAYBE_PepperCDMsRegisteredButAdapterNotPresent PepperCDMsRegisteredButAdapterNotPresent
+#endif
+
 IN_PROC_BROWSER_TEST_F(
     EncryptedMediaIsTypeSupportedPepperCDMRegisteredWithWrongPathTest,
-    PepperCDMsRegisteredButAdapterNotPresent) {
+    MAYBE_PepperCDMsRegisteredButAdapterNotPresent) {
   EXPECT_FALSE(IsConcreteSupportedKeySystem(kExternalClearKey));
   EXPECT_FALSE(IsSupportedKeySystemWithMediaMimeType(
       "video/webm", no_codecs(), kExternalClearKey));
