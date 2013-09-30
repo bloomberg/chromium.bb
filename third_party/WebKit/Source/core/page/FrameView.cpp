990f0e932d320527a26768643d0fc2abe52027d2
shawnsingh@chromium.org <shawnsingh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-09-30 18:11:37 +0000

    Composited fixed-pos element should still cause repaints if it paintsIntoCompositedAncestor
    A composited fixed-position element may still actually be set to
    paint into its composited ancestor, even though it has its own backing.
    ScrollingCoordinator and FrameView were not recognizing this situation
    and thus the fixed-position element did not repaint, appearing to be
    attached to the scrolling contents rather than the viewport.
    
    This patch makes the FrameView and ScrollingCoordinator aware of the
    scenario so that main-thread scrolling is forced and repaints occur.
    
    This patch is an exact re-land of 157920, which was reverted in 158540.
    It was reverted only because it had a very badly misleading description.
    
    TBR=enne@chromium.org
    BUG=151734
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/25110005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158541 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 3283cfd7118bfb1678a42772f661f6122402bfd3

--- third_party/WebKit/Source/core/page/FrameView.cpp
+++ third_party/WebKit/Source/core/page/FrameView.cpp
@@ -1354,12 +1354,15 @@ bool FrameView::scrollContentsFastPath(const IntSize& scrollDelta, const IntRect
         if (!renderer->style()->hasViewportConstrainedPosition())
             continue;
 
-        if (renderer->isComposited())
-            continue;
-
         // Fixed items should always have layers.
         ASSERT(renderer->hasLayer());
         RenderLayer* layer = toRenderBoxModelObject(renderer)->layer();
+
+        // Composited layers may still actually paint into their ancestor.
+        // If that happens, the viewport constrained object needs to be
+        // repainted on scroll.
+        if (layer->isComposited() && !layer->backing()->paintsIntoCompositedAncestor())
+            continue;
 
         if (layer->viewportConstrainedNotCompositedReason() == RenderLayer::NotCompositedForBoundsOutOfView
             || layer->viewportConstrainedNotCompositedReason() == RenderLayer::NotCompositedForNoVisibleContent) {
