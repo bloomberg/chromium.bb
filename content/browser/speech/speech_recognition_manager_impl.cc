611da54e078289271718c91bf533da758c4f3486
primiano@chromium.org <primiano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-30 15:41:07 +0000

    Web Speech API: cancel outstanding infobar requests when aborting.
    This CL enforces the cancellation of outstanding media requests
    for speech recognition sessions which are aborted before getting
    the media request approval (i.e. before the user clicks on the
    Allow/Deny media access request infobar).
    
    BUG=296690
    
    Review URL: https://codereview.chromium.org/25045007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225985 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2542d88ac804d6d628c87f74f3f5e77a53aacd2c

--- content/browser/speech/speech_recognition_manager_impl.cc
+++ content/browser/speech/speech_recognition_manager_impl.cc
@@ -609,6 +609,8 @@ void SpeechRecognitionManagerImpl::SessionDelete(Session* session) {
   DCHECK(session->recognizer.get() == NULL || !session->recognizer->IsActive());
   if (primary_session_id_ == session->id)
     primary_session_id_ = kSessionIDInvalid;
+  if (!session->context.label.empty())
+    media_stream_manager_->CancelRequest(session->context.label);
   sessions_.erase(session->id);
   delete session;
 }
