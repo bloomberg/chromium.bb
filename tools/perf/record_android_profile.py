a94e2b73e5af83cfebffe720fef5e3322179da96
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-09-30 11:23:01 +0000

    Telemetry / Android: adds prebuilt binaries for perf and tcpdump profilers.
    Automates the flow and eliminates the out-of-band profilers setup.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/24749002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225958 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e688c2e490707745a036e97e7b5dc8929fadf470

---
+++ tools/perf/record_android_profile.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+import tempfile
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir, 'telemetry'))
+
+from telemetry.core import browser_finder
+from telemetry.core import browser_options
+
+
+def _RunPrebuilt(options):
+  browser_to_create = browser_finder.FindBrowser(options)
+  with browser_to_create.Create() as browser:
+    browser.Start()
+    output_file = os.path.join(tempfile.mkdtemp(), options.profiler)
+    print '>> Starting profiler', options.profiler
+    browser.StartProfiling(options.profiler, output_file)
+    print 'Press enter or CTRL+C to stop'
+    try:
+      raw_input()
+    except KeyboardInterrupt:
+      pass
+    finally:
+      browser.StopProfiling()
+    print '<< Stopped profiler ', options.profiler
+
+
+if __name__ == '__main__':
+  browser_finder_options = browser_options.BrowserFinderOptions()
+  parser = browser_finder_options.CreateParser('')
+  profiler_options, _ = parser.parse_args()
+  sys.exit(_RunPrebuilt(profiler_options))
