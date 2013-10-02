bdcea0aab27af7416b9f5443fe7bd9f29bc74b96
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-02 08:33:22 +0000

    Remove false assertion in ApplyBlockElementCommand::formatSelection()
    Note: This patch is preparation of fixing issue 294456.
    
    This patch removes false assertion in ApplyBlockElementCommand::formatSelection(), when contents of being indent is modified, e.g. mutation event, |endOfNextParagraph| can hold removed contents.
    
    BUG=294456
    TEST=n/a
    R=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/25657004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@158701 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: e1524692d362e607e806569147096dfb8c38cb6a

--- third_party/WebKit/Source/core/editing/ApplyBlockElementCommand.cpp
+++ third_party/WebKit/Source/core/editing/ApplyBlockElementCommand.cpp
@@ -146,11 +146,9 @@ void ApplyBlockElementCommand::formatSelection(const VisiblePosition& startOfSel
         if (endAfterSelection.isNotNull() && !endAfterSelection.deepEquivalent().inDocument())
             break;
         // Sanity check: Make sure our moveParagraph calls didn't remove endOfNextParagraph.deepEquivalent().deprecatedNode()
-        // If somehow we did, return to prevent crashes.
-        if (endOfNextParagraph.isNotNull() && !endOfNextParagraph.deepEquivalent().inDocument()) {
-            ASSERT_NOT_REACHED();
+        // If somehow, e.g. mutation event handler, we did, return to prevent crashes.
+        if (endOfNextParagraph.isNotNull() && !endOfNextParagraph.deepEquivalent().inDocument())
             return;
-        }
         endOfCurrentParagraph = endOfNextParagraph;
     }
 }
