255ec9830b54cdd71d086acad421e01a64e534e4
bajones@chromium.org <bajones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 02:22:21 +0000

    Added WebGL Conformance test expectations for Windows
    BUG=225939
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/25803002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226665 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ecda276adeab2d9088562051806d7bc48e777938

--- content/test/gpu/gpu_tests/webgl_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl_conformance_expectations.py
@@ -16,6 +16,63 @@ from telemetry.page import test_expectations
 class WebGLConformanceExpectations(test_expectations.TestExpectations):
   def SetExpectations(self):
     # Sample Usage:
-    # self.Fail("gl-enable-vertex-attrib.html",
+    # self.Fail('gl-enable-vertex-attrib.html',
     #     ['mac', 'amd', ('nvidia', 0x1234)], bug=123)
-    pass
+
+    # Windows/Intel failures
+    self.Fail('conformance/textures/texture-size.html',
+        ['win', 'intel'], bug=121139)
+
+    # Windows 7/Intel failures
+    self.Fail('conformance/context/context-lost-restored.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/context/premultiplyalpha-test.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/extensions/oes-texture-float-with-image-data.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/extensions/oes-texture-float.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/limits/gl-min-attribs.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/limits/gl-max-texture-dimensions.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/limits/gl-min-textures.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/limits/gl-min-uniforms.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/rendering/gl-clear.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/copy-tex-image-and-sub-image-2d.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/gl-teximage.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/tex-image-and-sub-image-2d-with-array-buffer-view.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/tex-image-and-sub-image-2d-with-image-data.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/tex-image-and-sub-image-2d-with-image-data-rgb565.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/tex-image-and-sub-image-2d-with-image-data-rgba4444.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/tex-image-and-sub-image-2d-with-image-data-rgba5551.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/tex-image-with-format-and-type.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/tex-sub-image-2d.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/texparameter-test.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/texture-active-bind-2.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/texture-active-bind.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/texture-complete.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/texture-formats-test.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/texture-mips.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/texture-npot.html',
+        ['win7', 'intel'])
+    self.Fail('conformance/textures/texture-size-cube-maps.html',
+        ['win7', 'intel'])
