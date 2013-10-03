7c5b93092c21ad445e2472b8b780fb287362d887
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 10:42:07 +0000

    Files.app: Delay disappearing a completed progress item.
    Originally a progress item disappears immediately after it is completed.
    This CL adds the timeout callback to delay the disappearing.
    
    BUG=277432
    TEST=manually
    
    Review URL: https://codereview.chromium.org/25709003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226727 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1a8d8938aec783d2cc8bf663be45d1c37226bb36

--- chrome/browser/resources/file_manager/js/ui/progress_center_panel.js
+++ chrome/browser/resources/file_manager/js/ui/progress_center_panel.js
@@ -13,6 +13,15 @@
 var ProgressCenterPanel = function(element) {
   this.element_ = element;
   this.openView_ = this.element_.querySelector('#progress-center-open-view');
+
+  /**
+   * Timeout callback to remove items.
+   * @type {TimeoutManager}
+   * @private
+   */
+  this.removingTimeout_ = new ProgressCenterPanel.TimeoutManager(
+      this.completeItemRemoving_.bind(this));
+
   Object.freeze(this);
 
   // Register event handlers.
@@ -20,14 +29,46 @@ var ProgressCenterPanel = function(element) {
 };
 
 /**
- * Replaces the contents of this panel with the items.
- * @param {Array.<ProgressCenterItem>} items Items to be added to the panel.
+ * The default amount of milliseconds time, before a progress item will hide
+ * after the last update.
+ * @type {number}
+ * @private
+ * @const
  */
-ProgressCenterPanel.prototype.setItems = function(items) {
-  this.element_.innerText = '';
-  for (var i = 0; i < items.length; i++) {
-    this.addItem(items[i]);
-  }
+ProgressCenterPanel.HIDE_DELAY_TIME_MS_ = 2000;
+
+/**
+ * Whether to use the new progress center UI or not.
+ * TODO(hirono): Remove the flag after replacing the old butter bar with the new
+ * progress center.
+ * @type {boolean}
+ * @private
+ */
+ProgressCenterPanel.ENABLED_ = false;
+
+/**
+ * Utility for timeout callback.
+ *
+ * @param {function(*):*} callback Callbakc function.
+ * @constructor
+ */
+ProgressCenterPanel.TimeoutManager = function(callback) {
+  this.callback_ = callback;
+  this.id_ = null;
+  Object.seal(this);
+};
+
+/**
+ * Requests timeout. Previous request is canceled.
+ * @param {number} milliseconds Time to invoke the callback function.
+ */
+ProgressCenterPanel.TimeoutManager.prototype.request = function(milliseconds) {
+  if (this.id_)
+    clearTimeout(this.id_);
+  this.id_ = setTimeout(function() {
+    this.id_ = null;
+    this.callback_();
+  }.bind(this), milliseconds);
 };
 
 /**
@@ -38,6 +79,8 @@ ProgressCenterPanel.prototype.addItem = function(item) {
   this.openView_.insertBefore(
       this.createItemElement_(item),
       this.openView_.firstChild);
+  if (ProgressCenterPanel.ENABLED_)
+    this.element_.hidden = false;
 };
 
 /**
@@ -60,12 +103,38 @@ ProgressCenterPanel.prototype.updateItem = function(item) {
  * @param {number} id progress item ID.
  */
 ProgressCenterPanel.prototype.removeItem = function(id) {
+  // Obtain the item element.
   var itemElement = this.getItemElement_(id);
   if (!itemElement) {
     console.error('Invalid progress ID.');
     return;
   }
-  this.openView_.removeChild(itemElement);
+
+  // Check if already requested to remove.
+  if (itemElement.classList.contains('complete'))
+    return;
+
+  // Set timeout function to remove.
+  itemElement.classList.add('complete');
+  this.removingTimeout_.request(ProgressCenterPanel.HIDE_DELAY_TIME_MS_);
+};
+
+/**
+ * Completes item removing.
+ * @param {HTMLElement} itemElement Element to be removed.
+ * @private
+ */
+ProgressCenterPanel.prototype.completeItemRemoving_ = function(itemElement) {
+  // If there are still progressing items, return the function.
+  if (this.openView_.querySelectorAll('li:not(.complete)').length)
+    return;
+
+  // Clear the all compete item.
+  this.openView_.innerHTML = '';
+
+  // Hide the progress center.
+  this.element_.hidden = true;
+  this.element_.classList.remove('opened');
 };
 
 /**
