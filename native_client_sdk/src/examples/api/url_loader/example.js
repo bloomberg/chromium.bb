332e36ee2761e0e6c3cf167ca7515266c486de8c
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-03 21:02:44 +0000

    [NaCl SDK] Add test for url_loader example.
    BUG=none
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/23461007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@226846 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2d0f559496fa43c0a88526104840f56c33fe1bb1

--- native_client_sdk/src/examples/api/url_loader/example.js
+++ native_client_sdk/src/examples/api/url_loader/example.js
@@ -20,19 +20,19 @@ function loadUrl() {
 
 // Called by the common.js module.
 function handleMessage(message_event) {
-  var logElt = document.getElementById('generalOutput');
+  var logEl = document.getElementById('output');
   // Find the first line break.  This separates the URL data from the
   // result text.  Note that the result text can contain any number of
   // '\n' characters, so split() won't work here.
   var url = message_event.data;
   var result = '';
-  var eol_pos = message_event.data.indexOf("\n");
-  if (eol_pos != -1) {
-    url = message_event.data.substring(0, eol_pos);
-    if (eol_pos < message_event.data.length - 1) {
-      result = message_event.data.substring(eol_pos + 1);
+  var eolPos = message_event.data.indexOf('\n');
+  if (eolPos != -1) {
+    url = message_event.data.substring(0, eolPos);
+    if (eolPos < message_event.data.length - 1) {
+      result = message_event.data.substring(eolPos + 1);
     }
   }
-  logElt.textContent += 'FULLY QUALIFIED URL: ' + url + '\n';
-  logElt.textContent += 'RESULT:\n' + result + '\n';
+  logEl.textContent += 'FULLY QUALIFIED URL: ' + url + '\n';
+  logEl.textContent += 'RESULT:\n' + result + '\n';
 }
