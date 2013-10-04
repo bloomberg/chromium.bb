73a0ed879214a40611af4bf1b17eda3004af7a15
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-04 23:55:10 +0000

    Revert 227100 "Track active references in ShillClientHelper (Tak..."
    > Track active references in ShillClientHelper (Take 2)
    > To prevent Shill Service DBus ObjectProxy instances from accumulating,
    > remove them when the service becomes inactive.
    > 
    > Original CL: https://codereview.chromium.org/23658053/
    > 
    > BUG=223483
    > TBR=hashimoto@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/24558004
    
    Failure:
    http://build.chromium.org/p/chromium.memory/builders/Chromium%20OS%20%28amd64%29%20ASAN/builds/6931
    
    TBR=stevenjb@chromium.org
    
    Review URL: https://codereview.chromium.org/26075002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227139 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 08121c0fba21a200ddf8e6294cb312c1195071d7

--- chromeos/dbus/shill_service_client.cc
+++ chromeos/dbus/shill_service_client.cc
@@ -5,13 +5,11 @@
 #include "chromeos/dbus/shill_service_client.h"
 
 #include "base/bind.h"
-#include "base/memory/weak_ptr.h"
 #include "base/message_loop/message_loop.h"
 #include "base/stl_util.h"
 #include "base/values.h"
 #include "chromeos/dbus/shill_property_changed_observer.h"
 #include "chromeos/dbus/shill_service_client_stub.h"
-#include "chromeos/network/network_event_log.h"
 #include "dbus/bus.h"
 #include "dbus/message.h"
 #include "dbus/object_proxy.h"
@@ -56,18 +54,7 @@ class ShillServiceClientImpl : public ShillServiceClient {
  public:
   explicit ShillServiceClientImpl()
       : bus_(NULL),
-        weak_ptr_factory_(this) {
-  }
-
-  virtual ~ShillServiceClientImpl() {
-    for (HelperMap::iterator iter = helpers_.begin();
-         iter != helpers_.end(); ++iter) {
-      ShillClientHelper* helper = iter->second;
-      bus_->RemoveObjectProxy(shill::kFlimflamServiceName,
-                              helper->object_proxy()->object_path(),
-                              base::Bind(&base::DoNothing));
-      delete helper;
-    }
+        helpers_deleter_(&helpers_) {
   }
 
   virtual void AddPropertyChangedObserver(
@@ -235,33 +222,17 @@ class ShillServiceClientImpl : public ShillServiceClient {
       return it->second;
 
     // There is no helper for the profile, create it.
-    NET_LOG_DEBUG("AddShillClientHelper", service_path.value());
     dbus::ObjectProxy* object_proxy =
         bus_->GetObjectProxy(shill::kFlimflamServiceName, service_path);
-    ShillClientHelper* helper = new ShillClientHelper(object_proxy);
-    helper->SetReleasedCallback(
-        base::Bind(&ShillServiceClientImpl::NotifyReleased,
-                   weak_ptr_factory_.GetWeakPtr()));
+    ShillClientHelper* helper = new ShillClientHelper(bus_, object_proxy);
     helper->MonitorPropertyChanged(shill::kFlimflamServiceInterface);
     helpers_.insert(HelperMap::value_type(service_path.value(), helper));
     return helper;
   }
 
-  void NotifyReleased(ShillClientHelper* helper) {
-    // New Shill Service DBus objects are created relatively frequently, so
-    // remove them when they become inactive (no observers and no active method
-    // calls).
-    const dbus::ObjectPath& object_path = helper->object_proxy()->object_path();
-    NET_LOG_DEBUG("RemoveShillClientHelper", object_path.value());
-    bus_->RemoveObjectProxy(shill::kFlimflamServiceName,
-                            object_path, base::Bind(&base::DoNothing));
-    helpers_.erase(object_path.value());
-    delete helper;
-  }
-
   dbus::Bus* bus_;
   HelperMap helpers_;
-  base::WeakPtrFactory<ShillServiceClientImpl> weak_ptr_factory_;
+  STLValueDeleter<HelperMap> helpers_deleter_;
 
   DISALLOW_COPY_AND_ASSIGN(ShillServiceClientImpl);
 };
