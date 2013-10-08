55bfb60bc8aae725676483ff167422841a069a3b
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 08:11:34 +0000

    Moved all channel-handling logic into separate NativeMessagingChannel class.
    Changes in this CL:
     - NativeMessagingHost is used only for actual processing the messages.
     - NativeMessagingChannel implements receiving and sending messages.
     - NativeMessagingChannel takes ownership of the passed inout and output handles.
     - Both NativeMessagingChannel and NativeMessagingHost are explicitly marked as not thread-safe classes.
    
    BUG=173509
    
    Review URL: https://codereview.chromium.org/23903021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227492 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7459b5a58bce5d23fc79f6308ca5698b069466bf

---
+++ remoting/host/setup/native_messaging_channel.cc
@@ -0,0 +1,129 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/setup/native_messaging_channel.h"
+
+#include "base/basictypes.h"
+#include "base/bind.h"
+#include "base/callback.h"
+#include "base/location.h"
+#include "base/values.h"
+
+#if defined(OS_POSIX)
+#include <unistd.h>
+#endif
+
+namespace {
+
+base::PlatformFile DuplicatePlatformFile(base::PlatformFile handle) {
+  base::PlatformFile result;
+#if defined(OS_WIN)
+  if (!DuplicateHandle(GetCurrentProcess(),
+                       handle,
+                       GetCurrentProcess(),
+                       &result,
+                       0,
+                       FALSE,
+                       DUPLICATE_CLOSE_SOURCE | DUPLICATE_SAME_ACCESS)) {
+    PLOG(ERROR) << "Failed to duplicate handle " << handle;
+    return base::kInvalidPlatformFileValue;
+  }
+  return result;
+#elif defined(OS_POSIX)
+  result = dup(handle);
+  base::ClosePlatformFile(handle);
+  return result;
+#else
+#error Not implemented.
+#endif
+}
+
+}  // namespace
+
+namespace remoting {
+
+NativeMessagingChannel::NativeMessagingChannel(
+    scoped_ptr<Delegate> delegate,
+    base::PlatformFile input,
+    base::PlatformFile output)
+    : native_messaging_reader_(DuplicatePlatformFile(input)),
+      native_messaging_writer_(new NativeMessagingWriter(
+          DuplicatePlatformFile(output))),
+      delegate_(delegate.Pass()),
+      pending_requests_(0),
+      shutdown_(false),
+      weak_factory_(this) {
+  weak_ptr_ = weak_factory_.GetWeakPtr();
+}
+
+NativeMessagingChannel::~NativeMessagingChannel() {
+}
+
+void NativeMessagingChannel::Start(const base::Closure& quit_closure) {
+  DCHECK(CalledOnValidThread());
+  DCHECK(quit_closure_.is_null());
+  DCHECK(!quit_closure.is_null());
+
+  quit_closure_ = quit_closure;
+  native_messaging_reader_.Start(
+      base::Bind(&NativeMessagingChannel::ProcessMessage, weak_ptr_),
+      base::Bind(&NativeMessagingChannel::Shutdown, weak_ptr_));
+}
+
+void NativeMessagingChannel::ProcessMessage(scoped_ptr<base::Value> message) {
+  DCHECK(CalledOnValidThread());
+
+  // Don't process any more messages if Shutdown() has been called.
+  if (shutdown_)
+    return;
+
+  if (message->GetType() != base::Value::TYPE_DICTIONARY) {
+    LOG(ERROR) << "Expected DictionaryValue";
+    Shutdown();
+    return;
+  }
+
+  DCHECK_GE(pending_requests_, 0);
+  pending_requests_++;
+
+  scoped_ptr<base::DictionaryValue> message_dict(
+      static_cast<base::DictionaryValue*>(message.release()));
+  delegate_->ProcessMessage(
+      message_dict.Pass(),
+      base::Bind(&NativeMessagingChannel::SendResponse, weak_ptr_));
+}
+
+void NativeMessagingChannel::SendResponse(
+    scoped_ptr<base::DictionaryValue> response) {
+  DCHECK(CalledOnValidThread());
+
+  bool success = response && native_messaging_writer_;
+  if (success)
+    success = native_messaging_writer_->WriteMessage(*response);
+
+  if (!success) {
+    // Close the write pipe so no more responses will be sent.
+    native_messaging_writer_.reset();
+    Shutdown();
+  }
+
+  pending_requests_--;
+  DCHECK_GE(pending_requests_, 0);
+
+  if (shutdown_ && !pending_requests_)
+    quit_closure_.Run();
+}
+
+void NativeMessagingChannel::Shutdown() {
+  DCHECK(CalledOnValidThread());
+
+  if (shutdown_)
+    return;
+
+  shutdown_ = true;
+  if (!pending_requests_)
+    quit_closure_.Run();
+}
+
+}  // namespace remoting
---
+++ remoting/host/setup/native_messaging_channel.h
@@ -0,0 +1,94 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_SETUP_NATIVE_MESSAGING_CHANNEL_H_
+#define REMOTING_HOST_SETUP_NATIVE_MESSAGING_CHANNEL_H_
+
+#include "base/callback.h"
+#include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/memory/weak_ptr.h"
+#include "base/platform_file.h"
+#include "base/threading/non_thread_safe.h"
+#include "remoting/host/setup/native_messaging_reader.h"
+#include "remoting/host/setup/native_messaging_writer.h"
+
+namespace base {
+class DictionaryValue;
+class Value;
+}  // namespace base
+
+namespace remoting {
+
+// Implements reading messages and sending responses across the native messaging
+// host pipe. Delegates processing of received messages to Delegate.
+//
+// TODO(alexeypa): Add ability to switch between different |delegate_| pointers
+// on the fly. This is useful for implementing UAC-style elevation on Windows -
+// an unprivileged delegate could be replaced with another delegate that
+// forwards messages to the elevated instance of the native messaging host.
+class NativeMessagingChannel : public base::NonThreadSafe {
+ public:
+  // Used to sends a response back to the client app.
+  typedef base::Callback<void(scoped_ptr<base::DictionaryValue> response)>
+      SendResponseCallback;
+
+  class Delegate {
+   public:
+    virtual ~Delegate() {}
+
+    // Processes a message received from the client app. Invokes |done| to send
+    // a response back to the client app.
+    virtual void ProcessMessage(scoped_ptr<base::DictionaryValue> message,
+                                const SendResponseCallback& done) = 0;
+  };
+
+  // Constructs an object taking the ownership of |input| and |output|. Closes
+  // |input| and |output| to prevent the caller frmo using them.
+  NativeMessagingChannel(
+      scoped_ptr<Delegate> delegate,
+      base::PlatformFile input,
+      base::PlatformFile output);
+  ~NativeMessagingChannel();
+
+  // Starts reading and processing messages.
+  void Start(const base::Closure& quit_closure);
+
+ private:
+  // Processes a message received from the client app.
+  void ProcessMessage(scoped_ptr<base::Value> message);
+
+  // Sends a response back to the client app.
+  void SendResponse(scoped_ptr<base::DictionaryValue> response);
+
+  // Initiates shutdown and runs |quit_closure| if there are no pending requests
+  // left.
+  void Shutdown();
+
+  base::Closure quit_closure_;
+
+  NativeMessagingReader native_messaging_reader_;
+  scoped_ptr<NativeMessagingWriter> native_messaging_writer_;
+
+  // |delegate_| may post tasks to this object during destruction (but not
+  // afterwards), so it needs to be destroyed before other members of this class
+  // (except for |weak_factory_|).
+  scoped_ptr<Delegate> delegate_;
+
+  // Keeps track of pending requests. Used to delay shutdown until all responses
+  // have been sent.
+  int pending_requests_;
+
+  // True if Shutdown() has been called.
+  bool shutdown_;
+
+  base::WeakPtr<NativeMessagingChannel> weak_ptr_;
+  base::WeakPtrFactory<NativeMessagingChannel> weak_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(NativeMessagingChannel);
+};
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_SETUP_NATIVE_MESSAGING_CHANNEL_H_
