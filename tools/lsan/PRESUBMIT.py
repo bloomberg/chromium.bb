f2b31bb25a4820d6b91fdeeb7237e06615a60c2b
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 14:49:25 +0000

    Add a PRESUBMIT script for LSan suppressions.
    Also make the comments in the suppressions file more noticeable.
    
    BUG=NONE
    R=timurrrr@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/26490002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227515 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 09da63fc001607f9a0c93d95f384390932bd4952

---
+++ tools/lsan/PRESUBMIT.py
@@ -0,0 +1,36 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details on the presubmit API built into gcl.
+"""
+
+import re
+
+def CheckChange(input_api, output_api):
+  errors = []
+
+  for f in input_api.AffectedFiles():
+    if not f.LocalPath().endswith('suppressions.txt'):
+      continue
+    for line_num, line in enumerate(f.NewContents()):
+      line = line.strip()
+      if line.startswith('#') or not line:
+        continue
+      if not line.startswith('leak:'):
+        errors.append('"%s" should be "leak:..." in %s line %d' %
+                      (line, f.LocalPath(), line_num))
+  if errors:
+    return [output_api.PresubmitError('\n'.join(errors))]
+  return []
+
+def CheckChangeOnUpload(input_api, output_api):
+  return CheckChange(input_api, output_api)
+
+def CheckChangeOnCommit(input_api, output_api):
+  return CheckChange(input_api, output_api)
+
+def GetPreferredTrySlaves():
+  return ['linux_asan']
