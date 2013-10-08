bloomberg/chromium.bb

commit b2733c0785a9bc8ae88573a71812d218725249cb

PNaCl: Run the GCC torture tests on the PNaCl toolchain trybots
I've been running these tests in all my PNaCl-LLVM toolchain trybot
runs already.  Adding these by default will mean I won't need a local
hack to add these tests to the try jobs, and it will help prevent
other people's changes from breaking these tests.  These tests are
quite useful; I find they often catch bugs.

This adds 13 minutes to the running time of
nacl-toolchain-linux-pnacl-x86_64 which took 88 minutes before.

BUG= https://code.google.com/p/nativeclient/issues/detail?id=3696
TEST= toolchain trybots

Review URL: https://codereview.chromium.org/25807003

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12210 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: 1581090de6b3191cb52796a1e45bff4587d43b49

buildbot/buildbot_pnacl_toolchain_tests.sh
@@ -394,6 +394,11 @@ tc-test-bot() {
   # run the torture tests. the "trybot" phases take care of prerequisites
   # for both test sets
   for arch in ${archset}; do
+    if [[ "${arch}" == "x86-32" ]]; then
+      # Torture tests on x86-32 are covered by tc-tests-all in
+      # buildbot_pnacl.sh.
+      continue
+    fi
     echo "@@@BUILD_STEP torture_tests $arch @@@"
     ${TORTURE_TEST} pnacl ${arch} --bot --verbose \
       --concurrency=${PNACL_CONCURRENCY} || handle-error

buildbot/buildbot_pnacl.sh
@@ -571,6 +571,16 @@ tc-tests-all() {
   scons-stage-irt "arm" \
     "${scons_flags} use_sandboxed_translator=1 translate_fast=1" \
     "run_hello_world_test"
+
+  # Run the GCC torture tests just for x86-32.  Testing a single
+  # architecture gives good coverage without taking too long.  We
+  # don't test x86-64 here because some of the torture tests fail on
+  # the x86-64 toolchain trybot (though not the buildbots, apparently
+  # due to a hardware difference:
+  # https://code.google.com/p/nativeclient/issues/detail?id=3697).
+  echo "@@@BUILD_STEP torture_tests x86-32 @@@"
+  tools/toolchain_tester/torture_test.py pnacl x86-32 --bot --verbose \
+      --concurrency=8 || handle-error
 }
 
 tc-tests-fast() {

commit 746ecfa0a3cac2084afcbd5d195afe9427bde1c4

Fix failure on PNaCl FYI bots introduced by r12210: run "SDK" install step
The "llvm-test-suite" step on the PNaCl x86-32 FYI bots was failing with:

ERROR: sdk dir does not seem to exist
ERROR: have you run 'pnacl/build.sh sdk newlib' ?

Fix this by running the "sdk" step explicitly.

BUG= https://code.google.com/p/nativeclient/issues/detail?id=3696
TEST= none

Review URL: https://codereview.chromium.org/26278004

git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12216 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2

Former-commit-id: e9e92c8cfb2e22a1268dee3fc980b5721ca0bb97

buildbot/buildbot_pnacl_toolchain_tests.sh
@@ -404,6 +404,12 @@ tc-test-bot() {
       --concurrency=${PNACL_CONCURRENCY} || handle-error
   done
 
+  # llvm-test-suite below requires the SDK libraries to be installed.
+  # torture_tests above do this as a side effect, but we do it
+  # explicitly here for cases in which torture_tests are not run.
+  echo "@@@BUILD_STEP install sdk libraries @@@"
+  ${PNACL_BUILD} sdk
+
   local optset
   optset[1]="--opt O3f --opt O2b"
   for arch in ${archset}; do