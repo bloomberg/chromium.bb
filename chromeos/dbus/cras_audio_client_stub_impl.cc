a00b5b87677868f5eac76e35f3222accefdeea18
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-08 18:40:21 +0000

    All cras nodes should be inintialized as non-active in the stub implementation. This fixes the cras warning log issue when running ash_unittests.
    BUG=298367
    
    Review URL: https://codereview.chromium.org/26358005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227553 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 91f59b60976dc4b4c4f6308ade9683af348438a7

--- chromeos/dbus/cras_audio_client_stub_impl.cc
+++ chromeos/dbus/cras_audio_client_stub_impl.cc
@@ -18,7 +18,6 @@ void CrasAudioClientStubImpl::Init(dbus::Bus* bus) {
   node_1.device_name = "Fake Speaker";
   node_1.type = "INTERNAL_SPEAKER";
   node_1.name = "Speaker";
-  node_1.active = false;
   node_list_.push_back(node_1);
 
   AudioNode node_2;
@@ -27,9 +26,7 @@ void CrasAudioClientStubImpl::Init(dbus::Bus* bus) {
   node_2.device_name = "Fake Headphone";
   node_2.type = "HEADPHONE";
   node_2.name = "Headphone";
-  node_2.active = false;
   node_list_.push_back(node_2);
-  active_output_node_id_ = node_2.id;
 
   AudioNode node_3;
   node_3.is_input = false;
@@ -37,7 +34,6 @@ void CrasAudioClientStubImpl::Init(dbus::Bus* bus) {
   node_3.device_name = "Fake Bluetooth Headphone";
   node_3.type = "BLUETOOTH";
   node_3.name = "Headphone";
-  node_3.active = false;
   node_list_.push_back(node_3);
 
   // Fake audio input ndoes
@@ -47,7 +43,6 @@ void CrasAudioClientStubImpl::Init(dbus::Bus* bus) {
   node_4.device_name = "Fake Internal Mic";
   node_4.type = "INTERNAL_MIC";
   node_4.name = "Internal Mic";
-  node_4.active = false;
   node_list_.push_back(node_4);
 
   AudioNode node_5;
@@ -56,10 +51,7 @@ void CrasAudioClientStubImpl::Init(dbus::Bus* bus) {
   node_5.device_name = "Fake USB Mic";
   node_5.type = "USB";
   node_5.name = "Mic";
-  node_5.active = true;
   node_list_.push_back(node_5);
-
-  active_input_node_id_ = node_5.id;
 }
 
 CrasAudioClientStubImpl::~CrasAudioClientStubImpl() {
