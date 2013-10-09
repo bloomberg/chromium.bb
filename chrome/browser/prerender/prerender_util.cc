c1a0048d984cb171c2860167fd2ae58d40b2c057
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-09 07:59:34 +0000

    Revert 227685 "Added the Prerender.ModPagespeedHeader.* histogra..."
    > Added the Prerender.ModPagespeedHeader.* histograms to histograms.xml. 
    > Extended the data extraction to also handle X-Page-Speed headers.
    > 
    > (dup of 20832003 which I can't commit with my personal email account)
    > 
    > BUG=267695
    > 
    > Review URL: https://codereview.chromium.org/26279004
    
    This test caused a leak on the memory bots. Details can be found here:
    http://build.chromium.org/p/chromium.memory.fyi/builders/Linux%20Tests%20%28valgrind%29%283%29/builds/30782/
    
    TBR=matterbury@google.com
    
    Review URL: https://codereview.chromium.org/26670002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227696 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 103336fe74406de219ea3c5f13f4d5d19a22592d

--- chrome/browser/prerender/prerender_util.cc
+++ chrome/browser/prerender/prerender_util.cc
@@ -5,7 +5,6 @@
 #include "chrome/browser/prerender/prerender_util.h"
 
 #include "base/logging.h"
-#include "base/metrics/histogram.h"
 #include "base/metrics/sparse_histogram.h"
 #include "base/strings/string_util.h"
 #include "content/public/browser/resource_request_info.h"
@@ -14,60 +13,8 @@
 #include "url/url_canon.h"
 #include "url/url_parse.h"
 #include "url/url_util.h"
-#include "webkit/common/resource_type.h"
 
 namespace prerender {
-
-namespace {
-
-const char kModPagespeedHeader[] = "X-Mod-Pagespeed";
-const char kPageSpeedHeader[] = "X-Page-Speed";
-const char kPagespeedServerHistogram[] =
-    "Prerender.PagespeedHeader.ServerCounts";
-const char kPagespeedVersionHistogram[] =
-    "Prerender.PagespeedHeader.VersionCounts";
-
-enum PagespeedHeaderServerType {
-  PAGESPEED_TOTAL_RESPONSES = 0,
-  PAGESPEED_MOD_PAGESPEED_SERVER = 1,
-  PAGESPEED_NGX_PAGESPEED_SERVER = 2,
-  PAGESPEED_PAGESPEED_SERVICE_SERVER = 3,
-  PAGESPEED_UNKNOWN_SERVER = 4,
-  PAGESPEED_SERVER_MAXIMUM = 5
-};
-
-// Private function to parse the PageSpeed version number and encode it in
-// buckets 2 through 99: if it is in the format a.b.c.d-e the bucket will be
-// 2 + 2 * (max(c, 10) - 10) + (d > 1 ? 1 : 0); if it is not in this format
-// we return zero.
-int GetXModPagespeedBucketFromVersion(const std::string& version) {
-  int a, b, c, d, e;
-  int num_parsed = sscanf(version.c_str(), "%d.%d.%d.%d-%d",
-                          &a, &b, &c, &d, &e);
-  int output = 0;
-  if (num_parsed == 5) {
-    output = 2;
-    if (c > 10)
-      output += 2 * (c - 10);
-    if (d > 1)
-      output++;
-    if (output < 2 || output > 99)
-      output = 0;
-  }
-  return output;
-}
-
-// Private function to parse the X-Page-Speed header value and determine
-// whether it is in the PageSpeed Service format, namely m_n_dc were m_n is
-// a version number and dc is an encoded 2-character value.
-bool IsPageSpeedServiceVersionNumber(const std::string& version) {
-  int a, b;
-  char c, d, e;  // e is to detect EOL as we check that it /isn't/ converted.
-  int num_parsed = sscanf(version.c_str(), "%d_%d_%c%c%c", &a, &b, &c, &d, &e);
-  return (num_parsed == 4);
-}
-
-}  // namespace
 
 bool MaybeGetQueryStringBasedAliasURL(
     const GURL& url, GURL* alias_url) {
@@ -142,80 +89,44 @@ bool IsControlGroupExperiment(uint8 experiment_id) {
   return experiment_id == 7 || experiment_id == 8;
 }
 
-void GatherPagespeedData(const ResourceType::Type resource_type,
-                         const GURL& request_url,
-                         const net::HttpResponseHeaders* response_headers) {
-  if (resource_type != ResourceType::MAIN_FRAME ||
-      !request_url.SchemeIsHTTPOrHTTPS())
-    return;
-
-  // bucket 0 counts every response seen.
-  UMA_HISTOGRAM_ENUMERATION(kPagespeedServerHistogram,
-                            PAGESPEED_TOTAL_RESPONSES,
-                            PAGESPEED_SERVER_MAXIMUM);
-  if (!response_headers)
-    return;
-
-  void* iter = NULL;
-  std::string name;
-  std::string value;
-  while (response_headers->EnumerateHeaderLines(&iter, &name, &value)) {
-    if (name == kModPagespeedHeader) {
-      // Bucket 1 counts occurences of the X-Mod-Pagespeed header.
-      UMA_HISTOGRAM_ENUMERATION(kPagespeedServerHistogram,
-                                PAGESPEED_MOD_PAGESPEED_SERVER,
-                                PAGESPEED_SERVER_MAXIMUM);
-      if (!value.empty()) {
-        // If the header value is in the X-Mod-Pagespeed version number format
-        // then increment the appropriate bucket, otherwise increment bucket 1,
-        // which is the catch-all "unknown version number" bucket.
-        int bucket = GetXModPagespeedBucketFromVersion(value);
-        if (bucket > 0) {
-          UMA_HISTOGRAM_SPARSE_SLOWLY(kPagespeedVersionHistogram, bucket);
-        } else {
-          UMA_HISTOGRAM_SPARSE_SLOWLY(kPagespeedVersionHistogram, 1);
-        }
-      }
-      break;
-    } else if (name == kPageSpeedHeader) {
-      // X-Page-Speed header versions are either in the X-Mod-Pagespeed format,
-      // indicating an nginx installation, or they're in the PageSpeed Service
-      // format, indicating a PSS installation, or in some other format,
-      // indicating an unknown installation [possibly IISpeed].
-      if (!value.empty()) {
-        int bucket = GetXModPagespeedBucketFromVersion(value);
-        if (bucket > 0) {
-          // Bucket 2 counts occurences of the X-Page-Speed header with a
-          // value in the X-Mod-Pagespeed version number format. We also
-          // count these responses in the version histogram.
-          UMA_HISTOGRAM_ENUMERATION(kPagespeedServerHistogram,
-                                    PAGESPEED_NGX_PAGESPEED_SERVER,
-                                    PAGESPEED_SERVER_MAXIMUM);
-          UMA_HISTOGRAM_SPARSE_SLOWLY(kPagespeedVersionHistogram, bucket);
-        } else if (IsPageSpeedServiceVersionNumber(value)) {
-          // Bucket 3 counts occurences of the X-Page-Speed header with a
-          // value in the PageSpeed Service version number format.
-          UMA_HISTOGRAM_ENUMERATION(kPagespeedServerHistogram,
-                                    PAGESPEED_PAGESPEED_SERVICE_SERVER,
-                                    PAGESPEED_SERVER_MAXIMUM);
-        } else {
-          // Bucket 4 counts occurences of all other values.
-          UMA_HISTOGRAM_ENUMERATION(kPagespeedServerHistogram,
-                                    PAGESPEED_UNKNOWN_SERVER,
-                                    PAGESPEED_SERVER_MAXIMUM);
-        }
-      }
-      break;
-    }
-  }
-}
-
 void URLRequestResponseStarted(net::URLRequest* request) {
+  static const char* kModPagespeedHeader = "X-Mod-Pagespeed";
+  static const char* kModPagespeedHistogram = "Prerender.ModPagespeedHeader";
   const content::ResourceRequestInfo* info =
       content::ResourceRequestInfo::ForRequest(request);
-  GatherPagespeedData(info->GetResourceType(),
-                      request->url(),
-                      request->response_headers());
+  // Gather histogram information about the X-Mod-Pagespeed header.
+  if (info->GetResourceType() == ResourceType::MAIN_FRAME &&
+      request->url().SchemeIsHTTPOrHTTPS()) {
+    UMA_HISTOGRAM_SPARSE_SLOWLY(kModPagespeedHistogram, 0);
+    if (request->response_headers() &&
+        request->response_headers()->HasHeader(kModPagespeedHeader)) {
+      UMA_HISTOGRAM_SPARSE_SLOWLY(kModPagespeedHistogram, 1);
+
+      // Attempt to parse the version number, and encode it in buckets
+      // 2 through 99. 0 and 1 are used to store all pageviews and
+      // # pageviews with the MPS header (see above).
+      void* iter = NULL;
+      std::string mps_version;
+      if (request->response_headers()->EnumerateHeader(
+              &iter, kModPagespeedHeader, &mps_version) &&
+          !mps_version.empty()) {
+        // Mod Pagespeed versions are of the form a.b.c.d-e
+        int a, b, c, d, e;
+        int num_parsed = sscanf(mps_version.c_str(), "%d.%d.%d.%d-%d",
+                                &a, &b, &c, &d, &e);
+        if (num_parsed == 5) {
+          int output = 2;
+          if (c > 10)
+            output += 2 * (c - 10);
+          if (d > 1)
+            output++;
+          if (output < 2 || output >= 99)
+            output = 99;
+          UMA_HISTOGRAM_SPARSE_SLOWLY(kModPagespeedHistogram, output);
+        }
+      }
+    }
+  }
 }
 
 }  // namespace prerender
