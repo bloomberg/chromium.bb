88dcb178a9f45bfa268681e971f745bc52087278
tedchoc@chromium.org <tedchoc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-10 20:53:54 +0000

    Remove the no longer needed + button field trial.
    Metrics showed that the plus button was not getting enough use to justify
    it's space in the already constrained UI.
    
    BUG=305010
    
    Review URL: https://codereview.chromium.org/26861002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@227987 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 032d86e2f7743aa87d1a2fd9801a06d35ecd1155

--- chrome/browser/chrome_browser_field_trials_mobile.cc
+++ chrome/browser/chrome_browser_field_trials_mobile.cc
@@ -70,53 +70,12 @@ void DataCompressionProxyFieldTrials() {
   }
 }
 
-void NewTabButtonInToolbarFieldTrial(const CommandLine& parsed_command_line) {
-  // Do not enable this field trials for tablet devices.
-  if (parsed_command_line.HasSwitch(switches::kTabletUI))
-    return;
-
-  const char kPhoneNewTabToolbarButtonFieldTrialName[] =
-      "PhoneNewTabToolbarButton";
-  const base::FieldTrial::Probability kPhoneNewTabToolbarButtonDivisor = 100;
-
-  // 50/100 = 50% for Non-Stable users.
-  //  0/100 = 0%  for Stable users.
-  const base::FieldTrial::Probability kPhoneNewTabToolbarButtonNonStable = 50;
-  const base::FieldTrial::Probability kPhoneNewTabToolbarButtonStable = 0;
-  const char kEnabled[] = "Enabled";
-  const char kDisabled[] = "Disabled";
-
-  // Find out if this is a stable channel.
-  const bool kIsStableChannel =
-      chrome::VersionInfo::GetChannel() == chrome::VersionInfo::CHANNEL_STABLE;
-
-  // Experiment enabled until Jan 1, 2015. By default, disabled.
-  scoped_refptr<base::FieldTrial> trial(
-      base::FieldTrialList::FactoryGetFieldTrial(
-          kPhoneNewTabToolbarButtonFieldTrialName,
-          kPhoneNewTabToolbarButtonDivisor,
-          kDisabled, 2015, 1, 1, base::FieldTrial::ONE_TIME_RANDOMIZED, NULL));
-
-  const int kEnabledGroup = trial->AppendGroup(
-      kEnabled,
-      kIsStableChannel ?
-          kPhoneNewTabToolbarButtonStable : kPhoneNewTabToolbarButtonNonStable);
-
-  const int v = trial->group();
-  VLOG(1) << "Phone NewTab toolbar button enabled group id: " << kEnabledGroup
-          << ". Selected group id: " << v;
-}
-
 }  // namespace
 
 void SetupMobileFieldTrials(const CommandLine& parsed_command_line,
                             const base::Time& install_time,
                             PrefService* local_state) {
   DataCompressionProxyFieldTrials();
-
-#if defined(OS_ANDROID)
-  NewTabButtonInToolbarFieldTrial(parsed_command_line);
-#endif
 }
 
 }  // namespace chrome
