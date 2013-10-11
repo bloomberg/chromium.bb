fc5dd866d1e3ec739cbca1cf417348f6e2358836
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-11 03:43:26 +0000

    Revert "Disable flaky PepperCDMsNotRegistered test"
    This reverts commit 886db3998b1a39af870be699af75719aa4c08cdd [formerly cf1559f00543675039488cd7c62c1eaada336b2e].
    
    The flakiness of these tests might already be fixed by r227399. Tentatively
    enabling these tests for now. If they are still falky, I'll disable these
    tests again.
    
    TBR=ddorwin@chromium.org
    BUG=301571
    
    Review URL: https://codereview.chromium.org/26277004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228099 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 52e5af4a8d76473ea0f1827d807cd933129e5ad3

--- chrome/browser/media/encrypted_media_istypesupported_browsertest.cc
+++ chrome/browser/media/encrypted_media_istypesupported_browsertest.cc
@@ -868,14 +868,8 @@ IN_PROC_BROWSER_TEST_F(EncryptedMediaIsTypeSupportedWidevineTest,
 // Since this test fixture does not register the CDMs on the command line, the
 // check for the CDMs in chrome_key_systems.cc should fail, and they should not
 // be registered with KeySystems.
-#if defined(OS_WIN)
-// Flaky on Windows: crbug.com/301571.
-#define MAYBE_PepperCDMsNotRegistered DISABLED_PepperCDMsNotRegistered
-#else
-#define MAYBE_PepperCDMsNotRegistered PepperCDMsNotRegistered
-#endif
 IN_PROC_BROWSER_TEST_F(EncryptedMediaIsTypeSupportedTest,
-                       MAYBE_PepperCDMsNotRegistered) {
+                       PepperCDMsNotRegistered) {
   EXPECT_FALSE(IsConcreteSupportedKeySystem(kExternalClearKey));
   EXPECT_FALSE(IsSupportedKeySystemWithMediaMimeType(
       "video/webm", no_codecs(), kExternalClearKey));
