bloomberg/chromium.bb

commit 690d7c6bfc8dbe873d6e31b3e8fc27bdb167086e

Remove last dependencies on ui/base from ui/gfx
BUG=103304
R=sky@chromium.org

Review URL: https://codereview.chromium.org/23498059

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@225722 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 4ffa789e031a7f0c8345e56a3c0813cb148176b4

ui/base/win/dpi_setup.cc
@@ -1,0 +1,26 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/base/win/dpi_setup.h"
+
+#include "ui/base/layout.h"
+#include "ui/gfx/display.h"
+#include "ui/gfx/win/dpi.h"
+
+namespace ui {
+namespace win {
+
+void InitDeviceScaleFactor() {
+  float scale = 1.0f;
+  if (gfx::IsHighDPIEnabled()) {
+    float scale = gfx::Display::HasForceDeviceScaleFactor() ?
+        gfx::Display::GetForcedDeviceScaleFactor() : gfx::GetDPIScale();
+    // Quantize to nearest supported scale factor.
+    scale = ui::GetImageScale(ui::GetSupportedScaleFactor(scale));
+  }
+  gfx::InitDeviceScaleFactor(scale);
+}
+
+}  // namespace win
+}  // namespace ui

commit 03fbd82bcc6f9bbd148f95ae4752705c595ce29f

Correct bug in InitDeviceScaleFactor - hidden variable.
Bug was introduced during refactor in r225722

BUG=306268

Review URL: https://codereview.chromium.org/26444010

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228107 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: ce2dfcc2209c7e6702614768ecb4c61df0abffc8

ui/base/win/dpi_setup.cc
@@ -14,7 +14,7 @@ namespace win {
 void InitDeviceScaleFactor() {
   float scale = 1.0f;
   if (gfx::IsHighDPIEnabled()) {
-    float scale = gfx::Display::HasForceDeviceScaleFactor() ?
+    scale = gfx::Display::HasForceDeviceScaleFactor() ?
         gfx::Display::GetForcedDeviceScaleFactor() : gfx::GetDPIScale();
     // Quantize to nearest supported scale factor.
     scale = ui::GetImageScale(ui::GetSupportedScaleFactor(scale));