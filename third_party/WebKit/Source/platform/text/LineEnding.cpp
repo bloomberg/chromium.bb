845eeaf7322778c03814f4703cd39ff9f8c32a80
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-11 06:40:05 +0000

    Don't crash by form submission in a document with null encoding.
    The CString object which is passed to normalizeLineEndingsToCRLF() can be
    a null string. It is created in FormDataList::appendString(), and it
    produces a null CString if FormDataList::m_encoding is a null encoding.
    
    BUG=291744,306086
    TEST=automated
    
    Review URL: https://codereview.chromium.org/26960002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159428 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 9a8fdf4738b6dc725c7d70def304e90a75f9ad18

--- third_party/WebKit/Source/platform/text/LineEnding.cpp
+++ third_party/WebKit/Source/platform/text/LineEnding.cpp
@@ -202,6 +202,8 @@ void normalizeToCROrLF(const CString& from, Vector<char>& result, bool toCR)
 
 CString normalizeLineEndingsToCRLF(const CString& from)
 {
+    if (!from.length())
+        return from;
     CString result;
     CStringBuffer buffer(result);
     internalNormalizeLineEndingsToCRLF(from, buffer);
