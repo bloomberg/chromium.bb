bloomberg/chromium.bb

commit 48fdb2d78757ce4e8b0003c0637888f9bbc27521

Remove usage of deprecated DOM API
Use defaultPrevented instead of returnValue

BUG=None

Review URL: https://codereview.chromium.org/26924002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228073 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 3970fddcbe03a4c9fe5368a54962352391d2af8c

ui/webui/resources/js/util.js
@@ -216,7 +216,7 @@ function getRequiredElement(id) {
 // call into the browser to do the navigation.
 document.addEventListener('click', function(e) {
   // Allow preventDefault to work.
-  if (!e.returnValue)
+  if (!e.defaultPrevented)
     return;
 
   var el = e.target;

chrome/browser/resources/ntp_android/ntp_android.js
@@ -1183,9 +1183,7 @@ cr.define('ntp', function() {
    * @param {Object} evt User interface event that triggered the action.
    */
   function executePromoAction(evt) {
-    if (evt.preventDefault)
-      evt.preventDefault();
-    evt.returnValue = false;
+    evt.preventDefault();
     chrome.send('promoActionTriggered');
   }
 

commit 045beaa5847707fa065f9fbab1d9c3c0a7925f16

WebUI: Fix issue with chrome: and file: links
This is a regression from r228073

BUG=None
TBR=dbeam@chromium.org

Review URL: https://codereview.chromium.org/26477007

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@228191 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: a4a9cc92a14b740b72eaee172af02650cea6f2d2

ui/webui/resources/js/util.js
@@ -215,8 +215,7 @@ function getRequiredElement(id) {
 // Handle click on a link. If the link points to a chrome: or file: url, then
 // call into the browser to do the navigation.
 document.addEventListener('click', function(e) {
-  // Allow preventDefault to work.
-  if (!e.defaultPrevented)
+  if (e.defaultPrevented)
     return;
 
   var el = e.target;