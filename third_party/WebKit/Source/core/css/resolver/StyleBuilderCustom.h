0e4514349441d8e63ed339f9b21f4d9fed69a762
shawnsingh@chromium.org <shawnsingh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-16 22:43:28 +0000

    Build fix after Blink rev 159806
    In the process of rev 159806, an accessor implementation moved to a
    different place that caused StyleBuilderCustom.h to have a link error.
    This patch addes the #include to fix the build error.
    
    TBR=esprehn@chromium.org, jchaffraix@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/26699004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159808 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d4ff6e68b7b651558c6adfc2a34ae3ad0533eca8

--- third_party/WebKit/Source/core/css/resolver/StyleBuilderCustom.h
+++ third_party/WebKit/Source/core/css/resolver/StyleBuilderCustom.h
@@ -30,6 +30,7 @@
 #include "core/css/CSSValue.h"
 #include "core/css/resolver/StyleResolverState.h"
 #include "core/platform/LengthSize.h"
+#include "core/rendering/RenderView.h"
 #include "core/svg/SVGLength.h"
 
 namespace WebCore {
