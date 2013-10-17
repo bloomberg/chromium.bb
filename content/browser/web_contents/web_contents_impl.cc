c181022b8ab9acccfbcd7871c0ab5b541c5eca15
dconnelly@chromium.org <dconnelly@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-17 13:11:21 +0000

    Revert 229040 "Fix plugin HWNDs being shown, and also in the wro..."
    This broke interactive_ui_tests on Mac.
    
    > Fix plugin HWNDs being shown, and also in the wrong position, when they're openeing in a background tab.
    > 
    > This is a regression from r218548.
    > 
    > BUG=302574
    > R=scottmg@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/26359005
    
    TBR=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/26743005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@229113 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 26329c0092e737f2b824452eaf2cd2c5775fedce

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1203,7 +1203,6 @@ void WebContentsImpl::Observe(int type,
 }
 
 void WebContentsImpl::Init(const WebContents::CreateParams& params) {
-  should_normally_be_visible_ = !params.initially_hidden;
   render_manager_.Init(
       params.browser_context, params.site_instance, params.routing_id,
       params.main_frame_routing_id);
