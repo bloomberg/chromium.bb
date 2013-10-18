58d4032873985e69cedd2afabb8fb597f75ea044
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-18 02:10:13 +0000

    Move the linux subpixel tests to a virtual test suite and re-enable
    BUG=243378
    R=dpranke@chromium.org
    
    Review URL: https://codereview.chromium.org/27711007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@159928 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0da36cda4b3e447d6ee849f6e12bcdc99de70bcd

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/linux.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/linux.py
@@ -124,6 +124,15 @@ class LinuxPort(base.Port):
     def operating_system(self):
         return 'linux'
 
+    def virtual_test_suites(self):
+        result = super(LinuxPort, self).virtual_test_suites()
+        result.extend([
+            base.VirtualTestSuite('linux-subpixel',
+                                  'platform/linux/fast/text/subpixel',
+                                  ['--enable-webkit-text-subpixel-positioning']),
+        ])
+        return result
+
     #
     # PROTECTED METHODS
     #
