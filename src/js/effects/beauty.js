645ba2c926c098c45be0fc3917fe0e383b43fd91
Tomasz Mikolajewski <mtomasz@chromium.org>
2013-10-21 12:05:52 +0900

    Add the Beauty effect.
    This patch adds the Beauty effect, which makes the face's skin smooth.
    Also, it adjusts the contrast and brightness for more warmer colors.
    
    TEST=Choose from the ribbon. Verify.
    BUG=chromium:309498
    
    Change-Id: I4fc3e8b6af3caa8124ae09cda2bb7ab7b99e275f

---
+++ src/js/effects/beauty.js
@@ -0,0 +1,75 @@
+// Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+/**
+ * Namespace for the Camera app.
+ */
+var camera = camera || {};
+
+/**
+ * Namespace for effects.
+ */
+camera.effects = camera.effects || {};
+
+/**
+ * @param {camera.Tracker} tracker Head tracker object.
+ * @constructor
+ * @extends {camera.Effect}
+ */
+camera.effects.Beauty = function(tracker) {
+  camera.Effect.call(this, tracker);
+
+  /**
+   * @type {number}
+   * @private
+   */
+  this.brightness_ = 0.25;
+
+  /**
+   * @type {number}
+   * @private
+   */
+  this.contrast_ = 0.25;
+
+  // End of properties. Seal the object.
+  Object.seal(this);
+};
+
+camera.effects.Beauty.prototype = {
+  __proto__: camera.Effect.prototype
+};
+
+/**
+ * @override
+ */
+camera.effects.Beauty.prototype.randomize = function() {
+  this.brightness_ = Math.random() * 0.4;
+  this.contrast_ = Math.random() * 0.6 - 0.2;
+};
+
+/**
+ * @override
+ */
+camera.effects.Beauty.prototype.filterFrame = function(canvas) {
+  var exponent = 50 * (1 - this.tracker_.face.width);
+  canvas.denoise(exponent);
+  canvas.brightnessContrast(this.brightness_, this.contrast_);
+};
+
+/**
+ * @override
+ */
+camera.effects.Beauty.prototype.getTitle = function() {
+  return chrome.i18n.getMessage('beautyEffect');
+};
+
+/**
+ * @override
+ */
+camera.effects.Beauty.prototype.isSlow = function() {
+  return true;
+};
+
