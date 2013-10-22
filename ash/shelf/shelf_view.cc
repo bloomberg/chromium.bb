d9e92d09d31e3ea49de85030b02c3d20e9696c0b
simon.hong81@gmail.com <simon.hong81@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-22 23:27:09 +0000

    [ash] Refactor ShelfView::LayoutToIdealBounds()
    Remove redundant call of CalculateIdealBounds().
    AnimateToIdealBounds() also calls CalculateIdealBounds() and set overflow button rect.
    
    R=jamescook@chromium.org
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/34373002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230253 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 95c41072f6c6ba4749d9d62cc79bc9fa0a13e59f

--- ash/shelf/shelf_view.cc
+++ ash/shelf/shelf_view.cc
@@ -697,14 +697,14 @@ void ShelfView::EndDrag(bool cancel) {
 }
 
 void ShelfView::LayoutToIdealBounds() {
+  if (bounds_animator_->IsAnimating()) {
+    AnimateToIdealBounds();
+    return;
+  }
+
   IdealBounds ideal_bounds;
   CalculateIdealBounds(&ideal_bounds);
-
-  if (bounds_animator_->IsAnimating())
-    AnimateToIdealBounds();
-  else
-    views::ViewModelUtils::SetViewBoundsToIdealBounds(*view_model_);
-
+  views::ViewModelUtils::SetViewBoundsToIdealBounds(*view_model_);
   overflow_button_->SetBoundsRect(ideal_bounds.overflow_bounds);
 }
 
