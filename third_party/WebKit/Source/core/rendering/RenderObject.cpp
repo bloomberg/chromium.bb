65a31dc91768f5dca30a238c727fa7079ae924a8
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-22 19:43:07 +0000

    Remove bogus ASSERT(frame) from RenderObject::willBeDestroyed
    This ASSERT was added way back in http://trac.webkit.org/changeset/48070 but
    is false in practice (see attached bug). Given that we already handle frame-nullity
    in this method, I don't see a reason to carry this legacy ASSERT around with us.
    
    R=eae@chromium.org
    BUG=308786
    
    Review URL: https://codereview.chromium.org/35153004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160247 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ea7e5d16569972a643d6a1de89abe88ef2bb6a6b

--- third_party/WebKit/Source/core/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/core/rendering/RenderObject.cpp
@@ -2558,11 +2558,6 @@ void RenderObject::willBeDestroyed()
         children->destroyLeftoverChildren();
 
     // If this renderer is being autoscrolled, stop the autoscroll timer
-
-    // FIXME: RenderObject::destroy should not get called with a renderer whose document
-    // has a null frame, so we assert this. However, we don't want release builds to crash which is why we
-    // check that the frame is not null.
-    ASSERT(frame());
     if (Frame* frame = this->frame()) {
         if (frame->page())
             frame->page()->stopAutoscrollIfNeeded(this);
