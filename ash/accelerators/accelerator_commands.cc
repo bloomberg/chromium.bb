b009fdf95cd25218abcfefd69db6999cca3dba33
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-23 14:40:35 +0000

    Enable minmizing fullscreen, except for flash fullscreen
    shortcut for ash is reassigned to ctrl-m, and fullscreen flash always consumes ctrl-m and never send it back to us as unhandled, so this is no longer necessary.
    
    BUG=131709, 286515
    
    Review URL: https://codereview.chromium.org/35983002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230423 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dd2aef72a6f43c69a826bef7ca33b72040c3e115

--- ash/accelerators/accelerator_commands.cc
+++ ash/accelerators/accelerator_commands.cc
@@ -23,10 +23,7 @@ bool ToggleMinimized() {
     return true;
   }
   wm::WindowState* window_state = wm::GetWindowState(window);
-  // Disable the shortcut for minimizing full screen window due to
-  // crbug.com/131709, which is a crashing issue related to minimizing
-  // full screen pepper window.
-  if (window_state->IsFullscreen() || !window_state->CanMinimize())
+  if (!window_state->CanMinimize())
     return false;
   ash::Shell::GetInstance()->delegate()->RecordUserMetricsAction(
       ash::UMA_MINIMIZE_PER_KEY);
