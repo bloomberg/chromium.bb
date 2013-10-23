10b59df27ea84fb2181ea1be852695e762e0d961
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-23 03:53:28 +0000

    Constrains keyboard context menu origin to the visible bounds of view
    Without this we may try to show context menu outside the visible
    bounds, which looks odd.
    
    BUG=308886
    TEST=see bug
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/35153008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230309 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fcde6d3193e93855c5457462a8aaee3da61e681a

--- ui/views/widget/root_view.cc
+++ ui/views/widget/root_view.cc
@@ -120,8 +120,16 @@ void RootView::DispatchKeyEvent(ui::KeyEvent* event) {
   // keyboard.
   if (v && v->enabled() && ((event->key_code() == ui::VKEY_APPS) ||
      (event->key_code() == ui::VKEY_F10 && event->IsShiftDown()))) {
-    v->ShowContextMenu(v->GetKeyboardContextMenuLocation(),
-                       ui::MENU_SOURCE_KEYBOARD);
+    // Showing the context menu outside the visible bounds may result in a
+    // context menu appearing over a completely different window. Constrain
+    // location to visible bounds so this doesn't happen.
+    gfx::Rect visible_bounds(v->ConvertRectToWidget(v->GetVisibleBounds()));
+    visible_bounds.Offset(
+        widget_->GetClientAreaBoundsInScreen().OffsetFromOrigin());
+    gfx::Rect keyboard_loc(v->GetKeyboardContextMenuLocation(),
+                           gfx::Size(1, 1));
+    keyboard_loc.AdjustToFit(visible_bounds);
+    v->ShowContextMenu(keyboard_loc.origin(), ui::MENU_SOURCE_KEYBOARD);
     event->StopPropagation();
     return;
   }
