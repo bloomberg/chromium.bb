c3bf034ef5af5938cfae8260c601a4733f66e64d
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-23 04:18:27 +0000

    Get system salt asynchronously from DeviceId::GetMachineId
    Use GetSystemSalt instead of GetSystemSaltSync
    
    BUG=141009
    R=mpcomplete@chromium.org
    
    Review URL: https://codereview.chromium.org/30693005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230318 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 27fb3f8b6100a5aa734cc0abba3c7690b655c32a

--- chrome/browser/extensions/api/music_manager_private/device_id_chromeos.cc
+++ chrome/browser/extensions/api/music_manager_private/device_id_chromeos.cc
@@ -4,7 +4,6 @@
 
 #include "chrome/browser/extensions/api/music_manager_private/device_id.h"
 
-#include "base/message_loop/message_loop.h"
 #include "chromeos/cryptohome/system_salt_getter.h"
 
 namespace extensions {
@@ -13,18 +12,7 @@ namespace api {
 // ChromeOS: Use the System Salt.
 /* static */
 void DeviceId::GetMachineId(const IdCallback& callback) {
-  chromeos::SystemSaltGetter* c_home = chromeos::SystemSaltGetter::Get();
-  std::string result = c_home->GetSystemSaltSync();
-  if (result.empty()) {
-    // cryptohome must not be running; re-request after a delay.
-    const int64 kRequestSystemSaltDelayMs = 500;
-    base::MessageLoop::current()->PostDelayedTask(
-        FROM_HERE,
-        base::Bind(&DeviceId::GetMachineId, callback),
-        base::TimeDelta::FromMilliseconds(kRequestSystemSaltDelayMs));
-    return;
-  }
-  callback.Run(result);
+  chromeos::SystemSaltGetter::Get()->GetSystemSalt(callback);
 }
 
 }  // namespace api
