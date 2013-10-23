a6eb2815ea8811bbe59f9e880dd7c8e605f8907e
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-23 10:47:38 +0000

    Add UseCounters for Element.webkitRequestFullscreen/webkitRequestFullScreen
    Spawned from "Is there a plan for shipping unprefixed fullscreen API?"
    
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/iElkfxDU7d4/gV0HS8guXscJ
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/36573003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160329 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: bc68962a01f1c0a9832813285513edea25f30200

--- third_party/WebKit/Source/core/page/UseCounter.h
+++ third_party/WebKit/Source/core/page/UseCounter.h
@@ -235,6 +235,8 @@ public:
         PrefixedVideoDroppedFrameCount,
         SourceElementCandidate,
         SourceElementNonMatchingMedia,
+        PrefixedElementRequestFullscreen,
+        PrefixedElementRequestFullScreen,
         // Add new features immediately above this line. Don't change assigned
         // numbers of each items, and don't reuse unused slots.
         NumberOfFeatures, // This enum value must be last.
