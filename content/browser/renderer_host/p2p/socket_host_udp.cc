4daac765d27186237a1f18d7cdee1c060f198571
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-23 05:56:01 +0000

    Initialize variable to avoid false-positive uninit error from valgrind.
    This is the same as https://codereview.chromium.org/34383003/ but in another file.
    
    TBR=hclam@chromium.org
    BUG=308683
    
    Review URL: https://codereview.chromium.org/34623010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230341 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ac6edf45cf703a4d1fee814c5b0c31fced03ab2b

--- content/browser/renderer_host/p2p/socket_host_udp.cc
+++ content/browser/renderer_host/p2p/socket_host_udp.cc
@@ -175,7 +175,7 @@ void P2PSocketHostUdp::Send(const net::IPEndPoint& to,
   }
 
   if (!ContainsKey(connected_peers_, to)) {
-    P2PSocketHost::StunMessageType type;
+    P2PSocketHost::StunMessageType type = P2PSocketHost::StunMessageType();
     bool stun = GetStunPacketType(&*data.begin(), data.size(), &type);
     if (!stun || type == STUN_DATA_INDICATION) {
       LOG(ERROR) << "Page tried to send a data packet to " << to.ToString()
