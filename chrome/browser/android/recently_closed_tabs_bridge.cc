9d38cdb260e5e49e506ce930b50df972f5542d29
newt@chromium.org <newt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-24 07:48:30 +0000

    Add Java bridge for getting recently closed tabs.
    This enables Java classes to query the list of recently closed tabs and
    to clear that list.
    
    BUG=308820
    
    Review URL: https://codereview.chromium.org/36293002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230674 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 28af8345d84ea250dcd943240c4d77cfcca619f7

---
+++ chrome/browser/android/recently_closed_tabs_bridge.cc
@@ -0,0 +1,165 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/recently_closed_tabs_bridge.h"
+
+#include "base/android/jni_string.h"
+#include "chrome/browser/android/tab_android.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_android.h"
+#include "chrome/browser/sessions/session_restore.h"
+#include "chrome/browser/sessions/tab_restore_service.h"
+#include "chrome/browser/sessions/tab_restore_service_factory.h"
+#include "content/public/browser/web_contents.h"
+#include "jni/RecentlyClosedBridge_jni.h"
+
+using base::android::AttachCurrentThread;
+using base::android::ConvertUTF16ToJavaString;
+using base::android::ConvertUTF8ToJavaString;
+using base::android::ScopedJavaLocalRef;
+
+namespace {
+
+const int kMaxTabCount = 10;
+
+void AddTabToList(JNIEnv* env,
+                  TabRestoreService::Entry* entry,
+                  jobject jtabs_list) {
+  const TabRestoreService::Tab* tab =
+      static_cast<TabRestoreService::Tab*>(entry);
+  const sessions::SerializedNavigationEntry& current_navigation =
+      tab->navigations.at(tab->current_navigation_index);
+  Java_RecentlyClosedBridge_pushTab(
+      env, jtabs_list, entry->id,
+      ConvertUTF16ToJavaString(env, current_navigation.title()).Release(),
+      ConvertUTF8ToJavaString(env, current_navigation.virtual_url().spec())
+          .Release());
+}
+
+void AddTabsToList(JNIEnv* env,
+                   const TabRestoreService::Entries& entries,
+                   jobject jtabs_list) {
+  int added_count = 0;
+  for (TabRestoreService::Entries::const_iterator it = entries.begin();
+       it != entries.end() && added_count < kMaxTabCount; ++it) {
+    TabRestoreService::Entry* entry = *it;
+    DCHECK_EQ(entry->type, TabRestoreService::TAB);
+    if (entry->type == TabRestoreService::TAB) {
+      AddTabToList(env, entry, jtabs_list);
+      ++added_count;
+    }
+  }
+}
+
+}  // namespace
+
+RecentlyClosedTabsBridge::RecentlyClosedTabsBridge(Profile* profile)
+    : profile_(profile),
+      tab_restore_service_(NULL) {
+}
+
+RecentlyClosedTabsBridge::~RecentlyClosedTabsBridge() {
+  if (tab_restore_service_)
+    tab_restore_service_->RemoveObserver(this);
+}
+
+void RecentlyClosedTabsBridge::Destroy(JNIEnv* env, jobject obj) {
+  delete this;
+}
+
+void RecentlyClosedTabsBridge::SetRecentlyClosedCallback(JNIEnv* env,
+                                                         jobject obj,
+                                                         jobject jcallback) {
+  callback_.Reset(env, jcallback);
+}
+
+jboolean RecentlyClosedTabsBridge::GetRecentlyClosedTabs(JNIEnv* env,
+                                                         jobject obj,
+                                                         jobject jtabs_list) {
+  EnsureTabRestoreService();
+  if (!tab_restore_service_)
+    return false;
+
+  AddTabsToList(env, tab_restore_service_->entries(), jtabs_list);
+  return true;
+}
+
+jboolean RecentlyClosedTabsBridge::OpenRecentlyClosedTab(JNIEnv* env,
+                                                         jobject obj,
+                                                         jobject jtab,
+                                                         jint recent_tab_id) {
+  if (!tab_restore_service_)
+    return false;
+
+  // Find and remove the corresponding tab entry from TabRestoreService.
+  // We take ownership of the returned tab.
+  scoped_ptr<TabRestoreService::Tab> tab_entry(
+      tab_restore_service_->RemoveTabEntryById(recent_tab_id));
+  if (!tab_entry)
+    return false;
+
+  TabAndroid* tab_android = TabAndroid::GetNativeTab(env, jtab);
+  if (!tab_android)
+    return false;
+  content::WebContents* web_contents = tab_android->web_contents();
+  if (!web_contents)
+    return false;
+
+  // RestoreForeignSessionTab needs a SessionTab.
+  SessionTab session_tab;
+  session_tab.current_navigation_index = tab_entry->current_navigation_index;
+  session_tab.navigations = tab_entry->navigations;
+
+  SessionRestore::RestoreForeignSessionTab(web_contents,
+                                           session_tab,
+                                           NEW_FOREGROUND_TAB);
+  return true;
+}
+
+void RecentlyClosedTabsBridge::ClearRecentlyClosedTabs(JNIEnv* env,
+                                                       jobject obj) {
+  EnsureTabRestoreService();
+  if (tab_restore_service_)
+    tab_restore_service_->ClearEntries();
+}
+
+void RecentlyClosedTabsBridge::TabRestoreServiceChanged(
+    TabRestoreService* service) {
+  if (callback_.is_null())
+    return;
+  JNIEnv* env = AttachCurrentThread();
+  Java_RecentlyClosedCallback_onUpdated(env, callback_.obj());
+}
+
+void RecentlyClosedTabsBridge::TabRestoreServiceDestroyed(
+    TabRestoreService* service) {
+  tab_restore_service_ = NULL;
+}
+
+void RecentlyClosedTabsBridge::EnsureTabRestoreService() {
+  if (tab_restore_service_)
+    return;
+
+  tab_restore_service_ = TabRestoreServiceFactory::GetForProfile(profile_);
+
+  // TabRestoreServiceFactory::GetForProfile() can return NULL (e.g. in
+  // incognito mode).
+  if (tab_restore_service_) {
+    // This does nothing if the tabs have already been loaded or they
+    // shouldn't be loaded.
+    tab_restore_service_->LoadTabsFromLastSession();
+    tab_restore_service_->AddObserver(this);
+  }
+}
+
+static jint Init(JNIEnv* env, jobject obj, jobject jprofile) {
+  RecentlyClosedTabsBridge* bridge = new RecentlyClosedTabsBridge(
+      ProfileAndroid::FromProfileAndroid(jprofile));
+  return reinterpret_cast<jint>(bridge);
+}
+
+// static
+bool RecentlyClosedTabsBridge::Register(JNIEnv* env) {
+  return RegisterNativesImpl(env);
+}
---
+++ chrome/browser/android/recently_closed_tabs_bridge.h
@@ -0,0 +1,58 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_ANDROID_RECENTLY_CLOSED_TABS_BRIDGE_H_
+#define CHROME_BROWSER_ANDROID_RECENTLY_CLOSED_TABS_BRIDGE_H_
+
+#include <jni.h>
+
+#include "base/android/scoped_java_ref.h"
+#include "base/compiler_specific.h"
+#include "chrome/browser/sessions/tab_restore_service_observer.h"
+
+class Profile;
+class TabRestoreService;
+
+// Provides the list of recently closed tabs to Java.
+class RecentlyClosedTabsBridge : public TabRestoreServiceObserver {
+ public:
+  explicit RecentlyClosedTabsBridge(Profile* profile);
+
+  void Destroy(JNIEnv* env, jobject obj);
+  void SetRecentlyClosedCallback(JNIEnv* env, jobject obj, jobject jcallback);
+  jboolean GetRecentlyClosedTabs(JNIEnv* env, jobject obj, jobject jtabs);
+  jboolean OpenRecentlyClosedTab(JNIEnv* env, jobject obj, jobject jtab,
+                                 jint tab_id);
+  void ClearRecentlyClosedTabs(JNIEnv* env, jobject obj);
+
+  // Observer callback for TabRestoreServiceObserver. Notifies the registered
+  // callback that the recently closed tabs list has changed.
+  virtual void TabRestoreServiceChanged(TabRestoreService* service) OVERRIDE;
+
+  // Observer callback when our associated TabRestoreService is destroyed.
+  virtual void TabRestoreServiceDestroyed(TabRestoreService* service) OVERRIDE;
+
+  // Registers JNI methods.
+  static bool Register(JNIEnv* env);
+
+ private:
+  virtual ~RecentlyClosedTabsBridge();
+
+  // Construct and initialize tab_restore_service_ if it's NULL.
+  // tab_restore_service_ may still be NULL, however, in incognito mode.
+  void EnsureTabRestoreService();
+
+  // The callback to be notified when the list of recently closed tabs changes.
+  base::android::ScopedJavaGlobalRef<jobject> callback_;
+
+  // The profile whose recently closed tabs are being monitored.
+  Profile* profile_;
+
+  // TabRestoreService that we are observing.
+  TabRestoreService* tab_restore_service_;
+
+  DISALLOW_COPY_AND_ASSIGN(RecentlyClosedTabsBridge);
+};
+
+#endif  // CHROME_BROWSER_ANDROID_RECENTLY_CLOSED_TABS_BRIDGE_H_
