229130e54eeca5c0e53e47399c153f8a343c4b81
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-24 04:32:01 +0000

    Reable quickoffice and some other component apps for guest session
    This is to fix a regression from https://codereview.chromium.org/27564004/
    
    
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/33113006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230634 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6782a60e971b721fe4a17e902e240b48d4543d80

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -445,11 +445,7 @@ void ComponentLoader::AddDefaultComponentExtensionsWithBackgroundPages(
     Add(IDR_FEEDBACK_MANIFEST, base::FilePath(FILE_PATH_LITERAL("feedback")));
 
 #if defined(OS_CHROMEOS)
-  if (!skip_session_components &&
-      !command_line->HasSwitch(chromeos::switches::kGuestSession)) {
-    Add(IDR_WALLPAPERMANAGER_MANIFEST,
-        base::FilePath(FILE_PATH_LITERAL("chromeos/wallpaper_manager")));
-
+  if (!skip_session_components) {
 #if defined(GOOGLE_CHROME_BUILD)
     if (!command_line->HasSwitch(
             chromeos::switches::kDisableQuickofficeComponentApp)) {
@@ -481,6 +477,11 @@ void ComponentLoader::AddDefaultComponentExtensionsWithBackgroundPages(
           chromeos::switches::kEchoExtensionPath);
     }
     Add(IDR_ECHO_MANIFEST, echo_extension_path);
+
+    if (!command_line->HasSwitch(chromeos::switches::kGuestSession)) {
+      Add(IDR_WALLPAPERMANAGER_MANIFEST,
+          base::FilePath(FILE_PATH_LITERAL("chromeos/wallpaper_manager")));
+    }
 
     Add(IDR_NETWORK_CONFIGURATION_MANIFEST,
         base::FilePath(FILE_PATH_LITERAL("chromeos/network_configuration")));
