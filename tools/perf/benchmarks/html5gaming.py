13c4b0ed27d41823f6598f8fe4c46655b39ace44
prasadv@chromium.org <prasadv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-25 20:27:29 +0000

    [Telemetry]: Add Impact HTML5 Gaming benchmark suite.
    TEST= tools/perf/run_benchmark --browser=system html5gaming
    
    Review URL: https://codereview.chromium.org/32753005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231103 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c828ee5467d8c4b10db8c98291d3203d9ef6b452

---
+++ tools/perf/benchmarks/html5gaming.py
@@ -0,0 +1,48 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Impact HTML5 Gaming benchmark.
+
+Tests one very specific use case: smooth running games rendered with the
+<canvas> element. The score for the HTML5-Benchmark takes the total time the
+browser spent rendering frames (formula is 1000000/(sqrt(totalTime) + lagTime *
+0.1)). The benchmark automatically runs at a reasonable screen size. Final
+score is a indicator for the browser's ability to smoothly run HTML5 games."""
+
+import os
+
+from telemetry import test
+from telemetry.core import util
+from telemetry.page import page_measurement
+from telemetry.page import page_set
+
+
+class HTML5GamingMeasurement(page_measurement.PageMeasurement):
+  def MeasurePage(self, _, tab, results):
+    tab.ExecuteJavaScript('benchmark();')
+    # Default value of score element is 87485, its value is updated with actual
+    # score when test finish.
+    js_is_done = ('document.getElementById("score").innerHTML != "87485"')
+
+    def _IsDone():
+      return tab.EvaluateJavaScript(js_is_done)
+    util.WaitFor(_IsDone, 200, poll_interval=5)
+    result = int(tab.EvaluateJavaScript(
+        'document.getElementById("score").innerHTML'))
+    results.Add('Score', 'score', result)
+
+
+class HTML5Gaming(test.Test):
+  """Imapct HTML5 smooth running games benchmark suite."""
+  test = HTML5GamingMeasurement
+  def CreatePageSet(self, options):
+    return page_set.PageSet.FromDict({
+        'archive_data_file': '../page_sets/data/html5gaming.json',
+        'make_javascript_deterministic': False,
+        'pages': [
+          { 'url':
+              'http://html5-benchmark.com/'}
+           ]
+        }, os.path.abspath(__file__))
+
