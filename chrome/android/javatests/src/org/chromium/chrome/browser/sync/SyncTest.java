01fc830745c8ec3275f32b7b7826913977338e67
dfalcantara@chromium.org <dfalcantara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-25 01:07:33 +0000

    Really mark SyncTest as disabled
    Reverts https://codereview.chromium.org/39933002/ and disables the
    test properly.
    
    NOTRY=true
    BUG=311091
    R=nyquist@chromium.org
    
    Review URL: https://codereview.chromium.org/42103005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230892 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9c2d7580dd368a1596a1a0c0832fbd33816cfcfd

--- chrome/android/javatests/src/org/chromium/chrome/browser/sync/SyncTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/sync/SyncTest.java
@@ -6,7 +6,6 @@ package org.chromium.chrome.browser.sync;
 
 import android.accounts.Account;
 import android.app.Activity;
-import android.test.FlakyTest;
 import android.util.Log;
 
 import org.chromium.base.ThreadUtils;
@@ -139,8 +138,6 @@ public class SyncTest extends ChromiumTestShellTestBase {
     }
 
     @HostDrivenTest
-    /* http://crbug.com/311091 */
-    @FlakyTest
     public void testDisableAndEnableSync() throws InterruptedException {
         setupTestAccountAndSignInToSync(FOREIGN_SESSION_TEST_MACHINE_ID);
         Account account =
