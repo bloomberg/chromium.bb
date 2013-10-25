6aefda1261ac190c27638d34d3282b7e225dedf0
rpaquay@chromium.org <rpaquay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-25 19:05:11 +0000

    Look for disk uuid instead of dbus machine-id file.
    BUG=311310
    
    Review URL: https://codereview.chromium.org/41973002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231072 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 33593ee4ca19272f8729c43e883b25048f5f06ab

--- chrome/browser/extensions/api/music_manager_private/device_id_linux.cc
+++ chrome/browser/extensions/api/music_manager_private/device_id_linux.cc
@@ -4,22 +4,67 @@
 
 #include "chrome/browser/extensions/api/music_manager_private/device_id.h"
 
+#include <map>
+
 #include "base/bind.h"
 #include "base/file_util.h"
+#include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "content/public/browser/browser_thread.h"
 
 namespace {
 
-const char kDBusFilename[] = "/var/lib/dbus/machine-id";
+const char kDiskByUuidDirectoryName[] = "/dev/disk/by-uuid";
+const char* kDeviceNames[] = { "sda1", "hda1", "dm-0" };
 
-void GetDBusMachineId(const extensions::api::DeviceId::IdCallback& callback) {
+// Map from device name to disk uuid
+typedef std::map<base::FilePath, base::FilePath> DiskEntries;
+
+void GetDiskUuid(const extensions::api::DeviceId::IdCallback& callback) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::FILE));
 
+  DiskEntries disk_uuids;
+
+  base::FileEnumerator files(base::FilePath(kDiskByUuidDirectoryName),
+                             false,  // Recursive.
+                             base::FileEnumerator::FILES);
+  do {
+    base::FilePath file_path = files.Next();
+    if (file_path.empty())
+      break;
+
+    base::FilePath target_path;
+    if (!file_util::ReadSymbolicLink(file_path, &target_path))
+      continue;
+
+    base::FilePath device_name = target_path.BaseName();
+    base::FilePath disk_uuid = file_path.BaseName();
+    disk_uuids[device_name] = disk_uuid;
+  } while (true);
+
+  // Look for first device name matching an entry of |kDeviceNames|.
   std::string result;
-  if (!base::ReadFileToString(base::FilePath(kDBusFilename), &result)) {
-    DLOG(WARNING) << "Error reading dbus machine id file.";
-    result = "";
+  for (size_t i = 0; i < arraysize(kDeviceNames); i++) {
+    DiskEntries::iterator it =
+        disk_uuids.find(base::FilePath(kDeviceNames[i]));
+    if (it != disk_uuids.end()) {
+      DVLOG(1) << "Returning uuid: \"" << it->second.value()
+               << "\" for device \"" << it->first.value() << "\"";
+      result = it->second.value();
+      break;
+    }
+  }
+
+  // Log failure (at most once) for diagnostic purposes.
+  static bool error_logged = false;
+  if (result.empty() && !error_logged) {
+    error_logged = true;
+    LOG(ERROR) << "Could not find appropriate disk uuid.";
+    for (DiskEntries::iterator it = disk_uuids.begin();
+        it != disk_uuids.end(); ++it) {
+      LOG(ERROR) << "  DeviceID=" << it->first.value() << ", uuid="
+                 << it->second.value();
+    }
   }
 
   content::BrowserThread::PostTask(
@@ -28,12 +73,12 @@ void GetDBusMachineId(const extensions::api::DeviceId::IdCallback& callback) {
       base::Bind(callback, result));
 }
 
-}
+}  // namespace
 
 namespace extensions {
 namespace api {
 
-// Linux: Use the content of the "DBus" machine-id file.
+// Linux: Look for disk uuid
 /* static */
 void DeviceId::GetMachineId(const IdCallback& callback) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
@@ -41,7 +86,7 @@ void DeviceId::GetMachineId(const IdCallback& callback) {
   content::BrowserThread::PostTask(
       content::BrowserThread::FILE,
       FROM_HERE,
-      base::Bind(GetDBusMachineId, callback));
+      base::Bind(GetDiskUuid, callback));
 }
 
 }  // namespace api
