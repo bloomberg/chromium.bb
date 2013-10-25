12f715d591c9f7d12f9c5b2fb83378fe7b1eab74
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-25 08:12:30 +0000

    Use CastEnvironment and CastSender in CastSessionDelegate
    Implementing more of CastSessionDelegate by using CastEnvironment
    and CastSender. CastEnvironment is now properly configured.
    
    CastSender is now constructed but not configured correctly. This will
    be done in upcoming changes.
    
    BUG=301920
    
    Review URL: https://codereview.chromium.org/34743002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@230984 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bd42ac38a66cab5526123b18d694660b399445ce

--- chrome/renderer/media/cast_session_delegate.cc
+++ chrome/renderer/media/cast_session_delegate.cc
@@ -7,12 +7,54 @@
 #include "base/logging.h"
 #include "base/message_loop/message_loop_proxy.h"
 #include "content/public/renderer/render_thread.h"
+#include "media/cast/cast_config.h"
+#include "media/cast/cast_environment.h"
+#include "media/cast/cast_sender.h"
+
+using media::cast::AudioSenderConfig;
+using media::cast::CastEnvironment;
+using media::cast::CastSender;
+using media::cast::VideoSenderConfig;
 
 CastSessionDelegate::CastSessionDelegate()
-    : io_message_loop_proxy_(
-      content::RenderThread::Get()->GetIOMessageLoopProxy()) {
+    : audio_encode_thread_("CastAudioEncodeThread"),
+      video_encode_thread_("CastVideoEncodeThread"),
+      io_message_loop_proxy_(
+          content::RenderThread::Get()->GetIOMessageLoopProxy()) {
 }
 
 CastSessionDelegate::~CastSessionDelegate() {
   DCHECK(io_message_loop_proxy_->BelongsToCurrentThread());
+}
+
+void CastSessionDelegate::PrepareForSending() {
+  DCHECK(base::MessageLoopProxy::current() == io_message_loop_proxy_);
+
+  if (cast_environment_)
+    return;
+
+  audio_encode_thread_.Start();
+  video_encode_thread_.Start();
+
+  // CastSender uses the renderer's IO thread as the main thread. This reduces
+  // thread hopping for incoming video frames and outgoing network packets.
+  // There's no need to decode so no thread assigned for decoding.
+  cast_environment_ = new CastEnvironment(
+      &clock_,
+      base::MessageLoopProxy::current(),
+      audio_encode_thread_.message_loop_proxy(),
+      NULL,
+      video_encode_thread_.message_loop_proxy(),
+      NULL);
+
+  // TODO(hclam): A couple things need to be done here:
+  // 1. Pass audio and video configuration to CastSender.
+  // 2. Connect media::cast::PacketSender to net::Socket interface.
+  // 3. Implement VideoEncoderController to configure hardware encoder.
+  cast_sender_.reset(CastSender::CreateCastSender(
+      cast_environment_,
+      AudioSenderConfig(),
+      VideoSenderConfig(),
+      NULL,
+      NULL));
 }
--- chrome/renderer/media/cast_session_delegate.h
+++ chrome/renderer/media/cast_session_delegate.h
@@ -7,22 +7,47 @@
 
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
+#include "base/threading/thread.h"
 #include "base/threading/thread_checker.h"
+#include "base/time/default_tick_clock.h"
 
 namespace base {
 class MessageLoopProxy;
 }  // namespace base
 
+namespace media {
+namespace cast {
+class CastEnvironment;
+class CastSender;
+}  // namespace cast
+}  // namespace media
+
 // This class hosts CastSender and connects it to audio/video frame input
 // and network socket.
-// This class is created on the main thread and destroyed on the IO
+// This class is created on the render thread and destroyed on the IO
 // thread. All methods are accessible only on the IO thread.
 class CastSessionDelegate {
  public:
   CastSessionDelegate();
   ~CastSessionDelegate();
 
+  // Start encoding threads and configure CastSender. It is ready to accept
+  // audio/video frames after this call.
+  void PrepareForSending();
+
  private:
+  base::ThreadChecker thread_checker_;
+  scoped_refptr<media::cast::CastEnvironment> cast_environment_;
+  scoped_ptr<media::cast::CastSender> cast_sender_;
+
+  // Utilities threads owned by this class. They are used by CastSender for
+  // encoding.
+  base::Thread audio_encode_thread_;
+  base::Thread video_encode_thread_;
+
+  // Clock used by CastSender.
+  base::DefaultTickClock clock_;
+
   // Proxy to the IO message loop.
   const scoped_refptr<base::MessageLoopProxy> io_message_loop_proxy_;
 
