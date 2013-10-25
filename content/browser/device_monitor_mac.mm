8de7d49f2d4606eae3d78d2911b1f230b90b3c5a
mcasas@chromium.org <mcasas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-25 17:51:21 +0000

    Device monitor Mac bug corrected when removing audio.
    Silly typo made it test twice for video removal :(
    
    Please refer to bug for details on testing.
    
    BUG=311255
    
    Review URL: https://codereview.chromium.org/43633002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231053 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 15c7909897afb308b43fe4c5067dc97fb118ffdf

--- content/browser/device_monitor_mac.mm
+++ content/browser/device_monitor_mac.mm
@@ -112,7 +112,7 @@ void DeviceMonitorMacImpl::ConsolidateDevicesListAndNotify(
 
   if (video_device_added || video_device_removed)
     monitor_->NotifyDeviceChanged(base::SystemMonitor::DEVTYPE_VIDEO_CAPTURE);
-  if (audio_device_added || video_device_removed)
+  if (audio_device_added || audio_device_removed)
     monitor_->NotifyDeviceChanged(base::SystemMonitor::DEVTYPE_AUDIO_CAPTURE);
 }
 
