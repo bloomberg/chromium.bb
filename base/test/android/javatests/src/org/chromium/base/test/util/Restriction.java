343dde4df121d8aaac2820e10dac515b61cd6be0
tedchoc@chromium.org <tedchoc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-28 17:35:22 +0000

    Convert from strings to enums for Restrictions.
    My hope is to prevent any copy/paste/off by one char errors by using enums.
    
    BUG=309926
    
    Review URL: https://codereview.chromium.org/45373003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231351 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 28ba4b4653dc83b72dad09afbe7c08362e6a49c8

--- base/test/android/javatests/src/org/chromium/base/test/util/Restriction.java
+++ base/test/android/javatests/src/org/chromium/base/test/util/Restriction.java
@@ -12,12 +12,21 @@ import java.lang.annotation.Target;
 /**
  * An annotation for listing restrictions for a test method. For example, if a test method is only
  * applicable on a phone with small memory:
- *     @Restriction({"Phone", "smallMemory"})
+ *     @Restriction({RESTRICTION_TYPE_PHONE, RESTRICTION_TYPE_SMALL_MEMORY})
  * Test classes are free to define restrictions and enforce them using reflection at runtime.
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Restriction {
+    /** Specifies the test is only valid on phone form factors. */
+    public static final String RESTRICTION_TYPE_PHONE = "Phone";
+
+    /** Specifies the test is only valid on tablet form factors. */
+    public static final String RESTRICTION_TYPE_TABLET = "Tablet";
+
+    /** Specifies the test is only valid on devices with a small amount of memory. */
+    public static final String RESTRICTION_TYPE_SMALL_MEMORY = "Small_Memory";
+
     /**
      * @return A list of restrictions.
      */
