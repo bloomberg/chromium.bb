6ab54edf9f2c795684d5187c851c50dd401ed96e
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-29 05:50:38 +0000

    Add libcap2 and libcap-dev to install-build-deps.sh
    BUG=312525
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/50183002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231534 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d8f52322e65d7bcf0f5e2f275af06515272cc619

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -84,17 +84,17 @@ chromeos_dev_list="libbluetooth-dev"
 
 # Packages need for development
 dev_list="apache2.2-bin bison curl elfutils fakeroot flex g++ gperf
-          language-pack-fr libapache2-mod-php5 libasound2-dev libbrlapi-dev
-          libbz2-dev libcairo2-dev libcups2-dev libcurl4-gnutls-dev libelf-dev
-          libgconf2-dev libgl1-mesa-dev libglib2.0-dev libglu1-mesa-dev
-          libgnome-keyring-dev libgtk2.0-dev libkrb5-dev libnspr4-dev
-          libnss3-dev libpam0g-dev libpci-dev libpulse-dev libsctp-dev
-          libspeechd-dev libsqlite3-dev libssl-dev libudev-dev libwww-perl
-          libxslt1-dev libxss-dev libxt-dev libxtst-dev mesa-common-dev
-          openbox patch perl php5-cgi pkg-config python python-cherrypy3
-          python-dev python-psutil rpm ruby subversion ttf-dejavu-core
-          ttf-indic-fonts ttf-kochi-gothic ttf-kochi-mincho ttf-thai-tlwg
-          wdiff git-core libdrm-dev
+          language-pack-fr libapache2-mod-php5 libasound2-dev
+          libbrlapi-dev libbz2-dev libcairo2-dev libcap-dev libcups2-dev
+          libcurl4-gnutls-dev libelf-dev libgconf2-dev libgl1-mesa-dev
+          libglib2.0-dev libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev
+          libkrb5-dev libnspr4-dev libnss3-dev libpam0g-dev libpci-dev
+          libpulse-dev libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev
+          libudev-dev libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
+          mesa-common-dev openbox patch perl php5-cgi pkg-config python
+          python-cherrypy3 python-dev python-psutil rpm ruby subversion
+          ttf-dejavu-core ttf-indic-fonts ttf-kochi-gothic ttf-kochi-mincho
+          ttf-thai-tlwg wdiff git-core libdrm-dev
           $chromeos_dev_list"
 
 # 64-bit systems need a minimum set of 32-bit compat packages for the pre-built
@@ -108,7 +108,7 @@ fi
 chromeos_lib_list="libpulse0 libbz2-1.0"
 
 # Full list of required run-time libraries
-lib_list="libatk1.0-0 libc6 libasound2 libcairo2 libcups2 libexpat1
+lib_list="libatk1.0-0 libc6 libasound2 libcairo2 libcap2 libcups2 libexpat1
           libfontconfig1 libfreetype6 libglib2.0-0 libgnome-keyring0
           libgtk2.0-0 libpam0g libpango1.0-0 libpci3 libpcre3 libpixman-1-0
           libpng12-0 libspeechd2 libstdc++6 libsqlite3-0 libx11-6
