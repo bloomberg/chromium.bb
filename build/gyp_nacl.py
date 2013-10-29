2e798f478448ad29de4006a3448062555bea82d1
dschuff@chromium.org <dschuff@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-10-29 21:39:24 +0000

    Add gyp_nacl.py to make gyp_nacl importable on python 2.6
    Modeled after gyp_chromium.py in the Chromium repo.
    R=binji@chromium.org
    BUG=red NaCl SDK bot
    
    Review URL: https://codereview.chromium.org/50003003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12324 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 27a3334de65263852dbbe02a113c7395fd75b746

---
+++ build/gyp_nacl.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# Copyright (c) 2013 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This file is (possibly, depending on python version) imported by
+# gyp_nacl when GYP_PARALLEL=1 and it creates sub-processes
+# through the multiprocessing library.
+
+# Importing in Python 2.6 (fixed in 2.7) on Windows doesn't search for
+# imports that don't end in .py (and aren't directories with an
+# __init__.py). This wrapper makes "import gyp_nacl" work with
+# those old versions and makes it possible to execute gyp_nacl.py
+# directly on Windows where the extension is useful.
+
+import os
+
+path = os.path.dirname(os.path.abspath(__file__))
+execfile(os.path.join(path, 'gyp_nacl'))
