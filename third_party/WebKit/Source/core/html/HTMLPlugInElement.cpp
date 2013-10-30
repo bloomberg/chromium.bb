a41ed00d8f9b469724700f4e845a43d320ea199a
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-30 00:31:56 +0000

    HTMLPlugInElement should not use confusingAndOftenMisusedAttached()
    This if statement didn't make any sense. If you have a renderer then
    you're certainly attached so checking confusingAndOftenMisusedAttached()
    and renderer() was silly. Simplify the check and remove the confusing
    comment since that doesn't seem to be true anymore.
    
    I also added a FIXME since all this code related to widget updates looks
    wrong.
    
    Review URL: https://codereview.chromium.org/50163004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@160883 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: f60806a5151fcdcda335acffad155808e73cb3be

--- third_party/WebKit/Source/core/html/HTMLPlugInElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLPlugInElement.cpp
@@ -144,14 +144,10 @@ void HTMLPlugInElement::updateWidgetIfNecessary()
 
 void HTMLPlugInElement::detach(const AttachContext& context)
 {
-    // FIXME: Because of the insanity that is HTMLPlugInElement::
-    // willRecalcStyle, we can end up detaching during an attach() call, before
-    // we even have a renderer. In that case, don't mark the widget for update.
-    if (confusingAndOftenMisusedAttached() && renderer() && !useFallbackContent()) {
-        // Update the widget the next time we attach (detaching destroys the
-        // plugin).
+    // Update the widget the next time we attach (detaching destroys the plugin).
+    // FIXME: None of this "needsWidgetUpdate" related code looks right.
+    if (renderer() && !useFallbackContent())
         setNeedsWidgetUpdate(true);
-    }
 
     resetInstance();
 
