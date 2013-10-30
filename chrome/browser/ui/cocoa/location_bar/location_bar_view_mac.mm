f869ab5900482ecd728626c467d3b1de13f7ca15
npentrel@chromium.org <npentrel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 12:01:55 +0000

    On Cocoa this is due to: https://codereview.chromium.org/23704009/ This can be counteracted by changing the reverse iterator to a normal iterator.
    BUG=312582
    
    Review URL: https://codereview.chromium.org/50943002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231816 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cc0036a9c4da0d7e948fb88d011731d1076dd972

--- chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.mm
+++ chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.mm
@@ -353,9 +353,9 @@ void LocationBarViewMac::Layout() {
 
   // Iterate through |content_setting_decorations_| in reverse order so that
   // the order in which the decorations are drawn matches the Views code.
-  for (ScopedVector<ContentSettingDecoration>::reverse_iterator i =
-       content_setting_decorations_.rbegin();
-       i != content_setting_decorations_.rend(); ++i) {
+  for (ScopedVector<ContentSettingDecoration>::iterator i =
+       content_setting_decorations_.begin();
+       i != content_setting_decorations_.end(); ++i) {
     [cell addRightDecoration:*i];
   }
 
