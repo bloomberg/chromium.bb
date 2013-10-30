a42fded3deaafe8c808625485bd7754e9e50844c
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-10-30 19:22:59 +0000

    Disables GetTextfieldBaseline_FontFallbackTest on Windows.
    Regardless of Windows version, disables the test on Windows because the test is unstable on Windows and it fails depending on minor changes of test environments.
    
    This test will be rewritten in http://crrev.com/25039002 soon.
    
    BUG=311267
    TEST=Run views_unittests.
    
    Review URL: https://codereview.chromium.org/43363002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@231892 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 914bcd8860dee86808141011885b1c1afa60d24f

--- ui/views/controls/textfield/native_textfield_views_unittest.cc
+++ ui/views/controls/textfield/native_textfield_views_unittest.cc
@@ -1902,15 +1902,17 @@ TEST_F(NativeTextfieldViewsTest, TestLongPressInitiatesDragDrop) {
       kStringPoint, kStringPoint));
 }
 
-TEST_F(NativeTextfieldViewsTest, GetTextfieldBaseline_FontFallbackTest) {
 #if defined(OS_WIN)
-  // This test fails on some versions of Windows because two different strings
-  // have the same baseline.
-  if (base::win::GetVersion() <= base::win::VERSION_XP ||
-      base::win::VERSION_WIN8 <= base::win::GetVersion())
-    return;
+// This test fails on some versions of Windows because two different strings
+// have the same baseline.
+#define MAYBE_GetTextfieldBaseline_FontFallbackTest \
+    DISABLED_GetTextfieldBaseline_FontFallbackTest
+#else
+#define MAYBE_GetTextfieldBaseline_FontFallbackTest \
+    GetTextfieldBaseline_FontFallbackTest
 #endif
 
+TEST_F(NativeTextfieldViewsTest, MAYBE_GetTextfieldBaseline_FontFallbackTest) {
   InitTextfield(Textfield::STYLE_DEFAULT);
   textfield_->SetText(UTF8ToUTF16("abc"));
   const int old_baseline = textfield_->GetBaseline();
