b1da0b508b926bd5211a8263f01257493cf95978
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-10-31 10:33:12 +0000

    Add harness for running Unicode.org Bidi tests.
    This depends on having the BidiTest.txt file in your
    $CWD when executing blink_platform_unittests:
    http://www.unicode.org/Public/6.3.0/ucd/BidiTest.txt
    checked in:
    src/third_party/icu/source/test/testdata/BidiTest.txt
    
    Unfortunately since we can't depend on base/ directly from
    Blink we can't use base/path_service.h and thus can't
    reliably find the checked-in BidiTest.txt file to run this
    unittest on the bots.  For now if  BidiTest.txt is not in
    $CWD it just logs an ERROR and bails without failing.
    
    To run locally:
    ninja -C out/Debug blink_platform_unittests
    ./out/Debug/blink_platform_unittests \
      --gtest_filter=BidiResolver.BidiTest_txt
    
    Currently this skips all the tests for the new isolate
    control characters in Unicode 6.3.  Even still, it looks like
    we may have a bunch of corner cases in our UBA to fix.
    
    WARNING: Skipped 418143 tests.
    Ran 352098 tests: 44887 level failures, 19153 order failures.
    
    I suspect there are a couple harness-integration issues
    causing unnecessary level failures.  Order failures are more
    likely to represent user-observable bugs.
    
    I'm interested in getting this checked in so we can iterate
    on these failures.  We should consider checking in a golden
    file as well and comparing the output between runs instead of
    just the gtest asserts I added.  (Revisions for a later patch!)
    
    I split the BidiTest.txt parsing code out into its own header
    which hopefully we can upstream to Unicode.org since it seems
    silly for everyone to write this (non-trivial) logic themselves.
    We have no business maintaining this parsing code (especially
    since the test format has changed between unicode versions).
    
    There is another bidi test file from unicode.org:
    http://www.unicode.org/Public/6.3.0/ucd/BidiCharacterTest.txt
    But that will require another separate parser. :(
    
    BUG=311432
    
    Review URL: https://codereview.chromium.org/39523002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161046 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 92e94bd7201bb838feca4744bcc38f1295035878

--- third_party/WebKit/Source/platform/text/BidiResolverTest.cpp
+++ third_party/WebKit/Source/platform/text/BidiResolverTest.cpp
@@ -32,7 +32,9 @@
 #include "platform/text/BidiResolver.h"
 
 #include "platform/graphics/TextRunIterator.h"
+#include "platform/text/BidiTestHarness.h"
 #include "wtf/OwnPtr.h"
+#include <fstream>
 #include <gtest/gtest.h>
 
 namespace {
@@ -51,6 +53,196 @@ TEST(BidiResolver, Basic)
     TextDirection direction = bidiResolver.determineParagraphDirectionality(&hasStrongDirectionality);
     EXPECT_TRUE(hasStrongDirectionality);
     EXPECT_EQ(direction, LTR);
+}
+
+class BidiTestRunner {
+public:
+    BidiTestRunner()
+        : m_testsRun(0)
+        , m_testsSkipped(0)
+        , m_ignoredCharFailures(0)
+        , m_levelFailures(0)
+        , m_orderFailures(0)
+    {
+    }
+
+    void skipTestsWith(UChar codepoint)
+    {
+        m_skippedCodePoints.insert(codepoint);
+    }
+
+    void runTest(const std::basic_string<UChar>& input, const std::vector<int>& reorder,
+        const std::vector<int>& levels, bidi_test::ParagraphDirection,
+        const std::string& line, size_t lineNumber);
+
+    size_t m_testsRun;
+    size_t m_testsSkipped;
+    std::set<UChar> m_skippedCodePoints;
+    size_t m_ignoredCharFailures;
+    size_t m_levelFailures;
+    size_t m_orderFailures;
+};
+
+// Blink's UBA does not filter out control characters, etc. Maybe it should?
+// Instead it depends on later layers of Blink to simply ignore them.
+// This function helps us emulate that to be compatible with BidiTest.txt expectations.
+static bool isNonRenderedCodePoint(UChar c)
+{
+    // The tests also expect us to ignore soft-hyphen.
+    if (c == 0xAD)
+        return true;
+    // Control characters are not rendered:
+    return c >= 0x202A && c <= 0x202E;
+    // But it seems to expect LRI, etc. to be rendered!?
+}
+
+std::string diffString(const std::vector<int>& actual, const std::vector<int>& expected)
+{
+    std::ostringstream diff;
+    diff << "actual: ";
+    // This is the magical way to print a vector to a stream, clear, right?
+    std::copy(actual.begin(), actual.end(), std::ostream_iterator<int>(diff, " "));
+    diff << " expected: ";
+    std::copy(expected.begin(), expected.end(), std::ostream_iterator<int>(diff, " "));
+    return diff.str();
+}
+
+TextDirection determineParagraphDirectionality(const TextRun& textRun)
+{
+    BidiResolver<TextRunIterator, BidiCharacterRun> resolver;
+    resolver.setStatus(BidiStatus(LTR, false));
+    resolver.setPositionIgnoringNestedIsolates(TextRunIterator(&textRun, 0));
+    return resolver.determineParagraphDirectionality();
+}
+
+void BidiTestRunner::runTest(const std::basic_string<UChar>& input, const std::vector<int>& expectedOrder,
+    const std::vector<int>& expectedLevels, bidi_test::ParagraphDirection paragraphDirection,
+    const std::string& line, size_t lineNumber)
+{
+    if (!m_skippedCodePoints.empty()) {
+        for (size_t i = 0; i < input.size(); i++) {
+            if (m_skippedCodePoints.count(input[i])) {
+                m_testsSkipped++;
+                return;
+            }
+        }
+    }
+
+    m_testsRun++;
+
+    TextRun textRun(input.data(), input.size());
+    switch (paragraphDirection) {
+    case bidi_test::DirectionAutoLTR:
+        textRun.setDirection(determineParagraphDirectionality(textRun));
+        break;
+    case bidi_test::DirectionLTR:
+        textRun.setDirection(LTR);
+        break;
+    case bidi_test::DirectionRTL:
+        textRun.setDirection(RTL);
+        break;
+    }
+    BidiResolver<TextRunIterator, BidiCharacterRun> resolver;
+    resolver.setStatus(BidiStatus(textRun.direction(), textRun.directionalOverride()));
+    resolver.setPositionIgnoringNestedIsolates(TextRunIterator(&textRun, 0));
+
+    BidiRunList<BidiCharacterRun>& runs = resolver.runs();
+    resolver.createBidiRunsForLine(TextRunIterator(&textRun, textRun.length()));
+
+    std::ostringstream errorContext;
+    errorContext << ", line " << lineNumber << " \"" << line << "\"";
+    errorContext << " context: " << bidi_test::nameFromParagraphDirection(paragraphDirection);
+
+    std::vector<int> actualOrder;
+    std::vector<int> actualLevels;
+    actualLevels.assign(input.size(), -1);
+    BidiCharacterRun* run = runs.firstRun();
+    while (run) {
+        // Blink's UBA just makes runs, the actual ordering of the display of characters
+        // is handled later in our pipeline, so we fake it here:
+        bool reversed = run->reversed(false);
+        ASSERT(run->stop() >= run->start());
+        size_t length = run->stop() - run->start();
+        for (size_t i = 0; i < length; i++) {
+            int inputIndex = reversed ? run->stop() - i - 1 : run->start() + i;
+            if (!isNonRenderedCodePoint(input[inputIndex]))
+                actualOrder.push_back(inputIndex);
+            // BidiTest.txt gives expected level data in the order of the original input.
+            actualLevels[inputIndex] = run->level();
+        }
+        run = run->next();
+    }
+
+    if (expectedOrder.size() != actualOrder.size()) {
+        m_ignoredCharFailures++;
+        EXPECT_EQ(expectedOrder.size(), actualOrder.size()) << errorContext.str();
+    } else if (expectedOrder != actualOrder) {
+        m_orderFailures++;
+        printf("ORDER %s%s\n", diffString(actualOrder, expectedOrder).c_str(), errorContext.str().c_str());
+    }
+
+    if (expectedLevels.size() != actualLevels.size()) {
+        m_ignoredCharFailures++;
+        EXPECT_EQ(expectedLevels.size(), actualLevels.size()) << errorContext.str();
+    } else {
+        for (size_t i = 0; i < expectedLevels.size(); i++) {
+            // level == -1 means the level should be ignored.
+            if (expectedLevels[i] == actualLevels[i] || expectedLevels[i] == -1)
+                continue;
+
+            printf("LEVELS %s%s\n", diffString(actualLevels, expectedLevels).c_str(), errorContext.str().c_str());
+            m_levelFailures++;
+            break;
+        }
+    }
+}
+
+
+TEST(BidiResolver, BidiTest_txt)
+{
+    BidiTestRunner runner;
+    // Blink's Unicode Bidi Algorithm (UBA) doesn't yet support the
+    // new isolate directives from Unicode 6.3:
+    // http://www.unicode.org/reports/tr9/#Explicit_Directional_Isolates
+    runner.skipTestsWith(0x2066); // LRI
+    runner.skipTestsWith(0x2067); // RLI
+    runner.skipTestsWith(0x2068); // FSI
+    runner.skipTestsWith(0x2069); // PDI
+
+    // This code wants to use PathService from base/path_service.h
+    // but we aren't allowed to depend on base/ directly from Blink yet.
+    // Alternatively we could use:
+    // WebKit::Platform::current()->unitTestSupport()->webKitRootDir()
+    // and a relative path, but that would require running inside
+    // webkit_unit_tests (to have a functioning Platform object).
+    // The file we want is:
+    // src/third_party/icu/source/test/testdata/BidiTest.txt
+    // but we don't have any good way to find it from this unittest.
+    // Just assume we're running this test manually for now. On the
+    // bots we just print a warning that we can't find the test file.
+    std::string bidiTestPath = "BidiTest.txt";
+    std::ifstream bidiTestFile(bidiTestPath.c_str());
+    if (!bidiTestFile.is_open()) {
+        printf("ERROR: Failed to open BidiTest.txt, cannot run tests.\n");
+        return;
+    }
+
+    bidi_test::Harness<BidiTestRunner> harness(runner);
+    harness.parse(bidiTestFile);
+    bidiTestFile.close();
+
+    if (runner.m_testsSkipped)
+        printf("WARNING: Skipped %zu tests.\n", runner.m_testsSkipped);
+    printf("Ran %zu tests: %zu level failures %zu order failures.\n",
+        runner.m_testsRun, runner.m_levelFailures, runner.m_orderFailures);
+
+    // The unittest harness only pays attention to GTest output, so we verify
+    // that the tests behaved as expected:
+    EXPECT_EQ(runner.m_testsRun, 352098u);
+    EXPECT_EQ(runner.m_testsSkipped, 418143u);
+    EXPECT_EQ(runner.m_ignoredCharFailures, 0u);
+    EXPECT_EQ(runner.m_levelFailures, 44887u);
+    EXPECT_EQ(runner.m_orderFailures, 19153u);
 }
 
 }
