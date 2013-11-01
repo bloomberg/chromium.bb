9d5ed1b66fd57c93ff73cb801c3d24cf11169b1d
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-11-01 21:48:33 +0000

    PNaCl: Remove some tests' uses of "-nostdlib" in TRANSLATEFLAGS
    Since issue 3069 and others were fixed, this is no longer necessary
    for the barebones tests.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3069
    TEST=trybots
    
    Review URL: https://codereview.chromium.org/56173002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12348 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 9eb01cc0fbd070976af165c90b4f0f105766e36a

--- tests/barebones/fake_startup.c
+++ tests/barebones/fake_startup.c
@@ -13,27 +13,3 @@ extern int main(void);
 void _start(void) {
   main();
 }
-
-/* LLVM can generate references to memset/memcpy and other intrinsics
- * during optimization (opt) or during translation (llc).
- * Since barebones tests are compiled with -nostdlib those functions
- * are not provided by libc and will re-implement them here
- */
-
-typedef unsigned int size_t;
-
-void *memset(void *s, int c, size_t n) __attribute__((weak));
-void *memset(void *s, int c, size_t n) {
-  size_t i;
-  for (i = 0; i < n; i++)
-    ((char*)s)[i] = c;
-   return s;
-}
-
-void *memcpy(void *dest, const void *src, size_t n) __attribute__((weak));
-void *memcpy(void *dest, const void *src, size_t n) {
-  size_t i;
-  for (i = 0; i < n; i++)
-     ((char*)dest)[i] = ((char*)src)[i];
-  return dest;
-}
