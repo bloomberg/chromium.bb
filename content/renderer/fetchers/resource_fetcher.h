7e93f30f859025e6f78dfa95577a00b01a235575
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-01 15:26:36 +0000

    Revert 232369 "Revert 232312 "Cleanup ResourceFetcher in prepara..."
    Revert didn't fix failing test.
    
    > Revert 232312 "Cleanup ResourceFetcher in preparation to add to ..."
    > 
    > Seems to cause ResourceFetcherTests.ResourceFetcherDeletedInCallback
    > to timeout on Windows and crash on Linux.
    > 
    > > Cleanup ResourceFetcher in preparation to add to public content API,
    > > in preparation to move error page logic from content to chrome:
    > > 
    > > * Merge ResourceFetcher and ResourceFetcherWithTimeout.
    > > * Remove unnecessary member variables.
    > > * Remove Cancel() (All consumers were deleting and cancelling at the
    > >     same time, anyways, and deleting already cancels).
    > > * Make definition order match declaration order.
    > > * Fix outdated comments.
    > > 
    > > BUG=308232
    > > 
    > > Review URL: https://codereview.chromium.org/33363003
    > 
    > TBR=mmenke@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/55963002
    
    TBR=csharp@chromium.org
    
    Review URL: https://codereview.chromium.org/55943003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@232403 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9bcedf6ec06888b4972b770647e4b8d3b526abfb

--- content/renderer/fetchers/resource_fetcher.h
+++ content/renderer/fetchers/resource_fetcher.h
@@ -2,12 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
-// A wrapper around ResourceHandle and ResourceHandleClient that simplifies
-// the download of an HTTP object.  The interface is modeled after URLFetcher
-// in the /chrome/browser.
+// A wrapper that simplifies the download of an HTTP object.  The interface is
+// modeled after URLFetcher in /net/url_request/.
 //
-// ResourceFetcher::Delegate::OnURLFetchComplete will be called async after
-// the ResourceFetcher object is created.
+// The callback passed to the constructor will be called async after the
+// resource is retrieved.
 
 #ifndef CONTENT_RENDERER_FETCHERS_RESOURCE_FETCHER_H_
 #define CONTENT_RENDERER_FETCHERS_RESOURCE_FETCHER_H_
@@ -18,12 +17,12 @@
 #include "base/callback.h"
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/time/time.h"
 #include "base/timer/timer.h"
 #include "content/common/content_export.h"
 #include "third_party/WebKit/public/platform/WebURLLoaderClient.h"
 #include "third_party/WebKit/public/platform/WebURLRequest.h"
 #include "third_party/WebKit/public/platform/WebURLResponse.h"
-#include "url/gurl.h"
 
 class GURL;
 
@@ -40,23 +39,36 @@ class CONTENT_EXPORT ResourceFetcher
  public:
   // This will be called when the URL has been fetched, successfully or not.
   // If there is a failure, response and data will both be empty.  |response|
-  // and |data| are both valid until the URLFetcher instance is destroyed.
-  typedef base::Callback<void(const WebKit::WebURLResponse&,
-                              const std::string&)> Callback;
+  // and |data| are both valid until the ResourceFetcher instance is destroyed.
+  typedef base::Callback<void(const WebKit::WebURLResponse& response,
+                              const std::string& data)> Callback;
 
-  // We need a frame to make requests.
+  // A frame is needed to make requests.
   ResourceFetcher(
       const GURL& url, WebKit::WebFrame* frame,
       WebKit::WebURLRequest::TargetType target_type,
       const Callback& callback);
+
+  // Deleting a ResourceFetcher will safely cancel the request if it has not yet
+  // completed.
   virtual ~ResourceFetcher();
 
-  // Stop the request and don't call the callback.
-  void Cancel();
+  // Sets a timeout for the request.  Request must not yet have completed
+  // or timed out when called.
+  void SetTimeout(base::TimeDelta timeout);
 
-  bool completed() const { return completed_; }
+ private:
+  // Start the actual download.
+  void Start(const GURL& url, WebKit::WebFrame* frame,
+             WebKit::WebURLRequest::TargetType target_type);
 
- protected:
+  void RunCallback(const WebKit::WebURLResponse& response,
+                   const std::string& data);
+
+  // Callback for timer that limits how long we wait for the server.  If this
+  // timer fires and the request hasn't completed, we kill the request.
+  void TimeoutFired();
+
   // WebURLLoaderClient methods:
   virtual void willSendRequest(
       WebKit::WebURLLoader* loader, WebKit::WebURLRequest& new_request,
@@ -79,55 +91,25 @@ class CONTENT_EXPORT ResourceFetcher
 
   scoped_ptr<WebKit::WebURLLoader> loader_;
 
-  // URL we're fetching
-  GURL url_;
-
-  // Target type
-  WebKit::WebURLRequest::TargetType target_type_;
-
-  // A copy of the original resource response
-  WebKit::WebURLResponse response_;
-
-  // Set to true once the request is compelte.
+  // Set to true once the request is complete.
   bool completed_;
-
- private:
-  // Start the actual download.
-  void Start(WebKit::WebFrame* frame);
-
-  void RunCallback(const WebKit::WebURLResponse& response,
-                   const std::string& data);
-
-  // Callback when we're done
-  Callback callback_;
 
   // Buffer to hold the content from the server.
   std::string data_;
 
+  // A copy of the original resource response.
+  WebKit::WebURLResponse response_;
+
+  // Callback when we're done.
+  Callback callback_;
+
   // Buffer to hold metadata from the cache.
   std::string metadata_;
-};
 
-/////////////////////////////////////////////////////////////////////////////
-// A resource fetcher with a timeout
-class CONTENT_EXPORT ResourceFetcherWithTimeout
-    : NON_EXPORTED_BASE(public ResourceFetcher) {
- public:
-  ResourceFetcherWithTimeout(const GURL& url,
-                             WebKit::WebFrame* frame,
-                             WebKit::WebURLRequest::TargetType target_type,
-                             int timeout_secs,
-                             const Callback& callback);
-  virtual ~ResourceFetcherWithTimeout();
+  // Limit how long to wait for the server.
+  base::OneShotTimer<ResourceFetcher> timeout_timer_;
 
- private:
-  // Callback for timer that limits how long we wait for the alternate error
-  // page server.  If this timer fires and the request hasn't completed, we
-  // kill the request.
-  void TimeoutFired();
-
-  // Limit how long we wait for the alternate error page server.
-  base::OneShotTimer<ResourceFetcherWithTimeout> timeout_timer_;
+  DISALLOW_COPY_AND_ASSIGN(ResourceFetcher);
 };
 
 }  // namespace content
