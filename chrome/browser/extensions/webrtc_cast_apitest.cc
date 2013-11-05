391c8ce04d657c6c4fbadf9b83c2cc5754c12452
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-05 17:55:46 +0000

    Disable WebrtcCastApiTEst.Basics
    This test is failing only on Win7 and a DISABLED_ prefix couldn't
    disable it. Make sure this test is disabled for now.
    
    TBR=kalman@chromium.org
    BUG=314971
    
    Review URL: https://codereview.chromium.org/59383007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233024 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d9cbd050a270680e24f05c29d49966087d38be4d

--- chrome/browser/extensions/webrtc_cast_apitest.cc
+++ chrome/browser/extensions/webrtc_cast_apitest.cc
@@ -12,7 +12,10 @@ class WebrtcCastApiTest : public ExtensionApiTest {
 
 // Test running the test extension for Cast Mirroring API.
 IN_PROC_BROWSER_TEST_F(WebrtcCastApiTest, DISABLED_Basics) {
-  ASSERT_TRUE(RunExtensionSubtest("webrtc_cast", "basics.html"));
+  // TODO(hclam): See crbug.com/314971 for details.
+  // This test is failing on Win7 only and is still being executed
+  // even the test is disabled. Investigate and fix it.
+  // ASSERT_TRUE(RunExtensionSubtest("webrtc_cast", "basics.html"));
 }
 
 }  // namespace extensions
