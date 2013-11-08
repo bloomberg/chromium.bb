66fc0798545a099cebb817e8a480cb367a4ac797
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-08 17:35:01 +0000

    Revert 233840 "Add CreateRegionFromSkRegion and update DesktopRo..."
    Caused compile error on chrome os builders
    e.g. http://build.chromium.org/p/chromium.chromiumos/builders/ChromiumOS%20%28x86%29/builds/17724
    
    chromeos-chrome-33.0.1704.0_alpha-r1: ui/gfx/path_x11.cc: In function '_XRegion* gfx::CreateRegionFromSkRegion(const SkRegion&)':
    chromeos-chrome-33.0.1704.0_alpha-r1: ui/gfx/path_x11.cc:20:18: error: narrowing conversion of '(& i.SkRegion::Iterator::rect())->SkIRect::x()' from 'int' to 'short int' inside { } is ill-formed in C++11 [-Werror=narrowing]
    chromeos-chrome-33.0.1704.0_alpha-r1: ui/gfx/path_x11.cc:20:32: error: narrowing conversion of '(& i.SkRegion::Iterator::rect())->SkIRect::y()' from 'int' to 'short int' inside { } is ill-formed in C++11 [-Werror=narrowing]
    chromeos-chrome-33.0.1704.0_alpha-r1: ui/gfx/path_x11.cc:20:50: error: narrowing conversion of '(& i.SkRegion::Iterator::rect())->SkIRect::width()' from 'int' to 'short unsigned int' inside { } is ill-formed in C++11 [-Werror=narrowing]
    chromeos-chrome-33.0.1704.0_alpha-r1: ui/gfx/path_x11.cc:20:69: error: narrowing conversion of '(& i.SkRegion::Iterator::rect())->SkIRect::height()' from 'int' to 'short unsigned int' inside { } is ill-formed in C++11 [-Werror=narrowing]
    chromeos-chrome-33.0.1704.0_alpha-r1: cc1plus: all warnings being treated as errors
    
    > Add CreateRegionFromSkRegion and update DesktopRootWindowHostX11 to use it.
    > 
    > SkRegion can now be converted directly to an X11 Region, rather than via SkPath, thereby avoiding the lack of support for non-contiguous regions in the SkPath to Region conversion step.
    > 
    > This CL is deliberately a minimal fix, suitable for merging.
    > 
    > BUG=315236
    > 
    > Review URL: https://codereview.chromium.org/65463002
    
    TBR=wez@chromium.org
    
    Review URL: https://codereview.chromium.org/66533003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233932 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 21c8f5b8e33e4e28f177abacb774cc1ba1ac137b

--- ui/views/widget/desktop_aura/desktop_root_window_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_root_window_host_x11.cc
@@ -435,7 +435,9 @@ gfx::Rect DesktopRootWindowHostX11::GetWorkAreaBoundsInScreen() const {
 
 void DesktopRootWindowHostX11::SetShape(gfx::NativeRegion native_region) {
   if (native_region) {
-    Region region = gfx::CreateRegionFromSkRegion(*native_region);
+    SkPath path;
+    native_region->getBoundaryPath(&path);
+    Region region = gfx::CreateRegionFromSkPath(path);
     XShapeCombineRegion(
         xdisplay_, xwindow_, ShapeBounding, 0, 0, region, false);
     XDestroyRegion(region);
