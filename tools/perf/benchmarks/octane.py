380d5ac050dec301056ee39f8fddbbd8fddb7a5e
prasadv@chromium.org <prasadv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-08 05:45:30 +0000

    [Telemetry]: Upgrade telemetry Octane benchmark to v2.0. Also use web page reply instead of local test site.
    BUG=316252
    
    Review URL: https://codereview.chromium.org/60583004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233784 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b5af2c0bccb9da38c7480c56494cbba11042d335

--- tools/perf/benchmarks/octane.py
+++ tools/perf/benchmarks/octane.py
@@ -2,41 +2,54 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+"""Runs Octane 2.0 javascript benchmark.
+
+Octane 2.0 is a modern benchmark that measures a JavaScript engine's performance
+by running a suite of tests representative of today's complex and demanding web
+applications. Octane's goal is to measure the performance of JavaScript code
+found in large, real-world web applications.
+Octane 2.0 consists of 17 tests, four more than Octane v1.
+"""
+
 import os
 
+from metrics import statistics
 from telemetry import test
-from telemetry.core import util
 from telemetry.page import page_measurement
 from telemetry.page import page_set
 
 
 class _OctaneMeasurement(page_measurement.PageMeasurement):
+
+  def WillNavigateToPage(self, page, tab):
+    page.script_to_evaluate_on_commit = """
+        var __results = [];
+        var __real_log = window.console.log;
+        window.console.log = function(msg) {
+          __results.push(msg);
+          __real_log.apply(this, [msg]);
+        }
+        """
+
   def MeasurePage(self, _, tab, results):
     tab.WaitForJavaScriptExpression(
         'completed && !document.getElementById("progress-bar-container")', 300)
 
-    js_get_results = """
-var results = {};
-var result_divs = document.querySelectorAll('.p-result');
-for (var r in result_divs) {
-  if (result_divs[r].id && result_divs[r].id.indexOf('Result-') == 0) {
-    var key = result_divs[r].id.replace('Result-', '');
-    results[key] = result_divs[r].innerHTML;
-  }
-}
-var main_banner = document.getElementById("main-banner").innerHTML;
-var octane_score = main_banner.substr(main_banner.lastIndexOf(':') + 2);
-results['score'] = octane_score;
-JSON.stringify(results);
-"""
-    result_dict = eval(tab.EvaluateJavaScript(js_get_results))
-    for key, value in result_dict.iteritems():
-      if value == '...':
-        continue
-      data_type = 'unimportant'
-      if key == 'score':
-        data_type = 'default'
-      results.Add(key, 'score (bigger is better)', value, data_type=data_type)
+    results_log = tab.EvaluateJavaScript('__results')
+    all_scores = []
+    for output in results_log:
+      # Split the results into score and test name.
+      # results log e.g., "Richards: 18343"
+      score_and_name = output.split(': ', 2)
+      assert len(score_and_name) == 2, \
+        'Unexpected result format "%s"' % score_and_name
+      name = score_and_name[0]
+      score = int(score_and_name[1])
+      results.Add(name, 'score', score, data_type='unimportant')
+      # Collect all test scores to compute geometric mean.
+      all_scores.append(score)
+    total = statistics.GeometricMean(all_scores)
+    results.AddSummary('Score', 'score', total, chart_name='Total')
 
 
 class Octane(test.Test):
@@ -44,10 +57,13 @@ class Octane(test.Test):
   test = _OctaneMeasurement
 
   def CreatePageSet(self, options):
-    octane_dir = os.path.join(util.GetChromiumSrcDir(), 'chrome', 'test',
-                              'data', 'perf', 'third_party', 'octane')
     return page_set.PageSet.FromDict({
-        'pages': [
-          { 'url': 'file://index.html?auto=1' }
-          ]
-        }, octane_dir)
+      'archive_data_file': '../page_sets/data/octane.json',
+      'make_javascript_deterministic': False,
+      'pages': [{
+          'url':
+          'http://octane-benchmark.googlecode.com/svn/latest/index.html?auto=1'
+          }
+        ]
+      }, os.path.abspath(__file__))
+
