7c995fd81a539b0094929829dc2e071b31a3a0fd
Mike Frysinger <vapier@chromium.org>
2013-11-08 17:55:38 -0500

    cros: lint: add a docstring checker
    BUG=chromium:327969
    TEST=ran linter on various scripts and checked behavior
    
    Change-Id: I5055ee6b97595c756f78af342e0da26eabea2a2e
    Reviewed-on: https://chromium-review.googlesource.com/176182
    Reviewed-by: Matt Tennant <mtennant@chromium.org>
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Reviewed-by: David James <davidjames@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

---
+++ cros/commands/lint.py
@@ -0,0 +1,253 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This module is not automatically loaded by the `cros` helper.  The filename
+# would need a "cros_" prefix to make that happen.  It lives here so that it
+# is alongside the cros_lint.py file.
+
+# TODO: See about merging with the pep257 project:
+# https://github.com/GreenSteam/pep257
+
+"""Additional lint modules loaded by pylint.
+
+This is loaded by pylint directly via its pylintrc file:
+  load-plugins=chromite.cros.commands.lint
+
+Then pylint will import the register function and call it.  So we can have
+as many/few checkers as we want in this one module.
+"""
+
+import os
+
+from pylint.checkers import BaseChecker
+from pylint.interfaces import IASTNGChecker
+
+
+class DocStringChecker(BaseChecker):
+  """PyLint AST based checker to verify PEP 257 compliance
+
+  See our style guide for more info:
+  http://dev.chromium.org/chromium-os/python-style-guidelines#TOC-Describing-arguments-in-docstrings
+  """
+
+  __implements__ = IASTNGChecker
+
+  name = 'doc_string_checker'
+  priority = -1
+  MSG_ARGS = 'offset:%(offset)i: {%(line)s}'
+  msgs = {
+      # The C9xxx namespace should be reserved for our own use.
+      'C9001': ('Modules should have docstrings (even a one liner)',
+                ('Used when a module lacks a docstring entirely')),
+      'C9002': ('Classes should have docstrings (even a one liner)',
+                ('Used when a class lacks a docstring entirely')),
+      'C9003': ('Trailing whitespace in docstring'
+                ': %s' % MSG_ARGS,
+                ('Used whenever we find trailing whitespace')),
+      'C9004': ('Leading whitespace in docstring (excess or missing)'
+                ': %s' % MSG_ARGS,
+                ('Used whenever we find incorrect leading whitespace')),
+      'C9005': ('Closing triple quotes should not be cuddled',
+                ('Used when the closing quotes are not by themselves')),
+      'C9006': ('Section names should be preceded by one blank line'
+                ': %s' % MSG_ARGS,
+                ('Used when we detect misbehavior around sections')),
+      'C9007': ('Section names should be "Args:", "Returns:", and "Raises:"'
+                ': %s' % MSG_ARGS,
+                ('Used when we detect misbehavior around sections')),
+      'C9008': ('Sections should be in the order: Args, Returns, Raises',
+                ('Used when the various sections are misordered')),
+      'C9009': ('First line should be a short summary',
+                ('Used when a short doc string is on multiple lines')),
+      'C9010': ('Not all args mentioned in doc string: |%(arg)s|',
+                ('Used when not all arguments are in the doc string')),
+      'C9011': ('Variable args/keywords are named *args/**kwargs, not %(arg)s',
+                ('Used when funcs use different names for varargs')),
+  }
+  options = ()
+
+  def visit_function(self, node):
+    """Verify function docstrings"""
+    if node.doc:
+      lines = node.doc.split('\n')
+      self._check_common(node, lines)
+      self._check_last_line_function(node, lines)
+      self._check_section_lines(node, lines)
+      self._check_all_args_in_doc(node, lines)
+      self._check_func_signature(node)
+    else:
+      # This is what C0111 already does for us, so ignore.
+      pass
+
+  def visit_module(self, node):
+    """Verify module docstrings"""
+    if node.doc:
+      self._check_common(node)
+    else:
+      # Ignore stub __init__.py files.
+      if os.path.basename(node.file) == '__init__.py':
+        return
+      self.add_message('C9001', node=node)
+
+  def visit_class(self, node):
+    """Verify class docstrings"""
+    if node.doc:
+      self._check_common(node)
+    else:
+      self.add_message('C9002', node=node, line=node.tolineno)
+
+  def _check_common(self, node, lines=None):
+    """Common checks we enforce on all docstrings"""
+    if lines is None:
+      lines = node.doc.split('\n')
+
+    funcs = (
+        self._check_first_line,
+        self._check_whitespace,
+        self._check_last_line,
+    )
+    for f in funcs:
+      f(node, lines)
+
+  def _check_first_line(self, node, lines):
+    """Make sure first line is a short summary by itself"""
+    if lines[0] == '':
+      self.add_message('C9009', node=node, line=node.tolineno)
+
+  def _check_whitespace(self, node, lines):
+    """Verify whitespace is sane"""
+    # Verify no trailing whitespace.
+    # We skip the last line since it's supposed to be pure whitespace.
+    for l, i in zip(lines[:-1], xrange(len(lines))):
+      if l.rstrip() != l:
+        margs = {'offset': i, 'line': l}
+        self.add_message('C9003', node=node, line=node.tolineno, args=margs)
+
+    # Now specially handle the last line.  Note: if there's just one line,
+    # then we can check leading & trailing whitespace.
+    l = lines[-1]
+    if ((len(lines) == 1 and l.strip() != l) or
+        (l.strip() != '' and l.rstrip() != l)):
+      margs = {'offset': len(lines), 'line': l}
+      self.add_message('C9003', node=node, line=node.tolineno, args=margs)
+
+    # Should we scan for more than one new line in a row ?
+
+  def _check_last_line(self, node, lines):
+    """Make sure last line is all by itself"""
+    if len(lines) > 1:
+      if lines[-1].strip() != '':
+        self.add_message('C9005', node=node, line=node.tolineno)
+
+  def _check_last_line_function(self, node, lines):
+    """Make sure last line is indented"""
+    if len(lines) > 1:
+      if lines[-1] == '':
+        self.add_message('C9005', node=node, line=node.tolineno)
+
+  def _check_section_lines(self, node, lines):
+    """Verify each section (Args/Returns/Raises) is sane"""
+    # TODO: Should we handle Yields?  Or force it to be a Returns?
+    # TODO: Should we enforce Examples?
+    valid_sections = ('Args', 'Returns', 'Raises',)
+    lineno_sections = [-1] * len(valid_sections)
+    invalid_sections = (
+        # Handle common misnamings.
+        'arg', 'argument', 'arguments',
+        'ret', 'rets', 'return',
+        'raise', 'throw', 'throws',
+    )
+
+    last = lines[0].strip()
+    for line, i in zip(lines[1:], xrange(len(lines))):
+      l = line.strip()
+
+      # See if we can detect incorrect behavior.
+      section = l.split(':', 1)[0]
+      if section in valid_sections or section.lower() in invalid_sections:
+        margs = {'offset': i + 1, 'line': line}
+
+        # Make sure it has some number of leading whitespace.
+        if not line.startswith(' '):
+          self.add_message('C9004', node=node, line=node.tolineno, args=margs)
+
+        # Make sure it has a single trailing colon.
+        if l != '%s:' % section:
+          self.add_message('C9007', node=node, line=node.tolineno, args=margs)
+
+        # Make sure it's valid.
+        if section.lower() in invalid_sections:
+          self.add_message('C9007', node=node, line=node.tolineno, args=margs)
+        else:
+          # Gather the order of the sections.
+          lineno_sections[valid_sections.index(section)] = i
+
+        # Verify blank line before it.
+        if last != '':
+          self.add_message('C9006', node=node, line=node.tolineno, args=margs)
+
+      last = l
+
+    # Make sure the sections are in the right order.
+    valid_lineno = lambda x: x >= 0
+    lineno_sections = filter(valid_lineno, lineno_sections)
+    if lineno_sections != sorted(lineno_sections):
+      self.add_message('C9008', node=node, line=node.tolineno)
+
+  def _check_all_args_in_doc(self, node, lines):
+    """All function arguments are mentioned in doc"""
+    if not hasattr(node, 'argnames'):
+      return
+
+    # Locate the start of the args section.
+    arg_lines = []
+    for l in lines:
+      if arg_lines:
+        if l.strip() in ('', 'Returns:'):
+          break
+      elif l.strip() != 'Args:':
+        continue
+      arg_lines.append(l)
+    else:
+      # If they don't have an Args section, then give it a pass.
+      return
+
+    # Now verify all args exist.
+    # XXX: Should we verify arg order matches doc order ?
+    # XXX: Should we check indentation of wrapped docs ?
+    # XXX: Should we check amount of space after the : ?
+    for arg in node.args.args:
+      # Ignore class related args.
+      if arg.name in ('cls', 'self'):
+        continue
+      # Ignore ignored args.
+      if arg.name.startswith('_'):
+        continue
+
+      margs = {'arg': arg.name}
+      for l in arg_lines:
+        if l.lstrip().startswith('%s:' % arg.name):
+          break
+      else:
+        break
+    else:
+      return
+    self.add_message('C9010', node=node, line=node.tolineno, args=margs)
+
+  def _check_func_signature(self, node):
+    """Require *args to be named args, and **kwargs kwargs"""
+    vararg = node.args.vararg
+    if vararg and vararg != 'args' and vararg != '_args':
+      margs = {'arg': vararg}
+      self.add_message('C9011', node=node, line=node.tolineno, args=margs)
+
+    kwarg = node.args.kwarg
+    if kwarg and kwarg != 'kwargs' and kwarg != '_kwargs':
+      margs = {'arg': kwarg}
+      self.add_message('C9011', node=node, line=node.tolineno, args=margs)
+
+
+def register(linter):
+  """pylint will call this func to register all our checkers"""
+  linter.register_checker(DocStringChecker(linter))
---
+++ cros/commands/lint_unittest.py
@@ -0,0 +1,182 @@
+#!/usr/bin/python
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Test the lint module."""
+
+import collections
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('%s/../../..' % os.path.dirname(__file__)))
+
+from chromite.lib import cros_test_lib
+import lint
+
+
+class TestNode(object):
+  """Object good enough to stand in for lint funcs"""
+
+  Args = collections.namedtuple('Args', ('args', 'vararg', 'kwarg'))
+
+  def __init__(self, doc='', tolineno=0, path='foo.py', args=(), vararg='',
+               kwarg=''):
+    self.doc = doc
+    self.lines = doc.split('\n')
+    self.tolineno = tolineno
+    self.file = path
+    self.args = self.Args(args=args, vararg=vararg, kwarg=kwarg)
+
+  def argnames(self):
+    return self.args
+
+
+class DocStringCheckerTest(cros_test_lib.TestCase):
+  """Tests for DocStringChecker module"""
+
+  GOOD_FUNC_DOCSTRINGS = (
+      'Some string',
+      """Short summary
+
+      Body of text.
+      """,
+      """line o text
+
+      Body and comments on
+      more than one line.
+
+      Args:
+        moo: cow
+
+      Returns:
+        some value
+
+      Raises:
+        something else
+      """
+  )
+
+  BAD_FUNC_DOCSTRINGS = (
+      """
+      bad first line
+      """,
+      """ whitespace is wrong""",
+      """whitespace is wrong	""",
+      """Should be no trailing blank lines
+
+      Returns:
+        a value
+
+      """
+      """ok line
+
+      cuddled end""",
+      """we want Args/Returns not Arguments/Return
+
+      Arguments:
+      Return:
+      """,
+      """section order is wrong here
+
+      Raises:
+      Returns:
+      """,
+      """sections lack whitespace between them
+
+      Args:
+        foo: bar
+      Returns:
+        yeah
+      """,
+  )
+
+  # The current linter isn't good enough yet to detect these.
+  TODO_BAD_FUNC_DOCSTRINGS = (
+      """The returns section isn't a proper section
+
+      Args:
+        bloop: de
+
+      returns something
+      """,
+      """the indentation is incorrect
+
+        Args:
+          some: day
+      """,
+      """too many spaces after the colon
+
+      Args:
+        some:    day
+      """,
+  )
+
+  def add_message(self, msg_id, node=None, line=None, args=None):
+    """Capture lint checks"""
+    # We include node.doc here explicitly so the pretty assert message
+    # inclues it in the output automatically.
+    self.results.append((msg_id, node.doc, line, args))
+
+  def setUp(self):
+    self.results = []
+    self.checker = lint.DocStringChecker()
+    self.checker.add_message = self.add_message
+
+  def testGood_visit_function(self):
+    """Allow known good docstrings"""
+    for dc in self.GOOD_FUNC_DOCSTRINGS:
+      self.results = []
+      node = TestNode(doc=dc)
+      self.checker.visit_function(node)
+      self.assertEqual(self.results, [],
+                       msg='docstring was not accepted:\n"""%s"""' % dc)
+
+  def testBad_visit_function(self):
+    """Reject known bad docstrings"""
+    for dc in self.BAD_FUNC_DOCSTRINGS:
+      self.results = []
+      node = TestNode(doc=dc)
+      self.checker.visit_function(node)
+      self.assertNotEqual(self.results, [],
+                          msg='docstring was not rejected:\n"""%s"""' % dc)
+
+  def testSmoke_visit_module(self):
+    """Smoke test for modules"""
+    self.checker.visit_module(TestNode(doc='foo'))
+    self.assertEqual(self.results, [])
+    self.checker.visit_module(TestNode(doc='', path='/foo/__init__.py'))
+    self.assertEqual(self.results, [])
+
+  def testSmoke_visit_class(self):
+    """Smoke test for classes"""
+    self.checker.visit_class(TestNode(doc='bar'))
+
+  def testGood_check_first_line(self):
+    """Verify _check_first_line accepts good inputs"""
+    # pylint: disable=W0212
+    docstrings = (
+        'Some string',
+    )
+    for dc in docstrings:
+      self.results = []
+      node = TestNode(doc=dc)
+      self.checker._check_first_line(node, node.lines)
+      self.assertEqual(self.results, [],
+                       msg='docstring was not accepted:\n"""%s"""' % dc)
+
+  def testBad_check_first_line(self):
+    """Verify _check_first_line rejects bad inputs"""
+    # pylint: disable=W0212
+    docstrings = (
+        '\nSome string\n',
+    )
+    for dc in docstrings:
+      self.results = []
+      node = TestNode(doc=dc)
+      self.checker._check_first_line(node, node.lines)
+      self.assertEqual(len(self.results), 1)
+
+
+if __name__ == '__main__':
+  cros_test_lib.main()
