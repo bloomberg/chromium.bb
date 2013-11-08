e6624d504167473726eb724553b85691eb0b3e73
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-08 09:06:09 +0000

    Refactor taskbar anchor calculations in app_list_win.
    Cleanly separates the concerns of determining which edge the taskbar
    is pinned to, and finding the reference point for the given edge.
    
    Also makes several implicit assumptions into explicit checks. For
    example, before if a horizontal taskbar was not at the bottom, it was
    assumed to be at the top. Now we check that it is at the top, and if
    not, fall back on the default. This new behaviour should not be
    visible, but if the taskbar is in an unexpected place, it should fail
    gracefully.
    
    BUG=307837
    
    Review URL: https://codereview.chromium.org/63983002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@233836 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 20b64d9e1159b0f1d7a2127d555def6bfad86971

--- chrome/browser/ui/views/app_list/win/app_list_win.cc
+++ chrome/browser/ui/views/app_list/win/app_list_win.cc
@@ -13,6 +13,14 @@ namespace {
 
 static const wchar_t kTrayClassName[] = L"Shell_TrayWnd";
 
+// If the mouse cursor is less than this distance, in pixels, away from the
+// taskbar, it is considered to be in the taskbar for the purpose of anchoring.
+static const int kSnapDistance = 50;
+
+// The minimum distance, in pixels, to position the app list from the taskbar or
+// edge of screen.
+static const int kSnapOffset = 3;
+
 // Utility methods for showing the app list.
 // Attempts to find the bounds of the Windows taskbar. Returns true on success.
 // |rect| is in screen coordinates. If the taskbar is in autohide mode and is
@@ -31,75 +39,107 @@ bool GetTaskbarRect(gfx::Rect* rect) {
   return true;
 }
 
-gfx::Point FindReferencePoint(const gfx::Display& display,
-                              const gfx::Point& cursor) {
-  const int kSnapDistance = 50;
+// Represents one of the four edges of the screen.
+enum ScreenEdge {
+  SCREEN_EDGE_UNKNOWN,
+  SCREEN_EDGE_LEFT,
+  SCREEN_EDGE_RIGHT,
+  SCREEN_EDGE_TOP,
+  SCREEN_EDGE_BOTTOM
+};
 
-  // If we can't find the taskbar, snap to the bottom left.
-  // If the display size is the same as the work area, and does not contain the
-  // taskbar, either the taskbar is hidden or on another monitor, so just snap
-  // to the bottom left.
-  gfx::Rect taskbar_rect;
-  if (!GetTaskbarRect(&taskbar_rect) ||
-      (display.work_area() == display.bounds() &&
-          !display.work_area().Contains(taskbar_rect))) {
-    return display.work_area().bottom_left();
+// Determines which edge of the screen the taskbar is attached to. Returns
+// SCREEN_EDGE_UNKNOWN if the taskbar is unavailable, hidden, or not on the
+// current screen.
+ScreenEdge FindTaskbarEdge(const gfx::Display& display,
+                           const gfx::Rect& taskbar_rect) {
+  const gfx::Rect& screen_rect = display.bounds();
+
+  // If we can't find the taskbar, return SCREEN_EDGE_UNKNOWN. If the display
+  // size is the same as the work area, and does not contain the taskbar, either
+  // the taskbar is hidden or on another monitor.
+  if (display.work_area() == screen_rect &&
+      !display.work_area().Contains(taskbar_rect)) {
+    return SCREEN_EDGE_UNKNOWN;
   }
 
+  // Note: On Windows 8 the work area won't include split windows on the left or
+  // right, and neither will |taskbar_rect|.
+  if (taskbar_rect.width() == display.work_area().width()) {
+    // Taskbar is horizontal.
+    if (taskbar_rect.bottom() == screen_rect.bottom())
+      return SCREEN_EDGE_BOTTOM;
+    else if (taskbar_rect.y() == screen_rect.y())
+      return SCREEN_EDGE_TOP;
+  } else if (taskbar_rect.height() == display.work_area().height()) {
+    // Taskbar is vertical.
+    if (taskbar_rect.x() == screen_rect.x())
+      return SCREEN_EDGE_LEFT;
+    else if (taskbar_rect.right() == screen_rect.right())
+      return SCREEN_EDGE_RIGHT;
+  }
+
+  return SCREEN_EDGE_UNKNOWN;
+}
+
+gfx::Point FindReferencePoint(const gfx::Display& display,
+                              ScreenEdge taskbar_location,
+                              const gfx::Rect& taskbar_rect,
+                              const gfx::Point& cursor) {
   // Snap to the taskbar edge. If the cursor is greater than kSnapDistance away,
   // also move to the left (for horizontal taskbars) or top (for vertical).
   const gfx::Rect& screen_rect = display.bounds();
-  // First handle taskbar on bottom.
-  // Note on Windows 8 the work area won't include split windows on the left or
-  // right, and neither will |taskbar_rect|.
-  if (taskbar_rect.width() == display.work_area().width()) {
-    if (taskbar_rect.bottom() == screen_rect.bottom()) {
+  switch (taskbar_location) {
+    case SCREEN_EDGE_UNKNOWN:
+      // If we can't find the taskbar, snap to the bottom left.
+      return display.work_area().bottom_left();
+    case SCREEN_EDGE_LEFT:
+      if (cursor.x() - taskbar_rect.right() > kSnapDistance)
+        return gfx::Point(taskbar_rect.right(), screen_rect.y());
+
+      return gfx::Point(taskbar_rect.right(), cursor.y());
+    case SCREEN_EDGE_RIGHT:
+      if (taskbar_rect.x() - cursor.x() > kSnapDistance)
+        return gfx::Point(taskbar_rect.x(), screen_rect.y());
+
+      return gfx::Point(taskbar_rect.x(), cursor.y());
+    case SCREEN_EDGE_TOP:
+      if (cursor.y() - taskbar_rect.bottom() > kSnapDistance)
+        return gfx::Point(screen_rect.x(), taskbar_rect.bottom());
+
+      return gfx::Point(cursor.x(), taskbar_rect.bottom());
+    case SCREEN_EDGE_BOTTOM:
       if (taskbar_rect.y() - cursor.y() > kSnapDistance)
         return gfx::Point(screen_rect.x(), taskbar_rect.y());
 
       return gfx::Point(cursor.x(), taskbar_rect.y());
-    }
-
-    // Now try on the top.
-    if (cursor.y() - taskbar_rect.bottom() > kSnapDistance)
-      return gfx::Point(screen_rect.x(), taskbar_rect.bottom());
-
-    return gfx::Point(cursor.x(), taskbar_rect.bottom());
+    default:
+      NOTREACHED();
+      return gfx::Point();
   }
-
-  // Now try the left.
-  if (taskbar_rect.x() == screen_rect.x()) {
-    if (cursor.x() - taskbar_rect.right() > kSnapDistance)
-      return gfx::Point(taskbar_rect.right(), screen_rect.y());
-
-    return gfx::Point(taskbar_rect.right(), cursor.y());
-  }
-
-  // Finally, try the right.
-  if (taskbar_rect.x() - cursor.x() > kSnapDistance)
-    return gfx::Point(taskbar_rect.x(), screen_rect.y());
-
-  return gfx::Point(taskbar_rect.x(), cursor.y());
 }
 
 gfx::Point FindAnchorPoint(
     const gfx::Size view_size,
     const gfx::Display& display,
     const gfx::Point& cursor) {
-  const int kSnapOffset = 3;
-
   gfx::Rect bounds_rect(display.work_area());
   // Always subtract the taskbar area since work_area() will not subtract it
   // if the taskbar is set to auto-hide, and the app list should never overlap
   // the taskbar.
   gfx::Rect taskbar_rect;
-  if (GetTaskbarRect(&taskbar_rect))
+  ScreenEdge taskbar_edge = SCREEN_EDGE_UNKNOWN;
+  if (GetTaskbarRect(&taskbar_rect)) {
     bounds_rect.Subtract(taskbar_rect);
+    // Find which edge of the screen the taskbar is attached to.
+    taskbar_edge = FindTaskbarEdge(display, taskbar_rect);
+  }
 
   bounds_rect.Inset(view_size.width() / 2 + kSnapOffset,
                     view_size.height() / 2 + kSnapOffset);
 
-  gfx::Point anchor = FindReferencePoint(display, cursor);
+  gfx::Point anchor = FindReferencePoint(display, taskbar_edge, taskbar_rect,
+                                         cursor);
   anchor.SetToMax(bounds_rect.origin());
   anchor.SetToMin(bounds_rect.bottom_right());
   return anchor;
