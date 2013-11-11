be220dfe251727d7911a9b08962914a4565388e4
mcgrathr@chromium.org <mcgrathr@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-11-11 19:26:43 +0000

    toolchain_build: Update gcc revision
    This brings in:
    
    a50e063 Use appropriate DWARF2 EH pointer encoding for PIC
    
    BUG= none
    TEST= toolchain trybots
    R=sehr@chromium.org, mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/69263002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12374 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 8f3b34bb82f386a951f631b76c17de780359adb5

--- toolchain_build/toolchain_build.py
+++ toolchain_build/toolchain_build.py
@@ -16,7 +16,7 @@ import toolchain_main
 
 GIT_REVISIONS = {
     'binutils': '64380341b91b364918b9968c91e70a85cf24b8f6',
-    'gcc': '386e60ba600a7a3682321833d80c5130f6c87c74',
+    'gcc': 'a50e0638be88ed71a3b5017162dd852f4989e410',
     'newlib': '29add55b3da12509058c203d2016e8496131641c',
     }
 
