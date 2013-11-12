7b480a69cc7c9335e78be4d81008942d39aa7b9d
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-12 17:40:32 +0000

    Make media/W3C tests be normal js-tests.
    Right now they do a bunch of extraneous things, print their
    own TEST COMPLETE line  and don't set jsTestIsAsync for
    async tests, which makes them flaky.
    
    BUG=316429, 306249
    
    Review URL: https://codereview.chromium.org/68183005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161833 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c3912983ba643af354fa81d8f087a9ce824bf2b2

--- third_party/WebKit/LayoutTests/media/W3C/w3cwrapper.js
+++ third_party/WebKit/LayoutTests/media/W3C/w3cwrapper.js
@@ -7,55 +7,34 @@ function getAudioURI(dummy) {
   return "../../../content/test.wav";
 }
 
-function test(testFunction) {
-  description(document.title);
-  if (window.testRunner) {
-    testRunner.dumpAsText();
-    testRunner.waitUntilDone();
-  }
+function testStep(testFunction){
   try {
     testFunction();
-  }
-  catch (e) {
+  } catch (e) {
     testFailed('Aborted with exception: ' + e.message);
   }
-  debug('<br /><span class="pass">TEST COMPLETE</span>');
-  if (window.testRunner)
-    testRunner.notifyDone();
+}
+
+function test(testFunction) {
+  description(document.title);
+  testStep(testFunction);
 }
 
 function async_test(title, options) {
+  window.jsTestIsAsync = true;
   description(title);
-  if (window.testRunner) {
-    testRunner.dumpAsText();
-    testRunner.waitUntilDone();
+  return {
+    step: testStep,
+    done: finishJSTest
   }
-  var t = {
-    step: function(testFunction) {
-      try {
-        testFunction();
-      }
-      catch (e) {
-        testFailed('Aborted with exception: ' + e.message);
-      }
-    },
-
-    done: function() {
-      debug('<br /><span class="pass">TEST COMPLETE</span>');
-      if (window.testRunner) {
-        testRunner.notifyDone();
-      }
-    }
-  }
-
-  return t;
 }
 
 document.write("<p id=description></p><div id=console></div>");
-document.write("<scr" + "ipt src='../../../../resources/js-test.js'></" + "script>");
+document.write("<script src='../../../../resources/js-test.js'></" + "script>");
 
 assert_equals = function(a, b) { shouldBe('"' + a + '"', '"' + b + '"'); }
 assert_true = function(a) { shouldBeTrue("" + a); }
 assert_false = function(a) { shouldBeFalse("" + a); }
 
-var successfullyParsed = true;
+if (window.testRunner)
+  testRunner.dumpAsText();
