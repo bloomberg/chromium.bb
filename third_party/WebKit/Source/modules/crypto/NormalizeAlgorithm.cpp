bloomberg/chromium.bb

commit 82b6486d4406400bef2f32342cf591b170978041

Add operation name to SubtleCrypto exception messages.
This patch adds the name of the operation to the generated exception message,
which makes it a bit simpler to figure out which step of a promise chain
ended up erroring off.

It also wraps the error message in 'ExceptionMessages::failedToExecute' for
consistency.

BUG=270033

Review URL: https://codereview.chromium.org/70173002

git-svn-id: svn://svn.chromium.org/blink/trunk@161808 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: ac6debb279d77f8662d794a37e1d8db175f7f542

third_party/WebKit/Source/modules/crypto/NormalizeAlgorithm.cpp
@@ -32,6 +32,7 @@
 #include "modules/crypto/NormalizeAlgorithm.h"
 
 #include "bindings/v8/Dictionary.h"
+#include "bindings/v8/ExceptionMessages.h"
 #include "bindings/v8/ExceptionState.h"
 #include "core/dom/ExceptionCode.h"
 #include "platform/NotImplemented.h"
@@ -189,6 +190,11 @@ AlgorithmRegistry::AlgorithmRegistry()
 // tell what went wrong from the exception type alone (TypeError).
 class ExceptionContext {
 public:
+    explicit ExceptionContext(AlgorithmOperation op)
+        : m_op(op)
+    {
+    }
+
     void add(const char* message)
     {
         m_messages.append(message);
@@ -214,7 +220,7 @@ public:
             result.append(m_messages[i], strlen(m_messages[i]));
         }
 
-        return result.toString();
+        return ExceptionMessages::failedToExecute(algorithmOperationToName(m_op), "SubtleCrypto", result.toString());
     }
 
     String toString(const char* message) const
@@ -233,6 +239,8 @@ public:
     }
 
 private:
+    AlgorithmOperation m_op;
+
     // This inline size is large enough to avoid having to grow the Vector in
     // the majority of cases (up to 1 nested algorithm identifier).
     Vector<const char*, 10> m_messages;
@@ -496,12 +504,43 @@ bool normalizeAlgorithm(const Dictionary& raw, AlgorithmOperation op, blink::Web
 
 bool normalizeAlgorithm(const Dictionary& raw, AlgorithmOperation op, blink::WebCryptoAlgorithm& algorithm, ExceptionState& es)
 {
-    return normalizeAlgorithm(raw, op, algorithm, ExceptionContext(), es);
+    return normalizeAlgorithm(raw, op, algorithm, ExceptionContext(op), es);
 }
 
 const char* algorithmIdToName(blink::WebCryptoAlgorithmId id)
 {
     return AlgorithmRegistry::instance().lookupAlgorithmById(id)->algorithmName;
+}
+
+const char* algorithmOperationToName(AlgorithmOperation op)
+{
+    switch (op) {
+    case Encrypt:
+        return "encrypt";
+    case Decrypt:
+        return "decrypt";
+    case Sign:
+        return "sign";
+    case Verify:
+        return "verify";
+    case Digest:
+        return "digest";
+    case GenerateKey:
+        return "generateKey";
+    case ImportKey:
+        return "generateKey";
+    case DeriveKey:
+        return "deriveKey";
+    case WrapKey:
+        return "wrapKey";
+    case UnwrapKey:
+        return "unwrapKey";
+    case NumberOfAlgorithmOperations:
+        ASSERT_NOT_REACHED();
+        return "unknown";
+    };
+    ASSERT_NOT_REACHED();
+    return "unknown";
 }
 
 } // namespace WebCore

third_party/WebKit/LayoutTests/crypto/importKey-expected.txt
@@ -23,8 +23,8 @@ PASS crypto.subtle.importKey(keyFormat, data, algorithm, extractable, ['SIGN']) 
 PASS crypto.subtle.importKey('invalid format', data, algorithm, extractable, ['SIGN']) threw exception TypeError: Invalid keyFormat argument.
 PASS crypto.subtle.importKey(keyFormat, [], algorithm, extractable, keyUsages) threw exception TypeError: Invalid keyData argument.
 PASS crypto.subtle.importKey(keyFormat, null, algorithm, extractable, keyUsages) threw exception TypeError: Invalid keyData argument.
-PASS crypto.subtle.importKey(keyFormat, data, {name: 'hmac'}, extractable, keyUsages) threw exception TypeError: Algorithm: HMAC: HmacParams: hash: Missing or not a dictionary.
-PASS crypto.subtle.importKey(keyFormat, data, {name: 'sha-1'}, extractable, keyUsages) threw exception NotSupportedError: Algorithm: SHA-1: Unsupported operation.
+PASS crypto.subtle.importKey(keyFormat, data, {name: 'hmac'}, extractable, keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: HMAC: HmacParams: hash: Missing or not a dictionary.
+PASS crypto.subtle.importKey(keyFormat, data, {name: 'sha-1'}, extractable, keyUsages) threw exception NotSupportedError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: SHA-1: Unsupported operation.
 PASS successfullyParsed is true
 
 TEST COMPLETE

third_party/WebKit/LayoutTests/crypto/digest-expected.txt
@@ -6,9 +6,9 @@ On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE
 PASS crypto.subtle.digest({name: 'sha-1'}) threw exception TypeError: Failed to execute 'digest' on 'SubtleCrypto': 2 arguments required, but only 1 present..
 PASS crypto.subtle.digest({name: 'sha-1'}, null) threw exception TypeError: Invalid dataBuffer argument.
 PASS crypto.subtle.digest({name: 'sha-1'}, 10) threw exception TypeError: Invalid dataBuffer argument.
-PASS crypto.subtle.digest({name: 'sha'}, data) threw exception NotSupportedError: Algorithm: Unrecognized algorithm name.
-PASS crypto.subtle.digest(null, data) threw exception TypeError: Algorithm: Not an object.
-PASS crypto.subtle.digest({}, data) threw exception TypeError: Algorithm: name: Missing or not a string.
+PASS crypto.subtle.digest({name: 'sha'}, data) threw exception NotSupportedError: Failed to execute 'digest' on 'SubtleCrypto': Algorithm: Unrecognized algorithm name.
+PASS crypto.subtle.digest(null, data) threw exception TypeError: Failed to execute 'digest' on 'SubtleCrypto': Algorithm: Not an object.
+PASS crypto.subtle.digest({}, data) threw exception TypeError: Failed to execute 'digest' on 'SubtleCrypto': Algorithm: name: Missing or not a string.
 PASS: SHA-1 of [] should be [da39a3ee5e6b4b0d3255bfef95601890afd80709] and was
 PASS: SHA-224 of [] should be [d14a028c2a3a2bc9476102bb288234c415a2b01f828ea62ac5b3e42f] and was
 PASS: SHA-256 of [] should be [e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855] and was

third_party/WebKit/LayoutTests/crypto/encrypt-decrypt-expected.txt
@@ -44,9 +44,9 @@ PASS: Decryption should be [6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb7
 PASS: Decryption should be [6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411e5fbc1191a0a52eff6] and was
 PASS: Decryption should be [6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411e5fbc1191a0a52eff69f2445df4f9b17ad2b417be6] and was
 PASS: Decryption should be [] and was
-PASS crypto.subtle.encrypt({name: 'AES-CBC', iv: null}, keys.aesCbc, data) threw exception TypeError: Algorithm: AES-CBC: AesCbcParams: iv: Missing or not a ArrayBufferView.
-PASS crypto.subtle.encrypt({name: 'AES-CBC'}, keys.aesCbc, data) threw exception TypeError: Algorithm: AES-CBC: AesCbcParams: iv: Missing or not a ArrayBufferView.
-PASS crypto.subtle.encrypt({name: 'AES-CBC', iv: 3}, keys.aesCbc, data) threw exception TypeError: Algorithm: AES-CBC: AesCbcParams: iv: Missing or not a ArrayBufferView.
+PASS crypto.subtle.encrypt({name: 'AES-CBC', iv: null}, keys.aesCbc, data) threw exception TypeError: Failed to execute 'encrypt' on 'SubtleCrypto': Algorithm: AES-CBC: AesCbcParams: iv: Missing or not a ArrayBufferView.
+PASS crypto.subtle.encrypt({name: 'AES-CBC'}, keys.aesCbc, data) threw exception TypeError: Failed to execute 'encrypt' on 'SubtleCrypto': Algorithm: AES-CBC: AesCbcParams: iv: Missing or not a ArrayBufferView.
+PASS crypto.subtle.encrypt({name: 'AES-CBC', iv: 3}, keys.aesCbc, data) threw exception TypeError: Failed to execute 'encrypt' on 'SubtleCrypto': Algorithm: AES-CBC: AesCbcParams: iv: Missing or not a ArrayBufferView.
 PASS crypto.subtle.encrypt({name: 'AES-CBC', iv: new Uint8Array[0]}, keys.aesCbc, data) threw exception TypeError: undefined is not a function.
 PASS crypto.subtle.encrypt(aesCbc, keys.hmacSha1, data) threw exception NotSupportedError: key.algorithm does not match that of operation.
 PASS crypto.subtle.encrypt(aesCbc, keys.aesCbcJustDecrypt, data) threw exception NotSupportedError: key.usages does not permit this operation.

third_party/WebKit/LayoutTests/crypto/generateKey-expected.txt
@@ -6,33 +6,33 @@ On success, you will see a series of "PASS" messages, followed by "TEST COMPLETE
 PASS crypto.subtle.generateKey(aesCbc, extractable, -1) threw exception TypeError: The 3rd argument is neither an array, nor does it have indexed properties..
 PASS crypto.subtle.generateKey(aesCbc, extractable, null) threw exception TypeError: The 3rd argument is neither an array, nor does it have indexed properties..
 PASS crypto.subtle.generateKey(aesCbc, extractable, ['boo']) threw exception TypeError: Invalid keyUsages argument.
-PASS crypto.subtle.generateKey({ name: 'aes-cbc' }, extractable, keyUsages) threw exception TypeError: Algorithm: AES-CBC: AesKeyGenParams: length: Missing required property.
-PASS crypto.subtle.generateKey({ name: 'aes-cbc', length: 70000 }, extractable, keyUsages) threw exception TypeError: Algorithm: AES-CBC: AesKeyGenParams: length: Outside of numeric range.
-PASS crypto.subtle.generateKey({ name: 'aes-cbc', length: -3 }, extractable, keyUsages) threw exception TypeError: Algorithm: AES-CBC: AesKeyGenParams: length: Outside of numeric range.
-PASS crypto.subtle.generateKey({ name: 'aes-cbc', length: -Infinity }, extractable, keyUsages) threw exception TypeError: Algorithm: AES-CBC: AesKeyGenParams: length: Outside of numeric range.
-PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: -3}, extractable , keyUsages) threw exception TypeError: Algorithm: HMAC: HmacKeyParams: length: Outside of numeric range.
-PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: ''}, length: 48}, extractable , keyUsages) threw exception NotSupportedError: Algorithm: HMAC: HmacKeyParams: hash: Algorithm: Unrecognized algorithm name.
-PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: 5000000000}, extractable , keyUsages) threw exception TypeError: Algorithm: HMAC: HmacKeyParams: length: Outside of numeric range.
-PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: NaN}, extractable , keyUsages) threw exception TypeError: Algorithm: HMAC: HmacKeyParams: length: Is not a number.
-PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: Infinity}, extractable , keyUsages) threw exception TypeError: Algorithm: HMAC: HmacKeyParams: length: Outside of numeric range.
-PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: -Infinity}, extractable , keyUsages) threw exception TypeError: Algorithm: HMAC: HmacKeyParams: length: Outside of numeric range.
-PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: crypto}, extractable , keyUsages) threw exception TypeError: Algorithm: HMAC: HmacKeyParams: length: Is not a number.
-PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: undefined}, extractable , keyUsages) threw exception TypeError: Algorithm: HMAC: HmacKeyParams: length: Is not a number.
+PASS crypto.subtle.generateKey({ name: 'aes-cbc' }, extractable, keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: AES-CBC: AesKeyGenParams: length: Missing required property.
+PASS crypto.subtle.generateKey({ name: 'aes-cbc', length: 70000 }, extractable, keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: AES-CBC: AesKeyGenParams: length: Outside of numeric range.
+PASS crypto.subtle.generateKey({ name: 'aes-cbc', length: -3 }, extractable, keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: AES-CBC: AesKeyGenParams: length: Outside of numeric range.
+PASS crypto.subtle.generateKey({ name: 'aes-cbc', length: -Infinity }, extractable, keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: AES-CBC: AesKeyGenParams: length: Outside of numeric range.
+PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: -3}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: HMAC: HmacKeyParams: length: Outside of numeric range.
+PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: ''}, length: 48}, extractable , keyUsages) threw exception NotSupportedError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: HMAC: HmacKeyParams: hash: Algorithm: Unrecognized algorithm name.
+PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: 5000000000}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: HMAC: HmacKeyParams: length: Outside of numeric range.
+PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: NaN}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: HMAC: HmacKeyParams: length: Is not a number.
+PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: Infinity}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: HMAC: HmacKeyParams: length: Outside of numeric range.
+PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: -Infinity}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: HMAC: HmacKeyParams: length: Outside of numeric range.
+PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: crypto}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: HMAC: HmacKeyParams: length: Is not a number.
+PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: undefined}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: HMAC: HmacKeyParams: length: Is not a number.
 PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: '48'}, extractable , keyUsages) did not throw exception.
 PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: null}, extractable , keyUsages) did not throw exception.
 PASS crypto.subtle.generateKey({name: 'hmac', hash: {name: 'sha-256'}, length: true}, extractable , keyUsages) did not throw exception.
-PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5', modulusLength: -30}, extractable , keyUsages) threw exception TypeError: Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Outside of numeric range.
-PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5', modulusLength: NaN}, extractable , keyUsages) threw exception TypeError: Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Is not a number.
-PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5'}, extractable , keyUsages) threw exception TypeError: Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Missing required property.
-PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5', modulusLength: 10}, extractable , keyUsages) threw exception TypeError: Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
-PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5', modulusLength: 10, publicExponent: 10}, extractable , keyUsages) threw exception TypeError: Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
-PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5', modulusLength: 10, publicExponent: null}, extractable , keyUsages) threw exception TypeError: Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
-PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5', modulusLength: -30}, extractable , keyUsages) threw exception TypeError: Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Outside of numeric range.
-PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5', modulusLength: NaN}, extractable , keyUsages) threw exception TypeError: Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Is not a number.
-PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5'}, extractable , keyUsages) threw exception TypeError: Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Missing required property.
-PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5', modulusLength: 10}, extractable , keyUsages) threw exception TypeError: Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
-PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5', modulusLength: 10, publicExponent: 10}, extractable , keyUsages) threw exception TypeError: Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
-PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5', modulusLength: 10, publicExponent: null}, extractable , keyUsages) threw exception TypeError: Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
+PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5', modulusLength: -30}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Outside of numeric range.
+PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5', modulusLength: NaN}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Is not a number.
+PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5'}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Missing required property.
+PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5', modulusLength: 10}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
+PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5', modulusLength: 10, publicExponent: 10}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
+PASS crypto.subtle.generateKey({name: 'RSASSA-PKCS1-v1_5', modulusLength: 10, publicExponent: null}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSASSA-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
+PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5', modulusLength: -30}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Outside of numeric range.
+PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5', modulusLength: NaN}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Is not a number.
+PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5'}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: modulusLength: Missing required property.
+PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5', modulusLength: 10}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
+PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5', modulusLength: 10, publicExponent: 10}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
+PASS crypto.subtle.generateKey({name: 'RSAES-PKCS1-v1_5', modulusLength: 10, publicExponent: null}, extractable , keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: RSAES-PKCS1-v1_5: RsaKeyGenParams: publicExponent: Missing or not a Uint8Array.
 PASS successfullyParsed is true
 
 TEST COMPLETE

third_party/WebKit/LayoutTests/crypto/sign-verify-expected.txt
@@ -54,15 +54,15 @@ PASS verifyResult is false
 PASS crypto.subtle.verify(hmacSha1, keys.hmacSha1, null, data) threw exception TypeError: Invalid signature argument.
 PASS crypto.subtle.verify(hmacSha1, keys.hmacSha1, 'a', data) threw exception TypeError: Invalid signature argument.
 PASS crypto.subtle.verify(hmacSha1, keys.hmacSha1, [], data) threw exception TypeError: Invalid signature argument.
-PASS crypto.subtle.sign({name: 'sha-1'}, keys.hmacSha1, data) threw exception NotSupportedError: Algorithm: SHA-1: Unsupported operation.
-PASS crypto.subtle.sign({name: 'RSAES-PKCS1-v1_5'}, keys.hmacSha1, data) threw exception NotSupportedError: Algorithm: RSAES-PKCS1-v1_5: Unsupported operation.
+PASS crypto.subtle.sign({name: 'sha-1'}, keys.hmacSha1, data) threw exception NotSupportedError: Failed to execute 'sign' on 'SubtleCrypto': Algorithm: SHA-1: Unsupported operation.
+PASS crypto.subtle.sign({name: 'RSAES-PKCS1-v1_5'}, keys.hmacSha1, data) threw exception NotSupportedError: Failed to execute 'sign' on 'SubtleCrypto': Algorithm: RSAES-PKCS1-v1_5: Unsupported operation.
 PASS crypto.subtle.sign({name: 'hmac', hash: {name: 'sha-256'}}, keys.hmacSha1, data) threw exception NotSupportedError: key.algorithm does not match that of operation (HMAC's hash differs).
-PASS crypto.subtle.sign({name: 'hmac'}, keys.hmacSha1, data) threw exception TypeError: Algorithm: HMAC: HmacParams: hash: Missing or not a dictionary.
-PASS crypto.subtle.sign({name: 'hmac', hash: 3}, keys.hmacSha1, data) threw exception TypeError: Algorithm: HMAC: HmacParams: hash: Algorithm: Not an object.
-PASS crypto.subtle.sign({name: 'hmac', hash: null}, keys.hmacSha1, data) threw exception TypeError: Algorithm: HMAC: HmacParams: hash: Algorithm: Not an object.
-PASS crypto.subtle.sign({name: 'hmac', hash: {}}, keys.hmacSha1, data) threw exception TypeError: Algorithm: HMAC: HmacParams: hash: Algorithm: name: Missing or not a string.
-PASS crypto.subtle.sign({name: 'hmac', hash: {name: 'foo'}}, keys.hmacSha1, data) threw exception NotSupportedError: Algorithm: HMAC: HmacParams: hash: Algorithm: Unrecognized algorithm name.
-PASS crypto.subtle.sign({name: 'hmac', hash: {name: 'AES-CBC'}}, keys.hmacSha1, data) threw exception NotSupportedError: Algorithm: HMAC: HmacParams: hash: Algorithm: AES-CBC: Unsupported operation.
+PASS crypto.subtle.sign({name: 'hmac'}, keys.hmacSha1, data) threw exception TypeError: Failed to execute 'sign' on 'SubtleCrypto': Algorithm: HMAC: HmacParams: hash: Missing or not a dictionary.
+PASS crypto.subtle.sign({name: 'hmac', hash: 3}, keys.hmacSha1, data) threw exception TypeError: Failed to execute 'sign' on 'SubtleCrypto': Algorithm: HMAC: HmacParams: hash: Algorithm: Not an object.
+PASS crypto.subtle.sign({name: 'hmac', hash: null}, keys.hmacSha1, data) threw exception TypeError: Failed to execute 'sign' on 'SubtleCrypto': Algorithm: HMAC: HmacParams: hash: Algorithm: Not an object.
+PASS crypto.subtle.sign({name: 'hmac', hash: {}}, keys.hmacSha1, data) threw exception TypeError: Failed to execute 'sign' on 'SubtleCrypto': Algorithm: HMAC: HmacParams: hash: Algorithm: name: Missing or not a string.
+PASS crypto.subtle.sign({name: 'hmac', hash: {name: 'foo'}}, keys.hmacSha1, data) threw exception NotSupportedError: Failed to execute 'sign' on 'SubtleCrypto': Algorithm: HMAC: HmacParams: hash: Algorithm: Unrecognized algorithm name.
+PASS crypto.subtle.sign({name: 'hmac', hash: {name: 'AES-CBC'}}, keys.hmacSha1, data) threw exception NotSupportedError: Failed to execute 'sign' on 'SubtleCrypto': Algorithm: HMAC: HmacParams: hash: Algorithm: AES-CBC: Unsupported operation.
 PASS successfullyParsed is true
 
 TEST COMPLETE

third_party/WebKit/Source/modules/crypto/NormalizeAlgorithm.h
@@ -64,6 +64,10 @@ bool normalizeAlgorithm(const Dictionary&, AlgorithmOperation, blink::WebCryptoA
 // will be valid for the program's entire runtime.
 const char* algorithmIdToName(blink::WebCryptoAlgorithmId);
 
+// Returns a null-terminated C-string literal. Caller can assume the pointer
+// will be valid for the program's entire runtime.
+const char* algorithmOperationToName(AlgorithmOperation);
+
 } // namespace WebCore
 
 #endif

commit 5840ba351d6efeb878e3a2d94fb7f01766e73a93

Fix exception message for SubtleCrypto importKey to say "importKey".
This bug was introduced in r161808:
https://codereview.chromium.org/70173002/

BUG=270033

Review URL: https://codereview.chromium.org/68773008

git-svn-id: svn://svn.chromium.org/blink/trunk@161827 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: a282f4446b06ebf0dfe7d5f8e1c8238e92d3c18a

third_party/WebKit/Source/modules/crypto/NormalizeAlgorithm.cpp
@@ -528,7 +528,7 @@ const char* algorithmOperationToName(AlgorithmOperation op)
     case GenerateKey:
         return "generateKey";
     case ImportKey:
-        return "generateKey";
+        return "importKey";
     case DeriveKey:
         return "deriveKey";
     case WrapKey:

third_party/WebKit/LayoutTests/crypto/importKey-expected.txt
@@ -23,8 +23,8 @@ PASS crypto.subtle.importKey(keyFormat, data, algorithm, extractable, ['SIGN']) 
 PASS crypto.subtle.importKey('invalid format', data, algorithm, extractable, ['SIGN']) threw exception TypeError: Invalid keyFormat argument.
 PASS crypto.subtle.importKey(keyFormat, [], algorithm, extractable, keyUsages) threw exception TypeError: Invalid keyData argument.
 PASS crypto.subtle.importKey(keyFormat, null, algorithm, extractable, keyUsages) threw exception TypeError: Invalid keyData argument.
-PASS crypto.subtle.importKey(keyFormat, data, {name: 'hmac'}, extractable, keyUsages) threw exception TypeError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: HMAC: HmacParams: hash: Missing or not a dictionary.
-PASS crypto.subtle.importKey(keyFormat, data, {name: 'sha-1'}, extractable, keyUsages) threw exception NotSupportedError: Failed to execute 'generateKey' on 'SubtleCrypto': Algorithm: SHA-1: Unsupported operation.
+PASS crypto.subtle.importKey(keyFormat, data, {name: 'hmac'}, extractable, keyUsages) threw exception TypeError: Failed to execute 'importKey' on 'SubtleCrypto': Algorithm: HMAC: HmacParams: hash: Missing or not a dictionary.
+PASS crypto.subtle.importKey(keyFormat, data, {name: 'sha-1'}, extractable, keyUsages) threw exception NotSupportedError: Failed to execute 'importKey' on 'SubtleCrypto': Algorithm: SHA-1: Unsupported operation.
 PASS successfullyParsed is true
 
 TEST COMPLETE