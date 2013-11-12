fab48bd681d659ce6f51d2fa4a1a23c9a2a850f3
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-12 06:11:51 +0000

    cc: Make PictureLayerImpl::AsValue emit unrecorded region
    In retrospect, the unrecorded region is more useful to visualize.  Doing
    this in C++ means that JavaScript doesn't have to do the region
    subtraction.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/69253003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234429 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0800a15614f173fdb17f6dbf7b03607de2d8e374

--- cc/layers/picture_layer_impl.cc
+++ cc/layers/picture_layer_impl.cc
@@ -1065,7 +1065,11 @@ void PictureLayerImpl::AsValueInto(base::DictionaryValue* state) const {
   state->Set("tilings", tilings_->AsValue().release());
   state->Set("pictures", pile_->AsValue().release());
   state->Set("invalidation", invalidation_.AsValue().release());
-  state->Set("recorded_region", pile_->recorded_region().AsValue().release());
+
+  Region unrecorded_region(gfx::Rect(pile_->size()));
+  unrecorded_region.Subtract(pile_->recorded_region());
+  if (!unrecorded_region.IsEmpty())
+    state->Set("unrecorded_region", unrecorded_region.AsValue().release());
 
   scoped_ptr<base::ListValue> coverage_tiles(new base::ListValue);
   for (PictureLayerTilingSet::CoverageIterator iter(tilings_.get(),
