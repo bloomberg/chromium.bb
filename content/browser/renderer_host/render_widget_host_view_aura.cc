9ec1f7a6afafb0a63238a799ced16578bfb5452d
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 15:48:19 +0000

    When DWM compositing is disabled (i.e. XP, RDP) ensure that windowed NPAPI plugins don't cover UI dialogs. This includes things like omnibox, star window, profile selector, and bookmark editor.
    The solution is to start watching the root window as well as the parent window.
    BUG=299224
    R=ben@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=234708
    
    Review URL: https://codereview.chromium.org/69833002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234823 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9702820616f52aff8d22dc15e6f25cb86dcc90b2

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -1254,6 +1254,8 @@ void RenderWidgetHostViewAura::DidReceiveFrameFromRenderer() {
 #if defined(OS_WIN)
 void RenderWidgetHostViewAura::UpdateConstrainedWindowRects(
     const std::vector<gfx::Rect>& rects) {
+  if (rects == constrained_rects_)
+    return;
   constrained_rects_ = rects;
   UpdateCutoutRects();
 }
