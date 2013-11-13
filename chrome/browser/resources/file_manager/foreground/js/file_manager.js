fbb6a767978355061530ca064dc32dc23203c93d
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 20:24:27 +0000

    [Files.app] Show files beginning with '.' (dot)
    BUG=196539
    TEST=manually tested
    R=hirono@chromium.org
    
    Review URL: https://codereview.chromium.org/71503003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234889 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3ceebbc437850dd1ff29d03c138d825097491b9e

--- chrome/browser/resources/file_manager/foreground/js/file_manager.js
+++ chrome/browser/resources/file_manager/foreground/js/file_manager.js
@@ -907,7 +907,7 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
 
     this.fileFilter_ = new FileFilter(
         this.metadataCache_,
-        false  /* Don't show dot files by default. */);
+        true /* Show dot files by default. */);
 
     this.fileWatcher_ = new FileWatcher(this.metadataCache_);
     this.fileWatcher_.addEventListener(
