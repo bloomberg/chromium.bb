865b3d8d739ec68dbc720ecf7ea31ad35242f263
hfung@chromium.org <hfung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 01:30:17 +0000

    Try to fix flaky test TemplateURLTest.ReplacePageClassification
    BUG=316385
    
    Review URL: https://codereview.chromium.org/70163003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234698 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 57ac99b92afe5b8870d4d0022cfae8f16b442e65

--- chrome/browser/search_engines/template_url_unittest.cc
+++ chrome/browser/search_engines/template_url_unittest.cc
@@ -1208,12 +1208,8 @@ TEST_F(TemplateURLTest, ExtraQueryParams) {
 }
 
 // Tests replacing pageClassification.
-#if defined(OS_ANDROID)
-#define MAYBE_ReplacePageClassification DISABLED_ReplacePageClassification
-#else
-#define MAYBE_ReplacePageClassification ReplacePageClassification
-#endif
-TEST_F(TemplateURLTest, MAYBE_ReplacePageClassification) {
+TEST_F(TemplateURLTest, ReplacePageClassification) {
+  UIThreadSearchTermsData::SetGoogleBaseURL("http://www.google.com/");
   TemplateURLData data;
   data.input_encodings.push_back("UTF-8");
   data.SetURL("{google:baseURL}?{google:pageClassification}q={searchTerms}");
