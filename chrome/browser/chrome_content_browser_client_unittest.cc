e3a1960fd5129ea6ed241310578ac60a62cdc19e
samarth@chromium.org <samarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-13 23:34:48 +0000

    InstantExtended: update tests for Instant Extended (part 5).
    Turning on InstantExtended by default exposes some flakiness in the url rewrite
    browser test. Change the test to a unit test to remove flakiness. Also update
    OWNERS file to allow anyone to change the unit test (since anyone can already
    change the .cc/.h files + the browser tests).
    
    (Note that this is split off from http://crrev.com/24733003.)
    
    BUG=297915
    R=jered@chromium.org, sky@chromium.org
    
    Review URL: https://codereview.chromium.org/59493009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@234956 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 10a9bf9be363cf169ce304ec2a61f1e99b24c234

--- chrome/browser/chrome_content_browser_client_unittest.cc
+++ chrome/browser/chrome_content_browser_client_unittest.cc
@@ -4,6 +4,17 @@
 
 #include "chrome/browser/chrome_content_browser_client.h"
 
+#include "base/metrics/field_trial.h"
+#include "chrome/browser/search_engines/template_url_service.h"
+#include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/test/base/browser_with_test_window_test.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "components/variations/entropy_provider.h"
+#include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/navigation_entry.h"
+#include "content/public/browser/web_contents.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
@@ -20,3 +31,53 @@ TEST_F(ChromeContentBrowserClientTest, ShouldAssignSiteForURL) {
 }
 
 }  // namespace chrome
+
+#if !defined(OS_IOS) && !defined(OS_ANDROID)
+namespace content {
+
+class InstantNTPURLRewriteTest : public BrowserWithTestWindowTest {
+ protected:
+  virtual void SetUp() OVERRIDE {
+    BrowserWithTestWindowTest::SetUp();
+    field_trial_list_.reset(new base::FieldTrialList(
+        new metrics::SHA1EntropyProvider("42")));
+  }
+
+  void InstallTemplateURLWithNewTabPage(GURL new_tab_page_url) {
+    TemplateURLServiceFactory::GetInstance()->SetTestingFactoryAndUse(
+        profile(), &TemplateURLServiceFactory::BuildInstanceFor);
+    TemplateURLService* template_url_service =
+        TemplateURLServiceFactory::GetForProfile(browser()->profile());
+    ui_test_utils::WaitForTemplateURLServiceToLoad(template_url_service);
+
+    TemplateURLData data;
+    data.SetURL("http://foo.com/url?bar={searchTerms}");
+    data.new_tab_url = new_tab_page_url.spec();
+    TemplateURL* template_url = new TemplateURL(browser()->profile(), data);
+    // Takes ownership.
+    template_url_service->Add(template_url);
+    template_url_service->SetDefaultSearchProvider(template_url);
+  }
+
+  scoped_ptr<base::FieldTrialList> field_trial_list_;
+};
+
+TEST_F(InstantNTPURLRewriteTest, UberURLHandler_InstantExtendedNewTabPage) {
+  const GURL url_original("chrome://newtab");
+  const GURL url_rewritten("https://www.example.com/newtab");
+  InstallTemplateURLWithNewTabPage(url_rewritten);
+  ASSERT_TRUE(base::FieldTrialList::CreateFieldTrial("InstantExtended",
+      "Group1 use_cacheable_ntp:1"));
+
+  AddTab(browser(), GURL("chrome://blank"));
+  NavigateAndCommitActiveTab(url_original);
+
+  NavigationEntry* entry = browser()->tab_strip_model()->
+      GetActiveWebContents()->GetController().GetLastCommittedEntry();
+  ASSERT_TRUE(entry != NULL);
+  EXPECT_EQ(url_rewritten, entry->GetURL());
+  EXPECT_EQ(url_original, entry->GetVirtualURL());
+}
+
+}  // namespace content
+#endif  // !defined(OS_IOS) && !defined(OS_ANDROID)
