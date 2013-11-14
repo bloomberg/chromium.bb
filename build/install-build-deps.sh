5556bb247ee96dc5b29d9b960b9a69df9f5513ba
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-14 20:41:41 +0000

    Make install-build-deps.sh install xfonts-mathml.
    This provides the Symbol font, needed for tests.
    
    BUG=316955
    
    Review URL: https://codereview.chromium.org/72713004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235226 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b61f68846528218181f0aa0a0202cff5ddcedbcf

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -82,20 +82,19 @@ fi
 # Packages needed for chromeos only
 chromeos_dev_list="libbluetooth-dev"
 
-# Packages need for development
-dev_list="apache2.2-bin bison curl elfutils fakeroot flex g++ gperf
-          language-pack-fr libapache2-mod-php5 libasound2-dev
-          libbrlapi-dev libbz2-dev libcairo2-dev libcap-dev libcups2-dev
-          libcurl4-gnutls-dev libelf-dev libgconf2-dev libgl1-mesa-dev
-          libglib2.0-dev libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev
-          libkrb5-dev libnspr4-dev libnss3-dev libpam0g-dev libpci-dev
-          libpulse-dev libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev
-          libudev-dev libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
+# Packages needed for development
+dev_list="apache2.2-bin bison curl elfutils fakeroot flex g++ git-core gperf
+          language-pack-fr libapache2-mod-php5 libasound2-dev libbrlapi-dev
+          libbz2-dev libcairo2-dev libcap-dev libcups2-dev libcurl4-gnutls-dev
+          libdrm-dev libelf-dev libgconf2-dev libgl1-mesa-dev libglib2.0-dev
+          libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev libkrb5-dev
+          libnspr4-dev libnss3-dev libpam0g-dev libpci-dev libpulse-dev
+          libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev libudev-dev
+          libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
           mesa-common-dev openbox patch perl php5-cgi pkg-config python
           python-cherrypy3 python-dev python-psutil rpm ruby subversion
           ttf-dejavu-core ttf-indic-fonts ttf-kochi-gothic ttf-kochi-mincho
-          ttf-thai-tlwg wdiff git-core libdrm-dev
-          $chromeos_dev_list"
+          ttf-thai-tlwg wdiff xfonts-mathml $chromeos_dev_list"
 
 # 64-bit systems need a minimum set of 32-bit compat packages for the pre-built
 # NaCl binaries. These are always needed, regardless of whether or not we want
