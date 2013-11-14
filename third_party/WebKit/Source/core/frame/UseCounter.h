34c0e76fd468edcd51ae49b0e11e6f8ae05a0ebb
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-14 18:03:22 +0000

    Add a UseCounter for HTMLFrameElement.location
    It is not supported by IE, Firefox or Presto and can likely be removed.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/59713011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162036 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 4650f87b80e1d96254e6d5c495cde7d55c3bac5b

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -256,6 +256,7 @@ public:
         SVGInstanceRoot,
         ShowModalDialog,
         PrefixedPageVisibility,
+        HTMLFrameElementLocation,
         // Add new features immediately above this line. Don't change assigned
         // numbers of each items, and don't reuse unused slots.
         NumberOfFeatures, // This enum value must be last.
