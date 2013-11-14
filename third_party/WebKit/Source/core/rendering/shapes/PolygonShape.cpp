6137a6aadad6373b0052bfcd04f67f8f1e7eb32c
hmuller@adobe.com <hmuller@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-14 06:38:57 +0000

    [CSS Shapes] Empty polygons with non-zero shape-padding cause an ASSERT crash
    PolygonShape::shapePaddingBounds() and PolygonShape::shapeMarginBounds() no longer attempt to compute a new FloatPolygon when the original is empty, i.e. when it has less than three vertices.
    
    This patch fixes the clusterfuzz failure reported here: https://code.google.com/p/chromium/issues/detail?id=314402
    
    BUG=319069, 314402
    
    Review URL: https://codereview.chromium.org/68263018
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@161996 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: bd3c9f5a21ab4416c8fb90793621b8fbefdeee18

--- third_party/WebKit/Source/core/rendering/shapes/PolygonShape.cpp
+++ third_party/WebKit/Source/core/rendering/shapes/PolygonShape.cpp
@@ -182,7 +182,7 @@ static inline PassOwnPtr<FloatPolygon> computeShapeMarginBounds(const FloatPolyg
 const FloatPolygon& PolygonShape::shapePaddingBounds() const
 {
     ASSERT(shapePadding() >= 0);
-    if (!shapePadding())
+    if (!shapePadding() || m_polygon.isEmpty())
         return m_polygon;
 
     if (!m_paddingBounds)
@@ -194,7 +194,7 @@ const FloatPolygon& PolygonShape::shapePaddingBounds() const
 const FloatPolygon& PolygonShape::shapeMarginBounds() const
 {
     ASSERT(shapeMargin() >= 0);
-    if (!shapeMargin())
+    if (!shapeMargin() || m_polygon.isEmpty())
         return m_polygon;
 
     if (!m_marginBounds)
