a1ecd471a27f8d5b45eb5df66c1aa1ef7b25460c
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-15 00:52:31 +0000

    Fix crash in WebContentsViewAura::WindowObserver that happens when switching tabs. The exact repro steps were:
    1) open an empty page
    2) print preview
    3) open a new tab
    4) go back to original tab
    5) close print preview
    6) switch to the new tab
    
    The crash happened at step 4 because the second tab got removed from the same root window as the print preview's observer. We weren't unobserving all the children of the root window at that point.
    
    BUG=318791
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/69813005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235267 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 42971407d2af08e7165a4926ef5848e35fc5cc05

--- chrome/browser/ui/webui/print_preview/print_preview_ui_browsertest.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_ui_browsertest.cc
@@ -141,6 +141,21 @@ IN_PROC_BROWSER_TEST_F(PrintPreviewTest, WindowedNPAPIPluginHidden) {
       region_before.bottom == region_after.bottom;
   ASSERT_FALSE(rects_equal);
 }
+
+IN_PROC_BROWSER_TEST_F(PrintPreviewTest, NoCrashOnCloseWithOtherTabs) {
+  Print();
+
+  ui_test_utils::NavigateToURLWithDisposition(
+      browser(), GURL("about:blank"), NEW_FOREGROUND_TAB,
+      ui_test_utils::BROWSER_TEST_WAIT_FOR_NAVIGATION);
+
+  browser()->tab_strip_model()->ActivateTabAt(0, true);
+
+  // Navigate main tab to hide print preview.
+  ui_test_utils::NavigateToURL(browser(), GURL("about:blank"));
+
+  browser()->tab_strip_model()->ActivateTabAt(1, true);
+}
 #endif
 
 }  // namespace
