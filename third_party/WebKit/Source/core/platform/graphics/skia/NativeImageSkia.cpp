4bb737fcabace31973529e3d56567a555c814321
tfarina@chromium.org <tfarina@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-15 05:36:53 +0000

    getConfig() is deprecated, simply use config().
    BUG=None
    TEST=None, no functional changes
    R=senorblanco@chromium.org
    
    Review URL: https://codereview.chromium.org/61933004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162064 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 4d00ada6597cb4b79ea6db4f1de6e5bccd4694af

--- third_party/WebKit/Source/core/platform/graphics/skia/NativeImageSkia.cpp
+++ third_party/WebKit/Source/core/platform/graphics/skia/NativeImageSkia.cpp
@@ -385,7 +385,7 @@ void NativeImageSkia::draw(GraphicsContext* context, const SkRect& srcRect, cons
 static SkBitmap createBitmapWithSpace(const SkBitmap& bitmap, int spaceWidth, int spaceHeight)
 {
     SkBitmap result;
-    result.setConfig(bitmap.getConfig(),
+    result.setConfig(bitmap.config(),
         bitmap.width() + spaceWidth,
         bitmap.height() + spaceHeight);
     result.allocPixels();
