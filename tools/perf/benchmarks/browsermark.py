37828f98d9af35043e6a884d88778d3e56a1d6b2
prasadv@chromium.org <prasadv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-17 17:54:02 +0000

    [Telemetry]: Add Browsermark 2.0 benchmark suite.
    TEST= tools/perf/run_benchmark --browser=system browsermark
    
    Review URL: https://codereview.chromium.org/66513010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@235544 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f6cec5fc377c1e01de4921b8a14f05c797f856ef

---
+++ tools/perf/benchmarks/browsermark.py
@@ -0,0 +1,60 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Runs Browsermark CSS, DOM, WebGL, JS, resize and page load benchmarks.
+
+Browsermark benchmark suite have five test groups:
+a) CSS group: measures your browsers 2D and 3D performance, and finally executes
+  CSS Crunch test
+b) DOM group: measures variety of areas, like how well your browser traverse in
+  Document Object Model Tree or how fast your browser can create dynamic content
+c) General group: measures areas like resize and page load times
+d) Graphics group: tests browsers Graphics Processing Unit power by measuring
+  WebGL and Canvas performance
+e) Javascript group: executes number crunching by doing selected Array and
+  String operations
+Additionally Browsermark will test your browsers conformance, but conformance
+tests are not included in this suite.
+"""
+
+import os
+
+from telemetry import test
+from telemetry.page import page_measurement
+from telemetry.page import page_set
+
+
+class BrowsermarkMeasurement(page_measurement.PageMeasurement):
+
+  def MeasurePage(self, _, tab, results):
+    # Select nearest server(North America=1) and start test.
+    js_start_test =  """
+        for (var i=0; i < $('#continent a').length; i++) {
+          if (($('#continent a')[i]).getAttribute('data-id') == '1') {
+            $('#continent a')[i].click();
+            $('.start_test.enabled').click();
+          }
+        }
+        """
+    tab.ExecuteJavaScript(js_start_test)
+    tab.WaitForJavaScriptExpression(
+      'window.location.pathname.indexOf("results") != -1', 600)
+    result = int(tab.EvaluateJavaScript(
+        'document.getElementsByClassName("score")[0].innerHTML'))
+    results.Add('Score', 'score', result)
+
+
+class Browsermark(test.Test):
+  """Browsermark suite tests CSS, DOM, resize, page load, WebGL and JS."""
+  test = BrowsermarkMeasurement
+  def CreatePageSet(self, options):
+    return page_set.PageSet.FromDict({
+        'archive_data_file': '../page_sets/data/browsermark.json',
+        'make_javascript_deterministic': False,
+        'pages': [
+          { 'url':
+              'http://browsermark.rightware.com/tests/'}
+           ]
+        }, os.path.abspath(__file__))
+
