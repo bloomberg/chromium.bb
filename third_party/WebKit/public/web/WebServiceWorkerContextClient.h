cf51d536b96644e8642f84e99c8a5d513ae91fda
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-19 08:33:57 +0000

    Add EmbeddedWorker interfaces in Blink API
    Rough design doc: https://docs.google.com/a/chromium.org/document/d/1HzjaXttZcRr6cE7oINMzvlVskbGpGnB_Tn37ayjST9Q/edit
    
    Chromium-side patch: https://codereview.chromium.org/54573002/
    
    BUG=313530
    
    Review URL: https://codereview.chromium.org/61763004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162282 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ab84022fd30990d67181f5ff15818166eb4b3302

---
+++ third_party/WebKit/public/web/WebServiceWorkerContextClient.h
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2013 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef WebServiceWorkerContextClient_h
+#define WebServiceWorkerContextClient_h
+
+#include "WebWorkerPermissionClientProxy.h"
+
+namespace blink {
+
+class WebString;
+class WebServiceWorkerContextProxy;
+
+// This interface is implemented by the client. It is suppoed to be created
+// on the main thread and then passed on to the worker thread to be owned
+// by a newly created WorkerGlobalScope. All methods of this class are
+// called on the worker thread.
+class WebServiceWorkerContextClient {
+public:
+    // A new WorkerGlobalScope is created and started to run on the
+    // worker thread.
+    // This also gives back a proxy to the client to talk to the
+    // newly created WorkerGlobalScope. The proxy is held by WorkerGlobalScope
+    // and should not be held by the caller. No proxy methods should be called
+    // after workerContextDestroyed() is called.
+    virtual void workerContextStarted(WebServiceWorkerContextProxy*) { }
+
+    // WorkerGlobalScope.close() is called.
+    virtual void workerContextClosed() { }
+
+    // WorkerGlobalScope is destroyed. The client should clear the
+    // WebServiceWorkerGlobalScopeProxy when this is called.
+    virtual void workerContextDestroyed() { }
+
+    virtual void dispatchDevToolsMessage(const WebString&) { }
+    virtual void saveDevToolsAgentState(const WebString&) { }
+};
+
+} // namespace blink
+
+#endif // WebWorkerContextClient_h
