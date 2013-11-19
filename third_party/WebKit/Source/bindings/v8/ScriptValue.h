98ec1f65073df91b5cdae0f120d5143c322ba185
yhirano@chromium.org <yhirano@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-19 10:53:26 +0000

    Introduce ScriptValue::createUndefined
    Add ScriptValue::createUndefined static function which returns
    a ScriptValue representing an undefined value (not an empty value).
    
    BUG=320984
    R=haraken
    
    Review URL: https://codereview.chromium.org/76083002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162292 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 775911166943caeb2003297c16b83c023879d305

--- third_party/WebKit/Source/bindings/v8/ScriptValue.h
+++ third_party/WebKit/Source/bindings/v8/ScriptValue.h
@@ -69,6 +69,11 @@ public:
         return m_isolate;
     }
 
+    static ScriptValue createUndefined(v8::Isolate* isolate)
+    {
+        return ScriptValue(v8::Undefined(isolate), isolate);
+    }
+
     static ScriptValue createNull()
     {
         v8::Isolate* isolate = v8::Isolate::GetCurrent();
