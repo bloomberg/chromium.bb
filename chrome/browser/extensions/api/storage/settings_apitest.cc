6bdc6eb00b8c66f9368b307131573df9d0d91371
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-20 03:52:50 +0000

    Disable flaky test ExtensionSettingsApiTest.ManagedStorageEvents.
    This test is failing quite a bit on Linux Aura.
    
    BUG=321448
    TBR=kalman@chromium.org
    
    Review URL: https://codereview.chromium.org/77223003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236128 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a503807483c3242bb4e758a332177bbd1c2a36db

--- chrome/browser/extensions/api/storage/settings_apitest.cc
+++ chrome/browser/extensions/api/storage/settings_apitest.cc
@@ -554,7 +554,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, ManagedStorage) {
   ASSERT_TRUE(RunExtensionTest("settings/managed_storage")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, PRE_ManagedStorageEvents) {
+IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest,
+                       DISABLED_PRE_ManagedStorageEvents) {
   ResultCatcher catcher;
 
   // This test starts without any test extensions installed.
@@ -587,7 +588,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, PRE_ManagedStorageEvents) {
   EXPECT_TRUE(catcher.GetNextResult()) << catcher.message();
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest, ManagedStorageEvents) {
+IN_PROC_BROWSER_TEST_F(ExtensionSettingsApiTest,
+                       DISABLED_ManagedStorageEvents) {
   // This test runs after PRE_ManagedStorageEvents without having deleted the
   // profile, so the extension is still around. While the browser restarted the
   // policy went back to the empty default, and so the extension should receive
