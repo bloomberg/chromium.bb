71043754f05011106ec0b4d556ab2ba9f55b0044
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-20 02:40:21 +0000

    Rotate vk display 180 for easier experiment
    BUG=319493
    
    Review URL: https://codereview.chromium.org/62103019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236107 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bf058c4b21346faec56398cae07f4b5dabd98533

--- ash/display/virtual_keyboard_window_controller.h
+++ ash/display/virtual_keyboard_window_controller.h
@@ -8,6 +8,7 @@
 #include "ash/ash_export.h"
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
+#include "ui/gfx/display.h"
 
 namespace keyboard {
 class KeyboardController;
@@ -41,6 +42,9 @@ class ASH_EXPORT VirtualKeyboardWindowController {
 
  private:
   friend class test::VirtualKeyboardWindowControllerTest;
+
+  // Rotates virtual keyboard display by 180 degrees.
+  void FlipDisplay();
 
   RootWindowController* root_window_controller_for_test() {
     return root_window_controller_.get();
