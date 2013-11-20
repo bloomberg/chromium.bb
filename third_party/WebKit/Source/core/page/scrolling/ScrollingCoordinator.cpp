d8e1844c23571b14841c0f7088cbb08828868e8c
vollick@chromium.org <vollick@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-20 03:34:39 +0000

    Revert "Ignore should-scroll-on-main-thread if main frame is not scrollable"
    This reverts commit 3c8dc08b5ef0a09c2e6e7b6a2b9ed85be51577f4.
    
    Original change: https://codereview.chromium.org/40373002
    
    The original change was causing unexplained flake. This suggests that it was not
    correct in the first place. Reverting until the flake can be explained/fixed.
    
    TBR=dpranke@chromium.org
    BUG=321358
    
    Review URL: https://codereview.chromium.org/76983003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162327 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d6af7468994f5fa1fb5abd8c5aae1f28983a428c

--- third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
+++ third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
@@ -90,8 +90,6 @@ ScrollingCoordinator::ScrollingCoordinator(Page* page)
     : m_page(page)
     , m_scrollGestureRegionIsDirty(false)
     , m_touchEventTargetRectsAreDirty(false)
-    , m_wasFrameScrollable(false)
-    , m_lastMainThreadScrollingReasons(0)
 {
 }
 
@@ -152,12 +150,6 @@ void ScrollingCoordinator::updateAfterCompositingChange()
         updateTouchEventTargetRectsIfNeeded();
         m_touchEventTargetRectsAreDirty = false;
     }
-
-    FrameView* frameView = m_page->mainFrame()->view();
-    bool frameIsScrollable = frameView && frameView->isScrollable();
-    if (m_wasFrameScrollable != frameIsScrollable)
-        updateShouldUpdateScrollLayerPositionOnMainThread();
-    m_wasFrameScrollable = frameIsScrollable;
 
     const FrameTree& tree = m_page->mainFrame()->tree();
     for (const Frame* child = tree.firstChild(); child; child = child->tree().nextSibling()) {
@@ -601,10 +593,8 @@ void ScrollingCoordinator::recomputeWheelEventHandlerCountForFrameView(FrameView
 
 void ScrollingCoordinator::setShouldUpdateScrollLayerPositionOnMainThread(MainThreadScrollingReasons reasons)
 {
-    if (WebLayer* scrollLayer = scrollingWebLayerForScrollableArea(m_page->mainFrame()->view())) {
-        m_lastMainThreadScrollingReasons = reasons;
+    if (WebLayer* scrollLayer = scrollingWebLayerForScrollableArea(m_page->mainFrame()->view()))
         scrollLayer->setShouldScrollOnMainThread(reasons);
-    }
 }
 
 void ScrollingCoordinator::pageDestroyed()
@@ -869,11 +859,8 @@ bool ScrollingCoordinator::hasVisibleSlowRepaintViewportConstrainedObjects(Frame
 
 MainThreadScrollingReasons ScrollingCoordinator::mainThreadScrollingReasons() const
 {
-    // The main thread scrolling reasons are applicable to scrolls of the main
-    // frame. If it does not exist or if it is not scrollable, there is no
-    // reason to force main thread scrolling.
     FrameView* frameView = m_page->mainFrame()->view();
-    if (!frameView || !frameView->isScrollable())
+    if (!frameView)
         return static_cast<MainThreadScrollingReasons>(0);
 
     MainThreadScrollingReasons mainThreadScrollingReasons = (MainThreadScrollingReasons)0;
@@ -909,7 +896,7 @@ String ScrollingCoordinator::mainThreadScrollingReasonsAsText(MainThreadScrollin
 
 String ScrollingCoordinator::mainThreadScrollingReasonsAsText() const
 {
-    return mainThreadScrollingReasonsAsText(m_lastMainThreadScrollingReasons);
+    return mainThreadScrollingReasonsAsText(mainThreadScrollingReasons());
 }
 
 } // namespace WebCore
