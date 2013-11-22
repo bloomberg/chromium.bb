425daf99a2ef38d628992fe1c93051d4d282624a
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 01:03:43 +0000

    rAc: make sure border repaints on focus changes
    BUG=322225
    
    Review URL: https://codereview.chromium.org/80503003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236654 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 554332f6987a911b848a5c463c8d18e8b6899e5e

--- chrome/browser/ui/views/autofill/decorated_textfield.cc
+++ chrome/browser/ui/views/autofill/decorated_textfield.cc
@@ -130,11 +130,13 @@ views::View* DecoratedTextfield::GetEventHandlerForRect(const gfx::Rect& rect) {
 void DecoratedTextfield::OnFocus() {
   border_->set_has_focus(true);
   views::Textfield::OnFocus();
+  SchedulePaint();
 }
 
 void DecoratedTextfield::OnBlur() {
   border_->set_has_focus(false);
   views::Textfield::OnBlur();
+  SchedulePaint();
 }
 
 gfx::Size DecoratedTextfield::GetPreferredSize() {
