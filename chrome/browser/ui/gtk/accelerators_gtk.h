cd5747e64a809b198a747d396406ca92232884f4
alextaran@chromium.org <alextaran@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 20:56:46 +0000

    Add #include <vector> to accelerators_gtk.h
    This file uses std::vector but does not include corresponding header file.
    
    R=erg@chromium.org
    BUG=82098
    
    Review URL: https://codereview.chromium.org/83293002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236823 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7a1b13c67e4cedd8a6585e56b1d1572cd27ca236

--- chrome/browser/ui/gtk/accelerators_gtk.h
+++ chrome/browser/ui/gtk/accelerators_gtk.h
@@ -5,6 +5,8 @@
 #ifndef CHROME_BROWSER_UI_GTK_ACCELERATORS_GTK_H_
 #define CHROME_BROWSER_UI_GTK_ACCELERATORS_GTK_H_
 
+#include <vector>
+
 #include "base/containers/hash_tables.h"
 #include "ui/base/accelerators/accelerator.h"
 
