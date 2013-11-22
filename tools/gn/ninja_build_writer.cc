95e795e65dd4ed81c6a1f3bfbdbdd40621adc468
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-22 22:13:47 +0000

    Add "ninja show" and "ninja refresh" targets to GN build.
    "ninja show" will print out the build arguments for when you can't remember the configuration of your output directory.
    
    "ninja refresh" ignores dependencies and regenerates the ninja files.
    
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/82923005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@236840 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 05a8c33afaf3bedee93818f0a6c3bc0494113700

--- tools/gn/ninja_build_writer.cc
+++ tools/gn/ninja_build_writer.cc
@@ -136,8 +136,25 @@ void NinjaBuildWriter::WriteNinjaRules() {
   out_ << "  command = " << GetSelfInvocationCommand(build_settings_) << "\n";
   out_ << "  description = Regenerating ninja files\n\n";
 
+  // This rule will regenerate the ninja files when any input file has changed.
   out_ << "build build.ninja: gn\n"
        << "  depfile = build.ninja.d\n";
+
+  // Provide a way to force regenerating ninja files if the user is suspicious
+  // something is out-of-date. This will be "ninja refresh".
+  out_ << "\nbuild refresh: gn\n";
+
+  // Provide a way to see what flags are associated with this build:
+  // This will be "ninja show".
+  const CommandLine& our_cmdline = *CommandLine::ForCurrentProcess();
+  std::string args = our_cmdline.GetSwitchValueASCII("args");
+  out_ << "rule echo\n";
+  out_ << "  command = echo $text\n";
+  out_ << "  description = ECHO $desc\n";
+  out_ << "build show: echo\n";
+  out_ << "  desc = build arguments:\n";
+  out_ << "  text = " << (args.empty() ? std::string("<default args>") : args)
+       << "\n";
 
   // Input build files. These go in the ".d" file. If we write them as
   // dependencies in the .ninja file itself, ninja will expect the files to
