e6deb25c71f58348dac33cd7f182742ece2a9f13
jfweitz@chromium.org <jfweitz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-25 21:03:37 +0000

    Use validated_url to check whether it's a Cacheable NTP or not in DidFailProvisionalLoad.
    BUG=322249
    
    Review URL: https://codereview.chromium.org/84143002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237138 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4f74227fd877c0060a096838775c84cbdb793ed2

--- chrome/browser/ui/search/search_tab_helper.cc
+++ chrome/browser/ui/search/search_tab_helper.cc
@@ -256,11 +256,16 @@ void SearchTabHelper::DidFailProvisionalLoad(
     int64 /* frame_id */,
     const string16& frame_unique_name,
     bool is_main_frame,
-    const GURL& /* validated_url */,
+    const GURL& validated_url,
     int /* error_code */,
     const string16& /* error_description */,
     content::RenderViewHost* /* render_view_host */) {
-  if (is_main_frame && IsCacheableNTP(web_contents_)) {
+  Profile* profile =
+      Profile::FromBrowserContext(web_contents_->GetBrowserContext());
+  if (is_main_frame &&
+      chrome::ShouldUseCacheableNTP() &&
+      validated_url != GURL(chrome::kChromeSearchLocalNtpUrl) &&
+      chrome::IsNTPURL(validated_url, profile)) {
     RedirectToLocalNTP();
     RecordCacheableNTPLoadHistogram(false);
   }
