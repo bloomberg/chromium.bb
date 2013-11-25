69490981bd5a233322081efdcfae428a8327e4b1
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-25 11:22:03 +0000

    Disable PPAPI tests that time out on ChromiumOS
    BUG=323144
    TBR=yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/85583002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237055 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: beaf2bb860ff21a24a6c5e8610cde63d0d06d3e8

--- chrome/test/ppapi/ppapi_browsertest.cc
+++ chrome/test/ppapi/ppapi_browsertest.cc
@@ -719,7 +719,13 @@ TEST_PPAPI_OUT_OF_PROCESS(MAYBE_VarDeprecated)
 #undef PostMessage
 #endif
 // PostMessage tests.
-IN_PROC_BROWSER_TEST_F(PPAPITest, PostMessage) {
+// Fails on ChromeOS http://crbug.com/323144
+#if defined(OS_CHROMEOS)
+#define MAYBE_PostMessage DISABLED_PostMessage
+#else
+#define MAYBE_PostMessage PostMessage
+#endif
+IN_PROC_BROWSER_TEST_F(PPAPITest, MAYBE_PostMessage) {
   RunTestViaHTTP(
       LIST_TEST(PostMessage_SendInInit)
       LIST_TEST(PostMessage_SendingData)
@@ -733,9 +739,10 @@ IN_PROC_BROWSER_TEST_F(PPAPITest, PostMessage) {
       LIST_TEST(PostMessage_ExtraParam)
   );
 }
+#undef MAYBE_PostMessage
 
-// Flaky: crbug.com/269530
-#if defined(OS_WIN)
+// Flaky: crbug.com/269530 crbug.com/323144
+#if defined(OS_WIN) || defined(OS_CHROMEOS)
 #define MAYBE_PostMessage DISABLED_PostMessage
 #else
 #define MAYBE_PostMessage PostMessage
@@ -1574,8 +1581,8 @@ TEST_PPAPI_OUT_OF_PROCESS(Flash_GetLocalTimeZoneOffset)
 TEST_PPAPI_OUT_OF_PROCESS(Flash_GetProxyForURL)
 TEST_PPAPI_OUT_OF_PROCESS(Flash_GetSetting)
 TEST_PPAPI_OUT_OF_PROCESS(Flash_SetCrashData)
-// http://crbug.com/176822
-#if !defined(OS_WIN)
+// http://crbug.com/176822 http://crbug.com/323144
+#if !defined(OS_WIN) && !defined(OS_CHROMEOS)
 TEST_PPAPI_OUT_OF_PROCESS(FlashClipboard)
 #endif
 TEST_PPAPI_OUT_OF_PROCESS(FlashFile)
