0613ae1b8191732929b7d97d1e70935333766120
tburkard@chromium.org <tburkard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 00:58:00 +0000

    When there is a session storage namespace mismatch during prerender swap-in, attempt merging namespaces. Re-submission of https://codereview.chromium.org/59823003/ see other lgtm's there
    Adding missing new files
    
    Review URL: https://codereview.chromium.org/85763006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237206 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 47f8ed2a711a6ad810689bb6f7bf0ad2658bff90

---
+++ chrome/browser/prerender/prerender_pending_swap_throttle.cc
@@ -0,0 +1,64 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/prerender/prerender_pending_swap_throttle.h"
+
+#include "chrome/browser/prerender/prerender_final_status.h"
+#include "chrome/browser/prerender/prerender_manager.h"
+#include "chrome/browser/prerender/prerender_tracker.h"
+#include "chrome/browser/prerender/prerender_util.h"
+#include "content/public/browser/resource_controller.h"
+#include "content/public/browser/resource_request_info.h"
+#include "net/url_request/url_request.h"
+
+namespace prerender {
+
+PrerenderPendingSwapThrottle::PrerenderPendingSwapThrottle(
+    net::URLRequest* request,
+    PrerenderTracker* tracker)
+    : request_(request),
+      tracker_(tracker),
+      throttled_(false) {
+}
+
+void PrerenderPendingSwapThrottle::WillStartRequest(bool* defer) {
+  DCHECK(!throttled_);
+  const content::ResourceRequestInfo* info =
+      content::ResourceRequestInfo::ForRequest(request_);
+
+  // We only care about main frame loads.
+  if (info->GetResourceType() != ResourceType::MAIN_FRAME)
+    return;
+
+  int child_id = info->GetChildID();
+  int route_id = info->GetRouteID();
+
+  // Check if this request is for a URL we intend to swap in.
+  if (!tracker_->IsPendingSwapRequestOnIOThread(child_id, route_id,
+                                                  request_->url())) {
+    return;
+  }
+
+  *defer = true;
+  throttled_ = true;
+  tracker_->AddPendingSwapThrottleOnIOThread(child_id, route_id,
+                                               request_->url(),
+                                               this->AsWeakPtr());
+}
+
+void PrerenderPendingSwapThrottle::Resume() {
+  DCHECK(throttled_);
+
+  throttled_ = false;
+  controller()->Resume();
+}
+
+void PrerenderPendingSwapThrottle::Cancel() {
+  DCHECK(throttled_);
+
+  throttled_ = false;
+  controller()->Cancel();
+}
+
+}  // namespace prerender
