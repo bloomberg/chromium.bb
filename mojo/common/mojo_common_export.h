7d68340f6514e2d025a4288a7d596fff3659af5a
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 00:00:14 +0000

    Mojo: Fix our copyright notices.
    Apparently, our current notice doesn't have the "(c)". Maybe if I fix it
    in enough places, we'll stop copying-and-pasting in the (old)
    now-incorrect notice.
    
    (Also do some other drive-by comment/whitespace fixes.)
    
    R=sky@chromium.org, davemoore@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/86483006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237186 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4690580335f05a1e4bd81f8b27778dc507c19a36

--- mojo/common/mojo_common_export.h
+++ mojo/common/mojo_common_export.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -6,24 +6,29 @@
 #define MOJO_COMMON_MOJO_COMMON_EXPORT_H_
 
 #if defined(COMPONENT_BUILD)
+
 #if defined(WIN32)
 
 #if defined(MOJO_COMMON_IMPLEMENTATION)
 #define MOJO_COMMON_EXPORT __declspec(dllexport)
 #else
 #define MOJO_COMMON_EXPORT __declspec(dllimport)
-#endif  // defined(MOJO_COMMON_IMPLEMENTATION)
+#endif
 
 #else  // defined(WIN32)
+
 #if defined(MOJO_COMMON_IMPLEMENTATION)
 #define MOJO_COMMON_EXPORT __attribute__((visibility("default")))
 #else
 #define MOJO_COMMON_EXPORT
 #endif
-#endif
+
+#endif  // defined(WIN32)
 
 #else  // defined(COMPONENT_BUILD)
+
 #define MOJO_COMMON_EXPORT
-#endif
+
+#endif  // defined(COMPONENT_BUILD)
 
 #endif  // MOJO_COMMON_MOJO_COMMON_EXPORT_H_
