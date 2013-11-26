fb8c22153fdf3ca94be4f6fa2779f7fefb6787e6
rvargas@chromium.org <rvargas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-26 06:02:34 +0000

    Disk cache: Downgrade a DCHECK to a DLOG.
    Once in a while this hits a developer after going through a system crash (or
    forced restart), as expected, but the rest of the code solves the issue
    without user intervention.
    
    BUG=none
    TEST=none
    R=jar@chromium.org
    
    Review URL: https://codereview.chromium.org/85643005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237274 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b09106fc163bf0933f072b543ab6f2e2f52e988a

--- net/disk_cache/block_files.cc
+++ net/disk_cache/block_files.cc
@@ -83,7 +83,7 @@ bool BlockHeader::CreateMapBlock(int size, int* index) {
       disk_cache::FileLock lock(header_);
       int index_offset = j * 4 + 4 - target;
       *index = current * 32 + index_offset;
-      DCHECK_EQ(*index / 4, (*index + size - 1) / 4);
+      DLOG_IF(ERROR, *index / 4 == (*index + size - 1) / 4) << "Bit mismatch";
       uint32 to_add = ((1 << size) - 1) << index_offset;
       header_->num_entries++;
 
