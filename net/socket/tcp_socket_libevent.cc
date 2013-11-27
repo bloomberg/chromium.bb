d8a9f42ef71f7da06cfcf7a2db71e1d857b55446
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-27 01:51:59 +0000

    Handle an aborted connection before accept is called.
    This is the POSIX equivalent of https://codereview.chromium.org/25686003/.
    
    R=rpaquay@chromium.org,yzshen@chromium.org
    BUG=170595
    TEST=none
    
    Review URL: https://codereview.chromium.org/26389002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237478 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 753140881ae6adff6326a816720e63c97d77e96c

--- net/socket/tcp_socket_libevent.cc
+++ net/socket/tcp_socket_libevent.cc
@@ -69,6 +69,20 @@ bool SetTCPKeepAlive(int fd, bool enable, int delay) {
   return true;
 }
 
+int MapAcceptError(int os_error) {
+  switch (os_error) {
+    // If the client aborts the connection before the server calls accept,
+    // POSIX specifies accept should fail with ECONNABORTED. The server can
+    // ignore the error and just call accept again, so we map the error to
+    // ERR_IO_PENDING. See UNIX Network Programming, Vol. 1, 3rd Ed., Sec.
+    // 5.11, "Connection Abort before accept Returns".
+    case ECONNABORTED:
+      return ERR_IO_PENDING;
+    default:
+      return MapSystemError(os_error);
+  }
+}
+
 int MapConnectError(int os_error) {
   switch (os_error) {
     case EACCES:
@@ -567,7 +581,7 @@ int TCPSocketLibevent::AcceptInternal(scoped_ptr<TCPSocketLibevent>* socket,
                                        storage.addr,
                                        &storage.addr_len));
   if (new_socket < 0) {
-    int net_error = MapSystemError(errno);
+    int net_error = MapAcceptError(errno);
     if (net_error != ERR_IO_PENDING)
       net_log_.EndEventWithNetErrorCode(NetLog::TYPE_TCP_ACCEPT, net_error);
     return net_error;
