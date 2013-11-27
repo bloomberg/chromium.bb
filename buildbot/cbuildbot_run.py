1716ce8f692e3a33e0e7af3dc8ee7d4d11491bc3
Matt Tennant <mtennant@chromium.org>
2013-11-27 10:39:48 -0800

    cbuildbot_run.py: New BuilderRun module added, but not used, yet.
    This change introduces a new BuilderRun class that is intended to
    provide context for all state associated with one run through cbuildbot.
    It holds both the build_config and options objects, and provides support
    for saving pre-defined "run attributes".
    
    BUG=chromium:307321
    TEST=unittest
    
    Change-Id: I028c3cf3f973ac2fa111479c0fcbc798c0d2636b
    Reviewed-on: https://chromium-review.googlesource.com/178205
    Tested-by: Matt Tennant <mtennant@chromium.org>
    Reviewed-by: David James <davidjames@chromium.org>
    Commit-Queue: Matt Tennant <mtennant@chromium.org>

---
+++ buildbot/cbuildbot_run.py
@@ -0,0 +1,141 @@
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Provide a BuilderRun class for collecting info on one builder run."""
+
+import os
+
+from chromite.buildbot import manifest_version
+
+
+class RunAttributes(object):
+  """Hold all run attributes for a particular builder run."""
+
+  # A word of warning about the way run attributes currently work.  The
+  # value set (or altered) by a given stage can be seen by any subsequent
+  # stage, including stages on child processes.  Any value set (or
+  # altered) by a stage in a subprocess will not be seen by co-subprocesses
+  # (other stages being run in parallel at the same time) and will
+  # be *discarded* when that subprocess ends (and parent process resumes).
+  # This is expected behavior for forked processes.
+  # TODO(mtennant): Possibly design a mechanism to preserve run attribute
+  # changes in subprocess stages for subsequent stages (but probably not for
+  # co-subprocess stages).
+
+  __slots__ = (
+      'manifest_manager', # Set by ManifestVersionedSyncStage.
+      'release_tag',      # Set by ManifestVersionedSyncStage.
+  )
+
+
+class BuilderRun(object):
+  """Class to represent one run of a builder."""
+
+  # Class-level dict of RunAttributes objects to make it less
+  # problematic to send BuilderRun objects between processes through
+  # pickle.  The 'attrs' attribute on a BuilderRun object will look
+  # up the RunAttributes for that particular BuilderRun here.
+  _ATTRS = {}
+
+  __slots__ = (
+      'config',          # BuildConfig for this run.
+      'options',         # The cbuildbot options object for this run.
+
+      # Run attributes set/accessed by stages during the run.  To add support
+      # for a new run attribute add it to the RunAttributes class above.
+      '_attrs_id',
+
+      # Some pre-computed run configuration values.
+      'buildnumber',     # The build number for this run.
+      'buildroot',       # The build root path for this run.
+      'manifest_branch', # The manifest branch to build and test for this run.
+
+      # TODO(mtennant): Other candidates here include:
+      # trybot, buildbot, remote_trybot, chrome_root, chrome_version,
+      # test = (config build_tests AND option tests)
+  )
+
+  def __init__(self, options, build_config):
+    self.options = options
+    self.config = build_config
+
+    # Create the RunAttributes object for this BuilderRun and save
+    # the id number for it in order to look it up via attrs property.
+    attrs = RunAttributes()
+    self._ATTRS[id(attrs)] = attrs
+    self._attrs_id = id(attrs)
+
+    # Fill in values for all pre-computed "run configs" now, which are frozen
+    # by this time.
+
+    # TODO(mtennant): Should this use os.path.abspath like builderstage does?
+    self.buildroot = self.options.buildroot
+    self.buildnumber = self.options.buildnumber
+    self.manifest_branch = self.options.branch
+
+  @property
+  def attrs(self):
+    """Look up the RunAttributes object for this BuilderRun object."""
+    return self._ATTRS[self._attrs_id]
+
+  def ShouldUploadPrebuilts(self):
+    """Return True if this run should upload prebuilts."""
+    return self.options.prebuilts and self.config.prebuilts
+
+  def ShouldReexecAfterSync(self):
+    """Return True if this run should re-exec itself after sync stage."""
+    if self.options.postsync_reexec and self.config.postsync_reexec:
+      # Return True if this source is not in designated buildroot.
+      abs_buildroot = os.path.abspath(self.buildroot)
+      return not os.path.abspath(__file__).startswith(abs_buildroot)
+
+    return False
+
+  def ShouldPatchAfterSync(self):
+    """Return True if this run should patch changes after sync stage."""
+    return self.options.postsync_patch and self.config.postsync_patch
+
+  @classmethod
+  def GetVersionInfo(cls, buildroot):
+    """Helper for picking apart various version bits.
+
+    This method only exists so that tests can override it.
+    """
+    return manifest_version.VersionInfo.from_repo(buildroot)
+
+  def GetVersion(self):
+    """Calculate full R<chrome_version>-<chromeos_version> version string."""
+    verinfo = self.GetVersionInfo(self.buildroot)
+    if hasattr(self.attrs, 'release_tag'):
+      calc_version = 'R%s-%s' % (verinfo.chrome_branch,
+                                 self.attrs.release_tag)
+    else:
+      # Non-versioned builds need the build number to uniquify the image.
+      calc_version = 'R%s-%s-b%s' % (verinfo.chrome_branch,
+                                     verinfo.VersionString(),
+                                     self.buildnumber)
+
+    return calc_version
+
+
+class ChildBuilderRun(object):
+  """A BuilderRun for a "child" build config, overriding self.config."""
+
+  __slots__ = BuilderRun.__slots__ + (
+      '_builder_run',    # The full (master) BuilderRun.
+      'config',          # The child BuildConfig for this ChildBuilderRun.
+      'child_index',     # Index into self.full_config.child_configs.
+  )
+
+  def __init__(self, builder_run, child_index):
+    self._builder_run = builder_run
+    self.child_index = child_index
+    self.config = builder_run.config.child_configs[child_index]
+
+  def __getattr__(self, attr):
+    # Remember, __getattr__ only called if attribute was not found normally.
+    # Point at self._builder_run, but access self._builder_run in a way that
+    # does not cause infinite recursion.
+    full_builder_run = self.__getattribute__('_builder_run')
+    return getattr(full_builder_run, attr)
---
+++ buildbot/cbuildbot_run_unittest.py
@@ -0,0 +1,271 @@
+#!/usr/bin/python
+
+# Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import os
+import cPickle
+import sys
+
+sys.path.insert(0, os.path.abspath('%s/../..' % os.path.dirname(__file__)))
+from chromite.buildbot import cbuildbot_run
+from chromite.lib import cros_test_lib
+
+import mock
+
+DEFAULT_BUILDROOT = '/tmp/foo/bar/buildroot'
+DEFAULT_BUILDNUMBER = 12345
+DEFAULT_BRANCH = 'TheBranch'
+DEFAULT_CHROME_BRANCH = 'TheChromeBranch'
+DEFAULT_VERSION_STRING = 'TheVersionString'
+DEFAULT_BOARD = 'TheBoard'
+
+DEFAULT_OPTIONS = cros_test_lib.EasyAttr(buildroot=DEFAULT_BUILDROOT,
+                                         buildnumber=DEFAULT_BUILDNUMBER,
+                                         branch=DEFAULT_BRANCH,
+                                        )
+DEFAULT_CONFIG = cros_test_lib.EasyAttr(
+    master=True,
+    boards=[DEFAULT_BOARD],
+    child_configs=[cros_test_lib.EasyAttr(name='foo'),
+                   cros_test_lib.EasyAttr(name='bar'),
+                  ])
+
+
+def _NewBuilderRun(options=None, config=None):
+  """Create a BuilderRun objection from options and config values.
+
+  Args:
+    options: Specify options or default to DEFAULT_OPTIONS.
+    config: Specify build config or default to DEFAULT_CONFIG.
+  Returns:
+    BuilderRun object.
+  """
+  options = options or DEFAULT_OPTIONS
+  config = config or DEFAULT_CONFIG
+  return cbuildbot_run.BuilderRun(options, config)
+
+
+def _NewChildBuilderRun(child_index, options=None, config=None):
+  """Create a ChildBuilderRun objection from options and config values.
+
+  Args:
+    options: Specify options or default to DEFAULT_OPTIONS.
+    config: Specify build config or default to DEFAULT_CONFIG.
+  Returns:
+    ChildBuilderRun object.
+  """
+  run = _NewBuilderRun(options, config)
+  return cbuildbot_run.ChildBuilderRun(run, child_index)
+
+
+def _ExtendDefaultOptions(**kwargs):
+  """Extend DEFAULT_OPTIONS with keys/values in kwargs."""
+  options_kwargs = DEFAULT_OPTIONS.copy()
+  options_kwargs.update(kwargs)
+  return cros_test_lib.EasyAttr(**options_kwargs)
+
+
+def _ExtendDefaultConfig(**kwargs):
+  """Extend DEFAULT_CONFIG with keys/values in kwargs."""
+  config_kwargs = DEFAULT_CONFIG.copy()
+  config_kwargs.update(kwargs)
+  return cros_test_lib.EasyAttr(**config_kwargs)
+
+
+class BuilderRunPickleTest(cros_test_lib.TestCase):
+  """Make sure BuilderRun objects can be pickled."""
+
+  def testPickleBuilderRun(self):
+    run1 = _NewBuilderRun()
+    run1.attrs.release_tag = 'TheReleaseTag'
+    run2 = cPickle.loads(cPickle.dumps(run1, cPickle.HIGHEST_PROTOCOL))
+
+    self.assertEquals(run1.buildnumber, run2.buildnumber)
+    self.assertEquals(run1.config.boards, run2.config.boards)
+    self.assertEquals(run1.options.branch, run2.options.branch)
+    self.assertEquals(run1.attrs.release_tag, run2.attrs.release_tag)
+    self.assertRaises(AttributeError, getattr, run1.attrs, 'manifest_manager')
+    self.assertRaises(AttributeError, getattr, run2.attrs, 'manifest_manager')
+
+  def testPickleChildBuilderRun(self):
+    run1 = _NewChildBuilderRun(0)
+    run1.attrs.release_tag = 'TheReleaseTag'
+    run2 = cPickle.loads(cPickle.dumps(run1, cPickle.HIGHEST_PROTOCOL))
+
+    self.assertEquals(run1.child_index, run2.child_index)
+    self.assertEquals(run1.buildnumber, run2.buildnumber)
+    self.assertEquals(run1.config.name, run2.config.name)
+    self.assertEquals(run1.options.branch, run2.options.branch)
+    self.assertEquals(run1.attrs.release_tag, run2.attrs.release_tag)
+    self.assertRaises(AttributeError, getattr, run1.attrs, 'manifest_manager')
+    self.assertRaises(AttributeError, getattr, run2.attrs, 'manifest_manager')
+
+
+class BuilderRunTest(cros_test_lib.TestCase):
+  """Test the BuilderRun class."""
+
+  def testInit(self):
+    run = _NewBuilderRun()
+    self.assertEquals(DEFAULT_BUILDROOT, run.buildroot)
+    self.assertEquals(DEFAULT_BUILDNUMBER, run.buildnumber)
+    self.assertEquals(DEFAULT_BRANCH, run.manifest_branch)
+    self.assertEquals(DEFAULT_OPTIONS, run.options)
+    self.assertEquals(DEFAULT_CONFIG, run.config)
+    self.assertTrue(isinstance(run.attrs, cbuildbot_run.RunAttributes))
+
+  def testOptions(self):
+    options = _ExtendDefaultOptions(foo=True, bar=10)
+    run = _NewBuilderRun(options=options)
+
+    self.assertEquals(True, run.options.foo)
+    self.assertEquals(10, run.options.__getattr__('bar'))
+    self.assertRaises(AttributeError, run.options.__getattr__, 'baz')
+
+  def testConfig(self):
+    config = _ExtendDefaultOptions(foo=True, bar=10)
+    run = _NewBuilderRun(config=config)
+
+    self.assertEquals(True, run.config.foo)
+    self.assertEquals(10, run.config.__getattr__('bar'))
+    self.assertRaises(AttributeError, run.config.__getattr__, 'baz')
+
+  def testAttrs(self):
+    run = _NewBuilderRun()
+
+    # manifest_manager is a valid run attribute.  It gives Attribute error
+    # if accessed before being set, but thereafter works fine.
+    self.assertRaises(AttributeError, run.attrs.__getattribute__,
+                      'manifest_manager')
+    run.attrs.manifest_manager = 'foo'
+    self.assertEquals('foo', run.attrs.manifest_manager)
+    self.assertEquals('foo', run.attrs.__getattribute__('manifest_manager'))
+
+    # foobar is not a valid run attribute.  It gives AttributeError when
+    # accessed or changed.
+    self.assertRaises(AttributeError, run.attrs.__getattribute__, 'foobar')
+    self.assertRaises(AttributeError, run.attrs.__setattr__, 'foobar', 'foo')
+
+  def _RunAccessor(self, method, options_dict, config_dict):
+    """Run the given accessor method of the BuilderRun class.
+
+    Create a BuilderRun object with the options and config provided and
+    then return the result of calling the given method on it.
+
+    Args:
+      method: A BuilderRun method to call.
+      options_dict: Extend default options with this.
+      config_dict: Extend default config with this.
+    Returns:
+      Result of calling the given method.
+    """
+    options = _ExtendDefaultOptions(**options_dict)
+    config = _ExtendDefaultConfig(**config_dict)
+    run = _NewBuilderRun(options=options, config=config)
+    return method(run)
+
+  def testDualEnableSetting(self):
+    settings = {
+        'prebuilts': cbuildbot_run.BuilderRun.ShouldUploadPrebuilts,
+        'postsync_patch': cbuildbot_run.BuilderRun.ShouldPatchAfterSync,
+    }
+
+    # Both option and config enabled should result in True.
+    # Create truth table with three variables in this order:
+    # <key> option value, <key> config value (e.g. <key> == 'prebuilts').
+    truth_table = cros_test_lib.TruthTable(inputs=[(True, True)])
+
+    for inputs in truth_table:
+      option_val, config_val = inputs
+      for key, accessor in settings.iteritems():
+        self.assertEquals(
+            self._RunAccessor(accessor, {key: option_val}, {key: config_val}),
+            truth_table.GetOutput(inputs))
+
+  def testShouldReexecAfterSync(self):
+    # If option and config have postsync_reexec enabled, and this file is not
+    # in the build root, then we expect ShouldReexecAfterSync to return True.
+
+    # Construct a truth table across three variables in this order:
+    # postsync_reexec option value, postsync_reexec config value, same_root.
+    truth_table = cros_test_lib.TruthTable(inputs=[(True, True, False)])
+
+    for inputs in truth_table:
+      option_val, config_val, same_root = inputs
+
+      if same_root:
+        build_root = os.path.dirname(os.path.dirname(__file__))
+      else:
+        build_root = DEFAULT_BUILDROOT
+
+      result = self._RunAccessor(
+          cbuildbot_run.BuilderRun.ShouldReexecAfterSync,
+          {'postsync_reexec': option_val, 'buildroot': build_root},
+          {'postsync_reexec': config_val})
+
+      self.assertEquals(result, truth_table.GetOutput(inputs))
+
+
+class GetVersionTest(cros_test_lib.MockTestCase):
+  """Test the GetVersion and GetVersionInfo methods of BuilderRun class."""
+
+  def testGetVersionInfo(self):
+    verinfo = object()
+
+    with mock.patch('cbuildbot_run.manifest_version.VersionInfo.from_repo',
+                    return_value=verinfo) as m:
+      result = cbuildbot_run.BuilderRun.GetVersionInfo(DEFAULT_BUILDROOT)
+      self.assertEquals(result, verinfo)
+
+      m.assert_called_once_with(DEFAULT_BUILDROOT)
+
+  def _TestGetVersionReleaseTag(self, release_tag):
+    with mock.patch.object(cbuildbot_run.BuilderRun, 'GetVersionInfo') as m:
+      verinfo_mock = mock.Mock()
+      verinfo_mock.chrome_branch = DEFAULT_CHROME_BRANCH
+      verinfo_mock.VersionString = mock.Mock(return_value='VS')
+      m.return_value = verinfo_mock
+
+      # Prepare a real BuilderRun object with a release tag.
+      run = _NewBuilderRun()
+      if release_tag is not None:
+        run.attrs.release_tag = 'RT'
+
+      # Run the test return the result.
+      result = run.GetVersion()
+      m.assert_called_once_with(DEFAULT_BUILDROOT)
+      if release_tag is None:
+        verinfo_mock.VersionString.assert_called_once()
+
+      return result
+
+  def testGetVersionReleaseTag(self):
+    result = self._TestGetVersionReleaseTag('RT')
+    self.assertEquals('R%s-%s' % (DEFAULT_CHROME_BRANCH, 'RT'), result)
+
+  def testGetVersionNoReleaseTag(self):
+    result = self._TestGetVersionReleaseTag(None)
+    expected_result = ('R%s-%s-b%s' %
+                       (DEFAULT_CHROME_BRANCH, 'VS', DEFAULT_BUILDNUMBER))
+    self.assertEquals(result, expected_result)
+
+
+class ChildBuilderRunTest(cros_test_lib.TestCase):
+  """Test the ChildBuilderRun class"""
+
+  def testInit(self):
+    crun = _NewChildBuilderRun(0)
+    self.assertEquals(DEFAULT_BUILDROOT, crun.buildroot)
+    self.assertEquals(DEFAULT_BUILDNUMBER, crun.buildnumber)
+    self.assertEquals(DEFAULT_BRANCH, crun.manifest_branch)
+    self.assertEquals(DEFAULT_OPTIONS, crun.options)
+    self.assertEquals(DEFAULT_CONFIG.child_configs[0], crun.config)
+    self.assertEquals('foo', crun.config.name)
+    self.assertEquals(0, crun.child_index)
+    self.assertTrue(isinstance(crun.attrs, cbuildbot_run.RunAttributes))
+
+
+if __name__ == '__main__':
+  cros_test_lib.main(level=logging.DEBUG)
