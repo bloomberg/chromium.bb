ca7ab0bb60207711c461cc7ca54c717d33a20b4a
prasadv@chromium.org <prasadv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-27 03:38:52 +0000

    [Telemetry]: Increase wait time to fix timeout issues on some bots. Octane benchmark timeout on one of the bot, increasing wait time to see if this fixes the issue.
    Review URL: https://codereview.chromium.org/89793002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237493 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b4a9b28667ca9c4f96445baa41942e4ce70b9358

--- tools/perf/benchmarks/octane.py
+++ tools/perf/benchmarks/octane.py
@@ -33,7 +33,7 @@ class _OctaneMeasurement(page_measurement.PageMeasurement):
 
   def MeasurePage(self, _, tab, results):
     tab.WaitForJavaScriptExpression(
-        'completed && !document.getElementById("progress-bar-container")', 300)
+        'completed && !document.getElementById("progress-bar-container")', 600)
 
     results_log = tab.EvaluateJavaScript('__results')
     all_scores = []
