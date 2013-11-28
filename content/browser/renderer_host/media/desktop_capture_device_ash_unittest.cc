e738feea2c5038793ae4a91b00f8bda4702a715e
ernstm@chromium.org <ernstm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-28 01:09:27 +0000

    Revert 237645 "Fix memory leak in DesktopCaptureDeviceAshTest on..."
    DesktopCaptureApiTest.ChooseDesktopMedia still failing.
    
    > Fix memory leak in DesktopCaptureDeviceAshTest on Linux Chromium OS ASAN.
    > 
    > The aura::test::TestWindowDelegate should be held in a scoped_ptr.
    > 
    > TBR=hshi@chromium.org
    > TEST=run content_unittests locally.
    > BUG=
    > 
    > Review URL: https://codereview.chromium.org/93123002
    
    TBR=hshi@chromium.org
    
    Review URL: https://codereview.chromium.org/93363002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237652 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8fe8321285e6ca79094a90ef8e46c4cbc586beb4

--- content/browser/renderer_host/media/desktop_capture_device_ash_unittest.cc
+++ content/browser/renderer_host/media/desktop_capture_device_ash_unittest.cc
@@ -64,8 +64,8 @@ class DesktopCaptureDeviceAshTest : public testing::Test {
     // can use gfx::NativeWindow::GetWindowAtScreenPoint() to locate the
     // root window associated with the primary display.
     gfx::Rect desktop_bounds = root_window()->bounds();
-    window_delegate_.reset(new aura::test::TestWindowDelegate());
-    desktop_window_.reset(new aura::Window(window_delegate_.get()));
+    desktop_window_.reset(new aura::Window(
+        new aura::test::TestWindowDelegate()));
     desktop_window_->Init(ui::LAYER_TEXTURED);
     desktop_window_->SetBounds(desktop_bounds);
     aura::client::ParentWindowWithContext(
@@ -77,7 +77,6 @@ class DesktopCaptureDeviceAshTest : public testing::Test {
     helper_->RunAllPendingInMessageLoop();
     root_window()->RemoveChild(desktop_window_.get());
     desktop_window_.reset();
-    window_delegate_.reset();
     helper_->TearDown();
   }
 
@@ -88,7 +87,6 @@ class DesktopCaptureDeviceAshTest : public testing::Test {
   BrowserThreadImpl browser_thread_for_ui_;
   scoped_ptr<aura::test::AuraTestHelper> helper_;
   scoped_ptr<aura::Window> desktop_window_;
-  scoped_ptr<aura::test::TestWindowDelegate> window_delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(DesktopCaptureDeviceAshTest);
 };
