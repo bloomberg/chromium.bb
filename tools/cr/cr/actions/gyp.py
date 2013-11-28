85f2a8d3a14e7179e4ebebe3360273bb4d0825a8
iancottrell@chromium.org <iancottrell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-11-28 17:58:54 +0000

    Adding the gyp command.
    BUG=316397
    
    Review URL: https://codereview.chromium.org/91863002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@237813 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 28acb80ab30aece2e8a59db74bcff0d1fc992a50

---
+++ tools/cr/cr/actions/gyp.py
@@ -0,0 +1,26 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A module to add gyp support to cr."""
+
+import cr
+
+
+class GypPrepareOut(cr.PrepareOut):
+  """A prepare action that runs gyp whenever you select an output directory."""
+
+  ENABLED = cr.Config.From(
+      GYP_GENERATORS='ninja',
+      GYP_GENERATOR_FLAGS='output_dir={CR_OUT_BASE} config={CR_BUILDTYPE}',
+  )
+
+  def Prepare(self, context):
+    if context.verbose >= 1:
+      print context.Substitute('Invoking gyp with {GYP_GENERATOR_FLAGS}')
+    cr.Host.Execute(
+        context,
+        '{CR_SRC}/build/gyp_chromium',
+        '--depth={CR_SRC}',
+        '--check'
+    )
