cdee854df94d740d4f7c62094a3df8a6e638859c
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-11-28 12:01:20 +0000

    Remove usage of deprecated V8 APIs in V8ArrayBufferCustom.h
    BUG=324225
    R=dslomov@chromium.org
    
    Review URL: https://codereview.chromium.org/94693003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@162824 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ed4929ab96245a22e06772ea2cf0db0dc101a991

--- third_party/WebKit/Source/bindings/v8/custom/V8ArrayBufferCustom.h
+++ third_party/WebKit/Source/bindings/v8/custom/V8ArrayBufferCustom.h
@@ -38,14 +38,14 @@ class V8ArrayBufferDeallocationObserver: public WTF::ArrayBufferDeallocationObse
 public:
     virtual void arrayBufferDeallocated(unsigned sizeInBytes)
     {
-        v8::V8::AdjustAmountOfExternalAllocatedMemory(-static_cast<int>(sizeInBytes));
+        v8::Isolate::GetCurrent()->AdjustAmountOfExternalAllocatedMemory(-static_cast<int>(sizeInBytes));
     }
     static V8ArrayBufferDeallocationObserver* instanceTemplate();
 
 protected:
     virtual void blinkAllocatedMemory(unsigned sizeInBytes) OVERRIDE
     {
-        v8::V8::AdjustAmountOfExternalAllocatedMemory(static_cast<int>(sizeInBytes));
+        v8::Isolate::GetCurrent()->AdjustAmountOfExternalAllocatedMemory(static_cast<int>(sizeInBytes));
     }
 };
 
