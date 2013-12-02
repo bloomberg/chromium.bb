179f049748ad6231d3eac27093b2d98d95a978d0
vivek.vg@samsung.com <vivek.vg@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-02 17:40:02 +0000

    Update the metrics side bar pane and computed style pane after main frame resize.
    The metrics pane showing width and height of the main frame should be updated upon receiving
    the resize event. This also applies while docking/undocking the DevTools window.
    
    BUG=322997
    
    Review URL: https://codereview.chromium.org/84643002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163004 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 3f51cd921d3b6cf1c09375de938d3ef45638e90e

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
@@ -1003,6 +1003,7 @@ void InspectorPageAgent::didResizeMainFrame()
 {
     if (m_enabled && m_state->getBoolean(PageAgentState::showSizeOnResize))
         m_overlay->showAndHideViewSize(m_state->getBoolean(PageAgentState::showGridOnResize));
+    m_frontend->frameResized();
 }
 
 void InspectorPageAgent::didRecalculateStyle()
