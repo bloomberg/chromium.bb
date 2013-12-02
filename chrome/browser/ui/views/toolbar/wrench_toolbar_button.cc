a33ba42aa54bdfa55233226a7f61a101ab0bbef5
michaelpg@chromium.org <michaelpg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-02 23:43:15 +0000

    Show focus rect on Chrome menu button
    When the Chrome hotdog menu button is focused by pressing Alt-Shift-T or F6 on
    Windows/ChromeOS and tabbing over to itr, show the rectangular focus indicator.
    
    BUG=320759
    
    Review URL: https://codereview.chromium.org/83513004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238233 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f9c764b9f07e6b1ebed375480e421376d7139317

--- chrome/browser/ui/views/toolbar/wrench_toolbar_button.cc
+++ chrome/browser/ui/views/toolbar/wrench_toolbar_button.cc
@@ -34,6 +34,7 @@ void WrenchToolbarButton::OnPaint(gfx::Canvas* canvas) {
 
   wrench_icon_painter_->Paint(
       canvas, GetThemeProvider(), gfx::Rect(size()), GetCurrentBezelType());
+  OnPaintFocusBorder(canvas);
 }
 
 void WrenchToolbarButton::ScheduleWrenchIconPaint() {
