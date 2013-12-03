6d960a004bdc709cbbd476483a8655b3c7cf7f6e
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 12:28:03 +0000

    Add a no-op measurement
    Some pages you just want to let run while the profiler runs. This
    no_op measurement does just that.
    
    R=tonyg
    CC=eseidel
    
    Review URL: https://codereview.chromium.org/100933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238370 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 764bad33b598841333d4d1674bf5667957ada551

---
+++ tools/perf/measurements/no_op.py
@@ -0,0 +1,21 @@
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry.page import page_measurement
+
+class NoOp(page_measurement.PageMeasurement):
+  def __init__(self):
+    super(NoOp, self).__init__('no_op')
+
+  def CanRunForPage(self, page):
+    return hasattr(page, 'no_op')
+
+  def WillRunAction(self, page, tab, action):
+    pass
+
+  def DidRunAction(self, page, tab, action):
+    pass
+
+  def MeasurePage(self, page, tab, results):
+    pass
