0b838a9696ca9c0688371d5f2af65acd1b737540
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-03 04:36:03 +0000

    [Telemetry] Disable dom_perf on Android because it times out.
    Linux is a casualty because we currently don't have a clean system to
    differentiate the platforms.
    
    TBR=tonyg@chromium.org
    NOTRY=True
    BUG=
    
    Review URL: https://codereview.chromium.org/101043002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238298 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e11548d9348e7aec4a5d5c5b24dd5e0b1a89f6b3

--- tools/perf/benchmarks/dom_perf.py
+++ tools/perf/benchmarks/dom_perf.py
@@ -5,6 +5,7 @@
 import json
 import math
 import os
+import sys
 
 from telemetry import test
 from telemetry.core import util
@@ -73,6 +74,8 @@ class DomPerf(test.Test):
   Scores are not comparable across benchmark suite versions and higher scores
   means better performance: Bigger is better!"""
   test = _DomPerfMeasurement
+
+  enabled = not sys.platform.startwith('linux')
 
   def CreatePageSet(self, options):
     dom_perf_dir = os.path.join(util.GetChromiumSrcDir(), 'data', 'dom_perf')
