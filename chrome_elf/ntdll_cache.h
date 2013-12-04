8dabd9c81ae66ceba32987f86d9c46ada68a524e
caitkp@chromium.org <caitkp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 03:12:52 +0000

    Cache ntdll proc addresses in ELF
    BUG=
    
    Review URL: https://codereview.chromium.org/85403005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238539 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 77f43f93e32a3d9328329d8853f5e11f2a1909f8

---
+++ chrome_elf/ntdll_cache.h
@@ -0,0 +1,15 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_ELF_NTDLL_CACHE_H_
+#define CHROME_ELF_NTDLL_CACHE_H_
+
+#include "chrome_elf/chrome_elf_types.h"
+
+// Caches the addresses of all functions exported by ntdll in  |g_ntdll_lookup|.
+void InitCache();
+
+extern FunctionLookupTable g_ntdll_lookup;
+
+#endif  // CHROME_ELF_NTDLL_CACHE_H_
