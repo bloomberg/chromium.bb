6c1b14a3549d6fd3eca19f1b3bf2f0307434eaba
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 09:26:42 +0000

    Disable buffering in asan_symbolize.py This should help some buildbots that are timing out otherwise.
    BUG=323556
    TBR=maruel@chromium.org
    TEST=interactive_ui_tests pass on the Mac ASan 64 bots
    
    Review URL: https://codereview.chromium.org/104233002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238657 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c3998d38cf9c8f3008dc6c6b79a696bda5399115

--- tools/valgrind/asan/asan_symbolize.py
+++ tools/valgrind/asan/asan_symbolize.py
@@ -6,6 +6,7 @@
 
 from third_party import asan_symbolize
 
+import os
 import re
 import sys
 
@@ -16,7 +17,31 @@ def fix_filename(file_name):
   file_name = re.sub(".*crtstuff.c:0", "???:0", file_name)
   return file_name
 
+class Unbuffered(object):
+  """Disable buffering on a file object."""
+  def __init__(self, stream):
+    self.stream = stream
+
+  def write(self, data):
+    self.stream.write(data)
+    if '\n' in data:
+      self.stream.flush()
+
+  def __getattr__(self, attr):
+    return getattr(self.stream, attr)
+
+
+def disable_buffering():
+  """Makes this process and child processes stdout unbuffered."""
+  if not os.environ.get('PYTHONUNBUFFERED'):
+    # Since sys.stdout is a C++ object, it's impossible to do
+    # sys.stdout.write = lambda...
+    sys.stdout = Unbuffered(sys.stdout)
+    os.environ['PYTHONUNBUFFERED'] = 'x'
+
+
 def main():
+  disable_buffering()
   loop = asan_symbolize.SymbolizationLoop(binary_name_filter=fix_filename)
   loop.process_stdin()
 
