4d5cb54081317a186eb2e74dc21f7b91756e9b4b
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-04 17:26:19 +0000

    Reland 238671 "Added net::NetworkInterface::interface_index."
    net_unittests still failed after reverting the original CL, so
    it obviously wasn't the cause of the failures.
    
    > Revert 238671 "Added net::NetworkInterface::interface_index."
    > 
    > Speculative revert to see if it fixes the issues with net_unittests
    > on WinXP: http://build.chromium.org/p/chromium.win/builders/XP%20Tests%20%282%29/builds/32133/steps/net_unittests/logs/stdio
    > 
    > > Added net::NetworkInterface::interface_index.
    > > Not implemented for Android.
    > > 
    > > BUG=319068
    > > 
    > > Review URL: https://codereview.chromium.org/100703002
    > 
    > TBR=vitalybuka@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/101403004
    
    TBR=dubroy@chromium.org
    
    Review URL: https://codereview.chromium.org/105053002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238715 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 649dd8126f846d18fb434435ce18478144e7bc1c

--- net/base/net_util_posix.cc
+++ net/base/net_util_posix.cc
@@ -81,8 +81,12 @@ bool GetNetworkList(NetworkInterfaceList* networks) {
                          &address,
                          &network_prefix));
 
+    CHECK(network_tokenizer.GetNext());
+    uint32 index = 0;
+    CHECK(base::StringToUint(network_tokenizer.token(), &index));
+
     networks->push_back(
-        NetworkInterface(name, address, network_prefix));
+        NetworkInterface(name, index, address, network_prefix));
   }
   return true;
 #else
@@ -108,6 +112,7 @@ bool GetNetworkList(NetworkInterfaceList* networks) {
     struct sockaddr* addr = interface->ifa_addr;
     if (!addr)
       continue;
+
     // Skip unspecified addresses (i.e. made of zeroes) and loopback addresses
     // configured on non-loopback interfaces.
     int addr_size = 0;
@@ -144,7 +149,9 @@ bool GetNetworkList(NetworkInterfaceList* networks) {
         }
       }
 
-      networks->push_back(NetworkInterface(name, address.address(), net_mask));
+      networks->push_back(
+          NetworkInterface(name, if_nametoindex(name.c_str()),
+                           address.address(), net_mask));
     }
   }
 
--- net/base/net_util_unittest.cc
+++ net/base/net_util_unittest.cc
@@ -21,6 +21,15 @@
 #include "testing/gtest/include/gtest/gtest.h"
 #include "url/gurl.h"
 
+#if defined(OS_WIN)
+#include <iphlpapi.h>
+#include <objbase.h>
+#include "base/win/windows_version.h"
+#pragma comment(lib, "iphlpapi.lib")
+#elif !defined(OS_ANDROID)
+#include <net/if.h>
+#endif  // OS_WIN
+
 namespace net {
 
 namespace {
@@ -3310,6 +3319,28 @@ TEST(NetUtilTest, GetNetworkList) {
     EXPECT_FALSE(all_zeroes);
     EXPECT_GT(it->network_prefix, 1u);
     EXPECT_LE(it->network_prefix, it->address.size() * 8);
+
+#if defined(OS_WIN)
+    if (base::win::GetVersion() >= base::win::VERSION_VISTA) {
+      // WIN_XP has no used functions.
+      NET_LUID luid;
+      // On Windows |name| is NET_LUID.
+      ConvertInterfaceIndexToLuid(it->interface_index, &luid);
+      GUID guid;
+      ConvertInterfaceLuidToGuid(&luid, &guid);
+      LPOLESTR name;
+      StringFromCLSID(guid, &name);
+      EXPECT_STREQ(UTF8ToWide(it->name).c_str(), name);
+      CoTaskMemFree(name);
+    } else {
+      EXPECT_LT(it->interface_index, 1u << 24u);  // Must fit 0.x.x.x.
+      EXPECT_NE(it->interface_index, 0u);  // 0 means to use default.
+    }
+#elif !defined(OS_ANDROID)
+    char name[IF_NAMESIZE];
+    EXPECT_TRUE(if_indextoname(it->interface_index, name));
+    EXPECT_STREQ(it->name.c_str(), name);
+#endif
   }
 }
 
