3833e076db225d90f8a5eded9ede35d2ea66d7b7
shrikant@chromium.org <shrikant@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-05 00:16:42 +0000

    Changes to fix issue, when user tries to launch instance of new window from desktop when ASH is already open in Metro mode. Current implementation causes new window to be opened in desktop mode.
    TESTS=Please follow steps as mentioned in bug report.
    R=cpu@chromium.org, gab@chromium.org, sky@chromium.org, ananta@chromium.org
    BUG=320891
    
    Review URL: https://codereview.chromium.org/103513002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@238828 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bfc911b6fa6ab75ba047e6f8bff13c68dca1725c

--- chrome/browser/ui/startup/startup_browser_creator.cc
+++ chrome/browser/ui/startup/startup_browser_creator.cc
@@ -9,6 +9,7 @@
 
 #include "apps/app_load_service.h"
 #include "apps/switches.h"
+#include "ash/shell.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
@@ -296,9 +297,21 @@ bool StartupBrowserCreator::LaunchBrowser(
     StartupBrowserCreatorImpl lwp(cur_dir, command_line, this, is_first_run);
     const std::vector<GURL> urls_to_launch =
         GetURLsFromCommandLine(command_line, cur_dir, profile);
+    chrome::HostDesktopType host_desktop_type =
+        chrome::HOST_DESKTOP_TYPE_NATIVE;
+
+#if defined(OS_WIN) && defined(USE_ASH)
+    // We want to maintain only one type of instance for now, either ASH
+    // or desktop.
+    // TODO(shrikant): Remove this code once we decide on running both desktop
+    // and ASH instances side by side.
+    if (ash::Shell::HasInstance())
+      host_desktop_type = chrome::HOST_DESKTOP_TYPE_ASH;
+#endif
+
     const bool launched = lwp.Launch(profile, urls_to_launch,
                                in_synchronous_profile_launch_,
-                               chrome::HOST_DESKTOP_TYPE_NATIVE);
+                               host_desktop_type);
     in_synchronous_profile_launch_ = false;
     if (!launched) {
       LOG(ERROR) << "launch error";
