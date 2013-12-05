81ee4f7b1f11c4419cc1fba772e04c962bc88ae8
costan@gmail.com <costan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-05 02:54:08 +0000

    UseCounter for File.lastModifiedDate.
    The latest ED for the File API prescribes a lastModified attribute that
    returns a number and removes the lastModifiedDate that returned a date.
    This will help us decide if we can deprecate and remove the old
    attribute.
    
    TEST=Chromium compiles.
    BUG=
    
    Review URL: https://codereview.chromium.org/105763002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163211 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 325bff8e29bb6af867885cb8a15b13081752796b

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -271,6 +271,7 @@ public:
         SVGPointMatrixTransform,
         HTMLHtmlElementManifest,
         DOMFocusInOutEvent,
+        FileGetLastModifiedDate,
         // Add new features immediately above this line. Don't change assigned
         // numbers of each items, and don't reuse unused slots.
         NumberOfFeatures, // This enum value must be last.
