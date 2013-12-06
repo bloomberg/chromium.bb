bloomberg/chromium.bb

commit 0f80fc8f2a514e5a6855fb418650b68b3dc31875

Prepare for color input datalist support on Android
We are passing datalist suggestions when opening the color chooser.
Also adding color to input types that support datalist on Android.

BUG=242455

Review URL: https://codereview.chromium.org/23236002

git-svn-id: svn://svn.chromium.org/blink/trunk@161893 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 2efc41027584e594ddd1794730a2bae20d5506d6

third_party/WebKit/Source/web/ColorChooserUIController.cpp
@@ -28,6 +28,9 @@
 
 #include "ChromeClientImpl.h"
 #include "WebColorChooser.h"
+#include "WebColorSuggestion.h"
+#include "WebViewClient.h"
+#include "WebViewImpl.h"
 #include "platform/ColorChooserClient.h"
 #include "platform/graphics/Color.h"
 #include "public/platform/WebColor.h"
@@ -80,7 +83,10 @@ void ColorChooserUIController::didEndChooser()
 void ColorChooserUIController::openColorChooser()
 {
     ASSERT(!m_chooser);
-    m_chooser = m_chromeClient->createWebColorChooser(this, static_cast<WebColor>(m_client->currentColor().rgb()));
+    WebViewClient* webViewClient = static_cast<WebViewImpl*>(m_chromeClient->webView())->client();
+    if (!webViewClient)
+        return;
+    m_chooser = adoptPtr(webViewClient->createColorChooser(this, static_cast<WebColor>(m_client->currentColor().rgb())));
 }
 
 } // namespace blink

third_party/WebKit/Source/core/html/forms/ColorInputType.cpp
@@ -53,6 +53,11 @@ namespace WebCore {
 
 using namespace HTMLNames;
 
+// Upper limit of number of datalist suggestions shown.
+static const unsigned maxSuggestions = 1000;
+// Upper limit for the length of the labels for datalist suggestions.
+static const unsigned maxSuggestionLabelLength = 1000;
+
 static bool isValidColorString(const String& value)
 {
     if (value.isEmpty())
@@ -225,9 +230,9 @@ bool ColorInputType::shouldShowSuggestions() const
     return false;
 }
 
-Vector<Color> ColorInputType::suggestions() const
+Vector<ColorSuggestion> ColorInputType::suggestions() const
 {
-    Vector<Color> suggestions;
+    Vector<ColorSuggestion> suggestions;
     if (RuntimeEnabledFeatures::dataListElementEnabled()) {
         HTMLDataListElement* dataList = element().dataList();
         if (dataList) {
@@ -238,7 +243,10 @@ Vector<Color> ColorInputType::suggestions() const
                 Color color(option->value());
                 if (!color.isValid())
                     continue;
-                suggestions.append(color);
+                ColorSuggestion suggestion(color, option->label().left(maxSuggestionLabelLength));
+                suggestions.append(suggestion);
+                if (suggestions.size() >= maxSuggestions)
+                    break;
             }
         }
     }

third_party/WebKit/Source/core/html/forms/ColorInputType.h
@@ -47,7 +47,7 @@ public:
     virtual IntRect elementRectRelativeToRootView() const OVERRIDE;
     virtual Color currentColor() OVERRIDE;
     virtual bool shouldShowSuggestions() const OVERRIDE;
-    virtual Vector<Color> suggestions() const OVERRIDE;
+    virtual Vector<ColorSuggestion> suggestions() const OVERRIDE;
 
 private:
     ColorInputType(HTMLInputElement& element) : BaseClickableWithKeyInputType(element) { }

third_party/WebKit/Source/core/rendering/RenderThemeChromiumAndroid.cpp
@@ -28,6 +28,7 @@
 
 #include "CSSValueKeywords.h"
 #include "UserAgentStyleSheets.h"
+#include "core/html/forms/InputTypeNames.h"
 #include "core/platform/ScrollbarTheme.h"
 #include "core/rendering/PaintInfo.h"
 #include "core/rendering/RenderMediaControls.h"
@@ -105,7 +106,8 @@ int RenderThemeChromiumAndroid::menuListArrowPadding() const
 
 bool RenderThemeChromiumAndroid::supportsDataListUI(const AtomicString& type) const
 {
-    return false;
+    // FIXME: Add other input types.
+    return type == InputTypeNames::color();
 }
 
 } // namespace WebCore

third_party/WebKit/Source/platform/ColorSuggestion.h
@@ -28,31 +28,24 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef ColorChooserClient_h
-#define ColorChooserClient_h
+#ifndef ColorSuggestion_h
+#define ColorSuggestion_h
 
-#include "platform/ColorChooser.h"
-#include "platform/ColorSuggestion.h"
-#include "platform/PlatformExport.h"
-#include "platform/geometry/IntRect.h"
-#include "wtf/OwnPtr.h"
-#include "wtf/PassOwnPtr.h"
-#include "wtf/Vector.h"
+#include "platform/graphics/Color.h"
+#include "wtf/text/WTFString.h"
 
 namespace WebCore {
 
-class PLATFORM_EXPORT ColorChooserClient {
-public:
-    virtual ~ColorChooserClient();
+struct ColorSuggestion {
+    Color color;
+    String label;
 
-    virtual void didChooseColor(const Color&) = 0;
-    virtual void didEndChooser() = 0;
-    virtual IntRect elementRectRelativeToRootView() const = 0;
-    virtual Color currentColor() = 0;
-    virtual bool shouldShowSuggestions() const = 0;
-    virtual Vector<ColorSuggestion> suggestions() const = 0;
+    ColorSuggestion(const Color& colorValue, const String& label)
+    : color(colorValue)
+    , label(label)
+    { }
 };
 
 } // namespace WebCore
 
-#endif // ColorChooserClient_h
+#endif // ColorSuggestion_h

third_party/WebKit/Source/web/ChromeClientImpl.cpp
@@ -44,6 +44,7 @@
 #include "WebAXObject.h"
 #include "WebAutofillClient.h"
 #include "WebColorChooser.h"
+#include "WebColorSuggestion.h"
 #include "WebConsoleMessage.h"
 #include "WebCursorInfo.h"
 #include "WebFileChooserCompletionImpl.h"
@@ -615,13 +616,6 @@ PassOwnPtr<ColorChooser> ChromeClientImpl::createColorChooser(ColorChooserClient
         controller = adoptPtr(new ColorChooserUIController(this, chooserClient));
     controller->openUI();
     return controller.release();
-}
-PassOwnPtr<WebColorChooser> ChromeClientImpl::createWebColorChooser(WebColorChooserClient* chooserClient, const WebColor& initialColor)
-{
-    WebViewClient* client = m_webView->client();
-    if (!client)
-        return nullptr;
-    return adoptPtr(client->createColorChooser(chooserClient, initialColor));
 }
 
 PassRefPtr<DateTimeChooser> ChromeClientImpl::openDateTimeChooser(DateTimeChooserClient* pickerClient, const DateTimeChooserParameters& parameters)

third_party/WebKit/Source/web/ChromeClientImpl.h
@@ -131,7 +131,6 @@ public:
     virtual void annotatedRegionsChanged();
     virtual bool paintCustomOverhangArea(WebCore::GraphicsContext*, const WebCore::IntRect&, const WebCore::IntRect&, const WebCore::IntRect&);
     virtual PassOwnPtr<WebCore::ColorChooser> createColorChooser(WebCore::ColorChooserClient*, const WebCore::Color&) OVERRIDE;
-    PassOwnPtr<WebColorChooser> createWebColorChooser(WebColorChooserClient*, const WebColor&);
     virtual PassRefPtr<WebCore::DateTimeChooser> openDateTimeChooser(WebCore::DateTimeChooserClient*, const WebCore::DateTimeChooserParameters&) OVERRIDE;
     virtual void runOpenPanel(WebCore::Frame*, PassRefPtr<WebCore::FileChooser>);
     virtual void enumerateChosenDirectory(WebCore::FileChooser*);

third_party/WebKit/Source/web/ColorChooserPopupUIController.cpp
@@ -82,10 +82,10 @@ IntSize ColorChooserPopupUIController::contentSize()
 
 void ColorChooserPopupUIController::writeDocument(DocumentWriter& writer)
 {
-    Vector<Color> suggestions = m_client->suggestions();
+    Vector<ColorSuggestion> suggestions = m_client->suggestions();
     Vector<String> suggestionValues;
     for (unsigned i = 0; i < suggestions.size(); i++)
-        suggestionValues.append(suggestions[i].serialized());
+        suggestionValues.append(suggestions[i].color.serialized());
     IntRect anchorRectInScreen = m_chromeClient->rootViewToScreen(m_client->elementRectRelativeToRootView());
 
     PagePopupClient::addString("<!DOCTYPE html><head><meta charset='UTF-8'><style>\n", writer);

commit 1b0181d8d8c45689db296f79cc61a851d97892b7

Fix color chooser dialog not opening on Android
Regression caused by r161893. It caused the color chooser dialog to not open at all on Android.

BUG=None

Review URL: https://codereview.chromium.org/101793004

git-svn-id: svn://svn.chromium.org/blink/trunk@163314 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: e96288dc7852ba309d2f10f699c3ca4652f4b4c9

third_party/WebKit/Source/web/ColorChooserUIController.cpp
@@ -86,7 +86,8 @@ void ColorChooserUIController::openColorChooser()
     WebViewClient* webViewClient = static_cast<WebViewImpl*>(m_chromeClient->webView())->client();
     if (!webViewClient)
         return;
-    m_chooser = adoptPtr(webViewClient->createColorChooser(this, static_cast<WebColor>(m_client->currentColor().rgb())));
+    m_chooser = adoptPtr(webViewClient->createColorChooser(
+        this, static_cast<WebColor>(m_client->currentColor().rgb()), m_client->suggestions()));
 }
 
 } // namespace blink