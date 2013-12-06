62c11c27aee7ceef279812d3812a7f3e505f4f4a
sammc@chromium.org <sammc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-06 02:53:54 +0000

    Disable the AppKeepAliveService.
    Keeping Chrome alive while packaged app background pages allowed apps to
    keep the browser process alive with no visible windows or tray icons.
    This disables that keep alive until a decision is reached on how to
    handle app quits in a way that avoids this issue.
    
    BUG=326040
    
    Review URL: https://codereview.chromium.org/94113003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239106 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: da58e48b6571ec4a2ca39958b75fec1142a19ea6

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -220,7 +220,6 @@ EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   DownloadServiceFactory::GetInstance();
 #if defined(ENABLE_EXTENSIONS)
   AppShortcutManagerFactory::GetInstance();
-  apps::AppKeepAliveServiceFactory::GetInstance();
   apps::AppLoadServiceFactory::GetInstance();
   apps::AppRestoreServiceFactory::GetInstance();
   apps::ShellWindowGeometryCache::Factory::GetInstance();
