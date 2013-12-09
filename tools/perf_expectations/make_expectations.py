7013f86ac56646025cba194e147e97d6ce4bf0d1
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-09 23:41:47 +0000

    Revert 239579 "Update nacl_helper-data expectations"
    > Update nacl_helper-data expectations
    > 
    > Increase of about 2.5K. Confirmed with jln@ on #chromium that this is
    > reasonable from r239550.
    > 
    > Also, fix file mode opening in make_expectations.py so that
    > the script works on Windows and doesn't change every line.
    > 
    > R=vadimt@chromium.org, andrewhayden@chromium.org, jln@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/110793002
    
    TBR=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/107763003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239606 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bfbbba4aa52a1f6efa5109916edd1a6ffb4fefb1

--- tools/perf_expectations/make_expectations.py
+++ tools/perf_expectations/make_expectations.py
@@ -33,7 +33,7 @@ USAGE = ''
 
 def ReadFile(filename):
   try:
-    file = open(filename, 'rb')
+    file = open(filename, 'r')
   except IOError, e:
     print >> sys.stderr, ('I/O Error reading file %s(%s): %s' %
                           (filename, e.errno, e.strerror))
@@ -104,7 +104,7 @@ def GetRowDigest(rowdata, key):
 def WriteJson(filename, data, keys, calculate_sha1=True):
   """Write a list of |keys| in |data| to the file specified in |filename|."""
   try:
-    file = open(filename, 'wb')
+    file = open(filename, 'w')
   except IOError, e:
     print >> sys.stderr, ('I/O Error writing file %s(%s): %s' %
                           (filename, e.errno, e.strerror))
