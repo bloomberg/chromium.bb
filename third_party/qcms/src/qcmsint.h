87834158dbc7f9bfb41bc706a258975e4cff8b29
noel@chromium.org <noel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 16:42:41 +0000

    Apply libqcms thread safety patch
    qcms was patched in https://bugzilla.mozilla.org/show_bug.cgi?id=853169 to
    resolve thread safety issues when using libqcms library for threaded image
    decoding in Firefox (https://bugzilla.mozilla.org/show_bug.cgi?id=716140).
    Apply the thread safety patch to the chrome repository.
    
    BUG=327484
    TBR=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/108563012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240120 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 63588a5cb30196cb7527f367a68947ad54a5ef47

--- third_party/qcms/src/qcmsint.h
+++ third_party/qcms/src/qcmsint.h
@@ -304,3 +304,21 @@ void qcms_transform_data_rgba_out_lut_sse1(qcms_transform *transform,
                                           qcms_format_type output_format);
 
 extern qcms_bool qcms_supports_iccv4;
+
+
+#ifdef _MSC_VER
+
+long __cdecl _InterlockedIncrement(long volatile *);
+long __cdecl _InterlockedDecrement(long volatile *);
+#pragma intrinsic(_InterlockedIncrement)
+#pragma intrinsic(_InterlockedDecrement)
+
+#define qcms_atomic_increment(x) _InterlockedIncrement((long volatile *)&x)
+#define qcms_atomic_decrement(x) _InterlockedDecrement((long volatile*)&x)
+
+#else
+
+#define qcms_atomic_increment(x) __sync_add_and_fetch(&x, 1)
+#define qcms_atomic_decrement(x) __sync_sub_and_fetch(&x, 1)
+
+#endif
