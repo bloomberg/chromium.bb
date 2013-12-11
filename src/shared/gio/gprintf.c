5566c21bb75e13a22d7427df19c670f49ffbe2e2
bsy@google.com <bsy@google.com@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2013-12-11 23:52:59 +0000

    Use va_copy when available. Use knowledge of implementation on MSVS pre-2013 to polyfill.
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3181
    TEST= run_gprintf_test
    R=ncbray@chromium.org
    
    Review URL: https://codereview.chromium.org/110883008
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12528 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 7fc54845ba425b9a23eff6d02d9f508a9d7795e6

--- src/shared/gio/gprintf.c
+++ src/shared/gio/gprintf.c
@@ -1,7 +1,7 @@
 /*
- * Copyright 2008 The Native Client Authors. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can
- * be found in the LICENSE file.
+ * Copyright (c) 2008 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
  */
 
 /*
@@ -13,16 +13,35 @@
 
 #include "native_client/src/shared/gio/gio.h"
 
+/*
+ * Windows Visual Studio pre-2013 does not provide va_copy.  When
+ * compiled with a pre-2013 Visual Studio, we use knowledge of MSVS's
+ * implementation to poly-fill.  This is ugly, but the API for earlier
+ * versions is extremely unlikely to change, since it would break
+ * other existing code that depends on it.
+ */
+#if NACL_WINDOWS
+/* check definition of _MSC_VER first, in case we switch to clang */
+# if defined(_MSC_VER) && _MSC_VER < 1800
+#  define va_copy(dst, src) do { (dst) = (src); } while (0)
+# endif
+#endif
+
 size_t gvprintf(struct Gio *gp,
-             char const *fmt,
-             va_list    ap) {
+                char const *fmt,
+                va_list    ap) {
   size_t    bufsz = 1024;
   char      *buf = malloc(bufsz);
   int       rv;
+  va_list   ap_copy;
 
   if (!buf) return -1;
 
-  while ((rv = vsnprintf(buf, bufsz, fmt, ap)) < 0 || (unsigned) rv >= bufsz) {
+  va_copy(ap_copy, ap);
+
+  while ((rv = vsnprintf(buf, bufsz, fmt, ap_copy)) < 0 ||
+         (unsigned) rv >= bufsz) {
+    va_end(ap_copy);
     free(buf);
     buf = 0;
 
@@ -30,6 +49,13 @@ size_t gvprintf(struct Gio *gp,
      * Since the buffer size wasn't big enough, we want to double it.
      * Stop doubling when we reach SIZE_MAX / 2, though, otherwise we
      * risk wraparound.
+     *
+     * On Windows, vsnprintf returns -1 if the supplied buffer is not
+     * large enough; on Linux and OSX, it returns the number of actual
+     * characters that would have been output (excluding the NUL
+     * byte), which means a single resize would have been sufficient.
+     * Since buffer size increase should be infrequent, we do doubly
+     * on Linux and OSX as well.
      */
     if (bufsz < SIZE_MAX / 2) {
       bufsz *= 2;
@@ -39,7 +65,9 @@ size_t gvprintf(struct Gio *gp,
     if (!buf) {
       return (size_t) -1;
     }
+    va_copy(ap_copy, ap);
   }
+  va_end(ap_copy);
   if (rv >= 0) {
     rv = (int) (*gp->vtbl->Write)(gp, buf, rv);
   }
@@ -47,7 +75,6 @@ size_t gvprintf(struct Gio *gp,
 
   return rv;
 }
-
 
 size_t gprintf(struct Gio *gp,
                char const *fmt, ...) {
