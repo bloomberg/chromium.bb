3b062c68736eafb1d61fffb2a0691bd57cc73f42
vadimt@chromium.org <vadimt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 01:41:14 +0000

    Revert 239920 "Revert 239758 "Enable Audio/Video dev monitoring ..."
    Reverting the revert. Congrats, the original CL wasn't the cause for the failure; sorry for the mess.
    
    > Revert 239758 "Enable Audio/Video dev monitoring and Video captu..."
    > 
    > > Enable Audio/Video dev monitoring and Video capture via AVFoundation by default.
    > > 
    > > Flipping the command line flag from explicitly enabling AVFoundation to
    > > disable AVFoundation.
    > > 
    > > In the past there was a startup performance regression (http://crbug.com/311325 
    > > and http://crbug.com/311437), those type of regression should be monitored 
    > > carefully during the next couple of days.
    > > 
    > > BUG=288562
    > > 
    > > Review URL: https://codereview.chromium.org/101753003
    > 
    > TBR=mcasas@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/112033003
    
    TBR=vadimt@chromium.org
    
    Review URL: https://codereview.chromium.org/98333004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@239939 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a209538a03abf337dad4215aac2965419c0d0767

--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -40,7 +40,7 @@ MEDIA_EXPORT extern const char kAlsaOutputDevice[];
 
 #if defined(OS_MACOSX)
 MEDIA_EXPORT extern const char kDisableMainThreadAudio[];
-MEDIA_EXPORT extern const char kEnableAVFoundation[];
+MEDIA_EXPORT extern const char kDisableAVFoundation[];
 #endif
 
 #if defined(OS_WIN)
