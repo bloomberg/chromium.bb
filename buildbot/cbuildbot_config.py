eee83fed743798a033e2e5a08d8ce1041e74df08
Aviv Keshet <akeshet@chromium.org>
2013-12-11 12:02:39 -0800

    cbuildbot_config.py: run HWTest on daisy pfq
    BUG=chromium:323324
    TEST=None
    
    Change-Id: Ic4e86f8bb664ef5d643ed802d2fd31e3059d809d
    Reviewed-on: https://chromium-review.googlesource.com/179632
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Commit-Queue: Aviv Keshet <akeshet@chromium.org>
    Tested-by: Aviv Keshet <akeshet@chromium.org>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -965,6 +965,8 @@ chrome_pfq.add_config('lumpy-chrome-pfq',
 chrome_pfq.add_config('daisy_spring-chrome-pfq',
   arm,
   boards=['daisy_spring'],
+  hw_tests=HWTestConfig.DefaultListPFQ(),
+  upload_hw_test_artifacts=True,
 )
 
 chromium_info = chromium_pfq.derive(
