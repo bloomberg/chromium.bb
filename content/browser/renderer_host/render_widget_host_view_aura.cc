dbaccc8ee38ccbadfee09d671d293df63f59e973
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-11 22:37:36 +0000

    Fix a crash which occurs while trying to reparent plugin windows to a hidden parent window while switching tabs.
    The ShowWindowsCallback function assumes that all aura windows have a valid dispatcher which may not be true in cases where in we have render view which is no longer alive and we are trying to switch to a pending tab which is being closed.
    
    BUG=319007
    R=jam@chromium.org, sky@chromium.org, sky
    
    Review URL: https://codereview.chromium.org/101223008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240199 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2c329860aed6f52530be2121dbd0860b42cbb098

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -144,7 +144,8 @@ BOOL CALLBACK ShowWindowsCallback(HWND window, LPARAM param) {
   RenderWidgetHostViewAura* widget =
       reinterpret_cast<RenderWidgetHostViewAura*>(param);
 
-  if (GetProp(window, kWidgetOwnerProperty) == widget) {
+  if (GetProp(window, kWidgetOwnerProperty) == widget &&
+      widget->GetNativeView()->GetDispatcher()) {
     HWND parent = widget->GetNativeView()->GetDispatcher()->host()->
         GetAcceleratedWidget();
     SetParent(window, parent);
