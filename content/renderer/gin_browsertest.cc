1988b12b9f5363c17fdc973e4df10c372f04e0a8
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 07:56:53 +0000

    [gin] add a test that exercises gin-managed wrappers hanging around in blink
    The purpose of this test is to ensure that there are no unforseen bad
    interactions between the binding systems
    
    BUG=321631
    R=aa@chromium.org, abarth@chromium.org, jam@chromium.org
    
    Review URL: https://codereview.chromium.org/110053003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240263 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a6e36967359ca1d0591928282255b6b47bff25c3

---
+++ content/renderer/gin_browsertest.cc
@@ -0,0 +1,97 @@
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/command_line.h"
+#include "content/public/common/content_switches.h"
+#include "content/public/test/render_view_test.h"
+#include "content/renderer/render_view_impl.h"
+#include "gin/handle.h"
+#include "gin/per_isolate_data.h"
+#include "gin/wrappable.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/WebKit/public/web/WebFrame.h"
+#include "third_party/WebKit/public/web/WebKit.h"
+#include "third_party/WebKit/public/web/WebView.h"
+
+namespace gin {
+
+class TestGinObject : public Wrappable<TestGinObject> {
+ public:
+  static Handle<TestGinObject> Create(v8::Isolate* isolate, bool* alive) {
+    return CreateHandle(isolate, new TestGinObject(alive));
+  }
+
+ private:
+  TestGinObject(bool* alive) : alive_(alive) { *alive_ = true; }
+  virtual ~TestGinObject() { *alive_ = false; }
+
+  bool* alive_;
+
+  DISALLOW_COPY_AND_ASSIGN(TestGinObject);
+};
+
+INIT_WRAPPABLE(TestGinObject);
+
+}  // namespace gin
+
+namespace content {
+
+namespace {
+
+void RegisterTemplates(v8::Isolate* isolate) {
+  gin::PerIsolateData* data = gin::PerIsolateData::From(isolate);
+
+  v8::Handle<v8::ObjectTemplate> templ = v8::ObjectTemplate::New(isolate);
+  templ->SetInternalFieldCount(gin::kNumberOfInternalFields);
+  data->SetObjectTemplate(&gin::TestGinObject::kWrapperInfo, templ);
+}
+
+class GinBrowserTest : public RenderViewTest {
+ public:
+  GinBrowserTest() {}
+  virtual ~GinBrowserTest() {}
+
+  virtual void SetUp() OVERRIDE {
+    CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+        switches::kJavaScriptFlags, "--expose_gc");
+
+    RenderViewTest::SetUp();
+  }
+
+ private:
+
+  DISALLOW_COPY_AND_ASSIGN(GinBrowserTest);
+};
+
+// Test that garbage collection doesn't crash if a gin-wrapped object is
+// present.
+TEST_F(GinBrowserTest, GinAndGarbageCollection) {
+  LoadHTML("<!doctype html>");
+
+  bool alive = false;
+
+  {
+    v8::Isolate* isolate = blink::mainThreadIsolate();
+    v8::HandleScope handle_scope(isolate);
+    v8::Context::Scope context_scope(
+        view_->GetWebView()->mainFrame()->mainWorldScriptContext());
+
+    RegisterTemplates(isolate);
+
+    // We create the object inside a scope so it's not kept alive by a handle
+    // on the stack.
+    gin::TestGinObject::Create(isolate, &alive);
+  }
+
+  CHECK(alive);
+
+  // Should not crash.
+  v8::V8::LowMemoryNotification();
+
+  CHECK(!alive);
+}
+
+} // namespace
+
+}  // namespace content
