d227ff0d1394e4b019af134e530916ec89ab7e0c
jam@chromium.org <jam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-12 19:13:10 +0000

    Blink-side of making all the WebPermissionClient methods take a WebFrame. This is in advance of moving that interface to hang off WebFrame instead of WebView.
    BUG=304341
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/99793006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163803 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: da3fdbc9b5e5a9cba460a044c168f0bbb05e6dcc

--- third_party/WebKit/public/web/WebPermissionClient.h
+++ third_party/WebKit/public/web/WebPermissionClient.h
@@ -31,6 +31,8 @@
 #ifndef WebPermissionClient_h
 #define WebPermissionClient_h
 
+#define WEBPERMISSIONCLIENT_USES_FRAME_FOR_ALL_METHODS
+
 namespace blink {
 
 class WebDocument;
@@ -90,16 +92,23 @@ public:
     // Controls whether access to write the clipboard is allowed for this frame.
     virtual bool allowWriteToClipboard(WebFrame*, bool defaultValue) { return defaultValue; }
 
+#if defined(WEBPERMISSIONCLIENT_USES_FRAME_FOR_ALL_METHODS)
     // Controls whether enabling Web Components API for this frame.
-    virtual bool allowWebComponents(const WebDocument&, bool defaultValue) { return defaultValue; }
+    virtual bool allowWebComponents(WebFrame*, bool defaultValue) { return defaultValue; }
 
-    // Controls whether to enable MutationEvents for this document.
+    // Controls whether to enable MutationEvents for this frame.
     // The common use case of this method is actually to selectively disable MutationEvents,
     // but it's been named for consistency with the rest of the interface.
-    virtual bool allowMutationEvents(const WebDocument&, bool defaultValue) { return defaultValue; }
+    virtual bool allowMutationEvents(WebFrame*, bool defaultValue) { return defaultValue; }
 
     // Controls whether pushState and related History APIs are enabled for this frame.
+    virtual bool allowPushState(WebFrame*) { return true; }
+#else
+    // These methods are deprecated and will be removed after Chrome uses the new versions above.
+    virtual bool allowWebComponents(const WebDocument&, bool defaultValue) { return defaultValue; }
+    virtual bool allowMutationEvents(const WebDocument&, bool defaultValue) { return defaultValue; }
     virtual bool allowPushState(const WebDocument&) { return true; }
+#endif
 
     // Controls whether WebGL extension WEBGL_debug_renderer_info is allowed for this frame.
     virtual bool allowWebGLDebugRendererInfo(WebFrame*) { return false; }
