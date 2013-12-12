1ac67617e269c8a728c5446f1c98db005ffe7dff
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-12 23:04:43 +0000

    Add logging capability to CDM adapter.
    The CDM adapter is a pepper plugin and it should not depend on base/. Therefore we cannot use base/logging.h directly. Instead, add CDM_DLOG, which is a simplified version of it.
    
    Also add CdmAdapter::ConsoleLog() for JS console logs.
    
    BUG=320065
    TEST=Log works on Linux.
    R=dalecurtis@chromium.org, ddorwin@chromium.org
    
    Review URL: https://codereview.chromium.org/59093008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240462 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 84c811f2fba08bea4b51adeb8f7326d5c387364f

--- media/cdm/ppapi/cdm_adapter.cc
+++ media/cdm/ppapi/cdm_adapter.cc
@@ -5,7 +5,9 @@
 #include "media/cdm/ppapi/cdm_adapter.h"
 
 #include "media/cdm/ppapi/cdm_helpers.h"
+#include "media/cdm/ppapi/cdm_logging.h"
 #include "media/cdm/ppapi/supported_cdm_versions.h"
+#include "ppapi/c/ppb_console.h"
 
 #if defined(CHECK_DOCUMENT_URL)
 #include "ppapi/cpp/dev/url_util_dev.h"
@@ -13,6 +15,12 @@
 #endif  // defined(CHECK_DOCUMENT_URL)
 
 namespace {
+
+#if !defined(NDEBUG)
+  #define DLOG_TO_CONSOLE(message) LogToConsole(message);
+#else
+  #define DLOG_TO_CONSOLE(message) (void)(message);
+#endif
 
 bool IsMainThread() {
   return pp::Module::Get()->core()->IsMainThread();
@@ -230,7 +238,14 @@ bool CdmAdapter::CreateCdmInstance(const std::string& key_system) {
   PP_DCHECK(!cdm_);
   cdm_ = make_linked_ptr(CdmWrapper::Create(
       key_system.data(), key_system.size(), GetCdmHost, this));
-  return (cdm_ != NULL);
+  bool success = cdm_ != NULL;
+
+  const std::string message = "CDM instance for " + key_system +
+                              (success ? "" : " could not be") + " created.";
+  DLOG_TO_CONSOLE(message);
+  CDM_DLOG() << message;
+
+  return success;
 }
 
 // No KeyErrors should be reported in this function because they cannot be
@@ -781,6 +796,7 @@ bool CdmAdapter::IsValidVideoFrame(const LinkedVideoFrame& video_frame) {
       !video_frame->FrameBuffer() ||
       (video_frame->Format() != cdm::kI420 &&
        video_frame->Format() != cdm::kYv12)) {
+    CDM_DLOG() << "Invalid video frame!";
     return false;
   }
 
@@ -799,6 +815,15 @@ bool CdmAdapter::IsValidVideoFrame(const LinkedVideoFrame& video_frame) {
 
   return true;
 }
+
+#if !defined(NDEBUG)
+void CdmAdapter::LogToConsole(const pp::Var& value) {
+  PP_DCHECK(IsMainThread());
+  const PPB_Console* console = reinterpret_cast<const PPB_Console*>(
+      pp::Module::Get()->GetBrowserInterface(PPB_CONSOLE_INTERFACE));
+  console->Log(pp_instance(), PP_LOGLEVEL_LOG, value.pp_var());
+}
+#endif  // !defined(NDEBUG)
 
 void CdmAdapter::SendPlatformChallenge(
     const char* service_id, uint32_t service_id_length,
@@ -836,13 +861,15 @@ void CdmAdapter::SendPlatformChallenge(
 
 void CdmAdapter::EnableOutputProtection(uint32_t desired_protection_mask) {
 #if defined(OS_CHROMEOS)
-  output_protection_.EnableProtection(
+  int32_t result = output_protection_.EnableProtection(
       desired_protection_mask, callback_factory_.NewCallback(
           &CdmAdapter::EnableProtectionDone));
 
   // Errors are ignored since clients must call QueryOutputProtectionStatus() to
   // inspect the protection status on a regular basis.
-  // TODO(dalecurtis): It'd be nice to log a message or non-fatal error here...
+
+  if (result != PP_OK && result != PP_OK_COMPLETIONPENDING)
+    CDM_DLOG() << __FUNCTION__ << " failed!";
 #endif
 }
 
@@ -899,6 +926,7 @@ void CdmAdapter::SendPlatformChallengeDone(int32_t result) {
   challenge_in_progress_ = false;
 
   if (result != PP_OK) {
+    CDM_DLOG() << __FUNCTION__ << ": Platform challenge failed!";
     cdm::PlatformChallengeResponse response = {};
     cdm_->OnPlatformChallengeResponse(response);
     return;
@@ -928,7 +956,7 @@ void CdmAdapter::SendPlatformChallengeDone(int32_t result) {
 void CdmAdapter::EnableProtectionDone(int32_t result) {
   // Does nothing since clients must call QueryOutputProtectionStatus() to
   // inspect the protection status on a regular basis.
-  // TODO(dalecurtis): It'd be nice to log a message or non-fatal error here...
+  CDM_DLOG() << __FUNCTION__ << " : " << result;
 }
 
 void CdmAdapter::QueryOutputProtectionStatusDone(int32_t result) {
@@ -969,6 +997,7 @@ void* GetCdmHost(int host_interface_version, void* user_data) {
   PP_DCHECK(IsSupportedCdmHostVersion(host_interface_version));
 
   CdmAdapter* cdm_adapter = static_cast<CdmAdapter*>(user_data);
+  CDM_DLOG() << "Create CDM Host with version " << host_interface_version;
   switch (host_interface_version) {
     case cdm::Host_3::kVersion:
       return static_cast<cdm::Host_3*>(cdm_adapter);
