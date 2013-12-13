5fe927c2422cb366c078b3e0849c44c137039aea
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-13 16:25:53 +0000

    Revert 239894 "Linux Sandbox: check no threads before fork()."
    BUG=327241, 328249
    
    > Linux Sandbox: check no threads before fork().
    > 
    > Always check that no threads are running before fork().
    > 
    > BUG=327241
    > NOTRY=true
    > 
    > Review URL: https://codereview.chromium.org/108173008
    
    TBR=jln@chromium.org
    
    Review URL: https://codereview.chromium.org/106903012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@240670 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a439509cc837f9e1a4dc5eb96deb95345a58cbba

--- sandbox/linux/services/broker_process.cc
+++ sandbox/linux/services/broker_process.cc
@@ -21,7 +21,6 @@
 #include "base/pickle.h"
 #include "base/posix/eintr_wrapper.h"
 #include "base/posix/unix_domain_socket_linux.h"
-#include "base/process/process_metrics.h"
 #include "build/build_config.h"
 #include "sandbox/linux/services/linux_syscalls.h"
 
@@ -147,7 +146,6 @@ bool BrokerProcess::Init(bool (*sandbox_callback)(void)) {
     return false;
   }
 
-  DCHECK_EQ(1, base::GetNumberOfThreads(base::GetCurrentProcessHandle()));
   int child_pid = fork();
   if (child_pid == -1) {
     close(socket_pair[0]);
