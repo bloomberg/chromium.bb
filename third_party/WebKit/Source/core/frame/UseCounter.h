58ceea84d2b10c2de3853ebb7f89aa0056d09c09
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-13 11:00:45 +0000

    Add UseCounters for CSSStyleSheet IE extensions
    Ad-hoc testing reveals that none of these are implemented by Opera 12.16
    (Presto) or Firefox Nightly, so maybe they can be removed.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/114573002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163859 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: be49bb10f2bacc79f1a5d0c3a1285c8ca633dbae

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -278,6 +278,9 @@ public:
         ElementSetAttributeNodeNS, // Removed from DOM4.
         ElementPrefixedMatchesSelector,
         DOMImplementationCreateCSSStyleSheet,
+        CSSStyleSheetRules,
+        CSSStyleSheetAddRule,
+        CSSStyleSheetRemoveRule,
         // Add new features immediately above this line. Don't change assigned
         // numbers of each items, and don't reuse unused slots.
         NumberOfFeatures, // This enum value must be last.
