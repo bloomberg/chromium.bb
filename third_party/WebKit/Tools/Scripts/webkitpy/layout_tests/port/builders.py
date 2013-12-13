dbf57b466baf95be94f3253e6375964d2f7c555f
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-13 01:21:18 +0000

    Update remaining baselines after landing Mavericks baselines.
    Also, make builders.py aware of the new bot, so that garden-o-matic works.
    
    TBR=adamk@chromium.org
    BUG=314947
    
    Review URL: https://codereview.chromium.org/102413006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@163833 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 55e6a154e69a0e85aafab7853ad65ee31431f95a

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/builders.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/builders.py
@@ -51,6 +51,7 @@ _exact_matches = {
     "WebKit Mac10.7 (dbg)": {"port_name": "mac-lion"},
     "WebKit Mac10.8": {"port_name": "mac-mountainlion"},
     "WebKit Mac10.8 (retina)": {"port_name": "mac-retina"},
+    "WebKit Mac10.9": {"port_name": "mac-mavericks"},
     "WebKit Android (Nexus4)": {"port_name": "android"},
 }
 
@@ -64,6 +65,7 @@ _deps_builders = {
     "mac-snowleopard": "WebKit Mac10.6 (deps)",
     "mac-lion": "WebKit Mac10.6 (deps)",
     "mac-mountainlion": "WebKit Mac10.6 (deps)",
+    "mac-mavericks": "WebKit Mac10.6 (deps)",
     "mac-retina": "WebKit Mac10.6 (deps)",
 }
 
