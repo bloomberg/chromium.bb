c9d4f3c9b477c65ede41563c9bcaee89b2dd8548
scottmg@chromium.org <scottmg@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2013-12-14 00:33:21 +0000

    Add license header to ordered_dict
    http://code.activestate.com/recipes/576693/ specifies author and MIT
    license, but it wasn't included in the code. Add it to make chromium's
    check_licenses step happy.
    
    R=thakis@chromium.org
    TBR=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/103813008
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1818 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/ordered_dict.py
+++ pylib/gyp/ordered_dict.py
@@ -1,9 +1,30 @@
 # Unmodified from http://code.activestate.com/recipes/576693/
+# other than to add MIT license header (as specified on page, but not in code).
 # Linked from Python documentation here:
 # http://docs.python.org/2/library/collections.html#collections.OrderedDict
 #
 # This should be deleted once Py2.7 is available on all bots, see
 # http://crbug.com/241769.
+#
+# Copyright (c) 2009 Raymond Hettinger.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 # Backport of OrderedDict() class that runs on Python 2.4, 2.5, 2.6, 2.7 and pypy.
 # Passes Python2.7's test suite and incorporates all the latest updates.
