91f081261a448417d44408b71c4321930d4d9c9e
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-16 23:59:14 +0000

    Disable WebViewTest.Size for flakiness on Windows.
    BUG=329032
    TBR=lazyboy@chromium.org
    
    Review URL: https://codereview.chromium.org/93703007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241072 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 244ea1669e0047c0dccd6857ac4578ac6691b272

--- chrome/browser/apps/web_view_browsertest.cc
+++ chrome/browser/apps/web_view_browsertest.cc
@@ -1021,7 +1021,13 @@ IN_PROC_BROWSER_TEST_F(WebViewTest, ShimSrcAttribute) {
       << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(WebViewTest, Size) {
+// Disabled on Windows for flaky failures: crbug.com/329032
+#if defined(OS_WIN)
+#define MAYBE_Size DISABLED_Size
+#else
+#define MAYBE_Size Size
+#endif
+IN_PROC_BROWSER_TEST_F(WebViewTest, MAYBE_Size) {
   ASSERT_TRUE(RunPlatformAppTest("platform_apps/web_view/size")) << message_;
 }
 
