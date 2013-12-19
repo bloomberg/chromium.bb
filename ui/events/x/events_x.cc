430ba6ebc6c596b06bc2047aa8d25d263609cbe2
miletus@chromium.org <miletus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-19 01:39:39 +0000

    Truncating X event's coordinate instead of rounding
    In CL https://codereview.chromium.org/69653002
    I was thinking using rounding instead of truncating when converting
    X event's coordinate to ui::TouchEvent's location seems more correct.
    But it introduces the bug that an edge event can be rounded to location
    that is out of bounds, e.g. rightmost X event's x position on Pixel is
    2559, and it gets divided by device scale factor of 2 so becomes
    1279.5 and rounded to be 1280, which is out of bounds.
    So lets revert to the truncating behavior.
    
    BUG=328151
    TEST=follow the test at #1 & #5 at crbug.com/328151 and make sure
         the issue is fixed.
    
    Review URL: https://codereview.chromium.org/118423003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241746 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 29539696f99dab315ede049101c14681f62387f5

--- ui/events/x/events_x.cc
+++ ui/events/x/events_x.cc
@@ -429,8 +429,8 @@ gfx::Point EventLocationFromNative(const base::NativeEvent& native_event) {
     case GenericEvent: {
       XIDeviceEvent* xievent =
           static_cast<XIDeviceEvent*>(native_event->xcookie.data);
-      return gfx::Point(round(xievent->event_x),
-                        round(xievent->event_y));
+      return gfx::Point(static_cast<int>(xievent->event_x),
+                        static_cast<int>(xievent->event_y));
     }
   }
   return gfx::Point();
