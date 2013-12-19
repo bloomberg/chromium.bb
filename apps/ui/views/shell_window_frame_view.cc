260c50074d71262eba11ca27212d458d09ace918
calamity@chromium.org <calamity@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-19 04:06:34 +0000

    Fix the 1px transparent corners in the Windows app window title bar.
    This CL fixes the drawing of the shell window frame view to not draw a 1px gap
    into the corners of the title bar.
    
    BUG=317922
    
    Review URL: https://codereview.chromium.org/117863002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@241788 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5782a52a9e0945a220bf135d49210288ee08876c

--- apps/ui/views/shell_window_frame_view.cc
+++ apps/ui/views/shell_window_frame_view.cc
@@ -308,11 +308,8 @@ void ShellWindowFrameView::OnPaint(gfx::Canvas* canvas) {
   paint.setStyle(SkPaint::kFill_Style);
   paint.setColor(SK_ColorWHITE);
   gfx::Path path;
-  const int radius = frame_->IsMaximized() ? 0 : 1;
-  path.moveTo(0, radius);
-  path.lineTo(radius, 0);
-  path.lineTo(width() - radius - 1, 0);
-  path.lineTo(width(), radius + 1);
+  path.moveTo(0, 0);
+  path.lineTo(width(), 0);
   path.lineTo(width(), kCaptionHeight);
   path.lineTo(0, kCaptionHeight);
   path.close();
