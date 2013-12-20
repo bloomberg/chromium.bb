5ee03d679cd637a4834519055791779739aa4fce
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-20 10:25:11 +0000

    SyncFS: Fix when to call OnSyncIdle()
    Without this fix demoted changes are not correctly promoted
    
    BUG=330116, 240165
    TEST=manual with logging (update - delete conflict)
    R=nhiroki@chromium.org, tzik@chromium.org
    
    Review URL: https://codereview.chromium.org/119793002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242059 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 830edbed467b7983edbfb867f6ae58c98b5c4ca6

--- chrome/browser/sync_file_system/sync_process_runner.cc
+++ chrome/browser/sync_file_system/sync_process_runner.cc
@@ -93,14 +93,15 @@ void SyncProcessRunner::ScheduleIfNotRunning() {
 void SyncProcessRunner::OnChangesUpdated(
     int64 pending_changes) {
   DCHECK_GE(pending_changes, 0);
-  if (pending_changes_ != pending_changes) {
+  int64 old_pending_changes = pending_changes_;
+  pending_changes_ = pending_changes;
+  if (old_pending_changes != pending_changes) {
     if (pending_changes == 0)
       sync_service()->OnSyncIdle();
     util::Log(logging::LOG_VERBOSE, FROM_HERE,
               "[%s] pending_changes updated: %" PRId64,
               name_.c_str(), pending_changes);
   }
-  pending_changes_ = pending_changes;
   Schedule();
 }
 
