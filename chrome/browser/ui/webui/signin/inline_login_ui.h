90de0e6235b45ca88d9e8ae4f477ca2c9e8b41f9
guohui@chromium.org <guohui@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-12-20 19:12:57 +0000

    Refactor inline_login_ui
    BUG=
    
    Review URL: https://codereview.chromium.org/118343003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@242134 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 29125cfe3e83e71d7c9bbb920415dee4f470d833

--- chrome/browser/ui/webui/signin/inline_login_ui.h
+++ chrome/browser/ui/webui/signin/inline_login_ui.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_INLINE_LOGIN_UI_H_
-#define CHROME_BROWSER_UI_WEBUI_INLINE_LOGIN_UI_H_
+#ifndef CHROME_BROWSER_UI_WEBUI_SIGNIN_INLINE_LOGIN_UI_H_
+#define CHROME_BROWSER_UI_WEBUI_SIGNIN_INLINE_LOGIN_UI_H_
 
 #include "chrome/browser/extensions/signin/scoped_gaia_auth_extension.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
@@ -23,4 +23,4 @@ class InlineLoginUI : public ui::WebDialogUI {
   DISALLOW_COPY_AND_ASSIGN(InlineLoginUI);
 };
 
-#endif  // CHROME_BROWSER_UI_WEBUI_INLINE_LOGIN_UI_H_
+#endif  // CHROME_BROWSER_UI_WEBUI_SIGNIN_INLINE_LOGIN_UI_H_
