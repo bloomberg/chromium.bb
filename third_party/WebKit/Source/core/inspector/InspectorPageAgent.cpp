ac57535885eb7d0b33f1c3bc4f4a19dd54812838
kaznacheev@chromium.org <kaznacheev@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-12-23 11:55:23 +0000

    DevTools: Disable window size hint while remote debugging a device
    BUG=324589
    
    Review URL: https://codereview.chromium.org/112473003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164287 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: fe24b9a8929c36a7ff72500b37347c5ea68a23d6

--- third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorPageAgent.cpp
@@ -1003,8 +1003,10 @@ void InspectorPageAgent::didScroll()
 
 void InspectorPageAgent::didResizeMainFrame()
 {
+#if !OS(ANDROID)
     if (m_enabled && m_state->getBoolean(PageAgentState::showSizeOnResize))
         m_overlay->showAndHideViewSize(m_state->getBoolean(PageAgentState::showGridOnResize));
+#endif
     m_frontend->frameResized();
 }
 
