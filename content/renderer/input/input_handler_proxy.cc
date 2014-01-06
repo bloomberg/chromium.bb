60f42d0c155a49d1c1bf0c52ec51e1c68c73e45f
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-06 23:07:39 +0000

    Reenable overlay scrollbars
    BUG=326635
    TEST=Existing
    R=jdduke
    
    Review URL: https://codereview.chromium.org/124283003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243194 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4781c0287f5660e4f3cdcc8f4c0b8d24d79622ce

--- content/renderer/input/input_handler_proxy.cc
+++ content/renderer/input/input_handler_proxy.cc
@@ -229,6 +229,14 @@ InputHandlerProxy::EventDisposition InputHandlerProxy::HandleInputEvent(
     return DROP_EVENT;
   } else if (WebInputEvent::isKeyboardEventType(event.type)) {
     CancelCurrentFling();
+  } else if (event.type == WebInputEvent::MouseMove) {
+    const WebMouseEvent& mouse_event =
+        *static_cast<const WebMouseEvent*>(&event);
+    // TODO(tony): Ignore when mouse buttons are down?
+    // TODO(davemoore): This should never happen, but bug #326635 showed some
+    // surprising crashes.
+    CHECK(input_handler_);
+    input_handler_->MouseMoveAt(gfx::Point(mouse_event.x, mouse_event.y));
   }
 
   return DID_NOT_HANDLE;
