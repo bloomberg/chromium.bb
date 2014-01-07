258bff85ad128f0e1ff82891b1e3562281fee6b0
aruslan@chromium.org <aruslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-07 19:57:05 +0000

    Enable ClickListenerTest.
    Accidentally left disabled when the underlying issue was resolved.
    
    BUG=172786,156800
    
    Review URL: https://codereview.chromium.org/118333004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243370 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 061640b32aca7ec9a080dd2a29ce3b01e8ee118d

--- content/public/android/javatests/src/org/chromium/content/browser/ClickListenerTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ClickListenerTest.java
@@ -4,35 +4,31 @@
 
 package org.chromium.content.browser;
 
-import org.chromium.base.test.util.DisabledTest;
+import android.test.suitebuilder.annotation.MediumTest;
+
+import org.chromium.base.test.util.Feature;
 
 /**
  * Test suite for click listener validation in content detection.
  */
 public class ClickListenerTest extends ContentDetectionTestBase {
 
-    /*
     @MediumTest
     @Feature({"ContentDetection", "TabContents"})
-    http://crbug.com/172786
-    */
-    @DisabledTest
     public void testClickContentOnLink() throws Throwable {
         startActivityWithTestUrl("content/content_detection/click_listeners.html");
+        assertWaitForPageScaleFactorMatch(1.0f);
 
         // Clicks on addresses in links should change the url.
         scrollAndTapNavigatingOut("linktest");
         assertTrue(isCurrentTestUrl("content/content_detection/empty.html"));
     }
 
-    /*
     @MediumTest
     @Feature({"ContentDetection", "TabContents"})
-    http://crbug.com/172786
-    */
-    @DisabledTest
     public void testClickContentOnJSListener1() throws Throwable {
         startActivityWithTestUrl("content/content_detection/click_listeners.html");
+        assertWaitForPageScaleFactorMatch(1.0f);
 
         // Clicks on addresses in elements listening to click events should be
         // processed normally without address detection.
@@ -40,14 +36,11 @@ public class ClickListenerTest extends ContentDetectionTestBase {
         assertTrue(isCurrentTestUrl("content/content_detection/empty.html"));
     }
 
-    /*
     @MediumTest
     @Feature({"ContentDetection", "TabContents"})
-    http://crbug.com/172786
-    */
-    @DisabledTest
     public void testClickContentOnJSListener2() throws Throwable {
         startActivityWithTestUrl("content/content_detection/click_listeners.html");
+        assertWaitForPageScaleFactorMatch(1.0f);
 
         // Same as previous test, but using addEventListener instead of onclick.
         scrollAndTapNavigatingOut("clicktest2");
