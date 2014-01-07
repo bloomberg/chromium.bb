7ca872e8ca863e81ca08a3192ccea5e871c5e92f
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-07 11:33:53 +0000

    [rAC,OSX] Allow copy/paste in rAC sign-in dialog.
    Allows application-wide accelerators in the sign-in dialog by
    redispatching unhandled keys to the current event processing window.
    
    This will properly route them to OSX's application menu handler.
    
    BUG=324198
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/125373002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243263 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9cf607b3623cf3255174d64ca4f842a7c93ba148

--- chrome/browser/ui/cocoa/autofill/autofill_sign_in_container.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_sign_in_container.mm
@@ -6,14 +6,61 @@
 
 #import <Cocoa/Cocoa.h>
 
+#include "base/mac/foundation_util.h"
 #include "base/strings/sys_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_sign_in_delegate.h"
+#import "chrome/browser/ui/cocoa/browser_window_utils.h"
+#include "chrome/browser/ui/cocoa/chrome_event_processing_window.h"
 #include "chrome/browser/ui/chrome_style.h"
 #include "chrome/browser/ui/cocoa/autofill/autofill_dialog_cocoa.h"
 #include "components/autofill/content/browser/wallet/wallet_service_url.h"
+#include "content/public/browser/native_web_keyboard_event.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_view.h"
+
+namespace {
+
+// Platform version of the sign-in delegate, allows handling of hotkeys.
+class CocoaSignInDelegate : public autofill::AutofillDialogSignInDelegate {
+ public:
+  CocoaSignInDelegate(autofill::AutofillDialogView* dialog_view,
+                      content::WebContents* dialog_web_contents,
+                      content::WebContents* originating_web_contents,
+                      const gfx::Size& minimum_size,
+                      const gfx::Size& maximum_size,
+                      NSView* view)
+      : AutofillDialogSignInDelegate(dialog_view,
+                                     dialog_web_contents,
+                                     originating_web_contents,
+                                     minimum_size,
+                                     maximum_size),
+        view_(view) {}
+
+  // WebContentsDelegate implementation. Forwards all unhandled keyboard events
+  // to the current window.
+  virtual void HandleKeyboardEvent(
+      content::WebContents* source,
+      const content::NativeWebKeyboardEvent& event) OVERRIDE;
+
+ private:
+  NSView* view_;  // WebContentsView, used to redispatch key events.
+};
+
+void CocoaSignInDelegate::HandleKeyboardEvent(
+    content::WebContents* source,
+    const content::NativeWebKeyboardEvent& event) {
+  if (![BrowserWindowUtils shouldHandleKeyboardEvent:event])
+    return;
+
+  // Not invoking +[BrowserWindowUtils handleKeyboardEvent here], since the
+  // window in question is a ConstrainedWindowCustomWindow, not a BrowserWindow.
+  ChromeEventProcessingWindow* event_window =
+      base::mac::ObjCCastStrict<ChromeEventProcessingWindow>([view_ window]);
+  [event_window redispatchKeyEvent:event.os_event];
+}
+
+}  // namespace
 
 @implementation AutofillSignInContainer
 
@@ -45,12 +92,13 @@
   maxSize_.width = std::max(maxSize_.width, initialMinSize.width);
 
   signInDelegate_.reset(
-      new autofill::AutofillDialogSignInDelegate(
+      new CocoaSignInDelegate(
           dialog_,
           webContents_.get(),
           dialog_->delegate()->GetWebContents(),
           gfx::Size(NSSizeToCGSize(initialMinSize)),
-          gfx::Size(NSSizeToCGSize(maxSize_))));
+          gfx::Size(NSSizeToCGSize(maxSize_)),
+          [self view]));
   webContents_->GetController().LoadURL(
       dialog_->delegate()->SignInUrl(),
       content::Referrer(),
