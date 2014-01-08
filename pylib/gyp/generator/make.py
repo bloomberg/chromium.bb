2ebb8ddcc5d3711ed004bd7d0bdb5305a2b1aeba
jochen@chromium.org <jochen@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2014-01-08 10:10:11 +0000

    generator output should be relative to depth.
    Currently, it's a strange mix between depth, the filename, and generator
    output that happens to work if depth is .
    
    BUG=none
    TEST=test/generator-output/gyptest-depth.py
    R=joi@chromium.org
    
    Review URL: https://codereview.chromium.org/127893002
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1830 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/generator/make.py
+++ pylib/gyp/generator/make.py
@@ -1951,7 +1951,8 @@ def GenerateOutput(target_list, target_dicts, data, params):
     # We write the file in the base_path directory.
     output_file = os.path.join(options.depth, base_path, base_name)
     if options.generator_output:
-      output_file = os.path.join(options.generator_output, output_file)
+      output_file = os.path.join(
+          options.depth, options.generator_output, base_path, base_name)
     base_path = gyp.common.RelativePath(os.path.dirname(build_file),
                                         options.toplevel_dir)
     return base_path, output_file
@@ -1974,7 +1975,8 @@ def GenerateOutput(target_list, target_dicts, data, params):
   makefile_path = os.path.join(options.toplevel_dir, makefile_name)
   if options.generator_output:
     global srcdir_prefix
-    makefile_path = os.path.join(options.generator_output, makefile_path)
+    makefile_path = os.path.join(
+        options.toplevel_dir, options.generator_output, makefile_name)
     srcdir = gyp.common.RelativePath(srcdir, options.generator_output)
     srcdir_prefix = '$(srcdir)/'
 
