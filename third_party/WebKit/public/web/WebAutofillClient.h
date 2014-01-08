1b3a5d350112954bd6bb49f520407749dddf4369
isherman@chromium.org <isherman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-08 10:09:43 +0000

    Remove several obsolete methods from the WebAutofillClient interface.
    (This is a second attempt to land this, now that the Chromium repository has
     been properly readied.)
    
    BUG=330077
    TEST=compiles
    R=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/127593002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164674 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ef88071d9efeca1da127e2814d29157a845db6b7

--- third_party/WebKit/public/web/WebAutofillClient.h
+++ third_party/WebKit/public/web/WebAutofillClient.h
@@ -38,7 +38,6 @@ class WebFrame;
 class WebInputElement;
 class WebKeyboardEvent;
 class WebNode;
-class WebString;
 
 template <typename T> class WebVector;
 
@@ -54,38 +53,8 @@ public:
         MenuItemIDDataListEntry = -6
     };
 
-    // Informs the browser that the user has accepted an Autofill suggestion for
-    // a WebNode. A positive |itemID| is a unique id used to identify the set
-    // of Autofill profiles. If it is AutocompleteEntryMenuItemID, then the
-    // suggestion is an Autocomplete suggestion; and |value| stores the
-    // suggested text. |index| is an index of the selected suggestion in the
-    // list of suggestions provided by the client.
-    virtual void didAcceptAutofillSuggestion(const WebNode&,
-                                             const WebString& value,
-                                             const WebString& label,
-                                             int itemID,
-                                             unsigned index) { }
-
-    // Informs the browser that the user has selected an Autofill suggestion for
-    // a WebNode.  This happens when the user hovers over a suggestion or uses
-    // the arrow keys to navigate to a suggestion.
-    virtual void didSelectAutofillSuggestion(const WebNode&,
-                                             const WebString& name,
-                                             const WebString& label,
-                                             int itemID) { }
-
-    // Informs the browser that the user has cleared the selection from the
-    // Autofill suggestions popup. This happens when a user uses the arrow
-    // keys to navigate outside the range of possible selections.
-    virtual void didClearAutofillSelection(const WebNode&) { }
-
     // Informs the browser an interactive autocomplete has been requested.
     virtual void didRequestAutocomplete(WebFrame*, const WebFormElement&) { }
-
-    // Instructs the browser to remove the Autocomplete entry specified from
-    // its DB.
-    virtual void removeAutocompleteSuggestion(const WebString& name,
-                                              const WebString& value) { }
 
     // These methods are called when the users edits a text-field.
     virtual void textFieldDidEndEditing(const WebInputElement&) { }
