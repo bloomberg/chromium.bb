34025f777fcba6d9833f04b5ceee32bcb106c9b5
yuki.sekiguchi@access-company.com <yuki.sekiguchi@access-company.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-09 07:40:09 +0000

    table-caption stride over pages.
    In normal block layout, pagination struts are propagated from child
    blocks to parent blocks in order to indicate that an object needs to
    push to the next page. Table captions were setting a pagination strut
    for a push, but this strut was not being used.
    
    Made sure RenderTable applies the caption's pagination strut when
    placing the caption.
    
    Added strut of a RenderCaption to RenderCaption's position.
    
    BUG=329421
    
    Review URL: https://codereview.chromium.org/80343002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@164742 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c63c843d2d0f9f4e2199f78752a866e996eba7d4

--- third_party/WebKit/Source/core/rendering/RenderTable.cpp
+++ third_party/WebKit/Source/core/rendering/RenderTable.cpp
@@ -371,7 +371,12 @@ void RenderTable::layoutCaption(RenderTableCaption* caption)
         caption->layoutIfNeeded();
     }
     // Apply the margins to the location now that they are definitely available from layout
-    caption->setLogicalLocation(LayoutPoint(caption->marginStart(), caption->marginBefore() + logicalHeight()));
+    LayoutUnit captionLogicalTop = caption->marginBefore() + logicalHeight();
+    if (view()->layoutState()->isPaginated()) {
+        captionLogicalTop += caption->paginationStrut();
+        caption->setPaginationStrut(0);
+    }
+    caption->setLogicalLocation(LayoutPoint(caption->marginStart(), captionLogicalTop));
 
     if (!selfNeedsLayout() && caption->checkForRepaintDuringLayout())
         caption->repaintDuringLayoutIfMoved(captionRect);
