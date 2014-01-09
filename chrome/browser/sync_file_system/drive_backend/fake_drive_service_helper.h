ba9adce5041e17961184fe672937df195b0a1b5d
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 04:03:50 +0000

    [SyncFS] Move FakeDriveServiceHelper from V1 directory to V2.
    BUG=331991
    
    Review URL: https://codereview.chromium.org/108343009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243752 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 067224e500014261130ac125a4f2273afedfd379

--- chrome/browser/sync_file_system/drive_backend/fake_drive_service_helper.h
+++ chrome/browser/sync_file_system/drive_backend/fake_drive_service_helper.h
@@ -1,9 +1,9 @@
-// Copyright 2013 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_SYNC_FILE_SYSTEM_DRIVE_BACKEND_V1_FAKE_DRIVE_SERVICE_HELPER_H_
-#define CHROME_BROWSER_SYNC_FILE_SYSTEM_DRIVE_BACKEND_V1_FAKE_DRIVE_SERVICE_HELPER_H_
+#ifndef CHROME_BROWSER_SYNC_FILE_SYSTEM_DRIVE_BACKEND_FAKE_DRIVE_SERVICE_HELPER_H_
+#define CHROME_BROWSER_SYNC_FILE_SYSTEM_DRIVE_BACKEND_FAKE_DRIVE_SERVICE_HELPER_H_
 
 #include <string>
 
@@ -87,4 +87,4 @@ class FakeDriveServiceHelper {
 }  // namespace drive_backend
 }  // namespace sync_file_system
 
-#endif  // CHROME_BROWSER_SYNC_FILE_SYSTEM_DRIVE_BACKEND_V1_FAKE_DRIVE_SERVICE_HELPER_H_
+#endif  // CHROME_BROWSER_SYNC_FILE_SYSTEM_DRIVE_BACKEND_FAKE_DRIVE_SERVICE_HELPER_H_
