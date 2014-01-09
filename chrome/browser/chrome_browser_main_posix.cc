6fec2931d9a61563da3d7f9555d4b3490b6e843b
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-09 00:01:47 +0000

    Increase kShutdownDetectorThreadStackSize to avoid tcmalloc stack overflow.
    BUG=332297
    
    Review URL: https://codereview.chromium.org/127743002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@243702 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 84511467d19f63c582315e7733cc08bb9e1416ec

--- chrome/browser/chrome_browser_main_posix.cc
+++ chrome/browser/chrome_browser_main_posix.cc
@@ -314,7 +314,7 @@ void ChromeBrowserMainPartsPosix::PostMainMessageLoopStart() {
     g_shutdown_pipe_read_fd = pipefd[0];
     g_shutdown_pipe_write_fd = pipefd[1];
 #if !defined(ADDRESS_SANITIZER) && !defined(KEEP_SHADOW_STACKS)
-    const size_t kShutdownDetectorThreadStackSize = PTHREAD_STACK_MIN;
+    const size_t kShutdownDetectorThreadStackSize = PTHREAD_STACK_MIN * 2;
 #else
     // ASan instrumentation and -finstrument-functions (used for keeping the
     // shadow stacks) bloat the stack frames, so we need to increase the stack
