d6adbcc8cac1a701a2e446a4d780b889633a9cc6
dschuff@chromium.org <dschuff@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2014-01-16 01:12:52 +0000

    Factor toolchain_build_pnacl.py for canadian cross
    This allows building both 64 and 32 bit host binaries for Linux, as well
    as cross mingw from Linux. It knows the difference between cygwin and
    mingw but Windows build won't actually work yet without a few more
    changes.
    
    R=jvoung@chromium.org
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3719
    
    Review URL: https://codereview.chromium.org/139603003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12619 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: a96e95fcd4c2c8c102cbb86aa85a8d4bcebd8af0

--- toolchain_build/pnacl_commands.py
+++ toolchain_build/pnacl_commands.py
@@ -18,9 +18,6 @@ import platform_tools
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 NACL_DIR = os.path.dirname(SCRIPT_DIR)
 
-# Path to the hermetic cygwin
-CYGWIN_PATH = os.path.join(NACL_DIR, 'cygwin')
-
 # User-facing tools
 DRIVER_TOOLS = ['pnacl-' + tool + '.py' for tool in
                     ('abicheck', 'ar', 'as', 'clang', 'clang++', 'dis',
@@ -32,26 +29,27 @@ DRIVER_UTILS = [name + '.py' for name in
                      'elftools', 'filetype', 'ldtools', 'loader', 'pathtools',
                      'shelltools')]
 
-def InstallDriverScripts(subst, srcdir, dstdir):
+def InstallDriverScripts(subst, srcdir, dstdir, host_windows=False,
+                         host_64bit=False):
   srcdir = subst.SubstituteAbsPaths(srcdir)
   dstdir = subst.SubstituteAbsPaths(dstdir)
   file_tools.MakeDirectoryIfAbsent(os.path.join(dstdir, 'pydir'))
   for name in DRIVER_TOOLS + DRIVER_UTILS:
     shutil.copy(os.path.join(srcdir, name), os.path.join(dstdir, 'pydir'))
   # Install redirector sh/bat scripts
-  redirector = 'redirect.bat' if platform_tools.IsWindows() else 'redirect.sh'
   for name in DRIVER_TOOLS:
     # Chop the .py off the name
-    nopy_name = os.path.join(dstdir, name[:-3])
-    shutil.copy(os.path.join(srcdir, redirector), nopy_name)
+    nopy_name = os.path.join(dstdir, os.path.splitext(name)[0])
+    shutil.copy(os.path.join(srcdir, 'redirect.sh'), nopy_name)
     os.chmod(nopy_name,
+             stat.S_IRUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP)
+    if host_windows:
+      # Windows gets both sh and bat extensions so it works w/cygwin and without
+      batch_script = nopy_name + '.bat'
+      shutil.copy(os.path.join(srcdir, 'redirect.bat'), batch_script)
+      os.chmod(batch_script,
              stat.S_IRUSR | stat.S_IXUSR | stat.S_IRGRP | stat.S_IXGRP)
   # Install the driver.conf file
   with open(os.path.join(dstdir, 'driver.conf'), 'w') as f:
     print >> f, 'HAS_FRONTEND=1'
-    print >> f, 'HOST_ARCH=x86_32'
-  # On windows, copy the necessary cygwin DLLs
-  if platform_tools.IsWindows():
-    for lib in ('gcc_s-1', 'iconv-2', 'win1', 'intl-8', 'stdc++-6', 'z'):
-      shutil.copy(os.path.join(CYGWIN_PATH, 'bin', 'cyg' + lib + '.dll'),
-                  dstdir)
+    print >> f, 'HOST_ARCH=x86_64' if host_64bit else 'HOST_ARCH=x86_32'
--- toolchain_build/toolchain_build_pnacl.py
+++ toolchain_build/toolchain_build_pnacl.py
@@ -5,14 +5,20 @@
 
 """Recipes for PNaCl toolchain packages.
 
-The real entry plumbing is in toolchain_main.py.
+   Recipes consist of specially-structured dictionaries, with keys for package
+   name, type, commands to execute, etc. The structure is documented in the
+   PackageBuilder docstring in toolchain_main.py.
+
+   The real entry plumbing and CLI flags are also in toolchain_main.py.
 """
 
 # Done first to set up python module path
 import toolchain_env
 
+import fnmatch
 import logging
 import os
+import re
 import shutil
 import stat
 import sys
@@ -50,22 +56,125 @@ GIT_REPOS = {
 GIT_BASE_URL = 'https://chromium.googlesource.com/native_client/'
 GIT_DEPS_FILE = os.path.join(NACL_DIR, 'pnacl', 'COMPONENT_REVISIONS')
 
-CONFIGURE_CMD = ['sh', '%(src)s/configure']
-MAKE_PARALLEL_CMD = [ 'make', '-j%(cores)s']
-MAKE_BINUTILS_EXTRA = []
-CONFIGURE_FLAGS = []
-if platform_tools.IsLinux():
-  # TODO(dschuff): handle the 64+32-bit package on Linux, or get rid of it.
-  cc = 'gcc -m32'
-  cxx = 'g++ -m32'
-  CONFIGURE_FLAGS += ['--build=i686-linux', 'CC=' + cc, 'CXX=' + cxx]
-elif platform_tools.IsWindows():
-  command.Runnable.use_cygwin = True
-  CONFIGURE_FLAGS += ['--disable-nls']
-  MAKE_PARALLEL_CMD += ['HAVE_LIBICONV=no']
-  # Windows has issues with the intentionally-racy 'chew' target
-  MAKE_BINUTILS_EXTRA = ['-j1']
+# TODO(dschuff): Some of this cygwin/mingw logic duplicates stuff in command.py
+# and the mechanism for switching between cygwin and mingw is bad.
+# Path to the hermetic cygwin
+BUILD_CROSS_MINGW = False
+CYGWIN_PATH = os.path.join(NACL_DIR, 'cygwin')
+# Path to the mingw cross-compiler libs on Ubuntu
+CROSS_MINGW_LIBPATH = '/usr/lib/gcc/i686-w64-mingw32/4.6'
+# Path to the native mingw compiler is up here to make it easy to change for
+# local testing.
+MINGW_PATH = os.environ.get('MINGW', r'd:\src\mingw-exp\mingw32')
+
+ALL_ARCHES = ('x86-32', 'x86-64', 'arm', 'mips32')
+
 MAKE_DESTDIR_CMD = ['make', 'DESTDIR=%(abs_output)s']
+
+def NativeTriple():
+  if platform_tools.IsWindows():
+    if command.Runnable.use_cygwin:
+      return 'i686-pc-cygwin'
+    else:
+      return 'i686-w64-mingw32'
+  elif platform_tools.IsMacOS():
+    return 'x86_64-apple-darwin'
+  elif platform_tools.IsLinux():
+    if platform_tools.Is64BitLinux():
+      return 'x86_64-linux'
+    else:
+      return 'i686-linux'
+  else:
+    raise Exception('Unknown build architecture %s' % sys.platform)
+
+
+def TripleIsWindows(t):
+  return fnmatch.fnmatch(t, '*-mingw32*') or fnmatch.fnmatch(t, '*cygwin*')
+
+
+def ConfigureHostArchFlags(host):
+  configure_args = []
+  extra_cc_args = []
+
+  native = NativeTriple()
+  is_cross = host != native
+  if is_cross:
+    if platform_tools.Is64BitLinux() and fnmatch.fnmatch(host, '*-linux*'):
+      # 64 bit linux can build 32 bit linux binaries while still being a native
+      # build for our purposes. But it's not what config.guess will yield, so
+      # use --build to force it and make sure things build correctly.
+      configure_args.append('--build=' + host)
+      extra_cc_args = ['-m32']
+    else:
+      configure_args.append('--host=' + host)
+
+  extra_cxx_args = list(extra_cc_args)
+
+  compiler = {
+      # For now we only do native builds for linux and mac
+      'i686-linux': ('gcc', 'g++'), # treat 32-bit linux like a native build
+      'x86_64-linux': ('gcc', 'g++'),
+      # TODO(dschuff): switch to clang on mac
+      'x86_64-apple-darwin': ('gcc', 'g++'),
+      # Windows build should work for native and cross
+      'i686-w64-mingw32': ('i686-w64-mingw32-gcc', 'i686-w64-mingw32-g++'),
+      # TODO: add arm-hosted support
+  }
+  cc = compiler[host][0]
+  cxx = compiler[host][1]
+
+  if is_cross:
+    # LLVM's linux->mingw cross build isn't right without this?
+    configure_args.append('CC_FOR_BUILD=gcc')
+  configure_args.append('CC=' + ' '.join([cc] + extra_cc_args))
+  configure_args.append('CXX=' + ' '.join([cxx] + extra_cxx_args))
+
+  if TripleIsWindows(host):
+    # The i18n support brings in runtime dependencies on MinGW DLLs
+    # that we don't want to have to distribute alongside our binaries.
+    # So just disable it, and compiler messages will always be in US English.
+    configure_args.append('--disable-nls')
+  return configure_args
+
+def MakeCommand(host):
+  make_command = ['make']
+  if not platform_tools.IsWindows() or command.Runnable.use_cygwin:
+    # The make that ships with msys sometimes hangs when run with -j.
+    # The ming32-make that comes with the compiler itself reportedly doesn't
+    # have this problem, but it has issues with pathnames with LLVM's build.
+    make_command.append('-j%(cores)s')
+
+  if TripleIsWindows(host):
+    # There appears to be nothing we can pass at top-level configure time
+    # that will prevent the configure scripts from finding MinGW's libiconv
+    # and using it.  We have to force this variable into the environment
+    # of the sub-configure runs, which are run via make.
+    make_command.append('HAVE_LIBICONV=no')
+  return make_command
+
+
+def CopyWindowsHostLibs(host):
+  if not TripleIsWindows(host):
+    return []
+  if command.Runnable.use_cygwin:
+    libs = ('cyggcc_s-1.dll', 'cygiconv-2.dll', 'cygwin1.dll', 'cygintl-8.dll',
+            'cygstdc++-6.dll', 'cygz.dll')
+    return [command.Copy(
+                    os.path.join(CYGWIN_PATH, 'bin', lib),
+                    os.path.join('%(output)s', 'bin', lib))
+                for lib in libs]
+  if platform_tools.IsWindows():
+    lib_path = os.path.join(MINGW_PATH, 'bin')
+    # The native minGW compiler uses winpthread, but the Ubuntu cross compiler
+    # does not.
+    libs = ('libgcc_s_sjlj-1.dll', 'libstdc++-6.dll', 'libwinpthread-1.dll')
+  else:
+    lib_path = os.path.join(CROSS_MINGW_LIBPATH)
+    libs = ('libgcc_s_sjlj-1.dll', 'libstdc++-6.dll')
+  return [command.Copy(
+                  os.path.join(lib_path, lib),
+                  os.path.join('%(output)s', 'bin', lib))
+               for lib in libs]
 
 def GetGitSyncCmdCallback(revisions):
   """Return a callback which returns the git sync command for a component.
@@ -109,16 +218,27 @@ def HostToolsSources(GetGitSyncCmd):
   }
   return sources
 
-def HostTools():
+
+# Return the component name we'll use for a base component name and
+# a host triple.  The component names cannot contain dashes or other
+# non-identifier characters, because the names of the files uploaded
+# to Google Storage are constrained.  GNU configuration tuples contain
+# dashes, which we translate to underscores.
+def Mangle(component_name, extra):
+  return component_name + '_' + re.sub(r'[^A-Za-z0-9_/.]', '_', extra)
+
+def HostTools(host):
+  def H(component_name):
+    return Mangle(component_name, host)
   tools = {
-      'binutils_pnacl': {
+      H('binutils_pnacl'): {
           'dependencies': ['binutils_pnacl_src'],
           'type': 'build',
           'commands': [
               command.SkipForIncrementalCommand([
                   'sh',
                   '%(binutils_pnacl_src)s/configure'] +
-                  CONFIGURE_FLAGS +
+                  ConfigureHostArchFlags(host) +
                   ['--prefix=',
                   '--disable-silent-rules',
                   '--target=arm-pc-nacl',
@@ -133,19 +253,19 @@ def HostTools():
                   '--without-gas',
                   '--without-zlib',
                   '--with-sysroot=/arm-pc-nacl']),
-              command.Command(MAKE_PARALLEL_CMD + MAKE_BINUTILS_EXTRA),
+              command.Command(MakeCommand(host)),
               command.Command(MAKE_DESTDIR_CMD + ['install-strip'])] +
               [command.RemoveDirectory(command.path.join('%(abs_output)s', dir))
                for dir in ('arm-pc-nacl', 'lib', 'lib32')]
           },
-      'llvm': {
+      H('llvm'): {
           'dependencies': ['clang_src', 'llvm_src', 'binutils_pnacl_src'],
           'type': 'build',
           'commands': [
               command.SkipForIncrementalCommand([
                   'sh',
                   '%(llvm_src)s/configure'] +
-                  CONFIGURE_FLAGS +
+                  ConfigureHostArchFlags(host) +
                   ['--prefix=/',
                    '--enable-shared',
                    '--disable-zlib',
@@ -155,19 +275,21 @@ def HostTools():
                    '--program-prefix=',
                    '--enable-optimized',
                    '--with-clang-srcdir=%(abs_clang_src)s']),
-              command.Command(MAKE_PARALLEL_CMD + [
+              command.Command(MakeCommand(host) + [
                   'VERBOSE=1',
                   'NACL_SANDBOX=0',
                   'all']),
-              command.Command(MAKE_DESTDIR_CMD + ['install']),
-          ],
+              command.Command(MAKE_DESTDIR_CMD + ['install'])] +
+              CopyWindowsHostLibs(host),
       },
-      'driver': {
+      H('driver'): {
         'type': 'build',
         'inputs': { 'src': os.path.join(NACL_DIR, 'pnacl', 'driver')},
         'commands': [
             command.Runnable(pnacl_commands.InstallDriverScripts,
-                             '%(src)s', '%(output)s')
+                             '%(src)s', '%(output)s',
+                             host_windows=TripleIsWindows(host),
+                             host_64bit=fnmatch.fnmatch(host, 'x86_64*'))
         ],
       },
   }
@@ -227,11 +349,21 @@ if __name__ == '__main__':
   if args.legacy_repo_sync:
     SyncPNaClRepos(revisions)
     sys.exit(0)
+
   packages = {}
   packages.update(HostToolsSources(GetGitSyncCmdCallback(revisions)))
   packages.update(pnacl_targetlibs.TargetLibsSrc(
       GetGitSyncCmdCallback(revisions)))
-  packages.update(HostTools())
+
+
+  if platform_tools.Is64BitLinux():
+    hosts = ['i686-linux', NativeTriple()]
+  else:
+    hosts = [NativeTriple()]
+  if platform_tools.IsLinux() and BUILD_CROSS_MINGW:
+    hosts.append('i686-w64-mingw32')
+  for host in hosts:
+    packages.update(HostTools(host))
 
   tb = toolchain_main.PackageBuilder(packages,
                                      leftover_args)
