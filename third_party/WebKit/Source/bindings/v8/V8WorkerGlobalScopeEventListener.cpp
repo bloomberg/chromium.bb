1b7c11634df6998680154f267a2b2a6a7877d9be
amikhaylova@google.com <amikhaylova@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-16 15:58:52 +0000

    Show information about original functions instead of bound ones on DevTools timeline.
    This fixes issue with timeout on tests OpenSpecialTypes/FileManagerBrowserTest.*
    https://codereview.chromium.org/131293002/
    
    BUG=263762
    
    Review URL: https://codereview.chromium.org/137893012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165231 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c2f01e3ec9d58963eaa6728b4fc02e9bfdb8313c

--- third_party/WebKit/Source/bindings/v8/V8WorkerGlobalScopeEventListener.cpp
+++ third_party/WebKit/Source/bindings/v8/V8WorkerGlobalScopeEventListener.cpp
@@ -89,11 +89,12 @@ v8::Local<v8::Value> V8WorkerGlobalScopeEventListener::callListenerFunction(Exec
     if (InspectorInstrumentation::timelineAgentEnabled(context)) {
         String resourceName("undefined");
         int lineNumber = 1;
-        v8::ScriptOrigin origin = handlerFunction->GetScriptOrigin();
+        v8::Handle<v8::Function> originalFunction = getBoundFunction(handlerFunction);
+        v8::ScriptOrigin origin = originalFunction->GetScriptOrigin();
         if (!origin.ResourceName().IsEmpty()) {
             V8TRYCATCH_FOR_V8STRINGRESOURCE_RETURN(V8StringResource<>, stringResourceName, origin.ResourceName(), v8::Local<v8::Value>());
             resourceName = stringResourceName;
-            lineNumber = handlerFunction->GetScriptLineNumber() + 1;
+            lineNumber = originalFunction->GetScriptLineNumber() + 1;
         }
         cookie = InspectorInstrumentation::willCallFunction(context, resourceName, lineNumber);
     }
