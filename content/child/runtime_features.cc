a280fcd26d75a4b02974cd640d63563b719bd720
mariakhomenko@chromium.org <mariakhomenko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 20:47:26 +0000

    Revert of Enable OrientationEvent only for android. (https://codereview.chromium.org/131913003/)
    Reason for revert:
    Causes crashes on Android on loading a web page, more details in https://code.google.com/p/chromium/issues/detail?id=335258 
    
    Original issue's description:
    > Enable OrientationEvent only for android.
    > 
    > BUG=181136, 331337
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=245210
    
    TBR=darin@chromium.org,mahesh.kk@samsung.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=181136, 331337
    
    Review URL: https://codereview.chromium.org/130053005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245310 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 69d30f4fcb33e5bffef4b19bd4ff90859d782650

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -43,7 +43,6 @@ static void SetRuntimeFeatureDefaultsForPlatform() {
   // Android does not yet support NavigatorContentUtils.
   WebRuntimeFeatures::enableNavigatorContentUtils(false);
   WebRuntimeFeatures::enableTouchIconLoading(true);
-  WebRuntimeFeatures::enableOrientationEvent(true);
 #else
   WebRuntimeFeatures::enableNavigatorContentUtils(true);
 #endif  // defined(OS_ANDROID)
