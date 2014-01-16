4fbb641a2905d46aeeaae7694356c367765848c2
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-16 20:42:47 +0000

    events: Add a PRESUBMIT.py script to make sure the tries run enough tests.
    Changes in events can affect a number of different modules, so make sure
    the unit-tests for at least ash, aura, events, keyboard, and views are
    tried on the appropriate bots (linux_aura, win, win_rel, linux_chromeos,
    linux_chromeos_asan).
    
    BUG=318879
    R=stip@chromium.org
    
    Review URL: https://codereview.chromium.org/136853006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245309 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0ab7606b07d5a74c778e4c88bcc47c04e954b4e7

---
+++ ui/events/PRESUBMIT.py
@@ -0,0 +1,22 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Chromium presubmit script for src/ui/events
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details on the presubmit API built into gcl.
+"""
+
+def GetPreferredTrySlaves():
+  tests = set(['ash_unittests',
+               'aura_unittests',
+               'events_unittests',
+               'keyboard_unittests',
+               'views_unittests'])
+
+  return [('linux_aura', tests),
+          ('linux_chromeos', tests),
+          ('linux_chromeos_asan', tests),
+          ('win', tests),
+          ('win_rel', tests)]
