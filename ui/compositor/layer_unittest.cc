ea8234e33674bb25bbc871578f39e4933b7825d5
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-17 20:33:14 +0000

    cc: Remove ReadPixels from compositor layer_unittests.cc
    BUG=332167
    
    Review URL: https://codereview.chromium.org/133273026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245601 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: aa0da25253f3f41998d78e5a834fea038f86f2e9

--- ui/compositor/layer_unittest.cc
+++ ui/compositor/layer_unittest.cc
@@ -15,6 +15,8 @@
 #include "cc/layers/delegated_frame_provider.h"
 #include "cc/layers/delegated_frame_resource_collection.h"
 #include "cc/layers/layer.h"
+#include "cc/output/copy_output_request.h"
+#include "cc/output/copy_output_result.h"
 #include "cc/output/delegated_frame_data.h"
 #include "cc/test/pixel_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -126,8 +128,29 @@ class LayerWithRealCompositorTest : public testing::Test {
   }
 
   bool ReadPixels(SkBitmap* bitmap) {
-    return GetCompositor()->ReadPixels(bitmap,
-                                       gfx::Rect(GetCompositor()->size()));
+    return ReadPixels(bitmap, gfx::Rect(GetCompositor()->size()));
+  }
+
+  bool ReadPixels(SkBitmap* bitmap, gfx::Rect source_rect) {
+    scoped_refptr<ReadbackHolder> holder(new ReadbackHolder);
+    scoped_ptr<cc::CopyOutputRequest> request =
+        cc::CopyOutputRequest::CreateBitmapRequest(
+            base::Bind(&ReadbackHolder::OutputRequestCallback, holder));
+    request->set_area(source_rect);
+
+    GetCompositor()->root_layer()->RequestCopyOfOutput(request.Pass());
+
+    // Wait for copy response.
+    WaitForDraw();
+
+    if (holder->completed()) {
+      *bitmap = holder->result();
+      return true;
+    }
+
+    // Callback never called.
+    NOTREACHED();
+    return false;
   }
 
   void WaitForDraw() {
@@ -149,6 +172,29 @@ class LayerWithRealCompositorTest : public testing::Test {
   }
 
  private:
+  class ReadbackHolder : public base::RefCountedThreadSafe<ReadbackHolder> {
+   public:
+    ReadbackHolder() : completed_(false) {}
+
+    void OutputRequestCallback(scoped_ptr<cc::CopyOutputResult> result) {
+      DCHECK(!completed_);
+      result_ = result->TakeBitmap();
+      completed_ = true;
+    }
+    bool completed() const {
+      return completed_;
+    };
+    const SkBitmap& result() const { return *result_; }
+
+   private:
+    friend class base::RefCountedThreadSafe<ReadbackHolder>;
+
+    virtual ~ReadbackHolder() {}
+
+    scoped_ptr<SkBitmap> result_;
+    bool completed_;
+  };
+
   scoped_ptr<TestCompositorHost> window_;
 
   // The root directory for test files.
@@ -783,7 +829,7 @@ TEST_F(LayerWithRealCompositorTest, DrawPixels) {
   DrawTree(layer.get());
 
   SkBitmap bitmap;
-  ASSERT_TRUE(GetCompositor()->ReadPixels(&bitmap, gfx::Rect(viewport_size)));
+  ASSERT_TRUE(ReadPixels(&bitmap, gfx::Rect(viewport_size)));
   ASSERT_FALSE(bitmap.empty());
 
   SkAutoLockPixels lock(bitmap);
