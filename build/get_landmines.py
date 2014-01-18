fbb7f3589a2ea68b498726295cffc81f1e6c269a
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-18 02:03:57 +0000

    Add landmine for 2013 switch
    R=iannucci@chromium.org
    BUG=309197, 323300
    
    Review URL: https://codereview.chromium.org/141583006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245713 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7f9e6bb90376b811429e7763c6077f50ec6564d4

--- build/get_landmines.py
+++ build/get_landmines.py
@@ -45,6 +45,9 @@ def print_landmines(target):
       gyp_defines().get('target_arch') == 'x64' and
       gyp_defines().get('dcheck_always_on') == '1'):
     print "Switched win x64 trybots from VS2010 to VS2012."
+  if (platform() == 'win' and builder() == 'ninja' and
+      gyp_msvs_version() == '2013'):
+    print "Switched win from VS2010 to VS2013."
   print 'Need to clobber everything due to an IDL change in r154579 (blink)'
 
 
