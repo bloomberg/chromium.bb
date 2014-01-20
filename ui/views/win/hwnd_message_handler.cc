38ce68e9c8106578f5256b8f445de3145294ac58
mlamouri@chromium.org <mlamouri@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-20 00:41:29 +0000

    Windows: improve menu item initialization.
    This patch prevents the 'Restore' option to be shown when the window can
    not be resized and the 'Minimize' option is no longer disabled if the
    window can not be maximized.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/137793003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@245849 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 72bba9df68f0680e6c2afaf7a41e1ee38e3a1543

--- ui/views/win/hwnd_message_handler.cc
+++ ui/views/win/hwnd_message_handler.cc
@@ -1518,17 +1518,17 @@ void HWNDMessageHandler::OnInitMenu(HMENU menu) {
   bool is_restored = !is_fullscreen && !is_minimized && !is_maximized;
 
   ScopedRedrawLock lock(this);
-  EnableMenuItemByCommand(menu, SC_RESTORE, is_minimized || is_maximized);
+  EnableMenuItemByCommand(menu, SC_RESTORE, delegate_->CanResize() &&
+                          (is_minimized || is_maximized));
   EnableMenuItemByCommand(menu, SC_MOVE, is_restored);
   EnableMenuItemByCommand(menu, SC_SIZE, delegate_->CanResize() && is_restored);
   EnableMenuItemByCommand(menu, SC_MAXIMIZE, delegate_->CanMaximize() &&
                           !is_fullscreen && !is_maximized);
-  EnableMenuItemByCommand(menu, SC_MINIMIZE, delegate_->CanMaximize() &&
-                          !is_minimized);
+  EnableMenuItemByCommand(menu, SC_MINIMIZE, !is_minimized);
 
-  if (is_maximized)
+  if (is_maximized && delegate_->CanResize())
     ::SetMenuDefaultItem(menu, SC_RESTORE, FALSE);
-  else if (delegate_->CanMaximize())
+  else if (!is_maximized && delegate_->CanMaximize())
     ::SetMenuDefaultItem(menu, SC_MAXIMIZE, FALSE);
 }
 
