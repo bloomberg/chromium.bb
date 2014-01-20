bloomberg/chromium.bb

commit f9efc2e7ab27c9c7ffdac6ffff0a98040458fa81

Remove default parameter from virtual layoutBlock function
Remove default parameter from virtual layoutBlock function since it is only
called explicitly once on an instance of one particular overload of this method.

BUG=None

Review URL: https://codereview.chromium.org/135043007

git-svn-id: svn://svn.chromium.org/blink/trunk@165254 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 2cbcb76e7374ff6cfa1dab4923a54668fbd318b3

third_party/WebKit/Source/core/rendering/RenderBlockFlow.cpp
@@ -225,7 +225,7 @@ bool RenderBlockFlow::relayoutForPagination(bool hasSpecifiedPageLogicalHeight, 
         if (columnHeight && columnHeight != pageLogicalHeight) {
             statePusher.pop();
             setEverHadLayout(true);
-            layoutBlock(false, columnHeight);
+            layoutBlockFlow(false, columnHeight);
             return true;
         }
     }
@@ -249,7 +249,12 @@ bool RenderBlockFlow::isSelfCollapsingBlock() const
     return m_hasOnlySelfCollapsingChildren;
 }
 
-void RenderBlockFlow::layoutBlock(bool relayoutChildren, LayoutUnit pageLogicalHeight)
+void RenderBlockFlow::layoutBlock(bool relayoutChildren)
+{
+    layoutBlockFlow(relayoutChildren);
+}
+
+void RenderBlockFlow::layoutBlockFlow(bool relayoutChildren, LayoutUnit pageLogicalHeight)
 {
     ASSERT(needsLayout());
     ASSERT(isInlineBlockOrInlineTable() || !isInline());

third_party/WebKit/Source/core/rendering/RenderBlockFlow.h
@@ -57,7 +57,8 @@ public:
 
     virtual bool isRenderBlockFlow() const OVERRIDE FINAL { return true; }
 
-    virtual void layoutBlock(bool relayoutChildren, LayoutUnit pageLogicalHeight = 0) OVERRIDE;
+    virtual void layoutBlock(bool relayoutChildren) OVERRIDE;
+    void layoutBlockFlow(bool relayoutChildren, LayoutUnit pageLogicalHeight = 0);
 
     virtual void computeOverflow(LayoutUnit oldClientAfterEdge, bool recomputeFloats = false) OVERRIDE;
     virtual void deleteLineBoxTree() OVERRIDE FINAL;

third_party/WebKit/Source/core/rendering/RenderBlock.cpp
@@ -1530,7 +1530,7 @@ void RenderBlock::checkForPaginationLogicalHeightChange(LayoutUnit& pageLogicalH
     }
 }
 
-void RenderBlock::layoutBlock(bool, LayoutUnit)
+void RenderBlock::layoutBlock(bool)
 {
     ASSERT_NOT_REACHED();
     clearNeedsLayout();

third_party/WebKit/Source/core/rendering/RenderBlock.h
@@ -107,7 +107,7 @@ public:
     virtual void addChild(RenderObject* newChild, RenderObject* beforeChild = 0) OVERRIDE;
     virtual void removeChild(RenderObject*) OVERRIDE;
 
-    virtual void layoutBlock(bool relayoutChildren, LayoutUnit pageLogicalHeight = 0);
+    virtual void layoutBlock(bool relayoutChildren);
 
     void insertPositionedObject(RenderBox*);
     static void removePositionedObject(RenderBox*);

third_party/WebKit/Source/core/rendering/RenderDeprecatedFlexibleBox.cpp
@@ -250,7 +250,7 @@ void RenderDeprecatedFlexibleBox::computePreferredLogicalWidths()
     clearPreferredLogicalWidthsDirty();
 }
 
-void RenderDeprecatedFlexibleBox::layoutBlock(bool relayoutChildren, LayoutUnit)
+void RenderDeprecatedFlexibleBox::layoutBlock(bool relayoutChildren)
 {
     ASSERT(needsLayout());
 

third_party/WebKit/Source/core/rendering/RenderDeprecatedFlexibleBox.h
@@ -40,7 +40,7 @@ public:
 
     virtual void styleWillChange(StyleDifference, const RenderStyle* newStyle) OVERRIDE;
 
-    virtual void layoutBlock(bool relayoutChildren, LayoutUnit pageHeight = 0) OVERRIDE;
+    virtual void layoutBlock(bool relayoutChildren) OVERRIDE;
     void layoutHorizontalBox(bool relayoutChildren);
     void layoutVerticalBox(bool relayoutChildren);
 

third_party/WebKit/Source/core/rendering/RenderFlexibleBox.cpp
@@ -226,7 +226,7 @@ void RenderFlexibleBox::styleDidChange(StyleDifference diff, const RenderStyle* 
     }
 }
 
-void RenderFlexibleBox::layoutBlock(bool relayoutChildren, LayoutUnit)
+void RenderFlexibleBox::layoutBlock(bool relayoutChildren)
 {
     ASSERT(needsLayout());
 

third_party/WebKit/Source/core/rendering/RenderFlexibleBox.h
@@ -48,7 +48,7 @@ public:
     virtual bool isFlexibleBox() const OVERRIDE FINAL { return true; }
     virtual bool avoidsFloats() const OVERRIDE FINAL { return true; }
     virtual bool canCollapseAnonymousBlockChild() const OVERRIDE FINAL { return false; }
-    virtual void layoutBlock(bool relayoutChildren, LayoutUnit pageLogicalHeight = 0) OVERRIDE FINAL;
+    virtual void layoutBlock(bool relayoutChildren) OVERRIDE FINAL;
 
     virtual int baselinePosition(FontBaseline, bool firstLine, LineDirectionMode, LinePositionMode = PositionOnContainingLine) const OVERRIDE;
     virtual int firstLineBoxBaseline() const OVERRIDE;

commit 0b32296fc7ff52540e21843ef87c3f3ace5156b2

Mark private method layoutBlockFlow as inline
Mark private method layoutBlockFlow as inline since it is only used in
the implementation file and this appears to be performance critical code.  This
is an attempt to fix a regression introduced by r165254.  This also reduces the
binary size on clang by 40 bytes.

BUG=33574
R=esprehn@chromium.org, ch.dumez@samsung.com

Review URL: https://codereview.chromium.org/140323004

git-svn-id: svn://svn.chromium.org/blink/trunk@165419 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: c507d59ae5bff007dd14f3a585e5ad956a38ed2c

third_party/WebKit/Source/core/rendering/RenderBlockFlow.cpp
@@ -251,7 +251,7 @@ void RenderBlockFlow::layoutBlock(bool relayoutChildren)
     layoutBlockFlow(relayoutChildren);
 }
 
-void RenderBlockFlow::layoutBlockFlow(bool relayoutChildren, LayoutUnit pageLogicalHeight)
+inline void RenderBlockFlow::layoutBlockFlow(bool relayoutChildren, LayoutUnit pageLogicalHeight)
 {
     ASSERT(needsLayout());
     ASSERT(isInlineBlockOrInlineTable() || !isInline());

third_party/WebKit/Source/core/rendering/RenderBlockFlow.h
@@ -59,7 +59,6 @@ public:
     virtual bool isRenderBlockFlow() const OVERRIDE FINAL { return true; }
 
     virtual void layoutBlock(bool relayoutChildren) OVERRIDE;
-    void layoutBlockFlow(bool relayoutChildren, LayoutUnit pageLogicalHeight = 0);
 
     virtual void computeOverflow(LayoutUnit oldClientAfterEdge, bool recomputeFloats = false) OVERRIDE;
     virtual void deleteLineBoxTree() OVERRIDE FINAL;
@@ -257,6 +256,7 @@ protected:
     virtual void insertedIntoTree() OVERRIDE;
     virtual void willBeDestroyed() OVERRIDE;
 private:
+    void layoutBlockFlow(bool relayoutChildren, LayoutUnit pageLogicalHeight = 0);
     void layoutBlockChildren(bool relayoutChildren, LayoutUnit& maxFloatLogicalBottom, SubtreeLayoutScope&, LayoutUnit beforeEdge, LayoutUnit afterEdge);
 
     void layoutBlockChild(RenderBox* child, MarginInfo&, LayoutUnit& previousFloatLogicalBottom, LayoutUnit& maxFloatLogicalBottom);