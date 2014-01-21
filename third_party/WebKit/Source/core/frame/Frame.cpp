bloomberg/chromium.bb

commit fadd10894a90b60ff1804e08ee2e4704916879f5

Refactoring ImageBuffer to decouple it from Canvas2DLayerBridge
This change adds a new abstraction layer called ImageBufferSurface which is
used to provide a rendering surface to ImageBuffers.  The motivation is to
move the construction of the canvas layer bridge into HTMLCanvasElement, which
will make it possible, in a future change, to attach it to a per-WebView
Canvas2DLayerManager. Having one Manager per WebView (instead of per renderer
process) will make it possible to have canvas resource management policies
that depend on whether the owning WebView is active.

Other effects of this change:
* Changed all call sites that used Unaccelerated image buffers to stop
  using platform surfaces, and used malloc'ed buffers instead.
* Non-drawable image buffers no longer create an SkCanvas and a GraphicsContext
* It is now possible to create an accelerated ImageBuffer without a
  Canvas2DLayerBridge (useful for accelerated image filters).
* Buffer creation fallback sequence is no longer baked-in to the ImageBuffer
  constructor, making it possible for different call sites to have different
  fallback logic.
* ImageBuffer no longer provides support for device scale factors

BUG=244427
R=senorblanco,jamesr

Review URL: https://codereview.chromium.org/104023007

git-svn-id: svn://svn.chromium.org/blink/trunk@163572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 85d765073da760b855dfbd6d07715b2a0a652d0d

third_party/WebKit/Source/core/frame/Frame.cpp
@@ -649,9 +649,10 @@ PassOwnPtr<DragImage> Frame::nodeImage(Node* node)
     paintingRect.setWidth(paintingRect.width() * deviceScaleFactor);
     paintingRect.setHeight(paintingRect.height() * deviceScaleFactor);
 
-    OwnPtr<ImageBuffer> buffer(ImageBuffer::create(paintingRect.size(), deviceScaleFactor));
+    OwnPtr<ImageBuffer> buffer = ImageBuffer::create(paintingRect.size());
     if (!buffer)
         return nullptr;
+    buffer->context()->scale(FloatSize(deviceScaleFactor, deviceScaleFactor));
     buffer->context()->translate(-paintingRect.x(), -paintingRect.y());
     buffer->context()->clip(FloatRect(0, 0, paintingRect.maxX(), paintingRect.maxY()));
 
@@ -678,9 +679,10 @@ PassOwnPtr<DragImage> Frame::dragImageForSelection()
     paintingRect.setWidth(paintingRect.width() * deviceScaleFactor);
     paintingRect.setHeight(paintingRect.height() * deviceScaleFactor);
 
-    OwnPtr<ImageBuffer> buffer(ImageBuffer::create(paintingRect.size(), deviceScaleFactor));
+    OwnPtr<ImageBuffer> buffer = ImageBuffer::create(paintingRect.size());
     if (!buffer)
         return nullptr;
+    buffer->context()->scale(FloatSize(deviceScaleFactor, deviceScaleFactor));
     buffer->context()->translate(-paintingRect.x(), -paintingRect.y());
     buffer->context()->clip(FloatRect(0, 0, paintingRect.maxX(), paintingRect.maxY()));
 

third_party/WebKit/Source/core/platform/DragImage.cpp
@@ -163,9 +163,10 @@ PassOwnPtr<DragImage> DragImage::create(const KURL& url, const String& inLabel, 
     // fill the background
     IntSize scaledImageSize = imageSize;
     scaledImageSize.scale(deviceScaleFactor);
-    OwnPtr<ImageBuffer> buffer(ImageBuffer::create(scaledImageSize, deviceScaleFactor));
+    OwnPtr<ImageBuffer> buffer(ImageBuffer::create(scaledImageSize));
     if (!buffer)
         return nullptr;
+    buffer->context()->scale(FloatSize(deviceScaleFactor, deviceScaleFactor));
 
     const float DragLabelRadius = 5;
     const IntSize radii(DragLabelRadius, DragLabelRadius);

third_party/WebKit/Source/core/frame/ImageBitmap.cpp
@@ -63,7 +63,9 @@ ImageBitmap::ImageBitmap(HTMLVideoElement* video, const IntRect& cropRect)
     IntRect srcRect = intersection(cropRect, videoRect);
     IntRect dstRect(IntPoint(), srcRect.size());
 
-    OwnPtr<ImageBuffer> buf = ImageBuffer::create(videoRect.size(), 1, UnacceleratedNonPlatformBuffer);
+    OwnPtr<ImageBuffer> buf = ImageBuffer::create(videoRect.size());
+    if (!buf)
+        return;
     GraphicsContext* c = buf->context();
     c->clip(dstRect);
     c->translate(-srcRect.x(), -srcRect.y());
@@ -97,7 +99,9 @@ ImageBitmap::ImageBitmap(ImageData* data, const IntRect& cropRect)
 {
     IntRect srcRect = intersection(cropRect, IntRect(IntPoint(), data->size()));
 
-    OwnPtr<ImageBuffer> buf = ImageBuffer::create(data->size(), 1, UnacceleratedNonPlatformBuffer);
+    OwnPtr<ImageBuffer> buf = ImageBuffer::create(data->size());
+    if (!buf)
+        return;
     if (srcRect.width() > 0 && srcRect.height() > 0)
         buf->putByteArray(Premultiplied, data->data(), data->size(), srcRect, IntPoint(min(0, -cropRect.x()), min(0, -cropRect.y())));
 

third_party/WebKit/Source/core/html/HTMLCanvasElement.cpp
@@ -45,8 +45,11 @@
 #include "core/page/Settings.h"
 #include "core/rendering/RenderHTMLCanvas.h"
 #include "platform/MIMETypeRegistry.h"
+#include "platform/graphics/Canvas2DImageBufferSurface.h"
 #include "platform/graphics/GraphicsContextStateSaver.h"
 #include "platform/graphics/ImageBuffer.h"
+#include "platform/graphics/UnacceleratedImageBufferSurface.h"
+#include "platform/graphics/gpu/WebGLImageBufferSurface.h"
 #include "public/platform/Platform.h"
 
 namespace WebCore {
@@ -72,7 +75,6 @@ HTMLCanvasElement::HTMLCanvasElement(Document& document)
     , m_ignoreReset(false)
     , m_accelerationDisabled(false)
     , m_externallyAllocatedMemory(0)
-    , m_deviceScaleFactor(1)
     , m_originClean(true)
     , m_didFailToCreateImageBuffer(false)
     , m_didClearImageBuffer(false)
@@ -252,17 +254,14 @@ void HTMLCanvasElement::reset()
 
     IntSize oldSize = size();
     IntSize newSize(w, h);
-    float newDeviceScaleFactor = 1;
 
     // If the size of an existing buffer matches, we can just clear it instead of reallocating.
     // This optimization is only done for 2D canvases for now.
-    if (hadImageBuffer && oldSize == newSize && m_deviceScaleFactor == newDeviceScaleFactor && m_context && m_context->is2d()) {
+    if (hadImageBuffer && oldSize == newSize && m_context && m_context->is2d()) {
         if (!m_didClearImageBuffer)
             clearImageBuffer();
         return;
     }
-
-    m_deviceScaleFactor = newDeviceScaleFactor;
 
     setSurfaceSize(newSize);
 
@@ -399,12 +398,6 @@ PassRefPtr<ImageData> HTMLCanvasElement::getImageData()
     return toWebGLRenderingContext(m_context.get())->paintRenderingResultsToImageData();
 }
 
-IntSize HTMLCanvasElement::convertLogicalToDevice(const IntSize& logicalSize) const
-{
-    FloatSize deviceSize = logicalSize * m_deviceScaleFactor;
-    return expandedIntSize(deviceSize);
-}
-
 SecurityOrigin* HTMLCanvasElement::securityOrigin() const
 {
     return document().securityOrigin();
@@ -432,6 +425,25 @@ bool HTMLCanvasElement::shouldAccelerate(const IntSize& size) const
     return true;
 }
 
+PassOwnPtr<ImageBufferSurface> HTMLCanvasElement::createImageBufferSurface(const IntSize& deviceSize, int* msaaSampleCount)
+{
+    OpacityMode opacityMode = !m_context || m_context->hasAlpha() ? NonOpaque : Opaque;
+
+    *msaaSampleCount = 0;
+    if (is3D())
+        return adoptPtr(new WebGLImageBufferSurface(size(), opacityMode));
+
+    if (shouldAccelerate(deviceSize)) {
+        if (document().settings())
+            *msaaSampleCount = document().settings()->accelerated2dCanvasMSAASampleCount();
+        OwnPtr<ImageBufferSurface> surface = adoptPtr(new Canvas2DImageBufferSurface(size(), opacityMode, *msaaSampleCount));
+        if (surface->isValid())
+            return surface.release();
+    }
+
+    return adoptPtr(new UnacceleratedImageBufferSurface(size(), opacityMode));
+}
+
 void HTMLCanvasElement::createImageBuffer()
 {
     ASSERT(!m_imageBuffer);
@@ -439,7 +451,7 @@ void HTMLCanvasElement::createImageBuffer()
     m_didFailToCreateImageBuffer = true;
     m_didClearImageBuffer = true;
 
-    IntSize deviceSize = convertLogicalToDevice(size());
+    IntSize deviceSize = size();
     if (deviceSize.width() * deviceSize.height() > MaxCanvasArea)
         return;
 
@@ -449,16 +461,22 @@ void HTMLCanvasElement::createImageBuffer()
     if (!deviceSize.width() || !deviceSize.height())
         return;
 
-    RenderingMode renderingMode = is3D() ? TextureBacked : (shouldAccelerate(deviceSize) ? Accelerated : UnacceleratedNonPlatformBuffer);
-    int msaaSampleCount = 0;
-    if (document().settings())
-        msaaSampleCount = document().settings()->accelerated2dCanvasMSAASampleCount();
-    OpacityMode opacityMode = !m_context || m_context->hasAlpha() ? NonOpaque : Opaque;
-    m_imageBuffer = ImageBuffer::create(size(), m_deviceScaleFactor, renderingMode, opacityMode, msaaSampleCount);
-    if (!m_imageBuffer)
+    int msaaSampleCount;
+    OwnPtr<ImageBufferSurface> surface = createImageBufferSurface(deviceSize, &msaaSampleCount);
+    if (!surface->isValid())
         return;
+    m_imageBuffer = ImageBuffer::create(surface.release());
+
     m_didFailToCreateImageBuffer = false;
+
     setExternallyAllocatedMemory(4 * width() * height());
+
+    if (is3D()) {
+        // Early out for WebGL canvases
+        m_contextStateSaver.clear();
+        return;
+    }
+
     m_imageBuffer->context()->setShouldClampToSourceRect(false);
     m_imageBuffer->context()->setImageInterpolationQuality(DefaultInterpolationQuality);
     // Enabling MSAA overrides a request to disable antialiasing. This is true regardless of whether the
@@ -473,7 +491,7 @@ void HTMLCanvasElement::createImageBuffer()
     m_contextStateSaver = adoptPtr(new GraphicsContextStateSaver(*m_imageBuffer->context()));
 
     // Recalculate compositing requirements if acceleration state changed.
-    if (m_context && m_context->is2d())
+    if (m_context)
         scheduleLayerUpdate();
 }
 
@@ -539,12 +557,7 @@ void HTMLCanvasElement::clearCopiedImage()
 AffineTransform HTMLCanvasElement::baseTransform() const
 {
     ASSERT(hasImageBuffer() && !m_didFailToCreateImageBuffer);
-    IntSize unscaledSize = size();
-    IntSize size = convertLogicalToDevice(unscaledSize);
-    AffineTransform transform;
-    if (size.width() && size.height())
-        transform.scaleNonUniform(size.width() / unscaledSize.width(), size.height() / unscaledSize.height());
-    return m_imageBuffer->baseTransform() * transform;
+    return m_imageBuffer->baseTransform();
 }
 
 }

third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -45,6 +45,7 @@ class HTMLCanvasElement;
 class Image;
 class ImageData;
 class ImageBuffer;
+class ImageBufferSurface;
 class IntSize;
 
 class CanvasObserver {
@@ -77,7 +78,7 @@ public:
 
     void setSize(const IntSize& newSize)
     {
-        if (newSize == size() && m_deviceScaleFactor == 1)
+        if (newSize == size())
             return;
         m_ignoreReset = true;
         setWidth(newSize.width());
@@ -122,8 +123,6 @@ public:
 
     bool shouldAccelerate(const IntSize&) const;
 
-    float deviceScaleFactor() const { return m_deviceScaleFactor; }
-
     InsertionNotificationRequest insertedInto(ContainerNode*) OVERRIDE;
 
 private:
@@ -135,11 +134,11 @@ private:
 
     void reset();
 
+    PassOwnPtr<ImageBufferSurface> createImageBufferSurface(const IntSize& deviceSize, int* msaaSampleCount);
     void createImageBuffer();
     void clearImageBuffer();
 
     void setSurfaceSize(const IntSize&);
-    IntSize convertLogicalToDevice(const IntSize&) const;
 
     bool paintsIntoCanvasBuffer() const;
 
@@ -159,7 +158,6 @@ private:
 
     intptr_t m_externallyAllocatedMemory;
 
-    float m_deviceScaleFactor; // FIXME: This is always 1 and should probable be deleted
     bool m_originClean;
 
     // It prevents HTMLCanvasElement::buffer() from continuously re-attempting to allocate an imageBuffer

third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
@@ -1872,17 +1872,12 @@ PassRefPtr<ImageData> CanvasRenderingContext2D::createImageData(float sw, float 
     return createEmptyImageData(size);
 }
 
-PassRefPtr<ImageData> CanvasRenderingContext2D::getImageData(float sx, float sy, float sw, float sh, ExceptionState& exceptionState) const
-{
-    return getImageData(ImageBuffer::LogicalCoordinateSystem, sx, sy, sw, sh, exceptionState);
-}
-
 PassRefPtr<ImageData> CanvasRenderingContext2D::webkitGetImageDataHD(float sx, float sy, float sw, float sh, ExceptionState& exceptionState) const
 {
-    return getImageData(ImageBuffer::BackingStoreCoordinateSystem, sx, sy, sw, sh, exceptionState);
+    return getImageData(sx, sy, sw, sh, exceptionState);
 }
 
-PassRefPtr<ImageData> CanvasRenderingContext2D::getImageData(ImageBuffer::CoordinateSystem coordinateSystem, float sx, float sy, float sw, float sh, ExceptionState& exceptionState) const
+PassRefPtr<ImageData> CanvasRenderingContext2D::getImageData(float sx, float sy, float sw, float sh, ExceptionState& exceptionState) const
 {
     if (!canvas()->originClean()) {
         exceptionState.throwSecurityError("The canvas has been tainted by cross-origin data.");
@@ -1920,7 +1915,7 @@ PassRefPtr<ImageData> CanvasRenderingContext2D::getImageData(ImageBuffer::Coordi
     if (!buffer)
         return createEmptyImageData(imageDataRect.size());
 
-    RefPtr<Uint8ClampedArray> byteArray = buffer->getUnmultipliedImageData(imageDataRect, coordinateSystem);
+    RefPtr<Uint8ClampedArray> byteArray = buffer->getUnmultipliedImageData(imageDataRect);
     if (!byteArray)
         return 0;
 
@@ -1936,27 +1931,7 @@ void CanvasRenderingContext2D::putImageData(ImageData* data, float dx, float dy,
     putImageData(data, dx, dy, 0, 0, data->width(), data->height(), exceptionState);
 }
 
-void CanvasRenderingContext2D::webkitPutImageDataHD(ImageData* data, float dx, float dy, ExceptionState& exceptionState)
-{
-    if (!data) {
-        exceptionState.throwUninformativeAndGenericDOMException(TypeMismatchError);
-        return;
-    }
-    webkitPutImageDataHD(data, dx, dy, 0, 0, data->width(), data->height(), exceptionState);
-}
-
 void CanvasRenderingContext2D::putImageData(ImageData* data, float dx, float dy, float dirtyX, float dirtyY,
-    float dirtyWidth, float dirtyHeight, ExceptionState& exceptionState)
-{
-    putImageData(data, ImageBuffer::LogicalCoordinateSystem, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight, exceptionState);
-}
-
-void CanvasRenderingContext2D::webkitPutImageDataHD(ImageData* data, float dx, float dy, float dirtyX, float dirtyY, float dirtyWidth, float dirtyHeight, ExceptionState& exceptionState)
-{
-    putImageData(data, ImageBuffer::BackingStoreCoordinateSystem, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight, exceptionState);
-}
-
-void CanvasRenderingContext2D::putImageData(ImageData* data, ImageBuffer::CoordinateSystem coordinateSystem, float dx, float dy, float dirtyX, float dirtyY,
     float dirtyWidth, float dirtyHeight, ExceptionState& exceptionState)
 {
     if (!data) {
@@ -1987,19 +1962,14 @@ void CanvasRenderingContext2D::putImageData(ImageData* data, ImageBuffer::Coordi
     IntSize destOffset(static_cast<int>(dx), static_cast<int>(dy));
     IntRect destRect = enclosingIntRect(clipRect);
     destRect.move(destOffset);
-    destRect.intersect(IntRect(IntPoint(), coordinateSystem == ImageBuffer::LogicalCoordinateSystem ? buffer->logicalSize() : buffer->internalSize()));
+    destRect.intersect(IntRect(IntPoint(), buffer->size()));
     if (destRect.isEmpty())
         return;
     IntRect sourceRect(destRect);
     sourceRect.move(-destOffset);
 
-    buffer->putByteArray(Unmultiplied, data->data(), IntSize(data->width(), data->height()), sourceRect, IntPoint(destOffset), coordinateSystem);
+    buffer->putByteArray(Unmultiplied, data->data(), IntSize(data->width(), data->height()), sourceRect, IntPoint(destOffset));
 
-    if (coordinateSystem == ImageBuffer::BackingStoreCoordinateSystem) {
-        FloatRect dirtyRect = destRect;
-        dirtyRect.scale(1 / canvas()->deviceScaleFactor());
-        destRect = enclosingIntRect(dirtyRect);
-    }
     didDraw(destRect);
 }
 

third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.h
@@ -202,13 +202,14 @@ public:
     PassRefPtr<ImageData> createImageData(PassRefPtr<ImageData>, ExceptionState&) const;
     PassRefPtr<ImageData> createImageData(float width, float height, ExceptionState&) const;
     PassRefPtr<ImageData> getImageData(float sx, float sy, float sw, float sh, ExceptionState&) const;
-    PassRefPtr<ImageData> webkitGetImageDataHD(float sx, float sy, float sw, float sh, ExceptionState&) const;
     void putImageData(ImageData*, float dx, float dy, ExceptionState&);
     void putImageData(ImageData*, float dx, float dy, float dirtyX, float dirtyY, float dirtyWidth, float dirtyHeight, ExceptionState&);
-    void webkitPutImageDataHD(ImageData*, float dx, float dy, ExceptionState&);
-    void webkitPutImageDataHD(ImageData*, float dx, float dy, float dirtyX, float dirtyY, float dirtyWidth, float dirtyHeight, ExceptionState&);
 
-    float webkitBackingStorePixelRatio() const { return canvas()->deviceScaleFactor(); }
+    // Slated for deprecation:
+    void webkitPutImageDataHD(ImageData* image, float dx, float dy, ExceptionState& e) { putImageData(image, dx, dy, e); }
+    void webkitPutImageDataHD(ImageData* image, float dx, float dy, float dirtyX, float dirtyY, float dirtyWidth, float dirtyHeight, ExceptionState& e) { putImageData(image, dx, dy, dirtyX, dirtyY, dirtyWidth, dirtyHeight, e); }
+    PassRefPtr<ImageData> webkitGetImageDataHD(float sx, float sy, float sw, float sh, ExceptionState&) const;
+    float webkitBackingStorePixelRatio() const { return 1; }
 
     void reset();
 
@@ -316,9 +317,6 @@ private:
 
     template<class T> void fullCanvasCompositedFill(const T&);
     template<class T> void fullCanvasCompositedDrawImage(T*, const FloatRect&, const FloatRect&, CompositeOperator);
-
-    PassRefPtr<ImageData> getImageData(ImageBuffer::CoordinateSystem, float sx, float sy, float sw, float sh, ExceptionState&) const;
-    void putImageData(ImageData*, ImageBuffer::CoordinateSystem, float dx, float dy, float dirtyX, float dirtyY, float dirtyWidth, float dirtyHeight, ExceptionState&);
 
     bool focusRingCallIsValid(const Path&, Element*);
     void updateFocusRingAccessibility(const Path&, Element*);

third_party/WebKit/Source/core/html/canvas/WebGLRenderingContext.cpp
@@ -74,6 +74,7 @@
 #include "platform/NotImplemented.h"
 #include "platform/geometry/IntSize.h"
 #include "platform/graphics/Extensions3D.h"
+#include "platform/graphics/UnacceleratedImageBufferSurface.h"
 #include "platform/graphics/gpu/DrawingBuffer.h"
 
 #include "wtf/PassOwnPtr.h"
@@ -3362,10 +3363,9 @@ bool WebGLRenderingContext::validateTexFunc(const char* functionName, TexFuncVal
     return true;
 }
 
-PassRefPtr<Image> WebGLRenderingContext::drawImageIntoBuffer(Image* image, int width, int height, int deviceScaleFactor)
+PassRefPtr<Image> WebGLRenderingContext::drawImageIntoBuffer(Image* image, int width, int height)
 {
     IntSize size(width, height);
-    size.scale(deviceScaleFactor);
     ImageBuffer* buf = m_generatedImageCache.imageBuffer(size);
     if (!buf) {
         synthesizeGLError(GraphicsContext3D::OUT_OF_MEMORY, "texImage2D", "out of memory");
@@ -3437,7 +3437,7 @@ void WebGLRenderingContext::texImage2D(GC3Denum target, GC3Dint level, GC3Denum 
 
     RefPtr<Image> imageForRender = image->cachedImage()->imageForRenderer(image->renderer());
     if (imageForRender->isSVGImage())
-        imageForRender = drawImageIntoBuffer(imageForRender.get(), image->width(), image->height(), canvas()->deviceScaleFactor());
+        imageForRender = drawImageIntoBuffer(imageForRender.get(), image->width(), image->height());
 
     if (!imageForRender || !validateTexFunc("texImage2D", NotTexSubImage2D, SourceHTMLImageElement, target, level, internalformat, imageForRender->width(), imageForRender->height(), 0, format, type, 0, 0))
         return;
@@ -3676,7 +3676,7 @@ void WebGLRenderingContext::texSubImage2D(GC3Denum target, GC3Dint level, GC3Din
 
     RefPtr<Image> imageForRender = image->cachedImage()->imageForRenderer(image->renderer());
     if (imageForRender->isSVGImage())
-        imageForRender = drawImageIntoBuffer(imageForRender.get(), image->width(), image->height(), canvas()->deviceScaleFactor());
+        imageForRender = drawImageIntoBuffer(imageForRender.get(), image->width(), image->height());
 
     if (!imageForRender || !validateTexFunc("texSubImage2D", TexSubImage2D, SourceHTMLImageElement, target, level, format, imageForRender->width(), imageForRender->height(), 0, format, type, xoffset, yoffset))
         return;
@@ -5452,13 +5452,13 @@ ImageBuffer* WebGLRenderingContext::LRUImageBufferCache::imageBuffer(const IntSi
         ImageBuffer* buf = m_buffers[i].get();
         if (!buf)
             break;
-        if (buf->logicalSize() != size)
+        if (buf->size() != size)
             continue;
         bubbleToFront(i);
         return buf;
     }
 
-    OwnPtr<ImageBuffer> temp = ImageBuffer::create(size, 1);
+    OwnPtr<ImageBuffer> temp(ImageBuffer::create(size));
     if (!temp)
         return 0;
     i = std::min(m_capacity - 1, i);

commit 0cd23b8eb03903355d41505e9b0bfb47ae5ecac0

Fix scale applied to drag images when rendering with HiDPI
This patch fixes a regression that was introduced by r163572, which refactored
ImageBuffer. As a part of the refactor, resolutionScale was removed from
ImageBuffer, causing NativeImageSkia (constructed by ImageBuffer::copyImage)
to no longer correctly transport the resolution scale information. To avoid
further confusion, this change removes resolutionScale from NativeImageSkia.
The only call site that needed to extract resolution scale from NativeImageSkia
was in DragImage, and it turns out that it is easy to access the page's
resolution scale at all the call sites where DragImages are created with
a device scale baked in, so there was no need to transport resolution scale
information in NativeImageSkia in the first place.

BUG=330976
TEST=blink manual test chromium/drag-image-accounts-for-device-scale.html

Review URL: https://codereview.chromium.org/143633002

git-svn-id: svn://svn.chromium.org/blink/trunk@165463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 288922a06e2bc771b32023b05441ad5b289b6323

third_party/WebKit/Source/core/frame/Frame.cpp
@@ -702,7 +702,7 @@ PassOwnPtr<DragImage> Frame::nodeImage(Node* node)
     m_view->paintContents(buffer->context(), paintingRect);
 
     RefPtr<Image> image = buffer->copyImage();
-    return DragImage::create(image.get(), renderer->shouldRespectImageOrientation());
+    return DragImage::create(image.get(), renderer->shouldRespectImageOrientation(), deviceScaleFactor);
 }
 
 PassOwnPtr<DragImage> Frame::dragImageForSelection()
@@ -731,7 +731,7 @@ PassOwnPtr<DragImage> Frame::dragImageForSelection()
     m_view->paintContents(buffer->context(), paintingRect);
 
     RefPtr<Image> image = buffer->copyImage();
-    return DragImage::create(image.get());
+    return DragImage::create(image.get(), DoNotRespectImageOrientation, deviceScaleFactor);
 }
 
 double Frame::devicePixelRatio() const

third_party/WebKit/Source/platform/DragImage.cpp
@@ -66,7 +66,7 @@ const float kMaxDragLabelStringWidth = (kMaxDragLabelWidth - 2 * kDragLabelBorde
 const float kDragLinkLabelFontSize = 11;
 const float kDragLinkUrlFontSize = 10;
 
-PassOwnPtr<DragImage> DragImage::create(Image* image, RespectImageOrientationEnum shouldRespectImageOrientation)
+PassOwnPtr<DragImage> DragImage::create(Image* image, RespectImageOrientationEnum shouldRespectImageOrientation, float deviceScaleFactor)
 {
     if (!image)
         return nullptr;
@@ -98,14 +98,14 @@ PassOwnPtr<DragImage> DragImage::create(Image* image, RespectImageOrientationEnu
             canvas.concat(affineTransformToSkMatrix(orientation.transformFromDefault(sizeRespectingOrientation)));
             canvas.drawBitmapRect(bitmap->bitmap(), 0, destRect);
 
-            return adoptPtr(new DragImage(skBitmap, bitmap->resolutionScale()));
+            return adoptPtr(new DragImage(skBitmap, deviceScaleFactor));
         }
     }
 
     SkBitmap skBitmap;
     if (!bitmap->bitmap().copyTo(&skBitmap, SkBitmap::kARGB_8888_Config))
         return nullptr;
-    return adoptPtr(new DragImage(skBitmap, bitmap->resolutionScale()));
+    return adoptPtr(new DragImage(skBitmap, deviceScaleFactor));
 }
 
 static Font deriveDragLabelFont(int size, FontWeight fontWeight, const FontDescription& systemFont)
@@ -198,7 +198,7 @@ PassOwnPtr<DragImage> DragImage::create(const KURL& url, const String& inLabel, 
     buffer->context()->drawBidiText(urlFont, TextRunPaintInfo(textRun), textPos);
 
     RefPtr<Image> image = buffer->copyImage();
-    return DragImage::create(image.get());
+    return DragImage::create(image.get(), DoNotRespectImageOrientation, deviceScaleFactor);
 }
 
 DragImage::DragImage(const SkBitmap& bitmap, float resolutionScale)

third_party/WebKit/Source/platform/DragImage.h
@@ -39,7 +39,7 @@ class KURL;
 
 class PLATFORM_EXPORT DragImage {
 public:
-    static PassOwnPtr<DragImage> create(Image*, RespectImageOrientationEnum = DoNotRespectImageOrientation);
+    static PassOwnPtr<DragImage> create(Image*, RespectImageOrientationEnum = DoNotRespectImageOrientation, float deviceScaleFactor = 1);
     static PassOwnPtr<DragImage> create(const KURL&, const String& label, const FontDescription& systemFont, float deviceScaleFactor);
     ~DragImage();
 

third_party/WebKit/Source/platform/graphics/skia/NativeImageSkia.cpp
@@ -253,21 +253,18 @@ void NativeImageSkia::drawResampledBitmap(GraphicsContext* context, SkPaint& pai
 }
 
 NativeImageSkia::NativeImageSkia()
-    : m_resolutionScale(1)
-    , m_resizeRequests(0)
+    : m_resizeRequests(0)
 {
 }
 
-NativeImageSkia::NativeImageSkia(const SkBitmap& other, float resolutionScale)
+NativeImageSkia::NativeImageSkia(const SkBitmap& other)
     : m_image(other)
-    , m_resolutionScale(resolutionScale)
     , m_resizeRequests(0)
 {
 }
 
-NativeImageSkia::NativeImageSkia(const SkBitmap& image, float resolutionScale, const SkBitmap& resizedImage, const ImageResourceInfo& cachedImageInfo, int resizeRequests)
+NativeImageSkia::NativeImageSkia(const SkBitmap& image, const SkBitmap& resizedImage, const ImageResourceInfo& cachedImageInfo, int resizeRequests)
     : m_image(image)
-    , m_resolutionScale(resolutionScale)
     , m_resizedImage(resizedImage)
     , m_cachedImageInfo(cachedImageInfo)
     , m_resizeRequests(resizeRequests)

third_party/WebKit/Source/platform/graphics/skia/NativeImageSkia.h
@@ -79,9 +79,9 @@ public:
     // This factory method does a shallow copy of the passed-in SkBitmap
     // (ie., it references the same pixel data and bumps the refcount). Use
     // only when you want sharing semantics.
-    static PassRefPtr<NativeImageSkia> create(const SkBitmap& bitmap, float resolutionScale = 1)
+    static PassRefPtr<NativeImageSkia> create(const SkBitmap& bitmap)
     {
-        return adoptRef(new NativeImageSkia(bitmap, resolutionScale));
+        return adoptRef(new NativeImageSkia(bitmap));
     }
 
     // This method does a shallow copy of the internal SkBitmap (ie., it
@@ -89,7 +89,7 @@ public:
     // you want sharing semantics.
     PassRefPtr<NativeImageSkia> clone() const
     {
-        return adoptRef(new NativeImageSkia(m_image, m_resolutionScale, m_resizedImage, m_cachedImageInfo, m_resizeRequests));
+        return adoptRef(new NativeImageSkia(m_image, m_resizedImage, m_cachedImageInfo, m_resizeRequests));
     }
 
     ~NativeImageSkia();
@@ -110,8 +110,6 @@ public:
     // Get reference to the internal SkBitmap representing this image.
     const SkBitmap& bitmap() const { return m_image; }
     SkBitmap& bitmap() { return m_image; }
-
-    float resolutionScale() const { return m_resolutionScale; }
 
     // We can keep a resized version of the bitmap cached on this object.
     // This function will return true if there is a cached version of the given
@@ -142,7 +140,7 @@ public:
 private:
     NativeImageSkia();
 
-    NativeImageSkia(const SkBitmap&, float resolutionScale);
+    NativeImageSkia(const SkBitmap&);
 
     // ImageResourceInfo is used to uniquely identify cached or requested image
     // resizes.
@@ -158,7 +156,7 @@ private:
         SkIRect rectInSubset(const SkIRect& otherScaledImageRect);
     };
 
-    NativeImageSkia(const SkBitmap& image, float resolutionScale, const SkBitmap& resizedImage, const ImageResourceInfo&, int resizeRequests);
+    NativeImageSkia(const SkBitmap& image, const SkBitmap& resizedImage, const ImageResourceInfo&, int resizeRequests);
 
     // Returns true if the given resize operation should either resize the whole
     // image and cache it, or resize just the part it needs and throw the result
@@ -181,7 +179,6 @@ private:
 
     // The original image.
     SkBitmap m_image;
-    float m_resolutionScale;
 
     // The cached bitmap fragment. This is a subset of the scaled version of
     // |m_image|. empty() returns true if there is no cached image.