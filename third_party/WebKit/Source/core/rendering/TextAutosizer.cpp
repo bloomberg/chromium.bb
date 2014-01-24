39638487ac950eb93a770ed931b9f4cfa1321673
timvolodine@chromium.org <timvolodine@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-24 15:53:08 +0000

    TextAutosizer: fix second-pass container autosizing.
    This patch adds a check to properly autosize containers inside
    stale clusters during the second pass of the algorithm (see
    crbug.com/335589 for more details). Additionaly this patch
    removes an unused method and adds a test.
    
    BUG=335589,334228
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/141903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165757 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 5749ba4cf7e07652be15340068a98643326bfe8f

--- third_party/WebKit/Source/core/rendering/TextAutosizer.cpp
+++ third_party/WebKit/Source/core/rendering/TextAutosizer.cpp
@@ -322,7 +322,7 @@ void TextAutosizer::processCluster(TextAutosizingClusterInfo& clusterInfo, Rende
     float textWidth = clusterInfo.blockContainingAllText->contentLogicalWidth();
 
     Vector<TextAutosizingClusterInfo> clusterInfos(1, clusterInfo);
-    float multiplier =  computeMultiplier(clusterInfos, windowInfo, textWidth);
+    float multiplier = computeMultiplier(clusterInfos, windowInfo, textWidth);
 
     processClusterInternal(clusterInfo, container, subtreeRoot, windowInfo, multiplier);
 }
@@ -353,14 +353,19 @@ void TextAutosizer::secondPassProcessStaleNonAutosizedClusters()
         float multiplier = m_hashToMultiplier.get(hash);
         Vector<TextAutosizingClusterInfo>* val = m_nonAutosizedClusters.get(hash);
         for (Vector<TextAutosizingClusterInfo>::iterator it2 = val->begin(); it2 != val->end(); ++it2)
-            setMultiplierForCluster(multiplier, (*it2).root, *it2);
+            processStaleContainer(multiplier, (*it2).root, *it2);
     }
 }
 
-void TextAutosizer::setMultiplierForCluster(float localMultiplier, RenderObject* cluster, TextAutosizingClusterInfo& clusterInfo)
+void TextAutosizer::processStaleContainer(float multiplier, RenderBlock* cluster, TextAutosizingClusterInfo& clusterInfo)
 {
+    ASSERT(isAutosizingContainer(cluster));
+
     // This method is different from processContainer() mainly in that it does not recurse into sub-clusters.
-    // Multiplier updates are restricted to the specified cluster only.
+    // Multiplier updates are restricted to the specified cluster only. Also the multiplier > 1 by construction
+    // of m_hashesToAutosizeSecondPass, so we don't need to check it explicitly.
+    float localMultiplier = containerShouldBeAutosized(cluster) ? multiplier : 1;
+
     RenderObject* descendant = nextInPreOrderSkippingDescendantsOfContainers(cluster, cluster);
     while (descendant) {
         if (descendant->isText()) {
@@ -371,7 +376,7 @@ void TextAutosizer::setMultiplierForCluster(float localMultiplier, RenderObject*
         } else if (isAutosizingContainer(descendant)) {
             RenderBlock* descendantBlock = toRenderBlock(descendant);
             if (!isAutosizingCluster(descendantBlock, clusterInfo))
-                setMultiplierForCluster(localMultiplier, descendantBlock, clusterInfo);
+                processStaleContainer(multiplier, descendantBlock, clusterInfo);
         }
         descendant = nextInPreOrderSkippingDescendantsOfContainers(descendant, cluster);
     }
@@ -669,12 +674,6 @@ bool TextAutosizer::contentHeightIsConstrained(const RenderBlock* container)
             return false;
     }
     return false;
-}
-
-bool TextAutosizer::clusterShouldBeAutosized(TextAutosizingClusterInfo& clusterInfo, float blockWidth)
-{
-    Vector<TextAutosizingClusterInfo> clusterInfos(1, clusterInfo);
-    return compositeClusterShouldBeAutosized(clusterInfos, blockWidth);
 }
 
 bool TextAutosizer::compositeClusterShouldBeAutosized(Vector<TextAutosizingClusterInfo>& clusterInfos, float blockWidth)
