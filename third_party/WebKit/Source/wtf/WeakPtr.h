bfbfc752a1a63f66a68b9202e19f32a035925a7b
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-24 07:24:11 +0000

    Add an assertion and use get() in WeakPtr::operator->.
    This CL makes WTF::WeakPtr<>::operator-> synchronized with
    base::WeakPtr::operator-> in Chromium.
    
    BUG=none
    TEST=none; no behavior changes.
    
    Review URL: https://codereview.chromium.org/135473003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165711 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: fbd9a03fe639980a243ee79e54d06c9be6d1da3a

--- third_party/WebKit/Source/wtf/WeakPtr.h
+++ third_party/WebKit/Source/wtf/WeakPtr.h
@@ -88,7 +88,12 @@ public:
     WeakPtr(PassRefPtr<WeakReference<T> > ref) : m_ref(ref) { }
 
     T* get() const { return m_ref ? m_ref->get() : 0; }
-    T* operator->() const { return m_ref->get(); }
+
+    T* operator->() const
+    {
+        ASSERT(get());
+        return get();
+    }
 
     typedef RefPtr<WeakReference<T> > (WeakPtr::*UnspecifiedBoolType);
     operator UnspecifiedBoolType() const { return get() ? &WeakPtr::m_ref : 0; }
