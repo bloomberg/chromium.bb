2214c471b465cb741e2fde1f6561b10306236f0e
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-24 21:18:27 +0000

    Rewrite AUHAL unit tests to use the audio thread correctly.
    These tests should only access the audio stream on the audio
    thread.  I removed one test which was testing unsupported behavior
    and another which needlessly played sounds.  The Start() test now
    waits for the first callback before exiting instead of sleeping.
    
    The audio thread is co-opted by specifying a TYPE_UI MessageLoop
    which AudioManagerBase will pick up and use as the audio thread
    (only on OSX).
    
    BUG=none
    TEST=tests pass.
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/146053003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@246966 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 94de0edaefbab2218c99c5f2464ac33b8ce44dd2

--- media/audio/mac/audio_auhal_mac_unittest.cc
+++ media/audio/mac/audio_auhal_mac_unittest.cc
@@ -4,28 +4,30 @@
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
+#include "base/synchronization/waitable_event.h"
 #include "media/audio/audio_io.h"
 #include "media/audio/audio_manager.h"
-#include "media/audio/simple_sources.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using ::testing::_;
-using ::testing::AnyNumber;
-using ::testing::DoAll;
-using ::testing::Field;
-using ::testing::InSequence;
-using ::testing::Invoke;
-using ::testing::NiceMock;
-using ::testing::NotNull;
-using ::testing::Return;
-
-static const int kBitsPerSample = 16;
+using testing::_;
+using testing::DoAll;
+using testing::Return;
 
 // TODO(crogers): Most of these tests can be made platform agnostic.
 // http://crbug.com/223242
 
 namespace media {
+
+ACTION(ZeroBuffer) {
+  arg1->Zero();
+}
+
+ACTION_P(SignalEvent, event) {
+  event->Signal();
+}
 
 class MockAudioSourceCallback : public AudioOutputStream::AudioSourceCallback {
  public:
@@ -37,183 +39,74 @@ class MockAudioSourceCallback : public AudioOutputStream::AudioSourceCallback {
   MOCK_METHOD1(OnError, void(AudioOutputStream* stream));
 };
 
-// Convenience method which creates a default AudioOutputStream object but
-// also allows the user to modify the default settings.
-class AudioOutputStreamWrapper {
+class AUHALStreamTest : public testing::Test {
  public:
-  explicit AudioOutputStreamWrapper()
-      : audio_man_(AudioManager::CreateForTesting()),
-        format_(AudioParameters::AUDIO_PCM_LOW_LATENCY),
-        bits_per_sample_(kBitsPerSample) {
-    AudioParameters preferred_params =
-        audio_man_->GetDefaultOutputStreamParameters();
-    channel_layout_ = preferred_params.channel_layout();
-    channels_ = preferred_params.channels();
-    sample_rate_ = preferred_params.sample_rate();
-    samples_per_packet_ = preferred_params.frames_per_buffer();
+  AUHALStreamTest()
+      : message_loop_(base::MessageLoop::TYPE_UI),
+        manager_(AudioManager::CreateForTesting()) {}
+
+  virtual ~AUHALStreamTest() {
+    base::RunLoop().RunUntilIdle();
   }
 
-  ~AudioOutputStreamWrapper() {}
-
-  // Creates AudioOutputStream object using default parameters.
   AudioOutputStream* Create() {
-    return CreateOutputStream();
+    return manager_->MakeAudioOutputStream(
+        manager_->GetDefaultOutputStreamParameters(), "", "");
   }
-
-  // Creates AudioOutputStream object using non-default parameters where the
-  // frame size is modified.
-  AudioOutputStream* Create(int samples_per_packet) {
-    samples_per_packet_ = samples_per_packet;
-    return CreateOutputStream();
-  }
-
-  // Creates AudioOutputStream object using non-default parameters where the
-  // sample rate is modified.
-  AudioOutputStream* CreateWithSampleRate(int sample_rate) {
-    sample_rate_ = sample_rate;
-    return CreateOutputStream();
-  }
-
-  // Creates AudioOutputStream object using non-default parameters where the
-  // channel layout is modified.
-  AudioOutputStream* CreateWithLayout(ChannelLayout layout) {
-    channel_layout_ = layout;
-    channels_ = ChannelLayoutToChannelCount(layout);
-    return CreateOutputStream();
-  }
-
-  AudioParameters::Format format() const { return format_; }
-  int channels() const { return ChannelLayoutToChannelCount(channel_layout_); }
-  int bits_per_sample() const { return bits_per_sample_; }
-  int sample_rate() const { return sample_rate_; }
-  int samples_per_packet() const { return samples_per_packet_; }
 
   bool CanRunAudioTests() {
-    return audio_man_->HasAudioOutputDevices();
+    return manager_->HasAudioOutputDevices();
   }
+
+ protected:
+  base::MessageLoop message_loop_;
+  scoped_ptr<AudioManager> manager_;
+  MockAudioSourceCallback source_;
 
  private:
-  AudioOutputStream* CreateOutputStream() {
-    AudioParameters params;
-    params.Reset(format_, channel_layout_,
-                 channels_, 0,
-                 sample_rate_, bits_per_sample_,
-                 samples_per_packet_);
-
-    AudioOutputStream* aos = audio_man_->MakeAudioOutputStream(params,
-        std::string(), std::string());
-    EXPECT_TRUE(aos);
-    return aos;
-  }
-
-  scoped_ptr<AudioManager> audio_man_;
-
-  AudioParameters::Format format_;
-  ChannelLayout channel_layout_;
-  int channels_;
-  int bits_per_sample_;
-  int sample_rate_;
-  int samples_per_packet_;
+  DISALLOW_COPY_AND_ASSIGN(AUHALStreamTest);
 };
 
-// Test that we can get the hardware sample-rate.
-TEST(AUHALStreamTest, HardwareSampleRate) {
-  AudioOutputStreamWrapper aosw;
-  if (!aosw.CanRunAudioTests())
+TEST_F(AUHALStreamTest, HardwareSampleRate) {
+  if (!CanRunAudioTests())
     return;
-
-  int sample_rate = aosw.sample_rate();
-  EXPECT_GE(sample_rate, 16000);
-  EXPECT_LE(sample_rate, 192000);
+  const AudioParameters preferred_params =
+      manager_->GetDefaultOutputStreamParameters();
+  EXPECT_GE(preferred_params.sample_rate(), 16000);
+  EXPECT_LE(preferred_params.sample_rate(), 192000);
 }
 
-// Test Create(), Close() calling sequence.
-TEST(AUHALStreamTest, CreateAndClose) {
-  AudioOutputStreamWrapper aosw;
-  if (!aosw.CanRunAudioTests())
+TEST_F(AUHALStreamTest, CreateClose) {
+  if (!CanRunAudioTests())
     return;
-
-  AudioOutputStream* aos = aosw.Create();
-  aos->Close();
+  Create()->Close();
 }
 
-// Test Open(), Close() calling sequence.
-TEST(AUHALStreamTest, OpenAndClose) {
-  AudioOutputStreamWrapper aosw;
-  if (!aosw.CanRunAudioTests())
+TEST_F(AUHALStreamTest, CreateOpenClose) {
+  if (!CanRunAudioTests())
     return;
-
-  AudioOutputStream* aos = aosw.Create();
-  EXPECT_TRUE(aos->Open());
-  aos->Close();
+  AudioOutputStream* stream = Create();
+  EXPECT_TRUE(stream->Open());
+  stream->Close();
 }
 
-// Test Open(), Start(), Close() calling sequence.
-TEST(AUHALStreamTest, OpenStartAndClose) {
-  AudioOutputStreamWrapper aosw;
-  if (!aosw.CanRunAudioTests())
+TEST_F(AUHALStreamTest, CreateOpenStartStopClose) {
+  if (!CanRunAudioTests())
     return;
 
-  AudioOutputStream* aos = aosw.Create();
-  EXPECT_TRUE(aos->Open());
-  MockAudioSourceCallback source;
-  EXPECT_CALL(source, OnError(aos))
-      .Times(0);
-  aos->Start(&source);
-  aos->Close();
-}
+  AudioOutputStream* stream = Create();
+  EXPECT_TRUE(stream->Open());
 
-// Test Open(), Start(), Stop(), Close() calling sequence.
-TEST(AUHALStreamTest, OpenStartStopAndClose) {
-  AudioOutputStreamWrapper aosw;
-  if (!aosw.CanRunAudioTests())
-    return;
+  // Wait for the first data callback from the OS.
+  base::WaitableEvent event(false, false);
+  EXPECT_CALL(source_, OnMoreIOData(_, _, _))
+      .WillOnce(DoAll(ZeroBuffer(), SignalEvent(&event), Return(0)));
+  EXPECT_CALL(source_, OnError(_)).Times(0);
+  stream->Start(&source_);
+  event.Wait();
 
-  AudioOutputStream* aos = aosw.Create();
-  EXPECT_TRUE(aos->Open());
-  MockAudioSourceCallback source;
-  EXPECT_CALL(source, OnError(aos))
-      .Times(0);
-  aos->Start(&source);
-  aos->Stop();
-  aos->Close();
-}
-
-// This test produces actual audio for 0.5 seconds on the default audio device
-// at the hardware sample-rate (usually 44.1KHz).
-// Parameters have been chosen carefully so you should not hear
-// pops or noises while the sound is playing.
-TEST(AUHALStreamTest, AUHALStreamPlay200HzTone) {
-  AudioOutputStreamWrapper aosw;
-  if (!aosw.CanRunAudioTests())
-    return;
-
-  AudioOutputStream* aos = aosw.CreateWithLayout(CHANNEL_LAYOUT_MONO);
-
-  EXPECT_TRUE(aos->Open());
-
-  SineWaveAudioSource source(1, 200.0, aosw.sample_rate());
-  aos->Start(&source);
-  usleep(500000);
-
-  aos->Stop();
-  aos->Close();
-}
-
-// Test that Open() will fail with a sample-rate which isn't the hardware
-// sample-rate.
-TEST(AUHALStreamTest, AUHALStreamInvalidSampleRate) {
-  AudioOutputStreamWrapper aosw;
-  if (!aosw.CanRunAudioTests())
-    return;
-
-  int non_default_sample_rate = aosw.sample_rate() == 44100 ?
-      48000 : 44100;
-  AudioOutputStream* aos = aosw.CreateWithSampleRate(non_default_sample_rate);
-
-  EXPECT_FALSE(aos->Open());
-
-  aos->Close();
+  stream->Stop();
+  stream->Close();
 }
 
 }  // namespace media
