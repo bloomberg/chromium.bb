a480c25adbb6657d6088a73f27118d5ed45c0e5f
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-25 07:10:12 +0000

    Add the GetHelp app to the feedback whitelist.
    R=xiyuan@chromium.org
    BUG=330644
    
    Review URL: https://codereview.chromium.org/147453002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247078 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 99bb650fe3f78c4059a466e060c823a3fd823c59

--- chrome/browser/resources/feedback/js/event_handler.js
+++ chrome/browser/resources/feedback/js/event_handler.js
@@ -27,7 +27,8 @@ var whitelistedExtensionIds = [
   'eemlkeanncmjljgehlbplemhmdmalhdc', // Connectivity Diagnostics
   'kodldpbjkkmmnilagfdheibampofhaom', // Connectivity Diagnostics
   'kkebgepbbgbcmghedmmdfcbdcodlkngh', // Chrome OS Recovery Tool
-  'jndclpdbaamdhonoechobihbbiimdgai'  // Chrome OS Recovery Tool
+  'jndclpdbaamdhonoechobihbbiimdgai', // Chrome OS Recovery Tool
+  'ljoammodoonkhnehlncldjelhidljdpi'  // GetHelp app.
 ];
 
 /**
