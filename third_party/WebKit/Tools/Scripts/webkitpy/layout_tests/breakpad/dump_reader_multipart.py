3fb94f66867b54e205079684b309cfc47666f407
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-25 22:52:32 +0000

    Make the multipart dump reader more resillient to incorrect dumps
    If the content shell browser process dies between the time the minidump
    is written and the time it's turned into a multipart file, the dump
    reader will fail to read the dump and throw an exception instead.
    
    BUG=none
    R=dpranke@chromium.org
    
    Review URL: https://codereview.chromium.org/142323007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165816 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: a9fa6a8fd091fa31c6d1b617fdf12d8a0921a5d3

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/breakpad/dump_reader_multipart.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/breakpad/dump_reader_multipart.py
@@ -81,8 +81,11 @@ class DumpReaderMultipart(DumpReader):
         with self._host.filesystem.open_binary_file_for_reading(dump_file) as f:
             boundary = f.readline().strip()[2:]
             f.seek(0)
-            data = cgi.parse_multipart(f, {'boundary': boundary})
-            return data
+            try:
+                data = cgi.parse_multipart(f, {'boundary': boundary})
+                return data
+            except:
+                pass
         return None
 
     def _check_breakpad_tools_available(self):
