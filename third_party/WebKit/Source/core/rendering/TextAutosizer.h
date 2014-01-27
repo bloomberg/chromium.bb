a8fa3c99287987c4a86c40b2612fcccde7c4a2cc
timvolodine@chromium.org <timvolodine@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-27 15:17:23 +0000

    TextAutosizer: fix second-pass container autosizing.
    This patch adds a check to properly autosize containers inside
    stale clusters during the second pass of the algorithm (see
    crbug.com/335589 for more details). Additionaly this patch
    removes an unused method and adds a test.
    
    BUG=335589,334228
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=165757
    
    Review URL: https://codereview.chromium.org/141903002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165856 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: a7bcf948491dfee0a343396e902b95c70ff3ead0

--- third_party/WebKit/Source/core/rendering/TextAutosizer.h
+++ third_party/WebKit/Source/core/rendering/TextAutosizer.h
@@ -105,7 +105,6 @@ private:
     static bool containerContainsOneOfTags(const RenderBlock* cluster, const Vector<QualifiedName>& tags);
     static bool containerIsRowOfLinks(const RenderObject* container);
     static bool contentHeightIsConstrained(const RenderBlock* container);
-    static bool clusterShouldBeAutosized(TextAutosizingClusterInfo&, float blockWidth);
     static bool compositeClusterShouldBeAutosized(Vector<TextAutosizingClusterInfo>&, float blockWidth);
     static void measureDescendantTextWidth(const RenderBlock* container, TextAutosizingClusterInfo&, float minTextWidth, float& textWidth);
     unsigned computeCompositeClusterHash(Vector<TextAutosizingClusterInfo>&);
@@ -127,7 +126,7 @@ private:
 
     void addNonAutosizedCluster(unsigned key, TextAutosizingClusterInfo& value);
     void secondPassProcessStaleNonAutosizedClusters();
-    void setMultiplierForCluster(float localMultiplier, RenderObject* cluster, TextAutosizingClusterInfo&);
+    void processStaleContainer(float multiplier, RenderBlock* cluster, TextAutosizingClusterInfo&);
 
     Document* m_document;
 
