d6784e205e8d7d4e0b75b88b7f4a600d21a18413
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-27 19:32:26 +0000

    Re-land r246921: "Allow running content-shell headless with --dump-render-tree."
    This change reverts r247148 and re-lands r246921. It's unclear if there
    really is something wrong with this patch, and the crashes in
    run-webkit-tests were addressed in Blink r165816.
    
    TBR=jochen@chromium.org, sadrul@chromium.org
    BUG=316712
    
    Review URL: https://codereview.chromium.org/132903012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247278 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a659114b2a7b20b097e4046f79a07622eb3f7cd3

--- content/shell/browser/shell_aura.cc
+++ content/shell/browser/shell_aura.cc
@@ -6,155 +6,20 @@
 
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_view.h"
-#include "content/shell/browser/shell_aura.h"
-#include "ui/aura/client/aura_constants.h"
-#include "ui/aura/client/default_activation_client.h"
-#include "ui/aura/client/default_capture_client.h"
+#include "content/shell/browser/shell_platform_data_aura.h"
 #include "ui/aura/env.h"
-#include "ui/aura/layout_manager.h"
 #include "ui/aura/root_window.h"
-#include "ui/aura/test/test_focus_client.h"
 #include "ui/aura/test/test_screen.h"
-#include "ui/aura/test/test_window_tree_client.h"
 #include "ui/aura/window.h"
-#include "ui/base/ime/input_method.h"
-#include "ui/base/ime/input_method_delegate.h"
-#include "ui/base/ime/input_method_factory.h"
-#include "ui/gfx/screen.h"
 
 namespace content {
-
-namespace {
-
-class FillLayout : public aura::LayoutManager {
- public:
-  explicit FillLayout(aura::RootWindow* root)
-      : root_(root) {
-  }
-
-  virtual ~FillLayout() {}
-
- private:
-  // aura::LayoutManager:
-  virtual void OnWindowResized() OVERRIDE {
-  }
-
-  virtual void OnWindowAddedToLayout(aura::Window* child) OVERRIDE {
-    child->SetBounds(root_->window()->bounds());
-  }
-
-  virtual void OnWillRemoveWindowFromLayout(aura::Window* child) OVERRIDE {
-  }
-
-  virtual void OnWindowRemovedFromLayout(aura::Window* child) OVERRIDE {
-  }
-
-  virtual void OnChildWindowVisibilityChanged(aura::Window* child,
-                                              bool visible) OVERRIDE {
-  }
-
-  virtual void SetChildBounds(aura::Window* child,
-                              const gfx::Rect& requested_bounds) OVERRIDE {
-    SetChildBoundsDirect(child, requested_bounds);
-  }
-
-  aura::RootWindow* root_;
-
-  DISALLOW_COPY_AND_ASSIGN(FillLayout);
-};
-
-class MinimalInputEventFilter : public ui::internal::InputMethodDelegate,
-                                public ui::EventHandler {
- public:
-  explicit MinimalInputEventFilter(aura::RootWindow* root)
-      : root_(root),
-        input_method_(ui::CreateInputMethod(this,
-                                            gfx::kNullAcceleratedWidget)) {
-    input_method_->Init(true);
-    root_->window()->AddPreTargetHandler(this);
-    root_->window()->SetProperty(aura::client::kRootWindowInputMethodKey,
-                       input_method_.get());
-  }
-
-  virtual ~MinimalInputEventFilter() {
-    root_->window()->RemovePreTargetHandler(this);
-    root_->window()->SetProperty(aura::client::kRootWindowInputMethodKey,
-                       static_cast<ui::InputMethod*>(NULL));
-  }
-
- private:
-  // ui::EventHandler:
-  virtual void OnKeyEvent(ui::KeyEvent* event) OVERRIDE {
-    const ui::EventType type = event->type();
-    if (type == ui::ET_TRANSLATED_KEY_PRESS ||
-        type == ui::ET_TRANSLATED_KEY_RELEASE) {
-      // The |event| is already handled by this object, change the type of the
-      // event to ui::ET_KEY_* and pass it to the next filter.
-      static_cast<ui::TranslatedKeyEvent*>(event)->ConvertToKeyEvent();
-    } else {
-      if (input_method_->DispatchKeyEvent(*event))
-        event->StopPropagation();
-    }
-  }
-
-  // ui::InputMethodDelegate:
-  virtual bool DispatchKeyEventPostIME(
-      const base::NativeEvent& event) OVERRIDE {
-    ui::TranslatedKeyEvent aura_event(event, false /* is_char */);
-    return root_->AsWindowTreeHostDelegate()->OnHostKeyEvent(
-        &aura_event);
-  }
-
-  virtual bool DispatchFabricatedKeyEventPostIME(ui::EventType type,
-                                                 ui::KeyboardCode key_code,
-                                                 int flags) OVERRIDE {
-    ui::TranslatedKeyEvent aura_event(type == ui::ET_KEY_PRESSED, key_code,
-                                      flags);
-    return root_->AsWindowTreeHostDelegate()->OnHostKeyEvent(
-        &aura_event);
-  }
-
-  aura::RootWindow* root_;
-  scoped_ptr<ui::InputMethod> input_method_;
-
-  DISALLOW_COPY_AND_ASSIGN(MinimalInputEventFilter);
-};
-
-}
-
-ShellAuraPlatformData* Shell::platform_ = NULL;
-
-ShellAuraPlatformData::ShellAuraPlatformData() {
-  aura::TestScreen* screen = aura::TestScreen::Create();
-  gfx::Screen::SetScreenInstance(gfx::SCREEN_TYPE_NATIVE, screen);
-  root_window_.reset(screen->CreateRootWindowForPrimaryDisplay());
-  root_window_->host()->Show();
-  root_window_->window()->SetLayoutManager(new FillLayout(root_window_.get()));
-
-  focus_client_.reset(new aura::test::TestFocusClient());
-  aura::client::SetFocusClient(root_window_->window(), focus_client_.get());
-
-  activation_client_.reset(
-      new aura::client::DefaultActivationClient(root_window_->window()));
-  capture_client_.reset(
-      new aura::client::DefaultCaptureClient(root_window_->window()));
-  window_tree_client_.reset(
-      new aura::test::TestWindowTreeClient(root_window_->window()));
-  ime_filter_.reset(new MinimalInputEventFilter(root_window_.get()));
-}
-
-ShellAuraPlatformData::~ShellAuraPlatformData() {
-}
-
-void ShellAuraPlatformData::ResizeWindow(int width, int height) {
-  root_window_->host()->SetBounds(gfx::Rect(width, height));
-}
 
 // static
 void Shell::PlatformInitialize(const gfx::Size& default_window_size) {
   CHECK(!platform_);
-  aura::Env::CreateInstance();
-  platform_ = new ShellAuraPlatformData();
+  aura::TestScreen* screen = aura::TestScreen::Create();
+  gfx::Screen::SetScreenInstance(gfx::SCREEN_TYPE_NATIVE, screen);
+  platform_ = new ShellPlatformDataAura(default_window_size);
 }
 
 void Shell::PlatformExit() {
@@ -178,7 +43,9 @@ void Shell::PlatformSetIsLoading(bool loading) {
 
 void Shell::PlatformCreateWindow(int width, int height) {
   CHECK(platform_);
-  platform_->ResizeWindow(width, height);
+  if (!headless_)
+    platform_->ShowWindow();
+  platform_->ResizeWindow(gfx::Size(width, height));
 }
 
 void Shell::PlatformSetContents() {
@@ -195,7 +62,10 @@ void Shell::PlatformResizeSubViews() {
 }
 
 void Shell::Close() {
-  web_contents_.reset();
+  if (headless_)
+    delete this;
+  else
+    web_contents_.reset();
 }
 
 void Shell::PlatformSetTitle(const base::string16& title) {
