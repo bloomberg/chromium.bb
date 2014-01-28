3e9b033bbb5689adb7e1147d1dc4f14637b40f65
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-28 21:11:15 +0000

    Add support for two arguments to base::BindBlock.
    BUG=None
    
    Review URL: https://codereview.chromium.org/148263010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247502 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d15483914b628725d66f1fedbaadd90e18313de3

--- base/mac/bind_objc_block_unittest.mm
+++ base/mac/bind_objc_block_unittest.mm
@@ -4,6 +4,8 @@
 
 #import "base/mac/bind_objc_block.h"
 
+#include <string>
+
 #include "base/callback.h"
 #include "base/bind.h"
 #include "base/callback_helpers.h"
@@ -49,6 +51,17 @@ TEST(BindObjcBlockTest, TestArgument) {
   const int kArgument = 42;
   base::Callback<int(int)> c = base::BindBlock(^(int a){return a + 1;});
   EXPECT_EQ(kArgument + 1, c.Run(kArgument));
+}
+
+TEST(BindObjcBlockTest, TestTwoArguments) {
+  std::string result;
+  std::string* ptr = &result;
+  base::Callback<void(const std::string&, const std::string&)> c =
+      base::BindBlock(^(const std::string& a, const std::string& b) {
+          *ptr = a + b;
+      });
+  c.Run("forty", "two");
+  EXPECT_EQ(result, "fortytwo");
 }
 
 }  // namespace
