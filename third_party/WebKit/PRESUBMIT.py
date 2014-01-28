029ca9eafdebb08efa5c1991550e19bdeb242adc
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-28 10:41:17 +0000

    DevTools: Introduce scripts for generating png images from svg images and presubmit check for them.
    This patch adds a script to convert svg images to png images and a script to optimize png images in the devtools front-end Images folder.
    Presubmit check also added that checks that these scripts were used for all svg files that were changed/added with a patch.
    
    Landing PRESUBMIT.py changes TBR since I am only deleting devtools checks from this file.
    
    R=eustas@chromium.org, pfeldman@chromium.org, eustas, pfeldman
    TBR=thakis
    
    Review URL: https://codereview.chromium.org/130423008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@165926 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ff8cf59121867e7bee26897cd6061702f0211d93

--- third_party/WebKit/PRESUBMIT.py
+++ third_party/WebKit/PRESUBMIT.py
@@ -246,24 +246,6 @@ def _CheckChromiumPlatformMacros(input_api, output_api, source_file_filter=None)
     return []
 
 
-def _CompileDevtoolsFrontend(input_api, output_api):
-    if not input_api.platform.startswith('linux'):
-        return []
-    local_paths = [f.LocalPath() for f in input_api.AffectedFiles()]
-    if (any("devtools/front_end" in path for path in local_paths) or
-        any("InjectedScriptSource.js" in path for path in local_paths) or
-        any("InjectedScriptCanvasModuleSource.js" in path for path in local_paths)):
-        lint_path = input_api.os_path.join(input_api.PresubmitLocalPath(),
-            "Source", "devtools", "scripts", "compile_frontend.py")
-        out, _ = input_api.subprocess.Popen(
-            [input_api.python_executable, lint_path],
-            stdout=input_api.subprocess.PIPE,
-            stderr=input_api.subprocess.STDOUT).communicate()
-        if "WARNING" in out or "ERROR" in out:
-            return [output_api.PresubmitError(out)]
-    return []
-
-
 def _CheckForPrintfDebugging(input_api, output_api):
     """Generally speaking, we'd prefer not to land patches that printf
     debug output."""
@@ -294,7 +276,6 @@ def CheckChangeOnUpload(input_api, output_api):
     results.extend(_CommonChecks(input_api, output_api))
     results.extend(_CheckStyle(input_api, output_api))
     results.extend(_CheckForPrintfDebugging(input_api, output_api))
-    results.extend(_CompileDevtoolsFrontend(input_api, output_api))
     return results
 
 
