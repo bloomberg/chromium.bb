e28dfee9dd7c4c1aa6f345b78984f81cef65cfa3
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-28 23:57:01 +0000

    Use thread.PushSlice instead of Begin/EndSice to reduce timeline flake
    Telemetry's thread logic can actually handle slices even when they're added to
    the thread in arbitarary timestamp order. The BeginSlice/EndSlice calls are
    merely a helper. Since the timeline stream coming out of inspector actually has
    begin and end timestamps, just create slices and push them to the thread: this
    avoids flake from ordering issues rather than adding a hard dependency on
    timeline for the timestamp ordering of its data stream.
    
    BUG=321529
    
    Review URL: https://codereview.chromium.org/137143013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247532 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b1dfcba5c1116ff5dd7f8790cf29dbbf835fd0d7

--- tools/perf/measurements/loading_timeline_unittest.py
+++ tools/perf/measurements/loading_timeline_unittest.py
@@ -4,7 +4,7 @@
 from measurements import loading_timeline
 from telemetry.core import wpr_modes
 from telemetry.page import page_measurement_unittest_base
-from telemetry.unittest import DisabledTest, options_for_unittests
+from telemetry.unittest import options_for_unittests
 
 class LoadingTimelineUnitTest(
       page_measurement_unittest_base.PageMeasurementUnitTestBase):
@@ -12,8 +12,6 @@ class LoadingTimelineUnitTest(
     self._options = options_for_unittests.GetCopy()
     self._options.browser_options.wpr_mode = wpr_modes.WPR_OFF
 
-  # Flaky on XP+Vista: crbug.com/321529
-  @DisabledTest
   def testLoadingTimelineBasic(self):
     ps = self.CreatePageSetFromFileInUnittestDataDir('scrollable_page.html')
     measurement = loading_timeline.LoadingTimeline()
