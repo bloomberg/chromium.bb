33e7b6d5eafc827d3e09b543e7742e3c3ff0fbb7
kjellander@chromium.org <kjellander@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-29 17:40:57 +0000

    Update WebRTC test filter for Chromium tests.
    In https://codereview.chromium.org/137793008/ phoglund@
    changed the test filter for desktop tests after the renames in
    https://codereview.chromium.org/131203005/ and
    https://codereview.chromium.org/137753004/
    
    This CL makes similar change for Android.
    
    BUG=none
    TBR=frankf@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/144483003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247716 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7ca6525cf5e307c4c71717337997d05679e47414

--- build/android/buildbot/bb_run_bot.py
+++ build/android/buildbot/bb_run_bot.py
@@ -179,7 +179,7 @@ def GetBotStepMap():
           extra_gyp='include_tests=1 enable_tracing=1')),
       B('webrtc-chromium-tests', H(std_test_steps),
         T(['webrtc_chromium'],
-          [flakiness_server, '--gtest-filter=Webrtc*:WebRTC*'])),
+          [flakiness_server, '--gtest-filter=WebRtc*'])),
       B('webrtc-native-tests', H(std_test_steps),
         T(['webrtc_native'], [flakiness_server])),
 
