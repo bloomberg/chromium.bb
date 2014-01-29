29de185e6ef2555b153ac0aeed44350545a01534
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-29 18:54:02 +0000

    Add a UseCounter for the HTMLSourceElement.media IDL attribute
    The content attribute no longer does anything, so the IDL attribute
    should also be removed as soon as the usage allows.
    
    BUG=338197
    
    Review URL: https://codereview.chromium.org/146483002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166044 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 1f466ac6f0e5042a143e21e2d8f7308cdcbe8cc0

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -307,6 +307,7 @@ public:
         FormNameAccessForImageElement,
         FormNameAccessForPastNamesMap,
         FormAssociationByParser,
+        HTMLSourceElementMedia,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         NumberOfFeatures, // This enum value must be last.
