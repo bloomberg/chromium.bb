3efb3f5d17d1f9cccf10e47c44c89c9f8cf1d380
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 18:03:32 +0000

    Media Galleries: Initial media scanner implementation.
    BUG=161119
    
    Review URL: https://codereview.chromium.org/149363004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247993 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 70f5773069b025747bb08ace7b387e9380956fc9

--- chrome/browser/media_galleries/fileapi/media_path_filter.h
+++ chrome/browser/media_galleries/fileapi/media_path_filter.h
@@ -5,10 +5,13 @@
 #ifndef CHROME_BROWSER_MEDIA_GALLERIES_FILEAPI_MEDIA_PATH_FILTER_H_
 #define CHROME_BROWSER_MEDIA_GALLERIES_FILEAPI_MEDIA_PATH_FILTER_H_
 
+#include <string>
 #include <vector>
 
+#include "base/containers/hash_tables.h"
 #include "base/files/file_path.h"
 #include "base/sequence_checker.h"
+#include "chrome/browser/media_galleries/media_scan_types.h"
 
 // This class holds the list of file path extensions that we should expose on
 // media filesystem.
@@ -16,17 +19,38 @@ class MediaPathFilter {
  public:
   MediaPathFilter();
   ~MediaPathFilter();
+
+  // Returns true if |path| is a media file.
   bool Match(const base::FilePath& path);
+
+  // Returns the type of |path| or MEDIA_GALLERY_SCAN_FILE_TYPE_UNKNOWN if it
+  // is not a media file.
+  MediaGalleryScanFileType GetType(const base::FilePath& path);
 
  private:
   typedef std::vector<base::FilePath::StringType> MediaFileExtensionList;
 
+  // Key: .extension
+  // Value: MediaGalleryScanFileType, but stored as an int to allow "|="
+  typedef base::hash_map<base::FilePath::StringType, int> MediaFileExtensionMap;
+
   void EnsureInitialized();
+
+  void AddExtensionsToMediaFileExtensionMap(
+      const MediaFileExtensionList& extensions_list,
+      MediaGalleryScanFileType type);
+  void AddAdditionalExtensionsToMediaFileExtensionMap(
+      const base::FilePath::CharType* const* extensions_list,
+      size_t extensions_list_size,
+      MediaGalleryScanFileType type);
+  void AddExtensionToMediaFileExtensionMap(
+      const base::FilePath::CharType* extension,
+      MediaGalleryScanFileType type);
 
   // Checks |initialized_| is only accessed on one sequence.
   base::SequenceChecker sequence_checker_;
   bool initialized_;
-  MediaFileExtensionList media_file_extensions_;
+  MediaFileExtensionMap media_file_extensions_map_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaPathFilter);
 };
