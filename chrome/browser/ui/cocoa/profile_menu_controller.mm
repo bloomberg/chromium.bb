37de3b8566f9d664e1c2693b4f6af7cc479309f8
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 13:48:05 +0000

    [Mac] The GAIA profile photo should be resized in the Users controller menu
    BUG=327087
    TEST=Start chrome with the --google-profile-info flag, and a signed in
    user. If you choose 'Users' for the top menu bar, the signed in user's
    profile photo should be the same size as the other items.
    
    Review URL: https://codereview.chromium.org/149513007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247952 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3bf8496090eba766847539548415b74cd23f252b

--- chrome/browser/ui/cocoa/profile_menu_controller.mm
+++ chrome/browser/ui/cocoa/profile_menu_controller.mm
@@ -12,6 +12,7 @@
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_info_cache.h"
 #include "chrome/browser/profiles/profile_info_interface.h"
+#include "chrome/browser/profiles/profile_info_util.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/profiles/profile_metrics.h"
 #include "chrome/browser/ui/browser.h"
@@ -129,7 +130,16 @@ class Observer : public chrome::BrowserListObserver,
     if (dock) {
       [item setIndentationLevel:1];
     } else {
-      [item setImage:itemData.icon.ToNSImage()];
+      gfx::Image itemIcon = itemData.icon;
+      // The image might be too large and need to be resized (i.e. if this is
+      // a signed-in user using the GAIA profile photo).
+      if (itemIcon.Width() > profiles::kAvatarIconWidth ||
+          itemIcon.Height() > profiles::kAvatarIconHeight) {
+        itemIcon = profiles::GetAvatarIconForWebUI(itemIcon, true);
+      }
+      DCHECK(itemIcon.Width() <= profiles::kAvatarIconWidth);
+      DCHECK(itemIcon.Height() <= profiles::kAvatarIconHeight);
+      [item setImage:itemIcon.ToNSImage()];
       [item setState:itemData.active ? NSOnState : NSOffState];
     }
     [menu insertItem:item atIndex:i + offset];
