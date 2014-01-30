9330891f97851a769faabaa76d083b8c62c3ef36
aruslan@chromium.org <aruslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-30 07:09:14 +0000

    [Android] Add Android to --enable-new-profile-management about flags
    BUG=none
    
    Review URL: https://codereview.chromium.org/130803011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247863 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dc6a48a8158eae3387390bb474644e2fc3b84044

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1693,7 +1693,7 @@ const Experiment kExperiments[] = {
     "enable-new-profile-management",
     IDS_FLAGS_ENABLE_NEW_PROFILE_MANAGEMENT_NAME,
     IDS_FLAGS_ENABLE_NEW_PROFILE_MANAGEMENT_DESCRIPTION,
-    kOsMac | kOsWin | kOsLinux,
+    kOsAndroid | kOsMac | kOsWin | kOsLinux,
     SINGLE_VALUE_TYPE(switches::kNewProfileManagement)
   },
   {
