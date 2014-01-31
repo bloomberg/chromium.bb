e22541d5db833d91160b8f5c6fcc6ae8b101fc2c
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-01-31 15:22:17 +0000

    Revert of Web Animations: Remove dependency on legacy comparison functions (https://codereview.chromium.org/146003004/)
    Reason for revert:
    Regresses recalc performance in the case of transition all.
    
    Original issue's description:
    > Web Animations: Remove dependency on legacy comparison functions
    > 
    > As this has a potential to impact performance when evaluating transition
    > conditions, this patch is being landed in advance of the full removal so that
    > we can evaluate results on the perf bots.
    > 
    > BUG=334225
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=165716
    
    TBR=timloh@chromium.org
    BUG=334225
    
    Review URL: https://codereview.chromium.org/147793003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@166222 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6bee917fb73619f9dc09a8a06f8eb2f852a2cab5

--- third_party/WebKit/Source/core/animation/css/CSSAnimations.cpp
+++ third_party/WebKit/Source/core/animation/css/CSSAnimations.cpp
@@ -46,6 +46,7 @@
 #include "core/events/TransitionEvent.h"
 #include "core/events/WebKitAnimationEvent.h"
 #include "core/frame/UseCounter.h"
+#include "core/frame/animation/CSSPropertyAnimation.h"
 #include "core/rendering/RenderObject.h"
 #include "core/rendering/style/KeyframeList.h"
 #include "platform/animation/TimingFunction.h"
@@ -553,12 +554,12 @@ void CSSAnimations::calculateTransitionUpdateForProperty(CSSPropertyID id, const
     if (anim->duration() + anim->delay() <= 0)
         return;
 
+    if (CSSPropertyAnimation::propertiesEqual(id, &oldStyle, &style))
+        return;
     if (!to)
         to = CSSAnimatableValueFactory::create(id, style);
-    RefPtr<AnimatableValue> from = CSSAnimatableValueFactory::create(id, oldStyle);
-    if (to->equals(from.get()))
-        return;
 
+    RefPtr<AnimatableValue> from = CSSAnimatableValueFactory::create(id, oldStyle);
     // If we have multiple transitions on the same property, we will use the
     // last one since we iterate over them in order.
     if (AnimatableValue::usesDefaultInterpolation(to.get(), from.get()))
