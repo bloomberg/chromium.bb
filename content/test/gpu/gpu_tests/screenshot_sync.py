dc6cfbea8c7743232e2821e0b1fb6c43503430cb
bajones@chromium.org <bajones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-01-31 22:09:11 +0000

    Moved Screenshot sync test out of telemetry and into the gpu tests
    I meant to do this a little while ago, but it's become more urgent due
    to bot failures on this test.
    
    BUG=256848
    
    Review URL: https://codereview.chromium.org/150953002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248287 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: de145ac8f047909e8b90cb0ed3dcf619072eb9b9

---
+++ content/test/gpu/gpu_tests/screenshot_sync.py
@@ -0,0 +1,49 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import os
+
+from telemetry import test
+from telemetry.core import util
+from telemetry.page import page_set
+from telemetry.page import page_test
+
+data_path = os.path.join(
+    util.GetChromiumSrcDir(), 'content', 'test', 'data', 'gpu')
+
+class ScreenshotSyncValidator(page_test.PageTest):
+  def __init__(self):
+    super(ScreenshotSyncValidator, self).__init__('ValidatePage')
+
+  def CustomizeBrowserOptions(self, options):
+    options.AppendExtraBrowserArgs('--enable-gpu-benchmarking')
+
+  def ValidatePage(self, page, tab, results):
+    test_success = tab.EvaluateJavaScript('window.__testSuccess')
+    if not test_success:
+      message = tab.EvaluateJavaScript('window.__testMessage')
+      raise page_test.Failure(message)
+
+class ScreenshotSyncProcess(test.Test):
+  """Tests that screenhots are properly synchronized with the frame one which
+  they were requested"""
+  test = ScreenshotSyncValidator
+
+  def CreatePageSet(self, options):
+    page_set_dict = {
+      'description': 'Test cases for screenshot synchronization',
+      'user_agent_type': 'desktop',
+      'serving_dirs': [''],
+      'pages': [
+        {
+          'name': 'ScreenshotSync',
+          'url': 'file://screenshot_sync.html',
+          'navigate_steps': [
+            { 'action': 'navigate' },
+            { 'action': 'wait',
+              'javascript': 'window.__testComplete' }
+          ]
+        }
+      ]
+    }
+    return page_set.PageSet.FromDict(page_set_dict, data_path)
