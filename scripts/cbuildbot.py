ed0399e616c48e1096b39d6a23da7d6e850aeae9
Gaurav Shah <gauravsh@google.com>
2014-02-03 13:36:22 -0800

    cbuildbot: Fix warning text when doing remote trybot runs on branches
    Use branch name or 'ToT' depending on whether we are using a branched
    manifest.
    
    BUG=none
    TEST=ran locally.
    
    Change-Id: Ibcfc1d3e82ca505e424668cc6e4d3b764c95a9ab
    Reviewed-on: https://chromium-review.googlesource.com/184721
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Tested-by: Gaurav Shah <gauravsh@chromium.org>

--- scripts/cbuildbot.py
+++ scripts/cbuildbot.py
@@ -1286,9 +1286,10 @@ def _FinishParsing(options, args):
       cros_build_lib.Die('Cannot specify both --remote and --local')
 
     if not options.buildbot and not patches:
-      if not cros_build_lib.BooleanPrompt(
-          prompt="No patches were provided; are you sure you want to just "
-          "run a remote build of ToT?", default=False):
+      prompt = ('No patches were provided; are you sure you want to just '
+                'run a remote build of %s?' % (
+                    options.branch if options.branch else 'ToT'))
+      if not cros_build_lib.BooleanPrompt(prompt=prompt, default=False):
         cros_build_lib.Die('Must provide patches when running with --remote.')
 
     # --debug needs to be explicitly passed through for remote invocations.
