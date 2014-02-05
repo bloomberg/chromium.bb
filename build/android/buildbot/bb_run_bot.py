88d234578edc3442d524c9655e91afe2f26821d6
eugenis@chromium.org <eugenis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-05 13:47:04 +0000

    Enable stack tool and ASan symbolization of logcat on Android bots.
    TBR=navabi@chromium.org,craigdh@chromium.org,frankf@chromium.org
    BUG=325685
    
    Review URL: https://codereview.chromium.org/153733003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@248982 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 79d3d8d805ba7b17f71d780e5b039347c0a13417

--- build/android/buildbot/bb_run_bot.py
+++ build/android/buildbot/bb_run_bot.py
@@ -143,7 +143,7 @@ def GetBotStepMap():
       # Other waterfalls
       B('asan-builder-tests', H(compile_step,
                                 extra_gyp='asan=1 component=shared_library'),
-        T(std_tests, ['--asan'])),
+        T(std_tests, ['--asan', '--asan-symbolize'])),
       B('blink-try-builder', H(compile_step)),
       B('chromedriver-fyi-tests-dbg', H(std_test_steps),
         T(['chromedriver'], ['--install=ChromiumTestShell'])),
