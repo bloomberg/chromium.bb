600365fdac64b17938473d35c54a31689d9063de
powei@chromium.org <powei@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-06 18:31:07 +0000

    android: Migrate old content readback to use async readback (and delegated renderer)
    This patch removes the use of a helper context for content readback.  Instead,
    we use the async readback API.
    
    This patch is also setting delegated renderer as the default for android.
    
    android= https://chrome-internal-review.googlesource.com/#/c/153746/
    
    BUG=326363
    TBR=sievers@chromium.org
    NOTRY=true
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=248827
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=249081
    
    Review URL: https://codereview.chromium.org/143803004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249453 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 80ee233f3cb421f76ae537e5e9583f4d911e731c

--- cc/trees/layer_tree_host_impl_unittest.cc
+++ cc/trees/layer_tree_host_impl_unittest.cc
@@ -5453,12 +5453,16 @@ TEST_F(LayerTreeHostImplTest, CreateETC1UIResource) {
 
   EXPECT_EQ(0u, context3d->NumTextures());
 
+  gfx::Size size(4, 4);
+  // SkImageInfo has no support for ETC1.  The |info| below contains the right
+  // total pixel size for the bitmap but not the right height and width.  The
+  // correct width/height are passed directly to UIResourceBitmap.
   SkImageInfo info =
-      SkImageInfo::Make(4, 4, kPMColor_SkColorType, kPremul_SkAlphaType);
+      SkImageInfo::Make(4, 2, kAlpha_8_SkColorType, kPremul_SkAlphaType);
   skia::RefPtr<SkPixelRef> pixel_ref =
       skia::AdoptRef(SkMallocPixelRef::NewAllocate(info, 0, 0));
   pixel_ref->setImmutable();
-  UIResourceBitmap bitmap(pixel_ref);
+  UIResourceBitmap bitmap(pixel_ref, size);
   UIResourceId ui_resource_id = 1;
   host_impl_->CreateUIResource(ui_resource_id, bitmap);
   EXPECT_EQ(1u, context3d->NumTextures());
--- content/public/browser/android/content_view_core.h
+++ content/public/browser/android/content_view_core.h
@@ -12,6 +12,8 @@
 #include "content/common/content_export.h"
 #include "content/public/browser/navigation_controller.h"
 
+class SkBitmap;
+
 namespace cc {
 class Layer;
 }
@@ -48,9 +50,15 @@ class CONTENT_EXPORT ContentViewCore {
   virtual void LoadUrl(NavigationController::LoadURLParams& params) = 0;
   virtual jint GetCurrentRenderProcessId(JNIEnv* env, jobject obj) = 0;
   virtual void ShowPastePopup(int x, int y) = 0;
-  virtual unsigned int GetScaledContentTexture(
+
+  // Request a scaled content readback.  The result is passed through the
+  // callback.  The boolean parameter indicates whether the readback was a
+  // success or not.  The content is passed through the SkBitmap parameter.
+  // |out_size| is returned with the size of the content.
+  virtual void GetScaledContentBitmap(
       float scale,
-      gfx::Size* out_size) = 0;
+      gfx::Size* out_size,
+      const base::Callback<void(bool, const SkBitmap&)>& result_callback) = 0;
   virtual float GetDpiScale() const = 0;
   virtual void RequestContentClipping(const gfx::Rect& clipping,
                                       const gfx::Size& content_size) = 0;
