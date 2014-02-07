7794ffb27663802f0c10fb56c8cc5dc8881e05ac
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 18:49:32 +0000

    Revert of android: Do not call ContentViewCore when destroying RenderWidgetHostViewAndroid (https://codereview.chromium.org/151563005/)
    Reason for revert:
    Breaking Android tests.  If you don't call SetContentViewCore(NULL) we leave a reference to the RWHVA inside WindowAndroid's observers.  Also need to take care of the destructor path in RenderWidgetHostViewAndroid (does that have to avoid a potentially bad ContentViewCore as well?).  ContentViewCore should unset itself from RenderWidgetHostViewAndroid when it's no longer valid to call into it.
    
    Original issue's description:
    > android: Do not call ContentViewCore when destroying RenderWidgetHostViewAndroid
    > 
    > Reverse a change from https://codereview.chromium.org/26753005/ that caused
    > crashes.
    > 
    > BUG=324341
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=249654
    
    TBR=sievers@chromium.org,feng@chromium.org,powei@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=324341
    
    Review URL: https://codereview.chromium.org/154233004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249737 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3f716d79cac239e1fa4eee8ddef9c5939d4608f1

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -573,10 +573,7 @@ void RenderWidgetHostViewAndroid::RenderProcessGone(
 
 void RenderWidgetHostViewAndroid::Destroy() {
   RemoveLayers();
-
-  // No longer safe to call content_view_core_ at this point, so just set it to
-  // null (as oppose to calling SetContentViewCore).
-  content_view_core_ = NULL;
+  SetContentViewCore(NULL);
 
   // The RenderWidgetHost's destruction led here, so don't call it.
   host_ = NULL;
