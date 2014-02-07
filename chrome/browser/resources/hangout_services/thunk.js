d4ca4ae0d3b077825b60e60d71492983eefcdb9a
bemasc@chromium.org <bemasc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-07 21:23:47 +0000

    Clean up JS style and separate unrelated functionality.
    This change is not intended to have any effect on the
    extension's functionality or behavior.
    
    Review URL: https://codereview.chromium.org/149463008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249783 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4259ce953309b6454b572af71626e1bb5a82934d

--- chrome/browser/resources/hangout_services/thunk.js
+++ chrome/browser/resources/hangout_services/thunk.js
@@ -2,140 +2,128 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-chrome.runtime.onStartup.addListener(
-    function() {
-  chrome.alarms.create('hangout_services_fxpre', {'delayInMinutes': 2});
-});
-
-chrome.alarms.onAlarm.addListener(function(alarm) {
-  if (!alarm || alarm.name != 'hangout_services_fxpre')
-    return;
-
-  var e = document.createElement('iframe');
-  e.src = 'https://plus.google.com/hangouts/_/fxpre';
-  document.body.appendChild(e);
-});
-
 chrome.runtime.onMessageExternal.addListener(
     function(message, sender, sendResponse) {
-  function doSendResponse(value, error) {
-    var errorMessage = error || chrome.extension.lastError;
-    sendResponse({'value': value, 'error': errorMessage});
-  }
-
-  function getHost(url) {
-    if (!url)
-      return '';
-    // Use the DOM to parse the URL. Since we don't add the anchor to
-    // the page, this is the only reference to it and it will be
-    // deleted once it's gone out of scope.
-    var a = document.createElement('a');
-    a.href = url;
-    var origin = a.protocol + '//' + a.hostname;
-    if (a.port != '')
-      origin = origin + ':' + a.port;
-    origin = origin + '/';
-    return origin;
-  }
-
-  try {
-    var method = message['method'];
-    var origin = getHost(sender.url);
-    if (method == 'chooseDesktopMedia') {
-      var cancelId;
-      function sendResponseWithCancelId(streamId) {
-        var value = {'cancelId': cancelId,
-                     'streamId': streamId};
-        doSendResponse(value);
+      function doSendResponse(value, error) {
+        var errorMessage = error || chrome.extension.lastError;
+        sendResponse({'value': value, 'error': errorMessage});
       }
-      cancelId = chrome.desktopCapture.chooseDesktopMedia(
-          ['screen', 'window'], sender.tab, sendResponseWithCancelId);
-      return true;
-    } else if (method == 'cancelChooseDesktopMedia') {
-      var cancelId = message['cancelId'];
-      chrome.desktopCapture.cancelChooseDesktopMedia(cancelId);
-      doSendResponse();
-      return false;
-    } else if (method == 'cpu.getInfo') {
-      chrome.system.cpu.getInfo(doSendResponse);
-      return true;
-    } else if (method == 'logging.setMetadata') {
-      var metaData = message['metaData'];
-      chrome.webrtcLoggingPrivate.setMetaData(
-          sender.tab.id, origin, metaData, doSendResponse);
-      return true;
-    } else if (method == 'logging.start') {
-      chrome.webrtcLoggingPrivate.start(sender.tab.id, origin, doSendResponse);
-      return true;
-    } else if (method == 'logging.uploadOnRenderClose') {
-      chrome.webrtcLoggingPrivate.setUploadOnRenderClose(
-          sender.tab.id, origin, true);
-      doSendResponse();
-      return false;
-    } else if (method == 'logging.noUploadOnRenderClose') {
-      chrome.webrtcLoggingPrivate.setUploadOnRenderClose(
-          sender.tab.id, origin, false);
-      doSendResponse();
-      return false;
-    } else if (method == 'logging.stop') {
-      chrome.webrtcLoggingPrivate.stop(sender.tab.id, origin, doSendResponse);
-      return true;
-    } else if (method == 'logging.upload') {
-      chrome.webrtcLoggingPrivate.upload(sender.tab.id, origin, doSendResponse);
-      return true;
-    } else if (method == 'logging.stopAndUpload') {
-      chrome.webrtcLoggingPrivate.stop(
-          sender.tab.id, origin, function() {
-              chrome.webrtcLoggingPrivate.upload(
-                  sender.tab.id, origin, doSendResponse);
+
+      function getHost(url) {
+        if (!url)
+          return '';
+        // Use the DOM to parse the URL. Since we don't add the anchor to
+        // the page, this is the only reference to it and it will be
+        // deleted once it's gone out of scope.
+        var a = document.createElement('a');
+        a.href = url;
+        var origin = a.protocol + '//' + a.hostname;
+        if (a.port != '')
+          origin = origin + ':' + a.port;
+        origin = origin + '/';
+        return origin;
+      }
+
+      try {
+        var method = message['method'];
+        var origin = getHost(sender.url);
+        if (method == 'chooseDesktopMedia') {
+          var cancelId;
+          function sendResponseWithCancelId(streamId) {
+            var value = {'cancelId': cancelId, 'streamId': streamId};
+            doSendResponse(value);
+          }
+          cancelId = chrome.desktopCapture.chooseDesktopMedia(
+              ['screen', 'window'], sender.tab, sendResponseWithCancelId);
+          return true;
+        } else if (method == 'cancelChooseDesktopMedia') {
+          var cancelId = message['cancelId'];
+          chrome.desktopCapture.cancelChooseDesktopMedia(cancelId);
+          doSendResponse();
+          return false;
+        } else if (method == 'cpu.getInfo') {
+          chrome.system.cpu.getInfo(doSendResponse);
+          return true;
+        } else if (method == 'logging.setMetadata') {
+          var metaData = message['metaData'];
+          chrome.webrtcLoggingPrivate.setMetaData(
+              sender.tab.id, origin, metaData, doSendResponse);
+          return true;
+        } else if (method == 'logging.start') {
+          chrome.webrtcLoggingPrivate.start(
+              sender.tab.id, origin, doSendResponse);
+          return true;
+        } else if (method == 'logging.uploadOnRenderClose') {
+          chrome.webrtcLoggingPrivate.setUploadOnRenderClose(
+              sender.tab.id, origin, true);
+          doSendResponse();
+          return false;
+        } else if (method == 'logging.noUploadOnRenderClose') {
+          chrome.webrtcLoggingPrivate.setUploadOnRenderClose(
+              sender.tab.id, origin, false);
+          doSendResponse();
+          return false;
+        } else if (method == 'logging.stop') {
+          chrome.webrtcLoggingPrivate.stop(
+              sender.tab.id, origin, doSendResponse);
+          return true;
+        } else if (method == 'logging.upload') {
+          chrome.webrtcLoggingPrivate.upload(
+              sender.tab.id, origin, doSendResponse);
+          return true;
+        } else if (method == 'logging.stopAndUpload') {
+          chrome.webrtcLoggingPrivate.stop(sender.tab.id, origin, function() {
+            chrome.webrtcLoggingPrivate.upload(
+                sender.tab.id, origin, doSendResponse);
           });
-      return true;
-    } else if (method == 'logging.discard') {
-      chrome.webrtcLoggingPrivate.discard(
-          sender.tab.id, origin, doSendResponse);
-      return true;
-    } else if (method == 'getSinks') {
-      chrome.webrtcAudioPrivate.getSinks(doSendResponse);
-      return true;
-    } else if (method == 'getActiveSink') {
-      chrome.webrtcAudioPrivate.getActiveSink(sender.tab.id, doSendResponse);
-      return true;
-    } else if (method == 'setActiveSink') {
-      var sinkId = message['sinkId'];
-      chrome.webrtcAudioPrivate.setActiveSink(
-          sender.tab.id, sinkId, doSendResponse);
-      return true;
-    } else if (method == 'getAssociatedSink') {
-      var sourceId = message['sourceId'];
-      chrome.webrtcAudioPrivate.getAssociatedSink(
-          origin, sourceId, doSendResponse);
-      return true;
-    } else if (method == 'isExtensionEnabled') {
-      // This method is necessary because there may be more than one
-      // version of this extension, under different extension IDs. By
-      // first calling this method on the extension ID, the client can
-      // check if it's loaded; if it's not, the extension system will
-      // call the callback with no arguments and set
-      // chrome.runtime.lastError.
-      doSendResponse();
-      return false;
-    } else if (method == 'getNaclArchitecture') {
-      chrome.runtime.getPlatformInfo(function(obj) {
-        doSendResponse(obj.nacl_arch);
-      });
-      return true;
+          return true;
+        } else if (method == 'logging.discard') {
+          chrome.webrtcLoggingPrivate.discard(
+              sender.tab.id, origin, doSendResponse);
+          return true;
+        } else if (method == 'getSinks') {
+          chrome.webrtcAudioPrivate.getSinks(doSendResponse);
+          return true;
+        } else if (method == 'getActiveSink') {
+          chrome.webrtcAudioPrivate.getActiveSink(
+              sender.tab.id, doSendResponse);
+          return true;
+        } else if (method == 'setActiveSink') {
+          var sinkId = message['sinkId'];
+          chrome.webrtcAudioPrivate.setActiveSink(
+              sender.tab.id, sinkId, doSendResponse);
+          return true;
+        } else if (method == 'getAssociatedSink') {
+          var sourceId = message['sourceId'];
+          chrome.webrtcAudioPrivate.getAssociatedSink(
+              origin, sourceId, doSendResponse);
+          return true;
+        } else if (method == 'isExtensionEnabled') {
+          // This method is necessary because there may be more than one
+          // version of this extension, under different extension IDs. By
+          // first calling this method on the extension ID, the client can
+          // check if it's loaded; if it's not, the extension system will
+          // call the callback with no arguments and set
+          // chrome.runtime.lastError.
+          doSendResponse();
+          return false;
+        } else if (method == 'getNaclArchitecture') {
+          chrome.runtime.getPlatformInfo(function(obj) {
+            doSendResponse(obj.nacl_arch);
+          });
+          return true;
+        }
+        throw new Error('Unknown method: ' + method);
+      } catch (e) {
+        doSendResponse(null, e.name + ': ' + e.message);
+      }
     }
-    throw new Error('Unknown method: ' + method);
-  } catch (e) {
-    doSendResponse(null, e.name + ': ' + e.message);
-  }
-});
+);
 
 // If Hangouts connects with a port named 'onSinksChangedListener', we
 // will register a listener and send it a message {'eventName':
 // 'onSinksChanged'} whenever the event fires.
-chrome.runtime.onConnectExternal.addListener(
-    function(port) {
+chrome.runtime.onConnectExternal.addListener(function(port) {
   if (port.name == 'onSinksChangedListener') {
     function clientListener() {
       port.postMessage({'eventName': 'onSinksChanged'});
@@ -143,8 +131,8 @@ chrome.runtime.onConnectExternal.addListener(
     chrome.webrtcAudioPrivate.onSinksChanged.addListener(clientListener);
 
     port.onDisconnect(function() {
-        chrome.webrtcAudioPrivate.onSinksChanged.removeListener(
-            clientListener);
-      });
+      chrome.webrtcAudioPrivate.onSinksChanged.removeListener(
+          clientListener);
+    });
   }
 });
