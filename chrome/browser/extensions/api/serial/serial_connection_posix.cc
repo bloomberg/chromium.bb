8f35f9f3d07515fb62a5d545c575de5525885985
rockot@chromium.org <rockot@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-09 01:54:47 +0000

    Revert 249835 "Add fallback support for custom serial baud rates..."
    This has created more problems than it has resolved.
    
    > Add fallback support for custom serial baud rates on OS X.
    > 
    > Many drivers do not support the IOSSIOSPEED ioctl, but do
    > support arbitrary baud rates. This adds a fallback for cases
    > where IOSSIOSPEED fails.
    > 
    > BUG=337482
    > TEST=manual
    > 
    > Review URL: https://codereview.chromium.org/137653005
    
    TBR=rockot@chromium.org
    
    Review URL: https://codereview.chromium.org/137433014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@249955 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 49f1409173c38a9d9c42575aa23226bf85a279e5

--- chrome/browser/extensions/api/serial/serial_connection_posix.cc
+++ chrome/browser/extensions/api/serial/serial_connection_posix.cc
@@ -107,13 +107,7 @@ bool SetCustomBitrate(base::PlatformFile file,
   return ioctl(file, TIOCSSERIAL, &serial) >= 0;
 #elif defined(OS_MACOSX)
   speed_t speed = static_cast<speed_t>(bitrate);
-  if (ioctl(file, IOSSIOSPEED, &speed) >= 0)
-    return true;
-  // Where IOSSIOSPEED is unsupported, attempt to set
-  // the speed directly to the requested value.
-  cfsetispeed(config, speed);
-  cfsetospeed(config, speed);
-  return true;
+  return ioctl(file, IOSSIOSPEED, &speed) != -1;
 #else
   return false;
 #endif
