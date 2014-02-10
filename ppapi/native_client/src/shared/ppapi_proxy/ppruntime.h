976f575c0a798e3b034ea96687faff6832d0e016
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-10 16:56:26 +0000

    NaCl: Remove declaration of IrtInit(), which is no longer defined anywhere
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3186
    TEST=build
    
    Review URL: https://codereview.chromium.org/157803004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250121 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 230ea3b21a8daabde9b2e0dcd93dedb5b5a87003

--- ppapi/native_client/src/shared/ppapi_proxy/ppruntime.h
+++ ppapi/native_client/src/shared/ppapi_proxy/ppruntime.h
@@ -12,11 +12,6 @@
 
 EXTERN_C_BEGIN
 
-// Initialize srpc connection to the browser. Some APIs like manifest file
-// opening do not need full ppapi initialization and so can be used after
-// this function returns.
-int IrtInit(void);
-
 // The entry point for the main thread of the PPAPI plugin process.
 int PpapiPluginMain(void);
 
