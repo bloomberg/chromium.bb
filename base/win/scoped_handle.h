c6d8073c3f44f46f4dce4af652e69d2492fa5b25
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-11 14:07:34 +0000

    Remove obsolete comment referring to Receive() in scoped_handle.h.
    BUG=318531
    TBR=rvargas,cpu
    
    Review URL: https://codereview.chromium.org/159143004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250390 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 78fe8222c6cf520a83e931f4bf8dac4b3e742aaa

--- base/win/scoped_handle.h
+++ base/win/scoped_handle.h
@@ -27,11 +27,9 @@ namespace win {
 
 // Generic wrapper for raw handles that takes care of closing handles
 // automatically. The class interface follows the style of
-// the ScopedStdioHandle class with a few additions:
+// the ScopedStdioHandle class with one addition:
 //   - IsValid() method can tolerate multiple invalid handle values such as NULL
 //     and INVALID_HANDLE_VALUE (-1) for Win32 handles.
-//   - Receive() method allows to receive a handle value from a function that
-//     takes a raw handle pointer only.
 template <class Traits, class Verifier>
 class GenericScopedHandle {
   MOVE_ONLY_TYPE_FOR_CPP_03(GenericScopedHandle, RValue)
