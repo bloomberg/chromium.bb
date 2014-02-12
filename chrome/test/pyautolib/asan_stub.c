ca4c5dca096bd7c16d43145bea659902f2d3d6aa
yunlian@chromium.org <yunlian@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-12 22:33:28 +0000

    cros-asan: Fix crash in chromeos asan bot.
    When upgrade clang to clang 3.5, we need
    __asan_stack_malloc_0 in the asan_stub.
    
    BUG=None
    
    Review URL: https://codereview.chromium.org/151183007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@250806 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 8ed420c98cc6dc1da9d46ca42e47b82843f9de4e

--- chrome/test/pyautolib/asan_stub.c
+++ chrome/test/pyautolib/asan_stub.c
@@ -113,6 +113,7 @@ int __asan_option_detect_stack_use_after_return;
 typedef unsigned long long uptr;
 void __asan_poison_memory_region(void const volatile *addr, uptr size) { }
 void __asan_unpoison_memory_region(void const volatile *addr, uptr size) { }
+uptr __asan_stack_malloc_0(uptr size, uptr real_stack) {}
 uptr __asan_stack_malloc_1(uptr size, uptr real_stack) {}
 uptr __asan_stack_malloc_2(uptr size, uptr real_stack) {}
 uptr __asan_stack_malloc_3(uptr size, uptr real_stack) {}
@@ -123,6 +124,7 @@ uptr __asan_stack_malloc_7(uptr size, uptr real_stack) {}
 uptr __asan_stack_malloc_8(uptr size, uptr real_stack) {}
 uptr __asan_stack_malloc_9(uptr size, uptr real_stack) {}
 uptr __asan_stack_malloc_10(uptr size, uptr real_stack) {}
+void  __asan_stack_free_0(uptr ptr, uptr size, uptr real_stack) {}
 void  __asan_stack_free_1(uptr ptr, uptr size, uptr real_stack) {}
 void  __asan_stack_free_2(uptr ptr, uptr size, uptr real_stack) {}
 void  __asan_stack_free_3(uptr ptr, uptr size, uptr real_stack) {}
