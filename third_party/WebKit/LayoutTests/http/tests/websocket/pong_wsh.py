25b6f2ab8bc7db5998ead971d2c8ca5ffed4e1b3
ricea@chromium.org <ricea@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-12 14:34:28 +0000

    Add a test for the empty ping case.
    Verify that the WebSocket stack handles an empty ping correctly.
    
    BUG=338290
    TEST=run-webkits-tests.sh http/tests/websocket
    
    Review URL: https://codereview.chromium.org/145693004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167009 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d5fbc9683cfd63301845c26dfc86fc769bec573b

--- third_party/WebKit/LayoutTests/http/tests/websocket/pong_wsh.py
+++ third_party/WebKit/LayoutTests/http/tests/websocket/pong_wsh.py
@@ -1,3 +1,4 @@
+import cgi
 from mod_pywebsocket import common
 from mod_pywebsocket import msgutil
 
@@ -7,13 +8,20 @@ def web_socket_do_extra_handshake(request):
 
 
 def web_socket_transfer_data(request):
-    msgutil.send_ping(request, 'Hello, world!')
+    send_payload = ''
+    r = request.ws_resource.split('?', 1)
+    if len(r) == 2:
+        params = cgi.parse_qs(r[1])
+        if 'payload' in params:
+            send_payload = params['payload'][0]
+
+    msgutil.send_ping(request, send_payload)
 
     # We need to use an internal function to detect a pong frame from the client.
-    opcode, payload, final, reserved1, reserved2, reserved3 = request.ws_stream._receive_frame()
-    if opcode == common.OPCODE_PONG and payload == 'Hello, world!' and final and not reserved1 and not reserved2 and not reserved3:
+    opcode, recv_payload, final, reserved1, reserved2, reserved3 = request.ws_stream._receive_frame()
+    if opcode == common.OPCODE_PONG and recv_payload == send_payload and final and not reserved1 and not reserved2 and not reserved3:
         msgutil.send_message(request, 'PASS')
     else:
         msgutil.send_message(request,
                              'FAIL: Received unexpected frame: opcode = %r, payload = %r, final = %r, reserved1 = %r, reserved2 = %r, reserved3 = %r' %
-                             (opcode, payload, final, reserved1, reserved2, reserved3))
+                             (opcode, recv_payload, final, reserved1, reserved2, reserved3))
