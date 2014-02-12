fe27210c6ce8b545fcb7510db697aca939c3b483
xhwang@chromium.org <xhwang@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-12 20:56:18 +0000

    Fix CRLF line endings, tabs and trailing white spaces in testharnessreport.js.
    git-svn-id: svn://svn.chromium.org/blink/trunk@167038 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c21f5cb9e8b4d2c4edb35b9815702861eeb963a5

--- third_party/WebKit/LayoutTests/http/tests/w3c/resources/testharnessreport.js
+++ third_party/WebKit/LayoutTests/http/tests/w3c/resources/testharnessreport.js
@@ -21,14 +21,14 @@ if (self.testRunner) {
 // Function used to convert the test status code into
 // the corresponding string
 function convertResult(resultStatus){
-	if(resultStatus == 0)
-		return("PASS");
-	else if(resultStatus == 1)
-		return("FAIL");
-	else if(resultStatus == 2)
-		return("TIMEOUT");
-	else
-		return("NOTRUN");
+    if(resultStatus == 0)
+        return("PASS");
+    else if(resultStatus == 1)
+        return("FAIL");
+    else if(resultStatus == 2)
+        return("TIMEOUT");
+    else
+        return("NOTRUN");
 }
 
 /* Disable the default output of testharness.js.  The default output formats
@@ -38,42 +38,42 @@ function convertResult(resultStatus){
 */
 setup({"output":false});
 
-/*  Using a callback function, test results will be added to the page in a 
+/*  Using a callback function, test results will be added to the page in a
 *   manner that allows dumpAsText to produce readable test results
 */
 add_completion_callback(function (tests, harness_status){
-	
-	// Create element to hold results
-	var results = document.createElement("pre");
-	
-	// Declare result string
-	var resultStr = "\n";
-	
-	// Check harness_status.  If it is not 0, tests did not
-	// execute correctly, output the error code and message
-	if(harness_status.status != 0){
-		resultStr += "Harness Error. harness_status.status = " + 
-					 harness_status.status +
-					 " , harness_status.message = " +
-					 harness_status.message;
-	}
-	else {
-		// Iterate through tests array and build string that contains
-		// results for all tests
-		for(var i=0; i<tests.length; i++){				 
-			resultStr += convertResult(tests[i].status) + " " + 
-						( (tests[i].name!=null) ? tests[i].name : "" ) + " " +
-						( (tests[i].message!=null) ? tests[i].message : "" ) + 
-						"\n";
-		}			
-	}
 
-	// Set results element's innerHTML to the results string
-	results.innerHTML = resultStr;
+    // Create element to hold results
+    var results = document.createElement("pre");
 
-	// Add results element to document
-	document.body.appendChild(results);
+    // Declare result string
+    var resultStr = "\n";
 
-	if (self.testRunner)
-		testRunner.notifyDone();
+    // Check harness_status.  If it is not 0, tests did not
+    // execute correctly, output the error code and message
+    if(harness_status.status != 0){
+        resultStr += "Harness Error. harness_status.status = " +
+                     harness_status.status +
+                     " , harness_status.message = " +
+                     harness_status.message;
+    }
+    else {
+        // Iterate through tests array and build string that contains
+        // results for all tests
+        for(var i=0; i<tests.length; i++){
+            resultStr += convertResult(tests[i].status) + " " +
+                        ( (tests[i].name!=null) ? tests[i].name : "" ) + " " +
+                        ( (tests[i].message!=null) ? tests[i].message : "" ) +
+                        "\n";
+        }
+    }
+
+    // Set results element's innerHTML to the results string
+    results.innerHTML = resultStr;
+
+    // Add results element to document
+    document.body.appendChild(results);
+
+    if (self.testRunner)
+        testRunner.notifyDone();
 });
