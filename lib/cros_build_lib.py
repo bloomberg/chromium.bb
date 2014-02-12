d385bc64eb0230f1c5c2a950bc72c8285b8ddd43
Mike Frysinger <vapier@chromium.org>
2014-02-11 21:40:19 -0500

    cros_build_lib: new Collection helper
    This is like collections.namedtuple, but produces an object with mutable
    members.
    
    BUG=chromium:209442
    TEST=`./buildbot/run_tests` passes
    
    Change-Id: Ia74a7a119604f9ff64857e3366b612f092a1330f
    Reviewed-on: https://chromium-review.googlesource.com/186050
    Reviewed-by: Mike Frysinger <vapier@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- lib/cros_build_lib.py
+++ lib/cros_build_lib.py
@@ -1592,3 +1592,60 @@ def FromChrootPath(path):
   else:
     return os.path.join(constants.SOURCE_ROOT, constants.DEFAULT_CHROOT_DIR,
                         path.strip(os.path.sep))
+
+
+def Collection(classname, **kwargs):
+  """Create a new class with mutable named members.
+
+  This is like collections.namedtuple, but mutable.  Also similar to the
+  python 3.3 types.SimpleNamespace.
+
+  Example:
+    # Declare default values for this new class.
+    Foo = cros_build_lib.Collection('Foo', a=0, b=10)
+    # Create a new class but set b to 4.
+    foo = Foo(b=4)
+    # Print out a (will be the default 0) and b (will be 4).
+    print('a = %i, b = %i' % (foo.a, foo.b))
+  """
+  def sn_init(self, **kwargs):
+    """The new class's __init__ function."""
+    # First verify the kwargs don't have excess settings.
+    valid_keys = set(self.__slots__[1:])
+    these_keys = set(kwargs.keys())
+    invalid_keys = these_keys - valid_keys
+    if invalid_keys:
+      raise TypeError('invalid keyword arguments for this object: %r' %
+                      invalid_keys)
+
+    # Now initialize this object.
+    for k in valid_keys:
+      setattr(self, k, kwargs.get(k, self.__defaults__[k]))
+
+  def sn_repr(self):
+    """The new class's __repr__ function."""
+    return '%s(%s)' % (classname, ', '.join(
+        '%s=%r' % (k, getattr(self, k)) for k in self.__slots__[1:]))
+
+  # Give the new class a unique name and then generate the code for it.
+  classname = 'Collection_%s' % classname
+  expr = '\n'.join((
+      'class %(classname)s(object):',
+      '  __slots__ = ["__defaults__", "%(slots)s"]',
+      '  __defaults__ = {}',
+  )) % {
+      'classname': classname,
+      'slots': '", "'.join(sorted(str(k) for k in kwargs)),
+  }
+
+  # Create the class in a local namespace as exec requires.
+  namespace = {}
+  exec expr in namespace
+  new_class = namespace[classname]
+
+  # Bind the helpers.
+  new_class.__defaults__ = kwargs.copy()
+  new_class.__init__ = sn_init
+  new_class.__repr__ = sn_repr
+
+  return new_class
