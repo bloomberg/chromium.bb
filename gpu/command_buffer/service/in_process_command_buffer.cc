bloomberg/chromium.bb

commit 187ed84c0fe421ba59fc5dbf4f781141a3631616

[gpu] Remove StreamTexture(Manager) concept from command decoder
Instead add a simple GpuControl interface to attach a GLImage-wrapped
 SurfaceTexture ref to a texture which will provide the
 necessary hooks.

BUG=282700,309162
R=palmer@chromium.org, piman@chromium.org, reveman@chromium.org

Review URL: https://codereview.chromium.org/147463002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@247334 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 7bffe9e1e36489115a3ae4577b2e4f1a1253374b

gpu/command_buffer/service/in_process_command_buffer.cc
@@ -393,11 +393,8 @@ bool InProcessCommandBuffer::InitializeOnGpuThread(
       share_group = new gfx::GLShareGroup;
   }
 
-  StreamTextureManager* stream_texture_manager = NULL;
 #if defined(OS_ANDROID)
-  stream_texture_manager = stream_texture_manager_ =
-      context_group ? context_group->stream_texture_manager_.get()
-                    : new StreamTextureManagerInProcess;
+  stream_texture_manager_.reset(new StreamTextureManagerInProcess);
 #endif
 
   bool bind_generates_resource = false;
@@ -406,7 +403,6 @@ bool InProcessCommandBuffer::InitializeOnGpuThread(
                     : new gles2::ContextGroup(NULL,
                                               NULL,
                                               NULL,
-                                              stream_texture_manager,
                                               NULL,
                                               bind_generates_resource)));
 
@@ -521,6 +517,9 @@ bool InProcessCommandBuffer::DestroyOnGpuThread() {
   }
   context_ = NULL;
   surface_ = NULL;
+#if defined(OS_ANDROID)
+  stream_texture_manager_.reset();
+#endif
 
   g_all_shared_contexts.Pointer()->erase(this);
   return true;
@@ -736,6 +735,29 @@ void InProcessCommandBuffer::SendManagedMemoryStats(
 
 void InProcessCommandBuffer::Echo(const base::Closure& callback) {
   QueueTask(WrapCallback(callback));
+}
+
+uint32 InProcessCommandBuffer::CreateStreamTexture(uint32 texture_id) {
+  base::WaitableEvent completion(true, false);
+  uint32 stream_id = 0;
+  base::Callback<uint32(void)> task =
+      base::Bind(&InProcessCommandBuffer::CreateStreamTextureOnGpuThread,
+                 base::Unretained(this),
+                 texture_id);
+  QueueTask(
+      base::Bind(&RunTaskWithResult<uint32>, task, &stream_id, &completion));
+  completion.Wait();
+  return 0;
+}
+
+uint32 InProcessCommandBuffer::CreateStreamTextureOnGpuThread(
+    uint32 client_texture_id) {
+#if defined(OS_ANDROID)
+  return stream_texture_manager_->CreateStreamTexture(
+      client_texture_id, decoder_->GetContextGroup()->texture_manager());
+#else
+  return 0;
+#endif
 }
 
 gpu::error::Error InProcessCommandBuffer::GetLastError() {

commit 2c2f8a02466dada27c8a03aa727c91c778df1ed1

Fix video in android webview
Fix mistake introduced in r247334. Webview needs more tests.

BUG=

Review URL: https://codereview.chromium.org/162523002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251022 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: aa2c528f56c09e7d5e3cc0923ea246cd8c59514a

gpu/command_buffer/service/in_process_command_buffer.cc
@@ -597,7 +597,7 @@ uint32 InProcessCommandBuffer::CreateStreamTexture(uint32 texture_id) {
   QueueTask(
       base::Bind(&RunTaskWithResult<uint32>, task, &stream_id, &completion));
   completion.Wait();
-  return 0;
+  return stream_id;
 }
 
 uint32 InProcessCommandBuffer::CreateStreamTextureOnGpuThread(