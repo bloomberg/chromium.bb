e85b77e9dd35fc13c882799eb5e71c8654495902
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-14 00:25:18 +0000

    Fixes bug in paint scrollbar corner (again)
    I think this got borked during a merge. Adding back.
    
    TBR as this is same code as you reviewed.
    
    BUG=341787
    TEST=none
    R=sadrul@chromium.org
    TBR=sadrul@chromium.org
    
    Review URL: https://codereview.chromium.org/165473003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251203 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bb4263e7bd0b32306400f6973f3a0b08a1b787ad

--- ui/native_theme/native_theme_aura.cc
+++ ui/native_theme/native_theme_aura.cc
@@ -15,6 +15,7 @@
 #include "ui/gfx/rect.h"
 #include "ui/gfx/size.h"
 #include "ui/gfx/skbitmap_operations.h"
+#include "ui/gfx/skia_util.h"
 #include "ui/native_theme/common_theme.h"
 
 using gfx::NineImagePainter;
@@ -166,7 +167,11 @@ void NativeThemeAura::PaintScrollbarThumb(SkCanvas* sk_canvas,
 void NativeThemeAura::PaintScrollbarCorner(SkCanvas* canvas,
                                            State state,
                                            const gfx::Rect& rect) const {
-  canvas->drawColor(SkColorSetRGB(0xF1, 0xF1, 0xF1), SkXfermode::kSrc_Mode);
+  SkPaint paint;
+  paint.setColor(SkColorSetRGB(0xF1, 0xF1, 0xF1));
+  paint.setStyle(SkPaint::kFill_Style);
+  paint.setXfermodeMode(SkXfermode::kSrc_Mode);
+  canvas->drawIRect(RectToSkIRect(rect), paint);
 }
 
 NineImagePainter* NativeThemeAura::GetOrCreatePainter(
