2203fed2d0a11f5a3a390582c793824a2f7f8982
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-14 19:36:58 +0000

    NaCl: Remove references to a never-finished ARM Thumb sandbox
    I removed the "target_arm_thumb2" flag on the NaCl side, so this
    change is necessary to update nacl_revision in DEPS.
    
    Note that NaCl trusted code can still contain some Thumb code.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2224
    TEST=nacl_integration
    R=dschuff@chromium.org
    
    Review URL: https://codereview.chromium.org/167323002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251395 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b61bf8bc70cbc3380c40aada528ab27caa32812c

--- components/nacl/browser/nacl_browser.cc
+++ components/nacl/browser/nacl_browser.cc
@@ -61,9 +61,6 @@ const base::FilePath::StringType NaClIrtName() {
     irt_name.append(FILE_PATH_LITERAL("x86_32"));
 
 #elif defined(ARCH_CPU_ARMEL)
-  // TODO(mcgrathr): Eventually we'll need to distinguish arm32 vs thumb2.
-  // That may need to be based on the actual nexe rather than a static
-  // choice, which would require substantial refactoring.
   irt_name.append(FILE_PATH_LITERAL("arm"));
 #elif defined(ARCH_CPU_MIPSEL)
   irt_name.append(FILE_PATH_LITERAL("mips32"));
