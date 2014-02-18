2fcdfb1e05ae9fae52147b9ab4b66b79f3a5444f
vani.hegde@samsung.com <vani.hegde@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-18 21:38:48 +0000

    Revert "Change isBlock and isInline functions to check for isElementNode"
    This reverts commit 6415003fe8070545c67d82f88c69512bd38b2f9f.
    isElementNode() check added in isBlock() introduced a crash.
    Hence reverting the same.
    
    BUG=341783
    
    Review URL: https://codereview.chromium.org/169403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167362 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 3635f17d56dc9ce43c5f004e1d46d651f056a8f7

--- third_party/WebKit/Source/core/editing/htmlediting.cpp
+++ third_party/WebKit/Source/core/editing/htmlediting.cpp
@@ -309,12 +309,12 @@ VisiblePosition lastEditablePositionBeforePositionInRoot(const Position& positio
 // Whether or not content before and after this node will collapse onto the same line as it.
 bool isBlock(const Node* node)
 {
-    return node && node->isElementNode() && node->renderer() && !node->renderer()->isInline() && !node->renderer()->isRubyText();
+    return node && node->renderer() && !node->renderer()->isInline() && !node->renderer()->isRubyText();
 }
 
 bool isInline(const Node* node)
 {
-    return node && node->isElementNode() && node->renderer() && node->renderer()->isInline();
+    return node && node->renderer() && node->renderer()->isInline();
 }
 
 // FIXME: Deploy this in all of the places where enclosingBlockFlow/enclosingBlockFlowOrTableElement are used.
@@ -324,7 +324,7 @@ bool isInline(const Node* node)
 Element* enclosingBlock(Node* node, EditingBoundaryCrossingRule rule)
 {
     Node* enclosingNode = enclosingNodeOfType(firstPositionInOrBeforeNode(node), isBlock, rule);
-    return toElement(enclosingNode);
+    return enclosingNode && enclosingNode->isElementNode() ? toElement(enclosingNode) : 0;
 }
 
 TextDirection directionOfEnclosingBlock(const Position& position)
