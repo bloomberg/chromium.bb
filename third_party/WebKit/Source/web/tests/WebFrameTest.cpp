bloomberg/chromium.bb

commit 5f103447cdde0784500ecdabba49f779d8407ed9

Layout viewport constrained elements on frame rect resize
Previously if layoutSize doesn't change during WebWidget::resize(),
fixed-pos elements won't be relayouted, causing incorrect position
of them after visible viewport change.

Call setViewportConstrainedObjectsNeedsLayout() in
FrameView::setFrameRect().

BUG=338569
TEST=WebFrameTest.fixedPosElementInFixedViewport
TBR=aelias (Source/web/tests)

Review URL: https://codereview.chromium.org/139283011

git-svn-id: svn://svn.chromium.org/blink/trunk@167109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 258d062e0551a8ab3ca9f94a38506066a3c03fe2

third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -346,11 +346,13 @@ void FrameView::invalidateRect(const IntRect& rect)
 void FrameView::setFrameRect(const IntRect& newRect)
 {
     IntRect oldRect = frameRect();
-    if (newRect == oldRect)
+    bool widthChanged = oldRect.width() != newRect.width();
+    bool heightChanged = oldRect.height() != newRect.height();
+    if (!widthChanged && !heightChanged)
         return;
 
     // Autosized font sizes depend on the width of the viewing area.
-    if (newRect.width() != oldRect.width()) {
+    if (widthChanged) {
         if (isMainFrame()) {
             Page* page = m_frame->page();
             bool textAutosizingEnabled = m_frame->settings()->textAutosizingEnabled();
@@ -372,6 +374,8 @@ void FrameView::setFrameRect(const IntRect& newRect)
         if (renderView->usesCompositing())
             renderView->compositor()->frameViewDidChangeSize();
     }
+
+    viewportConstrainedVisibleContentRectChanged(widthChanged, heightChanged);
 }
 
 bool FrameView::scheduleAnimation()
@@ -1339,6 +1343,32 @@ LayoutRect FrameView::viewportConstrainedVisibleContentRect() const
     return viewportRect;
 }
 
+void FrameView::viewportConstrainedVisibleContentRectChanged(bool widthChanged, bool heightChanged)
+{
+    // If viewport is not enabled, frameRect change will cause layout size change and then layout.
+    // Otherwise, viewport constrained objects need their layout flags set separately to ensure
+    // they are positioned correctly.
+    if ((m_frame->settings() && !m_frame->settings()->viewportEnabled()) || !hasViewportConstrainedObjects())
+        return;
+
+    ViewportConstrainedObjectSet::const_iterator end = m_viewportConstrainedObjects->end();
+    for (ViewportConstrainedObjectSet::const_iterator it = m_viewportConstrainedObjects->begin(); it != end; ++it) {
+        RenderObject* renderer = *it;
+        RenderStyle* style = renderer->style();
+        if (widthChanged) {
+            if (style->width().isFixed() && (style->left().isAuto() || style->right().isAuto()))
+                renderer->setNeedsPositionedMovementLayout();
+            else
+                renderer->setNeedsLayout();
+        }
+        if (heightChanged) {
+            if (style->height().isFixed() && (style->top().isAuto() || style->bottom().isAuto()))
+                renderer->setNeedsPositionedMovementLayout();
+            else
+                renderer->setNeedsLayout();
+        }
+    }
+}
 
 IntSize FrameView::scrollOffsetForFixedPosition() const
 {
@@ -1596,18 +1626,6 @@ void FrameView::setScrollPositionNonProgrammatically(const IntPoint& scrollPoint
 
     TemporaryChange<bool> changeInProgrammaticScroll(m_inProgrammaticScroll, false);
     notifyScrollPositionChanged(newScrollPosition);
-}
-
-void FrameView::setViewportConstrainedObjectsNeedLayout()
-{
-    if (!hasViewportConstrainedObjects())
-        return;
-
-    ViewportConstrainedObjectSet::const_iterator end = m_viewportConstrainedObjects->end();
-    for (ViewportConstrainedObjectSet::const_iterator it = m_viewportConstrainedObjects->begin(); it != end; ++it) {
-        RenderObject* renderer = *it;
-        renderer->setNeedsLayout();
-    }
 }
 
 IntSize FrameView::layoutSize(IncludeScrollbarsInRect scrollbarInclusion) const

third_party/WebKit/Source/core/frame/FrameView.h
@@ -96,7 +96,6 @@ public:
 
     bool needsLayout() const;
     void setNeedsLayout();
-    void setViewportConstrainedObjectsNeedLayout();
 
     // Methods for getting/setting the size Blink should use to layout the contents.
     IntSize layoutSize(IncludeScrollbarsInRect = ExcludeScrollbars) const;
@@ -162,6 +161,7 @@ public:
     // This is different than visibleContentRect() in that it ignores negative (or overly positive)
     // offsets from rubber-banding, and it takes zooming into account.
     LayoutRect viewportConstrainedVisibleContentRect() const;
+    void viewportConstrainedVisibleContentRectChanged(bool viewportWidthChanged, bool viewportHeightChanged);
 
     AtomicString mediaType() const;
     void setMediaType(const AtomicString&);

third_party/WebKit/Source/core/page/Page.cpp
@@ -345,7 +345,7 @@ void Page::setPageScaleFactor(float scale, const IntPoint& origin)
         m_chrome->client().deviceOrPageScaleFactorChanged();
 
         if (view)
-            view->setViewportConstrainedObjectsNeedLayout();
+            view->viewportConstrainedVisibleContentRectChanged(true, true);
     }
 
     if (view && view->scrollPosition() != origin)

third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -5136,4 +5136,42 @@ TEST_F(WebFrameTest, heightChangeRepaint)
     }
 }
 
+TEST_F(WebFrameTest, fixedPositionInFixedViewport)
+{
+    UseMockScrollbarSettings mockScrollbarSettings;
+    registerMockedHttpURLLoad("fixed-position-in-fixed-viewport.html");
+    FrameTestHelpers::WebViewHelper webViewHelper;
+    webViewHelper.initializeAndLoad(m_baseURL + "fixed-position-in-fixed-viewport.html", true, 0, 0, enableViewportSettings);
+
+    WebView* webView = webViewHelper.webView();
+    webView->resize(WebSize(100, 100));
+    webView->layout();
+
+    Document* document = toWebFrameImpl(webView->mainFrame())->frame()->document();
+    Element* bottomFixed = document->getElementById("bottom-fixed");
+    Element* topBottomFixed = document->getElementById("top-bottom-fixed");
+    Element* rightFixed = document->getElementById("right-fixed");
+    Element* leftRightFixed = document->getElementById("left-right-fixed");
+
+    webView->resize(WebSize(100, 200));
+    webView->layout();
+    EXPECT_EQ(200, bottomFixed->offsetTop() + bottomFixed->offsetHeight());
+    EXPECT_EQ(200, topBottomFixed->offsetHeight());
+
+    webView->settings()->setMainFrameResizesAreOrientationChanges(false);
+    webView->resize(WebSize(200, 200));
+    webView->layout();
+    EXPECT_EQ(200, rightFixed->offsetLeft() + rightFixed->offsetWidth());
+    EXPECT_EQ(200, leftRightFixed->offsetWidth());
+
+    webView->settings()->setMainFrameResizesAreOrientationChanges(true);
+    // Will scale the page by 1.5.
+    webView->resize(WebSize(300, 330));
+    webView->layout();
+    EXPECT_EQ(220, bottomFixed->offsetTop() + bottomFixed->offsetHeight());
+    EXPECT_EQ(220, topBottomFixed->offsetHeight());
+    EXPECT_EQ(200, rightFixed->offsetLeft() + rightFixed->offsetWidth());
+    EXPECT_EQ(200, leftRightFixed->offsetWidth());
+}
+
 } // namespace

third_party/WebKit/Source/web/tests/data/fixed-position-in-fixed-viewport.html
@@ -1,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=400,height=400,initial-scale=1.0">
+  <style>
+    div {
+      position: fixed;
+      background-color: blue;
+    }
+  </style>
+</head>
+
+<body style="width: 500px; height: 500px">
+  <div id="bottom-fixed" style="left: 0px; bottom: 0px; width: 20px; height: 20px"></div>
+  <div id="top-bottom-fixed" style="left: 0px; top: 0px; bottom: 0px; width: 20px"></div>
+  <div id="right-fixed" style="top: 0px; right: 0px; width: 20px; height: 20px"></div>
+  <div id="left-right-fixed" style="top: 0px; left: 0px; right: 0px; height: 20px"></div>
+</body>
+</html>

commit 09621f2957a999caccfeb828daab984badd7971b

Fix FrameView movement issue
Fix bug caused by r167109 that FrameView movements were ignored.

BUG=344482
TEST=WebFrameTest.FrameViewSetFrameRect
R=leviw@chromium.org
TBR=(Source/web/tests)

Review URL: https://codereview.chromium.org/171413003

git-svn-id: svn://svn.chromium.org/blink/trunk@167424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 16eacfc8354d030e5239d54543ca46421557b273

third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -348,13 +348,11 @@ void FrameView::invalidateRect(const IntRect& rect)
 void FrameView::setFrameRect(const IntRect& newRect)
 {
     IntRect oldRect = frameRect();
-    bool widthChanged = oldRect.width() != newRect.width();
-    bool heightChanged = oldRect.height() != newRect.height();
-    if (!widthChanged && !heightChanged)
+    if (newRect == oldRect)
         return;
 
     // Autosized font sizes depend on the width of the viewing area.
-    if (widthChanged) {
+    if (newRect.width() != oldRect.width()) {
         if (isMainFrame()) {
             Page* page = m_frame->page();
             bool textAutosizingEnabled = m_frame->settings()->textAutosizingEnabled();
@@ -377,7 +375,7 @@ void FrameView::setFrameRect(const IntRect& newRect)
             renderView->compositor()->frameViewDidChangeSize();
     }
 
-    viewportConstrainedVisibleContentRectChanged(widthChanged, heightChanged);
+    viewportConstrainedVisibleContentSizeChanged(newRect.width() != oldRect.width(), newRect.height() != oldRect.height());
 }
 
 bool FrameView::scheduleAnimation()
@@ -1346,7 +1344,7 @@ LayoutRect FrameView::viewportConstrainedVisibleContentRect() const
     return viewportRect;
 }
 
-void FrameView::viewportConstrainedVisibleContentRectChanged(bool widthChanged, bool heightChanged)
+void FrameView::viewportConstrainedVisibleContentSizeChanged(bool widthChanged, bool heightChanged)
 {
     // If viewport is not enabled, frameRect change will cause layout size change and then layout.
     // Otherwise, viewport constrained objects need their layout flags set separately to ensure

third_party/WebKit/Source/core/frame/FrameView.h
@@ -160,7 +160,7 @@ public:
     // This is different than visibleContentRect() in that it ignores negative (or overly positive)
     // offsets from rubber-banding, and it takes zooming into account.
     LayoutRect viewportConstrainedVisibleContentRect() const;
-    void viewportConstrainedVisibleContentRectChanged(bool viewportWidthChanged, bool viewportHeightChanged);
+    void viewportConstrainedVisibleContentSizeChanged(bool widthChanged, bool heightChanged);
 
     AtomicString mediaType() const;
     void setMediaType(const AtomicString&);

third_party/WebKit/Source/core/page/Page.cpp
@@ -355,7 +355,7 @@ void Page::setPageScaleFactor(float scale, const IntPoint& origin)
         m_chrome->client().deviceOrPageScaleFactorChanged();
 
         if (view)
-            view->viewportConstrainedVisibleContentRectChanged(true, true);
+            view->viewportConstrainedVisibleContentSizeChanged(true, true);
     }
 
     if (view && view->scrollPosition() != origin)

third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -5128,4 +5128,16 @@ TEST_F(WebFrameTest, fixedPositionInFixedViewport)
     EXPECT_EQ(200, leftRightFixed->offsetWidth());
 }
 
+TEST_F(WebFrameTest, FrameViewSetFrameRect)
+{
+    FrameTestHelpers::WebViewHelper webViewHelper;
+    webViewHelper.initializeAndLoad("about:blank");
+
+    WebCore::FrameView* frameView = webViewHelper.webViewImpl()->mainFrameImpl()->frameView();
+    frameView->setFrameRect(WebCore::IntRect(0, 0, 200, 200));
+    EXPECT_EQ_RECT(WebCore::IntRect(0, 0, 200, 200), frameView->frameRect());
+    frameView->setFrameRect(WebCore::IntRect(100, 100, 200, 200));
+    EXPECT_EQ_RECT(WebCore::IntRect(100, 100, 200, 200), frameView->frameRect());
+}
+
 } // namespace