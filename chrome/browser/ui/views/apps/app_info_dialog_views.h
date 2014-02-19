2057edb3e9b6b731f9accb39e824e92da9de3365
sashab@chromium.org <sashab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-19 06:00:50 +0000

    Extend App Info dialog to include app shortcut and list of permissions
    Added a list of permissions to the app info dialog, as well as the app's
    shortcut icon and some basic app information (name, version and description).
    
    Context menu option to open the dialog is still behind the flag
    --enable-app-list-app-info.
    
    BUG=266739
    
    Review URL: https://codereview.chromium.org/146583010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@251954 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c58f78fe9234e5b233d10ec1d8c0783c40f970fa

--- chrome/browser/ui/views/apps/app_info_dialog_views.h
+++ chrome/browser/ui/views/apps/app_info_dialog_views.h
@@ -6,6 +6,9 @@
 #define CHROME_BROWSER_UI_VIEWS_APPS_APP_INFO_DIALOG_VIEWS_H_
 
 #include "base/compiler_specific.h"
+#include "chrome/browser/shell_integration.h"
+#include "ui/message_center/views/bounded_scroll_view.h"
+#include "ui/views/controls/image_view.h"
 #include "ui/views/window/dialog_delegate.h"
 
 class Profile;
@@ -18,8 +21,22 @@ namespace views {
 class Label;
 }
 
+// A scrollable list of permissions for the given app.
+class PermissionsScrollView : public message_center::BoundedScrollView {
+ public:
+  PermissionsScrollView(int min_height,
+                        int max_height,
+                        const extensions::Extension* app);
+
+ private:
+  virtual ~PermissionsScrollView();
+
+  views::View* inner_scrollable_view;
+};
+
 // View the information about a particular chrome application.
-class AppInfoView : public views::DialogDelegateView {
+class AppInfoView : public views::DialogDelegateView,
+                    public base::SupportsWeakPtr<AppInfoView> {
  public:
   AppInfoView(Profile* profile,
               const extensions::Extension* app,
@@ -42,15 +59,24 @@ class AppInfoView : public views::DialogDelegateView {
   virtual ui::ModalType GetModalType() const OVERRIDE;
   virtual base::string16 GetWindowTitle() const OVERRIDE;
 
+  // Called when the app's icon is loaded.
+  void OnAppImageLoaded(const gfx::Image& image);
+
   // Profile in which the shortcuts will be created.
   Profile* profile_;
 
   // UI elements on the dialog.
   views::Label* app_name_label;
   views::Label* app_description_label;
+  views::Label* app_version_label;
+  views::ImageView* app_icon;
+  views::Label* permission_list_heading;
+  PermissionsScrollView* permissions_scroll_view;
 
   const extensions::Extension* app_;
   base::Closure close_callback_;
+
+  base::WeakPtrFactory<AppInfoView> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AppInfoView);
 };
