34a3b3cc58877995d5ec2eecac78770435b35d22
dfalcantara@chromium.org <dfalcantara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-20 07:57:30 +0000

    Add InstallerDelegate for watching that a package gets installed
    * Class monitors for when the given package has been installed on the device.
    
    * Timeouts added to prevent this from spinning forever.
    
    * Test runs the InstallerDelegate on a separate thread so the Handler events are processed instead of sitting idly.
    
    * An attempt at using BroadcastReceivers was made to catch http://developer.android.com/reference/android/content/Intent.html#ACTION_PACKAGE_ADDED,
    but the broadcast is fired when the package is added and not when the package finishes installing.
    
    * Adds dfalcantara@ to OWNERs for app banners directories
    
    NOTRY=true
    BUG=341556
    TEST=InstallerDelegateTest
    R=yfriedman@chromium.org
    
    Review URL: https://codereview.chromium.org/156013005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252189 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 13d954df9638ae453e2b180e7aa3feffa5da768b

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/banners/InstallerDelegateTest.java
@@ -0,0 +1,150 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.banners;
+
+import android.content.pm.PackageInfo;
+import android.os.HandlerThread;
+import android.test.mock.MockPackageManager;
+
+import org.chromium.chrome.testshell.ChromiumTestShellTestBase;
+import org.chromium.content.browser.test.util.Criteria;
+import org.chromium.content.browser.test.util.CriteriaHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Tests the InstallerDelegate to make sure that it functions correctly and responds to changes
+ * in the PackageManager.
+ */
+public class InstallerDelegateTest extends ChromiumTestShellTestBase
+        implements InstallerDelegate.Observer{
+    private static final String MOCK_PACKAGE_NAME = "mock.package.name";
+
+    /**
+     * Returns a mocked set of installed packages.
+     */
+    public static class TestPackageManager extends MockPackageManager {
+        public boolean isInstalled = false;
+
+        @Override
+        public List<PackageInfo> getInstalledPackages(int flags) {
+            List<PackageInfo> packages = new ArrayList<PackageInfo>();
+
+            if (isInstalled) {
+                PackageInfo info = new PackageInfo();
+                info.packageName = MOCK_PACKAGE_NAME;
+                packages.add(info);
+            }
+
+            return packages;
+        }
+    }
+
+    private TestPackageManager mPackageManager;
+    private InstallerDelegate mTestDelegate;
+    private HandlerThread mThread;
+
+    // Variables for tracking the result.
+    private boolean mResultFinished;
+    private InstallerDelegate mResultDelegate;
+    private boolean mResultSuccess;
+
+    @Override
+    public void onInstallFinished(InstallerDelegate delegate, boolean success) {
+        mResultDelegate = delegate;
+        mResultSuccess = success;
+        mResultFinished = true;
+    }
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+
+        mPackageManager = new TestPackageManager();
+
+        // Create a thread for the InstallerDelegate to run on.  We need this thread because the
+        // InstallerDelegate's handler fails to be processed otherwise.
+        mThread = new HandlerThread("InstallerDelegateTest thread");
+        mThread.start();
+        mTestDelegate = new InstallerDelegate(
+                mThread.getLooper(), mPackageManager, this, MOCK_PACKAGE_NAME);
+
+        // Clear out the results from last time.
+        mResultDelegate = null;
+        mResultSuccess = false;
+        mResultFinished = false;
+    }
+
+    @Override
+    public void tearDown() throws Exception {
+        mThread.quit();
+        super.tearDown();
+    }
+
+    private void startMonitoring() throws InterruptedException {
+        mTestDelegate.start();
+
+        // Wait until we know that the Thread is running the InstallerDelegate task.
+        assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
+            @Override
+            public boolean isSatisfied() {
+                return mTestDelegate.isRunning();
+            }
+        }));
+    }
+
+    private void checkResults(boolean expectedResult) throws InterruptedException {
+        assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
+            @Override
+            public boolean isSatisfied() {
+                return !mTestDelegate.isRunning() && mResultFinished;
+            }
+        }));
+
+        assertEquals(expectedResult, mResultSuccess);
+        assertEquals(mTestDelegate, mResultDelegate);
+    }
+
+    /**
+     * Tests what happens when the InstallerDelegate detects that the package has successfully
+     * been installed.
+     */
+    public void testInstallSuccessful() throws InterruptedException {
+        mTestDelegate.setTimingForTests(1, 5000);
+        startMonitoring();
+
+        assertFalse(mResultSuccess);
+        assertNull(mResultDelegate);
+        assertFalse(mResultFinished);
+
+        mPackageManager.isInstalled = true;
+        checkResults(true);
+    }
+
+    /**
+     * Tests what happens when the InstallerDelegate task is canceled.
+     */
+    public void testInstallWaitUntilCancel() throws InterruptedException {
+        mTestDelegate.setTimingForTests(1, 5000);
+        startMonitoring();
+
+        assertFalse(mResultSuccess);
+        assertNull(mResultDelegate);
+        assertFalse(mResultFinished);
+
+        mTestDelegate.cancel();
+        checkResults(false);
+    }
+
+    /**
+     * Tests what happens when the InstallerDelegate times out.
+     */
+    public void testInstallTimeout() throws InterruptedException {
+        mTestDelegate.setTimingForTests(1, 50);
+        startMonitoring();
+        checkResults(false);
+    }
+}
