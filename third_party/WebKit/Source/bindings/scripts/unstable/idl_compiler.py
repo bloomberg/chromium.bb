2652ab6e0fc0aecad048b764e90c000aa4f56c8f
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-20 06:53:45 +0000

    Remove JSON and Pickle from idl_compiler.py
    No longer needed, as .h/.cpp output identical.
    (JSON and pickle were to check that parse tree was the same with Perl.)
    
    BUG=345137
    TBR=haraken
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/173083004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167451 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c2a120378d225601e0c65cd6efebab39400288e7

--- third_party/WebKit/Source/bindings/scripts/unstable/idl_compiler.py
+++ third_party/WebKit/Source/bindings/scripts/unstable/idl_compiler.py
@@ -46,10 +46,6 @@ import idl_reader
 
 def parse_options():
     parser = optparse.OptionParser()
-    # FIXME: The --dump-json-and-pickle option is only for debugging and will
-    # be removed once we complete migrating all IDL files from the Perl flow to
-    # the Python flow.
-    parser.add_option('--dump-json-and-pickle', action='store_true', default=False)
     parser.add_option('--idl-attributes-file')
     parser.add_option('--output-directory')
     parser.add_option('--interfaces-info-file')
@@ -62,25 +58,12 @@ def parse_options():
         parser.error('Must specify output directory using --output-directory.')
     if len(args) != 1:
         parser.error('Must specify exactly 1 input file as argument, but %d given.' % len(args))
-    options.idl_filename = os.path.realpath(args[0])
-    return options
-
-
-def write_json_and_pickle(definitions, interface_name, output_directory):
-    json_string = definitions.to_json()
-    json_basename = interface_name + '.json'
-    json_filename = os.path.join(output_directory, json_basename)
-    with open(json_filename, 'w') as json_file:
-        json_file.write(json_string)
-    pickle_basename = interface_name + '.pkl'
-    pickle_filename = os.path.join(output_directory, pickle_basename)
-    with open(pickle_filename, 'wb') as pickle_file:
-        pickle.dump(definitions, pickle_file)
+    idl_filename = os.path.realpath(args[0])
+    return options, idl_filename
 
 
 def main():
-    options = parse_options()
-    idl_filename = options.idl_filename
+    options, idl_filename = parse_options()
     basename = os.path.basename(idl_filename)
     interface_name, _ = os.path.splitext(basename)
     output_directory = options.output_directory
@@ -94,10 +77,7 @@ def main():
 
     reader = idl_reader.IdlReader(interfaces_info, options.idl_attributes_file, output_directory)
     definitions = reader.read_idl_definitions(idl_filename)
-    if options.dump_json_and_pickle:
-        write_json_and_pickle(definitions, interface_name, output_directory)
-        return
-    code_generator_v8.write_header_and_cpp(definitions, interface_name, interfaces_info, options.output_directory)
+    code_generator_v8.write_header_and_cpp(definitions, interface_name, interfaces_info, output_directory)
 
 
 if __name__ == '__main__':
