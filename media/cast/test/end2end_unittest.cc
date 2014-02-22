ac633bf3d849b81f4c30c5c9721c9ac1d1348978
hubbe@chromium.org <hubbe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-22 07:50:57 +0000

    Cast: Re-enable End2EndTest.StartSenderBeforeReceiver
    I was not able to re-produce the non-determism.
    
    BUG=314233
    
    Review URL: https://codereview.chromium.org/175213004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252761 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7111bbf08c97674f0959d642d287180af83e922c

--- media/cast/test/end2end_unittest.cc
+++ media/cast/test/end2end_unittest.cc
@@ -746,10 +746,7 @@ TEST_F(End2EndTest, MAYBE_LoopNoLossOpus) {
 }
 
 // This tests start sending audio and video before the receiver is ready.
-//
-// TODO(miu): Test disabled because of non-determinism.
-// http://crbug.com/314233
-TEST_F(End2EndTest, DISABLED_StartSenderBeforeReceiver) {
+TEST_F(End2EndTest, StartSenderBeforeReceiver) {
   SetupConfig(transport::kOpus, kDefaultAudioSamplingRate, false, 1);
   Create();
 
