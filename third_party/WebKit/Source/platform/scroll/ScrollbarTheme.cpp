9801c050963c8ada3736cd5898db4b08541158c0
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-22 02:27:44 +0000

    Remove no-longer-necessary platform/default stub for WebThemeEngine.h.
    Apparently I missed this when removing the others in r167145.
    
    R=jamesr@chromium.org
    BUG=327471
    
    Review URL: https://codereview.chromium.org/166273025
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@167626 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 15fbe4ac6a78e3175345d9ed01978b1caf8f7162

--- third_party/WebKit/Source/platform/scroll/ScrollbarTheme.cpp
+++ third_party/WebKit/Source/platform/scroll/ScrollbarTheme.cpp
@@ -34,7 +34,7 @@
 #if !OS(MACOSX)
 #include "public/platform/Platform.h"
 #include "public/platform/WebRect.h"
-#include "public/platform/default/WebThemeEngine.h"
+#include "public/platform/WebThemeEngine.h"
 #endif
 
 namespace WebCore {
