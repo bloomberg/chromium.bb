f0dd733b9b640da07afa88b868535418b440fbb0
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-24 21:27:08 +0000

    Revert 252920 "Update shell_aura.cc to get aura::Window for disp..."
    Experimental revert.  It seems that
    PPAPINaClGLibcTest.URLLoader1
    is failing more often than it used to after sometime around this change.
    
    This is the only change that seems plausible, especially given the error 
    message at the beginning of the test:
    PPAPINaClGLibcTest.URLLoader1 (run #1):
    [ RUN      ] PPAPINaClGLibcTest.URLLoader1
    [2776:2884:0224/095958:3880400:WARNING:chrome_browser_main_win.cc(321)] Command line too long for RegisterApplicationRestart
    [3500:3312:0224/100000:ERROR:singleton_hwnd.cc(43)] Cannot create windows on non-UI thread!
    HTTP server started on 127.0.0.1:51814...
    sending server_data: {"host": "127.0.0.1", "port": 51814} (36 bytes)
    ...
    
    garykac will send out more details about our investigation of this issue.
    
    Crossing my fingers.
    
    > Update shell_aura.cc to get aura::Window for dispatcher.
    > 
    > This change is related to CL: https://codereview.chromium.org/172743002 where
    > RootWindow was renamed to WindowEventDispatcher.
    > 
    > This fixes the Ozone build which uses shell_aura.cc
    > 
    > TBR=ben@chromium.org
    > NOTRY=true
    > 
    > Review URL: https://codereview.chromium.org/177713003
    
    TBR=dnicoara@chromium.org
    
    Review URL: https://codereview.chromium.org/178553004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@252976 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 27030f53863fe60cbf21ed469f6c538edc06cd2e

--- content/shell/browser/shell_aura.cc
+++ content/shell/browser/shell_aura.cc
@@ -51,7 +51,7 @@ void Shell::PlatformCreateWindow(int width, int height) {
 void Shell::PlatformSetContents() {
   CHECK(platform_);
   aura::Window* content = web_contents_->GetView()->GetNativeView();
-  aura::Window* parent = platform_->dispatcher()->window();
+  aura::Window* parent = platform_->window()->window();
   if (parent->Contains(content))
     return;
   parent->AddChild(content);
