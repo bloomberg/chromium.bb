2ef567f8ae0e0149f9f072e3261f4b988df47448
wfh@chromium.org <wfh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-24 22:08:12 +0000

    Fix copy-installer.bat to work with x64 output directories.
    BUG=None
    R=grt@chromium.org
    
    Review URL: https://codereview.chromium.org/147583004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253005 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3f990bae168dc4fc433ee50b97a08174fcd1fe82

--- tools/win/copy-installer.bat
+++ tools/win/copy-installer.bat
@@ -12,12 +12,12 @@ REM   \\build.share\<path_to_checkout>\src\tools\win\copy-installer.bat
 REM
 REM By default, the script will copy the Debug build in the tree, falling back
 REM to the Release build if one is not found.  Similarly, the ninja output
-REM directory is preferred over the devenv output directory.  Specify
-REM "out|build" and/or "Debug|Release" (case matters) on the command line in
-REM any order to influence selection.  The defaults for location and build type
-REM can also be overridden in a given build tree by creating a
-REM "copy-installer.cfg" file alongside the .gclient file that sets the OUTPUT
-REM and/or BUILDTYPE variables.
+REM directory is preferred over the devenv output directory.  The x86 build is
+REM preferred over the x64 build.  Specify any of "out|build", "Debug|Release"
+REM (case matters), or "x64" on the command line in any order to influence
+REM selection.  The defaults for location and build type can also be overridden
+REM in a given build tree by creating a "copy-installer.cfg" file alongside the
+REM .gclient file that sets any of OUTPUT, BUILDTYPE, or ARCH variables.
 REM
 REM Install Robocopy for superior performance on Windows XP if desired (it is
 REM present by default on Vista+).
@@ -31,22 +31,23 @@ SET FROM=%RET%
 REM Read local configuration (set OUTPUT and BUILDTYPE there).
 IF EXIST "%FROM%\..\copy-installer.cfg" CALL "%FROM%\..\copy-installer.cfg"
 
-REM Read OUTPUT and/or BUILDTYPE from command line.
+REM Read any of OUTPUT, BUILDTYPE, or ARCH from command line.
 FOR %%a IN (%1 %2) do (
 IF "%%a"=="out" SET OUTPUT=out
 IF "%%a"=="build" SET OUTPUT=build
 IF "%%a"=="Debug" SET BUILDTYPE=Debug
 IF "%%a"=="Release" SET BUILDTYPE=Release
+IF "%%s"=="x64" SET ARCH=_x64
 )
 
 CALL :_find_build
-IF "%OUTPUT%%BUILDTYPE%"=="" (
+IF "%OUTPUT%%BUILDTYPE%%ARCH%"=="" (
 ECHO No build found to copy.
 EXIT 1
 )
 
-SET FROM=%FROM%\%OUTPUT%\%BUILDTYPE%
-SET TO=\%OUTPUT%\%BUILDTYPE%
+SET FROM=%FROM%\%OUTPUT%\%BUILDTYPE%%ARCH%
+SET TO=\%OUTPUT%\%BUILDTYPE%%ARCH%
 
 REM Figure out what files to copy based on the component type (shared/static).
 IF EXIST "%FROM%\base.dll" (
@@ -111,6 +112,12 @@ FOR %%f IN (%BUILDTYPES%) DO (
 IF EXIST "%FROM%\%%o\%%f\mini_installer.exe" (
 SET OUTPUT=%%o
 SET BUILDTYPE=%%f
+GOTO :EOF
+)
+IF EXIST "%FROM%\%%o\%%f_x64\mini_installer.exe" (
+SET OUTPUT=%%o
+SET BUILDTYPE=%%f
+SET ARCH=_x64
 GOTO :EOF
 )
 )
