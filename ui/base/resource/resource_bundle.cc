59c95b074fbec41175a4686a4088303c39078714
tnagel@chromium.org <tnagel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-25 14:01:14 +0000

    Fix IWYU in resource_bundle.cc
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/176343002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253156 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2026979d26dea341154a8396eb793bacb055d6c4

--- ui/base/resource/resource_bundle.cc
+++ ui/base/resource/resource_bundle.cc
@@ -8,6 +8,7 @@
 
 #include "base/command_line.h"
 #include "base/file_util.h"
+#include "base/files/file.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/metrics/histogram.h"
