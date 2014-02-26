a7b284d304ffaed742d3c7f668627caebc1e7745
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 19:18:10 +0000

    Use mklink instead of fsutil to create hardlink
    fsutil requires elevation, so fails whenever building from a non-
    elevated command prompt, falling back to the copy. mklink does not
    need elevation, so use that instead.
    
    This will fail (and fall back to copy) if building on XP, but I
    assume many other things would fail there too.
    
    R=cpu@chromium.org, gab@chromium.org, zturner@chromium.org
    BUG=346839
    
    Review URL: https://codereview.chromium.org/179793006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253542 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f6179f7cf4f9ec507f0ba55deab6c34d94d183d8

--- chrome/tools/build/win/hardlink_failsafe.bat
+++ chrome/tools/build/win/hardlink_failsafe.bat
@@ -5,8 +5,9 @@
 :: Delete it if it existed
 if exist "%2" del "%2"
 
-:: Try to create a hardlink (argument are in reverse order). Hide errors if they occur, we have a fallback.
-fsutil hardlink create "%2" "%1" > nul
+:: Try to create a hardlink (argument are in reverse order). Hide errors if
+:: they occur, we have a fallback.
+mklink /h "%2" "%1" > nul
 
 :: If it failed, copy it instead. Don't hide errors if it fails.
 if errorlevel 1 copy "%1" "%2"
