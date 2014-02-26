28974af75944364d9696ebc824973723b35030aa
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-26 04:07:29 +0000

    Add some missing setlocals from various .bat files
    Shell PATH was growing quite long in some cases.
    
    R=gab@chromium.org, iannucci@chromium.org, johnw@chromium.org, maruel@chromium.org
    
    Review URL: https://codereview.chromium.org/180303003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@253347 0039d316-1c4b-4281-b951-d872f2087c98

--- gn.bat
+++ gn.bat
@@ -2,6 +2,7 @@
 :: Copyright 2013 The Chromium Authors. All rights reserved.
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
+setlocal
 
 :: This is required with cygwin only.
 PATH=%~dp0;%PATH%
