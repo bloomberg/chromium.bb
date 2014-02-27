d613160082857d32722205a7b3e28fa88aa680dc
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 13:38:28 +0000

    [Files.app Gallery] Prevent the placeholder from being visible in the message
    BUG=344269
    TEST=manually tested
    
    Review URL: https://codereview.chromium.org/182543004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253783 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e81800f265be518e3121c253079b9b276303c665

--- chrome/browser/resources/file_manager/foreground/js/photo/gallery.js
+++ chrome/browser/resources/file_manager/foreground/js/photo/gallery.js
@@ -655,7 +655,7 @@ Gallery.prototype.updateSelectionAndState_ = function() {
       // be recorded in the app state and the relaunch will just open the
       // gallery in the curDirEntry directory.
       window.top.document.title = this.context_.curDirEntry.name;
-      displayName = str('GALLERY_ITEMS_SELECTED', numSelectedItems);
+      displayName = strf('GALLERY_ITEMS_SELECTED', numSelectedItems);
     }
   }
 
