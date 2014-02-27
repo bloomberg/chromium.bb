d04c2aa90e0f267b05031e524eaa977eb0449a36
vangelis@chromium.org <vangelis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 22:47:48 +0000

    Disable accelerated video decode for switchable AMD gpus
    BUG=298968
    
    Review URL: https://codereview.chromium.org/183543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253954 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d5cf0c6131c4338ff85acc5fcef275f90db5dd0c

--- gpu/config/software_rendering_list_json.cc
+++ gpu/config/software_rendering_list_json.cc
@@ -18,7 +18,7 @@ const char kSoftwareRenderingListJson[] = LONG_STRING_CONST(
 {
   "name": "software rendering list",
   // Please update the version number whenever you change this file.
-  "version": "7.0",
+  "version": "7.1",
   "entries": [
     {
       "id": 1,
@@ -1020,7 +1020,20 @@ LONG_STRING_CONST(
       "features": [
         "accelerated_video_decode"
       ]
+    },
+    {
+      "id": 92,
+      "description": "Accelerated video decode does not work with the discrete GPU on AMD switchables",
+      "cr_bugs": [298968],
+      "os": {
+        "type": "win"
+      },
+      "multi_gpu_style": "amd_switchable",
+      "features": [
+        "accelerated_video_decode"
+      ]
     }
+
   ]
 }
 
