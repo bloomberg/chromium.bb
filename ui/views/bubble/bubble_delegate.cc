f6bd0008258803ed1d7d084a521c27232b4dd30c
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-02-27 13:39:22 +0000

    Disable Linux Aura offscreen bubble adjustments.
    This avoids bubbles being flipped and clipped.
    Set BubbleDelegateView::adjust_if_offscreen_ to false.
    (mimic Windows non-Aero/Glass logic, same defect)
    See before/after pics at http://crbug.com/347113#c4
    
    BUG=347113
    R=sky@chromium.org
    TEST=Linux Aura bubbles do not mirror/flip when anchored near a window edge (only to get clipped by a parent window edge on the opposite side). No changes to ChromeOS, which supports bubbles extending beyond the parent window bounds.
    
    Review URL: https://codereview.chromium.org/182343003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@253784 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 71bf02610623a157a1c2e523f27fbacaa3d59df0

--- ui/views/bubble/bubble_delegate.cc
+++ ui/views/bubble/bubble_delegate.cc
@@ -107,6 +107,9 @@ Widget* BubbleDelegateView::CreateBubble(BubbleDelegateView* bubble_delegate) {
   // the parent frame and let DWM handle compositing.  If not, then we don't
   // want to allow the bubble to extend the frame because it will be clipped.
   bubble_delegate->set_adjust_if_offscreen(ui::win::IsAeroGlassEnabled());
+#elif defined(OS_LINUX) && !defined(OS_CHROMEOS)
+  // Linux clips bubble windows that extend outside their parent window bounds.
+  bubble_delegate->set_adjust_if_offscreen(false);
 #endif
 
   bubble_delegate->SizeToContents();
