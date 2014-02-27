e4d3e893b4acad38f3cc90d444afcd081ba14aac
scottmg@chromium.org <scottmg@chromium.org@78cadc50-ecff-11dd-a971-7dbc132099af>
2014-02-27 21:55:20 +0000

    Clean up the filters in the MSVS generator, for VS >= 2010
    Addendum to r1804. The fix in that revision only works
    for VS 2008; for 2010 and later we generate one filter
    per source file, so in order to clean these up, we must
    prune sibling filters as long as they all have the same
    name.
    
    Patch by stk.haller@googlemail.com.
    
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/171713013
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1859 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/generator/msvs.py
+++ pylib/gyp/generator/msvs.py
@@ -1479,8 +1479,14 @@ def _AdjustSourcesAndConvertToFilterHierarchy(
 
   # Prune filters with a single child to flatten ugly directory structures
   # such as ../../src/modules/module1 etc.
-  while len(sources) == 1 and isinstance(sources[0], MSVSProject.Filter):
-    sources = sources[0].contents
+  if version.UsesVcxproj():
+    while all([isinstance(s, MSVSProject.Filter) for s in sources]) \
+        and len(set([s.name for s in sources])) == 1:
+      assert all([len(s.contents) == 1 for s in sources])
+      sources = [s.contents[0] for s in sources]
+  else:
+    while len(sources) == 1 and isinstance(sources[0], MSVSProject.Filter):
+      sources = sources[0].contents
 
   return sources, excluded_sources, excluded_idl
 
