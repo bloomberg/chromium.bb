d8e210d138e07c13aefae2ab2187641e51dd19bf
ed@opera.com <ed@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-02-28 18:21:51 +0000

    Add a UseCounter for SVGElement.className.
    BUG=
    
    Review URL: https://codereview.chromium.org/181783002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168147 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 25abc7a7bd20436da8336fb989c2e43cd9778c4c

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -297,6 +297,7 @@ public:
         HTMLAnchorElementPingAttribute = 276,
         JavascriptExhaustedMemory = 277,
         InsertAdjacentHTML = 278,
+        SVGClassName = 279,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         NumberOfFeatures, // This enum value must be last.
