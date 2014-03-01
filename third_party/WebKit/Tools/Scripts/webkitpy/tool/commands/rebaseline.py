66e8978496e2ed692bea5b8f1283fdde88df92c5
pkasting@chromium.org <pkasting@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-01 03:32:10 +0000

    Lower the simultaneous rebaseline limit from 200 to 50, in hopes of making the bot more likely to succeed at rebaselining.
    BUG=none
    TEST=none
    TBR=ojan
    
    Review URL: https://codereview.chromium.org/185173002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168233 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 864bade1e7e33da0903bd2ef0791d84d9df46f3d

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/rebaseline.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/rebaseline.py
@@ -595,7 +595,7 @@ class AutoRebaseline(AbstractParallelRebaselineCommand):
 
     # Rietveld uploader stinks. Limit the number of rebaselines in a given patch to keep upload from failing.
     # FIXME: http://crbug.com/263676 Obviously we should fix the uploader here.
-    MAX_LINES_TO_REBASELINE = 200
+    MAX_LINES_TO_REBASELINE = 50
 
     def __init__(self):
         super(AutoRebaseline, self).__init__(options=[
