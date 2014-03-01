a59ce8227c952b73c2406eea6907568263fb6ff0
Yu-Ju Hong <yjhong@chromium.org>
2014-02-28 17:45:45 -0800

    remote_access: do not add "sudo" if running as root
    BUG=None
    TEST=None
    
    Change-Id: If4c8c47c160d662e6e73a7a6e8a685b583df9f31
    Reviewed-on: https://chromium-review.googlesource.com/188417
    Reviewed-by: Chris Sosa <sosa@chromium.org>
    Tested-by: Yu-Ju Hong <yjhong@chromium.org>
    Commit-Queue: Yu-Ju Hong <yjhong@chromium.org>

--- lib/remote_access.py
+++ lib/remote_access.py
@@ -575,7 +575,7 @@ class RemoteDevice(object):
         self.CopyToWorkDir(f.name)
         env_file = os.path.join(self.work_dir, os.path.basename(f.name))
         new_cmd = ['source', '%s;' % env_file]
-        if remote_sudo:
+        if remote_sudo and self.username != ROOT_ACCOUNT:
           new_cmd += ['sudo', '-E']
 
         new_cmd += cmd
