f15592cd94fb3772735f53fb9d6a11ff53284aad
newt@chromium.org <newt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 04:19:21 +0000

    Reduce code duplication in ChromeBrowserProvider.
    Also, stop catching Exception, which is bad manners.
    
    Review URL: https://codereview.chromium.org/181003005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254652 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 190a736821bf1497acea49a2ab24a06190d7ebb9

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeBrowserProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeBrowserProvider.java
@@ -46,9 +46,8 @@ import java.util.Vector;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
- * This class provides various information of Chrome, like bookmarks, most
- * visited page etc. It is used to support android.provider.Browser.
- *
+ * This class provides access to user data stored in Chrome, such as bookmarks, most visited pages,
+ * etc. It is used to support android.provider.Browser.
  */
 public class ChromeBrowserProvider extends ContentProvider {
     private static final String TAG = "ChromeBrowserProvider";
@@ -115,7 +114,11 @@ public class ChromeBrowserProvider extends ContentProvider {
     /** The parameter used to specify whether this is a bookmark folder. */
     public static final String BOOKMARK_IS_FOLDER_PARAM = "isFolder";
 
-    /** Invalid id value for the Android ContentProvider API calls. */
+    /**
+     * Invalid ID value for the Android ContentProvider API calls.
+     * The value 0 is intentional: if the ID represents a bookmark node then it's the root node
+     * and not accessible. Otherwise it represents a SQLite row id, so 0 is also invalid.
+     */
     public static final long INVALID_CONTENT_PROVIDER_ID = 0;
 
     // ID used to indicate an invalid id for bookmark nodes.
@@ -303,20 +306,28 @@ public class ChromeBrowserProvider extends ContentProvider {
         return new ChromeBrowserProviderSuggestionsCursor(cursor);
     }
 
+    /**
+     * @see android.content.ContentUris#parseId(Uri)
+     * @return The id from a content URI or -1 if the URI has no id or is malformed.
+     */
+    private static long getContentUriId(Uri uri) {
+        try {
+            return ContentUris.parseId(uri);
+        } catch (UnsupportedOperationException e) {
+            return -1;
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
             String sortOrder) {
         if (!canHandleContentProviderApiCall()) return null;
 
         // Check for invalid id values if provided.
-        // If it represents a bookmark node then it's the root node. Don't provide access here.
-        // Otherwise it represents a SQLite row id, so 0 is invalid.
-        long bookmarkId = INVALID_CONTENT_PROVIDER_ID;
-        try {
-            bookmarkId = ContentUris.parseId(uri);
-            if (bookmarkId == INVALID_CONTENT_PROVIDER_ID) return null;
-        } catch (Exception e) {
-        }
+        long bookmarkId = getContentUriId(uri);
+        if (bookmarkId == INVALID_CONTENT_PROVIDER_ID) return null;
 
         int match = mUriMatcher.match(uri);
         Cursor cursor = null;
@@ -405,14 +416,8 @@ public class ChromeBrowserProvider extends ContentProvider {
         if (!canHandleContentProviderApiCall()) return 0;
 
         // Check for invalid id values if provided.
-        // If it represents a bookmark node then it's the root node and not mutable.
-        // Otherwise it represents a SQLite row id, so 0 is invalid.
-        long bookmarkId = INVALID_CONTENT_PROVIDER_ID;
-        try {
-            bookmarkId = ContentUris.parseId(uri);
-            if (bookmarkId == INVALID_CONTENT_PROVIDER_ID) return 0;
-        } catch (Exception e) {
-        }
+        long bookmarkId = getContentUriId(uri);
+        if (bookmarkId == INVALID_CONTENT_PROVIDER_ID) return 0;
 
         int match = mUriMatcher.match(uri);
         int result;
@@ -460,14 +465,8 @@ public class ChromeBrowserProvider extends ContentProvider {
         if (!canHandleContentProviderApiCall()) return 0;
 
         // Check for invalid id values if provided.
-        // If it represents a bookmark node then it's the root node and not mutable.
-        // Otherwise it represents a SQLite row id, so 0 is invalid.
-        long bookmarkId = INVALID_CONTENT_PROVIDER_ID;
-        try {
-            bookmarkId = ContentUris.parseId(uri);
-            if (bookmarkId == INVALID_CONTENT_PROVIDER_ID) return 0;
-        } catch (Exception e) {
-        }
+        long bookmarkId = getContentUriId(uri);
+        if (bookmarkId == INVALID_CONTENT_PROVIDER_ID) return 0;
 
         int match = mUriMatcher.match(uri);
         int result;
