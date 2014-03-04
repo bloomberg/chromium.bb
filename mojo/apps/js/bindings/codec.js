cbceb1f5fcb8e3293dd953314366ac358f755d86
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-04 16:20:34 +0000

    Makes core mojo binding js files explicitly define their name
    It's rather tricky to infer the name when executing via WebFrame. So,
    rather than attempting that I'm explicitly defining the module
    name.
    
    This also changes the binding generator to include the path too.
    
    BUG=none
    TEST=none
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/183893015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@254765 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a907c5fcc0c91a3723b2e544dbcdb5d6acb8cb0b

--- mojo/apps/js/bindings/codec.js
+++ mojo/apps/js/bindings/codec.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-define(function() {
+define("mojo/apps/js/bindings/codec", function() {
 
   // Memory -------------------------------------------------------------------
 
--- mojo/apps/js/bindings/connector.js
+++ mojo/apps/js/bindings/connector.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-define([
+define("mojo/apps/js/bindings/connector", [
   "mojo/apps/js/bindings/codec",
   "mojo/apps/js/bindings/core",
   "mojo/apps/js/bindings/support",
