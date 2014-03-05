1a3927751ea6243333f05edfc6a630a32c51227d
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-05 15:52:09 +0000

    Fix fast/repaint/block-layout-inline-children-replaced.html
    The test was loading an image but didn't wait for the image
    to fully load to get all the invalidations generated. The
    extra 2 invalidations come from the image (92 x 100)
    and its container (402 x 100).
    
    The fix involves allowing our invalidation harness to
    run asynchronously. While touching the code, removed
    some unneeded window-prefix.
    
    BUG=313447
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/184213004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168491 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: a6c89d72f99587f4fb37e6a633652d4420e574ac

--- third_party/WebKit/LayoutTests/fast/repaint/resources/text-based-repaint.js
+++ third_party/WebKit/LayoutTests/fast/repaint/resources/text-based-repaint.js
@@ -1,34 +1,50 @@
+// Asynchronous tests should manually call finishRepaintTest at the appropriate time.
+window.testIsAsync = false;
+
 function runRepaintTest()
 {
-    if (window.testRunner && window.internals) {
-        if (window.enablePixelTesting)
-            window.testRunner.dumpAsTextWithPixelResults();
-        else
-            window.testRunner.dumpAsText();
-
-        if (document.body)
-            document.body.offsetTop;
-        else if (document.documentElement)
-            document.documentElement.offsetTop;
-
-        window.internals.startTrackingRepaints(document);
-
-        repaintTest();
-
-        // force a style recalc.
-        var dummy = document.body.offsetTop;
-
-        var repaintRects = window.internals.repaintRectsAsText(document);
-
-        window.internals.stopTrackingRepaints(document);
-
-        var pre = document.createElement('pre');
-        document.body.appendChild(pre);
-        pre.textContent += repaintRects;
-
-        if (window.afterTest)
-            window.afterTest();
-    } else {
+    if (!window.testRunner || !window.internals) {
         setTimeout(repaintTest, 100);
+        return;
     }
+
+    if (window.enablePixelTesting)
+        testRunner.dumpAsTextWithPixelResults();
+    else
+        testRunner.dumpAsText();
+
+    if (window.testIsAsync)
+        testRunner.waitUntilDone();
+
+    if (document.body)
+        document.body.offsetTop;
+    else if (document.documentElement)
+        document.documentElement.offsetTop;
+
+    window.internals.startTrackingRepaints(document);
+
+    repaintTest();
+
+    if (!window.testIsAsync)
+        finishRepaintTest();
+}
+
+function finishRepaintTest()
+{
+    // Force a style recalc.
+    var dummy = document.body.offsetTop;
+
+    var repaintRects = window.internals.repaintRectsAsText(document);
+
+    internals.stopTrackingRepaints(document);
+
+    var pre = document.createElement('pre');
+    document.body.appendChild(pre);
+    pre.textContent += repaintRects;
+
+    if (window.afterTest)
+        window.afterTest();
+
+    if (window.testIsAsync)
+        testRunner.notifyDone();
 }
