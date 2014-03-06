b418fa9981ddf6167288e2e6a300b6963363eb4a
k.gurpreet@samsung.com <k.gurpreet@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-06 04:19:40 +0000

    hr color attribute not working.
    The noshade attribute is a boolean attribute and when set on hr element
    it shows a gray color. When there is color attribute the default gray
    color should be replaced by the color mentioned by the color attribute.
    Firefox and IE show the same behaviour but Blink is different. Making
    the behaviour of Blink similiar to Firefox and IE's behaviour.
    
    When the color attribute is present that value is applied and the
    default gray color is ignored. Incase of no color attribute the default
    gray color is applied.
    
    I already landed this patch on Webkit. Just modified the layout test file.
    http://trac.webkit.org/changeset/161334
    
    R=esprehn@chromium.org,eseidel@chromium.org
    BUG=175359
    
    Review URL: https://codereview.chromium.org/162993002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168571 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 98471db9c1cf5c56566a91a3a81c193423582d21

--- third_party/WebKit/Source/core/html/HTMLHRElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLHRElement.cpp
@@ -76,11 +76,13 @@ void HTMLHRElement::collectStyleForPresentationAttribute(const QualifiedName& na
         addHTMLColorToStyle(style, CSSPropertyBorderColor, value);
         addHTMLColorToStyle(style, CSSPropertyBackgroundColor, value);
     } else if (name == noshadeAttr) {
-        addPropertyToPresentationAttributeStyle(style, CSSPropertyBorderStyle, CSSValueSolid);
+        if (!hasAttribute(colorAttr)) {
+            addPropertyToPresentationAttributeStyle(style, CSSPropertyBorderStyle, CSSValueSolid);
 
-        RefPtrWillBeRawPtr<CSSPrimitiveValue> darkGrayValue = cssValuePool().createColorValue(Color::darkGray);
-        style->setProperty(CSSPropertyBorderColor, darkGrayValue);
-        style->setProperty(CSSPropertyBackgroundColor, darkGrayValue);
+            RefPtrWillBeRawPtr<CSSPrimitiveValue> darkGrayValue = cssValuePool().createColorValue(Color::darkGray);
+            style->setProperty(CSSPropertyBorderColor, darkGrayValue);
+            style->setProperty(CSSPropertyBackgroundColor, darkGrayValue);
+        }
     } else if (name == sizeAttr) {
         StringImpl* si = value.impl();
         int size = si->toInt();
