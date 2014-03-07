4bf2f87a8c263f48cc9abe62a69451cc5524b1cc
Dale Curtis <dalecurtis@chromium.org>
2014-03-07 13:56:41 -0800

    Updated documentation to remove SVN references.
    We now have a Git only repository!  \o/
    
    BUG=329002
    TEST=none
    
    Change-Id: Id22bd7ccdc20afba14b8cc970a9e44fc405c184d

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -5,8 +5,7 @@
 """Presubmit script for FFmpeg repository.
 
 Does the following:
-- Displays a message indicating that all changes must be pushed to SVN.
-- Prevents changes inside the git-svn and svn repositories.
+- Warns users that changes must be submitted via Gerrit.
 - Warns users when a change is made without updating the README file.
 """
 
@@ -23,38 +22,19 @@ def Color(color, string):
   return ''.join([color, string, '\033[m'])
 
 
-def _CheckForMainRepository(input_api, output_api):
-  """Ensure no commits are allowed to the Subversion repository."""
-  try:
-    stdout = subprocess.Popen('git config --get remote.origin.url', shell=True,
-                              cwd=input_api.PresubmitLocalPath(),
-                              stdout=subprocess.PIPE).communicate()[0]
-    if 'chromium/third_party/ffmpeg.git' in stdout:
-      return []
-  except Exception, e:
-    # Not everyone has a working Git configuration, so print the exception text
-    # and move on.
-    print e
-
-  return [output_api.PresubmitError('\n'.join([
-      'Commits to the FFmpeg repository must be made through Git. The easiest',
-      'way to do this is to switch to using NewGit:\n',
-      '   http://code.google.com/p/chromium/wiki/UsingNewGit\n',
-      'Alternatively, you can clone the repository directly using:\n',
-      '   git clone http://git.chromium.org/chromium/third_party/ffmpeg.git']))]
-
-
 def _WarnAboutManualSteps():
   """Warn about the manual steps required for rolling the FFmpeg repository."""
-  print Color(BOLD_RED, '[ REMEMBER ]'.center(70, '*'))
+  print Color(BOLD_RED, '[ WARNING ]'.center(70, '*'))
   print Color(
       BOLD_BLUE, '\n'.join([
+          'Changes to FFmpeg must uploaded to Gerrit not Rietveld.  You are',
+          'seeing this message because you''re trying to upload to Rietveld.',
+          'Please see README.chromium for details on using Gerrit.\n',
           'Updates to FFmpeg require the following additional manual steps:',
-          '  - Push of new sources from Git to Subversion. See sync_svn.py.',
           '  - External DEPS roll.',
           '  - If new source files were added, generate_gyp.py must be run.\n',
           'For help, please consult the following resources:',
-          '  - chromium/README.chromium help text.',
+          '  - README.chromium help text.',
           '  - Chromium development list: chromium-dev@chromium.org',
           '  - "Communication" section of http://www.chromium.org/developers']))
   print Color(BOLD_RED, 70 * '*')
@@ -76,6 +56,5 @@ def _WarnIfReadmeIsUnchanged(input_api, output_api):
 def CheckChangeOnUpload(input_api, output_api):
   _WarnAboutManualSteps()
   results = []
-  results.extend(_CheckForMainRepository(input_api, output_api))
   results.extend(_WarnIfReadmeIsUnchanged(input_api, output_api))
   return results
