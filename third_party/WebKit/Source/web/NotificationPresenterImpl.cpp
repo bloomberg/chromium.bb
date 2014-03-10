e62755efcc55b39e8a044ad7ec0b2ba6e3c2a693
peter@chromium.org <peter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-10 15:29:10 +0000

    Remove support for legacy WebKit Notifications
    This patch removes support for the legacy WebKit notifications from Blink,
    per the following Intent to Remove thread:
        https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/8vqyfHa8_dw
    
    The interactive_ui_tests and browser_tests failures have since been
    addressed by CL 187803005. This is a re-land of:
        https://codereview.chromium.org/184733002/
    
    TBR=atwilson@chromium.org, abarth@chromium.org
    BUG=348019
    
    Review URL: https://codereview.chromium.org/188433003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168836 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 96bb9057030aa3f87abdfcf85a0f4d14348740f5

--- third_party/WebKit/Source/web/NotificationPresenterImpl.cpp
+++ third_party/WebKit/Source/web/NotificationPresenterImpl.cpp
@@ -35,34 +35,12 @@
 #include "WebNotificationPermissionCallback.h"
 #include "WebNotificationPresenter.h"
 #include "core/dom/ExecutionContext.h"
-#include "modules/notifications/NotificationBase.h"
+#include "modules/notifications/Notification.h"
 #include "platform/weborigin/SecurityOrigin.h"
 
 using namespace WebCore;
 
 namespace blink {
-
-#if ENABLE(LEGACY_NOTIFICATIONS)
-class VoidCallbackClient : public WebNotificationPermissionCallback {
-public:
-    explicit VoidCallbackClient(PassOwnPtr<VoidCallback> callback)
-        : m_callback(callback)
-    {
-    }
-
-    virtual void permissionRequestComplete()
-    {
-        if (m_callback)
-            m_callback->handleEvent();
-        delete this;
-    }
-
-private:
-    virtual ~VoidCallbackClient() { }
-
-    OwnPtr<VoidCallback> m_callback;
-};
-#endif // ENABLE(LEGACY_NOTIFICATIONS)
 
 class NotificationPermissionCallbackClient : public WebNotificationPermissionCallback {
 public:
@@ -76,7 +54,7 @@ public:
     virtual void permissionRequestComplete()
     {
         if (m_callback)
-            m_callback->handleEvent(NotificationBase::permissionString(static_cast<NotificationClient::Permission>(m_presenter->checkPermission(WebSecurityOrigin(m_securityOrigin)))));
+            m_callback->handleEvent(Notification::permissionString(static_cast<NotificationClient::Permission>(m_presenter->checkPermission(WebSecurityOrigin(m_securityOrigin)))));
         delete this;
     }
 
@@ -98,19 +76,19 @@ bool NotificationPresenterImpl::isInitialized()
     return !!m_presenter;
 }
 
-bool NotificationPresenterImpl::show(NotificationBase* notification)
+bool NotificationPresenterImpl::show(Notification* notification)
 {
-    return m_presenter->show(PassRefPtr<NotificationBase>(notification));
+    return m_presenter->show(PassRefPtr<Notification>(notification));
 }
 
-void NotificationPresenterImpl::cancel(NotificationBase* notification)
+void NotificationPresenterImpl::cancel(Notification* notification)
 {
-    m_presenter->cancel(PassRefPtr<NotificationBase>(notification));
+    m_presenter->cancel(PassRefPtr<Notification>(notification));
 }
 
-void NotificationPresenterImpl::notificationObjectDestroyed(NotificationBase* notification)
+void NotificationPresenterImpl::notificationObjectDestroyed(Notification* notification)
 {
-    m_presenter->objectDestroyed(PassRefPtr<NotificationBase>(notification));
+    m_presenter->objectDestroyed(PassRefPtr<Notification>(notification));
 }
 
 NotificationClient::Permission NotificationPresenterImpl::checkPermission(ExecutionContext* context)
@@ -118,13 +96,6 @@ NotificationClient::Permission NotificationPresenterImpl::checkPermission(Execut
     int result = m_presenter->checkPermission(WebSecurityOrigin(context->securityOrigin()));
     return static_cast<NotificationClient::Permission>(result);
 }
-
-#if ENABLE(LEGACY_NOTIFICATIONS)
-void NotificationPresenterImpl::requestPermission(ExecutionContext* context, PassOwnPtr<VoidCallback> callback)
-{
-    m_presenter->requestPermission(WebSecurityOrigin(context->securityOrigin()), new VoidCallbackClient(callback));
-}
-#endif // ENABLE(LEGACY_NOTIFICATIONS)
 
 void NotificationPresenterImpl::requestPermission(ExecutionContext* context, WTF::PassOwnPtr<NotificationPermissionCallback> callback)
 {
