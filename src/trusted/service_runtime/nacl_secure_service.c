96b1a8569ec76a780f44e389accb99181382aed8
teravest@chromium.org <teravest@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2014-03-10 19:36:35 +0000

    Check NaClAppRuntimeHostSetup result.
    Previously, code was not checking the result of NaClAppRuntimeHostSetup(). This
    change explicitly checks the result of the function and fails if the setup was
    not completed successfully.
    
    This is motivated by wanting to make sure that the change at
      https://codereview.chromium.org/180973008/
    is safe.
    
    In local testing, adding this check is fine, even with moving the initialization of the reverse service as in the linked change.
    
    BUG=http://crbug.com/333950
    
    Review URL: https://codereview.chromium.org/185643007
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@12869 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 4333832ba6da4432eaf846e0bff08b29c722169e

--- src/trusted/service_runtime/nacl_secure_service.c
+++ src/trusted/service_runtime/nacl_secure_service.c
@@ -235,6 +235,7 @@ static void NaClSecureReverseClientCallback(
   struct NaClManifestProxy          *manifest_proxy;
   struct NaClReverseHostInterface   *reverse_host_interface;
   struct NaClReverseQuotaInterface  *reverse_quota_interface;
+  int runtime_host_setup_status;
   UNREFERENCED_PARAMETER(tif);
 
   NaClLog(4,
@@ -270,8 +271,12 @@ static void NaClSecureReverseClientCallback(
     NaClLog(LOG_FATAL, "Reverse interface ctor failed\n");
     goto cleanup_reverse_host_interface;
   }
-  NaClAppRuntimeHostSetup(nap, (struct NaClRuntimeHostInterface *)
-      reverse_host_interface);
+
+  runtime_host_setup_status = NaClAppRuntimeHostSetup(
+      nap, (struct NaClRuntimeHostInterface *) reverse_host_interface);
+  if (runtime_host_setup_status != LOAD_OK) {
+    NaClLog(LOG_FATAL, "NaClAppRuntimeHostSetup failed\n");
+  }
 
   reverse_quota_interface = (struct NaClReverseQuotaInterface *)
     malloc(sizeof *reverse_quota_interface);
