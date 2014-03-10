7d73d5e7413761de515401d43d552b178c5a62e6
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-10 18:33:21 +0000

    Avoid duplicate null check in ReplaceSelectionCommand::pastLastLeaf()
    Avoid duplicate null check in ReplaceSelectionCommand::pastLastLeaf() by
    calling m_lastNodeInserted->lastDescendant() directly instead of
    lastLeafInserted(). This also makes it clearer by we are checking that
    m_lastNodeInserted is not null as it is now explicitly use right after.
    This avoid avoids dereferencing a pointer as we now get a reference
    directly.
    
    R=adamk
    
    Review URL: https://codereview.chromium.org/189913017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@168848 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: e6242ebb9e25af5289f6e086995728a61ad5ced7

--- third_party/WebKit/Source/core/editing/ReplaceSelectionCommand.h
+++ third_party/WebKit/Source/core/editing/ReplaceSelectionCommand.h
@@ -67,7 +67,7 @@ private:
 
         Node* firstNodeInserted() const { return m_firstNodeInserted.get(); }
         Node* lastLeafInserted() const { return m_lastNodeInserted ? &m_lastNodeInserted->lastDescendant() : 0; }
-        Node* pastLastLeaf() const { return m_lastNodeInserted ? NodeTraversal::next(*lastLeafInserted()) : 0; }
+        Node* pastLastLeaf() const { return m_lastNodeInserted ? NodeTraversal::next(m_lastNodeInserted->lastDescendant()) : 0; }
 
     private:
         RefPtr<Node> m_firstNodeInserted;
