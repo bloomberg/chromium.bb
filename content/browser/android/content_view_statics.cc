0a08685e2ec98fc011c39a1f403c20ae72cd6dcb
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 09:13:15 +0000

    Fix bug causing blank pages on resume of Chrome for Android.
    https://codereview.chromium.org/14297022 had a faulty assumption that
    RenderProcessHost would be unique in the face of the underly
    RenderProcess crashing. This is not the case, so listen for the
    appropriate callback on RenderProcessHostObserver until the refactoring
    discussed in the above is complete.
    
    BUG=321610
    
    Review URL: https://codereview.chromium.org/179123002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256153 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 19a76641a994bfbd6d1a32b942f327d5a53ebf5f

--- content/browser/android/content_view_statics.cc
+++ content/browser/android/content_view_statics.cc
@@ -15,6 +15,7 @@
 #include "content/common/android/address_parser.h"
 #include "content/common/view_messages.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/render_process_host_observer.h"
 #include "jni/ContentViewStatics_jni.h"
 
 using base::android::ConvertJavaStringToUTF16;
@@ -33,30 +34,59 @@ namespace {
 // suspended as opposed to all the current renderer processes because the
 // suspend calls are refcounted within WebKitPlatformSupport and it expects a
 // perfectly matched number of resume calls.
-// Note that this vector is only accessed from the UI thread.
-base::LazyInstance<std::vector<int /* process id */> > g_suspended_processes =
-    LAZY_INSTANCE_INITIALIZER;
+// Note that this class is only accessed from the UI thread.
+class SuspendedProcessWatcher : public content::RenderProcessHostObserver {
+ public:
 
-// Suspends timers in all current render processes.
-void SuspendWebKitSharedTimers(std::vector<int>* suspended_processes) {
-  for (content::RenderProcessHost::iterator i(
-          content::RenderProcessHost::AllHostsIterator());
-       !i.IsAtEnd(); i.Advance()) {
-    content::RenderProcessHost* host = i.GetCurrentValue();
-    suspended_processes->push_back(host->GetID());
-    host->Send(new ViewMsg_SetWebKitSharedTimersSuspended(true));
+  // If the process crashes, stop watching the corresponding RenderProcessHost
+  // and ensure it doesn't get over-resumed.
+  virtual void RenderProcessExited(content::RenderProcessHost* host,
+                                   base::ProcessHandle handle,
+                                   base::TerminationStatus status,
+                                   int exit_code) OVERRIDE {
+    std::vector<int>::iterator pos = std::find(suspended_processes_.begin(),
+                                               suspended_processes_.end(),
+                                               host->GetID());
+    DCHECK_NE(pos, suspended_processes_.end());
+    host->RemoveObserver(this);
+    suspended_processes_.erase(pos);
   }
-}
 
-// Resumes timers in processes that were previously stopped.
-void ResumeWebkitSharedTimers(const std::vector<int>& suspended_processes) {
-  for (std::vector<int>::const_iterator it = suspended_processes.begin();
-       it != suspended_processes.end(); ++it) {
-    content::RenderProcessHost* host = content::RenderProcessHost::FromID(*it);
-    if (host)  // The process might have been killed since it was suspended.
+  // Suspends timers in all current render processes.
+  void SuspendWebKitSharedTimers() {
+    DCHECK(suspended_processes_.empty());
+
+    for (content::RenderProcessHost::iterator i(
+            content::RenderProcessHost::AllHostsIterator());
+         !i.IsAtEnd(); i.Advance()) {
+      content::RenderProcessHost* host = i.GetCurrentValue();
+      if (host->HasConnection()) {
+        host->AddObserver(this);
+        host->Send(new ViewMsg_SetWebKitSharedTimersSuspended(true));
+        suspended_processes_.push_back(host->GetID());
+      }
+    }
+  }
+
+  // Resumes timers in processes that were previously stopped.
+  void ResumeWebkitSharedTimers() {
+    for (std::vector<int>::const_iterator it = suspended_processes_.begin();
+         it != suspended_processes_.end(); ++it) {
+      content::RenderProcessHost* host =
+          content::RenderProcessHost::FromID(*it);
+      DCHECK(host);
+      host->RemoveObserver(this);
       host->Send(new ViewMsg_SetWebKitSharedTimersSuspended(false));
+    }
+    suspended_processes_.clear();
   }
-}
+
+ private:
+  std::vector<int /* RenderProcessHost id */> suspended_processes_;
+};
+
+base::LazyInstance<SuspendedProcessWatcher> g_suspended_processes_watcher =
+    LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
 
@@ -72,20 +102,17 @@ static jstring FindAddress(JNIEnv* env, jclass clazz, jstring addr) {
 static void SetWebKitSharedTimersSuspended(JNIEnv* env,
                                            jclass obj,
                                            jboolean suspend) {
-  std::vector<int>* suspended_processes = g_suspended_processes.Pointer();
   if (suspend) {
-    DCHECK(suspended_processes->empty());
-    SuspendWebKitSharedTimers(suspended_processes);
+    g_suspended_processes_watcher.Pointer()->SuspendWebKitSharedTimers();
   } else {
-    ResumeWebkitSharedTimers(*suspended_processes);
-    suspended_processes->clear();
+    g_suspended_processes_watcher.Pointer()->ResumeWebkitSharedTimers();
   }
 }
 
 namespace content {
 
 bool RegisterWebViewStatics(JNIEnv* env) {
-  return RegisterNativesImpl(env) >= 0;
+  return RegisterNativesImpl(env);
 }
 
 }  // namespace content
