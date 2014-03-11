0642818d0d707dc63e1f9a5b2e601a638d4aa33a
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 10:16:48 +0000

    Make ant failures easier to debug
    Normally, ant commands (even when successful) have lots of useless
    output. So, for most of our ant commands we pass the -quiet flag. This
    is much better for successful builds but it makes debugging failures
    more difficult (though even without -quiet ant failures are difficult to
    diagnose without -verbose.
    
    So, the only way that ant failures are easy to debug is if the -verbose
    flag is passed to ant. This flag is much too noisy to always pass. This
    change makes it so that if a call to ant fails, we repeat that call with
    the -verbose flag (and without -quiet) so that the output that the
    developer sees is most useful.
    
    Review URL: https://codereview.chromium.org/193283002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256179 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 05b8c6b14b4a700efacb6d55017702f178d56720

--- build/android/gyp/ant.py
+++ build/android/gyp/ant.py
@@ -10,6 +10,9 @@ Ant build scripts output "BUILD SUCCESSFUL" and build timing at the end of
 every build. In the Android build, this just adds a lot of useless noise to the
 build output. This script forwards its arguments to ant, and prints Ant's
 output up until the BUILD SUCCESSFUL line.
+
+Also, when a command fails, this script will re-run that ant command with the
+'-verbose' argument so that the failure is easier to debug.
 """
 
 import sys
@@ -20,13 +23,25 @@ from util import build_utils
 
 def main(argv):
   try:
-    stdout = build_utils.CheckOutput(['ant'] + argv[1:])
+    args = argv[1:]
+    stdout = build_utils.CheckOutput(['ant'] + args)
   except build_utils.CalledProcessError as e:
-    traceback.print_exc()
-    if '-quiet' in e.args:
-      sys.stderr.write('Tip: run the ant command above without the -quiet flag '
-          'to see more details on the error\n')
-    sys.exit(1)
+    # It is very difficult to diagnose ant failures without the '-verbose'
+    # argument. So, when an ant command fails, re-run it with '-verbose' so that
+    # the cause of the failure is easier to identify.
+    verbose_args = ['-verbose'] + [a for a in argv[1:] if a != '-quiet']
+    try:
+      stdout = build_utils.CheckOutput(['ant'] + verbose_args)
+    except build_utils.CalledProcessError as e:
+      traceback.print_exc()
+      sys.exit(1)
+
+    # If this did sys.exit(1), building again would succeed (which would be
+    # awkward). Instead, just print a big warning.
+    build_util.PrintBigWarning(
+        'This is unexpected. `ant ' + ' '.join(args) + '` failed.' +
+        'But, running `ant ' + ' '.join(verbose_args) + '` passed.')
+
   stdout = stdout.strip().split('\n')
   for line in stdout:
     if line.strip() == 'BUILD SUCCESSFUL':
