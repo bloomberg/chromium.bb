2247f6a8917c2a10db214709ff6705f9157b57a3
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-11 02:59:26 +0000

    Add an AtExitManager to the ipc fuzzer's replay process.
    This fixes an immediate failure with "Tried to RegisterCallback without an
    AtExitManager" when running ipc_fuzzer_replay in debug builds.
    
    R=inferno@chromium.org
    
    Review URL: https://codereview.chromium.org/191293016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256104 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 27ba9ae062f34945bcedb22e2e48a0ebdf146001

--- tools/ipc_fuzzer/replay/replay.cc
+++ tools/ipc_fuzzer/replay/replay.cc
@@ -3,9 +3,12 @@
 // found in the LICENSE file.
 
 #include <stdlib.h>
+
+#include "base/at_exit.h"
 #include "tools/ipc_fuzzer/replay/replay_process.h"
 
 int main(int argc, const char** argv) {
+  base::AtExitManager exit_manager;
   ipc_fuzzer::ReplayProcess replay;
   if (!replay.Initialize(argc, argv))
     return EXIT_FAILURE;
