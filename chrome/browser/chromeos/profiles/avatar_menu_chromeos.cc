001c702fcce2b402863aad3fdf482582b096b1a3
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 06:44:37 +0000

    Circular avatar icon for cros multiprofile
    BUG=338782
    R=skuhne@chromium.org
    
    Review URL: https://codereview.chromium.org/194653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256459 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 97c6e19dfb2d4412eed36c6a6a0b6f4ca2f6c724

--- chrome/browser/chromeos/profiles/avatar_menu_chromeos.cc
+++ chrome/browser/chromeos/profiles/avatar_menu_chromeos.cc
@@ -9,11 +9,23 @@
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "grit/theme_resources.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/image/image_skia_operations.h"
 
 // static
 void AvatarMenu::GetImageForMenuButton(Profile* profile,
                                        gfx::Image* image,
                                        bool* is_rectangle) {
+  static const gfx::ImageSkia* holder =
+      ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
+          IDR_AVATAR_HOLDER);
+  static const gfx::ImageSkia* holder_mask =
+      ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
+          IDR_AVATAR_HOLDER_MASK);
+  // ChromeOS avatar icon is circular.
+  *is_rectangle = false;
+
   // Find the user for this profile.
   std::string user_id_hash =
       chromeos::ProfileHelper::GetUserIdHashFromProfile(profile);
@@ -22,11 +34,17 @@ void AvatarMenu::GetImageForMenuButton(Profile* profile,
   for (chromeos::UserList::const_iterator it = users.begin();
       it != users.end(); ++it) {
     if ((*it)->username_hash() == user_id_hash) {
-      *image = gfx::Image((*it)->image());
-      break;
+      gfx::ImageSkia avatar = (*it)->image();
+      gfx::ImageSkia resized = gfx::ImageSkiaOperations::CreateResizedImage(
+          avatar, skia::ImageOperations::RESIZE_BEST, holder->size());
+      gfx::ImageSkia masked =
+          gfx::ImageSkiaOperations::CreateMaskedImage(resized, *holder_mask);
+      gfx::ImageSkia result =
+          gfx::ImageSkiaOperations::CreateSuperimposedImage(*holder, masked);
+      *image = gfx::Image(result);
+      return;
     }
+    LOG(FATAL) << "avatar image for the profile '"
+               << profile->GetProfileName() << "' not found";
   }
-
-  // ChromeOS user images are rectangular, unlike Chrome profile avatars.
-  *is_rectangle = true;
 }
