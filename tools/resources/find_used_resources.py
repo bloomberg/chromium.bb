cd6e3acfe3dd10dc79d06c2327d5d46a3650bb72
aurimas@google.com <aurimas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 02:24:22 +0000

    Add a script to extract used resource ids.
    Script extracts resource IDs from the build output for the resources
    that cause an "unknown pragma" warning.
    
    BUG=338759
    R=newt@chromium.org
    
    Review URL: https://codereview.chromium.org/195513003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256409 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 92363c86170e2cb4714bfad22496c03d5186f014

---
+++ tools/resources/find_used_resources.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+import re
+import sys
+
+usage = """find_used_resources.py
+
+Prints out (to sdout) the sorted list of resource ids that are part of unknown
+pragma warning in the given build log (via stdin).
+
+This script is used to find the resources that are actually compiled in Chrome
+in order to only include the needed strings/images in Chrome PAK files. The
+script parses out the list of used resource ids. These resource ids show up in
+the build output after building Chrome with gyp variable
+enable_resource_whitelist_generation set to 1. This gyp flag causes the compiler
+to print out a UnknownPragma message every time a resource id is used. E.g.:
+foo.cc:22:0: warning: ignoring #pragma IDS_FOO_BAR  [-Wunknown-pragmas]
+
+"""
+
+
+def GetResourceIdsInPragmaWarnings(input):
+  """Returns sorted set of resource ids that are inside unknown pragma warnings
+     for the given input.
+  """
+  used_resources = set()
+  unknown_pragma_warning_pattern = re.compile('warning: ignoring #pragma '
+      '(?P<resource_id>[A-Z0-9_]*)  \[-Wunknown-pragmas\]')
+  for ln in input:
+    match = unknown_pragma_warning_pattern.search(ln)
+    if match:
+      resource_id = match.group('resource_id')
+      used_resources.add(resource_id)
+  return sorted(used_resources)
+
+def Main():
+  if len(sys.argv) != 1:
+    sys.stderr.write(usage)
+    sys.exit(1)
+  else:
+    used_resources = GetResourceIdsInPragmaWarnings(sys.stdin)
+    for rid in used_resources:
+      sys.stdout.write(rid + '\n')
+
+if __name__ == '__main__':
+  Main()
