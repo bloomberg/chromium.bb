953b586de31e874e146d59fec2343630725a7f12
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-12 16:36:39 +0000

    Disable BrowserEncodingTest.TestEncodingAutoDetect
    BUG=351325
    TBR=cpu,jochen
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/195373002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256556 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e2c43edc6c827fef2b98061cee4df9129b815256

--- chrome/browser/browser_encoding_browsertest.cc
+++ chrome/browser/browser_encoding_browsertest.cc
@@ -221,7 +221,8 @@ IN_PROC_BROWSER_TEST_F(BrowserEncodingTest, TestOverrideEncoding) {
 // http://crbug.com/2927 for more details.
 //
 // This test fails frequently on the win_rel trybot. See http://crbug.com/122053
-#if defined(OS_WIN)
+// It also times out frequently on Mac dbg. See http://crbug.com/351325
+#if defined(OS_WIN) || defined(OS_MACOSX)
 #define MAYBE_TestEncodingAutoDetect DISABLED_TestEncodingAutoDetect
 #else
 #define MAYBE_TestEncodingAutoDetect TestEncodingAutoDetect
