379975b6ef996673ba7a909f277a449f9234e4a0
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-13 11:01:47 +0000

    DevTools: reapply r167585: Do not access WebViewImpl and WebDevToolsAgentClient when closing.
    TBR=yurys
    BUG=312393, 344623i
    
    Review URL: https://codereview.chromium.org/175313002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169115 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 8ff630557177433f49ca35955df6e61f9fabfa39

--- third_party/WebKit/Source/core/inspector/InspectorController.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorController.cpp
@@ -115,7 +115,6 @@ InspectorController::~InspectorController()
 {
     m_instrumentingAgents->reset();
     m_agents.discardAgents();
-    ASSERT(!m_inspectorClient);
 }
 
 PassOwnPtr<InspectorController> InspectorController::create(Page* page, InspectorClient* client)
--- third_party/WebKit/Source/core/page/Page.cpp
+++ third_party/WebKit/Source/core/page/Page.cpp
@@ -162,8 +162,6 @@ Page::~Page()
         frame->detachFromFrameHost();
     }
 
-    m_inspectorController->inspectedPageDestroyed();
-
     if (m_scrollingCoordinator)
         m_scrollingCoordinator->pageDestroyed();
 
