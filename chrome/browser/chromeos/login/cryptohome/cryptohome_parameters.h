228a319de3903376005ece8b5eef400b36e28bf9
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-13 18:29:02 +0000

    Add support for new calls in cryptohome.
    Related changes on chromeos side:
    
    https://chromium-review.googlesource.com/#/c/186561/
    https://chromium-review.googlesource.com/#/c/188490/
    
    New methods added: MountEx, CheckKeyEx, AddKeyEx, UpdateKeyEx (extended versions of Mount, CheckKey, AddKey calls).
    
    This is done to achieve several goals. One is to unify most of cryptohome calls by using protobuffers.
    Using protobuffers also makes most of the code resilient to implementation changes.
    These methods also introduce key annotations (labels), that are used to specify keys for operations on valuts with multiple keys, as well as introduce per-key privileges.
    
    First part of CL: https://chromiumcodereview.appspot.com/186663002/
    
    BUG=244472
    NOTRY=true
    only win_rel bot is not green, and this change does not affect win anyhow.
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=256823
    
    Review URL: https://codereview.chromium.org/198003002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256877 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b6262d50454777b2307e9ae53948a5561286f546

---
+++ chrome/browser/chromeos/login/cryptohome/cryptohome_parameters.h
@@ -0,0 +1,74 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_LOGIN_CRYPTOHOME_CRYPTOHOME_PARAMETERS_H_
+#define CHROME_BROWSER_CHROMEOS_LOGIN_CRYPTOHOME_CRYPTOHOME_PARAMETERS_H_
+
+#include <string>
+#include <vector>
+
+#include "base/basictypes.h"
+
+namespace cryptohome {
+
+enum AuthKeyPrivileges {
+  PRIV_MOUNT = 1 << 0,              // Can mount with this key.
+  PRIV_ADD = 1 << 1,                // Can add new keys.
+  PRIV_REMOVE = 1 << 2,             // Can remove other keys.
+  PRIV_MIGRATE = 1 << 3,            // Destroy all keys and replace with new.
+  PRIV_AUTHORIZED_UPDATE = 1 << 4,  // Key can be updated in place.
+  PRIV_DEFAULT = PRIV_MOUNT | PRIV_ADD | PRIV_REMOVE | PRIV_MIGRATE
+};
+
+// Identification of the user calling cryptohome method.
+struct Identification {
+  explicit Identification(const std::string& user_id) : user_id(user_id) {}
+  std::string user_id;
+};
+
+// Definition of the key (e.g. password) for the cryptohome.
+// It contains authorization data along with extra parameters like perimissions
+// associated with this key.
+struct KeyDefinition {
+  KeyDefinition(const std::string& key,
+                const std::string& label,
+                int /*AuthKeyPrivileges*/ privileges);
+  ~KeyDefinition();
+  std::string label;
+
+  int revision;
+  std::string key;
+
+  std::string encryption_key;
+  std::string signature_key;
+  // Privileges associated with key. Combination of |AuthKeyPrivileges| values.
+  int privileges;
+};
+
+// Authorization attempt data for user.
+struct Authorization {
+  Authorization(const std::string& key, const std::string& label);
+  explicit Authorization(const KeyDefinition& key);
+  std::string key;
+  std::string label;
+};
+
+// Parameters for Mount call.
+class MountParameters {
+ public:
+  explicit MountParameters(bool ephemeral);
+  ~MountParameters();
+
+  // If |true|, the mounted home dir will be backed by tmpfs. If |false|, the
+  // ephemeral users policy decides whether tmpfs or an encrypted directory is
+  // used as the backend.
+  bool ephemeral;
+
+  // If not empty, home dir will be created with these keys if it exist.
+  std::vector<KeyDefinition> create_keys;
+};
+
+}  // namespace cryptohome
+
+#endif  // CHROME_BROWSER_CHROMEOS_LOGIN_CRYPTOHOME_CRYPTOHOME_PARAMETERS_H_
---
+++ chrome/browser/chromeos/login/cryptohome/mock_homedir_methods.cc
@@ -0,0 +1,42 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/login/cryptohome/mock_homedir_methods.h"
+
+#include "chromeos/cryptohome/mock_async_method_caller.h"
+
+using ::testing::Invoke;
+using ::testing::WithArgs;
+using ::testing::_;
+
+namespace cryptohome {
+
+MockHomedirMethods::MockHomedirMethods()
+    : success_(false), return_code_(cryptohome::MOUNT_ERROR_NONE) {}
+
+MockHomedirMethods::~MockHomedirMethods() {}
+
+void MockHomedirMethods::SetUp(bool success, MountError return_code) {
+  success_ = success;
+  return_code_ = return_code;
+  ON_CALL(*this, CheckKeyEx(_, _, _)).WillByDefault(
+      WithArgs<2>(Invoke(this, &MockHomedirMethods::DoCallback)));
+  ON_CALL(*this, MountEx(_, _, _, _)).WillByDefault(
+      WithArgs<3>(Invoke(this, &MockHomedirMethods::DoMountCallback)));
+  ON_CALL(*this, AddKeyEx(_, _, _, _, _)).WillByDefault(
+      WithArgs<4>(Invoke(this, &MockHomedirMethods::DoCallback)));
+  ON_CALL(*this, UpdateKeyEx(_, _, _, _, _)).WillByDefault(
+      WithArgs<4>(Invoke(this, &MockHomedirMethods::DoCallback)));
+}
+
+void MockHomedirMethods::DoCallback(const Callback& callback) {
+  callback.Run(success_, return_code_);
+}
+
+void MockHomedirMethods::DoMountCallback(const MountCallback& callback) {
+  callback.Run(
+      success_, return_code_, MockAsyncMethodCaller::kFakeSanitizedUsername);
+}
+
+}  // namespace cryptohome
