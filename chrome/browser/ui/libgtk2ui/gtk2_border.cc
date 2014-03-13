17cf73bac0de0cc0c5040f5bf7f023b3c1553682
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-13 20:06:13 +0000

    linux_aura: Treat insets on toolbar buttons separately.
    This reverts r255406, which was a revert of r255091. Previous attempts
    at fixing this included:
    
    - Trying to track down each button and alter the minimum size, which was
      impractical.
    - Always using the views inset size. This clipped the gtk version of the
      toolbar back/forward/reload images. (Technically the views buttons also
      are clipped, but it doesn't matter since that area in the image is
       empty.)
    - Always using the gtk inset size. This broke buttons in dialogs because
      they require the larger insets for their text borders.
    
    BUG=351681, 349777, 349183, 344498, 339247
    TEST=In gtk theme mode, the icons aren't cilpped on the left and
    right. In dialogs (bookmark, fullscreen, and the "are you sure you want
    to leave this page" dialog, buttons widths have margins on each end of
    the text.
    
    Review URL: https://codereview.chromium.org/196023009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@256904 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: cd9d075939f8b87d1f6acc7cb5c709de4275ec31

--- chrome/browser/ui/libgtk2ui/gtk2_border.cc
+++ chrome/browser/ui/libgtk2ui/gtk2_border.cc
@@ -120,10 +120,18 @@ void Gtk2Border::Paint(const views::View& view, gfx::Canvas* canvas) {
 }
 
 gfx::Insets Gtk2Border::GetInsets() const {
+  // TODO(erg): We want to differentiate between buttons on the toolbar and
+  // buttons everywhere else. Right now, the only way to do this is to check
+  // the style. STYLE_BUTTON is the button style used in dialogs, and
+  // STYLE_TEXTUBTTON is the button style used in the toolbar, including all
+  // the buttons which are just toolbar images.
   ui::ThemeProvider* provider = owning_button_->GetThemeProvider();
-  if (!provider || !provider->UsingNativeTheme())
+  if (owning_button_->style() == views::Button::STYLE_BUTTON ||
+      (!provider || !provider->UsingNativeTheme()))
     return border_->GetInsets();
 
+  // On STYLE_TEXTUBTTON, we want the smaller insets so we can fit the GTK icon
+  // in the toolbar without cutting off the edges of the GTK image.
   return gtk2_ui_->GetButtonInsets();
 }
 
