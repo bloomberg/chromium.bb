37ec6377abdf58a51dbba965f4921c5ad7d28973
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-14 03:54:00 +0000

    Add test for http://trac.webkit.org/changeset/136277
    In studying RenderView::repaintViewAndCompositedLayers, we realized we were
    missing test coverage for it. This CL tests that it's working properly.
    
    R=esprehn@chromium.org
    
    Review URL: https://codereview.chromium.org/197073009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169204 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 1858ef883a4d7d19e765b034a4a53d664d8cc317

--- third_party/WebKit/LayoutTests/http/tests/css/resources/slow-loading-sheet.php
+++ third_party/WebKit/LayoutTests/http/tests/css/resources/slow-loading-sheet.php
@@ -1,6 +1,11 @@
 <?php
+
+$sleep = 100;
+if (array_key_exists('sleep', $_GET))
+    $sleep = $_GET['sleep'];
+
 // We sleep here so that we are have enough time to test the different attributes before the stylesheet is fully loaded.
-usleep(100);
+usleep($sleep);
 
 header("Expires: Thu, 01 Dec 2003 16:00:00 GMT");
 header("Cache-Control: no-cache, no-store, must-revalidate");
