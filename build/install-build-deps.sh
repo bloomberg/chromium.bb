c2a4708c633ea07074279a2a5e5d6c3d11d8b77f
tommycli@chromium.org <tommycli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-14 05:56:17 +0000

    Media Galleries API Metadata: Add libexif to list of Linux system library deps
    Adds libexif to list of system libs chromium build on Linux depends on. In service of https://codereview.chromium.org/191583002/
    
    BUG=318450
    
    Review URL: https://codereview.chromium.org/197653006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257017 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e8796018a6d4722e5c45f55f2f4c4975c682c519

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -98,11 +98,11 @@ dev_list="apache2.2-bin bison curl dpkg-dev elfutils fakeroot flex g++ git-core
           gperf language-pack-da language-pack-fr language-pack-he
           language-pack-zh-hant libapache2-mod-php5 libasound2-dev libbrlapi-dev
           libbz2-dev libcairo2-dev libcap-dev libcups2-dev libcurl4-gnutls-dev
-          libdrm-dev libelf-dev libgconf2-dev libgl1-mesa-dev libglib2.0-dev
-          libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev libkrb5-dev
-          libnspr4-dev libnss3-dev libpam0g-dev libpci-dev libpulse-dev
-          libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev libudev-dev
-          libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
+          libdrm-dev libelf-dev libexif-dev libgconf2-dev libgl1-mesa-dev
+          libglib2.0-dev libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev
+          libkrb5-dev libnspr4-dev libnss3-dev libpam0g-dev libpci-dev
+          libpulse-dev libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev
+          libudev-dev libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
           mesa-common-dev openbox patch perl php5-cgi pkg-config python
           python-cherrypy3 python-dev python-psutil rpm ruby subversion
           ttf-dejavu-core ttf-indic-fonts ttf-kochi-gothic ttf-kochi-mincho
@@ -120,7 +120,7 @@ chromeos_lib_list="libpulse0 libbz2-1.0"
 
 # Full list of required run-time libraries
 lib_list="libatk1.0-0 libc6 libasound2 libcairo2 libcap2 libcups2 libexpat1
-          libfontconfig1 libfreetype6 libglib2.0-0 libgnome-keyring0
+          libexif12 libfontconfig1 libfreetype6 libglib2.0-0 libgnome-keyring0
           libgtk2.0-0 libpam0g libpango1.0-0 libpci3 libpcre3 libpixman-1-0
           libpng12-0 libspeechd2 libstdc++6 libsqlite3-0 libx11-6
           libxau6 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxdmcp6
