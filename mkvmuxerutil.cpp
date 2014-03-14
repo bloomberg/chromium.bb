630a0e3c338e1b32bddf513a2dad807908d2976a
Vignesh Venkatasubramanian <vigneshv@google.com>
2014-03-14 14:37:35 -0700

    mkvmuxerutil: Getting rid of strict-aliasing warning
    Replacing a reinterpret_cast from float& to uint32& with a union to get rid of
    "dereferencing type-punned pointer will break strict-aliasing rules" warning.
    This warning is produced on passing -O3 in gcc.
    
    Change-Id: Ie942597f418e3659e3ff29a909c43dd99266c04d

--- mkvmuxerutil.cpp
+++ mkvmuxerutil.cpp
@@ -12,6 +12,7 @@
 #include <fcntl.h>
 #endif
 
+#include <cassert>
 #include <cmath>
 #include <cstdio>
 #ifdef _MSC_VER
@@ -160,16 +161,22 @@ int32 SerializeFloat(IMkvWriter* writer, float f) {
   if (!writer)
     return -1;
 
-  const uint32& val = reinterpret_cast<const uint32&>(f);
+  assert(sizeof(uint32) == sizeof(float));
+  // This union is merely used to avoid a reinterpret_cast from float& to
+  // uint32& which will result in violation of strict aliasing.
+  union U32 {
+    uint32 u32;
+    float f;
+  } value;
+  value.f = f;
 
   for (int32 i = 1; i <= 4; ++i) {
     const int32 byte_count = 4 - i;
     const int32 bit_count = byte_count * 8;
 
-    const uint32 bb = val >> bit_count;
-    const uint8 b = static_cast<uint8>(bb);
+    const uint8 byte = static_cast<uint8>(value.u32 >> bit_count);
 
-    const int32 status = writer->Write(&b, 1);
+    const int32 status = writer->Write(&byte, 1);
 
     if (status < 0)
       return status;
