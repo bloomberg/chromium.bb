caf1fee2ed31f06818681afdace3bc26fbea6d42
eustas@chromium.org <eustas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-17 13:41:04 +0000

    Do not reject AppCache requests for WebUI.
    BUG=315167
    
    Review URL: https://codereview.chromium.org/196793006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257418 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 31dc63566a43922d78d8fbdd41f53b22a512fc38

--- content/browser/webui/url_data_manager_backend.cc
+++ content/browser/webui/url_data_manager_backend.cc
@@ -56,6 +56,8 @@ const char kChromeURLContentSecurityPolicyHeaderBase[] =
 
 const char kChromeURLXFrameOptionsHeader[] = "X-Frame-Options: DENY";
 
+const int kNoRenderProcessId = -1;
+
 bool SchemeIsInSchemes(const std::string& scheme,
                        const std::vector<std::string>& schemes) {
   return std::find(schemes.begin(), schemes.end(), scheme) != schemes.end();
@@ -237,8 +239,10 @@ URLRequestChromeJob::~URLRequestChromeJob() {
 
 void URLRequestChromeJob::Start() {
   int render_process_id, unused;
-  ResourceRequestInfo::GetRenderFrameForRequest(
+  bool is_renderer_request = ResourceRequestInfo::GetRenderFrameForRequest(
       request_, &render_process_id, &unused);
+  if (!is_renderer_request)
+    render_process_id = kNoRenderProcessId;
   BrowserThread::PostTask(
       BrowserThread::UI,
       FROM_HERE,
@@ -368,6 +372,9 @@ void URLRequestChromeJob::CheckStoragePartitionMatches(
        url.host() == "thumb" ||
        url.host() == "thumb2" ||
        url.host() == "thumbnails")) {
+    allowed = true;
+  } else if (render_process_id == kNoRenderProcessId) {
+    // Request was not issued by renderer.
     allowed = true;
   } else {
     RenderProcessHost* process = RenderProcessHost::FromID(render_process_id);
