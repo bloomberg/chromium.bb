60ceaadb5664b2484712a775723ae966bad89dad
mpcomplete@google.com <mpcomplete@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-18 21:31:22 +0000

    Clean up mojo_translate.py a bit: - Most functions didn't need to be class methods. Moved them out of the class. - Unified how we handle each tree node. Each one always gets a MapFoo method;   no more AddFoo methods. - Used helper function MapTree to make the code shorter.
    R=davemoore@chromium.org
    
    Review URL: https://codereview.chromium.org/198213004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@257764 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4dd49e7dd6e6eaa40c16798b4974e006941ccb5e

--- mojo/public/bindings/pylib/parse/mojo_translate.py
+++ mojo/public/bindings/pylib/parse/mojo_translate.py
@@ -10,154 +10,119 @@ import os
 import sys
 
 
+def MapTree(func, tree, name):
+  if not tree:
+    return []
+  return [func(subtree) for subtree in tree if subtree[0] == name]
+
+def MapKind(kind):
+  map_to_kind = { 'bool': 'b',
+                  'int8': 'i8',
+                  'int16': 'i16',
+                  'int32': 'i32',
+                  'int64': 'i64',
+                  'uint8': 'u8',
+                  'uint16': 'u16',
+                  'uint32': 'u32',
+                  'uint64': 'u64',
+                  'float': 'f',
+                  'double': 'd',
+                  'string': 's',
+                  'handle': 'h',
+                  'handle<data_pipe_consumer>': 'h:d:c',
+                  'handle<data_pipe_producer>': 'h:d:p',
+                  'handle<message_pipe>': 'h:m'}
+  if kind.endswith('[]'):
+    return 'a:' + MapKind(kind[0:len(kind)-2])
+  if kind in map_to_kind:
+    return map_to_kind[kind]
+  return 'x:' + kind
+
+def MapOrdinal(ordinal):
+  if ordinal == None:
+    return None
+  return int(ordinal[1:])  # Strip leading '@'
+
+def GetAttribute(attributes, name):
+  out = None
+  if attributes:
+    for attribute in attributes:
+      if attribute[0] == 'ATTRIBUTE' and attribute[1] == name:
+        out = attribute[2]
+  return out
+
+def MapField(tree):
+  return {'name': tree[2],
+          'kind': MapKind(tree[1]),
+          'ordinal': MapOrdinal(tree[3]),
+          'default': tree[4]}
+
+def MapParameter(tree):
+  return {'name': tree[2],
+          'kind': MapKind(tree[1]),
+          'ordinal': MapOrdinal(tree[3])}
+
+def MapMethod(tree):
+  method = {'name': tree[1],
+            'parameters': MapTree(MapParameter, tree[2], 'PARAM'),
+            'ordinal': MapOrdinal(tree[3])}
+  if tree[4] != None:
+    method['response_parameters'] = MapTree(MapParameter, tree[4], 'PARAM')
+  return method
+
+def MapEnumField(tree):
+  return {'name': tree[1],
+          'value': tree[2]}
+
+def MapStruct(tree):
+  struct = {}
+  struct['name'] = tree[1]
+  # TODO(darin): Add support for |attributes|
+  #struct['attributes'] = MapAttributes(tree[2])
+  struct['fields'] = MapTree(MapField, tree[3], 'FIELD')
+  struct['enums'] = MapTree(MapEnum, tree[3], 'ENUM')
+  return struct
+
+def MapInterface(tree):
+  interface = {}
+  interface['name'] = tree[1]
+  interface['peer'] = GetAttribute(tree[2], 'Peer')
+  interface['methods'] = MapTree(MapMethod, tree[3], 'METHOD')
+  interface['enums'] = MapTree(MapEnum, tree[3], 'ENUM')
+  return interface
+
+def MapEnum(tree):
+  enum = {}
+  enum['name'] = tree[1]
+  enum['fields'] = MapTree(MapEnumField, tree[2], 'ENUM_FIELD')
+  return enum
+
+def MapModule(tree, name):
+  mojom = {}
+  mojom['name'] = name
+  mojom['namespace'] = tree[1]
+  mojom['structs'] = MapTree(MapStruct, tree[2], 'STRUCT')
+  mojom['interfaces'] = MapTree(MapInterface, tree[2], 'INTERFACE')
+  mojom['enums'] = MapTree(MapEnum, tree[2], 'ENUM')
+  return mojom
+
+def MapImport(tree):
+  import_item = {}
+  import_item['filename'] = tree[1]
+  return import_item
+
+
 class MojomBuilder():
   def __init__(self):
     self.mojom = {}
 
-  def MapKind(self, kind):
-    map_to_kind = { 'bool': 'b',
-                    'int8': 'i8',
-                    'int16': 'i16',
-                    'int32': 'i32',
-                    'int64': 'i64',
-                    'uint8': 'u8',
-                    'uint16': 'u16',
-                    'uint32': 'u32',
-                    'uint64': 'u64',
-                    'float': 'f',
-                    'double': 'd',
-                    'string': 's',
-                    'handle': 'h',
-                    'handle<data_pipe_consumer>': 'h:d:c',
-                    'handle<data_pipe_producer>': 'h:d:p',
-                    'handle<message_pipe>': 'h:m'}
-    if kind.endswith('[]'):
-      return 'a:' + self.MapKind(kind[0:len(kind)-2])
-    if kind in map_to_kind:
-      return map_to_kind[kind]
-    return 'x:' + kind
-
-
-  def MapOrdinal(self, ordinal):
-    if ordinal == None:
-      return None;
-    return int(ordinal[1:])  # Strip leading '@'
-
-
-  def GetAttribute(self, attributes, name):
-    out = None
-    if attributes:
-      for attribute in attributes:
-        if attribute[0] == 'ATTRIBUTE' and attribute[1] == name:
-          out = attribute[2]
-    return out
-
-
-  def MapFields(self, fields):
-    out = []
-    for field in fields:
-      if field[0] == 'FIELD':
-        out.append({'name': field[2],
-                    'kind': self.MapKind(field[1]),
-                    'ordinal': self.MapOrdinal(field[3]),
-                    'default': field[4]})
-    return out
-
-
-  def MapParameters(self, parameters):
-    out = []
-    for parameter in parameters:
-      if parameter[0] == 'PARAM':
-        out.append({'name': parameter[2],
-                    'kind': self.MapKind(parameter[1]),
-                    'ordinal': self.MapOrdinal(parameter[3])})
-    return out
-
-
-  def MapMethods(self, methods):
-    out = []
-    if methods:
-      for method in methods:
-        if method[0] == 'METHOD':
-          method_dict = {'name': method[1],
-                         'parameters': self.MapParameters(method[2]),
-                         'ordinal': self.MapOrdinal(method[3])}
-          if method[4] != None:
-            method_dict['response_parameters'] = self.MapParameters(method[4])
-          out.append(method_dict)
-    return out
-
-
-  def MapEnumFields(self, fields):
-    out = []
-    for field in fields:
-      if field[0] == 'ENUM_FIELD':
-        out.append({'name': field[1],
-                    'value': field[2]})
-    return out
-
-
-  def MapEnums(self, enums):
-    out = []
-    if enums:
-      for enum in enums:
-        if enum[0] == 'ENUM':
-          out.append({'name': enum[1],
-                      'fields': self.MapEnumFields(enum[2])})
-    return out
-
-
-  def AddStruct(self, name, attributes, body):
-    struct = {}
-    struct['name'] = name
-    # TODO(darin): Add support for |attributes|
-    #struct['attributes'] = MapAttributes(attributes)
-    struct['fields'] = self.MapFields(body)
-    struct['enums'] = self.MapEnums(body)
-    self.mojom['structs'].append(struct)
-
-  def AddInterface(self, name, attributes, body):
-    interface = {}
-    interface['name'] = name
-    interface['peer'] = self.GetAttribute(attributes, 'Peer')
-    interface['methods'] = self.MapMethods(body)
-    interface['enums'] = self.MapEnums(body)
-    self.mojom['interfaces'].append(interface)
-
-  def AddEnum(self, name, fields):
-    # TODO(mpcomplete): add support for specifying enums as types. Right now
-    # we just use int32.
-    enum = {}
-    enum['name'] = name
-    enum['fields'] = self.MapEnumFields(fields)
-    self.mojom['enums'].append(enum)
-
-  def AddModule(self, name, namespace, contents):
-    self.mojom['name'] = name
-    self.mojom['namespace'] = namespace
-    self.mojom['structs'] = []
-    self.mojom['interfaces'] = []
-    self.mojom['enums'] = []
-    for item in contents:
-      if item[0] == 'STRUCT':
-        self.AddStruct(name=item[1], attributes=item[2], body=item[3])
-      elif item[0] == 'INTERFACE':
-        self.AddInterface(name=item[1], attributes=item[2], body=item[3])
-      elif item[0] == 'ENUM':
-        self.AddEnum(name=item[1], fields=item[2])
-
-  def AddImport(self, filename):
-    import_item = {}
-    import_item['filename'] = filename
-    self.mojom['imports'].append(import_item)
-
   def Build(self, tree, name):
-    self.mojom['imports'] = []
-    for item in tree:
-      if item[0] == 'MODULE':
-        self.AddModule(name=name, namespace=item[1], contents=item[2])
-      elif item[0] == 'IMPORT':
-        self.AddImport(filename=item[1])
+    modules = [MapModule(item, name)
+        for item in tree if item[0] == 'MODULE']
+    if len(modules) != 1:
+      raise Exception('A mojom file must contain exactly 1 module.')
+    self.mojom = modules[0]
+    self.mojom['imports'] = MapTree(MapImport, tree, 'IMPORT')
     return self.mojom
 
 
