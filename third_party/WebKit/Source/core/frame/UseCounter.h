341cfb362b1e3092d8281effae07f4df16bc716e
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-19 02:52:47 +0000

    Measure XPath usage
    We currently don't know how common xpath usage
    is in the wild, but we should.
    
    Review URL: https://codereview.chromium.org/204003003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169510 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2392ca404eb703c41aedf387ea33a5db90d9e509

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -309,6 +309,9 @@ public:
         ElementAnimateKeyframeListEffectObjectTiming = 292,
         ElementAnimateKeyframeListEffectDoubleTiming = 293,
         ElementAnimateKeyframeListEffectNoTiming = 294,
+        DocumentXPathCreateExpression = 295,
+        DocumentXPathCreateNSResolver = 296,
+        DocumentXPathEvaluate = 297,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         NumberOfFeatures, // This enum value must be last.
