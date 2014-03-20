ff062f2eae637f6c95a1d00a8d13200fc4f22cef
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-20 21:04:48 +0000

    Don't delay callback if print without selection. Delayed callback unblock JS, which may delete frame.
    BUG=351992
    
    Review URL: https://codereview.chromium.org/206163002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@258407 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dfdb245893ca110667812789acdf4e4cecfff295

--- chrome/renderer/printing/print_web_view_helper.cc
+++ chrome/renderer/printing/print_web_view_helper.cc
@@ -662,10 +662,12 @@ void PrepareFrameAndViewForPrint::CopySelectionIfNeeded(
     const WebPreferences& preferences,
     const base::Closure& on_ready) {
   on_ready_ = on_ready;
-  if (should_print_selection_only_)
+  if (should_print_selection_only_) {
     CopySelection(preferences);
-  else
-    didStopLoading();
+  } else {
+    // Call immediately, async call crashes scripting printing.
+    CallOnReady();
+  }
 }
 
 void PrepareFrameAndViewForPrint::CopySelection(
