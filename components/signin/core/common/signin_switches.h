306e82f1b8ef74c586e198773002999d6e85b4c8
ernstm@chromium.org <ernstm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 21:29:08 +0000

    Revert 259334 "Revert 259327 "Componentize SigninManagerBase.""
    Something went wrong with the revert. Reverting the revert.
    
    > Revert 259327 "Componentize SigninManagerBase."
    > 
    > Compile failures.
    > 
    > > Componentize SigninManagerBase.
    > > 
    > > This CL does the following:
    > > - Eliminate's SigninManagerBase's |profile_| variable, instead having it get
    > >   the PrefService from the SigninClient
    > > - Moves switches and prefs that SigninManagerBases into the signin component
    > > - Componentizes SigninManagerBase
    > > 
    > > The |profile| argument to SigninManagerBase::Initialize() will be eliminated
    > > when SigninManager no longer requires it.
    > > 
    > > BUG=334161,334201
    > > TBR=thakis
    > > 
    > > Review URL: https://codereview.chromium.org/208333017
    > 
    > TBR=blundell@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/211623003
    
    TBR=ernstm@chromium.org
    
    Review URL: https://codereview.chromium.org/210243006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259337 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3c159db81259e534438f1c67162b63395b29789a

---
+++ components/signin/core/common/signin_switches.h
@@ -0,0 +1,16 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_SIGNIN_CORE_COMMON_SIGNIN_SWITCHES_H_
+#define COMPONENTS_SIGNIN_CORE_COMMON_SIGNIN_SWITCHES_H_
+
+namespace switches {
+
+// All switches in alphabetical order. The switches should be documented
+// alongside the definition of their values in the .cc file.
+extern const char kClearTokenService[];
+
+}  // namespace switches
+
+#endif  // COMPONENTS_SIGNIN_CORE_COMMON_SIGNIN_SWITCHES_H_
