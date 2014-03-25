63234cd09cc65a16f1d7489684b5dbd536d2bbdd
bolian@chromium.org <bolian@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-25 22:12:52 +0000

    Rename and add more page loading metrics for Chrome Proxy tests.
    Split out to be used by: https://codereview.chromium.org/191383003/.
    
    BUG=320748
    
    Review URL: https://codereview.chromium.org/199653007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259360 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 06a7343025d7f3c8d8e9fb3fb9abd948fca55beb

--- tools/perf/metrics/loading.py
+++ tools/perf/metrics/loading.py
@@ -15,12 +15,43 @@ class LoadingMetric(Metric):
 
   def AddResults(self, tab, results):
     load_timings = tab.EvaluateJavaScript('window.performance.timing')
-    load_time_ms = (
-      float(load_timings['loadEventStart']) -
-      load_timings['navigationStart'])
-    dom_content_loaded_time_ms = (
+
+    # NavigationStart relative markers in milliseconds.
+    load_start = (
+      float(load_timings['loadEventStart']) - load_timings['navigationStart'])
+    results.Add('load_start', 'ms', load_start)
+
+    dom_content_loaded_start = (
       float(load_timings['domContentLoadedEventStart']) -
       load_timings['navigationStart'])
-    results.Add('load_time', 'ms', load_time_ms)
-    results.Add('dom_content_loaded_time', 'ms',
-                dom_content_loaded_time_ms)
+    results.Add('dom_content_loaded_start', 'ms', dom_content_loaded_start)
+
+    fetch_start = (
+        float(load_timings['fetchStart']) - load_timings['navigationStart'])
+    results.Add('fetch_start', 'ms', fetch_start, data_type='unimportant')
+
+    request_start = (
+        float(load_timings['requestStart']) - load_timings['navigationStart'])
+    results.Add('request_start', 'ms', request_start, data_type='unimportant')
+
+    # Phase measurements in milliseconds.
+    domain_lookup_duration = (
+        float(load_timings['domainLookupEnd']) -
+        load_timings['domainLookupStart'])
+    results.Add('domain_lookup_duration', 'ms', domain_lookup_duration,
+                data_type='unimportant')
+
+    connect_duration = (
+        float(load_timings['connectEnd']) - load_timings['connectStart'])
+    results.Add('connect_duration', 'ms', connect_duration,
+                data_type='unimportant')
+
+    request_duration = (
+        float(load_timings['responseStart']) - load_timings['requestStart'])
+    results.Add('request_duration', 'ms', request_duration,
+                data_type='unimportant')
+
+    response_duration = (
+        float(load_timings['responseEnd']) - load_timings['responseStart'])
+    results.Add('response_duration', 'ms', response_duration,
+                data_type='unimportant')
