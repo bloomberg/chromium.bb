6a4f541138b634e28cbfcb5dd127826b6a1e5d58
rjwright@chromium.org <rjwright@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-25 07:52:59 +0000

    Web Animations API: Remove literal defaults from TimingInput
    BUG=
    
    Review URL: https://codereview.chromium.org/209683002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169917 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: b6df1f3b0d2bdc92d7652c087b5dfa924d31187c

--- third_party/WebKit/Source/core/animation/TimingInput.cpp
+++ third_party/WebKit/Source/core/animation/TimingInput.cpp
@@ -107,11 +107,11 @@ Timing TimingInput::convert(const Dictionary& timingInputDictionary)
     // FIXME: This method needs to be refactored to handle invalid
     // null, NaN, Infinity values better.
     // See: http://www.w3.org/TR/WebIDL/#es-double
-    double startDelay = 0;
+    double startDelay = Timing::defaults().startDelay;
     timingInputDictionary.get("delay", startDelay);
     setStartDelay(result, startDelay);
 
-    double endDelay = 0;
+    double endDelay = Timing::defaults().endDelay;
     timingInputDictionary.get("endDelay", endDelay);
     setEndDelay(result, endDelay);
 
@@ -119,11 +119,11 @@ Timing TimingInput::convert(const Dictionary& timingInputDictionary)
     timingInputDictionary.get("fill", fillMode);
     setFillMode(result, fillMode);
 
-    double iterationStart = 0;
+    double iterationStart = Timing::defaults().iterationStart;
     timingInputDictionary.get("iterationStart", iterationStart);
     setIterationStart(result, iterationStart);
 
-    double iterationCount = 1;
+    double iterationCount = Timing::defaults().iterationCount;
     timingInputDictionary.get("iterations", iterationCount);
     setIterationCount(result, iterationCount);
 
@@ -132,7 +132,7 @@ Timing TimingInput::convert(const Dictionary& timingInputDictionary)
         setIterationDuration(result, iterationDuration);
     }
 
-    double playbackRate = 1;
+    double playbackRate = Timing::defaults().playbackRate;
     timingInputDictionary.get("playbackRate", playbackRate);
     setPlaybackRate(result, playbackRate);
 
