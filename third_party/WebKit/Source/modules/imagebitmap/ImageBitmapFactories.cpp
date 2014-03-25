e4d878ad57f8c5b478096c6dbc9ebc7a397885f2
junov@chromium.org <junov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-03-25 19:03:18 +0000

    Fix crash when creating an ImageBitmap from an invalid canvas
    BUG=354356
    
    Review URL: https://codereview.chromium.org/211313003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@169973 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: e9c887a80115ddc5c011380f132fe4b36359caf0

--- third_party/WebKit/Source/modules/imagebitmap/ImageBitmapFactories.cpp
+++ third_party/WebKit/Source/modules/imagebitmap/ImageBitmapFactories.cpp
@@ -67,7 +67,12 @@ static ScriptPromise fulfillImageBitmap(ExecutionContext* context, PassRefPtrWil
 {
     RefPtr<ScriptPromiseResolver> resolver = ScriptPromiseResolver::create(context);
     ScriptPromise promise = resolver->promise();
-    resolver->resolve(imageBitmap);
+    if (imageBitmap) {
+        resolver->resolve(imageBitmap);
+    } else {
+        v8::Isolate* isolate = ScriptState::current()->isolate();
+        resolver->reject(ScriptValue(v8::Null(isolate), isolate));
+    }
     return promise;
 }
 
@@ -185,8 +190,9 @@ ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, 
         exceptionState.throwDOMException(IndexSizeError, String::format("The source %s provided is 0.", sw ? "height" : "width"));
         return ScriptPromise();
     }
+
     // FIXME: make ImageBitmap creation asynchronous crbug.com/258082
-    return fulfillImageBitmap(eventTarget.executionContext(), ImageBitmap::create(canvas, IntRect(sx, sy, sw, sh)));
+    return fulfillImageBitmap(eventTarget.executionContext(), canvas->buffer() ? ImageBitmap::create(canvas, IntRect(sx, sy, sw, sh)) : nullptr);
 }
 
 ScriptPromise ImageBitmapFactories::createImageBitmap(EventTarget& eventTarget, Blob* blob, ExceptionState& exceptionState)
