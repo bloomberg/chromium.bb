b485e8c0194a65becc65e42a2d7b53cd6582261f
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-27 21:24:24 +0000

    update headers
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@259992 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9866a1b3b357ed087ae95a3be5c80626596ead56

--- content/browser/accessibility/accessibility_mode_browsertest.cc
+++ content/browser/accessibility/accessibility_mode_browsertest.cc
@@ -10,10 +10,10 @@
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/url_constants.h"
+#include "content/public/test/content_browser_test.h"
+#include "content/public/test/content_browser_test_utils.h"
 #include "content/shell/browser/shell.h"
 #include "content/test/accessibility_browser_test_utils.h"
-#include "content/test/content_browser_test.h"
-#include "content/test/content_browser_test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace content {
