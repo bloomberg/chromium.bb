2e3e10cccba59ebfeaa53e3a38908647361660f7
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 23:32:25 +0000

    Revert 260201 "Give localserver time to shut down"
    > Give localserver time to shut down
    > 
    > On machines with slow I/O, the localserver might not shut down immediately,
    > wait for it if necessary.
    > 
    > BUG=
    > 
    > Review URL: https://codereview.chromium.org/145873004
    
    TBR=sigbjorn@opera.com
    
    Review URL: https://codereview.chromium.org/217913002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260300 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 897e712dd5645f49eae7f5d4bd28866440949d30

--- net/test/spawned_test_server/local_test_server.cc
+++ net/test/spawned_test_server/local_test_server.cc
@@ -10,7 +10,6 @@
 #include "base/path_service.h"
 #include "base/process/kill.h"
 #include "base/strings/string_number_conversions.h"
-#include "base/test/test_timeouts.h"
 #include "base/values.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/net_errors.h"
@@ -127,10 +126,6 @@ bool LocalTestServer::Stop() {
 
 #if defined(OS_WIN)
   // This kills all the processes in the job object.
-  TerminateJobObject(job_handle_.Get(), 1);
-  // This is done asynchronously so wait a bit for the processes to be killed.
-  WaitForSingleObject(job_handle_.Get(),
-                      TestTimeouts::action_timeout().InMilliseconds());
   job_handle_.Close();
 #endif
 
