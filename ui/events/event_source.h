09d7aa1831aa3660f1e50e3049aadce7748424c3
kpschoedel@chromium.org <kpschoedel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-28 15:32:16 +0000

    events: Introduce EventRewriter.
    EventSource gets a list of EventRewriters, which each get a chance to
    rewrite an event before it gets sent to the EventProcessor.
    
    BUG=354033
    R=sadrul@chromium.org
    
    Review URL: https://codereview.chromium.org/210203002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260132 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9e6028da5f17d054e570790e6e6d5a6e3349b52f

--- ui/events/event_source.h
+++ ui/events/event_source.h
@@ -5,6 +5,8 @@
 #ifndef UI_EVENTS_EVENT_SOURCE_H_
 #define UI_EVENTS_EVENT_SOURCE_H_
 
+#include <vector>
+
 #include "ui/events/event_dispatcher.h"
 #include "ui/events/events_export.h"
 
@@ -12,17 +14,32 @@ namespace ui {
 
 class Event;
 class EventProcessor;
+class EventRewriter;
 
 // EventSource receives events from the native platform (e.g. X11, win32 etc.)
 // and sends the events to an EventProcessor.
 class EVENTS_EXPORT EventSource {
  public:
-  virtual ~EventSource() {}
+  EventSource();
+  virtual ~EventSource();
 
   virtual EventProcessor* GetEventProcessor() = 0;
 
+  // Adds a rewriter to modify events before they are sent to the
+  // EventProcessor. The rewriter must be explicitly removed from the
+  // EventSource before the rewriter is destroyed. The EventSource
+  // does not take ownership of the rewriter.
+  void AddEventRewriter(EventRewriter* rewriter);
+  void RemoveEventRewriter(EventRewriter* rewriter);
+
  protected:
   EventDispatchDetails SendEventToProcessor(Event* event);
+
+ private:
+  typedef std::vector<EventRewriter*> EventRewriterList;
+  EventDispatchDetails DeliverEventToProcessor(Event* event);
+  EventRewriterList rewriter_list_;
+  DISALLOW_COPY_AND_ASSIGN(EventSource);
 };
 
 }  // namespace ui
