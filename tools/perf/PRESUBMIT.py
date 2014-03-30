8737fdbd5477015079c24b5f51cee2523d45ecdf
qyearsley@chromium.org <qyearsley@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-03-30 16:52:17 +0000

    Add JSON check in tools/perf presubmit script.
    BUG=
    
    Review URL: https://codereview.chromium.org/215443002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260430 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 09e151be40670907e28608064013cdd38fa0d7a1

--- tools/perf/PRESUBMIT.py
+++ tools/perf/PRESUBMIT.py
@@ -1,26 +1,51 @@
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+"""Presubmit script for changes affecting tools/perf/.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into gcl (and git-cl).
+"""
+
 import os
 import sys
 
-
 PYLINT_BLACKLIST = []
-PYLINT_DISABLED_WARNINGS = ['R0923', 'R0201', 'E1101']
+PYLINT_DISABLED_WARNINGS = [
+    'R0923',  # Interface not implemented
+    'R0201',  # Method could be a function
+    'E1101',  # Non-existent member is accessed.
+]
 
 
 def _CommonChecks(input_api, output_api):
+  """Performs common checks, which includes running pylint."""
   results = []
   old_sys_path = sys.path
   try:
+    # Modules in tools/perf depend on telemetry.
     sys.path = [os.path.join(os.pardir, 'telemetry')] + sys.path
     results.extend(input_api.canned_checks.RunPylint(
         input_api, output_api,
         black_list=PYLINT_BLACKLIST,
         disabled_warnings=PYLINT_DISABLED_WARNINGS))
+    results.extend(_CheckJson(input_api, output_api))
   finally:
     sys.path = old_sys_path
   return results
+
+
+def _CheckJson(input_api, output_api):
+  """Checks whether JSON files in this change can be parsed."""
+  affected_paths = input_api.change.AbsoluteLocalPaths()
+  json_filenames = [name for name in affected_paths if name.endswith('.json')]
+  for filename in json_filenames:
+    try:
+      input_api.json.load(open(filename))
+    except ValueError:
+      return [output_api.PresubmitError('Error parsing JSON in %s!' % filename)]
+  return []
 
 
 def CheckChangeOnUpload(input_api, output_api):
