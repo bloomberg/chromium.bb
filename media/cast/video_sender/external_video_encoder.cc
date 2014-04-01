2064ee5acdfa931d284e9caa45af1478cede3bf1
hshi@chromium.org <hshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-01 22:55:03 +0000

    Cast v2 mirroring: handle BitstreamBufferReady with no matching encoded frame data.
    The HW encoder may send BitstreamBufferReady even before EncodeVideo is called, for
    example to send SPS and PPS headers. We should not notify error in this case, but
    should drop the encoded data and still add back the output buffer to the encoder.
    
    BUG=354267
    TEST=trybot
    R=hclam@chromium.org, pwestin@google.com
    
    Review URL: https://codereview.chromium.org/213723002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@260967 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6c150435f583fc46b8538a21c2405c66d3152aca

--- media/cast/video_sender/external_video_encoder.cc
+++ media/cast/video_sender/external_video_encoder.cc
@@ -212,46 +212,45 @@ class LocalVideoEncodeAcceleratorClient
       NotifyError(media::VideoEncodeAccelerator::kPlatformFailureError);
       return;
     }
-    if (encoded_frame_data_storage_.empty()) {
-      NOTREACHED();
-      NotifyError(media::VideoEncodeAccelerator::kPlatformFailureError);
-      return;
+    if (!encoded_frame_data_storage_.empty()) {
+      scoped_ptr<transport::EncodedVideoFrame> encoded_frame(
+          new transport::EncodedVideoFrame());
+
+      encoded_frame->codec = codec_;
+      encoded_frame->key_frame = key_frame;
+      encoded_frame->last_referenced_frame_id = last_encoded_frame_id_;
+      last_encoded_frame_id_++;
+      encoded_frame->frame_id = last_encoded_frame_id_;
+      encoded_frame->rtp_timestamp = GetVideoRtpTimestamp(
+          encoded_frame_data_storage_.front().capture_time);
+      if (key_frame) {
+        // Self referenced.
+        encoded_frame->last_referenced_frame_id = encoded_frame->frame_id;
+      }
+
+      encoded_frame->data.insert(
+          0, static_cast<const char*>(output_buffer->memory()), payload_size);
+
+      cast_environment_->PostTask(
+          CastEnvironment::MAIN,
+          FROM_HERE,
+          base::Bind(&LogFrameEncodedEvent,
+                     cast_environment_,
+                     cast_environment_->Clock()->NowTicks(),
+                     encoded_frame->rtp_timestamp,
+                     encoded_frame->frame_id));
+
+      cast_environment_->PostTask(
+          CastEnvironment::MAIN,
+          FROM_HERE,
+          base::Bind(encoded_frame_data_storage_.front().frame_encoded_callback,
+                     base::Passed(&encoded_frame),
+                     encoded_frame_data_storage_.front().capture_time));
+
+      encoded_frame_data_storage_.pop_front();
+    } else {
+      VLOG(1) << "BitstreamBufferReady(): no encoded frame data available";
     }
-    scoped_ptr<transport::EncodedVideoFrame> encoded_frame(
-        new transport::EncodedVideoFrame());
-
-    encoded_frame->codec = codec_;
-    encoded_frame->key_frame = key_frame;
-    encoded_frame->last_referenced_frame_id = last_encoded_frame_id_;
-    last_encoded_frame_id_++;
-    encoded_frame->frame_id = last_encoded_frame_id_;
-    encoded_frame->rtp_timestamp =
-        GetVideoRtpTimestamp(encoded_frame_data_storage_.front().capture_time);
-    if (key_frame) {
-      // Self referenced.
-      encoded_frame->last_referenced_frame_id = encoded_frame->frame_id;
-    }
-
-    encoded_frame->data.insert(
-        0, static_cast<const char*>(output_buffer->memory()), payload_size);
-
-    cast_environment_->PostTask(
-        CastEnvironment::MAIN,
-        FROM_HERE,
-        base::Bind(&LogFrameEncodedEvent,
-                   cast_environment_,
-                   cast_environment_->Clock()->NowTicks(),
-                   encoded_frame->rtp_timestamp,
-                   encoded_frame->frame_id));
-
-    cast_environment_->PostTask(
-        CastEnvironment::MAIN,
-        FROM_HERE,
-        base::Bind(encoded_frame_data_storage_.front().frame_encoded_callback,
-                   base::Passed(&encoded_frame),
-                   encoded_frame_data_storage_.front().capture_time));
-
-    encoded_frame_data_storage_.pop_front();
 
     // We need to re-add the output buffer to the encoder after we are done
     // with it.
