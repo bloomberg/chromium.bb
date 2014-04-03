352c47cecdd1ec53275cf399f35ef6ce08495818
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-03 16:29:40 +0000

    Adds 192kHz support for WebRTC capturing.
    BUG=none
    TEST=WebRTC demos on Windows using a capture device which supports 192kHz.
    
    Review URL: https://codereview.chromium.org/223933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261441 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b9dc739eddf3ed5759293e0cf15427dd75175774

--- content/renderer/media/webrtc_audio_capturer.cc
+++ content/renderer/media/webrtc_audio_capturer.cc
@@ -28,7 +28,8 @@ namespace {
 // for its current sample rate (set by the user) on Windows and Mac OS X.
 // The listed rates below adds restrictions and WebRtcAudioDeviceImpl::Init()
 // will fail if the user selects any rate outside these ranges.
-const int kValidInputRates[] = {96000, 48000, 44100, 32000, 16000, 8000};
+const int kValidInputRates[] =
+    {192000, 96000, 48000, 44100, 32000, 16000, 8000};
 #elif defined(OS_LINUX) || defined(OS_OPENBSD)
 const int kValidInputRates[] = {48000, 44100};
 #elif defined(OS_ANDROID)
