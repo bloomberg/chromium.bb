fef5a2d1aa5dfdcfcffe88d2d1d6c030ae5e6e7c
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-03 19:48:14 +0000

    envsetup: Stop looking at CHROME_ANDROID_OFFICIAL_BUILD.
    Depends on https://chrome-internal-review.googlesource.com/159155
    
    BUG=330631
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/223073002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@261509 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4c43740467133b9ef0a998907194fb3d8e81dccf

--- build/android/envsetup.sh
+++ build/android/envsetup.sh
@@ -45,15 +45,6 @@ export PATH=$PATH:${ANDROID_SDK_ROOT}/platform-tools
 # Must be after CHROME_SRC is set.
 export PATH=$PATH:${CHROME_SRC}/build/android
 
-if [[ -n "$CHROME_ANDROID_OFFICIAL_BUILD" ]]; then
-  # These defines are used by various chrome build scripts to tag the binary's
-  # version string as 'official' in linux builds (e.g. in
-  # chrome/trunk/src/chrome/tools/build/version.py).
-  export OFFICIAL_BUILD=1
-  export CHROMIUM_BUILD="_google_chrome"
-  export CHROME_BUILD_TYPE="_official"
-fi
-
 android_gyp() {
   echo "Please call build/gyp_chromium instead. android_gyp is going away."
   "${CHROME_SRC}/build/gyp_chromium" --depth="${CHROME_SRC}" --check "$@"
