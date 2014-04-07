e5a6ac04f229277576fea490accaf17786843cc2
scheib@chromium.org <scheib@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-07 17:28:42 +0000

    Measure usage for Element method webkitRequestPointerLock.
    BUG=359747
    
    Review URL: https://codereview.chromium.org/216883011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@170982 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: b92aff3e4f00958f84e0418e31296d38dba147e4

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -342,6 +342,7 @@ public:
         // The above items are available in M35 branch.
 
         SVGForeignObjectElement = 325,
+        PrefixedElementRequestPointerLock = 326,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         NumberOfFeatures, // This enum value must be last.
