522467225755cb3dd7ac5d787c257c936add1369
simonhong@chromium.org <simonhong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-08 16:31:46 +0000

    cc: cleanup: remove unused member variable in cc::Layer
    R=enne@chromium.org
    BUG=NONE
    TEST=NONE(trivial change)
    
    Review URL: https://codereview.chromium.org/227373009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262434 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 30aa9fcce8e577f71a7e951db3a3128b79aae6cf

--- cc/layers/layer.h
+++ cc/layers/layer.h
@@ -573,7 +573,6 @@ class CC_EXPORT Layer : public base::RefCounted<Layer>,
   // transformed relative to this layer, defines the maximum scroll offset for
   // this layer.
   int scroll_clip_layer_id_;
-  bool scrollable_ : 1;
   bool should_scroll_on_main_thread_ : 1;
   bool have_wheel_event_handlers_ : 1;
   bool have_scroll_event_handlers_ : 1;
