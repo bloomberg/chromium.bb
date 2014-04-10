4c15b15f15cb5f1b8514ee6f860f43ff6fa2f539
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-10 19:18:53 +0000

    [Linux] Fix browser relaunching, which broke after enabling NO_NEW_PRIVS by default.
    BUG=358713, 361943
    TEST=Go to about:flags, change a flag, and click relaunch. The browser starts again.
    
    Review URL: https://codereview.chromium.org/231843007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263046 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: bda6438c3d043f58b27137ff4b25ba2bafb41425

--- chrome/browser/first_run/upgrade_util_linux.cc
+++ chrome/browser/first_run/upgrade_util_linux.cc
@@ -23,7 +23,10 @@ double saved_last_modified_time_of_exe = 0;
 namespace upgrade_util {
 
 bool RelaunchChromeBrowser(const CommandLine& command_line) {
-  return base::LaunchProcess(command_line, base::LaunchOptions(), NULL);
+  base::LaunchOptions options;
+  // Don't set NO_NEW_PRIVS on the relaunched browser process.
+  options.allow_new_privs = true;
+  return base::LaunchProcess(command_line, options, NULL);
 }
 
 bool IsUpdatePendingRestart() {
