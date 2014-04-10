3580c82d445f81fffb42e85f6b798e80650109ad
mostynb@opera.com <mostynb@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-10 14:09:24 +0000

    disable stack trace unittests for uClibc builds
    Since we disabled StackTrace::OutputToStream and StackTrace::Print
    for uClibc builds, we should disable the related unittests for uClibc
    too.
    
    BUG=361130
    
    Review URL: https://codereview.chromium.org/227503009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@262984 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 882ccdfa7158fa337159ea927df964e9a4b4b192

--- base/debug/stack_trace_unittest.cc
+++ base/debug/stack_trace_unittest.cc
@@ -35,6 +35,7 @@ typedef testing::Test StackTraceTest;
 #else
 #define MAYBE_OutputToStream OutputToStream
 #endif
+#if !defined(__UCLIBC__)
 TEST_F(StackTraceTest, MAYBE_OutputToStream) {
   StackTrace trace;
 
@@ -130,6 +131,7 @@ TEST_F(StackTraceTest, DebugOutputToStream) {
 TEST_F(StackTraceTest, DebugPrintBacktrace) {
   StackTrace().Print();
 }
+#endif  // !defined(__UCLIBC__)
 
 #if defined(OS_POSIX) && !defined(OS_ANDROID)
 #if !defined(OS_IOS)
