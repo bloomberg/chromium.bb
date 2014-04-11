b3de1fee01616c4bef14457bb8e5801879f7d0a0
paulwhitton@google.com <paulwhitton@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 13:07:26 +0000

    Enhancements to command_line frontend to memory_inspector
    Removed redundant fields for output line for process list.
    Cosmetic change to the output line for process stats.
    Automatically select device if only one device is connected
    
    BUG=340294
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/222263003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263210 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d738a9068edc50288d9190846340b48f83a4c81e

--- tools/memory_inspector/memory_inspector/frontends/command_line.py
+++ tools/memory_inspector/memory_inspector/frontends/command_line.py
@@ -17,9 +17,9 @@ def main(argv):
   parser.add_option('-b', '--backend', help='Backend name '
                     '(e.g., Android)', type='string', default='Android')
   parser.add_option('-s', '--device_id', help='Device '
-                    'id (e.g., Android serial)', type='string',)
+                    'id (e.g., Android serial)', type='string')
   parser.add_option('-p', '--process_id', help='Target process id',
-                    type='int',)
+                    type='int')
   parser.add_option('-m', '--filter_process_name', help='Process '
                     'name to match', type='string')
   (options, args) = parser.parse_args()
@@ -31,24 +31,43 @@ def main(argv):
     return -1
   if args[0] == 'devices':
     _ListDevices(options.backend)
-  elif args[0] == 'ps':
-    if not options.device_id:
-      print 'You need to specify a DEVICE'
-      return -1
-    else:
-      _ListProcesses(options.backend, options.device_id,
+    return 0
+
+  number_of_devices = 0
+  if options.device_id:
+    device_id = options.device_id
+    number_of_devices = 1
+  else:
+    for device in backends.ListDevices():
+      if device.backend.name == options.backend:
+        number_of_devices += 1
+        device_id = device.id
+
+  if number_of_devices == 0:
+    print "No devices connected"
+    return -1
+
+  if number_of_devices > 1:
+    print ('More than 1 device connected. You need to provide'
+        ' --device_id')
+    return -1
+
+  if args[0] == 'ps':
+    _ListProcesses(options.backend, device_id,
                      options.filter_process_name)
-  elif args[0] == 'stats':
-    if not options.device_id or not options.process_id:
-      print 'You need to specify a DEVICE-ID and PROCESS-ID'
+    return 0
+
+  if args[0] == 'stats':
+    if not options.process_id:
+      print 'You need to provide --process_id'
       return -1
     else:
-      _ListProcessStats(options.backend, options.device_id,
+      _ListProcessStats(options.backend, device_id,
                         options.process_id)
+    return 0
   else:
     print 'Invalid command entered'
     return -1
-  print ''
 
 
 def _ListDevices(backend_name):
@@ -63,24 +82,24 @@ def _ListProcesses(backend_name, device_id, filter_process_name):
   device = backends.GetDevice(backend_name, device_id)
   if not device:
     print 'Device', device_id, 'does not exist'
+    return
+  if not filter_process_name:
+    print 'Listing all processes'
   else:
-    if not filter_process_name:
-      print 'Listing all processes'
-    else:
-      print 'Listing processes matching ' + filter_process_name.lower()
-    print ''
-    device.Initialize()
-    _PrintProcessStatsHeadingLine()
-    for process in device.ListProcesses():
-      if (not filter_process_name or
-          filter_process_name.lower() in process.name.lower()):
-        stats = process.GetStats()
-        _PrintProcessStats(process, stats)
+    print 'Listing processes matching ' + filter_process_name.lower()
+  print ''
+  device.Initialize()
+  _PrintProcessHeadingLine()
+  for process in device.ListProcesses():
+    if (not filter_process_name or
+        filter_process_name.lower() in process.name.lower()):
+      stats = process.GetStats()
+      _PrintProcess(process, stats)
 
 
 def _ListProcessStats(backend_name, device_id, process_id):
   """Prints process stats periodically and displays an error if the
-     process or device does not exist
+     process or device does not exist.
   """
   device = backends.GetDevice(backend_name, device_id)
   if not device:
@@ -100,8 +119,21 @@ def _ListProcessStats(backend_name, device_id, process_id):
       time.sleep(1)
 
 
+def _PrintProcessHeadingLine():
+  print '%-10s : %-50s : %12s %12s %12s' % (
+      'Process ID', 'Process Name', 'RUN_TIME', 'THREADS','MEM_RSS_KB')
+  print ''
+
+
+def _PrintProcess(process, stats):
+  run_time_min, run_time_sec = divmod(stats.run_time, 60)
+  print '%10s : %-50s : %6s m %2s s %8s %12s' % (
+      process.pid, process.name, run_time_min, run_time_sec,
+      stats.threads, stats.vm_rss)
+
+
 def _PrintProcessStatsHeadingLine():
-  print '%-10s : %-50s : %12s %12s %13s %10s %14s' % (
+  print '%-10s : %-50s : %12s %12s %13s %12s %14s' % (
         'Process ID', 'Process Name', 'RUN_TIME', 'THREADS',
         'CPU_USAGE', 'MEM_RSS_KB', 'PAGE_FAULTS')
   print ''
@@ -109,7 +141,7 @@ def _PrintProcessStatsHeadingLine():
 
 def _PrintProcessStats(process, stats):
   run_time_min, run_time_sec = divmod(stats.run_time, 60)
-  print '%10s : %-50s : %6s m %2s s %8s %12s %12s %11s' % (
+  print '%10s : %-50s : %6s m %2s s %8s %12s %13s %11s' % (
         process.pid, process.name, run_time_min, run_time_sec,
         stats.threads, stats.cpu_usage, stats.vm_rss, stats.page_faults)
 
