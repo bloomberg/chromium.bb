2ac3ad9c86e80a1a01591aab42d02598a8b09b84
gcasto@chromium.org <gcasto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-11 03:00:18 +0000

    [Password Generation] Don't generate passwords for custom passphrase users.
    BUG=358998
    
    Review URL: https://codereview.chromium.org/217423018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263156 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2ee1b2cafafa165f12728e7538e81932388e5c53

--- chrome/browser/password_manager/chrome_password_manager_client.cc
+++ chrome/browser/password_manager/chrome_password_manager_client.cc
@@ -161,8 +161,13 @@ ChromePasswordManagerClient::GetProbabilityForExperiment(
 bool ChromePasswordManagerClient::IsPasswordSyncEnabled() {
   ProfileSyncService* sync_service =
       ProfileSyncServiceFactory::GetForProfile(GetProfile());
-  if (sync_service && sync_service->HasSyncSetupCompleted())
+  // Don't consider sync enabled if the user has a custom passphrase. See
+  // crbug.com/358998 for more details.
+  if (sync_service &&
+      sync_service->HasSyncSetupCompleted() &&
+      !sync_service->IsUsingSecondaryPassphrase()) {
     return sync_service->GetActiveDataTypes().Has(syncer::PASSWORDS);
+  }
   return false;
 }
 
