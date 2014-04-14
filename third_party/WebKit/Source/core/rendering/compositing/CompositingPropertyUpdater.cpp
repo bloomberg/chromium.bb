1ff9e3a0255ebaf59269604005a6bb76734f9ded
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-14 23:27:48 +0000

    Fix m_needsToUpdateAncestorDependentProperties ASSERT
    In certain scrolling modes, we can trigger the ASSERT guarding the
    RenderLayer::ancestorDependentProperties because we dirty the
    ancestorDependentProperties when the visibility of a RenderLayer, but we defer
    computing whether the visibility has changed until we compute the
    decendant-dependant properties.
    
    This CL adds a call to updateDescendantDependentFlags in
    CompositingPropertyUpdater to ensure that these properties are clean before
    cleaning the AncestorDependentProperties.
    
    R=vollick@chromium.org
    BUG=362753
    
    Review URL: https://codereview.chromium.org/236303009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171514 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ef5e2b4899358cdf61349fcac7366aac58a9db26

--- third_party/WebKit/Source/core/rendering/compositing/CompositingPropertyUpdater.cpp
+++ third_party/WebKit/Source/core/rendering/compositing/CompositingPropertyUpdater.cpp
@@ -14,6 +14,7 @@ CompositingPropertyUpdater::CompositingPropertyUpdater(RenderLayer* rootRenderLa
     : m_geometryMap(UseTransforms)
     , m_rootRenderLayer(rootRenderLayer)
 {
+    rootRenderLayer->updateDescendantDependentFlags();
 }
 
 CompositingPropertyUpdater::~CompositingPropertyUpdater()
