0caeb1181e6dffd87e87068e6b40b6fa8d93539f
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-15 01:25:14 +0000

    Remove unused parts of WebInputEventFactoryWin.
    Almost everything seems to go through the Aura path now.
    
    BUG=363455
    
    Review URL: https://codereview.chromium.org/235883008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171521 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: fbb7b4eb9ed3b004421fe5cceaace4117ea4260b

--- third_party/WebKit/public/web/win/WebInputEventFactory.h
+++ third_party/WebKit/public/web/win/WebInputEventFactory.h
@@ -38,22 +38,10 @@
 namespace blink {
 
 class WebKeyboardEvent;
-class WebMouseEvent;
-class WebMouseWheelEvent;
 
 class WebInputEventFactory {
 public:
     BLINK_EXPORT static WebKeyboardEvent keyboardEvent(HWND, UINT, WPARAM, LPARAM);
-    BLINK_EXPORT static WebMouseEvent mouseEvent(HWND, UINT, WPARAM, LPARAM);
-    BLINK_EXPORT static WebMouseWheelEvent mouseWheelEvent(HWND, UINT, WPARAM, LPARAM);
-
-    // Windows only provides information on whether a click was a single or
-    // double click, while we need to know the click count past two. The
-    // WebInputEventFactory keeps internal state to allow it to synthesize
-    // that information. In some cases, like fast-running tests, that
-    // information is known to be stale and needs to be reset; that is the
-    // function of resetLastClickState().
-    BLINK_EXPORT static void resetLastClickState();
     BLINK_EXPORT static bool isSystemKeyEvent(const WebKeyboardEvent&);
 };
 
