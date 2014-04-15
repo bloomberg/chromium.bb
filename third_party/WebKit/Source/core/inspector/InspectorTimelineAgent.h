f5d39af707fd44977fe2071664d8131fa72c1445
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-15 12:14:17 +0000

    Move counters update into a sepearate event
    Counter updates are sent as sepearate events. This allows to simplify timeline model on the front-end side and will simplify switching timeline to the trace events.
    
    Removed JS heap counters from regular events. JS heap size changes for selected event are not shown except for GCEvent, instead new heap size graph is added to the memory view.
    
    BUG=361045
    R=pfeldman@chromium.org
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=171555
    
    Review URL: https://codereview.chromium.org/237003002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@171567 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 3380a64a6be762b1228b2109628d57845ab50650

--- third_party/WebKit/Source/core/inspector/InspectorTimelineAgent.h
+++ third_party/WebKit/Source/core/inspector/InspectorTimelineAgent.h
@@ -263,7 +263,6 @@ private:
     void pushCurrentRecord(PassRefPtr<JSONObject> data, const String& type, bool captureCallStack, LocalFrame*, bool hasLowLevelDetails = false);
     TimelineThreadState& threadState(ThreadIdentifier);
 
-    void setCounters(TypeBuilder::Timeline::TimelineEvent*);
     void setFrameIdentifier(TypeBuilder::Timeline::TimelineEvent* record, LocalFrame*);
     void populateImageDetails(JSONObject* data, const RenderImage&);
 
@@ -274,6 +273,7 @@ private:
 
     void addRecordToTimeline(PassRefPtr<TypeBuilder::Timeline::TimelineEvent>, double ts);
     void innerAddRecordToTimeline(PassRefPtr<TypeBuilder::Timeline::TimelineEvent>);
+    void addCountersUpdate(TypeBuilder::Array<TypeBuilder::Timeline::TimelineEvent>* records);
     void clearRecordStack();
     PassRefPtr<TypeBuilder::Timeline::TimelineEvent> createRecordForEvent(const TraceEventDispatcher::TraceEvent&, const String& type, PassRefPtr<JSONObject> data);
 
