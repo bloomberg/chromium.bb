167fd22108281d93130b0cd501ad78a7bd2ae6fe
jbroman@chromium.org <jbroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 19:26:43 +0000

    Remove call to WebSettings::setEditableLinkBehaviorNeverLive.
    After the corresponding Blink change lands, this behavior will
    be baked into Blink.
    
    Blink CL: https://codereview.chromium.org/236203005/
    
    BUG=362961
    
    Review URL: https://codereview.chromium.org/236163005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263966 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f1c26ce6e4e86e76b06e093089a51ce454525ce3

--- content/renderer/web_preferences.cc
+++ content/renderer/web_preferences.cc
@@ -163,12 +163,6 @@ void ApplyWebPreferences(const WebPreferences& prefs, WebView* web_view) {
   settings->setCookieEnabled(prefs.cookie_enabled);
   settings->setNavigateOnDragDrop(prefs.navigate_on_drag_drop);
 
-  // This setting affects the behavior of links in an editable region:
-  // clicking the link should select it rather than navigate to it.
-  // Safari uses the same default. It is unlikley an embedder would want to
-  // change this, since it would break existing rich text editors.
-  settings->setEditableLinkBehaviorNeverLive();
-
   settings->setJavaEnabled(prefs.java_enabled);
 
   // By default, allow_universal_access_from_file_urls is set to false and thus
