337c589e84ecd08de7169e2555d6205368e881ee
Han Shen <shenhan@chromium.org>
2014-04-15 10:36:45 -0700

    Change toolchain major to use svn-mirror/google/gcc-4_9-mobile.
    TEST=None
    BUG=None
    
    Change-Id: I4869adfe4c133221cfbe586e22cad6901b734d2b
    Reviewed-on: https://chromium-review.googlesource.com/194936
    Reviewed-by: Luis Lozano <llozano@chromium.org>
    Commit-Queue: Han Shen <shenhan@google.com>
    Tested-by: Han Shen <shenhan@google.com>

--- buildbot/cbuildbot_config.py
+++ buildbot/cbuildbot_config.py
@@ -1354,7 +1354,7 @@ _toolchain_major = _cros_sdk.add_config('toolchain-major',
   latest_toolchain=True,
   prebuilts=False,
   trybot_list=False,
-  gcc_githash='gcc.gnu.org/branches/google/gcc-4_9-mobile',
+  gcc_githash='svn-mirror/google/gcc-4_9-mobile',
   description='Test next major toolchain revision',
 )
 
