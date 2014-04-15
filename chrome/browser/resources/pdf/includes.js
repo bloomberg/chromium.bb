90fc1dc0e8a9e7e7462489ec401b4a2faed15c8d
raymes@chromium.org <raymes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-15 14:42:19 +0000

    Remove the need for <polymer-selector> in the PDF plugin.
    <polymer-selector> is no longer necessary as button selection state is kept
    in the viewport. This also removes the now unused polymer-selector libraries
    from third_party/.
    
    BUG=303491
    
    Review URL: https://codereview.chromium.org/236733003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@263849 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: daa82051edc3ec197f698a58815274f4ffe9c2ba

--- chrome/browser/resources/pdf/includes.js
+++ chrome/browser/resources/pdf/includes.js
@@ -3,7 +3,7 @@
 // found in the LICENSE file.
 
 <include src="../../../../third_party/polymer/platform/platform.js">
-<include src="../../../../third_party/polymer/vulcanized/polymer-elements.js">
+<include src="../../../../third_party/polymer/polymer/polymer.js">
 <include src="html_office/elements/viewer-toolbar/viewer-toolbar.js">
 <include src="html_office/elements/viewer-progress-bar/viewer-progress-bar.js">
 <include src="html_office/elements/viewer-password-screen/viewer-password-screen.js">
