19238a3d45d9b53fe6172e70f8331a8e5f1855d1
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-16 07:05:09 +0000

    Omnibox: Fix HQP Crash on Bad FormatURL and can_inline Interaction
    The comment in this change explains it all.
    
    I cannot test this fix because I don't know what particular data causes
    it, but I think my theory is good and I'm confident based on the
    stack traces that it fixes the linked crashes.
    
    BUG=359270
    
    Review URL: https://codereview.chromium.org/222783006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264143 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f5ac0d7595d7fdcdac2592d8b91c80a21b4bbf6e

--- chrome/browser/autocomplete/history_quick_provider.cc
+++ chrome/browser/autocomplete/history_quick_provider.cc
@@ -262,8 +262,18 @@ AutocompleteMatch HistoryQuickProvider::QuickMatchToACMatch(
   match.contents_class =
       SpansFromTermMatch(new_matches, match.contents.length(), true);
 
-  if (history_match.can_inline()) {
-    DCHECK(!new_matches.empty());
+  // Set |inline_autocompletion| and |allowed_to_be_default_match| if possible.
+  // The second part of this test can happen if the only match(es) of the user's
+  // term occur in places FormatUrl() decides to omit in the formatted url.
+  // In these cases, it's impossible to set |inline_autocompletion| correctly
+  // and hence the match cannot be the default match.  I (mpearson@) believe
+  // this is likely caused by the mismatch that offsets are originally
+  // computed with respect to the cleaned-up URL yet then applied and
+  // updated by FormatUrl() as if they applied to the original string.
+  // See crbug.com/252630.
+  // TODO(mpearson): replacing the second clause with a DCHECK after fixing
+  // 252630.
+  if (history_match.can_inline() && !new_matches.empty()) {
     size_t inline_autocomplete_offset = new_matches[0].offset +
         new_matches[0].length;
     // |inline_autocomplete_offset| may be beyond the end of the
