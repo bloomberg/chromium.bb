bloomberg/chromium.bb

commit 3a869019eb76b3079944228a7e2fa4a9789681bf

animation: Fix the fade animation to work in both directions
We'll use this for fading out windows.

src/animation.c
@@ -206,7 +206,7 @@ weston_view_animation_run(struct weston_view *view,
 	weston_matrix_init(&animation->transform.matrix);
 	wl_list_insert(&view->geometry.transformation_list,
 		       &animation->transform.link);
-	weston_spring_init(&animation->spring, 200.0, 0.0, 1.0);
+	weston_spring_init(&animation->spring, 200.0, start, stop);
 	animation->spring.friction = 700;
 	animation->animation.frame_counter = 0;
 	animation->animation.frame = weston_view_animation_frame;
@@ -290,17 +290,17 @@ weston_fade_run(struct weston_view *view,
 {
 	struct weston_view_animation *fade;
 
-	fade = weston_view_animation_run(view, 0, end,
+	fade = weston_view_animation_run(view, start, end,
 					 fade_frame, reset_alpha,
 					 done, data, NULL);
 
 	if (fade == NULL)
 		return NULL;
 
-	weston_spring_init(&fade->spring, k, start, end);
+	fade->spring.k = 1000.0;
 
-	fade->spring.friction = 1400;
-	fade->spring.previous = -(end - start) * 0.03;
+	fade->spring.friction = 4000;
+	fade->spring.previous = start - (end - start) * 0.1;
 
 	view->alpha = start;
 

commit a4a6f161290d88895cb7a7172fcee21b1546e0da

animation: Fix input panel slide animation
The position for the slide animation was calculated assuming the value
of the spring was always between 0.0 and 1.0. Commit 3a869019 broke
that assumption, and the result was that the panel would be positioned
at an invisible part of screen. Since there would be no output repaints
scheduled, the result of the animation would only be seen if something
else triggered a repaint (such as a mouse cursor movement).

This patch changes the values for the slide animation's spring to range
between 0.0 and 1.0, thus fixing the position of the panel and the lack
of scheduled repaints problem.

https://bugs.freedesktop.org/show_bug.cgi?id=77347

src/animation.c
@@ -389,7 +389,7 @@ weston_slide_run(struct weston_view *view, float start, float stop,
 	if (!animation)
 		return NULL;
 
-	weston_spring_init(&animation->spring, 400.0, start, stop);
+	weston_spring_init(&animation->spring, 400.0, 0.0, 1.0);
 	animation->spring.friction = 600;
 	animation->spring.clip = WESTON_SPRING_BOUNCE;
 