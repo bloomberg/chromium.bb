c55c09eeab30037f04951eafe4ea41f5d04bad1f
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-18 01:21:41 +0000

    Revert 264672 "Disallow top-level navigation in gaia iframe"
    Broke InlineLoginUISafeIframeBrowserTest.TopFrameNavigationDisallowed:
    http://build.chromium.org/p/chromium.mac/builders/Mac10.6%20Tests%20%282%29/builds/50346/steps/browser_tests/logs/TopFrameNavigationDisallowed
    
    > Disallow top-level navigation in gaia iframe
    > 
    > BUG=364100
    > 
    > Review URL: https://codereview.chromium.org/239143009
    
    TBR=guohui@chromium.org
    
    Review URL: https://codereview.chromium.org/242273004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@264698 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ceff7f98f61beae2e4be5fdcd5c963c2260f3d5d

--- chrome/browser/resources/inline_login/inline_login.js
+++ chrome/browser/resources/inline_login/inline_login.js
@@ -18,16 +18,10 @@ cr.define('inline.login', function() {
   var authExtHost;
 
   /**
-   * Whether the auth ready event has been fired, for testing purpose.
-   */
-  var authReadyFired;
-
-  /**
    * Handler of auth host 'ready' event.
    */
   function onAuthReady() {
     $('contents').classList.toggle('loading', false);
-    authReadyFired = true;
   }
 
   /**
@@ -75,23 +69,7 @@ cr.define('inline.login', function() {
     $('contents').classList.toggle('loading', true);
   }
 
-  /**
-   * Returns the auth host instance, for testing purpose.
-   */
-  function getAuthExtHost() {
-    return authExtHost;
-  }
-
-  /**
-   * Returns whether the auth UI is ready, for testing purpose.
-   */
-  function isAuthReady() {
-    return authReadyFired;
-  }
-
   return {
-    getAuthExtHost: getAuthExtHost,
-    isAuthReady: isAuthReady,
     initialize: initialize,
     loadAuthExtension: loadAuthExtension,
     closeDialog: closeDialog,
