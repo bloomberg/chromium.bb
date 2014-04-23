3106dabc1ffa0c3e2dc47509846c32e188406bdb
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 16:03:07 +0000

    Instrumented libraries: run the build script through pyformat.
    Fixes a lot of the style violations (but not all).
    
    BUG=313751
    R=glider@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/246403009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265659 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 65c214a22389745ee6e92eaddd847ea181d3c916

--- third_party/instrumented_libraries/download_build_install.py
+++ third_party/instrumented_libraries/download_build_install.py
@@ -20,24 +20,25 @@ import sys
 # uses XORIGIN to build library and after that replaces it to $ORIGIN
 # directly in .so file.
 SUPPORTED_SANITIZERS = {
-  'asan': {
-    'compiler_flags': '-fsanitize=address -gline-tables-only -fPIC -w',
-    'linker_flags': '-fsanitize=address -Wl,-z,origin -Wl,-R,XORIGIN/.'
-  },
-  'msan': {
-    'compiler_flags': '-fsanitize=memory -fsanitize-memory-track-origins '
-                      '-gline-tables-only -fPIC -w',
-    'linker_flags': '-fsanitize=memory -Wl,-z,origin -Wl,-R,XORIGIN/.'
-  },
-  'tsan': {
-    'compiler_flags': '-fsanitize=thread -gline-tables-only -fPIC -w',
-    'linker_flags': '-fsanitize=thread -Wl,-z,origin -Wl,-R,XORIGIN/.'
-  },
+    'asan': {
+        'compiler_flags': '-fsanitize=address -gline-tables-only -fPIC -w',
+        'linker_flags': '-fsanitize=address -Wl,-z,origin -Wl,-R,XORIGIN/.'
+    },
+    'msan': {
+        'compiler_flags': '-fsanitize=memory -fsanitize-memory-track-origins '
+                          '-gline-tables-only -fPIC -w',
+        'linker_flags': '-fsanitize=memory -Wl,-z,origin -Wl,-R,XORIGIN/.'
+    },
+    'tsan': {
+        'compiler_flags': '-fsanitize=thread -gline-tables-only -fPIC -w',
+        'linker_flags': '-fsanitize=thread -Wl,-z,origin -Wl,-R,XORIGIN/.'
+    },
 }
 
 
 class ScopedChangeDirectory(object):
   """Changes current working directory and restores it back automatically."""
+
   def __init__(self, path):
     self.path = path
     self.old_path = ''
@@ -58,11 +59,12 @@ def get_script_absolute_path():
 def get_library_build_dependencies(library):
   command = 'apt-get -s build-dep %s | grep Inst | cut -d " " -f 2' % library
   command_result = subprocess.Popen(command, stdout=subprocess.PIPE,
-      shell=True)
+                                    shell=True)
   if command_result.wait():
-    raise Exception("Failed to determine build dependencies for %s" % library)
+    raise Exception('Failed to determine build dependencies for %s' % library)
   build_dependencies = [l.strip() for l in command_result.stdout]
   return build_dependencies
+
 
 def check_library_build_dependencies(library):
   build_dependencies = get_library_build_dependencies(library)
@@ -72,12 +74,13 @@ def check_library_build_dependencies(library):
     print >> sys.stderr, 'sudo apt-get -y --no-remove build-dep %s' % library
     sys.exit(1)
 
+
 def shell_call(command, verbose=False, environment=None):
   """ Wrapper on subprocess.Popen
-  
+
   Calls command with specific environment and verbosity using
   subprocess.Popen
-  
+
   Args:
     command: Command to run in shell.
     verbose: If False, hides all stdout and stderr in case of successful build.
@@ -90,17 +93,20 @@ def shell_call(command, verbose=False, environment=None):
   Raises:
     Exception: if return code after call is not zero.
   """
-  child = subprocess.Popen(command, stdout=subprocess.PIPE,
-      stderr=subprocess.STDOUT, env=environment, shell=True)
+  child = subprocess.Popen(
+      command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+      env=environment, shell=True)
   stdout, stderr = child.communicate()
   if verbose or child.returncode:
     print stdout
   if child.returncode:
-    raise Exception("Failed to run: %s" % command)
+    raise Exception('Failed to run: %s' % command)
+
 
 def run_shell_commands(commands, verbose=False, environment=None):
   for command in commands:
     shell_call(command, verbose, environment)
+
 
 def destdir_configure_make_install(parsed_arguments, environment,
                                    install_prefix):
@@ -116,15 +122,17 @@ def destdir_configure_make_install(parsed_arguments, environment,
   # in dependent libraries.
   shell_call('rm %s/lib/*.la -f' % install_prefix)
 
+
 def prefix_configure_make_install(parsed_arguments, environment,
                                   install_prefix):
   configure_command = './configure %s --prefix=%s' % (
       parsed_arguments.custom_configure_flags, install_prefix)
   shell_call(configure_command, parsed_arguments.verbose, environment)
   shell_call('make -j%s' % parsed_arguments.jobs,
-      parsed_arguments.verbose, environment)
+             parsed_arguments.verbose, environment)
   shell_call('make -j%s install' % parsed_arguments.jobs,
-      parsed_arguments.verbose, environment)
+             parsed_arguments.verbose, environment)
+
 
 def nss_make_and_copy(parsed_arguments, environment, install_prefix):
   # NSS uses a build system that's different from configure/make/install. All
@@ -150,7 +158,7 @@ def nss_make_and_copy(parsed_arguments, environment, install_prefix):
   with ScopedChangeDirectory('nss') as cd_nss:
     # -j is not supported
     shell_call('make %s' % ' '.join(make_args), parsed_arguments.verbose,
-        environment)
+               environment)
     # 'make install' is not supported. Copy the DSOs manually.
     install_dir = '%s/lib/' % install_prefix
     for (dirpath, dirnames, filenames) in os.walk('./lib/'):
@@ -158,40 +166,45 @@ def nss_make_and_copy(parsed_arguments, environment, install_prefix):
         if filename.endswith('.so'):
           full_path = os.path.join(dirpath, filename)
           if parsed_arguments.verbose:
-            print "download_build_install.py: installing %s" % full_path
+            print 'download_build_install.py: installing %s' % full_path
           shutil.copy(full_path, install_dir)
+
 
 def libcap2_make_install(parsed_arguments, environment, install_prefix):
   # libcap2 doesn't come with a configure script
-  make_args = ['%s="%s"' % (name, environment[name]) for name in ['CC', 'CXX',
-    'CFLAGS', 'CXXFLAGS', 'LDFLAGS']]
+  make_args = [
+      '%s="%s"' % (name, environment[name])
+      for name in['CC', 'CXX', 'CFLAGS', 'CXXFLAGS', 'LDFLAGS']]
   shell_call('make -j%s %s' % (parsed_arguments.jobs, ' '.join(make_args)),
-    parsed_arguments.verbose, environment)
+             parsed_arguments.verbose, environment)
   install_args = [
-    'DESTDIR=%s' % install_prefix,
-    # Do not install in lib64/.
-    'lib=lib',
-    # Skip a step that requires sudo.
-    'RAISE_SETFCAP=no'
+      'DESTDIR=%s' % install_prefix,
+      # Do not install in lib64/.
+      'lib=lib',
+      # Skip a step that requires sudo.
+      'RAISE_SETFCAP=no'
   ]
-  shell_call('make -j%s install %s' % (parsed_arguments.jobs,
-    ' '.join(install_args)), parsed_arguments.verbose, environment)
+  shell_call('make -j%s install %s' %
+             (parsed_arguments.jobs, ' '.join(install_args)),
+             parsed_arguments.verbose, environment)
+
 
 def libpci3_make_install(parsed_arguments, environment, install_prefix):
   # pciutils doesn't have a configure script
   # This build script follows debian/rules.
-  
+
   # `make install' will create a "$(DESTDIR)-udeb" directory alongside destdir.
   # We don't want that in our product dir, so we use an intermediate directory.
   destdir = 'debian/pciutils'
-  make_args = ['%s="%s"' % (name, environment[name]) for name in ['CC', 'CXX',
-    'CFLAGS', 'CXXFLAGS', 'LDFLAGS']]
+  make_args = [
+      '%s="%s"' % (name, environment[name])
+      for name in['CC', 'CXX', 'CFLAGS', 'CXXFLAGS', 'LDFLAGS']]
   make_args.append('SHARED=yes')
   paths = [
-    'LIBDIR=/lib/',
-    'PREFIX=/usr',
-    'SBINDIR=/usr/bin',
-    'IDSDIR=/usr/share/misc',
+      'LIBDIR=/lib/',
+      'PREFIX=/usr',
+      'SBINDIR=/usr/bin',
+      'IDSDIR=/usr/share/misc',
   ]
   install_args = ['DESTDIR=%s' % destdir]
   run_shell_commands([
@@ -200,13 +213,14 @@ def libpci3_make_install(parsed_arguments, environment, install_prefix):
       'make -j%s %s install' % (
           parsed_arguments.jobs,
           ' '.join(install_args + paths))],
-      parsed_arguments.verbose, environment)
+                     parsed_arguments.verbose, environment)
   # Now move the contents of the temporary destdir to their final place.
   run_shell_commands([
       'cp %s/* %s/ -rd' % (destdir, install_prefix),
       'install -m 644 lib/libpci.so* %s/lib/' % install_prefix,
       'ln -s libpci.so.3.1.8 %s/lib/libpci.so.3' % install_prefix],
-      parsed_arguments.verbose, environment)
+                     parsed_arguments.verbose, environment)
+
 
 def build_and_install(parsed_arguments, environment, install_prefix):
   if parsed_arguments.build_method == 'destdir':
@@ -228,11 +242,12 @@ def build_and_install(parsed_arguments, environment, install_prefix):
     libpci3_make_install(parsed_arguments, environment, install_prefix)
   else:
     raise Exception('Unrecognized build method: %s' %
-        parsed_arguments.build_method)
+                    parsed_arguments.build_method)
+
 
 def download_build_install(parsed_arguments):
   sanitizer_params = SUPPORTED_SANITIZERS[parsed_arguments.sanitizer_type]
-  
+
   environment = os.environ.copy()
   # Usage of environment variables CC and CXX prefers usage flags --c-compiler
   # and --cxx-compiler
@@ -241,8 +256,9 @@ def download_build_install(parsed_arguments):
   if 'CXX' not in environment and parsed_arguments.cxx_compiler:
     environment['CXX'] = parsed_arguments.cxx_compiler
   environment['CFLAGS'] = '%s %s' % (sanitizer_params['compiler_flags'],
-      parsed_arguments.custom_c_compiler_flags)
-  environment['CXXFLAGS'] = '%s %s' % (sanitizer_params['compiler_flags'],
+                                     parsed_arguments.custom_c_compiler_flags)
+  environment['CXXFLAGS'] = '%s %s' % (
+      sanitizer_params['compiler_flags'],
       parsed_arguments.custom_cxx_compiler_flags)
 
   install_prefix = '%s/%s/instrumented_libraries/%s' % (
@@ -252,12 +268,12 @@ def download_build_install(parsed_arguments):
 
   # Make sure the linker searches the instrumented libraries dir for
   # library dependencies.
-  environment['LDFLAGS'] = '%s -L%s/lib %s' % (sanitizer_params['linker_flags'],
-      install_prefix,
-      parsed_arguments.custom_linker_flags)
+  environment['LDFLAGS'] = '%s -L%s/lib %s' % (
+      sanitizer_params['linker_flags'],
+      install_prefix, parsed_arguments.custom_linker_flags)
 
   library_directory = '%s/%s' % (parsed_arguments.intermediate_directory,
-      parsed_arguments.library)
+                                 parsed_arguments.library)
 
   # A failed build might have left a dirty source tree behind.
   if os.path.exists(library_directory):
@@ -266,24 +282,27 @@ def download_build_install(parsed_arguments):
 
   with ScopedChangeDirectory(library_directory) as cd_library:
     shell_call('apt-get source %s' % parsed_arguments.library,
-        parsed_arguments.verbose)
+               parsed_arguments.verbose)
     # There should be exactly one subdirectory after downloading a package.
     subdirectories = [d for d in os.listdir('.') if os.path.isdir(d)]
     if len(subdirectories) != 1:
       raise (Exception('There was not one directory after downloading '
-          'a package %s' % parsed_arguments.library))
+                       'a package %s' % parsed_arguments.library))
     with ScopedChangeDirectory(subdirectories[0]):
       # Here we are in the package directory.
       if parsed_arguments.run_before_build:
-        shell_call('%s/%s' % (os.path.relpath(cd_library.old_path),
-            parsed_arguments.run_before_build), parsed_arguments.verbose)
+        shell_call(
+            '%s/%s' %
+            (os.path.relpath(cd_library.old_path),
+             parsed_arguments.run_before_build),
+            parsed_arguments.verbose)
       try:
         build_and_install(parsed_arguments, environment, install_prefix)
       except Exception as exception:
         print exception
-        print "Failed to build library %s." % parsed_arguments.library
-        print ("Probably, some of its dependencies are not installed: %s" %
-            ' '.join(get_library_build_dependencies(parsed_arguments.library)))
+        print 'Failed to build library %s.' % parsed_arguments.library
+        print ('Probably, some of its dependencies are not installed: %s' %
+               ' '.join(get_library_build_dependencies(parsed_arguments.library)))
         sys.exit(1)
 
   # Touch a txt file to indicate library is installed.
@@ -297,20 +316,22 @@ def download_build_install(parsed_arguments):
 
 def main():
   argument_parser = argparse.ArgumentParser(
-      description = 'Download, build and install instrumented library')
-  
+      description='Download, build and install instrumented library')
+
   argument_parser.add_argument('-j', '--jobs', type=int, default=1)
   argument_parser.add_argument('-l', '--library', required=True)
-  argument_parser.add_argument('-i', '--product-directory', default='.',
+  argument_parser.add_argument(
+      '-i', '--product-directory', default='.',
       help='Relative path to the directory with chrome binaries')
-  argument_parser.add_argument('-m', '--intermediate-directory', default='.',
+  argument_parser.add_argument(
+      '-m', '--intermediate-directory', default='.',
       help='Relative path to the directory for temporary build files')
   argument_parser.add_argument('--custom-configure-flags', default='')
   argument_parser.add_argument('--custom-c-compiler-flags', default='')
   argument_parser.add_argument('--custom-cxx-compiler-flags', default='')
   argument_parser.add_argument('--custom-linker-flags', default='')
   argument_parser.add_argument('-s', '--sanitizer-type', required=True,
-      choices=SUPPORTED_SANITIZERS.keys()) 
+                               choices=SUPPORTED_SANITIZERS.keys())
   argument_parser.add_argument('-v', '--verbose', action='store_true')
   argument_parser.add_argument('--check-build-deps', action='store_true')
   argument_parser.add_argument('--c-compiler')
@@ -330,7 +351,7 @@ def main():
   # Ensure all build dependencies are installed.
   if parsed_arguments.check_build_deps:
     check_library_build_dependencies(parsed_arguments.library)
-    
+
   download_build_install(parsed_arguments)
 
 
