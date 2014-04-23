133b4d1bcf810507d0619ec2b6b7501d66cadbd2
ppi@chromium.org <ppi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 23:28:00 +0000

    Fix build/android/enable_asserts.py.
    The script seems to have been broken in
    https://codereview.chromium.org/221823011 (missing device serial when
    creating DeviceUtils).
    
    This fixes the script by iterating over all connected devices.
    
    BUG=267773
    
    Review URL: https://codereview.chromium.org/249533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265770 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9a0f030df471a7d8df41a8258631b0063adc815c

--- build/android/enable_asserts.py
+++ build/android/enable_asserts.py
@@ -6,6 +6,7 @@
 
 """Enables dalvik vm asserts in the android device."""
 
+from pylib import android_commands
 from pylib.device import device_utils
 import optparse
 import sys
@@ -21,10 +22,13 @@ def main(argv):
       help='Removes the dalvik.vm.enableassertions property')
   options, _ = option_parser.parse_args(argv)
 
-  device = device_utils.DeviceUtils()
-  if options.set_asserts != None:
-    if device.old_interface.SetJavaAssertsEnabled(options.set_asserts):
-      device.old_interface.Reboot(full_reboot=False)
+  # TODO(jbudorick): Accept optional serial number and run only for the
+  # specified device when present.
+  devices = android_commands.GetAttachedDevices()
+  for device in [device_utils.DeviceUtils(serial) for serial in devices]:
+    if options.set_asserts != None:
+      if device.old_interface.SetJavaAssertsEnabled(options.set_asserts):
+        device.old_interface.Reboot(full_reboot=False)
 
 
 if __name__ == '__main__':
