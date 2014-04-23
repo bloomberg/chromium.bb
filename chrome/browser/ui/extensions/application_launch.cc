259198e73ee15e37479cb9a48b910792d7f01efa
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 14:30:40 +0000

    Remove finch experiment enforcing signin to launch packaged apps.
    We are not going ahead with this policy.
    
    BUG=322153
    
    Review URL: https://codereview.chromium.org/246993002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265638 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 85e13139aca55c68ef7c65fce2e92fc2b7ffbcff

--- chrome/browser/ui/extensions/application_launch.cc
+++ chrome/browser/ui/extensions/application_launch.cc
@@ -8,7 +8,6 @@
 
 #include "apps/launcher.h"
 #include "base/command_line.h"
-#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/app_mode/app_mode_utils.h"
@@ -18,7 +17,6 @@
 #include "chrome/browser/extensions/launch_util.h"
 #include "chrome/browser/extensions/tab_helper.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/ui/app_list/app_list_service.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
@@ -30,11 +28,9 @@
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/web_applications/web_app.h"
 #include "chrome/common/chrome_switches.h"
-#include "chrome/common/extensions/extension_constants.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
 #include "chrome/common/extensions/manifest_url_handler.h"
 #include "chrome/common/url_constants.h"
-#include "components/signin/core/browser/signin_manager.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_view.h"
@@ -45,7 +41,6 @@
 #include "extensions/common/constants.h"
 #include "extensions/common/extension.h"
 #include "grit/generated_resources.h"
-#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/window_open_disposition.h"
 #include "ui/gfx/rect.h"
 
@@ -333,29 +328,6 @@ WebContents* OpenEnabledApplication(const AppLaunchParams& params) {
     // window we can open them on the right desktop.
     PerAppSettingsServiceFactory::GetForBrowserContext(profile)->
         SetDesktopLastLaunchedFrom(extension->id(), params.desktop_type);
-#if !defined(OS_CHROMEOS)
-    SigninManager* signin_manager =
-        SigninManagerFactory::GetForProfile(profile);
-    if (extension->id() != extension_misc::kSettingsAppId &&
-        signin_manager && signin_manager->GetAuthenticatedUsername().empty()) {
-      const char kEnforceSigninToUseAppsFieldTrial[] = "EnforceSigninToUseApps";
-
-      std::string field_trial_value =
-          base::FieldTrialList::FindFullName(kEnforceSigninToUseAppsFieldTrial);
-
-      // Only enforce signin if the field trial is set.
-      if (!field_trial_value.empty()) {
-        GURL gurl(l10n_util::GetStringFUTF8(
-            IDS_APP_LAUNCH_NOT_SIGNED_IN_LINK,
-            base::UTF8ToUTF16(extension->id())));
-        chrome::NavigateParams navigate_params(profile, gurl,
-                                               content::PAGE_TRANSITION_LINK);
-        navigate_params.host_desktop_type = params.desktop_type;
-        chrome::Navigate(&navigate_params);
-        return NULL;
-      }
-    }
-#endif
 
     apps::LaunchPlatformAppWithCommandLine(
         profile, extension, params.command_line, params.current_directory);
