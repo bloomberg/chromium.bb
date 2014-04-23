45df8f5e9637f5f7413a80b926a70be625601787
Yu-Ju Hong <yjhong@chromium.org>
2014-04-23 22:42:05 +0000

    Revert "add a ReportBuildStartStage"
    This reverts commit db2dedd3c8e03fea1fc7ecc827f48349f1c256c6.
    
    BUG=chromium:366333
    TEST=None
    
    Change-Id: Iede7e728b7a31d19e206b303940b3173a840b24b
    Reviewed-on: https://chromium-review.googlesource.com/196655
    Reviewed-by: Yu-Ju Hong <yjhong@chromium.org>
    Tested-by: Yu-Ju Hong <yjhong@chromium.org>
    Reviewed-by: David James <davidjames@chromium.org>

--- scripts/cbuildbot.py
+++ scripts/cbuildbot.py
@@ -363,7 +363,6 @@ class Builder(object):
         print_report = False
         success = self._ReExecuteInBuildroot(sync_instance)
       else:
-        self._RunStage(stages.ReportBuildStartStage)
         self.RunStages()
 
     except Exception as ex:
