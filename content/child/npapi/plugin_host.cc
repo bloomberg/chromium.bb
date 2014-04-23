7cfb10d99e463a29b02cf161b8976a8088d63619
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-23 18:57:21 +0000

    Return stable storage from NPN_UserAgent.
    On OSX and Linux and in some cases on Windows, this was returning c_str() from a
    temporary std::string instance, resulting in use-after-free errors.
    
    BUG=365444
    
    Review URL: https://codereview.chromium.org/248833002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265699 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 33091bea397cb6d7ca3116f001764865c636b9bd

--- content/child/npapi/plugin_host.cc
+++ content/child/npapi/plugin_host.cc
@@ -6,6 +6,7 @@
 
 #include "base/command_line.h"
 #include "base/file_util.h"
+#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/string_piece.h"
@@ -599,7 +600,13 @@ const char* NPN_UserAgent(NPP id) {
         "Gecko/20061103 Firefox/2.0a1";
 #endif
 
-  return content::GetContentClient()->GetUserAgent().c_str();
+  // Provide a consistent user-agent string with memory that lasts
+  // long enough for the caller to read it.
+  static base::LazyInstance<std::string>::Leaky leaky_user_agent =
+    LAZY_INSTANCE_INITIALIZER;
+  if (leaky_user_agent == NULL)
+    leaky_user_agent.Get() = content::GetContentClient()->GetUserAgent();
+  return leaky_user_agent.Get().c_str();
 }
 
 void NPN_Status(NPP id, const char* message) {
