b80e2b20e913c940cced8a9557bcc7252842fd8b
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 01:47:53 +0000

    cros: Restore accidentally deleted multiprofile prefs.
    BUG=364599
    
    Review URL: https://codereview.chromium.org/256403003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265813 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 144453a069b90a2b35f06cd48d7baf7f9cd9cc28

--- chrome/browser/chromeos/login/multi_profile_user_controller.cc
+++ chrome/browser/chromeos/login/multi_profile_user_controller.cc
@@ -63,6 +63,14 @@ void MultiProfileUserController::RegisterProfilePrefs(
       prefs::kMultiProfileUserBehavior,
       kBehaviorUnrestricted,
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
+  registry->RegisterBooleanPref(
+      prefs::kMultiProfileNeverShowIntro,
+      false,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
+  registry->RegisterBooleanPref(
+      prefs::kMultiProfileWarningShowDismissed,
+      false,
+      user_prefs::PrefRegistrySyncable::SYNCABLE_PREF);
 }
 
 MultiProfileUserController::UserAllowedInSessionResult
