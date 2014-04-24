0137aef2a320457db5da73878b8b33939f795729
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-24 18:36:03 +0000

    net: add (*.)app.manilla.com to HSTS preloaded.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@265966 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2e67465a74025543dfca92ab484891e56c45beb7

--- net/http/transport_security_state_static.h
+++ net/http/transport_security_state_static.h
@@ -949,6 +949,7 @@ static const struct HSTSPreload kPreloadedSTS[] = {
   {18, true, "\014dillonkorman\003com", true, kNoPins, DOMAIN_NOT_PINNED },
   {12, true, "\006edmodo\003com", true, kNoPins, DOMAIN_NOT_PINNED },
   {23, false, "\003www\013eternalgoth\002co\002uk", true, kNoPins, DOMAIN_NOT_PINNED },
+  {17, true, "\003app\007manilla\003com", true, kNoPins, DOMAIN_NOT_PINNED },
 };
 static const size_t kNumPreloadedSTS = ARRAYSIZE_UNSAFE(kPreloadedSTS);
 
