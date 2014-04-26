4fc2037595ee476570891eff4e952ae673a5209e
prasadv@chromium.org <prasadv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-26 00:48:56 +0000

    Avoid syncing chrome reference build and LayoutTests to speed up sync. Assuming this might also fix 'Out of Memory' issue on Windows bisect bot BUG= NOTRY=true
    Review URL: https://codereview.chromium.org/253573002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266311 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 715cafdad103400b361a287ab40ce438d8d8e734

--- tools/bisect_utils.py
+++ tools/bisect_utils.py
@@ -40,7 +40,12 @@ DEFAULT_GCLIENT_CUSTOM_DEPS = {
         "chrome/deps/adobe/flash/binaries/ppapi/win/.git",
     "src/third_party/adobe/flash/binaries/ppapi/win_x64":
         "https://chrome-internal.googlesource.com/"
-        "chrome/deps/adobe/flash/binaries/ppapi/win_x64/.git",}
+        "chrome/deps/adobe/flash/binaries/ppapi/win_x64/.git",
+    "src/chrome/tools/test/reference_build/chrome_win": None,
+    "src/chrome/tools/test/reference_build/chrome_mac": None,
+    "src/chrome/tools/test/reference_build/chrome_linux": None,
+    "src/third_party/WebKit/LayoutTests": None,
+    "src/tools/valgrind": None,}
 
 GCLIENT_SPEC_DATA = [
   { "name"        : "src",
