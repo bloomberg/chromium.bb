bloomberg/chromium.bb

commit 6543cf912b92a0453fead76a3d3e36a98d320d8e

Web Animations: Avoid animating between null CSS image values
After a CSSImageSetValue is destructed the pointer to it from the
StyleFetchedImageSet is cleared. Calling
StyleFetchedImageSet::cssValue() after this point will return null.
Ideally we should reconstruct the original CSSImageSetvalue and
return that however there is not enough information in the
StyleFetchedImageSet to do this.

The code path to hit this condition is an obscure edge case
(see test) and the introduction of Oilpan should resolve the need to
nullify the CSSValue pointer in the first place.
With this in mind we should simply avoid crashing and introduce mildly
incorrect behaviour instead of adding complexity to the code while we
wait for Oilpan to complete.

BUG=363567

Review URL: https://codereview.chromium.org/255563002

git-svn-id: svn://svn.chromium.org/blink/trunk@172717 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 3f1bd04ac6e359d5f931dda607bfa920e9a7f590

third_party/WebKit/Source/core/animation/css/CSSAnimatableValueFactory.cpp
@@ -162,8 +162,11 @@ inline static PassRefPtrWillBeRawPtr<AnimatableValue> createFromLengthSize(const
 
 inline static PassRefPtrWillBeRawPtr<AnimatableValue> createFromStyleImage(StyleImage* image)
 {
-    if (image)
-        return AnimatableImage::create(*image);
+    if (image) {
+        RefPtr<CSSValue> cssValue = image->cssValue();
+        if (cssValue)
+            return AnimatableImage::create(cssValue.release());
+    }
     return AnimatableUnknown::create(CSSValueNone);
 }
 

third_party/WebKit/LayoutTests/animations/deleted-image-set-transition-crash-expected.txt
@@ -1,0 +1,1 @@
+This test passes if it does not crash.

third_party/WebKit/LayoutTests/animations/deleted-image-set-transition-crash.html
@@ -1,0 +1,18 @@
+<!DOCTYPE html>
+<style>
+div { background: url(#); }
+div { background: -webkit-image-set(url(#) 1x); }
+</style>
+<div style="transition: 1s">This test passes if it does not crash.</div>
+<script>
+if (window.testRunner) {
+  testRunner.dumpAsText();
+  testRunner.waitUntilDone();
+}
+window.addEventListener('load', function() {
+  document.styleSheets[0].deleteRule(1);
+  if (window.testRunner) {
+    testRunner.notifyDone();
+  }
+});
+</script>

third_party/WebKit/Source/core/animation/AnimatableImage.h
@@ -40,9 +40,9 @@ namespace WebCore {
 class AnimatableImage FINAL : public AnimatableValue {
 public:
     virtual ~AnimatableImage() { }
-    static PassRefPtrWillBeRawPtr<AnimatableImage> create(const StyleImage& image)
+    static PassRefPtrWillBeRawPtr<AnimatableImage> create(PassRefPtrWillBeRawPtr<CSSValue> value)
     {
-        return create(image.cssValue());
+        return adoptRefWillBeNoop(new AnimatableImage(value));
     }
     CSSValue* toCSSValue() const { return m_value.get(); }
 
@@ -53,13 +53,10 @@ protected:
     virtual bool usesDefaultInterpolationWith(const AnimatableValue*) const OVERRIDE;
 
 private:
-    static PassRefPtrWillBeRawPtr<AnimatableImage> create(PassRefPtrWillBeRawPtr<CSSValue> value)
-    {
-        return adoptRefWillBeNoop(new AnimatableImage(value));
-    }
     AnimatableImage(PassRefPtrWillBeRawPtr<CSSValue> value)
         : m_value(value)
     {
+        ASSERT(m_value.get());
     }
     virtual AnimatableType type() const OVERRIDE { return TypeImage; }
     virtual bool equalTo(const AnimatableValue*) const OVERRIDE;

commit 00534bfac38357ed67b20a90def908b08b622515

Oilpan: Fix build broken by r172717
TBR=haraken@chromium.org
BUG=

Review URL: https://codereview.chromium.org/251923002

git-svn-id: svn://svn.chromium.org/blink/trunk@172723 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: 1109ea887578cb9188f1504f552a88b96bc75d81

third_party/WebKit/Source/core/animation/css/CSSAnimatableValueFactory.cpp
@@ -163,8 +163,7 @@ inline static PassRefPtrWillBeRawPtr<AnimatableValue> createFromLengthSize(const
 inline static PassRefPtrWillBeRawPtr<AnimatableValue> createFromStyleImage(StyleImage* image)
 {
     if (image) {
-        RefPtr<CSSValue> cssValue = image->cssValue();
-        if (cssValue)
+        if (RefPtrWillBeRawPtr<CSSValue> cssValue = image->cssValue())
             return AnimatableImage::create(cssValue.release());
     }
     return AnimatableUnknown::create(CSSValueNone);