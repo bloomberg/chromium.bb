5ffe6f4e5d6c78e4e7b7fc4509fd143791201360
msimonides@opera.com <msimonides@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-28 13:31:55 +0000

    Revert LOGs inadvertently added in r265615.
    Review URL: https://codereview.chromium.org/256593006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266539 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 097f165a2b75f52ae283989dbca8949fc574bb96

--- chrome/browser/extensions/crx_installer.cc
+++ chrome/browser/extensions/crx_installer.cc
@@ -167,7 +167,6 @@ CrxInstaller::CrxInstaller(
 }
 
 CrxInstaller::~CrxInstaller() {
-  LOG(WARNING) << "Destroying";
   // Make sure the UI is deleted on the ui thread.
   if (client_) {
     BrowserThread::DeleteSoon(BrowserThread::UI, FROM_HERE, client_);
@@ -836,7 +835,6 @@ void CrxInstaller::NotifyCrxInstallComplete(bool success) {
 
 void CrxInstaller::CleanupTempFiles() {
   if (!installer_task_runner_->RunsTasksOnCurrentThread()) {
-    LOG(WARNING) << "Post CleanupTempFiles";
     if (!installer_task_runner_->PostTask(
             FROM_HERE,
             base::Bind(&CrxInstaller::CleanupTempFiles, this))) {
@@ -845,7 +843,6 @@ void CrxInstaller::CleanupTempFiles() {
     return;
   }
 
-  LOG(WARNING) << "CleanupTempFiles";
   // Delete the temp directory and crx file as necessary.
   if (!temp_dir_.value().empty()) {
     file_util::DeleteFile(temp_dir_, true);
