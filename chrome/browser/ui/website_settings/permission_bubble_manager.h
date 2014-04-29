223be326fd6834584c09ad3e405e401c1f8d4032
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-29 11:53:43 +0000

    [WebsiteSettings] Update permission bubble manager policy
    This policy is more in accord with the design doc changes. Moves to
    showing the dialog upon DOMContentLoaded in source pages, and then
    subsequently for user gestures.
    
    R=leng@chromium.org
    BUG=332115,364159
    
    Review URL: https://codereview.chromium.org/243543003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@266855 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ff534325e9ae7a8886dc77a38d5ff90aa2e2b9cd

--- chrome/browser/ui/website_settings/permission_bubble_manager.h
+++ chrome/browser/ui/website_settings/permission_bubble_manager.h
@@ -7,7 +7,6 @@
 
 #include <vector>
 
-#include "base/timer/timer.h"
 #include "chrome/browser/ui/website_settings/permission_bubble_view.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "content/public/browser/web_contents_user_data.h"
@@ -56,10 +55,6 @@ class PermissionBubbleManager
   // take ownership of the view.
   virtual void SetView(PermissionBubbleView* view) OVERRIDE;
 
- protected:
-  // Sets the coalesce time interval to |interval_ms|. For testing only.
-  void SetCoalesceIntervalForTesting(int interval_ms);
-
  private:
   friend class PermissionBubbleManagerTest;
   friend class DownloadRequestLimiterTest;
@@ -67,15 +62,12 @@ class PermissionBubbleManager
 
   explicit PermissionBubbleManager(content::WebContents* web_contents);
 
-  // contents::WebContentsObserver:
-  // TODO(leng):  Investigate the ordering and timing of page loading and
-  // permission requests with iFrames. DocumentOnLoadCompletedInMainFrame()
-  // and DocumentLoadedInFrame() might be needed as well.
-  virtual void DidFinishLoad(
-      int64 frame_id,
-      const GURL& validated_url,
-      bool is_main_frame,
-      content::RenderViewHost* render_view_host) OVERRIDE;
+  // WebContentsObserver:
+
+  // TODO(leng): Finalize policy for permission requests with iFrames.
+  // DocumentLoadedInFrame() might be needed as well.
+  virtual void DocumentOnLoadCompletedInMainFrame(int32 page_id) OVERRIDE;
+
   // If a page on which permissions requests are pending is navigated,
   // they will be finalized as if canceled by the user.
   virtual void NavigationEntryCommitted(
@@ -90,7 +82,6 @@ class PermissionBubbleManager
   virtual void Deny() OVERRIDE;
   virtual void Closing() OVERRIDE;
 
-  // Called when the coalescing timer is done. Presents the bubble.
   void ShowBubble();
 
   // Finalize the pending permissions request.
@@ -110,10 +101,9 @@ class PermissionBubbleManager
   std::vector<PermissionBubbleRequest*> requests_;
   std::vector<PermissionBubbleRequest*> queued_requests_;
   GURL request_url_;
+  bool request_url_has_loaded_;
   std::vector<bool> accept_states_;
   bool customization_mode_;
-
-  scoped_ptr<base::Timer> timer_;
 };
 
 #endif  // CHROME_BROWSER_UI_WEBSITE_SETTINGS_PERMISSION_BUBBLE_MANAGER_H_
