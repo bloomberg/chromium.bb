a2ec9b901a7ce1fbd8d1d189a6933a04807db4cc
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-29 03:17:53 +0000

    Run the expected output for RefTests with virtual test suite arguments.
    Currently we're always using an empty argument list (oops), because
    we look up the arguments with the test name after removing the virtual
    test suite part from the test name.
    
    R=dpranke@chromium.org
    BUG=367315, 368008
    
    Review URL: https://codereview.chromium.org/257843006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172820 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 3399e54930ba74a4349bb98051096b0357be0b33

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/single_test_runner.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/single_test_runner.py
@@ -358,7 +358,7 @@ class SingleTestRunner(object):
         for expectation, reference_filename in putAllMismatchBeforeMatch(self._reference_files):
             reference_test_name = self._port.relative_test_filename(reference_filename)
             reference_test_names.append(reference_test_name)
-            driver_input = DriverInput(reference_test_name, self._timeout, image_hash=None, should_run_pixel_test=True, args=self._port.lookup_virtual_test_args(reference_test_name))
+            driver_input = DriverInput(reference_test_name, self._timeout, image_hash=None, should_run_pixel_test=True, args=self._port.lookup_virtual_test_args(self._test_name))
             reference_output = self._driver.run_test(driver_input, self._stop_when_done)
             test_result = self._compare_output_with_reference(reference_output, test_output, reference_filename, expectation == '!=')
 
