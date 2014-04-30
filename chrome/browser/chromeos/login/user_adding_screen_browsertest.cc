242fcc574ba22ecbd799157d237857af6c6fefb8
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-04-30 12:50:06 +0000

    Disable UserAddingScreenTest.AddingSeveralUsers, very flaky.
    BUG=368621
    NOTRY=true
    NOTREECHECKS=true
    TBR=dpolukhin@chromium.org
    
    Review URL: https://codereview.chromium.org/268433002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267196 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4817c9e61709c15d156f8c9568fdfeb650e51d2e

--- chrome/browser/chromeos/login/user_adding_screen_browsertest.cc
+++ chrome/browser/chromeos/login/user_adding_screen_browsertest.cc
@@ -114,7 +114,8 @@ IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, PRE_AddingSeveralUsers) {
   StartupUtils::MarkOobeCompleted();
 }
 
-IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, AddingSeveralUsers) {
+// Very flaky, see http://crbug.com/368621
+IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, DISABLED_AddingSeveralUsers) {
   EXPECT_EQ(ash::SessionStateDelegate::SESSION_STATE_LOGIN_PRIMARY,
             ash::Shell::GetInstance()->session_state_delegate()->
                 GetSessionState());
