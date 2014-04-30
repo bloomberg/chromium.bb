8e95d96440eb33e388750bf593f982751972555b
zerny@chromium.org <zerny@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-04-30 11:45:23 +0000

    Oilpan: Revert r169564: Disable the GC_PLUGIN_IGNORE annotation when compiling with asan.
    A fixed clang/asan has been rolled.
    See https://code.google.com/p/address-sanitizer/issues/detail?id=279
    
    R=ager@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/257403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@172972 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 5c5c1a9c4c1a77a0c4ef49cbfcddcdeb5252edbc

--- third_party/WebKit/Source/platform/heap/Heap.h
+++ third_party/WebKit/Source/platform/heap/Heap.h
@@ -1144,7 +1144,7 @@ T* adoptRefCountedGarbageCollected(T* ptr)
 // ignore a particular class or field when checking for proper usage. When using
 // GC_PLUGIN_IGNORE a bug-number should be provided as an argument where the
 // bug describes what needs to happen to remove the GC_PLUGIN_IGNORE again.
-#if COMPILER(CLANG) && !defined(ADDRESS_SANITIZER)
+#if COMPILER(CLANG)
 #define STACK_ALLOCATED()                                       \
     private:                                                    \
         __attribute__((annotate("blink_stack_allocated")))      \
