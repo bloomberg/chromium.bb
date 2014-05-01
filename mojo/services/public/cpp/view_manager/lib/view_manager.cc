398d9a561c3fbac49da0ee446aaf35ca1df7202e
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 04:23:47 +0000

    First step at synchronizing client model changes with service.
    R=sky@chromium.org
    http://crbug.com/365012
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=266940
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=267293
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=267311
    
    Review URL: https://codereview.chromium.org/258623005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267424 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1ab0b22f5527ea1d3eb94b8ed429edd4dfa17c94

---
+++ mojo/services/public/cpp/view_manager/lib/view_manager.cc
@@ -0,0 +1,20 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "mojo/services/public/cpp/view_manager/view_manager.h"
+
+#include "mojo/services/public/cpp/view_manager/lib/view_manager_synchronizer.h"
+
+namespace mojo {
+namespace services {
+namespace view_manager {
+
+ViewManager::ViewManager(Shell* shell)
+    : shell_(shell),
+      synchronizer_(new ViewManagerSynchronizer(this)) {}
+ViewManager::~ViewManager() {}
+
+}  // namespace view_manager
+}  // namespace services
+}  // namespace mojo
---
+++ mojo/services/public/cpp/view_manager/lib/view_manager_synchronizer.h
@@ -0,0 +1,82 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MOJO_SERVICES_PUBLIC_CPP_VIEW_MANAGER_LIB_VIEW_MANAGER_SYNCHRONIZER_H_
+#define MOJO_SERVICES_PUBLIC_CPP_VIEW_MANAGER_LIB_VIEW_MANAGER_SYNCHRONIZER_H_
+
+#include "base/basictypes.h"
+#include "base/memory/scoped_vector.h"
+#include "mojo/public/cpp/bindings/remote_ptr.h"
+#include "mojo/services/public/interfaces/view_manager/view_manager.mojom.h"
+
+namespace mojo {
+namespace services {
+namespace view_manager {
+
+class ViewManager;
+class ViewManagerTransaction;
+
+// Manages the connection with the View Manager service.
+class ViewManagerSynchronizer : public IViewManagerClient {
+ public:
+  explicit ViewManagerSynchronizer(ViewManager* view_manager);
+  virtual ~ViewManagerSynchronizer();
+
+  // API exposed to the node implementation that pushes local changes to the
+  // service.
+  uint16_t CreateViewTreeNode();
+  void AddChild(uint16_t child_id, uint16_t parent_id);
+  void RemoveChild(uint16_t child_id, uint16_t parent_id);
+
+ private:
+  friend class ViewManagerTransaction;
+  typedef ScopedVector<ViewManagerTransaction> Transactions;
+
+  // Overridden from IViewManagerClient:
+  virtual void OnConnectionEstablished(uint16 connection_id) OVERRIDE;
+  virtual void OnNodeHierarchyChanged(uint32 node,
+                                      uint32 new_parent,
+                                      uint32 old_parent,
+                                      uint32 change_id) OVERRIDE;
+  virtual void OnNodeViewReplaced(uint32_t node,
+                                  uint32_t new_view_id,
+                                  uint32_t old_view_id,
+                                  uint32_t change_id) OVERRIDE;
+
+  // Called to schedule a sync of the client model with the service after a
+  // return to the message loop.
+  void ScheduleSync();
+
+  // Sync the client model with the service by enumerating the pending
+  // transaction queue and applying them in order.
+  void DoSync();
+
+  // Used by individual transactions to generate a connection-specific change
+  // id.
+  // TODO(beng): What happens when there are more than sizeof(int) changes in
+  //             the queue?
+  uint32_t GetNextChangeId();
+
+  // Removes |transaction| from the pending queue. |transaction| must be at the
+  // front of the queue.
+  void RemoveFromPendingQueue(ViewManagerTransaction* transaction);
+
+  ViewManager* view_manager_;
+  bool connected_;
+  uint16_t connection_id_;
+  uint16_t next_id_;
+  uint32_t next_change_id_;
+
+  Transactions pending_transactions_;
+
+  RemotePtr<IViewManager> service_;
+
+  DISALLOW_COPY_AND_ASSIGN(ViewManagerSynchronizer);
+};
+
+}  // namespace view_manager
+}  // namespace services
+}  // namespace mojo
+
+#endif  // MOJO_SERVICES_PUBLIC_CPP_VIEW_MANAGER_LIB_VIEW_MANAGER_SYNCHRONIZER_H_
