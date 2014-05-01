261bc62545dbafd228acd39b0b738ba4fe277764
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-01 08:56:04 +0000

    Add some CHECKs to debug issue 363052
    BUG=363052
    
    Review URL: https://codereview.chromium.org/264713004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267497 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dff9bf6630fde412f380eb2a0b60d95964862c69

--- chrome/browser/ui/cocoa/extensions/browser_action_button.mm
+++ chrome/browser/ui/cocoa/extensions/browser_action_button.mm
@@ -132,6 +132,8 @@ class ExtensionActionIconFactoryBridge
     ExtensionAction* browser_action =
         extensions::ExtensionActionManager::Get(browser->profile())->
         GetBrowserAction(*extension);
+    CHECK(browser_action)
+        << "Don't create a BrowserActionButton if there is no browser action.";
     [cell setExtensionAction:browser_action];
     [cell
         accessibilitySetOverrideValue:base::SysUTF8ToNSString(extension->name())
@@ -335,6 +337,7 @@ class ExtensionActionIconFactoryBridge
 - (void)drawWithFrame:(NSRect)cellFrame inView:(NSView*)controlView {
   gfx::ScopedNSGraphicsContextSaveGState scopedGState;
   [super drawWithFrame:cellFrame inView:controlView];
+  CHECK(extensionAction_);
   bool enabled = extensionAction_->GetIsVisible(tabId_);
   const NSSize imageSize = self.image.size;
   const NSRect imageRect =
