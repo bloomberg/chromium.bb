0d1f248d8a7b2145f9ea9d726bd079a17a9cc46f
viettrungluu@chromium.org <viettrungluu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-02 17:54:43 +0000

    Reenable UserAddingScreenTest.AddingSeveralUsers.
    [Not for commit yet. Just making sure I can repro it on the trybots.]
    
    R=darin@chromium.org
    BUG=368621
    
    Review URL: https://codereview.chromium.org/268473005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@267860 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0f5f8ee8edaa4cc9fa53290942be92126a58ed1a

--- chrome/browser/chromeos/login/user_adding_screen_browsertest.cc
+++ chrome/browser/chromeos/login/user_adding_screen_browsertest.cc
@@ -114,8 +114,7 @@ IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, PRE_AddingSeveralUsers) {
   StartupUtils::MarkOobeCompleted();
 }
 
-// Very flaky, see http://crbug.com/368621
-IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, DISABLED_AddingSeveralUsers) {
+IN_PROC_BROWSER_TEST_F(UserAddingScreenTest, AddingSeveralUsers) {
   EXPECT_EQ(ash::SessionStateDelegate::SESSION_STATE_LOGIN_PRIMARY,
             ash::Shell::GetInstance()->session_state_delegate()->
                 GetSessionState());
