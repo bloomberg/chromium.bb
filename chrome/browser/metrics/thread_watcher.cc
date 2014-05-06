c50b54f7f4241114f33589d53fa291dc690fa716
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-06 00:26:19 +0000

    ThreadWatcher - On Android, don't generate the crash dump if StartupTimebomb goes off.
    When StartupTimebomb goes off, collect the duration stats (difference
    between when the timebomb went off and when it is started). We collect
    this duration in terms of clock time, montonic clock time and thread CPU
    time.
    
    We wanted to see when the browser process is stopped, does
    TimeTicks::Now() account for the time the browser process was stopped or
    not. If the TimeTicks doesn't account this time properly, then we could
    have false positive jank related crashes.
    
    BUG=366699
    R=jar@chromium.org
    
    Review URL: https://codereview.chromium.org/269763002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268338 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c602cbff2a44591140591c168d930787e28b63bf

--- chrome/browser/metrics/thread_watcher.cc
+++ chrome/browser/metrics/thread_watcher.cc
@@ -53,6 +53,13 @@ void NullPointerCrash(int line_number) {
 #endif
 }
 
+#if !defined(OS_ANDROID) || !defined(NDEBUG)
+// TODO(rtenneti): Enabled crashing, after getting data.
+NOINLINE void StartupCrash() {
+  NullPointerCrash(__LINE__);
+}
+#endif  // OS_ANDROID
+
 NOINLINE void ShutdownCrash() {
   NullPointerCrash(__LINE__);
 }
@@ -882,19 +889,47 @@ class StartupWatchDogThread : public base::Watchdog {
   // alarming.
   explicit StartupWatchDogThread(const base::TimeDelta& duration)
       : base::Watchdog(duration, "Startup watchdog thread", true) {
+#if defined(OS_ANDROID)
+    // TODO(rtenneti): Delete this code, after getting data.
+    start_time_clock_= base::Time::Now();
+    start_time_monotonic_ = base::TimeTicks::Now();
+    start_time_thread_now_ = base::TimeTicks::IsThreadNowSupported()
+        ? base::TimeTicks::ThreadNow() : base::TimeTicks::Now();
+#endif  // OS_ANDROID
   }
 
   // Alarm is called if the time expires after an Arm() without someone calling
   // Disarm(). When Alarm goes off, in release mode we get the crash dump
   // without crashing and in debug mode we break into the debugger.
   virtual void Alarm() OVERRIDE {
-#ifndef NDEBUG
-    DCHECK(false);
-#else
-    WatchDogThread::PostTask(FROM_HERE,
-                             base::Bind(&base::debug::DumpWithoutCrashing));
-#endif
+#if !defined(NDEBUG)
+    StartupCrash();
+    return;
+#elif !defined(OS_ANDROID)
+    WatchDogThread::PostTask(FROM_HERE, base::Bind(&StartupCrash));
+    return;
+#else  // Android release: gather stats to figure out when to crash.
+    // TODO(rtenneti): Delete this code, after getting data.
+    UMA_HISTOGRAM_TIMES("StartupTimebomm.Alarm.TimeDuration",
+                        base::Time::Now() - start_time_clock_);
+    UMA_HISTOGRAM_TIMES("StartupTimebomm.Alarm.TimeTicksDuration",
+                        base::TimeTicks::Now() - start_time_monotonic_);
+    if (base::TimeTicks::IsThreadNowSupported()) {
+      UMA_HISTOGRAM_TIMES(
+          "StartupTimebomm.Alarm.ThreadNowDuration",
+          base::TimeTicks::ThreadNow() - start_time_thread_now_);
+    }
+    return;
+#endif  // OS_ANDROID
   }
+
+ private:
+#if defined(OS_ANDROID)
+  // TODO(rtenneti): Delete this code, after getting data.
+  base::Time start_time_clock_;
+  base::TimeTicks start_time_monotonic_;
+  base::TimeTicks start_time_thread_now_;
+#endif  // OS_ANDROID
 
   DISALLOW_COPY_AND_ASSIGN(StartupWatchDogThread);
 };
@@ -916,6 +951,7 @@ class ShutdownWatchDogThread : public base::Watchdog {
     ShutdownCrash();
   }
 
+ private:
   DISALLOW_COPY_AND_ASSIGN(ShutdownWatchDogThread);
 };
 }  // namespace
