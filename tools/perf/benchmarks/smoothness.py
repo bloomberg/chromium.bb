ed89a320d99e91e9d8dda5d05345bb8a48ba732a
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 02:13:20 +0000

    [Telemetry] Disable failing polymer smoothness benchmarks on Android.
    BUG=370725
    NOTRY=True
    TBR=dtu@chromium.org
    
    Review URL: https://codereview.chromium.org/272533002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268656 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ab8c365e2c1c1ecb1ebabd66859d94779d1efb18

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -124,7 +124,7 @@ class SmoothnessToughPinchZoomCases(test.Test):
   page_set = 'page_sets/tough_pinch_zoom_cases.py'
 
 
-@test.Enabled('android')
+@test.Disabled  # crbug.com/370725
 class SmoothnessPolymer(test.Test):
   """Measures rendering statistics for Polymer cases.
   """
@@ -132,7 +132,7 @@ class SmoothnessPolymer(test.Test):
   page_set = 'page_sets/polymer.py'
 
 
-@test.Enabled('android')
+@test.Disabled  # crbug.com/370725
 class SmoothnessFastPathPolymer(test.Test):
   """Measures rendering statistics for the Polymer cases without GPU
   rasterization using bleeding edge rendering fast paths.
@@ -144,7 +144,7 @@ class SmoothnessFastPathPolymer(test.Test):
     silk_flags.CustomizeBrowserOptionsForFastPath(options)
 
 
-@test.Enabled('android')
+@test.Disabled  # crbug.com/370725
 class SmoothnessGpuRasterizationPolymer(test.Test):
   """Measures rendering statistics for the Polymer cases with GPU rasterization
   """
@@ -155,7 +155,7 @@ class SmoothnessGpuRasterizationPolymer(test.Test):
     silk_flags.CustomizeBrowserOptionsForGpuRasterization(options)
 
 
-@test.Enabled('android')
+@test.Disabled  # crbug.com/370725
 class SmoothnessFastPathGpuRasterizationPolymer(
     SmoothnessGpuRasterizationPolymer):
   """Measures rendering statistics for the Polymer cases with GPU rasterization
