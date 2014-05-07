349fd1888ee16a61e96ea2618907b956f2f93b63
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 17:37:58 +0000

    Prefill sample page with #fragment hash when there is one. When we follow links from other chrome apps doc that has #fragment, we merely take users to the samples page, this change will also prefill the searchbox with the fragment so that the list of samples are already filtered by the fragment/query.
    BUG=None
    Test=Following links to samples page should filter the samples based
    on which link you clicked. e.g.
    from https://developer.chrome.com/extensions/webRequest, if you click
    on the link "web request samples":
    For more example code, see the <a>web request samples</a>.
    , it would take you to samples page which already filters out the page
    by webRequest related samples only.
    
    Review URL: https://codereview.chromium.org/268783007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268842 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5d6dbef2488ec32a81f4e6cba905097c3d5651a9

--- chrome/common/extensions/docs/static/js/samples.js
+++ chrome/common/extensions/docs/static/js/samples.js
@@ -5,9 +5,11 @@
 (function() {
   var searchBox = document.getElementById('search_input');
   var samples = document.getElementsByClassName('sample');
+  var SEARCH_PREFIX = 'search:';
 
   function filterSamples() {
     var searchText = searchBox.value.toLowerCase();
+    window.location.hash = SEARCH_PREFIX + encodeURIComponent(searchText);
     for (var i = 0; i < samples.length; ++i) {
       var sample = samples[i];
       var sampleTitle = '';
@@ -20,14 +22,24 @@
         sample.style.display = '';
     }
   }
+  function updateSearchBox(value) {
+    searchBox.value = value;
+    filterSamples();
+  }
   searchBox.addEventListener('search', filterSamples);
   searchBox.addEventListener('keyup', filterSamples);
 
   var apiFilterItems = document.getElementById('api_filter_items');
   apiFilterItems.addEventListener('click', function(event) {
     if (event.target instanceof HTMLAnchorElement) {
-      searchBox.value = event.target.innerText;
-      filterSamples();
+      updateSearchBox(event.target.innerText);
     }
   });
+
+  // If we have a #fragment that corresponds to a search, prefill the search box
+  // with it.
+  var fragment = window.location.hash.substr(1);
+  if (fragment.substr(0, SEARCH_PREFIX.length) == SEARCH_PREFIX) {
+    updateSearchBox(decodeURIComponent(fragment.substr(SEARCH_PREFIX.length)));
+  }
 })();
