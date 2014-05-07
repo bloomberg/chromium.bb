69aa0dc3fe8529c730864026d940a212ae84b08a
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-07 23:10:09 +0000

    [Telemetry] Disable crashing memory.tough_dom_memory_cases on Android.
    BUG=371153
    NOTRY=True
    TBR=dtu@chromium.org
    
    Review URL: https://codereview.chromium.org/270403004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@268990 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 36e99f3e42850647ae251648c3c8c6b4e251d8d3

--- tools/perf/benchmarks/memory.py
+++ tools/perf/benchmarks/memory.py
@@ -23,6 +23,7 @@ class Reload2012Q3(test.Test):
   page_set = 'page_sets/top_desktop_sites_2012Q3.py'
 
 
+@test.Disabled('android')  # crbug.com/371153
 class MemoryToughDomMemoryCases(test.Test):
   test = memory.Memory
   page_set = 'page_sets/tough_dom_memory_cases.py'
