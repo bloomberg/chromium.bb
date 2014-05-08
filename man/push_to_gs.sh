153369385408055a7ed1accb479d68bff42016cf
iannucci@chromium.org <iannucci@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-08 20:22:22 +0000

    Fix typo in push_to_gs.sh which causes incorrect upload.
    R=vadimsh@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/267393015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@269082 0039d316-1c4b-4281-b951-d872f2087c98

--- man/push_to_gs.sh
+++ man/push_to_gs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 MY_DIR=$(dirname "$(readlink "$0")")
-gsutil cp -r -z html -a public-read  "$MY_DIR/html" gs://chrome-infra-docs/flat/depot_tools/docs/html
+gsutil cp -r -z html -a public-read  "$MY_DIR/html" gs://chrome-infra-docs/flat/depot_tools/docs/
