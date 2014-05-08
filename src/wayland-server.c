6e8a6624030df0ac7656f8743ca537c1414ada0f
Jasper St. Pierre <jstpierre@mecheye.net>
2014-05-08 10:24:06 -0400

    server: Create the socket FD after taking the lock
    We're going to split out the lock-taking to another function so we
    can repetitively try locks.

--- src/wayland-server.c
+++ src/wayland-server.c
@@ -1083,12 +1083,6 @@ wl_display_add_socket(struct wl_display *display, const char *name)
 	if (s == NULL)
 		return -1;
 
-	s->fd = wl_os_socket_cloexec(PF_LOCAL, SOCK_STREAM, 0);
-	if (s->fd < 0) {
-		wl_socket_destroy(s);
-		return -1;
-	}
-
 	if (name == NULL)
 		name = getenv("WAYLAND_DISPLAY");
 	if (name == NULL)
@@ -1111,6 +1105,12 @@ wl_display_add_socket(struct wl_display *display, const char *name)
 
 	s->fd_lock = get_socket_lock(s);
 	if (s->fd_lock < 0) {
+		wl_socket_destroy(s);
+		return -1;
+	}
+
+	s->fd = wl_os_socket_cloexec(PF_LOCAL, SOCK_STREAM, 0);
+	if (s->fd < 0) {
 		wl_socket_destroy(s);
 		return -1;
 	}
