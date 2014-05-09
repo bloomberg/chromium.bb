9a2d1ae5aa1dc83d2ee62ef6da612d9a017d2fe2
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 15:06:33 +0000

    Cleanup: Move pieces of ManagePasswordItemView into an anonymous namespace.
    A few methods on ManagePasswordItemView don't need to be on the object at
    all, as they don't depend on any of its parts. We can safely move them to
    an anonymous namespace to clean up the header.
    
    BUG=261628
    
    Review URL: https://codereview.chromium.org/273593007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269304 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c0af90aec0d1ccedb3ede85d85cf33d5b3fca4a3

--- chrome/browser/ui/views/passwords/manage_password_item_view.cc
+++ chrome/browser/ui/views/passwords/manage_password_item_view.cc
@@ -48,126 +48,9 @@ int SecondFieldWidth() {
           .width());
 }
 
-}  // namespace
+enum ColumnSets { TWO_COLUMN_SET = 0, THREE_COLUMN_SET };
 
-// Pending View
-ManagePasswordItemView::PendingView::PendingView(
-    ManagePasswordItemView* parent) {
-  views::GridLayout* layout = new views::GridLayout(this);
-  SetLayoutManager(layout);
-
-  parent->BuildColumnSet(layout, TWO_COLUMN_SET);
-  layout->StartRowWithPadding(
-      0, TWO_COLUMN_SET, 0, views::kRelatedControlVerticalSpacing);
-  layout->AddView(parent->GenerateUsernameLabel());
-  layout->AddView(parent->GeneratePasswordLabel());
-  layout->AddPaddingRow(0, views::kRelatedControlVerticalSpacing);
-}
-
-ManagePasswordItemView::PendingView::~PendingView() {}
-
-// Manage View
-ManagePasswordItemView::ManageView::ManageView(ManagePasswordItemView* parent)
-    : parent_(parent) {
-  views::GridLayout* layout = new views::GridLayout(this);
-  SetLayoutManager(layout);
-
-  ui::ResourceBundle* rb = &ui::ResourceBundle::GetSharedInstance();
-  delete_button_ = new views::ImageButton(this);
-  delete_button_->SetImage(views::ImageButton::STATE_NORMAL,
-                           rb->GetImageNamed(IDR_CLOSE_2).ToImageSkia());
-  delete_button_->SetImage(views::ImageButton::STATE_HOVERED,
-                           rb->GetImageNamed(IDR_CLOSE_2_H).ToImageSkia());
-  delete_button_->SetImage(views::ImageButton::STATE_PRESSED,
-                           rb->GetImageNamed(IDR_CLOSE_2_P).ToImageSkia());
-
-  parent->BuildColumnSet(layout, THREE_COLUMN_SET);
-  layout->StartRowWithPadding(
-      0, THREE_COLUMN_SET, 0, views::kRelatedControlVerticalSpacing);
-  layout->AddView(parent->GenerateUsernameLabel());
-  layout->AddView(parent->GeneratePasswordLabel());
-  layout->AddView(delete_button_);
-  layout->AddPaddingRow(0, views::kRelatedControlVerticalSpacing);
-}
-
-void ManagePasswordItemView::ManageView::ButtonPressed(views::Button* sender,
-                                                       const ui::Event& event) {
-  DCHECK_EQ(delete_button_, sender);
-  parent_->NotifyClickedDelete();
-}
-
-ManagePasswordItemView::ManageView::~ManageView() {}
-
-// Undo View
-ManagePasswordItemView::UndoView::UndoView(ManagePasswordItemView* parent)
-    : parent_(parent) {
-  views::GridLayout* layout = new views::GridLayout(this);
-  SetLayoutManager(layout);
-
-  views::Label* text =
-      new views::Label(l10n_util::GetStringUTF16(IDS_MANAGE_PASSWORDS_DELETED));
-  text->SetHorizontalAlignment(gfx::ALIGN_LEFT);
-  text->SetFontList(ui::ResourceBundle::GetSharedInstance().GetFontList(
-      ui::ResourceBundle::SmallFont));
-
-  undo_link_ =
-      new views::Link(l10n_util::GetStringUTF16(IDS_MANAGE_PASSWORDS_UNDO));
-  undo_link_->SetHorizontalAlignment(gfx::ALIGN_RIGHT);
-  undo_link_->set_listener(this);
-  undo_link_->SetUnderline(false);
-  undo_link_->SetFontList(ui::ResourceBundle::GetSharedInstance().GetFontList(
-      ui::ResourceBundle::SmallFont));
-
-  parent->BuildColumnSet(layout, TWO_COLUMN_SET);
-  layout->StartRowWithPadding(
-      0, TWO_COLUMN_SET, 0, views::kRelatedControlVerticalSpacing);
-  layout->AddView(text);
-  layout->AddView(undo_link_);
-  layout->AddPaddingRow(0, views::kRelatedControlVerticalSpacing);
-}
-
-void ManagePasswordItemView::UndoView::LinkClicked(views::Link* sender,
-                                                   int event_flags) {
-  DCHECK_EQ(undo_link_, sender);
-  parent_->NotifyClickedUndo();
-}
-
-ManagePasswordItemView::UndoView::~UndoView() {}
-
-// ManagePasswordItemView
-ManagePasswordItemView::ManagePasswordItemView(
-    ManagePasswordsBubbleModel* manage_passwords_bubble_model,
-    autofill::PasswordForm password_form,
-    Position position)
-    : model_(manage_passwords_bubble_model),
-      password_form_(password_form),
-      delete_password_(false) {
-  views::FillLayout* layout = new views::FillLayout();
-  SetLayoutManager(layout);
-
-  // When a password is displayed as the first item in a list, it has borders
-  // on both the top and bottom. When it's in the middle of a list, or at the
-  // end, it has a border only on the bottom.
-  SetBorder(views::Border::CreateSolidSidedBorder(
-      position == FIRST_ITEM ? 1 : 0,
-      0,
-      1,
-      0,
-      GetNativeTheme()->GetSystemColor(
-          ui::NativeTheme::kColorId_EnabledMenuButtonBorderColor)));
-
-  if (password_manager::ui::IsPendingState(model_->state())) {
-    AddChildView(new PendingView(this));
-  } else {
-    AddChildView(new ManageView(this));
-  }
-  GetLayoutManager()->Layout(this);
-}
-
-ManagePasswordItemView::~ManagePasswordItemView() {}
-
-void ManagePasswordItemView::BuildColumnSet(views::GridLayout* layout,
-                                            int column_set_id) {
+void BuildColumnSet(views::GridLayout* layout, int column_set_id) {
   views::ColumnSet* column_set = layout->AddColumnSet(column_set_id);
 
   // The username/"Deleted!" field.
@@ -201,21 +84,143 @@ void ManagePasswordItemView::BuildColumnSet(views::GridLayout* layout,
   column_set->AddPaddingColumn(0, views::kItemLabelSpacing);
 }
 
-views::Label* ManagePasswordItemView::GenerateUsernameLabel() const {
-  views::Label* label = new views::Label(password_form_.username_value);
+views::Label* GenerateUsernameLabel(const autofill::PasswordForm& form) {
+  views::Label* label = new views::Label(form.username_value);
   label->SetFontList(ui::ResourceBundle::GetSharedInstance().GetFontList(
       ui::ResourceBundle::SmallFont));
   label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   return label;
 }
 
-views::Label* ManagePasswordItemView::GeneratePasswordLabel() const {
-  views::Label* label = new views::Label(password_form_.password_value);
+views::Label* GeneratePasswordLabel(const autofill::PasswordForm& form) {
+  views::Label* label = new views::Label(form.password_value);
   label->SetFontList(ui::ResourceBundle::GetSharedInstance().GetFontList(
       ui::ResourceBundle::SmallFont));
   label->SetHorizontalAlignment(gfx::ALIGN_LEFT);
   label->SetObscured(true);
   return label;
+}
+
+}  // namespace
+
+// Pending View
+ManagePasswordItemView::PendingView::PendingView(
+    ManagePasswordItemView* parent) {
+  views::GridLayout* layout = new views::GridLayout(this);
+  SetLayoutManager(layout);
+
+  BuildColumnSet(layout, TWO_COLUMN_SET);
+  layout->StartRowWithPadding(
+      0, TWO_COLUMN_SET, 0, views::kRelatedControlVerticalSpacing);
+  layout->AddView(GenerateUsernameLabel(parent->password_form_));
+  layout->AddView(GeneratePasswordLabel(parent->password_form_));
+  layout->AddPaddingRow(0, views::kRelatedControlVerticalSpacing);
+}
+
+ManagePasswordItemView::PendingView::~PendingView() {
+}
+
+// Manage View
+ManagePasswordItemView::ManageView::ManageView(ManagePasswordItemView* parent)
+    : parent_(parent) {
+  views::GridLayout* layout = new views::GridLayout(this);
+  SetLayoutManager(layout);
+
+  ui::ResourceBundle* rb = &ui::ResourceBundle::GetSharedInstance();
+  delete_button_ = new views::ImageButton(this);
+  delete_button_->SetImage(views::ImageButton::STATE_NORMAL,
+                           rb->GetImageNamed(IDR_CLOSE_2).ToImageSkia());
+  delete_button_->SetImage(views::ImageButton::STATE_HOVERED,
+                           rb->GetImageNamed(IDR_CLOSE_2_H).ToImageSkia());
+  delete_button_->SetImage(views::ImageButton::STATE_PRESSED,
+                           rb->GetImageNamed(IDR_CLOSE_2_P).ToImageSkia());
+
+  BuildColumnSet(layout, THREE_COLUMN_SET);
+  layout->StartRowWithPadding(
+      0, THREE_COLUMN_SET, 0, views::kRelatedControlVerticalSpacing);
+  layout->AddView(GenerateUsernameLabel(parent->password_form_));
+  layout->AddView(GeneratePasswordLabel(parent->password_form_));
+  layout->AddView(delete_button_);
+  layout->AddPaddingRow(0, views::kRelatedControlVerticalSpacing);
+}
+
+void ManagePasswordItemView::ManageView::ButtonPressed(views::Button* sender,
+                                                       const ui::Event& event) {
+  DCHECK_EQ(delete_button_, sender);
+  parent_->NotifyClickedDelete();
+}
+
+ManagePasswordItemView::ManageView::~ManageView() {
+}
+
+// Undo View
+ManagePasswordItemView::UndoView::UndoView(ManagePasswordItemView* parent)
+    : parent_(parent) {
+  views::GridLayout* layout = new views::GridLayout(this);
+  SetLayoutManager(layout);
+
+  views::Label* text =
+      new views::Label(l10n_util::GetStringUTF16(IDS_MANAGE_PASSWORDS_DELETED));
+  text->SetHorizontalAlignment(gfx::ALIGN_LEFT);
+  text->SetFontList(ui::ResourceBundle::GetSharedInstance().GetFontList(
+      ui::ResourceBundle::SmallFont));
+
+  undo_link_ =
+      new views::Link(l10n_util::GetStringUTF16(IDS_MANAGE_PASSWORDS_UNDO));
+  undo_link_->SetHorizontalAlignment(gfx::ALIGN_RIGHT);
+  undo_link_->set_listener(this);
+  undo_link_->SetUnderline(false);
+  undo_link_->SetFontList(ui::ResourceBundle::GetSharedInstance().GetFontList(
+      ui::ResourceBundle::SmallFont));
+
+  BuildColumnSet(layout, TWO_COLUMN_SET);
+  layout->StartRowWithPadding(
+      0, TWO_COLUMN_SET, 0, views::kRelatedControlVerticalSpacing);
+  layout->AddView(text);
+  layout->AddView(undo_link_);
+  layout->AddPaddingRow(0, views::kRelatedControlVerticalSpacing);
+}
+
+void ManagePasswordItemView::UndoView::LinkClicked(views::Link* sender,
+                                                   int event_flags) {
+  DCHECK_EQ(undo_link_, sender);
+  parent_->NotifyClickedUndo();
+}
+
+ManagePasswordItemView::UndoView::~UndoView() {
+}
+
+// ManagePasswordItemView
+ManagePasswordItemView::ManagePasswordItemView(
+    ManagePasswordsBubbleModel* manage_passwords_bubble_model,
+    autofill::PasswordForm password_form,
+    Position position)
+    : model_(manage_passwords_bubble_model),
+      password_form_(password_form),
+      delete_password_(false) {
+  views::FillLayout* layout = new views::FillLayout();
+  SetLayoutManager(layout);
+
+  // When a password is displayed as the first item in a list, it has borders
+  // on both the top and bottom. When it's in the middle of a list, or at the
+  // end, it has a border only on the bottom.
+  SetBorder(views::Border::CreateSolidSidedBorder(
+      position == FIRST_ITEM ? 1 : 0,
+      0,
+      1,
+      0,
+      GetNativeTheme()->GetSystemColor(
+          ui::NativeTheme::kColorId_EnabledMenuButtonBorderColor)));
+
+  if (password_manager::ui::IsPendingState(model_->state())) {
+    AddChildView(new PendingView(this));
+  } else {
+    AddChildView(new ManageView(this));
+  }
+  GetLayoutManager()->Layout(this);
+}
+
+ManagePasswordItemView::~ManagePasswordItemView() {
 }
 
 void ManagePasswordItemView::Refresh() {
--- chrome/browser/ui/views/passwords/manage_password_item_view.h
+++ chrome/browser/ui/views/passwords/manage_password_item_view.h
@@ -48,7 +48,6 @@ class ManagePasswordItemView : public views::View {
                                const ui::Event& event) OVERRIDE;
 
     views::ImageButton* delete_button_;
-
     ManagePasswordItemView* parent_;
   };
 
@@ -65,7 +64,6 @@ class ManagePasswordItemView : public views::View {
     virtual void LinkClicked(views::Link* source, int event_flags) OVERRIDE;
 
     views::Link* undo_link_;
-
     ManagePasswordItemView* parent_;
   };
 
@@ -77,15 +75,7 @@ class ManagePasswordItemView : public views::View {
       Position position);
 
  private:
-  enum ColumnSets { TWO_COLUMN_SET = 0, THREE_COLUMN_SET };
-
   virtual ~ManagePasswordItemView();
-
-  views::Label* GenerateUsernameLabel() const;
-  views::Label* GeneratePasswordLabel() const;
-
-  // Build a two-label column set.
-  void BuildColumnSet(views::GridLayout*, int column_set_id);
 
   void NotifyClickedDelete();
   void NotifyClickedUndo();
