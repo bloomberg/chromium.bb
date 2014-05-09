3703260483b59d5170c7c4f5f5ad365a4a6f97da
namnguyen@chromium.org <namnguyen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 00:07:07 +0000

    base: Add __ARM_ARCH_6ZK__ to atomic ops
    ChromiumOS fails to compile libbase on Raspberry Pi because it defines
    __ARM_ARCH_6ZK__, not among any of the defines we use to test for armv6.
    
    BUG=chromium:369131
    TEST=build okay
    R=thakis@chromium.org, digit@chromium.org
    
    Review URL: https://codereview.chromium.org/273553002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269117 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 780492125b123804f3dfa353eaf19d35f2c908e6

--- base/atomicops_internals_arm_gcc.h
+++ base/atomicops_internals_arm_gcc.h
@@ -63,7 +63,7 @@ inline void MemoryBarrier() {
     defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || \
     defined(__ARM_ARCH_6__) || defined(__ARM_ARCH_6J__) || \
     defined(__ARM_ARCH_6K__) || defined(__ARM_ARCH_6Z__) || \
-    defined(__ARM_ARCH_6KZ__) || defined(__ARM_ARCH_6T2__)
+    defined(__ARM_ARCH_6ZK__) || defined(__ARM_ARCH_6T2__)
 
 inline Atomic32 NoBarrier_CompareAndSwap(volatile Atomic32* ptr,
                                          Atomic32 old_value,
