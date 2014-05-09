cac0764c8968b1dfa767c3ee9558e5219d590574
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 02:07:58 +0000

    Revert of Reland another small part of r267947. (https://codereview.chromium.org/268363014/)
    Reason for revert:
    Broke Linux Asan
    http://build.chromium.org/p/chromium.memory/builders/Linux%20ASan%20LSan%20Tests%20%282%29/builds/2440/steps/content_unittests/logs/DeferredLoadTest
    
    Original issue's description:
    > Reland another small part of r267947.
    > 
    > This just contains only the download_to_file chuck from r267947 which is
    > unlikely to break any layout tests, existing unit tests or chrome production
    > code.
    > 
    > Tests ran: both content_shell and content_unittests
    > 
    > BUG=265753, 338338, 237249
    > R=jam@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=269129
    
    TBR=jam@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=265753, 338338, 237249
    
    Review URL: https://codereview.chromium.org/273973002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269148 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5e18947cf672190030cbffadc967f16ccf25e817

--- content/child/resource_dispatcher.cc
+++ content/child/resource_dispatcher.cc
@@ -47,7 +47,10 @@ void RemoteToLocalTimeTicks(
   *time = converter.ToLocalTimeTicks(remote_time).ToTimeTicks();
 }
 
-void CrashOnMapFailure() {
+
+}  // namespace
+
+static void CrashOnMapFailure() {
 #if defined(OS_WIN)
   DWORD last_err = GetLastError();
   base::debug::Alias(&last_err);
@@ -56,15 +59,13 @@ void CrashOnMapFailure() {
 }
 
 // Each resource request is assigned an ID scoped to this process.
-int MakeRequestID() {
+static int MakeRequestID() {
   // NOTE: The resource_dispatcher_host also needs probably unique
   // request_ids, so they count down from -2 (-1 is a special we're
   // screwed value), while the renderer process counts up.
   static int next_request_id = 0;
   return next_request_id++;
 }
-
-}  // namespace
 
 // ResourceLoaderBridge implementation ----------------------------------------
 
@@ -162,6 +163,10 @@ IPCResourceLoaderBridge::~IPCResourceLoaderBridge() {
     // removed us when the renderer sends the ReceivedAllData message.
     dispatcher_->RemovePendingRequest(request_id_);
 
+    if (request_.download_to_file) {
+      dispatcher_->message_sender()->Send(
+          new ResourceHostMsg_ReleaseDownloadedFile(request_id_));
+    }
   }
 }
 
@@ -185,8 +190,7 @@ bool IPCResourceLoaderBridge::Start(RequestPeer* peer) {
                                                request_.resource_type,
                                                request_.origin_pid,
                                                frame_origin_,
-                                               request_.url,
-                                               request_.download_to_file);
+                                               request_.url);
 
   return dispatcher_->message_sender()->Send(
       new ResourceHostMsg_RequestResource(routing_id_, request_id_, request_));
@@ -557,16 +561,11 @@ int ResourceDispatcher::AddPendingRequest(RequestPeer* callback,
                                           ResourceType::Type resource_type,
                                           int origin_pid,
                                           const GURL& frame_origin,
-                                          const GURL& request_url,
-                                          bool download_to_file) {
+                                          const GURL& request_url) {
   // Compute a unique request_id for this renderer process.
   int id = MakeRequestID();
-  pending_requests_[id] = PendingRequestInfo(callback,
-                                             resource_type,
-                                             origin_pid,
-                                             frame_origin,
-                                             request_url,
-                                             download_to_file);
+  pending_requests_[id] = PendingRequestInfo(
+      callback, resource_type, origin_pid, frame_origin, request_url);
   return id;
 }
 
@@ -578,11 +577,6 @@ bool ResourceDispatcher::RemovePendingRequest(int request_id) {
   PendingRequestInfo& request_info = it->second;
   ReleaseResourcesInMessageQueue(&request_info.deferred_message_queue);
   pending_requests_.erase(it);
-
-  if (request_info.download_to_file) {
-    message_sender_->Send(
-        new ResourceHostMsg_ReleaseDownloadedFile(request_id));
-  }
 
   return true;
 }
@@ -621,10 +615,9 @@ void ResourceDispatcher::SetDefersLoading(int request_id, bool value) {
   }
 }
 
-void ResourceDispatcher::DidChangePriority(int routing_id,
-                                           int request_id,
-                                           net::RequestPriority new_priority,
-                                           int intra_priority_value) {
+void ResourceDispatcher::DidChangePriority(
+    int routing_id, int request_id, net::RequestPriority new_priority,
+    int intra_priority_value) {
   DCHECK(ContainsKey(pending_requests_, request_id));
   message_sender()->Send(new ResourceHostMsg_DidChangePriority(
       request_id, new_priority, intra_priority_value));
@@ -634,7 +627,6 @@ ResourceDispatcher::PendingRequestInfo::PendingRequestInfo()
     : peer(NULL),
       resource_type(ResourceType::SUB_RESOURCE),
       is_deferred(false),
-      download_to_file(false),
       blocked_response(false),
       buffer_size(0) {
 }
@@ -644,8 +636,7 @@ ResourceDispatcher::PendingRequestInfo::PendingRequestInfo(
     ResourceType::Type resource_type,
     int origin_pid,
     const GURL& frame_origin,
-    const GURL& request_url,
-    bool download_to_file)
+    const GURL& request_url)
     : peer(peer),
       resource_type(resource_type),
       origin_pid(origin_pid),
@@ -653,7 +644,6 @@ ResourceDispatcher::PendingRequestInfo::PendingRequestInfo(
       url(request_url),
       frame_origin(frame_origin),
       response_url(request_url),
-      download_to_file(download_to_file),
       request_start(base::TimeTicks::Now()),
       blocked_response(false) {}
 
