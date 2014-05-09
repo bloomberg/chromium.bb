00bf17d2a154878ab408faac3b9bcfdd13e0eab8
kaliamoorthi@chromium.org <kaliamoorthi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-09 16:16:42 +0000

    Modifies presubmit script to acknowledge suffix in policy test case
    The CL modifies the presubmit script to acknowledge that some policy test cases could have a suffix added to it.
    
    BUG=371277
    
    Review URL: https://codereview.chromium.org/271863002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@269316 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6651fb2ffe02472d523aca705907324b067ef586

--- components/policy/resources/PRESUBMIT.py
+++ components/policy/resources/PRESUBMIT.py
@@ -52,7 +52,9 @@ def _CheckPolicyTestCases(input_api, output_api, policies):
   policy_test_cases_file = input_api.os_path.join(
       root, 'chrome', 'test', 'data', 'policy', 'policy_test_cases.json')
   test_names = input_api.json.load(open(policy_test_cases_file)).keys()
-  tested_policies = frozenset(name for name in test_names if name[:2] != '--')
+  tested_policies = frozenset(name.partition('.')[0]
+                              for name in test_names
+                              if name[:2] != '--')
   policy_names = frozenset(policy['name'] for policy in policies)
 
   # Finally check if any policies are missing.
