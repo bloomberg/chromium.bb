619b6f56785a224c7f7a34cbe1a42e68a11d2295
Michael Whapples <mwhapples@aim.com>
2014-05-12 22:24:53 +0100

    Restored some code accidentally removed and removal of some nonsense code.

--- liblouis/compileTranslationTable.c
+++ liblouis/compileTranslationTable.c
@@ -69,6 +69,56 @@ strdupWrapper (const char *string)
 }
 
 
+char *EXPORT_CALL
+lou_getProgramPath ()
+{
+  char *path = NULL;
+  HMODULE handle;
+
+  if ((handle = GetModuleHandle (NULL)))
+    {
+      size_t size = 0X80;
+      char *buffer = NULL;
+
+      while (1)
+	{
+	  buffer = reallocWrapper (buffer, size <<= 1);
+
+	  {
+	    DWORD length = GetModuleFileName (handle, buffer, size);
+
+	    if (!length)
+	      {
+		printf ("GetModuleFileName\n");
+		exit (3);
+	      }
+
+	    if (length < size)
+	      {
+		buffer[length] = 0;
+		path = strdupWrapper (buffer);
+
+		while (length > 0)
+		  if (path[--length] == '\\')
+		    break;
+
+		strncpy (path, path, length + 1);
+		path[length + 1] = '\0';
+		break;
+	      }
+	  }
+	}
+
+      free (buffer);
+    }
+  else
+    {
+      printf ("GetModuleHandle\n");
+      exit (3);
+    }
+
+  return path;
+}
 
 #define PATH_SEP ';'
 #define DIR_SEP '\\'
