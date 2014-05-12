7272885b8c6044c04fbdb4aa878535a0a40aca98
Michael Whapples <mwhapples@aim.com>
2014-05-12 13:54:43 +0100

    Changed a printf statement to logging

--- liblouis/compileTranslationTable.c
+++ liblouis/compileTranslationTable.c
@@ -68,57 +68,7 @@ strdupWrapper (const char *string)
   return address;
 }
 
-char *EXPORT_CALL
-lou_getProgramPath ()
-{
-  char *path = NULL;
-  HMODULE handle;
 
-  if ((handle = GetModuleHandle (NULL)))
-    {
-      size_t size = 0X80;
-      char *buffer = NULL;
-
-      while (1)
-	{
-	  buffer = reallocWrapper (buffer, size <<= 1);
-
-	  {
-	    DWORD length = GetModuleFileName (handle, buffer, size);
-
-	    if (!length)
-	      {
-		printf ("GetModuleFileName\n");
-		exit (3);
-		3;
-	      }
-
-	    if (length < size)
-	      {
-		buffer[length] = 0;
-		path = strdupWrapper (buffer);
-
-		while (length > 0)
-		  if (path[--length] == '\\')
-		    break;
-
-		strncpy (path, path, length + 1);
-		path[length + 1] = '\0';
-		break;
-	      }
-	  }
-	}
-
-      free (buffer);
-    }
-  else
-    {
-      printf ("GetModuleHandle\n");
-      exit (3);
-    }
-
-  return path;
-}
 
 #define PATH_SEP ';'
 #define DIR_SEP '\\'
@@ -131,8 +81,7 @@ lou_getProgramPath ()
 void
 outOfMemory ()
 {
-  fprintf (stderr,
-	   "liblouis: Insufficient memory\n");
+  lou_log(LOG_FATAL, "liblouis: Insufficient memory\n");
   exit (3);
 }
 
