a3c0e5694b488b352b2ad1b6b19764c8e4a37951
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-13 15:05:31 +0000

    ServiceWorker: Ensure getServiced() resolved with JS array
    Just an additional test assertion.
    
    BUG=372749
    R=kinuko@chromium.org
    
    Review URL: https://codereview.chromium.org/283673004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@173962 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 9c4d483baeefc72e79f27d4322a5fb9679a3c446

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/postmessage-to-client-worker.js
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/postmessage-to-client-worker.js
@@ -2,6 +2,8 @@ self.onmessage = function(e) {
     self.clients.getServiced().then(function(clients) {
         clients.forEach(function(client) {
             client.postMessage('Sending message via clients');
+            if (!Array.isArray(clients))
+                client.postMessage('clients is not an array');
             client.postMessage('quit');
         });
     });
