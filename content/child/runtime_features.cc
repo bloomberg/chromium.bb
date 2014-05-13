d617cef4f3094e9931db16e1be3718609fbbf41b
chrishtr@chromium.org <chrishtr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 03:46:23 +0000

    Turn on the squashing runtime setting if specified on the commandline.
    BUG=
    
    Review URL: https://codereview.chromium.org/282653002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270002 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 89cc962f1946acc4714eec90228f7b3e5214323b

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -155,6 +155,9 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
 
   if (command_line.HasSwitch(switches::kEnablePreciseMemoryInfo))
     WebRuntimeFeatures::enablePreciseMemoryInfo(true);
+
+  if (command_line.HasSwitch(switches::kEnableLayerSquashing))
+    WebRuntimeFeatures::enableLayerSquashing(true);
 }
 
 }  // namespace content
