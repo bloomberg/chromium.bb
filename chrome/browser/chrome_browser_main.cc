20cd4a866f3ca2e150aeb155f0543694e9ab9e1b
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-13 15:57:47 +0000

    DevTools: allow binding remote debugging to port 0.
    BUG=372560
    
    Review URL: https://codereview.chromium.org/288493007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270108 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3afdc4682bdc546ed2d6622c272b5198060e4d59

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -457,7 +457,7 @@ void LaunchDevToolsHandlerIfNeeded(const CommandLine& command_line) {
     std::string port_str =
         command_line.GetSwitchValueASCII(::switches::kRemoteDebuggingPort);
     int port;
-    if (base::StringToInt(port_str, &port) && port > 0 && port < 65535) {
+    if (base::StringToInt(port_str, &port) && port >= 0 && port < 65535) {
       g_browser_process->CreateDevToolsHttpProtocolHandler(
           chrome::HOST_DESKTOP_TYPE_NATIVE,
           "127.0.0.1",
