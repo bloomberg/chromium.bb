fe797ee9afc9bc8a0cefe505daaccc39b7014394
yefim@chromium.org <yefim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-14 01:55:24 +0000

    Restored an option to enable enhanced bookmarks experiment from Finch. It was removed when we switched to Chrome sync enabled experiment. Now we need both Chrome sync experiment for logged in users and Finch for the rest.
    BUG=321393
    TBR=rlarocque
    
    Review URL: https://codereview.chromium.org/265843009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270288 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: edf82d1c54334d45226bbb91d2cb51cd000f7b6f

--- chrome/browser/bookmarks/enhanced_bookmarks_features.h
+++ chrome/browser/bookmarks/enhanced_bookmarks_features.h
@@ -11,18 +11,37 @@
 
 class PrefService;
 
+// States for bookmark experiment. They are set by Chrome sync into
+// sync_driver::prefs::kEnhancedBookmarksExperimentEnabled user preference and
+// used for UMA reporting as well.
 enum BookmarksExperimentState {
   kNoBookmarksExperiment,
   kBookmarksExperimentEnabled,
-  kBookmarksExperimentEnabledUserOptOut
+  kBookmarksExperimentEnabledUserOptOut,
+  kBookmarksExperimentEnabledFromFinch,
+  kBookmarksExperimentOptOutFromFinch,
+  kBookmarksExperimentEnabledFromFinchUserSignedIn,
+  kBookmarksExperimentEnumSize
 };
 
+// Returns true and sets |extension_id| if bookmarks experiment enabled
+//         false if no bookmark experiment or extension id is empty.
+bool GetBookmarksExperimentExtensionID(const PrefService* user_prefs,
+                                       std::string* extension_id);
+
+// Updates bookmark experiment state based on information from Chrome sync
+// and Finch experiments.
+void UpdateBookmarksExperimentState(const PrefService* user_prefs,
+                                    PrefService* local_state,
+                                    bool user_signed_in);
+
 // Sets flag to opt-in user into Finch experiment.
-void UpdateBookmarksExperiment(
+void ForceFinchBookmarkExperimentIfNeeded(
     PrefService* local_state,
     BookmarksExperimentState bookmarks_experiment_state);
 
 // Returns true if enhanced bookmarks experiment is enabled.
+// Experiment could be enable from Chrome sync or from Finch.
 bool IsEnhancedBookmarksExperimentEnabled();
 
 // Returns true when flag enable-dom-distiller is set or enabled from Finch.
@@ -30,8 +49,5 @@ bool IsEnableDomDistillerSet();
 
 // Returns true when flag enable-sync-articles is set or enabled from Finch.
 bool IsEnableSyncArticlesSet();
-
-// Get extension id from Finch EnhancedBookmarks group parameters.
-std::string GetEnhancedBookmarksExtensionIdFromFinch();
 
 #endif  // CHROME_BROWSER_BOOKMARKS_ENHANCED_BOOKMARKS_FEATURES_H_
