f6b4ef24e09b7fac300a1904c8682b0a4945d7d6
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-15 20:32:14 +0000

    Revert 270766 "[telemetry] Make testMeasurementSmoke generate se..."
    Causing Android Tests failure in telemetry_perf_unittests.
    
    > [telemetry] Make testMeasurementSmoke generate separate tests.
    > 
    > This way failures in one benchmark won't stop the rest of the tests, and it'll print separate GTest steps for each benchmark.
    > The test names follow the format BenchmarkSmokeTest.benchmark.name so you can still find BenchmarkSmokeTest with a search.
    > 
    > Also:
    > - Rename it to the more apt BenchmarkSmoke.
    > - Print Chrome stack trace when there's a tab crash at Chrome startup.
    > 
    > 
    > BUG=368401,371613,373385
    > TEST=tools/perf/run_tests benchmark
    > NOTRY=true
    > 
    > Review URL: https://codereview.chromium.org/287773002
    
    TBR=dtu@chromium.org
    
    Review URL: https://codereview.chromium.org/289053002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@270798 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 27b57935d1aff03999cbbb89ca7e5a912c453373

---
+++ tools/perf/measurements/measurement_unittest.py
@@ -0,0 +1,73 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import os
+import unittest
+
+from telemetry import test
+from telemetry.core import discover
+from telemetry.page import page_measurement
+from telemetry.unittest import options_for_unittests
+
+
+class MeasurementUnitTest(unittest.TestCase):
+
+  # TODO(achuith): Fix crbug.com/351114.
+  @test.Disabled('android', 'chromeos', 'win')
+  def testMeasurementSmoke(self):
+    # Run all Measurements against the first Page in the PageSet of the first
+    # Benchmark that uses them.
+    #
+    # Ideally this test would be comprehensive, but the above serves as a
+    # kind of smoke test.
+    measurements_dir = os.path.dirname(__file__)
+    top_level_dir = os.path.dirname(measurements_dir)
+    benchmarks_dir = os.path.join(top_level_dir, 'benchmarks')
+
+    all_measurements = discover.DiscoverClasses(
+        measurements_dir, top_level_dir, page_measurement.PageMeasurement,
+        pattern='*.py').values()
+    all_benchmarks = discover.DiscoverClasses(
+        benchmarks_dir, top_level_dir, test.Test, pattern='*.py').values()
+
+    for benchmark in all_benchmarks:
+      if benchmark.test not in all_measurements:
+        # If the benchmark is not in measurements, then it is not composable.
+        # Ideally we'd like to test these as well, but the non-composable
+        # benchmarks are usually long-running benchmarks.
+        continue
+
+      if hasattr(benchmark, 'generated_profile_archive'):
+        # We'd like to test these, but don't know how yet.
+        continue
+
+      # Only measure a single page so that this test cycles reasonably quickly.
+      benchmark.options['pageset_repeat'] = 1
+      benchmark.options['page_repeat'] = 1
+
+      class SinglePageBenchmark(benchmark):  # pylint: disable=W0232
+        def CreatePageSet(self, options):
+          # pylint: disable=E1002
+          ps = super(SinglePageBenchmark, self).CreatePageSet(options)
+          ps.pages = ps.pages[:1]
+          return ps
+
+      logging.info('running: %s', benchmark)
+
+      # Set the benchmark's default arguments.
+      options = options_for_unittests.GetCopy()
+      options.output_format = 'none'
+      parser = options.CreateParser()
+
+      benchmark.AddCommandLineArgs(parser)
+      test.AddCommandLineArgs(parser)
+      benchmark.SetArgumentDefaults(parser)
+      options.MergeDefaultValues(parser.get_default_values())
+
+      benchmark.ProcessCommandLineArgs(None, options)
+      test.ProcessCommandLineArgs(None, options)
+
+      self.assertEqual(0, SinglePageBenchmark().Run(options),
+                       msg='Failed: %s' % benchmark)
