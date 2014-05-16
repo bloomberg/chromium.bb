c47a00fe3572aa50c023d9d343843329e11535b2
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-16 21:11:19 +0000

    Revert of Fix selection of label having control element. (https://codereview.chromium.org/258933006/)
    Reason for revert:
    Causes bug 370859
    
    BUG=148912,370859
    TBR=keishi
    
    Original issue's description:
    > Fix selection of label having control element.
    >
    > Any click event on label goes to its control element. So, if
    > we tried to select the text inside label by dragging the mouse
    > or by double clicking on the text, it went to its control
    > element. This patch checks whether there is selection or not,
    > after the click is happened. If there is selection then do
    > nothing else continue.
    >
    > New Layout test added:
    >  fast/forms/label/label-selection.html
    >
    > BUG=148912
    >
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=173310
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=174193
    
    Review URL: https://codereview.chromium.org/290153002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174210 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 10aeadbe8ce71607407eb69cba39df733f2403c3

--- third_party/WebKit/Source/core/html/HTMLLabelElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLLabelElement.cpp
@@ -27,9 +27,7 @@
 
 #include "HTMLNames.h"
 #include "core/dom/ElementTraversal.h"
-#include "core/editing/FrameSelection.h"
 #include "core/events/Event.h"
-#include "core/frame/LocalFrame.h"
 #include "core/html/FormAssociatedElement.h"
 
 namespace WebCore {
@@ -138,11 +136,6 @@ void HTMLLabelElement::defaultEventHandler(Event* evt)
     static bool processingClick = false;
 
     if (evt->type() == EventTypeNames::click && !processingClick) {
-        // If text of label element is selected, do not pass
-        // the event to control element.
-        if (document().frame()->selection().selection().isRange())
-            return;
-
         RefPtrWillBeRawPtr<HTMLElement> element = control();
 
         // If we can't find a control or if the control received the click
