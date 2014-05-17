f735f788dc6371e82cd370087c1462605568847e
achaulk@chromium.org <achaulk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-17 04:54:40 +0000

    Add header requires for cros build
    BUG=341554
    TEST=none
    
    Review URL: https://codereview.chromium.org/285383003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271162 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b5189d8dd65615691b1c77e0ff60313f7e828625

--- components/feedback/feedback_uploader.h
+++ components/feedback/feedback_uploader.h
@@ -12,6 +12,7 @@
 #include "base/file_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
 
