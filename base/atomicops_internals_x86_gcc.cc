1f9384139904ef5bf9feb9b64b400f2be6830b07
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-19 22:37:20 +0000

    base atomicops: Drop SSE2 detection, we always require SSE2 starting in m35.
    BUG=348761,94925
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/291993003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@271506 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3dd9f21718ea149dc06ad5a906a1d6dbe9f94dfd

--- base/atomicops_internals_x86_gcc.cc
+++ base/atomicops_internals_x86_gcc.cc
@@ -40,7 +40,6 @@
 // default values should hopefully be pretty safe.
 struct AtomicOps_x86CPUFeatureStruct AtomicOps_Internalx86CPUFeatures = {
   false,          // bug can't exist before process spawns multiple threads
-  false,          // no SSE2
 };
 
 namespace {
@@ -82,9 +81,6 @@ void AtomicOps_Internalx86CPUFeaturesInit() {
   } else {
     AtomicOps_Internalx86CPUFeatures.has_amd_lock_mb_bug = false;
   }
-
-  // edx bit 26 is SSE2 which we use to tell use whether we can use mfence
-  AtomicOps_Internalx86CPUFeatures.has_sse2 = ((edx >> 26) & 1);
 }
 
 class AtomicOpsx86Initializer {
