9b0daf187aea3a8295b9f8e76a4b9043d25d36eb
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-20 00:33:35 +0000

    Remove waitAndTest from html imports tests
    The waitAndTest system was flaky, it was busy waiting on a condition to
    become true using requestAnimationFrame. We should just use the load
    events on the links instead. This simplifies the tests and makes them
    more stable.
    
    R=morrita@chromium.org
    
    Review URL: https://codereview.chromium.org/299533002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174331 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 9a1af0d802317e77e629215261deaac8bf18f47e

--- third_party/WebKit/LayoutTests/fast/html/imports/resources/import-helpers.js
+++ third_party/WebKit/LayoutTests/fast/html/imports/resources/import-helpers.js
@@ -1,45 +1,4 @@
 
-
-function waitAndTest(tests)
-{
-    window.jsTestIsAsync = true;
-
-    function runNext()
-    {
-        var options = tests.shift();
-        if (!options)
-            return requestAnimationFrame(function() { finishJSTest(); }, 0);
-        return runSingleTest(options);
-    }
-
-    function runSingleTest(options)
-    {
-        var interval = 1;
-        var ntries = 8;
-        function checkWhenReady()
-        {
-            if (--ntries < 0) {
-                testFailed("Timed out");
-                return finishJSTest();
-            }
-
-            interval *= 2;
-            if (!options.ready())
-                return setTimeout(checkWhenReady, interval);
-
-            options.test();
-            return runNext();
-        }
-
-        debug(options.description);
-        if (options.setup)
-            options.setup();
-        checkWhenReady();
-    }
-
-    window.setTimeout(runNext, 0);
-}
-
 function createPlaceholder()
 {
     var link = document.createElement("link");
