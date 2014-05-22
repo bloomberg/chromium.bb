a7f5e55d6cb8364190889ad05867fd5140c00a47
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 04:10:49 +0000

    Reland "Move a good set of gfx unit tests into gfx_unittests target."
    It was reverted in r270600 because it broke compilation:
    lib/libgfx.so:error: undefined reference to 'XCreateRegion'
    lib/libgfx.so:error: undefined reference to 'XUnionRectWithRegion'
    lib/libgfx.so:error: undefined reference to 'XPolygonRegion'
    
    Original description:
    This requires to major changes:
    
    1- It requires the introduction of GfxTestSuite to allow us to
    initialize the ResourceBundle which is required for some of these tests
    to pass.
    2- It requires to split gfx_unittests target out of gfx.gyp, into
    gfx_tests.gyp. This is necessary because otherwise a circular dependency
    between ui_base.gyp and gfx.gyp would arise.
    
    The circular dependency is the form of:
    gfx.gyp -> ui_base.gyp -> gfx.gyp
    
    And is created because gfx_unittests now requires ResourceBundle to link
    which is in ui_base target.
    
    BUG=331829
    TEST=ui_unittests,gfx_unittests
    R=sadrul@chromium.org, danakj@chromium.org
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/291753002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272088 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9f86ec0740f2a2e5be556fa0e9ab5bb01a4ad484

---
+++ ui/gfx/test/run_all_unittests.cc
@@ -0,0 +1,49 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/bind.h"
+#include "base/compiler_specific.h"
+#include "base/macros.h"
+#include "base/path_service.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/base/ui_base_paths.h"
+
+namespace {
+
+class GfxTestSuite : public base::TestSuite {
+ public:
+  GfxTestSuite(int argc, char** argv) : base::TestSuite(argc, argv) {}
+
+ protected:
+  virtual void Initialize() OVERRIDE {
+    base::TestSuite::Initialize();
+    ui::RegisterPathProvider();
+
+    base::FilePath ui_test_pak_path;
+    ASSERT_TRUE(PathService::Get(ui::UI_TEST_PAK, &ui_test_pak_path));
+    ui::ResourceBundle::InitSharedInstanceWithPakPath(ui_test_pak_path);
+  }
+
+  virtual void Shutdown() OVERRIDE {
+    ui::ResourceBundle::CleanupSharedInstance();
+    base::TestSuite::Shutdown();
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(GfxTestSuite);
+};
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  GfxTestSuite test_suite(argc, argv);
+
+  return base::LaunchUnitTests(
+      argc,
+      argv,
+      base::Bind(&GfxTestSuite::Run, base::Unretained(&test_suite)));
+}
