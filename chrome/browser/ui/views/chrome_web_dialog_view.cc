88e2f846cb6fef3c763da1886827c0fc15e1006b
dzhioev@chromium.org <dzhioev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-22 21:48:42 +0000

    ChromeVox enabled for every WebDialog created by call to ShowWebDialog.
    While I was investigating http://crbug.com/355650 I noticed that
    ChromeVox doesn't work not only in cloud print dialog but in every dialog that
    was created by ShowWebDialog. This problem can be fixed by creating extension
    web contents observer for contents of dialog.
    
    BUG=355650
    TEST=manually
    
    Review URL: https://codereview.chromium.org/256783006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272333 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5fd7f0ba9a223bcff949b0ed013f0e2e20aba973

--- chrome/browser/ui/views/chrome_web_dialog_view.cc
+++ chrome/browser/ui/views/chrome_web_dialog_view.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/extensions/chrome_extension_web_contents_observer.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/webui/chrome_web_contents_handler.h"
 #include "ui/views/controls/webview/web_dialog_view.h"
@@ -14,19 +15,19 @@ gfx::NativeWindow ShowWebDialog(gfx::NativeWindow parent,
                                 content::BrowserContext* context,
                                 ui::WebDialogDelegate* delegate) {
   views::Widget* widget = NULL;
+  views::WebDialogView* view =
+      new views::WebDialogView(context, delegate, new ChromeWebContentsHandler);
   if (parent) {
-    widget = views::Widget::CreateWindowWithParent(
-        new views::WebDialogView(context,
-                                 delegate,
-                                 new ChromeWebContentsHandler),
-        parent);
+    widget = views::Widget::CreateWindowWithParent(view, parent);
   } else {
     // We shouldn't be called with a NULL parent, but sometimes are.
-    widget = views::Widget::CreateWindow(
-        new views::WebDialogView(context,
-                                 delegate,
-                                 new ChromeWebContentsHandler));
+    widget = views::Widget::CreateWindow(view);
   }
+
+  // Observer is needed for ChromeVox extension to send messages between content
+  // and background scripts.
+  extensions::ChromeExtensionWebContentsObserver::CreateForWebContents(
+      view->web_contents());
 
   widget->Show();
   return widget->GetNativeWindow();
