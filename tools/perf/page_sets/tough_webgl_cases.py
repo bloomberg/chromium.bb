c15e53f63be0bc6075004314337bd8514e33c7a2
nednguyen@google.com <nednguyen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 22:45:28 +0000

    Add page.CanRunOnBrowser. Add WebGL check for tough_webgl_cases pages
    BUG=373812
    
    Review URL: https://codereview.chromium.org/288313005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272620 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 43434893b3aaf4ca074e440e6ef00f297da7ac33

--- tools/perf/page_sets/tough_webgl_cases.py
+++ tools/perf/page_sets/tough_webgl_cases.py
@@ -1,6 +1,8 @@
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import logging
+
 # pylint: disable=W0401,W0614
 from telemetry.page.actions.all_page_actions import *
 from telemetry.page import page as page_module
@@ -12,6 +14,12 @@ class ToughWebglCasesPage(page_module.Page):
   def __init__(self, url, page_set):
     super(ToughWebglCasesPage, self).__init__(url=url, page_set=page_set)
     self.archive_data_file = 'data/tough_webgl_cases.json'
+
+  def CanRunOnBrowser(self, browser_info):
+    if not browser_info.HasWebGLSupport():
+      logging.warning('Browser does not support webgl, skipping test')
+      return False
+    return True
 
   def RunNavigateSteps(self, action_runner):
     action_runner.NavigateToPage(self)
