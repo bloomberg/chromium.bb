aa8856f13a923ef4f7d1e5adf536dccca4e02135
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-23 22:21:24 +0000

    Always assert needsAttach() in ~ContainerNode
    This should be true even if we're not in Oilpan mode. By the time the
    node is being destroyed we better be detached already. Indeed the
    call to removeDetachedChildren() has this ASSERT too.
    
    Review URL: https://codereview.chromium.org/287193016
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174721 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: bb1961a53b38b08bceb76f9c4269415acd8798dc

--- third_party/WebKit/Source/core/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/core/dom/ContainerNode.cpp
@@ -93,9 +93,8 @@ void ContainerNode::parserTakeAllChildrenFrom(ContainerNode& oldParent)
 
 ContainerNode::~ContainerNode()
 {
-#if ENABLE(OILPAN)
     ASSERT(needsAttach());
-#else
+#if !ENABLE(OILPAN)
     willBeDeletedFromDocument();
     removeDetachedChildren();
 #endif
