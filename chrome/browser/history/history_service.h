418fd1e12204515c5bf27441c84af2826eb98e77
sdefresne@chromium.org <sdefresne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-23 10:25:30 +0000

    Cleanup android specific code of //chrome/browser/history
    Forward-declare AndroidHistoryProviderService in history_service.h
    instead of including android_history_provider_service.h since the
    type is only used for a friendship declaration.
    
    BUG=371820
    
    Review URL: https://codereview.chromium.org/285763004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272464 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5f66681f8b2670fd074e84b78336c9efdb735ecb

--- chrome/browser/history/history_service.h
+++ chrome/browser/history/history_service.h
@@ -39,9 +39,8 @@
 #include "ui/base/layout.h"
 
 #if defined(OS_ANDROID)
-#include "chrome/browser/history/android/android_history_provider_service.h"
+class AndroidHistoryProviderService;
 #endif
-
 class BookmarkService;
 class GURL;
 class HistoryURLProvider;
