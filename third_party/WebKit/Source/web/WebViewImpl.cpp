6e6a2a2cbdbb1307ed4d934ad3044b8bdc82c8b9
enne@chromium.org <enne@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-24 00:54:24 +0000

    Change default force composite setting to true
    BUG=361729
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=174521
    
    Review URL: https://codereview.chromium.org/261143003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174734 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d0bee8805e0da4abfb615a7bd657ce34c0667996

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -3811,6 +3811,9 @@ void WebViewImpl::setIsAcceleratedCompositingActive(bool active)
     if (m_isAcceleratedCompositingActive == active)
         return;
 
+    if (!m_client)
+        return;
+
     if (!active) {
         m_isAcceleratedCompositingActive = false;
         // We need to finish all GL rendering before sending didDeactivateCompositor() to prevent
