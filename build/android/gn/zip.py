79405a5810e7591f9e336659f57b9f223d009c65
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-26 04:01:47 +0000

    Add java_cpp_template template
    This is the GN version of build/java_cpp_template.gypi.
    
    It defines a template that wraps an action_foreach that generates .java
    files using the host C preprocessor.
    
    The major difference in the GN version is that it takes all the
    generated java files and zips them together in a single .srcjar. When
    such a target is included in the srcjar_deps of a java library, the
    .java files in the .srcjar will be treated much like files listed in
    that libraries java_sources (particularly they will be compiled and
    included in the .jar/.dex).
    
    Depends on https://crrev.com/264773014/
    
    BUG=359249
    
    Review URL: https://codereview.chromium.org/264923007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272775 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 6fad603bfebe6314ecaa4d74a3ffdca430eeb8a6

---
+++ build/android/gn/zip.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Archives a set of files.
+"""
+
+import ast
+import optparse
+import os
+import sys
+import zipfile
+
+def DoZip(inputs, output, base_dir):
+  with zipfile.ZipFile(output, 'w') as outfile:
+    for f in inputs:
+      outfile.write(f, os.path.relpath(f, base_dir))
+
+def main():
+  parser = optparse.OptionParser()
+  parser.add_option('--inputs', help='List of files to archive.')
+  parser.add_option('--output', help='Path to output archive.')
+  parser.add_option('--base-dir',
+                    help='If provided, the paths in the archive will be '
+                    'relative to this directory', default='.')
+
+  options, _ = parser.parse_args()
+
+  inputs = ast.literal_eval(options.inputs)
+  output = options.output
+  base_dir = options.base_dir
+
+  DoZip(inputs, output, base_dir)
+
+
+if __name__ == '__main__':
+  sys.exit(main())
