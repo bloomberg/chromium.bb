46683df3fc370e80b64dc360405987a50497a038
davidben@chromium.org <davidben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-27 21:24:54 +0000

    Hide the deletion confirmation overlay when the page state changes.
    BUG=377386
    TEST=See bug, comment #2.
    
    Review URL: https://codereview.chromium.org/300063009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273037 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: dad37468058caad381d686c1d2c57b1c561d7e6d

--- chrome/browser/resources/history/history.js
+++ chrome/browser/resources/history/history.js
@@ -996,6 +996,9 @@ HistoryView.prototype.positionNotificationBar = function() {
  * @private
  */
 HistoryView.prototype.clear_ = function() {
+  if ($('alertOverlay').classList.contains('showing'))
+    hideConfirmationOverlay();
+
   this.resultDiv_.textContent = '';
 
   this.currentVisits_.forEach(function(visit) {
