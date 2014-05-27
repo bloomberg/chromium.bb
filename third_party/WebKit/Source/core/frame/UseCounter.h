5d93b43e6ca4a33c701fe238d7648cb75c9b6240
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-05-27 20:13:48 +0000

    Add use counters for Encoding API entry points
    BUG=377874
    
    Review URL: https://codereview.chromium.org/296413009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@174891 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2424b7904e9c3b01971571be6a221aa45dfdb8c3

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -431,6 +431,10 @@ public:
         CSSCharsetRuleEncoding = 426,
         DocumentSetCharset = 427,
         DocumentDefaultCharset = 428,
+        TextEncoderConstructor = 429,
+        TextEncoderEncode = 430,
+        TextDecoderConstructor = 431,
+        TextDecoderDecode= 432,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
