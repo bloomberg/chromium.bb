55c974d623a343761f03a4d964db1e4f9b418335
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-27 17:46:37 +0000

    Refactor and move ash independent accelerator handling code in nested loop to ui/wm/core
    I also renamed classes to NestedAcceleratorXxx. I felt this is a bit more clearer than NestedDispatcher, especially in ui/wm/core. Please let me know if you disagree or have better suggestion. I'm happy to rename them.
    
    BUG=None
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=272740
    
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/298703007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@272995 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b6d1724783fa378a745be25973c1b908d8679620

--- ui/wm/core/nested_accelerator_controller.h
+++ ui/wm/core/nested_accelerator_controller.h
@@ -2,27 +2,28 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef ASH_ACCELERATORS_NESTED_DISPATCHER_CONTROLLER_H_
-#define ASH_ACCELERATORS_NESTED_DISPATCHER_CONTROLLER_H_
+#ifndef UI_WM_CORE_NESTED_ACCELERATOR_CONTROLLER_H_
+#define UI_WM_CORE_NESTED_ACCELERATOR_CONTROLLER_H_
 
-#include "ash/ash_export.h"
 #include "base/callback.h"
 #include "base/message_loop/message_loop.h"
 #include "ui/wm/public/dispatcher_client.h"
+#include "ui/wm/wm_export.h"
 
-namespace ash {
+namespace wm {
 
-class AcceleratorDispatcher;
+class NestedAcceleratorDelegate;
+class NestedAcceleratorDispatcher;
 
 // Creates a dispatcher which wraps another dispatcher.
 // The outer dispatcher runs first and performs ash specific handling.
 // If it does not consume the event it forwards the event to the nested
 // dispatcher.
-class ASH_EXPORT NestedDispatcherController
+class WM_EXPORT NestedAcceleratorController
     : public aura::client::DispatcherClient {
  public:
-  NestedDispatcherController();
-  virtual ~NestedDispatcherController();
+  explicit NestedAcceleratorController(NestedAcceleratorDelegate* delegate);
+  virtual ~NestedAcceleratorController();
 
   // aura::client::DispatcherClient:
   virtual void RunWithDispatcher(
@@ -31,11 +32,12 @@ class ASH_EXPORT NestedDispatcherController
 
  private:
   base::Closure quit_closure_;
-  scoped_ptr<AcceleratorDispatcher> accelerator_dispatcher_;
+  scoped_ptr<NestedAcceleratorDispatcher> accelerator_dispatcher_;
+  scoped_ptr<NestedAcceleratorDelegate> dispatcher_delegate_;
 
-  DISALLOW_COPY_AND_ASSIGN(NestedDispatcherController);
+  DISALLOW_COPY_AND_ASSIGN(NestedAcceleratorController);
 };
 
-}  // namespace ash
+}  // namespace wm
 
-#endif  // ASH_ACCELERATORS_NESTED_DISPATCHER_CONTROLLER_H_
+#endif  // UI_WM_CORE_NESTED_ACCELERATOR_CONTROLLER_H_
