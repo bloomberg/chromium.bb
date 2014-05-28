2d3fd6b0a51d41d79af1a9cee2d548bc7c830cf4
Leon Scroggins III <scroggo@google.com>
2014-05-28 15:23:04 -0400

    Add a makefile, based on Skia's gyp file.
    Make jsoncpp a static library, only needed by tests.
    
    BUG:15087860
    Change-Id: I7d8b91ac954a6d497f305870af92e5113daa4f79

---
+++ Android.mk
@@ -0,0 +1,30 @@
+BASE_PATH := $(call my-dir)
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	src/lib_json/json_reader.cpp \
+	chromium-overrides/src/lib_json/json_value.cpp \
+	src/lib_json/json_writer.cpp
+
+LOCAL_C_INCLUDES:= \
+	$(LOCAL_PATH)/chromium-overrides/include \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/src/lib_json
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+	$(LOCAL_PATH)/chromium-overrides/include \
+	$(LOCAL_PATH)/include
+
+LOCAL_CFLAGS := \
+	-DJSON_USE_EXCEPTION=0
+
+LOCAL_MODULE_TAGS := \
+	tests
+
+LOCAL_MODULE := \
+	libjsoncpp
+
+include external/stlport/libstlport.mk
+include $(BUILD_STATIC_LIBRARY)
