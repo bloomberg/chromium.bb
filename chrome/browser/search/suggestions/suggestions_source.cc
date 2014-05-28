1a854485bffd423d2f66144b5e12a2ae89db232f
mathp@chromium.org <mathp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 13:44:02 +0000

    [Suggestions] Add support for thumbnails in chrome://suggestions debug page
    This will fetch server thumbnails if available.
    
    BUG=None
    TEST=Manual
    
    Review URL: https://codereview.chromium.org/292433018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273204 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 26fc6965cbd8996f65548468416096d0b8153918

--- chrome/browser/search/suggestions/suggestions_source.cc
+++ chrome/browser/search/suggestions/suggestions_source.cc
@@ -6,19 +6,22 @@
 
 #include <vector>
 
+#include "base/barrier_closure.h"
+#include "base/base64.h"
 #include "base/bind.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/strings/string16.h"
+#include "base/strings/string_piece.h"
 #include "base/strings/string_util.h"
-#include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/instant_io_context.h"
-#include "chrome/browser/search/suggestions/proto/suggestions.pb.h"
 #include "chrome/browser/search/suggestions/suggestions_service.h"
 #include "chrome/browser/search/suggestions/suggestions_service_factory.h"
 #include "chrome/common/url_constants.h"
 #include "net/base/escape.h"
 #include "net/url_request/url_request.h"
+#include "ui/gfx/codec/png_codec.h"
+#include "ui/gfx/image/image_skia.h"
 #include "url/gurl.h"
 
 namespace suggestions {
@@ -32,24 +35,72 @@ const char kHtmlHeader[] =
 const char kHtmlBody[] = "</head>\n<body>\n";
 const char kHtmlFooter[] = "</body>\n</html>\n";
 
+// Fills |output| with the HTML needed to display the suggestions.
+void RenderOutputHtml(const SuggestionsProfile& profile,
+                      const std::map<GURL, std::string>& base64_encoded_pngs,
+                      std::string* output) {
+  std::vector<std::string> out;
+  out.push_back(kHtmlHeader);
+  out.push_back(kHtmlBody);
+  out.push_back("<h1>Suggestions</h1>\n<ul>");
+
+  size_t size = profile.suggestions_size();
+  for (size_t i = 0; i < size; ++i) {
+    const ChromeSuggestion& suggestion = profile.suggestions(i);
+    std::string line;
+    line += "<li><a href=\"";
+    line += net::EscapeForHTML(suggestion.url());
+    line += "\" target=\"_blank\">";
+    line += net::EscapeForHTML(suggestion.title());
+    std::map<GURL, std::string>::const_iterator it =
+        base64_encoded_pngs.find(GURL(suggestion.url()));
+    if (it != base64_encoded_pngs.end()) {
+      line += "<br><img src='";
+      line += it->second;
+      line += "'>";
+    }
+    line += "</a></li>\n";
+    out.push_back(line);
+  }
+  out.push_back("</ul>");
+  out.push_back(kHtmlFooter);
+  *output = JoinString(out, "");
+}
+
+// Fills |output| with the HTML needed to display that no suggestions are
+// available.
+void RenderOutputHtmlNoSuggestions(std::string* output) {
+  std::vector<std::string> out;
+  out.push_back(kHtmlHeader);
+  out.push_back(kHtmlBody);
+  out.push_back("<h1>Suggestions</h1>\n");
+  out.push_back("<p>You have no suggestions.</p>\n");
+  out.push_back(kHtmlFooter);
+  *output = JoinString(out, "");
+}
+
 }  // namespace
 
 SuggestionsSource::SuggestionsSource(Profile* profile)
-    : profile_(profile),
-      weak_ptr_factory_(this) {
-}
+    : profile_(profile), weak_ptr_factory_(this) {}
 
-SuggestionsSource::~SuggestionsSource() {
-}
+SuggestionsSource::~SuggestionsSource() {}
+
+SuggestionsSource::RequestContext::RequestContext(
+    const SuggestionsProfile& suggestions_profile_in,
+    const content::URLDataSource::GotDataCallback& callback_in)
+    : suggestions_profile(suggestions_profile_in),   // Copy.
+      callback(callback_in)  // Copy.
+{}
+
+SuggestionsSource::RequestContext::~RequestContext() {}
 
 std::string SuggestionsSource::GetSource() const {
   return chrome::kChromeUISuggestionsHost;
 }
 
 void SuggestionsSource::StartDataRequest(
-    const std::string& path,
-    int render_process_id,
-    int render_frame_id,
+    const std::string& path, int render_process_id, int render_frame_id,
     const content::URLDataSource::GotDataCallback& callback) {
   SuggestionsServiceFactory* suggestions_service_factory =
       SuggestionsServiceFactory::GetInstance();
@@ -87,33 +138,55 @@ bool SuggestionsSource::ShouldServiceRequest(
 void SuggestionsSource::OnSuggestionsAvailable(
     const content::URLDataSource::GotDataCallback& callback,
     const SuggestionsProfile& suggestions_profile) {
-  // Render HTML.
-  std::vector<std::string> out;
-  out.push_back(kHtmlHeader);
-  out.push_back(kHtmlBody);
-  out.push_back("<h1>Suggestions</h1>\n");
-
   size_t size = suggestions_profile.suggestions_size();
-  if (size == 0) {
-    out.push_back("<p>You have no suggestions.</p>\n");
+  if (!size) {
+    std::string output;
+    RenderOutputHtmlNoSuggestions(&output);
+    callback.Run(base::RefCountedString::TakeString(&output));
   } else {
-    out.push_back("<ol>\n");
+    RequestContext* context = new RequestContext(suggestions_profile, callback);
+    base::Closure barrier = BarrierClosure(
+        size, base::Bind(&SuggestionsSource::OnThumbnailsFetched,
+                         weak_ptr_factory_.GetWeakPtr(), context));
     for (size_t i = 0; i < size; ++i) {
       const ChromeSuggestion& suggestion = suggestions_profile.suggestions(i);
-      std::string line;
-      line.append("<li><a href=\"");
-      line.append(net::EscapeForHTML(suggestion.url()));
-      line.append("\" target=\"_blank\">");
-      line.append(net::EscapeForHTML(suggestion.title()));
-      line.append("</a></li>\n");
-      out.push_back(line);
+      // Fetch the thumbnail for this URL (exercising the fetcher). After all
+      // fetches are done, including NULL callbacks for unavailable thumbnails,
+      // SuggestionsSource::OnThumbnailsFetched will be called.
+      SuggestionsService* suggestions_service(
+          SuggestionsServiceFactory::GetForProfile(profile_));
+      suggestions_service->GetPageThumbnail(
+          GURL(suggestion.url()),
+          base::Bind(&SuggestionsSource::OnThumbnailAvailable,
+                     weak_ptr_factory_.GetWeakPtr(), context, barrier));
     }
-    out.push_back("</ol>\n");
   }
-  out.push_back(kHtmlFooter);
+}
 
-  std::string out_html = JoinString(out, "");
-  callback.Run(base::RefCountedString::TakeString(&out_html));
+void SuggestionsSource::OnThumbnailsFetched(RequestContext* context) {
+  scoped_ptr<RequestContext> context_deleter(context);
+
+  std::string output;
+  RenderOutputHtml(context->suggestions_profile, context->base64_encoded_pngs,
+                   &output);
+  context->callback.Run(base::RefCountedString::TakeString(&output));
+}
+
+void SuggestionsSource::OnThumbnailAvailable(RequestContext* context,
+                                             base::Closure barrier,
+                                             const GURL& url,
+                                             const SkBitmap* bitmap) {
+  if (bitmap) {
+    std::vector<unsigned char> output;
+    gfx::PNGCodec::EncodeBGRASkBitmap(*bitmap, false, &output);
+
+    std::string encoded_output;
+    base::Base64Encode(std::string(output.begin(), output.end()),
+                       &encoded_output);
+    context->base64_encoded_pngs[url] = "data:image/png;base64,";
+    context->base64_encoded_pngs[url] += encoded_output;
+  }
+  barrier.Run();
 }
 
 }  // namespace suggestions
--- chrome/browser/search/suggestions/suggestions_source.h
+++ chrome/browser/search/suggestions/suggestions_source.h
@@ -5,21 +5,24 @@
 #ifndef CHROME_BROWSER_SEARCH_SUGGESTIONS_SUGGESTIONS_SOURCE_H_
 #define CHROME_BROWSER_SEARCH_SUGGESTIONS_SUGGESTIONS_SOURCE_H_
 
+#include <map>
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/callback.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/search/suggestions/proto/suggestions.pb.h"
 #include "content/public/browser/url_data_source.h"
+#include "url/gurl.h"
 
 class Profile;
+class SkBitmap;
 
 namespace base {
 class RefCountedMemory;
 }  // namespace base
 
 namespace suggestions {
-
-class SuggestionsProfile;
 
 // SuggestionsSource renders a webpage to list SuggestionsService data.
 class SuggestionsSource : public content::URLDataSource {
@@ -29,9 +32,7 @@ class SuggestionsSource : public content::URLDataSource {
   // content::URLDataSource implementation.
   virtual std::string GetSource() const OVERRIDE;
   virtual void StartDataRequest(
-      const std::string& path,
-      int render_process_id,
-      int render_frame_id,
+      const std::string& path, int render_process_id, int render_frame_id,
       const content::URLDataSource::GotDataCallback& callback) OVERRIDE;
   virtual std::string GetMimeType(const std::string& path) const OVERRIDE;
   virtual base::MessageLoop* MessageLoopForRequestPath(
@@ -42,10 +43,30 @@ class SuggestionsSource : public content::URLDataSource {
  private:
   virtual ~SuggestionsSource();
 
-  // Callback passed to a SuggestionsService instance.
+  // Container for the state of a request.
+  struct RequestContext {
+    RequestContext(
+        const suggestions::SuggestionsProfile& suggestions_profile_in,
+        const content::URLDataSource::GotDataCallback& callback_in);
+    ~RequestContext();
+
+    const suggestions::SuggestionsProfile suggestions_profile;
+    const content::URLDataSource::GotDataCallback callback;
+    std::map<GURL, std::string> base64_encoded_pngs;
+  };
+
+  // Callback for suggestions from SuggestionsService.
   void OnSuggestionsAvailable(
       const content::URLDataSource::GotDataCallback& callback,
       const SuggestionsProfile& suggestions_profile);
+
+  // Callback for responses from each ThumbnailManager request.
+  void OnThumbnailAvailable(RequestContext* context, base::Closure barrier,
+                            const GURL& url, const SkBitmap* bitmap);
+
+  // Callback for when all requests are complete. Renders the output webpage and
+  // passes the result to the original caller.
+  void OnThumbnailsFetched(RequestContext* context);
 
   // Only used when servicing requests on the UI thread.
   Profile* const profile_;
