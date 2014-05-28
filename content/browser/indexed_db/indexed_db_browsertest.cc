c6e8635139b25b92abee5d1ede9b3f6dcf031447
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-28 19:55:50 +0000

    Disable IndexedDBBrowserTestSingleProcess.RenderThreadShutdownTest under LSan.
    BUG=378404
    TBR=jsbell@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/303763003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273329 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 98b5cc77cd3a5d21ad260927f097df9e7a1c7a78

--- content/browser/indexed_db/indexed_db_browsertest.cc
+++ content/browser/indexed_db/indexed_db_browsertest.cc
@@ -609,7 +609,8 @@ class IndexedDBBrowserTestSingleProcess : public IndexedDBBrowserTest {
 };
 
 // Crashing on Android due to kSingleProcess flag: http://crbug.com/342525
-#if defined(OS_ANDROID)
+// Leaks memory by design: http://crbug.com/378404
+#if defined(OS_ANDROID) || defined(LEAK_SANITIZER)
 #define MAYBE_RenderThreadShutdownTest DISABLED_RenderThreadShutdownTest
 #else
 #define MAYBE_RenderThreadShutdownTest RenderThreadShutdownTest
