ad2e003c5d11cffaabeaac5a0f01e4510cfaf3e1
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-29 16:34:26 +0000

    Pepper: Move several tests to content_browsertests.
    This moves a bunch of non-NaCl pepper tests to browser_tests. I moved a bunch
    of the ones that looked easiest here, and more will be moved over in follow-on
    changes.
    
    BUG=371873
    R=dmichael@chromium.org
    
    Review URL: https://codereview.chromium.org/308663002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273494 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 585765a178036aa75ef4f5a176e25431e5dc6387

--- chrome/test/ppapi/ppapi_browsertest.cc
+++ chrome/test/ppapi/ppapi_browsertest.cc
@@ -179,8 +179,6 @@ IN_PROC_BROWSER_TEST_F(PPAPIBrokerInfoBarTest, Allowed) {
   RunTest("Broker_IsAllowedPermissionGranted");
 }
 
-TEST_PPAPI_IN_PROCESS(Console)
-TEST_PPAPI_OUT_OF_PROCESS(Console)
 TEST_PPAPI_NACL(Console)
 
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(USE_AURA)
@@ -196,7 +194,6 @@ TEST_PPAPI_NACL(MAYBE_Core)
 // Non-NaCl TraceEvent tests are in content/test/ppapi/ppapi_browsertest.cc.
 TEST_PPAPI_NACL(TraceEvent)
 
-TEST_PPAPI_OUT_OF_PROCESS(InputEvent)
 TEST_PPAPI_NACL(InputEvent)
 
 // Flaky on Linux and Windows. http://crbug.com/135403
@@ -209,14 +206,6 @@ TEST_PPAPI_NACL(InputEvent)
 TEST_PPAPI_OUT_OF_PROCESS(MAYBE_ImeInputEvent)
 TEST_PPAPI_NACL(MAYBE_ImeInputEvent)
 
-// "Instance" tests are really InstancePrivate tests. InstancePrivate is not
-// supported in NaCl, so these tests are only run trusted.
-// Also note that these tests are run separately on purpose (versus collapsed
-// in to one IN_PROC_BROWSER_TEST_F macro), because some of them have leaks
-// on purpose that will look like failures to tests that are run later.
-TEST_PPAPI_IN_PROCESS(Instance_ExecuteScript);
-TEST_PPAPI_OUT_OF_PROCESS(Instance_ExecuteScript)
-
 // We run and reload the RecursiveObjects test to ensure that the InstanceObject
 // (and others) are properly cleaned up after the first run.
 IN_PROC_BROWSER_TEST_F(PPAPITest, Instance_RecursiveObjects) {
@@ -228,30 +217,7 @@ IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest,
                        DISABLED_Instance_RecursiveObjects) {
   RunTestAndReload("Instance_RecursiveObjects");
 }
-TEST_PPAPI_IN_PROCESS(Instance_LeakedObjectDestructors);
-TEST_PPAPI_OUT_OF_PROCESS(Instance_LeakedObjectDestructors);
 
-IN_PROC_BROWSER_TEST_F(PPAPITest,
-                       Instance_ExecuteScriptAtInstanceShutdown) {
-  // In other tests, we use one call to RunTest so that the tests can all run
-  // in one plugin instance. This saves time on loading the plugin (especially
-  // for NaCl). Here, we actually want to destroy the Instance, to test whether
-  // the destructor can run ExecuteScript successfully. That's why we have two
-  // separate calls to RunTest; the second one forces a navigation which
-  // destroys the instance from the prior RunTest.
-  // See test_instance_deprecated.cc for more information.
-  RunTest("Instance_SetupExecuteScriptAtInstanceShutdown");
-  RunTest("Instance_ExecuteScriptAtInstanceShutdown");
-}
-IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest,
-                       Instance_ExecuteScriptAtInstanceShutdown) {
-  // (See the comment for the in-process version of this test above)
-  RunTest("Instance_SetupExecuteScriptAtInstanceShutdown");
-  RunTest("Instance_ExecuteScriptAtInstanceShutdown");
-}
-
-TEST_PPAPI_IN_PROCESS(Graphics2D)
-TEST_PPAPI_OUT_OF_PROCESS(Graphics2D)
 // Graphics2D_Dev isn't supported in NaCl, only test the other interfaces
 // TODO(jhorwich) Enable when Graphics2D_Dev interfaces are proxied in NaCl.
 TEST_PPAPI_NACL(Graphics2D_InvalidResource)
@@ -296,21 +262,7 @@ TEST_PPAPI_IN_PROCESS(MAYBE_IN_Graphics3D)
 TEST_PPAPI_OUT_OF_PROCESS(MAYBE_OUT_Graphics3D)
 TEST_PPAPI_NACL(MAYBE_NACL_Graphics3D)
 
-TEST_PPAPI_IN_PROCESS(ImageData)
-TEST_PPAPI_OUT_OF_PROCESS(ImageData)
 TEST_PPAPI_NACL(ImageData)
-
-TEST_PPAPI_IN_PROCESS(BrowserFont)
-// crbug.com/308949
-#if defined(OS_WIN)
-#define MAYBE_OUT_BrowserFont DISABLED_BrowserFont
-#else
-#define MAYBE_OUT_BrowserFont BrowserFont
-#endif
-TEST_PPAPI_OUT_OF_PROCESS(MAYBE_OUT_BrowserFont)
-
-TEST_PPAPI_IN_PROCESS(Buffer)
-TEST_PPAPI_OUT_OF_PROCESS(Buffer)
 
 // TCPSocket and TCPSocketPrivate tests.
 #define RUN_TCPSOCKET_SUBTESTS \
@@ -620,35 +572,11 @@ TEST_PPAPI_OUT_OF_PROCESS(PaintAggregator)
 TEST_PPAPI_NACL(PaintAggregator)
 
 // TODO(danakj): http://crbug.com/115286
-TEST_PPAPI_IN_PROCESS(DISABLED_Scrollbar)
-// http://crbug.com/89961
-TEST_PPAPI_OUT_OF_PROCESS(DISABLED_Scrollbar)
-// TODO(danakj): http://crbug.com/115286
 TEST_PPAPI_NACL(DISABLED_Scrollbar)
 
-TEST_PPAPI_IN_PROCESS(URLUtil)
-TEST_PPAPI_OUT_OF_PROCESS(URLUtil)
-
-TEST_PPAPI_IN_PROCESS(CharSet)
-TEST_PPAPI_OUT_OF_PROCESS(CharSet)
-
-TEST_PPAPI_IN_PROCESS(Var)
-TEST_PPAPI_OUT_OF_PROCESS(Var)
 TEST_PPAPI_NACL(Var)
 
-TEST_PPAPI_IN_PROCESS(VarResource)
-TEST_PPAPI_OUT_OF_PROCESS(VarResource)
 TEST_PPAPI_NACL(VarResource)
-
-// Flaky on mac, http://crbug.com/121107
-#if defined(OS_MACOSX)
-#define MAYBE_VarDeprecated DISABLED_VarDeprecated
-#else
-#define MAYBE_VarDeprecated VarDeprecated
-#endif
-
-TEST_PPAPI_IN_PROCESS(VarDeprecated)
-TEST_PPAPI_OUT_OF_PROCESS(MAYBE_VarDeprecated)
 
 // PostMessage tests.
 #define RUN_POSTMESSAGE_SUBTESTS \
@@ -689,12 +617,7 @@ IN_PROC_BROWSER_TEST_F(PPAPINaClPNaClNonSfiTest,
   RUN_POSTMESSAGE_SUBTESTS;
 }
 
-TEST_PPAPI_IN_PROCESS(Memory)
-TEST_PPAPI_OUT_OF_PROCESS(Memory)
 TEST_PPAPI_NACL(Memory)
-
-TEST_PPAPI_IN_PROCESS(VideoDecoderDev)
-TEST_PPAPI_OUT_OF_PROCESS(VideoDecoderDev)
 
 // FileIO tests.
 #define RUN_FILEIO_SUBTESTS \
--- content/test/ppapi/ppapi_browsertest.cc
+++ content/test/ppapi/ppapi_browsertest.cc
@@ -27,11 +27,96 @@ namespace {
     RunTest(STRIP_PREFIXES(test_name)); \
   }
 
+TEST_PPAPI_IN_PROCESS(BrowserFont)
+// crbug.com/308949
+#if defined(OS_WIN)
+#define MAYBE_OUT_BrowserFont DISABLED_BrowserFont
+#else
+#define MAYBE_OUT_BrowserFont BrowserFont
+#endif
+TEST_PPAPI_OUT_OF_PROCESS(MAYBE_OUT_BrowserFont)
+
+TEST_PPAPI_IN_PROCESS(Buffer)
+TEST_PPAPI_OUT_OF_PROCESS(Buffer)
+
+TEST_PPAPI_IN_PROCESS(CharSet)
+TEST_PPAPI_OUT_OF_PROCESS(CharSet)
+
+TEST_PPAPI_IN_PROCESS(Console)
+TEST_PPAPI_OUT_OF_PROCESS(Console)
+
 TEST_PPAPI_IN_PROCESS(Crypto)
 TEST_PPAPI_OUT_OF_PROCESS(Crypto)
 
+TEST_PPAPI_IN_PROCESS(Graphics2D)
+TEST_PPAPI_OUT_OF_PROCESS(Graphics2D)
+
+TEST_PPAPI_IN_PROCESS(ImageData)
+TEST_PPAPI_OUT_OF_PROCESS(ImageData)
+
+TEST_PPAPI_OUT_OF_PROCESS(InputEvent)
+
+// "Instance" tests are really InstancePrivate tests. InstancePrivate is not
+// supported in NaCl, so these tests are only run trusted.
+// Also note that these tests are run separately on purpose (versus collapsed
+// in to one IN_PROC_BROWSER_TEST_F macro), because some of them have leaks
+// on purpose that will look like failures to tests that are run later.
+TEST_PPAPI_IN_PROCESS(Instance_ExecuteScript)
+TEST_PPAPI_OUT_OF_PROCESS(Instance_ExecuteScript)
+
+IN_PROC_BROWSER_TEST_F(PPAPITest,
+                       Instance_ExecuteScriptAtInstanceShutdown) {
+  // In other tests, we use one call to RunTest so that the tests can all run
+  // in one plugin instance. This saves time on loading the plugin (especially
+  // for NaCl). Here, we actually want to destroy the Instance, to test whether
+  // the destructor can run ExecuteScript successfully. That's why we have two
+  // separate calls to RunTest; the second one forces a navigation which
+  // destroys the instance from the prior RunTest.
+  // See test_instance_deprecated.cc for more information.
+  RunTest("Instance_SetupExecuteScriptAtInstanceShutdown");
+  RunTest("Instance_ExecuteScriptAtInstanceShutdown");
+}
+IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest,
+                       Instance_ExecuteScriptAtInstanceShutdown) {
+  // (See the comment for the in-process version of this test above)
+  RunTest("Instance_SetupExecuteScriptAtInstanceShutdown");
+  RunTest("Instance_ExecuteScriptAtInstanceShutdown");
+}
+
+TEST_PPAPI_IN_PROCESS(Instance_LeakedObjectDestructors)
+TEST_PPAPI_OUT_OF_PROCESS(Instance_LeakedObjectDestructors)
+
+TEST_PPAPI_IN_PROCESS(Memory)
+TEST_PPAPI_OUT_OF_PROCESS(Memory)
+
+// TODO(danakj): http://crbug.com/115286
+TEST_PPAPI_IN_PROCESS(DISABLED_Scrollbar)
+// http://crbug.com/89961
+TEST_PPAPI_OUT_OF_PROCESS(DISABLED_Scrollbar)
+
 TEST_PPAPI_IN_PROCESS(TraceEvent)
 TEST_PPAPI_OUT_OF_PROCESS(TraceEvent)
+
+TEST_PPAPI_IN_PROCESS(URLUtil)
+TEST_PPAPI_OUT_OF_PROCESS(URLUtil)
+
+TEST_PPAPI_IN_PROCESS(Var)
+TEST_PPAPI_OUT_OF_PROCESS(Var)
+
+// Flaky on mac, http://crbug.com/121107
+#if defined(OS_MACOSX)
+#define MAYBE_VarDeprecated DISABLED_VarDeprecated
+#else
+#define MAYBE_VarDeprecated VarDeprecated
+#endif
+TEST_PPAPI_IN_PROCESS(VarDeprecated)
+TEST_PPAPI_OUT_OF_PROCESS(MAYBE_VarDeprecated)
+
+TEST_PPAPI_IN_PROCESS(VarResource)
+TEST_PPAPI_OUT_OF_PROCESS(VarResource)
+
+TEST_PPAPI_IN_PROCESS(VideoDecoderDev)
+TEST_PPAPI_OUT_OF_PROCESS(VideoDecoderDev)
 
 }  // namespace
 }  // namespace content
