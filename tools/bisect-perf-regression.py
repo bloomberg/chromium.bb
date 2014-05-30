662a622e283ab86826a886c87ff18af6042d3184
prasadv@chromium.org <prasadv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-30 23:21:55 +0000

    Use python fallback for unzipping in extracting build on mac if file is greater than 4GB
    BUG=379195
    
    Review URL: https://codereview.chromium.org/306023008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273929 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1570e3f5b0b5f246c32771f757a5f564e9d79325

--- tools/bisect-perf-regression.py
+++ tools/bisect-perf-regression.py
@@ -522,10 +522,15 @@ def ExtractZip(filename, output_dir, verbose=True):
   # handle links and file bits (executable), which is much
   # easier then trying to do that with ZipInfo options.
   #
+  # The Mac Version of unzip unfortunately does not support Zip64, whereas
+  # the python module does, so we have to fallback to the python zip module
+  # on Mac if the filesize is greater than 4GB.
+  #
   # On Windows, try to use 7z if it is installed, otherwise fall back to python
   # zip module and pray we don't have files larger than 512MB to unzip.
   unzip_cmd = None
-  if IsMac() or IsLinux():
+  if ((IsMac() and os.path.getsize(filename) < 4 * 1024 * 1024 * 1024)
+      or IsLinux()):
     unzip_cmd = ['unzip', '-o']
   elif IsWindows() and os.path.exists('C:\\Program Files\\7-Zip\\7z.exe'):
     unzip_cmd = ['C:\\Program Files\\7-Zip\\7z.exe', 'x', '-y']
@@ -541,12 +546,16 @@ def ExtractZip(filename, output_dir, verbose=True):
     if result:
       raise IOError('unzip failed: %s => %s' % (str(command), result))
   else:
-    assert IsWindows()
+    assert IsWindows() or IsMac()
     zf = zipfile.ZipFile(filename)
     for name in zf.namelist():
       if verbose:
         print 'Extracting %s' % name
       zf.extract(name, output_dir)
+      if IsMac():
+        # Restore permission bits.
+        os.chmod(os.path.join(output_dir, name),
+                 zf.getinfo(name).external_attr >> 16L)
 
 
 def RunProcess(command):
