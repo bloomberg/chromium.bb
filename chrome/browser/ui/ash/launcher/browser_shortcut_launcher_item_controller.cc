8af20c1ed6c4eea25164a48cdf1cc31f4fd637d7
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-05-31 03:46:19 +0000

    Fixing problem with minimizing of applications to incorrect locations
    Windows did not get tagged for their applications and as such the shrink/zoom target was showing to the wrong location.
    
    BUG=378806, 378803
    TEST=visual
    
    Review URL: https://codereview.chromium.org/306083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@273998 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 092f9df533b1f81e9f082b68048b76c9c44a5671

--- chrome/browser/ui/ash/launcher/browser_shortcut_launcher_item_controller.cc
+++ chrome/browser/ui/ash/launcher/browser_shortcut_launcher_item_controller.cc
@@ -103,8 +103,14 @@ void BrowserShortcutLauncherItemController::UpdateBrowserItemState() {
 void BrowserShortcutLauncherItemController::SetShelfIDForBrowserWindowContents(
     Browser* browser,
     content::WebContents* web_contents) {
-  if (!IsBrowserRepresentedInBrowserList(browser))
+  // We need to call SetShelfIDForWindow for V1 applications since they are
+  // content which might change and as such change the application type.
+  if (!browser ||
+      !launcher_controller()->IsBrowserFromActiveUser(browser) ||
+      browser->host_desktop_type() != chrome::HOST_DESKTOP_TYPE_ASH ||
+      chrome::IsTrustedPopupWindowWithScheme(browser, content::kChromeUIScheme))
     return;
+
   ash::SetShelfIDForWindow(
       launcher_controller()->GetShelfIDForWebContents(web_contents),
       browser->window()->GetNativeWindow());
