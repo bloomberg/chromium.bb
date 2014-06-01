2a5950cb16b1f0e9c521728a35d85f5ddfcc4fb5
robhogan@gmail.com <robhogan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-01 12:48:36 +0000

    Strip anonymous blocks when change in style removes need for them
    When a block element becomes a float or out of flow we should strip
    any anonymous blocks wrapping its inline siblings.
    
    BUG=331251
    
    Review URL: https://codereview.chromium.org/253313005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175220 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 4d21befe3b7be53a1bd8d289813f00d9570024f0

--- third_party/WebKit/Source/core/rendering/RenderBlock.h
+++ third_party/WebKit/Source/core/rendering/RenderBlock.h
@@ -370,6 +370,8 @@ private:
 
     void addChildIgnoringAnonymousColumnBlocks(RenderObject* newChild, RenderObject* beforeChild = 0);
 
+    void removeAnonymousWrappersIfRequired();
+
     virtual bool isSelfCollapsingBlock() const OVERRIDE;
 
     void insertIntoTrackedRendererMaps(RenderBox* descendant, TrackedDescendantsMap*&, TrackedContainerMap*&);
