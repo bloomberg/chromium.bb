d6c8be8e516d03b8de6c9a3d43068c4fcecd151e
iannucci@chromium.org <iannucci@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-02 23:12:52 +0000

    Bump version to 5 to clear out slow win7 git rollout.
    The previous rollout pushed the xp version of chromium git v4 to windows7. This rollout pushes the correct windows 7 version.
    
    R=mmoss@chromium.org, szager@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/311713002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@274363 0039d316-1c4b-4281-b951-d872f2087c98

--- bootstrap/win/win_tools.bat
+++ bootstrap/win/win_tools.bat
@@ -64,12 +64,12 @@ goto :GIT_190_CHECK
 
 
 :GIT_190_CHECK
-set GIT_ERASE_TOP=3
-set GIT_VERSION=1.9.0.chromium.4
+set GIT_ERASE_TOP=4
+set GIT_VERSION=1.9.0.chromium.5
 
 if not "%DEPOT_TOOLS_GIT_BLEEDING%" == "1" goto :GIT_190_CHECK_REST
-set GIT_ERASE_TOP=3
-set GIT_VERSION=1.9.0.chromium.4
+set GIT_ERASE_TOP=4
+set GIT_VERSION=1.9.0.chromium.5
 
 :GIT_190_CHECK_REST
 if "%DEPOT_TOOLS_GIT_190%" == "0" goto :GIT_1852_CHECK
