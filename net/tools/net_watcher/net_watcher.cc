210e9ac7f97e29a89a1e3bf68f214cffeafa9830
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-03 06:59:15 +0000

    Tweak to r274378 to make net_watcher build on chromeos bots.
    BUG=380044
    TBR=rsleevi@chromium.org
    
    Review URL: https://codereview.chromium.org/315523002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274425 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 064064c88e45a8505821656cae6304178d410ae1

--- net/tools/net_watcher/net_watcher.cc
+++ net/tools/net_watcher/net_watcher.cc
@@ -21,7 +21,7 @@
 #include "net/proxy/proxy_config_service.h"
 #include "net/proxy/proxy_service.h"
 
-#if defined(USE_GLIB)
+#if defined(USE_GLIB) && !defined(OS_CHROMEOS)
 #include <glib-object.h>
 #endif
 
@@ -131,7 +131,7 @@ int main(int argc, char* argv[]) {
 #if defined(OS_MACOSX)
   base::mac::ScopedNSAutoreleasePool pool;
 #endif
-#if defined(USE_GLIB)
+#if defined(USE_GLIB) && !defined(OS_CHROMEOS)
   // g_type_init will be deprecated in 2.36. 2.35 is the development
   // version for 2.36, hence do not call g_type_init starting 2.35.
   // http://developer.gnome.org/gobject/unstable/gobject-Type-Information.html#g-type-init
@@ -140,7 +140,7 @@ int main(int argc, char* argv[]) {
   // Normally handled by BrowserMainLoop::InitializeToolkit().
   g_type_init();
 #endif
-#endif  // defined(USE_GLIB)
+#endif  // defined(USE_GLIB) && !defined(OS_CHROMEOS)
   base::AtExitManager exit_manager;
   base::CommandLine::Init(argc, argv);
   logging::LoggingSettings settings;
