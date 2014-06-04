bloomberg/chromium.bb

commit 7585ff7c78dcd90af200f6878c9fbca5bd63ba09

Move deferred loading logic from WebMediaPlayerClientImpl to HTMLMediaElement.
BUG=350571
TEST=All layout tests still pass.

Review URL: https://codereview.chromium.org/203213008

git-svn-id: svn://svn.chromium.org/blink/trunk@169669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: d2cb8136ba54d6d1b30a823a650bd1b610bc2543

third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -84,6 +84,7 @@
 
 using namespace std;
 using blink::WebInbandTextTrack;
+using blink::WebMediaPlayer;
 using blink::WebMimeRegistry;
 
 namespace WebCore {
@@ -273,6 +274,7 @@ HTMLMediaElement::HTMLMediaElement(const QualifiedName& tagName, Document& docum
     , m_closedCaptionsVisible(false)
     , m_completelyLoaded(false)
     , m_havePreparedToPlay(false)
+    , m_delayingLoadForPreloadNone(false)
     , m_tracksAreReady(true)
     , m_haveVisibleTextTrack(false)
     , m_processingPreferenceChange(false)
@@ -413,7 +415,7 @@ void HTMLMediaElement::parseAttribute(const QualifiedName& name, const AtomicStr
 
         // The attribute must be ignored if the autoplay attribute is present
         if (!autoplay() && m_player)
-            m_player->setPreload(m_preload);
+            setPlayerPreload();
 
     } else if (name == mediagroupAttr)
         setMediaGroup(value);
@@ -829,15 +831,13 @@ void HTMLMediaElement::loadResource(const KURL& url, ContentType& contentType, c
 
     WTF_LOG(Media, "HTMLMediaElement::loadResource - m_currentSrc -> %s", urlForLoggingMedia(m_currentSrc).utf8().data());
 
-    blink::WebMediaPlayer::LoadType loadType = blink::WebMediaPlayer::LoadTypeURL;
-
     startProgressEventTimer();
 
     // Reset display mode to force a recalculation of what to show because we are resetting the player.
     setDisplayMode(Unknown);
 
     if (!autoplay())
-        m_player->setPreload(m_preload);
+        setPlayerPreload();
 
     if (fastHasAttribute(mutedAttr))
         m_muted = true;
@@ -849,14 +849,11 @@ void HTMLMediaElement::loadResource(const KURL& url, ContentType& contentType, c
 
     if (url.protocolIs(mediaSourceBlobProtocol)) {
         if (isMediaStreamURL(url.string())) {
-            loadType = blink::WebMediaPlayer::LoadTypeMediaStream;
             m_userGestureRequiredForPlay = false;
         } else {
             m_mediaSource = HTMLMediaSource::lookup(url.string());
 
             if (m_mediaSource) {
-                loadType = blink::WebMediaPlayer::LoadTypeMediaSource;
-
                 if (!m_mediaSource->attachToElement(this)) {
                     // Forget our reference to the MediaSource, so we leave it alone
                     // while processing remainder of load failure.
@@ -868,7 +865,13 @@ void HTMLMediaElement::loadResource(const KURL& url, ContentType& contentType, c
     }
 
     if (attemptLoad && canLoadURL(url, contentType, keySystem)) {
-        m_player->load(loadType, url);
+        ASSERT(!webMediaPlayer());
+
+        if (m_preload == MediaPlayer::None) {
+            m_delayingLoadForPreloadNone = true;
+        } else {
+            m_player->load(loadType(), m_currentSrc, corsMode());
+        }
     } else {
         mediaLoadingFailed(MediaPlayer::FormatError);
     }
@@ -879,6 +882,34 @@ void HTMLMediaElement::loadResource(const KURL& url, ContentType& contentType, c
 
     if (renderer())
         renderer()->updateFromElement();
+}
+
+void HTMLMediaElement::setPlayerPreload()
+{
+    m_player->setPreload(m_preload);
+
+    if (m_delayingLoadForPreloadNone && m_preload != MediaPlayer::None)
+        startDelayedLoad();
+}
+
+void HTMLMediaElement::startDelayedLoad()
+{
+    ASSERT(m_delayingLoadForPreloadNone);
+
+    m_delayingLoadForPreloadNone = false;
+
+    m_player->load(loadType(), m_currentSrc, corsMode());
+}
+
+WebMediaPlayer::LoadType HTMLMediaElement::loadType() const
+{
+    if (m_mediaSource)
+        return WebMediaPlayer::LoadTypeMediaSource;
+
+    if (isMediaStreamURL(m_currentSrc.string()))
+        return WebMediaPlayer::LoadTypeMediaStream;
+
+    return WebMediaPlayer::LoadTypeURL;
 }
 
 static bool trackIndexCompare(TextTrack* a,
@@ -1667,7 +1698,9 @@ void HTMLMediaElement::prepareToPlay()
     if (m_havePreparedToPlay)
         return;
     m_havePreparedToPlay = true;
-    m_player->prepareToPlay();
+
+    if (m_delayingLoadForPreloadNone)
+        startDelayedLoad();
 }
 
 void HTMLMediaElement::seek(double time, ExceptionState& exceptionState)
@@ -3119,6 +3152,8 @@ void HTMLMediaElement::clearMediaPlayer(int flags)
 
     closeMediaSource();
 
+    m_delayingLoadForPreloadNone = false;
+
     clearMediaPlayerAndAudioSourceProviderClient();
 
     stopPeriodicTimers();
@@ -3579,14 +3614,14 @@ void HTMLMediaElement::applyMediaFragmentURI()
     }
 }
 
-MediaPlayerClient::CORSMode HTMLMediaElement::mediaPlayerCORSMode() const
+WebMediaPlayer::CORSMode HTMLMediaElement::corsMode() const
 {
     const AtomicString& crossOriginMode = fastGetAttribute(crossoriginAttr);
     if (crossOriginMode.isNull())
-        return Unspecified;
+        return WebMediaPlayer::CORSModeUnspecified;
     if (equalIgnoringCase(crossOriginMode, "use-credentials"))
-        return UseCredentials;
-    return Anonymous;
+        return WebMediaPlayer::CORSModeUseCredentials;
+    return WebMediaPlayer::CORSModeAnonymous;
 }
 
 void HTMLMediaElement::mediaPlayerSetWebLayer(blink::WebLayer* webLayer)

third_party/WebKit/Source/core/html/HTMLMediaElement.h
@@ -326,9 +326,6 @@ private:
     virtual void mediaPlayerRequestSeek(double) OVERRIDE FINAL;
     virtual void mediaPlayerRepaint() OVERRIDE FINAL;
     virtual void mediaPlayerSizeChanged() OVERRIDE FINAL;
-
-    virtual CORSMode mediaPlayerCORSMode() const OVERRIDE FINAL;
-
     virtual void mediaPlayerSetWebLayer(blink::WebLayer*) OVERRIDE FINAL;
     virtual void mediaPlayerSetOpaque(bool) OVERRIDE FINAL;
     virtual void mediaPlayerMediaSourceOpened(blink::WebMediaSource*) OVERRIDE FINAL;
@@ -353,6 +350,9 @@ private:
     void loadInternal();
     void selectMediaResource();
     void loadResource(const KURL&, ContentType&, const String& keySystem);
+    void setPlayerPreload();
+    void startDelayedLoad();
+    blink::WebMediaPlayer::LoadType loadType() const;
     void scheduleNextSourceChild();
     void loadNextSourceChild();
     void userCancelledLoad();
@@ -413,6 +413,8 @@ private:
     bool isBlocked() const;
     bool isBlockedOnMediaController() const;
     bool isAutoplaying() const { return m_autoplaying; }
+
+    blink::WebMediaPlayer::CORSMode corsMode() const;
 
     Timer<HTMLMediaElement> m_loadTimer;
     Timer<HTMLMediaElement> m_progressEventTimer;
@@ -492,6 +494,7 @@ private:
 
     bool m_completelyLoaded : 1;
     bool m_havePreparedToPlay : 1;
+    bool m_delayingLoadForPreloadNone : 1;
 
     bool m_tracksAreReady : 1;
     bool m_haveVisibleTextTrack : 1;

third_party/WebKit/Source/platform/graphics/media/MediaPlayer.h
@@ -58,8 +58,6 @@ typedef int GC3Dint;
 
 class MediaPlayerClient {
 public:
-    enum CORSMode { Unspecified, Anonymous, UseCredentials };
-
     virtual ~MediaPlayerClient() { }
 
     // the network state has changed
@@ -92,8 +90,6 @@ public:
     // the movie size has changed
     virtual void mediaPlayerSizeChanged() = 0;
 
-    virtual CORSMode mediaPlayerCORSMode() const = 0;
-
     virtual void mediaPlayerSetWebLayer(blink::WebLayer*) = 0;
     virtual void mediaPlayerSetOpaque(bool) = 0;
 
@@ -116,9 +112,7 @@ public:
     MediaPlayer() { }
     virtual ~MediaPlayer() { }
 
-    virtual void load(blink::WebMediaPlayer::LoadType, const String& url) = 0;
-
-    virtual void prepareToPlay() = 0;
+    virtual void load(blink::WebMediaPlayer::LoadType, const String& url, blink::WebMediaPlayer::CORSMode) = 0;
 
     virtual void play() = 0;
     virtual void pause() = 0;

third_party/WebKit/Source/web/AssertMatchingEnums.cpp
@@ -575,10 +575,6 @@ COMPILE_ASSERT_MATCHING_ENUM(WebURLResponse::HTTP_0_9, ResourceResponse::HTTP_0_
 COMPILE_ASSERT_MATCHING_ENUM(WebURLResponse::HTTP_1_0, ResourceResponse::HTTP_1_0);
 COMPILE_ASSERT_MATCHING_ENUM(WebURLResponse::HTTP_1_1, ResourceResponse::HTTP_1_1);
 
-COMPILE_ASSERT_MATCHING_ENUM(WebMediaPlayer::CORSModeUnspecified, MediaPlayerClient::Unspecified);
-COMPILE_ASSERT_MATCHING_ENUM(WebMediaPlayer::CORSModeAnonymous, MediaPlayerClient::Anonymous);
-COMPILE_ASSERT_MATCHING_ENUM(WebMediaPlayer::CORSModeUseCredentials, MediaPlayerClient::UseCredentials);
-
 COMPILE_ASSERT_MATCHING_ENUM(WebFormElement::AutocompleteResultSuccess, HTMLFormElement::AutocompleteResultSuccess);
 COMPILE_ASSERT_MATCHING_ENUM(WebFormElement::AutocompleteResultErrorDisabled, HTMLFormElement::AutocompleteResultErrorDisabled);
 COMPILE_ASSERT_MATCHING_ENUM(WebFormElement::AutocompleteResultErrorCancel, HTMLFormElement::AutocompleteResultErrorCancel);

third_party/WebKit/Source/web/WebMediaPlayerClientImpl.cpp
@@ -178,27 +178,9 @@ void WebMediaPlayerClientImpl::requestSeek(double time)
 }
 
 // MediaPlayer -------------------------------------------------
-
-void WebMediaPlayerClientImpl::load(WebMediaPlayer::LoadType loadType, const WTF::String& url)
+void WebMediaPlayerClientImpl::load(WebMediaPlayer::LoadType loadType, const WTF::String& url, WebMediaPlayer::CORSMode corsMode)
 {
-    m_url = KURL(ParsedURLString, url);
-    m_loadType = loadType;
-
-    if (m_preload == MediaPlayer::None) {
-#if ENABLE(WEB_AUDIO)
-        m_audioSourceProvider.wrap(0); // Clear weak reference to m_webMediaPlayer's WebAudioSourceProvider.
-#endif
-        m_webMediaPlayer.clear();
-        m_delayingLoad = true;
-    } else
-        loadInternal();
-}
-
-void WebMediaPlayerClientImpl::loadInternal()
-{
-#if ENABLE(WEB_AUDIO)
-    m_audioSourceProvider.wrap(0); // Clear weak reference to m_webMediaPlayer's WebAudioSourceProvider.
-#endif
+    ASSERT(!m_webMediaPlayer);
 
     // FIXME: Remove this cast
     LocalFrame* frame = mediaElement().document().frame();
@@ -211,24 +193,27 @@ void WebMediaPlayerClientImpl::loadInternal()
     // if necessary.
     m_needsWebLayerForVideo = frame->contentRenderer()->compositor()->hasAcceleratedCompositing();
 
-    m_webMediaPlayer = createWebMediaPlayer(this, m_url, frame);
-    if (m_webMediaPlayer) {
+    KURL kurl(ParsedURLString, url);
+    m_webMediaPlayer = createWebMediaPlayer(this, kurl, frame);
+    if (!m_webMediaPlayer)
+        return;
+
 #if ENABLE(WEB_AUDIO)
-        // Make sure if we create/re-create the WebMediaPlayer that we update our wrapper.
-        m_audioSourceProvider.wrap(m_webMediaPlayer->audioSourceProvider());
+    // Make sure if we create/re-create the WebMediaPlayer that we update our wrapper.
+    m_audioSourceProvider.wrap(m_webMediaPlayer->audioSourceProvider());
 #endif
 
-        m_webMediaPlayer->setVolume(mediaElement().playerVolume());
+    m_webMediaPlayer->setVolume(mediaElement().playerVolume());
 
-        // Tell WebMediaPlayer about the poster image URL.
-        m_webMediaPlayer->setPoster(poster);
+    m_webMediaPlayer->setPoster(poster);
 
-        // Tell WebMediaPlayer about any connected CDM (may be null).
-        m_webMediaPlayer->setContentDecryptionModule(HTMLMediaElementEncryptedMedia::contentDecryptionModule(mediaElement()));
+#if OS(ANDROID)
+    m_usePaintOnAndroid = (loadType != WebMediaPlayer::LoadTypeMediaStream);
+#endif
 
-        WebMediaPlayer::CORSMode corsMode = static_cast<WebMediaPlayer::CORSMode>(m_client->mediaPlayerCORSMode());
-        m_webMediaPlayer->load(m_loadType, m_url, corsMode);
-    }
+    // Tell WebMediaPlayer about any connected CDM (may be null).
+    m_webMediaPlayer->setContentDecryptionModule(HTMLMediaElementEncryptedMedia::contentDecryptionModule(mediaElement()));
+    m_webMediaPlayer->load(loadType, kurl, corsMode);
 }
 
 void WebMediaPlayerClientImpl::play()
@@ -258,12 +243,6 @@ void WebMediaPlayerClientImpl::hideFullscreenOverlay()
 bool WebMediaPlayerClientImpl::canShowFullscreenOverlay() const
 {
     return m_webMediaPlayer && m_webMediaPlayer->canEnterFullscreen();
-}
-
-void WebMediaPlayerClientImpl::prepareToPlay()
-{
-    if (m_delayingLoad)
-        startDelayedLoad();
 }
 
 IntSize WebMediaPlayerClientImpl::naturalSize() const
@@ -388,7 +367,7 @@ void WebMediaPlayerClientImpl::paint(GraphicsContext* context, const IntRect& re
         // On Android, video frame is emitted as GL_TEXTURE_EXTERNAL_OES texture. We use a different path to
         // paint the video frame into the context.
 #if OS(ANDROID)
-        if (m_loadType != WebMediaPlayer::LoadTypeMediaStream) {
+        if (m_usePaintOnAndroid) {
             paintOnAndroid(context, rect, context->getNormalizedAlpha());
             return;
         }
@@ -414,9 +393,6 @@ void WebMediaPlayerClientImpl::setPreload(MediaPlayer::Preload preload)
 
     if (m_webMediaPlayer)
         m_webMediaPlayer->setPreload(static_cast<WebMediaPlayer::Preload>(preload));
-
-    if (m_delayingLoad && m_preload != MediaPlayer::None)
-        startDelayedLoad();
 }
 
 bool WebMediaPlayerClientImpl::hasSingleSecurityOrigin() const
@@ -529,23 +505,14 @@ void WebMediaPlayerClientImpl::paintOnAndroid(WebCore::GraphicsContext* context,
 }
 #endif
 
-void WebMediaPlayerClientImpl::startDelayedLoad()
-{
-    ASSERT(m_delayingLoad);
-    ASSERT(!m_webMediaPlayer);
-
-    m_delayingLoad = false;
-
-    loadInternal();
-}
-
 WebMediaPlayerClientImpl::WebMediaPlayerClientImpl(MediaPlayerClient* client)
     : m_client(client)
-    , m_delayingLoad(false)
     , m_preload(MediaPlayer::Auto)
     , m_needsWebLayerForVideo(false)
     , m_rate(1.0)
-    , m_loadType(WebMediaPlayer::LoadTypeURL)
+#if OS(ANDROID)
+    , m_usePaintOnAndroid(false)
+#endif
 {
     ASSERT(m_client);
 }

third_party/WebKit/Source/web/WebMediaPlayerClientImpl.h
@@ -95,10 +95,9 @@ public:
 
     // MediaPlayer methods:
     virtual WebMediaPlayer* webMediaPlayer() const OVERRIDE;
-    virtual void load(WebMediaPlayer::LoadType, const WTF::String& url) OVERRIDE;
+    virtual void load(WebMediaPlayer::LoadType, const WTF::String& url, WebMediaPlayer::CORSMode) OVERRIDE;
     virtual void play() OVERRIDE;
     virtual void pause() OVERRIDE;
-    virtual void prepareToPlay() OVERRIDE;
     virtual bool supportsSave() const OVERRIDE;
     virtual WebCore::IntSize naturalSize() const OVERRIDE;
     virtual bool hasVideo() const OVERRIDE;
@@ -138,10 +137,13 @@ public:
 private:
     explicit WebMediaPlayerClientImpl(WebCore::MediaPlayerClient*);
 
-    void startDelayedLoad();
-    void loadInternal();
-
     WebCore::HTMLMediaElement& mediaElement() const;
+
+    WebCore::MediaPlayerClient* m_client;
+    OwnPtr<WebMediaPlayer> m_webMediaPlayer;
+    WebCore::MediaPlayer::Preload m_preload;
+    bool m_needsWebLayerForVideo;
+    double m_rate;
 
 #if OS(ANDROID)
     // FIXME: This path "only works" on Android. It is a workaround for the problem that Skia could not handle Android's GL_TEXTURE_EXTERNAL_OES
@@ -149,15 +151,8 @@ private:
     // https://code.google.com/p/skia/issues/detail?id=1189
     void paintOnAndroid(WebCore::GraphicsContext*, const WebCore::IntRect&, uint8_t alpha);
     SkBitmap m_bitmap;
+    bool m_usePaintOnAndroid;
 #endif
-
-    WebCore::MediaPlayerClient* m_client;
-    OwnPtr<WebMediaPlayer> m_webMediaPlayer;
-    WebCore::KURL m_url;
-    bool m_delayingLoad;
-    WebCore::MediaPlayer::Preload m_preload;
-    bool m_needsWebLayerForVideo;
-    double m_rate;
 
 #if ENABLE(WEB_AUDIO)
     // AudioClientImpl wraps an AudioSourceProviderClient.
@@ -206,8 +201,6 @@ private:
 
     AudioSourceProviderImpl m_audioSourceProvider;
 #endif
-
-    WebMediaPlayer::LoadType m_loadType;
 };
 
 } // namespace blink

commit 5c9b922e1a3fbb0fe44f3af8e7896cf94959a2c2

Don't delay loading if autoplay is set and preload equals 'none'.
This fixes a regression that was accidentally introduced in r169669.
If autoplay is set, then loading should not be delayed even if preload
was set to "none".

BUG=372770, 378682
TEST=LayoutTests/media/autoplay-with-preload-none.html

Review URL: https://codereview.chromium.org/313963004

git-svn-id: svn://svn.chromium.org/blink/trunk@175517 bbb929c8-8fbe-4397-9dbb-9b2b20218538

Former-commit-id: ef8357c07a6ec1506e1dda4b9bc3babaf78ec115

third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -889,7 +889,7 @@ void HTMLMediaElement::loadResource(const KURL& url, ContentType& contentType, c
     if (attemptLoad && canLoadURL(url, contentType, keySystem)) {
         ASSERT(!webMediaPlayer());
 
-        if (m_preload == MediaPlayer::None)
+        if (!autoplay() && m_preload == MediaPlayer::None)
             m_delayingLoadForPreloadNone = true;
         else
             startPlayerLoad();

third_party/WebKit/LayoutTests/media/autoplay-with-preload-none-expected.txt
@@ -1,0 +1,5 @@
+This is a testharness.js-based test.
+PASS audio.autoplay with preload="none" 
+PASS video.autoplay with preload="none" 
+Harness: the test ran to completion.
+

third_party/WebKit/LayoutTests/media/autoplay-with-preload-none.html
@@ -1,0 +1,36 @@
+<!doctype html>
+<title>autoplay with preload set to none.</title>
+<script src="../resources/testharness.js"></script>
+<script src="../resources/testharnessreport.js"></script>
+<script src="media-file.js"></script>
+<div id="log"></div>
+<script>
+function autoplay_test(tagName, src)
+{
+    async_test(function(t)
+    {
+        var e = document.createElement(tagName);
+        e.src = src;
+        e.preload = "none";
+        e.autoplay = true;
+        var actual_events = [];
+        var expected_events = ['canplay', 'play', 'playing', 'canplaythrough'];
+        expected_events.forEach(function(type)
+        {
+            e.addEventListener(type, t.step_func(function()
+            {
+                assert_equals(e.readyState, e.HAVE_ENOUGH_DATA);
+                assert_false(e.paused);
+                actual_events.push(type);
+                if (type == 'canplaythrough') {
+                    assert_array_equals(actual_events, expected_events);
+                    t.done();
+                }
+            }));
+        });
+    }, tagName + '.autoplay with preload="none"');
+}
+
+autoplay_test('audio', findMediaFile('audio', 'content/test'));
+autoplay_test('video', findMediaFile('video', 'content/test'));
+</script>