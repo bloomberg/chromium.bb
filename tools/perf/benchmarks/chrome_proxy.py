7106fd80aa232b626583ac5f5406900c5ec1dbb9
nednguyen@google.com <nednguyen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 22:11:18 +0000

    Convert page_set in chrome_proxy benchmarks to use the actual page_set objects.
    BUG=362293
    
    Review URL: https://codereview.chromium.org/315083002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274924 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3cb06ab80f056537e806d7c5459357a7ea020ba4

--- tools/perf/benchmarks/chrome_proxy.py
+++ tools/perf/benchmarks/chrome_proxy.py
@@ -4,12 +4,17 @@
 
 from telemetry import test
 from measurements import chrome_proxy
-
+from page_sets.chrome_proxy import bypass
+from page_sets.chrome_proxy import fallback_viaheader
+from page_sets.chrome_proxy import safebrowsing
+from page_sets.chrome_proxy import smoke
+from page_sets.chrome_proxy import synthetic
+from page_sets.chrome_proxy import top_20
 
 class ChromeProxyLatency(test.Test):
   tag = 'latency'
   test = chrome_proxy.ChromeProxyLatency
-  page_set = 'page_sets/chrome_proxy/top_20.py'
+  page_set = top_20.Top20PageSet()
   options = {'pageset_repeat_iters': 2}
 
   def CustomizeBrowserOptions(self, options):
@@ -19,22 +24,22 @@ class ChromeProxyLatency(test.Test):
 class ChromeProxyLatencyDirect(test.Test):
   tag = 'latency_direct'
   test = chrome_proxy.ChromeProxyLatency
-  page_set = 'page_sets/chrome_proxy/top_20.py'
+  page_set = top_20.Top20PageSet()
   options = {'pageset_repeat_iters': 2}
 
 
 class ChromeProxyLatencySynthetic(ChromeProxyLatency):
-  page_set = 'page_sets/chrome_proxy/synthetic.py'
+  page_set = synthetic.SyntheticPageSet()
 
 
 class ChromeProxyLatencySyntheticDirect(ChromeProxyLatencyDirect):
-  page_set = 'page_sets/chrome_proxy/synthetic.py'
+  page_set = synthetic.SyntheticPageSet()
 
 
 class ChromeProxyDataSaving(test.Test):
   tag = 'data_saving'
   test = chrome_proxy.ChromeProxyDataSaving
-  page_set = 'page_sets/chrome_proxy/top_20.py'
+  page_set = top_20.Top20PageSet()
   options = {'pageset_repeat_iters': 1}
   def CustomizeBrowserOptions(self, options):
     options.AppendExtraBrowserArgs('--enable-spdy-proxy-auth')
@@ -44,48 +49,48 @@ class ChromeProxyDataSavingDirect(test.Test):
   tag = 'data_saving_direct'
   test = chrome_proxy.ChromeProxyDataSaving
   options = {'pageset_repeat_iters': 2}
-  page_set = 'page_sets/chrome_proxy/top_20.py'
+  page_set = top_20.Top20PageSet()
 
 
 class ChromeProxyDataSavingSynthetic(ChromeProxyDataSaving):
-  page_set = 'page_sets/chrome_proxy/synthetic.py'
+  page_set = synthetic.SyntheticPageSet()
 
 
 class ChromeProxyDataSavingSyntheticDirect(ChromeProxyDataSavingDirect):
-  page_set = 'page_sets/chrome_proxy/synthetic.py'
+  page_set = synthetic.SyntheticPageSet()
 
 
 class ChromeProxyHeaderValidation(test.Test):
   tag = 'header_validation'
   test = chrome_proxy.ChromeProxyHeaders
-  page_set = 'page_sets/chrome_proxy/top_20.py'
+  page_set = top_20.Top20PageSet()
 
 
 class ChromeProxyBypass(test.Test):
   tag = 'bypass'
   test = chrome_proxy.ChromeProxyBypass
-  page_set = 'page_sets/chrome_proxy/bypass.py'
+  page_set = bypass.BypassPageSet()
 
 
 class ChromeProxySafeBrowsing(test.Test):
   tag = 'safebrowsing'
   test = chrome_proxy.ChromeProxySafebrowsing
-  page_set = 'page_sets/chrome_proxy/safebrowsing.py'
+  page_set = safebrowsing.SafebrowsingPageSet()
 
 
 class ChromeProxyHTTPFallbackProbeURL(test.Test):
   tag = 'fallback-probe'
   test = chrome_proxy.ChromeProxyHTTPFallbackProbeURL
-  page_set = 'page_sets/chrome_proxy/synthetic.py'
+  page_set = synthetic.SyntheticPageSet()
 
 
 class ChromeProxyHTTPFallbackViaHeader(test.Test):
   tag = 'fallback-viaheader'
   test = chrome_proxy.ChromeProxyHTTPFallbackViaHeader
-  page_set = 'page_sets/chrome_proxy/fallback_viaheader.py'
+  page_set = fallback_viaheader.FallbackViaHeaderPageSet()
 
 
 class ChromeProxySmoke(test.Test):
   tag = 'smoke'
   test = chrome_proxy.ChromeProxySmoke
-  page_set = 'page_sets/chrome_proxy/smoke.py'
+  page_set = smoke.SmokePageSet()
