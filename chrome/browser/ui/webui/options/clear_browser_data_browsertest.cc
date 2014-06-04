bf6ddd5de367722ce45ec3fd11a1d651d39a8906
mek@chromium.org <mek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 21:10:12 +0000

    ClearBrowserDataBrowserTest.CommitButtonDisabledWhileDeletionInProgress is still extremely flaky on all bots, so disabling again.
    http://chromium-build-logs.appspot.com/gtest_query?gtest_query=ClearBrowserDataBrowserTest.CommitButtonDisabledWhileDeletionInProgress
    
    BUG=379574
    TBR=engedy@chromium.org
    NOTRY=true
    NOTREECHECKS=true
    
    Review URL: https://codereview.chromium.org/316053003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274904 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 34aa923b27ffa622ac429f0bf31d8477fd362514

--- chrome/browser/ui/webui/options/clear_browser_data_browsertest.cc
+++ chrome/browser/ui/webui/options/clear_browser_data_browsertest.cc
@@ -51,8 +51,9 @@ class ClearBrowserDataBrowserTest : public OptionsUIBrowserTest {
   }
 };
 
+// Fails flakily. http://crbug.com/379574
 IN_PROC_BROWSER_TEST_F(ClearBrowserDataBrowserTest,
-                       CommitButtonDisabledWhileDeletionInProgress) {
+                       DISABLED_CommitButtonDisabledWhileDeletionInProgress) {
   const char kTimePeriodSelectorId[] = "#clear-browser-data-time-period";
   const char kCommitButtonId[] = "#clear-browser-data-commit";
   BrowsingDataRemoverCompletionInhibitor completion_inhibitor;
