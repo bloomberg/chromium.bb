b905d5f0234c09c2f6250be17db96b3fedbf8d30
nednguyen@google.com <nednguyen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 12:35:38 +0000

    Enable the page_set of benchmark test file to use actual page set object. This allows us to remove the page_set.FromFile(..) method.
    BUG=362293
    
    Review URL: https://codereview.chromium.org/312883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274792 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3d0638544c06118333e0c5d0d74789217fed18e9

--- tools/perf/benchmarks/memory.py
+++ tools/perf/benchmarks/memory.py
@@ -4,26 +4,30 @@
 from telemetry import test
 
 from measurements import memory
+from page_sets import mobile_memory
+from page_sets import top_25
+from page_sets import top_desktop_sites_2012Q3
+from page_sets import tough_dom_memory_cases
 
 
 @test.Disabled('android')  # crbug.com/370977
 class MemoryMobile(test.Test):
   test = memory.Memory
-  page_set = 'page_sets/mobile_memory.py'
+  page_set = mobile_memory.MobileMemoryPageSet()
 
 
 class MemoryTop25(test.Test):
   test = memory.Memory
-  page_set = 'page_sets/top_25.py'
+  page_set = top_25.Top25PageSet()
 
 
 class Reload2012Q3(test.Test):
   tag = 'reload'
   test = memory.Memory
-  page_set = 'page_sets/top_desktop_sites_2012Q3.py'
+  page_set = top_desktop_sites_2012Q3.Top2012Q3PageSet()
 
 
 @test.Disabled('android')  # crbug.com/371153
 class MemoryToughDomMemoryCases(test.Test):
   test = memory.Memory
-  page_set = 'page_sets/tough_dom_memory_cases.py'
+  page_set = tough_dom_memory_cases.ToughDomMemoryCasesPageSet()
