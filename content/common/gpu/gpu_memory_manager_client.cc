55419835cb3aea8c99620c1ec35edda753e52621
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 18:54:24 +0000

    Revert 274326 "Lobotomize the GPU memory manager" BUG=380666
    > Lobotomize the GPU memory manager
    > 
    > On desktop, always set the memory limit to 256MB, and ask that the renderer
    > only use that memory to draw content that is near the viewport. The effective
    > limit prior to this patch was 384MB, but would quickly get cut down as multiple
    > windows are opened, but would not fall too much lower than 256MB (e.g, 128
    > on Mac).
    > 
    > On mobile, use all available memory for the current, only, renderer.
    > 
    > Do not take into account unmanaged (e.g, WebGL) memory usage.
    > 
    > BUG=377065
    > 
    > Review URL: https://codereview.chromium.org/308743005
    
    TBR=ccameron@chromium.org
    
    Review URL: https://codereview.chromium.org/313163002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274879 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fee1504370b2a009015985cdd84caf12e0c7ec6e

--- content/common/gpu/gpu_memory_manager_client.cc
+++ content/common/gpu/gpu_memory_manager_client.cc
@@ -20,6 +20,13 @@ GpuMemoryManagerClientState::GpuMemoryManagerClientState(
       has_surface_(has_surface),
       visible_(visible),
       list_iterator_valid_(false),
+      managed_memory_stats_received_(false),
+      bytes_nicetohave_limit_low_(0),
+      bytes_nicetohave_limit_high_(0),
+      bytes_allocation_when_visible_(0),
+      bytes_allocation_ideal_nicetohave_(0),
+      bytes_allocation_ideal_required_(0),
+      bytes_allocation_ideal_minimum_(0),
       hibernated_(false) {
 }
 
