a58c26f14efb731429de75c0b26317e5a85c0460
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-04 23:47:16 +0000

    Update DoYouEvenBench to Speedometer.
    BUG=365446
    
    Review URL: https://codereview.chromium.org/310343002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@274955 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0e8f4c21f08c73acbfb3f03b36fe9d6ec87b532c

---
+++ tools/perf/benchmarks/speedometer.py
@@ -0,0 +1,46 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Apple's Speedometer performance benchmark.
+
+Speedometer measures simulated user interactions in web applications.
+
+The current benchmark uses TodoMVC to simulate user actions for adding,
+completing, and removing to-do items. Speedometer repeats the same actions using
+DOM APIs - a core set of web platform APIs used extensively in web applications-
+as well as six popular JavaScript frameworks: Ember.js, Backbone.js, jQuery,
+AngularJS, React, and Flight. Many of these frameworks are used on the most
+popular websites in the world, such as Facebook and Twitter. The performance of
+these types of operations depends on the speed of the DOM APIs, the JavaScript
+engine, CSS style resolution, layout, and other technologies.
+"""
+
+import os
+
+from telemetry import test
+from telemetry.page import page_measurement
+from telemetry.page import page_set
+
+
+class SpeedometerMeasurement(page_measurement.PageMeasurement):
+
+  def MeasurePage(self, _, tab, results):
+    tab.WaitForDocumentReadyStateToBeComplete()
+    tab.ExecuteJavaScript('benchmarkClient.iterationCount = 10; startTest();')
+    tab.WaitForJavaScriptExpression(
+        'benchmarkClient._finishedTestCount == benchmarkClient.testsCount', 600)
+    results.Add(
+        'Total', 'ms', tab.EvaluateJavaScript('benchmarkClient._timeValues'))
+
+
+class Speedometer(test.Test):
+  test = SpeedometerMeasurement
+
+  def CreatePageSet(self, options):
+    ps = page_set.PageSet(
+        file_path=os.path.abspath(__file__),
+        archive_data_file='../page_sets/data/speedometer.json',
+        make_javascript_deterministic=False)
+    ps.AddPageWithDefaultRunNavigate('http://browserbench.org/Speedometer/')
+    return ps
