57b349bca2cc3c50e146c4364108ddf43d0a4082
dgozman@chromium.org <dgozman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-05 15:20:57 +0000

    Do not set touch region on PageOverlay layer.
    With new touch hit testing in compositor, which looks through all
    the layers hitting the point, we don't need touch regions on overlay
    layer anymore.
    
    Note, that we still need main-thread scrolling to stay in sync with
    page contents.
    
    BUG=304271
    
    Review URL: https://codereview.chromium.org/305383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175578 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: aae8e39705dd1b2b53430e092a83f7de41ef3d83

--- third_party/WebKit/Source/web/PageOverlay.cpp
+++ third_party/WebKit/Source/web/PageOverlay.cpp
@@ -122,14 +122,9 @@ void PageOverlay::update()
         if (WebCore::Page* page = m_viewImpl->page())
             page->inspectorController().willAddPageOverlay(m_layer.get());
 
-        // Compositor hit-testing does not know how to deal with layers that may be
-        // transparent to events (see http://crbug.com/269598). So require
-        // scrolling and touches on this layer to go to the main thread.
+        // This is required for contents of overlay to stay in sync with the page while scrolling.
         WebLayer* platformLayer = m_layer->platformLayer();
         platformLayer->setShouldScrollOnMainThread(true);
-        WebVector<WebRect> webRects(static_cast<size_t>(1));
-        webRects[0] = WebRect(0, 0, INT_MAX, INT_MAX);
-        platformLayer->setTouchEventHandlerRegion(webRects);
     }
 
     FloatSize size(m_viewImpl->size());
