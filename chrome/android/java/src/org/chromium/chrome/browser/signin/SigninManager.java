4fa209becdcc528b2e9d23647946ff8b7f133fae
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 14:11:08 +0000

    Avoid calling Activity.isDestroyed().
    That call is only available since API level 17.
    
    This refactoring replaces SigninManager.Observer with SigninManager.Delegate,
    which has the same callbacks and 2 additional calls to get the Activity to use
    for sign-in and another to check if that Activity hasn't been destroyed next.
    
    BUG=380210
    
    Review URL: https://codereview.chromium.org/312923002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275089 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1bbbe75dad238c4d06af34d126e74cb7e7a6c52c

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
@@ -13,6 +13,8 @@ import android.content.DialogInterface;
 import android.os.Handler;
 import android.util.Log;
 
+import org.chromium.base.ActivityState;
+import org.chromium.base.ApplicationStatus;
 import org.chromium.base.CalledByNative;
 import org.chromium.base.ObserverList;
 import org.chromium.base.ThreadUtils;
@@ -204,7 +206,7 @@ public class SigninManager {
             return;
         }
 
-        if (mSignInActivity.isDestroyed()) {
+        if (ApplicationStatus.getStateForActivity(mSignInActivity) == ActivityState.DESTROYED) {
             // The activity is no longer running, cancel sign in.
             cancelSignIn();
             return;
