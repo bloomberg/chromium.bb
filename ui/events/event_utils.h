b6e77981ca39e8112f105653f5a409553552ee06
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 20:11:22 +0000

    Revert the code that should not have been included in r275101
    BUG=379171
    TBR=sadrul@chromium.org
    
    Review URL: https://codereview.chromium.org/311053010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275228 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 06d28020a12597c1381daa0542938f3320e849bc

--- ui/events/event_utils.h
+++ ui/events/event_utils.h
@@ -162,11 +162,6 @@ EVENTS_EXPORT bool IsMouseEventFromTouch(UINT message);
 EVENTS_EXPORT uint16 GetScanCodeFromLParam(LPARAM lParam);
 EVENTS_EXPORT LPARAM GetLParamFromScanCode(uint16 scan_code);
 
-// Overrides the ui::KeyEvent's flags for test.
-// They're not exported to prevent it from leaking into production code.
-void SetKeyEventFlagsForTest(int flags);
-void ResetKeyEventFlagsForTest();
-
 #endif
 
 // Registers a custom event type.
