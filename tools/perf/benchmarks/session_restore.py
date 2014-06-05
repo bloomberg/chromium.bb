d6169e2547a1147919e9fb02db19efecb57a4975
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 16:53:47 +0000

    [Telemetry] Enable session restore benchmark on linux.
    I believe it was disabled because the clear_system_cache binary wasn't checked
    into cloud storage yet. It is now.
    
    BUG=325479
    
    Review URL: https://codereview.chromium.org/311053006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275163 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9efa42ae172e240c01be9caed5bb942bc0459494

--- tools/perf/benchmarks/session_restore.py
+++ tools/perf/benchmarks/session_restore.py
@@ -6,8 +6,7 @@ from measurements import session_restore
 from telemetry import test
 
 
-# crbug.com/325479: Disabling this test for now since it never ran before.
-@test.Disabled('android', 'linux')
+@test.Disabled('android')  # crbug.com/325479
 class SessionRestoreColdTypical25(test.Test):
   tag = 'cold'
   test = session_restore.SessionRestore
