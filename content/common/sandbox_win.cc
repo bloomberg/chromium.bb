40cfbf2d8352c23ba1985f51a696492c34958bc0
wfh@chromium.org <wfh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 07:25:34 +0000

    Add assistant_x64.dll to renderer process DLL blacklist.
    This is causing a lot of crashes on Win64.
    
    BUG=380963
    
    Review URL: https://codereview.chromium.org/317633008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275037 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a0f2c2cec26c06683c3c71c4766bc9510bdc36fb

--- content/common/sandbox_win.cc
+++ content/common/sandbox_win.cc
@@ -41,10 +41,12 @@ namespace {
 // For more information about how this list is generated, and how to get off
 // of it, see:
 // https://sites.google.com/a/chromium.org/dev/Home/third-party-developers
+// If the size of this list exceeds 64, change kTroublesomeDllsMaxCount.
 const wchar_t* const kTroublesomeDlls[] = {
   L"adialhk.dll",                 // Kaspersky Internet Security.
   L"acpiz.dll",                   // Unknown.
   L"akinsofthook32.dll",          // Akinsoft Software Engineering.
+  L"assistant_x64.dll",           // Unknown.
   L"avcuf64.dll",                 // Bit Defender Internet Security x64.
   L"avgrsstx.dll",                // AVG 8.
   L"babylonchromepi.dll",         // Babylon translator.
