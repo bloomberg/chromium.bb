bbb2691acf157018973f4dfcb475b5e523394b7a
engedy@chromium.org <engedy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-05 15:10:41 +0000

    Do not try to verify focus placement in ClearBrowserDataBrowserTest.
    The test was flaky because of a more general bug in the WebUI framework (crbug.com/380737). This latter bug exhibits itself when, during opening a WebUI overlay, the BrowserWindow loses focus -- which happens a lot on build bots running multiple browser tests in parallel.
    
    BUG=379574
    
    Review URL: https://codereview.chromium.org/313843002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275128 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 22a408c9137b1983188d5a6ade4702ce7ecac897

--- chrome/browser/ui/webui/options/clear_browser_data_browsertest.cc
+++ chrome/browser/ui/webui/options/clear_browser_data_browsertest.cc
@@ -29,16 +29,6 @@ class ClearBrowserDataBrowserTest : public OptionsUIBrowserTest {
     return element_enabled;
   }
 
-  bool IsElementInFocus(const std::string& selector) {
-    bool element_in_focus = false;
-    EXPECT_TRUE(content::ExecuteScriptAndExtractBool(
-        GetSettingsFrame(),
-        "window.domAutomationController.send(document.querySelector('" +
-            selector + "') == document.activeElement);",
-        &element_in_focus));
-    return element_in_focus;
-  }
-
  private:
   void GetElementEnabledState(
       const std::string& selector,
@@ -51,18 +41,14 @@ class ClearBrowserDataBrowserTest : public OptionsUIBrowserTest {
   }
 };
 
-// Fails flakily. http://crbug.com/379574
 IN_PROC_BROWSER_TEST_F(ClearBrowserDataBrowserTest,
-                       DISABLED_CommitButtonDisabledWhileDeletionInProgress) {
-  const char kTimePeriodSelectorId[] = "#clear-browser-data-time-period";
+                       CommitButtonDisabledWhileDeletionInProgress) {
   const char kCommitButtonId[] = "#clear-browser-data-commit";
   BrowsingDataRemoverCompletionInhibitor completion_inhibitor;
 
   // Navigate to the Clear Browsing Data dialog to ensure that the commit button
   // is initially enabled, usable, and gets disabled after having been pressed.
-  // Furthermore, verify that the time period combo-box gets the initial focus.
   NavigateToSettingsSubpage(chrome::kClearBrowserDataSubPage);
-  EXPECT_TRUE(IsElementInFocus(kTimePeriodSelectorId));
   ASSERT_NO_FATAL_FAILURE(ClickElement(kCommitButtonId));
   EXPECT_FALSE(IsElementEnabled(kCommitButtonId));
 
