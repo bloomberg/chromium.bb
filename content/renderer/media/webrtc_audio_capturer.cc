4f7fb36451fbaee57d5981ef108af2d3990fc588
posciak@chromium.org <posciak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-06 10:57:21 +0000

    Remove volume level DCHECK in WebRtcAudioCapturer on CrOS
    As per discussion in the associated bug, this DCHECK may not make sense
    on CrOS.
    
    BUG=chrome-os-partner:29388
    TEST=Run Debug Chrome, DCHECK not hit
    
    Review URL: https://codereview.chromium.org/315073004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275392 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 21f6da375c0c4b3407f6741149bcd9d45fa26203

--- content/renderer/media/webrtc_audio_capturer.cc
+++ content/renderer/media/webrtc_audio_capturer.cc
@@ -457,7 +457,7 @@ void WebRtcAudioCapturer::Capture(media::AudioBus* audio_source,
 // CaptureCallback.
 #if defined(OS_WIN) || defined(OS_MACOSX)
   DCHECK_LE(volume, 1.0);
-#elif defined(OS_LINUX) || defined(OS_OPENBSD)
+#elif (defined(OS_LINUX) && !defined(OS_CHROMEOS)) || defined(OS_OPENBSD)
   // We have a special situation on Linux where the microphone volume can be
   // "higher than maximum". The input volume slider in the sound preference
   // allows the user to set a scaling that is higher than 100%. It means that
