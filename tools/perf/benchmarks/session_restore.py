f4c5e51e21e1bc0a9cb96e7860a7c14be2fe36f0
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-06 16:46:00 +0000

    [Telemetry] Make session restore benchmark not require arguments.
    Previously, this benchmark required a separate profile generation step as wel
    as arguments. This isn't a Telemetric way to write a benchmark. Now, if a
    profile isn't given, it generates the correct profile for the benchmark.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/317733002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@275458 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e273fc52988c340ba035c4f477fd8b1247e7a735

--- tools/perf/benchmarks/session_restore.py
+++ tools/perf/benchmarks/session_restore.py
@@ -2,12 +2,35 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+import tempfile
+
 from measurements import session_restore
+from measurements import session_restore_with_url
+from profile_creators import small_profile_creator
 from telemetry import test
+from telemetry.page import profile_generator
+
+
+class _SessionRestoreTest(test.Test):
+
+  @classmethod
+  def ProcessCommandLineArgs(cls, parser, args):
+    super(_SessionRestoreTest, cls).ProcessCommandLineArgs(parser, args)
+    profile_type = 'small_profile'
+    if not args.browser_options.profile_dir:
+      profile_dir = os.path.join(tempfile.gettempdir(), profile_type)
+      if not os.path.exists(profile_dir):
+        new_args = args.Copy()
+        new_args.pageset_repeat = 1
+        new_args.output_dir = profile_dir
+        profile_generator.GenerateProfiles(
+            small_profile_creator.SmallProfileCreator, profile_type, new_args)
+      args.browser_options.profile_dir = os.path.join(profile_dir, profile_type)
 
 
 @test.Disabled('android')  # crbug.com/325479
-class SessionRestoreColdTypical25(test.Test):
+class SessionRestoreColdTypical25(_SessionRestoreTest):
   tag = 'cold'
   test = session_restore.SessionRestore
   page_set = 'page_sets/typical_25.py'
@@ -15,9 +38,27 @@ class SessionRestoreColdTypical25(test.Test):
              'pageset_repeat': 5}
 
 
-class SessionRestoreWarmTypical25(test.Test):
+class SessionRestoreWarmTypical25(_SessionRestoreTest):
   tag = 'warm'
   test = session_restore.SessionRestore
   page_set = 'page_sets/typical_25.py'
   options = {'warm': True,
              'pageset_repeat': 20}
+
+
+class SessionRestoreWithUrlCold(_SessionRestoreTest):
+  """Measure Chrome cold session restore with startup URLs."""
+  tag = 'cold'
+  test = session_restore_with_url.SessionRestoreWithUrl
+  page_set = 'page_sets/startup_pages.py'
+  options = {'cold': True,
+             'pageset_repeat': 5}
+
+
+class SessionRestoreWithUrlWarm(_SessionRestoreTest):
+  """Measure Chrome warm session restore with startup URLs."""
+  tag = 'warm'
+  test = session_restore_with_url.SessionRestoreWithUrl
+  page_set = 'page_sets/startup_pages.py'
+  options = {'warm': True,
+             'pageset_repeat': 10}
