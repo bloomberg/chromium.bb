08c6bc8af585cd92d6098b0e29168ba253061851
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-06 21:51:01 +0000

    Restore sanity to RenderLayerStackingNode::shouldBeNormalFlowOnly
    This code evolved and grew over the years, but it seems to be trying to compute
    something very simple.
    
    Review URL: https://codereview.chromium.org/307933010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175704 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0a2c81614017c9d19273d5457e9a1cbf4916f4b0

--- third_party/WebKit/Source/core/rendering/RenderLayerStackingNode.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerStackingNode.cpp
@@ -183,7 +183,6 @@ void RenderLayerStackingNode::updateNormalFlowList()
     ASSERT(m_layerListMutationAllowed);
 
     for (RenderLayer* child = layer()->firstChild(); child; child = child->nextSibling()) {
-        // Ignore non-overflow layers and reflections.
         if (child->stackingNode()->isNormalFlowOnly() && (!layer()->reflectionInfo() || layer()->reflectionInfo()->reflectionLayer() != child)) {
             if (!m_normalFlowList)
                 m_normalFlowList = adoptPtr(new Vector<RenderLayerStackingNode*>);
@@ -205,23 +204,15 @@ void RenderLayerStackingNode::collectLayers(OwnPtr<Vector<RenderLayerStackingNod
 
     layer()->updateDescendantDependentFlags();
 
-    // Overflow layers are just painted by their enclosing layers, so they don't get put in zorder lists.
     if (!isNormalFlowOnly()) {
-        // Determine which buffer the child should be in.
         OwnPtr<Vector<RenderLayerStackingNode*> >& buffer = (zIndex() >= 0) ? posBuffer : negBuffer;
-
-        // Create the buffer if it doesn't exist yet.
         if (!buffer)
             buffer = adoptPtr(new Vector<RenderLayerStackingNode*>);
-
-        // Append ourselves at the end of the appropriate buffer.
         buffer->append(this);
     }
 
-    // Recur into our children to collect more layers, but only if we don't establish a stacking context.
     if (!isStackingContext()) {
         for (RenderLayer* child = layer()->firstChild(); child; child = child->nextSibling()) {
-            // Ignore reflections.
             if (!layer()->reflectionInfo() || layer()->reflectionInfo()->reflectionLayer() != child)
                 child->stackingNode()->collectLayers(posBuffer, negBuffer);
         }
@@ -303,27 +294,11 @@ void RenderLayerStackingNode::updateStackingNodesAfterStyleChange(const RenderSt
         clearZOrderLists();
 }
 
+// FIXME: Rename shouldBeNormalFlowOnly to something more accurate now that CSS
+// 2.1 defines the term "normal flow".
 bool RenderLayerStackingNode::shouldBeNormalFlowOnly() const
 {
-    RenderLayerModelObject* renderer = this->renderer();
-
-    const bool couldBeNormalFlow = renderer->hasOverflowClip()
-        || renderer->hasReflection()
-        || renderer->hasMask()
-        || renderer->isCanvas()
-        || renderer->isVideo()
-        || renderer->isEmbeddedObject()
-        || renderer->isRenderIFrame()
-        || (renderer->style()->specifiesColumns() && !layer()->isRootLayer());
-
-    const bool preventsElementFromBeingNormalFlow = renderer->isPositioned()
-        || renderer->hasTransform()
-        || renderer->hasClipPath()
-        || renderer->hasFilter()
-        || renderer->hasBlendMode()
-        || layer()->isTransparent();
-
-    return couldBeNormalFlow && !preventsElementFromBeingNormalFlow;
+    return !isStackingContext() && !renderer()->isPositioned();
 }
 
 void RenderLayerStackingNode::updateIsNormalFlowOnly()
