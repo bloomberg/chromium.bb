f32abab3df0355211cc92567bcdfd4fa3a304b18
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-06 02:01:58 +0000

    Psuedo revert "s/scheduleLayerUpdate/setNeedsCompositingUpdate in setWebLayer."
    This method has been renamed already, so it's not a strict revert, but
    it restores the original behavior.
    
    BUG=none
    TEST=browser_tests --gtest_filter=WebViewTest*Auto*
    TBR=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/316373005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175620 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 96a3d1767c43c788d31d99d5c14262214f57dddc

--- third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
+++ third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
@@ -291,7 +291,7 @@ void WebPluginContainerImpl::setWebLayer(WebLayer* layer)
     // If anyone of the layers is null we need to switch between hardware
     // and software compositing.
     if (!m_webLayer || !layer)
-        m_element->setNeedsCompositingUpdate();
+        m_element->scheduleSVGFilterLayerUpdateHack();
     if (m_webLayer)
         GraphicsLayer::unregisterContentsLayer(m_webLayer);
     if (layer)
