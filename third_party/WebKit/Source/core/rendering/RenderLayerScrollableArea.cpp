918ee547026edb8670d65d44cbab85b859fb9651
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-07 03:47:32 +0000

    Remove incremental compositing update in RenderLayerScrollableArea::updateAfterStyleChange
    We don't appear to need this incremental update anymore.
    
    BUG=343756
    
    Review URL: https://codereview.chromium.org/317373004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175730 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 5170a420da9156ad97ef802855156366d3c9bd27

--- third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.cpp
@@ -778,13 +778,6 @@ void RenderLayerScrollableArea::updateAfterStyleChange(const RenderStyle* oldSty
     updateScrollCornerStyle();
     updateResizerAreaSet();
     updateResizerStyle();
-
-    // FIXME: Remove incremental compositing updates after fixing the chicken/egg issues
-    // https://code.google.com/p/chromium/issues/detail?id=343756
-    DisableCompositingQueryAsserts disabler;
-    RenderLayer* layer = this->layer();
-    if (layer->hasCompositedLayerMapping() && layer->compositedLayerMapping()->updateGraphicsLayerConfiguration(GraphicsLayerUpdater::ForceUpdate))
-        layer->compositor()->setCompositingLayersNeedRebuild();
 }
 
 void RenderLayerScrollableArea::updateAfterCompositingChange()
