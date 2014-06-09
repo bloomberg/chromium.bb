aa520d1b325ab9943975cbc978cad38f4babcf72
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-09 12:43:50 +0000

    Move trace event based implementation parts of TimelineModel into TracingTimelineModel
    BUG=361045
    
    Review URL: https://codereview.chromium.org/318343003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175790 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 80bd45164035387d0cc247598a962225d144922a

--- third_party/WebKit/LayoutTests/inspector/tracing-test.js
+++ third_party/WebKit/LayoutTests/inspector/tracing-test.js
@@ -4,19 +4,29 @@ function initialize_TracingTest()
 // FIXME: remove when tracing is out of experimental
 WebInspector.inspectorView.showPanel("timeline");
 InspectorTest.tracingModel = new WebInspector.TracingModel();
+InspectorTest.tracingTimelineModel = new WebInspector.TracingTimelineModel(InspectorTest.tracingModel);
 
 InspectorTest.invokeWithTracing = function(categoryFilter, functionName, callback)
 {
-    InspectorTest.tracingModel.start(categoryFilter, "", onTracingStarted);
+    InspectorTest.tracingTimelineModel.addEventListener(WebInspector.TimelineModel.Events.RecordingStarted, onTracingStarted, this);
+    InspectorTest.tracingTimelineModel._startRecordingWithCategories(categoryFilter);
 
-    function onTracingStarted(error)
+    function onTracingStarted(event)
     {
+        InspectorTest.tracingTimelineModel.removeEventListener(WebInspector.TimelineModel.Events.RecordingStarted, onTracingStarted, this);
         InspectorTest.invokePageFunctionAsync(functionName, onPageActionsDone);
     }
 
     function onPageActionsDone()
     {
-        InspectorTest.tracingModel.stop(InspectorTest.safeWrap(callback));
+        InspectorTest.tracingTimelineModel.addEventListener(WebInspector.TimelineModel.Events.RecordingStopped, onTracingComplete, this);
+        InspectorTest.tracingTimelineModel.stopRecording();
+    }
+
+    function onTracingComplete(event)
+    {
+        InspectorTest.tracingTimelineModel.removeEventListener(WebInspector.TimelineModel.Events.RecordingStopped, onTracingComplete, this);
+        callback();
     }
 }
 
