b314c12c085a747ebd34d59d6fb303331c1b8e44
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-10 23:30:16 +0000

    Make domdistiller.js not modify `window`
    Currently when we inject javascript into the page, we clobber a whole
    bunch of names on the page's window object.
    
    This moves the included javascript into the anonymous function so that
    defining a function/var doesn't add that name to the window object and
    also passes a new object with the window as prototype as the 'window'
    variable to that function so that if it explicitly modifies window it
    will modify the new object instead.
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/320113002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276203 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1cdeaddc33f8053eb8f194c5d757ac76fa270078

--- components/dom_distiller/core/javascript/domdistiller.js
+++ components/dom_distiller/core/javascript/domdistiller.js
@@ -2,16 +2,24 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// These includes will be processed at build time by grit.
-<include src="../../../../third_party/dom_distiller_js/package/js/domdistiller.js"/>
-
 // Applies DomDistillerJs to the content of the page and returns a
 // DomDistillerResults (as a javascript object/dict).
 (function() {
   try {
+    // The generated domdistiller.js accesses the window object only explicitly
+    // via the window name. So, we create a new object with the normal window
+    // object as its prototype and initialize the domdistiller.js with that new
+    // context so that it doesn't change the real window object.
+    function initialize(window) {
+      // This include will be processed at build time by grit.
+      <include src="../../../../third_party/dom_distiller_js/package/js/domdistiller.js"/>
+    }
+    context = Object.create(window);
+    initialize(context);
+
     // The OPTIONS placeholder will be replaced with the DomDistillerOptions at
     // runtime.
-    res = com.dom_distiller.DomDistiller.applyWithOptions($$OPTIONS);
+    res = context.com.dom_distiller.DomDistiller.applyWithOptions($$OPTIONS);
     return res;
   } catch (e) {
     window.console.error("Error during distillation: " + e);
