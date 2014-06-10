3f91491ee7c3aebeb3c663cf691378cc331605a8
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-10 00:47:25 +0000

    Set process name and pid when faking out a crash for asan.
    This is a follow on patch / fix after r175488, where I started
    forcing a "crash" if we detected output from the sanitizer after a
    test. We were not setting the process name or pid, which caused
    problems later when writing out the crash logs.
    
    TBR=ojan@chromium.org
    BUG=336971
    
    Review URL: https://codereview.chromium.org/325993002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@175838 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: f8e428d451832ef2a8767fb40d538c5471d068aa

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/driver.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/driver.py
@@ -170,6 +170,8 @@ class Driver(object):
         if not crashed and 'AddressSanitizer' in self.error_from_test:
             self.error_from_test = 'OUTPUT CONTAINS "AddressSanitizer", so we are treating this test as if it crashed, even though it did not.\n\n' + self.error_from_test
             crashed = True
+            self._crashed_process_name = "unknown process name"
+            self._crashed_pid = 0
 
         if stop_when_done or crashed or timed_out or leaked:
             # We call stop() even if we crashed or timed out in order to get any remaining stdout/stderr output.
