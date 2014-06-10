4c253c4d02eb6ec9fb57db18023d30bb7678bf29
jfb@chromium.org <jfb@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2014-06-10 03:22:04 +0000

    llvm_change_try_helper: update to toolchain_build
    TBR= mseaborn@chromium.org
    TEST= none
    BUG= none
    
    Review URL: https://codereview.chromium.org/320393004
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@13308 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: f1b987b80e0a92f9d41be039c940b12de114443e

--- pnacl/llvm_change_try_helper.sh
+++ pnacl/llvm_change_try_helper.sh
@@ -12,7 +12,7 @@
 # This script assumes you are using a Git checkout of the
 # native_client source tree.
 #
-# Example usage: After making changes to pnacl/git/llvm and committing
+# Example usage: After making changes to toolchain_build/src/llvm and committing
 # them, you can test the changes with the following:
 #
 #   ./pnacl/llvm_change_try_helper.sh  # Commits a trybot-only change
@@ -29,7 +29,7 @@ elif [ "$#" = 0 ]; then
   component=llvm
 else
   echo "Usage: $0 [<component-name>]"
-  echo "where <component-name> is the name of a subdirectory of pnacl/git/"
+  echo "where <component-name> is the name of a subdirectory of toolchain_build/src/"
   exit 1
 fi
 
@@ -37,7 +37,7 @@ top_dir="$(pwd)"
 
 mkdir -p pnacl/not_for_commit
 
-pushd pnacl/git/${component}
+pushd toolchain_build/src/${component}
 # Save the commit ID plus its subject line to give readable context.
 git log --no-walk --pretty="format:%H%n%ad%n%s" HEAD \
     > $top_dir/pnacl/not_for_commit/${component}_commit_id
