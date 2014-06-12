5e4d5e5b024b870c0ada91c17a48c2bcc82ed163
ernstm@chromium.org <ernstm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-12 11:04:58 +0000

    telemetry: add screenshot measurement
    R=tonyg@chromium.org,nduca@chromium.org
    BUG=373045
    
    Review URL: https://codereview.chromium.org/271733008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276623 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 132fb473538d8391a7585bdb481da400550372e1

---
+++ tools/perf/measurements/screenshot.py
@@ -0,0 +1,48 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import os
+
+from telemetry.page import page_measurement
+from telemetry.page import page_test
+
+
+class Screenshot(page_measurement.PageMeasurement):
+  def __init__(self):
+    super(Screenshot, self).__init__(
+        action_name_to_run = 'RunPrepareForScreenshot',
+        is_action_name_to_run_optional=True)
+
+  @classmethod
+  def AddCommandLineArgs(cls, parser):
+    parser.add_option('--png-outdir',
+                      help='Output directory for the PNG files')
+
+  @classmethod
+  def ProcessCommandLineArgs(cls, parser, args):
+    if not args.png_outdir:
+      parser.error('Please specify --png-outdir')
+    cls._png_outdir = args.png_outdir
+
+  def MeasurePage(self, page, tab, results):
+    if not tab.screenshot_supported:
+      raise page_test.TestNotSupportedOnPlatformFailure(
+          'Browser does not support screenshotting')
+
+    tab.WaitForDocumentReadyStateToBeComplete()
+    screenshot = tab.Screenshot(60)
+
+    outpath = os.path.abspath(
+        os.path.join(self._png_outdir, page.file_safe_name)) + '.png'
+
+    if os.path.exists(outpath):
+      previous_mtime = os.path.getmtime(outpath)
+    else:
+      previous_mtime = -1
+
+    screenshot.WritePngFile(outpath)
+
+    saved_picture_count = 0
+    if os.path.exists(outpath) and os.path.getmtime(outpath) > previous_mtime:
+      saved_picture_count = 1
+    results.Add('saved_picture_count', 'count', saved_picture_count)
---
+++ tools/perf/measurements/screenshot_unittest.py
@@ -0,0 +1,36 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import logging
+import shutil
+import tempfile
+
+from measurements import screenshot
+from telemetry.page import page_measurement_unittest_base
+from telemetry.page import page_test
+from telemetry.unittest import options_for_unittests
+
+
+class ScreenshotUnitTest(
+      page_measurement_unittest_base.PageMeasurementUnitTestBase):
+  def setUp(self):
+    self._options = options_for_unittests.GetCopy()
+    self._options.png_outdir = tempfile.mkdtemp('_png_test')
+
+  def tearDown(self):
+    shutil.rmtree(self._options.png_outdir)
+
+  def testScreenshot(self):
+    ps = self.CreatePageSetFromFileInUnittestDataDir('blank.html')
+    measurement = screenshot.Screenshot()
+    try:
+      results = self.RunMeasurement(measurement, ps, options=self._options)
+    except page_test.TestNotSupportedOnPlatformFailure as failure:
+      logging.warning(str(failure))
+      return
+
+    saved_picture_count = results.FindAllPageSpecificValuesNamed(
+        'saved_picture_count')
+    self.assertEquals(len(saved_picture_count), 1)
+    self.assertGreater(saved_picture_count[0].GetRepresentativeNumber(), 0)
