d453e1a6a92246b135d577d3d7e5ed3bb41380a7
ycheo@chromium.org <ycheo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 05:20:17 +0000

    aw: Add EME key system tests in android_webview_instrumentation_tests.
    Review URL: https://codereview.chromium.org/328713002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276932 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 5ba417126af4952d1c6f79a332354b422d9fc6ea

---
+++ android_webview/javatests/src/org/chromium/android_webview/test/KeySystemTest.java
@@ -0,0 +1,64 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.test;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.chromium.android_webview.AwContents;
+import org.chromium.base.test.util.Feature;
+
+/**
+ * TestSuite for EME key systems.
+ */
+public class KeySystemTest extends AwTestBase {
+
+    private TestAwContentsClient mContentsClient = new TestAwContentsClient();
+    private AwContents mAwContents;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        final AwTestContainerView testContainerView =
+                createAwTestContainerViewOnMainSync(mContentsClient);
+        mAwContents = testContainerView.getAwContents();
+        enableJavaScriptOnUiThread(mAwContents);
+
+        loadDataSync(mAwContents, mContentsClient.getOnPageFinishedHelper(),
+            getKeySystemTestPage(), "text/html", false);
+    }
+
+    private String getKeySystemTestPage() {
+        return "<html> <script>" +
+                "function isKeySystemSupported(keySystem) {" +
+                    "var video = document.createElement('video');" +
+                    "return video.canPlayType('video/mp4', keySystem);" +
+                "}" +
+                "</script> </html>";
+    }
+
+    private String IsKeySystemSupported(String keySystem) throws Exception {
+        return executeJavaScriptAndWaitForResult(mAwContents, mContentsClient,
+                  "isKeySystemSupported('" + keySystem + "')");
+    }
+
+    @Feature({"AndroidWebView"})
+    @SmallTest
+    public void testSupportClearKeySystem() throws Throwable {
+        assertEquals("\"maybe\"", IsKeySystemSupported("webkit-org.w3.clearkey"));
+    }
+
+    @Feature({"AndroidWebView"})
+    @SmallTest
+    public void testSupportWidevineKeySystem() throws Throwable {
+        assertEquals("\"maybe\"", IsKeySystemSupported("com.widevine.alpha"));
+    }
+
+    @Feature({"AndroidWebView"})
+    @SmallTest
+    public void testNotSupportFooKeySystem() throws Throwable {
+        assertEquals("\"\"", IsKeySystemSupported("com.foo.keysystem"));
+    }
+}
