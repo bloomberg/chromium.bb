74dfef567836fbde935220bb54af6ae5ee46d1e5
varkha@chromium.org <varkha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 04:17:43 +0000

    Prevents error when using build/install-build-deps.sh --quick-check
    BUG=380556
    TEST=run "build/install-build-deps.sh --quick-check". Should not produce error described in the bug.
    
    Review URL: https://codereview.chromium.org/330153002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276922 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 9289b4e99a23444328d95a736b54a42aab21bb27

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -148,7 +148,7 @@ nacl_list="bison cmake xvfb gawk texinfo autoconf libtool
            libgpm2:i386 libncurses5:i386
            g++-mingw-w64-i686 libtinfo-dev libtinfo-dev:i386
            libglib2.0-0:i386 libnss3:i386
-           libgconf-2-4:i386 libfontconfig:i386
+           libgconf-2-4:i386 libfontconfig1:i386
            libpango1.0-0:i386 libxi6:i386 libxcursor1:i386 libxcomposite1:i386
            libasound2:i386 libxdamage1:i386 libxtst6:i386 libxrandr2:i386
            libcap2:i386 libgtk2.0-0:i386 libxss1:i386
