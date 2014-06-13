3eb5391f4f6a7b494bd2fc5afb30b58824d7761b
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-13 08:27:01 +0000

    Add UseCounter for Element.createShadowRoot()
    BUG=383749
    TEST=none
    R=yutak@chromium.org
    
    Review URL: https://codereview.chromium.org/334833003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176093 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d42c98a207ff29efad3e82ceb7141e17a712f407

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -453,6 +453,7 @@ public:
         WebkitEditableContentChangedOnContentEditable = 453,
         WebkitEditableContentChangedOnNotNode = 454,
         HTMLImports = 455,
+        ElementCreateShadowRoot = 456,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
