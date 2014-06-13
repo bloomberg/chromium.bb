e37908700597927329b91efb2349a3e66803d1ca
nednguyen@google.com <nednguyen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 22:16:21 +0000

    Convert results.Add to results.AddValue for cpu metrics.
    BUG=383100
    
    Review URL: https://codereview.chromium.org/330513006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277107 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 93bc9c08125bd74514d33f3cb326bafe5297a709

--- tools/perf/metrics/cpu.py
+++ tools/perf/metrics/cpu.py
@@ -3,6 +3,8 @@
 # found in the LICENSE file.
 
 from metrics import Metric
+from telemetry.value import scalar
+
 
 class CpuMetric(Metric):
   """Calulates CPU load over a span of time."""
@@ -32,8 +34,9 @@ class CpuMetric(Metric):
     for process_type in self._results:
       trace_name_for_process = '%s_%s' % (trace_name, process_type.lower())
       cpu_percent = 100 * self._results[process_type]
-      results.Add(trace_name_for_process, '%', cpu_percent,
-                  chart_name='cpu_utilization', data_type='unimportant')
+      results.AddValue(scalar.ScalarValue(
+          results.current_page, 'cpu_utilization.%s' % trace_name_for_process,
+          '%', cpu_percent, important=False))
 
 
 def _SubtractCpuStats(cpu_stats, start_cpu_stats):
