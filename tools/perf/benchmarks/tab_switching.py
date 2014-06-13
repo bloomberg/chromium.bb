addcbf5bdfebec1585a662f20949d35da172dbd4
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-13 16:33:51 +0000

    [Telemetry] Disable new tab_switching tests on Android to fix bots
    BUG=379561
    TBR=tonyg@chromium.org
    NOTRY=True
    
    Review URL: https://codereview.chromium.org/330283003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277036 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c690e56a50db1df2659090ab1e962d945e38f8b4

--- tools/perf/benchmarks/tab_switching.py
+++ tools/perf/benchmarks/tab_switching.py
@@ -7,16 +7,19 @@ from telemetry import test
 from measurements import tab_switching
 
 
+@test.Disabled('android')  # crbug.com/379561
 class TabSwitchingTop10(test.Test):
   test = tab_switching.TabSwitching
   page_set = 'page_sets/top_10.py'
 
 
+@test.Disabled('android')  # crbug.com/379561
 class TabSwitchingTypical25(test.Test):
   test = tab_switching.TabSwitching
   page_set = 'page_sets/typical_25.py'
 
 
+@test.Disabled('android')  # crbug.com/379561
 class TabSwitchingFiveBlankTabs(test.Test):
   test = tab_switching.TabSwitching
   page_set = 'page_sets/five_blank_pages.py'
