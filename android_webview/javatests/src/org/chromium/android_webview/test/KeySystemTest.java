f3f296ec0a4c2b8dc8b77d4fcabafef45db98003
dpranke@chromium.org <dpranke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-14 23:31:47 +0000

    Disable KeySystemTest.testSupportWidevineKeySystem
    TBR=ycheo@chromium.org
    BUG=384753
    
    Review URL: https://codereview.chromium.org/332063002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277280 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 36db0c2847193edc90ffde211d86bcb79e05c1a1

--- android_webview/javatests/src/org/chromium/android_webview/test/KeySystemTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/KeySystemTest.java
@@ -5,8 +5,8 @@
 package org.chromium.android_webview.test;
 
 import android.test.suitebuilder.annotation.SmallTest;
-
 import org.chromium.android_webview.AwContents;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 
 /**
@@ -50,8 +50,12 @@ public class KeySystemTest extends AwTestBase {
         assertEquals("\"maybe\"", IsKeySystemSupported("webkit-org.w3.clearkey"));
     }
 
+    /*
     @Feature({"AndroidWebView"})
     @SmallTest
+    crbug.com/384753.
+    */
+    @DisabledTest
     public void testSupportWidevineKeySystem() throws Throwable {
         assertEquals("\"maybe\"", IsKeySystemSupported("com.widevine.alpha"));
     }
