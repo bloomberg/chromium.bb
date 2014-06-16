8e21e0dee470987e3003f77b8eabcc1cb585deda
kelvinp@chromium.org <kelvinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-16 22:49:33 +0000

    SinonJS is a standalone and test framework agnostic JavaScript test spies, stubs and mocks.
    It is similar to GMocks and it makes unit testing in JavaScript easier.
    
    Security 
    These libraries will be used for internal testing only and will not be shipped. It will not have any security impact on chrome. 
    
    BUG=378537
    
    Review URL: https://codereview.chromium.org/309703002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277555 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: f2694b16075ed297cf2129b850f2be7ba3cb8aae

--- tools/licenses.py
+++ tools/licenses.py
@@ -52,6 +52,7 @@ PRUNE_PATHS = set([
     os.path.join('third_party','pylib'),
     os.path.join('third_party','pywebsocket'),
     os.path.join('third_party','qunit'),
+    os.path.join('third_party','sinonjs'),
     os.path.join('third_party','syzygy'),
     os.path.join('tools','gn'),
 
