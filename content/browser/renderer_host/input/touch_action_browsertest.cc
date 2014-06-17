ccdbcd640b7f33939c5e0fa100f77a0802e07a25
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-17 03:14:18 +0000

    TSANv2: Disable flaky TouchActionBrowserTest.TouchActionNone.
    BUG=376668
    TBR=rbyers@chromium.org
    
    Review URL: https://codereview.chromium.org/338473004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@277643 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: fa3379c15acf2d2cbdb397a6a70c627f66c6a207

--- content/browser/renderer_host/input/touch_action_browsertest.cc
+++ content/browser/renderer_host/input/touch_action_browsertest.cc
@@ -192,8 +192,11 @@ IN_PROC_BROWSER_TEST_F(TouchActionBrowserTest, DISABLED_DefaultAuto) {
 // Verify that touching a touch-action: none region disables scrolling and
 // enables all touch events to be sent.
 // Disabled on MacOS because it doesn't support touch input.
-// Flaky on OS_CHROMEOS crbug/376695.
+// Flaky on OS_CHROMEOS http://crbug.com/376695.
+// Also flaky on Linux Tests (TSan v2) http://crbug.com/376668.
 #if defined(OS_MACOSX) || defined(OS_CHROMEOS)
+#define MAYBE_TouchActionNone DISABLED_TouchActionNone
+#elif defined(THREAD_SANITIZER) && defined(OS_LINUX)
 #define MAYBE_TouchActionNone DISABLED_TouchActionNone
 #else
 #define MAYBE_TouchActionNone TouchActionNone
