3ca99e0633aaf3129b0b7d87f9f786cfcc0bb387
ksakamoto@chromium.org <ksakamoto@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-17 06:43:02 +0000

    Add UseCounter for FontFaceSet#ready()
    In the latest editor's draft spec [1], FontFaceSet#ready has been
    changed from a method to an attribute. We are likely to deprecate
    ready() in favor of ready attribute. To prepare for a deprecation
    process, this patch adds a UseCounter for FontFaceSet#ready().
    
    [1] http://dev.w3.org/csswg/css-font-loading/#dom-fontfaceset-ready
    
    BUG=380990
    
    Review URL: https://codereview.chromium.org/339563006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176288 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ac927c90566c046f2b6da55b1200ddd429772f6d

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -461,6 +461,7 @@ public:
         EditingAppleStyleSpanClass = 461,
         EditingAppleTabSpanClass = 462,
         HTMLImportsAsyncAttribute = 463,
+        FontFaceSetReady = 464,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
