42a34af971223df851167c2365d4c02f79400f36
robhogan@gmail.com <robhogan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-19 22:29:09 +0000

    Refactor avoidsFloats() to reflect CSS2.1 spec more clearly
    Introduce the 'createsBlockFormatting' concept to RenderBlock::avoidsFloats()
    and remove a couple of now-redundant virtualizations of the function.
    
    BUG=
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=173549
    
    Review URL: https://codereview.chromium.org/268833003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176557 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: a3111c95ae3ff877e6df7d0b7a60fa820e1f2331

--- third_party/WebKit/Source/core/rendering/RenderBlock.h
+++ third_party/WebKit/Source/core/rendering/RenderBlock.h
@@ -326,6 +326,7 @@ protected:
 
     void setDesiredColumnCountAndWidth(int, LayoutUnit);
 
+    bool avoidsOrIgnoresFloats() { return isFloatingOrOutOfFlowPositioned() || avoidsFloats(); }
 public:
     virtual void computeOverflow(LayoutUnit oldClientAfterEdge, bool = false);
 protected:
@@ -388,8 +389,6 @@ private:
     void paintCarets(PaintInfo&, const LayoutPoint&);
 
     bool hasCaret() const;
-
-    virtual bool avoidsFloats() const OVERRIDE;
 
     bool hitTestColumns(const HitTestRequest&, HitTestResult&, const HitTestLocation& locationInContainer, const LayoutPoint& accumulatedOffset, HitTestAction);
     bool hitTestContents(const HitTestRequest&, HitTestResult&, const HitTestLocation& locationInContainer, const LayoutPoint& accumulatedOffset, HitTestAction);
@@ -469,8 +468,6 @@ protected:
     // For a page height of 800px, the first rule will return 800 if the value passed in is 0. The second rule will simply return 0.
     enum PageBoundaryRule { ExcludePageBoundary, IncludePageBoundary };
     LayoutUnit nextPageLogicalTop(LayoutUnit logicalOffset, PageBoundaryRule = ExcludePageBoundary) const;
-
-    bool createsBlockFormattingContext() const;
 
 public:
     LayoutUnit pageLogicalTopForOffset(LayoutUnit offset) const;
