071dc8f550f6a2513e94c97bb0dab30e56e0f734
eroman@chromium.org <eroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-19 22:31:24 +0000

    [WebCrypto] Replace KeyPair with an Object.
    This corresponds with the spec update:
    https://dvcs.w3.org/hg/webcrypto-api/rev/309495c2794e
    
    Which changes KeyPair --> CryptoKeyPair (of type Dictionary).
    
    BUG=386757
    
    Review URL: https://codereview.chromium.org/333293004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@176558 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 19edf020c3760df2512303c3095c7f562578f80d

--- third_party/WebKit/Source/modules/crypto/CryptoResultImpl.cpp
+++ third_party/WebKit/Source/modules/crypto/CryptoResultImpl.cpp
@@ -38,7 +38,6 @@
 #include "core/dom/DOMException.h"
 #include "core/dom/ExecutionContext.h"
 #include "modules/crypto/Key.h"
-#include "modules/crypto/KeyPair.h"
 #include "modules/crypto/NormalizeAlgorithm.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebArrayBuffer.h"
@@ -154,8 +153,21 @@ void CryptoResultImpl::completeWithKey(const blink::WebCryptoKey& key)
 
 void CryptoResultImpl::completeWithKeyPair(const blink::WebCryptoKey& publicKey, const blink::WebCryptoKey& privateKey)
 {
-    if (m_resolver)
-        m_resolver->resolve(KeyPair::create(publicKey, privateKey));
+    if (m_resolver) {
+        ScriptState* scriptState = m_resolver->scriptState();
+        ScriptState::Scope scope(scriptState);
+
+        // FIXME: Use Dictionary instead, to limit amount of direct v8 access used from WebCore.
+        v8::Handle<v8::Object> keyPair = v8::Object::New(scriptState->isolate());
+
+        v8::Handle<v8::Value> publicKeyValue = toV8NoInline(Key::create(publicKey), scriptState->context()->Global(), scriptState->isolate());
+        v8::Handle<v8::Value> privateKeyValue = toV8NoInline(Key::create(privateKey), scriptState->context()->Global(), scriptState->isolate());
+
+        keyPair->Set(v8::String::NewFromUtf8(scriptState->isolate(), "publicKey"), publicKeyValue);
+        keyPair->Set(v8::String::NewFromUtf8(scriptState->isolate(), "privateKey"), privateKeyValue);
+
+        m_resolver->resolve(v8::Handle<v8::Value>(keyPair));
+    }
 }
 
 bool CryptoResultImpl::cancelled() const
