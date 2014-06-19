4c5b85cfd17408d1e63f58806f2545e710679dac
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-19 21:53:39 +0000

    PPAPI: Move PPB_TraceEvent to have dev-channel permissions
    PPB_TraceEvent should be available to developers who want to profile
    their application on Dev channel or Canary.
    
    BUG=386636
    
    Review URL: https://codereview.chromium.org/349443002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278499 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0b4fc6a04202520560b91598f50fbd86469f8a2d

--- ppapi/thunk/interfaces_ppb_public_dev_channel.h
+++ ppapi/thunk/interfaces_ppb_public_dev_channel.h
@@ -14,4 +14,12 @@ PROXIED_IFACE(PPB_FILEMAPPING_INTERFACE_0_1, PPB_FileMapping_0_1)
 PROXIED_IFACE(PPB_MESSAGING_INTERFACE_1_1, PPB_Messaging_1_1)
 PROXIED_IFACE(PPB_VIDEODECODER_INTERFACE_0_1, PPB_VideoDecoder_0_1)
 
+// Note, PPB_TraceEvent is special. We don't want to actually make it stable,
+// but we want developers to be able to leverage it when running Chrome Dev or
+// Chrome Canary.
+PROXIED_IFACE(PPB_TRACE_EVENT_DEV_INTERFACE_0_1,
+              PPB_Trace_Event_Dev_0_1)
+PROXIED_IFACE(PPB_TRACE_EVENT_DEV_INTERFACE_0_2,
+              PPB_Trace_Event_Dev_0_2)
+
 #include "ppapi/thunk/interfaces_postamble.h"
