c9e2be36635dee5d1acb6723a31f81aad30e46b7
eseidel@chromium.org <eseidel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 21:42:58 +0000

    Disable broken Android image load test in preparation for re-landing preloader changes.
    As much as I would like to keep all tests working,
    it seems unreasonable to hold-back performance improvements
    for all of Chromium (particularly android) to keep
    this test "passing".
    
    https://codereview.chromium.org/302063002/
    
    BUG=387101
    TBR=mnaganov@chromium.org
    
    Review URL: https://codereview.chromium.org/343213002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278830 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: da74003903db0763f8df6973453f607153e91b97

--- android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
@@ -1783,8 +1783,10 @@ public class AwSettingsTest extends AwTestBase {
                     views.getContainer1(), views.getClient1(), 1));
     }
 
-    @SmallTest
-    @Feature({"AndroidWebView", "Preferences"})
+    // @SmallTest
+    // @Feature({"AndroidWebView", "Preferences"})
+    // http://crbug.com/387101
+    @DisabledTest
     public void testBlockNetworkImagesDoesNotBlockDataUrlImage() throws Throwable {
         final TestAwContentsClient contentClient = new TestAwContentsClient();
         final AwTestContainerView testContainerView =
