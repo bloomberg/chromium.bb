42be6ecbed313f623c4b25c20dc87a0596643a5f
dnicoara@chromium.org <dnicoara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 17:18:38 +0000

    Add display_unittests to chrome_tests.py
    BUG=381329
    
    Review URL: https://codereview.chromium.org/332103003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278737 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: eedd88315a698b8f9974a04b78dd9140dc91b36d

--- tools/valgrind/chrome_tests.py
+++ tools/valgrind/chrome_tests.py
@@ -418,6 +418,9 @@ class ChromeTests:
     return self.SimpleTest("mojo_view_manager_lib",
                            "mojo_view_manager_lib_unittests")
 
+  def TestDisplay(self):
+    return self.SimpleTest("display", "display_unittests")
+
   # Valgrind timeouts are in seconds.
   UI_VALGRIND_ARGS = ["--timeout=14400", "--trace_children", "--indirect"]
   # UI test timeouts are in milliseconds.
@@ -661,6 +664,7 @@ class ChromeTests:
     "mojo_service_manager": TestMojoServiceManager,
     "mojo_view_manager": TestMojoViewManager,
     "mojo_view_manager_lib": TestMojoViewManagerLib,
+    "display": TestDisplay,
   }
 
 
