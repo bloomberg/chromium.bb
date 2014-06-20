47688f3d670af14c3149537fa0f67169c0201361
bengr@chromium.org <bengr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 00:08:10 +0000

    Changed BrowserKeyedContextService name of DataReductionProxySettingsAndroid
    BUG=386593
    
    Review URL: https://codereview.chromium.org/346833002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278540 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b06772ce62d52ee39ab6ed9cb656378943583ed5

--- chrome/browser/net/spdyproxy/data_reduction_proxy_settings_factory_android.cc
+++ chrome/browser/net/spdyproxy/data_reduction_proxy_settings_factory_android.cc
@@ -35,7 +35,7 @@ DataReductionProxySettingsFactoryAndroid::GetInstance() {
 DataReductionProxySettingsFactoryAndroid::
 DataReductionProxySettingsFactoryAndroid()
     : BrowserContextKeyedServiceFactory(
-        "ProfileSyncService",
+        "DataReductionProxySettingsAndroid",
         BrowserContextDependencyManager::GetInstance()) {
 }
 
