ae2ee37ab1ca1ef34cccfaf31f8de6e12e32a49f
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 05:33:39 +0000

    Re-enable PlatformAppBrowserTest.Messaging
    The source of the flakiness should have been fixed in r277688
    
    BUG=171450
    
    Review URL: https://codereview.chromium.org/337213004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278619 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 7fc5b452cfa82c8481b910b1e6fa078599419826

--- chrome/browser/apps/app_browsertest.cc
+++ chrome/browser/apps/app_browsertest.cc
@@ -1041,13 +1041,7 @@ IN_PROC_BROWSER_TEST_F(PlatformAppBrowserTest, ComponentAppBackgroundPage) {
   ASSERT_TRUE(launched_listener.WaitUntilSatisfied());
 }
 
-// Flakes on Windows: http://crbug.com/171450
-#if defined(OS_WIN)
-#define MAYBE_Messaging DISABLED_Messaging
-#else
-#define MAYBE_Messaging Messaging
-#endif
-IN_PROC_BROWSER_TEST_F(PlatformAppBrowserTest, MAYBE_Messaging) {
+IN_PROC_BROWSER_TEST_F(PlatformAppBrowserTest, Messaging) {
   ExtensionApiTest::ResultCatcher result_catcher;
   LoadAndLaunchPlatformApp("messaging/app2", "Launched");
   LoadAndLaunchPlatformApp("messaging/app1", "Launched");
