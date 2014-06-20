f8a16c6468db01fee630e8951a34dbba6ffda5aa
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-20 16:38:26 +0000

    Revert 278655 "Include loaded modules in safe browsing client in..."
    > Include loaded modules in safe browsing client incident reports.
    > 
    > This includes the sanitized paths of all loaded modules and an
    > indication for those that host active LSPs.
    > 
    > BUG=386156
    > 
    > Review URL: https://codereview.chromium.org/323953002
    
    TBR=pmonette@google.com,grt@chromium.org
    
    Review URL: https://codereview.chromium.org/347123002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@278721 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ac8a16bc1ec072211e6e24a781d0bfd19c6ad9b8

--- chrome/browser/safe_browsing/environment_data_collection_win.cc
+++ chrome/browser/safe_browsing/environment_data_collection_win.cc
@@ -4,94 +4,13 @@
 
 #include "chrome/browser/safe_browsing/environment_data_collection_win.h"
 
-#include <windows.h>
-#include <set>
-
-#include "base/strings/string16.h"
-#include "base/strings/string_util.h"
-#include "base/strings/utf_string_conversions.h"
-#include "chrome/browser/install_verification/win/module_info.h"
-#include "chrome/browser/install_verification/win/module_verification_common.h"
-#include "chrome/browser/net/service_providers_win.h"
-#include "chrome/browser/safe_browsing/path_sanitizer.h"
 #include "chrome/common/safe_browsing/csd.pb.h"
 
 namespace safe_browsing {
 
-namespace {
-
-// Helper function for expanding all environment variables in |path|.
-base::string16 ExpandEnvironmentVariables(const base::string16& path) {
-  static const DWORD kMaxBuffer = 32 * 1024;  // Max according to MSDN.
-  base::string16 path_expanded;
-  DWORD path_len = MAX_PATH;
-  do {
-    DWORD result = ExpandEnvironmentStrings(
-        path.c_str(), WriteInto(&path_expanded, path_len), path_len);
-    if (!result) {
-      // Failed to expand variables. Return the original string.
-      DPLOG(ERROR) << path;
-      break;
-    }
-    if (result <= path_len)
-      return path_expanded.substr(0, result - 1);
-    path_len = result;
-  } while (path_len < kMaxBuffer);
-
-  return path;
-}
-
-}  // namespace
-
-bool CollectDlls(ClientIncidentReport_EnvironmentData_Process* process) {
-  // Retrieve the module list.
-  std::set<ModuleInfo> loaded_modules;
-  if (!GetLoadedModules(&loaded_modules))
-    return false;
-
-  // Sanitize path of each module and add it to the incident report.
-  PathSanitizer path_sanitizer;
-  for (std::set<ModuleInfo>::const_iterator it = loaded_modules.begin();
-       it != loaded_modules.end();
-       ++it) {
-    base::FilePath dll_path(it->name);
-    path_sanitizer.StripHomeDirectory(&dll_path);
-
-    ClientIncidentReport_EnvironmentData_Process_Dll* dll = process->add_dll();
-    dll->set_path(base::WideToUTF8(dll_path.value()));
-    dll->set_base_address(it->base_address);
-    dll->set_length(it->size);
-  }
-
-  return true;
-}
-
-void RecordLspFeature(ClientIncidentReport_EnvironmentData_Process* process) {
-  WinsockLayeredServiceProviderList lsp_list;
-  GetWinsockLayeredServiceProviders(&lsp_list);
-
-  // For each LSP, we extract and sanitize the path.
-  PathSanitizer path_sanitizer;
-  std::set<base::string16> lsp_paths;
-  for (size_t i = 0; i < lsp_list.size(); ++i) {
-    base::FilePath lsp_path(ExpandEnvironmentVariables(lsp_list[i].path));
-    path_sanitizer.StripHomeDirectory(&lsp_path);
-    lsp_paths.insert(lsp_path.value());
-  }
-
-  // Look for a match between LSPs and loaded dlls.
-  for (int i = 0; i < process->dll_size(); ++i) {
-    if (lsp_paths.count(base::UTF8ToWide(process->dll(i).path()))) {
-      process->mutable_dll(i)
-          ->add_feature(ClientIncidentReport_EnvironmentData_Process_Dll::LSP);
-    }
-  }
-}
-
 void CollectPlatformProcessData(
     ClientIncidentReport_EnvironmentData_Process* process) {
-  CollectDlls(process);
-  RecordLspFeature(process);
+  // TODO(pmonette): collect dlls and lsps.
 }
 
 }  // namespace safe_browsing
--- chrome/browser/safe_browsing/environment_data_collection_win.h
+++ chrome/browser/safe_browsing/environment_data_collection_win.h
@@ -7,16 +7,7 @@
 
 namespace safe_browsing {
 
-class ClientIncidentReport_EnvironmentData_Process;
-
-// Collects then populates |process| with the sanitized paths of all DLLs
-// loaded in the current process. Return false if an error occurred while
-// querying for the loaded dlls.
-bool CollectDlls(ClientIncidentReport_EnvironmentData_Process* process);
-
-// For each of the dlls in this already populated incident report,
-// check one of them is a registered LSP.
-void RecordLspFeature(ClientIncidentReport_EnvironmentData_Process* process);
+// TODO(pmonette): put DLL and LSP collection declarations here.
 
 }  // namespace safe_browsing
 
