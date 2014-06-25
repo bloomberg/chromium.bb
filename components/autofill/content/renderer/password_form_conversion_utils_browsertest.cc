744f7c0bcdd368f26e59c2d3068d5289d8c6d6c9
engedy@chromium.org <engedy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-06-25 12:01:48 +0000

    Reduce boilerplate in PasswordFormConversionUtilsTest.
    Introduce a PasswordFormBuilder so that we can describe testing forms in a more concise and clearer way (instead of using raw HTML in string literals).
    
    This is not critical right now, but will be very useful once we have a lot more tests for autocomplete attributes.
    
    BUG=375333
    
    Review URL: https://codereview.chromium.org/336403003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@279682 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 35a1807bff337f6c41998b272bf9cd2e6370dde8

--- components/autofill/content/renderer/password_form_conversion_utils_browsertest.cc
+++ components/autofill/content/renderer/password_form_conversion_utils_browsertest.cc
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 
 #include "base/strings/string16.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "components/autofill/content/renderer/password_form_conversion_utils.h"
 #include "components/autofill/core/common/password_form.h"
@@ -20,10 +21,77 @@ using blink::WebVector;
 namespace autofill {
 namespace {
 
+const char kTestFormActionURL[] = "http://cnn.com";
+
+// A builder to produce HTML code for a password form composed of the desired
+// number and kinds of username and password fields.
+class PasswordFormBuilder {
+ public:
+  // Creates a builder to start composing a new form. The form will have the
+  // specified |action| URL.
+  explicit PasswordFormBuilder(const char* action) {
+    base::StringAppendF(
+        &html_, "<FORM name=\"Test\" action=\"%s\" method=\"post\">", action);
+  }
+
+  // Appends a new text-type field at the end of the form, having the specified
+  // |name_and_id| and |value| attributes.
+  void AddUsernameField(const char* name_and_id, const char* value) {
+    base::StringAppendF(
+        &html_,
+        "<INPUT type=\"text\" name=\"%s\" id=\"%s\" value=\"%s\"/>",
+        name_and_id, name_and_id, value);
+  }
+
+  // Appends a new password-type field at the end of the form, having the
+  // specified |name_and_id| and |value| attributes.
+  void AddPasswordField(const char* name_and_id, const char* value) {
+    base::StringAppendF(
+        &html_,
+        "<INPUT type=\"password\" name=\"%s\" id=\"%s\" value=\"%s\"/>",
+        name_and_id, name_and_id, value);
+  }
+
+  // Appends a new submit-type field at the end of the form.
+  void AddSubmitButton() {
+    html_ += "<INPUT type=\"submit\" name=\"submit\" value=\"Submit\"/>";
+  }
+
+  // Returns the HTML code for the form containing the fields that have been
+  // added so far.
+  std::string ProduceHTML() const {
+    return html_ + "</FORM>";
+  }
+
+ private:
+  std::string html_;
+
+  DISALLOW_COPY_AND_ASSIGN(PasswordFormBuilder);
+};
+
 class PasswordFormConversionUtilsTest : public content::RenderViewTest {
  public:
   PasswordFormConversionUtilsTest() : content::RenderViewTest() {}
   virtual ~PasswordFormConversionUtilsTest() {}
+
+ protected:
+  // Loads the given |html|, retrieves the sole WebFormElement from it, and then
+  // calls CreatePasswordForm() to convert it into a |password_form|. Note that
+  // ASSERT() can only be used in void functions, this is why |password_form| is
+  // passed in as a pointer to a scoped_ptr.
+  void LoadHTMLAndConvertForm(const std::string& html,
+                              scoped_ptr<PasswordForm>* password_form) {
+    LoadHTML(html.c_str());
+
+    WebFrame* frame = GetMainFrame();
+    ASSERT_NE(static_cast<WebFrame*>(NULL), frame);
+
+    WebVector<WebFormElement> forms;
+    frame->document().forms(forms);
+    ASSERT_EQ(1U, forms.size());
+
+    *password_form = CreatePasswordForm(forms[0]);
+  }
 
  private:
   DISALLOW_COPY_AND_ASSIGN(PasswordFormConversionUtilsTest);
@@ -32,26 +100,18 @@ class PasswordFormConversionUtilsTest : public content::RenderViewTest {
 }  // namespace
 
 TEST_F(PasswordFormConversionUtilsTest, ValidWebFormElementToPasswordForm) {
-  LoadHTML("<FORM name=\"TestForm\" action=\"http://cnn.com\" method=\"post\">"
-           " <INPUT type=\"text\" name=\"username\" "
-               " id=\"username\" value=\"johnsmith\"/>"
-           " <INPUT type=\"submit\" name=\"submit\" value=\"Submit\"/>"
-           " <INPUT type=\"password\" name=\"password\" id=\"password\" "
-               " value=\"secret\"/>"
-           "</FORM>");
+  PasswordFormBuilder builder(kTestFormActionURL);
+  builder.AddUsernameField("username", "johnsmith");
+  builder.AddSubmitButton();
+  builder.AddPasswordField("password", "secret");
+  std::string html = builder.ProduceHTML();
 
-  WebFrame* frame = GetMainFrame();
-  ASSERT_NE(static_cast<WebFrame*>(NULL), frame);
-
-  WebVector<WebFormElement> forms;
-  frame->document().forms(forms);
-  ASSERT_EQ(1U, forms.size());
-
-  scoped_ptr<PasswordForm> password_form = CreatePasswordForm(forms[0]);
+  scoped_ptr<PasswordForm> password_form;
+  ASSERT_NO_FATAL_FAILURE(LoadHTMLAndConvertForm(html, &password_form));
   ASSERT_NE(static_cast<PasswordForm*>(NULL), password_form.get());
 
   EXPECT_EQ("data:", password_form->signon_realm);
-  EXPECT_EQ(GURL("http://cnn.com"), password_form->action);
+  EXPECT_EQ(GURL(kTestFormActionURL), password_form->action);
   EXPECT_EQ(base::UTF8ToUTF16("username"), password_form->username_element);
   EXPECT_EQ(base::UTF8ToUTF16("johnsmith"), password_form->username_value);
   EXPECT_EQ(base::UTF8ToUTF16("password"), password_form->password_element);
@@ -64,62 +124,43 @@ TEST_F(PasswordFormConversionUtilsTest, ValidWebFormElementToPasswordForm) {
 }
 
 TEST_F(PasswordFormConversionUtilsTest, InvalidWebFormElementToPasswordForm) {
-  LoadHTML("<FORM name=\"TestForm\" action=\"invalid\" method=\"post\">"
-           " <INPUT type=\"text\" name=\"username\" "
-               " id=\"username\" value=\"johnsmith\"/>"
-           " <INPUT type=\"submit\" name=\"submit\" value=\"Submit\"/>"
-           " <INPUT type=\"password\" name=\"password\" id=\"password\" "
-               " value=\"secret\"/>"
-           "</FORM>");
+  PasswordFormBuilder builder("invalid_target");
+  builder.AddUsernameField("username", "johnsmith");
+  builder.AddSubmitButton();
+  builder.AddPasswordField("password", "secret");
+  std::string html = builder.ProduceHTML();
 
-  WebFrame* frame = GetMainFrame();
-  ASSERT_NE(static_cast<WebFrame*>(NULL), frame);
-
-  WebVector<WebFormElement> forms;
-  frame->document().forms(forms);
-  ASSERT_EQ(1U, forms.size());
-
-  scoped_ptr<PasswordForm> password_form = CreatePasswordForm(forms[0]);
-  EXPECT_EQ(static_cast<PasswordForm*>(NULL), password_form.get());
+  scoped_ptr<PasswordForm> password_form;
+  ASSERT_NO_FATAL_FAILURE(LoadHTMLAndConvertForm(html, &password_form));
+  ASSERT_EQ(static_cast<PasswordForm*>(NULL), password_form.get());
 }
 
 TEST_F(PasswordFormConversionUtilsTest,
        WebFormWithMultipleUseNameAndPassWordFieldsToPasswordForm) {
-  LoadHTML("<FORM name=\"TestForm\" action=\"http://cnn.com\" method=\"post\">"
-           " <INPUT type=\"text\" name=\"username1\" "
-               " id=\"username1\" value=\"John\"/>"
-           " <INPUT type=\"password\" name=\"password1\" id=\"password1\" "
-               " value=\"encrypted\"/>"
-           " <INPUT type=\"text\" name=\"username2\" "
-               " id=\"username2\" value=\"Smith\"/>"
-           " <INPUT type=\"password\" name=\"password2\" id=\"password2\" "
-               " value=\"secret\"/>"
-           " <INPUT type=\"text\" name=\"username3\" "
-               " id=\"username3\" value=\"JohnSmith\"/>"
-           " <INPUT type=\"password\" name=\"password3\" id=\"password3\" "
-               " value=\"secret\"/>"
-           " <INPUT type=\"submit\" name=\"submit\" value=\"Submit\"/>"
-           "</FORM>");
+  PasswordFormBuilder builder(kTestFormActionURL);
+  builder.AddUsernameField("username1", "John");
+  builder.AddPasswordField("password1", "oldsecret");
+  builder.AddUsernameField("username2", "Smith");
+  builder.AddPasswordField("password2", "secret");
+  builder.AddUsernameField("username3", "JohnSmith");
+  builder.AddPasswordField("password3", "secret");
+  builder.AddSubmitButton();
+  std::string html = builder.ProduceHTML();
 
-  WebFrame* frame = GetMainFrame();
-  ASSERT_NE(static_cast<WebFrame*>(NULL), frame);
-
-  WebVector<WebFormElement> forms;
-  frame->document().forms(forms);
-  ASSERT_EQ(1U, forms.size());
-
-  scoped_ptr<PasswordForm> password_form = CreatePasswordForm(forms[0]);
+  scoped_ptr<PasswordForm> password_form;
+  ASSERT_NO_FATAL_FAILURE(LoadHTMLAndConvertForm(html, &password_form));
   ASSERT_NE(static_cast<PasswordForm*>(NULL), password_form.get());
 
   EXPECT_EQ("data:", password_form->signon_realm);
-  EXPECT_EQ(GURL("http://cnn.com"), password_form->action);
+  EXPECT_EQ(GURL(kTestFormActionURL), password_form->action);
   EXPECT_EQ(base::UTF8ToUTF16("username1"), password_form->username_element);
   EXPECT_EQ(base::UTF8ToUTF16("John"), password_form->username_value);
   EXPECT_EQ(base::UTF8ToUTF16("password2"), password_form->password_element);
   EXPECT_EQ(base::UTF8ToUTF16("secret"), password_form->password_value);
   EXPECT_EQ(base::UTF8ToUTF16("password1"),
             password_form->old_password_element);
-  EXPECT_EQ(base::UTF8ToUTF16("encrypted"), password_form->old_password_value);
+  EXPECT_EQ(base::UTF8ToUTF16("oldsecret"), password_form->old_password_value);
+  ASSERT_EQ(2u, password_form->other_possible_usernames.size());
   EXPECT_EQ(base::UTF8ToUTF16("Smith"),
             password_form->other_possible_usernames[0]);
   EXPECT_EQ(base::UTF8ToUTF16("JohnSmith"),
@@ -133,26 +174,16 @@ TEST_F(PasswordFormConversionUtilsTest,
 
 TEST_F(PasswordFormConversionUtilsTest,
        WebFormwithThreeDifferentPasswordsToPasswordForm) {
-  LoadHTML("<FORM name=\"TestForm\" action=\"http://cnn.com\" method=\"post\">"
-           " <INPUT type=\"text\" name=\"username\" "
-               " id=\"username\" value=\"John\"/>"
-           " <INPUT type=\"password\" name=\"password1\" id=\"password1\" "
-               " value=\"secret\"/>"
-           " <INPUT type=\"password\" name=\"password2\" id=\"password2\" "
-               " value=\"encrypted\"/>"
-           " <INPUT type=\"password\" name=\"password3\" id=\"password3\" "
-               " value=\"key\"/>"
-           " <INPUT type=\"submit\" name=\"submit\" value=\"Submit\"/>"
-           "</FORM>");
+  PasswordFormBuilder builder(kTestFormActionURL);
+  builder.AddUsernameField("username1", "John");
+  builder.AddPasswordField("password1", "alpha");
+  builder.AddPasswordField("password2", "beta");
+  builder.AddPasswordField("password3", "gamma");
+  builder.AddSubmitButton();
+  std::string html = builder.ProduceHTML();
 
-  WebFrame* frame = GetMainFrame();
-  ASSERT_NE(static_cast<WebFrame*>(NULL), frame);
-
-  WebVector<WebFormElement> forms;
-  frame->document().forms(forms);
-  ASSERT_EQ(1U, forms.size());
-
-  scoped_ptr<PasswordForm> password_form = CreatePasswordForm(forms[0]);
+  scoped_ptr<PasswordForm> password_form;
+  ASSERT_NO_FATAL_FAILURE(LoadHTMLAndConvertForm(html, &password_form));
   ASSERT_EQ(static_cast<PasswordForm*>(NULL), password_form.get());
 }
 
