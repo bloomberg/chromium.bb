d3d5d775c0d39abaea1fbd0b3978879a5a5d7aa1
ch.dumez@samsung.com <ch.dumez@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-06-27 04:54:11 +0000

    Add move constructor and assignment operator to RefPtr class
    Add move constructor and assignment operator to RefPtr class. As a result, we
    can now use the implicitly defined move constructor and assignment operator for
    Atomic String. Also, other classes with RefPtr data members like CString now
    get a better default move constructor and assignment operator.
    
    The release binary is ~21Kb smaller, there is no diff on the generated asm
    for CString and AtomicString. However, there is a small diff for WTFString.o:
    http://pastebin.com/RTzX23JV
    
    The generated assembly for "CString WTF::String::latin1() const" and
    "CString WTF::String::ascii() const" is slightly smaller, likely due to
    CString's improved move copy constructor as these methods return CStrings.
    
    Review URL: https://codereview.chromium.org/354023003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@177066 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: a22ee11452e880809cf36b36c29ccf876fcc164e

--- third_party/WebKit/Source/wtf/RefPtr.h
+++ third_party/WebKit/Source/wtf/RefPtr.h
@@ -44,6 +44,11 @@ namespace WTF {
         ALWAYS_INLINE RefPtr(const RefPtr& o) : m_ptr(o.m_ptr) { refIfNotNull(m_ptr); }
         template<typename U> RefPtr(const RefPtr<U>& o, EnsurePtrConvertibleArgDecl(U, T)) : m_ptr(o.get()) { refIfNotNull(m_ptr); }
 
+#if COMPILER_SUPPORTS(CXX_RVALUE_REFERENCES)
+        RefPtr(RefPtr&& o) : m_ptr(o.m_ptr) { o.m_ptr = 0; }
+        RefPtr& operator=(RefPtr&&);
+#endif
+
         // See comments in PassRefPtr.h for an explanation of why this takes a const reference.
         template<typename U> RefPtr(const PassRefPtr<U>&, EnsurePtrConvertibleArgDecl(U, T));
 
@@ -102,6 +107,14 @@ namespace WTF {
         swap(ptr);
         return *this;
     }
+
+#if COMPILER_SUPPORTS(CXX_RVALUE_REFERENCES)
+    template<typename T> inline RefPtr<T>& RefPtr<T>::operator=(RefPtr&& o)
+    {
+        swap(o);
+        return *this;
+    }
+#endif
 
     template<typename T> template<typename U> inline RefPtr<T>& RefPtr<T>::operator=(const RefPtr<U>& o)
     {
