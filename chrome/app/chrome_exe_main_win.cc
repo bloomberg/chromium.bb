0126351ef94eea264cc9fe1a1fceacbeb70d41b8
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-01 02:38:50 +0000

    Don't set dpi aware before Win7
    Because we don't support DirectWrite before Win7, enabling hidpi
    on Vista means that when DPI scale is set there, font rendering
    will be with GDI which results in bad kerning. So, leave Vista
    as DPI-unaware.
    
    See top left of http://imgur.com/FoIHHTh for how "changing font
    size" appears -- so this will probably be relatively common on
    Vista. I don't think there's a separate place to change just the
    font size, vs. changing DPI.
    
    R=cpu@chromium.org
    BUG=380298
    
    Review URL: https://codereview.chromium.org/346603006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@280736 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2db3e4820b099e96c9de55a635e045d6ad97d0eb

--- chrome/app/chrome_exe_main_win.cc
+++ chrome/app/chrome_exe_main_win.cc
@@ -10,6 +10,7 @@
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
+#include "base/win/windows_version.h"
 #include "chrome/app/client_util.h"
 #include "chrome/browser/chrome_process_finder_win.h"
 #include "chrome/browser/policy/policy_path_parser.h"
@@ -97,7 +98,11 @@ int main() {
   // The exit manager is in charge of calling the dtors of singletons.
   base::AtExitManager exit_manager;
 
-  gfx::EnableHighDPISupport();
+  // We don't want to set DPI awareness on Vista because we don't support
+  // DirectWrite there. GDI fonts are kerned very badly, so better to leave
+  // DPI-unaware and at effective 1.0. See also ShouldUseDirectWrite().
+  if (base::win::GetVersion() > base::win::VERSION_VISTA)
+    gfx::EnableHighDPISupport();
 
   if (AttemptFastNotify(*CommandLine::ForCurrentProcess()))
     return 0;
