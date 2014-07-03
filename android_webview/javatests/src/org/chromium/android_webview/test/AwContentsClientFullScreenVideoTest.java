2c99c8b12e97bb330d1f935705a871f18a3e30ab
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 03:48:26 +0000

    Temporarily disable WebView fullscreen video tests until we land fullscreen IPC changes
    there are 2 changes needs to be submitted before we reenable the tests:
    1. https://codereview.chromium.org/290643005/ and
    2. https://codereview.chromium.org/292573004/
    
    the blink change will break all the tests, so we need to reenable the tests after the chromium change lands
    
    BUG=367346
    
    Review URL: https://codereview.chromium.org/364043002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281165 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 29d0ed694a1905c6505cae2ebb909abab8fbc8b4

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenVideoTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientFullScreenVideoTest.java
@@ -12,6 +12,7 @@ import junit.framework.Assert;
 
 import org.chromium.android_webview.test.util.VideoTestWebServer;
 import org.chromium.base.CommandLine;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.browser.test.util.DOMUtils;
@@ -45,8 +46,12 @@ public class AwContentsClientFullScreenVideoTest extends AwTestBase {
         if (webServer != null) webServer.getTestWebServer().shutdown();
     }
 
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    crbug.com/367346
+    */
+    @DisabledTest
     public void testOnShowAndHideCustomViewWithCallback() throws Throwable {
         doOnShowAndHideCustomViewTest(new Runnable() {
             @Override
@@ -56,8 +61,12 @@ public class AwContentsClientFullScreenVideoTest extends AwTestBase {
         });
     }
 
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    crbug.com/367346
+    */
+    @DisabledTest
     public void testOnShowAndHideCustomViewWithBackKeyLegacy() throws Throwable {
         // When html controls are enabled we skip this test because pressing the back key
         // moves away from the current activity instead of exiting fullscreen mode.
@@ -76,8 +85,12 @@ public class AwContentsClientFullScreenVideoTest extends AwTestBase {
         });
     }
 
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    crbug.com/367346
+    */
+    @DisabledTest
     public void testOnShowAndHideCustomViewWithJavascript() throws Throwable {
         doOnShowAndHideCustomViewTest(new Runnable() {
             @Override
@@ -87,8 +100,12 @@ public class AwContentsClientFullScreenVideoTest extends AwTestBase {
         });
     }
 
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    crbug.com/367346
+    */
+    @DisabledTest
     public void testOnShowCustomViewAndPlayWithHtmlControl() throws Throwable {
         if (!areHtmlControlsEnabled())
             return;
