566c457137d0942b73f127d0609ffd529a18933c
tmdiep@chromium.org <tmdiep@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 03:45:25 +0000

    Do not create custom jumplists in tests
    This prevents leakage of files in
    ~\AppData\Roaming\Microsoft\Windows\Recent\CustomDestinations
    due to a side effect of browser navigations in tests.
    
    BUG=389375
    TEST=To verify that there are no regressions - on Win 7+, perform some
    browser navigations and right click on chrome's taskbar icon. Verify
    that the jumplist entries are updated accordingly.
    To verify the fix - execute some browser tests and verify that no
    *.customDestinations-ms files are added to the folder above.
    
    Review URL: https://codereview.chromium.org/361823007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281162 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 376a784dae081655b22aa08e359774482437b139

--- chrome/browser/jumplist_updater_win.cc
+++ chrome/browser/jumplist_updater_win.cc
@@ -8,10 +8,12 @@
 #include <propkey.h>
 #include <shobjidl.h>
 
+#include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/path_service.h"
 #include "base/win/win_util.h"
 #include "base/win/windows_version.h"
+#include "chrome/common/chrome_switches.h"
 
 namespace {
 
@@ -108,7 +110,9 @@ JumpListUpdater::~JumpListUpdater() {
 // static
 bool JumpListUpdater::IsEnabled() {
   // JumpList is implemented only on Windows 7 or later.
-  return base::win::GetVersion() >= base::win::VERSION_WIN7;
+  // Do not create custom JumpLists in tests. See http://crbug.com/389375.
+  return base::win::GetVersion() >= base::win::VERSION_WIN7 &&
+         !CommandLine::ForCurrentProcess()->HasSwitch(switches::kTestType);
 }
 
 bool JumpListUpdater::BeginUpdate() {
