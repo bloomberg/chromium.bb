ea70e1301e27a16f05b34ad17e8e3535e6dc6732
bengr@chromium.org <bengr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-03 00:00:07 +0000

    Elide data reduction proxy credentials from NetLog
    BUG=345907
    
    Review URL: https://codereview.chromium.org/361053002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281117 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a35b272e567c06ca18a7b12f5c7d5e40e1d5ea82

--- net/http/http_log_util.cc
+++ net/http/http_log_util.cc
@@ -7,6 +7,7 @@
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "net/http/http_auth_challenge_tokenizer.h"
+#include "net/http/http_util.h"
 
 namespace net {
 
@@ -33,39 +34,57 @@ bool ShouldRedactChallenge(HttpAuthChallengeTokenizer* challenge) {
 
 }  // namespace
 
+#if defined(SPDY_PROXY_AUTH_ORIGIN)
+void ElideChromeProxyDirective(const std::string& header_value,
+                               const std::string& directive,
+                               std::string::const_iterator* redact_begin,
+                               std::string::const_iterator* redact_end) {
+  HttpUtil::ValuesIterator it(header_value.begin(), header_value.end(), ',');
+  while (it.GetNext()) {
+    if (LowerCaseEqualsASCII(it.value_begin(),
+                             it.value_begin() + directive.size(),
+                             directive.c_str())) {
+      *redact_begin = it.value_begin();
+      *redact_end = it.value_end();
+      return;
+    }
+  }
+}
+#endif
+
 std::string ElideHeaderValueForNetLog(NetLog::LogLevel log_level,
                                       const std::string& header,
                                       const std::string& value) {
+  std::string::const_iterator redact_begin = value.begin();
+  std::string::const_iterator redact_end = value.begin();
 #if defined(SPDY_PROXY_AUTH_ORIGIN)
-  if (!base::strcasecmp(header.c_str(), "proxy-authorization") ||
-      !base::strcasecmp(header.c_str(), "proxy-authenticate")) {
-    return "[elided]";
+  if (!base::strcasecmp(header.c_str(), "chrome-proxy")) {
+    ElideChromeProxyDirective(value, "sid=", &redact_begin, &redact_end);
   }
 #endif
 
-  if (log_level < NetLog::LOG_STRIP_PRIVATE_DATA)
-    return value;
+  if (redact_begin == redact_end &&
+      log_level >= NetLog::LOG_STRIP_PRIVATE_DATA) {
 
-  // Note: this logic should be kept in sync with stripCookiesAndLoginInfo in
-  // chrome/browser/resources/net_internals/log_view_painter.js.
+    // Note: this logic should be kept in sync with stripCookiesAndLoginInfo in
+    // chrome/browser/resources/net_internals/log_view_painter.js.
 
-  std::string::const_iterator redact_begin = value.begin();
-  std::string::const_iterator redact_end = value.begin();
-  if (!base::strcasecmp(header.c_str(), "set-cookie") ||
-      !base::strcasecmp(header.c_str(), "set-cookie2") ||
-      !base::strcasecmp(header.c_str(), "cookie") ||
-      !base::strcasecmp(header.c_str(), "authorization") ||
-      !base::strcasecmp(header.c_str(), "proxy-authorization")) {
-    redact_begin = value.begin();
-    redact_end = value.end();
-  } else if (!base::strcasecmp(header.c_str(), "www-authenticate") ||
-             !base::strcasecmp(header.c_str(), "proxy-authenticate")) {
-    // Look for authentication information from data received from the server in
-    // multi-round Negotiate authentication.
-    HttpAuthChallengeTokenizer challenge(value.begin(), value.end());
-    if (ShouldRedactChallenge(&challenge)) {
-      redact_begin = challenge.params_begin();
-      redact_end = challenge.params_end();
+    if (!base::strcasecmp(header.c_str(), "set-cookie") ||
+        !base::strcasecmp(header.c_str(), "set-cookie2") ||
+        !base::strcasecmp(header.c_str(), "cookie") ||
+        !base::strcasecmp(header.c_str(), "authorization") ||
+        !base::strcasecmp(header.c_str(), "proxy-authorization")) {
+      redact_begin = value.begin();
+      redact_end = value.end();
+    } else if (!base::strcasecmp(header.c_str(), "www-authenticate") ||
+               !base::strcasecmp(header.c_str(), "proxy-authenticate")) {
+      // Look for authentication information from data received from the server
+      // in multi-round Negotiate authentication.
+      HttpAuthChallengeTokenizer challenge(value.begin(), value.end());
+      if (ShouldRedactChallenge(&challenge)) {
+        redact_begin = challenge.params_begin();
+        redact_end = challenge.params_end();
+      }
     }
   }
 
