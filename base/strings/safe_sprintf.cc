9d871541e12a419aa90a76fa9696b84790a69905
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-04 22:10:39 +0000

    Extend workaround from https://codereview.chromium.org/98403005 to clang/win.
    BUG=82385
    TBR=ch.dumez@samsung.com
    
    Review URL: https://codereview.chromium.org/364363002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281446 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1b129fb8f8097b0ab2aaddfc31b02672096ceea0

--- base/strings/safe_sprintf.cc
+++ base/strings/safe_sprintf.cc
@@ -110,8 +110,12 @@ class Buffer {
 // The following assertion does not build on Mac and Android. This is because
 // static_assert only works with compile-time constants, but mac uses
 // libstdc++4.2 and android uses stlport, which both don't mark
-// numeric_limits::max() as constexp.
-#if __cplusplus >= 201103 && !defined(OS_ANDROID) && !defined(OS_MACOSX) && !defined(OS_IOS)
+// numeric_limits::max() as constexp.  Likewise, MSVS2013's standard library
+// also doesn't mark max() as constexpr yet. cl.exe supports static_cast but
+// doesn't really implement constexpr yet so it doesn't complain, but clang
+// does.
+#if __cplusplus >= 201103 && !defined(OS_ANDROID) && !defined(OS_MACOSX) && \
+    !defined(OS_IOS) && !(defined(__clang__) && defined(OS_WIN))
     COMPILE_ASSERT(kSSizeMaxConst == \
                    static_cast<size_t>(std::numeric_limits<ssize_t>::max()),
                    kSSizeMax_is_the_max_value_of_an_ssize_t);
