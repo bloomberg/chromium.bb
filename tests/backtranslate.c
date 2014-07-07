14db6e188242c7de00ba88f1ab789c020ee186b0
Christian Egli <christian.egli@sbs.ch>
2014-07-07 13:57:31 +0200

    Add more words to the backtranslate test

--- tests/backtranslate.c
+++ tests/backtranslate.c
@@ -10,17 +10,20 @@ typedef struct test {
 } test_s;
 
 test_s tests[] = {
-  {"M5", "Men"},
-  {"M>k", "Mark"},
-  {"K5", "Ken"}
+  {",k5", "Ken"},
+  {",m5", "Men"},
+  {",m>k", "Mark"},
+  {",f5", "Fen"},
+  {",l5", "Len"},
+  {",t5", "Ten"},
+  NULL
 };
 
 int main(int argc, char **argv) {
   int result = 0;
-  int number_of_tests = sizeof(tests) / sizeof(tests[0]);
   const char *tbl = "en-us-g2.ctb";
 
-  for (int i = 0; i<2; i++)
+  for (int i = 0; tests[i].input; i++)
     result |= check_backtranslation(tbl, tests[i].input, NULL, tests[i].expected);
 
   return result;
