02cab794e1087cd97c001c004fec4d9e18ba4e35
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-08 11:23:19 +0000

    Undefine _POSIX_C_SOURCE before redefining in third_party/harfbuzz-ng/src/hb-blob.cc (again).
    BUG=391520
    TBR=eae@chromium.org,jshin@chromium.org
    
    Review URL: https://codereview.chromium.org/372843004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281718 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ac5bbee70c9a46f7abe563004fb47581afb18cf9

--- third_party/harfbuzz-ng/src/hb-blob.cc
+++ third_party/harfbuzz-ng/src/hb-blob.cc
@@ -25,6 +25,9 @@
  */
 
 /* http://www.oracle.com/technetwork/articles/servers-storage-dev/standardheaderfiles-453865.html */
+#if defined(_POSIX_C_SOURCE)
+#undef _POSIX_C_SOURCE
+#endif
 #define _POSIX_C_SOURCE 199309L
 
 #include "hb-private.hh"
