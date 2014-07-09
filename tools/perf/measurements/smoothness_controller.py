d1d94f346a78f58e7f157468f92039d6b039a4bf
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 23:20:57 +0000

    Revert of Update smoothness_controller to use results.AddValue(...) (https://codereview.chromium.org/380803002/)
    Reason for revert:
    Possibly caused Android Tests failures. See http://build.chromium.org/p/chromium.linux/builders/Android%20Tests/builds/14520/steps/telemetry_perf_unittests/logs/stdio
    
    E.g.
    Traceback (most recent call last):
      _RunPage at tools/telemetry/telemetry/page/page_runner.py:534
        test.RunPage(page, page_state.tab, results)
      RunPage at tools/telemetry/telemetry/page/page_test.py:247
        self.ValidatePage(page, tab, results)
      ValidatePage at tools/telemetry/telemetry/page/page_measurement.py:60
        self.MeasurePage(page, tab, results)
      MeasurePage at tools/perf/measurements/smoothness.py:36
        self._smoothness_controller.AddResults(tab, results)
      AddResults at tools/perf/measurements/smoothness_controller.py:100
        results.current_page, r.name, r.unit, r.value))
      __init__ at tools/telemetry/telemetry/value/scalar.py:19
        assert isinstance(value, numbers.Number)
    AssertionError
    
    Original issue's description:
    > Update smoothness_controller to use results.AddValue(...)
    > 
    > BUG=391943
    > TBR=nduca@chromium.org
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=282131
    
    TBR=nduca@chromium.org,nednguyen@google.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=391943
    
    Review URL: https://codereview.chromium.org/382673003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282170 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4d3217808680a9b5e15f3067eae2b472d526ca8f

--- tools/perf/measurements/smoothness_controller.py
+++ tools/perf/measurements/smoothness_controller.py
@@ -9,7 +9,6 @@ from telemetry.page import page_measurement
 from telemetry.page.actions import action_runner
 from telemetry.web_perf import timeline_interaction_record as tir_module
 from telemetry.web_perf.metrics import smoothness
-from telemetry.value import scalar
 
 
 RUN_SMOOTH_ACTIONS = 'RunSmoothAllActions'
@@ -96,8 +95,7 @@ class SmoothnessController(object):
       for r in tab.browser.platform.GetRawDisplayFrameRateMeasurements():
         if r.value is None:
           raise MissingDisplayFrameRateError(r.name)
-        results.AddValue(scalar.ScalarValue(
-            results.current_page, r.name, r.unit, r.value))
+        results.Add(r.name, r.unit, r.value)
 
   def CleanUp(self, tab):
     if tab.browser.platform.IsRawDisplayFrameRateSupported():
