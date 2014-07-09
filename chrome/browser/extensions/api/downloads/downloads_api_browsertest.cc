632c6505261f662e0d3a73d5b529798458fe0ac2
samuong@chromium.org <samuong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 05:23:04 +0000

    Disable DownloadExtensionTest_Download_InvalidURLs on Mac
    BUG=392288
    TBR=benjhayden@chromium.org
    
    Review URL: https://codereview.chromium.org/371363008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@281950 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 30759127f43d6be58149b1fa363813fb016f5b71

--- chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_browsertest.cc
@@ -1680,9 +1680,18 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
       download_url.c_str())).c_str());
 }
 
+// flaky on mac: crbug.com/392288
+#if defined(OS_MACOSX)
+#define MAYBE_DownloadExtensionTest_Download_InvalidURLs \
+        DISABLED_DownloadExtensionTest_Download_InvalidURLs
+#else
+#define MAYBE_DownloadExtensionTest_Download_InvalidURLs \
+        DownloadExtensionTest_Download_InvalidURLs
+#endif
+
 // Test that downloading invalid URLs immediately returns kInvalidURLError.
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
-                       DownloadExtensionTest_Download_InvalidURLs) {
+                       MAYBE_DownloadExtensionTest_Download_InvalidURLs) {
   LoadExtension("downloads_split");
   GoOnTheRecord();
 
