f1c8c54675c0d9abc4e3409639f9ce9b6afe7ee5
nednguyen@google.com <nednguyen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-09 20:10:33 +0000

    Update screenshot to use results.AddValue(...)
    BUG=391943
    TBR=nduca@chromium.org
    
    Review URL: https://codereview.chromium.org/380783003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282106 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 851c12558e35c951b5b8ca89020c164f808dc553

--- tools/perf/measurements/screenshot.py
+++ tools/perf/measurements/screenshot.py
@@ -5,6 +5,7 @@ import os
 
 from telemetry.page import page_measurement
 from telemetry.page import page_test
+from telemetry.value import scalar
 
 
 class Screenshot(page_measurement.PageMeasurement):
@@ -45,4 +46,6 @@ class Screenshot(page_measurement.PageMeasurement):
     saved_picture_count = 0
     if os.path.exists(outpath) and os.path.getmtime(outpath) > previous_mtime:
       saved_picture_count = 1
-    results.Add('saved_picture_count', 'count', saved_picture_count)
+    results.AddValue(scalar.ScalarValue(
+        results.current_page, 'saved_picture_count', 'count',
+        saved_picture_count))
