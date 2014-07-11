9167d995d06e34571b24a45447b124d5d6ed70cd
vivek.vg@samsung.com <vivek.vg@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-11 09:36:03 +0000

    [BuildBreak] Linux: Fix g++ 4.8 compilation for error: multi-line comment [-Werror=comment]
    While compling on linux with g++ 4.8, the file chrome/test/ppapi/ppapi_browsertest.cc is throwing
    
    ../../chrome/test/ppapi/ppapi_browsertest.cc:452:1: error: multi-line comment [-Werror=comment]
    cc1plus: all warnings being treated as errors
    
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/386933002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@282597 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 1ef5ea5ecd9562abf18997d02aa867d4a3330475

--- chrome/test/ppapi/ppapi_browsertest.cc
+++ chrome/test/ppapi/ppapi_browsertest.cc
@@ -449,7 +449,7 @@ TEST_PPAPI_NACL(HostResolverPrivate_ResolveIPv4)
 // TODO(yhirano): I remove this subtest from RUN_URLLOADER_SUBTESTS_3
 // in order to change the behavior in Blink.
 // Restore the subtest once it is landed.
-//      LIST_TEST(URLLoader_UntrustedHttpRequests)      \
+//      LIST_TEST(URLLoader_UntrustedHttpRequests)
 
 // Note: we do not support Trusted APIs in NaCl, so these will be skipped.
 #define RUN_URLLOADER_TRUSTED_SUBTESTS \
