fc4adf9dffee0933d09de4178178af24c2e896d7
kozyatinskiy@google.com <kozyatinskiy@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-14 14:35:25 +0000

    Added declarations to InjectedScriptExterns: JavaScriptCallFrame.prototype.evaluateWithExceptionDetails = function(expression) { } InjectedScriptHostClass.prototype.eval = function(expression) InjectedScriptHostClass.prototype.evaluateWithExceptionDetails = function(expression) { } These methods was added in https://codereview.chromium.org/289423002/
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/390853002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178059 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 4b26afd8c84f08ad20427378573fecf19b3bc3d5

--- third_party/WebKit/Source/core/inspector/InjectedScriptExterns.js
+++ third_party/WebKit/Source/core/inspector/InjectedScriptExterns.js
@@ -87,7 +87,11 @@ InjectedScriptHostClass.prototype.getEventListeners = function(object) { }
 /**
  * @param {string} expression
  */
-InjectedScriptHostClass.prototype.evaluate = function(expression) { }
+InjectedScriptHostClass.prototype.eval = function(expression) { }
+/**
+ * @param {string} expression
+ */
+InjectedScriptHostClass.prototype.evaluateWithExceptionDetails = function(expression) { }
 /**
  * @param {*} fn
  */
@@ -143,7 +147,10 @@ function JavaScriptCallFrame()
 JavaScriptCallFrame.prototype.scopeType = function(index) { }
 
 JavaScriptCallFrame.prototype.restart = function() { }
-
+/**
+ * @param {string} expression
+ */
+JavaScriptCallFrame.prototype.evaluateWithExceptionDetails = function(expression) { }
 /**
  * @param {number} scopeNumber
  * @param {string} variableName
