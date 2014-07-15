fb48eb2de56df526c3c538964032fc4b5139472e
hidehiko@chromium.org <hidehiko@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2014-07-15 04:35:09 +0000

    Wrap irt_interfaces.h by extern "C".
    We'll use this file from Chrome, and Chrome uses C++ code.
    So, we wraps the header by extern "C" if this is included from
    .cc files.
    
    TEST=Ran trybots.
    BUG=358465
    R=mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/387733005
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@13489 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 0301feb59dd3e9385eb5b4cb1dd0320bedf98844

--- src/nonsfi/irt/irt_interfaces.h
+++ src/nonsfi/irt/irt_interfaces.h
@@ -7,9 +7,15 @@
 #ifndef NATIVE_CLIENT_SRC_NONSFI_IRT_IRT_INTERFACES_H_
 #define NATIVE_CLIENT_SRC_NONSFI_IRT_IRT_INTERFACES_H_ 1
 
+#include "native_client/src/include/nacl_base.h"
+
+EXTERN_C_BEGIN
+
 typedef void (*nacl_entry_func_t)(void *args);
 
 int nacl_irt_nonsfi_entry(int argc, char **argv, char **environ,
                           nacl_entry_func_t entry_func);
+
+EXTERN_C_END
 
 #endif
