212ca320fb9a5d944411ed6566d8c31ada1df0a5
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-15 19:12:24 +0000

    content: Enable impl-side painting on Windows.
    BUG=169282
    
    Review URL: https://codereview.chromium.org/392623003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283229 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: a36569e7070a5cdc3b759586f3f2feb2b208bf80

--- content/browser/gpu/compositor_util.cc
+++ content/browser/gpu/compositor_util.cc
@@ -230,7 +230,7 @@ bool IsImplSidePaintingEnabled() {
       switches::kEnableBleedingEdgeRenderingFastPaths))
     return true;
 
-#if defined(OS_MACOSX) || defined(OS_WIN)
+#if defined(OS_MACOSX)
   return false;
 #else
   return IsThreadedCompositingEnabled();
