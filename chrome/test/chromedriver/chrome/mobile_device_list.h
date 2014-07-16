7bedce2db43aaffb1d091145a7f9bdf352148e97
samuong@chromium.org <samuong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-16 05:20:35 +0000

    [chromedriver] Hardcode mobile devices list.
    Previously chromedriver generated the mobile devices list from OverridesUI.js in Blink, however changes introduced in Blink revision 177868 break our tests. I'm hardcoding the value of our mobile devices list to what is was prior to this Blink change, until we implement a new way to generate the list.
    
    BUG=392430
    
    Review URL: https://codereview.chromium.org/390513002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283354 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 91d2128aa3070960c36c83c8b1c57ba458033e4c

---
+++ chrome/test/chromedriver/chrome/mobile_device_list.h
@@ -0,0 +1,10 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_TEST_CHROMEDRIVER_CHROME_MOBILE_DEVICE_LIST_H_
+#define CHROME_TEST_CHROMEDRIVER_CHROME_MOBILE_DEVICE_LIST_H_
+
+extern const char kMobileDevices[];
+
+#endif  // CHROME_TEST_CHROMEDRIVER_CHROME_MOBILE_DEVICE_LIST_H_
