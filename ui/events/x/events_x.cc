4ba7fd698165ecf6d4884fa9a9d9956f8e982691
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-18 03:06:09 +0000

    x11: Add a missing break statement from r283931.
    BUG=none
    TBR=kpschoedel@chromium.org
    
    Review URL: https://codereview.chromium.org/403713002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@283987 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3b67224783df5f125242a411578dbb97a26ed8fd

--- ui/events/x/events_x.cc
+++ ui/events/x/events_x.cc
@@ -601,6 +601,7 @@ uint32 PlatformKeycodeFromNative(const base::NativeEvent& native_event) {
           NOTREACHED();
           break;
       }
+      break;
     }
     default:
       NOTREACHED();
