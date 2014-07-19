1d36441e9de0dfc17e99a04f862c7fffa7d452db
ivandavid@chromium.org <ivandavid@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-19 03:22:08 +0000

    Modifications to the layout test framework so that it can use browser_tests as a driver. This is for end to end testing for print preview. BUG=388517
    Review URL: https://codereview.chromium.org/401883007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178511 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 90986b4dd0bb478028bad24b9442e55c8d1ea331

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/driver.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/driver.py
@@ -150,7 +150,8 @@ class Driver(object):
         Returns a DriverOutput object.
         """
         start_time = time.time()
-        self.start(driver_input.should_run_pixel_test, driver_input.args)
+        stdin_deadline = start_time + int(driver_input.timeout) / 2000.0
+        self.start(driver_input.should_run_pixel_test, driver_input.args, stdin_deadline)
         test_begin_time = time.time()
         self.error_from_test = str()
         self.err_seen_eof = False
@@ -265,7 +266,7 @@ class Driver(object):
             return True
         return False
 
-    def start(self, pixel_tests, per_test_args):
+    def start(self, pixel_tests, per_test_args, deadline):
         new_cmd_line = self.cmd_line(pixel_tests, per_test_args)
         if not self._server_process or new_cmd_line != self._current_cmd_line:
             self._start(pixel_tests, per_test_args)
@@ -430,7 +431,8 @@ class Driver(object):
             or self._read_header(block, line, 'Content-Length: ', '_content_length', int)
             or self._read_header(block, line, 'ActualHash: ', 'content_hash')
             or self._read_header(block, line, 'DumpMalloc: ', 'malloc')
-            or self._read_header(block, line, 'DumpJSHeap: ', 'js_heap')):
+            or self._read_header(block, line, 'DumpJSHeap: ', 'js_heap')
+            or self._read_header(block, line, 'StdinPath', 'stdin_path')):
             return
         # Note, we're not reading ExpectedHash: here, but we could.
         # If the line wasn't a header, we just append it to the content.
@@ -507,6 +509,7 @@ class ContentBlock(object):
         self.decoded_content = None
         self.malloc = None
         self.js_heap = None
+        self.stdin_path = None
 
     def decode_content(self):
         if self.encoding == 'base64' and self.content is not None:
