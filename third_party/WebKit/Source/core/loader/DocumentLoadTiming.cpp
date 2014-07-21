8954ef316baa46b64a8ea6d314bbb6d2e918968a
ppi@chromium.org <ppi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-21 13:29:14 +0000

    Fix Performance::now() for browser navigations.
    Performance::now() is defined[1] as number of milliseconds since
    navigationStart. It works correctly for renderer-initiated navigations,
    but for browser-initiated ones we override the navigationStart with the
    recording taken on the browser side and this is not represented in
    Performace::now().
    
    Among other reasons to be sad about this, this bug makes Chromium's
    pagecyclers not represent the renderer initialization times in their
    plt measurements.
    
    This patch fixes this by adjusting the fields that are supposed to
    correspond to navigationStart when we change navigationStart from the
    embedder.
    
    [1] http://www.w3.org/TR/hr-time/#dom-performance-now
    
    BUG=376004
    
    Review URL: https://codereview.chromium.org/379873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178558 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 266aab48af80afbacf2af3eea7ef2a6cb8a40e30

--- third_party/WebKit/Source/core/loader/DocumentLoadTiming.cpp
+++ third_party/WebKit/Source/core/loader/DocumentLoadTiming.cpp
@@ -74,7 +74,18 @@ void DocumentLoadTiming::markNavigationStart()
 void DocumentLoadTiming::setNavigationStart(double navigationStart)
 {
     ASSERT(m_referenceMonotonicTime && m_referenceWallTime);
+
+    // This should be used only to indicate that the navigation started in the
+    // embedder before the renderer was created.
+    ASSERT(navigationStart <= m_navigationStart);
     m_navigationStart = navigationStart;
+
+    // |m_referenceMonotonicTime| and |m_referenceWallTime| represent
+    // navigationStart. When the embedder sets navigationStart (because the
+    // navigation started earlied on the browser side), we need to adjust these
+    // as well.
+    m_referenceWallTime = monotonicTimeToPseudoWallTime(navigationStart);
+    m_referenceMonotonicTime = navigationStart;
 }
 
 void DocumentLoadTiming::addRedirect(const KURL& redirectingUrl, const KURL& redirectedUrl)
