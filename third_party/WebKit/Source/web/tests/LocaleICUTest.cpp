5f09442eba45414614d60c2b440e429dc7c640a7
jshin@chromium.org <jshin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-21 18:03:35 +0000

    Update Layout test and webkit test after ICU52 roll
    Now that ICU was rolled to ICU 52, two layout tests marked as failure
    pass beause the expectation was updated in advance to prepare for ICU 52 update.
    
    Likewise, a test for shortStandAloneMonth names in ru_RU should be
    re-enabled.
    
    BUG=378919
    TEST=1.LayoutTest: fast/forms/datetimelocal-multiple-fields/datetimelocal-multiple-fields-*
    2. webkit_unit_tests: LocaleICU*
    TBR=tkent@chromium.org
    
    Review URL: https://codereview.chromium.org/403393004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178574 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2da77fe509be02abc8e17802e593f1bbcc05a15b

--- third_party/WebKit/Source/web/tests/LocaleICUTest.cpp
+++ third_party/WebKit/Source/web/tests/LocaleICUTest.cpp
@@ -209,16 +209,9 @@ TEST_F(LocaleICUTest, shortMonthLabels)
     EXPECT_STREQ("12\xE6\x9C\x88", shortStandAloneMonthLabel("ja_JP", 11).utf8().data());
 
     EXPECT_STREQ("\xD0\xBC\xD0\xB0\xD1\x80\xD1\x82\xD0\xB0", shortMonthLabel("ru_RU", 2).utf8().data());
-#if 0
-    // TODO(jshin): Disable these temporarily until ICU update is completed.
-    // See http://crbug.com/377042
     EXPECT_STREQ("\xD0\x9C\xD0\xB0\xD1\x80\xD1\x82", shortStandAloneMonthLabel("ru_RU", 2).utf8().data());
-#endif
     EXPECT_STREQ("\xD0\xBC\xD0\xB0\xD1\x8F", shortMonthLabel("ru_RU", 4).utf8().data());
-#if 0
-    // Same as above
     EXPECT_STREQ("\xD0\x9C\xD0\xB0\xD0\xB9", shortStandAloneMonthLabel("ru_RU", 4).utf8().data());
-#endif
 }
 
 TEST_F(LocaleICUTest, timeAMPMLabels)
