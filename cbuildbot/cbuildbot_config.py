47cbcefccf37de45a7eb3128cedb37ce3cdb42b9
Yu-Ju Hong <yjhong@chromium.org>
2014-07-22 11:34:26 -0700

    cbuildbot_config: add a boardless Chromium PFQ master config
    This CL adds a boardless config chromium-pfq-master. This will
    eventually replace x86-generic-chromium-pfq as the PFQ master.
    
    The next steps are:
      - Assign a machine to it in the waterfall configs
      - Upload a CL to turn on master=true for this config, and make
        x86-generic-chromium-pfq a slave config
    
    BUG=chromium:363659
    TEST=unittest
    TEST=Ran the config using remote trybot (by stealing an existing config name)
    
    Change-Id: I26f78adf2357283ca600823346f207f49113df3b
    Reviewed-on: https://chromium-review.googlesource.com/209362
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Commit-Queue: Yu-Ju Hong <yjhong@chromium.org>
    Tested-by: Yu-Ju Hong <yjhong@chromium.org>

--- cbuildbot/cbuildbot_config.py
+++ cbuildbot/cbuildbot_config.py
@@ -1179,6 +1179,19 @@ internal_chromium_pfq = internal.derive(
   prebuilts=constants.PUBLIC,
 )
 
+# TODO: this config will replace x86-generic-chromium-pfq as the
+# master config once the waterfall change is completed
+# (crbug.com/363659).
+internal_chromium_pfq.add_config('master-chromium-pfq',
+  boards=[],
+  master=False,
+  push_overlays=constants.BOTH_OVERLAYS,
+  afdo_update_ebuild=True,
+  sync_chrome=False,
+  chrome_sdk=False,
+  important=False,
+)
+
 internal_chromium_pfq.add_config('x86-generic-chromium-pfq',
   boards=['x86-generic'],
   master=True,
