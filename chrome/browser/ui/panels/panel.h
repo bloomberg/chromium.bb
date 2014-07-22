b2d4bf4d077ca13e7a290401512f2e26ffe00cb6
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-22 21:10:54 +0000

    Revert 284725 "Use ExtensionRegistryObserver instead of DEPRECAT..."
    This appears to have broke
    StackedPanelBrowserTest.AddNewPanelFromDifferentProfile on windows
    interactive.
    http://build.chromium.org/p/chromium.win/builders/Win%207%20Tests%20x64%20%281%29/builds/15556/steps/interactive_ui_tests/logs/AddNewPanelFromDifferentProfile
    
    [ RUN      ] StackedPanelBrowserTest.AddNewPanelFromDifferentProfile
    Backtrace:
            extensions::ExtensionRegistry::RemoveObserver
            [0x00000001430B3030+32]
            (No symbol) [0x00000000086491C0]
            base::SupportsWeakPtr\u003CObserverListBase\u003Caura::WindowObserver>
            >::AsWeakPtr [0x000007FEEF42B175+85]
    
    StackedPanelBrowserTest.AddNewPanelFromDifferentProfile (run #2):
    ...
    
    
    > Use ExtensionRegistryObserver instead of DEPRECATED extension notify.
    > 
    > BUG=354458
    > 
    > Review URL: https://codereview.chromium.org/262223003
    
    TBR=limasdf@gmail.com
    
    Review URL: https://codereview.chromium.org/409983002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284769 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4fd26bf4e30c347f224598c9a05bf048e6e01824

--- chrome/browser/ui/panels/panel.h
+++ chrome/browser/ui/panels/panel.h
@@ -10,7 +10,6 @@
 #include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
 #include "base/strings/string16.h"
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/command_updater_delegate.h"
@@ -18,7 +17,6 @@
 #include "chrome/browser/ui/panels/panel_constants.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "extensions/browser/extension_registry_observer.h"
 #include "ui/base/base_window.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/rect.h"
@@ -38,7 +36,6 @@ struct NativeWebKeyboardEvent;
 
 namespace extensions {
 class Extension;
-class ExtensionRegistry;
 class WindowController;
 }
 
@@ -53,8 +50,7 @@ class WindowController;
 //   other Panels. For example deleting a panel would rearrange other panels.
 class Panel : public ui::BaseWindow,
               public CommandUpdaterDelegate,
-              public content::NotificationObserver,
-              public extensions::ExtensionRegistryObserver {
+              public content::NotificationObserver {
  public:
   enum ExpansionState {
     // The panel is fully expanded with both title-bar and the client-area.
@@ -148,12 +144,6 @@ class Panel : public ui::BaseWindow,
   virtual void Observe(int type,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
-
-  // extensions::ExtensionRegistryObserver implementation.
-  virtual void OnExtensionUnloaded(
-      content::BrowserContext* browser_context,
-      const extensions::Extension* extension,
-      extensions::UnloadedExtensionInfo::Reason reason) OVERRIDE;
 
   // Construct a native panel implementation.
   static NativePanel* CreateNativePanel(Panel* panel,
@@ -401,11 +391,6 @@ class Panel : public ui::BaseWindow,
   const SessionID session_id_;
   scoped_ptr<extensions::WindowController> extension_window_controller_;
   scoped_ptr<PanelHost> panel_host_;
-
-  // Listen to extension unloaded notification.
-  ScopedObserver<extensions::ExtensionRegistry,
-                 extensions::ExtensionRegistryObserver>
-      extension_registry_observer_;
 
   // Icon showed in the task bar.
   gfx::Image app_icon_;
