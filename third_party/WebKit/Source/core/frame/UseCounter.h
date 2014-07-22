c23fbd5922b67af57fc4fd56a71371060c0b63d2
jinho.bang@samsung.com <jinho.bang@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-22 14:40:06 +0000

    Adding use counters for non-standard WebKitPoint.
    BUG=395918
    
    Review URL: https://codereview.chromium.org/406193002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178661 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c7551d9decc668d9d4817b544744fb8582249f58

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -486,6 +486,7 @@ public:
         VideoFullscreenAllowedExemption = 485,
         PPAPIRequestAllowedByObjectSrc = 486,
         PPAPIRequestBypassedObjectSrc = 487,
+        WebKitPoint = 488,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
