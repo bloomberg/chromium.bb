7cfcf7aa2803feae3be61629e655769daee11148
jdonnelly@chromium.org <jdonnelly@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 00:47:06 +0000

    Revert 284761 "Create a ManifestPermission implementation for Au..."
    AutomationApiTest.GetTreeByTabId was disabled because it consistently fails on chromeos (see http://crbug.com/379927). Reverting this change since it re-enables that test.
    
    > Create a ManifestPermission implementation for Automation.
    > 
    > BUG=
    > 
    > Review URL: https://codereview.chromium.org/377553003
    
    TBR=aboxhall@chromium.org
    
    Review URL: https://codereview.chromium.org/411583006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284824 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d432f8bed35a2bf22e807c3908a460ab32ba8613

--- chrome/browser/extensions/api/automation/automation_apitest.cc
+++ chrome/browser/extensions/api/automation/automation_apitest.cc
@@ -96,7 +96,7 @@ IN_PROC_BROWSER_TEST_F(AutomationApiTest, TestRendererAccessibilityEnabled) {
 }
 
 #if defined(ADDRESS_SANITIZER)
-#define Maybe_SanityCheck SanityCheck
+#define Maybe_SanityCheck DISABLED_SanityCheck
 #else
 #define Maybe_SanityCheck SanityCheck
 #endif
@@ -112,7 +112,7 @@ IN_PROC_BROWSER_TEST_F(AutomationApiTest, Unit) {
 }
 
 // Test is failing on ASAN bots, crbug.com/379927
-IN_PROC_BROWSER_TEST_F(AutomationApiTest, GetTreeByTabId) {
+IN_PROC_BROWSER_TEST_F(AutomationApiTest, DISABLED_GetTreeByTabId) {
   StartEmbeddedTestServer();
   ASSERT_TRUE(RunExtensionSubtest("automation/tests/tabs", "tab_id.html"))
       << message_;
@@ -120,7 +120,7 @@ IN_PROC_BROWSER_TEST_F(AutomationApiTest, GetTreeByTabId) {
 
 #if defined(OS_LINUX) && defined(ADDRESS_SANITIZER)
 // Failing on Linux ASan bot: http://crbug.com/391279
-#define MAYBE_Events Events
+#define MAYBE_Events DISABLED_Events
 #else
 #define MAYBE_Events Events
 #endif
@@ -133,7 +133,7 @@ IN_PROC_BROWSER_TEST_F(AutomationApiTest, MAYBE_Events) {
 
 #if defined(OS_LINUX) && defined(ADDRESS_SANITIZER)
 // Timing out on linux ASan bot: http://crbug.com/385701
-#define MAYBE_Actions Actions
+#define MAYBE_Actions DISABLED_Actions
 #else
 #define MAYBE_Actions Actions
 #endif
@@ -145,7 +145,7 @@ IN_PROC_BROWSER_TEST_F(AutomationApiTest, MAYBE_Actions) {
 }
 
 #if defined(ADDRESS_SANITIZER)
-#define Maybe_Location Location
+#define Maybe_Location DISABLED_Location
 #else
 #define Maybe_Location Location
 #endif
@@ -164,7 +164,7 @@ IN_PROC_BROWSER_TEST_F(AutomationApiTest, TabsAutomationBooleanPermissions) {
 
 // See crbug.com/384673
 #if defined(ADDRESS_SANITIZER) || defined(OS_CHROMEOS) || defined(OS_LINUX)
-#define Maybe_TabsAutomationBooleanActions TabsAutomationBooleanActions
+#define Maybe_TabsAutomationBooleanActions DISABLED_TabsAutomationBooleanActions
 #else
 #define Maybe_TabsAutomationBooleanActions TabsAutomationBooleanActions
 #endif
