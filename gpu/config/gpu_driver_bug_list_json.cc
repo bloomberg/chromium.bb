8bcb06b8033705ff5bc7c255c1c384decf2fb339
jmadill@chromium.org <jmadill@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 21:00:42 +0000

    Force disable-d3d11 for Matrox video cards.
    These cards either do not support D3D11 fully, or interact with an
    unknown ANGLE bug to cause black screens and an unusable browser.
    Force off D3D11 until we can diagnose if a fix is possible.
    
    BUG=395861
    
    Review URL: https://codereview.chromium.org/409223004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285039 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: b2984ae8cc137929819d71985a30571f8dd54356

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -19,7 +19,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "6.6",
+  "version": "6.7",
   "entries": [
     {
       "id": 1,
@@ -1101,6 +1101,18 @@ LONG_STRING_CONST(
       "vendor_id": "0x10de",
       "features": [
         "scalarize_vec_and_mat_constructor_args"
+      ]
+    },
+    {
+      "id": 86,
+      "description": "Disable use of Direct3D 11 on Matrox video cards",
+      "cr_bugs": [395861],
+      "os": {
+        "type": "win"
+      },
+      "vendor_id": "0x102b",
+      "features": [
+        "disable_d3d11"
       ]
     }
   ]
