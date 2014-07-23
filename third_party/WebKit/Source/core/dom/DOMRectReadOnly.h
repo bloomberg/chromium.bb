3f2e5bc4d879f9785a57480261ed1d1904258b7b
jinho.bang@samsung.com <jinho.bang@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-23 15:44:32 +0000

    Implement DOMRect of geometry interfaces.
    The DOMRect can represent a rectangle.
    
    The specification:
    http://dev.w3.org/fxtf/geometry/
    
    Intent to Implement:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/V_bJNtOg0oM
    
    BUG=388780
    
    Review URL: https://codereview.chromium.org/403383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178770 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: cbfb8e068dc4f423f3c710ce0dd2a96caddb6d16

---
+++ third_party/WebKit/Source/core/dom/DOMRectReadOnly.h
@@ -0,0 +1,39 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DOMRectReadOnly_h
+#define DOMRectReadOnly_h
+
+#include "platform/heap/Handle.h"
+
+namespace WebCore {
+
+class DOMRectReadOnly : public GarbageCollected<DOMRectReadOnly> {
+public:
+    static DOMRectReadOnly* create(double x, double y, double width, double height);
+
+    double x() const { return m_x; }
+    double y() const { return m_y; }
+    double width() const { return m_width; }
+    double height() const { return m_height; }
+
+    double top() const { return std::min(m_y, m_y + m_height); }
+    double right() const { return std::max(m_x, m_x + m_width); }
+    double bottom() const { return std::max(m_y, m_y + m_height); }
+    double left() const { return std::min(m_x, m_x + m_width); }
+
+    void trace(Visitor*) { }
+
+protected:
+    DOMRectReadOnly(double x, double y, double width, double height);
+
+    double m_x;
+    double m_y;
+    double m_width;
+    double m_height;
+};
+
+} // namespace WebCore
+
+#endif
