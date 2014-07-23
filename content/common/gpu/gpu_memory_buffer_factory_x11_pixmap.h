efce1dc94684e1df3f2afec9c295547c3560b599
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 17:17:24 +0000

    content: Fix incorrect GpuMemoryBufferFactoryX11Pixmap comment.
    BUG=
    TBR=piman@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/411023002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284975 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 891cda4ec97a861dcdb27980bd25a6ad430e66b8

--- content/common/gpu/gpu_memory_buffer_factory_x11_pixmap.h
+++ content/common/gpu/gpu_memory_buffer_factory_x11_pixmap.h
@@ -28,8 +28,7 @@ class GpuMemoryBufferFactoryX11Pixmap {
   // Destroy a previously created GPU memory buffer.
   void DestroyGpuMemoryBuffer(const gfx::GpuMemoryBufferId& id);
 
-  // Creates a GLImage instance for a GPU memory buffer. Successful completion
-  // moves ownership of GPU memory buffer to GLImage instance.
+  // Creates a GLImage instance for a GPU memory buffer.
   scoped_refptr<gfx::GLImage> AcquireImageForGpuMemoryBuffer(
       const gfx::GpuMemoryBufferId& id,
       const gfx::Size& size,
