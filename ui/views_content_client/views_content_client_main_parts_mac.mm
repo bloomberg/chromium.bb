5e8beb1104b07691269d144b8dd1d512ad90fac2
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-23 08:46:04 +0000

    Remaining bits to get views_examples_with_content_exe to work on Mac
    The example window currently doesn't activate itself properly because it
    lacks an activation policy and a menubar.
    
    This CL sets the activation policy, adds a menubar and defers window
    creation until the applicationDidFinishLoading notification. Creating a
    window before this can activate and receive key events, but the menubar
    will not switch properly.
    
    With this, views_examples_with_content_exe compiles and runs nicely on a
    Mac build with `GYP_DEFINES=toolkit-views=1`. Note the `WebView` example
    will crash for now because views::NativeViewHost is not yet implemented
    for mac.
    
    BUG=395507
    
    Review URL: https://codereview.chromium.org/309483009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@284883 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 86487aa3a77b454917b5a1cb0c42b05cb6b22136

--- ui/views_content_client/views_content_client_main_parts_mac.mm
+++ ui/views_content_client/views_content_client_main_parts_mac.mm
@@ -2,12 +2,27 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import <Cocoa/Cocoa.h>
+
 #include "base/files/file_path.h"
+#include "base/mac/scoped_nsobject.h"
 #include "base/path_service.h"
 #include "content/public/common/content_paths.h"
 #include "content/shell/browser/shell_browser_context.h"
 #include "ui/views_content_client/views_content_client.h"
 #include "ui/views_content_client/views_content_client_main_parts.h"
+
+// A simple NSApplicationDelegate that provides a basic mainMenu and can
+// activate a task when the application has finished loading.
+@interface ViewsContentClientAppController : NSObject<NSApplicationDelegate> {
+ @private
+  base::Closure task_;
+}
+
+// Set the task to run after receiving -applicationDidFinishLaunching:.
+- (void)setTask:(const base::Closure&)task;
+
+@end
 
 namespace ui {
 
@@ -18,12 +33,14 @@ class ViewsContentClientMainPartsMac : public ViewsContentClientMainParts {
   ViewsContentClientMainPartsMac(
       const content::MainFunctionParams& content_params,
       ViewsContentClient* views_content_client);
-  virtual ~ViewsContentClientMainPartsMac() {}
+  virtual ~ViewsContentClientMainPartsMac();
 
   // content::BrowserMainParts:
   virtual void PreMainMessageLoopRun() OVERRIDE;
 
  private:
+  base::scoped_nsobject<ViewsContentClientAppController> app_controller_;
+
   DISALLOW_COPY_AND_ASSIGN(ViewsContentClientMainPartsMac);
 };
 
@@ -34,12 +51,25 @@ ViewsContentClientMainPartsMac::ViewsContentClientMainPartsMac(
   // Cache the child process path to avoid triggering an AssertIOAllowed.
   base::FilePath child_process_exe;
   PathService::Get(content::CHILD_PROCESS_EXE, &child_process_exe);
+
+  app_controller_.reset([[ViewsContentClientAppController alloc] init]);
+  [[NSApplication sharedApplication] setDelegate:app_controller_];
 }
 
 void ViewsContentClientMainPartsMac::PreMainMessageLoopRun() {
   ViewsContentClientMainParts::PreMainMessageLoopRun();
 
-  views_content_client()->task().Run(browser_context(), NULL);
+  // On Mac, the task must be deferred to applicationDidFinishLaunching. If not,
+  // the widget can activate, but (even if configured) the mainMenu won't be
+  // ready to switch over in the OSX UI, so it will look strange.
+  NSWindow* window_context = nil;
+  [app_controller_ setTask:base::Bind(views_content_client()->task(),
+                                      base::Unretained(browser_context()),
+                                      base::Unretained(window_context))];
+}
+
+ViewsContentClientMainPartsMac::~ViewsContentClientMainPartsMac() {
+  [[NSApplication sharedApplication] setDelegate:nil];
 }
 
 }  // namespace
@@ -53,3 +83,36 @@ ViewsContentClientMainParts* ViewsContentClientMainParts::Create(
 }
 
 }  // namespace ui
+
+@implementation ViewsContentClientAppController
+
+- (void)setTask:(const base::Closure&)task {
+  task_ = task;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification*)aNotification {
+  // To get key events, the application needs to have an activation policy.
+  // Unbundled apps (i.e. those without an Info.plist) default to
+  // NSApplicationActivationPolicyProhibited.
+  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+
+  // Create a basic mainMenu object using the executable filename.
+  base::scoped_nsobject<NSMenu> mainMenu([[NSMenu alloc] initWithTitle:@""]);
+  NSMenuItem* appMenuItem =
+      [mainMenu addItemWithTitle:@"" action:NULL keyEquivalent:@""];
+  [NSApp setMainMenu:mainMenu];
+
+  base::scoped_nsobject<NSMenu> appMenu([[NSMenu alloc] initWithTitle:@""]);
+  NSString* appName = [[NSProcessInfo processInfo] processName];
+  // TODO(tapted): Localize "Quit" if this is ever used for a released binary.
+  // At the time of writing, ui_strings.grd has "Close" but not "Quit".
+  NSString* quitTitle = [@"Quit " stringByAppendingString:appName];
+  [appMenu addItemWithTitle:quitTitle
+                     action:@selector(terminate:)
+              keyEquivalent:@"q"];
+  [appMenuItem setSubmenu:appMenu];
+
+  task_.Run();
+}
+
+@end
