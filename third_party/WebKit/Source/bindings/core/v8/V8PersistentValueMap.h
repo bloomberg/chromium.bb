7948e199fad310c0f96bcdf930d12d7f2eac4f51
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-23 19:58:47 +0000

    When overwriting a persistent handle, actually overwrite it
    BUG=390928
    R=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/412843003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178780 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 73439ee8d076ef553092af7ba9d6e6d130428f7a

--- third_party/WebKit/Source/bindings/core/v8/V8PersistentValueMap.h
+++ third_party/WebKit/Source/bindings/core/v8/V8PersistentValueMap.h
@@ -65,7 +65,7 @@ public:
         Impl* impl, KeyType key, v8::PersistentContainerValue value)
     {
         v8::PersistentContainerValue oldValue = Get(impl, key);
-        impl->add(key, value);
+        impl->set(key, value);
         return oldValue;
     }
     static v8::PersistentContainerValue Get(const Impl* impl, KeyType key)
