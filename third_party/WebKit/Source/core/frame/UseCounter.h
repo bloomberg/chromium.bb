19014546397960402d787f7edb61b8d7ff644284
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-23 16:51:35 +0000

    Add a UseCounter for HTMLPreElement.width
    The content attribute has no effect, so this API is completely useless.
    It is widely implemented, but was removed from spec, so attempting
    removal seems worthwhile, as it could leave HTMLPreElement empty.
    
    http://whatwg.org/html#the-pre-element
    
    Review URL: https://codereview.chromium.org/415533002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178773 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6a10196c1cbb926e63de6d367750d78b1361c5a6

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -487,6 +487,7 @@ public:
         PPAPIRequestAllowedByObjectSrc = 486,
         PPAPIRequestBypassedObjectSrc = 487,
         WebKitPoint = 488,
+        HTMLPreElementWidth = 489,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
