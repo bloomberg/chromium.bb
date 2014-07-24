bdfb2f238cf6660c80f2328d94db92b792af39a5
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-24 16:04:32 +0000

    Add UseCounters for non-standard WheelEvent.wheelDelta* attributes
    In ad-hoc testing neither Firefox nor IE11 support these attributes,
    making them good candidates for deprecation and eventual removal.
    
    Review URL: https://codereview.chromium.org/416103003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178849 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c54dd023660215412c59cbcb8fd7df86fde774cf

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -487,6 +487,9 @@ public:
         WebKitPoint = 488,
         HTMLPreElementWidth = 489,
         PrefixedHTMLElementDropzone = 490,
+        WheelEventWheelDeltaX = 491,
+        WheelEventWheelDeltaY = 492,
+        WheelEventWheelDelta = 493,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
