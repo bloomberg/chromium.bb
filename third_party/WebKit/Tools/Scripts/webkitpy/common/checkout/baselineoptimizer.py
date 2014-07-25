58c4d567d3e5211c0d6ef8299b49059f944fb85d
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-25 22:57:01 +0000

    Fix BaselineOptimizerTest break
    TBR=dpranke@chromium.org
    BUG=397678
    
    Review URL: https://codereview.chromium.org/421623005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@178973 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: df32e99eabd9bd17ea187cb0029e0a42945692f8

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/baselineoptimizer.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/baselineoptimizer.py
@@ -314,7 +314,7 @@ class BaselineOptimizer(object):
                 break
 
         _log.debug("Deleting redundant virtual root expected result.")
-        if self._skip_scm_commands:
+        if self._skip_scm_commands and virtual_root_expected_baseline_path in self._files_to_add:
             self._files_to_add.remove(virtual_root_expected_baseline_path)
         if self._scm.exists(virtual_root_expected_baseline_path):
             _log.debug("    Deleting (SCM): " + virtual_root_expected_baseline_path)
