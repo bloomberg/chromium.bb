0a92953e7022a400fabe0407ff8b9bc67f423970
Emil Velikov <emil.l.velikov@gmail.com>
2014-07-27 17:27:50 +0100

    libdrm,intel: Add Android build
    Contains the following patches squashed in:
    
        commit f340a8b9f2b84d5762553bef046914e0bde20795
        Author: Chad Versace <chad.versace@linux.intel.com>
        Date: Wed, 21 Dec 2011 11:43:57 -0800
    
            libdrm,intel: Add Android makefiles (v2)
    
            This enables libdrm.so and libdrm_intel.so to build on Android
            IceCreamSandwich.
    
            v2: Link libdrm_intel to libpciaccess.
    
            Change-Id: Ie5ed4bc0e6b4f9f819e3ec44488e385c35e97128
            Signed-off-by: Chad Versace <chad.versace@linux.intel.com>
    
        commit 8fb3f42389dea34218ed1fe59550ec2abb4d6953
        Author: Andrew Boie <andrew.p.boie@intel.com>
        Date: Wed, 26 Sep 2012 13:32:05 -0700
    
            libdrm, libdrm_intel: Skip driver name checks
    
            These libraries have 'optional' tags, which means they won't get
            built unless something else depends on them or they are added to
            PRODUCT_PACKAGES. There's no need for additional filtering.
    
            Change-Id: I5d90969f38671f8144c0dc27d47144b3f09a15ce
            Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

---
+++ Android.mk
@@ -0,0 +1,45 @@
+#
+# Copyright © 2011 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice (including the next
+# paragraph) shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LIBDRM_TOP := $(LOCAL_PATH)
+
+# Import variables LIBDRM_FILES, LIBDRM_HEADERS
+include $(LOCAL_PATH)/Makefile.sources
+
+LOCAL_MODULE := libdrm
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(LIBDRM_FILES)
+
+LOCAL_C_INCLUDES := \
+	$(LIBDRM_TOP)/include/drm
+
+LOCAL_CFLAGS := \
+	-DHAVE_LIBDRM_ATOMIC_PRIMITIVES=1
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(LOCAL_PATH)/intel/Android.mk
---
+++ intel/Android.mk
@@ -0,0 +1,50 @@
+#
+# Copyright © 2011 Intel Corporation
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the "Software"),
+# to deal in the Software without restriction, including without limitation
+# the rights to use, copy, modify, merge, publish, distribute, sublicense,
+# and/or sell copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice (including the next
+# paragraph) shall be included in all copies or substantial portions of the
+# Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+# Import variables LIBDRM_INTEL_FILES, LIBDRM_INTEL_HEADERS
+include $(LOCAL_PATH)/Makefile.sources
+
+LOCAL_MODULE := libdrm_intel
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SHARED_LIBRARIES := libdrm
+
+LOCAL_SRC_FILES := $(LIBDRM_INTEL_FILES)
+
+LOCAL_C_INCLUDES := \
+	$(LIBDRM_TOP) \
+	$(LIBDRM_TOP)/intel \
+	$(LIBDRM_TOP)/include/drm \
+	external/libpciaccess/include
+
+LOCAL_CFLAGS := \
+	-DHAVE_LIBDRM_ATOMIC_PRIMITIVES=1
+
+LOCAL_SHARED_LIBRARIES := \
+	libdrm \
+	libpciaccess
+
+include $(BUILD_SHARED_LIBRARY)
