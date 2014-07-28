7c499386f598c812f07beda03e9f6f95cbc6f539
mkosiba@chromium.org <mkosiba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-28 12:54:38 +0000

    [android_webview] Don't unconditionally add pak files to system image.
    The make rules we have make it impossible to not include the
    android_webview pak files in the system image.
    
    BUG=396741
    R=primiano@chromium.org
    
    Review URL: https://codereview.chromium.org/414823002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285893 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 49f9587f47446b7bba0f5636d9b4b8d50cb77e30

--- android_webview/build/resources_config.mk
+++ android_webview/build/resources_config.mk
@@ -2,6 +2,10 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# This file defines resources-related build variables which are shared by the
+# android_webview/Android.mk file and the frameworks/webview/chromium/Android.mk
+# file.
+
 android_webview_manifest_file := $(call my-dir)/AndroidManifest.xml
 
 # Resources.
@@ -18,6 +22,78 @@ android_webview_resources_dirs := \
     $(call intermediates-dir-for,GYP,ui_strings_grd)/ui_strings_grd/res_grit \
     $(call intermediates-dir-for,GYP,content_strings_grd)/content_strings_grd/res_grit
 
+android_webview_asset_dirs := \
+    $(call intermediates-dir-for,APPS,webviewchromium-paks)
+
 android_webview_aapt_flags := --auto-add-overlay
 android_webview_aapt_flags += --extra-packages org.chromium.ui
 android_webview_aapt_flags += --extra-packages org.chromium.content
+android_webview_aapt_flags += -0 pak
+
+android_webview_system_pak_targets := \
+        webviewchromium_pak \
+        webviewchromium_webkit_strings_am.pak \
+        webviewchromium_webkit_strings_ar.pak \
+        webviewchromium_webkit_strings_bg.pak \
+        webviewchromium_webkit_strings_bn.pak \
+        webviewchromium_webkit_strings_ca.pak \
+        webviewchromium_webkit_strings_cs.pak \
+        webviewchromium_webkit_strings_da.pak \
+        webviewchromium_webkit_strings_de.pak \
+        webviewchromium_webkit_strings_el.pak \
+        webviewchromium_webkit_strings_en-GB.pak \
+        webviewchromium_webkit_strings_en-US.pak \
+        webviewchromium_webkit_strings_es-419.pak \
+        webviewchromium_webkit_strings_es.pak \
+        webviewchromium_webkit_strings_et.pak \
+        webviewchromium_webkit_strings_fa.pak \
+        webviewchromium_webkit_strings_fil.pak \
+        webviewchromium_webkit_strings_fi.pak \
+        webviewchromium_webkit_strings_fr.pak \
+        webviewchromium_webkit_strings_gu.pak \
+        webviewchromium_webkit_strings_he.pak \
+        webviewchromium_webkit_strings_hi.pak \
+        webviewchromium_webkit_strings_hr.pak \
+        webviewchromium_webkit_strings_hu.pak \
+        webviewchromium_webkit_strings_id.pak \
+        webviewchromium_webkit_strings_it.pak \
+        webviewchromium_webkit_strings_ja.pak \
+        webviewchromium_webkit_strings_kn.pak \
+        webviewchromium_webkit_strings_ko.pak \
+        webviewchromium_webkit_strings_lt.pak \
+        webviewchromium_webkit_strings_lv.pak \
+        webviewchromium_webkit_strings_ml.pak \
+        webviewchromium_webkit_strings_mr.pak \
+        webviewchromium_webkit_strings_ms.pak \
+        webviewchromium_webkit_strings_nb.pak \
+        webviewchromium_webkit_strings_nl.pak \
+        webviewchromium_webkit_strings_pl.pak \
+        webviewchromium_webkit_strings_pt-BR.pak \
+        webviewchromium_webkit_strings_pt-PT.pak \
+        webviewchromium_webkit_strings_ro.pak \
+        webviewchromium_webkit_strings_ru.pak \
+        webviewchromium_webkit_strings_sk.pak \
+        webviewchromium_webkit_strings_sl.pak \
+        webviewchromium_webkit_strings_sr.pak \
+        webviewchromium_webkit_strings_sv.pak \
+        webviewchromium_webkit_strings_sw.pak \
+        webviewchromium_webkit_strings_ta.pak \
+        webviewchromium_webkit_strings_te.pak \
+        webviewchromium_webkit_strings_th.pak \
+        webviewchromium_webkit_strings_tr.pak \
+        webviewchromium_webkit_strings_uk.pak \
+        webviewchromium_webkit_strings_vi.pak \
+        webviewchromium_webkit_strings_zh-CN.pak \
+        webviewchromium_webkit_strings_zh-TW.pak
+
+android_webview_final_pak_names := \
+  $(patsubst webviewchromium_pak,webviewchromium.pak, \
+    $(patsubst webviewchromium_webkit_strings_%,%, \
+      $(android_webview_system_pak_targets)))
+
+# This list will be used to force the .pak files to be copied into the
+# intermediates folder before invoking appt from the Android 'glue layer'
+# makefile.
+android_webview_intermediates_pak_additional_deps := \
+  $(foreach name,$(android_webview_final_pak_names), \
+    $(call intermediates-dir-for,APPS,webviewchromium-paks)/$(name))
--- android_webview/webview_pak.mk
+++ android_webview/webview_pak.mk
@@ -5,9 +5,32 @@
 # This makefile fragment describes how to install a Chromium pak into the
 # Android framework for use by WebView.
 
+my_local_module := $(LOCAL_MODULE)
+my_local_module_stem := $(LOCAL_MODULE_STEM)
+my_local_built_module_stem := $(LOCAL_BUILT_MODULE_STEM)
+
+# This module is used to copy the .pak files to /system/webview/paks in the
+# system image.
 LOCAL_MODULE_CLASS := GYP
 LOCAL_MODULE_SUFFIX := .pak
 LOCAL_MODULE_PATH := $(TARGET_OUT_JAVA_LIBRARIES)/webview/paks
+
+include $(BUILD_SYSTEM)/base_rules.mk
+
+built_by_gyp := $(call intermediates-dir-for,GYP,shared)/$(LOCAL_BUILT_MODULE_STEM)
+
+$(eval $(call copy-one-file,$(built_by_gyp),$(LOCAL_BUILT_MODULE)))
+
+include $(CLEAR_VARS)
+
+# This module is used to copy the .pak files to an intermediates folder so that
+# they can be picked up by AAPT and included in an .apk.
+LOCAL_MODULE := res-$(my_local_module)
+LOCAL_MODULE_STEM := $(my_local_module_stem)
+LOCAL_BUILT_MODULE_STEM := $(my_local_built_module_stem)
+LOCAL_MODULE_CLASS := GYP
+LOCAL_MODULE_SUFFIX := .pak
+LOCAL_MODULE_PATH := $(call intermediates-dir-for,APPS,webviewchromium-paks)
 
 include $(BUILD_SYSTEM)/base_rules.mk
 
