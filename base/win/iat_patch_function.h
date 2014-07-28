08c60a41f3fdbf9b829d7ddb31b226079de8cab0
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-28 22:07:05 +0000

    Revert 285977 "Attempt to load system fonts using Custom Font in..."
    > Attempt to load system fonts using Custom Font interface of directwrite
    > instead of system font collection interface.
    > The theory is that system font collection may sometime appear to hang or just not load fonts on systems with
    > large number of fonts (in order of 1000+). With custom font collection we want to take control of font loading part and explore ways to improve it further.
    > 
    > This particular approach is loading all fonts inside renderer, we can potentially use this same code load fonts from browser side as and when required.
    > 
    > BUG=394164
    > R=cpu@chromium.org, jam@chromium.org, scottmg@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/406693003
    
    TBR=shrikant@chromium.org
    
    Review URL: https://codereview.chromium.org/423823007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@285994 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 073cc281af990556c5f80a863ed09bacbe8f916e

--- base/win/iat_patch_function.h
+++ base/win/iat_patch_function.h
@@ -59,7 +59,6 @@ class BASE_EXPORT IATPatchFunction {
 
   void* original_function() const;
 
-
  private:
   HMODULE module_handle_;
   void* intercept_function_;
@@ -68,8 +67,6 @@ class BASE_EXPORT IATPatchFunction {
 
   DISALLOW_COPY_AND_ASSIGN(IATPatchFunction);
 };
-
-BASE_EXPORT DWORD ModifyCode(void* old_code, void* new_code, int length);
 
 }  // namespace win
 }  // namespace base
