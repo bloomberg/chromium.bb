053cf2c3681a4ec712683620c46b21537d46ccae
changwan@chromium.org <changwan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-29 00:21:34 +0000

    Make ContentViewCore's SPen functions public
    BUG=397463
    
    Review URL: https://codereview.chromium.org/418313002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286039 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 70f8f2701d2b80f1318c65dc822b95005d43f974

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -112,6 +112,8 @@ public class ContentViewCore
     private static final int IS_LONG_TAP = 2;
 
     // These values are obtained from Samsung.
+    // TODO(changwan): refactor SPen related code into a separate class. See
+    // http://crbug.com/398169.
     private static final int SPEN_ACTION_DOWN = 211;
     private static final int SPEN_ACTION_UP = 212;
     private static final int SPEN_ACTION_MOVE = 213;
@@ -1122,7 +1124,12 @@ public class ContentViewCore
 
     // End FrameLayout overrides.
 
-    private static boolean isSPenSupported(Context context) {
+    /**
+     * TODO(changwan): refactor SPen related code into a separate class. See
+     * http://crbug.com/398169.
+     * @return Whether SPen is supported on the device.
+     */
+    public static boolean isSPenSupported(Context context) {
         if (sIsSPenSupported == null)
             sIsSPenSupported = detectSPenSupport(context);
         return sIsSPenSupported.booleanValue();
@@ -1141,7 +1148,16 @@ public class ContentViewCore
         return false;
     }
 
-    private static int convertSPenEventAction(int eventActionMasked) {
+    /**
+     * Convert SPen event action into normal event action.
+     * TODO(changwan): refactor SPen related code into a separate class. See
+     * http://crbug.com/398169.
+     *
+     * @param eventActionMasked Input event action. It is assumed that it is masked as the values
+                                cannot be ORed.
+     * @return Event action after the conversion
+     */
+    public static int convertSPenEventAction(int eventActionMasked) {
         // S-Pen support: convert to normal stylus event handling
         switch (eventActionMasked) {
             case SPEN_ACTION_DOWN:
