6e83e2b1acaa21c858a87d332ffeead55d2a3f02
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-29 12:36:52 +0000

    Add UseCounters for non-standard, internally unused reflected attributes
    http://whatwg.org/html#htmlhtmlelement
    http://whatwg.org/html#htmlmetaelement
    http://whatwg.org/html#htmlobjectelement
    http://whatwg.org/html#htmltablecellelement
    
    None of these attributes are used internally, so the only way sites
    can depend on them is via these bindings.
    
    Review URL: https://codereview.chromium.org/428893002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179140 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 4e06646cefe48ec0e8a6fbdd149ae9eddbe41917

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -486,6 +486,11 @@ public:
         WheelEventWheelDelta = 493,
         SendBeacon = 494,
         SendBeaconQuotaExceeded = 495,
+        HTMLHtmlElementVersion = 496,
+        HTMLMetaElementScheme = 497,
+        HTMLObjectElementStandby = 498,
+        HTMLTableCellElementChar = 499,
+        HTMLTableCellElementCharOff = 500,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
