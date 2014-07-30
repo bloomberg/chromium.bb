9a90898144c236342c8a2904f8938dd9fd4a0183
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2014-07-30 22:25:42 +0000

    Tweak copyright notices to pass presubmit checks: src/trusted/
    These presubmit failures are an annoyance and get in the way of
    contributors using the Commit Queue.
    
    BUG=none
    TEST="git cl presubmit"
    R=jvoung@chromium.org
    
    Review URL: https://codereview.chromium.org/430883002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@13565 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: da6476a58eee49e642b3cff117193ee9129c3f2e

--- src/trusted/validator_mips/dgen/dgen_opt.py
+++ src/trusted/validator_mips/dgen/dgen_opt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 #
-# Copyright 2012 The Native Client Authors.  All rights reserved.
-# Use of this source code is governed by a BSD-style license that can
-# be found in the LICENSE file.
+# Copyright 2012 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 # Copyright 2012, Google Inc.
 #
 
