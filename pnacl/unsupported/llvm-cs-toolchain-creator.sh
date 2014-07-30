4617e36f68bb5c24b9ddd8b3977869a9f053b98f
mseaborn@chromium.org <mseaborn@chromium.org@fcba33aa-ac0c-11dd-b9e7-8d5594d729c2>
2014-07-30 23:36:54 +0000

    Tweak copyright notices to pass presubmit checks: remaining files
    These presubmit failures are an annoyance and get in the way of
    contributors using the Commit Queue.
    
    BUG=none
    TEST="git cl presubmit"
    R=jvoung@chromium.org
    
    Review URL: https://codereview.chromium.org/428693003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@13568 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 2d3be97c37757cdb79871a5731d24cb6e880a795

--- pnacl/unsupported/llvm-cs-toolchain-creator.sh
+++ pnacl/unsupported/llvm-cs-toolchain-creator.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Copyright 2010 The Native Client Authors.  All rights reserved.
-# Use of this source code is governed by a BSD-style license that can
-# be found in the LICENSE file.
+# Copyright 2010 The Native Client Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 #
 #@ This script builds and llvm/cs based cross toolchain for
 #@ benchmarking puposes
