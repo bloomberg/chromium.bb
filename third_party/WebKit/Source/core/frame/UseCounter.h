6ec22c65022881e8b79a5bf97abccb55cc9fbf3e
philipj@opera.com <philipj@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-30 19:05:50 +0000

    Add UseCounters for non-standard MouseEvent IDL attributes
    Review URL: https://codereview.chromium.org/431653003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179247 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: adf99215bec8ef7d17036004e7826e0aeaa916e8

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -489,6 +489,12 @@ public:
         HTMLTableCellElementChar = 499,
         HTMLTableCellElementCharOff = 500,
         SVGSMILElementInDocument = 501,
+        MouseEventOffsetX = 502,
+        MouseEventOffsetY = 503,
+        MouseEventX = 504,
+        MouseEventY = 505,
+        MouseEventFromElement = 506,
+        MouseEventToElement = 507,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
