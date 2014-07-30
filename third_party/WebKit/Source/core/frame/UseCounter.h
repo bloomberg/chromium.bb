bd1c721d5befff7fadbd1f995d7e68a62e4cf664
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-07-30 22:45:35 +0000

    Add use counter for web file system
    This CL adds three use counters for file system to see how often the feature is
    used on the main thread, in workers, and synchronously in workers.
    
    R=esprehn@chromium.org
    
    Review URL: https://codereview.chromium.org/426343002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179264 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 10e124514f970d3e47090f5c538a2002e7af9a02

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -490,6 +490,9 @@ public:
         MouseEventY = 505,
         MouseEventFromElement = 506,
         MouseEventToElement = 507,
+        RequestFileSystem = 508,
+        RequestFileSystemWorker = 509,
+        RequestFileSystemSyncWorker = 510,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
