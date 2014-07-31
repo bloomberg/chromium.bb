48285fb30c6ec0fc131ba028aa55e8612c780f7b
erikchen@chromium.org <erikchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-07-31 02:13:02 +0000

    Revert of Fix early out in UpdateAutoplayStatus. (https://codereview.chromium.org/405253004/)
    Reason for revert:
    Dependent on a CL (https://codereview.chromium.org/373323003/) that causes a DOA on 38-dev.
    
    https://code.google.com/p/chromium/issues/detail?id=398751
    
    Original issue's description:
    > Fix early out in UpdateAutoplayStatus.
    > 
    > The intention is to disable the update when the feature is
    > disabled on the commandline for performance testing purposes.
    > 
    > BUG=390799
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=284852
    
    TBR=jam@chromium.org,tedchoc@chromium.org,chrishtr@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=390799
    
    Review URL: https://codereview.chromium.org/435513002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@286685 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: d10b2fa8bf4f078af6eb39ba60f864ca11ebef52

--- chrome/browser/android/voice_search_tab_helper.cc
+++ chrome/browser/android/voice_search_tab_helper.cc
@@ -4,11 +4,9 @@
 
 #include "chrome/browser/android/voice_search_tab_helper.h"
 
-#include "base/command_line.h"
 #include "components/google/core/browser/google_util.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/common/content_switches.h"
 #include "content/public/common/web_preferences.h"
 #include "jni/VoiceSearchTabHelper_jni.h"
 
@@ -22,24 +20,21 @@ bool RegisterVoiceSearchTabHelper(JNIEnv* env) {
 static void UpdateAutoplayStatus(JNIEnv* env,
                                  jobject obj,
                                  jobject j_web_contents) {
-  // In the case where media autoplay has been disabled by default (e.g. in
-  // performance media tests) do not update it based on navigation changes.
-  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
-  if (command_line.HasSwitch(
-      switches::kDisableGestureRequirementForMediaPlayback))
-    return;
-
   WebContents* web_contents = WebContents::FromJavaWebContents(j_web_contents);
   content::RenderViewHost* host = web_contents->GetRenderViewHost();
   content::WebPreferences prefs = host->GetWebkitPreferences();
 
-  bool gesture_required =
-      !google_util::IsGoogleSearchUrl(web_contents->GetLastCommittedURL());
+  // In the case where media autoplay has been enabled by default (e.g. in
+  // performance media tests) do not update it based on navigation changes.
+  //
+  // Note that GetWekitPreferences() is 'stateless'. It returns the default
+  // webkit preferences configuration from command line switches.
+  if (!prefs.user_gesture_required_for_media_playback)
+    return;
 
-  if (gesture_required != prefs.user_gesture_required_for_media_playback) {
-    // TODO(chrishtr): this is wrong. user_gesture_required_for_media_playback
-    // will be reset the next time a preference changes.
-    prefs.user_gesture_required_for_media_playback = gesture_required;
-    host->UpdateWebkitPreferences(prefs);
-  }
+  // TODO(chrishtr): this is wrong. user_gesture_required_for_media_playback
+  // will be reset the next time a preference changes.
+  prefs.user_gesture_required_for_media_playback =
+      !google_util::IsGoogleSearchUrl(web_contents->GetLastCommittedURL());
+  host->UpdateWebkitPreferences(prefs);
 }
