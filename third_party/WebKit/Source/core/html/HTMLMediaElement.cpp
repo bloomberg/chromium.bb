598d756d2b3070b84f75ca529c590c8c9053de6e
alancutter@chromium.org <alancutter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-01 05:58:45 +0000

    Revert of Always notify the MediaPlayer of any seek. (https://codereview.chromium.org/431903003/)
    Reason for revert:
    Test now flakily crashing on all platforms after this change:
    http/tests/media/media-source/mediasource-duration.html
    
    http://test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=http%2Ftests%2Fmedia%2Fmedia-source%2Fmediasource-duration.html
    
    Example build: http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux/builds/34873
    
    BUG=399523
    
    Original issue's description:
    > Always notify the MediaPlayer of any seek.
    > 
    > This change removes short circuit logic for seeking to the current position.
    > There are certain situations, like seeking to a truncated duration, where the
    > HTMLMediaElement is not in the best position to determine whether a call to
    > the MediaPlayer is necessary or not. This change allows the
    > MediaPlayer/WebMediaPlayer implementation to determine what the best course
    > of action should be. This also makes the seeking behavior more spec
    > compliant because even a seek to the current position should have the
    > seeking attribute be true until a stable state occurs. This is guaranteed
    > when calling into the MediaPlayer and allows all seeks to be treated the
    > same at the HTMLMediaElement level.
    > 
    > BUG=266631
    > TEST=LayoutTests/media/video-seek-to-current-position.html
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=179347
    
    TBR=acolwell@chromium.org,philipj@opera.com,srirama.m@samsung.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=266631
    
    Review URL: https://codereview.chromium.org/431273003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179368 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 7b0ea0cb1d2a525aa5b317b6b903048529396e2b

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -1937,6 +1937,7 @@ void HTMLMediaElement::seek(double time, ExceptionState& exceptionState)
 
     // 3 - Set the seeking IDL attribute to true.
     // The flag will be cleared when the engine tells us the time has actually changed.
+    bool previousSeekStillPending = m_seeking;
     m_seeking = true;
 
     // 5 - If the new playback position is later than the end of the media resource, then let it be the end
@@ -1963,16 +1964,30 @@ void HTMLMediaElement::seek(double time, ExceptionState& exceptionState)
     // attribute then set the seeking IDL attribute to false and abort these steps.
     RefPtrWillBeRawPtr<TimeRanges> seekableRanges = seekable();
 
-    if (!seekableRanges->length()) {
+    // Short circuit seeking to the current time by just firing the events if no seek is required.
+    // Don't skip calling the media engine if we are in poster mode because a seek should always
+    // cancel poster display.
+    bool noSeekRequired = !seekableRanges->length() || (time == now && displayMode() != Poster);
+
+    if (noSeekRequired) {
+        if (time == now) {
+            scheduleEvent(EventTypeNames::seeking);
+            if (previousSeekStillPending)
+                return;
+            // FIXME: There must be a stable state before timeupdate+seeked are dispatched and seeking
+            // is reset to false. See http://crbug.com/266631
+            scheduleTimeupdateEvent(false);
+            scheduleEvent(EventTypeNames::seeked);
+        }
         m_seeking = false;
         return;
     }
-
     time = seekableRanges->nearest(time);
 
-    if (m_playing && m_lastSeekTime < now)
-        addPlayedRange(m_lastSeekTime, now);
-
+    if (m_playing) {
+        if (m_lastSeekTime < now)
+            addPlayedRange(m_lastSeekTime, now);
+    }
     m_lastSeekTime = time;
     m_sentEndEvent = false;
 
