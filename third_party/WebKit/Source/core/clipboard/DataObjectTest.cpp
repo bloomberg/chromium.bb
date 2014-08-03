802aacc4766b21bc51cf2710ad2ffc1b2c0d7397
costan@gmail.com <costan@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-03 02:36:21 +0000

    Remove mocking from DataObjectTest.cpp
    This must land after the CL that adds WebBlobRegistry mocking to the
    Platform used by blink unit tests.
    CL: https://codereview.chromium.org/434833002
    
    This change takes advantage of the CL above to remove the Platform
    mocking from the DataObject unit test.
    
    BUG=398366
    
    Review URL: https://codereview.chromium.org/434823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179451 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 3a73e5eaa95417e3c9172c29a49ef41abb020e53

--- third_party/WebKit/Source/core/clipboard/DataObjectTest.cpp
+++ third_party/WebKit/Source/core/clipboard/DataObjectTest.cpp
@@ -7,146 +7,23 @@
 
 #include "core/clipboard/DataObjectItem.h"
 #include "public/platform/Platform.h"
-#include "public/platform/WebBlobRegistry.h"
-#include "public/platform/WebFileUtilities.h"
-#include "public/platform/WebMimeRegistry.h"
 #include "public/platform/WebUnitTestSupport.h"
 #include <gtest/gtest.h>
 
 namespace blink {
 
-class MockMimeRegistry : public WebMimeRegistry {
-public:
-    virtual SupportsType supportsMIMEType(const WebString& mimeType) OVERRIDE
-    {
-        return SupportsType::MayBeSupported;
-    }
-
-    virtual SupportsType supportsImageMIMEType(const WebString& mimeType) OVERRIDE
-    {
-        return SupportsType::MayBeSupported;
-    }
-
-    virtual SupportsType supportsJavaScriptMIMEType(const WebString& mimeType) OVERRIDE
-    {
-        return SupportsType::MayBeSupported;
-    }
-
-    virtual SupportsType supportsMediaMIMEType(const WebString& mimeType, const WebString& codecs, const WebString& keySystem) OVERRIDE
-    {
-        return SupportsType::MayBeSupported;
-    }
-
-    virtual bool supportsMediaSourceMIMEType(const WebString& mimeType, const WebString& codecs) OVERRIDE
-    {
-        return false;
-    }
-
-    virtual bool supportsEncryptedMediaMIMEType(const WebString& keySystem, const WebString& mimeType, const WebString& codecs) OVERRIDE
-    {
-        return false;
-    }
-
-    virtual SupportsType supportsNonImageMIMEType(const WebString& mimeType) OVERRIDE
-    {
-        return SupportsType::MayBeSupported;
-    }
-
-    virtual WebString mimeTypeForExtension(const WebString& fileExtension) OVERRIDE
-    {
-        return WebString();
-    }
-
-    virtual WebString wellKnownMimeTypeForExtension(const WebString& fileExtension) OVERRIDE
-    {
-        return WebString();
-    }
-
-    virtual WebString mimeTypeFromFile(const WebString& filePath) OVERRIDE
-    {
-        return WebString();
-    }
-};
-
-class MockFileUtilities : public WebFileUtilities {
-public:
-    ~MockFileUtilities() { }
-};
-
-class RegistryMockPlatform : public Platform {
-public:
-    RegistryMockPlatform(Platform* oldPlatform)
-        : m_oldPlatform(oldPlatform)
-    {
-    }
-
-    virtual ~RegistryMockPlatform() { }
-
-    virtual WebBlobRegistry* blobRegistry() OVERRIDE
-    {
-        return &m_mockBlobRegistry;
-    }
-
-    virtual WebMimeRegistry* mimeRegistry() OVERRIDE
-    {
-        return &m_mockMimeRegistry;
-    }
-
-    virtual WebFileUtilities* fileUtilities() OVERRIDE
-    {
-        return &m_mockFileUtilities;
-    }
-
-    virtual WebUnitTestSupport* unitTestSupport() OVERRIDE
-    {
-        return m_oldPlatform->unitTestSupport();
-    }
-
-    virtual void cryptographicallyRandomValues(unsigned char* buffer, size_t length) OVERRIDE
-    {
-        m_oldPlatform->cryptographicallyRandomValues(buffer, length);
-    }
-
-    virtual const unsigned char* getTraceCategoryEnabledFlag(const char* categoryName) OVERRIDE
-    {
-        return m_oldPlatform->getTraceCategoryEnabledFlag(categoryName);
-    }
-
-protected:
-    WebBlobRegistry m_mockBlobRegistry;
-    MockMimeRegistry m_mockMimeRegistry;
-    MockFileUtilities m_mockFileUtilities;
-    Platform* m_oldPlatform;
-};
-
 class DataObjectTest : public ::testing::Test {
 public:
-    DataObjectTest() { }
+    DataObjectTest()
+        : m_dataObject(DataObject::create())
+    {
+    }
 
 protected:
-    virtual void SetUp() OVERRIDE
-    {
-        m_oldPlatform = Platform::current();
-        m_mockPlatform = adoptPtr(new RegistryMockPlatform(m_oldPlatform));
-        Platform::initialize(m_mockPlatform.get());
-
-        m_dataObject = DataObject::create();
-    }
-    virtual void TearDown() OVERRIDE
-    {
-        // clear() invokes the File destructor, which uses WebBlobRegistry, so
-        // clear() must be called before restoring the original Platform
-        m_dataObject.clear();
-        Heap::collectAllGarbage();
-        Platform::initialize(m_oldPlatform);
-    }
-
     RefPtrWillBePersistent<DataObject> m_dataObject;
-    OwnPtr<RegistryMockPlatform> m_mockPlatform;
-    Platform* m_oldPlatform;
 };
 
-TEST_F(DataObjectTest, addItemWithFilename)
+TEST_F(DataObjectTest, addItemWithFilenameAndNoTitle)
 {
     String filePath = Platform::current()->unitTestSupport()->webKitRootDir();
     filePath.append("/Source/core/clipboard/DataObjectTest.cpp");
@@ -163,6 +40,26 @@ TEST_F(DataObjectTest, addItemWithFilename)
     EXPECT_TRUE(file->hasBackingFile());
     EXPECT_EQ(File::IsUserVisible, file->userVisibility());
     EXPECT_EQ(filePath, file->path());
+}
+
+TEST_F(DataObjectTest, addItemWithFilenameAndTitle)
+{
+    String filePath = Platform::current()->unitTestSupport()->webKitRootDir();
+    filePath.append("/Source/core/clipboard/DataObjectTest.cpp");
+
+    m_dataObject->addFilename(filePath, "name.cpp");
+    EXPECT_EQ(1U, m_dataObject->length());
+
+    RefPtrWillBeRawPtr<DataObjectItem> item = m_dataObject->item(0);
+    EXPECT_EQ(DataObjectItem::FileKind, item->kind());
+
+    RefPtrWillBeRawPtr<Blob> blob = item->getAsFile();
+    ASSERT_TRUE(blob->isFile());
+    RefPtrWillBeRawPtr<File> file = toFile(blob.get());
+    EXPECT_TRUE(file->hasBackingFile());
+    EXPECT_EQ(File::IsUserVisible, file->userVisibility());
+    EXPECT_EQ(filePath, file->path());
+    EXPECT_EQ("name.cpp", file->name());
 }
 
 } // namespace blink
