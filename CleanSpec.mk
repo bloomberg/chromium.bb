d69cc400f5c2e2148480e6252dafc542a646ae56
Emil Velikov <emil.l.velikov@gmail.com>
2014-08-04 19:49:08 +0100

    android: add CleanSpec.mk
    The file contains rules that are executed on incremental builds. This
    way one can avoid doing a full clean and ensure that the new object
    (library) is correctly build. Let's also cleanup the headers.
    
    Inspired by the work of Chih-Wei Huang, from the Android-x86 project.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

---
+++ CleanSpec.mk
@@ -0,0 +1,3 @@
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/include/libdrm)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/include/freedreno)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/obj/SHARED_LIBRARIES/libdrm_*intermediates)
