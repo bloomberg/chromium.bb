8d9429ab2954703c443840048b2cd0865decf6d8
sdefresne@chromium.org <sdefresne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-04 14:17:40 +0000

    Partially move history_types.{cc,h} to //components/history
    Move all types from //chrome/browser/history/history_types.{cc,h} that do
    not depends on content types to //components/history.
    
    BUG=371816
    TBR=msw@chromium.org
    TBR=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/439713004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287333 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2d4b32a15f37f037541b4ae6e59569ad8d1936cb

--- components/history/core/browser/page_usage_data.cc
+++ components/history/core/browser/page_usage_data.cc
@@ -1,8 +1,8 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/history/page_usage_data.h"
+#include "components/history/core/browser/page_usage_data.h"
 
 #include <algorithm>
 
