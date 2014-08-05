a7389ba4019291b7ac254e030136bb162ca68f53
noms@chromium.org <noms@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-05 00:49:23 +0000

    Bring back Gaia avatars if --new-avatar-menu is turned on.
    This should work whether or not --google-profile-info is turned on.
    
    BUG=399678
    TEST=Flip --new-avatar-menu (but not --google-profile-info). You should
    be able to choose your Gaia avatar from the manageProfile dialog.
    
    Review URL: https://codereview.chromium.org/417043008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287423 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: c8fe83871e647dec96307d40e5fcb796efd4f213

--- components/signin/core/common/profile_management_switches.cc
+++ components/signin/core/common/profile_management_switches.cc
@@ -122,8 +122,8 @@ bool IsFastUserSwitching() {
 }
 
 bool IsGoogleProfileInfo() {
-  return CheckFlag(switches::kGoogleProfileInfo,
-                   STATE_NEW_PROFILE_MANAGEMENT);
+  return IsNewAvatarMenu() ||
+      CheckFlag(switches::kGoogleProfileInfo, STATE_NONE);
 }
 
 bool IsNewAvatarMenu() {
