e3b926c9ef0c43bd7dbac59c2f5621b99fd04696
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-06 17:33:29 +0000

    Enable QUIC_VERSION_21.
    Review URL: https://codereview.chromium.org/417653003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@287798 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ee5775192969b026b40de1322c34a62409ae6ee5

--- net/http/http_network_session.cc
+++ net/http/http_network_session.cc
@@ -94,7 +94,7 @@ HttpNetworkSession::Params::Params()
       quic_max_packet_length(kDefaultMaxPacketSize),
       enable_user_alternate_protocol_ports(false),
       quic_crypto_client_stream_factory(NULL) {
-  quic_supported_versions.push_back(QUIC_VERSION_19);
+  quic_supported_versions.push_back(QUIC_VERSION_21);
 }
 
 HttpNetworkSession::Params::~Params() {}
