f79efa853c181069dae6595cb9562fcd6ef5d428
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-07 22:41:06 +0000

    Speculative crash fix in LinkStyle
    Making sure LinkStyle::loadingFrame() is not null before use.
    
    BUG=400564
    TEST=none
    R=esprehn@chromium.org
    
    Review URL: https://codereview.chromium.org/452563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179763 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6e21064411a21f8a28c1e65d459b691d346b25c3

--- third_party/WebKit/Source/core/html/HTMLLinkElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLLinkElement.cpp
@@ -691,14 +691,12 @@ void LinkStyle::process()
         m_loading = true;
 
         bool mediaQueryMatches = true;
-        if (!m_owner->media().isEmpty()) {
-            LocalFrame* frame = loadingFrame();
-            if (Document* document = loadingFrame()->document()) {
-                RefPtr<RenderStyle> documentStyle = StyleResolver::styleForDocument(*document);
-                RefPtrWillBeRawPtr<MediaQuerySet> media = MediaQuerySet::create(m_owner->media());
-                MediaQueryEvaluator evaluator(frame);
-                mediaQueryMatches = evaluator.eval(media.get());
-            }
+        LocalFrame* frame = loadingFrame();
+        if (!m_owner->media().isEmpty() && frame && frame->document()) {
+            RefPtr<RenderStyle> documentStyle = StyleResolver::styleForDocument(*frame->document());
+            RefPtrWillBeRawPtr<MediaQuerySet> media = MediaQuerySet::create(m_owner->media());
+            MediaQueryEvaluator evaluator(frame);
+            mediaQueryMatches = evaluator.eval(media.get());
         }
 
         // Don't hold up render tree construction and script execution on stylesheets
