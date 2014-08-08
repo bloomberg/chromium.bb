5954286b0a011207c08b8b15a22a9be53b73bab4
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-08 16:05:41 +0000

    DevTools: DataGrid: turn "element" getter to function.
    BUG=
    
    Review URL: https://codereview.chromium.org/449413002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179842 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 5df426e8f44a099f8756cfca1f76c5a7410d3920

--- third_party/WebKit/LayoutTests/inspector/profiler/heap-snapshot-test.js
+++ third_party/WebKit/LayoutTests/inspector/profiler/heap-snapshot-test.js
@@ -582,7 +582,7 @@ InspectorTest.columnContents = function(column, row)
     for (var node = parent.children[0]; node; node = node.traverseNextNode(true, parent, true)) {
         if (!node.selectable)
             continue;
-        var content = node.element.children[columnOrdinal];
+        var content = node.element().children[columnOrdinal];
         // Do not inlcude percents
         if (content.firstElementChild)
             content = content.firstElementChild;
