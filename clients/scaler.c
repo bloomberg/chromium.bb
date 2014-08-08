0fc3786dffe08068df9e53f7ae4dc225094b6444
Bill Spitzak <spitzak@gmail.com>
2014-08-08 13:00:00 -0700

    weston-scaler --help works even if not run inside wayland

--- clients/scaler.c
+++ clients/scaler.c
@@ -274,12 +274,6 @@ main(int argc, char *argv[])
 	struct timeval tv;
 	int i;
 
-	d = display_create(&argc, argv);
-	if (d == NULL) {
-		fprintf(stderr, "failed to create display: %m\n");
-		return -1;
-	}
-
 	box.mode = MODE_SRC_DST;
 
 	for (i = 1; i < argc; i++) {
@@ -295,6 +289,12 @@ main(int argc, char *argv[])
 			usage(argv[0]);
 			exit(1);
 		}
+	}
+
+	d = display_create(&argc, argv);
+	if (d == NULL) {
+		fprintf(stderr, "failed to create display: %m\n");
+		return -1;
 	}
 
 	gettimeofday(&tv, NULL);
