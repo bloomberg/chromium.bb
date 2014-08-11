c5b1206c722eb6b1304667bc1955937a29659129
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-11 21:49:14 +0000

    The chromevox presubmit check should only trigger for .js / .py files.
    Review URL: https://codereview.chromium.org/459573002
    
    Cr-Commit-Position: refs/heads/master@{#288802}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288802 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 0d7f3ca5b0b22f1a1566d7f9dbe7f088862b351c

--- chrome/browser/resources/chromeos/chromevox/PRESUBMIT.py
+++ chrome/browser/resources/chromeos/chromevox/PRESUBMIT.py
@@ -6,15 +6,26 @@
 
 def CheckChangeOnUpload(input_api, output_api):
   paths = input_api.AbsoluteLocalPaths()
-  def FileFilter(path):
+
+  def ShouldCheckFile(path):
+    return path.endswith('.js') or path.endswith('.py')
+
+  def ScriptFilter(path):
     return (path.endswith('check_chromevox.py') or
             path.endswith('jscompilerwrapper.py') or
             path.endswith('jsbundler.py'))
+
+  # Only care about changes to JS files or the scripts that check them.
+  paths = [p for p in paths if ShouldCheckFile(p)]
+  if not paths:
+    return []
+
   # If changing what the presubmit script uses, run the check on all
   # scripts.  Otherwise, let CheckChromeVox figure out what scripts to
   # compile, if any, based on the changed paths.
-  if any((FileFilter(p) for p in paths)):
+  if any((ScriptFilter(p) for p in paths)):
     paths = None
+
   import sys
   if not sys.platform.startswith('linux'):
     return []
