209240664f1c5eea5f6b701a34bcdde18805a351
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-11 15:17:40 +0000

    Revert of DevTools: Introduce module initializers (https://codereview.chromium.org/451713002/)
    Reason for revert:
    Breaks android_aosp bot and prevents Blink roll.
    
    http://build.chromium.org/p/tryserver.chromium.linux/builders/android_aosp/builds/4536/steps/compile/logs/stdio
    
    make: *** No rule to make target `out/target/product/generic/obj/GYP/shared_intermediates/resources/inspector/documentation/DocumentationView.js', needed by `out/target/product/generic/obj/GYP/shared_intermediates/devtools/devtools_resources.grd'.  Stop.
    
    
    Original issue's description:
    > DevTools: Introduce module initializers
    > 
    > For all lazy-loaded modules and workers, _module.js files are introduced, which
    > importScript() all scripts for a particular module.
    > 
    > This allows to:
    > - get rid of importScript() calls intertwined with meaningful code,
    > - simplify module loading, since Runtime does not need to read the
    >   module descriptor to determine the resource names.
    > 
    > This approach is similar to how we implement
    > the CSS loading in inspector.css.
    > 
    > R=pfeldman, vsevik
    > BUG=391566
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=179948
    
    TBR=pfeldman@chromium.org,vsevik@chromium.org,lushnikov@chromium.org,eustas@chromium.org,apavlov@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=391566
    
    Review URL: https://codereview.chromium.org/458753003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@179963 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: e0100fb20374e510438ff85f38dbc81b7befd2a5

--- third_party/WebKit/LayoutTests/inspector/editor/editor-test.js
+++ third_party/WebKit/LayoutTests/inspector/editor/editor-test.js
@@ -3,7 +3,7 @@ function initialize_EditorTests()
 
 InspectorTest.createTestEditor = function(clientHeight, textEditorDelegate)
 {
-    runtime.loadModule("source_frame");
+    loadScript("source_frame/SourceFrame.js");
     var textEditor = new WebInspector.CodeMirrorTextEditor("", textEditorDelegate || new WebInspector.TextEditorDelegate());
     if (clientHeight)
         textEditor.element.style.height = clientHeight + "px";
