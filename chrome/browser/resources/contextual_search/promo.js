627fb562a7fd9d1b4fbcc0c9d7486ddc4fa3de2a
mathp@chromium.org <mathp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-12 15:18:38 +0000

    [Search] Make the text swoosh away off the screen when the user taps the opt-in link.
    Previously, nothing would happen. Now there is a transition, everything slides away to the bottom of the screen and the rest is loaded.
    
    BUG=402017
    TEST=manual
    R=huangs@chromium.org, pedrosimonetti@chromium.org
    
    Review URL: https://codereview.chromium.org/456833003
    
    Cr-Commit-Position: refs/heads/master@{#288974}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288974 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: db0f0c91630189937fd7cc627c2e6766fa8783ba

--- chrome/browser/resources/contextual_search/promo.js
+++ chrome/browser/resources/contextual_search/promo.js
@@ -1,5 +1,14 @@
 /* Copyright 2014 The Chromium Authors. All rights reserved.
    Use of this source code is governed by a BSD-style license that can be
    found in the LICENSE file.
-   TODO(mathp): Add some JS here.
 */
+
+/**
+ * On load, registers the handler to add the 'hide' class to the container
+ * element in order to hide it.
+ */
+window.onload = function() {
+  $('optin-label').addEventListener('click', function() {
+    $('container').classList.add('hide');
+  });
+};
