2e0fefd9fb5ad80bf0093bd05f42e21515cfdee7
raymes@google.com <raymes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-12 00:59:59 +0000

    Update OOP PDF to use the newer version of polymer
    R=rsadam@chromium.org
    TBR=thestig@chromium.org
    
    BUG=397519
    
    Review URL: https://codereview.chromium.org/459613005
    
    Cr-Commit-Position: refs/heads/master@{#288853}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@288853 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 43ebf82a66be875f5ea4be4fcec5930bddaed8e2

--- chrome/browser/resources/pdf/includes.js
+++ chrome/browser/resources/pdf/includes.js
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-<include src="../../../../third_party/polymer_legacy/platform/platform.js">
-<include src="../../../../third_party/polymer_legacy/polymer/polymer.js">
+<include src="../../../../third_party/polymer/platform/platform.js">
+<include src="../../../../third_party/polymer/polymer/polymer.js">
 <include src="html_office/elements/viewer-toolbar/viewer-toolbar.js">
 <include src="html_office/elements/viewer-progress-bar/viewer-progress-bar.js">
 <include src="html_office/elements/viewer-password-screen/viewer-password-screen.js">
