c2fd63c7a3c315fe4a4eae17222085ac5f84ca04
Don Garrett <dgarrett@google.com>
2014-08-12 14:56:01 -0700

    licensing: Split licenses.py -> licenses_lib.py and licenses.py.
    Turn license into a stand alone script that uses a helper library to do
    it's work. Split up it's 'main' method into distinct stages.
    
    Also, fix nit's in hook script from a previous CL.
    
    BUG=None
    TEST=cros lint + manual testing.
    CQ-DEPEND=CL:212124
    
    Change-Id: Ic81d50260a6b7603904aabc38b3877ad59abb2b7
    Reviewed-on: https://chromium-review.googlesource.com/212137
    Tested-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Commit-Queue: Don Garrett <dgarrett@chromium.org>

--- licensing/ebuild_license_hook.py
+++ licensing/ebuild_license_hook.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
-#
-# Copyright (c) 2014 The Chromium OS Authors. All rights reserved.
+# Copyright 2014 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-#
+
 """Emerge hook to pre-parse and verify license information.
 
 Called from src/scripts/hooks/install/gen-package-licenses.sh as part of a
@@ -12,16 +11,16 @@ package emerge.
 
 from chromite.lib import commandline
 
-from chromite.licensing import licenses
+from chromite.licensing import licenses_lib
 
 
 def main(args):
   parser = commandline.ArgumentParser(usage=__doc__)
-  parser.add_argument("--builddir", type="path", dest="builddir",
-                      help="Take $PORTAGE_BUILDDIR as argument.")
+  parser.add_argument('--builddir', type='path', dest='builddir',
+                      help='Take $PORTAGE_BUILDDIR as argument.')
 
   opts = parser.parse_args(args)
+  opts.Freeze()
 
-
-  licensing = licenses.Licensing(None, None, True)
+  licensing = licenses_lib.Licensing(None, None, True)
   licensing.HookPackageProcess(opts.builddir)
