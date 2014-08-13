b99ed0acac47d0aa9d0897ff0e11ba76c5d6225e
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-13 20:05:00 +0000

    Revert 289111 "Revert 288954 "[Sync] Don't synchronously stop da..."
    > Revert 288954 "[Sync] Don't synchronously stop datatypes that en..."
    > 
    > Reason for revert: see crbug.com/403098
    > 
    > > [Sync] Don't synchronously stop datatypes that encounter errors
    > > 
    > > The datatype logic may be operating unders assumptions that break when
    > > the type is stopped from under it, possibly triggering crashes.
    > > 
    > > BUG=402595,403098
    > > 
    > > Review URL: https://codereview.chromium.org/462863002
    > 
    > TBR=zea@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/467923002
    
    TBR=oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/471683002
    
    Cr-Commit-Position: refs/heads/master@{#289357}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289357 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 133fcb6469e37e2cc240cf1e7060922317a6c209

--- chrome/browser/sync/glue/frontend_data_type_controller.cc
+++ chrome/browser/sync/glue/frontend_data_type_controller.cc
@@ -153,9 +153,12 @@ void FrontendDataTypeController::OnSingleDataTypeUnrecoverableError(
   if (!start_callback_.is_null()) {
     syncer::SyncMergeResult local_merge_result(type());
     local_merge_result.set_error(error);
-    start_callback_.Run(RUNTIME_ERROR,
-                        local_merge_result,
-                        syncer::SyncMergeResult(type()));
+    base::MessageLoop::current()->PostTask(
+        FROM_HERE,
+        base::Bind(start_callback_,
+                   RUNTIME_ERROR,
+                   local_merge_result,
+                   syncer::SyncMergeResult(type())));
   }
 }
 
