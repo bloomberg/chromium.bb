f578d4cd6aac8eedc6f94e32972071067dfd728b
megjablon@chromium.org <megjablon@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-14 14:43:47 +0000

    Fixing DataReductionProxy.BypassedBytes.ShortAudioVideo UMA. Currently the UMA tells us whenever audio/video is bypassed as collateral damage of a short bypass. We want to know when audio or video is the cause of a bypass so we now report to DataReductionProxy.BypassedBytes.ShortTriggeringRequestAudioVideo when audio or video triggers a bypass.
    BUG=401257
    
    Review URL: https://codereview.chromium.org/447933002
    
    Cr-Commit-Position: refs/heads/master@{#289569}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289569 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 64d97df4807ac5ec81c5226e89466cc61f2101c0

--- components/data_reduction_proxy/browser/data_reduction_proxy_usage_stats.cc
+++ components/data_reduction_proxy/browser/data_reduction_proxy_usage_stats.cc
@@ -191,17 +191,19 @@ void DataReductionProxyUsageStats::RecordBypassedBytesHistograms(
                         DataReductionProxyUsageStats::TRIGGERING_REQUEST,
                         content_length);
     triggering_request_ = false;
-  }
 
-  std::string mime_type;
-  request.GetMimeType(&mime_type);
-  // MIME types are named by <media-type>/<subtype>. We check to see if the
-  // media type is audio or video.
-  if (mime_type.compare(0, 6, "audio/") == 0  ||
-      mime_type.compare(0, 6, "video/") == 0) {
-    RecordBypassedBytes(last_bypass_type_,
-                        DataReductionProxyUsageStats::AUDIO_VIDEO,
-                        content_length);
+    // We only record when audio or video triggers a bypass. We don't care
+    // about audio and video bypassed as collateral damage.
+    std::string mime_type;
+    request.GetMimeType(&mime_type);
+    // MIME types are named by <media-type>/<subtype>. We check to see if the
+    // media type is audio or video.
+    if (mime_type.compare(0, 6, "audio/") == 0  ||
+        mime_type.compare(0, 6, "video/") == 0) {
+      RecordBypassedBytes(last_bypass_type_,
+                          DataReductionProxyUsageStats::AUDIO_VIDEO,
+                          content_length);
+    }
   }
 
   if (last_bypass_type_ != BYPASS_EVENT_TYPE_MAX) {
