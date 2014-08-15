b8aeeaef087fa2571aaaf64a8a8dbec06a0e0e41
Marc-Antoine Ruel <maruel@chromium.org>
2014-08-15 14:02:05 -0400

    Greatly improve subprocess42.
    Separate soft_timeout from hard_timeout. This will be useful for the swarming
    bot code. Improve recv_out() and recv_err().
    
    Improve the unit tests to be much faster.
    
    R=csharp@chromium.org
    BUG=
    
    Review URL: https://codereview.appspot.com/126270044

--- tests/subprocess42_test.py
+++ tests/subprocess42_test.py
@@ -3,6 +3,7 @@
 # Use of this source code is governed under the Apache License, Version 2.0 that
 # can be found in the LICENSE file.
 
+import itertools
 import logging
 import os
 import sys
@@ -16,12 +17,61 @@ from utils import subprocess42
 OUTPUT = os.path.join(ROOT_DIR, 'tests', 'subprocess42', 'output.py')
 
 
+# Disable pre-set unbuffered output to not interfere with the testing being done
+# here. Otherwise everything would test with unbuffered; which is fine but
+# that's not what we specifically want to test here.
+ENV = os.environ.copy()
+ENV.pop('PYTHONUNBUFFERED', None)
+
+
 def to_native_eol(string):
   if string is None:
     return string
   if sys.platform == 'win32':
     return string.replace('\n', '\r\n')
   return string
+
+
+def get_output_sleep_proc(flush, unbuffered, sleep_duration):
+  """Returns process with universal_newlines=True that prints to stdout before
+  after a sleep.
+
+  It also optionally sys.stdout.flush() before the sleep and optionally enable
+  unbuffered output in python.
+  """
+  command = [
+    'import sys,time',
+    'print(\'A\')',
+  ]
+  if flush:
+    # Sadly, this doesn't work otherwise in some combination.
+    command.append('sys.stdout.flush()')
+  command.extend((
+    'time.sleep(%s)' % sleep_duration,
+    'print(\'B\')',
+  ))
+  cmd = [sys.executable, '-c', ';'.join(command)]
+  if unbuffered:
+    cmd.append('-u')
+  return subprocess42.Popen(
+      cmd, env=ENV, stdout=subprocess42.PIPE, universal_newlines=True)
+
+
+def get_output_sleep_proc_err(sleep_duration):
+  """Returns process with universal_newlines=True that prints to stderr before
+  and after a sleep.
+  """
+  command = [
+    'import sys,time',
+    'sys.stderr.write(\'A\\n\')',
+  ]
+  command.extend((
+    'time.sleep(%s)' % sleep_duration,
+    'sys.stderr.write(\'B\\n\')',
+  ))
+  cmd = [sys.executable, '-c', ';'.join(command)]
+  return subprocess42.Popen(
+      cmd, env=ENV, stderr=subprocess42.PIPE, universal_newlines=True)
 
 
 class Subprocess42Test(unittest.TestCase):
@@ -71,6 +121,7 @@ class Subprocess42Test(unittest.TestCase):
     for i, (data, expected) in enumerate(test_data):
       stdout, stderr, code, duration = subprocess42.call_with_timeout(
           [sys.executable, OUTPUT] + data[0],
+          env=ENV,
           stderr=data[1],
           timeout=data[2])
       self.assertTrue(duration > 0.0001, (data, duration))
@@ -84,6 +135,7 @@ class Subprocess42Test(unittest.TestCase):
       # Try again with universal_newlines=True.
       stdout, stderr, code, duration = subprocess42.call_with_timeout(
           [sys.executable, OUTPUT] + data[0],
+          env=ENV,
           stderr=data[1],
           timeout=data[2],
           universal_newlines=True)
@@ -93,6 +145,7 @@ class Subprocess42Test(unittest.TestCase):
           (i,) + expected)
 
   def test_recv_any(self):
+    # Test all pipe direction and output scenarios.
     combinations = [
       {
         'cmd': ['out_print', 'err_print'],
@@ -158,59 +211,104 @@ class Subprocess42Test(unittest.TestCase):
         'expected': {'stdout': 'printingprinting'},
       },
     ]
-    for i, data in enumerate(combinations):
-      cmd = [sys.executable, OUTPUT] + data['cmd']
+    for i, testcase in enumerate(combinations):
+      cmd = [sys.executable, OUTPUT] + testcase['cmd']
       p = subprocess42.Popen(
-          cmd, stdout=data['stdout'], stderr=data['stderr'])
+          cmd, env=ENV, stdout=testcase['stdout'], stderr=testcase['stderr'])
       actual = {}
       while p.poll() is None:
-        pipe, d = p.recv_any()
-        if pipe is not None:
+        pipe, data = p.recv_any()
+        if data:
           actual.setdefault(pipe, '')
-          actual[pipe] += d
+          actual[pipe] += data
+
+      # The process exited, read any remaining data in the pipes.
       while True:
-        pipe, d = p.recv_any()
+        pipe, data = p.recv_any()
         if pipe is None:
           break
         actual.setdefault(pipe, '')
-        actual[pipe] += d
+        actual[pipe] += data
       self.assertEqual(
-          data['expected'], actual, (i, data['cmd'], data['expected'], actual))
+          testcase['expected'],
+          actual,
+          (i, testcase['cmd'], testcase['expected'], actual))
       self.assertEqual((None, None), p.recv_any())
       self.assertEqual(0, p.returncode)
 
-  @staticmethod
-  def _get_output_sleep_proc(flush, env, duration):
-    command = [
-      'import sys,time',
-      'print(\'A\')',
-    ]
-    if flush:
-      # Sadly, this doesn't work otherwise in some combination.
-      command.append('sys.stdout.flush()')
-    command.extend((
-      'time.sleep(%s)' % duration,
-      'print(\'B\')',
-    ))
-    return subprocess42.Popen(
-        [
-          sys.executable,
-          '-c',
-          ';'.join(command),
-        ],
-        stdout=subprocess42.PIPE,
-        universal_newlines=True,
-        env=env)
+  def test_recv_any_different_buffering(self):
+    # Specifically test all buffering scenarios.
+    for flush, unbuffered in itertools.product([True, False], [True, False]):
+      actual = ''
+      proc = get_output_sleep_proc(flush, unbuffered, 0.5)
+      while True:
+        p, data = proc.recv_any()
+        if not p:
+          break
+        self.assertEqual('stdout', p)
+        self.assertTrue(data, (p, data))
+        actual += data
 
-  def test_yield_any_None(self):
+      self.assertEqual('A\nB\n', actual)
+      # Contrary to yield_any() or recv_any(0), wait() needs to be used here.
+      proc.wait()
+      self.assertEqual(0, proc.returncode)
+
+  def test_recv_any_timeout_0(self):
+    # rec_any() is expected to timeout and return None with no data pending at
+    # least once, due to the sleep of 'duration' and the use of timeout=0.
+    for flush, unbuffered in itertools.product([True, False], [True, False]):
+      for duration in (0.05, 0.1, 0.5, 2):
+        try:
+          actual = ''
+          proc = get_output_sleep_proc(flush, unbuffered, duration)
+          got_none = False
+          while True:
+            p, data = proc.recv_any(timeout=0)
+            if not p:
+              if proc.poll() is None:
+                got_none = True
+                continue
+              break
+            self.assertEqual('stdout', p)
+            self.assertTrue(data, (p, data))
+            actual += data
+
+          self.assertEqual('A\nB\n', actual)
+          self.assertEqual(0, proc.returncode)
+          self.assertEqual(True, got_none)
+          break
+        except AssertionError:
+          if duration != 2:
+            print('Sleeping rocks. Trying slower.')
+            continue
+          raise
+
+  def _test_recv_common(self, proc, is_err):
+    actual = ''
+    while True:
+      if is_err:
+        data = proc.recv_err()
+      else:
+        data = proc.recv_out()
+      if not data:
+        break
+      self.assertTrue(data)
+      actual += data
+
+    self.assertEqual('A\nB\n', actual)
+    proc.wait()
+    self.assertEqual(0, proc.returncode)
+
+  def test_yield_any_no_timeout(self):
     for duration in (0.05, 0.1, 0.5, 2):
       try:
-        proc = self._get_output_sleep_proc(True, {}, duration)
+        proc = get_output_sleep_proc(True, True, duration)
         expected = [
           'A\n',
           'B\n',
         ]
-        for p, data in proc.yield_any(timeout=None):
+        for p, data in proc.yield_any():
           self.assertEqual('stdout', p)
           self.assertEqual(expected.pop(0), data)
         self.assertEqual(0, proc.returncode)
@@ -218,20 +316,41 @@ class Subprocess42Test(unittest.TestCase):
         break
       except AssertionError:
         if duration != 2:
-          print('Sleeping rocks. trying more slowly.')
+          print('Sleeping rocks. Trying slower.')
           continue
         raise
 
-  def test_yield_any_0(self):
+  def test_yield_any_hard_timeout(self):
+    # Kill the process due to hard_timeout.
+    proc = get_output_sleep_proc(True, True, 10)
+    got_none = False
+    actual = ''
+    for p, data in proc.yield_any(hard_timeout=1):
+      if not data:
+        got_none = True
+        continue
+      self.assertEqual('stdout', p)
+      actual += data
+    if sys.platform == 'win32':
+      self.assertEqual(1, proc.returncode)
+    else:
+      self.assertEqual(-9, proc.returncode)
+    self.assertEqual('A\n', actual)
+    # No None is returned, since it's not using soft_timeout.
+    self.assertEqual(False, got_none)
+
+  def test_yield_any_soft_timeout_0(self):
+    # rec_any() is expected to timeout and return None with no data pending at
+    # least once, due to the sleep of 'duration' and the use of timeout=0.
     for duration in (0.05, 0.1, 0.5, 2):
       try:
-        proc = self._get_output_sleep_proc(True, {}, duration)
+        proc = get_output_sleep_proc(True, True, duration)
         expected = [
           'A\n',
           'B\n',
         ]
         got_none = False
-        for p, data in proc.yield_any(timeout=0):
+        for p, data in proc.yield_any(soft_timeout=0):
           if not p:
             got_none = True
             continue
@@ -243,91 +362,14 @@ class Subprocess42Test(unittest.TestCase):
         break
       except AssertionError:
         if duration != 2:
-          print('Sleeping rocks. trying more slowly.')
+          print('Sleeping rocks. Trying slower.')
           continue
         raise
 
-  def test_recv_any_None(self):
-    values = (
-        (True, ['A\n', 'B\n'], {}),
-        (False, ['A\nB\n'], {}),
-        (False, ['A\n', 'B\n'], {'PYTHONUNBUFFERED': 'x'}),
-    )
-    for flush, exp, env in values:
-      for duration in (0.05, 0.1, 0.5, 2):
-        expected = exp[:]
-        try:
-          proc = self._get_output_sleep_proc(flush, env, duration)
-          while True:
-            p, data = proc.recv_any(timeout=None)
-            if not p:
-              break
-            self.assertEqual('stdout', p)
-            if not expected:
-              self.fail(data)
-            e = expected.pop(0)
-            if env:
-              # Buffering is truly a character-level and could get items
-              # individually. This is usually seen only during high load, try
-              # compiling at the same time to reproduce it.
-              if len(data) < len(e):
-                expected.insert(0, e[len(data):])
-                e = e[:len(data)]
-            self.assertEqual(e, data)
-          # Contrary to yield_any() or recv_any(0), wait() needs to be used
-          # here.
-          proc.wait()
-          self.assertEqual([], expected)
-          self.assertEqual(0, proc.returncode)
-        except AssertionError:
-          if duration != 2:
-            print('Sleeping rocks. trying more slowly.')
-            continue
-          raise
-
-  def test_recv_any_0(self):
-    values = (
-        (True, ['A\n', 'B\n'], {}),
-        (False, ['A\nB\n'], {}),
-        (False, ['A\n', 'B\n'], {'PYTHONUNBUFFERED': 'x'}),
-    )
-    for i, (flush, exp, env) in enumerate(values):
-      for duration in (0.1, 0.5, 2):
-        expected = exp[:]
-        try:
-          proc = self._get_output_sleep_proc(flush, env, duration)
-          got_none = False
-          while True:
-            p, data = proc.recv_any(timeout=0)
-            if not p:
-              if proc.poll() is None:
-                got_none = True
-                continue
-              break
-            self.assertEqual('stdout', p)
-            if not expected:
-              self.fail(data)
-            e = expected.pop(0)
-            if sys.platform == 'win32':
-              # Buffering is truly a character-level on Windows and could get
-              # items individually.
-              if len(data) < len(e):
-                expected.insert(0, e[len(data):])
-                e = e[:len(data)]
-            self.assertEqual(e, data)
-
-          self.assertEqual(0, proc.returncode)
-          self.assertEqual([], expected)
-          self.assertEqual(True, got_none)
-        except Exception as e:
-          if duration != 2:
-            print('Sleeping rocks. trying more slowly.')
-            continue
-          print >> sys.stderr, 'Failure at index %d' % i
-          raise
-
 
 if __name__ == '__main__':
+  if '-v' in sys.argv:
+    unittest.TestCase.maxDiff = None
   logging.basicConfig(
       level=logging.DEBUG if '-v' in sys.argv else logging.ERROR)
   unittest.main()
--- utils/subprocess42.py
+++ utils/subprocess42.py
@@ -20,6 +20,10 @@ import subprocess
 from subprocess import PIPE, STDOUT, call, check_output  # pylint: disable=W0611
 
 
+# Default maxsize argument.
+MAX_SIZE = 16384
+
+
 if subprocess.mswindows:
   import msvcrt  # pylint: disable=F0401
   from ctypes import wintypes
@@ -49,8 +53,20 @@ if subprocess.mswindows:
   def recv_multi_impl(conns, maxsize, timeout):
     """Reads from the first available pipe.
 
-    If timeout is None, it's blocking. If timeout is 0, it is not blocking.
+    It will immediately return on a closed connection, independent of timeout.
+
+    Arguments:
+    - maxsize: Maximum number of bytes to return. Defaults to MAX_SIZE.
+    - timeout: If None, it is blocking. If 0 or above, will return None if no
+          data is available within |timeout| seconds.
+
+    Returns:
+      tuple(int(index), str(data), bool(closed)).
     """
+    assert conns
+    assert timeout is None or isinstance(timeout, (int, float)), timeout
+    maxsize = max(maxsize or MAX_SIZE, 1)
+
     # TODO(maruel): Use WaitForMultipleObjects(). Python creates anonymous pipes
     # for proc.stdout and proc.stderr but they are implemented as named pipes on
     # Windows. Since named pipes are not waitable object, they can't be passed
@@ -59,27 +75,24 @@ if subprocess.mswindows:
     # object and remembering the pending ReadFile() calls. This will require
     # some re-architecture to store the relevant event handle and OVERLAPPEDIO
     # object in Popen or the file object.
-    maxsize = max(maxsize or 16384, 1)
-    if timeout:
-      start = time.time()
+    start = time.time()
     handles = [
       (i, msvcrt.get_osfhandle(c.fileno())) for i, c in enumerate(conns)
     ]
-    while handles:
+    while True:
       for index, handle in handles:
         try:
           avail = min(PeekNamedPipe(handle), maxsize)
           if avail:
-            return index, ReadFile(handle, avail)[1]
-          if (timeout and (time.time() - start) >= timeout) or timeout == 0:
-            return None, None
-          # Polling rocks.
-          time.sleep(0.001)
+            return index, ReadFile(handle, avail)[1], False
         except OSError:
-          handles.remove((index, handle))
-          break
-    # Nothing to wait for.
-    return None, None
+          # The pipe closed.
+          return index, None, True
+
+      if timeout is not None and (time.time() - start) >= timeout:
+        return None, None, False
+      # Polling rocks.
+      time.sleep(0.001)
 
 else:
   import fcntl  # pylint: disable=F0401
@@ -88,14 +101,29 @@ else:
   def recv_multi_impl(conns, maxsize, timeout):
     """Reads from the first available pipe.
 
-    If timeout is None, it's blocking. If timeout is 0, it is not blocking.
+    It will immediately return on a closed connection, independent of timeout.
+
+    Arguments:
+    - maxsize: Maximum number of bytes to return. Defaults to MAX_SIZE.
+    - timeout: If None, it is blocking. If 0 or above, will return None if no
+          data is available within |timeout| seconds.
+
+    Returns:
+      tuple(int(index), str(data), bool(closed)).
     """
+    assert conns
+    assert timeout is None or isinstance(timeout, (int, float)), timeout
+    maxsize = max(maxsize or MAX_SIZE, 1)
+
+    # select(timeout=0) will block, it has to be a value > 0.
+    if timeout == 0:
+      timeout = 0.001
     try:
       r, _, _ = select.select(conns, [], [], timeout)
     except select.error:
-      return None, None
+      r = None
     if not r:
-      return None, None
+      return None, None, False
 
     conn = r[0]
     # Temporarily make it non-blocking.
@@ -106,8 +134,11 @@ else:
       # pylint: disable=E1101
       fcntl.fcntl(conn, fcntl.F_SETFL, flags | os.O_NONBLOCK)
     try:
-      data = conn.read(max(maxsize or 16384, 1))
-      return conns.index(conn), data
+      data = conn.read(maxsize)
+      if not data:
+        # On posix, this means the channel closed.
+        return conns.index(conn), None, True
+      return conns.index(conn), data, False
     finally:
       if not conn.closed:
         fcntl.fcntl(conn, fcntl.F_SETFL, flags)
@@ -146,83 +177,136 @@ class Popen(subprocess.Popen):
       self.end = time.time()
     return ret
 
-  def yield_any(self, timeout=None):
+  def yield_any(self, maxsize=None, hard_timeout=None, soft_timeout=None):
     """Yields output until the process terminates or is killed by a timeout.
 
     Yielded values are in the form (pipename, data).
 
-    If timeout is None, it is blocking. If timeout is 0, it doesn't block. This
-    is not generally useful to use timeout=0.
+    Arguments:
+    - maxsize: See recv_any().
+    - hard_timeout: If None, the process is never killed. If set, the process is
+          killed after |hard_timeout| seconds.
+    - soft_timeout: If None, the call is blocking. If set, yields None, None
+          if no data is available within |soft_timeout| seconds. It resets
+          itself after each yield.
     """
-    remaining = 0
+    if hard_timeout is not None:
+      # hard_timeout=0 means the process is not even given a little chance to
+      # execute and will be immediately killed.
+      assert isinstance(hard_timeout, (int, float)) and hard_timeout > 0., (
+          hard_timeout)
+    if soft_timeout is not None:
+      # soft_timeout=0 effectively means that the pipe is continuously polled.
+      assert isinstance(soft_timeout, (int, float)) and soft_timeout >= 0, (
+          soft_timeout)
+
+    last_yield = time.time()
     while self.poll() is None:
-      if timeout:
-        # While these float() calls seem redundant, they are to force
-        # ResetableTimeout to "render" itself into a float. At each call, the
-        # resulting value could be different, depending if a .reset() call
-        # occurred.
-        remaining = max(float(timeout) - self.duration(), 0.001)
-      else:
-        remaining = timeout
-      t, data = self.recv_any(timeout=remaining)
-      if data or timeout == 0:
-        yield (t, data)
-      if timeout and self.duration() >= float(timeout):
+      t, data = self.recv_any(
+          maxsize=maxsize,
+          timeout=self._calc_timeout(hard_timeout, soft_timeout, last_yield))
+      if data or soft_timeout is not None:
+        yield t, data
+        last_yield = time.time()
+
+      if hard_timeout and self.duration() >= hard_timeout:
         break
-    if self.poll() is None and timeout and self.duration() >= float(timeout):
-      logging.debug('Kill %s %s', self.duration(), float(timeout))
+
+    if self.poll() is None and hard_timeout:
+      logging.debug('Kill %s %s', self.duration(), hard_timeout)
       self.kill()
     self.wait()
+
     # Read all remaining output in the pipes.
     while True:
-      t, data = self.recv_any()
+      t, data = self.recv_any(maxsize=maxsize)
       if not data:
         break
-      yield (t, data)
+      yield t, data
+
+  def _calc_timeout(self, hard_timeout, soft_timeout, last_yield):
+    """Returns the timeout to be used on the next recv_any() in yield_any().
+
+    It depends on both timeout. It returns None if no timeout is used. Otherwise
+    it returns a value >= 0.001. It's not 0 because it's effectively polling, on
+    linux it can peg a single core, so adding 1ms sleep does a tremendous
+    difference.
+    """
+    hard_remaining = (
+        max(hard_timeout - self.duration(), 0) if hard_timeout else None)
+    soft_remaining = (
+        max(soft_timeout - (time.time() - last_yield), 0)
+        if soft_timeout is not None else None)
+    if hard_remaining is None:
+      return soft_remaining
+    if soft_remaining is None:
+      return hard_remaining
+    return min(hard_remaining, soft_remaining)
 
   def recv_any(self, maxsize=None, timeout=None):
-    """Reads from stderr and if empty, from stdout.
+    """Reads from the first pipe available from stdout and stderr.
 
-    If timeout is None, it is blocking. If timeout is 0, it doesn't block.
+    Arguments:
+    - maxsize: Maximum number of bytes to return. Defaults to MAX_SIZE.
+    - timeout: If None, it is blocking. If 0 or above, will return None if no
+          data is available within |timeout| seconds.
+
+    Returns:
+      tuple(int(index) or None, str(data)).
     """
-    pipes = [
-      x for x in ((self.stderr, 'stderr'), (self.stdout, 'stdout')) if x[0]
-    ]
-    if len(pipes) == 2 and self.stderr.fileno() == self.stdout.fileno():
-      pipes.pop(0)
-    if not pipes:
-      return None, None
-    conns, names = zip(*pipes)
-    index, data = recv_multi_impl(conns, maxsize, timeout)
-    if index is None:
-      return index, data
-    if not data:
-      self._close(names[index])
-      return None, None
-    if self.universal_newlines:
-      data = self._translate_newlines(data)
-    return names[index], data
+    # recv_multi_impl will early exit on a closed connection. Loop accordingly
+    # to simplify call sites.
+    while True:
+      pipes = [
+        x for x in ((self.stderr, 'stderr'), (self.stdout, 'stdout')) if x[0]
+      ]
+      # If both stdout and stderr have the exact file handle, they are
+      # effectively the same pipe. Deduplicate it since otherwise it confuses
+      # recv_multi_impl().
+      if len(pipes) == 2 and self.stderr.fileno() == self.stdout.fileno():
+        pipes.pop(0)
+
+      if not pipes:
+        return None, None
+      start = time.time()
+      conns, names = zip(*pipes)
+      index, data, closed = recv_multi_impl(conns, maxsize, timeout)
+      if index is None:
+        return index, data
+      if closed:
+        self._close(names[index])
+        if not data:
+          # Loop again. The other pipe may still be open.
+          if timeout:
+            timeout -= (time.time() - start)
+          continue
+
+      if self.universal_newlines:
+        data = self._translate_newlines(data)
+      return names[index], data
 
   def recv_out(self, maxsize=None, timeout=None):
-    """Reads from stdout asynchronously."""
+    """Reads from stdout synchronously with timeout."""
     return self._recv('stdout', maxsize, timeout)
 
   def recv_err(self, maxsize=None, timeout=None):
-    """Reads from stderr asynchronously."""
+    """Reads from stderr synchronously with timeout."""
     return self._recv('stderr', maxsize, timeout)
 
   def _close(self, which):
+    """Closes either stdout or stderr."""
     getattr(self, which).close()
     setattr(self, which, None)
 
   def _recv(self, which, maxsize, timeout):
+    """Reads from one of stdout or stderr synchronously with timeout."""
     conn = getattr(self, which)
     if conn is None:
       return None
-    data = recv_multi_impl([conn], maxsize, timeout)
-    if not data:
-      return self._close(which)
-    if self.universal_newlines:
+    _, data, closed = recv_multi_impl([conn], maxsize, timeout)
+    if closed:
+      self._close(which)
+    if self.universal_newlines and data:
       data = self._translate_newlines(data)
     return data
 
@@ -240,7 +324,7 @@ def call_with_timeout(cmd, timeout, **kwargs):
   if timeout:
     out = ''
     err = ''
-    for t, data in proc.yield_any(timeout):
+    for t, data in proc.yield_any(hard_timeout=timeout):
       if t == 'stdout':
         out += data
       else:
