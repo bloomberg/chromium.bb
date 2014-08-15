f058d806d3c3300f5cd6df5d8763588c8b8fb617
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-15 05:35:46 +0000

    Hack to ensure that menu separators display correctly on Windows with fractional scales.
    For more context please refer to the following bug https://code.google.com/p/chromium/issues/detail?id=403955
    
    Reason for this bug is our familiar friend scaling as in views not landing on physical pixel boundaries
    when the display scale on the machine is a fractional scale like 1.25 / 1.5 etc.
    
    Hack for now is to move the separator 1 pixel down if the scale is fractional to ensure that it plays nice
    with the clip rect which is rounded up.
    
    BUG=402818
    
    Review URL: https://codereview.chromium.org/473943003
    
    Cr-Commit-Position: refs/heads/master@{#289806}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@289806 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 4a24405cf6baa09d0c6ff447d68512bcef527a5e

--- ui/views/controls/menu/menu_separator_win.cc
+++ ui/views/controls/menu/menu_separator_win.cc
@@ -10,6 +10,7 @@
 
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/rect.h"
+#include "ui/gfx/win/dpi.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/native_theme/native_theme_aura.h"
 #include "ui/views/controls/menu/menu_config.h"
@@ -41,6 +42,16 @@ void MenuSeparator::OnPaint(gfx::Canvas* canvas) {
 
   ui::NativeTheme::ExtraParams extra;
   extra.menu_separator.has_gutter = config.render_gutter;
+
+  // Hack to get the separator to display correctly on Windows where we may
+  // have fractional scales. We move the separator 1 pixel down to ensure that
+  // it falls within the clipping rect which is scaled up.
+  float device_scale = gfx::win::GetDeviceScaleFactor();
+  bool is_fractional_scale =
+      (device_scale - static_cast<int>(device_scale) != 0);
+  if (is_fractional_scale && separator_bounds.y() == 0)
+    separator_bounds.set_y(1);
+
   config.native_theme->Paint(
       canvas->sk_canvas(), ui::NativeTheme::kMenuPopupSeparator,
       ui::NativeTheme::kNormal, separator_bounds, extra);
