132ece1a92de99502ef13f57408d0333d76f27f6
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-16 05:49:25 +0000

    Display quic-connection-options passed in the command line in QUIC tab of net-internals page. connection-options also includes enable-pacing argument which is displayed as PACE.
    Fixes the rch's comments in https://codereview.chromium.org/471613002/
    
    R=rch@chromium.org, thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/470493004
    
    Cr-Commit-Position: refs/heads/master@{#290095}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290095 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ecb4dc3a3e12cb6694715cffe1255087df327750

--- net/http/http_network_session.cc
+++ net/http/http_network_session.cc
@@ -22,6 +22,7 @@
 #include "net/quic/quic_crypto_client_stream_factory.h"
 #include "net/quic/quic_protocol.h"
 #include "net/quic/quic_stream_factory.h"
+#include "net/quic/quic_utils.h"
 #include "net/socket/client_socket_factory.h"
 #include "net/socket/client_socket_pool_manager_impl.h"
 #include "net/socket/next_proto.h"
@@ -250,8 +251,13 @@ base::Value* HttpNetworkSession::QuicInfoToValue() const {
   dict->SetBoolean("quic_enabled", params_.enable_quic);
   dict->SetBoolean("enable_quic_port_selection",
                    params_.enable_quic_port_selection);
-  dict->SetBoolean("enable_quic_pacing",
-                   ContainsQuicTag(params_.quic_connection_options, kPACE));
+  base::ListValue* connection_options = new base::ListValue;
+  for (QuicTagVector::const_iterator it =
+           params_.quic_connection_options.begin();
+       it != params_.quic_connection_options.end(); ++it) {
+    connection_options->AppendString("'" + QuicUtils::TagToString(*it) + "'");
+  }
+  dict->Set("connection_options", connection_options);
   dict->SetBoolean("enable_quic_time_based_loss_detection",
                    params_.enable_quic_time_based_loss_detection);
   dict->SetString("origin_to_force_quic_on",
