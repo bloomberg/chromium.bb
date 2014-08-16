1092d7195168b1e042c35fff2b7e2f5100d054ae
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-16 16:57:01 +0000

    Don't repaint squashed, fixed position layers during scrolling
    There's no need to repaint fixed position layers that are squashed. Necessarily
    they must be squashed onto other fixed position layers, which means they don't
    move during scrolling.
    
    R=vollick@chromium.org
    
    Review URL: https://codereview.chromium.org/478823002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180410 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: e3e69aa37520b1f1e66e85f4399f02d4ccd2e7a3

--- third_party/WebKit/Source/core/frame/FrameView.cpp
+++ third_party/WebKit/Source/core/frame/FrameView.cpp
@@ -1269,17 +1269,12 @@ bool FrameView::scrollContentsFastPath(const IntSize& scrollDelta)
     ViewportConstrainedObjectSet::const_iterator end = m_viewportConstrainedObjects->end();
     for (ViewportConstrainedObjectSet::const_iterator it = m_viewportConstrainedObjects->begin(); it != end; ++it) {
         RenderObject* renderer = *it;
-        // m_viewportConstrainedObjects should not contain non-viewport constrained objects.
         ASSERT(renderer->style()->hasViewportConstrainedPosition());
-
-        // Fixed items should always have layers.
         ASSERT(renderer->hasLayer());
         RenderLayer* layer = toRenderBoxModelObject(renderer)->layer();
 
-        // Layers that paint into their ancestor or into a grouped backing will still need
-        // to apply a paint invalidation. If the layer paints into its own backing, then
-        // it does not need paint invalidation just to scroll.
-        if (layer->compositingState() == PaintsIntoOwnBacking)
+        CompositingState state = layer->compositingState();
+        if (state == PaintsIntoOwnBacking || state == PaintsIntoGroupedBacking)
             continue;
 
         if (layer->hasAncestorWithFilterOutsets()) {
