c08bb64b118eecee73aefcf6e313ccee17d4d216
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-17 20:18:53 +0000

    Revert 290179 "Revert 290152 "gpu: ContextProvider::BindToCurren..."
    This CL wasn't the cause of the flakiness. The problem still isn't solved however.
    
    BUG=404443
    
    > Revert 290152 "gpu: ContextProvider::BindToCurrentThread() shoul..."
    > 
    > Speculative revert because of flakiness on GPU bots. See Issue 404443.
    > 
    > BUG=404443 
    > 
    > > gpu: ContextProvider::BindToCurrentThread() should be called one time.
    > > 
    > > BUG=404121
    > > 
    > > Review URL: https://codereview.chromium.org/476193002
    > 
    > TBR=dongseong.hwang@intel.com
    > 
    > Review URL: https://codereview.chromium.org/482563002
    
    TBR=kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/485543002
    
    Cr-Commit-Position: refs/heads/master@{#290192}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290192 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: e06ff5885d09dbd955877b731c9cbc531b0adf5f

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -1177,10 +1177,10 @@ RenderThreadImpl::SharedMainThreadContextProvider() {
       shared_main_thread_contexts_ = ContextProviderCommandBuffer::Create(
           CreateOffscreenContext3d(), "Offscreen-MainThread");
     }
+    if (shared_main_thread_contexts_ &&
+        !shared_main_thread_contexts_->BindToCurrentThread())
+      shared_main_thread_contexts_ = NULL;
   }
-  if (shared_main_thread_contexts_ &&
-      !shared_main_thread_contexts_->BindToCurrentThread())
-    shared_main_thread_contexts_ = NULL;
   return shared_main_thread_contexts_;
 }
 
