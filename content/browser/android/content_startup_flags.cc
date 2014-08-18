05e9c789365c5d2e3a7f1c18ebb794591a7cdfd5
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-18 21:21:50 +0000

    Remove the acclerated overflow scroll and fixed root background flags.
    These flags are merged into --{dis,en}able-prefer-compositing-to-lcd-text
    which controls these along with fixed position.
    
    Depends on https://codereview.chromium.org/479653003/.
    
    R=piman@chromium.org, aelias
    BUG=365851
    
    Review URL: https://codereview.chromium.org/479673003
    
    Cr-Commit-Position: refs/heads/master@{#290364}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290364 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 97dbbbcef5b60f78bf275b5f2e60b73e590223ba

--- content/browser/android/content_startup_flags.cc
+++ content/browser/android/content_startup_flags.cc
@@ -52,7 +52,6 @@ void SetContentCommandLineFlags(bool single_process,
     parsed_command_line->AppendSwitch(switches::kSingleProcess);
   }
 
-  parsed_command_line->AppendSwitch(switches::kEnableAcceleratedOverflowScroll);
   parsed_command_line->AppendSwitch(switches::kEnableBeginFrameScheduling);
 
   parsed_command_line->AppendSwitch(switches::kEnablePinch);
