c2d0029005155cc500d9f18e05e7f824621490ec
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-18 19:30:07 +0000

    Speculative fix for atomic flag in UsbEventHandler. The write memory barrier for running_ in ~UsbEventHandler didn't have a corresponding read memory barrier anywhere and thus was useless.
    BUG=None
    
    Review URL: https://codereview.chromium.org/486613003
    
    Cr-Commit-Position: refs/heads/master@{#290329}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290329 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 2b566ac6bf0c099a63b885d4a856a1583e7eb243

--- components/usb_service/usb_context.cc
+++ components/usb_service/usb_context.cc
@@ -4,6 +4,7 @@
 
 #include "components/usb_service/usb_context.h"
 
+#include "base/atomicops.h"
 #include "base/logging.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/platform_thread.h"
@@ -25,7 +26,7 @@ class UsbContext::UsbEventHandler : public base::PlatformThread::Delegate {
   virtual void ThreadMain() OVERRIDE;
 
  private:
-  volatile bool running_;
+  base::subtle::Atomic32 running_;
   libusb_context* context_;
   base::PlatformThreadHandle thread_handle_;
   base::WaitableEvent start_polling_;
@@ -33,19 +34,17 @@ class UsbContext::UsbEventHandler : public base::PlatformThread::Delegate {
 };
 
 UsbContext::UsbEventHandler::UsbEventHandler(libusb_context* context)
-    : running_(true),
-      context_(context),
+    : context_(context),
       thread_handle_(0),
       start_polling_(false, false) {
+  base::subtle::Release_Store(&running_, 1);
   bool success = base::PlatformThread::Create(0, this, &thread_handle_);
   DCHECK(success) << "Failed to create USB IO handling thread.";
   start_polling_.Wait();
 }
 
 UsbContext::UsbEventHandler::~UsbEventHandler() {
-  running_ = false;
-  // Spreading running_ to the UsbEventHandler thread.
-  base::subtle::MemoryBarrier();
+  base::subtle::Release_Store(&running_, 0);
   libusb_interrupt_handle_event(context_);
   base::PlatformThread::Join(thread_handle_);
 }
@@ -53,10 +52,11 @@ UsbContext::UsbEventHandler::~UsbEventHandler() {
 void UsbContext::UsbEventHandler::ThreadMain() {
   base::PlatformThread::SetName("UsbEventHandler");
   VLOG(1) << "UsbEventHandler started.";
-  if (running_) {
+
+  if (base::subtle::Acquire_Load(&running_)) {
     start_polling_.Signal();
   }
-  while (running_) {
+  while (base::subtle::Acquire_Load(&running_)) {
     const int rv = libusb_handle_events(context_);
     if (rv != LIBUSB_SUCCESS) {
       VLOG(1) << "Failed to handle events: " << ConvertErrorToString(rv);
