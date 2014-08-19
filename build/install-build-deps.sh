b32f7d4fd795b82730593c330761faa1f56fe75f
mmoss@chromium.org <mmoss@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-19 18:31:00 +0000

    Add git-svn dependency.
    This is needed by committers for repos that are still svn-based.
    
    R=iannucci@chromium.org
    BUG=366926
    
    Review URL: https://codereview.chromium.org/489573002
    
    Cr-Commit-Position: refs/heads/master@{#290620}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290620 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 48d96e931b8df7c9c107261ba2439e95c7c91cd8

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -95,15 +95,15 @@ chromeos_dev_list="libbluetooth-dev"
 
 # Packages needed for development
 dev_list="apache2.2-bin bison curl dpkg-dev elfutils devscripts fakeroot flex
-          fonts-thai-tlwg g++ git-core gperf language-pack-da language-pack-fr
-          language-pack-he language-pack-zh-hant libapache2-mod-php5
-          libasound2-dev libbrlapi-dev libav-tools libbz2-dev libcairo2-dev
-          libcap-dev libcups2-dev libcurl4-gnutls-dev libdrm-dev libelf-dev
-          libexif-dev libgconf2-dev libgl1-mesa-dev libglib2.0-dev
-          libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev libkrb5-dev
-          libnspr4-dev libnss3-dev libpam0g-dev libpci-dev libpulse-dev
-          libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev libudev-dev
-          libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
+          fonts-thai-tlwg g++ git-core git-svn gperf language-pack-da
+          language-pack-fr language-pack-he language-pack-zh-hant
+          libapache2-mod-php5 libasound2-dev libbrlapi-dev libav-tools
+          libbz2-dev libcairo2-dev libcap-dev libcups2-dev libcurl4-gnutls-dev
+          libdrm-dev libelf-dev libexif-dev libgconf2-dev libgl1-mesa-dev
+          libglib2.0-dev libglu1-mesa-dev libgnome-keyring-dev libgtk2.0-dev
+          libkrb5-dev libnspr4-dev libnss3-dev libpam0g-dev libpci-dev
+          libpulse-dev libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev
+          libudev-dev libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
           mesa-common-dev openbox patch perl php5-cgi pkg-config python
           python-cherrypy3 python-dev python-psutil rpm ruby subversion
           ttf-dejavu-core ttf-indic-fonts ttf-kochi-gothic ttf-kochi-mincho
