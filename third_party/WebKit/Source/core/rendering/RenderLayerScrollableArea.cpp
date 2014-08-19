14af176a9ec891f50c2fccc6335da25c9b0c75fd
vollick@chromium.org <vollick@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2014-08-19 21:02:50 +0000

    Avoid double scrollbars when the HTML element is not the render view
    The code in RenderLayerScrollableArea for updating after a style change
    was slightly wrong. It attempted to avoid creating extra scrollbars for
    the viewport but doesn't quite handle the html/body duality correctly.
    
    The code is convoluted, but the scrollbars of the FrameView are
    determined by the scrollbar modes (in addition to the document size,
    obviously), and these modes are computed in
    FrameView::calculateScrollbarModesForLayoutAndSetViewportRenderer
    based on the "viewport defining element." The code in
    RenderLayerScrollableArea was a crude approximation of viewport
    defining element. I've changed the code to make this more explicit.
    
    BUG=374156
    
    Review URL: https://codereview.chromium.org/451063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@180600 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 1e52b0833e542f917e9b9fea2aa9c17c6920191a

--- third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerScrollableArea.cpp
@@ -46,6 +46,7 @@
 
 #include "core/accessibility/AXObjectCache.h"
 #include "core/css/PseudoStyleRequest.h"
+#include "core/dom/Node.h"
 #include "core/dom/shadow/ShadowRoot.h"
 #include "core/editing/FrameSelection.h"
 #include "core/frame/FrameView.h"
@@ -694,10 +695,19 @@ static bool overflowDefinesAutomaticScrollbar(EOverflow overflow)
     return overflow == OAUTO || overflow == OOVERLAY;
 }
 
+// This function returns true if the given box requires overflow scrollbars (as
+// opposed to the 'viewport' scrollbars managed by the RenderLayerCompositor).
+// FIXME: we should use the same scrolling machinery for both the viewport and
+// overflow. Currently, we need to avoid producing scrollbars here if they'll be
+// handled externally in the RLC.
+static bool canHaveOverflowScrollbars(const RenderBox& box)
+{
+    return !box.isRenderView() && box.document().viewportDefiningElement() != box.node();
+}
+
 void RenderLayerScrollableArea::updateAfterStyleChange(const RenderStyle* oldStyle)
 {
-    // RenderView shouldn't provide scrollbars on its own.
-    if (box().isRenderView())
+    if (!canHaveOverflowScrollbars(box()))
         return;
 
     if (!m_scrollDimensionsDirty)
