118bb3796b04ebf0137ebd7431bfcef0b2d436ce
hanxi@chromium.org <hanxi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-21 18:10:51 +0000

    Introduce WebViewGuestDelegate.
    BUG=352290
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=291002
    
    Review URL: https://codereview.chromium.org/481003002
    
    Cr-Commit-Position: refs/heads/master@{#291133}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291133 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 871125e6c47a814dcba53b01f5033a3a06565f48

---
+++ chrome/browser/guest_view/web_view/chrome_web_view_guest_delegate.h
@@ -0,0 +1,85 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_GUEST_VIEW_WEB_VIEW_CHROME_WEB_VIEW_GUEST_DELEGATE_H_
+#define CHROME_BROWSER_GUEST_VIEW_WEB_VIEW_CHROME_WEB_VIEW_GUEST_DELEGATE_H_
+
+#include "chrome/browser/guest_view/web_view/web_view_guest.h"
+#include "extensions/browser/guest_view/web_view/web_view_guest_delegate.h"
+
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/chromeos/accessibility/accessibility_manager.h"
+#endif
+
+class RenderViewContextMenu;
+
+namespace ui {
+class SimpleMenuModel;
+}  // namespace ui
+
+class ChromeWebViewGuestDelegate : public extensions::WebViewGuestDelegate {
+ public :
+  explicit ChromeWebViewGuestDelegate(
+      extensions::WebViewGuest* web_view_guest);
+  virtual ~ChromeWebViewGuestDelegate();
+
+  // WebViewGuestDelegate implementation.
+  virtual double GetZoom() OVERRIDE;
+  virtual bool HandleContextMenu(
+      const content::ContextMenuParams& params) OVERRIDE;
+  virtual void OnAttachWebViewHelpers(content::WebContents* contents) OVERRIDE;
+  virtual void OnDidCommitProvisionalLoadForFrame(bool is_main_frame) OVERRIDE;
+  virtual void OnDidInitialize() OVERRIDE;
+  virtual void OnDocumentLoadedInFrame(
+      content::RenderFrameHost* render_frame_host) OVERRIDE;
+  virtual void OnGuestDestroyed() OVERRIDE;
+  virtual void OnSetZoom(double zoom_factor) OVERRIDE;
+  virtual void OnShowContextMenu(
+      int request_id,
+      const MenuItemVector* items) OVERRIDE;
+
+ private:
+  content::WebContents* guest_web_contents() const {
+    return web_view_guest_->guest_web_contents();
+  }
+
+  // Returns the top level items (ignoring submenus) as Value.
+  static scoped_ptr<base::ListValue> MenuModelToValue(
+      const ui::SimpleMenuModel& menu_model);
+
+  void InjectChromeVoxIfNeeded(content::RenderViewHost* render_view_host);
+
+#if defined(OS_CHROMEOS)
+  // Notification of a change in the state of an accessibility setting.
+  void OnAccessibilityStatusChanged(
+      const chromeos::AccessibilityStatusEventDetails& details);
+#endif
+
+  // A counter to generate a unique request id for a context menu request.
+  // We only need the ids to be unique for a given WebViewGuest.
+  int pending_context_menu_request_id_;
+
+  // Set to |true| if ChromeVox was already injected in main frame.
+  bool chromevox_injected_;
+
+  // Stores the current zoom factor.
+  double current_zoom_factor_;
+
+  // Holds the RenderViewContextMenu that has been built but yet to be
+  // shown. This is .Reset() after ShowContextMenu().
+  scoped_ptr<RenderViewContextMenu> pending_menu_;
+
+  extensions::WebViewGuest* web_view_guest_;
+
+#if defined(OS_CHROMEOS)
+  // Subscription to receive notifications on changes to a11y settings.
+  scoped_ptr<chromeos::AccessibilityStatusSubscription>
+      accessibility_subscription_;
+#endif
+
+  DISALLOW_COPY_AND_ASSIGN(ChromeWebViewGuestDelegate);
+};
+
+#endif  // CHROME_BROWSER_GUEST_VIEW_WEB_VIEW_CHROME_WEB_VIEW_GUEST_DELEGATE_H_
+
---
+++ extensions/browser/guest_view/web_view/web_view_guest_delegate.h
@@ -0,0 +1,72 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EXTENSIONS_BROWSER_GUEST_VIEW_WEB_VIEW_WEB_VIEW_GUEST_DELEGATE_H_
+#define EXTENSIONS_BROWSER_GUEST_VIEW_WEB_VIEW_WEB_VIEW_GUEST_DELEGATE_H_
+
+#include "extensions/browser/guest_view/guest_view_base.h"
+
+namespace content {
+class RenderViewHost;
+class WebContents;
+}
+
+namespace extensions {
+
+namespace api {
+namespace web_view_internal{
+
+struct ContextMenuItem;
+
+}  // namespace web_view_internal
+}  // namespace api
+
+// A delegate class of WebViewGuest that are not a part of chrome.
+class WebViewGuestDelegate {
+ public :
+  WebViewGuestDelegate();
+  virtual ~WebViewGuestDelegate();
+
+  typedef std::vector<linked_ptr<api::web_view_internal::ContextMenuItem> >
+      MenuItemVector;
+
+  // Returns the current zoom factor.
+  virtual double GetZoom() = 0;
+
+  // Called when context menu operation was handled.
+  virtual bool HandleContextMenu(const content::ContextMenuParams& params) = 0;
+
+  // Called to attach helpers just after additional initialization is performed.
+  virtual void OnAttachWebViewHelpers(content::WebContents* contents) = 0;
+
+  // Called when the guest WebContents commits a provisional load in any frame.
+  virtual void OnDidCommitProvisionalLoadForFrame(bool is_main_frame) = 0;
+
+  // Called just after additional initialization is performed.
+  virtual void OnDidInitialize() = 0;
+
+  virtual void OnDocumentLoadedInFrame(
+      content::RenderFrameHost* render_frame_host) = 0;
+
+  // Called immediately after the guest WebContents has been destroyed.
+  virtual void OnGuestDestroyed() = 0;
+
+  // Called when to set the zoom factor.
+  virtual void OnSetZoom(double zoom_factor) = 0;
+
+  // Shows the context menu for the guest.
+  // |items| acts as a filter. This restricts the current context's default
+  // menu items to contain only the items from |items|.
+  // |items| == NULL means no filtering will be applied.
+  virtual void OnShowContextMenu(
+      int request_id,
+      const MenuItemVector* items) = 0;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(WebViewGuestDelegate);
+};
+
+}  // namespace extensions
+
+#endif  // EXTENSIONS_BROWSER_GUEST_VIEW_WEB_VIEW_WEB_VIEW_GUEST_DELEGATE_H_
