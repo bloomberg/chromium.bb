81890f1e5176f2a613bbfa56141c44743d6538ac
ernstm@chromium.org <ernstm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-21 00:10:51 +0000

    Revert 290845 "Forward input and compositing tasks to the Blink ..."
    Speculative revert. Probably broke various smoothness benchmarks on Mac and Win.
    
    > Forward input and compositing tasks to the Blink scheduler
    > 
    > Instead of posting input and compositor tasks directly to the 
    > main thread message loop, forward them to the Blink scheduler 
    > so they can be prioritized over other tasks.
    > 
    > BUG=391005
    > 
    > Review URL: https://codereview.chromium.org/363383002
    
    TBR=skyostil@chromium.org
    
    Review URL: https://codereview.chromium.org/488273004
    
    Cr-Commit-Position: refs/heads/master@{#290957}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@290957 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: 3b430ba157c011c3e2a2eda39d8831d98f1b639c

--- content/renderer/input/input_event_filter.h
+++ content/renderer/input/input_event_filter.h
@@ -18,7 +18,6 @@
 
 namespace base {
 class MessageLoopProxy;
-class SingleThreadTaskRunner;
 }
 
 namespace IPC {
@@ -39,10 +38,8 @@ namespace content {
 class CONTENT_EXPORT InputEventFilter : public InputHandlerManagerClient,
                                         public IPC::MessageFilter {
  public:
-  InputEventFilter(
-      IPC::Listener* main_listener,
-      const scoped_refptr<base::SingleThreadTaskRunner>& main_task_runner,
-      const scoped_refptr<base::MessageLoopProxy>& target_loop);
+  InputEventFilter(IPC::Listener* main_listener,
+                   const scoped_refptr<base::MessageLoopProxy>& target_loop);
 
   // The |handler| is invoked on the thread associated with |target_loop| to
   // handle input events matching the filtered routes.
@@ -76,7 +73,7 @@ class CONTENT_EXPORT InputEventFilter : public InputHandlerManagerClient,
   void SendMessage(scoped_ptr<IPC::Message> message);
   void SendMessageOnIOThread(scoped_ptr<IPC::Message> message);
 
-  scoped_refptr<base::SingleThreadTaskRunner> main_task_runner_;
+  scoped_refptr<base::MessageLoopProxy> main_loop_;
   IPC::Listener* main_listener_;
 
   // The sender_ only gets invoked on the thread corresponding to io_loop_.
