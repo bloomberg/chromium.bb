fe0dcb1c7b60fb8c092d0e412e86eaed3563f6da
amistry@chromium.org <amistry@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2014-08-22 00:58:32 +0000

    Whitelist the new hotwording component extension for media capture.
    Stolen from https://codereview.chromium.org/118373003
    
    BUG=397019
    
    Review URL: https://codereview.chromium.org/488353004
    
    Cr-Commit-Position: refs/heads/master@{#291283}
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@291283 0039d316-1c4b-4281-b951-d872f2087c98
    
    Former-commit-id: ae0ec2528bc6987342cbc6ffd937ed0dcfc8f553

--- chrome/browser/media/media_capture_devices_dispatcher.cc
+++ chrome/browser/media/media_capture_devices_dispatcher.cc
@@ -100,6 +100,7 @@ const content::MediaStreamDevice* FindDeviceWithId(
 // 3. Flutter gesture recognition extension.
 // 4. TODO(smus): Airbender experiment 1.
 // 5. TODO(smus): Airbender experiment 2.
+// 6. Hotwording component extension.
 // Once http://crbug.com/292856 is fixed, remove this whitelist.
 bool IsMediaRequestWhitelistedForExtension(
     const extensions::Extension* extension) {
@@ -107,7 +108,8 @@ bool IsMediaRequestWhitelistedForExtension(
       extension->id() == "bepbmhgboaologfdajaanbcjmnhjmhfn" ||
       extension->id() == "jokbpnebhdcladagohdnfgjcpejggllo" ||
       extension->id() == "clffjmdilanldobdnedchkdbofoimcgb" ||
-      extension->id() == "nnckehldicaciogcbchegobnafnjkcne";
+      extension->id() == "nnckehldicaciogcbchegobnafnjkcne" ||
+      extension->id() == "nbpagnldghgfoolbancepceaanlmhfmd";
 }
 
 bool IsBuiltInExtension(const GURL& origin) {
