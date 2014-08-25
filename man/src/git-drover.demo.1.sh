a2e54c17a59a28ee82b8ebba7b30eba5a37c36c3
mmoss@chromium.org <mmoss@chromium.org>
2014-08-25 23:58:16 +0000

    Add some more context to git-drover tutorial.
    BUG=407284
    R=iannucci@google.com
    
    Review URL: https://codereview.chromium.org/507513002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@291619 0039d316-1c4b-4281-b951-d872f2087c98

--- man/src/git-drover.demo.1.sh
+++ man/src/git-drover.demo.1.sh
@@ -18,11 +18,25 @@ comment Before working with branches, you must \'gclient sync \
 set_user branch.maintainer
 tick 1000
 
+echo "# Make sure we have the most up-to-date branch sources."
+run git fetch
+echo
+echo "# Here's the commit we want to 'drover'."
 run git log -n 1 --pretty=fuller
+echo
+echo "# Checkout the branch we want to 'drover' to."
 run git checkout -b drover_9999 branch-heads/9999
-echo "# DO NOT leave off the '-x' flag"
+echo
+echo "# Now do the 'drover'."
+echo "# IMPORTANT!!! Do Not leave off the '-x' flag"
 run git cherry-pick -x $(git show-ref -s pick_commit)
+echo
+echo "# That took the code authored by some.commiter and commited it to the"
+echo "# branch by branch.maintainer (us)."
 run git log -n 1 --pretty=fuller
+echo
+echo "# Looks good. Ship it!"
 pcommand git cl upload
 echo "# Get LGTM or TBR."
 run git cl land
+echo "# Or skip the LGTM/TBR and just 'git cl land --bypass-hooks'"
