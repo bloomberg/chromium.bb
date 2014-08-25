bloomberg/chromium.bb

commit cfb608559d0a95d7d5206af025669a1e99f8fb80

Report local errors to the server directly via ereporter2 service.
This permits having better coverage on the client behavior. While a side-channel
(off server) would be better, this is already a good information channel. Since
ereporter2 is already a reusable component, it'd be a trivial change to hard
code the server url to something else. It'll be improved as a follow up as
needed. As such, there's a 'v=1' parameter meaning it's version 1, for
futureproofness.

Add a fair amount of unit test to ensure it works in various conditions.

R=vadimsh@chromium.org
BUG=swarming:119

Review URL: https://codereview.appspot.com/102530045

utils/net.py
@@ -230,22 +230,30 @@ def split_server_request_url(url):
   return urlhost, urlpath
 
 
-def get_http_service(urlhost):
+def get_http_service(urlhost, allow_cached=False, use_count_key=None):
   """Returns existing or creates new instance of HttpService that can send
   requests to given base urlhost.
   """
+  def new_service():
+    return HttpService(
+        urlhost,
+        engine=RequestsLibEngine(get_cacerts_bundle()),
+        authenticator=create_authenticator(urlhost),
+        use_count_key=use_count_key)
+
   # Ensure consistency in url naming.
   urlhost = str(urlhost).lower().rstrip('/')
+
   # Do not use COUNT_KEY with Google Storage (since it breaks a signature).
-  use_count_key = not GS_STORAGE_HOST_URL_RE.match(urlhost)
+  if use_count_key is None:
+    use_count_key = not GS_STORAGE_HOST_URL_RE.match(urlhost)
+
+  if not allow_cached:
+    return new_service()
   with _http_services_lock:
     service = _http_services.get(urlhost)
     if not service:
-      service = HttpService(
-          urlhost,
-          engine=RequestsLibEngine(get_cacerts_bundle()),
-          authenticator=create_authenticator(urlhost),
-          use_count_key=use_count_key)
+      service = new_service()
       _http_services[urlhost] = service
     return service
 

auth.py
@@ -5,7 +5,7 @@
 
 """Client tool to perform various authentication related tasks."""
 
-__version__ = '0.3'
+__version__ = '0.3.1'
 
 import optparse
 import sys
@@ -14,6 +14,7 @@ from third_party import colorama
 from third_party.depot_tools import fix_encoding
 from third_party.depot_tools import subcommand
 
+from utils import on_error
 from utils import net
 from utils import oauth
 from utils import tools
@@ -132,16 +133,13 @@ class OptionParserAuth(tools.OptionParserWithLogging):
     options.service = options.service.rstrip('/')
     if not options.service:
       self.error('--service is required.')
+    on_error.report_on_exception_exit(options.service)
     return options, args
 
 
 def main(args):
   dispatcher = subcommand.CommandDispatcher(__name__)
-  try:
-    return dispatcher.execute(OptionParserAuth(version=__version__), args)
-  except Exception as e:
-    tools.report_error(e)
-    return 1
+  return dispatcher.execute(OptionParserAuth(version=__version__), args)
 
 
 if __name__ == '__main__':

isolate.py
@@ -13,6 +13,8 @@ See more information at
 """
 # Run ./isolate.py --help for more detailed information.
 
+__version__ = '0.3.2'
+
 import datetime
 import logging
 import optparse
@@ -33,10 +35,8 @@ from third_party.depot_tools import fix_encoding
 from third_party.depot_tools import subcommand
 
 from utils import file_path
+from utils import on_error
 from utils import tools
-
-
-__version__ = '0.3.1'
 
 
 class ExecutionError(Exception):
@@ -1047,6 +1047,7 @@ def CMDarchive(parser, args):
   isolateserver.process_isolate_server_options(parser, options)
   if args:
     parser.error('Unsupported argument: %s' % args)
+  on_error.report_on_exception_exit(options.isolate_server)
   cwd = os.getcwd()
   with tools.Profiler('GenerateHashtable'):
     success = False
@@ -1512,11 +1513,7 @@ class OptionParserIsolate(tools.OptionParserWithLogging):
 
 def main(argv):
   dispatcher = subcommand.CommandDispatcher(__name__)
-  try:
-    return dispatcher.execute(OptionParserIsolate(version=__version__), argv)
-  except Exception as e:
-    tools.report_error(e)
-    return 1
+  return dispatcher.execute(OptionParserIsolate(version=__version__), argv)
 
 
 if __name__ == '__main__':

isolateserver.py
@@ -5,7 +5,7 @@
 
 """Archives a set of files or directories to a server."""
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 
 import functools
 import hashlib
@@ -29,6 +29,7 @@ from third_party.depot_tools import subcommand
 
 from utils import file_path
 from utils import net
+from utils import on_error
 from utils import threading_utils
 from utils import tools
 
@@ -2337,6 +2338,7 @@ def process_isolate_server_options(parser, options):
       new[2] = ''
     new[2] = new[2].rstrip('/')
     options.isolate_server = urlparse.urlunparse(new)
+    on_error.report_on_exception_exit(options.isolate_server)
     return
 
   if file_path.is_url(options.indir):
@@ -2392,11 +2394,7 @@ class OptionParserIsolateServer(tools.OptionParserWithLogging):
 
 def main(args):
   dispatcher = subcommand.CommandDispatcher(__name__)
-  try:
-    return dispatcher.execute(OptionParserIsolateServer(), args)
-  except Exception as e:
-    tools.report_error(e)
-    return 1
+  return dispatcher.execute(OptionParserIsolateServer(), args)
 
 
 if __name__ == '__main__':

run_isolated.py
@@ -14,7 +14,7 @@ file. All content written to this directory will be uploaded upon termination
 and the .isolated file describing this directory will be printed to stdout.
 """
 
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 import ctypes
 import logging
@@ -31,6 +31,7 @@ import time
 from third_party.depot_tools import fix_encoding
 
 from utils import lru
+from utils import on_error
 from utils import threading_utils
 from utils import tools
 from utils import zip_package
@@ -674,8 +675,8 @@ def run_tha_test(isolated_hash, storage, cache, extra_args):
           cache=cache,
           outdir=run_dir,
           require_command=True)
-    except isolateserver.ConfigError as e:
-      tools.report_error(e)
+    except isolateserver.ConfigError:
+      on_error.report(None)
       return 1
 
     change_tree_read_only(run_dir, settings.read_only)
@@ -704,8 +705,8 @@ def run_tha_test(isolated_hash, storage, cache, extra_args):
         logging.info(
             'Command finished with exit code %d (%s)',
             result, hex(0xffffffff & result))
-    except OSError as e:
-      tools.report_error('Failed to run %s; cwd=%s: %s' % (command, cwd, e))
+    except OSError:
+      on_error.report('Failed to run %s; cwd=%s' % (command, cwd))
       result = 1
 
   finally:
@@ -815,21 +816,15 @@ def main(args):
   policies = CachePolicies(
       options.max_cache_size, options.min_free_space, options.max_items)
 
-  try:
-    # |options.cache| path may not exist until DiskCache() instance is created.
-    cache = DiskCache(
-        options.cache, policies, isolateserver.get_hash_algo(options.namespace))
-    remote = options.isolate_server or options.indir
-    with isolateserver.get_storage(remote, options.namespace) as storage:
-      # Hashing schemes used by |storage| and |cache| MUST match.
-      assert storage.hash_algo == cache.hash_algo
-      return run_tha_test(
-          options.isolated or options.hash, storage, cache, args)
-  except Exception as e:
-    # Make sure any exception is logged.
-    tools.report_error(e)
-    logging.exception(e)
-    return 1
+  # |options.cache| path may not exist until DiskCache() instance is created.
+  cache = DiskCache(
+      options.cache, policies, isolateserver.get_hash_algo(options.namespace))
+  remote = options.isolate_server or options.indir
+  with isolateserver.get_storage(remote, options.namespace) as storage:
+    # Hashing schemes used by |storage| and |cache| MUST match.
+    assert storage.hash_algo == cache.hash_algo
+    return run_tha_test(
+        options.isolated or options.hash, storage, cache, args)
 
 
 if __name__ == '__main__':

swarming.py
@@ -5,7 +5,7 @@
 
 """Client tool to trigger tasks or retrieve results from a Swarming server."""
 
-__version__ = '0.4.10'
+__version__ = '0.4.11'
 
 import datetime
 import getpass
@@ -28,6 +28,7 @@ from third_party.depot_tools import subcommand
 from utils import file_path
 from third_party.chromium import natsort
 from utils import net
+from utils import on_error
 from utils import threading_utils
 from utils import tools
 from utils import zip_package
@@ -555,8 +556,7 @@ def upload_zip_bundle(isolate_server, bundle):
     bundle: instance of ZipPackage to upload.
 
   Returns:
-    URL to get the file from on success.
-    None on failure.
+    URL to get the file from.
   """
   # Swarming bot would need to be able to grab the file from the storage
   # using raw HTTP GET. Use 'default' namespace so that the raw data returned
@@ -564,17 +564,13 @@ def upload_zip_bundle(isolate_server, bundle):
   # data yet. This namespace have nothing to do with |namespace| passed to
   # run_isolated.py that is used to store files for isolated task.
   logging.info('Zipping up and uploading files...')
-  try:
-    start_time = now()
-    isolate_item = isolateserver.BufferItem(
-        bundle.zip_into_buffer(), high_priority=True)
-    with isolateserver.get_storage(isolate_server, 'default') as storage:
-      uploaded = storage.upload_items([isolate_item])
-      bundle_url = storage.get_fetch_url(isolate_item)
-    elapsed = now() - start_time
-  except (IOError, OSError) as exc:
-    tools.report_error('Failed to upload the zip file: %s' % exc)
-    return None
+  start_time = now()
+  isolate_item = isolateserver.BufferItem(
+      bundle.zip_into_buffer(), high_priority=True)
+  with isolateserver.get_storage(isolate_server, 'default') as storage:
+    uploaded = storage.upload_items([isolate_item])
+    bundle_url = storage.get_fetch_url(isolate_item)
+  elapsed = now() - start_time
   if isolate_item in uploaded:
     logging.info('Upload complete, time elapsed: %f', elapsed)
   else:
@@ -596,17 +592,16 @@ def trigger_by_manifest(swarming, manifest):
   manifest_text = manifest.to_json()
   result = net.url_read(swarming + '/test', data={'request': manifest_text})
   if not result:
-    tools.report_error('Failed to trigger task %s' % manifest.task_name)
+    on_error.report('Failed to trigger task %s' % manifest.task_name)
     return None
   try:
     data = json.loads(result)
-  except (ValueError, TypeError) as e:
+  except (ValueError, TypeError):
     msg = '\n'.join((
         'Failed to trigger task %s' % manifest.task_name,
         'Manifest: %s' % manifest_text,
-        'Bad response: %s' % result,
-        str(e)))
-    tools.report_error(msg)
+        'Bad response: %s' % result))
+    on_error.report(msg)
     return None, None
   if not data:
     return None, None
@@ -653,8 +648,10 @@ def trigger_task_shards(
     manifests.append(manifest)
 
   # Upload zip bundle file to get its URL.
-  bundle_url = upload_zip_bundle(isolate_server, bundle)
-  if not bundle_url:
+  try:
+    bundle_url = upload_zip_bundle(isolate_server, bundle)
+  except (IOError, OSError):
+    on_error.report('Failed to upload the zip file for task %s' % task_name)
     return None, None
 
   # Attach that file to all manifests.
@@ -697,13 +694,13 @@ def isolated_to_hash(isolate_server, namespace, arg, algo, verbose):
   if arg.endswith('.isolated'):
     file_hash = archive(isolate_server, namespace, arg, algo, verbose)
     if not file_hash:
-      tools.report_error('Archival failure %s' % arg)
+      on_error.report('Archival failure %s' % arg)
       return None, True
     return file_hash, True
   elif isolateserver.is_valid_hash(arg, algo):
     return arg, False
   else:
-    tools.report_error('Invalid hash %s' % arg)
+    on_error.report('Invalid hash %s' % arg)
     return None, False
 
 
@@ -959,8 +956,8 @@ def CMDcollect(parser, args):
         options.decorate,
         options.print_status_updates,
         options.task_output_dir)
-  except Failure as e:
-    tools.report_error(e)
+  except Failure:
+    on_error.report(None)
     return 1
 
 
@@ -1048,12 +1045,12 @@ def CMDrun(parser, args):
         profile=options.profile,
         priority=options.priority)
   except Failure as e:
-    tools.report_error(
+    on_error.report(
         'Failed to trigger %s(%s): %s' %
         (options.task_name, args[0], e.args[0]))
     return 1
   if not tasks:
-    tools.report_error('Failed to trigger the task.')
+    on_error.report('Failed to trigger the task.')
     return 1
   if task_name != options.task_name:
     print('Triggered task: %s' % task_name)
@@ -1067,8 +1064,8 @@ def CMDrun(parser, args):
         options.decorate,
         options.print_status_updates,
         options.task_output_dir)
-  except Failure as e:
-    tools.report_error(e)
+  except Failure:
+    on_error.report(None)
     return 1
 
 
@@ -1120,8 +1117,8 @@ def CMDtrigger(parser, args):
         }
         tools.write_json(options.dump_json, data, True)
     return int(not tasks)
-  except Failure as e:
-    tools.report_error(e)
+  except Failure:
+    on_error.report(None)
     return 1
 
 
@@ -1149,11 +1146,7 @@ class OptionParserSwarming(tools.OptionParserWithLogging):
 
 def main(args):
   dispatcher = subcommand.CommandDispatcher(__name__)
-  try:
-    return dispatcher.execute(OptionParserSwarming(version=__version__), args)
-  except Exception as e:
-    tools.report_error(e)
-    return 1
+  return dispatcher.execute(OptionParserSwarming(version=__version__), args)
 
 
 if __name__ == '__main__':

tests/on_error_test.py
@@ -1,0 +1,418 @@
+#!/usr/bin/env python
+# Copyright 2014 The Swarming Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0 that
+# can be found in the LICENSE file.
+
+import BaseHTTPServer
+import atexit
+import cgi
+import getpass
+import json
+import logging
+import os
+import platform
+import re
+import socket
+import ssl
+import subprocess
+import sys
+import threading
+import unittest
+import urllib
+import urlparse
+
+TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(TESTS_DIR)
+sys.path.insert(0, ROOT_DIR)
+sys.path.insert(0, os.path.join(ROOT_DIR, 'third_party'))
+
+from depot_tools import auto_stub
+from utils import on_error
+
+
+PEM = os.path.join(TESTS_DIR, 'self_signed.pem')
+
+
+# Access to a protected member XXX of a client class - pylint: disable=W0212
+
+
+def _serialize_env():
+  return dict(
+      (unicode(k), unicode(v.encode('ascii', 'replace')))
+      for k, v in os.environ.iteritems())
+
+
+class HttpsServer(BaseHTTPServer.HTTPServer):
+  def __init__(self, addr, cls, hostname, pem):
+    BaseHTTPServer.HTTPServer.__init__(self, addr, cls)
+    self.hostname = hostname
+    self.pem = pem
+    self.socket = ssl.wrap_socket(
+        self.socket,
+        server_side=True,
+        certfile=self.pem)
+    self.keep_running = True
+    self.requests = []
+    self._thread = None
+
+  @property
+  def url(self):
+    return 'https://%s:%d' % (self.hostname, self.server_address[1])
+
+  def start(self):
+    assert not self._thread
+
+    def _server_loop():
+      while self.keep_running:
+        self.handle_request()
+
+    self._thread = threading.Thread(name='http', target=_server_loop)
+    self._thread.daemon = True
+    self._thread.start()
+
+    while True:
+      # Ensures it is up.
+      try:
+        urllib.urlopen(self.url + '/_warmup').read()
+      except IOError:
+        continue
+      return
+
+  def stop(self):
+    self.keep_running = False
+    urllib.urlopen(self.url + '/_quit').read()
+    self._thread.join()
+    self._thread = None
+
+  def register_call(self, request):
+    if request.path not in ('/_quit', '/_warmup'):
+      self.requests.append((request.path, request.parse_POST()))
+
+
+class Handler(BaseHTTPServer.BaseHTTPRequestHandler):
+  def log_message(self, fmt, *args):
+    logging.debug(
+        '%s - - [%s] %s',
+        self.address_string(), self.log_date_time_string(), fmt % args)
+
+  def parse_POST(self):
+    ctype, pdict = cgi.parse_header(self.headers['Content-Type'])
+    if ctype == 'multipart/form-data':
+      return cgi.parse_multipart(self.rfile, pdict)
+    if ctype == 'application/x-www-form-urlencoded':
+      length = int(self.headers['Content-Length'])
+      return urlparse.parse_qs(self.rfile.read(length), keep_blank_values=1)
+    if ctype in ('application/json', 'application/json; charset=utf-8'):
+      length = int(self.headers['Content-Length'])
+      return json.loads(self.rfile.read(length))
+    assert False, ctype
+
+  def do_GET(self):
+    self.server.register_call(self)
+    self.send_response(200)
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    self.wfile.write('Rock on')
+
+  def do_POST(self):
+    self.server.register_call(self)
+    self.send_response(200)
+    self.send_header('Content-type', 'application/json; charset=utf-8')
+    self.end_headers()
+    data = {
+      'id': '1234',
+      'url': 'https://localhost/error/1234',
+    }
+    self.wfile.write(json.dumps(data))
+
+
+def start_server():
+  """Starts an HTTPS web server and returns the port bound."""
+  # A premade passwordless self-signed certificate. It works because urllib
+  # doesn't verify the certificate validity.
+  httpd = HttpsServer(('127.0.0.1', 0), Handler, 'localhost', pem=PEM)
+  httpd.start()
+  return httpd
+
+
+class OnErrorBase(auto_stub.TestCase):
+  HOSTNAME = socket.getfqdn()
+
+  def setUp(self):
+    super(OnErrorBase, self).setUp()
+    os.chdir(TESTS_DIR)
+    self._atexit = []
+    self.mock(atexit, 'register', self._atexit.append)
+    self.mock(on_error, '_ENABLED_DOMAINS', (self.HOSTNAME,))
+    self.mock(on_error, '_HOSTNAME', None)
+    self.mock(on_error, '_SERVER', None)
+
+
+class OnErrorTest(OnErrorBase):
+  def test_report(self):
+    url = 'https://localhost/'
+    on_error.report_on_exception_exit(url)
+    self.assertEqual([on_error._check_for_exception_on_exit], self._atexit)
+    self.assertEqual('https://localhost', on_error._SERVER.urlhost)
+    self.assertEqual(self.HOSTNAME, on_error._HOSTNAME)
+    with self.assertRaises(ValueError):
+      on_error.report_on_exception_exit(url)
+
+  def test_no_http(self):
+    # http:// url are denied.
+    url = 'http://localhost/'
+    self.assertIs(False, on_error.report_on_exception_exit(url))
+    self.assertEqual([], self._atexit)
+
+
+class OnErrorServerTest(OnErrorBase):
+  def call(self, url, arg, returncode):
+    cmd = [sys.executable, 'on_error_test.py', 'run_shell_out', url, arg]
+    proc = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, env=os.environ)
+    out = proc.communicate()[0]
+    logging.debug('\n%s', out)
+    self.assertEqual(returncode, proc.returncode)
+    return out
+
+  def one_request(self, httpd):
+    self.assertEqual(1, len(httpd.requests))
+    resource, params = httpd.requests[0]
+    self.assertEqual('/ereporter2/api/v1/on_error', resource)
+    self.assertEqual(['r', 'v'], params.keys())
+    self.assertEqual('1', params['v'])
+    return params['r']
+
+  def test_shell_out_normal(self):
+    # Rerun itself, report an error, ensure the error was reported.
+    httpd = start_server()
+    out = self.call(httpd.url, 'normal', 1)
+    self.assertEqual([], httpd.requests)
+    self.assertEqual('Failure to register the handler\n', out)
+    httpd.stop()
+
+  def test_shell_out_hacked(self):
+    # Rerun itself, report an error, ensure the error was reported.
+    httpd = start_server()
+    out = self.call(httpd.url, 'hacked', 0)
+    self.assertEqual([], httpd.requests)
+    self.assertEqual('', out)
+    httpd.stop()
+
+  def test_shell_out_report(self):
+    # Rerun itself, report an error manually, ensure the error was reported.
+    httpd = start_server()
+    out = self.call(httpd.url, 'report', 0)
+    expected = (
+        'Sending the report ... done.\n'
+        'Report URL: https://localhost/error/1234\n'
+        'Oh dang\n')
+    self.assertEqual(expected, out)
+
+    actual = self.one_request(httpd)
+    self.assertGreater(actual.pop('duration'), 0.000001)
+    expected = {
+      u'args': [
+        u'on_error_test.py', u'run_shell_out', unicode(httpd.url), u'report',
+      ],
+      u'category': u'report',
+      u'cwd': unicode(os.getcwd()),
+      u'env': _serialize_env(),
+      u'hostname': unicode(socket.getfqdn()),
+      u'message': u'Oh dang',
+      u'os': unicode(sys.platform),
+      u'python_version': unicode(platform.python_version()),
+      u'source': u'on_error_test.py',
+      u'user': unicode(getpass.getuser()),
+      # The version was added dynamically for testing purpose.
+      u'version': u'123',
+    }
+    self.assertEqual(expected, actual)
+    httpd.stop()
+
+  def test_shell_out_exception(self):
+    # Rerun itself, report an exception manually, ensure the error was reported.
+    httpd = start_server()
+    out = self.call(httpd.url, 'exception', 0)
+    expected = (
+        'Sending the crash report ... done.\n'
+        'Report URL: https://localhost/error/1234\n'
+        'Really\nYou are not my type\n')
+    self.assertEqual(expected, out)
+
+    actual = self.one_request(httpd)
+    self.assertGreater(actual.pop('duration'), 0.000001)
+    # Remove numbers so editing the code doesn't invalidate the expectation.
+    actual['stack'] = re.sub(r' \d+', ' 0', actual['stack'])
+    expected = {
+      u'args': [
+        u'on_error_test.py', u'run_shell_out', unicode(httpd.url), u'exception',
+      ],
+      u'cwd': unicode(os.getcwd()),
+      u'category': u'exception',
+      u'env': _serialize_env(),
+      u'exception_type': u'TypeError',
+      u'hostname': unicode(socket.getfqdn()),
+      u'message': u'Really\nYou are not my type',
+      u'os': unicode(sys.platform),
+      u'python_version': unicode(platform.python_version()),
+      u'source': u'on_error_test.py',
+      u'stack':
+        u'File "on_error_test.py", line 0, in run_shell_out\n'
+        u'  raise TypeError(\'You are not my type\')',
+      u'user': unicode(getpass.getuser()),
+    }
+    self.assertEqual(expected, actual)
+    httpd.stop()
+
+  def test_shell_out_exception_no_msg(self):
+    # Rerun itself, report an exception manually, ensure the error was reported.
+    httpd = start_server()
+    out = self.call(httpd.url, 'exception_no_msg', 0)
+    expected = (
+        'Sending the crash report ... done.\n'
+        'Report URL: https://localhost/error/1234\n'
+        'You are not my type #2\n')
+    self.assertEqual(expected, out)
+
+    actual = self.one_request(httpd)
+    self.assertGreater(actual.pop('duration'), 0.000001)
+    # Remove numbers so editing the code doesn't invalidate the expectation.
+    actual['stack'] = re.sub(r' \d+', ' 0', actual['stack'])
+    expected = {
+      u'args': [
+        u'on_error_test.py', u'run_shell_out', unicode(httpd.url),
+        u'exception_no_msg',
+      ],
+      u'category': u'exception',
+      u'cwd': unicode(os.getcwd()),
+      u'env': _serialize_env(),
+      u'exception_type': u'TypeError',
+      u'hostname': unicode(socket.getfqdn()),
+      u'message': u'You are not my type #2',
+      u'os': unicode(sys.platform),
+      u'python_version': unicode(platform.python_version()),
+      u'source': u'on_error_test.py',
+      u'stack':
+        u'File "on_error_test.py", line 0, in run_shell_out\n'
+        u'  raise TypeError(\'You are not my type #2\')',
+      u'user': unicode(getpass.getuser()),
+    }
+    self.assertEqual(expected, actual)
+    httpd.stop()
+
+  def test_shell_out_crash(self):
+    # Rerun itself, report an error with a crash, ensure the error was reported.
+    httpd = start_server()
+    out = self.call(httpd.url, 'crash', 1)
+    expected = (
+        'Traceback (most recent call last):\n'
+        '  File "on_error_test.py", line 0, in <module>\n'
+        '    sys.exit(run_shell_out(sys.argv[2], sys.argv[3]))\n'
+        '  File "on_error_test.py", line 0, in run_shell_out\n'
+        '    raise ValueError(\'Oops\')\n'
+        'ValueError: Oops\n'
+        'Sending the crash report ... done.\n'
+        'Report URL: https://localhost/error/1234\n'
+        'Process exited due to exception\n'
+        'Oops\n')
+    # Remove numbers so editing the code doesn't invalidate the expectation.
+    self.assertEqual(expected, re.sub(r' \d+', ' 0', out))
+
+    actual = self.one_request(httpd)
+    # Remove numbers so editing the code doesn't invalidate the expectation.
+    actual['stack'] = re.sub(r' \d+', ' 0', actual['stack'])
+    self.assertGreater(actual.pop('duration'), 0.000001)
+    expected = {
+      u'args': [
+        u'on_error_test.py', u'run_shell_out', unicode(httpd.url), u'crash',
+      ],
+      u'category': u'exception',
+      u'cwd': unicode(os.getcwd()),
+      u'env': _serialize_env(),
+      u'exception_type': u'ValueError',
+      u'hostname': unicode(socket.getfqdn()),
+      u'message': u'Process exited due to exception\nOops',
+      u'os': unicode(sys.platform),
+      u'python_version': unicode(platform.python_version()),
+      u'source': u'on_error_test.py',
+      # The stack trace is stripped off the heading and absolute paths.
+      u'stack':
+        u'File "on_error_test.py", line 0, in <module>\n'
+        u'  sys.exit(run_shell_out(sys.argv[2], sys.argv[3]))\n'
+        u'File "on_error_test.py", line 0, in run_shell_out\n'
+        u'  raise ValueError(\'Oops\')',
+      u'user': unicode(getpass.getuser()),
+    }
+    self.assertEqual(expected, actual)
+    httpd.stop()
+
+  def test_shell_out_crash_server_down(self):
+    # Rerun itself, report an error, ensure the error was reported.
+    out = self.call('https://localhost:1', 'crash', 1)
+    expected = (
+        'Traceback (most recent call last):\n'
+        '  File "on_error_test.py", line 0, in <module>\n'
+        '    sys.exit(run_shell_out(sys.argv[2], sys.argv[3]))\n'
+        '  File "on_error_test.py", line 0, in run_shell_out\n'
+        '    raise ValueError(\'Oops\')\n'
+        'ValueError: Oops\n'
+        'Sending the crash report ... failed!\n'
+        'Process exited due to exception\n'
+        'Oops\n')
+    # Remove numbers so editing the code doesn't invalidate the expectation.
+    self.assertEqual(expected, re.sub(r' \d+', ' 0', out))
+
+
+def run_shell_out(url, mode):
+  if mode != 'normal':
+    # Hack it out so registering works.
+    on_error._ENABLED_DOMAINS = (socket.getfqdn(),)
+
+  if not on_error.report_on_exception_exit(url):
+    print 'Failure to register the handler'
+    return 1
+
+  # Hack out certificate verification because we are using a self-signed
+  # certificate here. In practice, the SSL certificate is signed to guard
+  # against MITM attacks.
+  on_error._SERVER.engine.session.verify = False
+
+  if mode == 'crash':
+    # Sadly, net is a bit overly verbose, which breaks
+    # test_shell_out_crash_server_down.
+    logging.error = lambda *_, **_kwargs: None
+    logging.warning = lambda *_, **_kwargs: None
+    raise ValueError('Oops')
+
+  if mode == 'report':
+    # Generate a manual report without an exception frame. Also set the version
+    # value.
+    setattr(sys.modules['__main__'], '__version__', '123')
+    on_error.report('Oh dang')
+
+  if mode == 'exception':
+    # Report from inside an exception frame.
+    try:
+      raise TypeError('You are not my type')
+    except TypeError:
+      on_error.report('Really')
+
+  if mode == 'exception_no_msg':
+    # Report from inside an exception frame.
+    try:
+      raise TypeError('You are not my type #2')
+    except TypeError:
+      on_error.report(None)
+  return 0
+
+
+if __name__ == '__main__':
+  if len(sys.argv) == 4 and sys.argv[1] == 'run_shell_out':
+    sys.exit(run_shell_out(sys.argv[2], sys.argv[3]))
+
+  if '-v' in sys.argv:
+    unittest.TestCase.maxDiff = None
+  logging.basicConfig(
+      level=logging.DEBUG if '-v' in sys.argv else logging.ERROR)
+  unittest.main()

commit 46266fecb56eec23ab441dd883f29f014c05263c

Make net.url_open reuse connections again.
The regression was introduced in cfb60855.

R=maruel@chromium.org
BUG=

Review URL: https://codereview.appspot.com/134740043

utils/net.py
@@ -245,7 +245,7 @@ def split_server_request_url(url):
   return urlhost, urlpath
 
 
-def get_http_service(urlhost, allow_cached=False, use_count_key=None):
+def get_http_service(urlhost, allow_cached=True, use_count_key=None):
   """Returns existing or creates new instance of HttpService that can send
   requests to given base urlhost.
   """