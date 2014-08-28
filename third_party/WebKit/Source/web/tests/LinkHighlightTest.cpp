8d693b84211252a7844c76630da67e85a471afb6
zeeshanq@chromium.org <zeeshanq@chromium.org>
2014-08-28 03:46:00 +0000

    Reduce hit test on ShowPress by moving hit testing to WebViewImpl.
    Previously there was a hit test for link highlighting in WebViewImpl and then a                                          
    separate one in EventHandler::handleGestureEvent. This CL makes it so that a                                             
    single hit test in WebViewImpl is reused by the EventHandler. Scroll events                                              
    still need to hit test in a separate fashion though and their handling has been                                          
    refactored into a separate code path.
    
    BUG=381728
    
    Review URL: https://codereview.chromium.org/490783003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181009 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0f1441e34a9dd7ab957ede38010dd7facbb87d4a

--- third_party/WebKit/Source/web/tests/LinkHighlightTest.cpp
+++ third_party/WebKit/Source/web/tests/LinkHighlightTest.cpp
@@ -29,6 +29,8 @@
 #include "bindings/core/v8/ExceptionStatePlaceholder.h"
 #include "core/dom/Node.h"
 #include "core/frame/FrameView.h"
+#include "core/page/EventHandler.h"
+#include "core/page/Page.h"
 #include "core/page/TouchDisambiguation.h"
 #include "core/testing/URLTestHelpers.h"
 #include "platform/geometry/IntRect.h"
@@ -52,6 +54,12 @@ using namespace blink;
 
 namespace {
 
+GestureEventWithHitTestResults getTargetedEvent(WebViewImpl* webViewImpl, WebGestureEvent& touchEvent)
+{
+    PlatformGestureEventBuilder platformEvent(webViewImpl->mainFrameImpl()->frameView(), touchEvent);
+    return webViewImpl->page()->deprecatedLocalMainFrame()->eventHandler().targetGestureEvent(platformEvent, true);
+}
+
 TEST(LinkHighlightTest, verifyWebViewImplIntegration)
 {
     const std::string baseURL("http://www.test.com/");
@@ -72,53 +80,31 @@ TEST(LinkHighlightTest, verifyWebViewImplIntegration)
     touchEvent.x = 20;
     touchEvent.y = 20;
 
-    {
-        PlatformGestureEventBuilder platformEvent(webViewImpl->mainFrameImpl()->frameView(), touchEvent);
-        Node* touchNode = webViewImpl->bestTapNode(platformEvent);
-        ASSERT_TRUE(touchNode);
-    }
+    ASSERT_TRUE(webViewImpl->bestTapNode(getTargetedEvent(webViewImpl, touchEvent)));
 
     touchEvent.y = 40;
-    {
-        PlatformGestureEventBuilder platformEvent(webViewImpl->mainFrameImpl()->frameView(), touchEvent);
-        EXPECT_FALSE(webViewImpl->bestTapNode(platformEvent));
-    }
+    EXPECT_FALSE(webViewImpl->bestTapNode(getTargetedEvent(webViewImpl, touchEvent)));
 
     touchEvent.y = 20;
     // Shouldn't crash.
-
-    {
-        PlatformGestureEventBuilder platformEvent(webViewImpl->mainFrameImpl()->frameView(), touchEvent);
-        webViewImpl->enableTapHighlightAtPoint(platformEvent);
-    }
+    webViewImpl->enableTapHighlightAtPoint(getTargetedEvent(webViewImpl, touchEvent));
 
     EXPECT_TRUE(webViewImpl->linkHighlight(0));
     EXPECT_TRUE(webViewImpl->linkHighlight(0)->contentLayer());
     EXPECT_TRUE(webViewImpl->linkHighlight(0)->clipLayer());
 
     // Find a target inside a scrollable div
-
     touchEvent.y = 100;
-    {
-        PlatformGestureEventBuilder platformEvent(webViewImpl->mainFrameImpl()->frameView(), touchEvent);
-        webViewImpl->enableTapHighlightAtPoint(platformEvent);
-    }
-
+    webViewImpl->enableTapHighlightAtPoint(getTargetedEvent(webViewImpl, touchEvent));
     ASSERT_TRUE(webViewImpl->linkHighlight(0));
 
     // Don't highlight if no "hand cursor"
     touchEvent.y = 220; // An A-link with cross-hair cursor.
-    {
-        PlatformGestureEventBuilder platformEvent(webViewImpl->mainFrameImpl()->frameView(), touchEvent);
-        webViewImpl->enableTapHighlightAtPoint(platformEvent);
-    }
+    webViewImpl->enableTapHighlightAtPoint(getTargetedEvent(webViewImpl, touchEvent));
     ASSERT_EQ(0U, webViewImpl->numLinkHighlights());
 
     touchEvent.y = 260; // A text input box.
-    {
-        PlatformGestureEventBuilder platformEvent(webViewImpl->mainFrameImpl()->frameView(), touchEvent);
-        webViewImpl->enableTapHighlightAtPoint(platformEvent);
-    }
+    webViewImpl->enableTapHighlightAtPoint(getTargetedEvent(webViewImpl, touchEvent));
     ASSERT_EQ(0U, webViewImpl->numLinkHighlights());
 
     Platform::current()->unitTestSupport()->unregisterAllMockedURLs();
@@ -158,11 +144,11 @@ TEST(LinkHighlightTest, resetDuringNodeRemoval)
     touchEvent.x = 20;
     touchEvent.y = 20;
 
-    PlatformGestureEventBuilder platformEvent(webViewImpl->mainFrameImpl()->frameView(), touchEvent);
-    Node* touchNode = webViewImpl->bestTapNode(platformEvent);
+    GestureEventWithHitTestResults targetedEvent = getTargetedEvent(webViewImpl, touchEvent);
+    Node* touchNode = webViewImpl->bestTapNode(targetedEvent);
     ASSERT_TRUE(touchNode);
 
-    webViewImpl->enableTapHighlightAtPoint(platformEvent);
+    webViewImpl->enableTapHighlightAtPoint(targetedEvent);
     ASSERT_TRUE(webViewImpl->linkHighlight(0));
 
     GraphicsLayer* highlightLayer = webViewImpl->linkHighlight(0)->currentGraphicsLayerForTesting();
