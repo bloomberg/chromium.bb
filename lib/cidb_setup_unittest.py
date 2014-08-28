9838291db5bc279a1351b5cf4913b2de63e05165
Mike Frysinger <vapier@chromium.org>
2014-08-28 02:30:33 -0400

    cidb: convert unittest to cros_test_lib
    The unittest when written didn't have a way of clearing its setup and
    so couldn't use cros_test_lib.  But that's been fixed, so we can cut
    it over to our common testing framework.
    
    BUG=chromium:407472
    TEST=`./run_tests` passes
    TEST=`./run_tests --network` passes
    
    Change-Id: I1632ea9ea122b575939e981eaa807e3d53aba30b
    Reviewed-on: https://chromium-review.googlesource.com/214692
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>

--- lib/cidb_setup_unittest.py
+++ lib/cidb_setup_unittest.py
@@ -3,40 +3,26 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Unit tests for cidb.py Setup methods.
+"""Unit tests for cidb.py Setup methods."""
 
-These unit tests require that cros_test_lib is NOT impoted (importing
-cros_test_lib
-are in a dedicated module, separate from any other
-unit tests which may be added in future to cidb_unittest. That is because
-these tests require us NOT to import cros_test_lib
-"""
-
-
-import mox
 import os
 import sys
-import unittest
 
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(
     os.path.abspath(__file__)))))
 
 from chromite.cbuildbot import constants
 from chromite.lib import cidb
+from chromite.lib import cros_test_lib
 
-class CIDBConnectionFactoryTest(unittest.TestCase):
+class CIDBConnectionFactoryTest(cros_test_lib.MoxTestCase):
   """Test that CIDBConnectionFactory behaves as expected."""
 
   def setUp(self):
-    self.mox = mox.Mox()
     # Ensure that we do not create any live connections in this unit test.
     self.mox.StubOutWithMock(cidb, 'CIDBConnection')
     # pylint: disable-msg=W0212
     cidb.CIDBConnectionFactory._ClearCIDBSetup()
-
-  def tearDown(self):
-    self.mox.UnsetStubs()
-    self.mox.VerifyAll()
 
   def testGetConnectionBeforeSetup(self):
     """Calling GetConnection before Setup should raise exception."""
@@ -121,4 +107,4 @@ class CIDBConnectionFactoryTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-  unittest.main()
+  cros_test_lib.main()
