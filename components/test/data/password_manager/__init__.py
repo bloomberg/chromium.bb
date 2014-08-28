4e1035b362bb190e04b3847fe41cc08b92954682
rchtara <rchtara@chromium.org>
2014-08-28 03:13:13 -0700

    Make the password manager python tests a module. Add __init__.py file to the password manager python tests to make it a module so that it could be imported from python files in the parent folder. BUG= 408198
    Review URL: https://codereview.chromium.org/511943002
    
    Cr-Commit-Position: refs/heads/master@{#292357}
    
    Former-commit-id: 24dcd28998eee1d37c67ccb680a654a9dc8e341d

---
+++ components/test/data/password_manager/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2014 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""A package to hold all the files for the Chrome password manager python tests.
+"""
