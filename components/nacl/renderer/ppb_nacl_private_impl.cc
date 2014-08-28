785c89880ee8aa01f3553c8dd37bf3b071031844
jvoung <jvoung@chromium.org>
2014-08-28 14:46:52 -0700

    Set RequestContextObject for PNaCl pexe fetches, to fix On-Demand update.
    Add more checks to test to detect that the resource type of
    the URL request is indeed OBJECT.
    Would like to get this merged to the M38 branch.
    
    BUG=401755
    
    Review URL: https://codereview.chromium.org/471233003
    
    Cr-Commit-Position: refs/heads/master@{#292470}
    
    Former-commit-id: eeba9846656053be6dc90a6f7bf164e5695b5a22

--- components/nacl/renderer/ppb_nacl_private_impl.cc
+++ components/nacl/renderer/ppb_nacl_private_impl.cc
@@ -1666,6 +1666,7 @@ void StreamPexe(PP_Instance instance,
   url_request.addHTTPHeaderField(
       blink::WebString::fromUTF8("Accept"),
       blink::WebString::fromUTF8("application/x-pnacl, */*"));
+  url_request.setRequestContext(blink::WebURLRequest::RequestContextObject);
   downloader->Load(url_request);
 }
 
