c675fead264a332d4ada975daa5c1315cf2e7d18
vabr <vabr@chromium.org>
2014-08-29 00:40:36 -0700

    Disable FeedbackApiTest.Basic on Win & Linux
    The test fails on some Win and Linux bots, more info on the bug.
    
    BUG=408917
    NOTREECHECKS=true
    NOTRY=true
    TBR=rkc@chromium.org
    
    Review URL: https://codereview.chromium.org/517953002
    
    Cr-Commit-Position: refs/heads/master@{#292585}
    
    Former-commit-id: 41ced7c873562cd11119167cc2c0f30e98652abd

--- chrome/browser/extensions/api/feedback_private/feedback_private_apitest.cc
+++ chrome/browser/extensions/api/feedback_private/feedback_private_apitest.cc
@@ -14,7 +14,14 @@ class FeedbackApiTest: public ExtensionApiTest {
   virtual ~FeedbackApiTest() {}
 };
 
-IN_PROC_BROWSER_TEST_F(FeedbackApiTest, Basic) {
+// Fails on Linux/Win.  http://crbug.com/408917
+#if defined(OS_WIN) || defined(OS_LINUX)
+#define MAYBE_Basic DISABLED_Basic
+#else
+#define MAYBE_Basic Basic
+#endif
+
+IN_PROC_BROWSER_TEST_F(FeedbackApiTest, MAYBE_Basic) {
   EXPECT_TRUE(RunExtensionTest("feedback_private/basic")) << message_;
 }
 
