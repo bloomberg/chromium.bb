b5b2b7e2c58160099fe4ef6f28f9087794ab9ec8
kbr <kbr@chromium.org>
2014-08-29 19:34:28 -0700

    Increase per-test timeout in WebGL conformance suite to 180s from 120s.
    A couple of the tests are close to the 120s limit on the Debug bots.
    
    BUG=409118
    
    Review URL: https://codereview.chromium.org/524033002
    
    Cr-Commit-Position: refs/heads/master@{#292766}
    
    Former-commit-id: d5e2bf5c309cdfb9fe27cb9a06277899e7cc0f52

--- content/test/gpu/gpu_tests/webgl_conformance.py
+++ content/test/gpu/gpu_tests/webgl_conformance.py
@@ -92,7 +92,7 @@ class WebglConformancePage(page_module.Page):
   def RunNavigateSteps(self, action_runner):
     action_runner.NavigateToPage(self)
     action_runner.WaitForJavaScriptCondition(
-        'webglTestHarness._finished', timeout_in_seconds=120)
+        'webglTestHarness._finished', timeout_in_seconds=180)
 
 
 class WebglConformance(benchmark_module.Benchmark):
