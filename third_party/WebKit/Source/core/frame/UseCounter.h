b8aa7049c4925608a303858356224890a7b89786
sunil.ratnu@samsung.com <sunil.ratnu@samsung.com>
2014-09-01 05:36:56 +0000

    Add use counter for the firefox extension containsNode()
    This is a firefox extension API (https://developer.mozilla.org/En/DOM/Selection).
    No use counter is present for this API, so adding it here to measure the usage.
    
    Bug=None
    
    Review URL: https://codereview.chromium.org/518753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181148 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0d91ded63135349f0d926bab4a3ac4fb1b080276

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -492,6 +492,7 @@ public:
         Sizes = 522,
         SrcsetXDescriptor = 523,
         SrcsetWDescriptor = 524,
+        SelectionContainsNode = 525,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
