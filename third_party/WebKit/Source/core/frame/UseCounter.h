1edabb22357f98be56e547f9cef8f8f25862be5f
v.keshav@samsung.com <v.keshav@samsung.com>
2014-09-03 08:48:45 +0000

    ended attribute has been deprecated. Adding usecounter to gather current usage statistics and let users know that this attribute may be removed in the future and will be replaced with active attribute
    BUG=392785
    
    New Spec= http://dev.w3.org/2011/webrtc/editor/getusermedia.html#mediastream
    Old Spec= http://dev.w3.org/2011/webrtc/editor/archives/20130824/getusermedia.html#mediastream
    
    Review URL: https://codereview.chromium.org/467093003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181297 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: df8ad2daf9eb4176f5a3bfdc14aa3537fcc27eab

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -494,6 +494,7 @@ public:
         SrcsetXDescriptor = 523,
         SrcsetWDescriptor = 524,
         SelectionContainsNode = 525,
+        MediaStreamEnded = 526,
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
         // Also, run update_use_counter_feature_enum.py in chromium/src/tools/metrics/histograms/
