b38d99213c8ac7644dd8ba6ef8b14d3d286b7b8e
hongbo.min@intel.com <hongbo.min@intel.com>
2014-09-03 02:21:26 +0000

    Speculative fix to the crash when calling Canvas2DLayerBridge::mailboxReleased
    The reference to Canvas2DLayerBridge held by MailboxInfo must be transferred
    when it is copied out, otherwise it would canuse undefined behaviour.
    
    BUG=405289
    
    Review URL: https://codereview.chromium.org/521103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181273 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0c221618a1271bafaac3de5596ac7fbf3057341f

--- third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridge.cpp
+++ third_party/WebKit/Source/platform/graphics/Canvas2DLayerBridge.cpp
@@ -567,10 +567,11 @@ Platform3DObject Canvas2DLayerBridge::getBackingTexture()
 
 Canvas2DLayerBridge::MailboxInfo::MailboxInfo(const MailboxInfo& other) {
     // This copy constructor should only be used for Vector reallocation
-    // Assuming 'other' is to be destroyed, we transfer m_image ownership
-    // rather than do a refcount dance.
+    // Assuming 'other' is to be destroyed, we transfer m_image and
+    // m_parentLayerBridge ownership rather than do a refcount dance.
     memcpy(&m_mailbox, &other.m_mailbox, sizeof(m_mailbox));
     m_image = const_cast<MailboxInfo*>(&other)->m_image.release();
+    m_parentLayerBridge = const_cast<MailboxInfo*>(&other)->m_parentLayerBridge.release();
     m_status = other.m_status;
 }
 
