3db4f9251d2701fdf78e6681c36f4815acd69d5b
hubbe <hubbe@chromium.org>
2014-09-03 11:28:08 -0700

    Revert of Enable castv2 perf tests again (patchset #1 id:1 of https://codereview.chromium.org/511393007/)
    Reason for revert:
    Fails with low event counts on several platforms:
    crbug.com/410391
    
    Original issue's description:
    > Enable castv2 perf tests again
    >
    > .. and switch evil network profile for bad network profile
    >
    > BUG=356842
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/26535f57480deb8b95e7ca8746af100cc4970a89 [formerly 18b429984ee371349c4f80b215896098ede8d9f3]
    
    TBR=miu@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=356842
    
    Review URL: https://codereview.chromium.org/540433002
    
    Cr-Commit-Position: refs/heads/master@{#293176}
    
    Former-commit-id: 71992c77b8cdb27ea11920536ac3dd19ff5fedf8

--- chrome/browser/extensions/api/cast_streaming/performance_test.cc
+++ chrome/browser/extensions/api/cast_streaming/performance_test.cc
@@ -72,7 +72,7 @@ enum TestFlags {
   k30fps               = 1 << 4, // use 30 fps video
   k60fps               = 1 << 5, // use 60 fps video
   kProxyWifi           = 1 << 6, // Run UDP through UDPProxy wifi profile
-  kProxyBad            = 1 << 7, // Run UDP through UDPProxy bad profile
+  kProxyEvil           = 1 << 7, // Run UDP through UDPProxy evil profile
   kSlowClock           = 1 << 8, // Receiver clock is 10 seconds slow
   kFastClock           = 1 << 9, // Receiver clock is 10 seconds fast
 };
@@ -314,8 +314,8 @@ class CastV2PerformanceTest
       suffix += "_60fps";
     if (HasFlag(kProxyWifi))
       suffix += "_wifi";
-    if (HasFlag(kProxyBad))
-      suffix += "_bad";
+    if (HasFlag(kProxyEvil))
+      suffix += "_evil";
     if (HasFlag(kSlowClock))
       suffix += "_slow";
     if (HasFlag(kFastClock))
@@ -595,7 +595,7 @@ class CastV2PerformanceTest
     receiver->Start();
 
     scoped_ptr<media::cast::test::UDPProxy> udp_proxy;
-    if (HasFlag(kProxyWifi) || HasFlag(kProxyBad)) {
+    if (HasFlag(kProxyWifi) || HasFlag(kProxyEvil)) {
       net::IPEndPoint proxy_end_point = GetFreeLocalPort();
       if (HasFlag(kProxyWifi)) {
         udp_proxy = media::cast::test::UDPProxy::Create(
@@ -604,12 +604,12 @@ class CastV2PerformanceTest
             media::cast::test::WifiNetwork().Pass(),
             media::cast::test::WifiNetwork().Pass(),
             NULL);
-      } else if (HasFlag(kProxyBad)) {
+      } else if (HasFlag(kProxyEvil)) {
         udp_proxy = media::cast::test::UDPProxy::Create(
             proxy_end_point,
             receiver_end_point,
-            media::cast::test::BadNetwork().Pass(),
-            media::cast::test::BadNetwork().Pass(),
+            media::cast::test::EvilNetwork().Pass(),
+            media::cast::test::EvilNetwork().Pass(),
             NULL);
       }
       receiver_end_point = proxy_end_point;
@@ -660,7 +660,8 @@ class CastV2PerformanceTest
 
 }  // namespace
 
-IN_PROC_BROWSER_TEST_P(CastV2PerformanceTest, Performance) {
+// crbug.com/356842
+IN_PROC_BROWSER_TEST_P(CastV2PerformanceTest, DISABLED_Performance) {
   RunTest("CastV2Performance");
 }
 
@@ -675,6 +676,6 @@ INSTANTIATE_TEST_CASE_P(
         kUseGpu | k60fps,
         kUseGpu | k24fps | kDisableVsync,
         kUseGpu | k30fps | kProxyWifi,
-        kUseGpu | k30fps | kProxyBad,
+        kUseGpu | k30fps | kProxyEvil,
         kUseGpu | k30fps | kSlowClock,
         kUseGpu | k30fps | kFastClock));
