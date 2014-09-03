d84675707c19e3c77a27de8c8949abf938235af7
mkwst@chromium.org <mkwst@chromium.org>
2014-09-03 14:12:02 +0000

    Verify that ServiceWorkers may not be registered on unauthenticated origins.
    This patch also serves to verify that https://codereview.chromium.org/529403002/
    has the expected effect of resolving '*.test' hosts to 127.0.0.1.
    
    BUG=410190
    
    Review URL: https://codereview.chromium.org/533093002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181318 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 7555257841b922863f718827c2d7d49ee2a9fdef

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/test-helpers.js
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/test-helpers.js
@@ -141,6 +141,7 @@ function wait_for_state(test, worker, state) {
 function get_host_info() {
     var ORIGINAL_HOST = '127.0.0.1';
     var REMOTE_HOST = 'localhost';
+    var UNAUTHENTICATED_HOST = 'example.test';
     var HTTP_PORT = 8000;
     var HTTPS_PORT = 8443;
     try {
@@ -157,7 +158,8 @@ function get_host_info() {
         HTTP_ORIGIN: 'http://' + ORIGINAL_HOST + ':' + HTTP_PORT,
         HTTPS_ORIGIN: 'https://' + ORIGINAL_HOST + ':' + HTTPS_PORT,
         HTTP_REMOTE_ORIGIN: 'http://' + REMOTE_HOST + ':' + HTTP_PORT,
-        HTTPS_REMOTE_ORIGIN: 'https://' + REMOTE_HOST + ':' + HTTPS_PORT
+        HTTPS_REMOTE_ORIGIN: 'https://' + REMOTE_HOST + ':' + HTTPS_PORT,
+        UNAUTHENTICATED_ORIGIN: 'http://' + UNAUTHENTICATED_HOST + ':' + HTTP_PORT
     };
 }
 
