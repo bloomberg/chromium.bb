0cc3dca62849165cea1bd5a5d929f57da1f26063
sullivan <sullivan@chromium.org>
2014-09-03 17:19:31 -0700

    Re-recorded tough_energy_cases.
    Fixed bug in recording where it would not download credentials.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/538603002
    
    Cr-Commit-Position: refs/heads/master@{#293233}
    
    Former-commit-id: 776c7121698e0892ab8eefae478ab15dac2572f8

--- tools/perf/page_sets/tough_energy_cases.py
+++ tools/perf/page_sets/tough_energy_cases.py
@@ -11,16 +11,19 @@ class ToughEnergyCasesPage(page_module.Page):
     super(ToughEnergyCasesPage, self).__init__(url=url, page_set=page_set)
     self.credentials_path = 'data/credentials.json'
 
+class CodePenPage(ToughEnergyCasesPage):
 
-class GmailPage(ToughEnergyCasesPage):
+  def __init__(self, url, page_set):
+    super(CodePenPage, self).__init__(url, page_set)
+    self.credentials = 'codepen'
 
-  """ Why: productivity, top google properties """
 
-  def __init__(self, page_set):
-    super(GmailPage, self).__init__(
-      url='https://mail.google.com/mail/',
-      page_set=page_set)
+class GooglePage(ToughEnergyCasesPage):
 
+  def __init__(self, url, page_set):
+    super(GooglePage, self).__init__(
+        url=url,
+        page_set=page_set)
     self.credentials = 'google'
 
   def RunNavigateSteps(self, action_runner):
@@ -31,26 +34,89 @@ class GmailPage(ToughEnergyCasesPage):
 
 
 class ToughEnergyCasesPageSet(page_set_module.PageSet):
-
-  """ Pages for measuring Chrome power draw. """
+  """Pages for measuring Chrome power draw."""
 
   def __init__(self):
     super(ToughEnergyCasesPageSet, self).__init__(
-      archive_data_file='data/tough_energy_cases.json',
-      bucket=page_set_module.PUBLIC_BUCKET,
-      credentials_path='data/credentials.json')
+        archive_data_file='data/tough_energy_cases.json',
+        bucket=page_set_module.PUBLIC_BUCKET,
+        credentials_path='data/credentials.json')
 
-    # Why: Above the fold animated gif running in the background
-    self.AddPage(ToughEnergyCasesPage(
-      'file://tough_energy_cases/above-fold-animated-gif.html',
-      self))
-    # TODO(dominikg): fix crbug.com/386152
-    #self.AddPage(GmailPage(self))
-    # Why: Below the fold animated gif
-    self.AddPage(ToughEnergyCasesPage(
-      'file://tough_energy_cases/below-fold-animated-gif.html',
-      self))
-    # Why: Below the fold flash animation
-    self.AddPage(ToughEnergyCasesPage(
-      'file://tough_energy_cases/below-fold-flash.html',
-      self))
+    # Why: productivity, top google properties
+    self.AddPage(GooglePage('https://mail.google.com/mail/', self))
+
+    # Why: Image constantly changed in the background, above the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/eIutG', self))
+
+    # Why: Image constantly changed in the background, below the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/zcDdv', self))
+
+    # Why: CSS Animation, above the fold
+    self.AddPage(CodePenPage(
+         'http://codepen.io/testificate364/debug/nrbDc', self))
+
+    # Why: CSS Animation, below the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/fhKCg', self))
+
+    # Why: requestAnimationFrame, above the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/paJhg',self))
+
+    # Why: requestAnimationFrame, below the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/yaosK', self))
+
+    # Why: setTimeout animation, above the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/DLbxg', self))
+
+    # Why: setTimeout animation, below the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/kFvpd', self))
+
+    # Why: setInterval animation, above the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/lEhyw', self))
+
+    # Why: setInterval animation, below the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/zhgBD', self))
+
+    # Why: Animated GIF, above the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/jetyn', self))
+
+    # Why: Animated GIF, below the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/Kvdxs', self))
+
+    # Why: HTML5 video, above the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/lJAiH', self))
+
+    # Why: HTML5 video, below the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/EFceH', self))
+
+    # Why: PostMessage between frames, above the fold
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/pgBHu', self))
+
+    # Why: Asynchronous XHR continually running
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/iwAfJ', self))
+
+    # Why: Web Worker continually running
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/ckItK', self))
+
+    # Why: flash video
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/cFEaD', self))
+
+    # Why: Blank page in the foreground
+    self.AddPage(CodePenPage(
+        'http://codepen.io/testificate364/debug/HdIgr', self))
