81860b302154d1fd3cb62e31cedddd07fee1fbfe
horo@chromium.org <horo@chromium.org>
2014-09-04 23:26:49 +0000

    [ServiceWorker] Refactor fetch.html layouttest to use service_worker_test()
    In this change, I remove the test of fetch('http://www.example.com/foo') because it is insufficient.
    
    TEST=LayoutTests/http/tests/serviceworker/fetch.html
    BUG=
    
    Review URL: https://codereview.chromium.org/532163003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181418 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: efca24d41081eec01a64a3795d1c2c5dc4060536

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/fetch-worker.js
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/fetch-worker.js
@@ -1,41 +1,43 @@
-self.onmessage = function(e) {
-    var message = e.data;
-    if ('port' in message) {
-        port = message.port;
-        doNextFetchTest(port);
-    }
-};
+importScripts('worker-test-harness.js');
+importScripts('test-helpers.js');
 
-var testTargets = [
-    'other.html',
-    'http://',
-    'http://www.example.com/foo',
-    'fetch-status.php?status=200',
-    'fetch-status.php?status=404'
-];
+async_test(function(t) {
+    fetch('http://')
+      .then(
+        unreached_rejection(t, 'fetch of invalid URL must fail'),
+        function(e) {
+          assert_equals(e.message, 'Invalid URL');
+          t.done();
+        })
+      .catch(unreached_rejection(t));
+  }, 'Fetch invalid URL in ServiceWorkerGlobalScope');
 
-function doNextFetchTest(port) {
+async_test(function(t) {
+    fetch('fetch-status.php?status=200')
+      .then(function(response) {
+          assert_equals(response.status, 200);
+          assert_equals(response.statusText, 'OK');
+          t.done();
+        })
+      .catch(unreached_rejection(t));
+  }, 'Fetch result of 200 response in ServiceWorkerGlobalScope');
 
+async_test(function(t) {
+    fetch('fetch-status.php?status=404')
+      .then(function(response) {
+          assert_equals(response.status, 404);
+          assert_equals(response.statusText, 'Not Found');
+          t.done();
+        })
+      .catch(unreached_rejection(t));
+  }, 'Fetch result of 404 response in ServiceWorkerGlobalScope');
+
+test(function(t) {
     function runInfiniteFetchLoop() {
       fetch('dummy.html')
         .then(function() { runInfiniteFetchLoop(); });
     }
-
-    if (testTargets.length == 0) {
-      // Destroying the execution context while fetch is happening
-      // should not cause a crash.
-      runInfiniteFetchLoop();
-
-      port.postMessage('quit');
-      return;
-    }
-    var target = testTargets.shift();
-    fetch(target)
-    .then(function(response) {
-        port.postMessage('Resolved: ' + target + ' [' + response.status + ']' + response.statusText);
-        doNextFetchTest(port);
-    }).catch(function(e) {
-        port.postMessage('Rejected: ' + target + ' : '+ e.message);
-        doNextFetchTest(port);
-    });
-};
+    runInfiniteFetchLoop();
+  },
+  'Destroying the execution context while fetch is happening should not ' +
+      'cause a crash.');
