839e333d5239c2908702ab1c0df3cef5c0c209f8
lazyboy <lazyboy@chromium.org>
2014-09-04 14:36:30 -0700

    Fix and enable WebViewInteractiveTest.Focus_InputMethod
    There were couple issues that were fixed:
    1. If oninput listener fires before the guest is requested to "wait
    for oninput", we would timeout. This is taken care of by a flag.
    2. TextInputClient::SetCompositionText() call sends an IPC to the
    renderer/, if the next JavaScript step runs before the IPC is sent
    then that would fail the test. This is taken care of by sending
    executing empty JavaScript code on the embedder.
    
    BUG=387956
    Test=Internal test only change, nothing visible.
    
    Review URL: https://codereview.chromium.org/532883003
    
    Cr-Commit-Position: refs/heads/master@{#293341}
    
    Former-commit-id: f69da7c88e832aa7751d5756c7beff3e183e905f

--- chrome/test/data/extensions/platform_apps/web_view/focus/inject_input_method.js
+++ chrome/test/data/extensions/platform_apps/web_view/focus/inject_input_method.js
@@ -15,6 +15,18 @@ var sendMessage = function(data) {
   embedder.postMessage(JSON.stringify(data), '*');
 };
 
+var waitingForInput = false;
+
+var onInputFired = function() {
+  sendMessage(['response-waitForOnInput', onInputState.value]);
+  onInputState.fired = false;
+  if (inputElement1.value == 'InputTest456') {
+    // Prepare for next step, step 3.
+    inputElement1.value = '';
+  }
+  waitingForInput = false;
+};
+
 var onInputState = {fired: false, value: ''};
 // Waits for oninput event to fire on |inputElement1|.
 // Upon receiving the event, we ping back the embedder with the value of
@@ -23,13 +35,9 @@ var waitForOnInput = function() {
   var inputElement1 = document.querySelector('input');
   LOG('inputElement1: ' + inputElement1);
   if (onInputState.fired) {
-    sendMessage(['response-waitForOnInput', onInputState.value]);
-
-    onInputState.fired = false;
-    if (inputElement1.value == 'InputTest456') {
-      // Prepare for next step, step 3.
-      inputElement1.value = '';
-    }
+    onInputFired();
+  } else {
+    waitingForInput = true;
   }
 };
 
@@ -41,6 +49,9 @@ var waitForFocus = function() {
     LOG('inputElement1.oninput: ' + inputElement1.value);
     onInputState.fired = true;
     onInputState.value = inputElement1.value;
+    if (waitingForInput) {
+      onInputFired();
+    }
   };
   var inputElement1FocusListener = function() {
     LOG('inputElement1.focus');
