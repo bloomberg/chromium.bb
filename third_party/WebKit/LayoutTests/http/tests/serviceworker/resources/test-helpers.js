972dd2f08f1d7a5933e934090bcbaaa18d014f0b
nhiroki@chromium.org <nhiroki@chromium.org>
2014-09-05 11:02:03 +0000

    ServiceWorker: Clean up fetch-event.html
    This change...
    - removes unnecessary 'wait_for_update' calls because a register promise
    is resolved with a registration object that has an installing worker.
    - removes 't.step_func' from promise chains.
    - unloads iframes gracefully.
    
    
    BUG=401381
    TEST=run_webkit_tests.py --debug http/tests/serviceworker/fetch-event.html
    
    Review URL: https://codereview.chromium.org/531423002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181465 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 03dc8d0df5e6edb8cd785a2b88f0158912bc34ae

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/test-helpers.js
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/test-helpers.js
@@ -82,6 +82,22 @@ function wait_for_state(test, worker, state) {
     }));
 }
 
+function wait_for_activated(test, registration) {
+  var expected_state = 'activated';
+  if (registration.active) {
+    if (registration.active.state === expected_state)
+      return Promise.resolve(registration.active);
+    else
+      return wait_for_state(test, registration.active, expected_state);
+  }
+  if (registration.waiting)
+    return wait_for_state(test, registration.waiting, expected_state);
+  if (registration.installing)
+    return wait_for_state(test, registration.installing, expected_state);
+  return Promise.reject(
+      new Error('registration must have at least one version'));
+}
+
 (function() {
   function fetch_tests_from_worker(worker) {
     return new Promise(function(resolve, reject) {
