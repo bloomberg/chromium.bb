ed59b0d26680c9984cd46854598db4f2c811672d
ttuttle <ttuttle@chromium.org>
2014-09-06 06:12:10 -0700

    ReadIpHelper: Zero out before calling GetAdaptersAddresses
    This is a first-ditch attempt at fixing http://crbug.com/401655.
    
    BUG=401655
    
    Review URL: https://codereview.chromium.org/539313002
    
    Cr-Commit-Position: refs/heads/master@{#293610}
    
    Former-commit-id: 469b8770baa97d9d114df4bd97995f60fb9e39fe

--- net/dns/dns_config_service_win.cc
+++ net/dns/dns_config_service_win.cc
@@ -123,6 +123,7 @@ scoped_ptr<IP_ADAPTER_ADDRESSES, base::FreeDeleter> ReadIpHelper(ULONG flags) {
   for (unsigned tries = 0; (tries < 3) && (rv == ERROR_BUFFER_OVERFLOW);
        tries++) {
     out.reset(static_cast<PIP_ADAPTER_ADDRESSES>(malloc(len)));
+    memset(out.get(), 0, len);
     rv = GetAdaptersAddresses(AF_UNSPEC, flags, NULL, out.get(), &len);
   }
   if (rv != NO_ERROR)
