33bad73b273bcb2ebac430d7b572930c0fbfbeed
Benjamin Gaignard <benjamin.gaignard@linaro.org>
2014-09-07 18:26:06 +0100

    modetest: Add support of STI driver
    Reviewed-by: Jakob Bornecrantz <jakob@vmware.com>

--- tests/modetest/modetest.c
+++ tests/modetest/modetest.c
@@ -1437,7 +1437,7 @@ int main(int argc, char **argv)
 	int drop_master = 0;
 	int test_vsync = 0;
 	int test_cursor = 0;
-	const char *modules[] = { "i915", "radeon", "nouveau", "vmwgfx", "omapdrm", "exynos", "tilcdc", "msm" };
+	const char *modules[] = { "i915", "radeon", "nouveau", "vmwgfx", "omapdrm", "exynos", "tilcdc", "msm", "sti" };
 	char *device = NULL;
 	char *module = NULL;
 	unsigned int i;
