8dd8a9b3f2d5b119395cf527f5e2e683e29d7ecb
pdr@chromium.org <pdr@chromium.org>
2014-09-07 19:16:19 +0000

    [trivial] Remove RenderBox::MarginSign
    This patch removes the unused MarginSign enum. I noticed this while
    searching how we name enums in Blink as part of another patch.
    
    Since I was in the area, I fixed the comment about margins as well.
    
    R=esprehn
    
    Review URL: https://codereview.chromium.org/548113002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181528 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: f09857c5bf7707e6dba1fa9156116c100d208f8f

--- third_party/WebKit/Source/core/rendering/RenderBox.h
+++ third_party/WebKit/Source/core/rendering/RenderBox.h
@@ -321,13 +321,12 @@ public:
         m_marginBox.setEnd(styleToUse->writingMode(), styleToUse->direction(), value);
     }
 
-    // The following five functions are used to implement collapsing margins.
+    // The following functions are used to implement collapsing margins.
     // All objects know their maximal positive and negative margins.  The
     // formula for computing a collapsed margin is |maxPosMargin| - |maxNegmargin|.
     // For a non-collapsing box, such as a leaf element, this formula will simply return
     // the margin of the element.  Blocks override the maxMarginBefore and maxMarginAfter
     // methods.
-    enum MarginSign { PositiveMargin, NegativeMargin };
     virtual bool isSelfCollapsingBlock() const { return false; }
     virtual LayoutUnit collapsedMarginBefore() const { return marginBefore(); }
     virtual LayoutUnit collapsedMarginAfter() const { return marginAfter(); }
