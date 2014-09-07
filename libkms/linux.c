f3fd11beaa70b0624ab1d6c7d8e25c65eea942ac
Emil Velikov <emil.l.velikov@gmail.com>
2014-09-07 17:54:39 +0100

    libkms: build the intel backend only when needed
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Jakob Bornecrantz <jakob@vmware.com>

--- libkms/linux.c
+++ libkms/linux.c
@@ -103,25 +103,31 @@ linux_from_sysfs(int fd, struct kms_driver **out)
 	if (ret)
 		return ret;
 
+#ifdef HAVE_INTEL
 	if (!strcmp(name, "intel"))
 		ret = intel_create(fd, out);
+	else
+#endif
 #ifdef HAVE_VMWGFX
-	else if (!strcmp(name, "vmwgfx"))
+	if (!strcmp(name, "vmwgfx"))
 		ret = vmwgfx_create(fd, out);
+	else
 #endif
 #ifdef HAVE_NOUVEAU
-	else if (!strcmp(name, "nouveau"))
+	if (!strcmp(name, "nouveau"))
 		ret = nouveau_create(fd, out);
+	else
 #endif
 #ifdef HAVE_RADEON
-	else if (!strcmp(name, "radeon"))
+	if (!strcmp(name, "radeon"))
 		ret = radeon_create(fd, out);
+	else
 #endif
 #ifdef HAVE_EXYNOS
-	else if (!strcmp(name, "exynos"))
+	if (!strcmp(name, "exynos"))
 		ret = exynos_create(fd, out);
-#endif
 	else
+#endif
 		ret = -ENOSYS;
 
 	free(name);
