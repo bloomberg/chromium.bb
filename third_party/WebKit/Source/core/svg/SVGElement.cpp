929a50fab53ff4c04ad49d76f3a4a755bc1f2c2d
sigbjornf@opera.com <sigbjornf@opera.com>
2014-09-07 08:26:31 +0000

    Make SVGElement::removeEventListener() mirror its addEventListener()
    Simplify the implementation by having it mirror the addEventListener() version.
    
    Which means creating a correct protector and passing it inwards.
    
    Review URL: https://codereview.chromium.org/547103002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181510 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: bb106a6e118934c07ba01306ad364fe5e4d01e9a

--- third_party/WebKit/Source/core/svg/SVGElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGElement.cpp
@@ -757,32 +757,23 @@ bool SVGElement::addEventListener(const AtomicString& eventType, PassRefPtr<Even
     return true;
 }
 
-bool SVGElement::removeEventListener(const AtomicString& eventType, PassRefPtr<EventListener> listener, bool useCapture)
+bool SVGElement::removeEventListener(const AtomicString& eventType, PassRefPtr<EventListener> prpListener, bool useCapture)
 {
-    WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> > instances;
-    collectInstancesForSVGElement(this, instances);
-    if (instances.isEmpty())
-        return Node::removeEventListener(eventType, listener.get(), useCapture);
-
-    // EventTarget::removeEventListener creates a PassRefPtr around the given EventListener
-    // object when creating a temporary RegisteredEventListener object used to look up the
-    // event listener in a cache. If we want to be able to call removeEventListener() multiple
-    // times on different nodes, we have to delay its immediate destruction, which would happen
-    // after the first call below.
-    // XXX is that true?
-    RefPtr<EventListener> protector(listener);
+    RefPtr<EventListener> listener = prpListener;
 
     // Remove event listener from regular DOM element
-    if (!Node::removeEventListener(eventType, listener.get(), useCapture))
+    if (!Node::removeEventListener(eventType, listener, useCapture))
         return false;
 
     // Remove event listener from all shadow tree DOM element instances
+    WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> > instances;
+    collectInstancesForSVGElement(this, instances);
     const WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> >::const_iterator end = instances.end();
     for (WillBeHeapHashSet<RawPtrWillBeWeakMember<SVGElement> >::const_iterator it = instances.begin(); it != end; ++it) {
         SVGElement* shadowTreeElement = *it;
         ASSERT(shadowTreeElement);
 
-        shadowTreeElement->Node::removeEventListener(eventType, listener.get(), useCapture);
+        shadowTreeElement->Node::removeEventListener(eventType, listener, useCapture);
     }
 
     return true;
