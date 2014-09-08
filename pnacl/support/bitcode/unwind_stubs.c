aa2acc92795d5093057043d3fea9702801daf9f8
dbrazdil@google.com <dbrazdil@google.com>
2014-09-08 22:58:49 +0000

    Fixed signatures of _Unwind_* stubs in PNaCl support library
    PNaCl provides stubs for _Unwind_* functions when C++ exceptions 
    are disabled. To simplify the source code, these were generated
    by a macro that defined functions of the right name but with wrong
    signatures, and therefore risking introduction of undefined
    behaviour. Even though the code works, it complicates analysis and
    transformation of the code, such as application of control-flow
    integrity enforcement.
    
    TEST= compiled and invoked manually, PNaCl toolchain trybots
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/499673004
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@13712 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: a62c902c37685234dfeac252e839a78b304135ea

--- pnacl/support/bitcode/unwind_stubs.c
+++ pnacl/support/bitcode/unwind_stubs.c
@@ -15,28 +15,86 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-#define STUB(sym) \
-  void sym() { \
-    char msg[] = "Aborting: " #sym " called " \
-                 "(C++ exception handling is disabled)\n"; \
-    write(2, msg, sizeof(msg) - 1); \
-    abort(); \
-  }
+/*
+ * unwind.h will generate static definitions of some of the functions when
+ * the __arm__ macro is defined but we want to provide our own, exported
+ * stubs because this will be used as portable bitcode.
+ */
+#undef __arm__
+#include <unwind.h>
 
-STUB(_Unwind_DeleteException)
-STUB(_Unwind_GetRegionStart)
-STUB(_Unwind_GetDataRelBase)
-STUB(_Unwind_GetGR)
-STUB(_Unwind_GetIP)
-STUB(_Unwind_GetIPInfo)
-STUB(_Unwind_GetTextRelBase)
-STUB(_Unwind_GetLanguageSpecificData)
-STUB(_Unwind_SetGR)
-STUB(_Unwind_SetIP)
-STUB(_Unwind_PNaClSetResult0)
-STUB(_Unwind_PNaClSetResult1)
-STUB(_Unwind_RaiseException)
-STUB(_Unwind_Resume_or_Rethrow)
-STUB(_Unwind_Resume)
-STUB(_Unwind_GetCFA)
-STUB(_Unwind_Backtrace)
+#define STUB                                                      \
+  char msg1[] = "Aborting: ";                                     \
+  char msg2[] = " called (C++ exception handling is disabled)\n"; \
+  write(2, msg1, sizeof(msg1) - 1);                               \
+  write(2, __func__, sizeof(__func__) - 1);                       \
+  write(2, msg2, sizeof(msg2) - 1);                               \
+  abort();
+
+void _Unwind_DeleteException(struct _Unwind_Exception *e) {
+  STUB
+}
+
+_Unwind_Ptr _Unwind_GetRegionStart(struct _Unwind_Context *c) {
+  STUB
+}
+
+_Unwind_Ptr _Unwind_GetDataRelBase(struct _Unwind_Context *c) {
+  STUB
+}
+
+_Unwind_Word _Unwind_GetGR(struct _Unwind_Context *c, int i) {
+  STUB
+}
+
+_Unwind_Ptr _Unwind_GetIP(struct _Unwind_Context *c) {
+  STUB
+}
+
+_Unwind_Ptr _Unwind_GetIPInfo(struct _Unwind_Context *c, int *i) {
+  STUB
+}
+
+_Unwind_Ptr _Unwind_GetTextRelBase(struct _Unwind_Context *c) {
+  STUB
+}
+
+void *_Unwind_GetLanguageSpecificData(struct _Unwind_Context *c) {
+  STUB
+}
+
+void _Unwind_SetGR(struct _Unwind_Context *c, int i, _Unwind_Word w) {
+  STUB
+}
+
+void _Unwind_SetIP(struct _Unwind_Context *c, _Unwind_Ptr p) {
+  STUB
+}
+
+void _Unwind_PNaClSetResult0(struct _Unwind_Context *c, _Unwind_Word w) {
+  STUB
+}
+
+void _Unwind_PNaClSetResult1(struct _Unwind_Context *c, _Unwind_Word w) {
+  STUB
+}
+
+_Unwind_Reason_Code _Unwind_RaiseException(struct _Unwind_Exception *e) {
+  STUB
+}
+
+_Unwind_Reason_Code _Unwind_Resume_or_Rethrow(struct _Unwind_Exception *e) {
+  STUB
+}
+
+void _Unwind_Resume(struct _Unwind_Exception *e) {
+  STUB
+}
+
+_Unwind_Word _Unwind_GetCFA(struct _Unwind_Context *c) {
+  STUB
+}
+
+_Unwind_Reason_Code _Unwind_Backtrace(_Unwind_Trace_Fn fn, void *p) {
+  STUB
+}
