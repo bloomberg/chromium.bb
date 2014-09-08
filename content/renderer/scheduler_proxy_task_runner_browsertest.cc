df324139c2e80c47976f7fad3f54fe5b4c363ac6
jam <jam@chromium.org>
2014-09-08 07:38:55 -0700

    Revert of Revert of Forward input and compositing tasks to the Blink scheduler (patchset #1 id:1 of https://codereview.chromium.org/548253002/)
    Reason for revert:
    this didn't reduce flakiness
    
    Original issue's description:
    > Revert of Forward input and compositing tasks to the Blink scheduler (patchset #21 id:400001 of https://codereview.chromium.org/363383002/)
    >
    > Reason for revert:
    > suspecting it's causing the high rate of flakes in Instrumentation test ContentShellTest
    >
    > BUG=411756
    >
    > Original issue's description:
    > > Forward input and compositing tasks to the Blink scheduler
    > >
    > > Instead of posting input and compositor tasks directly to the
    > > main thread message loop, forward them to the Blink scheduler
    > > so they can be prioritized over other tasks.
    > >
    > > BUG=391005
    > >
    > > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=290845
    > >
    > > Committed: https://chromium.googlesource.com/chromium/src/+/61e297288492a282b359360abb9f81fa393a6a2f [formerly 68bab3290b45a8610ad875d3c92530477c16a948]
    >
    > TBR=jamesr@chromium.org,alexclarke@chromium.org,picksi@chromium.org,eseidel@chromium.org,skyostil@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=391005
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/8920226fbe3cf3c82a342e1455a2b27a817163ba [formerly 66f85d5405417e6ddd11e1b9be677062a2e3ae4f]
    
    TBR=jamesr@chromium.org,alexclarke@chromium.org,picksi@chromium.org,eseidel@chromium.org,skyostil@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=411756
    
    Review URL: https://codereview.chromium.org/548333002
    
    Cr-Commit-Position: refs/heads/master@{#293722}
    
    Former-commit-id: 479004ba3d84ec815444e9279adcda7ed056afcb

---
+++ content/renderer/scheduler_proxy_task_runner_browsertest.cc
@@ -0,0 +1,85 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/public/browser/content_browser_client.h"
+#include "content/public/common/content_client.h"
+#include "content/public/renderer/content_renderer_client.h"
+#include "content/renderer/render_process_impl.h"
+#include "content/renderer/render_thread_impl.h"
+#include "content/renderer/scheduler_proxy_task_runner.h"
+#include "content/test/mock_render_process.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace content {
+namespace {
+
+void TestTask(int value, int* result) {
+  *result = (*result << 4) | value;
+}
+
+}  // namespace
+
+class DummyListener : public IPC::Listener {
+ public:
+  virtual bool OnMessageReceived(const IPC::Message& message) OVERRIDE {
+    return true;
+  }
+};
+
+TEST(SchedulerProxyTaskRunnerBrowserTest, TestTaskPosting) {
+  ContentClient content_client;
+  ContentBrowserClient content_browser_client;
+  ContentRendererClient content_renderer_client;
+  SetContentClient(&content_client);
+  SetBrowserClientForTesting(&content_browser_client);
+  SetRendererClientForTesting(&content_renderer_client);
+  base::MessageLoopForIO message_loop;
+
+  std::string channel_id =
+      IPC::Channel::GenerateVerifiedChannelID(std::string());
+  DummyListener dummy_listener;
+  scoped_ptr<IPC::Channel> channel(
+      IPC::Channel::CreateServer(channel_id, &dummy_listener));
+  EXPECT_TRUE(channel->Connect());
+
+  scoped_ptr<MockRenderProcess> mock_process(new MockRenderProcess);
+  // Owned by mock_process.
+  RenderThreadImpl* thread = new RenderThreadImpl(channel_id);
+  thread->EnsureWebKitInitialized();
+
+  scoped_refptr<base::SingleThreadTaskRunner> input_task_runner =
+      make_scoped_refptr(new SchedulerProxyTaskRunner<
+          &blink::WebSchedulerProxy::postInputTask>());
+  scoped_refptr<base::SingleThreadTaskRunner> compositor_task_runner =
+      make_scoped_refptr(new SchedulerProxyTaskRunner<
+          &blink::WebSchedulerProxy::postCompositorTask>());
+
+  int input_order = 0;
+  int compositor_order = 0;
+
+  input_task_runner->PostTask(FROM_HERE,
+                              base::Bind(&TestTask, 1, &input_order));
+  compositor_task_runner->PostTask(FROM_HERE,
+                                   base::Bind(&TestTask, 1, &compositor_order));
+  input_task_runner->PostTask(FROM_HERE,
+                              base::Bind(&TestTask, 2, &input_order));
+  compositor_task_runner->PostTask(FROM_HERE,
+                                   base::Bind(&TestTask, 2, &compositor_order));
+
+  input_task_runner->PostTask(FROM_HERE,
+                              base::Bind(&TestTask, 3, &input_order));
+  input_task_runner->PostTask(FROM_HERE,
+                              base::Bind(&TestTask, 4, &input_order));
+  compositor_task_runner->PostTask(FROM_HERE,
+                                   base::Bind(&TestTask, 3, &compositor_order));
+  compositor_task_runner->PostTask(FROM_HERE,
+                                   base::Bind(&TestTask, 4, &compositor_order));
+
+  message_loop.RunUntilIdle();
+
+  EXPECT_EQ(0x1234, input_order);
+  EXPECT_EQ(0x1234, compositor_order);
+}
+
+}  // namespace content
