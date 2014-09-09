68a23db245e7aaa8a29add88e72ae3910a0e7ab9
timloh@chromium.org <timloh@chromium.org>
2014-09-09 06:11:11 +0000

    Create timing functions directly instead of using parser in KeyframeEffectModelTest
    As the tests in KeyframeEffectModelTest are unit tests, it doesn't make
    sense for them to use the parser to construct timing functions when
    they can be constructed directly.
    
    Review URL: https://codereview.chromium.org/552123003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181609 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: b9a99728f5b536df536e63598cd5b8125655c319

--- third_party/WebKit/Source/core/animation/KeyframeEffectModelTest.cpp
+++ third_party/WebKit/Source/core/animation/KeyframeEffectModelTest.cpp
@@ -35,8 +35,6 @@
 #include "core/animation/animatable/AnimatableLength.h"
 #include "core/animation/animatable/AnimatableUnknown.h"
 #include "core/css/CSSPrimitiveValue.h"
-#include "core/css/parser/BisonCSSParser.h"
-#include "core/css/resolver/CSSToStyleMap.h"
 #include <gtest/gtest.h>
 
 using namespace blink;
@@ -141,9 +139,8 @@ TEST(AnimationKeyframeEffectModel, DISABLED_CompositeAdd)
 TEST(AnimationKeyframeEffectModel, CompositeEaseIn)
 {
     AnimatableValueKeyframeVector keyframes = keyframesAtZeroAndOne(pixelAnimatableValue(3.0), pixelAnimatableValue(5.0));
-    RefPtrWillBeRawPtr<CSSValue> timingFunction = BisonCSSParser::parseAnimationTimingFunctionValue("ease-in");
     keyframes[0]->setComposite(AnimationEffect::CompositeReplace);
-    keyframes[0]->setEasing(CSSToStyleMap::mapAnimationTimingFunction(timingFunction.get(), true));
+    keyframes[0]->setEasing(CubicBezierTimingFunction::preset(CubicBezierTimingFunction::EaseIn));
     keyframes[1]->setComposite(AnimationEffect::CompositeReplace);
     RefPtrWillBeRawPtr<AnimatableValueKeyframeEffectModel> effect = AnimatableValueKeyframeEffectModel::create(keyframes);
     expectDoubleValue(3.8579516, effect->sample(0, 0.6, duration)->at(0));
@@ -153,9 +150,8 @@ TEST(AnimationKeyframeEffectModel, CompositeEaseIn)
 TEST(AnimationKeyframeEffectModel, CompositeCubicBezier)
 {
     AnimatableValueKeyframeVector keyframes = keyframesAtZeroAndOne(pixelAnimatableValue(3.0), pixelAnimatableValue(5.0));
-    RefPtrWillBeRawPtr<CSSValue> timingFunction = BisonCSSParser::parseAnimationTimingFunctionValue("cubic-bezier(0.42, 0, 0.58, 1)");
     keyframes[0]->setComposite(AnimationEffect::CompositeReplace);
-    keyframes[0]->setEasing(CSSToStyleMap::mapAnimationTimingFunction(timingFunction.get(), true));
+    keyframes[0]->setEasing(CubicBezierTimingFunction::create(0.42, 0, 0.58, 1));
     keyframes[1]->setComposite(AnimationEffect::CompositeReplace);
     RefPtrWillBeRawPtr<AnimatableValueKeyframeEffectModel> effect = AnimatableValueKeyframeEffectModel::create(keyframes);
     expectDoubleValue(4.3363357, effect->sample(0, 0.6, duration)->at(0));
