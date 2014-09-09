d6411b24475b8d9808f95b437f5803409b695582
bashi@chromium.org <bashi@chromium.org>
2014-09-09 06:11:40 +0000

    Add FontFaceDescriptors IDL dictionary
    To follow the spec[1]. This is the first place to use IDL dictionary
    execept for testing, so this CL also includes build related changes.
    Existing tests(e.g. fast/css/fontface-constructor-error.html) should
    cover this change.
    
    [1] http://dev.w3.org/csswg/css-font-loading/#fontface-interface
    
    BUG=403150
    
    Review URL: https://codereview.chromium.org/481453003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181610 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d671ccdca4dd5a48e1bc907ee1284342db189929

--- third_party/WebKit/Source/core/css/FontFace.h
+++ third_party/WebKit/Source/core/css/FontFace.h
@@ -49,6 +49,7 @@ class CSSValueList;
 class Dictionary;
 class Document;
 class ExceptionState;
+class FontFaceDescriptors;
 class StylePropertySet;
 class StyleRuleFontFace;
 
@@ -57,9 +58,9 @@ class FontFace : public RefCountedWillBeGarbageCollectedFinalized<FontFace>, pub
 public:
     enum LoadStatus { Unloaded, Loading, Loaded, Error };
 
-    static PassRefPtrWillBeRawPtr<FontFace> create(ExecutionContext*, const AtomicString& family, PassRefPtr<ArrayBuffer> source, const Dictionary&);
-    static PassRefPtrWillBeRawPtr<FontFace> create(ExecutionContext*, const AtomicString& family, PassRefPtr<ArrayBufferView>, const Dictionary&);
-    static PassRefPtrWillBeRawPtr<FontFace> create(ExecutionContext*, const AtomicString& family, const String& source, const Dictionary&);
+    static PassRefPtrWillBeRawPtr<FontFace> create(ExecutionContext*, const AtomicString& family, PassRefPtr<ArrayBuffer> source, const FontFaceDescriptors*);
+    static PassRefPtrWillBeRawPtr<FontFace> create(ExecutionContext*, const AtomicString& family, PassRefPtr<ArrayBufferView>, const FontFaceDescriptors*);
+    static PassRefPtrWillBeRawPtr<FontFace> create(ExecutionContext*, const AtomicString& family, const String& source, const FontFaceDescriptors*);
     static PassRefPtrWillBeRawPtr<FontFace> create(Document*, const StyleRuleFontFace*);
 
     ~FontFace();
@@ -108,7 +109,7 @@ public:
 
 private:
     FontFace();
-    FontFace(ExecutionContext*, const AtomicString& family, const Dictionary& descriptors);
+    FontFace(ExecutionContext*, const AtomicString& family, const FontFaceDescriptors*);
 
     void initCSSFontFace(Document*, PassRefPtrWillBeRawPtr<CSSValue> src);
     void initCSSFontFace(const unsigned char* data, unsigned size);
