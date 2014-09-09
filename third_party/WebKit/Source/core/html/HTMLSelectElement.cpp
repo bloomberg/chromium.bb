1d0e903f8203a4b08e702a9d9f91ce0e1b597e50
ziran.sun@samsung.com <ziran.sun@samsung.com>
2014-09-09 00:17:31 +0000

    Adjust autofilled property for <search> input and <select> elements
    Set value of autofilled property to false in the following cases:
      - Cancel search via cancel button in <search> element.
      - Delete autofilled selected <select> option.
    
    This patch also
      - Merged test case for editing text in autofilled textarea.
    
    R=tkent@chromium.org
    BUG=395627
    
    Review URL: https://codereview.chromium.org/537393002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181589 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 17597906a42743b5f69e6692df9cbe58951f0a7c

--- third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLSelectElement.cpp
@@ -913,6 +913,8 @@ void HTMLSelectElement::optionRemoved(const HTMLOptionElement& option)
         m_activeSelectionAnchorIndex--;
     if (listIndex <= m_activeSelectionEndIndex)
         m_activeSelectionEndIndex--;
+    if (listIndex == selectedIndex())
+        setAutofilled(false);
 }
 
 void HTMLSelectElement::selectOption(int optionIndex, SelectOptionFlags flags)
