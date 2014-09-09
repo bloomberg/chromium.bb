bloomberg/chromium.bb

commit 8bee66d7dd682dcf14387c2650c39ba447d221c6

Start migrating .isolated handling code into isolated_format.py.
This change only move a few utility code, as it represents a large chunk of the
mecanical changes that needs to be done. Code will be moved afterward.

R=vadimsh@chromium.org
BUG=

Review URL: https://codereview.appspot.com/131610043

run_isolated.py
@@ -38,6 +38,7 @@ from utils import tools
 from utils import zip_package
 
 import auth
+import isolated_format
 import isolateserver
 
 
@@ -79,6 +80,7 @@ def get_as_zip_package(executable=True):
   assert BASE_DIR
   package = zip_package.ZipPackage(root=BASE_DIR)
   package.add_python_file(THIS_FILE_PATH, '__main__.py' if executable else None)
+  package.add_python_file(os.path.join(BASE_DIR, 'isolated_format.py'))
   package.add_python_file(os.path.join(BASE_DIR, 'isolateserver.py'))
   package.add_python_file(os.path.join(BASE_DIR, 'auth.py'))
   package.add_directory(os.path.join(BASE_DIR, 'third_party'))
@@ -896,7 +898,7 @@ def main(args):
 
   # |options.cache| path may not exist until DiskCache() instance is created.
   cache = DiskCache(
-      options.cache, policies, isolateserver.get_hash_algo(options.namespace))
+      options.cache, policies, isolated_format.get_hash_algo(options.namespace))
 
   remote = options.isolate_server or options.indir
   if file_path.is_url(remote):

isolate.py
@@ -26,6 +26,7 @@ import sys
 
 import auth
 import isolate_format
+import isolated_format
 import isolateserver
 import run_isolated
 import trace_inputs
@@ -381,7 +382,7 @@ def chromium_save_isolated(isolated, data, path_variables, algo):
     slavepath = isolated[:-len('.isolated')] + '.%d.isolated' % index
     tools.write_json(slavepath, f, True)
     data.setdefault('includes', []).append(
-        isolateserver.hash_file(slavepath, algo))
+        isolated_format.hash_file(slavepath, algo))
     files.append(os.path.basename(slavepath))
 
   files.extend(isolateserver.save_isolated(isolated, data))
@@ -485,7 +486,7 @@ class SavedState(Flattenable):
   # Bump this version whenever the saved state changes. It is also keyed on the
   # .isolated file version so any change in the generator will invalidate .state
   # files.
-  EXPECTED_VERSION = isolateserver.ISOLATED_FILE_VERSION + '.2'
+  EXPECTED_VERSION = isolated_format.ISOLATED_FILE_VERSION + '.2'
 
   def __init__(self, isolated_basedir):
     """Creates an empty SavedState.
@@ -501,7 +502,7 @@ class SavedState(Flattenable):
 
     # The default algorithm used.
     self.OS = sys.platform
-    self.algo = isolateserver.SUPPORTED_ALGOS['sha-1']
+    self.algo = isolated_format.SUPPORTED_ALGOS['sha-1']
     self.child_isolated_files = []
     self.command = []
     self.config_variables = {}
@@ -565,12 +566,12 @@ class SavedState(Flattenable):
       return dict((k, data[k]) for k in ('h', 'l', 'm', 's') if k in data)
 
     out = {
-      'algo': isolateserver.SUPPORTED_ALGOS_REVERSE[self.algo],
+      'algo': isolated_format.SUPPORTED_ALGOS_REVERSE[self.algo],
       'files': dict(
           (filepath, strip(data)) for filepath, data in self.files.iteritems()),
       # The version of the .state file is different than the one of the
       # .isolated file.
-      'version': isolateserver.ISOLATED_FILE_VERSION,
+      'version': isolated_format.ISOLATED_FILE_VERSION,
     }
     if self.command:
       out['command'] = self.command
@@ -600,9 +601,9 @@ class SavedState(Flattenable):
 
     # Converts human readable form back into the proper class type.
     algo = data.get('algo')
-    if not algo in isolateserver.SUPPORTED_ALGOS:
+    if not algo in isolated_format.SUPPORTED_ALGOS:
       raise isolateserver.ConfigError('Unknown algo \'%s\'' % out.algo)
-    out.algo = isolateserver.SUPPORTED_ALGOS[algo]
+    out.algo = isolated_format.SUPPORTED_ALGOS[algo]
 
     # Refuse the load non-exact version, even minor difference. This is unlike
     # isolateserver.load_isolated(). This is because .isolated.state could have
@@ -627,7 +628,7 @@ class SavedState(Flattenable):
   def flatten(self):
     """Makes sure 'algo' is in human readable form."""
     out = super(SavedState, self).flatten()
-    out['algo'] = isolateserver.SUPPORTED_ALGOS_REVERSE[out['algo']]
+    out['algo'] = isolated_format.SUPPORTED_ALGOS_REVERSE[out['algo']]
     return out
 
   def __str__(self):
@@ -1014,7 +1015,7 @@ def prepare_for_archival(options, cwd):
   for item in isolated_files:
     item_path = os.path.join(
         os.path.dirname(complete_state.isolated_filepath), item)
-    # Do not use isolateserver.hash_file() here because the file is
+    # Do not use isolated_format.hash_file() here because the file is
     # likely smallish (under 500kb) and its file size is needed.
     with open(item_path, 'rb') as f:
       content = f.read()

isolated_format.py
@@ -1,0 +1,56 @@
+# Copyright 2014 The Swarming Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0 that
+# can be found in the LICENSE file.
+
+"""Understands .isolated files and can do local operations on them."""
+
+import hashlib
+import re
+
+
+# Version stored and expected in .isolated files.
+ISOLATED_FILE_VERSION = '1.4'
+
+
+# Chunk size to use when doing disk I/O.
+DISK_FILE_CHUNK = 1024 * 1024
+
+
+# Sadly, hashlib uses 'sha1' instead of the standard 'sha-1' so explicitly
+# specify the names here.
+SUPPORTED_ALGOS = {
+  'md5': hashlib.md5,
+  'sha-1': hashlib.sha1,
+  'sha-512': hashlib.sha512,
+}
+
+
+# Used for serialization.
+SUPPORTED_ALGOS_REVERSE = dict((v, k) for k, v in SUPPORTED_ALGOS.iteritems())
+
+
+def is_valid_hash(value, algo):
+  """Returns if the value is a valid hash for the corresponding algorithm."""
+  size = 2 * algo().digest_size
+  return bool(re.match(r'^[a-fA-F0-9]{%d}$' % size, value))
+
+
+def get_hash_algo(_namespace):
+  """Return hash algorithm class to use when uploading to given |namespace|."""
+  # TODO(vadimsh): Implement this at some point.
+  return hashlib.sha1
+
+
+def hash_file(filepath, algo):
+  """Calculates the hash of a file without reading it all in memory at once.
+
+  |algo| should be one of hashlib hashing algorithm.
+  """
+  digest = algo()
+  with open(filepath, 'rb') as f:
+    while True:
+      chunk = f.read(DISK_FILE_CHUNK)
+      if not chunk:
+        break
+      digest.update(chunk)
+  return digest.hexdigest()

isolateserver.py
@@ -8,7 +8,6 @@
 __version__ = '0.3.4'
 
 import functools
-import hashlib
 import json
 import logging
 import os
@@ -34,12 +33,11 @@ from utils import threading_utils
 from utils import tools
 
 import auth
+import isolated_format
 
 
 # Version of isolate protocol passed to the server in /handshake request.
 ISOLATE_PROTOCOL_VERSION = '1.0'
-# Version stored and expected in .isolated files.
-ISOLATED_FILE_VERSION = '1.4'
 
 
 # The number of files to check the isolate server per /pre-upload query.
@@ -69,9 +67,6 @@ ALREADY_COMPRESSED_TYPES = [
 UNKNOWN_FILE_SIZE = None
 
 
-# Chunk size to use when doing disk I/O.
-DISK_FILE_CHUNK = 1024 * 1024
-
 # Chunk size to use when reading from network stream.
 NET_IO_FILE_CHUNK = 16 * 1024
 
@@ -90,19 +85,6 @@ DEADLOCK_TIMEOUT = 5 * 60
 # the required files. This is intended to let the user (or buildbot) know that
 # the program is still running.
 DELAY_BETWEEN_UPDATES_IN_SECS = 30
-
-
-# Sadly, hashlib uses 'sha1' instead of the standard 'sha-1' so explicitly
-# specify the names here.
-SUPPORTED_ALGOS = {
-  'md5': hashlib.md5,
-  'sha-1': hashlib.sha1,
-  'sha-512': hashlib.sha512,
-}
-
-
-# Used for serialization.
-SUPPORTED_ALGOS_REVERSE = dict((v, k) for k, v in SUPPORTED_ALGOS.iteritems())
 
 
 DEFAULT_BLACKLIST = (
@@ -135,27 +117,6 @@ class MappingError(OSError):
   pass
 
 
-def is_valid_hash(value, algo):
-  """Returns if the value is a valid hash for the corresponding algorithm."""
-  size = 2 * algo().digest_size
-  return bool(re.match(r'^[a-fA-F0-9]{%d}$' % size, value))
-
-
-def hash_file(filepath, algo):
-  """Calculates the hash of a file without reading it all in memory at once.
-
-  |algo| should be one of hashlib hashing algorithm.
-  """
-  digest = algo()
-  with open(filepath, 'rb') as f:
-    while True:
-      chunk = f.read(DISK_FILE_CHUNK)
-      if not chunk:
-        break
-      digest.update(chunk)
-  return digest.hexdigest()
-
-
 def stream_read(stream, chunk_size):
   """Reads chunks from |stream| and yields them."""
   while True:
@@ -165,7 +126,7 @@ def stream_read(stream, chunk_size):
     yield data
 
 
-def file_read(filepath, chunk_size=DISK_FILE_CHUNK, offset=0):
+def file_read(filepath, chunk_size=isolated_format.DISK_FILE_CHUNK, offset=0):
   """Yields file content in chunks of |chunk_size| starting from |offset|."""
   with open(filepath, 'rb') as f:
     if offset:
@@ -209,7 +170,8 @@ def zip_compress(content_generator, level=7):
     yield tail
 
 
-def zip_decompress(content_generator, chunk_size=DISK_FILE_CHUNK):
+def zip_decompress(
+    content_generator, chunk_size=isolated_format.DISK_FILE_CHUNK):
   """Reads zipped data from |content_generator| and yields decompressed data.
 
   Decompresses data in small chunks (no larger than |chunk_size|) so that
@@ -396,7 +358,7 @@ class Storage(object):
   def __init__(self, storage_api):
     self._storage_api = storage_api
     self._use_zip = is_namespace_with_compression(storage_api.namespace)
-    self._hash_algo = get_hash_algo(storage_api.namespace)
+    self._hash_algo = isolated_format.get_hash_algo(storage_api.namespace)
     self._cpu_thread_pool = None
     self._net_thread_pool = None
 
@@ -404,7 +366,7 @@ class Storage(object):
   def hash_algo(self):
     """Hashing algorithm used to name files in storage based on their content.
 
-    Defined by |namespace|. See also 'get_hash_algo'.
+    Defined by |namespace|. See also isolated_format.get_hash_algo().
     """
     return self._hash_algo
 
@@ -633,7 +595,7 @@ class Storage(object):
         # Prepare reading pipeline.
         stream = self._storage_api.fetch(digest)
         if self._use_zip:
-          stream = zip_decompress(stream, DISK_FILE_CHUNK)
+          stream = zip_decompress(stream, isolated_format.DISK_FILE_CHUNK)
         # Run |stream| through verifier that will assert its size.
         verifier = FetchStreamVerifier(stream, size)
         # Verified stream goes to |sink|.
@@ -1333,12 +1295,6 @@ class MemoryCache(LocalCache):
       os.chmod(dest, file_mode & self._file_mode_mask)
 
 
-def get_hash_algo(_namespace):
-  """Return hash algorithm class to use when uploading to given |namespace|."""
-  # TODO(vadimsh): Implement this at some point.
-  return hashlib.sha1
-
-
 def is_namespace_with_compression(namespace):
   """Returns True if given |namespace| stores compressed objects."""
   return namespace.endswith(('-gzip', '-deflate'))
@@ -1607,7 +1563,7 @@ def process_input(filepath, prevdict, read_only, algo):
       # Reuse the previous hash if available.
       out['h'] = prevdict.get('h')
     if not out.get('h'):
-      out['h'] = hash_file(filepath, algo)
+      out['h'] = isolated_format.hash_file(filepath, algo)
   else:
     # If the timestamp wasn't updated, carry on the link destination.
     if prevdict.get('t') == out['t']:
@@ -1635,7 +1591,7 @@ def save_isolated(isolated, data):
   Returns the list of child isolated files that are included by |isolated|.
   """
   # Make sure the data is valid .isolated data by 'reloading' it.
-  algo = SUPPORTED_ALGOS[data['algo']]
+  algo = isolated_format.SUPPORTED_ALGOS[data['algo']]
   load_isolated(json.dumps(data), algo)
   tools.write_json(isolated, data, True)
   return []
@@ -1698,30 +1654,32 @@ def load_isolated(content, algo):
   except ValueError:
     raise ConfigError('Expected valid version, got %r' % value)
 
-  expected_version = tuple(map(int, ISOLATED_FILE_VERSION.split('.')))
+  expected_version = tuple(
+      map(int, isolated_format.ISOLATED_FILE_VERSION.split('.')))
   # Major version must match.
   if version[0] != expected_version[0]:
     raise ConfigError(
         'Expected compatible \'%s\' version, got %r' %
-        (ISOLATED_FILE_VERSION, value))
+        (isolated_format.ISOLATED_FILE_VERSION, value))
 
   if algo is None:
     # TODO(maruel): Remove the default around Jan 2014.
     # Default the algorithm used in the .isolated file itself, falls back to
     # 'sha-1' if unspecified.
-    algo = SUPPORTED_ALGOS_REVERSE[data.get('algo', 'sha-1')]
+    algo = isolated_format.SUPPORTED_ALGOS_REVERSE[data.get('algo', 'sha-1')]
 
   for key, value in data.iteritems():
     if key == 'algo':
       if not isinstance(value, basestring):
         raise ConfigError('Expected string, got %r' % value)
-      if value not in SUPPORTED_ALGOS:
+      if value not in isolated_format.SUPPORTED_ALGOS:
         raise ConfigError(
             'Expected one of \'%s\', got %r' %
-            (', '.join(sorted(SUPPORTED_ALGOS)), value))
-      if value != SUPPORTED_ALGOS_REVERSE[algo]:
+            (', '.join(sorted(isolated_format.SUPPORTED_ALGOS)), value))
+      if value != isolated_format.SUPPORTED_ALGOS_REVERSE[algo]:
         raise ConfigError(
-            'Expected \'%s\', got %r' % (SUPPORTED_ALGOS_REVERSE[algo], value))
+            'Expected \'%s\', got %r' %
+            (isolated_format.SUPPORTED_ALGOS_REVERSE[algo], value))
 
     elif key == 'command':
       if not isinstance(value, list):
@@ -1748,7 +1706,7 @@ def load_isolated(content, algo):
             if not isinstance(subsubvalue, int):
               raise ConfigError('Expected int, got %r' % subsubvalue)
           elif subsubkey == 'h':
-            if not is_valid_hash(subsubvalue, algo):
+            if not isolated_format.is_valid_hash(subsubvalue, algo):
               raise ConfigError('Expected sha-1, got %r' % subsubvalue)
           elif subsubkey == 's':
             if not isinstance(subsubvalue, (int, long)):
@@ -1778,7 +1736,7 @@ def load_isolated(content, algo):
       if not value:
         raise ConfigError('Expected non-empty includes list')
       for subvalue in value:
-        if not is_valid_hash(subvalue, algo):
+        if not isolated_format.is_valid_hash(subvalue, algo):
           raise ConfigError('Expected sha-1, got %r' % subvalue)
 
     elif key == 'os':
@@ -1996,7 +1954,7 @@ def fetch_isolated(isolated_hash, storage, cache, outdir, require_command):
 
     with tools.Profiler('GetIsolateds'):
       # Optionally support local files by manually adding them to cache.
-      if not is_valid_hash(isolated_hash, algo):
+      if not isolated_format.is_valid_hash(isolated_hash, algo):
         logging.debug('%s is not a valid hash, assuming a file', isolated_hash)
         try:
           isolated_hash = fetch_queue.inject_local_file(isolated_hash, algo)
@@ -2115,12 +2073,13 @@ def archive_files_to_storage(storage, files, blacklist):
           handle, isolated = tempfile.mkstemp(dir=tempdir, suffix='.isolated')
           os.close(handle)
           data = {
-              'algo': SUPPORTED_ALGOS_REVERSE[storage.hash_algo],
+              'algo':
+                  isolated_format.SUPPORTED_ALGOS_REVERSE[storage.hash_algo],
               'files': metadata,
-              'version': ISOLATED_FILE_VERSION,
+              'version': isolated_format.ISOLATED_FILE_VERSION,
           }
           save_isolated(isolated, data)
-          h = hash_file(isolated, storage.hash_algo)
+          h = isolated_format.hash_file(isolated, storage.hash_algo)
           items_to_upload.extend(items)
           items_to_upload.append(
               FileItem(
@@ -2131,7 +2090,7 @@ def archive_files_to_storage(storage, files, blacklist):
           results.append((h, f))
 
         elif os.path.isfile(filepath):
-          h = hash_file(filepath, storage.hash_algo)
+          h = isolated_format.hash_file(filepath, storage.hash_algo)
           items_to_upload.append(
             FileItem(
                 path=filepath,

swarming.py
@@ -33,6 +33,7 @@ from utils import tools
 from utils import zip_package
 
 import auth
+import isolated_format
 import isolateserver
 import run_isolated
 
@@ -555,7 +556,7 @@ def archive(isolate_server, namespace, isolated, algo, verbose):
     logging.info(' '.join(cmd))
     if subprocess.call(cmd, verbose):
       return
-    return isolateserver.hash_file(isolated, algo)
+    return isolated_format.hash_file(isolated, algo)
   finally:
     if tempdir:
       shutil.rmtree(tempdir)
@@ -717,7 +718,7 @@ def isolated_to_hash(isolate_server, namespace, arg, algo, verbose):
       on_error.report('Archival failure %s' % arg)
       return None, True
     return file_hash, True
-  elif isolateserver.is_valid_hash(arg, algo):
+  elif isolated_format.is_valid_hash(arg, algo):
     return arg, False
   else:
     on_error.report('Invalid hash %s' % arg)

tests/isolate_smoke_test.py
@@ -20,7 +20,7 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, ROOT_DIR)
 
 import isolate
-import isolateserver
+import isolated_format
 from utils import file_path
 import trace_test_util
 # Create shortcuts.
@@ -237,7 +237,7 @@ class IsolateTempdir(unittest.TestCase):
         # Upgrade the value to unicode so diffing the structure in case of
         # test failure is easier, since the basestring type must match,
         # str!=unicode.
-        v[u'h'] = unicode(isolateserver.hash_file(filepath, ALGO))
+        v[u'h'] = unicode(isolated_format.hash_file(filepath, ALGO))
 
     if empty_file:
       item = files[empty_file]
@@ -256,7 +256,7 @@ class IsolateTempdir(unittest.TestCase):
       u'algo': u'sha-1',
       u'files': self._gen_files(read_only, empty_file, False),
       u'relative_cwd': unicode(RELATIVE_CWD[self.case()]),
-      u'version': unicode(isolate.isolateserver.ISOLATED_FILE_VERSION),
+      u'version': unicode(isolated_format.ISOLATED_FILE_VERSION),
     }
     if read_only is not None:
       expected[u'read_only'] = read_only
@@ -497,7 +497,7 @@ class Isolate_hashtable(IsolateOutdir):
       for v in self._gen_files(False, empty_file, False).itervalues()
     ]
     expected.append(
-        unicode(isolateserver.hash_file(self.isolated, ALGO)))
+        unicode(isolated_format.hash_file(self.isolated, ALGO)))
     return expected
 
   def _expected_hash_tree(self, empty_file):
@@ -521,7 +521,7 @@ class Isolate_hashtable(IsolateOutdir):
     out = self._test_all_items_invalid('hashtable')
     expected = (
         '%s  isolate_smoke_test.isolated\n' %
-        isolateserver.hash_file(self.isolated, ALGO))
+        isolated_format.hash_file(self.isolated, ALGO))
     self.assertEqual(expected, out)
     self._expected_hash_tree(None)
 
@@ -544,8 +544,8 @@ class Isolate_hashtable(IsolateOutdir):
     tree = self._gen_expected_tree(None)
     isolated_base = self.isolated[:-len('.isolated')]
     isolated_hashes = [
-      unicode(isolateserver.hash_file(isolated_base + '.0.isolated', ALGO)),
-      unicode(isolateserver.hash_file(isolated_base + '.1.isolated', ALGO)),
+      unicode(isolated_format.hash_file(isolated_base + '.0.isolated', ALGO)),
+      unicode(isolated_format.hash_file(isolated_base + '.1.isolated', ALGO)),
     ]
     tree.extend(isolated_hashes)
     self.assertEqual(sorted(tree), map(unicode, self._result_tree()))
@@ -558,7 +558,7 @@ class Isolate_hashtable(IsolateOutdir):
       u'files': {u'split.py': files['split.py']},
       u'includes': isolated_hashes,
       u'relative_cwd': unicode(RELATIVE_CWD[self.case()]),
-      u'version': unicode(isolate.isolateserver.ISOLATED_FILE_VERSION),
+      u'version': unicode(isolated_format.ISOLATED_FILE_VERSION),
     }
     self.assertEqual(expected, json.load(open(self.isolated, 'r')))
 
@@ -566,7 +566,7 @@ class Isolate_hashtable(IsolateOutdir):
     expected = {
       u'algo': u'sha-1',
       u'files': {key: files[key]},
-      u'version': unicode(isolate.isolateserver.ISOLATED_FILE_VERSION),
+      u'version': unicode(isolated_format.ISOLATED_FILE_VERSION),
     }
     self.assertEqual(
         expected, json.load(open(isolated_base + '.0.isolated', 'r')))
@@ -575,7 +575,7 @@ class Isolate_hashtable(IsolateOutdir):
     expected = {
       u'algo': u'sha-1',
       u'files': {key: files[key]},
-      u'version': unicode(isolate.isolateserver.ISOLATED_FILE_VERSION),
+      u'version': unicode(isolated_format.ISOLATED_FILE_VERSION),
     }
     self.assertEqual(
         expected, json.load(open(isolated_base + '.1.isolated', 'r')))
@@ -610,7 +610,7 @@ class Isolate_hashtable(IsolateOutdir):
         str(v['h'])
         for v in self._gen_files(False, None, False).itervalues() if 'h' in v
       ]
-      expected.append(isolateserver.hash_file(self.isolated, ALGO))
+      expected.append(isolated_format.hash_file(self.isolated, ALGO))
       self.assertEqual(sorted(expected), self._result_tree())
       self._expect_results(['symlink_full.py'], None, None, None)
 
@@ -621,7 +621,7 @@ class Isolate_hashtable(IsolateOutdir):
         str(v['h'])
         for v in self._gen_files(False, None, False).itervalues() if 'h' in v
       ]
-      expected.append(isolateserver.hash_file(self.isolated, ALGO))
+      expected.append(isolated_format.hash_file(self.isolated, ALGO))
       self.assertEqual(sorted(expected), self._result_tree())
       self._expect_results(['symlink_partial.py'], None, None, None)
 
@@ -633,7 +633,7 @@ class Isolate_hashtable(IsolateOutdir):
         str(v['h'])
         for v in self._gen_files(False, None, False).itervalues() if 'h' in v
       ]
-      expected.append(isolateserver.hash_file(self.isolated, ALGO))
+      expected.append(isolated_format.hash_file(self.isolated, ALGO))
       self.assertEqual(sorted(expected), self._result_tree())
       self._expect_results(['symlink_outside_build_root.py'], None, None, None)
 

tests/isolate_test.py
@@ -19,6 +19,7 @@ sys.path.insert(0, os.path.join(ROOT_DIR, 'third_party'))
 
 from depot_tools import auto_stub
 import isolate
+import isolated_format
 from utils import file_path
 from utils import tools
 
@@ -31,7 +32,7 @@ def _size(*args):
 
 
 def hash_file(*args):
-  return isolate.isolateserver.hash_file(os.path.join(ROOT_DIR, *args), ALGO)
+  return isolated_format.hash_file(os.path.join(ROOT_DIR, *args), ALGO)
 
 
 class IsolateBase(auto_stub.TestCase):
@@ -260,7 +261,7 @@ class IsolateLoad(IsolateBase):
         },
       },
       'relative_cwd': os.path.join(u'tests', 'isolate'),
-      'version': isolate.isolateserver.ISOLATED_FILE_VERSION,
+      'version': isolated_format.ISOLATED_FILE_VERSION,
     }
     self._cleanup_isolated(expected_isolated)
     self.assertEqual(expected_isolated, actual_isolated)
@@ -323,7 +324,7 @@ class IsolateLoad(IsolateBase):
         },
       },
       'relative_cwd': os.path.join(u'tests', 'isolate'),
-      'version': isolate.isolateserver.ISOLATED_FILE_VERSION,
+      'version': isolated_format.ISOLATED_FILE_VERSION,
     }
     self._cleanup_isolated(expected_isolated)
     self.assertEqual(expected_isolated, actual_isolated)
@@ -386,7 +387,7 @@ class IsolateLoad(IsolateBase):
         },
       },
       'relative_cwd': os.path.join(u'tests', 'isolate'),
-      'version': isolate.isolateserver.ISOLATED_FILE_VERSION,
+      'version': isolated_format.ISOLATED_FILE_VERSION,
     }
     self._cleanup_isolated(expected_isolated)
     self.assertEqual(expected_isolated, actual_isolated)
@@ -469,7 +470,7 @@ class IsolateLoad(IsolateBase):
         },
       },
       'relative_cwd': os.path.join(u'tests', 'isolate'),
-      'version': isolate.isolateserver.ISOLATED_FILE_VERSION,
+      'version': isolated_format.ISOLATED_FILE_VERSION,
     }
     self._cleanup_isolated(expected_isolated)
     self.assertEqual(expected_isolated, actual_isolated)
@@ -558,7 +559,7 @@ class IsolateLoad(IsolateBase):
         },
       },
       'relative_cwd': os.path.join(u'tests', 'isolate'),
-      'version': isolate.isolateserver.ISOLATED_FILE_VERSION,
+      'version': isolated_format.ISOLATED_FILE_VERSION,
     }
     self._cleanup_isolated(expected_isolated)
     self.assertEqual(expected_isolated, actual_isolated)
@@ -646,7 +647,7 @@ class IsolateLoad(IsolateBase):
         unicode(hash_file(os.path.join(self.directory, 'foo.1.isolated'))),
       ],
       u'relative_cwd': u'.',
-      u'version': unicode(isolate.isolateserver.ISOLATED_FILE_VERSION),
+      u'version': unicode(isolated_format.ISOLATED_FILE_VERSION),
     }
     self._cleanup_isolated(expected_isolated_master)
     self.assertEqual(expected_isolated_master, actual_isolated_master)
@@ -663,7 +664,7 @@ class IsolateLoad(IsolateBase):
           u's': _size('tests', 'isolate', 'test', 'data', 'foo.txt'),
         },
       },
-      u'version': unicode(isolate.isolateserver.ISOLATED_FILE_VERSION),
+      u'version': unicode(isolated_format.ISOLATED_FILE_VERSION),
     }
     self._cleanup_isolated(expected_isolated_0)
     self.assertEqual(expected_isolated_0, actual_isolated_0)
@@ -680,7 +681,7 @@ class IsolateLoad(IsolateBase):
           u's': _size('tests', 'isolate', 'files1', 'subdir', '42.txt'),
         },
       },
-      u'version': unicode(isolate.isolateserver.ISOLATED_FILE_VERSION),
+      u'version': unicode(isolated_format.ISOLATED_FILE_VERSION),
     }
     self._cleanup_isolated(expected_isolated_1)
     self.assertEqual(expected_isolated_1, actual_isolated_1)
@@ -865,7 +866,7 @@ class IsolateLoad(IsolateBase):
         'command': command,
         'files': {unicode(f):{} for f in expected_files},
         'relative_cwd': relative_cwd,
-        'version': isolate.isolateserver.ISOLATED_FILE_VERSION,
+        'version': isolated_format.ISOLATED_FILE_VERSION,
       }
       self.assertEqual(expected, c.saved_state.to_isolated())
 
@@ -1060,7 +1061,7 @@ class IsolateLoad(IsolateBase):
           for f in expected_files
         },
         'relative_cwd': relative_cwd,
-        'version': isolate.isolateserver.ISOLATED_FILE_VERSION,
+        'version': isolated_format.ISOLATED_FILE_VERSION,
       }
       self.assertEqual(expected, c.saved_state.to_isolated())
 
@@ -1230,7 +1231,7 @@ class IsolateCommand(IsolateBase):
     expected_isolated = (
         '{"algo":"sha-1","command":["foo"],"files":{},'
         '"relative_cwd":".","version":"%s"}'
-    ) % isolate.isolateserver.ISOLATED_FILE_VERSION
+    ) % isolated_format.ISOLATED_FILE_VERSION
     self.assertEqual(expected_isolated, actual_isolated)
     isolated_data = json.loads(actual_isolated)
 

tests/isolated_format_test.py
@@ -1,0 +1,33 @@
+#!/usr/bin/env python
+# Copyright 2014 The Swarming Authors. All rights reserved.
+# Use of this source code is governed under the Apache License, Version 2.0 that
+# can be found in the LICENSE file.
+
+import hashlib
+import logging
+import sys
+import unittest
+
+# net_utils adjusts sys.path.
+import net_utils
+
+import isolated_format
+
+
+ALGO = hashlib.sha1
+
+
+class TestCase(net_utils.TestCase):
+  def test_get_hash_algo(self):
+    # Tests here assume ALGO is used for default namespaces, check this
+    # assumption.
+    self.assertIs(isolated_format.get_hash_algo('default'), ALGO)
+    self.assertIs(isolated_format.get_hash_algo('default-gzip'), ALGO)
+
+
+if __name__ == '__main__':
+  if '-v' in sys.argv:
+    unittest.TestCase.maxDiff = None
+  logging.basicConfig(
+      level=(logging.DEBUG if '-v' in sys.argv else logging.ERROR))
+  unittest.main()

commit 14b5fd823e2a3f0ac702ae297c8315f55820914c

Fix regression introduced in 8bee66d7dd682dcf14387c2650c39ba447d221c6.
When the function was moved, one reference was not upgraded.

R=vadimsh@chromium.org
BUG=

Review URL: https://codereview.appspot.com/137340044

run_isolated.py
@@ -572,7 +572,7 @@ class DiskCache(isolateserver.LocalCache):
         previous.remove(filename)
         continue
       # An untracked file.
-      if not isolateserver.is_valid_hash(filename, self.hash_algo):
+      if not isolated_format.is_valid_hash(filename, self.hash_algo):
         logging.warning('Removing unknown file %s from cache', filename)
         try_remove(self._path(filename))
         continue