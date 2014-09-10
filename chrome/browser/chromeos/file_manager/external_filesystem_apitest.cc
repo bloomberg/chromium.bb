48c36993769d2e2d6b356fc4a626188d78ab3603
satorux <satorux@chromium.org>
2014-09-10 01:52:31 -0700

    Fix an outdated comment in external_filesystem_apitest.cc
    TEST=n/a
    BUG=none
    
    Review URL: https://codereview.chromium.org/560753002
    
    Cr-Commit-Position: refs/heads/master@{#294145}
    
    Former-commit-id: 6f3fd4fc5df531ca96e701b9a722b133b24f4430

--- chrome/browser/chromeos/file_manager/external_filesystem_apitest.cc
+++ chrome/browser/chromeos/file_manager/external_filesystem_apitest.cc
@@ -44,8 +44,7 @@
 //
 // The tests cover following scenarios:
 // - Performing file system operations on external file systems from an
-//   extension with fileManagerPrivate permission (i.e. a file browser
-//   extension).
+//   app with fileManagerPrivate permission (i.e. Files.app).
 // - Performing read/write operations from file handler extensions. These
 //   extensions need a file browser extension to give them permissions to access
 //   files. This also includes file handler extensions in filesystem API.
