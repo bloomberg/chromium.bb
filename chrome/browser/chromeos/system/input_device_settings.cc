4fd47b8466e29c2db0c5792f7c976022634ad5c8
adlr <adlr@chromium.org>
2014-09-09 17:58:21 -0700

    Chrome OS Touchpad Settings: Always update natural scrolling pref.
    Natural (aka australian) scrolling occurs outside Chrome in the X11
    touchpad driver. Chrome dictates via an inputcontrol script what the
    value (true/false) should be. The script notes (in the user's homedir)
    what the last set value is so that added devices get it applied.
    
    Unfortunately, what was happening was Chrome would update the pref
    before the user logged in, so the inputcontrol script couldn't make a
    note. Later, when a user was logged in and a new device was attached,
    the script wouldn't know if australian scrolling should be on or off.
    
    To fix, we always set australian scrolling whenever we consider
    sending touchpad value changes to the inputcontrol script.
    
    BUG=chromium:406480
    TEST=Tested that when user logs in, chrome always sends the current
    value of natural scrolling to the inputcontrol script. Also tested
    that changing non-mouse/tp settings doesn't trigger calls to
    inputcontrol script.
    
    Review URL: https://codereview.chromium.org/553153004
    
    Cr-Commit-Position: refs/heads/master@{#294058}
    
    Former-commit-id: dc6bae3bdc5154b73600a1180c0a82859b271a3f

--- chrome/browser/chromeos/system/input_device_settings.cc
+++ chrome/browser/chromeos/system/input_device_settings.cc
@@ -353,7 +353,10 @@ bool TouchpadSettings::Update(const TouchpadSettings& settings,
     if (argv)
       AddTPControlArguments("tapdrag", tap_dragging_.value(), argv);
   }
-  if (natural_scroll_.Update(settings.natural_scroll_)) {
+  natural_scroll_.Update(settings.natural_scroll_);
+  // Always send natural scrolling to the shell command, as a workaround.
+  // See crbug.com/406480
+  if (natural_scroll_.is_set()) {
     updated = true;
     if (argv)
       AddTPControlArguments("australian_scrolling", natural_scroll_.value(),
