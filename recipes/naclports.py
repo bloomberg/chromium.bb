0bd95bcdc54db940ab51cd687a2211521c8ffa1f
bradnelson@google.com <bradnelson@google.com>
2014-09-10 01:25:38 +0000

    Switching off svn in naclports fetch recipe.
    BUG=https://code.google.com/p/chromium/issues/detail?id=409185
    TEST=local
    R=iannucci@chromium.org
    
    Review URL: https://codereview.chromium.org/555383002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@291900 0039d316-1c4b-4281-b951-d872f2087c98

--- recipes/naclports.py
+++ recipes/naclports.py
@@ -24,23 +24,14 @@ class Naclports(recipe_util.Recipe):
     }
     spec = {
       'solutions': [solution],
-      'svn_url': 'https://naclports.googlecode.com/svn',
-      'svn_branch': 'trunk/src',
-      'svn_ref': 'master',
     }
-    if props.get('submodule_git_svn_spec'):
-      spec['submodule_git_svn_spec'] = props['submodule_git_svn_spec']
     if props.get('target_os'):
       spec['target_os'] = props['target_os'].split(',')
     if props.get('target_os_only'):
       spec['target_os_only'] = props['target_os_only']
-    checkout_type = 'gclient_git_svn'
-    if props.get('nosvn'):
-      checkout_type = 'gclient_git'
-    spec_type = '%s_spec' % checkout_type
     return {
-      'type': checkout_type,
-      spec_type: spec,
+      'type': 'gclient_git',
+      'gclient_git_spec': spec,
     }
 
   @staticmethod
