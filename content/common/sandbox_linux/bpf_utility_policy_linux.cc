c8038f3a581ce339df3785767b98bd3f76161493
mdempsky <mdempsky@chromium.org>
2014-09-11 19:44:46 -0700

    Trim utility process sandbox down
    BUG=325893
    
    Review URL: https://codereview.chromium.org/356283002
    
    Cr-Commit-Position: refs/heads/master@{#294532}
    
    Former-commit-id: c6d19cf91bc709f1fa52b4e9e4a44dc4eb5562be

--- content/common/sandbox_linux/bpf_utility_policy_linux.cc
+++ content/common/sandbox_linux/bpf_utility_policy_linux.cc
@@ -37,34 +37,20 @@ ResultExpr UtilityProcessPolicy::EvaluateSyscall(int sysno) const {
     case __NR_ioctl:
       return sandbox::RestrictIoctl();
     // Allow the system calls below.
-    // The baseline policy allows __NR_clock_gettime. Allow
-    // clock_getres() for V8. crbug.com/329053.
-    case __NR_clock_getres:
     case __NR_fdatasync:
     case __NR_fsync:
-    case __NR_getpriority:
 #if defined(__i386__) || defined(__x86_64__)
     case __NR_getrlimit:
 #endif
 #if defined(__i386__) || defined(__arm__)
     case __NR_ugetrlimit:
 #endif
-    case __NR_mremap:  // See crbug.com/149834.
     case __NR_pread64:
     case __NR_pwrite64:
-    case __NR_sched_getaffinity:
-    case __NR_sched_get_priority_max:
-    case __NR_sched_get_priority_min:
-    case __NR_sched_getparam:
-    case __NR_sched_getscheduler:
-    case __NR_sched_setscheduler:
-    case __NR_setpriority:
     case __NR_sysinfo:
     case __NR_times:
     case __NR_uname:
       return Allow();
-    case __NR_prlimit64:
-      return Error(EPERM);  // See crbug.com/160157.
     default:
       // Default on the content baseline policy.
       return SandboxBPFBasePolicy::EvaluateSyscall(sysno);
