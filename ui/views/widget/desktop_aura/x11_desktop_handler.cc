33bce00c17c865339be22fcfb18e56b155f87a34
joleksy <joleksy@opera.com>
2014-09-11 23:36:20 -0700

    Handle _NET_ACTIVE_WINDOW or FocusIn & FocusOut, not both.
    When WM supports _NET_ACTIVE_WINDOW notification, we get OnActiveWindowChanged() from two places:
     - X11DesktopHandler::ProcessXEvent (FocusIn)
     - X11DesktopHandler::DispatchEvent (PropertyNotify)
    
    One is enough, two can lead to confusion. FocusIn/Out should not be handled if WM supports active window.
    
    BUG=None
    TEST=None
    
    Review URL: https://codereview.chromium.org/549713003
    
    Cr-Commit-Position: refs/heads/master@{#294550}
    
    Former-commit-id: deadc732ad03f3907012dce61923ef9ff26baa3b

--- ui/views/widget/desktop_aura/x11_desktop_handler.cc
+++ ui/views/widget/desktop_aura/x11_desktop_handler.cc
@@ -134,11 +134,13 @@ bool X11DesktopHandler::IsActiveWindow(::Window window) const {
 void X11DesktopHandler::ProcessXEvent(XEvent* event) {
   switch (event->type) {
     case FocusIn:
-      if (current_window_ != event->xfocus.window)
+      if (!wm_supports_active_window_ &&
+          current_window_ != event->xfocus.window)
         OnActiveWindowChanged(event->xfocus.window, ACTIVE);
       break;
     case FocusOut:
-      if (current_window_ == event->xfocus.window)
+      if (!wm_supports_active_window_ &&
+          current_window_ == event->xfocus.window)
         OnActiveWindowChanged(None, NOT_ACTIVE);
       break;
     default:
