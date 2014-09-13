5a64a2fccf7c38b8bee1d0204ea2cb2a59665084
jbroman <jbroman@chromium.org>
2014-09-13 14:15:54 -0700

    Telemetry: Fix exact_matches in telemetry.test_runner.
    Previously, if exact_matches was True but there were no exact matches,
    it would return fuzzy matches instead. This is not what the caller asked
    for.
    
    GPU test names are changed to match the buildbot config, since they were
    previously relying on fuzzy matching.
    
    SHERIFFS: May cause errors like:
    
      No test named "some_test_name".
    
      Available tests are:
        ...
    
    If so, this CL can be safely reverted.
    
    BUG=413334,413442
    
    Review URL: https://codereview.chromium.org/560153004
    
    Cr-Commit-Position: refs/heads/master@{#294757}
    
    Former-commit-id: 566ed9f6e71cea313c88e8f259eca76581b72d67

--- content/test/gpu/gpu_tests/pixel.py
+++ content/test/gpu/gpu_tests/pixel.py
@@ -148,7 +148,6 @@ class _PixelValidator(cloud_storage_test_base.ValidatorBase):
 
 class Pixel(cloud_storage_test_base.TestBase):
   test = _PixelValidator
-  page_set = page_sets.PixelTestsPageSet
 
   @classmethod
   def AddTestCommandLineArgs(cls, group):
@@ -159,7 +158,7 @@ class Pixel(cloud_storage_test_base.TestBase):
         default=default_reference_image_dir)
 
   def CreatePageSet(self, options):
-    page_set = super(Pixel, self).CreatePageSet(options)
+    page_set = page_sets.PixelTestsPageSet()
     for page in page_set.pages:
       page.script_to_evaluate_on_commit = test_harness_script
     return page_set
