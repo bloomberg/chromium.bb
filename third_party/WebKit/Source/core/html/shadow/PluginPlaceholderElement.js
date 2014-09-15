ea105c1b9308f432630d11670739b9469822fa59
jbroman@chromium.org <jbroman@chromium.org>
2014-09-15 14:37:23 +0000

    Create a dummy resource for PluginPlaceholderElement.js.
    This prepares for the landing of
      https://codereview.chromium.org/522783002/
    
    which now requires a three-sided change due to changes in how Blink accesses
    private script resources.
    
    Part 2: https://codereview.chromium.org/556793006/
    
    BUG=364716
    
    Review URL: https://codereview.chromium.org/570863002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@181980 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 40517c82923a12688acbef550c56c884e1708217

---
+++ third_party/WebKit/Source/core/html/shadow/PluginPlaceholderElement.js
@@ -0,0 +1,7 @@
+/**
+ * This is a placeholder to create the
+ * IDR_PRIVATE_SCRIPT_PLUGINPLACEHOLDERELEMENT_JS resource.
+ *
+ * It will be replaced by a complete file in:
+ *   https://codereview.chromium.org/522783002/
+ */
