8495925f56ef2c5ba6cc2b8b72f3ef700a55a52a
wajahat.s <wajahat.s@samsung.com>
2014-09-15 05:50:18 -0700

    Rename tabBase to tab
    In EmptyTabObserver tabBase needs to be renamed to tab
    as per patch https://codereview.chromium.org/148523013
    
    BUG=339423
    
    Review URL: https://codereview.chromium.org/549803003
    
    Cr-Commit-Position: refs/heads/master@{#294807}
    
    Former-commit-id: 7a06cb2e0d3edf4a7bd4e94f0a6292d13b871489

--- chrome/android/java/src/org/chromium/chrome/browser/EmptyTabObserver.java
+++ chrome/android/java/src/org/chromium/chrome/browser/EmptyTabObserver.java
@@ -42,10 +42,10 @@ public class EmptyTabObserver implements TabObserver {
     public void onWebContentsInstantSupportDisabled() { }
 
     @Override
-    public void onLoadStarted(Tab tabBase) { }
+    public void onLoadStarted(Tab tab) { }
 
     @Override
-    public void onLoadStopped(Tab tabBase) { }
+    public void onLoadStopped(Tab tab) { }
 
     @Override
     public void onLoadProgressChanged(Tab tab, int progress) { }
