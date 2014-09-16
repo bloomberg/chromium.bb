fdd606b4a93d2e97f5a3b34b4fc0c04197d6a1a6
Nico Weber <thakis@chromium.org>
2014-09-15 18:26:40 -0700

    Use a C++11 feature in a single file, to discover if this works everywhere.
    Similar to https://codereview.chromium.org/385743002 , but in a file
    that's also built in host targets. Depends on r#294584.
    
    If your bot can't deal with this, please let me know!
    
    BUG=none
    
    Committed: https://crrev.com/fdb8587e0974464067e63f696a08c0521e47e12f [formerly 5928c2f978a43488a2f45fe7da1d91dfdba1d593]
    Cr-Commit-Position: refs/heads/master@{#294687}
    
    R=ajwong@chromium.org
    
    Review URL: https://codereview.chromium.org/443483002
    
    Cr-Commit-Position: refs/heads/master@{#294961}
    
    Former-commit-id: 3249a55da6e8331b70938a86e27359d432908608

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -137,30 +137,14 @@
 // method in the parent class.
 // Use like:
 //   virtual void foo() OVERRIDE;
-#if defined(__clang__) || defined(COMPILER_MSVC)
 #define OVERRIDE override
-#elif defined(COMPILER_GCC) && __cplusplus >= 201103 && \
-      (__GNUC__ * 10000 + __GNUC_MINOR__ * 100) >= 40700
-// GCC 4.7 supports explicit virtual overrides when C++11 support is enabled.
-#define OVERRIDE override
-#else
-#define OVERRIDE
-#endif
 
 // Annotate a virtual method indicating that subclasses must not override it,
 // or annotate a class to indicate that it cannot be subclassed.
 // Use like:
 //   virtual void foo() FINAL;
 //   class B FINAL : public A {};
-#if defined(__clang__) || defined(COMPILER_MSVC)
 #define FINAL final
-#elif defined(COMPILER_GCC) && __cplusplus >= 201103 && \
-      (__GNUC__ * 10000 + __GNUC_MINOR__ * 100) >= 40700
-// GCC 4.7 supports explicit virtual overrides when C++11 support is enabled.
-#define FINAL final
-#else
-#define FINAL
-#endif
 
 // Annotate a function indicating the caller must examine the return value.
 // Use like:
