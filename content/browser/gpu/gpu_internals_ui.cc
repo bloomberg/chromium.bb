bb8473a1f61af4af1e96e378d6a26de3461e045e
dcower <dcower@chromium.org>
2014-09-15 21:43:34 -0700

    Added "Compositing manager" status to about:gpu
    BUG=410560
    TEST=about:gpu page on Linux under X11
    R=zmo@chromium.org
    
    Review URL: https://codereview.chromium.org/570173003
    
    Cr-Commit-Position: refs/heads/master@{#295007}
    
    Former-commit-id: f7d19a3dc4071fc8e196488a867bae731fd8a68d

--- content/browser/gpu/gpu_internals_ui.cc
+++ content/browser/gpu/gpu_internals_ui.cc
@@ -4,6 +4,10 @@
 
 #include "content/browser/gpu/gpu_internals_ui.h"
 
+#if defined(OS_LINUX) && defined(USE_X11)
+#include <X11/Xlib.h>
+#endif
+
 #include <string>
 
 #include "base/bind.h"
@@ -37,6 +41,7 @@
 
 #if defined(OS_LINUX) && defined(USE_X11)
 #include "ui/base/x/x11_util.h"
+#include "ui/gfx/x/x11_atom_cache.h"
 #endif
 
 namespace content {
@@ -178,6 +183,16 @@ base::DictionaryValue* GpuInfoAsDictionaryValue() {
     const char kGDMSession[] = "GDMSESSION";
     if (env->GetVar(kGDMSession, &value))
       basic_info->Append(NewDescriptionValuePair(kGDMSession, value));
+    const char* kAtomsToCache[] = {
+        "_NET_WM_CM_S0",
+        NULL
+    };
+    ui::X11AtomCache atom_cache(gfx::GetXDisplay(), kAtomsToCache);
+    std::string compositing_manager = XGetSelectionOwner(
+        gfx::GetXDisplay(),
+        atom_cache.GetAtom("_NET_WM_CM_S0")) != None ? "Yes" : "No";
+    basic_info->Append(
+        NewDescriptionValuePair("Compositing manager", compositing_manager));
   }
 #endif
   std::string direct_rendering = gpu_info.direct_rendering ? "Yes" : "No";
