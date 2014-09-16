ebeaea0d4a08a3ea22772a26d6b533aa56995667
calamity <calamity@chromium.org>
2014-09-15 22:00:25 -0700

    Add scoped path overrides for system directories for ExtensionBrowserTests.
    This CL adds path overrides for shortcut directories in testing environments.
    This will ensure tests don't pollute the build bots with shortcuts
    
    BUG=411970
    
    Review URL: https://codereview.chromium.org/565523003
    
    Cr-Commit-Position: refs/heads/master@{#295012}
    
    Former-commit-id: 6c0732b86d853652de1a2759bf76b39d4cc916a0

--- chrome/browser/extensions/extension_browsertest.cc
+++ chrome/browser/extensions/extension_browsertest.cc
@@ -69,6 +69,13 @@ ExtensionBrowserTest::ExtensionBrowserTest()
       override_prompt_for_external_extensions_(
           FeatureSwitch::prompt_for_external_extensions(),
           false),
+#if defined(OS_WIN)
+      user_desktop_override_(base::DIR_USER_DESKTOP),
+      common_desktop_override_(base::DIR_COMMON_DESKTOP),
+      user_quick_launch_override_(base::DIR_USER_QUICK_LAUNCH),
+      start_menu_override_(base::DIR_START_MENU),
+      common_start_menu_override_(base::DIR_COMMON_START_MENU),
+#endif
       profile_(NULL) {
   EXPECT_TRUE(temp_dir_.CreateUniqueTempDir());
 }
--- chrome/browser/extensions/extension_browsertest.h
+++ chrome/browser/extensions/extension_browsertest.h
@@ -11,6 +11,7 @@
 
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
+#include "base/test/scoped_path_override.h"
 #include "chrome/browser/extensions/extension_test_notification_observer.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -374,6 +375,15 @@ class ExtensionBrowserTest : virtual public InProcessBrowserTest {
   // Disable external install UI.
   extensions::FeatureSwitch::ScopedOverride
       override_prompt_for_external_extensions_;
+
+#if defined(OS_WIN)
+  // Use mock shortcut directories to ensure app shortcuts are cleaned up.
+  base::ScopedPathOverride user_desktop_override_;
+  base::ScopedPathOverride common_desktop_override_;
+  base::ScopedPathOverride user_quick_launch_override_;
+  base::ScopedPathOverride start_menu_override_;
+  base::ScopedPathOverride common_start_menu_override_;
+#endif
 
   // The default profile to be used.
   Profile* profile_;
