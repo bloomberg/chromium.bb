1528ab3cc65f07f7c83ec13a04af2441b3e19747
fs@opera.com <fs@opera.com>
2014-09-17 15:15:09 +0000

    Align assumptions in RenderSVGResourceSolidColor with other paint servers
    Since the SVG fonts code-path no longer directly call
    RenderSVGResource::applyResource, the same pre-conditions should hold
    for RenderSVGResourceSolidColor as for the other paint servers. Drop the
    existing (stale) comment and assert that |object| and |style| are
    non-null instead.
    
    Review URL: https://codereview.chromium.org/559653006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182159 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d3c6305279f665547de023044a33f344ad9f4eae

--- third_party/WebKit/Source/core/rendering/svg/RenderSVGResourceSolidColor.cpp
+++ third_party/WebKit/Source/core/rendering/svg/RenderSVGResourceSolidColor.cpp
@@ -42,37 +42,35 @@ RenderSVGResourceSolidColor::~RenderSVGResourceSolidColor()
 
 bool RenderSVGResourceSolidColor::applyResource(RenderObject* object, RenderStyle* style, GraphicsContext*& context, unsigned short resourceMode)
 {
-    // We are NOT allowed to ASSERT(object) here, unlike all other resources.
-    // RenderSVGResourceSolidColor is the only resource which may be used from HTML, when rendering
-    // SVG Fonts for a HTML document. This will be indicated by a null RenderObject pointer.
+    ASSERT(object);
+    ASSERT(style);
     ASSERT(context);
     ASSERT(resourceMode != ApplyToDefaultMode);
-
-    const SVGRenderStyle* svgStyle = style ? &style->svgStyle() : 0;
 
     bool isRenderingMask = false;
     if (object->frame() && object->frame()->view())
         isRenderingMask = object->frame()->view()->paintBehavior() & PaintBehaviorRenderingSVGMask;
 
+    const SVGRenderStyle& svgStyle = style->svgStyle();
+
     if (resourceMode & ApplyToFillMode) {
-        if (!isRenderingMask && svgStyle)
-            context->setAlphaAsFloat(svgStyle->fillOpacity());
+        if (!isRenderingMask)
+            context->setAlphaAsFloat(svgStyle.fillOpacity());
         else
             context->setAlphaAsFloat(1);
         context->setFillColor(m_color);
         if (!isRenderingMask)
-            context->setFillRule(svgStyle ? svgStyle->fillRule() : RULE_NONZERO);
+            context->setFillRule(svgStyle.fillRule());
 
         if (resourceMode & ApplyToTextMode)
             context->setTextDrawingMode(TextModeFill);
     } else if (resourceMode & ApplyToStrokeMode) {
         // When rendering the mask for a RenderSVGResourceClipper, the stroke code path is never hit.
         ASSERT(!isRenderingMask);
-        context->setAlphaAsFloat(svgStyle ? svgStyle->strokeOpacity() : 1);
+        context->setAlphaAsFloat(svgStyle.strokeOpacity());
         context->setStrokeColor(m_color);
 
-        if (style)
-            SVGRenderSupport::applyStrokeStyleToContext(context, style, object);
+        SVGRenderSupport::applyStrokeStyleToContext(context, style, object);
 
         if (resourceMode & ApplyToTextMode)
             context->setTextDrawingMode(TextModeStroke);
