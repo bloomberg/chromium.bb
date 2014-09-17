06a6cbefc15831bf898ddf31fdc9de84aa1a6b80
Gabe Black <gabeblack@chromium.org>
2014-09-17 14:00:05 -0700

    gs: Move over to a prerelease version of gsutil which retries DNS failures.
    The Google Storage folks have put together a prerelease version of gsutil
    which will retry on DNS failures and would like us to try it out. We can do
    that by switching to it and then grepping for "GS_ERROR:" followed by
    something with "Unable to find the server" in the logs.
    
    Note that this also moves us up to gsutil version 4.7 from version 4.5.
    Hopefully whatever the differences are between these versions don't cause any
    additional problems.
    
    BUG=chromium:410031
    TEST=A butterfly-paladin tryjob.
    
    Change-Id: I5316011b7ebe007ad383b4e5f34a42eab4eeb2ce
    Reviewed-on: https://chromium-review.googlesource.com/218731
    Reviewed-by: David James <davidjames@chromium.org>
    Tested-by: Gabe Black <gabeblack@chromium.org>
    Commit-Queue: Gabe Black <gabeblack@chromium.org>
    Commit-Queue: Aviv Keshet <akeshet@chromium.org>

--- lib/gs.py
+++ lib/gs.py
@@ -199,8 +199,8 @@ class GSContext(object):
   # (1*sleep) the first time, then (2*sleep), continuing via attempt * sleep.
   DEFAULT_SLEEP_TIME = 60
 
-  GSUTIL_TAR = 'gsutil_4.5.tar.gz'
-  GSUTIL_URL = PUBLIC_BASE_HTTPS_URL + 'pub/%s' % GSUTIL_TAR
+  GSUTIL_TAR = 'gsutil_4.7pre_retrydns.tar.gz'
+  GSUTIL_URL = PUBLIC_BASE_HTTPS_URL + 'prerelease/%s' % GSUTIL_TAR
   GSUTIL_API_SELECTOR = 'JSON'
 
   RESUMABLE_UPLOAD_ERROR = ('Too many resumable upload attempts failed without '
