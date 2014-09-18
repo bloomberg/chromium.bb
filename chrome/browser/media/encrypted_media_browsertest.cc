082d65b67c21f86b32f87896c296d4847fdde9c4
Elliot Glaysher <erg@chromium.org>
2014-09-18 15:05:17 -0700

    Disable ECKEncryptedMediaTest.FileIOTest
    BUG=415742
    TBR=danbeam@chromium.org
    
    Review URL: https://codereview.chromium.org/582233002
    
    Cr-Commit-Position: refs/heads/master@{#295577}
    
    Former-commit-id: 5af9a9c9d2d559014734c2eb2247d42f3fc0e740

--- chrome/browser/media/encrypted_media_browsertest.cc
+++ chrome/browser/media/encrypted_media_browsertest.cc
@@ -637,7 +637,14 @@ IN_PROC_BROWSER_TEST_F(ECKEncryptedMediaTest, DISABLED_CDMExpectedCrash) {
       "plugin crash");
 }
 
-IN_PROC_BROWSER_TEST_F(ECKEncryptedMediaTest, FileIOTest) {
+// Disabled on Windows because this flakily causes failures in ~File, which
+// causes base::ThreadRestrictions::AssertIOAllowed problems. crbug.com/415742
+#if defined(OS_WIN)
+#define MAYBE_FileIOTest DISABLED_FileIOTest
+#else
+#define MAYBE_FileIOTest FileIOTest
+#endif
+IN_PROC_BROWSER_TEST_F(ECKEncryptedMediaTest, MAYBE_FileIOTest) {
   TestNonPlaybackCases(kExternalClearKeyFileIOTestKeySystem,
                        kFileIOTestSuccess);
 }
