22ae4a86bedcaa9a8e85aa106d498dc4c6f6e62c
Christian Egli <christian.egli@sbs.ch>
2014-09-18 12:09:31 +0200

    Add license headers to the files in test
    at least to the ones that are copyright by Swiss Library for the blind.
    
    The files are all licensed under GNU All-Permissive
    License (http://www.gnu.org/licenses/license-list.html#GNUAllPermissive)
    
    Related to #22

--- tests/hash_collision.c
+++ tests/hash_collision.c
@@ -1,3 +1,12 @@
+/* liblouis Braille Translation and Back-Translation Library
+
+Copyright (C) 1013 Swiss Library for the Blind, Visually Impaired and Print Disabled
+
+Copying and distribution of this file, with or without modification,
+are permitted in any medium without royalty provided the copyright
+notice and this notice are preserved. This file is offered as-is,
+without any warranty. */
+
 #include <stdio.h>
 #include "brl_checks.h"
 #include "liblouis.h"
--- tests/typeform.c
+++ tests/typeform.c
@@ -1,3 +1,12 @@
+/* liblouis Braille Translation and Back-Translation Library
+
+Copyright (C) 2012 Swiss Library for the Blind, Visually Impaired and Print Disabled
+
+Copying and distribution of this file, with or without modification,
+are permitted in any medium without royalty provided the copyright
+notice and this notice are preserved. This file is offered as-is,
+without any warranty. */
+
 /* This is not a test per se, i.e. it doesn't invoke any functionality
    of liblouis and compares the result with a known good value.
 
