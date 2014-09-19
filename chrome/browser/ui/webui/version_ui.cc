3b351ec6e4a21a50c006fbce3eead07b9070e1ee
newt <newt@chromium.org>
2014-09-19 11:40:32 -0700

    Make Android about:version page more similar to other platforms.
    Chrome for Android can use the same code for displaying the application
    version on the about:version page as other platforms. There's no need to
    special-case it.
    
    Review URL: https://codereview.chromium.org/583003002
    
    Cr-Commit-Position: refs/heads/master@{#295746}
    
    Former-commit-id: 4da78f3fc41cc4adfcc75ab943893fc359f3d2c1

--- chrome/browser/ui/webui/version_ui.cc
+++ chrome/browser/ui/webui/version_ui.cc
@@ -44,6 +44,7 @@ content::WebUIDataSource* CreateVersionUIDataSource(Profile* profile) {
 
   // Localized and data strings.
   html_source->AddLocalizedString("title", IDS_ABOUT_VERSION_TITLE);
+  html_source->AddLocalizedString("application_label", IDS_PRODUCT_NAME);
   chrome::VersionInfo version_info;
   html_source->AddString("version", version_info.Version());
   html_source->AddString("version_modifier",
@@ -56,22 +57,11 @@ content::WebUIDataSource* CreateVersionUIDataSource(Profile* profile) {
   html_source->AddString("js_version", v8::V8::GetVersion());
 
 #if defined(OS_ANDROID)
-  html_source->AddLocalizedString("application_label",
-                                  IDS_ABOUT_VERSION_APPLICATION);
   html_source->AddString("os_version", AndroidAboutAppInfo::GetOsInfo());
-  base::android::BuildInfo* android_build_info =
-      base::android::BuildInfo::GetInstance();
-  html_source->AddString("application_name",
-                         android_build_info->package_label());
-  html_source->AddString("application_version_name",
-                         android_build_info->package_version_name());
-  html_source->AddString("application_version_code",
-                         android_build_info->package_version_code());
   html_source->AddLocalizedString("build_id_name",
                                   IDS_ABOUT_VERSION_BUILD_ID);
   html_source->AddString("build_id", CHROME_BUILD_ID);
 #else
-  html_source->AddLocalizedString("application_label", IDS_PRODUCT_NAME);
   html_source->AddString("os_version", std::string());
   html_source->AddString("flash_plugin", "Flash");
   // Note that the Flash version is retrieve asynchronously and returned in
