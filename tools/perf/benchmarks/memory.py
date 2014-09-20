24973b8e1af239f84ec928c83372cb693d8e5806
tonyg <tonyg@chromium.org>
2014-09-19 19:43:44 -0700

    [Telemetry] Make memory benchmark enable/disables match old master.cfg.
    BUG=388885
    
    Review URL: https://codereview.chromium.org/590593002
    
    Cr-Commit-Position: refs/heads/master@{#295850}
    
    Former-commit-id: 2d95506a5563805111f6193a9818ab160eb7361b

--- tools/perf/benchmarks/memory.py
+++ tools/perf/benchmarks/memory.py
@@ -7,25 +7,26 @@ import page_sets
 from telemetry import benchmark
 
 
+@benchmark.Enabled('android')
 class MemoryMobile(benchmark.Benchmark):
   test = memory.Memory
   page_set = page_sets.MobileMemoryPageSet
 
 
-@benchmark.Disabled('android')
+@benchmark.Disabled
 class MemoryTop25(benchmark.Benchmark):
   test = memory.Memory
   page_set = page_sets.Top25PageSet
 
 
-@benchmark.Disabled('android')
+@benchmark.Disabled
 class Reload2012Q3(benchmark.Benchmark):
   tag = 'reload'
   test = memory.Memory
   page_set = page_sets.Top2012Q3PageSet
 
 
-@benchmark.Disabled('android')  # crbug.com/371153
+@benchmark.Disabled  # crbug.com/371153
 class MemoryToughDomMemoryCases(benchmark.Benchmark):
   test = memory.Memory
   page_set = page_sets.ToughDomMemoryCasesPageSet
