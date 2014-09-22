7fc0016b1cc28d749e20c497671eb5504c47d310
jiayl <jiayl@chromium.org>
2014-09-22 09:52:41 -0700

    Use SafeWrapCurrent to make sure JingleThreadWrapper ctor does not fail.
    BUG=413853
    
    Review URL: https://codereview.chromium.org/585133002
    
    Cr-Commit-Position: refs/heads/master@{#295997}
    
    Former-commit-id: ab5818151f79f1f99f2fb3567721cd05a536ea8c

--- jingle/glue/thread_wrapper.cc
+++ jingle/glue/thread_wrapper.cc
@@ -57,7 +57,7 @@ JingleThreadWrapper::JingleThreadWrapper(
   DCHECK(!rtc::Thread::Current());
   weak_ptr_ = weak_ptr_factory_.GetWeakPtr();
   rtc::MessageQueueManager::Add(this);
-  CHECK(WrapCurrent());
+  SafeWrapCurrent();
 }
 
 JingleThreadWrapper::~JingleThreadWrapper() {
