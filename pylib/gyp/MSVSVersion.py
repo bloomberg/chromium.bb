ce6c5076aa984698f479a62db02432ce4266fe87
scottmg@chromium.org <scottmg@chromium.org>
2014-09-23 16:21:23 +0000

    Don't return unfound VS version, raise instead
    This fallback behaviour may have worked at some point, but doesn't
    any more, so simply error out with a clear message rather than trying
    to continue.
    
    R=timurrrr@chromium.org
    BUG=https://groups.google.com/a/chromium.org/forum/#!topic/chromium-dev/O_OGfq2nhSw
    
    Review URL: https://codereview.chromium.org/595543003
    
    git-svn-id: http://gyp.googlecode.com/svn/trunk@1978 78cadc50-ecff-11dd-a971-7dbc132099af

--- pylib/gyp/MSVSVersion.py
+++ pylib/gyp/MSVSVersion.py
@@ -401,9 +401,5 @@ def SelectVisualStudioVersion(version='auto'):
   version = str(version)
   versions = _DetectVisualStudioVersions(version_map[version], 'e' in version)
   if not versions:
-    if version == 'auto':
-      # Default to 2005 if we couldn't find anything
-      return _CreateVersion('2005', None)
-    else:
-      return _CreateVersion(version, None)
+    raise ValueError('Could not locate Visual Studio installation.')
   return versions[0]
