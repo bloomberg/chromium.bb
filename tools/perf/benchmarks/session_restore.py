a6db266057668e3b398d0b5bd964e2097d968ca6
tonyg <tonyg@chromium.org>
2014-09-23 20:49:41 -0700

    [Telemetry] Do not run session_restore benchmarks on reference builds.
    They were accidentally enabled with the switch to recipes and they don't work
    because the older builds don't necessarily work with the generated profile.
    
    BUG=388885
    TBR=dtu@chromium.org
    
    Review URL: https://codereview.chromium.org/596783005
    
    Cr-Commit-Position: refs/heads/master@{#296342}
    
    Former-commit-id: 6cf222b3094b4a67b1fcc5095fac6f7b8b418a33

--- tools/perf/benchmarks/session_restore.py
+++ tools/perf/benchmarks/session_restore.py
@@ -30,7 +30,8 @@ class _SessionRestoreTest(benchmark.Benchmark):
       args.browser_options.profile_dir = os.path.join(profile_dir, profile_type)
 
 
-@benchmark.Disabled('android', 'linux')  # crbug.com/325479, crbug.com/381990
+# crbug.com/325479, crbug.com/381990
+@benchmark.Disabled('android', 'linux', 'reference')
 class SessionRestoreColdTypical25(_SessionRestoreTest):
   tag = 'cold'
   test = session_restore.SessionRestore
@@ -39,7 +40,8 @@ class SessionRestoreColdTypical25(_SessionRestoreTest):
              'pageset_repeat': 5}
 
 
-@benchmark.Disabled('android', 'linux')  # crbug.com/325479, crbug.com/381990
+# crbug.com/325479, crbug.com/381990
+@benchmark.Disabled('android', 'linux', 'reference')
 class SessionRestoreWarmTypical25(_SessionRestoreTest):
   tag = 'warm'
   test = session_restore.SessionRestore
@@ -49,7 +51,7 @@ class SessionRestoreWarmTypical25(_SessionRestoreTest):
 
 
 # crbug.com/325479, crbug.com/381990, crbug.com/405386
-@benchmark.Disabled('android', 'linux', 'snowleopard')
+@benchmark.Disabled('android', 'linux', 'reference', 'snowleopard')
 class SessionRestoreWithUrlCold(_SessionRestoreTest):
   """Measure Chrome cold session restore with startup URLs."""
   tag = 'cold'
@@ -60,7 +62,7 @@ class SessionRestoreWithUrlCold(_SessionRestoreTest):
 
 
 # crbug.com/325479, crbug.com/381990, crbug.com/405386
-@benchmark.Disabled('android', 'linux', 'snowleopard')
+@benchmark.Disabled('android', 'linux', 'reference', 'snowleopard')
 class SessionRestoreWithUrlWarm(_SessionRestoreTest):
   """Measure Chrome warm session restore with startup URLs."""
   tag = 'warm'
