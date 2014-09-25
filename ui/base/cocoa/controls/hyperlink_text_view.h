5ab36d789695f6a7f1f063d681b77594f4dff2ff
avi <avi@chromium.org>
2014-09-25 13:47:38 -0700

    Fix component build after HyperlinkTextView move.
    HyperlinkTextView moved components in 0fe1fa39159345b81180318b7d65473064053534 [formerly 0e6cd43eb9a467c81382e37f686f3ebfdda5c1d3]; this marks it as being exported for use in other components.
    
    BUG=402485
    TEST=component build builds
    
    Review URL: https://codereview.chromium.org/602093004
    
    Cr-Commit-Position: refs/heads/master@{#296787}
    
    Former-commit-id: b930c8711443cd1f32f0834ba7ed454890824020

--- ui/base/cocoa/controls/hyperlink_text_view.h
+++ ui/base/cocoa/controls/hyperlink_text_view.h
@@ -4,11 +4,14 @@
 
 #import <Cocoa/Cocoa.h>
 
+#include "ui/base/ui_base_export.h"
+
 @class NSColor;
 
 // HyperlinkTextView is an NSTextView subclass for unselectable, linkable text.
 // This subclass doesn't show the text caret or IBeamCursor, whereas the base
 // class NSTextView displays both with full keyboard accessibility enabled.
+UI_BASE_EXPORT
 @interface HyperlinkTextView : NSTextView {
  @private
   BOOL refusesFirstResponder_;
