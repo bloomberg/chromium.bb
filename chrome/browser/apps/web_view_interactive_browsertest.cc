5a88107adccbb843dcf66d19a85975e2057464d0
John Abd-El-Malek <jam@chromium.org>
2014-09-25 15:50:45 -0700

    Disable WebViewInteractiveTest.EditCommandsNoMenu since it's flaky.
    BUG=417892
    TBR=fsamuel@chromium.org
    
    Review URL: https://codereview.chromium.org/604103004
    
    Cr-Commit-Position: refs/heads/master@{#296812}
    
    Former-commit-id: b8b19ea7b49b34cde808bb895d052bd719c561c5

--- chrome/browser/apps/web_view_interactive_browsertest.cc
+++ chrome/browser/apps/web_view_interactive_browsertest.cc
@@ -741,7 +741,8 @@ IN_PROC_BROWSER_TEST_F(WebViewInteractiveTest, EditCommands) {
 }
 
 // Tests that guests receive edit commands and respond appropriately.
-IN_PROC_BROWSER_TEST_F(WebViewInteractiveTest, EditCommandsNoMenu) {
+// http://crbug.com/417892
+IN_PROC_BROWSER_TEST_F(WebViewInteractiveTest, DISABLED_EditCommandsNoMenu) {
   SetupTest("web_view/edit_commands_no_menu",
       "/extensions/platform_apps/web_view/edit_commands_no_menu/"
       "guest.html");
