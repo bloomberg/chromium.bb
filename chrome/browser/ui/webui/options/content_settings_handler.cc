08f777018809ec26d902010424ff68b0ed69ccee
Shrikant Kelkar <shrikant@chromium.org>
2014-09-25 15:30:05 -0700

    Revert "Fix a bug where ContentSettingsHandler did not listen for OTR content settings changes."
    This reverts commit 34aa931f2345a6783c7c3d10c0dd2687ef1e8afc [formerly 3d148e7d2053a90d90c13dff7055b8f7f44f5695].
    
    BUG=417597
    TBR=bauerb@chromium.org,dhnishi@chromium.org,cpu@chromium.org
    
    Review URL: https://codereview.chromium.org/595213006
    
    Cr-Commit-Position: refs/heads/master@{#296810}
    
    Former-commit-id: c9c8408f23260f4259e11c0fa7f7110836e3cd7d

--- chrome/browser/ui/webui/options/content_settings_handler.cc
+++ chrome/browser/ui/webui/options/content_settings_handler.cc
@@ -527,21 +527,16 @@ void ContentSettingsHandler::Observe(
     const content::NotificationDetails& details) {
   switch (type) {
     case chrome::NOTIFICATION_PROFILE_DESTROYED: {
-      Profile* profile = content::Source<Profile>(source).ptr();
-      if (profile->IsOffTheRecord()) {
+      if (content::Source<Profile>(source).ptr()->IsOffTheRecord()) {
         web_ui()->CallJavascriptFunction(
             "ContentSettingsExceptionsArea.OTRProfileDestroyed");
-        observer_.Remove(profile->GetHostContentSettingsMap());
       }
       break;
     }
 
     case chrome::NOTIFICATION_PROFILE_CREATED: {
-      Profile* profile = content::Source<Profile>(source).ptr();
-      if (profile->IsOffTheRecord()) {
+      if (content::Source<Profile>(source).ptr()->IsOffTheRecord())
         UpdateAllOTRExceptionsViewsFromModel();
-        observer_.Add(profile->GetHostContentSettingsMap());
-      }
       break;
     }
 
