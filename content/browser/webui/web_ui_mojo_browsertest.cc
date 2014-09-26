88d3dc3617d27403521fcd1fc2b85bc13ee851ff
Per <perkj@google.com>
2014-09-26 10:43:06 +0200

    Disable WebUIMojoTest.EndToEndPing on Windows.
    TBR=viettrungluu@chromium.org
    BUG=418019
    
    Review URL: https://codereview.chromium.org/609743002
    
    Cr-Commit-Position: refs/heads/master@{#296906}
    
    Former-commit-id: 923cc8ab4abcbcdff8b38f35b4575bb4343cb93b

--- content/browser/webui/web_ui_mojo_browsertest.cc
+++ content/browser/webui/web_ui_mojo_browsertest.cc
@@ -197,7 +197,13 @@ class WebUIMojoTest : public ContentBrowserTest {
 
 // Loads a webui page that contains mojo bindings and verifies a message makes
 // it from the browser to the page and back.
-IN_PROC_BROWSER_TEST_F(WebUIMojoTest, EndToEndPing) {
+// Fails on Win only.  http://crbug.com/418019
+#if defined(OS_WIN)
+#define MAYBE_EndToEndPing DISABLED_EndToEndPing
+#else
+#define MAYBE_EndToEndPing EndToEndPing
+#endif
+IN_PROC_BROWSER_TEST_F(WebUIMojoTest, MAYBE_EndToEndPing) {
   // Currently there is no way to have a generated file included in the isolate
   // files. If the bindings file doesn't exist assume we're on such a bot and
   // pass.
