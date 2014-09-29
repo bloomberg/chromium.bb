b464a22f420121ec18467d88858dca211b5ad38f
pfeldman@chromium.org <pfeldman@chromium.org>
2014-09-29 11:59:57 +0000

    DevTools: include the stylesheets into the views instead of appending to head.
    BUG=417275
    R=lushnikov@chromium.org
    
    Review URL: https://codereview.chromium.org/601743003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182835 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 5b14d8b8d341743e798a8dda26a7a677c0773216

--- third_party/WebKit/LayoutTests/inspector/profiler/heap-snapshot-test.js
+++ third_party/WebKit/LayoutTests/inspector/profiler/heap-snapshot-test.js
@@ -687,7 +687,7 @@ InspectorTest.takeAndOpenSnapshot = function(generator, callback)
     callback = InspectorTest.safeWrap(callback);
     var snapshot = generator();
     var profileType =  WebInspector.ProfileTypeRegistry.instance.heapSnapshotProfileType;
-    function pushGeneratedSnapshot(reportProgress, callback)
+    function pushGeneratedSnapshot(reportProgress, callback2)
     {
         var profile = profileType.profileBeingRecorded();
         if (reportProgress) {
@@ -696,7 +696,7 @@ InspectorTest.takeAndOpenSnapshot = function(generator, callback)
         }
         snapshot.snapshot.typeId = "HEAP";
         profileType._addHeapSnapshotChunk({data: JSON.stringify(snapshot)});
-        setTimeout(callback, 0);
+        setTimeout(callback2, 0);
     }
     InspectorTest.override(HeapProfilerAgent, "takeHeapSnapshot", pushGeneratedSnapshot);
     InspectorTest._takeAndOpenSnapshotCallback = callback;
