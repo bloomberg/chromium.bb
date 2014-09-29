3f7efb16c02fe9697e200fa465cae593a5858a77
philipj@opera.com <philipj@opera.com>
2014-09-29 15:09:15 +0000

    Simplify MediaController with C++11 range-base for loops
    Some uses of MediaElementSequence::const_iterator remain, where the
    first media element in m_mediaElements is handled differently.
    
    Review URL: https://codereview.chromium.org/612873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@182845 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 4e2358c174af4cc0a2d122098148e03312c2e1cb

--- third_party/WebKit/Source/core/html/MediaController.cpp
+++ third_party/WebKit/Source/core/html/MediaController.cpp
@@ -134,8 +134,8 @@ double MediaController::duration() const
     // FIXME: Investigate caching the maximum duration and only updating the cached value
     // when the slaved media elements' durations change.
     double maxDuration = 0;
-    for (MediaElementSequence::const_iterator it = m_mediaElements.begin(); it != m_mediaElements.end(); ++it) {
-        double duration = (*it)->duration();
+    for (const HTMLMediaElement* element : m_mediaElements) {
+        double duration = element->duration();
         if (std::isnan(duration))
             continue;
         maxDuration = std::max(maxDuration, duration);
@@ -173,8 +173,8 @@ void MediaController::setCurrentTime(double time)
     m_clock->setCurrentTime(time);
 
     // Seek each slaved media element to the new playback position relative to the media element timeline.
-    for (MediaElementSequence::const_iterator it = m_mediaElements.begin(); it != m_mediaElements.end(); ++it)
-        (*it)->seek(time);
+    for (HTMLMediaElement* element : m_mediaElements)
+        element->seek(time);
 
     scheduleTimeupdateEvent();
 }
@@ -197,8 +197,8 @@ void MediaController::play()
 {
     // When the play() method is invoked, the user agent must invoke the play method of each
     // slaved media element in turn,
-    for (MediaElementSequence::const_iterator it = m_mediaElements.begin(); it != m_mediaElements.end(); ++it)
-        (*it)->play();
+    for (HTMLMediaElement* element : m_mediaElements)
+        element->play();
 
     // and then invoke the unpause method of the MediaController.
     unpause();
@@ -245,8 +245,8 @@ void MediaController::setPlaybackRate(double rate)
     // playback rate to the new value,
     m_clock->setPlayRate(rate);
 
-    for (MediaElementSequence::const_iterator it = m_mediaElements.begin(); it != m_mediaElements.end(); ++it)
-        (*it)->updatePlaybackRate();
+    for (HTMLMediaElement* element : m_mediaElements)
+        element->updatePlaybackRate();
 
     // then queue a task to fire a simple event named ratechange at the MediaController.
     scheduleEvent(EventTypeNames::ratechange);
@@ -271,8 +271,8 @@ void MediaController::setVolume(double level, ExceptionState& exceptionState)
     // and queue a task to fire a simple event named volumechange at the MediaController.
     scheduleEvent(EventTypeNames::volumechange);
 
-    for (MediaElementSequence::const_iterator it = m_mediaElements.begin(); it != m_mediaElements.end(); ++it)
-        (*it)->updateVolume();
+    for (HTMLMediaElement* element : m_mediaElements)
+        element->updateVolume();
 }
 
 void MediaController::setMuted(bool flag)
@@ -287,8 +287,8 @@ void MediaController::setMuted(bool flag)
     // and queue a task to fire a simple event named volumechange at the MediaController.
     scheduleEvent(EventTypeNames::volumechange);
 
-    for (MediaElementSequence::const_iterator it = m_mediaElements.begin(); it != m_mediaElements.end(); ++it)
-        (*it)->updateVolume();
+    for (HTMLMediaElement* element : m_mediaElements)
+        element->updateVolume();
 }
 
 static const AtomicString& playbackStateWaiting()
@@ -471,8 +471,8 @@ void MediaController::updatePlaybackState()
 
 void MediaController::updateMediaElements()
 {
-    for (MediaElementSequence::const_iterator it = m_mediaElements.begin(); it != m_mediaElements.end(); ++it)
-        (*it)->updatePlayState();
+    for (HTMLMediaElement* element : m_mediaElements)
+        element->updatePlayState();
 }
 
 void MediaController::bringElementUpToSpeed(HTMLMediaElement* element)
@@ -500,9 +500,7 @@ bool MediaController::isRestrained() const
 
     bool anyAutoplayingAndPaused = false;
     bool allPaused = true;
-    for (MediaElementSequence::const_iterator it = m_mediaElements.begin(); it != m_mediaElements.end(); ++it) {
-        HTMLMediaElement* element = *it;
-
+    for (const HTMLMediaElement* element : m_mediaElements) {
         if (element->isAutoplaying() && element->paused())
             anyAutoplayingAndPaused = true;
 
@@ -526,9 +524,7 @@ bool MediaController::isBlocked() const
         return true;
 
     bool allPaused = true;
-    for (MediaElementSequence::const_iterator it = m_mediaElements.begin(); it != m_mediaElements.end(); ++it) {
-        HTMLMediaElement* element = *it;
-
+    for (const HTMLMediaElement* element : m_mediaElements) {
         // or if any of its slaved media elements are blocked media elements,
         if (element->isBlocked())
             return true;
@@ -558,8 +554,8 @@ bool MediaController::hasEnded() const
         return false;
 
     bool allHaveEnded = true;
-    for (MediaElementSequence::const_iterator it = m_mediaElements.begin(); it != m_mediaElements.end(); ++it) {
-        if (!(*it)->ended())
+    for (const HTMLMediaElement* element : m_mediaElements) {
+        if (!element->ended())
             allHaveEnded = false;
     }
     return allHaveEnded;
