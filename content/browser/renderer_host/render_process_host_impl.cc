eae35279a75e80df4b7d8148a5b1bd5c45c36da6
morrita <morrita@chromium.org>
2014-09-30 16:59:04 -0700

    Always activate MojoApplicationHost
    Before this change, MojoApplicatioHost was activated only if some
    browser-side modules requested it. There was no way for renderer
    to activate it.
    
    This CL let MojoApplicationHost always get activated. Now
    renderer can request services it needs without its own activation flow.
    
    R=darin@chromium.org,tsepez@chromium.org
    TEST=none
    BUG=none
    
    Committed: https://crrev.com/a64836df3641bcd92731eb6e0e74756cbabdc8e3 [formerly bd0aa4dc467c10a4301e44084d8abbba92a35e56]
    Cr-Commit-Position: refs/heads/master@{#294969}
    
    Review URL: https://codereview.chromium.org/563373002
    
    Cr-Commit-Position: refs/heads/master@{#297551}
    
    Former-commit-id: 2c0a563d3019940343e804c7e975bef1bd0f5b54

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -440,7 +440,6 @@ RenderProcessHostImpl::RenderProcessHostImpl(
 #endif
       pending_views_(0),
       mojo_application_host_(new MojoApplicationHost),
-      mojo_activation_required_(false),
       visible_widgets_(0),
       backgrounded_(true),
       is_initialized_(false),
@@ -643,20 +642,6 @@ bool RenderProcessHostImpl::Init() {
 
   is_initialized_ = true;
   return true;
-}
-
-void RenderProcessHostImpl::MaybeActivateMojo() {
-  // TODO(darin): Following security review, we can unconditionally initialize
-  // Mojo in all renderers. We will then be able to directly call Activate()
-  // from OnProcessLaunched.
-  if (!mojo_activation_required_)
-    return;  // Waiting on someone to require Mojo.
-
-  if (!GetHandle())
-    return;  // Waiting on renderer startup.
-
-  if (!mojo_application_host_->did_activate())
-    mojo_application_host_->Activate(this, GetHandle());
 }
 
 bool RenderProcessHostImpl::ShouldUseMojoChannel() const {
@@ -1936,7 +1921,6 @@ void RenderProcessHostImpl::ProcessDied(bool already_dead) {
   }
 
   mojo_application_host_.reset(new MojoApplicationHost);
-  mojo_activation_required_ = false;
 
   // It's possible that one of the calls out to the observers might have caused
   // this object to be no longer needed.
@@ -2103,7 +2087,7 @@ void RenderProcessHostImpl::OnProcessLaunched() {
   // Allow Mojo to be setup before the renderer sees any Chrome IPC messages.
   // This way, Mojo can be safely used from the renderer in response to any
   // Chrome IPC message.
-  MaybeActivateMojo();
+  mojo_application_host_->Activate(this, GetHandle());
 
   if (channel_mojo_host_)
     channel_mojo_host_->OnClientLaunched(GetHandle());
@@ -2245,11 +2229,6 @@ void RenderProcessHostImpl::DecrementWorkerRefCount() {
   --worker_ref_count_;
   if (worker_ref_count_ == 0)
     Cleanup();
-}
-
-void RenderProcessHostImpl::EnsureMojoActivated() {
-  mojo_activation_required_ = true;
-  MaybeActivateMojo();
 }
 
 }  // namespace content
