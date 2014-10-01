bb4f0f5d10a6ad951e7294a1af034dc8873508a2
davidben <davidben@chromium.org>
2014-10-01 14:36:43 -0700

    Remove unnecessary ifdefs for maximum SSL version.
    Only the bundled BoringSSL is supported; the maximum version is TLS 1.2.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/611263003
    
    Cr-Commit-Position: refs/heads/master@{#297712}
    
    Former-commit-id: 8a895ccf9de442b7c0c255ac268a557b47e84fb9

--- net/ssl/ssl_config.cc
+++ net/ssl/ssl_config.cc
@@ -4,26 +4,11 @@
 
 #include "net/ssl/ssl_config.h"
 
-#if defined(USE_OPENSSL)
-#include <openssl/ssl.h>
-#endif
-
 namespace net {
 
 const uint16 kDefaultSSLVersionMin = SSL_PROTOCOL_VERSION_SSL3;
 
-const uint16 kDefaultSSLVersionMax =
-#if defined(USE_OPENSSL)
-#if defined(SSL_OP_NO_TLSv1_2)
-    SSL_PROTOCOL_VERSION_TLS1_2;
-#elif defined(SSL_OP_NO_TLSv1_1)
-    SSL_PROTOCOL_VERSION_TLS1_1;
-#else
-    SSL_PROTOCOL_VERSION_TLS1;
-#endif
-#else
-    SSL_PROTOCOL_VERSION_TLS1_2;
-#endif
+const uint16 kDefaultSSLVersionMax = SSL_PROTOCOL_VERSION_TLS1_2;
 
 SSLConfig::CertAndStatus::CertAndStatus() : cert_status(0) {}
 
