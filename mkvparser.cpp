623d1826130bd240add5ceaf6dff0ee87387937b
James Zern <jzern@google.com>
2014-09-30 22:51:47 -0700

    DoLoadCluster: tolerate empty clusters
    previously only empty clusters of known size were accepted; with
    clusters of unknown size the cluster has been successfully parsed and a
    working copy of the file position updated, so parsing can continue.
    clusters of this type are discarded as clusters of known size currently
    are.
    
    Change-Id: I3ef848768519ab7f1887a11629c50af72249cead

--- mkvparser.cpp
+++ mkvparser.cpp
@@ -23,7 +23,7 @@ void mkvparser::GetVersion(int& major, int& minor, int& build, int& revision) {
   major = 1;
   minor = 0;
   build = 0;
-  revision = 28;
+  revision = 29;
 }
 
 long long mkvparser::ReadUInt(IMkvReader* pReader, long long pos, long& len) {
@@ -1159,10 +1159,8 @@ long Segment::DoLoadCluster(long long& pos, long& len) {
   }
 
   if (status == 0) {  // no entries found
-    if (cluster_size < 0)
-      return E_FILE_FORMAT_INVALID;  // TODO: handle this
-
-    pos += cluster_size;
+    if (cluster_size >= 0)
+      pos += cluster_size;
 
     if ((total >= 0) && (pos >= total)) {
       m_pos = total;
