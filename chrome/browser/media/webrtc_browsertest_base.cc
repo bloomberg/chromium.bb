302278ef3d142c7b4ceaef2d1d8003b8dfa55f0f
phoglund <phoglund@chromium.org>
2014-10-02 03:59:49 -0700

    Revert of No longer requiring pixel output for WebRTC tests. (patchset #4 id:60001 of https://codereview.chromium.org/611953004/)
    Reason for revert:
    Turns out this change makes video+canvas grabbing produce junk; this is not good since our tests depend on that working. Reverting.
    
    Original issue's description:
    > No longer requiring pixel output for WebRTC tests.
    >
    > We used to require pixel output for our canvas-scraping, but that
    > appears to no longer be required. This speeds up the test 3x on my
    > win machine, from 30 seconds to about 10.
    >
    > The content_browsertests on ChromeOS will still enable pixel
    > output since it seems the requirements there are different - video
    > tags will not work unless that is enabled, it seems. I think
    > that's fine since our original problem was with the browser_tests
    > anyway, and I think the content_browsertests are probably fast
    > enough on ChromeOS anyway (whereas the browser_tests weren't).
    >
    > BUG=396505
    >
    > Committed: https://crrev.com/982a6cd9dd8a67c83158f91b1e9ec8c8d356d73f [formerly e6a342dcab57c3a2de1fd21301f37fdf3f8ad070]
    > Cr-Commit-Position: refs/heads/master@{#297654}
    
    TBR=tommi@chromium.org,xians@google.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=396505
    
    Review URL: https://codereview.chromium.org/619003003
    
    Cr-Commit-Position: refs/heads/master@{#297800}
    
    Former-commit-id: b8c01fc3dc165b8d3d6aea166776f07b25f06b04

--- chrome/browser/media/webrtc_browsertest_base.cc
+++ chrome/browser/media/webrtc_browsertest_base.cc
@@ -82,6 +82,8 @@ WebRtcTestBase::WebRtcTestBase(): detect_errors_in_javascript_(false) {
   // set operation is idempotent.
   logging::SetLogMessageHandler(&JavascriptErrorDetectingLogHandler);
   hit_javascript_errors_.Get() = false;
+
+  EnablePixelOutput();
 }
 
 WebRtcTestBase::~WebRtcTestBase() {
