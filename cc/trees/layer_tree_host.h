0a9ec60c0dc4edb3250691bc34d9a4e83312abf7
siva.gunturi <siva.gunturi@samsung.com>
2014-10-02 11:05:10 -0700

    [cc_cleanup] remove impl_transform_ variable.
    This variable is unused, this can be removed.
    
    Review URL: https://codereview.chromium.org/618693003
    
    Cr-Commit-Position: refs/heads/master@{#297862}
    
    Former-commit-id: a2845429be2657089d776806c2395bf91353f902

--- cc/trees/layer_tree_host.h
+++ cc/trees/layer_tree_host.h
@@ -400,7 +400,6 @@ class CC_EXPORT LayerTreeHost {
   float page_scale_factor_;
   float min_page_scale_factor_;
   float max_page_scale_factor_;
-  gfx::Transform impl_transform_;
   bool has_gpu_rasterization_trigger_;
   bool content_is_suitable_for_gpu_rasterization_;
   bool gpu_rasterization_histogram_recorded_;
