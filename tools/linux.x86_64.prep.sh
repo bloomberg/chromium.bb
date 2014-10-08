e654b40558b71b2f108be9a2cc8344e8abc84196
jvoung@chromium.org <jvoung@chromium.org>
2014-10-08 17:30:18 +0000

    Fix typo in tools/linux.x86_64.prep.sh (gilb vs glib)
    BUG=none
    R=dschuff@chromium.org
    
    Review URL: https://codereview.chromium.org/635383002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@13866 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 776393f85880a6e285ccb2e674657bb21261bee6

--- tools/linux.x86_64.prep.sh
+++ tools/linux.x86_64.prep.sh
@@ -79,7 +79,7 @@ fi
 # libtinfo5 is needed by gdb
 ensure_installed '/lib/i386-linux-gnu/libtinfo.so.5' 'libtinfo5:i386'
 # glib is needed by qemu
-ensure_installed '/lib/i386-linux-gnu/libglib-2.0.so.0' 'libgilb2.0-0:i386'
+ensure_installed '/lib/i386-linux-gnu/libglib-2.0.so.0' 'libglib2.0-0:i386'
 # 32-bit libc headers and libraries
 ensure_installed '/usr/include/i386-linux-gnu/asm/errno.h' 'linux-libc-dev:i386'
 # 32-bit linux-libc-dev doesn't have ieee754.h when installed on x86-64
