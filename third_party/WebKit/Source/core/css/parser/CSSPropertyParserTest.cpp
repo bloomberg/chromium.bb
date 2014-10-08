1bf31d85759b85d71f44cfa324788da488e455e6
timloh@chromium.org <timloh@chromium.org>
2014-10-08 03:24:32 +0000

    Small clean-up to GridTrackLimits test
    Review URL: https://codereview.chromium.org/629213003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@183374 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: de0b8250357e3fd70e795b18c10d4758f59f8f83

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParserTest.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParserTest.cpp
@@ -7,7 +7,6 @@
 
 #include "core/css/CSSValueList.h"
 #include "core/css/parser/CSSParser.h"
-#include "wtf/dtoa/utils.h"
 
 #include <gtest/gtest.h>
 
@@ -31,27 +30,23 @@ TEST(CSSPropertyParserTest, GridTrackLimits)
         const char* input;
         const size_t output;
     } testCases[] = {
-        {CSSPropertyGridTemplateColumns, "grid-template-columns: repeat(999999, 20px);", 999999},
-        {CSSPropertyGridTemplateRows, "grid-template-rows: repeat(999999, 20px);", 999999},
-        {CSSPropertyGridTemplateColumns, "grid-template-columns: repeat(1000000, 10%);", 1000000},
-        {CSSPropertyGridTemplateRows, "grid-template-rows: repeat(1000000, 10%);", 1000000},
-        {CSSPropertyGridTemplateColumns, "grid-template-columns: repeat(1000000, (first) min-content (last));", 1000000},
-        {CSSPropertyGridTemplateRows, "grid-template-rows: repeat(1000000, (first) min-content (last));", 1000000},
-        {CSSPropertyGridTemplateColumns, "grid-template-columns: repeat(1000001, auto);", 1000000},
-        {CSSPropertyGridTemplateRows, "grid-template-rows: repeat(1000001, auto);", 1000000},
-        {CSSPropertyGridTemplateColumns, "grid-template-columns: repeat(400000, 2em minmax(10px, max-content) 0.5fr);", 999999},
-        {CSSPropertyGridTemplateRows, "grid-template-rows: repeat(400000, 2em minmax(10px, max-content) 0.5fr);", 999999},
-        {CSSPropertyGridTemplateColumns, "grid-template-columns: repeat(600000, (first) 3vh 10% 2fr (nav) 10px auto 1fr 6em (last));", 999999},
-        {CSSPropertyGridTemplateRows, "grid-template-rows: repeat(600000, (first) 3vh 10% 2fr (nav) 10px auto 1fr 6em (last));", 999999},
+        {CSSPropertyGridTemplateColumns, "repeat(999999, 20px)", 999999},
+        {CSSPropertyGridTemplateRows, "repeat(999999, 20px)", 999999},
+        {CSSPropertyGridTemplateColumns, "repeat(1000000, 10%)", 1000000},
+        {CSSPropertyGridTemplateRows, "repeat(1000000, 10%)", 1000000},
+        {CSSPropertyGridTemplateColumns, "repeat(1000000, (first) min-content (last))", 1000000},
+        {CSSPropertyGridTemplateRows, "repeat(1000000, (first) min-content (last))", 1000000},
+        {CSSPropertyGridTemplateColumns, "repeat(1000001, auto)", 1000000},
+        {CSSPropertyGridTemplateRows, "repeat(1000001, auto)", 1000000},
+        {CSSPropertyGridTemplateColumns, "repeat(400000, 2em minmax(10px, max-content) 0.5fr)", 999999},
+        {CSSPropertyGridTemplateRows, "repeat(400000, 2em minmax(10px, max-content) 0.5fr)", 999999},
+        {CSSPropertyGridTemplateColumns, "repeat(600000, (first) 3vh 10% 2fr (nav) 10px auto 1fr 6em (last))", 999999},
+        {CSSPropertyGridTemplateRows, "repeat(600000, (first) 3vh 10% 2fr (nav) 10px auto 1fr 6em (last))", 999999},
     };
 
-    CSSParser parser(strictCSSParserContext());
-    RefPtrWillBeRawPtr<MutableStylePropertySet> propertySet = MutableStylePropertySet::create();
-
-    for (unsigned i = 0; i < ARRAY_SIZE(testCases); ++i) {
-        ASSERT_TRUE(parser.parseDeclaration(propertySet.get(), testCases[i].input, 0, 0));
-        RefPtrWillBeRawPtr<CSSValue> value = propertySet->getPropertyCSSValue(testCases[i].propertyID);
-
+    for (unsigned i = 0; i < WTF_ARRAY_LENGTH(testCases); ++i) {
+        RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(testCases[i].propertyID, testCases[i].input);
+        ASSERT_TRUE(value);
         ASSERT_TRUE(value->isValueList());
         EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), testCases[i].output);
     }
