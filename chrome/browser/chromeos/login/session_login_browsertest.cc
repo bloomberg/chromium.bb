50e1951ac0b90fc209a1d9fcd44354e3622e0a00
kjellander <kjellander@chromium.org>
2014-10-08 07:21:41 -0700

    Disable BrowserLoginTest.BrowserActive browser test.
    This test hangs flakily.
    
    NOTRY=true
    BUG=421453
    TBR=nkostylev@chromium.org
    
    Review URL: https://codereview.chromium.org/641683002
    
    Cr-Commit-Position: refs/heads/master@{#298703}
    
    Former-commit-id: 3ac456f47b67b0e6b35e96a2caef1dc00e67e7c7

--- chrome/browser/chromeos/login/session_login_browsertest.cc
+++ chrome/browser/chromeos/login/session_login_browsertest.cc
@@ -44,7 +44,8 @@ IN_PROC_BROWSER_TEST_F(BrowserLoginTest, PRE_BrowserActive) {
   chromeos::StartupUtils::MarkOobeCompleted();
 }
 
-IN_PROC_BROWSER_TEST_F(BrowserLoginTest, BrowserActive) {
+// Hangs flakily. See http://crbug.com/421453.
+IN_PROC_BROWSER_TEST_F(BrowserLoginTest, DISABLED_BrowserActive) {
   EXPECT_EQ(ash::SessionStateDelegate::SESSION_STATE_LOGIN_PRIMARY,
             ash::Shell::GetInstance()->session_state_delegate()->
                 GetSessionState());
