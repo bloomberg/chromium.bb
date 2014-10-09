1912324955f8c6fce0f7765fe22d7a3d31bc5b38
anujk.sharma <anujk.sharma@samsung.com>
2014-10-09 10:25:25 -0700

    Apply automatic range checks to content enum types across IPC.
    This is part of a long-running background task to remove the remaining
    uses of the unchecked IPC_ENUM_TRAITS() macro.
    
    BUG=246708
    
    Review URL: https://codereview.chromium.org/635253002
    
    Cr-Commit-Position: refs/heads/master@{#298923}
    
    Former-commit-id: 098510bfbef457641abed93223f5294c13c2cd74

--- chrome/common/chrome_utility_printing_messages.h
+++ chrome/common/chrome_utility_printing_messages.h
@@ -49,7 +49,8 @@ IPC_STRUCT_TRAITS_BEGIN(printing::PrinterSemanticCapsAndDefaults)
   IPC_STRUCT_TRAITS_MEMBER(default_dpi)
 IPC_STRUCT_TRAITS_END()
 
-IPC_ENUM_TRAITS(printing::PwgRasterTransformType)
+IPC_ENUM_TRAITS_MAX_VALUE(printing::PwgRasterTransformType,
+                          printing::TRANSFORM_TYPE_LAST)
 
 IPC_STRUCT_TRAITS_BEGIN(printing::PwgRasterSettings)
   IPC_STRUCT_TRAITS_MEMBER(odd_page_transform)
