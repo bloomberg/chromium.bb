a0b111c60ad066c03926c6c966462a70ebe044d1
ricow <ricow@google.com>
2014-10-09 04:02:33 -0700

    Disable ManagePasswordsBubbleViewTest.TwoTabsWithBubble on linux
    BUG=421792
    R=vasilii@chromium.org
    TBR=vasilii@chromium.org
    
    Review URL: https://codereview.chromium.org/638373002
    
    Cr-Commit-Position: refs/heads/master@{#298846}
    
    Former-commit-id: 1f90ea420c4b983f224aa35d05d397e168929e6e

--- chrome/browser/ui/views/passwords/manage_passwords_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/passwords/manage_passwords_bubble_view_browsertest.cc
@@ -212,7 +212,13 @@ IN_PROC_BROWSER_TEST_F(ManagePasswordsBubbleViewTest, CloseOnKey) {
   EXPECT_FALSE(ManagePasswordsBubbleView::IsShowing());
 }
 
-IN_PROC_BROWSER_TEST_F(ManagePasswordsBubbleViewTest, TwoTabsWithBubble) {
+// Fails on linux rel32, see crbug.com/421792
+#if defined(OS_LINUX)
+#define MAYBE_TwoTabsWithBubble DISABLED_TwoTabsWithBubble
+#else
+#define MAYBE_TwoTabsWithBubble TwoTabsWithBubble
+#endif
+IN_PROC_BROWSER_TEST_F(ManagePasswordsBubbleViewTest, MAYBE_TwoTabsWithBubble) {
   // Set up the first tab with the bubble.
   SetupPendingPassword();
   EXPECT_TRUE(ManagePasswordsBubbleView::IsShowing());
