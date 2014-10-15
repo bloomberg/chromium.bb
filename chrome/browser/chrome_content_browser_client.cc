1da9d65872f9705eb62d8f9499f9e70229aad3f1
teravest <teravest@chromium.org>
2014-10-15 12:46:02 -0700

    Remove some whitelist entries for socket.
    These application IDs no longer need access to the private socket interface now that https://code.google.com/p/chromium/issues/detail?id=308259 is resolved.
    
    BUG=234789
    
    Review URL: https://codereview.chromium.org/652293004
    
    Cr-Commit-Position: refs/heads/master@{#299746}
    
    Former-commit-id: 9213b26a352db67accada5c9002c830aac783258

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -313,8 +313,6 @@ const char* const kPredefinedAllowedSocketOrigins[] = {
   "jdfhpkjeckflbbleddjlpimecpbjdeep",  // see crbug.com/142514
   "iabmpiboiopbgfabjmgeedhcmjenhbla",  // see crbug.com/165080
   "B7CF8A292249681AF81771650BA4CEEAF19A4560",  // see crbug.com/165080
-  "6EAED1924DB611B6EEF2A664BD077BE7EAD33B8F",  // see crbug.com/234789
-  "4EB74897CB187C7633357C2FE832E0AD6A44883A",  // see crbug.com/234789
   "7525AF4F66763A70A883C4700529F647B470E4D2",  // see crbug.com/238084
   "0B549507088E1564D672F7942EB87CA4DAD73972",  // see crbug.com/238084
   "864288364E239573E777D3E0E36864E590E95C74"   // see crbug.com/238084
