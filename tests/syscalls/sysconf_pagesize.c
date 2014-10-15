73b63615cd78c3f9661461cad8228dde518a03c8
mseaborn@chromium.org <mseaborn@chromium.org>
2014-10-15 16:37:16 +0000

    Clean up sysconf() test to address a TODO that depended on a toolchain roll
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3909
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=2714
    TEST=run_sysconf_pagesize_test
    
    Review URL: https://codereview.chromium.org/655963003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@13915 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 1534a6d6d9baddc9904cc160bb24cf716e1b5c94

--- tests/syscalls/sysconf_pagesize.c
+++ tests/syscalls/sysconf_pagesize.c
@@ -5,24 +5,10 @@
  */
 
 #include <stdio.h>
-
-#if defined(__GLIBC__)
 #include <unistd.h>
-#else
-/*
- * TODO(bsy): remove when newlib toolchain catches up
- * http://code.google.com/p/nativeclient/issues/detail?id=2714
- */
-#include "native_client/src/trusted/service_runtime/include/sys/unistd.h"
-#endif
 
 int main(void) {
-#if defined(__GLIBC__)
   long rv = sysconf(_SC_PAGESIZE);
-#else
-  long rv = sysconf(NACL_ABI__SC_PAGESIZE);
-#endif
-
   printf("%ld\n", rv);
   return rv != (1<<16);
 }
