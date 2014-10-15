de7473078794c0fd21f38f650595bbcab72193bd
kalman <kalman@chromium.org>
2014-10-14 17:20:06 -0700

    Add some null checks to the extensions bindings privates() method.
    BUG=359442
    R=mek@chromium.org
    
    Review URL: https://codereview.chromium.org/658653002
    
    Cr-Commit-Position: refs/heads/master@{#299595}
    
    Former-commit-id: ad8ddd0d53b6f38806495396d26cfa4d6991e939

--- extensions/renderer/module_system.cc
+++ extensions/renderer/module_system.cc
@@ -561,12 +561,18 @@ v8::Handle<v8::String> ModuleSystem::WrapSource(v8::Handle<v8::String> source) {
 void ModuleSystem::Private(const v8::FunctionCallbackInfo<v8::Value>& args) {
   CHECK_EQ(1, args.Length());
   CHECK(args[0]->IsObject());
+  CHECK(!args[0]->IsNull());
   v8::Local<v8::Object> obj = args[0].As<v8::Object>();
   v8::Local<v8::String> privates_key =
       v8::String::NewFromUtf8(GetIsolate(), "privates");
   v8::Local<v8::Value> privates = obj->GetHiddenValue(privates_key);
   if (privates.IsEmpty()) {
     privates = v8::Object::New(args.GetIsolate());
+    if (privates.IsEmpty()) {
+      GetIsolate()->ThrowException(
+          v8::String::NewFromUtf8(GetIsolate(), "Failed to create privates"));
+      return;
+    }
     obj->SetHiddenValue(privates_key, privates);
   }
   args.GetReturnValue().Set(privates);
