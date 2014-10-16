c6f677d78714858d2d5e27ed707dcf12acf2c4e0
dcheng <dcheng@chromium.org>
2014-10-15 23:38:51 -0700

    Use puts() instead of pluginLog() to avoid running JS during detach.
    BUG=363099,371084
    
    Review URL: https://codereview.chromium.org/656223002
    
    Cr-Commit-Position: refs/heads/master@{#299860}
    
    Former-commit-id: eb036181ac1826a236c71226b071656d07b4ca7f

--- content/shell/tools/plugin/main.cpp
+++ content/shell/tools/plugin/main.cpp
@@ -348,8 +348,11 @@ NPError NPP_Destroy(NPP instance, NPSavedData **save)
         if (obj->evaluateScriptOnMouseDownOrKeyDown)
             free(obj->evaluateScriptOnMouseDownOrKeyDown);
 
-        if (obj->logDestroy)
-            pluginLog(instance, "NPP_Destroy");
+        if (obj->logDestroy) {
+            // Note: this intentionally avoids using pluginLog(), because that
+            // requires running JS during document detach, which is forbidden.
+            puts("PLUGIN: NPP_Destroy");
+        }
 
 #ifdef XP_MACOSX
         if (obj->coreAnimationLayer)
