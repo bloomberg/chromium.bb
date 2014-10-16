645266dbfd6779eb4aa2863c468b01a4f006460f
jln <jln@chromium.org>
2014-10-16 14:24:41 -0700

    SafeSPrintf: use C++ variadic template
    Revert 9501dd32f86ce11daca34be03cccafee8f483bdd [formerly 543be0953e1ea908c2b5bbc005af3f3d61eb09f1] and always use C++11
    variadic template.
    
    Also add mdempsky@ and jln@ as owners.
    
    Review URL: https://codereview.chromium.org/665483002
    
    Cr-Commit-Position: refs/heads/master@{#299977}
    
    Former-commit-id: ad396f7c3ba3253b0376088c2f11c59e9f4eac96

--- base/strings/safe_sprintf_unittest.cc
+++ base/strings/safe_sprintf_unittest.cc
@@ -264,6 +264,13 @@ TEST(SafeSPrintfTest, NArgs) {
   EXPECT_EQ(10, SafeSNPrintf(buf, 11, "%c%c%c%c%c%c%c%c%c%c",
                              1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
   EXPECT_EQ("\1\2\3\4\5\6\7\10\11\12", std::string(buf));
+
+  EXPECT_EQ(11, SafeSPrintf(buf, "%c%c%c%c%c%c%c%c%c%c%c",
+                            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
+  EXPECT_EQ("\1\2\3\4\5\6\7\10\11\12\13", std::string(buf));
+  EXPECT_EQ(11, SafeSNPrintf(buf, 12, "%c%c%c%c%c%c%c%c%c%c%c",
+                             1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11));
+  EXPECT_EQ("\1\2\3\4\5\6\7\10\11\12\13", std::string(buf));
 }
 
 TEST(SafeSPrintfTest, DataTypes) {
