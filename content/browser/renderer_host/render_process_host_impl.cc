f2d6d5cc3c5093ff2c05302d9c280474c8d0acbf
morrita <morrita@chromium.org>
2014-10-16 15:26:26 -0700

    RPHI::AddRoute: Turn a DCHECK() to a CHECK() again.
    This is a revival of [1], which was reverted [2] due to triggering
    huge crash. Now the cause of the crash is fixed by [3].
    This CHECK() aims to prevent similar bug in the future.
    
    [1] crrev.com/997dd2762d23ae875b139599a54528310ad9c5cd [formerly d3e91b36c3c29aee92159db9d5cc331cd3f97241]
    [2] crrev.com/9251d612c46e5f476d0822433e851bd5f3dc80df [formerly 9a04b3c3f9c85da4510eba67095b61bec78d7b6b]
    [3] crrev.com/d165be055c7c65160f343a7feab21fe07fd3b0a5 [formerly dcdb02fab210ec5f7b8b560075ce96d0f48f344c]
    
    R=nasko@chromium.org, kareng@chromium.org
    BUG=381990
    
    Review URL: https://codereview.chromium.org/640223005
    
    Cr-Commit-Position: refs/heads/master@{#299994}
    
    Former-commit-id: 0181f4ac89929f78f9e88ca18e971031a21d5ae2

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -921,8 +921,10 @@ ServiceRegistry* RenderProcessHostImpl::GetServiceRegistry() {
 void RenderProcessHostImpl::AddRoute(
     int32 routing_id,
     IPC::Listener* listener) {
-  DCHECK(widget_helper_->IsRoutingIDProbablyValid(routing_id))
-      << "Found Routing ID conflicts: " << routing_id;
+  CHECK(widget_helper_->IsRoutingIDProbablyValid(routing_id))
+      << "Found Invalid Routing ID: " << routing_id;
+  CHECK(!listeners_.Lookup(routing_id))
+      << "Found Routing ID Conflict: " << routing_id;
   listeners_.AddWithID(listener, routing_id);
 }
 
