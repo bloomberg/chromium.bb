0431cd5eaff17f549eeb4531a50317d0afaa81df
bondd <bondd@chromium.org>
2014-10-21 11:40:28 -0700

    Add 'X' button to 'Manage search engines' tab order.
    'X' delete button is now part of the focus order in the 'Manage search
    engines' options page.
    
    BUG=276874
    
    Review URL: https://codereview.chromium.org/666823003
    
    Cr-Commit-Position: refs/heads/master@{#300528}
    
    Former-commit-id: e0738528aa9effbaaf3d6d8850940f7ff126c24c

--- chrome/browser/resources/options/search_engine_manager_engine_list.js
+++ chrome/browser/resources/options/search_engine_manager_engine_list.js
@@ -111,6 +111,7 @@ cr.define('options.search_engines', function() {
         this.classList.add('default');
 
       this.deletable = engine.canBeRemoved;
+      this.closeButtonElement.tabIndex = 0;
 
       // Construct the name column.
       var nameColEl = this.ownerDocument.createElement('div');
