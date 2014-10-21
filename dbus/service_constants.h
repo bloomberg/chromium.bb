8b21f008a6e98f7ae4b4456fb75065feec15feda
Paul Stewart <pstew@chromium.org>
2014-10-21 08:20:56 -0700

    shill: Service: Add ManagedCredentials property
    Add a property to indicate that the credentials of this service
    is managed, and does not need validation before use.
    
    BUG=chromium:424036
    TEST=Compile shill
    
    Change-Id: I842a98b6a201ccaefd8f63cfd9c5cc993bbb2ee0
    Reviewed-on: https://chromium-review.googlesource.com/224581
    Reviewed-by: Peter Qiu <zqiu@chromium.org>
    Tested-by: Paul Stewart <pstew@chromium.org>
    Commit-Queue: Paul Stewart <pstew@chromium.org>

--- dbus/service_constants.h
+++ dbus/service_constants.h
@@ -846,6 +846,7 @@ const char kL2tpIpsecCaCertPemProperty[] = "L2TPIPsec.CACertPEM";
 const char kL2tpIpsecTunnelGroupProperty[] = "L2TPIPsec.TunnelGroup";
 const char kL2tpIpsecXauthPasswordProperty[] = "L2TPIPsec.XauthPassword";
 const char kL2tpIpsecXauthUserProperty[] = "L2TPIPsec.XauthUser";
+const char kManagedCredentialsProperty[] = "ManagedCredentials";
 const char kOpenVPNCaCertPemProperty[] = "OpenVPN.CACertPEM";
 const char kOpenVPNCertProperty[] = "OpenVPN.Cert";
 const char kOpenVPNExtraCertPemProperty[] = "OpenVPN.ExtraCertPEM";
