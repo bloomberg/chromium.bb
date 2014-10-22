fe7512131266e129c51e9391a18959764bccb2a7
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2014-10-22 15:23:36 +0200

    Make telemetry_unittests.py work on Windows
    BUG=422235
    R=bartfab@chromium.org
    
    Review URL: https://codereview.chromium.org/647103003
    
    Cr-Commit-Position: refs/heads/master@{#300676}
    
    Former-commit-id: f9f4000b3b94e82f7cf22277c649618f0bb97b66

--- testing/scripts/telemetry_unittests.py
+++ testing/scripts/telemetry_unittests.py
@@ -18,6 +18,7 @@ def main_run(args):
 
   with common.temporary_file() as tempfile_path:
     rc = common.run_command([
+        sys.executable,
         os.path.join(args.paths['build'], 'scripts', 'tools', 'runit.py'),
         '--show-path',
         os.path.join(args.paths['build'], 'scripts', 'slave', 'runtest.py'),
