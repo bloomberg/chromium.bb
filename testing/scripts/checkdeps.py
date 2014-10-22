34c77626cc86c118c16adc1745c4bfd87561cfb6
jam <jam@chromium.org>
2014-10-22 09:38:25 -0700

    Revert of Add src-side launcher for telemetry_unittests (patchset #7 id:120001 of https://codereview.chromium.org/649683005/)
    Reason for revert:
    windows trybots broken
    
    please be around after landing changes and verify that bots are working
    
    Original issue's description:
    > Add src-side launcher for telemetry_unittests
    >
    > This will need a recipe-side change to pass the paths known to recipe
    > (from the path recipe module) and properties (from the properties
    > recipe module).
    >
    > Each of these key-value pairs is passed as two command line arguments,
    > so that parsing them is easy and rock solid (avoids escaping issues),
    > and is also easy to convert to dict python-side.
    >
    > BUG=422235
    > R=iannucci@chromium.org
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/85fbbf62a588a9d621205e42428382735c6075f4 [formerly 5316422143176a4a6457ce173ec9597a2e0eda69]
    
    TBR=iannucci@chromium.org,phajdan.jr@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=422235
    
    Review URL: https://codereview.chromium.org/655843004
    
    Cr-Commit-Position: refs/heads/master@{#300687}
    
    Former-commit-id: 18b0c3bbb6a6a64236dc3bd162f9f77b7d9387a2

--- testing/scripts/checkdeps.py
+++ testing/scripts/checkdeps.py
@@ -3,18 +3,41 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import argparse
+import contextlib
 import json
 import os
+import subprocess
 import sys
+import tempfile
 
 
-import common
+SCRIPT_DIR = os.path.abspath(os.path.dirname(__file__))
+SRC_DIR = os.path.abspath(
+    os.path.join(SCRIPT_DIR, os.path.pardir, os.path.pardir))
 
 
-def main_run(args):
-  with common.temporary_file() as tempfile_path:
-    rc = common.run_command([
-        os.path.join(common.SRC_DIR, 'buildtools', 'checkdeps', 'checkdeps.py'),
+def run_command(argv):
+  print 'Running %r' % argv
+  rc = subprocess.call(argv)
+  print 'Command %r returned exit code %d' % (argv, rc)
+  return rc
+
+
+@contextlib.contextmanager
+def temporary_file():
+  fd, path = tempfile.mkstemp()
+  os.close(fd)
+  try:
+    yield path
+  finally:
+    os.remove(path)
+
+
+def mode_run(args):
+  with temporary_file() as tempfile_path:
+    rc = run_command([
+        os.path.join(SRC_DIR, 'buildtools', 'checkdeps', 'checkdeps.py'),
         '--json', tempfile_path
     ])
 
@@ -26,21 +49,27 @@ def main_run(args):
     for violation in result['violations']:
       result_set.add((result['dependee_path'], violation['include_path']))
 
-  json.dump({
-      'valid': True,
-      'failures': ['%s: %s' % (r[0], r[1]) for r in result_set],
-  }, args.output)
+  with open(args.output, 'w') as f:
+    json.dump({
+        'valid': True,
+        'failures': ['%s: %s' % (r[0], r[1]) for r in result_set],
+    }, f)
 
   return rc
 
 
-def main_compile_targets(args):
-  json.dump([], args.output)
+def main(argv):
+  parser = argparse.ArgumentParser()
+
+  subparsers = parser.add_subparsers()
+
+  run_parser = subparsers.add_parser('run')
+  run_parser.add_argument('--output', required=True)
+  run_parser.set_defaults(func=mode_run)
+
+  args = parser.parse_args(argv)
+  return args.func(args)
 
 
 if __name__ == '__main__':
-  funcs = {
-    'run': main_run,
-    'compile_targets': main_compile_targets,
-  }
-  sys.exit(common.run_script(sys.argv[1:], funcs))
+  sys.exit(main(sys.argv[1:]))
