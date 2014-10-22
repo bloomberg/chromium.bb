2927eee4afe077b7cb7e9adf26dbeb98e36771a3
yurys@chromium.org <yurys@chromium.org>
2014-10-22 08:32:43 +0000

    DevTools: remove unnecessary setInterval from worker-events.html test
    BUG=None
    
    Review URL: https://codereview.chromium.org/643533003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184152 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 016f349b25de1ae94e49713c02219e933ac36178

--- third_party/WebKit/LayoutTests/inspector/tracing/resources/worker.js
+++ third_party/WebKit/LayoutTests/inspector/tracing/resources/worker.js
@@ -1,10 +1,5 @@
-onmessage = function(event) {
-    doWork();
-    // FIXME: we need a better way of waiting for layout/repainting to happen
-    setInterval(doWork, 1);
-};
 var message_id = 0;
-function doWork()
+onmessage = function(event)
 {
-    postMessage("Message #" + message_id++);
-}
+    postMessage("Ack #" + message_id++);
+};
