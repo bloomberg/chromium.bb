eb3b210dc58ac60fa2f7c9e302f3e15c5ff9934b
mostynb <mostynb@opera.com>
2014-10-22 08:10:05 -0700

    remove OVERRIDE/FINAL presubmit checks now the macros are gone
    BUG=417463
    
    Review URL: https://codereview.chromium.org/670073003
    
    Cr-Commit-Position: refs/heads/master@{#300681}
    
    Former-commit-id: a924ebe97ccde57415f5c7fd94109663d2d1d6a2

--- base/PRESUBMIT.py
+++ base/PRESUBMIT.py
@@ -8,10 +8,6 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details on the presubmit API built into gcl.
 """
 
-import re
-
-BASE_SOURCE_FILES=(r'^base/.*\.(cc|h|mm)$',)
-
 def _CheckNoInterfacesInBase(input_api, output_api):
   """Checks to make sure no files in libbase.a have |@interface|."""
   pattern = input_api.re.compile(r'^\s*@interface', input_api.re.MULTILINE)
@@ -40,45 +36,8 @@ def _CommonChecks(input_api, output_api):
   results.extend(_CheckNoInterfacesInBase(input_api, output_api))
   return results
 
-def _CheckOverrideFinal(input_api, output_api,
-                       whitelist=BASE_SOURCE_FILES, blacklist=None):
-  """Make sure new lines of code don't use the OVERRIDE or FINAL macros."""
-
-  # TODO(mostynb): remove this check once the macros are removed
-  # from base/compiler_specific.h.
-
-  errors = []
-
-  source_file_filter = lambda x: input_api.FilterSourceFile(
-    x, white_list=BASE_SOURCE_FILES, black_list=None)
-
-  override_files = []
-  final_files = []
-
-  for f in input_api.AffectedSourceFiles(source_file_filter):
-    contents = input_api.ReadFile(f, 'rb')
-
-    # "override" and "final" should be used instead of OVERRIDE/FINAL now.
-    if re.search(r"\bOVERRIDE\b", contents):
-      override_files.append(f.LocalPath())
-
-    if re.search(r"\bFINAL\b", contents):
-      final_files.append(f.LocalPath())
-
-  if override_files:
-    return [output_api.PresubmitError(
-      'These files use OVERRIDE instead of using override:',
-      items=override_files)]
-  if final_files:
-    return [output_api.PresubmitError(
-      'These files use FINAL instead of using final:',
-      items=final_files)]
-
-  return []
-
 def CheckChangeOnUpload(input_api, output_api):
   results = []
-  results.extend(_CheckOverrideFinal(input_api, output_api))
   results.extend(_CommonChecks(input_api, output_api))
   return results
 
