b58c4e2d957f5ff603d17d9f38335209ce3cdfb7
boliu <boliu@chromium.org>
2014-10-23 16:55:38 -0700

    aw: Do not limit number of tiles
    By making the limit unrealistically high.
    
    BUG=426548
    
    Review URL: https://codereview.chromium.org/660383004
    
    Cr-Commit-Position: refs/heads/master@{#300991}
    
    Former-commit-id: 9448dfa74ccf2ecbe7aa8be87701d5fc9fedf215

--- android_webview/browser/browser_view_renderer.cc
+++ android_webview/browser/browser_view_renderer.cc
@@ -61,8 +61,9 @@ void BrowserViewRenderer::CalculateTileMemoryPolicy() {
     g_memory_override_in_bytes *= 1024 * 1024;
   }
 
-  // Also use a high tile limit since there are no file descriptor issues.
-  GlobalTileManager::GetInstance()->SetTileLimit(1000);
+  // There is no need to limit number of tiles, so use an effectively unlimited
+  // value as the limit.
+  GlobalTileManager::GetInstance()->SetTileLimit(10 * 1000 * 1000);
 }
 
 BrowserViewRenderer::BrowserViewRenderer(
