ed4873355aa2069748a1a6fdea902059e39d1f48
yhirano@chromium.org <yhirano@chromium.org>
2014-10-24 00:39:11 +0000

    Fix broken Promise-related layout tests.
    Because of the bindings change related to Promise-returning functions (see
    http://crbug.com/359411), some layout tests that had been written before that
    have been broken. As the bindings change is done, this CL fixes these tests.
    
    BUG=362992
    
    Review URL: https://codereview.chromium.org/669353002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184319 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 442a455913eabb7e07ce5f43047a13a37c5dc19a

--- third_party/WebKit/LayoutTests/fast/canvas/resources/canvas-createImageBitmap-invalid-args-in-workers.js
+++ third_party/WebKit/LayoutTests/fast/canvas/resources/canvas-createImageBitmap-invalid-args-in-workers.js
@@ -4,12 +4,30 @@ self.jsTestIsAsync = true;
 
 description('Test createImageBitmap with invalid arguments in workers.');
 
+var reason;
+
+function shouldBeRejected(promise, message) {
+    return promise.then(function() {
+        testFailed('Resolved unexpectedly: ' + message);
+    }, function(e) {
+        reason = e;
+        testPassed('Rejected as expected: ' + message);
+        shouldBeTrue('reason instanceof Error');
+        debug(String(e));
+    });
+}
+
 var data;
 
 self.addEventListener('message', function(e) {
-  data = e.data;
-  shouldThrow("createImageBitmap(null, 0, 0, 10, 10)");
-  shouldThrow("createImageBitmap(data, 0, 0, 10, 0)");
-  shouldThrow("createImageBitmap(data, 0, 0, 0, 10)");
-  finishJSTest();
+    data = e.data;
+    Promise.resolve().then(function() {
+        return shouldBeRejected(createImageBitmap(null, 0, 0, 10, 10), 'null');
+    }).then(function() {
+        return shouldBeRejected(createImageBitmap(data, 0, 0, 10, 0), 'invalid area');
+    }).then(function() {
+        return shouldBeRejected(createImageBitmap(data, 0, 0, 0, 10), 'invalid area');
+    }).catch(function(e) {
+        testFailed('Unexpected rejection: ' + e);
+    }).then(finishJSTest, finishJSTest);
 });
