9a9aabb144c1e683344e831e55c9d0cee4d2afd5
Kouhei Ueno <kouhei@chromium.org>
2014-10-24 13:27:50 +0900

    Revert "Enabled the App Info dialog on the Extensions page by default"
    The patch has caused compilation error again.
    
    TBR=sashab
    NOTREECHECK=true
    
    > This reverts commit 19552dfc4c67bf74d48d73be634c29ceb75ce150 [formerly 11778438dcb7670324f52e5df111e4007b9224ac].
    >
    > Enabled the App Info dialog on the Extensions page by default
    >
    > Changed the flag '--enable-extension-info-dialog' to
    > '--disable-extension-info-dialog', which enables the App Info dialog as
    > the default dialog for displaying permissions on the extensions page.
    > Does not affect Mac platforms (where the old-style dialog is still
    > used).
    >
    > BUG=395495
    > Committed: https://crrev.com/f2b4eaa1e35900b72dc31eec351e72e565e1ff2e [formerly a867ca2b5e42207b65e9d07702138ff79bed0bb0]
    > Cr-Commit-Position: refs/heads/master@{#301006}
    >
    > Committed: https://crrev.com/19552dfc4c67bf74d48d73be634c29ceb75ce150 [formerly 11778438dcb7670324f52e5df111e4007b9224ac]
    > Cr-Commit-Position: refs/heads/master@{#301046}
    
    Review URL: https://codereview.chromium.org/671373002
    
    Cr-Commit-Position: refs/heads/master@{#301047}
    
    Former-commit-id: b9f5c40b0e3252fe2ba8fe1ce8ee347f9e0b78ab

--- chrome/browser/ui/webui/extensions/extension_settings_handler.cc
+++ chrome/browser/ui/webui/extensions/extension_settings_handler.cc
@@ -108,22 +108,9 @@ using content::RenderViewHost;
 using content::WebContents;
 
 namespace {
-
 const char kAppsDeveloperToolsExtensionId[] =
     "ohmmkhmmmpcnpikjeljgnaoabkaalbgc";
-
-// Returns true if the extensions page should display the new-style extension
-// info dialog. If false, display the old permissions dialog.
-bool ShouldDisplayExtensionInfoDialog() {
-#if defined(OS_MACOSX)
-  return false;
-#else
-  return !base::CommandLine::ForCurrentProcess()->HasSwitch(
-      extensions::switches::kDisableExtensionInfoDialog);
-#endif
 }
-
-}  // namespace
 
 namespace extensions {
 
@@ -553,7 +540,8 @@ void ExtensionSettingsHandler::GetLocalizedValues(
       l10n_util::GetStringUTF16(IDS_EXTENSIONS_RELOAD_UNPACKED));
   source->AddString("extensionSettingsOptions",
       l10n_util::GetStringUTF16(IDS_EXTENSIONS_OPTIONS_LINK));
-  if (ShouldDisplayExtensionInfoDialog()) {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableExtensionInfoDialog)) {
     source->AddString("extensionSettingsPermissions",
                       l10n_util::GetStringUTF16(IDS_EXTENSIONS_INFO_LINK));
   } else {
@@ -1214,7 +1202,8 @@ void ExtensionSettingsHandler::HandlePermissionsMessage(
 
   // Show the new-style extensions dialog when the flag is set. The flag cannot
   // be set on Mac platforms.
-  if (ShouldDisplayExtensionInfoDialog()) {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableExtensionInfoDialog)) {
     UMA_HISTOGRAM_ENUMERATION("Apps.AppInfoDialog.Launches",
                               AppInfoLaunchSource::FROM_EXTENSIONS_PAGE,
                               AppInfoLaunchSource::NUM_LAUNCH_SOURCES);
