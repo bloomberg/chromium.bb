956920b3f900e6750f6767e0b4522563a613c7f6
mthiesse <mthiesse@chromium.org>
2014-10-27 12:28:54 -0700

    Add openCV dependency to install-build-deps.sh
    Context: https://codereview.chromium.org/660893002/
    
    BUG=424345
    
    Review URL: https://codereview.chromium.org/646033009
    
    Cr-Commit-Position: refs/heads/master@{#301404}
    
    Former-commit-id: 776da3a896a263d1edd906e6d29e4faad63c9d73

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -105,7 +105,7 @@ dev_list="apache2.2-bin bison cdbs curl dpkg-dev elfutils devscripts fakeroot
           libpulse-dev libsctp-dev libspeechd-dev libsqlite3-dev libssl-dev
           libudev-dev libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev
           mesa-common-dev openbox patch perl php5-cgi pkg-config python
-          python-cherrypy3 python-crypto python-dev python-openssl
+          python-cherrypy3 python-crypto python-dev python-opencv python-openssl
           python-psutil rpm ruby subversion ttf-dejavu-core ttf-indic-fonts
           ttf-kochi-gothic ttf-kochi-mincho wdiff xfonts-mathml zip
           $chromeos_dev_list"
