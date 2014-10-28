6190fd3bdb0d57a2834812eaa591d56ee4f60998
shuchen <shuchen@chromium.org>
2014-10-28 08:15:32 -0700

    Always call ChangeInputMethodInternal in SetState.
    For lock/unlock screen, the input method id may not be changed, but IMF
    still needs to notify IME extensions about the current screen type (e.g.
    'lock', 'normal', etc.) via chrome.input.ime.onActivate event, which is
    triggered by ChangeInputMethodInternal.
    
    BUG=427910
    TEST=Verified on linux_chromeos.
    
    Review URL: https://codereview.chromium.org/687623002
    
    Cr-Commit-Position: refs/heads/master@{#301633}
    
    Former-commit-id: fad5fa9ec041ffa0085e422c3e2f0caca07f8108

--- chrome/browser/chromeos/input_method/input_method_manager_impl.cc
+++ chrome/browser/chromeos/input_method/input_method_manager_impl.cc
@@ -797,11 +797,6 @@ void InputMethodManagerImpl::SetState(
   DCHECK(state.get());
   InputMethodManagerImpl::StateImpl* new_impl_state =
       static_cast<InputMethodManagerImpl::StateImpl*>(state.get());
-  const bool need_update_current_input_method =
-      (state_.get()
-           ? state_->current_input_method.id() !=
-                 new_impl_state->current_input_method.id()
-           : true);
 #if defined(USE_ATHENA)
   const bool profile_changed = (state_.get()
       ? state_->profile != new_impl_state->profile
@@ -820,16 +815,12 @@ void InputMethodManagerImpl::SetState(
     // indicator is used by only keyboard layout input methods.
     MaybeInitializeCandidateWindowController();
 
-    if (need_update_current_input_method) {
-      ChangeInputMethodInternal(state_->current_input_method,
-                                false /* show_message */,
-                                true /* notify_menu */);
-    } else {
-      // Update input method indicators (e.g. "US", "DV") in Chrome windows.
-      FOR_EACH_OBSERVER(InputMethodManager::Observer,
-                        observers_,
-                        InputMethodChanged(this, false /* show_message */));
-    }
+    // Always call ChangeInputMethodInternal even when the input method id
+    // remain unchanged, because onActivate event needs to be sent to IME
+    // extension to update the current screen type correctly.
+    ChangeInputMethodInternal(state_->current_input_method,
+                              false /* show_message */,
+                              true /* notify_menu */);
   }
 }
 
