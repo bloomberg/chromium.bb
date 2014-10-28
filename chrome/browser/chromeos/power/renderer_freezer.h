a8374d3ce463e38a75767d1630b97418990c6a9f
piman <piman@chromium.org>
2014-10-28 16:49:44 -0700

    Revert of chromeos: Add non-extension renderers to the freezer cgroup (patchset #8 id:140001 of https://codereview.chromium.org/656953002/)
    Reason for revert:
    New test has leaks and breaks LSAN build: http://build.chromium.org/p/chromium.memory/builders/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/builds/3840/steps/unit_tests/logs/stdio
    
    Original issue's description:
    > chromeos: Add non-extension renderers to the freezer cgroup
    >
    > In preparation for lucid sleep, we will be putting all chrome renderers
    > that do not host GCM extensions into a special freezer cgroup.  All
    > processes in this cgroup will be frozen when the system suspends and
    > will not be thawed until the system has fully resumed.
    >
    > The code to freeze and thaw the cgroup has existed for a while.  This
    > code actually starts putting processes into the cgroup.
    >
    > BUG=364339
    >
    > Committed: https://crrev.com/4f9f83e1405e287282906513404c353801574e8f [formerly d1a35405d1aba4894d56a927e35a8cfaa73b4875]
    > Cr-Commit-Position: refs/heads/master@{#301706}
    
    TBR=derat@chromium.org,rockot@chromium.org,nasko@chromium.org,chirantan@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=364339
    
    Review URL: https://codereview.chromium.org/687023002
    
    Cr-Commit-Position: refs/heads/master@{#301736}
    
    Former-commit-id: 481c45d4b51eac71f394f9e28edcb9247df09722

--- chrome/browser/chromeos/power/renderer_freezer.h
+++ chrome/browser/chromeos/power/renderer_freezer.h
@@ -5,24 +5,14 @@
 #ifndef CHROME_BROWSER_CHROMEOS_POWER_RENDERER_FREEZER_H_
 #define CHROME_BROWSER_CHROMEOS_POWER_RENDERER_FREEZER_H_
 
-#include <set>
-
 #include "base/callback.h"
 #include "base/cancelable_callback.h"
 #include "base/macros.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
-#include "base/process/kill.h"
 #include "base/time/time.h"
 #include "chromeos/chromeos_export.h"
 #include "chromeos/dbus/power_manager_client.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
-#include "content/public/browser/render_process_host_observer.h"
-
-namespace content {
-class RenderProcessHost;
-}
 
 namespace chromeos {
 
@@ -30,26 +20,18 @@ namespace chromeos {
 // them after the system fully resumes.  This class registers itself as a
 // PowerManagerClient::Observer on creation and unregisters itself on
 // destruction.
-class CHROMEOS_EXPORT RendererFreezer
-    : public PowerManagerClient::Observer,
-      public content::NotificationObserver,
-      public content::RenderProcessHostObserver {
+class CHROMEOS_EXPORT RendererFreezer : public PowerManagerClient::Observer {
  public:
   class Delegate {
    public:
     virtual ~Delegate() {}
 
-    // If |frozen| is true, marks the renderer process |handle| to be frozen
-    // when FreezeRenderers() is called; otherwise marks it to remain unfrozen.
-    virtual void SetShouldFreezeRenderer(base::ProcessHandle handle,
-                                         bool frozen) = 0;
-
-    // Freezes the renderers marked for freezing by SetShouldFreezeRenderer().
-    // Returns true if the operation was successful.
+    // Freezes the chrome renderers.  Returns true if the operation was
+    // successful.
     virtual bool FreezeRenderers() = 0;
 
-    // Thaws the chrome renderers that were frozen by the call to
-    // FreezeRenderers().  Returns true if the operation was successful.
+    // Thaws the chrome renderers.  Returns true if the operation was
+    // successful.
     virtual bool ThawRenderers() = 0;
 
     // Returns true iff the delegate is capable of freezing renderers.
@@ -57,45 +39,22 @@ class CHROMEOS_EXPORT RendererFreezer
   };
 
   explicit RendererFreezer(scoped_ptr<Delegate> delegate);
-  ~RendererFreezer() override;
+  virtual ~RendererFreezer();
 
-  // PowerManagerClient::Observer implementation.
-  void SuspendImminent() override;
-  void SuspendDone(const base::TimeDelta& sleep_duration) override;
-
-  // content::NotificationObserver implementation.
-  void Observe(int type,
-               const content::NotificationSource& source,
-               const content::NotificationDetails& details) override;
-
-  // content::RenderProcessHostObserver overrides.
-  void RenderProcessExited(content::RenderProcessHost* host,
-                           base::TerminationStatus status,
-                           int exit_code) override;
-  void RenderProcessHostDestroyed(content::RenderProcessHost* host) override;
+  // PowerManagerClient::Observer implementation
+  virtual void SuspendImminent() override;
+  virtual void SuspendDone(const base::TimeDelta& sleep_duration) override;
 
  private:
-  // Called whenever a new renderer process is created.
-  void OnRenderProcessCreated(content::RenderProcessHost* rph);
-
   // Called when all asynchronous work is complete and renderers can be frozen.
   void OnReadyToSuspend(const base::Closure& power_manager_callback);
 
   // Used to ensure that renderers do not get frozen if the suspend is canceled.
   base::CancelableClosure suspend_readiness_callback_;
 
-  // Tracks if the renderers are currently frozen.
   bool frozen_;
 
-  // Delegate that takes care of actually freezing and thawing renderers for us.
   scoped_ptr<Delegate> delegate_;
-
-  // Set that keeps track of the RenderProcessHosts for processes that are
-  // hosting GCM extensions.
-  std::set<int> gcm_extension_processes_;
-
-  // Manages notification registrations.
-  content::NotificationRegistrar registrar_;
 
   base::WeakPtrFactory<RendererFreezer> weak_factory_;
 
