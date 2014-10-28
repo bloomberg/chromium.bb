35efcaff93f4aa6b3d925b4dbc0ae4270c9f03c0
kcarattini <kcarattini@chromium.org>
2014-10-27 18:28:22 -0700

    Hotword Audio Verification App: UI polishes.
    Adds urls to the settings and 'learn more' links.
    Adds CSS animation to the speech training pages.
    Adds steps through speech training UI with clicks.
    
    BUG=390086
    
    Review URL: https://codereview.chromium.org/666073005
    
    Cr-Commit-Position: refs/heads/master@{#301542}
    
    Former-commit-id: d21850201a4ab626fdc703d9e534c04b683895d3

--- chrome/browser/resources/hotword_audio_verification/main.js
+++ chrome/browser/resources/hotword_audio_verification/main.js
@@ -8,75 +8,86 @@ document.addEventListener('DOMContentLoaded', function() {
   var flow = new Flow();
   flow.startFlow();
 
-  // Make the close buttons close the app window.
-  var closeButtons = document.getElementsByClassName('close');
-  for (var i = 0; i < closeButtons.length; ++i) {
-    var closeButton = closeButtons[i];
-    closeButton.addEventListener('click', function(e) {
+  var closeAppWindow = function(e) {
+    var classes = e.target.classList;
+    if (classes.contains('close') || classes.contains('finish-button')) {
       appWindow.close();
       e.preventDefault();
-    });
-  }
+    }
+  };
 
-  $('ah-cancel-button').addEventListener('click', function(e) {
-    appWindow.close();
-    e.preventDefault();
-  });
-
-  $('done-button').addEventListener('click', function(e) {
-    appWindow.close();
-    e.preventDefault();
-  });
-
-  $('ho-cancel-button').addEventListener('click', function(e) {
-    appWindow.close();
-    e.preventDefault();
-  });
-
-  $('hw-cancel-button').addEventListener('click', function(e) {
-    appWindow.close();
-    e.preventDefault();
-  });
-
-  $('st-cancel-button').addEventListener('click', function(e) {
-    appWindow.close();
-    e.preventDefault();
-  });
-
-  $('ah-agree-button').addEventListener('click', function(e) {
-    // TODO(kcarattini): Set the Audio History setting.
-    appWindow.close();
-    e.preventDefault();
-  });
+  // TODO(kcarattini): Cancel training in NaCl module for hotword-only and
+  // speech-training close and cancel buttons.
+  $('steps').addEventListener('click', closeAppWindow);
 
   $('hw-agree-button').addEventListener('click', function(e) {
     flow.advanceStep();
     e.preventDefault();
   });
 
-  // TODO(kcarattini): Remove this once speech training is implemented. The
-  // way to get to the next page will be to complete the speech training.
-  $('st-training').addEventListener('click', function(e) {
-    if (chrome.hotwordPrivate.setAudioLoggingEnabled)
-      chrome.hotwordPrivate.setAudioLoggingEnabled(true, function() {});
-
-    if (chrome.hotwordPrivate.setHotwordAlwaysOnSearchEnabled) {
-      chrome.hotwordPrivate.setHotwordAlwaysOnSearchEnabled(true,
-          flow.advanceStep.bind(flow));
-    }
+  $('settings-link').addEventListener('click', function(e) {
+    chrome.browser.openTab({'url': 'chrome://settings'}, function() {});
     e.preventDefault();
   });
 
-  // TODO(kcarattini): Remove this once speech training is implemented. The
-  // way to get to the next page will be to complete the speech training.
-  $('ho-training').addEventListener('click', function(e) {
-    if (chrome.hotwordPrivate.setAudioLoggingEnabled)
-      chrome.hotwordPrivate.setAudioLoggingEnabled(true, function() {});
+  /**
+   * Updates steps of the training UI.
+   * @param {string} pagePrefix Prefix of the element ids for this page.
+   * @param {Event} e The click event.
+   */
+  function doTraining(pagePrefix, e) {
+    // TODO(kcarattini): Update this to respond to a hotword trigger once
+    // speech training is implemented.
+    var steps = $(pagePrefix + '-training').querySelectorAll('.train');
+    var index = Array.prototype.indexOf.call(steps, e.currentTarget);
+    if (!steps[index])
+      return;
 
-    if (chrome.hotwordPrivate.setHotwordAlwaysOnSearchEnabled) {
-      chrome.hotwordPrivate.setHotwordAlwaysOnSearchEnabled(true,
-          flow.advanceStep.bind(flow));
+    // TODO(kcarattini): Localize this string.
+    steps[index].querySelector('.text').textContent = 'Recorded';
+    steps[index].classList.remove('listening');
+    steps[index].classList.add('recorded');
+
+    if (steps[index + 1]) {
+      steps[index + 1].classList.remove('not-started');
+      steps[index + 1].classList.add('listening');
     }
-    e.preventDefault();
-  });
+  }
+
+  /**
+   * Adds event listeners to the training UI.
+   * @param {string} pagePrefix Prefix of the element ids for this page.
+   */
+  function addTrainingEventListeners(pagePrefix) {
+    var steps = $(pagePrefix + '-training').querySelectorAll('.train');
+    for (var i = 0; i < steps.length; ++i) {
+      steps[i].addEventListener('click', function(e) {
+        doTraining(pagePrefix, e);
+        e.preventDefault();
+
+        if (e.currentTarget != steps[steps.length - 1])
+          return;
+
+        // Update the 'Cancel' button.
+        var buttonElem = $(pagePrefix + '-cancel-button');
+        // TODO(kcarattini): Localize this string.
+        buttonElem.textContent = 'Please wait ...';
+        buttonElem.classList.add('grayed-out');
+        buttonElem.classList.remove('finish-button');
+
+        setTimeout(function() {
+          if (chrome.hotwordPrivate.setAudioLoggingEnabled)
+            chrome.hotwordPrivate.setAudioLoggingEnabled(true, function() {});
+
+          if (chrome.hotwordPrivate.setHotwordAlwaysOnSearchEnabled) {
+            chrome.hotwordPrivate.setHotwordAlwaysOnSearchEnabled(true,
+                flow.advanceStep.bind(flow));
+          }
+        }, 2000);
+      });
+    }
+  }
+
+  addTrainingEventListeners('speech-training');
+  addTrainingEventListeners('hotword-only');
 });
