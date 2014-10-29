7ae75ee24bd3162e268e91c4e87afd2cba71ccfb
benchan <benchan@chromium.org>
2014-10-29 14:33:27 -0700

    Fix a 'type-limits' warning in StringSlicer::FindValidBoundaryAfter
    StringSlicer::FindValidBoundaryAfter passes a size_t value to the
    'length' argument of U16_SET_CP_LIMIT, which expects the argument to be
    an int32. When compiling with -Werror=type-limits, that results in the
    following compilation error:
    
    ../../ui/gfx/text_elider.cc: In member function 'size_t
    gfx::StringSlicer::FindValidBoundaryAfter(size_t) const':
    ../../ui/gfx/text_elider.cc:150:59: error: comparison of unsigned
    expression < 0 is always false [-Werror=type-limits]
         U16_SET_CP_LIMIT(text_.data(), 0, index, text_.length());
                                                               ^
    ../../third_party/icu/source/common/unicode/utf16.h:618:41: note: in
    definition of macro 'U16_SET_CP_LIMIT'
         if((start)<(i) && ((i)<(length) || (length)<0) &&
    U16_IS_LEAD((s)[(i)-1]) && U16_IS_TRAIL((s)[i])) { \
                                             ^
    
    This CL fixes the 'type-limits' warning above.
    
    BUG=424334, 427726
    TEST=Build successfully with '-Werror=type-limits' using the GN flow.
    
    Review URL: https://codereview.chromium.org/681123006
    
    Cr-Commit-Position: refs/heads/master@{#301935}
    
    Former-commit-id: 492b0cf0da08a9d102bb5465b298b797ef312068

--- ui/gfx/text_elider.cc
+++ ui/gfx/text_elider.cc
@@ -17,6 +17,7 @@
 #include "base/i18n/char_iterator.h"
 #include "base/i18n/rtl.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
 #include "base/strings/sys_string_conversions.h"
@@ -146,9 +147,13 @@ size_t StringSlicer::FindValidBoundaryBefore(size_t index) const {
 
 size_t StringSlicer::FindValidBoundaryAfter(size_t index) const {
   DCHECK_LE(index, text_.length());
-  if (index != text_.length())
-    U16_SET_CP_LIMIT(text_.data(), 0, index, text_.length());
-  return index;
+  if (index == text_.length())
+    return index;
+
+  int32_t text_index = base::checked_cast<int32_t>(index);
+  int32_t text_length = base::checked_cast<int32_t>(text_.length());
+  U16_SET_CP_LIMIT(text_.data(), 0, text_index, text_length);
+  return static_cast<size_t>(text_index);
 }
 
 base::string16 ElideFilename(const base::FilePath& filename,
