c95ab79cd6f2303e2d9975ec09bd8048a6a0ee9b
cpu <cpu@chromium.org>
2014-10-28 17:37:00 -0700

    Fixing rlz mutex
    0 - Remove special case for Vista.
    
    1- Remove the low integrity logic, broken, wether or not to wait
       has nothing to do with setting the low integrity label, which
       was racy and (sometimes) broken.
    
    2- Cleanup of style.
    
    BUG=426573
    
    Review URL: https://codereview.chromium.org/686733003
    
    Cr-Commit-Position: refs/heads/master@{#301748}
    
    Former-commit-id: 7cfb043eb32cdee60d4a233c350281a1f3624412

--- rlz/win/lib/lib_mutex.h
+++ rlz/win/lib/lib_mutex.h
@@ -1,8 +1,6 @@
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-// Mutex to guarantee serialization of RLZ key accesses.
 
 #ifndef RLZ_WIN_LIB_LIB_MUTEX_H_
 #define RLZ_WIN_LIB_LIB_MUTEX_H_
@@ -10,13 +8,13 @@
 #include <windows.h>
 
 namespace rlz_lib {
-
+// Cross-process mutex to guarantee serialization of RLZ key accesses.
 class LibMutex {
  public:
   LibMutex();
   ~LibMutex();
 
-  bool failed(void) { return !acquired_; }
+  bool failed() const { return !acquired_; }
 
  private:
   bool acquired_;
