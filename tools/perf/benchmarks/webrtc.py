b0cb67806e0027c05af8b206a0f885766e076a5c
dpranke <dpranke@chromium.org>
2014-10-28 17:58:32 -0700

    Disable WebRTC benchmark due to flakiness.
    TBR=tonyg@chromium.org, phoglund@chromium.org
    BUG=428157
    
    Review URL: https://codereview.chromium.org/684023002
    
    Cr-Commit-Position: refs/heads/master@{#301753}
    
    Former-commit-id: 64709a0055eb91ceb5d29b4d6bfdaefa8380143d

--- tools/perf/benchmarks/webrtc.py
+++ tools/perf/benchmarks/webrtc.py
@@ -7,6 +7,8 @@ import page_sets
 from telemetry import benchmark
 
 
+# this test is currently flaky: crbug.com/428157
+@benchmark.Disabled()
 class WebRTC(benchmark.Benchmark):
   """Obtains WebRTC metrics for a real-time video tests."""
   test = webrtc.WebRTC
