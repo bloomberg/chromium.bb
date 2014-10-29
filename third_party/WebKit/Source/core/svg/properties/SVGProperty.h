4e3d4d783070366f70f5f37b87c4512dc70e4ce0
sigbjornf@opera.com <sigbjornf@opera.com>
2014-10-29 19:43:53 +0000

    Oilpan: pull SVGPropertyBase off the heap.
    Having the SVG property base class derive from RefCountedGarbageCollected,
    but its derived classes all be created and kept as ref-counted pointers
    doesn't bring any benefits.
    
    Revert back to deriving from RefCounted<> until the entire SVG property
    hierarchy can be moved to the Oilpan heap, and ref-counts be avoided
    altogether for these objects.
    
    R=haraken
    BUG=340522
    
    Review URL: https://codereview.chromium.org/686163002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184600 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: e0c3596aab88ea3939ad56ec844be308d89d35b6

--- third_party/WebKit/Source/core/svg/properties/SVGProperty.h
+++ third_party/WebKit/Source/core/svg/properties/SVGProperty.h
@@ -43,7 +43,7 @@ namespace blink {
 class SVGElement;
 class SVGAnimationElement;
 
-class SVGPropertyBase : public RefCountedWillBeRefCountedGarbageCollected<SVGPropertyBase> {
+class SVGPropertyBase : public RefCounted<SVGPropertyBase> {
     WTF_MAKE_NONCOPYABLE(SVGPropertyBase);
 
 public:
@@ -84,36 +84,17 @@ public:
         m_ownerList = ownerList;
     }
 
-    virtual void trace(Visitor* visitor)
-    {
-        visitor->trace(m_ownerList);
-    }
-
 protected:
     explicit SVGPropertyBase(AnimatedPropertyType type)
         : m_type(type)
         , m_ownerList(nullptr)
     {
-#if ENABLE(OILPAN)
-        // FIXME: Oilpan: the objects that derive from this RefCountedGarbageCollected<>
-        // base object are all RefPtr<T>-exposed (i.e., create()s returning a PassRefPtr<T>.)
-        //
-        // Following r183582, RefCountedGarbageCollected<>::m_refCount is initialized
-        // as 0, which is safe assuming the constructor result is initially handled/returned
-        // as a raw pointer. But to support an "RefCounted-identical" view of such a fresh
-        // object, we need to adjust the RefCountedGarbageCollected<>::m_refCount to be
-        // its assumed 1 for these derived objects to function as expected.
-        //
-        // This would be simpler if the derived SVG property objects created GCed objects
-        // proper. Remove this ref count adjustment when this happens.
-        ref();
-#endif
     }
 
 private:
     const AnimatedPropertyType m_type;
 
-    RawPtrWillBeMember<SVGPropertyBase> m_ownerList;
+    SVGPropertyBase* m_ownerList;
 };
 
 }
