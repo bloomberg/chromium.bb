aae3cc5e3d3b091b9d0e3432fff5e50923fc1cd1
yurys@chromium.org <yurys@chromium.org>
2014-10-30 14:43:13 +0000

    DevTools: add Timeline.start command use counter
    This is going to be used for tracking usage of the deprecated Timeline implementation.
    
    BUG=428701
    
    Review URL: https://codereview.chromium.org/695453002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184653 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2e3d7d99b23c8bc72e7ebecb4932c898736c2a50

--- third_party/WebKit/Source/core/inspector/InspectorTimelineAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTimelineAgent.cpp
@@ -37,6 +37,7 @@
 #include "core/frame/FrameHost.h"
 #include "core/frame/FrameView.h"
 #include "core/frame/LocalFrame.h"
+#include "core/frame/UseCounter.h"
 #include "core/inspector/ConsoleMessage.h"
 #include "core/inspector/IdentifiersFactory.h"
 #include "core/inspector/InspectorClient.h"
@@ -355,6 +356,11 @@ void InspectorTimelineAgent::start(ErrorString* errorString, const int* maxCallS
     if (!m_frontend)
         return;
     m_state->setBoolean(TimelineAgentState::startedFromProtocol, true);
+
+    if (LocalFrame* frame = mainFrame()) {
+        if (UseCounter* useCounter = UseCounter::getFrom(frame->document()))
+            useCounter->count(UseCounter::TimelineStart);
+    }
 
     if (isStarted()) {
         *errorString = "Timeline is already started";
