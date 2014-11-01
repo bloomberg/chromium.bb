efc0449ea629b6b1a35d0e0065e5d11f658fb51b
zmo <zmo@chromium.org>
2014-11-01 09:25:50 -0700

    Revert of Revert of Add logs to debug a flakiness on GPU Mac bots. (patchset #1 id:1 of https://codereview.chromium.org/684253003/)
    Reason for revert:
    Ah, reverted the wrong CL.
    
    Original issue's description:
    > Revert of Add logs to debug a flakiness on GPU Mac bots. (patchset #2 id:20001 of https://codereview.chromium.org/684423005/)
    >
    > Reason for revert:
    > Should turn on logging in telemetry_gpu_tests in general.
    >
    > Original issue's description:
    > > Add logs to debug a flakiness on GPU Mac bots.
    > >
    > > This is temporary and will be removed after we pin down the issue.
    > >
    > > BUG=424024
    > > TEST=
    > > R=kbr@chromium.org
    > > TBR=dgozman@chromium.org
    > > NOTRY=true
    > >
    > > Committed: https://crrev.com/d4f534b6bf18b4b9abfba5ed2aec263ead7596d4 [formerly 16aa242c3456dc68cd91ef2c683158799c8441b0]
    > > Cr-Commit-Position: refs/heads/master@{#302371}
    >
    > TBR=dgozman@chromium.org,kbr@chromium.org,pfeldman@chromium.org,yurys@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=424024
    >
    > Committed: https://crrev.com/56cf1b42db04742f3b4b561d24992f9f94fba3f3 [formerly ec506e7f57922e5a068d76128f020877bc7986cb]
    > Cr-Commit-Position: refs/heads/master@{#302387}
    
    TBR=dgozman@chromium.org,kbr@chromium.org,pfeldman@chromium.org,yurys@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=424024
    
    Review URL: https://codereview.chromium.org/698783002
    
    Cr-Commit-Position: refs/heads/master@{#302388}
    
    Former-commit-id: ce1b1bf47b9a1f60d604a5f766a28e4948e598dc

--- content/browser/devtools/devtools_http_handler_impl.cc
+++ content/browser/devtools/devtools_http_handler_impl.cc
@@ -163,12 +163,14 @@ class DevToolsHttpHandlerImpl::BrowserTarget {
         tracing_handler_(new devtools::tracing::TracingHandler(
             devtools::tracing::TracingHandler::Browser)),
         protocol_handler_(new DevToolsProtocolHandlerImpl()) {
+    VLOG(2) << "BrowserTarget::BrowserTarget()";
     protocol_handler_->SetNotifier(
         base::Bind(&BrowserTarget::Respond, base::Unretained(this)));
     protocol_handler_->SetTracingHandler(tracing_handler_.get());
   }
 
   ~BrowserTarget() {
+    VLOG(2) << "BrowserTarget::~BrowserTarget()";
     STLDeleteElements(&handlers_);
   }
 
@@ -700,6 +702,7 @@ void DevToolsHttpHandlerImpl::OnWebSocketRequestUI(
   std::string browser_prefix = "/devtools/browser";
   size_t browser_pos = request.path.find(browser_prefix);
   if (browser_pos == 0) {
+    VLOG(2) << "OnWebSocketRequestUI: new BrowserTarget";
     BrowserTarget* browser_target = new BrowserTarget(
         thread_->message_loop(), server_.get(), connection_id);
     browser_target->RegisterHandler(
