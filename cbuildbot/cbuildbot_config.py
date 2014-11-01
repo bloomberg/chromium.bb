5ec59ef91802eaf3a8951a6a2abf651468866035
David James <davidjames@chromium.org>
2014-11-01 17:09:23 +0000

    Revert "cbuildbot_config: Switch daisy_skate in for daisy_spring in Chrome PFQ."
    Builders are failing with the following error:
      daisy_skate-chrome-pfq did not start
    
    This will require a more complicated transition to make 
    work.
    
    1. Set up the new machines.
    2. Set up the new Chrome PFQs & mark them as important.
    3. Set up setup_board to use daisy_skate Chrome prebuilts
       for arm platforms.
    4. Mark daisy_spring Chrome PFQ as not important.
    5. Shut down daisy_spring Chrome PFQ.
    
    BUG=chromium:429304
    TEST=none
    
    Change-Id: I9e7594ba2661ca7a23d234ece5f4b76c57beb112
    Reviewed-on: https://chromium-review.googlesource.com/226940
    Reviewed-by: David James <davidjames@chromium.org>
    Tested-by: David James <davidjames@chromium.org>

--- cbuildbot/cbuildbot_config.py
+++ cbuildbot/cbuildbot_config.py
@@ -1264,9 +1264,9 @@ chrome_pfq.add_config('lumpy-chrome-pfq',
       minimum_duts=0, timeout=2*HWTestConfig.DEFAULT_HW_TEST_TIMEOUT),
 )
 
-chrome_pfq.add_config('daisy_skate-chrome-pfq',
+chrome_pfq.add_config('daisy_spring-chrome-pfq',
   non_testable_builder,
-  boards=['daisy_skate'],
+  boards=['daisy_spring'],
   hw_tests=HWTestConfig.DefaultListPFQ(),
 )
 
