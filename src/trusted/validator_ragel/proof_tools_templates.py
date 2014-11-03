d44cd7bf8f91e0488556a7a0badef5ac6b4fac44
shyamsundarr@chromium.org <shyamsundarr@chromium.org>
2014-11-03 20:32:38 +0000

    enable packed double precision 3 operand mem/simd register avx1 instructions.
    All these instructions allow the forms:
    op src1=xmm src2=xmm/mem src3=xmm
    and
    op src1=ymm src2=ymm/mem src3=ymm
    
    The enabled instructions are:
    
    'vaddpd', 'vaddsubpd', 'vandnpd', 'vandpd', 'vdivpd', 'vhaddpd',
    'vhsubpd', 'vmaxpd', 'vminpd', 'vmulpd', 'vorpd', 'vsubpd', 'vunpckhpd',
    'vunpcklpd', 'vxorpd'
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=1972
    R=kschimpf@google.com
    
    Review URL: https://codereview.chromium.org/697143002
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@14020 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 9845a26a4a59f1eb04432cce1aa8015788f23a75

--- src/trusted/validator_ragel/proof_tools_templates.py
+++ src/trusted/validator_ragel/proof_tools_templates.py
@@ -31,3 +31,27 @@ def LockedRegWithRegOrMem16bit(mnemonic_name, bitness):
   lock = proof_tools.LockPrefix()
   return (proof_tools.OpsProd(lock, instr, reg16, mem) |
           proof_tools.OpsProd(instr, reg16, reg16 | mem))
+
+
+def XmmYmmOrMemory3operand(mnemonic_name, bitness):
+  """Set of 3 operand xmm/ymm/memory ops (memory is a possible source operand).
+
+  e.g.
+
+  instr xmm1/mem, xmm2, xmm3
+  or
+  instr ymm1/mem, ymm2, ymm3
+
+  Args:
+    mnemonic_name: the mnemonic we are producing the pattern for.
+    bitness: the bitness of the architecture (32 or 64)
+  Returns:
+    All possible disassembly sequences that follow the pattern.
+  """
+  xmm = proof_tools.AllXMMOperands(bitness)
+  ymm = proof_tools.AllYMMOperands(bitness)
+  mem = proof_tools.AllMemoryOperands(bitness)
+  mnemonic = proof_tools.MnemonicOp(mnemonic_name)
+
+  return (proof_tools.OpsProd(mnemonic, (xmm | mem), xmm, xmm) |
+          proof_tools.OpsProd(mnemonic, (ymm | mem), ymm, ymm))
