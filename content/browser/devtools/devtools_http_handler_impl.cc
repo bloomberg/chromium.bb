8f16e4cd8dd6cc922705f1f6ea5833aabbc3e3f2
zmo <zmo@chromium.org>
2014-11-03 10:51:28 -0800

    Adding more logging to see why DevToolsHttpHandlerImpl::BrowserTarget is flaky.
    BUG=424024
    TEST=
    TBR=kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/702503002
    
    Cr-Commit-Position: refs/heads/master@{#302453}
    
    Former-commit-id: d3ac15e57aade01f1524f554e00c6113e8e9b364

--- content/browser/devtools/devtools_http_handler_impl.cc
+++ content/browser/devtools/devtools_http_handler_impl.cc
@@ -455,6 +455,7 @@ void DevToolsHttpHandlerImpl::OnHttpRequest(
 void DevToolsHttpHandlerImpl::OnWebSocketRequest(
     int connection_id,
     const net::HttpServerRequestInfo& request) {
+  VLOG(2) << "OnWebSocketRequest";
   BrowserThread::PostTask(
       BrowserThread::UI,
       FROM_HERE,
@@ -696,8 +697,11 @@ void DevToolsHttpHandlerImpl::OnDiscoveryPageRequestUI(int connection_id) {
 void DevToolsHttpHandlerImpl::OnWebSocketRequestUI(
     int connection_id,
     const net::HttpServerRequestInfo& request) {
-  if (!thread_)
+  VLOG(2) << "OnWebSocketRequestUI : " << request.path;
+  if (!thread_) {
+    VLOG(2) << "OnWebSocketRequestUI : !thread_";
     return;
+  }
 
   std::string browser_prefix = "/devtools/browser";
   size_t browser_pos = request.path.find(browser_prefix);
