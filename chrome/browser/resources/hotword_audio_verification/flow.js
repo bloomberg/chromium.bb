6bb201179322051ad95e2808834320fcbd909fd9
kcarattini <kcarattini@chromium.org>
2014-11-03 18:38:18 -0800

    Hotword Audio Verification App: update training UI on hotword trigger.
    BUG=390086
    
    Review URL: https://codereview.chromium.org/693433002
    
    Cr-Commit-Position: refs/heads/master@{#302554}
    
    Former-commit-id: 960939ae20f289d38da08b3f943c16e725deb339

--- chrome/browser/resources/hotword_audio_verification/flow.js
+++ chrome/browser/resources/hotword_audio_verification/flow.js
@@ -23,6 +23,17 @@
   ];
 
   /**
+   * The launch mode. This enum needs to be kept in sync with that of
+   * the same name in hotword_service.h.
+   * @enum {number}
+   */
+  var LaunchMode = {
+    HOTWORD_ONLY: 0,
+    HOTWORD_AND_AUDIO_HISTORY: 1,
+    RETRAIN: 2
+  };
+
+  /**
    * Class to control the page flow of the always-on hotword and
    * Audio History opt-in process.
    * @constructor
@@ -30,6 +41,24 @@
   function Flow() {
     this.currentStepIndex_ = -1;
     this.currentFlow_ = [];
+
+    /**
+     * Whether this flow is currently in the process of training a voice model.
+     * @private {LaunchMode}
+     */
+    this.launchMode_ = LaunchMode.HOTWORD_AND_AUDIO_HISTORY;
+
+    /**
+     * Whether this flow is currently in the process of training a voice model.
+     * @private {boolean}
+     */
+    this.training_ = false;
+
+    /**
+     * Prefix of the element ids for the page that is currently training.
+     * @private {string}
+     */
+    this.trainingPagePrefix_ = '';
   }
 
   /**
@@ -49,17 +78,127 @@
       chrome.hotwordPrivate.getLaunchState(this.startFlowForMode_.bind(this));
   };
 
+  /**
+   * Starts the training process.
+   */
+  Flow.prototype.startTraining = function() {
+    // Don't start a training session if one already exists.
+    if (this.training_)
+      return;
+
+    this.training_ = true;
+    if (this.launchMode_ == LaunchMode.HOTWORD_ONLY ||
+        this.launchMode_ == LaunchMode.RETRAIN) {
+      this.trainingPagePrefix_ = 'hotword-only';
+    } else if (this.launchMode_ == LaunchMode.HOTWORD_AND_AUDIO_HISTORY) {
+      this.trainingPagePrefix_ = 'speech-training';
+    }
+
+    if (chrome.hotwordPrivate.onHotwordTriggered) {
+      chrome.hotwordPrivate.onHotwordTriggered.addListener(
+          this.handleHotwordTrigger_.bind(this));
+    }
+    if (chrome.hotwordPrivate.startTraining)
+      chrome.hotwordPrivate.startTraining();
+  };
+
+  /**
+   * Stops the training process.
+   */
+  Flow.prototype.stopTraining = function() {
+    if (!this.training_)
+      return;
+
+    this.training_ = false;
+    if (chrome.hotwordPrivate.onHotwordTriggered) {
+      chrome.hotwordPrivate.onHotwordTriggered.
+          removeListener(this.handleHotwordTrigger_);
+    }
+    if (chrome.hotwordPrivate.stopTraining)
+      chrome.hotwordPrivate.stopTraining();
+  };
+
+  /**
+   * Handles the speaker model finalized event.
+   */
+  Flow.prototype.onSpeakerModelFinalized = function() {
+    this.stopTraining();
+
+    if (chrome.hotwordPrivate.setAudioLoggingEnabled)
+      chrome.hotwordPrivate.setAudioLoggingEnabled(true, function() {});
+
+    if (chrome.hotwordPrivate.setHotwordAlwaysOnSearchEnabled) {
+      chrome.hotwordPrivate.setHotwordAlwaysOnSearchEnabled(true,
+          this.advanceStep.bind(this));
+    }
+  };
+
   // ---- private methods:
 
   /**
+   * Completes the training process.
+   * @private
+   */
+  Flow.prototype.finalizeSpeakerModel_ = function() {
+    if (!this.training_)
+      return;
+
+    if (chrome.hotwordPrivate.finalizeSpeakerModel)
+      chrome.hotwordPrivate.finalizeSpeakerModel();
+
+    // TODO(kcarattini): Implement a notification that speaker model has been
+    // finalized instead of setting a timeout.
+    setTimeout(this.onSpeakerModelFinalized.bind(this), 2000);
+  };
+
+  /**
+   * Handles a hotword trigger event and updates the training UI.
+   * @private
+   */
+  Flow.prototype.handleHotwordTrigger_ = function() {
+    var curStep =
+        $(this.trainingPagePrefix_ + '-training').querySelector('.listening');
+    // TODO(kcarattini): Localize this string.
+    curStep.querySelector('.text').textContent = 'Recorded';
+    curStep.classList.remove('listening');
+    curStep.classList.add('recorded');
+
+    var steps =
+        $(this.trainingPagePrefix_ + '-training').querySelectorAll('.train');
+    var index = Array.prototype.indexOf.call(steps, curStep);
+    if (steps[index + 1]) {
+      steps[index + 1].classList.remove('not-started');
+      steps[index + 1].classList.add('listening');
+      return;
+    }
+
+    // Only the last step makes it here.
+    var buttonElem = $(this.trainingPagePrefix_ + '-cancel-button');
+    // TODO(kcarattini): Localize this string.
+    buttonElem.textContent = 'Please wait ...';
+    buttonElem.classList.add('grayed-out');
+    buttonElem.classList.remove('finish-button');
+
+    this.finalizeSpeakerModel_();
+  };
+
+  /**
    * Gets and starts the appropriate flow for the launch mode.
+   * @param {chrome.hotwordPrivate.LaunchState} state Launch state of the
+   *     Hotword Audio Verification App.
    * @private
    */
   Flow.prototype.startFlowForMode_ = function(state) {
+    this.launchMode_ = state.launchMode;
     assert(state.launchMode >= 0 && state.launchMode < FLOWS.length,
            'Invalid Launch Mode.');
     this.currentFlow_ = FLOWS[state.launchMode];
     this.advanceStep();
+    // If the flow begins with a a training step, then start the training flow.
+    if (state.launchMode == LaunchMode.HOTWORD_ONLY ||
+        state.launchMode == LaunchMode.SPEECH_TRAINING) {
+      this.startTraining();
+    }
   };
 
   /**
--- chrome/browser/resources/hotword_audio_verification/main.js
+++ chrome/browser/resources/hotword_audio_verification/main.js
@@ -11,17 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
   var closeAppWindow = function(e) {
     var classes = e.target.classList;
     if (classes.contains('close') || classes.contains('finish-button')) {
+      flow.stopTraining();
       appWindow.close();
       e.preventDefault();
     }
   };
 
-  // TODO(kcarattini): Cancel training in NaCl module for hotword-only and
-  // speech-training close and cancel buttons.
   $('steps').addEventListener('click', closeAppWindow);
 
   $('hw-agree-button').addEventListener('click', function(e) {
     flow.advanceStep();
+    flow.startTraining();
     e.preventDefault();
   });
 
@@ -29,65 +29,4 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.browser.openTab({'url': 'chrome://settings'}, function() {});
     e.preventDefault();
   });
-
-  /**
-   * Updates steps of the training UI.
-   * @param {string} pagePrefix Prefix of the element ids for this page.
-   * @param {Event} e The click event.
-   */
-  function doTraining(pagePrefix, e) {
-    // TODO(kcarattini): Update this to respond to a hotword trigger once
-    // speech training is implemented.
-    var steps = $(pagePrefix + '-training').querySelectorAll('.train');
-    var index = Array.prototype.indexOf.call(steps, e.currentTarget);
-    if (!steps[index])
-      return;
-
-    // TODO(kcarattini): Localize this string.
-    steps[index].querySelector('.text').textContent = 'Recorded';
-    steps[index].classList.remove('listening');
-    steps[index].classList.add('recorded');
-
-    if (steps[index + 1]) {
-      steps[index + 1].classList.remove('not-started');
-      steps[index + 1].classList.add('listening');
-    }
-  }
-
-  /**
-   * Adds event listeners to the training UI.
-   * @param {string} pagePrefix Prefix of the element ids for this page.
-   */
-  function addTrainingEventListeners(pagePrefix) {
-    var steps = $(pagePrefix + '-training').querySelectorAll('.train');
-    for (var i = 0; i < steps.length; ++i) {
-      steps[i].addEventListener('click', function(e) {
-        doTraining(pagePrefix, e);
-        e.preventDefault();
-
-        if (e.currentTarget != steps[steps.length - 1])
-          return;
-
-        // Update the 'Cancel' button.
-        var buttonElem = $(pagePrefix + '-cancel-button');
-        // TODO(kcarattini): Localize this string.
-        buttonElem.textContent = 'Please wait ...';
-        buttonElem.classList.add('grayed-out');
-        buttonElem.classList.remove('finish-button');
-
-        setTimeout(function() {
-          if (chrome.hotwordPrivate.setAudioLoggingEnabled)
-            chrome.hotwordPrivate.setAudioLoggingEnabled(true, function() {});
-
-          if (chrome.hotwordPrivate.setHotwordAlwaysOnSearchEnabled) {
-            chrome.hotwordPrivate.setHotwordAlwaysOnSearchEnabled(true,
-                flow.advanceStep.bind(flow));
-          }
-        }, 2000);
-      });
-    }
-  }
-
-  addTrainingEventListeners('speech-training');
-  addTrainingEventListeners('hotword-only');
 });
