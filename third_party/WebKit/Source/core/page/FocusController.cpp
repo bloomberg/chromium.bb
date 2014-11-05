56a3bd946969e6ecc25bbf52c795a9689c6ac345
tkent@chromium.org <tkent@chromium.org>
2014-11-05 16:43:28 +0000

    Fix a bug of sequential focus navigation with <select multiple>, and so on.
    <select multiple>, <select size=2-or-greater>, and <input
    type=date/datetime-local/month/time/week> had a bug that they prevented
    sequential focus navigation even if they were not focusabble.
    
    Change nextNodeWithGreaterTabIndex() so that it uses adjustedTabIndex().
    
    BUG=425937
    TEST=automated
    
    Review URL: https://codereview.chromium.org/681103003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184877 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 5c3391fbfb7e3f3fb5c266fabd587b7ae114ea78

--- third_party/WebKit/Source/core/page/FocusController.cpp
+++ third_party/WebKit/Source/core/page/FocusController.cpp
@@ -546,7 +546,8 @@ static Node* nextNodeWithGreaterTabIndex(Node* start, int tabIndex)
     int winningTabIndex = std::numeric_limits<short>::max() + 1;
     Node* winner = nullptr;
     for (Node& node : NodeTraversal::startsAt(start)) {
-        if (shouldVisit(&node) && node.tabIndex() > tabIndex && node.tabIndex() < winningTabIndex) {
+        int currentTabIndex = adjustedTabIndex(&node);
+        if (shouldVisit(&node) && currentTabIndex > tabIndex && currentTabIndex < winningTabIndex) {
             winner = &node;
             winningTabIndex = node.tabIndex();
         }
