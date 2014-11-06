2df0edc408b9ef221fa1587c527e4170a6057a24
amistry <amistry@chromium.org>
2014-11-05 20:53:13 -0800

    Allow new hotwording to be enabled via a field trial.
    BUG=397019
    
    Review URL: https://codereview.chromium.org/704023002
    
    Cr-Commit-Position: refs/heads/master@{#302963}
    
    Former-commit-id: 510ed8e880ae17f79a84c2ed3bc1a87300a83119

--- chrome/browser/search/hotword_service.cc
+++ chrome/browser/search/hotword_service.cc
@@ -161,6 +161,7 @@ namespace hotword_internal {
 // Constants for the hotword field trial.
 const char kHotwordFieldTrialName[] = "VoiceTrigger";
 const char kHotwordFieldTrialDisabledGroupName[] = "Disabled";
+const char kHotwordFieldTrialExperimentalGroupName[] = "Experimental";
 // Old preference constant.
 const char kHotwordUnusablePrefName[] = "hotword.search_enabled";
 // String passed to indicate the training state has changed.
@@ -182,6 +183,13 @@ bool HotwordService::DoesHotwordSupportLanguage(Profile* profile) {
 
 // static
 bool HotwordService::IsExperimentalHotwordingEnabled() {
+  std::string group = base::FieldTrialList::FindFullName(
+      hotword_internal::kHotwordFieldTrialName);
+  if (!group.empty() &&
+      group == hotword_internal::kHotwordFieldTrialExperimentalGroupName) {
+    return true;
+  }
+
   CommandLine* command_line = CommandLine::ForCurrentProcess();
   return command_line->HasSwitch(switches::kEnableExperimentalHotwording);
 }
