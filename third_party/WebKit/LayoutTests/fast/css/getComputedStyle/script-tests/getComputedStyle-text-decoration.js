789794fcd1112777094422392f536397819132f3
timloh@chromium.org <timloh@chromium.org>
2014-11-06 00:34:26 +0000

    Remove some testing of getPropertyCSSValue in fast/css/getComputedStyle
    As getPropertyCSSValue is no longer web-exposed we should stop testing
    it so we can delete the interface.
    
    BUG=428595
    
    Review URL: https://codereview.chromium.org/703793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@184888 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 15fc1f42626b0c63f1943e045bb771dc1c2c35f8

--- third_party/WebKit/LayoutTests/fast/css/getComputedStyle/script-tests/getComputedStyle-text-decoration.js
+++ third_party/WebKit/LayoutTests/fast/css/getComputedStyle/script-tests/getComputedStyle-text-decoration.js
@@ -1,19 +1,7 @@
-function testElementStyle(propertyJS, propertyCSS, type, value)
-{
-    if (type != null) {
-        shouldBe("e.style." + propertyJS, "'" + value + "'");
-        shouldBe("e.style.getPropertyCSSValue('" + propertyCSS + "').toString()", "'" + type + "'");
-        shouldBe("e.style.getPropertyCSSValue('" + propertyCSS + "').cssText", "'" + value + "'");
-    } else
-        shouldBeNull("e.style.getPropertyCSSValue('" + propertyCSS + "')");
-}
-
-function testComputedStyle(propertyJS, propertyCSS, type, value)
+function testComputedStyle(propertyJS, value)
 {
     computedStyle = window.getComputedStyle(e, null);
     shouldBe("computedStyle." + propertyJS, "'" + value + "'");
-    shouldBe("computedStyle.getPropertyCSSValue('" + propertyCSS + "').toString()", "'" + type + "'");
-    shouldBe("computedStyle.getPropertyCSSValue('" + propertyCSS + "').cssText", "'" + value + "'");
 }
 
 description("Test to make sure text-decoration property returns values properly.")
@@ -25,75 +13,63 @@ document.body.appendChild(testContainer);
 testContainer.innerHTML = '<div id="test">hello world</div>';
 debug("Initial value:");
 e = document.getElementById('test');
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "none solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "none solid rgb(0, 0, 0)");
 debug('');
 
 debug("Initial value (explicit):");
 e.style.textDecoration = 'initial';
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "none solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "none solid rgb(0, 0, 0)");
 debug('');
 
 debug("Value 'none':");
 e.style.textDecoration = 'none';
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "none solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "none solid rgb(0, 0, 0)");
 debug('');
 
 debug("Value 'underline':");
 e.style.textDecoration = 'underline';
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "underline solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "underline solid rgb(0, 0, 0)");
 debug('');
 
 debug("Value 'overline':");
 e.style.textDecoration = 'overline';
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "overline solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "overline solid rgb(0, 0, 0)");
 debug('');
 
 debug("Value 'line-through':");
 e.style.textDecoration = 'line-through';
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "line-through solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "line-through solid rgb(0, 0, 0)");
 debug('');
 
 debug("Value 'underline overline line-through':");
 e.style.textDecoration = 'underline overline line-through';
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "underline overline line-through solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "underline overline line-through solid rgb(0, 0, 0)");
 debug('');
 
 debug("Value 'blink' (valid but ignored):");
 e.style.textDecoration = 'blink';
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "none solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "none solid rgb(0, 0, 0)");
 debug('');
 
 debug("Value '':");
 e.style.textDecoration = '';
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "none solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "none solid rgb(0, 0, 0)");
 debug('');
 
 testContainer.innerHTML = '<div id="test-parent" style="text-decoration: underline;">hello <span id="test-ancestor" style="text-decoration: inherit;">world</span></div>';
 debug("Parent gets 'underline' value:");
 e = document.getElementById('test-parent');
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "underline solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "underline solid rgb(0, 0, 0)");
 debug('');
 
 debug("Ancestor should explicitly inherit value from parent when 'inherit' value is used:");
 e = document.getElementById('test-ancestor');
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSValueList]", "underline solid rgb(0, 0, 0)");
+testComputedStyle("textDecoration", "underline solid rgb(0, 0, 0)");
 debug('');
 
 debug("Ancestor should not implicitly inherit value from parent (i.e. when value is void):");
 e.style.textDecoration = '';
-testElementStyle("textDecoration", "text-decoration", null, '');
-testComputedStyle("textDecoration", "text-decoration", "[object CSSPrimitiveValue]", "none");
+testComputedStyle("textDecoration", "none");
 debug('');
 
 document.body.removeChild(testContainer);
