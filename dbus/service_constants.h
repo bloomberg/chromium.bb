a3027345b2783cdcb345834b507703aaf5e28f69
Peter Qiu <zqiu@chromium.org>
2014-11-10 14:14:50 -0800

    apmanager: Add DBus constants for AP Configuration
    Add constants used for configuring AP service.
    
    BUG=chromium:430536
    TEST=unittests
    
    Change-Id: Iba344e0031afd20547fdeb81cff97dace59d24b3
    Reviewed-on: https://chromium-review.googlesource.com/228886
    Reviewed-by: Peter Qiu <zqiu@chromium.org>
    Commit-Queue: Peter Qiu <zqiu@chromium.org>
    Tested-by: Peter Qiu <zqiu@chromium.org>

--- dbus/service_constants.h
+++ dbus/service_constants.h
@@ -1898,12 +1898,41 @@ const char kManagerInterface[] = "org.chromium.apmanager.Manager";
 const char kManagerPath[] = "/manager";
 const char kServiceInterface[] = "org.chromium.apmanager.Service";
 const char kConfigInterface[] = "org.chromium.apmanager.Config";
+const char kConfigError[] = "org.chromium.apmanager.Config.Error";
 const char kClientInterface[] = "org.chromium.apmanager.Client";
 const char kDeviceInterface[] = "org.chromium.apmanager.Device";
 
 // Manager Methods.
 const char kCreateServiceMethod[] = "CreateService";
 const char kRemoveServiceMethod[] = "RemoveService";
+
+// Config Properties.
+const char kConfigSSIDProperty[] = "Ssid";
+const char kConfigInterfaceNameProperty[] = "InterfaceName";
+const char kConfigSecurityModeProperty[] = "SecurityMode";
+const char kConfigPassphraseProperty[] = "Passphrase";
+const char kConfigHwModeProperty[] = "HwMode";
+const char kConfigOperationModeProperty[] = "OperationMode";
+const char kConfigChannelProperty[] = "Channel";
+const char kConfigHiddenNetworkProperty[] = "HiddenNetwork";
+const char kConfigBridgeInterfaceProperty[] = "BridgeInterface";
+const char kConfigServerAddressIndexProperty[] = "ServerAddressIndex";
+
+// Security modes.
+const char kSecurityModeNone[] = "none";
+const char kSecurityModeRSN[] = "rsn";
+
+// Hardware modes.
+const char kHwMode80211a[] = "802.11a";
+const char kHwMode80211b[] = "802.11b";
+const char kHwMode80211g[] = "802.11g";
+const char kHwMode80211n[] = "802.11n";
+const char kHwMode80211ac[] = "802.11ac";
+
+// Operation modes.
+const char kOperationModeServer[] = "server";
+const char kOperationModeBridge[] = "bridge";
+
 }  // namespace apmanager
 
 #endif  // DBUS_SERVICE_CONSTANTS_H_
