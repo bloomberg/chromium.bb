6ddffc1a30de9fc4e77926b27f6efdf61a0eb233
mvanouwerkerk <mvanouwerkerk@chromium.org>
2014-11-11 11:21:25 -0800

    Add a PushClient getter in WebFrameTestProxy.
    BUG=304341,350378
    
    Review URL: https://codereview.chromium.org/720443002
    
    Cr-Commit-Position: refs/heads/master@{#303685}
    
    Former-commit-id: f25dd9c51401c8d4f91ab497eebdda41aa272df7

--- content/shell/renderer/test_runner/web_frame_test_proxy.h
+++ content/shell/renderer/test_runner/web_frame_test_proxy.h
@@ -269,6 +269,10 @@ class WebFrameTestProxy : public Base {
     return base_proxy_->GetWebMIDIClient();
   }
 
+  virtual blink::WebPushClient* pushClient() {
+    return base_proxy_->GetWebPushClient();
+  }
+
   virtual bool willCheckAndDispatchMessageEvent(
       blink::WebLocalFrame* source_frame,
       blink::WebFrame* target_frame,
