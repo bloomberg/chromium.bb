1b9d9674ecb3fd4faeb63dc106bb1ab09561588e
kalman <kalman@chromium.org>
2014-11-11 13:13:54 -0800

    Docserver: Return a Future rather than None when the MemcacheObjectStore fails to write an item to memcache. This may be the cause for some recent failures (see bug), or may not, but either way it's a bug.
    BUG=432152
    R=rockot@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/714173003
    
    Cr-Commit-Position: refs/heads/master@{#303721}
    
    Former-commit-id: ef848de091cb90bd752af7042769b6135f92f471

--- chrome/common/extensions/docs/server2/memcache_object_store.py
+++ chrome/common/extensions/docs/server2/memcache_object_store.py
@@ -3,38 +3,42 @@
 # found in the LICENSE file.
 
 import logging
+import traceback
 
 from appengine_wrappers import memcache
 from future import Future
 from object_store import ObjectStore
 
-class _AsyncMemcacheIOFuture(object):
-  def __init__(self, rpc):
-    self._rpc = rpc
-
-  def Get(self):
-    return self._rpc.get_result()
 
 class MemcacheObjectStore(ObjectStore):
   def __init__(self, namespace):
     self._namespace = namespace
 
   def SetMulti(self, mapping):
-    # talking_alarm_clock always fails because the zip is too big.
-    # TODO(kalman): store example zips in blobstore.
-    if any(key.find('talking_alarm_clock') != -1 for key in mapping.iterkeys()):
-      return Future(value=None)
+    # Some files are too big to fit in memcache, and will throw a ValueError if
+    # we try. That's caught below, but to avoid log spew as much as possible
+    # (and to try to store as many things as possible, since this is Set*Multi*
+    # after all, and there may be other things to store from |mapping|), delete
+    # the paths which we know don't work.
+    #
+    # TODO(kalman): Store big things (like example zips) in blobstore so that
+    # this doesn't happen.
+    log_spewers = ('assets/remote-debugging/remote-debug-banner.ai',)
+    for spewer in log_spewers:
+      mapping.pop(spewer, None)
+
     try:
       rpc = memcache.Client().set_multi_async(mapping,
-          namespace=self._namespace)
-      return _AsyncMemcacheIOFuture(rpc)
+                                              namespace=self._namespace)
+      return Future(callback=rpc.get_result)
     except ValueError as e:
-      logging.error('Caught "ValueError: %s" when mapping keys %s' % (
-          e, mapping.keys()))
+      logging.error('Caught error when memcache-ing keys %s: %s' %
+                    (mapping.keys(), traceback.format_exc()))
+      return Future(value=None)
 
   def GetMulti(self, keys):
     rpc = memcache.Client().get_multi_async(keys, namespace=self._namespace)
-    return _AsyncMemcacheIOFuture(rpc)
+    return Future(callback=rpc.get_result)
 
   def DelMulti(self, keys):
     memcache.delete_multi(keys, namespace=self._namespace)
