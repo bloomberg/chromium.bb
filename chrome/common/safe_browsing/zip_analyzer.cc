b2fd64904009f29cb3c7610aaa90a2ba5f68c80e
anujk.sharma <anujk.sharma@samsung.com>
2014-11-11 19:29:39 -0800

    [chrome/common/safe_browsing] Convert VLOGs to DVLOGs
    BUG=101424
    
    Review URL: https://codereview.chromium.org/694323004
    
    Cr-Commit-Position: refs/heads/master@{#303785}
    
    Former-commit-id: e1e602b008aab47682675d4d2beaed90a06f2d1c

--- chrome/common/safe_browsing/zip_analyzer.cc
+++ chrome/common/safe_browsing/zip_analyzer.cc
@@ -17,18 +17,18 @@ void AnalyzeZipFile(base::File zip_file, Results* results) {
   // no way to know if it did that or not. Assume it did (that's the common
   // case).
   if (!reader.OpenFromPlatformFile(zip_file.TakePlatformFile())) {
-    VLOG(1) << "Failed to open zip file";
+    DVLOG(1) << "Failed to open zip file";
     return;
   }
 
   bool advanced = true;
   for (; reader.HasMore(); advanced = reader.AdvanceToNextEntry()) {
     if (!advanced) {
-      VLOG(1) << "Could not advance to next entry, aborting zip scan.";
+      DVLOG(1) << "Could not advance to next entry, aborting zip scan.";
       return;
     }
     if (!reader.OpenCurrentEntryInZip()) {
-      VLOG(1) << "Failed to open current entry in zip file";
+      DVLOG(1) << "Failed to open current entry in zip file";
       continue;
     }
     const base::FilePath& file = reader.current_entry_info()->file_path();
@@ -38,12 +38,12 @@ void AnalyzeZipFile(base::File zip_file, Results* results) {
       if (download_protection_util::IsArchiveFile(file)) {
         results->has_archive = true;
       } else {
-        VLOG(2) << "Downloaded a zipped executable: " << file.value();
+        DVLOG(2) << "Downloaded a zipped executable: " << file.value();
         results->has_executable = true;
         break;
       }
     } else {
-      VLOG(3) << "Ignoring non-binary file: " << file.value();
+      DVLOG(3) << "Ignoring non-binary file: " << file.value();
     }
   }
   results->success = true;
