f7a818976cb8f2368461f386ea480e87e09adefb
oshima <oshima@chromium.org>
2014-11-13 10:00:14 -0800

    Remove DCHECK that is causing problem in login prompt
    Let me remove this as this didn't exist before. I'll investigate if this is a bug on caller side and add this back if necessary.
    
    BUG=432778
    TBR=msw@chromium.org
    
    Review URL: https://codereview.chromium.org/724853002
    
    Cr-Commit-Position: refs/heads/master@{#304044}
    
    Former-commit-id: a34e8b721e0a44868b734380024dbd46f7f53bd2

--- components/constrained_window/constrained_window_views.cc
+++ components/constrained_window/constrained_window_views.cc
@@ -155,7 +155,6 @@ views::Widget* CreateWebModalDialogViews(views::WidgetDelegate* dialog,
 // TODO(gbillock): Replace this with PopupManager calls.
 views::Widget* CreateBrowserModalDialogViews(views::DialogDelegate* dialog,
                                              gfx::NativeView parent) {
-  DCHECK_NE(ui::MODAL_TYPE_CHILD, dialog->GetModalType());
   DCHECK_NE(ui::MODAL_TYPE_NONE, dialog->GetModalType());
 
   views::Widget* widget =
