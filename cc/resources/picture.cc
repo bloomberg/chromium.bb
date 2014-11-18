c1b3ae78e84f1c9675b174147fe8c9b2509a289d
mtklein <mtklein@chromium.org>
2014-11-18 09:52:36 -0800

    Try switching cc::Picture to SkRTree.
    Skia's been testing this on pictures from Chrome with promising results:
      - Never seen a correctness problem.
      - Building an SkRTree is always faster than an SkTileGrid,
        sometimes much faster.
      - While SkTileGrid serves exact tile queries a little faster,
        SkRTree is faster than SkTileGrid for non-exact queries.
      - We notice Chrome regularly makes non-exact queries hitting 2-4 tiles.
    
    So we think this is probably at least a record-speed win, and there's a
    reasonable chance it improves playback a little too.
    
    Is there any better way to answer these performance questions other than
    landing and seeing what chromeperf turns up?
    
    BUG=skia:3085
    
    Review URL: https://codereview.chromium.org/727413002
    
    Cr-Commit-Position: refs/heads/master@{#304634}
    
    Former-commit-id: 8c91f2c1aa4bb1ac639dd617620016b891315edf

--- cc/resources/picture.cc
+++ cc/resources/picture.cc
@@ -210,7 +210,8 @@ void Picture::Record(ContentLayerClient* painter,
   DCHECK(!picture_);
   DCHECK(!tile_grid_info.fTileInterval.isEmpty());
 
-  SkTileGridFactory factory(tile_grid_info);
+  // TODO(mtklein): If SkRTree sticks, clean up tile_grid_info.  skbug.com/3085
+  SkRTreeFactory factory;
   SkPictureRecorder recorder;
 
   skia::RefPtr<SkCanvas> canvas;
