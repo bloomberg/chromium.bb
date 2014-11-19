f040b1d6a24ae4065b7ebb396757fbf80890723c
ljagielski <ljagielski@opera.com>
2014-11-19 01:05:13 -0800

    SearchEngineTabHelper: added delegate_ member initializer to constructor.
    BUG=none
    
    Review URL: https://codereview.chromium.org/735683002
    
    Cr-Commit-Position: refs/heads/master@{#304789}
    
    Former-commit-id: a45e34acb629d58dcc3e8f9a61b2974934d4fb31

--- chrome/browser/ui/search_engines/search_engine_tab_helper.cc
+++ chrome/browser/ui/search_engines/search_engine_tab_helper.cc
@@ -96,6 +96,7 @@ bool SearchEngineTabHelper::OnMessageReceived(const IPC::Message& message) {
 
 SearchEngineTabHelper::SearchEngineTabHelper(WebContents* web_contents)
     : content::WebContentsObserver(web_contents),
+      delegate_(nullptr),
       weak_ptr_factory_(this) {
   DCHECK(web_contents);
 }
