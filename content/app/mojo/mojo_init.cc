3c45c675b30f4bb5e646518faa1a99e6f7762b02
morrita <morrita@chromium.org>
2014-11-19 16:14:55 -0800

    Mojo: Raise the message size limit.
    The default message size limit 4MB is too small for Chrome IPC.
    
    TEST=Layout Tests with a flag on
    BUG=377980
    R=viettrungluu@chromium.org, jam@chromium.org, nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/741813002
    
    Cr-Commit-Position: refs/heads/master@{#304938}
    
    Former-commit-id: 872df152e57fe8ba6ebf668531147e482ca4f73f

--- content/app/mojo/mojo_init.cc
+++ content/app/mojo/mojo_init.cc
@@ -5,12 +5,18 @@
 #include "content/app/mojo/mojo_init.h"
 
 #include "base/memory/scoped_ptr.h"
+#include "mojo/edk/embedder/configuration.h"
 #include "mojo/edk/embedder/embedder.h"
 #include "mojo/edk/embedder/simple_platform_support.h"
 
 namespace content {
 
 void InitializeMojo() {
+  // Things like content_shell and DevTools ocassionally send big
+  // message which includes whole rendered screen or all loaded
+  // scripts. The buffer size has to be big enough to allow such use
+  // cases.
+  mojo::embedder::GetConfiguration()->max_message_num_bytes = 64*1024*1024;
   mojo::embedder::Init(scoped_ptr<mojo::embedder::PlatformSupport>(
       new mojo::embedder::SimplePlatformSupport()));
 }
