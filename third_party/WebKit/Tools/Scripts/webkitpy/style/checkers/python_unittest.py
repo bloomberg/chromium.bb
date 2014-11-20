fe903c9f8dfc332b6773548cff71709c4157b224
vapier@chromium.org <vapier@chromium.org>
2014-11-20 22:36:51 +0000

    Revert of Revert of update test-webkitpy for updated pylint syntax (patchset #1 id:1 of https://codereview.chromium.org/747603002/)
    Reason for revert:
    Support for pylint-1.3 is being re-added, so we'll need this fix again.
    
    Original issue's description:
    > Revert of update test-webkitpy for updated pylint syntax (patchset #1 id:1 of https://codereview.chromium.org/719943007/)
    > 
    > Reason for revert:
    > python_unittest.py is failing at head: https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%2032/builds/30687/steps/webkit_python_tests/logs/stdio
    > 
    > Original issue's description:
    > > update test-webkitpy for updated pylint syntax.
    > > 
    > > R=dpranke@chromium.org
    > > BUG=431514
    > > 
    > > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=185550
    > 
    > TBR=dpranke@chromium.org,vapier@chromium.org
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=431514
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=185708
    
    TBR=dpranke@chromium.org,pdr@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=431514
    
    Review URL: https://codereview.chromium.org/744023003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185711 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: be8a263dd42fd75688f5dcbd3e44489ebcc5f7f4

--- third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/python_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/python_unittest.py
@@ -59,5 +59,6 @@ class PythonCheckerTest(unittest.TestCase):
 
         self.assertEqual(errors, [
             (4, "pep8/W291", 5, "trailing whitespace"),
-            (4, "pylint/E0602", 5, "Undefined variable 'error'"),
+            (4, "pylint/E0602(undefined-variable)", 5,
+             "[] Undefined variable 'error'"),
             ])
