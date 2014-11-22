2f7706e9a90d37d8ccfe1db88eb158d8f0d8eed6
robhogan@gmail.com <robhogan@gmail.com>
2014-11-22 20:42:16 +0000

    When no clearance applies to an element with 'clear' set, place the element below the float just as we would if it was clear:none
    If an element has 'clear' set but we don't actually calculate clearance
    for it then it should be placed as though it had 'clear:none'. This is
    especially pertinent when the element has margin-top that puts it clear of any float.
    
    BUG=434048
    
    Review URL: https://codereview.chromium.org/732583004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@185842 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: cad053eed876f3ba8fbc07b03c1f8a3f65be44e7

--- third_party/WebKit/Source/core/rendering/RenderBlockFlow.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBlockFlow.cpp
@@ -1466,8 +1466,7 @@ void RenderBlockFlow::marginBeforeEstimateForChild(RenderBox* child, LayoutUnit&
             break;
     }
 
-    // Give up if there is clearance on the box, since it probably won't collapse into us.
-    if (!grandchildBox || grandchildBox->style()->clear() != CNONE)
+    if (!grandchildBox)
         return;
 
     // Make sure to update the block margins now for the grandchild box so that we're looking at current values.
@@ -1479,6 +1478,12 @@ void RenderBlockFlow::marginBeforeEstimateForChild(RenderBox* child, LayoutUnit&
             grandchildBlock->setHasMarginAfterQuirk(grandchildBox->style()->hasMarginAfterQuirk());
         }
     }
+
+    // If we have a 'clear' value but also have a margin we may not actually require clearance to move past any floats.
+    // If that's the case we want to be sure we estimate the correct position including margins after any floats rather
+    // than use 'clearance' later which could give us the wrong position.
+    if (grandchildBox->style()->clear() != CNONE && childBlockFlow->marginBeforeForChild(grandchildBox) == 0)
+        return;
 
     // Collapse the margin of the grandchild box with our own to produce an estimate.
     childBlockFlow->marginBeforeEstimateForChild(grandchildBox, positiveMarginBefore, negativeMarginBefore, discardMarginBefore);
