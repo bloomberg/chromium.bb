cbab502839661af31099ce733f210c4a993f8f35
grt <grt@chromium.org>
2014-11-23 21:15:17 -0800

    Simplified regression test for crbug.com/433928.
    BUG=433928
    R=robertshield@chromium.org
    
    Review URL: https://codereview.chromium.org/741953002
    
    Cr-Commit-Position: refs/heads/master@{#305401}
    
    Former-commit-id: 88144e0e889c0f545741136f6da31b336a1fbb7f

--- chrome/browser/safe_browsing/incident_reporting/download_metadata_manager_unittest.cc
+++ chrome/browser/safe_browsing/incident_reporting/download_metadata_manager_unittest.cc
@@ -462,6 +462,9 @@ TEST_P(SetRequestTest, SetRequest) {
   }
   manager_.GetDownloadDetails(&profile_, details_getter.GetCallback());
 
+  // In http://crbug.com/433928, open after SetRequest(nullpr) caused a crash.
+  test_item_->NotifyObserversDownloadOpened();
+
   ShutdownDownloadManager();
 
   scoped_ptr<DownloadMetadata> metadata(ReadTestMetadataFile());
@@ -484,29 +487,5 @@ INSTANTIATE_TEST_CASE_P(
                      testing::Values("none", "pending"),
                      testing::Values("waiting", "loaded"),
                      testing::Values("clear", "set")));
-
-class DownloadMetadataManagerTest : public DownloadMetadataManagerTestBase {
- protected:
-};
-
-// Test that that opening an item when there is no corresponding metadata works.
-TEST_F(DownloadMetadataManagerTest, OpenDownloadNoMetadata) {
-  // Add a download manager and some items.
-  WriteTestMetadataFile();
-  AddDownloadManager();
-  AddDownloadItems();
-
-  // Allow the metadata manager to discover that no metadata is available.
-  RunAllTasks();
-
-  // Clear metadata.
-  manager_.SetRequest(test_item_.get(), nullptr);
-
-  // Open an item.
-  test_item_->NotifyObserversDownloadOpened();
-
-  // Shut down.
-  ShutdownDownloadManager();
-}
 
 }  // namespace safe_browsing
