b575cc9e440ad23efa5106602fbadbf9b73b9bb0
peter <peter@chromium.org>
2014-11-24 13:26:31 -0800

    Add a browser test to verify that double-closing a notification does not crash.
    BUG=435201
    
    Review URL: https://codereview.chromium.org/752533002
    
    Cr-Commit-Position: refs/heads/master@{#305495}
    
    Former-commit-id: c50e90e50d4d105f0d2260385a2090d9108c9fa3

--- chrome/browser/notifications/notification_browsertest.cc
+++ chrome/browser/notifications/notification_browsertest.cc
@@ -150,9 +150,13 @@ class NotificationsTest : public InProcessBrowserTest {
   bool CheckOriginInSetting(const ContentSettingsForOneType& settings,
                             const GURL& origin);
 
-  GURL GetTestPageURL() const {
+  GURL GetTestPageURLForFile(const std::string& file) const {
     return embedded_test_server()->GetURL(
-      "/notifications/notification_tester.html");
+      std::string("/notifications/") + file);
+  }
+
+  GURL GetTestPageURL() const {
+    return GetTestPageURLForFile("notification_tester.html");
   }
 
  private:
@@ -888,4 +892,32 @@ IN_PROC_BROWSER_TEST_F(NotificationsTest, TestNotificationInvalidIcon) {
 
   notification = *notifications.rbegin();
   EXPECT_TRUE(notification->icon().IsEmpty());
+}
+
+IN_PROC_BROWSER_TEST_F(NotificationsTest, TestNotificationDoubleClose) {
+  ASSERT_TRUE(embedded_test_server()->InitializeAndWaitUntilReady());
+  AllowAllOrigins();
+
+  ui_test_utils::NavigateToURL(
+      browser(), GetTestPageURLForFile("notification-double-close.html"));
+  ASSERT_EQ(0, GetNotificationPopupCount());
+
+  std::string result = CreateNotification(
+      browser(), true, "", "Title1", "Body1", "chat");
+  EXPECT_NE("-1", result);
+
+  ASSERT_EQ(1, GetNotificationCount());
+  message_center::NotificationList::Notifications notifications =
+      message_center::MessageCenter::Get()->GetVisibleNotifications();
+  message_center::MessageCenter::Get()->RemoveNotification(
+      (*notifications.rbegin())->id(),
+      true);  // by_user
+
+  ASSERT_EQ(0, GetNotificationCount());
+
+  // Calling WebContents::IsCrashed() will return FALSE here, even if the WC did
+  // crash. Work around this timing issue by creating another notification,
+  // which requires interaction with the renderer process.
+  result = CreateNotification(browser(), true, "", "Title1", "Body1", "chat");
+  EXPECT_NE("-1", result);
 }
