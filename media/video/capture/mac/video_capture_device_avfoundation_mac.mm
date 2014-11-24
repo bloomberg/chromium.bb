538f02a28a51b8992cad403372f397833b044908
tommi <tommi@chromium.org>
2014-11-24 07:33:17 -0800

    Fix a bug where we would fail to unlock the frame buffer in avfoundation video capture. I'm also setting the lock flags to read-only since it doesn't look like we will ever write to that buffer (at least there's no explanation for why we need write access).  I don't know for sure if this will improve performance or not.
    Review URL: https://codereview.chromium.org/727733003
    
    Cr-Commit-Position: refs/heads/master@{#305422}
    
    Former-commit-id: 5d0966b4c76f67d4725f26d0862f706e940877dc

--- media/video/capture/mac/video_capture_device_avfoundation_mac.mm
+++ media/video/capture/mac/video_capture_device_avfoundation_mac.mm
@@ -256,7 +256,7 @@
   CVImageBufferRef videoFrame =
       CoreMediaGlue::CMSampleBufferGetImageBuffer(sampleBuffer);
   // Lock the frame and calculate frame size.
-  const int kLockFlags = 0;
+  const int kLockFlags = kCVPixelBufferLock_ReadOnly;
   if (CVPixelBufferLockBaseAddress(videoFrame, kLockFlags) ==
           kCVReturnSuccess) {
     void* baseAddress = CVPixelBufferGetBaseAddress(videoFrame);
@@ -270,10 +270,14 @@
         gfx::Size(frameWidth, frameHeight),
         frameRate_,
         media::PIXEL_FORMAT_UYVY);
-    base::AutoLock lock(lock_);
-    if (!frameReceiver_)
-      return;
-    frameReceiver_->ReceiveFrame(addressToPass, frameSize, captureFormat, 0, 0);
+    {
+      base::AutoLock lock(lock_);
+      if (frameReceiver_) {
+        frameReceiver_->ReceiveFrame(
+            addressToPass, frameSize, captureFormat, 0, 0);
+      }
+    }
+
     CVPixelBufferUnlockBaseAddress(videoFrame, kLockFlags);
   }
 }
