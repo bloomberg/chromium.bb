7cf5a5e031aef376d3c0a79168de8f5fbc65aba4
samuong <samuong@chromium.org>
2014-11-24 11:15:06 -0800

    [chromedriver] Change minimum supported version to 37.0.2062.0
    BUG=
    TBR=stgao@chromium.org
    
    Review URL: https://codereview.chromium.org/755683002
    
    Cr-Commit-Position: refs/heads/master@{#305467}
    
    Former-commit-id: a332e2ab0f955188266a624a887eacc04ec79a3f

--- chrome/test/chromedriver/chrome/version.cc
+++ chrome/test/chromedriver/chrome/version.cc
@@ -9,7 +9,7 @@
 namespace {
 
 // This variable must be able to be found and parsed by the upload script.
-const int kMinimumSupportedChromeVersion[] = {37, 0, 2063, 0};
+const int kMinimumSupportedChromeVersion[] = {37, 0, 2062, 0};
 
 }  // namespace
 
