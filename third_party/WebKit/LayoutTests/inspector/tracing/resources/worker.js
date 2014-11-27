b2494c35dfa58d3b37568b426efb7b52ec8c114d
alph@chromium.org <alph@chromium.org>
2014-11-27 16:18:07 +0000

    Unflake worker-js-profile.html
    BUG=434090,434690
    
    Review URL: https://codereview.chromium.org/765683003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186128 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: cc104a99ad5517230a535fb205f0cdd65f42b106

--- third_party/WebKit/LayoutTests/inspector/tracing/resources/worker.js
+++ third_party/WebKit/LayoutTests/inspector/tracing/resources/worker.js
@@ -2,4 +2,7 @@ var message_id = 0;
 onmessage = function(event)
 {
     postMessage("Ack #" + message_id++);
+    // Two setTimeout's ensure the JSFrame for onmessage is created between two instant event.
+    setTimeout(function() {}, 0);
+    setTimeout(function() {}, 0);
 };
