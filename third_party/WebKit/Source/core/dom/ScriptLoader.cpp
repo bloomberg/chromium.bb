2e698ec5dae3fe5b4da16af5232bb759a869ac4f
pfeldman@chromium.org <pfeldman@chromium.org>
2014-11-28 14:18:31 +0000

    Revert of Stop sniffing 'image/*' into script. (patchset #4 id:60001 of https://codereview.chromium.org/755993003/)
    Reason for revert:
    Not sure if this was intended or broke the web, reverting.
    
    https://storage.googleapis.com/chromium-layout-test-archives/WebKit_Android__Nexus4_/results/layout-test-results/loader/reload-subresource-when-type-changes-diff.txt
    
    Original issue's description:
    > Stop sniffing 'image/*' into script.
    > 
    > If a resource is served with an image MIME type, we should not sniff it
    > into script when loaded via <script>. See [1] and [2] for examples of
    > the badness this can cause.
    > 
    > [1]: http://iamajin.blogspot.in/2014/11/when-gifs-serve-javascript.html
    > [2]: http://jklmnn.de/imagejs/
    > 
    > I hope this change is web-compatible, but adding a UseCounter just to
    > make sure we can keep an eye on things as it rolls out through Beta
    > and Stable.
    > 
    > BUG=433049
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=186168
    
    TBR=jochen@chromium.org,tsepez@chromium.org,mkwst@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=433049
    
    Review URL: https://codereview.chromium.org/766943002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186180 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 8b6a168e7fd985b72d694edd41280d0b092cd766

--- third_party/WebKit/Source/core/dom/ScriptLoader.cpp
+++ third_party/WebKit/Source/core/dom/ScriptLoader.cpp
@@ -38,7 +38,6 @@
 #include "core/fetch/FetchRequest.h"
 #include "core/fetch/ResourceFetcher.h"
 #include "core/fetch/ScriptResource.h"
-#include "core/frame/UseCounter.h"
 #include "core/html/HTMLScriptElement.h"
 #include "core/html/imports/HTMLImport.h"
 #include "core/html/parser/HTMLParserIdioms.h"
@@ -342,12 +341,6 @@ void ScriptLoader::executeScript(const ScriptSourceCode& sourceCode, double* com
         ScriptResource* resource = m_resource ? m_resource.get() : sourceCode.resource();
         if (resource && !resource->mimeTypeAllowedByNosniff()) {
             contextDocument->addConsoleMessage(ConsoleMessage::create(SecurityMessageSource, ErrorMessageLevel, "Refused to execute script from '" + resource->url().elidedString() + "' because its MIME type ('" + resource->mimeType() + "') is not executable, and strict MIME type checking is enabled."));
-            return;
-        }
-
-        if (resource && resource->mimeType().lower().startsWith("image/")) {
-            contextDocument->addConsoleMessage(ConsoleMessage::create(SecurityMessageSource, ErrorMessageLevel, "Refused to execute script from '" + resource->url().elidedString() + "' because its MIME type ('" + resource->mimeType() + "') is not executable."));
-            UseCounter::count(frame, UseCounter::BlockedSniffingImageToScript);
             return;
         }
 
