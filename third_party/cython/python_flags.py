c2dc0361a98d1c14c907199d0d5ce4990766eed2
qsr <qsr@chromium.org>
2014-12-01 09:50:49 -0800

    Update cython rules.
    This allow to produce the source of a python module without creating the
    shared library so that it can be embedded in another application.
    
    R=sdefresne@chromium.org
    
    Review URL: https://codereview.chromium.org/766193002
    
    Cr-Commit-Position: refs/heads/master@{#306204}
    
    Former-commit-id: 7c28a2158a62f2f025f91a13c19c104425e0c190

--- third_party/cython/python_flags.py
+++ third_party/cython/python_flags.py
@@ -16,9 +16,6 @@ def main():
   """
   parser = argparse.ArgumentParser(
       description='Retrieves compilation options for python modules.')
-  parser.add_argument('--gn',
-                      help='Returns all values in a format suitable for gn',
-                      action='store_true')
   parser.add_argument('--libraries', help='Returns libraries',
                       action='store_true')
   parser.add_argument('--includes', help='Returns includes',
@@ -36,11 +33,6 @@ def main():
     libraries = b.get_libraries(ext)
     if sys.platform == 'darwin':
       libraries.append('python%s' % sys.version[:3])
-    if not opts.gn and sys.platform in ['darwin', 'linux2']:
-      # In case of GYP output for darwin and linux prefix all
-      # libraries (if there are any) so the result can be used as a
-      # compiler argument. GN handles platform-appropriate prefixing itself.
-      libraries = ['-l%s' % library for library in libraries]
     result.extend(libraries)
   if opts.includes:
     result = result  + b.include_dirs
@@ -48,11 +40,8 @@ def main():
     if sys.platform == 'darwin':
       result.append('%s/lib' % sysconfig.get_config_vars('prefix')[0])
 
-  if opts.gn:
-    for x in result:
-      print x
-  else:
-    print ''.join(['"%s"' % x for x in result])
+  for x in result:
+    print x
 
 if __name__ == '__main__':
   main()
