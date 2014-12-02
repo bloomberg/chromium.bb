6cecbfee875b34df23faadc701ae671ff4232456
kbr@chromium.org <kbr@chromium.org>
2014-12-02 20:08:23 +0000

    Attempt fix again for UnicodeEncodeError on Android bot.
    It looks to me like the previous attempt reversed "decode" and "encode".
    
    Tested locally. run-webkit-tests still works on Linux.
    
    BUG=386343
    
    Review URL: https://codereview.chromium.org/756393003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186332 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: e7035ccf3752aa2a8298e12ed59c488ac62fb50e

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/android.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/android.py
@@ -257,9 +257,7 @@ class AndroidCommands(object):
         result = self._executive.run_command(self.adb_command() + command, error_handler=error_handler, debug_logging=self._debug_logging)
 
         # We limit the length to avoid outputting too verbose commands, such as "adb logcat".
-        # Also make sure that the output is ascii-encoded to avoid confusing other parts of
-        # the system.
-        self._log_debug('Run adb result: ' + result[:80].encode('ascii', errors='replace'))
+        self._log_debug('Run adb result: ' + result[:80])
         return result
 
     def get_serial(self):
