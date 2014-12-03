a6d2a87864e1c64a7ba00df791fce17c89bd4fee
spang <spang@chromium.org>
2014-12-03 14:09:25 -0800

    ozone: dri: Mark DSI connectors as internal
    So that ash detects the scale factor automatically.
    
    TEST=manual on rush_ryu
    BUG=chrome-os-partner:34264
    
    Review URL: https://codereview.chromium.org/763343004
    
    Cr-Commit-Position: refs/heads/master@{#306685}
    
    Former-commit-id: 83e6b065f6117a1c71426507ea9418850c0c7dc7

--- ui/ozone/platform/dri/display_snapshot_dri.cc
+++ ui/ozone/platform/dri/display_snapshot_dri.cc
@@ -16,6 +16,10 @@
 #include "ui/ozone/platform/dri/dri_util.h"
 #include "ui/ozone/platform/dri/dri_wrapper.h"
 
+#if !defined(DRM_MODE_CONNECTOR_DSI)
+#define DRM_MODE_CONNECTOR_DSI 16
+#endif
+
 namespace ui {
 
 namespace {
@@ -30,6 +34,7 @@ DisplayConnectionType GetDisplayType(drmModeConnector* connector) {
       return DISPLAY_CONNECTION_TYPE_DVI;
     case DRM_MODE_CONNECTOR_LVDS:
     case DRM_MODE_CONNECTOR_eDP:
+    case DRM_MODE_CONNECTOR_DSI:
       return DISPLAY_CONNECTION_TYPE_INTERNAL;
     case DRM_MODE_CONNECTOR_DisplayPort:
       return DISPLAY_CONNECTION_TYPE_DISPLAYPORT;
