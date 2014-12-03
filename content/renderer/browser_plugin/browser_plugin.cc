3393e36d1477dc77127dedc78d20e5ff66a3a5ba
rfevang <rfevang@chromium.org>
2014-12-02 19:17:32 -0800

    Revert of Remove repaint flag in BrowserPluginHostMsg_ResizeGuest IPC (patchset #1 id:1 of https://codereview.chromium.org/769943002/)
    Reason for revert:
    Seems to cause ExtensionBrowserTest.LoadChromeExtensionsWithOptionsParamWhenEmbedded to flakily fail. Fails in about 1 out of 5 attempts locally on my linux machine, much more often than that on the Win 7 dbg builder.
    
    Test fails with message: [19984:19984:1202/185946:FATAL:render_widget_host_impl.cc(1584)] Check failed: repaint_ack_pending_.
    
    Original issue's description:
    > Remove repaint flag in BrowserPluginHostMsg_ResizeGuest IPC
    >
    > BUG=434226
    >
    > Committed: https://crrev.com/ce18b18224d6d76e9eee768a1dcabd42ddc87c6c [formerly 520b543435cdfc2c316c7ccc6225d29c974c93a9]
    > Cr-Commit-Position: refs/heads/master@{#306216}
    
    TBR=lazyboy@chromium.org,kenrb@chromium.org,fsamuel@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=434226
    
    Review URL: https://codereview.chromium.org/779513002
    
    Cr-Commit-Position: refs/heads/master@{#306536}
    
    Former-commit-id: dbf76266874b4b48ac25b9c66fb7b014998e5920

--- content/renderer/browser_plugin/browser_plugin.cc
+++ content/renderer/browser_plugin/browser_plugin.cc
@@ -447,7 +447,10 @@ void BrowserPlugin::PopulateResizeGuestParameters(
     BrowserPluginHostMsg_ResizeGuest_Params* params) {
   params->view_size = view_size;
   params->scale_factor = GetDeviceScaleFactor();
-  last_device_scale_factor_ = params->scale_factor;
+  if (last_device_scale_factor_ != params->scale_factor) {
+    last_device_scale_factor_ = params->scale_factor;
+    params->repaint = true;
+  }
 }
 
 void BrowserPlugin::updateFocus(bool focused) {
