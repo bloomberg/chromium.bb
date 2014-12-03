0f882590bf819f0ad2db44b422c850d206dfadff
pkotwicz <pkotwicz@chromium.org>
2014-12-03 13:59:31 -0800

    Revert of Disable flaky OutOfProcessPPAPITest.MediaStreamAudioTrack on Windows (patchset #1 id:1 of https://codereview.chromium.org/770423005/)
    Reason for revert:
    Caused compile failure
    
    Original issue's description:
    > Disable flaky OutOfProcessPPAPITest.MediaStreamAudioTrack on Windows
    >
    > BUG=438729
    > TEST=None
    > TBR=bbudge
    > NOTRY=true
    >
    > Committed: https://crrev.com/580b54f97b199d18469b9a7e1d5f60980b689f6f [formerly cd4450ef786b0a8f801d17a0563eeb67198de8e0]
    > Cr-Commit-Position: refs/heads/master@{#306680}
    
    TBR=bbudge@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=438729
    
    Review URL: https://codereview.chromium.org/776703005
    
    Cr-Commit-Position: refs/heads/master@{#306684}
    
    Former-commit-id: de44b7145d13b900f8f46fc7aa011a4125eb6d63

--- content/test/ppapi/ppapi_browsertest.cc
+++ content/test/ppapi/ppapi_browsertest.cc
@@ -101,13 +101,7 @@ IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest,
   RunTestAndReload("Instance_RecursiveObjects");
 }
 
-#if defined(OS_WIN)
-// Flaky on Windows (crbug.com/438729)
-#define MAYBE_MediaStreamAudioTrack DISABLED_MediaStreamAudioTrack;
-#else
-#define MAYBE_MediaStreamAudioTrack MediaStreamAudioTrack;
-#endif
-TEST_PPAPI_OUT_OF_PROCESS(MAYBE_MediaStreamAudioTrack)
+TEST_PPAPI_OUT_OF_PROCESS(MediaStreamAudioTrack)
 
 TEST_PPAPI_OUT_OF_PROCESS(MediaStreamVideoTrack)
 
