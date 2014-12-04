b85cd45fe5e343de373e47a6483c95bd995f576a
scottmg <scottmg@chromium.org>
2014-12-03 16:25:20 -0800

    win: Disable use of FMA3 versions of CRT functions on x64
    Using the FMA3 path in the CRT math functions is suspected of causing
    crashes when disabled at the OS level.
    
    R=cpu@chromium.org
    BUG=436603
    
    Review URL: https://codereview.chromium.org/771183004
    
    Cr-Commit-Position: refs/heads/master@{#306728}
    
    Former-commit-id: 255cf5511b0932fd3e500089ca3d53bd7e5c13c9

--- chrome/app/chrome_main.cc
+++ chrome/app/chrome_main.cc
@@ -31,6 +31,15 @@ DLLEXPORT int __cdecl ChromeMain(HINSTANCE instance,
 #elif defined(OS_POSIX)
 int ChromeMain(int argc, const char** argv) {
 #endif
+#if defined(OS_WIN) && defined(ARCH_CPU_X86_64)
+  // VS2013 only checks the existence of FMA3 instructions, not the enabled-ness
+  // of them at the OS level (this is fixed in VS2015). We force off usage of
+  // FMA3 instructions in the CRT to avoid using that path and hitting illegal
+  // instructions when running on CPUs that support FMA3, but OSs that don't.
+  // See http://crbug.com/436603.
+  _set_FMA3_enable(0);
+#endif  // WIN && ARCH_CPU_X86_64
+
   ChromeMainDelegate chrome_main_delegate;
   content::ContentMainParams params(&chrome_main_delegate);
 
