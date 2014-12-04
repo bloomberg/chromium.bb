f6cfd70cf1c104849f92f94e73edc78a6e4f9c41
yukishiino@chromium.org <yukishiino@chromium.org>
2014-12-04 10:37:10 +0000

    bindings: Retires public/platform/WebArrayBuffer.h.
    As promised, removes public/platform/WebArrayBuffer.h.
    public/web/WebArrayBuffer.h provides the same functionality as before.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/773133007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186496 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 503890ecc80110ca9c6d859bc83c12d4b49f5ba5

--- third_party/WebKit/public/web/WebArrayBuffer.h
+++ third_party/WebKit/public/web/WebArrayBuffer.h
@@ -1,9 +1,83 @@
-// Copyright 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/*
+ * Copyright (C) 2011 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following disclaimer
+ * in the documentation and/or other materials provided with the
+ * distribution.
+ *     * Neither the name of Google Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
-// Delegate everything to public/platform/WebArrayBuffer.h for transition
-// period from public/platform/WebArrayBuffer.h to public/web/WebArrayBuffer.h.
-// FIXME: Once the clients get the transition done, the content of
-// public/platform/WebArrayBuffer.h will be moved here.
-#include "../platform/WebArrayBuffer.h"
+#ifndef WebArrayBuffer_h
+#define WebArrayBuffer_h
+
+#include "../platform/WebCommon.h"
+#include "../platform/WebPrivatePtr.h"
+
+namespace v8 {
+class Value;
+template <class T> class Handle;
+}
+
+#if INSIDE_BLINK
+namespace WTF { template <typename T> class PassRefPtr; }
+#endif
+
+namespace blink {
+
+class DOMArrayBuffer;
+
+class WebArrayBuffer {
+public:
+    ~WebArrayBuffer() { reset(); }
+
+    WebArrayBuffer() { }
+    WebArrayBuffer(const WebArrayBuffer& b) { assign(b); }
+    WebArrayBuffer& operator=(const WebArrayBuffer& b)
+    {
+        assign(b);
+        return *this;
+    }
+
+    BLINK_EXPORT static WebArrayBuffer create(unsigned numElements, unsigned elementByteSize);
+
+    BLINK_EXPORT void reset();
+    BLINK_EXPORT void assign(const WebArrayBuffer&);
+
+    bool isNull() const { return m_private.isNull(); }
+    BLINK_EXPORT void* data() const;
+    BLINK_EXPORT unsigned byteLength() const;
+
+#if INSIDE_BLINK
+    BLINK_EXPORT WebArrayBuffer(const WTF::PassRefPtr<DOMArrayBuffer>&);
+    BLINK_EXPORT WebArrayBuffer& operator=(const WTF::PassRefPtr<DOMArrayBuffer>&);
+    BLINK_EXPORT operator WTF::PassRefPtr<DOMArrayBuffer>() const;
+#endif
+
+protected:
+    WebPrivatePtr<DOMArrayBuffer> m_private;
+};
+
+} // namespace blink
+
+#endif // WebArrayBuffer_h
