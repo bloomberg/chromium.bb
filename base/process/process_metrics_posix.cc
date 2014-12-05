4fbeebedf68292f8b42b29aa7ca3772091512c9c
prashhir <prashhir@cisco.com>
2014-12-04 21:20:13 -0800

    Use base::Time constants in base module rather than defining locally.
    BUG=412254
    
    Review URL: https://codereview.chromium.org/773673003
    
    Cr-Commit-Position: refs/heads/master@{#306986}
    
    Former-commit-id: fd55f2e85b0f02f1662b1c2c5b5e79fbc0b8567f

--- base/process/process_metrics_posix.cc
+++ base/process/process_metrics_posix.cc
@@ -12,9 +12,8 @@
 namespace base {
 
 int64 TimeValToMicroseconds(const struct timeval& tv) {
-  static const int kMicrosecondsPerSecond = 1000000;
   int64 ret = tv.tv_sec;  // Avoid (int * int) integer overflow.
-  ret *= kMicrosecondsPerSecond;
+  ret *= Time::kMicrosecondsPerSecond;
   ret += tv.tv_usec;
   return ret;
 }
