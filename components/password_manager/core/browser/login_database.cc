99ccaadc6f8906a6654cda944888361f0ab435dd
vabr <vabr@chromium.org>
2014-12-08 05:54:54 -0800

    [Password manager] Remove temporary logging
    This CL removes VLOGs added to investigate the referenced bug.
    They are no longer needed.
    
    BUG=423327
    
    Review URL: https://codereview.chromium.org/784863002
    
    Cr-Commit-Position: refs/heads/master@{#307238}
    
    Former-commit-id: 737cd5803cb08eb6699967e7f1ca5d1ce2a7de04

--- components/password_manager/core/browser/login_database.cc
+++ components/password_manager/core/browser/login_database.cc
@@ -428,9 +428,6 @@ PasswordStoreChangeList LoginDatabase::AddLogin(const PasswordForm& form) {
           ENCRYPTION_RESULT_SUCCESS)
     return list;
 
-  // This logging is only temporary, to investigate http://crbug.com/423327.
-  VLOG(4) << "AddLogin with signon_realm = " << form.signon_realm;
-
   // You *must* change LoginTableColumns if this query changes.
   sql::Statement s(db_.GetCachedStatement(SQL_FROM_HERE,
       "INSERT INTO logins "
@@ -472,9 +469,6 @@ PasswordStoreChangeList LoginDatabase::UpdateLogin(const PasswordForm& form) {
   if (EncryptedString(form.password_value, &encrypted_password) !=
           ENCRYPTION_RESULT_SUCCESS)
     return PasswordStoreChangeList();
-
-  // This logging is only temporary, to investigate http://crbug.com/423327.
-  VLOG(4) << "UpdateLogin with signon_realm = " << form.signon_realm;
 
   // Replacement is necessary to deal with updating imported credentials. See
   // crbug.com/349138 for details.
@@ -694,17 +688,12 @@ bool LoginDatabase::GetLogins(const PasswordForm& form,
   } else {
     psl_domain_match_metric = PSL_DOMAIN_MATCH_NOT_USED;
     s.Assign(db_.GetCachedStatement(SQL_FROM_HERE, sql_query.c_str()));
-    // This logging is only temporary, to investigate http://crbug.com/423327.
-    VLOG(4) << "Executing SQL query [" << sql_query << "] with realm ["
-            << form.signon_realm << "]";
     s.BindString(0, form.signon_realm);
   }
 
   while (s.Step()) {
     scoped_ptr<PasswordForm> new_form(new PasswordForm());
     EncryptionResult result = InitPasswordFormFromStatement(new_form.get(), s);
-    // This logging is only temporary, to investigate http://crbug.com/423327.
-    VLOG(4) << "Encryption result = " << result;
     if (result == ENCRYPTION_RESULT_SERVICE_FAILURE)
       return false;
     if (result == ENCRYPTION_RESULT_ITEM_FAILURE)
@@ -734,8 +723,6 @@ bool LoginDatabase::GetLogins(const PasswordForm& form,
         new_form->action = form.action;
       }
     }
-    // This logging is only temporary, to investigate http://crbug.com/423327.
-    VLOG(4) << "Found form with username " << new_form->username_value;
     forms->push_back(new_form.release());
   }
   UMA_HISTOGRAM_ENUMERATION("PasswordManager.PslDomainMatchTriggering",
--- components/password_manager/core/browser/password_form_manager.cc
+++ components/password_manager/core/browser/password_form_manager.cc
@@ -381,8 +381,6 @@ void PasswordFormManager::FetchMatchingLoginsFromPasswordStore(
     NOTREACHED();
     return;
   }
-  // This logging is only temporary, to investigate http://crbug.com/423327.
-  VLOG(4) << "Asking the store for logins for this form: " << observed_form_;
   password_store->GetLogins(observed_form_, prompt_policy, this);
 }
 
@@ -604,8 +602,6 @@ void PasswordFormManager::SaveAsNewLogin(bool reset_preferred_login) {
 
   pending_credentials_.date_created = Time::Now();
   SanitizePossibleUsernames(&pending_credentials_);
-  // This logging is only temporary, to investigate http://crbug.com/423327.
-  VLOG(4) << "Adding this login: " << pending_credentials_;
   password_store->AddLogin(pending_credentials_);
 
   if (reset_preferred_login) {
@@ -640,8 +636,6 @@ void PasswordFormManager::UpdatePreferredLoginState(
       iter->second->preferred = false;
       if (user_action_ == kUserActionNone)
         user_action_ = kUserActionChoose;
-      // This logging is only temporary, to investigate http://crbug.com/423327.
-      VLOG(4) << "Updating this login: " << *iter->second;
       password_store->UpdateLogin(*iter->second);
     }
   }
@@ -684,8 +678,6 @@ void PasswordFormManager::UpdateLogin() {
     // username, so we delete the old credentials and add a new one instead.
     password_store->RemoveLogin(pending_credentials_);
     pending_credentials_.username_value = selected_username_;
-    // This logging is only temporary, to investigate http://crbug.com/423327.
-    VLOG(4) << "Adding this login: " << pending_credentials_;
     password_store->AddLogin(pending_credentials_);
   } else if ((observed_form_.scheme == PasswordForm::SCHEME_HTML) &&
              (observed_form_.origin.spec().length() >
@@ -710,8 +702,6 @@ void PasswordFormManager::UpdateLogin() {
     PasswordForm copy(pending_credentials_);
     copy.origin = observed_form_.origin;
     copy.action = observed_form_.action;
-    // This logging is only temporary, to investigate http://crbug.com/423327.
-    VLOG(4) << "Adding this login: " << copy;
     password_store->AddLogin(copy);
   } else if (observed_form_.new_password_element.empty() &&
              (pending_credentials_.password_element.empty() ||
@@ -727,12 +717,8 @@ void PasswordFormManager::UpdateLogin() {
     pending_credentials_.password_element = observed_form_.password_element;
     pending_credentials_.username_element = observed_form_.username_element;
     pending_credentials_.submit_element = observed_form_.submit_element;
-    // This logging is only temporary, to investigate http://crbug.com/423327.
-    VLOG(4) << "Adding this login: " << pending_credentials_;
     password_store->AddLogin(pending_credentials_);
   } else {
-    // This logging is only temporary, to investigate http://crbug.com/423327.
-    VLOG(4) << "Updating this login: " << pending_credentials_;
     password_store->UpdateLogin(pending_credentials_);
   }
 }
