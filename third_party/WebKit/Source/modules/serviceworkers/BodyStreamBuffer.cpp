08bc82e5727836888bd150b2557e092aa31c86f4
horo@chromium.org <horo@chromium.org>
2014-12-09 08:50:02 +0000

    [ServiceWorker] Introduce BodyStreamBuffer.
    Currently we use BlobDataHandle as the body data of Request and Response object.
    BlobDataHandle can only handle static data.
    It can't handle progressive data.
    We will use BodyStreamBuffer for Response objects which are created in FetchManager to support progressive loading.
    
    BUG=436424
    
    Review URL: https://codereview.chromium.org/770233002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186765 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 448bfbe4dc028271e5349e63216c9738dfa695d1

---
+++ third_party/WebKit/Source/modules/serviceworkers/BodyStreamBuffer.cpp
@@ -0,0 +1,145 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "modules/serviceworkers/BodyStreamBuffer.h"
+
+#include "core/dom/DOMArrayBuffer.h"
+
+namespace blink {
+
+namespace {
+
+class BlobCreator final : public BodyStreamBuffer::Observer {
+public:
+    BlobCreator(BodyStreamBuffer* buffer, BodyStreamBuffer::BlobHandleCreatorClient* client)
+        : m_buffer(buffer)
+        , m_client(client)
+        , m_blobData(BlobData::create())
+    {
+    }
+    ~BlobCreator() override { }
+    void trace(Visitor* visitor) override
+    {
+        visitor->trace(m_buffer);
+        visitor->trace(m_client);
+        BodyStreamBuffer::Observer::trace(visitor);
+    }
+    void onWrite() override
+    {
+        ASSERT(m_buffer);
+        while (RefPtr<DOMArrayBuffer> buf = m_buffer->read()) {
+            m_blobData->appendBytes(buf->data(), buf->byteLength());
+        }
+    }
+    void onClose() override
+    {
+        ASSERT(m_buffer);
+        const long long size = m_blobData->length();
+        m_client->didCreateBlobHandle(BlobDataHandle::create(m_blobData.release(), size));
+        cleanup();
+    }
+    void onError() override
+    {
+        ASSERT(m_buffer);
+        m_client->didFail(m_buffer->exception());
+        cleanup();
+    }
+    void start()
+    {
+        ASSERT(!m_buffer->isObserverRegistered());
+        m_buffer->registerObserver(this);
+        onWrite();
+        if (m_buffer->hasError()) {
+            return onError();
+        }
+        if (m_buffer->isClosed())
+            return onClose();
+    }
+    void cleanup()
+    {
+        m_buffer->unregisterObserver();
+        m_buffer.clear();
+        m_client.clear();
+        m_blobData.clear();
+    }
+private:
+    Member<BodyStreamBuffer> m_buffer;
+    Member<BodyStreamBuffer::BlobHandleCreatorClient> m_client;
+    OwnPtr<BlobData> m_blobData;
+};
+
+} // namespace
+
+PassRefPtr<DOMArrayBuffer> BodyStreamBuffer::read()
+{
+    if (m_queue.isEmpty())
+        return PassRefPtr<DOMArrayBuffer>();
+    return m_queue.takeFirst();
+}
+
+void BodyStreamBuffer::write(PassRefPtr<DOMArrayBuffer> chunk)
+{
+    ASSERT(!m_isClosed);
+    ASSERT(!m_exception);
+    ASSERT(chunk);
+    m_queue.append(chunk);
+    if (m_observer)
+        m_observer->onWrite();
+}
+
+void BodyStreamBuffer::close()
+{
+    ASSERT(!m_isClosed);
+    ASSERT(!m_exception);
+    m_isClosed = true;
+    if (m_observer)
+        m_observer->onClose();
+}
+
+void BodyStreamBuffer::error(PassRefPtrWillBeRawPtr<DOMException> exception)
+{
+    ASSERT(exception);
+    ASSERT(!m_isClosed);
+    ASSERT(!m_exception);
+    m_exception = exception;
+    if (m_observer)
+        m_observer->onError();
+}
+
+bool BodyStreamBuffer::readAllAndCreateBlobHandle(BlobHandleCreatorClient* client)
+{
+    if (m_observer)
+        return false;
+    BlobCreator* blobCreator = new BlobCreator(this, client);
+    blobCreator->start();
+    return true;
+}
+
+bool BodyStreamBuffer::registerObserver(Observer* observer)
+{
+    if (m_observer)
+        return false;
+    ASSERT(observer);
+    m_observer = observer;
+    return true;
+}
+
+void BodyStreamBuffer::unregisterObserver()
+{
+    m_observer.clear();
+}
+
+void BodyStreamBuffer::trace(Visitor* visitor)
+{
+    visitor->trace(m_exception);
+    visitor->trace(m_observer);
+}
+
+BodyStreamBuffer::BodyStreamBuffer()
+    : m_isClosed(false)
+{
+}
+
+} // namespace blink
---
+++ third_party/WebKit/Source/modules/serviceworkers/BodyStreamBuffer.h
@@ -0,0 +1,69 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BodyStreamBuffer_h
+#define BodyStreamBuffer_h
+
+#include "core/dom/DOMException.h"
+#include "platform/blob/BlobData.h"
+#include "platform/heap/Heap.h"
+#include "wtf/Deque.h"
+#include "wtf/RefPtr.h"
+
+namespace blink {
+
+class DOMArrayBuffer;
+
+class BodyStreamBuffer final : public GarbageCollectedFinalized<BodyStreamBuffer> {
+public:
+    class Observer : public GarbageCollectedFinalized<Observer> {
+    public:
+        virtual ~Observer() { }
+        virtual void onWrite() = 0;
+        virtual void onClose() = 0;
+        virtual void onError() = 0;
+        virtual void trace(Visitor*) { }
+    };
+    class BlobHandleCreatorClient : public GarbageCollectedFinalized<BlobHandleCreatorClient> {
+    public:
+        virtual ~BlobHandleCreatorClient() { }
+        virtual void didCreateBlobHandle(PassRefPtr<BlobDataHandle>) = 0;
+        virtual void didFail(PassRefPtrWillBeRawPtr<DOMException>) = 0;
+        virtual void trace(Visitor*) { }
+    };
+    BodyStreamBuffer();
+    ~BodyStreamBuffer() { }
+
+    PassRefPtr<DOMArrayBuffer> read();
+    bool isClosed() const { return m_isClosed; }
+    bool hasError() const { return m_exception; }
+    PassRefPtrWillBeRawPtr<DOMException> exception() const { return m_exception; }
+
+    // Can't call after close() or error() was called.
+    void write(PassRefPtr<DOMArrayBuffer>);
+    // Can't call after close() or error() was called.
+    void close();
+    // Can't call after close() or error() was called.
+    void error(PassRefPtrWillBeRawPtr<DOMException>);
+
+    // This function registers an observer so it fails and returns false when an
+    // observer was already registered
+    bool readAllAndCreateBlobHandle(BlobHandleCreatorClient*);
+
+    // When an observer was registered this function fails and returns false.
+    bool registerObserver(Observer*);
+    void unregisterObserver();
+    bool isObserverRegistered() const { return m_observer.get(); }
+    void trace(Visitor*);
+
+private:
+    Deque<RefPtr<DOMArrayBuffer> > m_queue;
+    bool m_isClosed;
+    RefPtrWillBeMember<DOMException> m_exception;
+    Member<Observer> m_observer;
+};
+
+} // namespace blink
+
+#endif // BodyStreamBuffer_h
