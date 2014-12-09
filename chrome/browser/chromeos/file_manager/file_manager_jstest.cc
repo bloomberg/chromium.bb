8d232d93f249442dc35e91d2a90a695dda0bd592
hirono <hirono@chromium.org>
2014-12-08 21:46:18 -0800

    Files.app: Move FileManagerJSTest resources to the same directory with production code.
    BUG=439349
    TEST=browser_tests --gtest_filter="FileManagerJsTest.*"
    
    Review URL: https://codereview.chromium.org/792443003
    
    Cr-Commit-Position: refs/heads/master@{#307423}
    
    Former-commit-id: e4f0e97cd65cd5202d0cba2011b5c1fafe0e5c14

--- chrome/browser/chromeos/file_manager/file_manager_jstest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_jstest.cc
@@ -4,22 +4,28 @@
 
 #include <vector>
 
+#include "base/path_service.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/browser_test_utils.h"
+#include "net/base/filename_util.h"
 
 class FileManagerJsTest : public InProcessBrowserTest {
  public:
   // Runs all test functions in |file|, waiting for them to complete.
   void RunTest(const base::FilePath& file) {
-    GURL url = ui_test_utils::GetTestUrl(
-        base::FilePath(FILE_PATH_LITERAL("file_manager/unit_tests")), file);
+    base::FilePath root_path;
+    ASSERT_TRUE(PathService::Get(base::DIR_SOURCE_ROOT, &root_path));
+
+    const GURL url = net::FilePathToFileURL(
+        root_path.Append(FILE_PATH_LITERAL("ui/file_manager/file_manager"))
+            .Append(file));
     ui_test_utils::NavigateToURL(browser(), url);
 
-    content::WebContents* web_contents =
+    content::WebContents* const web_contents =
         browser()->tab_strip_model()->GetActiveWebContents();
     ASSERT_TRUE(web_contents);
 
@@ -28,65 +34,62 @@ class FileManagerJsTest : public InProcessBrowserTest {
   }
 };
 
-IN_PROC_BROWSER_TEST_F(
-    FileManagerJsTest, NavigationListModelTest) {
+IN_PROC_BROWSER_TEST_F(FileManagerJsTest, NavigationListModelTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("navigation_list_model_unittest.html")));
+      FILE_PATH_LITERAL("foreground/js/navigation_list_model_unittest.html")));
 }
 
-IN_PROC_BROWSER_TEST_F(
-    FileManagerJsTest, FileOperationHandlerTest) {
+IN_PROC_BROWSER_TEST_F(FileManagerJsTest, FileOperationHandlerTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("file_operation_handler_unittest.html")));
+      FILE_PATH_LITERAL("background/js/file_operation_handler_unittest.html")));
 }
 
-IN_PROC_BROWSER_TEST_F(
-    FileManagerJsTest, ProgressCenterItemGroupTest) {
+IN_PROC_BROWSER_TEST_F(FileManagerJsTest, ProgressCenterItemGroupTest) {
+  RunTest(base::FilePath(FILE_PATH_LITERAL(
+      "foreground/js/progress_center_item_group_unittest.html")));
+}
+
+IN_PROC_BROWSER_TEST_F(FileManagerJsTest, DeviceHandlerTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("progress_center_item_group_unittest.html")));
+      FILE_PATH_LITERAL("background/js/device_handler_unittest.html")));
 }
 
-IN_PROC_BROWSER_TEST_F(
-    FileManagerJsTest, DeviceHandlerTest) {
-  RunTest(base::FilePath(FILE_PATH_LITERAL("device_handler_unittest.html")));
-}
-
-IN_PROC_BROWSER_TEST_F(
-    FileManagerJsTest, MetadataCacheTest) {
-  RunTest(base::FilePath(FILE_PATH_LITERAL("metadata_cache_unittest.html")));
+IN_PROC_BROWSER_TEST_F(FileManagerJsTest, MetadataCacheTest) {
+  RunTest(base::FilePath(FILE_PATH_LITERAL(
+      "foreground/js/metadata/metadata_cache_unittest.html")));
 }
 
 IN_PROC_BROWSER_TEST_F(FileManagerJsTest, FileOperationManagerTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("file_operation_manager_unittest.html")));
+      FILE_PATH_LITERAL("background/js/file_operation_manager_unittest.html")));
 }
 
 IN_PROC_BROWSER_TEST_F(FileManagerJsTest, ImportHistoryTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("import_history_unittest.html")));
+      FILE_PATH_LITERAL("background/js/import_history_unittest.html")));
 }
 
 IN_PROC_BROWSER_TEST_F(FileManagerJsTest, VolumeManagerTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("volume_manager_unittest.html")));
+      FILE_PATH_LITERAL("background/js/volume_manager_unittest.html")));
 }
 
 IN_PROC_BROWSER_TEST_F(FileManagerJsTest, DirectoryTreeTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("directory_tree_unittest.html")));
+      FILE_PATH_LITERAL("foreground/js/ui/directory_tree_unittest.html")));
 }
 
 IN_PROC_BROWSER_TEST_F(FileManagerJsTest, MediaScannerTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("media_scanner_unittest.html")));
+      FILE_PATH_LITERAL("background/js/media_scanner_unittest.html")));
 }
 
 IN_PROC_BROWSER_TEST_F(FileManagerJsTest, LRUCacheTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("lru_cache_unittest.html")));
+      FILE_PATH_LITERAL("common/js/lru_cache_unittest.html")));
 }
 
-IN_PROC_BROWSER_TEST_F(FileManagerJsTest, MediaImporterTest) {
+IN_PROC_BROWSER_TEST_F(FileManagerJsTest, MediaImportHandlerTest) {
   RunTest(base::FilePath(
-      FILE_PATH_LITERAL("media_import_handler_unittest.html")));
+      FILE_PATH_LITERAL("background/js/media_import_handler_unittest.html")));
 }
