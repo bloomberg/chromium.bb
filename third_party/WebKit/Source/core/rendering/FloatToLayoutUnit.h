ec8b1927291482f18f410bf09f68fda1d3399c00
hartmanng@chromium.org <hartmanng@chromium.org>
2014-12-10 19:45:08 +0000

    Add more intermediate float-backed types for LineBoxes.
    This CL adds FloatPointLineLayoutPoint,
    FloatSizeLineLayoutSize, and
    FloatRectLineLayoutRect, as described as
    part of step 2 in
    https://docs.google.com/a/chromium.org/document/d/1fro9Drq78rYBwr6K9CPK-y0TDSVxlBuXl6A54XnKAyE/edit.
    
    These classes may not be 100% complete, and may need
    a few additional methods added on in future CLs.
    
    BUG=321237
    
    Review URL: https://codereview.chromium.org/750623003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@186869 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 4fd05b4795ac56b643dd4d03ce4f3cb7bc50507a

--- third_party/WebKit/Source/core/rendering/FloatToLayoutUnit.h
+++ third_party/WebKit/Source/core/rendering/FloatToLayoutUnit.h
@@ -15,16 +15,16 @@
 #define FloatToLayoutUnit_h
 
 #include "platform/LayoutUnit.h"
+#include "platform/geometry/FloatPoint.h"
+#include "platform/geometry/FloatRect.h"
+#include "platform/geometry/FloatSize.h"
+#include "platform/geometry/LayoutPoint.h"
+#include "platform/geometry/LayoutRect.h"
+#include "platform/geometry/LayoutSize.h"
 
 namespace blink {
 
-class FloatPoint;
-class FloatRect;
-class FloatSize;
-class LayoutUnit;
-class LayoutPoint;
-class LayoutRect;
-class LayoutSize;
+class IntPoint;
 
 class FloatLineLayoutUnit {
 public:
@@ -598,10 +598,137 @@ private:
     LayoutUnit m_value;
 };
 
+class FloatPointLineLayoutPoint {
+public:
+    FloatPointLineLayoutPoint() { }
+    FloatPointLineLayoutPoint(const FloatPoint& floatPoint) : m_value(floatPoint) { }
+    FloatPointLineLayoutPoint(FloatLineLayoutUnit x, FloatLineLayoutUnit y) : m_value(x.toFloat(), y.toFloat()) { }
+    FloatPointLineLayoutPoint(const IntPoint& intPoint) : m_value(intPoint) { }
+    FloatPointLineLayoutPoint(const LayoutPoint& layoutPoint) : m_value(layoutPoint) { }
+
+    operator FloatPoint() const { return toFloatPoint(); }
+    operator LayoutPoint() const { return toLayoutPoint(); }
+
+    FloatPoint toFloatPoint() const
+    {
+        return m_value;
+    }
+
+    LayoutPoint toLayoutPoint() const
+    {
+        return LayoutPoint(m_value);
+    }
+
+    FloatLineLayoutUnit x() const { return FloatLineLayoutUnit(m_value.x()); }
+    FloatLineLayoutUnit y() const { return FloatLineLayoutUnit(m_value.y()); }
+
+    void setX(FloatLineLayoutUnit x) { m_value.setX(x.toFloat()); }
+    void setY(FloatLineLayoutUnit y) { m_value.setY(y.toFloat()); }
+    void set(FloatLineLayoutUnit x, FloatLineLayoutUnit y) { m_value.set(x.toFloat(), y.toFloat()); }
+
+    void move(FloatLineLayoutUnit dx, FloatLineLayoutUnit dy) { m_value.move(dx.toFloat(), dy.toFloat()); }
+    void moveBy(const LayoutPoint& layoutPoint) { m_value.moveBy(layoutPoint); }
+
+    void scale(FloatLineLayoutUnit sx, FloatLineLayoutUnit sy) { m_value.scale(sx.toFloat(), sy.toFloat()); }
+
+private:
+    FloatPoint m_value;
+};
+
+class FloatSizeLineLayoutSize {
+public:
+    FloatSizeLineLayoutSize() { }
+    FloatSizeLineLayoutSize(const FloatSize& floatSize) : m_value(floatSize) { }
+    FloatSizeLineLayoutSize(FloatLineLayoutUnit width, FloatLineLayoutUnit height) : m_value(width.toFloat(), height.toFloat()) { }
+    FloatSizeLineLayoutSize(const IntSize& size) : m_value(size) { }
+    FloatSizeLineLayoutSize(const LayoutSize& size) : m_value(size) { }
+
+    operator FloatSize() const { return toFloatSize(); }
+    operator LayoutSize() const { return toLayoutSize(); }
+
+    FloatSize toFloatSize() const
+    {
+        return m_value;
+    }
+
+    LayoutSize toLayoutSize() const
+    {
+        return LayoutSize(m_value);
+    }
+
+    FloatLineLayoutUnit width() const { return FloatLineLayoutUnit(m_value.width()); }
+    FloatLineLayoutUnit height() const { return FloatLineLayoutUnit(m_value.height()); }
+
+    void setWidth(FloatLineLayoutUnit width) { m_value.setWidth(width.toFloat()); }
+    void setHeight(FloatLineLayoutUnit height) { m_value.setHeight(height.toFloat()); }
+
+private:
+    FloatSize m_value;
+};
+
+class FloatRectLineLayoutRect {
+public:
+    FloatRectLineLayoutRect() { }
+    FloatRectLineLayoutRect(const FloatRect& floatRect) : m_value(floatRect) { }
+    FloatRectLineLayoutRect(const FloatPointLineLayoutPoint& location, const FloatSizeLineLayoutSize& size)
+        : m_value(location.toFloatPoint(), size.toFloatSize()) { }
+    FloatRectLineLayoutRect(FloatLineLayoutUnit x, FloatLineLayoutUnit y, FloatLineLayoutUnit width, FloatLineLayoutUnit height)
+        : m_value(x.toFloat(), y.toFloat(), width.toFloat(), height.toFloat()) { }
+    FloatRectLineLayoutRect(const IntRect& rect) : m_value(rect) { }
+    FloatRectLineLayoutRect(const LayoutRect& rect) : m_value(rect) { }
+
+    operator FloatRect() const { return toFloatRect(); }
+    operator LayoutRect() const { return toLayoutRect(); }
+
+    FloatRect toFloatRect() const
+    {
+        return m_value;
+    }
+
+    LayoutRect toLayoutRect() const
+    {
+        return LayoutRect(m_value);
+    }
+
+    FloatPointLineLayoutPoint location() const { return FloatPointLineLayoutPoint(m_value.location()); }
+    FloatSizeLineLayoutSize size() const { return FloatSizeLineLayoutSize(m_value.size()); }
+
+    void setLocation(const FloatPointLineLayoutPoint& location) { m_value.setLocation(location.toFloatPoint()); }
+    void setSize(const FloatSizeLineLayoutSize& size) { m_value.setSize(size.toFloatSize()); }
+
+    FloatLineLayoutUnit x() const { return FloatLineLayoutUnit(m_value.x()); }
+    FloatLineLayoutUnit y() const { return FloatLineLayoutUnit(m_value.y()); }
+    FloatLineLayoutUnit maxX() const { return FloatLineLayoutUnit(m_value.maxX()); }
+    FloatLineLayoutUnit maxY() const { return FloatLineLayoutUnit(m_value.maxY()); }
+    FloatLineLayoutUnit width() const { return FloatLineLayoutUnit(m_value.width()); }
+    FloatLineLayoutUnit height() const { return FloatLineLayoutUnit(m_value.height()); }
+
+    void setX(FloatLineLayoutUnit x) { m_value.setX(x.toFloat()); }
+    void setY(FloatLineLayoutUnit y) { m_value.setY(y.toFloat()); }
+    void setWidth(FloatLineLayoutUnit width) { m_value.setWidth(width.toFloat()); }
+    void setHeight(FloatLineLayoutUnit height) { m_value.setHeight(height.toFloat()); }
+
+    void unite(const FloatRectLineLayoutRect& rect) { m_value.unite(rect.toFloatRect()); }
+
+    void scale(FloatLineLayoutUnit s) { m_value.scale(s.toFloat()); }
+    void scale(FloatLineLayoutUnit sx, FloatLineLayoutUnit sy) { m_value.scale(sx.toFloat(), sy.toFloat()); }
+
+    bool intersects(const FloatRectLineLayoutRect& rect) const { return m_value.intersects(rect.toFloatRect()); }
+
+private:
+    FloatRect m_value;
+};
+
 #if ENABLE(LAYOUT_UNIT_IN_INLINE_BOXES)
 using LineLayoutUnit = LayoutUnitLineLayoutUnit;
+using LineLayoutPoint = LayoutPointLineLayoutPoint;
+using LineLayoutRect = LayoutRectLineLayoutRect;
+using LineLayoutSize = LayoutSizeLineLayoutSize;
 #else // ENABLE(LAYOUT_UNIT_IN_INLINE_BOXES)
 using LineLayoutUnit = FloatLineLayoutUnit;
+using LineLayoutPoint = FloatPointLineLayoutPoint;
+using LineLayoutRect = FloatRectLineLayoutRect;
+using LineLayoutSize = FloatSizeLineLayoutSize;
 #endif // ENABLE(LAYOUT_UNIT_IN_INLINE_BOXES)
 
 using FloatWillBeLayoutUnit = float;
