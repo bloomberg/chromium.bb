60b881d15df732e21b4b078d2b6b3cf96cc67912
rvargas <rvargas@chromium.org>
2014-12-10 10:32:57 -0800

    Update browser/sessions to use the new version of LaunchProcess.
    BUG=417532
    R=marja@chromium.org
    
    Review URL: https://codereview.chromium.org/788043003
    
    Cr-Commit-Position: refs/heads/master@{#307735}
    
    Former-commit-id: 094124b9e9f1df4833ae9d55a411ade6eed41697

--- chrome/browser/sessions/session_restore_browsertest.cc
+++ chrome/browser/sessions/session_restore_browsertest.cc
@@ -994,7 +994,7 @@ IN_PROC_BROWSER_TEST_F(SessionRestoreTest,
 
   ui_test_utils::BrowserAddedObserver window_observer;
 
-  base::LaunchProcess(app_launch_arguments, base::LaunchOptionsForTest(), NULL);
+  base::LaunchProcess(app_launch_arguments, base::LaunchOptionsForTest());
 
   Browser* app_window = window_observer.WaitForSingleNewBrowser();
   ASSERT_EQ(2u, active_browser_list_->size());
