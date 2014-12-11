976a47e5d62c7bc54af9364c5e772675b90ab081
dschuff@chromium.org <dschuff@chromium.org>
2014-12-11 21:35:13 +0000

    toolchain_build: Use 'build_noncanonical' target type for target libraries
    This CL introdudes a new build target type, 'build_noncanonical'. Previously
    we were using 'work' targets for target libraries on non-x86-64-linux bots so that
    only one bot would upload them. But since we now us package_version to extract
    the fresh packages instead of --install, we are getting stale packages from
    DEPS for the noncanonical targets rather than the freshly-built ones.
    
    build_noncanonical targets are memoized but use a different key for the
    same build signature to avoid collisions between canonical
    and non-canonical builds.
    
    However the package_version tools don't have a way to
    distinguish canonical packages (i.e. those we want to
    upload) from non-canonical ones; they only know how to process all the archives that are present. We can't just
    leave out the the non-canonical packages entirely from the
    json files because they are extracted by the package_version tool. So we do 2 invocations of
    toolchain_build_pnacl: the first builds just the canonical
    packages. We upload these, and then we do a second build
    with everything and extract that locally for testing.
    
    R=dyen@chromium.org
    
    Review URL: https://codereview.chromium.org/784283003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@14208 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: 4492d6aae94a66efb19718a1a26051128ee1c271

--- toolchain_build/toolchain_main.py
+++ toolchain_build/toolchain_main.py
@@ -84,11 +84,15 @@ class PackageBuilder(object):
                 key name>},
           },
           '<package name>': {
-            'type': 'build',
+            'type': 'build', [or 'build_noncanonical']
                 # Build packages are memoized, and will build only if their
                 # inputs have changed. Their inputs consist of the output of
                 # their package dependencies plus any file or directory inputs
                 # given by their 'inputs' member
+                # build_noncanonical packages are memoized in the same way, but
+                # their cache storage keys get the build platform name appended.
+                # This means they can be built by multiple bots without
+                # collisions, but only one will be canonical.
             'dependencies':  # optional
               [<list of package depdenencies>],
             'inputs': # optional
@@ -184,11 +188,15 @@ class PackageBuilder(object):
     if 'type' not in package_info:
       raise Exception('package %s does not have a type' % package)
     type_text = package_info['type']
-    if type_text not in ('source', 'build', 'work'):
+    if type_text not in ('source', 'build', 'build_noncanonical', 'work'):
       raise Exception('package %s has unrecognized type: %s' %
                       (package, type_text))
     is_source_target = type_text == 'source'
-    is_build_target = type_text == 'build'
+    is_build_target = type_text in ('build', 'build_noncanonical')
+    build_signature_key_extra = ''
+    if type_text == 'build_noncanonical':
+      build_signature_key_extra = '_' + pynacl.gsd_storage.LegalizeName(
+          pynacl.platform.PlatformTriple())
 
     if 'commands' not in package_info:
       raise Exception('package %s does not have any commands' % package)
@@ -202,6 +210,10 @@ class PackageBuilder(object):
     if is_source_target and not (
         self._options.sync_sources or self._options.sync_sources_only):
       logging.debug('Sync skipped: not running commands for %s' % package)
+      return
+
+    if type_text == 'build_noncanonical' and self._options.canonical_only:
+      logging.debug('Non-canonical build of %s skipped' % package)
       return
 
     pynacl.log_tools.WriteAnnotatorLine(
@@ -260,7 +272,8 @@ class PackageBuilder(object):
         working_dir=work_dir,
         memoize=is_build_target,
         signature_file=self._signature_file,
-        subdir=output_subdir)
+        subdir=output_subdir,
+        bskey_extra = build_signature_key_extra)
 
     if not is_source_target and self._options.install:
       install = pynacl.platform.CygPath(self._options.install)
@@ -444,6 +457,10 @@ class PackageBuilder(object):
         '-i', '--ignore-dependencies', dest='ignore_dependencies',
         default=False, action='store_true',
         help='Ignore target dependencies and build only the specified target.')
+    parser.add_option(
+        '--canonical-only', dest='canonical_only',
+        default=False, action='store_true',
+        help='Do not build build_noncanonical targets')
     parser.add_option('--install', dest='install',
                       help='After building, copy contents of build packages' +
                       ' to the specified directory')
