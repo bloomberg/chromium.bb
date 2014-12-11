9bb1771683a28729d8f7730b614e863ab8b11ba7
mtklein <mtklein@chromium.org>
2014-12-11 10:24:36 -0800

    Make DYNAMIC_ANNOTATIONS_ENABLED imply SK_DYNAMIC_ANNOTATIONS_ENABLED.
    This ought to make Chrome see Skia's TSAN annotations, and so clear up
    the intermittent TSAN warnings we're seeing from TRACE macros.
    
    BUG=430815
    
    Review URL: https://codereview.chromium.org/794653003
    
    Cr-Commit-Position: refs/heads/master@{#307934}
    
    Former-commit-id: c0cde4562edd192d487b19de9876bc3bcde64c26

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -273,6 +273,11 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_LEGACY_PICTURE_DRAW_API
 #endif
 
+// If this goes well, we can have Skia respect DYNAMIC_ANNOTATIONS_ENABLED directly.
+#if DYNAMIC_ANNOTATIONS_ENABLED
+#    define SK_DYNAMIC_ANNOTATIONS_ENABLED 1
+#endif
+
 // ===== End Chrome-specific definitions =====
 
 #endif
