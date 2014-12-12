4d97721d51e3209642a67ea6c6aecf4477786b72
glider <glider@chromium.org>
2014-12-12 08:03:23 -0800

    Remove exceptions for third_party/libc++/trunk/include/support/solaris and third_party/libc++/trunk/src/support/solaris/xlocale.c from the license checker.
    BUG=329819
    R=phajdan@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/764113004
    
    Cr-Commit-Position: refs/heads/master@{#308092}
    
    Former-commit-id: fa1f70ba4398e0b3f0a2e4976f872a83d83eafc6

--- tools/checklicenses/checklicenses.py
+++ tools/checklicenses/checklicenses.py
@@ -211,14 +211,6 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
     'third_party/lcov/contrib/galaxy/genflat.pl': [
         'GPL (v2 or later)',
     ],
-    'third_party/libc++/trunk/include/support/solaris': [
-        # http://llvm.org/bugs/show_bug.cgi?id=18291
-        'UNKNOWN',
-    ],
-    'third_party/libc++/trunk/src/support/solaris/xlocale.c': [
-        # http://llvm.org/bugs/show_bug.cgi?id=18291
-        'UNKNOWN',
-    ],
     'third_party/libc++/trunk/test': [
         # http://llvm.org/bugs/show_bug.cgi?id=18291
         'UNKNOWN',
