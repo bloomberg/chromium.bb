51777f7b86e62ac44ecf2cb59e3fb50b139e1f8e
sigbjornf@opera.com <sigbjornf@opera.com>
2014-12-12 18:52:22 +0000

    Oilpan: fix build after r187036.
    TBR=oilpan-reviews,haraken
    BUG=437846
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/799723004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187044 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 60af1aa54a1e7b87efeefacdd3ef6395943766e8

--- third_party/WebKit/Source/core/css/RuleSetTest.cpp
+++ third_party/WebKit/Source/core/css/RuleSetTest.cpp
@@ -142,7 +142,7 @@ TEST(RuleSetTest, findBestRuleSetAndAdd_Host)
 
     helper.addCSSRules(":host { }");
     RuleSet& ruleSet = helper.ruleSet();
-    const Vector<RuleData>* rules = ruleSet.shadowHostRules();
+    const WillBeHeapVector<RuleData>* rules = ruleSet.shadowHostRules();
     ASSERT_EQ(1u, rules->size());
 }
 
@@ -152,7 +152,7 @@ TEST(RuleSetTest, findBestRuleSetAndAdd_HostWithId)
 
     helper.addCSSRules(":host(#x) { }");
     RuleSet& ruleSet = helper.ruleSet();
-    const Vector<RuleData>* rules = ruleSet.shadowHostRules();
+    const WillBeHeapVector<RuleData>* rules = ruleSet.shadowHostRules();
     ASSERT_EQ(1u, rules->size());
 }
 
@@ -162,7 +162,7 @@ TEST(RuleSetTest, findBestRuleSetAndAdd_HostContext)
 
     helper.addCSSRules(":host-context(*) { }");
     RuleSet& ruleSet = helper.ruleSet();
-    const Vector<RuleData>* rules = ruleSet.shadowHostRules();
+    const WillBeHeapVector<RuleData>* rules = ruleSet.shadowHostRules();
     ASSERT_EQ(1u, rules->size());
 }
 
@@ -172,7 +172,7 @@ TEST(RuleSetTest, findBestRuleSetAndAdd_HostContextWithId)
 
     helper.addCSSRules(":host-context(#x) { }");
     RuleSet& ruleSet = helper.ruleSet();
-    const Vector<RuleData>* rules = ruleSet.shadowHostRules();
+    const WillBeHeapVector<RuleData>* rules = ruleSet.shadowHostRules();
     ASSERT_EQ(1u, rules->size());
 }
 
@@ -182,7 +182,7 @@ TEST(RuleSetTest, findBestRuleSetAndAdd_HostAndHostContextNotInRightmost)
 
     helper.addCSSRules(":host-context(#x) .y, :host(.a) > #b  { }");
     RuleSet& ruleSet = helper.ruleSet();
-    const Vector<RuleData>* shadowRules = ruleSet.shadowHostRules();
+    const WillBeHeapVector<RuleData>* shadowRules = ruleSet.shadowHostRules();
     const TerminatedArray<RuleData>* idRules = ruleSet.idRules("b");
     const TerminatedArray<RuleData>* classRules = ruleSet.classRules("y");
     ASSERT_EQ(0u, shadowRules->size());
@@ -196,7 +196,7 @@ TEST(RuleSetTest, findBestRuleSetAndAdd_HostAndClass)
 
     helper.addCSSRules(".foo:host { }");
     RuleSet& ruleSet = helper.ruleSet();
-    const Vector<RuleData>* rules = ruleSet.shadowHostRules();
+    const WillBeHeapVector<RuleData>* rules = ruleSet.shadowHostRules();
     ASSERT_EQ(0u, rules->size());
 }
 
@@ -206,7 +206,7 @@ TEST(RuleSetTest, findBestRuleSetAndAdd_HostContextAndClass)
 
     helper.addCSSRules(".foo:host-context(*) { }");
     RuleSet& ruleSet = helper.ruleSet();
-    const Vector<RuleData>* rules = ruleSet.shadowHostRules();
+    const WillBeHeapVector<RuleData>* rules = ruleSet.shadowHostRules();
     ASSERT_EQ(0u, rules->size());
 }
 
