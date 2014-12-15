a3190adf3b6b8e82210b798c748d30ae5757ce28
David James <davidjames@google.com>
2014-12-15 11:26:46 -0800

    Clear old binhosts.
    Currently as builders are added and removed from paladin / pfq, the
    binhost files stick around forever, and this can cause all kinds of
    breakage due to stale binaries. Fix this.
    
    BUG=chromium:442276
    TEST=local run of script to confirm it commits right files
    TEST=unit tests
    
    Change-Id: I1d313d20600af4778877afdccb90d2825dd838ac
    Reviewed-on: https://chromium-review.googlesource.com/235803
    Reviewed-by: David James <davidjames@chromium.org>
    Commit-Queue: David James <davidjames@chromium.org>
    Tested-by: David James <davidjames@chromium.org>

--- scripts/upload_prebuilts_unittest.py
+++ scripts/upload_prebuilts_unittest.py
@@ -462,7 +462,7 @@ class TestMain(cros_test_lib.MoxTestCase):
     prebuilt.PrebuiltUploader.__init__(options.upload, expected_gs_acl_path,
                                        options.upload, mox.IgnoreArg(),
                                        options.build_path, options.packages,
-                                       False, options.binhost_conf_dir, False,
+                                       False, '/trunk/foo', False,
                                        target, options.slave_targets,
                                        mox.IgnoreArg())
     self.mox.StubOutWithMock(prebuilt.PrebuiltUploader, 'SyncHostPrebuilts')
