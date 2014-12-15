003c1ebf55234762170ae485bdd66b8757482d1f
Bertrand Simonnet <bsimonnet@chromium.org>
2014-12-15 23:19:35 +0000

    cbuildbot_config: enable separatedebug for gizmo
    BUG=chromium:437964
    TEST=trybot run on gizmo-paladin
    
    Change-Id: I2c93093bd3172104ebd8addf1fd9b8c107374089
    Reviewed-on: https://chromium-review.googlesource.com/235817
    Trybot-Ready: Bertrand Simonnet <bsimonnet@chromium.org>
    Tested-by: Bertrand Simonnet <bsimonnet@chromium.org>
    Reviewed-by: David James <davidjames@chromium.org>
    Commit-Queue: Bertrand Simonnet <bsimonnet@chromium.org>

--- cbuildbot/cbuildbot_config.py
+++ cbuildbot/cbuildbot_config.py
@@ -2030,6 +2030,10 @@ _paladin_full_boards = frozenset([
   'nyan',
 ])
 
+_paladin_separate_symbols = frozenset([
+  'gizmo',
+])
+
 def _CreatePaladinConfigs():
   for board in _paladin_boards:
     assert board in _base_configs, '%s not in _base_configs' % board
@@ -2049,6 +2053,8 @@ def _CreatePaladinConfigs():
       customizations = customizations.derive(
           internal, official_chrome,
           manifest=constants.OFFICIAL_MANIFEST)
+    if board in _paladin_separate_symbols:
+      customizations.update(separate_debug_symbols=True)
 
     if board not in _paladin_default_vmtest_boards:
       vm_tests = []
