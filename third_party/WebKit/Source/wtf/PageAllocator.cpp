ffbc7289fbcf8e818f076d5737d7c3f3844cf844
hiroshige@chromium.org <hiroshige@chromium.org>
2014-12-15 13:03:50 +0000

    Revert of Re-land: 178586 Avoid fragmenting address space on small windows systems. (patchset #3 id:40001 of https://codereview.chromium.org/717263005/)
    Reason for revert:
    This CL crashed all layout tests on XP bot.
    
    Original issue's description:
    > Re-land: 178586 Avoid fragmenting address space on small windows systems.
    > 
    > The concern about IsWow64Process() is not founded per jschuh@ (but the effect of
    > changing the allocator behaviour may be). We now have a clean baseline to test
    > against.
    > 
    > R=cevans@chromium.org
    > BUG=394591
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=185559
    
    TBR=cevans@chromium.org,tsepez@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=394591
    
    Review URL: https://codereview.chromium.org/806683003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187141 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 67943b69f558efa0acbb2c55b87a92dab764a5dd

--- third_party/WebKit/Source/wtf/PageAllocator.cpp
+++ third_party/WebKit/Source/wtf/PageAllocator.cpp
@@ -69,15 +69,8 @@ static bool shouldUseAddressHint()
     // is running under WOW64, then it has at least 3 GB available (and likely
     // 4 GB depending upon the OS version), and we want use the additional
     // randomness.
-    static BOOL bIsWow64 = -1;
-    if (bIsWow64 == -1) {
-        BOOL result = IsWow64Process(GetCurrentProcess(), &bIsWow64);
-        if (!result) {
-            ASSERT_NOT_REACHED();
-            bIsWow64 = 0;
-        }
-    }
-    return !!bIsWow64;
+    // TODO(tsepez): presently disabled due to IsWow64Process() compatibility.
+    return true;
 #else // CPU(32BIT)
     return true;
 #endif // CPU(32BIT)
