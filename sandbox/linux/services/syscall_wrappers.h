efb53bface5486793b57b79381e7790acdb5ae5c
rickyz <rickyz@chromium.org>
2014-12-15 18:40:18 -0800

    Use the libc clone wrapper in sys_clone.
    Previously, we directly invoked the syscall, which would not update
    libc's PID cache in the child. Although the libc wrapper function
    updates the PID cache, it unfortunately requires that the child run on a
    different stack, even if CLONE_VM is not specified. We work around this
    by briefly switching stacks in the child, then using longjmp to switch
    back. This gives us a version of clone with fork-like behavior, which is
    what we need for starting processes in new namespaces.
    
    BUG=312380
    
    Review URL: https://codereview.chromium.org/801033002
    
    Cr-Commit-Position: refs/heads/master@{#308510}
    
    Former-commit-id: d8a593bceaf0a4b38d06a8c13b948202d205f1b6

--- sandbox/linux/services/syscall_wrappers.h
+++ sandbox/linux/services/syscall_wrappers.h
@@ -16,7 +16,7 @@ namespace sandbox {
 
 // Provide direct system call wrappers for a few common system calls.
 // These are guaranteed to perform a system call and do not rely on things such
-// as caching the current pid (c.f. getpid()).
+// as caching the current pid (c.f. getpid()) unless otherwise specified.
 
 SANDBOX_EXPORT pid_t sys_getpid(void);
 
@@ -24,9 +24,15 @@ SANDBOX_EXPORT pid_t sys_gettid(void);
 
 SANDBOX_EXPORT long sys_clone(unsigned long flags);
 
-// |regs| is not supported and must be passed as nullptr.
+// |regs| is not supported and must be passed as nullptr. |child_stack| must be
+// nullptr, since otherwise this function cannot safely return.  As a
+// consequence, this function does not support CLONE_VM.
+//
+// This function uses the libc clone wrapper (which updates libc's pid cache)
+// internally, so callers may expect things like getpid() to work correctly
+// after in both the child and parent.
 SANDBOX_EXPORT long sys_clone(unsigned long flags,
-                              void* child_stack,
+                              decltype(nullptr) child_stack,
                               pid_t* ptid,
                               pid_t* ctid,
                               decltype(nullptr) regs);
