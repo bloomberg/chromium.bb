9d09416aaed31044bafe70e543ca9d3e6e4a0f18
shend@chromium.org <shend@chromium.org>
2014-12-16 05:57:11 +0000

    Correctly handle comma separated values in CSSKeyframesRule::findRule.
    This patch makes findRule case-insensitive and also correctly resolves
    'from' and 'to' when the search key is a comma separated list.
    
    BUG=442165
    
    Review URL: https://codereview.chromium.org/807743002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187225 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0718d4fadded2d8e6b77a7238c7fa881344c4dcc

--- third_party/WebKit/Source/core/css/CSSKeyframesRule.cpp
+++ third_party/WebKit/Source/core/css/CSSKeyframesRule.cpp
@@ -71,16 +71,9 @@ void StyleRuleKeyframes::wrapperRemoveKeyframe(unsigned index)
 
 int StyleRuleKeyframes::findKeyframeIndex(const String& key) const
 {
-    String percentageString;
-    if (equalIgnoringCase(key, "from"))
-        percentageString = "0%";
-    else if (equalIgnoringCase(key, "to"))
-        percentageString = "100%";
-    else
-        percentageString = key;
-
+    Vector<double> keys = *CSSParser::parseKeyframeKeyList(key);
     for (size_t i = m_keyframes.size(); i--; ) {
-        if (m_keyframes[i]->keyText() == percentageString)
+        if (m_keyframes[i]->keys() == keys)
             return i;
     }
     return -1;
