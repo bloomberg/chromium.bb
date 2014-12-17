a95f19cfacd13f5211a4636b92354967d3096370
glider <glider@chromium.org>
2014-12-17 00:03:28 -0800

    Update the upstream version of asan_symbolize.py to r222535, which introduces support for the --dsym-hint option.
    Add a dSYM hint producer to tools/valgrind/asan/asan_symbolize.py,
    which builds paths to .dSYM files for executables and frameworks inside the
    .app bundles, assuming the dSYM files are in the product dir.
    
    BUG=148383, 242503, 170739, 166857
    R=earthdok@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/797253002
    
    Cr-Commit-Position: refs/heads/master@{#308758}
    
    Former-commit-id: 7e52eba6cc9095bb535a2b8c85062e7f4262c48a

--- tools/valgrind/asan/asan_symbolize.py
+++ tools/valgrind/asan/asan_symbolize.py
@@ -45,13 +45,60 @@ def set_symbolizer_path():
     os.environ['LLVM_SYMBOLIZER_PATH'] = os.path.abspath(symbolizer_path)
 
 
+# Construct a path to the .dSYM bundle for the given binary.
+# There are three possible cases for binary location in Chromium:
+# 1. The binary is a standalone executable or dynamic library in the product
+#    dir, the debug info is in "binary.dSYM" in the product dir.
+# 2. The binary is a standalone framework or .app bundle, the debug info is in
+#    "Framework.framework.dSYM" or "App.app.dSYM" in the product dir.
+# 3. The binary is a framework or an .app bundle within another .app bundle
+#    (e.g. Outer.app/Contents/Versions/1.2.3.4/Inner.app), and the debug info
+#    is in Inner.app.dSYM in the product dir.
+# The first case is handled by llvm-symbolizer, so we only need to construct
+# .dSYM paths for .app bundles and frameworks.
+# We're assuming that there're no more than two nested bundles in the binary
+# path. Only one of these bundles may be a framework and frameworks cannot
+# contain other bundles.
+def chrome_dsym_hints(binary):
+  path_parts = binary.split(os.path.sep)
+  app_positions = []
+  framework_positions = []
+  for index, part in enumerate(path_parts):
+    if part.endswith('.app'):
+      app_positions.append(index)
+    elif part.endswith('.framework'):
+      framework_positions.append(index)
+  assert len(framework_positions) <= 1, \
+      "The path contains more than one framework: %s" % binary
+  bundle_positions = app_positions + framework_positions
+  bundle_positions.sort()
+  assert len(bundle_positions) <= 2, \
+      "The path contains more than two nested bundles: %s" % binary
+  if len(bundle_positions) == 0:
+    # Case 1: this is a standalone executable or dylib.
+    return []
+  assert (len(bundle_positions) == 1 or
+          len(app_positions) == 2 or
+          app_positions[0] < framework_positions[0]), \
+      "The path contains an app bundle inside a framework: %s" % binary
+  # Cases 2 and 3. The outermost bundle (which is the only bundle in the case 2)
+  # is located in the product dir.
+  outermost_bundle = bundle_positions[0]
+  product_dir = path_parts[:outermost_bundle]
+  # In case 2 this is the same as |outermost_bundle|.
+  innermost_bundle = bundle_positions[-1]
+  dsym_path = product_dir + [path_parts[innermost_bundle]]
+  result = '%s.dSYM' % os.path.sep.join(dsym_path)
+  return [result]
+
+
 def main():
   disable_buffering()
   set_symbolizer_path()
   asan_symbolize.demangle = True
   asan_symbolize.fix_filename_patterns = sys.argv[1:]
   asan_symbolize.logfile = sys.stdin
-  loop = asan_symbolize.SymbolizationLoop()
+  loop = asan_symbolize.SymbolizationLoop(dsym_hint_producer=chrome_dsym_hints)
   loop.process_logfile()
 
 if __name__ == '__main__':
