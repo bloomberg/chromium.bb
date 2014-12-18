9ce2ced5870d3076914ca159a84187eececd18eb
reed <reed@chromium.org>
2014-12-18 08:19:54 -0800

    remove unneeded flag SK_SUPPORT_LEGACY_TEXTRENDERMODE
    BUG=
    TBR=robertphilips
    
    Review URL: https://codereview.chromium.org/792013003
    
    Cr-Commit-Position: refs/heads/master@{#309018}
    
    Former-commit-id: 1f98d1856384ba96ca56238ef3c4aaeaf68dfa42

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -265,10 +265,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_IGNORE_GPU_DITHER
 #endif
 
-#ifndef SK_SUPPORT_LEGACY_TEXTRENDERMODE
-#   define SK_SUPPORT_LEGACY_TEXTRENDERMODE
-#endif
-
 #ifndef SK_IGNORE_GPU_LAYER_HOISTING
 #   define SK_IGNORE_GPU_LAYER_HOISTING
 #endif
