3252b576f0300cc2adf3c5ea92060d683a97d909
tedchoc <tedchoc@chromium.org>
2014-12-17 18:03:15 -0800

    [Android] Sanitze referrer for context menu downloads.
    BUG=435479
    
    Review URL: https://codereview.chromium.org/787163003
    
    Cr-Commit-Position: refs/heads/master@{#308926}
    
    Former-commit-id: 932d2284ca890b3d9b0cb69dec14d924879de7c5

--- content/browser/android/download_controller_android_impl.cc
+++ content/browser/android/download_controller_android_impl.cc
@@ -371,14 +371,17 @@ DownloadControllerAndroidImpl::JavaObject*
 void DownloadControllerAndroidImpl::StartContextMenuDownload(
     const ContextMenuParams& params, WebContents* web_contents, bool is_link) {
   const GURL& url = is_link ? params.link_url : params.src_url;
-  const GURL& referrer = params.frame_url.is_empty() ?
+  const GURL& referring_url = params.frame_url.is_empty() ?
       params.page_url : params.frame_url;
   DownloadManagerImpl* dlm = static_cast<DownloadManagerImpl*>(
       BrowserContext::GetDownloadManager(web_contents->GetBrowserContext()));
   scoped_ptr<DownloadUrlParameters> dl_params(
       DownloadUrlParameters::FromWebContents(web_contents, url));
-  dl_params->set_referrer(
-      Referrer(referrer, params.referrer_policy));
+  content::Referrer referrer = content::Referrer::SanitizeForRequest(
+      url,
+      content::Referrer(referring_url.GetAsReferrer(),
+                        params.referrer_policy));
+  dl_params->set_referrer(referrer);
   if (is_link)
     dl_params->set_referrer_encoding(params.frame_charset);
   else
