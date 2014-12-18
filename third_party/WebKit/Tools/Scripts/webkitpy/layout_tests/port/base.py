00df497811086672d57d90c486cf4e437810843a
enne@chromium.org <enne@chromium.org>
2014-12-18 22:11:50 +0000

    Impl-side painting for scrollbars/ and prefer_compositing layout tests
    BUG=381919
    
    Review URL: https://codereview.chromium.org/805253003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187492 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: a73076089187b2cec0749cddf36d75da0a8ea8bd

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -1562,6 +1562,7 @@ class Port(object):
             PhysicalTestSuite('fast', ['--enable-impl-side-painting']),
             PhysicalTestSuite('ietestcenter', ['--enable-impl-side-painting']),
             PhysicalTestSuite('media', ['--enable-impl-side-painting']),
+            PhysicalTestSuite('scrollbars', ['--enable-impl-side-painting']),
             PhysicalTestSuite('svg', ['--enable-impl-side-painting']),
             PhysicalTestSuite('tables', ['--enable-impl-side-painting']),
             PhysicalTestSuite('transforms', ['--enable-impl-side-painting']),
