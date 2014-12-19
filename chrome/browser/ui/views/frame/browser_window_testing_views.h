5c3b428b0a2429210ecb6150bfe628669322bb2d
tfarina <tfarina@chromium.org>
2014-12-18 16:59:34 -0800

    Cleanup: Move browser_window_testing_views.h into c/b/ui/views/ directory.
    This interface is views specific, so it shouldn't be in c/b/ui/
    directory, which is the place from cross-platform interfaces. And it is
    only used by c/b/ui/views test files.
    
    BUG=None
    TEST=browser_tests
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/811593006
    
    Cr-Commit-Position: refs/heads/master@{#309121}
    
    Former-commit-id: 8451cc1a50896771d5819cc9fe563a2b8cf2ca45

--- chrome/browser/ui/views/frame/browser_window_testing_views.h
+++ chrome/browser/ui/views/frame/browser_window_testing_views.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_BROWSER_WINDOW_TESTING_VIEWS_H_
-#define CHROME_BROWSER_UI_BROWSER_WINDOW_TESTING_VIEWS_H_
+#ifndef CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_WINDOW_TESTING_VIEWS_H_
+#define CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_WINDOW_TESTING_VIEWS_H_
 
 class BookmarkBarView;
 class LocationBarView;
@@ -33,4 +33,4 @@ class BrowserWindowTesting {
   virtual ~BrowserWindowTesting() {}
 };
 
-#endif  // CHROME_BROWSER_UI_BROWSER_WINDOW_TESTING_VIEWS_H_
+#endif  // CHROME_BROWSER_UI_VIEWS_FRAME_BROWSER_WINDOW_TESTING_VIEWS_H_
