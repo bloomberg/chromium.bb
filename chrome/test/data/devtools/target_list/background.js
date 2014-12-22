0185deedc3f6edcd9950e71ffb4dea3fdd32585d
vkuzkokov <vkuzkokov@chromium.org>
2014-12-22 08:18:33 -0800

    [DevTools] Fix RemoteDebuggingTest.RemoteDebugger
    It has been flakily failing on ASAN builds for a while now.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/824693003
    
    Cr-Commit-Position: refs/heads/master@{#309430}
    
    Former-commit-id: f8b4cd5a2d74644cf24061bbbfdc3f635e27df7a

--- chrome/test/data/devtools/target_list/background.js
+++ chrome/test/data/devtools/target_list/background.js
@@ -124,33 +124,30 @@ chrome.test.runTests([
   function discoverTargets() {
     var testPageUrl = chrome.extension.getURL('test_page.html');
 
-    function onUpdated(updatedTabId) {
-      requestUrl('/json', function(text) {
-        var targets = JSON.parse(text);
-        waitForTab(
-            function(tab) {
-              return tab.id == updatedTabId && tab.status == "complete"
-            },
-            function() {
-              checkTarget(targets, 'about:blank', 'page');
-              checkTarget(targets,
-                  chrome.extension.getURL('_generated_background_page.html'),
-                  'background_page',
-                  'Remote Debugger Test');
-              var target = checkTarget(targets,
-                  testPageUrl, 'page', 'Test page',
-                  chrome.extension.getURL('favicon.png'));
-
-              extensionTargetId = target.id;
-              extensionDevtoolsFrontendUrl = target.devtoolsFrontendUrl;
-              extensionWebSocketDebuggerUrl = target.webSocketDebuggerUrl;
-
-              chrome.test.succeed();
-            });
-        });
-    }
-    listenOnce(chrome.tabs.onUpdated, onUpdated);
     chrome.tabs.create({url: testPageUrl});
+    waitForTab(
+        function(tab) {
+          return tab.url == testPageUrl && tab.status == "complete";
+        },
+        function() {
+          requestUrl('/json', function(text) {
+            var targets = JSON.parse(text);
+            checkTarget(targets, 'about:blank', 'page');
+            checkTarget(targets,
+                chrome.extension.getURL('_generated_background_page.html'),
+                'background_page',
+                'Remote Debugger Test');
+            var target = checkTarget(targets,
+                testPageUrl, 'page', 'Test page',
+                chrome.extension.getURL('favicon.png'));
+
+            extensionTargetId = target.id;
+            extensionDevtoolsFrontendUrl = target.devtoolsFrontendUrl;
+            extensionWebSocketDebuggerUrl = target.webSocketDebuggerUrl;
+
+            chrome.test.succeed();
+          });
+        });
   },
 
   function versionInfo() {
