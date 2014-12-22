0460d8eff4d93751de085a128e900ce91175923c
vadimt <vadimt@chromium.org>
2014-12-22 12:31:56 -0800

    Instrumenting WindowImpl::WndProc to find jank Prior instrumentations didn't catch the jank, i.e. most jank is inside "440919 <<MessagePumpForUI::ProcessNextWindowsMessage>>", but outside of other instrumentations. This means that the places I suspected are sources of jank actually aren't.
    Widening the search.
    
    BUG=440919
    
    Review URL: https://codereview.chromium.org/816113003
    
    Cr-Commit-Position: refs/heads/master@{#309457}
    
    Former-commit-id: fa84141823aeaad1c993d6eb24975df6fdc8017f

--- ui/gfx/win/window_impl.cc
+++ ui/gfx/win/window_impl.cc
@@ -9,6 +9,7 @@
 #include "base/bind.h"
 #include "base/debug/alias.h"
 #include "base/memory/singleton.h"
+#include "base/profiler/scoped_tracker.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/synchronization/lock.h"
 #include "base/win/wrapped_window_proc.h"
@@ -282,6 +283,10 @@ LRESULT CALLBACK WindowImpl::WndProc(HWND hwnd,
                                      WPARAM w_param,
                                      LPARAM l_param) {
   if (message == WM_NCCREATE) {
+    // TODO(vadimt): Remove ScopedTracker below once crbug.com/440919 is fixed.
+    tracked_objects::ScopedTracker tracking_profile1(
+        FROM_HERE_WITH_EXPLICIT_FUNCTION("440919 WindowImpl::WndProc1"));
+
     CREATESTRUCT* cs = reinterpret_cast<CREATESTRUCT*>(l_param);
     WindowImpl* window = reinterpret_cast<WindowImpl*>(cs->lpCreateParams);
     DCHECK(window);
@@ -292,6 +297,10 @@ LRESULT CALLBACK WindowImpl::WndProc(HWND hwnd,
       window->got_valid_hwnd_ = true;
     return TRUE;
   }
+
+  // TODO(vadimt): Remove ScopedTracker below once crbug.com/440919 is fixed.
+  tracked_objects::ScopedTracker tracking_profile2(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION("440919 WindowImpl::WndProc2"));
 
   WindowImpl* window = reinterpret_cast<WindowImpl*>(GetWindowUserData(hwnd));
   if (!window)
