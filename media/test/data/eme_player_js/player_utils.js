08fd28668c1e08a7e8cf500aec56289978ea8df7
jrummell <jrummell@chromium.org>
2014-12-23 10:57:27 -0800

    Fix Flaky EncryptedMediaTest
    Tests are flaky as setMediaKeys() was not completing before the
    "encrypted" event was generated. Change delays setting the video
    source until setMediaKeys() completes.
    
    BUG=444683
    TEST=Failing tests run fine locally
    
    Review URL: https://codereview.chromium.org/823743002
    
    Cr-Commit-Position: refs/heads/master@{#309568}
    
    Former-commit-id: 37edfbac393c04549884ec15ce612a376a5be6ff

--- media/test/data/eme_player_js/player_utils.js
+++ media/test/data/eme_player_js/player_utils.js
@@ -80,19 +80,21 @@ PlayerUtils.registerEMEEventListeners = function(player) {
   try {
     Utils.timeLog('Setting video media keys: ' + player.testConfig.keySystem);
     if (typeof navigator.requestMediaKeySystemAccess == 'function') {
-      navigator.requestMediaKeySystemAccess(player.testConfig.keySystem)
+      return navigator.requestMediaKeySystemAccess(player.testConfig.keySystem)
           .then(function(access) { return access.createMediaKeys(); })
           .then(function(mediaKeys) { player.video.setMediaKeys(mediaKeys); })
           .catch(function(error) { Utils.failTest(error, NOTSUPPORTEDERROR); });
     } else {
       // TODO(jrummell): Remove this once the blink change for
       // requestMediaKeySystemAccess lands.
-      MediaKeys.create(player.testConfig.keySystem)
+      return MediaKeys.create(player.testConfig.keySystem)
           .then(function(mediaKeys) { player.video.setMediaKeys(mediaKeys); })
           .catch(function(error) { Utils.failTest(error, NOTSUPPORTEDERROR); });
     }
   } catch (e) {
     Utils.failTest(e);
+    // Return a failing promise.
+    return new Promise(function(resolve, reject) { reject(Error(e)); });
   }
 };
 
@@ -149,8 +151,9 @@ PlayerUtils.setVideoSource = function(player) {
 };
 
 PlayerUtils.initEMEPlayer = function(player) {
-  this.registerEMEEventListeners(player);
-  this.setVideoSource(player);
+  var promise = this.registerEMEEventListeners(player);
+  promise.then(function(result) { PlayerUtils.setVideoSource(player); })
+      .catch(function(error) { Utils.failTest(error, NOTSUPPORTEDERROR); });
 };
 
 PlayerUtils.initPrefixedEMEPlayer = function(player) {
