cd165d3da84230c036a28ff6c69024a32e576425
merkulova <merkulova@chromium.org>
2014-12-23 04:39:15 -0800

    Special check on Supervised User creation flow added to prevent misleading shelf alignment.
    BUG=405361
    
    Review URL: https://codereview.chromium.org/783663003
    
    Cr-Commit-Position: refs/heads/master@{#309555}
    
    Former-commit-id: e2b59c4c229ec214150f522f4b3f6b02d9cefaee

--- ash/shelf/shelf_layout_manager.cc
+++ ash/shelf/shelf_layout_manager.cc
@@ -252,7 +252,8 @@ bool ShelfLayoutManager::SetAlignment(ShelfAlignment alignment) {
     return false;
 
   alignment_ = alignment;
-  if (state_.is_screen_locked || state_.is_adding_user_screen) {
+  if (Shell::GetInstance()->session_state_delegate()->IsUserSessionBlocked() ||
+      state_.is_adding_user_screen) {
     // The shelf will itself move to the bottom while locked. If a request is
     // sent to move while being locked, we postpone the move until the lock
     // screen goes away.
@@ -270,7 +271,8 @@ ShelfAlignment ShelfLayoutManager::GetAlignment() const {
   // bottom alignment. Note: We cannot use state_.is_screen_locked here since
   // that flag gets set later than the SessionStateDelegate reports a locked
   // screen which leads in
-  if (state_.is_screen_locked || state_.is_adding_user_screen)
+  if (Shell::GetInstance()->session_state_delegate()->IsUserSessionBlocked() ||
+      state_.is_adding_user_screen)
     return SHELF_ALIGNMENT_BOTTOM;
   return alignment_;
 }
