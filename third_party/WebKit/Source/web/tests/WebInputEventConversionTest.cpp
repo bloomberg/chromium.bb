81be71032c7509f51ef5cd1224ba35d32bff9a46
ccameron@chromium.org <ccameron@chromium.org>
2015-01-02 21:06:13 +0000

    Use a separate page for all WebInputEventConversionTest instances
    This may be causing crashes on Android bots
    
    BUG=
    
    Review URL: https://codereview.chromium.org/828303003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187821 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 936cac04a9994dc47f675ce5cf2974d529bcd4ab

--- third_party/WebKit/Source/web/tests/WebInputEventConversionTest.cpp
+++ third_party/WebKit/Source/web/tests/WebInputEventConversionTest.cpp
@@ -216,7 +216,7 @@ TEST(WebInputEventConversionTest, WebTouchEventBuilder)
 
 TEST(WebInputEventConversionTest, InputEventsScaling)
 {
-    const std::string baseURL("http://www.test.com/");
+    const std::string baseURL("http://www.test1.com/");
     const std::string fileName("fixed_layout.html");
 
     URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(baseURL.c_str()), WebString::fromUTF8("fixed_layout.html"));
@@ -731,7 +731,7 @@ TEST(WebInputEventConversionTest, PinchViewportOffset)
 
 TEST(WebInputEventConversionTest, ElasticOverscroll)
 {
-    const std::string baseURL("http://www.test4.com/");
+    const std::string baseURL("http://www.test5.com/");
     const std::string fileName("fixed_layout.html");
 
     URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(baseURL.c_str()), WebString::fromUTF8("fixed_layout.html"));
@@ -792,7 +792,7 @@ TEST(WebInputEventConversionTest, ElasticOverscroll)
 
 TEST(WebInputEventConversionTest, WebMouseWheelEventBuilder)
 {
-    const std::string baseURL("http://www.test5.com/");
+    const std::string baseURL("http://www.test6.com/");
     const std::string fileName("fixed_layout.html");
 
     URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(baseURL.c_str()), WebString::fromUTF8("fixed_layout.html"));
@@ -823,7 +823,7 @@ TEST(WebInputEventConversionTest, WebMouseWheelEventBuilder)
 
 TEST(WebInputEventConversionTest, PlatformWheelEventBuilder)
 {
-    const std::string baseURL("http://www.test6.com/");
+    const std::string baseURL("http://www.test7.com/");
     const std::string fileName("fixed_layout.html");
 
     URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(baseURL.c_str()), WebString::fromUTF8("fixed_layout.html"));
