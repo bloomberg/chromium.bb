1589e153be3f35d30df942d249b50513549c8113
sigbjornf@opera.com <sigbjornf@opera.com>
2015-01-02 18:42:49 +0000

    Do not create a temporary Range object on updating spell checking markers.
    As these markers are conditionally updated on every edit operation,
    avoid creating a temporary Range object when doing so. With Oilpan
    enabled, this avoids creating a GCed object that is only delay'edly
    released.
    
    Improves Oilpan performance on blink_perf.dom.textarea-edit by 10%.
    
    R=haraken
    BUG=388681
    
    Review URL: https://codereview.chromium.org/828293002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@187820 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 9938f3a029645174215220196e98438cd3bf656a

--- third_party/WebKit/Source/core/editing/SpellChecker.cpp
+++ third_party/WebKit/Source/core/editing/SpellChecker.cpp
@@ -733,8 +733,10 @@ void SpellChecker::updateMarkersForWordsAffectedByEditing(bool doNotRemoveIfSele
     // of marker that contains the word in question, and remove marker on that whole range.
     Document* document = frame().document();
     ASSERT(document);
-    RefPtrWillBeRawPtr<Range> wordRange = Range::create(*document, startOfFirstWord.deepEquivalent(), endOfLastWord.deepEquivalent());
-    document->markers().removeMarkers(wordRange.get(), DocumentMarker::MisspellingMarkers(), DocumentMarkerController::RemovePartiallyOverlappingMarker);
+    Node* startNode = startOfFirstWord.deepEquivalent().containerNode();
+    int startOffset = startOfFirstWord.deepEquivalent().computeOffsetInContainerNode();
+    int endOffset = endOfLastWord.deepEquivalent().computeOffsetInContainerNode();
+    document->markers().removeMarkers(startNode, startOffset, endOffset - startOffset, DocumentMarker::MisspellingMarkers(), DocumentMarkerController::RemovePartiallyOverlappingMarker);
 }
 
 void SpellChecker::didEndEditingOnTextField(Element* e)
