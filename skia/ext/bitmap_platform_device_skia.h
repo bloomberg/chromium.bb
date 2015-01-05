649c99578f5efabb811f98eefb2347bfe5cba06c
gunsch <gunsch@chromium.org>
2015-01-05 14:59:50 -0800

    virtual/override specifier cleanup in skia/ext/.
    These are changes detected by Chromecast not covered by the Linux build.
    
    See: https://codereview.chromium.org/831863003
    
    R=thakis@chromium.org
    BUG=417463
    
    Review URL: https://codereview.chromium.org/836723004
    
    Cr-Commit-Position: refs/heads/master@{#309984}
    
    Former-commit-id: ffedd1e87bcfca0c50c75f8ae776a071f3075634

--- skia/ext/bitmap_platform_device_skia.h
+++ skia/ext/bitmap_platform_device_skia.h
@@ -35,13 +35,12 @@ class BitmapPlatformDevice : public SkBitmapDevice, public PlatformDevice {
   // we ever have to share state between some native drawing UI and Skia, like
   // the Windows and Mac versions of this class do.
   explicit BitmapPlatformDevice(const SkBitmap& other);
-  virtual ~BitmapPlatformDevice();
+  ~BitmapPlatformDevice() override;
 
-  virtual PlatformSurface BeginPlatformPaint() override;
+  PlatformSurface BeginPlatformPaint() override;
 
  protected:
-  virtual SkBaseDevice* onCreateCompatibleDevice(const CreateInfo& info)
-    override;
+  SkBaseDevice* onCreateCompatibleDevice(const CreateInfo& info) override;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(BitmapPlatformDevice);
