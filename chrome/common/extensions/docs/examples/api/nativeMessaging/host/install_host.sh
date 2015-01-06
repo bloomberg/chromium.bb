55573c00f2bdd34195e86a6586779d8b5e46ab3e
PatrickKettner <PatrickKettner@gmail.com>
2015-01-05 16:53:01 -0800

    Fix string escaping on OS X paths
    Currently, unless you install as root, "Application Support" exists in the path, and the space borks everything up.
    
    Review URL: https://codereview.chromium.org/569083002
    
    Cr-Commit-Position: refs/heads/master@{#310013}
    
    Former-commit-id: 8249145b13b4893f0de507cbe26de72dd7ffbd79

--- chrome/common/extensions/docs/examples/api/nativeMessaging/host/install_host.sh
+++ chrome/common/extensions/docs/examples/api/nativeMessaging/host/install_host.sh
@@ -10,8 +10,8 @@ if [ $(uname -s) == 'Darwin' ]; then
   if [ "$(whoami)" == "root" ]; then
     TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
   else
-    TARGET_DIR=\
-        "$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+    TARGET_DIR="
+        $HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
   fi
 else
   if [ "$(whoami)" == "root" ]; then
@@ -24,17 +24,17 @@ fi
 HOST_NAME=com.google.chrome.example.echo
 
 # Create directory to store native messaging host.
-mkdir -p $TARGET_DIR
+mkdir -p "$TARGET_DIR"
 
 # Copy native messaging host manifest.
-cp $DIR/$HOST_NAME.json $TARGET_DIR
+cp $DIR/$HOST_NAME.json "$TARGET_DIR"
 
 # Update host path in the manifest.
 HOST_PATH=$DIR/native-messaging-example-host
 ESCAPED_HOST_PATH=${HOST_PATH////\\/}
-sed -i -e "s/HOST_PATH/$ESCAPED_HOST_PATH/" $TARGET_DIR/$HOST_NAME.json
+sed -i -e "s/HOST_PATH/$ESCAPED_HOST_PATH/" "$TARGET_DIR/$HOST_NAME.json"
 
 # Set permissions for the manifest so that all users can read it.
-chmod o+r $TARGET_DIR/$HOST_NAME.json
+chmod o+r "$TARGET_DIR/$HOST_NAME.json"
 
 echo Native messaging host $HOST_NAME has been installed.
