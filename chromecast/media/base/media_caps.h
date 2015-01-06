644728d5e24c8b720319e10bd32faf3cf5e48da7
servolk <servolk@chromium.org>
2015-01-06 12:17:05 -0800

    Chromecast: Add media_caps.h to unblock internal builds
    media_caps.h is included by some of the Chromecast code,
    but none of the functions from there are being used at the
    moment.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/830423005
    
    Cr-Commit-Position: refs/heads/master@{#310128}
    
    Former-commit-id: 17445da82d3d68e3f1e8ced61a1f2a6264180d31

---
+++ chromecast/media/base/media_caps.h
@@ -0,0 +1,19 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_MEDIA_BASE_MEDIA_CAPS_
+#define CHROMECAST_MEDIA_BASE_MEDIA_CAPS_
+
+namespace media {
+
+bool HdmiSinkSupportsAC3();
+bool HdmiSinkSupportsDTS();
+bool HdmiSinkSupportsDTSHD();
+bool HdmiSinkSupportsEAC3();
+bool HdmiSinkSupportsPcmSurroundSound();
+
+}  // namespace media
+
+#endif  // CHROMECAST_MEDIA_BASE_MEDIA_CAPS_
+
