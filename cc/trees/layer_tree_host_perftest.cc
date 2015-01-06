e29879e785d0fb30de6f4f2166740dadd3350073
hendrikw <hendrikw@chromium.org>
2015-01-05 20:16:40 -0800

    cc: Turn off verify_property_trees
    Followup to https://codereview.chromium.org/802343007/
    
    cc_perftests are failing in LayerTreeHostCommon::CalculateDrawProperties, the
    difference in width and height of visible_content_rect() and
    visible_rect_from_property_trees() is 2, and the tolerance is 1.
    CHECK(!failed) is hit.
    
    Until the issue is resolved, set verify_property_trees to false
    
    BUG=444219
    
    Review URL: https://codereview.chromium.org/823753002
    
    Cr-Commit-Position: refs/heads/master@{#310048}
    
    Former-commit-id: d1896bcd60222e0999da63947ec60af7506c73d7

--- cc/trees/layer_tree_host_perftest.cc
+++ cc/trees/layer_tree_host_perftest.cc
@@ -242,13 +242,23 @@ class ScrollingLayerTreePerfTest : public LayerTreeHostPerfTestJsonReader {
 TEST_F(ScrollingLayerTreePerfTest, LongScrollablePageSingleThread) {
   SetTestName("long_scrollable_page");
   ReadTestFile("long_scrollable_page");
+  // TODO(vollick): Remove verify_property_trees setting after
+  // crbug.com/444219 is fixed.
+  bool old_verify_property_trees = verify_property_trees();
+  set_verify_property_trees(false);
   RunTest(false, false, false);
+  set_verify_property_trees(old_verify_property_trees);
 }
 
 TEST_F(ScrollingLayerTreePerfTest, LongScrollablePageThreadedImplSide) {
   SetTestName("long_scrollable_page_threaded_impl_side");
   ReadTestFile("long_scrollable_page");
+  // TODO(vollick): Remove verify_property_trees setting after
+  // crbug.com/444219 is fixed.
+  bool old_verify_property_trees = verify_property_trees();
+  set_verify_property_trees(false);
   RunTestWithImplSidePainting();
+  set_verify_property_trees(old_verify_property_trees);
 }
 
 static void EmptyReleaseCallback(uint32 sync_point, bool lost_resource) {}
