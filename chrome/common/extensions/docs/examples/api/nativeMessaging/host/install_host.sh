bb21b85006aaaa40cf6755d43d5110b07f1335aa
rob <rob@robwu.nl>
2015-01-06 09:44:27 -0800

    Fix string escaping in (un)install scripts of native messaging example
    NOTRY=true
    BUG=401797
    R= sergeyu@chromium.org
    
    Review URL: https://codereview.chromium.org/824783004
    
    Cr-Commit-Position: refs/heads/master@{#310106}
    
    Former-commit-id: 25a73d005e46ac0b9e8499e4c51f8c6305b56e87

--- chrome/common/extensions/docs/examples/api/nativeMessaging/host/install_host.sh
+++ chrome/common/extensions/docs/examples/api/nativeMessaging/host/install_host.sh
@@ -6,12 +6,11 @@
 set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-if [ $(uname -s) == 'Darwin' ]; then
+if [ "$(uname -s)" == "Darwin" ]; then
   if [ "$(whoami)" == "root" ]; then
     TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
   else
-    TARGET_DIR="
-        $HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+    TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
   fi
 else
   if [ "$(whoami)" == "root" ]; then
@@ -27,7 +26,7 @@ HOST_NAME=com.google.chrome.example.echo
 mkdir -p "$TARGET_DIR"
 
 # Copy native messaging host manifest.
-cp $DIR/$HOST_NAME.json "$TARGET_DIR"
+cp "$DIR/$HOST_NAME.json" "$TARGET_DIR"
 
 # Update host path in the manifest.
 HOST_PATH=$DIR/native-messaging-example-host
@@ -37,4 +36,4 @@ sed -i -e "s/HOST_PATH/$ESCAPED_HOST_PATH/" "$TARGET_DIR/$HOST_NAME.json"
 # Set permissions for the manifest so that all users can read it.
 chmod o+r "$TARGET_DIR/$HOST_NAME.json"
 
-echo Native messaging host $HOST_NAME has been installed.
+echo "Native messaging host $HOST_NAME has been installed."
--- chrome/common/extensions/docs/examples/api/nativeMessaging/host/uninstall_host.sh
+++ chrome/common/extensions/docs/examples/api/nativeMessaging/host/uninstall_host.sh
@@ -5,21 +5,20 @@
 
 set -e
 
-if [ $(uname -s) == 'Darwin' ]; then
+if [ "$(uname -s)" == "Darwin" ]; then
   if [ "$(whoami)" == "root" ]; then
     TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
   else
-    TARGET_DIR=\
-        "$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
+    TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
   fi
 else
   if [ "$(whoami)" == "root" ]; then
     TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
   else
-    TARGET_DIR='$HOME/.config/google-chrome/NativeMessagingHosts'
+    TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
   fi
 fi
 
 HOST_NAME=com.google.chrome.example.echo
-rm $TARGET_DIR/com.google.chrome.example.echo.json
-echo Native messaging host $HOST_NAME has been uninstalled.
+rm "$TARGET_DIR/com.google.chrome.example.echo.json"
+echo "Native messaging host $HOST_NAME has been uninstalled."
