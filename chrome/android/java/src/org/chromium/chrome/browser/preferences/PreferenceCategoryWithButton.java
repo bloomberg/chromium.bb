8684dc33cb3c7c65ab0bc83685d5f58e771d72e2
twellington <twellington@chromium.org>
2015-01-06 19:44:56 -0800

    Add contentDescriptions to PreferenceCategoryWithButton icons
    BUG=446372
    
    Review URL: https://codereview.chromium.org/807683003
    
    Cr-Commit-Position: refs/heads/master@{#310229}
    
    Former-commit-id: 2bdcbe888b0ba79ee55f9d6353ad02d6b67ec256

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/PreferenceCategoryWithButton.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/PreferenceCategoryWithButton.java
@@ -5,8 +5,10 @@
 package org.chromium.chrome.browser.preferences;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.preference.Preference;
 import android.preference.PreferenceGroup;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -20,10 +22,17 @@ import org.chromium.chrome.R;
  */
 public class PreferenceCategoryWithButton extends PreferenceGroup implements OnClickListener {
 
+    private String mContentDescription;
+
     public PreferenceCategoryWithButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         setLayoutResource(R.layout.preference_category);
         setSelectable(false);
+
+        TypedArray a = context.obtainStyledAttributes(attrs,
+                new int[] {android.R.attr.contentDescription});
+        mContentDescription = a.getString(0);
+        a.recycle();
     }
 
     @Override
@@ -33,7 +42,12 @@ public class PreferenceCategoryWithButton extends PreferenceGroup implements OnC
         // which needs a top padding of 16dp; we don't want this top padding for
         // PreferenceCategoryWithButton views.
         view.setPadding(view.getPaddingLeft(), 0, view.getPaddingRight(), view.getPaddingBottom());
-        view.findViewById(android.R.id.icon).setOnClickListener(this);
+        View button = view.findViewById(android.R.id.icon);
+        button.setOnClickListener(this);
+
+        if (!TextUtils.isEmpty(mContentDescription)) {
+            button.setContentDescription(mContentDescription);
+        }
     }
 
     @Override
