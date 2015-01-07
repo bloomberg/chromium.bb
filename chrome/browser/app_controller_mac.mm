598236b2c0a15ea8b9b8de7b6b356daa761058e4
mitchelljones <mitchelljones@chromium.org>
2015-01-06 18:51:34 -0800

    Fixed extension::is_hosted_app crash in AppController.
    r309996 introduced a potential crash, as it wasn't being checked if
    the extension existed first. This CL fixes the bug by adding this
    check.
    
    BUG=443120,444731
    
    Review URL: https://codereview.chromium.org/840693002
    
    Cr-Commit-Position: refs/heads/master@{#310223}
    
    Former-commit-id: b118a0ca3c627340f119eeb1ac29ed4648656e27

--- chrome/browser/app_controller_mac.mm
+++ chrome/browser/app_controller_mac.mm
@@ -1235,7 +1235,7 @@ class AppControllerProfileObserver : public ProfileInfoCacheObserver {
         const extensions::Extension* extension = registry->GetExtensionById(
             web_app::GetExtensionIdFromApplicationName(browser->app_name()),
             extensions::ExtensionRegistry::ENABLED);
-        if (extension->is_hosted_app())
+        if (extension && extension->is_hosted_app())
           continue;
       }
       browserWindows.insert(browser->window()->GetNativeWindow());
