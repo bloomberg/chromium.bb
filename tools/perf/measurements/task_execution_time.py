109b45015f2b935f83069ffd1ede5e24737853cb
mek <mek@chromium.org>
2015-01-08 09:03:57 -0800

    Revert of Start recording IO thread tasks as well as main thread tasks (patchset #12 id:210001 of https://codereview.chromium.org/750123003/)
    Reason for revert:
    Causes test failures on two bots:
    http://build.chromium.org/p/chromium.linux/builders/Linux%20Tests%20%28dbg%29%281%29/builds/37369
    http://build.chromium.org/p/chromium.mac/builders/Mac10.9%20Tests%20%28dbg%29/builds/1731
    
    [1/1] measurements.task_execution_time_unittest.TaskExecutionTimeUnitTest.testSlicesConformToRequiredNamingConventionsUsingDummyPage failed unexpectedly:
      Unable to find file in Cloud Storage: /mnt/data/b/build/slave/Linux_Tests__dbg__1_/build/src/tools/telemetry/unittest_data/test_000.wpr
      Could not find Flash at /mnt/data/b/build/slave/Linux_Tests__dbg__1_/build/src/third_party/adobe/flash/binaries/ppapi/linux/libpepflashplayer.so. Continuing without Flash.
      To run with Flash, check it out via http://go/read-src-internal
      Traceback (most recent call last):
        File "/mnt/data/b/build/slave/Linux_Tests__dbg__1_/build/src/tools/telemetry/telemetry/decorators.py", line 55, in wrapper
          func(*args, **kwargs)
        File "/mnt/data/b/build/slave/Linux_Tests__dbg__1_/build/src/tools/perf/measurements/task_execution_time_unittest.py", line 75, in testSlicesConformToRequiredNamingConventionsUsingDummyPage
          self.assertGreater(use_counts, 0)
      AssertionError: 0 not greater than 0
    
    Original issue's description:
    > Start recording IO thread tasks as well as main thread tasks.
    >
    > Recently a new metric "task_execution_time" was added. This reports the top 10 slowest tasks on the main renderer thread. With this update task_execution_time is extended to additionally report the top 10 slowest tasks on the browser and renderer IO threads.
    >
    > BUG=421434
    >
    > Committed: https://crrev.com/5812785773e22d85bb96294c0a30d8cacc2cae5b [formerly e70bc338dfa4eb6e25d0491275c54d3efdc6332b]
    > Cr-Commit-Position: refs/heads/master@{#310482}
    
    TBR=skyostil@google.com,alexclarke@google.com,petrcermak@google.com,ssid@google.com,rmcilroy@google.com,skyostil@chromium.org,petrcermak@chromium.org,rmcilroy@chromium.org,tonyg@chromium.org,picksi@chromium.org
    NOTREECHECKS=true
    NOTRY=true
    BUG=421434
    
    Review URL: https://codereview.chromium.org/784273008
    
    Cr-Commit-Position: refs/heads/master@{#310518}
    
    Former-commit-id: 96f142a3652ea5144c1da97fa50cdc3e49f26ce0

--- tools/perf/measurements/task_execution_time.py
+++ tools/perf/measurements/task_execution_time.py
@@ -4,42 +4,33 @@
 
 from telemetry.core.platform import tracing_category_filter
 from telemetry.core.platform import tracing_options
-from telemetry.page import page_test
 from telemetry.timeline.model import TimelineModel
+from telemetry.page import page_test
 from telemetry.util import statistics
 from telemetry.value import scalar
 
+_CATEGORIES = ['webkit.console',
+               'blink.console',
+               'benchmark',
+               'toplevel',
+               'blink',
+               'blink_gc',
+               'cc',
+               'v8']
 
 class TaskExecutionTime(page_test.PageTest):
 
   _TIME_OUT_IN_SECONDS = 60
   _NUMBER_OF_RESULTS_TO_DISPLAY = 10
-  _BROWSER_THREADS = ['Chrome_ChildIOThread',
-                      'Chrome_IOThread']
-  _RENDERER_THREADS = ['Chrome_ChildIOThread',
-                       'Chrome_IOThread',
-                       'CrRendererMain']
-  _CATEGORIES = ['benchmark',
-                 'blink',
-                 'blink.console',
-                 'blink_gc',
-                 'cc',
-                 'gpu',
-                 'ipc',
-                 'toplevel',
-                 'v8',
-                 'webkit.console']
 
   def __init__(self):
     super(TaskExecutionTime, self).__init__('RunPageInteractions')
-    self._renderer_process = None
-    self._browser_process = None
-    self._results = None
+    self._renderer_thread = None
 
   def WillNavigateToPage(self, page, tab):
     category_filter = tracing_category_filter.TracingCategoryFilter()
 
-    for category in self._CATEGORIES:
+    for category in _CATEGORIES:
       category_filter.AddIncludedCategory(category)
 
     options = tracing_options.TracingOptions()
@@ -51,54 +42,30 @@ class TaskExecutionTime(page_test.PageTest):
   def DidRunActions(self, page, tab):
     trace_data = tab.browser.platform.tracing_controller.Stop()
     timeline_model = TimelineModel(trace_data)
-
-    self._renderer_process = timeline_model.GetRendererProcessFromTabId(tab.id)
-    self._browser_process = timeline_model.browser_process
+    self._renderer_thread = timeline_model.GetRendererThreadFromTabId(tab.id)
 
   def ValidateAndMeasurePage(self, page, tab, results):
-    self._results = results
+    tasks = TaskExecutionTime.GetTasks(self._renderer_thread.parent)
 
-    for thread in self._BROWSER_THREADS:
-      self._AddTasksToResults(self._browser_process, thread)
-
-    for thread in self._RENDERER_THREADS:
-      self._AddTasksToResults(self._renderer_process, thread)
-
-  def _AddTasksToResults(self, process, thread_name):
-    if process is None:
-      return
-
-    tasks = TaskExecutionTime._GetTasksForThread(process, thread_name)
-
-    sorted_tasks = sorted(
-        tasks,
-        key=lambda slice: slice.median_self_duration,
-        reverse=True)
+    sorted_tasks = sorted(tasks,
+      key=lambda slice: slice.median_self_duration, reverse=True)
 
     for task in sorted_tasks[:self.GetExpectedResultCount()]:
-      self._results.AddValue(scalar.ScalarValue(
-          self._results.current_page,
-          task.key,
-          'ms',
-          task.median_self_duration,
-          description='Slowest tasks'))
+      results.AddValue(scalar.ScalarValue(
+        results.current_page,
+        task.key,
+        'ms',
+        task.median_self_duration,
+        description = 'Slowest tasks'))
 
   @staticmethod
-  def _GetTasksForThread(process, target_thread):
+  def GetTasks(process):
     task_dictionary = {}
     depth = 1
-
-    for thread in process.threads.values():
-      if thread.name != target_thread:
-        continue
-      for parent, task_slice in enumerate(thread.IterAllSlices()):
-        _ProcessTasksForThread(
-            task_dictionary,
-            process.name + ':' + thread.name,
-            task_slice,
-            depth,
-            parent)
-
+    for parent, task_slice in enumerate(
+          process.IterAllSlicesOfName('MessageLoop::RunTask')):
+      ProcessTasksForSlice(task_dictionary, task_slice, depth, parent)
+    # Return dictionary flattened into a list
     return task_dictionary.values()
 
   @staticmethod
@@ -106,62 +73,54 @@ class TaskExecutionTime(page_test.PageTest):
     return TaskExecutionTime._NUMBER_OF_RESULTS_TO_DISPLAY
 
 
-def _ProcessTasksForThread(dictionary, thread_name, task_slice, depth, parent):
-  if task_slice.self_thread_time is None:
-    # Early out if this slice is a TRACE_EVENT_INSTANT, as it has no duration.
-    return
-
-  self_duration = task_slice.self_thread_time
-
-  reported_name = thread_name + ':'
-
-  if 'src_func' in task_slice.args:
-    # Data contains the name of the timed function, use it as the name.
-    reported_name += task_slice.args['src_func']
-  elif 'line' in task_slice.args:
-    # Data contains IPC class and line numbers, use these as the name.
-    reported_name += 'IPC_Class_' + str(task_slice.args['class'])
-    reported_name += ':Line_' + str(task_slice.args['line'])
-  else:
-    # Fallback to use the name of the task slice.
-    reported_name += task_slice.name.lower()
-
-  # Replace any '.'s with '_'s as V8 uses them and it confuses the dashboard.
-  reported_name = reported_name.replace('.', '_')
-
-  if reported_name in dictionary:
-    # The dictionary contains an entry for this (e.g. from an earlier slice),
-    # add the new duration so we can calculate a median value later on.
-    dictionary[reported_name].Update(self_duration, depth, parent)
-  else:
-    # This is a new task so create a new entry for it
-    dictionary[reported_name] = SliceData(
-        reported_name,
-        self_duration,
-        depth,
-        parent)
-
-  # Process sub slices recursively.
-  for sub_slice in task_slice.sub_slices:
-    _ProcessTasksForThread(
-        dictionary,
-        thread_name,
-        sub_slice,
-        depth + 1,
-        parent)
-
-
 class SliceData:
-
-  def __init__(self, key, self_duration, depth, parent):
+  def __init__(self, key, self_duration, total_duration, depth, parent):
     self.key = key
+    self.count = 1
+
     self.self_durations = [self_duration]
+    self.min_self_duration = self_duration
+    self.max_self_duration = self_duration
+
+    self.total_durations = [total_duration]
+    self.min_total_duration = total_duration
+    self.max_total_duration = total_duration
+
     self.tree_location = [(depth, parent)]
 
-  def Update(self, self_duration, depth, parent):
+  def Update(self, self_duration, total_duration, depth, parent):
+    self.count += 1
+
     self.self_durations.append(self_duration)
+    self.min_self_duration = min(self.min_self_duration, self_duration)
+    self.max_self_duration = max(self.max_self_duration, self_duration)
+
+    self.total_durations.append(total_duration)
+    self.min_total_duration = min(self.min_total_duration, total_duration)
+    self.max_total_duration = max(self.max_total_duration, total_duration)
+
     self.tree_location.append((depth, parent))
 
   @property
   def median_self_duration(self):
     return statistics.Median(self.self_durations)
+
+
+def ProcessTasksForSlice(dictionary, task_slice, depth, parent):
+  # Deal with TRACE_EVENT_INSTANTs that have no duration
+  self_duration = task_slice.self_thread_time or 0.0
+  total_duration = task_slice.thread_duration or 0.0
+
+  # There is at least one task that is tracked as both uppercase and lowercase,
+  # forcing the name to lowercase coalesces both.
+  key = task_slice.name.lower()
+  if key in dictionary:
+    dictionary[key].Update(
+        self_duration, total_duration, depth, parent)
+  else:
+    dictionary[key] = SliceData(
+        key, self_duration, total_duration, depth, parent)
+
+  # Process sub slices recursively
+  for sub_slice in task_slice.sub_slices:
+    ProcessTasksForSlice(dictionary, sub_slice, depth + 1, parent)
--- tools/perf/measurements/task_execution_time_unittest.py
+++ tools/perf/measurements/task_execution_time_unittest.py
@@ -3,18 +3,13 @@
 # found in the LICENSE file.
 
 from measurements import task_execution_time
-from telemetry import decorators
 from telemetry.core import wpr_modes
 from telemetry.page import page as page_module
-from telemetry.results import page_test_results
-from telemetry.timeline import model as model_module
-from telemetry.timeline import slice as slice_data
 from telemetry.unittest_util import options_for_unittests
 from telemetry.unittest_util import page_test_test_case
 
 
 class TestTaskExecutionTimePage(page_module.Page):
-
   def __init__(self, page_set, base_dir):
     super(TestTaskExecutionTimePage, self).__init__(
         'file://blank.html', page_set, base_dir)
@@ -27,125 +22,28 @@ class TestTaskExecutionTimePage(page_module.Page):
 
 
 class TaskExecutionTimeUnitTest(page_test_test_case.PageTestTestCase):
-
   def setUp(self):
     self._options = options_for_unittests.GetCopy()
     self._options.browser_options.wpr_mode = wpr_modes.WPR_OFF
 
-  @decorators.Disabled('win')
-  def testSomeResultsReturnedFromDummyPage(self):
+  def testCorrectNumberOfResultsReturned(self):
     ps = self.CreateEmptyPageSet()
     ps.AddUserStory(TestTaskExecutionTimePage(ps, ps.base_dir))
     measurement = task_execution_time.TaskExecutionTime()
 
     results = self.RunMeasurement(measurement, ps, options=self._options)
 
-    self.assertGreater(len(results.all_page_specific_values), 0)
+    self.assertEquals(
+        task_execution_time.TaskExecutionTime.GetExpectedResultCount(),
+        len(results.all_page_specific_values))
 
-  @decorators.Disabled('win')
-  def testSlicesConformToRequiredNamingConventionsUsingDummyPage(self):
-    """This test ensures the presence of required keywords.
-
-       Some arbitrary keywords are required to generate the names of the top 10
-       tasks. The code has a weak dependancy on 'src_func', 'class' and 'line'
-       existing; if they exist in a slice's args they are used to generate a
-       name, if they don't exists the code falls back to using the name of the
-       slice, which is less clear.
-
-       If the code has been refactored and these keywords no longer exist
-       the code that relies on them in task_execution_time.py should be
-       updated to use the appropriate technique for assertaining this data
-       (and this test changed in the same way).
-    """
+  def testResultsAreDecreasing(self):
     ps = self.CreateEmptyPageSet()
     ps.AddUserStory(TestTaskExecutionTimePage(ps, ps.base_dir))
     measurement = task_execution_time.TaskExecutionTime()
 
-    self.RunMeasurement(measurement, ps, options=self._options)
+    results = self.RunMeasurement(measurement, ps, options=self._options)
 
-    required_keywords = {'src_func': 0, 'class': 0, 'line': 0}
-
-    # Check all slices and count the uses of the required keywords.
-    for thread in measurement._renderer_process.threads.values():
-      for slice_info in thread.IterAllSlices():
-        _CheckSliceForKeywords(slice_info, required_keywords)
-
-    # Confirm that all required keywords have at least one instance.
-    for use_counts in required_keywords.values():
-      self.assertGreater(use_counts, 0)
-
-  @decorators.Disabled('win')
-  def testMockedResults(self):
-    task_execution_time_metric = task_execution_time.TaskExecutionTime()
-    ps = self.CreateEmptyPageSet()
-
-    # Get the name of a thread used by task_execution_time metric and set up
-    # some dummy execution data pretending to be from that thread & process.
-    first_thread_name = task_execution_time_metric._RENDERER_THREADS[0]
-    data = TaskExecutionTestData(first_thread_name)
-    task_execution_time_metric._renderer_process = data._renderer_process
-
-    # Pretend we're about to run the tests to silence lower level asserts.
-    data.results.WillRunPage(TestTaskExecutionTimePage(ps, ps.base_dir))
-    data.AddSlice('fast', 0, 1)
-    data.AddSlice('medium', 0, 500)
-    data.AddSlice('slow', 0, 1000)
-
-    # Run the code we are testing.
-    task_execution_time_metric.ValidateAndMeasurePage(None, None, data.results)
-
-    # Confirm we get back 3 results that are correctly sorted and named
-    self.assertEqual(len(data.results.all_page_specific_values), 3)
-    self.assertEqual(
-        data.results.all_page_specific_values[0].name,
-        'process 1:' + first_thread_name + ':slow')
-    self.assertEqual(
-        data.results.all_page_specific_values[1].name,
-        'process 1:' + first_thread_name + ':medium')
-    self.assertEqual(
-        data.results.all_page_specific_values[2].name,
-        'process 1:' + first_thread_name + ':fast')
-    self.assertEqual(data.results.all_page_specific_values[0].value, 1000)
-    self.assertEqual(data.results.all_page_specific_values[1].value, 500)
-    self.assertEqual(data.results.all_page_specific_values[2].value, 1)
-
-
-def _CheckSliceForKeywords(slice_info, required_keywords):
-  for argument in slice_info.args:
-    if argument in required_keywords:
-      required_keywords[argument] += 1
-  # recurse into our sub-slices
-  for sub_slice in slice_info.sub_slices:
-    _CheckSliceForKeywords(sub_slice, required_keywords)
-
-
-class TaskExecutionTestData(object):
-
-  def __init__(self, thread_name):
-    self._model = model_module.TimelineModel()
-    self._renderer_process = self._model.GetOrCreateProcess(1)
-    self._renderer_thread = self._renderer_process.GetOrCreateThread(2)
-    self._renderer_thread.name = thread_name
-    self._results = page_test_results.PageTestResults()
-    self._metric = None
-    self._ps = None
-
-  @property
-  def results(self):
-    return self._results
-
-  @property
-  def metric(self):
-    return self._metric
-
-  def AddSlice(self, name, timestamp, duration):
-    self._renderer_thread.all_slices.append(
-        slice_data.Slice(
-            None,
-            'category',
-            name,
-            timestamp,
-            duration,
-            timestamp,
-            duration,
-            []))
+    for first, second in zip(
+        results.all_page_specific_values, results.all_page_specific_values[1:]):
+      self.assertGreaterEqual(first.value, second.value)
