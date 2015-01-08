45a2f19ccc57ba4493ba8faad178c9a8eb06da8f
bratell <bratell@opera.com>
2015-01-08 10:55:55 -0800

    Adding missing include for base/bind.h
    This code uses base::Bind and in default configurations
    "base/bind.h" is included through
    chrome/browser/extensions/extension_service.h ->
    chrome/browser/extensions/blacklist.h ->
    chrome/browser/safe_browsing/database_manager.h ->
    chrome/browser/safe_browsing/protocol_manager.h ->
    ...
    
    but in my configuration the code needs to be good code and include
    headers for code it uses itself.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/838003003
    
    Cr-Commit-Position: refs/heads/master@{#310549}
    
    Former-commit-id: 7bc7846ce63fc60f2c87b279c3a506c76c2a5323

--- chrome/browser/extensions/shared_module_service.cc
+++ chrome/browser/extensions/shared_module_service.cc
@@ -7,6 +7,8 @@
 #include <set>
 #include <vector>
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/version.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/pending_extension_manager.h"
