f1d5fa0490f24bad575154bc1de9f64058b53faa
bradnelson@google.com <bradnelson@google.com>
2015-01-08 01:47:34 +0000

    Switching canonicalize_timestamps.sh to use git.
    BUG=None
    TEST=local
    R=ncbray@chromium.org
    
    Review URL: https://codereview.chromium.org/841733003
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@14269 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: f8ee543a740a973d935d95eeb7ec3fc5ffcc0ad3

--- tools/canonicalize_timestamps.sh
+++ tools/canonicalize_timestamps.sh
@@ -5,45 +5,38 @@
 
 # This script must be run from the native_client/tools directory.
 # It canonicalizes the timestamps of all the files in the argument
-# directories to the svn revision date of the last commit that might
+# directories to the git revision date of the last commit that might
 # have changed the toolchain contents.  This way, if all the files
 # are identical in two builds, the tarballs will be identical too.
 
 # Fail if any command fails or if any undefined $variable is evaluated.
 set -u -e -o pipefail
 
-# Choose the appropriate svn command to use (taken from glibc_revision.sh).
-if [ "`uname -s`" = Darwin -o "`uname -o`" != "Cygwin" ]; then
-  SVN=svn
-else
-  SVN=svn.bat
-fi
-
 # These are the files whose commits represent "changing the toolchain".
 timestamp_files='Makefile REVISIONS'
 
-# Get the time a file was last changed by a subversion commit (YYYYMMDDHHMMSS).
-# svn info emits a line like:
-#       Text Last Updated: 2011-12-22 00:44:12 +0000 (Thu, 22 Dec 2011)
+# Get the time a file was last changed by a commit (YYYYMMDDHHMMSS).
+# git info emits a line like:
+#       2015-01-07 21:55:50 +0000
 # This function turns that into:
-#       20111222004412
+#       20150107215550
 # Such strings are comparable as integers to deduce their order in time.
-svn_timestamp() {
-  LC_ALL=C TZ=UTC $SVN info "$1" |
-  sed -n 's/^Text Last Updated: \([0-9]\{4\}\)-\([0-9]\{1,2\}\)-\([0-9]\{1,2\}\) \([0-9]\{1,2\}\):\([0-9]\{1,2\}\):\([0-9]\{1,2\}\) .*$/\1\2\3\4\5\6/p'
+git_timestamp() {
+  LC_ALL=C TZ=UTC git log -1 --format=%ci "$1" |
+  sed -n 's/^\([0-9]\{4\}\)-\([0-9]\{1,2\}\)-\([0-9]\{1,2\}\) \([0-9]\{1,2\}\):\([0-9]\{1,2\}\):\([0-9]\{1,2\}\) .*$/\1\2\3\4\5\6/p'
 }
 
 # Find the latest timestamp among the $timestamp_files.
 timestamp=0
 for timestamp_file in $timestamp_files; do
-  file_timestamp=`svn_timestamp "$timestamp_file"`
+  file_timestamp=`git_timestamp "$timestamp_file"`
   if [ "$file_timestamp" -gt $timestamp ]; then
     timestamp=$file_timestamp
   fi
 done
 
 if [ $timestamp -eq 0 ]; then
-  echo >&2 "Failed to glean svn revision timestamp from $timestamp_files"
+  echo >&2 "Failed to glean git revision timestamp from $timestamp_files"
   exit 1
 fi
 
