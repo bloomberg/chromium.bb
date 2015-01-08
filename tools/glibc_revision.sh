96942ca128ef3ca9f11713e6885a769066321156
bradnelson@google.com <bradnelson@google.com>
2015-01-08 03:24:10 +0000

    Switching glibc_version.sh to use git.
    Using the git hash in place of the svn revision.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3928
    TEST=None
    TBR=ncbray@chromium.org
    
    Review URL: https://codereview.chromium.org/794583005
    
    git-svn-id: svn://svn.chromium.org/native_client/trunk/src/native_client@14270 fcba33aa-ac0c-11dd-b9e7-8d5594d729c2
    
    Former-commit-id: d53738ca490fa727039de6e31d854742444d9b2d

--- tools/glibc_revision.sh
+++ tools/glibc_revision.sh
@@ -3,23 +3,4 @@
 # of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-REVISION=0
-cd "$(dirname "$0")"
-if [[ "$(uname -s)" = Darwin ]] || [[ "$(uname -o)" != "Cygwin" ]] ; then
-  SVN="svn"
-else
-  SVN="svn.bat"
-fi
-for i in REVISIONS glibc_revision.sh Makefile ; do
-  NREV="$("$SVN" log "$i" 2>/dev/null | head -n 2 | tail -n 1 |
-                                                sed -e s'+r\([0-9]*\) .*+\1+')"
-  if [[ "$NREV" -gt "$REVISION" ]]; then
-    REVISION="$NREV"
-  fi
-done
-echo "$REVISION"
-if [[ "$REVISION" == "0" ]]; then
-  exit 1
-else
-  exit 0
-fi
+git rev-parse HEAD
