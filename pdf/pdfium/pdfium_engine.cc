9858f2e302aa99471d9e7aa2c6b4bb75030c19ff
deepak.m1 <deepak.m1@samsung.com>
2015-01-08 21:16:03 -0800

    copying to clipboard should add a newline before the content of the next page.
    When we selected some text that spreads in multiple pages then content of
    pages should be separated by new line as happening in the Adobe Reader.
    code added for checking selection in newpage, then add a newline char.
    
    BUG=84387
    
    Review URL: https://codereview.chromium.org/824363002
    
    Cr-Commit-Position: refs/heads/master@{#310714}
    
    Former-commit-id: d209afa5561213383fa0bc18423a28c684dbe142

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -2251,11 +2251,14 @@ void PDFiumEngine::InvalidateAllPages() {
 
 std::string PDFiumEngine::GetSelectedText() {
   base::string16 result;
+  base::string16 new_line_char = base::UTF8ToUTF16("\n");
   for (size_t i = 0; i < selection_.size(); ++i) {
     if (i > 0 &&
         selection_[i - 1].page_index() > selection_[i].page_index()) {
-      result = selection_[i].GetText() + result;
+      result = selection_[i].GetText() + new_line_char + result;
     } else {
+      if (i > 0)
+        result.append(new_line_char);
       result.append(selection_[i].GetText());
     }
   }
