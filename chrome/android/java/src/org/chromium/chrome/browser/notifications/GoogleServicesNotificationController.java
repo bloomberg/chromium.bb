fd2c131ee4a6841c3465ea2c840a20788dd1b252
nyquist <nyquist@chromium.org>
2015-01-09 15:02:02 -0800

    Fix Google notification controller for proguard.
    The GoogleServicesNotificationController#getInstance() method is getting
    removed by proguard in downstream release builds, and is therefore not
    accessible by test classes.
    
    Add @VisibleForTesting annotation to make it always accessible.
    
    BUG=428882
    
    Review URL: https://codereview.chromium.org/823603007
    
    Cr-Commit-Position: refs/heads/master@{#310880}
    
    Former-commit-id: 60b831d31c8af2bbedb94c2c320ab08fb6c141d0

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/GoogleServicesNotificationController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/GoogleServicesNotificationController.java
@@ -49,6 +49,7 @@ public class GoogleServicesNotificationController {
         }
     }
 
+    @VisibleForTesting
     public static GoogleServicesNotificationController getInstance() {
         synchronized (LOCK) {
             return sInstance;
