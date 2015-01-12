d83d564dfb8df4d6c9d44c1867f56b57cabb66fc
wittman <wittman@chromium.org>
2015-01-12 10:22:23 -0800

    Add test for clicking bookmark star in presence of ctrl-D keybinding
    Tests that clicking the star brings up the bookmark UI, if no extension
    requests to override ctrl-D and the user has assigned it to an extension.
    This tests the functionality implemented in
    https://crrev.com/0455c0efe7a12950926b082ba61ff7c2a1720111 [formerly 54a7f399b0613cbae272fbe0ba97447be6517a99]
    
    BUG=426791
    
    Review URL: https://codereview.chromium.org/839883004
    
    Cr-Commit-Position: refs/heads/master@{#311070}
    
    Former-commit-id: 8347900ea1585146c58180ee339f44b4b699d6fa

--- chrome/browser/ui/views/extensions/bookmark_override_browsertest.cc
+++ chrome/browser/ui/views/extensions/bookmark_override_browsertest.cc
@@ -7,8 +7,14 @@
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/views/bookmarks/bookmark_bubble_view.h"
+#include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/location_bar/star_view.h"
+#include "chrome/browser/ui/views/toolbar/toolbar_view.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "extensions/common/manifest_constants.h"
+#include "extensions/test/result_catcher.h"
 
 typedef ExtensionApiTest BookmarkOverrideTest;
 
@@ -19,6 +25,42 @@ const char kBookmarkKeybinding[] = "Command+D";
 #else
 const char kBookmarkKeybinding[] = "Ctrl+D";
 #endif  // defined(OS_MACOSX)
+}
+
+// Test that clicking the star brings up the bookmark UI, if no extension
+// requests to override ctrl-D and the user has assigned it to an extension.
+IN_PROC_BROWSER_TEST_F(BookmarkOverrideTest, NonOverrideStarClick) {
+  ASSERT_TRUE(test_server()->Start());
+  ASSERT_TRUE(RunExtensionTest("keybinding/basics")) << message_;
+  const extensions::Extension* extension = GetSingleLoadedExtension();
+
+  extensions::CommandService* command_service =
+      extensions::CommandService::Get(browser()->profile());
+
+  // Simulate the user setting the keybinding to Ctrl+D.
+  command_service->UpdateKeybindingPrefs(
+      extension->id(), extensions::manifest_values::kBrowserActionCommandEvent,
+      kBookmarkKeybinding);
+
+  // Check that the BookmarkBubbleView is shown when clicking on the star.
+  BrowserView* browser_view = reinterpret_cast<BrowserView*>(
+      browser()->window());
+  views::View* star_view =
+      browser_view->GetToolbarView()->location_bar()->star_view();
+
+  ui::MouseEvent pressed_event(ui::ET_MOUSE_PRESSED, gfx::Point(), gfx::Point(),
+                               ui::EF_LEFT_MOUSE_BUTTON,
+                               ui::EF_LEFT_MOUSE_BUTTON);
+  ui::MouseEvent released_event(ui::ET_MOUSE_RELEASED, gfx::Point(),
+                                gfx::Point(), ui::EF_LEFT_MOUSE_BUTTON,
+                                ui::EF_LEFT_MOUSE_BUTTON);
+
+  // Verify that clicking once shows the bookmark bubble.
+  EXPECT_FALSE(BookmarkBubbleView::IsShowing());
+  star_view->OnMousePressed(pressed_event);
+  EXPECT_FALSE(BookmarkBubbleView::IsShowing());
+  star_view->OnMouseReleased(released_event);
+  EXPECT_TRUE(BookmarkBubbleView::IsShowing());
 }
 
 // Test that invoking the IDC_BOOKMARK_PAGE command (as done by the wrench menu)
