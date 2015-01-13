746771cc3e2cf7a11aaca223f8bb0111e3b16895
igsolla <igsolla@chromium.org>
2015-01-13 06:13:51 -0800

    [WebView] Pass the correct context to JsDialogHelper.showDialog.
    JsDialogHelper expects a context of Activity. However, the
    WebView now wraps the context inside a ContextWrapper, so we never
    show JS dialogs. This addresses problem 2 in crbug/447607
    
    We also handle the case now when the WebView is not created with
    an Activity context so that the JS dialog cannot be shown.
    
    BUG=447607
    
    Review URL: https://codereview.chromium.org/839623005
    
    Cr-Commit-Position: refs/heads/master@{#311266}
    
    Former-commit-id: b92868410cc0f2430afbe648ef17b3adef6a5fca

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewContentsClientAdapter.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewContentsClientAdapter.java
@@ -52,6 +52,7 @@ import org.chromium.base.ThreadUtils;
 import org.chromium.base.TraceEvent;
 import org.chromium.content.browser.ContentView;
 import org.chromium.content.browser.ContentViewClient;
+import org.chromium.content.browser.ContentViewCore;
 
 import java.lang.ref.WeakReference;
 import java.net.URISyntaxException;
@@ -766,8 +767,9 @@ public class WebViewContentsClientAdapter extends AwContentsClient {
                         new JsPromptResultReceiverAdapter(receiver).getPromptResult();
                 if (TRACE) Log.d(TAG, "onJsAlert");
                 if (!mWebChromeClient.onJsAlert(mWebView, url, message, res)) {
-                    new JsDialogHelper(res, JsDialogHelper.ALERT, null, message, url)
-                            .showDialog(mContext);
+                    if (!showDefaultJsDialog(res, JsDialogHelper.ALERT, null, message, url)) {
+                        receiver.cancel();
+                    }
                 }
             } else {
                 receiver.cancel();
@@ -786,8 +788,9 @@ public class WebViewContentsClientAdapter extends AwContentsClient {
                         new JsPromptResultReceiverAdapter(receiver).getPromptResult();
                 if (TRACE) Log.d(TAG, "onJsBeforeUnload");
                 if (!mWebChromeClient.onJsBeforeUnload(mWebView, url, message, res)) {
-                    new JsDialogHelper(res, JsDialogHelper.UNLOAD, null, message, url)
-                            .showDialog(mContext);
+                    if (!showDefaultJsDialog(res, JsDialogHelper.UNLOAD, null, message, url)) {
+                        receiver.cancel();
+                    }
                 }
             } else {
                 receiver.cancel();
@@ -806,8 +809,9 @@ public class WebViewContentsClientAdapter extends AwContentsClient {
                         new JsPromptResultReceiverAdapter(receiver).getPromptResult();
                 if (TRACE) Log.d(TAG, "onJsConfirm");
                 if (!mWebChromeClient.onJsConfirm(mWebView, url, message, res)) {
-                    new JsDialogHelper(res, JsDialogHelper.CONFIRM, null, message, url)
-                            .showDialog(mContext);
+                    if (!showDefaultJsDialog(res, JsDialogHelper.CONFIRM, null, message, url)) {
+                        receiver.cancel();
+                    }
                 }
             } else {
                 receiver.cancel();
@@ -827,8 +831,10 @@ public class WebViewContentsClientAdapter extends AwContentsClient {
                         new JsPromptResultReceiverAdapter(receiver).getPromptResult();
                 if (TRACE) Log.d(TAG, "onJsPrompt");
                 if (!mWebChromeClient.onJsPrompt(mWebView, url, message, defaultValue, res)) {
-                    new JsDialogHelper(res, JsDialogHelper.PROMPT, defaultValue, message, url)
-                            .showDialog(mContext);
+                    if (!showDefaultJsDialog(
+                            res, JsDialogHelper.PROMPT, defaultValue, message, url)) {
+                        receiver.cancel();
+                    }
                 }
             } else {
                 receiver.cancel();
@@ -836,6 +842,23 @@ public class WebViewContentsClientAdapter extends AwContentsClient {
         } finally {
             TraceEvent.end("WebViewContentsClientAdapter.handleJsPrompt");
         }
+    }
+
+    /**
+     * Try to show the default JS dialog and return whether the dialog was shown.
+     */
+    private boolean showDefaultJsDialog(JsPromptResult res, int jsDialogType, String defaultValue,
+            String message, String url) {
+        Context activityContext = ContentViewCore.activityFromContext(mContext);
+        if (activityContext == null) {
+            return false;
+        }
+        // TODO(igsolla): the activity context should be retrieved inside JsDialogHelper.showDialog
+        // but for that we need it to return a boolean. Also, doing it here means that we can fix
+        // problem 2 in crbug/447607 before M is released.
+        new JsDialogHelper(res, jsDialogType, defaultValue, message, url)
+            .showDialog(activityContext);
+        return true;
     }
 
     @Override
