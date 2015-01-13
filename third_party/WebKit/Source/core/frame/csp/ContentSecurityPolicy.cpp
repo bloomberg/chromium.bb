a911c587525fa3e2a187e19adf627c5ba5611ac7
jww@chromium.org <jww@chromium.org>
2015-01-13 19:23:34 +0000

    Added more useful message error message for plugin-types CSP directive.
    As pointed out in
    http://lists.w3.org/Archives/Public/public-webappsec/2015Jan/0096.html,
    it's a bit confusing for developers that 'none' is not a valid value for
    the CSP directive plugin-types. It was suggested that an improved
    console error message pointing developers to object-src might be
    helpful. This adds such an error message if the plugin-types value is
    set to 'none'.
    
    Review URL: https://codereview.chromium.org/811703006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188320 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 7244400ce22e569410df6194d2b7d75545dfcd89

--- third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.cpp
+++ third_party/WebKit/Source/core/frame/csp/ContentSecurityPolicy.cpp
@@ -796,6 +796,8 @@ void ContentSecurityPolicy::reportInvalidPluginTypes(const String& pluginType)
     String message;
     if (pluginType.isNull())
         message = "'plugin-types' Content Security Policy directive is empty; all plugins will be blocked.\n";
+    else if (pluginType == "'none'")
+        message = "Invalid plugin type in 'plugin-types' Content Security Policy directive: '" + pluginType + "'. Did you mean to set the object-src directive to 'none'?\n";
     else
         message = "Invalid plugin type in 'plugin-types' Content Security Policy directive: '" + pluginType + "'.\n";
     logToConsole(message);
