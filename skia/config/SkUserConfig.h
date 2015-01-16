81f5ee6f2ebf0c5c71e4171f3ac0d25251a752dd
reed <reed@google.com>
2015-01-15 16:43:25 -0800

    add flag to disable new mipmap logic (until we can rebaseline)
    BUG=
    
    Review URL: https://codereview.chromium.org/857463002
    
    Cr-Commit-Position: refs/heads/master@{#311787}
    
    Former-commit-id: 15b47e8b90c9316f3c6c8ac9e12243b651762875

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -275,6 +275,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_IGNORE_GPU_DITHER
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_MIPLEVELCHOICE
+#   define SK_SUPPORT_LEGACY_MIPLEVELCHOICE
+#endif
+
 #ifndef SK_SUPPORT_LEGACY_ADDOVAL
 #   define SK_SUPPORT_LEGACY_ADDOVAL
 #endif
