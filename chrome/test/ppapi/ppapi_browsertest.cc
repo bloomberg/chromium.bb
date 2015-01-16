8d45968e7bfec7948ef82be1dbc2487331742c83
teravest <teravest@chromium.org>
2015-01-16 08:50:24 -0800

    Fix PackagedAppTest when disable_nacl=1
    All of the PackagedAppTest tests in ppapi_browser_tests.cc require that NaCl is
    enabled to run successfully.
    
    This change removes PackagedAppTest (and respective tests) when DISABLE_NACL is
    true.
    
    BUG=449260
    
    Review URL: https://codereview.chromium.org/856583002
    
    Cr-Commit-Position: refs/heads/master@{#311911}
    
    Former-commit-id: 4d270bc63b32c958f6dd089ac7280d57a7d6fcd4

--- chrome/test/ppapi/ppapi_browsertest.cc
+++ chrome/test/ppapi/ppapi_browsertest.cc
@@ -1455,6 +1455,7 @@ TEST_PPAPI_OUT_OF_PROCESS(TalkPrivate)
 TEST_PPAPI_OUT_OF_PROCESS(OutputProtectionPrivate)
 #endif
 
+#if !defined(DISABLE_NACL)
 class PackagedAppTest : public ExtensionBrowserTest {
  public:
   explicit PackagedAppTest(const std::string& toolchain)
@@ -1498,10 +1499,8 @@ class NonSfiPackagedAppTest : public PackagedAppTest {
   NonSfiPackagedAppTest() : PackagedAppTest("nonsfi") { }
 
   void SetUpCommandLine(base::CommandLine* command_line) override {
-#if !defined(DISABLE_NACL)
     PackagedAppTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(switches::kEnableNaClNonSfiMode);
-#endif
   }
 };
 
@@ -1511,10 +1510,8 @@ class NonSfiPackagedAppTest : public PackagedAppTest {
 class TransitionalNonSfiPackagedAppTest : public NonSfiPackagedAppTest {
  public:
   void SetUpCommandLine(base::CommandLine* command_line) override {
-#if !defined(DISABLE_NACL)
     NonSfiPackagedAppTest::SetUpCommandLine(command_line);
     command_line->AppendSwitch(switches::kUseNaClHelperNonSfi);
-#endif
   }
 };
 
@@ -1534,7 +1531,6 @@ IN_PROC_BROWSER_TEST_F(TransitionalNonSfiPackagedAppTest,
   RunTests();
 }
 
-#if !defined(DISABLE_NACL)
 class MojoPPAPITest : public InProcessBrowserTest {
  public:
   MojoPPAPITest() : InProcessBrowserTest() { }
