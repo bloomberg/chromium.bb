9adc277e09050e059d088ade344716b2b6277fe1
mgiuca <mgiuca@chromium.org>
2015-01-15 21:03:58 -0800

    Re-enable AppCache.
    This feature was inadvertently disabled in r310975.
    
    BUG=449016
    
    Review URL: https://codereview.chromium.org/853713004
    
    Cr-Commit-Position: refs/heads/master@{#311842}
    
    Former-commit-id: b5e494b311191b5c74d75b50f93bacb7a1aebb77

--- content/browser/renderer_host/render_view_host_impl.cc
+++ content/browser/renderer_host/render_view_host_impl.cc
@@ -332,6 +332,7 @@ WebPreferences RenderViewHostImpl::ComputeWebkitPrefs(const GURL& url) {
 
   prefs.remote_fonts_enabled =
       !command_line.HasSwitch(switches::kDisableRemoteFonts);
+  prefs.application_cache_enabled = true;
 
   prefs.local_storage_enabled =
       !command_line.HasSwitch(switches::kDisableLocalStorage);
