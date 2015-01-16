80a5a1bfe6816cf48156c4aacc2097f914a0c7cb
estade <estade@chromium.org>
2015-01-15 16:38:50 -0800

    Make HyperlinkPreference optionally look like a link.
    Use this option in AutofillWalletPreferences. We can also use this to simplify BandwidthReductionPreferences (which is where this code was mostly copied from), but that's in the internal repo, so is a different CL.
    
    BUG=446700
    
    Review URL: https://codereview.chromium.org/853863002
    
    Cr-Commit-Position: refs/heads/master@{#311783}
    
    Former-commit-id: e20555d490b11357f0f178783a6e180e82e17468

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/HyperlinkPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/HyperlinkPreference.java
@@ -9,6 +9,7 @@ import android.content.res.TypedArray;
 import android.preference.Preference;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 import org.chromium.chrome.R;
@@ -20,12 +21,14 @@ public class HyperlinkPreference extends Preference {
 
     private final int mTitleResId;
     private final int mUrlResId;
+    private final boolean mImitateWebLink;
 
     public HyperlinkPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray a = context.obtainStyledAttributes(attrs,
                 R.styleable.HyperlinkPreference, 0, 0);
         mUrlResId = a.getResourceId(R.styleable.HyperlinkPreference_url, 0);
+        mImitateWebLink = a.getBoolean(R.styleable.HyperlinkPreference_imitateWebLink, false);
         a.recycle();
         mTitleResId = getTitleRes();
     }
@@ -40,5 +43,18 @@ public class HyperlinkPreference extends Preference {
         super.onBindView(view);
         TextView titleView = (TextView) view.findViewById(android.R.id.title);
         titleView.setSingleLine(false);
+
+        if (mImitateWebLink) {
+            setSelectable(false);
+
+            titleView.setClickable(true);
+            titleView.setTextColor(titleView.getPaint().linkColor);
+            titleView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    HyperlinkPreference.this.onClick();
+                }
+            });
+        }
     }
 }
