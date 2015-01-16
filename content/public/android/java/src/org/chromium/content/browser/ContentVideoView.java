57695ca1e826ea9d1e9290f5fe86288d67725f12
william.xie <william.xie@intel.com>
2015-01-16 08:32:02 -0800

    Ensure fullscreen media control bar is persistent
    Currently during fullscreen video playback, the media control
    bar is resizable by double tap or pinch, while the video is
    persistent, hence breaks the layout.
    
    This patch reslove the problem by disable MultiTouchZoomSupport
    and DoubleTapSupport for fullscreen video playback.
    
    BUG=448738
    
    Review URL: https://codereview.chromium.org/851933004
    
    Cr-Commit-Position: refs/heads/master@{#311908}
    
    Former-commit-id: 3a3b8df91ef3d958b1f4ee9f8025eecfad7d6b6c

--- content/public/android/java/src/org/chromium/content/browser/ContentVideoView.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentVideoView.java
@@ -87,6 +87,7 @@ public class ContentVideoView extends FrameLayout
     private View mProgressView;
 
     private final ContentVideoViewClient mClient;
+    private final ContentViewCore mContentViewCore;
 
     private boolean mInitialOrientation;
     private boolean mPossibleAccidentalChange;
@@ -162,11 +163,12 @@ public class ContentVideoView extends FrameLayout
         }
     };
 
-    private ContentVideoView(Context context, long nativeContentVideoView,
-            ContentVideoViewClient client) {
+    private ContentVideoView(Context context, ContentViewCore contentViewCore,
+            long nativeContentVideoView) {
         super(context);
         mNativeContentVideoView = nativeContentVideoView;
-        mClient = client;
+        mContentViewCore = contentViewCore;
+        mClient = mContentViewCore.getContentVideoViewClient();
         mUmaRecorded = false;
         mPossibleAccidentalChange = false;
         initResources(context);
@@ -360,8 +362,11 @@ public class ContentVideoView extends FrameLayout
         ThreadUtils.assertOnUiThread();
         Context context = contentViewCore.getContext();
         ContentVideoViewClient client = contentViewCore.getContentVideoViewClient();
-        ContentVideoView videoView = new ContentVideoView(context, nativeContentVideoView, client);
+        ContentVideoView videoView = new ContentVideoView(
+                context, contentViewCore, nativeContentVideoView);
         client.enterFullscreenVideo(videoView);
+        contentViewCore.updateDoubleTapSupport(false);
+        contentViewCore.updateMultiTouchZoomSupport(false);
         return videoView;
     }
 
@@ -388,6 +393,8 @@ public class ContentVideoView extends FrameLayout
             }
             nativeExitFullscreen(mNativeContentVideoView, relaseMediaPlayer);
             mNativeContentVideoView = 0;
+            mContentViewCore.updateDoubleTapSupport(true);
+            mContentViewCore.updateMultiTouchZoomSupport(true);
         }
     }
 
