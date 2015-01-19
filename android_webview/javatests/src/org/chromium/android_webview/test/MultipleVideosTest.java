64ebf232773b2249e2b1be526fbc87112823ae1e
hugo.holgersson <hugo.holgersson@sonymobile.com>
2015-01-19 02:47:30 -0800

    Add multiple video test
    Bug happened on a page that has two <video>s.
    Add such test page along with a test for the bug.
    
    BUG=431705
    
    Review URL: https://codereview.chromium.org/847473002
    
    Cr-Commit-Position: refs/heads/master@{#312093}
    
    Former-commit-id: 02309c99bf147fb3c0d3130c5bb515b0a65b9a93

---
+++ android_webview/javatests/src/org/chromium/android_webview/test/MultipleVideosTest.java
@@ -0,0 +1,96 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.android_webview.test;
+
+import android.test.suitebuilder.annotation.MediumTest;
+
+import org.chromium.android_webview.test.util.VideoSurfaceViewUtils;
+import org.chromium.base.test.util.Feature;
+import org.chromium.content.browser.ContentViewCore;
+import org.chromium.content.browser.test.util.DOMUtils;
+import org.chromium.content_public.browser.WebContents;
+
+import java.util.concurrent.Callable;
+
+/**
+ * Tests pages with multiple videos.
+ */
+public class MultipleVideosTest extends AwTestBase {
+    private static final String MULTIPLE_VIDEOS_TEST_URL =
+            "file:///android_asset/multiple_videos_test.html";
+    // These values must be kept in sync with the strings in above html.
+    private static final String FIRST_VIDEO_ID = "firstVideo";
+    private static final String SECOND_VIDEO_ID = "secondVideo";
+    private static final String PLAY_FIRST_BUTTON_ID = "playFirstButton";
+    private static final String PLAY_SECOND_BUTTON_ID = "playSecondButton";
+
+    private TestAwContentsClient mContentsClient;
+    private AwTestContainerView mTestContainerView;
+    private ContentViewCore mContentViewCore;
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        mContentsClient = new TestAwContentsClient();
+        mTestContainerView = createAwTestContainerViewOnMainSync(mContentsClient);
+        mContentViewCore = mTestContainerView.getContentViewCore();
+        enableJavaScriptOnUiThread(mTestContainerView.getAwContents());
+    }
+
+    @MediumTest
+    @Feature({"AndroidWebView"})
+    public void testFirstVideoPausesWhenSecondVideoStarts()
+            throws Throwable {
+        // To test video hole surfaces we must set force-flag before loading page.
+        VideoSurfaceViewUtils.forceUseVideoHoleSurfaceView();
+        loadTestPage();
+
+        // Play the first video.
+        tapFirstPlayButton();
+        assertTrue(DOMUtils.waitForVideoPlay(getWebContentsOnUiThread(), FIRST_VIDEO_ID));
+
+        // Verify that there is one video hole surface.
+        VideoSurfaceViewUtils.pollAndAssertContainsOneVideoHoleSurfaceView(this,
+                mTestContainerView);
+
+        // Start the second video.
+        tapSecondPlayButton();
+        assertTrue(DOMUtils.waitForVideoPlay(getWebContentsOnUiThread(), SECOND_VIDEO_ID));
+
+        // Verify that the first video pauses once the second video starts.
+        assertFalse(DOMUtils.isVideoPaused(getWebContentsOnUiThread(), SECOND_VIDEO_ID));
+        assertTrue(DOMUtils.isVideoPaused(getWebContentsOnUiThread(), FIRST_VIDEO_ID));
+
+        // Verify that there is still only one video hole surface.
+        VideoSurfaceViewUtils.pollAndAssertContainsOneVideoHoleSurfaceView(this,
+                mTestContainerView);
+    }
+
+    private void loadTestPage() throws Exception {
+        loadUrlSync(mTestContainerView.getAwContents(),
+                mContentsClient.getOnPageFinishedHelper(), MULTIPLE_VIDEOS_TEST_URL);
+    }
+
+    private void tapFirstPlayButton() throws Exception {
+        DOMUtils.clickNode(this, mContentViewCore, PLAY_FIRST_BUTTON_ID);
+    }
+
+    private void tapSecondPlayButton() throws Exception {
+        DOMUtils.clickNode(this, mContentViewCore, PLAY_SECOND_BUTTON_ID);
+    }
+
+    private WebContents getWebContentsOnUiThread() {
+        try {
+            return runTestOnUiThreadAndGetResult(new Callable<WebContents>() {
+                @Override
+                public WebContents call() throws Exception {
+                    return mContentViewCore.getWebContents();
+                }
+            });
+        } catch (Exception e) {
+            fail(e.getMessage());
+            return null;
+        }
+    }
+}
