8bf9177f33a4b43763ff1d0b4e54190fd541030a
dcheng@chromium.org <dcheng@chromium.org>
2015-01-20 18:31:23 +0000

    Revert 188574 "Reland factor out window proxy management portion..."
    > Reland factor out window proxy management portions of ScriptController.
    > 
    > Right now, ScriptController is in charge of initializing window proxies,
    > clearing them for navigation/close, and maintaining a mapping of worlds
    > to window proxies. However, ScriptController is LocalFrame-specific, and
    > RemoteFrame also needs to expose a DOMWindow for scripting purposes. To
    > share code between the two, the window proxy management has been moved
    > out of ScriptController, leaving only LocalFrame-specific logic such as
    > wrapping NPObject for plugins or script evaluation.
    > 
    > Eventually, RemoteFrame will also embed a WindowProxyManager, and
    > WindowProxyManager will be extended to allow it be passed between frames
    > when transitioning between local and remote frames.
    > 
    > For integration with devtools, WindowProxyManager still contains some
    > LocalFrame-specific code.
    > 
    > BUG=425623
    > 
    > Review URL: https://codereview.chromium.org/854453003
    
    TBR=dcheng@chromium.org
    
    Review URL: https://codereview.chromium.org/864523002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@188690 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6608433169a7fb0dbf45ce60129f6993b8606123

--- third_party/WebKit/Source/bindings/core/v8/NPV8Object.cpp
+++ third_party/WebKit/Source/bindings/core/v8/NPV8Object.cpp
@@ -40,7 +40,6 @@
 #include "bindings/core/v8/npruntime_priv.h"
 #include "core/frame/LocalDOMWindow.h"
 #include "core/frame/LocalFrame.h"
-#include "platform/ScriptForbiddenScope.h"
 #include "platform/UserGestureIndicator.h"
 #include "wtf/OwnPtr.h"
 #include "wtf/StringExtras.h"
@@ -343,9 +342,6 @@ bool _NPN_Evaluate(NPP npp, NPObject* npObject, NPString* npScript, NPVariant* r
 bool _NPN_EvaluateHelper(NPP npp, bool popupsAllowed, NPObject* npObject, NPString* npScript, NPVariant* result)
 {
     VOID_TO_NPVARIANT(*result);
-    if (ScriptForbiddenScope::isScriptForbidden())
-        return false;
-
     if (!npObject)
         return false;
 
