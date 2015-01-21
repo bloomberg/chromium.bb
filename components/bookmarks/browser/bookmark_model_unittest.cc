514b286d885d8349ec8f1b050355959eaac038a4
chrishtr <chrishtr@chromium.org>
2015-01-21 15:10:23 -0800

    Revert of Eliminate components_unittests' dependence on chrome resources. (patchset #8 id:180001 of https://codereview.chromium.org/856163002/)
    Reason for revert:
    Causes Mac ASAN bot to fail on BookmarkModelTest.Sort. The failures look the same as those referenced in crbug.com/450464.
    
    https://build.chromium.org/p/chromium.memory/builders/Mac%20ASan%2064%20Tests%20%281%29/builds/6320
    
    Original issue's description:
    > Eliminate components_unittests' dependence on chrome resources.
    >
    > This CL changes components_unittests to create its own pakfile rather than
    > relying on the chrome pakfile. To do this it adds a repack step that repacks
    > the pakfiles that components_unittests needs into a
    > components_unittests_resources.pak file, and then loads that pakfile
    > explicitly.
    >
    > This change means that components_unittests now passes after a clean build,
    > whereas before it would fail due to missing resources.
    >
    > This is based on blundell's change: https://codereview.chromium.org/258043003/
    >
    > BUG=348563,450464
    > R=blundell@chromium.org, thakis@chromium.org
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/37719d41d2e38eb4cc807f976eeafdf2f00c562a [formerly 045c9bf1d36f35643a077891dedf24054f2032a3]
    >
    > Committed: https://chromium.googlesource.com/chromium/src/+/2a7d3400d00c193ea01d29deca3fb0616c76dbbc [formerly e98e8e7654b7e51f98e92514897f8e1284eaa571]
    
    TBR=blundell@chromium.org,thakis@chromium.org,maruel@chromium.org,jam@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=348563,450464
    
    Review URL: https://codereview.chromium.org/867473002
    
    Cr-Commit-Position: refs/heads/master@{#312490}
    
    Former-commit-id: a2e3cc7a3586e7a27b414c47ae4db3d10efe40ba

--- components/bookmarks/browser/bookmark_model_unittest.cc
+++ components/bookmarks/browser/bookmark_model_unittest.cc
@@ -986,7 +986,6 @@ TEST_F(BookmarkModelTestWithProfile, CreateAndRestore) {
   }
 }
 
-#if !defined(OS_ANDROID) && !defined(OS_IOS)  // http://crbug.com/450464
 TEST_F(BookmarkModelTest, Sort) {
   // Populate the bookmark bar node with nodes for 'B', 'a', 'd' and 'C'.
   // 'C' and 'a' are folders.
@@ -1017,7 +1016,6 @@ TEST_F(BookmarkModelTest, Sort) {
   EXPECT_EQ(parent->GetChild(2)->GetTitle(), ASCIIToUTF16("B"));
   EXPECT_EQ(parent->GetChild(3)->GetTitle(), ASCIIToUTF16("d"));
 }
-#endif
 
 TEST_F(BookmarkModelTest, Reorder) {
   // Populate the bookmark bar node with nodes 'A', 'B', 'C' and 'D'.
--- components/test/run_all_unittests.cc
+++ components/test/run_all_unittests.cc
@@ -3,7 +3,6 @@
 // found in the LICENSE file.
 
 #include "base/bind.h"
-#include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/metrics/statistics_recorder.h"
 #include "base/path_service.h"
@@ -15,6 +14,10 @@
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
 #include "url/url_util.h"
+
+#if defined(OS_MACOSX)
+#include "base/mac/bundle_locations.h"
+#endif
 
 #if !defined(OS_IOS)
 #include "ui/gl/gl_surface.h"
@@ -53,16 +56,41 @@ class ComponentsTestSuite : public base::TestSuite {
     invalidation::android::RegisterInvalidationJni(env);
 #endif
 
+#if defined(OS_MACOSX) && !defined(OS_IOS)
+    // Look in the framework bundle for resources.
+    base::FilePath path;
+    PathService::Get(base::DIR_EXE, &path);
+
+    // TODO(tfarina): This is temporary. The right fix is to write a
+    // framework-Info.plist and integrate that into the build.
+    // Hardcode the framework name here to avoid having to depend on chrome's
+    // common target for chrome::kFrameworkName.
+#if defined(GOOGLE_CHROME_BUILD)
+    path = path.AppendASCII("Google Chrome Framework.framework");
+#elif defined(CHROMIUM_BUILD)
+    path = path.AppendASCII("Chromium Framework.framework");
+#else
+#error Unknown branding
+#endif
+
+    base::mac::SetOverrideFrameworkBundlePath(path);
+#endif
+
     ui::RegisterPathProvider();
 
+    // TODO(tfarina): This should be changed to InitSharedInstanceWithPakFile()
+    // so we can load our pak file instead of chrome.pak. crbug.com/348563
+    ui::ResourceBundle::InitSharedInstanceWithLocale(
+        "en-US", NULL, ui::ResourceBundle::LOAD_COMMON_RESOURCES);
     base::FilePath resources_pack_path;
-#if defined(OS_ANDROID)
-    PathService::Get(ui::DIR_RESOURCE_PAKS_ANDROID, &resources_pack_path);
-#else
+#if !defined(OS_ANDROID)
     PathService::Get(base::DIR_MODULE, &resources_pack_path);
+#else
+    PathService::Get(ui::DIR_RESOURCE_PAKS_ANDROID, &resources_pack_path);
 #endif
-    ui::ResourceBundle::InitSharedInstanceWithPakPath(
-        resources_pack_path.AppendASCII("components_unittests_resources.pak"));
+    ui::ResourceBundle::GetSharedInstance().AddDataPackFromPath(
+        resources_pack_path.AppendASCII("resources.pak"),
+        ui::SCALE_FACTOR_NONE);
 
     // These schemes need to be added globally to pass tests of
     // autocomplete_input_unittest.cc and content_settings_pattern*
@@ -78,6 +106,10 @@ class ComponentsTestSuite : public base::TestSuite {
 
   void Shutdown() override {
     ui::ResourceBundle::CleanupSharedInstance();
+
+#if defined(OS_MACOSX) && !defined(OS_IOS)
+  base::mac::SetOverrideFrameworkBundle(NULL);
+#endif
 
     base::TestSuite::Shutdown();
   }
