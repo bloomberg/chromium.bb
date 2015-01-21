fddcbebbb469a0b0edcdace9c512f3ab36964921
vitalybuka <vitalybuka@chromium.org>
2015-01-21 11:54:54 -0800

    Check if WebContents is being destroyed before creating infobar.
    This code is showing infobar on Cancel() or dialog close.
    Still dialog maybe closed with web_contents, so we need to check for this case and suppress infobar.
    
    BUG=430369, 450295
    
    Committed: https://crrev.com/19bea3861c2436aac3748e9c2f4c2951e5fe4a9a [formerly 713c4bb97b822070bb433ec5d824bf1c00e14e6f]
    Cr-Commit-Position: refs/heads/master@{#311957}
    
    Review URL: https://codereview.chromium.org/847143005
    
    Cr-Commit-Position: refs/heads/master@{#312400}
    
    Former-commit-id: 612dc5e5e6b287ec08f8a851fcb75cf1eb43939e

--- chrome/browser/ui/cocoa/content_settings/collected_cookies_mac_browsertest.mm
+++ chrome/browser/ui/cocoa/content_settings/collected_cookies_mac_browsertest.mm
@@ -4,6 +4,7 @@
 
 #import "chrome/browser/ui/cocoa/content_settings/collected_cookies_mac.h"
 
+#include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/test/base/in_process_browser_test.h"
@@ -12,44 +13,91 @@
 
 using web_modal::WebContentsModalDialogManager;
 
-typedef InProcessBrowserTest CollectedCookiesMacTest;
+class CollectedCookiesMacTest : public InProcessBrowserTest {
+ public:
+  void SetUpOnMainThread() override {
+    // Spawn a cookies dialog.  Note that |cookies_dialog_| will delete itself
+    // automatically when it closes.
+    cookies_dialog_ = new CollectedCookiesMac(
+        browser()->tab_strip_model()->GetActiveWebContents());
 
+    content::WebContents* web_contents =
+        browser()->tab_strip_model()->GetActiveWebContents();
+    WebContentsModalDialogManager* web_contents_modal_dialog_manager =
+        WebContentsModalDialogManager::FromWebContents(web_contents);
+    EXPECT_TRUE(web_contents_modal_dialog_manager->IsDialogActive());
+  }
+
+  // Shows infobar when dialog is closes.
+  void SetDialogChanged() {
+    [cookies_dialog_->sheet_controller() blockOrigin:nullptr];
+  }
+
+  void CloseCookiesDialog() {
+    cookies_dialog_->PerformClose();
+    content::RunAllPendingInMessageLoop();
+    content::WebContents* web_contents =
+        browser()->tab_strip_model()->GetActiveWebContents();
+    WebContentsModalDialogManager* web_contents_modal_dialog_manager =
+        WebContentsModalDialogManager::FromWebContents(web_contents);
+    EXPECT_FALSE(web_contents_modal_dialog_manager->IsDialogActive());
+  }
+
+  size_t infobar_count() const {
+    content::WebContents* web_contents =
+        browser()->tab_strip_model()->GetActiveWebContents();
+    return web_contents ?
+        InfoBarService::FromWebContents(web_contents)->infobar_count() : 0;
+  }
+
+  CollectedCookiesWindowController* sheet_controller() {
+    return cookies_dialog_->sheet_controller();
+  }
+
+ private:
+  CollectedCookiesMac* cookies_dialog_ = nullptr;
+};
+
+// Tests closing dialog without changing data.
 IN_PROC_BROWSER_TEST_F(CollectedCookiesMacTest, Close) {
-  content::WebContents* web_contents =
-      browser()->tab_strip_model()->GetActiveWebContents();
-  WebContentsModalDialogManager* web_contents_modal_dialog_manager =
-      WebContentsModalDialogManager::FromWebContents(web_contents);
-  EXPECT_FALSE(web_contents_modal_dialog_manager->IsDialogActive());
+  CloseCookiesDialog();
+  EXPECT_EQ(0u, infobar_count());
+}
 
-  // Deletes itself.
-  CollectedCookiesMac* dialog = new CollectedCookiesMac(
-      browser()->tab_strip_model()->GetActiveWebContents());
-  EXPECT_TRUE(web_contents_modal_dialog_manager->IsDialogActive());
+// Tests closing dialog with changing data. Dialog will show infobar.
+// TODO(vitalybuka): Fix and re-enable http://crbug.com/450295
+IN_PROC_BROWSER_TEST_F(CollectedCookiesMacTest, DISABLED_ChangeAndClose) {
+  SetDialogChanged();
+  CloseCookiesDialog();
+  EXPECT_EQ(1u, infobar_count());
+}
 
-  dialog->PerformClose();
+// Tests closing tab after changing dialog data. Changed dialog should not
+// show infobar or crash because InfoBarService is gone.
+// TODO(vitalybuka): Fix and re-enable http://crbug.com/450295
+IN_PROC_BROWSER_TEST_F(CollectedCookiesMacTest, DISABLED_ChangeAndCloseTab) {
+  SetDialogChanged();
+
+  // Closing the owning tab will close dialog.
+  browser()->tab_strip_model()->GetActiveWebContents()->Close();
+
   content::RunAllPendingInMessageLoop();
-  EXPECT_FALSE(web_contents_modal_dialog_manager->IsDialogActive());
+  EXPECT_EQ(0u, infobar_count());
 }
 
 IN_PROC_BROWSER_TEST_F(CollectedCookiesMacTest, Outlets) {
-  // Deletes itself.
-  CollectedCookiesMac* dialog = new CollectedCookiesMac(
-      browser()->tab_strip_model()->GetActiveWebContents());
-  CollectedCookiesWindowController* sheet_controller =
-      dialog->sheet_controller();
+  EXPECT_TRUE([sheet_controller() allowedTreeController]);
+  EXPECT_TRUE([sheet_controller() blockedTreeController]);
+  EXPECT_TRUE([sheet_controller() allowedOutlineView]);
+  EXPECT_TRUE([sheet_controller() blockedOutlineView]);
+  EXPECT_TRUE([sheet_controller() infoBar]);
+  EXPECT_TRUE([sheet_controller() infoBarIcon]);
+  EXPECT_TRUE([sheet_controller() infoBarText]);
+  EXPECT_TRUE([sheet_controller() tabView]);
+  EXPECT_TRUE([sheet_controller() blockedScrollView]);
+  EXPECT_TRUE([sheet_controller() blockedCookiesText]);
+  EXPECT_TRUE([sheet_controller() cookieDetailsViewPlaceholder]);
 
-  EXPECT_TRUE([sheet_controller allowedTreeController]);
-  EXPECT_TRUE([sheet_controller blockedTreeController]);
-  EXPECT_TRUE([sheet_controller allowedOutlineView]);
-  EXPECT_TRUE([sheet_controller blockedOutlineView]);
-  EXPECT_TRUE([sheet_controller infoBar]);
-  EXPECT_TRUE([sheet_controller infoBarIcon]);
-  EXPECT_TRUE([sheet_controller infoBarText]);
-  EXPECT_TRUE([sheet_controller tabView]);
-  EXPECT_TRUE([sheet_controller blockedScrollView]);
-  EXPECT_TRUE([sheet_controller blockedCookiesText]);
-  EXPECT_TRUE([sheet_controller cookieDetailsViewPlaceholder]);
-
-  [sheet_controller closeSheet:nil];
+  [sheet_controller() closeSheet:nil];
   content::RunAllPendingInMessageLoop();
 }
--- chrome/browser/ui/views/collected_cookies_views.cc
+++ chrome/browser/ui/views/collected_cookies_views.cc
@@ -206,7 +206,13 @@ base::string16 CollectedCookiesViews::GetDialogButtonLabel(
 }
 
 bool CollectedCookiesViews::Cancel() {
-  if (status_changed_) {
+  // If the user closes our parent tab while we're still open, this method will
+  // (eventually) be called in response to a WebContentsDestroyed() call from
+  // the WebContentsImpl to its observers.  But since the InfoBarService is also
+  // torn down in response to WebContentsDestroyed(), it may already be null.
+  // Since the tab is going away anyway, we can just omit showing an infobar,
+  // which prevents any attempt to access a null InfoBarService.
+  if (status_changed_ && !web_contents_->IsBeingDestroyed()) {
     CollectedCookiesInfoBarDelegate::Create(
         InfoBarService::FromWebContents(web_contents_));
   }
