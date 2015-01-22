697d71cb9dd2ba3ae69ec8cbe4660a3fcf1686a0
anujk.sharma <anujk.sharma@samsung.com>
2015-01-21 18:35:02 -0800

    Using "static_assert" in lieu of "COMPILE_ASSERT"  in crypto module
    All our toolchains support c++'s static_assert now- COMPILE_ASSERT can be removed now.
    
    BUG=442514
    
    Review URL: https://codereview.chromium.org/860323002
    
    Cr-Commit-Position: refs/heads/master@{#312535}
    
    Former-commit-id: 3cec2aba36e9cef361ad822f63800c91358c5c1d

--- crypto/nss_util.cc
+++ crypto/nss_util.cc
@@ -659,11 +659,11 @@ class NSSInitSingleton {
     EnsureNSPRInit();
 
     // We *must* have NSS >= 3.14.3.
-    COMPILE_ASSERT(
+    static_assert(
         (NSS_VMAJOR == 3 && NSS_VMINOR == 14 && NSS_VPATCH >= 3) ||
         (NSS_VMAJOR == 3 && NSS_VMINOR > 14) ||
         (NSS_VMAJOR > 3),
-        nss_version_check_failed);
+        "nss version check failed");
     // Also check the run-time NSS version.
     // NSS_VersionCheck is a >= check, not strict equality.
     if (!NSS_VersionCheck("3.14.3")) {
