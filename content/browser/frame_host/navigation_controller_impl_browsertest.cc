63dd0dc3f1dfd163b3c74a88bfac35a25d445d05
kochi <kochi@chromium.org>
2015-01-23 01:30:41 -0800

    Revert of Remove the use of page id from building the commit params. (patchset #3 id:40001 of https://codereview.chromium.org/839413004/)
    Reason for revert:
    The series of changes (r312793, r312792, r312791) are suspected to break
    pushState() related layout tests in Blink.  Might be some of them only affected,
    but let me revert all of them.
    
    BUG=451351
    
    Original issue's description:
    > Remove the use of page id from building the commit params.
    >
    > BUG=369661
    > TEST=tests stay green
    >
    > Committed: https://crrev.com/5507fe3aa8dbdb28734607b2e7649a2cd4da848d [formerly 66f3ec8e5ee6c3b528b5a6054cdca85ac0b29699]
    > Cr-Commit-Position: refs/heads/master@{#312791}
    
    TBR=creis@chromium.org,avi@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=369661
    
    Review URL: https://codereview.chromium.org/867963002
    
    Cr-Commit-Position: refs/heads/master@{#312803}
    
    Former-commit-id: 96545af0f94ec1ac19a57c86806c95834de5d33c

--- content/browser/frame_host/navigation_controller_impl_browsertest.cc
+++ content/browser/frame_host/navigation_controller_impl_browsertest.cc
@@ -4,29 +4,17 @@
 
 #include "base/bind.h"
 #include "base/strings/stringprintf.h"
-#include "content/browser/frame_host/frame_tree.h"
 #include "content/browser/frame_host/navigation_controller_impl.h"
 #include "content/browser/frame_host/navigation_entry_impl.h"
-#include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/browser/web_contents_observer.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/public/test/content_browser_test.h"
 #include "content/public/test/content_browser_test_utils.h"
-#include "content/public/test/test_utils.h"
 #include "content/shell/browser/shell.h"
-#include "content/test/content_browser_test_utils_internal.h"
-#include "net/dns/mock_host_resolver.h"
-#include "net/test/embedded_test_server/embedded_test_server.h"
 
 namespace content {
 
 class NavigationControllerBrowserTest : public ContentBrowserTest {
- protected:
-  void SetUpOnMainThread() override {
-    host_resolver()->AddRule("*", "127.0.0.1");
-    ASSERT_TRUE(embedded_test_server()->InitializeAndWaitUntilReady());
-  }
 };
 
 IN_PROC_BROWSER_TEST_F(NavigationControllerBrowserTest, LoadDataWithBaseURL) {
@@ -88,98 +76,5 @@ IN_PROC_BROWSER_TEST_F(NavigationControllerBrowserTest,
             controller.GetLastCommittedEntry()->GetURL());
 }
 
-struct FrameNavigateParamsCapturer : public WebContentsObserver {
- public:
-  explicit FrameNavigateParamsCapturer(FrameTreeNode* node)
-      : WebContentsObserver(
-            node->current_frame_host()->delegate()->GetAsWebContents()),
-        frame_tree_node_id_(node->frame_tree_node_id()),
-        message_loop_runner_(new MessageLoopRunner) {}
-
-  void Wait() {
-    message_loop_runner_->Run();
-  }
-
-  const FrameNavigateParams& params() const {
-    return params_;
-  }
-
- private:
-  void DidNavigateAnyFrame(RenderFrameHost* render_frame_host,
-                           const LoadCommittedDetails& details,
-                           const FrameNavigateParams& params) override {
-    RenderFrameHostImpl* rfh =
-        static_cast<RenderFrameHostImpl*>(render_frame_host);
-    if (rfh->frame_tree_node()->frame_tree_node_id() != frame_tree_node_id_)
-      return;
-
-    params_ = params;
-    message_loop_runner_->Quit();
-  }
-
-  // The id of the FrameTreeNode in which navigations are peformed.
-  int frame_tree_node_id_;
-
-  // The params of the last navigation.
-  FrameNavigateParams params_;
-
-  // The MessageLoopRunner used to spin the message loop.
-  scoped_refptr<MessageLoopRunner> message_loop_runner_;
-};
-
-// Verify that PAGE_TRANSITION_AUTO_SUBFRAME and PAGE_TRANSITION_MANUAL_SUBFRAME
-// are properly set for subframe navigations.
-IN_PROC_BROWSER_TEST_F(NavigationControllerBrowserTest,
-                       ManualAndAutoSubframeNavigationTransitions) {
-  GURL main_url(
-      embedded_test_server()->GetURL("/frame_tree/page_with_one_frame.html"));
-  NavigateToURL(shell(), main_url);
-
-  // It is safe to obtain the root frame tree node here, as it doesn't change.
-  FrameTreeNode* root =
-      static_cast<WebContentsImpl*>(shell()->web_contents())->
-          GetFrameTree()->root();
-
-  ASSERT_EQ(1U, root->child_count());
-  ASSERT_NE(nullptr, root->child_at(0));
-
-  {
-    // Navigate the iframe to a new URL; expect a manual subframe transition.
-    FrameNavigateParamsCapturer capturer(root->child_at(0));
-    GURL frame_url(
-          embedded_test_server()->GetURL("/frame_tree/2-1.html"));
-    NavigateFrameToURL(root->child_at(0), frame_url);
-    capturer.Wait();
-    EXPECT_EQ(ui::PAGE_TRANSITION_MANUAL_SUBFRAME,
-              capturer.params().transition);
-  }
-
-  {
-    // History navigations should result in an auto subframe transition.
-    FrameNavigateParamsCapturer capturer(root->child_at(0));
-    shell()->web_contents()->GetController().GoBack();
-    capturer.Wait();
-    EXPECT_EQ(ui::PAGE_TRANSITION_AUTO_SUBFRAME, capturer.params().transition);
-  }
-
-  {
-    // History navigations should result in an auto subframe transition.
-    FrameNavigateParamsCapturer capturer(root->child_at(0));
-    shell()->web_contents()->GetController().GoForward();
-    capturer.Wait();
-    EXPECT_EQ(ui::PAGE_TRANSITION_AUTO_SUBFRAME, capturer.params().transition);
-  }
-
-  {
-    // Navigate the iframe to a new URL; expect a manual subframe transition.
-    FrameNavigateParamsCapturer capturer(root->child_at(0));
-    GURL frame_url(
-          embedded_test_server()->GetURL("/frame_tree/2-3.html"));
-    NavigateFrameToURL(root->child_at(0), frame_url);
-    capturer.Wait();
-    EXPECT_EQ(ui::PAGE_TRANSITION_MANUAL_SUBFRAME,
-              capturer.params().transition);
-  }
-}
-
 }  // namespace content
+
