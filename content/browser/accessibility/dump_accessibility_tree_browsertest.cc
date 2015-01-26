0e37f52e605f8e2f5483664b8d5da45d738f46e0
kouhei <kouhei@chromium.org>
2015-01-25 18:17:52 -0800

    Reenable/rebaseline DumpAccessibilityTreeTest.AccessibilityANoText
    This CL reenables the disabled test:
    - DumpAccessibilityTreeTest.AccessibilityANoText.
    
    The test result is rebaselined so that it matches what spec says.
    The alttext for failed <img> load should show filename of the src.
    http://www.w3.org/TR/1998/REC-html40-19980424/appendix/notes.html#altgen
    
    BUG=448518,421300
    R=dmazzoni@chromium.org
    
    Review URL: https://codereview.chromium.org/866373002
    
    Cr-Commit-Position: refs/heads/master@{#313041}
    
    Former-commit-id: 5ad886dd80d7ed7cf8cb7dedbcf9f56531d8c75a

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -124,8 +124,7 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityANameCalc) {
   RunHtmlTest(FILE_PATH_LITERAL("a-name-calc.html"));
 }
 
-IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
-                       MAYBE(AccessibilityANoText)) {
+IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityANoText) {
   RunHtmlTest(FILE_PATH_LITERAL("a-no-text.html"));
 }
 
