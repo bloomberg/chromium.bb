a3af67716427e4e92abc1c98152549440f34918a
merkulova <merkulova@chromium.org>
2015-01-26 06:51:02 -0800

    Supervised icon added to unicorn userpods on account picker screen.
    BUG=450795
    
    Review URL: https://codereview.chromium.org/869543005
    
    Cr-Commit-Position: refs/heads/master@{#313066}
    
    Former-commit-id: 576f7d5d4c42bbf8bc7f07e20f1882bc4fd0c7cb

--- chrome/browser/chromeos/login/screens/user_selection_screen.cc
+++ chrome/browser/chromeos/login/screens/user_selection_screen.cc
@@ -36,6 +36,7 @@ const char kKeyEmailAddress[] = "emailAddress";
 const char kKeyEnterpriseDomain[] = "enterpriseDomain";
 const char kKeyPublicAccount[] = "publicAccount";
 const char kKeySupervisedUser[] = "supervisedUser";
+const char kKeyChildUser[] = "childUser";
 const char kKeySignedIn[] = "signedIn";
 const char kKeyCanRemove[] = "canRemove";
 const char kKeyIsOwner[] = "isOwner";
@@ -141,12 +142,14 @@ void UserSelectionScreen::FillUserDictionary(
       user->GetType() == user_manager::USER_TYPE_PUBLIC_ACCOUNT;
   const bool is_supervised_user =
       user->GetType() == user_manager::USER_TYPE_SUPERVISED;
+  const bool is_child_user = user->GetType() == user_manager::USER_TYPE_CHILD;
 
   user_dict->SetString(kKeyUsername, user_id);
   user_dict->SetString(kKeyEmailAddress, user->display_email());
   user_dict->SetString(kKeyDisplayName, user->GetDisplayName());
   user_dict->SetBoolean(kKeyPublicAccount, is_public_session);
   user_dict->SetBoolean(kKeySupervisedUser, is_supervised_user);
+  user_dict->SetBoolean(kKeyChildUser, is_child_user);
   user_dict->SetInteger(kKeyInitialAuthType, auth_type);
   user_dict->SetBoolean(kKeySignedIn, user->is_logged_in());
   user_dict->SetBoolean(kKeyIsOwner, is_owner);
