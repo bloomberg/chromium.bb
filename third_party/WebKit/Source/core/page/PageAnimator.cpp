0826c741630aabaa6aaf5a529e4cc52c208530bd
leviw@chromium.org <leviw@chromium.org>
2015-01-27 19:40:58 +0000

    Revert of PageAnimator: estimate number of documents and reserveInitialCapacity (patchset #1 id:1 of https://codereview.chromium.org/883573003/)
    Reason for revert:
    Appears to be causing crashes in browser tests due to bad LocalFrame casts: http://build.chromium.org/p/chromium.webkit/builders/Linux%20Tests%20(dbg)/builds/5376/steps/content_browsertests/logs/KillingRendererClearsDescendantProxies
    
    Original issue's description:
    > PageAnimator: estimate number of documents and reserveInitialCapacity
    > 
    > PageAnimator::serviceScriptedAnimation is called every frame.
    > The vector expansion should be kept minimal for performance.
    > 
    > The CL adds reserveInitialCapacity to avoid dynamic expand.
    > 
    > BUG=452351
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=189003
    
    TBR=dstockwell@chromium.org,haraken@chromium.org,kouhei@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=452351
    
    Review URL: https://codereview.chromium.org/880053002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189051 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2ce1b2aa1b88ed079b9e2d43cf2021021187e3ba

--- third_party/WebKit/Source/core/page/PageAnimator.cpp
+++ third_party/WebKit/Source/core/page/PageAnimator.cpp
@@ -41,8 +41,6 @@ void PageAnimator::serviceScriptedAnimations(double monotonicAnimationStartTime)
     TemporaryChange<bool> servicing(m_servicingAnimations, true);
 
     WillBeHeapVector<RefPtrWillBeMember<Document>> documents;
-    ASSERT(m_page->mainFrame());
-    documents.reserveInitialCapacity(m_page->mainFrame()->tree().scopedChildCount());
     for (Frame* frame = m_page->mainFrame(); frame; frame = frame->tree().traverseNext()) {
         if (frame->isLocalFrame())
             documents.append(toLocalFrame(frame)->document());
