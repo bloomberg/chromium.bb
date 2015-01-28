656783cb2a15e2abaa408266e4352d5ee8a92bf7
earthdok <earthdok@chromium.org>
2015-01-28 07:33:51 -0800

    Do not override CHROME_DEVEL_SANDBOX in tools/valgrind/valgrind_test.py
    Setting that variable to an empty string is not a supported way of disabling the
    sandbox. tools/valgrind/chrome_tests.py is supposed to pass "--no-sandbox" which
    takes care of that.
    
    BUG=245376
    TBR=jln@chromium.org, timurrrr@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/880023003
    
    Cr-Commit-Position: refs/heads/master@{#313517}
    
    Former-commit-id: 65f9d720f95b3c6f8465ad2fe7d49ac098b51a31

--- tools/valgrind/valgrind_test.py
+++ tools/valgrind/valgrind_test.py
@@ -380,12 +380,6 @@ class ValgrindTool(BaseTool):
 
     # The Valgrind command is constructed.
 
-    # Valgrind doesn't play nice with the Chrome sandbox.  Empty this env var
-    # set by runtest.py to disable the sandbox.
-    if os.environ.get("CHROME_DEVEL_SANDBOX", None):
-      logging.info("Removing CHROME_DEVEL_SANDBOX from environment")
-      os.environ["CHROME_DEVEL_SANDBOX"] = ''
-
     # Handle --indirect_webkit_layout separately.
     if self._options.indirect_webkit_layout:
       # Need to create the wrapper before modifying |proc|.
