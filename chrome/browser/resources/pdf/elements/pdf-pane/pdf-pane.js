d78a07d1e7d33deb747053a13de6f9e0e5ff604b
alexandrec <alexandrec@chromium.org>
2015-01-28 16:08:16 -0800

    Add a slide-in pane to the OOP PDF viewer.
    This CL adds the groundwork for a slide-in pane to be used by the OOP PDF Viewer. It adheres to the principles of Material Design, and will be used as a base for the bookmarks pane (which will display a list of bookmarks) as well as an info pane to show metadata of the PDF.
    
    BUG=110020
    
    Review URL: https://codereview.chromium.org/864753002
    
    Cr-Commit-Position: refs/heads/master@{#313631}
    
    Former-commit-id: 01e81ff908f129955e769d96b8626ae127a90a0e

---
+++ chrome/browser/resources/pdf/elements/pdf-pane/pdf-pane.js
@@ -0,0 +1,15 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+Polymer('pdf-pane', {
+    heading: '',
+
+    ready: function() {
+      // Prevent paper-dialog-base from scrolling back to top.
+      this.$.dialog.openAction = function() {};
+    },
+    toggle: function() {
+      this.$.dialog.toggle();
+    }
+});
