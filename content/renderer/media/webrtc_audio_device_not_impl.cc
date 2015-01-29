1ffb983df5df396e2ec619a05609487f3f7f5372
Tommi <tommi@chromium.org>
2015-01-29 17:25:43 +0100

    Remove deprecated (and unused) method.
    R=henrika@chromium.org
    TBR=henrika@chromium.org
    
    Review URL: https://codereview.chromium.org/884873005
    
    Cr-Commit-Position: refs/heads/master@{#313719}
    
    Former-commit-id: 826564b973277afd04d0835e9cd0eecd4b89c707

--- content/renderer/media/webrtc_audio_device_not_impl.cc
+++ content/renderer/media/webrtc_audio_device_not_impl.cc
@@ -10,10 +10,6 @@ WebRtcAudioDeviceNotImpl::WebRtcAudioDeviceNotImpl()
     : last_process_time_(base::TimeTicks::Now()) {
 }
 
-int32_t WebRtcAudioDeviceNotImpl::ChangeUniqueId(const int32_t id) {
-  return 0;
-}
-
 int64_t WebRtcAudioDeviceNotImpl::TimeUntilNextProcess() {
   const int64_t kMillisecondsBetweenProcessCalls = 5000;
   base::TimeDelta delta_time = (base::TimeTicks::Now() - last_process_time_);
