dd9fd4161c210a62fdfc36e27ab0d2b2a3f42121
Sam Clegg <sbc@chromium.org>
2015-01-29 17:10:13 -0800

    Fix nacl-clang redirectors
    Disable special case in the redirector code that was
    truncating argv0 in the target process.  This was
    causing strange errors from i686-nacl-ld:
    /x86_64-nacl-ld.exe: cannot open linker script file ldscripts/elf_i386_nacl.xc: No such file or directory
    
    Also, fix warnings and add debugging support.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=4060
    TEST=local testing with mingw build (nacl-clang toolchain) and cl.exe built (gcc toolchain)
    NOTRY=true (only pnacl toolchain is effected by this change)
    R=dschuff@chromium.org
    
    Review URL: https://codereview.chromium.org/882283002
    
    
    Former-commit-id: 4b258d144235533c2f3fbb6e9946020760c8ddf8

--- tools/redirector/redirector.c
+++ tools/redirector/redirector.c
@@ -9,8 +9,6 @@
 
 #include "native_client/tools/redirector/redirector.h"
 
-#define FILE_NOT_FOUND_ERROR_MESSAGE "Can not find filename to execute!\r\n"
-
 /*
  * The toolchain redirector reads it's name and invokes the appropriate binary
  * from a location with cygwin1.dll nearby so that all cygwin-oriented
@@ -25,10 +23,13 @@
  * cygwin has not been located yet.
  *
  * Use the following commands:
- *   cl /c /O2 /GS- redirector.c
+ *   cl /c /O2 /GS- redirector.c /DCYGWIN_REDIRECTORS=1
  *   link /subsystem:console /MERGE:.rdata=.text /NODEFAULTLIB
  *        redirector.obj kernel32.lib user32.lib
  */
+
+static int debug_enabled = 0;
+
 
 /*
  * Wraps GetModuleFileNameW to always succeed in finding a buffer of
@@ -121,11 +122,30 @@ int is_driver(const wchar_t *option) {
 }
 
 static
+void debug_print(const char* message) {
+  HANDLE handle;
+  DWORD wrote;
+  if (!debug_enabled)
+    return;
+  handle = GetStdHandle(STD_ERROR_HANDLE);
+  WriteFile(handle, message, lstrlen(message), &wrote, NULL);
+}
+
+static
+void debug_printW(const wchar_t* message) {
+  char message_utf8[256];
+  if (!debug_enabled)
+    return;
+  WideCharToMultiByte(CP_UTF8, 0, message, -1, message_utf8,
+                      sizeof(message_utf8), NULL, NULL);
+  debug_print(message_utf8);
+}
+
+static
 void println_redirect(const redirect_t *redirect) {
   const wchar_t *str;
   DWORD tmp;
-  HANDLE output;
-  output = GetStdHandle(STD_OUTPUT_HANDLE);
+  HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
   for (str = redirect->from; *str; ++str)
     WriteFile(output, str, 1, &tmp, NULL);
   WriteFile(output, L"|", 1, &tmp, NULL);
@@ -140,22 +160,37 @@ void println_redirect(const redirect_t *redirect) {
 int main() {
   wchar_t *newpath = NULL, *oldpath = NULL;
   const wchar_t *cmdline, *arguments, *selector;
+  const char* error_message = "Can not find filename to execute!";
+  const char* error_prefix = "redirector: ";
   DWORD length;
+  DWORD exitcode;
   int done;
   int redirect_index;
   int length_from;
   int length_to;
-  int i;
-  int tmp;
   HANDLE output;
   int n_redirects = sizeof(redirects)/sizeof(redirect_t);
   static STARTUPINFOW si = {sizeof(STARTUPINFOW), 0};
   static PROCESS_INFORMATION pi;
 
+  char var[10];
+  if (GetEnvironmentVariable("NACL_REDIRECTOR_DEBUG", var, 10)) {
+    if (lstrcmp(var, "1") == 0)
+      debug_enabled = 1;
+  }
+
   length = get_module_name_safely(&oldpath);
-  if (length == 0) goto ShowErrorMessage;
+  if (length == 0) {
+    error_message = "Unable to determine executable name.";
+    goto ShowErrorMessage;
+  }
+  debug_print("module name: ");
+  debug_printW(oldpath);
+  debug_print("\n");
+
   /* If redirector is called as "redirector.exe", dump redirector table.  */
   if (check_path(oldpath, L"redirector.exe")) {
+    debug_print("dumping redirect table:\n");
     output = GetStdHandle(STD_OUTPUT_HANDLE);
     for (redirect_index = 0;
          redirect_index < n_redirects;
@@ -164,37 +199,77 @@ int main() {
     }
     ExitProcess(0);
   }
+
+  /* Lookup executable name in redirect table */
   for (redirect_index = 0;
        redirect_index < n_redirects;
        redirect_index++) {
     if (check_path(oldpath, redirects[redirect_index].from))
       break;
   }
-  if (redirect_index >= n_redirects) goto ShowErrorMessage;
+  if (redirect_index >= n_redirects) {
+    error_message = "Executable not found in redirect table.";
+    goto ShowErrorMessage;
+  }
+
   newpath = HeapAlloc(GetProcessHeap(), 0, sizeof(wchar_t)*(length + 64));
-  if (!newpath) goto ShowErrorMessage;
+  if (!newpath) {
+    error_message = "HeapAlloc failed";
+    goto ShowErrorMessage;
+  }
+
   length_from = lstrlenW(redirects[redirect_index].from);
   length_to = lstrlenW(redirects[redirect_index].to);
   lstrcpynW(newpath, oldpath, length - length_from + 1);
   lstrcpyW(newpath + length - length_from, redirects[redirect_index].to);
+  HeapFree(GetProcessHeap(), 0, oldpath);
+  oldpath = NULL;
+  debug_print("target: ");
+  debug_printW(newpath);
+  debug_print("\n");
 
   selector = redirects[redirect_index].args;
+  debug_print("selector: ");
+  debug_printW(selector);
+  debug_print("\n");
+
+  /*
+   * The gcc (cygwin) redirectors truncate argv0 here to make error messages
+   * less confusing.  For nacl-clang it seems that we need to preserve the
+   * full argv0.
+   * TODO(sbc): Remove this once we are sure the cygwin behaviour is
+   * non-essential.
+   */
+#ifdef CYGWIN_REDIRECTORS
   cmdline = redirects[redirect_index].to;
   if (!is_driver(selector)) {
-    cmdline = find_last_slash_or_backslash_delimiter (cmdline);
+    cmdline = find_last_slash_or_backslash_delimiter(cmdline);
   } else {
     cmdline = newpath;
   }
-  HeapFree(GetProcessHeap(), 0, oldpath);
-  oldpath = NULL;
+#else
+  cmdline = newpath;
+#endif
+
+  debug_print("cmdline: ");
+  debug_printW(cmdline);
+  debug_print("\n");
+
   arguments = find_program_arguments();
-  if (!arguments) goto ShowErrorMessage;
+  if (!arguments) {
+    error_message = "GetCommandLine failed";
+    goto ShowErrorMessage;
+  }
   length = lstrlenW(cmdline);
   done = lstrlenW(selector);
   oldpath = HeapAlloc(GetProcessHeap(), 0,
     (lstrlenW(arguments) + length + done + 2) * sizeof(wchar_t));
-  if (!oldpath) goto ShowErrorMessage;
+  if (!oldpath) {
+    error_message = "HeapAlloc failed";
+    goto ShowErrorMessage;
+  }
   lstrcpyW(oldpath, cmdline);
+
   /*
    * cmdline == newpath means it's some kind of gcc driver and we need to
    * handle this case specially: we need to put the -V option to be the first.
@@ -215,28 +290,38 @@ int main() {
       done = 0;
     }
   }
+
   lstrcpyW(oldpath + length, L" ");
   lstrcpyW(oldpath + length + 1, selector);
   lstrcpyW(oldpath + length + done + 1, arguments);
-  if (CreateProcessW(newpath, oldpath, NULL, NULL, TRUE,
+  if (!CreateProcessW(newpath, oldpath, NULL, NULL, TRUE,
                      CREATE_UNICODE_ENVIRONMENT, NULL, NULL, &si, &pi) != 0) {
-    WaitForSingleObject(pi.hProcess, INFINITE);
-    HeapFree(GetProcessHeap(), 0, newpath);
-    HeapFree(GetProcessHeap(), 0, oldpath);
-    DWORD exitcode;
-    if (GetExitCodeProcess(pi.hProcess, &exitcode)) {
-      CloseHandle(pi.hProcess);
-      CloseHandle(pi.hThread);
-      ExitProcess(exitcode);
-    }
+    error_message = "CreateProcess failed";
+    goto ShowErrorMessage;
+  }
+
+  WaitForSingleObject(pi.hProcess, INFINITE);
+  HeapFree(GetProcessHeap(), 0, newpath);
+  HeapFree(GetProcessHeap(), 0, oldpath);
+  if (!GetExitCodeProcess(pi.hProcess, &exitcode)) {
     CloseHandle(pi.hProcess);
     CloseHandle(pi.hThread);
+    error_message = "GetExitCodeProcess failed";
+    goto ShowErrorMessage;
   }
+  CloseHandle(pi.hProcess);
+  CloseHandle(pi.hThread);
+  ExitProcess(exitcode);
+  return 0;
+
 ShowErrorMessage:
   if (newpath) HeapFree(GetProcessHeap(), 0, newpath);
   if (oldpath) HeapFree(GetProcessHeap(), 0, oldpath);
-  WriteFile(GetStdHandle(STD_ERROR_HANDLE), FILE_NOT_FOUND_ERROR_MESSAGE,
-            sizeof(FILE_NOT_FOUND_ERROR_MESSAGE) - 1, &length, NULL);
+
+  output = GetStdHandle(STD_ERROR_HANDLE);
+  WriteFile(output, error_prefix, lstrlen(error_prefix), &length, NULL);
+  WriteFile(output, error_message, lstrlen(error_message), &length, NULL);
+  WriteFile(output, "\r\n", 2, &length, NULL);
   ExitProcess(1);
 
   return 1;
