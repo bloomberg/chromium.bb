a7b2d9fcebd46d7e74b5f38129551db74f00299c
dmazzoni <dmazzoni@chromium.org>
2015-01-30 23:10:28 -0800

    Add more logging to track down flakiness.
    BUG=440579
    TBR=nasko
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/893773002
    
    Cr-Commit-Position: refs/heads/master@{#314051}
    
    Former-commit-id: 041d87ffac000386810baf9cddfac46e20c8862b

--- content/browser/renderer_host/legacy_render_widget_host_win.cc
+++ content/browser/renderer_host/legacy_render_widget_host_win.cc
@@ -147,6 +147,15 @@ LRESULT LegacyRenderWidgetHostHWND::OnGetObject(UINT message,
   // because it sometimes gets sign-extended incorrectly (but not always).
   DWORD obj_id = static_cast<DWORD>(static_cast<DWORD_PTR>(l_param));
 
+  // http://crbug.com/440579 TODO(dmazzoni): remove this logging when
+  // flakiness is fixed.
+  LOG(INFO) << "LegacyRenderWidgetHostHWND::OnGetObject"
+            << " message=" << message
+            << " w_param=" << w_param
+            << " l_param=" << l_param
+            << " obj_id=" << obj_id
+            << " host_=" << host_;
+
   if (kIdScreenReaderHoneyPot == obj_id) {
     // When an MSAA client has responded to our fake event on this id,
     // enable screen reader support.
@@ -159,14 +168,18 @@ LRESULT LegacyRenderWidgetHostHWND::OnGetObject(UINT message,
 
   RenderWidgetHostImpl* rwhi = RenderWidgetHostImpl::From(
       host_->GetRenderWidgetHost());
-  if (!rwhi)
+  if (!rwhi) {
+    LOG(WARNING) << "No RWHI";
     return static_cast<LRESULT>(0L);
+  }
 
   BrowserAccessibilityManagerWin* manager =
       static_cast<BrowserAccessibilityManagerWin*>(
           rwhi->GetRootBrowserAccessibilityManager());
-  if (!manager)
+  if (!manager) {
+    LOG(WARNING) << "No manager";
     return static_cast<LRESULT>(0L);
+  }
 
   base::win::ScopedComPtr<IAccessible> root(
       manager->GetRoot()->ToBrowserAccessibilityWin());
