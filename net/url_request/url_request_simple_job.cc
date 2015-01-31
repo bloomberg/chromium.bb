f0028958f200fbaef22cf1d4589fcce9d0d66137
vadimt <vadimt@chromium.org>
2015-01-30 16:19:20 -0800

    Undoing instrumentations since the bug is fixed.
    BUG=422489
    TBR=mmenke
    
    Review URL: https://codereview.chromium.org/889653004
    
    Cr-Commit-Position: refs/heads/master@{#314038}
    
    Former-commit-id: 500c3d5d8faa5147a2301016f4916b0054e2c2a3

--- net/url_request/url_request_simple_job.cc
+++ net/url_request/url_request_simple_job.cc
@@ -10,7 +10,6 @@
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted_memory.h"
 #include "base/message_loop/message_loop.h"
-#include "base/profiler/scoped_tracker.h"
 #include "base/threading/worker_pool.h"
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
@@ -26,10 +25,6 @@ void CopyData(const scoped_refptr<IOBuffer>& buf,
               int buf_size,
               const scoped_refptr<base::RefCountedMemory>& data,
               int64 data_offset) {
-  // TODO(vadimt): Remove ScopedTracker below once crbug.com/422489 is fixed.
-  tracked_objects::ScopedTracker tracking_profile(
-      FROM_HERE_WITH_EXPLICIT_FUNCTION("422489 CopyData"));
-
   memcpy(buf->data(), data->front() + data_offset, buf_size);
 }
 
@@ -65,11 +60,6 @@ URLRequestSimpleJob::~URLRequestSimpleJob() {}
 
 bool URLRequestSimpleJob::ReadRawData(IOBuffer* buf, int buf_size,
                                       int* bytes_read) {
-  // TODO(vadimt): Remove ScopedTracker below once crbug.com/422489 is fixed.
-  tracked_objects::ScopedTracker tracking_profile(
-      FROM_HERE_WITH_EXPLICIT_FUNCTION(
-          "422489 URLRequestSimpleJob::ReadRawData"));
-
   DCHECK(bytes_read);
   buf_size = static_cast<int>(
       std::min(static_cast<int64>(buf_size),
@@ -124,11 +114,6 @@ void URLRequestSimpleJob::StartAsync() {
     return;
 
   if (ranges().size() > 1) {
-    // TODO(vadimt): Remove ScopedTracker below once crbug.com/422489 is fixed.
-    tracked_objects::ScopedTracker tracking_profile(
-        FROM_HERE_WITH_EXPLICIT_FUNCTION(
-            "422489 URLRequestSimpleJob::StartAsync 1"));
-
     NotifyDone(URLRequestStatus(URLRequestStatus::FAILED,
                                 ERR_REQUEST_RANGE_NOT_SATISFIABLE));
     return;
@@ -137,36 +122,16 @@ void URLRequestSimpleJob::StartAsync() {
   if (!ranges().empty() && range_parse_result() == OK)
     byte_range_ = ranges().front();
 
-  int result;
-  {
-    // TODO(vadimt): Remove ScopedTracker below once crbug.com/422489 is fixed.
-    // Remove the block and assign 'result' in its declaration.
-    tracked_objects::ScopedTracker tracking_profile(
-        FROM_HERE_WITH_EXPLICIT_FUNCTION(
-            "422489 URLRequestSimpleJob::StartAsync 2"));
+  const int result =
+      GetRefCountedData(&mime_type_, &charset_, &data_,
+                        base::Bind(&URLRequestSimpleJob::OnGetDataCompleted,
+                                   weak_factory_.GetWeakPtr()));
 
-    result =
-        GetRefCountedData(&mime_type_, &charset_, &data_,
-                          base::Bind(&URLRequestSimpleJob::OnGetDataCompleted,
-                                     weak_factory_.GetWeakPtr()));
-  }
-
-  if (result != ERR_IO_PENDING) {
-    // TODO(vadimt): Remove ScopedTracker below once crbug.com/422489 is fixed.
-    tracked_objects::ScopedTracker tracking_profile(
-        FROM_HERE_WITH_EXPLICIT_FUNCTION(
-            "422489 URLRequestSimpleJob::StartAsync 3"));
-
+  if (result != ERR_IO_PENDING)
     OnGetDataCompleted(result);
-  }
 }
 
 void URLRequestSimpleJob::OnGetDataCompleted(int result) {
-  // TODO(vadimt): Remove ScopedTracker below once crbug.com/422489 is fixed.
-  tracked_objects::ScopedTracker tracking_profile(
-      FROM_HERE_WITH_EXPLICIT_FUNCTION(
-          "422489 URLRequestSimpleJob::OnGetDataCompleted"));
-
   if (result == OK) {
     // Notify that the headers are complete
     if (!byte_range_.ComputeBounds(data_->size())) {
