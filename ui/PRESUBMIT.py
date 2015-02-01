dce4d0e090b71e46e65e185d699653e1ae858a55
tfarina <tfarina@chromium.org>
2015-02-01 09:38:23 -0800

    ui: Add PRESUBMIT script to check the usage of scoped_ptr.
    This script should check the usage of scoped_ptr in files changed in the
    change list when uploading to codereview.chromium.org. That way we
    should avoid future regressions.
    
    BUG=None
    TEST=git cl presubmit -uv
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/867163003
    
    Cr-Commit-Position: refs/heads/master@{#314091}
    
    Former-commit-id: a852d632b1169f5c49c88803d4690f663a67ea4e

---
+++ ui/PRESUBMIT.py
@@ -0,0 +1,44 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Presubmit script for ui.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into depot_tools.
+"""
+
+INCLUDE_CPP_FILES_ONLY = (
+  r'.*\.(cc|h|mm)$',
+)
+
+def CheckScopedPtr(input_api, output_api,
+                   white_list=INCLUDE_CPP_FILES_ONLY, black_list=None):
+  black_list = tuple(black_list or input_api.DEFAULT_BLACK_LIST)
+  source_file_filter = lambda x: input_api.FilterSourceFile(x,
+                                                            white_list,
+                                                            black_list)
+  errors = []
+  for f in input_api.AffectedSourceFiles(source_file_filter):
+    for line_number, line in f.ChangedContents():
+      # Disallow:
+      # scoped_ptr<T>()
+      if re.search(r'\bscoped_ptr<.*?>\(\)', line):
+        errors.append(output_api.PresubmitError(
+          '%s:%d uses scoped_ptr<T>(). Use nullptr instead.' %
+          (f.LocalPath(), line_number)))
+  return errors
+
+
+def CheckChange(input_api, output_api):
+  results = []
+  results += CheckScopedPtr(input_api, output_api)
+  return results
+
+
+def CheckChangeOnUpload(input_api, output_api):
+  return CheckChange(input_api, output_api)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+  return CheckChange(input_api, output_api)
