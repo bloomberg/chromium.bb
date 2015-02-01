1377e01151073a55e5e93ebd51bfac61bf9a7dc1
kalman <kalman@chromium.org>
2015-01-31 16:33:47 -0800

    Replace the sample Calendar extension with a deprecation notice linking to a working (also By Google) Calendar extension.
    BUG=435290
    R=aboxhall@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/890173003
    
    Cr-Commit-Position: refs/heads/master@{#314075}
    
    Former-commit-id: 8a9227c890e05dd05adbebd00523222107e82919

--- chrome/common/extensions/docs/examples/extensions/calendar/javascript/options.js
+++ chrome/common/extensions/docs/examples/extensions/calendar/javascript/options.js
@@ -4,62 +4,14 @@
  * LICENSE file.
  */
 
-//Contains true if multiple calendar option is checked, false otherwise.
-var isMultiCalendar;
+var $ = document.getElementById.bind(document);
 
-//adding listener when body is loaded to call init function.
-window.addEventListener('load', init, false);
+var url = 'https://chrome.google.com/webstore/detail/' +
+          'google-calendar-by-google/gmbgaklkmjakoegficnlkhebmhkjfich';
 
-/**
- * Sets the value of multiple calendar checkbox based on value from
- * local storage.
- */
- function init() {
-  isMultiCalendar = JSON.parse(localStorage.multiCalendar || false);
-  $('multiCalendar').checked = isMultiCalendar;
-  $('multiCalendarText').innerHTML =
-      chrome.i18n.getMessage('multiCalendarText');
-  $('multiCalendar').addEventListener('click', save);
-  $('optionsTitle').innerHTML = chrome.i18n.getMessage('optionsTitle');
-  $('imageTooltip').title = chrome.i18n.getMessage('imageTooltip');
-  $('imageTooltip').alt = chrome.i18n.getMessage('imageTooltip');
-  $('multiCalendarText').title = chrome.i18n.getMessage('multiCalendarToolTip');
-  $('multiCalendar').title = chrome.i18n.getMessage('multiCalendarToolTip');
-  $('extensionName').innerHTML = chrome.i18n.getMessage('extensionName');
-  if (chrome.i18n.getMessage('direction') == 'rtl') {
-    $('body').style.direction = 'rtl';
-  }
-};
-
-/**
- * Saves the value of the checkbox into local storage.
- */
-function save() {
-  var multiCalendarId = $('multiCalendar');
-  localStorage.multiCalendar = multiCalendarId.checked;
-  if (multiCalendarId) {
-    multiCalendar.disabled = true;
-  }
-  $('status').innerHTML = chrome.i18n.getMessage('status_saving');
-  $('status').style.display = 'block';
-
-  // Sends message to the background page notifying it that the settings
-  // have updated.
-  chrome.runtime.getBackgroundPage(function(bg) {
-    bg.onSettingsChange();
-    statusSaved();
-  });
-};
-
-function statusSaved() {
-  if ($('multiCalendar')) {
-    if ($('multiCalendar').disabled) {
-      $('status').innerHTML = chrome.i18n.getMessage('status_saved');
-      $('status').style.display = 'block';
-      setTimeout(
-          function() { $('status').style.display = 'none'; }
-          , 1500);
-    }
-    $('multiCalendar').disabled = false;
-  }
+$('name').textContent = chrome.i18n.getMessage('name');
+$('link').href = url;
+$('remove').onclick = function() {
+  chrome.management.uninstallSelf({showConfirmDialog: true});
+  window.close();
 };
