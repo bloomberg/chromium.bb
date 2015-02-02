7151d597cf4367b60db712e5d8c27613a96ec577
ppi <ppi@chromium.org>
2015-02-02 05:25:44 -0800

    Filter out log lines when parsing output of --gtest_list_tests.
    This patch makes the Chromium apptest runner resilient to missing apps
    (e.g. tracing.mojo) polluting the output of mojo_shell that it tries to
    parse.
    
    BUG=450356
    TBR=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/892933002
    
    Cr-Commit-Position: refs/heads/master@{#314134}
    
    Former-commit-id: 554d1a97e618d951e5b30132b69ec51515d6839d

--- mojo/tools/gtest.py
+++ mojo/tools/gtest.py
@@ -87,6 +87,10 @@ def _gtest_list_tests(gtest_list_tests_output):
   """Returns a list of strings formatted as TestSuite.TestFixture from the
   output of running --gtest_list_tests on a GTEST application."""
 
+  # Remove log lines.
+  gtest_list_tests_output = (
+      re.sub("^\[.*\n", "", gtest_list_tests_output, flags=re.MULTILINE))
+
   if not re.match("^(\w*\.\r?\n(  \w*\r?\n)+)+", gtest_list_tests_output):
     raise Exception("Unrecognized --gtest_list_tests output:\n%s" %
                     gtest_list_tests_output)
