4c869da2f996dd215814b5cea432cfaa8d049fdc
sigbjornf@opera.com <sigbjornf@opera.com>
2015-02-02 14:51:07 +0000

    Ensure that performance.memory is updated on first access.
    A platform's implementation of monotonicallyIncreasingTime() is only
    required to be non-decreasing over time. Hence when checking if memory
    usage info should be updated for the first time, do not assume the
    function returns a value that will trigger the initial update.
    
    R=haraken
    BUG=454380
    
    Review URL: https://codereview.chromium.org/893803003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189339 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c0948914f7a403c318f8065b7ea16cef13ac6e30

--- third_party/WebKit/Source/core/timing/MemoryInfo.cpp
+++ third_party/WebKit/Source/core/timing/MemoryInfo.cpp
@@ -42,11 +42,13 @@
 
 namespace blink {
 
+static const double TwentyMinutesInSeconds = 20 * 60;
+
 class HeapSizeCache {
     WTF_MAKE_NONCOPYABLE(HeapSizeCache); WTF_MAKE_FAST_ALLOCATED;
 public:
     HeapSizeCache()
-        : m_lastUpdateTime(0)
+        : m_lastUpdateTime(monotonicallyIncreasingTime() - TwentyMinutesInSeconds)
     {
     }
 
@@ -67,8 +69,6 @@ private:
     {
         // We rate-limit queries to once every twenty minutes to make it more difficult
         // for attackers to compare memory usage before and after some event.
-        const double TwentyMinutesInSeconds = 20 * 60;
-
         double now = monotonicallyIncreasingTime();
         if (now - m_lastUpdateTime >= TwentyMinutesInSeconds) {
             update();
