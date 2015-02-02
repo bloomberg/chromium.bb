49bce258e2b5cf3eecec276e163ef89f8c576cdc
sigbjornf <sigbjornf@opera.com>
2015-02-02 15:08:17 -0800

    Upgrade Blink to milliseconds-based last modified filetimes, part 2.
    With WebFileInfo::modificationTimeMS now available, have
    content::FileInfoToWebFileInfo() set it accordingly and alongside
    modificationTime.
    
    1: [blink] add WebFileInfo::modificationTimeMS [ https://codereview.chromium.org/873723004/ ]
    2: [chromium] *this one* [ https://codereview.chromium.org/884413002/ ]
    3: [blink] make use of modificationTimeMS [ https://codereview.chromium.org/884393002/ ]
    4: [chromium] set modificationTime to something msec-based [ https://codereview.chromium.org/862203003/ ]
    5: [blink] switch to using modificationTime instead of *MS [ https://codereview.chromium.org/882343002/ ]
    6: [chromium] stop setting modificationTimeMS [ https://codereview.chromium.org/890523002/ ]
    7: [blink] remove modificationTimeMS [ https://codereview.chromium.org/869613005/ ]
    
    R=kinuko,jam
    BUG=451747
    
    Review URL: https://codereview.chromium.org/884413002
    
    Cr-Commit-Position: refs/heads/master@{#314214}
    
    Former-commit-id: 21810c1f4e59ba515a5c7fd22815fb913be48322

--- content/child/file_info_util.cc
+++ content/child/file_info_util.cc
@@ -13,10 +13,14 @@ void FileInfoToWebFileInfo(const base::File::Info& file_info,
                            blink::WebFileInfo* web_file_info) {
   DCHECK(web_file_info);
   // WebKit now expects NaN as uninitialized/null Date.
-  if (file_info.last_modified.is_null())
+  if (file_info.last_modified.is_null()) {
     web_file_info->modificationTime = std::numeric_limits<double>::quiet_NaN();
-  else
+    web_file_info->modificationTimeMS =
+        std::numeric_limits<double>::quiet_NaN();
+  } else {
     web_file_info->modificationTime = file_info.last_modified.ToDoubleT();
+    web_file_info->modificationTimeMS = file_info.last_modified.ToJsTime();
+  }
   web_file_info->length = file_info.size;
   if (file_info.is_directory)
     web_file_info->type = blink::WebFileInfo::TypeDirectory;
