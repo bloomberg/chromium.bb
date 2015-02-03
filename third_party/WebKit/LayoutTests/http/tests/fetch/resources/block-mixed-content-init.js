27768b0cec691878454750f051409f08df434ba0
hiroshige@chromium.org <hiroshige@chromium.org>
2015-02-03 07:43:57 +0000

    [Fetch] Add layout tests for blocking mixed content with redirects
    BUG=444493
    
    Review URL: https://codereview.chromium.org/885173003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189390 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6dde4f299da132f85a5e8e89f4716152f5fc19c5

--- third_party/WebKit/LayoutTests/http/tests/fetch/resources/block-mixed-content-init.js
+++ third_party/WebKit/LayoutTests/http/tests/fetch/resources/block-mixed-content-init.js
@@ -1,8 +1,15 @@
 if (window.testRunner) {
   // In Chromium we need to change the setting to disallow displaying insecure
   // contents.
+
+  // By default, LayoutTest content_shell returns allowed for
+  // "Should fetching request be blocked as mixed content?" at Step 5:
+  // https://w3c.github.io/webappsec/specs/mixedcontent/#should-block-fetch
+  // > If the user agent has been instructed to allow mixed content
+  // Turning the switch below to false makes this condition above false, and
+  // make content_shell to run into Step 7 to test mixed content blocking.
   testRunner.overridePreference('WebKitAllowRunningInsecureContent', false);
 }
 
 if (location.protocol != 'https:')
-    location = 'https://127.0.0.1:8443/' + location.pathname;
+    location = 'https://127.0.0.1:8443' + location.pathname;
--- third_party/WebKit/LayoutTests/http/tests/fetch/script-tests/block-mixed-content.js
+++ third_party/WebKit/LayoutTests/http/tests/fetch/script-tests/block-mixed-content.js
@@ -3,30 +3,82 @@ if (self.importScripts) {
 }
 
 var BASE_URL =
-  'http://127.0.0.1:8000/serviceworker/resources/fetch-access-control.php?';
+  'http://127.0.0.1:8000/serviceworker/resources/fetch-access-control.php?ACAOrigin=*&label=';
+var HTTPS_BASE_URL =
+  'https://127.0.0.1:8443/serviceworker/resources/fetch-access-control.php?ACAOrigin=*&label=';
+var HTTPS_OTHER_BASE_URL =
+  'https://localhost:8443/serviceworker/resources/fetch-access-control.php?ACAOrigin=*&label=';
 
-async_test(function(t) {
-    fetch(BASE_URL + 'ACAOrigin=*&label=same-origin', {mode: 'same-origin'})
-      .then(
-        t.unreached_func('should be blocked as mixed content (same-origin)'),
-        function() { t.done(); })
-      .catch(unreached_rejection(t));
-  }, 'Block fetch() as mixed content (same-origin)');
+var REDIRECT_URL =
+  'http://127.0.0.1:8000/serviceworker/resources/redirect.php?ACAOrigin=*&Redirect=';
+var HTTPS_REDIRECT_URL =
+  'https://127.0.0.1:8443/serviceworker/resources/redirect.php?ACAOrigin=*&Redirect=';
+var HTTPS_OTHER_REDIRECT_URL =
+  'https://localhost:8443/serviceworker/resources/redirect.php?ACAOrigin=*&Redirect=';
 
-async_test(function(t) {
-    fetch(BASE_URL + 'ACAOrigin=*&label=cors', {mode: 'cors'})
-      .then(t.unreached_func(
-        'should be blocked as mixed content (cors)'),
-        function() { t.done(); })
-      .catch(unreached_rejection(t));
-  }, 'Block fetch() as mixed content (cors)');
-
-async_test(function(t) {
-    fetch(BASE_URL + 'ACAOrigin=*&label=no-cors', {mode: 'no-cors'})
-      .then(t.unreached_func(
-        'should be blocked as mixed content (no-cors)'),
-        function() { t.done(); })
-      .catch(unreached_rejection(t));
-  }, 'Block fetch() as mixed content (no-cors)');
+['same-origin', 'cors', 'no-cors'].forEach(function(mode) {
+    promise_test(function(t) {
+        return Promise.resolve()
+          .then(function() {
+              // Test 1: Must fail: blocked as mixed content.
+              return fetch(BASE_URL + 'test1-' + mode, {mode: mode})
+                .then(t.unreached_func('Test 1: Must be blocked (' +
+                                       mode + ', HTTPS->HTTP)'),
+                      function() {});
+            })
+          .then(function() {
+              // Block mixed content in redirects.
+              // Test 2: Must fail: original fetch is not blocked but
+              // redirect is blocked.
+              return fetch(HTTPS_REDIRECT_URL +
+                           encodeURIComponent(BASE_URL + 'test2-' + mode),
+                           {mode: mode})
+                .then(t.unreached_func('Test 2: Must be blocked (' +
+                                       mode + ', HTTPS->HTTPS->HTTP)'),
+                      function() {});
+            })
+          .then(function() {
+              // Test 3: Must fail: original fetch is blocked.
+              return fetch(REDIRECT_URL +
+                           encodeURIComponent(HTTPS_BASE_URL + 'test3-' + mode),
+                           {mode: mode})
+                .then(t.unreached_func('Test 3: Must be blocked (' +
+                                       mode + ', HTTPS->HTTP->HTTPS)'),
+                      function() {});
+            })
+          .then(function() {
+              // Test 4: Must success.
+              // Test that the mixed contents above are not rejected due to
+              return fetch(HTTPS_REDIRECT_URL +
+                           encodeURIComponent(HTTPS_BASE_URL + 'test4-' + mode),
+                           {mode: mode})
+                .then(function(res) {assert_equals(res.status, 200); },
+                      t.unreached_func('Test 4: Must success (' +
+                                       mode + ', HTTPS->HTTPS->HTTPS)'));
+            })
+          .then(function() {
+              // Test 5: Must success if mode is not 'same-origin'.
+              // Test that the mixed contents above are not rejected due to
+              // CORS check.
+              return fetch(HTTPS_OTHER_REDIRECT_URL +
+                           encodeURIComponent(HTTPS_BASE_URL + 'test5-' + mode),
+                           {mode: mode})
+                .then(function(res) {
+                    if (mode === 'same-origin') {
+                      assert_unreached(
+                        'Test 5: Cross-origin HTTPS request must fail: ' +
+                        'mode = ' + mode);
+                    }
+                  },
+                  function() {
+                    if (mode !== 'same-origin') {
+                      assert_unreached(
+                        'Test 5: Cross-origin HTTPS request must success: ' +
+                        'mode = ' + mode);
+                    }
+                  });
+            });
+      }, 'Block fetch() as mixed content (' + mode + ')');
+  });
 
 done();
