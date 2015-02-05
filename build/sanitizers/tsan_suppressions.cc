251fc29aa50e93acee9ecd0d0e631a86fd80e80b
glider <glider@chromium.org>
2015-02-05 07:39:18 -0800

    Suppress a lock inversion in dbus::Bus::ShutdownAndBlock()
    BUG=455638
    TBR=satorux@chromium.org
    
    Review URL: https://codereview.chromium.org/901153005
    
    Cr-Commit-Position: refs/heads/master@{#314813}
    
    Former-commit-id: 5de672fb7058a1846961b9113b7e559b94a6882d

--- build/sanitizers/tsan_suppressions.cc
+++ build/sanitizers/tsan_suppressions.cc
@@ -316,6 +316,9 @@ char kTSanDefaultSuppressions[] =
 // https://crbug.com/448203
 "race:blink::RemoteFrame::detach\n"
 
+// https://crbug.com/455638
+"deadlock:dbus::Bus::ShutdownAndBlock\n"
+
 // https://crbug.com/455665
 "race:mojo::common::*::tick_clock\n"
 
