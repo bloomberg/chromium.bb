22decf5ceb396ffe2357aad1a04a69fa6108e61f
danakj <danakj@chromium.org>
2015-02-04 17:52:32 -0800

    cc: Trace the viewport size changing.
    This will help in tracking down window resize bugs.
    
    R=enne, vmpstr
    BUG=454333
    
    Review URL: https://codereview.chromium.org/864713004
    
    Cr-Commit-Position: refs/heads/master@{#314715}
    
    Former-commit-id: 5dba0c6a2a55bfc0387d014c0c17bd9f57d4b9eb

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -2195,6 +2195,10 @@ void LayerTreeHostImpl::ReleaseGL() {
 void LayerTreeHostImpl::SetViewportSize(const gfx::Size& device_viewport_size) {
   if (device_viewport_size == device_viewport_size_)
     return;
+  TRACE_EVENT_INSTANT2("cc", "LayerTreeHostImpl::SetViewportSize",
+                       TRACE_EVENT_SCOPE_THREAD, "width",
+                       device_viewport_size.width(), "height",
+                       device_viewport_size.height());
 
   if (pending_tree_)
     active_tree_->SetViewportSizeInvalid();
