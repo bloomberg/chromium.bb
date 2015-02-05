6aa27747ea22a55699199996a7da1576f124d526
rch <rch@chromium.org>
2015-02-05 14:24:58 -0800

    Switch to QUIC version 24.
    Review URL: https://codereview.chromium.org/904723002
    
    Cr-Commit-Position: refs/heads/master@{#314901}
    
    Former-commit-id: eb69a8a42edf1a6b80081baccb34ab38ad68473f

--- net/http/http_network_session.cc
+++ net/http/http_network_session.cc
@@ -97,7 +97,7 @@ HttpNetworkSession::Params::Params()
       enable_user_alternate_protocol_ports(false),
       quic_crypto_client_stream_factory(NULL),
       proxy_delegate(NULL) {
-  quic_supported_versions.push_back(QUIC_VERSION_23);
+  quic_supported_versions.push_back(QUIC_VERSION_24);
 }
 
 HttpNetworkSession::Params::~Params() {}
