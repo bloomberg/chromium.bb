ac7ff7f18d96ee717b05d38b7de0c5293797d0ab
jkarlin@chromium.org <jkarlin@chromium.org>
2015-02-06 17:12:05 +0000

    Fix prefetch-purpose layout test to support caching
    Prefetching behavior was recently changed to skip validation on the
    first reuse of a prefetched resource within 5 minutes. This broke the
    prefetch-purpose test because the test relied on the "cache-control:
    no-cache" of the prefetched resource forcing a revalidation.
    
    This CL changes the test to expect that the second request will be
    served from cache.
    
    BUG=448274
    
    Review URL: https://codereview.chromium.org/850753002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189653 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d420478df36c4536bfa5db00fbaec59f4df8f3a5

--- third_party/WebKit/LayoutTests/http/tests/misc/resources/prefetch-purpose.php
+++ third_party/WebKit/LayoutTests/http/tests/misc/resources/prefetch-purpose.php
@@ -1,24 +1,15 @@
-<?php setcookie("Purpose", $_SERVER["HTTP_PURPOSE"]);
-
-if (isset($_COOKIE['Purpose'])) {
-    setcookie("Purpose", "", time() - 3600);
-    echo "<h1>The cookie was set!</h1>";
-    echo "<p>Purpose: ";
-    echo $_COOKIE['Purpose'];
-} else {
-    echo "<h1>BAD BROWSER NO COOKIE</h1>";
-}
+<?php
+echo "Purpose: " . $_SERVER["HTTP_PURPOSE"];
 ?>
 
 <script>
 testRunner.notifyDone();
 </script>
 
-<p>This test verifies that prefetches are sent with the HTTP request header
-<b>Purpose: prefetch</b>.  To do this, the root page has a prefetch
-link targetting this subresource which contains a PHP script
-(resources/prefetch-purpose.php).  That initial prefetch of this
-resource sets a cookie.  Later, the root page sets window.location to
-target this script, which verifies the presence of the cookie, and
-generates the happy test output that you hopefully see right now.
-
+<p>This test verifies that prefetches are sent with the HTTP request
+header <b>Purpose: prefetch</b>.  To do this, the root page has a
+prefetch link targetting this subresource which contains a PHP script
+(resources/prefetch-purpose.php).  The PHP prints the value of the
+Purpose header into the document.  Later, the root page sets
+window.location to target this script, which should have "Purpose:
+prefetch" in its output if it's served from cache.
