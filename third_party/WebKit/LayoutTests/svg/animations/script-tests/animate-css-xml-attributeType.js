28910b87a2595fc6248f6bf1d3f19c56e9a6f6e5
ed@opera.com <ed@opera.com>
2015-02-06 14:54:01 +0000

    Tweak test in preparation for fixing bug 400725.
    Since rect.x will be made into a presentation attribute,
    pick another element and attribute for this test.
    
    BUG=
    TBR=fs@opera.com
    
    Review URL: https://codereview.chromium.org/890123003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@189647 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ab710b4484522f6a05e1d2df547a274a9d764de8

--- third_party/WebKit/LayoutTests/svg/animations/script-tests/animate-css-xml-attributeType.js
+++ third_party/WebKit/LayoutTests/svg/animations/script-tests/animate-css-xml-attributeType.js
@@ -2,61 +2,59 @@ description("Tests that XML and CSS attributeTypes can be switched between.");
 createSVGTestCase();
 
 // Setup test document
-var rect = createSVGElement("rect");
-rect.setAttribute("id", "rect");
-rect.setAttribute("x", "100");
-rect.setAttribute("width", "100");
-rect.setAttribute("height", "100");
-rect.setAttribute("fill", "green");
-rect.setAttribute("onclick", "executeTest()");
+var polygon = createSVGElement("polygon");
+polygon.setAttribute("id", "polygon");
+polygon.setAttribute("points", "100 0 200 0 200 100 100 100");
+polygon.setAttribute("fill", "green");
+polygon.setAttribute("onclick", "executeTest()");
 
 var set = createSVGElement("set");
 set.setAttribute("id", "set");
-set.setAttribute("attributeName", "x");
+set.setAttribute("attributeName", "points");
 set.setAttribute("attributeType", "XML");
-set.setAttribute("to", "300");
+set.setAttribute("to", "300 0 400 0 400 100 300 100");
 set.setAttribute("begin", "click");
-rect.appendChild(set);
-rootSVGElement.appendChild(rect);
+polygon.appendChild(set);
+rootSVGElement.appendChild(polygon);
 
 // Setup animation test
 function sample1() {
-    shouldBeCloseEnough("rect.x.animVal.value", "100");
-    shouldBe("rect.x.baseVal.value", "100");
+    shouldBeCloseEnough("polygon.animatedPoints.getItem(0).x", "100");
+    shouldBe("polygon.points.getItem(0).x", "100");
 }
 
 function sample2() {
-    shouldBeCloseEnough("rect.x.animVal.value", "300");
+    shouldBeCloseEnough("polygon.animatedPoints.getItem(0).x", "300");
     // change the animationType to CSS which is invalid.
     set.setAttribute("attributeType", "CSS");
 }
 
 function sample3() {
     // verify that the animation resets.
-    shouldBeCloseEnough("rect.x.animVal.value", "100");
+    shouldBeCloseEnough("polygon.animatedPoints.getItem(0).x", "100");
     // change the animation to a CSS animatable value.
     set.setAttribute("attributeName", "opacity");
     set.setAttribute("to", "0.8");
 }
 
 function sample4() {
-    shouldBeCloseEnough("parseFloat(getComputedStyle(rect).opacity)", "0.8");
+    shouldBeCloseEnough("parseFloat(getComputedStyle(polygon).opacity)", "0.8");
     // change the animation to a non-CSS animatable value.
-    set.setAttribute("attributeName", "x");
-    set.setAttribute("to", "200");
+    set.setAttribute("attributeName", "points");
+    set.setAttribute("to", "200 0 300 0 300 100 200 100");
 }
 
 function sample5() {
     // verify that the animation does not run.
-    shouldBeCloseEnough("rect.x.animVal.value", "100");
-    shouldBeCloseEnough("parseFloat(getComputedStyle(rect).opacity)", "1.0");
+    shouldBeCloseEnough("polygon.animatedPoints.getItem(0).x", "100");
+    shouldBeCloseEnough("parseFloat(getComputedStyle(polygon).opacity)", "1.0");
     // change the animationType to XML which is valid.
     set.setAttribute("attributeType", "XML");
 }
 
 function sample6() {
-    shouldBeCloseEnough("rect.x.animVal.value", "200");
-    shouldBe("rect.x.baseVal.value", "100");
+    shouldBeCloseEnough("polygon.animatedPoints.getItem(0).x", "200");
+    shouldBe("polygon.points.getItem(0).x", "100");
 }
 
 function executeTest() {
