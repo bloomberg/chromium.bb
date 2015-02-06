bc7da7b28d4ed55da3941ecc3d34e899cf820f43
jinlong.zhai <jinlong.zhai@samsung.com>
2015-02-06 15:20:06 -0800

    Change g_webrtc_logging_delegate from process-level to RenderThread-level
    --single-process mode may happen crash in file of webrtc_logging.cc where code
    is CHECK(!g_webrtc_logging_delegate). The orginal code is to expect there to
    be only one g_webrtc_logging_delegate instance in a single render process.
    g_webrtc_logging_delegate is created when a RenderThreadImpl is created.
    But in --single-process model all RenderThreadImpl are created in browser
    process and so g_webrtc_logging_delegate may be created many times in a single
    process. Then CHECK(!g_webrtc_logging_delegate) will fail.
    
    In this patch will change g_webrtc_logging_delegate from process-level to
    RenderThread-Level. Since every render process will only keep one RenderThread.
    
    BUG=455573
    
    Review URL: https://codereview.chromium.org/889313003
    
    Cr-Commit-Position: refs/heads/master@{#315131}
    
    Former-commit-id: 1fb332dc1f81bb05716342704b7f969af6921f3e

--- content/renderer/media/webrtc_logging.cc
+++ content/renderer/media/webrtc_logging.cc
@@ -5,19 +5,22 @@
 #include "content/renderer/media/webrtc_logging.h"
 
 #include "base/time/time.h"
+#include "base/lazy_instance.h"
+#include "base/threading/thread_local.h"
 #include "content/public/renderer/webrtc_log_message_delegate.h"
 #include "third_party/webrtc/overrides/webrtc/base/logging.h"
 
 namespace content {
 
-// Shall only be set once and never go back to NULL.
-WebRtcLogMessageDelegate* g_webrtc_logging_delegate = NULL;
+// Shall only be set once within a RenderThread and never go back to NULL.
+base::LazyInstance<base::ThreadLocalPointer<WebRtcLogMessageDelegate> >::Leaky
+    g_webrtc_logging_delegate_tls = LAZY_INSTANCE_INITIALIZER;
 
 void InitWebRtcLoggingDelegate(WebRtcLogMessageDelegate* delegate) {
-  CHECK(!g_webrtc_logging_delegate);
+  CHECK(!g_webrtc_logging_delegate_tls.Pointer()->Get());
   CHECK(delegate);
 
-  g_webrtc_logging_delegate = delegate;
+  g_webrtc_logging_delegate_tls.Pointer()->Set(delegate);
 }
 
 void InitWebRtcLogging() {
@@ -26,8 +29,8 @@ void InitWebRtcLogging() {
 }
 
 void WebRtcLogMessage(const std::string& message) {
-  if (g_webrtc_logging_delegate)
-    g_webrtc_logging_delegate->LogMessage(message);
+  if (g_webrtc_logging_delegate_tls.Pointer()->Get())
+    g_webrtc_logging_delegate_tls.Pointer()->Get()->LogMessage(message);
 }
 
 }  // namespace content
