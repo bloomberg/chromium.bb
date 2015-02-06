49185b80e233c92e389e29a4cc3c447bab88287f
erikchen <erikchen@chromium.org>
2015-02-05 16:11:04 -0800

    Make Telemetry tests check the configuration of the OSX Keychain.
    Historically, the OSX Keychain has been misconfigured on some perf bots, which
    has caused Telemtry tests to stall. This CL adds checks to emit warnings if the
    Keychain is misconfigured.
    
    I added checks for three common misconfigurations:
      - Is the default Keychain locked.
      - Is the default Keychain configured to automatically lock after a period of
      time.
      - Are the ACLs of a commonly accessed key in the default keychain
      misconfigured for the perf bots.
    
    The first and third checks are performed by short C programs since the
    information is easily available through the Security framework, but not from
    the security CLI.
    
    BUG=443340
    
    Review URL: https://codereview.chromium.org/809473004
    
    Cr-Commit-Position: refs/heads/master@{#314921}
    
    Former-commit-id: 0e07c2d7073bcb7b644fe1fe02031c55f5c1ccbe

--- tools/perf/metrics/keychain_metric.py
+++ tools/perf/metrics/keychain_metric.py
@@ -2,9 +2,11 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import logging
 import sys
 
 from metrics import Metric
+from telemetry.util.mac import keychain_helper
 from telemetry.value import histogram_util
 from telemetry.value import scalar
 
@@ -18,12 +20,44 @@ class KeychainMetric(Metric):
   DISPLAY_NAME = 'OSX_Keychain_Access'
   HISTOGRAM_NAME = 'OSX.Keychain.Access'
 
+  @staticmethod
+  def _CheckKeychainConfiguration():
+    """
+    On OSX, it is possible for a misconfigured keychain to cause the
+    Telemetry tests to stall. This method confirms that the keychain is in a
+    sane state that will not cause this behavior. Three conditions are checked:
+      - The keychain is unlocked.
+      - The keychain will not auto-lock after a period of time.
+      - The ACLs are correctly configured on the relevant keychain items.
+    """
+    warning_suffix = ('which will cause some Telemetry tests to stall when run'
+        ' on a headless machine (e.g. perf bot).')
+    if keychain_helper.IsKeychainLocked():
+      logging.warning('The default keychain is locked, %s', warning_suffix)
+
+    if keychain_helper.DoesKeychainHaveTimeout():
+      logging.warning('The default keychain is configured to automatically'
+          ' lock itself have a period of time, %s', warning_suffix)
+
+    chrome_acl_configured = (keychain_helper.
+        IsKeychainConfiguredForBotsWithChrome())
+    chromium_acl_configured = (keychain_helper.
+        IsKeychainConfiguredForBotsWithChromium())
+    acl_warning = ('A commonly used %s key stored in the default keychain does'
+        ' not give decryption access to all applications, %s')
+    if not chrome_acl_configured:
+      logging.warning(acl_warning, 'Chrome', warning_suffix)
+    if not chromium_acl_configured:
+      logging.warning(acl_warning, 'Chromium', warning_suffix)
+
   @classmethod
   def CustomizeBrowserOptions(cls, options):
     """Adds a browser argument that allows for the collection of keychain
     metrics. Has no effect on non-Mac platforms."""
     if sys.platform != 'darwin':
       return
+
+    KeychainMetric._CheckKeychainConfiguration()
 
     options.AppendExtraBrowserArgs(['--enable-stats-collection-bindings'])
 
