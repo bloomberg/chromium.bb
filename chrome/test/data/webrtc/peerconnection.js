c5fd5751ddc20f02c1bcbbc3956b152f1c7726ea
yukishiino <yukishiino@chromium.org>
2015-02-07 04:33:37 -0800

    webrtc: Fixes WebRtcBrowserTest to use DOM attributes correctly.
    WebRtcBrowserTest.RunsAudioVideoWebRTCCallInTwoTabs is assuming that DOM attributes are own properties on a JS obejct, which is not correct.  Blink (binding) team is working to move DOM attributes from own properties to properties on prototype chains, and then this test will break.
    
    This CL fixes the test so that it stringifies not only own properties but also attributes on prototype chains.
    
    BUG=43394
    
    Review URL: https://codereview.chromium.org/904823002
    
    Cr-Commit-Position: refs/heads/master@{#315198}
    
    Former-commit-id: e27865b72eab201b557a60b88bb648e3f8b0843f

--- chrome/test/data/webrtc/peerconnection.js
+++ chrome/test/data/webrtc/peerconnection.js
@@ -57,7 +57,7 @@ function createLocalOffer(constraints) {
         success_('createOffer');
         setLocalDescription(peerConnection, localOffer);
 
-        returnToTest('ok-' + JSON.stringify(localOffer));
+        returnToTest('ok-' + stringifyDOMObject_(localOffer));
       },
       function(error) { failure_('createOffer', error); },
       constraints);
@@ -89,7 +89,7 @@ function receiveOfferFromPeer(sessionDescJson, constraints) {
       function(answer) {
         success_('createAnswer');
         setLocalDescription(peerConnection, answer);
-        returnToTest('ok-' + JSON.stringify(answer));
+        returnToTest('ok-' + stringifyDOMObject_(answer));
       },
       function(error) { failure_('createAnswer', error); },
       constraints);
@@ -178,7 +178,7 @@ function getAllIceCandidates() {
     return;
   }
 
-  returnToTest(JSON.stringify(gIceCandidates));
+  returnToTest(stringifyDOMObject_(gIceCandidates));
 }
 
 /**
@@ -245,7 +245,7 @@ function success_(method) {
 
 /** @private */
 function failure_(method, error) {
-  throw failTest(method + '() failed: ' + JSON.stringify(error));
+  throw failTest(method + '() failed: ' + stringifyDOMObject_(error));
 }
 
 /** @private */
@@ -277,6 +277,28 @@ function addStreamCallback_(event) {
 function removeStreamCallback_(event) {
   debug('Call ended.');
   document.getElementById('remote-view').src = '';
+}
+
+/**
+ * Stringifies a DOM object.
+ *
+ * This function stringifies not only own properties but also DOM attributes
+ * which are on a prototype chain.  Note that JSON.stringify only stringifies
+ * own properties.
+ * @private
+ */
+function stringifyDOMObject_(object)
+{
+  function deepCopy(src) {
+    if (typeof src != "object")
+      return src;
+    var dst = Array.isArray(src) ? [] : {};
+    for (var property in src) {
+      dst[property] = deepCopy(src[property]);
+    }
+    return dst;
+  }
+  return JSON.stringify(deepCopy(object));
 }
 
 /**
