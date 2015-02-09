85f8db5f11dd9e9b80a497e87b9e914955d84735
sigbjornf <sigbjornf@opera.com>
2015-02-09 08:45:48 -0800

    Upgrade Blink to milliseconds-based last modified filetimes, part 4.
    Have content::FileInfoToWebFileInfo() set the temporarily Blink-unused
    field WebFileInfo::modificationTime to something milliseconds-based
    
    Once landed, this is to allow Blink to switch to no longer using
    WebFileInfo::modificationTimeMS, but modificationTime only (again.)
    
    This is the fourth patch in the following series,
    
    1: [blink] add WebFileInfo::modificationTimeMS [ https://codereview.chromium.org/873723004/ ]
    2: [chromium] fill in modificationTimeMS [ https://codereview.chromium.org/884413002/ ]
    3: [blink] make use of modificationTimeMS [ https://codereview.chromium.org/884393002/ ]
    4: [chromium] *this one* [ https://codereview.chromium.org/862203003/ ]
    5: [blink] switch to using modificationTime instead of *MS [ https://codereview.chromium.org/882343002/ ]
    6: [chromium] stop setting modificationTimeMS [ https://codereview.chromium.org/890523002/ ]
    7: [blink] remove modificationTimeMS [ https://codereview.chromium.org/869613005/ ]
    
    R=jochen
    BUG=451747
    
    Review URL: https://codereview.chromium.org/862203003
    
    Cr-Commit-Position: refs/heads/master@{#315322}
    
    Former-commit-id: 08b24b4f9ea7ad41a31347da059a35fb4ad592b3

--- content/child/file_info_util.cc
+++ content/child/file_info_util.cc
@@ -12,13 +12,13 @@ namespace content {
 void FileInfoToWebFileInfo(const base::File::Info& file_info,
                            blink::WebFileInfo* web_file_info) {
   DCHECK(web_file_info);
-  // WebKit now expects NaN as uninitialized/null Date.
+  // Blink now expects NaN as uninitialized/null Date.
   if (file_info.last_modified.is_null()) {
     web_file_info->modificationTime = std::numeric_limits<double>::quiet_NaN();
     web_file_info->modificationTimeMS =
         std::numeric_limits<double>::quiet_NaN();
   } else {
-    web_file_info->modificationTime = file_info.last_modified.ToDoubleT();
+    web_file_info->modificationTime = file_info.last_modified.ToJsTime();
     web_file_info->modificationTimeMS = file_info.last_modified.ToJsTime();
   }
   web_file_info->length = file_info.size;
