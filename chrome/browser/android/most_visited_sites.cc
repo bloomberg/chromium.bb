11ad9f2b672332d4f7c5e9520776a5ce6aba14d5
mathp <mathp@chromium.org>
2015-02-10 13:59:57 -0800

    [Most Visited] Also check if ProfileSyncService is done configuring
    There is a state where Sync is initialized but not "configured". We now query both before determining if we should verify history sync state.
    
    BUG=435663
    
    Review URL: https://codereview.chromium.org/916443004
    
    Cr-Commit-Position: refs/heads/master@{#315652}
    
    Former-commit-id: cd0b0383e0eefdc8ca7daa6f10200bc9cfc4f0f2

--- chrome/browser/android/most_visited_sites.cc
+++ chrome/browser/android/most_visited_sites.cc
@@ -182,7 +182,7 @@ SyncState GetSyncState(Profile* profile) {
     return SyncState::SYNC_OR_HISTORY_SYNC_DISABLED;
   return suggestions::GetSyncState(
       sync->IsSyncEnabledAndLoggedIn(),
-      sync->SyncActive(),
+      sync->SyncActive() && sync->ConfigurationDone(),
       sync->GetActiveDataTypes().Has(syncer::HISTORY_DELETE_DIRECTIVES));
 }
 
