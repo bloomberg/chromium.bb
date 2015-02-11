b9f49eceb59867f2976c9335a468c9199ac44945
gunsch <gunsch@chromium.org>
2015-02-11 11:30:04 -0800

    Chromecast: allow platform to add custom native bindings (take 2).
    Forgot to 'git add' on the previous CL.
    
    R=lcwu@chromium.org
    BUG=internal b/19004169
    
    Review URL: https://codereview.chromium.org/914863005
    
    Cr-Commit-Position: refs/heads/master@{#315811}
    
    Former-commit-id: 5342378a108f0a6b524f444a6819a4e199d16e06

---
+++ chromecast/renderer/cast_content_renderer_client_simple.cc
@@ -0,0 +1,14 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/renderer/cast_content_renderer_client.h"
+
+namespace chromecast {
+namespace shell {
+
+void PlatformAddRendererNativeBindings(blink::WebLocalFrame* frame) {
+}
+
+}  // namespace shell
+}  // namespace chromecast
