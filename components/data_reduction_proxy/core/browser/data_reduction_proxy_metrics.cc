6927ef2b8ec980101bb56df9702d592bd71273bd
sclittle <sclittle@chromium.org>
2015-02-11 16:12:37 -0800

    Change Net.DCL_DRPEnabled_Unknown histogram to not count bypassed bytes.
    Previously, the Net.DailyContentLength_DataReductionProxyEnabled_Unknown
    histogram counted bytes bypassed due to "Chrome-Proxy: block-once",
    bytes bypassed due to local bypass rules (e.g. http://localhost), and
    bytes bypassed due to another proxy overriding the DRP.
    
    This change reclassifies these three kinds of bypassed bytes into
    Net.DailyContentLength_DataReductionProxyEnabled_ShortBypass instead,
    since each of these kinds of bypasses last 0 seconds (i.e. affects only
    1 request).
    
    BUG=454972
    
    Review URL: https://codereview.chromium.org/917813003
    
    Cr-Commit-Position: refs/heads/master@{#315877}
    
    Former-commit-id: 8449a3be6869d9997587f04c575a1d02ef4015ab

--- components/data_reduction_proxy/core/browser/data_reduction_proxy_metrics.cc
+++ components/data_reduction_proxy/core/browser/data_reduction_proxy_metrics.cc
@@ -11,12 +11,16 @@
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_settings.h"
 #include "components/data_reduction_proxy/core/browser/data_reduction_proxy_statistics_prefs.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_headers.h"
+#include "components/data_reduction_proxy/core/common/data_reduction_proxy_params.h"
 #include "components/data_reduction_proxy/core/common/data_reduction_proxy_pref_names.h"
 #include "net/base/host_port_pair.h"
+#include "net/base/load_flags.h"
 #include "net/http/http_response_headers.h"
 #include "net/proxy/proxy_config.h"
 #include "net/proxy/proxy_retry_info.h"
+#include "net/proxy/proxy_server.h"
 #include "net/proxy/proxy_service.h"
+#include "net/url_request/url_request.h"
 #include "net/url_request/url_request_context.h"
 #include "url/url_constants.h"
 
@@ -302,6 +306,16 @@ DataReductionProxyRequestType GetDataReductionProxyRequestType(
     NOTREACHED();
     return UNKNOWN_TYPE;
   }
+
+  // Check for a Data Reduction Proxy via header before checking if proxies are
+  // bypassed, to avoid misreporting cases where the Data Reduction Proxy was
+  // bypassed between the request being sent out and the response coming in.
+  if (request.response_info().headers.get() &&
+      HasDataReductionProxyViaHeader(request.response_info().headers.get(),
+                                     NULL)) {
+    return VIA_DATA_REDUCTION_PROXY;
+  }
+
   base::TimeDelta bypass_delay;
   if (params.AreDataReductionProxiesBypassed(
           request, data_reduction_proxy_config, &bypass_delay)) {
@@ -309,11 +323,19 @@ DataReductionProxyRequestType GetDataReductionProxyRequestType(
       return LONG_BYPASS;
     return SHORT_BYPASS;
   }
-  if (request.response_info().headers.get() &&
-      HasDataReductionProxyViaHeader(request.response_info().headers.get(),
-                                     NULL)) {
-    return VIA_DATA_REDUCTION_PROXY;
+
+  // Treat bypasses that only apply to the individual request as SHORT_BYPASS.
+  // This includes bypasses triggered by "Chrome-Proxy: block-once", bypasses
+  // due to other proxies overriding the Data Reduction Proxy, and bypasses due
+  // to local bypass rules.
+  if ((request.load_flags() & net::LOAD_BYPASS_PROXY) ||
+      (!request.proxy_server().IsEmpty() &&
+       !params.IsDataReductionProxy(request.proxy_server(), NULL)) ||
+      params.IsBypassedByDataReductionProxyLocalRules(
+          request, data_reduction_proxy_config)) {
+    return SHORT_BYPASS;
   }
+
   return UNKNOWN_TYPE;
 }
 
