2f9611db5ac733dafe061957dfd9e56557631c62
achaulk <achaulk@chromium.org>
2015-02-12 13:26:01 -0800

    Selectively revert vaapi_drm_picture.cc to not use an ozone GLImage
    This changes the deallocation order which causes video corruption
    
    BUG=457381
    TEST=manual test with switch_1080p_720p.mp4
    TBR=posciak
    
    Review URL: https://codereview.chromium.org/914923004
    
    Cr-Commit-Position: refs/heads/master@{#316053}
    
    Former-commit-id: d9a24d0319c42b239f6cdf1e937ebf87b10ddc63

--- content/common/gpu/media/vaapi_drm_picture.cc
+++ content/common/gpu/media/vaapi_drm_picture.cc
@@ -102,14 +102,17 @@ bool VaapiDrmPicture::Initialize() {
   if (!make_context_current_.Run())
     return false;
 
-  gfx::ScopedTextureBinder texture_binder(GL_TEXTURE_EXTERNAL_OES,
-                                          texture_id());
-  gl_image_ = ui::GpuMemoryBufferFactoryOzoneNativeBuffer::CreateImageForPixmap(
-      pixmap_, size(), gfx::GpuMemoryBuffer::BGRA_8888, GL_RGBA);
-  if (!gl_image_) {
-    LOG(ERROR) << "Failed to create GLImage";
+  // Create an EGLImage out of the same buffer.
+  gl_image_ = new gfx::GLImageLinuxDMABuffer(size(), GL_RGBA);
+  if (!gl_image_->Initialize(base::FileDescriptor(dmabuf_fd, false),
+                             gfx::GpuMemoryBuffer::BGRA_8888, dmabuf_pitch)) {
+    LOG(ERROR) << "Failed to create a GLImageLinuxDMABuffer for a NativePixmap";
     return false;
   }
+
+  // Bind the EGLImage to the given GL texture.
+  gfx::ScopedTextureBinder texture_binder(GL_TEXTURE_EXTERNAL_OES,
+                                          texture_id());
   if (!gl_image_->BindTexImage(GL_TEXTURE_EXTERNAL_OES)) {
     LOG(ERROR) << "Failed to bind texture to GLImage";
     return false;
