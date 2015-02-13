1f484ac8a7f5e11a6372e9bc15df7057540b2b1b
rogerta <rogerta@chromium.org>
2015-02-13 11:36:11 -0800

    Make sure key::kEnableWebBasedSignin policy also turns on command line arg kEnableIframeBasedSignin.  Regression in M41.
    BUG=457625
    
    Review URL: https://codereview.chromium.org/927573002
    
    Cr-Commit-Position: refs/heads/master@{#316262}
    
    Former-commit-id: b964c203e990c51d0fc8f5c945312fd6e6455bc2

--- chrome/browser/policy/chrome_browser_policy_connector.cc
+++ chrome/browser/policy/chrome_browser_policy_connector.cc
@@ -131,9 +131,13 @@ void ChromeBrowserPolicyConnector::AppendExtraFlagPerPolicy() {
       chrome_policy.GetValue(key::kEnableWebBasedSignin);
   bool enabled = false;
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  if (policy_value && policy_value->GetAsBoolean(&enabled) && enabled &&
-      !command_line->HasSwitch(switches::kEnableWebBasedSignin)) {
-    command_line->AppendSwitch(switches::kEnableWebBasedSignin);
+  if (policy_value && policy_value->GetAsBoolean(&enabled) && enabled) {
+    if (!command_line->HasSwitch(switches::kEnableWebBasedSignin))
+      command_line->AppendSwitch(switches::kEnableWebBasedSignin);
+    // In M41, to fully enable web-based sign in, the kEnableIframeBasedSignin
+    // must also be specified.
+    if (!command_line->HasSwitch(switches::kEnableIframeBasedSignin))
+      command_line->AppendSwitch(switches::kEnableIframeBasedSignin);
   }
 }
 
