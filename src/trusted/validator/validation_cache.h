00905d531c51a6650f5dd2a485085a11307d4cdd
Mark Seaborn <mseaborn@chromium.org>
2015-02-12 17:19:03 -0800

    Cleanup: Move the definition of NaClValidationCache into src/public/
    The Chromium side uses this struct, so it should be defined in
    src/public/.  This will help us clean up the Chromium-side DEPS files.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=2832
    TEST=Tested against a Chromium trybot (linux_chromium_rel_ng)
    R=teravest@chromium.org
    
    Review URL: https://codereview.chromium.org/925643002
    
    
    Former-commit-id: df03d3e4acbfc8252774a015db7cbfb4222a473b

--- src/trusted/validator/validation_cache.h
+++ src/trusted/validator/validation_cache.h
@@ -11,53 +11,9 @@
 
 #include "native_client/src/include/nacl_base.h"
 #include "native_client/src/include/portability.h"
+#include "native_client/src/public/validation_cache.h"
 
 EXTERN_C_BEGIN
-
-struct NaClValidationMetadata;
-
-/*
- * This interface allows the validator to query a database of validation results
- * while hiding details of how the database is implemented.
- *
- * query = cache->CreateQuery(cache->handle);
- * CreateQuery: create an opaque query object, given an opaque context object.
- * The context object contains persistent variables that will be used for all
- * queries, whereas the query object contains information relevant to a single
- * validation result.
- *
- * cache->AddData(query, data, data_size);
- * AddData: add a blob of binary data to the query.  Conceptually, the query
- * will concatenate all the binary data it is given, in the order it is given,
- * and use the concatenated blob as a key to look up validation results in a
- * database.  In practice, all of the data is hashed into a reasonabally sized
- * key.  The validation cache doesn't care what data it is given, it is the
- * responsibility of the validator to provide enough information to uniquely
- * identify the validation result.  This gives flexibility to use different
- * types of keys for different validators and different sources of code.
- *
- * validation_passed = cache->QueryKnownToValidate(query);
- * QueryKnownToValidate: the key is complete, query the validation status.
- * This should only be called once.  AddData must not be called after calling
- * this function.
- *
- * cache->SetKnownToValidate(query);
- * SetKnownToValidate: set the database entry for the given key.
- * QueryKnownToValidate must be called first.
- *
- * cache->DestroyQuery(query);
- * DestroyQuery: cleanup and deallocate the query object.
- */
-
-struct NaClValidationCache {
-  void *handle;
-  void *(*CreateQuery)(void *handle);
-  void (*AddData)(void *query, const unsigned char *data, size_t length);
-  int (*QueryKnownToValidate)(void *query);
-  void (*SetKnownToValidate)(void *query);
-  void (*DestroyQuery)(void *query);
-  int (*CachingIsInexpensive)(const struct NaClValidationMetadata *metadata);
-};
 
 extern int NaClCachingIsInexpensive(struct NaClValidationCache *cache,
                                     const struct
