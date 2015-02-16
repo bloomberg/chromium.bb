03595caf833d5888916764143dee49aba3dd6ea6
fmalita <fmalita@chromium.org>
2015-02-16 13:00:41 -0800

    Remove SK_SUPPORT_LEGACY_ADDRRECT
    TBR=reed@google.com,robertphillips@google.com
    BUG=458898
    
    Review URL: https://codereview.chromium.org/931463002
    
    Cr-Commit-Position: refs/heads/master@{#316504}
    
    Former-commit-id: 2214663a56798add41d6ac09a893c09c77b328ba

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -265,10 +265,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_ADDOVAL
 #endif
 
-#ifndef SK_SUPPORT_LEGACY_ADDRRECT
-#   define SK_SUPPORT_LEGACY_ADDRRECT
-#endif
-
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
