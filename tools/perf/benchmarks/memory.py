889417b3f4f13893a8b32428d61aa3abec886b3e
petrcermak <petrcermak@chromium.org>
2015-02-18 04:44:02 -0800

    Re-enable and remove obsolete memory tests
    This patch removes the following three memory tests:
    
      memory.reload.top_desktop_sites_2012Q3_stress
      memory.tough_dom_memory_cases
      memory.typical_mobile_sites
    
    And re-enables the following memory test:
    
      memory.top_7_stress
    
    BUG=
    
    Review URL: https://codereview.chromium.org/931243002
    
    Cr-Commit-Position: refs/heads/master@{#316805}
    
    Former-commit-id: f183071967245d3ea096596adf3677c0c2aca2d6

--- tools/perf/benchmarks/memory.py
+++ tools/perf/benchmarks/memory.py
@@ -17,7 +17,6 @@ class MemoryMobile(benchmark.Benchmark):
     return 'memory.mobile_memory'
 
 
-@benchmark.Disabled  # http://crbug.com/440305
 class MemoryTop7Stress(benchmark.Benchmark):
   """Use (recorded) real world web sites and measure memory consumption."""
   test = memory.Memory
@@ -26,40 +25,3 @@ class MemoryTop7Stress(benchmark.Benchmark):
   @classmethod
   def Name(cls):
     return 'memory.top_7_stress'
-
-
-@benchmark.Disabled
-class Reload2012Q3(benchmark.Benchmark):
-  """Memory consumption for a set of top pages from 2012.
-
-  Performs reloading and garbage collecting on each page load."""
-  tag = 'reload'
-  test = memory.Memory
-  page_set = page_sets.Top2012Q3StressPageSet
-
-  @classmethod
-  def Name(cls):
-    return 'memory.reload.top_desktop_sites_2012Q3_stress'
-
-
-@benchmark.Disabled  # crbug.com/371153
-class MemoryToughDomMemoryCases(benchmark.Benchmark):
-  test = memory.Memory
-  page_set = page_sets.ToughDomMemoryCasesPageSet
-
-  @classmethod
-  def Name(cls):
-    return 'memory.tough_dom_memory_cases'
-
-
-@benchmark.Disabled('chromeos', 'linux', 'mac', 'win')
-@benchmark.Enabled('has tabs')
-class TypicalMobileSites(benchmark.Benchmark):
-  """Long memory test."""
-  test = memory.Memory
-  page_set = page_sets.TypicalMobileSitesPageSet
-  options = {'pageset_repeat': 15}
-  @classmethod
-  def Name(cls):
-    return 'memory.typical_mobile_sites'
-
