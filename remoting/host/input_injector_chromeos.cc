902cb2c054da0016449dd6f62cb164ddb7acf38b
kelvinp <kelvinp@chromium.org>
2015-02-18 14:57:01 -0800

    Fix breaking change in It2me Host on Ozone
    Due to a recent threading changes, the Ozone SystemInputInjector must be
    created on the UI thread.
    
    This CL moves the creation of the input injector and input controller on
    to the browser UI thread.
    
    BUG=458314
    
    Committed: https://crrev.com/2196b387ad3cdd660ed98019db1de230f6869078 [formerly 9c59eedd7720845a5bc93b6dbc72639305c52ea7]
    Cr-Commit-Position: refs/heads/master@{#316883}
    
    Review URL: https://codereview.chromium.org/931393003
    
    Cr-Commit-Position: refs/heads/master@{#316908}
    
    Former-commit-id: ab05ec8fc74d598df6cc5114eb9f657be54fca42

--- remoting/host/input_injector_chromeos.cc
+++ remoting/host/input_injector_chromeos.cc
@@ -46,8 +46,7 @@ ui::EventFlags MouseButtonToUIFlags(MouseEvent::MouseButton button) {
 // This class is run exclusively on the UI thread of the browser process.
 class InputInjectorChromeos::Core {
  public:
-  Core(scoped_ptr<ui::SystemInputInjector> delegate_,
-       ui::InputController* input_controller);
+  Core();
 
   // Mirrors the public InputInjectorChromeos interface.
   void InjectClipboardEvent(const ClipboardEvent& event);
@@ -74,13 +73,7 @@ class InputInjectorChromeos::Core {
   DISALLOW_COPY_AND_ASSIGN(Core);
 };
 
-InputInjectorChromeos::Core::Core(scoped_ptr<ui::SystemInputInjector> delegate,
-                                  ui::InputController* input_controller)
-    : delegate_(delegate.Pass()),
-      input_controller_(input_controller),
-      saved_auto_repeat_enabled_(false) {
-  DCHECK(delegate_);
-  DCHECK(input_controller_);
+InputInjectorChromeos::Core::Core() : saved_auto_repeat_enabled_(false) {
 }
 
 void InputInjectorChromeos::Core::InjectClipboardEvent(
@@ -156,6 +149,13 @@ void InputInjectorChromeos::Core::InjectMouseEvent(const MouseEvent& event) {
 
 void InputInjectorChromeos::Core::Start(
     scoped_ptr<protocol::ClipboardStub> client_clipboard) {
+  ui::OzonePlatform* ozone_platform = ui::OzonePlatform::GetInstance();
+  delegate_ = ozone_platform->CreateSystemInputInjector();
+  DCHECK(delegate_);
+  input_controller_ = ozone_platform->GetInputController();
+  DCHECK(input_controller_);
+
+  // Implemented by remoting::ClipboardAura.
   clipboard_ = Clipboard::Create();
   clipboard_->Start(client_clipboard.Pass());
   point_transformer_.reset(new PointTransformer());
@@ -164,9 +164,7 @@ void InputInjectorChromeos::Core::Start(
 InputInjectorChromeos::InputInjectorChromeos(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner)
     : input_task_runner_(task_runner) {
-  ui::OzonePlatform* ozone_platform = ui::OzonePlatform::GetInstance();
-  core_.reset(new Core(ozone_platform->CreateSystemInputInjector(),
-                       ozone_platform->GetInputController()));
+  core_.reset(new Core());
 }
 
 InputInjectorChromeos::~InputInjectorChromeos() {
