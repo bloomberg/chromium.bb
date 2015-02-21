6abeeb1f3a2af744775a8c302f6348c6eff249dc
jlklein <jlklein@chromium.org>
2015-02-20 17:19:02 -0800

    Add a <cr-dropdown-menu> custom element to cr_elements.
    BUG=
    
    Review URL: https://codereview.chromium.org/917893004
    
    Cr-Commit-Position: refs/heads/master@{#317473}
    
    Former-commit-id: c7d1f48530806e896c976fdbd0b878915f54bc72

---
+++ ui/webui/resources/cr_elements/cr_dropdown_menu/cr_dropdown_menu.js
@@ -0,0 +1,52 @@
+/* Copyright 2015 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file. */
+
+/**
+ * @fileoverview
+ * 'cr-dropdown-menu' is a Chrome-specific wrapper around paper-dropdown-menu.
+ * It includes a paper-dropdown so its content should just be a core-menu and
+ * items.
+ *
+ * Example:
+ *   <cr-dropdown-menu>
+ *     <core-menu>
+ *       <paper-item>Chrome</paper-item>
+ *       <paper-item>Firefox</paper-item>
+ *       <paper-item>IE</paper-item>
+ *       <paper-item>Opera</paper-item>
+ *     </core-menu>
+ *   </cr-dropdown-menu>
+ *
+ * @group Chrome Elements
+ * @element cr-dropdown-menu
+ */
+
+Polymer('cr-dropdown-menu', {
+  publish: {
+    /**
+     * True if the menu is open.
+     *
+     * @attribute opened
+     * @type boolean
+     * @default false
+     */
+    opened: false,
+
+    /**
+     * A label for the control. The label is displayed if no item is selected.
+     *
+     * @attribute label
+     * @type string
+     * @default '<Dropdown Menu Label>'
+     */
+    label: '<Dropdown Menu Label>',
+  },
+
+  /** @override */
+  domReady: function() {
+    assert(
+        this.querySelector('.menu'),
+        'cr-dropdown-menu must have a menu child with class="menu".');
+  },
+});
