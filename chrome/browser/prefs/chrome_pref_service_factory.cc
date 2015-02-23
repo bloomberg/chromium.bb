a096dc356f173b46070eb51863923a0abefb2b73
bauerb <bauerb@chromium.org>
2015-02-23 04:17:33 -0800

    Add more checks to investigate SupervisedUserPrefStore crash at startup.
    BUG=425785
    
    Review URL: https://codereview.chromium.org/938303003
    
    Cr-Commit-Position: refs/heads/master@{#317570}
    
    Former-commit-id: 6c69ca54a2a70fd265f9917417e97c95eb1c812b

--- chrome/browser/prefs/chrome_pref_service_factory.cc
+++ chrome/browser/prefs/chrome_pref_service_factory.cc
@@ -441,9 +441,12 @@ void PrepareFactory(
 
 #if defined(ENABLE_SUPERVISED_USERS)
   if (supervised_user_settings) {
-    factory->set_supervised_user_prefs(
-        make_scoped_refptr(
-            new SupervisedUserPrefStore(supervised_user_settings)));
+    scoped_refptr<PrefStore> supervised_user_prefs = make_scoped_refptr(
+        new SupervisedUserPrefStore(supervised_user_settings));
+    // TODO(bauerb): Temporary CHECK while investigating
+    // https://crbug.com/425785. Remove when that bug is fixed.
+    CHECK(async || supervised_user_prefs->IsInitializationComplete());
+    factory->set_supervised_user_prefs(supervised_user_prefs);
   }
 #endif
 
