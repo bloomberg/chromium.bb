7233bbeb9f2f034b5dd563d951a0bd0ab8bfd65d
pdr@chromium.org <pdr@chromium.org>
2015-02-23 03:35:34 +0000

    Revert of Check for an active document in WebPluginContainerImpl::windowClipRect (patchset #1 id:1 of https://codereview.chromium.org/944753004/)
    Reason for revert:
    This did not fix the crash but it gave us enough info to fix this (in a followup).
    
    Original issue's description:
    > Check for an active document in WebPluginContainerImpl::windowClipRect
    > 
    > This is a speculative fix for crbug.com/459713. The exact cause is not
    > clear in our crash reports but this change may fix an issue where the
    > document is being destroyed. A Document's m_frame is only reset
    > during destruction, after the lifecycle is set to be not active, so
    > this patch adds a check for isActive. Additionally, this patch will
    > move the crash location if the node's renderer is null.
    > 
    > BUG=459713
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=190582
    
    TBR=eae@chromium.org,esprehn@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=459713
    
    Review URL: https://codereview.chromium.org/947873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@190633 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0aa6c226a63f330f2c129fd9349146a3c616d341

--- third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
+++ third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
@@ -972,7 +972,10 @@ IntRect WebPluginContainerImpl::windowClipRect() const
     IntRect clipRect =
         convertToContainingWindow(IntRect(0, 0, width(), height()));
 
-    if (m_element->document().isActive()) {
+    // document().renderView() can be 0 when we receive messages from the
+    // plugins while we are destroying a frame.
+    // FIXME: Can we just check m_element->document().isActive() ?
+    if (m_element->renderer()->document().renderView()) {
         // Take our element and get the clip rect from the enclosing layer and
         // frame view.
         clipRect.intersect(
