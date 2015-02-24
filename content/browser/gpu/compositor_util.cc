4db31f0793ff6992a261960c26ea7a9daa690df7
jbauman <jbauman@chromium.org>
2015-02-24 14:44:20 -0800

    Revert of Use Surfaces on Windows, Linux, and Mac OS (try #5) (patchset #4 id:60001 of https://codereview.chromium.org/923833002/)
    Reason for revert:
    BUG=461491
    
    Original issue's description:
    > Use Surfaces on Windows, Linux, and Mac OS (try #5)
    >
    > Surfaces should be feature-complete, so try enabling them on Windows, Linux, and Mac OS. They're still disabled on Chrome OS because they rely on single-threaded browser compositing, and on Android because they haven't been implemented there yet.
    >
    > BUG=334090
    > TBR=piman@chromium.org
    >
    > Committed: https://crrev.com/600eede0ef809b8fe0d1d647b1dbd877d3769808 [formerly eb367c0bfe3dbac1eb7a439101afda5ffc990d65]
    > Cr-Commit-Position: refs/heads/master@{#317716}
    
    TBR=
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=334090
    
    Review URL: https://codereview.chromium.org/959493003
    
    Cr-Commit-Position: refs/heads/master@{#317906}
    
    Former-commit-id: 5883bf1d7070c79caa878e6e0f45ff717c50be73

--- content/browser/gpu/compositor_util.cc
+++ content/browser/gpu/compositor_util.cc
@@ -316,19 +316,12 @@ bool IsThreadedGpuRasterizationEnabled() {
 bool UseSurfacesEnabled() {
 #if defined(OS_ANDROID)
   return false;
-#endif
-  bool enabled = false;
-#if (defined(USE_AURA) && !defined(OS_CHROMEOS)) || defined(OS_MACOSX)
-  enabled = true;
-#endif
-
+#else
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
 
-  // Flags override.
-  enabled |= command_line.HasSwitch(switches::kUseSurfaces);
-  enabled &= !command_line.HasSwitch(switches::kDisableSurfaces);
-  return enabled;
+  return command_line.HasSwitch(switches::kUseSurfaces);
+#endif
 }
 
 int GpuRasterizationMSAASampleCount() {
