f7b0586f164f3157fae228c5e413a38d1650cada
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2015-02-24 16:29:39 +0100

    exynos: fimg2d: whitespace fix in g2d_flush
    Signed-off-by: Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>

--- exynos/exynos_fimg2d.c
+++ exynos/exynos_fimg2d.c
@@ -190,7 +190,7 @@ static int g2d_flush(struct g2d_context *ctx)
 	int ret;
 	struct drm_exynos_g2d_set_cmdlist cmdlist;
 
-	if (ctx->cmd_nr  == 0 && ctx->cmd_buf_nr == 0)
+	if (ctx->cmd_nr == 0 && ctx->cmd_buf_nr == 0)
 		return -1;
 
 	if (ctx->cmdlist_nr >= G2D_MAX_CMD_LIST_NR) {
