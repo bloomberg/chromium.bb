9a0980a23f55e68aa994116e5070db58893e14b8
Garret Kelly <gdk@chromium.org>
2015-02-24 11:19:00 -0500

    system_api: add kCryptohomeGetEndorsementInfo
    Expose the D-Bus method name for getting the EK from cryptohome through
    system_api's service constants header.
    
    BUG=chrome-os-partner:36594
    TEST=emerge-amd64-generic system_api
    
    Change-Id: I099decf00176e74258030884921a57e873501393
    Reviewed-on: https://chromium-review.googlesource.com/252710
    Reviewed-by: Darren Krahn <dkrahn@chromium.org>
    Commit-Queue: Garret Kelly <gdk@chromium.org>
    Tested-by: Garret Kelly <gdk@chromium.org>

--- dbus/service_constants.h
+++ dbus/service_constants.h
@@ -138,6 +138,7 @@ const char kCryptohomeSetBootAttribute[] = "SetBootAttribute";
 const char kCryptohomeFlushAndSignBootAttributes[] =
     "FlushAndSignBootAttributes";
 const char kCryptohomeGetLoginStatus[] = "GetLoginStatus";
+const char kCryptohomeGetEndorsementInfo[] = "GetEndorsementInfo";
 
 // Signals
 const char kSignalAsyncCallStatus[] = "AsyncCallStatus";
