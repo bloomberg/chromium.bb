ef52c98a1424848e28901649203c9f90a1bac678
yhirano <yhirano@chromium.org>
2015-02-25 07:32:38 -0800

    MidiManagerUsb should be able to accept multiple devices.
    This CL fixes MidiManagerUsb's bug which prevented the manager from
    listening input events coming from multiple MIDI devices.
    
    BUG=None
    R=toyoshim@chromium.org
    
    Review URL: https://codereview.chromium.org/943333006
    
    Cr-Commit-Position: refs/heads/master@{#318048}
    
    Former-commit-id: 34d00754b9a79bcec3099737c851a07c3cfaa7fd

--- media/midi/midi_manager_usb.cc
+++ media/midi/midi_manager_usb.cc
@@ -83,6 +83,7 @@ void MidiManagerUsb::OnEnumerateDevicesDone(bool result,
     return;
   }
   devices->swap(devices_);
+  std::vector<UsbMidiJack> input_jacks;
   for (size_t i = 0; i < devices_.size(); ++i) {
     UsbMidiDescriptorParser parser;
     std::vector<uint8> descriptor = devices_[i]->GetDescriptor();
@@ -96,7 +97,6 @@ void MidiManagerUsb::OnEnumerateDevicesDone(bool result,
       initialize_callback_.Run(MIDI_INITIALIZATION_ERROR);
       return;
     }
-    std::vector<UsbMidiJack> input_jacks;
     for (size_t j = 0; j < jacks.size(); ++j) {
       if (jacks[j].direction() == UsbMidiJack::DIRECTION_OUT) {
         output_streams_.push_back(new UsbMidiOutputStream(jacks[j]));
@@ -123,8 +123,8 @@ void MidiManagerUsb::OnEnumerateDevicesDone(bool result,
         AddInputPort(port);
       }
     }
-    input_stream_.reset(new UsbMidiInputStream(input_jacks, this));
   }
+  input_stream_.reset(new UsbMidiInputStream(input_jacks, this));
   initialize_callback_.Run(MIDI_OK);
 }
 
