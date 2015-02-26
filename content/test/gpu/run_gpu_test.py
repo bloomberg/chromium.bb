7d616d3e552f6e5d77a729bdaff8436ca672807d
eakuefner <eakuefner@chromium.org>
2015-02-25 18:51:27 -0800

    Revert of [Telemetry] Pass test_runner environment in local args instead of a global variable (patchset #9 id:160001 of https://codereview.chromium.org/942663002/)
    Reason for revert:
    crbug.com/462063
    
    Original issue's description:
    > [Telemetry] Pass test_runner environment in local args instead of a global variable
    >
    > Also adds more fields to environment to narrow the scope of
    > benchmark and user story set discovery. This should avoid problems
    > with adding Python files to unrelated directories, and hides PageTests
    > from external Telemetry benchmark runners like run_gpu_tests.py and
    > chrome_proxy's run_benchmark.
    >
    > R=dtu,nednguyen,sullivan,kbr@chromium.org,bolian
    > BUG=460181
    > TEST=tools/perf/run_benchmark; content/test/gpu/run_gpu_tests.py; tools/chrome_proxy/run_benchmark  # All return a full and correct test list.
    >
    > Committed: https://crrev.com/6f0d40e116cb2ffdc6a0d62158ffdc1db79f89af [formerly 1da5f7f70ea6dc7dd0667ea78637802c76305f5a]
    > Cr-Commit-Position: refs/heads/master@{#318149}
    
    TBR=bolian@chromium.org,dtu@chromium.org,kbr@chromium.org,nednguyen@google.com,sullivan@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=460181
    
    Review URL: https://codereview.chromium.org/955183003
    
    Cr-Commit-Position: refs/heads/master@{#318175}
    
    Former-commit-id: f29d78349447bc24719a98a80f1d90ae1f431952

--- content/test/gpu/run_gpu_test.py
+++ content/test/gpu/run_gpu_test.py
@@ -12,6 +12,7 @@ sys.path.append(os.path.join(os.path.dirname(__file__),
     os.pardir, os.pardir, os.pardir, 'tools', 'telemetry'))
 
 from telemetry import benchmark_runner
+from telemetry.core import environment
 
 
 def _LaunchDBus():
@@ -84,14 +85,12 @@ def _ShutdownDBus():
 
 
 if __name__ == '__main__':
-  top_level_dir = os.path.dirname(os.path.realpath(__file__))
-  environment = benchmark_runner.Environment(
-      top_level_dir=top_level_dir,
-      benchmark_dirs=[os.path.join(top_level_dir, 'gpu_tests')])
+  base_dir = os.path.dirname(os.path.realpath(__file__))
+  benchmark_runner.config = environment.Environment([base_dir])
 
   did_launch_dbus = _LaunchDBus()
   try:
-    retcode = benchmark_runner.main(environment)
+    retcode = benchmark_runner.main()
   finally:
     if did_launch_dbus:
       _ShutdownDBus()
