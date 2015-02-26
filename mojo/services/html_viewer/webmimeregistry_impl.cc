b94515bbab0dbc6319d36f43072ce3e1239823f1
ddorwin <ddorwin@chromium.org>
2015-02-26 10:54:06 -0800

    Remove obsolete supportsEncryptedMediaMIMEType() from html_viewer.
    This method has been removed from WebMimeRegistry.
    
    BUG=385874
    
    Review URL: https://codereview.chromium.org/937323003
    
    Cr-Commit-Position: refs/heads/master@{#318273}
    
    Former-commit-id: 82fb05447756b104d3bd2e654195784cb281bc8c

--- mojo/services/html_viewer/webmimeregistry_impl.cc
+++ mojo/services/html_viewer/webmimeregistry_impl.cc
@@ -101,30 +101,6 @@ bool WebMimeRegistryImpl::supportsMediaSourceMIMEType(
                                                      parsed_codec_ids);
 }
 
-bool WebMimeRegistryImpl::supportsEncryptedMediaMIMEType(
-    const blink::WebString& key_system,
-    const blink::WebString& mime_type,
-    const blink::WebString& codecs) {
-  // Only supports ASCII parameters.
-  if (!base::IsStringASCII(key_system) || !base::IsStringASCII(mime_type) ||
-      !base::IsStringASCII(codecs)) {
-    return false;
-  }
-
-  if (key_system.isEmpty())
-    return false;
-
-  const std::string mime_type_ascii = base::UTF16ToASCII(mime_type);
-
-  std::vector<std::string> codec_vector;
-  bool strip_suffix = !net::IsStrictMediaMimeType(mime_type_ascii);
-  net::ParseCodecString(base::UTF16ToASCII(codecs), &codec_vector,
-                        strip_suffix);
-
-  return media::IsSupportedKeySystemWithMediaMimeType(
-      mime_type_ascii, codec_vector, base::UTF16ToASCII(key_system));
-}
-
 blink::WebMimeRegistry::SupportsType
     WebMimeRegistryImpl::supportsNonImageMIMEType(
     const blink::WebString& mime_type) {
