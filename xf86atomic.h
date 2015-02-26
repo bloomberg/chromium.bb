7caa442e761ab5e48698c937aea9ce18f4522ecb
Maarten Lankhorst <maarten.lankhorst@ubuntu.com>
2015-02-26 11:54:02 +0100

    Use __sync_add_and_fetch instead of __sync_fetch_and_add for atomic_dec_and_test
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@ubuntu.com>
    Acked-By: Emil Velikov <emil.l.velikov@gmail.com>

--- xf86atomic.h
+++ xf86atomic.h
@@ -50,7 +50,7 @@ typedef struct {
 # define atomic_set(x, val) ((x)->atomic = (val))
 # define atomic_inc(x) ((void) __sync_fetch_and_add (&(x)->atomic, 1))
 # define atomic_inc_return(x) (__sync_add_and_fetch (&(x)->atomic, 1))
-# define atomic_dec_and_test(x) (__sync_fetch_and_add (&(x)->atomic, -1) == 1)
+# define atomic_dec_and_test(x) (__sync_add_and_fetch (&(x)->atomic, -1) == 0)
 # define atomic_add(x, v) ((void) __sync_add_and_fetch(&(x)->atomic, (v)))
 # define atomic_dec(x, v) ((void) __sync_sub_and_fetch(&(x)->atomic, (v)))
 # define atomic_cmpxchg(x, oldv, newv) __sync_val_compare_and_swap (&(x)->atomic, oldv, newv)
