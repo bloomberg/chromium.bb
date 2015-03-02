d7562180396d91e03d7a01f8fc43abb2f10f8e16
dgozman <dgozman@chromium.org>
2015-03-02 10:06:54 -0800

    [DevTools] Delete old toolbox on debug reload.
    BUG=none
    
    Review URL: https://codereview.chromium.org/967213003
    
    Cr-Commit-Position: refs/heads/master@{#318722}
    
    Former-commit-id: 789d213fc1ee8fed970337e888ae9c82877e03b3

--- chrome/browser/devtools/devtools_window.cc
+++ chrome/browser/devtools/devtools_window.cc
@@ -857,6 +857,8 @@ void DevToolsWindow::WebContentsCreated(WebContents* source_contents,
   if (target_url.SchemeIs(content::kChromeDevToolsScheme) &&
       target_url.path().rfind("toolbox.html") != std::string::npos) {
     CHECK(can_dock_);
+    if (toolbox_web_contents_)
+      delete toolbox_web_contents_;
     toolbox_web_contents_ = new_contents;
   }
 }
