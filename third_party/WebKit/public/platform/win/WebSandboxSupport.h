4a594f0eb1a1df3db9a27b44f1001b076377d99b
jbroman@chromium.org <jbroman@chromium.org>
2015-03-04 15:20:35 +0000

    [Win] Remove WebSandboxSupport::ensureFontLoaded.
    It has been unused for a year now, since:
      https://codereview.chromium.org/195813013
    
    BUG=463506
    
    Review URL: https://codereview.chromium.org/977563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@191288 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0df6e4b1c3ca73bace3acab5bf83626b8718738b

--- third_party/WebKit/public/platform/win/WebSandboxSupport.h
+++ third_party/WebKit/public/platform/win/WebSandboxSupport.h
@@ -31,20 +31,11 @@
 #ifndef WebSandboxSupport_h
 #define WebSandboxSupport_h
 
-typedef struct HFONT__* HFONT;
-
 namespace blink {
 
 // Put methods here that are required due to sandbox restrictions.
 class WebSandboxSupport {
 public:
-    // Sometimes a Win32 API call will fail because a font is not loaded,
-    // and due to sandbox restrictions, the current process may be unable
-    // to access the filesystem to load the font. So, this call serves as
-    // a failover to ask the embedder to try some other way to load the
-    // font (usually by delegating to an empowered process to have it load
-    // the font). Returns true if the font was successfully loaded.
-    virtual bool ensureFontLoaded(HFONT) = 0;
 };
 
 } // namespace blink
