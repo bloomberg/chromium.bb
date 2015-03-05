9be7fcc5f8a1c70f1aa79d2fd66edc2b5b1d4af3
vollick <vollick@chromium.org>
2015-03-05 09:00:34 -0800

    Disable session_restore.warm.typical_25 on XP
    The test is quite flaky and causing many failures. Disable for now.
    
    TBR=erikchen
    BUG=463677
    
    Review URL: https://codereview.chromium.org/982803002
    
    Cr-Commit-Position: refs/heads/master@{#319268}
    
    Former-commit-id: b6501276333660f626cb597a94bfb1cd9012ec1c

--- tools/perf/benchmarks/session_restore.py
+++ tools/perf/benchmarks/session_restore.py
@@ -85,7 +85,7 @@ class SessionRestoreColdTypical25(_SessionRestoreTypical25):
 
 
 # crbug.com/325479, crbug.com/381990
-@benchmark.Disabled('android', 'linux', 'reference')
+@benchmark.Disabled('android', 'linux', 'reference', 'xp')
 class SessionRestoreWarmTypical25(_SessionRestoreTypical25):
   """Test without clearing system cache or profile before repeats.
 
