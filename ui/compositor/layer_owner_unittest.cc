492357d8a7d24f5e3bc85cf840cc0d213c3aaad7
bruthig <bruthig@chromium.org>
2015-03-06 12:35:42 -0800

    Reset the root layer on the compositor when recreating a root layer.
    This is going to be used by https://codereview.chromium.org/975943002/
    
    TEST=LayerOwnerTest.RecreateRootLayerWithNullCompositor
    TEST=LayerOwnerTest.RecreateRootLayerWithCompositor
    
    BUG=337596
    
    Review URL: https://codereview.chromium.org/983933002
    
    Cr-Commit-Position: refs/heads/master@{#319487}
    
    Former-commit-id: 26d2ed6065c28b46c56a1266a6c7e28c9db55d45

--- ui/compositor/layer_owner_unittest.cc
+++ ui/compositor/layer_owner_unittest.cc
@@ -4,10 +4,14 @@
 
 #include "ui/compositor/layer_owner.h"
 
+#include "base/test/null_task_runner.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "ui/compositor/compositor.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animator.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
+#include "ui/compositor/test/context_factories_for_test.h"
+#include "ui/gfx/native_widget_types.h"
 
 namespace ui {
 
@@ -28,6 +32,37 @@ TEST(LayerOwnerTest, RecreateLayerHonorsTargetVisibilityAndOpacity) {
   scoped_ptr<Layer> old_layer(owner.RecreateLayer());
   EXPECT_FALSE(owner.layer()->visible());
   EXPECT_EQ(0.0f, owner.layer()->opacity());
+}
+
+TEST(LayerOwnerTest, RecreateRootLayerWithNullCompositor) {
+  LayerOwner owner;
+  Layer* layer = new Layer;
+  owner.SetLayer(layer);
+
+  scoped_ptr<Layer> layer_copy = owner.RecreateLayer();
+
+  EXPECT_EQ(nullptr, owner.layer()->GetCompositor());
+  EXPECT_EQ(nullptr, layer_copy->GetCompositor());
+}
+
+TEST(LayerOwnerTest, RecreateRootLayerWithCompositor) {
+  LayerOwner owner;
+  Layer* layer = new Layer;
+  owner.SetLayer(layer);
+
+  ui::ContextFactory* context_factory =
+      ui::InitializeContextFactoryForTests(false);
+  scoped_refptr<base::SingleThreadTaskRunner> task_runner =
+      new base::NullTaskRunner();
+  scoped_ptr<ui::Compositor> compositor(new ui::Compositor(
+      gfx::kNullAcceleratedWidget, context_factory, task_runner));
+  compositor->SetRootLayer(layer);
+
+  scoped_ptr<Layer> layer_copy = owner.RecreateLayer();
+
+  EXPECT_EQ(compositor.get(), owner.layer()->GetCompositor());
+  EXPECT_EQ(owner.layer(), compositor->root_layer());
+  EXPECT_EQ(nullptr, layer_copy->GetCompositor());
 }
 
 }  // namespace ui
