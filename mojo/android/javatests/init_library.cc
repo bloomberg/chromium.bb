95335343ab2df024c25f2a68a898ee11e3588b52
michaelbai <michaelbai@chromium.org>
2015-03-09 11:40:52 -0700

    Refactoring mojo android init_library
    Make it to use base::android::OnJNIOnLoad interface.
    
    BUG=447393
    
    Review URL: https://codereview.chromium.org/984163002
    
    Cr-Commit-Position: refs/heads/master@{#319700}
    
    Former-commit-id: fe321fca86e26ad616c7cb9f5b303394f22b8bb5

--- mojo/android/javatests/init_library.cc
+++ mojo/android/javatests/init_library.cc
@@ -2,10 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/android/base_jni_onload.h"
 #include "base/android/base_jni_registrar.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
-#include "base/android/library_loader/library_loader_hooks.h"
+#include "base/bind.h"
 #include "mojo/android/javatests/mojo_test_case.h"
 #include "mojo/android/javatests/validation_test_util.h"
 #include "mojo/android/system/core_impl.h"
@@ -20,28 +21,28 @@ base::android::RegistrationMethod kMojoRegisteredMethods[] = {
   { "ValidationTestUtil", mojo::android::RegisterValidationTestUtil },
 };
 
-bool RegisterMojoJni(JNIEnv* env) {
-  return RegisterNativeMethods(env, kMojoRegisteredMethods,
-                               arraysize(kMojoRegisteredMethods));
+bool RegisterJNI(JNIEnv* env) {
+  return base::android::RegisterJni(env) &&
+      RegisterNativeMethods(env, kMojoRegisteredMethods,
+                            arraysize(kMojoRegisteredMethods));
+}
+
+bool Init() {
+  mojo::embedder::Init(scoped_ptr<mojo::embedder::PlatformSupport>(
+      new mojo::embedder::SimplePlatformSupport()));
+  return true;
 }
 
 }  // namespace
 
 JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
-  base::android::InitVM(vm);
-  JNIEnv* env = base::android::AttachCurrentThread();
-
-  if (!base::android::RegisterLibraryLoaderEntryHook(env))
+  std::vector<base::android::RegisterCallback> register_callbacks;
+  register_callbacks.push_back(base::Bind(&RegisterJNI));
+  std::vector<base::android::InitCallback> init_callbacks;
+  init_callbacks.push_back(base::Bind(&Init));
+  if (!base::android::OnJNIOnLoadRegisterJNI(vm, register_callbacks) ||
+      !base::android::OnJNIOnLoadInit(init_callbacks))
     return -1;
-
-  if (!base::android::RegisterJni(env))
-    return -1;
-
-  if (!RegisterMojoJni(env))
-    return -1;
-
-  mojo::embedder::Init(scoped_ptr<mojo::embedder::PlatformSupport>(
-      new mojo::embedder::SimplePlatformSupport()));
 
   return JNI_VERSION_1_4;
 }
