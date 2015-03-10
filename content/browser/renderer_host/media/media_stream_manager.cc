f96e212106f04db698215979f898fef2ca21271f
erikchen <erikchen@chromium.org>
2015-03-10 13:35:08 -0700

    Add yet more profiling to MediaStreamManager::StartMonitoringOnUIThread.
    BUG=458404
    
    Review URL: https://codereview.chromium.org/998633002
    
    Cr-Commit-Position: refs/heads/master@{#319959}
    
    Former-commit-id: e635856043412d86aba672baad83cb949b389e98

--- content/browser/renderer_host/media/media_stream_manager.cc
+++ content/browser/renderer_host/media/media_stream_manager.cc
@@ -994,10 +994,25 @@ void MediaStreamManager::StartMonitoring() {
 #if defined(OS_MACOSX)
 void MediaStreamManager::StartMonitoringOnUIThread() {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  // TODO(erikchen): Remove ScopedTracker below once crbug.com/458404 is fixed.
+  tracked_objects::ScopedTracker tracking_profile1(
+      FROM_HERE_WITH_EXPLICIT_FUNCTION(
+          "458404 MediaStreamManager::GetBrowserMainLoop"));
   BrowserMainLoop* browser_main_loop = content::BrowserMainLoop::GetInstance();
   if (browser_main_loop) {
-    browser_main_loop->device_monitor_mac()
-        ->StartMonitoring(audio_manager_->GetWorkerTaskRunner());
+    // TODO(erikchen): Remove ScopedTracker below once crbug.com/458404 is
+    // fixed.
+    tracked_objects::ScopedTracker tracking_profile2(
+        FROM_HERE_WITH_EXPLICIT_FUNCTION(
+            "458404 MediaStreamManager::GetWorkerTaskRunner"));
+    const scoped_refptr<base::SingleThreadTaskRunner> task_runner =
+        audio_manager_->GetWorkerTaskRunner();
+    // TODO(erikchen): Remove ScopedTracker below once crbug.com/458404 is
+    // fixed.
+    tracked_objects::ScopedTracker tracking_profile3(
+        FROM_HERE_WITH_EXPLICIT_FUNCTION(
+            "458404 MediaStreamManager::DeviceMonitorMac::StartMonitoring"));
+    browser_main_loop->device_monitor_mac()->StartMonitoring(task_runner);
   }
 }
 #endif
