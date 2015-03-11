c4178ea3c70cde3aa23d23e6e91651f0d260fa32
Prathmesh Prabhu <pprabhu@chromium.org>
2015-03-10 17:14:04 -0700

    cq_stats: Fix settings typo.
    BUG=None.
    TEST=deploy to debug app instance.
    
    Change-Id: I45edb340e6ef1d0d6a09f6ddb2d40ce21c6da3d8
    Reviewed-on: https://chromium-review.googlesource.com/258510
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Commit-Queue: Prathmesh Prabhu <pprabhu@chromium.org>
    Tested-by: Prathmesh Prabhu <pprabhu@chromium.org>

--- appengine/cq_stats/cq_stats/settings.py
+++ appengine/cq_stats/cq_stats/settings.py
@@ -95,7 +95,7 @@ DATABASES = {'default': default_database}
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['chromiumos-build-annotator.googleplex.com',
-                 'chromiumos-build-annotator-dbg.googleplex.cop']
+                 'chromiumos-build-annotator-dbg.googleplex.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
