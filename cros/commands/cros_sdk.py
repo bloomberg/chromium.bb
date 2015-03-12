e8f51d0b2fc8fb084010b474ef5a9378c9b4a228
Don Garrett <dgarrett@google.com>
2015-03-12 15:39:09 -0700

    brillo sdk: Add support for associating a workspace with an SDK.
    You can now update the SDK associated with a workspace. This will
    fetch the SDK (if needed) into a standard cache directory, and the
    version will be recorded on the workspace.
    
    tot and latest are supported for workspaces, but are shared amoung all
    workspaces using them.
    
    BUG=brillo:562
    TEST=Manual testing (unittests to come).
    
    Change-Id: I8d21a402891362fdc9c7dff7ec4101f70038289d
    Reviewed-on: https://chromium-review.googlesource.com/259836
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Commit-Queue: Don Garrett <dgarrett@chromium.org>
    Tested-by: Don Garrett <dgarrett@chromium.org>

--- cros/commands/cros_sdk.py
+++ cros/commands/cros_sdk.py
@@ -12,28 +12,42 @@ from chromite import cros
 from chromite.cbuildbot import cbuildbot_config
 from chromite.cbuildbot import constants
 from chromite.cbuildbot import repository
+from chromite.lib import bootstrap_lib
 from chromite.lib import cros_build_lib
 from chromite.lib import osutils
 from chromite.lib import project_sdk
+from chromite.lib import workspace_lib
 
 
 @cros.CommandDecorator('sdk')
 class SdkCommand(cros.CrosCommand):
   """Manage Project SDK installations."""
 
-  def _UpdateProjectSdk(self, sdk_dir, version):
-    """Switch the current SDK to the specified Project SDK Manfiest and sync.
+  @classmethod
+  def _UpdateWorkspaceSdk(cls, workspace_path, version):
+    """Install specified SDK, and associate the workspace with it."""
+    bootstrap_path = bootstrap_lib.FindBootstrapPath()
+
+    if project_sdk.FindSourceRoot(bootstrap_path):
+      cros_build_lib.Die('brillo sdk must run from a git clone. brbug.com/580')
+
+    sdk_path = bootstrap_lib.ComputeSdkPath(bootstrap_path, version)
+
+    # If this version already exists, no need to reinstall it.
+    if not project_sdk.FindSourceRoot(sdk_path):
+      cls._UpdateSdk(sdk_path, version)
+
+    # Store the new version in the workspace.
+    workspace_lib.SetActiveSdkVersion(workspace_path, version)
+
+  @classmethod
+  def _UpdateSdk(cls, sdk_dir, version):
+    """Install the specified SDK at the specified location.
 
     Args:
-      sdk_dir: Directory in which to create a repo, or subdir of existing repo.
+      sdk_dir: Directory in which to create a repo.
       version: Project SDK version to sync.
     """
-    # If the specified directory is inside an existing repo, move to the root.
-    src_root = project_sdk.FindSourceRoot(sdk_dir)
-    if src_root:
-      sdk_dir = src_root
-      # TODO(brbug.com/164): Blow away chroot, if it exists.
-
     # Create the SDK dir, if it doesn't already exist.
     osutils.SafeMakedirs(sdk_dir)
 
@@ -56,24 +70,54 @@ class SdkCommand(cros.CrosCommand):
     repo.Sync()
 
   @classmethod
+  def _HandleUpdate(cls, workspace_path, sdk_dir, version):
+    if sdk_dir:
+      # Install the SDK to an explicit location.
+      cls._UpdateSdk(sdk_dir, version)
+
+    if workspace_path:
+      # If we are in a workspace, update the SDK for that workspace.
+      cls._UpdateWorkspaceSdk(workspace_path, version)
+
+  @classmethod
+  def _FindVersion(cls, workspace_path, sdk_dir):
+    if sdk_dir:
+      return project_sdk.FindVersion(sdk_dir)
+
+    if workspace_path:
+      return workspace_lib.GetActiveSdkVersion(workspace_path)
+
+    return None
+
+  @classmethod
   def AddParser(cls, parser):
     super(cls, SdkCommand).AddParser(parser)
 
-    parser.add_argument('--sdk-dir', default='.',
-                        help='Directory to install/update the Project SDK in.')
-    parser.add_argument('--update', default=None,
-                        help='Update/create Project SDK at given version.')
+    parser.add_argument(
+        '--sdk-dir', help='Force install to specific directory.')
+    parser.add_argument(
+        '--update', help='Update the SDK to version 1.2.3, tot, latest')
 
   def Run(self):
     """Run cros sdk."""
     self.options.Freeze()
 
-    if self.options.update is None:
-      version = project_sdk.FindVersion(self.options.sdk_dir)
-      if version is None:
-        cros_build_lib.Die('No valid SDK found.')
-      logging.info('Version: %s', version)
-    else:
-      logging.info('Updating to: %s', self.options.update)
-      # If we updating an existing SDK, move from nested to root dir.
-      self._UpdateProjectSdk(self.options.sdk_dir, self.options.update)
+    workspace_path = workspace_lib.WorkspacePath()
+    sdk_dir = self.options.sdk_dir
+
+    if not sdk_dir and not workspace_path:
+      cros_build_lib.Die('You must be in a workspace, or specifiy --sdk-dir.')
+
+    # Perform the update.
+    if self.options.update:
+      logging.info('Updating...')
+      self._HandleUpdate(workspace_path, sdk_dir, self.options.update)
+
+    # Find the version (possibly post-update). We re-detect as a
+    # temp hack for discovering what version 'latest' resolved as.
+    version = self._FindVersion(workspace_path, sdk_dir)
+
+    if version is None:
+      cros_build_lib.Die('No valid SDK found.')
+
+    logging.info('Version: %s', version)
---
+++ cros/commands/cros_sdk_unittest.py
@@ -0,0 +1,112 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""This module tests the brillo sdk command."""
+
+from __future__ import print_function
+
+import mock
+import os
+
+from chromite.cbuildbot import constants
+from chromite.cbuildbot import repository
+from chromite.cros.commands import cros_sdk
+from chromite.lib import bootstrap_lib
+from chromite.lib import cros_test_lib
+from chromite.lib import osutils
+from chromite.lib import project_sdk
+from chromite.lib import workspace_lib
+
+# Unittests often access internals.
+# pylint: disable=protected-access
+
+class CrosSdkTest(cros_test_lib.MockTempDirTestCase):
+  """Test class for our BuildCommand class."""
+
+  def setUp(self):
+    self.sdk_version = 'sdk_dir_version'
+    self.workspace_version = 'work_version'
+
+    self.bootstrap_path = os.path.join(self.tempdir, 'bootstrap')
+    self.sdk_path = os.path.join(self.tempdir, 'sdk')
+    self.workspace_path = os.path.join(self.tempdir, 'workspace')
+
+    # Workspace is supposed to exist in advance.
+    osutils.SafeMakedirs(self.workspace_path)
+
+    self.PatchObject(bootstrap_lib, 'FindBootstrapPath',
+                     return_value=self.bootstrap_path)
+    self.PatchObject(project_sdk, 'FindVersion',
+                     return_value=self.sdk_version)
+    self.PatchObject(workspace_lib, 'GetActiveSdkVersion',
+                     return_value=self.workspace_version)
+
+    # Prevent actually downloading a repository.
+    self.mock_repo = self.PatchObject(repository, 'RepoRepository')
+
+  def testFindVersion(self):
+    # If we only no the workspace, use the workspace version.
+    self.assertEqual(
+        self.workspace_version,
+        cros_sdk.SdkCommand._FindVersion(self.workspace_path, None))
+
+    # If we have an explicit sdk path, use it's version.
+    self.assertEqual(
+        self.sdk_version,
+        cros_sdk.SdkCommand._FindVersion(None, self.sdk_path))
+    self.assertEqual(
+        self.sdk_version,
+        cros_sdk.SdkCommand._FindVersion(self.workspace_path, self.sdk_path))
+
+  def testHandleExplicitUpdate(self):
+    cros_sdk.SdkCommand._HandleUpdate(None, self.sdk_path, '1.2.3')
+
+    # Given the explicit path and version, sync what we expect, and where.
+    expected = [mock.call(constants.EXTERNAL_GOB_URL +
+                          constants.MANIFEST_VERSIONS_SUFFIX,
+                          self.sdk_path,
+                          depth=1,
+                          manifest='project-sdk/1.2.3.xml'),
+                mock.call().Sync()]
+
+    self.assertEqual(expected, self.mock_repo.mock_calls)
+
+  def testHandleLatestUpdate(self):
+    cros_sdk.SdkCommand._HandleUpdate(None, self.sdk_path, 'latest')
+
+    # Given the explicit path and version, sync what we expect, and where.
+    expected = [mock.call(constants.EXTERNAL_GOB_URL +
+                          constants.MANIFEST_VERSIONS_SUFFIX,
+                          self.sdk_path,
+                          depth=1,
+                          manifest='project-sdk/latest.xml'),
+                mock.call().Sync()]
+
+    self.assertEqual(expected, self.mock_repo.mock_calls)
+
+  def testHandleTotUpdate(self):
+    cros_sdk.SdkCommand._HandleUpdate(None, self.sdk_path, 'tot')
+
+    # Given the explicit path and version, sync what we expect, and where.
+    expected = [mock.call(constants.MANIFEST_URL,
+                          self.sdk_path,
+                          depth=None,
+                          manifest=constants.PROJECT_MANIFEST),
+                mock.call().Sync()]
+
+    self.assertEqual(expected, self.mock_repo.mock_calls)
+
+  def testHandleWorkspaceUpdate(self):
+    cros_sdk.SdkCommand._HandleUpdate(self.workspace_path, None, '1.2.3')
+
+    # Given the explicit path and version, sync what we expect, and where.
+    expected = [mock.call(constants.EXTERNAL_GOB_URL +
+                          constants.MANIFEST_VERSIONS_SUFFIX,
+                          os.path.join(self.tempdir,
+                                       'bootstrap/sdk_checkouts/1.2.3'),
+                          depth=1,
+                          manifest='project-sdk/1.2.3.xml'),
+                mock.call().Sync()]
+
+    self.assertEqual(expected, self.mock_repo.mock_calls)
