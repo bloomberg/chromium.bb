fe63cf1fd9f079ab82b3f69773c042c3da471cf6
boliu <boliu@chromium.org>
2015-03-11 19:59:35 -0700

    base: Bump default android version to 5.1.99
    The Android Lollipop mr1 release is 5.1.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1001603003
    
    Cr-Commit-Position: refs/heads/master@{#320216}
    
    Former-commit-id: d253dc2f0a8b87ded7126500871b11daf818caf3

--- base/sys_info_android.cc
+++ base/sys_info_android.cc
@@ -60,7 +60,7 @@ namespace {
 // version to avoid unnecessarily comparison errors with the latest release.
 // This should be manually kept up-to-date on each Android release.
 const int kDefaultAndroidMajorVersion = 5;
-const int kDefaultAndroidMinorVersion = 0;
+const int kDefaultAndroidMinorVersion = 1;
 const int kDefaultAndroidBugfixVersion = 99;
 
 // Parse out the OS version numbers from the system properties.
