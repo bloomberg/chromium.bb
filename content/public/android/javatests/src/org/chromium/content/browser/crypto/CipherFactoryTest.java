96c3c94cac48399d4c124f3477b15bfa6348e71e
mariakhomenko <mariakhomenko@chromium.org>
2015-03-13 11:26:25 -0700

    Fix a flaky test.
    Also adds test size annotations to the tests in the suite.
    
    BUG=466860
    
    Review URL: https://codereview.chromium.org/1002013004
    
    Cr-Commit-Position: refs/heads/master@{#320535}
    
    Former-commit-id: 4f135a0ffe10bacb034cfd2ad726f15a25416c56

--- content/public/android/javatests/src/org/chromium/content/browser/crypto/CipherFactoryTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/crypto/CipherFactoryTest.java
@@ -6,6 +6,7 @@ package org.chromium.content.browser.crypto;
 
 import android.os.Bundle;
 import android.test.InstrumentationTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
 
 import org.chromium.base.ThreadUtils;
 import org.chromium.content.browser.crypto.CipherFactory.CipherDataObserver;
@@ -86,6 +87,7 @@ public class CipherFactoryTest extends InstrumentationTestCase {
     /**
      * {@link Cipher} instances initialized using the same parameters work in exactly the same way.
      */
+    @MediumTest
     public void testCipherUse() throws Exception {
         // Check encryption.
         Cipher aEncrypt = CipherFactory.getInstance().getCipher(Cipher.ENCRYPT_MODE);
@@ -103,6 +105,7 @@ public class CipherFactoryTest extends InstrumentationTestCase {
      * Restoring a {@link Bundle} containing the same parameters already in use by the
      * {@link CipherFactory} should keep the same keys.
      */
+    @MediumTest
     public void testSameBundleRestoration() throws Exception {
         // Create two bundles with the same saved state.
         Bundle aBundle = new Bundle();
@@ -131,6 +134,7 @@ public class CipherFactoryTest extends InstrumentationTestCase {
      * by the {@link CipherFactory} should fail. Any Ciphers created after the failed restoration
      * attempt should use the already-existing keys.
      */
+    @MediumTest
     public void testDifferentBundleRestoration() throws Exception {
         // Restore one set of parameters.
         Bundle aBundle = new Bundle();
@@ -157,6 +161,7 @@ public class CipherFactoryTest extends InstrumentationTestCase {
     /**
      * Restoration from a {@link Bundle} missing data should fail.
      */
+    @MediumTest
     public void testIncompleteBundleRestoration() throws Exception {
         // Make sure we handle the null case.
         assertFalse(CipherFactory.getInstance().restoreFromBundle(null));
@@ -179,6 +184,7 @@ public class CipherFactoryTest extends InstrumentationTestCase {
      * parameters from a {@link Bundle} before this point should result in {@link Cipher}s using the
      * restored parameters instead of any generated ones.
      */
+    @MediumTest
     public void testRestorationSucceedsBeforeCipherCreated() throws Exception {
         byte[] iv = mNumberProvider.getBytes(CipherFactory.NUM_BYTES, (byte) 50);
         byte[] key = mNumberProvider.getBytes(CipherFactory.NUM_BYTES, (byte) 100);
@@ -196,6 +202,7 @@ public class CipherFactoryTest extends InstrumentationTestCase {
      * If the {@link CipherFactory} has already generated parameters, restorations of different data
      * should fail. All {@link Cipher}s should use the generated parameters.
      */
+    @MediumTest
     public void testRestorationDiscardsAfterOtherCipherAlreadyCreated() throws Exception {
         byte[] iv = mNumberProvider.getBytes(CipherFactory.NUM_BYTES, (byte) 50);
         byte[] key = mNumberProvider.getBytes(CipherFactory.NUM_BYTES, (byte) 100);
@@ -215,6 +222,7 @@ public class CipherFactoryTest extends InstrumentationTestCase {
     /**
      * Data saved out to the {@link Bundle} should match what is held by the {@link CipherFactory}.
      */
+    @MediumTest
     public void testSavingToBundle() throws Exception {
         // Nothing should get saved out before Cipher data exists.
         Bundle initialBundle = new Bundle();
@@ -236,6 +244,7 @@ public class CipherFactoryTest extends InstrumentationTestCase {
     /**
      * Checks that an observer is notified when cipher data is created.
      */
+    @MediumTest
     public void testCipherFactoryObserver() throws Exception {
         TestCipherDataObserver observer = new TestCipherDataObserver();
         CipherFactory.getInstance().addCipherDataObserver(observer);
@@ -256,8 +265,11 @@ public class CipherFactoryTest extends InstrumentationTestCase {
      * Verifies that if the observer is attached after cipher data has already been
      * created the observer doesn't fire.
      */
+    @MediumTest
     public void testCipherFactoryObserverTooLate() throws Exception {
         CipherFactory.getInstance().getCipher(Cipher.DECRYPT_MODE);
+        // Ensures that cipher finishes initializing before running the rest of the test.
+        ThreadUtils.runOnUiThreadBlocking(mEmptyRunnable);
         TestCipherDataObserver observer = new TestCipherDataObserver();
         CipherFactory.getInstance().addCipherDataObserver(observer);
         ThreadUtils.runOnUiThreadBlocking(mEmptyRunnable);
