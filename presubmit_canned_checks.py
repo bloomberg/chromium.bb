e969cc742fdd77023d19c51bff19f2cdfb1a1662
enne@chromium.org <enne@chromium.org>
2015-03-13 18:56:54 +0000

    Fix CheckPatchFormatted message for subdirectories
    Thanks to tfarina for the code suggestion.
    
    BUG=443718
    
    Review URL: https://codereview.chromium.org/990403002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@294436 0039d316-1c4b-4281-b951-d872f2087c98

--- presubmit_canned_checks.py
+++ presubmit_canned_checks.py
@@ -1097,11 +1097,13 @@ def CheckPatchFormatted(input_api, output_api):
   cmd = ['cl', 'format', '--dry-run', input_api.PresubmitLocalPath()]
   code, _ = git_cl.RunGitWithCode(cmd, suppress_stderr=True)
   if code == 2:
+    short_path = input_api.basename(input_api.PresubmitLocalPath())
+    full_path = input_api.os_path.relpath(input_api.PresubmitLocalPath(),
+                                          input_api.change.RepositoryRoot())
     return [output_api.PresubmitPromptWarning(
       'The %s directory requires source formatting. '
       'Please run git cl format %s' %
-      (input_api.basename(input_api.PresubmitLocalPath()),
-       input_api.basename(input_api.PresubmitLocalPath())))]
+      (short_path, full_path))]
   # As this is just a warning, ignore all other errors if the user
   # happens to have a broken clang-format, doesn't use git, etc etc.
   return []
