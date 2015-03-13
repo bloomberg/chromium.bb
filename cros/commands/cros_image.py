198359a10fbf64dde356072ca631cc03adea52fc
Steve Fung <stevefung@chromium.org>
2015-03-13 16:32:50 -0700

    brillo image: map command to build_image
    As a first step of building a brillo image, map the `brillo image`
    command to make use of the existing build_image script.
    
    BUG=brillo:568
    TEST=./cros_image_unittest
    TEST=`brillo image --board=panther --enable_rootfs_verification=False`
    CQ-DEPEND=CL:260479
    
    Change-Id: I1dd21f3a39575db9aaf7e0e82b28533071887d39
    Reviewed-on: https://chromium-review.googlesource.com/260077
    Reviewed-by: David Pursell <dpursell@chromium.org>
    Commit-Queue: Steve Fung <stevefung@chromium.org>
    Tested-by: Steve Fung <stevefung@chromium.org>

--- cros/commands/cros_image.py
+++ cros/commands/cros_image.py
@@ -2,26 +2,88 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""This module is currently an example Cros command."""
+"""Build an image."""
 
 from __future__ import print_function
 
-from chromite.lib import cros_build_lib
+import os
 
+from chromite.cbuildbot import constants
+from chromite.lib import cros_build_lib
 from chromite import cros
+
+
+IMAGE_TYPES = ['base', 'dev', 'test', 'factory_test', 'factory_install', []]
 
 
 @cros.CommandDecorator('image')
 class ImageCommand(cros.CrosCommand):
-  """Currently an example command.
+  """Create an image
 
-  For more information see cros.CrosCommand.
+  Creates an image from the specified board.
   """
   @classmethod
   def AddParser(cls, parser):
     super(ImageCommand, cls).AddParser(parser)
-    parser.set_defaults(usage='Example image help')
-    parser.add_argument('--myoption', help='Example option')
+    parser.set_defaults(usage='Create an image')
+    parser.add_argument('--adjust_part', help="Adjustments to apply to "
+                        "partition table (LABEL:[+-=]SIZE) e.g. ROOT-A:+1G.")
+    parser.add_argument('--board', help="The board to build an image for.")
+    parser.add_argument('--boot_args', help="Additional boot arguments to pass "
+                        "to the commandline")
+    parser.add_argument('--enable_bootcache', default=False, type='bool',
+                        help="Default all bootloaders to use boot cache.")
+    parser.add_argument('--enable_rootfs_verification', default=True,
+                        type='bool', help="Default all bootloaders to use "
+                        "kernel-based root fs integrity checking.")
+    parser.add_argument('--output_root', type='path', help="Directory in which "
+                        "to place image result directories (named by version)")
+    parser.add_argument('--disk_layout',
+                        help="The disk layout type to use for this image.")
+    parser.add_argument('--enable_serial', help="Enable serial port for "
+                        "printk() calls. Example values: ttyS0")
+    parser.add_argument('--kernel_log_level', default=7, type=int,
+                        help="The log level to add to the kernel command line.")
+    parser.add_argument('image_types', nargs='*', choices=IMAGE_TYPES,
+                        default=None, help="The image types to build.")
 
   def Run(self):
-    cros_build_lib.Info('My options are %r', self.options)
+    self.options.Freeze()
+
+    cmd = [os.path.join(constants.CROSUTILS_DIR, 'build_image')]
+
+    if self.options.board:
+      cmd.append('--board=%s' % self.options.board)
+
+    if self.options.adjust_part:
+      cmd.append('--adjust_part=%s' % self.options.adjust_part)
+
+    if self.options.boot_args:
+      cmd.append('--boot_args=%s' % self.options.boot_args)
+
+    if self.options.enable_bootcache:
+      cmd.append('--enable_bootcache')
+    else:
+      cmd.append('--noenable_bootcache')
+
+    if self.options.enable_rootfs_verification:
+      cmd.append('--enable_rootfs_verification')
+    else:
+      cmd.append('--noenable_rootfs_verification')
+
+    if self.options.output_root:
+      cmd.append('--output_root=%s' % self.options.output_root)
+
+    if self.options.disk_layout:
+      cmd.append('--disk_layout=%s' % self.options.disk_layout)
+
+    if self.options.enable_serial:
+      cmd.append('--enable_serial=%s' % self.options.enable_serial)
+
+    if self.options.kernel_log_level:
+      cmd.append('--loglevel=%s' % self.options.kernel_log_level)
+
+    if self.options.image_types:
+      cmd.extend(self.options.image_types)
+
+    cros_build_lib.RunCommand(cmd)
--- cros/commands/cros_image_unittest.py
+++ cros/commands/cros_image_unittest.py
@@ -6,22 +6,142 @@
 
 from __future__ import print_function
 
-import argparse
-
 from chromite.cros.commands import cros_image
+from chromite.lib import commandline
 from chromite.lib import cros_test_lib
 
 
 class ImageCommandTest(cros_test_lib.TestCase):
   """Test class for our ImageCommand class."""
 
-  def testParser(self):
-    """Tests that our example parser works as expected."""
-    fake_parser = argparse.ArgumentParser()
+  def testParserDefaults(self):
+    """Tests that the parser's default values are correct."""
+    fake_parser = commandline.ArgumentParser()
     fake_subparser = fake_parser.add_subparsers()
     image_parser = fake_subparser.add_parser('image')
     cros_image.ImageCommand.AddParser(image_parser)
 
-    options = fake_parser.parse_args(['image', '--myoption=awesome'])
+    options = fake_parser.parse_args(['image'])
     instance = options.cros_class(options)
-    self.assertEqual(instance.options.myoption, 'awesome')
+    self.assertEqual(instance.options.adjust_part, None)
+    self.assertEqual(instance.options.board, None)
+    self.assertEqual(instance.options.boot_args, None)
+    self.assertFalse(instance.options.enable_bootcache, False)
+    self.assertTrue(instance.options.enable_rootfs_verification, True)
+    self.assertEqual(instance.options.output_root, None)
+    self.assertEqual(instance.options.disk_layout, None)
+    self.assertEqual(instance.options.enable_serial, None)
+    self.assertEqual(instance.options.kernel_log_level, 7)
+    self.assertEqual(instance.options.image_types, [])
+
+  def testParserSetValues(self):
+    """Tests that the parser reads in values correctly."""
+    fake_parser = commandline.ArgumentParser()
+    fake_subparser = fake_parser.add_subparsers()
+    image_parser = fake_subparser.add_parser('image')
+    cros_image.ImageCommand.AddParser(image_parser)
+
+    options = fake_parser.parse_args(['image',
+                                      '--adjust_part=PARTX',
+                                      '--board=fooboard',
+                                      '--boot_args=bar',
+                                      '--enable_bootcache=True',
+                                      '--enable_rootfs_verification=False',
+                                      '--output_root=/foo/bar/baz',
+                                      '--disk_layout=fooboard/layout.conf',
+                                      '--enable_serial=ttyS0',
+                                      '--kernel_log_level=2',
+                                      'dev', 'test'])
+    instance = options.cros_class(options)
+    self.assertEqual(instance.options.adjust_part, 'PARTX')
+    self.assertEqual(instance.options.board, 'fooboard')
+    self.assertEqual(instance.options.boot_args, 'bar')
+    self.assertTrue(instance.options.enable_bootcache)
+    self.assertFalse(instance.options.enable_rootfs_verification)
+    self.assertEqual(instance.options.output_root, '/foo/bar/baz')
+    self.assertEqual(instance.options.disk_layout, 'fooboard/layout.conf')
+    self.assertEqual(instance.options.enable_serial, 'ttyS0')
+    self.assertEqual(instance.options.kernel_log_level, 2)
+    self.assertEqual(instance.options.image_types, ['dev', 'test'])
+
+  def testParserSetValuesDefaults(self):
+    """Tests that the parser reads in values correctly when set to defaults."""
+    fake_parser = commandline.ArgumentParser()
+    fake_subparser = fake_parser.add_subparsers()
+    image_parser = fake_subparser.add_parser('image')
+    cros_image.ImageCommand.AddParser(image_parser)
+
+    options = fake_parser.parse_args(['image',
+                                      '--adjust_part=PARTX',
+                                      '--board=fooboard',
+                                      '--boot_args=bar',
+                                      '--enable_bootcache=False',
+                                      '--enable_rootfs_verification=True',
+                                      '--output_root=/foo/bar/baz',
+                                      '--disk_layout=fooboard/layout.conf',
+                                      '--enable_serial=ttyS0',
+                                      '--kernel_log_level=7',
+                                      'dev', 'test'])
+    instance = options.cros_class(options)
+    self.assertEqual(instance.options.adjust_part, 'PARTX')
+    self.assertEqual(instance.options.board, 'fooboard')
+    self.assertEqual(instance.options.boot_args, 'bar')
+    self.assertFalse(instance.options.enable_bootcache)
+    self.assertTrue(instance.options.enable_rootfs_verification)
+    self.assertEqual(instance.options.output_root, '/foo/bar/baz')
+    self.assertEqual(instance.options.disk_layout, 'fooboard/layout.conf')
+    self.assertEqual(instance.options.enable_serial, 'ttyS0')
+    self.assertEqual(instance.options.kernel_log_level, 7)
+    self.assertEqual(instance.options.image_types, ['dev', 'test'])
+
+  def testParserSetBooleansTrue(self):
+    """Tests that the parser reads in booleans correctly when set to True."""
+    fake_parser = commandline.ArgumentParser()
+    fake_subparser = fake_parser.add_subparsers()
+    image_parser = fake_subparser.add_parser('image')
+    cros_image.ImageCommand.AddParser(image_parser)
+
+    options = fake_parser.parse_args(['image',
+                                      '--board=fooboard',
+                                      '--enable_bootcache=True',
+                                      '--enable_rootfs_verification=True',
+                                      'dev', 'test'])
+    instance = options.cros_class(options)
+    self.assertEqual(instance.options.board, 'fooboard')
+    self.assertTrue(instance.options.enable_bootcache)
+    self.assertTrue(instance.options.enable_rootfs_verification)
+    self.assertEqual(instance.options.image_types, ['dev', 'test'])
+
+  def testParserSetBooleansFalse(self):
+    """Tests that the parser reads in booleans correctly when set to False."""
+    fake_parser = commandline.ArgumentParser()
+    fake_subparser = fake_parser.add_subparsers()
+    image_parser = fake_subparser.add_parser('image')
+    cros_image.ImageCommand.AddParser(image_parser)
+
+    options = fake_parser.parse_args(['image',
+                                      '--board=fooboard',
+                                      '--enable_bootcache=False',
+                                      '--enable_rootfs_verification=False',
+                                      'dev', 'test'])
+    instance = options.cros_class(options)
+    self.assertEqual(instance.options.board, 'fooboard')
+    self.assertFalse(instance.options.enable_bootcache)
+    self.assertFalse(instance.options.enable_rootfs_verification)
+    self.assertEqual(instance.options.image_types, ['dev', 'test'])
+
+  def testParserBooleanDefaults(self):
+    """Tests that the parser sets booleans to defaults when they are omitted."""
+    fake_parser = commandline.ArgumentParser()
+    fake_subparser = fake_parser.add_subparsers()
+    image_parser = fake_subparser.add_parser('image')
+    cros_image.ImageCommand.AddParser(image_parser)
+
+    options = fake_parser.parse_args(['image',
+                                      '--board=fooboard',
+                                      'dev', 'test'])
+    instance = options.cros_class(options)
+    self.assertEqual(instance.options.board, 'fooboard')
+    self.assertFalse(instance.options.enable_bootcache)
+    self.assertTrue(instance.options.enable_rootfs_verification)
+    self.assertEqual(instance.options.image_types, ['dev', 'test'])
