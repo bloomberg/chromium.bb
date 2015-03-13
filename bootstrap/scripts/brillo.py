c06e75709a39cf34f75ffe0b6bbb4f6bb1174634
Don Garrett <dgarrett@google.com>
2015-03-13 12:10:43 -0700

    bootstrap/brillo: Brillo wrapper understands workspaces.
    If you are using the bootstrap wrapper in a workspace that is
    associated with an SDK, the bootstrap will run the command in the
    proper SDK.
    
    If you are using a repo, things are left along.
    
    If you are running "brillo sdk" the command will be run from the
    boostrap location.
    
    All of this only works if you are using the 'brillo' command, and if
    you are running it from 'chromite/bootstrap'. That's in your path,
    right?
    
    BUG=brillo:561
    TEST=Unitests and some manual.
    
    Change-Id: Ie7c0453de198b9ede7dbb175cdf08f20741f94b1
    Reviewed-on: https://chromium-review.googlesource.com/260023
    Trybot-Ready: Don Garrett <dgarrett@chromium.org>
    Tested-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Gilad Arnold <garnold@chromium.org>
    Reviewed-by: Bertrand Simonnet <bsimonnet@chromium.org>
    Commit-Queue: Don Garrett <dgarrett@chromium.org>

--- bootstrap/scripts/brillo.py
+++ bootstrap/scripts/brillo.py
@@ -16,20 +16,39 @@ from __future__ import print_function
 
 import os
 
+from chromite.lib import bootstrap_lib
 from chromite.lib import git
+from chromite.lib import workspace_lib
 
 
-def main(args):
+def LocateBrilloCommand(args):
+  relative_cmd = os.path.join('chromite', 'bin', 'brillo')
+  bootstrap_path = bootstrap_lib.FindBootstrapPath()
+
   if len(args) >= 1 and args[0] == 'sdk':
     # Run 'brillo sdk' from the repository containing this command.
-    bootstrap_dir = os.path.dirname(os.path.abspath(__file__))
-    bin_cmd = os.path.join(bootstrap_dir, '..', '..', 'bin', 'brillo')
-  else:
-    # Run all other commands from 'brillo' wrapper in repo detected via CWD.
-    repo_root = git.FindRepoCheckoutRoot(os.getcwd())
-    if repo_root is None:
-      print('Unable to detect which SDK you want to use.')
-      return 1
-    bin_cmd = os.path.join(repo_root, 'chromite', 'bin', 'brillo')
+    return os.path.join(bootstrap_path, relative_cmd)
+
+  # If we are in a workspace, and the workspace has an associated SDK, use it.
+  workspace_path = workspace_lib.WorkspacePath()
+  if workspace_path:
+    sdk_path = bootstrap_lib.GetActiveSdkPath(bootstrap_path, workspace_path)
+    # Use SDK associated with workspace, or nothing.
+    return os.path.join(sdk_path, relative_cmd) if sdk_path else None
+
+  # Run all other commands from 'brillo' wrapper in repo detected via CWD.
+  repo_root = git.FindRepoCheckoutRoot(os.getcwd())
+  if repo_root:
+    return os.path.join(repo_root, relative_cmd)
+
+  # Couldn't find the real brillo command to run.
+  return None
+
+def main(args):
+  bin_cmd = LocateBrilloCommand(args)
+
+  if bin_cmd is None:
+    print('Unable to detect which SDK you want to use.')
+    return 1
 
   os.execv(bin_cmd, [bin_cmd] + args)
--- bootstrap/scripts/brillo_unittest.py
+++ bootstrap/scripts/brillo_unittest.py
@@ -7,44 +7,147 @@
 from __future__ import print_function
 
 import mock
+import os
 
+from chromite.lib import bootstrap_lib
 from chromite.lib import cros_test_lib
 from chromite.lib import git
+from chromite.lib import workspace_lib
 
 from chromite.bootstrap.scripts import brillo
 
 
-class TestBootstrapBrilloCmd(cros_test_lib.TestCase):
+class TestBootstrapBrilloCmd(cros_test_lib.MockTestCase):
   """Tests for the bootstrap brillo command."""
 
-  @mock.patch.object(git, 'FindRepoCheckoutRoot',
-                     autospec=True, return_value='/repo')
-  @mock.patch('os.execv', autospec=True)
-  def _wrapperHelper(self, args, expectedInRepo, mock_exec, _mock_root):
+  def setUp(self):
+    # Make certain we never exec anything.
+    self.mock_exec = self.PatchObject(os, 'execv', autospec=True)
+
+    # Define some directories that might be looked up.
+    self.mock_bootstrap_path = self.PatchObject(
+        bootstrap_lib, 'FindBootstrapPath', autospec=True,
+        return_value='/bootstrap')
+
+    self.mock_workspace_path = self.PatchObject(
+        workspace_lib, 'WorkspacePath', autospec=True)
+
+    self.mock_workspace_active_path = self.PatchObject(
+        bootstrap_lib, 'GetActiveSdkPath', autospec=True)
+
+    self.mock_repo_root = self.PatchObject(
+        git, 'FindRepoCheckoutRoot', autospec=True)
+
+  def _LocateCmdTester(self, args, expected):
     """Helper for testing what brillo.main execs."""
-    brillo.main(args)
+    result = brillo.LocateBrilloCommand(args)
+    self.assertEqual(expected, result)
 
-    self.assertEqual(len(mock_exec.call_args_list), 1)
-    pos_args, kw_args = mock_exec.call_args_list[0]
-    binary, arguments = pos_args
+  def _verifyLocateBrilloCommand(self, expected):
+    # We should always use the bootstrap brillo command for 'sdk'.
+    self.assertEqual(expected,
+                     brillo.LocateBrilloCommand(['flash']))
+    self.assertEqual(expected,
+                     brillo.LocateBrilloCommand(['flash', '--help']))
 
-    if expectedInRepo:
-      self.assertEqual(binary, '/repo/chromite/bin/brillo')
-    else:
-      self.assertTrue(binary.endswith, '../../bin/brillo')
+  def _verifyLocateBrilloCommandSpecialSdkHandling(self):
+    # We should always use the bootstrap brillo command for 'sdk'.
+    self.assertEqual('/bootstrap/chromite/bin/brillo',
+                     brillo.LocateBrilloCommand(['sdk']))
+    self.assertEqual('/bootstrap/chromite/bin/brillo',
+                     brillo.LocateBrilloCommand(['sdk', '--help']))
 
-    self.assertEqual(arguments, [binary] + args)
-    self.assertEqual(kw_args, {})
-
-  def testSdkInGitRepo(self):
+  def testCommandLookupActiveWorkspace(self):
     """Test that sdk commands are run in the Git Repository."""
-    self._wrapperHelper(['sdk'], False)
-    self._wrapperHelper(['sdk', '--help'], False)
-    self._wrapperHelper(['sdk', 'millium', 'shrimp', 'and', 'hand'], False)
+    self.mock_workspace_path.return_value = '/workspace'
+    self.mock_workspace_active_path.return_value = '/sdk_roots/1.2.3'
+    self.mock_repo_root.return_value = None
 
-  def testBrilloCommandInRepository(self):
-    """Test that non-sdk commands are run in the Repo checkout."""
-    self._wrapperHelper([], True)
-    self._wrapperHelper(['flash'], True)
-    self._wrapperHelper(['flash', '--help'], True)
-    self._wrapperHelper(['super', 'long', 'arg', 'list', 'of', 'stuff'], True)
+    self._verifyLocateBrilloCommand('/sdk_roots/1.2.3/chromite/bin/brillo')
+    self._verifyLocateBrilloCommandSpecialSdkHandling()
+
+    # Having a repo root shouldn't affect the result.
+    self.mock_repo_root.return_value = '/repo'
+
+    self._verifyLocateBrilloCommand('/sdk_roots/1.2.3/chromite/bin/brillo')
+    self._verifyLocateBrilloCommandSpecialSdkHandling()
+
+  def testCommandLookupInactiveWorkspace(self):
+    """Test that sdk commands are run in the Git Repository."""
+    self.mock_workspace_path.return_value = '/workspace'
+    self.mock_workspace_active_path.return_value = None
+    self.mock_repo_root.return_value = None
+
+    self._verifyLocateBrilloCommand(None)
+    self._verifyLocateBrilloCommandSpecialSdkHandling()
+
+    # Having a repo root shouldn't affect the result.
+    self.mock_repo_root.return_value = '/repo'
+
+    self._verifyLocateBrilloCommand(None)
+    self._verifyLocateBrilloCommandSpecialSdkHandling()
+
+  def testCommandLookupRepo(self):
+    """Test that sdk commands are run in the Git Repository."""
+    self.mock_workspace_path.return_value = None
+    self.mock_workspace_active_path.return_value = None
+    self.mock_repo_root.return_value = '/repo'
+
+    self._verifyLocateBrilloCommand('/repo/chromite/bin/brillo')
+    self._verifyLocateBrilloCommandSpecialSdkHandling()
+
+  def testCommandLookupBootstrapOnly(self):
+    """Test that sdk commands are run in the Git Repository."""
+    self.mock_workspace_path.return_value = None
+    self.mock_workspace_active_path.return_value = None
+    self.mock_repo_root.return_value = None
+
+    self._verifyLocateBrilloCommand(None)
+    self._verifyLocateBrilloCommandSpecialSdkHandling()
+
+  def testMainInActiveWorkspace(self):
+    self.mock_workspace_path.return_value = '/workspace'
+    self.mock_workspace_active_path.return_value = '/sdk_roots/1.2.3'
+    self.mock_repo_root.return_value = None
+
+    brillo.main(['flash', '--help'])
+
+    expected_cmd = '/sdk_roots/1.2.3/chromite/bin/brillo'
+
+    self.assertEqual(
+        [mock.call(expected_cmd, [expected_cmd, 'flash', '--help'])],
+        self.mock_exec.call_args_list)
+
+  def testMainInRepo(self):
+    self.mock_workspace_path.return_value = None
+    self.mock_workspace_active_path.return_value = None
+    self.mock_repo_root.return_value = '/repo'
+
+    brillo.main(['flash', '--help'])
+
+    expected_cmd = '/repo/chromite/bin/brillo'
+
+    self.assertEqual(
+        [mock.call(expected_cmd, [expected_cmd, 'flash', '--help'])],
+        self.mock_exec.call_args_list)
+
+  def testMainNoCmd(self):
+    self.mock_workspace_path.return_value = None
+    self.mock_workspace_active_path.return_value = None
+    self.mock_repo_root.return_value = None
+
+    self.assertEqual(1, brillo.main(['flash', '--help']))
+    self.assertEqual([], self.mock_exec.call_args_list)
+
+  def testMainSdkCmd(self):
+    self.mock_workspace_path.return_value = None
+    self.mock_workspace_active_path.return_value = None
+    self.mock_repo_root.return_value = None
+
+    brillo.main(['sdk', '--help'])
+
+    expected_cmd = '/bootstrap/chromite/bin/brillo'
+
+    self.assertEqual(
+        [mock.call(expected_cmd, [expected_cmd, 'sdk', '--help'])],
+        self.mock_exec.call_args_list)
