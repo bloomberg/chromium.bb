9b726d60626469056bc300eb4e9bd776c066e97e
reed <reed@chromium.org>
2015-03-15 14:24:37 -0700

    add guard SK_SUPPORT_LEGACY_FILTERLEVEL_ENUM for new skia api
    BUG=
    TBR=fmalita@chromium.org
    
    Review URL: https://codereview.chromium.org/1012533002
    
    Cr-Commit-Position: refs/heads/master@{#320687}
    
    Former-commit-id: 30599ab53a436da9c1f3ba83b11d3d74c74e7084

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -257,6 +257,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_IGNORE_ETC1_SUPPORT
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_FILTERLEVEL_ENUM
+#   define SK_SUPPORT_LEGACY_FILTERLEVEL_ENUM
+#endif
+
 #ifndef    SK_IGNORE_GPU_DITHER
 #   define SK_IGNORE_GPU_DITHER
 #endif
