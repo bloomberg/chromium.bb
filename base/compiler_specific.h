469893109d74395548ae3139e3b271ae4db63c14
thestig <thestig@chromium.org>
2015-03-16 15:36:55 -0700

    Revert of Revert of MSan: check data is initialized when serializing with base::Pickle. (patchset #1 id:1 of https://codereview.chromium.org/985893002/)
    Reason for revert:
    The bugs that caused the revert have been fixed - Unreverting.
    
    Original issue's description:
    > Revert of MSan: check data is initialized when serializing with base::Pickle. (patchset #1 id:1 of https://codereview.chromium.org/988693004/)
    >
    > Reason for revert:
    > Found errors w.r.t. IPC messages. Reverting for now while people fix the errors.
    >
    > Original issue's description:
    > > MSan: check data is initialized when serializing with base::Pickle.
    > >
    > > Serialization implies that the data is going to leave the process one way or
    > > another, and it's never a good idea to leak uninitialized bytes. In most cases
    > > MSan would catch this later on (e.g. on a write()). There are exceptions to
    > > that, however, such as writing to shared memory. It doesn't hurt to have an
    > > extra check here.
    > >
    > > BUG=463906
    > > R=thakis@chromium.org
    > >
    > > Committed: https://crrev.com/0173b76690952b09ad728968634a0142906a5839 [formerly 2c705b038413d30d7aae5ff5d09882575e93c898]
    > > Cr-Commit-Position: refs/heads/master@{#319459}
    >
    > TBR=thakis@chromium.org,earthdok@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=463906
    >
    > Committed: https://crrev.com/3b8b2f88cacb6c8894a1422f9fff8cf5c14ef9d7 [formerly d74906086c38b72bed2017a772c67f3c3fde2709]
    > Cr-Commit-Position: refs/heads/master@{#319510}
    
    TBR=thakis@chromium.org,earthdok@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=463906
    
    Review URL: https://codereview.chromium.org/1008393004
    
    Cr-Commit-Position: refs/heads/master@{#320808}
    
    Former-commit-id: 1a42b4077dd8b49ef92c5f9a517eeeb9bfc3be88

--- base/compiler_specific.h
+++ base/compiler_specific.h
@@ -175,9 +175,17 @@
 // Mark a memory region fully initialized.
 // Use this to annotate code that deliberately reads uninitialized data, for
 // example a GC scavenging root set pointers from the stack.
-#define MSAN_UNPOISON(p, s)  __msan_unpoison(p, s)
+#define MSAN_UNPOISON(p, size)  __msan_unpoison(p, size)
+
+// Check a memory region for initializedness, as if it was being used here.
+// If any bits are uninitialized, crash with an MSan report.
+// Use this to sanitize data which MSan won't be able to track, e.g. before
+// passing data to another process via shared memory.
+#define MSAN_CHECK_MEM_IS_INITIALIZED(p, size) \
+    __msan_check_mem_is_initialized(p, size)
 #else  // MEMORY_SANITIZER
-#define MSAN_UNPOISON(p, s)
+#define MSAN_UNPOISON(p, size)
+#define MSAN_CHECK_MEM_IS_INITIALIZED(p, size)
 #endif  // MEMORY_SANITIZER
 
 // Macro useful for writing cross-platform function pointers.
