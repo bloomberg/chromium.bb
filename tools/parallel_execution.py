bloomberg/chromium.bb

commit 3c979cb54e46d986f0b54be1e47a8103e6514e6c

Switch most paths to be unicode and enforce it.
Why? Because passing a str instance to any python function calling the Win32 API
will use the ANSI version *A(), which is locked to the current code page. Using
an unicode instace will make the stdlib use the the unicode version instead
*W(). This is necessary with path with non ASCII characters in them.

Doing this change only for Windows would not be practical and it also helps in
other platforms, as the default encoding on other platforms is ASCII instead of
ANSI (effectively, a random code page). This also mean potential incompatibility
with unicde paths. Note that it may not always be ASCII, it could be raw UTF-8.
Its interpretation will depend on the phase of the moon and $LANG.

R=vadimsh@chromium.org
BUG=

Review URL: https://codereview.appspot.com/212340043

tools/parallel_execution.py
@@ -42,7 +42,7 @@ def capture(cmd):
 
 def trigger(swarming_server, isolate_server, task_name, isolated_hash, args):
   """Triggers a specified .isolated file."""
-  fd, jsonfile = tempfile.mkstemp(prefix='swarming')
+  fd, jsonfile = tempfile.mkstemp(prefix=u'swarming')
   os.close(fd)
   try:
     cmd = [

example/1_isolate_server.py
@@ -23,7 +23,7 @@ import common
 
 def main():
   options = common.parse_args(use_isolate_server=True, use_swarming=False)
-  tempdir = tempfile.mkdtemp(prefix='hello_world')
+  tempdir = tempfile.mkdtemp(prefix=u'hello_world')
   try:
     # All the files are put in a temporary directory. This is optional and
     # simply done so the current directory doesn't have the following files

example/2_swarming_run_auto_upload.py
@@ -23,7 +23,7 @@ import common
 
 def main():
   options = common.parse_args(use_isolate_server=True, use_swarming=True)
-  tempdir = tempfile.mkdtemp(prefix='hello_world')
+  tempdir = tempfile.mkdtemp(prefix=u'hello_world')
   try:
     # All the files are put in a temporary directory. This is optional and
     # simply done so the current directory doesn't have the following files

example/3_swarming_trigger_collect.py
@@ -25,7 +25,7 @@ import common
 def main():
   options = common.parse_args(use_isolate_server=True, use_swarming=True)
   try:
-    tempdir = tempfile.mkdtemp(prefix='hello_world')
+    tempdir = tempfile.mkdtemp(prefix=u'hello_world')
     try:
       # All the files are put in a temporary directory. This is optional and
       # simply done so the current directory doesn't have the following files

example/4_swarming_run_manual_upload.py
@@ -41,7 +41,7 @@ def main():
     common.note(
         'Archiving custom .isolated file to %s' % options.isolate_server)
     handle, isolated = tempfile.mkstemp(
-        prefix='hello_world', suffix='.isolated')
+        prefix=u'hello_world', suffix=u'.isolated')
     os.close(handle)
     try:
       data = {

isolate.py
@@ -1004,7 +1004,7 @@ def CMDrun(parser, args):
   cmd = tools.fix_python_path(cmd)
 
   outdir = run_isolated.make_temp_dir(
-      'isolate-%s' % datetime.date.today(),
+      u'isolate-%s' % datetime.date.today(),
       os.path.dirname(complete_state.root_dir))
   try:
     # TODO(maruel): Use run_isolated.run_tha_test().

isolateserver.py
@@ -1946,8 +1946,8 @@ def archive_files_to_storage(storage, files, blacklist):
 
           # Create the .isolated file.
           if not tempdir:
-            tempdir = tempfile.mkdtemp(prefix='isolateserver')
-          handle, isolated = tempfile.mkstemp(dir=tempdir, suffix='.isolated')
+            tempdir = tempfile.mkdtemp(prefix=u'isolateserver')
+          handle, isolated = tempfile.mkstemp(dir=tempdir, suffix=u'.isolated')
           os.close(handle)
           data = {
               'algo':
@@ -2166,7 +2166,7 @@ def process_cache_options(options):
 
     # |options.cache| path may not exist until DiskCache() instance is created.
     return DiskCache(
-        os.path.abspath(options.cache),
+        unicode(os.path.abspath(options.cache)),
         policies,
         isolated_format.get_hash_algo(options.namespace))
   else:

run_isolated.py
@@ -147,8 +147,8 @@ def run_tha_test(isolated_hash, storage, cache, leak_temp_dir, extra_args):
     extra_args: optional arguments to add to the command stated in the .isolate
                 file.
   """
-  run_dir = make_temp_dir('run_tha_test', cache.cache_dir)
-  out_dir = unicode(make_temp_dir('isolated_out', cache.cache_dir))
+  run_dir = make_temp_dir(u'run_tha_test', cache.cache_dir)
+  out_dir = unicode(make_temp_dir(u'isolated_out', cache.cache_dir))
   result = 0
   try:
     try:

commit 4f67f71a11b91382704061b061778d67952d764b

Fix regression from 3c979cb54e which broke run_on_bots.py.
Convert the assert to not fire up.

R=vadimsh@chromium.org
BUG=chromium:466751

Review URL: https://codereview.appspot.com/217870043

tools/parallel_execution.py
@@ -32,7 +32,7 @@ def task_to_name(name, dimensions, isolated_hash):
 
 
 def capture(cmd):
-  assert all(isinstance(i, str) for i in cmd), cmd
+  assert all(isinstance(i, basestring) for i in cmd), cmd
   start = time.time()
   p = subprocess.Popen(
       [sys.executable] + cmd, cwd=ROOT_DIR, stdout=subprocess.PIPE)