c7063ed67bb1ea023712a4ae2da57254eb410954
reed <reed@google.com>
2015-03-16 17:29:39 -0700

    add SK_LEGACY_DRAWPICTURECALLBACK guard flag
    Used until chrome switches to the new skia api
    
    BUG=
    TBR=robertphilips@chromium.org
    
    Review URL: https://codereview.chromium.org/1014473005
    
    Cr-Commit-Position: refs/heads/master@{#320830}
    
    Former-commit-id: c97047888c9e13602ee390c99fd78b05efe7bc26

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -245,6 +245,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_GETTOPDEVICE
 #endif
 
+#ifndef    SK_LEGACY_DRAWPICTURECALLBACK
+#   define SK_LEGACY_DRAWPICTURECALLBACK
+#endif
+
 #ifndef    SK_SUPPORT_LEGACY_GETDEVICE
 #   define SK_SUPPORT_LEGACY_GETDEVICE
 #endif
