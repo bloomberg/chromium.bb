b6200f1690c1b098c44118f7bb8f154717222354
benwells <benwells@chromium.org>
2015-03-17 23:28:45 -0700

    Revert of Fix scrollbar painting issue when scaling (patchset #5 id:80001 of https://codereview.chromium.org/841933002/)
    Reason for revert:
    This is causing test failures on memory bots, for some reason. For example:
    
    http://build.chromium.org/p/chromium.memory.fyi/builders/Linux%20ChromeOS%20MSan%20Tests/builds/13
    
    Example log output:
    [ RUN      ] NineImagePainterTest.PaintScale
    ../../ui/gfx/nine_image_painter_unittest.cc:63: Failure
    Value of: result.getColor(x, y)
      Actual: 4278255360
    Expected: 0xFFFF0000
    Which is: 4294901760
    ../../ui/gfx/nine_image_painter_unittest.cc:63: Failure
    Value of: result.getColor(x, y)
      Actual: 4278255360
    Expected: 0xFFFF0000
    Which is: 4294901760
    <snip, it goes on for a bit>
    
    It is failing on a range of bots, including the above, but also:
    http://build.chromium.org/p/chromium.memory.fyi/builders/Windows%20Unit%20%28DrMemory%29/builds/6
    http://build.chromium.org/p/chromium.memory/builders/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/builds/11
    
    Original issue's description:
    > Fix scrollbar painting issue when scaling
    >
    > Calculation of dest width and height for nine image is not correct. We
    > should use scale factor in transform matrix from canvas to calculate
    > dest width/height, while use image_scale_ factor in canvas to calculate
    > src width/height.
    >
    > BUG=447102
    >
    > Committed: https://crrev.com/1cd4867c1af018fa0ed67fd2b64c6fa1a3e80207 [formerly aaa36193441db0782d5a425ac81931d03adafe7f]
    > Cr-Commit-Position: refs/heads/master@{#321061}
    
    TBR=ananta@chromium.org,sky@chromium.org,reed@chromium.org,reed@google.com,qiankun.miao@intel.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=447102
    
    Review URL: https://codereview.chromium.org/1006423009
    
    Cr-Commit-Position: refs/heads/master@{#321084}
    
    Former-commit-id: 1c8e95e59152c5ba889a4c771b83c1f0cd031833

--- ui/gfx/nine_image_painter_unittest.cc
+++ ui/gfx/nine_image_painter_unittest.cc
@@ -5,10 +5,8 @@
 #include "ui/gfx/nine_image_painter.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/insets.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/gfx/image/image_skia.h"
 
 namespace gfx {
 
@@ -29,41 +27,6 @@ TEST(NineImagePainterTest, GetSubsetRegions) {
   EXPECT_EQ(gfx::Rect(0, 47, 2, 3), rects[6]);
   EXPECT_EQ(gfx::Rect(2, 47, 34, 3), rects[7]);
   EXPECT_EQ(gfx::Rect(36, 47, 4, 3), rects[8]);
-}
-
-TEST(NineImagePainterTest, PaintScale) {
-  SkBitmap src;
-  src.allocN32Pixels(100, 100);
-  src.eraseColor(SK_ColorRED);
-  src.eraseArea(SkIRect::MakeXYWH(10, 10, 80, 80), SK_ColorGREEN);
-
-  gfx::ImageSkia image(gfx::ImageSkiaRep(src, 0.0f));
-  gfx::Insets insets(10, 10, 10, 10);
-  gfx::NineImagePainter painter(image, insets);
-
-  int image_scale = 2;
-  bool is_opaque = true;
-  gfx::Canvas canvas(gfx::Size(400, 400), image_scale, is_opaque);
-  canvas.Scale(2, 1);
-
-  gfx::Rect bounds(0, 0, 100, 100);
-  painter.Paint(&canvas, bounds);
-
-  SkBitmap result;
-  const SkISize size = canvas.sk_canvas()->getDeviceSize();
-  result.allocN32Pixels(size.width(), size.height());
-  canvas.sk_canvas()->readPixels(&result, 0, 0);
-
-  SkIRect green_rect = SkIRect::MakeLTRB(40, 20, 360, 180);
-  for (int y = 0; y < 200; y++) {
-    for (int x = 0; x < 400; x++) {
-      if (green_rect.contains(x, y)) {
-        EXPECT_EQ(SK_ColorGREEN, result.getColor(x, y));
-      } else {
-        EXPECT_EQ(SK_ColorRED, result.getColor(x, y));
-      }
-    }
-  }
 }
 
 }  // namespace gfx
