a6c36b5d308aed9c18a4c05161c17f372c389d7f
JF Bastien <jfb@chromium.org>
2015-03-18 12:53:40 -0700

    Overflow test: improve testing of signed values around 0
    azakai reports a failure with overflow of signed 8-bit values on 0 - -128. This
    test tries to capture this corner case, but seems to pass as expected. Note that
    the compiler builtin doesn't support 8-bit values.
    
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=4125
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=4102
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3434
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3459
    R=dschuff@chromium.org, azakai@mozilla.com
    TEST= ./scons bitcode=1 run_overflow_test
    
    Review URL: https://codereview.chromium.org/1010333002
    
    
    Former-commit-id: 7fe2747e474154a659967ac9ac68f099cba2d64d

--- tests/toolchain/overflow.cc
+++ tests/toolchain/overflow.cc
@@ -26,7 +26,14 @@
 
 using namespace std;
 
-#define NOINLINE __attribute__((noinline))
+// The compiler can do further optimizations when it sees values coming in to
+// the overflow builtins. Run this test with and without inlining to make sure
+// there's test coverage for overflow with constant, and overflow with variable.
+#if SHOULD_INLINE
+# define MAYBE_INLINE __attribute__((always_inline))
+#else
+# define MAYBE_INLINE __attribute__((noinline))
+#endif
 
 // Datatypes supported by the overflow builtins.
 // DO(TYPE, SIGN, LENGTH)
@@ -43,74 +50,88 @@ static_assert(((sizeof(int) != sizeof(long)) ||
               "output would be different.");
 
 // The builtins have C-style names, create C++ overloads for them.
-template <typename T> NOINLINE bool add(T, T, T*);
-template <typename T> NOINLINE bool sub(T, T, T*);
-template <typename T> NOINLINE bool mul(T, T, T*);
+template <typename T> MAYBE_INLINE bool add(T, T, T*);
+template <typename T> MAYBE_INLINE bool sub(T, T, T*);
+template <typename T> MAYBE_INLINE bool mul(T, T, T*);
 
 #define DECLARE_OVERLOADS(TYPE, SIGN, LENGTH)                       \
   template <>                                                       \
-  NOINLINE bool add<TYPE>(TYPE lhs, TYPE rhs, TYPE * res) {         \
+  MAYBE_INLINE bool add<TYPE>(TYPE lhs, TYPE rhs, TYPE * res) {     \
     return __builtin_##SIGN##add##LENGTH##_overflow(lhs, rhs, res); \
   }                                                                 \
   template <>                                                       \
-  NOINLINE bool sub<TYPE>(TYPE lhs, TYPE rhs, TYPE * res) {         \
+  MAYBE_INLINE bool sub<TYPE>(TYPE lhs, TYPE rhs, TYPE * res) {     \
     return __builtin_##SIGN##sub##LENGTH##_overflow(lhs, rhs, res); \
   }                                                                 \
   template <>                                                       \
-  NOINLINE bool mul<TYPE>(TYPE lhs, TYPE rhs, TYPE * res) {         \
+  MAYBE_INLINE bool mul<TYPE>(TYPE lhs, TYPE rhs, TYPE * res) {     \
     return __builtin_##SIGN##mul##LENGTH##_overflow(lhs, rhs, res); \
   }
 FOR_EACH_TYPE(DECLARE_OVERLOADS)
 
-template <typename T>
-void test_single(bool (*op)(T, T, T*), char rep, T lhs, T rhs) {
-  const auto w = numeric_limits<T>::digits10 + 2;
-  T res;
-  cout << setw(w) << lhs << ' ' << rep << ' ' << setw(w) << rhs << " = "
-       << setw(w);
-  if ((*op)(lhs, rhs, &res))
-    cout << "overflow";
-  else
-    cout << res;
-  cout << '\n';
-}
-
-static const int iterations = 3;
+#define TEST_SINGLE(OP, REP, LHS, RHS)                                     \
+  do {                                                                     \
+    const auto w = numeric_limits<T>::digits10 + 2;                        \
+    T res;                                                                 \
+    cout << setw(w) << LHS << ' ' << REP << ' ' << setw(w) << RHS << " = " \
+         << setw(w);                                                       \
+    if (OP(LHS, RHS, &res))                                                \
+      cout << "overflow";                                                  \
+    else                                                                   \
+      cout << res;                                                         \
+    cout << '\n';                                                          \
+  } while (0)
 
 // Test the combinations of OP(LHS, RHS). We start at the *_START values and
 // iterate for `iterations` repetitions using the *_OP operator on values at
 // each repetition.
-#define ITERATE(OP, REP, LHS_START, RHS_START, LHS_OP, RHS_OP)           \
-  do {                                                                   \
-    T lhs = LHS_START;                                                   \
-    for (int lhs_values = 0; lhs_values != iterations; ++lhs_values) {   \
-      T rhs = RHS_START;                                                 \
-      for (int rhs_values = 0; rhs_values != iterations; ++rhs_values) { \
-        test_single(OP<T>, REP, lhs, rhs);                               \
-        RHS_OP rhs;                                                      \
-      }                                                                  \
-      LHS_OP lhs;                                                        \
-    }                                                                    \
+#define ITERATE(OP, REP, LHS_START, RHS_START, LHS_OP, RHS_OP, ITERS) \
+  do {                                                                \
+    T lhs = LHS_START;                                                \
+    for (int lhs_values = 0; lhs_values != ITERS; ++lhs_values) {     \
+      T rhs = RHS_START;                                              \
+      for (int rhs_values = 0; rhs_values != ITERS; ++rhs_values) {   \
+        TEST_SINGLE(OP<T>, REP, lhs, rhs);                            \
+        RHS_OP rhs;                                                   \
+      }                                                               \
+      LHS_OP lhs;                                                     \
+    }                                                                 \
   } while (0)
 
 template <typename T>
 void test_type() {
+  int iterations = 3;
+  bool is_signed = numeric_limits<T>::is_signed;
   T min = numeric_limits<T>::min();
   T max = numeric_limits<T>::max();
-  cout << "\nTesting " << sizeof(T) << " byte "
-       << (numeric_limits<T>::is_signed ? "" : "un") << "signed integer:\n";
-  ITERATE(add, '+', min, min, ++, ++);
-  ITERATE(add, '+', min, max, ++, --);
-  ITERATE(add, '+', max, max, --, --);
-  ITERATE(add, '+', max, min, --, ++);
-  ITERATE(sub, '-', min, min, ++, ++);
-  ITERATE(sub, '-', min, max, ++, --);
-  ITERATE(sub, '-', max, max, --, --);
-  ITERATE(sub, '-', max, min, --, ++);
-  ITERATE(mul, '*', min, min, ++, ++);
-  ITERATE(mul, '*', min, max, ++, --);
-  ITERATE(mul, '*', max, max, --, --);
-  ITERATE(mul, '*', max, min, --, ++);
+  cout << "\nTesting " << sizeof(T) << " byte " << (is_signed ? "" : "un")
+       << "signed integer:\n";
+  ITERATE(add, '+', min, min, ++, ++, iterations);
+  ITERATE(add, '+', min, max, ++, --, iterations);
+  ITERATE(add, '+', max, max, --, --, iterations);
+  ITERATE(add, '+', max, min, --, ++, iterations);
+  ITERATE(sub, '-', min, min, ++, ++, iterations);
+  ITERATE(sub, '-', min, max, ++, --, iterations);
+  ITERATE(sub, '-', max, max, --, --, iterations);
+  ITERATE(sub, '-', max, min, --, ++, iterations);
+  ITERATE(mul, '*', min, min, ++, ++, iterations);
+  ITERATE(mul, '*', min, max, ++, --, iterations);
+  ITERATE(mul, '*', max, max, --, --, iterations);
+  ITERATE(mul, '*', max, min, --, ++, iterations);
+  if (is_signed) {
+    ITERATE(add, '+', 0, min, (void), ++, iterations);
+    ITERATE(add, '+', 0, max, (void), --, iterations);
+    ITERATE(add, '+', min, 0, ++, (void), iterations);
+    ITERATE(add, '+', max, 0, --, (void), iterations);
+    ITERATE(sub, '-', 0, min, (void), ++, iterations);
+    ITERATE(sub, '-', 0, max, (void), --, iterations);
+    ITERATE(sub, '-', min, 0, ++, (void), iterations);
+    ITERATE(sub, '-', max, 0, --, (void), iterations);
+    ITERATE(mul, '*', 0, min, (void), ++, iterations);
+    ITERATE(mul, '*', 0, max, (void), --, iterations);
+    ITERATE(mul, '*', min, 0, ++, (void), iterations);
+    ITERATE(mul, '*', max, 0, --, (void), iterations);
+  }
 }
 
 int main() {
