bloomberg/chromium.bb

commit 0f4f7dc2b2ace02a3fd474edf19958ab8538edd1

Revamp Autofill Wallet settings
this is round 1. It is a good intermediate state between what we have now and what's in the mocks.

BUG=446698

Review URL: https://codereview.chromium.org/996973003

Cr-Commit-Position: refs/heads/master@{#320411}

Former-commit-id: 2499b74015ee2d97674eb7f8a132e3ff0c3b0eb1

chrome/browser/resources/options/autofill_options_list.js
@@ -11,13 +11,13 @@ cr.define('options.autofillOptions', function() {
   /**
    * @return {!HTMLButtonElement}
    */
-  function AutofillEditProfileButton(guid, edit) {
+  function AutofillEditProfileButton(edit) {
     var editButtonEl = /** @type {HTMLButtonElement} */(
         document.createElement('button'));
     editButtonEl.className = 'list-inline-button custom-appearance';
     editButtonEl.textContent =
         loadTimeData.getString('autofillEditProfileButton');
-    editButtonEl.onclick = function(e) { edit(guid); };
+    editButtonEl.onclick = edit;
 
     editButtonEl.onmousedown = function(e) {
       // Don't select the row when clicking the button.
@@ -31,14 +31,15 @@ cr.define('options.autofillOptions', function() {
 
   /**
    * Creates a new address list item.
-   * @param {Array} entry An array of the form [guid, label].
+   * @param {Object} entry An object with metadata about an address profile.
    * @constructor
    * @extends {options.DeletableItem}
    */
   function AddressListItem(entry) {
     var el = cr.doc.createElement('div');
-    el.guid = entry[0];
-    el.label = entry[1];
+    for (var key in entry) {
+      el[key] = entry[key];
+    }
     el.__proto__ = AddressListItem.prototype;
     el.decorate();
 
@@ -58,26 +59,28 @@ cr.define('options.autofillOptions', function() {
       label.textContent = this.label;
       this.contentElement.appendChild(label);
 
+      if (!this.isLocal)
+        this.deletable = false;
+
       // The 'Edit' button.
+      var guid = this.guid;
       var editButtonEl = AutofillEditProfileButton(
-        this.guid,
-        AutofillOptions.loadAddressEditor);
+          function() { AutofillOptions.loadAddressEditor(guid); });
       this.contentElement.appendChild(editButtonEl);
     },
   };
 
   /**
    * Creates a new credit card list item.
-   * @param {Array} entry An array of the form [guid, label, icon].
+   * @param {Object} entry An object with metadata about a credit card.
    * @constructor
    * @extends {options.DeletableItem}
    */
   function CreditCardListItem(entry) {
     var el = cr.doc.createElement('div');
-    el.guid = entry[0];
-    el.label = entry[1];
-    el.icon = entry[2];
-    el.description = entry[3];
+    for (var key in entry) {
+      el[key] = entry[key];
+    }
     el.__proto__ = CreditCardListItem.prototype;
     el.decorate();
 
@@ -97,16 +100,26 @@ cr.define('options.autofillOptions', function() {
       label.textContent = this.label;
       this.contentElement.appendChild(label);
 
-      // The credit card icon.
-      var icon = this.ownerDocument.createElement('img');
-      icon.src = this.icon;
-      icon.alt = this.description;
-      this.contentElement.appendChild(icon);
+      if (!this.isLocal)
+        this.deletable = false;
+
+      var guid = this.guid;
+      if (this.isCached) {
+        var localCopyText = this.ownerDocument.createElement('span');
+        localCopyText.textContent =
+            loadTimeData.getString('autofillDescribeLocalCopy');
+        this.contentElement.appendChild(localCopyText);
+
+        var clearLocalCopyButton = AutofillEditProfileButton(
+            function() { chrome.send('clearLocalCardCopy', [guid]); });
+        clearLocalCopyButton.textContent =
+            loadTimeData.getString('autofillClearLocalCopyButton');
+        this.contentElement.appendChild(clearLocalCopyButton);
+      }
 
       // The 'Edit' button.
       var editButtonEl = AutofillEditProfileButton(
-        this.guid,
-        AutofillOptions.loadCreditCardEditor);
+          function() { AutofillOptions.loadCreditCardEditor(guid); });
       this.contentElement.appendChild(editButtonEl);
     },
   };
@@ -362,7 +375,7 @@ cr.define('options.autofillOptions', function() {
 
     /** @override */
     activateItemAtIndex: function(index) {
-      AutofillOptions.loadAddressEditor(this.dataModel.item(index)[0]);
+      AutofillOptions.loadAddressEditor(this.dataModel.item(index));
     },
 
     /**
@@ -375,7 +388,7 @@ cr.define('options.autofillOptions', function() {
 
     /** @override */
     deleteItemAtIndex: function(index) {
-      AutofillOptions.removeData(this.dataModel.item(index)[0],
+      AutofillOptions.removeData(this.dataModel.item(index).guid,
                                  'Options_AutofillAddressDeleted');
     },
   };
@@ -396,7 +409,7 @@ cr.define('options.autofillOptions', function() {
 
     /** @override */
     activateItemAtIndex: function(index) {
-      AutofillOptions.loadCreditCardEditor(this.dataModel.item(index)[0]);
+      AutofillOptions.loadCreditCardEditor(this.dataModel.item(index));
     },
 
     /**
@@ -409,7 +422,7 @@ cr.define('options.autofillOptions', function() {
 
     /** @override */
     deleteItemAtIndex: function(index) {
-      AutofillOptions.removeData(this.dataModel.item(index)[0],
+      AutofillOptions.removeData(this.dataModel.item(index).guid,
                                  'Options_AutofillCreditCardDeleted');
     },
   };

chrome/app/chromium_strings.grd
@@ -973,6 +973,9 @@ Signing in anyway will merge Chromium information like bookmarks, history, and o
       <message name="IDS_AUTOFILL_WALLET_UPGRADE_CHROME_ERROR" desc="Text explaining that user must upgrade Chrome to use Wallet.">
         You must upgrade Chromium to use Google Wallet [<ph name="ERROR_CODE">$1<ex>54</ex></ph>].
       </message>
+      <message name="IDS_AUTOFILL_DESCRIBE_LOCAL_COPY" desc="Text label that described a Wallet credit card which has been copied to the local Chrome instance.">
+        Copied to Chromium
+      </message>
 
       <!-- Network Error Pages -->
       <if expr="not is_android">

chrome/app/generated_resources.grd
@@ -10503,6 +10503,9 @@ Chrome ran out of memory.
         <message name="IDS_AUTOFILL_EDIT_PROFILE_BUTTON" desc="The label of the button that edits the currently selected Autofill profile.">
           Edit...
         </message>
+        <message name="IDS_AUTOFILL_CLEAR_LOCAL_COPY_BUTTON" desc="The label of the button that clears the local copy of a Wallet Credit Card.">
+          Clear copy
+        </message>
         <message name="IDS_OPTIONS_MANAGE_AUTOFILL_SETTINGS_LINK" desc="The label of the 'Manage Autofill settings' link">
           Manage Autofill settings
         </message>

chrome/app/google_chrome_strings.grd
@@ -896,6 +896,9 @@ Signing in anyway will merge Chrome information like bookmarks, history, and oth
       <message name="IDS_AUTOFILL_WALLET_UPGRADE_CHROME_ERROR" desc="Text explaining that user must upgrade Chrome to use Wallet.">
         You must upgrade Chrome to use Google Wallet [<ph name="ERROR_CODE">$1<ex>54</ex></ph>].
       </message>
+      <message name="IDS_AUTOFILL_DESCRIBE_LOCAL_COPY" desc="Text label that described a Wallet credit card which has been copied to the local Chrome instance.">
+        Copied to Chrome
+      </message>
 
       <!-- Network Error Pages -->
       <if expr="not is_android">

chrome/browser/resources/options/autofill_options.css
@@ -17,13 +17,12 @@
   text-overflow: ellipsis;
 }
 
-.autofill-list-item + img {
-  -webkit-padding-end: 20px;
-  vertical-align: top;
-}
-
 #autofill-options > div:last-child {
   margin-top: 15px;
+}
+
+#autofill-options .deletable-item > span {
+  color: #777;
 }
 
 #autofill-options > div.settings-list > div:last-child {
@@ -37,6 +36,7 @@
 }
 
 #autofill-options .list-inline-button {
+  -webkit-margin-start: 12px;
   margin-top: 0;
   vertical-align: top;
 }

chrome/browser/resources/options/autofill_options.html
@@ -2,6 +2,15 @@
   <div class="close-button"></div>
   <h1 i18n-content="autofillOptionsPage"></h1>
   <div class="content-area">
+    <div id="autofill-wallet-setting-area" class="checkbox">
+      <label>
+        <input pref="autofill.wallet_import_enabled" type="checkbox">
+        <span>
+          Show addresses and credit cards from Google Payments
+          <!-- A reminder not to launch this UI. -->
+          <span style="font-size: 80%">translate me!</span>
+        </span>
+    </div>
 <if expr="is_macosx">
     <div class="checkbox">
       <label>
@@ -15,24 +24,6 @@
     <!-- TODO(estade): This section shouldn't show if you're not signed into
          sync. Or perhaps it should show with a link to go sign in with sync?
       -->
-    <div id="autofill-wallet-setting-area" class="settings-row" hidden>
-      <div class="checkbox">
-        <label>
-          <input pref="autofill.wallet_import_enabled" type="checkbox">
-          <span>
-            Include
-              <a target="_blank" i18n-values="href:manageWalletAddressesUrl">
-                  addresses</a>
-            and
-              <a target="_blank"
-                  i18n-values="href:manageWalletPaymentMethodsUrl">cards</a>
-            stored in your Google Wallet
-            <!-- A reminder not to launch this UI. -->
-            <span style="font-size: 80%">translate me!</span>
-          </span>
-      </div>
-    </div>
-
     <h3 i18n-content="autofillAddresses"></h3>
     <div class="settings-list">
       <list id="address-list"></list>

chrome/browser/resources/options/autofill_options.js
@@ -181,25 +181,31 @@ cr.define('options', function() {
     },
 
     /**
-     * Requests profile data for the address represented by |guid| from the
-     * PersonalDataManager. Once the data is loaded, the AutofillOptionsHandler
-     * calls showEditAddressOverlay().
-     * @param {string} guid The GUID of the address to edit.
+     * For local Autofill data, this function causes the AutofillOptionsHandler
+     * to call showEditAddressOverlay(). For Wallet data, the user is
+     * redirected to the Wallet web interface.
+     * @param {Object} entry The relevant entry in data model.
      * @private
      */
-    loadAddressEditor_: function(guid) {
-      chrome.send('loadAddressEditor', [guid]);
+    loadAddressEditor_: function(entry) {
+      if (entry.isLocal)
+        chrome.send('loadAddressEditor', [entry.guid]);
+      else
+        window.open(loadTimeData.getString('manageWalletAddressesUrl'));
     },
 
     /**
-     * Requests profile data for the credit card represented by |guid| from the
-     * PersonalDataManager. Once the data is loaded, the AutofillOptionsHandler
-     * calls showEditCreditCardOverlay().
-     * @param {string} guid The GUID of the credit card to edit.
+     * For local Autofill data, this function causes the AutofillOptionsHandler
+     * to call showEditCreditCardOverlay(). For Wallet data, the user is
+     * redirected to the Wallet web interface.
+     * @param {Object} entry The relevant entry in data model.
      * @private
      */
-    loadCreditCardEditor_: function(guid) {
-      chrome.send('loadCreditCardEditor', [guid]);
+    loadCreditCardEditor_: function(entry) {
+      if (entry.isLocal)
+        chrome.send('loadCreditCardEditor', [entry.guid]);
+      else
+        window.open(loadTimeData.getString('manageWalletPaymentMethodsUrl'));
     },
 
     /**
@@ -242,12 +248,12 @@ cr.define('options', function() {
     AutofillOptions.getInstance().removeData_(guid, metricsAction);
   };
 
-  AutofillOptions.loadAddressEditor = function(guid) {
-    AutofillOptions.getInstance().loadAddressEditor_(guid);
+  AutofillOptions.loadAddressEditor = function(entry) {
+    AutofillOptions.getInstance().loadAddressEditor_(entry);
   };
 
-  AutofillOptions.loadCreditCardEditor = function(guid) {
-    AutofillOptions.getInstance().loadCreditCardEditor_(guid);
+  AutofillOptions.loadCreditCardEditor = function(entry) {
+    AutofillOptions.getInstance().loadCreditCardEditor_(entry);
   };
 
   AutofillOptions.editAddress = function(address) {

chrome/browser/ui/webui/options/autofill_options_handler.cc
@@ -22,6 +22,7 @@
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/autofill/country_combobox_model.h"
 #include "chrome/common/url_constants.h"
+#include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "components/autofill/content/browser/wallet/wallet_service_url.h"
 #include "components/autofill/core/browser/autofill_country.h"
@@ -63,6 +64,17 @@ static const char kCountryField[] = "country";
 
 static const char kComponents[] = "components";
 static const char kLanguageCode[] = "languageCode";
+
+scoped_ptr<base::DictionaryValue> CreditCardToDictionary(
+    const CreditCard& card) {
+  scoped_ptr<base::DictionaryValue> value(new base::DictionaryValue);
+  value->SetString("guid", card.guid());
+  value->SetString("label", card.Label());
+  value->SetBoolean("isLocal", card.record_type() == CreditCard::LOCAL_CARD);
+  value->SetBoolean("isCached",
+                    card.record_type() == CreditCard::FULL_SERVER_CARD);
+  return value.Pass();
+}
 
 // Fills |components| with the address UI components that should be used to
 // input an address for |country_code| when UI BCP 47 language code is
@@ -319,6 +331,8 @@ void AutofillOptionsHandler::GetLocalizedValues(
     { "autofillAddAddress", IDS_AUTOFILL_ADD_ADDRESS_BUTTON },
     { "autofillAddCreditCard", IDS_AUTOFILL_ADD_CREDITCARD_BUTTON },
     { "autofillEditProfileButton", IDS_AUTOFILL_EDIT_PROFILE_BUTTON },
+    { "autofillDescribeLocalCopy", IDS_AUTOFILL_DESCRIBE_LOCAL_COPY },
+    { "autofillClearLocalCopyButton", IDS_AUTOFILL_CLEAR_LOCAL_COPY_BUTTON },
     { "helpButton", IDS_AUTOFILL_HELP_LABEL },
     { "addAddressTitle", IDS_AUTOFILL_ADD_ADDRESS_CAPTION },
     { "editAddressTitle", IDS_AUTOFILL_EDIT_ADDRESS_CAPTION },
@@ -402,8 +416,8 @@ void AutofillOptionsHandler::RegisterMessages() {
       base::Bind(&AutofillOptionsHandler::ValidatePhoneNumbers,
                  base::Unretained(this)));
   web_ui()->RegisterMessageCallback(
-      "remaskServerCards",
-      base::Bind(&AutofillOptionsHandler::RemaskServerCards,
+      "clearLocalCardCopy",
+      base::Bind(&AutofillOptionsHandler::RemaskServerCard,
                  base::Unretained(this)));
 }
 
@@ -446,8 +460,7 @@ void AutofillOptionsHandler::LoadAutofillData() {
   if (!IsPersonalDataLoaded())
     return;
 
-  const std::vector<AutofillProfile*>& profiles =
-      personal_data_->web_profiles();
+  const std::vector<AutofillProfile*>& profiles = personal_data_->GetProfiles();
   std::vector<base::string16> labels;
   AutofillProfile::CreateDifferentiatingLabels(
       profiles,
@@ -457,26 +470,24 @@ void AutofillOptionsHandler::LoadAutofillData() {
 
   base::ListValue addresses;
   for (size_t i = 0; i < profiles.size(); ++i) {
-    base::ListValue* entry = new base::ListValue();
-    entry->Append(new base::StringValue(profiles[i]->guid()));
-    entry->Append(new base::StringValue(labels[i]));
-    addresses.Append(entry);
+    // Skip showing auxiliary profiles (e.g. Mac Contacts) for now.
+    if (profiles[i]->record_type() == AutofillProfile::AUXILIARY_PROFILE)
+      continue;
+
+    scoped_ptr<base::DictionaryValue> value(new base::DictionaryValue);
+    value->SetString("guid", profiles[i]->guid());
+    value->SetString("label", labels[i]);
+    value->SetBoolean("isLocal", profiles[i]->record_type() ==
+                                     AutofillProfile::LOCAL_PROFILE);
+    addresses.Append(value.release());
   }
 
   web_ui()->CallJavascriptFunction("AutofillOptions.setAddressList", addresses);
 
   base::ListValue credit_cards;
-  const std::vector<CreditCard*>& cards = personal_data_->GetLocalCreditCards();
+  const std::vector<CreditCard*>& cards = personal_data_->GetCreditCards();
   for (const CreditCard* card : cards) {
-    // TODO(estade): this should be a dictionary.
-    base::ListValue* entry = new base::ListValue();
-    entry->Append(new base::StringValue(card->guid()));
-    entry->Append(new base::StringValue(card->Label()));
-    entry->Append(new base::StringValue(
-        webui::GetBitmapDataUrlFromResource(
-            CreditCard::IconResourceId(card->type()))));
-    entry->Append(new base::StringValue(card->TypeForDisplay()));
-    credit_cards.Append(entry);
+    credit_cards.Append(CreditCardToDictionary(*card).release());
   }
 
   web_ui()->CallJavascriptFunction("AutofillOptions.setCreditCardList",
@@ -694,11 +705,17 @@ void AutofillOptionsHandler::ValidatePhoneNumbers(const base::ListValue* args) {
   scoped_ptr<base::ListValue> list_value = ValidatePhoneArguments(args);
 
   web_ui()->CallJavascriptFunction(
-    "AutofillEditAddressOverlay.setValidatedPhoneNumbers", *list_value);
+      "AutofillEditAddressOverlay.setValidatedPhoneNumbers", *list_value);
 }
 
-void AutofillOptionsHandler::RemaskServerCards(const base::ListValue* args) {
-  personal_data_->ResetFullServerCards();
+void AutofillOptionsHandler::RemaskServerCard(const base::ListValue* args) {
+  std::string guid;
+  if (!args->GetString(0, &guid)) {
+    NOTREACHED();
+    return;
+  }
+
+  personal_data_->ResetFullServerCard(guid);
 }
 
 bool AutofillOptionsHandler::IsPersonalDataLoaded() const {

commit e2dd4125dd2c66b200f70923b715279449f455a1

Fix some closure compiler errors introduced in 0f4f7dc2b2ace [formerly 2499b74015ee2]
BUG=none

Review URL: https://codereview.chromium.org/1008373011

Cr-Commit-Position: refs/heads/master@{#321052}

Former-commit-id: 8271e59c40d86f8adf3b58e766508e03c6a44743

chrome/browser/resources/options/autofill_options_list.js
@@ -2,6 +2,18 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+/**
+ * @typedef {{
+ *   guid: string,
+ *   label: string,
+ *   sublabel: string,
+ *   isLocal: boolean,
+ *   isCached: boolean
+ * }}
+ * @see chrome/browser/ui/webui/options/autofill_options_handler.cc
+ */
+var AutofillEntityMetadata;
+
 cr.define('options.autofillOptions', function() {
   /** @const */ var DeletableItem = options.DeletableItem;
   /** @const */ var DeletableItemList = options.DeletableItemList;
@@ -30,9 +42,7 @@ cr.define('options.autofillOptions', function() {
     return editButtonEl;
   }
 
-  /**
-   * @return {!HTMLSpanElement}
-   */
+  /** @return {!Element} */
   function CreateGoogleAccountLabel() {
     var label = document.createElement('div');
     label.className = 'deemphasized hides-on-hover';
@@ -42,16 +52,16 @@ cr.define('options.autofillOptions', function() {
 
   /**
    * Creates a new address list item.
-   * @param {Object} entry An object with metadata about an address profile.
    * @constructor
+   * @param {AutofillEntityMetadata} metadata Details about an address profile.
    * @extends {options.DeletableItem}
+   * @see chrome/browser/ui/webui/options/autofill_options_handler.cc
    */
-  function AddressListItem(entry) {
+  function AddressListItem(metadata) {
     var el = cr.doc.createElement('div');
-    for (var key in entry) {
-      el[key] = entry[key];
-    }
     el.__proto__ = AddressListItem.prototype;
+    /** @private */
+    el.metadata_ = metadata;
     el.decorate();
 
     return el;
@@ -66,21 +76,21 @@ cr.define('options.autofillOptions', function() {
 
       var label = this.ownerDocument.createElement('div');
       label.className = 'autofill-list-item';
-      label.textContent = this.label;
+      label.textContent = this.metadata_.label;
       this.contentElement.appendChild(label);
 
       var sublabel = this.ownerDocument.createElement('div');
       sublabel.className = 'deemphasized';
-      sublabel.textContent = this.sublabel;
+      sublabel.textContent = this.metadata_.sublabel;
       this.contentElement.appendChild(sublabel);
 
-      if (!this.isLocal) {
+      if (!this.metadata_.isLocal) {
         this.deletable = false;
         this.contentElement.appendChild(CreateGoogleAccountLabel());
       }
 
       // The 'Edit' button.
-      var guid = this.guid;
+      var guid = this.metadata_.guid;
       var editButtonEl = AutofillEditProfileButton(
           function() { AutofillOptions.loadAddressEditor(guid); });
       this.contentElement.appendChild(editButtonEl);
@@ -89,16 +99,15 @@ cr.define('options.autofillOptions', function() {
 
   /**
    * Creates a new credit card list item.
-   * @param {Object} entry An object with metadata about a credit card.
+   * @param {AutofillEntityMetadata} metadata Details about a credit card.
    * @constructor
    * @extends {options.DeletableItem}
    */
-  function CreditCardListItem(entry) {
+  function CreditCardListItem(metadata) {
     var el = cr.doc.createElement('div');
-    for (var key in entry) {
-      el[key] = entry[key];
-    }
     el.__proto__ = CreditCardListItem.prototype;
+    /** @private */
+    el.metadata_ = metadata;
     el.decorate();
 
     return el;
@@ -113,21 +122,21 @@ cr.define('options.autofillOptions', function() {
 
       var label = this.ownerDocument.createElement('div');
       label.className = 'autofill-list-item';
-      label.textContent = this.label;
+      label.textContent = this.metadata_.label;
       this.contentElement.appendChild(label);
 
       var sublabel = this.ownerDocument.createElement('div');
       sublabel.className = 'deemphasized';
-      sublabel.textContent = this.sublabel;
+      sublabel.textContent = this.metadata_.sublabel;
       this.contentElement.appendChild(sublabel);
 
-      if (!this.isLocal) {
+      if (!this.metadata_.isLocal) {
         this.deletable = false;
         this.contentElement.appendChild(CreateGoogleAccountLabel());
       }
 
-      var guid = this.guid;
-      if (this.isCached) {
+      var guid = this.metadata_.guid;
+      if (this.metadata_.isCached) {
         var localCopyText = this.ownerDocument.createElement('span');
         localCopyText.className = 'hide-until-hover deemphasized';
         localCopyText.textContent =
@@ -404,10 +413,10 @@ cr.define('options.autofillOptions', function() {
 
     /**
      * @override
-     * @param {Array} entry
+     * @param {AutofillEntityMetadata} metadata
      */
-    createItem: function(entry) {
-      return new AddressListItem(entry);
+    createItem: function(metadata) {
+      return new AddressListItem(metadata);
     },
 
     /** @override */
@@ -438,10 +447,10 @@ cr.define('options.autofillOptions', function() {
 
     /**
      * @override
-     * @param {Array} entry
+     * @param {AutofillEntityMetadata} metadata
      */
-    createItem: function(entry) {
-      return new CreditCardListItem(entry);
+    createItem: function(metadata) {
+      return new CreditCardListItem(metadata);
     },
 
     /** @override */