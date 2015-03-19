1768142babe46388a4c90867d96fabffa4f27a99
alemate <alemate@chromium.org>
2015-03-19 10:19:03 -0700

    Chromeos: Remove CaptivePortalBypassProxy user option.
    BUG=431230
    TEST=manual
    
    Review URL: https://codereview.chromium.org/1014913002
    
    Cr-Commit-Position: refs/heads/master@{#321384}
    
    Former-commit-id: 0e2a2f9bb849f98a4d584f89cdebf85d3dded704

--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -206,9 +206,6 @@ cr.define('options', function() {
         }
         options.network.NetworkList.refreshNetworkData(
             loadTimeData.getValue('networkData'));
-        if (loadTimeData.getBoolean('enableCaptivePortalBypassProxyOption')) {
-          $('captive-portal-bypass-proxy-div').hidden = false;
-        }
       }
 
       // On Startup section.
