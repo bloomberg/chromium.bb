dafbd6078bb6baad97134e32ac9efb0c9c476b68
twellington <twellington@chromium.org>
2015-03-19 10:20:05 -0700

    Remove work around for AppCompat switch thumb issues
    Original work around done in: crbug.com/455327 and crbug.com/459782
    
    BUG=440601
    
    Review URL: https://codereview.chromium.org/1019593002
    
    Cr-Commit-Position: refs/heads/master@{#321385}
    
    Former-commit-id: 821dbcff61cfb0ab1d602ef505b9545599a86410

--- chrome/android/java/src/org/chromium/chrome/browser/widget/ChromeSwitchCompat.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/ChromeSwitchCompat.java
@@ -5,16 +5,11 @@
 package org.chromium.chrome.browser.widget;
 
 import android.content.Context;
-import android.content.res.ColorStateList;
-import android.graphics.PorterDuff;
-import android.graphics.drawable.Drawable;
 import android.support.v7.widget.SwitchCompat;
 import android.util.AttributeSet;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Switch;
-
-import org.chromium.chrome.R;
 
 /**
  * This class fixes the accessibility of SwitchCompat so it's read as "on switch" instead of
@@ -24,24 +19,15 @@ import org.chromium.chrome.R;
  * "Checkbox". This works around the bug by marking accessibility events from SwitchCompat with the
  * Switch class name, which TalkBack recognizes.
  *
- * It also fixes the thumb drawable being too small and disappearing on certain devices
- * and the disabled tint color being incorrect. http://crbug.com/455327
- *
  * TODO(newt): Delete this class once the support library is fixed. http://b/19110477
  */
 public class ChromeSwitchCompat extends SwitchCompat {
-    private Drawable mThumbDrawable;
-    private ColorStateList mTint;
 
     /**
      * Constructor for inflating from XML.
      */
     public ChromeSwitchCompat(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setThumbResource(R.drawable.switch_thumb);
-        mThumbDrawable = getThumbDrawable();
-        mThumbDrawable.mutate();
-        mTint = getResources().getColorStateList(R.color.switch_thumb_tint);
     }
 
     @Override
@@ -54,18 +40,5 @@ public class ChromeSwitchCompat extends SwitchCompat {
     public void onInitializeAccessibilityNodeInfo(AccessibilityNodeInfo info) {
         super.onInitializeAccessibilityNodeInfo(info);
         info.setClassName(Switch.class.getName());
-    }
-
-    @Override
-    protected void drawableStateChanged() {
-        super.drawableStateChanged();
-        updateThumbTintColor();
-    }
-
-    private boolean updateThumbTintColor() {
-        if (mTint == null) return false;
-        mThumbDrawable.setColorFilter(mTint.getColorForState(mThumbDrawable.getState(), 0),
-                PorterDuff.Mode.SRC_IN);
-        return true;
     }
 }
