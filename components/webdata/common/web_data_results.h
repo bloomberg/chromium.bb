7e1fd56958c075fd09383565ca65d07d8211133f
pcc <pcc@chromium.org>
2015-03-20 15:00:33 -0700

    Make WDDestroyableResult<T> derive from WDResult<T>.
    This fixes an issue found by -fsanitize=cfi-derived-cast caused by code that
    casts a WDResult<T> pointer to WDDestroyableResult<T>.
    
    BUG=457523
    R=caitkp@chromium.org
    
    Review URL: https://codereview.chromium.org/1021043002
    
    Cr-Commit-Position: refs/heads/master@{#321644}
    
    Former-commit-id: d317e89dc1ae054c4d5e1672ebde8ed0ad3d8899

--- components/webdata/common/web_data_results.h
+++ components/webdata/common/web_data_results.h
@@ -81,14 +81,13 @@ template <class T> class WDResult : public WDTypedResult {
   DISALLOW_COPY_AND_ASSIGN(WDResult);
 };
 
-template <class T> class WDDestroyableResult : public WDTypedResult {
+template <class T> class WDDestroyableResult : public WDResult<T> {
  public:
   WDDestroyableResult(
       WDResultType type,
       const T& v,
       const DestroyCallback& callback)
-      : WDTypedResult(type),
-        value_(v),
+      : WDResult<T>(type, v),
         callback_(callback) {
   }
 
@@ -102,13 +101,7 @@ template <class T> class WDDestroyableResult : public WDTypedResult {
     }
   }
 
-  // Return a single value result.
-  T GetValue() const {
-    return value_;
-  }
-
  private:
-  T value_;
   DestroyCallback callback_;
 
   DISALLOW_COPY_AND_ASSIGN(WDDestroyableResult);
