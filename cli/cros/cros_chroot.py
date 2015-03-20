d69cb0a7121e8c30f3b884ca3ae391727ae1d9e6
Don Garrett <dgarrett@google.com>
2015-03-19 19:49:31 -0700

    brillo chroot: Create new command for entering the chroot.
    This is a new command for entering a chroot that is workspace aware.
    
    It's barebones, but it's enough to be useful.
    
    BUG=brillo:629
    TEST=Manual + Unittests.
    
    Change-Id: I20315655234662d7c36114c2de3a5611891dd483
    Reviewed-on: https://chromium-review.googlesource.com/261399
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Commit-Queue: Don Garrett <dgarrett@chromium.org>
    Tested-by: Don Garrett <dgarrett@chromium.org>

---
+++ cli/cros/cros_chroot.py
@@ -0,0 +1,48 @@
+# Copyright 2015 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""cros chroot: Enter the chroot for the current build environment."""
+
+from __future__ import print_function
+
+import argparse
+
+from chromite.cli import command
+from chromite.lib import commandline
+from chromite.lib import cros_build_lib
+
+
+@command.CommandDecorator('chroot')
+class ChrootCommand(command.CrosCommand):
+  """Enter the chroot."""
+
+  # Override base class property to enable stats upload.
+  upload_stats = True
+
+  @classmethod
+  def AddParser(cls, parser):
+    """Adds a parser."""
+    super(cls, ChrootCommand).AddParser(parser)
+    parser.add_argument(
+        'command', nargs=argparse.REMAINDER,
+        help='(optional) Command to execute inside the chroot.')
+
+  def Run(self):
+    """Runs `cros chroot`."""
+    self.options.Freeze()
+
+    commandline.RunInsideChroot(self, auto_detect_brick=False)
+
+    cmd = self.options.command
+
+    # If -- was used to separate out the command from arguments, ignore it.
+    if cmd and cmd[0] == '--':
+      cmd = cmd[1:]
+
+    # If there is no command, run bash.
+    if not cmd:
+      cmd = ['bash']
+
+    result = cros_build_lib.RunCommand(cmd, print_cmd=False, error_code_ok=True)
+    return result.returncode
