3a2e3f5b95c09125e5617643b8fbea38e06fdc4d
estark <estark@chromium.org>
2015-03-20 18:01:02 -0700

    Add unit tests for CertificateErrorReporter
    These tests are for multiple requests, sent sequentially and
    simultaneously.
    
    BUG=466629
    
    Review URL: https://codereview.chromium.org/1004793004
    
    Cr-Commit-Position: refs/heads/master@{#321675}
    
    Former-commit-id: 334ee519dd3162fbdeaf18230ce618bdf34ea4a1

--- chrome/browser/net/certificate_error_reporter_unittest.cc
+++ chrome/browser/net/certificate_error_reporter_unittest.cc
@@ -4,6 +4,9 @@
 
 #include "chrome/browser/net/certificate_error_reporter.h"
 
+#include <set>
+#include <string>
+
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/files/file_path.h"
@@ -40,6 +43,7 @@ using net::URLRequest;
 namespace {
 
 const char kHostname[] = "test.mail.google.com";
+const char kSecondRequestHostname[] = "test2.mail.google.com";
 const char kDummyFailureLog[] = "dummy failure log";
 const char kTestCertFilename[] = "test_mail_google_com.pem";
 
@@ -73,9 +77,12 @@ void EnableUrlRequestMocks(bool enable) {
   net::URLRequestMockDataJob::AddUrlHandler();
 }
 
-// Check that data uploaded in the request matches the test data (an
-// SSL report for |kHostname| as returned by |GetTestSSLInfo()|).
-void CheckUploadData(URLRequest* request) {
+// Check that data uploaded in the request matches the test data (an SSL
+// report for one of the given hostnames, with the info returned by
+// |GetTestSSLInfo()|). The hostname sent in the report will be erased
+// from |expected_hostnames|.
+void CheckUploadData(URLRequest* request,
+                     std::set<std::string>* expected_hostnames) {
   const net::UploadDataStream* upload = request->get_upload();
   ASSERT_TRUE(upload);
   ASSERT_TRUE(upload->GetElementReaders());
@@ -89,7 +96,9 @@ void CheckUploadData(URLRequest* request) {
 
   uploaded_request.ParseFromString(upload_data);
 
-  EXPECT_EQ(kHostname, uploaded_request.hostname());
+  EXPECT_EQ(1u, expected_hostnames->count(uploaded_request.hostname()));
+  expected_hostnames->erase(uploaded_request.hostname());
+
   EXPECT_EQ(GetPEMEncodedChain(), uploaded_request.cert_chain());
   EXPECT_EQ(1, uploaded_request.pin().size());
   EXPECT_EQ(kDummyFailureLog, uploaded_request.pin().Get(0));
@@ -104,6 +113,7 @@ class TestCertificateErrorReporterNetworkDelegate : public NetworkDelegateImpl {
  public:
   TestCertificateErrorReporterNetworkDelegate()
       : url_request_destroyed_callback_(base::Bind(&base::DoNothing)),
+        all_url_requests_destroyed_callback_(base::Bind(&base::DoNothing)),
         num_requests_(0) {}
 
   ~TestCertificateErrorReporterNetworkDelegate() override {}
@@ -115,12 +125,26 @@ class TestCertificateErrorReporterNetworkDelegate : public NetworkDelegateImpl {
     EXPECT_EQ(expected_url_, request->url());
     EXPECT_EQ("POST", request->method());
 
-    CheckUploadData(request);
+    std::string uploaded_request_hostname;
+    CheckUploadData(request, &expected_hostnames_);
+    expected_hostnames_.erase(uploaded_request_hostname);
     return net::OK;
   }
 
   void OnURLRequestDestroyed(URLRequest* request) override {
     url_request_destroyed_callback_.Run();
+
+    if (expected_hostnames_.empty())
+      all_url_requests_destroyed_callback_.Run();
+  }
+
+  void ExpectHostname(const std::string& hostname) {
+    expected_hostnames_.insert(hostname);
+  }
+
+  void set_all_url_requests_destroyed_callback(
+      const base::Closure& all_url_requests_destroyed_callback) {
+    all_url_requests_destroyed_callback_ = all_url_requests_destroyed_callback;
   }
 
   void set_url_request_destroyed_callback(
@@ -136,8 +160,10 @@ class TestCertificateErrorReporterNetworkDelegate : public NetworkDelegateImpl {
 
  private:
   base::Closure url_request_destroyed_callback_;
+  base::Closure all_url_requests_destroyed_callback_;
   int num_requests_;
   GURL expected_url_;
+  std::set<std::string> expected_hostnames_;
 
   DISALLOW_COPY_AND_ASSIGN(TestCertificateErrorReporterNetworkDelegate);
 };
@@ -164,15 +190,50 @@ class CertificateErrorReporterTest : public ::testing::Test {
   TestURLRequestContext context_;
 };
 
+void SendReport(TestCertificateErrorReporterNetworkDelegate* network_delegate,
+                TestURLRequestContext* context,
+                const std::string& report_hostname,
+                const GURL& url,
+                int request_sequence_number) {
+  base::RunLoop run_loop;
+  network_delegate->set_url_request_destroyed_callback(run_loop.QuitClosure());
+
+  network_delegate->set_expected_url(url);
+  network_delegate->ExpectHostname(report_hostname);
+
+  CertificateErrorReporter reporter(context, url);
+
+  EXPECT_EQ(request_sequence_number, network_delegate->num_requests());
+
+  reporter.SendReport(CertificateErrorReporter::REPORT_TYPE_PINNING_VIOLATION,
+                      report_hostname, GetTestSSLInfo());
+  run_loop.Run();
+
+  EXPECT_EQ(request_sequence_number + 1, network_delegate->num_requests());
+}
+
 // Test that CertificateErrorReporter::SendReport creates a URLRequest
 // for the endpoint and sends the expected data.
 TEST_F(CertificateErrorReporterTest, SendReportSendsRequest) {
+  GURL url = net::URLRequestMockDataJob::GetMockHttpsUrl("dummy data", 1);
+  SendReport(network_delegate(), context(), kHostname, url, 0);
+}
+
+TEST_F(CertificateErrorReporterTest, SendMultipleReportsSequentially) {
+  GURL url = net::URLRequestMockDataJob::GetMockHttpsUrl("dummy data", 1);
+  SendReport(network_delegate(), context(), kHostname, url, 0);
+  SendReport(network_delegate(), context(), kSecondRequestHostname, url, 1);
+}
+
+TEST_F(CertificateErrorReporterTest, SendMultipleReportsSimultaneously) {
   base::RunLoop run_loop;
-  network_delegate()->set_url_request_destroyed_callback(
+  network_delegate()->set_all_url_requests_destroyed_callback(
       run_loop.QuitClosure());
 
   GURL url = net::URLRequestMockDataJob::GetMockHttpsUrl("dummy data", 1);
   network_delegate()->set_expected_url(url);
+  network_delegate()->ExpectHostname(kHostname);
+  network_delegate()->ExpectHostname(kSecondRequestHostname);
 
   CertificateErrorReporter reporter(context(), url);
 
@@ -180,9 +241,12 @@ TEST_F(CertificateErrorReporterTest, SendReportSendsRequest) {
 
   reporter.SendReport(CertificateErrorReporter::REPORT_TYPE_PINNING_VIOLATION,
                       kHostname, GetTestSSLInfo());
+  reporter.SendReport(CertificateErrorReporter::REPORT_TYPE_PINNING_VIOLATION,
+                      kSecondRequestHostname, GetTestSSLInfo());
+
   run_loop.Run();
 
-  EXPECT_EQ(1, network_delegate()->num_requests());
+  EXPECT_EQ(2, network_delegate()->num_requests());
 }
 
 // Test that pending URLRequests get cleaned up when the reporter is
@@ -196,6 +260,7 @@ TEST_F(CertificateErrorReporterTest, PendingRequestGetsDeleted) {
       base::FilePath(FILE_PATH_LITERAL("empty.html")),
       net::URLRequestMockHTTPJob::START, net::ERR_IO_PENDING);
   network_delegate()->set_expected_url(url);
+  network_delegate()->ExpectHostname(kHostname);
 
   EXPECT_EQ(0, network_delegate()->num_requests());
 
@@ -212,21 +277,8 @@ TEST_F(CertificateErrorReporterTest, PendingRequestGetsDeleted) {
 
 // Test that a request that returns an error gets cleaned up.
 TEST_F(CertificateErrorReporterTest, ErroredRequestGetsDeleted) {
-  base::RunLoop run_loop;
-  network_delegate()->set_url_request_destroyed_callback(
-      run_loop.QuitClosure());
-
   GURL url = net::URLRequestFailedJob::GetMockHttpsUrl(net::ERR_FAILED);
-  network_delegate()->set_expected_url(url);
-
-  EXPECT_EQ(0, network_delegate()->num_requests());
-
-  CertificateErrorReporter reporter(context(), url);
-  reporter.SendReport(CertificateErrorReporter::REPORT_TYPE_PINNING_VIOLATION,
-                      kHostname, GetTestSSLInfo());
-  run_loop.Run();
-
-  EXPECT_EQ(1, network_delegate()->num_requests());
+  SendReport(network_delegate(), context(), kHostname, url, 0);
 }
 
 }  // namespace
