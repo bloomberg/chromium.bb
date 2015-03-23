ad75d046a5f09b51fd7ddc1434fec675e5b5b7ed
reed <reed@google.com>
2015-03-23 15:54:37 -0700

    add SK_SUPPORT_LEGACY_INT_COLORMATRIX to stage skia change
    Will support this skia change: https://codereview.chromium.org/967943002/
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1031713003
    
    Cr-Commit-Position: refs/heads/master@{#321885}
    
    Former-commit-id: 533e541cc4cdfed4f1461e63eb19333b84a3b8da

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -269,6 +269,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_IGNORE_GPU_DITHER
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_INT_COLORMATRIX
+#   define SK_SUPPORT_LEGACY_INT_COLORMATRIX
+#endif
+
 #ifndef    SK_LEGACY_STROKE_CURVES
 #   define SK_LEGACY_STROKE_CURVES
 #endif
