7301f2f02816c5d44ee75dd0689c806c5aabdbda
Akira TAGOH <akira@tagoh.org>
2015-03-23 13:18:49 +0900

    Remove the dead code

--- src/fcdir.c
+++ src/fcdir.c
@@ -136,8 +136,6 @@ FcFileScanFontConfig (FcFontSet		*set,
 		ret = FcFalse;
 	    }
 	}
-	else if (font)
-	    FcPatternDestroy (font);
 	id++;
     } while (font && ret && id < count);
     return ret;
