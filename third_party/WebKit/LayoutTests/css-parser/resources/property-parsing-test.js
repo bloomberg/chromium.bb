248c2bdc7747bad71629bf4ed80d970f8ea91914
timloh@chromium.org <timloh@chromium.org>
2015-03-24 08:57:29 +0000

    Rewrite parsing-css-allowed-string-characters.html to use property-parsing-test.js
    This patch rewrites the test parsing-css-allowed-string-characters.html
    to use property-parsing-test.js. The file is renamed to allow it to go
    through the CQ.
    
    Review URL: https://codereview.chromium.org/1030543002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192420 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6e8e6e2791dbb42ee690780554fbe38efdc78d59

--- third_party/WebKit/LayoutTests/css-parser/resources/property-parsing-test.js
+++ third_party/WebKit/LayoutTests/css-parser/resources/property-parsing-test.js
@@ -16,15 +16,17 @@ function assert_valid_value(property, value, serializedValue) {
     if (arguments.length != 3)
         serializedValue = value;
 
+    stringifiedValue = JSON.stringify(value);
+
     test(function(){
         assert_true(CSS.supports(convert_to_dashes(property), value));
-    }, "CSS.supports('" + property + "', '" + value + "') should return true");
+    }, "CSS.supports('" + property + "', " + stringifiedValue + ") should return true");
 
     test(function(){
         var div = document.createElement('div');
         div.style[property] = value;
         assert_not_equals(div.style[property], "");
-    }, "e.style['" + property + "'] = '" + value + "' should set the value");
+    }, "e.style['" + property + "'] = " + stringifiedValue + " should set the value");
 
     test(function(){
         var div = document.createElement('div');
@@ -33,17 +35,19 @@ function assert_valid_value(property, value, serializedValue) {
         assert_equals(readValue, serializedValue);
         div.style[property] = readValue;
         assert_equals(div.style[property], readValue);
-    }, "Serialization should round-trip after setting e.style['" + property + "'] = '" + value + "'");
+    }, "Serialization should round-trip after setting e.style['" + property + "'] = " + stringifiedValue);
 }
 
 function assert_invalid_value(property, value) {
+    stringifiedValue = JSON.stringify(value);
+
     test(function(){
         assert_false(CSS.supports(convert_to_dashes(property), value));
-    }, "CSS.supports('" + property + "', '" + value + "') should return false");
+    }, "CSS.supports('" + property + "', " + stringifiedValue + ") should return false");
 
     test(function(){
         var div = document.createElement('div');
         div.style[property] = value;
         assert_equals(div.style[property], "");
-    }, "e.style['" + property + "'] = '" + value + "' should not set the value");
+    }, "e.style['" + property + "'] = " + stringifiedValue + " should not set the value");
 }
