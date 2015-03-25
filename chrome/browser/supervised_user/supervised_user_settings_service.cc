1e1cb4cff1e3442527321df1a0b68350673ce5e3
bauerb <bauerb@chromium.org>
2015-03-24 17:44:46 -0700

    Reland "Handle unsucessful JsonPrefStore initialization in SupervisedUserSettingsService."
    > According to crash reports, it's not very common but still possible for the JsonPrefStore used by SupervisedUserSettingsService to fail initialization (which happens if the profile directory doesn't exist).
    
    Review URL: https://codereview.chromium.org/960233003
    
    This reverts commit fd68afb157d3894f65cdb48c080303211a74b7a4 [formerly 3490af836c15f44229dedea4b3d80c1869d6e1fe].
    
    TBR=treib@chromium.org
    BUG=463646
    
    Review URL: https://codereview.chromium.org/1016423003
    
    Cr-Commit-Position: refs/heads/master@{#322105}
    
    Former-commit-id: 4f3a803c5fcdd82dfb7e6a134106ddde1dd7525c

--- chrome/browser/supervised_user/supervised_user_settings_service.cc
+++ chrome/browser/supervised_user/supervised_user_settings_service.cc
@@ -50,7 +50,10 @@ bool SettingShouldApplyToPrefs(const std::string& name) {
 }  // namespace
 
 SupervisedUserSettingsService::SupervisedUserSettingsService()
-    : active_(false), local_settings_(new base::DictionaryValue) {}
+    : active_(false),
+      initialization_failed_(false),
+      local_settings_(new base::DictionaryValue) {
+}
 
 SupervisedUserSettingsService::~SupervisedUserSettingsService() {}
 
@@ -97,7 +100,9 @@ void SupervisedUserSettingsService::SetActive(bool active) {
 }
 
 bool SupervisedUserSettingsService::IsReady() {
-  return store_->IsInitializationComplete();
+  // Initialization cannot be complete but have failed at the same time.
+  DCHECK(!(store_->IsInitializationComplete() && initialization_failed_));
+  return initialization_failed_ || store_->IsInitializationComplete();
 }
 
 void SupervisedUserSettingsService::Clear() {
@@ -299,9 +304,16 @@ void SupervisedUserSettingsService::OnPrefValueChanged(const std::string& key) {
 }
 
 void SupervisedUserSettingsService::OnInitializationCompleted(bool success) {
+  if (!success) {
+    // If this happens, it means the profile directory was not found. There is
+    // not much we can do, but the whole profile will probably be useless
+    // anyway. Just mark initialization as failed and continue otherwise,
+    // because subscribers might still expect to be called back.
+    initialization_failed_ = true;
+  }
+
   // TODO(bauerb): Temporary CHECK while investigating https://crbug.com/425785.
   // Remove (or change back to DCHECK) once the bug is fixed.
-  CHECK(success);
   CHECK(IsReady());
   InformSubscribers();
 }
@@ -354,7 +366,7 @@ base::DictionaryValue* SupervisedUserSettingsService::GetDictionaryAndSplitKey(
 
 scoped_ptr<base::DictionaryValue> SupervisedUserSettingsService::GetSettings() {
   DCHECK(IsReady());
-  if (!active_)
+  if (!active_ || initialization_failed_)
     return scoped_ptr<base::DictionaryValue>();
 
   scoped_ptr<base::DictionaryValue> settings(local_settings_->DeepCopy());
