aec2cad529f2f4d7305e5640fa4b58388d574b5a
rbyers <rbyers@chromium.org>
2015-03-26 18:24:40 -0700

    Revert of Enable gpu_times for all platforms other than windows. (patchset #1 id:1 of https://codereview.chromium.org/1029423002/)
    Reason for revert:
    
    I'm seeing most perf bots failing gpu_times ever since this landed (Eg.
    http://goo.gl/uLzMVe).
    
    I missed this earlier because we had a bunch of persistent redness today caused by multiple bad V8 rolls and didn't notice this crept in as well.
    
    Failure is just on cuteoverload.com with this error:
    MarkerMismatchError: Number or order of timeline markers does not match provided labels
    
    Original issue's description:
    > Enable gpu_times for all platforms other than windows.
    >
    > R=nednguyen@google.com
    > BUG=453131, 455292
    >
    > Committed: https://crrev.com/53f5b1b87f9e0ae524cd6d8615bafb7f30c80d5e [formerly 95325bb92cdce4b3c4fd93ae3509e72f89536c9c]
    > Cr-Commit-Position: refs/heads/master@{#322273}
    
    TBR=nednguyen@google.com,dyen@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=453131, 455292
    
    Review URL: https://codereview.chromium.org/1039083002
    
    Cr-Commit-Position: refs/heads/master@{#322514}
    
    Former-commit-id: fb35ddbcc65dfcb4841298f233076be87e06236e

--- tools/perf/benchmarks/gpu_times.py
+++ tools/perf/benchmarks/gpu_times.py
@@ -27,7 +27,7 @@ class _GPUTimes(benchmark.Benchmark):
     return (isinstance(value, gpu_timeline.GPUTimelineListOfValues) or
             isinstance(value, gpu_timeline.GPUTimelineValue))
 
-@benchmark.Disabled('win')  # http://crbug.com/453131
+@benchmark.Disabled  # http://crbug.com/455292
 class GPUTimesKeyMobileSites(_GPUTimes):
   """Measures GPU timeline metric on key mobile sites."""
   page_set = page_sets.KeyMobileSitesSmoothPageSet
@@ -36,7 +36,7 @@ class GPUTimesKeyMobileSites(_GPUTimes):
   def Name(cls):
     return 'gpu_times.key_mobile_sites_smooth'
 
-@benchmark.Disabled('win')  # http://crbug.com/453131
+@benchmark.Disabled  # http://crbug.com/455292
 class GPUTimesGpuRasterizationKeyMobileSites(_GPUTimes):
   """Measures GPU timeline metric on key mobile sites with GPU rasterization.
   """
@@ -48,7 +48,7 @@ class GPUTimesGpuRasterizationKeyMobileSites(_GPUTimes):
   def Name(cls):
     return 'gpu_times.gpu_rasterization.key_mobile_sites_smooth'
 
-@benchmark.Disabled('win')  # http://crbug.com/453131
+@benchmark.Disabled  # http://crbug.com/453131, http://crbug.com/455292
 class GPUTimesTop25Sites(_GPUTimes):
   """Measures GPU timeline metric for the top 25 sites."""
   page_set = page_sets.Top25SmoothPageSet
@@ -57,7 +57,7 @@ class GPUTimesTop25Sites(_GPUTimes):
   def Name(cls):
     return 'gpu_times.top_25_smooth'
 
-@benchmark.Disabled('win')  # http://crbug.com/453131
+@benchmark.Disabled  # http://crbug.com/455292
 class GPUTimesGpuRasterizationTop25Sites(_GPUTimes):
   """Measures GPU timeline metric for the top 25 sites with GPU rasterization.
   """
