487d75949e8ab42b505029662cbb4217c601dc43
timloh@chromium.org <timloh@chromium.org>
2015-03-27 00:32:50 +0000

    Promote new CSS parser to experimental
    This patch promotes the new CSS parser to experimental, rebaselining the
    tests with changed behaviour. The new parser will now handle everything
    except for when the inspector uses a CSSParserObserver, which I'll add
    support for soon.
    
    In tests I ran on a Nexus 7 (2013 model), the new parser performed on
    average 18% faster than the existing parser (independent of stylesheet
    size). The only case which which didn't show an improvement were those
    with extremely large strings ("url(data:font/opentype;base64,..." for
    example).
    
    There are quite a few really small behaviour changes. The following
    changes affected existing layout tests which have been updated in this
    patch:
    
    - <an+b> serialization now matches the spec instead of just copying the
    input. http://dev.w3.org/csswg/css-syntax/#serializing-anb
    
    - @supports requires whitespace after not/or/and and before or/and. This
    matches the spec and Firefox. Usage of the invalid syntax is virtually
    zero. http://dev.w3.org/csswg/css-conditional-3/#at-supports
    
    - CSSSupportsRule now serializes the condition instead of just copying
    the input.
    
    - Escapes with value 0 get replaced by U+FFFD
    http://dev.w3.org/csswg/css-syntax/#consume-escaped-code-point
    
    - Namespaces in selectors are only allowed in front of element names.
    http://dev.w3.org/csswg/selectors-4/#grammar
    
    - \r\n is considered a single whitespace (a single whitespace after an
    escaped character is ignored, so we won't leave an extra whitespace now).
    http://dev.w3.org/csswg/css-syntax/#input-preprocessing
    
    - Escapes are now allowed in dimension types (although it probably never
    makes sense to write 16\70\78 instead of 16px...)
    
    - Strings will now be terminated at a newline (a <bad-string-token> is
    produced), instead of continuing to look ahead of an ending quote.
    http://dev.w3.org/csswg/css-syntax/#consume-string-token
    
    I also noticed a few more behaviour changes which our layout tests don't
    cover yet. Most of these are covered by unit tests, but I'll add layout
    tests for some of these shortly:
    
    - Null characters are now replaced by U+FFFD during tokenization.
    http://dev.w3.org/csswg/css-syntax/#input-preprocessing
    
    - We now reject scientific notation of the form "16e-px"
    - We now reject percentages with extra %s (left: 10%%%)
    
    - We now allow :nth-* case insensitively
    - We now allow idents to start with --
    
    - We now parse <an+b> and <quirky-color> in terms of CSS tokens
    http://dev.w3.org/csswg/css-syntax/#the-anb-type
    https://quirks.spec.whatwg.org/#the-hashless-hex-color-quirk
    
    BUG=330389
    
    Review URL: https://codereview.chromium.org/1015303002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192641 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c57eb4f60b3d2ae600daa21842cf66c65c89601b

--- third_party/WebKit/LayoutTests/fast/css/script-tests/string-quote-binary.js
+++ third_party/WebKit/LayoutTests/fast/css/script-tests/string-quote-binary.js
@@ -5,23 +5,24 @@ description(
 var inputs = ["'\\0\\1\\2\\3\\4\\5\\6\\7\\8\\9\\a\\b\\c\\d\\e\\f'",
               "'\\10\\11\\12\\13\\14\\15\\16\\17\\18\\19\\1a\\1b\\1c\\1d\\1e\\1f\\7f'",
               "'\\A\\B\\C\\D\\E\\F\\1A\\1B\\1C\\1D\\1E\\1F\\7F'",
-              "'\\0 \\1 \\2 '",
-              "'\\0  \\1  \\2  '",
-              "'\\0   \\1   \\2   '",
+              "'\\3 \\1 \\2 '",
+              "'\\3  \\1  \\2  '",
+              "'\\3   \\1   \\2   '",
               "'\\00000f\\00000g'",
-              "'\\0 0\\0 1\\0 2\\0 3\\0 4\\0 5\\0 6\\0 7\\0 8\\0 9'",
-              "'\\0 A\\0 B\\0 C\\0 D\\0 E\\0 F\\0 G'",
-              "'\\0 a\\0 b\\0 c\\0 d\\0 e\\0 f\\0 g'"];
-var expected = ["'\\0\\1\\2\\3\\4\\5\\6\\7\\8\\9\\a\\b\\c\\d\\e\\f'",
+              "'\\1 0\\1 1\\1 2\\1 3\\1 4\\1 5\\1 6\\1 7\\1 8\\1 9'",
+              "'\\1 A\\1 B\\1 C\\1 D\\1 E\\1 F\\1 G'",
+              "'\\1 a\\1 b\\1 c\\1 d\\1 e\\1 f\\1 g'"];
+// Null is replaced with U+FFFD as per css-syntax
+var expected = ["'\uFFFD\\1\\2\\3\\4\\5\\6\\7\\8\\9\\a\\b\\c\\d\\e\\f'",
                 "'\\10\\11\\12\\13\\14\\15\\16\\17\\18\\19\\1a\\1b\\1c\\1d\\1e\\1f\\7f'",
                 "'\\a\\b\\c\\d\\e\\f\\1a\\1b\\1c\\1d\\1e\\1f\\7f'",
-                "'\\0\\1\\2'",          // No space after each control character.
-                "'\\0  \\1  \\2  '",    // One space delimiter (that will be ignored by the CSS parser), plus one actual space.
-                "'\\0   \\1   \\2   '", // One space delimiter, plus two actual spaces.
-                "'\\f\\0g'",
-                "'\\0 0\\0 1\\0 2\\0 3\\0 4\\0 5\\0 6\\0 7\\0 8\\0 9'", // Need a space before [0-9A-Fa-f], but not before [Gg].
-                "'\\0 A\\0 B\\0 C\\0 D\\0 E\\0 F\\0G'",
-                "'\\0 a\\0 b\\0 c\\0 d\\0 e\\0 f\\0g'"];
+                "'\\3\\1\\2'",          // No space after each control character.
+                "'\\3  \\1  \\2  '",    // One space delimiter (that will be ignored by the CSS parser), plus one actual space.
+                "'\\3   \\1   \\2   '", // One space delimiter, plus two actual spaces.
+                "'\\f\uFFFDg'",
+                "'\\1 0\\1 1\\1 2\\1 3\\1 4\\1 5\\1 6\\1 7\\1 8\\1 9'", // Need a space before [0-9A-Fa-f], but not before [Gg].
+                "'\\1 A\\1 B\\1 C\\1 D\\1 E\\1 F\\1G'",
+                "'\\1 a\\1 b\\1 c\\1 d\\1 e\\1 f\\1g'"];
 
 var testElement = document.createElement('div');
 for (var i = 0; i < inputs.length; ++i) {
