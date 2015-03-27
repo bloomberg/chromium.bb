babca6ecefdc8856b543f70ae8fee7a1a2045052
dyen <dyen@chromium.org>
2015-03-27 16:37:29 -0700

    Enable gpu_times for android.
    The gpu_times metric has been known to be flaky but it seems that the
    flakiness only affects desktop devices. Enable it on android only.
    
    R=nednguyen@chromium.org
    BUG=455292
    
    Review URL: https://codereview.chromium.org/1041963002
    
    Cr-Commit-Position: refs/heads/master@{#322677}
    
    Former-commit-id: b2512c2807be33d2956bb2d4ed5dfda7979a8a14

--- tools/perf/benchmarks/gpu_times.py
+++ tools/perf/benchmarks/gpu_times.py
@@ -27,7 +27,7 @@ class _GPUTimes(benchmark.Benchmark):
     return (isinstance(value, gpu_timeline.GPUTimelineListOfValues) or
             isinstance(value, gpu_timeline.GPUTimelineValue))
 
-@benchmark.Disabled  # http://crbug.com/455292
+@benchmark.Enabled('android')  # http://crbug.com/453131
 class GPUTimesKeyMobileSites(_GPUTimes):
   """Measures GPU timeline metric on key mobile sites."""
   page_set = page_sets.KeyMobileSitesSmoothPageSet
@@ -36,7 +36,7 @@ class GPUTimesKeyMobileSites(_GPUTimes):
   def Name(cls):
     return 'gpu_times.key_mobile_sites_smooth'
 
-@benchmark.Disabled  # http://crbug.com/455292
+@benchmark.Enabled('android')  # http://crbug.com/453131
 class GPUTimesGpuRasterizationKeyMobileSites(_GPUTimes):
   """Measures GPU timeline metric on key mobile sites with GPU rasterization.
   """
@@ -48,7 +48,7 @@ class GPUTimesGpuRasterizationKeyMobileSites(_GPUTimes):
   def Name(cls):
     return 'gpu_times.gpu_rasterization.key_mobile_sites_smooth'
 
-@benchmark.Disabled  # http://crbug.com/453131, http://crbug.com/455292
+@benchmark.Enabled('android')  # http://crbug.com/453131
 class GPUTimesTop25Sites(_GPUTimes):
   """Measures GPU timeline metric for the top 25 sites."""
   page_set = page_sets.Top25SmoothPageSet
@@ -57,7 +57,7 @@ class GPUTimesTop25Sites(_GPUTimes):
   def Name(cls):
     return 'gpu_times.top_25_smooth'
 
-@benchmark.Disabled  # http://crbug.com/455292
+@benchmark.Enabled('android')  # http://crbug.com/453131
 class GPUTimesGpuRasterizationTop25Sites(_GPUTimes):
   """Measures GPU timeline metric for the top 25 sites with GPU rasterization.
   """
@@ -68,4 +68,3 @@ class GPUTimesGpuRasterizationTop25Sites(_GPUTimes):
   @classmethod
   def Name(cls):
     return 'gpu_times.gpu_rasterization.top_25_smooth'
-
