16fc491641649cc2ce2475dc43595c4d153f62a8
pkasting@chromium.org <pkasting@chromium.org>
2015-03-28 03:12:52 +0000

    Revert of range.getBoundingClientRect() returns incorrect value for collapsed ranges (patchset #3 id:40001 of https://codereview.chromium.org/998263002/)
    Reason for revert:
    Seems to have broken CvoxShadowUnitTest.MultilineWrap in the chromevox_tests.
    
    Original issue's description:
    > range.getBoundingClientRect() returns incorrect value for collapsed ranges
    > 
    > Current implementation returns empty rectangle if
    > either width or height is zero. But as per W3C specification [1],
    > getBoundingClientRect() should return union of rectangles
    > of which the height or width is not zero.
    > 
    > [1] http://www.w3.org/TR/cssom-view/#dom-range-getclientrects
    > 
    > BUG=435438
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=192699
    
    TBR=keishi@chromium.org,vivekg@chromium.org,eae@chromium.org,kandalkar.abhijeet58@gmail.com,abhijeet.k@samsung.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=435438
    
    Review URL: https://codereview.chromium.org/1044593002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192714 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 489026519c55a21db14ff4898497bcab0e5e1fe9

--- third_party/WebKit/Source/core/dom/Range.cpp
+++ third_party/WebKit/Source/core/dom/Range.cpp
@@ -1775,18 +1775,9 @@ FloatRect Range::boundingRect() const
     Vector<FloatQuad> quads;
     getBorderAndTextQuads(quads);
 
-    if (quads.isEmpty())
-        return FloatRect();
-
     FloatRect result;
-    for (const FloatQuad& quad : quads) {
-        if (result.isEmpty()) {
-            result = quad.boundingBox();
-            continue;
-        }
-
-        result.uniteEvenIfEmpty(quad.boundingBox());
-    }
+    for (const FloatQuad& quad : quads)
+        result.unite(quad.boundingBox());
 
     return result;
 }
