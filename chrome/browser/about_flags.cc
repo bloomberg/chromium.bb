5891b69d8065205273e0f643d3954a75d13f742d
pdr <pdr@chromium.org>
2015-03-31 15:51:58 -0700

    [slimming paint] Add slimming paint to chrome://flags
    As we get closer to shipping we would like a lower-friction way to
    enable slimming paint, particularly on android. This patch adds an
    about://flags entry for enabling slimming paint. This flag will be
    removed once slimming paint ships.
    
    Review URL: https://codereview.chromium.org/1041173003
    
    Cr-Commit-Position: refs/heads/master@{#323136}
    
    Former-commit-id: cd73ed36b9af06489a964489ad7629695fa07b78

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -778,6 +778,13 @@ const Experiment kExperiments[] = {
         switches::kDisableThreadedGpuRasterization)
   },
   {
+    "enable-slimming-paint",
+    IDS_FLAGS_ENABLE_SLIMMING_PAINT_NAME,
+    IDS_FLAGS_ENABLE_SLIMMING_PAINT_DESCRIPTION,
+    kOsAll,
+    SINGLE_VALUE_TYPE(switches::kEnableSlimmingPaint)
+  },
+  {
     "enable-experimental-web-platform-features",
     IDS_FLAGS_EXPERIMENTAL_WEB_PLATFORM_FEATURES_NAME,
     IDS_FLAGS_EXPERIMENTAL_WEB_PLATFORM_FEATURES_DESCRIPTION,
