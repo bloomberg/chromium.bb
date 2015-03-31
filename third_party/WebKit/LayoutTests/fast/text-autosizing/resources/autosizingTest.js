c4bd858839f5b3ad20aed059c2513e45cee076ca
wkorman@chromium.org <wkorman@chromium.org>
2015-03-31 03:30:14 +0000

    Fix missing space in autosizing info message.
    BUG=
    
    Review URL: https://codereview.chromium.org/1045933003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192791 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c780ad7080d81d8e3300cff324079c78bf83fd2a

--- third_party/WebKit/LayoutTests/fast/text-autosizing/resources/autosizingTest.js
+++ third_party/WebKit/LayoutTests/fast/text-autosizing/resources/autosizingTest.js
@@ -13,7 +13,7 @@ function initAutosizingTest() {
         window.internals.settings.setTextAutosizingEnabled(true);
         setWindowSizeOverride(320, 480);
     } else if (window.console && console.warn) {
-        console.warn("This test depends on Text Autosizing being enabled. Run with content shell"
+        console.warn("This test depends on Text Autosizing being enabled. Run with content shell "
                 + "and --run-layout-test or manually enable Text Autosizing and either use a "
                 + "mobile device with 320px device-width (like Nexus S or iPhone), or define "
                 + "DEBUG_TEXT_AUTOSIZING_ON_DESKTOP.");
