33adc8f7154716c15175c2806b6b8f5ff7c834d6
jkarlin@chromium.org <jkarlin@chromium.org>
2015-03-31 21:53:53 +0000

    [NetInfo] Usage tracking
    BUG=368358
    
    Review URL: https://codereview.chromium.org/1049743002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192859 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: be90756f7a73b304af886d6737dbfa79965cf2c4

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -677,6 +677,7 @@ public:
         UpgradeInsecureRequestsUpgradedRequest = 741,
         DocumentDesignMode = 742,
         GlobalCacheStorage = 743,
+        NetInfo = 744,
 
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
