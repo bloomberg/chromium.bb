67d6a0c20f5eeab71ae79b77c629c128d4b14a53
horo@chromium.org <horo@chromium.org>
2015-04-01 02:26:02 +0000

    [DevTools] Fix bug in WebInspector.ServiceWorkerManager._workerVersionUpdated
    In current implementation the registration is not displayed which has both non-REDUNDANT and REDUNDANT versions.
    If the registration has one non-REDUNDANT version, it should be displayed.
    
    BUG=466871
    
    Review URL: https://codereview.chromium.org/1046833002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@192894 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6fcc35d58a4d6b53b167803e09c058cb4e4cd62e

--- third_party/WebKit/LayoutTests/http/tests/inspector/service-workers/service-workers-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/service-workers/service-workers-test.js
@@ -5,16 +5,6 @@ InspectorTest.registerServiceWorker = function(script, scope)
     return new Promise(function(resolve, reject){
         var args = {script: script, scope: scope};
         var jsonArgs = JSON.stringify(args).replace(/\"/g, "\\\"");
-        function innerCallback(msg)
-        {
-            if (msg.messageText.indexOf("registerServiceWorker success") !== -1)
-                resolve();
-            else if (msg.messageText.indexOf("registerServiceWorker fail") !== -1)
-                reject();
-            else
-                InspectorTest.addConsoleSniffer(innerCallback);
-        }
-        InspectorTest.addConsoleSniffer(innerCallback);
         InspectorTest.evaluateInPage("registerServiceWorker(\"" + jsonArgs + "\")");
     });
 }
@@ -24,17 +14,53 @@ InspectorTest.unregisterServiceWorker = function(scope)
     return new Promise(function(resolve, reject){
         var args = {scope: scope};
         var jsonArgs = JSON.stringify(args).replace(/\"/g, "\\\"");
-        function innerCallback(msg)
-        {
-            if (msg.messageText.indexOf("unregisterServiceWorker success") !== -1)
-                resolve();
-            else if (msg.messageText.indexOf("unregisterServiceWorker fail") !== -1)
-                reject();
-            else
-                InspectorTest.addConsoleSniffer(innerCallback);
-        }
-        InspectorTest.addConsoleSniffer(innerCallback);
         InspectorTest.evaluateInPage("unregisterServiceWorker(\"" + jsonArgs + "\")");
+    });
+}
+
+function replaceInnerTextAll(rootElement, selectors, replacementString)
+{
+    var elements = rootElement.querySelectorAll(selectors);
+    for (var i = 0; i < elements.length; i++)
+        elements[i].innerText = replacementString;
+}
+
+function modifyTestUnfriendlyText(rootElement)
+{
+    replaceInnerTextAll(rootElement, ".service-worker-script-last-modified", "LAST-MODIFIED");
+    replaceInnerTextAll(rootElement, ".service-worker-script-response-time", "RESPONSE-TIME");
+}
+
+InspectorTest.dumpServiceWorkersView = function(scopes)
+{
+    var swView = WebInspector.panels.resources.visibleView;
+    modifyTestUnfriendlyText(swView._root);
+    var results = [];
+    var expectedTitles = scopes.map(function(scope) {return "Scope: " + (new URL(scope).pathname)});
+    results.push("==== ServiceWorkersView ====");
+    results.push(swView._root.querySelector(".service-workers-origin-title").innerText);
+    var registrationElements = swView._root.querySelectorAll(".service-workers-registration");
+    for (var i = 0; i < registrationElements.length; i++) {
+        var registrationElement = registrationElements[i];
+        var title = registrationElement.querySelector(".service-workers-registration-title").innerText;
+        if (!expectedTitles.some(function(expectedTitle) { return title.indexOf(expectedTitle) != -1; }))
+            continue;
+        results.push(title);
+        var versionElements = registrationElement.querySelectorAll(".service-workers-version-row");
+        for (var j = 0; j < versionElements.length; j++) {
+            if (versionElements[j].innerText.length)
+                results.push(versionElements[j].innerText);
+        }
+    }
+    results.push("============================");
+    return results;
+}
+
+InspectorTest.deleteServiceWorkerRegistration = function(scope)
+{
+    InspectorTest.serviceWorkerManager.registrations().valuesArray().map(function (registration) {
+        if (registration.scopeURL == scope)
+            InspectorTest.serviceWorkerManager.deleteRegistration(registration.id);
     });
 }
 
