da82b9b8dfa1115bd81a415ab81dbd2f9545c119
pan.deng <pan.deng@intel.com>
2015-03-31 20:22:05 -0700

    Enable skp dump for SP
    BUG=465232
    
    Review URL: https://codereview.chromium.org/1039073003
    
    Cr-Commit-Position: refs/heads/master@{#323177}
    
    Former-commit-id: 0bd2a738f107ad7021c70bfd36ae41e4565fe946

--- cc/layers/picture_layer.cc
+++ cc/layers/picture_layer.cc
@@ -181,8 +181,26 @@ skia::RefPtr<SkPicture> PictureLayer::GetPicture() const {
   if (!DrawsContent())
     return skia::RefPtr<SkPicture>();
 
-  int width = bounds().width();
-  int height = bounds().height();
+  gfx::Size layer_size = bounds();
+  const LayerTreeSettings& settings = layer_tree_host()->settings();
+
+  if (settings.use_display_lists) {
+    scoped_ptr<RecordingSource> recording_source;
+    recording_source.reset(
+        new DisplayListRecordingSource(settings.default_tile_grid_size));
+    Region recording_invalidation;
+    recording_source->UpdateAndExpandInvalidation(
+        client_, &recording_invalidation, layer_size, gfx::Rect(layer_size),
+        update_source_frame_number_, RecordingSource::RECORD_NORMALLY);
+
+    scoped_refptr<RasterSource> raster_source =
+        recording_source->CreateRasterSource(false);
+
+    return raster_source->GetFlattenedPicture();
+  }
+
+  int width = layer_size.width();
+  int height = layer_size.height();
 
   SkPictureRecorder recorder;
   SkCanvas* canvas = recorder.beginRecording(width, height, nullptr, 0);
