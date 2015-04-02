8695a312c17a9acaf581567ee441ca41e83d1162
skyostil <skyostil@chromium.org>
2015-04-02 06:52:58 -0700

    Re-enable spaceport on linux
    This test doesn't appear to be crashing anymore.
    
    BUG=469157
    
    Review URL: https://codereview.chromium.org/1046183002
    
    Cr-Commit-Position: refs/heads/master@{#323474}
    
    Former-commit-id: 8974ede5746376319c6c545e6beebeda22b66cda

--- tools/perf/benchmarks/spaceport.py
+++ tools/perf/benchmarks/spaceport.py
@@ -94,8 +94,7 @@ class _SpaceportMeasurement(page_test.PageTest):
 
 
 # crbug.com/166703: This test frequently times out on Windows.
-# crbug.com/469224: Test is failing on linux as well.
-@benchmark.Disabled('mac', 'win', 'linux')
+@benchmark.Disabled('mac', 'win')
 class Spaceport(benchmark.Benchmark):
   """spaceport.io's PerfMarks benchmark.
 
