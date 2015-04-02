f75b9eb717a5a6c17d05a50b6975b513288ea7e5
cwgreene <cwgreene@amazon.com>
2015-04-02 13:42:04 -0700

    Expose CC.host_wrapper and CXX.host_wrapper.
    Currently, the gyp variables CC_wrapper and CXX_wrapper can be set in
    the chromium.gyp_env file. However, the host compiler wrapper variable
    cannot. By exposing this variable, embedders can more easily
    use tools like ccache or icecream when developing for android.
    
    BUG=471340
    
    Review URL: https://codereview.chromium.org/1044423005
    
    Cr-Commit-Position: refs/heads/master@{#323557}
    
    Former-commit-id: 73e65d4adbbc1cc83f7e59b2dfe60107c2dd186e

--- build/gyp_helper.py
+++ build/gyp_helper.py
@@ -25,10 +25,12 @@ def apply_gyp_environment_from_file(file_path):
   supported_vars = (
       'CC',
       'CC_wrapper',
+      'CC.host_wrapper',
       'CHROMIUM_GYP_FILE',
       'CHROMIUM_GYP_SYNTAX_CHECK',
       'CXX',
       'CXX_wrapper',
+      'CXX.host_wrapper',
       'GYP_DEFINES',
       'GYP_GENERATOR_FLAGS',
       'GYP_CROSSCOMPILE',
