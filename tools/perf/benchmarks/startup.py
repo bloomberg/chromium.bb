6de4efd07aa788a2349ddfad417347029fdac736
erikchen <erikchen@chromium.org>
2015-04-03 11:28:20 -0700

    Telemetry: Add simple startup benchmark that uses a large profile.
    This CL adds two benchmarks: "startup.large_profile.cold.blank_page" and
    "startup.large_profile.warm.blank_page". It also adds pre-generated large
    profiles for windows, mac, and linux.
    
    BUG=472711
    
    Review URL: https://codereview.chromium.org/1056623002
    
    Cr-Commit-Position: refs/heads/master@{#323777}
    
    Former-commit-id: bc7b42b8c88eb24f92b62010d37d8c53d9b242aa

--- tools/perf/benchmarks/startup.py
+++ tools/perf/benchmarks/startup.py
@@ -57,3 +57,35 @@ class StartupWarmBlankPage(_StartupWarm):
   @classmethod
   def Name(cls):
     return 'startup.warm.blank_page'
+
+@benchmark.Enabled('has tabs')
+@benchmark.Enabled('win', 'linux', 'mac')
+class StartupLargeProfileColdBlankPage(_StartupCold):
+  """Measures cold startup time with a large profile."""
+  tag = 'cold'
+  page_set = page_sets.BlankPageSet
+  options = {'pageset_repeat': 1}
+
+  def __init__(self, max_failures=None):
+    super(StartupLargeProfileColdBlankPage, self).__init__(max_failures)
+    self.generated_profile_archive = "large_profile.zip"
+
+  @classmethod
+  def Name(cls):
+    return 'startup.large_profile.cold.blank_page'
+
+@benchmark.Enabled('has tabs')
+@benchmark.Enabled('win', 'linux', 'mac')
+class StartupLargeProfileWarmBlankPage(_StartupWarm):
+  """Measures warm startup time with a large profile."""
+  tag = 'warm'
+  page_set = page_sets.BlankPageSet
+  options = {'pageset_repeat': 1}
+
+  def __init__(self, max_failures=None):
+    super(StartupLargeProfileWarmBlankPage, self).__init__(max_failures)
+    self.generated_profile_archive = "large_profile.zip"
+
+  @classmethod
+  def Name(cls):
+    return 'startup.large_profile.warm.blank_page'
