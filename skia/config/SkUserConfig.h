e24295749d681505afd80154784539efe06d9f30
reed <reed@chromium.org>
2015-04-02 22:34:14 -0700

    add SK_SUPPORT_LEGACY_SCALAR_XFERMODES to stage skia speedup for xfermodes
    needed to enable opts in https://codereview.chromium.org/1043413002/
    
    BUG=
    TBR=fmalita@chromium.org
    NOTRY=True
    ... cq not working :(
    
    Review URL: https://codereview.chromium.org/1054083002
    
    Cr-Commit-Position: refs/heads/master@{#323638}
    
    Former-commit-id: d9d94b9352486e833787988d909a523a8bb0e894

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -253,6 +253,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_GETDEVICE
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_SCALAR_XFERMODES
+#   define SK_SUPPORT_LEGACY_SCALAR_XFERMODES
+#endif
+
 #ifndef    SK_SUPPORT_LEGACY_PUBLIC_IMAGEINFO_FIELDS
 #   define SK_SUPPORT_LEGACY_PUBLIC_IMAGEINFO_FIELDS
 #endif
