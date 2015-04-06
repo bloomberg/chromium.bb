ef5f1c9ad81ac455a62fa9ae56822dd430396628
khorimoto <khorimoto@chromium.org>
2015-04-06 14:13:11 -0700

    Localize the Accessibility Page.
    BUG=472677
    
    Review URL: https://codereview.chromium.org/1061703003
    
    Cr-Commit-Position: refs/heads/master@{#323952}
    
    Former-commit-id: 6f6eba38275695220ec63e358d83d9e3b548c7ad

--- chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
@@ -13,15 +13,77 @@ namespace {
 // the following name. These names must be kept in sync.
 const char kLocalizedStringsFile[] = "strings.js";
 
+void AddA11yStrings(content::WebUIDataSource* html_source) {
+  html_source->AddLocalizedString(
+      "a11yPageTitle", IDS_MD_SETTINGS_ACCESSIBILITY_PAGE_TITLE);
+  html_source->AddLocalizedString(
+      "accessibilityMoreFeaturesLink",
+      IDS_SETTINGS_ACCESSIBILITY_MORE_FEATURES_LINK);
+  html_source->AddLocalizedString(
+      "accessibilityOptionsInMenuLabel",
+      IDS_SETTINGS_ACCESSIBILITY_OPTIONS_IN_MENU_LABEL);
+  html_source->AddLocalizedString(
+      "accessibilityLargeMouseCursorLabel",
+      IDS_SETTINGS_ACCESSIBILITY_LARGE_MOUSE_CURSOR_LABEL);
+  html_source->AddLocalizedString(
+      "accessibilityHighContrastLabel",
+      IDS_SETTINGS_ACCESSIBILITY_HIGH_CONTRAST_LABEL);
+  html_source->AddLocalizedString(
+      "accessibilityStickyKeysLabel",
+      IDS_SETTINGS_ACCESSIBILITY_STICKY_KEYS_LABEL);
+  html_source->AddLocalizedString(
+      "accessibilityStickyKeysSublabel",
+      IDS_SETTINGS_ACCESSIBILITY_STICKY_KEYS_SUBLABEL);
+  html_source->AddLocalizedString(
+      "accessibilityChromeVoxLabel",
+      IDS_SETTINGS_ACCESSIBILITY_CHROMEVOX_LABEL);
+  html_source->AddLocalizedString(
+      "accessibilityChromeVoxSublabel",
+      IDS_SETTINGS_ACCESSIBILITY_CHROMEVOX_SUBLABEL);
+  html_source->AddLocalizedString(
+      "accessibilityScreenMagnifierLabel",
+      IDS_SETTINGS_ACCESSIBILITY_SCREEN_MAGNIFIER_LABEL);
+  html_source->AddLocalizedString(
+      "accessibilityTapDraggingLabel",
+      IDS_SETTINGS_ACCESSIBILITY_TAP_DRAGGING_LABEL);
+  html_source->AddLocalizedString(
+      "accessibilityClickOnStopLabel",
+      IDS_SETTINGS_ACCESSIBILITY_CLICK_ON_STOP_LABEL);
+  html_source->AddLocalizedString(
+      "accessibilityDelayBeforeClickLabel",
+      IDS_SETTINGS_ACCESSIBILITY_DELAY_BEFORE_CLICK_LABEL);
+  html_source->AddLocalizedString(
+      "accessibilityDelayBeforeClickExtremelyShort",
+      IDS_SETTINGS_ACCESSIBILITY_DELAY_BEFORE_CLICK_EXTREMELY_SHORT);
+  html_source->AddLocalizedString(
+      "accessibilityDelayBeforeClickVeryShort",
+      IDS_SETTINGS_ACCESSIBILITY_DELAY_BEFORE_CLICK_VERY_SHORT);
+  html_source->AddLocalizedString(
+      "accessibilityDelayBeforeClickShort",
+      IDS_SETTINGS_ACCESSIBILITY_DELAY_BEFORE_CLICK_SHORT);
+  html_source->AddLocalizedString(
+      "accessibilityDelayBeforeClickLong",
+      IDS_SETTINGS_ACCESSIBILITY_DELAY_BEFORE_CLICK_LONG);
+  html_source->AddLocalizedString(
+      "accessibilityDelayBeforeClickVeryLong",
+      IDS_SETTINGS_ACCESSIBILITY_DELAY_BEFORE_CLICK_VERY_LONG);
+  html_source->AddLocalizedString(
+      "accessibilityOnScreenKeyboardLabel",
+      IDS_SETTINGS_ACCESSIBILITY_ON_SCREEN_KEYBOARD_LABEL);
+}
+
+void AddDownloadsStrings(content::WebUIDataSource* html_source) {
+  html_source->AddLocalizedString(
+      "downloadsPageTitle", IDS_MD_SETTINGS_DOWNLOADS_PAGE_TITLE);
+}
+
 }  // namespace
 
 namespace settings {
 
 void AddLocalizedStrings(content::WebUIDataSource* html_source) {
-  html_source->AddLocalizedString(
-      "a11yPageTitle", IDS_MD_SETTINGS_ACCESSIBILITY_PAGE_TITLE);
-  html_source->AddLocalizedString(
-      "downloadsPageTitle", IDS_MD_SETTINGS_DOWNLOADS_PAGE_TITLE);
+  AddA11yStrings(html_source);
+  AddDownloadsStrings(html_source);
 
   html_source->SetJsonPath(kLocalizedStringsFile);
 }
