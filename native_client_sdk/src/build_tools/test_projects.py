e97e8078df0f578613b36739559aa3df0c513d91
lionel.g.landwerlin <lionel.g.landwerlin@intel.com>
2015-04-07 02:16:29 -0700

    nacl sdk: add PPB_VideoEncoder example
    BUG=455409
    
    Review URL: https://codereview.chromium.org/1037793003
    
    Cr-Commit-Position: refs/heads/master@{#324026}
    
    Former-commit-id: 8358371b07de0489a18a0f182188cdbf1c54919d

--- native_client_sdk/src/build_tools/test_projects.py
+++ native_client_sdk/src/build_tools/test_projects.py
@@ -73,6 +73,7 @@ DISABLED_TESTS = [
     # http://crbug.com/262379.
     {'name': 'graphics_3d', 'platform': ('win', 'linux', 'mac')},
     {'name': 'video_decode', 'platform': ('win', 'linux', 'mac')},
+    {'name': 'video_encode', 'platform': ('win', 'linux', 'mac')},
     # media_stream_audio uses audio input devices which are not supported.
     {'name': 'media_stream_audio', 'platform': ('win', 'linux', 'mac')},
     # media_stream_video uses 3D and webcam which are not supported.
