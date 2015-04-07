a6bc699e93403ff6cfddf66eb3d515c3c2435b0a
aiolos <aiolos@chromium.org>
2015-04-07 11:06:38 -0700

    Revert of Revert of Revert of Revert of Revert of Enable ServiceWorkerPerfTest (service_worker.service_worker) (patchset #1 id:1 of https://codereview.chromium.org/1055533003/)
    Reason for revert:
    This is still failing on Android1.
    
    crbug.com/474658
    
    Original issue's description:
    > Revert of Revert of Revert of Revert of Enable ServiceWorkerPerfTest (service_worker.service_worker) (patchset #1 id:1 of https://codereview.chromium.org/1000273003/)
    >
    > Reason for revert:
    > screenrecord is installed on Android One trybots.
    > https://code.google.com/p/chromium/issues/detail?id=469841#c8
    >
    > Original issue's description:
    > > Revert of Revert of Revert of Enable ServiceWorkerPerfTest (service_worker.service_worker) (patchset #1 id:1 of https://codereview.chromium.org/1027743004/)
    > >
    > > Reason for revert:
    > > Failing on Android One perf bot which doesn't have "screenrecord".
    > >
    > > http://crbug.com/469841
    > >
    > > Original issue's description:
    > > > Revert of Revert of Enable ServiceWorkerPerfTest (service_worker.service_worker) (patchset #1 id:1 of https://codereview.chromium.org/959793002/)
    > > >
    > > > Reason for revert:
    > > > avconv is available on Nexus 10.
    > > > https://code.google.com/p/chromium/issues/detail?id=433943#c37
    > > >
    > > > Original issue's description:
    > > > > Revert of Enable ServiceWorkerPerfTest (service_worker.service_worker) (patchset #1 id:1 of https://codereview.chromium.org/954663002/)
    > > > >
    > > > > Reason for revert:
    > > > > Failing on Nexus 10.
    > > > >
    > > > > Original issue's description:
    > > > > > Enable ServiceWorkerPerfTest (service_worker.service_worker)
    > > > > >
    > > > > > I think https://codereview.chromium.org/884573004/ fixed the ServiceWorker test flakiness.
    > > > > >
    > > > > > BUG=433943
    > > > > > TEST=./tools/perf/run_benchmark --browser-executable=~/chromium/src/out/Debug/chrome --also-run-disabled-tests service_worker.service_worker
    > > > > >
    > > > > > Committed: https://crrev.com/f10a201a475341760a365bd5ecddab3139f2657a [formerly adaa44fc1275085c09fff18d8f2dfbddd88bc186]
    > > > > > Cr-Commit-Position: refs/heads/master@{#317807}
    > > > >
    > > > > TBR=falken@chromium.org,nednguyen@google.com,sullivan@chromium.org,horo@chromium.org
    > > > > NOPRESUBMIT=true
    > > > > NOTREECHECKS=true
    > > > > NOTRY=true
    > > > > BUG=433943
    > > > >
    > > > > Committed: https://crrev.com/db3146ae1cbf50aca0d4c36ae14b64eead4b8df7 [formerly 6aa34fa4e41d77e92d86f3f0c5f80fab9bdda749]
    > > > > Cr-Commit-Position: refs/heads/master@{#318066}
    > > >
    > > > TBR=falken@chromium.org,nednguyen@google.com,sullivan@chromium.org,simonhatch@chromium.org
    > > > NOPRESUBMIT=true
    > > > NOTREECHECKS=true
    > > > NOTRY=true
    > > > BUG=433943
    > > >
    > > > Committed: https://crrev.com/349dee51e84d1a20309d51b8d59f1dbda30fece7 [formerly e3f37ac21edb64dcf805007a382aff9405642948]
    > > > Cr-Commit-Position: refs/heads/master@{#321759}
    > >
    > > TBR=falken@chromium.org,nednguyen@google.com,sullivan@chromium.org,simonhatch@chromium.org
    > > NOPRESUBMIT=true
    > > NOTREECHECKS=true
    > > NOTRY=true
    > > BUG=433943
    > >
    > > Committed: https://crrev.com/85b123fc489d0710265dfd4609dfb46284d76632 [formerly 7e894073084371a6c8c31827a0f0c9cdb37d4ed6]
    > > Cr-Commit-Position: refs/heads/master@{#321897}
    >
    > TBR=falken@chromium.org,nednguyen@google.com,sullivan@chromium.org,simonhatch@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=433943
    >
    > Committed: https://crrev.com/6bd59c3c5d4442778d499fce9e450bacf3a5937e [formerly 19bdfcf548e5d04bfa1357d4df742664d9d5dfa9]
    > Cr-Commit-Position: refs/heads/master@{#323261}
    
    TBR=falken@chromium.org,nednguyen@google.com,sullivan@chromium.org,simonhatch@chromium.org,horo@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=433943
    
    Review URL: https://codereview.chromium.org/1064073002
    
    Cr-Commit-Position: refs/heads/master@{#324070}
    
    Former-commit-id: 8d0d03be9aaed6121770b32b3f4efd7830c2d962

--- tools/perf/benchmarks/service_worker.py
+++ tools/perf/benchmarks/service_worker.py
@@ -173,6 +173,8 @@ class _ServiceWorkerMicroBenchmarkMeasurement(page_test.PageTest):
         browser_process, 'IOThread', filter_text , results)
 
 
+# TODO(simonhatch): Temporarily disabling (http://crbug.com/433943)
+@benchmark.Disabled
 class ServiceWorkerPerfTest(benchmark.Benchmark):
   """Performance test on public applications using ServiceWorker"""
   test = _ServiceWorkerMeasurement
