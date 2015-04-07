1da47325b2a35acddd36d853c0a6c3c9d93f79cf
marja <marja@chromium.org>
2015-04-07 07:14:52 -0700

    Extend the BigJs benchmark: add a YouTube page.
    The page uses a big parsing blocking script and is thus interesting for the
    BigJs benchmark.
    
    R=skyostil@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1060563005
    
    Cr-Commit-Position: refs/heads/master@{#324038}
    
    Former-commit-id: d14ac22f2e83cc15556d02e944b80955421172c8

--- tools/perf/page_sets/big_js.py
+++ tools/perf/page_sets/big_js.py
@@ -14,12 +14,15 @@ class BigJsPageSet(page_set_module.PageSet):
       bucket=page_set_module.PARTNER_BUCKET,
       user_agent_type='desktop')
 
-    # Page sets with only one page don't work well, since we end up reusing a
+    # www.foo.com is a dummy page to make it easier to run only one interesting
+    # page at a time. You can't just run the interesting page on its own: Page
+    # sets with only one page don't work well, since we end up reusing a
     # renderer all the time and it keeps its memory caches alive (see
-    # crbug.com/403735). Add a dummy second page here.
+    # crbug.com/403735).
     urls_list = [
-        'http://beta.unity3d.com/jonas/DT2/',
-        'http://www.foo.com',
+       'http://beta.unity3d.com/jonas/DT2/',
+       'https://www.youtube.com/watch?v=IJNR2EpS0jw',
+       'http://www.foo.com',
     ]
     for url in urls_list:
       self.AddUserStory(page_module.Page(url, self))
