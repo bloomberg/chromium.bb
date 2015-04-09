070b4af6556de97bba822976480ab1929368b18f
majidvp <majidvp@chromium.org>
2015-04-08 17:11:11 -0700

    Improve hit-testing telemetry test
    Wait for 'polymer-ready' event instead of hard-coded 2 seconds. Pay selector
    cost once to ensure iterations are only capturing the cost of hit-testing.
    
    Note that this CL is expected to reduce thread times on the dashboard.
    
    BUG=472632
    
    Review URL: https://codereview.chromium.org/1066983002
    
    Cr-Commit-Position: refs/heads/master@{#324313}
    
    Former-commit-id: 894a75d9fc52dd3280469df6406b3096743e7a09

--- tools/perf/page_sets/key_hit_test_cases.py
+++ tools/perf/page_sets/key_hit_test_cases.py
@@ -5,44 +5,39 @@
 from telemetry.page import page as page_module
 from telemetry.page import page_set as page_set_module
 
+from page_sets import polymer
 
-class KeyHitTestCasesPage(page_module.Page):
-
-  def __init__(self, url, page_set):
-    super(KeyHitTestCasesPage, self).__init__(
-        url=url, page_set=page_set, credentials_path = 'data/credentials.json')
-    self.user_agent_type = 'mobile'
-
-  def RunNavigateSteps(self, action_runner):
-    super(KeyHitTestCasesPage, self).RunNavigateSteps(action_runner)
-    action_runner.Wait(2)
-
-  def RunPageInteractions(self, action_runner):
-    action_runner.Wait(2)
-    for _ in xrange(100):
-        self.TapButton(action_runner)
-
-
-class PaperCalculatorHitTest(KeyHitTestCasesPage):
+class PaperCalculatorHitTest(polymer.PolymerPage):
 
   def __init__(self, page_set):
     super(PaperCalculatorHitTest, self).__init__(
       # Generated from https://github.com/zqureshi/paper-calculator
       # vulcanize --inline --strip paper-calculator/demo.html
       url='file://key_hit_test_cases/paper-calculator-no-rendering.html',
-      page_set=page_set)
+      page_set=page_set, run_no_page_interactions=False)
 
-  def TapButton(self, action_runner):
-    interaction = action_runner.BeginInteraction(
-        'Action_TapAction')
-    action_runner.TapElement(element_function='''
-        document.querySelector(
+    self.user_agent_type = 'mobile'
+
+  def PerformPageInteractions(self, action_runner):
+    # pay cost of selecting tap target only once
+    action_runner.ExecuteJavaScript('''
+        window.__tapTarget = document.querySelector(
             'body /deep/ #outerPanels'
         ).querySelector(
             '#standard'
         ).shadowRoot.querySelector(
             'paper-calculator-key[label="5"]'
         )''')
+    action_runner.WaitForJavaScriptCondition(
+        'window.__tapTarget != null')
+
+    for _ in xrange(100):
+        self.TapButton(action_runner)
+
+  def TapButton(self, action_runner):
+    interaction = action_runner.BeginInteraction(
+        'Action_TapAction')
+    action_runner.TapElement(element_function='''window.__tapTarget''')
     interaction.End()
 
 
