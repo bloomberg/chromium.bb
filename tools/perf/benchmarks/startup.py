d8400d88db3ecb2637fb9ed7bc068f0cba9bed6c
erikchen <erikchen@chromium.org>
2015-04-13 15:21:51 -0700

    Telemetry: Reenable large profile startup benchmark.
    The benchmark now uses a 10 minutes timeout to wait for browser startup. It was
    previously failing on the Telemetry waterfall, presumably because the timeout
    was only 30 seconds.
    
    BUG=473827
    
    Review URL: https://codereview.chromium.org/1073103003
    
    Cr-Commit-Position: refs/heads/master@{#324920}
    
    Former-commit-id: a19fa19344af709f50e1d1edca3a1f1d0ec9a174

--- tools/perf/benchmarks/startup.py
+++ tools/perf/benchmarks/startup.py
@@ -60,9 +60,6 @@ class StartupWarmBlankPage(_StartupWarm):
 
 @benchmark.Enabled('has tabs')
 @benchmark.Enabled('win', 'linux', 'mac')
-# TODO(erikchen): Investigate source of 30s browser hang on startup.
-# http://crbug.com/473827
-@benchmark.Disabled
 class StartupLargeProfileColdBlankPage(_StartupCold):
   """Measures cold startup time with a large profile."""
   tag = 'cold'
@@ -73,15 +70,15 @@ class StartupLargeProfileColdBlankPage(_StartupCold):
     super(StartupLargeProfileColdBlankPage, self).__init__(max_failures)
     self.generated_profile_archive = "large_profile.zip"
 
+  def CustomizeBrowserOptions(self, options):
+    options.browser_startup_timeout = 10 * 60
+
   @classmethod
   def Name(cls):
     return 'startup.large_profile.cold.blank_page'
 
 @benchmark.Enabled('has tabs')
 @benchmark.Enabled('win', 'linux', 'mac')
-# TODO(erikchen): Investigate source of 30s browser hang on startup.
-# http://crbug.com/473827
-@benchmark.Disabled
 class StartupLargeProfileWarmBlankPage(_StartupWarm):
   """Measures warm startup time with a large profile."""
   tag = 'warm'
@@ -91,6 +88,9 @@ class StartupLargeProfileWarmBlankPage(_StartupWarm):
   def __init__(self, max_failures=None):
     super(StartupLargeProfileWarmBlankPage, self).__init__(max_failures)
     self.generated_profile_archive = "large_profile.zip"
+
+  def CustomizeBrowserOptions(self, options):
+    options.browser_startup_timeout = 10 * 60
 
   @classmethod
   def Name(cls):
