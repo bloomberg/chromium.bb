13f5880775a704f468a4b6137484b305873d8fac
engedy <engedy@chromium.org>
2015-04-14 01:59:30 -0700

    Restrict affiliaton-based matching to be enabled only when sync without a custom passphrase is enabled.
    BUG=437865
    
    Review URL: https://codereview.chromium.org/1075073006
    
    Cr-Commit-Position: refs/heads/master@{#325016}
    
    Former-commit-id: e60831f0d36f9691256a42d19689ffbcfcb8e13f

--- chrome/browser/sync/glue/password_data_type_controller.h
+++ chrome/browser/sync/glue/password_data_type_controller.h
@@ -9,6 +9,7 @@
 
 #include "base/memory/ref_counted.h"
 #include "components/sync_driver/non_ui_data_type_controller.h"
+#include "components/sync_driver/sync_service_observer.h"
 
 class Profile;
 class ProfileSyncComponentsFactory;
@@ -20,7 +21,8 @@ class PasswordStore;
 namespace browser_sync {
 
 // A class that manages the startup and shutdown of password sync.
-class PasswordDataTypeController : public sync_driver::NonUIDataTypeController {
+class PasswordDataTypeController : public sync_driver::NonUIDataTypeController,
+                                   public sync_driver::SyncServiceObserver {
  public:
   PasswordDataTypeController(
       ProfileSyncComponentsFactory* profile_sync_factory,
@@ -37,6 +39,10 @@ class PasswordDataTypeController : public sync_driver::NonUIDataTypeController {
   bool PostTaskOnBackendThread(const tracked_objects::Location& from_here,
                                const base::Closure& task) override;
   bool StartModels() override;
+  void StopModels() override;
+
+  // sync_driver::SyncServiceObserver:
+  void OnStateChanged() override;
 
  private:
   Profile* const profile_;
