78e19dd5b6eac6b8c8a079f2f44aab843f011829
pdr@chromium.org <pdr@chromium.org>
2015-04-15 05:34:46 +0000

    Convert LayoutDrawingRecorder to be a simple helper of DrawingRecorder
    This patch makes LayoutDrawingRecorder a non-virual subclass of
    DrawingRecorder instead of carrying a DrawingRecorder member. This is
    just a cleanup and should have no observable differences.
    
    Review URL: https://codereview.chromium.org/1083223002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193749 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 64e02ed1a16c1e2f906b091f8e876929776707fa

--- third_party/WebKit/Source/core/paint/LayoutObjectDrawingRecorder.h
+++ third_party/WebKit/Source/core/paint/LayoutObjectDrawingRecorder.h
@@ -6,9 +6,7 @@
 #define LayoutObjectDrawingRecorder_h
 
 #include "core/layout/PaintPhase.h"
-#include "platform/geometry/FloatRect.h"
 #include "platform/geometry/LayoutRect.h"
-#include "platform/graphics/paint/DisplayItem.h"
 #include "platform/graphics/paint/DrawingRecorder.h"
 
 namespace blink {
@@ -16,21 +14,17 @@ namespace blink {
 class GraphicsContext;
 class LayoutObject;
 
-class LayoutObjectDrawingRecorder {
+// Convienance constructors for creating DrawingRecorders.
+class LayoutObjectDrawingRecorder final : public DrawingRecorder {
 public:
     LayoutObjectDrawingRecorder(GraphicsContext& context, const LayoutObject& layoutObject, DisplayItem::Type displayItemType, const LayoutRect& clip)
-        : m_drawingRecorder(context, layoutObject, displayItemType, pixelSnappedIntRect(clip)) { }
+        : DrawingRecorder(context, layoutObject, displayItemType, pixelSnappedIntRect(clip)) { }
 
     LayoutObjectDrawingRecorder(GraphicsContext& context, const LayoutObject& layoutObject, PaintPhase phase, const FloatRect& clip)
-        : m_drawingRecorder(context, layoutObject, DisplayItem::paintPhaseToDrawingType(phase), clip) { }
+        : DrawingRecorder(context, layoutObject, DisplayItem::paintPhaseToDrawingType(phase), clip) { }
 
     LayoutObjectDrawingRecorder(GraphicsContext& context, const LayoutObject& layoutObject, DisplayItem::Type type, const FloatRect& clip)
-        : m_drawingRecorder(context, layoutObject, type, clip) { }
-
-    bool canUseCachedDrawing() const { return m_drawingRecorder.canUseCachedDrawing(); }
-
-private:
-    DrawingRecorder m_drawingRecorder;
+        : DrawingRecorder(context, layoutObject, type, clip) { }
 };
 
 } // namespace blink
