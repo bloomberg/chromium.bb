2f66a3f949530074bc5151ea8daf6c2b7abd07a7
Dirk Pranke <dpranke@chromium.org>
2015-04-16 18:43:11 -0700

    Whitespace change to test the new GYP waterfalls.
    TBR=bradnelson@chromium.org
    BUG=472266
    
    Review URL: https://codereview.chromium.org/1098613002

--- buildbot/buildbot_run.py
+++ buildbot/buildbot_run.py
@@ -3,9 +3,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-
 """Argument-less script to select what to run on the buildbots."""
-
 
 import filecmp
 import os
