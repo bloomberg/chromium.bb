0fde74c96af1fca55d9b44b3ed76098854ad1fd7
shivamidow@gmail.com <shivamidow@gmail.com>
2015-04-17 07:00:25 +0000

    Position of absolutely positioned image should not be calculated accumulatively.
    Image having positin:absolute moves down 18px for a single layout.
    This is because 18px, <br> line height is accumulated while placing the image in layout.
    This is unexpected, out of flow positioned element having position:absolute or position:fixed
    in InlineFlowBox should not be affected by its siblings.
    
    BUG=435922
    
    Review URL: https://codereview.chromium.org/1085773002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@193926 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: cb705ce846517aa3cf4807f289e93887a39d00a7

--- third_party/WebKit/Source/core/layout/line/InlineFlowBox.cpp
+++ third_party/WebKit/Source/core/layout/line/InlineFlowBox.cpp
@@ -257,8 +257,11 @@ void InlineFlowBox::attachLineBoxToLayoutObject()
 void InlineFlowBox::adjustPosition(FloatWillBeLayoutUnit dx, FloatWillBeLayoutUnit dy)
 {
     InlineBox::adjustPosition(dx, dy);
-    for (InlineBox* child = firstChild(); child; child = child->nextOnLine())
+    for (InlineBox* child = firstChild(); child; child = child->nextOnLine()) {
+        if (child->layoutObject().isOutOfFlowPositioned())
+            continue;
         child->adjustPosition(dx, dy);
+    }
     if (m_overflow)
         m_overflow->move(dx, dy); // FIXME: Rounding error here since overflow was pixel snapped, but nobody other than list markers passes non-integral values here.
 }
