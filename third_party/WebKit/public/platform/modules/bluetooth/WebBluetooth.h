ae15cea7313fdb96a07ae5e867a5aa5d3b58fcd2
ortuno@chromium.org <ortuno@chromium.org>
2015-04-20 22:14:34 +0000

    bluetooh: Define new connectGATT and WebBluetoothGATTRemoteServer
    Initial implementation of connectGATT. Next steps is to actually connect to the device.
    
    First of a set of 3 patches to implement connectGATT.
    
      [1] This patch.
      [2] https://codereview.chromium.org/1082273006
      [3] https://codereview.chromium.org/1060593004
    
    
    BUG=421668,476735
    
    Review URL: https://codereview.chromium.org/1093633003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194058 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0c1be4b9494c166031acdc25928ea11e0000d2b6

--- third_party/WebKit/public/platform/modules/bluetooth/WebBluetooth.h
+++ third_party/WebKit/public/platform/modules/bluetooth/WebBluetooth.h
@@ -6,23 +6,43 @@
 #define WebBluetooth_h
 
 #include "public/platform/WebCallbacks.h"
+#include "public/platform/WebString.h"
 
 namespace blink {
 
 struct WebBluetoothDevice;
 struct WebBluetoothError;
+struct WebBluetoothGATTRemoteServer;
 
 // Success and failure callbacks for requestDevice.
 // WebBluetoothDevice and WebBluetoothError object ownership is transfered.
 typedef WebCallbacks<WebBluetoothDevice, WebBluetoothError> WebBluetoothRequestDeviceCallbacks;
 
+// Success and failure callbacks for connectGATT.
+typedef WebCallbacks<WebBluetoothGATTRemoteServer, WebBluetoothError> WebBluetoothConnectGATTCallbacks;
+
 class WebBluetooth {
 public:
     virtual ~WebBluetooth() { }
 
-    // Requests a bluetooth device.
+    // BluetoothDiscovery Methods:
+    // See https://webbluetoothcg.github.io/web-bluetooth/#idl-def-bluetoothdiscovery
     // WebBluetoothRequestDeviceCallbacks ownership transferred to the client.
     virtual void requestDevice(WebBluetoothRequestDeviceCallbacks*) = 0;
+
+    // BluetoothDevice methods:
+    // See https://webbluetoothcg.github.io/web-bluetooth/#idl-def-bluetoothdevice
+    // WebBluetoothConnectGATTCallbacks ownership transferred to the callee.
+    virtual void connectGATT(const WebString& /* deviceInstanceID */,
+        WebBluetoothConnectGATTCallbacks*) { }
+
+    // BluetoothGATTRemoteServer methods:
+    // See https://webbluetoothcg.github.io/web-bluetooth/#idl-def-bluetoothgattremoteserver
+    virtual void disconnect() { }
+    // TODO(ortuno): Properly define these methods once WebBluetoothServiceUuid
+    // and WebBluetoothGATTService are defined.
+    // virtual void getPrimaryService() { }
+    // virtual void getPrimaryServices() { }
 };
 
 } // namespace blink
