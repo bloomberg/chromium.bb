be87693e0d63f54518025609c9eab24ede87d892
ben <ben@chromium.org>
2015-04-20 12:11:58 -0700

    Fork a mandoline executable (shell embedder) from mojo shell.
    R=sky@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/1094883003
    
    Cr-Commit-Position: refs/heads/master@{#325889}
    
    Former-commit-id: eabf815bf9290e9bad4c5da08be8b566e24a9f67

---
+++ mandoline/app/desktop/main.cc
@@ -0,0 +1,24 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/at_exit.h"
+#include "base/command_line.h"
+#include "mandoline/app/desktop/launcher_process.h"
+#include "mojo/shell/child_process.h"
+#include "mojo/shell/init.h"
+#include "mojo/shell/switches.h"
+
+int main(int argc, char** argv) {
+  base::AtExitManager at_exit;
+  base::CommandLine::Init(argc, argv);
+
+  mojo::shell::InitializeLogging();
+
+  const base::CommandLine& command_line =
+      *base::CommandLine::ForCurrentProcess();
+  if (command_line.HasSwitch(switches::kChildProcess))
+    return mojo::shell::ChildProcessMain();
+
+  return mandoline::LauncherProcessMain(argc, argv);
+}
