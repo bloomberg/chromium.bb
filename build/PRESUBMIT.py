843cf7cb18c94dbf6c55283fb2d23df9edd9c3ea
daniimms <daniimms@amazon.com>
2015-04-21 11:11:37 -0700

    Remove unused constant in build/PRESUBMIT.py
    R=dpranke@chromium.org
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/1095333003
    
    Cr-Commit-Position: refs/heads/master@{#326073}
    
    Former-commit-id: 3a38a6024d19c55055ac5d7ff10dbd5d4dc52a4c

--- build/PRESUBMIT.py
+++ build/PRESUBMIT.py
@@ -2,8 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-WHITELIST = [ r'.+_test.py$' ]
-
 
 def _RunTests(input_api, output_api):
   return (input_api.canned_checks.RunUnitTestsInDirectory(
