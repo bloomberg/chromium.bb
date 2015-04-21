97013a3ba9995a654dd6620bc4dd12323725410e
ortuno@chromium.org <ortuno@chromium.org>
2015-04-21 00:49:35 +0000

    bluetooth: Remove the old versions of the Bluetooth API headers.
    These are no longer necessary now that Chromium uses the new paths.
    
    This is part of a three-sided patch:
      [1] https://codereview.chromium.org/1059793002
      [2] https://codereview.chromium.org/1059753003
      [3] This patch.
    
    BUG=453032
    
    Review URL: https://codereview.chromium.org/1060643002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194063 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 1d508397805d2a47e180a5c81b9e7814a2488221

--- third_party/WebKit/public/platform/WebBluetooth.h
+++
@@ -1,6 +0,0 @@
-// Copyright 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// TODO(ortuno): Will be removed in https://crrev.com/1060643002
-#include "public/platform/modules/bluetooth/WebBluetooth.h"
