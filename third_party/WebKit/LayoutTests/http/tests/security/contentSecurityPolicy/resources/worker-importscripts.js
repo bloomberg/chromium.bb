7c51942c32487292ad0a6f35711af0145c905425
estark@chromium.org <estark@chromium.org>
2015-04-21 16:21:00 +0000

    Use the served CSP header for dedicated workers
    This CL makes workers obey the CSP that was served as a header along
    with the worker script, instead of inheriting the CSP from the document
    that spawned the worker. (Blob and file URLs still inherit the CSP from
    the parent document.)
    
    BUG=474872
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=194094
    
    Review URL: https://codereview.chromium.org/1100583003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194143 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d3fa29fa65e2c7a11f862b72b75d1e053e62d143

--- third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/worker-importscripts.js
+++
@@ -1,7 +0,0 @@
-try {
-    importScripts("http://localhost:8000/security/contentSecurityPolicy/resources/post-message.js");
-    postMessage("importScripts allowed");
-} catch(e) {
-    postMessage("importScripts blocked: " + e);
-}
-
