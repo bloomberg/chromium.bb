8db41f56dee9696904e8cbcf8148799d66783a5e
qiankun.miao <qiankun.miao@intel.com>
2015-04-21 10:55:53 -0700

    Remove PeerConnection runtime flag for chromium
    This feature is in stable status since 2013. No runtime check in blink
    currently. So Just remove it.
    
    The blink side patch is at: https://codereview.chromium.org/1054313003/
    
    BUG=402536
    
    Review URL: https://codereview.chromium.org/1061253002
    
    Cr-Commit-Position: refs/heads/master@{#326069}
    
    Former-commit-id: d607c41285e819b5a71c76e0e9f8e15b7547513a

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -116,9 +116,6 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
     WebRuntimeFeatures::enableSharedWorker(false);
 
 #if defined(OS_ANDROID)
-  if (command_line.HasSwitch(switches::kDisableWebRTC))
-    WebRuntimeFeatures::enablePeerConnection(false);
-
   // WebAudio is enabled by default on ARM and X86, if the MediaCodec
   // API is available.
   WebRuntimeFeatures::enableWebAudio(
