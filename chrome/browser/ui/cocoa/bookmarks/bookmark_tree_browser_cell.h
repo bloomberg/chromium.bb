3128fbbf81d86191e808fa3a66576f6e82cb51c2
erikchen <erikchen@chromium.org>
2015-04-21 17:50:58 -0700

    mac: Don't redeclare properties in BookmarkTreeBrowserCell.
    In the OSX 10.10 SDK, |target| and |action| are properties on NSCell. There is
    no point in redeclaring them if they already exist.
    
    BUG=463170
    
    Review URL: https://codereview.chromium.org/1095953003
    
    Cr-Commit-Position: refs/heads/master@{#326199}
    
    Former-commit-id: be024adb2e7f29ed7a446120d679db0996bbedee

--- chrome/browser/ui/cocoa/bookmarks/bookmark_tree_browser_cell.h
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_tree_browser_cell.h
@@ -20,13 +20,24 @@ class BookmarkNode;
  @private
   const bookmarks::BookmarkNode* bookmarkNode_;  // weak
   NSMatrix* matrix_;  // weak
+
+  // NSCell does not implement the |target| or |action| properties. Subclasses
+  // that need this functionality are expected to implement this functionality.
   id target_;  // weak
   SEL action_;
 }
 
 @property(nonatomic, assign) NSMatrix* matrix;
+
+#if !defined(MAC_OS_X_VERSION_10_10) || \
+    MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_10
+// In OSX SDK <= 10.9, there are setters and getters for target and action in
+// NSCell, but no properties. In the OSX 10.10 SDK, the properties are defined
+// as atomic. There is no point in redeclaring the properties if they already
+// exist.
 @property(nonatomic, assign) id target;
 @property(nonatomic, assign) SEL action;
+#endif  // MAC_OS_X_VERSION_10_10
 
 - (const bookmarks::BookmarkNode*)bookmarkNode;
 - (void)setBookmarkNode:(const bookmarks::BookmarkNode*)bookmarkNode;
