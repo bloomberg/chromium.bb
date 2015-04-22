d226317e50b6144e0889095c95d81ecc8c6e48fc
vollick <vollick@chromium.org>
2015-04-22 07:38:07 -0700

    Re-enable draw properties benchmark on Android
    There had been errors on android one and nexus 6 that
    prevented this benchmark from being enabled on android,
    but with https://codereview.chromium.org/1095163002,
    this appears to have been addressed.
    
    BUG=471786
    
    Review URL: https://codereview.chromium.org/1097403002
    
    Cr-Commit-Position: refs/heads/master@{#326294}
    
    Former-commit-id: 749f9ad297d27aa3d5e6b15866360b3e80013cb1

--- tools/perf/benchmarks/draw_properties.py
+++ tools/perf/benchmarks/draw_properties.py
@@ -9,8 +9,7 @@ import page_sets
 
 
 # This benchmark depends on tracing categories available in M43
-@benchmark.Disabled('reference',  # http://crbug.com/463111
-                    'android')    # http://crbug.com/471786
+@benchmark.Disabled('reference')
 class DrawPropertiesToughScrolling(benchmark.Benchmark):
   test = draw_properties.DrawProperties
   page_set = page_sets.ToughScrollingCasesPageSet
@@ -20,8 +19,7 @@ class DrawPropertiesToughScrolling(benchmark.Benchmark):
 
 
 # This benchmark depends on tracing categories available in M43
-@benchmark.Disabled('reference','win',  # http://crbug.com/463111
-                    'android')          # http://crbug.com/471786
+@benchmark.Disabled('reference','win')  # http://crbug.com/463111
 class DrawPropertiesTop25(benchmark.Benchmark):
   """Measures the relative performance of CalcDrawProperties vs computing draw
   properties from property trees.
