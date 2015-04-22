c40f1addd424da5cac523d27b644421d83fe56e0
vitbar <vitbar@yandex-team.ru>
2015-04-22 06:19:55 -0700

    Corrected the TemplateURLTest.ExtractSearchTermsFromUTF8URL test.
    Extended characters are represented in URL with percent-escaping always.
    
    R=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/1090863004
    
    Cr-Commit-Position: refs/heads/master@{#326282}
    
    Former-commit-id: 7060e23cd7908532ee22d7835212d7afb8d236bd

--- components/search_engines/template_url_unittest.cc
+++ components/search_engines/template_url_unittest.cc
@@ -1154,8 +1154,8 @@ TEST_F(TemplateURLTest, ExtractSearchTermsFromUTF8URL) {
 
   // Russian text encoded with UTF-8.
   EXPECT_TRUE(url.ExtractSearchTermsFromURL(
-      GURL("http://utf-8.ru/?q=\xD0\x97\xD0\xB4\xD1\x80\xD0\xB0\xD0\xB2\xD1\x81"
-           "\xD1\x82\xD0\xB2\xD1\x83\xD0\xB9,+\xD0\xBC\xD0\xB8\xD1\x80!"),
+      GURL("http://utf-8.ru/?q=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82"
+           "%D0%B2%D1%83%D0%B9,+%D0%BC%D0%B8%D1%80!"),
       search_terms_data_, &result));
   EXPECT_EQ(
       base::WideToUTF16(
@@ -1164,16 +1164,16 @@ TEST_F(TemplateURLTest, ExtractSearchTermsFromUTF8URL) {
       result);
 
   EXPECT_TRUE(url.ExtractSearchTermsFromURL(
-      GURL("http://utf-8.ru/#q=\xD0\xB4\xD0\xB2\xD0\xB0+\xD1\x81\xD0\xBB"
-           "\xD0\xBE\xD0\xB2\xD0\xB0"),
+      GURL("http://utf-8.ru/#q=%D0%B4%D0%B2%D0%B0+%D1%81%D0%BB%D0%BE%D0%B2"
+           "%D0%B0"),
       search_terms_data_, &result));
   EXPECT_EQ(
       base::WideToUTF16(L"\x0434\x0432\x0430 \x0441\x043B\x043E\x0432\x0430"),
       result);
 
   EXPECT_TRUE(url.ExtractSearchTermsFromURL(
-      GURL("http://utf-8.ru/path/\xD0\xB1\xD1\x83\xD0\xBA\xD0\xB2\xD1\x8B%20"
-           "\xD0\x90%20\xD0\xB8%20A"),
+      GURL("http://utf-8.ru/path/%D0%B1%D1%83%D0%BA%D0%B2%D1%8B%20%D0%90%20"
+           "%D0%B8%20A"),
       search_terms_data_, &result));
   EXPECT_EQ(
       base::WideToUTF16(L"\x0431\x0443\x043A\x0432\x044B \x0410 \x0438 A"),
