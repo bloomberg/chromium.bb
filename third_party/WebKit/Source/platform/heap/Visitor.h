a8073f760a89684f16eaa12dfb6ee1b742cedb5a
inferno@chromium.org <inferno@chromium.org>
2015-04-22 18:05:29 +0000

    Add forbidGCDuringConstruction function to ease in UBSAN VPTR function blacklisting since operator new() is too generic to blacklist.
    BUG=476063
    R=haraken@chromium.org,oilpan-reviews@chromium.org
    
    Review URL: https://codereview.chromium.org/1064693005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@194228 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: a019c39c603cdf87c774359eb522e0b63875f9f7

--- third_party/WebKit/Source/platform/heap/Visitor.h
+++ third_party/WebKit/Source/platform/heap/Visitor.h
@@ -842,14 +842,20 @@ private:
 //    GarbageCollectedMixinConstructorMarker's constructor takes care of
 //    this and the field is declared by way of USING_GARBAGE_COLLECTED_MIXIN().
 
+// TODO(inferno): Remove forbidGCDuringConstruction() function once UBSan VPTR supports
+// function attribute level blacklisting. See crbug.com/476073.
 #define DEFINE_GARBAGE_COLLECTED_MIXIN_CONSTRUCTOR_MARKER(TYPE)                         \
 public:                                                                                 \
+    ALWAYS_INLINE static void forbidGCDuringConstruction(void* object)                  \
+    {                                                                                   \
+        ThreadState* state = ThreadStateFor<ThreadingTrait<TYPE>::Affinity>::state();   \
+        state->enterGCForbiddenScopeIfNeeded(&(reinterpret_cast<TYPE*>(object)->m_mixinConstructorMarker)); \
+    }                                                                                   \
     GC_PLUGIN_IGNORE("crbug.com/456823")                                                \
     void* operator new(size_t size)                                                     \
     {                                                                                   \
         void* object = TYPE::allocateObject(size);                                      \
-        ThreadState* state = ThreadStateFor<ThreadingTrait<TYPE>::Affinity>::state();   \
-        state->enterGCForbiddenScopeIfNeeded(&(reinterpret_cast<TYPE*>(object)->m_mixinConstructorMarker)); \
+        forbidGCDuringConstruction(object);                                             \
         return object;                                                                  \
     }                                                                                   \
     GarbageCollectedMixinConstructorMarker m_mixinConstructorMarker;                    \
