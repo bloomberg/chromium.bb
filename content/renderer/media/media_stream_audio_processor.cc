d584f1865d166abddf55685edbf78ace4852f4c5
aluebs <aluebs@chromium.org>
2015-04-22 08:59:31 -0700

    Add FieldTrial for 48kHz Audio Processing in WebRTC
    BUG=478942
    
    Review URL: https://codereview.chromium.org/1097213002
    
    Cr-Commit-Position: refs/heads/master@{#326312}
    
    Former-commit-id: 4555ac061b6bd3eaf934ee9ae0c6ceff91afcae0

--- content/renderer/media/media_stream_audio_processor.cc
+++ content/renderer/media/media_stream_audio_processor.cc
@@ -85,10 +85,17 @@ bool IsDelayAgnosticAecEnabled() {
 }
 
 bool IsBeamformingEnabled(const MediaAudioConstraints& audio_constraints) {
-  return audio_constraints.GetProperty(
-             MediaAudioConstraints::kGoogBeamforming) ||
-         base::FieldTrialList::FindFullName("ChromebookBeamforming") ==
-             "Enabled";
+  return base::FieldTrialList::FindFullName("ChromebookBeamforming") ==
+             "Enabled" ||
+         audio_constraints.GetProperty(MediaAudioConstraints::kGoogBeamforming);
+}
+
+bool IsAudioProcessing48kHzSupportEnabled(
+    const MediaAudioConstraints& audio_constraints) {
+  return base::FieldTrialList::FindFullName("AudioProcessing48kHzSupport") ==
+             "Enabled" ||
+         audio_constraints.GetProperty(
+             MediaAudioConstraints::kGoogAudioProcessing48kHzSupport);
 }
 
 }  // namespace
@@ -465,8 +472,8 @@ void MediaStreamAudioProcessor::InitializeAudioProcessingModule(
   const bool goog_beamforming = IsBeamformingEnabled(audio_constraints);
   const bool goog_high_pass_filter = audio_constraints.GetProperty(
       MediaAudioConstraints::kGoogHighpassFilter);
-  audio_proc_48kHz_support_ = audio_constraints.GetProperty(
-      MediaAudioConstraints::kGoogAudioProcessing48kHzSupport);
+  audio_proc_48kHz_support_ =
+      IsAudioProcessing48kHzSupportEnabled(audio_constraints);
   // Return immediately if no goog constraint is enabled.
   if (!echo_cancellation && !goog_experimental_aec && !goog_ns &&
       !goog_high_pass_filter && !goog_typing_detection &&
