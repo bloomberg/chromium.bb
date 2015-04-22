65b4d633c356efc9fb149c20eb0322ef3b29f94f
jam <jam@chromium.org>
2015-04-22 15:53:57 -0700

    Disable flaky TabindexFocusDirectorySelected tests.
    They flaked 45 times in the last week.
    
    BUG=469061
    TBR=yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/1101753003
    
    Cr-Commit-Position: refs/heads/master@{#326385}
    
    Former-commit-id: bb916a2bf8ec4dc4194b3d049c2286102f7b0f2d

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -1154,8 +1154,9 @@ INSTANTIATE_TEST_CASE_P(
                                     "tabindexFocusDownloads"),
                       TestParameter(IN_GUEST_MODE, "tabindexFocusDownloads")));
 
+// http://crbug.com/469061
 INSTANTIATE_TEST_CASE_P(
-    TabindexFocusDirectorySelected,
+    DISABLED_TabindexFocusDirectorySelected,
     FileManagerBrowserTest,
     ::testing::Values(TestParameter(NOT_IN_GUEST_MODE,
                                     "tabindexFocusDirectorySelected")));
