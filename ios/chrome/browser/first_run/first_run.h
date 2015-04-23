4675d7d1bb7b29574ca85a7b8e110b4d72d0d3e1
sdefresne <sdefresne@chromium.org>
2015-04-23 10:21:22 -0700

    [iOS] Upstream FirstRun
    FirstRun provides a collection of methods to check whether the application
    is run for the first time and to control behaviour of code run at that
    point.
    
    BUG=429756
    
    Review URL: https://codereview.chromium.org/1102723003
    
    Cr-Commit-Position: refs/heads/master@{#326561}
    
    Former-commit-id: 5c18107becbe1ed7d0eac82fc1a19b2565ef6049

---
+++ ios/chrome/browser/first_run/first_run.h
@@ -0,0 +1,61 @@
+// Copyright 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This file contains the iOS equivalent of FirstRun.
+
+#ifndef IOS_CHROME_BROWSER_FIRST_RUN_FIRST_RUN_H_
+#define IOS_CHROME_BROWSER_FIRST_RUN_FIRST_RUN_H_
+
+#include "base/basictypes.h"
+
+namespace base {
+class FilePath;
+}
+
+namespace user_prefs {
+class PrefRegistrySyncable;
+}
+
+// A collection of functions to manage the user experience when running
+// this application for the first time. The iOS implementation is significantly
+// simpler than other platforms because it is designed to be preemptive and
+// stops user from doing anything else until the First Run UX is completed
+// or explicitly skipped.
+class FirstRun {
+ public:
+  // Returns true if this is the first time chrome is run for this user.
+  static bool IsChromeFirstRun();
+
+  // Creates the sentinel file that signals that chrome has been configured iff
+  // the file does not exist yet. Returns true if the file was created and false
+  // if the file already exists or could not be created.
+  static bool CreateSentinel();
+
+  // Removes the sentinel file created in ConfigDone(). Returns false if the
+  // sentinel file could not be removed.
+  static bool RemoveSentinel();
+
+  // Get RLZ ping delay pref name.
+  static const char* GetPingDelayPrefName();
+
+  // Register user preferences.
+  static void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry);
+
+ private:
+  // Gives the full path to the sentinel file. The file might not exist.
+  static bool GetFirstRunSentinelFilePath(base::FilePath* path);
+
+  enum FirstRunState {
+    FIRST_RUN_UNKNOWN,  // The state is not tested or set yet.
+    FIRST_RUN_TRUE,
+    FIRST_RUN_FALSE
+  };
+
+  // This variable should only be accessed through IsChromeFirstRun().
+  static FirstRunState first_run_;
+
+  DISALLOW_IMPLICIT_CONSTRUCTORS(FirstRun);
+};
+
+#endif  // IOS_CHROME_BROWSER_FIRST_RUN_FIRST_RUN_H_
---
+++ ios/chrome/browser/first_run/first_run.mm
@@ -0,0 +1,79 @@
+// Copyright 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/first_run/first_run.h"
+
+#import <Foundation/Foundation.h>
+
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
+#include "base/path_service.h"
+#include "components/pref_registry/pref_registry_syncable.h"
+#include "ios/chrome/browser/chrome_paths.h"
+
+namespace {
+
+// The absence of kSentinelFile file will tell us it is a first run.
+const char kSentinelFile[] = "First Run";
+
+// RLZ ping delay pref name.
+const char kPingDelayPrefName[] = "distribution.ping_delay";
+
+}  // namespace
+
+FirstRun::FirstRunState FirstRun::first_run_ = FIRST_RUN_UNKNOWN;
+
+// static
+bool FirstRun::GetFirstRunSentinelFilePath(base::FilePath* path) {
+  base::FilePath first_run_sentinel;
+  if (!PathService::Get(ios::DIR_USER_DATA, &first_run_sentinel))
+    return false;
+  *path = first_run_sentinel.AppendASCII(kSentinelFile);
+  return true;
+}
+
+// static
+bool FirstRun::IsChromeFirstRun() {
+  if (first_run_ != FIRST_RUN_UNKNOWN)
+    return first_run_ == FIRST_RUN_TRUE;
+
+  base::FilePath first_run_sentinel;
+  if (!GetFirstRunSentinelFilePath(&first_run_sentinel) ||
+      base::PathExists(first_run_sentinel)) {
+    first_run_ = FIRST_RUN_FALSE;
+    return false;
+  }
+  first_run_ = FIRST_RUN_TRUE;
+  return true;
+}
+
+// static
+bool FirstRun::RemoveSentinel() {
+  base::FilePath first_run_sentinel;
+  if (!GetFirstRunSentinelFilePath(&first_run_sentinel))
+    return false;
+  return base::DeleteFile(first_run_sentinel, false);
+}
+
+// static
+bool FirstRun::CreateSentinel() {
+  base::FilePath first_run_sentinel;
+  if (!GetFirstRunSentinelFilePath(&first_run_sentinel) ||
+      base::PathExists(first_run_sentinel))
+    return false;
+  return base::WriteFile(first_run_sentinel, "", 0) != -1;
+}
+
+// static
+const char* FirstRun::GetPingDelayPrefName() {
+  return kPingDelayPrefName;
+}
+
+// static
+void FirstRun::RegisterProfilePrefs(
+    user_prefs::PrefRegistrySyncable* registry) {
+  registry->RegisterIntegerPref(
+      GetPingDelayPrefName(), 0,
+      user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
+}
