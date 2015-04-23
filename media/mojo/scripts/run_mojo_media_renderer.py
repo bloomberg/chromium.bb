ed61f7fe235e0039d14f5762bdee75f0f84940c2
msw <msw@chromium.org>
2015-04-23 16:31:08 -0700

    Update run_mojo_media_renderer.py for shell and service changes.
    Updates the script to handle recent changes:
      html_viewer has moved to components.
      kiosk_wm is run by default.
      services built locally, don't need origin mapping?
      avoid html_document.cc:150 Check failed: !is_headless_
      --args-for is deprecated (pass app args as shell args)
    
    Also update mojo_media_application.cc arg init.
    
    Tested that this works with:
      gn gen out/Debug --args='is_debug=true use_goma=true proprietary_codecs=true ffmpeg_branding="ChromeOS" enable_media_mojo_renderer=true'
      ninja -C out/Debug/ -j 1500 -l 10 mojo media/mojo/services components/html_viewer mojo/services/network
      media/mojo/scripts/run_mojo_media_renderer.py --build-dir out/Debug --verbose http://wxh.kir/ensemble/kqed.html
    
    BUG=NONE
    TEST=Script works (or closer to working...)
    R=sky@chromium.org,xhwang@chromium.org
    
    Review URL: https://codereview.chromium.org/1088993004
    
    Cr-Commit-Position: refs/heads/master@{#326690}
    
    Former-commit-id: 5ea51a1f1ce3b2e200fc95b232120c1582f31eab

--- media/mojo/scripts/run_mojo_media_renderer.py
+++ media/mojo/scripts/run_mojo_media_renderer.py
@@ -3,16 +3,13 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
-# The script follows mojo/services/html_viewer/view_url.py and modified it for
-# test the mojo media renderer. The page will be rendered in a headless mode.
-#
-# TODO(xhwang): Explore the possibility of running this with the Kiosk window
-# manager. See http://crbug.com/467176
+# The script launches mojo_shell to test the mojo media renderer.
 
 import argparse
 import os
 import subprocess
 import sys
+
 
 root_path = os.path.realpath(
     os.path.join(
@@ -22,6 +19,7 @@ root_path = os.path.realpath(
         os.pardir,
         os.pardir))
 
+
 def _BuildShellCommand(args):
   sdk_version = subprocess.check_output(["cat",
       "third_party/mojo/src/mojo/public/VERSION"], cwd=root_path)
@@ -29,23 +27,10 @@ def _BuildShellCommand(args):
 
   shell_command = [os.path.join(build_dir, "mojo_shell")]
 
-  options = []
-
-  options.append(
-      "--origin=https://storage.googleapis.com/mojo/services/linux-x64/%s" %
-           sdk_version)
-  options.append("--url-mappings=mojo:html_viewer=file://%s/html_viewer.mojo,"
-                 "mojo:media=file://%s/media.mojo" % (build_dir, build_dir))
-
-  args_for_html_viewer = "--enable-mojo-media-renderer --is-headless "
+  options = ["--enable-mojo-media-renderer"]
   if args.verbose:
-    args_for_html_viewer += \
-        "--vmodule=pipeline*=3,*renderer_impl*=3,*mojo_demuxer*=3"
-  options.append("--args-for=mojo:html_viewer %s" % args_for_html_viewer)
-
-  if args.verbose:
-    args_for_media = "--vmodule=mojo*service=3"
-    options.append("--args-for=mojo:media %s" % args_for_media)
+    options.append("--vmodule=pipeline*=3,*renderer_impl*=3,"
+                   "*mojo_demuxer*=3,mojo*service=3")
 
   full_command = shell_command + options + [args.url]
 
@@ -54,13 +39,12 @@ def _BuildShellCommand(args):
 
   return full_command
 
+
 def main():
   parser = argparse.ArgumentParser(
       description="View a URL with HTMLViewer with mojo media renderer. "
-                  "You must have built //mojo/services/html_viewer, "
-                  "//mojo/services/network and //media/mojo/services first. "
-                  " Note that this will currently often fail spectacularly due "
-                  " to lack of binary stability in Mojo.")
+                  "You must have built //mojo, //components/html_viewer, "
+                  "//mojo/services/network and //media/mojo/services first.")
   parser.add_argument(
       "--build-dir",
       help="Path to the dir containing the linux-x64 binaries relative to the "
@@ -68,8 +52,7 @@ def main():
       default="out/Release")
   parser.add_argument("--verbose", help="Increase output verbosity.",
                       action="store_true")
-  parser.add_argument("url",
-                      help="The URL to be viewed")
+  parser.add_argument("url", help="The URL to be viewed")
 
   args = parser.parse_args()
   return subprocess.call(_BuildShellCommand(args))
--- media/mojo/services/mojo_media_application.cc
+++ media/mojo/services/mojo_media_application.cc
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/command_line.h"
 #include "base/logging.h"
-#include "base/strings/utf_string_conversions.h"
 #include "media/mojo/services/mojo_renderer_service.h"
 #include "mojo/application/application_runner_chromium.h"
 #include "third_party/mojo/src/mojo/public/c/system/main.h"
@@ -21,17 +19,6 @@ class MojoMediaApplication
  public:
   // mojo::ApplicationDelegate implementation.
   void Initialize(mojo::ApplicationImpl* app) override {
-    base::CommandLine::StringVector command_line_args;
-#if defined(OS_WIN)
-    for (const auto& arg : app->args())
-      command_line_args.push_back(base::UTF8ToUTF16(arg));
-#elif defined(OS_POSIX)
-    command_line_args = app->args();
-#endif
-
-    base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-    command_line->InitFromArgv(command_line_args);
-
     logging::LoggingSettings settings;
     settings.logging_dest = logging::LOG_TO_SYSTEM_DEBUG_LOG;
     logging::InitLogging(settings);
