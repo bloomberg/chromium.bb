103dac97f7deeff2c874590b60a3db13c387ec7e
rsorokin <rsorokin@chromium.org>
2015-04-24 10:06:19 -0700

    ChromeOS Gaia: Include gaia_password_changed into custom_elements_oobe
    BUG=464238
    TBR=nkostylev@chromium.org
    
    Review URL: https://codereview.chromium.org/1104793003
    
    Cr-Commit-Position: refs/heads/master@{#326822}
    
    Former-commit-id: e30a6cf15e4ba916ed2958ccb3e9947fe61517ff

--- chrome/browser/resources/chromeos/login/custom_elements_oobe.js
+++ chrome/browser/resources/chromeos/login/custom_elements_oobe.js
@@ -10,4 +10,5 @@
 <include src="gaia_buttons.js">
 <include src="gaia_input_form.js">
 <include src="offline_gaia.js">
+<include src="gaia_password_changed.js">
 <include src="notification_card.js">
