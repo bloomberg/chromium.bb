0712c8cd37773b75b47b6a963eeb515c4fa6bfcf
jdonnelly <jdonnelly@chromium.org>
2015-04-24 10:25:09 -0700

    Revert of Implementing a BLE connection finder. (patchset #6 id:100001 of https://codereview.chromium.org/1094273003/)
    Reason for revert:
    This change broke the chromium perf dashboard:
    
    https://build.chromium.org/p/chromium/builders/Linux/builds/61221
    
    by adding a new static initializer:
    
    const std::string kSmartLockServiceUUID =
        "b3b7e28e-a000-3e17-bd86-6e97b9e28c11";
    
    I think you want static const char* instead.
    
    Original issue's description:
    > Implementing a BLE connection finder.
    >
    > This CL adds a connection finder class. The connection finder scans for
    > Bluetooth Low Energy devices advertising a given target service.
    >
    > The current version only works when the remote device advertising the
    > target service is sending all its primary services UUID.
    >
    > BUG=479673
    > R=msarda
    > TBR=tengs
    >
    > Committed: https://crrev.com/5d7a6cedd52c04eba3192a7e5c74a196848084f6 [formerly f56835d5211fc533a02f143e06eb310764f6b901]
    > Cr-Commit-Position: refs/heads/master@{#326807}
    
    TBR=msarda@chromium.org,tengs@chromium.org,sacomoto@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=479673
    
    Review URL: https://codereview.chromium.org/1105863003
    
    Cr-Commit-Position: refs/heads/master@{#326826}
    
    Former-commit-id: 79289369bee811b37d810710fbcfe44a822d525c

--- components/proximity_auth/ble/proximity_auth_ble_system.cc
+++ components/proximity_auth/ble/proximity_auth_ble_system.cc
@@ -4,33 +4,16 @@
 
 #include "components/proximity_auth/ble/proximity_auth_ble_system.h"
 
-#include "base/bind.h"
 #include "base/logging.h"
-#include "base/memory/scoped_ptr.h"
-#include "components/proximity_auth/connection.h"
-#include "device/bluetooth/bluetooth_device.h"
-
-#include "components/proximity_auth/ble/bluetooth_low_energy_connection_finder.h"
 
 namespace proximity_auth {
 
-const std::string kSmartLockServiceUUID =
-    "b3b7e28e-a000-3e17-bd86-6e97b9e28c11";
-
-void ConnectionCallback(
-    scoped_ptr<device::BluetoothGattConnection> connection) {
-  VLOG(1) << "Connection established";
-}
-
 ProximityAuthBleSystem::ProximityAuthBleSystem() {
   VLOG(1) << "Starting Proximity Auth over Bluetooth Low Energy.";
-  BluetoothLowEnergyConnectionFinder* connection_finder =
-      new BluetoothLowEnergyConnectionFinder(kSmartLockServiceUUID);
-  connection_finder->Find(base::Bind(&ConnectionCallback));
 }
 
 ProximityAuthBleSystem::~ProximityAuthBleSystem() {
   VLOG(1) << "Stopping Proximity over Bluetooth Low Energy.";
 }
 
-}  // proximity_auth
+}  // namespace proximity_auth
