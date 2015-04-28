ea73cb00bc85e4041e4b9e4d6c460682ad28b69d
Giulio Camuffo <giuliocamuffo@gmail.com>
2015-04-28 22:57:18 +0300

    wayland-egl: add a core header
    The new core header doesn't include any other header, since it really
    is not needed.
    
    Reviewed-by: Jason Ekstrand <jason@jlekstrand.net>
    Reviewed-by: Pekka Paalanen <pekka.paalanen@collabora.co.uk>

--- src/wayland-egl-core.h
+++ src/wayland-egl-core.h
@@ -21,10 +21,36 @@
  * OF THIS SOFTWARE.
  */
 
-#ifndef WAYLAND_EGL_H
-#define WAYLAND_EGL_H
+#ifndef WAYLAND_EGL_CORE_H
+#define WAYLAND_EGL_CORE_H
 
-#include <wayland-client.h>
-#include "wayland-egl-core.h"
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+#define WL_EGL_PLATFORM 1
+
+struct wl_egl_window;
+struct wl_surface;
+
+struct wl_egl_window *
+wl_egl_window_create(struct wl_surface *surface,
+		     int width, int height);
+
+void
+wl_egl_window_destroy(struct wl_egl_window *egl_window);
+
+void
+wl_egl_window_resize(struct wl_egl_window *egl_window,
+		     int width, int height,
+		     int dx, int dy);
+
+void
+wl_egl_window_get_attached_size(struct wl_egl_window *egl_window,
+				int *width, int *height);
+
+#ifdef  __cplusplus
+}
+#endif
 
 #endif
