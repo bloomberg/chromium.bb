bloomberg/chromium.bb

commit 9abed9715d1ea956ecaef2bed358f83dc6cf58c6

Actually disable Wallet card saving on Linux.
This isn't the ideal solution because I'd like to be able to pass a gyp-time flag to re-enable it (for testing purposes), but it's the smallest patch for merging.

BUG=468045

Review URL: https://codereview.chromium.org/1099993002

Cr-Commit-Position: refs/heads/master@{#326296}

Former-commit-id: 423d45cbeb758ac43ad1456ae76987ba67a196e8

chrome/browser/ui/views/autofill/card_unmask_prompt_views.cc
@@ -328,7 +328,8 @@ void CardUnmaskPromptViews::AnimationProgressed(
     const gfx::Animation* animation) {
   uint8_t alpha = static_cast<uint8_t>(animation->CurrentValueBetween(0, 255));
   progress_overlay_->SetAlpha(alpha);
-  storage_row_->SetAlpha(255 - alpha);
+  if (storage_row_)
+    storage_row_->SetAlpha(255 - alpha);
 }
 
 void CardUnmaskPromptViews::InitIfNecessary() {

components/autofill.gypi
@@ -203,12 +203,10 @@
         'autofill/core/browser/webdata/autofill_webdata_service_observer.h',
       ],
       'conditions': [
-        ['desktop_linux==1', {
+        ['desktop_linux != 1', {
           # Controls whether Wallet cards can be saved to the local instance of
           # chrome.
-          'defines': [ 'ENABLE_SAVE_WALLET_CARDS_LOCALLY=0' ],
-        }, {
-          'defines': [ 'ENABLE_SAVE_WALLET_CARDS_LOCALLY=1' ],
+          'defines': [ 'ENABLE_SAVE_WALLET_CARDS_LOCALLY' ],
         }],
       ],
 

components/autofill/core/browser/BUILD.gn
@@ -120,10 +120,8 @@ static_library("browser") {
   ]
 
   # Controls whether Wallet cards can be saved to the local instance of chrome.
-  if (is_desktop_linux) {
-    defines = [ "ENABLE_SAVE_WALLET_CARDS_LOCALLY=0" ]
-  } else {
-    defines = [ "ENABLE_SAVE_WALLET_CARDS_LOCALLY=1" ]
+  if (!is_desktop_linux) {
+    defines = [ "ENABLE_SAVE_WALLET_CARDS_LOCALLY" ]
   }
 
   deps = [

components/autofill/core/browser/personal_data_manager_unittest.cc
@@ -15,6 +15,7 @@
 #include "base/prefs/pref_service.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/synchronization/waitable_event.h"
+#include "components/autofill/core/browser/autofill_experiments.h"
 #include "components/autofill/core/browser/autofill_profile.h"
 #include "components/autofill/core/browser/autofill_test_utils.h"
 #include "components/autofill/core/browser/form_structure.h"
@@ -494,6 +495,16 @@ TEST_F(PersonalDataManagerTest, UpdateServerCreditCards) {
   base::MessageLoop::current()->Run();
 
   ASSERT_EQ(3U, personal_data_->GetCreditCards().size());
+
+  if (!OfferStoreUnmaskedCards()) {
+    for (CreditCard* card : personal_data_->GetCreditCards()) {
+      EXPECT_EQ(CreditCard::MASKED_SERVER_CARD, card->record_type());
+    }
+    // The rest of this test doesn't work if we're force-masking all unmasked
+    // cards.
+    return;
+  }
+
   // The GUIDs will be different, so just compare the data.
   for (size_t i = 0; i < 3; ++i)
     EXPECT_EQ(0, server_cards[i].Compare(*personal_data_->GetCreditCards()[i]));
@@ -3143,6 +3154,16 @@ TEST_F(PersonalDataManagerTest, UpdateServerCreditCardUsageStats) {
   base::MessageLoop::current()->Run();
 
   ASSERT_EQ(3U, personal_data_->GetCreditCards().size());
+
+  if (!OfferStoreUnmaskedCards()) {
+    for (CreditCard* card : personal_data_->GetCreditCards()) {
+      EXPECT_EQ(CreditCard::MASKED_SERVER_CARD, card->record_type());
+    }
+    // The rest of this test doesn't work if we're force-masking all unmasked
+    // cards.
+    return;
+  }
+
   // The GUIDs will be different, so just compare the data.
   for (size_t i = 0; i < 3; ++i)
     EXPECT_EQ(0, server_cards[i].Compare(*personal_data_->GetCreditCards()[i]));

commit d5076a6c8c56560bb2a03512d341a6e48676cfa2

Fix crash on Linux caused by null deref
(caused by 9abed9715d1e [formerly 423d45cbeb75])

BUG=481754

Review URL: https://codereview.chromium.org/1108173003

Cr-Commit-Position: refs/heads/master@{#327228}

Former-commit-id: e9eb2e67dbc5b6b6b59bc5888f679952e96d3c24

chrome/browser/ui/views/autofill/card_unmask_prompt_views.cc
@@ -109,7 +109,8 @@ void CardUnmaskPromptViews::GotVerificationResult(
     // TODO(estade): it's somewhat jarring when the error comes back too
     // quickly.
     overlay_animation_.Reset();
-    storage_row_->SetAlpha(255);
+    if (storage_row_)
+      storage_row_->SetAlpha(255);
     progress_overlay_->SetVisible(false);
 
     if (allow_retry) {