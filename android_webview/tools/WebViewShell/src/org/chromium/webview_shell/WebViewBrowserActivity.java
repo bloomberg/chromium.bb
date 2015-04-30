b3e28ba4d57a8013d768f10144efb798c3411343
timvolodine <timvolodine@chromium.org>
2015-04-30 09:50:22 -0700

    [Android WebView] Add url bar to WebViewShell and make it visible as app.
    This patch adds some basic browsing functionality for manual testing.
    It adds a url bar and a button to the WebViewBrowserActivity to
    manually load urls using webview. JavaScript and geolocation are
    enabled by default now.
    
    BUG=481508
    
    Review URL: https://codereview.chromium.org/1110523002
    
    Cr-Commit-Position: refs/heads/master@{#327725}
    
    Former-commit-id: 48944a7383fc9ca47260181fc9f8ffbc999cf5e2

--- android_webview/tools/WebViewShell/src/org/chromium/webview_shell/WebViewBrowserActivity.java
+++ android_webview/tools/WebViewShell/src/org/chromium/webview_shell/WebViewBrowserActivity.java
@@ -5,38 +5,94 @@
 package org.chromium.webview_shell;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
- * This activity is designed for URL browsing testing of WebView. It takes a URL as an argument, and
- * displays the page.
+ * This activity is designed for starting a "mini-browser" for manual testing of WebView.
+ * It takes an optional URL as an argument, and displays the page. There is a URL bar
+ * on top of the webview for manually specifying URLs to load.
  */
 public class WebViewBrowserActivity extends Activity {
+    private EditText mUrlBar;
+    private WebView mWebView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setTitle(
                 getResources().getString(R.string.title_activity_browser));
-        setContentView(R.layout.activity_webview);
-        WebView webView = (WebView) findViewById(R.id.webview);
+        setContentView(R.layout.activity_webview_browser);
+        mWebView = (WebView) findViewById(R.id.webview);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setGeolocationEnabled(true);
 
-        webView.setWebViewClient(new WebViewClient() {
+        mWebView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView webView, String url) {
                 return false;
             }
         });
 
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onGeolocationPermissionsShowPrompt(String origin,
+                    GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+
+        mUrlBar = (EditText) findViewById(R.id.url_field);
+        mUrlBar.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View view, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_UP) {
+                    loadUrlFromUrlBar(view);
+                    return true;
+                }
+                return false;
+            }
+        });
+
         String url = getUrlFromIntent(getIntent());
-        webView.loadUrl(url);
+        if (url != null) {
+            mUrlBar.setText(url, TextView.BufferType.EDITABLE);
+            loadUrl(url);
+        }
+    }
+
+    public void loadUrlFromUrlBar(View view) {
+        loadUrl(mUrlBar.getText().toString());
+        hideKeyboard(mUrlBar);
+    }
+
+    private void loadUrl(String url) {
+        mWebView.loadUrl(url);
+        mWebView.requestFocus();
+    }
+
+    /**
+     * Hides the keyboard.
+     * @param view The {@link View} that is currently accepting input.
+     * @return Whether the keyboard was visible before.
+     */
+    private static boolean hideKeyboard(View view) {
+        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(
+                Context.INPUT_METHOD_SERVICE);
+        return imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
     private static String getUrlFromIntent(Intent intent) {
         return intent != null ? intent.getDataString() : null;
     }
-
 }
