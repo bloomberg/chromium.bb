7c76b56a0cbd87d5ed6a673a300df6095053c51f
dbeam <dbeam@chromium.org>
2015-04-30 15:40:46 -0700

    Revert of Adding strict mode for bookmark_manager file for better error checking. (patchset #1 id:1 of https://codereview.chromium.org/1115683002/)
    Reason for revert:
    > LGTM assuming this doesn't trigger any errors :)
    
    It did on the closure bots. Given that this change is solely about better error checking and it's hampering our ability to do compile-time checking with closure compiler, I'm reverting.
    
    deepak.m1@: you may consider adding these statement inside of a scope (e.g. inside cr.define()) as we do a lot of file concatenation and some files might not work with strict mode.
    
    Here's how you check if closure compiler likes your change locally:
    https://code.google.com/p/chromium/wiki/ClosureCompilation
    
    Original issue's description:
    > Adding strict mode for bookmark_manager file for better error checking.
    >
    > Adding strict mode to bookmark manager's js files to detect error at load time itself.
    >
    > BUG=177166
    >
    > Committed: https://crrev.com/b701daa353fc74b0b79899f42ef43904ead103f1 [formerly 66c786a59403f02dc49df0090cece8e0e3cc3b1b]
    > Cr-Commit-Position: refs/heads/master@{#327679}
    
    TBR=bauerb@chromium.org,deepak.m1@samsung.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=177166
    
    Review URL: https://codereview.chromium.org/1117143002
    
    Cr-Commit-Position: refs/heads/master@{#327819}
    
    Former-commit-id: 55ca7b8160d679f0662e9e205824ae1a53f87a5c

--- chrome/browser/resources/bookmark_manager/js/main.js
+++ chrome/browser/resources/bookmark_manager/js/main.js
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-'use strict';
-
 (function() {
 /** @const */ var BookmarkList = bmm.BookmarkList;
 /** @const */ var BookmarkTree = bmm.BookmarkTree;
