ab8dce18b686982212a0b954445af313d64c83f6
tomfinegan <tomfinegan@chromium.org>
2015-05-01 15:56:23 -0700

    vpx_video_decoder: limit decode threads to number of processors.
    Avoid decoding VP8A with a single thread.
    
    BUG=480724
    
    Review URL: https://codereview.chromium.org/1120643005
    
    Cr-Commit-Position: refs/heads/master@{#328005}
    
    Former-commit-id: 40de287cfbf7a843f903b72ed15c2f82cb0537ea

--- media/filters/vpx_video_decoder.cc
+++ media/filters/vpx_video_decoder.cc
@@ -17,6 +17,7 @@
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/sys_byteorder.h"
+#include "base/sys_info.h"
 #include "base/trace_event/trace_event.h"
 #include "media/base/bind_to_current_loop.h"
 #include "media/base/decoder_buffer.h"
@@ -64,6 +65,8 @@ static int GetThreadCount(const VideoDecoderConfig& config) {
         decode_threads = 4;
     }
 
+    decode_threads = std::min(decode_threads,
+                              base::SysInfo::NumberOfProcessors());
     return decode_threads;
   }
 
