cd79f83999518ddaf47e678b45f0a0561d1ec6d8
reillyg <reillyg@chromium.org>
2015-05-05 14:00:13 -0700

    Set the content shell data path to the app shell test data directory.
    When running app_shell-based tests the content shell data path should be
    set to the test data directory so that tests do not interfere with each
    other or result in persistent state changes. This is what is done by the
    ContentTestLauncherDelegate for content_shell-based tests.
    
    BUG=483328
    
    Review URL: https://codereview.chromium.org/1118183002
    
    Cr-Commit-Position: refs/heads/master@{#328400}
    
    Former-commit-id: 4a96f6295a17790036908f527d841e9ef7d0a7d6

--- extensions/shell/test/shell_test_launcher_delegate.cc
+++ extensions/shell/test/shell_test_launcher_delegate.cc
@@ -4,7 +4,9 @@
 
 #include "extensions/shell/test/shell_test_launcher_delegate.h"
 
+#include "base/command_line.h"
 #include "base/test/test_suite.h"
+#include "content/shell/common/shell_switches.h"
 #include "extensions/shell/app/shell_main_delegate.h"
 
 namespace extensions {
@@ -16,6 +18,8 @@ int AppShellTestLauncherDelegate::RunTestSuite(int argc, char** argv) {
 bool AppShellTestLauncherDelegate::AdjustChildProcessCommandLine(
     base::CommandLine* command_line,
     const base::FilePath& temp_data_dir) {
+  command_line->AppendSwitchPath(switches::kContentShellDataPath,
+                                 temp_data_dir);
   return true;
 }
 
