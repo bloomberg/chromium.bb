ece0afe5005ed12fa59eb71afe11290844ff4b2a
brettw <brettw@chromium.org>
2015-05-05 13:41:35 -0700

    Make the GN Windows component build work.
    With this patch the Windows GN component build builds without errors.
    
    CQ_EXTRA_TRYBOTS=tryserver.chromium.linux:android_chromium_gn_compile_dbg,android_chromium_gn_compile_rel;tryserver.chromium.win:win8_chromium_gn_rel,win8_chromium_gn_dbg
    BUG=470212
    
    Review URL: https://codereview.chromium.org/1128563003
    
    Cr-Commit-Position: refs/heads/master@{#328397}
    
    Former-commit-id: 355adebf1a09206f905839b5af568558c2cd059c

--- chrome/installer/test/run_all_tests.cc
+++ chrome/installer/test/run_all_tests.cc
@@ -5,7 +5,7 @@
 #include "base/test/test_suite.h"
 
 int main(int argc, char **argv) {
-  TestSuite test_suite(argc, argv);
+  base::TestSuite test_suite(argc, argv);
 
   return test_suite.Run();
 }
