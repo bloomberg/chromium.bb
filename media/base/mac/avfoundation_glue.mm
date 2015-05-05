22a32741b87d105e9537358473d1f61fc83f952e
tommi <tommi@chromium.org>
2015-05-05 03:21:01 -0700

    Add a check to IsAVFoundationSupportedHelper to catch race. I suspect we might be running into a race there now in some cases where QTKit is all of a sudden being used when we should clearly be using AVFoundation.
    R=magjed@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1054253002
    
    Cr-Commit-Position: refs/heads/master@{#328307}
    
    Former-commit-id: 3c5c56260910abc517d5b436d0c6caf6d84ceb4d

--- media/base/mac/avfoundation_glue.mm
+++ media/base/mac/avfoundation_glue.mm
@@ -120,6 +120,8 @@ class AVFoundationInternal {
 // This contains the logic of checking whether AVFoundation is supported.
 // It's called only once and the results are cached in a static bool.
 bool IsAVFoundationSupportedHelper() {
+  CHECK([NSThread isMainThread]);
+
   // AVFoundation is only available on OS Lion and above.
   if (!base::mac::IsOSLionOrLater()) {
     LogCaptureApi(CAPTURE_API_QTKIT_DUE_TO_OS_PREVIOUS_TO_LION);
