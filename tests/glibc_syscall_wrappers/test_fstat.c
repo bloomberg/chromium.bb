cd439e8ffb181266f47a62d3c18f0af5550b85c2
Roland McGrath <mcgrathr@chromium.org>
2015-05-05 11:28:22 -0700

    Drop struct stat layout knowledge from test_fstat
    The exact layout of glibc's 'struct stat' is not something
    special we want to make ABI stable beyond the general state of
    ABI stability for glibc.  The nacl_arm_glibc layout is different
    from the old nacl_x86_glibc layout, so the test fails.  Just stop
    testing for the exact layout.
    
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3068
    TEST= trybots
    R=phosek@chromium.org, mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/1120763005
    
    
    Former-commit-id: c403984108045d0c020b1392d1dedcc20d26c4cd

--- tests/glibc_syscall_wrappers/test_fstat.c
+++ tests/glibc_syscall_wrappers/test_fstat.c
@@ -12,48 +12,12 @@
 
 int main(int argc, char** argv) {
   struct stat st;
-  FILE* file;
+  FILE *file;
   int fd;
   if (2 != argc) {
     printf("Usage: sel_ldr test_fstat.nexe test_stat_data\n");
     return 1;
   }
-  st.st_size = 0;
-  printf("st_dev offset %d, size %d\n",
-         offsetof(struct stat, st_dev),
-         sizeof(st.st_dev));
-  printf("st_ino offset %d, size %d\n",
-         offsetof(struct stat, st_ino),
-         sizeof(st.st_ino));
-  printf("st_mode offset %d, size %d\n",
-         offsetof(struct stat, st_mode),
-         sizeof(st.st_mode));
-  printf("st_nlink offset %d, size %d\n",
-         offsetof(struct stat, st_nlink),
-         sizeof(st.st_nlink));
-  printf("st_uid offset %d, size %d\n",
-         offsetof(struct stat, st_uid),
-         sizeof(st.st_uid));
-  printf("st_gid offset %d, size %d\n",
-         offsetof(struct stat, st_gid),
-         sizeof(st.st_gid));
-  printf("st_rdev offset %d, size %d\n",
-         offsetof(struct stat, st_rdev),
-         sizeof(st.st_rdev));
-  printf("st_size offset %d, size %d\n",
-         offsetof(struct stat, st_size),
-         sizeof(st.st_size));
-  printf("st_blksize offset %d, size %d\n",
-         offsetof(struct stat, st_blksize),
-         sizeof(st.st_blksize));
-  printf("st_blocks offset %d, size %d\n",
-         offsetof(struct stat, st_blocks),
-         sizeof(st.st_blocks));
-  printf("st_atim offset %d, size %d\n",
-         offsetof(struct stat, st_atim),
-         sizeof(st.st_atim));
-  printf("struct st size %d\n",
-         sizeof(st));
   file = fopen(argv[1], "r");
   if (file == NULL)
     return 2;
