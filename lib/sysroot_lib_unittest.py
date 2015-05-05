27e333e4d2acaf5bb06c94e073abb6cdf05a09b8
Bertrand SIMONNET <bsimonnet@chromium.org>
2015-05-05 14:38:38 -0700

    sysroot_lib_unittest: Fake being root.
    Most sysroot commands require root privilege and will shell out to sudo.
    Faking being root makes the unittests much faster.
    
    BUG=None
    TEST=unittests.
    
    Change-Id: I707aadab565fe198c1d6178925c7af648efc4879
    Reviewed-on: https://chromium-review.googlesource.com/269512
    Tested-by: Bertrand Simonnet <bsimonnet@chromium.org>
    Trybot-Ready: Bertrand Simonnet <bsimonnet@chromium.org>
    Reviewed-by: Christopher Wiley <wiley@chromium.org>
    Commit-Queue: Bertrand Simonnet <bsimonnet@chromium.org>

--- lib/sysroot_lib_unittest.py
+++ lib/sysroot_lib_unittest.py
@@ -14,89 +14,92 @@ from chromite.lib import osutils
 from chromite.lib import sysroot_lib
 
 
-class SysrootLibTest(cros_test_lib.TestCase):
+class SysrootLibTest(cros_test_lib.MockTempDirTestCase):
   """Unittest for sysroot_lib.py"""
+
+  def setUp(self):
+    """Setup the test environment."""
+    # Fake being root to avoid running all filesystem commands with
+    # SudoRunCommand.
+    self.PatchObject(os, 'getuid', return_value=0)
 
   def testGetStandardField(self):
     """Tests that standard field can be fetched correctly."""
-    with osutils.TempDir(sudo_rm=True) as tempdir:
-      sysroot = sysroot_lib.Sysroot(tempdir)
-      sysroot.WriteConfig('FOO="bar"')
-      self.assertEqual('bar', sysroot.GetStandardField('FOO'))
+    sysroot = sysroot_lib.Sysroot(self.tempdir)
+    sysroot.WriteConfig('FOO="bar"')
+    self.assertEqual('bar', sysroot.GetStandardField('FOO'))
 
-      # Works with multiline strings
-      multiline = """foo
+    # Works with multiline strings
+    multiline = """foo
 bar
 baz
 """
-      sysroot.WriteConfig('TEST="%s"' % multiline)
-      self.assertEqual(multiline, sysroot.GetStandardField('TEST'))
+    sysroot.WriteConfig('TEST="%s"' % multiline)
+    self.assertEqual(multiline, sysroot.GetStandardField('TEST'))
 
   def testReadWriteCache(self):
     """Tests that we can write and read to the cache."""
-    with osutils.TempDir(sudo_rm=True) as tempdir:
-      sysroot = sysroot_lib.Sysroot(tempdir)
+    sysroot = sysroot_lib.Sysroot(self.tempdir)
 
-      # If a field is not defined we get None.
-      self.assertEqual(None, sysroot.GetCachedField('foo'))
+    # If a field is not defined we get None.
+    self.assertEqual(None, sysroot.GetCachedField('foo'))
 
-      # If we set a field, we can get it.
-      sysroot.SetCachedField('foo', 'bar')
-      self.assertEqual('bar', sysroot.GetCachedField('foo'))
+    # If we set a field, we can get it.
+    sysroot.SetCachedField('foo', 'bar')
+    self.assertEqual('bar', sysroot.GetCachedField('foo'))
 
-      # Setting a field in an existing cache preserve the previous values.
-      sysroot.SetCachedField('hello', 'bonjour')
-      self.assertEqual('bar', sysroot.GetCachedField('foo'))
-      self.assertEqual('bonjour', sysroot.GetCachedField('hello'))
+    # Setting a field in an existing cache preserve the previous values.
+    sysroot.SetCachedField('hello', 'bonjour')
+    self.assertEqual('bar', sysroot.GetCachedField('foo'))
+    self.assertEqual('bonjour', sysroot.GetCachedField('hello'))
 
-      # Setting a field to None unsets it.
-      sysroot.SetCachedField('hello', None)
-      self.assertEqual(None, sysroot.GetCachedField('hello'))
+    # Setting a field to None unsets it.
+    sysroot.SetCachedField('hello', None)
+    self.assertEqual(None, sysroot.GetCachedField('hello'))
 
   def testErrorOnBadCachedValue(self):
     """Tests that we detect bad value for the sysroot cache."""
-    with osutils.TempDir(sudo_rm=True) as tempdir:
-      sysroot = sysroot_lib.Sysroot(tempdir)
+    sysroot = sysroot_lib.Sysroot(self.tempdir)
 
-      forbidden = [
-          'hello"bonjour',
-          'hello\\bonjour',
-          'hello\nbonjour',
-          'hello$bonjour',
-          'hello`bonjour',
-      ]
-      for value in forbidden:
-        with self.assertRaises(ValueError):
-          sysroot.SetCachedField('FOO', value)
+    forbidden = [
+        'hello"bonjour',
+        'hello\\bonjour',
+        'hello\nbonjour',
+        'hello$bonjour',
+        'hello`bonjour',
+    ]
+    for value in forbidden:
+      with self.assertRaises(ValueError):
+        sysroot.SetCachedField('FOO', value)
 
   def testProfileGeneration(self):
     """Tests that we generate the portage profile correctly."""
     # pylint: disable=protected-access
-    with osutils.TempDir(sudo_rm=True) as tempsysroot:
-      with osutils.TempDir() as tempdir:
-        overlays = [os.path.join(tempdir, letter) for letter in ('a', 'b', 'c')]
-        for o in overlays:
-          osutils.SafeMakedirs(o)
-        sysroot = sysroot_lib.Sysroot(tempsysroot)
+    overlay_dir = os.path.join(self.tempdir, 'overlays')
+    sysroot_dir = os.path.join(self.tempdir, 'sysroot')
+    overlays = [os.path.join(overlay_dir, letter) for letter in ('a', 'b', 'c')]
+    for o in overlays:
+      osutils.SafeMakedirs(o)
+    sysroot = sysroot_lib.Sysroot(sysroot_dir)
 
-        sysroot.WriteConfig(sysroot_lib._DictToKeyValue(
-            {'ARCH': 'arm',
-             'BOARD_OVERLAY': '\n'.join(overlays)}))
+    sysroot.WriteConfig(sysroot_lib._DictToKeyValue(
+        {'ARCH': 'arm',
+         'BOARD_OVERLAY': '\n'.join(overlays)}))
 
-        sysroot._GenerateProfile()
+    sysroot._GenerateProfile()
 
-        profile_link = os.path.join(sysroot.path, 'etc', 'make.profile')
-        profile_parent = osutils.ReadFile(
-            os.path.join(profile_link, 'parent')).splitlines()
-        self.assertTrue(os.path.islink(profile_link))
-        self.assertEqual(1, len(profile_parent))
-        self.assertTrue(re.match('chromiumos:.*arm.*', profile_parent[0]))
+    profile_link = os.path.join(sysroot.path, 'etc', 'make.profile')
+    profile_parent = osutils.ReadFile(
+        os.path.join(profile_link, 'parent')).splitlines()
+    self.assertTrue(os.path.islink(profile_link))
+    self.assertEqual(1, len(profile_parent))
+    self.assertTrue(re.match('chromiumos:.*arm.*', profile_parent[0]))
 
-        profile_dir = os.path.join(overlays[1], 'profiles', 'base')
-        osutils.SafeMakedirs(profile_dir)
+    profile_dir = os.path.join(overlays[1], 'profiles', 'base')
+    osutils.SafeMakedirs(profile_dir)
 
-        sysroot._GenerateProfile()
-        profile_parent = osutils.ReadFile(
-            os.path.join(profile_link, 'parent')).splitlines()
-        self.assertEqual(2, len(profile_parent))
-        self.assertEqual(profile_dir, profile_parent[1])
+    sysroot._GenerateProfile()
+    profile_parent = osutils.ReadFile(
+        os.path.join(profile_link, 'parent')).splitlines()
+    self.assertEqual(2, len(profile_parent))
+    self.assertEqual(profile_dir, profile_parent[1])
