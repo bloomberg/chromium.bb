8a4883f6f96c0a8ac675d3369b2aa5a640d2c519
yoshiki <yoshiki@chromium.org>
2015-05-07 02:42:32 -0700

    Reland of [Download Notification] Add cancel-button and download-removed tests
    The original CL (crrev.com/24185c2b) had been reverted due to the memory leak (crrev.com/a8d1b6a3). But memory leak has been fixed so this patch reland the original CL.
    
    The diff of this CL is completely same as the original.
    
    Original CL's description:
        > [Download Notification] Add cancel-button and download-removed tests
        >
        > This patch adds the following tests:
        > - Clicking cancel-button while download is in progress
        > - Clicking cancel-button after download is finished
        > - Download is removed externally
        >
        > BUG=480489
        > TEST=manually tested
        >
        > Committed: https://crrev.com/3fb884894e57a59ce73feb6af33dd948d0eb4743 [formerly 24185c2b385071c1ba007b3cc16f52692dd78d02]
        > Cr-Commit-Position: refs/heads/master@{#328059}
    
    TBR=yoshiki@chromium.org, asanka@chromium.org
    BUG=480489
    
    Review URL: https://codereview.chromium.org/1132483002
    
    Cr-Commit-Position: refs/heads/master@{#328735}
    
    Former-commit-id: 31f75c00b6b26521182bb38d6875d5b1dfbc6770

--- chrome/browser/download/notification/download_notification_browsertest.cc
+++ chrome/browser/download/notification/download_notification_browsertest.cc
@@ -321,57 +321,109 @@ class DownloadNotificationTest : public DownloadNotificationTestBase {
 };
 
 IN_PROC_BROWSER_TEST_F(DownloadNotificationTest, DownloadFile) {
-  GURL url(net::URLRequestSlowDownloadJob::kUnknownSizeUrl);
-
-  // Starts a download.
-  NotificationAddObserver download_start_notification_observer;
-  ui_test_utils::NavigateToURL(browser(), url);
-  EXPECT_TRUE(download_start_notification_observer.Wait());
-
-  // Confirms that a notification is created.
-  std::string notification_id =
-      download_start_notification_observer.notification_id();
-  EXPECT_FALSE(notification_id.empty());
-
-  // Confirms that a download is also started.
-  std::vector<content::DownloadItem*> downloads;
-  GetDownloadManager(browser())->GetAllDownloads(&downloads);
-  EXPECT_EQ(1u, downloads.size());
-  content::DownloadItem* download = downloads[0];
+  CreateDownload();
 
   EXPECT_EQ(l10n_util::GetStringFUTF16(
                 IDS_DOWNLOAD_STATUS_IN_PROGRESS_TITLE,
-                download->GetFileNameToReportUser().LossyDisplayName()),
-            GetNotification(notification_id)->title());
+                download_item()->GetFileNameToReportUser().LossyDisplayName()),
+            GetNotification(notification_id())->title());
   EXPECT_EQ(message_center::NOTIFICATION_TYPE_PROGRESS,
-            GetNotification(notification_id)->type());
+            GetNotification(notification_id())->type());
 
   // Requests to complete the download.
   ui_test_utils::NavigateToURL(
       browser(), GURL(net::URLRequestSlowDownloadJob::kFinishDownloadUrl));
 
   // Waits for download completion.
-  while (download->GetState() != content::DownloadItem::COMPLETE) {
+  while (download_item()->GetState() != content::DownloadItem::COMPLETE) {
     NotificationUpdateObserver download_change_notification_observer;
     download_change_notification_observer.Wait();
   }
 
   EXPECT_EQ(l10n_util::GetStringFUTF16(
                 IDS_DOWNLOAD_STATUS_DOWNLOADED_TITLE,
-                download->GetFileNameToReportUser().LossyDisplayName()),
-            GetNotification(notification_id)->title());
+                download_item()->GetFileNameToReportUser().LossyDisplayName()),
+            GetNotification(notification_id())->title());
   EXPECT_EQ(message_center::NOTIFICATION_TYPE_SIMPLE,
-            GetNotification(notification_id)->type());
+            GetNotification(notification_id())->type());
 
   // Opens the message center.
   GetMessageCenter()->SetVisibility(message_center::VISIBILITY_MESSAGE_CENTER);
 
   // Try to open the downloaded item by clicking the notification.
   EXPECT_FALSE(GetDownloadManagerDelegate()->opened());
-  GetMessageCenter()->ClickOnNotification(notification_id);
+  GetMessageCenter()->ClickOnNotification(notification_id());
   EXPECT_TRUE(GetDownloadManagerDelegate()->opened());
 
-  EXPECT_FALSE(GetNotification(notification_id));
+  EXPECT_FALSE(GetNotification(notification_id()));
+}
+
+IN_PROC_BROWSER_TEST_F(DownloadNotificationTest,
+                       CloseNotificationAfterDownload) {
+  CreateDownload();
+
+  // Requests to complete the download.
+  ui_test_utils::NavigateToURL(
+      browser(), GURL(net::URLRequestSlowDownloadJob::kFinishDownloadUrl));
+
+  // Waits for download completion.
+  while (download_item()->GetState() != content::DownloadItem::COMPLETE) {
+    NotificationUpdateObserver download_change_notification_observer;
+    download_change_notification_observer.Wait();
+  }
+
+  // Opens the message center.
+  GetMessageCenter()->SetVisibility(message_center::VISIBILITY_MESSAGE_CENTER);
+
+  // Closes the notification.
+  NotificationRemoveObserver notification_close_observer;
+  GetMessageCenter()->RemoveNotification(notification_id(), true /* by_user */);
+  EXPECT_EQ(notification_id(), notification_close_observer.Wait());
+
+  EXPECT_EQ(0u, GetMessageCenter()->GetVisibleNotifications().size());
+
+  // Confirms that a download is also started.
+  std::vector<content::DownloadItem*> downloads;
+  GetDownloadManager(browser())->GetAllDownloads(&downloads);
+  EXPECT_EQ(1u, downloads.size());
+  EXPECT_EQ(content::DownloadItem::COMPLETE, downloads[0]->GetState());
+}
+
+IN_PROC_BROWSER_TEST_F(DownloadNotificationTest,
+                       CloseNotificationWhileDownloading) {
+  CreateDownload();
+
+  // Closes the notification.
+  NotificationRemoveObserver notification_close_observer;
+  GetMessageCenter()->RemoveNotification(notification_id(), true /* by_user */);
+  EXPECT_EQ(notification_id(), notification_close_observer.Wait());
+
+  EXPECT_EQ(0u, GetMessageCenter()->GetVisibleNotifications().size());
+
+  // Confirms that a download is still in progress.
+  std::vector<content::DownloadItem*> downloads;
+  GetDownloadManager(browser())->GetAllDownloads(&downloads);
+  EXPECT_EQ(1u, downloads.size());
+  EXPECT_EQ(content::DownloadItem::IN_PROGRESS, downloads[0]->GetState());
+
+  // Cleans the downloading.
+  downloads[0]->Cancel(true);
+}
+
+IN_PROC_BROWSER_TEST_F(DownloadNotificationTest, DownloadRemoved) {
+  CreateDownload();
+
+  NotificationRemoveObserver notification_close_observer;
+  download_item()->Remove();
+  EXPECT_EQ(notification_id(), notification_close_observer.Wait());
+
+  // Confirms that the notification is removed.
+  EXPECT_EQ(0u, GetMessageCenter()->GetVisibleNotifications().size());
+
+  // Confirms that the download item is removed.
+  std::vector<content::DownloadItem*> downloads;
+  GetDownloadManager(browser())->GetAllDownloads(&downloads);
+  EXPECT_EQ(0u, downloads.size());
 }
 
 IN_PROC_BROWSER_TEST_F(DownloadNotificationTest, DownloadMultipleFiles) {
