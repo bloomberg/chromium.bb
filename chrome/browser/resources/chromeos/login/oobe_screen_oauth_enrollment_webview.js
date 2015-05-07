eb458859dd8954f35d8e7eca4f5a59b634dac687
dzhioev <dzhioev@chromium.org>
2015-05-07 16:20:24 -0700

    Further improvements in new enrollment UI.
    What was done:
    * Status pages have new desing now (<notification-card> element).
    * Close button have a correct color depending of background color.
    * Introduced new step 'attribute-promtp-error' which is <notification-card> too.
    
    BUG=471934
    TEST=manually,enrollment works
    
    Review URL: https://codereview.chromium.org/1119933003
    
    Cr-Commit-Position: refs/heads/master@{#328875}
    
    Former-commit-id: 5b421b6533bc26a603da85e14891b426c616bf02

--- chrome/browser/resources/chromeos/login/oobe_screen_oauth_enrollment_webview.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_oauth_enrollment_webview.js
@@ -9,6 +9,9 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
   /** @const */ var STEP_ERROR = 'error';
   /** @const */ var STEP_SUCCESS = 'success';
 
+  /* TODO(dzhioev): define this step on C++ side.
+  /** @const */ var STEP_ATTRIBUTE_PROMPT_ERROR = 'attribute-prompt-error';
+
   /** @const */ var HELP_TOPIC_ENROLLMENT = 4631259;
 
   return {
@@ -104,8 +107,16 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
                 false);
           }).bind(this);
 
-      $('oauth-enroll-error-retry').addEventListener('click',
-                                                     this.doRetry_.bind(this));
+      $('oauth-enroll-error-card').addEventListener('buttonclick',
+                                                    this.doRetry_.bind(this));
+      function doneCallback() {
+        chrome.send('oauthEnrollClose', ['done']);
+      };
+
+      $('oauth-enroll-attribute-prompt-error-card').addEventListener(
+          'buttonclick', doneCallback);
+      $('oauth-enroll-success-card').addEventListener(
+          'buttonclick', doneCallback);
 
       $('oauth-enroll-cancel-button').addEventListener('mousedown',
                                                        function(e) {
@@ -152,14 +163,6 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
         button.addEventListener('click', handler);
         buttons.push(button);
       }
-
-      makeButton(
-          'oauth-enroll-done-button',
-          ['oauth-enroll-focus-on-success'],
-          loadTimeData.getString('oauthEnrollDone'),
-          function() {
-            chrome.send('oauthEnrollClose', ['done']);
-          });
 
       makeButton(
           'oauth-enroll-continue-button',
@@ -231,50 +234,16 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
     },
 
     /**
-     * Returns whether |step| is the device attribute update error or
-     * not.
-     */
-    isAttributeUpdateError: function(step) {
-      return this.currentStep_ == STEP_ATTRIBUTE_PROMPT && step == STEP_ERROR;
-    },
-
-    /**
-     * Shows cancel or done button.
-     */
-    hideCancelShowDone: function(hide) {
-      $('oauth-enroll-cancel-button').hidden = hide;
-      $('oauth-enroll-cancel-button').disabled = hide;
-
-      $('oauth-enroll-done-button').hidden = !hide;
-      $('oauth-enroll-done-button').disabled = !hide;
-    },
-
-    /**
      * Switches between the different steps in the enrollment flow.
      * @param {string} step the steps to show, one of "signin", "working",
      * "attribute-prompt", "error", "success".
      */
     showStep: function(step) {
-      var focusStep = step;
-
       this.classList.toggle('oauth-enroll-state-' + this.currentStep_, false);
       this.classList.toggle('oauth-enroll-state-' + step, true);
 
-      /**
-       * In case of device attribute update error done button should be shown
-       * instead of cancel button and focus on success,
-       * because enrollment has completed
-       */
-      if (this.isAttributeUpdateError(step)) {
-        focusStep = STEP_SUCCESS;
-        this.hideCancelShowDone(true);
-      } else {
-        if (step == STEP_ERROR)
-          this.hideCancelShowDone(false);
-      }
-
       var focusElements =
-          this.querySelectorAll('.oauth-enroll-focus-on-' + focusStep);
+          this.querySelectorAll('.oauth-enroll-focus-on-' + step);
       for (var i = 0; i < focusElements.length; ++i) {
         if (getComputedStyle(focusElements[i])['display'] != 'none') {
           focusElements[i].focus();
@@ -290,8 +259,15 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
      * @param {boolean} retry whether the retry link should be shown.
      */
     showError: function(message, retry) {
-      $('oauth-enroll-error-message').textContent = message;
-      $('oauth-enroll-error-retry').hidden = !retry;
+      if (this.currentStep_ == STEP_ATTRIBUTE_PROMPT) {
+        $('oauth-enroll-attribute-prompt-error-card').textContent =
+          message;
+        this.showStep(STEP_ATTRIBUTE_PROMPT_ERROR);
+        return;
+      }
+      $('oauth-enroll-error-card').textContent = message;
+      $('oauth-enroll-error-card').buttonLabel =
+          retry ? loadTimeData.getString('oauthEnrollRetry') : '';
       this.showStep(STEP_ERROR);
     },
 
