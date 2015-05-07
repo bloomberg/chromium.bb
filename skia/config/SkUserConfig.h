d7800622a9d96facecb38c470d052adf8bc276b9
reed <reed@google.com>
2015-05-07 07:52:35 -0700

    remove SK_SUPPORT_LEGACY_ROUND_MIPMAP_LEVEL_CHOICE to get new 'floor' behavior
    BUG=479025
    
    Review URL: https://codereview.chromium.org/1131693002
    
    Cr-Commit-Position: refs/heads/master@{#328763}
    
    Former-commit-id: f44aff1b0a24cf15d09eafc0c49ce64850795d1f

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -249,10 +249,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_LEGACY_DRAWPICTURECALLBACK
 #endif
 
-#ifndef    SK_SUPPORT_LEGACY_ROUND_MIPMAP_LEVEL_CHOICE
-#   define SK_SUPPORT_LEGACY_ROUND_MIPMAP_LEVEL_CHOICE
-#endif
-
 #ifndef    SK_SUPPORT_LEGACY_GETDEVICE
 #   define SK_SUPPORT_LEGACY_GETDEVICE
 #endif
