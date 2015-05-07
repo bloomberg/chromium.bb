68d43e167572c0203a5a6e32604762f32d5db062
Christian Egli <christian.egli@sbs.ch>
2015-05-07 12:01:44 +0200

    Added missing license header
    See #22

--- python/setup.py
+++ python/setup.py
@@ -1,3 +1,23 @@
+# Liblouis Python ctypes bindings
+#
+#  Copyright (C) 2009 Eitan Isaacson <eitan@ascender.com>
+#  Copyright (C) 2010 James Teh <jamie@jantrid.net>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Library General Public
+# License as published by the Free Software Foundation; either
+# version 2 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Library General Public License for more details.
+#
+# You should have received a copy of the GNU Library General Public
+# License along with this library; if not, write to the
+# Free Software Foundation, Inc., Franklin Street, Fifth Floor,
+# Boston MA  02110-1301 USA.
+
 r"""Python bindings for liblouis
 """
 
