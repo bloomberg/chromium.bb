06cde95f72ea4b8398c2ed1e85f614d81dec3f1c
Christian Egli <christian.egli@sbs.ch>
2015-05-07 12:10:50 +0200

    Add missing license header
    See #22

--- windows/include/config.h
+++ windows/include/config.h
@@ -1,2 +1,25 @@
+/* liblouis Braille Translation and Back-Translation  Library
+
+   Copyright (C) 2014 ViewPlus Technologies, Inc. www.viewplus.com
+   and the liblouis team. http://liblouis.org
+   All rights reserved
+
+   This file is free software; you can redistribute it and/or modify it
+   under the terms of the Lesser or Library GNU General Public License 
+   as published by the
+   Free Software Foundation; either version 3, or (at your option) any
+   later version.
+
+   This file is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+   Library GNU General Public License for more details.
+
+   You should have received a copy of the Library GNU General Public 
+   License along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+   */
+
 #define PACKAGE_VERSION "liblouis-2.5.2"
 
