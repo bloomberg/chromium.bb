480a9ec42fb438e0461372bb34eb3626e64ddcd9
Christian Egli <christian.egli@sbs.ch>
2015-05-07 09:23:41 +0200

    Added a license header
    See #22

--- tests/outpos.c
+++ tests/outpos.c
@@ -1,3 +1,12 @@
+/* liblouis Braille Translation and Back-Translation Library
+
+Copyright (C) 2012 James Teh <jamie@nvaccess.org>
+
+Copying and distribution of this file, with or without modification,
+are permitted in any medium without royalty provided the copyright
+notice and this notice are preserved. This file is offered as-is,
+without any warranty. */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
