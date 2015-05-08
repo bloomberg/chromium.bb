983707351d52763ae9855bc7c1d363dbe07094db
rch <rch@chromium.org>
2015-05-08 12:09:00 -0700

    Add AllReadDataConsumed and AllWriteDataConsumed methods to SocketDataProvider so that all provider subclasses can have their state verified.
    Committed: https://crrev.com/5d3fe4a2e20302f6d7c536d8ad48b5882c60231d [formerly bce10d97d6fc5233071427cf622aaad7abde9e19]
    Cr-Commit-Position: refs/heads/master@{#328154}
    
    Committed: https://crrev.com/29e3c11fdec9c490faef42ba31c5441a7b2865e1 [formerly 269875cf0d23881a7ad119bae730ff54fdd4dd0b]
    Cr-Commit-Position: refs/heads/master@{#328351}
    
    Committed: https://crrev.com/b50c70fe0686a3689c098f93b92540002fc2b1b8 [formerly d833a674a5e4f3b4e47f229b3d5411a8d509c2c0]
    Cr-Commit-Position: refs/heads/master@{#328909}
    
    Review URL: https://codereview.chromium.org/1114383003
    
    Cr-Commit-Position: refs/heads/master@{#329003}
    
    Former-commit-id: 1d6f67fe72b4a9ae94fcb0a6e051d803a180bde1

--- net/socket/socket_test_util.h
+++ net/socket/socket_test_util.h
@@ -198,6 +198,8 @@ class SocketDataProvider {
   virtual MockRead OnRead() = 0;
   virtual MockWriteResult OnWrite(const std::string& data) = 0;
   virtual void Reset() = 0;
+  virtual bool AllReadDataConsumed() const = 0;
+  virtual bool AllWriteDataConsumed() const = 0;
 
   // Accessor for the socket which is using the SocketDataProvider.
   AsyncSocket* socket() { return socket_; }
@@ -294,6 +296,8 @@ class StaticSocketDataProvider : public SocketDataProvider {
   MockRead OnRead() override;
   MockWriteResult OnWrite(const std::string& data) override;
   void Reset() override;
+  bool AllReadDataConsumed() const override;
+  bool AllWriteDataConsumed() const override;
 
   size_t read_index() const { return helper_.read_index(); }
   size_t write_index() const { return helper_.write_index(); }
@@ -499,6 +503,8 @@ class SequencedSocketData : public SocketDataProvider {
   MockRead OnRead() override;
   MockWriteResult OnWrite(const std::string& data) override;
   void Reset() override;
+  bool AllReadDataConsumed() const override;
+  bool AllWriteDataConsumed() const override;
 
   // Returns true if all data has been read.
   bool at_read_eof() const;
