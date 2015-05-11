aecdac64e500791136a51e6b27b08b0f2463f7a4
r.kasibhatla <r.kasibhatla@samsung.com>
2015-05-11 03:52:05 -0700

    [Cleanup] Remove duplicated check from TopControlsManager::ScrollBy
    Check for top bar control height is getting unneccessarily duplicated in
    TopControlsManager::ScrollBy. The duplicated code seems to have got
    added in the merging of the below two patches:
    https://crrev.com/982333003 and https://crrev.com/961023002
    
    BUG=None
    
    R=bokan,aelias
    
    TESTS=Not Required
    
    Review URL: https://codereview.chromium.org/1124843002
    
    Cr-Commit-Position: refs/heads/master@{#329138}
    
    Former-commit-id: 4c0a51aeb774512d9f2560e84dfd2be0a0687018

--- cc/input/top_controls_manager.cc
+++ cc/input/top_controls_manager.cc
@@ -107,9 +107,6 @@ gfx::Vector2dF TopControlsManager::ScrollBy(
   if (pinch_gesture_active_)
     return pending_delta;
 
-  if (!TopControlsHeight())
-    return pending_delta;
-
   if (permitted_state_ == SHOWN && pending_delta.y() > 0)
     return pending_delta;
   else if (permitted_state_ == HIDDEN && pending_delta.y() < 0)
