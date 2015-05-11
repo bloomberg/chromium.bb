e67643ed5abd8c75bc7186b3ffefb225113d520c
asvitkine <asvitkine@chromium.org>
2015-05-11 09:46:29 -0700

    Remove SafeBrowsing LightSpeed experiment code.
    The experiment is now concluded.
    
    BUG=450037
    
    Review URL: https://codereview.chromium.org/1126233005
    
    Cr-Commit-Position: refs/heads/master@{#329159}
    
    Former-commit-id: 5bef655c3a9bed25ff2bb87584effebd2940268a

--- chrome/browser/safe_browsing/safe_browsing_service.cc
+++ chrome/browser/safe_browsing/safe_browsing_service.cc
@@ -12,6 +12,7 @@
 #include "base/command_line.h"
 #include "base/debug/leak_tracker.h"
 #include "base/lazy_instance.h"
+#include "base/metrics/field_trial.h"
 #include "base/path_service.h"
 #include "base/prefs/pref_change_registrar.h"
 #include "base/prefs/pref_service.h"
@@ -38,7 +39,6 @@
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "components/startup_metric_utils/startup_metric_utils.h"
-#include "components/variations/variations_associated_data.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/cookie_store_factory.h"
 #include "content/public/browser/notification_service.h"
@@ -582,11 +582,6 @@ void SafeBrowsingService::RefreshState() {
       break;
     }
   }
-
-  // TODO(asvitkine): Experimental code for measuring start up impact of SB.
-  // Remove when experimentation is complete. http://crbug.com/450037
-  if (!variations::GetVariationParamValue("LightSpeed", "DisableSB").empty())
-    enable = false;
 
   if (enable)
     Start();
