77a1538a192b3b71ea71972fd91d5b91ded4ecc2
mtklein <mtklein@chromium.org>
2015-05-12 15:37:52 -0700

    Add a guard define to keep Chromium/Blink on Sk4f xfermodes.
    BUG=
    
    Review URL: https://codereview.chromium.org/1137513004
    
    Cr-Commit-Position: refs/heads/master@{#329519}
    
    Former-commit-id: 711c75232fc39b4b04caa7647cec0a3ff19de6ae

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -273,6 +273,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_LEGACY_STROKE_CURVES
 #endif
 
+#ifndef    SK_PREFER_LEGACY_FLOAT_XFERMODES
+#   define SK_PREFER_LEGACY_FLOAT_XFERMODES
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
