6d14750225e95775b7a3bae44938ecee4fb86420
binjin <binjin@chromium.org>
2015-05-13 10:05:15 -0700

    Revert of Allow user to manually fetch remote commands (patchset #3 id:40001 of https://codereview.chromium.org/1106193003/)
    Reason for revert:
    crash occasionally on CrOS
    
    Original issue's description:
    > Allow user to manually fetch remote commands
    >
    > Currently remote commands fetch depends solely on invalidation, evaluate and
    > possibly remove this after invalidation service is landed and tested.
    >
    > BUG=480982
    >
    > Committed: https://crrev.com/c5008a6c563910bf6e535bd8526d77feb56abbfc [formerly 610f47342c8d725e239513ac17727a176d161495]
    > Cr-Commit-Position: refs/heads/master@{#327765}
    
    TBR=bartfab@chromium.org,estade@chromium.org
    BUG=480982,486672
    
    Review URL: https://codereview.chromium.org/1132223003
    
    Cr-Commit-Position: refs/heads/master@{#329660}
    
    Former-commit-id: d7f2f2b28ae05528a593104806e0c9975a8025b1

--- chrome/browser/ui/webui/policy_ui.cc
+++ chrome/browser/ui/webui/policy_ui.cc
@@ -36,7 +36,6 @@
 #include "components/policy/core/common/policy_namespace.h"
 #include "components/policy/core/common/policy_service.h"
 #include "components/policy/core/common/policy_types.h"
-#include "components/policy/core/common/remote_commands/remote_commands_service.h"
 #include "components/policy/core/common/schema.h"
 #include "components/policy/core/common/schema_map.h"
 #include "components/policy/core/common/schema_registry.h"
@@ -53,7 +52,6 @@
 #include "ui/base/l10n/time_format.h"
 
 #if defined(OS_CHROMEOS)
-#include "chrome/browser/browser_process_platform_part.h"
 #include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.h"
 #include "chrome/browser/chromeos/policy/device_local_account_policy_service.h"
@@ -809,18 +807,6 @@ void PolicyUIHandler::HandleInitialized(const base::ListValue* args) {
 }
 
 void PolicyUIHandler::HandleReloadPolicies(const base::ListValue* args) {
-#if defined(OS_CHROMEOS)
-  // Allow user to manually fetch remote commands, in case invalidation
-  // service is not working properly.
-  // TODO(binjin): evaluate and possibly remove this after invalidation
-  // service is landed and tested. http://crbug.com/480982
-  policy::BrowserPolicyConnectorChromeOS* connector =
-      g_browser_process->platform_part()->browser_policy_connector_chromeos();
-  connector->GetDeviceCloudPolicyManager()
-      ->core()
-      ->remote_commands_service()
-      ->FetchRemoteCommands();
-#endif
   GetPolicyService()->RefreshPolicies(
       base::Bind(&PolicyUIHandler::OnRefreshPoliciesDone,
                  weak_factory_.GetWeakPtr()));
