3073db7150d3a10ed9d52124edf35f00e0ec86ed
michaelpg <michaelpg@chromium.org>
2015-05-12 20:15:27 -0700

    Convert cr-settings-drawer to Polymer 0.8
    BUG=485381
    
    Review URL: https://codereview.chromium.org/1130113005
    
    Cr-Commit-Position: refs/heads/master@{#329577}
    
    Former-commit-id: dccd714e0fdc6a8c1e7c7b4a456358dd215dfe64

--- chrome/browser/resources/settings/settings_drawer/settings_drawer.js
+++ chrome/browser/resources/settings/settings_drawer/settings_drawer.js
@@ -9,50 +9,53 @@
  *
  * Example:
  *
- *     <core-drawer-panel>
- *       <cr-settings-drawer drawer selectedId="{{selectedId}}"
- *           pages="{{pages}}">
+ *     <paper-drawer-panel>
+ *       <cr-settings-drawer drawer selected-id="{{selectedId}}"
+ *           pages="[[pages]]">
  *       </cr-settings-drawer>
- *       <cr-settings-main main selectedId="{{selectedId}}" pages="{{pages}}">
+ *       <cr-settings-main main selected-id="{{selectedId}}" pages="[[pages]]">
  *       </cr-settings-main>
- *     </core-drawer-panel>
+ *     </paper-drawer-panel>
  *
  * @group Chrome Settings Elements
  * @element cr-settings-drawer
  */
-Polymer('cr-settings-drawer', {
-  publish: {
+Polymer({
+  is: 'cr-settings-drawer',
+
+  properties: {
     /**
      * Pages to include in the navigation.
-     *
-     * @attribute pages
-     * @type Array<!Object>
-     * @default null
+     * @type {!Array<!HTMLElement>}
      */
-    pages: null,
+    pages: {
+      type: Array,
+      value: function() { return []; },
+    },
 
     /**
      * ID of the currently selected page.
-     *
-     * @attribute selectedId
-     * @type string
-     * default ''
+     * @type {string}
      */
-    selectedId: '',
-  },
+    selectedId: {
+      type: String,
+      notify: true,
+    },
 
-  /** @override */
-  created: function() {
-    this.pages = [];
-  },
-
-  /**
-   * @type {Object}
-   * TODO(michaelpg): Create custom element and data source for user card.
-   */
-  user: {
-    name: 'Chrome User',
-    email: 'user@example.com',
-    iconUrl: 'chrome://theme/IDR_PROFILE_AVATAR_23@1x',
+    /**
+     * @private {!Object}
+     * TODO(michaelpg): Create custom element and data source for user card.
+     */
+    user_: {
+      type: Object,
+      value: function() {
+        return {
+          name: 'Chrome User',
+          email: 'user@example.com',
+          iconUrl: 'chrome://theme/IDR_PROFILE_AVATAR_23@1x',
+        };
+      },
+      readOnly: true,
+    },
   },
 });
