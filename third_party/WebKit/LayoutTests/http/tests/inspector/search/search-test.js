bd3ccd77d02c75265cc41d40b902f1ec21b9e8fe
lushnikov@chromium.org <lushnikov@chromium.org>
2015-05-15 17:10:18 +0000

    DevTools: migrate source-search-scope-many-projects.html off from MockWorkspace
    The patch migrates source-search-scope-many-projects.html from MockWorkspace
    to TestFileSystem.
    
    It also moves away from sync XHR's and unskips the test.
    
    BUG=466529
    R=pfeldman_slow
    
    Review URL: https://codereview.chromium.org/1136283005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195414 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6ec605d0c887b1635d2c89ec6318be9e18af2733

--- third_party/WebKit/LayoutTests/http/tests/inspector/search/search-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/search/search-test.js
@@ -22,4 +22,42 @@ InspectorTest.dumpSearchMatches = function(searchMatches)
     InspectorTest.addResult("");
 };
 
+InspectorTest.runSearchAndDumpResults = function(scope, searchConfig, sortByURI, callback)
+{
+    var searchResults = [];
+    var progress = new WebInspector.Progress();
+    scope.performSearch(searchConfig, progress, searchResultCallback, searchFinishedCallback);
+
+    function searchResultCallback(searchResult)
+    {
+        searchResults.push(searchResult);
+    }
+
+    function searchFinishedCallback()
+    {
+        function comparator(searchResultA, searchResultB)
+        {
+            return searchResultA.uiSourceCode.uri().compareTo(searchResultB.uiSourceCode.uri());
+        }
+        if (sortByURI)
+            searchResults.sort(comparator);
+
+        for (var i = 0; i < searchResults.length; ++i) {
+            var searchResult = searchResults[i];
+            var uiSourceCode = searchResult.uiSourceCode;
+            var searchMatches = searchResult.searchMatches;
+
+            if (!searchMatches.length)
+                continue;
+            InspectorTest.addResult("Search result #" + (i + 1) + ": uiSourceCode.uri = " + uiSourceCode.uri());
+            for (var j = 0; j < searchMatches.length; ++j) {
+                var lineNumber = searchMatches[j].lineNumber;
+                var lineContent = searchMatches[j].lineContent;
+                InspectorTest.addResult("  search match #" + (j + 1) + ": lineNumber = " + lineNumber + ", lineContent = '" + lineContent + "'");
+            }
+        }
+        callback();
+    }
+}
+
 };
