3cd85535e3dc37c42f80a2d4174150fbc51c7010
thakis <thakis@chromium.org>
2015-05-16 20:30:55 -0700

    Work around a clang/win bug, add a .h file to gyp/gn files.
    clang can't handle dllexported classes with only member initializers yet
    (see crbug and http://llvm.org/PR23542). As a workaround, don't dllexport
    this class.  This has the effect of emitting the class's constructor into
    every translation unit that uses it instead of just every translation unit of
    the media component, but in a way that seems nicer anyways since there's no
    .cc file for this header and so currently this relies on some translation unit
    in media including this header by lucky accident (else the constructor wouldn't
    be defined anywhere.)
    
    Once the clang bug is fixed we can consider adding this back; in the meantime
    this seems like a tiny price to pay to get the bots back green.
    
    Also add the .h file to the relevant gyp/gn files, else the analyze
    step will make the trybots think that nothing changed when this
    .h file is touched.
    
    BUG=488634,467779
    TBR=sandersd
    
    Review URL: https://codereview.chromium.org/1133423003
    
    Cr-Commit-Position: refs/heads/master@{#330299}
    
    Former-commit-id: 8a0f0753b1ece73c5636b263f4c4a13f75fcb2cd

--- media/base/cdm_config.h
+++ media/base/cdm_config.h
@@ -12,7 +12,7 @@ namespace media {
 // The runtime configuration for new CDM instances as computed by
 // |requestMediaKeySystemAccess|. This is in some sense the Chromium-side
 // counterpart of Blink's WebMediaKeySystemConfiguration.
-struct MEDIA_EXPORT CdmConfig {
+struct CdmConfig {
   // Allow access to a distinctive identifier.
   bool allow_distinctive_identifier = false;
 
--- media/media.gyp
+++ media/media.gyp
@@ -259,6 +259,7 @@
         'base/byte_queue.h',
         'base/cdm_callback_promise.cc',
         'base/cdm_callback_promise.h',
+        'base/cdm_config.h',
         'base/cdm_context.cc',
         'base/cdm_context.h',
         'base/cdm_factory.cc',
