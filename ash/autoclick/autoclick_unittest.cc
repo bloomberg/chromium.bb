19aa08c16fd2765defa06b13045b975115556a8e
xdai <xdai@chromium.org>
2015-05-19 23:53:38 -0700

    Disable AutoclickTest.DISABLED_SynthesizedMouseMovesIgnored test on official build since it has been failing for several days.
    BUG=489896
    
    Review URL: https://codereview.chromium.org/1124113006
    
    Cr-Commit-Position: refs/heads/master@{#330705}
    
    Former-commit-id: 3686570d07c015646957cf737999b3077cd0fca9

--- ash/autoclick/autoclick_unittest.cc
+++ ash/autoclick/autoclick_unittest.cc
@@ -269,7 +269,13 @@ TEST_F(AutoclickTest, UserInputCancelsAutoclick) {
   EXPECT_EQ(0u, events.size());
 }
 
-TEST_F(AutoclickTest, SynthesizedMouseMovesIgnored) {
+// Fails on official cros trunk build. See crbug.com/489896.
+#if defined(OFFICIAL_BUILD)
+#define MAYBE_SynthesizedMouseMovesIgnored DISABLED_SynthesizedMouseMovesIgnored
+#else
+#define MAYBE_SynthesizedMouseMovesIgnored SynthesizedMouseMovesIgnored
+#endif
+TEST_F(AutoclickTest, MAYBE_SynthesizedMouseMovesIgnored) {
   GetAutoclickController()->SetEnabled(true);
   std::vector<ui::MouseEvent> events;
   GetEventGenerator().MoveMouseTo(100, 100);
