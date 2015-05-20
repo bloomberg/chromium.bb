3f273f1fc1656df110ec7eeb9a48b7f439ab6674
Mike Frysinger <vapier@chromium.org>
2015-05-20 06:48:14 -0400

    gs: update to 4.12
    BUG=None
    TEST=`./cbuildbot/run_tests` passes
    TEST=`./gs_unittest --network` passes
    
    Change-Id: Ia6c1afd7cc4f6b1244b4b583b119df8dc3c626ff
    Reviewed-on: https://chromium-review.googlesource.com/272710
    Trybot-Ready: Mike Frysinger <vapier@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>
    Commit-Queue: Mike Frysinger <vapier@chromium.org>
    Tested-by: Mike Frysinger <vapier@chromium.org>

--- lib/gs.py
+++ lib/gs.py
@@ -253,7 +253,7 @@ class GSContext(object):
   # (1*sleep) the first time, then (2*sleep), continuing via attempt * sleep.
   DEFAULT_SLEEP_TIME = 60
 
-  GSUTIL_VERSION = '4.11'
+  GSUTIL_VERSION = '4.12'
   GSUTIL_TAR = 'gsutil_%s.tar.gz' % GSUTIL_VERSION
   GSUTIL_URL = (PUBLIC_BASE_HTTPS_URL +
                 'chromeos-mirror/gentoo/distfiles/%s' % GSUTIL_TAR)
