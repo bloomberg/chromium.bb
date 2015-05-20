ef1e95477f21894cc12c8adf19a5a5d637e3a8ff
cevans@chromium.org <cevans@chromium.org>
2015-05-20 06:32:48 +0000

    Remove Windows-specific crash code now that we've fixed Windows stack traces.
    BUG=449349
    
    Review URL: https://codereview.chromium.org/1150493002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195589 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 7718ba71e092a93de10189bb6f7f4218a13db0b3

--- third_party/WebKit/Source/wtf/PartitionAlloc.cpp
+++ third_party/WebKit/Source/wtf/PartitionAlloc.cpp
@@ -301,18 +301,6 @@ bool partitionAllocGenericShutdown(PartitionRootGeneric* root)
 #if !CPU(64BIT)
 static NEVER_INLINE void partitionOutOfMemoryWithLotsOfUncommitedPages()
 {
-#if OS(WIN)
-    // Crash at a special address (0x9b)
-    // to be easily distinguished on crash reports.
-    // This is because crash stack traces are inaccurate on Windows and
-    // partitionOutOfMemoryWithLotsOfUncommitedPages might be not included
-    // in the stack traces.
-    reinterpret_cast<void(*)()>(0x9b)();
-#endif
-
-    // On non-Windows environment, IMMEDIATE_CRASH is sufficient
-    // because partitionOutOfMemoryWithLotsOfUncommitedPages will appear
-    // in crash stack traces.
     IMMEDIATE_CRASH();
 }
 #endif
