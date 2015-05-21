9eceecdfd2fe3c3914bb12583815c5fdab2114be
Aviv Keshet <akeshet@chromium.org>
2015-05-20 17:23:26 -0700

    cidb: log a warning messing on query retries
    BUG=chromium:488737
    TEST=None
    
    Change-Id: Iecd79a6541aa27abf220286bc6907520c97d67ef
    Reviewed-on: https://chromium-review.googlesource.com/272540
    Trybot-Ready: Aviv Keshet <akeshet@chromium.org>
    Tested-by: Aviv Keshet <akeshet@chromium.org>
    Reviewed-by: Prathmesh Prabhu <pprabhu@chromium.org>

--- lib/cidb.py
+++ lib/cidb.py
@@ -60,6 +60,8 @@ def _IsRetryableException(e):
   if isinstance(e, sqlalchemy.exc.OperationalError):
     error_code = e.orig.args[0]
     if error_code in _RETRYABLE_OPERATIONAL_ERROR_CODES:
+      logging.warning('Encountered retryable cidb exception %s, retrying....',
+                      e)
       return True
 
   return False
