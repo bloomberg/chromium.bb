bryceharrington/weston

tests: Add client helper routines for output and reference filenames

tests/weston-test-client-helper.h
@@ -146,4 +146,10 @@ void
 expect_protocol_error(struct client *client,
 		      const struct wl_interface *intf, uint32_t code);
 
+char*
+screenshot_output_filename(const char* basename, uint32_t seq);
+
+char*
+screenshot_reference_filename(const char* basename, uint32_t seq);
+
 #endif

bloomberg/chromium.bb

Signed-off-by: Bryce Harrington <bryce@osg.samsung.com>
Reviewed-By: Derek Foreman <derekf@osg.samsung.com>

tests/weston-test-client-helper.h
@@ -180,4 +180,10 @@ void
 expect_protocol_error(struct client *client,
 		      const struct wl_interface *intf, uint32_t code);
 
+char*
+screenshot_output_filename(const char *basename, uint32_t seq);
+
+char*
+screenshot_reference_filename(const char *basename, uint32_t seq);
+
 #endif