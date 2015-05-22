0421425dadc84e9c2632b6136a1377cd15928030
fmalita <fmalita@chromium.org>
2015-05-21 17:54:58 -0700

    Remove SK_LEGACY_STROKE_CURVES
    BUG=490269
    TBR=caryclark@google.com,reed@google.com
    
    Review URL: https://codereview.chromium.org/954943005
    
    Cr-Commit-Position: refs/heads/master@{#331038}
    
    Former-commit-id: 2a3211d56b0395c3881d8303b43d39a59391fe9d

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -277,10 +277,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_INT_COLORMATRIX
 #endif
 
-#ifndef    SK_LEGACY_STROKE_CURVES
-#   define SK_LEGACY_STROKE_CURVES
-#endif
-
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
