6d463e05e976ac7505b3edf7bd6e1e0142a748d1
sigbjornf@opera.com <sigbjornf@opera.com>
2015-05-22 18:21:23 +0000

    Adjust FrameFetchContext clearing logic when clearing HTMLImportsController
    Have the clearing of a Document's HTMLImportsController only clear its
    FrameFetchContext if there's no DocumentLoader attached. If a loader is
    attached, it is possibly shared amongst multiple Documents and it is
    responsible for clearing the fetch context.
    
    This mirrors the condition used during detach().
    
    R=dcheng
    BUG=491051
    
    Review URL: https://codereview.chromium.org/1156663003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195801 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 06342180bfe05f252e9b552fe42c094b3b895c14

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -820,7 +820,7 @@ void Document::setImportsController(HTMLImportsController* controller)
 {
     ASSERT(!m_importsController || !controller);
     m_importsController = controller;
-    if (!m_importsController)
+    if (!m_importsController && !loader())
         m_fetcher->clearContext();
 }
 
