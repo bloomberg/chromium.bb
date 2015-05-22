0af30adb45811a416ccd821ec31c386fc7818450
wfh <wfh@chromium.org>
2015-05-22 14:07:04 -0700

    Do not attempt to open a channel to a plugin in Plugin Data Remover if there are no plugins available.
    BUG=485886
    
    Review URL: https://codereview.chromium.org/1144353003
    
    Cr-Commit-Position: refs/heads/master@{#331168}
    
    Former-commit-id: 2327c7044eeabc2e70700ff7f752e4b2e2978657

--- content/browser/plugin_data_remover_impl.cc
+++ content/browser/plugin_data_remover_impl.cc
@@ -91,9 +91,14 @@ class PluginDataRemoverImpl::Context
     std::vector<WebPluginInfo> plugins;
     plugin_service->GetPluginInfoArray(
         GURL(), mime_type, false, &plugins, NULL);
-    base::FilePath plugin_path;
-    if (!plugins.empty())  // May be empty for some tests.
-      plugin_path = plugins[0].path;
+
+    if (plugins.empty()) {
+      // May be empty for some tests and on the CrOS login OOBE screen.
+      event_->Signal();
+      return;
+    }
+
+    base::FilePath plugin_path = plugins[0].path;
 
     DCHECK_CURRENTLY_ON(BrowserThread::IO);
     remove_start_time_ = base::Time::Now();
