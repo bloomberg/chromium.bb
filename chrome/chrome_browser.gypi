bloomberg/chromium.bb

commit c1d073b6778795ee706868f43fb3b430ef0e809e

Offer to download the Software removal tool when the sw_reporter component found something that it can handle.
For chrome/app/chrome_command_ids.h
TBR=cpu@chromium.org

BUG=416582

Review URL: https://codereview.chromium.org/599653002

Cr-Commit-Position: refs/heads/master@{#296594}

Former-commit-id: 2b3b655281789477955499659b5b06eba5faacfc

chrome/chrome_browser.gypi
@@ -767,7 +767,7 @@
       'browser/net/quota_policy_channel_id_store.h',
       'browser/net/referrer.cc',
       'browser/net/referrer.h',
-      'browser/net/resource_prefetch_predictor_observer.cc',  
+      'browser/net/resource_prefetch_predictor_observer.cc',
       'browser/net/resource_prefetch_predictor_observer.h',
       'browser/net/safe_search_util.cc',
       'browser/net/safe_search_util.h',
@@ -840,19 +840,19 @@
       'browser/predictors/predictor_database_factory.h',
       'browser/predictors/predictor_table_base.cc',
       'browser/predictors/predictor_table_base.h',
-      'browser/predictors/resource_prefetch_common.cc', 
-      'browser/predictors/resource_prefetch_common.h',  
-      'browser/predictors/resource_prefetch_predictor.cc',  
-      'browser/predictors/resource_prefetch_predictor.h', 
-      'browser/predictors/resource_prefetch_predictor_factory.cc',  
-      'browser/predictors/resource_prefetch_predictor_factory.h', 
-      'browser/predictors/resource_prefetch_predictor_tab_helper.cc', 
-      'browser/predictors/resource_prefetch_predictor_tab_helper.h',  
-      'browser/predictors/resource_prefetch_predictor_tables.cc', 
-      'browser/predictors/resource_prefetch_predictor_tables.h',  
-      'browser/predictors/resource_prefetcher.cc',  
-      'browser/predictors/resource_prefetcher.h', 
-      'browser/predictors/resource_prefetcher_manager.cc',  
+      'browser/predictors/resource_prefetch_common.cc',
+      'browser/predictors/resource_prefetch_common.h',
+      'browser/predictors/resource_prefetch_predictor.cc',
+      'browser/predictors/resource_prefetch_predictor.h',
+      'browser/predictors/resource_prefetch_predictor_factory.cc',
+      'browser/predictors/resource_prefetch_predictor_factory.h',
+      'browser/predictors/resource_prefetch_predictor_tab_helper.cc',
+      'browser/predictors/resource_prefetch_predictor_tab_helper.h',
+      'browser/predictors/resource_prefetch_predictor_tables.cc',
+      'browser/predictors/resource_prefetch_predictor_tables.h',
+      'browser/predictors/resource_prefetcher.cc',
+      'browser/predictors/resource_prefetcher.h',
+      'browser/predictors/resource_prefetcher_manager.cc',
       'browser/predictors/resource_prefetcher_manager.h',
       'browser/pref_service_flags_storage.cc',
       'browser/pref_service_flags_storage.h',
@@ -1054,6 +1054,8 @@
       'browser/resources_util.h',
       'browser/safe_browsing/safe_browsing_tab_observer.cc',
       'browser/safe_browsing/safe_browsing_tab_observer.h',
+      'browser/safe_browsing/srt_global_error_win.cc',
+      'browser/safe_browsing/srt_global_error_win.h',
       'browser/search/contextual_search_promo_source_android.cc',
       'browser/search/contextual_search_promo_source_android.h',
       'browser/search/iframe_source.cc',
@@ -1069,7 +1071,7 @@
       'browser/search/local_ntp_source.cc',
       'browser/search/local_ntp_source.h',
       'browser/search/most_visited_iframe_source.cc',
-      'browser/search/most_visited_iframe_source.h',
+      'browser/searuch/most_visited_iframe_source.h',
       'browser/search/search.cc',
       'browser/search/search.h',
       'browser/search/suggestions/image_manager_impl.cc',

chrome/app/chrome_command_ids.h
@@ -213,6 +213,7 @@
 #define IDC_DISTILL_PAGE                40244
 #define IDC_HELP_MENU                   40245
 #define IDC_EXTENSIONS_OVERFLOW_MENU    40246
+#define IDC_SHOW_SRT_BUBBLE             40247
 
 // Spell-check
 // Insert any additional suggestions before _LAST; these have to be consecutive.

chrome/app/chromium_strings.grd
@@ -905,6 +905,25 @@ Signing in anyway will merge Chromium information like bookmarks, history, and o
         </message>
       </if>
 
+      <!-- SRT bubble messages -->
+      <if expr="is_win">
+        <if expr="use_titlecase">
+          <message name="IDS_SRT_MENU_ITEM" desc="In Title Case: Text for the Chrome menu option replacing Update required.">
+            Chromium Behaving Strangely?
+          </message>
+        </if>
+        <if expr="not use_titlecase">
+          <message name="IDS_SRT_MENU_ITEM" desc="Text for the Chrome menu option replacing Update required.">
+            Chromium behaving strangely?
+          </message>
+        </if>
+        <message name="IDS_SRT_BUBBLE_TITLE" desc="Text for the title of the software removal tool bubble view.">
+            Chromium behaving strangely?
+        </message>
+        <message name="IDS_SRT_BUBBLE_TEXT" desc="Text for the software removal tool bubble view full description.">
+          Chromium detected that your browser settings may have been changed by a program on your computer without your knowledge. You can use  Google's Software Removal Tool to remove that program.
+        </message>
+      </if>
       <!-- Sync/sign-in error messages -->
       <if expr="not chromeos">
         <message name="IDS_SYNC_PASSPHRASE_ERROR_BUBBLE_VIEW_MESSAGE" desc="Message in the sync error bubble view when the user needs to update their sync passphrase.">

chrome/app/generated_resources.grd
@@ -8065,6 +8065,11 @@ Keep your key file in a safe place. You will need it to create new versions of y
         Help make Google Chrome better by reporting the current settings
       </message>
 
+      <!-- SRT bubble messages -->
+      <message name="IDS_SRT_BUBBLE_DOWNLOAD_BUTTON_TEXT" desc="Download button of the software removal tool bubble">
+        Get the Software Removal Tool
+      </message>
+
       <!-- Upgrade bubble messages -->
       <message name="IDS_REENABLE_UPDATES" desc="Text for the button the user clicks to re-enable automatic updates.">
         Enable autoupdate

chrome/app/google_chrome_strings.grd
@@ -829,6 +829,25 @@ Signing in anyway will merge Chrome information like bookmarks, history, and oth
         </message>
       </if>
 
+      <!-- SRT bubble messages -->
+      <if expr="is_win">
+        <if expr="use_titlecase">
+          <message name="IDS_SRT_MENU_ITEM" desc="In Title Case: Text for the Chrome menu option replacing Update required.">
+            Chrome Behaving Strangely?
+          </message>
+        </if>
+        <if expr="not use_titlecase">
+          <message name="IDS_SRT_MENU_ITEM" desc="Text for the Chrome menu option replacing Update required.">
+            Chrome behaving strangely?
+          </message>
+        </if>
+        <message name="IDS_SRT_BUBBLE_TITLE" desc="Text for the title of the software removal tool bubble view.">
+            Chrome behaving strangely?
+        </message>
+        <message name="IDS_SRT_BUBBLE_TEXT" desc="Text for the software removal tool bubble view full description.">
+          Chrome detected that your browser settings may have been changed by a program on your computer without your knowledge. You can use Google's Software Removal Tool to remove that program.
+        </message>
+      </if>
       <!-- Sync/sign-in error messages -->
       <if expr="not chromeos">
         <message name="IDS_SYNC_PASSPHRASE_ERROR_BUBBLE_VIEW_MESSAGE" desc="Message in the sync error bubble view when the user needs to update their sync passphrase.">

chrome/browser/component_updater/sw_reporter_installer_win.cc
@@ -28,12 +28,18 @@
 #include "base/win/registry.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/safe_browsing/srt_global_error_win.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/global_error/global_error_service.h"
+#include "chrome/browser/ui/global_error/global_error_service_factory.h"
 #include "chrome/common/pref_names.h"
 #include "components/component_updater/component_updater_paths.h"
 #include "components/component_updater/component_updater_service.h"
 #include "components/component_updater/component_updater_utils.h"
 #include "components/component_updater/default_component_installer.h"
 #include "components/component_updater/pref_names.h"
+#include "components/pref_registry/pref_registry_syncable.h"
 #include "content/public/browser/browser_thread.h"
 
 using content::BrowserThread;
@@ -77,16 +83,98 @@ const wchar_t kSoftwareRemovalToolRegistryKey[] =
     L"Software\\Google\\Software Removal Tool";
 const wchar_t kExitCodeRegistryValueName[] = L"ExitCode";
 
+// Exit codes that identify that a cleanup is needed.
+const int kCleanupNeeded = 0;
+const int kPostRebootCleanupNeeded = 4;
+
 void ReportUmaStep(SwReporterUmaValue value) {
   UMA_HISTOGRAM_ENUMERATION("SoftwareReporter.Step", value, SW_REPORTER_MAX);
+}
+
+void ReportUmaVersion(const base::Version& version) {
+  DCHECK(!version.components().empty());
+  UMA_HISTOGRAM_SPARSE_SLOWLY("SoftwareReporter.MinorVersion",
+                              version.components().back());
+  // The major version uses the 1st component value (when there is more than
+  // one, since the last one is always the minor version) as a hi word in a
+  // double word. The low word is either the second component (when there are
+  // only three) or the 3rd one if there are at least 4. E.g., for W.X.Y.Z, we
+  // ignore X, and Z is the minor version. We compute the major version with W
+  // as the hi word, and Y as the low word. For X.Y.Z, we use X and Y as hi and
+  // low words, and if we would have Y.Z we would use Y as the hi word and 0 as
+  // the low word. major version is 0 if the version only has one component.
+  uint32_t major_version = 0;
+  if (version.components().size() > 1)
+    major_version = 0x10000 * version.components()[0];
+  if (version.components().size() < 4 && version.components().size() > 2)
+    major_version += version.components()[1];
+  else if (version.components().size() > 3)
+    major_version += version.components()[2];
+  UMA_HISTOGRAM_SPARSE_SLOWLY("SoftwareReporter.MajorVersion", major_version);
 }
 
 // This function is called on the UI thread to report the SwReporter exit code
 // and then clear it from the registry as well as clear the execution state
 // from the local state. This could be called from an interruptible worker
-// thread so should be resilient to unexpected shutdown.
-void ReportAndClearExitCode(int exit_code) {
+// thread so should be resilient to unexpected shutdown. |version| is provided
+// so the kSwReporterPromptVersion prefs can be set.
+void ReportAndClearExitCode(int exit_code, const std::string& version) {
   UMA_HISTOGRAM_SPARSE_SLOWLY("SoftwareReporter.ExitCode", exit_code);
+  if (g_browser_process && g_browser_process->local_state()) {
+    g_browser_process->local_state()->SetInteger(prefs::kSwReporterLastExitCode,
+                                                 exit_code);
+  }
+
+  if (exit_code == kPostRebootCleanupNeeded || exit_code == kCleanupNeeded) {
+    // Find the last active browser, which may be NULL, in which case we won't
+    // show the prompt this time and will wait until the next run of the
+    // reporter. We can't use other ways of finding a browser because we don't
+    // have a profile.
+    chrome::HostDesktopType desktop_type = chrome::GetActiveDesktop();
+    Browser* browser = chrome::FindLastActiveWithHostDesktopType(desktop_type);
+    if (browser) {
+      Profile* profile = browser->profile();
+      DCHECK(profile);
+      // Now that we have a profile, make sure we have a tabbed browser since we
+      // need to anchor the bubble to the toolbar's wrench menu. Create one if
+      // none exist already.
+      if (browser->type() != Browser::TYPE_TABBED) {
+        browser = chrome::FindTabbedBrowser(profile, false, desktop_type);
+        if (!browser)
+          browser = new Browser(Browser::CreateParams(profile, desktop_type));
+      }
+      const std::string prompt_version =
+          profile->GetPrefs()->GetString(prefs::kSwReporterPromptVersion);
+      // Don't show the prompt again if it's been shown before.
+      if (prompt_version.empty()) {
+        profile->GetPrefs()->SetString(prefs::kSwReporterPromptVersion,
+                                       version);
+        profile->GetPrefs()->SetInteger(prefs::kSwReporterPromptReason,
+                                        exit_code);
+        GlobalErrorService* global_error_service =
+            GlobalErrorServiceFactory::GetForProfile(profile);
+        SRTGlobalError* global_error = new SRTGlobalError(global_error_service);
+        // |global_error_service| takes ownership of |global_error| and keeps it
+        // alive until RemoveGlobalError() is called, and even then, the object
+        // is not destroyed, the caller of RemoveGlobalError is responsible to
+        // destroy it, and in the case of the SRTGlobalError, it deletes itself
+        // but only after the bubble has been interacted with.
+        global_error_service->AddGlobalError(global_error);
+
+        // Do not try to show bubble if another GlobalError is already showing
+        // one. The bubble will be shown once the others have been dismissed.
+        const GlobalErrorService::GlobalErrorList& global_errors(
+            global_error_service->errors());
+        GlobalErrorService::GlobalErrorList::const_iterator it;
+        for (it = global_errors.begin(); it != global_errors.end(); ++it) {
+          if ((*it)->GetBubbleView())
+            break;
+        }
+        if (it == global_errors.end())
+          global_error->ShowBubbleView(browser);
+      }
+    }
+  }
 
   base::win::RegKey srt_key(
       HKEY_CURRENT_USER, kSoftwareRemovalToolRegistryKey, KEY_WRITE);
@@ -97,7 +185,8 @@ void ReportAndClearExitCode(int exit_code) {
 // wait for termination to collect its exit code. This task could be interrupted
 // by a shutdown at anytime, so it shouldn't depend on anything external that
 // could be shutdown beforehand.
-void LaunchAndWaitForExit(const base::FilePath& exe_path) {
+void LaunchAndWaitForExit(const base::FilePath& exe_path,
+                          const std::string& version) {
   const base::CommandLine reporter_command_line(exe_path);
   base::ProcessHandle scan_reporter_process = base::kNullProcessHandle;
   if (!base::LaunchProcess(reporter_command_line,
@@ -114,16 +203,10 @@ void LaunchAndWaitForExit(const base::FilePath& exe_path) {
   base::CloseProcessHandle(scan_reporter_process);
   scan_reporter_process = base::kNullProcessHandle;
   // It's OK if this doesn't complete, the work will continue on next startup.
-  BrowserThread::PostTask(BrowserThread::UI,
-                          FROM_HERE,
-                          base::Bind(&ReportAndClearExitCode, exit_code));
-}
-
-void ExecuteReporter(const base::FilePath& install_dir) {
-  base::WorkerPool::PostTask(
+  BrowserThread::PostTask(
+      BrowserThread::UI,
       FROM_HERE,
-      base::Bind(&LaunchAndWaitForExit, install_dir.Append(kSwReporterExeName)),
-      true);
+      base::Bind(&ReportAndClearExitCode, exit_code, version));
 }
 
 class SwReporterInstallerTraits : public ComponentInstallerTraits {
@@ -147,6 +230,7 @@ class SwReporterInstallerTraits : public ComponentInstallerTraits {
                               const base::FilePath& install_dir,
                               scoped_ptr<base::DictionaryValue> manifest) {
     DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+    ReportUmaVersion(version);
 
     wcsncpy_s(version_dir_,
               _MAX_PATH,
@@ -155,6 +239,7 @@ class SwReporterInstallerTraits : public ComponentInstallerTraits {
 
     // A previous run may have results in the registry, so check and report
     // them if present.
+    std::string version_string(version.GetString());
     base::win::RegKey srt_key(
         HKEY_CURRENT_USER, kSoftwareRemovalToolRegistryKey, KEY_READ);
     DWORD exit_code;
@@ -162,7 +247,7 @@ class SwReporterInstallerTraits : public ComponentInstallerTraits {
         srt_key.ReadValueDW(kExitCodeRegistryValueName, &exit_code) ==
             ERROR_SUCCESS) {
       ReportUmaStep(SW_REPORTER_REGISTRY_EXIT_CODE);
-      ReportAndClearExitCode(exit_code);
+      ReportAndClearExitCode(exit_code, version_string);
     }
 
     // If we can't access local state, we can't see when we last ran, so
@@ -181,7 +266,12 @@ class SwReporterInstallerTraits : public ComponentInstallerTraits {
           prefs::kSwReporterLastTimeTriggered,
           base::Time::Now().ToInternalValue());
 
-      ExecuteReporter(install_dir);
+      base::WorkerPool::PostTask(
+          FROM_HERE,
+          base::Bind(&LaunchAndWaitForExit,
+                     install_dir.Append(kSwReporterExeName),
+                     version_string),
+          true);
     }
   }
 
@@ -224,10 +314,6 @@ wchar_t SwReporterInstallerTraits::version_dir_[] = {};
 
 void RegisterSwReporterComponent(ComponentUpdateService* cus,
                                  PrefService* prefs) {
-  // The Sw reporter shouldn't run if the user isn't reporting metrics.
-  if (!ChromeMetricsServiceAccessor::IsMetricsReportingEnabled())
-    return;
-
   // Install the component.
   scoped_ptr<ComponentInstallerTraits> traits(
       new SwReporterInstallerTraits(prefs));
@@ -239,6 +325,20 @@ void RegisterSwReporterComponent(ComponentUpdateService* cus,
 
 void RegisterPrefsForSwReporter(PrefRegistrySimple* registry) {
   registry->RegisterInt64Pref(prefs::kSwReporterLastTimeTriggered, 0);
+  registry->RegisterIntegerPref(prefs::kSwReporterLastExitCode, -1);
+}
+
+void RegisterProfilePrefsForSwReporter(
+    user_prefs::PrefRegistrySyncable* registry) {
+  registry->RegisterIntegerPref(
+      prefs::kSwReporterPromptReason,
+      -1,
+      user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
+
+  registry->RegisterStringPref(
+      prefs::kSwReporterPromptVersion,
+      "",
+      user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
 }
 
 }  // namespace component_updater

chrome/browser/component_updater/sw_reporter_installer_win.h
@@ -8,6 +8,10 @@
 class PrefRegistrySimple;
 class PrefService;
 
+namespace user_prefs {
+class PrefRegistrySyncable;
+}
+
 namespace component_updater {
 
 class ComponentUpdateService;
@@ -17,8 +21,12 @@ class ComponentUpdateService;
 void RegisterSwReporterComponent(ComponentUpdateService* cus,
                                  PrefService* prefs);
 
-// Register user preferences related to the SwReporter.
+// Register local state preferences related to the SwReporter.
 void RegisterPrefsForSwReporter(PrefRegistrySimple* registry);
+
+// Register profile preferences related to the SwReporter.
+void RegisterProfilePrefsForSwReporter(
+    user_prefs::PrefRegistrySyncable* registry);
 
 }  // namespace component_updater
 

chrome/browser/prefs/browser_prefs.cc
@@ -484,6 +484,7 @@ void RegisterProfilePrefs(user_prefs::PrefRegistrySyncable* registry) {
 #endif
 
 #if defined(OS_WIN)
+  component_updater::RegisterProfilePrefsForSwReporter(registry);
   NetworkProfileBubble::RegisterProfilePrefs(registry);
 #endif
 

commit 8f93d530a2600db119d3222b7f422d23703b2627

Use 'search' instead of 'searuch'.
This typo was introduced with
c1d073b6778795ee706868f43fb3b430ef0e809e [formerly 2b3b655281789477955499659b5b06eba5faacfc]
https://codereview.chromium.org/599653002
and has survived several attempts to fix it.

Review URL: https://codereview.chromium.org/1148953011

Cr-Commit-Position: refs/heads/master@{#331420}

Former-commit-id: 0bc8b58d4c25353d7d8bce3d9ccdda36e2701ebd

chrome/chrome_browser.gypi
@@ -677,6 +677,7 @@
       'browser/search/instant_service_observer.cc',
       'browser/search/instant_service_observer.h',
       'browser/search/most_visited_iframe_source.cc',
+      'browser/search/most_visited_iframe_source.h',
       'browser/search/search.cc',
       'browser/search/search.h',
       'browser/search/suggestions/image_fetcher_impl.cc',
@@ -687,7 +688,6 @@
       'browser/search/suggestions/suggestions_source.h',
       'browser/search/thumbnail_source.cc',
       'browser/search/thumbnail_source.h',
-      'browser/searuch/most_visited_iframe_source.h',
       'browser/shell_integration.cc',
       'browser/shell_integration.h',
       'browser/shell_integration_android.cc',