31ae9621fa9936f639916ff3cf40bd6cc6fe380e
mnaganov <mnaganov@chromium.org>
2015-05-27 08:44:58 -0700

    Add prune paths from Android WebView to licenses.py
    BUG=486158
    
    Review URL: https://codereview.chromium.org/1153763006
    
    Cr-Commit-Position: refs/heads/master@{#331577}
    
    Former-commit-id: 8754c6f9c2a85d1f2da5890a4ea33f4bc09f6911

--- tools/licenses.py
+++ tools/licenses.py
@@ -28,6 +28,9 @@ PRUNE_PATHS = set([
     # Placeholder directory only, not third-party code.
     os.path.join('third_party','adobe'),
 
+    # Apache 2.0 license. See crbug.com/140478
+    os.path.join('third_party','bidichecker'),
+
     # Build files only, not third-party code.
     os.path.join('third_party','widevine'),
 
@@ -39,6 +42,7 @@ PRUNE_PATHS = set([
     os.path.join('third_party','bison'),
     os.path.join('third_party','blanketjs'),
     os.path.join('third_party','cygwin'),
+    os.path.join('third_party','gles2_conform'),
     os.path.join('third_party','gnu_binutils'),
     os.path.join('third_party','gold'),
     os.path.join('third_party','gperf'),
@@ -68,11 +72,18 @@ PRUNE_PATHS = set([
     os.path.join('third_party', 'pcre'),
     os.path.join('third_party', 'psutils'),
     os.path.join('third_party', 'sawbuck'),
+    # See crbug.com/350472
+    os.path.join('chrome', 'browser', 'resources', 'chromeos', 'quickoffice'),
+    # Chrome for Android proprietary code.
+    os.path.join('clank'),
 
     # Redistribution does not require attribution in documentation.
     os.path.join('third_party','directxsdk'),
     os.path.join('third_party','platformsdk_win2008_6_1'),
     os.path.join('third_party','platformsdk_win7'),
+
+    # For testing only, presents on some bots.
+    os.path.join('isolate_deps_dir'),
 ])
 
 # Directories we don't scan through.
