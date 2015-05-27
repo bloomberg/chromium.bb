4d2d0c3079755c8a2494b8f8cd7bdf74bbf90e9d
skyostil <skyostil@chromium.org>
2015-05-27 07:25:23 -0700

    pylib: Add support for the new ChromePublic APK
    TEST=tools/profile_chrome_startup.py --browser chromium
    
    Review URL: https://codereview.chromium.org/1158913004
    
    Cr-Commit-Position: refs/heads/master@{#331570}
    
    Former-commit-id: c2ac21e8aeb804194f0d98bd5129982998f04cce

--- build/android/pylib/constants/__init__.py
+++ build/android/pylib/constants/__init__.py
@@ -70,6 +70,12 @@ PACKAGE_INFO = {
         '/data/local/chrome-command-line',
         'chrome_devtools_remote',
         None),
+    'chromium': PackageInfo(
+        'org.chromium.chrome',
+        'com.google.android.apps.chrome.Main',
+        '/data/local/chrome-command-line',
+        'chrome_devtools_remote',
+        None),
     'legacy_browser': PackageInfo(
         'com.google.android.browser',
         'com.android.browser.BrowserActivity',
