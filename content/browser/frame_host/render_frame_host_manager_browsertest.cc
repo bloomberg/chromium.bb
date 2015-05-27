f1381aab07f889a540e6a9fdfbdd646b87c1f760
alexmos <alexmos@chromium.org>
2015-05-27 15:43:46 -0700

    Fix tests relying on post_message.html to null-check event.ports.
    https://codereview.chromium.org/1154853003 changed MessageEvent's
    ports to be null by default to better follow the spec
    (https://html.spec.whatwg.org/multipage/comms.html#dom-messageevent-ports),
    and our test didn't check for this.
    
    BUG=492649
    
    Review URL: https://codereview.chromium.org/1150313006
    
    Cr-Commit-Position: refs/heads/master@{#331677}
    
    Former-commit-id: 5e7426a75e5884e8140d4240cce853dea0c2619e

--- content/browser/frame_host/render_frame_host_manager_browsertest.cc
+++ content/browser/frame_host/render_frame_host_manager_browsertest.cc
@@ -546,13 +546,6 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest, DisownSubframeOpener) {
                             "window.frames[0].opener = null;"));
 }
 
-// This test currently fails on Android, see http://crbug.com/492649.
-#if defined(OS_ANDROID)
-#define MAYBE_SupportCrossProcessPostMessage \
-    DISABLED_SupportCrossProcessPostMessage
-#else
-#define MAYBE_SupportCrossProcessPostMessage SupportCrossProcessPostMessage
-#endif
 // Test for crbug.com/99202.  PostMessage calls should still work after
 // navigating the source and target windows to different sites.
 // Specifically:
@@ -563,7 +556,7 @@ IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest, DisownSubframeOpener) {
 // 5) Post a message from "foo" to a subframe of opener, which replies back.
 // 6) Post a message from _blank to a subframe of "foo".
 IN_PROC_BROWSER_TEST_F(RenderFrameHostManagerTest,
-                       MAYBE_SupportCrossProcessPostMessage) {
+                       SupportCrossProcessPostMessage) {
   StartServer();
 
   // Load a page with links that open in a new window.
