ae4b18d404c40e4e0ceb4b3e9aa942d4f4dfc2c9
esprehn@chromium.org <esprehn@chromium.org>
2015-05-27 00:26:10 +0000

    Remove dead code from updateLayoutAndStyleForPainting.
    Calling setFrameRect with the frameRect() is a no-op.
    
    Review URL: https://codereview.chromium.org/1159873002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@195941 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 20136205cf97a7a841b909e9d014da96a2e2234b

--- third_party/WebKit/Source/core/page/PageAnimator.cpp
+++ third_party/WebKit/Source/core/page/PageAnimator.cpp
@@ -96,14 +96,6 @@ void PageAnimator::updateLayoutAndStyleForPainting(LocalFrame* rootFrame)
 
     TemporaryChange<bool> servicing(m_updatingLayoutAndStyleForPainting, true);
 
-    // In order for our child HWNDs (NativeWindowWidgets) to update properly,
-    // they need to be told that we are updating the screen. The problem is that
-    // the native widgets need to recalculate their clip region and not overlap
-    // any of our non-native widgets. To force the resizing, call
-    // setFrameRect(). This will be a quick operation for most frames, but the
-    // NativeWindowWidgets will update a proper clipping region.
-    view->setFrameRect(view->frameRect());
-
     // setFrameRect may have the side-effect of causing existing page layout to
     // be invalidated, so layout needs to be called last.
     view->updateLayoutAndStyleForPainting();
