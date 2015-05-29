3c10f03e0b5d5e5016368e2ed8e32ed50a920299
aruslan <aruslan@chromium.org>
2015-05-29 16:05:22 -0700

    Postpone sync till settings are paused/quit
    BUG=493834
    
    Review URL: https://codereview.chromium.org/1156063004
    
    Cr-Commit-Position: refs/heads/master@{#332071}
    
    Former-commit-id: 0230d1baac5df9357d53b2436f859cb4a8b9c7ac

--- chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunSignInProcessor.java
+++ chrome/android/java/src/org/chromium/chrome/browser/firstrun/FirstRunSignInProcessor.java
@@ -167,12 +167,16 @@ public class FirstRunSignInProcessor {
             return;
         }
 
+        final boolean delaySync = getFirstRunFlowSignInSetupSync(mActivity);
+        final int delaySyncType = delaySync
+                ? SigninManager.SIGNIN_SYNC_SETUP_IN_PROGRESS
+                : SigninManager.SIGNIN_SYNC_IMMEDIATELY;
         signinManager.signInToSelectedAccount(mActivity, account,
-                mSignInType, SigninManager.SIGNIN_SYNC_IMMEDIATELY, mShowSignInNotification,
+                mSignInType, delaySyncType, mShowSignInNotification,
                 new SignInFlowObserver() {
                     private void completeSignIn() {
                         // Show sync settings if user pressed the "Settings" button.
-                        if (getFirstRunFlowSignInSetupSync(mActivity)) {
+                        if (delaySync) {
                             openSyncSettings(
                                     ChromeSigninController.get(mActivity).getSignedInAccountName());
                         }
