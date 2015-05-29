570124ffdd45090b6e28ea16c52c3834fe084eb1
jbroman <jbroman@chromium.org>
2015-05-29 10:33:52 -0700

    ui: Change kUIEnableSlimmingPaint to kUIDisableSlimmingPaint in the header, too.
    This was changed in the source file about two weeks ago:
      https://chromium.googlesource.com/chromium/src/+/6decaaddf3 [formerly 7d4e7af2da]
    
    The header file was never changed. This causes no build errors because the
    symbol is unused, but it still seems better to have it corrected.
    
    Review URL: https://codereview.chromium.org/1158393007
    
    Cr-Commit-Position: refs/heads/master@{#332001}
    
    Former-commit-id: 35411be880615e548ac2df01befc462aa8ae4821

--- ui/compositor/compositor_switches.h
+++ ui/compositor/compositor_switches.h
@@ -11,10 +11,10 @@ namespace switches {
 
 COMPOSITOR_EXPORT extern const char kEnableHardwareOverlays[];
 COMPOSITOR_EXPORT extern const char kEnablePixelOutputInTests[];
+COMPOSITOR_EXPORT extern const char kUIDisableSlimmingPaint[];
 COMPOSITOR_EXPORT extern const char kUIDisableThreadedCompositing[];
 COMPOSITOR_EXPORT extern const char kUIEnableCompositorAnimationTimelines[];
 COMPOSITOR_EXPORT extern const char kUIEnableImplSidePainting[];
-COMPOSITOR_EXPORT extern const char kUIEnableSlimmingPaint[];
 COMPOSITOR_EXPORT extern const char kUIEnableZeroCopy[];
 COMPOSITOR_EXPORT extern const char kUIShowPaintRects[];
 
