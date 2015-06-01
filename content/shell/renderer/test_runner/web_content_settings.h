ab0334b26c9bc4302b699a9909bdd9f7e2a694e5
jochen <jochen@chromium.org>
2015-06-01 03:04:56 -0700

    Fix include guard for web_content_settings.h
    TBR=mlamouri@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1163813002
    
    Cr-Commit-Position: refs/heads/master@{#332175}
    
    Former-commit-id: 90c6e3a807316f21faab6876f24f7b385be57e1c

--- content/shell/renderer/test_runner/web_content_settings.h
+++ content/shell/renderer/test_runner/web_content_settings.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_SHELL_RENDERER_TEST_RUNNER_WEB_PERMISSIONS_H_
-#define CONTENT_SHELL_RENDERER_TEST_RUNNER_WEB_PERMISSIONS_H_
+#ifndef CONTENT_SHELL_RENDERER_TEST_RUNNER_WEB_CONTENT_SETTINGS_H_
+#define CONTENT_SHELL_RENDERER_TEST_RUNNER_WEB_CONTENT_SETTINGS_H_
 
 #include "base/macros.h"
 #include "third_party/WebKit/public/web/WebContentSettingsClient.h"
@@ -64,4 +64,4 @@ class WebContentSettings : public blink::WebContentSettingsClient {
 
 }  // namespace content
 
-#endif  // CONTENT_SHELL_RENDERER_TEST_RUNNER_WEB_PERMISSIONS_H_
+#endif  // CONTENT_SHELL_RENDERER_TEST_RUNNER_WEB_CONTENT_SETTINGS_H_
