f884f299863683d8ce0b4374d1e98ed9a897345b
krstnmnlsn <krstnmnlsn@chromium.org>
2015-06-01 23:42:18 -0700

    Moving BluetoothDiscoveryFilter into its own file.
    Extracting the BluetoothDiscoveryFilter class from bluetooth_discovery_session.h and bluetooth_discovery_session.cc into its own files: bluetooth_discovery_filter.h and bluetooth_discovery_filter.cc.
    
    BUG=495305
    
    Review URL: https://codereview.chromium.org/1163843005
    
    Cr-Commit-Position: refs/heads/master@{#332351}
    
    Former-commit-id: 7f9715680150b2f0c3ae46a227075859c73042b2

--- device/bluetooth/bluetooth_discovery_session.h
+++ device/bluetooth/bluetooth_discovery_session.h
@@ -9,66 +9,10 @@
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "device/bluetooth/bluetooth_adapter.h"
+#include "device/bluetooth/bluetooth_discovery_filter.h"
 #include "device/bluetooth/bluetooth_export.h"
 
 namespace device {
-
-// used to keep discovery filter that might be Used to limit reported devices.
-class DEVICE_BLUETOOTH_EXPORT BluetoothDiscoveryFilter {
- public:
-  // Possible transports to use for scan filter.
-  enum Transport {
-    TRANSPORT_CLASSIC = 0x01,
-    TRANSPORT_LE = 0x02,
-    TRANSPORT_DUAL = (TRANSPORT_CLASSIC | TRANSPORT_LE)
-  };
-  using TransportMask = uint8_t;
-
-  BluetoothDiscoveryFilter(TransportMask transport);
-  ~BluetoothDiscoveryFilter();
-
-  // These getters return true when given field is set in filter, and copy this
-  // value to |out_*| parameter. If value is not set, returns false.
-  // Thes setters assign given value to proper filter field.
-  bool GetRSSI(int16_t* out_rssi) const;
-  void SetRSSI(int16_t rssi);
-  bool GetPathloss(uint16_t* out_pathloss) const;
-  void SetPathloss(uint16_t pathloss);
-
-  // Return and set transport field of this filter.
-  TransportMask GetTransport() const;
-  void SetTransport(TransportMask transport);
-
-  // Make |out_uuids| represent all uuids assigned to this filter.
-  void GetUUIDs(std::set<device::BluetoothUUID>& out_uuids) const;
-
-  // Add UUID to internal UUIDs filter. If UUIDs filter doesn't exist, it will
-  // be created.
-  void AddUUID(const device::BluetoothUUID& uuid);
-
-  // Copy content of |filter| and assigns it to this filter.
-  void CopyFrom(const BluetoothDiscoveryFilter& filter);
-
-  // Check if two filters are equal.
-  bool Equals(const BluetoothDiscoveryFilter& filter) const;
-
-  // Returns true if all fields in filter are empty
-  bool IsDefault() const;
-
-  // Returns result of merging two filters together. If at least one of the
-  // filters is NULL this will return an empty filter
-  static scoped_ptr<device::BluetoothDiscoveryFilter> Merge(
-      const device::BluetoothDiscoveryFilter* filter_a,
-      const device::BluetoothDiscoveryFilter* filter_b);
-
- private:
-  scoped_ptr<int16_t> rssi_;
-  scoped_ptr<uint16_t> pathloss_;
-  TransportMask transport_;
-  ScopedVector<device::BluetoothUUID> uuids_;
-
-  DISALLOW_COPY_AND_ASSIGN(BluetoothDiscoveryFilter);
-};
 
 // BluetoothDiscoverySession represents a current active or inactive device
 // discovery session. Instances of this class are obtained by calling
