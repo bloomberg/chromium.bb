ef831c5b2b31b4fde3d58e59d537c9d80d503c3b
kojii@chromium.org <kojii@chromium.org>
2015-06-03 07:35:44 +0000

    W3C test_importer.py includes 'support' and exclude 'tools' directories
    The test_importer.py does not copy HTML files without proper meta data. This
    eliminates HTML files to load into IFRAME for instances today. The test format
    guidelines defines that 'support' directory contains files to run tests,
    so we should copy all files in this directory regardless of file types.
    http://testthewebforward.org/docs/test-format-guidelines.html#support-files
    
    This change fixes 4 CSS Writing Modes tests.
    
    Also the guideline defines that 'tools' directory contains files not for a
    browser (e.g., a script for generating test files). This directory can be
    excluded.
    http://testthewebforward.org/docs/test-format-guidelines.html#tools
    
    This change eliminates the following file from the current file set:
      web-platform-tests/subresource-integrity/tools/generate_javascript.py
    and more large files (~300k) are coming to the CSS WG test repositry.
    
    BUG=492664
    
    Review URL: https://codereview.chromium.org/1167723002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196379 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 0fc8904772db4f938c57360ada9fd37ccd5da4c7

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_importer.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_importer.py
@@ -196,7 +196,14 @@ class TestImporter(object):
             reftests = 0
             jstests = 0
 
-            DIRS_TO_SKIP = ('.git', '.hg', 'test-plan')
+            # Files in 'tools' are not for browser testing (e.g., a script for generating test files).
+            # http://testthewebforward.org/docs/test-format-guidelines.html#tools
+            DIRS_TO_SKIP = ('.git', '.hg', 'test-plan', 'tools')
+
+            # Need to copy all files in 'support', including HTML without meta data.
+            # http://testthewebforward.org/docs/test-format-guidelines.html#support-files
+            DIRS_TO_INCLUDE = ('resources', 'support')
+
             if dirs:
                 for d in DIRS_TO_SKIP:
                     if d in dirs:
@@ -240,7 +247,7 @@ class TestImporter(object):
                     copy_list.append({'src': fullpath, 'dest': filename})
                     continue
 
-                if root.endswith('resources'):
+                if os.path.basename(root) in DIRS_TO_INCLUDE:
                     copy_list.append({'src': fullpath, 'dest': filename})
                     continue
 
