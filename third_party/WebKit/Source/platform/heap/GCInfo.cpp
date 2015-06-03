f49729b80b5ed396ff23c322b34c3adfef2adeb3
haraken@chromium.org <haraken@chromium.org>
2015-06-03 07:04:54 +0000

    Oilpan: assertObjectHasGCInfo should not allow access to orphaned pages
    This CL removes ASSERT(page->orphaned() || ...) from assertObjectHasGCInfo(),
    since orphaned pages must not be accessed in any way.
    
    BUG=475801
    
    Review URL: https://codereview.chromium.org/1159703002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196375 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 34b9f88a9971d1b7a0566ba97cec0716f591276f

--- third_party/WebKit/Source/platform/heap/GCInfo.cpp
+++ third_party/WebKit/Source/platform/heap/GCInfo.cpp
@@ -76,7 +76,8 @@ void assertObjectHasGCInfo(const void* payload, size_t gcInfoIndex)
     // On component builds we cannot compare the gcInfos as they are statically
     // defined in each of the components and hence will not match.
     BasePage* page = pageFromObject(payload);
-    ASSERT(page->orphaned() || HeapObjectHeader::fromPayload(payload)->gcInfoIndex() == gcInfoIndex);
+    ASSERT(!page->orphaned());
+    ASSERT(HeapObjectHeader::fromPayload(payload)->gcInfoIndex() == gcInfoIndex);
 #endif
 }
 #endif
