53d9d354610c716807880635ab80501d974d51e5
dmazzoni <dmazzoni@chromium.org>
2015-06-02 17:50:36 -0700

    Check the camel case option for both the to and from string enum functions.
    BUG=495323
    
    Review URL: https://codereview.chromium.org/1159953007
    
    Cr-Commit-Position: refs/heads/master@{#332513}
    
    Former-commit-id: 46991dfb271f15ce35c712ca1281274f2118dacb

--- tools/json_schema_compiler/cc_generator.py
+++ tools/json_schema_compiler/cc_generator.py
@@ -973,7 +973,10 @@ class _Generator(object):
       # This is broken up into all ifs with no else ifs because we get
       # "fatal error C1061: compiler limit : blocks nested too deeply"
       # on Windows.
-      (c.Append('if (enum_string == "%s")' % enum_value.name)
+      name = enum_value.name
+      if 'camel_case_enum_to_string' in self._namespace.compiler_options:
+        name = enum_value.CamelName()
+      (c.Append('if (enum_string == "%s")' % name)
         .Append('  return %s;' %
             self._type_helper.GetEnumValue(type_, enum_value)))
     (c.Append('return %s;' % self._type_helper.GetEnumNoneValue(type_))
