393b6815a5abbc55d3e4f93814e6f7b1a55de2c1
Aviv Keshet <akeshet@chromium.org>
2015-06-04 22:59:09 +0000

    Revert "image_test: Fail the build if image tests fail"
    BUG=chromium:416022
    TEST=None
    
    This reverts commit 6397d6409001da437be2a61e1edb16466b000b27.
    
    Change-Id: I2ed4399dfcb02f75dbafb7266fe41337be2955bd
    Reviewed-on: https://chromium-review.googlesource.com/275341
    Reviewed-by: David James <davidjames@chromium.org>
    Tested-by: Aviv Keshet <akeshet@chromium.org>

--- cbuildbot/stages/test_stages.py
+++ cbuildbot/stages/test_stages.py
@@ -312,6 +312,7 @@ class ASyncHWTestStage(HWTestStage, generic_stages.ForgivingBuilderStage):
 
 
 class ImageTestStage(generic_stages.BoardSpecificBuilderStage,
+                     generic_stages.ForgivingBuilderStage,
                      generic_stages.ArchivingStageMixin):
   """Stage that launches tests on the produced disk image."""
 
