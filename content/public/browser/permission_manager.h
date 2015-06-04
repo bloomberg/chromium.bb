934ec10f54e2b700cf4279cf9b8c331f4f2a18a0
mlamouri <mlamouri@chromium.org>
2015-06-04 03:23:47 -0700

    Add comments in content::PermissionManager header file.
    BUG=491923
    
    Review URL: https://codereview.chromium.org/1159303002
    
    Cr-Commit-Position: refs/heads/master@{#332810}
    
    Former-commit-id: bd1e00237854794330af744a8f23328845b4f231

--- content/public/browser/permission_manager.h
+++ content/public/browser/permission_manager.h
@@ -14,10 +14,18 @@ namespace content {
 enum class PermissionType;
 class RenderFrameHost;
 
+// This class allows the content layer to manipulate permissions. It has to be
+// implemented by the embedder which ultimately handles the permission
+// management for the content layer.
 class CONTENT_EXPORT PermissionManager {
  public:
   virtual ~PermissionManager() = default;
 
+  // Requests a permission on behalf of a frame identified by render_frame_host.
+  // The |request_id| is an identifier that can later be used if the request is
+  // cancelled (see CancelPermissionRequest).
+  // When the permission request is handled, whether it failed, timed out or
+  // succeeded, the |callback| will be run.
   virtual void RequestPermission(
       PermissionType permission,
       RenderFrameHost* render_frame_host,
@@ -26,20 +34,29 @@ class CONTENT_EXPORT PermissionManager {
       bool user_gesture,
       const base::Callback<void(PermissionStatus)>& callback) = 0;
 
+  // Cancels a previously requested permission. The given parameter must match
+  // the ones passed to the RequestPermission call.
   virtual void CancelPermissionRequest(PermissionType permission,
                                        RenderFrameHost* render_frame_host,
                                        int request_id,
                                        const GURL& requesting_origin) = 0;
 
+  // Returns the permission status of a given requesting_origin/embedding_origin
+  // tuple. This is not taking a RenderFrameHost because the call might happen
+  // outside of a frame context.
   virtual PermissionStatus GetPermissionStatus(
       PermissionType permission,
       const GURL& requesting_origin,
       const GURL& embedding_origin) = 0;
 
+  // Sets the permission back to its default for the requesting_origin/
+  // embedding_origin tuple.
   virtual void ResetPermission(PermissionType permission,
                                const GURL& requesting_origin,
                                const GURL& embedding_origin) = 0;
 
+  // Registers a permission usage.
+  // TODO(mlamouri): see if we can remove this from the PermissionManager.
   virtual void RegisterPermissionUsage(PermissionType permission,
                                        const GURL& requesting_origin,
                                        const GURL& embedding_origin) = 0;
@@ -53,6 +70,9 @@ class CONTENT_EXPORT PermissionManager {
       const GURL& embedding_origin,
       const base::Callback<void(PermissionStatus)>& callback) = 0;
 
+  // Unregisters from permission status change notifications.
+  // The |subscription_id| must match the value returned by the
+  // SubscribePermissionStatusChange call.
   virtual void UnsubscribePermissionStatusChange(int subscription_id) = 0;
 };
 
