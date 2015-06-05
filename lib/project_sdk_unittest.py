a58426ac8d11e6efaf5a2130a0cd264c2a99768f
David Pursell <dpursell@chromium.org>
2015-06-05 13:58:06 -0700

    project_sdk_unittest: Add function to isolate env checks.
    The unit tests for environment verification were getting a little messy,
    in that adding new checks meant you had to go back and mock out that
    check for each previous test in order to avoid contaminating tests.
    
    This CL just adds a helper function to set up an environment, so that
    tests can easily isolate the single condition under test.
    
    BUG=None
    TEST=cbuildbot/run_tests
    
    Change-Id: I772e9a5b5c09c11bec689cfe4d8a89357c8859e3
    Reviewed-on: https://chromium-review.googlesource.com/275695
    Trybot-Ready: David Pursell <dpursell@chromium.org>
    Reviewed-by: Ralph Nathan <ralphnathan@chromium.org>
    Commit-Queue: David Pursell <dpursell@chromium.org>
    Tested-by: David Pursell <dpursell@chromium.org>

--- lib/project_sdk_unittest.py
+++ lib/project_sdk_unittest.py
@@ -82,71 +82,77 @@ class ProjectSdkVerifyFake(cros_test_lib.MockTempDirTestCase):
   def setUp(self):
     self.rc_mock = self.StartPatcher(cros_build_lib_unittest.RunCommandMock())
 
+  def MockEnvironment(self, bash=True, git='2.2.0', curl=True, suid=True):
+    """Mocks an environment for the specified checks.
+
+    When adding a new environment check, add it to this function so that
+    each test can isolate just the check it wants to test.
+
+    Args:
+      bash: True to mock a valid bash environment.
+      git: If set, git version to mock.
+      curl: True to mock a valid curl environment.
+      suid: True to mock a valid suid environment.
+    """
+    if bash:
+      self.rc_mock.AddCmdResult(['/bin/bash', '--version'])
+    if git:
+      self.rc_mock.AddCmdResult(['git', '--version'],
+                                output='git version %s' % git)
+    if curl:
+      self.rc_mock.AddCmdResult(['curl', '--version'])
+    if suid:
+      self.PatchObject(os, 'statvfs', autospec=True).return_value.f_flag = 0x0
+
+  def testMockEnvironment(self):
+    """Test that MockEnvironment() by itself sets up a valid env."""
+    self.MockEnvironment()
+
+    self.assertTrue(project_sdk.VerifyEnvironment(self.tempdir))
+
   def testGTrusty(self):
     """Test with mock of 'gTrusty' distribution."""
-    self.rc_mock.AddCmdResult(['/bin/bash', '--version'])
-    self.rc_mock.AddCmdResult(['git', '--version'],
-                              output='git version 2.2.0.rc0.207.ga3a616c')
-    self.rc_mock.AddCmdResult(['curl', '--version'])
+    self.MockEnvironment(git='2.2.0.rc0.207.ga3a616c')
 
     self.assertTrue(project_sdk.VerifyEnvironment(self.tempdir))
 
   def testUbuntu14(self):
     """Test with mock of 'Ubuntu LTS 14' distribution."""
-    self.rc_mock.AddCmdResult(['/bin/bash', '--version'])
-    self.rc_mock.AddCmdResult(['git', '--version'],
-                              output='git version 2.1.0')
-    self.rc_mock.AddCmdResult(['curl', '--version'])
+    self.MockEnvironment(git='2.1.0')
 
     self.assertTrue(project_sdk.VerifyEnvironment(self.tempdir))
 
   def testGitNewEnough(self):
     """Test with mock of git 1.8."""
-    self.rc_mock.AddCmdResult(['/bin/bash', '--version'])
-    self.rc_mock.AddCmdResult(['git', '--version'],
-                              output='git version 1.8.3.1')
-    self.rc_mock.AddCmdResult(['curl', '--version'])
+    self.MockEnvironment(git='1.8.3.1')
 
     self.assertTrue(project_sdk.VerifyEnvironment(self.tempdir))
 
   def testFailNoBash(self):
     """Test with mock of no bash present."""
+    self.MockEnvironment(bash=False)
     self.rc_mock.AddCmdResult(['/bin/bash', '--version'], returncode=127)
-    self.rc_mock.AddCmdResult(['git', '--version'],
-                              output='git version 2.2.0.rc0.207.ga3a616c')
-    self.rc_mock.AddCmdResult(['curl', '--version'])
 
     self.assertFalse(project_sdk.VerifyEnvironment(self.tempdir))
 
   def testFailGitTooOld(self):
     """Test with mock of git too old to use."""
-    self.rc_mock.AddCmdResult(['/bin/bash', '--version'])
-    self.rc_mock.AddCmdResult(['git', '--version'],
-                              output='git version 1.7.10.4')
-    self.rc_mock.AddCmdResult(['curl', '--version'])
+    self.MockEnvironment(git='1.7.10.4')
 
     self.assertFalse(project_sdk.VerifyEnvironment(self.tempdir))
 
   def testFailNoCurl(self):
     """Test with mock of no curl present."""
-    self.rc_mock.AddCmdResult(['/bin/bash', '--version'])
-    self.rc_mock.AddCmdResult(['git', '--version'],
-                              output='git version 2.2.0.rc0.207.ga3a616c')
+    self.MockEnvironment(curl=False)
     self.rc_mock.AddCmdResult(['curl', '--version'], returncode=127)
 
     self.assertFalse(project_sdk.VerifyEnvironment(self.tempdir))
 
   def testFailSuid(self):
     """Test with SUID is disabled."""
-    self.rc_mock.AddCmdResult(['/bin/bash', '--version'])
-    self.rc_mock.AddCmdResult(['git', '--version'],
-                              output='git version 2.2.0.rc0.207.ga3a616c')
-    self.rc_mock.AddCmdResult(['curl', '--version'])
-
-    mock_stat = self.PatchObject(os, 'statvfs', autospec=True)
-
+    self.MockEnvironment(suid=False)
     # The os.ST_NOSUID constant wasn't added until python-3.2.
-    mock_stat.return_value.f_flag = 0x2
+    self.PatchObject(os, 'statvfs', autospec=True).return_value.f_flag = 0x2
 
     self.assertFalse(project_sdk.VerifyEnvironment(self.tempdir))
 
