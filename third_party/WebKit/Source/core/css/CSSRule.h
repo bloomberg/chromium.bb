0b5603be7c97aeb6f6670bba63a4c9f594381861
sunil.ratnu@samsung.com <sunil.ratnu@samsung.com>
2015-06-10 15:24:42 +0000

    Remove CSSRule.UNKNOWN_RULE and CSSUnknownRule interface
    The CSSUnknownRule interface was introduced in DOM Level 2 Style, but
    has been removed in CSSOM:
    
    http://www.w3.org/TR/DOM-Level-2-Style/css.html#CSS-CSSUnknownRule
    http://dev.w3.org/csswg/cssom/
    This patch removes it completely.
    
    The link to Intent to Deprecate and Remove is here:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/zzeNGPP1DcA
    
    BUG=496383
    
    Review URL: https://codereview.chromium.org/1154753006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@196877 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 6466a097d7552c489855841287430aa1d7e23c65

--- third_party/WebKit/Source/core/css/CSSRule.h
+++ third_party/WebKit/Source/core/css/CSSRule.h
@@ -41,19 +41,15 @@ public:
     virtual ~CSSRule() { }
 
     enum Type {
-        UNKNOWN_RULE,
-        STYLE_RULE,
-        CHARSET_RULE,
-        IMPORT_RULE,
-        MEDIA_RULE,
-        FONT_FACE_RULE,
-        PAGE_RULE,
-        // 7 was VARIABLES_RULE; we now match other browsers with 7 as
-        // KEYFRAMES_RULE:
-        // <https://bugs.webkit.org/show_bug.cgi?id=71293>.
-        KEYFRAMES_RULE,
+        STYLE_RULE = 1,
+        CHARSET_RULE = 2,
+        IMPORT_RULE = 3,
+        MEDIA_RULE = 4,
+        FONT_FACE_RULE = 5,
+        PAGE_RULE = 6,
+        KEYFRAMES_RULE = 7,
         WEBKIT_KEYFRAMES_RULE = KEYFRAMES_RULE,
-        KEYFRAME_RULE,
+        KEYFRAME_RULE = 8,
         WEBKIT_KEYFRAME_RULE = KEYFRAME_RULE,
         SUPPORTS_RULE = 12,
         VIEWPORT_RULE = 15,
