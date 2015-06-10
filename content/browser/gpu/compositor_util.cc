80a36a212ad9b516111e110f8ee5eacd6a4fe894
reveman <reveman@chromium.org>
2015-06-10 16:37:21 -0700

    content: Enable one-copy tile updates on Android.
    BUG=460288
    
    Review URL: https://codereview.chromium.org/1173893003
    
    Cr-Commit-Position: refs/heads/master@{#333846}
    
    Former-commit-id: 10c3fcaaab39e07f969c5a8c549e876b2f1a66cc

--- content/browser/gpu/compositor_util.cc
+++ content/browser/gpu/compositor_util.cc
@@ -233,19 +233,11 @@ int NumberOfRendererRasterThreads() {
 }
 
 bool IsOneCopyUploadEnabled() {
-  if (IsZeroCopyUploadEnabled())
-    return false;
-
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
-  if (command_line.HasSwitch(switches::kEnableOneCopy))
-    return true;
   if (command_line.HasSwitch(switches::kDisableOneCopy))
     return false;
 
-#if defined(OS_ANDROID)
-  return false;
-#endif
   return true;
 }
 
