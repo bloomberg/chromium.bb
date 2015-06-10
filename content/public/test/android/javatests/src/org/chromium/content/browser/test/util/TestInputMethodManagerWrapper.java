31d72157c20ffded7b85fb0d8609be7b82df64d9
changwan <changwan@chromium.org>
2015-06-10 02:10:12 -0700

    Make sure multi chracter codepoints are deleted correctly
    deleteSurroundingText() only deletes one character even for multi-character
    codepoint.
    
    On the blink side, we have InputMethodController::extendSelectionAndDelete()
    to make sure selection and deletion respect Unicode boundaries. However,
    AdapterInputConnection keeps track of selection region separately, and this
    value is incorrectly updated.
    
    On top of adding a new test for this case, it extends
    waitAndVerifyEditorCallback to also check the outbound calls to
    InputMethodManager.
    
    The above extension found that testEnterKeyEventWhileComposingText fails
    because there is hidden discrepancy between blink implementation and
    what we report to InputMethodManager. So I've added a TODO for that.
    
    BUG=497091
    
    Review URL: https://codereview.chromium.org/1165793007
    
    Cr-Commit-Position: refs/heads/master@{#333704}
    
    Former-commit-id: 5f6e036312bc4a978768f5b5971eee1a5ec9f272

--- content/public/test/android/javatests/src/org/chromium/content/browser/test/util/TestInputMethodManagerWrapper.java
+++ content/public/test/android/javatests/src/org/chromium/content/browser/test/util/TestInputMethodManagerWrapper.java
@@ -13,12 +13,62 @@ import android.view.inputmethod.InputConnection;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.browser.input.InputMethodManagerWrapper;
 
+/**
+ * Overrides InputMethodManagerWrapper for testing purposes.
+ */
 public class TestInputMethodManagerWrapper extends InputMethodManagerWrapper {
+    /**
+     * A simple class to set start and end in int type.
+     */
+    public static class Range {
+        private int mStart;
+        private int mEnd;
+
+        public Range(int start, int end) {
+            mStart = start;
+            mEnd = end;
+        }
+
+        public void set(int start, int end) {
+            mStart = start;
+            mEnd = end;
+        }
+
+        public int start() {
+            return mStart;
+        }
+
+        public int end() {
+            return mEnd;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Range)) return false;
+            if (o == this) return true;
+            Range r = (Range) o;
+            return mStart == r.mStart && mEnd == r.mEnd;
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            return prime * mStart + mEnd;
+        }
+
+        @Override
+        public String toString() {
+            return "[ " + mStart + ", " + mEnd + " ]";
+        }
+    }
+
     private final ContentViewCore mContentViewCore;
     private InputConnection mInputConnection;
     private int mShowSoftInputCounter = 0;
     private int mUpdateSelectionCounter = 0;
     private EditorInfo mEditorInfo;
+    private final Range mSelection = new Range(0, 0);
+    private final Range mComposition = new Range(-1, -1);
 
     public TestInputMethodManagerWrapper(ContentViewCore contentViewCore) {
         super(null);
@@ -57,6 +107,8 @@ public class TestInputMethodManagerWrapper extends InputMethodManagerWrapper {
     public void updateSelection(View view, int selStart, int selEnd,
             int candidatesStart, int candidatesEnd) {
         mUpdateSelectionCounter++;
+        mSelection.set(selStart, selEnd);
+        mComposition.set(candidatesStart, candidatesEnd);
     }
 
     public int getShowSoftInputCounter() {
@@ -70,5 +122,12 @@ public class TestInputMethodManagerWrapper extends InputMethodManagerWrapper {
     public EditorInfo getEditorInfo() {
         return mEditorInfo;
     }
-}
 
+    public Range getSelection() {
+        return mSelection;
+    }
+
+    public Range getComposition() {
+        return mComposition;
+    }
+}
