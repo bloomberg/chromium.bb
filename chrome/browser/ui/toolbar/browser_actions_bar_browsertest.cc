c73f81589e8959611e815172a1f29036440a0348
kjellander <kjellander@chromium.org>
2015-06-10 03:00:14 -0700

    Disable BrowserActionsBarBrowserTest.BrowserActionPopupTest on Mac ASan.
    BUG=498665
    TBR=rdevlin.cronin@chromium.org
    
    Review URL: https://codereview.chromium.org/1173933003
    
    Cr-Commit-Position: refs/heads/master@{#333708}
    
    Former-commit-id: 4d058780a083239c728509535cdadae6ad626e95

--- chrome/browser/ui/toolbar/browser_actions_bar_browsertest.cc
+++ chrome/browser/ui/toolbar/browser_actions_bar_browsertest.cc
@@ -335,7 +335,14 @@ IN_PROC_BROWSER_TEST_F(BrowserActionsBarRedesignBrowserTest,
   EXPECT_FALSE(browser_actions_bar()->OverflowedActionButtonWantsToRun());
 }
 
-IN_PROC_BROWSER_TEST_F(BrowserActionsBarBrowserTest, BrowserActionPopupTest) {
+// Flaky on Mac ASan 64 bot. See http://crbug.com/498665.
+#if defined(OS_MACOSX) && defined(ADDRESS_SANITIZER)
+#define MAYBE_BrowserActionPopupTest DISABLED_BrowserActionPopupTest
+#else
+#define MAYBE_BrowserActionPopupTest BrowserActionPopupTest
+#endif
+IN_PROC_BROWSER_TEST_F(BrowserActionsBarBrowserTest,
+                       MAYBE_BrowserActionPopupTest) {
   // Load up two extensions that have browser action popups.
   base::FilePath data_dir =
       test_data_dir_.AppendASCII("api_test").AppendASCII("browser_action");
