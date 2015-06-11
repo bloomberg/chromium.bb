424d085fec441d9a92a9631ee5fca32a14733948
shadi <shadi@chromium.org>
2015-06-10 19:18:04 -0700

    Add support for chrome sign-in in Telemetry using login helpers.
    This approach eliminates the need for browser_credentials.
    
    BUG=496836
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1143513002
    
    Cr-Commit-Position: refs/heads/master@{#333874}
    
    Former-commit-id: da23d82bb41c0d2666dfbf4fef6f6009b5addab1

---
+++ tools/perf/page_sets/chrome_signin.py
@@ -0,0 +1,35 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry.page import page as page_module
+from telemetry.page import page_set as page_set_module
+
+from page_sets.login_helpers import chrome_login
+
+
+class ChromeSigninPage(page_module.Page):
+  """A page that signs in a user to Chrome."""
+
+  def __init__(self, page_set):
+    super(ChromeSigninPage, self).__init__(
+        url='chrome://signin-internals',
+        page_set=page_set,
+        credentials_path='data/chrome_signin_credentials.json',
+        credentials_bucket=page_set_module.INTERNAL_BUCKET)
+    self.user_agent_type = 'desktop'
+
+  def RunPageInteractions(self, action_runner):
+    # Use page.credentials_path because it is automatically translated into a
+    # full path relative to the page.
+    chrome_login.LoginChromeAccount(action_runner, 'chrome',
+        credentials_path=self.credentials_path)
+
+
+class ChromeSigninPageSet(page_set_module.PageSet):
+
+  def __init__(self):
+    super(ChromeSigninPageSet, self).__init__(
+        archive_data_file='data/chrome_signin_archive.json',
+        bucket=page_set_module.INTERNAL_BUCKET)
+    self.AddUserStory(ChromeSigninPage(self))
