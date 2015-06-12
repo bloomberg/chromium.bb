bc6307299499d2ff6cc26cb808de2eb073453f86
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com>
2015-06-12 13:02:00 +0000

    Use lambda instead of the 'CSSPropertyIDComparer' class
    Review URL: https://codereview.chromium.org/1182433002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197034 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c9e532558d1f629c5b4d4a8ddc8245d922309427

--- third_party/WebKit/Source/core/css/StylePropertySet.cpp
+++ third_party/WebKit/Source/core/css/StylePropertySet.cpp
@@ -500,31 +500,25 @@ CSSStyleDeclaration* MutableStylePropertySet::ensureCSSStyleDeclaration()
     return m_cssomWrapper.get();
 }
 
-
-class CSSPropertyIDComparer {
-public:
-    explicit CSSPropertyIDComparer(CSSPropertyID propertyID)
-        : m_propertyID(static_cast<uint16_t>(propertyID)) { }
-    bool operator()(const CSSProperty& property) const
-    {
-        if (property.metadata().m_propertyID == m_propertyID) {
-            // Only enabled properties should be part of the style.
-            ASSERT(CSSPropertyMetadata::isEnabledProperty(propertyID()));
-            return true;
-        }
-        return false;
-    }
-
-private:
-    CSSPropertyID propertyID() const { return static_cast<CSSPropertyID>(m_propertyID); }
-    uint16_t m_propertyID; // Store as uint16_t to avoid casts while comparing.
-};
-
 int MutableStylePropertySet::findPropertyIndex(CSSPropertyID propertyID) const
 {
     const CSSProperty* begin = m_propertyVector.data();
     const CSSProperty* end = begin + m_propertyVector.size();
-    const CSSProperty* it = std::find_if(begin, end, CSSPropertyIDComparer(propertyID));
+    // Convert here propertyID into an uint16_t to compare it with the metadata's m_propertyID to avoid
+    // the compiler converting it to an int multiple times in the loop.
+    uint16_t id = static_cast<uint16_t>(propertyID);
+
+    auto compare = [propertyID, id](const CSSProperty& property) -> bool
+    {
+        if (property.metadata().m_propertyID == id) {
+            // Only enabled properties should be part of the style.
+            ASSERT(CSSPropertyMetadata::isEnabledProperty(propertyID));
+            return true;
+        }
+        return false;
+    };
+
+    const CSSProperty* it = std::find_if(begin, end, compare);
 
     return (it == end) ? -1 : it - begin;
 }
