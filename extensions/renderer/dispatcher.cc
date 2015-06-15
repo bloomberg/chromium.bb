9b8851968459beb5e1c64c36b511e37c96e62033
skobes <skobes@chromium.org>
2015-06-15 12:31:00 -0700

    Revert of Destroy ContentWatcher on renderer process shutdown (patchset #1 id:1 of https://codereview.chromium.org/1184913003/)
    Reason for revert:
    Suspected cause of browser_tests failures on Linux ChromiumOS Tests (1).
    
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/4407
    
    FormAutocompleteTest.AutoCompleteOffInputSubmit
    FormAutocompleteTest.NormalFormSubmit
    FormAutocompleteTest.AutoCompleteOffFormSubmit
    
    Received signal 11 SEGV_MAPERR 000000000008
    #0 0x0000017d3248 base::debug::(anonymous namespace)::StackDumpSignalHandler()
    #1 0x7f226996acb0 \u003Cunknown>
    #2 0x0000031213a2 blink::WebDocument::watchCSSSelectors()
    #3 0x000004bfbab6 extensions::ContentWatcher::DidCreateDocumentElement()
    #4 0x000004bd08e2 extensions::Dispatcher::DidCreateDocumentElement()
    #5 0x000003f51137 content::RenderFrameImpl::didCreateDocumentElement()
    
    Original issue's description:
    > Destroy ContentWatcher on renderer process shutdown
    >
    > Before this CL, extensions::Dispatcher held ContentWatcher even after
    > Blink shutdown. This caused problems with object lifetimes.
    >
    > ContentWatcher "has-a" WebString, which is only valid before Blink shutdown,
    > as StringImpl held by WebString is allocated on partitionAlloc, which backing
    > is bulk-freed on Blink shutdown.
    >
    > This CL fixes the issue by explicitly releasing ContentWatcher when
    > extensions::Dispatcher notices that Blink is about to be shutdown from
    > OnRendererProcessShutdown call.
    >
    > BUG=488461
    >
    > Committed: https://crrev.com/5278310621d560ee546b413f71c6ffd2b8648695 [formerly 88b136202cad86f56feccf2af413ae4082594e7f]
    > Cr-Commit-Position: refs/heads/master@{#334411}
    
    TBR=jyasskin@chromium.org,kouhei@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=488461
    
    Review URL: https://codereview.chromium.org/1183203003
    
    Cr-Commit-Position: refs/heads/master@{#334436}
    
    Former-commit-id: 05884d5293a876f084011ad65738d4ca629918e8

--- extensions/renderer/dispatcher.cc
+++ extensions/renderer/dispatcher.cc
@@ -788,7 +788,6 @@ void Dispatcher::IdleNotification() {
 void Dispatcher::OnRenderProcessShutdown() {
   v8_schema_registry_.reset();
   forced_idle_timer_.reset();
-  content_watcher_.reset();
 }
 
 void Dispatcher::OnActivateExtension(const std::string& extension_id) {
