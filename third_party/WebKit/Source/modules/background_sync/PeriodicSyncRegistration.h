0b1eb62bd30a0955934aebf913ef0a53cc2e21ce
paritosh.in@samsung.com <paritosh.in@samsung.com>
2015-06-16 13:24:50 +0000

    Removing Nullable<unsigned long> for PeriodicSyncRegistration::m_minPeriod
    As m_minPeriod will get initialized through dictionary member minPeriod
    it will have default value 0.
    
    This cl also removes has*() for minPeriod, networkState, powerState and
    tag attribute, as all these are dictionary members with default values, has*() will always return true.
    
    BUG=497142
    
    Review URL: https://codereview.chromium.org/1189473007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197171 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2c5fb7b4af1c1df10b3bbb671e926c3b456b41bf

--- third_party/WebKit/Source/modules/background_sync/PeriodicSyncRegistration.h
+++ third_party/WebKit/Source/modules/background_sync/PeriodicSyncRegistration.h
@@ -25,19 +25,15 @@ public:
 
     virtual ~PeriodicSyncRegistration();
 
-    bool hasMinPeriod() const { return !m_minPeriod.isNull(); }
-    unsigned long minPeriod() const { return m_minPeriod.get(); }
+    unsigned long minPeriod() const { return m_minPeriod; }
     void setMinPeriod(unsigned long value) { m_minPeriod = value; }
 
-    bool hasNetworkState() const { return !m_networkState.isNull(); }
     String networkState() const { return m_networkState; }
     void setNetworkState(String value) { m_networkState = value; }
 
-    bool hasPowerState() const { return !m_powerState.isNull(); }
     String powerState() const { return m_powerState; }
     void setPowerState(String value) { m_powerState = value; }
 
-    bool hasTag() const { return !m_tag.isNull(); }
     String tag() const { return m_tag; }
     void setTag(String value) { m_tag = value; }
 
@@ -49,7 +45,7 @@ private:
     PeriodicSyncRegistration(int64_t id, const PeriodicSyncRegistrationOptions&, ServiceWorkerRegistration*);
 
     int64_t m_id;
-    Nullable<unsigned long> m_minPeriod;
+    unsigned long m_minPeriod;
     String m_networkState;
     String m_powerState;
     String m_tag;
