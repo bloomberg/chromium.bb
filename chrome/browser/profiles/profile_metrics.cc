92db6d37cf3fbbd3698b947998728e687095b532
bcwhite <bcwhite@chromium.org>
2015-06-17 09:17:01 -0700

    Remove comment references to Chrome Frame.
    It's useful to keep the tests against a zero-count
    number of profiles because such conditions have been
    known to happen during startup while things are still
    being initialized.
    
    BUG=331517
    
    Review URL: https://codereview.chromium.org/1186953004
    
    Cr-Commit-Position: refs/heads/master@{#334848}
    
    Former-commit-id: d59b96fc8279052c5b7171e620db4648c07a484b

--- chrome/browser/profiles/profile_metrics.cc
+++ chrome/browser/profiles/profile_metrics.cc
@@ -147,7 +147,7 @@ bool ProfileMetrics::CountProfileInformation(ProfileManager* manager,
   size_t number_of_profiles = info_cache.GetNumberOfProfiles();
   counts->total = number_of_profiles;
 
-  // Ignore other metrics if we have no profiles, e.g. in Chrome Frame tests.
+  // Ignore other metrics if we have no profiles.
   if (!number_of_profiles)
     return false;
 
@@ -208,7 +208,7 @@ void ProfileMetrics::LogNumberOfProfiles(ProfileManager* manager) {
   bool success = CountProfileInformation(manager, &counts);
   UMA_HISTOGRAM_COUNTS_100("Profile.NumberOfProfiles", counts.total);
 
-  // Ignore other metrics if we have no profiles, e.g. in Chrome Frame tests.
+  // Ignore other metrics if we have no profiles.
   if (success) {
     UMA_HISTOGRAM_COUNTS_100("Profile.NumberOfManagedProfiles",
                              counts.supervised);
