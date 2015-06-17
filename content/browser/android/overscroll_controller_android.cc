a9037099b77d180b480da4675abf81ab22271743
jmanko <jmanko@opera.com>
2015-06-17 13:07:06 -0700

    Add missing null-check in OverscrollControllerAndroid::Animate()
    Without it, there's a crash when running with
    disable-overscroll-edge-effect switch.
    
    BUG=501472
    
    Review URL: https://codereview.chromium.org/1195453003
    
    Cr-Commit-Position: refs/heads/master@{#334897}
    
    Former-commit-id: e3de7fc7dbb642ed034afa1c1fed70a748a60f35

--- content/browser/android/overscroll_controller_android.cc
+++ content/browser/android/overscroll_controller_android.cc
@@ -226,7 +226,7 @@ void OverscrollControllerAndroid::OnOverscrolled(
 bool OverscrollControllerAndroid::Animate(base::TimeTicks current_time,
                                           cc::Layer* parent_layer) {
   DCHECK(parent_layer);
-  if (!enabled_)
+  if (!enabled_ || !glow_effect_)
     return false;
 
   return glow_effect_->Animate(current_time, parent_layer);
