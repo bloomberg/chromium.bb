9795469a22df663cada2cd330b40ea45f3ed2309
ojan@chromium.org <ojan@chromium.org>
2015-06-17 20:21:35 +0000

    Parsing expectations is n^2 in the number of tests per line.
    When merging expectations in different expectations files, we
    do an n^2 operation. For each line/test combination, we merge
    them. Instead, this algorithm only needs to merge a given
    pair of lines once.
    
    Add a cache of merged lines so that we reuse merged lines
    when we've already seen a given pair.
    
    This hits our current code base because we have ~25k
    slimmingpaint tests that are skipped in TestExpectations.
    Then running with "-i virtual" on the commandline creates
    a fake expectations model to merge with that. On my z620,
    this patch takes the time to merge the models from >4 minutes
    to ~2 seconds.
    
    Review URL: https://codereview.chromium.org/1182253003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197278 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 1be3034d6330b6efead3fa9486fbbc6b6b50b61e

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/models/test_expectations.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/models/test_expectations.py
@@ -30,6 +30,8 @@
 for layout tests.
 """
 
+from collections import defaultdict
+
 import logging
 import re
 
@@ -562,10 +564,26 @@ class TestExpectationsModel(object):
     def merge_model(self, other):
         self._merge_test_map(self._test_to_expectations, other._test_to_expectations)
 
-        for test, line in other._test_to_expectation_line.items():
+        # merge_expectation_lines is O(tests per line). Therefore, this loop
+        # is O((tests per line)^2) which is really expensive when a line
+        # contains a lot of tests. Cache the output of merge_expectation_lines
+        # so that we only call that n^2 in the number of *lines*.
+        merge_lines_cache = defaultdict(dict)
+
+        for test, other_line in other._test_to_expectation_line.items():
+            merged_line = None
             if test in self._test_to_expectation_line:
-                line = TestExpectationLine.merge_expectation_lines(self._test_to_expectation_line[test], line, model_all_expectations=False)
-            self._test_to_expectation_line[test] = line
+                self_line = self._test_to_expectation_line[test]
+
+                if other_line not in merge_lines_cache[self_line]:
+                    merge_lines_cache[self_line][other_line] = TestExpectationLine.merge_expectation_lines(
+                        self_line, other_line, model_all_expectations=False)
+
+                merged_line = merge_lines_cache[self_line][other_line]
+            else:
+                merged_line = other_line
+
+            self._test_to_expectation_line[test] = merged_line
 
         self._merge_dict_of_sets(self._expectation_to_tests, other._expectation_to_tests)
         self._merge_dict_of_sets(self._timeline_to_tests, other._timeline_to_tests)
