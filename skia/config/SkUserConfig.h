0de65bcdf878f1da316cc7903c794eb3d0654cf7
reed <reed@google.com>
2015-06-17 09:50:27 -0700

    add SK_SUPPORT_LEGACY_DATARELEASEPROC_PARAMS flag
    in support of https://codereview.chromium.org/1193553002/
    
    BUG=501376
    NOTRY=True
    just adding a new #define
    
    Review URL: https://codereview.chromium.org/1184953005
    
    Cr-Commit-Position: refs/heads/master@{#334855}
    
    Former-commit-id: ef0d34a4b63b5622029860e16edae9416b27ba57

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -273,6 +273,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_INT_COLORMATRIX
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_DATARELEASEPROC_PARAMS
+#   define SK_SUPPORT_LEGACY_DATARELEASEPROC_PARAMS
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
