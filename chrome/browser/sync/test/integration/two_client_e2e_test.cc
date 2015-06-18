007a026f3b55fe8e1913959ee940c3e2d0b89d0b
maniscalco <maniscalco@chromium.org>
2015-06-18 13:19:51 -0700

    [Sync] Add end-to-end test for syncing items added when type is disabled
    Verify that bookmarks added when bookmark syncing is disabled are
    properly synced once bookmark syncing is re-enabled.
    
    TestTracker 934886
    
    BUG=500105
    
    Review URL: https://codereview.chromium.org/1191893007
    
    Cr-Commit-Position: refs/heads/master@{#335110}
    
    Former-commit-id: 67cef47ff4fb546436cf29e351fe807144c57ccf

--- chrome/browser/sync/test/integration/two_client_e2e_test.cc
+++ chrome/browser/sync/test/integration/two_client_e2e_test.cc
@@ -4,15 +4,18 @@
 
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/sync/test/integration/bookmarks_helper.h"
+#include "chrome/browser/sync/test/integration/profile_sync_service_harness.h"
 #include "chrome/browser/sync/test/integration/sync_test.h"
+#include "components/bookmarks/browser/bookmark_node.h"
 
-// The E2E tests are designed to run only against real backend servers. They are
-// disabled on regular bots. http://crbug.com/431366
+// The E2E_ONLY tests are designed to run only against real backend
+// servers. They are disabled on regular bots. http://crbug.com/431366
 #define E2E_ONLY(x) DISABLED_##x
 
 using bookmarks_helper::AddURL;
 using bookmarks_helper::AwaitAllModelsMatch;
 using bookmarks_helper::CountAllBookmarks;
+using bookmarks_helper::GetBookmarkBarNode;
 
 class TwoClientE2ETest : public SyncTest {
  public:
@@ -75,4 +78,34 @@ IN_PROC_BROWSER_TEST_F(TwoClientE2ETest, E2E_ONLY(TwoClientsAddBookmarks)) {
     ASSERT_EQ(CountAllBookmarks(i), init_bookmarks_count + num_clients()) <<
         "Total bookmark count is wrong.";
   }
+}
+
+// Verify that a bookmark added on a client with bookmark syncing disabled gets
+// synced to a second client once bookmark syncing is re-enabled.
+IN_PROC_BROWSER_TEST_F(TwoClientE2ETest, AddBookmarkWhileDisabled) {
+  ASSERT_TRUE(SetupSync()) << "SetupSync() failed.";
+  ASSERT_TRUE(AwaitAllModelsMatch())
+      << "Initial bookmark models did not match for all profiles";
+  const int initial_count = CountAllBookmarks(0);
+
+  // Verify that we can sync. Add a bookmark on the first client and verify it's
+  // synced to the second client.
+  const std::string url_title = "a happy little url";
+  const GURL url("https://example.com");
+  ASSERT_TRUE(AddURL(0, GetBookmarkBarNode(0), 0, url_title, url) != NULL);
+  ASSERT_TRUE(AwaitAllModelsMatch());
+  ASSERT_EQ(initial_count + 1, CountAllBookmarks(0));
+  ASSERT_EQ(initial_count + 1, CountAllBookmarks(1));
+
+  // Disable bookmark syncing on the first client, add another bookmark,
+  // re-enable bookmark syncing and see that the second bookmark reaches the
+  // second client.
+  ASSERT_TRUE(GetClient(0)->DisableSyncForDatatype(syncer::BOOKMARKS));
+  const std::string url_title_2 = "another happy little url";
+  const GURL url_2("https://example.com/second");
+  ASSERT_TRUE(AddURL(0, GetBookmarkBarNode(0), 0, url_title_2, url_2) != NULL);
+  ASSERT_TRUE(GetClient(0)->EnableSyncForDatatype(syncer::BOOKMARKS));
+  ASSERT_TRUE(AwaitAllModelsMatch());
+  ASSERT_EQ(initial_count + 2, CountAllBookmarks(0));
+  ASSERT_EQ(initial_count + 2, CountAllBookmarks(1));
 }
