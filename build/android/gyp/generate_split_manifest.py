8dbe3cb182861c8d66e865bd18fc9294df0d7c1e
agrieve <agrieve@chromium.org>
2015-06-18 09:26:18 -0700

    Fix invalid depfile generation in generate_split_manifest.py
    This caused ninja to crash for incremental builds of ChromeModern.apk
    
    BUG=447152
    
    Review URL: https://codereview.chromium.org/1187903003
    
    Cr-Commit-Position: refs/heads/master@{#335060}
    
    Former-commit-id: afad5c2282c80622b28d112f757fe3d6c06dfa1a

--- build/android/gyp/generate_split_manifest.py
+++ build/android/gyp/generate_split_manifest.py
@@ -90,7 +90,7 @@ def main():
   if options.depfile:
     build_utils.WriteDepfile(
         options.depfile,
-        [main_manifest] + build_utils.GetPythonDependencies())
+        [options.main_manifest] + build_utils.GetPythonDependencies())
 
 
 if __name__ == '__main__':
