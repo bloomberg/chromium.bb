6c4d261781c5a232dc8502e9454b754857867abc
rsadam <rsadam@chromium.org>
2015-06-18 10:18:16 -0700

    Punts gesture editing to next milestone.
    BUG=
    
    Review URL: https://codereview.chromium.org/1176153005
    
    Cr-Commit-Position: refs/heads/master@{#335068}
    
    Former-commit-id: 545ea10f82d534003d677dabc61a410fa4af59e1

--- ui/keyboard/keyboard_util.cc
+++ ui/keyboard/keyboard_util.cc
@@ -172,7 +172,7 @@ bool IsGestureEditingEnabled() {
   std::string keyboard_switch =
       base::CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
           switches::kGestureEditing);
-  return keyboard_switch != switches::kGestureEditingDisabled;
+  return keyboard_switch == switches::kGestureEditingEnabled;
 }
 
 bool IsSmartDeployEnabled() {
