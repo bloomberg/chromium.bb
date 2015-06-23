f768688e30f64156a201b31e60b3a83d9d78dac7
leilei <leilei@chromium.org>
2015-06-23 01:35:38 -0700

    [Media Router] Add integration tests and e2e tests for media router and presentation APIs.
    BUG=464227
    
    Review URL: https://codereview.chromium.org/1180013008
    
    Cr-Commit-Position: refs/heads/master@{#335647}
    
    Former-commit-id: ec50c44f4753c202c4578b3370aac4b26cf3e45d

---
+++ chrome/test/media_router/media_router_base_browsertest.cc
@@ -0,0 +1,128 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/media_router/media_router_base_browsertest.h"
+
+#include "base/bind.h"
+#include "base/command_line.h"
+#include "base/path_service.h"
+#include "base/timer/elapsed_timer.h"
+#include "chrome/browser/extensions/unpacked_installer.h"
+#include "chrome/common/chrome_switches.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/public/common/content_switches.h"
+#include "content/public/test/test_utils.h"
+#include "extensions/browser/process_manager.h"
+
+namespace {
+// Command line argument to specify CRX extension location.
+const char kExtensionCrx[] = "extension-crx";
+// Command line argument to specify unpacked extension location.
+const char kExtensionUnpacked[] = "extension-unpacked";
+}  // namespace
+
+namespace media_router {
+
+MediaRouterBaseBrowserTest::MediaRouterBaseBrowserTest()
+    : extension_load_event_(false, false),
+      extension_host_created_(false) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+          switches::kEnableMediaRouter);
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kEnableBlinkFeatures, "Presentation");
+}
+
+MediaRouterBaseBrowserTest::~MediaRouterBaseBrowserTest() {
+}
+
+void MediaRouterBaseBrowserTest::SetUp() {
+  ParseCommandLine();
+  ExtensionBrowserTest::SetUp();
+}
+
+void MediaRouterBaseBrowserTest::TearDown() {
+  ExtensionBrowserTest::TearDown();
+}
+
+void MediaRouterBaseBrowserTest::SetUpOnMainThread() {
+  extensions::ProcessManager* process_manager =
+      extensions::ProcessManager::Get(browser()->profile());
+  DCHECK(process_manager);
+  process_manager->AddObserver(this);
+  InstallAndEnableMRExtension();
+  extension_load_event_.Wait();
+}
+
+void MediaRouterBaseBrowserTest::TearDownOnMainThread() {
+  UninstallMRExtension();
+  extensions::ProcessManager* process_manager =
+      extensions::ProcessManager::Get(browser()->profile());
+  DCHECK(process_manager);
+  process_manager->RemoveObserver(this);
+}
+
+void MediaRouterBaseBrowserTest::InstallAndEnableMRExtension() {
+  if (is_unpacked()) {
+    const extensions::Extension* extension = LoadExtension(extension_unpacked_);
+    extension_id_ = extension->id();
+  } else {
+    NOTIMPLEMENTED();
+  }
+}
+
+void MediaRouterBaseBrowserTest::UninstallMRExtension() {
+  if (!extension_id_.empty()) {
+    UninstallExtension(extension_id_);
+  }
+}
+
+void MediaRouterBaseBrowserTest::ConditionalWait(
+    base::TimeDelta timeout, base::TimeDelta interval,
+    const base::Callback<bool(void)>& callback) {
+  base::ElapsedTimer timer;
+  while (!callback.Run() && timer.Elapsed() < timeout) {
+    base::RunLoop run_loop;
+    base::MessageLoop::current()->PostDelayedTask(
+        FROM_HERE, run_loop.QuitClosure(), interval);
+    run_loop.Run();
+  }
+}
+
+void MediaRouterBaseBrowserTest::Wait(base::TimeDelta timeout) {
+  base::RunLoop run_loop;
+  base::MessageLoop::current()->PostDelayedTask(
+      FROM_HERE, run_loop.QuitClosure(), timeout);
+  run_loop.Run();
+}
+
+void MediaRouterBaseBrowserTest::OnBackgroundHostCreated(
+    extensions::ExtensionHost* host) {
+  extension_host_created_ = true;
+  DVLOG(0) << "Host created";
+  extension_load_event_.Signal();
+}
+
+void MediaRouterBaseBrowserTest::ParseCommandLine() {
+  DVLOG(0) << "ParseCommandLine";
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+
+  extension_crx_ = command_line->GetSwitchValuePath(kExtensionCrx);
+  extension_unpacked_ = command_line->GetSwitchValuePath(kExtensionUnpacked);
+
+  // Check if there is mr_extension folder under PRODUCT_DIR folder.
+  if (extension_crx_.empty() && extension_unpacked_.empty()) {
+    base::FilePath base_dir;
+    ASSERT_TRUE(PathService::Get(base::DIR_EXE, &base_dir));
+    base::FilePath extension_path =
+        base_dir.Append(FILE_PATH_LITERAL("mr_extension/"));
+    if (PathExists(extension_path)) {
+      extension_unpacked_ = extension_path;
+    }
+  }
+
+  // Exactly one of these two arguments should be provided.
+  ASSERT_NE(extension_crx_.empty(), extension_unpacked_.empty());
+}
+
+}  // namespace media_router
---
+++ chrome/test/media_router/media_router_e2e_browsertest.cc
@@ -0,0 +1,155 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/media_router/media_router_e2e_browsertest.h"
+
+#include "base/command_line.h"
+#include "base/stl_util.h"
+#include "chrome/browser/media/router/media_router.h"
+#include "chrome/browser/media/router/media_router_mojo_impl.h"
+#include "chrome/browser/media/router/media_router_mojo_impl_factory.h"
+#include "chrome/browser/media/router/media_source.h"
+#include "chrome/browser/media/router/media_source_helper.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/sessions/session_tab_helper.h"
+#include "chrome/browser/ui/browser_finder.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/test/base/ui_test_utils.h"
+#include "content/public/test/test_utils.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+// Use the following command to run e2e browser tests:
+// ./out/Debug/browser_tests --user-data-dir=<empty user data dir> \
+//   --extension-unpacked=<mr extension dir> \
+//   --receiver=<chromecast device name> \
+//   --enable-pixel-output-in-tests --run-manual \
+//   --gtest_filter=MediaRouterE2EBrowserTest.<test case name> \
+//   --enable-logging=stderr \
+//   --whitelisted-extension-id=enhhojjnijigcajfphajepfemndkmdlo \
+//   --ui-test-action-timeout=200000 \
+//   --enable-media-router
+
+namespace {
+// Command line argument to specify receiver,
+const char kReceiver[] = "receiver";
+// URL to launch Castv2Player_Staging app on Chromecast
+const char kCastAppPresentationUrl[] =
+    "https://google.com/cast#__castAppId__=BE6E4473";
+}  // namespace
+
+namespace media_router {
+
+MediaRouterE2EBrowserTest::MediaRouterE2EBrowserTest()
+    : media_router_(nullptr) {
+}
+
+MediaRouterE2EBrowserTest::~MediaRouterE2EBrowserTest() {
+}
+
+void MediaRouterE2EBrowserTest::SetUpOnMainThread() {
+  MediaRouterBaseBrowserTest::SetUpOnMainThread();
+  media_router_ =
+      MediaRouterMojoImplFactory::GetApiForBrowserContext(browser()->profile());
+  DCHECK(media_router_);
+}
+
+void MediaRouterE2EBrowserTest::TearDownOnMainThread() {
+  MediaRouterBaseBrowserTest::TearDownOnMainThread();
+  media_router_ = nullptr;
+}
+
+void MediaRouterE2EBrowserTest::OnRouteResponseReceived(
+    scoped_ptr<MediaRoute> route,
+    const std::string& error) {
+  ASSERT_TRUE(route.get());
+  route_id_ = route->media_route_id();
+}
+
+void MediaRouterE2EBrowserTest::CreateMediaRoute(
+    const MediaSource& source, const GURL& origin, int tab_id) {
+  DCHECK(media_router_);
+  observer_.reset(new TestMediaSinksObserver(media_router_, source));
+
+  DVLOG(1) << "Receiver name: " << receiver_;
+  // Wait for MediaSinks compatible with |source| to be discovered.
+  ConditionalWait(base::TimeDelta::FromSeconds(30),
+                  base::TimeDelta::FromSeconds(1),
+                  base::Bind(&MediaRouterE2EBrowserTest::IsSinkDiscovered,
+                             base::Unretained(this)));
+
+  const auto& sink_map = observer_->sink_map;
+  const auto it = sink_map.find(receiver_);
+  const MediaSink& sink = it->second;
+
+  // The callback will set route_id_ when invoked.
+  media_router_->CreateRoute(
+      source.id(), sink.id(), origin, tab_id,
+      base::Bind(&MediaRouterE2EBrowserTest::OnRouteResponseReceived,
+                 base::Unretained(this)));
+
+  // Wait for the route request to be fulfilled (and route to be started).
+  ConditionalWait(base::TimeDelta::FromSeconds(30),
+                  base::TimeDelta::FromSeconds(1),
+                  base::Bind(&MediaRouterE2EBrowserTest::IsRouteCreated,
+                             base::Unretained(this)));
+}
+
+void MediaRouterE2EBrowserTest::StopMediaRoute() {
+  ASSERT_FALSE(route_id_.empty());
+
+  media_router_->CloseRoute(route_id_);
+
+  observer_.reset();
+  route_id_.clear();
+}
+
+void MediaRouterE2EBrowserTest::ParseCommandLine() {
+  MediaRouterBaseBrowserTest::ParseCommandLine();
+  base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
+
+  receiver_ = command_line->GetSwitchValueASCII(kReceiver);
+  ASSERT_FALSE(receiver_.empty());
+}
+
+bool MediaRouterE2EBrowserTest::IsSinkDiscovered() const {
+  return ContainsKey(observer_->sink_map, receiver_);
+}
+
+bool MediaRouterE2EBrowserTest::IsRouteCreated() const {
+  return !route_id_.empty();
+}
+
+// Test cases
+
+IN_PROC_BROWSER_TEST_F(MediaRouterE2EBrowserTest, MANUAL_TabMirroring) {
+  EXPECT_EQ(1u, chrome::GetTotalBrowserCount());
+  EXPECT_EQ(1, browser()->tab_strip_model()->count());
+
+  ui_test_utils::NavigateToURLBlockUntilNavigationsComplete(
+      browser(), GURL("about:blank"), 1);
+  int tab_id = SessionTabHelper::IdForTab(
+      browser()->tab_strip_model()->GetActiveWebContents());
+
+  // Wait for 30 seconds to make sure the route is stable.
+  CreateMediaRoute(MediaSourceForTab(tab_id), GURL("http://origin/"), tab_id);
+  Wait(base::TimeDelta::FromSeconds(30));
+
+  // Wait for 10 seconds to make sure route has been stopped.
+  StopMediaRoute();
+  Wait(base::TimeDelta::FromSeconds(10));
+}
+
+IN_PROC_BROWSER_TEST_F(MediaRouterE2EBrowserTest, MANUAL_CastApp) {
+  // Wait for 30 seconds to make sure the route is stable.
+  CreateMediaRoute(MediaSourceForPresentationUrl(kCastAppPresentationUrl),
+                   GURL("http://origin/"), kInvalidTabId);
+  Wait(base::TimeDelta::FromSeconds(30));
+
+  // Wait for 10 seconds to make sure route has been stopped.
+  StopMediaRoute();
+  Wait(base::TimeDelta::FromSeconds(10));
+}
+
+}  // namespace media_router
