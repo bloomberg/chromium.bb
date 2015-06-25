689075d2f79651b2b96b20dc1447840fa55e948c
tonychun <tonychun@google.com>
2015-06-25 16:29:37 -0700

    Added support to get refresh token from disk using username and refresh token path.
    I used a RefreshTokenStore to find, parse, and extract the correct refresh token from the .json file. I did not add unit test support to set refresh_token_store to a test_refresh_token_store in this CL, but I plan to have it when I refactor the GetRefreshToken functionality to the Chromoting environment.
    
    All try bots succeeded.
    
    BUG=
    
    Committed: https://crrev.com/6d41d67b254dface9f76ef66c88830ddfa92fd47 [formerly 11b95ec1381f463ebdfbddefc5583bcde52bf517]
    Cr-Commit-Position: refs/heads/master@{#336249}
    
    Review URL: https://codereview.chromium.org/1212503003
    
    Cr-Commit-Position: refs/heads/master@{#336296}
    
    Former-commit-id: 8815bf76a835e06ae6b8eb0916070e068aeae4f8

--- remoting/test/chromoting_test_driver.cc
+++ remoting/test/chromoting_test_driver.cc
@@ -5,13 +5,17 @@
 #include <string>
 
 #include "base/command_line.h"
+#include "base/files/file_path.h"
 #include "base/test/test_suite.h"
 #include "base/test/test_switches.h"
+#include "remoting/test/refresh_token_store.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace switches {
 const char kSingleProcessTestsSwitchName[] = "single-process-tests";
 const char kUserNameSwitchName[] = "username";
+const char kHostNameSwitchName[] = "hostname";
+const char kRefreshTokenPathSwitchName[] = "refresh-token-path";
 }
 
 int main(int argc, char* argv[]) {
@@ -28,16 +32,41 @@ int main(int argc, char* argv[]) {
   // To avoid shared resource contention, tests will be run one at a time.
   command_line->AppendSwitch(switches::kSingleProcessTestsSwitchName);
 
+  // The username is used to run the tests and determines which refresh token to
+  // select in the refresh token file.
   std::string username =
-    command_line->GetSwitchValueASCII(switches::kUserNameSwitchName);
+      command_line->GetSwitchValueASCII(switches::kUserNameSwitchName);
 
-  // Verify that the username is passed in as an argument.
   if (username.empty()) {
-    LOG(ERROR) << "No username passed in, can't authenticate without that!";
+    LOG(ERROR) << "No username passed in, can't authenticate or run tests!";
     return -1;
   }
   DVLOG(1) << "Running chromoting tests as: " << username;
 
+  base::FilePath refresh_token_path =
+     command_line->GetSwitchValuePath(switches::kRefreshTokenPathSwitchName);
+
+  // The hostname determines which host to initiate a session with from the list
+  // returned from the Directory service.
+  std::string hostname =
+      command_line->GetSwitchValueASCII(switches::kHostNameSwitchName);
+
+  if (hostname.empty()) {
+    LOG(ERROR) << "No hostname passed in, connect to host requires hostname!";
+    return -1;
+  }
+  DVLOG(1) << "Chromoting tests will connect to: " << hostname;
+
+  // TODO(TonyChun): Move this logic into a shared environment class.
+  scoped_ptr<remoting::test::RefreshTokenStore> refresh_token_store =
+      remoting::test::RefreshTokenStore::OnDisk(username, refresh_token_path);
+
+  std::string refresh_token = refresh_token_store->FetchRefreshToken();
+
+  if (refresh_token.empty()) {
+    // RefreshTokenStore already logs which specific error occured.
+    return -1;
+  }
+
   return 0;
 }
-
