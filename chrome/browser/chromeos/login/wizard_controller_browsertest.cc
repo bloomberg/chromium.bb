a015ad61d41b5d2d987e647e8d7e799843fd217b
kalman <kalman@chromium.org>
2015-06-26 11:20:18 -0700

    Disable WizardControllerProxyAuthOnSigninSuite/WizardControllerProxyAuthOnSigninTest.ProxyAuthDialogOnSigninScreen/1.
    BUG=504928
    R=grt@chromium.org
    TBR=dzhioev@chromium.org
    
    Review URL: https://codereview.chromium.org/1216683002
    
    Cr-Commit-Position: refs/heads/master@{#336415}
    
    Former-commit-id: 11f59773c7a94271d75ae03f4bade1729c7f1438

--- chrome/browser/chromeos/login/wizard_controller_browsertest.cc
+++ chrome/browser/chromeos/login/wizard_controller_browsertest.cc
@@ -1025,8 +1025,9 @@ class WizardControllerProxyAuthOnSigninTest
   DISALLOW_COPY_AND_ASSIGN(WizardControllerProxyAuthOnSigninTest);
 };
 
+// Disabled, see https://crbug.com/504928.
 IN_PROC_BROWSER_TEST_P(WizardControllerProxyAuthOnSigninTest,
-                       ProxyAuthDialogOnSigninScreen) {
+                       DISABLED_ProxyAuthDialogOnSigninScreen) {
   content::WindowedNotificationObserver auth_needed_waiter(
       chrome::NOTIFICATION_AUTH_NEEDED,
       content::NotificationService::AllSources());
