c1b3270de1997edc202607c5f3aef453ac733c9c
lanwei@chromium.org <lanwei@chromium.org>
2015-06-26 14:50:27 +0000

    Populates sourceDevice attribute into KeyboardEvent
    Since now we have InputDevice information, in this CL we populates sourceDevice
    attribute into KeyboardEvent, which has firesTouchEvents set to be false,
    because we treat all the keyboard events the same no matter they are from physical
    keyboard or virtual keyboard (it sends keyboard events to chromium not touch events).
    
    The Intent-to-Implement: UIEvent.sourceDevice & InputDevice.firesTouchEvents link:
    https://groups.google.com/a/chromium.org/forum/#!searchin/blink-dev/inputdevice/blink-dev/wjtZl9jpCpI/xC0JTRKgc9kJ 
    
    This CL is depended on Issue 1157173003, which creates the InputDevice class
    (https://codereview.chromium.org/1157173003/)
    and Issue 1160753005, which Implement UIEvent.sourceDevice
    (https://codereview.chromium.org/1160753005/)
    
    BUG=476530
    
    Review URL: https://codereview.chromium.org/1182313006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@197921 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 53e5a41782680871239e0e2b9594f76dab6ac508

--- third_party/WebKit/Source/core/events/KeyboardEvent.cpp
+++ third_party/WebKit/Source/core/events/KeyboardEvent.cpp
@@ -101,8 +101,7 @@ KeyboardEvent::KeyboardEvent()
 }
 
 KeyboardEvent::KeyboardEvent(const PlatformKeyboardEvent& key, AbstractView* view)
-    : UIEventWithKeyState(eventTypeForKeyboardEventType(key.type()),
-                          true, true, view, 0, key.ctrlKey(), key.altKey(), key.shiftKey(), key.metaKey())
+    : UIEventWithKeyState(eventTypeForKeyboardEventType(key.type()), true, true, view, 0, key.ctrlKey(), key.altKey(), key.shiftKey(), key.metaKey(), InputDevice::doesntFireTouchEventsInputDevice())
     , m_keyEvent(adoptPtr(new PlatformKeyboardEvent(key)))
     , m_keyIdentifier(key.keyIdentifier())
     , m_code(key.code())
@@ -114,7 +113,7 @@ KeyboardEvent::KeyboardEvent(const PlatformKeyboardEvent& key, AbstractView* vie
 }
 
 KeyboardEvent::KeyboardEvent(const AtomicString& eventType, const KeyboardEventInit& initializer)
-    : UIEventWithKeyState(eventType, initializer.bubbles(), initializer.cancelable(), initializer.view(), initializer.detail(), initializer.ctrlKey(), initializer.altKey(), initializer.shiftKey(), initializer.metaKey())
+    : UIEventWithKeyState(eventType, initializer.bubbles(), initializer.cancelable(), initializer.view(), initializer.detail(), initializer.ctrlKey(), initializer.altKey(), initializer.shiftKey(), initializer.metaKey(), initializer.sourceDevice())
     , m_keyIdentifier(initializer.keyIdentifier())
     , m_location(initializer.location())
     , m_isAutoRepeat(initializer.repeat())
@@ -123,7 +122,7 @@ KeyboardEvent::KeyboardEvent(const AtomicString& eventType, const KeyboardEventI
 
 KeyboardEvent::KeyboardEvent(const AtomicString& eventType, bool canBubble, bool cancelable, AbstractView *view,
     const String& keyIdentifier, const String& code, const String& key, unsigned location, bool ctrlKey, bool altKey, bool shiftKey, bool metaKey)
-    : UIEventWithKeyState(eventType, canBubble, cancelable, view, 0, ctrlKey, altKey, shiftKey, metaKey)
+    : UIEventWithKeyState(eventType, canBubble, cancelable, view, 0, ctrlKey, altKey, shiftKey, metaKey, InputDevice::doesntFireTouchEventsInputDevice())
     , m_keyIdentifier(keyIdentifier)
     , m_code(code)
     , m_key(key)
