dbb4c1b895b7197d1737bb00572cf2fcfbe1be9b
penghuang <penghuang@chromium.org>
2015-06-29 08:07:22 -0700

    Add texture coordinates and sizes in mojo YUVVideoQuadState.
    Video playback doesn't work in mandoline, because mojo YUVVideoQuadState
    doesn't have texture coordinates and sizes for cc::YUVVideoDrawQuad.
    This CL fixes this issue.
    
    BUG=504941
    
    Review URL: https://codereview.chromium.org/1209423003
    
    Cr-Commit-Position: refs/heads/master@{#336563}
    
    Former-commit-id: 4f06d6aa8a1a1f2db83e2df1414a59216f9d219d

--- mojo/converters/surfaces/surfaces_type_converters.cc
+++ mojo/converters/surfaces/surfaces_type_converters.cc
@@ -166,10 +166,10 @@ bool ConvertDrawQuad(const QuadPtr& input,
       yuv_quad->SetAll(
           sqs, input->rect.To<gfx::Rect>(), input->opaque_rect.To<gfx::Rect>(),
           input->visible_rect.To<gfx::Rect>(), input->needs_blending,
-          gfx::RectF(),  // TODO(sky): ya tex coord rect
-          gfx::RectF(),  // TODO(sky): uv tex coord rect
-          gfx::Size(),   // TODO(sky): ya texture size
-          gfx::Size(),   // TODO(sky): uv texture size
+          yuv_state->ya_tex_coord_rect.To<gfx::RectF>(),
+          yuv_state->uv_tex_coord_rect.To<gfx::RectF>(),
+          yuv_state->ya_tex_size.To<gfx::Size>(),
+          yuv_state->uv_tex_size.To<gfx::Size>(),
           yuv_state->y_plane_resource_id, yuv_state->u_plane_resource_id,
           yuv_state->v_plane_resource_id, yuv_state->a_plane_resource_id,
           static_cast<cc::YUVVideoDrawQuad::ColorSpace>(
@@ -319,10 +319,10 @@ QuadPtr TypeConverter<QuadPtr, cc::DrawQuad>::Convert(
       const cc::YUVVideoDrawQuad* yuv_quad =
           cc::YUVVideoDrawQuad::MaterialCast(&input);
       YUVVideoQuadStatePtr yuv_state = YUVVideoQuadState::New();
-      // TODO(sky): ya_tex_coord_rect
-      // TODO(sky): uv_tex_coord_rect
-      // TODO(sky): ya_texture_size
-      // TODO(sky): uv_texture_size
+      yuv_state->ya_tex_coord_rect = RectF::From(yuv_quad->ya_tex_coord_rect);
+      yuv_state->uv_tex_coord_rect = RectF::From(yuv_quad->uv_tex_coord_rect);
+      yuv_state->ya_tex_size = Size::From(yuv_quad->ya_tex_size);
+      yuv_state->uv_tex_size = Size::From(yuv_quad->uv_tex_size);
       yuv_state->y_plane_resource_id = yuv_quad->y_plane_resource_id();
       yuv_state->u_plane_resource_id = yuv_quad->u_plane_resource_id();
       yuv_state->v_plane_resource_id = yuv_quad->v_plane_resource_id();
