6ca186c21ac84ee8132e5ee696ecddbd36e4fa4f
tobiasjs <tobiasjs@chromium.org>
2015-06-30 02:50:07 -0700

    Add tests for EGL client extensions and extension bits
    Add tests to ensure that:
    
    a) Platform and client extensions are kept separate, and are both filtered.
    b) As well as filtering the extension strings returned by eglQueryString,
       the extension presence bits in DriverEGL::ext are correctly set.
    
    BUG=502353
    
    Review URL: https://codereview.chromium.org/1217873002
    
    Cr-Commit-Position: refs/heads/master@{#336755}
    
    Former-commit-id: 03962f9b02d62493eee010a233cd4b9949eaa5cc

--- ui/gl/egl_api_unittest.cc
+++ ui/gl/egl_api_unittest.cc
@@ -13,6 +13,7 @@ namespace gfx {
 class EGLApiTest : public testing::Test {
  public:
   void SetUp() override {
+    fake_client_extension_string_ = "";
     fake_extension_string_ = "";
 
     // TODO(dyen): Add a way to bind mock drivers for testing.
@@ -29,6 +30,7 @@ class EGLApiTest : public testing::Test {
     api_.reset(nullptr);
     g_driver_egl.ClearBindings();
 
+    fake_client_extension_string_ = "";
     fake_extension_string_ = "";
   }
 
@@ -39,10 +41,13 @@ class EGLApiTest : public testing::Test {
       api_->InitializeWithCommandLine(&g_driver_egl, command_line);
     else
       api_->Initialize(&g_driver_egl);
+    g_driver_egl.InitializeExtensionBindings();
   }
 
-  void SetFakeExtensionString(const char* fake_string) {
+  void SetFakeExtensionString(const char* fake_string,
+                              const char* fake_client_string) {
     fake_extension_string_ = fake_string;
+    fake_client_extension_string_ = fake_client_string;
   }
 
   static EGLBoolean GL_BINDING_CALL FakeInitialize(EGLDisplay display,
@@ -53,52 +58,83 @@ class EGLApiTest : public testing::Test {
 
   static const char* GL_BINDING_CALL FakeQueryString(EGLDisplay dpy,
                                                      EGLint name) {
-    return fake_extension_string_;
+    if (dpy == EGL_NO_DISPLAY) {
+      return fake_client_extension_string_;
+    } else {
+      return fake_extension_string_;
+    }
   }
 
   static EGLDisplay GL_BINDING_CALL FakeGetCurrentDisplay() {
-    return nullptr;
+    return reinterpret_cast<EGLDisplay>(0x1);
   }
 
   static EGLDisplay GL_BINDING_CALL FakeGetDisplay(
       EGLNativeDisplayType native_display) {
-    return EGL_NO_DISPLAY;
+    return reinterpret_cast<EGLDisplay>(0x1);
   }
 
   static EGLint GL_BINDING_CALL FakeGetError() {
     return EGL_SUCCESS;
   }
 
-  const char* GetExtensions() {
-    EGLDisplay display = api_->eglGetCurrentDisplayFn();
-    return api_->eglQueryStringFn(display, EGL_EXTENSIONS);
+  std::pair<const char*, const char*> GetExtensions() {
+    return std::make_pair(
+        api_->eglQueryStringFn(EGL_NO_DISPLAY, EGL_EXTENSIONS),
+        api_->eglQueryStringFn(api_->eglGetCurrentDisplayFn(), EGL_EXTENSIONS));
   }
 
  protected:
   static const char* fake_extension_string_;
+  static const char* fake_client_extension_string_;
 
   scoped_ptr<RealEGLApi> api_;
 };
 
 const char* EGLApiTest::fake_extension_string_ = "";
+const char* EGLApiTest::fake_client_extension_string_ = "";
 
-TEST_F(EGLApiTest, DisabledExtensionStringTest) {
-  static const char* kFakeExtensions = "EGL_EXT_1 EGL_EXT_2"
-                                       " EGL_EXT_3 EGL_EXT_4";
-  static const char* kFakeDisabledExtensions = "EGL_EXT_1,EGL_EXT_2,EGL_FAKE";
-  static const char* kFilteredExtensions = "EGL_EXT_3 EGL_EXT_4";
+TEST_F(EGLApiTest, DisabledExtensionBitTest) {
+  static const char* kFakeExtensions = "EGL_KHR_fence_sync";
+  static const char* kFakeClientExtensions = "";
+  static const char* kFakeDisabledExtensions = "EGL_KHR_fence_sync";
 
-  SetFakeExtensionString(kFakeExtensions);
+  SetFakeExtensionString(kFakeExtensions, kFakeClientExtensions);
   InitializeAPI(nullptr);
 
-  EXPECT_STREQ(kFakeExtensions, GetExtensions());
+  EXPECT_TRUE(g_driver_egl.ext.b_EGL_KHR_fence_sync);
 
   base::CommandLine command_line(base::CommandLine::NO_PROGRAM);
   command_line.AppendSwitchASCII(switches::kDisableGLExtensions,
                                  kFakeDisabledExtensions);
   InitializeAPI(&command_line);
 
-  EXPECT_STREQ(kFilteredExtensions, GetExtensions());
+  EXPECT_FALSE(g_driver_egl.ext.b_EGL_KHR_fence_sync);
+}
+
+TEST_F(EGLApiTest, DisabledExtensionStringTest) {
+  static const char* kFakeExtensions = "EGL_EXT_1 EGL_EXT_2"
+                                       " EGL_EXT_3 EGL_EXT_4";
+  static const char* kFakeClientExtensions =
+      "EGL_CLIENT_EXT_1 EGL_CLIENT_EXT_2";
+  static const char* kFakeDisabledExtensions =
+      "EGL_EXT_1,EGL_EXT_2,EGL_FAKE,EGL_CLIENT_EXT_1";
+  static const char* kFilteredExtensions = "EGL_EXT_3 EGL_EXT_4";
+  static const char* kFilteredClientExtensions = "EGL_CLIENT_EXT_2";
+
+  SetFakeExtensionString(kFakeExtensions, kFakeClientExtensions);
+  InitializeAPI(nullptr);
+
+  EXPECT_STREQ(kFakeClientExtensions, GetExtensions().first);
+  EXPECT_STREQ(kFakeExtensions, GetExtensions().second);
+
+  base::CommandLine command_line(base::CommandLine::NO_PROGRAM);
+  command_line.AppendSwitchASCII(switches::kDisableGLExtensions,
+                                 kFakeDisabledExtensions);
+  InitializeAPI(&command_line);
+
+  EXPECT_STREQ(kFilteredClientExtensions, GetExtensions().first);
+  EXPECT_STREQ(kFilteredExtensions, GetExtensions().second);
 }
 
 }  // namespace gfx
