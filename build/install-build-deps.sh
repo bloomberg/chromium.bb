a48f7ca23087eb826a1199824c7ebc7e2d05db57
abarth <abarth@chromium.org>
2015-06-30 16:24:39 -0700

    Add python-yaml to install-build-deps
    We'd like to use the |yaml| Python package to parse yaml files.
    
    R=cjhopman@chromium.org
    
    Review URL: https://codereview.chromium.org/1219623009
    
    Cr-Commit-Position: refs/heads/master@{#336913}
    
    Former-commit-id: df3a88fdd315c9fe3d7a65850d919e976dd2d6b2

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -109,8 +109,9 @@ dev_list="apache2.2-bin bison cdbs curl dpkg-dev elfutils devscripts fakeroot
           libwww-perl libxslt1-dev libxss-dev libxt-dev libxtst-dev openbox
           patch perl php5-cgi pkg-config python python-cherrypy3 python-crypto
           python-dev python-numpy python-opencv python-openssl python-psutil
-          rpm ruby subversion ttf-dejavu-core ttf-indic-fonts ttf-kochi-gothic
-          ttf-kochi-mincho wdiff xfonts-mathml zip $chromeos_dev_list"
+          python-yaml rpm ruby subversion ttf-dejavu-core ttf-indic-fonts
+          ttf-kochi-gothic ttf-kochi-mincho wdiff xfonts-mathml zip
+          $chromeos_dev_list"
 
 # 64-bit systems need a minimum set of 32-bit compat packages for the pre-built
 # NaCl binaries.
