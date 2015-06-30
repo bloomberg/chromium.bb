9a61c96e44ed2381f64e8b3cd563e6632776156f
juliusa <juliusa@google.com>
2015-06-30 16:41:14 -0700

    Parameterized GetSpellCheckLanguagesFromAcceptLanguages unit tests in spellcheck_service_unittest.cc
    BUG=5102
    
    Review URL: https://codereview.chromium.org/1211913004
    
    Cr-Commit-Position: refs/heads/master@{#336920}
    
    Former-commit-id: 86930bf1d005558e89667f27dd9eed2a47381e42

--- chrome/browser/spellchecker/spellcheck_service_unittest.cc
+++ chrome/browser/spellchecker/spellcheck_service_unittest.cc
@@ -4,21 +4,37 @@
 
 #include "chrome/browser/spellchecker/spellcheck_service.h"
 
-#include <algorithm>
-
 #include "base/memory/scoped_ptr.h"
+#include "base/strings/string_split.h"
 #include "chrome/browser/spellchecker/feedback_sender.h"
 #include "chrome/browser/spellchecker/spellcheck_factory.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+struct SpellcheckLanguageTestCase {
+  SpellcheckLanguageTestCase(const std::string& unsplit_accept_languages,
+                             const std::string& spellcheck_dictionary,
+                             const std::string& unsplit_expected_languages)
+      : spellcheck_dictionary(spellcheck_dictionary) {
+    base::SplitString(unsplit_accept_languages, ',', &accept_languages);
+    base::SplitString(unsplit_expected_languages, ',',
+                      &expected_spellcheck_languages);
+  }
+  ~SpellcheckLanguageTestCase() {}
+
+  std::vector<std::string> accept_languages;
+  std::string spellcheck_dictionary;
+  std::vector<std::string> expected_spellcheck_languages;
+};
+
 static scoped_ptr<KeyedService> BuildSpellcheckService(
     content::BrowserContext* profile) {
   return make_scoped_ptr(new SpellcheckService(static_cast<Profile*>(profile)));
 }
 
-class SpellcheckServiceTest : public testing::Test {
+class SpellcheckServiceUnitTest
+    : public testing::TestWithParam<SpellcheckLanguageTestCase> {
  protected:
   void SetUp() override {
     // Use SetTestingFactoryAndUse to force creation and initialization.
@@ -31,77 +47,22 @@ class SpellcheckServiceTest : public testing::Test {
   TestingProfile profile_;
 };
 
-TEST_F(SpellcheckServiceTest, GetSpellCheckLanguages1) {
-  std::vector<std::string> accept_languages;
-  accept_languages.push_back("en");
-  accept_languages.push_back("en-US");
+INSTANTIATE_TEST_CASE_P(
+    SpellcheckLanguageTestCases,
+    SpellcheckServiceUnitTest,
+    testing::Values(
+        SpellcheckLanguageTestCase("en,en-US", "en-US", "en-US"),
+        SpellcheckLanguageTestCase("en-US,en", "en-US", "en-US"),
+        SpellcheckLanguageTestCase("en,en-US,en-AU", "en-US", "en-US,en-AU"),
+        SpellcheckLanguageTestCase("en,en-US,fr", "en-US", "en-US,fr"),
+        SpellcheckLanguageTestCase("en,en-JP,fr,aa", "fr", "fr")));
+
+TEST_P(SpellcheckServiceUnitTest, GetSpellcheckLanguages) {
   std::vector<std::string> languages;
 
   SpellcheckService::GetSpellCheckLanguagesFromAcceptLanguages(
-      accept_languages, "en-US", &languages);
+      GetParam().accept_languages, GetParam().spellcheck_dictionary,
+      &languages);
 
-  EXPECT_EQ(1U, languages.size());
-  EXPECT_EQ("en-US", languages[0]);
-}
-
-TEST_F(SpellcheckServiceTest, GetSpellCheckLanguages2) {
-  std::vector<std::string> accept_languages;
-  accept_languages.push_back("en-US");
-  accept_languages.push_back("en");
-  std::vector<std::string> languages;
-
-  SpellcheckService::GetSpellCheckLanguagesFromAcceptLanguages(
-      accept_languages, "en-US", &languages);
-
-  EXPECT_EQ(1U, languages.size());
-  EXPECT_EQ("en-US", languages[0]);
-}
-
-TEST_F(SpellcheckServiceTest, GetSpellCheckLanguages3) {
-  std::vector<std::string> accept_languages;
-  accept_languages.push_back("en");
-  accept_languages.push_back("en-US");
-  accept_languages.push_back("en-AU");
-  std::vector<std::string> languages;
-
-  SpellcheckService::GetSpellCheckLanguagesFromAcceptLanguages(
-      accept_languages, "en-US", &languages);
-
-  EXPECT_EQ(2U, languages.size());
-
-  std::sort(languages.begin(), languages.end());
-  EXPECT_EQ("en-AU", languages[0]);
-  EXPECT_EQ("en-US", languages[1]);
-}
-
-TEST_F(SpellcheckServiceTest, GetSpellCheckLanguages4) {
-  std::vector<std::string> accept_languages;
-  accept_languages.push_back("en");
-  accept_languages.push_back("en-US");
-  accept_languages.push_back("fr");
-  std::vector<std::string> languages;
-
-  SpellcheckService::GetSpellCheckLanguagesFromAcceptLanguages(
-      accept_languages, "en-US", &languages);
-
-  EXPECT_EQ(2U, languages.size());
-
-  std::sort(languages.begin(), languages.end());
-  EXPECT_EQ("en-US", languages[0]);
-  EXPECT_EQ("fr", languages[1]);
-}
-
-TEST_F(SpellcheckServiceTest, GetSpellCheckLanguages5) {
-  std::vector<std::string> accept_languages;
-  accept_languages.push_back("en");
-  accept_languages.push_back("en-JP");  // Will not exist.
-  accept_languages.push_back("fr");
-  accept_languages.push_back("aa");  // Will not exist.
-  std::vector<std::string> languages;
-
-  SpellcheckService::GetSpellCheckLanguagesFromAcceptLanguages(
-      accept_languages, "fr", &languages);
-
-  EXPECT_EQ(1U, languages.size());
-  EXPECT_EQ("fr", languages[0]);
+  EXPECT_EQ(GetParam().expected_spellcheck_languages, languages);
 }
