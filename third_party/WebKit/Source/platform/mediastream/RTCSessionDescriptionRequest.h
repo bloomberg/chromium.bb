087bd41da0399d037b252c32b886a5ad0fae52e6
haraken@chromium.org <haraken@chromium.org>
2015-07-07 03:22:31 +0000

    Oilpan: Eagerly finalize RTCSessionDescriptionRequest
    RTCSessionDescriptionRequest::m_extraData must be eagerly finalized.
    Otherwise, it can happen that the RTCSessionDescriptionRequest is accessed
    after the RTCSessionDescriptionRequest gets destructed in an early stage
    of the lazy sweeping.
    
    BUG=507307
    
    Review URL: https://codereview.chromium.org/1227533004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198357 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2091ea758987dda8561efb3498551600e91cc89a

--- third_party/WebKit/Source/platform/mediastream/RTCSessionDescriptionRequest.h
+++ third_party/WebKit/Source/platform/mediastream/RTCSessionDescriptionRequest.h
@@ -53,6 +53,8 @@ public:
     ExtraData* extraData() const { return m_extraData.get(); }
     void setExtraData(PassOwnPtr<ExtraData> extraData) { m_extraData = extraData; }
 
+    // Need to eagerly finalize ExtraData.
+    EAGERLY_FINALIZE();
     DEFINE_INLINE_VIRTUAL_TRACE() { }
 
 protected:
