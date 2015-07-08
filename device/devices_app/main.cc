5a6ee72f2269f5a7c8ae459b311fe1efa3f66dba
rockot <rockot@chromium.org>
2015-07-07 18:57:26 -0700

    Reland: Add a public cpp targets for devices_app
    This exposes the bare minimum stuff necessary to build a devices
    app delegate, hiding the impl details behind a private
    dependency.
    
    Reland corrects a missing include in the app's main.cc.
    
    This is a reland of the reland! The reland was reverted due
    to suspicion of cast_shell_linux breakage, but that was
    caused by something else.
    
    BUG=None
    TBR=reillyg@chromium.org,sky@chromium.org
    
    Review URL: https://codereview.chromium.org/1213613018
    
    Cr-Commit-Position: refs/heads/master@{#337725}
    
    Former-commit-id: 50dc5691dca512231732f98043cd181edece239a

--- device/devices_app/main.cc
+++ device/devices_app/main.cc
@@ -3,12 +3,13 @@
 // found in the LICENSE file.
 
 #include "base/sequenced_task_runner.h"
-#include "device/devices_app/devices_app.h"
+#include "device/devices_app/public/cpp/devices_app_factory.h"
+#include "mojo/application/public/cpp/application_delegate.h"
 #include "mojo/application/public/cpp/application_runner.h"
 #include "third_party/mojo/src/mojo/public/c/system/main.h"
 
 MojoResult MojoMain(MojoHandle shell_handle) {
   mojo::ApplicationRunner runner(
-      device::DevicesApp::CreateDelegate(nullptr).release());
+      device::DevicesAppFactory::CreateApp(nullptr).release());
   return runner.Run(shell_handle);
 }
