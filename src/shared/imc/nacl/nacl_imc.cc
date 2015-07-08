3955f9295246056b200407d5d5a0524ac0dcf883
mseaborn <mseaborn@chromium.org>
2015-07-08 14:58:00 -0700

    Remove unused untrusted implementations of NaClSendDatagram() and others
    In trusted code, NaClSendDatagram() and NaClReceiveDatagram() are part
    of the internal layers of IMC, on top of which higher-level NaClDesc
    passing is implemented.  It does not make sense to have untrusted
    implementations of these, and they are not used anyway.  Their
    presence was confusing to someone new to this code (see
    https://groups.google.com/d/msg/native-client-discuss/5B5LFptZTyM/fI48VK4PDhUJ).
    
    We can also remove some other unused definitions from this file.
    
    BUG=none
    TEST=build
    
    Review URL: https://codereview.chromium.org/1210053007
    
    
    Former-commit-id: b1a5d2159561eaffcc24151d0ac7cf39cf7d37ba

--- src/shared/imc/nacl/nacl_imc.cc
+++ src/shared/imc/nacl/nacl_imc.cc
@@ -12,32 +12,8 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
-#include <algorithm>
-
-#include "native_client/src/shared/imc/nacl_imc_c.h"
-
-#if defined(__native_client__)
 #include "native_client/src/public/imc_syscalls.h"
-typedef NaClAbiNaClImcMsgHdr NaClImcMsgHdr;
-#endif
-
-/* Duplicate a NaCl file descriptor. */
-NaClHandle NaClDuplicateNaClHandle(NaClHandle handle) {
-  return dup(handle);
-}
-
-int NaClWouldBlock() {
-  return (errno == EAGAIN) ? 1 : 0;
-}
-
-NaClHandle NaClBoundSocket(const NaClSocketAddress* address) {
-  /*
-   * TODO(shiki): Switch to the following once the make_bound_sock() prototype
-   *              is cleaned up.
-   * return make_bound_sock(address);
-   */
-  return -1;
-}
+#include "native_client/src/shared/imc/nacl_imc_c.h"
 
 int NaClSocketPair(NaClHandle pair[2]) {
   return imc_socketpair(pair);
@@ -47,52 +23,9 @@ int NaClClose(NaClHandle handle) {
   return close(handle);
 }
 
-int NaClSendDatagram(NaClHandle handle, const NaClMessageHeader* message,
-                     int flags) {
-  return imc_sendmsg(handle, (const NaClImcMsgHdr *) message,
-                     flags);
-}
-
-int NaClSendDatagramTo(const NaClMessageHeader* message, int flags,
-                       const NaClSocketAddress* name) {
-  return -1;  /* TODO(bsy): how to implement this for NaCl? */
-}
-
-int NaClReceiveDatagram(NaClHandle handle, NaClMessageHeader* message,
-                        int flags) {
-  return imc_recvmsg(handle, (NaClImcMsgHdr *) message, flags);
-}
-
 NaClHandle NaClCreateMemoryObject(size_t length, int executable) {
   if (executable) {
     return -1;  /* Will never work with NaCl and should never be invoked. */
   }
   return imc_mem_obj_create(length);
-}
-
-void* NaClMap(struct NaClDescEffector* effp,
-              void* start, size_t length, int prot, int flags,
-              NaClHandle memory, off_t offset) {
-  static int posix_prot[4] = {
-    PROT_NONE,
-    PROT_READ,
-    PROT_WRITE,
-    PROT_READ | PROT_WRITE
-  };
-  int adjusted = 0;
-
-  if (flags & NACL_MAP_SHARED) {
-    adjusted |= MAP_SHARED;
-  }
-  if (flags & NACL_MAP_PRIVATE) {
-    adjusted |= MAP_PRIVATE;
-  }
-  if (flags & NACL_MAP_FIXED) {
-    adjusted |= MAP_FIXED;
-  }
-  return mmap(start, length, posix_prot[prot & 3], adjusted, memory, offset);
-}
-
-int NaClUnmap(void* start, size_t length) {
-  return munmap(start, length);
 }
