8f9a6843ae5c4d6b0b0f09ef81eeb62372ceeb21
esprehn <esprehn@chromium.org>
2015-07-08 15:08:55 -0700

    Don't put every <img> in the page in a WebVector in AwRenderViewExt::OnDocumentHasImagesRequest.
    This method was doing:
    
      blink::WebVector<blink::WebElement> images;
      webview->mainFrame()->document().images(images);
      hasImages = !images.isEmpty();
    
    which meant Blink would walk the entire document putting every <img>
    into a Vector, then it would copy that Vector into this WebVector all
    so this code could check if the Vector wasn't empty.
    
    Instead just use the NodeList version of getElementsByHTMLTagName,
    which we can already just call in this file with GetImgChild. This
    only walks until it finds the first <img> and doesn't need additional
    storage.
    
    Review URL: https://codereview.chromium.org/1220333011
    
    Cr-Commit-Position: refs/heads/master@{#337920}
    
    Former-commit-id: fa3437e3e8a3b37180709dab2852951c9f0ed797

--- android_webview/renderer/aw_render_view_ext.cc
+++ android_webview/renderer/aw_render_view_ext.cc
@@ -54,12 +54,11 @@ GURL GetAbsoluteSrcUrl(const blink::WebElement& element) {
   return GetAbsoluteUrl(element, element.getAttribute("src"));
 }
 
-blink::WebElement GetImgChild(const blink::WebElement& element) {
+blink::WebElement GetImgChild(const blink::WebNode& node) {
   // This implementation is incomplete (for example if is an area tag) but
   // matches the original WebViewClassic implementation.
 
-  blink::WebElementCollection collection =
-      element.getElementsByHTMLTagName("img");
+  blink::WebElementCollection collection = node.getElementsByHTMLTagName("img");
   DCHECK(!collection.isNull());
   return collection.firstItem();
 }
@@ -179,9 +178,9 @@ void AwRenderViewExt::OnDocumentHasImagesRequest(int id) {
   if (render_view()) {
     blink::WebView* webview = render_view()->GetWebView();
     if (webview) {
-      blink::WebVector<blink::WebElement> images;
-      webview->mainFrame()->document().images(images);
-      hasImages = !images.isEmpty();
+      blink::WebDocument document = webview->mainFrame()->document();
+      const blink::WebElement child_img = GetImgChild(document);
+      hasImages = !child_img.isNull();
     }
   }
   Send(new AwViewHostMsg_DocumentHasImagesResponse(routing_id(), id,
