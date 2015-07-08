c1cf65520bc191f18637e00e0887f631e6289e01
penghuang <penghuang@chromium.org>
2015-07-08 12:10:31 -0700

    Use a different User-Agent for Android build.
    It is hard to use desktop version website to debug html_viewer on Andoid,
    so change the User-Agent to get the mobile version website on Android.
    
    BUG=None
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/1213403003
    
    Cr-Commit-Position: refs/heads/master@{#337874}
    
    Former-commit-id: e9f3b858148ac4b1bde0e06698861cae7c6cd7eb

--- mojo/common/user_agent.cc
+++ mojo/common/user_agent.cc
@@ -4,13 +4,21 @@
 
 #include "mojo/common/user_agent.h"
 
+#include "build/build_config.h"
+
 namespace mojo {
 namespace common {
 
 std::string GetUserAgent() {
   // TODO(jam): change depending on OS
+#if defined(OS_ANDROID)
+  return "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 7 Build/LMY48G) "
+         "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.68 "
+         "Safari/537.36";
+#else
   return "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like "
          "Gecko) Chrome/42.0.2311.68 Safari/537.36";
+#endif
 }
 
 }  // namespace common
