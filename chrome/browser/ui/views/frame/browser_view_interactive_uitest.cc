91b167651ec90e83a1506263e9c2f36e49225873
varkha <varkha@chromium.org>
2015-07-09 15:18:28 -0700

    [linux_aura] Enables BrowserViewTest.FullscreenClearsFocus on desktop Linux
    This test now passes on desktop Linux Aura. Also cleans up includes.
    
    BUG=163931
    
    Review URL: https://codereview.chromium.org/1230583009
    
    Cr-Commit-Position: refs/heads/master@{#338161}
    
    Former-commit-id: 2b550c2b815c9001e562b0d7ba5fa7959342ea87

--- chrome/browser/ui/views/frame/browser_view_interactive_uitest.cc
+++ chrome/browser/ui/views/frame/browser_view_interactive_uitest.cc
@@ -1,43 +1,20 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "chrome/browser/ui/views/frame/browser_view.h"
 
-#include "chrome/app/chrome_command_ids.h"
-#include "chrome/browser/search/search.h"
 #include "chrome/browser/ui/browser_commands.h"
-#include "chrome/browser/ui/omnibox/omnibox_view.h"
-#include "chrome/browser/ui/search/instant_test_utils.h"
-#include "chrome/browser/ui/tabs/tab_strip_model.h"
-#include "chrome/browser/ui/views/bookmarks/bookmark_bar_view.h"
-#include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
-#include "chrome/browser/ui/views/frame/top_container_view.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
-#include "chrome/browser/ui/views/tabs/tab.h"
-#include "chrome/browser/ui/views/toolbar/toolbar_view.h"
-#include "chrome/common/instant_types.h"
-#include "chrome/common/url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
-#include "components/omnibox/browser/autocomplete_controller.h"
-#include "ui/views/controls/webview/webview.h"
 #include "ui/views/focus/focus_manager.h"
-#include "ui/views/window/non_client_view.h"
-
-#if defined(OS_CHROMEOS)
-#include "chrome/browser/chrome_notification_types.h"
-#include "content/public/browser/notification_service.h"
-#include "ui/compositor/layer.h"
-#include "ui/compositor/scoped_animation_duration_scale_mode.h"
-#endif
 
 using views::FocusManager;
 
 typedef InProcessBrowserTest BrowserViewTest;
 
 // Active window and focus testing is not reliable on Windows crbug.com/79493
-// TODO(linux_aura) http://crbug.com/163931
-#if defined(OS_WIN) || (defined(OS_LINUX) && !defined(OS_CHROMEOS) && defined(USE_AURA))
+#if defined(OS_WIN)
 #define MAYBE_FullscreenClearsFocus DISABLED_FullscreenClearsFocus
 #else
 #define MAYBE_FullscreenClearsFocus FullscreenClearsFocus
