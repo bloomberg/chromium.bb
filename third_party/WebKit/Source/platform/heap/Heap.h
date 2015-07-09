22288f381067c5c617ec0462cd8a2ea46ac91b47
haraken@chromium.org <haraken@chromium.org>
2015-07-09 13:49:22 +0000

    Oilpan: Freed memory region should be always poisoned
    ASAN_POISON should be called for an address range zapped with FILL_ZERO_IF_PRODUCTION.
    ASAN_UNPOISON should be called for an address range unzapped with FILL_ZERO_IF_NOT_PRODUCTION.
    This invariant is currently broken, and some address ranges are left without poisoned.
    This CL guarantees the invariant.
    
    BUG=480837
    
    Review URL: https://codereview.chromium.org/1224253002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198602 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 4f1aa5418d9674f4e370b977b765444e1d24f359

--- third_party/WebKit/Source/platform/heap/Heap.h
+++ third_party/WebKit/Source/platform/heap/Heap.h
@@ -82,8 +82,12 @@ const uint8_t orphanedZapValue = 240;
 // In production builds, memory is not zapped (for performance). The memory
 // is just zeroed out when it is added to the free list.
 #if ENABLE(ASSERT) || defined(LEAK_SANITIZER) || defined(ADDRESS_SANITIZER)
-#define FILL_ZERO_IF_PRODUCTION(address, size) FreeList::zapFreedMemory(address, size)
-#define FILL_ZERO_IF_NOT_PRODUCTION(address, size) memset((address), 0, (size))
+#define FILL_ZERO_IF_PRODUCTION(address, size) \
+    FreeList::zapFreedMemory(address, size);   \
+    ASAN_POISON_MEMORY_REGION(address, size)
+#define FILL_ZERO_IF_NOT_PRODUCTION(address, size) \
+    memset((address), 0, (size));                  \
+    ASAN_UNPOISON_MEMORY_REGION(address, size)
 #else
 #define FILL_ZERO_IF_PRODUCTION(address, size) memset((address), 0, (size))
 #define FILL_ZERO_IF_NOT_PRODUCTION(address, size) do { } while (false)
@@ -1317,7 +1321,6 @@ inline Address NormalPageHeap::allocateObject(size_t allocationSize, size_t gcIn
         ASSERT(!(reinterpret_cast<uintptr_t>(result) & allocationMask));
 
         // Unpoison the memory used for the object (payload).
-        ASAN_UNPOISON_MEMORY_REGION(result, allocationSize - sizeof(HeapObjectHeader));
         FILL_ZERO_IF_NOT_PRODUCTION(result, allocationSize - sizeof(HeapObjectHeader));
         ASSERT(findPageFromAddress(headerAddress + allocationSize - 1));
         return result;
