671e2ba507880b74094d4cc5213c1dd7bbc9cd89
reed <reed@google.com>
2015-07-10 11:46:55 -0700

    add SK_SUPPORT_LEGACY_ONDRAWIMAGERECT
    Needed so we can land this skia CL
    https://codereview.chromium.org/1228083004#
    
    Review URL: https://codereview.chromium.org/1232633003
    
    Cr-Commit-Position: refs/heads/master@{#338329}
    
    Former-commit-id: c6a411f211bb44986ce1cae6a027d8786891d809

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -253,6 +253,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_IGNORE_ETC1_SUPPORT
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_ONDRAWIMAGERECT
+#   define SK_SUPPORT_LEGACY_ONDRAWIMAGERECT
+#endif
+
 #ifndef    SK_IGNORE_GPU_DITHER
 #   define SK_IGNORE_GPU_DITHER
 #endif
