086f0d4b0d95ed6acd569f37b4cf93c05f9492ef
pdr@chromium.org <pdr@chromium.org>
2015-07-10 16:32:35 +0000

    Prevent reentrant plugin container crash by restricting element access
    PPAPI plugins can synchronously check isRectTopmost() from within the
    plugin container's dispose() function. This could crash because
    the reentrant call could access the the plugin container's containing
    document which may no longer be valid. This patch clears m_element
    before destroying the plugin which stops reentrant access.
    
    A test has been added that matches the PPAPI crash stacks we have seen
    from user reports.
    
    BUG=508592,493375
    
    Review URL: https://codereview.chromium.org/1232053002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198694 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: f927348ff202a1e55bb292a6fad6d3634573118d

--- third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
+++ third_party/WebKit/Source/web/WebPluginContainerImpl.cpp
@@ -316,10 +316,8 @@ void WebPluginContainerImpl::setWebLayer(WebLayer* layer)
     if (!needsCompositingUpdate)
         return;
 
-#if ENABLE(OILPAN)
     if (!m_element)
         return;
-#endif
 
     m_element->setNeedsCompositingUpdate();
     // Being composited or not affects the self painting layer bit
@@ -727,6 +725,11 @@ void WebPluginContainerImpl::dispose()
 
     for (size_t i = 0; i < m_pluginLoadObservers.size(); ++i)
         m_pluginLoadObservers[i]->clearPluginContainer();
+
+    // Clear m_element before destroying the plugin to ensure re-entrant callbacks
+    // do not have access to the element state.
+    m_element = nullptr;
+
     m_webPlugin->destroy();
     m_webPlugin = nullptr;
 
@@ -734,7 +737,6 @@ void WebPluginContainerImpl::dispose()
         GraphicsLayer::unregisterContentsLayer(m_webLayer);
 
     m_pluginLoadObservers.clear();
-    m_element = nullptr;
 }
 
 #if ENABLE(OILPAN)
