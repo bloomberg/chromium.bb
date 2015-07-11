eac5a9b45106c97d48fe7f828f55e712def43af4
mkwst@chromium.org <mkwst@chromium.org>
2015-07-11 10:33:31 +0000

    Deprecate 'url::Origin' (2/3).
    See patch #1 for a detailed explanation.
    
    Patch 1 (Chromium): https://codereview.chromium.org/1227173003
    Patch 2 (Blink): This patch.
    Patch 3 (Chromium): https://codereview.chromium.org/1228713006
    
    BUG=490074
    
    Review URL: https://codereview.chromium.org/1227993002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198732 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 447fb416e670b9de3e46879c0aac0e7dd308358b

--- third_party/WebKit/public/platform/WebSerializedOrigin.h
+++ third_party/WebKit/public/platform/WebSerializedOrigin.h
@@ -8,7 +8,7 @@
 #include "WebString.h"
 
 #if !INSIDE_BLINK
-#include <url/origin.h>
+#include <url/deprecated_serialized_origin.h>
 #endif
 
 namespace blink {
@@ -25,8 +25,8 @@ public:
 #if INSIDE_BLINK
     BLINK_PLATFORM_EXPORT WebSerializedOrigin(const SecurityOrigin&);
 #else
-    WebSerializedOrigin(const url::Origin& origin) : m_string(WebString::fromUTF8(origin.string())) { }
-    operator url::Origin() const { return url::Origin(m_string.utf8()); }
+    WebSerializedOrigin(const url::DeprecatedSerializedOrigin& origin) : m_string(WebString::fromUTF8(origin.string())) { }
+    operator url::DeprecatedSerializedOrigin() const { return url::DeprecatedSerializedOrigin(m_string.utf8()); }
 #endif
 
     const WebString& string() const
