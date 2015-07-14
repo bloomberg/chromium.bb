abab185b56d1529418e43a99f888a6cd917ded7d
Christian Egli <christian.egli@sbs.ch>
2015-07-14 11:14:47 +0200

    Show failed translations in utf8
    i.e. display the failure message properly even if expected is utf8.

--- tests/brl_checks.c
+++ tests/brl_checks.c
@@ -20,12 +20,132 @@ print_int_array(const char *prefix, int *pos_list, int len)
   printf("\n");
 }
 
+#define UTF8_BUFSIZE 32
+#define UNICODE_SURROGATE_PAIR -1
+#define UNICODE_BAD_INPUT -1
+
+/* Input: a Unicode code point, "ucs".
+
+   Output: UTF-8 characters in buffer "utf8".
+
+   Return value: the number of bytes written into "utf8", or -1 if
+   there was an error.
+
+   This adds a zero byte to the end of the string. It assumes that the
+   buffer "utf8" has at least seven bytes of space to write to. */
+
+/* from http://std.dkuug.dk/jtc1/sc2/WG2/docs/n1335.html
+
+R.4 Mapping from UCS-4 form to UTF-8 form
+
+Table 4 defines in mathematical notation the mapping from the UCS-4
+coded representation form to the UTF-8 coded representation form.
+
+In the left column (UCS-4) the notation x indicates the four-octet
+coded representation of a single character of the UCS. In the right
+column (UTF-8) x indicates the corresponding integer value.
+
+NOTE 3 - Values of x in the range 0000 D800 .. 0000 DFFF are reserved
+for the UTF-16 form and do not occur in UCS-4. The values 0000 FFFE
+and 0000 FFFF also do not occur (see clause 8). The mappings of these
+code positions in UTF-8 are undefined.
+
+NOTE 4 - The algorithm for converting from UCS-4 to UTF-8 can be
+summarised as follows.
+
+For each coded character in UCS-4 the length of octet sequence in
+UTF-8 is determined by the entry in the right column of Table 1. The
+bits in the UCS-4 coded representation, starting from the least
+significant bit, are then distributed across the free bit positions in
+order of increasing significance until no more free bit positions are
+available.
+
+Table 4 - Mapping from UCS-4 to UTF-8
+
+Range of values			Sequence of
+in UCS-4  			octets in UTF-8
+
+x = 0000 0000 .. 0000 007F;	x;
+x = 0000 0080 .. 0000 07FF;	C0 + x/2**6;
+				80 + x%2**6;
+x = 0000 0800 .. 0000 FFFF;	E0 + x/2**12;
+	(see Note 3)		80 + x/2**6%2**6;
+				80 + x%2**6;
+x = 0001 0000 .. 001F FFFF;	F0 + x/2**18;
+				80 + x/2**12%2**6;
+				80 + x/2**6%2**6;
+				80 + x%2**6;
+x = 0020 0000 .. 03FF FFFF;	F8 + x/2**24;
+				80 + x/2**18%2**6;
+				80 + x/2**12%2**6;
+				80 + x/2**6%2**6;
+				80 + x%2**6;
+x = 0400 0000 .. 7FFF FFFF;	FC + x/2**30;
+				80 + x/2**24%2**6;
+				80 + x/2**18%2**6;
+				80 + x/2**12%2**6;
+				80 + x/2**6%2**6;
+				80 + x%2**6; */
+
+int ucs_to_utf8 (widechar ucs, unsigned char * utf8)
+{
+    if (ucs < 0x80) {
+      utf8[0] = ucs;
+      utf8[1] = '\0';
+      return 1;
+    } else if (ucs < 0x800) {
+      utf8[0] = (ucs >> 6)   | 0xC0;
+      utf8[1] = (ucs & 0x3F) | 0x80;
+      utf8[2] = '\0';
+      return 2;
+    } else if (ucs < 0xFFFF) {
+      if (ucs >= 0xD800 && ucs <= 0xDFFF) {
+	/* Ill-formed. */
+	return UNICODE_SURROGATE_PAIR;
+      }
+      utf8[0] = ((ucs >> 12)       ) | 0xE0;
+      utf8[1] = ((ucs >> 6 ) & 0x3F) | 0x80;
+      utf8[2] = ((ucs      ) & 0x3F) | 0x80;
+      utf8[3] = '\0';
+      return 3;
+    } else if (ucs < 0x1FFFFF) {
+      utf8[0] = 0xF0 | ((ucs >> 18));
+      utf8[1] = 0x80 | ((ucs >> 12) & 0x3F);
+      utf8[2] = 0x80 | ((ucs >> 6) & 0x3F);
+      utf8[3] = 0x80 | ((ucs & 0x3F));
+        utf8[4] = '\0';
+        return 4;
+    } else if (ucs < 0x3FFFFFF) {
+      utf8[0] = 0xF0 | ((ucs >> 24));
+      utf8[1] = 0x80 | ((ucs >> 18) & 0x3F);
+      utf8[2] = 0x80 | ((ucs >> 12) & 0x3F);
+      utf8[3] = 0x80 | ((ucs >> 6) & 0x3F);
+      utf8[4] = 0x80 | ((ucs & 0x3F));
+      utf8[5] = '\0';
+      return 5;
+    } else if (ucs < 0x7FFFFFFF) {
+      utf8[0] = 0xF0 | ((ucs >> 30));
+      utf8[1] = 0x80 | ((ucs >> 24) & 0x3F);
+      utf8[2] = 0x80 | ((ucs >> 18) & 0x3F);
+      utf8[3] = 0x80 | ((ucs >> 12) & 0x3F);
+      utf8[4] = 0x80 | ((ucs >> 6) & 0x3F);
+      utf8[5] = 0x80 | ((ucs & 0x3F));
+      utf8[6] = '\0';
+      return 6;
+    }
+    return UNICODE_BAD_INPUT;
+}
+
 void
 print_widechars(widechar * buf, int len)
 {
   int i;
-  for (i = 0; i < len; i++)
-    printf("%c", buf[i]);
+  unsigned char utf8[UTF8_BUFSIZE];
+
+  for (i = 0; i < len; i++) {
+    ucs_to_utf8(buf[i], utf8);
+    printf("%s", utf8);
+  }
 }
 
 /* Helper function to convert a typeform string of '0's, '1's, '2's etc.
@@ -133,24 +253,34 @@ int check_with_mode(
       rv = 1;
       outbuf[outlen] = 0;
       printf("Input: '%s'\n", str);
-      printf("Expected: '%s'\n", expected);
+      printf("Expected: '%s' (length %d)\n", expected, expectedlen);
       printf("Received: '");
       print_widechars(outbuf, outlen);
-      printf("'\n");
+      printf("' (length %d)\n", outlen);
+
       if (i < outlen && i < expectedlen) 
 	{
-	  printf("Diff: Expected '%c' but recieved '%c' in index %d\n",
-		 expectedbuf[i], outbuf[i], i);
+	  unsigned char expected_utf8[UTF8_BUFSIZE];
+	  unsigned char out_utf8[UTF8_BUFSIZE];
+
+	  ucs_to_utf8(expectedbuf[i], expected_utf8);
+	  ucs_to_utf8(outbuf[i], out_utf8);
+	  printf("Diff: Expected '%s' but received '%s' in index %d\n",
+		 expected_utf8, out_utf8, i);
 	}
       else if (i < expectedlen)
 	{
-	  printf("Diff: Expected '%c' but recieved nothing in index %d\n",
-		 expectedbuf[i], i);
+	  unsigned char expected_utf8[UTF8_BUFSIZE];
+	  ucs_to_utf8(expectedbuf[i], expected_utf8);
+	  printf("Diff: Expected '%s' but received nothing in index %d\n",
+		 expected_utf8, i);
 	}
       else 
 	{
-	  printf("Diff: Expected nothing but recieved '%c' in index %d\n",
-		  outbuf[i], i);
+	  unsigned char out_utf8[UTF8_BUFSIZE];
+	  ucs_to_utf8(outbuf[i], out_utf8);
+	  printf("Diff: Expected nothing but received '%s' in index %d\n",
+		 out_utf8, i);
 	}
     }
 
