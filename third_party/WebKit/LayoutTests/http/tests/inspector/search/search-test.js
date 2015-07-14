56e4c3b43375d531b10b43457d1e54b8dc4d7528
lushnikov@chromium.org <lushnikov@chromium.org>
2015-07-14 13:32:34 +0000

    DevTools: split test http/tests/inspector/search/source-frame-replace.html
    The source-frame-replace.html occasionally timeouts. The patch
    splits it into 4.
    
    BUG=508162
    R=pfeldman, sergeyv
    
    Review URL: https://codereview.chromium.org/1232003006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@198860 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 066e25021ecef011f4ea9b1a3de17cca1b6624fc

--- third_party/WebKit/LayoutTests/http/tests/inspector/search/search-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/search/search-test.js
@@ -60,4 +60,60 @@ InspectorTest.runSearchAndDumpResults = function(scope, searchConfig, sortByURI,
     }
 }
 
+InspectorTest.replaceAndDumpChange = function(sourceFrame, searchConfig, replacement, replaceAll)
+{
+    var modifiers = [];
+    if (searchConfig.isRegex)
+        modifiers.push("regex");
+    if (searchConfig.caseSensitive)
+        modifiers.push("caseSensitive");
+    if (replaceAll)
+        modifiers.push("replaceAll");
+    var modifiersString = modifiers.length ?  " (" + modifiers.join(", ") + ")" : "";
+
+    InspectorTest.addResult("Running replace test for /" + searchConfig.query + "/" + replacement + "/ " + modifiersString + ":");
+
+    editor = sourceFrame._textEditor;
+    var oldLines = [];
+    for (var i = 0; i < editor.linesCount; ++i)
+        oldLines.push(editor.line(i));
+    var searchableView = WebInspector.panels.sources.sourcesView().searchableView();
+    searchableView.showSearchField();
+
+    searchableView._caseSensitiveButton.setToggled(searchConfig.caseSensitive);
+    searchableView._regexButton.setToggled(searchConfig.isRegex);
+    searchableView._searchInputElement.value = searchConfig.query;
+    searchableView._replaceCheckboxElement.checked = true;
+    searchableView._updateSecondRowVisibility();
+    searchableView._replaceInputElement.value = replacement;
+    searchableView._performSearch(true, true);
+    if (replaceAll)
+        searchableView._replaceAll()
+     else
+        searchableView._replace();
+
+    var newLines = [];
+    for (var i = 0; i < editor.linesCount; ++i)
+        newLines.push(editor.line(i));
+
+    for (var i = 0; i < newLines.length; ++i) {
+        if (oldLines[i] === newLines[i])
+            continue;
+        var oldLine = oldLines[i];
+        var newLine = newLines[i];
+
+        var prefixLength = 0;
+        for (var j = 0; j < oldLine.length && j < newLine.length && newLine[j] === oldLine[j]; ++j)
+            ++prefixLength;
+        var postfixLength = 0;
+        for (var j = 0; j < oldLine.length && j < newLine.length && newLine[newLine.length - j - 1] === oldLine[oldLine.length - j - 1]; ++j)
+            ++postfixLength;
+        var prefix = oldLine.substring(0, prefixLength);
+        var removed = oldLine.substring(prefixLength, oldLine.length - postfixLength);
+        var added = newLine.substring(prefixLength, newLine.length - postfixLength);
+        var postfix = oldLine.substring(oldLine.length - postfixLength);
+        InspectorTest.addResult("  - " + prefix + "#" + removed + "#" + added + "#" + postfix);
+    }
+}
+
 };
