01e4546ff34a57faaefd41fce323c691902501c5
Christian König <christian.koenig@amd.com>
2015-07-15 10:56:20 +0200

    amdgpu: fix bs buffer size for vce test
    Signed-off-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Jammy Zhou <Jammy.Zhou@amd.com>

--- tests/amdgpu/vce_tests.c
+++ tests/amdgpu/vce_tests.c
@@ -363,7 +363,7 @@ static void check_result(struct amdgpu_vce_encode *enc)
 
 static void amdgpu_cs_vce_encode(void)
 {
-	uint32_t vbuf_size, bs_size = 0x2000, cpb_size;
+	uint32_t vbuf_size, bs_size = 0x154000, cpb_size;
 	int i, r;
 
 	vbuf_size = enc.width * enc.height * 1.5;
