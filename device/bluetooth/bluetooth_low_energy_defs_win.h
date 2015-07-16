c9b2eb2fac9607465f776cf9e454e9c9511a75e6
brucedawson <brucedawson@chromium.org>
2015-07-16 16:17:24 -0700

    Conditionally disable bthledef.h workaround
    The bthledef.h header file is fixed in the Windows 10 SDK. This change
    detects when that SDK is being used and conditionally applies a fix. The
    conditional fix can be removed once we fully move to the Windows 10 SDK.
    
    The original workaround was checked in by cl
    https://codereview.chromium.org/720033003
    
    R=rpaquay@chromium.org
    BUG=491424
    
    Review URL: https://codereview.chromium.org/1233393002
    
    Cr-Commit-Position: refs/heads/master@{#339160}
    
    Former-commit-id: 80821ed5454c87b7b7387756d818ee4de8e4c15e

--- device/bluetooth/bluetooth_low_energy_defs_win.h
+++ device/bluetooth/bluetooth_low_energy_defs_win.h
@@ -8,10 +8,16 @@
 #include <windows.h>
 #include <cfg.h>
 #include <devpkey.h>
+#include <ntverp.h>  // For VER_PRODUCTBUILD
 #include <setupapi.h>
 
+#if VER_PRODUCTBUILD > 9600
+// bthledef.h is fixed in the Windows 10 SDK and the extra pop then triggers a
+// warning, so we skip it when VER_PRODUCTBUILD is > 9600 (8.1 SDK)
+#include <bthledef.h>
+#else
 #pragma warning(push)
-// bthledef.h is buggy and contains
+// bthledef.h in the Windows 8.1 SDK is buggy and contains
 //   #pragma pop
 // which should be
 //   #pragma warning(pop)
@@ -21,6 +27,7 @@
 #include <bthledef.h>
 #pragma warning(pop)
 #pragma warning(pop)
+#endif
 
 #include <bluetoothapis.h>
 #include <bluetoothleapis.h>
