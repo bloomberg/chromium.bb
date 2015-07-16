a649467f0548381491b6b300aa8888e26ea31ab1
guoweis <guoweis@chromium.org>
2015-07-16 13:56:10 -0700

    Host name should be used in CONNECT when connecting to TURN through proxy.
    BUG=webrtc:4828
    
    Review URL: https://codereview.chromium.org/1221623009
    
    Cr-Commit-Position: refs/heads/master@{#339116}
    
    Former-commit-id: cfed738ea0c152aaaf12fd4774945f82de2a09f8

--- content/browser/renderer_host/p2p/socket_host_tcp.cc
+++ content/browser/renderer_host/p2p/socket_host_tcp.cc
@@ -86,13 +86,15 @@ bool P2PSocketHostTcpBase::Init(const net::IPEndPoint& local_address,
   state_ = STATE_CONNECTING;
 
   net::HostPortPair dest_host_port_pair;
-  // If there is no resolved address, let's try with domain name, assuming
-  // socket layer will do the DNS resolve.
-  if (remote_address.ip_address.address().empty()) {
-    DCHECK(!remote_address.hostname.empty());
+  // If there is a domain name, let's try it first, it's required by some proxy
+  // to only take hostname for CONNECT. If it has been DNS resolved, the result
+  // is likely cached and shouldn't cause 2nd DNS resolution in the case of
+  // direct connect (i.e. no proxy).
+  if (!remote_address.hostname.empty()) {
     dest_host_port_pair = net::HostPortPair(remote_address.hostname,
                                             remote_address.ip_address.port());
   } else {
+    DCHECK(!remote_address.ip_address.address().empty());
     dest_host_port_pair = net::HostPortPair::FromIPEndPoint(
         remote_address.ip_address);
   }
