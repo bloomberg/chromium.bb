a4274d4d0c15b4b893d3812ff4e60df64d3c1d5b
junov@chromium.org <junov@chromium.org>
2015-07-17 19:35:33 +0000

    Canvas.toDataURL to use SkBitmap::readPixels to avoid uninitialized memory
    This change refactors toDataURL to make it read the canvas data into
    a pre-initialized buffer to avoid accessing unitialized memory in cases
    where a GPU readback fails silently.
    
    BUG=504690
    
    Review URL: https://codereview.chromium.org/1234083003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199131 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: a0c3c7d3d865911a190bee95a82dd593aa6bf169

--- third_party/WebKit/Source/platform/graphics/ImageBuffer.cpp
+++ third_party/WebKit/Source/platform/graphics/ImageBuffer.cpp
@@ -326,8 +326,7 @@ void ImageBuffer::putByteArray(Multiply multiplied, const unsigned char* source,
     canvas()->writePixels(info, srcAddr, srcBytesPerRow, destX, destY);
 }
 
-template <typename T>
-static bool encodeImage(T& source, const String& mimeType, const double* quality, Vector<char>* output)
+static bool encodeImage(const ImageDataBuffer& source, const String& mimeType, const double* quality, Vector<char>* output)
 {
     Vector<unsigned char>* encodedImage = reinterpret_cast<Vector<unsigned char>*>(output);
 
@@ -350,17 +349,6 @@ static bool encodeImage(T& source, const String& mimeType, const double* quality
     }
 
     return true;
-}
-
-String ImageBuffer::toDataURL(const String& mimeType, const double* quality) const
-{
-    ASSERT(MIMETypeRegistry::isSupportedImageMIMETypeForEncoding(mimeType));
-
-    Vector<char> encodedImage;
-    if (!isSurfaceValid() || !encodeImage(m_surface->bitmap(), mimeType, quality, &encodedImage))
-        return "data:,";
-
-    return "data:" + mimeType + ";base64," + base64Encode(encodedImage);
 }
 
 String ImageDataBuffer::toDataURL(const String& mimeType, const double* quality) const
