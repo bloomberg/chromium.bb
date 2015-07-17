11d70460f9f824b51a18850aae29ef011d331fc8
sunnyps <sunnyps@chromium.org>
2015-07-16 19:02:19 -0700

    cc: Call PrepareTiles when becoming visible in browser only.
    Calling PrepareTiles on each visibility change might have caused a v8
    idle time benchmark regression. We only need to call PrepareTiles for
    the SingleThreadProxy wait for ready to draw mode.
    
    R=brianderson@chromium.org,danakj@chromium.org
    TEST=LayerTreeHostTestReadyToDrawVisibility
    BUG=499040
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1239843002
    
    Cr-Commit-Position: refs/heads/master@{#339201}
    
    Former-commit-id: c67e9d831fa3753fc5434229d3275b1c0155edae

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -1988,11 +1988,9 @@ void LayerTreeHostImpl::SetVisible(bool visible) {
   else
     EvictAllUIResources();
 
-  // Call PrepareTiles unconditionally on visibility change since this tab may
-  // never get another draw or timer tick. When becoming visible we care about
-  // unblocking the scheduler which might be waiting for activation / ready to
-  // draw. When becoming invisible we care about evicting tiles immediately.
-  PrepareTiles();
+  // PrepareTiles when becoming invisible so that we evict tiles immediately.
+  if (!visible_)
+    PrepareTiles();
 
   if (!renderer_)
     return;
