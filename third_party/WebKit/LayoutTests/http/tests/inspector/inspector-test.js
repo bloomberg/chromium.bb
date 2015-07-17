31680d46eb00fb5f7188773b132c0e4b4c0ae9cc
yurys@chromium.org <yurys@chromium.org>
2015-07-17 14:23:02 +0000

    DevTools: don't pollute stderr with inspector protocol messages
    Disable protocol logging that was enabled for some tests. It can be reenabled later when we have a good reason for that.
    
    BUG=None
    TBR=sergeyv
    
    Review URL: https://codereview.chromium.org/1235253010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199103 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: e749e6023b941c3593dae45bae51136a01cdeb60

--- third_party/WebKit/LayoutTests/http/tests/inspector/inspector-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/inspector-test.js
@@ -973,9 +973,6 @@ function runTest(enableWatchDogWhileDebugging)
             }
         }
 
-        if (testPath.startsWith("LayoutTests/inspector/elements/styles"))
-            InspectorTest.startDumpingProtocolMessages();
-
         // 3. Run test function.
         Promise.all(promises).then(function() {
             if (lastLoadedPanel)
