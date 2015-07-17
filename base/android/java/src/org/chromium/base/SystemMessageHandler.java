1a1b31728f9382b426da7e6311ca9f596863e9de
jdduke <jdduke@chromium.org>
2015-07-17 13:56:49 -0700

    [Android] Use public Message.setAsynchronous when available
    L MR1 made public the Message.setAsynchronous() method. Use the method
    directly when available.
    
    BUG=407133
    
    Review URL: https://codereview.chromium.org/1242513002
    
    Cr-Commit-Position: refs/heads/master@{#339328}
    
    Former-commit-id: 8084d01e2bf47f91761c93b302203da32ac6177f

--- base/android/java/src/org/chromium/base/SystemMessageHandler.java
+++ base/android/java/src/org/chromium/base/SystemMessageHandler.java
@@ -4,16 +4,16 @@
 
 package org.chromium.base;
 
+import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
-import android.util.Log;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 class SystemMessageHandler extends Handler {
 
-    private static final String TAG = "SystemMessageHandler";
+    private static final String TAG = "cr.SysMessageHandler";
 
     private static final int SCHEDULED_WORK = 1;
     private static final int DELAYED_SCHEDULED_WORK = 2;
@@ -22,27 +22,8 @@ class SystemMessageHandler extends Handler {
     private long mMessagePumpDelegateNative = 0;
     private long mDelayedScheduledTimeTicks = 0;
 
-    // Reflected API for marking a message as asynchronous. This is a workaround
-    // to provide fair Chromium task dispatch when served by the Android UI
-    // thread's Looper, avoiding stalls when the Looper has a sync barrier.
-    // Note: Use of this API is experimental and likely to evolve in the future.
-    private Method mMessageMethodSetAsynchronous;
-
     private SystemMessageHandler(long messagePumpDelegateNative) {
         mMessagePumpDelegateNative = messagePumpDelegateNative;
-
-        try {
-            Class<?> messageClass = Class.forName("android.os.Message");
-            mMessageMethodSetAsynchronous = messageClass.getMethod(
-                    "setAsynchronous", new Class[]{boolean.class});
-        } catch (ClassNotFoundException e) {
-            Log.e(TAG, "Failed to find android.os.Message class:" + e);
-        } catch (NoSuchMethodException e) {
-            Log.e(TAG, "Failed to load Message.setAsynchronous method:" + e);
-        } catch (RuntimeException e) {
-            Log.e(TAG, "Exception while loading Message.setAsynchronous method: " + e);
-        }
-
     }
 
     @Override
@@ -77,28 +58,92 @@ class SystemMessageHandler extends Handler {
     }
 
     private Message obtainAsyncMessage(int what) {
+        // Marking the message async provides fair Chromium task dispatch when
+        // served by the Android UI thread's Looper, avoiding stalls when the
+        // Looper has a sync barrier.
         Message msg = Message.obtain();
         msg.what = what;
-        if (mMessageMethodSetAsynchronous != null) {
-            // If invocation fails, assume this is indicative of future
-            // failures, and avoid log spam by nulling the reflected method.
-            try {
-                mMessageMethodSetAsynchronous.invoke(msg, true);
-            } catch (IllegalAccessException e) {
-                Log.e(TAG, "Illegal access to asynchronous message creation, disabling.");
-                mMessageMethodSetAsynchronous = null;
-            } catch (IllegalArgumentException e) {
-                Log.e(TAG, "Illegal argument for asynchronous message creation, disabling.");
-                mMessageMethodSetAsynchronous = null;
-            } catch (InvocationTargetException e) {
-                Log.e(TAG, "Invocation exception during asynchronous message creation, disabling.");
-                mMessageMethodSetAsynchronous = null;
-            } catch (RuntimeException e) {
-                Log.e(TAG, "Runtime exception during asynchronous message creation, disabling.");
-                mMessageMethodSetAsynchronous = null;
+        MessageCompat.setAsynchronous(msg, true);
+        return msg;
+    }
+
+    /**
+     * Abstraction utility class for marking a Message as asynchronous. Prior
+     * to L MR1 the async Message API was hidden, and for such cases we fall
+     * back to using reflection to obtain the necessary method.
+     */
+    private static class MessageCompat {
+        /**
+         * @See android.os.Message#setAsynchronous(boolean)
+         */
+        public static void setAsynchronous(Message message, boolean async) {
+            IMPL.setAsynchronous(message, async);
+        }
+
+        interface MessageWrapperImpl {
+            /**
+             * @See android.os.Message#setAsynchronous(boolean)
+             */
+            public void setAsynchronous(Message message, boolean async);
+        }
+
+        static final MessageWrapperImpl IMPL;
+        static {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1) {
+                IMPL = new LollipopMr1MessageWrapperImpl();
+            } else {
+                IMPL = new LegacyMessageWrapperImpl();
             }
         }
-        return msg;
+
+        static class LollipopMr1MessageWrapperImpl implements MessageWrapperImpl {
+            @Override
+            public void setAsynchronous(Message msg, boolean async) {
+                msg.setAsynchronous(async);
+            }
+        }
+
+        static class LegacyMessageWrapperImpl implements MessageWrapperImpl {
+            // Reflected API for marking a message as asynchronous.
+            // Note: Use of this API is experimental and likely to evolve in the future.
+            private Method mMessageMethodSetAsynchronous;
+
+            LegacyMessageWrapperImpl() {
+                try {
+                    Class<?> messageClass = Class.forName("android.os.Message");
+                    mMessageMethodSetAsynchronous =
+                            messageClass.getMethod("setAsynchronous", new Class[] {boolean.class});
+                } catch (ClassNotFoundException e) {
+                    Log.e(TAG, "Failed to find android.os.Message class", e);
+                } catch (NoSuchMethodException e) {
+                    Log.e(TAG, "Failed to load Message.setAsynchronous method", e);
+                } catch (RuntimeException e) {
+                    Log.e(TAG, "Exception while loading Message.setAsynchronous method", e);
+                }
+            }
+
+            @Override
+            public void setAsynchronous(Message msg, boolean async) {
+                if (mMessageMethodSetAsynchronous == null) return;
+                // If invocation fails, assume this is indicative of future
+                // failures, and avoid log spam by nulling the reflected method.
+                try {
+                    mMessageMethodSetAsynchronous.invoke(msg, async);
+                } catch (IllegalAccessException e) {
+                    Log.e(TAG, "Illegal access to async message creation, disabling.");
+                    mMessageMethodSetAsynchronous = null;
+                } catch (IllegalArgumentException e) {
+                    Log.e(TAG, "Illegal argument for async message creation, disabling.");
+                    mMessageMethodSetAsynchronous = null;
+                } catch (InvocationTargetException e) {
+                    Log.e(TAG, "Invocation exception during async message creation, disabling.");
+                    mMessageMethodSetAsynchronous = null;
+                } catch (RuntimeException e) {
+                    Log.e(TAG, "Runtime exception during async message creation, disabling.");
+                    mMessageMethodSetAsynchronous = null;
+                }
+            }
+        }
     }
 
     @CalledByNative
