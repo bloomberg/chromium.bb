669399ff6221edf50672bccb3dbfd95ea6aeb427
mlamouri@chromium.org <mlamouri@chromium.org>
2015-07-17 10:09:30 +0000

    Presentation API: shell implementation of PresentationRequest.
    Creates a simple shell for the new PresentationRequest interface in order
    to allow incremental implementation. This is also testing the presence
    of the interface.
    
    BUG=510483
    
    Review URL: https://codereview.chromium.org/1238973006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199094 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: c50035cc93b451b7468c89c33650399b16972e3d

---
+++ third_party/WebKit/Source/modules/presentation/PresentationRequest.cpp
@@ -0,0 +1,83 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "modules/presentation/PresentationRequest.h"
+
+#include "bindings/core/v8/ExceptionState.h"
+#include "bindings/core/v8/ScriptPromise.h"
+#include "bindings/core/v8/ScriptPromiseResolver.h"
+#include "core/dom/ExecutionContext.h"
+#include "modules/EventTargetModules.h"
+
+namespace blink {
+
+// static
+PresentationRequest* PresentationRequest::create(ExecutionContext* executionContext, const String& url, ExceptionState& exceptionState)
+{
+    KURL parsedUrl = KURL(executionContext->url(), url);
+    if (!parsedUrl.isValid() || parsedUrl.protocolIsAbout()) {
+        exceptionState.throwTypeError("'" + url + "' can't be resolved to a valid URL.");
+        return nullptr;
+    }
+
+    PresentationRequest* request = new PresentationRequest(executionContext, parsedUrl);
+    request->suspendIfNeeded();
+    return request;
+}
+
+const AtomicString& PresentationRequest::interfaceName() const
+{
+    return EventTargetNames::PresentationRequest;
+}
+
+ExecutionContext* PresentationRequest::executionContext() const
+{
+    return ActiveDOMObject::executionContext();
+}
+
+bool PresentationRequest::hasPendingActivity() const
+{
+    // Prevents garbage collecting of this object when not hold by another
+    // object but still has listeners registered.
+    return hasEventListeners();
+}
+
+ScriptPromise PresentationRequest::start(ScriptState* scriptState)
+{
+    // TODO(mlamouri): migrate from Presentation, see https://crbug.com/510483
+    RefPtrWillBeRawPtr<ScriptPromiseResolver> resolver = ScriptPromiseResolver::create(scriptState);
+    ScriptPromise promise = resolver->promise();
+    return promise;
+}
+
+ScriptPromise PresentationRequest::join(ScriptState* scriptState, const String& id)
+{
+    // TODO(mlamouri): migrate from Presentation, see https://crbug.com/510483
+    RefPtrWillBeRawPtr<ScriptPromiseResolver> resolver = ScriptPromiseResolver::create(scriptState);
+    ScriptPromise promise = resolver->promise();
+    return promise;
+}
+
+ScriptPromise PresentationRequest::getAvailability(ScriptState* scriptState)
+{
+    // TODO(mlamouri): migrate from Presentation, see https://crbug.com/510483
+    RefPtrWillBeRawPtr<ScriptPromiseResolver> resolver = ScriptPromiseResolver::create(scriptState);
+    ScriptPromise promise = resolver->promise();
+    return promise;
+}
+
+DEFINE_TRACE(PresentationRequest)
+{
+    RefCountedGarbageCollectedEventTargetWithInlineData<PresentationRequest>::trace(visitor);
+    ActiveDOMObject::trace(visitor);
+}
+
+PresentationRequest::PresentationRequest(ExecutionContext* executionContext, const KURL& url)
+    : ActiveDOMObject(executionContext)
+    , m_url(url)
+{
+}
+
+} // namespace blink
