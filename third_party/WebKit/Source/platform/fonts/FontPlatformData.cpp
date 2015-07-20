b08d41768e1cfedeb783ae6d05750b7a0910dd06
eae@chromium.org <eae@chromium.org>
2015-07-20 22:27:07 +0000

    Reland unit tests for FontPlatformData::hasSpaceInLigaturesOrKerning
    Add a set of unit tests for the hasSpaceInLigaturesOrKerning method that
    verifies that the correct value is returned for a font that has space in
    either the GPOS or GSUB tables vs for one that does not. Also tests that
    the check is bypassed and false is returned if kerning and ligatures are
    both disabled. Finally, it fixes the failure mode in that the method now
    returns false in cases where the font tables cannot be accessed.
    
    Originally committed as r199059 (bbb929c88fbe43979dbb9b2b20218538), then
    reverted as r199072 (bbb929c88fbe43979dbb9b2b20218538) becasue it caused
    android asserts in the test. This re-land disabled the tests on Android.
    
    R=jchaffraix@chromium.org
    
    Review URL: https://codereview.chromium.org/1229853005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199186 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d63c0cd305fa73a4ca38f41165b0f89629988d93

--- third_party/WebKit/Source/platform/fonts/FontPlatformData.cpp
+++ third_party/WebKit/Source/platform/fonts/FontPlatformData.cpp
@@ -289,7 +289,7 @@ bool FontPlatformData::hasSpaceInLigaturesOrKerning(
 {
     const HarfBuzzFace* hbFace = harfBuzzFace();
     if (!hbFace)
-        return true;
+        return false;
 
     hb_face_t* face = hbFace->face();
     ASSERT(face);
@@ -301,11 +301,11 @@ bool FontPlatformData::hasSpaceInLigaturesOrKerning(
     // will be rendering using a fallback font, which grantees that it cannot
     // affect the shape of the preceding word.
     if (!hb_font_get_glyph(font.get(), spaceCharacter, 0, &space))
-        return true;
+        return false;
 
     if (!hb_ot_layout_has_substitution(face)
         && !hb_ot_layout_has_positioning(face)) {
-        return true;
+        return false;
     }
 
     bool foundSpaceInTable = false;
