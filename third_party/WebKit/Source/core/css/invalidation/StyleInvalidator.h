b16905bd6e194979014148db747e44f26be0d7de
keishi@chromium.org <keishi@chromium.org>
2015-07-22 09:34:24 +0000

    Oilpan: Remove raw pointer to DescendantInvalidationSet from StyleInvalidator::RecursionData
    BUG=509911
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=199288
    
    Review URL: https://codereview.chromium.org/1225233005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199294 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 656f41b948a1fd18badfc8f26e354d490eec3972

--- third_party/WebKit/Source/core/css/invalidation/StyleInvalidator.h
+++ third_party/WebKit/Source/core/css/invalidation/StyleInvalidator.h
@@ -29,7 +29,9 @@ public:
     DECLARE_TRACE();
 
 private:
-    struct RecursionData {
+    class RecursionData {
+        STACK_ALLOCATED();
+    public:
         RecursionData()
             : m_invalidateCustomPseudo(false)
             , m_wholeSubtreeInvalid(false)
@@ -47,7 +49,7 @@ private:
         bool treeBoundaryCrossing() const { return m_treeBoundaryCrossing; }
         bool insertionPointCrossing() const { return m_insertionPointCrossing; }
 
-        using InvalidationSets = Vector<const DescendantInvalidationSet*, 16>;
+        using InvalidationSets = WillBeHeapVector<RawPtrWillBeMember<const DescendantInvalidationSet>, 16>;
         InvalidationSets m_invalidationSets;
         bool m_invalidateCustomPseudo;
         bool m_wholeSubtreeInvalid;
@@ -60,6 +62,7 @@ private:
     bool checkInvalidationSetsAgainstElement(Element&, RecursionData&);
 
     class RecursionCheckpoint {
+        STACK_ALLOCATED();
     public:
         RecursionCheckpoint(RecursionData* data)
             : m_prevInvalidationSetsSize(data->m_invalidationSets.size())
@@ -84,6 +87,7 @@ private:
         bool m_prevWholeSubtreeInvalid;
         bool m_treeBoundaryCrossing;
         bool m_insertionPointCrossing;
+        // This is a stack reference and need not separate tracing.
         RecursionData* m_data;
     };
 
