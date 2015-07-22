551699be98266b51dd24029e7fefc4958da5e7a6
eae@chromium.org <eae@chromium.org>
2015-07-22 01:41:39 +0000

    Remove virtual antialiasedtext test suite
    Remove the virtual antialiasedtext test suite and enable text antialias
    by default for the fast/text directory.
    
    R=pdr@chromium.org
    
    Review URL: https://codereview.chromium.org/1230633003 .
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199251 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: f8a238725b1e0b63b73d4712e129dba54abad548

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -1589,6 +1589,7 @@ class Port(object):
         return [
             # For example, to turn on force-compositing-mode in the svg/ directory:
             # PhysicalTestSuite('svg', ['--force-compositing-mode']),
+            PhysicalTestSuite('fast/text', ["--enable-direct-write", "--enable-font-antialiasing"]),
         ]
 
     def virtual_test_suites(self):
