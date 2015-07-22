5ab2f2baadb27967b755ed6fd623d292ff43d4fb
mgiuca <mgiuca@chromium.org>
2015-07-21 20:50:32 -0700

    Disable KeyboardOperations/FileManagerBrowserTest for being flaky.
    TBR=hirono@chromium.org
    NOTRY=true
    BUG=512669
    
    Review URL: https://codereview.chromium.org/1250983002
    
    Cr-Commit-Position: refs/heads/master@{#339830}
    
    Former-commit-id: 908a415599f0cd45143ae2c405b2551f6463a276

--- chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
+++ chrome/browser/chromeos/file_manager/file_manager_browsertest.cc
@@ -129,7 +129,8 @@ WRAPPED_INSTANTIATE_TEST_CASE_P(
 #if defined(DISABLE_SLOW_FILESAPP_TESTS) || defined(OFFICIAL_BUILD)
 #define MAYBE_KeyboardOperations DISABLED_KeyboardOperations
 #else
-#define MAYBE_KeyboardOperations KeyboardOperations
+// Flaky on other builds. http://crbug.com/512669
+#define MAYBE_KeyboardOperations DISABLED_KeyboardOperations
 #endif
 WRAPPED_INSTANTIATE_TEST_CASE_P(
     MAYBE_KeyboardOperations,
