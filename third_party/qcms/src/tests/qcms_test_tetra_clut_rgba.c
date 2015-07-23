fd6e7f3479c0920cbcdc747a7a3dee6ffe6b1632
noel <noel@chromium.org>
2015-07-23 05:00:08 -0700

    [qcms] Update the CLUT test harness
    Change validate() to return if there are color transform output
    differences, and report the the number of differences (if any).
    Don't exit if there are differences, continue instead.
    
    Add options to control the test image width and height and also
    the number of test iterations. Set default test iterations to 1
    which the user can override with the -i {iterations} option.
    
    Include the average test time in the reported results.
    
    BUG=506607
    
    Review URL: https://codereview.chromium.org/1247453004
    
    Cr-Commit-Position: refs/heads/master@{#340074}
    
    Former-commit-id: 1c49dceffac339a1d6e5bb5ea264c06a2b204c22

--- third_party/qcms/src/tests/qcms_test_tetra_clut_rgba.c
+++ third_party/qcms/src/tests/qcms_test_tetra_clut_rgba.c
@@ -12,7 +12,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-// External qcms functions being tested.
+// External qcms tetra clut interpolators.
+
 extern void qcms_transform_data_tetra_clut_rgba(qcms_transform *transform,
                                                 unsigned char *src,
                                                 unsigned char *dest,
@@ -25,60 +26,81 @@ extern void qcms_transform_data_tetra_clut_rgba_sse2(qcms_transform *transform,
                                                      size_t length,
                                                      qcms_format_type output_format);
 
-#define PIXEL_SIZE    4
+#define PIXEL_SIZE 4
 
-static void generate_source(unsigned char *src, size_t length) {
-    size_t i;
+static void generate_source(unsigned char *src, size_t length)
+{
     size_t bytes = length * PIXEL_SIZE;
+    size_t i;
 
     for (i = 0; i < bytes; ++i) {
         *src++ = rand() & 255;
     }
 }
 
-static float *create_lut(uint32_t lutSize) {
+static float *create_lut(size_t lutSize)
+{
     float *lut = malloc(lutSize * sizeof(float));
-    uint32_t i;
+    size_t i;
 
-    for (i = 0; i < lutSize; i++) {
+    for (i = 0; i < lutSize; ++i) {
         lut[i] = (rand() & 255) * (1.0f / 255.0f);
     }
 
     return lut;
 }
 
-static int validate(unsigned char *dst0, unsigned char *dst1, size_t length) {
-    size_t i;
-    size_t bytes = length * PIXEL_SIZE;
+static int diffs;
 
-    for (i = 0; i < bytes; ++i) {
-        // This should account for differences resulted from rounding operations.
-        if (abs((int)dst0[i] - (int)dst1[i]) > 1) {
-            return -1;
+static int validate(unsigned char *dst0, unsigned char *dst1, size_t length, int limit)
+{
+    size_t bytes = length * PIXEL_SIZE;
+    size_t i;
+
+    // Compare dst0/dst0 byte-by-byte, allowing for minor differences due
+    // to SSE rounding modes (controlled by the limit argument).
+
+    if (limit < 0)
+        limit = 255; // Ignore all differences.
+
+    for (diffs = 0, i = 0; i < bytes; ++i) {
+        if (abs((int)dst0[i] - (int)dst1[i]) > limit) {
+            ++diffs;
         }
     }
 
-    return 0;
+    return !diffs;
 }
 
-int main(int argc, const char **args) {
+int main(int argc, const char **argv)
+{
     qcms_transform transform0, transform1;
-    qcms_format_type output_format = {2, 0};
+    qcms_format_type format = {2, 0};
     uint16_t samples = 33;
-    uint32_t lutSize;
+    size_t lutSize;
     float *lut0, *lut1;
 
-    size_t length = 2000;
+    int iterations = 1;
+    size_t height = 2000;
+    size_t width = 2000;
+    size_t length;
+
     double time0, time1;
     int i;
-    int iterations = 1000;
 
-    // Allow testing with different number of iterations to reduce measurement noise.
-    if (argc > 1) {
-        iterations = atoi(args[1]);
+    while (argc > 1) {
+        if (strcmp(argv[1], "-i") == 0)
+            iterations = abs(atoi(argv[2]));
+        else if (strcmp(argv[1], "-w") == 0)
+            width = (size_t) abs(atoi(argv[2]));
+        else if (strcmp(argv[1], "-h") == 0)
+            height = (size_t) abs(atoi(argv[2]));
+        (--argc, ++argv);
     }
 
-    printf("Running qcms_transform_data_tetra_clut_rgba test for %d iterations\n", iterations);
+    printf("Test qcms clut transforms for %d iterations\n", iterations);
+    printf("Test image size %u x %u pixels\n", width, height);
+    length = width * height;
     fflush(stdout);
 
     srand(0);
@@ -100,27 +122,29 @@ int main(int argc, const char **args) {
     transform1.g_clut = &lut1[1];
     transform1.b_clut = &lut1[2];
 
+    // Re-generate and use different data sources during the iteration loop
+    // to avoid compiler / cache optimizations that may affect performance.
+
     time0 = 0.0;
     time1 = 0.0;
 
-    // Re-generate and use different data sources the during iteration loop
-    // to avoid caching or compiler optimizations that may affect performance.
-	
     for (i = 0; i < iterations; ++i) {
         unsigned char *src0 = (unsigned char *)calloc(length, PIXEL_SIZE);
-        unsigned char *dst0 = (unsigned char *)calloc(length, PIXEL_SIZE);
         unsigned char *src1 = (unsigned char *)calloc(length, PIXEL_SIZE);
+        unsigned char *dst0 = (unsigned char *)calloc(length, PIXEL_SIZE);
         unsigned char *dst1 = (unsigned char *)calloc(length, PIXEL_SIZE);
 
         generate_source(src0, length);
         memcpy(src1, src0, length * PIXEL_SIZE);
 
-        TIME(qcms_transform_data_tetra_clut_rgba(&transform0, src0, dst0, length, output_format), &time0);
-        TIME(qcms_transform_data_tetra_clut_rgba_sse2(&transform1, src1, dst1, length, output_format), &time1);
+#define TRANSFORM_TEST0 qcms_transform_data_tetra_clut_rgba
+#define TRANSFORM_TEST1 qcms_transform_data_tetra_clut_rgba_sse2
 
-        if (validate(dst0, dst1, length) != 0) {
-            fprintf(stderr, "Failed to validate output!\n");
-            return EXIT_FAILURE;
+        TIME(TRANSFORM_TEST0(&transform0, src0, dst0, length, format), &time0);
+        TIME(TRANSFORM_TEST1(&transform1, src1, dst1, length, format), &time1);
+
+        if (!validate(dst0, dst1, length, 0)) {
+            fprintf(stderr, "Invalid transform output: %d diffs\n", diffs);
         }
 
         free(src0);
@@ -129,10 +153,15 @@ int main(int argc, const char **args) {
         free(dst1);
     }
 
-    printf("Test Results:\n");
-    printf("%.6lf seconds for qcms_transform_data_tetra_clut_rgba\n", time0);
-    printf("%.6lf seconds for qcms_transform_data_tetra_clut_rgba_sse2\n", time1);
-    printf("%.6lf speedup after %d iterations\n\n", time0 / time1, iterations);
+#define STRINGIZE(s) #s
+#define STRING(s) STRINGIZE(s)
+
+    printf("%.6lf (avg %.6lf) seconds " STRING(TRANSFORM_TEST0) "\n",
+           time0, time0 / iterations);
+    printf("%.6lf (avg %.6lf) seconds " STRING(TRANSFORM_TEST1) "\n",
+           time1, time1 / iterations);
+    printf("%.6lf speedup after %d iterations\n\n",
+           time0 / time1, iterations);
 
     free(lut0);
     free(lut1);
