af3433d4f98278df6915c6849f2f9c5cc8a081b4
kbr <kbr@chromium.org>
2015-07-22 18:19:24 -0700

    Wrapper script for isolating telemetry_gpu_unittests.
    The contract is that the recipe will pass:
      --test-launcher-summary-output [filename]
    to the isolate, and consume the simplified JSON output from that file.
    
    Tested locally with the isolate built from telemetry_gpu_unittests_run.
    
    BUG=507796
    TBR=phajdan.jr@chromium.org
    
    Review URL: https://codereview.chromium.org/1242753008
    
    Cr-Commit-Position: refs/heads/master@{#340018}
    
    Former-commit-id: 51c451bc5e984a6c0e39961453281158d733578d

---
+++ testing/scripts/run_telemetry_as_googletest.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Runs isolate bundled Telemetry unittests.
+
+This script attempts to emulate the contract of gtest-style tests
+invoked via recipes. The main contract is that the caller passes the
+argument:
+
+  --test-launcher-summary-output=[FILENAME]
+
+json is written to that file in the format produced by
+common.parse_common_test_results.
+
+This script is intended to be the base command invoked by the isolate,
+followed by a subsequent Python script. It could be generalized to
+invoke an arbitrary executable.
+"""
+
+import argparse
+import json
+import os
+import sys
+
+
+import common
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    '--test-launcher-summary-output',
+    type=argparse.FileType('w'),
+    required=True)
+  args, rest_args = parser.parse_known_args()
+  with common.temporary_file() as tempfile_path:
+    rc = common.run_command([sys.executable] + rest_args + [
+      '--write-full-results-to', tempfile_path,
+    ])
+    with open(tempfile_path) as f:
+      results = json.load(f)
+    parsed_results = common.parse_common_test_results(results,
+                                                      test_separator='.')
+    failures = parsed_results['unexpected_failures']
+
+    json.dump({
+        'valid': bool(rc <= common.MAX_FAILURES_EXIT_STATUS and
+                      ((rc == 0) or failures)),
+        'failures': failures.keys(),
+    }, args.test_launcher_summary_output)
+
+  return rc
+
+
+# This is not really a "script test" so does not need to manually add
+# any additional compile targets.
+def main_compile_targets(args):
+  json.dump([], args.output)
+
+
+if __name__ == '__main__':
+  # Conform minimally to the protocol defined by ScriptTest.
+  if 'compile_targets' in sys.argv:
+    funcs = {
+      'run': None,
+      'compile_targets': main_compile_targets,
+    }
+    sys.exit(common.run_script(sys.argv[1:], funcs))
+  sys.exit(main())
