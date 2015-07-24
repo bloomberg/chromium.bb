9e090af393834c1ba0ab315358882c4b680b9def
hiroshige@chromium.org <hiroshige@chromium.org>
2015-07-24 10:22:05 +0000

    Refactoring: Introduce DestinationHandle::create()
    BUG=512807
    
    Review URL: https://codereview.chromium.org/1244403002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199433 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 87eab136c1446eb9a42ef32bd9d8697acea26d89

--- third_party/WebKit/Source/modules/fetch/DataConsumerTee.cpp
+++ third_party/WebKit/Source/modules/fetch/DataConsumerTee.cpp
@@ -325,6 +325,12 @@ private:
 
 class DestinationHandle final : public WebDataConsumerHandle {
 public:
+    static PassOwnPtr<WebDataConsumerHandle> create(PassRefPtr<DestinationContext::Proxy> contextProxy)
+    {
+        return adoptPtr(new DestinationHandle(contextProxy));
+    }
+
+private:
     DestinationHandle(PassRefPtr<DestinationContext::Proxy> contextProxy) : m_contextProxy(contextProxy) { }
 
 private:
@@ -420,8 +426,8 @@ void DataConsumerTee::create(ExecutionContext* executionContext, PassOwnPtr<WebD
 
     root->initialize(new SourceContext(root, src, context1, context2, executionContext));
 
-    *dest1 = adoptPtr(new DestinationHandle(DestinationContext::Proxy::create(context1, tracker)));
-    *dest2 = adoptPtr(new DestinationHandle(DestinationContext::Proxy::create(context2, tracker)));
+    *dest1 = DestinationHandle::create(DestinationContext::Proxy::create(context1, tracker));
+    *dest2 = DestinationHandle::create(DestinationContext::Proxy::create(context2, tracker));
 }
 
 void DataConsumerTee::create(ExecutionContext* executionContext, PassOwnPtr<FetchDataConsumerHandle> src, OwnPtr<FetchDataConsumerHandle>* dest1, OwnPtr<FetchDataConsumerHandle>* dest2)
