8e747b590a2d7b6c9ec46397d4cb71272d55b749
rouslan <rouslan@chromium.org>
2015-07-27 14:14:32 -0700

    Autofill should process IME text changes
    Autofill ignores all text field changes that are not user gestures. When
    Android IME extends selection and deletes the text in an input field,
    autofill should perceive it as a user gesture and process the text field
    value change appropriately.
    
    To test:
    1) Enable password generation in chrome://flags
    2) Navigate to a sign up form like https://secure.imdb.com/register-imdb
    3) Choose a generated password
    4) Tap and hold the generated password
    5) Delete the generated password
    The "confirm password" field should now be empty.
    
    BUG=504288
    
    Review URL: https://codereview.chromium.org/1224373004
    
    Cr-Commit-Position: refs/heads/master@{#340562}
    
    Former-commit-id: 3daac7a1c9a5867f7e9c5ef742724d480afb3f5e

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -1557,7 +1557,11 @@ void RenderFrameImpl::OnExecuteNoValueEditCommand(const std::string& name) {
 void RenderFrameImpl::OnExtendSelectionAndDelete(int before, int after) {
   if (!GetRenderWidget()->ShouldHandleImeEvent())
     return;
+
+  DCHECK(!WebUserGestureIndicator::isProcessingUserGesture());
+
   ImeEventGuard guard(GetRenderWidget());
+  blink::WebScopedUserGesture gesture_indicator;
   frame_->extendSelectionAndDelete(before, after);
 }
 
