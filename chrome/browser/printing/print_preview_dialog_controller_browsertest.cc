385d394cc3c5d45dfd733db8e03c26910633b5c1
afakhry <afakhry@chromium.org>
2015-07-28 16:34:45 -0700

    Make The task manager browser tests for the print preview work under --site-per-process
    The newly added test fails under --site-per-process. The reason is we
    used to navigate to the NTP which used to create an extra task
    (SubframeTask) in the task manager.
    
    Failure link is here:
    http://build.chromium.org/p/chromium.fyi/builders/Site%20Isolation%20Linux/builds/3418
    
    TBR=nick@chromium.org, thestig@chromium.org
    BUG=470995
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review URL: https://codereview.chromium.org/1256173003
    
    Cr-Commit-Position: refs/heads/master@{#340805}
    
    Former-commit-id: 46d4b6d4fb946f47137458e82390a773e1b5cc32

--- chrome/browser/printing/print_preview_dialog_controller_browsertest.cc
+++ chrome/browser/printing/print_preview_dialog_controller_browsertest.cc
@@ -373,7 +373,7 @@ IN_PROC_BROWSER_TEST_F(PrintPreviewDialogControllerBrowserTest,
   // Navigating away from the current page in the current tab for which a print
   // preview is displayed will cancel the print preview and hence the task
   // manger shouldn't show a printing task.
-  ui_test_utils::NavigateToURL(browser(), GURL(chrome::kChromeUINewTabURL));
+  ui_test_utils::NavigateToURL(browser(), GURL("about:blank"));
   EXPECT_EQ(2U, GetTrackedTags().size());
   EXPECT_EQ(2U, task_manager.tasks().size());
 
