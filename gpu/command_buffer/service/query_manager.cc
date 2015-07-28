faa5ab24ea77aaf05229b6aa74e8586693d02b2a
dyen <dyen@chromium.org>
2015-07-28 14:34:37 -0700

    Make sure active queries map is cleared upon query manager destroy.
    When the active queries map was not cleared, it could possibly
    cause an assert in QueryManager::~QueryManager() when it validates
    that all queries have been deleted. This is because the active
    queries map holds a reference to the query.
    
    BUG=509896
    
    Review URL: https://codereview.chromium.org/1261653003
    
    Cr-Commit-Position: refs/heads/master@{#340770}
    
    Former-commit-id: 4823ae39c9fab9acca0e294e1ce7ca8d4bb77e13

--- gpu/command_buffer/service/query_manager.cc
+++ gpu/command_buffer/service/query_manager.cc
@@ -787,6 +787,7 @@ QueryManager::~QueryManager() {
 void QueryManager::Destroy(bool have_context) {
   pending_queries_.clear();
   pending_transfer_queries_.clear();
+  active_queries_.clear();
   while (!queries_.empty()) {
     Query* query = queries_.begin()->second.get();
     query->Destroy(have_context);
