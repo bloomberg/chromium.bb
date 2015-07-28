2b261ed1fe2332ea58ba2ad318b0ac77985d2309
mkwst <mkwst@chromium.org>
2015-07-28 07:14:07 -0700

    Test casts between 'WebSecurityOrigin' and 'url::Origin'.
    "Surely this is tested by existing code!" turns out to be a bad way to
    test any code whatsoever.
    
    BUG=514067
    R=jochen@chromium.org,ricea@chromium.org
    
    Review URL: https://codereview.chromium.org/1262593002
    
    Cr-Commit-Position: refs/heads/master@{#340679}
    
    Former-commit-id: bb341a25655f4f053a50fc6797b1cdd0ceaa09e1

--- content/child/blink_platform_impl_unittest.cc
+++ content/child/blink_platform_impl_unittest.cc
@@ -8,8 +8,9 @@
 #include "base/time/time.h"
 #include "net/base/ip_address_number.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/WebKit/public/platform/WebSecurityOrigin.h"
 #include "third_party/WebKit/public/platform/WebString.h"
-#include "third_party/WebKit/public/web/WebSecurityOrigin.h"
+#include "url/origin.h"
 
 namespace content {
 
@@ -75,6 +76,51 @@ TEST(BlinkPlatformTest, portAllowed) {
   EXPECT_FALSE(platform_impl.portAllowed(GURL("ws://example.com:21")));
   EXPECT_TRUE(platform_impl.portAllowed(GURL("http://example.com:80")));
   EXPECT_TRUE(platform_impl.portAllowed(GURL("http://example.com:8889")));
+}
+
+TEST(BlinkPlatformTest, castWebSecurityOrigin) {
+  struct TestCase {
+    const char* origin;
+    const char* scheme;
+    const char* host;
+    uint16 port;
+  } cases[] = {
+      {"http://example.com", "http", "example.com", 80},
+      {"http://example.com:80", "http", "example.com", 80},
+      {"http://example.com:81", "http", "example.com", 81},
+      {"https://example.com", "https", "example.com", 443},
+      {"https://example.com:443", "https", "example.com", 443},
+      {"https://example.com:444", "https", "example.com", 444},
+  };
+
+  for (const auto& test : cases) {
+    blink::WebSecurityOrigin web_origin =
+        blink::WebSecurityOrigin::createFromString(
+            blink::WebString::fromUTF8(test.origin));
+    EXPECT_EQ(test.scheme, web_origin.protocol().utf8());
+    EXPECT_EQ(test.host, web_origin.host().utf8());
+    EXPECT_EQ(test.port, web_origin.effectivePort());
+
+    url::Origin url_origin = web_origin;
+    EXPECT_EQ(test.scheme, url_origin.scheme());
+    EXPECT_EQ(test.host, url_origin.host());
+    EXPECT_EQ(test.port, url_origin.port());
+
+    web_origin = url::Origin(GURL(test.origin));
+    EXPECT_EQ(test.scheme, web_origin.protocol().utf8());
+    EXPECT_EQ(test.host, web_origin.host().utf8());
+    EXPECT_EQ(test.port, web_origin.effectivePort());
+  }
+
+  blink::WebSecurityOrigin web_origin =
+      blink::WebSecurityOrigin::createUnique();
+  EXPECT_TRUE(web_origin.isUnique());
+
+  url::Origin url_origin = web_origin;
+  EXPECT_TRUE(url_origin.unique());
+
+  web_origin = url::Origin(GURL(""));
+  EXPECT_TRUE(web_origin.isUnique());
 }
 
 }  // namespace content
