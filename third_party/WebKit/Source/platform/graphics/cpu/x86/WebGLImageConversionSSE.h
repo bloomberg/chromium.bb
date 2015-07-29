63590ece14e30fe13724d26126e987147a14a5a9
claudia.rogoz@intel.com <claudia.rogoz@intel.com>
2015-07-29 08:02:36 +0000

    Update WebGLImageConversion header with new SSE functions.
    Created 3 new SSE implementations for functions handling image
    conversion for WebGL 2.
    
    Speedup on Ubuntu Intel(R) Core(TM) i7-4790K CPU @ 4.00GHz:
    
    ---Pack RGBA8 To RA8---
    
    Simple: 0.015652
    SSE2: 0.003849
    Speedup: 4.066906
    
    ---UNPack RGBA5551 To RGBA8---
    
    Simple: 0.018795
    SSE2: 0.003649
    Speedup: 5.151343
    
    
    ---UNPack RGBA4444 to RGBA8---
    
    Simple: 0.021212
    SSE2: 0.003640
    Speedup: 5.827220
    
    BUG=497352
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel,mac_blink_rel,win_blink_rel
    
    Review URL: https://codereview.chromium.org/1243423002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199639 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: d279de75633e23a4e0c71c79e501e42209326add

--- third_party/WebKit/Source/platform/graphics/cpu/x86/WebGLImageConversionSSE.h
+++ third_party/WebKit/Source/platform/graphics/cpu/x86/WebGLImageConversionSSE.h
@@ -13,6 +13,99 @@ namespace blink {
 
 namespace SIMD {
 
+ALWAYS_INLINE void unpackOneRowOfRGBA4444LittleToRGBA8(const uint16_t*& source, uint8_t*& destination, unsigned& pixelsPerRow)
+{
+    __m128i immediate0x0f = _mm_set1_epi16(0x0F);
+    unsigned pixelsPerRowTrunc = (pixelsPerRow / 8) * 8;
+    for (unsigned i = 0; i < pixelsPerRowTrunc; i += 8) {
+
+        __m128i packedValue = _mm_loadu_si128((__m128i*)source);
+        __m128i r = _mm_srli_epi16(packedValue, 12);
+        __m128i g = _mm_and_si128(_mm_srli_epi16(packedValue, 8), immediate0x0f);
+        __m128i b = _mm_and_si128(_mm_srli_epi16(packedValue, 4), immediate0x0f);
+        __m128i a = _mm_and_si128(packedValue, immediate0x0f);
+        __m128i componentR = _mm_or_si128(_mm_slli_epi16(r, 4), r);
+        __m128i componentG = _mm_or_si128(_mm_slli_epi16(g, 4), g);
+        __m128i componentB = _mm_or_si128(_mm_slli_epi16(b, 4), b);
+        __m128i componentA = _mm_or_si128(_mm_slli_epi16(a, 4), a);
+
+        __m128i componentRG = _mm_or_si128(_mm_slli_epi16(componentG, 8), componentR);
+        __m128i componentBA = _mm_or_si128(_mm_slli_epi16(componentA, 8), componentB);
+        __m128i componentRGBA1 = _mm_unpackhi_epi16(componentRG, componentBA);
+        __m128i componentRGBA2 = _mm_unpacklo_epi16(componentRG, componentBA);
+
+        _mm_storeu_si128((__m128i*)destination, componentRGBA2);
+        _mm_storeu_si128((__m128i*)(destination + 16), componentRGBA1);
+
+        source += 8;
+        destination += 32;
+    }
+    pixelsPerRow -= pixelsPerRowTrunc;
+}
+
+ALWAYS_INLINE void unpackOneRowOfRGBA5551LittleToRGBA8(const uint16_t*& source, uint8_t*& destination, unsigned& pixelsPerRow)
+{
+    __m128i immediate0x1f = _mm_set1_epi16(0x1F);
+    __m128i immediate0x7 = _mm_set1_epi16(0x7);
+    __m128i immediate0x1 = _mm_set1_epi16(0x1);
+    unsigned pixelsPerRowTrunc = (pixelsPerRow / 8) * 8;
+    for (unsigned i = 0; i < pixelsPerRowTrunc; i += 8) {
+
+        __m128i packedValue = _mm_loadu_si128((__m128i*)source);
+        __m128i r = _mm_srli_epi16(packedValue, 11);
+        __m128i g = _mm_and_si128(_mm_srli_epi16(packedValue, 6), immediate0x1f);
+        __m128i b = _mm_and_si128(_mm_srli_epi16(packedValue, 1), immediate0x1f);
+        __m128i componentR = _mm_or_si128(_mm_slli_epi16(r, 3), _mm_and_si128(r, immediate0x7));
+        __m128i componentG = _mm_or_si128(_mm_slli_epi16(g, 3), _mm_and_si128(g, immediate0x7));
+        __m128i componentB = _mm_or_si128(_mm_slli_epi16(b, 3), _mm_and_si128(b, immediate0x7));
+        __m128i componentA = _mm_cmpeq_epi16(_mm_and_si128(packedValue, immediate0x1), immediate0x1);
+
+        __m128i componentRG = _mm_or_si128(_mm_slli_epi16(componentG, 8), componentR);
+        __m128i componentBA = _mm_or_si128(_mm_slli_epi16(componentA, 8), componentB);
+        __m128i componentRGBA1 = _mm_unpackhi_epi16(componentRG, componentBA);
+        __m128i componentRGBA2 = _mm_unpacklo_epi16(componentRG, componentBA);
+
+        _mm_storeu_si128((__m128i*)destination, componentRGBA2);
+        _mm_storeu_si128((__m128i*)(destination + 16), componentRGBA1);
+
+        source += 8;
+        destination += 32;
+    }
+    pixelsPerRow -= pixelsPerRowTrunc;
+}
+
+ALWAYS_INLINE void packOneRowOfRGBA8LittleToRA8(const uint8_t*& source, uint8_t*& destination, unsigned& pixelsPerRow)
+{
+
+    float tmp[4];
+    unsigned pixelsPerRowTrunc = (pixelsPerRow / 4) * 4;
+    const __m128 maxPixelValue = _mm_set1_ps(255.0);
+    for (unsigned i = 0; i < pixelsPerRowTrunc; i += 4) {
+        __m128 scaleFactor = _mm_set_ps(source[15] ? source[15] : 255.0,
+            source[11] ? source[11] : 255.0,
+            source[7] ? source[7] : 255.0,
+            source[3] ? source[3] : 255.0);
+        __m128 sourceR = _mm_set_ps(source[12], source[8], source[4], source[0]);
+
+        sourceR = _mm_mul_ps(sourceR, _mm_div_ps(maxPixelValue, scaleFactor));
+
+        _mm_storeu_ps(tmp, sourceR);
+
+        destination[0] = static_cast<uint8_t>(tmp[0]);
+        destination[1] = static_cast<uint8_t>(source[3]);
+        destination[2] = static_cast<uint8_t>(tmp[1]);
+        destination[3] = static_cast<uint8_t>(source[7]);
+        destination[4] = static_cast<uint8_t>(tmp[2]);
+        destination[5] = static_cast<uint8_t>(source[11]);
+        destination[6] = static_cast<uint8_t>(tmp[3]);
+        destination[7] = static_cast<uint8_t>(source[15]);
+
+        source += 16;
+        destination += 8;
+    }
+    pixelsPerRow -= pixelsPerRowTrunc;
+}
+
 ALWAYS_INLINE void unpackOneRowOfBGRA8LittleToRGBA8(const uint32_t*& source, uint32_t*& destination, unsigned& pixelsPerRow)
 {
     __m128i bgra, rgba;
--- third_party/WebKit/Source/platform/graphics/gpu/WebGLImageConversion.cpp
+++ third_party/WebKit/Source/platform/graphics/gpu/WebGLImageConversion.cpp
@@ -441,6 +441,9 @@ template<> void unpack<WebGLImageConversion::DataFormatBGRA8, uint8_t, uint8_t>(
 
 template<> void unpack<WebGLImageConversion::DataFormatRGBA5551, uint16_t, uint8_t>(const uint16_t* source, uint8_t* destination, unsigned pixelsPerRow)
 {
+#if CPU(X86) || CPU(X86_64)
+    SIMD::unpackOneRowOfRGBA5551LittleToRGBA8(source, destination, pixelsPerRow);
+#endif
 #if HAVE(ARM_NEON_INTRINSICS)
     SIMD::unpackOneRowOfRGBA5551ToRGBA8(source, destination, pixelsPerRow);
 #endif
@@ -460,6 +463,9 @@ template<> void unpack<WebGLImageConversion::DataFormatRGBA5551, uint16_t, uint8
 
 template<> void unpack<WebGLImageConversion::DataFormatRGBA4444, uint16_t, uint8_t>(const uint16_t* source, uint8_t* destination, unsigned pixelsPerRow)
 {
+#if CPU(X86) || CPU(X86_64)
+    SIMD::unpackOneRowOfRGBA4444LittleToRGBA8(source, destination, pixelsPerRow);
+#endif
 #if HAVE(ARM_NEON_INTRINSICS)
     SIMD::unpackOneRowOfRGBA4444ToRGBA8(source, destination, pixelsPerRow);
 #endif
@@ -660,6 +666,9 @@ template<> void pack<WebGLImageConversion::DataFormatRA8, WebGLImageConversion::
 // FIXME: this routine is lossy and must be removed.
 template<> void pack<WebGLImageConversion::DataFormatRA8, WebGLImageConversion::AlphaDoUnmultiply, uint8_t, uint8_t>(const uint8_t* source, uint8_t* destination, unsigned pixelsPerRow)
 {
+#if CPU(X86) || CPU(X86_64)
+    SIMD::packOneRowOfRGBA8LittleToRA8(source, destination, pixelsPerRow);
+#endif
     for (unsigned i = 0; i < pixelsPerRow; ++i) {
         float scaleFactor = source[3] ? 255.0f / source[3] : 1.0f;
         uint8_t sourceR = static_cast<uint8_t>(static_cast<float>(source[0]) * scaleFactor);
