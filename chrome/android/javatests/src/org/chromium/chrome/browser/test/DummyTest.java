eac67473354d6b414a02c889e7ed39a948d29038
pkotwicz <pkotwicz@chromium.org>
2015-07-29 07:03:44 -0700

    Reland of Move DummyTest from ChromeShellTest to ChromePublicTest (patchset #1 id:1 of https://codereview.chromium.org/1256813002/)
    Reason for revert:
    The webkit bot should now be fixed not to run ChromeShellTest host driven tests
    
    Original issue's description:
    > Revert of Move DummyTest from ChromeShellTest to ChromePublicTest (patchset #1 id:1 of https://codereview.chromium.org/1243773005/)
    >
    > Reason for revert:
    > Revert patch suspected for causing test failure
    >
    > Test: HostDriven_DummyTest.testPass
    > Builder: Android Tests (dbg)
    > Build: https://build.chromium.org/p/chromium.webkit/builders/Android%20Tests%20%28dbg%29/builds/27205
    >
    > Original issue's description:
    > > Move DummyTest from ChromeShellTest to ChromePublicTest
    > >
    > > BUG=505040
    > > TEST=None
    > >
    > > Committed: https://crrev.com/2592396514d7011b8d0b7a711ebe04d6e0e4c1d9 [formerly e51c61f0fe65248104069ba5d9c2e489383cd530]
    > > Cr-Commit-Position: refs/heads/master@{#340121}
    >
    > TBR=jbudorick@chromium.org,tedchoc@chromium.org,pkotwicz@chromium.org
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=505040
    >
    > Committed: https://crrev.com/973d3ab7e3ad57c3a43774df203c3baecea16c47 [formerly 5be8e8b6b8f5297bfc14243f196a6d2ed47292dd]
    > Cr-Commit-Position: refs/heads/master@{#340252}
    
    TBR=jbudorick@chromium.org,tedchoc@chromium.org,alancutter@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=505040
    
    Review URL: https://codereview.chromium.org/1265653002
    
    Cr-Commit-Position: refs/heads/master@{#340885}
    
    Former-commit-id: d133b72bbfbc013a41b20fde16bc2afcbec873d0

--- chrome/android/javatests/src/org/chromium/chrome/browser/test/DummyTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/test/DummyTest.java
@@ -5,12 +5,20 @@
 package org.chromium.chrome.browser.test;
 
 import org.chromium.base.test.util.HostDrivenTest;
-import org.chromium.chrome.shell.ChromeShellTestBase;
+import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.chrome.test.ChromeActivityTestCaseBase;
 
 /**
  * Dummy test suite for verifying the host-driven test framework.
  */
-public class DummyTest extends ChromeShellTestBase {
+public class DummyTest extends ChromeActivityTestCaseBase<ChromeActivity> {
+    public DummyTest() {
+        super(ChromeActivity.class);
+    }
+
+    @Override
+    public void startMainActivity() {}
+
     @HostDrivenTest
     public void testPass() {}
 }
