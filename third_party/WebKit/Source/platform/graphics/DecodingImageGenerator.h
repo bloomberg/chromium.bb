9d18388f045ec641f77027720e5473164a83f1fd
fmalita@chromium.org <fmalita@chromium.org>
2015-07-29 01:19:13 +0000

    Prepare DecodingImageGenerator for SkImageGenerator API switch
    Add temporary ifdefs to facilitate migration to the new SkImageGenerator
    API.  These are to be removed as soon as we drop
    SK_LEGACY_IMAGE_GENERATOR_ENUMS_AND_OPTIONS from Chromium's
    SkUserConfig.h (http://crrev.com/1262623005).
    
    R=reed@google.com
    
    Review URL: https://codereview.chromium.org/1261123002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199618 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: b10039e6e972a1a66a4217085d0e5ac97f3051f8

--- third_party/WebKit/Source/platform/graphics/DecodingImageGenerator.h
+++ third_party/WebKit/Source/platform/graphics/DecodingImageGenerator.h
@@ -54,7 +54,15 @@ public:
 
 protected:
     SkData* onRefEncodedData() override;
-    Result onGetPixels(const SkImageInfo&, void* pixels, size_t rowBytes, const Options&, SkPMColor ctable[], int* ctableCount) override;
+
+#ifdef SK_LEGACY_IMAGE_GENERATOR_ENUMS_AND_OPTIONS
+    Result onGetPixels(const SkImageInfo&, void* pixels, size_t rowBytes, const Options&,
+        SkPMColor ctable[], int* ctableCount) override;
+#else
+    bool onGetPixels(const SkImageInfo&, void* pixels, size_t rowBytes,
+        SkPMColor ctable[], int* ctableCount) override;
+#endif
+
     bool onGetYUV8Planes(SkISize sizes[3], void* planes[3], size_t rowBytes[3], SkYUVColorSpace*) override;
 
 private:
