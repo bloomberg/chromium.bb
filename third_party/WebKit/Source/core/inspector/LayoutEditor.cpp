963a23bf31ce9fea6cf36fc39b161d4df9ab154b
sergeyv@chromium.org <sergeyv@chromium.org>
2015-07-30 15:41:59 +0000

    Devtools[LayoutEditor]: Add margin handles
    BUG=
    
    Review URL: https://codereview.chromium.org/1268483002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@199742 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ce99c08b3372a5bfbbab3ec3e9dcf1bf7ad37d67

--- third_party/WebKit/Source/core/inspector/LayoutEditor.cpp
+++ third_party/WebKit/Source/core/inspector/LayoutEditor.cpp
@@ -69,6 +69,46 @@ FloatQuad orthogonalVectors(const FloatQuad& quad)
     return result;
 }
 
+float det(float a11, float a12, float a21, float a22)
+{
+    return a11 * a22 - a12 * a21;
+}
+
+FloatPoint projection(const FloatPoint& anchor, const FloatPoint& orthogonalVector, const FloatPoint& point)
+{
+    float a1 = orthogonalVector.x();
+    float b1 = orthogonalVector.y();
+    float c1 = - anchor.x() * a1 - anchor.y() * b1;
+
+    float a2 = orthogonalVector.y();
+    float b2 = - orthogonalVector.x();
+    float c2 = - point.x() * a2 - point.y() * b2;
+    float d = det(a1, b1, a2, b2);
+    ASSERT(d < 0);
+    float x = - det(c1, b1, c2, b2) / d;
+    float y = - det(a1, c1, a2, c2) / d;
+    return FloatPoint(x, y);
+}
+
+FloatPoint translatePoint(const FloatPoint& point, const FloatPoint& orthogonalVector, float distance)
+{
+    float d = sqrt(orthogonalVector.x() * orthogonalVector.x() + orthogonalVector.y() * orthogonalVector.y());
+    float dx = - orthogonalVector.y() * distance / d;
+    float dy = orthogonalVector.x() * distance / d;
+    return FloatPoint(point.x() + dx, point.y() + dy);
+}
+
+FloatQuad translateAndProject(const FloatQuad& origin, const FloatQuad& orthogonals, const FloatQuad& projectOn, float distance)
+{
+    FloatQuad result;
+    result.setP1(projection(projectOn.p1(), orthogonals.p1(), translatePoint(origin.p1(), orthogonals.p1(), distance)));
+    result.setP2(projection(projectOn.p2(), orthogonals.p2(), translatePoint(origin.p2(), orthogonals.p2(), distance)));
+    // We want to translate at top and bottom point in the same direction, so we use p1 here.
+    result.setP3(projection(projectOn.p3(), orthogonals.p3(), translatePoint(origin.p3(), orthogonals.p1(), distance)));
+    result.setP4(projection(projectOn.p4(), orthogonals.p4(), translatePoint(origin.p4(), orthogonals.p2(), distance)));
+    return result;
+}
+
 } // namespace
 
 LayoutEditor::LayoutEditor(InspectorCSSAgent* cssAgent)
@@ -97,19 +137,25 @@ PassRefPtr<JSONObject> LayoutEditor::buildJSONInfo() const
     if (!m_element)
         return nullptr;
 
-    FloatQuad padding, unused;
-    InspectorHighlight::buildNodeQuads(m_element.get(), &unused, &padding, &unused, &unused);
-
-    FloatQuad paddingMeans = means(padding);
-    FloatQuad paddingOrthogonalVectors = orthogonalVectors(padding);
+    FloatQuad content, padding, border, margin;
+    InspectorHighlight::buildNodeQuads(m_element.get(), &content, &padding, &border, &margin);
+    FloatQuad orthogonals = orthogonalVectors(padding);
 
     RefPtr<JSONObject> object = JSONObject::create();
     RefPtr<JSONArray> anchors = JSONArray::create();
+    FloatQuad contentMeans = means(content);
 
-    appendAnchorFor(anchors.get(), "padding-top", paddingMeans.p1(), paddingOrthogonalVectors.p1());
-    appendAnchorFor(anchors.get(), "padding-right", paddingMeans.p2(), paddingOrthogonalVectors.p2());
-    appendAnchorFor(anchors.get(), "padding-bottom", paddingMeans.p3(), paddingOrthogonalVectors.p3());
-    appendAnchorFor(anchors.get(), "padding-left", paddingMeans.p4(), paddingOrthogonalVectors.p4());
+    FloatQuad paddingHandles = translateAndProject(contentMeans, orthogonals, padding, 0);
+    appendAnchorFor(anchors.get(), "padding-top", paddingHandles.p1(), orthogonals.p1());
+    appendAnchorFor(anchors.get(), "padding-right", paddingHandles.p2(), orthogonals.p2());
+    appendAnchorFor(anchors.get(), "padding-bottom", paddingHandles.p3(), orthogonals.p3());
+    appendAnchorFor(anchors.get(), "padding-left", paddingHandles.p4(), orthogonals.p4());
+
+    FloatQuad marginHandles = translateAndProject(contentMeans, orthogonals, margin, 12);
+    appendAnchorFor(anchors.get(), "margin-top", marginHandles.p1(), orthogonals.p1());
+    appendAnchorFor(anchors.get(), "margin-right", marginHandles.p2(), orthogonals.p2());
+    appendAnchorFor(anchors.get(), "margin-bottom", marginHandles.p3(), orthogonals.p3());
+    appendAnchorFor(anchors.get(), "margin-left", marginHandles.p4(), orthogonals.p4());
 
     object->setArray("anchors", anchors.release());
     return object.release();
