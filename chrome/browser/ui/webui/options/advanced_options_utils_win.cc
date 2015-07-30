193f3a03cb1fc03b4713eb49061d17ba9f91fdce
davidben <davidben@chromium.org>
2015-07-30 16:37:11 -0700

    Run the certificate management dialog with BaseShellDialogImpl.
    This avoids the nested event loop.
    
    BUG=513113
    TEST=Set Windows to 'classic' theme.
         Open Settings -> Show advanced settings -> Manage certificates
         Move the dialog around. See screenshot in bug.
    
    Review URL: https://codereview.chromium.org/1250823005
    
    Cr-Commit-Position: refs/heads/master@{#341224}
    
    Former-commit-id: 307679f882171bdaca4f5cc2015a93bb9fd7ef24

--- chrome/browser/ui/webui/options/advanced_options_utils_win.cc
+++ chrome/browser/ui/webui/options/advanced_options_utils_win.cc
@@ -10,17 +10,64 @@
 #include <shellapi.h>
 
 #include "base/bind.h"
+#include "base/bind_helpers.h"
+#include "base/location.h"
+#include "base/message_loop/message_loop.h"
 #include "base/path_service.h"
 #include "base/threading/thread.h"
 #include "chrome/browser/browser_process.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
+#include "ui/shell_dialogs/base_shell_dialog_win.h"
 #include "ui/views/win/hwnd_util.h"
 
 using content::BrowserThread;
 using content::WebContents;
 
 namespace options {
+
+namespace {
+
+// Shows a Windows certificate management dialog on the dialog thread.
+class ManageCertificatesDialog : public ui::BaseShellDialogImpl {
+ public:
+  ManageCertificatesDialog() {}
+
+  // Shows the dialog and calls |callback| when the dialog closes. The caller
+  // must ensure the ManageCertificatesDialog remains valid until then.
+  void Show(HWND parent, const base::Closure& callback) {
+    if (IsRunningDialogForOwner(parent)) {
+      base::MessageLoop::current()->PostTask(FROM_HERE, callback);
+      return;
+    }
+
+    RunState run_state = BeginRun(parent);
+    run_state.dialog_thread->task_runner()->PostTaskAndReply(
+        FROM_HERE, base::Bind(&ManageCertificatesDialog::ShowOnDialogThread,
+                              base::Unretained(this), run_state),
+        base::Bind(&ManageCertificatesDialog::OnDialogClosed,
+                   base::Unretained(this), run_state, callback));
+  }
+
+ private:
+  void ShowOnDialogThread(const RunState& run_state) {
+    CRYPTUI_CERT_MGR_STRUCT cert_mgr = {0};
+    cert_mgr.dwSize = sizeof(CRYPTUI_CERT_MGR_STRUCT);
+    cert_mgr.hwndParent = run_state.owner;
+    ::CryptUIDlgCertMgr(&cert_mgr);
+  }
+
+  void OnDialogClosed(const RunState& run_state,
+                      const base::Closure& callback) {
+    EndRun(run_state);
+    // May delete |this|.
+    callback.Run();
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(ManageCertificatesDialog);
+};
+
+}  // namespace
 
 // Callback that opens the Internet Options control panel dialog with the
 // Connections tab selected.
@@ -58,11 +105,13 @@ void AdvancedOptionsUtilities::ShowNetworkProxySettings(
 
 void AdvancedOptionsUtilities::ShowManageSSLCertificates(
       WebContents* web_contents) {
-  CRYPTUI_CERT_MGR_STRUCT cert_mgr = { 0 };
-  cert_mgr.dwSize = sizeof(CRYPTUI_CERT_MGR_STRUCT);
-  cert_mgr.hwndParent = views::HWNDForNativeWindow(
-      web_contents->GetTopLevelNativeWindow());
-  ::CryptUIDlgCertMgr(&cert_mgr);
+  HWND parent =
+      views::HWNDForNativeWindow(web_contents->GetTopLevelNativeWindow());
+
+  ManageCertificatesDialog* dialog = new ManageCertificatesDialog;
+  dialog->Show(
+      parent,
+      base::Bind(&base::DeletePointer<ManageCertificatesDialog>, dialog));
 }
 
 }  // namespace options
