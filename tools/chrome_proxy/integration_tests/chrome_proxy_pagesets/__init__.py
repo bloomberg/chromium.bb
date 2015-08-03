244198bd91c6b63ef4b39ca9d3abd99cd48855b2
aiolos <aiolos@chromium.org>
2015-08-03 14:41:10 -0700

    Revert of Create classes_util API, change discover to return a list instead of a dict. (patchset #1 id:1 of https://codereview.chromium.org/1262623007/)
    Reason for revert:
    Landed even though the patch failed.
    
    Original issue's description:
    > Reland of Create classes_util API, change discover to return a list instead of a dict. (patchset #1 id:1 of https://codereview.chromium.org/1263063003/)
    >
    > Reason for revert:
    > The telemetry tests aren't broken. The perf_unittests are because they don't pick tests in a deterministic way. Reverting after a change to make them deterministic.
    >
    > Original issue's description:
    > > Revert of Create classes_util API, change discover to return a list instead of a dict. (patchset #5 id:120001 of https://codereview.chromium.org/1244223002/)
    > >
    > > Reason for revert:
    > > This appears to break the XP telemetry tests. Details on the bug.
    > >
    > > Original issue's description:
    > > > Create classes_util API, change discover to return a list instead of a dict.
    > > >
    > > > BUG=498968
    > > > CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    > > >
    > > > Committed: https://crrev.com/2544b13f09e38a3b38f75361d6b8cf5a615f38bf [formerly e6cbec4747aa7fe3c96b1cdb89de21ae77b30ab0]
    > > > Cr-Commit-Position: refs/heads/master@{#341129}
    > >
    > > TBR=dtu@chromium.org,bengr@chromium.org,aiolos@chromium.org
    > > NOPRESUBMIT=true
    > > NOTREECHECKS=true
    > > NOTRY=true
    > > BUG=498968
    > >
    > > Committed: https://crrev.com/19265e625cf0b51926c2cbe1cb4538a986fa6a13 [formerly c85e3e1af7e170d5f8ec2b012df6e9337d2352bb]
    > > Cr-Commit-Position: refs/heads/master@{#341218}
    >
    > TBR=dtu@chromium.org,bengr@chromium.org,avi@chromium.org
    > BUG=498968
    >
    > Committed: https://crrev.com/235f4b4ba986a416c7de0f5b77ee5f762ef084b0 [formerly 2c1531af216c026be0b4aed672dc89d264f95a72]
    > Cr-Commit-Position: refs/heads/master@{#341603}
    
    TBR=dtu@chromium.org,bengr@chromium.org,avi@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=498968
    
    Review URL: https://codereview.chromium.org/1272433002
    
    Cr-Commit-Position: refs/heads/master@{#341621}
    
    Former-commit-id: fd9cce5babbe6f4341793c2cd21753a9fe773244

--- tools/chrome_proxy/integration_tests/chrome_proxy_pagesets/__init__.py
+++ tools/chrome_proxy/integration_tests/chrome_proxy_pagesets/__init__.py
@@ -6,9 +6,8 @@ import inspect
 import os
 import sys
 
+from telemetry.core import discover
 from telemetry import story
-from telemetry.util import classes_util
-
 import video
 
 
@@ -16,7 +15,8 @@ import video
 start_dir = os.path.dirname(os.path.abspath(__file__))
 top_level_dir = os.path.abspath(os.path.join(start_dir, os.pardir, os.pardir))
 base_class = story.StorySet
-for cls in classes_util.DiscoverClasses(start_dir, top_level_dir, base_class):
+for cls in discover.DiscoverClasses(
+    start_dir, top_level_dir, base_class).values():
   setattr(sys.modules[__name__], cls.__name__, cls)
 
 # DiscoverClasses makes the assumption that there is exactly one matching
