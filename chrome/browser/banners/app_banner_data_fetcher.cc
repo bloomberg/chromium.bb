d29a5c1ed7cd825bf36f573d8a61974f5c6dda44
dominickn <dominickn@chromium.org>
2015-08-03 19:16:56 -0700

    Make desktop banner prompts respect bypass flag when a web app is already installed.
    Desktop app banners are currently not shown if the web app has already
    been added to the shelf. This should be overruled by
    --bypass-app-banner-engagement-checks
    
    BUG=513582
    R=benwells@chromium.org
    
    Review URL: https://codereview.chromium.org/1259543002
    
    Cr-Commit-Position: refs/heads/master@{#341671}
    
    Former-commit-id: 98d5901a6cd71bfdca36e92db2f51015df000ba8

--- chrome/browser/banners/app_banner_data_fetcher.cc
+++ chrome/browser/banners/app_banner_data_fetcher.cc
@@ -5,6 +5,7 @@
 #include "chrome/browser/banners/app_banner_data_fetcher.h"
 
 #include "base/bind.h"
+#include "base/command_line.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/banners/app_banner_debug_log.h"
@@ -14,6 +15,7 @@
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/manifest/manifest_icon_selector.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/render_messages.h"
 #include "components/rappor/rappor_utils.h"
 #include "content/public/browser/browser_context.h"
@@ -284,7 +286,9 @@ void AppBannerDataFetcher::OnDidGetManifest(
   app_title_ = web_app_data_.name.string();
 
   if (IsWebAppInstalled(web_contents->GetBrowserContext(),
-                        manifest.start_url)) {
+                        manifest.start_url) &&
+      !base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kBypassAppBannerEngagementChecks)) {
     OutputDeveloperNotShownMessage(web_contents, kBannerAlreadyAdded);
     Cancel();
     return;
