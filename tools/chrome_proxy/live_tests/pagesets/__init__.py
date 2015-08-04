c4a5ba0458c05fa7f7a39fabadbc1966427ab1ff
jochen <jochen@chromium.org>
2015-08-04 00:05:43 -0700

    Revert of Create classes_util API, change discover to return a list instead of a dict. (patchset #2 id:280001 of https://codereview.chromium.org/1262623007/ )
    Reason for revert:
    fails reliably on XP tests: https://build.chromium.org/p/chromium.win/builders/XP%20Tests%20(1)/builds/39209/steps/telemetry_perf_unittests/logs/stdio
    
    Original issue's description:
    > Reland of Create classes_util API, change discover to return a list instead of a dict. (patchset #1 id:1 of https://codereview.chromium.org/1263063003/)
    >
    > Reason for revert:
    > The telemetry tests aren't broken. The perf_unittests are because they don't pick tests in a deterministic way. Reverting after a change to make them deterministic.
    >
    > Original issue's description:
    > > Revert of Create classes_util API, change discover to return a list instead of a dict. (patchset #5 id:120001 of https://codereview.chromium.org/1244223002/)
    > >
    > > Reason for revert:
    > > This appears to break the XP telemetry tests. Details on the bug.
    > >
    > > Original issue's description:
    > > > Create classes_util API, change discover to return a list instead of a dict.
    > > >
    > > > BUG=498968
    > > > CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    > > >
    > > > Committed: https://crrev.com/2544b13f09e38a3b38f75361d6b8cf5a615f38bf [formerly e6cbec4747aa7fe3c96b1cdb89de21ae77b30ab0]
    > > > Cr-Commit-Position: refs/heads/master@{#341129}
    > >
    > > TBR=dtu@chromium.org,bengr@chromium.org,aiolos@chromium.org
    > > NOPRESUBMIT=true
    > > NOTREECHECKS=true
    > > NOTRY=true
    > > BUG=498968
    > >
    > > Committed: https://crrev.com/19265e625cf0b51926c2cbe1cb4538a986fa6a13 [formerly c85e3e1af7e170d5f8ec2b012df6e9337d2352bb]
    > > Cr-Commit-Position: refs/heads/master@{#341218}
    >
    > TBR=dtu@chromium.org,bengr@chromium.org,avi@chromium.org
    > BUG=498968
    >
    > CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    >
    > Committed: https://crrev.com/0df0ad3ff9386c749a621ae6f9293230e2a907f0 [formerly 7e6fefcf3b839256c97e2e7bc0bfc3c53af75c8d]
    > Cr-Commit-Position: refs/heads/master@{#341646}
    
    TBR=dtu@chromium.org,bengr@chromium.org,avi@chromium.org,aiolos@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=498968
    
    Review URL: https://codereview.chromium.org/1267243002
    
    Cr-Commit-Position: refs/heads/master@{#341692}
    
    Former-commit-id: bf85d83ff178f0ce51e4f91641ab7887397a5e20

--- tools/chrome_proxy/live_tests/pagesets/__init__.py
+++ tools/chrome_proxy/live_tests/pagesets/__init__.py
@@ -6,14 +6,15 @@ import inspect
 import os
 import sys
 
+from telemetry.core import discover
 from telemetry import story
-from telemetry.util import classes_util
 
 
 # Import all submodules' StorySet classes.
 start_dir = os.path.dirname(os.path.abspath(__file__))
 top_level_dir = os.path.abspath(os.path.join(start_dir, os.pardir, os.pardir))
 base_class = story.StorySet
-for cls in classes_util.DiscoverClasses(start_dir, top_level_dir, base_class):
+for cls in discover.DiscoverClasses(
+    start_dir, top_level_dir, base_class).values():
   setattr(sys.modules[__name__], cls.__name__, cls)
 
