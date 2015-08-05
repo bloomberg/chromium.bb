de2e851bd0abf16f888fe6bb71ccd40fa1e551f6
liaoyuke <liaoyuke@chromium.org>
2015-08-05 13:37:04 -0700

    Update usb_key_codes.h to support RepeatInvertingSingleCharacter test case
    BUG=
    
    Review URL: https://codereview.chromium.org/1260693008
    
    Cr-Commit-Position: refs/heads/master@{#341963}
    
    Former-commit-id: 8d886efa9b1dea591bd37d6dca0fd4aac8506601

--- remoting/protocol/usb_key_codes.h
+++ remoting/protocol/usb_key_codes.h
@@ -6,8 +6,11 @@
 #define REMOTING_PROTOCOL_USB_KEY_CODES_H_
 
 // Frequently used USB key-codes (not comprehensive--add more as needed).
+// TODO(liaoyuke): This could be cleaned up by reusing the data in
+// "ui/events/keycodes/dom/keycode_converter_data.inc".
 
 enum {
+  kUsbA            = 0x070004,
   kUsbN            = 0x070011,
   kUsbW            = 0x07001a,
   kUsbEnter        = 0x070028,
@@ -20,6 +23,7 @@ enum {
   kUsbLeftShift    = 0x0700e1,
   kUsbLeftAlt      = 0x0700e2,
   kUsbLeftOs       = 0x0700e3,
+  kUsbRightArrow   = 0x07004f,
   kUsbRightControl = 0x0700e4,
   kUsbRightShift   = 0x0700e5,
   kUsbRightAlt     = 0x0700e6,
