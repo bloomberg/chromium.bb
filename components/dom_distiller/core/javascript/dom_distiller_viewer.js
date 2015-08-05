ee9db1f6098dafe963a46a7f96257a4b5a019f0f
wychen <wychen@chromium.org>
2015-08-05 09:40:13 -0700

    In reader mode, set theme-color meta tag for toolbar
    BUG=485171
    
    Review URL: https://codereview.chromium.org/1242323005
    
    Cr-Commit-Position: refs/heads/master@{#341914}
    
    Former-commit-id: 44f27c52db7ee29b60d45b8ac8146cdee342de4f

--- components/dom_distiller/core/javascript/dom_distiller_viewer.js
+++ components/dom_distiller/core/javascript/dom_distiller_viewer.js
@@ -77,17 +77,23 @@ function useFontFamily(fontFamily) {
 // CSS classes must agree with distilledpage.css.
 function useTheme(theme) {
   var cssClass;
+  var toolbarColor;
   if (theme == "sepia") {
     cssClass = "sepia";
+    toolbarColor = "#BF9A73";
   } else if (theme == "dark") {
     cssClass = "dark";
+    toolbarColor = "#1A1A1A";
   } else {
     cssClass = "light";
+    toolbarColor = "#F5F5F5";
   }
   // Relies on the classname order of the body being Theme class, then Font
   // Family class.
   var fontFamilyClass = document.body.className.split(" ")[1];
   document.body.className = cssClass + " " + fontFamilyClass;
+
+  document.getElementById('theme-color').content = toolbarColor;
 }
 
 var updateLoadingIndicator = function() {
