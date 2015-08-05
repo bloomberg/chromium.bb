37206f635a9046f52466180fb85db2a960cedcd2
keishi@chromium.org <keishi@chromium.org>
2015-08-05 04:13:13 +0000

    Revert of mac: Use a placeholder string for the family name of the system font. (patchset #8 id:140001 of https://codereview.chromium.org/1266403002/ )
    Reason for revert:
    Suspect for breaking WebKit Mac10.10 bot.
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.10/builds/1796
    blame range is blink 199996:199998
    
    Original issue's description:
    > mac: Use a placeholder string for the family name of the system font.
    > 
    > This CL is inspired by mmaxfield@apple.com and
    > http://trac.webkit.org/changeset/184353.
    > 
    > The system font varies by OS version, as well as the version of the OSX SDK that
    > the binary is compiled against. This CL uses a placeholder string for the family
    > name of the system font that is passed around.
    > 
    > This CL has two positive effects.
    > 
    > 1) Logic in LayoutTestControl* was checking the family name of the font directly
    > against the string "Lucida Grande". This was broken any time the system font
    > wasn't "Lucida Grande".
    > 2) Layout tests were failing on OSX 10.10, 10.11, and when the binary was
    > compiled against the OSX 10.10 SDK. This CL fixes this particular set of
    > problems by always using the font "Lucida Grande" for layout tests.
    > 
    > This CL has one non-regression behavior change. If the font family of a text
    > control is set to "Lucida Grande", its width is no longer modified by the
    > special logic in LayoutTestControl*. This isn't a regression because there is no
    > reason to expect the special logic of LayoutTestControl to apply to the text
    > control to begin with (since "Lucida Grande" may or may not be the system font).
    > If we really want the system font to always have this behavior, we should
    > implement font family keywords like "-apple-system". See 515989 for more
    > details.
    > 
    > BUG=515989, 515836
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=199998
    
    TBR=thakis@chromium.org,erikchen@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=515989, 515836
    
    Review URL: https://codereview.chromium.org/1274563002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200022 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: e6f4a461162c9f680d254dc7082dc4c29d1c3eaa

--- third_party/WebKit/Source/core/layout/LayoutThemeMac.mm
+++ third_party/WebKit/Source/core/layout/LayoutThemeMac.mm
@@ -238,7 +238,7 @@ void LayoutThemeMac::systemFont(CSSValueID systemFontID, FontStyle& fontStyle, F
     fontStyle = ([fontManager traitsOfFont:font] & NSItalicFontMask) ? FontStyleItalic : FontStyleNormal;
     fontWeight = toFontWeight([fontManager weightOfFont:font]);
     fontSize = [font pointSize];
-    fontFamily = @"BlinkMacSystemFont";
+    fontFamily = [font webCoreFamilyName];
 }
 
 static RGBA32 convertNSColorToColor(NSColor *color)
@@ -592,7 +592,7 @@ void LayoutThemeMac::setFontFromControlSize(ComputedStyle& style, NSControlSize 
     fontDescription.setGenericFamily(FontDescription::SerifFamily);
 
     NSFont* font = [NSFont systemFontOfSize:[NSFont systemFontSizeForControlSize:controlSize]];
-    fontDescription.firstFamily().setFamily(@"BlinkMacSystemFont");
+    fontDescription.firstFamily().setFamily([font webCoreFamilyName]);
     fontDescription.setComputedSize([font pointSize] * style.effectiveZoom());
     fontDescription.setSpecifiedSize([font pointSize] * style.effectiveZoom());
 
