88546ebc9d41c582e9786f822dd57a663a2ce531
groby <groby@chromium.org>
2015-08-06 10:27:20 -0700

    [Translate] Add unittest infrastructure for TranslatePref
    In preparation for further work on TranslatePref, this sets up
    a test fixture that allows unit tests for TranslatePref objects.
    
    BUG=none
    R=hajimehoshi@chromium.org
    
    Review URL: https://codereview.chromium.org/1263233006
    
    Cr-Commit-Position: refs/heads/master@{#342140}
    
    Former-commit-id: b358dd34ac2e5c8bad2816e1d45502c121f55f9a

--- components/translate/core/browser/translate_prefs_unittest.cc
+++ components/translate/core/browser/translate_prefs_unittest.cc
@@ -8,6 +8,8 @@
 #include <string>
 #include <vector>
 
+#include "base/prefs/scoped_user_pref_update.h"
+#include "components/pref_registry/testing_pref_service_syncable.h"
 #include "components/translate/core/browser/translate_download_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -119,6 +121,84 @@ TEST(TranslatePrefsTest, CreateBlockedLanguagesNonEnglishUI) {
                           *it));
     }
   }
+}
+
+class TranslatePrefTest : public testing::Test {
+ protected:
+  TranslatePrefTest() : prefs_(new user_prefs::TestingPrefServiceSyncable()) {
+#if defined(OS_CHROMEOS)
+    const char* preferred_languages_prefs =
+        "settings.language.preferred_languages";
+#else
+    const char* preferred_languages_prefs = NULL;
+#endif
+    translate_prefs_.reset(new translate::TranslatePrefs(
+        prefs_.get(), "intl.accept_languages", preferred_languages_prefs));
+    TranslatePrefs::RegisterProfilePrefs(prefs_->registry());
+  }
+
+  void SetLastDeniedTime(const std::string& language, base::Time time) {
+    DictionaryPrefUpdate update(
+        prefs_.get(), TranslatePrefs::kPrefTranslateLastDeniedTimeForLanguage);
+    update.Get()->SetDouble(language, time.ToJsTime());
+  }
+
+  base::Time GetLastDeniedTime(const std::string& language) {
+    const base::DictionaryValue* last_denied_time_dict = prefs_->GetDictionary(
+        TranslatePrefs::kPrefTranslateLastDeniedTimeForLanguage);
+    double time = 0;
+    bool result = last_denied_time_dict->GetDouble(language, &time);
+    if (!result)
+      return base::Time::Max();
+    return base::Time::FromJsTime(time);
+  }
+
+  scoped_ptr<user_prefs::TestingPrefServiceSyncable> prefs_;
+  scoped_ptr<translate::TranslatePrefs> translate_prefs_;
+};
+
+TEST_F(TranslatePrefTest, UpdateLastDeniedTime) {
+  const char kLanguage[] = "en";
+  base::Time now = base::Time::Now();
+  base::Time two_days_ago = now - base::TimeDelta::FromDays(2);
+
+  // Test that denials with more than 24 hours difference between them do not
+  // block the language.
+  translate_prefs_->ResetDenialState();
+  SetLastDeniedTime(kLanguage, two_days_ago);
+  ASSERT_FALSE(translate_prefs_->IsTooOftenDenied(kLanguage));
+  translate_prefs_->UpdateLastDeniedTime(kLanguage);
+  base::Time last_denied = GetLastDeniedTime(kLanguage);
+  EXPECT_FALSE(last_denied.is_max());
+  EXPECT_GE(last_denied, now);
+  EXPECT_LT(last_denied - now, base::TimeDelta::FromSeconds(10));
+  EXPECT_FALSE(translate_prefs_->IsTooOftenDenied(kLanguage));
+
+  // Ensure the first use simply writes the update time.
+  translate_prefs_->ResetDenialState();
+  translate_prefs_->UpdateLastDeniedTime(kLanguage);
+  last_denied = GetLastDeniedTime(kLanguage);
+  EXPECT_FALSE(last_denied.is_max());
+  EXPECT_GE(last_denied, now);
+  EXPECT_LT(last_denied - now, base::TimeDelta::FromSeconds(10));
+  EXPECT_FALSE(translate_prefs_->IsTooOftenDenied(kLanguage));
+
+  // If it's denied again within the 24 hour period, language should be
+  // permanently denied.
+  translate_prefs_->UpdateLastDeniedTime(kLanguage);
+  last_denied = GetLastDeniedTime(kLanguage);
+  EXPECT_FALSE(last_denied.is_max());
+  EXPECT_GE(last_denied, now);
+  EXPECT_LT(last_denied - now, base::TimeDelta::FromSeconds(10));
+  EXPECT_TRUE(translate_prefs_->IsTooOftenDenied(kLanguage));
+
+  // If the language is already permanently denied, don't bother updating the
+  // last_denied time.
+  ASSERT_TRUE(translate_prefs_->IsTooOftenDenied(kLanguage));
+  SetLastDeniedTime(kLanguage, two_days_ago);
+  translate_prefs_->UpdateLastDeniedTime(kLanguage);
+  last_denied = GetLastDeniedTime(kLanguage);
+  EXPECT_EQ(last_denied, two_days_ago);
 }
 
 }  // namespace translate
