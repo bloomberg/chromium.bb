92aacc31cd8dff87e9210e116254ee107bdd681a
bungeman <bungeman@chromium.org>
2015-08-06 13:18:42 -0700

    Remove SkTemplates.h include from pepper.
    Skia is looking to make SkTemplates.h private, since it isn't
    intended for external use. pepper_flash_renderer_host.cc included
    this file, but doesn't appear to use anything from it. From the
    history it looks like it used to be used for SkAutoSTMalloc, but
    that use was replaced with std::vector. This change removes the no
    longer needed include and simplifies the newer code.
    
    Review URL: https://codereview.chromium.org/1275653004
    
    Cr-Commit-Position: refs/heads/master@{#342189}
    
    Former-commit-id: acf5187cb4e9d94ff116b74f00ee7d92af5aa6b2

--- chrome/renderer/pepper/pepper_flash_renderer_host.cc
+++ chrome/renderer/pepper/pepper_flash_renderer_host.cc
@@ -30,7 +30,6 @@
 #include "third_party/skia/include/core/SkMatrix.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkPoint.h"
-#include "third_party/skia/include/core/SkTemplates.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 #include "ui/gfx/geometry/rect.h"
 #include "url/gurl.h"
@@ -271,9 +270,7 @@ int32_t PepperFlashRendererHost::OnDrawGlyphs(
   // Build up the skia advances.
   size_t glyph_count = params.glyph_indices.size();
   if (glyph_count) {
-    std::vector<SkPoint> storage;
-    storage.resize(glyph_count);
-    SkPoint* sk_positions = &storage[0];
+    std::vector<SkPoint> sk_positions(glyph_count);
     for (uint32_t i = 0; i < glyph_count; i++) {
       sk_positions[i].set(x, y);
       x += SkFloatToScalar(params.glyph_advances[i].x);
@@ -281,7 +278,7 @@ int32_t PepperFlashRendererHost::OnDrawGlyphs(
     }
 
     canvas->drawPosText(
-        &params.glyph_indices[0], glyph_count * 2, sk_positions, paint);
+        &params.glyph_indices[0], glyph_count * 2, &sk_positions[0], paint);
   }
 
   if (needs_unmapping)
