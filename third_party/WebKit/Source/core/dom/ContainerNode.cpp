c41a73382e73f5987eab70aecab1b749c17db5d6
kouhei@chromium.org <kouhei@chromium.org>
2015-08-06 06:10:13 +0000

    parserRemoveChild: Avoid unintended DOM modifications after user script run.
    Surprisingly, ContainerNode::parserRemoveChild may run arbitrary user script during its DOM modification if its target contained iframes.
    Before this CL, this could lead to corrupt DOM tree, as the target node could be moved during parserRemoveChild execution.
    
    This CL adds a bail-out if stmt after disconnecting child frame to abort if precondition has changed.
    
    BUG=516377
    
    Review URL: https://codereview.chromium.org/1277793002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200098 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2c36e1fa592c341f27f758cf8b6770957c9bfdd4

--- third_party/WebKit/Source/core/dom/ContainerNode.cpp
+++ third_party/WebKit/Source/core/dom/ContainerNode.cpp
@@ -620,15 +620,18 @@ void ContainerNode::parserRemoveChild(Node& oldChild)
     ASSERT(oldChild.parentNode() == this);
     ASSERT(!oldChild.isDocumentFragment());
 
-    Node* prev = oldChild.previousSibling();
-    Node* next = oldChild.nextSibling();
-
+    // This may cause arbitrary Javascript execution via onunload handlers.
     if (oldChild.connectedSubframeCount())
         ChildFrameDisconnector(oldChild).disconnect();
+
+    if (oldChild.parentNode() != this)
+        return;
 
     ChildListMutationScope(*this).willRemoveChild(oldChild);
     oldChild.notifyMutationObserversNodeWillDetach();
 
+    Node* prev = oldChild.previousSibling();
+    Node* next = oldChild.nextSibling();
     removeBetween(prev, next, oldChild);
 
     notifyNodeRemoved(oldChild);
