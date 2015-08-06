39334fcba0086ca230af7e794a3152153c4b29cd
estade <estade@chromium.org>
2015-08-06 12:55:29 -0700

    Add channel-specific product logo to chrome://help
    BUG=511000
    
    Review URL: https://codereview.chromium.org/1269383005
    
    Cr-Commit-Position: refs/heads/master@{#342183}
    
    Former-commit-id: 9836302ea4ffb887ce6ffa597c1ee5484d2f1091

--- chrome/browser/ui/webui/help/help_ui.cc
+++ chrome/browser/ui/webui/help/help_ui.cc
@@ -6,11 +6,14 @@
 
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/help/help_handler.h"
+#include "chrome/common/channel_info.h"
 #include "chrome/common/url_constants.h"
+#include "components/version_info/version_info.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "grit/browser_resources.h"
+#include "grit/theme_resources.h"
 
 namespace {
 
@@ -24,6 +27,35 @@ content::WebUIDataSource* CreateAboutPageHTMLSource() {
   source->AddResourcePath("channel_change_page.js", IDR_CHANNEL_CHANGE_PAGE_JS);
   source->SetDefaultResource(IDR_HELP_HTML);
   source->DisableDenyXFrameOptions();
+
+  int product_logo = 0;
+  switch (chrome::GetChannel()) {
+#if defined(GOOGLE_CHROME_BUILD)
+    case version_info::Channel::CANARY:
+      product_logo = IDR_PRODUCT_LOGO_32_CANARY;
+      break;
+    case version_info::Channel::DEV:
+      product_logo = IDR_PRODUCT_LOGO_32_DEV;
+      break;
+    case version_info::Channel::BETA:
+      product_logo = IDR_PRODUCT_LOGO_32_BETA;
+      break;
+    case version_info::Channel::STABLE:
+      product_logo = IDR_PRODUCT_LOGO_32;
+      break;
+#else
+    case version_info::Channel::CANARY:
+    case version_info::Channel::DEV:
+    case version_info::Channel::BETA:
+    case version_info::Channel::STABLE:
+      NOTREACHED();
+#endif
+    case version_info::Channel::UNKNOWN:
+      product_logo = IDR_PRODUCT_LOGO_32;
+      break;
+  }
+
+  source->AddResourcePath("current-channel-logo", product_logo);
   return source;
 }
 
