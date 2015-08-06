ef1198ab0fee07853df79f4f914b9a0161549be1
skirmani <skirmani@google.com>
2015-08-06 13:26:12 -0700

    Tools for being able to sign into fake OS accounts and fake app accounts when running tests. Real OS accounts are also supported, but take significantly longer because accounts must be authenticated. It is recommended to use fake OS accounts unless real ones are necessary.
    CQ-DEPEND=CL:1259253007
    
    Review URL: https://codereview.chromium.org/1258563005
    
    Cr-Commit-Position: refs/heads/master@{#342190}
    
    Former-commit-id: 6832765af577d32c774763e5abe8486aa8db7478

---
+++ chrome/android/javatests/src/org/chromium/chrome/test/util/ChromeSigninUtilsTest.java
@@ -0,0 +1,144 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.test.util;
+
+import android.test.InstrumentationTestCase;
+import android.test.suitebuilder.annotation.MediumTest;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.chromium.base.test.util.Restriction;
+import org.chromium.sync.signin.ChromeSigninController;
+
+/**
+ * Tests for {@link ChromeSigninUtils}.
+ */
+public class ChromeSigninUtilsTest extends InstrumentationTestCase {
+    private static final String FAKE_ACCOUNT_USERNAME = "test@google.com";
+    private static final String FAKE_ACCOUNT_PASSWORD = "$3cr3t";
+    private static final String GOOGLE_ACCOUNT_USERNAME = "chromiumforandroid01@gmail.com";
+    private static final String GOOGLE_ACCOUNT_PASSWORD = "chromeforandroid";
+    private static final String GOOGLE_ACCOUNT_TYPE = "mail";
+
+    private ChromeSigninUtils mSigninUtil;
+    private ChromeSigninController mSigninController;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mSigninUtil = new ChromeSigninUtils(this);
+        mSigninController = ChromeSigninController.get(getInstrumentation().getTargetContext());
+        mSigninController.clearSignedInUser();
+        mSigninUtil.removeAllFakeAccountsFromOs();
+        mSigninUtil.removeAllGoogleAccountsFromOs();
+    }
+
+    @SmallTest
+    public void testActivityIsNotSignedInOnAppOrFakeOSorGoogleOS() {
+        assertFalse("Should not be signed into app.",
+                mSigninController.isSignedIn());
+        assertFalse("Should not be signed into OS with fake account.",
+                mSigninUtil.isExistingFakeAccountOnOs(FAKE_ACCOUNT_USERNAME));
+        assertFalse("Should not be signed in on OS with Google account.",
+                mSigninUtil.isExistingGoogleAccountOnOs(GOOGLE_ACCOUNT_USERNAME));
+    }
+
+    @SmallTest
+    public void testIsSignedInOnApp() {
+        mSigninUtil.addAccountToApp(FAKE_ACCOUNT_USERNAME);
+        assertTrue("Should be signed on app.",
+                mSigninController.isSignedIn());
+        assertFalse("Should not be signed on OS with fake account.",
+                mSigninUtil.isExistingFakeAccountOnOs(FAKE_ACCOUNT_USERNAME));
+        assertFalse("Should not be signed in on OS with Google account.",
+                mSigninUtil.isExistingGoogleAccountOnOs(GOOGLE_ACCOUNT_USERNAME));
+    }
+
+    @SmallTest
+    public void testIsSignedInOnFakeOS() {
+        mSigninUtil.addFakeAccountToOs(FAKE_ACCOUNT_USERNAME, FAKE_ACCOUNT_PASSWORD);
+        assertFalse("Should not be signed in on app.",
+                mSigninController.isSignedIn());
+        assertTrue("Should be signed in on OS with fake account.",
+                mSigninUtil.isExistingFakeAccountOnOs(FAKE_ACCOUNT_USERNAME));
+        assertFalse("Should not be signed in on OS with Google account.",
+                mSigninUtil.isExistingGoogleAccountOnOs(GOOGLE_ACCOUNT_USERNAME));
+    }
+
+    @MediumTest
+    @Restriction(Restriction.RESTRICTION_TYPE_INTERNET)
+    public void testIsSignedInOnGoogleOS() {
+        mSigninUtil.addGoogleAccountToOs(GOOGLE_ACCOUNT_USERNAME, GOOGLE_ACCOUNT_PASSWORD,
+                GOOGLE_ACCOUNT_TYPE);
+        assertFalse("Should not be signed into app.",
+                mSigninController.isSignedIn());
+        assertFalse("Should not be signed into OS with fake account.",
+                mSigninUtil.isExistingFakeAccountOnOs(FAKE_ACCOUNT_USERNAME));
+        assertTrue("Should be signed in on OS with Google account.",
+                mSigninUtil.isExistingGoogleAccountOnOs(GOOGLE_ACCOUNT_USERNAME));
+    }
+
+    @SmallTest
+    public void testIsSignedInOnFakeOSandApp() {
+        mSigninUtil.addAccountToApp(FAKE_ACCOUNT_USERNAME);
+        mSigninUtil.addFakeAccountToOs(FAKE_ACCOUNT_USERNAME, FAKE_ACCOUNT_PASSWORD);
+        assertTrue("Should be signed in on app.",
+                mSigninController.isSignedIn());
+        assertTrue("Should be signed in on OS with fake account.",
+                mSigninUtil.isExistingFakeAccountOnOs(FAKE_ACCOUNT_USERNAME));
+        assertFalse("Should not be signed in on OS with Google account.",
+                mSigninUtil.isExistingGoogleAccountOnOs(GOOGLE_ACCOUNT_USERNAME));
+    }
+
+    @MediumTest
+    @Restriction(Restriction.RESTRICTION_TYPE_INTERNET)
+    public void testIsSignedInOnAppAndGoogleOS() {
+        mSigninUtil.addAccountToApp(FAKE_ACCOUNT_USERNAME);
+        mSigninUtil.addGoogleAccountToOs(GOOGLE_ACCOUNT_USERNAME, GOOGLE_ACCOUNT_PASSWORD,
+                GOOGLE_ACCOUNT_TYPE);
+        assertTrue("Should be signed into app.",
+                mSigninController.isSignedIn());
+        assertFalse("Should not be signed into OS with fake account.",
+                mSigninUtil.isExistingFakeAccountOnOs(FAKE_ACCOUNT_USERNAME));
+        assertTrue("Should be signed in on OS with Google account.",
+                mSigninUtil.isExistingGoogleAccountOnOs(GOOGLE_ACCOUNT_USERNAME));
+    }
+
+    @MediumTest
+    @Restriction(Restriction.RESTRICTION_TYPE_INTERNET)
+    public void testIsSignedInOnFakeOSandGoogleOS() {
+        mSigninUtil.addFakeAccountToOs(FAKE_ACCOUNT_USERNAME, FAKE_ACCOUNT_PASSWORD);
+        mSigninUtil.addGoogleAccountToOs(GOOGLE_ACCOUNT_USERNAME, GOOGLE_ACCOUNT_PASSWORD,
+                GOOGLE_ACCOUNT_TYPE);
+        assertFalse("Should not be signed into app.",
+                mSigninController.isSignedIn());
+        assertTrue("Should be signed into OS with fake account.",
+                mSigninUtil.isExistingFakeAccountOnOs(FAKE_ACCOUNT_USERNAME));
+        assertTrue("Should be signed in on OS with Google account.",
+                mSigninUtil.isExistingGoogleAccountOnOs(GOOGLE_ACCOUNT_USERNAME));
+    }
+
+    @MediumTest
+    @Restriction(Restriction.RESTRICTION_TYPE_INTERNET)
+    public void testIsSignedInOnAppAndFakeOSandGoogleOS() {
+        mSigninUtil.addAccountToApp(FAKE_ACCOUNT_USERNAME);
+        mSigninUtil.addFakeAccountToOs(FAKE_ACCOUNT_USERNAME, FAKE_ACCOUNT_PASSWORD);
+        mSigninUtil.addGoogleAccountToOs(GOOGLE_ACCOUNT_USERNAME, GOOGLE_ACCOUNT_PASSWORD,
+                GOOGLE_ACCOUNT_TYPE);
+        assertTrue("Should be signed into app.",
+                mSigninController.isSignedIn());
+        assertTrue("Should be signed into OS with fake account.",
+                mSigninUtil.isExistingFakeAccountOnOs(FAKE_ACCOUNT_USERNAME));
+        assertTrue("Should be signed in on OS with Google account.",
+                mSigninUtil.isExistingGoogleAccountOnOs(GOOGLE_ACCOUNT_USERNAME));
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        mSigninController.clearSignedInUser();
+        mSigninUtil.removeAllFakeAccountsFromOs();
+        mSigninUtil.removeAllGoogleAccountsFromOs();
+        super.tearDown();
+    }
+}
