a8c999d4fb88ffba7d890b6eae58d8162e102350
davidben <davidben@chromium.org>
2015-08-06 16:09:49 -0700

    Show the Windows certificate viewer on the BaseShellDialogImpl thread.
    This is a speculative fix to https://crbug.com/513852, but also a
    worthwhile thing to do in general, as nested event loops do strange
    things (see TEST line below).
    
    BUG=513852
    TEST=Open two browser windows on Windows and load an HTTPS URL in each.
         Click the lock icon in each, then click Certificate information in each.
         Two dialogs should appear.
         It should be possible to dismiss the first certificate dialog to by
         pressing OK before dismissing the second.
    
    Review URL: https://codereview.chromium.org/1270753002
    
    Cr-Commit-Position: refs/heads/master@{#342225}
    
    Former-commit-id: 8b4c03d2062e8584b6de62001cea543c5a5efa68

--- chrome/browser/ui/views/certificate_viewer_win.cc
+++ chrome/browser/ui/views/certificate_viewer_win.cc
@@ -8,53 +8,79 @@
 #include <cryptuiapi.h>
 #pragma comment(lib, "cryptui.lib")
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
+#include "base/location.h"
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
+#include "base/task_runner.h"
+#include "base/threading/thread.h"
 #include "chrome/browser/ui/host_desktop.h"
 #include "net/cert/x509_certificate.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_tree_host.h"
+#include "ui/shell_dialogs/base_shell_dialog_win.h"
 
 namespace {
 
-void ShowCertificateViewerImpl(content::WebContents* web_contents,
-                               HWND parent,
-                               net::X509Certificate* cert) {
-  // Create a new cert context and store containing just the certificate
-  // and its intermediate certificates.
-  PCCERT_CONTEXT cert_list = cert->CreateOSCertChainForCert();
-  CHECK(cert_list);
+// Shows a Windows certificate viewer dialog on a background thread to avoid
+// nested message loops.
+class CertificateViewerDialog : public ui::BaseShellDialogImpl {
+ public:
+  CertificateViewerDialog() {}
 
-  CRYPTUI_VIEWCERTIFICATE_STRUCT view_info = { 0 };
-  view_info.dwSize = sizeof(view_info);
-  // We set our parent to the tab window. This makes the cert dialog created
-  // in CryptUIDlgViewCertificate modal to the browser.
-  view_info.hwndParent = parent;
-  view_info.dwFlags = CRYPTUI_DISABLE_EDITPROPERTIES |
-                      CRYPTUI_DISABLE_ADDTOSTORE;
-  view_info.pCertContext = cert_list;
-  HCERTSTORE cert_store = cert_list->hCertStore;
-  view_info.cStores = 1;
-  view_info.rghStores = &cert_store;
-  BOOL properties_changed;
+  // Shows the dialog and calls |callback| when the dialog closes. The caller
+  // must ensure the CertificateViewerDialog remains valid until then.
+  void Show(HWND parent,
+            net::X509Certificate* cert,
+            const base::Closure& callback) {
+    if (IsRunningDialogForOwner(parent)) {
+      base::MessageLoop::current()->PostTask(FROM_HERE, callback);
+      return;
+    }
 
-  // We must allow nested tasks to dispatch so that, e.g. gpu tasks are
-  // processed for painting. This allows a second window to continue painting
-  // while the the certificate dialog is open.
-  base::MessageLoop::ScopedNestableTaskAllower allow(
-      base::MessageLoop::current());
+    RunState run_state = BeginRun(parent);
+    run_state.dialog_thread->task_runner()->PostTaskAndReply(
+        FROM_HERE,
+        base::Bind(&CertificateViewerDialog::ShowOnDialogThread,
+                   base::Unretained(this), run_state, make_scoped_refptr(cert)),
+        base::Bind(&CertificateViewerDialog::OnDialogClosed,
+                   base::Unretained(this), run_state, callback));
+  }
 
-  // Tell the message loop to only handle certain types of messages while the
-  // dialog is open to avoid bad things happening. See https://crbug.com/344012
-  // for details.
-  base::MessageLoop::current()->set_os_modal_loop(true);
-  // This next call blocks but keeps processing windows messages, making it
-  // modal to the browser window.
-  ::CryptUIDlgViewCertificate(&view_info, &properties_changed);
+ private:
+  void ShowOnDialogThread(const RunState& run_state,
+                          const scoped_refptr<net::X509Certificate>& cert) {
+    // Create a new cert context and store containing just the certificate
+    // and its intermediate certificates.
+    PCCERT_CONTEXT cert_list = cert->CreateOSCertChainForCert();
+    CHECK(cert_list);
 
-  base::MessageLoop::current()->set_os_modal_loop(false);
-  CertFreeCertificateContext(cert_list);
-}
+    CRYPTUI_VIEWCERTIFICATE_STRUCT view_info = {0};
+    view_info.dwSize = sizeof(view_info);
+    view_info.hwndParent = run_state.owner;
+    view_info.dwFlags =
+        CRYPTUI_DISABLE_EDITPROPERTIES | CRYPTUI_DISABLE_ADDTOSTORE;
+    view_info.pCertContext = cert_list;
+    HCERTSTORE cert_store = cert_list->hCertStore;
+    view_info.cStores = 1;
+    view_info.rghStores = &cert_store;
+
+    BOOL properties_changed;
+    ::CryptUIDlgViewCertificate(&view_info, &properties_changed);
+
+    CertFreeCertificateContext(cert_list);
+  }
+
+  void OnDialogClosed(const RunState& run_state,
+                      const base::Closure& callback) {
+    EndRun(run_state);
+    // May delete |this|.
+    callback.Run();
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(CertificateViewerDialog);
+};
 
 }  // namespace
 
@@ -63,9 +89,10 @@ void ShowCertificateViewer(content::WebContents* web_contents,
                            net::X509Certificate* cert) {
   if (chrome::GetHostDesktopTypeForNativeWindow(parent) !=
       chrome::HOST_DESKTOP_TYPE_ASH) {
-    ShowCertificateViewerImpl(
-        web_contents,
-        parent->GetHost()->GetAcceleratedWidget(), cert);
+    CertificateViewerDialog* dialog = new CertificateViewerDialog;
+    dialog->Show(
+        parent->GetHost()->GetAcceleratedWidget(), cert,
+        base::Bind(&base::DeletePointer<CertificateViewerDialog>, dialog));
   } else {
     NOTIMPLEMENTED();
   }
