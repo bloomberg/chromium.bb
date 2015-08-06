a2e358ede4cd8d426a1ca38a498f6e39389242b5
mfomitchev <mfomitchev@chromium.org>
2015-08-06 11:20:03 -0700

    Fixing a rename typo.
    Test was renamed by accident in crrev.com/1277633003
    
    TBR=asargent@chromium.org
    BUG=517238
    
    Review URL: https://codereview.chromium.org/1275943003
    
    Cr-Commit-Position: refs/heads/master@{#342147}
    
    Former-commit-id: 0dba113abffbdaecdf49e714a8f651eb9a65d0b4

--- chrome/browser/extensions/active_script_controller_browsertest.cc
+++ chrome/browser/extensions/active_script_controller_browsertest.cc
@@ -469,14 +469,14 @@ class FlagOffActiveScriptControllerBrowserTest
 
 // Times out frequently on CrOS: http://crbug.com/517238
 #if defined(OS_CHROMEOS)
-#define MAYBE_FlagOffActiveScriptControllerBrowserTest \
-        DISABLED_FlagOffActiveScriptControllerBrowserTest
+#define MAYBE_ScriptsExecuteWhenFlagAbsent \
+        DISABLED_ScriptsExecuteWhenFlagAbsent
 #else
-#define MAYBE_FlagOffActiveScriptControllerBrowserTest \
-        FlagOffActiveScriptControllerBrowserTest
+#define MAYBE_ScriptsExecuteWhenFlagAbsent \
+        ScriptsExecuteWhenFlagAbsent
 #endif
 IN_PROC_BROWSER_TEST_F(FlagOffActiveScriptControllerBrowserTest,
-                       MAYBE_FlagOffActiveScriptControllerBrowserTest) {
+                       MAYBE_ScriptsExecuteWhenFlagAbsent) {
   const char* const kExtensionNames[] = {
     "content_scripts_all_hosts",
     "inject_scripts_all_hosts",
