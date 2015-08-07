f49bc6ccf92f21c546883d79e63d0527cd39f886
sigbjornf@opera.com <sigbjornf@opera.com>
2015-08-07 15:31:30 +0000

    Oilpan: catch some self-referential leaks (main thread.)
    Track the number of SelfKeepAlive<>s that are currently active on
    the main thread, so as to be able to detect if any remains on
    shutdown. A clear indication of a leak, if so.
    
    Also add some debugging support for detecting thread shutdown
    leaks due to Persistent<>s not being released; see
    PersistentRegion::dumpLivePersistents() and ThreadState::cleanup().
    
    R=haraken
    BUG=420515
    
    Review URL: https://codereview.chromium.org/1275863002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200174 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: a7a612a8152441f669010fc043e1c9cc10daa22b

--- third_party/WebKit/Source/platform/heap/PersistentNode.cpp
+++ third_party/WebKit/Source/platform/heap/PersistentNode.cpp
@@ -7,6 +7,10 @@
 
 #include "platform/heap/Handle.h"
 
+#ifndef NDEBUG
+#include <stdio.h>
+#endif
+
 namespace blink {
 
 PersistentRegion::~PersistentRegion()
@@ -31,6 +35,25 @@ int PersistentRegion::numberOfPersistents()
     ASSERT(persistentCount == m_persistentCount);
     return persistentCount;
 }
+
+#ifndef NDEBUG
+void PersistentRegion::dumpLivePersistents()
+{
+    class Object;
+    using GCObject = GarbageCollected<Object>;
+
+    fprintf(stderr, "Live Persistent(heap object, trace method):\n");
+    for (PersistentNodeSlots* slots = m_slots; slots; slots = slots->m_next) {
+        for (int i = 0; i < PersistentNodeSlots::slotCount; ++i) {
+            if (!slots->m_slot[i].isUnused()) {
+                Persistent<GCObject>* persistent = reinterpret_cast<Persistent<GCObject>*>(slots->m_slot[i].self());
+                ASSERT(persistent);
+                fprintf(stderr, "%p => (%p, %p)\n", persistent, persistent->get(), slots->m_slot[i].traceCallback());
+            }
+        }
+    }
+}
+#endif
 
 void PersistentRegion::ensurePersistentNodeSlots(void* self, TraceCallback trace)
 {
