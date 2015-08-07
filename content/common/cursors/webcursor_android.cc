1b310997dd9c05b90679ba9707274c9667364334
mfomitchev <mfomitchev@chromium.org>
2015-08-06 17:12:29 -0700

    Aura on Android: Expanding the empty webcursor implementation.
    Expanding the empty webcursor implementation so that we can use it with RenderWidgetHostViewAura.
    Currently mouse cursors on Android are always rendered as a small circle. Until the time we invest into better cursor support on Android platform, this stubbed implementation should be fine - at least this will compile.
    
    BUG=507792
    
    Review URL: https://codereview.chromium.org/1255113006
    
    Cr-Commit-Position: refs/heads/master@{#342239}
    
    Former-commit-id: 2977ebf3b41b57398762743080108623bf15c8d1

--- content/common/cursors/webcursor_android.cc
+++ content/common/cursors/webcursor_android.cc
@@ -9,6 +9,18 @@
 
 namespace content {
 
+gfx::NativeCursor WebCursor::GetNativeCursor() {
+  return gfx::kNullCursor;
+}
+
+#if defined(USE_AURA)
+// In the future when we want to support cursors of various kinds in Aura on
+// Android, we should switch to using webcursor_aura rather than add an
+// implementation here.
+void WebCursor::SetDisplayInfo(const gfx::Display& display) {
+}
+#endif
+
 void WebCursor::InitPlatformData() {
 }
 
