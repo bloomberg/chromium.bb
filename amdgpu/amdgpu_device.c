a30da8e9b3bb74d62b641a9fa4c0100ad47f2bc4
Emil Velikov <emil.l.velikov@gmail.com>
2015-08-07 17:20:51 +0100

    amdgpu: cosmetic chances in license boilerplate
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Jammy Zhou <Jammy.Zhou@amd.com>

--- amdgpu/amdgpu_device.c
+++ amdgpu/amdgpu_device.c
@@ -19,13 +19,12 @@
  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  * OTHER DEALINGS IN THE SOFTWARE.
  *
-*/
+ */
 
 /**
  * \file amdgpu_device.c
  *
  *  Implementation of functions for AMD GPU device
- *
  *
  */
 
