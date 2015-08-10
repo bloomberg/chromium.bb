7007bc6116d0dc343e749a974c355ea073aff45a
amistry <amistry@chromium.org>
2015-08-10 16:50:44 -0700

    Prevent a thread startup race in ScheduleWorkTest which causes a nullptr dereference of a message pump.
    Review URL: https://codereview.chromium.org/1283543007
    
    Cr-Commit-Position: refs/heads/master@{#342741}
    
    Former-commit-id: 7fc519fa5164f2eb7c718de2d0ed7500e11fc690

--- base/message_loop/message_pump_perftest.cc
+++ base/message_loop/message_pump_perftest.cc
@@ -70,6 +70,13 @@ class ScheduleWorkTest : public testing::Test {
     {
       target_.reset(new Thread("target"));
       target_->StartWithOptions(Thread::Options(target_type, 0u));
+
+      // Without this, it's possible for the scheduling threads to start and run
+      // before the target thread. In this case, the scheduling threads will
+      // call target_message_loop()->ScheduleWork(), which dereferences the
+      // loop's message pump, which is only created after the target thread has
+      // finished starting.
+      target_->WaitUntilThreadStarted();
     }
 
     ScopedVector<Thread> scheduling_threads;
