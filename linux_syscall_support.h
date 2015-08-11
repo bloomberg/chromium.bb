4fc942258fe5509549333b9487ec018e3c8c5b10
mseaborn@chromium.org <mseaborn@chromium.org>
2015-08-11 21:15:24 +0000

    MIPS Android: use asm/sgidefs.h instead of sgidefs.h
    Including LSS from Chromium's base_unittests breaks compilation on MIPS
    Android. Breakpad works around this by suppling an sgidefs.h that just
    includes asm/sgidefs.h. This CL makes LSS include asm/sgidefs.h
    directly when building for MIPS Android, preventing other projects for
    having to apply the same workaround.
    
    BUG=512384
    
    Patch contributed by Ian Cullinan (cullinan@amazon.com).
    
    R=mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/1248033002 .
    
    git-svn-id: http://linux-syscall-support.googlecode.com/svn/trunk/lss@36 829466d3-f3f5-3ae4-62ad-de35cf9bba21

--- linux_syscall_support.h
+++ linux_syscall_support.h
@@ -118,7 +118,14 @@ extern "C" {
 
 #ifdef __mips__
 /* Include definitions of the ABI currently in use.                          */
+#ifdef __ANDROID__
+/* Android doesn't have sgidefs.h, but does have asm/sgidefs.h,
+ * which has the definitions we need.
+ */
+#include <asm/sgidefs.h>
+#else
 #include <sgidefs.h>
+#endif
 #endif
 #endif
 
