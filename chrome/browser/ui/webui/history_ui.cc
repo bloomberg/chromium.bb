f836bf2ebea53b3d3dc840fbd15f2061613c2dc4
akuegel <akuegel@chromium.org>
2015-08-13 05:48:25 -0700

    Remove unused IDS_HISTORY_(UN)LOCK_BUTTON strings.
    This was missed in https://chromiumcodereview.appspot.com/15415006
    
    BUG=241383
    
    Review URL: https://codereview.chromium.org/1293483002
    
    Cr-Commit-Position: refs/heads/master@{#343184}
    
    Former-commit-id: 70be04b5394b77368b63850a8e7164c2eec5cf3b

--- chrome/browser/ui/webui/history_ui.cc
+++ chrome/browser/ui/webui/history_ui.cc
@@ -183,10 +183,8 @@ content::WebUIDataSource* CreateHistoryUIHTMLSource(Profile* profile) {
   source->AddLocalizedString("inContentPack", IDS_HISTORY_IN_CONTENT_PACK);
   source->AddLocalizedString("allowItems", IDS_HISTORY_FILTER_ALLOW_ITEMS);
   source->AddLocalizedString("blockItems", IDS_HISTORY_FILTER_BLOCK_ITEMS);
-  source->AddLocalizedString("lockButton", IDS_HISTORY_LOCK_BUTTON);
   source->AddLocalizedString("blockedVisitText",
                              IDS_HISTORY_BLOCKED_VISIT_TEXT);
-  source->AddLocalizedString("unlockButton", IDS_HISTORY_UNLOCK_BUTTON);
   source->AddLocalizedString("hasSyncedResults",
                              IDS_HISTORY_HAS_SYNCED_RESULTS);
   source->AddLocalizedString("noSyncedResults", IDS_HISTORY_NO_SYNCED_RESULTS);
