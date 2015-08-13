7d62d2181b4ad5dbcaf27e37755be8abba834efd
penghuang <penghuang@chromium.org>
2015-08-13 07:50:00 -0700

    Mandoline: move TextInputType converter to blink_text_input_type_converters.*
    BUG=498624
    
    Review URL: https://codereview.chromium.org/1288133002
    
    Cr-Commit-Position: refs/heads/master@{#343192}
    
    Former-commit-id: 1ee669e11d465a1706028cb8502c6915c323851f

---
+++ components/html_viewer/blink_text_input_type_converters.cc
@@ -0,0 +1,54 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/html_viewer/blink_text_input_type_converters.h"
+
+#include "base/basictypes.h"
+#include "base/macros.h"
+
+namespace mojo {
+
+#define TEXT_INPUT_TYPE_ASSERT(NAME, Name) \
+  COMPILE_ASSERT(static_cast<int32>(TEXT_INPUT_TYPE_##NAME) == \
+                 static_cast<int32>(blink::WebTextInputType##Name), \
+                 text_input_type_should_match)
+TEXT_INPUT_TYPE_ASSERT(NONE, None);
+TEXT_INPUT_TYPE_ASSERT(TEXT, Text);
+TEXT_INPUT_TYPE_ASSERT(PASSWORD, Password);
+TEXT_INPUT_TYPE_ASSERT(SEARCH, Search);
+TEXT_INPUT_TYPE_ASSERT(EMAIL, Email);
+TEXT_INPUT_TYPE_ASSERT(NUMBER, Number);
+TEXT_INPUT_TYPE_ASSERT(TELEPHONE, Telephone);
+TEXT_INPUT_TYPE_ASSERT(URL, URL);
+TEXT_INPUT_TYPE_ASSERT(DATE, Date);
+TEXT_INPUT_TYPE_ASSERT(DATE_TIME, DateTime);
+TEXT_INPUT_TYPE_ASSERT(DATE_TIME_LOCAL, DateTimeLocal);
+TEXT_INPUT_TYPE_ASSERT(MONTH, Month);
+TEXT_INPUT_TYPE_ASSERT(TIME, Time);
+TEXT_INPUT_TYPE_ASSERT(WEEK, Week);
+TEXT_INPUT_TYPE_ASSERT(TEXT_AREA, TextArea);
+
+#define TEXT_INPUT_FLAG_ASSERT(NAME, Name) \
+  COMPILE_ASSERT(static_cast<int32>(TEXT_INPUT_FLAG_##NAME) == \
+                 static_cast<int32>(blink::WebTextInputFlag##Name), \
+                 text_input_flag_should_match)
+TEXT_INPUT_FLAG_ASSERT(NONE, None);
+TEXT_INPUT_FLAG_ASSERT(AUTO_COMPLETE_ON, AutocompleteOn);
+TEXT_INPUT_FLAG_ASSERT(AUTO_COMPLETE_OFF, AutocompleteOff);
+TEXT_INPUT_FLAG_ASSERT(AUTO_CORRECT_ON, AutocorrectOn);
+TEXT_INPUT_FLAG_ASSERT(AUTO_CORRECT_OFF, AutocorrectOff);
+TEXT_INPUT_FLAG_ASSERT(SPELL_CHECK_ON, SpellcheckOn);
+TEXT_INPUT_FLAG_ASSERT(SPELL_CHECK_OFF, SpellcheckOff);
+TEXT_INPUT_FLAG_ASSERT(AUTO_CAPITALIZE_NONE, AutocapitalizeNone);
+TEXT_INPUT_FLAG_ASSERT(AUTO_CAPITALIZE_CHARACTERS, AutocapitalizeCharacters);
+TEXT_INPUT_FLAG_ASSERT(AUTO_CAPITALIZE_WORDS, AutocapitalizeWords);
+TEXT_INPUT_FLAG_ASSERT(AUTO_CAPITALIZE_SENTENCES, AutocapitalizeSentences);
+
+TextInputType
+TypeConverter<TextInputType, blink::WebTextInputType>::Convert(
+    const blink::WebTextInputType& input) {
+  return static_cast<TextInputType>(input);
+}
+
+}  // namespace mojo
--- components/html_viewer/html_frame.cc
+++ components/html_viewer/html_frame.cc
@@ -15,6 +15,7 @@
 #include "components/html_viewer/ax_provider_impl.h"
 #include "components/html_viewer/blink_basic_type_converters.h"
 #include "components/html_viewer/blink_input_events_type_converters.h"
+#include "components/html_viewer/blink_text_input_type_converters.h"
 #include "components/html_viewer/blink_url_request_type_converters.h"
 #include "components/html_viewer/geolocation_client_impl.h"
 #include "components/html_viewer/global_state.h"
@@ -71,53 +72,6 @@ using mojo::ServiceProviderPtr;
 using mojo::URLResponsePtr;
 using mojo::View;
 using mojo::WeakBindToRequest;
-
-namespace mojo {
-
-#define TEXT_INPUT_TYPE_ASSERT(NAME, Name) \
-  COMPILE_ASSERT(static_cast<int32>(TEXT_INPUT_TYPE_##NAME) == \
-                 static_cast<int32>(blink::WebTextInputType##Name), \
-                 text_input_type_should_match)
-TEXT_INPUT_TYPE_ASSERT(NONE, None);
-TEXT_INPUT_TYPE_ASSERT(TEXT, Text);
-TEXT_INPUT_TYPE_ASSERT(PASSWORD, Password);
-TEXT_INPUT_TYPE_ASSERT(SEARCH, Search);
-TEXT_INPUT_TYPE_ASSERT(EMAIL, Email);
-TEXT_INPUT_TYPE_ASSERT(NUMBER, Number);
-TEXT_INPUT_TYPE_ASSERT(TELEPHONE, Telephone);
-TEXT_INPUT_TYPE_ASSERT(URL, URL);
-TEXT_INPUT_TYPE_ASSERT(DATE, Date);
-TEXT_INPUT_TYPE_ASSERT(DATE_TIME, DateTime);
-TEXT_INPUT_TYPE_ASSERT(DATE_TIME_LOCAL, DateTimeLocal);
-TEXT_INPUT_TYPE_ASSERT(MONTH, Month);
-TEXT_INPUT_TYPE_ASSERT(TIME, Time);
-TEXT_INPUT_TYPE_ASSERT(WEEK, Week);
-TEXT_INPUT_TYPE_ASSERT(TEXT_AREA, TextArea);
-
-#define TEXT_INPUT_FLAG_ASSERT(NAME, Name) \
-  COMPILE_ASSERT(static_cast<int32>(TEXT_INPUT_FLAG_##NAME) == \
-                 static_cast<int32>(blink::WebTextInputFlag##Name), \
-                 text_input_flag_should_match)
-TEXT_INPUT_FLAG_ASSERT(NONE, None);
-TEXT_INPUT_FLAG_ASSERT(AUTO_COMPLETE_ON, AutocompleteOn);
-TEXT_INPUT_FLAG_ASSERT(AUTO_COMPLETE_OFF, AutocompleteOff);
-TEXT_INPUT_FLAG_ASSERT(AUTO_CORRECT_ON, AutocorrectOn);
-TEXT_INPUT_FLAG_ASSERT(AUTO_CORRECT_OFF, AutocorrectOff);
-TEXT_INPUT_FLAG_ASSERT(SPELL_CHECK_ON, SpellcheckOn);
-TEXT_INPUT_FLAG_ASSERT(SPELL_CHECK_OFF, SpellcheckOff);
-TEXT_INPUT_FLAG_ASSERT(AUTO_CAPITALIZE_NONE, AutocapitalizeNone);
-TEXT_INPUT_FLAG_ASSERT(AUTO_CAPITALIZE_CHARACTERS, AutocapitalizeCharacters);
-TEXT_INPUT_FLAG_ASSERT(AUTO_CAPITALIZE_WORDS, AutocapitalizeWords);
-TEXT_INPUT_FLAG_ASSERT(AUTO_CAPITALIZE_SENTENCES, AutocapitalizeSentences);
-
-template <>
-struct TypeConverter<TextInputType, blink::WebTextInputType> {
-  static TextInputType Convert(const blink::WebTextInputType& input) {
-    return static_cast<TextInputType>(input);
-  }
-};
-
-}  // namespace mojo
 
 namespace html_viewer {
 namespace {
