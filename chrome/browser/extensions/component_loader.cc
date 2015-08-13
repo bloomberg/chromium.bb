b0c6062f2df64bf1bcc9c11cef4685ea349ae1d0
pneubeck <pneubeck@chromium.org>
2015-08-13 01:00:51 -0700

    Remove the experimental network configuration extension.
    This extension was used during the initial implementation of
    the networkingPrivate API but development of it wasn't continued.
    Time to remove it.
    
    BUG=520392
    
    Review URL: https://codereview.chromium.org/1282253002
    
    Cr-Commit-Position: refs/heads/master@{#343166}
    
    Former-commit-id: f811f7630ed00d55799675507f6894c44f610698

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -665,9 +665,6 @@ void ComponentLoader::AddDefaultComponentExtensionsWithBackgroundPages(
     Add(IDR_FIRST_RUN_DIALOG_MANIFEST,
         base::FilePath(FILE_PATH_LITERAL("chromeos/first_run/app")));
 
-    Add(IDR_NETWORK_CONFIGURATION_MANIFEST,
-        base::FilePath(FILE_PATH_LITERAL("chromeos/network_configuration")));
-
     Add(IDR_CONNECTIVITY_DIAGNOSTICS_MANIFEST,
         base::FilePath(extension_misc::kConnectivityDiagnosticsPath));
     Add(IDR_CONNECTIVITY_DIAGNOSTICS_LAUNCHER_MANIFEST,
