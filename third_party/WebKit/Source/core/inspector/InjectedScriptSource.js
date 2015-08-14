684326ebc0a4b6e1ef8693acb79bcc429be1017d
pfeldman@chromium.org <pfeldman@chromium.org>
2015-08-14 16:55:27 +0000

    Revert of DevTools: roll closure compiler to v20150609 (patchset #2 id:20001 of https://codereview.chromium.org/1286353002/ )
    Reason for revert:
    Exception in thread "main" java.lang.NoClassDefFoundError: org/kohsuke/args4j/CmdLineException
    	at java.lang.Class.getDeclaredMethods0(Native Method)
    	at java.lang.Class.privateGetDeclaredMethods(Class.java:2693)
    	at java.lang.Class.privateGetMethodRecursive(Class.java:3040)
    	at java.lang.Class.getMethod0(Class.java:3010)
    	at java.lang.Class.getMethod(Class.java:1776)
    	at sun.launcher.LauncherHelper.validateMainClass(LauncherHelper.java:544)
    	at sun.launcher.LauncherHelper.checkAndLoadMain(LauncherHelper.java:526)
    Caused by: java.lang.ClassNotFoundException: org.kohsuke.args4j.CmdLineException
    	at java.net.URLClassLoader$1.run(URLClassLoader.java:372)
    	at java.net.URLClassLoader$1.run(URLClassLoader.java:361)
    	at java.security.AccessController.doPrivileged(Native Method)
    	at java.net.URLClassLoader.findClass(URLClassLoader.java:360)
    	at java.lang.ClassLoader.loadClass(ClassLoader.java:424)
    	at sun.misc.Launcher$AppClassLoader.loadClass(Launcher.java:309)
    	at java.lang.ClassLoader.loadClass(ClassLoader.java:357)
    	... 7 more
    
    
    Original issue's description:
    > DevTools: roll closure compiler to v20150609
    > 
    > The new version is capable of handling promise types correctly, whenever
    > a Promise.then'ed function returns another promise.
    > 
    > BUG=508960
    > R=sergeyv, pfeldman
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=200520
    
    TBR=sergeyv@chromium.org,lushnikov@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=508960
    
    Review URL: https://codereview.chromium.org/1295813004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200548 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 8a8689eee3f47e465403eb36960f5db163d5eca2

--- third_party/WebKit/Source/core/inspector/InjectedScriptSource.js
+++ third_party/WebKit/Source/core/inspector/InjectedScriptSource.js
@@ -418,7 +418,7 @@ InjectedScript.prototype = {
             return false;
         object = /** @type {!Object} */ (object);
         var descriptors = [];
-        var iter = this._propertyDescriptors(object, ownProperties, accessorPropertiesOnly, undefined);
+        var iter = this._propertyDescriptors(object, ownProperties, accessorPropertiesOnly);
         // Go over properties, wrap object values.
         for (var descriptor of iter) {
             if ("get" in descriptor)
