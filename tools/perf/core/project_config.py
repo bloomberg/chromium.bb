7119d947e3d164f7ab71f4e65a2b8f6e07ed7a56
aiolos <aiolos@chromium.org>
2015-08-14 19:54:25 -0700

    Add dependency_manager initialization to binary_manager
    BUG=510231
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Committed: https://crrev.com/152053791eb26551fbd209f7343fa64612778f8f [formerly f592ba18312f43fe4186cc526db00960a1db0b48]
    Cr-Commit-Position: refs/heads/master@{#343320}
    
    Review URL: https://codereview.chromium.org/1280903003
    
    Cr-Commit-Position: refs/heads/master@{#343547}
    
    Former-commit-id: 780b193b7e92cef04c6c1063068e6f5edbf8e3f2

---
+++ tools/perf/core/project_config.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import os
+import sys
+
+sys.path.append(os.path.join(
+    os.path.dirname(__file__), os.pardir, os.pardir, 'telemetry'))
+from telemetry import benchmark_runner
+
+top_level_dir = os.path.dirname(os.path.realpath(
+    os.path.join(__file__, os.pardir)))
+
+config = benchmark_runner.ProjectConfig(
+    top_level_dir=top_level_dir,
+    benchmark_dirs=[os.path.join(top_level_dir, 'benchmarks')])
+
+config.telemetry_dir = os.path.realpath(os.path.join(
+    top_level_dir, os.pardir, 'telemetry'))
