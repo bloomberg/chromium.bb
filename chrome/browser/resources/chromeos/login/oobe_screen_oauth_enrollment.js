cb5389bfe227ce125e2c57692129d0f15c6b1cc5
glevin <glevin@chromium.org>
2015-08-16 11:52:51 -0700

    Focus DONE button on last Enterprise Enrollment card
    BUG=514207
    TEST=Go through new Enterprise Enrollment flow.  Upon successful signin
    ("...successfully enrolled..."), the DONE button should start with
    focus, and respond to the ENTER key.  Also, upon failure (e.g. trying
    EE with a non-enterprise account), the TRY AGAIN button should also
    start with focus.
    
    Review URL: https://codereview.chromium.org/1289983005
    
    Cr-Commit-Position: refs/heads/master@{#343607}
    
    Former-commit-id: 402e34aa2b8adbdb608cb4936bbadfb3b2c1ec74

--- chrome/browser/resources/chromeos/login/oobe_screen_oauth_enrollment.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_oauth_enrollment.js
@@ -49,7 +49,6 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
     decorate: function() {
       var webview = document.createElement('webview');
       webview.id = webview.name = 'oauth-enroll-auth-view';
-      webview.classList.toggle('oauth-enroll-focus-on-signin', true);
       $('oauth-enroll-auth-view-container').appendChild(webview);
       this.authenticator_ = new cr.login.Authenticator(webview);
 
@@ -238,13 +237,16 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
       this.classList.toggle('oauth-enroll-state-' + this.currentStep_, false);
       this.classList.toggle('oauth-enroll-state-' + step, true);
 
-      var focusElements =
-          this.querySelectorAll('.oauth-enroll-focus-on-' + step);
-      for (var i = 0; i < focusElements.length; ++i) {
-        if (getComputedStyle(focusElements[i])['display'] != 'none') {
-          focusElements[i].focus();
-          break;
-        }
+      if (step == STEP_SIGNIN) {
+        $('oauth-enroll-auth-view').focus();
+      } else if (step == STEP_ERROR) {
+        $('oauth-enroll-error-card').submitButton.focus();
+      } else if (step == STEP_SUCCESS) {
+        $('oauth-enroll-success-card').submitButton.focus();
+      } else if (step == STEP_ATTRIBUTE_PROMPT) {
+        $('oauth-enroll-asset-id').focus();
+      } else if (step == STEP_ATTRIBUTE_PROMPT_ERROR) {
+        $('oauth-enroll-attribute-prompt-error-card').submitButton.focus();
       }
       this.currentStep_ = step;
     },
