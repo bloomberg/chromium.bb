045ef2f38dbeb19cb0862e0d0a23d2cce9c6e1bf
tkent@chromium.org <tkent@chromium.org>
2015-08-17 02:22:09 +0000

    Revert of Fix flaky crash in WebPagePopupImpl::closePopup. (patchset #1 id:1 of https://codereview.chromium.org/1291303002/ )
    Reason for revert:
    Caused another issue.
    
    
    Original issue's description:
    > Fix flaky crash in WebPagePopupImpl::closePopup.
    > 
    > Reentrance to WebViewImpl::closePagePopup is possible.  closePopup crashed in
    > that case because m_page was valid and m_page->mainFrame() was nullptr.
    > 
    > The details:
    > If WebViewImpl::closePagePopup is called, and the LocalFrame in the page popup
    > has the last references to the page popup owner Element,
    >  1. WebPagePopupImpl::closePopup() calls destroyPage().
    >  2. destroyPage() calls Page::willBeDestroyed().
    >  3. willBeDestroyed() destructs the LocalFrame.
    >  4. The LocalFrame destructor destructs the owner Element.
    >  5. The owner Element destructor destructs PickerIndicatorElement.
    >  6. PickerIndicatorElement destructor calls WebViewImpl::closePagePopup().
    > 
    > This CL changes WebViewImpl::closePagePopup() so that m_pagePopup is cleared
    > before calling WebPagePopupImpl::closePopup() to avoid reentrancy to it.
    > 
    > BUG=454043
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=200528
    
    TBR=keishi@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=454043
    
    Review URL: https://codereview.chromium.org/1297883002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200621 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 37426a99eab7f9be145de77db10b6b09e5f68839

--- third_party/WebKit/Source/web/WebViewImpl.cpp
+++ third_party/WebKit/Source/web/WebViewImpl.cpp
@@ -1646,15 +1646,11 @@ void WebViewImpl::closePagePopup(PagePopup* popup)
 {
     ASSERT(popup);
     WebPagePopupImpl* popupImpl = toWebPagePopupImpl(popup);
-    // It's possible that closePagePopup is called from pagePopup->closePopup()
-    // below because the main frame of the page popup can have the last
-    // reference to the pagePopupOwner Element. So, we clear m_pagePopup before
-    // pagePopup->closePopup(), and do nothing if m_pagepopup is nullptr.
-    ASSERT(!m_pagePopup || m_pagePopup.get() == popupImpl);
-    if (!m_pagePopup || m_pagePopup.get() != popupImpl)
+    ASSERT(m_pagePopup.get() == popupImpl);
+    if (m_pagePopup.get() != popupImpl)
         return;
-    RefPtr<WebPagePopupImpl> pagePopup = m_pagePopup.release();
-    pagePopup->closePopup();
+    m_pagePopup->closePopup();
+    m_pagePopup = nullptr;
     disablePopupMouseWheelEventListener();
 }
 
