e593ccc16510e5e9a127e2d3f676f1075b0762fe
Mark Seaborn <mseaborn@chromium.org>
2015-08-18 11:03:36 -0700

    PNaCl: Cleanup: Remove now-unused SRPC code from translator IRT interfaces
    Also remove the code for handling the earlier env var that takes a
    colon-separated list (NACL_IRT_PNACL_TRANSLATOR_LINK_INPUTS).
    
    BUG=https://code.google.com/p/chromium/issues/detail?id=302078
    TEST=run_hello_world_test use_sandboxed_translator=1
    R=dschuff@chromium.org
    
    Review URL: https://codereview.chromium.org/1291863002 .
    
    
    Former-commit-id: 418cdf131d3df79f0e91f631306c15c91e1b3b0f

--- src/untrusted/irt/irt_pnacl_translator_compile.c
+++ src/untrusted/irt/irt_pnacl_translator_compile.c
@@ -4,15 +4,13 @@
  * found in the LICENSE file.
  */
 
-#include <argz.h>
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "native_client/src/shared/platform/nacl_log.h"
-#include "native_client/src/shared/srpc/nacl_srpc.h"
 #include "native_client/src/untrusted/irt/irt_dev.h"
 #include "native_client/src/untrusted/irt/irt_interfaces.h"
 #include "native_client/src/untrusted/irt/irt_pnacl_translator_common.h"
@@ -22,114 +20,10 @@ static const char kOutputVar[] = "NACL_IRT_PNACL_TRANSLATOR_COMPILE_OUTPUT";
 static const char kArgVar[] = "NACL_IRT_PNACL_TRANSLATOR_COMPILE_ARG";
 static const char kThreadsVar[] = "NACL_IRT_PNACL_TRANSLATOR_COMPILE_THREADS";
 
-static const int kMaxObjectFiles = 16;
-
-static const struct nacl_irt_pnacl_compile_funcs *g_funcs;
-
-static void stream_init_with_split(
-    NaClSrpcRpc *rpc, NaClSrpcArg **in_args,
-    NaClSrpcArg **out_args, NaClSrpcClosure *done) {
-  int num_threads = in_args[0]->u.ival;
-  if (num_threads < 0 || num_threads > kMaxObjectFiles) {
-    NaClLog(LOG_FATAL, "Invalid # of threads (%d)\n", num_threads);
-  }
-  int fd_start = 1;
-  int i = fd_start;
-  int num_valid_fds = 0;
-  int obj_fds[kMaxObjectFiles];
-  while (num_valid_fds < kMaxObjectFiles && in_args[i]->u.hval >= 0) {
-    obj_fds[num_valid_fds] = in_args[i]->u.hval;
-    ++i;
-    ++num_valid_fds;
-  }
-  /*
-   * Convert the null-delimited strings into an array of
-   * null-terminated strings.
-   */
-  char *cmd_argz = in_args[kMaxObjectFiles + fd_start]->arrays.carr;
-  size_t cmd_argz_len = in_args[kMaxObjectFiles + fd_start]->u.count;
-  size_t argc = argz_count(cmd_argz, cmd_argz_len);
-  char **argv = (char **)malloc((argc + 1) * sizeof(char *));
-  argz_extract(cmd_argz, cmd_argz_len, argv);
-  char *result = g_funcs->init_callback(num_threads, obj_fds, num_valid_fds,
-                                        argv, argc);
-  free(argv);
-  if (result != NULL) {
-    rpc->result = NACL_SRPC_RESULT_APP_ERROR;
-    /*
-     * SRPC wants to free() the string, so just strdup here so that the
-     * init_callback implementation doesn't have to know if the string
-     * comes from malloc or new.  On error, we don't care so much
-     * about leaking this bit of memory.
-     */
-    out_args[0]->arrays.str = strdup(result);
-  } else {
-    rpc->result = NACL_SRPC_RESULT_OK;
-    out_args[0]->arrays.str = strdup("");
-  }
-  done->Run(done);
-}
-
-static void stream_chunk(NaClSrpcRpc *rpc, NaClSrpcArg **in_args,
-                         NaClSrpcArg **out_args, NaClSrpcClosure *done) {
-  int result = g_funcs->data_callback(
-      in_args[0]->arrays.carr,
-      in_args[0]->u.count);
-  rpc->result = result == 0 ? NACL_SRPC_RESULT_OK : NACL_SRPC_RESULT_APP_ERROR;
-  done->Run(done);
-}
-
-static void stream_end(NaClSrpcRpc *rpc, NaClSrpcArg **in_args,
-                       NaClSrpcArg **out_args, NaClSrpcClosure *done) {
-  char *result = g_funcs->end_callback();
-  /* Fill in the deprecated return values with dummy values. */
-  out_args[0]->u.ival = 0;
-  out_args[1]->arrays.str = strdup("");
-  out_args[2]->arrays.str = strdup("");
-  if (result != NULL) {
-    rpc->result = NACL_SRPC_RESULT_APP_ERROR;
-    /* SRPC wants to free(), so strdup() and leak to hide this detail. */
-    out_args[3]->arrays.str = strdup(result);
-  } else {
-    rpc->result = NACL_SRPC_RESULT_OK;
-    out_args[3]->arrays.str = strdup("");
-  }
-  done->Run(done);
-}
-
-static const struct NaClSrpcHandlerDesc srpc_methods[] = {
-  /*
-   * Protocol for streaming:
-   * StreamInitWithSplit(num_threads, obj_fd x 16, cmdline_flags) -> error_str
-   * StreamChunk(data) +
-   * TODO(jvoung): remove these is_shared_lib, etc.
-   * StreamEnd() -> (is_shared_lib, soname, dependencies, error_str)
-   */
-  { "StreamInitWithSplit:ihhhhhhhhhhhhhhhhC:s", stream_init_with_split },
-  { "StreamChunk:C:", stream_chunk },
-  { "StreamEnd::isss", stream_end },
-  { NULL, NULL },
-};
-
 static void serve_translate_request(
     const struct nacl_irt_pnacl_compile_funcs *funcs) {
   const char *input_filename = getenv(kInputVar);
   const char *threads_str = getenv(kThreadsVar);
-  if (input_filename == NULL && threads_str == NULL) {
-    /*
-     * TODO(mseaborn): After the PNaCl toolchain revision has been updated,
-     * this SRPC case will no longer be used and can be removed.
-     */
-    g_funcs = funcs;
-    if (!NaClSrpcModuleInit()) {
-      NaClLog(LOG_FATAL, "NaClSrpcModuleInit() failed\n");
-    }
-    if (!NaClSrpcAcceptClientConnection(srpc_methods)) {
-      NaClLog(LOG_FATAL, "NaClSrpcAcceptClientConnection() failed\n");
-    }
-    return;
-  }
-
   if (input_filename == NULL) {
     NaClLog(LOG_FATAL, "serve_translate_request: Env var %s not set\n",
             kInputVar);
--- src/untrusted/irt/irt_pnacl_translator_link.c
+++ src/untrusted/irt/irt_pnacl_translator_link.c
@@ -7,80 +7,21 @@
 #include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "native_client/src/shared/platform/nacl_log.h"
-#include "native_client/src/shared/srpc/nacl_srpc.h"
 #include "native_client/src/untrusted/irt/irt_dev.h"
 #include "native_client/src/untrusted/irt/irt_interfaces.h"
 #include "native_client/src/untrusted/irt/irt_pnacl_translator_common.h"
 
 static const char kInputVar[] = "NACL_IRT_PNACL_TRANSLATOR_LINK_INPUT";
-static const char kInputsVar[] = "NACL_IRT_PNACL_TRANSLATOR_LINK_INPUTS";
 static const char kOutputVar[] = "NACL_IRT_PNACL_TRANSLATOR_LINK_OUTPUT";
-
-static const int kMaxObjectFiles = 16;
-
-static int (*g_func)(int nexe_fd,
-                     const int *obj_file_fds,
-                     int obj_file_fd_count);
-
-static void handle_link_request(NaClSrpcRpc *rpc,
-                                NaClSrpcArg **in_args,
-                                NaClSrpcArg **out_args,
-                                NaClSrpcClosure *done) {
-  int obj_file_count = in_args[0]->u.ival;
-  int nexe_fd = in_args[kMaxObjectFiles + 1]->u.hval;
-
-  if (obj_file_count < 1 || obj_file_count > kMaxObjectFiles) {
-    NaClLog(LOG_FATAL, "Bad object file count (%i)\n", obj_file_count);
-  }
-  int obj_file_fds[obj_file_count];
-  for (int i = 0; i < obj_file_count; i++) {
-    obj_file_fds[i] = in_args[i + 1]->u.hval;
-  }
-
-  int result = g_func(nexe_fd, obj_file_fds, obj_file_count);
-
-  rpc->result = result == 0 ? NACL_SRPC_RESULT_OK : NACL_SRPC_RESULT_APP_ERROR;
-  done->Run(done);
-}
-
-static const struct NaClSrpcHandlerDesc srpc_methods[] = {
-  { "RunWithSplit:ihhhhhhhhhhhhhhhhh:", handle_link_request },
-  { NULL, NULL },
-};
-
-static size_t count_colon_separators(const char *str) {
-  size_t count = 0;
-  for (; *str != '\0'; ++str) {
-    if (*str == ':')
-      ++count;
-  }
-  return count;
-}
 
 static void serve_link_request(int (*func)(int nexe_fd,
                                            const int *obj_file_fds,
                                            int obj_file_fd_count)) {
-  const char *inputs_str = getenv(kInputsVar);
   const char *output_filename = getenv(kOutputVar);
-  if (output_filename == NULL) {
-    /*
-     * TODO(mseaborn): After the PNaCl toolchain revision has been updated,
-     * this SRPC case will no longer be used and can be removed.
-     */
-    g_func = func;
-    if (!NaClSrpcModuleInit()) {
-      NaClLog(LOG_FATAL, "NaClSrpcModuleInit() failed\n");
-    }
-    if (!NaClSrpcAcceptClientConnection(srpc_methods)) {
-      NaClLog(LOG_FATAL, "NaClSrpcAcceptClientConnection() failed\n");
-    }
-    return;
-  }
-
   if (output_filename == NULL)
     NaClLog(LOG_FATAL, "serve_link_request: Env var %s not set\n", kOutputVar);
 
@@ -90,41 +31,6 @@ static void serve_link_request(int (*func)(int nexe_fd,
     NaClLog(LOG_FATAL,
             "serve_link_request: Failed to open output file \"%s\": %s\n",
             output_filename, strerror(errno));
-  }
-
-  if (inputs_str != NULL) {
-    /*
-     * Open input files.  Parse colon-separated list.  This is the old
-     * case, which can't handle colons in filenames and so doesn't work
-     * when using Windows' absolute filenames.
-     *
-     * TODO(mseaborn): After the PNaCl toolchain revision has been updated,
-     * this case will no longer be used and can be removed.
-     */
-    if (*inputs_str == '\0')
-      NaClLog(LOG_FATAL, "serve_link_request: List of inputs is empty\n");
-    char *inputs_copy = strdup(inputs_str);
-    size_t inputs_count = 1 + count_colon_separators(inputs_copy);
-    int input_fds[inputs_count];
-    char *pos = inputs_copy;
-    size_t i = 0;
-    while (pos != NULL) {
-      char *input_filename = pos;
-      strsep(&pos, ":");
-      int input_fd = open(input_filename, O_RDONLY);
-      if (input_fd < 0) {
-        NaClLog(LOG_FATAL,
-                "serve_link_request: Failed to open input file \"%s\": %s\n",
-                input_filename, strerror(errno));
-      }
-      assert(i < inputs_count);
-      input_fds[i++] = input_fd;
-    }
-    assert(i == inputs_count);
-    free(inputs_copy);
-
-    func(nexe_fd, input_fds, inputs_count);
-    return;
   }
 
   /* Open input files. */
