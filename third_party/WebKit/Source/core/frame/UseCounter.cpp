2b0a3e50e27a8da84d3b401c367f99165ba173d5
ddorwin@chromium.org <ddorwin@chromium.org>
2015-08-19 13:06:16 +0000

    Update prefixed EME deprecation message.
    BUG=442555
    
    Review URL: https://codereview.chromium.org/1297433004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@200824 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 604beae9f98b61b843acb145d68409ba2617151c

--- third_party/WebKit/Source/core/frame/UseCounter.cpp
+++ third_party/WebKit/Source/core/frame/UseCounter.cpp
@@ -859,10 +859,10 @@ String UseCounter::deprecationMessage(Feature feature)
     case PrefixedMediaAddKey:
     case PrefixedMediaGenerateKeyRequest:
     case PrefixedMediaCancelKeyRequest:
-        return "The prefixed Encrypted Media Extensions APIs are deprecated and will be removed in M46 (beta around September 2015). Please use 'navigator.requestMediaKeySystemAccess()' instead.";
+        return "The prefixed Encrypted Media Extensions APIs are deprecated and will be removed in M47 (beta around October 2015). Please use 'navigator.requestMediaKeySystemAccess()' instead.";
 
     case CanPlayTypeKeySystem:
-        return "canPlayType()'s 'keySystem' parameter is deprecated and will be ignored in M46 (beta around September 2015). Please use 'navigator.requestMediaKeySystemAccess()' instead.";
+        return "canPlayType()'s 'keySystem' parameter is deprecated and will be ignored in M47 (beta around October 2015). Please use 'navigator.requestMediaKeySystemAccess()' instead.";
 
     case SVGSVGElementForceRedraw:
         return "'SVGSVGElement.forceRedraw()' is deprecated, please do not use it. It is a no-op, as per SVG2 (https://svgwg.org/svg2-draft/struct.html#__svg__SVGSVGElement__forceRedraw).";
