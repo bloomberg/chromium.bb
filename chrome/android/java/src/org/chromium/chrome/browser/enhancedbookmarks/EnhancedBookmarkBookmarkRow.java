e74e1aa97d306f9730bfef975e1779193f59bc76
twellington <twellington@chromium.org>
2015-08-21 15:30:58 -0700

    Turn off interpolation for bookmark favicons
    BUG=
    
    Review URL: https://codereview.chromium.org/1309943002
    
    Cr-Commit-Position: refs/heads/master@{#344884}
    
    Former-commit-id: c8fdfd0a45e7d139cef348a2be917d746dd6b269

--- chrome/android/java/src/org/chromium/chrome/browser/enhancedbookmarks/EnhancedBookmarkBookmarkRow.java
+++ chrome/android/java/src/org/chromium/chrome/browser/enhancedbookmarks/EnhancedBookmarkBookmarkRow.java
@@ -91,7 +91,7 @@ public class EnhancedBookmarkBookmarkRow extends EnhancedBookmarkRow implements 
         } else {
             RoundedBitmapDrawable roundedIcon = RoundedBitmapDrawableFactory.create(
                     getResources(),
-                    Bitmap.createScaledBitmap(icon, mDisplayedIconSize, mDisplayedIconSize, true));
+                    Bitmap.createScaledBitmap(icon, mDisplayedIconSize, mDisplayedIconSize, false));
             roundedIcon.setCornerRadius(mCornerRadius);
             mIconImageView.setImageDrawable(roundedIcon);
         }
