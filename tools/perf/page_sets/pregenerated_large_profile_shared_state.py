d8b322ddbb17966cc80ce6c2d5226cacc13df6f2
nednguyen <nednguyen@google.com>
2015-08-20 18:50:36 -0700

    [Telemetry] Remove the reference to perf directory in shared_page_state
    BUG=522982
    
    Review URL: https://codereview.chromium.org/1308513003
    
    Cr-Commit-Position: refs/heads/master@{#344648}
    
    Former-commit-id: 67bb2e239516d1cba02a28b418be94327660dab6

--- tools/perf/page_sets/pregenerated_large_profile_shared_state.py
+++ tools/perf/page_sets/pregenerated_large_profile_shared_state.py
@@ -1,11 +1,19 @@
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import os
+
 from telemetry.page import shared_page_state
+
 
 
 class PregeneratedLargeProfileSharedState(shared_page_state.SharedPageState):
   def __init__(self, test, finder_options, story_set):
     super(PregeneratedLargeProfileSharedState, self).__init__(
         test, finder_options, story_set)
-    self.SetPregeneratedProfileArchive('large_profile.zip')
+    perf_dir = os.path.join(os.path.dirname(__file__), os.path.pardir)
+    profile_archive_dir = os.path.join(
+        perf_dir, 'generated_profiles', self._possible_browser.target_os,
+        'large_profile.zip')
+
+    self.SetPregeneratedProfileArchive(profile_archive_dir)
