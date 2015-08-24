1e316d0a60a809877e1d9323c108d24bf9f1d9d4
mdempsky <mdempsky@chromium.org>
2015-08-24 15:40:26 -0700

    sandbox/linux/bpf_dsl: add DumpBPF::StringPrintProgram
    Android doesn't have open_memstream, so the code had to be cleaned up
    to use something else.  base::StringAppendF seems like the least bad
    option, and this code will eventually be test-only code anyway.
    
    While here, clean up the code to be clang-format'able.  Also, satisfy
    my pedantic side by switching from int casts to using <inttypes.h>.
    
    BUG=414363
    
    Review URL: https://codereview.chromium.org/1310683002
    
    Cr-Commit-Position: refs/heads/master@{#345195}
    
    Former-commit-id: e1bc35ab4d1f3da73d0532dbbcc3999fcb599fe3

--- sandbox/linux/bpf_dsl/dump_bpf.h
+++ sandbox/linux/bpf_dsl/dump_bpf.h
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+
 #include "sandbox/linux/bpf_dsl/codegen.h"
 #include "sandbox/sandbox_export.h"
 
@@ -12,6 +14,10 @@ class SANDBOX_EXPORT DumpBPF {
  public:
   // PrintProgram writes |program| in a human-readable format to stderr.
   static void PrintProgram(const CodeGen::Program& program);
+
+  // StringPrintProgram writes |program| in a human-readable format to
+  // a std::string.
+  static std::string StringPrintProgram(const CodeGen::Program& program);
 };
 
 }  // namespace bpf_dsl
