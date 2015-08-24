355a0f590a365ab33831f48d6a3bce0daababc6d
erikchen <erikchen@chromium.org>
2015-08-24 10:26:39 -0700

    Revert of [Telemetry] [RELAND] Remove the reference to perf directory in shared_page_state (patchset #2 id:20001 of https://codereview.chromium.org/1309853002/ )
    Reason for revert:
    Causes failures in chromium.perf.
    
    See https://code.google.com/p/chromium/issues/detail?id=524027 for more details.
    
    Original issue's description:
    > [Telemetry] [RELAND] Remove the reference to perf directory in shared_page_state
    >
    > BUG=522982
    > CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    >
    > TBR=erikchen@chromium.org
    >
    > Committed: https://crrev.com/531c2fbbefc5bf331261823148945647d3c4b733 [formerly 46aa7ffeb91f12560cf2cf6803a8de916013efd1]
    > Cr-Commit-Position: refs/heads/master@{#344850}
    
    TBR=prasadv@chromium.org,nednguyen@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=522982
    
    Review URL: https://codereview.chromium.org/1305993003
    
    Cr-Commit-Position: refs/heads/master@{#345107}
    
    Former-commit-id: f21b27b707aa6fdbea6d4975479fb860d7eccb7c

--- tools/perf/page_sets/pregenerated_large_profile_shared_state.py
+++ tools/perf/page_sets/pregenerated_large_profile_shared_state.py
@@ -1,19 +1,11 @@
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-import os
-
 from telemetry.page import shared_page_state
-
 
 
 class PregeneratedLargeProfileSharedState(shared_page_state.SharedPageState):
   def __init__(self, test, finder_options, story_set):
     super(PregeneratedLargeProfileSharedState, self).__init__(
         test, finder_options, story_set)
-    perf_dir = os.path.join(os.path.dirname(__file__), os.path.pardir)
-    profile_archive_dir = os.path.join(
-        perf_dir, 'generated_profiles', self._possible_browser.target_os,
-        'large_profile.zip')
-
-    self.SetPregeneratedProfileArchiveDir(profile_archive_dir)
+    self.SetPregeneratedProfileArchive('large_profile.zip')
