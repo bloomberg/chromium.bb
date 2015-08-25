816d085d71345fc657de6a4594c742d6a6161123
rmistry@google.com <rmistry@google.com>
2015-08-25 16:57:02 +0000

    get_depends_on_patchset is actually a POST endpoint
    Context= https://groups.google.com/a/chromium.org/d/msg/infra-dev/GNKEiqLqKPg/M_rjXvwvLQAJ
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1315893002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@296430 0039d316-1c4b-4281-b951-d872f2087c98

--- rietveld.py
+++ rietveld.py
@@ -89,7 +89,7 @@ class Rietveld(object):
     url = '/%d/patchset/%d/get_depends_on_patchset' % (issue, patchset)
     resp = None
     try:
-      resp = json.loads(self.get(url))
+      resp = json.loads(self.post(url, []))
     except (urllib2.HTTPError, ValueError):
       # The get_depends_on_patchset endpoint does not exist on this Rietveld
       # instance yet. Ignore the error and proceed.
