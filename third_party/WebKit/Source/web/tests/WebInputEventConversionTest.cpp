c8a8f0925d2d65b0814607cce50a09255ddcfdea
ericwilligers@chromium.org <ericwilligers@chromium.org>
2015-08-26 03:54:25 +0000

    WebInputEventConversionTest: each test uses a distict baseURL
    WebInputEventConversionTest.ElasticOverscrollWithPageReload was failing:
    [ RUN      ] WebInputEventConversionTest.ElasticOverscrollWithPageReload
    Check failed: url_to_reponse_info_.find(url) == url_to_reponse_info_.end().
    
    BUG=521229
    
    Review URL: https://codereview.chromium.org/1302313008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201194 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 72ab14dd3d6fbc6d57a622a9a759c2250bb0e6e8

--- third_party/WebKit/Source/web/tests/WebInputEventConversionTest.cpp
+++ third_party/WebKit/Source/web/tests/WebInputEventConversionTest.cpp
@@ -823,7 +823,7 @@ TEST(WebInputEventConversionTest, ElasticOverscroll)
 // Page reload/navigation should not reset elastic overscroll.
 TEST(WebInputEventConversionTest, ElasticOverscrollWithPageReload)
 {
-    const std::string baseURL("http://www.test5.com/");
+    const std::string baseURL("http://www.test6.com/");
     const std::string fileName("fixed_layout.html");
 
     URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(baseURL.c_str()), WebString::fromUTF8("fixed_layout.html"));
@@ -860,7 +860,7 @@ TEST(WebInputEventConversionTest, ElasticOverscrollWithPageReload)
 
 TEST(WebInputEventConversionTest, WebMouseWheelEventBuilder)
 {
-    const std::string baseURL("http://www.test6.com/");
+    const std::string baseURL("http://www.test7.com/");
     const std::string fileName("fixed_layout.html");
 
     URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(baseURL.c_str()), WebString::fromUTF8("fixed_layout.html"));
@@ -892,7 +892,7 @@ TEST(WebInputEventConversionTest, WebMouseWheelEventBuilder)
 
 TEST(WebInputEventConversionTest, PlatformWheelEventBuilder)
 {
-    const std::string baseURL("http://www.test7.com/");
+    const std::string baseURL("http://www.test8.com/");
     const std::string fileName("fixed_layout.html");
 
     URLTestHelpers::registerMockedURLFromBaseURL(WebString::fromUTF8(baseURL.c_str()), WebString::fromUTF8("fixed_layout.html"));
