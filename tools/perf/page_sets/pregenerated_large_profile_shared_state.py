181d247e08e38688e7c5dd491c3e9f8b4c45f0ab
nednguyen <nednguyen@google.com>
2015-08-26 14:49:31 -0700

    [Telemetry] [RELAND] Remove the reference to perf directory in shared_page_state
    Looking at the log in https://code.google.com/p/chromium/issues/detail?id=524027
    and compare with the log after revert, it seems to me the only
    different is the path of profile archive directory:
    
    + Log before revert: Using profile directory:'C:\b\build\slave\Win_7_ATI_GPU_Perf__4_\build\src\tools\perf\page_sets\..\generated_profiles\win32\large_profile'
    (https://code.google.com/p/chromium/issues/detail?id=524027)
    
    + Log after revert : Using profile directory:'C:\b\build\slave\Win_7_ATI_GPU_Perf__4_\build\src\tools\perf\generated_profiles\win32\large_profile'
    (http://build.chromium.org/p/chromium.perf/builders/Win%207%20ATI%20GPU%20Perf%20%284%29/builds/1360)
    
    *Update: reason why rmtree fail on windows when the path has the extra "\..\":
    http://stackoverflow.com/questions/1857335/is-there-any-length-limits-of-file-path-in-ntfs#1857477
    
    BUG=522982, 524027
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1316103002
    
    Cr-Commit-Position: refs/heads/master@{#345689}
    
    Former-commit-id: 60cbb11f984d5b19a0ecb5cd072be6b6e6d34990

--- tools/perf/page_sets/pregenerated_large_profile_shared_state.py
+++ tools/perf/page_sets/pregenerated_large_profile_shared_state.py
@@ -1,6 +1,8 @@
 # Copyright 2015 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import os
+
 from telemetry.page import shared_page_state
 
 
@@ -8,4 +10,10 @@ class PregeneratedLargeProfileSharedState(shared_page_state.SharedPageState):
   def __init__(self, test, finder_options, story_set):
     super(PregeneratedLargeProfileSharedState, self).__init__(
         test, finder_options, story_set)
-    self.SetPregeneratedProfileArchive('large_profile.zip')
+    perf_dir = os.path.normpath(
+        os.path.join(os.path.dirname(__file__), os.path.pardir))
+    profile_archive_dir = os.path.join(
+        perf_dir, 'generated_profiles', self._possible_browser.target_os,
+        'large_profile.zip')
+
+    self.SetPregeneratedProfileArchiveDir(profile_archive_dir)
