dd05580874b886eb6d9aaacd32a11bc231c867d1
estark@chromium.org <estark@chromium.org>
2015-08-26 20:52:34 +0000

    Split up mixed content devtools test into 3 separate tests
    When testing all three mixed content types in one test, the test was
    flaky because DevTools received requestWillBeSent events in different
    orders. Instead of making sure that the requests are always sent in the
    same order, this CL splits up the test into 3 separate tests, one for
    each mixed content type, with common code in
    resources/mixed-content-type-test.js.
    
    BUG=523500
    
    Review URL: https://codereview.chromium.org/1303033004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201247 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: be8b51b80b6ba1d43aada6044585d4e349665a95

---
+++ third_party/WebKit/LayoutTests/http/tests/inspector-protocol/resources/mixed-content-type-test.js
@@ -0,0 +1,30 @@
+if (window.testRunner) {
+    testRunner.overridePreference("WebKitAllowRunningInsecureContent", true);
+    testRunner.overridePreference("WebKitAllowDisplayingInsecureContent", true);
+}
+
+function test()
+{
+    InspectorTest.eventHandler["Network.requestWillBeSent"] = onRequestWillBeSent;
+    InspectorTest.sendCommand("Network.enable", {}, didEnableNetwork);
+
+    function didEnableNetwork(messageObject)
+    {
+        if (messageObject.error) {
+            InspectorTest.log("FAIL: Couldn't enable network agent" + messageObject.error.message);
+            InspectorTest.completeTest();
+            return;
+        }
+        InspectorTest.log("Network agent enabled");
+        InspectorTest.sendCommand("Runtime.evaluate", { "expression": "addIframeWithMixedContent()" });
+    }
+
+    var numRequests = 0;
+    function onRequestWillBeSent(event) {
+        var req = event.params.request;
+        InspectorTest.log("Mixed content type of " + req.url + ": " + req.mixedContentType);
+        numRequests++;
+        if (numRequests == 2)
+            InspectorTest.completeTest();
+    }
+}
