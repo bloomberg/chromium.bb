fb484fb3c200e3b41bd99f27c739717321be364a
senorblanco <senorblanco@chromium.org>
2015-08-27 14:05:02 -0700

    Define SK_SAVE_LAYER_BOUNDS_ARE_FILTERED for an upcoming Skia change.
    Also hide code that depends on saveLayer() bounds being filtered behind
    the ifdef.
    
    BUG=skia:3194
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1316243002
    
    Cr-Commit-Position: refs/heads/master@{#345985}
    
    Former-commit-id: 0c3b9ee8472937beddabf1f5fa08355cb33aa4dc

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -226,6 +226,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_SHADERBITMAPTYPE
 #endif
 
+#ifndef    SK_SAVE_LAYER_BOUNDS_ARE_FILTERED
+#   define SK_SAVE_LAYER_BOUNDS_ARE_FILTERED
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
