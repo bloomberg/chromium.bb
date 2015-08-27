f1c5cf597aa25bec2761320bd5d8d282d77feff3
xunjieli <xunjieli@chromium.org>
2015-08-27 14:36:29 -0700

    Add a test to make sure ExtendedResponseInfo#getTotalBytesReceived is correct
    BUG=525070
    
    Review URL: https://codereview.chromium.org/1307313003
    
    Cr-Commit-Position: refs/heads/master@{#345995}
    
    Former-commit-id: f803e40a9a842c5df9c85c88a408a12de5357fc7

--- components/cronet/android/test/javatests/src/org/chromium/net/QuicTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/QuicTest.java
@@ -86,8 +86,11 @@ public class QuicTest extends CronetTestBase {
         listener.blockForDone();
 
         assertEquals(200, listener.mResponseInfo.getHttpStatusCode());
-        assertEquals("This is a simple text file served by QUIC.\n", listener.mResponseAsString);
+        String expectedContent = "This is a simple text file served by QUIC.\n";
+        assertEquals(expectedContent, listener.mResponseAsString);
         assertEquals("quic/1+spdy/3", listener.mResponseInfo.getNegotiatedProtocol());
+        assertEquals(
+                expectedContent.length(), listener.mExtendedResponseInfo.getTotalReceivedBytes());
 
         // This test takes a long time, since the update will only be scheduled
         // after kUpdatePrefsDelayMs in http_server_properties_manager.cc.
@@ -118,8 +121,10 @@ public class QuicTest extends CronetTestBase {
         request2.start();
         listener2.blockForDone();
         assertEquals(200, listener2.mResponseInfo.getHttpStatusCode());
-        assertEquals("This is a simple text file served by QUIC.\n", listener2.mResponseAsString);
+        assertEquals(expectedContent, listener2.mResponseAsString);
         assertEquals("quic/1+spdy/3", listener2.mResponseInfo.getNegotiatedProtocol());
+        assertEquals(
+                expectedContent.length(), listener2.mExtendedResponseInfo.getTotalReceivedBytes());
     }
 
     // Returns whether a file contains a particular string.
