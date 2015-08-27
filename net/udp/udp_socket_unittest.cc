276f9fd4e0f948487a5d070e5b6a25c34a49684b
ellyjones <ellyjones@chromium.org>
2015-08-27 09:28:05 -0700

    UDPSocketTest: disable use of 192.168.1.1 on iOS
    This address isn't available if the network isn't available and causes flaky
    test failures.
    
    BUG=523225
    
    Review URL: https://codereview.chromium.org/1321463003
    
    Cr-Commit-Position: refs/heads/master@{#345896}
    
    Former-commit-id: 74165fab66c8804876a6617ce0c74bd37679c259

--- net/udp/udp_socket_unittest.cc
+++ net/udp/udp_socket_unittest.cc
@@ -480,14 +480,7 @@ TEST_F(UDPSocketTest, VerifyConnectBindsAddr) {
   DCHECK(simple_message == str);
 }
 
-// TODO(ellyjones): This test is flaky on iOS devices bots.
-// http://crbug.com/523225
-#if defined(OS_IOS) && !TARGET_IPHONE_SIMULATOR
-#define MAYBE_ClientGetLocalPeerAddresses FLAKY_ClientGetLocalPeerAddresses
-#else
-#define MAYBE_ClientGetLocalPeerAddresses ClientGetLocalPeerAddresses
-#endif
-TEST_F(UDPSocketTest, MAYBE_ClientGetLocalPeerAddresses) {
+TEST_F(UDPSocketTest, ClientGetLocalPeerAddresses) {
   struct TestData {
     std::string remote_address;
     std::string local_address;
@@ -495,8 +488,9 @@ TEST_F(UDPSocketTest, MAYBE_ClientGetLocalPeerAddresses) {
   } tests[] = {
     { "127.0.00.1", "127.0.0.1", false },
     { "::1", "::1", true },
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_IOS)
     // Addresses below are disabled on Android. See crbug.com/161248
+    // They are also disabled on iOS. See https://crbug.com/523225
     { "192.168.1.1", "127.0.0.1", false },
     { "2001:db8:0::42", "::1", true },
 #endif
