980a5d3a423597ae6e2236ae30828da48ea24080
olli.raula <olli.raula@intel.com>
2015-08-27 21:43:28 -0700

    Remove use of JSONReader::DeprecatedRead from host_config.cc
    Remove use of deprecated function DeprecatedRead from
    remoting/host/host_config.cc
    
    BUG=523194
    
    Review URL: https://codereview.chromium.org/1319793003
    
    Cr-Commit-Position: refs/heads/master@{#346079}
    
    Former-commit-id: ab51ffcbcaf6dea98d13c99d6a904cddcd970855

--- remoting/host/host_config.cc
+++ remoting/host/host_config.cc
@@ -14,8 +14,8 @@ namespace remoting {
 
 scoped_ptr<base::DictionaryValue> HostConfigFromJson(
     const std::string& json) {
-  scoped_ptr<base::Value> value(
-      base::JSONReader::DeprecatedRead(json, base::JSON_ALLOW_TRAILING_COMMAS));
+  scoped_ptr<base::Value> value =
+      base::JSONReader::Read(json, base::JSON_ALLOW_TRAILING_COMMAS);
   if (!value || !value->IsType(base::Value::TYPE_DICTIONARY)) {
     LOG(WARNING) << "Failed to parse host config from JSON";
     return nullptr;
