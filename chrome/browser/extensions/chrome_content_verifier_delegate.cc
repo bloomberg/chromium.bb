55cc995a3622befeace6a86cf1bbc68c33d190ca
asargent <asargent@chromium.org>
2015-08-31 18:38:29 -0700

    Change default content verification mode for Google Chrome builds
    It's been configured to be set to 'enforce' mode via the server-side
    experiment for quite a while, so it makes sense to have it turned on by
    default in the code as well.
    
    BUG=526873
    
    Review URL: https://codereview.chromium.org/1307103003
    
    Cr-Commit-Position: refs/heads/master@{#346552}
    
    Former-commit-id: d522554d6554e461249a0041716d1574cb34e3e3

--- chrome/browser/extensions/chrome_content_verifier_delegate.cc
+++ chrome/browser/extensions/chrome_content_verifier_delegate.cc
@@ -41,7 +41,12 @@ namespace extensions {
 ContentVerifierDelegate::Mode ChromeContentVerifierDelegate::GetDefaultMode() {
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
 
-  Mode experiment_value = NONE;
+  Mode experiment_value;
+#if defined(GOOGLE_CHROME_BUILD)
+  experiment_value = ContentVerifierDelegate::ENFORCE;
+#else
+  experiment_value = ContentVerifierDelegate::NONE;
+#endif
   const std::string group =
       base::FieldTrialList::FindFullName(kContentVerificationExperimentName);
   if (group == "EnforceStrict")
@@ -50,6 +55,8 @@ ContentVerifierDelegate::Mode ChromeContentVerifierDelegate::GetDefaultMode() {
     experiment_value = ContentVerifierDelegate::ENFORCE;
   else if (group == "Bootstrap")
     experiment_value = ContentVerifierDelegate::BOOTSTRAP;
+  else if (group == "None")
+    experiment_value = ContentVerifierDelegate::NONE;
 
   // The field trial value that normally comes from the server can be
   // overridden on the command line, which we don't want to allow since
