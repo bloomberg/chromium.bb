c0e035844824929a4732d460998243a5283bb1e2
sdefresne <sdefresne@chromium.org>
2015-09-01 06:45:49 -0700

    Fix clobber build on iOS.
    http://crrev.com/1310073004 renamed the generated file ios_strings_resources.h
    to ios_strings.h but forgot to fix some #includes. This followup CL do rename
    them to get the clobber build to work.
    
    BUG=475514
    
    Review URL: https://codereview.chromium.org/1316183003
    
    Cr-Commit-Position: refs/heads/master@{#346643}
    
    Former-commit-id: 2681bd6ff49ed94e63bbfd73df8d6a265d1906c8

--- ios/chrome/browser/autofill/form_input_accessory_view.mm
+++ ios/chrome/browser/autofill/form_input_accessory_view.mm
@@ -12,7 +12,7 @@
 #import "ios/chrome/browser/autofill/form_input_accessory_view_delegate.h"
 #import "ios/chrome/browser/ui/image_util.h"
 #include "ios/chrome/browser/ui/ui_util.h"
-#include "ios/chrome/grit/ios_strings_resources.h"
+#include "ios/chrome/grit/ios_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace {
