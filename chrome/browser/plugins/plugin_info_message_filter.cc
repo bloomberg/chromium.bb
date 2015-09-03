8ebbc047b4650b52385b189046f395852ebc1c3a
tommycli <tommycli@chromium.org>
2015-09-03 14:06:10 -0700

    Plugins: Always mark plugins within Chrome Apps <webview> as Unauthorized.
    Previously, they were marked as Unauthorized only when they were set as ALLOW
    in Content Settings. This created a (small?) security issue when Plugins were
    marked as DETECT or BLOCK.
    
    BUG=527620
    
    Review URL: https://codereview.chromium.org/1325163002
    
    Cr-Commit-Position: refs/heads/master@{#347241}
    
    Former-commit-id: 550b22226d00e17b4d387b22d9ee10a3b64d977f

--- chrome/browser/plugins/plugin_info_message_filter.cc
+++ chrome/browser/plugins/plugin_info_message_filter.cc
@@ -454,18 +454,21 @@ void PluginInfoMessageFilter::Context::DecidePluginStatus(
                   : ChromeViewHostMsg_GetPluginInfo_Status::kBlocked;
   }
 
-  if (*status == ChromeViewHostMsg_GetPluginInfo_Status::kAllowed) {
-    // Allow an embedder of <webview> to block a plugin from being loaded inside
-    // the guest. In order to do this, set the status to 'Unauthorized' here,
-    // and update the status as appropriate depending on the response from the
-    // embedder.
 #if defined(ENABLE_EXTENSIONS)
+  // Allow an embedder of <webview> to block a plugin from being loaded inside
+  // the guest. In order to do this, set the status to 'Unauthorized' here,
+  // and update the status as appropriate depending on the response from the
+  // embedder.
+  if (*status == ChromeViewHostMsg_GetPluginInfo_Status::kAllowed ||
+      *status == ChromeViewHostMsg_GetPluginInfo_Status::kBlocked ||
+      *status ==
+          ChromeViewHostMsg_GetPluginInfo_Status::kPlayImportantContent) {
     if (extensions::WebViewRendererState::GetInstance()->IsGuest(
-        render_process_id_))
+            render_process_id_))
       *status = ChromeViewHostMsg_GetPluginInfo_Status::kUnauthorized;
 
-#endif
   }
+#endif
 }
 
 bool PluginInfoMessageFilter::Context::FindEnabledPlugin(
