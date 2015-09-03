56f3bc104384d0b2e2d7d86208e5d57325184f04
dbeam <dbeam@chromium.org>
2015-09-03 10:53:16 -0700

    Fix fonts and text direction on all chrome:// pages on iOS
    R=stuartmorgan@chromium.org
    BUG=527608
    
    Review URL: https://codereview.chromium.org/1312123006
    
    Cr-Commit-Position: refs/heads/master@{#347182}
    
    Former-commit-id: e203f8a90a37ea2af1f70f93d57f91a5eedcf657

--- ios/web/webui/shared_resources_data_source_ios.cc
+++ ios/web/webui/shared_resources_data_source_ios.cc
@@ -55,16 +55,8 @@ void SharedResourcesDataSourceIOS::StartDataRequest(
   WebClient* web_client = GetWebClient();
 
   if (idr == IDR_WEBUI_CSS_TEXT_DEFAULTS) {
-    std::vector<std::string> placeholders;
-    placeholders.push_back(webui::GetTextDirection());  // $1
-    placeholders.push_back(webui::GetFontFamily());  // $2
-    placeholders.push_back(webui::GetFontSize());  // $3
-
-    const std::string& chrome_shared =
-        web_client->GetDataResource(idr, ui::SCALE_FACTOR_NONE).as_string();
-    std::string replaced =
-        base::ReplaceStringPlaceholders(chrome_shared, placeholders, nullptr);
-    bytes = base::RefCountedString::TakeString(&replaced);
+    std::string css = webui::GetWebUiCssTextDefaults();
+    bytes = base::RefCountedString::TakeString(&css);
   } else {
     bytes = web_client->GetDataResourceBytes(idr);
   }
