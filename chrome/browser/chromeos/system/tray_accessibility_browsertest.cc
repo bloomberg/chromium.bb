1a8ca446e4dcea02dae2ba198798deb41bd00176
yoshiki <yoshiki@chromium.org>
2015-09-03 23:32:45 -0700

    Re-enable flaky tests in TrayAcceccibilityTest
    These tests have been disabled for a while due to flakiness, but as I tested on ToT, it looks non-flaky. I ran them 100 times locally and 20 times on trybots, but they were never failed. Hence this patch just re-enables them now.
    
    BUG=396342, 396318
    TEST=run locally and on trybot
    TBR=dmazzoni@chromium.org, oshima@chromium.org
    
    Review URL: https://codereview.chromium.org/1330603004
    
    Cr-Commit-Position: refs/heads/master@{#347344}
    
    Former-commit-id: 064fa9edd269836e634a852f8911899eb8f5c9de

--- chrome/browser/chromeos/system/tray_accessibility_browsertest.cc
+++ chrome/browser/chromeos/system/tray_accessibility_browsertest.cc
@@ -277,8 +277,7 @@ IN_PROC_BROWSER_TEST_P(TrayAccessibilityTest, LoginStatus) {
   EXPECT_EQ(ash::user::LOGGED_IN_USER, GetLoginStatus());
 }
 
-// http://crbug.com/396342
-IN_PROC_BROWSER_TEST_P(TrayAccessibilityTest, DISABLED_ShowTrayIcon) {
+IN_PROC_BROWSER_TEST_P(TrayAccessibilityTest, ShowTrayIcon) {
   SetLoginStatus(ash::user::LOGGED_IN_NONE);
 
   // Confirms that the icon is invisible before login.
@@ -347,8 +346,7 @@ IN_PROC_BROWSER_TEST_P(TrayAccessibilityTest, DISABLED_ShowTrayIcon) {
   EXPECT_FALSE(IsTrayIconVisible());
 }
 
-// http://crbug.com/396342
-IN_PROC_BROWSER_TEST_P(TrayAccessibilityTest, DISABLED_ShowMenu) {
+IN_PROC_BROWSER_TEST_P(TrayAccessibilityTest, ShowMenu) {
   // Login
   user_manager::UserManager::Get()->UserLoggedIn(
       "owner@invalid.domain", "owner@invalid.domain", true);
@@ -416,9 +414,7 @@ IN_PROC_BROWSER_TEST_P(TrayAccessibilityTest, DISABLED_ShowMenu) {
   EXPECT_FALSE(CanCreateMenuItem());
 }
 
-// http://crbug.com/396318
-IN_PROC_BROWSER_TEST_P(TrayAccessibilityTest,
-    DISABLED_ShowMenuWithShowMenuOption) {
+IN_PROC_BROWSER_TEST_P(TrayAccessibilityTest, ShowMenuWithShowMenuOption) {
   // Login
   user_manager::UserManager::Get()->UserLoggedIn(
       "owner@invalid.domain", "owner@invalid.domain", true);
