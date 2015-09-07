6c88173f7ecc2470341ca96e91b6ccb0a5efbef6
Emil Velikov <emil.l.velikov@gmail.com>
2015-09-07 14:13:32 +0100

    util_math: add MAX3 macro
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Acked-by: Alex Deucher <alexander.deucher@amd.com>

--- util_math.h
+++ util_math.h
@@ -26,6 +26,7 @@
 
 #define MIN2( A, B )   ( (A)<(B) ? (A) : (B) )
 #define MAX2( A, B )   ( (A)>(B) ? (A) : (B) )
+#define MAX3( A, B, C ) ((A) > (B) ? MAX2(A, C) : MAX2(B, C))
 
 #define __align_mask(value, mask)  (((value) + (mask)) & ~(mask))
 #define ALIGN(value, alignment)    __align_mask(value, (typeof(value))((alignment) - 1))
