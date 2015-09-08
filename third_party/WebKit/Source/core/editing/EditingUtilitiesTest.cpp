693d8d6dd31a15cab37be720f8a7c1adbd7308ca
sigbjornf@opera.com <sigbjornf@opera.com>
2015-09-08 10:37:08 +0000

    Oilpan: fix build after r201904.
    TBR=oilpan-reviews,yosin
    BUG=513568
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1303993005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201907 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: b7ac310062d223bee05205d66795c2544cc1ceb6

--- third_party/WebKit/Source/core/editing/EditingUtilitiesTest.cpp
+++ third_party/WebKit/Source/core/editing/EditingUtilitiesTest.cpp
@@ -84,7 +84,7 @@ TEST_F(EditingUtilitiesTest, isFirstPositionAfterTable)
     const char* bodyContent = "<div contenteditable id=host><table id=table><tr><td>1</td></tr></table><b id=two>22</b></div>";
     const char* shadowContent = "<content select=#two></content><content select=#table></content>";
     setBodyContent(bodyContent);
-    RefPtrWillBeRawPtr<ShadowRoot> shadowRoot = setShadowContent(shadowContent, "host");
+    setShadowContent(shadowContent, "host");
     updateLayoutAndStyleForPainting();
     Node* host = document().getElementById("host");
     Node* table = document().getElementById("table");
