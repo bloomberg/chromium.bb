b9fd64b0095b8ccffd3a7b158277d849bc9b897b
haraken@chromium.org <haraken@chromium.org>
2015-09-08 10:00:17 +0000

    Oilpan: Re-add a GC_PROFILING macro to GCInfo::className()
    This is a partial revert of r201891
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1315443010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@201906 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: ea29343cc541d72451c0f6eea2977db5dab2a83e

--- third_party/WebKit/Source/platform/heap/GCInfo.h
+++ third_party/WebKit/Source/platform/heap/GCInfo.h
@@ -119,7 +119,7 @@ struct GCInfo {
     FinalizationCallback m_finalize;
     bool m_nonTrivialFinalizer;
     bool m_hasVTable;
-#if ENABLE(DETAILED_MEMORY_INFRA)
+#if ENABLE(GC_PROFILING) || ENABLE(DETAILED_MEMORY_INFRA)
     const String className() const { return m_className(); }
     GetClassNameCallback m_className;
 #endif
@@ -174,7 +174,7 @@ struct GCInfoAtBase {
             FinalizerTrait<T>::finalize,
             FinalizerTrait<T>::nonTrivialFinalizer,
             WTF::IsPolymorphic<T>::value,
-#if ENABLE(DETAILED_MEMORY_INFRA)
+#if ENABLE(GC_PROFILING) || ENABLE(DETAILED_MEMORY_INFRA)
             TypenameStringTrait<T>::get
 #endif
         };
