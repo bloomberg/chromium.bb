f54ff89f67bdafb3813806db512e4763830b3c40
tapted <tapted@chromium.org>
2015-09-09 05:37:42 -0700

    [MacViews] Show comboboxes with a native NSMenu
    The positioning and fancy scrolling behavior of native Combobox popup
    menus on Mac is fiddly to replicate. We already show context menus in
    MacViews with NSMenu, and it's straightforward to adapt combobox menus
    to be shown with NSMenu APIs as well, so long as the menu is backed by a
    ui::MenuModel.
    
    To achieve the positioning with NSMenu APIs, an NSView is required for
    the anchor. For this, insert a temporary, hidden NSButton in the window
    which is removed as soon as the menu is dismissed.
    
    BUG=522365
    
    Review URL: https://codereview.chromium.org/1321023005
    
    Cr-Commit-Position: refs/heads/master@{#347913}
    
    Former-commit-id: 2d2db5f4507e613a181df03dae66f7ae28f24f49

--- ui/views/controls/menu/menu_runner_cocoa_unittest.mm
+++ ui/views/controls/menu/menu_runner_cocoa_unittest.mm
@@ -4,24 +4,63 @@
 
 #import "ui/views/controls/menu/menu_runner_impl_cocoa.h"
 
+#import <Cocoa/Cocoa.h>
+
 #include "base/strings/utf_string_conversions.h"
+#import "testing/gtest_mac.h"
 #include "ui/base/models/simple_menu_model.h"
 #include "ui/events/event_utils.h"
 #include "ui/views/test/views_test_base.h"
 
 namespace views {
 namespace test {
+namespace {
+
+class TestModel : public ui::SimpleMenuModel {
+ public:
+  TestModel() : ui::SimpleMenuModel(&delegate_), delegate_(this) {}
+
+  void set_checked_command(int command) { checked_command_ = command; }
+
+ private:
+  class Delegate : public ui::SimpleMenuModel::Delegate {
+   public:
+    explicit Delegate(TestModel* model) : model_(model) {}
+    bool IsCommandIdChecked(int command_id) const override {
+      return command_id == model_->checked_command_;
+    }
+    bool IsCommandIdEnabled(int command_id) const override { return true; }
+    bool GetAcceleratorForCommandId(int command_id,
+                                    ui::Accelerator* accelerator) override {
+      return false;
+    }
+    void ExecuteCommand(int command_id, int event_flags) override {}
+
+   private:
+    TestModel* model_;
+
+    DISALLOW_COPY_AND_ASSIGN(Delegate);
+  };
+
+ private:
+  int checked_command_ = -1;
+  Delegate delegate_;
+
+  DISALLOW_COPY_AND_ASSIGN(TestModel);
+};
+
+}  // namespace
 
 class MenuRunnerCocoaTest : public ViewsTestBase {
  public:
-  MenuRunnerCocoaTest() : runner_(NULL) {}
+  MenuRunnerCocoaTest() {}
   ~MenuRunnerCocoaTest() override {}
 
   void SetUp() override {
     ViewsTestBase::SetUp();
 
-    menu_.reset(new ui::SimpleMenuModel(NULL));
-    menu_->AddItem(0, base::ASCIIToUTF16("Menu Item"));
+    menu_.reset(new TestModel());
+    menu_->AddCheckItem(0, base::ASCIIToUTF16("Menu Item"));
 
     runner_ = new internal::MenuRunnerImplCocoa(menu_.get());
     EXPECT_FALSE(runner_->IsRunning());
@@ -46,9 +85,33 @@ class MenuRunnerCocoaTest : public ViewsTestBase {
         NULL, NULL, gfx::Rect(), MENU_ANCHOR_TOPLEFT, MenuRunner::CONTEXT_MENU);
   }
 
+  // Runs then cancels a combobox menu and captures the frame of the anchoring
+  // view.
+  MenuRunner::RunResult RunMenuAt(const gfx::Rect& anchor) {
+    last_anchor_frame_ = NSZeroRect;
+
+    // Should be one child (the compositor layer) before showing, and it should
+    // go up by one (the anchor view) while the menu is shown.
+    EXPECT_EQ(1u, [[parent_->GetNativeView() subviews] count]);
+    CFRunLoopPerformBlock(CFRunLoopGetCurrent(), kCFRunLoopCommonModes, ^{
+      NSArray* subviews = [parent_->GetNativeView() subviews];
+      EXPECT_EQ(2u, [subviews count]);
+      last_anchor_frame_ = [[subviews objectAtIndex:1] frame];
+      runner_->Cancel();
+    });
+    MenuRunner::RunResult result = runner_->RunMenuAt(
+        parent_, nullptr, anchor, MENU_ANCHOR_TOPLEFT, MenuRunner::COMBOBOX);
+
+    // Ensure the anchor view is removed.
+    EXPECT_EQ(1u, [[parent_->GetNativeView() subviews] count]);
+    return result;
+  }
+
  protected:
-  scoped_ptr<ui::SimpleMenuModel> menu_;
-  internal::MenuRunnerImplCocoa* runner_;
+  scoped_ptr<TestModel> menu_;
+  internal::MenuRunnerImplCocoa* runner_ = nullptr;
+  views::Widget* parent_ = nullptr;
+  NSRect last_anchor_frame_ = NSZeroRect;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(MenuRunnerCocoaTest);
@@ -101,6 +164,59 @@ TEST_F(MenuRunnerCocoaTest, CancelWithoutRunning) {
 TEST_F(MenuRunnerCocoaTest, DeleteWithoutRunning) {
   runner_->Release();
   runner_ = NULL;
+}
+
+// Tests anchoring of the menus used for toolkit-views Comboboxes.
+TEST_F(MenuRunnerCocoaTest, ComboboxAnchoring) {
+  const int kWindowHeight = 200;
+  const int kWindowOffset = 100;
+
+  parent_ = new views::Widget();
+  parent_->Init(CreateParams(Widget::InitParams::TYPE_WINDOW_FRAMELESS));
+  parent_->SetBounds(
+      gfx::Rect(kWindowOffset, kWindowOffset, 300, kWindowHeight));
+  parent_->Show();
+
+  // Combobox at 20,10 in the Widget.
+  const gfx::Rect combobox_rect(20, 10, 80, 50);
+
+  // Menu anchor rects are always in screen coordinates. The window is frameless
+  // so offset by the bounds.
+  gfx::Rect anchor_rect = combobox_rect;
+  anchor_rect.Offset(kWindowOffset, kWindowOffset);
+  RunMenuAt(anchor_rect);
+
+  // Nothing is checked, so the anchor view should have no height, to ensure the
+  // menu goes below the anchor rect. There should also be no x-offset since the
+  // there is no need to line-up text.
+  EXPECT_NSEQ(
+      NSMakeRect(combobox_rect.x(), kWindowHeight - combobox_rect.bottom(),
+                 combobox_rect.width(), 0),
+      last_anchor_frame_);
+
+  menu_->set_checked_command(0);
+  RunMenuAt(anchor_rect);
+
+  // Native constant used by MenuRunnerImplCocoa.
+  const CGFloat kNativeCheckmarkWidth = 18;
+
+  // There is now a checked item, so the anchor should be vertically centered
+  // inside the combobox, and offset by the width of the checkmark column.
+  EXPECT_EQ(combobox_rect.x() - kNativeCheckmarkWidth,
+            last_anchor_frame_.origin.x);
+  EXPECT_EQ(kWindowHeight - combobox_rect.CenterPoint().y(),
+            NSMidY(last_anchor_frame_));
+  EXPECT_EQ(combobox_rect.width(), NSWidth(last_anchor_frame_));
+  EXPECT_NE(0, NSHeight(last_anchor_frame_));
+
+  // In RTL, Cocoa messes up the positioning unless the anchor rectangle is
+  // offset to the right of the view. The offset for the checkmark is also
+  // skipped, to give a better match to native behavior.
+  base::i18n::SetICUDefaultLocale("he");
+  RunMenuAt(anchor_rect);
+  EXPECT_EQ(combobox_rect.right(), last_anchor_frame_.origin.x);
+
+  parent_->CloseNow();
 }
 
 }  // namespace test
--- ui/views/controls/menu/menu_runner_impl_cocoa.mm
+++ ui/views/controls/menu/menu_runner_impl_cocoa.mm
@@ -5,18 +5,85 @@
 #import "ui/views/controls/menu/menu_runner_impl_cocoa.h"
 
 #import "ui/base/cocoa/menu_controller.h"
+#include "ui/base/models/menu_model.h"
 #include "ui/events/event_utils.h"
 #include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/mac/coordinate_conversion.h"
 #include "ui/views/controls/menu/menu_runner_impl_adapter.h"
+#include "ui/views/widget/widget.h"
 
 namespace views {
 namespace internal {
+namespace {
+
+// The menu run types that should show a native NSMenu rather than a toolkit-
+// views menu. Only supported when the menu is backed by a ui::MenuModel.
+const int kNativeRunTypes = MenuRunner::CONTEXT_MENU | MenuRunner::COMBOBOX;
+
+const CGFloat kNativeCheckmarkWidth = 18;
+const CGFloat kNativeMenuItemHeight = 18;
+
+// Returns the first item in |menu_controller|'s menu that will be checked.
+NSMenuItem* FirstCheckedItem(MenuController* menu_controller) {
+  for (NSMenuItem* item in [[menu_controller menu] itemArray]) {
+    if ([menu_controller model]->IsItemCheckedAt([item tag]))
+      return item;
+  }
+  return nil;
+}
+
+// Places a temporary, hidden NSView at |screen_bounds| within |window|. Used
+// with -[NSMenu popUpMenuPositioningItem:atLocation:inView:] to position the
+// menu for a combobox. The caller must remove the returned NSView from its
+// superview when the menu is closed.
+base::scoped_nsobject<NSView> CreateMenuAnchorView(
+    NSWindow* window,
+    const gfx::Rect& screen_bounds,
+    NSMenuItem* checked_item) {
+  NSRect rect = gfx::ScreenRectToNSRect(screen_bounds);
+  rect.origin = [window convertScreenToBase:rect.origin];
+  rect = [[window contentView] convertRect:rect fromView:nil];
+
+  // If there's no checked item (e.g. Combobox::STYLE_ACTION), NSMenu will
+  // anchor at the top left of the frame. Action buttons should anchor below.
+  if (!checked_item) {
+    rect.size.height = 0;
+    if (base::i18n::IsRTL())
+      rect.origin.x += rect.size.width;
+  } else {
+    // To ensure a consistent anchoring that's vertically centered in the
+    // bounds, fix the height to be the same as a menu item.
+    rect.origin.y = NSMidY(rect) - kNativeMenuItemHeight / 2;
+    rect.size.height = kNativeMenuItemHeight;
+    if (base::i18n::IsRTL()) {
+      // The Views menu controller flips the MenuAnchorPosition value from left
+      // to right in RTL. NSMenu does this automatically: the menu opens to the
+      // left of the anchor, but AppKit doesn't account for the anchor width.
+      // So the width needs to be added to anchor at the right of the view.
+      // Note the checkmark width is not also added - it doesn't quite line up
+      // the text. A Yosemite NSComboBox doesn't line up in RTL either: just
+      // adding the width is a good match for the native behavior.
+      rect.origin.x += rect.size.width;
+    } else {
+      rect.origin.x -= kNativeCheckmarkWidth;
+    }
+  }
+
+  // A plain NSView will anchor below rather than "over", so use an NSButton.
+  base::scoped_nsobject<NSView> anchor_view(
+      [[NSButton alloc] initWithFrame:rect]);
+  [anchor_view setHidden:YES];
+  [[window contentView] addSubview:anchor_view];
+  return anchor_view;
+}
+
+}  // namespace
 
 // static
 MenuRunnerImplInterface* MenuRunnerImplInterface::Create(
     ui::MenuModel* menu_model,
     int32 run_types) {
-  if ((run_types & MenuRunner::CONTEXT_MENU) != 0 &&
+  if ((run_types & kNativeRunTypes) != 0 &&
       (run_types & MenuRunner::IS_NESTED) == 0) {
     return new MenuRunnerImplCocoa(menu_model);
   }
@@ -51,12 +118,29 @@ MenuRunner::RunResult MenuRunnerImplCocoa::RunMenuAt(Widget* parent,
                                                      const gfx::Rect& bounds,
                                                      MenuAnchorPosition anchor,
                                                      int32 run_types) {
-  DCHECK(run_types & MenuRunner::CONTEXT_MENU);
+  DCHECK(run_types & kNativeRunTypes);
   DCHECK(!IsRunning());
   closing_event_time_ = base::TimeDelta();
-  [NSMenu popUpContextMenu:[menu_controller_ menu]
-                 withEvent:[NSApp currentEvent]
-                   forView:nil];
+
+  if (run_types & MenuRunner::CONTEXT_MENU) {
+    [NSMenu popUpContextMenu:[menu_controller_ menu]
+                   withEvent:[NSApp currentEvent]
+                     forView:nil];
+  } else if (run_types & MenuRunner::COMBOBOX) {
+    DCHECK(parent);
+    NSMenuItem* checked_item = FirstCheckedItem(menu_controller_);
+    base::scoped_nsobject<NSView> anchor_view(
+        CreateMenuAnchorView(parent->GetNativeWindow(), bounds, checked_item));
+    NSMenu* menu = [menu_controller_ menu];
+    [menu setMinimumWidth:bounds.width() + kNativeCheckmarkWidth];
+    [menu popUpMenuPositioningItem:checked_item
+                        atLocation:NSZeroPoint
+                            inView:anchor_view];
+    [anchor_view removeFromSuperview];
+  } else {
+    NOTREACHED();
+  }
+
   closing_event_time_ = ui::EventTimeForNow();
 
   if (delete_after_run_) {
