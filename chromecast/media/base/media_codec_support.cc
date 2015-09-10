b06041eaa53a1cd0855302fec91cbfa8a7a095b3
erickung <erickung@chromium.org>
2015-09-09 20:40:55 -0700

    Chromecast: Checks if default codec type is supported
    This is a temporary solution to unblock cast built from chromium
    upstream to check if default codec type is supported if OEM shared
    library implementation returns MediaCodecSupportShlib::kDefault.
    This can be reverted once the code is able to wire to chrome media
    MimeUtil component.
    
    BUB= b/22465408
    
    Review URL: https://codereview.chromium.org/1329333002
    
    Cr-Commit-Position: refs/heads/master@{#348087}
    
    Former-commit-id: bc11fc995f8a467a4246113949323b93e02fb31b

--- chromecast/media/base/media_codec_support.cc
+++ chromecast/media/base/media_codec_support.cc
@@ -5,6 +5,7 @@
 #include "chromecast/media/base/media_codec_support.h"
 
 #include "base/bind.h"
+#include "base/strings/string_util.h"
 #include "chromecast/media/base/media_caps.h"
 #include "chromecast/public/media_codec_support_shlib.h"
 #include "net/base/mime_util.h"
@@ -31,7 +32,26 @@ bool IsCodecSupported(const std::string& codec) {
     return ::media::HdmiSinkSupportsEAC3();
   }
 
-  return true;
+  // TODO(erickung): This is the temporary solution to allow cast working
+  // properly when checking default codec type is supported on not. The change
+  // should be reverted once the function is able to call chrome media function
+  // directly.
+  if (codec == "1" /*PCM*/ || codec == "vorbis" || codec == "opus" ||
+      codec == "theora" || codec == "vp8" || codec == "vp8.0" ||
+      codec == "vp9" || codec == "vp9.0")
+    return true;
+
+  if (codec == "mp3" || codec == "mp4a.66" || codec == "mp4a.67" ||
+      codec == "mp4a.68" || codec == "mp4a.69" || codec == "mp4a.6B" ||
+      codec == "mp4a.40.2" || codec == "mp4a.40.02" || codec == "mp4a.40.29" ||
+      codec == "mp4a.40.5" || codec == "mp4a.40.05" || codec == "mp4a.40")
+    return true;
+
+  if (base::StartsWith(codec, "avc1", base::CompareCase::SENSITIVE) ||
+      base::StartsWith(codec, "avc3", base::CompareCase::SENSITIVE))
+    return true;
+
+  return false;
 }
 
 }  // namespace
