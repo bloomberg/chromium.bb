fdc1b4a4cf3896fd8c523b361171f85dc2ac07c0
dbeam <dbeam@chromium.org>
2015-09-10 14:28:46 -0700

    Detect missing files when doing bower updates
    R=jlklein@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/1337533002
    
    Cr-Commit-Position: refs/heads/master@{#348241}
    
    Former-commit-id: 1765a29fe0cf293471fd9f987fdd6364ff05917c

--- tools/polymer/txt_to_polymer_grdp.py
+++ tools/polymer/txt_to_polymer_grdp.py
@@ -4,6 +4,7 @@
 # found in the LICENSE file.
 
 from __future__ import with_statement
+import os
 import string
 import sys
 
@@ -49,6 +50,7 @@ def PathToGritId(path):
   table = string.maketrans(string.lowercase + '/.-', string.uppercase + '___')
   return 'IDR_POLYMER_1_0_' + path.translate(table)
 
+
 def SortKey(record):
   return (record, PathToGritId(record))
 
@@ -56,11 +58,25 @@ def SortKey(record):
 def ParseRecord(record):
   return record.strip()
 
+
+class FileNotFoundException(Exception):
+  pass
+
+
+_HERE = os.path.dirname(os.path.realpath(__file__))
+_POLYMER_DIR = os.path.join(_HERE, os.pardir, os.pardir,
+    'third_party', 'polymer', 'v1_0', 'components-chromium')
+
+
 def main(argv):
   with open(argv[1]) as f:
     records = [ParseRecord(r) for r in f if not r.isspace()]
   lines = { 'v_1_0': [] }
   for path in sorted(set(records), key=SortKey):
+    full_path = os.path.normpath(os.path.join(_POLYMER_DIR, path))
+    if not os.path.exists(full_path):
+      raise FileNotFoundException('%s not found' % full_path)
+
     template = DEFINITION_TEMPLATE_1_0
     lines['v_1_0'].append(
         template % (PathToGritId(path), path))
