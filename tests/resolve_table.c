26fabec9b74f7e8b141a88bcb70927882815a142
Christian Egli <christian.egli@sbs.ch>
2015-09-11 16:28:40 +0200

    Use the error module from gnulib
    So that check_yaml also compiles on systems that are not based on glibc.
    
    Fixes #124

--- tests/resolve_table.c
+++ tests/resolve_table.c
@@ -7,6 +7,7 @@ are permitted in any medium without royalty provided the copyright
 notice and this notice are preserved. This file is offered as-is,
 without any warranty. */
 
+#include <config.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
