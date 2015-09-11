5989784d516d535e78740be10843dbef13e0aa59
pcc <pcc@chromium.org>
2015-09-11 13:19:15 -0700

    binutils: Embed a relative path to the LLVM lib directory in binaries' rpaths.
    This ensures that the correct version of libstdc++ is loaded by gold
    before it loads the LLVM LTO plugin. See bug for details.
    
    BUG=521143
    R=thestig@chromium.org,thakis@chromium.org,krasin@google.com
    
    Review URL: https://codereview.chromium.org/1330433002
    
    Cr-Commit-Position: refs/heads/master@{#348478}
    
    Former-commit-id: 2b4b72b16d655c497dcc4693d3577bf01b71e992

--- third_party/binutils/build-one.sh
+++ third_party/binutils/build-one.sh
@@ -14,7 +14,31 @@ if [ -z "$1" ]; then
 fi
 
 cd "$1"
+
+# Ask the dynamic loader to load libstdc++ from the LLVM build directory if
+# available. That copy of libstdc++ is required by the gold plugin in the same
+# directory. The dynamic loader expects the relative path to start with $ORIGIN,
+# but because of escaping issues
+# (https://sourceware.org/ml/binutils/2009-05/msg00252.html)
+# we embed a dummy path with $ replaced with z and fix it up later.
+export LDFLAGS='-Wl,-rpath,zORIGIN/../../../../llvm-build/Release+Asserts/lib'
+
 ./configure --enable-gold=default --enable-threads --enable-plugins \
   --prefix=/build/output
 make -j8 all
 make install
+
+# Save the list of binaries. The sed -i command will leave .orig files behind.
+# We don't want them to appear in the for loop below.
+bins="$(echo /build/output/*/bin/*)"
+
+# Fix up zORIGIN -> $ORIGIN.
+sed -i.orig 's,zORIGIN,$ORIGIN,g' $bins
+
+# Verify that we changed only one byte per executable.
+for bin in $bins; do
+  test "`cmp -l $bin.orig $bin | wc -l`" = 1 || \
+    (echo "$bin: verification failed" && exit 1)
+done
+
+rm /build/output/*/bin/*.orig
