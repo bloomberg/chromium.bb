f37267fee6e8a8fdab5155df9b5bd38e82663bf5
wkorman@chromium.org <wkorman@chromium.org>
2015-09-14 19:03:53 +0000

    Revert "Remove obsolete LTR scrollbar padding hack."
    This reverts commit 0e054f07273d9f5104b4b3926dc438fa794436fe.
    
    Causes caret to disappear at edge of input fields in some situations.
    
    BUG=523969,474759
    
    Review URL: https://codereview.chromium.org/1334383002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202221 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2e65d3841eae75678d5438d3c82652d887d73baf

--- third_party/WebKit/Source/core/layout/LayoutBlockFlowLine.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlowLine.cpp
@@ -1841,6 +1841,7 @@ bool LayoutBlockFlow::matchedEndLine(LineLayoutState& layoutState, const InlineB
 }
 
 bool LayoutBlockFlow::generatesLineBoxesForInlineChild(LayoutObject* inlineObj)
+
 {
     ASSERT(inlineObj->parent() == this);
 
@@ -1852,9 +1853,13 @@ bool LayoutBlockFlow::generatesLineBoxesForInlineChild(LayoutObject* inlineObj)
     return !it.atEnd();
 }
 
+
 void LayoutBlockFlow::addOverflowFromInlineChildren()
 {
     LayoutUnit endPadding = hasOverflowClip() ? paddingEnd() : LayoutUnit();
+    // FIXME: Need to find another way to do this, since scrollbars could show when we don't want them to.
+    if (hasOverflowClip() && !endPadding && node() && node()->isRootEditableElement() && style()->isLeftToRightDirection())
+        endPadding = 1;
     for (RootInlineBox* curr = firstRootBox(); curr; curr = curr->nextRootBox()) {
         addLayoutOverflow(curr->paddedLayoutOverflowRect(endPadding));
         LayoutRect visualOverflow = curr->visualOverflowRect(curr->lineTop(), curr->lineBottom());
