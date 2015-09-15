08fe173f4d6e4293ec696155472731f1733fe0c3
chrishtr@chromium.org <chrishtr@chromium.org>
2015-09-15 00:36:42 +0000

    Fix pixel-snapping when painting embedded objects.
    BUG=530647
    
    Review URL: https://codereview.chromium.org/1340673002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202239 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: bef05158380fa18c64ea3d64bc1d78b866fa308f

--- third_party/WebKit/Source/core/paint/EmbeddedObjectPainter.cpp
+++ third_party/WebKit/Source/core/paint/EmbeddedObjectPainter.cpp
@@ -47,12 +47,11 @@ void EmbeddedObjectPainter::paintReplaced(const PaintInfo& paintInfo, const Layo
     if (LayoutObjectDrawingRecorder::useCachedDrawingIfPossible(*context, m_layoutEmbeddedObject, paintInfo.phase, paintOffset))
         return;
 
-    FloatRect contentRect(m_layoutEmbeddedObject.contentBoxRect());
-    contentRect.moveBy(roundedIntPoint(paintOffset));
+    LayoutRect contentRect(m_layoutEmbeddedObject.contentBoxRect());
+    contentRect.moveBy(paintOffset);
     LayoutObjectDrawingRecorder drawingRecorder(*context, m_layoutEmbeddedObject, paintInfo.phase, contentRect, paintOffset);
     GraphicsContextStateSaver stateSaver(*context);
-    // TODO(chrishtr): this should be pixel-snapped.
-    context->clip(contentRect);
+    context->clip(pixelSnappedIntRect(contentRect));
 
     Font font = replacementTextFont();
     // TODO(trchen): Speculative fix for crbug.com/481880
@@ -63,17 +62,19 @@ void EmbeddedObjectPainter::paintReplaced(const PaintInfo& paintInfo, const Layo
     TextRun textRun(m_layoutEmbeddedObject.unavailablePluginReplacementText());
     FloatSize textGeometry(font.width(textRun), font.fontMetrics().height());
 
-    FloatRect backgroundRect(0, 0, textGeometry.width() + 2 * replacementTextRoundedRectLeftRightTextMargin, replacementTextRoundedRectHeight);
+    LayoutRect backgroundRect(0, 0, textGeometry.width() + 2 * replacementTextRoundedRectLeftRightTextMargin, replacementTextRoundedRectHeight);
     backgroundRect.move(contentRect.center() - backgroundRect.center());
+    backgroundRect = LayoutRect(pixelSnappedIntRect(backgroundRect));
     Path roundedBackgroundRect;
-    roundedBackgroundRect.addRoundedRect(backgroundRect, FloatSize(replacementTextRoundedRectRadius, replacementTextRoundedRectRadius));
+    FloatRect floatBackgroundRect(backgroundRect);
+    roundedBackgroundRect.addRoundedRect(floatBackgroundRect, FloatSize(replacementTextRoundedRectRadius, replacementTextRoundedRectRadius));
     context->setFillColor(scaleAlpha(Color::white, replacementTextRoundedRectOpacity));
     context->fillPath(roundedBackgroundRect);
 
     FloatRect textRect(FloatPoint(), textGeometry);
-    textRect.move(contentRect.center() - textRect.center());
+    textRect.move(FloatPoint(contentRect.center()) - textRect.center());
     TextRunPaintInfo runInfo(textRun);
-    runInfo.bounds = backgroundRect;
+    runInfo.bounds = floatBackgroundRect;
     context->setFillColor(scaleAlpha(Color::black, replacementTextTextOpacity));
     context->drawBidiText(font, runInfo, textRect.location() + FloatSize(0, font.fontMetrics().ascent()));
 }
