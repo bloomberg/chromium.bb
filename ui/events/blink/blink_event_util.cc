d14672211092745753166d1ce8da9a61f95b8f1c
jdduke <jdduke@chromium.org>
2015-09-15 10:53:07 -0700

    Allow touch orientations of [-180, 180]
    The Android API documentation dicates that touch values have an
    orientation of [-90, 90]. However, this can be violated when the
    touchscreen is rotated. Account for this by relaxing the allowed
    value range.
    
    BUG=522605
    
    Review URL: https://codereview.chromium.org/1343603005
    
    Cr-Commit-Position: refs/heads/master@{#348928}
    
    Former-commit-id: 099f7aebf6e32174cf6ccbcb0d4d47fdaa4a54f3

--- ui/events/blink/blink_event_util.cc
+++ ui/events/blink/blink_event_util.cc
@@ -129,25 +129,19 @@ WebTouchPoint CreateWebTouchPoint(const MotionEvent& event,
   DCHECK_GE(major_radius, 0);
   DCHECK_GE(minor_radius, 0);
   DCHECK_GE(major_radius, minor_radius);
-  if (event.GetToolType(pointer_index) == MotionEvent::TOOL_TYPE_STYLUS) {
-    // Orientation lies in [-180, 180] for a stylus. Normalise to [-90, 90).
-    // Allow a small bound tolerance to account for floating point conversion.
-    // TODO(e_hakkinen): crbug.com/493728: Pass also unaltered orientation
-    //                   to touch in order not to lose quadrant information.
-    DCHECK_GT(orientation_deg, -180.01f);
-    DCHECK_LT(orientation_deg, 180.01f);
-    if (orientation_deg >= 90.f)
-      orientation_deg -= 180.f;
-    else if (orientation_deg < -90.f)
-      orientation_deg += 180.f;
-  } else {
-    // Orientation lies in [-90, 90] for a touch. Normalise to [-90, 90).
-    // Allow a small bound tolerance to account for floating point conversion.
-    DCHECK_GT(orientation_deg, -90.01f);
-    DCHECK_LT(orientation_deg, 90.01f);
-    if (orientation_deg >= 90.f)
-      orientation_deg -= 180.f;
-  }
+  // Orientation lies in [-180, 180] for a stylus, and [-90, 90] for other
+  // touchscreen inputs. There are exceptions on Android when a device is
+  // rotated, yielding touch orientations in the range of [-180, 180].
+  // Regardless, normalise to [-90, 90), allowing a small tolerance to account
+  // for floating point conversion.
+  // TODO(e_hakkinen): Also pass unaltered stylus orientation, avoiding loss of
+  // quadrant information, see crbug.com/493728.
+  DCHECK_GT(orientation_deg, -180.01f);
+  DCHECK_LT(orientation_deg, 180.01f);
+  if (orientation_deg >= 90.f)
+    orientation_deg -= 180.f;
+  else if (orientation_deg < -90.f)
+    orientation_deg += 180.f;
   if (orientation_deg >= 0) {
     // The case orientation_deg == 0 is handled here on purpose: although the
     // 'else' block is equivalent in this case, we want to pass the 0 value
