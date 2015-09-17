061ac45496fce0808f2b5d6d34d21389c7ae0437
tfarina <tfarina@chromium.org>
2015-09-17 09:49:49 -0700

    forward2: Fix some typos found in socket.h.
    This patch fixes the PF vs AF mistake and updates WaitForEvent
    documentation to be more clear.
    
    BUG=None
    R=wangxianzhu@chromium.org
    
    Review URL: https://codereview.chromium.org/1349993002
    
    Cr-Commit-Position: refs/heads/master@{#349427}
    
    Former-commit-id: 422a0075b8b3f75d558753b959326386f8e4a766

--- tools/android/forwarder2/socket.h
+++ tools/android/forwarder2/socket.h
@@ -122,15 +122,14 @@ class Socket {
   bool InitSocketInternal();
   void SetSocketError();
 
-  // Waits until either the Socket or the |exit_notifier_fd_| has received an
-  // event.
+  // Waits until a read or write i/o event has happened.
   bool WaitForEvent(EventType type, int timeout_secs);
 
   int socket_;
   int port_;
   bool socket_error_;
 
-  // Family of the socket (PF_INET, PF_INET6 or PF_UNIX).
+  // Family of the socket (AF_INET, AF_INET6 or PF_UNIX).
   int family_;
 
   SockAddr addr_;
