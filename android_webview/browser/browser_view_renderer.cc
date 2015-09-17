61abb80b3f110ad29e1a72adbd2e3ab4cb2fa9dc
hush <hush@chromium.org>
2015-09-16 18:57:12 -0700

    Use software draw fallback tick when viewport is empty
    It is possible for WebView size to be empty while getting fallback
    ticks. An example would be typing in a text field in landscape mode
    where the input method window shrinks the WebView height to 0.
    
    Hardware draws with empty damage rect (the WebView viewport) trip a
    bunch of dchecks in Chromium. So use software draws for such fallback
    ticks instead.
    
    BUG=532724
    
    Review URL: https://codereview.chromium.org/1349793005
    
    Cr-Commit-Position: refs/heads/master@{#349316}
    
    Former-commit-id: ad507b26639ddc131a717f537f14b18748a4c150

--- android_webview/browser/browser_view_renderer.cc
+++ android_webview/browser/browser_view_renderer.cc
@@ -685,7 +685,7 @@ void BrowserViewRenderer::FallbackTickFired() {
   fallback_tick_fired_.Cancel();
   fallback_tick_pending_ = false;
   if (compositor_) {
-    if (hardware_enabled_) {
+    if (hardware_enabled_ && !size_.IsEmpty()) {
       CompositeHw();
     } else {
       ForceFakeCompositeSW();
