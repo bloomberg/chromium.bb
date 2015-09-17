eea79c9e1478eb165397bd3304811c131b504575
wangxianzhu@chromium.org <wangxianzhu@chromium.org>
2015-09-17 04:04:56 +0000

    Change output method of paint invalidation failure message
    Previously we remove all contents of the test html and insert message.
    This makes the actual result invisible.
    
    Put a mesage box at the top-right corner to indicate paint invalidation
    objects mismatch. pdr@ contributed the idea and also he svg support
    code.
    
    BUG=524134
    
    Review URL: https://codereview.chromium.org/1340423004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202425 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: baf05ccbd159642e1e98fa08a2800fa6c3b010c2

--- third_party/WebKit/LayoutTests/paint/invalidation/spv2/resources/paint-invalidation-test.js
+++ third_party/WebKit/LayoutTests/paint/invalidation/spv2/resources/paint-invalidation-test.js
@@ -40,22 +40,51 @@ function finishPaintInvalidationTest()
 
     testRunner.layoutAndPaintAsyncThen(function()
     {
-        document.body.offsetTop;
         var paintInvalidationObjects = JSON.stringify(internals.trackedPaintInvalidationObjects());
         var expectedPaintInvalidationObjects = JSON.stringify(window.expectedPaintInvalidationObjects);
         internals.stopTrackingPaintInvalidationObjects();
 
         if (paintInvalidationObjects != expectedPaintInvalidationObjects) {
-            var message = 'Paint invalidation objects mismatch.'
+            testRunner.logToStderr('Paint invalidation objects mismatch.'
                 + '\nExpected:\n' + expectedPaintInvalidationObjects.replace(/","/g, '",\n "')
-                + '\nActual:\n' + paintInvalidationObjects.replace(/","/g, '",\n "');
-            testRunner.logToStderr(message);
-            while (document.body.firstChild)
-                document.body.removeChild(document.body.firstChild);
-            var pre = document.createElement('pre');
-            pre.appendChild(document.createTextNode('(To copy results as text, see stderr.)\n\n' + message));
-            document.body.appendChild(pre);
+                + '\nActual:\n' + paintInvalidationObjects.replace(/","/g, '",\n "'));
+
+            if (document.rootElement && document.rootElement.nodeName == "svg") {
+                var svgns = "http://www.w3.org/2000/svg"
+                var rect = document.createElementNS(svgns, 'rect');
+                rect.setAttribute('width', '45%');
+                rect.setAttribute('height', '10%');
+                rect.setAttribute('x', '55%');
+                rect.setAttribute('fill', 'white');
+                rect.setAttribute('stroke', 'black');
+                rect.setAttribute('stroke-width', '1px');
+                document.documentElement.appendChild(rect);
+                var text = document.createElementNS(svgns, 'text');
+                text.setAttribute('x', '100%');
+                text.setAttribute('y', '5%');
+                text.setAttribute('text-anchor', 'end');
+                text.textContent = "Paint invalidation objects mismatch. See stderr for details.";
+                document.documentElement.appendChild(text);
+            } else {
+                var div = document.createElement('div');
+                div.style.zIndex = '2147483647';
+                div.style.visibility = 'visible';
+                div.style.position = 'fixed';
+                div.style.top = '0';
+                div.style.right = '0';
+                div.style.width = '350px';
+                div.style.height = '16px';
+                div.style.color = 'black';
+                div.style.fontSize = '12px';
+                div.style.lineHeight = '14px';
+                div.style.fontFamily = 'Arial';
+                div.style.border = '1px solid black';
+                div.style.background = 'white';
+                div.appendChild(document.createTextNode('Paint invalidation objects mismatch. See stderr for details.'));
+                document.body.appendChild(div);
+            }
         }
+
         testRunner.notifyDone();
     });
 }
