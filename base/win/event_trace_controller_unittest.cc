3baaa224552696d641c8bdb4ac56faa6f707b36c
brucedawson <brucedawson@chromium.org>
2015-09-18 17:27:02 -0700

    Disable EtwTraceControllerTest.EnableDisable
    This test is hanging on some Windows 7 machines and the fix is not
    obvious. Disabling while I investigate.
    
    R=danakj@chromium.org
    BUG=525297
    
    Review URL: https://codereview.chromium.org/1358583004
    
    Cr-Commit-Position: refs/heads/master@{#349818}
    
    Former-commit-id: 7f847477cdd4f1f297eb5f17dc0fba9e12b00a92

--- base/win/event_trace_controller_unittest.cc
+++ base/win/event_trace_controller_unittest.cc
@@ -179,7 +179,8 @@ TEST_F(EtwTraceControllerTest, StartFileSession) {
   base::DeleteFile(temp, false);
 }
 
-TEST_F(EtwTraceControllerTest, EnableDisable) {
+// Disable until bug 525297 is fixed (hangs on Windows 7?)
+TEST_F(EtwTraceControllerTest, DISABLE_EnableDisable) {
   TestingProvider provider(test_provider_);
 
   EXPECT_EQ(ERROR_SUCCESS, provider.Register());
