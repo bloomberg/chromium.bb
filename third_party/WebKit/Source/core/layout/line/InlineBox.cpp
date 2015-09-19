e913630eb74ee63d3278ccc7a7aa3582347284b9
pdr@chromium.org <pdr@chromium.org>
2015-09-19 04:38:49 +0000

    Skip InlineBox paint invalidation on document destruction
    This patch skips invalidation of InlineBox paint when the document is
    being destroyed. On manual tests measuing the wikipedia page for cats,
    Document::destroy was sped up 25%.
    
    Review URL: https://codereview.chromium.org/1356943006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@202566 bbb929c8-8fbe-4397-9dbb-9b2b20218538
    
    Former-commit-id: 2f80b0fbf0393e1d84787c8151a72f4eb790715c

--- third_party/WebKit/Source/core/layout/line/InlineBox.cpp
+++ third_party/WebKit/Source/core/layout/line/InlineBox.cpp
@@ -61,7 +61,10 @@ InlineBox::~InlineBox()
 
 void InlineBox::destroy()
 {
-    m_layoutObject.invalidateDisplayItemClient(*this);
+    // We do not need to issue invalidations if the page is being destroyed
+    // since these objects will never be repainted.
+    if (!m_layoutObject.documentBeingDestroyed())
+        m_layoutObject.invalidateDisplayItemClient(*this);
     delete this;
 }
 
