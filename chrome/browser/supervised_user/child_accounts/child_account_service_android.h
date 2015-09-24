065b7ad3c7b848c69541df9e95b96352997fb8a0
knn <knn@chromium.org>
2015-09-24 10:23:15 -0700

    ChildAccountService[Java] delegates everything to native side.
    Most of the child account detection/enforcement logic has been divided amongst
    the ChildAccountInfoFetcher and ForcedSigninProcessor.
    
    ForcedSigninProcessor will make sure that child accounts are detected and
    are signed in.
    ChildAccountInfoFetcher only activates on signin and informs the
    native ChildAccountService about the child account and takes responsiblity for
    any further updates (graduation) or account changes.
    
    Simple Reland of http://crrev.com/1341423005
    
    Depends on http://crrev.com/1368663002
    Dependent patchset http://crrev.com/1353393002
    
    TBR=bauerb@chromium.org
    
    BUG=518830
    
    Review URL: https://codereview.chromium.org/1369503002
    
    Cr-Commit-Position: refs/heads/master@{#350591}
    
    Former-commit-id: b79915d32be7c40da7970d7820915d5a9ff9e676

--- chrome/browser/supervised_user/child_accounts/child_account_service_android.h
+++ chrome/browser/supervised_user/child_accounts/child_account_service_android.h
@@ -7,13 +7,7 @@
 
 #include <jni.h>
 
-// Returns whether the Java part has determined the child account status.
-// In this case, |is_child_account| will be set to the value.
-bool GetJavaChildAccountStatus(bool* is_child_account);
-
 // Register native methods.
 bool RegisterChildAccountService(JNIEnv* env);
-
-void ChildStatusInvalidationReceived();
 
 #endif  // CHROME_BROWSER_SUPERVISED_USER_CHILD_ACCOUNTS_CHILD_ACCOUNT_SERVICE_ANDROID_H_
