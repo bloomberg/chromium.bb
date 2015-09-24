b402e9c1b97b68596e478d469a30957caef3b0db
perezju <perezju@chromium.org>
2015-09-24 06:29:08 -0700

    Disable failing: org.chromium.net.QuicTest#testQuicLoadUrl (again)
    Test started failing, probably due to:
    https://codereview.chromium.org/1360063002
    
    BUG=535462
    
    Review URL: https://codereview.chromium.org/1367963002
    
    Cr-Commit-Position: refs/heads/master@{#350543}
    
    Former-commit-id: 8191e96d7166bbb9f3bcdf58656fa641c9e23320

--- components/cronet/android/test/javatests/src/org/chromium/net/QuicTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/QuicTest.java
@@ -4,8 +4,8 @@
 
 package org.chromium.net;
 
+import android.test.suitebuilder.annotation.LargeTest;
 import android.test.suitebuilder.annotation.SmallTest;
-import android.test.suitebuilder.annotation.Suppress;
 
 import org.chromium.base.Log;
 import org.chromium.base.annotations.SuppressFBWarnings;
@@ -70,11 +70,10 @@ public class QuicTest extends CronetTestBase {
         assertEquals("quic/1+spdy/3", listener.mNegotiatedProtocol);
     }
 
-    // Test disabled: http://crbug.com/535462
-    //@LargeTest
-    //@Feature({"Cronet"})
-    @Suppress
-    public void testQuicLoadUrl() throws Exception {
+    // TODO(sclittle): Re-enable test after fixing http://crbug.com/535462
+    @LargeTest
+    @Feature({"Cronet"})
+    public void disable_testQuicLoadUrl() throws Exception {
         String quicURL = QuicTestServer.getServerURL() + "/simple.txt";
         TestUrlRequestListener listener = new TestUrlRequestListener();
 
