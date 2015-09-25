3323059846223c9b6619fe157254ea8f735229af
eranm <eranm@chromium.org>
2015-09-25 05:50:35 -0700

    Certificate Transparency: Require diverse Signed Certificate Timestamps
    Update the certificate policy enforcement code to catch up with the
    requirement of one SCT from a Google-operated log and one from
    non-Google operated log, see discussion in:
    
    https://groups.google.com/a/chromium.org/forum/#!topic/ct-policy/Udh1ddi1MrY
    
    BUG=425174
    
    Review URL: https://codereview.chromium.org/1326803002
    
    Cr-Commit-Position: refs/heads/master@{#350810}
    
    Former-commit-id: 5eccb2ca1d7420c5420646177b2ceb8bf9c2bfa3

--- net/cert/ct_known_logs.h
+++ net/cert/ct_known_logs.h
@@ -8,6 +8,7 @@
 #include <vector>
 
 #include "base/memory/ref_counted.h"
+#include "base/strings/string_piece.h"
 #include "net/base/net_export.h"
 
 namespace net {
@@ -20,6 +21,8 @@ namespace ct {
 // and trusted logs.
 NET_EXPORT std::vector<scoped_refptr<CTLogVerifier>>
 CreateLogVerifiersForKnownLogs();
+
+NET_EXPORT bool IsLogOperatedByGoogle(base::StringPiece log_id);
 
 }  // namespace ct
 
