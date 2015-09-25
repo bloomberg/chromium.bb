e2dc82cbf120d688aa7eb82e22bccd454235bfd7
penghuang <penghuang@chromium.org>
2015-09-25 12:20:08 -0700

    Mandoline: GpuState: Do not crash if GPU info isn't collected successfully.
    In some cases (Windows remote desktop, etc), we cannot get GPU information
    successfully, but mandoline is still functional, so it is not necessary to
    crash.
    
    BUG=525159
    
    Review URL: https://codereview.chromium.org/1365313003
    
    Cr-Commit-Position: refs/heads/master@{#350882}
    
    Former-commit-id: b8a23f2e51c9013c3a37c0e63dab2b3b709a0b8c

--- components/mus/gles2/gpu_state.cc
+++ components/mus/gles2/gpu_state.cc
@@ -7,7 +7,7 @@
 
 #include "base/command_line.h"
 #include "gpu/config/gpu_info_collector.h"
-#include "ui/gl/gl_switches.h"
+#include "ui/gl/gl_implementation.h"
 
 namespace mus {
 
@@ -16,12 +16,20 @@ GpuState::GpuState()
       sync_point_manager_(new gpu::SyncPointManager(true)),
       share_group_(new gfx::GLShareGroup),
       mailbox_manager_(new gpu::gles2::MailboxManagerImpl) {
-  base::CommandLine* cmd = base::CommandLine::ForCurrentProcess();
-  if (!cmd->HasSwitch(switches::kOverrideUseGLWithOSMesaForTests)) {
+  // TODO(penghuang): investigate why gpu::CollectBasicGraphicsInfo() failed on
+  // windows remote desktop.
+  const gfx::GLImplementation impl = gfx::GetGLImplementation();
+  if (impl != gfx::kGLImplementationNone &&
+      impl != gfx::kGLImplementationOSMesaGL &&
+      impl != gfx::kGLImplementationMockGL) {
     gpu::CollectInfoResult result = gpu::CollectBasicGraphicsInfo(&gpu_info_);
-    CHECK(result == gpu::kCollectInfoSuccess);
-    result = gpu::CollectContextGraphicsInfo(&gpu_info_);
-    CHECK(result == gpu::kCollectInfoSuccess);
+    LOG_IF(ERROR, result != gpu::kCollectInfoSuccess)
+        << "Collect basic graphics info failed!";
+  }
+  if (impl != gfx::kGLImplementationNone) {
+    gpu::CollectInfoResult result = gpu::CollectContextGraphicsInfo(&gpu_info_);
+    LOG_IF(ERROR, result != gpu::kCollectInfoSuccess)
+        << "Collect context graphics info failed!";
   }
   control_thread_.Start();
 }
