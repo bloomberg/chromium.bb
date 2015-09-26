913e8ecd8a9742efb4416fcabc868f2bc73edfda
dgozman <dgozman@chromium.org>
2015-09-25 19:39:11 -0700

    [DevTools] Do not use AboutToNavigateRenderFrame in DevToolsUIBindings.
    DidStartProvisionalLoadForFrame is enough here.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1360143003
    
    Cr-Commit-Position: refs/heads/master@{#350974}
    
    Former-commit-id: ee6eca69ffbd1d510d02397df5047f27cc256e8e

--- chrome/browser/devtools/devtools_ui_bindings.cc
+++ chrome/browser/devtools/devtools_ui_bindings.cc
@@ -300,11 +300,11 @@ class DevToolsUIBindings::FrontendWebContentsObserver
  private:
   // contents::WebContentsObserver:
   void RenderProcessGone(base::TerminationStatus status) override;
-  // TODO(creis): Replace with RenderFrameCreated when http://crbug.com/425397
-  // is fixed.  See also http://crbug.com/424641.
-  void AboutToNavigateRenderFrame(
-      content::RenderFrameHost* old_host,
-      content::RenderFrameHost* new_host) override;
+  void DidStartProvisionalLoadForFrame(
+      content::RenderFrameHost* render_frame_host,
+      const GURL& validated_url,
+      bool is_error_page,
+      bool is_iframe_srcdoc) override;
   void DocumentOnLoadCompletedInMainFrame() override;
   void DidNavigateMainFrame(
       const content::LoadCommittedDetails& details,
@@ -346,12 +346,14 @@ void DevToolsUIBindings::FrontendWebContentsObserver::RenderProcessGone(
 }
 
 void DevToolsUIBindings::FrontendWebContentsObserver::
-    AboutToNavigateRenderFrame(content::RenderFrameHost* old_host,
-                               content::RenderFrameHost* new_host) {
-  if (new_host->GetParent())
+    DidStartProvisionalLoadForFrame(content::RenderFrameHost* render_frame_host,
+                                    const GURL& validated_url,
+                                    bool is_error_page,
+                                    bool is_iframe_srcdoc) {
+  if (render_frame_host->GetParent())
     return;
   devtools_bindings_->frontend_host_.reset(
-      content::DevToolsFrontendHost::Create(new_host,
+      content::DevToolsFrontendHost::Create(render_frame_host,
                                             devtools_bindings_));
 }
 
