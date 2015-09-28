598535652be6a5a397090574edb7e37e16e532b5
xlai <xlai@chromium.org>
2015-09-28 12:40:26 -0700

    Make the canvas in race layout test smaller
    BUG=536293
    
    Review URL: https://codereview.chromium.org/1375573002
    
    Cr-Commit-Position: refs/heads/master@{#351121}
    
    Former-commit-id: db8e0663bba7dc9b484324a2d527e047fa5493ee

--- third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-toBlob-toDataURL-race.js
+++ third_party/WebKit/LayoutTests/fast/canvas/script-tests/canvas-toBlob-toDataURL-race.js
@@ -12,16 +12,16 @@ var canvasCtxs = [];
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#EE21AF";
-ctx.fillRect(0, 0, 2500, 1750);
+ctx.fillRect(0, 0, 250, 150);
 
 function testIfAllImagesAreCorrect()
 {
     // All resultant images should be the same as both async and main threads use the same image encoder
     var imageMatched = true;
-    var firstImageData = canvasCtxs[0].getImageData(0, 0, 2500, 1750).data;
+    var firstImageData = canvasCtxs[0].getImageData(0, 0, 250, 150).data;
     for (var i = 1; i < (numToBlobCalls + numToDataURLCalls); i++) 
     {
-        var nextImageData = canvasCtxs[i].getImageData(0, 0, 2500, 1750).data;
+        var nextImageData = canvasCtxs[i].getImageData(0, 0, 250, 150).data;
         for (var k = 0; k < firstImageData.length; k++) 
         {
             if (firstImageData[k]!=nextImageData[k]) 
@@ -58,7 +58,7 @@ function createTestCase(i)
 
     var newImg = new Image();
     newImg.onload = function() {
-        ctx_test.drawImage(newImg, 0, 0, 2500, 1750);
+        ctx_test.drawImage(newImg, 0, 0, 250, 150);
         onCanvasDrawCompleted(ctx_test);
     }    
     testImages[i] = newImg;
