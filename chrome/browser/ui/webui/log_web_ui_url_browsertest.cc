667a7db4b441848690714f43adc64c42d02c7285
dbeam <dbeam@chromium.org>
2015-09-28 19:27:08 -0700

    Add an uber page browser test for WebUI URL logging.
    BUG=535059,385352
    R=creis@chromium.org
    
    Review URL: https://codereview.chromium.org/1374553002
    
    Cr-Commit-Position: refs/heads/master@{#351241}
    
    Former-commit-id: 1f3b25c8d78be73787d4d4c577d2f5e9f826ab46

--- chrome/browser/ui/webui/log_web_ui_url_browsertest.cc
+++ chrome/browser/ui/webui/log_web_ui_url_browsertest.cc
@@ -10,10 +10,15 @@
 #include "base/test/histogram_tester.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
+#include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
+#include "content/public/common/url_constants.h"
+#include "content/public/test/browser_test_utils.h"
+#include "grit/generated_resources.h"
 #include "testing/gmock/include/gmock/gmock.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
 using base::Bucket;
@@ -49,6 +54,56 @@ IN_PROC_BROWSER_TEST_F(LogWebUIUrlTest, TestHistoryFrame) {
   EXPECT_THAT(GetSamples(), ElementsAre(Bucket(history_frame_url_hash, 2)));
 }
 
-// TODO(dbeam): test an uber page URL like chrome://history.
+#if defined(ENABLE_EXTENSIONS)
+IN_PROC_BROWSER_TEST_F(LogWebUIUrlTest, TestUberPage) {
+  content::WebContents* tab =
+      browser()->tab_strip_model()->GetActiveWebContents();
+
+  base::string16 history_title = l10n_util::GetStringUTF16(IDS_HISTORY_TITLE);
+
+  {
+    content::TitleWatcher title_watcher(tab, history_title);
+    ui_test_utils::NavigateToURL(browser(), GURL(chrome::kChromeUIHistoryURL));
+    ASSERT_EQ(history_title, title_watcher.WaitAndGetTitle());
+  }
+
+  std::string scheme(content::kChromeUIScheme);
+  GURL uber_url(scheme + "://" + chrome::kChromeUIUberHost);
+  uint32 uber_url_hash = base::Hash(uber_url.spec());
+
+  GURL history_frame_url(chrome::kChromeUIHistoryFrameURL);
+  uint32 history_frame_url_hash = base::Hash(history_frame_url.spec());
+
+  EXPECT_THAT(GetSamples(), ElementsAre(Bucket(history_frame_url_hash, 1),
+                                        Bucket(uber_url_hash, 1)));
+
+  {
+    content::TitleWatcher title_watcher(tab, history_title);
+    chrome::Reload(browser(), CURRENT_TAB);
+    ASSERT_EQ(history_title, title_watcher.WaitAndGetTitle());
+  }
+
+  EXPECT_THAT(GetSamples(), ElementsAre(Bucket(history_frame_url_hash, 2),
+                                        Bucket(uber_url_hash, 2)));
+
+  {
+    // Pretend a user clicked on "Extensions".
+    base::string16 extensions_title =
+        l10n_util::GetStringUTF16(IDS_MANAGE_EXTENSIONS_SETTING_WINDOWS_TITLE);
+    content::TitleWatcher title_watcher(tab, extensions_title);
+    std::string javascript =
+        "uber.invokeMethodOnWindow(window, 'showPage', {pageId: 'extensions'})";
+    ASSERT_TRUE(content::ExecuteScript(tab, javascript));
+    ASSERT_EQ(extensions_title, title_watcher.WaitAndGetTitle());
+  }
+
+  GURL extensions_frame_url(chrome::kChromeUIExtensionsFrameURL);
+  uint32 extensions_frame_url_hash = base::Hash(extensions_frame_url.spec());
+
+  EXPECT_THAT(GetSamples(), ElementsAre(Bucket(extensions_frame_url_hash, 1),
+                                        Bucket(history_frame_url_hash, 2),
+                                        Bucket(uber_url_hash, 2)));
+}
+#endif
 
 }  // namespace webui
