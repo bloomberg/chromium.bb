a3377cd1149bb0372f71b279e8bb1cb8427063dd
Lucas Tanure <tanure@linux.com>
2015-09-30 09:38:37 -0300

    ivi-layout-transition: Standardize the check for layout_transition_register
    Check the return from layout_transition_register in order to fix
    potential leak of memory pointed to by transition. And don't register a
    null transition.
    
    Signed-off-by: Lucas Tanure <tanure@linux.com>
    Reviewed-by: Bryce Harrington <bryce@osg.samsung.com>

--- ivi-shell/ivi-layout-transition.c
+++ ivi-shell/ivi-layout-transition.c
@@ -418,7 +418,9 @@ ivi_layout_transition_move_resize_view(struct ivi_layout_surface *surface,
 		transition_move_resize_view_destroy,
 		duration);
 
-	layout_transition_register(transition);
+	if(transition && layout_transition_register(transition))
+		return;
+	layout_transition_destroy(transition);
 }
 
 /* fade transition */
@@ -512,8 +514,9 @@ create_visibility_transition(struct ivi_layout_surface *surface,
 		destroy_func,
 		duration);
 
-	if (!layout_transition_register(transition))
-		layout_transition_destroy(transition);
+	if (transition && layout_transition_register(transition))
+		return;
+	layout_transition_destroy(transition);
 }
 
 static void
