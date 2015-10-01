0161550fb4ea7eafe626726c3e99ef20b35bc5c1
blundell <blundell@chromium.org>
2015-09-30 23:30:17 -0700

    Refactor internals of IsMetricsAndCrashReportingEnabled()
    This CL refactors the internals of
    ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled() in order to
    prepare for an upcoming move of most of the logic of that method into
    MetricsServiceAccessor. Most of the complexity is to smoothly handle the fact
    that on Android and ChromeOS, the determination of whether the user has enabled
    metrics reporting does not come from the kMetricsReportingEnabled pref. To
    handle that, this CL decomposes
    ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled() as follows:
    
    1. Adds an IsMetricsReportingEnabledWithPrefValue() function, which takes in
       the pref value as a bool and returns whether metrics reporting is enabled.
       This method has the actual logic to compute whether metrics reporting is
       enabled: on an unofficial build, metrics reporting is always disabled; on an
       official build, metrics reporting is disabled if field trials are forced and
       otherwise is enabled iff the pref value is true.
    2. Adds an IsMetricsReportingEnabled() function, which takes in a PrefService
       and calls IsMetricsReportingEnabledWithPrefValue(), passing the value of
       kMetricsReportingEnabled in that PrefService. This function is for usage
       on all platforms other than Android and ChromeOS.
    3. Changes IsMetricsAndCrashReportingEnabled() to simply call the appropriate
       one of the two functions above on a per-platform basis, passing the
       appropriate value for the parameter.
    
    This structure enables the methods in (1) and (2) to be componentized as-is
    into metrics::MetricsServiceAccessor in a followup CL.  It also provides a
    smooth migration path to the upcoming world where all platforms use the
    kMetricsReportingEnabled pref:
    metrics::MetricsServiceAccessor::IsMetricsReportingEnabledWithPrefValue() can
    be consolidated into
    metrics::MetricsServiceAccessor::IsMetricsReportingEnabled(), with
    ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled() becoming a
    straight pass-through to
    metrics::MetricsServiceAccessor::IsMetricsReportingEnabled() on all platforms.
    
    BUG=508014
    
    Review URL: https://codereview.chromium.org/1367183003
    
    Cr-Commit-Position: refs/heads/master@{#351737}
    
    Former-commit-id: 916626e838f929974e623485b39b16d4788d32c9

--- chrome/browser/metrics/chrome_metrics_service_accessor.cc
+++ chrome/browser/metrics/chrome_metrics_service_accessor.cc
@@ -4,13 +4,12 @@
 
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 
+#include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/prefs/pref_service.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/metrics/metrics_services_manager.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
-#include "components/metrics/metrics_service.h"
+#include "components/metrics/metrics_pref_names.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/settings/cros_settings.h"
@@ -18,33 +17,54 @@
 
 // static
 bool ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled() {
-  // If the user permits metrics reporting with the checkbox in the
-  // prefs, we turn on recording.  We disable metrics completely for
-  // non-official builds, or when field trials are forced.
-  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kForceFieldTrials)) {
-    return false;
-  }
+#if !defined(OS_CHROMEOS) && !defined(OS_ANDROID)
+  return IsMetricsReportingEnabled(g_browser_process->local_state());
+#else
+  // ChromeOS and Android currently obtain the value for whether the user has
+  // obtain metrics reporting in non-standard ways.
+  // TODO(gayane): Consolidate metric prefs on all platforms and eliminate this
+  // special-case code, instead having all platforms go through the above flow.
+  // http://crbug.com/362192, http://crbug.com/532084
+  bool pref_value = false;
 
-  bool enabled = false;
-#if defined(GOOGLE_CHROME_BUILD)
 #if defined(OS_CHROMEOS)
-  // TODO(gayane): The check is added as a temporary fix for unittests. It's not
-  // expected to happen from production code. This should be cleaned up soon
-  // when metrics pref from cros will be eliminated.
+  // TODO(gayane): The check is added as a temporary fix for unittests. It's
+  // not expected to happen from production code. This should be cleaned up
+  // soon when metrics pref from cros will be eliminated.
   if (chromeos::CrosSettings::IsInitialized()) {
     chromeos::CrosSettings::Get()->GetBoolean(chromeos::kStatsReportingPref,
-                                              &enabled);
+                                              &pref_value);
   }
-#elif defined(OS_ANDROID)
-  enabled = g_browser_process->local_state()->GetBoolean(
+#else  // Android
+  pref_value = g_browser_process->local_state()->GetBoolean(
       prefs::kCrashReportingEnabled);
+#endif  // defined(OS_CHROMEOS)
+
+  return IsMetricsReportingEnabledWithPrefValue(pref_value);
+#endif  // !defined(OS_CHROMEOS) && !defined(OS_ANDROID)
+}
+
+// static
+bool ChromeMetricsServiceAccessor::IsMetricsReportingEnabled(
+    PrefService* pref_service) {
+  return IsMetricsReportingEnabledWithPrefValue(
+      pref_service->GetBoolean(metrics::prefs::kMetricsReportingEnabled));
+}
+
+// static
+bool ChromeMetricsServiceAccessor::IsMetricsReportingEnabledWithPrefValue(
+    bool enabled_in_prefs) {
+#if defined(GOOGLE_CHROME_BUILD)
+  // In official builds, disable metrics when reporting field trials are
+  // forced; otherwise, use the value of the user's preference to determine
+  // whether to enable metrics reporting.
+  return !base::CommandLine::ForCurrentProcess()->HasSwitch(
+             switches::kForceFieldTrials) &&
+         enabled_in_prefs;
 #else
-  enabled = g_browser_process->local_state()->
-      GetBoolean(metrics::prefs::kMetricsReportingEnabled);
-#endif  // #if defined(OS_CHROMEOS)
+  // In non-official builds, disable metrics reporting completely.
+  return false;
 #endif  // defined(GOOGLE_CHROME_BUILD)
-  return enabled;
 }
 
 // static
--- chrome/browser/metrics/chrome_metrics_service_accessor_unittest.cc
+++ chrome/browser/metrics/chrome_metrics_service_accessor_unittest.cc
@@ -4,6 +4,8 @@
 
 #include "chrome/browser/metrics/chrome_metrics_service_accessor.h"
 
+#include "base/base_switches.h"
+#include "base/command_line.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/scoped_testing_local_state.h"
 #include "chrome/test/base/testing_browser_process.h"
@@ -42,6 +44,16 @@ TEST_F(ChromeMetricsServiceAccessorTest, MetricsReportingEnabled) {
   EXPECT_TRUE(
       ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled());
   GetLocalState()->ClearPref(pref);
+  EXPECT_FALSE(
+      ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled());
+
+  // If field trials are forced, metrics should always be disabled, regardless
+  // of the value of the pref.
+  base::CommandLine::ForCurrentProcess()->AppendSwitch(
+      switches::kForceFieldTrials);
+  EXPECT_FALSE(
+      ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled());
+  GetLocalState()->SetBoolean(pref, true);
   EXPECT_FALSE(
       ChromeMetricsServiceAccessor::IsMetricsAndCrashReportingEnabled());
 #endif  // !defined(OS_CHROMEOS)
