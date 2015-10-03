9383f78a4dd2f0e1110c8ff64ed4dcaa32d64f99
erikchen <erikchen@chromium.org>
2015-10-02 23:18:26 -0700

    Telemetry: Reenable session restore tests on Linux, XP, and reference builds.
    All of the cited bugs have been marked as fixed, some from over a year ago. If
    this CL causes any problems, the relevant bots should be disabled, and a new bug
    filed.
    
    BUG=
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1383513008
    
    Cr-Commit-Position: refs/heads/master@{#352245}
    
    Former-commit-id: 742fe833a300ea64f97b4ea8341f8181407522dc

--- tools/perf/benchmarks/session_restore.py
+++ tools/perf/benchmarks/session_restore.py
@@ -61,8 +61,7 @@ class _SessionRestoreTypical25(perf_benchmark.PerfBenchmark):
     is_cold = (self.tag == 'cold')
     return session_restore.SessionRestore(cold=is_cold)
 
-# crbug.com/325479, crbug.com/381990, crbug.com/511273
-@benchmark.Disabled('android', 'linux', 'reference')
+@benchmark.Disabled('android')
 class SessionRestoreColdTypical25(_SessionRestoreTypical25):
   """Test by clearing system cache and profile before repeats."""
   tag = 'cold'
@@ -73,8 +72,7 @@ class SessionRestoreColdTypical25(_SessionRestoreTypical25):
     return 'session_restore.cold.typical_25'
 
 
-# crbug.com/325479, crbug.com/381990, crbug.com/511273
-@benchmark.Disabled('android', 'linux', 'reference', 'xp')
+@benchmark.Disabled('android')
 class SessionRestoreWarmTypical25(_SessionRestoreTypical25):
   """Test without clearing system cache or profile before repeats.
 
