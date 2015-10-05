c5383be93ef2933d016c7f74ed5377d4ab3e12d6
fbarchard <fbarchard@google.com>
2015-10-05 16:50:19 -0700

    Remove unused ConvertNV21ToYUV function. libyuv:NV21ToI420 is replacement if needed, optimized for Arm and Intel.
    R=xhwang@chromium.org
    BUG=libyuv:73
    
    Review URL: https://codereview.chromium.org/1389753003
    
    Cr-Commit-Position: refs/heads/master@{#352467}
    
    Former-commit-id: 442f96156e339538169148ce40cde81e75ea2255

--- media/base/yuv_convert.h
+++ media/base/yuv_convert.h
@@ -149,13 +149,6 @@ MEDIA_EXPORT void ConvertYUY2ToYUV(const uint8* src,
                                    int width,
                                    int height);
 
-MEDIA_EXPORT void ConvertNV21ToYUV(const uint8* src,
-                                   uint8* yplane,
-                                   uint8* uplane,
-                                   uint8* vplane,
-                                   int width,
-                                   int height);
-
 // Empty SIMD register state after calling optimized scaler functions.
 MEDIA_EXPORT void EmptyRegisterState();
 
