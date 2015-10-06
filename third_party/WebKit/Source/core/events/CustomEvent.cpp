dce435370f0c511af5121313058c0475a8afa4a5
bashi <bashi@chromium.org>
2015-10-05 17:41:25 -0700

    Reland: Store |detail| as a hidden value of custom event wrappers
    The original CL was reverted because it broke google image search.
    This CL stores |detail| as a hidden value when initCustomEvent() is called
    instead of serializing it.
    
    Original description:
    
    We cannot hold strong references from Blink to V8. This means
    that we shouldn't use ScriptValue in DOM impl objects.
    To stop using ScriptValue in CustomEvent, store |detail|
    as a hidden value of custom event wrappers and return it when
    the getter of |detail| is called.
    
    BUG=529941
    
    Review URL: https://codereview.chromium.org/1383543004
    
    Cr-Commit-Position: refs/heads/master@{#352490}
    
    Former-commit-id: ce8a2dfe58ba9c8a9fe999124a5a7eaabf1e0851

--- third_party/WebKit/Source/core/events/CustomEvent.cpp
+++ third_party/WebKit/Source/core/events/CustomEvent.cpp
@@ -27,6 +27,7 @@
 #include "core/events/CustomEvent.h"
 
 #include "bindings/core/v8/SerializedScriptValue.h"
+#include "bindings/core/v8/SerializedScriptValueFactory.h"
 
 namespace blink {
 
@@ -37,18 +38,15 @@ CustomEvent::CustomEvent()
 CustomEvent::CustomEvent(const AtomicString& type, const CustomEventInit& initializer)
     : Event(type, initializer)
 {
-    if (initializer.hasDetail())
-        m_detail = initializer.detail();
 }
 
 CustomEvent::~CustomEvent()
 {
 }
 
-void CustomEvent::initCustomEvent(const AtomicString& type, bool canBubble, bool cancelable, const ScriptValue& detail)
+void CustomEvent::initCustomEvent(const AtomicString& type, bool canBubble, bool cancelable, const ScriptValue&)
 {
     initEvent(type, canBubble, cancelable);
-    m_detail = detail;
 }
 
 void CustomEvent::initCustomEvent(const AtomicString& type, bool canBubble, bool cancelable, PassRefPtr<SerializedScriptValue> serializedDetail)
