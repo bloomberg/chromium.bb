d2a5989be987427e0d3d89418dabe1f98697c067
dschuyler <dschuyler@chromium.org>
2015-10-08 16:49:03 -0700

    [MD settings] adding load time data behavior for i18n strings
    This is a step toward adding this behavior to CL 1388353003.
    
    BUG=425627
    
    Review URL: https://codereview.chromium.org/1391333005
    
    Cr-Commit-Position: refs/heads/master@{#353174}
    
    Former-commit-id: 2aee820e2030bb54441910e3dc668a44598c5417

---
+++ chrome/browser/resources/settings/i18n_behavior/i18n_behavior.js
@@ -0,0 +1,27 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'I18nBehavior' is a behavior to mix in loading of
+ * internationalization strings.
+ *
+ * Example:
+ *   behaviors: [
+ *     I18nBehavior,
+ *   ],
+ *
+ * @group Chrome Settings Elements
+ */
+
+var I18nBehavior = {
+  /**
+   * @param {string} id The ID of the string to translate.
+   * @param {...var_args} Placeholders required by the string ($1-9).
+   * @return {string} A translated, substituted string.
+   */
+  i18n: function(id, var_args) {
+    return loadTimeData.getStringF.apply(loadTimeData, arguments);
+  },
+};
