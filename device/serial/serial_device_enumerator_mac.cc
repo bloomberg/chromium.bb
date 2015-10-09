8ed911573c9715f5431f6e8963eceb2ce01901f6
charliea <charliea@chromium.org>
2015-10-08 19:13:19 -0700

    Revert "Extracts more information from serial devices on Mac"
    This reverts commit 74e071a71472c96e432e5d703f5c278c3a0f5bc3 [formerly d1540f60edad27a6599b000e80413dce5f5b224a].
    
    A similar CL to the one being reverted has caused a regression on
    Windows where Arduino serial devices that were showing up before are no
    longer showing up. I think that it makes sense to take a more
    reasoned approach here and use a combination of the old method
    and the new method until we're sure that the new method of
    enumerating serial devices is strictly better than the old.
    (We can verify this using UMA metrics.) For now, though, we'll
    just want to eliminate any chance of regression.
    
    This CL will also need need to be cherrypicked onto current
    release branches.
    
    BUG=540889
    
    Review URL: https://codereview.chromium.org/1398923002
    
    Cr-Commit-Position: refs/heads/master@{#353213}
    
    Former-commit-id: 2251715f036cd70ab4c24f3cfb180c0c75f61726

--- device/serial/serial_device_enumerator_mac.cc
+++ device/serial/serial_device_enumerator_mac.cc
@@ -4,90 +4,14 @@
 
 #include "device/serial/serial_device_enumerator_mac.h"
 
-#include <IOKit/serial/IOSerialKeys.h>
-#include <IOKit/usb/IOUSBLib.h>
-
 #include "base/files/file_enumerator.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
-#include "base/mac/scoped_cftyperef.h"
-#include "base/mac/scoped_ioobject.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/strings/pattern.h"
 #include "base/strings/string_util.h"
-#include "base/strings/sys_string_conversions.h"
 
 namespace device {
-
-namespace {
-
-// Searches a service and all ancestor services for a property with the
-// specified key, returning NULL if no such key was found.
-CFTypeRef GetCFProperty(io_service_t service, const CFStringRef key) {
-  // We search for the specified property not only on the specified service, but
-  // all ancestors of that service. This is important because if a device is
-  // both serial and USB, in the registry tree it appears as a serial service
-  // with a USB service as its ancestor. Without searching ancestors services
-  // for the specified property, we'd miss all USB properties.
-  return IORegistryEntrySearchCFProperty(
-      service, kIOServicePlane, key, NULL,
-      kIORegistryIterateRecursively | kIORegistryIterateParents);
-}
-
-// Searches a service and all ancestor services for a string property with the
-// specified key, returning NULL if no such key was found.
-CFStringRef GetCFStringProperty(io_service_t service, const CFStringRef key) {
-  CFTypeRef value = GetCFProperty(service, key);
-  if (value && (CFGetTypeID(value) == CFStringGetTypeID()))
-    return static_cast<CFStringRef>(value);
-
-  return NULL;
-}
-
-// Searches a service and all ancestor services for a number property with the
-// specified key, returning NULL if no such key was found.
-CFNumberRef GetCFNumberProperty(io_service_t service, const CFStringRef key) {
-  CFTypeRef value = GetCFProperty(service, key);
-  if (value && (CFGetTypeID(value) == CFNumberGetTypeID()))
-    return static_cast<CFNumberRef>(value);
-
-  return NULL;
-}
-
-// Searches the specified service for a string property with the specified key,
-// sets value to that property's value, and returns whether the operation was
-// successful.
-bool GetStringProperty(io_service_t service,
-                       const CFStringRef key,
-                       mojo::String* value) {
-  CFStringRef propValue = GetCFStringProperty(service, key);
-  if (propValue) {
-    *value = base::SysCFStringRefToUTF8(propValue);
-    return value;
-  }
-
-  return false;
-}
-
-// Searches the specified service for a uint16 property with the specified key,
-// sets value to that property's value, and returns whether the operation was
-// successful.
-bool GetUInt16Property(io_service_t service,
-                       const CFStringRef key,
-                       uint16_t* value) {
-  CFNumberRef propValue = GetCFNumberProperty(service, key);
-  if (propValue) {
-    int intValue;
-    if (CFNumberGetValue(propValue, kCFNumberIntType, &intValue)) {
-      *value = static_cast<uint16_t>(intValue);
-      return true;
-    }
-  }
-
-  return false;
-}
-
-}  // namespace
 
 // static
 scoped_ptr<SerialDeviceEnumerator> SerialDeviceEnumerator::Create() {
@@ -98,66 +22,39 @@ SerialDeviceEnumeratorMac::SerialDeviceEnumeratorMac() {}
 
 SerialDeviceEnumeratorMac::~SerialDeviceEnumeratorMac() {}
 
+// TODO(rockot): Use IOKit to enumerate serial interfaces.
 mojo::Array<serial::DeviceInfoPtr> SerialDeviceEnumeratorMac::GetDevices() {
+  const base::FilePath kDevRoot("/dev");
+  const int kFilesAndSymLinks =
+      base::FileEnumerator::FILES | base::FileEnumerator::SHOW_SYM_LINKS;
+
+  std::set<std::string> valid_patterns;
+  valid_patterns.insert("/dev/*Bluetooth*");
+  valid_patterns.insert("/dev/*Modem*");
+  valid_patterns.insert("/dev/*bluetooth*");
+  valid_patterns.insert("/dev/*modem*");
+  valid_patterns.insert("/dev/*serial*");
+  valid_patterns.insert("/dev/tty.*");
+  valid_patterns.insert("/dev/cu.*");
+
   mojo::Array<serial::DeviceInfoPtr> devices(0);
+  base::FileEnumerator enumerator(kDevRoot, false, kFilesAndSymLinks);
+  do {
+    const base::FilePath next_device_path(enumerator.Next());
+    const std::string next_device = next_device_path.value();
+    if (next_device.empty())
+      break;
 
-  // Make a service query to find all serial devices.
-  CFMutableDictionaryRef matchingDict =
-      IOServiceMatching(kIOSerialBSDServiceValue);
-  if (!matchingDict)
-    return devices.Pass();
-
-  io_iterator_t it;
-  kern_return_t kr =
-      IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDict, &it);
-  if (kr != KERN_SUCCESS)
-    return devices.Pass();
-
-  base::mac::ScopedIOObject<io_iterator_t> scoped_it(it);
-  base::mac::ScopedIOObject<io_service_t> scoped_device;
-  while (scoped_device.reset(IOIteratorNext(scoped_it.get())), scoped_device) {
-    serial::DeviceInfoPtr callout_info(serial::DeviceInfo::New());
-
-    uint16_t vendorId;
-    if (GetUInt16Property(scoped_device.get(), CFSTR(kUSBVendorID),
-                          &vendorId)) {
-      callout_info->has_vendor_id = true;
-      callout_info->vendor_id = vendorId;
+    std::set<std::string>::const_iterator i = valid_patterns.begin();
+    for (; i != valid_patterns.end(); ++i) {
+      if (base::MatchPattern(next_device, *i)) {
+        serial::DeviceInfoPtr info(serial::DeviceInfo::New());
+        info->path = next_device;
+        devices.push_back(info.Pass());
+        break;
+      }
     }
-
-    uint16_t productId;
-    if (GetUInt16Property(scoped_device.get(), CFSTR(kUSBProductID),
-                          &productId)) {
-      callout_info->has_product_id = true;
-      callout_info->product_id = productId;
-    }
-
-    mojo::String displayName;
-    if (GetStringProperty(scoped_device.get(), CFSTR(kUSBProductString),
-                          &displayName)) {
-      callout_info->display_name = displayName;
-    }
-
-    // Each serial device has two "paths" in /dev/ associated with it: a
-    // "dialin" path starting with "tty" and a "callout" path starting with
-    // "cu". Each of these is considered a different device from Chrome's
-    // standpoint, but both should share the device's USB properties.
-    mojo::String dialinDevice;
-    if (GetStringProperty(scoped_device.get(), CFSTR(kIODialinDeviceKey),
-                          &dialinDevice)) {
-      serial::DeviceInfoPtr dialin_info = callout_info.Clone();
-      dialin_info->path = dialinDevice;
-      devices.push_back(dialin_info.Pass());
-    }
-
-    mojo::String calloutDevice;
-    if (GetStringProperty(scoped_device.get(), CFSTR(kIOCalloutDeviceKey),
-                          &calloutDevice)) {
-      callout_info->path = calloutDevice;
-      devices.push_back(callout_info.Pass());
-    }
-  }
-
+  } while (true);
   return devices.Pass();
 }
 
