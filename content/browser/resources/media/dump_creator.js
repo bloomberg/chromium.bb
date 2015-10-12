3f89b9f7d64b506a19084fc21c6a3cf05e05f6e4
grunell <grunell@chromium.org>
2015-10-12 06:08:33 -0700

    Update chrome://webrtc-internals audio recordings info text.
    Also set max width of that paragraph.
    
    This is a follow-up to https://codereview.chromium.org/1272223003/
    
    BUG=516584,541286
    
    Review URL: https://codereview.chromium.org/1399713003
    
    Cr-Commit-Position: refs/heads/master@{#353518}
    
    Former-commit-id: ecdd295dfbcc389b77f0b82a5788f27b92ba6d87

--- content/browser/resources/media/dump_creator.js
+++ content/browser/resources/media/dump_creator.js
@@ -33,26 +33,26 @@ var DumpCreator = (function() {
         '</button></a></div>' +
         '<p><label><input type=checkbox>' +
         'Enable diagnostic audio recordings</label></p>' +
-        '<p>A diagnostic audio recording is used for analyzing audio' +
-        ' problems. It contains the audio played out from the speaker and' +
-        ' recorded from the microphone and is saved to the local disk.' +
-        ' Checking this box will enable the recording for ongoing WebRTC' +
-        ' calls and for future WebRTC calls. When the box is unchecked or' +
-        ' this page is closed, all ongoing recordings will be stopped and' +
-        ' this recording functionality will be disabled for future WebRTC' +
-        ' calls. Recordings in multiple tabs are supported as well as' +
-        ' multiple recordings in the same tab. When enabling, you select a' +
-        ' base filename to save the dump(s) to. The base filename will have a' +
-        ' suffix appended to it as &lt;base filename&gt;.&lt;render process' +
-        ' ID&gt;.&lt;recording ID&gt;. If recordings are' +
-        ' disabled and then enabled using the same base filename, the' +
-        ' file(s) will be appended to and may become invalid. It is' +
-        ' recommended to choose a new base filename each time or move' +
-        ' the resulting files before enabling again. If track processing is' +
-        ' disabled (--disable-audio-track-processing): (1) Only one recording' +
-        ' per render process is supported. (2) When the box is unchecked or' +
-        ' this page is closed, ongoing recordings will continue until the' +
-        ' call ends or the page with the recording is closed</p>';
+        '<p class=audio-recordings-info>A diagnostic audio recording is used' +
+        ' for analyzing audio problems. It consists of two files and contains' +
+        ' the audio played out from the speaker and recorded from the' +
+        ' microphone and is saved to the local disk. Checking this box will' +
+        ' enable the recording for ongoing WebRTC calls and for future WebRTC' +
+        ' calls. When the box is unchecked or this page is closed, all' +
+        ' ongoing recordings will be stopped and this recording' +
+        ' functionality will be disabled for future WebRTC calls. Recordings' +
+        ' in multiple tabs are supported as well as multiple recordings in' +
+        ' the same tab. When enabling, you select a base filename to which' +
+        ' suffixes will be appended as</p>' +
+        '<p><div>&lt;base filename&gt;.&lt;render process ID&gt;' +
+        '.aec_dump.&lt;recording ID&gt;</div>' +
+        '<div>&lt;base filename&gt;.&lt;render process ID&gt;' +
+        '.source_input.&lt;stream ID&gt;.pcm</div></p>' +
+        '<p class=audio-recordings-info>If recordings are disabled and then' +
+        ' enabled using the same base filename, the files will be appended' +
+        ' to and may become invalid. It is recommended to choose a new base' +
+        ' filename each time or move the produced files before enabling' +
+        ' again.</p>';
 
     content.getElementsByTagName('a')[0].addEventListener(
         'click', this.onDownloadData_.bind(this));
