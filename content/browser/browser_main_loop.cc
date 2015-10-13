6cbd972c525729afe30273dbd0072b03a0dfc971
thestig <thestig@chromium.org>
2015-10-13 10:31:17 -0700

    Linux: Bump up the file descriptor limit for the browser process.
    BUG=539567
    
    Review URL: https://codereview.chromium.org/1398853005
    
    Cr-Commit-Position: refs/heads/master@{#353779}
    
    Former-commit-id: 357a22f6e3ade4fb00f79dcf5f201266fad50b57

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -60,7 +60,6 @@
 #include "content/public/common/content_switches.h"
 #include "content/public/common/main_function_params.h"
 #include "content/public/common/result_codes.h"
-#include "crypto/nss_util.h"
 #include "device/battery/battery_status_service.h"
 #include "media/audio/audio_manager.h"
 #include "media/base/media.h"
@@ -460,10 +459,10 @@ void BrowserMainLoop::EarlyInitialization() {
   // definitely harmless, so retained as a reminder of this
   // requirement for gconf.
   g_type_init();
-#endif
+#endif  // !GLIB_CHECK_VERSION(2, 35, 0)
 
   SetUpGLibLogHandler();
-#endif
+#endif  // defined(USE_GLIB)
 
   if (parts_)
     parts_->PreEarlyInitialization();
@@ -472,7 +471,13 @@ void BrowserMainLoop::EarlyInitialization() {
   // We use quite a few file descriptors for our IPC, and the default limit on
   // the Mac is low (256), so bump it up.
   base::SetFdLimit(1024);
-#endif
+#elif defined(OS_LINUX)
+  // Same for Linux. The default various per distro, but it is 1024 on Fedora.
+  // Low soft limits combined with liberal use of file descriptors means power
+  // users can easily hit this limit with many open tabs. Bump up the limit to
+  // an arbitrarily high number. See https://crbug.com/539567
+  base::SetFdLimit(8192);
+#endif  // default(OS_MACOSX)
 
 #if defined(OS_WIN)
   net::EnsureWinsockInit();
