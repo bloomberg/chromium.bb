99d322dd6acda58155b8de3f53ffbb3f4f8dcc5a
eae <eae@chromium.org>
2015-10-13 06:10:41 -0700

    Enable complex text for fast/text tests
    R=drott@chromium.org
    BUG=404597
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1400143002
    
    Cr-Commit-Position: refs/heads/master@{#353729}
    
    Former-commit-id: 29595fd381b8e4872d6e926fb138680e7e67c8d3

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/base.py
@@ -1587,7 +1587,7 @@ class Port(object):
         return [
             # For example, to turn on force-compositing-mode in the svg/ directory:
             # PhysicalTestSuite('svg', ['--force-compositing-mode']),
-            PhysicalTestSuite('fast/text', ["--enable-direct-write", "--enable-font-antialiasing"]),
+            PhysicalTestSuite('fast/text', ["--always-use-complex-text", "--enable-direct-write", "--enable-font-antialiasing"]),
         ]
 
     def virtual_test_suites(self):
