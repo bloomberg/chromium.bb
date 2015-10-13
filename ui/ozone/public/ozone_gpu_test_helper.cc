34a6d6f47736cfcd49bc490a9994dc013c29f3be
dnicoara <dnicoara@chromium.org>
2015-10-13 11:24:02 -0700

    [Ozone] Update GPUTestHelper to properly use the message filter
    Normally messages sent to the GPU process are first passed on the IO
    thread to the message filter. If they aren't handled by the filter they
    are then forwarded to the GPU main thread. This patch adds the ability
    to handle the messages in the filter before sending it to the GPU main
    thread.
    
    BUG=536113
    
    Review URL: https://codereview.chromium.org/1308993007
    
    Cr-Commit-Position: refs/heads/master@{#353797}
    
    Former-commit-id: 18c866f9299f102e617b983ee6f611c6e2aa371d

--- ui/ozone/public/ozone_gpu_test_helper.cc
+++ ui/ozone/public/ozone_gpu_test_helper.cc
@@ -19,14 +19,35 @@ namespace {
 
 const int kGpuProcessHostId = 1;
 
-}  // namespace
-
-static void DispatchToGpuPlatformSupportHostTask(IPC::Message* msg) {
+void DispatchToGpuPlatformSupportHostTask(IPC::Message* msg) {
   ui::OzonePlatform::GetInstance()
       ->GetGpuPlatformSupportHost()
       ->OnMessageReceived(*msg);
   delete msg;
 }
+
+void DispatchToGpuPlatformSupportTask(IPC::Message* msg) {
+  ui::OzonePlatform::GetInstance()->GetGpuPlatformSupport()->OnMessageReceived(
+      *msg);
+  delete msg;
+}
+
+void DispatchToGpuPlatformSupportTaskOnIO(
+    const scoped_refptr<base::SingleThreadTaskRunner>& gpu_task_runner,
+    IPC::Message* msg) {
+  IPC::MessageFilter* filter = ui::OzonePlatform::GetInstance()
+                                   ->GetGpuPlatformSupport()
+                                   ->GetMessageFilter();
+  if (filter && filter->OnMessageReceived(*msg)) {
+    delete msg;
+    return;
+  }
+
+  gpu_task_runner->PostTask(FROM_HERE,
+                            base::Bind(DispatchToGpuPlatformSupportTask, msg));
+}
+
+}  // namespace
 
 class FakeGpuProcess : public IPC::Sender {
  public:
@@ -60,30 +81,27 @@ class FakeGpuProcess : public IPC::Sender {
   scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner_;
 };
 
-static void DispatchToGpuPlatformSupportTask(IPC::Message* msg) {
-  ui::OzonePlatform::GetInstance()->GetGpuPlatformSupport()->OnMessageReceived(
-      *msg);
-  delete msg;
-}
-
 class FakeGpuProcessHost {
  public:
   FakeGpuProcessHost(
-      const scoped_refptr<base::SingleThreadTaskRunner>& gpu_task_runner)
-      : gpu_task_runner_(gpu_task_runner) {}
+      const scoped_refptr<base::SingleThreadTaskRunner>& gpu_task_runner,
+      const scoped_refptr<base::SingleThreadTaskRunner>& gpu_io_task_runner)
+      : gpu_task_runner_(gpu_task_runner),
+        gpu_io_task_runner_(gpu_io_task_runner) {}
   ~FakeGpuProcessHost() {}
 
   void Init() {
     base::Callback<void(IPC::Message*)> sender =
-        base::Bind(&DispatchToGpuPlatformSupportTask);
+        base::Bind(&DispatchToGpuPlatformSupportTaskOnIO, gpu_task_runner_);
 
     ui::OzonePlatform::GetInstance()
         ->GetGpuPlatformSupportHost()
-        ->OnChannelEstablished(kGpuProcessHostId, gpu_task_runner_, sender);
+        ->OnChannelEstablished(kGpuProcessHostId, gpu_io_task_runner_, sender);
   }
 
  private:
   scoped_refptr<base::SingleThreadTaskRunner> gpu_task_runner_;
+  scoped_refptr<base::SingleThreadTaskRunner> gpu_io_task_runner_;
 };
 
 OzoneGpuTestHelper::OzoneGpuTestHelper() {
@@ -106,7 +124,8 @@ bool OzoneGpuTestHelper::Initialize(
                             base::Unretained(fake_gpu_process_.get())));
   fake_gpu_process_->Init();
 
-  fake_gpu_process_host_.reset(new FakeGpuProcessHost(gpu_task_runner));
+  fake_gpu_process_host_.reset(new FakeGpuProcessHost(
+      gpu_task_runner, io_helper_thread_->task_runner()));
   fake_gpu_process_host_->Init();
 
   return true;
