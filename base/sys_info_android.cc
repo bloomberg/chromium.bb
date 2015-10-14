db55673d2be605e24b2c0e4b034b81c5f68ef32f
boliu <boliu@chromium.org>
2015-10-14 09:52:30 -0700

    base: Bump default android version to 6.0.99
    The Android Marshmallow release is 6.0.
    
    Review URL: https://codereview.chromium.org/1395633004
    
    Cr-Commit-Position: refs/heads/master@{#354042}
    
    Former-commit-id: 61dcc027ad677ac9f917fca4b931498df853f924

--- base/sys_info_android.cc
+++ base/sys_info_android.cc
@@ -59,8 +59,8 @@ namespace {
 // cannot be acquired. Use the latest Android release with a higher bug fix
 // version to avoid unnecessarily comparison errors with the latest release.
 // This should be manually kept up-to-date on each Android release.
-const int kDefaultAndroidMajorVersion = 5;
-const int kDefaultAndroidMinorVersion = 1;
+const int kDefaultAndroidMajorVersion = 6;
+const int kDefaultAndroidMinorVersion = 0;
 const int kDefaultAndroidBugfixVersion = 99;
 
 // Parse out the OS version numbers from the system properties.
