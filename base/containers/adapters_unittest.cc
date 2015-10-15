b80a20fe6996941ee89b8a9e30c56bc70f1be373
mdempsky <mdempsky@chromium.org>
2015-10-14 18:06:10 -0700

    Extend base::Reversed to support iterating arrays
    C++11's range-based for syntax supports iterating arrays,
    so it makes sense to support arrays in base::Reversed too.
    
    Review URL: https://codereview.chromium.org/1360423002
    
    Cr-Commit-Position: refs/heads/master@{#354181}
    
    Former-commit-id: 137978257f920ebdaa7d65493f39194a6a4a2e0c

--- base/containers/adapters_unittest.cc
+++ base/containers/adapters_unittest.cc
@@ -25,7 +25,19 @@ TEST(AdaptersTest, Reversed) {
   EXPECT_EQ(101, v[2]);
 }
 
-TEST(AdaptersTest, ConstReversed) {
+TEST(AdaptersTest, ReversedArray) {
+  int v[3] = {3, 2, 1};
+  int j = 0;
+  for (int& i : base::Reversed(v)) {
+    EXPECT_EQ(++j, i);
+    i += 100;
+  }
+  EXPECT_EQ(103, v[0]);
+  EXPECT_EQ(102, v[1]);
+  EXPECT_EQ(101, v[2]);
+}
+
+TEST(AdaptersTest, ReversedConst) {
   std::vector<int> v;
   v.push_back(3);
   v.push_back(2);
