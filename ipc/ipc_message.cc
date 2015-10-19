e7c47967731811dd7e493bc0885bb3e8ca6a5178
erikchen <erikchen@chromium.org>
2015-10-19 15:23:32 -0700

    ipc: Add a member to IPC::Message::Header on Windows.
    According to my most recent debugging experiments, the root cause of the crashes
    that happened the first time I tried to make this change have magically
    disappeared. See the bug for more details.
    
    BUG=527588
    
    Review URL: https://codereview.chromium.org/1400713002
    
    Cr-Commit-Position: refs/heads/master@{#354889}
    
    Former-commit-id: c32bea67c022c6c385463f0eec027f18b2915300

--- ipc/ipc_message.cc
+++ ipc/ipc_message.cc
@@ -50,7 +50,7 @@ Message::~Message() {
 Message::Message() : base::Pickle(sizeof(Header)) {
   header()->routing = header()->type = 0;
   header()->flags = GetRefNumUpper24();
-#if defined(OS_MACOSX)
+#if USE_ATTACHMENT_BROKER
   header()->num_brokered_attachments = 0;
 #endif
 #if defined(OS_POSIX)
@@ -66,7 +66,7 @@ Message::Message(int32_t routing_id, uint32_t type, PriorityValue priority)
   header()->type = type;
   DCHECK((priority & 0xffffff00) == 0);
   header()->flags = priority | GetRefNumUpper24();
-#if defined(OS_MACOSX)
+#if USE_ATTACHMENT_BROKER
   header()->num_brokered_attachments = 0;
 #endif
 #if defined(OS_POSIX)
