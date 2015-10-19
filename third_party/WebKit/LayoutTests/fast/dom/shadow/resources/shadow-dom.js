322f2e32e8eb6978f68fe27385ffb04ecf3b1a6b
hayato <hayato@chromium.org>
2015-10-19 02:30:20 -0700

    Remove shadow-dom-util.js.
    shadow-dom-util.js has only one funciton. Let's merge it with shadow-dom.js.
    
    BUG=531990
    
    Review URL: https://codereview.chromium.org/1411163002
    
    Cr-Commit-Position: refs/heads/master@{#354744}
    
    Former-commit-id: b379eb2637d5db0f2ebea8ee3c96562d65a13c8d

--- third_party/WebKit/LayoutTests/fast/dom/shadow/resources/shadow-dom.js
+++ third_party/WebKit/LayoutTests/fast/dom/shadow/resources/shadow-dom.js
@@ -298,3 +298,36 @@ function backgroundColorShouldNotBe(selector, color)
     var unevaledString = '"' + color.replace(/\\/g, "\\\\").replace(/"/g, "\"") + '"';
     shouldNotBe(text, unevaledString);
 }
+
+function getElementByIdConsideringShadowDOM(root, id) {
+    function iter(root, id) {
+        if (!root)
+            return null;
+
+        if (root.id == id)
+            return root;
+
+        // We don't collect div having a shadow root, since we cannot point it correctly.
+        // Such div should have an inner div to be pointed correctly.
+        for (var child = root.firstChild; child; child = child.nextSibling) {
+            var node = iter(child, id);
+            if (node != null)
+                return node;
+        }
+
+        if (root.nodeType != 1)
+            return null;
+
+        for (var shadowRoot = internals.youngestShadowRoot(root); shadowRoot; shadowRoot = shadowRoot.olderShadowRoot) {
+            var node = iter(shadowRoot, id);
+            if (node != null)
+                return node;
+        }
+
+        return null;
+    };
+
+    if (!window.internals)
+        return null;
+    return iter(root, id);
+}
