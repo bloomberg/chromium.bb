4ae6c71a3a60c4d50cf2ca906ed421aa6d8d2e98
limasdf <limasdf@gmail.com>
2015-10-19 03:50:45 -0700

    Resubmit/ Remove deprecated extension notification from content_settings
    Removing deprecated extension notification from content_settings and
    use ExtensionRegistryObserver instead.
    
    The previous failure of linux asan test(https://codereview.chromium.org/276573011/) is
    fixed from somewhere during last several months. So, submit again.
    
    R=bauerb@chromium.org
    BUG=354046
    TEST=Run linux asan test.
    
    Review URL: https://codereview.chromium.org/1411133002
    
    Cr-Commit-Position: refs/heads/master@{#354756}
    
    Former-commit-id: 46c622a9ca32cd794039351ea2c488af67d7e395

--- chrome/browser/content_settings/content_settings_internal_extension_provider.cc
+++ chrome/browser/content_settings/content_settings_internal_extension_provider.cc
@@ -38,9 +38,9 @@ const char* const kRemotingViewerWhitelist[] = {
 
 }  // namespace
 
-
 InternalExtensionProvider::InternalExtensionProvider(Profile* profile)
-    : registrar_(new content::NotificationRegistrar) {
+    : registrar_(new content::NotificationRegistrar),
+      extension_registry_(extensions::ExtensionRegistry::Get(profile)) {
   for (size_t i = 0; i < arraysize(kRemotingViewerWhitelist); ++i)
     chrome_remote_desktop_.insert(kRemotingViewerWhitelist[i]);
 
@@ -53,12 +53,7 @@ InternalExtensionProvider::InternalExtensionProvider(Profile* profile)
   registrar_->Add(this,
                   extensions::NOTIFICATION_EXTENSION_HOST_CREATED,
                   content::Source<Profile>(profile));
-  registrar_->Add(this,
-                  extensions::NOTIFICATION_EXTENSION_LOADED_DEPRECATED,
-                  content::Source<Profile>(profile));
-  registrar_->Add(this,
-                  extensions::NOTIFICATION_EXTENSION_UNLOADED_DEPRECATED,
-                  content::Source<Profile>(profile));
+  extension_registry_->AddObserver(this);
 }
 
 InternalExtensionProvider::~InternalExtensionProvider() {
@@ -88,91 +83,81 @@ void InternalExtensionProvider::Observe(
     int type,
     const content::NotificationSource& source,
     const content::NotificationDetails& details) {
-  switch (type) {
-    case extensions::NOTIFICATION_EXTENSION_HOST_CREATED: {
-      const extensions::ExtensionHost* host =
-          content::Details<extensions::ExtensionHost>(details).ptr();
-      if (host->extension()->is_platform_app()) {
-        SetContentSettingForExtension(host->extension(), CONTENT_SETTING_BLOCK);
+  DCHECK_EQ(extensions::NOTIFICATION_EXTENSION_HOST_CREATED, type);
+  const extensions::ExtensionHost* host =
+      content::Details<extensions::ExtensionHost>(details).ptr();
+  if (host->extension()->is_platform_app()) {
+    SetContentSettingForExtension(host->extension(), CONTENT_SETTING_BLOCK);
 
-        // White-list CRD's v2 app, until crbug.com/134216 is complete.
-        const char* const kAppWhitelist[] = {
-          "2775E568AC98F9578791F1EAB65A1BF5F8CEF414",
-          "4AA3C5D69A4AECBD236CAD7884502209F0F5C169",
-          "97B23E01B2AA064E8332EE43A7A85C628AADC3F2",
-          "9E930B2B5EABA6243AE6C710F126E54688E8FAF6",
-          "C449A798C495E6CF7D6AF10162113D564E67AD12",
-          "E410CDAB2C6E6DD408D731016CECF2444000A912",
-          "EBA908206905323CECE6DC4B276A58A0F4AC573F",
+    // White-list CRD's v2 app, until crbug.com/134216 is complete.
+    const char* const kAppWhitelist[] = {
+        "2775E568AC98F9578791F1EAB65A1BF5F8CEF414",
+        "4AA3C5D69A4AECBD236CAD7884502209F0F5C169",
+        "97B23E01B2AA064E8332EE43A7A85C628AADC3F2",
+        "9E930B2B5EABA6243AE6C710F126E54688E8FAF6",
+        "C449A798C495E6CF7D6AF10162113D564E67AD12",
+        "E410CDAB2C6E6DD408D731016CECF2444000A912",
+        "EBA908206905323CECE6DC4B276A58A0F4AC573F",
 
-          // http://crbug.com/327507
-          "FAFE8EFDD2D6AE2EEB277AFEB91C870C79064D9E",
-          "3B52D273A271D4E2348233E322426DBAE854B567",
-          "5DF6ADC8708DF59FCFDDBF16AFBFB451380C2059",
-          "1037DEF5F6B06EA46153AD87B6C5C37440E3F2D1",
-          "F5815DAFEB8C53B078DD1853B2059E087C42F139",
-          "6A08EFFF9C16E090D6DCC7EC55A01CADAE840513",
+        // http://crbug.com/327507
+        "FAFE8EFDD2D6AE2EEB277AFEB91C870C79064D9E",
+        "3B52D273A271D4E2348233E322426DBAE854B567",
+        "5DF6ADC8708DF59FCFDDBF16AFBFB451380C2059",
+        "1037DEF5F6B06EA46153AD87B6C5C37440E3F2D1",
+        "F5815DAFEB8C53B078DD1853B2059E087C42F139",
+        "6A08EFFF9C16E090D6DCC7EC55A01CADAE840513",
 
-          // http://crbug.com/354258
-          "C32D6D93E12F5401DAA3A723E0C3CC5F25429BA4",
-          "9099782647D39C778E15C8C6E0D23C88F5CDE170",
-          "B7D5B52D1E5B106288BD7F278CAFA5E8D76108B0",
-          "89349DBAA2C4022FB244AA50182AB60934EB41EE",
-          "CB593E510640572A995CB1B6D41BD85ED51E63F8",
-          "1AD1AC86C87969CD3434FA08D99DBA6840AEA612",
-          "9C2EA21D7975BDF2B3C01C3A454EE44854067A6D",
-          "D2C488C80C3C90C3E01A991112A05E37831E17D0",
-          "6EEC061C0E74B46C7B5BE2EEFA49436368F4988F",
-          "8B344D9E8A4C505EF82A0DBBC25B8BD1F984E777",
-          "E06AFCB1EB0EFD237824CC4AC8FDD3D43E8BC868",
+        // http://crbug.com/354258
+        "C32D6D93E12F5401DAA3A723E0C3CC5F25429BA4",
+        "9099782647D39C778E15C8C6E0D23C88F5CDE170",
+        "B7D5B52D1E5B106288BD7F278CAFA5E8D76108B0",
+        "89349DBAA2C4022FB244AA50182AB60934EB41EE",
+        "CB593E510640572A995CB1B6D41BD85ED51E63F8",
+        "1AD1AC86C87969CD3434FA08D99DBA6840AEA612",
+        "9C2EA21D7975BDF2B3C01C3A454EE44854067A6D",
+        "D2C488C80C3C90C3E01A991112A05E37831E17D0",
+        "6EEC061C0E74B46C7B5BE2EEFA49436368F4988F",
+        "8B344D9E8A4C505EF82A0DBBC25B8BD1F984E777",
+        "E06AFCB1EB0EFD237824CC4AC8FDD3D43E8BC868",
 
-          // http://crbug.com/386324
-          "F76F43EFFF56BF17A9868A5243F339BA28746632",
-          "C6EA52B92F80878515F94137020F01519357E5B5",
-          "E466389F058ABD73FF6FDD06F768A351FCBF8532",
-          "40063F1CF7B68BA847A26FA6620DDF156171D23C",
-          "A6FD8E15353CF1F5C3D0A7B20E1D10AEA4DD3E6A",
-          "57AC4D9E6BD8A2D0A70056B5FAC2378CAA588912",
-          "02037314DA4D913640DCF0E296A7D01F4FD793EC",
-          "B6EC0809BC63E10B431C5E4AA3645232CA86B2A5",
-          "48CA541313139786F056DBCB504A1025CFF5D2E3",
-          "05106136AE7F08A3C181D4648E5438350B1D2B4F"
-        };
-        if (extensions::SimpleFeature::IsIdInArray(
-                host->extension()->id(),
-                kAppWhitelist,
-                arraysize(kAppWhitelist))) {
-          SetContentSettingForExtensionAndResource(
-              host->extension(),
-              ChromeContentClient::kRemotingViewerPluginPath,
-              CONTENT_SETTING_ALLOW);
-        }
-      }
-
-      break;
+        // http://crbug.com/386324
+        "F76F43EFFF56BF17A9868A5243F339BA28746632",
+        "C6EA52B92F80878515F94137020F01519357E5B5",
+        "E466389F058ABD73FF6FDD06F768A351FCBF8532",
+        "40063F1CF7B68BA847A26FA6620DDF156171D23C",
+        "A6FD8E15353CF1F5C3D0A7B20E1D10AEA4DD3E6A",
+        "57AC4D9E6BD8A2D0A70056B5FAC2378CAA588912",
+        "02037314DA4D913640DCF0E296A7D01F4FD793EC",
+        "B6EC0809BC63E10B431C5E4AA3645232CA86B2A5",
+        "48CA541313139786F056DBCB504A1025CFF5D2E3",
+        "05106136AE7F08A3C181D4648E5438350B1D2B4F"};
+    if (extensions::SimpleFeature::IsIdInArray(
+            host->extension()->id(), kAppWhitelist, arraysize(kAppWhitelist))) {
+      SetContentSettingForExtensionAndResource(
+          host->extension(), ChromeContentClient::kRemotingViewerPluginPath,
+          CONTENT_SETTING_ALLOW);
     }
-    case extensions::NOTIFICATION_EXTENSION_LOADED_DEPRECATED: {
-      const extensions::Extension* extension =
-          content::Details<extensions::Extension>(details).ptr();
-      ApplyPluginContentSettingsForExtension(extension, CONTENT_SETTING_ALLOW);
-      break;
-    }
-    case extensions::NOTIFICATION_EXTENSION_UNLOADED_DEPRECATED: {
-      const UnloadedExtensionInfo& info =
-          *(content::Details<UnloadedExtensionInfo>(details).ptr());
-      ApplyPluginContentSettingsForExtension(info.extension,
-                                             CONTENT_SETTING_DEFAULT);
-      break;
-    }
-    default:
-      NOTREACHED();
   }
+}
+
+void InternalExtensionProvider::OnExtensionLoaded(
+    content::BrowserContext* browser_context,
+    const extensions::Extension* extension) {
+  ApplyPluginContentSettingsForExtension(extension, CONTENT_SETTING_ALLOW);
+}
+
+void InternalExtensionProvider::OnExtensionUnloaded(
+    content::BrowserContext* browser_context,
+    const extensions::Extension* extension,
+    extensions::UnloadedExtensionInfo::Reason reason) {
+  ApplyPluginContentSettingsForExtension(extension, CONTENT_SETTING_DEFAULT);
 }
 
 void InternalExtensionProvider::ShutdownOnUIThread() {
   DCHECK(CalledOnValidThread());
   RemoveAllObservers();
   registrar_.reset();
+  extension_registry_->RemoveObserver(this);
 }
 
 void InternalExtensionProvider::ApplyPluginContentSettingsForExtension(
--- chrome/browser/content_settings/content_settings_internal_extension_provider.h
+++ chrome/browser/content_settings/content_settings_internal_extension_provider.h
@@ -15,18 +15,21 @@
 #include "components/content_settings/core/common/content_settings.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "extensions/browser/extension_registry_observer.h"
 
 class Profile;
 
 namespace extensions {
 class Extension;
+class ExtensionRegistry;
 }
 
 namespace content_settings {
 
 // A content settings provider which disables certain plugins for platform apps.
 class InternalExtensionProvider : public ObservableProvider,
-                                  public content::NotificationObserver {
+                                  public content::NotificationObserver,
+                                  public extensions::ExtensionRegistryObserver {
  public:
   explicit InternalExtensionProvider(Profile* profile);
 
@@ -52,6 +55,14 @@ class InternalExtensionProvider : public ObservableProvider,
                const content::NotificationSource& source,
                const content::NotificationDetails& details) override;
 
+  // extensions::ExtensionRegistryObserver implementation.
+  void OnExtensionLoaded(content::BrowserContext* browser_context,
+                         const extensions::Extension* extension) override;
+  void OnExtensionUnloaded(
+      content::BrowserContext* browser_context,
+      const extensions::Extension* extension,
+      extensions::UnloadedExtensionInfo::Reason reason) override;
+
  private:
   void ApplyPluginContentSettingsForExtension(
       const extensions::Extension* extension,
@@ -71,6 +82,8 @@ class InternalExtensionProvider : public ObservableProvider,
 
   // Extension IDs used by the Chrome Remote Desktop app.
   std::set<std::string> chrome_remote_desktop_;
+
+  extensions::ExtensionRegistry* extension_registry_;
 
   DISALLOW_COPY_AND_ASSIGN(InternalExtensionProvider);
 };
