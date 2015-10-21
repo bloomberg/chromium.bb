8297a310d76bb0312ced3f452e94f6a99c825fdd
bshe <bshe@chromium.org>
2015-10-21 11:21:53 -0700

    Introduce InputMethodAuraAndroid for Aura on Android
    This add an InputMethodAuraAndroid class for Aura on Android.
    Note: the class is not being used anywhere yet and will only be used when
    android and use_aura are true.
    
    BUG=507792
    
    Review URL: https://codereview.chromium.org/1394883004
    
    Cr-Commit-Position: refs/heads/master@{#355345}
    
    Former-commit-id: 001e85a661d29ff805cd059f007f133e8fa54116

--- ui/base/ime/input_method_factory.cc
+++ ui/base/ime/input_method_factory.cc
@@ -17,6 +17,8 @@
 #elif defined(USE_AURA) && defined(OS_LINUX) && defined(USE_X11) && \
       !defined(OS_CHROMEOS)
 #include "ui/base/ime/input_method_auralinux.h"
+#elif defined(OS_ANDROID)
+#include "ui/base/ime/input_method_android.h"
 #else
 #include "ui/base/ime/input_method_minimal.h"
 #endif
@@ -59,6 +61,8 @@ scoped_ptr<InputMethod> CreateInputMethod(
 #elif defined(USE_AURA) && defined(OS_LINUX) && defined(USE_X11) && \
       !defined(OS_CHROMEOS)
   return make_scoped_ptr(new InputMethodAuraLinux(delegate));
+#elif defined(OS_ANDROID)
+  return make_scoped_ptr(new InputMethodAndroid(delegate));
 #else
   return make_scoped_ptr(new InputMethodMinimal(delegate));
 #endif
