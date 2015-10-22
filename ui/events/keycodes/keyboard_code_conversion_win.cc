a21e20debac01cfae49b3ab42baa87d2e6ad6c33
dtapuska <dtapuska@chromium.org>
2015-10-22 14:05:47 -0700

    Fix Alt-PrintScreen, Hanja,Han/Yeong keys in Korean layout on Windows
    Windows generates unique scan codes for certain key combinations.
    Since the corresponding physical keys have multiple scan code
    definitions map them before the NativeKeyCode table that has
    a 1:1 mapping.
    
    BUG=542577
    
    Review URL: https://codereview.chromium.org/1416333002
    
    Cr-Commit-Position: refs/heads/master@{#355640}
    
    Former-commit-id: 98a52ecd417da4aadc2a467ed868f138273f2658

--- ui/events/keycodes/keyboard_code_conversion_win.cc
+++ ui/events/keycodes/keyboard_code_conversion_win.cc
@@ -9,8 +9,14 @@ namespace ui {
 
 namespace {
 
+const WORD kAltPrintScreenScanCode = 0x0054;
+const WORD kPrintScreenScanCode = 0xe037;
 const WORD kCtrlPauseScanCode = 0xe046;
 const WORD kPauseScanCode = 0x0045;
+const WORD kHanjaKeyKoreanLayoutScanCode = 0xe0f1;
+const WORD kHanjaKeyScanCode = 0x0071;
+const WORD kHanYeongKeyKoreanLayoutScanCode = 0xe0f2;
+const WORD kHanYeongKeyScanCode = 0x0072;
 
 }  // namespace
 
@@ -23,10 +29,23 @@ KeyboardCode KeyboardCodeForWindowsKeyCode(WORD keycode) {
 }
 
 DomCode CodeForWindowsScanCode(WORD scan_code) {
-  // Ctrl-Pause generates a special scancode; make sure
-  // we undo this special mapping.
-  if (scan_code == kCtrlPauseScanCode)
-    scan_code = kPauseScanCode;
+  // There are a few instances where multiple scancodes map to the same
+  // physical key. Instead of teaching the NativeKeyCode table to support
+  // multiple values; we do this simple translation before calling it.
+  switch (scan_code) {
+    case kAltPrintScreenScanCode:
+      scan_code = kPrintScreenScanCode;
+      break;
+    case kCtrlPauseScanCode:
+      scan_code = kPauseScanCode;
+      break;
+    case kHanjaKeyKoreanLayoutScanCode:
+      scan_code = kHanjaKeyScanCode;
+      break;
+    case kHanYeongKeyKoreanLayoutScanCode:
+      scan_code = kHanYeongKeyScanCode;
+      break;
+  }
 
   return ui::KeycodeConverter::NativeKeycodeToDomCode(scan_code);
 }
