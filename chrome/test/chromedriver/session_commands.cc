9a5531e297261350e8f2c686f3cdef7972cf2dcd
gmanikpure <gmanikpure@chromium.org>
2015-10-22 10:22:32 -0700

    [ChromeDriver] Include chromedriver version number in capabilities.
    BUG=chromedriver:1246
    
    Review URL: https://codereview.chromium.org/1423593002
    
    Cr-Commit-Position: refs/heads/master@{#355563}
    
    Former-commit-id: 3b2e09ec0a839d5c6c13ff0a35c42b31a3e7720a

--- chrome/test/chromedriver/session_commands.cc
+++ chrome/test/chromedriver/session_commands.cc
@@ -92,17 +92,14 @@ scoped_ptr<base::DictionaryValue> CreateCapabilities(Chrome* chrome) {
   caps->SetBoolean("acceptSslCerts", true);
   caps->SetBoolean("nativeEvents", true);
   caps->SetBoolean("hasTouchScreen", chrome->HasTouchScreen());
-  scoped_ptr<base::DictionaryValue> chrome_caps(new base::DictionaryValue());
 
   ChromeDesktopImpl* desktop = NULL;
   Status status = chrome->GetAsDesktop(&desktop);
   if (status.IsOk()) {
-    chrome_caps->SetString(
-        "userDataDir",
-        desktop->command().GetSwitchValueNative("user-data-dir"));
+    caps->SetString("chrome.userDataDir",
+                    desktop->command().GetSwitchValueNative("user-data-dir"));
   }
 
-  caps->Set("chrome", chrome_caps.release());
   return caps.Pass();
 }
 
