6110899dd325643c34763a8c8ca57d3fc218cf6c
nasko <nasko@chromium.org>
2015-10-23 16:40:15 -0700

    Add SiteIsolationExtension field trial.
    This CL adds a field trial named "SiteIsolationExtension" to allow us
    to experiment with enabling --isolate-extensions mode of the Site Isolation
    project.
    
    BUG=546050
    
    Review URL: https://codereview.chromium.org/1421513004
    
    Cr-Commit-Position: refs/heads/master@{#355922}
    
    Former-commit-id: 78b4e77d39697b6abbf0081795a02ce64c19d6d8

--- chrome/common/extensions/extension_process_policy.cc
+++ chrome/common/extensions/extension_process_policy.cc
@@ -5,6 +5,8 @@
 #include "chrome/common/extensions/extension_process_policy.h"
 
 #include "base/command_line.h"
+#include "base/metrics/field_trial.h"
+#include "base/strings/string_util.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_constants.h"
 #include "extensions/common/constants.h"
@@ -66,8 +68,16 @@ bool CrossesExtensionProcessBoundary(
 }
 
 bool IsIsolateExtensionsEnabled() {
-  return base::CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kIsolateExtensions);
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kIsolateExtensions)) {
+    return true;
+  }
+
+  const std::string group_name =
+      base::FieldTrialList::FindFullName("SiteIsolationExtensions");
+  // Use StartsWith() for more flexibility (e.g. multiple Enabled groups).
+  return base::StartsWith(group_name, "Enabled",
+                          base::CompareCase::INSENSITIVE_ASCII);
 }
 
 }  // namespace extensions
