acdce753ed2a405f36e8b0b984c5860e7d3c8723
apacible <apacible@chromium.org>
2015-10-26 14:07:11 -0700

    [Media Router] Fix SinkIconType enum.
    Review URL: https://codereview.chromium.org/1411863005
    
    Cr-Commit-Position: refs/heads/master@{#356122}
    
    Former-commit-id: 332ba2a82d0e07805440916bc9cadd96ddcf52ea

--- chrome/browser/resources/media_router/media_router_data.js
+++ chrome/browser/resources/media_router/media_router_data.js
@@ -38,8 +38,8 @@ cr.define('media_router', function() {
     CAST: 0,
     CAST_AUDIO: 1,
     CAST_AUDIO_GROUP: 2,
-    GENERIC: 2,
-    HANGOUT: 3,
+    GENERIC: 3,
+    HANGOUT: 4,
   };
 
   /**
