50289c267b7af0a14b29b0365a47792dd360bed8
akuegel@chromium.org <akuegel@chromium.org>
2015-10-28 15:41:25 +0000

    Revert of Generalize python_git_runner for use in non-git scripts. (patchset #1 id:1 of https://codereview.chromium.org/1415993003/ )
    Reason for revert:
    Speculative revert in case this caused the bots to fail bot_update.
    
    Original issue's description:
    > Generalize python_git_runner for use in non-git scripts.
    > 
    > Also fixes an msys (git-bash) incompatibility, similar to the one
    > fixed in crrev.com/4845f0ed694ff071da5f6ee937a0bce2ea4e6fd2.
    > 
    > This change doesn't actually use the wrapper in any new places, but
    > subsequent changes will be made to use it in places where
    > 'exec python' is called directly, like gclient and fetch.
    > 
    > BUG=411518
    > R=iannucci@google.com
    > 
    > Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=297357
    
    TBR=iannucci@google.com,iannucci@chromium.org,mmoss@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=411518
    
    Review URL: https://codereview.chromium.org/1414923008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@297358 0039d316-1c4b-4281-b951-d872f2087c98

--- python_git_runner.sh
+++ python_git_runner.sh
@@ -1,14 +1,14 @@
-# Copyright 2015 The Chromium Authors. All rights reserved.
+# Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 ## This file is designed to be sourced from a bash script whose name takes the
-## form 'command-name'. This script will then instead invoke
-## '[depot_tools]/command_name.py' correctly under mingw as well
+## form 'git-sub-command'. This script will then instead invoke
+## '[depot_tools]/git_sub_command.py' correctly under mingw as well
 ## as posix-ey systems, passing along all other command line flags.
 
 ## Example:
-## echo ". python_runner.sh" > git-foo-command
+## echo ". python_git_runner.sh" > git-foo-command
 ## ./git-foo-command  #=> runs `python git_foo_command.py`
 
 ## Constants
@@ -19,14 +19,14 @@ PYTHONDONTWRITEBYTECODE=1
 #
 # SCRIPT defaults to the basename of $0, with dashes replaced with underscores
 
-DEPOT_TOOLS="${0%/*}"
-# Sometimes commands will run with no path (e.g. a git command run from within
-# the depot_tools dir itself). In that case, treat it like it was run like:
-# "./command"
-if [[ "$DEPOT_TOOLS" = "$0" ]]; then
-  DEPOT_TOOLS="."
+if [[ $OSTYPE = msys ]]
+then
+  DEPOT_TOOLS="${0%\\*}"
+  BASENAME="${0##*\\}"
+else
+  DEPOT_TOOLS="${0%/*}"
+  BASENAME="${0##*/}"
 fi
-BASENAME="${0##*/}"
 SCRIPT="${SCRIPT-${BASENAME//-/_}.py}"
 
 if [[ -e "$DEPOT_TOOLS/python.bat" && $OSTYPE = msys ]]; then
