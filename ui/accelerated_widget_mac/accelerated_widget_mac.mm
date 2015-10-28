235a507f52671154ce60e2f1d0ab65c24ccea0b2
shrike <shrike@chromium.org>
2015-10-27 17:12:01 -0700

    Fix compile error accessing contentsStyle property in 10.11 SDK.
    BUG=548377
    
    Review URL: https://codereview.chromium.org/1423093004
    
    Cr-Commit-Position: refs/heads/master@{#356446}
    
    Former-commit-id: a88f62b41cdce337b9d0c74a57b01957790e8605

--- ui/accelerated_widget_mac/accelerated_widget_mac.mm
+++ ui/accelerated_widget_mac/accelerated_widget_mac.mm
@@ -9,6 +9,7 @@
 #include "base/lazy_instance.h"
 #include "base/mac/mac_util.h"
 #include "base/mac/scoped_cftyperef.h"
+#include "base/mac/sdk_forward_declarations.h"
 #include "base/message_loop/message_loop.h"
 #include "base/trace_event/trace_event.h"
 #include "third_party/skia/include/core/SkCanvas.h"
