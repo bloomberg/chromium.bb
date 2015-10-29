1e8d70128e325ec1e5df26cddb8c4ae2093eeacd
bustamante <bustamante@google.com>
2015-10-29 14:14:04 -0700

    Look for extra via header in DRP check, added debug info.
    If an extra header is specified in the --extra-chrome-proxy-via-header flag
    look for it in the initial DRP check, before running the test.
    
    Also adding debug info by storing the last response headers in a global
    variable.  If the DRP check fails, it will show up in the dump.
    
    BUG=549267
    
    Review URL: https://codereview.chromium.org/1408393009
    
    Cr-Commit-Position: refs/heads/master@{#356948}
    
    Former-commit-id: 480cdded0237f6ce7e06501725bb022e155c123d

--- tools/chrome_proxy/common/chrome_proxy_measurements.py
+++ tools/chrome_proxy/common/chrome_proxy_measurements.py
@@ -31,6 +31,9 @@ def WaitForViaHeader(tab, url="http://check.googlezip.net/test.html"):
         'var xmlhttp = new XMLHttpRequest();'
         'xmlhttp.open("HEAD",url,true);'
         'xmlhttp.onload=function(e) {'
+          # Store the last response received for debugging, this will be shown
+          # in telemetry dumps if the request fails or times out.
+          'window.last_xhr_response_headers = xmlhttp.getAllResponseHeaders();'
           'var via=xmlhttp.getResponseHeader("via");'
           'if (via && via.indexOf(wanted_via) != -1) {'
             'window.via_header_found = true;'
@@ -49,8 +52,13 @@ def WaitForViaHeader(tab, url="http://check.googlezip.net/test.html"):
 
   # Ensure the page has started loading before attempting the DRP check.
   tab.WaitForJavaScriptExpression('performance.timing.loadEventStart', 60)
+
+  expected_via_header = metrics.CHROME_PROXY_VIA_HEADER
+  if ChromeProxyValidation.extra_via_header:
+    expected_via_header = ChromeProxyValidation.extra_via_header
+
   tab.WaitForJavaScriptExpression(
-    'PollDRPCheck("%s", "%s")' % (url, metrics.CHROME_PROXY_VIA_HEADER), 60)
+      'PollDRPCheck("%s", "%s")' % (url, expected_via_header), 60)
 
 
 class ChromeProxyValidation(page_test.PageTest):
