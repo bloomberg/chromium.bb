37a406d495cb8fef434ad9010c5549619e6dedbc
nednguyen <nednguyen@google.com>
2015-10-30 01:26:16 -0700

    [Perf] Disable scheduler.tough_scheduling_cases on reference build
    TBR=aiolos@chromium.org
    BUG=549428
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1428883003
    
    Cr-Commit-Position: refs/heads/master@{#357062}
    
    Former-commit-id: 1a2715c7977a7b70c6500ed09a51d3f19da99fbc

--- tools/perf/benchmarks/scheduler.py
+++ tools/perf/benchmarks/scheduler.py
@@ -1,13 +1,15 @@
 # Copyright 2014 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+from telemetry import benchmark
 
 from core import perf_benchmark
-
 from measurements import smoothness
+
 import page_sets
 
 
+@benchmark.Disabled('reference')  # crbug.com/549428
 class SchedulerToughSchedulingCases(perf_benchmark.PerfBenchmark):
   """Measures rendering statistics while interacting with pages that have
   challenging scheduling properties.
