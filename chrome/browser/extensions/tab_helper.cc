508e00daebe83f1526aec4a7f6fa63554979811b
rob <rob@robwu.nl>
2015-11-02 16:43:17 -0800

    Fix indentation of IPC_MESSAGE_HANDLER macros
    Review URL: https://codereview.chromium.org/1421813007
    
    Cr-Commit-Position: refs/heads/master@{#357490}
    
    Former-commit-id: a16a07e447042dd096eb925e07e12780d5fb831f

--- chrome/browser/extensions/tab_helper.cc
+++ chrome/browser/extensions/tab_helper.cc
@@ -277,10 +277,10 @@ bool TabHelper::OnMessageReceived(const IPC::Message& message,
                                   content::RenderFrameHost* render_frame_host) {
   bool handled = true;
   IPC_BEGIN_MESSAGE_MAP_WITH_PARAM(TabHelper, message, render_frame_host)
-  IPC_MESSAGE_HANDLER(ExtensionHostMsg_InlineWebstoreInstall,
-                      OnInlineWebstoreInstall)
-  IPC_MESSAGE_HANDLER(ExtensionHostMsg_GetAppInstallState,
-                      OnGetAppInstallState);
+    IPC_MESSAGE_HANDLER(ExtensionHostMsg_InlineWebstoreInstall,
+                        OnInlineWebstoreInstall)
+    IPC_MESSAGE_HANDLER(ExtensionHostMsg_GetAppInstallState,
+                        OnGetAppInstallState)
     IPC_MESSAGE_HANDLER(ExtensionHostMsg_ContentScriptsExecuting,
                         OnContentScriptsExecuting)
     IPC_MESSAGE_UNHANDLED(handled = false)
