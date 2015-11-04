7287c961c261f68360081f5c40bd5f7cbb67ba15
dschuyler <dschuyler@chromium.org>
2015-11-04 14:42:04 -0800

    [MD settings] adding font settings files
    This CL adds the new files needed for font
    settings (web content) in the appearance
    settings.
    
    BUG=531786
    
    Review URL: https://codereview.chromium.org/1415463013
    
    Cr-Commit-Position: refs/heads/master@{#357916}
    
    Former-commit-id: 8eb14f22d158e59ff8794b533f72783f6faa175f

---
+++ chrome/browser/resources/settings/appearance_page/appearance_font_menu.js
@@ -0,0 +1,177 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * 'settings-appearance-font-menu' is a control for displaying font options
+ * in the appearance settings.
+ *
+ * Example:
+ *
+ *   <settings-appearance-font-menu pref="{{prefs.foo}}">
+ *   </settings-appearance-font-menu>
+ *
+ * @group Chrome Settings Elements
+ * @element settings-appearance-font-menu
+ */
+Polymer({
+  is: 'settings-appearance-font-menu',
+
+  properties: {
+    /**
+     * A text label for the drop-down menu.
+     */
+    label: {
+      type: String,
+    },
+
+    /**
+     * List of options for the drop-down menu.
+     * @type {!Array<{0: (Object|number|string), 1: string,
+     *   2: (string|undefined)}>}
+     */
+    menuOptions: {
+      notify: true,
+      type: Array,
+      value: function() { return []; },
+    },
+
+    /**
+     * A single Preference object being tracked.
+     * @type {?PrefObject}
+     */
+    pref: {
+      type: Object,
+      notify: true,
+    },
+
+    /**
+     * Either loading text or the label for the drop-down menu.
+     * @private
+     */
+    menuLabel_: {
+      type: String,
+      value: function() { return loadTimeData.getString('loading'); },
+    },
+
+     /**
+     * A reverse lookup from the menu value back to the index in the
+     * menuOptions array.
+     * @private {!Object<string, string>}
+     */
+    menuMap_: {
+      type: Object,
+      value: function() { return {}; },
+    },
+
+    /**
+     * The current selected item (an index number as a string).
+     * @private
+     */
+    selected_: {
+      notify: true,
+      observer: 'onSelectedChanged_',
+      type: String,
+    },
+
+    /**
+     * The current selected pref value.
+     * @private
+     */
+    selectedValue_: {
+      type: String,
+    },
+
+    /**
+     * Whether to show the custom font size menu item.
+     * @private
+     */
+    showNotFoundValue_: {
+      type: Boolean,
+    },
+  },
+
+  behaviors: [
+    I18nBehavior
+  ],
+
+  observers: [
+    'checkSetup_(menuOptions, selectedValue_)',
+    'prefChanged_(pref.value)',
+  ],
+
+  /**
+   * Check to see if we have all the pieces needed to enable the control.
+   * @param {!Array<{0: (Object|number|string), 1: string,
+   *   2: (string|undefined)}>} menuOptions
+   * @param {string} selectedValue_
+   * @private
+   */
+  checkSetup_: function(menuOptions, selectedValue_) {
+    if (!this.menuOptions.length) {
+      return;
+    }
+
+    if (!Object.keys(this.menuMap_).length) {
+      // Create a map from index value [0] back to the index i.
+      var result = {};
+      for (var i = 0; i < this.menuOptions.length; ++i)
+        result[JSON.stringify(this.menuOptions[i][0])] = i.toString();
+      this.menuMap_ = result;
+    }
+
+    // We need the menuOptions and the selectedValue_.  They may arrive
+    // at different times (each is asynchronous).
+    this.selected_ = this.getItemIndex(this.selectedValue_);
+    this.menuLabel_ = this.label;
+    this.$.dropdownMenu.disabled = false;
+  },
+
+  /**
+   * @param {string} item A value from the menuOptions array.
+   * @return {string}
+   * @private
+   */
+  getItemIndex: function(item) {
+    var result = this.menuMap_[item];
+    if (result)
+      return result;
+    this.showNotFoundValue_ = true;
+    // The 'not found' item is added as the last of the options.
+    return (this.menuOptions.length).toString();
+  },
+
+  /**
+   * @param {string} index An index into the menuOptions array.
+   * @return {Object|number|string|undefined}
+   * @private
+   */
+  getItemValue: function(index) {
+    if (this.menuOptions.length) {
+      var result = this.menuOptions[index];
+      if (result)
+        return result[0];
+    }
+    return undefined;
+  },
+
+  /**
+   * Pass the selection change to the pref value.
+   * @private
+   */
+  onSelectedChanged_: function() {
+    var prefValue = this.getItemValue(this.selected_);
+    if (prefValue !== undefined) {
+      this.selectedValue_ = JSON.stringify(prefValue);
+      this.set('pref.value', prefValue);
+    }
+  },
+
+  /**
+   * @param {number|string} value A value from the menuOptions array.
+   * @private
+   */
+  prefChanged_: function(value) {
+    this.selectedValue_ = JSON.stringify(value);
+  },
+});
---
+++ chrome/browser/resources/settings/appearance_page/appearance_fonts_page.js
@@ -0,0 +1,227 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * This is the absolute difference maintained between standard and
+ * fixed-width font sizes. http://crbug.com/91922.
+ * @const
+ */
+var SIZE_DIFFERENCE_FIXED_STANDARD = 3;
+
+var FONT_SIZE_RANGE = [
+  9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36,
+  40, 44, 48, 56, 64, 72,
+];
+
+var FONT_SIZE_RANGE_LIMIT = FONT_SIZE_RANGE.length - 1;
+
+var MINIMUM_FONT_SIZE_RANGE = [
+  6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 20, 22, 24
+];
+
+var MINIMUM_FONT_SIZE_RANGE_LIMIT = MINIMUM_FONT_SIZE_RANGE.length - 1;
+
+/**
+ * 'settings-appearance-page' is the settings page containing appearance
+ * settings.
+ *
+ * Example:
+ *
+ *   <settings-appearance-fonts-page prefs="{{prefs}}">
+ *   </settings-appearance-fonts-page>
+ *
+ * @group Chrome Settings Elements
+ * @element settings-appearance-page
+ */
+Polymer({
+  is: 'settings-appearance-fonts-page',
+
+  properties: {
+    /**
+     * The font size used by default.
+     * @private
+     */
+    defaultFontSize_: {
+      type: Number,
+    },
+
+    /**
+     * The value of the font size slider.
+     * @private
+     */
+    fontSizeIndex_: {
+      type: Number,
+    },
+
+    /**
+     * Common font sizes.
+     * @private {!Array<number>}
+     */
+    fontSizeRange_: {
+      readOnly: true,
+      type: Array,
+      value: FONT_SIZE_RANGE,
+    },
+
+    /**
+     * Upper bound of the font size slider.
+     * @private
+     */
+    fontSizeRangeLimit_: {
+      readOnly: true,
+      type: Number,
+      value: MINIMUM_FONT_SIZE_RANGE_LIMIT,
+    },
+
+    /**
+     * The interactive value of the minimum font size slider.
+     * @private
+     */
+    immediateMinimumSizeIndex_: {
+      type: Number,
+    },
+
+    /**
+     * The interactive value of the font size slider.
+     * @private
+     */
+    immediateSizeIndex_: {
+      type: Number,
+    },
+
+    /**
+     * Reasonable, minimum font sizes.
+     * @private {!Array<number>}
+     */
+    minimumFontSizeRange_: {
+      readOnly: true,
+      type: Array,
+      value: MINIMUM_FONT_SIZE_RANGE,
+    },
+
+    /**
+     * Upper bound of the minimum font size slider.
+     * @private
+     */
+    minimumFontSizeRangeLimit_: {
+      readOnly: true,
+      type: Number,
+      value: MINIMUM_FONT_SIZE_RANGE_LIMIT,
+    },
+
+    /**
+     * The font size used at minimum.
+     * @private
+     */
+    minimumFontSize_: {
+      type: Number,
+    },
+
+    /**
+     * The value of the minimum font size slider.
+     * @private
+     */
+    minimumSizeIndex_: {
+      type: Number,
+    },
+
+    /**
+     * Preferences state.
+     */
+    prefs: {
+      type: Object,
+      notify: true,
+    },
+  },
+
+  /**
+   * This is the absolute difference maintained between standard and
+   * fixed-width font sizes. http://crbug.com/91922.
+   * @const
+   */
+  SIZE_DIFFERENCE_FIXED_STANDARD: 3,
+
+  observers: [
+    'fontSizeChanged_(prefs.webkit.webprefs.default_font_size.value)',
+    'minimumFontSizeChanged_(prefs.webkit.webprefs.minimum_font_size.value)',
+  ],
+
+  ready: function() {
+    var self = this;
+    cr.define('Settings', function() {
+      return {
+        setFontsData: function() {
+          return self.setFontsData_.apply(self, arguments);
+        },
+      };
+    });
+    chrome.send('fetchFontsData');
+  },
+
+  /**
+   * @param {number} value The intermediate slider value.
+   * @private
+   */
+  immediateSizeIndexChanged_: function(value) {
+    this.set('prefs.webkit.webprefs.default_font_size.value',
+        this.fontSizeRange_[this.immediateSizeIndex_]);
+  },
+
+  /**
+   * @param {number} value The intermediate slider value.
+   * @private
+   */
+  immediateMinimumSizeIndexChanged_: function(value) {
+    this.set('prefs.webkit.webprefs.minimum_font_size.value',
+        this.minimumFontSizeRange_[this.immediateMinimumSizeIndex_]);
+  },
+
+  /**
+   * @param {!Array<{0: string, 1: (string|undefined), 2: (string|undefined)}>}
+   *   fontList The font menu options.
+   * @param {!Array<{0: string, 1: string}>} encodingList The encoding menu
+   *   options.
+   * @private
+   */
+  setFontsData_: function(fontList, encodingList) {
+    this.$.standardFont.menuOptions = fontList;
+    this.$.serifFont.menuOptions = fontList;
+    this.$.sansSerifFont.menuOptions = fontList;
+    this.$.fixedFont.menuOptions = fontList;
+    this.$.encoding.menuOptions = encodingList;
+  },
+
+  /**
+   * @param {number} value The changed font size slider value.
+   * @private
+   */
+  fontSizeChanged_: function(value) {
+    this.defaultFontSize_ = value;
+    if (!this.$.sizeSlider.dragging) {
+      this.fontSizeIndex_ = this.fontSizeRange_.indexOf(value);
+      this.set('prefs.webkit.webprefs.default_fixed_font_size.value',
+        value - SIZE_DIFFERENCE_FIXED_STANDARD);
+    }
+  },
+
+  /**
+   * @param {number} value The changed font size slider value.
+   * @private
+   */
+  minimumFontSizeChanged_: function(value) {
+    this.minimumFontSize_ = value;
+    if (!this.$.minimumSizeSlider.dragging)
+      this.minimumSizeIndex_ = this.minimumFontSizeRange_.indexOf(value);
+  },
+
+  /**
+   * Creates an html style value.
+   * @param {number} fontSize The font size to use.
+   * @param {string} fontFamily The name of the font family use.
+   * @private
+   */
+  computeStyle_: function(fontSize, fontFamily) {
+    return 'font-size: ' + fontSize + "px; font-family: '" + fontFamily + "';";
+  },
+});
