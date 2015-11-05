959efdac9d90e7ba430c553c687fad04f8f7eab7
phoglund <phoglund@chromium.org>
2015-11-05 07:11:27 -0800

    Re-enabling 720p win browser test.
    BUG=webrtc:4986
    
    Review URL: https://codereview.chromium.org/1414723011
    
    Cr-Commit-Position: refs/heads/master@{#358056}
    
    Former-commit-id: 2800ed2f4bed3d743fa60db28a0d9c67f6181fe8

--- chrome/browser/media/chrome_webrtc_video_quality_browsertest.cc
+++ chrome/browser/media/chrome_webrtc_video_quality_browsertest.cc
@@ -67,14 +67,9 @@ static const struct VideoQualityTestConfig {
   { "360p", 640, 360,
     test::kReferenceFileName360p,
     WebRtcTestBase::kAudioVideoCallConstraints360p },
-    // TODO(phoglund): Temporarily disabled on Windows because 720p has become
-    // very, very slow on Windows (550s).
-    // See https://code.google.com/p/webrtc/issues/detail?id=4986.
-#if !defined(OS_WIN)
     { "720p", 1280, 720,
     test::kReferenceFileName720p,
     WebRtcTestBase::kAudioVideoCallConstraints720p },
-#endif
 };
 
 // Test the video quality of the WebRTC output.
