5fc4751005c24a37a1de59072803269abb2b7e0c
mostynb <mostynb@opera.com>
2015-11-05 07:12:17 -0800

    ozone: evdev: Add missing errno.h #include in event_converter_evdev.cc
    Review URL: https://codereview.chromium.org/1419813009
    
    Cr-Commit-Position: refs/heads/master@{#358057}
    
    Former-commit-id: 57b2ce49938e985cbb5b951055734549eb5589df

--- ui/events/ozone/evdev/event_converter_evdev.cc
+++ ui/events/ozone/evdev/event_converter_evdev.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <errno.h>
 #include <linux/input.h>
 
 #include "ui/events/ozone/evdev/event_converter_evdev.h"
