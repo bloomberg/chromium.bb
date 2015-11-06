9169e2236678a3580c101c1453d27d3c07b50ff9
grt <grt@chromium.org>
2015-11-06 13:56:06 -0800

    Implement UncheckedMalloc and UncheckedCalloc for platforms that stub out EnableTerminationOnOutOfMemory.
    BUG=552017
    R=stuartmorgan@chromium.org
    
    Review URL: https://codereview.chromium.org/1418003009
    
    Cr-Commit-Position: refs/heads/master@{#358415}
    
    Former-commit-id: 5fb37b7f4d3de55e132651c483fb4ba4a8bb3060

--- base/process/memory_stubs.cc
+++ base/process/memory_stubs.cc
@@ -4,6 +4,8 @@
 
 #include "base/process/memory.h"
 
+#include <stdlib.h>
+
 namespace base {
 
 void EnableTerminationOnOutOfMemory() {
@@ -14,6 +16,24 @@ void EnableTerminationOnHeapCorruption() {
 
 bool AdjustOOMScore(ProcessId process, int score) {
   return false;
+}
+
+// UncheckedMalloc and Calloc exist so that platforms making use of
+// EnableTerminationOnOutOfMemory have a way to allocate memory without
+// crashing. This _stubs.cc file is for platforms that do not support
+// EnableTerminationOnOutOfMemory (note the empty implementation above). As
+// such, these two Unchecked.alloc functions need only trivially pass-through to
+// their respective stdlib function since those functions will return null on a
+// failure to allocate.
+
+bool UncheckedMalloc(size_t size, void** result) {
+  *result = malloc(size);
+  return *result != nullptr;
+}
+
+bool UncheckedCalloc(size_t num_items, size_t size, void** result) {
+  *result = calloc(num_items, size);
+  return *result != nullptr;
 }
 
 }  // namespace base
