f5bf44ed85fd54cc74d7d453edb16626a4b57346
hablich <hablich@chromium.org>
2015-11-07 10:37:28 -0800

    Enable reporting per specific pages for new memory histograms
    V8.MemoryHeapUsed and V8.MemoryHeapCommitted didn't support
    reporting per specific pages like Gmail or Inbox. With that
    CL it does.
    
    R=jochen@chromium.org,ulan@chromium.org,asvitkine@chromium.org
    
    Review URL: https://codereview.chromium.org/1429303003
    
    Cr-Commit-Position: refs/heads/master@{#358535}
    
    Former-commit-id: 4598ddcef9994c9c400624bba1718a23a14cd775

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -455,6 +455,8 @@ RenderThreadImpl::HistogramCustomizer::HistogramCustomizer() {
   custom_histograms_.insert("V8.MemoryExternalFragmentationTotal");
   custom_histograms_.insert("V8.MemoryHeapSampleTotalCommitted");
   custom_histograms_.insert("V8.MemoryHeapSampleTotalUsed");
+  custom_histograms_.insert("V8.MemoryHeapUsed");
+  custom_histograms_.insert("V8.MemoryHeapCommitted");
 }
 
 RenderThreadImpl::HistogramCustomizer::~HistogramCustomizer() {}
