8273d9b20df738ffb136bd1e584d80511bf5a2bf
nednguyen <nednguyen@google.com>
2015-11-09 07:46:24 -0800

    Revert of Move finding the reference build over to the binary_manager. (patchset #7 id:140001 of https://codereview.chromium.org/1426403004/ )
    Reason for revert:
    Make list perf tests failed on windows:
    
    (http://build.chromium.org/p/chromium.perf/builders/Win%207%20Perf%20%281%29/builds/4491/steps/steps/logs/stdio)
    
    Traceback (most recent call last):
      <module> at tools\perf\run_benchmark:20
        sys.exit(main())
      main at tools\perf\run_benchmark:17
        return benchmark_runner.main(config)
      main at tools\telemetry\telemetry\benchmark_runner.py:411
        return command().Run(options)
      Run at tools\telemetry\telemetry\benchmark_runner.py:147
        possible_reference_browser = browser_finder.FindBrowser(args)
      Cacher at tools\telemetry\telemetry\decorators.py:34
        cacher.__cache[key] = obj(*args, **kwargs)
      FindBrowser at tools\telemetry\telemetry\internal\browser\browser_finder.py:76
        curr_browsers = finder.FindAllAvailableBrowsers(options, device)
      FindAllAvailableBrowsers at tools\telemetry\telemetry\internal\backends\chrome\desktop_browser_finder.py:229
        'reference_build', arch_name, os_name)
      FetchPath at tools\telemetry\telemetry\internal\util\binary_manager.py:48
        binary_name, '%s_%s' % (platform, arch), try_support_binaries=True)
      FetchPath at tools\telemetry\catapult_base\dependency_manager\dependency_manager.py:113
        path = self._CloudStoragePath(dependency_info)
      _CloudStoragePath at tools\telemetry\catapult_base\dependency_manager\dependency_manager.py:268
        download_path, unzip_location, dependency_info.path_within_archive)
      _UnzipFile at tools\telemetry\catapult_base\dependency_manager\dependency_manager.py:301
        tmp_location = tempfile.mkdtemp(dir=os_tmp_dir)
      mkdtemp at C:\b\depot_tools\python276_bin\lib\tempfile.py:333
        _os.mkdir(file, 0700)
    WindowsError: [Error 3] The system cannot find the path specified: '\\tmp\\tmpgsf8my'
    
    Locals:
      dir    : '\\tmp'
      e      : WindowsError(3, 'The system cannot find the path specified')
      file   : '\\tmp\\tmpgsf8my'
      name   : 'gsf8my'
      names  : <tempfile._RandomNameSequence instance at 0x03C7F170>
      prefix : 'tmp'
      seq    : 0
      suffix : ''
    
    Original issue's description:
    > Move finding the reference build over to the binary_manager.
    >
    > This will update the reference build to 46.0.2490.80 on linux, mac and win.
    > There will be follow up cl's to remove the old reference builds once this
    > cl has landed without issues.
    >
    > BUG=467260
    > CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    >
    > Committed: https://crrev.com/4dbc4e7ad39c6c708b56e38f36c019b665e81e62 [formerly 725ea716157d597ba71958330fcff81a40da3093]
    > Cr-Commit-Position: refs/heads/master@{#358585}
    
    TBR=eakuefner@chromium.org,nednguyen@chromium.org,aiolos@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=467260
    
    Review URL: https://codereview.chromium.org/1424213008
    
    Cr-Commit-Position: refs/heads/master@{#358592}
    
    Former-commit-id: a8ca03e5e7ac091985b2b38bc919072bcb4dff48

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -386,7 +386,6 @@ class SmoothnessGpuRasterizationToughScrollingCases(_Smoothness):
   def Name(cls):
     return 'smoothness.gpu_rasterization.tough_scrolling_cases'
 
-
 @benchmark.Disabled('android')  # http://crbug.com/531593
 class SmoothnessToughImageDecodeCases(_Smoothness):
   page_set = page_sets.ToughImageDecodeCasesPageSet
@@ -394,7 +393,6 @@ class SmoothnessToughImageDecodeCases(_Smoothness):
   @classmethod
   def Name(cls):
     return 'smoothness.tough_image_decode_cases'
-
 
 @benchmark.Disabled('android')  # http://crbug.com/513699
 class SmoothnessImageDecodingCases(_Smoothness):
