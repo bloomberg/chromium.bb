c92e521c6c692d755c9685ccb83b8c652df27ffc
nednguyen <nednguyen@google.com>
2015-11-09 17:13:09 -0800

    [tools/perf] Add dummy benchmark that produces Gaussian value with a known mean and std
    *Perf sherrif: these benchmarks are used for testing
    telemetry integration with perf dashboard & bisect bot
    only, and do not reflect actual performance data of the
    browser. Please feel free to ignore these benchmarks.
    
    BUG=537676
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    The cycle times of these benchmarks are relative short:
    
    On linux desktop:
    real	0m4.617s
    user	0m2.234s
    sys	0m2.325s
    
    On Android nexus 5:
    real	0m16.390s
    user	0m2.471s
    sys	0m4.416s
    
    Review URL: https://codereview.chromium.org/1410683014
    
    Cr-Commit-Position: refs/heads/master@{#358724}
    
    Former-commit-id: a45527d34f4d916952dcf9dcdb258f50fdfb0649

---
+++ tools/perf/benchmarks/dummy_benchmark.py
@@ -0,0 +1,58 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""
+Benchmarks in this file is created for the purpose of testing telemetry
+integration with perf dashboard and bisect bot. The number they produce aren't
+meant to represent any actual performance data of the browser.
+"""
+
+import random
+
+from core import perf_benchmark
+
+from telemetry.value import scalar
+from telemetry.page import page_test
+
+import page_sets
+
+
+class _DummyTest(page_test.PageTest):
+  def __init__(self, avg, std):
+    super(_DummyTest, self).__init__()
+    self._avg = avg
+    self._std = std
+
+  def ValidateAndMeasurePage(self, page, tab, results):
+    results.AddValue(scalar.ScalarValue(
+        page=page,
+        name='gaussian-value', units='ms',
+        value=random.gauss(self._avg, self._std),
+        description=('Random number that follows the Gaussian distribution '
+                     'with mean=%s and std=%s' % (self._avg, self._std))))
+
+
+class _DummyBenchmark(perf_benchmark.PerfBenchmark):
+  page_set = page_sets.DummyStorySet
+
+
+class DummyBenchmarkOne(_DummyBenchmark):
+  """ A low noise benchmark with mean=100 & std=1. """
+
+  def CreatePageTest(self, options):
+    return _DummyTest(100, 1)
+
+  @classmethod
+  def Name(cls):
+    return 'dummy_benchmark.stable_benchmark_1'
+
+
+class DummyBenchmarkTwo(_DummyBenchmark):
+  """ A noisy benchmark with mean=50 & std=20. """
+
+  def CreatePageTest(self, options):
+    return _DummyTest(50, 20)
+
+  @classmethod
+  def Name(cls):
+    return 'dummy_benchmark.noisy_benchmark_1'
