5ea8472d44f27b7c1bcfa8c5633fbcd49fa76c92
dtseng <dtseng@chromium.org>
2015-11-10 11:47:08 -0800

    Make the Next keymap clearer; modify modKeyStr for Next/force next modes.
    KeySequence depends on cvox.ChromeVox.modKeyStr to know what the CV modifier keys are. It strips the Cvox modifiers from the key event when matching. This meant a key map that included meta and shift would not match against a command if the modKeyStr is Search+Shift. As a result, previous jump commands as specified, are broken/never match.
    
    Customize cvox.ChromeVox.modKeyStr as a getter in the same style as cvox.ChromeVox.earcons as a read-only getter.
    
    TEST=Try previous/next jump commands, next/previous element/line, and all other commands in force next mode.
    
    Review URL: https://codereview.chromium.org/1423033011
    
    Cr-Commit-Position: refs/heads/master@{#358879}
    
    Former-commit-id: 1d6ca05525eaeeff6508f98a23ee93309c1a081c

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
@@ -121,6 +121,14 @@ Background = function() {
     }).bind(this)
   });
 
+  Object.defineProperty(cvox.ChromeVox, 'modKeyStr', {
+    get: function() {
+      return (this.mode_ == ChromeVoxMode.CLASSIC || this.mode_ ==
+              ChromeVoxMode.COMPAT) ?
+          'Search+Shift' : 'Search';
+    }.bind(this)
+  });
+
   document.addEventListener(
       'keydown', cvox.ChromeVoxKbHandler.basicKeyDownActionsListener, true);
   cvox.ChromeVoxKbHandler.commandHandler = this.onGotCommand.bind(this);
