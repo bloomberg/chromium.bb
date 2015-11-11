bcea20b5caa381abb2cda18dd9d25bced9a67469
tommi <tommi@chromium.org>
2015-11-11 02:13:13 -0800

    Revert of [ExtensionView] Add extension id whitelisting for ExtensionView. (patchset #3 id:100001 of https://codereview.chromium.org/1419143009/ )
    Reason for revert:
    Reverting due to global variable (sizes regression).
    
    Original issue's description:
    > [ExtensionView] Add extension id whitelisting for ExtensionView.
    >
    > Per discussion with Chrome Eng Review, all extension ids that can be loaded by ExtensionView will be explicitly whitelisted in extension_view_whitelist.cc/h.
    >
    > This change whitelists the extension id used in browser tests, as well extension ids related to Media Router.
    >
    > This also removes some old logging and simplifies the test skeleton extensions.
    >
    > BUG=552207, 552208
    >
    > Committed: https://crrev.com/3fdd6cdfe9155c1a159d2fe33adc444a0f7aa70a [formerly 47fa637f50f0bbf984b14b8dacbf8ed66e552091]
    > Cr-Commit-Position: refs/heads/master@{#359053}
    
    TBR=brettw@chromium.org,apacible@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=552207, 552208
    
    Review URL: https://codereview.chromium.org/1434913002
    
    Cr-Commit-Position: refs/heads/master@{#359077}
    
    Former-commit-id: 169f5e3e4e9a0e0b1daa79d951ba18a7b1bb0c9a

--- extensions/browser/guest_view/extension_view/extension_view_guest.cc
+++ extensions/browser/guest_view/extension_view/extension_view_guest.cc
@@ -11,7 +11,6 @@
 #include "extensions/browser/api/extensions_api_client.h"
 #include "extensions/browser/bad_message.h"
 #include "extensions/browser/guest_view/extension_view/extension_view_constants.h"
-#include "extensions/browser/guest_view/extension_view/whitelist/extension_view_whitelist.h"
 #include "extensions/common/constants.h"
 #include "extensions/common/extension_messages.h"
 #include "extensions/strings/grit/extensions_strings.h"
@@ -72,8 +71,7 @@ void ExtensionViewGuest::CreateWebContents(
   std::string extension_id;
   create_params.GetString(extensionview::kAttributeExtension, &extension_id);
 
-  if (!crx_file::id_util::IdIsValid(extension_id) ||
-      !IsExtensionIdWhitelisted(extension_id)) {
+  if (!crx_file::id_util::IdIsValid(extension_id)) {
     callback.Run(nullptr);
     return;
   }
