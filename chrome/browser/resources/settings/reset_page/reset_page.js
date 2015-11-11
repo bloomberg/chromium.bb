6d260726c3d53fc584c055f8af87d2e915dff069
dpapad <dpapad@chromium.org>
2015-11-10 17:45:21 -0800

    MD Settings: Create reset profile dialog only when needed.
     - Moving reset dialog to its own Polymer element <settings-reset-profile-dialog>.
     - Adding dialog to the DOM, only when user requests to reset the profile.
    
    Before this CL the dialog was always added to the DOM but it was hidden.
    
    BUG=546840
    
    Review URL: https://codereview.chromium.org/1419053008
    
    Cr-Commit-Position: refs/heads/master@{#359008}
    
    Former-commit-id: cb11d007e02ad0b8c90e35e5bcb8e0a6b76975ea

--- chrome/browser/resources/settings/reset_page/reset_page.js
+++ chrome/browser/resources/settings/reset_page/reset_page.js
@@ -21,56 +21,15 @@
 Polymer({
   is: 'settings-reset-page',
 
-  properties: {
-    feedbackInfo_: String,
-  },
-
-  attached: function() {
-    cr.define('SettingsResetPage', function() {
-      return {
-        doneResetting: function() {
-          this.$.resetSpinner.active = false;
-          this.$.resetDialog.close();
-        }.bind(this),
-
-        setFeedbackInfo: function(data) {
-          this.set('feedbackInfo_', data.feedbackInfo);
-          this.async(function() {
-            this.$.resetDialog.center();
-          });
-        }.bind(this),
-      };
-    }.bind(this));
-  },
 
   /** @private */
   onShowDialog_: function() {
-    this.$.resetDialog.open();
-    chrome.send('onShowResetProfileDialog');
-  },
+     var dialog = document.createElement('settings-reset-profile-dialog');
+     this.shadowRoot.appendChild(dialog);
+     dialog.open();
 
-  /** @private */
-  onCancelTap_: function() {
-    this.$.resetDialog.close();
-    chrome.send('onHideResetProfileDialog');
+     dialog.addEventListener('iron-overlay-closed', function(event) {
+       dialog.remove();
+     });
   },
-
-  /** @private */
-  onResetTap_: function() {
-    this.$.resetSpinner.active = true;
-    chrome.send('performResetProfileSettings', [this.$.sendSettings.checked]);
-  },
-
-  /** @private */
-  onLearnMoreTap_: function() {
-    window.open(loadTimeData.getString('resetPageLearnMoreUrl'));
-  },
-
-  /** @private */
-  onSendSettingsChange_: function() {
-    // TODO(dpapad): Update how settings info is surfaced when final mocks
-    // exist.
-    this.$.settings.hidden = !this.$.sendSettings.checked;
-    this.$.resetDialog.center();
-  }
 });
---
+++ chrome/browser/resources/settings/reset_page/reset_profile_dialog.js
@@ -0,0 +1,67 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * 'settings-reset-profile-dialog' is the dialog shown for clearing profile
+ * settings.
+ *
+ * @group Chrome Settings Elements
+ * @element settings-reset-profile-dialog
+ */
+Polymer({
+  is: 'settings-reset-profile-dialog',
+
+  properties: {
+    feedbackInfo_: String,
+  },
+
+  attached: function() {
+    cr.define('SettingsResetPage', function() {
+      return {
+        doneResetting: function() {
+          this.$.resetSpinner.active = false;
+          this.$.dialog.close();
+        }.bind(this),
+
+        setFeedbackInfo: function(data) {
+          this.set('feedbackInfo_', data.feedbackInfo);
+          this.async(function() {
+            this.$.dialog.center();
+          });
+        }.bind(this),
+      };
+    }.bind(this));
+  },
+
+  open: function() {
+    this.$.dialog.open();
+    chrome.send('onShowResetProfileDialog');
+  },
+
+  /** @private */
+  onCancelTap_: function() {
+    this.$.dialog.close();
+    chrome.send('onHideResetProfileDialog');
+  },
+
+  /** @private */
+  onResetTap_: function() {
+    this.$.resetSpinner.active = true;
+    chrome.send('performResetProfileSettings', [this.$.sendSettings.checked]);
+  },
+
+  /** @private */
+  onLearnMoreTap_: function() {
+    window.open(loadTimeData.getString('resetPageLearnMoreUrl'));
+  },
+
+  /** @private */
+  onSendSettingsChange_: function() {
+    // TODO(dpapad): Update how settings info is surfaced when final mocks
+    // exist.
+    this.$.settings.hidden = !this.$.sendSettings.checked;
+    this.$.dialog.center();
+  },
+});
