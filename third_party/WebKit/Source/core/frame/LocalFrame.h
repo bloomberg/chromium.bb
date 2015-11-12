d0fd79ef73779e8cabb247a0054ec6e12cb7e1f2
sigbjornf <sigbjornf@opera.com>
2015-11-11 22:28:49 -0800

    Oilpan: add missing LocalFrame::m_pluginElements annotation.
    TBR=oilpan-reviews
    BUG=553613
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1438143002
    
    Cr-Commit-Position: refs/heads/master@{#359274}
    
    Former-commit-id: 3e263b2c3d78cb49686201045b922f095bc91eda

--- third_party/WebKit/Source/core/frame/LocalFrame.h
+++ third_party/WebKit/Source/core/frame/LocalFrame.h
@@ -235,6 +235,7 @@ private:
     // weak callback, the set itself is not on the heap and the
     // references are bare pointers (rather than WeakMembers.)
     // See LocalFrame::clearWeakMembers().
+    GC_PLUGIN_IGNORE("553613")
     HashSet<HTMLPlugInElement*> m_pluginElements;
 #endif
 
