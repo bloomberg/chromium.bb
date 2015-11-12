63827a4658665dcc26daa98176538af8ab26e68b
cco3 <cco3@chromium.org>
2015-11-12 13:42:43 -0800

    Disable Physical Web UMA
    The Physical Web feature should not rely on the native library, so
    in the future we need to figure out a way to do UMA without it.  For
    now we just disable UMA.
    
    BUG=529962
    
    Review URL: https://codereview.chromium.org/1437223002
    
    Cr-Commit-Position: refs/heads/master@{#359397}
    
    Former-commit-id: 191eb066eb8190ed0df13e9321481ccd8093ecb1

--- chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebUma.java
+++ chrome/android/java/src/org/chromium/chrome/browser/physicalweb/PhysicalWebUma.java
@@ -4,29 +4,20 @@
 
 package org.chromium.chrome.browser.physicalweb;
 
-import org.chromium.base.metrics.RecordHistogram;
-import org.chromium.base.metrics.RecordUserAction;
-
-import java.util.concurrent.TimeUnit;
-
 /**
  * Centralizes UMA data collection for the Physical Web feature.
+ * TODO(cco3) Figure out how to implement these methods without the native library.
  */
 public final class PhysicalWebUma {
     public static void onNotificationPressed() {
-        RecordUserAction.record("PhysicalWeb.NotificationPressed");
     }
 
     public static void onPwsResponse(long duration) {
-        RecordHistogram.recordTimesHistogram("PhysicalWeb.RoundTripTimeMilliseconds", duration,
-                                             TimeUnit.MILLISECONDS);
     }
 
     public static void onUrlsDisplayed(int numUrls) {
-        RecordHistogram.recordCountHistogram("PhysicalWeb.TotalBeaconsDetected", numUrls);
     }
 
     public static void onUrlSelected() {
-        RecordUserAction.record("PhysicalWeb.UrlSelected");
     }
 }
