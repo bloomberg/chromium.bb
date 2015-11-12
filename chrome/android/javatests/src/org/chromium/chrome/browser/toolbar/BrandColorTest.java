33cd7bbb519c14960cca829a93882acf8921fae0
aurimas <aurimas@chromium.org>
2015-11-12 15:23:09 -0800

    Enable theme-color on Chrome tabbed mode for phones by default.
    - Remove about:flags enable-theme-color-in-tabbed-mode
    - Enable theme-color for tabbed mode for phones for all channels.
    
    BUG=482207
    
    Review URL: https://codereview.chromium.org/1434123002
    
    Cr-Commit-Position: refs/heads/master@{#359430}
    
    Former-commit-id: 151479f43c58255e210c1d0f730ebf979ea360f6

--- chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/BrandColorTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/toolbar/BrandColorTest.java
@@ -13,13 +13,11 @@ import android.text.TextUtils;
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.SysUtils;
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
-import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.util.ColorUtils;
 import org.chromium.chrome.test.ChromeActivityTestCaseBase;
 import org.chromium.chrome.test.util.DisableInTabbedMode;
@@ -30,7 +28,6 @@ import org.chromium.content.browser.test.util.CriteriaHelper;
 /**
  * Contains tests for the brand color feature.
  */
-@CommandLineFlags.Add(ChromeSwitches.ENABLE_THEME_COLOR_IN_TABBED_MODE)
 public class BrandColorTest extends ChromeActivityTestCaseBase<ChromeActivity> {
 
     public BrandColorTest() {
