53c03dc1b9953c66067e189f40201cc75365b1a0
dpapad <dpapad@chromium.org>
2015-11-11 19:12:47 -0800

    MD Settings: Remove oblolete files from date_time_page/.
    BUG=546835
    
    Review URL: https://codereview.chromium.org/1439893003
    
    Cr-Commit-Position: refs/heads/master@{#359222}
    
    Former-commit-id: f5579d560d1f47e86ac4ddecf9d32b2bbc377914

--- chrome/browser/resources/settings/date_time_page/demo.js
+++
@@ -1,9 +0,0 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// Wire up the prefs to the date/time page.
-window.addEventListener('polymer-ready', function() {
-  var page = document.querySelector('settings-date-time-page');
-  page.prefs = document.querySelector('settings-prefs');
-});
