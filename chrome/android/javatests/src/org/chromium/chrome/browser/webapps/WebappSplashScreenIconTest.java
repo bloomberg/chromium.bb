4ea7c444bf1682fda06a3edc10d10a6ab76cc098
dgn <dgn@chromium.org>
2015-11-12 04:20:02 -0800

    Mark WebappSplashScreenIconTest#testShowSplashIcon as flaky
    Added in https://codereview.chromium.org/1414873004
    
    TBR=dfalcantara@chromium.org
    BUG=554929
    
    Review URL: https://codereview.chromium.org/1436163003
    
    Cr-Commit-Position: refs/heads/master@{#359304}
    
    Former-commit-id: 567ee4cbb84855641304da750f4865b95eb18054

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappSplashScreenIconTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappSplashScreenIconTest.java
@@ -7,6 +7,7 @@ package org.chromium.chrome.browser.webapps;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.test.FlakyTest;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -38,7 +39,11 @@ public class WebappSplashScreenIconTest extends WebappActivityTestBase {
         startWebappActivity();
     }
 
-    @SmallTest
+    /**
+     * Marked as flaky on http://crbug.com/554929
+     * @SmallTest
+     */
+    @FlakyTest
     @Feature({"Webapps"})
     public void testShowSplashIcon() {
         ViewGroup splashScreen = getActivity().getSplashScreenForTests();
