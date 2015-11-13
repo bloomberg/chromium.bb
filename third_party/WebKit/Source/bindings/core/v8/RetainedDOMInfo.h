3e12810460fa3137f94a192de55bbcd73616d512
sigbjornf <sigbjornf@opera.com>
2015-11-13 05:25:13 -0800

    Oilpan: account for RetainedDOMInfo::m_root temporary Node reference.
    R=haraken
    BUG=553613
    
    Review URL: https://codereview.chromium.org/1440003002
    
    Cr-Commit-Position: refs/heads/master@{#359540}
    
    Former-commit-id: 8c5b27606015cfa4b0f69d6a0accd8632b2a31d8

--- third_party/WebKit/Source/bindings/core/v8/RetainedDOMInfo.h
+++ third_party/WebKit/Source/bindings/core/v8/RetainedDOMInfo.h
@@ -32,6 +32,7 @@
 #define RetainedDOMInfo_h
 
 #include "bindings/core/v8/RetainedObjectInfo.h"
+#include "platform/heap/Handle.h"
 #include <v8-profiler.h>
 
 namespace blink {
@@ -56,6 +57,7 @@ public:
 private:
     // V8 guarantees to keep RetainedObjectInfos alive only during a GC or heap snapshotting round, when renderer
     // doesn't get control. This allows us to use raw pointers.
+    GC_PLUGIN_IGNORE("553613")
     Node* m_root;
 };
 
