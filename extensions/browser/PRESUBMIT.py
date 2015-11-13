a8140c678822c534992a01e1fb653d748f59c554
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2015-11-13 16:42:13 +0100

    Delete PRESUBMIT checks using StrictEnumValueChecker (known false positives)
    See https://groups.google.com/a/chromium.org/d/msg/chromium-dev/DpEpdev5zmc/iOgwpdJaEAAJ for context.
    
    R=finnur@chromium.org
    TBR=thakis
    
    BUG=555540, 546633
    
    Review URL: https://codereview.chromium.org/1439193003 .
    
    Cr-Commit-Position: refs/heads/master@{#359554}
    
    Former-commit-id: 64749d907f25d5cb7241b90be42a22d52b4c02bc

--- extensions/browser/PRESUBMIT.py
+++ extensions/browser/PRESUBMIT.py
@@ -8,39 +8,7 @@ See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details on the presubmit API built into depot_tools.
 """
 
-import sys
-
-def _CreateHistogramValueChecker(input_api, output_api, path):
-  original_sys_path = sys.path
-
-  try:
-    sys.path.append(input_api.os_path.join(
-        input_api.PresubmitLocalPath(), '..', '..', 'tools',
-        'strict_enum_value_checker'))
-    from strict_enum_value_checker import StrictEnumValueChecker
-  finally:
-    sys.path = original_sys_path
-
-  return StrictEnumValueChecker(input_api, output_api,
-      start_marker='enum HistogramValue {', end_marker='  // Last entry:',
-      path=path)
-
-
-def _RunHistogramValueCheckers(input_api, output_api):
-  results = []
-  histogram_paths = ('extensions/browser/extension_event_histogram_value.h',
-                     'extensions/browser/extension_function_histogram_value.h')
-  for path in histogram_paths:
-    results += _CreateHistogramValueChecker(input_api, output_api, path).Run()
-  return results
-
-
 def CheckChangeOnUpload(input_api, output_api):
   results = []
-  results += _RunHistogramValueCheckers(input_api, output_api)
   results += input_api.canned_checks.CheckPatchFormatted(input_api, output_api)
   return results
-
-
-def CheckChangeOnCommit(input_api, output_api):
-  return _RunHistogramValueCheckers(input_api, output_api)
