3873fd1767d696c845ae505db6ef9986914c0ec3
mkwst <mkwst@chromium.org>
2015-11-17 08:24:24 -0800

    Implement 'URLSearchParams'
    This patch implements the 'URLSearchParams' interface (https://url.spec.whatwg.org/#interface-urlsearchparams), extracted
    from Sigbjorn's excellent https://codereview.chromium.org/143313002/.
    The bits of that patch which integrate 'URLSearchParams' with 'URL' and
    'URLUtils' are dependent on Node moving to Oilpan, and will be added in
    a subsequent patch.
    
    Intent to Implement and Ship: https://groups.google.com/a/chromium.org/d/msg/blink-dev/grHZDbldP04/JdsoQ169AQAJ
    
    BUG=303152
    
    Review URL: https://codereview.chromium.org/1442643008
    
    Cr-Commit-Position: refs/heads/master@{#360086}
    
    Former-commit-id: dff906eef78f37d1b44bb67785440366245ddb9d

---
+++ third_party/WebKit/Source/core/dom/URLSearchParams.cpp
@@ -0,0 +1,188 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "config.h"
+#include "core/dom/URLSearchParams.h"
+
+#include "platform/weborigin/KURL.h"
+#include "wtf/text/StringBuilder.h"
+#include "wtf/text/TextEncoding.h"
+
+namespace blink {
+
+namespace {
+
+class URLSearchParamsIterationSource final : public PairIterable<String, String>::IterationSource {
+public:
+    URLSearchParamsIterationSource(Vector<std::pair<String, String>> params) : m_params(params), m_current(0) { }
+
+    bool next(ScriptState*, String& key, String& value, ExceptionState&) override
+    {
+        if (m_current >= m_params.size())
+            return false;
+
+        key = m_params[m_current].first;
+        value = m_params[m_current].second;
+        m_current++;
+        return true;
+    }
+
+private:
+    Vector<std::pair<String, String>> m_params;
+    size_t m_current;
+};
+
+} // namespace
+
+URLSearchParams* URLSearchParams::create(const URLSearchParamsInit& init)
+{
+    if (init.isUSVString())
+        return new URLSearchParams(init.getAsUSVString());
+    if (init.isURLSearchParams())
+        return new URLSearchParams(init.getAsURLSearchParams());
+
+    ASSERT(init.isNull());
+    return new URLSearchParams(String());
+}
+
+URLSearchParams::URLSearchParams(const String& queryString)
+{
+    if (!queryString.isEmpty())
+        setInput(queryString);
+}
+
+URLSearchParams::URLSearchParams(URLSearchParams* searchParams)
+{
+    ASSERT(searchParams);
+    m_params = searchParams->m_params;
+}
+
+URLSearchParams::~URLSearchParams()
+{
+}
+
+static String decodeString(String input)
+{
+    return decodeURLEscapeSequences(input.replace('+', ' '));
+}
+
+void URLSearchParams::setInput(const String& queryString)
+{
+    ASSERT(m_params.isEmpty());
+    size_t start = 0;
+    size_t queryStringLength = queryString.length();
+    while (start < queryStringLength) {
+        size_t nameStart = start;
+        size_t nameValueEnd = queryString.find('&', start);
+        if (nameValueEnd == kNotFound)
+            nameValueEnd = queryStringLength;
+        if (nameValueEnd > start) {
+            size_t endOfName = queryString.find('=', start);
+            if (endOfName == kNotFound || endOfName > nameValueEnd)
+                endOfName = nameValueEnd;
+            String name = decodeString(queryString.substring(nameStart, endOfName - nameStart));
+            String value;
+            if (endOfName != nameValueEnd)
+                value = decodeString(queryString.substring(endOfName + 1, nameValueEnd - endOfName - 1));
+            if (value.isNull())
+                value = "";
+            m_params.append(std::make_pair(name, value));
+        }
+        start = nameValueEnd + 1;
+    }
+}
+static String encodeString(const String& input)
+{
+    return encodeWithURLEscapeSequences(input).replace("%20", "+");
+}
+
+String URLSearchParams::toString() const
+{
+    StringBuilder result;
+    for (size_t i = 0; i < m_params.size(); ++i) {
+        if (i)
+            result.append('&');
+        result.append(encodeString(m_params[i].first));
+        result.append('=');
+        result.append(encodeString(m_params[i].second));
+    }
+    return result.toString();
+}
+
+void URLSearchParams::append(const String& name, const String& value)
+{
+    m_params.append(std::make_pair(name, value));
+}
+
+void URLSearchParams::deleteAllWithName(const String& name)
+{
+    for (size_t i = 0; i < m_params.size();) {
+        if (m_params[i].first == name)
+            m_params.remove(i);
+        else
+            i++;
+    }
+}
+
+String URLSearchParams::get(const String& name) const
+{
+    for (const auto& param : m_params) {
+        if (param.first == name)
+            return param.second;
+    }
+    return String();
+}
+
+Vector<String> URLSearchParams::getAll(const String& name) const
+{
+    Vector<String> result;
+    for (const auto& param : m_params) {
+        if (param.first == name)
+            result.append(param.second);
+    }
+    return result;
+}
+
+bool URLSearchParams::has(const String& name) const
+{
+    for (const auto& param : m_params) {
+        if (param.first == name)
+            return true;
+    }
+    return false;
+}
+
+void URLSearchParams::set(const String& name, const String& value)
+{
+    bool foundMatch = false;
+    for (size_t i = 0; i < m_params.size();) {
+        // If there are any name-value whose name is 'name', set
+        // the value of the first such name-value pair to 'value'
+        // and remove the others.
+        if (m_params[i].first == name) {
+            if (!foundMatch) {
+                m_params[i++].second = value;
+                foundMatch = true;
+            } else {
+                m_params.remove(i);
+            }
+        } else {
+            i++;
+        }
+    }
+    // Otherwise, append a new name-value pair to the list.
+    if (!foundMatch)
+        append(name, value);
+}
+
+DEFINE_TRACE(URLSearchParams)
+{
+}
+
+PairIterable<String, String>::IterationSource* URLSearchParams::startIteration(ScriptState*, ExceptionState&)
+{
+    return new URLSearchParamsIterationSource(m_params);
+}
+
+} // namespace blink
---
+++ third_party/WebKit/Source/core/dom/URLSearchParams.h
@@ -0,0 +1,58 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef URLSearchParams_h
+#define URLSearchParams_h
+
+#include "bindings/core/v8/Iterable.h"
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "bindings/core/v8/UnionTypesCore.h"
+#include "platform/heap/Handle.h"
+#include "wtf/Forward.h"
+#include "wtf/text/WTFString.h"
+#include <utility>
+
+namespace blink {
+
+class ExceptionState;
+
+typedef USVStringOrURLSearchParams URLSearchParamsInit;
+
+class CORE_EXPORT URLSearchParams final : public GarbageCollectedFinalized<URLSearchParams>, public ScriptWrappable, public PairIterable<String, String> {
+    DEFINE_WRAPPERTYPEINFO();
+
+public:
+    // TODO(mkwst): We should support integration with URLUtils, as explored in
+    // https://codereview.chromium.org/143313002/. That approach is totally
+    // reasonable, but relies on Node switching to Oilpan. Sigbjorn assures me
+    // that this will happen Real Soon Now(tm).
+    static URLSearchParams* create(const URLSearchParamsInit&);
+
+    // TODO(mkwst): ScriptWrappable doesn't have a destructor with Oilpan, so this
+    // won't need to be virtual once that's the default.
+    virtual ~URLSearchParams();
+
+    // URLSearchParams interface methods
+    String toString() const;
+    void append(const String& name, const String& value);
+    void deleteAllWithName(const String&);
+    String get(const String&) const;
+    Vector<String> getAll(const String&) const;
+    bool has(const String&) const;
+    void set(const String& name, const String& value);
+    void setInput(const String&);
+
+    DECLARE_TRACE();
+
+private:
+    explicit URLSearchParams(const String&);
+    explicit URLSearchParams(URLSearchParams*);
+    Vector<std::pair<String, String>> m_params;
+
+    IterationSource* startIteration(ScriptState*, ExceptionState&) override;
+};
+
+} // namespace blink
+
+#endif // URLSearchParams_h
