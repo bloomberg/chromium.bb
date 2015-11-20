d67a59cb5b23e6db5942e4de8b8c1708e9d573df
mdjones <mdjones@chromium.org>
2015-11-20 15:31:13 -0800

    Wrap closePanel function for Reader Mode
    This change has two purposes; handle the cases when there is no
    panel to close and make testing easier by overriding close
    functionality without creating a test panel.
    
    BUG=557526
    
    Review URL: https://codereview.chromium.org/1460753002
    
    Cr-Commit-Position: refs/heads/master@{#360950}
    
    Former-commit-id: 895c1391fb2e298393e49761411993ba08639fc5

--- chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModeManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/dom_distiller/ReaderModeManager.java
@@ -156,7 +156,7 @@ public class ReaderModeManager extends TabModelSelectorTabObserver
 
     @Override
     public void onHidden(Tab tab) {
-        mReaderModePanel.closePanel(StateChangeReason.UNKNOWN, false);
+        closeReaderPanel(StateChangeReason.UNKNOWN, false);
     }
 
     @Override
@@ -201,7 +201,7 @@ public class ReaderModeManager extends TabModelSelectorTabObserver
             if (DomDistillerUrlUtils.isDistilledPage(tab.getUrl())) {
                 tabInfo.setStatus(STARTED);
                 mReaderModePageUrl = tab.getUrl();
-                mReaderModePanel.closePanel(StateChangeReason.CONTENT_CHANGED, true);
+                closeReaderPanel(StateChangeReason.CONTENT_CHANGED, true);
             }
             // Make sure there is a distillability delegate set on the WebContents.
             setDistillabilityCallback();
@@ -212,12 +212,10 @@ public class ReaderModeManager extends TabModelSelectorTabObserver
 
     @Override
     public void onToggleFullscreenMode(Tab tab, boolean enable) {
-        if (mReaderModePanel == null) return;
-
         // Temporarily hide the reader mode panel while fullscreen is enabled.
         if (enable) {
             mIsFullscreenModeEntered = true;
-            mReaderModePanel.closePanel(StateChangeReason.FULLSCREEN_ENTERED, false);
+            closeReaderPanel(StateChangeReason.FULLSCREEN_ENTERED, false);
         } else {
             mIsFullscreenModeEntered = false;
             requestReaderPanelShow(StateChangeReason.FULLSCREEN_EXITED);
@@ -229,16 +227,16 @@ public class ReaderModeManager extends TabModelSelectorTabObserver
     @Override
     public void onAddInfoBar(InfoBarContainer container, InfoBar infoBar, boolean isFirst) {
         // Temporarily hides the reader mode button while the infobars are shown.
-        if (isFirst && mReaderModePanel != null) {
+        if (isFirst) {
             mIsInfobarContainerShown = true;
-            mReaderModePanel.closePanel(StateChangeReason.INFOBAR_SHOWN, false);
+            closeReaderPanel(StateChangeReason.INFOBAR_SHOWN, false);
         }
     }
 
     @Override
     public void onRemoveInfoBar(InfoBarContainer container, InfoBar infoBar, boolean isLast) {
         // Re-shows the reader mode button if necessary once the infobars are dismissed.
-        if (isLast && mReaderModePanel != null) {
+        if (isLast) {
             mIsInfobarContainerShown = false;
             requestReaderPanelShow(StateChangeReason.INFOBAR_HIDDEN);
         }
@@ -304,7 +302,7 @@ public class ReaderModeManager extends TabModelSelectorTabObserver
                 mTabStatusMap.get(readerTabId).setUrl(validatedUrl);
                 if (DomDistillerUrlUtils.isDistilledPage(validatedUrl)) {
                     mTabStatusMap.get(readerTabId).setStatus(STARTED);
-                    mReaderModePanel.closePanel(StateChangeReason.UNKNOWN, true);
+                    closeReaderPanel(StateChangeReason.UNKNOWN, true);
                     mReaderModePageUrl = validatedUrl;
                 }
             }
@@ -328,7 +326,7 @@ public class ReaderModeManager extends TabModelSelectorTabObserver
                 mReaderModePageUrl = null;
                 if (mTabStatusMap.containsKey(readerTabId)
                         && mTabStatusMap.get(readerTabId).getStatus() != POSSIBLE) {
-                    mReaderModePanel.closePanel(StateChangeReason.UNKNOWN, false);
+                    closeReaderPanel(StateChangeReason.UNKNOWN, false);
                 } else {
                     requestReaderPanelShow(StateChangeReason.UNKNOWN);
                 }
@@ -356,12 +354,22 @@ public class ReaderModeManager extends TabModelSelectorTabObserver
     }
 
     /**
+     * A wrapper for the close method of the Reader Mode panel that checks for null and can be
+     * overridden for testing.
+     * @param reason The reason the panel is closing.
+     * @param animate True if the panel should animate closed.
+     */
+    private void closeReaderPanel(StateChangeReason reason, boolean animate) {
+        if (mReaderModePanel == null) return;
+        mReaderModePanel.closePanel(reason, animate);
+    }
+
+    /**
      * Orientation change event handler. Simply close the panel.
      */
     public void onOrientationChange() {
-        if (mReaderModePanel == null) return;
         // Close to reset the panel then immediately show again.
-        mReaderModePanel.closePanel(StateChangeReason.UNKNOWN, false);
+        closeReaderPanel(StateChangeReason.UNKNOWN, false);
         requestReaderPanelShow(StateChangeReason.UNKNOWN);
     }
 
