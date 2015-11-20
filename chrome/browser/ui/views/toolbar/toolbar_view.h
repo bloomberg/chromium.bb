9c1d88dd702490a537bc87936c2a82fca6cf6b35
tdanderson <tdanderson@chromium.org>
2015-11-20 06:38:55 -0800

    Remove unused ToolbarView::kVertSpacing
    Remove unused constant
    ToolbarView::kVertSpacing.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/1460913006
    
    Cr-Commit-Position: refs/heads/master@{#360816}
    
    Former-commit-id: 459e03113d2f7d797956edb405faca3c7e8c0864

--- chrome/browser/ui/views/toolbar/toolbar_view.h
+++ chrome/browser/ui/views/toolbar/toolbar_view.h
@@ -152,10 +152,6 @@ class ToolbarView : public views::AccessiblePaneView,
     // The apparent horizontal space between most items, and the vertical
     // padding above and below them.
     kStandardSpacing = 3,
-
-    // The top of the toolbar has an edge we have to skip over in addition to
-    // the standard spacing.
-    kVertSpacing = 5,
   };
 
  protected:
