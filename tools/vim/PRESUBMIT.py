1f0e55f4f6c630ad4c979875e9f98e63df12d6af
asanka <asanka@chromium.org>
2015-11-20 18:08:03 -0800

    [tools/vim] Only run PRESUBMIT unittests for changes that touch YCM config.
    BUG=554183
    
    Review URL: https://codereview.chromium.org/1468513002
    
    Cr-Commit-Position: refs/heads/master@{#360985}
    
    Former-commit-id: 167e1ea5c14c634c118c5155b5ab9daaab2902ee

--- tools/vim/PRESUBMIT.py
+++ tools/vim/PRESUBMIT.py
@@ -8,5 +8,23 @@ Runs Python unit tests in /tools/vim/tests on upload.
 """
 
 def CheckChangeOnUpload(input_api, output_api):
-  return input_api.canned_checks.RunUnitTestsInDirectory(
-      input_api, output_api, 'tests', whitelist=r'.*test.py')
+  results = []
+
+  # affected_files is list of files affected by this change. The paths are
+  # relative to the directory containing PRESUBMIT.py.
+  affected_files = [
+      input_api.os_path.relpath(f, input_api.PresubmitLocalPath())
+      for f in input_api.AbsoluteLocalPaths()]
+
+  # Run the chromium.ycm_extra_conf_unittest test if the YCM config file is
+  # changed or if any change is affecting the tests directory. This specific
+  # test requires access to 'ninja' and hasn't been tested on platforms other
+  # than Linux.
+  if 'chromium.ycm_extra_conf.py' in affected_files or \
+      any([input_api.re.match(r'tests(/|\\)',f) for f in affected_files]):
+    results += input_api.RunTests(
+        input_api.canned_checks.GetUnitTests(
+            input_api, output_api,
+            ['tests/chromium.ycm_extra_conf_unittest.py']))
+
+  return results
