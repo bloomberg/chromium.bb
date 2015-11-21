3a0bf28b2024b783e4038f2cf03c8564797df3f4
haraken <haraken@chromium.org>
2015-11-21 01:02:45 -0800

    Revert of Switch V8HiddenValue from hidden values to privates (patchset #7 id:120001 of https://codereview.chromium.org/1416053012/ )
    Reason for revert:
    This CL broke tests in oilpan builds.
    
    https://storage.googleapis.com/chromium-layout-test-archives/WebKit_Linux_Oilpan_ASAN/5177/layout-test-results/results.html
    
    Original issue's description:
    > Switch V8HiddenValue from hidden values to privates
    >
    > Privates are like ES6 symbols, but are not accesible by JavaScript.
    > Hidden values on the other hand will be deleted form the API soon.
    >
    > BUG=none
    > R=haraken@chromium.org
    >
    > Committed: https://crrev.com/6996e742261e0b02df30304a96a5f55809adc4e0 [formerly b1802c0fc28635a50e7f55e40c71e69281cc85f3]
    > Cr-Commit-Position: refs/heads/master@{#360786}
    
    TBR=vogelheim@chromium.org,sigbjornf@opera.com,jochen@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=none
    
    Review URL: https://codereview.chromium.org/1466733006
    
    Cr-Commit-Position: refs/heads/master@{#361016}
    
    Former-commit-id: dfb67af2fb65ecb0daec0498140ad6d7a9a0ea59

--- third_party/WebKit/Source/bindings/core/v8/V8HiddenValue.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8HiddenValue.cpp
@@ -25,25 +25,17 @@ V8_HIDDEN_VALUES(V8_DEFINE_METHOD);
 
 v8::Local<v8::Value> V8HiddenValue::getHiddenValue(ScriptState* scriptState, v8::Local<v8::Object> object, v8::Local<v8::String> key)
 {
-    v8::Local<v8::Context> context = scriptState->context();
-    v8::Local<v8::Private> privateKey = v8::Private::ForApi(scriptState->isolate(), key);
-    v8::Local<v8::Value> value;
-    // Callsites interpret an empty handle has absence of a result.
-    if (!v8CallBoolean(object->HasPrivate(context, privateKey)))
-        return v8::Local<v8::Value>();
-    if (object->GetPrivate(context, privateKey).ToLocal(&value))
-        return value;
-    return v8::Local<v8::Value>();
+    return object->GetHiddenValue(key);
 }
 
 bool V8HiddenValue::setHiddenValue(ScriptState* scriptState, v8::Local<v8::Object> object, v8::Local<v8::String> key, v8::Local<v8::Value> value)
 {
-    return v8CallBoolean(object->SetPrivate(scriptState->context(), v8::Private::ForApi(scriptState->isolate(), key), value));
+    return object->SetHiddenValue(key, value);
 }
 
 bool V8HiddenValue::deleteHiddenValue(ScriptState* scriptState, v8::Local<v8::Object> object, v8::Local<v8::String> key)
 {
-    return v8CallBoolean(object->DeletePrivate(scriptState->context(), v8::Private::ForApi(scriptState->isolate(), key)));
+    return object->DeleteHiddenValue(key);
 }
 
 v8::Local<v8::Value> V8HiddenValue::getHiddenValueFromMainWorldWrapper(ScriptState* scriptState, ScriptWrappable* wrappable, v8::Local<v8::String> key)
