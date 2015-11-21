529ce0fb7939c2fef47bfd0b58f2be0b4bf2bc5c
derekjchow <derekjchow@chromium.org>
2015-11-20 19:45:13 -0800

    [Chromecast] Add setters to CastSysInfoDummy.
    Enables changing values in CastSysInfoDummy for unit testing.
    
    R=halliwell@chromium.org,slan@chromium.org
    
    Review URL: https://codereview.chromium.org/1466003003
    
    Cr-Commit-Position: refs/heads/master@{#360996}
    
    Former-commit-id: f4f2c9f991147df31b60ecbef9526c4ed7f0b784

--- chromecast/base/cast_sys_info_dummy.h
+++ chromecast/base/cast_sys_info_dummy.h
@@ -12,6 +12,7 @@
 
 namespace chromecast {
 
+// Dummy implementation of CastSysInfo. Fields can be overwritten for test.
 class CastSysInfoDummy : public CastSysInfo {
  public:
   CastSysInfoDummy();
@@ -35,7 +36,42 @@ class CastSysInfoDummy : public CastSysInfo {
   std::string GetGlRenderer() override;
   std::string GetGlVersion() override;
 
+  void SetBuildTypeForTesting(BuildType build_type);
+  void SetSystemReleaseChannelForTesting(
+      const std::string& system_release_channel);
+  void SetSerialNumberForTesting(const std::string& serial_number);
+  void SetProductNameForTesting(const std::string& product_name);
+  void SetDeviceModelForTesting(const std::string& device_model);
+  void SetBoardNameForTesting(const std::string& board_name);
+  void SetBoardRevisionForTesting(const std::string& board_revision);
+  void SetManufacturerForTesting(const std::string& manufacturer);
+  void SetSystemBuildNumberForTesting(const std::string& system_build_number);
+  void SetFactoryCountryForTesting(const std::string& factory_country);
+  void SetFactoryLocaleForTesting(const std::string& factory_locale);
+  void SetWifiInterfaceForTesting(const std::string& wifi_interface);
+  void SetApInterfaceForTesting(const std::string& ap_interface);
+  void SetGlVendorForTesting(const std::string& gl_vendor);
+  void SetGlRendererForTesting(const std::string& gl_renderer);
+  void SetGlVersionForTesting(const std::string& gl_version);
+
  private:
+  BuildType build_type_;
+  std::string system_release_channel_;
+  std::string serial_number_;
+  std::string product_name_;
+  std::string device_model_;
+  std::string board_name_;
+  std::string board_revision_;
+  std::string manufacturer_;
+  std::string system_build_number_;
+  std::string factory_country_;
+  std::string factory_locale_;
+  std::string wifi_interface_;
+  std::string ap_interface_;
+  std::string gl_vendor_;
+  std::string gl_renderer_;
+  std::string gl_version_;
+
   DISALLOW_COPY_AND_ASSIGN(CastSysInfoDummy);
 };
 
