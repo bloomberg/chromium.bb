5ff22497fc44ecc2ff6521a7572eb74a93355eb2
sdefresne <sdefresne@chromium.org>
2015-11-23 09:27:40 -0800

    Provider API for UpdatableConfig.
    BUG=475442
    
    Review URL: https://codereview.chromium.org/1471783002
    
    Cr-Commit-Position: refs/heads/master@{#361131}
    
    Former-commit-id: e102ab8c09f75131bd87d86830078219a0f94b0b

--- ios/public/test/test_chrome_browser_provider.h
+++ ios/public/test/test_chrome_browser_provider.h
@@ -12,6 +12,7 @@ namespace ios {
 
 class FakeProfileOAuth2TokenServiceIOSProvider;
 class FakeStringProvider;
+class TestUpdatableResourceProvider;
 
 class TestChromeBrowserProvider : public ChromeBrowserProvider {
  public:
@@ -23,6 +24,7 @@ class TestChromeBrowserProvider : public ChromeBrowserProvider {
 
   // ChromeBrowserProvider:
   ChromeIdentityService* GetChromeIdentityService() override;
+  UpdatableResourceProvider* GetUpdatableResourceProvider() override;
   StringProvider* GetStringProvider() override;
 
   // Returns the string provider as a |FakeStringProvider|.
@@ -31,6 +33,7 @@ class TestChromeBrowserProvider : public ChromeBrowserProvider {
  private:
   scoped_ptr<ChromeIdentityService> chrome_identity_service_;
   scoped_ptr<FakeStringProvider> string_provider_;
+  scoped_ptr<TestUpdatableResourceProvider> test_updatable_resource_provider_;
 
   DISALLOW_COPY_AND_ASSIGN(TestChromeBrowserProvider);
 };
--- ios/public/test/test_chrome_browser_provider.mm
+++ ios/public/test/test_chrome_browser_provider.mm
@@ -7,12 +7,14 @@
 #include "base/logging.h"
 #include "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
 #include "ios/public/test/fake_string_provider.h"
+#import "ios/public/test/test_updatable_resource_provider.h"
 
 namespace ios {
 
 TestChromeBrowserProvider::TestChromeBrowserProvider()
     : chrome_identity_service_(new ios::ChromeIdentityService),
-      string_provider_(new FakeStringProvider) {}
+      string_provider_(new FakeStringProvider),
+      test_updatable_resource_provider_(new TestUpdatableResourceProvider) {}
 
 TestChromeBrowserProvider::~TestChromeBrowserProvider() {
 }
@@ -30,6 +32,11 @@ ChromeIdentityService* TestChromeBrowserProvider::GetChromeIdentityService() {
 
 StringProvider* TestChromeBrowserProvider::GetStringProvider() {
   return string_provider_.get();
+}
+
+UpdatableResourceProvider*
+TestChromeBrowserProvider::GetUpdatableResourceProvider() {
+  return test_updatable_resource_provider_.get();
 }
 
 FakeStringProvider*
