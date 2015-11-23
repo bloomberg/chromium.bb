2f24c8c60b3d35b8f63ba86aa679288ed9e3db6f
rune <rune@opera.com>
2015-11-23 08:30:03 -0800

    Remove extraneous SubtreeStyleChange for min/maxlength changes.
    Validation already takes care of :valid/:invalid changes through
    invalidation sets. This reduces the number of elements being recalculated
    and gets rid of a SubtreeStyleChange which relies on sibling tree recalcs.
    
    R=tkent@chromium.org
    BUG=557440
    
    Review URL: https://codereview.chromium.org/1458363003
    
    Cr-Commit-Position: refs/heads/master@{#361123}
    
    Former-commit-id: 81fb8abcd74847f62bf89e45e6dda0c7464e08c4

--- third_party/WebKit/Source/core/html/HTMLInputElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLInputElement.cpp
@@ -1703,7 +1703,6 @@ void HTMLInputElement::parseMaxLengthAttribute(const AtomicString& value)
     m_maxLength = maxLength;
     if (oldMaxLength != maxLength)
         updateValueIfNeeded();
-    setNeedsStyleRecalc(SubtreeStyleChange, StyleChangeReasonForTracing::fromAttribute(maxlengthAttr));
     setNeedsValidityCheck();
 }
 
@@ -1715,7 +1714,6 @@ void HTMLInputElement::parseMinLengthAttribute(const AtomicString& value)
     if (minLength < 0)
         minLength = 0;
     m_minLength = minLength;
-    setNeedsStyleRecalc(SubtreeStyleChange, StyleChangeReasonForTracing::fromAttribute(minlengthAttr));
     setNeedsValidityCheck();
 }
 
