0d13ef89e5106cd27b7478ec908eb8c4f92dedeb
maxbogue <maxbogue@chromium.org>
2015-11-23 12:16:06 -0800

    [Sync] Re-enable testAndroidSyncSettingsPostsNotifications.
    BUG=527856
    
    Review URL: https://codereview.chromium.org/1434413002
    
    Cr-Commit-Position: refs/heads/master@{#361164}
    
    Former-commit-id: 0e8c02ee3e3619d98ae48aceac93456c8b271af0

--- sync/android/javatests/src/org/chromium/sync/AndroidSyncSettingsTest.java
+++ sync/android/javatests/src/org/chromium/sync/AndroidSyncSettingsTest.java
@@ -11,7 +11,6 @@ import android.test.InstrumentationTestCase;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.ThreadUtils;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.sync.AndroidSyncSettings.AndroidSyncSettingsObserver;
 import org.chromium.sync.signin.AccountManagerHelper;
@@ -312,10 +311,8 @@ public class AndroidSyncSettingsTest extends InstrumentationTestCase {
                 AndroidSyncSettings.getContractAuthority(mContext));
     }
 
-    /*@SmallTest
-    @Feature({"Sync"})*/
-    // http://crbug.com/527856
-    @DisabledTest
+    @SmallTest
+    @Feature({"Sync"})
     public void testAndroidSyncSettingsPostsNotifications() throws InterruptedException {
         // Turn on syncability.
         mSyncContentResolverDelegate.setMasterSyncAutomatically(true);
