d13c22c2dd20c3ea83499fc38c2573082d7266c0
jmadill <jmadill@chromium.org>
2015-11-24 15:18:18 -0800

    Add more WebGL CTS supressions.
    Both Mac and Linux/AMD are failing two tests whose suppressions were
    recently lifted. Reinstate those failing expectations.
    
    BUG=483282,295792
    TBR=kbr@chromium.org,bajones@chromium.org,zmo@chromium.org
    
    Review URL: https://codereview.chromium.org/1476613002
    
    Cr-Commit-Position: refs/heads/master@{#361485}
    
    Former-commit-id: 2ace562d2f89d852201d1558c5f8508d981da138

--- content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
@@ -216,6 +216,12 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['mac'], bug=483282)
     self.Fail('conformance2/textures/misc/texture-npot.html',
         ['mac'], bug=483282)
+    self.Fail('conformance2/textures/misc/tex-storage-compressed-formats.html',
+        ['mac'], bug=295792)
+
+    # Mac 10.9 only.
+    self.Fail('conformance2/textures/misc/tex-mipmap-levels.html',
+        ['mavericks'], bug=483282)
 
     # Linux only.
     self.Skip('deqp/functional/gles3/shaderswitch.html',
@@ -245,7 +251,10 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['linux', 'amd'], bug=483282)
     self.Fail('conformance2/renderbuffers/framebuffer-texture-layer.html',
         ['linux', 'amd'], bug=295792)
-
+    self.Fail('conformance2/textures/misc/tex-mipmap-levels.html',
+        ['linux', 'amd'], bug=483282)
+    self.Fail('conformance2/textures/misc/tex-storage-compressed-formats.html',
+        ['linux', 'amd'], bug=295792)
 
     # Conflicting expectations to test that the
     # "Expectations Have No collisions" unittest works.
