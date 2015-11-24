3304627a55e265a02c8285cf1808c2cc48b971d1
aizatsky <aizatsky@chromium.org>
2015-11-24 12:23:08 -0800

    Initializing ICU in ftp_directory_listing_fuzzer.
    This fixes the current crash we are seeing.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1473513002
    
    Cr-Commit-Position: refs/heads/master@{#361426}
    
    Former-commit-id: dbf4b22d7bf917099479b739e71dd12cdabe8296

--- testing/libfuzzer/fuzzers/ftp_directory_listing_fuzzer.cc
+++ testing/libfuzzer/fuzzers/ftp_directory_listing_fuzzer.cc
@@ -5,8 +5,22 @@
 #include <string>
 #include <vector>
 
+#include "base/at_exit.h"
+#include "base/i18n/icu_util.h"
 #include "base/time/time.h"
 #include "net/ftp/ftp_directory_listing_parser.h"
+
+struct TestCase {
+  TestCase() {
+    CHECK(base::i18n::InitializeICU());
+  }
+
+  // used by ICU integration.
+  base::AtExitManager at_exit_manager;
+};
+
+TestCase* test_case = new TestCase();
+
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const unsigned char* data,
