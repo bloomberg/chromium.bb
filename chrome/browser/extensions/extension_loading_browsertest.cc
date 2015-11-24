4504bb15fe3965a31974b7bf979e9fec370e2202
magjed <magjed@chromium.org>
2015-11-24 04:41:14 -0800

    Disable KeepAliveWithDevToolsOpenOnReload test on Win XP
    This test is flaky on the Win XP bot. See bug for more info.
    
    BUG=560716
    TBR=rockot
    
    Review URL: https://codereview.chromium.org/1472043002
    
    Cr-Commit-Position: refs/heads/master@{#361327}
    
    Former-commit-id: c3a0a603db5fd9a50b531a9b6056d0b230f141e8

--- chrome/browser/extensions/extension_loading_browsertest.cc
+++ chrome/browser/extensions/extension_loading_browsertest.cc
@@ -8,6 +8,7 @@
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
 #include "base/version.h"
+#include "base/win/windows_version.h"
 #include "chrome/browser/extensions/devtools_util.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -97,6 +98,12 @@ IN_PROC_BROWSER_TEST_F(ExtensionLoadingTest,
 // Tests the behavior described in http://crbug.com/532088.
 IN_PROC_BROWSER_TEST_F(ExtensionLoadingTest,
                        KeepAliveWithDevToolsOpenOnReload) {
+#if defined(OS_WIN)
+  // Flaky on Win XP SP3. http://crbug.com/560716.
+  if (base::win::GetVersion() <= base::win::VERSION_SERVER_2003)
+    return;
+#endif
+
   embedded_test_server()->ServeFilesFromDirectory(
       base::FilePath(FILE_PATH_LITERAL("chrome/test/data")));
   ASSERT_TRUE(embedded_test_server()->Start());
