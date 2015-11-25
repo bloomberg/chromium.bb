7fd044790085acd61b9a6c9c163cc087c2af671f
avi <avi@chromium.org>
2015-11-24 23:42:53 -0800

    Remove COMPILE_ASSERT.
    BUG=442514
    
    Review URL: https://codereview.chromium.org/1472023003
    
    Cr-Commit-Position: refs/heads/master@{#361602}
    
    Former-commit-id: 7c021364e9a9455a28a5e213dbd0a098c48a4189

--- base/macros.h
+++ base/macros.h
@@ -55,24 +55,6 @@
 template <typename T, size_t N> char (&ArraySizeHelper(T (&array)[N]))[N];
 #define arraysize(array) (sizeof(ArraySizeHelper(array)))
 
-// The COMPILE_ASSERT macro can be used to verify that a compile time
-// expression is true. For example, you could use it to verify the
-// size of a static array:
-//
-//   COMPILE_ASSERT(arraysize(content_type_names) == CONTENT_NUM_TYPES,
-//                  content_type_names_incorrect_size);
-//
-// or to make sure a struct is smaller than a certain size:
-//
-//   COMPILE_ASSERT(sizeof(foo) < 128, foo_too_large);
-//
-// The second argument to the macro is the name of the variable. If
-// the expression is false, most compilers will issue a warning/error
-// containing the name of the variable.
-
-#undef COMPILE_ASSERT
-#define COMPILE_ASSERT(expr, msg) static_assert(expr, #msg)
-
 // bit_cast<Dest,Source> is a template function that implements the
 // equivalent of "*reinterpret_cast<Dest*>(&source)".  We need this in
 // very low-level functions like the protobuf library and fast math
