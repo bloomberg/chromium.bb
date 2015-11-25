5255cd4f8a6524a55b454edfb7908eb02d80b638
rmistry <rmistry@google.com>
2015-11-25 07:46:58 -0800

    Add CT DM isolate file and script that will be run on swarming slaves.
    The code here will be invoked by the recipe in https://codereview.chromium.org/1441173003/
    
    BUG=skia:2545
    
    Review URL: https://codereview.chromium.org/1469823003
    
    Cr-Commit-Position: refs/heads/master@{#361666}
    
    Former-commit-id: e0df8b98317e0967d30ea4a7c4a47ad42a1eb2cf

---
+++ content/test/ct/run_ct_dm.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# Copyright (c) 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""This script is meant to be run on a Swarming bot."""
+
+import argparse
+import os
+import subprocess
+import sys
+
+
+PARENT_DIR = os.path.dirname(os.path.realpath(__file__))
+
+SKIA_SRC_DIR = os.path.normpath(os.path.join(
+    PARENT_DIR, os.pardir, os.pardir, os.pardir, os.pardir, 'skia'))
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s', '--slave_num', required=True, type=int,
+                      help='The slave num of this CT run.')
+  args = parser.parse_args()
+
+  dm_path = os.path.join(SKIA_SRC_DIR, 'out', 'Debug', 'dm')
+  skps_dir = os.path.join(PARENT_DIR, 'slave%d' % args.slave_num, 'skps')
+  resource_path = os.path.join(SKIA_SRC_DIR, 'resources')
+
+  # TODO(rmistry): Double check the below DM configuration with mtklein@. We
+  # need a basic configuration that can help catch bugs like the ones listed in
+  # skbug.com/4416
+  cmd = [
+      dm_path,
+      '--src', 'skp',
+      '--skps', skps_dir,
+      '--resourcePath', resource_path,
+      '--config', '8888',
+      '--verbose',
+  ]
+  return subprocess.call(cmd)
+
+
+if __name__ == '__main__':
+  sys.exit(main())
