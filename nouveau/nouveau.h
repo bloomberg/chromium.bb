f208f2afdee6222ea8f6692a6ec4381cc999eb05
Ben Skeggs <bskeggs@redhat.com>
2015-11-26 08:24:55 +1000

    nouveau: clean up nouveau.h, noting deprecated members/functions
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>
    Tested-by: Samuel Pitoiset <samuel.pitoiset@gmail.com>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>

--- nouveau/nouveau.h
+++ nouveau/nouveau.h
@@ -4,23 +4,42 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define NOUVEAU_DEVICE_CLASS       0x80000000
-#define NOUVEAU_FIFO_CHANNEL_CLASS 0x80000001
-#define NOUVEAU_NOTIFIER_CLASS     0x80000002
-#define NOUVEAU_PARENT_CLASS       0xffffffff
+/* Supported class information, provided by the kernel */
+struct nouveau_sclass {
+	int32_t oclass;
+	int minver;
+	int maxver;
+};
 
-struct nouveau_list {
-	struct nouveau_list *prev;
-	struct nouveau_list *next;
+/* Client-provided array describing class versions that are desired.
+ *
+ * These are used to match against the kernel's list of supported classes.
+ */
+struct nouveau_mclass {
+	int32_t oclass;
+	int version;
+	void *data;
 };
 
 struct nouveau_object {
 	struct nouveau_object *parent;
 	uint64_t handle;
 	uint32_t oclass;
-	uint32_t length;
-	void *data;
+	uint32_t length;	/* deprecated */
+	void *data;		/* deprecated */
 };
+
+int nouveau_object_new(struct nouveau_object *parent, uint64_t handle,
+		       uint32_t oclass, void *data, uint32_t length,
+		       struct nouveau_object **);
+void nouveau_object_del(struct nouveau_object **);
+int nouveau_object_mthd(struct nouveau_object *, uint32_t mthd,
+			void *data, uint32_t size);
+int nouveau_object_sclass_get(struct nouveau_object *,
+			      struct nouveau_sclass **);
+void nouveau_object_sclass_put(struct nouveau_sclass **);
+int nouveau_object_mclass(struct nouveau_object *,
+			  const struct nouveau_mclass *);
 
 struct nouveau_drm {
 	struct nouveau_object client;
@@ -40,81 +59,11 @@ nouveau_drm(struct nouveau_object *obj)
 int nouveau_drm_new(int fd, struct nouveau_drm **);
 void nouveau_drm_del(struct nouveau_drm **);
 
-struct nouveau_fifo {
-	struct nouveau_object *object;
-	uint32_t channel;
-	uint32_t pushbuf;
-	uint64_t unused1[3];
-};
-
-struct nv04_fifo {
-	struct nouveau_fifo base;
-	uint32_t vram;
-	uint32_t gart;
-	uint32_t notify;
-};
-
-struct nvc0_fifo {
-	struct nouveau_fifo base;
-	uint32_t notify;
-};
-
-#define NVE0_FIFO_ENGINE_GR  0x00000001
-#define NVE0_FIFO_ENGINE_VP  0x00000002
-#define NVE0_FIFO_ENGINE_PPP 0x00000004
-#define NVE0_FIFO_ENGINE_BSP 0x00000008
-#define NVE0_FIFO_ENGINE_CE0 0x00000010
-#define NVE0_FIFO_ENGINE_CE1 0x00000020
-#define NVE0_FIFO_ENGINE_ENC 0x00000040
-
-struct nve0_fifo {
-	struct {
-		struct nouveau_fifo base;
-		uint32_t notify;
-	};
-	uint32_t engine;
-};
-
-struct nv04_notify {
-	struct nouveau_object *object;
-	uint32_t offset;
-	uint32_t length;
-};
-
-/* Supported class information, provided by the kernel */
-struct nouveau_sclass {
-	int32_t oclass;
-	int minver;
-	int maxver;
-};
-
-/* Client-provided array describing class versions that are desired.
- *
- * These are used to match against the kernel's list of supported classes.
- */
-struct nouveau_mclass {
-	int32_t oclass; /* 0 == EOL */
-	int version;
-	void *data;
-};
-
-int  nouveau_object_new(struct nouveau_object *parent, uint64_t handle,
-			uint32_t oclass, void *data, uint32_t length,
-			struct nouveau_object **);
-void nouveau_object_del(struct nouveau_object **);
-int  nouveau_object_mthd(struct nouveau_object *, uint32_t mthd,
-			 void *data, uint32_t size);
-int  nouveau_object_sclass_get(struct nouveau_object *,
-			       struct nouveau_sclass **);
-void nouveau_object_sclass_put(struct nouveau_sclass **);
-int  nouveau_object_mclass(struct nouveau_object *,
-			   const struct nouveau_mclass *);
-
 struct nouveau_device {
 	struct nouveau_object object;
-	int fd;
-	uint32_t lib_version;
-	uint32_t drm_version;
+	int fd;			/* deprecated */
+	uint32_t lib_version;	/* deprecated */
+	uint32_t drm_version;	/* deprecated */
 	uint32_t chipset;
 	uint64_t vram_size;
 	uint64_t gart_size;
@@ -122,20 +71,23 @@ struct nouveau_device {
 	uint64_t gart_limit;
 };
 
-int  nouveau_device_new(struct nouveau_object *parent, int32_t oclass,
-			void *data, uint32_t size, struct nouveau_device **);
-int  nouveau_device_wrap(int fd, int close, struct nouveau_device **);
-int  nouveau_device_open(const char *busid, struct nouveau_device **);
+int nouveau_device_new(struct nouveau_object *parent, int32_t oclass,
+		       void *data, uint32_t size, struct nouveau_device **);
 void nouveau_device_del(struct nouveau_device **);
-int  nouveau_getparam(struct nouveau_device *, uint64_t param, uint64_t *value);
-int  nouveau_setparam(struct nouveau_device *, uint64_t param, uint64_t value);
+
+int nouveau_getparam(struct nouveau_device *, uint64_t param, uint64_t *value);
+int nouveau_setparam(struct nouveau_device *, uint64_t param, uint64_t value);
+
+/* deprecated */
+int nouveau_device_wrap(int fd, int close, struct nouveau_device **);
+int nouveau_device_open(const char *busid, struct nouveau_device **);
 
 struct nouveau_client {
 	struct nouveau_device *device;
 	int id;
 };
 
-int  nouveau_client_new(struct nouveau_device *, struct nouveau_client **);
+int nouveau_client_new(struct nouveau_device *, struct nouveau_client **);
 void nouveau_client_del(struct nouveau_client **);
 
 union nouveau_bo_config {
@@ -182,22 +134,27 @@ struct nouveau_bo {
 	union nouveau_bo_config config;
 };
 
-int  nouveau_bo_new(struct nouveau_device *, uint32_t flags, uint32_t align,
-		    uint64_t size, union nouveau_bo_config *,
+int nouveau_bo_new(struct nouveau_device *, uint32_t flags, uint32_t align,
+		   uint64_t size, union nouveau_bo_config *,
+		   struct nouveau_bo **);
+int nouveau_bo_wrap(struct nouveau_device *, uint32_t handle,
 		    struct nouveau_bo **);
-int  nouveau_bo_wrap(struct nouveau_device *, uint32_t handle,
-		     struct nouveau_bo **);
-int  nouveau_bo_name_ref(struct nouveau_device *dev, uint32_t name,
-			 struct nouveau_bo **);
-int  nouveau_bo_name_get(struct nouveau_bo *, uint32_t *name);
+int nouveau_bo_name_ref(struct nouveau_device *v, uint32_t name,
+			struct nouveau_bo **);
+int nouveau_bo_name_get(struct nouveau_bo *, uint32_t *name);
 void nouveau_bo_ref(struct nouveau_bo *, struct nouveau_bo **);
-int  nouveau_bo_map(struct nouveau_bo *, uint32_t access,
+int nouveau_bo_map(struct nouveau_bo *, uint32_t access,
+		   struct nouveau_client *);
+int nouveau_bo_wait(struct nouveau_bo *, uint32_t access,
 		    struct nouveau_client *);
-int  nouveau_bo_wait(struct nouveau_bo *, uint32_t access,
-		     struct nouveau_client *);
-int  nouveau_bo_prime_handle_ref(struct nouveau_device *dev, int prime_fd,
-				 struct nouveau_bo **);
-int  nouveau_bo_set_prime(struct nouveau_bo *bo, int *prime_fd);
+int nouveau_bo_prime_handle_ref(struct nouveau_device *, int prime_fd,
+				struct nouveau_bo **);
+int nouveau_bo_set_prime(struct nouveau_bo *, int *prime_fd);
+
+struct nouveau_list {
+	struct nouveau_list *prev;
+	struct nouveau_list *next;
+};
 
 struct nouveau_bufref {
 	struct nouveau_list thead;
@@ -219,8 +176,8 @@ struct nouveau_bufctx {
 	int relocs;
 };
 
-int  nouveau_bufctx_new(struct nouveau_client *, int bins,
-			struct nouveau_bufctx **);
+int nouveau_bufctx_new(struct nouveau_client *, int bins,
+		       struct nouveau_bufctx **);
 void nouveau_bufctx_del(struct nouveau_bufctx **);
 struct nouveau_bufref *
 nouveau_bufctx_refn(struct nouveau_bufctx *, int bin,
@@ -249,16 +206,16 @@ struct nouveau_pushbuf_refn {
 	uint32_t flags;
 };
 
-int  nouveau_pushbuf_new(struct nouveau_client *, struct nouveau_object *channel,
-			 int nr, uint32_t size, bool immediate,
-			 struct nouveau_pushbuf **);
+int nouveau_pushbuf_new(struct nouveau_client *, struct nouveau_object *chan,
+			int nr, uint32_t size, bool immediate,
+			struct nouveau_pushbuf **);
 void nouveau_pushbuf_del(struct nouveau_pushbuf **);
-int  nouveau_pushbuf_space(struct nouveau_pushbuf *, uint32_t dwords,
-			   uint32_t relocs, uint32_t pushes);
+int nouveau_pushbuf_space(struct nouveau_pushbuf *, uint32_t dwords,
+			  uint32_t relocs, uint32_t pushes);
 void nouveau_pushbuf_data(struct nouveau_pushbuf *, struct nouveau_bo *,
 			  uint64_t offset, uint64_t length);
-int  nouveau_pushbuf_refn(struct nouveau_pushbuf *,
-			  struct nouveau_pushbuf_refn *, int nr);
+int nouveau_pushbuf_refn(struct nouveau_pushbuf *,
+			 struct nouveau_pushbuf_refn *, int nr);
 /* Emits a reloc into the push buffer at the current position, you *must*
  * have previously added the referenced buffer to a buffer context, and
  * validated it against the current push buffer.
@@ -266,10 +223,54 @@ int  nouveau_pushbuf_refn(struct nouveau_pushbuf *,
 void nouveau_pushbuf_reloc(struct nouveau_pushbuf *, struct nouveau_bo *,
 			   uint32_t data, uint32_t flags,
 			   uint32_t vor, uint32_t tor);
-int  nouveau_pushbuf_validate(struct nouveau_pushbuf *);
+int nouveau_pushbuf_validate(struct nouveau_pushbuf *);
 uint32_t nouveau_pushbuf_refd(struct nouveau_pushbuf *, struct nouveau_bo *);
-int  nouveau_pushbuf_kick(struct nouveau_pushbuf *, struct nouveau_object *channel);
+int nouveau_pushbuf_kick(struct nouveau_pushbuf *, struct nouveau_object *chan);
 struct nouveau_bufctx *
 nouveau_pushbuf_bufctx(struct nouveau_pushbuf *, struct nouveau_bufctx *);
 
+#define NOUVEAU_DEVICE_CLASS       0x80000000
+#define NOUVEAU_FIFO_CHANNEL_CLASS 0x80000001
+#define NOUVEAU_NOTIFIER_CLASS     0x80000002
+
+struct nouveau_fifo {
+	struct nouveau_object *object;
+	uint32_t channel;
+	uint32_t pushbuf;
+	uint64_t unused1[3];
+};
+
+struct nv04_fifo {
+	struct nouveau_fifo base;
+	uint32_t vram;
+	uint32_t gart;
+	uint32_t notify;
+};
+
+struct nvc0_fifo {
+	struct nouveau_fifo base;
+	uint32_t notify;
+};
+
+#define NVE0_FIFO_ENGINE_GR  0x00000001
+#define NVE0_FIFO_ENGINE_VP  0x00000002
+#define NVE0_FIFO_ENGINE_PPP 0x00000004
+#define NVE0_FIFO_ENGINE_BSP 0x00000008
+#define NVE0_FIFO_ENGINE_CE0 0x00000010
+#define NVE0_FIFO_ENGINE_CE1 0x00000020
+#define NVE0_FIFO_ENGINE_ENC 0x00000040
+
+struct nve0_fifo {
+	struct {
+		struct nouveau_fifo base;
+		uint32_t notify;
+	};
+	uint32_t engine;
+};
+
+struct nv04_notify {
+	struct nouveau_object *object;
+	uint32_t offset;
+	uint32_t length;
+};
 #endif
