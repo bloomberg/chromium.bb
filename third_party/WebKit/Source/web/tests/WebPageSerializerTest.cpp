e22a3ae938d869d978d6b4a289e16f120b95fd11
esprehn <esprehn@chromium.org>
2015-12-02 16:18:33 -0800

    Vastly simplify WebPageSerializerTest.
    This test can be made a lot simpler and easier to understand.
    
    Review URL: https://codereview.chromium.org/1493063002
    
    Cr-Commit-Position: refs/heads/master@{#362830}
    
    Former-commit-id: e867bd7fed113cc9dc7ccb0e070a5f2916e07d0c

--- third_party/WebKit/Source/web/tests/WebPageSerializerTest.cpp
+++ third_party/WebKit/Source/web/tests/WebPageSerializerTest.cpp
@@ -32,94 +32,70 @@
 #include "public/web/WebPageSerializer.h"
 
 #include "platform/testing/URLTestHelpers.h"
+#include "platform/weborigin/KURL.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebString.h"
 #include "public/platform/WebURL.h"
-#include "public/platform/WebURLRequest.h"
-#include "public/platform/WebURLResponse.h"
 #include "public/platform/WebUnitTestSupport.h"
-#include "public/web/WebDocument.h"
-#include "public/web/WebFrame.h"
 #include "public/web/WebPageSerializerClient.h"
-#include "public/web/WebView.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "web/WebLocalFrameImpl.h"
+#include "web/WebViewImpl.h"
 #include "web/tests/FrameTestHelpers.h"
-
-using blink::Document;
-using blink::URLTestHelpers::toKURL;
+#include "wtf/text/StringBuilder.h"
 
 namespace blink {
 
 namespace {
-class SimpleWebPageSerializerClient : public  WebPageSerializerClient {
+class SimpleWebPageSerializerClient final : public WebPageSerializerClient {
 public:
-    std::string toString() const { return m_string; }
+    String toString() { return m_builder.toString(); }
 
 private:
     void didSerializeDataForFrame(const WebCString& data, PageSerializationStatus) final
     {
-        m_string += data;
+        m_builder.append(data.data(), data.length());
     }
 
-    std::string m_string;
+    StringBuilder m_builder;
 };
 
 } // namespace
 
 class WebPageSerializerTest : public testing::Test {
-public:
-    WebPageSerializerTest()
-        : m_supportedSchemes(static_cast<size_t>(3))
-    {
-        m_supportedSchemes[0] = "http";
-        m_supportedSchemes[1] = "https";
-        m_supportedSchemes[2] = "file";
-
-        registerMockedImageURL("http://www.test.com/awesome.png");
-    }
-
 protected:
-    void SetUp() override
+    WebPageSerializerTest()
     {
         m_helper.initialize();
     }
 
-    void TearDown() override
+    ~WebPageSerializerTest() override
     {
         Platform::current()->unitTestSupport()->unregisterAllMockedURLs();
     }
 
-    void registerMockedURLLoad(const std::string& url, const WebString& fileName)
-    {
-        URLTestHelpers::registerMockedURLLoad(toKURL(url), fileName, WebString::fromUTF8("pageserialization/"), WebString::fromUTF8("text/html"));
-    }
-
-    void registerMockedImageURL(const std::string& url)
+    void registerMockedImageURL(const String& url)
     {
         // Image resources need to be mocked, but irrelevant here what image they map to.
-        URLTestHelpers::registerMockedURLLoad(toKURL(url), "pageserialization/awesome.png");
+        URLTestHelpers::registerMockedURLLoad(KURL(ParsedURLString, url), "pageserialization/awesome.png");
     }
 
-    void loadURLInTopFrame(const WebURL& url)
+    String serializeFile(const String& url, const String& fileName)
     {
-        FrameTestHelpers::loadFrame(m_helper.webView()->mainFrame(), url.string().utf8());
+        KURL parsedURL(ParsedURLString, url);
+        URLTestHelpers::registerMockedURLLoad(parsedURL, fileName, "pageserialization/", "text/html");
+        FrameTestHelpers::loadFrame(mainFrameImpl(), url.utf8().data());
+        WebVector<WebURL> links(&parsedURL, 1);
+        WebVector<WebString> localPaths(&"local", 1);
+        SimpleWebPageSerializerClient serializerClient;
+        WebPageSerializer::serialize(mainFrameImpl(), &serializerClient, links, localPaths, "");
+        return serializerClient.toString();
     }
 
-    static bool webVectorContains(const WebVector<WebURL>& vector, const char* url)
+    WebLocalFrameImpl* mainFrameImpl()
     {
-        return vector.contains(WebURL(toKURL(std::string(url))));
+        return m_helper.webViewImpl()->mainFrameImpl();
     }
-
-    // Useful for debugging.
-    static void printWebURLs(const WebVector<WebURL>& urls)
-    {
-        for (size_t i = 0; i < urls.size(); i++)
-            printf("%s\n", urls[i].spec().data());
-    }
-
-    WebView* webView() const { return m_helper.webView(); }
-
-    WebVector<WebCString> m_supportedSchemes;
 
 private:
     FrameTestHelpers::WebViewHelper m_helper;
@@ -127,16 +103,7 @@ private:
 
 TEST_F(WebPageSerializerTest, URLAttributeValues)
 {
-    WebURL topFrameURL = toKURL("http://www.test.com");
-    registerMockedURLLoad(topFrameURL.spec(), WebString::fromUTF8("url_attribute_values.html"));
     registerMockedImageURL("javascript:\"");
-
-    loadURLInTopFrame(topFrameURL);
-
-    SimpleWebPageSerializerClient serializerClient;
-    WebVector<WebURL> links(&topFrameURL, 1);
-    WebVector<WebString> localPaths(&"local", 1);
-    WebPageSerializer::serialize(webView()->mainFrame()->toWebLocalFrame(), &serializerClient, links, localPaths, "");
 
     const char* expectedHTML =
         "\n<!-- saved from url=(0020)http://www.test.com/ -->\n"
@@ -145,21 +112,12 @@ TEST_F(WebPageSerializerTest, URLAttributeValues)
         "<a href=\"http://www.test.com/local#&quot;\">local</a>\n"
         "<a href=\"http://www.example.com/#&quot;&gt;&lt;script&gt;alert(0)&lt;/script&gt;\">external</a>\n"
         "</body></html>";
-    EXPECT_EQ(expectedHTML, serializerClient.toString());
+    String actualHTML = serializeFile("http://www.test.com", "url_attribute_values.html");
+    EXPECT_EQ(expectedHTML, actualHTML);
 }
 
 TEST_F(WebPageSerializerTest, EncodingAndNormalization)
 {
-    WebURL topFrameURL = toKURL("http://www.test.com");
-    registerMockedURLLoad(topFrameURL.spec(), WebString::fromUTF8("encoding_normalization.html"));
-
-    loadURLInTopFrame(topFrameURL);
-
-    SimpleWebPageSerializerClient serializerClient;
-    WebVector<WebURL> links(&topFrameURL, 1);
-    WebVector<WebString> localPaths(&"local", 1);
-    WebPageSerializer::serialize(webView()->mainFrame()->toWebLocalFrame(), &serializerClient, links, localPaths, "");
-
     const char* expectedHTML =
         "<!DOCTYPE html>\n"
         "<!-- saved from url=(0020)http://www.test.com/ -->\n"
@@ -168,21 +126,14 @@ TEST_F(WebPageSerializerTest, EncodingAndNormalization)
         "</head><body>\n"
         "\xe4\xc5\xd1\xe2\n"
         "\n</body></html>";
-    EXPECT_EQ(expectedHTML, serializerClient.toString());
+    String actualHTML = serializeFile("http://www.test.com", "encoding_normalization.html");
+    EXPECT_EQ(expectedHTML, actualHTML);
 }
 
-TEST_F(WebPageSerializerTest, fromUrlWithMinusMinus)
+TEST_F(WebPageSerializerTest, FromUrlWithMinusMinus)
 {
-    WebURL topFrameURL = toKURL("http://www.test.com?--x--");
-    registerMockedURLLoad(topFrameURL.spec(), WebString::fromUTF8("text_only_page.html"));
-    loadURLInTopFrame(topFrameURL);
-
-    SimpleWebPageSerializerClient serializerClient;
-    WebVector<WebURL> links(&topFrameURL, 1);
-    WebVector<WebString> localPaths(&"local", 1);
-    WebPageSerializer::serialize(webView()->mainFrame()->toWebLocalFrame(), &serializerClient, links, localPaths, "");
-
-    EXPECT_EQ("<!-- saved from url=(0030)http://www.test.com/?-%2Dx-%2D -->", serializerClient.toString().substr(1, 60));
+    String actualHTML = serializeFile("http://www.test.com?--x--", "text_only_page.html");
+    EXPECT_EQ("<!-- saved from url=(0030)http://www.test.com/?-%2Dx-%2D -->", actualHTML.substring(1, 60));
 }
 
 } // namespace blink
