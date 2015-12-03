e722acf23d44e3fd554c88fcc89557b63f2abc54
olivierrobin <olivierrobin@chromium.org>
2015-12-03 05:21:11 -0800

    [TTS] Add command to open Touch to search preferences.
    BUG=503585
    
    Review URL: https://codereview.chromium.org/1498813002
    
    Cr-Commit-Position: refs/heads/master@{#362969}
    
    Former-commit-id: 95e2926b29b8d8c7390ea64b73e98f978b6ea0ff

--- ios/chrome/browser/ui/commands/ios_command_ids.h
+++ ios/chrome/browser/ui/commands/ios_command_ids.h
@@ -33,6 +33,7 @@
 #define IDC_SETUP_FOR_TESTING                          40916
 #define IDC_SHOW_OTHER_DEVICES                         40917
 #define IDC_CLOSE_ALL_INCOGNITO_TABS                   40918
+#define IDC_SHOW_CONTEXTUAL_SEARCH_SETTINGS            40919
 #define IDC_CLOSE_SETTINGS_AND_OPEN_URL                40920
 #define IDC_REQUEST_DESKTOP_SITE                       40921
 #define IDC_CLEAR_BROWSING_DATA_IOS                    40924
