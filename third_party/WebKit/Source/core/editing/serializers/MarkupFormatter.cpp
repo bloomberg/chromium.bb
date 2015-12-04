f3f6d9c8f633aa772789c3cca71ed044af581d12
tkent <tkent@chromium.org>
2015-12-03 19:53:26 -0800

    XMLSerializer: Escape \r, \n and \t in attribute values.
    The latest draft [1] doesn't say they should be escaped, but practically they
    should be escaped to keep attribute values correctly because XML parsers normalize
    unescaped these characters to spaces [2].
    
    The new behavior matches to Microsoft Edge.
    
    [1] https://w3c.github.io/DOM-Parsing/#dfn-concept-serialize-attr-value
    [2] http://www.w3.org/TR/REC-xml/#AVNormalize
    
    BUG=418531
    TEST=automated
    
    Review URL: https://codereview.chromium.org/1494083002
    
    Cr-Commit-Position: refs/heads/master@{#363109}
    
    Former-commit-id: a52138fd2c37006ece8c33ee1450c9316f975e66

--- third_party/WebKit/Source/core/editing/serializers/MarkupFormatter.cpp
+++ third_party/WebKit/Source/core/editing/serializers/MarkupFormatter.cpp
@@ -78,6 +78,9 @@ void MarkupFormatter::appendCharactersReplacingEntities(StringBuilder& result, c
     DEFINE_STATIC_LOCAL(const CString, gtReference, ("&gt;"));
     DEFINE_STATIC_LOCAL(const CString, quotReference, ("&quot;"));
     DEFINE_STATIC_LOCAL(const CString, nbspReference, ("&nbsp;"));
+    DEFINE_STATIC_LOCAL(const CString, tabReference, ("&#9;"));
+    DEFINE_STATIC_LOCAL(const CString, lineFeedReference, ("&#10;"));
+    DEFINE_STATIC_LOCAL(const CString, carriageReturnReference, ("&#13;"));
 
     static const EntityDescription entityMaps[] = {
         { '&', ampReference, EntityAmp },
@@ -85,6 +88,9 @@ void MarkupFormatter::appendCharactersReplacingEntities(StringBuilder& result, c
         { '>', gtReference, EntityGt },
         { '"', quotReference, EntityQuot },
         { noBreakSpaceCharacter, nbspReference, EntityNbsp },
+        { '\t', tabReference, EntityTab },
+        { '\n', lineFeedReference, EntityLineFeed },
+        { '\r', carriageReturnReference, EntityCarriageReturn },
     };
 
     if (!(offset + length))
