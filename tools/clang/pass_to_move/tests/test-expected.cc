f39b947c883f81f71bdd3f106edf901c457a6ba8
Daniel Cheng <dcheng@chromium.org>
2015-12-04 20:45:28 -0800

    Clang tool to convert scoper.Pass() to std::move(scoper).
    BUG=557422
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/1501103002 .
    
    Cr-Commit-Position: refs/heads/master@{#363337}
    
    Former-commit-id: 2d022200804d95d3d1a35e6792ca819f7895e255

---
+++ tools/clang/pass_to_move/tests/test-expected.cc
@@ -0,0 +1,33 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+struct A {
+  A&& Pass();
+};
+
+struct B {
+  B& Pass();
+};
+
+struct C {
+  A a;
+};
+
+struct D {
+  D&& NotPass();
+};
+
+void F() {
+  A a1;
+  A a2 = std::move(a1);
+
+  B b1;
+  B b2 = b2.Pass();
+
+  C c;
+  A a3 = std::move(c.a);
+
+  D d1;
+  D d2 = d1.NotPass();
+}
