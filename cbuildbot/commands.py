f55077ec8a6fcf4d3597e28ec418d705e4eb98e9
Shuqian Zhao <shuqianz@chromium.org>
2015-12-08 14:57:51 -0800

    commands.py: Add newline to separate json dictionary string from other outputs
    This is a followup cl for an already fixed bug. However, there is
    another place where also writes the json dictionary to the stdout needs
    newline to separate the string from other outputs.
    
    BUG=chromium:564768
    TEST=unittest
    
    Change-Id: Ie7abfa897689bad8da32a749113ebd33aab91675
    Reviewed-on: https://chromium-review.googlesource.com/316933
    Commit-Ready: Don Garrett <dgarrett@chromium.org>
    Tested-by: Shuqian Zhao <shuqianz@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>

--- cbuildbot/commands.py
+++ cbuildbot/commands.py
@@ -934,6 +934,7 @@ def RunHWTestSuite(build, suite, board, pool=None, num=None, file_bugs=None,
       if running_json_dump_flag:
         s = ''.join(result.task_summary_json['shards'][0]['outputs'])
         sys.stdout.write(s)
+        sys.stdout.write('\n')
         i = s.find(JSON_DICT_START) + len(JSON_DICT_START)
         j = s.find(JSON_DICT_END)
         json_dump_result = json.loads(s[i:j])
