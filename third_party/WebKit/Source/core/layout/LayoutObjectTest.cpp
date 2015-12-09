b8328280b7f1354c533525125b84441b4a02ebeb
jchaffraix <jchaffraix@chromium.org>
2015-12-09 09:40:17 -0800

    Land some simple testing for LayoutObject
    This is a split of https://codereview.chromium.org/351973004
    that wanted to land some unit testing before its time. This
    change just lands the unit tests from the previous CL, which
    checks the LayoutObjects generated with 'display' and the
    result of LayoutObject::containingBlock.
    
    Review URL: https://codereview.chromium.org/1503363002
    
    Cr-Commit-Position: refs/heads/master@{#364101}
    
    Former-commit-id: f28a13d2f411f747e23134cd73716f69ad667287

--- third_party/WebKit/Source/core/layout/LayoutObjectTest.cpp
+++ third_party/WebKit/Source/core/layout/LayoutObjectTest.cpp
@@ -6,11 +6,14 @@
 #include "core/layout/LayoutObject.h"
 
 #include "core/layout/LayoutTestHelper.h"
+#include "core/layout/LayoutView.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace blink {
 
 class LayoutObjectTest : public RenderingTest {
+protected:
+    LayoutView& layoutView() const { return *document().layoutView(); }
 };
 
 TEST_F(LayoutObjectTest, LayoutDecoratedNameCalledWithPositionedObject)
@@ -21,6 +24,87 @@ TEST_F(LayoutObjectTest, LayoutDecoratedNameCalledWithPositionedObject)
     LayoutObject* obj = div->layoutObject();
     ASSERT(obj);
     EXPECT_STREQ("LayoutBlockFlow (positioned)", obj->decoratedName().ascii().data());
+}
+
+
+// Some display checks.
+TEST_F(LayoutObjectTest, DisplayNoneCreateObject)
+{
+    setBodyInnerHTML("<div style='display:none'></div>");
+    EXPECT_EQ(nullptr, document().body()->firstChild()->layoutObject());
+}
+
+TEST_F(LayoutObjectTest, DisplayBlockCreateObject)
+{
+    setBodyInnerHTML("<foo style='display:block'></foo>");
+    LayoutObject* layoutObject = document().body()->firstChild()->layoutObject();
+    EXPECT_NE(nullptr, layoutObject);
+    EXPECT_TRUE(layoutObject->isLayoutBlockFlow());
+    EXPECT_FALSE(layoutObject->isInline());
+}
+
+TEST_F(LayoutObjectTest, DisplayInlineBlockCreateObject)
+{
+    setBodyInnerHTML("<foo style='display:inline-block'></foo>");
+    LayoutObject* layoutObject = document().body()->firstChild()->layoutObject();
+    EXPECT_NE(nullptr, layoutObject);
+    EXPECT_TRUE(layoutObject->isLayoutBlockFlow());
+    EXPECT_TRUE(layoutObject->isInline());
+}
+
+
+
+// Containing block test.
+TEST_F(LayoutObjectTest, ContainingBlockLayoutViewShouldBeNull)
+{
+    EXPECT_EQ(nullptr, layoutView().containingBlock());
+}
+
+TEST_F(LayoutObjectTest, ContainingBlockBodyShouldBeDocumentElement)
+{
+    EXPECT_EQ(document().body()->layoutObject()->containingBlock(), document().documentElement()->layoutObject());
+}
+
+TEST_F(LayoutObjectTest, ContainingBlockDocumentElementShouldBeLayoutView)
+{
+    EXPECT_EQ(document().documentElement()->layoutObject()->containingBlock(), layoutView());
+}
+
+TEST_F(LayoutObjectTest, ContainingBlockStaticLayoutObjectShouldBeParent)
+{
+    setBodyInnerHTML("<foo style='position:static'></foo>");
+    LayoutObject* bodyLayoutObject = document().body()->layoutObject();
+    LayoutObject* layoutObject = bodyLayoutObject->slowFirstChild();
+    EXPECT_EQ(layoutObject->containingBlock(), bodyLayoutObject);
+}
+
+TEST_F(LayoutObjectTest, ContainingBlockAbsoluteLayoutObjectShouldBeLayoutView)
+{
+    setBodyInnerHTML("<foo style='position:absolute'></foo>");
+    LayoutObject* layoutObject = document().body()->layoutObject()->slowFirstChild();
+    EXPECT_EQ(layoutObject->containingBlock(), layoutView());
+}
+
+TEST_F(LayoutObjectTest, ContainingBlockAbsoluteLayoutObjectShouldBeNonStaticallyPositionedBlockAncestor)
+{
+    setBodyInnerHTML("<div style='position:relative'><bar style='position:absolute'></bar></div>");
+    LayoutObject* containingBlocklayoutObject = document().body()->layoutObject()->slowFirstChild();
+    LayoutObject* layoutObject = containingBlocklayoutObject->slowFirstChild();
+    EXPECT_EQ(layoutObject->containingBlock(), containingBlocklayoutObject);
+}
+
+TEST_F(LayoutObjectTest, ContainingBlockAbsoluteLayoutObjectShouldNotBeNonStaticlyPositionedInlineAncestor)
+{
+    setBodyInnerHTML("<span style='position:relative'><bar style='position:absolute'></bar></span>");
+    LayoutObject* bodyLayoutObject = document().body()->layoutObject();
+    LayoutObject* layoutObject = bodyLayoutObject->slowFirstChild()->slowFirstChild();
+
+    // Sanity check: Make sure we don't generate anonymous objects.
+    EXPECT_EQ(nullptr, bodyLayoutObject->slowFirstChild()->nextSibling());
+    EXPECT_EQ(nullptr, layoutObject->slowFirstChild());
+    EXPECT_EQ(nullptr, layoutObject->nextSibling());
+
+    EXPECT_EQ(layoutObject->containingBlock(), bodyLayoutObject);
 }
 
 }
