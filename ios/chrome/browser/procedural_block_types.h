6b70062ffded0b93699d88928b81da80f46d707a
marq <marq@chromium.org>
2015-12-09 09:56:35 -0800

    Adds an ProcedurealBlockWithBool block type.
    BUG=
    
    Review URL: https://codereview.chromium.org/1513483004
    
    Cr-Commit-Position: refs/heads/master@{#364104}
    
    Former-commit-id: 39f72fd932d5c4677767a7b2e84058d17e1f2e0e

--- ios/chrome/browser/procedural_block_types.h
+++ ios/chrome/browser/procedural_block_types.h
@@ -5,9 +5,15 @@
 #ifndef IOS_CHROME_BROWSER_PROCEDURAL_BLOCK_TYPES_H_
 #define IOS_CHROME_BROWSER_PROCEDURAL_BLOCK_TYPES_H_
 
+#include <Foundation/Foundation.h>
+
 class GURL;
 
 // A generic procedural block type that takes a |GURL| and returns nothing.
 typedef void (^ProceduralBlockWithURL)(const GURL&);
+
+// A block that takes a bool and returns nothing, as used for UIView animation
+// completion.
+typedef void (^ProceduralBlockWithBool)(BOOL);
 
 #endif  // IOS_CHROME_BROWSER_PROCEDURAL_BLOCK_TYPES_H_
