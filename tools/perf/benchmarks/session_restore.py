c2b7e8170560804fb22e3a1840441aa07b364c1e
aiolos <aiolos@chromium.org>
2015-12-08 17:47:09 -0800

    Enable session_restore cold/warm typical_25 on reference build.
    BUG=539011,546723
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1509043002
    
    Cr-Commit-Position: refs/heads/master@{#363885}
    
    Former-commit-id: 4062a3e23cae2602b9dca05ba2350ad0ede5a5fe

--- tools/perf/benchmarks/session_restore.py
+++ tools/perf/benchmarks/session_restore.py
@@ -47,8 +47,7 @@ class _SessionRestoreTypical25(perf_benchmark.PerfBenchmark):
     return session_restore.SessionRestore(cold=is_cold)
 
 @benchmark.Disabled('android',
-                    'mac',  # crbug.com/563594
-                    'reference')  # crbug.com/539011
+                    'mac')  # crbug.com/563594
 class SessionRestoreColdTypical25(_SessionRestoreTypical25):
   """Test by clearing system cache and profile before repeats."""
   tag = 'cold'
@@ -61,7 +60,7 @@ class SessionRestoreColdTypical25(_SessionRestoreTypical25):
 
 @benchmark.Disabled('android',
                     'mac',  # crbug.com/563594
-                    'linux', 'xp', 'reference')  # crbug.com/539056
+                    'linux', 'xp')  # crbug.com/539056
 class SessionRestoreWarmTypical25(_SessionRestoreTypical25):
   """Test without clearing system cache or profile before repeats.
 
