ea1ba7e06442225f349830ef36532465809f1967
mmoroz <mmoroz@chromium.org>
2015-12-11 07:51:25 -0800

    fuzzer for QUIC net::CryptoFramer::ParseMessage implemented
    R=aizatsky@chromium.org, inferno@chromium.org, krasin@chromium.org
    BUG=566504
    
    Review URL: https://codereview.chromium.org/1508343005
    
    Cr-Commit-Position: refs/heads/master@{#364708}
    
    Former-commit-id: 4617e91a29d423fc85827a9f6bb769620c11d9fd

---
+++ testing/libfuzzer/fuzzers/quic_crypto_framer_parse_message_fuzzer.cc
@@ -0,0 +1,17 @@
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <vector>
+
+#include "base/strings/string_piece.h"
+#include "net/quic/crypto/crypto_framer.h"
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
+  base::StringPiece crypto_input(reinterpret_cast<const char *>(data), size);
+  std::unique_ptr<net::CryptoHandshakeMessage> handshake_message(
+      net::CryptoFramer::ParseMessage(crypto_input));
+
+  return 0;
+}
