ba8211c3c2d0b52ee5999759a67233b14a972af3
pavely <pavely@chromium.org>
2015-12-11 15:26:12 -0800

    [Sync] Enable TokenServiceTableTest tests on mac
    The issue I believe was that test interacts with mac keychain.
    The solution is to direct test to use MockAppleKeychain instead.
    
    R=rogerta@chromium.org
    BUG=228943
    
    Review URL: https://codereview.chromium.org/1519623002
    
    Cr-Commit-Position: refs/heads/master@{#364832}
    
    Former-commit-id: b7643bf88ad03846bf172205311c1b611929226f

--- components/signin/core/browser/webdata/token_service_table_unittest.cc
+++ components/signin/core/browser/webdata/token_service_table_unittest.cc
@@ -5,6 +5,7 @@
 #include "base/files/scoped_temp_dir.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/time/time.h"
+#include "components/os_crypt/os_crypt.h"
 #include "components/signin/core/browser/webdata/token_service_table.h"
 #include "components/webdata/common/web_database.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -18,6 +19,9 @@ class TokenServiceTableTest : public testing::Test {
 
  protected:
   void SetUp() override {
+#if defined(OS_MACOSX)
+    OSCrypt::UseMockKeychain(true);
+#endif
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
     file_ = temp_dir_.path().AppendASCII("TestWebDatabase");
 
@@ -35,18 +39,7 @@ class TokenServiceTableTest : public testing::Test {
   DISALLOW_COPY_AND_ASSIGN(TokenServiceTableTest);
 };
 
-// Flaky on mac_rel. See http://crbug.com/228943
-#if defined(OS_MACOSX)
-#define MAYBE_TokenServiceGetAllRemoveAll DISABLED_TokenServiceGetAllRemoveAll
-#define MAYBE_TokenServiceGetSet DISABLED_TokenServiceGetSet
-#define MAYBE_TokenServiceRemove DISABLED_TokenServiceRemove
-#else
-#define MAYBE_TokenServiceGetAllRemoveAll TokenServiceGetAllRemoveAll
-#define MAYBE_TokenServiceGetSet TokenServiceGetSet
-#define MAYBE_TokenServiceRemove TokenServiceRemove
-#endif
-
-TEST_F(TokenServiceTableTest, MAYBE_TokenServiceGetAllRemoveAll) {
+TEST_F(TokenServiceTableTest, TokenServiceGetAllRemoveAll) {
   std::map<std::string, std::string> out_map;
   std::string service;
   std::string service2;
@@ -75,7 +68,7 @@ TEST_F(TokenServiceTableTest, MAYBE_TokenServiceGetAllRemoveAll) {
   EXPECT_EQ("cheese", out_map.find(service)->second);
 }
 
-TEST_F(TokenServiceTableTest, MAYBE_TokenServiceGetSet) {
+TEST_F(TokenServiceTableTest, TokenServiceGetSet) {
   std::map<std::string, std::string> out_map;
   std::string service;
   service = "testservice";
@@ -100,7 +93,7 @@ TEST_F(TokenServiceTableTest, MAYBE_TokenServiceGetSet) {
   EXPECT_EQ("ham", out_map.find(service)->second);
 }
 
-TEST_F(TokenServiceTableTest, MAYBE_TokenServiceRemove) {
+TEST_F(TokenServiceTableTest, TokenServiceRemove) {
   std::map<std::string, std::string> out_map;
   std::string service;
   std::string service2;
