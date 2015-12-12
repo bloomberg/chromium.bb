42f622993dde67375f0e2a8308703676ccb12865
zqzhang <zqzhang@chromium.org>
2015-12-12 11:53:50 -0800

    Update media notification when page title changes
    This CL makes the media notification keep in sync when the page title
    changes.
    
    BUG=550424
    
    Review URL: https://codereview.chromium.org/1417743005
    
    Cr-Commit-Position: refs/heads/master@{#364938}
    
    Former-commit-id: d0b0d5fe705a29ff989dd3708d61154e8c600024

--- content/public/android/java/src/org/chromium/content_public/browser/WebContents.java
+++ content/public/android/java/src/org/chromium/content_public/browser/WebContents.java
@@ -8,6 +8,7 @@ import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.os.Parcelable;
 
+import org.chromium.base.ObserverList;
 import org.chromium.base.VisibleForTesting;
 
 /**
@@ -321,6 +322,12 @@ public interface WebContents extends Parcelable {
      * @param observer The observer to remove.
      */
     void removeObserver(WebContentsObserver observer);
+
+    /**
+     * @return The list of observers.
+     */
+    @VisibleForTesting
+    ObserverList.RewindableIterator<WebContentsObserver> getObserversForTesting();
 
     /**
      * Called when context menu gets opened.
