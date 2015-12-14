8b946dec70ca7985098525d0e9dccc8daa42ef47
Dale Curtis <dalecurtis@chromium.org>
2015-12-14 12:56:10 -0800

    Revert "lavf/mov: add support for sidx fragment indexes"
    This reverts commit 4ab56667594842283dc5ae07f0daba2a2cb4d3af.
    
    BUG=568336
    
    Change-Id: I1f6a06a95912d8a4c06e9cce521d0a647e2a5eed

--- libavformat/isom.h
+++ libavformat/isom.h
@@ -103,7 +103,6 @@ typedef struct MOVSbgp {
 typedef struct MOVFragmentIndexItem {
     int64_t moof_offset;
     int64_t time;
-    int headers_read;
 } MOVFragmentIndexItem;
 
 typedef struct MOVFragmentIndex {
@@ -201,7 +200,6 @@ typedef struct MOVContext {
     int has_looked_for_mfra;
     MOVFragmentIndex** fragment_index_data;
     unsigned fragment_index_count;
-    int fragment_index_complete;
     int atom_depth;
     unsigned int aax_mode;  ///< 'aax' file has been detected
     uint8_t file_key[20];
