6e6e2d78003bdd8f0cb88da11e439e8f811da289
sdefresne <sdefresne@chromium.org>
2015-12-14 05:19:26 -0800

    Upstream iOS concrete implementation of MetricsHelper class.
    BUG=567727
    
    Review URL: https://codereview.chromium.org/1522843002
    
    Cr-Commit-Position: refs/heads/master@{#365015}
    
    Former-commit-id: e9e69603a1c8904cca2be0849b89b6e61586c0dc

---
+++ ios/chrome/browser/interstitials/ios_chrome_metrics_helper.cc
@@ -0,0 +1,36 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/interstitials/ios_chrome_metrics_helper.h"
+
+#include "components/history/core/browser/history_service.h"
+#include "components/keyed_service/core/service_access_type.h"
+#include "components/rappor/rappor_service.h"
+#include "ios/chrome/browser/application_context.h"
+#include "ios/chrome/browser/history/history_service_factory.h"
+#include "ios/public/provider/chrome/browser/browser_state/chrome_browser_state.h"
+#include "ios/web/public/web_state/web_state.h"
+
+IOSChromeMetricsHelper::IOSChromeMetricsHelper(
+    web::WebState* web_state,
+    const GURL& request_url,
+    const security_interstitials::MetricsHelper::ReportDetails report_details)
+    : security_interstitials::MetricsHelper(
+          request_url,
+          report_details,
+          ios::HistoryServiceFactory::GetForBrowserState(
+              ios::ChromeBrowserState::FromBrowserState(
+                  web_state->GetBrowserState()),
+              ServiceAccessType::EXPLICIT_ACCESS),
+          GetApplicationContext()->GetRapporService()) {}
+
+IOSChromeMetricsHelper::~IOSChromeMetricsHelper() {}
+
+void IOSChromeMetricsHelper::RecordExtraUserDecisionMetrics(
+    security_interstitials::MetricsHelper::Decision decision) {}
+
+void IOSChromeMetricsHelper::RecordExtraUserInteractionMetrics(
+    security_interstitials::MetricsHelper::Interaction interaction) {}
+
+void IOSChromeMetricsHelper::RecordExtraShutdownMetrics() {}
