5be1faddae7da70d32bc15f2682c5cd70dd7f00b
dyen <dyen@chromium.org>
2015-12-14 08:29:16 -0800

    Revert of Synchronize GPU process before running trace test. (patchset #4 id:60001 of https://codereview.chromium.org/1521463002/ )
    Reason for revert:
    Still causing flakes
    
    Original issue's description:
    > Synchronize GPU process before running trace test.
    >
    > Enabling trace categories is always asynchronous so inherently racey,
    > currently there is no way to turn tracing on synchronously so the best
    > way to run trace tests reliably is to synchronize the GPU process using
    > readpixels.
    >
    > R=kbr@chromium.org, nednguyen@google.com
    > BUG=455324, 512622
    > TEST=test using win_chromium_rel_ng multiple times
    >
    > Committed: https://crrev.com/de5925f45758754a449a43388c1ef87a9e644451 [formerly 2efac79ea8e44a7d2af827c13172caaa2b350ffa]
    > Cr-Commit-Position: refs/heads/master@{#364894}
    
    TBR=kbr@chromium.org,nednguyen@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=455324, 512622
    
    Review URL: https://codereview.chromium.org/1525633002
    
    Cr-Commit-Position: refs/heads/master@{#365031}
    
    Former-commit-id: 676080ffa157feeff647feae61271c58572fb31c

--- content/test/gpu/gpu_tests/trace_test_expectations.py
+++ content/test/gpu/gpu_tests/trace_test_expectations.py
@@ -11,7 +11,7 @@ class TraceTestExpectations(GpuTestExpectations):
     # Sample Usage:
     # self.Fail('TraceTest.Canvas2DRedBox',
     #     ['mac', 'amd', ('nvidia', 0x1234)], bug=123)
-    pass
+    self.Skip('*', bug=512622)
 
 class DeviceTraceTestExpectations(GpuTestExpectations):
   def SetExpectations(self):
