63f0c01dbf15aa6608982b32fcceb4f9737337de
mmoroz <mmoroz@chromium.org>
2015-12-16 09:40:15 -0800

    update of build configurations for courgette_fuzzer (disable) and quic_crypto_framer_parse_message_fuzzer (test -> fuzzer_test) remove redundant header file from quic_crypto_framer_parse_message_fuzzer
    R=inferno@chromium.org
    TBR=aizatsky@chromium.org,krasin@chromium.org
    
    Review URL: https://codereview.chromium.org/1531673003
    
    Cr-Commit-Position: refs/heads/master@{#365553}
    
    Former-commit-id: 3ff45f6ae30dbee65562237b04da95149feca676

--- testing/libfuzzer/fuzzers/quic_crypto_framer_parse_message_fuzzer.cc
+++ testing/libfuzzer/fuzzers/quic_crypto_framer_parse_message_fuzzer.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <vector>
-
 #include "base/strings/string_piece.h"
 #include "net/quic/crypto/crypto_framer.h"
 
