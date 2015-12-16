36288ba274c7fb9573b3bbac76801a280a17a63d
khushalsagar <khushalsagar@chromium.org>
2015-12-16 13:44:22 -0800

    tools/perf: Enable thread_times tests.
    BUG=568120
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect
    
    Review URL: https://codereview.chromium.org/1532443002
    
    Cr-Commit-Position: refs/heads/master@{#365624}
    
    Former-commit-id: c98cf6986175a5511b3293bef601980ccc0aa3ee

--- tools/perf/benchmarks/benchmark_smoke_unittest.py
+++ tools/perf/benchmarks/benchmark_smoke_unittest.py
@@ -31,7 +31,6 @@ from benchmarks import spaceport
 from benchmarks import speedometer
 from benchmarks import sunspider
 from benchmarks import text_selection
-from benchmarks import thread_times
 
 
 def SmokeTestGenerator(benchmark):
@@ -88,8 +87,7 @@ _BLACK_LIST_TEST_MODULES = {
     speedometer,  # Takes 101 seconds.
     jetstream,  # Take 206 seconds.
     text_selection, # Always fails on cq bot.
-    memory,  # Flaky on bots, crbug.com/513767
-    thread_times, # Flaky on bots, crbug.com/568120
+    memory  # Flaky on bots, crbug.com/513767
 }
 
 # Some smoke benchmark tests that run quickly on desktop platform can be very
