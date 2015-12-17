0051ee7a2f419ed2f9ca30fb7b0a035682fd4664
nparker <nparker@chromium.org>
2015-12-16 21:22:26 -0800

    For downloads via Flash, log to Rappor the eTLD+1 of the requestor site.
    This will help us see which domains are landing dangerous binaries.
    
    BUG=569717
    
    Review URL: https://codereview.chromium.org/1526163004
    
    Cr-Commit-Position: refs/heads/master@{#365747}
    
    Former-commit-id: eee015448dec00db8aa157b5ae0dc0ea1274622e

--- chrome/browser/safe_browsing/unverified_download_policy.cc
+++ chrome/browser/safe_browsing/unverified_download_policy.cc
@@ -13,34 +13,62 @@
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/safe_browsing/unverified_download_field_trial.h"
 #include "chrome/common/safe_browsing/download_protection_util.h"
+#include "components/rappor/rappor_service.h"
+#include "components/rappor/rappor_utils.h"
 #include "content/public/browser/browser_thread.h"
 
 namespace safe_browsing {
 
 namespace {
+using content::BrowserThread;
+
+// Record the uma_file_type to UMA as an enum, and the URL's eTLD+1 to Rappor.
+void RecordPolicyMetricOnUIThread(const std::string& metric_name,
+                                  int uma_file_type,
+                                  const GURL& requestor) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  UMA_HISTOGRAM_SPARSE_SLOWLY(metric_name, uma_file_type);
+
+  rappor::RapporService* rappor_service = g_browser_process->rappor_service();
+  if (rappor_service && requestor.is_valid()) {
+    rappor_service->RecordSample(
+        metric_name, rappor::SAFEBROWSING_RAPPOR_TYPE,
+        rappor::GetDomainAndRegistrySampleFromGURL(requestor));
+  }
+}
+
+void RecordPolicyMetric(const std::string& metric_name,
+                        int uma_file_type,
+                        const GURL& requestor) {
+  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
+                          base::Bind(&RecordPolicyMetricOnUIThread, metric_name,
+                                     uma_file_type, requestor));
+}
 
 void RespondWithPolicy(
     const base::FilePath& file,
     const UnverifiedDownloadCheckCompletionCallback& callback,
+    const GURL& requestor,
     UnverifiedDownloadPolicy policy) {
   int uma_file_type =
       download_protection_util::GetSBClientDownloadExtensionValueForUMA(file);
   if (policy == UnverifiedDownloadPolicy::ALLOWED) {
-    UMA_HISTOGRAM_SPARSE_SLOWLY("SafeBrowsing.UnverifiedDownloads.Allowed",
-                                uma_file_type);
+    RecordPolicyMetric("SafeBrowsing.UnverifiedDownloads.Allowed",
+                       uma_file_type, requestor);
   } else {
-    UMA_HISTOGRAM_SPARSE_SLOWLY("SafeBrowsing.UnverifiedDownloads.Blocked",
-                                uma_file_type);
+    RecordPolicyMetric("SafeBrowsing.UnverifiedDownloads.Blocked",
+                       uma_file_type, requestor);
   }
-  content::BrowserThread::PostTask(content::BrowserThread::UI, FROM_HERE,
+  BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
                                    base::Bind(callback, policy));
 }
 
 void CheckFieldTrialOnAnyThread(
     const base::FilePath& file,
+    const GURL& requestor,
     const UnverifiedDownloadCheckCompletionCallback& callback) {
   bool is_allowed = IsUnverifiedDownloadAllowedByFieldTrial(file);
-  RespondWithPolicy(file, callback, is_allowed
+  RespondWithPolicy(file, callback, requestor, is_allowed
                                         ? UnverifiedDownloadPolicy::ALLOWED
                                         : UnverifiedDownloadPolicy::DISALLOWED);
 }
@@ -50,7 +78,7 @@ void CheckWhitelistOnIOThread(
     const GURL& requestor,
     const base::FilePath& file,
     const UnverifiedDownloadCheckCompletionCallback& callback) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
+  DCHECK_CURRENTLY_ON(BrowserThread::IO);
   int uma_file_type =
       download_protection_util::GetSBClientDownloadExtensionValueForUMA(file);
 
@@ -59,22 +87,24 @@ void CheckWhitelistOnIOThread(
     // field trial list. Instead allow the download. We are assuming that if the
     // SafeBrowsing service was disabled for this user, then we shouldn't
     // interefere with unverified downloads.
-    UMA_HISTOGRAM_SPARSE_SLOWLY(
+    RecordPolicyMetric(
         "SafeBrowsing.UnverifiedDownloads.AllowedDueToDisabledService",
-        uma_file_type);
-    RespondWithPolicy(file, callback, UnverifiedDownloadPolicy::ALLOWED);
+        uma_file_type, requestor);
+    RespondWithPolicy(file, callback, requestor,
+                      UnverifiedDownloadPolicy::ALLOWED);
     return;
   }
 
   if (service->database_manager() &&
       service->database_manager()->MatchDownloadWhitelistUrl(requestor)) {
-    UMA_HISTOGRAM_SPARSE_SLOWLY(
-        "SafeBrowsing.UnverifiedDownloads.AllowedByWhitelist", uma_file_type);
-    RespondWithPolicy(file, callback, UnverifiedDownloadPolicy::ALLOWED);
+    RecordPolicyMetric("SafeBrowsing.UnverifiedDownloads.AllowedByWhitelist",
+                       uma_file_type, requestor);
+    RespondWithPolicy(file, callback, requestor,
+                      UnverifiedDownloadPolicy::ALLOWED);
     return;
   }
 
-  CheckFieldTrialOnAnyThread(file, callback);
+  CheckFieldTrialOnAnyThread(file, requestor, callback);
 }
 
 }  // namespace
@@ -86,14 +116,14 @@ void CheckUnverifiedDownloadPolicy(
   if (requestor.is_valid()) {
     scoped_refptr<SafeBrowsingService> service =
         g_browser_process->safe_browsing_service();
-    content::BrowserThread::PostTask(
-        content::BrowserThread::IO, FROM_HERE,
+    BrowserThread::PostTask(
+        BrowserThread::IO, FROM_HERE,
         base::Bind(&CheckWhitelistOnIOThread, service, requestor, file,
                    callback));
     return;
   }
 
-  CheckFieldTrialOnAnyThread(file, callback);
+  CheckFieldTrialOnAnyThread(file, GURL(), callback);
 }
 
 }  // namespace safe_browsing
