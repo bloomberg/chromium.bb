99b36435202aff77469afb5218e208cd7aa05fa0
jochen <jochen@chromium.org>
2015-12-21 05:57:10 -0800

    Remove call to DumpWithoutCrashing, as we have enough data know
    Eventually, it would be nice if WebUI code was cleaned up, but until
    then it's safe to just ignore those aclls
    
    BUG=532172
    R=mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/1540893002
    
    Cr-Commit-Position: refs/heads/master@{#366373}
    
    Former-commit-id: d781f050bc3831330cd3110f3e7d6ee1d82d9f0d

--- content/browser/webui/web_ui_impl.cc
+++ content/browser/webui/web_ui_impl.cc
@@ -237,9 +237,8 @@ void WebUIImpl::ExecuteJavascript(const base::string16& javascript) {
           // It's possible to load about:blank in a Web UI renderer.
           // See http://crbug.com/42547
           target_frame->GetLastCommittedURL().spec() == url::kAboutBlankURL)) {
-      // Don't crash when we try to inject JavaScript into a non-WebUI page, but
-      // upload a crash report anyways. http://crbug.com/516690
-      base::debug::DumpWithoutCrashing();
+      // Silently ignore the request. Would be nice to clean-up WebUI so we
+      // could turn this into a CHECK(). http://crbug.com/516690.
       return;
     }
     target_frame->ExecuteJavaScript(javascript);
