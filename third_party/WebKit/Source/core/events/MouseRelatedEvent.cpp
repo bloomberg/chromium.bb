fcdc24a450e36bb16ec626d1493479841b25dce1
nzolghadr <nzolghadr@chromium.org>
2015-12-21 14:30:45 -0800

    Make MouseEvent offsetX/Y relative to the padding box
    Subtract borderleft/top width from the offsetX/Y to
    match FireFox behaviour which seems more reasonable.
    
    BUG=570666
    
    Review URL: https://codereview.chromium.org/1538893003
    
    Cr-Commit-Position: refs/heads/master@{#366475}
    
    Former-commit-id: ad76338520534c07a557bb68db876fbf2c34ce49

--- third_party/WebKit/Source/core/events/MouseRelatedEvent.cpp
+++ third_party/WebKit/Source/core/events/MouseRelatedEvent.cpp
@@ -145,9 +145,16 @@ void MouseRelatedEvent::computeRelativePosition()
     // Must have an updated layout tree for this math to work correctly.
     targetNode->document().updateLayoutIgnorePendingStylesheets();
 
-    // Adjust offsetLocation to be relative to the target's position.
+    // Adjust offsetLocation to be relative to the target's padding box.
     if (LayoutObject* r = targetNode->layoutObject()) {
         FloatPoint localPos = r->absoluteToLocal(FloatPoint(absoluteLocation()), UseTransforms);
+
+        // Adding this here to address crbug.com/570666. Basically we'd like to
+        // find the local coordinates relative to the padding box not the border box.
+        LayoutBoxModelObject* layoutBox = toLayoutBoxModelObject(r);
+        if (layoutBox)
+            localPos.move(-layoutBox->borderLeft(), -layoutBox->borderTop());
+
         m_offsetLocation = roundedLayoutPoint(localPos);
         float scaleFactor = 1 / pageZoomFactor(this);
         if (scaleFactor != 1.0f)
