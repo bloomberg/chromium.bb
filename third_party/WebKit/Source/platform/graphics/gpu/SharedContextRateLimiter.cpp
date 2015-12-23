fe77af4bef5e50b17c3db53171ff184edfcb543e
junov <junov@chromium.org>
2015-12-23 11:11:29 -0800

    Adding null pointer checks to SharedContextRateLimiter
    BUG=570615
    TBR=senorblanco@chromium.org
    
    Review URL: https://codereview.chromium.org/1542283002
    
    Cr-Commit-Position: refs/heads/master@{#366760}
    
    Former-commit-id: bad953220c6e7b4b4ee88905e68188074a8827b3

--- third_party/WebKit/Source/platform/graphics/gpu/SharedContextRateLimiter.cpp
+++ third_party/WebKit/Source/platform/graphics/gpu/SharedContextRateLimiter.cpp
@@ -22,39 +22,53 @@ PassOwnPtr<SharedContextRateLimiter> SharedContextRateLimiter::create(unsigned m
 
 SharedContextRateLimiter::SharedContextRateLimiter(unsigned maxPendingTicks)
     : m_maxPendingTicks(maxPendingTicks)
+    , m_canUseSyncQueries(false)
 {
     m_contextProvider = adoptPtr(Platform::current()->createSharedOffscreenGraphicsContext3DProvider());
+    if (!m_contextProvider)
+        return;
+
     WebGraphicsContext3D* context = m_contextProvider->context3d();
-    OwnPtr<Extensions3DUtil> extensionsUtil = Extensions3DUtil::create(context);
-    // TODO(junov): when the GLES 3.0 command buffer is ready, we could use fenceSync instead
-    m_canUseSyncQueries = extensionsUtil->supportsExtension("GL_CHROMIUM_sync_query");
+    if (context && !context->isContextLost()) {
+        OwnPtr<Extensions3DUtil> extensionsUtil = Extensions3DUtil::create(context);
+        // TODO(junov): when the GLES 3.0 command buffer is ready, we could use fenceSync instead
+        m_canUseSyncQueries = extensionsUtil->supportsExtension("GL_CHROMIUM_sync_query");
+    }
 }
 
 void SharedContextRateLimiter::tick()
 {
+    if (!m_contextProvider)
+        return;
+
     WebGraphicsContext3D* context = m_contextProvider->context3d();
-    if (context && !context->isContextLost()) {
-        m_queries.append(m_canUseSyncQueries ? context->createQueryEXT() : 0);
+
+    if (!context || context->isContextLost())
+        return;
+
+    m_queries.append(m_canUseSyncQueries ? context->createQueryEXT() : 0);
+    if (m_canUseSyncQueries) {
+        context->beginQueryEXT(GL_COMMANDS_COMPLETED_CHROMIUM, m_queries.last());
+        context->endQueryEXT(GL_COMMANDS_COMPLETED_CHROMIUM);
+    }
+    if (m_queries.size() > m_maxPendingTicks) {
         if (m_canUseSyncQueries) {
-            context->beginQueryEXT(GL_COMMANDS_COMPLETED_CHROMIUM, m_queries.last());
-            context->endQueryEXT(GL_COMMANDS_COMPLETED_CHROMIUM);
-        }
-        if (m_queries.size() > m_maxPendingTicks) {
-            if (m_canUseSyncQueries) {
-                WGC3Duint result;
-                context->getQueryObjectuivEXT(m_queries.first(), GL_QUERY_RESULT_EXT, &result);
-                context->deleteQueryEXT(m_queries.first());
-                m_queries.removeFirst();
-            } else {
-                context->finish();
-                reset();
-            }
+            WGC3Duint result;
+            context->getQueryObjectuivEXT(m_queries.first(), GL_QUERY_RESULT_EXT, &result);
+            context->deleteQueryEXT(m_queries.first());
+            m_queries.removeFirst();
+        } else {
+            context->finish();
+            reset();
         }
     }
 }
 
 void SharedContextRateLimiter::reset()
 {
+    if (!m_contextProvider)
+        return;
+
     WebGraphicsContext3D* context = m_contextProvider->context3d();
     if (context && !context->isContextLost()) {
         while (m_queries.size() > 0) {
