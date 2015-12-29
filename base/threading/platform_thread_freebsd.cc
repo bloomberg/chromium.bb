0bc8de4fd8785427976d03dc86e9a9bbfa06cceb
l2d4y3 <l2d4y3@gmail.com>
2015-12-28 19:38:29 -0800

    Fix platform_thread_freebsd.cc
    Review URL: https://codereview.chromium.org/1547283002
    
    Cr-Commit-Position: refs/heads/master@{#367047}
    
    Former-commit-id: 5da36b56a662e9a1bac59eaa48c319b08092d9f1

--- base/threading/platform_thread_freebsd.cc
+++ base/threading/platform_thread_freebsd.cc
@@ -16,7 +16,6 @@
 
 #if !defined(OS_NACL)
 #include <pthread.h>
-#include <sys/prctl.h>
 #include <sys/types.h>
 #include <unistd.h>
 #endif
@@ -36,7 +35,7 @@ const ThreadPriorityToNiceValuePair kThreadPriorityToNiceValueMap[4] = {
     {ThreadPriority::NORMAL, 0},
     {ThreadPriority::DISPLAY, -6},
     {ThreadPriority::REALTIME_AUDIO, -10},
-}
+};
 
 bool SetCurrentThreadPriorityForPlatform(ThreadPriority priority) {
 #if !defined(OS_NACL)
