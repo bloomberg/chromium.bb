1170f8bf0a5bcc90e94f215dd64c530949f46dc8
mmoroz <mmoroz@chromium.org>
2015-12-30 07:59:07 -0800

    port libxml_xml_read_memory_fuzzer (from kcc's libfuzzer-bot) and disable error messages from the parser
    R=aizatsky@chromium.org, inferno@chromium.org,kcc@chromium.org
    TBR=krasin@chromium.org
    BUG=569578
    
    Review URL: https://codereview.chromium.org/1558483003
    
    Cr-Commit-Position: refs/heads/master@{#367154}
    
    Former-commit-id: 58074fe91075fdbde6c2454d153af3dfa7248be7

---
+++ testing/libfuzzer/fuzzers/libxml_xml_read_memory_fuzzer.cc
@@ -0,0 +1,20 @@
+// Copyright (c) 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "libxml/parser.h"
+
+void ignore (void * ctx, const char * msg, ...) {
+  // Error handler to avoid spam of error messages from libxml parser.
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
+  xmlSetGenericErrorFunc(NULL, &ignore);
+
+  if (auto doc = xmlReadMemory(reinterpret_cast<const char *>(data),
+                               size, "noname.xml", NULL, 0)) {
+    xmlFreeDoc(doc);
+  }
+
+  return 0;
+}
