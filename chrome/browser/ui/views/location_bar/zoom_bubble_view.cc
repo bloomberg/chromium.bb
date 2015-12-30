fb107c88912be273f43eebcf8c19ec575b932c2f
estade <estade@chromium.org>
2015-12-30 14:57:00 -0800

    Remove unnecessary call to SetHorizontalAlignment in ZoomBubbleView
    ALIGN_CENTER is already default for STYLE_BUTTON.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1556543002
    
    Cr-Commit-Position: refs/heads/master@{#367198}
    
    Former-commit-id: b95abc9b01c6db74179c70bd4d516944a063b522

--- chrome/browser/ui/views/location_bar/zoom_bubble_view.cc
+++ chrome/browser/ui/views/location_bar/zoom_bubble_view.cc
@@ -191,7 +191,6 @@ void ZoomBubbleView::Init() {
   views::LabelButton* set_default_button = new views::LabelButton(
       this, l10n_util::GetStringUTF16(IDS_ZOOM_SET_DEFAULT));
   set_default_button->SetStyle(views::Button::STYLE_BUTTON);
-  set_default_button->SetHorizontalAlignment(gfx::ALIGN_CENTER);
   grid_layout->AddView(set_default_button);
 
   StartTimerIfNecessary();
