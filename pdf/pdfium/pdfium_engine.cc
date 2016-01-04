0a0ec6982e906eb42926bbdd075c935c7e60d10d
thestig <thestig@chromium.org>
2016-01-04 13:18:17 -0800

    Simplify PDFiumEngine::GetFlattenedPrintData().
    Review URL: https://codereview.chromium.org/1549013003
    
    Cr-Commit-Position: refs/heads/master@{#367370}
    
    Former-commit-id: a45c0ddbd45a6a38f7a71d29304ceb8eb0a63222

--- pdf/pdfium/pdfium_engine.cc
+++ pdf/pdfium/pdfium_engine.cc
@@ -1445,29 +1445,17 @@ pp::Buffer_Dev PDFiumEngine::PrintPagesAsRasterPDF(
 }
 
 pp::Buffer_Dev PDFiumEngine::GetFlattenedPrintData(const FPDF_DOCUMENT& doc) {
+  pp::Buffer_Dev buffer;
   int page_count = FPDF_GetPageCount(doc);
-  bool flatten_succeeded = true;
   for (int i = 0; i < page_count; ++i) {
     FPDF_PAGE page = FPDF_LoadPage(doc, i);
     DCHECK(page);
-    if (page) {
-      int flatten_ret = FPDFPage_Flatten(page, FLAT_PRINT);
-      FPDF_ClosePage(page);
-      if (flatten_ret == FLATTEN_FAIL) {
-        flatten_succeeded = false;
-        break;
-      }
-    } else {
-      flatten_succeeded = false;
-      break;
-    }
-  }
-  if (!flatten_succeeded) {
-    FPDF_CloseDocument(doc);
-    return pp::Buffer_Dev();
+    int flatten_ret = FPDFPage_Flatten(page, FLAT_PRINT);
+    FPDF_ClosePage(page);
+    if (flatten_ret == FLATTEN_FAIL)
+      return buffer;
   }
 
-  pp::Buffer_Dev buffer;
   PDFiumMemBufferFileWrite output_file_write;
   if (FPDF_SaveAsCopy(doc, &output_file_write, 0)) {
     buffer = pp::Buffer_Dev(
