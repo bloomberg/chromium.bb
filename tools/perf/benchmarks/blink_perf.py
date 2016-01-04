8e36547217eeeecb3435739de80ee5091df73a6d
wangxianzhu <wangxianzhu@chromium.org>
2016-01-04 11:43:32 -0800

    PerformanceTests/Layout/large-table-background-change* to Paint
    The large-table-background-change* tests are actually testing
    performance of paint invalidation and paint.
    
    Justifications for the new benchmark (https://docs.google.com/document/d/1bBKyYCW3VlUUPDpQE4xvrMFdA6tovQMZoqO9KCcmqqQ/edit#):
    
    1. Demonstrate an example regression or improvement.
    CL: https://codereview.chromium.org/1549693002/
    Try results:
      https://codereview.chromium.org/1542263004/
      https://codereview.chromium.org/1543263003/
      https://codereview.chromium.org/1543333002/
    
    2. Monitor the benchmark or metric until alerting is stable.
    The tests has been running under blink_perf.layout for some days.
    
    3. Benchmarks should run for less than 5 minutes on linux perf trybots.
    The new benchmark run for a few seconds.
    
    4. Whenever possible, avoid creating new benchmarks.
    The new benchmark includes some tests that previously under blink_perf.layout.
    This CL doesn't add new tests or new metrics.
    
    5. Run benchmarks on perf trybots on all platforms before submitting.
    Done.
    
    BUG=34736
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect
    
    Review URL: https://codereview.chromium.org/1550833002
    
    Cr-Commit-Position: refs/heads/master@{#367344}
    
    Former-commit-id: cae008ceb3508fb4607a841bfd9d99492c304b05

--- tools/perf/benchmarks/blink_perf.py
+++ tools/perf/benchmarks/blink_perf.py
@@ -255,6 +255,19 @@ class BlinkPerfLayoutFullLayout(BlinkPerfLayout):
     return 'blink_perf.layout_full_frame'
 
 
+class BlinkPerfPaint(perf_benchmark.PerfBenchmark):
+  tag = 'paint'
+  test = _BlinkPerfMeasurement
+
+  @classmethod
+  def Name(cls):
+    return 'blink_perf.paint'
+
+  def CreateStorySet(self, options):
+    path = os.path.join(BLINK_PERF_BASE_DIR, 'Paint')
+    return CreateStorySetFromPath(path, SKIPPED_FILE)
+
+
 @benchmark.Disabled('win',     # crbug.com/488493
                     'android') # crbug.com/527156
 class BlinkPerfParser(perf_benchmark.PerfBenchmark):
