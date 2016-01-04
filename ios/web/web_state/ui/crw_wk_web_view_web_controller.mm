86c6b5c2a6f40b3a2a9a7d48d7f9fd2624fdc689
eugenebut <eugenebut@chromium.org>
2016-01-04 14:30:00 -0800

    [ios] Make sure that |didFinishNavigation| is signalled for fast goBack.
    A fast back/forward may not call |webView:didFinishNavigation:|, so
    finishing the navigation should be signalled explicitely when WKWebView
    isLoading is changed to NO.
    
    BUG=571584
    
    Review URL: https://codereview.chromium.org/1546603005
    
    Cr-Commit-Position: refs/heads/master@{#367390}
    
    Former-commit-id: 7ce600bd5b62b2c79b288a5bf493251b8e8aa59a

--- ios/web/web_state/ui/crw_wk_web_view_web_controller.mm
+++ ios/web/web_state/ui/crw_wk_web_view_web_controller.mm
@@ -1616,6 +1616,12 @@ WKWebViewErrorSource WKWebViewErrorSourceFromError(NSError* error) {
     [self addActivityIndicatorTask];
   } else {
     [self clearActivityIndicatorTasks];
+    if ([self currentBackForwardListItemHolder]->navigation_type() ==
+        WKNavigationTypeBackForward) {
+      // A fast back/forward may not call |webView:didFinishNavigation:|, so
+      // finishing the navigation should be signalled explicitly.
+      [self didFinishNavigation];
+    }
   }
 }
 
