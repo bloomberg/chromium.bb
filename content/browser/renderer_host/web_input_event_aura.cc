aea717f1e55ff951854690d3fa526a322fdc7dd2
dtapuska <dtapuska@chromium.org>
2016-01-04 15:56:56 -0800

    Keyboard events with keypad had wrong keycodes on ozone.
    Ozone is generating virtual keys not like Windows and X11 are generating
    them. The removal of some code undercovered the issue. Add back some
    of the partial conversion code (numpad conversions only) to address
    the issue for the users. Whether numpad keys are "located" or not
    needs to be re-evaluated and all platforms made consistent.
    Documentation claims that numpad events are located but the
    platforms generate them to be consistent with windows.
    
    Ozone seemed to be the only platform that didn't generate
    them. Ideally the fix should be in the
    xkb_keyboard_layout_engine.cc part of ozone but for
    simplicity sake and that this needs to be backported to M48; restore some of
    https://codereview.chromium.org/1308063007 CL with respect to rewrite numpads based events.
    
    BUG=570388
    
    Review URL: https://codereview.chromium.org/1550953002
    
    Cr-Commit-Position: refs/heads/master@{#367418}
    
    Former-commit-id: f3c39ae68b3c462a7918b8ae092e6d322c418047

--- content/browser/renderer_host/web_input_event_aura.cc
+++ content/browser/renderer_host/web_input_event_aura.cc
@@ -13,6 +13,7 @@
 #include "ui/events/event.h"
 #include "ui/events/event_utils.h"
 #include "ui/events/keycodes/dom/keycode_converter.h"
+#include "ui/events/keycodes/keyboard_code_conversion.h"
 
 namespace content {
 
@@ -86,7 +87,11 @@ blink::WebKeyboardEvent MakeWebKeyboardEventFromAuraEvent(
   if (webkit_event.modifiers & blink::WebInputEvent::AltKey)
     webkit_event.isSystemKey = true;
 
-  webkit_event.windowsKeyCode = event.key_code();
+  // TODO(dtapuska): crbug.com/570388. Ozone appears to deliver
+  // key_code events that aren't "located" for the keypad like
+  // Windows and X11 do and blink expects.
+  webkit_event.windowsKeyCode =
+      ui::NonLocatedToLocatedKeypadKeyboardCode(event.key_code(), event.code());
   webkit_event.nativeKeyCode =
     ui::KeycodeConverter::DomCodeToNativeKeycode(event.code());
   webkit_event.domCode = static_cast<int>(event.code());
