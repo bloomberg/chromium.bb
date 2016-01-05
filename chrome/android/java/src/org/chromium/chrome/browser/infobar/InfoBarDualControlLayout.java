cb0e5f8b822c887fbb727fb25a152830c0b4efab
dfalcantara <dfalcantara@chromium.org>
2016-01-05 10:37:32 -0800

    Add ViewGroup for handling pairs of infobar controls
    A recurring pattern in the infobar spec is to have two
    controls that are placed horizontally if they fit, but
    get stacked vertically otherwise.  This happens for
    buttons on the infobar row and for any control where
    there is both a label and a value (e.g. spinners or
    credit card information).  The only differences
    between them are defined by how the two controls are
    laid out.
    
    This CL introduces the InfoBarDualControlLayout for
    handling these cases, but doesn't change the infobar
    classes to use it, yet.  It's meant to be general
    enough for all of the cases above to plug into it.
    
    BUG=543205
    
    Review URL: https://codereview.chromium.org/1553813002
    
    Cr-Commit-Position: refs/heads/master@{#367590}
    
    Former-commit-id: 32f6f55409009f43e0425008bcc20aa57dbeac8e

---
+++ chrome/android/java/src/org/chromium/chrome/browser/infobar/InfoBarDualControlLayout.java
@@ -0,0 +1,189 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.infobar;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.chromium.base.ApiCompatibilityUtils;
+import org.chromium.base.VisibleForTesting;
+import org.chromium.chrome.R;
+
+/**
+ * Automatically lays out one or two Views for an infobar, placing them on the same row if possible
+ * and stacking them otherwise.
+ *
+ * Use cases of this Layout include placement of infobar buttons and placement of TextViews inside
+ * of spinner controls (http://goto.google.com/infobar-spec).
+ *
+ * Layout parameters (i.e. margins) are ignored to enforce infobar consistency.  Alignment defines
+ * where the controls are placed (for RTL, flip everything):
+ *
+ * ALIGN_START                      ALIGN_APART                      ALIGN_END
+ * -----------------------------    -----------------------------    -----------------------------
+ * | PRIMARY SECONDARY         |    | SECONDARY         PRIMARY |    |         SECONDARY PRIMARY |
+ * -----------------------------    -----------------------------    -----------------------------
+ *
+ * Controls are stacked automatically when they don't fit on the same row, with each control taking
+ * up the full available width and with the primary control sitting on top of the secondary.
+ * -----------------------------
+ * | PRIMARY------------------ |
+ * | SECONDARY---------------- |
+ * -----------------------------
+ *
+ * TODO(dfalcantara): Remove the VisibleForTesting annotations when controls that this land.
+ */
+@VisibleForTesting
+public final class InfoBarDualControlLayout extends ViewGroup {
+    public static final int ALIGN_START = 0;
+    public static final int ALIGN_END = 1;
+    public static final int ALIGN_APART = 2;
+
+    private final int mHorizontalMarginBetweenViews;
+
+    private int mAlignment = ALIGN_START;
+    private int mStackedMargin;
+
+    private boolean mIsStacked;
+    private View mPrimaryView;
+    private View mSecondaryView;
+
+    /**
+     * Construct a new InfoBarDualControlLayout.
+     *
+     * See {@link ViewGroup} for parameter details.  attrs may be null if constructed dynamically.
+     */
+    @VisibleForTesting
+    public InfoBarDualControlLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        // Cache dimensions.
+        Resources resources = getContext().getResources();
+        mHorizontalMarginBetweenViews =
+                resources.getDimensionPixelSize(R.dimen.infobar_control_margin_between_items);
+    }
+
+    /**
+     * Define how the controls will be laid out.
+     *
+     * @param alignment One of ALIGN_START, ALIGN_APART, ALIGN_END.
+     */
+    @VisibleForTesting
+    void setAlignment(int alignment) {
+        mAlignment = alignment;
+    }
+
+    /**
+     * Sets the margin between the controls when they're stacked.  By default, there is no margin.
+     */
+    @VisibleForTesting
+    void setStackedMargin(int stackedMargin) {
+        mStackedMargin = stackedMargin;
+    }
+
+    @Override
+    public void onViewAdded(View child) {
+        super.onViewAdded(child);
+
+        if (mPrimaryView == null) {
+            mPrimaryView = child;
+        } else if (mSecondaryView == null) {
+            mSecondaryView = child;
+        } else {
+            throw new IllegalStateException("Too many children added to InfoBarDualControlLayout");
+        }
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        mIsStacked = false;
+
+        // Measure the primary View, allowing it to be as wide as the Layout.
+        int maxWidth = MeasureSpec.getMode(widthMeasureSpec) == MeasureSpec.UNSPECIFIED
+                ? Integer.MAX_VALUE : MeasureSpec.getSize(widthMeasureSpec);
+        int unspecifiedSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+        measureChild(mPrimaryView, unspecifiedSpec, unspecifiedSpec);
+
+        int layoutWidth = mPrimaryView.getMeasuredWidth();
+        int layoutHeight = mPrimaryView.getMeasuredHeight();
+
+        if (mSecondaryView != null) {
+            // Measure the secondary View, allowing it to be as wide as the layout.
+            measureChild(mSecondaryView, unspecifiedSpec, unspecifiedSpec);
+            int combinedWidth = mPrimaryView.getMeasuredWidth()
+                    + mHorizontalMarginBetweenViews + mSecondaryView.getMeasuredWidth();
+
+            if (combinedWidth > maxWidth) {
+                // Stack the Views on top of each other.
+                mIsStacked = true;
+
+                int widthSpec = MeasureSpec.makeMeasureSpec(maxWidth, MeasureSpec.EXACTLY);
+                mPrimaryView.measure(widthSpec, unspecifiedSpec);
+                mSecondaryView.measure(widthSpec, unspecifiedSpec);
+
+                layoutWidth = maxWidth;
+                layoutHeight = mPrimaryView.getMeasuredHeight() + mStackedMargin
+                        + mSecondaryView.getMeasuredHeight();
+            } else {
+                // The Views fit side by side.  Check which is taller to find the layout height.
+                layoutWidth = combinedWidth;
+                layoutHeight = Math.max(layoutHeight, mSecondaryView.getMeasuredHeight());
+            }
+        }
+
+        setMeasuredDimension(resolveSize(layoutWidth, widthMeasureSpec),
+                resolveSize(layoutHeight, heightMeasureSpec));
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        int width = right - left;
+        boolean isRtl = ApiCompatibilityUtils.isLayoutRtl(this);
+        boolean isPrimaryOnRight = (isRtl && mAlignment == ALIGN_START)
+                || (!isRtl && (mAlignment == ALIGN_APART || mAlignment == ALIGN_END));
+
+        int primaryRight = isPrimaryOnRight ? width : mPrimaryView.getMeasuredWidth();
+        int primaryLeft = primaryRight - mPrimaryView.getMeasuredWidth();
+        int primaryHeight = mPrimaryView.getMeasuredHeight();
+        mPrimaryView.layout(primaryLeft, 0, primaryRight, primaryHeight);
+
+        if (mIsStacked) {
+            // Fill out the row.  onMeasure() should have already applied the correct width.
+            int secondaryTop = primaryHeight + mStackedMargin;
+            int secondaryBottom = secondaryTop + mSecondaryView.getMeasuredHeight();
+            mSecondaryView.layout(
+                    0, secondaryTop, mSecondaryView.getMeasuredWidth(), secondaryBottom);
+        } else if (mSecondaryView != null) {
+            // Center the secondary View vertically with the primary View.
+            int secondaryHeight = mSecondaryView.getMeasuredHeight();
+            int primaryCenter = primaryHeight / 2;
+            int secondaryTop = primaryCenter - (secondaryHeight / 2);
+            int secondaryBottom = secondaryTop + secondaryHeight;
+
+            // Determine where to place the secondary View.
+            int secondaryLeft;
+            int secondaryRight;
+            if (mAlignment == ALIGN_APART) {
+                // Put the second View on the other side of the Layout from the primary View.
+                secondaryLeft = isPrimaryOnRight ? 0 : width - mSecondaryView.getMeasuredWidth();
+                secondaryRight = secondaryLeft + mSecondaryView.getMeasuredWidth();
+            } else if (isPrimaryOnRight) {
+                // Sit to the left of the primary View.
+                secondaryRight = primaryLeft - mHorizontalMarginBetweenViews;
+                secondaryLeft = secondaryRight - mSecondaryView.getMeasuredWidth();
+            } else {
+                // Sit to the right of the primary View.
+                secondaryLeft = primaryRight + mHorizontalMarginBetweenViews;
+                secondaryRight = secondaryLeft + mSecondaryView.getMeasuredWidth();
+            }
+
+            mSecondaryView.layout(
+                    secondaryLeft, secondaryTop, secondaryRight, secondaryBottom);
+        }
+    }
+}
---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/infobar/InfoBarDualControlLayoutTest.java
@@ -0,0 +1,198 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.infobar;
+
+import static org.chromium.chrome.browser.infobar.InfoBarDualControlLayout.ALIGN_APART;
+import static org.chromium.chrome.browser.infobar.InfoBarDualControlLayout.ALIGN_END;
+import static org.chromium.chrome.browser.infobar.InfoBarDualControlLayout.ALIGN_START;
+
+import android.content.Context;
+import android.test.InstrumentationTestCase;
+import android.test.MoreAsserts;
+import android.test.UiThreadTest;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.view.View;
+import android.view.View.MeasureSpec;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Space;
+
+import org.chromium.chrome.R;
+
+/**
+ * Tests for InfoBarDualControlLayout.
+ */
+public class InfoBarDualControlLayoutTest extends InstrumentationTestCase {
+    private static final int PRIMARY_HEIGHT = 16;
+    private static final int SECONDARY_HEIGHT = 8;
+    private static final int STACKED_MARGIN = 4;
+
+    private int mMaxInfoBarWidth;
+    private int mTinyControlWidth;
+    private Context mContext;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mContext = getInstrumentation().getTargetContext();
+        mContext.setTheme(R.style.MainTheme);
+        mMaxInfoBarWidth = mContext.getResources().getDimensionPixelSize(R.dimen.infobar_max_width);
+        mTinyControlWidth = mMaxInfoBarWidth / 4;
+    }
+
+    @SmallTest
+    @UiThreadTest
+    public void testAlignSideBySide() {
+        runLayoutTest(ALIGN_START, false, false);
+        runLayoutTest(ALIGN_START, false, true);
+        runLayoutTest(ALIGN_START, true, false);
+        runLayoutTest(ALIGN_START, true, true);
+
+        runLayoutTest(ALIGN_APART, false, false);
+        runLayoutTest(ALIGN_APART, false, true);
+        runLayoutTest(ALIGN_APART, true, false);
+        runLayoutTest(ALIGN_APART, true, true);
+
+        runLayoutTest(ALIGN_END, false, false);
+        runLayoutTest(ALIGN_END, false, true);
+        runLayoutTest(ALIGN_END, true, false);
+        runLayoutTest(ALIGN_END, true, true);
+    }
+
+    /** Lays out two controls that fit on the same line. */
+    private void runLayoutTest(int alignment, boolean isRtl, boolean addSecondView) {
+        InfoBarDualControlLayout layout = new InfoBarDualControlLayout(mContext, null);
+        layout.setAlignment(alignment);
+        layout.setLayoutDirection(isRtl ? View.LAYOUT_DIRECTION_RTL : View.LAYOUT_DIRECTION_LTR);
+        layout.setLayoutParams(
+                new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+
+        View primary = new Space(mContext);
+        primary.setMinimumWidth(mTinyControlWidth);
+        primary.setMinimumHeight(PRIMARY_HEIGHT);
+        layout.addView(primary);
+
+        View secondary = null;
+        if (addSecondView) {
+            secondary = new Space(mContext);
+            secondary.setMinimumWidth(mTinyControlWidth);
+            secondary.setMinimumHeight(SECONDARY_HEIGHT);
+            layout.addView(secondary);
+        }
+
+        // Trigger the measurement & layout algorithms.
+        int parentWidthSpec = MeasureSpec.makeMeasureSpec(mMaxInfoBarWidth, MeasureSpec.EXACTLY);
+        int parentHeightSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+        layout.measure(parentWidthSpec, parentHeightSpec);
+        layout.layout(0, 0, layout.getMeasuredWidth(), layout.getMeasuredHeight());
+
+        // Confirm that the primary View is in the correct place.
+        if ((isRtl && alignment == ALIGN_START)
+                || (!isRtl && (alignment == ALIGN_APART || alignment == ALIGN_END))) {
+            assertEquals("Primary should be on the right.", mMaxInfoBarWidth, primary.getRight());
+        } else {
+            assertEquals("Primary should be on the left.", 0, primary.getLeft());
+        }
+        assertEquals(mTinyControlWidth, primary.getMeasuredWidth());
+        assertEquals(PRIMARY_HEIGHT, primary.getMeasuredHeight());
+        MoreAsserts.assertNotEqual(primary.getLeft(), primary.getRight());
+
+        // Confirm that the secondary View is in the correct place.
+        if (secondary != null) {
+            assertEquals(mTinyControlWidth, secondary.getMeasuredWidth());
+            assertEquals(SECONDARY_HEIGHT, secondary.getMeasuredHeight());
+            MoreAsserts.assertNotEqual(secondary.getLeft(), secondary.getRight());
+            if (alignment == ALIGN_START) {
+                if (isRtl) {
+                    // Secondary View is immediately to the left of the parent.
+                    assertTrue(secondary.getRight() < primary.getLeft());
+                    MoreAsserts.assertNotEqual(0, secondary.getLeft());
+                } else {
+                    // Secondary View is immediately to the right of the parent.
+                    assertTrue(primary.getRight() < secondary.getLeft());
+                    MoreAsserts.assertNotEqual(mMaxInfoBarWidth, secondary.getRight());
+                }
+            } else if (alignment == ALIGN_APART) {
+                if (isRtl) {
+                    // Secondary View is on the far right.
+                    assertTrue(primary.getRight() < secondary.getLeft());
+                    assertEquals(mMaxInfoBarWidth, secondary.getRight());
+                } else {
+                    // Secondary View is on the far left.
+                    assertTrue(secondary.getRight() < primary.getLeft());
+                    assertEquals(0, secondary.getLeft());
+                }
+            } else {
+                assertEquals(ALIGN_END, alignment);
+                if (isRtl) {
+                    // Secondary View is immediately to the right of the parent.
+                    assertTrue(primary.getRight() < secondary.getLeft());
+                    MoreAsserts.assertNotEqual(mMaxInfoBarWidth, secondary.getRight());
+                } else {
+                    // Secondary View is immediately to the left of the parent.
+                    assertTrue(secondary.getRight() < primary.getLeft());
+                    MoreAsserts.assertNotEqual(0, secondary.getLeft());
+                }
+            }
+
+            // Confirm that the secondary View is centered with respect to the first.
+            int primaryCenter = (primary.getTop() + primary.getBottom()) / 2;
+            int secondaryCenter = (secondary.getTop() + secondary.getBottom()) / 2;
+            assertEquals(primaryCenter, secondaryCenter);
+        }
+
+        assertEquals(layout.getMeasuredHeight(), primary.getMeasuredHeight());
+    }
+
+    @SmallTest
+    @UiThreadTest
+    public void testStacked() {
+        runStackedLayoutTest(ALIGN_START, false);
+        runStackedLayoutTest(ALIGN_START, true);
+        runStackedLayoutTest(ALIGN_APART, false);
+        runStackedLayoutTest(ALIGN_APART, true);
+        runStackedLayoutTest(ALIGN_END, false);
+        runStackedLayoutTest(ALIGN_END, true);
+    }
+
+    /** Runs a test where the controls don't fit on the same line. */
+    private void runStackedLayoutTest(int alignment, boolean isRtl) {
+        InfoBarDualControlLayout layout = new InfoBarDualControlLayout(mContext, null);
+        layout.setAlignment(alignment);
+        layout.setStackedMargin(STACKED_MARGIN);
+        layout.setLayoutDirection(isRtl ? View.LAYOUT_DIRECTION_RTL : View.LAYOUT_DIRECTION_LTR);
+        layout.setLayoutParams(
+                new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT));
+
+        View primary = new Space(mContext);
+        primary.setMinimumWidth(mTinyControlWidth);
+        primary.setMinimumHeight(PRIMARY_HEIGHT);
+        layout.addView(primary);
+
+        View secondary = new Space(mContext);
+        secondary.setMinimumWidth(mMaxInfoBarWidth);
+        secondary.setMinimumHeight(SECONDARY_HEIGHT);
+        layout.addView(secondary);
+
+        // Trigger the measurement & layout algorithms.
+        int parentWidthSpec =
+                MeasureSpec.makeMeasureSpec(mMaxInfoBarWidth, MeasureSpec.AT_MOST);
+        int parentHeightSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+        layout.measure(parentWidthSpec, parentHeightSpec);
+        layout.layout(0, 0, layout.getMeasuredWidth(), layout.getMeasuredHeight());
+
+        assertEquals(0, primary.getLeft());
+        assertEquals(0, secondary.getLeft());
+
+        assertEquals(mMaxInfoBarWidth, primary.getRight());
+        assertEquals(mMaxInfoBarWidth, secondary.getRight());
+
+        assertEquals(mMaxInfoBarWidth, primary.getMeasuredWidth());
+        assertEquals(mMaxInfoBarWidth, secondary.getMeasuredWidth());
+        assertEquals(mMaxInfoBarWidth, layout.getMeasuredWidth());
+
+        assertEquals(primary.getBottom() + STACKED_MARGIN, secondary.getTop());
+        MoreAsserts.assertNotEqual(layout.getMeasuredHeight(), primary.getMeasuredHeight());
+    }
+}
