cbde1da6dfa8f4acd9bb2e8b1c1d36284cfc5bfd
brucedawson <brucedawson@chromium.org>
2016-01-05 14:53:31 -0800

    Remove duplicated/old line of code from bad merge
    While doing the merge for Patch Set 19: git pull in CL
    crrev.com/1422773008 I inadvertently retained both the fixed line
    of code *and* the original line of code. This was discovered because
    the old line of code triggered the original warning.
    
    The duplicate line of code should be harmless (a NOP) but needs to
    be removed.
    
    BUG=440500
    
    Review URL: https://codereview.chromium.org/1558293003
    
    Cr-Commit-Position: refs/heads/master@{#367674}
    
    Former-commit-id: dce36b87947c528aaf81e73285e85fb2b754bb67

--- third_party/WebKit/Source/wtf/BitVector.cpp
+++ third_party/WebKit/Source/wtf/BitVector.cpp
@@ -77,7 +77,6 @@ BitVector::OutOfLineBits* BitVector::OutOfLineBits::create(size_t numBits)
     // Because of the way BitVector stores the pointer, memory tools
     // will erroneously report a leak here.
     WTF_INTERNAL_LEAK_SANITIZER_DISABLED_SCOPE;
-    numBits = (numBits + bitsInPointer() - 1) & ~(bitsInPointer() - 1);
     numBits = (numBits + bitsInPointer() - 1) & ~(bitsInPointer() - static_cast<size_t>(1));
     size_t size = sizeof(OutOfLineBits) + sizeof(uintptr_t) * (numBits / bitsInPointer());
     void* allocation = Partitions::bufferMalloc(size, WTF_HEAP_PROFILER_TYPE_NAME(OutOfLineBits));
