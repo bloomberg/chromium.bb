97eb070ce86200922b8cdc132929551c02b62820
tommycli <tommycli@chromium.org>
2016-01-06 10:29:11 -0800

    Revert of Plugin Power Saver: Fix flaky pixel tests on ChromeOS=1 on Linux. (patchset #2 id:20001 of https://codereview.chromium.org/1554213003/ )
    Reason for revert:
    Breaks MSaN bots. Doesn't completely fix the problem anyways. :(
    
    Original issue's description:
    > Plugin Power Saver: Fix flaky pixel tests on ChromeOS=1 on Linux.
    >
    > This fixes the pixel test flakiness (at least on my machine).
    >
    > Flakiness exhibited here: http://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=browser_tests&tests=PluginPowerSaver
    >
    > BUG=574266
    > TEST=MANUAL
    >
    > Committed: https://crrev.com/bae6a6bd7d7610f9083dc2576b878978572591ca [formerly 93b5dcc9e49a28747b227ffec714d1335b016426]
    > Cr-Commit-Position: refs/heads/master@{#367460}
    
    TBR=thestig@chromium.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=574266
    
    Review URL: https://codereview.chromium.org/1567663002
    
    Cr-Commit-Position: refs/heads/master@{#367864}
    
    Former-commit-id: bb16cd9710b61e4fef7cc256fbc5e3776443f820

--- chrome/browser/plugins/plugin_power_saver_browsertest.cc
+++ chrome/browser/plugins/plugin_power_saver_browsertest.cc
@@ -37,7 +37,6 @@
 #include "ui/gfx/geometry/point.h"
 #include "ui/gfx/screen.h"
 #include "ui/gfx/switches.h"
-#include "ui/gl/gl_switches.h"
 
 namespace {
 
@@ -284,13 +283,9 @@ class PluginPowerSaverBrowserTest : public InProcessBrowserTest {
     // Allows us to use the same reference image on HiDPI/Retina displays.
     command_line->AppendSwitchASCII(switches::kForceDeviceScaleFactor, "1");
 
-#if defined(OS_CHROMEOS)
-    // ChromeOS builds running on Linux use OSMesa by default. This is flaky
-    // with pixel tests, so force these environments to use hardware GPU.
-    command_line->AppendSwitch(switches::kUseGpuInTests);
-#else
-    // Software rendering is faster and more reliable for these tests.
-    // Unfortunately ChromeOS does not support software rendering.
+#if !defined(OS_CHROMEOS)
+    // These pixel tests are flaky on MSan bots with hardware rendering.
+    // However, ChromeOS does not support software compositing.
     command_line->AppendSwitch(switches::kDisableGpu);
 #endif
   }
