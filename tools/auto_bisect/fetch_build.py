f5a63f88677683bc6bf26764649dcb1eb3714d63
aiolos <aiolos@chromium.org>
2016-01-07 12:55:39 -0800

    Update fetch_build to use the catapult version of cloud_storage.
    This is part of the migration work needed before we can remove the version
    in telemetry.
    
    BUG=565604
    
    Review URL: https://codereview.chromium.org/1566243002
    
    Cr-Commit-Position: refs/heads/master@{#368152}
    
    Former-commit-id: 2959b2468aa74d02cbc6e08cdfaf7d834e9c87b8

--- tools/auto_bisect/fetch_build.py
+++ tools/auto_bisect/fetch_build.py
@@ -22,7 +22,10 @@ import shutil
 import sys
 import zipfile
 
-# Telemetry (src/tools/telemetry) is expected to be in the PYTHONPATH.
+_CATAPULT_BASE_PATH = os.path.abspath(os.path.join(
+    __file__, '..', '..', 'third_party', 'catapult', 'catapult_base'))
+if _CATAPULT_BASE_PATH not in sys.path:
+  sys.path.insert(1, _CATAPULT_BASE_PATH)
 from catapult_base import cloud_storage
 
 import bisect_utils
