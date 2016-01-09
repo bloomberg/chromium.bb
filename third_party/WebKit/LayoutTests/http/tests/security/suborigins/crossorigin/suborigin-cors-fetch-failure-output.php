744223e8544c87399fcf1ff8624d6818d8d899d1
jww <jww@chromium.org>
2016-01-08 19:29:49 -0800

    Add tests for Fetch preflight behavior with Suborigins
    BUG=336894
    
    Review URL: https://codereview.chromium.org/1543683002
    
    Cr-Commit-Position: refs/heads/master@{#368517}
    
    Former-commit-id: b9dcbd864c9490d9705b341d04a32e5c700a9396

---
+++ third_party/WebKit/LayoutTests/http/tests/security/suborigins/crossorigin/suborigin-cors-fetch-failure-output.php
@@ -0,0 +1,55 @@
+<?php
+header("Content-Security-Policy: suborigin foobar");
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<script>
+if (window.testRunner) {
+    testRunner.waitUntilDone();
+    testRunner.dumpAsText();
+}
+console.log("If a Suborigin makes a request, a response without an Access-Control-Allow-Suborigin header should fail and should output a reasonable error message.");
+
+function success() {
+    alert("PASS: Fetch correctly failed");
+    next();
+}
+
+function failure() {
+    alert("FAIL: Fetch incorrectly succeeded");
+    next();
+}
+
+// First one should fail with preflight failure. Second one should
+// fail with access control header failure.
+var tests = [
+    function() {
+        var headers = new Headers();
+        headers.append("x-custom-header", "foobar");
+        var options = {
+            headers: headers
+        };
+        fetch("http://127.0.0.1:8000/security/resources/cors-script.php?cors=false", options)
+            .then(failure)
+            .catch(success);
+    },
+    function() {
+        fetch("http://127.0.0.1:8000/security/resources/cors-script.php?cors=false")
+            .then(failure)
+            .catch(success);
+    }
+];
+
+function next() {
+    if (tests.length !== 0) {
+        tests.shift()();
+    } else if (window.testRunner) {
+        testRunner.notifyDone();
+    }
+}
+
+next();
+</script>
+</body>
+</html>
---
+++ third_party/WebKit/LayoutTests/http/tests/security/suborigins/crossorigin/suborigin-cors-fetch-preflight.php
@@ -0,0 +1,98 @@
+<?php
+header("Content-Security-Policy: suborigin foobar");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Allow suborigin in HTTP header</title>
+<script src="/resources/testharness.js"></script>
+<script src="/resources/testharnessreport.js"></script>
+<script src="/security/suborigins/resources/suborigin-cors-lib.js"></script>
+</head>
+<body>
+<div id="container"></div>
+<script>
+// Fetch tests
+var SuboriginFetchTest = function(pass, name, src, crossoriginValue) {
+    SuboriginTest.call(this, pass, "Fetch: " + name, src, crossoriginValue);
+}
+
+SuboriginFetchTest.prototype.execute = function() {
+    var test = async_test(this.name);
+    var headers = new Headers();
+    // Set a custom header to force a preflight. Even though the
+    // scheme/host/port of the source and destination origins are the same, the
+    // Suborigin should cause the request to be treated as cross-origin.
+    headers.append("x-custom-header", "foobar");
+    var options = {
+        method: "GET",
+        headers: headers,
+        credentials: (this.crossoriginValue === 'use-credentials') ? 'include' : 'exclude',
+        mode: 'cors'
+    }
+
+    var response_func;
+    var error_func;
+    if (this.pass) {
+        response_func = test.step_func_done();
+        error_func = test.unreached_func("Good Fetch fired error handler.");
+    } else {
+        response_func = test.unreached_func("Bad Fetch successful.");
+        error_func = test.step_func_done();
+    }
+    fetch(this.src, options).then(response_func).catch(error_func);
+};
+
+var xorigin_preflight_script = "http://127.0.0.1:8000/security/resources/cors-script.php";
+
+// Fetch preflight tests
+new SuboriginFetchTest(
+    false,
+    "Complex anonymous Fetch preflight, no AC for custom header",
+    xorigin_preflight_script + "?cors=http://foobar_127.0.0.1:8000",
+    "anonymous").execute();
+
+new SuboriginFetchTest(
+    true,
+    "Complex anonymous Fetch preflight, has AC for custom header",
+    xorigin_preflight_script + "?cors=http://foobar_127.0.0.1:8000&custom=x-custom-header",
+    "anonymous").execute();
+
+new SuboriginFetchTest(
+    false,
+    "Complex anonymous Fetch preflight with '*' ACAO, no AC for custom header",
+    xorigin_preflight_script + "?cors=*",
+    "anonymous").execute();
+
+new SuboriginFetchTest(
+    true,
+    "Complex anonymous Fetch preflight with '*' ACAO, has AC for custom header",
+    xorigin_preflight_script + "?cors=*&custom=x-custom-header",
+    "anonymous").execute();
+
+new SuboriginFetchTest(
+    false,
+    "Complex Fetch with credentials preflight, no AC for custom header",
+    xorigin_preflight_script + "?cors=http://foobar_127.0.0.1:8000&credentials=true",
+    "use-credentials").execute();
+
+new SuboriginFetchTest(
+    true,
+    "Complex Fetch with credentials preflight, has AC for custom header",
+    xorigin_preflight_script + "?cors=http://foobar_127.0.0.1:8000&credentials=true&custom=x-custom-header",
+    "use-credentials").execute();
+
+new SuboriginFetchTest(
+    false,
+    "Complex Fetch with credentials preflight with '*' ACAO, no AC for custom header",
+    xorigin_preflight_script + "?cors=*&credentials=true",
+    "use-credentials").execute();
+
+new SuboriginFetchTest(
+    false,
+    "Complex Fetch with credentials preflight with '*' ACAO, has AC for custom header",
+    xorigin_preflight_script + "?cors=*&credentials=true&custom=x-custom-header",
+    "use-credentials").execute();
+</script>
+</body>
+</html>
