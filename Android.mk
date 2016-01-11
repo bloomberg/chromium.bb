2eb3b4d7ea1a3cbb0f8775de3b6d92c0a6152f9f
Derek Sollenberger <djsollen@google.com>
2016-01-11 14:41:40 -0500

    Upgrade jsoncpp to version 1.0.0 to match the copy used by Skia's testing tools.
    This mirrors the contents of the upstream repository at...
    https://github.com/open-source-parsers/jsoncpp/commit/7165f6ac4c482e68475c9e1dac086f9e12fff0d0
    
    Bug: 17997234
    Change-Id: I87a8312f9d030e5027f4ca5b1568f8374a856632

--- Android.mk
+++ Android.mk
@@ -5,16 +5,14 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
 	src/lib_json/json_reader.cpp \
-	chromium-overrides/src/lib_json/json_value.cpp \
+	src/lib_json/json_value.cpp \
 	src/lib_json/json_writer.cpp
 
 LOCAL_C_INCLUDES:= \
-	$(LOCAL_PATH)/chromium-overrides/include \
 	$(LOCAL_PATH)/include \
 	$(LOCAL_PATH)/src/lib_json
 
 LOCAL_EXPORT_C_INCLUDE_DIRS := \
-	$(LOCAL_PATH)/chromium-overrides/include \
 	$(LOCAL_PATH)/include
 
 LOCAL_CFLAGS := \
