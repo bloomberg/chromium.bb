1fdb6489fecd3e8096c04d250fdac890bad72e92
boliu <boliu@chromium.org>
2016-01-11 08:54:25 -0800

    Fix jar_toc.py to include public/protected methods
    Otherwise a method-only change does not cause dependent jars to be
    rebuilt.
    
    Review URL: https://codereview.chromium.org/1573023002
    
    Cr-Commit-Position: refs/heads/master@{#368604}
    
    Former-commit-id: 1b4abd14aae0fcffc65b96a5fa6554290386cc4b

--- build/android/gyp/jar_toc.py
+++ build/android/gyp/jar_toc.py
@@ -53,11 +53,13 @@ def CallJavap(classpath, classes):
 def ExtractToc(disassembled_classes):
   # javap output is structured by indent (2-space) levels.
   good_patterns = [
-      '^[^ ]', # This includes all class/function/member signatures.
+      '^[^ ]', # This includes all class signatures.
       '^  SourceFile:',
       '^  minor version:',
       '^  major version:',
       '^  Constant value:',
+      '^  public ',
+      '^  protected ',
       ]
   bad_patterns = [
       '^const #', # Matches the constant pool (i.e. literals used in the class).
