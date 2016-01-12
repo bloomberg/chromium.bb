02deeed615d36610d0020f4ffdb2d8f9bab5fea8
blundell <blundell@chromium.org>
2016-01-12 06:27:04 -0800

    Introduce IOSChromeSecurityStateModelClient
    This class is the //ios/chrome-specific implementation of
    security_state::SecurityStateModelClient.
    
    BUG=515071
    
    Review URL: https://codereview.chromium.org/1574113003
    
    Cr-Commit-Position: refs/heads/master@{#368870}
    
    Former-commit-id: a643a0c85c5f60bb0b5d248b5f793fb052bfc205

---
+++ ios/chrome/browser/ssl/ios_chrome_security_state_model_client.cc
@@ -0,0 +1,101 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/chrome/browser/ssl/ios_chrome_security_state_model_client.h"
+
+#include "base/command_line.h"
+#include "base/metrics/field_trial.h"
+#include "base/metrics/histogram_macros.h"
+#include "ios/web/public/cert_store.h"
+#include "ios/web/public/navigation_item.h"
+#include "ios/web/public/navigation_manager.h"
+#include "ios/web/public/origin_util.h"
+#include "ios/web/public/security_style.h"
+#include "ios/web/public/ssl_status.h"
+#include "ios/web/public/web_state/web_state.h"
+#include "net/cert/x509_certificate.h"
+
+DEFINE_WEB_STATE_USER_DATA_KEY(IOSChromeSecurityStateModelClient);
+
+namespace {
+
+// Converts a web::SecurityStyle (an indicator of a request's
+// overall security level computed by //ios/web) into a
+// SecurityStateModel::SecurityLevel (a finer-grained SecurityStateModel
+// concept that can express all of SecurityStateModel's policies that
+// //ios/web doesn't necessarily know about).
+security_state::SecurityStateModel::SecurityLevel
+GetSecurityLevelForSecurityStyle(web::SecurityStyle style) {
+  switch (style) {
+    case web::SECURITY_STYLE_UNKNOWN:
+      NOTREACHED();
+      return security_state::SecurityStateModel::NONE;
+    case web::SECURITY_STYLE_UNAUTHENTICATED:
+      return security_state::SecurityStateModel::NONE;
+    case web::SECURITY_STYLE_AUTHENTICATION_BROKEN:
+      return security_state::SecurityStateModel::SECURITY_ERROR;
+    case web::SECURITY_STYLE_WARNING:
+      // //ios/web currently doesn't use this style.
+      NOTREACHED();
+      return security_state::SecurityStateModel::SECURITY_WARNING;
+    case web::SECURITY_STYLE_AUTHENTICATED:
+      return security_state::SecurityStateModel::SECURE;
+  }
+  return security_state::SecurityStateModel::NONE;
+}
+
+}  // namespace
+
+IOSChromeSecurityStateModelClient::IOSChromeSecurityStateModelClient(
+    web::WebState* web_state)
+    : web_state_(web_state),
+      security_state_model_(new security_state::SecurityStateModel()) {
+  security_state_model_->SetClient(this);
+}
+
+IOSChromeSecurityStateModelClient::~IOSChromeSecurityStateModelClient() {}
+
+const security_state::SecurityStateModel::SecurityInfo&
+IOSChromeSecurityStateModelClient::GetSecurityInfo() const {
+  return security_state_model_->GetSecurityInfo();
+}
+
+bool IOSChromeSecurityStateModelClient::RetrieveCert(
+    scoped_refptr<net::X509Certificate>* cert) {
+  web::NavigationItem* item =
+      web_state_->GetNavigationManager()->GetVisibleItem();
+  if (!item)
+    return false;
+
+  int cert_id = item->GetSSL().cert_id;
+  return web::CertStore::GetInstance()->RetrieveCert(cert_id, cert);
+}
+
+bool IOSChromeSecurityStateModelClient::UsedPolicyInstalledCertificate() {
+  return false;
+}
+
+bool IOSChromeSecurityStateModelClient::IsOriginSecure(const GURL& url) {
+  return web::IsOriginSecure(url);
+}
+
+void IOSChromeSecurityStateModelClient::GetVisibleSecurityState(
+    security_state::SecurityStateModel::VisibleSecurityState* state) {
+  web::NavigationItem* item =
+      web_state_->GetNavigationManager()->GetVisibleItem();
+  if (!item || item->GetSSL().security_style == web::SECURITY_STYLE_UNKNOWN) {
+    *state = security_state::SecurityStateModel::VisibleSecurityState();
+    return;
+  }
+
+  state->initialized = true;
+  state->url = item->GetURL();
+  const web::SSLStatus& ssl = item->GetSSL();
+  state->initial_security_level =
+      GetSecurityLevelForSecurityStyle(ssl.security_style);
+  state->cert_id = ssl.cert_id;
+  state->cert_status = ssl.cert_status;
+  state->connection_status = ssl.connection_status;
+  state->security_bits = ssl.security_bits;
+}
---
+++ ios/chrome/browser/ssl/ios_chrome_security_state_model_client.h
@@ -0,0 +1,45 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_BROWSER_SSL_IOS_CHROME_SECURITY_STATE_MODEL_CLIENT_H_
+#define IOS_CHROME_BROWSER_SSL_IOS_CHROME_SECURITY_STATE_MODEL_CLIENT_H_
+
+#include "base/macros.h"
+#include "components/security_state/security_state_model.h"
+#include "components/security_state/security_state_model_client.h"
+#include "ios/web/public/web_state/web_state_user_data.h"
+
+namespace web {
+class WebState;
+}  // namespace web
+
+// Uses a WebState to provide a SecurityStateModel with the
+// information that it needs to determine the page's security status.
+class IOSChromeSecurityStateModelClient
+    : public security_state::SecurityStateModelClient,
+      public web::WebStateUserData<IOSChromeSecurityStateModelClient> {
+ public:
+  ~IOSChromeSecurityStateModelClient() override;
+
+  const security_state::SecurityStateModel::SecurityInfo& GetSecurityInfo()
+      const;
+
+  // SecurityStateModelClient:
+  void GetVisibleSecurityState(
+      security_state::SecurityStateModel::VisibleSecurityState* state) override;
+  bool RetrieveCert(scoped_refptr<net::X509Certificate>* cert) override;
+  bool UsedPolicyInstalledCertificate() override;
+  bool IsOriginSecure(const GURL& url) override;
+
+ private:
+  explicit IOSChromeSecurityStateModelClient(web::WebState* web_state);
+  friend class web::WebStateUserData<IOSChromeSecurityStateModelClient>;
+
+  web::WebState* web_state_;
+  scoped_ptr<security_state::SecurityStateModel> security_state_model_;
+
+  DISALLOW_COPY_AND_ASSIGN(IOSChromeSecurityStateModelClient);
+};
+
+#endif  // IOS_CHROME_BROWSER_SSL_IOS_CHROME_SECURITY_STATE_MODEL_CLIENT_H_
