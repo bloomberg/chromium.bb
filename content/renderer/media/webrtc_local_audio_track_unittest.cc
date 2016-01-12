ef7114edd421cef9a432957ea6731f30fb1529c5
glider <glider@chromium.org>
2016-01-12 03:58:03 -0800

    Disable WebRtcLocalAudioTrackTest.ConnectTracksToDifferentCapturers under TSan.
    TBR=dalecurtis@chromium.org,reillyg@chromium.org
    BUG=576634
    
    Review URL: https://codereview.chromium.org/1577163004
    
    Cr-Commit-Position: refs/heads/master@{#368852}
    
    Former-commit-id: 9ae42c18f71af06ad3abe9443602abcaf4fd5d31

--- content/renderer/media/webrtc_local_audio_track_unittest.cc
+++ content/renderer/media/webrtc_local_audio_track_unittest.cc
@@ -391,7 +391,15 @@ TEST_F(WebRtcLocalAudioTrackTest, StartAndStopAudioTracks) {
 }
 
 // Create a new capturer with new source, connect it to a new audio track.
-TEST_F(WebRtcLocalAudioTrackTest, ConnectTracksToDifferentCapturers) {
+#if defined(THREAD_SANITIZER)
+// Fails under TSan, see https://crbug.com/576634.
+#define MAYBE_ConnectTracksToDifferentCapturers \
+    DISABLED_ConnectTracksToDifferentCapturers
+#else
+#define MAYBE_ConnectTracksToDifferentCapturers \
+    ConnectTracksToDifferentCapturers
+#endif
+TEST_F(WebRtcLocalAudioTrackTest, MAYBE_ConnectTracksToDifferentCapturers) {
   // Setup the first audio track and start it.
   scoped_refptr<WebRtcLocalAudioTrackAdapter> adapter_1(
       WebRtcLocalAudioTrackAdapter::Create(std::string(), NULL));
