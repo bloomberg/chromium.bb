7f3642e109dd35821a31e513dcd7162045b2ade2
caryclark <caryclark@google.com>
2016-01-12 11:06:15 -0800

    add legacy bitmap filter flag for skia
    An upcoming Skia CL improves the performance for filtered bitmaps.
    Rearranging some expressions that compute the filter kernel causes
    0.01% - 0.02% of the pixels to differ by (1,1,1). This changes around
    240 layout test results.
    
    Suppress the Skia CL with this guard until the tests can be
    rebaselined.
    
    R=fmalita@chromium.org
    BUG=skia:2261
    
    Review URL: https://codereview.chromium.org/1583533002
    
    Cr-Commit-Position: refs/heads/master@{#368932}
    
    Former-commit-id: 2b1553885ea734afa886e19f0545a4814b66651f

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -234,6 +234,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_PATH_MEASURE_TVALUE
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_BITMAP_FILTER
+#   define SK_SUPPORT_LEGACY_BITMAP_FILTER
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
