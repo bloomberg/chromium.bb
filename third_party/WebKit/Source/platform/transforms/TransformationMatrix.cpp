5cc07168ab21d4dcd94de1ca5a76702215ef4c38
dskiba <dskiba@google.com>
2016-01-12 10:58:19 -0800

    Remove adjoint function in ARM64 mode.
    It's not used in ARM64 mode and thus causes an error when compiling with Clang.
    
    BUG=539781
    
    Review URL: https://codereview.chromium.org/1573603002
    
    Cr-Commit-Position: refs/heads/master@{#368930}
    
    Former-commit-id: 8728255324b547c126a4af31ab36489b47652e61

--- third_party/WebKit/Source/platform/transforms/TransformationMatrix.cpp
+++ third_party/WebKit/Source/platform/transforms/TransformationMatrix.cpp
@@ -148,6 +148,7 @@ static double determinant4x4(const TransformationMatrix::Matrix4& m)
          - d1 * determinant3x3(a2, a3, a4, b2, b3, b4, c2, c3, c4);
 }
 
+#if !CPU(ARM64)
 // adjoint( original_matrix, inverse_matrix )
 //
 //   calculate the adjoint of a 4x4 matrix
@@ -209,6 +210,7 @@ static inline void adjoint(const TransformationMatrix::Matrix4& matrix, Transfor
     result[2][3]  = - determinant3x3(a1, a2, a3, b1, b2, b3, d1, d2, d3);
     result[3][3]  =   determinant3x3(a1, a2, a3, b1, b2, b3, c1, c2, c3);
 }
+#endif
 
 // Returns false if the matrix is not invertible
 static bool inverse(const TransformationMatrix::Matrix4& matrix, TransformationMatrix::Matrix4& result)
