0084ee213d9f0218dd37553126095f076077f838
dalecurtis <dalecurtis@chromium.org>
2016-01-12 16:27:30 -0800

    Add field trial for unified media pipeline on Android.
    Will allow a field trial config to be pushed for dev/beta experiments
    in the near future.  The primary goals of this experiment are:
    - collect UMA information on codec usage on android.
    - smoke out crashes, missing features, bugs in the pipeline.
    
    There will be four experiment groups: Disabled (Default),
    Control (Disabled), Enabled, and EnabledByFlag.
    
    BUG=533190
    TEST=Correct player is created when --force-fieldtrials is used.
    
    Review URL: https://codereview.chromium.org/1584533002
    
    Cr-Commit-Position: refs/heads/master@{#369049}
    
    Former-commit-id: 302b1c501e1eb8ffca0acf9e40aa8608a777ba0e

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -20,6 +20,7 @@
 #include "base/macros.h"
 #include "base/memory/shared_memory.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
 #include "base/process/process.h"
 #include "base/stl_util.h"
@@ -2364,13 +2365,29 @@ blink::WebMediaPlayer* RenderFrameImpl::createMediaPlayer(
 #if defined(OS_ANDROID)
   // We must use WMPA in when accelerated video decode is disabled becuase WMPI
   // is unlikely to have a fallback decoder.
-  if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableUnifiedMediaPipeline) ||
-      base::CommandLine::ForCurrentProcess()->HasSwitch(
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kDisableAcceleratedVideoDecode) ||
       !media::MediaCodecUtil::IsMediaCodecAvailable() ||
       media::MediaCodecUtil::IsHLSPath(url)) {
     return CreateAndroidWebMediaPlayer(client, encrypted_client, params);
+  } else {
+    // TODO(dalecurtis): This experiment is temporary and should be removed once
+    // we have enough data to support the primacy of the unified media pipeline;
+    // see http://crbug.com/533190 for details.
+    //
+    // Note: It's important to query the field trial state first, to ensure that
+    // UMA reports the correct group.
+    const std::string group_name =
+        base::FieldTrialList::FindFullName("UnifiedMediaPipelineTrial");
+    const bool enabled_via_cli =
+        base::CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kEnableUnifiedMediaPipeline);
+    const bool enable_unified_media_pipeline =
+        enabled_via_cli ||
+        base::StartsWith(group_name, "Enabled", base::CompareCase::SENSITIVE);
+
+    if (!enable_unified_media_pipeline)
+      return CreateAndroidWebMediaPlayer(client, encrypted_client, params);
   }
 #endif  // defined(OS_ANDROID)
 
