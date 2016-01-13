e7af42d6ef4a841c0b9be9ce6a5509a37b0cb316
nisse <nisse@chromium.org>
2016-01-13 01:44:34 -0800

    Delete testing of GetPixelWidth and GetPixelWidth, so they can be deleted in webrtc.
    BUG=webrtc:5426
    
    Review URL: https://codereview.chromium.org/1588493002
    
    Cr-Commit-Position: refs/heads/master@{#369125}
    
    Former-commit-id: dcc767d00df3ef57018e0f7f2a5d2566920a0bf3

--- content/renderer/media/webrtc/media_stream_remote_video_source.cc
+++ content/renderer/media/webrtc/media_stream_remote_video_source.cc
@@ -104,10 +104,6 @@ void MediaStreamRemoteVideoSource::RemoteVideoSourceDelegate::RenderFrame(
 
     gfx::Size size(frame->GetWidth(), frame->GetHeight());
 
-    // Non-square pixels are unsupported.
-    DCHECK_EQ(frame->GetPixelWidth(), 1u);
-    DCHECK_EQ(frame->GetPixelHeight(), 1u);
-
     // Make a shallow copy. Both |frame| and |video_frame| will share a single
     // reference counted frame buffer. Const cast and hope no one will overwrite
     // the data.
