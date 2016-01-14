4828c710b63ccd051554aaaf4d096a2fee8a9024
hcarmona <hcarmona@chromium.org>
2016-01-13 21:43:47 -0800

    Fix NULL crash in PrintViewManager::PrintPreviewDone.
    BUG=538612
    
    Review URL: https://codereview.chromium.org/1581083004
    
    Cr-Commit-Position: refs/heads/master@{#369341}
    
    Former-commit-id: 8720794e92dc9c82466a608d2cec1dc8fc8adabb

--- chrome/browser/printing/print_preview_dialog_controller.cc
+++ chrome/browser/printing/print_preview_dialog_controller.cc
@@ -166,8 +166,12 @@ void PrintPreviewDialogController::PrintPreview(WebContents* initiator) {
   PrintPreviewDialogController* dialog_controller = GetInstance();
   if (!dialog_controller)
     return;
-  if (!dialog_controller->GetOrCreatePreviewDialog(initiator))
-    PrintViewManager::FromWebContents(initiator)->PrintPreviewDone();
+  if (!dialog_controller->GetOrCreatePreviewDialog(initiator)) {
+    PrintViewManager* print_view_manager =
+        PrintViewManager::FromWebContents(initiator);
+    if (print_view_manager)
+      print_view_manager->PrintPreviewDone();
+  }
 }
 
 WebContents* PrintPreviewDialogController::GetOrCreatePreviewDialog(
