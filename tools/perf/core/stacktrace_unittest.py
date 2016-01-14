48423bda22f35fdcf9442ec34721647e41451911
dyen <dyen@chromium.org>
2016-01-14 05:50:52 -0800

    Reland of Added unit test for stack traces in telemetry crashes. (patchset #1 id:1 of https://codereview.chromium.org/1580313002/ )
    Reason for revert:
    Try again disabling 10.6
    
    Original issue's description:
    > Revert of Added unit test for stack traces in telemetry crashes. (patchset #17 id:320001 of https://codereview.chromium.org/1498633003/ )
    >
    > Reason for revert:
    > Test fails on some bots.
    >
    > Original issue's description:
    > > Added unit test for stack traces in telemetry crashes.
    > >
    > > This CL also fixes an issue on Mac where telemetry does not
    > > know how to locate chromium_framework for the symbols.
    > >
    > > R=nednguyen@google.com
    > > BUG=563716
    > > TEST=ran unit test locally
    > >
    > > Committed: https://crrev.com/ec54321a6f1d2ce78358d5506a7802186dac59f4 [formerly a20e03ee6906c50bdfaf8fb34fe684b4a89db852]
    > > Cr-Commit-Position: refs/heads/master@{#368997}
    >
    > TBR=nednguyen@google.com
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=563716
    >
    > Committed: https://crrev.com/c5f20394d6dc48374e0438edf6720eb34e93da67 [formerly f0cb0bf2dd7f677423318d2318902311e635538d]
    > Cr-Commit-Position: refs/heads/master@{#369056}
    
    R=nednguyen@google.com
    BUG=563716
    
    Review URL: https://codereview.chromium.org/1581143002
    
    Cr-Commit-Position: refs/heads/master@{#369404}
    
    Former-commit-id: 5f0df0518d4befa6b0c597d0ccdcfa8e50367d00

---
+++ tools/perf/core/stacktrace_unittest.py
@@ -0,0 +1,30 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry.core import exceptions
+from telemetry import decorators
+from telemetry.testing import tab_test_case
+
+class TabStackTraceTest(tab_test_case.TabTestCase):
+
+  # For now just work on a single platform (mac).
+  @decorators.Enabled('mac')
+   # Stack traces do not currently work on 10.6, but they are also being
+   # disabled shortly so just disable it for now.
+  @decorators.Disabled('snowleopard')
+  def testStackTrace(self):
+    try:
+      self._tab.Navigate('chrome://crash', timeout=5)
+    except exceptions.DevtoolsTargetCrashException as e:
+      self.assertIn('Thread 0 (crashed)', '\n'.join(e.stack_trace))
+
+  # Currently stack traces do not work on windows: http://crbug.com/476110
+  # Currently symbols do not work on swarming: http://crbug.com/563716
+  # Linux stack traces depends on fission support: http://crbug.com/405623
+  @decorators.Disabled('all')
+  def testCrashSymbols(self):
+    try:
+      self._tab.Navigate('chrome://crash', timeout=5)
+    except exceptions.DevtoolsTargetCrashException as e:
+      self.assertIn('CrashIntentionally', '\n'.join(e.stack_trace))
