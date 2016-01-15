fe03694ddcb91717c1f041c64835d8bdefdbdfcb
xhwang <xhwang@chromium.org>
2016-01-14 23:03:12 -0800

    media: Require HW_SECURE_CODECS for other key systems on Android.
    On Android, for Widevine we know when hardware secure codecs are required based
    on robustness level. For Clear Key, it's never required.
    
    All other key systems that we know of requires hardware secure codecs. Setting
    this assumption in the code.
    
    Note that this CL depends on http://chromiumcodereview.appspot.com/1585083003/
    
    BUG=577777
    
    Review URL: https://codereview.chromium.org/1586043003
    
    Cr-Commit-Position: refs/heads/master@{#369697}
    
    Former-commit-id: d63955e64530163c0c3119e08b60053034e99f19

--- media/base/key_systems.cc
+++ media/base/key_systems.cc
@@ -783,8 +783,8 @@ EmeConfigRule KeySystemsImpl::GetRobustnessConfigRule(
     return EmeConfigRule::NOT_SUPPORTED;
   }
 
-  if (key_system == kWidevineKeySystem) {
 #if defined(OS_CHROMEOS)
+  if (key_system == kWidevineKeySystem) {
     // TODO(ddorwin): Remove this once we have confirmed it is not necessary.
     // See https://crbug.com/482277
     if (robustness == EmeRobustness::EMPTY)
@@ -802,11 +802,16 @@ EmeConfigRule KeySystemsImpl::GetRobustnessConfigRule(
         max_robustness == EmeRobustness::HW_SECURE_ALL) {
       return EmeConfigRule::IDENTIFIER_RECOMMENDED;
     }
-#elif defined(OS_ANDROID)
-    if (robustness > EmeRobustness::SW_SECURE_CRYPTO)
-      return EmeConfigRule::HW_SECURE_CODECS_REQUIRED;
-#endif  // defined(OS_CHROMEOS)
   }
+#elif defined(OS_ANDROID)
+  // Require hardware secure codecs for Widevine when SW_SECURE_DECODE or above
+  // is specified, or for all other key systems (excluding Clear Key).
+  if ((key_system == kWidevineKeySystem &&
+       robustness >= EmeRobustness::SW_SECURE_DECODE) ||
+      !IsClearKey(key_system)) {
+    return EmeConfigRule::HW_SECURE_CODECS_REQUIRED;
+  }
+#endif  // defined(OS_CHROMEOS)
 
   return EmeConfigRule::SUPPORTED;
 }
