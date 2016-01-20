cb463f4b7e1bbff85fe601c41ae222f130aa2be5
pilgrim <pilgrim@chromium.org>
2016-01-19 17:42:21 -0800

    [Line Layout API] Convert RootInlineBoxPainter to use line layout API shim
    Classes outside of core/layout/ that need a reference to an InlineBox's
    layoutObject must currently use the API-busting shim we've set up to
    get a reference to a LineLayoutItem's underlying layoutObject.
    
    BUG=499321
    
    Review URL: https://codereview.chromium.org/1605113002
    
    Cr-Commit-Position: refs/heads/master@{#370268}
    
    Former-commit-id: 0a2bdab0439d46f28a4e0d557670218d5455710e

--- third_party/WebKit/Source/core/paint/RootInlineBoxPainter.cpp
+++ third_party/WebKit/Source/core/paint/RootInlineBoxPainter.cpp
@@ -6,13 +6,14 @@
 
 #include "core/layout/line/EllipsisBox.h"
 #include "core/layout/line/RootInlineBox.h"
+#include "core/paint/LineLayoutPaintShim.h"
 #include "core/paint/PaintInfo.h"
 
 namespace blink {
 
 void RootInlineBoxPainter::paintEllipsisBox(const PaintInfo& paintInfo, const LayoutPoint& paintOffset, LayoutUnit lineTop, LayoutUnit lineBottom) const
 {
-    if (m_rootInlineBox.hasEllipsisBox() && paintInfo.shouldPaintWithinRoot(&m_rootInlineBox.layoutObject()) && m_rootInlineBox.lineLayoutItem().style()->visibility() == VISIBLE
+    if (m_rootInlineBox.hasEllipsisBox() && paintInfo.shouldPaintWithinRoot(LineLayoutPaintShim::layoutObjectFrom(m_rootInlineBox.lineLayoutItem())) && m_rootInlineBox.lineLayoutItem().style()->visibility() == VISIBLE
         && paintInfo.phase == PaintPhaseForeground)
         m_rootInlineBox.ellipsisBox()->paint(paintInfo, paintOffset, lineTop, lineBottom);
 }
