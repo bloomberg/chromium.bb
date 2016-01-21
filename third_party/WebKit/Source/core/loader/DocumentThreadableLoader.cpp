7860e84eea45839689c13699d636511f0d16f045
horo <horo@chromium.org>
2016-01-21 04:24:40 -0800

    Add null check of the return value of Document::frame() in DTL::reportResponseReceived()
    BUG=578849
    
    Review URL: https://codereview.chromium.org/1603783002
    
    Cr-Commit-Position: refs/heads/master@{#370674}
    
    Former-commit-id: d2535dca624b5531247dbcbc2048dded6e9c45fc

--- third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentThreadableLoader.cpp
@@ -570,6 +570,11 @@ void DocumentThreadableLoader::handlePreflightResponse(const ResourceResponse& r
 void DocumentThreadableLoader::reportResponseReceived(unsigned long identifier, const ResourceResponse& response)
 {
     LocalFrame* frame = document().frame();
+    // We are seeing crashes caused by nullptr (crbug.com/578849). But the frame
+    // must be set here. TODO(horo): Find the root cause of the unset frame.
+    ASSERT(frame);
+    if (!frame)
+        return;
     DocumentLoader* loader = frame->loader().documentLoader();
     TRACE_EVENT_INSTANT1("devtools.timeline", "ResourceReceiveResponse", TRACE_EVENT_SCOPE_THREAD, "data", InspectorReceiveResponseEvent::data(identifier, frame, response));
     InspectorInstrumentation::didReceiveResourceResponse(frame, identifier, loader, response, resource() ? resource()->loader() : 0);
