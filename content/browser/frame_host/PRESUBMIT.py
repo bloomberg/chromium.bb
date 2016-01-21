cb28d6ac6e11d2fa86942b86417a07c59de9d024
dcheng <dcheng@chromium.org>
2016-01-21 15:35:15 -0800

    Add PRESUBMIT.py to automatically include site isolation trybot.
    BUG=579704
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review URL: https://codereview.chromium.org/1611553003
    
    Cr-Commit-Position: refs/heads/master@{#370829}
    
    Former-commit-id: 140da978fecc7edba890aeaf4bb4f2cd350faedb

---
+++ content/browser/frame_host/PRESUBMIT.py
@@ -0,0 +1,40 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""Presubmit script for //content/browser/frame_host.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into depot_tools.
+"""
+
+import re
+
+
+def GetPreferredTryMasters(project, change):
+  return {'tryserver.chromium.linux': ['linux_site_isolation'],}
+
+
+def PostUploadHook(cl, change, output_api):
+  """git cl upload will call this hook after the issue is created/modified.
+
+  This hook adds extra try bots to the CL description in order to run site
+  isolation tests in addition to CQ try bots.
+  """
+  rietveld_obj = cl.RpcServer()
+  issue = cl.issue
+  description = rietveld_obj.get_description(issue)
+  if re.search(r'^CQ_INCLUDE_TRYBOTS=.*', description, re.M | re.I):
+    return []
+
+  masters = GetPreferredTryMasters(None, change)
+  results = []
+  new_description = description
+  new_description += '\nCQ_INCLUDE_TRYBOTS=%s' % ';'.join(
+      '%s:%s' % (master, ','.join(bots))
+      for master, bots in masters.iteritems())
+  results.append(output_api.PresubmitNotifyResult(
+      'Automatically added site isolation trybots to run tests on CQ.'))
+
+  rietveld_obj.update_description(issue, new_description)
+
+  return results
