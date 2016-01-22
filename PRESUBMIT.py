dcee61481f82f6976adbec1d2dc1556bf5d5f317
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2016-01-22 16:48:22 +0100

    src/PRESUBMIT.py: do not trigger experimental or triggered builders
    Depends on https://codereview.chromium.org/1587893003/
    
    BUG=580556
    R=tandrii@chromium.org
    
    Review URL: https://codereview.chromium.org/1620343003 .
    
    Cr-Commit-Position: refs/heads/master@{#370969}
    
    Former-commit-id: 699ec315713583756ac6cda69ddfe0b9193c8e52

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1951,30 +1951,3 @@ def CheckChangeOnCommit(input_api, output_api):
   results.extend(input_api.canned_checks.CheckChangeHasDescription(
       input_api, output_api))
   return results
-
-
-def GetPreferredTryMasters(project, change):
-  import json
-  import os.path
-  import platform
-  import subprocess
-
-  cq_config_path = os.path.join(
-      change.RepositoryRoot(), 'infra', 'config', 'cq.cfg')
-  # commit_queue.py below is a script in depot_tools directory, which has a
-  # 'builders' command to retrieve a list of CQ builders from the CQ config.
-  is_win = platform.system() == 'Windows'
-  masters = json.loads(subprocess.check_output(
-      ['commit_queue', 'builders', cq_config_path], shell=is_win))
-
-  try_config = {}
-  for master in masters:
-    try_config.setdefault(master, {})
-    for builder in masters[master]:
-      # Do not trigger presubmit builders, since they're likely to fail
-      # (e.g. OWNERS checks before finished code review), and we're
-      # running local presubmit anyway.
-      if 'presubmit' not in builder:
-        try_config[master][builder] = ['defaulttests']
-
-  return try_config
