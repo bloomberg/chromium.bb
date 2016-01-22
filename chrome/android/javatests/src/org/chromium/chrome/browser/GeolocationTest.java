5914c0c9efa8181d9712f79695156e7e4cc80065
mikecase <mikecase@chromium.org>
2016-01-21 17:48:09 -0800

    Revert of Fix org.chromium.chrome.browser.GeolocationTest (patchset #1 id:1 of https://codereview.chromium.org/1599113002/ )
    Reason for revert:
    Causing downstream test failures on ToT bots (blocking Chromium rolls downstream)
    
    Original issue's description:
    > Fix org.chromium.chrome.browser.GeolocationTest
    >
    > The geolocation tests did not work because:
    > - They referred to incorrect test pages.
    >   Correct test pages:
    >     https://chrome-internal-review.googlesource.com/#/c/27084/3/test/data/android/geolocation/geolocation_on_load.html
    >     https://chrome-internal-review.googlesource.com/#/c/27084/3/test/data/android/geolocation/geolocation_watch_on_load.html
    > - A race condition was causing LocationProviderAdapter.newLocationAvailable() to
    >   be often called prior to LocationProviderFactory.LocationProvider.start().
    >
    > In addition to fixing the above problems, this CL makes
    > org.chromium.chrome.browser.GeolocationTest more similar to
    > - org.chromium.android_webview.test.GeolocationTest
    > - org.chromium.content.browser.ContentViewLocationTest
    > In particular, all of the tests now:
    > - Use the same test page
    > - Use MockLocationProvider
    >
    > BUG=141518
    > Test=Less flakiness
    >
    > Committed: https://crrev.com/f8d961928bc8c97c051fd1831a27ff090733cb12 [formerly 5228d5afe9d76431d7cca53eb1ea73b715e49e7e]
    > Cr-Commit-Position: refs/heads/master@{#370807}
    
    TBR=mvanouwerkerk@chromium.org,tedchoc@chromium.org,pkotwicz@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=141518
    
    Review URL: https://codereview.chromium.org/1614843005
    
    Cr-Commit-Position: refs/heads/master@{#370871}
    
    Former-commit-id: 29dfa24c18f479c9e68d45a7daf9ba48afe00019

--- chrome/android/javatests/src/org/chromium/chrome/browser/GeolocationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/GeolocationTest.java
@@ -4,20 +4,22 @@
 
 package org.chromium.chrome.browser;
 
-import android.test.suitebuilder.annotation.MediumTest;
-import android.test.suitebuilder.annotation.Smoke;
+import android.location.Location;
+import android.test.FlakyTest;
 
-import org.chromium.base.test.util.Feature;
+import org.chromium.base.ThreadUtils;
 import org.chromium.chrome.browser.infobar.InfoBarContainer;
 import org.chromium.chrome.browser.tab.EmptyTabObserver;
 import org.chromium.chrome.browser.tab.Tab;
+import org.chromium.chrome.browser.tab.TabObserver;
 import org.chromium.chrome.test.ChromeActivityTestCaseBase;
 import org.chromium.chrome.test.util.InfoBarTestAnimationListener;
 import org.chromium.chrome.test.util.InfoBarUtil;
 import org.chromium.chrome.test.util.TestHttpServerClient;
+import org.chromium.content.browser.LocationProviderAdapter;
 import org.chromium.content.browser.LocationProviderFactory;
+import org.chromium.content.browser.LocationProviderFactory.LocationProvider;
 import org.chromium.content.browser.test.util.CallbackHelper;
-import org.chromium.content.browser.test.util.MockLocationProvider;
 
 /**
  * Test suite for Geo-Location functionality.
@@ -31,34 +33,8 @@ public class GeolocationTest extends ChromeActivityTestCaseBase<ChromeActivity> 
     private static final double LATITUDE = 51.01;
     private static final double LONGITUDE = 0.23;
     private static final float ACCURACY = 10;
-    private static final String TEST_FILE = "content/test/data/android/geolocation.html";
 
     private InfoBarTestAnimationListener mListener;
-
-    /**
-     * Waits till the geolocation JavaScript callback is called the specified number of times.
-     */
-    private class GeolocationUpdateWaiter extends EmptyTabObserver {
-        private CallbackHelper mCallbackHelper;
-        private int mExpectedCount;
-
-        public GeolocationUpdateWaiter() {
-            mCallbackHelper = new CallbackHelper();
-        }
-
-        @Override
-        public void onTitleUpdated(Tab tab) {
-            String expectedTitle = "Count:" + mExpectedCount;
-            if (getActivity().getActivityTab().getTitle().equals(expectedTitle)) {
-                mCallbackHelper.notifyCalled();
-            }
-        }
-
-        public void waitForNumUpdates(int numUpdates) throws Exception {
-            mExpectedCount = numUpdates;
-            mCallbackHelper.waitForCallback(0);
-        }
-    }
 
     public GeolocationTest() {
         super(ChromeActivity.class);
@@ -73,58 +49,143 @@ public class GeolocationTest extends ChromeActivityTestCaseBase<ChromeActivity> 
         mListener =  new InfoBarTestAnimationListener();
         container.setAnimationListener(mListener);
 
-        LocationProviderFactory.setLocationProviderImpl(new MockLocationProvider());
+        LocationProviderFactory.setLocationProviderImpl(new LocationProvider() {
+            private boolean mIsRunning;
+
+            @Override
+            public boolean isRunning() {
+                return mIsRunning;
+            }
+
+            @Override
+            public void start(boolean gpsEnabled) {
+                mIsRunning = true;
+            }
+
+            @Override
+            public void stop() {
+                mIsRunning = false;
+            }
+        });
     }
 
     /**
      * Verify Geolocation creates an InfoBar and receives a mock location.
+     *
+     * Fails frequently.
+     * Bug 141518
+     * @Smoke
+     * @MediumTest
+     * @Feature({"Location", "Main"})
+     *
      * @throws Exception
      */
-    @Smoke
-    @MediumTest
-    @Feature({"Location", "Main"})
+    @FlakyTest
     public void testGeolocationPlumbing() throws Exception {
         final String url = TestHttpServerClient.getUrl(
-                "content/test/data/android/geolocation.html");
+                "chrome/test/data/geolocation/geolocation_on_load.html");
 
         Tab tab = getActivity().getActivityTab();
-        GeolocationUpdateWaiter updateWaiter = new GeolocationUpdateWaiter();
-        tab.addObserver(updateWaiter);
-
+        final CallbackHelper loadCallback = new CallbackHelper();
+        TabObserver observer = new EmptyTabObserver() {
+            @Override
+            public void onLoadStopped(Tab tab, boolean toDifferentDocument) {
+                // If the device has a cached non-mock location, we won't get back our
+                // lat/long, so checking that it has "#pass" is sufficient.
+                if (tab.getUrl().startsWith(url + "#pass|")) {
+                    loadCallback.notifyCalled();
+                }
+            }
+        };
+        tab.addObserver(observer);
         loadUrl(url);
-        runJavaScriptCodeInCurrentTab("initiate_getCurrentPosition()");
         assertTrue("InfoBar not added.", mListener.addInfoBarAnimationFinished());
         assertTrue("OK button wasn't found", InfoBarUtil.clickPrimaryButton(getInfoBars().get(0)));
-        updateWaiter.waitForNumUpdates(1);
 
-        tab.removeObserver(updateWaiter);
+        sendLocation(createMockLocation(LATITUDE, LONGITUDE, ACCURACY));
+        loadCallback.waitForCallback(0);
+        tab.removeObserver(observer);
     }
 
     /**
      * Verify Geolocation creates an InfoBar and receives multiple locations.
+     *
+     * Bug 141518
+     * @MediumTest
+     * @Feature({"Location"})
+     *
      * @throws Exception
      */
-    @MediumTest
-    @Feature({"Location"})
+    @FlakyTest
     public void testGeolocationWatch() throws Exception {
         final String url = TestHttpServerClient.getUrl(
-                "content/test/data/android/geolocation.html");
+                "chrome/test/data/geolocation/geolocation_on_load.html");
 
         Tab tab = getActivity().getActivityTab();
-        GeolocationUpdateWaiter updateWaiter = new GeolocationUpdateWaiter();
-        tab.addObserver(updateWaiter);
-
+        final CallbackHelper loadCallback0 = new CallbackHelper();
+        TabObserver observer = new EmptyTabObserver() {
+            @Override
+            public void onLoadStopped(Tab tab, boolean toDifferentDocument) {
+                // If the device has a cached non-mock location, we won't get back our
+                // lat/long, so checking that it has "#pass" is sufficient.
+                if (tab.getUrl().startsWith(url + "#pass|0|")) {
+                    loadCallback0.notifyCalled();
+                }
+            }
+        };
+        tab.addObserver(observer);
         loadUrl(url);
-        runJavaScriptCodeInCurrentTab("initiate_watchPosition()");
         assertTrue("InfoBar not added.", mListener.addInfoBarAnimationFinished());
-        assertTrue("OK button wasn't found", InfoBarUtil.clickPrimaryButton(getInfoBars().get(0)));
-        updateWaiter.waitForNumUpdates(2);
 
-        tab.removeObserver(updateWaiter);
+        assertTrue("OK button wasn't found", InfoBarUtil.clickPrimaryButton(getInfoBars().get(0)));
+        sendLocation(createMockLocation(LATITUDE, LONGITUDE, ACCURACY));
+        loadCallback0.waitForCallback(0);
+        tab.removeObserver(observer);
+
+        // Send another location: it'll update the URL again, so increase the count.
+        final CallbackHelper loadCallback1 = new CallbackHelper();
+        observer = new EmptyTabObserver() {
+            @Override
+            public void onLoadStopped(Tab tab, boolean toDifferentDocument) {
+                // If the device has a cached non-mock location, we won't get back our
+                // lat/long, so checking that it has "#pass" is sufficient.
+                if (tab.getUrl().startsWith(url + "#pass|1|")) {
+                    loadCallback1.notifyCalled();
+                }
+            }
+        };
+        tab.addObserver(observer);
+        sendLocation(createMockLocation(LATITUDE + 1, LONGITUDE - 1, ACCURACY - 1));
+        loadCallback1.waitForCallback(0);
+        tab.removeObserver(observer);
     }
 
     @Override
     public void startMainActivity() throws InterruptedException {
         startMainActivityOnBlankPage();
+    }
+
+    private Location createMockLocation(double latitude, double longitude, float accuracy) {
+        Location mockLocation = new Location(LOCATION_PROVIDER_MOCK);
+        mockLocation.setLatitude(latitude);
+        mockLocation.setLongitude(longitude);
+        mockLocation.setAccuracy(accuracy);
+        mockLocation.setTime(System.currentTimeMillis());
+        return mockLocation;
+    }
+
+    private void sendLocation(final Location location) {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                LocationProviderAdapter.newLocationAvailable(
+                        location.getLatitude(), location.getLongitude(),
+                        location.getTime() / 1000.0,
+                        location.hasAltitude(), location.getAltitude(),
+                        location.hasAccuracy(), location.getAccuracy(),
+                        location.hasBearing(), location.getBearing(),
+                        location.hasSpeed(), location.getSpeed());
+            }
+        });
     }
 }
