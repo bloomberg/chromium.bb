99f63ec3604d5650af79c7579fbcdc6373439352
pkasting <pkasting@chromium.org>
2016-01-22 15:37:01 -0800

    Fix bug when drawing background tabs in incognito windows with custom themes.
    If a theme provided a replacement frame image, but not a replacement incognito
    frame or incognito background tab image, the theme service would synthesize one,
    but the tab drawing code would not realize this.  As a result it would cache the
    appearance of non-hovered background tabs, leading to visual appearance glitches
    since not all those tabs should look the same.
    
    BUG=580677
    TEST=Install https://chrome.google.com/webstore/detail/tree-branches/bgdeemcfmmabkdhbnhmkhpadancpkgol?hl=en-US , open an incognito window, open several tabs, and ensure the background tab images line up with the frame and the tab appearance doesn't change abruptly when hovering any such tabs.
    
    Review URL: https://codereview.chromium.org/1623643002
    
    Cr-Commit-Position: refs/heads/master@{#371081}
    
    Former-commit-id: 65740e3efadf5dad31f11b6d50eb6b63ccb3f2d7

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -1396,12 +1396,15 @@ int TabStrip::GetBackgroundResourceId(bool* custom_image) const {
   // If a custom theme does not provide a replacement tab background, but does
   // provide a replacement frame image, HasCustomImage() on the tab background
   // ID will return false, but the theme provider will make a custom image from
-  // the frame image.
+  // the frame image.  Furthermore, since the theme provider will create the
+  // incognito frame image from the normal frame image, in incognito mode we
+  // need to look for a custom incognito _or_ regular frame image.
   const bool incognito = controller()->IsIncognito();
   const int id = incognito ?
       IDR_THEME_TAB_BACKGROUND_INCOGNITO : IDR_THEME_TAB_BACKGROUND;
   const int frame_id = incognito ? IDR_THEME_FRAME_INCOGNITO : IDR_THEME_FRAME;
-  *custom_image = tp->HasCustomImage(id) || tp->HasCustomImage(frame_id);
+  *custom_image = tp->HasCustomImage(id) || tp->HasCustomImage(frame_id) ||
+      (incognito && tp->HasCustomImage(IDR_THEME_FRAME));
   return id;
 }
 
