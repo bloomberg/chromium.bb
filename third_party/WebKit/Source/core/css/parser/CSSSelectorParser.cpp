0c8c3b97b97345d3b3b7da65c307eb72c092452c
fs <fs@opera.com>
2016-01-25 03:48:31 -0800

    Match 'i' attribute selector modifier case-insensitively
    The 'i' should be treated as an identifier, so matching should be ASCII
    case-insensitive.
    
    BUG=580446
    
    Review URL: https://codereview.chromium.org/1626563002
    
    Cr-Commit-Position: refs/heads/master@{#371228}
    
    Former-commit-id: 8c4aee69ad7391e4afc02276987958091038e308

--- third_party/WebKit/Source/core/css/parser/CSSSelectorParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSSelectorParser.cpp
@@ -610,7 +610,7 @@ CSSSelector::AttributeMatchType CSSSelectorParser::consumeAttributeFlags(CSSPars
     if (range.peek().type() != IdentToken)
         return CSSSelector::CaseSensitive;
     const CSSParserToken& flag = range.consumeIncludingWhitespace();
-    if (String(flag.value()) == "i")
+    if (flag.valueEqualsIgnoringCase("i"))
         return CSSSelector::CaseInsensitive;
     m_failedParsing = true;
     return CSSSelector::CaseSensitive;
