4a63e0fa21e86dc5199df61a782d29e8483104e4
zhaoqin <zhaoqin@chromium.org>
2016-01-25 13:40:00 -0800

    Fix zlib_uncompress_fuzzer failure on Windows
    - fix zlib_uncompress_fuzzer build failure on Windows:
      change buffer_length from size_t to uLongf
    - fix zlib_uncompress_fuzzer run failure on Windows:
      use buffer from heap instead of stack to avoid _chkstk failure
    
    R=aizatsky@chromium.org
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/1635703002
    
    Cr-Commit-Position: refs/heads/master@{#371320}
    
    Former-commit-id: 8c375fbf2c234840400f28e8cf0303eddaa1dfca

--- testing/libfuzzer/fuzzers/zlib_uncompress_fuzzer.cc
+++ testing/libfuzzer/fuzzers/zlib_uncompress_fuzzer.cc
@@ -3,16 +3,22 @@
 // found in the LICENSE file.
 
 #include <stdint.h>
+#include <string.h>
 
 #include "third_party/zlib/zlib.h"
 
 // Entry point for LibFuzzer.
 extern "C" int LLVMFuzzerTestOneInput(const unsigned char *data, size_t size) {
-  uint8_t buffer[1024 * 1024] = { 0 };
-  size_t buffer_length = sizeof(buffer);
-  if (Z_OK != uncompress(buffer, &buffer_length, data, size)) {
-    return 0;
-  }
+    const int NUM_ITEMS = 1024 * 1024;
+    const int BUF_SIZE  = NUM_ITEMS * sizeof(uint8_t);
+    uint8_t *buffer = new uint8_t[NUM_ITEMS];
+    uLongf buffer_length = (uLongf)BUF_SIZE;
+    memset(buffer, 0, BUF_SIZE);
 
-  return 0;
+    if (Z_OK != uncompress(buffer, &buffer_length, data, size)) {
+        delete[] buffer;
+        return 0;
+    }
+    delete[] buffer;
+    return 0;
 }
