ed546c2974096f07dea2f3011eea4e04029c0b20
haraken <haraken@chromium.org>
2016-01-25 04:59:06 -0800

    Split CSSPropertyParserTest into pieces
    After we enabled Oilpan, CSSPropertyParserTest started timeout on Windows Debug
    because it allocates a crazy number of CSS objects and causes many GCs.
    This CL splits the test into pieces so that each test doesn't hit the timeout.
    
    BUG=580999
    
    Review URL: https://codereview.chromium.org/1630583002
    
    Cr-Commit-Position: refs/heads/master@{#371234}
    
    Former-commit-id: 644ce84d644644862c4c2e6ba3fc3be6eb946cc9

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParserTest.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParserTest.cpp
@@ -10,9 +10,9 @@
 
 namespace blink {
 
-static unsigned computeNumberOfTracks(CSSValueList* valueList)
+static int computeNumberOfTracks(CSSValueList* valueList)
 {
-    unsigned numberOfTracks = 0;
+    int numberOfTracks = 0;
     for (auto& value : *valueList) {
         if (value->isGridLineNamesValue())
             continue;
@@ -21,37 +21,132 @@ static unsigned computeNumberOfTracks(CSSValueList* valueList)
     return numberOfTracks;
 }
 
-TEST(CSSPropertyParserTest, DISABLED_GridTrackLimits)
+TEST(CSSPropertyParserTest, GridTrackLimit1)
 {
-    struct {
-        const CSSPropertyID propertyID;
-        const char* input;
-        const size_t output;
-    } testCases[] = {
-        {CSSPropertyGridTemplateColumns, "repeat(999999, 20px)", 999999},
-        {CSSPropertyGridTemplateRows, "repeat(999999, 20px)", 999999},
-        {CSSPropertyGridTemplateColumns, "repeat(1000000, 10%)", 1000000},
-        {CSSPropertyGridTemplateRows, "repeat(1000000, 10%)", 1000000},
-        {CSSPropertyGridTemplateColumns, "repeat(1000000, [first] min-content [last])", 1000000},
-        {CSSPropertyGridTemplateRows, "repeat(1000000, [first] min-content [last])", 1000000},
-        {CSSPropertyGridTemplateColumns, "repeat(1000001, auto)", 1000000},
-        {CSSPropertyGridTemplateRows, "repeat(1000001, auto)", 1000000},
-        {CSSPropertyGridTemplateColumns, "repeat(400000, 2em minmax(10px, max-content) 0.5fr)", 999999},
-        {CSSPropertyGridTemplateRows, "repeat(400000, 2em minmax(10px, max-content) 0.5fr)", 999999},
-        {CSSPropertyGridTemplateColumns, "repeat(600000, [first] 3vh 10% 2fr [nav] 10px auto 1fr 6em [last])", 999999},
-        {CSSPropertyGridTemplateRows, "repeat(600000, [first] 3vh 10% 2fr [nav] 10px auto 1fr 6em [last])", 999999},
-        {CSSPropertyGridTemplateColumns, "repeat(100000000000000000000, 10% 1fr)", 1000000},
-        {CSSPropertyGridTemplateRows, "repeat(100000000000000000000, 10% 1fr)", 1000000},
-        {CSSPropertyGridTemplateColumns, "repeat(100000000000000000000, 10% 5em 1fr auto auto 15px min-content)", 999999},
-        {CSSPropertyGridTemplateRows, "repeat(100000000000000000000, 10% 5em 1fr auto auto 15px min-content)", 999999},
-    };
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateColumns, "repeat(999999, 20px)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 999999);
+}
 
-    for (unsigned i = 0; i < WTF_ARRAY_LENGTH(testCases); ++i) {
-        RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(testCases[i].propertyID, testCases[i].input);
-        ASSERT_TRUE(value);
-        ASSERT_TRUE(value->isValueList());
-        EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), testCases[i].output);
-    }
+TEST(CSSPropertyParserTest, GridTrackLimit2)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateRows, "repeat(999999, 20px)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 999999);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit3)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateColumns, "repeat(1000000, 10%)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 1000000);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit4)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateRows, "repeat(1000000, 10%)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 1000000);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit5)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateColumns, "repeat(1000000, [first] min-content [last])");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 1000000);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit6)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateRows, "repeat(1000000, [first] min-content [last])");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 1000000);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit7)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateColumns, "repeat(1000001, auto)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 1000000);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit8)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateRows, "repeat(1000001, auto)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 1000000);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit9)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateColumns, "repeat(400000, 2em minmax(10px, max-content) 0.5fr)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 999999);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit10)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateRows, "repeat(400000, 2em minmax(10px, max-content) 0.5fr)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 999999);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit11)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateColumns, "repeat(600000, [first] 3vh 10% 2fr [nav] 10px auto 1fr 6em [last])");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 999999);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit12)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateRows, "repeat(600000, [first] 3vh 10% 2fr [nav] 10px auto 1fr 6em [last])");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 999999);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit13)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateColumns, "repeat(100000000000000000000, 10% 1fr)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 1000000);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit14)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateRows, "repeat(100000000000000000000, 10% 1fr)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 1000000);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit15)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateColumns, "repeat(100000000000000000000, 10% 5em 1fr auto auto 15px min-content)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 999999);
+}
+
+TEST(CSSPropertyParserTest, GridTrackLimit16)
+{
+    RefPtrWillBeRawPtr<CSSValue> value = CSSParser::parseSingleValue(CSSPropertyGridTemplateRows, "repeat(100000000000000000000, 10% 5em 1fr auto auto 15px min-content)");
+    ASSERT_TRUE(value);
+    ASSERT_TRUE(value->isValueList());
+    EXPECT_EQ(computeNumberOfTracks(toCSSValueList(value.get())), 999999);
 }
 
 } // namespace blink
