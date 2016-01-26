4480ddb71dfa8d169aa944dfdb1ec2c873f9ad61
nednguyen <nednguyen@google.com>
2016-01-26 12:39:57 -0800

    [tools/perf] Resolve the import error when record_wpr tries to discover benchmarks
    BUG=581103
    
    Review URL: https://codereview.chromium.org/1633143003
    
    Cr-Commit-Position: refs/heads/master@{#371586}
    
    Former-commit-id: 5b2e8d78eebbc33c577e5e04fa01ef00fed890d7

--- tools/perf/scripts_smoke_unittest.py
+++ tools/perf/scripts_smoke_unittest.py
@@ -9,8 +9,6 @@ import sys
 import tempfile
 import unittest
 
-from telemetry import decorators
-
 
 class ScriptsSmokeTest(unittest.TestCase):
 
@@ -50,7 +48,6 @@ class ScriptsSmokeTest(unittest.TestCase):
     self.assertEquals(return_code, 0, stdout)
     self.assertIn('optional arguments:', stdout)
 
-  @decorators.Disabled('all')  # crbug.com/581103
   def testRunRecordWprList(self):
     return_code, stdout = self.RunPerfScript('record_wpr --list-benchmarks')
     # TODO(nednguyen): Remove this once we figure out why importing
