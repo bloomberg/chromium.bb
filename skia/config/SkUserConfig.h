ba70695b39faf6063b704b25338d4b4efa0dbac9
caryclark <caryclark@google.com>
2016-01-26 13:40:12 -0800

    add legacy eval cubic
    An upcoming CL in Skia uses SIMD instructions to
    compute the coefficients for cubic and quadratic
    polynomials. This introduces some minor differences.
    
    Add a legacy flag to permit cubic eval() to work
    as it did in the past until the appropriate
    layout tests can be rebaselined.
    
    R=fmalita@chromium.org
    
    Review URL: https://codereview.chromium.org/1632313002
    
    Cr-Commit-Position: refs/heads/master@{#371606}
    
    Former-commit-id: 8fe699c7f4a45aef051a9528972e9b637ab5ebb5

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -234,6 +234,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_BITMAP_FILTER
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_EVAL_CUBIC
+#   define SK_SUPPORT_LEGACY_EVAL_CUBIC
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
