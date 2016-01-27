8949c76726f9759788b5baf9404a84003ab5d9b8
gcasto <gcasto@chromium.org>
2016-01-26 16:45:35 -0800

    Disable IndexedDBBrowserTestWithGCExposed.BlobDidAckPrefetch
    TBR=cmumford@chromium.org
    BUG=459835
    
    Review URL: https://codereview.chromium.org/1636203002
    
    Cr-Commit-Position: refs/heads/master@{#371664}
    
    Former-commit-id: 79be48223cecd3cac44513c441388cdd4e912231

--- content/browser/indexed_db/indexed_db_browsertest.cc
+++ content/browser/indexed_db/indexed_db_browsertest.cc
@@ -461,14 +461,9 @@ IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTestWithGCExposed, DISABLED_BlobDidAck) {
   EXPECT_EQ(0UL, blob_context->context()->blob_count());
 }
 
-// Very flaky on Linux. See crbug.com/459835.
-#if defined(OS_LINUX)
-#define MAYBE_BlobDidAckPrefetch DISABLED_BlobDidAckPrefetch
-#else
-#define MAYBE_BlobDidAckPrefetch BlobDidAckPrefetch
-#endif
+// Flaky. See crbug.com/459835.
 IN_PROC_BROWSER_TEST_F(IndexedDBBrowserTestWithGCExposed,
-                       MAYBE_BlobDidAckPrefetch) {
+                       DISABLED_BlobDidAckPrefetch) {
   SimpleTest(GetTestUrl("indexeddb", "blob_did_ack_prefetch.html"));
   // Wait for idle so that the blob ack has time to be received/processed by
   // the browser process.
