1918bb347532fb42a8cd71f6bb69c961f0f52492
rune <rune@opera.com>
2016-01-27 22:45:37 -0800

    ::before/::after are not features for invalidation.
    We store a flag for finding ::before or ::after in
    InvalidationSetFeatures, yet they are not added as features to
    invalidation sets. That means we need to handle *::before as a universal
    selector and cause subtree invalidations.
    
    R=ericwilligers@chromium.org
    BUG=581675
    
    Review URL: https://codereview.chromium.org/1639133003
    
    Cr-Commit-Position: refs/heads/master@{#372035}
    
    Former-commit-id: c3937b7ccbe9b8ad5afd3210831daaac5e2cb213

--- third_party/WebKit/Source/core/css/RuleFeature.cpp
+++ third_party/WebKit/Source/core/css/RuleFeature.cpp
@@ -280,21 +280,29 @@ ALWAYS_INLINE InvalidationSet& RuleFeatureSet::ensurePseudoInvalidationSet(CSSSe
 
 bool RuleFeatureSet::extractInvalidationSetFeature(const CSSSelector& selector, InvalidationSetFeatures& features)
 {
-    if (selector.match() == CSSSelector::Tag && selector.tagQName().localName() != starAtom)
+    if (selector.match() == CSSSelector::Tag && selector.tagQName().localName() != starAtom) {
         features.tagName = selector.tagQName().localName();
-    else if (selector.match() == CSSSelector::Id)
+        return true;
+    }
+    if (selector.match() == CSSSelector::Id) {
         features.id = selector.value();
-    else if (selector.match() == CSSSelector::Class)
+        return true;
+    }
+    if (selector.match() == CSSSelector::Class) {
         features.classes.append(selector.value());
-    else if (selector.isAttributeSelector())
+        return true;
+    }
+    if (selector.isAttributeSelector()) {
         features.attributes.append(selector.attribute().localName());
-    else if (selector.pseudoType() == CSSSelector::PseudoWebKitCustomElement)
+        return true;
+    }
+    if (selector.pseudoType() == CSSSelector::PseudoWebKitCustomElement) {
         features.customPseudoElement = true;
-    else if (selector.pseudoType() == CSSSelector::PseudoBefore || selector.pseudoType() == CSSSelector::PseudoAfter)
+        return true;
+    }
+    if (selector.pseudoType() == CSSSelector::PseudoBefore || selector.pseudoType() == CSSSelector::PseudoAfter)
         features.hasBeforeOrAfter = true;
-    else
-        return false;
-    return true;
+    return false;
 }
 
 InvalidationSet* RuleFeatureSet::invalidationSetForSelector(const CSSSelector& selector, InvalidationType type)
