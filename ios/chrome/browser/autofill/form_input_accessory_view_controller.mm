e60e6ab67b8325f6aade36b021768f1259dde2c6
lpromero <lpromero@chromium.org>
2016-01-28 14:44:21 -0800

    Revert "Add an accessibility notification when new suggestions are shown."
    This reverts commit fc9c5b46cd307723fcc1763008083ae28e9e94f7 [formerly 1602a2c9d3bffb19087140f22ac0b9462144e8d5].
    
    This breaks VoiceOver as described in http://crbug.com/581813
    
    BUG=581813
    R=rohitrao@chromium.org,pkl@chromium.org
    
    Review URL: https://codereview.chromium.org/1640273003
    
    Cr-Commit-Position: refs/heads/master@{#372191}
    
    Former-commit-id: 51c10f2256cc81207fd494f31a56df23bf594ba5

--- ios/chrome/browser/autofill/form_input_accessory_view_controller.mm
+++ ios/chrome/browser/autofill/form_input_accessory_view_controller.mm
@@ -16,13 +16,11 @@
 #import "ios/chrome/browser/autofill/form_suggestion_view.h"
 #import "ios/chrome/browser/passwords/password_generation_utils.h"
 #include "ios/chrome/browser/ui/ui_util.h"
-#include "ios/chrome/grit/ios_strings.h"
 #include "ios/web/public/test/crw_test_js_injection_receiver.h"
 #import "ios/web/public/url_scheme_util.h"
 #import "ios/web/public/web_state/crw_web_view_proxy.h"
 #include "ios/web/public/web_state/url_verification_constants.h"
 #include "ios/web/public/web_state/web_state.h"
-#include "ui/base/l10n/l10n_util.h"
 #include "url/gurl.h"
 
 namespace autofill {
@@ -379,9 +377,6 @@ bool ComputeFramesOfKeyboardParts(UIView* inputAccessoryView,
       [inputAccessoryView addSubview:_customAccessoryView];
     }
   }
-  UIAccessibilityPostNotification(
-      UIAccessibilityScreenChangedNotification,
-      l10n_util::GetNSString(IDS_IOS_AUTOFILL_ACCNAME_PREVIOUS_FIELD));
 }
 
 - (void)restoreDefaultInputAccessoryView {
