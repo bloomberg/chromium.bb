0e5d3ada954623ba7dee7831c45d7d912f5f1a38
estade <estade@chromium.org>
2016-01-27 22:15:40 -0800

    Repaint ImageButton when image is changed.
    This matches behavior of ImageView::SetImage.
    
    BUG=581965
    
    Review URL: https://codereview.chromium.org/1641993002
    
    Cr-Commit-Position: refs/heads/master@{#372031}
    
    Former-commit-id: 6d865d247c9b1394124cbbdd562d9a1843015019

--- ui/views/controls/button/image_button.cc
+++ ui/views/controls/button/image_button.cc
@@ -47,9 +47,11 @@ const gfx::ImageSkia& ImageButton::GetImage(ButtonState state) const {
   return images_[state];
 }
 
-void ImageButton::SetImage(ButtonState state, const gfx::ImageSkia* image) {
-  images_[state] = image ? *image : gfx::ImageSkia();
+void ImageButton::SetImage(ButtonState for_state, const gfx::ImageSkia* image) {
+  images_[for_state] = image ? *image : gfx::ImageSkia();
   PreferredSizeChanged();
+  if (state() == for_state)
+    SchedulePaint();
 }
 
 void ImageButton::SetBackground(SkColor color,
