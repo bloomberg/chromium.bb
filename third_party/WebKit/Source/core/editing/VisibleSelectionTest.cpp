dbdbd0bd550da9a6d72ca9fcaeed156565e67888
yosin <yosin@chromium.org>
2016-01-28 18:48:15 -0800

    Introduce SelectionAdjuster to adjust selections between DOM tree version and composed tree version
    This patch introduces |SelectionAdjuster| class to consolidate functions
    adjusting DOM tree version and composed tree version of selections in
    |VisibleSelection| class and |SelectionEditor| in one place to avoid having
    six parameters constructor, and rude name function |createWithoutValidation()|
    to simplify source code for improving readability.
    
    Note: For ease of reviewing to compare changes, this patch doesn't leave
    functions in original place. Following patch moves them to
    "SelectionAdjust.{cpp,h}".
    
    This patch is a preparation of introducing |m_hasTrailingWhitespace| into
    |VisibleSelection| class, which makes six parameters constructor to seven
    parameters constructor without this patch.
    
    Note: This patch is re-land of crrev.com/1630903005, which caused
    crbug.com/581800, since it didn't notify selection observer about change.
    To avoid this regression, this patch introduces |SelectionAdjusterTest|.
    
    BUG=581800
    TEST=n/a; no behavior changes
    
    Review URL: https://codereview.chromium.org/1642823003
    
    Cr-Commit-Position: refs/heads/master@{#372264}
    
    Former-commit-id: f3b5b839c341444ea2d08154a7fc080244cfc24e

--- third_party/WebKit/Source/core/editing/VisibleSelectionTest.cpp
+++ third_party/WebKit/Source/core/editing/VisibleSelectionTest.cpp
@@ -351,4 +351,65 @@ TEST_F(VisibleSelectionTest, WordGranularity)
     }
 }
 
+// TODO(yosin) We should move |SelectionAdjustTest| to
+// "SelectionAdjustTest.cpp"
+class SelectionAdjusterTest : public EditingTestBase  {
+};
+
+// TODO(yosin) We should move |MockVisibleSelectionChangeObserver| to
+// "SelectionAdjustTest.cpp"
+class MockVisibleSelectionChangeObserver final :
+    public NoBaseWillBeGarbageCollectedFinalized<MockVisibleSelectionChangeObserver>,
+    public VisibleSelectionChangeObserver {
+    WTF_MAKE_NONCOPYABLE(MockVisibleSelectionChangeObserver);
+    WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(MockVisibleSelectionChangeObserver);
+public:
+    MockVisibleSelectionChangeObserver() = default;
+    ~MockVisibleSelectionChangeObserver() final = default;
+
+    int callCounter() const { return m_callCounter; }
+
+private:
+    // VisibleSelectionChangeObserver interface.
+    void didChangeVisibleSelection() final { ++m_callCounter; }
+
+    int m_callCounter = 0;
+};
+
+TEST_F(SelectionAdjusterTest, adjustSelectionInComposedTree)
+{
+    setBodyContent("<div id=sample>foo</div>");
+    MockVisibleSelectionChangeObserver selectionObserver;
+    VisibleSelectionInComposedTree selectionInComposedTree;
+    selectionInComposedTree.setChangeObserver(selectionObserver);
+
+    Node* const sample = document().getElementById("sample");
+    Node* const foo = sample->firstChild();
+    // Select "foo"
+    VisibleSelection selection(Position(foo, 0), Position(foo, 3));
+    SelectionAdjuster::adjustSelectionInComposedTree(&selectionInComposedTree, selection);
+    EXPECT_EQ(PositionInComposedTree(foo, 0), selectionInComposedTree.start());
+    EXPECT_EQ(PositionInComposedTree(foo, 3), selectionInComposedTree.end());
+    EXPECT_EQ(1, selectionObserver.callCounter()) << "adjustSelectionInComposedTree() should call didChangeVisibleSelection()";
+}
+
+TEST_F(SelectionAdjusterTest, adjustSelectionInDOMTree)
+{
+    setBodyContent("<div id=sample>foo</div>");
+    MockVisibleSelectionChangeObserver selectionObserver;
+    VisibleSelection selection;
+    selection.setChangeObserver(selectionObserver);
+
+    Node* const sample = document().getElementById("sample");
+    Node* const foo = sample->firstChild();
+    // Select "foo"
+    VisibleSelectionInComposedTree selectionInComposedTree(
+        PositionInComposedTree(foo, 0),
+        PositionInComposedTree(foo, 3));
+    SelectionAdjuster::adjustSelectionInDOMTree(&selection, selectionInComposedTree);
+    EXPECT_EQ(Position(foo, 0), selection.start());
+    EXPECT_EQ(Position(foo, 3), selection.end());
+    EXPECT_EQ(1, selectionObserver.callCounter()) << "adjustSelectionInDOMTree() should call didChangeVisibleSelection()";
+}
+
 } // namespace blink
