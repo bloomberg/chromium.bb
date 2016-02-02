ab72aae893f259e32d3c4060567d9213502f959d
tridgell <tridgell@google.com>
2016-02-01 20:15:37 -0800

    Typed CSS OM: MatrixTransformComponent change member variables
    Change way matrix is stored / represented.
    
    Use a TransformationMatrix as a member variable.
    
    Allows for extensibility and easy implementation of asMatrix.
    
    BUG=545318
    
    Review URL: https://codereview.chromium.org/1597893004
    
    Cr-Commit-Position: refs/heads/master@{#372886}
    
    Former-commit-id: aad1e31c53c27e37d182a2a7f8e7e3fd28f8ee90

--- third_party/WebKit/Source/core/css/cssom/MatrixTransformComponent.h
+++ third_party/WebKit/Source/core/css/cssom/MatrixTransformComponent.h
@@ -6,6 +6,7 @@
 #define MatrixTransformComponent_h
 
 #include "core/css/cssom/TransformComponent.h"
+#include "platform/transforms/TransformationMatrix.h"
 
 namespace blink {
 
@@ -27,30 +28,30 @@ public:
     }
 
     // 2D matrix attributes
-    double a() const { return m_m11; }
-    double b() const { return m_m12; }
-    double c() const { return m_m21; }
-    double d() const { return m_m22; }
-    double e() const { return m_m41; }
-    double f() const { return m_m42; }
+    double a() const { return m_matrix->a(); }
+    double b() const { return m_matrix->b(); }
+    double c() const { return m_matrix->c(); }
+    double d() const { return m_matrix->d(); }
+    double e() const { return m_matrix->e(); }
+    double f() const { return m_matrix->f(); }
 
     // 3D matrix attributes
-    double m11() const { return m_m11; }
-    double m12() const { return m_m12; }
-    double m13() const { return m_m13; }
-    double m14() const { return m_m14; }
-    double m21() const { return m_m21; }
-    double m22() const { return m_m22; }
-    double m23() const { return m_m23; }
-    double m24() const { return m_m24; }
-    double m31() const { return m_m31; }
-    double m32() const { return m_m32; }
-    double m33() const { return m_m33; }
-    double m34() const { return m_m34; }
-    double m41() const { return m_m41; }
-    double m42() const { return m_m42; }
-    double m43() const { return m_m43; }
-    double m44() const { return m_m44; }
+    double m11() const { return m_matrix->m11(); }
+    double m12() const { return m_matrix->m12(); }
+    double m13() const { return m_matrix->m13(); }
+    double m14() const { return m_matrix->m14(); }
+    double m21() const { return m_matrix->m21(); }
+    double m22() const { return m_matrix->m22(); }
+    double m23() const { return m_matrix->m23(); }
+    double m24() const { return m_matrix->m24(); }
+    double m31() const { return m_matrix->m31(); }
+    double m32() const { return m_matrix->m32(); }
+    double m33() const { return m_matrix->m33(); }
+    double m34() const { return m_matrix->m34(); }
+    double m41() const { return m_matrix->m41(); }
+    double m42() const { return m_matrix->m42(); }
+    double m43() const { return m_matrix->m43(); }
+    double m44() const { return m_matrix->m44(); }
 
     TransformComponentType type() const override { return m_is2D ? MatrixType : Matrix3DType; }
 
@@ -59,22 +60,7 @@ public:
 private:
     MatrixTransformComponent(double a, double b, double c, double d, double e, double f)
         : TransformComponent()
-        , m_m11(a)
-        , m_m12(b)
-        , m_m13(0)
-        , m_m14(0)
-        , m_m21(c)
-        , m_m22(d)
-        , m_m23(0)
-        , m_m24(0)
-        , m_m31(0)
-        , m_m32(0)
-        , m_m33(1)
-        , m_m34(0)
-        , m_m41(e)
-        , m_m42(f)
-        , m_m43(0)
-        , m_m44(1)
+        , m_matrix(adoptPtr(new TransformationMatrix(a, b, c, d, e, f)))
         , m_is2D(true)
     { }
 
@@ -83,41 +69,15 @@ private:
         double m31, double m32, double m33, double m34,
         double m41, double m42, double m43, double m44)
         : TransformComponent()
-        , m_m11(m11)
-        , m_m12(m12)
-        , m_m13(m13)
-        , m_m14(m14)
-        , m_m21(m21)
-        , m_m22(m22)
-        , m_m23(m23)
-        , m_m24(m24)
-        , m_m31(m31)
-        , m_m32(m32)
-        , m_m33(m33)
-        , m_m34(m34)
-        , m_m41(m41)
-        , m_m42(m42)
-        , m_m43(m43)
-        , m_m44(m44)
+        , m_matrix(adoptPtr(new TransformationMatrix(m11, m12, m13, m14, m21, m22, m23, m24, m31, m32, m33, m34, m41, m42, m43, m44)))
         , m_is2D(false)
     { }
 
-    double m_m11;
-    double m_m12;
-    double m_m13;
-    double m_m14;
-    double m_m21;
-    double m_m22;
-    double m_m23;
-    double m_m24;
-    double m_m31;
-    double m_m32;
-    double m_m33;
-    double m_m34;
-    double m_m41;
-    double m_m42;
-    double m_m43;
-    double m_m44;
+    // TransformationMatrix needs to be 16-byte aligned. PartitionAlloc
+    // supports 16-byte alignment but Oilpan doesn't. So we use an OwnPtr
+    // to allocate TransformationMatrix on PartitionAlloc.
+    // TODO(oilpan): Oilpan should support 16-byte aligned allocations.
+    OwnPtr<const TransformationMatrix> m_matrix;
     bool m_is2D;
 };
 
