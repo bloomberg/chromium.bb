c73049ddb0e42d2e70b69c235d2eaa1b8c3ce130
simonhatch <simonhatch@chromium.org>
2016-02-01 16:26:45 -0800

    Disable v8.infinite_scroll on linux.
    BUG=583101
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:mac_10_10_perf_bisect;tryserver.chromium.perf:win_perf_bisect;tryserver.chromium.perf:android_nexus5_perf_bisect
    
    Review URL: https://codereview.chromium.org/1651083003
    
    Cr-Commit-Position: refs/heads/master@{#372837}
    
    Former-commit-id: 3ff8e36bd2041e1ee2089e21d3f97c004a2252ef

--- tools/perf/benchmarks/v8.py
+++ tools/perf/benchmarks/v8.py
@@ -53,7 +53,9 @@ class V8DetachedContextAgeInGC(perf_benchmark.PerfBenchmark):
 
 # Disabled on reference builds because they don't support the new
 # Tracing.requestMemoryDump DevTools API. See http://crbug.com/540022.
-@benchmark.Disabled('reference', 'android')  # crbug.com/579546
+@benchmark.Disabled('reference',
+                    'android',   # crbug.com/579546
+                    'linux')     # crbug.com/583101
 class V8InfiniteScroll(perf_benchmark.PerfBenchmark):
   """Measures V8 GC metrics and memory usage while scrolling the top web pages.
   http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
