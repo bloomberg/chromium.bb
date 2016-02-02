8d9ac8ec8e5ce4499c1a949eee2de6902ad4d327
nektar <nektar@chromium.org>
2016-02-01 18:50:29 -0800

    Disables content editable tests on the Mac until work to fix text navigation and selection is completed.
    BUG=571712
    TBR=dmazzoni@chromium.org
    
    Review URL: https://codereview.chromium.org/1654403002
    
    Cr-Commit-Position: refs/heads/master@{#372874}
    
    Former-commit-id: 941dc8e0717d3b098a41d6c79902733f8c20fafb

--- content/browser/accessibility/dump_accessibility_tree_browsertest.cc
+++ content/browser/accessibility/dump_accessibility_tree_browsertest.cc
@@ -710,8 +710,9 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest, AccessibilityDt) {
   RunHtmlTest(FILE_PATH_LITERAL("dt.html"));
 }
 
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(OS_MACOSX)
 // Flaky failures: http://crbug.com/445929.
+// Mac failures: http://crbug.com/571712.
 #define MAYBE_AccessibilityContenteditableDescendants \
     DISABLED_AccessibilityContenteditableDescendants
 #else
@@ -728,8 +729,9 @@ IN_PROC_BROWSER_TEST_F(DumpAccessibilityTreeTest,
   RunHtmlTest(FILE_PATH_LITERAL("element-class-id-src-attr.html"));
 }
 
-#if defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(OS_MACOSX)
 // Flaky failures: http://crbug.com/445929.
+// Mac failures: http://crbug.com/571712.
 #define MAYBE_AccessibilityContenteditableDescendantsWithSelection \
     DISABLED_AccessibilityContenteditableDescendantsWithSelection
 #else
