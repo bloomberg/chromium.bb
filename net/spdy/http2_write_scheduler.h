f420e8d9a3d7549c518c11a715f306c4539b92cf
bnc <bnc@chromium.org>
2016-02-04 12:30:45 -0800

    Rename spdy_priority_tree*
    Rename spdy_priority_tree*.  This CL is solely so that
    https://crrev.com/1660283002 can be rebased on it, and thus version history
    is not completely lost.
    
    This CL lands part of server change 112594726 by mpw.
    
    BUG=488484
    
    Review URL: https://codereview.chromium.org/1668773003
    
    Cr-Commit-Position: refs/heads/master@{#373607}
    
    Former-commit-id: 371faba5e24c2baebe69dc7105ac80ad132d9c20

--- net/spdy/http2_write_scheduler.h
+++ net/spdy/http2_write_scheduler.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef NET_SPDY_SPDY_PRIORITY_TREE_H_
-#define NET_SPDY_SPDY_PRIORITY_TREE_H_
+#ifndef NET_SPDY_HTTP2_WRITE_SCHEDULER_H_
+#define NET_SPDY_HTTP2_WRITE_SCHEDULER_H_
 
 #include <cmath>
 #include <deque>
@@ -560,4 +560,4 @@ bool SpdyPriorityTree<NodeId>::ValidateInvariantsForTests() const {
 
 }  // namespace net
 
-#endif  // NET_SPDY_SPDY_PRIORITY_TREE_H_
+#endif  // NET_SPDY_HTTP2_WRITE_SCHEDULER_H_
--- net/spdy/http2_write_scheduler_test.cc
+++ net/spdy/http2_write_scheduler_test.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "net/spdy/spdy_priority_tree.h"
+#include "net/spdy/http2_write_scheduler.h"
 
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
