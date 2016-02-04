168220a3d9823d6eea1a8ed0036e8340a53cd1f5
ssid <ssid@chromium.org>
2016-02-04 07:34:50 -0800

    Reland of move --no-sandbox flag from command line for telemetry (patchset #1 id:1 of https://codereview.chromium.org/1659033002/ )
    Reason for revert:
    All the bots that failed were running Precise and have been moved
    to Trusty now. crbug.com/583408.
    
    Original issue's description:
    > Revert of Remove --no-sandbox flag from command line for telemetry (patchset #4 id:60001 of https://codereview.chromium.org/1584583003/ )
    >
    > Reason for revert:
    > MemoryInfra benchmarks are failing after this CL.
    > crbug.com/583101
    >
    > Original issue's description:
    > > Remove --no-sandbox flag from command line for telemetry
    > >
    > > Now that the browser is able to dump for child processes the
    > > --no-sandbox flag is not required for getting memory statistics. So,
    > > this is removed form the telemetry benchmarks.
    > >
    > > BUG=461788
    > > CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:win_perf_bisect
    > >
    > > Committed: https://crrev.com/ced1ee2cf558dc993d05343af4d8cf52020d8f13 [formerly 2bf18cfdfa1b8b0bd829edea2d90277e7e5b0b71]
    > > Cr-Commit-Position: refs/heads/master@{#372667}
    >
    > TBR=primiano@chromium.org,nednguyen@google.com
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=461788
    >
    > Committed: https://crrev.com/239564c75c2374c69fe8b46818d4e8e7687fb4b2 [formerly 203ca2a0729aa404fc724583a360377a3793cae4]
    > Cr-Commit-Position: refs/heads/master@{#372927}
    
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:linux_perf_bisect;tryserver.chromium.perf:win_perf_bisect
    TBR=primiano@chromium.org,nednguyen@google.com
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=461788
    
    Review URL: https://codereview.chromium.org/1663053005
    
    Cr-Commit-Position: refs/heads/master@{#373540}
    
    Former-commit-id: 0ef5d829a8b875ab67abc58840aa4a01e6a88df9

--- base/trace_event/memory_dump_manager.cc
+++ base/trace_event/memory_dump_manager.cc
@@ -420,9 +420,7 @@ void MemoryDumpManager::ContinueAsyncProcessDump(
     if (!mdpinfo->disabled) {
       if (mdpinfo->consecutive_failures >= kMaxConsecutiveFailuresCount) {
         mdpinfo->disabled = true;
-        disabled_reason =
-            "Dump failure, possibly related with sandboxing (crbug.com/461788)."
-            " Try --no-sandbox.";
+        disabled_reason = "Dump failed multiple times consecutively.";
       } else if (post_task_failed && mdpinfo->task_runner) {
         // Don't disable unbound dump providers. The utility thread is normally
         // shutdown when disabling the trace and getting here in this case is
