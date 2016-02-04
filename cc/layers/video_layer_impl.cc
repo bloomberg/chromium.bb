3ca7f30e6af4482f228f98783fc1c9de15bd45b2
wuchengli <wuchengli@chromium.org>
2016-02-04 00:12:06 -0800

    VideoLayerImpl: subtract 1 from visible size when calculating tex stretch.
    The video texture only has "visible rect" valid and we're sampling
    outside of that visible rect and into garbage texture space due to
    bilinear filtering. The fix is to stretch the video such that it will
    always have an extra set of texels to sample from. Subtract one from
    the visible size when calculating the tex stretch x and y scale.
    
    BUG=575587
    TEST=Test youtube.com/watch?v=CoOFjXk4xLY on nyan_big and peach_pi.
    Run cc_unittests.
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review URL: https://codereview.chromium.org/1645353003
    
    Cr-Commit-Position: refs/heads/master@{#373478}
    
    Former-commit-id: 64647caadd62fc639f6a3bb194557d8447f6d419

--- cc/layers/video_layer_impl.cc
+++ cc/layers/video_layer_impl.cc
@@ -184,11 +184,17 @@ void VideoLayerImpl::AppendQuads(RenderPass* render_pass,
   if (visible_quad_rect.IsEmpty())
     return;
 
-  // Pixels for macroblocked formats.
+  // Pixels for macroblocked formats. To prevent sampling outside the visible
+  // rect, stretch the video if needed.
+  gfx::Rect visible_sample_rect = frame_->visible_rect();
+  if (visible_rect.width() < coded_size.width() && visible_rect.width() > 1)
+    visible_sample_rect.set_width(visible_rect.width() - 1);
+  if (visible_rect.height() < coded_size.height() && visible_rect.height() > 1)
+    visible_sample_rect.set_height(visible_rect.height() - 1);
   const float tex_width_scale =
-      static_cast<float>(visible_rect.width()) / coded_size.width();
+      static_cast<float>(visible_sample_rect.width()) / coded_size.width();
   const float tex_height_scale =
-      static_cast<float>(visible_rect.height()) / coded_size.height();
+      static_cast<float>(visible_sample_rect.height()) / coded_size.height();
 
   switch (frame_resource_type_) {
     // TODO(danakj): Remove this, hide it in the hardware path.
@@ -257,12 +263,12 @@ void VideoLayerImpl::AppendQuads(RenderPass* render_pass,
           static_cast<float>(ya_tex_size.width()) / uv_tex_size.width();
       float uv_subsampling_factor_y =
           static_cast<float>(ya_tex_size.height()) / uv_tex_size.height();
-      gfx::RectF ya_tex_coord_rect(visible_rect);
+      gfx::RectF ya_tex_coord_rect(visible_sample_rect);
       gfx::RectF uv_tex_coord_rect(
-          visible_rect.x() / uv_subsampling_factor_x,
-          visible_rect.y() / uv_subsampling_factor_y,
-          visible_rect.width() / uv_subsampling_factor_x,
-          visible_rect.height() / uv_subsampling_factor_y);
+          visible_sample_rect.x() / uv_subsampling_factor_x,
+          visible_sample_rect.y() / uv_subsampling_factor_y,
+          visible_sample_rect.width() / uv_subsampling_factor_x,
+          visible_sample_rect.height() / uv_subsampling_factor_y);
 
       YUVVideoDrawQuad* yuv_video_quad =
           render_pass->CreateAndAppendDrawQuad<YUVVideoDrawQuad>();
