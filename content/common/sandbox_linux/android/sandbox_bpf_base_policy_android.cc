94ac60442d41997d6d89dc870ef937cc0062c8ef
rsesek <rsesek@chromium.org>
2016-02-05 14:57:38 -0800

    [Android] Fix Microdump generation when Seccomp-BPF is enabled.
    BUG=584518,439573
    
    Review URL: https://codereview.chromium.org/1669043003
    
    Cr-Commit-Position: refs/heads/master@{#373934}
    
    Former-commit-id: 4fe32a5a3c3c5db910517f70f45ea03ee1c676ed

--- content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.cc
+++ content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.cc
@@ -4,16 +4,32 @@
 
 #include "content/common/sandbox_linux/android/sandbox_bpf_base_policy_android.h"
 
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/socket.h>
 #include <sys/syscall.h>
 #include <sys/types.h>
 
 #include "build/build_config.h"
 #include "sandbox/linux/bpf_dsl/bpf_dsl.h"
 
+using sandbox::bpf_dsl::AllOf;
 using sandbox::bpf_dsl::Allow;
+using sandbox::bpf_dsl::AnyOf;
+using sandbox::bpf_dsl::Arg;
+using sandbox::bpf_dsl::If;
+using sandbox::bpf_dsl::Error;
 using sandbox::bpf_dsl::ResultExpr;
 
 namespace content {
+
+#ifndef SOCK_CLOEXEC
+#define SOCK_CLOEXEC O_CLOEXEC
+#endif
+
+#ifndef SOCK_NONBLOCK
+#define SOCK_NONBLOCK O_NONBLOCK
+#endif
 
 SandboxBPFBasePolicyAndroid::SandboxBPFBasePolicyAndroid()
     : SandboxBPFBasePolicy() {}
@@ -30,8 +46,10 @@ ResultExpr SandboxBPFBasePolicyAndroid::EvaluateSyscall(int sysno) const {
     case __NR_flock:
 #if defined(__x86_64__) || defined(__aarch64__)
     case __NR_newfstatat:
+    case __NR_getdents64:
 #elif defined(__i386__) || defined(__arm__) || defined(__mips__)
     case __NR_fstatat64:
+    case __NR_getdents:
 #endif
     case __NR_getpriority:
     case __NR_ioctl:
@@ -56,8 +74,30 @@ ResultExpr SandboxBPFBasePolicyAndroid::EvaluateSyscall(int sysno) const {
     case __NR_getrlimit:
 #endif
     case __NR_uname:
+
+    // Permit socket operations so that renderers can connect to logd and
+    // debuggerd. The arguments to socket() are further restricted below.
+    case __NR_socket:
+    case __NR_connect:
+
+    // Ptrace is allowed so the Breakpad Microdumper can fork in a renderer
+    // and then ptrace the parent.
+    case __NR_ptrace:
       override_and_allow = true;
       break;
+  }
+
+  if (sysno == __NR_socket) {
+    const Arg<int> domain(0);
+    const Arg<int> type(1);
+    const Arg<int> protocol(2);
+    const int kSockFlags = SOCK_CLOEXEC | SOCK_NONBLOCK;
+    return If(AllOf(domain == AF_UNIX,
+                    AnyOf((type & ~kSockFlags) == SOCK_DGRAM,
+                          (type & ~kSockFlags) == SOCK_STREAM),
+                    protocol == 0),
+              Allow())
+           .Else(Error(EPERM));
   }
 
   if (override_and_allow)
