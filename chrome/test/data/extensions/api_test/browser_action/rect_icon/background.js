bloomberg/chromium.bb

commit 638a2d5038b31a0e9dbfe12bd32a89a57e360b4e

Fix bug in browser action icon scaling for non-1x scale factors.
skia::ImageOperations::Resize needs pixels, and we were passing dp.

Also, ignore the icon size key and just use the icon size itself. This fixes a bug (evidenced again by Shoptagr) where an icon size such as

"19": "icons/19.png"  // 19.png is actually 50x50

would be scaled incorrectly. At this point, the API should take a list rather than a dict, but it's probably not possible to change that and maintain backcompat.

BUG=581232
TBR=benwells@chromium.org

Review URL: https://codereview.chromium.org/1637763002

Cr-Commit-Position: refs/heads/master@{#371904}

Former-commit-id: 52969d7388642a56447da090bf577974a6bf562f

chrome/browser/extensions/extension_action.cc
@@ -129,14 +129,6 @@ bool ExtensionAction::ParseIconFromCanvasDictionary(
     gfx::ImageSkia* icon) {
   for (base::DictionaryValue::Iterator iter(dict); !iter.IsAtEnd();
        iter.Advance()) {
-    int icon_size = 0;
-    // Chrome helpfully scales the provided icon(s), but let's not go overboard.
-    const int kActionIconMaxSize = 10 * extension_misc::EXTENSION_ICON_ACTION;
-    if (!base::StringToInt(iter.key(), &icon_size) || icon_size <= 0 ||
-        icon_size > kActionIconMaxSize) {
-      continue;
-    }
-
     const base::BinaryValue* image_data;
     std::string binary_string64;
     IPC::Message pickle;
@@ -155,8 +147,16 @@ bool ExtensionAction::ParseIconFromCanvasDictionary(
     if (!IPC::ReadParam(&pickle, &pickle_iter, &bitmap))
       return false;
     CHECK(!bitmap.isNull());
+
+    // Chrome helpfully scales the provided icon(s), but let's not go overboard.
+    const int kActionIconMaxSize = 10 * extension_misc::EXTENSION_ICON_ACTION;
+    if (bitmap.drawsNothing() || bitmap.width() != bitmap.height() ||
+        bitmap.width() > kActionIconMaxSize) {
+      continue;
+    }
+
     float scale =
-        static_cast<float>(icon_size) / ExtensionAction::ActionIconSize();
+        static_cast<float>(bitmap.width()) / ExtensionAction::ActionIconSize();
     icon->AddRepresentation(gfx::ImageSkiaRep(bitmap, scale));
   }
   return true;

chrome/browser/ui/extensions/icon_with_badge_image_source.cc
@@ -11,6 +11,7 @@
 #include "base/logging.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
+#include "chrome/browser/extensions/extension_action.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 #include "ui/base/material_design/material_design_controller.h"
@@ -90,12 +91,13 @@ SkPaint* GetBadgeTextPaintSingleton() {
 }
 
 gfx::ImageSkiaRep ScaleImageSkiaRep(const gfx::ImageSkiaRep& rep,
-                                    int target_width,
+                                    int target_width_dp,
                                     float target_scale) {
+  int width_px = target_width_dp * target_scale;
   return gfx::ImageSkiaRep(
       skia::ImageOperations::Resize(rep.sk_bitmap(),
                                     skia::ImageOperations::RESIZE_BEST,
-                                    target_width, target_width),
+                                    width_px, width_px),
       target_scale);
 }
 
@@ -130,14 +132,16 @@ void IconWithBadgeImageSource::Draw(gfx::Canvas* canvas) {
   gfx::ImageSkia skia = icon_.AsImageSkia();
   gfx::ImageSkiaRep rep = skia.GetRepresentation(canvas->image_scale());
   if (rep.scale() != canvas->image_scale()) {
-    skia.AddRepresentation(
-        ScaleImageSkiaRep(rep, skia.width(), canvas->image_scale()));
+    skia.AddRepresentation(ScaleImageSkiaRep(
+        rep, ExtensionAction::ActionIconSize(), canvas->image_scale()));
   }
   if (grayscale_)
     skia = gfx::ImageSkiaOperations::CreateHSLShiftedImage(skia, {-1, 0, 0.6});
 
-  int x_offset = std::floor((size().width() - icon_.Width()) / 2.0);
-  int y_offset = std::floor((size().height() - icon_.Height()) / 2.0);
+  int x_offset =
+      std::floor((size().width() - ExtensionAction::ActionIconSize()) / 2.0);
+  int y_offset =
+      std::floor((size().height() - ExtensionAction::ActionIconSize()) / 2.0);
   canvas->DrawImageInt(skia, x_offset, y_offset);
 
   // Draw a badge on the provided browser action icon's canvas.

commit 69edf304b48a6dbe2a3319d2d1d69885ae2817dd

[Extensions] Allow extensions to set a rectangular icon for the extension action
Support rectangular icon images for the extension actions. This regressed in
revision 638a2d5038b31a0e9dbfe12bd32a89a57e360b4e [formerly 52969d7388642a56447da090bf577974a6bf562f]. Fix, and add a test.

BUG=582924

Review URL: https://codereview.chromium.org/1669163002

Cr-Commit-Position: refs/heads/master@{#373904}

Former-commit-id: e2a93e361c49b55cd3a2d750e494389c39f489b5

chrome/browser/extensions/api/extension_action/browser_action_apitest.cc
@@ -31,6 +31,7 @@
 #include "extensions/browser/process_manager.h"
 #include "extensions/browser/test_extension_registry_observer.h"
 #include "extensions/common/feature_switch.h"
+#include "extensions/test/extension_test_message_listener.h"
 #include "extensions/test/result_catcher.h"
 #include "grit/theme_resources.h"
 #include "net/dns/mock_host_resolver.h"
@@ -734,6 +735,19 @@ IN_PROC_BROWSER_TEST_F(BrowserActionApiTest, BrowserActionPopupWithIframe) {
   EXPECT_EQ("DONE", result);
 
   EXPECT_TRUE(actions_bar->HidePopup());
+}
+
+IN_PROC_BROWSER_TEST_F(BrowserActionApiTest, BrowserActionWithRectangularIcon) {
+  ExtensionTestMessageListener ready_listener("ready", true);
+  ASSERT_TRUE(LoadExtension(
+      test_data_dir_.AppendASCII("browser_action").AppendASCII("rect_icon")));
+  EXPECT_TRUE(ready_listener.WaitUntilSatisfied());
+  gfx::Image first_icon = GetBrowserActionsBar()->GetIcon(0);
+  ResultCatcher catcher;
+  ready_listener.Reply(std::string());
+  EXPECT_TRUE(catcher.GetNextResult());
+  gfx::Image next_icon = GetBrowserActionsBar()->GetIcon(0);
+  EXPECT_FALSE(gfx::test::AreImagesEqual(first_icon, next_icon));
 }
 
 }  // namespace

chrome/browser/extensions/api/extension_action/extension_action_api.cc
@@ -503,6 +503,11 @@ bool ExtensionActionSetIconFunction::RunExtensionAction() {
     EXTENSION_FUNCTION_VALIDATE(
         ExtensionAction::ParseIconFromCanvasDictionary(*canvas_set, &icon));
 
+    if (icon.isNull()) {
+      error_ = "Icon invalid.";
+      return false;
+    }
+
     extension_action_->SetIcon(tab_id_, gfx::Image(icon));
   } else if (details_->GetInteger("iconIndex", &icon_index)) {
     // Obsolete argument: ignore it.

chrome/browser/extensions/extension_action.cc
@@ -150,10 +150,8 @@ bool ExtensionAction::ParseIconFromCanvasDictionary(
 
     // Chrome helpfully scales the provided icon(s), but let's not go overboard.
     const int kActionIconMaxSize = 10 * extension_misc::EXTENSION_ICON_ACTION;
-    if (bitmap.drawsNothing() || bitmap.width() != bitmap.height() ||
-        bitmap.width() > kActionIconMaxSize) {
+    if (bitmap.drawsNothing() || bitmap.width() > kActionIconMaxSize)
       continue;
-    }
 
     float scale =
         static_cast<float>(bitmap.width()) / ExtensionAction::ActionIconSize();

chrome/test/data/extensions/api_test/browser_action/rect_icon/background.js
@@ -1,0 +1,10 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.sendMessage('ready', function() {
+  chrome.browserAction.setIcon({path: 'rectangle.png'}, function() {
+    chrome.test.assertNoLastError();
+    chrome.test.succeed();
+  });
+});