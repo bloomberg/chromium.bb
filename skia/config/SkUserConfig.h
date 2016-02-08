2724bd48f93462cdb761e4a36b8e2d8c775d5ff7
reed <reed@google.com>
2016-02-08 11:14:57 -0800

    rm SK_SUPPORT_LEGACY_NEG_SCALE_HQ flag
    BUG=
    
    Review URL: https://codereview.chromium.org/1668263002
    
    Cr-Commit-Position: refs/heads/master@{#374151}
    
    Former-commit-id: b85b39a11a6b60cca5aa671bf81433b7b42893a5

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -240,10 +240,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_EVAL_CUBIC
 #endif
 
-#ifndef    SK_SUPPORT_LEGACY_NEG_SCALE_HQ
-#   define SK_SUPPORT_LEGACY_NEG_SCALE_HQ
-#endif
-
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
