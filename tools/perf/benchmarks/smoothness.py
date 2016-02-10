887f480d0ab700fb28888a8daa2b74325361a310
jaydasika <jaydasika@chromium.org>
2016-02-09 23:56:01 -0800

    Re-enable smoothness.key_desktop_move_cases on win.
    The bug for which it was disabled is now fixed.
    
    BUG=582472
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:android_s5_perf_cq;tryserver.chromium.perf:mac_retina_perf_cq;tryserver.chromium.perf:linux_perf_cq
    
    Review URL: https://codereview.chromium.org/1682883002
    
    Cr-Commit-Position: refs/heads/master@{#374623}
    
    Former-commit-id: b3d7475f3fa4f09042d685fb499e02fb02b23956

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -106,8 +106,7 @@ class SmoothnessMaps(perf_benchmark.PerfBenchmark):
 
 
 @benchmark.Disabled('android',
-                    'mac',     # crbug.com/567802
-                    'win')     # crbug.com/582472
+                    'mac')     # crbug.com/567802
 class SmoothnessKeyDesktopMoveCases(_Smoothness):
   page_set = page_sets.KeyDesktopMoveCasesPageSet
 
