c77177dc3f6f25b7f7184e66e9a9298552845697
jbudorick <jbudorick@chromium.org>
2016-02-11 07:58:45 -0800

    [Android] Lazily import lighttpd_server.
    BUG=
    TBR=mikecase@chromium.org
    
    Review URL: https://codereview.chromium.org/1688173002
    
    Cr-Commit-Position: refs/heads/master@{#374903}
    
    Former-commit-id: 5420de8f4c340b187292554a6f75000dd653bdcf

--- build/android/pylib/base/base_test_runner.py
+++ build/android/pylib/base/base_test_runner.py
@@ -15,7 +15,6 @@ from devil.android import forwarder
 from devil.android import ports
 from pylib.valgrind_tools import CreateTool
 # TODO(frankf): Move this to pylib/utils
-import lighttpd_server
 
 
 # A file on device to store ports of net test server. The format of the file is
@@ -86,6 +85,7 @@ class BaseTestRunner(object):
       port: port on which we want to the http server bind.
       extra_config_contents: Extra config contents for the HTTP server.
     """
+    import lighttpd_server
     self._http_server = lighttpd_server.LighttpdServer(
         document_root, port=port, extra_config_contents=extra_config_contents)
     if self._http_server.StartupHttpServer():
