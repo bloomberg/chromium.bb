c11b61484c910605dc811384b035b2580057e27d
dpapad <dpapad@chromium.org>
2016-02-12 12:43:29 -0800

    MD Settings: Manage search engines 2/3, chrome.send wrapper.
    BUG=479359
    
    Review URL: https://codereview.chromium.org/1683023002
    
    Cr-Commit-Position: refs/heads/master@{#375248}
    
    Former-commit-id: b55115192818b27de05cf8c85ae87e14657a5016

---
+++ chrome/browser/resources/settings/search_engines_page/search_engines_browser_proxy.js
@@ -0,0 +1,97 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview A helper object used from the "Manage search engines" section
+ * to interact with the browser.
+ */
+
+/**
+ * @typedef {{canBeDefault: boolean,
+ *            canBeEdited: boolean,
+ *            canBeRemoved: boolean,
+ *            default: boolean,
+ *            displayName: string,
+ *            extension: (Object|undefined),
+ *            iconURL: (string|undefined),
+ *            isOmniboxExtension: boolean,
+ *            keyword: string,
+ *            modelIndex: number,
+ *            name: string,
+ *            url: string,
+ *            urlLocked: boolean}}
+ * @see chrome/browser/ui/webui/settings/search_engine_manager_handler.cc
+ */
+var SearchEngine;
+
+/**
+ * @typedef {{
+ *   defaults: !Array<!SearchEngine>,
+ *   others: !Array<!SearchEngine>,
+ *   extensions: !Array<!SearchEngine>
+ * }}
+ */
+var SearchEnginesInfo;
+
+cr.define('settings', function() {
+  /** @constructor */
+  function SearchEnginesBrowserProxy() {};
+
+  // The singleton instance_ is replaced with a test version of this wrapper
+  // during testing.
+  cr.addSingletonGetter(SearchEnginesBrowserProxy);
+
+  SearchEnginesBrowserProxy.prototype = {
+    /** @param {number} modelIndex */
+    setDefaultSearchEngine: function(modelIndex) {
+      chrome.send('setDefaultSearchEngine', [modelIndex]);
+    },
+
+    /** @param {number} modelIndex */
+    removeSearchEngine: function(modelIndex) {
+      chrome.send('removeSearchEngine', [modelIndex]);
+    },
+
+    /** @param {number} modelIndex */
+    searchEngineEditStarted: function(modelIndex) {
+      chrome.send('searchEngineEditStarted', [modelIndex]);
+    },
+
+    searchEngineEditCancelled: function() {
+      chrome.send('searchEngineEditCancelled');
+    },
+
+    /**
+     * @param {string} searchEngine
+     * @param {string} keyword
+     * @param {string} queryUrl
+     */
+    searchEngineEditCompleted: function(searchEngine, keyword, queryUrl) {
+      chrome.send('searchEngineEditCompleted', [
+        searchEngine, keyword, queryUrl,
+      ]);
+    },
+
+    /**
+     * @return {!Promise<!SearchEnginesInfo>}
+     */
+    getSearchEnginesList: function() {
+      return cr.sendWithPromise('getSearchEnginesList');
+    },
+
+    /**
+     * @param {string} fieldName
+     * @param {string} fieldValue
+     * @return {!Promise<boolean>}
+     */
+    validateSearchEngineInput: function(fieldName, fieldValue) {
+      return cr.sendWithPromise(
+          'validateSearchEngineInput', fieldName, fieldValue);
+    },
+  };
+
+  return {
+    SearchEnginesBrowserProxy: SearchEnginesBrowserProxy,
+  };
+});
