7d81e731efdec21c7ed60a1107fef97b6e2f5097
shans <shans@chromium.org>
2016-02-14 21:22:08 -0800

    Add custom property handling to CSS.supports(prop, value);
    BUG=584683
    
    Review URL: https://codereview.chromium.org/1682373006
    
    Cr-Commit-Position: refs/heads/master@{#375409}
    
    Former-commit-id: 5a38f8ba215c8051af69ba252ce82541b21d64f5

--- third_party/WebKit/Source/core/css/DOMWindowCSS.cpp
+++ third_party/WebKit/Source/core/css/DOMWindowCSS.cpp
@@ -33,6 +33,7 @@
 #include "core/css/CSSPropertyMetadata.h"
 #include "core/css/StylePropertySet.h"
 #include "core/css/parser/CSSParser.h"
+#include "core/css/parser/CSSVariableParser.h"
 #include "wtf/text/StringBuilder.h"
 #include "wtf/text/WTFString.h"
 
@@ -41,8 +42,14 @@ namespace blink {
 bool DOMWindowCSS::supports(const String& property, const String& value)
 {
     CSSPropertyID unresolvedProperty = unresolvedCSSPropertyID(property);
-    if (unresolvedProperty == CSSPropertyInvalid)
+    if (unresolvedProperty == CSSPropertyInvalid) {
+        if (RuntimeEnabledFeatures::cssVariablesEnabled() && CSSVariableParser::isValidVariableName(property)) {
+            RefPtrWillBeRawPtr<MutableStylePropertySet> dummyStyle = MutableStylePropertySet::create(HTMLStandardMode);
+            return CSSParser::parseValueForCustomProperty(dummyStyle.get(), "--valid", value, false, 0);
+        }
         return false;
+    }
+
     ASSERT(CSSPropertyMetadata::isEnabledProperty(unresolvedProperty));
 
     // This will return false when !important is present
