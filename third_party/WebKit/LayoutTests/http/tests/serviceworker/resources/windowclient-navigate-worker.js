16e2be47ee746017017f9fde05950030cc709625
jinho.bang <jinho.bang@samsung.com>
2016-02-16 11:40:05 -0800

    ServiceWorker: Rewrite windowclient-navigate.html test.
    This CL splits the test into each test case to find a failure reason easily.
    Also, adds a test case for in-scope-but-not-controlled to the test.
    The test case comes from the spec dicussion[1] and the comment[2] in other CL.
    
    [1] https://github.com/slightlyoff/ServiceWorker/issues/752
    [2] https://codereview.chromium.org/1481753002/#msg8
    
    BUG=567049
    
    Review URL: https://codereview.chromium.org/1604893002
    
    Cr-Commit-Position: refs/heads/master@{#375629}
    
    Former-commit-id: 47a728652bd829f25edff357b5a640f9c78c328f

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/windowclient-navigate-worker.js
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/windowclient-navigate-worker.js
@@ -1,26 +1,45 @@
-var port = null;
+function match_query(query_string) {
+  return self.location.search.substr(1) == query_string;
+}
 
-self.onmessage = function(e) {
-  port = e.data.port;
-  port.onmessage = on_message;
-  port.postMessage('ready');
-};
-
-function on_message(e) {
-  var url = e.data;
-  var client;
-
-  self.clients.matchAll({ includeUncontrolled : true }).then(function(cs) {
-        cs.forEach(function(c) { c.frameType == 'nested' && (client = c); });
-        return client.navigate(url);
-      })
-    .then(function(c) {
-        if (!c)
-          port.postMessage(c);
-        else
-          port.postMessage(c.url);
-      })
-    .catch(function(e) {
-        port.postMessage(e.name);
+function receive_event(event_name) {
+  return new Promise(function(resolve) {
+        self.addEventListener(event_name, resolve, false);
       });
+}
+
+function navigate_test(e) {
+  var port = e.data.port;
+  var url = e.data.url;
+
+  return clients.matchAll({ includeUncontrolled : true })
+    .then(function(client_list) {
+        for (var i = 0; i < client_list.length; i++) {
+          var client = client_list[i];
+          if (client.frameType == 'nested') {
+            return client.navigate(url);
+          }
+        }
+        port.postMessage('Could not found window client.');
+      })
+    .then(function(new_client) {
+        if (new_client === null)
+          port.postMessage(new_client);
+        else
+          port.postMessage(new_client.url);
+      })
+    .catch(function(error) {
+        port.postMessage(error.name);
+      });
+}
+if (match_query('installing')) {
+  // If the query string is "?installing", then do test on installing worker.
+  // This is only for in-scope-but-not-controlled test.
+  receive_event('install').then(function(e) {
+      e.waitUntil(receive_event('message').then(navigate_test));
+    });
+} else {
+  receive_event('message').then(function(e) {
+      e.waitUntil(navigate_test(e));
+    });
 }
