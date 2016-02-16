2df3869019c6fc6590288b16a9a6e1cea8290722
dcastagna <dcastagna@chromium.org>
2016-02-16 15:52:54 -0800

    Disable CHROMIUM_ycbcr_420v_image for GL Core Profile.
    CHROMIUM_ycbcr_420v_image extension uses a shader on the GPU process
    to perform a YUV to RGB conversion.
    
    The shader GLSL version is 110 and it's not supported in Core profile.
    This patch disables for the extension for Core profile so that we
    can land crrev.com/1686443002 before we address and fix
    crbug.com/587158.
    
    BUG=587158
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1700913003
    
    Cr-Commit-Position: refs/heads/master@{#375710}
    
    Former-commit-id: 028f05d534f35622cd0dd8542c6eb3aedb46d69a

--- gpu/command_buffer/service/feature_info.cc
+++ gpu/command_buffer/service/feature_info.cc
@@ -1005,8 +1005,12 @@ void FeatureInfo::InitializeFeatures() {
 #if defined(OS_MACOSX)
   if (gfx::GetGLImplementation() != gfx::kGLImplementationOSMesaGL) {
     AddExtensionString("GL_CHROMIUM_iosurface");
-    AddExtensionString("GL_CHROMIUM_ycbcr_420v_image");
-    feature_flags_.chromium_image_ycbcr_420v = true;
+    // TODO(dcastagna): Make this format work with GL Core Profile.
+    // crbug.com/587158
+    if (!gl_version_info_->is_desktop_core_profile) {
+      AddExtensionString("GL_CHROMIUM_ycbcr_420v_image");
+      feature_flags_.chromium_image_ycbcr_420v = true;
+    }
   }
 #endif
 
