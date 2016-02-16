66e1614122c22d455cc3a28db08b3afac2b7be29
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2016-02-16 01:57:52 +0000

    configure: Make jpeglib an optional dependency.
    It doesn’t make sense to fail the entire build when jpeglib isn’t
    present, so this commit makes it optional just like libwebp in the
    previous one, disabled with --without-jpeg and forced with --with-jpeg.
    
    Signed-off-by: Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
    Reviewed-by: Bryce Harrington <bryce@osg.samsung.com>

--- shared/image-loader.c
+++ shared/image-loader.c
@@ -30,12 +30,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <jpeglib.h>
 #include <png.h>
 #include <pixman.h>
 
 #include "shared/helpers.h"
 #include "image-loader.h"
+
+#ifdef HAVE_JPEG
+#include <jpeglib.h>
+#endif
 
 #ifdef HAVE_WEBP
 #include <webp/decode.h>
@@ -46,6 +49,14 @@ stride_for_width(int width)
 {
 	return width * 4;
 }
+
+static void
+pixman_image_destroy_func(pixman_image_t *image, void *data)
+{
+	free(data);
+}
+
+#ifdef HAVE_JPEG
 
 static void
 swizzle_row(JSAMPLE *row, JDIMENSION width)
@@ -66,12 +77,6 @@ static void
 error_exit(j_common_ptr cinfo)
 {
 	longjmp(cinfo->client_data, 1);
-}
-
-static void
-pixman_image_destroy_func(pixman_image_t *image, void *data)
-{
-	free(data);
 }
 
 static pixman_image_t *
@@ -131,6 +136,17 @@ load_jpeg(FILE *fp)
 
 	return pixman_image;
 }
+
+#else
+
+static pixman_image_t *
+load_jpeg(FILE *fp)
+{
+	fprintf(stderr, "JPEG support disabled at compile-time\n");
+	return NULL;
+}
+
+#endif
 
 static inline int
 multiply_alpha(int alpha, int color)
