028cd5719f12d4dcfb90f67e6b04b6b6207ca01a
dbeam <dbeam@chromium.org>
2016-02-17 14:40:53 -0800

    Add license to third_party/polymer/v1_0/generate_gyp.sh
    TBR=sky@chromium.org
    NOTRY=true
    TEST=green tree
    BUG=none
    
    Review URL: https://codereview.chromium.org/1703153003
    
    Cr-Commit-Position: refs/heads/master@{#376008}
    
    Former-commit-id: f62a67a362ee758e32311ef2680c5e1d71a24ce3

--- third_party/polymer/v1_0/generate_gyp.sh
+++ third_party/polymer/v1_0/generate_gyp.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 
 dirs=$(find components-chromium -type d | grep -v 'components-chromium/polymer')
 
