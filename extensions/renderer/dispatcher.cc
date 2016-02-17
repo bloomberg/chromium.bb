933d67f737db1c11d2ae8e0765c103e28babfa3d
jochen <jochen@chromium.org>
2016-02-17 13:22:17 -0800

    Invalidate all extensions contexts on shutdown
    BUG=584716
    R=rdevlin.cronin@chromium.org,yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/1702863004
    
    Cr-Commit-Position: refs/heads/master@{#375983}
    
    Former-commit-id: 5b04bec4148bfdb8b70aa1d227be91b67e6f368a

--- extensions/renderer/dispatcher.cc
+++ extensions/renderer/dispatcher.cc
@@ -953,6 +953,10 @@ void Dispatcher::OnRenderProcessShutdown() {
   v8_schema_registry_.reset();
   forced_idle_timer_.reset();
   content_watcher_.reset();
+  script_context_set_->ForEach(
+      std::string(), nullptr,
+      base::Bind(&ScriptContextSet::Remove,
+                 base::Unretained(script_context_set_.get())));
 }
 
 void Dispatcher::OnActivateExtension(const std::string& extension_id) {
