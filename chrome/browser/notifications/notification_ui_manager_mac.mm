d38f7cbe4faf1b8745de2a06e2c5e871708d4ce9
miguelg <miguelg@chromium.org>
2016-02-18 02:46:58 -0800

    Implement notification buttons for Mac.
    BUG=571056
    
    Review URL: https://codereview.chromium.org/1696683002
    
    Cr-Commit-Position: refs/heads/master@{#376137}
    
    Former-commit-id: 10e4640fe2e4125eb603153697a99999d04d0761

--- chrome/browser/notifications/notification_ui_manager_mac.mm
+++ chrome/browser/notifications/notification_ui_manager_mac.mm
@@ -32,12 +32,13 @@
 // notification#tag in NSUserNotification.identifier (10.9)
 // notification#icon in NSUserNotification.contentImage (10.9)
 // Site settings button is implemented as NSUserNotification's action button
-// notification.requireInteraction cannot be implemented
+// Not possible to implement:
+// -notification.requireInteraction
+// -The event associated to the close button
 
 // TODO(miguelg) implement the following features
 // - Sound names can be implemented by setting soundName in NSUserNotification
 //   NSUserNotificationDefaultSoundName gives you the platform default.
-// - Actions are only possible in 10.10.
 
 namespace {
 
@@ -125,13 +126,40 @@ void NotificationUIManagerMac::Add(const Notification& notification,
   // Buttons
   if ([toast respondsToSelector:@selector(_showsButtons)]) {
     [toast setValue:@YES forKey:@"_showsButtons"];
-    [toast setActionButtonTitle:l10n_util::GetNSString(
-                                    IDS_NOTIFICATION_BUTTON_SETTINGS)];
     // A default close button label is provided by the platform but we
     // explicitly override it in case the user decides to not
     // use the OS language in Chrome.
     [toast setOtherButtonTitle:l10n_util::GetNSString(
                                    IDS_NOTIFICATION_BUTTON_CLOSE)];
+
+    // Display the Settings button as the action button if there either are no
+    // developer-provided action buttons, or the alternate action menu is not
+    // available on this Mac version. This avoids needlessly showing the menu.
+    if (notification.buttons().empty() ||
+        ![toast respondsToSelector:@selector(_alwaysShowAlternateActionMenu)]) {
+      [toast setActionButtonTitle:l10n_util::GetNSString(
+                                      IDS_NOTIFICATION_BUTTON_SETTINGS)];
+    } else {
+      // Otherwise show the alternate menu, then show the developer actions and
+      // finally the settings one.
+      DCHECK(
+          [toast respondsToSelector:@selector(_alwaysShowAlternateActionMenu)]);
+      DCHECK(
+          [toast respondsToSelector:@selector(_alternateActionButtonTitles)]);
+
+      // utf8 ellipsis:
+      [toast setActionButtonTitle:@"\u2026"];
+      [toast setValue:@YES
+               forKey:@"_alwaysShowAlternateActionMenu"];
+
+      NSMutableArray* buttons = [NSMutableArray arrayWithCapacity:3];
+      for (const auto& action : notification.buttons())
+        [buttons addObject:base::SysUTF16ToNSString(action.title)];
+      [buttons
+          addObject:l10n_util::GetNSString(IDS_NOTIFICATION_BUTTON_SETTINGS)];
+
+      [toast setValue:buttons forKey:@"_alternateActionButtonTitles"];
+    }
   }
 
   // Tag
@@ -268,18 +296,51 @@ void NotificationUIManagerMac::CancelAll() {
 
   GURL origin(notificationOrigin);
 
+  // Initialize operation and button index for the case where the
+  // notification itself was clicked.
   PlatformNotificationServiceImpl::NotificationOperation operation =
-      notification.activationType ==
-              NSUserNotificationActivationTypeActionButtonClicked
-          ? PlatformNotificationServiceImpl::NOTIFICATION_SETTINGS
-          : PlatformNotificationServiceImpl::NOTIFICATION_CLICK;
+      PlatformNotificationServiceImpl::NOTIFICATION_CLICK;
+  int buttonIndex = -1;
+
+  // Determine whether the user clicked on a button, and if they did, whether it
+  // was a developer-provided button or the mandatory Settings button.
+  if (notification.activationType ==
+      NSUserNotificationActivationTypeActionButtonClicked) {
+    NSArray* alternateButtons = @[];
+    if ([notification
+            respondsToSelector:@selector(_alternateActionButtonTitles)]) {
+      alternateButtons =
+          [notification valueForKey:@"_alternateActionButtonTitles"];
+    }
+
+    bool multipleButtons = (alternateButtons.count > 0);
+
+    // No developer actions, just the settings button.
+    if (!multipleButtons) {
+      operation = PlatformNotificationServiceImpl::NOTIFICATION_SETTINGS;
+      buttonIndex = -1;
+    } else {
+      // 0 based array containing.
+      // Button 1
+      // Button 2 (optional)
+      // Settings
+      NSNumber* actionIndex =
+          [notification valueForKey:@"_alternateActionIndex"];
+      operation = (actionIndex.unsignedLongValue == alternateButtons.count - 1)
+                      ? PlatformNotificationServiceImpl::NOTIFICATION_SETTINGS
+                      : PlatformNotificationServiceImpl::NOTIFICATION_CLICK;
+      buttonIndex =
+          (actionIndex.unsignedLongValue == alternateButtons.count - 1)
+              ? -1
+              : actionIndex.intValue;
+    }
+  }
 
   PlatformNotificationServiceImpl::GetInstance()
       ->ProcessPersistentNotificationOperation(
           operation, base::SysNSStringToUTF8(persistentProfileId),
           [isIncognito boolValue], origin,
-          persistentNotificationId.longLongValue,
-          -1 /* buttons not yet implemented */);
+          persistentNotificationId.longLongValue, buttonIndex);
 }
 
 - (BOOL)userNotificationCenter:(NSUserNotificationCenter*)center
