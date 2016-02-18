73bae509019c33c4fc329a15fb5c2aea31dc1c9b
Thomas Davies <thdavies@cisco.com>
2016-02-18 18:03:30 +0000

    Add quantisation matrices and selection functions.
    Change-Id: Ic7a57957ad47e78300e73b85fa26e56b594a3a90

--- vp10/common/quant_common.h
+++ vp10/common/quant_common.h
@@ -13,6 +13,7 @@
 
 #include "vpx/vpx_codec.h"
 #include "vp10/common/seg_common.h"
+#include "vp10/common/enums.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -22,12 +23,38 @@ extern "C" {
 #define MAXQ 255
 #define QINDEX_RANGE (MAXQ - MINQ + 1)
 #define QINDEX_BITS 8
+#if CONFIG_AOM_QM
+// Total number of QM sets stored
+#define NUM_QM_LEVELS 16
+/* Offset into the list of QMs. Actual number of levels used is
+   (NUM_QM_LEVELS-AOM_QM_OFFSET)
+   Lower value of AOM_QM_OFFSET implies more heavily weighted matrices.*/
+#define AOM_QM_FIRST 8
+#define AOM_QM_LAST NUM_QM_LEVELS
+#endif
+
+struct VP10Common;
 
 int16_t vp10_dc_quant(int qindex, int delta, vpx_bit_depth_t bit_depth);
 int16_t vp10_ac_quant(int qindex, int delta, vpx_bit_depth_t bit_depth);
 
 int vp10_get_qindex(const struct segmentation *seg, int segment_id,
                     int base_qindex);
+#if CONFIG_AOM_QM
+// Reduce the large number of quantizers to a smaller number of levels for which
+// different matrices may be defined
+static inline int aom_get_qmlevel(int qindex) {
+  int qmlevel =
+      (qindex * (AOM_QM_LAST - AOM_QM_FIRST) + QINDEX_RANGE / 2) / QINDEX_RANGE;
+  qmlevel = VPXMIN(qmlevel + AOM_QM_FIRST, NUM_QM_LEVELS - 1);
+  return qmlevel;
+}
+void aom_qm_init(struct VP10Common *cm);
+qm_val_t *aom_iqmatrix(struct VP10Common *cm, int qindex, int comp,
+                       int log2sizem2, int is_intra);
+qm_val_t *aom_qmatrix(struct VP10Common *cm, int qindex, int comp,
+                      int log2sizem2, int is_intra);
+#endif
 
 #ifdef __cplusplus
 }  // extern "C"
