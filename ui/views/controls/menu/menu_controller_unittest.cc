6853ebfbd4df7c9207f9de8b82db24ae3cc6e2b8
jonross <jonross@chromium.org>
2016-02-19 08:19:55 -0800

    Disable MenuControllerTest failing on Valgrind
    A test added in https://codereview.chromium.org/1696393003/ has a leak on Chromium OS that Valgrind picked up. Disabling on Chromium OS as I investigate.
    
    TBR=sadrul@chromium.org
    TEST=MenuControllerTest.AsynchronousRepostEventDeletesController
    BUG=586624
    
    Review URL: https://codereview.chromium.org/1713703003
    
    Cr-Commit-Position: refs/heads/master@{#376452}
    
    Former-commit-id: aa83ef86ff3c21a5d94519b616764aaacbb755d4

--- ui/views/controls/menu/menu_controller_unittest.cc
+++ ui/views/controls/menu/menu_controller_unittest.cc
@@ -953,9 +953,16 @@ TEST_F(MenuControllerTest, AsynchronousNestedExitOutermost) {
   RunMenu();
 }
 
+#if defined(OS_CHROMEOS)
+#define MAYBE_AsynchronousRepostEventDeletesController \
+  DISABLED_AsynchronousRepostEventDeletesController
+#else
+#define MAYBE_AsynchronousRepostEventDeletesController \
+  AsynchronousRepostEventDeletesController
+#endif
 // Tests that having the MenuController deleted during RepostEvent does not
 // cause a crash. ASAN bots should not detect use-after-free in MenuController.
-TEST_F(MenuControllerTest, AsynchronousRepostEventDeletesController) {
+TEST_F(MenuControllerTest, MAYBE_AsynchronousRepostEventDeletesController) {
   MenuController* controller = menu_controller();
   scoped_ptr<TestMenuControllerDelegate> nested_delegate(
       new TestMenuControllerDelegate());
