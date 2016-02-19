9db998665b8ba97084c3b90f232a529d261321e6
eae <eae@chromium.org>
2016-02-18 21:31:01 -0800

    Fix getBoundingClientRect for collapsed ranges
    Change the Range::boundingRect implementation to correctly handle ranges
    that have been collapsed by using the uniteIfNonZero method which unlike
    the default unite method handles empty, zero-width or zero-height rects.
    
    TEST=fast/dom/Range/collapsed-range-bounding-client-rect.html
    BUG=574363
    R=szager@chromium.org
    
    Review URL: https://codereview.chromium.org/1661213002
    
    Cr-Commit-Position: refs/heads/master@{#376398}
    
    Former-commit-id: 96162cc18afb00bc4430c4de5a5ba56a79a0aab3

--- third_party/WebKit/Source/core/dom/Range.cpp
+++ third_party/WebKit/Source/core/dom/Range.cpp
@@ -1635,8 +1635,16 @@ FloatRect Range::boundingRect() const
     getBorderAndTextQuads(quads);
 
     FloatRect result;
-    for (const FloatQuad& quad : quads)
-        result.unite(quad.boundingBox());
+    // As per section 10 in https://www.w3.org/TR/cssom-view/
+    // "Return a static DOMRect object describing the smallest rectangle that
+    // includes the first rectangle in list and all of the remaining rectangles
+    // of which the height or width is not zero."
+    for (const FloatQuad& quad : quads) {
+        if (result.isEmpty())
+            result.uniteIfNonZero(quad.boundingBox());
+        else
+            result.unite(quad.boundingBox()); // Skips empty rects.
+    }
 
     return result;
 }
