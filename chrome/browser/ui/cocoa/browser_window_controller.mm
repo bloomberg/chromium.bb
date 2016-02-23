ba4e7a33f9bba7f56ba1cf39f06827abd0626642
spqchan <spqchan@chromium.org>
2016-02-23 15:22:12 -0800

    Revert to Immersive Fullscreen on OSX for a special case
    If the user is using multiple monitors and turned off "Separate Space
    in Each Display", use Immersive Fullscreen so that the other monitors
    won't blank out.
    
    BUG=582944,580843
    
    Review URL: https://codereview.chromium.org/1725643002
    
    Cr-Commit-Position: refs/heads/master@{#377116}
    
    Former-commit-id: d310f829e7f38f725a5b695ea701a3ffa3b3cf89

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -101,6 +101,7 @@
 #include "ui/base/l10n/l10n_util_mac.h"
 #import "ui/gfx/mac/coordinate_conversion.h"
 #include "ui/gfx/mac/scoped_cocoa_disable_screen_updates.h"
+#include "ui/gfx/screen.h"
 
 using bookmarks::BookmarkModel;
 using bookmarks::BookmarkNode;
@@ -2017,11 +2018,18 @@ willAnimateFromState:(BookmarkBar::State)oldState
 
 - (void)enterWebContentFullscreen {
   // HTML5 Fullscreen should only use AppKit fullscreen in 10.10+.
+  // However, if the user is using multiple monitors and turned off
+  // "Separate Space in Each Display", use Immersive Fullscreen so
+  // that the other monitors won't blank out.
+  gfx::Screen* screen = gfx::Screen::GetScreen();
+  BOOL hasMultipleMonitors = screen && screen->GetNumDisplays() > 1;
   if (chrome::mac::SupportsSystemFullscreen() &&
-      base::mac::IsOSYosemiteOrLater())
+      base::mac::IsOSYosemiteOrLater() &&
+      !(hasMultipleMonitors && ![NSScreen screensHaveSeparateSpaces])) {
     [self enterAppKitFullscreen];
-  else
+  } else {
     [self enterImmersiveFullscreen];
+  }
 
   if (!exclusiveAccessController_->url().is_empty())
     [self updateFullscreenExitBubble];
