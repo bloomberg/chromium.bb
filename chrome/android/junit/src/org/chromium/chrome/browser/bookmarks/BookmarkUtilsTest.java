5693fe8223548a41b59922ddc30d44d76a4ae0ef
bburns <bburns@chromium.org>
2016-02-23 10:20:02 -0800

    Add some initial unit tests for saveOfflinePages.
    fgorski, ianwen: code review
    bauerb: owners
    
    add a return that was missing.
    
    BUG=NONE
    
    Review URL: https://codereview.chromium.org/1684373003
    
    Cr-Commit-Position: refs/heads/master@{#377018}
    
    Former-commit-id: c5eca3a4ecd742f80d32d0aba6f93463df85ca3c

--- chrome/android/junit/src/org/chromium/chrome/browser/bookmarks/BookmarkUtilsTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/bookmarks/BookmarkUtilsTest.java
@@ -6,29 +6,43 @@ package org.chromium.chrome.browser.bookmarks;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
+
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
+import org.chromium.base.ApplicationStatus;
+import org.chromium.base.BaseChromiumApplication;
 import org.chromium.base.test.util.Feature;
+import org.chromium.chrome.browser.snackbar.SnackbarManager;
+import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.components.bookmarks.BookmarkId;
 import org.chromium.components.bookmarks.BookmarkType;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.testing.local.LocalRobolectricTestRunner;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.robolectric.Robolectric;
 import org.robolectric.annotation.Config;
 
 /**
  * Robolectric tests for {@link BookmarkUtils}.
  */
 @RunWith(LocalRobolectricTestRunner.class)
-@Config(manifest = Config.NONE)
+@Config(manifest = Config.NONE, application = BaseChromiumApplication.class)
 public class BookmarkUtilsTest {
     @Mock private Context mContext;
     @Mock private BookmarkModel mBookmarkModel;
@@ -65,5 +79,66 @@ public class BookmarkUtilsTest {
         // Verify that the intent contains the right WEB_CONTENTS extra.
         assertEquals(mWebContents, intentArgumentCaptor.getValue()
                 .getParcelableExtra(BookmarkEditActivity.INTENT_WEB_CONTENTS));
+    }
+
+    @Test
+    @Feature({"Bookmark"})
+    public void testSaveBookmarkOfflineNoBookmark() {
+        ApplicationStatus.initialize((BaseChromiumApplication) Robolectric.application);
+        Tab tab = new Tab(0, true, null);
+        BookmarkModel model = mock(BookmarkModel.class);
+        SnackbarManager snackbarManager = mock(SnackbarManager.class);
+        Activity activity = mock(Activity.class);
+
+        when(model.doesBookmarkExist(new BookmarkId(12345L, BookmarkType.NORMAL)))
+                .thenReturn(false);
+        BookmarkUtils.saveBookmarkOffline(12345L, model, tab, snackbarManager, activity);
+        verify(model).doesBookmarkExist(new BookmarkId(12345L, BookmarkType.NORMAL));
+
+        verifyNoMoreInteractions(model);
+        verifyNoMoreInteractions(snackbarManager);
+        verifyNoMoreInteractions(activity);
+    }
+
+    @Test
+    @Feature({"Bookmark"})
+    public void testSaveBookmarkOfflineSadTab() {
+        ApplicationStatus.initialize((BaseChromiumApplication) Robolectric.application);
+        Tab tab = new Tab(0, true, null);
+        BookmarkModel model = mock(BookmarkModel.class);
+        SnackbarManager snackbarManager = mock(SnackbarManager.class);
+        Activity activity = mock(Activity.class);
+
+        tab.setIsShowingErrorPage(true);
+
+        when(model.doesBookmarkExist(new BookmarkId(12345L, BookmarkType.NORMAL))).thenReturn(true);
+        BookmarkUtils.saveBookmarkOffline(12345L, model, tab, snackbarManager, activity);
+        verify(model).doesBookmarkExist(new BookmarkId(12345L, BookmarkType.NORMAL));
+
+        verifyNoMoreInteractions(model);
+        verifyNoMoreInteractions(snackbarManager);
+        verifyNoMoreInteractions(activity);
+    }
+
+    @Test
+    @Feature({"Bookmark"})
+    public void testSaveBookmarkOffline() {
+        ApplicationStatus.initialize((BaseChromiumApplication) Robolectric.application);
+        Tab tab = new Tab(0, true, null);
+        BookmarkModel model = mock(BookmarkModel.class);
+        SnackbarManager snackbarManager = mock(SnackbarManager.class);
+        Activity activity = mock(Activity.class);
+        BookmarkId id = new BookmarkId(12345L, BookmarkType.NORMAL);
+        when(model.doesBookmarkExist(id)).thenReturn(true);
+        doNothing().when(model).saveOfflinePage(eq(id), any(WebContents.class),
+                any(BookmarkModel.AddBookmarkCallback.class));
+        BookmarkUtils.saveBookmarkOffline(12345L, model, tab, snackbarManager, activity);
+        verify(model).doesBookmarkExist(id);
+        verify(model).saveOfflinePage(eq(id), any(WebContents.class),
+                any(BookmarkModel.AddBookmarkCallback.class));
+
+        verifyNoMoreInteractions(model);
+        verifyNoMoreInteractions(snackbarManager);
+        verifyNoMoreInteractions(activity);
     }
 }
