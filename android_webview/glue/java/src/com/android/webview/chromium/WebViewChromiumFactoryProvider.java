0d819b54e69d45fb7ee040022ed0e6751118367e
torne <torne@chromium.org>
2016-02-23 12:51:24 -0800

    Remove obsolete command line flag.
    enable-dcheck hasn't done anything for a long time, we no longer support
    toggling DCHECK at runtime and they are entirely compiled out of release
    builds unless dcheck_always_on=1 is set (at which point they crash by
    default without needing to be runtime-enabled).
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1729573002
    
    Cr-Commit-Position: refs/heads/master@{#377079}
    
    Former-commit-id: 9aab13ccc6998c63acef84a523c29b40f38ef343

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -121,13 +121,6 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
             CommandLine.init(null);
         }
 
-        CommandLine cl = CommandLine.getInstance();
-        // TODO: currently in a relase build the DCHECKs only log. We either need to insall
-        // a report handler with SetLogReportHandler to make them assert, or else compile
-        // them out of the build altogether (b/8284203). Either way, so long they're
-        // compiled in, we may as unconditionally enable them here.
-        cl.appendSwitch("enable-dcheck");
-
         ThreadUtils.setWillOverrideUiThread();
         // Load chromium library.
         AwBrowserProcess.loadLibrary(getWrappedCurrentApplicationContext());
