fc5186fbeb19f90276e594128fe9ec827ae6f50c
melandory <melandory@chromium.org>
2016-02-23 10:53:33 -0800

    [Smart Lock, UI] Fix title for federated credentials.
    BUG=542138
    
    Review URL: https://codereview.chromium.org/1717263002
    
    Cr-Commit-Position: refs/heads/master@{#377029}
    
    Former-commit-id: 9eb8628f36b65e040270529120d84111ff450fe1

--- chrome/browser/password_manager/save_password_infobar_delegate.cc
+++ chrome/browser/password_manager/save_password_infobar_delegate.cc
@@ -96,7 +96,7 @@ SavePasswordInfoBarDelegate::SavePasswordInfoBarDelegate(
   PasswordTittleType type =
       form_to_save_->pending_credentials().federation_url.is_empty()
       ? PasswordTittleType::SAVE_PASSWORD
-      : PasswordTittleType::UPDATE_PASSWORD;
+      : PasswordTittleType::SAVE_ACCOUNT;
   GetSavePasswordDialogTitleTextAndLinkRange(
       web_contents->GetVisibleURL(), form_to_save_->observed_form().origin,
       is_smartlock_branding_enabled, type,
