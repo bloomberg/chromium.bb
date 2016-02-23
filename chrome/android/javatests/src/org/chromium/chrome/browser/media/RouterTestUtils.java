6fd18e6d2457d4cdff6838b85df57a30d316d94b
zqzhang <zqzhang@chromium.org>
2016-02-23 03:56:24 -0800

    Separating shared test utils from media remote tests
    This patch separates the test utils from media remote
    tests (CastTestBase), which can be shared with media router tests.
    
    BUG=587870
    
    Review URL: https://codereview.chromium.org/1724433002
    
    Cr-Commit-Position: refs/heads/master@{#376969}
    
    Former-commit-id: 757d5ffd1b481d5407b5466e8a6efd287ebe1e86

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/RouterTestUtils.java
@@ -0,0 +1,160 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.media;
+
+import android.app.Instrumentation;
+import android.os.SystemClock;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentManager;
+import android.view.MotionEvent;
+import android.view.View;
+
+import org.chromium.base.ThreadUtils;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.content.browser.test.util.Criteria;
+import org.chromium.content.browser.test.util.CriteriaHelper;
+import org.chromium.content.browser.test.util.TestTouchUtils;
+
+import java.util.ArrayList;
+import java.util.concurrent.Callable;
+
+/**
+ * Test utils shared by MediaRouter and MediaRemote.
+ */
+public class RouterTestUtils {
+    public static View waitForRouteButton(
+            final ChromeActivity activity, final String chromecastName,
+            int maxTimeoutMs, int intervalMs) {
+        return waitForView(new Callable<View>() {
+            @Override
+            public View call() {
+                DialogFragment mediaRouteListFragment = getDialogFragment(activity);
+                if (mediaRouteListFragment == null || mediaRouteListFragment.getDialog() == null) {
+                    return null;
+                }
+                View mediaRouteList =
+                        mediaRouteListFragment.getDialog().findViewById(R.id.mr_chooser_list);
+                if (mediaRouteList == null) return null;
+                ArrayList<View> routesWanted = new ArrayList<View>();
+                mediaRouteList.findViewsWithText(routesWanted, chromecastName,
+                        View.FIND_VIEWS_WITH_TEXT);
+                if (routesWanted.size() == 0) return null;
+
+                return routesWanted.get(0);
+            }
+            }, maxTimeoutMs, intervalMs);
+    }
+
+    public static DialogFragment getDialogFragment(ChromeActivity activity) {
+        FragmentManager fm = activity.getSupportFragmentManager();
+        if (fm == null) return null;
+        return (DialogFragment) fm.findFragmentByTag(
+            "android.support.v7.mediarouter:MediaRouteChooserDialogFragment");
+    }
+
+    public static View waitForView(
+            final Callable<View> getViewCallable, int maxTimeoutMs, int intervalMs) {
+        try {
+            CriteriaHelper.pollForUIThreadCriteria(new Criteria() {
+                    @Override
+                    public boolean isSatisfied() {
+                        try {
+                            return getViewCallable.call() != null;
+                        } catch (Exception e) {
+                            return false;
+                        }
+                    }
+                }, maxTimeoutMs, intervalMs);
+            return getViewCallable.call();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * Click a button. Unlike {@link RouterTestUtils#mouseSingleClickView} this directly accesses
+     * the view and does not send motion events though the message queue. As such it doesn't require
+     * the view to have been created by the instrumented activity, but gives less flexibility than
+     * mouseSingleClickView. For example, if the view is hierachical, then clickButton will always
+     * act on specified view, whereas mouseSingleClickView will send the events to the appropriate
+     * child view. It is hence only really appropriate for simple views such as buttons.
+     *
+     * @param button the button to be clicked.
+     */
+    public static void clickButton(final View button) {
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+                @Override
+            public void run() {
+                // Post the actual click to the button's message queue, to ensure that it has been
+                // inflated before the click is received.
+                button.post(new Runnable() {
+                        @Override
+                    public void run() {
+                        button.performClick();
+                    }
+                });
+            }
+        });
+    }
+
+    private static void sendMouseAction(Instrumentation instrumentation, int action, long downTime,
+            float x, float y) {
+        long eventTime = SystemClock.uptimeMillis();
+        MotionEvent.PointerCoords coords[] = new MotionEvent.PointerCoords[1];
+        coords[0] = new MotionEvent.PointerCoords();
+        coords[0].x = x;
+        coords[0].y = y;
+        MotionEvent.PointerProperties properties[] = new MotionEvent.PointerProperties[1];
+        properties[0] = new MotionEvent.PointerProperties();
+        properties[0].id = 0;
+        properties[0].toolType = MotionEvent.TOOL_TYPE_MOUSE;
+        MotionEvent event = MotionEvent.obtain(downTime, eventTime, action, 1, properties, coords,
+                0, 0, 0.0f, 0.0f, 0, 0, 0, 0);
+        instrumentation.sendPointerSync(event);
+        instrumentation.waitForIdleSync();
+    }
+
+    /**
+     * Sends (synchronously) a single mosue click to an absolute screen coordinates.
+     *
+     * @param instrumentation Instrumentation object used by the test.
+     * @param x Screen absolute x location.
+     * @param y Screen absolute y location.
+     */
+    private static void mouseSingleClick(Instrumentation instrumentation, float x, float y) {
+        long downTime = SystemClock.uptimeMillis();
+        sendMouseAction(instrumentation, MotionEvent.ACTION_DOWN, downTime, x, y);
+        sendMouseAction(instrumentation, MotionEvent.ACTION_UP, downTime, x, y);
+    }
+
+    /**
+     * Sends (synchronously) a single mouse click to the View at the specified coordinates.
+     *
+     * @param instrumentation Instrumentation object used by the test.
+     * @param v The view the coordinates are relative to.
+     * @param x Relative x location to the view.
+     * @param y Relative y location to the view.
+     */
+    public static void mouseSingleClickView(Instrumentation instrumentation, View v, int x,
+            int y) {
+        int location[] = TestTouchUtils.getAbsoluteLocationFromRelative(v, x, y);
+        int absoluteX = location[0];
+        int absoluteY = location[1];
+        mouseSingleClick(instrumentation, absoluteX, absoluteY);
+    }
+
+    /**
+     * Sends (synchronously) a single mouse click to the center of the View.
+     *
+     * @param instrumentation Instrumentation object used by the test.
+     * @param v The view the coordinates are relative to.
+     */
+    public static void mouseSingleClickView(Instrumentation instrumentation, View v) {
+        int x = v.getWidth() / 2;
+        int y = v.getHeight() / 2;
+        mouseSingleClickView(instrumentation, v, x, y);
+    }
+}
--- chrome/android/javatests/src/org/chromium/chrome/browser/media/remote/CastTestBase.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/media/remote/CastTestBase.java
@@ -5,34 +5,29 @@
 package org.chromium.chrome.browser.media.remote;
 
 import android.app.Dialog;
-import android.app.Instrumentation;
 import android.graphics.Rect;
 import android.os.Environment;
 import android.os.StrictMode;
-import android.os.SystemClock;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
-import android.view.MotionEvent;
 import android.view.View;
 
 import junit.framework.Assert;
 
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
-import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeActivity;
+import org.chromium.chrome.browser.media.RouterTestUtils;
 import org.chromium.chrome.browser.media.remote.RemoteVideoInfo.PlayerState;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.test.ChromeActivityTestCaseBase;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.browser.test.util.DOMUtils;
 import org.chromium.content.browser.test.util.JavaScriptUtils;
-import org.chromium.content.browser.test.util.TestTouchUtils;
 import org.chromium.content.browser.test.util.UiUtils;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.net.test.EmbeddedTestServer;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.Callable;
@@ -315,34 +310,11 @@ public abstract class CastTestBase extends ChromeActivityTestCaseBase<ChromeActi
             fail();
         }
 
-        View testRouteButton = waitForRouteButton(chromecastName);
+        View testRouteButton = RouterTestUtils.waitForRouteButton(
+                getActivity(), chromecastName, MAX_VIEW_TIME_MS, VIEW_RETRY_MS);
         assertNotNull("Test route not found", testRouteButton);
 
-        mouseSingleClickView(getInstrumentation(), testRouteButton);
-    }
-
-    protected View waitForRouteButton(final String chromecastName) {
-        return waitForView(new Callable<View>() {
-            @Override
-            public View call() {
-                FragmentManager fm = getActivity().getSupportFragmentManager();
-                if (fm == null) return null;
-                DialogFragment mediaRouteListFragment = (DialogFragment) fm.findFragmentByTag(
-                        "android.support.v7.mediarouter:MediaRouteChooserDialogFragment");
-                if (mediaRouteListFragment == null || mediaRouteListFragment.getDialog() == null) {
-                    return null;
-                }
-                View mediaRouteList =
-                        mediaRouteListFragment.getDialog().findViewById(R.id.mr_chooser_list);
-                if (mediaRouteList == null) return null;
-                ArrayList<View> routesWanted = new ArrayList<View>();
-                mediaRouteList.findViewsWithText(routesWanted, chromecastName,
-                        View.FIND_VIEWS_WITH_TEXT);
-                if (routesWanted.size() == 0) return null;
-
-                return routesWanted.get(0);
-            }
-        }, MAX_VIEW_TIME_MS);
+        RouterTestUtils.mouseSingleClickView(getInstrumentation(), testRouteButton);
     }
 
     protected void checkDisconnected() {
@@ -366,7 +338,7 @@ public abstract class CastTestBase extends ChromeActivityTestCaseBase<ChromeActi
         tapCastButton(tab, videoRect);
 
         // Wait for the disconnect button
-        final View disconnectButton = waitForView(new Callable<View>() {
+        final View disconnectButton = RouterTestUtils.waitForView(new Callable<View>() {
             @Override
             public View call() {
                 FragmentManager fm = getActivity().getSupportFragmentManager();
@@ -383,11 +355,11 @@ public abstract class CastTestBase extends ChromeActivityTestCaseBase<ChromeActi
                 //               https://crbug/548599
                 return dialog.findViewById(android.R.id.button1);
             }
-        }, MAX_VIEW_TIME_MS);
+        }, MAX_VIEW_TIME_MS, VIEW_RETRY_MS);
 
         assertNotNull("No disconnect button", disconnectButton);
 
-        clickButton(disconnectButton);
+        RouterTestUtils.clickButton(disconnectButton);
     }
 
     /*
@@ -414,32 +386,6 @@ public abstract class CastTestBase extends ChromeActivityTestCaseBase<ChromeActi
                 .getCurrentlyPlayingMediaRouteController() instanceof DefaultMediaRouteController));
     }
 
-    /**
-     * Click a button. Unlike {@link CastTestBase#mouseSingleClickView} this directly accesses the
-     * view and does not send motion events though the message queue. As such it doesn't require the
-     * view to have been created by the instrumented activity, but gives less flexibility than
-     * mouseSingleClickView. For example, if the view is hierachical, then clickButton will always
-     * act on specified view, whereas mouseSingleClickView will send the events to the appropriate
-     * child view. It is hence only really appropriate for simple views such as buttons.
-     *
-     * @param button the button to be clicked.
-     */
-    protected void clickButton(final View button) {
-        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
-                @Override
-            public void run() {
-                // Post the actual click to the button's message queue, to ensure that it has been
-                // inflated before the click is received.
-                button.post(new Runnable() {
-                        @Override
-                    public void run() {
-                        button.performClick();
-                    }
-                });
-            }
-        });
-    }
-
     protected void sleepNoThrow(long timeout) {
         try {
             Thread.sleep(timeout);
@@ -458,19 +404,6 @@ public abstract class CastTestBase extends ChromeActivityTestCaseBase<ChromeActi
 
     protected void tapPlayPauseButton(final Tab tab, final Rect videoRect) {
         tapButton(tab, playPauseButton(videoRect));
-    }
-
-    protected View waitForView(Callable<View> getViewCallable, int timeoutMs) {
-        for (int time = 0; time < timeoutMs; time += VIEW_RETRY_MS) {
-            try {
-                View result = ThreadUtils.runOnUiThreadBlocking(getViewCallable);
-                if (result != null) return result;
-            } catch (Exception e) {
-                fail(e.toString());
-            }
-            sleepNoThrow(VIEW_RETRY_MS);
-        }
-        return null;
     }
 
     protected CastNotificationControl waitForCastNotification() {
@@ -627,65 +560,8 @@ public abstract class CastTestBase extends ChromeActivityTestCaseBase<ChromeActi
                         ((float) (rect.top + rect.bottom)) / 2)
                 + core.getTopControlsHeightPix();
         // Click using a virtual mouse, since a touch may result in a disambiguation pop-up.
-        mouseSingleClickView(getInstrumentation(), tab.getView(), clickX, clickY);
+        RouterTestUtils.mouseSingleClickView(getInstrumentation(), tab.getView(), clickX, clickY);
     }
 
-    private static void sendMouseAction(Instrumentation instrumentation, int action, long downTime,
-            float x, float y) {
-        long eventTime = SystemClock.uptimeMillis();
-        MotionEvent.PointerCoords coords[] = new MotionEvent.PointerCoords[1];
-        coords[0] = new MotionEvent.PointerCoords();
-        coords[0].x = x;
-        coords[0].y = y;
-        MotionEvent.PointerProperties properties[] = new MotionEvent.PointerProperties[1];
-        properties[0] = new MotionEvent.PointerProperties();
-        properties[0].id = 0;
-        properties[0].toolType = MotionEvent.TOOL_TYPE_MOUSE;
-        MotionEvent event = MotionEvent.obtain(downTime, eventTime, action, 1, properties, coords,
-                0, 0, 0.0f, 0.0f, 0, 0, 0, 0);
-        instrumentation.sendPointerSync(event);
-        instrumentation.waitForIdleSync();
-    }
-
-    /**
-     * Sends (synchronously) a single mosue click to an absolute screen coordinates.
-     *
-     * @param instrumentation Instrumentation object used by the test.
-     * @param x Screen absolute x location.
-     * @param y Screen absolute y location.
-     */
-    private static void mouseSingleClick(Instrumentation instrumentation, float x, float y) {
-        long downTime = SystemClock.uptimeMillis();
-        sendMouseAction(instrumentation, MotionEvent.ACTION_DOWN, downTime, x, y);
-        sendMouseAction(instrumentation, MotionEvent.ACTION_UP, downTime, x, y);
-    }
-
-    /**
-     * Sends (synchronously) a single mouse click to the View at the specified coordinates.
-     *
-     * @param instrumentation Instrumentation object used by the test.
-     * @param v The view the coordinates are relative to.
-     * @param x Relative x location to the view.
-     * @param y Relative y location to the view.
-     */
-    private static void mouseSingleClickView(Instrumentation instrumentation, View v, int x,
-            int y) {
-        int location[] = TestTouchUtils.getAbsoluteLocationFromRelative(v, x, y);
-        int absoluteX = location[0];
-        int absoluteY = location[1];
-        mouseSingleClick(instrumentation, absoluteX, absoluteY);
-    }
-
-    /**
-     * Sends (synchronously) a single mouse click to the center of the View.
-     *
-     * @param instrumentation Instrumentation object used by the test.
-     * @param v The view the coordinates are relative to.
-     */
-    private static void mouseSingleClickView(Instrumentation instrumentation, View v) {
-        int x = v.getWidth() / 2;
-        int y = v.getHeight() / 2;
-        mouseSingleClickView(instrumentation, v, x, y);
-    }
 
 }
