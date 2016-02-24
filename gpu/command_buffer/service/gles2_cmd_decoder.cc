546252b5e8076faa06c8039663ff873f12ba2de4
yunchao.he <yunchao.he@intel.com>
2016-02-23 19:28:18 -0800

    Command buffer: width/height/depth(texture dimension) equal to 0 is valid. I think that it just does nothing in this situation if all other parameters and conditions are valid.
    This change can fix some bugs in negativetextureapi.html in deqp.
    
    BUG=565347
    TEST=gpu_unittests, webgl_conformance, webgl2_conformance, deqp/functional/gles3/negativetextureapi.html
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1718423003
    
    Cr-Commit-Position: refs/heads/master@{#377206}
    
    Former-commit-id: b22486777902c050ca3e90bee66d81b03dcf5688

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -10645,7 +10645,7 @@ bool GLES2DecoderImpl::ValidateCompressedTexDimensions(
     case GL_COMPRESSED_SRGB8_PUNCHTHROUGH_ALPHA1_ETC2:
     case GL_COMPRESSED_RGBA8_ETC2_EAC:
     case GL_COMPRESSED_SRGB8_ALPHA8_ETC2_EAC:
-      if (width <= 0 || height <= 0 || depth <= 0) {
+      if (width < 0 || height < 0 || depth < 0) {
         LOCAL_SET_GL_ERROR(
             GL_INVALID_OPERATION, function_name,
             "width, height, or depth invalid");
