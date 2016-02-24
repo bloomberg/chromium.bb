594ae5a516cfff8c0ce9eaa097beb2575a1598a3
twellington <twellington@chromium.org>
2016-02-23 20:37:52 -0800

    Disable document mode on tablets
    BUG=589317
    
    Review URL: https://codereview.chromium.org/1728143002
    
    Cr-Commit-Position: refs/heads/master@{#377220}
    
    Former-commit-id: b8282cae00d055a886ab9f18cd6c3fa42bcc6a0f

--- ui/android/java/src/org/chromium/ui/base/DeviceFormFactor.java
+++ ui/android/java/src/org/chromium/ui/base/DeviceFormFactor.java
@@ -5,6 +5,7 @@
 package org.chromium.ui.base;
 
 import android.content.Context;
+import android.util.DisplayMetrics;
 
 import org.chromium.base.annotations.CalledByNative;
 
@@ -30,10 +31,22 @@ public class DeviceFormFactor {
     }
 
     /**
+     * @param context Android's context.
      * @return True if the current screen's minimum dimension is larger than 720dp.
      */
     public static boolean isLargeTablet(Context context) {
         int minimumScreenWidthDp = context.getResources().getConfiguration().smallestScreenWidthDp;
         return minimumScreenWidthDp >= MINIMUM_LARGE_TABLET_WIDTH_DP;
+    }
+
+    /**
+     * @param context Android's context.
+     * @return True if the device's minimum dimension is larger than 600dp.
+     */
+    public static boolean isDeviceTablet(Context context) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        int smallestDeviceWidthDp = (int) Math.min(metrics.heightPixels / metrics.density,
+                metrics.widthPixels / metrics.density);
+        return smallestDeviceWidthDp >= MINIMUM_TABLET_WIDTH_DP;
     }
 }
