92b2a5b6485b78cf0e48ac4cdaa0454da6ee440c
avallee <avallee@chromium.org>
2016-02-24 14:16:36 -0800

    [NaCL SDK] nacl_io: Re-enable UnixSocketMultithreadedTest.SendRecv
    Spamminess and flakiness should be fixed by the previous CL.
    
    BUG=588784
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_nacl_sdk;tryserver.chromium.mac:mac_nacl_sdk;tryserver.chromium.win:win_nacl_sdk
    
    Review URL: https://codereview.chromium.org/1726723002
    
    Cr-Commit-Position: refs/heads/master@{#377406}
    
    Former-commit-id: b91db5809d1122ee4356f395dbf7b1808d1a1f21

--- native_client_sdk/src/tests/nacl_io_test/socket_test.cc
+++ native_client_sdk/src/tests/nacl_io_test/socket_test.cc
@@ -539,7 +539,7 @@ class UnixSocketMultithreadedTest : public UnixSocketTest {
 
 }  // namespace
 
-TEST_F(UnixSocketMultithreadedTest, DISABLED_SendRecv) {
+TEST_F(UnixSocketMultithreadedTest, SendRecv) {
   pthread_t thread = CreateThread();
 
   uint8_t pattern[] = {0xA5, 0x00, 0xC3, 0xFF};
