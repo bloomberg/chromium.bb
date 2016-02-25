5199bfb39276ca31dda88462866827d38af88af8
Dirk Pranke <dpranke@chromium.org>
2016-02-25 13:50:39 -0800

    Fix mock_drt_unittest failures.
    Oh sure, we break mock_drt and test the broken behavior, and
    when I fix mock_drt it doesn't occur to me that the tests will
    still be wrong (and now fail ...).
    
    TBR=ojan@chromium.org
    BUG=589709
    
    Review URL: https://codereview.chromium.org/1735393002 .
    
    Cr-Commit-Position: refs/heads/master@{#377676}
    
    Former-commit-id: 1b132daf178c2c9c19432e5434318ad4a4fd758e

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/mock_drt_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/mock_drt_unittest.py
@@ -110,7 +110,7 @@ class MockDRTTest(unittest.TestCase):
         return (drt_input, drt_output)
 
     def expected_output(self, port, test_name, pixel_tests, text_output, expected_checksum):
-        output = ['Content-Type: text/plain\n']
+        output = ['#READY\n', 'Content-Type: text/plain\n']
         if text_output:
             output.append(text_output)
         output.append('#EOF\n')
@@ -152,7 +152,7 @@ class MockDRTTest(unittest.TestCase):
         res = mock_drt.main(['--run-layout-test', '--platform', 'test', '-'],
                             host, stdin, stdout, stderr)
         self.assertEqual(res, 0)
-        self.assertEqual(stdout.getvalue(), '')
+        self.assertEqual(stdout.getvalue(), '#READY\n')
         self.assertEqual(stderr.getvalue(), '')
         self.assertEqual(host.filesystem.written_files, {})
 
@@ -161,15 +161,19 @@ class MockDRTTest(unittest.TestCase):
         self.assertTest('http/tests/passes/text.html', True)
 
     def test_pixeltest__fails(self):
-        self.assertTest('failures/expected/image_checksum.html', pixel_tests=True,
-            expected_checksum='image_checksum-checksum',
-            drt_output=['Content-Type: text/plain\n',
-                        'image_checksum-txt',
-                        '#EOF\n',
-                        '\n',
-                        'ActualHash: image_checksum-checksum\n',
-                        'ExpectedHash: image_checksum-checksum\n',
-                        '#EOF\n'])
+        self.assertTest('failures/expected/image_checksum.html',
+                        pixel_tests=True,
+                        expected_checksum='image_checksum-checksum',
+                        drt_output=[
+                            '#READY\n',
+                            'Content-Type: text/plain\n',
+                            'image_checksum-txt',
+                            '#EOF\n',
+                            '\n',
+                            'ActualHash: image_checksum-checksum\n',
+                            'ExpectedHash: image_checksum-checksum\n',
+                            '#EOF\n',
+                        ])
 
     def test_textonly(self):
         self.assertTest('passes/image.html', False)
@@ -190,13 +194,17 @@ class MockDRTTest(unittest.TestCase):
         self.assertTest('passes/mismatch.html', True, expected_checksum='mock-checksum', expected_text='reference text\n')
 
     def test_audio(self):
-        self.assertTest('passes/audio.html', pixel_tests=True,
-                        drt_output=['Content-Type: audio/wav\n',
-                         'Content-Transfer-Encoding: base64\n',
-                         'YXVkaW8td2F2',
-                         '\n',
-                         '#EOF\n',
-                         '#EOF\n'])
+        self.assertTest('passes/audio.html',
+                        pixel_tests=True,
+                        drt_output=[
+                            '#READY\n',
+                            'Content-Type: audio/wav\n',
+                            'Content-Transfer-Encoding: base64\n',
+                            'YXVkaW8td2F2',
+                            '\n',
+                            '#EOF\n',
+                            '#EOF\n',
+                        ])
 
     def test_virtual(self):
         self.assertTest('virtual/passes/text.html', True)
