a7cad3147650dc5535e7e9d6f07afd8d86992d23
gabadie <gabadie@chromium.org>
2016-02-25 06:09:43 -0800

    tools/android/loading: Implements WprBackend
    BUG=582080
    
    Review URL: https://codereview.chromium.org/1722243002
    
    Cr-Commit-Position: refs/heads/master@{#377569}
    
    Former-commit-id: c1adaaec9a61bb544d81d9218433990a1a159311

---
+++ tools/android/loading/wpr_backend.py
@@ -0,0 +1,124 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Opens and modifies WPR archive.
+"""
+
+import collections
+import os
+import re
+import sys
+
+
+_SRC_DIR = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), '..', '..', '..'))
+
+_WEBPAGEREPLAY_DIR = os.path.join(_SRC_DIR, 'third_party', 'webpagereplay')
+_WEBPAGEREPLAY_HTTPARCHIVE = os.path.join(_WEBPAGEREPLAY_DIR, 'httparchive.py')
+
+sys.path.append(os.path.join(_SRC_DIR, 'third_party', 'webpagereplay'))
+import httparchive
+
+# Regex used to parse httparchive.py stdout's when listing all urls.
+_PARSE_WPR_REQUEST_REGEX = re.compile(r'^\S+\s+(?P<url>\S+)')
+
+
+class WprUrlEntry(object):
+  """Wpr url entry holding request and response infos. """
+
+  def __init__(self, wpr_request, wpr_response):
+    self._wpr_response = wpr_response
+    self.url = self._ExtractUrl(str(wpr_request))
+
+  def GetResponseHeadersDict(self):
+    """Get a copied dictionary of available headers.
+
+    Returns:
+      dict(name -> value)
+    """
+    headers = collections.defaultdict(list)
+    for (key, value) in self._wpr_response.headers:
+      headers[key].append(value)
+    return {k: ','.join(v) for (k, v) in headers.items()}
+
+  def SetResponseHeader(self, name, value):
+    """Set a header value.
+
+    In the case where the <name> response header is present more than once
+    in the response header list, then the given value is set only to the first
+    occurrence of that given headers, and the next ones are removed.
+
+    Args:
+      name: The name of the response header to set.
+      value: The value of the response header to set.
+    """
+    new_headers = []
+    new_header_set = False
+    for header in self._wpr_response.headers:
+      if header[0] != name:
+        new_headers.append(header)
+      elif not new_header_set:
+        new_header_set = True
+        new_headers.append((name, value))
+    if new_header_set:
+      self._wpr_response.headers = new_headers
+    else:
+      self._wpr_response.headers.append((name, value))
+
+  def DeleteResponseHeader(self, name):
+    """Delete a header.
+
+    In the case where the <name> response header is present more than once
+    in the response header list, this method takes care of removing absolutely
+    all them.
+
+    Args:
+      name: The name of the response header field to delete.
+    """
+    self._wpr_response.headers = \
+        [x for x in self._wpr_response.headers if x[0] != name]
+
+  @classmethod
+  def _ExtractUrl(cls, request_string):
+    match = _PARSE_WPR_REQUEST_REGEX.match(request_string)
+    assert match, 'Looks like there is an issue with: {}'.format(request_string)
+    return match.group('url')
+
+
+class WprArchiveBackend(object):
+  """WPR archive back-end able to read and modify. """
+
+  def __init__(self, wpr_archive_path):
+    """Constructor:
+
+    Args:
+      wpr_archive_path: The path of the WPR archive to read/modify.
+    """
+    self._wpr_archive_path = wpr_archive_path
+    self._http_archive = httparchive.HttpArchive.Load(wpr_archive_path)
+
+  def ListUrlEntries(self):
+    """Iterates over all url entries
+
+    Returns:
+      A list of WprUrlEntry.
+    """
+    return [WprUrlEntry(request, self._http_archive[request])
+            for request in self._http_archive.get_requests()]
+
+  def Persist(self):
+    """Persists the archive to disk. """
+    self._http_archive.Persist(self._wpr_archive_path)
+
+
+if __name__ == '__main__':
+  import argparse
+  parser = argparse.ArgumentParser(description='Tests cache back-end.')
+  parser.add_argument('wpr_archive', type=str)
+  command_line_args = parser.parse_args()
+
+  wpr_backend = WprArchiveBackend(command_line_args.wpr_archive)
+  url_entries = wpr_backend.ListUrlEntries()
+  print url_entries[0].url
+  wpr_backend.Persist()
---
+++ tools/android/loading/wpr_backend_unittest.py
@@ -0,0 +1,92 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+from wpr_backend import WprUrlEntry
+
+
+class MockWprResponse(object):
+  def __init__(self, headers):
+    self.headers = headers
+
+class WprUrlEntryTest(unittest.TestCase):
+
+  @classmethod
+  def _CreateWprUrlEntry(cls, headers):
+    wpr_response = MockWprResponse(headers)
+    return WprUrlEntry('GET http://a.com/', wpr_response)
+
+  def test_ExtractUrl(self):
+    self.assertEquals('http://aa.bb/c',
+                      WprUrlEntry._ExtractUrl('GET http://aa.bb/c'))
+    self.assertEquals('http://aa.b/c',
+                      WprUrlEntry._ExtractUrl('POST http://aa.b/c'))
+    self.assertEquals('http://a.bb/c',
+                      WprUrlEntry._ExtractUrl('WHATEVER http://a.bb/c'))
+    self.assertEquals('https://aa.bb/c',
+                      WprUrlEntry._ExtractUrl('GET https://aa.bb/c'))
+    self.assertEquals('http://aa.bb',
+                      WprUrlEntry._ExtractUrl('GET http://aa.bb'))
+    self.assertEquals('http://aa.bb',
+                      WprUrlEntry._ExtractUrl('GET http://aa.bb FOO BAR'))
+
+  def test_GetResponseHeadersDict(self):
+    entry = self._CreateWprUrlEntry([('header0', 'value0'),
+                                     ('header1', 'value1'),
+                                     ('header0', 'value2'),
+                                     ('header2', 'value3'),
+                                     ('header0', 'value4')])
+    headers = entry.GetResponseHeadersDict()
+    self.assertEquals(3, len(headers))
+    self.assertEquals('value0,value2,value4', headers['header0'])
+    self.assertEquals('value1', headers['header1'])
+    self.assertEquals('value3', headers['header2'])
+
+  def test_SetResponseHeader(self):
+    entry = self._CreateWprUrlEntry([('header0', 'value0'),
+                                     ('header1', 'value1')])
+    entry.SetResponseHeader('new_header0', 'new_value0')
+    headers = entry.GetResponseHeadersDict()
+    self.assertEquals(3, len(headers))
+    self.assertEquals('new_value0', headers['new_header0'])
+    self.assertEquals('new_header0', entry._wpr_response.headers[2][0])
+
+    entry = self._CreateWprUrlEntry([('header0', 'value0'),
+                                     ('header1', 'value1'),
+                                     ('header2', 'value1'),])
+    entry.SetResponseHeader('header1', 'new_value1')
+    headers = entry.GetResponseHeadersDict()
+    self.assertEquals(3, len(headers))
+    self.assertEquals('new_value1', headers['header1'])
+    self.assertEquals('header1', entry._wpr_response.headers[1][0])
+
+    entry = self._CreateWprUrlEntry([('header0', 'value0'),
+                                     ('header1', 'value1'),
+                                     ('header2', 'value2'),
+                                     ('header1', 'value3'),
+                                     ('header3', 'value4')])
+    entry.SetResponseHeader('header1', 'new_value2')
+    headers = entry.GetResponseHeadersDict()
+    self.assertEquals(4, len(headers))
+    self.assertEquals('new_value2', headers['header1'])
+    self.assertEquals('header1', entry._wpr_response.headers[1][0])
+    self.assertEquals('header3', entry._wpr_response.headers[3][0])
+    self.assertEquals('value4', entry._wpr_response.headers[3][1])
+
+  def test_DeleteResponseHeader(self):
+    entry = self._CreateWprUrlEntry([('header0', 'value0'),
+                                     ('header1', 'value1'),
+                                     ('header0', 'value2'),
+                                     ('header2', 'value3')])
+    entry.DeleteResponseHeader('header1')
+    self.assertNotIn('header1', entry.GetResponseHeadersDict())
+    self.assertEquals(2, len(entry.GetResponseHeadersDict()))
+    entry.DeleteResponseHeader('header0')
+    self.assertNotIn('header0', entry.GetResponseHeadersDict())
+    self.assertEquals(1, len(entry.GetResponseHeadersDict()))
+
+
+if __name__ == '__main__':
+  unittest.main()
