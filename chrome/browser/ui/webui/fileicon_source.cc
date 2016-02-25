22ac768dcc83288d818883e6ac70d6d17fed960d
mmenke <mmenke@chromium.org>
2016-02-25 15:17:53 -0800

    Remove use of URL_SPECIAL_CHARS from fileicon_source.
    We're removing this, in favor of PATH_SEPARATORS and
    URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS, to reduce
    the chances of security issues with unescaping going
    forward.
    
    BUG=589257
    
    Review URL: https://codereview.chromium.org/1731523002
    
    Cr-Commit-Position: refs/heads/master@{#377697}
    
    Former-commit-id: c85758a9e3f5549748e52bc95bc7dcbd94243781

--- chrome/browser/ui/webui/fileicon_source.cc
+++ chrome/browser/ui/webui/fileicon_source.cc
@@ -42,8 +42,9 @@ void GetFilePathAndQuery(const std::string& url,
   // We receive the url with chrome://fileicon/ stripped but GURL expects it.
   const GURL gurl("chrome://fileicon/" + url);
   std::string path = net::UnescapeURLComponent(
-      gurl.path().substr(1), (net::UnescapeRule::URL_SPECIAL_CHARS |
-                              net::UnescapeRule::SPACES));
+      gurl.path().substr(1),
+      net::UnescapeRule::URL_SPECIAL_CHARS_EXCEPT_PATH_SEPARATORS |
+          net::UnescapeRule::PATH_SEPARATORS | net::UnescapeRule::SPACES);
 
   *file_path = base::FilePath::FromUTF8Unsafe(path);
   *file_path = file_path->NormalizePathSeparators();
