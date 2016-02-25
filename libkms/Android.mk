42745396734be0c21e62254b73bd7ca44b482d41
Chih-Wei Huang <cwhuang@android-x86.org>
2016-02-26 00:41:53 +0800

    android: add virgl to be a valid driver
    To avoid the warning:
    
    external/libdrm/libkms/Android.mk:17: invalid GPU drivers: virgl
    
    Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>
    Reviewed-by: Emil Velikov <emil.l.velikov@gmail.com>

--- libkms/Android.mk
+++ libkms/Android.mk
@@ -3,12 +3,14 @@ DRM_GPU_DRIVERS := $(strip $(filter-out swrast, $(BOARD_GPU_DRIVERS)))
 intel_drivers := i915 i965 i915g ilo
 radeon_drivers := r300g r600g radeonsi
 nouveau_drivers := nouveau
+virgl_drivers := virgl
 vmwgfx_drivers := vmwgfx
 
 valid_drivers := \
 	$(intel_drivers) \
 	$(radeon_drivers) \
 	$(nouveau_drivers) \
+	$(virgl_drivers) \
 	$(vmwgfx_drivers)
 
 # warn about invalid drivers
