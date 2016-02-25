f9739d75ee71953d8b637fe2c996e31373e90a1c
mseaborn <mseaborn@chromium.org>
2016-02-25 13:44:15 -0800

    pthread.h: Remove unneeded #define, NC_INVALID_HANDLE
    This was used to initialise some padding fields.  We can initialise
    them with 0 rather than -1.  The former is slightly more efficient
    because it allows variables to be placed in BSS.
    
    Also make the INITIALIZER macros a little more readable by using the
    PTHREAD_MUTEX_* enum names instead of hard-coding numeric values.
    
    BUG=none
    TEST=run_thread_test
    
    Review URL: https://codereview.chromium.org/1724973002
    
    
    Former-commit-id: 1481b58cb78a33c3b0d3107b51e71c9364dc2f9a

--- src/untrusted/pthread/pthread.h
+++ src/untrusted/pthread/pthread.h
@@ -150,26 +150,23 @@ typedef struct {
   int dummy; /**< Reserved; rwlocks don't have attributes */
 } pthread_rwlockattr_t;
 
-/** A value that represents an uninitialized handle. */
-#define NC_INVALID_HANDLE -1
-
 /** Illegal thread ID value. */
 #define NACL_PTHREAD_ILLEGAL_THREAD_ID ((pthread_t) 0)
 
 /** Statically initializes a pthread_mutex_t representing a recursive mutex. */
 #define PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP \
-    {0, 1, NACL_PTHREAD_ILLEGAL_THREAD_ID, 0, NC_INVALID_HANDLE}
+    {0, PTHREAD_MUTEX_RECURSIVE, NACL_PTHREAD_ILLEGAL_THREAD_ID, 0, 0}
 /** Statically initializes a pthread_mutex_t representing a fast mutex. */
 #define PTHREAD_MUTEX_INITIALIZER \
-    {0, 0, NACL_PTHREAD_ILLEGAL_THREAD_ID, 0, NC_INVALID_HANDLE}
+    {0, PTHREAD_MUTEX_NORMAL, NACL_PTHREAD_ILLEGAL_THREAD_ID, 0, 0}
 /**
  * Statically initializes a pthread_mutex_t representing an
  * error-checking mutex.
  */
 #define PTHREAD_ERRORCHECK_MUTEX_INITIALIZER_NP \
-    {0, 2, NACL_PTHREAD_ILLEGAL_THREAD_ID, 0, NC_INVALID_HANDLE}
+    {0, PTHREAD_MUTEX_ERRORCHECK, NACL_PTHREAD_ILLEGAL_THREAD_ID, 0, 0}
 /** Statically initializes a condition variable (pthread_cond_t). */
-#define PTHREAD_COND_INITIALIZER {0, NC_INVALID_HANDLE}
+#define PTHREAD_COND_INITIALIZER {0, 0}
 /** Statically initializes a rwlock (pthread_rwlock_t). */
 #define PTHREAD_RWLOCK_INITIALIZER \
     {PTHREAD_MUTEX_INITIALIZER, 0, 0, NACL_PTHREAD_ILLEGAL_THREAD_ID, \
