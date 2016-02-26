b4b7531e3533c96c6882f266d891dc93d572eabe
hiroshige <hiroshige@chromium.org>
2016-02-26 09:20:12 -0800

    Remove ExecutionContextTask subclasses: MarkingBooleanTask
    BUG=478194
    
    Review URL: https://codereview.chromium.org/1722463002
    
    Cr-Commit-Position: refs/heads/master@{#377909}
    
    Former-commit-id: 1f6a28c6f84b18945ed645004d474f61aab18767

--- third_party/WebKit/Source/core/dom/MainThreadTaskRunnerTest.cpp
+++ third_party/WebKit/Source/core/dom/MainThreadTaskRunnerTest.cpp
@@ -38,26 +38,10 @@
 
 namespace blink {
 
-class MarkingBooleanTask final : public ExecutionContextTask {
-public:
-    static PassOwnPtr<MarkingBooleanTask> create(bool* toBeMarked)
-    {
-        return adoptPtr(new MarkingBooleanTask(toBeMarked));
-    }
-
-
-    ~MarkingBooleanTask() override { }
-
-private:
-    MarkingBooleanTask(bool* toBeMarked) : m_toBeMarked(toBeMarked) { }
-
-    void performTask(ExecutionContext* context) override
-    {
-        *m_toBeMarked = true;
-    }
-
-    bool* m_toBeMarked;
-};
+static void markBoolean(bool* toBeMarked)
+{
+    *toBeMarked = true;
+}
 
 TEST(MainThreadTaskRunnerTest, PostTask)
 {
@@ -65,7 +49,7 @@ TEST(MainThreadTaskRunnerTest, PostTask)
     OwnPtrWillBeRawPtr<MainThreadTaskRunner> runner = MainThreadTaskRunner::create(context.get());
     bool isMarked = false;
 
-    runner->postTask(BLINK_FROM_HERE, MarkingBooleanTask::create(&isMarked));
+    runner->postTask(BLINK_FROM_HERE, createSameThreadTask(&markBoolean, &isMarked));
     EXPECT_FALSE(isMarked);
     blink::testing::runPendingTasks();
     EXPECT_TRUE(isMarked);
@@ -78,7 +62,7 @@ TEST(MainThreadTaskRunnerTest, SuspendTask)
     bool isMarked = false;
 
     context->setTasksNeedSuspension(true);
-    runner->postTask(BLINK_FROM_HERE, MarkingBooleanTask::create(&isMarked));
+    runner->postTask(BLINK_FROM_HERE, createSameThreadTask(&markBoolean, &isMarked));
     runner->suspend();
     blink::testing::runPendingTasks();
     EXPECT_FALSE(isMarked);
@@ -96,7 +80,7 @@ TEST(MainThreadTaskRunnerTest, RemoveRunner)
     bool isMarked = false;
 
     context->setTasksNeedSuspension(true);
-    runner->postTask(BLINK_FROM_HERE, MarkingBooleanTask::create(&isMarked));
+    runner->postTask(BLINK_FROM_HERE, createSameThreadTask(&markBoolean, &isMarked));
     runner.clear();
     blink::testing::runPendingTasks();
     EXPECT_FALSE(isMarked);
