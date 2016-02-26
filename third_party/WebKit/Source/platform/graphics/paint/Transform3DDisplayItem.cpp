73f3ad833c1b93e75ad57d734027b870265921ab
wkorman <wkorman@chromium.org>
2016-02-25 19:49:58 -0800

    Fix quaternion typo and remove unnecessary WTF qualifiers.
    Review URL: https://codereview.chromium.org/1742563003
    
    Cr-Commit-Position: refs/heads/master@{#377801}
    
    Former-commit-id: b3927609c4307362514e02fb00680112c9112ad2

--- third_party/WebKit/Source/platform/graphics/paint/Transform3DDisplayItem.cpp
+++ third_party/WebKit/Source/platform/graphics/paint/Transform3DDisplayItem.cpp
@@ -27,13 +27,13 @@ void BeginTransform3DDisplayItem::appendToWebDisplayItemList(const IntRect& visu
 }
 
 #ifndef NDEBUG
-void BeginTransform3DDisplayItem::dumpPropertiesAsDebugString(WTF::StringBuilder& stringBuilder) const
+void BeginTransform3DDisplayItem::dumpPropertiesAsDebugString(StringBuilder& stringBuilder) const
 {
     PairedBeginDisplayItem::dumpPropertiesAsDebugString(stringBuilder);
     TransformationMatrix::DecomposedType decomposition;
     if (m_transform.decompose(decomposition)) {
-        stringBuilder.append(WTF::String::format(
-            ", translate: [%lf,%lf,%lf], scale: [%lf,%lf,%lf], skew: [%lf,%lf,%lf], quarternion: [%lf,%lf,%lf,%lf], perspective: [%lf,%lf,%lf,%lf]",
+        stringBuilder.append(String::format(
+            ", translate: [%lf,%lf,%lf], scale: [%lf,%lf,%lf], skew: [%lf,%lf,%lf], quaternion: [%lf,%lf,%lf,%lf], perspective: [%lf,%lf,%lf,%lf]",
             decomposition.translateX, decomposition.translateY, decomposition.translateZ,
             decomposition.scaleX, decomposition.scaleY, decomposition.scaleZ,
             decomposition.skewXY, decomposition.skewXZ, decomposition.skewYZ,
