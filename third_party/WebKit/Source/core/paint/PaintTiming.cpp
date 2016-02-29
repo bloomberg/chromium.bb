53fb7763633f236291f40dc83da2885d26cdfd69
bmcquade <bmcquade@chromium.org>
2016-02-29 07:12:03 -0800

    Mark first paint as part of marking first image/text/contentful paint.
    First paint necessarily comes at or before more specific types of paints.
    
    BUG=590454
    
    Review URL: https://codereview.chromium.org/1734403004
    
    Cr-Commit-Position: refs/heads/master@{#378209}
    
    Former-commit-id: ab2d663fc2312b2aead7b8de54f2d596a54e2797

--- third_party/WebKit/Source/core/paint/PaintTiming.cpp
+++ third_party/WebKit/Source/core/paint/PaintTiming.cpp
@@ -23,14 +23,57 @@ PaintTiming& PaintTiming::from(Document& document)
     return *timing;
 }
 
-PaintTiming::PaintTiming(Document& document)
-    : m_document(document)
+void PaintTiming::markFirstPaint()
 {
+    // Test that m_firstPaint is non-zero here, as well as in setFirstPaint, so
+    // we avoid invoking monotonicallyIncreasingTime() on every call to
+    // markFirstPaint().
+    if (m_firstPaint != 0.0)
+        return;
+    setFirstPaint(monotonicallyIncreasingTime());
+    notifyPaintTimingChanged();
+}
+
+void PaintTiming::markFirstContentfulPaint()
+{
+    // Test that m_firstContentfulPaint is non-zero here, as well as in
+    // setFirstContentfulPaint, so we avoid invoking
+    // monotonicallyIncreasingTime() on every call to
+    // markFirstContentfulPaint().
+    if (m_firstContentfulPaint != 0.0)
+        return;
+    setFirstContentfulPaint(monotonicallyIncreasingTime());
+    notifyPaintTimingChanged();
+}
+
+void PaintTiming::markFirstTextPaint()
+{
+    if (m_firstTextPaint != 0.0)
+        return;
+    m_firstTextPaint = monotonicallyIncreasingTime();
+    setFirstContentfulPaint(m_firstTextPaint);
+    TRACE_EVENT_MARK_WITH_TIMESTAMP1("blink.user_timing", "firstTextPaint", m_firstTextPaint, "frame", frame());
+    notifyPaintTimingChanged();
+}
+
+void PaintTiming::markFirstImagePaint()
+{
+    if (m_firstImagePaint != 0.0)
+        return;
+    m_firstImagePaint = monotonicallyIncreasingTime();
+    setFirstContentfulPaint(m_firstImagePaint);
+    TRACE_EVENT_MARK_WITH_TIMESTAMP1("blink.user_timing", "firstImagePaint", m_firstImagePaint, "frame", frame());
+    notifyPaintTimingChanged();
 }
 
 DEFINE_TRACE(PaintTiming)
 {
     visitor->trace(m_document);
+}
+
+PaintTiming::PaintTiming(Document& document)
+    : m_document(document)
+{
 }
 
 LocalFrame* PaintTiming::frame() const
@@ -44,48 +87,21 @@ void PaintTiming::notifyPaintTimingChanged()
         m_document->loader()->didChangePerformanceTiming();
 }
 
-void PaintTiming::markFirstPaint()
+void PaintTiming::setFirstPaint(double stamp)
 {
     if (m_firstPaint != 0.0)
         return;
-    m_firstPaint = monotonicallyIncreasingTime();
+    m_firstPaint = stamp;
     TRACE_EVENT_INSTANT1("blink.user_timing", "firstPaint", TRACE_EVENT_SCOPE_PROCESS, "frame", frame());
-    notifyPaintTimingChanged();
 }
 
-void PaintTiming::markFirstTextPaint()
-{
-    if (m_firstTextPaint != 0.0)
-        return;
-    m_firstTextPaint = monotonicallyIncreasingTime();
-    TRACE_EVENT_MARK_WITH_TIMESTAMP1("blink.user_timing", "firstTextPaint", m_firstTextPaint, "frame", frame());
-
-    if (firstContentfulPaint() == 0.0)
-        markFirstContentfulPaint();
-    else
-        notifyPaintTimingChanged();
-}
-
-void PaintTiming::markFirstImagePaint()
-{
-    if (m_firstImagePaint != 0.0)
-        return;
-    m_firstImagePaint = monotonicallyIncreasingTime();
-    TRACE_EVENT_MARK_WITH_TIMESTAMP1("blink.user_timing", "firstImagePaint", m_firstImagePaint, "frame", frame());
-
-    if (firstContentfulPaint() == 0.0)
-        markFirstContentfulPaint();
-    else
-        notifyPaintTimingChanged();
-}
-
-void PaintTiming::markFirstContentfulPaint()
+void PaintTiming::setFirstContentfulPaint(double stamp)
 {
     if (m_firstContentfulPaint != 0.0)
         return;
-    m_firstContentfulPaint = monotonicallyIncreasingTime();
+    setFirstPaint(stamp);
+    m_firstContentfulPaint = stamp;
     TRACE_EVENT_INSTANT1("blink.user_timing", "firstContentfulPaint", TRACE_EVENT_SCOPE_PROCESS, "frame", frame());
-    notifyPaintTimingChanged();
 }
 
 } // namespace blink
