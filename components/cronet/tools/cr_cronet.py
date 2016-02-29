a99918299c328e4b3b3530bc011f0abb332e7c10
xunjieli <xunjieli@chromium.org>
2016-02-29 14:28:20 -0800

    Use fast-local-dev in Cronet's cr_cronet.py script
    The previous CL missed a whitespace.
    
    Review URL: https://codereview.chromium.org/1748083002
    
    Cr-Commit-Position: refs/heads/master@{#378292}
    
    Former-commit-id: 22f28b3f0ff14b5997401aa66a2476797fb8e1c1

--- components/cronet/tools/cr_cronet.py
+++ components/cronet/tools/cr_cronet.py
@@ -31,7 +31,7 @@ def install(release_arg):
 def test(release_arg, extra_options):
   return run('build/android/test_runner.py instrumentation '+ \
              release_arg + ' --test-apk=CronetTestInstrumentation' + \
-             '--fast-local-dev',
+             ' --fast-local-dev',
              extra_options)
 
 
