cabadcf62fe6a0fa8a3b2fc65da97cd88b7371d8
mattcary <mattcary@chromium.org>
2016-03-01 08:38:26 -0800

    Add occurrence tracking to a multigraph.
    The idea is to annotate node bags with things like "75% of urls were after first
    contentful paint".
    
    Review URL: https://codereview.chromium.org/1735093002
    
    Cr-Commit-Position: refs/heads/master@{#378453}
    
    Former-commit-id: de1d607e176ce97c3857d2c8231a2fd4a862f625

--- tools/android/loading/resource_sack_unittest.py
+++ tools/android/loading/resource_sack_unittest.py
@@ -59,6 +59,36 @@ class ResourceSackTestCase(unittest.TestCase):
     self.assertEqual(set(['0/', 'data:fake/content']),
                      set([bag.label for bag in sack.bags]))
 
+  def test_Occurrence(self):
+    # There are two graph shapes. The first one is added to the sack three
+    # times, and the second once. The second graph has one sibling that doesn't
+    # appear in the first as well as a new child.
+    shape1 = [MakeRequest(0, 'null'), MakeRequest(1, 0), MakeRequest(2, 0)]
+    shape2 = [MakeRequest(0, 'null'), MakeRequest(1, 0),
+              MakeRequest(3, 0), MakeRequest(4, 1)]
+    graphs = [TestResourceGraph.FromRequestList(shape1),
+              TestResourceGraph.FromRequestList(shape1),
+              TestResourceGraph.FromRequestList(shape1),
+              TestResourceGraph.FromRequestList(shape2)]
+    sack = resource_sack.GraphSack()
+    for g in graphs:
+      sack.ConsumeGraph(g)
+    # Map a graph to a list of nodes that are in its filter.
+    filter_sets = {
+        graphs[0]: set([0, 1, 2]),
+        graphs[1]: set([0, 1, 2]),
+        graphs[2]: set([0, 1]),
+        graphs[3]: set([0, 3])}
+    sack.FilterOccurrence(
+        'test', lambda graph: lambda node:
+            int(node.ShortName()) in filter_sets[graph])
+    labels = {bag.label: bag for bag in sack.bags}
+    self.assertAlmostEqual(1, labels['0/'].GetOccurrence('test'), 3)
+    self.assertAlmostEqual(0.75, labels['1/'].GetOccurrence('test'), 3)
+    self.assertAlmostEqual(0.667, labels['2/'].GetOccurrence('test'), 3)
+    self.assertAlmostEqual(1, labels['3/'].GetOccurrence('test'), 3)
+    self.assertAlmostEqual(0, labels['4/'].GetOccurrence('test'), 3)
+
 
 if __name__ == '__main__':
   unittest.main()
