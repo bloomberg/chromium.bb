237d6a78ca164f90265f9d2cc90129c4c966f840
nicholss <nicholss@chromium.org>
2016-02-29 16:03:25 -0800

    Fixes ui/gfx builds for iOS.
    There was a breaking change including surface_io.h for iOS. IOSurface is a Mac-only API.
    
    Review URL: https://codereview.chromium.org/1748703002
    
    Cr-Commit-Position: refs/heads/master@{#378328}
    
    Former-commit-id: 0e21f6fd91ef9ebaf5e6e2684e8e17a0f6c5ad25

--- ui/gfx/gpu_memory_buffer.h
+++ ui/gfx/gpu_memory_buffer.h
@@ -18,7 +18,7 @@
 
 #if defined(USE_OZONE)
 #include "ui/gfx/native_pixmap_handle_ozone.h"
-#elif defined(OS_MACOSX)
+#elif defined(OS_MACOSX) && !defined(OS_IOS)
 #include "ui/gfx/mac/io_surface.h"
 #endif
 
@@ -48,7 +48,7 @@ struct GFX_EXPORT GpuMemoryBufferHandle {
   int32_t stride;
 #if defined(USE_OZONE)
   NativePixmapHandle native_pixmap_handle;
-#elif defined(OS_MACOSX)
+#elif defined(OS_MACOSX) && !defined(OS_IOS)
   ScopedRefCountedIOSurfaceMachPort mach_port;
 #endif
 };
