1b1a6d4d36c4eed4e881da198596e7bbde92cc42
dschuff <dschuff@chromium.org>
2016-03-01 08:50:15 -0800

    Re-enable PnaclTranslationCacheTest.StoreLargeOnDisk
    This was disabled for WinXP, which we are no longer testing.
    
    R=bradn@chromium.org
    BUG=468741
    
    Review URL: https://codereview.chromium.org/1748763003
    
    Cr-Commit-Position: refs/heads/master@{#378458}
    
    Former-commit-id: 3dc3d7d5a2f8c97d7371fe7870bee5e6609f3b32

--- components/nacl/browser/pnacl_translation_cache_unittest.cc
+++ components/nacl/browser/pnacl_translation_cache_unittest.cc
@@ -226,11 +226,6 @@ TEST_F(PnaclTranslationCacheTest, StoreSmallOnDisk) {
 }
 
 TEST_F(PnaclTranslationCacheTest, StoreLargeOnDisk) {
-#if defined(OS_WIN)
-  // Flaky on XP bot http://crbug.com/468741
-  if (base::win::GetVersion() <= base::win::VERSION_XP)
-    return;
-#endif
   // Test a value too large(?) for a single I/O operation
   InitBackend(false);
   const std::string large_buffer(kLargeNexeSize, 'a');
