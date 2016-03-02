841c6a9a275ad6b2059579185153613f2d95985d
xhwang <xhwang@chromium.org>
2016-03-01 19:49:56 -0800

    media: Add media pipeline fuzzer test.
    BUG=584119
    TEST=Add a fuzzer test.
    
    Review URL: https://codereview.chromium.org/1757603002
    
    Cr-Commit-Position: refs/heads/master@{#378679}
    
    Former-commit-id: 2fe13fa6192debf74dc920d02a3396d8f3529574

---
+++ media/test/pipeline_integration_fuzzertest.cc
@@ -0,0 +1,34 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/at_exit.h"
+#include "base/command_line.h"
+#include "base/logging.h"
+#include "media/base/pipeline_status.h"
+#include "media/test/pipeline_integration_test_base.h"
+
+// Entry point for LibFuzzer.
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
+  // Media pipeline starts new threads, which needs AtExitManager.
+  base::AtExitManager at_exit;
+
+  // Media pipeline checks command line arguments internally.
+  base::CommandLine::Init(0, nullptr);
+
+  media::PipelineIntegrationTestBase test;
+
+  media::PipelineStatus pipeline_status =
+      test.Start(data, size, media::PipelineIntegrationTestBase::kClockless);
+  if (pipeline_status != media::PIPELINE_OK)
+    return 0;
+
+  test.Play();
+  pipeline_status = test.WaitUntilEndedOrError();
+  if (pipeline_status != media::PIPELINE_OK)
+    return 0;
+
+  test.Seek(base::TimeDelta());
+
+  return 0;
+}
