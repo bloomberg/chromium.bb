1ff422a22a8e1f86de9ad52f668a8e809734ee18
mmoroz <mmoroz@chromium.org>
2016-03-03 08:09:01 -0800

    media: small update for pipeline integration fuzzertest.
    - rename to media_pipeline_integration_fuzzer as other fuzzers
    - include stddef.h and stdint.h (tend to include explicitly for all fuzzers)
    
    R=dalecurtis@chromium.org, inferno@chromium.org, kcc@chromium.org, xhwang@chromium.org
    BUG=584119
    TBR=aizatsky@chromium.org
    
    Review URL: https://codereview.chromium.org/1755803003
    
    Cr-Commit-Position: refs/heads/master@{#379013}
    
    Former-commit-id: 1499f38b48041e4dad05ed20f20ed4d6414bb9d6

--- media/test/pipeline_integration_fuzzertest.cc
+++ media/test/pipeline_integration_fuzzertest.cc
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <stddef.h>
+#include <stdint.h>
+
 #include "base/at_exit.h"
 #include "base/command_line.h"
 #include "base/logging.h"
