1a2cbd8aa96a045e62e98012ed7578c2015dadbf
hiroshige <hiroshige@chromium.org>
2016-03-03 17:44:51 -0800

    Set WebURLLoadTiming to responses in URLTestHelpers
    WebURLLoaderImpl sets WebURLLoadTiming, but URLTestHelpers doesn't.
    WebURLLoadTiming is required when testing ThreadableLoader in https://codereview.chromium.org/1257343003/.
    
    BUG=532364
    
    Review URL: https://codereview.chromium.org/1724903003
    
    Cr-Commit-Position: refs/heads/master@{#379171}
    
    Former-commit-id: cc005587531ad4f19d5ed479d557442d88cb0c83

--- third_party/WebKit/Source/platform/testing/URLTestHelpers.cpp
+++ third_party/WebKit/Source/platform/testing/URLTestHelpers.cpp
@@ -34,6 +34,7 @@
 #include "public/platform/Platform.h"
 #include "public/platform/WebURL.h"
 #include "public/platform/WebURLError.h"
+#include "public/platform/WebURLLoadTiming.h"
 #include "public/platform/WebURLResponse.h"
 #include "public/platform/WebUnitTestSupport.h"
 
@@ -54,19 +55,27 @@ void registerMockedURLLoad(const WebURL& fullURL, const WebString& fileName, con
 
 void registerMockedURLLoad(const WebURL& fullURL, const WebString& fileName, const WebString& relativeBaseDirectory, const WebString& mimeType)
 {
+    WebURLLoadTiming timing;
+    timing.initialize();
+
     WebURLResponse response(fullURL);
     response.setMIMEType(mimeType);
     response.setHTTPStatusCode(200);
+    response.setLoadTiming(timing);
 
     registerMockedURLLoadWithCustomResponse(fullURL, fileName, relativeBaseDirectory, response);
 }
 
 void registerMockedErrorURLLoad(const WebURL& fullURL)
 {
+    WebURLLoadTiming timing;
+    timing.initialize();
+
     WebURLResponse response;
     response.initialize();
     response.setMIMEType("image/png");
     response.setHTTPStatusCode(404);
+    response.setLoadTiming(timing);
 
     WebURLError error;
     error.reason = 404;
