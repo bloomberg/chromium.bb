59f7dd649426b26f9dc50540c47d47ae79b18e48
Nathan E. Egge <negge@mozilla.com>
2016-03-06 10:59:29 -0500

    Add Daala entropy coder.
    Change-Id: I2849a50163268d58cc5d80aacfec1fd02299ca43

--- aom_dsp/aom_dsp.mk
+++ aom_dsp/aom_dsp.mk
@@ -44,6 +44,15 @@ endif
 # intra predictions
 DSP_SRCS-yes += intrapred.c
 
+ifeq ($(CONFIG_DAALA_EC),yes)
+DSP_SRCS-yes += entenc.c
+DSP_SRCS-yes += entenc.h
+DSP_SRCS-yes += entdec.c
+DSP_SRCS-yes += entdec.h
+DSP_SRCS-yes += entcode.c
+DSP_SRCS-yes += entcode.h
+endif
+
 ifeq ($(CONFIG_USE_X86INC),yes)
 DSP_SRCS-$(HAVE_SSE) += x86/intrapred_sse2.asm
 DSP_SRCS-$(HAVE_SSE2) += x86/intrapred_sse2.asm
