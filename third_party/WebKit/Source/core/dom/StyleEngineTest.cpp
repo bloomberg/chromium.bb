cb17aea3531d045778c8cf831355d37cc3b662e7
treib <treib@chromium.org>
2016-03-07 11:46:56 -0800

    Reland of Avoid nuking everything when injecting stylesheet. (patchset #1 id:1 of https://codereview.chromium.org/1770903002/ )
    Reason for revert:
    Relanding since the original CL doesn't seem to be the cause of the flakiness.
    
    Original issue's description:
    > Revert of Avoid nuking everything when injecting stylesheet. (patchset #1 id:1 of https://codereview.chromium.org/1762443004/ )
    >
    > Reason for revert:
    > Seems to have introduced a (slightly flaky) use-after-free on Mac ASan 64:
    > https://build.chromium.org/p/chromium.memory/builders/Mac%20ASan%2064%20Tests%20%281%29?numbuilds=100
    >
    > "All_ParameterizedWebFrameTest.SetForceZeroLayoutHeightWorksAcrossNavigations_1" fails pretty consistently (only 1 success out of ~20 runs), a few others fail only occasionally.
    >
    > Original issue's description:
    > > Avoid nuking everything when injecting stylesheet.
    > >
    > > Instead do an analyzed update of the Document scope.
    > >
    > > Committed: https://crrev.com/3ec6987fa302036a0abc31e3c7243f94ced3359d [formerly 6ddb19de461880fa47a5c563f30119e6d05b20be]
    > > Cr-Commit-Position: refs/heads/master@{#379528}
    >
    > TBR=dstockwell@chromium.org,rune@opera.com
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    >
    > Committed: https://crrev.com/9a56f142ed3db810f1a6033f28c02c50311ca03c [formerly 3b2214c75296d5ee376f7a3853af14ab7aafb98d]
    > Cr-Commit-Position: refs/heads/master@{#379580}
    
    TBR=dstockwell@chromium.org,rune@opera.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1767163002
    
    Cr-Commit-Position: refs/heads/master@{#379615}
    
    Former-commit-id: 701f238b27019e76f57af621f52ad7f5a53456d3

--- third_party/WebKit/Source/core/dom/StyleEngineTest.cpp
+++ third_party/WebKit/Source/core/dom/StyleEngineTest.cpp
@@ -6,7 +6,9 @@
 
 #include "core/css/StyleSheetContents.h"
 #include "core/dom/Document.h"
+#include "core/dom/NodeComputedStyle.h"
 #include "core/frame/FrameView.h"
+#include "core/html/HTMLElement.h"
 #include "core/testing/DummyPageHolder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -38,6 +40,30 @@ TEST_F(StyleEngineTest, DocumentDirtyAfterInject)
     document().view()->updateAllLifecyclePhases();
 
     EXPECT_TRUE(isDocumentStyleSheetCollectionClean());
+}
+
+TEST_F(StyleEngineTest, AnalyzedInject)
+{
+    document().body()->setInnerHTML("<style>div { color: red }</style><div id='t1'>Green</div><div></div>", ASSERT_NO_EXCEPTION);
+    document().view()->updateAllLifecyclePhases();
+
+    Element* t1 = document().getElementById("t1");
+    ASSERT_TRUE(t1);
+    ASSERT_TRUE(t1->computedStyle());
+    EXPECT_EQ(makeRGB(255, 0, 0), t1->computedStyle()->visitedDependentColor(CSSPropertyColor));
+
+    unsigned beforeCount = styleEngine().styleForElementCount();
+
+    RefPtrWillBeRawPtr<StyleSheetContents> parsedSheet = StyleSheetContents::create(CSSParserContext(document(), nullptr));
+    parsedSheet->parseString("#t1 { color: green }");
+    styleEngine().injectAuthorSheet(parsedSheet);
+    document().view()->updateAllLifecyclePhases();
+
+    unsigned afterCount = styleEngine().styleForElementCount();
+    EXPECT_EQ(1u, afterCount - beforeCount);
+
+    ASSERT_TRUE(t1->computedStyle());
+    EXPECT_EQ(makeRGB(0, 128, 0), t1->computedStyle()->visitedDependentColor(CSSPropertyColor));
 }
 
 } // namespace blink
