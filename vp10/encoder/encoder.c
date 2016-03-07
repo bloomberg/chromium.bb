1579133291fcb900d20e1d3917e7cef6af9f6a55
Nathan E. Egge <negge@dgql.org>
2016-03-07 13:31:40 -0500

    Use OD_DIVU for small divisions in temporal_filter.
    Replaces an approximate unsigned integer division with the bit exact
     OD_ILOG() implementation.
    Removes the need to call vp10_temporal_filter_init() before calling
     vp10_temporal_filter_apply_c() by using a static table of constants.
    
    ntt-short-1:
    
              MEDIUM (%) HIGH (%)
        PSNR -0.023045   0.115705
     PSNRHVS  0.023327   0.110230
        SSIM -0.039964   0.083594
    FASTSSIM  0.037416  -0.100936
    
    subset1:
    
             RATE (%) DSNR (dB)
        PSNR 0.00000  0.00000
     PSNRHVS 0.00000  0.00000
        SSIM 0.00000  0.00000
    FASTSSIM 0.00000  0.00000
    
    Change-Id: I97c5817463fcd8cb557c403a143b9cfaee4f102c

--- vp10/encoder/encoder.c
+++ vp10/encoder/encoder.c
@@ -323,7 +323,6 @@ void vp10_initialize_enc(void) {
     vp10_init_me_luts();
     vp10_rc_init_minq_luts();
     vp10_entropy_mv_init();
-    vp10_temporal_filter_init();
     vp10_encode_token_init();
     init_done = 1;
   }
