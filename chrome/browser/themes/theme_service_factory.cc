a80b3f4d54655416916a314dad184462f83ee903
wfh <wfh@chromium.org>
2016-03-07 11:38:11 -0800

    Revert of Monitor the system native frame color on Win 10. (patchset #4 id:60001 of https://codereview.chromium.org/1744483002/ )
    Reason for revert:
    This breaks the win10 bot. see bug 592676
    
    Original issue's description:
    > Monitor the system native frame color on Win 10.
    >
    > GetColor(COLOR_FRAME...) will now report the actual in-use color for Win 10
    > native frames.  This is not used yet, but will be useful for:
    > (a) Dynamically choosing the right incognito icon color
    > (b) Dynamically choosing the right COLOR_TOOLBAR_TOP_SEPARATOR
    > (c) Eventually, converting Win 10 to custom-drawing the frame but still
    >     respecting the user's color choice
    >
    > BUG=505013,585470
    > TEST=none
    >
    > Committed: https://crrev.com/57ade888bba31b2b81348029c8d720d1ab80ec33 [formerly 9fcf69e0bc440b16661c39c4b9f13182f0430725]
    > Cr-Commit-Position: refs/heads/master@{#378989}
    
    TBR=estade@chromium.org,bsep@chromium.org,pkasting@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=505013,585470,592676
    
    Review URL: https://codereview.chromium.org/1771933002
    
    Cr-Commit-Position: refs/heads/master@{#379611}
    
    Former-commit-id: 5855bedc3af9746d0a68471f182dcc99bbfa3299

--- chrome/browser/themes/theme_service_factory.cc
+++ chrome/browser/themes/theme_service_factory.cc
@@ -16,9 +16,7 @@
 #include "extensions/browser/extension_registry.h"
 #include "extensions/browser/extension_registry_factory.h"
 
-#if defined(OS_WIN)
-#include "chrome/browser/themes/theme_service_win.h"
-#elif defined(USE_AURA) && defined(USE_X11) && !defined(OS_CHROMEOS)
+#if defined(USE_AURA) && defined(USE_X11) && !defined(OS_CHROMEOS)
 #include "chrome/browser/themes/theme_service_aurax11.h"
 #include "ui/views/linux_ui/linux_ui.h"
 #endif
@@ -57,9 +55,7 @@ ThemeServiceFactory::~ThemeServiceFactory() {}
 KeyedService* ThemeServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* profile) const {
   ThemeService* provider = NULL;
-#if defined(OS_WIN)
-  provider = new ThemeServiceWin;
-#elif defined(USE_AURA) && defined(USE_X11) && !defined(OS_CHROMEOS)
+#if defined(USE_AURA) && defined(USE_X11) && !defined(OS_CHROMEOS)
   provider = new ThemeServiceAuraX11;
 #else
   provider = new ThemeService;
