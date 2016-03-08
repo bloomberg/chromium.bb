29635b35a79bcab8067842de744733f3a0e9fd63
hbos <hbos@chromium.org>
2016-03-08 05:23:43 -0800

    Added H264 to chrome_webrtc_video_quality_browsertest.cc.
    BUG=chromium:500605, chromium:468365
    
    Review URL: https://codereview.chromium.org/1771323002
    
    Cr-Commit-Position: refs/heads/master@{#379819}
    
    Former-commit-id: fd8c149beed9435fa64e82ffeb6b67dae2925594

--- chrome/browser/media/chrome_webrtc_video_quality_browsertest.cc
+++ chrome/browser/media/chrome_webrtc_video_quality_browsertest.cc
@@ -30,6 +30,8 @@
 #include "chrome/test/base/in_process_browser_test.h"
 #include "components/infobars/core/infobar.h"
 #include "content/public/browser/notification_service.h"
+#include "content/public/common/feature_h264_with_openh264_ffmpeg.h"
+#include "content/public/common/features.h"
 #include "content/public/test/browser_test_utils.h"
 #include "media/base/media_switches.h"
 #include "net/test/embedded_test_server/embedded_test_server.h"
@@ -374,3 +376,19 @@ IN_PROC_BROWSER_TEST_P(WebRtcVideoQualityBrowserTest,
                        MANUAL_TestVideoQualityVp9) {
   TestVideoQuality("VP9");
 }
+
+#if BUILDFLAG(RTC_USE_H264)
+
+IN_PROC_BROWSER_TEST_P(WebRtcVideoQualityBrowserTest,
+                       MANUAL_TestVideoQualityH264) {
+  // Only run test if run-time feature corresponding to |rtc_use_h264| is on.
+  if (!base::FeatureList::IsEnabled(content::kWebRtcH264WithOpenH264FFmpeg)) {
+    LOG(WARNING) << "Run-time feature WebRTC-H264WithOpenH264FFmpeg disabled. "
+        "Skipping WebRtcVideoQualityBrowserTest.MANUAL_TestVideoQualityH264 "
+        "(test \"OK\")";
+    return;
+  }
+  TestVideoQuality("H264");
+}
+
+#endif  // BUILDFLAG(RTC_USE_H264)
