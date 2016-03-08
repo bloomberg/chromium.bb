068da034ee613bd7d35c66d31514a56507781087
nyquist <nyquist@chromium.org>
2016-03-07 20:19:47 -0800

    Add Android fonts to blimp engine
    The blimp engine is currently using Linux fonts, which works well
    for Linux clients. However, for Android clients, Skia should be
    instructed to use Android fonts instead to ensure that the font
    rendering works correctly.
    
    This CL adds a starting set of fonts to be used, ensures that they
    are part of the engine bundle, and provides documentation for how
    to use them in locally hosted engines and also for how to keep the
    fonts up to date.
    
    This also updates the engine manifest and starts loading a new
    dynamic library while running the engine plus fixes the documentation
    for running the engine as a container.
    
    BUG=585668, 588929
    
    Review URL: https://codereview.chromium.org/1735783002
    
    Cr-Commit-Position: refs/heads/master@{#379734}
    
    Former-commit-id: 7b1d1d89b247c0bd82d527c6ded5da4128883274

--- blimp/engine/start_engine.sh
+++ blimp/engine/start_engine.sh
@@ -8,11 +8,12 @@ stunnel \
   -p /engine/data/stunnel.pem \
   -P /engine/stunnel.pid \
   -d 25466 -r 25467 -f &
-/engine/blimp_engine_app \
+LD_LIBRARY_PATH=/engine/ /engine/blimp_engine_app \
   --disable-gpu \
   --use-remote-compositing \
   --disable-cached-picture-raster \
   --blimp-client-token-path=/engine/data/client_token \
+  --android-fonts-path=/engine/fonts \
   $@ &
 
 # Stop execution if either stunnel or blimp_engine_app die.
