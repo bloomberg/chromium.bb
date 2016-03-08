6196039380b7d5768848f03b88e4e12b35cb1a48
phajdan.jr@chromium.org <phajdan.jr@chromium.org>
2016-03-08 21:28:48 +0000

    depot_tools: expose path to download_from_google_storage
    BUG=585401
    
    Review URL: https://codereview.chromium.org/1770383004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@299158 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ recipe_modules/depot_tools/api.py
@@ -0,0 +1,10 @@
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from recipe_engine import recipe_api
+
+class DepotToolsApi(recipe_api.RecipeApi):
+  @property
+  def download_from_google_storage_path(self):
+    return self.package_resource('download_from_google_storage.py')
