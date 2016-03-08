f7f0c21e54b663ee0b0942933d88fbeb0a50a2bb
hzl <hzl@google.com>
2016-03-08 14:00:55 -0800

    Raise exception when all devices fail to list tests. When devices fail to list tests, instead of treating the result of list tests, which is None as an empty list, we should raise an exception.
    BUG=585585
    
    Review URL: https://codereview.chromium.org/1716863002
    
    Cr-Commit-Position: refs/heads/master@{#379925}
    
    Former-commit-id: 84802d80ccf691838d7e3d8fcbb3a87ae5e6eca9

--- build/android/pylib/local/device/local_device_gtest_run.py
+++ build/android/pylib/local/device/local_device_gtest_run.py
@@ -315,8 +315,11 @@ class LocalDeviceGtestRun(local_device_test_run.LocalDeviceTestRun):
 
     # Query all devices in case one fails.
     test_lists = self._env.parallel_devices.pMap(list_tests).pGet(None)
-    # TODO(agrieve): Make this fail rather than return an empty list when
-    #     all devices fail.
+
+    # If all devices failed to list tests, raise an exception.
+    if all([tl is None for tl in test_lists]):
+      raise device_errors.CommandFailedError(
+          'Failed to list tests on any device')
     return list(sorted(set().union(*[set(tl) for tl in test_lists if tl])))
 
   #override
