8762a4cb9ed4681f4c25ec8f14962befc6c6d807
wjmaclean <wjmaclean@chromium.org>
2016-03-09 06:13:51 -0800

    Frames received after guest detaches should be displayed.
    In RenderWidgetHostViewGuest::OnSwapCompositorFrame() we currently
    detect if the guest has been detached, and if it has we clear the
    compositor surface and exit without processing the frame we've been
    given.
    
    Based on the repro app in the associated bug, it seems that if this
    happens, it prevents subsequent frames from being sent, presumably
    on account of the most recent frame remaining unsatisfied.
    
    This CL modifies the logic to complete processing of the frame, and
    then clearing the surface. This appears to prevent the behaviour
    observed in the bug.
    
    BUG=581440
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review URL: https://codereview.chromium.org/1772393002
    
    Cr-Commit-Position: refs/heads/master@{#380134}
    
    Former-commit-id: 17f1c1ded552cbcd11cac1b7985593ea2c847808

--- content/browser/frame_host/render_widget_host_view_guest.cc
+++ content/browser/frame_host/render_widget_host_view_guest.cc
@@ -207,12 +207,6 @@ void RenderWidgetHostViewGuest::SetTooltipText(
 void RenderWidgetHostViewGuest::OnSwapCompositorFrame(
     uint32_t output_surface_id,
     scoped_ptr<cc::CompositorFrame> frame) {
-  if (!guest_ || !guest_->attached()) {
-    // We shouldn't hang on to a surface while we are detached.
-    ClearCompositorSurfaceIfNecessary();
-    return;
-  }
-
   last_scroll_offset_ = frame->metadata.root_scroll_offset;
 
   cc::RenderPass* root_pass =
@@ -266,6 +260,12 @@ void RenderWidgetHostViewGuest::OnSwapCompositorFrame(
                                           ack_callback);
 
   ProcessFrameSwappedCallbacks();
+
+  // If after detaching we are sent a frame, we should finish processing it, and
+  // then we should clear the surface so that we are not holding resources we
+  // no longer need.
+  if (!guest_ || !guest_->attached())
+    ClearCompositorSurfaceIfNecessary();
 }
 
 bool RenderWidgetHostViewGuest::OnMessageReceived(const IPC::Message& msg) {
