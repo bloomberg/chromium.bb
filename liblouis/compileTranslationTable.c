fb65ff0d254ef6bbbd600bcc8ae66b376f546b7b
Michael Curran <mick@nvaccess.org>
2016-03-09 14:57:39 +1000

    Stop buffer overrun in lou_getProgramPath, and also free memory after usage.
    As compilation on Windows could  define "UNICODE", GetModuleFilename may provide a unicode path, yet,  further code expects it to be single-byte chars.
    Therefore, explicitly use GetModuleFilenameA.

--- liblouis/compileTranslationTable.c
+++ liblouis/compileTranslationTable.c
@@ -84,7 +84,9 @@ lou_getProgramPath ()
 	  buffer = reallocWrapper (buffer, size <<= 1);
 
 	  {
-	    DWORD length = GetModuleFileName (handle, buffer, size);
+		// As the "UNICODE" Windows define may have been set at compilation,
+		// This call must be specifically GetModuleFilenameA as further code expects it to be single byte chars.
+		DWORD length = GetModuleFileNameA (handle, buffer, size);
 
 	    if (!length)
 	      {
@@ -4552,8 +4554,11 @@ getTablePath()
 		   "tables");
 #ifdef _WIN32
   path = lou_getProgramPath ();
-  if (path != NULL && path[0] != '\0')
-    cp += sprintf (cp, ",%s%s", path, "\\share\\liblouis\\tables");
+  if (path != NULL) {
+    if(path[0] != '\0')
+      cp += sprintf (cp, ",%s%s", path, "\\share\\liblouis\\tables");
+    free(path);
+  }
 #else
   cp += sprintf (cp, ",%s", TABLESDIR);
 #endif
