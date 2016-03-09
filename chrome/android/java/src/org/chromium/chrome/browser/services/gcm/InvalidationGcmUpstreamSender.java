8b0f327f330270f2dd583a381455696c90761f0a
khushalsagar <khushalsagar@chromium.org>
2016-03-08 16:26:31 -0800

    GCM: Fix strict mode violation in InvalidationGcmUpstreamSender.
    The InvalidationGcmUpstreamSender receives the callback for OAuth tokens
    on the UI thread and proceeds to send the upstream message, which can
    trigger reads from disk.
    
    Use an AsyncTask to send the upstream messages after receiving the token.
    
    Review URL: https://codereview.chromium.org/1756523002
    
    Cr-Commit-Position: refs/heads/master@{#379990}
    
    Former-commit-id: 774ff5f8146efc71a3907abd9dafc8b97057fd41

--- chrome/android/java/src/org/chromium/chrome/browser/services/gcm/InvalidationGcmUpstreamSender.java
+++ chrome/android/java/src/org/chromium/chrome/browser/services/gcm/InvalidationGcmUpstreamSender.java
@@ -5,7 +5,10 @@
 package org.chromium.chrome.browser.services.gcm;
 
 import android.accounts.Account;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Parcel;
 import android.util.Log;
 
 import com.google.android.gms.gcm.GoogleCloudMessaging;
@@ -41,13 +44,22 @@ public class InvalidationGcmUpstreamSender extends GcmUpstreamSenderService {
             return;
         }
 
+        final Bundle dataToSend = createDeepCopy(data);
+        final Context applicationContext = getApplicationContext();
+
         // Attempt to retrieve a token for the user.
         OAuth2TokenService.getOAuth2AccessToken(this, account,
                 SyncConstants.CHROME_SYNC_OAUTH2_SCOPE,
                 new AccountManagerHelper.GetAuthTokenCallback() {
                     @Override
-                    public void tokenAvailable(String token) {
-                        sendUpstreamMessage(to, data, token);
+                    public void tokenAvailable(final String token) {
+                        new AsyncTask<Void, Void, Void>() {
+                            @Override
+                            protected Void doInBackground(Void... voids) {
+                                sendUpstreamMessage(to, dataToSend, token, applicationContext);
+                                return null;
+                            }
+                        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
                     }
 
                     @Override
@@ -58,12 +70,14 @@ public class InvalidationGcmUpstreamSender extends GcmUpstreamSenderService {
                 });
     }
 
-    private void sendUpstreamMessage(String to, Bundle data, String token) {
+    /*
+     * This function runs on a thread from the AsyncTask.THREAD_POOL_EXECUTOR.
+     */
+    private void sendUpstreamMessage(String to, Bundle data, String token, Context context) {
         // Add the OAuth2 token to the bundle. The token should have the prefix Bearer added to it.
         data.putString("Authorization", "Bearer " + token);
         if (!isMessageWithinLimit(data)) {
-            GcmUma.recordGcmUpstreamHistogram(
-                    getApplicationContext(), GcmUma.UMA_UPSTREAM_SIZE_LIMIT_EXCEEDED);
+            GcmUma.recordGcmUpstreamHistogram(context, GcmUma.UMA_UPSTREAM_SIZE_LIMIT_EXCEEDED);
             return;
         }
         String msgId = UUID.randomUUID().toString();
@@ -71,8 +85,7 @@ public class InvalidationGcmUpstreamSender extends GcmUpstreamSenderService {
             GoogleCloudMessaging.getInstance(getApplicationContext()).send(to, msgId, 1, data);
         } catch (IOException | IllegalArgumentException exception) {
             Log.w(TAG, "Send message failed");
-            GcmUma.recordGcmUpstreamHistogram(getApplicationContext(),
-                    GcmUma.UMA_UPSTREAM_SEND_FAILED);
+            GcmUma.recordGcmUpstreamHistogram(context, GcmUma.UMA_UPSTREAM_SEND_FAILED);
         }
     }
 
@@ -85,5 +98,17 @@ public class InvalidationGcmUpstreamSender extends GcmUpstreamSenderService {
             return false;
         }
         return true;
+    }
+
+    /*
+     * Creates and returns a deep copy of the original Bundle.
+     */
+    private Bundle createDeepCopy(Bundle original) {
+        Parcel temp = Parcel.obtain();
+        original.writeToParcel(temp, 0);
+        temp.setDataPosition(0);
+        Bundle copy = temp.readBundle();
+        temp.recycle();
+        return copy;
     }
 }
