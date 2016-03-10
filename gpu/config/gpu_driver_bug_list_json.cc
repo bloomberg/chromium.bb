33a6e452edfcdd5a26e1d746508e23d7b7526666
jbauman <jbauman@chromium.org>
2016-03-09 19:53:02 -0800

    Disable DirectComposition on Windows <10
    On Windows 8 and 8.1 the DirectComposition content doesn't seem to
    appear until after the window does its initial show animation. However
    the GDI content behind it still updates, which causes the window to
    flash black for a few frames when it's first created.
    
    This seems to be fixed on Windows 10, but before that there doesn't seem
    to be a good workaround. So for now we'll disable DirectComposition on
    Windows 8 and 8.1.
    
    BUG=593274,588588
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1778593004
    
    Cr-Commit-Position: refs/heads/master@{#380326}
    
    Former-commit-id: fdcc8842ff96722b79c1569156baf326147ef1cb

--- gpu/config/gpu_driver_bug_list_json.cc
+++ gpu/config/gpu_driver_bug_list_json.cc
@@ -19,7 +19,7 @@ const char kGpuDriverBugListJson[] = LONG_STRING_CONST(
 {
   "name": "gpu driver bug list",
   // Please update the version number whenever you change this file.
-  "version": "8.47",
+  "version": "8.48",
   "entries": [
     {
       "id": 1,
@@ -1779,6 +1779,21 @@ LONG_STRING_CONST(
       "gl_renderer": ".*Mali-4.*",
       "features": [
         "surface_texture_cant_detach"
+      ]
+    },
+    {
+      "id": 149,
+      "description": "Direct composition flashes black initially on Win <10",
+      "cr_bugs": [588588],
+      "os": {
+        "type": "win",
+        "version": {
+          "op": "<",
+          "value": "10.0"
+        }
+      },
+      "features": [
+        "disable_direct_composition"
       ]
     }
   ]
