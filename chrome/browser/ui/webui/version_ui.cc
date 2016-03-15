a1432a55ade88faff34c82d35c3c0d51f2b40d0d
wfh <wfh@chromium.org>
2016-03-14 19:44:49 -0700

    Output the version of MSVS in chrome://version
    BUG=440500
    
    Review URL: https://codereview.chromium.org/1800773002
    
    Cr-Commit-Position: refs/heads/master@{#381159}
    
    Former-commit-id: 30ff76ae0a9bd4f054e51520d19453420d534abf

--- chrome/browser/ui/webui/version_ui.cc
+++ chrome/browser/ui/webui/version_ui.cc
@@ -139,10 +139,16 @@ WebUIDataSource* CreateVersionUIDataSource() {
 #if defined(OS_WIN)
 #if defined(__clang__)
   html_source->AddString("compiler", "clang");
+#elif defined(_MSC_VER) && _MSC_VER == 1900
+  html_source->AddString("compiler", "MSVC 2015");
+#elif defined(_MSC_VER) && _MSC_VER == 1800
+  html_source->AddString("compiler", "MSVC 2013");
+#elif defined(_MSC_VER)
+#error "Unsupported version of MSVC."
 #else
-  html_source->AddString("compiler", "MSVC");
+  html_source->AddString("compiler", "Unknown");
 #endif
-#endif
+#endif  // defined(OS_WIN)
 
   html_source->SetJsonPath("strings.js");
   html_source->AddResourcePath(version_ui::kVersionJS, IDR_VERSION_UI_JS);
