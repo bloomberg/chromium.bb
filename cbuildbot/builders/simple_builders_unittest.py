5851d2018a268db6ec8450abd453f59d773f8e12
Chris Sosa <sosa@chromium.org>
2016-03-15 15:04:10 -0700

    Re-land: CBuildbot: fix and improve documentation for HWTestConfig blocking behavior.
    The concept of blocking was introduced to allow some suites
    to block whether or not we run others. This was mis-used in a few
    places and so I've gone ahead and fleshed out documentation to be
    better.
    
    Outside of documentation, it also didn't make sense when some tests were
    scheduled before (but not blocking) and after as it always ran
    blocking suites first (which wasn't documented). Instead I've changed
    and documented blocking behavior to be a join which allows more
    flexible scheduling.
    
    BUG=chromium:594315
    TEST=Unitests + Additional unittest to catch original regression with blocking
    
    Change-Id: Iafe68108c821313a624dde4a8499b5f2f7f10a89
    Reviewed-on: https://chromium-review.googlesource.com/332861
    Commit-Ready: Chris Sosa <sosa@chromium.org>
    Tested-by: Chris Sosa <sosa@chromium.org>
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>

--- cbuildbot/builders/simple_builders_unittest.py
+++ cbuildbot/builders/simple_builders_unittest.py
@@ -1,7 +1,6 @@
 # Copyright 2015 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-
 """Unittests for simpler builders."""
 
 from __future__ import print_function
@@ -17,6 +16,7 @@ from chromite.cbuildbot.builders import simple_builders
 from chromite.lib import cros_test_lib
 from chromite.lib import osutils
 from chromite.lib import parallel
+from chromite.lib import parallel_unittest
 from chromite.scripts import cbuildbot
 
 
@@ -27,12 +27,31 @@ class SimpleBuilderTest(cros_test_lib.MockTempDirTestCase):
   """Tests for the main code paths in simple_builders.SimpleBuilder"""
 
   def setUp(self):
+    # List of all stages that would have been called as part of this run.
+    self.called_stages = []
+
+    # Simple new function that redirects RunStage to record all stages to be
+    # run rather than mock them completely. These can be used in a test to
+    # assert something has been called.
+    def run_stage(_class_instance, stage_name, *_args, **_kwargs):
+      self.called_stages.append(stage_name)
+
+    # Parallel version.
+    def run_parallel_stages(_class_instance, stages_list):
+      for stage in stages_list:
+        self.called_stages.append(type(stage))
+
     self.buildroot = os.path.join(self.tempdir, 'buildroot')
     chroot_path = os.path.join(self.buildroot, constants.DEFAULT_CHROOT_DIR)
     osutils.SafeMakedirs(os.path.join(chroot_path, 'tmp'))
 
-    self.PatchObject(generic_builders.Builder, '_RunStage')
-    self.PatchObject(simple_builders.SimpleBuilder, '_RunParallelStages')
+    # Parallel mock is used since some work in simple builders get done on a
+    # separate process.
+    self.StartPatcher(parallel_unittest.ParallelMock())
+    self.PatchObject(generic_builders.Builder, '_RunStage',
+                     new=run_stage)
+    self.PatchObject(simple_builders.SimpleBuilder, '_RunParallelStages',
+                     new=run_parallel_stages)
     self.PatchObject(cbuildbot_run._BuilderRunBase, 'GetVersion',
                      return_value='R32-1234.0.0')
 
@@ -43,7 +62,7 @@ class SimpleBuilderTest(cros_test_lib.MockTempDirTestCase):
     # Mimic exiting a 'with' statement.
     self._manager.__exit__(None, None, None)
 
-  def _initConfig(self, bot_id, extra_argv=None):
+  def _initConfig(self, bot_id, extra_argv=None, override_hw_test_config=None):
     """Return normal options/build_config for |bot_id|"""
     site_config = chromeos_config.GetConfig()
     build_config = copy.deepcopy(site_config[bot_id])
@@ -58,6 +77,12 @@ class SimpleBuilderTest(cros_test_lib.MockTempDirTestCase):
 
     # Yikes.
     options.managed_chrome = build_config['sync_chrome']
+
+    # Iterate through override and update HWTestConfig attributes.
+    if override_hw_test_config:
+      for key, val in override_hw_test_config.iteritems():
+        for hw_test in build_config.hw_tests:
+          setattr(hw_test, key, val)
 
     return cbuildbot_run.BuilderRun(
         options, site_config, build_config, self._manager)
@@ -98,3 +123,26 @@ class SimpleBuilderTest(cros_test_lib.MockTempDirTestCase):
     builder_run = self._initConfig('lumpy-release', extra_argv=extra_argv)
     builder_run.attrs.chrome_version = 'TheChromeVersion'
     simple_builders.SimpleBuilder(builder_run).RunStages()
+
+  def testThatWeScheduleHWTestsRegardlessOfBlocking(self):
+    """Verify RunStages for boards w/hwtests (blocking).
+
+    Make sure the same stages get scheduled regardless of whether their hwtest
+    suites are marked blocking or not.
+    """
+    extra_argv = ['--hwtest']
+    builder_run_without_blocking = self._initConfig(
+        'lumpy-release', extra_argv=extra_argv,
+        override_hw_test_config=dict(blocking=False))
+    builder_run_with_blocking = self._initConfig(
+        'lumpy-release', extra_argv=extra_argv,
+        override_hw_test_config=dict(blocking=True))
+    builder_run_without_blocking.attrs.chrome_version = 'TheChromeVersion'
+    builder_run_with_blocking.attrs.chrome_version = 'TheChromeVersion'
+
+    simple_builders.SimpleBuilder(builder_run_without_blocking).RunStages()
+    without_blocking_stages = list(self.called_stages)
+
+    self.called_stages = []
+    simple_builders.SimpleBuilder(builder_run_with_blocking).RunStages()
+    self.assertEqual(without_blocking_stages, self.called_stages)
