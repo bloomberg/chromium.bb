869c00870b044d655d2ccdf44622a67ba6511e43
mattcary <mattcary@chromium.org>
2016-03-16 06:06:46 -0700

    tools/android/loading: Remove spaces from network emulation names to make scriptability easier.
    Review URL: https://codereview.chromium.org/1802743003
    
    Cr-Commit-Position: refs/heads/master@{#381439}
    
    Former-commit-id: e5aa4f8b6b4b06e0a163098f31748d048ff50b98

--- tools/android/loading/emulation.py
+++ tools/android/loading/emulation.py
@@ -14,16 +14,16 @@ import json
 NETWORK_CONDITIONS = {
     'GPRS': {
         'download': 50 * 1024 / 8, 'upload': 20 * 1024 / 8, 'latency': 500},
-    'Regular 2G': {
+    'Regular2G': {
         'download': 250 * 1024 / 8, 'upload': 50 * 1024 / 8, 'latency': 300},
-    'Good 2G': {
+    'Good2G': {
         'download': 450 * 1024 / 8, 'upload': 150 * 1024 / 8, 'latency': 150},
-    'Regular 3G': {
+    'Regular3G': {
         'download': 750 * 1024 / 8, 'upload': 250 * 1024 / 8, 'latency': 100},
-    'Good 3G': {
+    'Good3G': {
         'download': 1.5 * 1024 * 1024 / 8, 'upload': 750 * 1024 / 8,
         'latency': 40},
-    'Regular 4G': {
+    'Regular4G': {
         'download': 4 * 1024 * 1024 / 8, 'upload': 3 * 1024 * 1024 / 8,
         'latency': 20},
     'DSL': {
