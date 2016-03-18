79cb9805c1c5aa08d99e423eb93fb24d251be0d9
Tom Finegan <tomfinegan@google.com>
2016-03-18 11:35:31 -0700

    Android.mk: Update source file locations.
    Change-Id: I92cdf3c5616edf0450bce85963f415e10c5cb21d

--- Android.mk
+++ Android.mk
@@ -2,9 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE:= libwebm
-LOCAL_SRC_FILES:= mkvparser.cpp \
-                  mkvreader.cpp \
-                  mkvmuxer.cpp \
-                  mkvmuxerutil.cpp \
-                  mkvwriter.cpp
+LOCAL_SRC_FILES:= common/file_util.cc \
+                  common/hdr_util.cc \
+                  mkvparser/mkvparser.cpp \
+                  mkvparser/mkvreader.cpp \
+                  mkvmuxer/mkvmuxer.cpp \
+                  mkvmuxer/mkvmuxerutil.cpp \
+                  mkvmuxer/mkvwriter.cpp
 include $(BUILD_STATIC_LIBRARY)
