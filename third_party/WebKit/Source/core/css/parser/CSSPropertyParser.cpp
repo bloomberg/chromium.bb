f0ab9e84dffccf4d137472072ca846b4d7e017a2
rob.buis <rob.buis@samsung.com>
2016-03-17 18:20:41 -0700

    Make parsing of <transform> more strict
    Make parsing of <transform> more strict by checking
    that something valid follows after a comma.
    
    BUG=593321
    
    Review URL: https://codereview.chromium.org/1811953002
    
    Cr-Commit-Position: refs/heads/master@{#381864}
    
    Former-commit-id: 04dfd7b740446fe6cead94132546d4dfdb69c2e8

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParser.cpp
@@ -1807,6 +1807,8 @@ static PassRefPtrWillBeRawPtr<CSSValue> consumeTransformValue(CSSParserTokenRang
         if (functionId == CSSValueSkew && consumeCommaIncludingWhitespace(args)) {
             transformValue->append(parsedValue);
             parsedValue = consumeAngle(args);
+            if (!parsedValue)
+                return nullptr;
         }
         break;
     case CSSValueScaleX:
@@ -1819,6 +1821,8 @@ static PassRefPtrWillBeRawPtr<CSSValue> consumeTransformValue(CSSParserTokenRang
         if (functionId == CSSValueScale && consumeCommaIncludingWhitespace(args)) {
             transformValue->append(parsedValue);
             parsedValue = consumeNumber(args, ValueRangeAll);
+            if (!parsedValue)
+                return nullptr;
         }
         break;
     case CSSValuePerspective:
@@ -1834,6 +1838,8 @@ static PassRefPtrWillBeRawPtr<CSSValue> consumeTransformValue(CSSParserTokenRang
         if (functionId == CSSValueTranslate && consumeCommaIncludingWhitespace(args)) {
             transformValue->append(parsedValue);
             parsedValue = consumeLengthOrPercent(args, cssParserMode, ValueRangeAll);
+            if (!parsedValue)
+                return nullptr;
         }
         break;
     case CSSValueTranslateZ:
@@ -1852,6 +1858,8 @@ static PassRefPtrWillBeRawPtr<CSSValue> consumeTransformValue(CSSParserTokenRang
         if (!consumeNumbers(args, transformValue, 3) || !consumeCommaIncludingWhitespace(args))
             return nullptr;
         parsedValue = consumeAngle(args);
+        if (!parsedValue)
+            return nullptr;
         break;
     case CSSValueTranslate3d:
         if (!consumeTranslate3d(args, cssParserMode, transformValue))
