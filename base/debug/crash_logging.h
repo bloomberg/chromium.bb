28c4a88721231555ac7561c8b54ad8ec70b9de36
eroman <eroman@chromium.org>
2016-03-18 15:02:39 -0700

    Adjust some comments in crash_logging.h.
    Review URL: https://codereview.chromium.org/1818583002
    
    Cr-Commit-Position: refs/heads/master@{#382105}
    
    Former-commit-id: a69dd91854350b4a9ab39e73bbd32f7902e73433

--- base/debug/crash_logging.h
+++ base/debug/crash_logging.h
@@ -25,7 +25,7 @@ namespace debug {
 
 class StackTrace;
 
-// Set or clear a specific key-value pair from the crash metadata. Keys and
+// Sets or clears a specific key-value pair from the crash metadata. Keys and
 // values are terminated at the null byte.
 BASE_EXPORT void SetCrashKeyValue(const base::StringPiece& key,
                                   const base::StringPiece& value);
@@ -75,11 +75,11 @@ struct BASE_EXPORT CrashKey {
 BASE_EXPORT size_t InitCrashKeys(const CrashKey* const keys, size_t count,
                                  size_t chunk_max_length);
 
-// Returns the correspnding crash key object or NULL for a given key.
+// Returns the corresponding crash key object or NULL for a given key.
 BASE_EXPORT const CrashKey* LookupCrashKey(const base::StringPiece& key);
 
 // In the platform crash reporting implementation, these functions set and
-// clear the NUL-termianted key-value pairs.
+// clear the NUL-terminated key-value pairs.
 typedef void (*SetCrashKeyValueFuncT)(const base::StringPiece&,
                                       const base::StringPiece&);
 typedef void (*ClearCrashKeyValueFuncT)(const base::StringPiece&);
