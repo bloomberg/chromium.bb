df67865e91ef94dddd705be0a1b4fedf7fe488d2
calamity <calamity@chromium.org>
2016-03-21 01:08:53 -0700

    Add a 100 bucket to the EngagementScoreBuckets histogram.
    BUG=464234
    
    Review URL: https://codereview.chromium.org/1798333002
    
    Cr-Commit-Position: refs/heads/master@{#382255}
    
    Former-commit-id: 04d576377538c83eb9cccac0c71cc47ab8bdf9d9

--- chrome/browser/engagement/site_engagement_metrics.cc
+++ chrome/browser/engagement/site_engagement_metrics.cc
@@ -13,8 +13,8 @@ namespace {
 // SiteEngagementService.EngagementScoreBucket_* histogram. If these bases
 // change, the EngagementScoreBuckets suffix in histograms.xml should be
 // updated.
-const int kEngagementBucketHistogramBuckets[] = {0,  10, 20, 30, 40,
-                                                 50, 60, 70, 80, 90};
+const int kEngagementBucketHistogramBuckets[] = {0,  10, 20, 30, 40, 50,
+                                                 60, 70, 80, 90, 100};
 
 }  // namespace
 
