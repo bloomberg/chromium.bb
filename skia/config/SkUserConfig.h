34d3996cb1f25667659aeeec03c84357bfed0c65
reed <reed@google.com>
2016-03-21 20:56:20 -0700

    add SK_SUPPORT_LEGACY_NEW_SURFACE_API flag for future skia CL
    BUG=
    
    TBR=fmalita
    
    Review URL: https://codereview.chromium.org/1823003002
    
    Cr-Commit-Position: refs/heads/master@{#382503}
    
    Former-commit-id: 4582692c7e665a2d96f22b13764a5b42fd92740a

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -243,6 +243,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_IMAGEFACTORY
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_NEW_SURFACE_API
+#   define SK_SUPPORT_LEGACY_NEW_SURFACE_API
+#endif
+
 #ifndef    SK_SUPPORT_LEGACY_EVAL_CUBIC
 #   define SK_SUPPORT_LEGACY_EVAL_CUBIC
 #endif
