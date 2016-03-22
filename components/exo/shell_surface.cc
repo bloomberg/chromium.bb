d7829f7a3c9ae57a69fbf805df3738dd9c90d823
reveman <reveman@chromium.org>
2016-03-22 15:11:43 -0700

    exo: Fix DCHECK in ShellSurface::OnWindowActivated.
    This DCHECK is only valid if gained_active or lost_active
    equals the native window.
    
    BUG=549781
    TEST=exo_unittests
    TBR=dcastagna@chromium.org
    
    Review URL: https://codereview.chromium.org/1823913004
    
    Cr-Commit-Position: refs/heads/master@{#382705}
    
    Former-commit-id: 799324ac96cb110489e0b58e367615779a256095

--- components/exo/shell_surface.cc
+++ components/exo/shell_surface.cc
@@ -403,13 +403,12 @@ void ShellSurface::OnWindowActivated(
     aura::client::ActivationChangeObserver::ActivationReason reason,
     aura::Window* gained_active,
     aura::Window* lost_active) {
-  DCHECK(activatable_);
-
   if (!widget_)
     return;
 
   if (gained_active == widget_->GetNativeWindow() ||
       lost_active == widget_->GetNativeWindow()) {
+    DCHECK(activatable_);
     Configure();
   }
 }
