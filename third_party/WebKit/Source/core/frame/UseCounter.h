e141434ebe468275a6a57147f3366de6128b57da
jsbell <jsbell@chromium.org>
2016-03-21 23:27:56 -0700

    Measure usage of IDBFactory.webkitGetDatabaseNames()
    Non-standard method; it would be nice to deprecate/remove this if usage
    is low enough, or push for standardization if usage is too high.
    
    BUG=596543
    R=mpearson@chromium.com,philipj@opera.com
    
    Review URL: https://codereview.chromium.org/1819943002
    
    Cr-Commit-Position: refs/heads/master@{#382517}
    
    Former-commit-id: a7608cf660f98528d51e73ad780b774b67258cd1

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -1089,6 +1089,7 @@ public:
         V8Window_WebkitSpeechRecognitionError_ConstructorGetter = 1270,
         V8Window_WebkitSpeechRecognitionEvent_ConstructorGetter = 1271,
         V8Window_SpeechSynthesis_AttributeGetter = 1272,
+        V8IDBFactory_WebkitGetDatabaseNames_Method = 1273,
 
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
