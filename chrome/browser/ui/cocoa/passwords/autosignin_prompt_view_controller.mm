bb07c941dd0b4d27a52d942ffc3a772547f905f5
vasilii <vasilii@chromium.org>
2016-03-23 07:16:43 -0700

    Increase the font of the buttons in the first run autosign-in promo.
    BUG=550906
    
    Review URL: https://codereview.chromium.org/1822293003
    
    Cr-Commit-Position: refs/heads/master@{#382845}
    
    Former-commit-id: f6f9a197f2969a05fd812c08a2dae264f52707d4

--- chrome/browser/ui/cocoa/passwords/autosignin_prompt_view_controller.mm
+++ chrome/browser/ui/cocoa/passwords/autosignin_prompt_view_controller.mm
@@ -14,6 +14,24 @@
 #include "ui/base/cocoa/controls/hyperlink_text_view.h"
 #include "ui/base/l10n/l10n_util.h"
 
+namespace {
+
+// Returns a NSRegularControlSize button. It's used for improving the contrast
+// due to Accessabilty standards.
+NSButton* BiggerDialogButton(NSString* title) {
+  base::scoped_nsobject<NSButton> button(
+      [[NSButton alloc] initWithFrame:NSZeroRect]);
+  CGFloat fontSize = [NSFont systemFontSizeForControlSize:NSRegularControlSize];
+  [button setFont:[NSFont systemFontOfSize:fontSize]];
+  [button setTitle:title];
+  [button setBezelStyle:NSRoundedBezelStyle];
+  [[button cell] setControlSize:NSRegularControlSize];
+  [button sizeToFit];
+  return button.autorelease();
+}
+
+}  // namespace
+
 @interface AutoSigninPromptViewController () {
   NSButton* _okButton;
   NSButton* _turnOffButton;
@@ -66,14 +84,14 @@
 
   // Buttons.
   _okButton =
-      DialogButton(l10n_util::GetNSString(IDS_AUTO_SIGNIN_FIRST_RUN_OK));
+      BiggerDialogButton(l10n_util::GetNSString(IDS_AUTO_SIGNIN_FIRST_RUN_OK));
   [_okButton setTarget:self];
   [_okButton setAction:@selector(onOkClicked:)];
   [_okButton setKeyEquivalent:@"\r"];
   [view addSubview:_okButton];
 
-  _turnOffButton =
-      DialogButton(l10n_util::GetNSString(IDS_AUTO_SIGNIN_FIRST_RUN_TURN_OFF));
+  _turnOffButton = BiggerDialogButton(
+       l10n_util::GetNSString(IDS_AUTO_SIGNIN_FIRST_RUN_TURN_OFF));
   [_turnOffButton setTarget:self];
   [_turnOffButton setAction:@selector(onTurnOffClicked:)];
   [view addSubview:_turnOffButton];
