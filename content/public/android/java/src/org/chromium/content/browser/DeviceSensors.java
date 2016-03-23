a857296fc35953f0a98fa7a7dbe0cbd327bb601b
myrth <myrth@yandex-team.ru>
2016-03-23 01:47:30 -0700

    Suppress exception on unregistering device sensor.
    Digma tablets iDxD7/iDxD10 tend to throw exceptions
    on sensor unregistering. I believe this is a firmware
    issue. Suppress this error since it's ok to move on,
    if the sensor is already unregistered.
    
    Similar crash in mozilla tracker:
    https://goo.gl/iD8sRb
    Note the device list out there, it also mentions
    iDxD* devices, along with some others.
    
    R=mvanouwerkerk@chromium.org,timvolodine@chromium.org
    BUG=596453
    
    Review URL: https://codereview.chromium.org/1819293002
    
    Cr-Commit-Position: refs/heads/master@{#382810}
    
    Former-commit-id: a82bdf8c0f978ea78873ccebf45ea64c3e0a7d94

--- content/public/android/java/src/org/chromium/content/browser/DeviceSensors.java
+++ content/public/android/java/src/org/chromium/content/browser/DeviceSensors.java
@@ -686,8 +686,16 @@ class DeviceSensors implements SensorEventListener {
         @Override
         public void unregisterListener(SensorEventListener listener, int sensorType) {
             List<Sensor> sensors = mSensorManager.getSensorList(sensorType);
-            if (!sensors.isEmpty()) {
+            if (sensors.isEmpty()) {
+                return;
+            }
+            try {
                 mSensorManager.unregisterListener(listener, sensors.get(0));
+            } catch (IllegalArgumentException e) {
+                // Suppress occasional exception on Digma iDxD* devices:
+                // Receiver not registered: android.hardware.SystemSensorManager$1
+                // See crbug.com/596453.
+                Log.w(TAG, "Failed to unregister device sensor " + sensors.get(0).getName());
             }
         }
     }
