0063f7b9b5f23bb75d669a4f13721795466049a7
ikilpatrick <ikilpatrick@chromium.org>
2016-03-22 21:11:54 -0700

    Implement PaintRenderingContext2D off BaseRenderingContext2D
    This exposes the subset of the API which we'll need for csspaint.
    https://drafts.css-houdini.org/css-paint-api/#paintrenderingcontext2d
    
    Most of the code is just a straight pass-through to the BaseRenderingContext2D.
    
    BUG=578252
    
    Review URL: https://codereview.chromium.org/1778413003
    
    Cr-Commit-Position: refs/heads/master@{#382788}
    
    Former-commit-id: 9bb8650f88cf0cf08861d3a3351214183b3dc64a

---
+++ third_party/WebKit/Source/modules/csspaint/PaintRenderingContext2D.h
@@ -0,0 +1,76 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef PaintRenderingContext2D_h
+#define PaintRenderingContext2D_h
+
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "modules/ModulesExport.h"
+#include "modules/canvas2d/BaseRenderingContext2D.h"
+#include "third_party/skia/include/core/SkCanvas.h"
+
+namespace blink {
+
+class CanvasImageSource;
+class Color;
+class ImageBuffer;
+
+class MODULES_EXPORT PaintRenderingContext2D : public BaseRenderingContext2D, public RefCountedWillBeGarbageCollectedFinalized<PaintRenderingContext2D>, public ScriptWrappable {
+    DEFINE_WRAPPERTYPEINFO();
+    WILL_BE_USING_GARBAGE_COLLECTED_MIXIN(PaintRenderingContext2D);
+    WTF_MAKE_NONCOPYABLE(PaintRenderingContext2D);
+    USING_FAST_MALLOC_WILL_BE_REMOVED(PaintRenderingContext2D);
+public:
+    static PassRefPtrWillBeRawPtr<PaintRenderingContext2D> create(PassOwnPtr<ImageBuffer> imageBuffer)
+    {
+        return adoptRefWillBeNoop(new PaintRenderingContext2D(imageBuffer));
+    }
+
+    // BaseRenderingContext2D
+
+    // PaintRenderingContext2D doesn't have any pixel readback so the origin
+    // is always clean, and unable to taint it.
+    bool originClean() const final { return true; }
+    void setOriginTainted() final { }
+    bool wouldTaintOrigin(CanvasImageSource*) final { return false; }
+
+    int width() const final;
+    int height() const final;
+
+    bool hasImageBuffer() const final { return m_imageBuffer; }
+    ImageBuffer* imageBuffer() const final { return m_imageBuffer.get(); }
+
+    bool parseColorOrCurrentColor(Color&, const String& colorString) const final;
+
+    SkCanvas* drawingCanvas() const final;
+    SkCanvas* existingDrawingCanvas() const final;
+    void disableDeferral(DisableDeferralReason) final { }
+
+    AffineTransform baseTransform() const final;
+
+    void didDraw(const SkIRect& dirtyRect) final;
+
+    // TODO(ikilpatrick): We'll need to either only accept resolved filters
+    // from a typed-om <filter> object, or use the appropriate style resolution
+    // host to determine 'em' units etc in filters. At the moment just pretend
+    // that we don't have a filter set.
+    bool stateHasFilter() final { return false; }
+    SkImageFilter* stateGetFilter() final { return nullptr; }
+
+    void validateStateStack() final;
+
+    bool hasAlpha() const final { return true; }
+
+    // PaintRenderingContext2D cannot lose it's context.
+    bool isContextLost() const final { return false; }
+
+private:
+    explicit PaintRenderingContext2D(PassOwnPtr<ImageBuffer>);
+
+    OwnPtr<ImageBuffer> m_imageBuffer;
+};
+
+} // namespace blink
+
+#endif // PaintRenderingContext2D_h
