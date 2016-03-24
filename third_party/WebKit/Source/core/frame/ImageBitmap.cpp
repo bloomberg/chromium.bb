1fffed1f0a2ae1fc9d80c3a2730a5522eb01f97a
xidachen <xidachen@chromium.org>
2016-03-23 18:00:34 -0700

    Use preroll to force image-decoding
    In ImageBitmap::cropImage, we use readPixels to read a 1*1 subregion to
    force image-decoding to happen. Instead we should use preroll as pointed
    out in here:
    https://codereview.chromium.org/1800153003/
    
    Review URL: https://codereview.chromium.org/1824413002
    
    Cr-Commit-Position: refs/heads/master@{#382998}
    
    Former-commit-id: 721b0e4ad853fc754a1242da323dec4d84ac94e7

--- third_party/WebKit/Source/core/frame/ImageBitmap.cpp
+++ third_party/WebKit/Source/core/frame/ImageBitmap.cpp
@@ -136,12 +136,8 @@ static PassRefPtr<StaticBitmapImage> cropImage(Image* image, const IntRect& crop
         if (flipY)
             return StaticBitmapImage::create(flipSkImageVertically(skiaImage->newSubset(srcRect), premultiplyAlpha ? PremultiplyAlpha : DontPremultiplyAlpha));
         SkImage* croppedSkImage = skiaImage->newSubset(srcRect);
-        if (cropRect == imgRect) {
-            // Call readPixels to trigger image decoding. Only read 1*1 subregion to be efficient.
-            SkImageInfo info = SkImageInfo::MakeN32(1, 1, premultiplyAlpha ? kPremul_SkAlphaType : kUnpremul_SkAlphaType);
-            OwnPtr<uint8_t[]> imagePixels = adoptArrayPtr(new uint8_t[info.bytesPerPixel()]);
-            croppedSkImage->readPixels(info, imagePixels.get(), info.bytesPerPixel(), 0, 0);
-        }
+        // Call preroll to trigger image decoding.
+        croppedSkImage->preroll();
         return StaticBitmapImage::create(adoptRef(croppedSkImage));
     }
 
