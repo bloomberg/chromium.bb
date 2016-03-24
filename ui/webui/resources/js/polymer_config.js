9aa4e3c44fd5ddc1c4499b25c46d2b72418f3c73
michaelpg <michaelpg@chromium.org>
2016-03-24 11:32:48 -0700

    Remove deprecated polymer_config.html
    This is a follow-up to http://crrev.com/1767913002.
    
    Review URL: https://codereview.chromium.org/1830753003
    
    Cr-Commit-Position: refs/heads/master@{#383102}
    
    Former-commit-id: f2f9cbfb9595be07c31f95393f06c522af36b929

--- ui/webui/resources/js/polymer_config.js
+++ ui/webui/resources/js/polymer_config.js
@@ -2,9 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-if (typeof Polymer == 'undefined') {
+if (typeof Polymer == 'undefined')
   Polymer = {dom: 'shadow'};
-} else {
-  // TODO(michaelpg): Change back to error once polymer_config.html is removed.
-  console.warn('Polymer is already defined.');
-}
+else
+  console.error('Polymer is already defined.');
