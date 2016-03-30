6481c24a19c9a36eae8a97506dafcf606cf35fc0
Tom Finegan <tomfinegan@google.com>
2016-03-30 10:51:40 -0700

    webvtt: Fix include in vttreader.
    Change-Id: I270acebca3e38114362af4d682d5034fda942c84

--- webvtt/vttreader.cc
+++ webvtt/vttreader.cc
@@ -6,7 +6,7 @@
 // in the file PATENTS.  All contributing project authors may
 // be found in the AUTHORS file in the root of the source tree.
 
-#include "./vttreader.h"  // NOLINT
+#include "webvtt/vttreader.h"
 
 namespace libwebvtt {
 
