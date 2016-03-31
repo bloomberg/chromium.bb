2d7d31528c4948751d81d7fb2e0f8a6254f00ffe
lambroslambrou <lambroslambrou@chromium.org>
2016-03-31 14:11:07 -0700

    [remoting android] Add new remoting_host_apk build target
    This adds a new target which builds a bare-bones APK.
    
    Review URL: https://codereview.chromium.org/1836173002
    
    Cr-Commit-Position: refs/heads/master@{#384387}
    
    Former-commit-id: e305fd56b4800b39cab3dcb4502c0f491b4d5e78

---
+++ remoting/android/host/src/org/chromium/chromoting/host/MainActivity.java
@@ -0,0 +1,24 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chromoting.host;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Main screen of the Chromoting Host application.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
