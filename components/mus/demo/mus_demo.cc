f484e95a6cfc3044b1664679894d7276ac663f76
kylechar <kylechar@chromium.org>
2016-03-31 06:11:35 -0700

    Add mus_demo mojo app.
    Simple mojo app that connects to mus, creates a new window and draws
    into the window. Draws a spinning square.
    
    Provides a simple way to test mus graphics output without the complexity
    of starting mash.
    
    BUG=599162
    
    Review URL: https://codereview.chromium.org/1832943002
    
    Cr-Commit-Position: refs/heads/master@{#384259}
    
    Former-commit-id: bd601e5a5d235c1c933190286c04db14b458971e

---
+++ components/mus/demo/mus_demo.cc
@@ -0,0 +1,159 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/mus/demo/mus_demo.h"
+
+#include "base/time/time.h"
+#include "components/bitmap_uploader/bitmap_uploader.h"
+#include "components/mus/public/cpp/window_tree_host_factory.h"
+#include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkColor.h"
+#include "third_party/skia/include/core/SkImageInfo.h"
+#include "third_party/skia/include/core/SkPaint.h"
+#include "third_party/skia/include/core/SkRect.h"
+#include "ui/gfx/geometry/rect.h"
+
+namespace mus_demo {
+
+namespace {
+
+// Milliseconds between frames.
+const int64_t kFrameDelay = 33;
+
+// Size of square in pixels to draw.
+const int kSquareSize = 300;
+
+const SkColor kBgColor = SK_ColorRED;
+const SkColor kFgColor = SK_ColorYELLOW;
+
+void DrawSquare(const gfx::Rect& bounds, double angle, SkCanvas* canvas) {
+  // Create SkRect to draw centered inside the bounds.
+  gfx::Point top_left = bounds.CenterPoint();
+  top_left.Offset(-kSquareSize / 2, -kSquareSize / 2);
+  SkRect rect =
+      SkRect::MakeXYWH(top_left.x(), top_left.y(), kSquareSize, kSquareSize);
+
+  // Set SkPaint to fill solid color.
+  SkPaint paint;
+  paint.setStyle(SkPaint::kFill_Style);
+  paint.setColor(kFgColor);
+
+  // Rotate the canvas.
+  const gfx::Size canvas_size = bounds.size();
+  if (angle != 0.0) {
+    canvas->translate(SkFloatToScalar(canvas_size.width() * 0.5f),
+                      SkFloatToScalar(canvas_size.height() * 0.5f));
+    canvas->rotate(angle);
+    canvas->translate(-SkFloatToScalar(canvas_size.width() * 0.5f),
+                      -SkFloatToScalar(canvas_size.height() * 0.5f));
+  }
+
+  canvas->drawRect(rect, paint);
+}
+
+}  // namespace
+
+MusDemo::MusDemo() {}
+
+MusDemo::~MusDemo() {}
+
+void MusDemo::Initialize(mojo::Connector* connector,
+                         const mojo::Identity& identity,
+                         uint32_t id) {
+  connector_ = connector;
+
+  CreateWindowTreeHost(connector_, this, &window_tree_host_, this);
+  window_tree_host_->SetTitle("MUS Demo");
+}
+
+bool MusDemo::AcceptConnection(mojo::Connection* connection) {
+  return true;
+}
+
+void MusDemo::OnEmbed(mus::Window* window) {
+  window_ = window;
+
+  // Initialize bitmap uploader for sending frames to MUS.
+  uploader_.reset(new bitmap_uploader::BitmapUploader(window_));
+  uploader_->Init(connector_);
+
+  // Draw initial frame and start the timer to regularly draw frames.
+  DrawFrame();
+  timer_.Start(FROM_HERE, base::TimeDelta::FromMilliseconds(kFrameDelay),
+               base::Bind(&MusDemo::DrawFrame, base::Unretained(this)));
+}
+
+void MusDemo::OnUnembed(mus::Window* root) {}
+
+void MusDemo::OnConnectionLost(mus::WindowTreeConnection* connection) {
+  timer_.Stop();
+}
+
+void MusDemo::SetWindowManagerClient(mus::WindowManagerClient* client) {}
+
+bool MusDemo::OnWmSetBounds(mus::Window* window, gfx::Rect* bounds) {
+  return true;
+}
+
+bool MusDemo::OnWmSetProperty(mus::Window* window,
+                              const std::string& name,
+                              scoped_ptr<std::vector<uint8_t>>* new_data) {
+  return true;
+}
+
+mus::Window* MusDemo::OnWmCreateTopLevelWindow(
+    std::map<std::string, std::vector<uint8_t>>* properties) {
+  // TODO(kylechar): Check if this should return something useful.
+  return nullptr;
+}
+
+void MusDemo::OnAccelerator(uint32_t id, const ui::Event& event) {}
+
+void MusDemo::AllocBitmap() {
+  const gfx::Rect bounds = window_->GetBoundsInRoot();
+
+  // Allocate bitmap the same size as the window for drawing.
+  bitmap_.reset();
+  SkImageInfo image_info = SkImageInfo::MakeN32(bounds.width(), bounds.height(),
+                                                kPremul_SkAlphaType);
+  bitmap_.allocPixels(image_info);
+}
+
+void MusDemo::DrawFrame() {
+  angle_ += 2.0;
+  if (angle_ >= 360.0)
+    angle_ = 0.0;
+
+  const gfx::Rect bounds = window_->GetBoundsInRoot();
+
+  // Check that bitmap and window sizes match, otherwise reallocate bitmap.
+  const SkImageInfo info = bitmap_.info();
+  if (info.width() != bounds.width() || info.height() != bounds.height()) {
+    AllocBitmap();
+  }
+
+  // Draw the rotated square on background in bitmap.
+  SkCanvas canvas(bitmap_);
+  canvas.clear(kBgColor);
+  // TODO(kylechar): Add GL drawing instead of software rasterization in future.
+  DrawSquare(bounds, angle_, &canvas);
+  canvas.flush();
+
+  // Copy pixels data into vector that will be passed to BitmapUploader.
+  // TODO(rjkroege): Make a 1/0-copy bitmap uploader for the contents of a
+  // SkBitmap.
+  bitmap_.lockPixels();
+  const unsigned char* addr =
+      static_cast<const unsigned char*>(bitmap_.getPixels());
+  const int bytes = bounds.width() * bounds.height() * 4;
+  scoped_ptr<std::vector<unsigned char>> data(
+      new std::vector<unsigned char>(addr, addr + bytes));
+  bitmap_.unlockPixels();
+
+  // Send frame to MUS via BitmapUploader.
+  uploader_->SetBitmap(bounds.width(), bounds.height(), std::move(data),
+                       bitmap_uploader::BitmapUploader::BGRA);
+}
+
+}  // namespace mus_demo
---
+++ components/mus/demo/mus_demo.h
@@ -0,0 +1,91 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_MUS_DEMO_MUS_DEMO_H_
+#define COMPONENTS_MUS_DEMO_MUS_DEMO_H_
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "base/callback.h"
+#include "base/macros.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/timer/timer.h"
+#include "components/mus/public/cpp/window.h"
+#include "components/mus/public/cpp/window_manager_delegate.h"
+#include "components/mus/public/cpp/window_tree_delegate.h"
+#include "components/mus/public/interfaces/window_tree_host.mojom.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
+#include "mojo/shell/public/cpp/connector.h"
+#include "mojo/shell/public/cpp/shell_client.h"
+#include "third_party/skia/include/core/SkBitmap.h"
+
+namespace bitmap_uploader {
+class BitmapUploader;
+}
+
+namespace mus_demo {
+
+// A simple MUS Demo mojo app. This app connects to the mojo:mus, creates a new
+// window and draws a spinning square in the center of the window. Provides a
+// simple way to demonstrate that the graphic stack works as intended.
+class MusDemo : public mojo::ShellClient,
+                public mus::WindowTreeDelegate,
+                public mus::WindowManagerDelegate {
+ public:
+  MusDemo();
+  ~MusDemo() override;
+
+ private:
+  // mojo::ShellClient:
+  void Initialize(mojo::Connector* connector,
+                  const mojo::Identity& identity,
+                  uint32_t id) override;
+  bool AcceptConnection(mojo::Connection* connection) override;
+
+  // WindowTreeDelegate:
+  void OnEmbed(mus::Window* root) override;
+  void OnUnembed(mus::Window* root) override;
+  void OnConnectionLost(mus::WindowTreeConnection* connection) override;
+
+  // WindowManagerDelegate:
+  void SetWindowManagerClient(mus::WindowManagerClient* client) override;
+  bool OnWmSetBounds(mus::Window* window, gfx::Rect* bounds) override;
+  bool OnWmSetProperty(mus::Window* window,
+                       const std::string& name,
+                       scoped_ptr<std::vector<uint8_t>>* new_data) override;
+  mus::Window* OnWmCreateTopLevelWindow(
+      std::map<std::string, std::vector<uint8_t>>* properties) override;
+  void OnAccelerator(uint32_t id, const ui::Event& event) override;
+
+  // Allocate a bitmap the same size as the window to draw into.
+  void AllocBitmap();
+
+  // Draws one frame, incrementing the rotation angle.
+  void DrawFrame();
+
+  mojo::Connector* connector_ = nullptr;
+
+  mus::Window* window_ = nullptr;
+  mus::mojom::WindowTreeHostPtr window_tree_host_;
+
+  // Used to send frames to mus.
+  scoped_ptr<bitmap_uploader::BitmapUploader> uploader_;
+
+  // Bitmap that is the same size as our client window area.
+  SkBitmap bitmap_;
+
+  // Timer for calling DrawFrame().
+  base::RepeatingTimer timer_;
+
+  // Current rotation angle for drawing.
+  double angle_ = 0.0;
+
+  DISALLOW_COPY_AND_ASSIGN(MusDemo);
+};
+
+}  // namespace mus_demo
+
+#endif  // COMPONENTS_MUS_DEMO_MUS_DEMO_H_
