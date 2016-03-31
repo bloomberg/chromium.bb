2e5875684f530de9a7d35af1b728239341727e9b
kbr <kbr@chromium.org>
2016-03-30 17:40:39 -0700

    Add optional GPU tryservers to content/test/gpu PRESUBMIT check.
    This will cause the WebGL 2.0 conformance tests, and other tests, to
    be run on the commit queue for changes in this directory.
    
    BUG=none
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1846653002
    
    Cr-Commit-Position: refs/heads/master@{#384165}
    
    Former-commit-id: 1a1ef2f111e127b0bac5bbcacccc22c09b964a14

--- content/test/gpu/PRESUBMIT.py
+++ content/test/gpu/PRESUBMIT.py
@@ -2,6 +2,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+"""Top-level presubmit script for content/test/gpu.
+
+See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
+for more details about the presubmit API built into depot_tools.
+"""
+
+import re
+
 def _PyLintChecks(input_api, output_api):
   pylint_checks = input_api.canned_checks.GetPylint(input_api, output_api,
           extra_paths_list=_GetPathsToPrepend(input_api), pylintrc='pylintrc')
@@ -57,4 +65,32 @@ def CheckChangeOnCommit(input_api, output_api):
   results = []
   results.extend(_PyLintChecks(input_api, output_api))
   results.extend(_WebGLTextExpectationsTests(input_api, output_api))
+  return results
+
+def PostUploadHook(cl, change, output_api):
+  """git cl upload will call this hook after the issue is created/modified.
+
+  This hook adds extra try bots list to the CL description in order to run
+  Blink tests in addition to CQ try bots.
+  """
+  rietveld_obj = cl.RpcServer()
+  issue = cl.issue
+  description = rietveld_obj.get_description(issue)
+  if re.search(r'^CQ_INCLUDE_TRYBOTS=.*', description, re.M | re.I):
+    return []
+
+  bots = [
+    'tryserver.chromium.win:win_optional_gpu_tests_rel',
+    'tryserver.chromium.mac:mac_optional_gpu_tests_rel',
+  ]
+
+  results = []
+  new_description = description
+  new_description += '\nCQ_INCLUDE_TRYBOTS=%s' % ';'.join(bots)
+  results.append(output_api.PresubmitNotifyResult(
+      'Automatically added optional GPU tests to run on CQ.'))
+
+  if new_description != description:
+    rietveld_obj.update_description(issue, new_description)
+
   return results
