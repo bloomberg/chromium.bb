baf0cebbbdcfd6af8e37d6d144fee1747a934b3d
maksim.sisov <maksim.sisov@intel.com>
2016-03-31 02:11:36 -0700

    When building Debug build with enable_wifi_display flag on, compiler complains about DCHECK_EQ not declared. Added base/logging.h header.
    Review URL: https://codereview.chromium.org/1842303002
    
    Cr-Commit-Position: refs/heads/master@{#384227}
    
    Former-commit-id: bb0241f2281d8a3d1fed1775b1f815c55a67592c

--- extensions/renderer/api/display_source/wifi_display/wifi_display_elementary_stream_descriptor.cc
+++ extensions/renderer/api/display_source/wifi_display/wifi_display_elementary_stream_descriptor.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/logging.h"
 #include "extensions/renderer/api/display_source/wifi_display/wifi_display_elementary_stream_descriptor.h"
 
 #include <cstring>
