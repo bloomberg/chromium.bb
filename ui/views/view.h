368c3ce968ce60302a535cbcb249f8311c3e69e1
sky <sky@chromium.org>
2016-03-31 13:12:41 -0700

    Make View::SchedulePaint() non-virtual
    it's implemented in terms of SchedulePaint(gfx::Rect), so need reason
    for it to be virtual.
    
    BUG=none
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/1843363003
    
    Cr-Commit-Position: refs/heads/master@{#384368}
    
    Former-commit-id: ac42469d379fa25f95d6831fff807e8ceb784339

--- ui/views/view.h
+++ ui/views/view.h
@@ -489,7 +489,7 @@ class VIEWS_EXPORT View : public ui::LayerDelegate,
   // transformations are applied to it to convert it into the parent coordinate
   // system before propagating SchedulePaint up the view hierarchy.
   // TODO(beng): Make protected.
-  virtual void SchedulePaint();
+  void SchedulePaint();
   virtual void SchedulePaintInRect(const gfx::Rect& r);
 
   // Called by the framework to paint a View. Performs translation and clipping
