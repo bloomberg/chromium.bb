51bcc4301eed6876b97c9679a0e92da76d8cdd76
iannucci@chromium.org <iannucci@chromium.org>
2016-04-01 01:24:12 +0000

    Fix bash math for `git bash` alias.
    Previous calculation was off-by-one.
    
    TBR=dnj@chromium.org, mmoss@chromium.org
    BUG=598956
    
    Review URL: https://codereview.chromium.org/1848793004 .
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@299619 0039d316-1c4b-4281-b951-d872f2087c98

--- bootstrap/win/git-bash.template.sh
+++ bootstrap/win/git-bash.template.sh
@@ -5,7 +5,7 @@ UNIX_BASE=`cygpath "$WIN_BASE"`
 export PATH=$PATH:$UNIX_BASE/SVN_BIN_DIR:$UNIX_BASE/PYTHON_BIN_DIR:$UNIX_BASE/PYTHON_BIN_DIR/Scripts
 export PYTHON_DIRECT=1
 export PYTHONUNBUFFERED=1
-if [[ $# > 1 ]]; then
+if [[ $# > 0 ]]; then
   $UNIX_BASE/GIT_BIN_DIR/bin/bash.exe "$@"
 else
   $UNIX_BASE/GIT_BIN_DIR/git-bash.exe &
