eed06d620fa4814a596c359b2d8bba4ad524c934
iannucci@chromium.org <iannucci@chromium.org>
2016-04-01 00:59:42 +0000

    Better tooling
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@299614 0039d316-1c4b-4281-b951-d872f2087c98

--- gclient.py
+++ gclient.py
@@ -2303,7 +2303,7 @@ def main(argv):
     return 2
   fix_encoding.fix_encoding()
   disable_buffering()
-  colorama.init()
+  colorama.init(wrap="TERM" not in os.environ)
   dispatcher = subcommand.CommandDispatcher(__name__)
   try:
     return dispatcher.execute(OptionParser(), argv)
