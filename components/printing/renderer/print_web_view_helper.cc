4109151d1bcd6877fc9c06f6b67de041dff34bc5
robhogan <robhogan@gmail.com>
2016-04-01 01:29:34 -0700

    Print with the correct physical dimensions when specified
    If you specify a div in cm, mm or a similar unit of physical measurement and
    print it you don't get an element of the expected size on paper. This is due
    to the scaling factor we use when resizing the page for printing.
    
    The scaling factor of 1.25 was introduced by https://trac.webkit.org/changeset/22945
    and adopted in the Gtk port by http://wkbug.com/15576. It seems to
    be a magic number that was 'about right'. From trial and error 1.33 seems to be
    the magic number that produces output with the correct physical size.
    
    I don't know how to test this.
    
    BUG=273306
    
    Review URL: https://codereview.chromium.org/1817873002
    
    Cr-Commit-Position: refs/heads/master@{#384518}
    
    Former-commit-id: a7fd168b96a0f6896b4a4e3d88921998e56f49c0

--- components/printing/renderer/print_web_view_helper.cc
+++ components/printing/renderer/print_web_view_helper.cc
@@ -78,6 +78,9 @@ enum PrintPreviewHelperEvents {
 
 const double kMinDpi = 1.0;
 
+// Also set in third_party/WebKit/Source/core/page/PrintContext.cpp
+const float kPrintingMinimumShrinkFactor = 1.333f;
+
 #if defined(ENABLE_PRINT_PREVIEW)
 bool g_is_preview_enabled = true;
 
@@ -677,15 +680,19 @@ PrepareFrameAndViewForPrint::~PrepareFrameAndViewForPrint() {
 
 void PrepareFrameAndViewForPrint::ResizeForPrinting() {
   // Layout page according to printer page size. Since WebKit shrinks the
-  // size of the page automatically (from 125% to 200%) we trick it to
-  // think the page is 125% larger so the size of the page is correct for
+  // size of the page automatically (from 133.3% to 200%) we trick it to
+  // think the page is 133.3% larger so the size of the page is correct for
   // minimum (default) scaling.
+  // The scaling factor 1.25 was originally chosen as a magic number that
+  // was 'about right'. However per https://crbug.com/273306 1.333 seems to be
+  // the number that produces output with the correct physical size for elements
+  // that are specified in cm, mm, pt etc.
   // This is important for sites that try to fill the page.
-  // The 1.25 value is |printingMinimumShrinkFactor|.
   gfx::Size print_layout_size(web_print_params_.printContentArea.width,
                               web_print_params_.printContentArea.height);
   print_layout_size.set_height(
-      static_cast<int>(static_cast<double>(print_layout_size.height()) * 1.25));
+      static_cast<int>(static_cast<double>(print_layout_size.height()) *
+                       kPrintingMinimumShrinkFactor));
 
   if (!frame())
     return;
@@ -1780,12 +1787,12 @@ void PrintWebViewHelper::PrintPageInternal(
 
 #if defined(ENABLE_PRINT_PREVIEW)
   if (params.params.display_header_footer) {
-    // TODO(thestig): Figure out why Linux needs this. The value may be
-    // |printingMinimumShrinkFactor|.
+    // TODO(thestig): Figure out why Linux needs this. It is almost certainly
+    // |printingMinimumShrinkFactor| from Blink.
 #if defined(OS_WIN)
     const float fudge_factor = 1;
 #else
-    const float fudge_factor = 1.25;
+    const float fudge_factor = kPrintingMinimumShrinkFactor;
 #endif
     // |page_number| is 0-based, so 1 is added.
     PrintHeaderAndFooter(canvas, params.page_number + 1,
