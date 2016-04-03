0a8b3127dafe75125a434174291833dd90adcd96
davidben <davidben@chromium.org>
2016-04-03 12:14:33 -0700

    Add a deprecation warning for DHE.
    Link to Intent thread:
    https://groups.google.com/a/chromium.org/d/msg/blink-dev/AAdv838-koo/bJv17voIBAAJ
    
    BUG=598109
    TEST=Visiting https://dh1024.badssl.com/ and opening the JS console shows a deprecation warning.
    
    Review URL: https://codereview.chromium.org/1851203002
    
    Cr-Commit-Position: refs/heads/master@{#384836}
    
    Former-commit-id: 157660a6a5c953b387361c3890cdfa39f042d0e5

--- chrome/renderer/chrome_render_frame_observer.cc
+++ chrome/renderer/chrome_render_frame_observer.cc
@@ -5,6 +5,7 @@
 #include "chrome/renderer/chrome_render_frame_observer.h"
 
 #include <stddef.h>
+#include <string.h>
 
 #include <limits>
 #include <string>
@@ -28,6 +29,8 @@
 #include "content/public/renderer/render_view.h"
 #include "extensions/common/constants.h"
 #include "net/base/url_util.h"
+#include "net/ssl/ssl_cipher_suite_names.h"
+#include "net/ssl/ssl_connection_status_flags.h"
 #include "skia/ext/image_operations.h"
 #include "third_party/WebKit/public/platform/WebImage.h"
 #include "third_party/WebKit/public/platform/modules/app_banner/WebAppBannerPromptReply.h"
@@ -247,9 +250,10 @@ void ChromeRenderFrameObserver::DidFinishDocumentLoad() {
       switches::kAllowInsecureLocalhost);
   WebDataSource* ds = render_frame()->GetWebFrame()->dataSource();
 
+  SSLStatus ssl_status = render_frame()->GetRenderView()->GetSSLStatusOfFrame(
+      render_frame()->GetWebFrame());
+
   if (allow_localhost) {
-    SSLStatus ssl_status = render_frame()->GetRenderView()->GetSSLStatusOfFrame(
-        render_frame()->GetWebFrame());
     bool is_cert_error = net::IsCertStatusError(ssl_status.cert_status) &&
                          !net::IsCertStatusMinorError(ssl_status.cert_status);
     bool is_localhost = net::IsLocalhost(GURL(ds->request().url()).host());
@@ -265,6 +269,25 @@ void ChromeRenderFrameObserver::DidFinishDocumentLoad() {
                   "tampering. Get a valid SSL certificate before"
                   " releasing your website to the public.")));
     }
+  }
+
+  // DHE is deprecated and will be removed in M52. See https://crbug.com/598109.
+  // TODO(davidben): Remove this logic when DHE is removed.
+  uint16_t cipher_suite =
+      net::SSLConnectionStatusToCipherSuite(ssl_status.connection_status);
+  const char* key_exchange;
+  const char* unused;
+  bool is_aead_unused;
+  net::SSLCipherSuiteToStrings(&key_exchange, &unused, &unused, &is_aead_unused,
+                               cipher_suite);
+  if (strcmp(key_exchange, "DHE_RSA") == 0) {
+    render_frame()->GetWebFrame()->addMessageToConsole(blink::WebConsoleMessage(
+        blink::WebConsoleMessage::LevelWarning,
+        base::ASCIIToUTF16("This site requires a DHE-based SSL cipher suite. "
+                           "These are deprecated and will be removed in M52, "
+                           "around July 2016. See "
+                           "https://www.chromestatus.com/feature/"
+                           "5752033759985664 for more details.")));
   }
 }
 
