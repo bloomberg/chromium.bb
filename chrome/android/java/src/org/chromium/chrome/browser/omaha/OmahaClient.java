c7d971299f531403d0219a8f2a88baf7d28e5a07
wnwen <wnwen@chromium.org>
2016-04-04 11:08:50 -0700

    Add null-check in OmahaClient for handling intent.
    Since getRequestGenerator() is used without null guards in all
    subsequent calls, it should be verified before making those calls.
    
    This check will be extremely quick if ChromeApplication returns null,
    and it will only incur a one-time cost otherwise, and this is not on
    the UI thread, so it should have minimal performance impact.
    
    BUG=584114
    
    Review URL: https://codereview.chromium.org/1856823002
    
    Cr-Commit-Position: refs/heads/master@{#384955}
    
    Former-commit-id: 85705c2257f562b092c8d04996683439e43ce76f

--- chrome/android/java/src/org/chromium/chrome/browser/omaha/OmahaClient.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omaha/OmahaClient.java
@@ -198,6 +198,10 @@ public class OmahaClient extends IntentService {
             return;
         }
 
+        if (getRequestGenerator() == null) {
+            return;
+        }
+
         if (!mStateHasBeenRestored) {
             restoreState();
         }
