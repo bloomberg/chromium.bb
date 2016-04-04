2803b6dfb272df4791e3d551598d52e2a1a97877
szager <szager@chromium.org>
2016-04-04 14:53:41 -0700

    IntersectionObserver: use an idle callback to send notifications.
    Re-landing after original:
    
    https://codereview.chromium.org/1780163002/
    
    ... was reverted by:
    
    https://codereview.chromium.org/1776493002/
    
    With this change, the tests can no longer use setTimeout(0) to wait
    for notifications to be delivered.  Instead, use takeRecords() to
    proactively grab notifications right after they are generated
    (typically in a RAF right after a layout change).
    
    These tests tickle a bug in ScriptedIdleTaskController, hence the
    LeakExpectations entries.
    
    BUG=540528
    R=ojan@chromium.org,haraken@chromium.org,sigbjornf@opera.com
    
    Review URL: https://codereview.chromium.org/1806133002
    
    Cr-Commit-Position: refs/heads/master@{#385021}
    
    Former-commit-id: 4cf47cb23a655b02d36e64ea3f986ff2900f5bdd

--- third_party/WebKit/Source/core/dom/IntersectionObserverController.h
+++ third_party/WebKit/Source/core/dom/IntersectionObserverController.h
@@ -6,8 +6,8 @@
 #define IntersectionObserverController_h
 
 #include "core/dom/Element.h"
+#include "core/dom/IdleRequestCallback.h"
 #include "core/dom/IntersectionObserver.h"
-#include "platform/Timer.h"
 #include "platform/heap/Handle.h"
 #include "wtf/HashSet.h"
 
@@ -16,16 +16,17 @@
 
 namespace blink {
 
-class IntersectionObserverController : public GarbageCollectedFinalized<IntersectionObserverController>, public ActiveDOMObject {
+class IntersectionObserverController : public IdleRequestCallback, public ActiveDOMObject {
     USING_GARBAGE_COLLECTED_MIXIN(IntersectionObserverController);
 public:
     static IntersectionObserverController* create(Document*);
     ~IntersectionObserverController();
 
     void resume() override;
+    void handleEvent(IdleDeadline*) override;
 
     void scheduleIntersectionObserverForDelivery(IntersectionObserver&);
-    void deliverIntersectionObservations(Timer<IntersectionObserverController>*);
+    void deliverIntersectionObservations();
     void computeTrackedIntersectionObservations();
     void addTrackedObserver(IntersectionObserver&);
     void removeTrackedObserversForRoot(const Node&);
@@ -36,13 +37,13 @@ private:
     explicit IntersectionObserverController(Document*);
 
 private:
-    Timer<IntersectionObserverController> m_timer;
     // IntersectionObservers for which this is the tracking document.
     HeapHashSet<WeakMember<IntersectionObserver>> m_trackedIntersectionObservers;
     // IntersectionObservers for which this is the execution context of the callback.
     HeapHashSet<Member<IntersectionObserver>> m_pendingIntersectionObservers;
 
-    bool m_timerFiredWhileSuspended;
+    int m_callbackID;
+    bool m_callbackFiredWhileSuspended;
 };
 
 } // namespace blink
