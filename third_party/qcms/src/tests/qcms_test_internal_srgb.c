bae0f5884aa3f7d17d754de20582d2ceab0153c0
noel <noel@chromium.org>
2016-04-03 23:18:33 -0700

    [qcms] Update internal sRGB profile test report output
    Clarify what is being tested, output consistent test result
    printf lines.
    
    TBR=radu.velea@intler.com
    BUG=580917
    
    Review URL: https://codereview.chromium.org/1851423002
    
    Cr-Commit-Position: refs/heads/master@{#384854}
    
    Former-commit-id: 54ff10e8edd9df4cc0c9c2386855fa6c21e88fd9

--- third_party/qcms/src/tests/qcms_test_internal_srgb.c
+++ third_party/qcms/src/tests/qcms_test_internal_srgb.c
@@ -22,7 +22,7 @@ static struct XYZNumber D65 = {
     0xf351, 0x10000, 0x116cc // ( 0.950455, 1.000000, 1.089050 )
 };
 
-static float check_profile_pcs_white_point(const qcms_profile *profile)
+static void check_profile_pcs_white_point(const qcms_profile *profile)
 {
     float rX = s15Fixed16Number_to_float(profile->redColorant.X);
     float gX = s15Fixed16Number_to_float(profile->greenColorant.X);
@@ -34,6 +34,8 @@ static float check_profile_pcs_white_point(const qcms_profile *profile)
     float gZ = s15Fixed16Number_to_float(profile->greenColorant.Z);
     float bZ = s15Fixed16Number_to_float(profile->blueColorant.Z);
 
+    printf("Test PCS white point against expected D50 XYZ values\n");
+
     float X = rX + gX + bX;
     float Y = rY + gY + bY;
     float Z = rZ + gZ + bZ;
@@ -41,14 +43,14 @@ static float check_profile_pcs_white_point(const qcms_profile *profile)
     float x = X / (X + Y + Z);
     float y = Y / (X + Y + Z);
 
-    printf("Test PCS white point error\n");
-    printf("Profile D50 White point test: xyY [ %.6f %.6f %.6f ]\n", x, y, Y);
+    printf("Computed profile D50 White point xyY = [%.6f %.6f %.6f]\n", x, y, Y);
 
-    float xerr = x - 0.345702915;
+    float xerr = x - 0.345702915; // Compute error to ICC spec D50 xyY.
     float yerr = y - 0.358538597;
     float Yerr = Y - 1.000000000;
 
-    return sqrt((xerr * xerr) + (yerr * yerr) + (Yerr * Yerr));
+    printf("D50 white point error = %.6f\n", (float)
+           sqrt((xerr * xerr) + (yerr * yerr) + (Yerr * Yerr)));
 }
 
 static void check_profile_media_white_point(const qcms_profile *profile)
@@ -58,19 +60,18 @@ static void check_profile_media_white_point(const qcms_profile *profile)
     int errZ = profile->mediaWhitePoint.Z - D65.Z;
 
     printf("Test media white point against expected D65 XYZ values\n");
-    printf("Internal values = [0x%X, 0x%X, 0x%X]\n",
-            profile->mediaWhitePoint.X, profile->mediaWhitePoint.Y, profile->mediaWhitePoint.Z);
-
-    printf("White point error = [%d, %d, %d]\n\n", errX, errY, errZ);
+    printf("Internal profile D65 values = [0x%X, 0x%X, 0x%X]\n",
+           profile->mediaWhitePoint.X, profile->mediaWhitePoint.Y, profile->mediaWhitePoint.Z);
+    printf("D65 media white point error = [%d, %d, %d]\n\n", errX, errY, errZ);
 }
 
 static s15Fixed16Number check_profile_primaries(const qcms_profile *profile)
 {
-    s15Fixed16Number ret = 0;
     s15Fixed16Number sRGB_internal[3][3];
+    s15Fixed16Number primary_error;
     int i, j;
 
-    printf("Test qcms internal sRGB color primaries against sRGB IEC61966-2.1 color primaries\n");
+    printf("Test qcms internal sRGB color primaries\n");
 
     sRGB_internal[0][0] = profile->redColorant.X;
     sRGB_internal[1][0] = profile->redColorant.Y;
@@ -82,16 +83,17 @@ static s15Fixed16Number check_profile_primaries(const qcms_profile *profile)
     sRGB_internal[1][2] = profile->blueColorant.Y;
     sRGB_internal[2][2] = profile->blueColorant.Z;
 
+    primary_error = 0;
     for (i = 0; i < 3; i++) {
         for (j = 0; j < 3; j++) {
             s15Fixed16Number tmp = sRGB_internal[i][j] - sRGB_reference[i][j];
-            printf("\t%d", tmp);
-            ret += abs(tmp);
+            printf(" %d", tmp);
+            primary_error += abs(tmp);
         }
         printf("\n");
     }
 
-    return ret;
+    return primary_error;
 }
 
 static int qcms_test_internal_srgb(size_t width,
@@ -103,7 +105,6 @@ static int qcms_test_internal_srgb(size_t width,
 {
     qcms_profile *profile = qcms_profile_sRGB();
     s15Fixed16Number primary_error;
-    float pcs_error;
 
     if (qcms_profile_is_bogus(profile)) {
         fprintf(stderr, "Failure: the internal sRGB profile failed the bogus profile check\n");
@@ -113,15 +114,15 @@ static int qcms_test_internal_srgb(size_t width,
 
     // Compute tristimulus matrix error.
     primary_error = check_profile_primaries(profile);
-    printf("Total error = 0x%x [%.6f]\n\n", primary_error, primary_error / 65536.0);
+    printf("Total primary error = 0x%x [%.6f]\n\n", primary_error, primary_error / 65536.0);
+
     // Verify media white point correctness.
     check_profile_media_white_point(profile);
-    // Compute distance from ICC D50 xyY.
-    pcs_error = check_profile_pcs_white_point(profile);
-    printf("White point error: %.6f\n", pcs_error);
+
+    // Verify PCS white point correctness.
+    check_profile_pcs_white_point(profile);
 
     qcms_profile_release(profile);
-
     return primary_error;
 }
 
