6705f672e979712c19c82d717e03d514572720a9
dyen <dyen@chromium.org>
2016-04-06 17:23:31 -0700

    Added new stack trace unit tests for breakpad symbol file cases.
    R=nednguyen@google.com
    BUG=561447
    
    Review URL: https://codereview.chromium.org/1857363002
    
    Cr-Commit-Position: refs/heads/master@{#385600}
    
    Former-commit-id: 2314f69c996e342ba66c71921d2f5927b246921d

--- tools/perf/core/stacktrace_unittest.py
+++ tools/perf/core/stacktrace_unittest.py
@@ -2,6 +2,9 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import tempfile
+import os
+
 from telemetry.core import exceptions
 from telemetry import decorators
 from telemetry.testing import tab_test_case
@@ -18,10 +21,9 @@ class TabStackTraceTest(tab_test_case.TabTestCase):
   # disabled shortly so just disable it for now.
   @decorators.Disabled('snowleopard')
   def testStackTrace(self):
-    try:
+    with self.assertRaises(exceptions.DevtoolsTargetCrashException) as c:
       self._tab.Navigate('chrome://crash', timeout=5)
-    except exceptions.DevtoolsTargetCrashException as e:
-      self.assertIn('Thread 0 (crashed)', '\n'.join(e.stack_trace))
+      self.assertIn('Thread 0 (crashed)', '\n'.join(c.exception.stack_trace))
 
   # Stack traces aren't working on Android yet.
   # TODO(kbr): this test doesn't work on the Windows Swarming bots
@@ -29,7 +31,36 @@ class TabStackTraceTest(tab_test_case.TabTestCase):
   @decorators.Enabled('mac', 'linux')
   @decorators.Disabled('snowleopard')
   def testCrashSymbols(self):
-    try:
+    with self.assertRaises(exceptions.DevtoolsTargetCrashException) as c:
       self._tab.Navigate('chrome://crash', timeout=5)
-    except exceptions.DevtoolsTargetCrashException as e:
-      self.assertIn('CrashIntentionally', '\n'.join(e.stack_trace))
+      self.assertIn('CrashIntentionally', '\n'.join(c.exception.stack_trace))
+
+  # The breakpad file specific test only apply to platforms which use the
+  # breakpad symbol format. This also must be tested in isolation because it can
+  # potentially interfere with other tests symbol parsing.
+  @decorators.Enabled('mac', 'linux')
+  @decorators.Isolated
+  def testBadBreakpadFileIgnored(self):
+    # pylint: disable=protected-access
+    executable_path = self._browser._browser_backend._executable
+    executable = os.path.basename(executable_path)
+    with tempfile.NamedTemporaryFile(mode='wt',
+                                     dir=os.path.dirname(executable_path),
+                                     prefix=executable + '.breakpad',
+                                     delete=True) as f:
+      # Do not write anything to file, it will just be an invalid empty file.
+      f.flush()
+      with self.assertRaises(exceptions.DevtoolsTargetCrashException) as c:
+        self._tab.Navigate('chrome://crash', timeout=5)
+        # The symbol directory should now contain our breakpad file.
+        tmp_dir = os.path.join(self._browser._browser_backend._tmp_minidump_dir)
+
+        # Symbol directory should have been created.
+        symbol_dir = os.path.join(tmp_dir, 'symbols', executable)
+        self.assertTrue(os.path.isdir(symbol_dir))
+
+        # A single symbol file should still exist here.
+        self.assertEqual(1, len(os.listdir(symbol_dir)))
+
+        # Stack trace should still work.
+        self.assertIn('CrashIntentionally', '\n'.join(c.exception.stack_trace))
