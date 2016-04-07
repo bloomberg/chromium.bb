0e837ae6ecc475b02d7114dc10480543d6be98ff
Akira TAGOH <akira@tagoh.org>
2016-04-07 12:01:14 +0900

    Modernize fc-blanks.py
    fc-blanks.py now works on both python2 and 3

--- fc-blanks/fc-blanks.py
+++ fc-blanks/fc-blanks.py
@@ -1,8 +1,11 @@
 #! /usr/bin/python
 
+from __future__ import absolute_import
+from __future__ import print_function
 import urllib2
 import sys
 from lxml import html
+from six.moves import range
 
 fp = urllib2.urlopen('http://unicode.org/cldr/utility/list-unicodeset.jsp?a=[%3AGC%3DZs%3A][%3ADI%3A]&abb=on&ucd=on&esc=on&g')
 data = fp.read()
@@ -47,12 +50,12 @@ for i in p:
         fescape = False
     elif i >= '0' and i <= '9' or i.lower() >= 'a' and i.lower() <= 'f':
         if fescape == True:
-            raise RuntimeError, "Unexpected escape code"
+            raise RuntimeError("Unexpected escape code")
         if funicode == True:
             v <<= 4
             v += int(i, 16)
         else:
-            raise RuntimeError, "Unable to parse Unicode"
+            raise RuntimeError("Unable to parse Unicode")
     elif i == ' ':
         if fescape == True:
             funicode = True
@@ -71,13 +74,13 @@ for i in p:
         frange = False
     elif i == '-':
         if fescape == True:
-            raise RuntimeError, "Unexpected escape code"
+            raise RuntimeError("Unexpected escape code")
         vbegin = v
         v = 0
         funicode = False
         frange = True
     else:
-        raise RuntimeError, "Unable to parse Unicode: %s" % i
+        raise RuntimeError("Unable to parse Unicode: %s" % i)
 
     if fprocess == True:
         vbegin = 0
@@ -111,22 +114,22 @@ while True:
     s = sys.stdin.readline().rstrip()
     if s == "@@@":
         break
-    print s
+    print(s)
 
-print "static FcChar32 _fcBlanks[%s] = {" % (ncode + 1)
+print("static FcChar32 _fcBlanks[%s] = {" % (ncode + 1))
 k = 0
-for i in sorted(l, key=lambda(a): a[0]):
+for i in sorted(l, key=lambda a: a[0]):
     for j in range(i[0], i[1] + 1):
         if k != 0:
-            print ","
-        print "    0x%04x" % j,
+            print(",")
+        print("    0x%04x" % j, end=' ')
         k += 1
 
-print "};"
-print '''
+print("};")
+print('''
 static FcBlanks fcBlanks = {
     %s,
     -1,
     _fcBlanks
 };
-''' % (ncode + 1)
+''' % (ncode + 1))
