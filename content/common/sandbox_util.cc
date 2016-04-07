69ca55129664917a82aceb755f53aac2209206b7
erikchen <erikchen@chromium.org>
2016-04-07 01:54:35 -0700

    Update implementation of BrokerGetFileHandleForProcess.
    Use IPC::GetPlatformFileForTransit instead of content::BrokerDuplicateHandle.
    
    BUG=493414
    
    Review URL: https://codereview.chromium.org/1866233002
    
    Cr-Commit-Position: refs/heads/master@{#385696}
    
    Former-commit-id: 05fcaf5a1fb2771ae1dca9f1bbdd6d08b74dace4

--- content/common/sandbox_util.cc
+++ content/common/sandbox_util.cc
@@ -6,6 +6,7 @@
 
 #include "build/build_config.h"
 #include "content/public/common/sandbox_init.h"
+#include "ipc/ipc_platform_file.h"
 
 #if defined(OS_POSIX)
 #include <unistd.h>
@@ -17,33 +18,7 @@ IPC::PlatformFileForTransit BrokerGetFileHandleForProcess(
     base::PlatformFile handle,
     base::ProcessId target_process_id,
     bool should_close_source) {
-  IPC::PlatformFileForTransit out_handle;
-#if defined(OS_WIN)
-  DWORD options = DUPLICATE_SAME_ACCESS;
-  if (should_close_source)
-    options |= DUPLICATE_CLOSE_SOURCE;
-  HANDLE raw_handle = INVALID_HANDLE_VALUE;
-  if (content::BrokerDuplicateHandle(handle, target_process_id, &raw_handle, 0,
-                                     options)) {
-    out_handle = IPC::PlatformFileForTransit(raw_handle, target_process_id);
-  } else {
-    out_handle = IPC::InvalidPlatformFileForTransit();
-  }
-#elif defined(OS_POSIX)
-  // If asked to close the source, we can simply re-use the source fd instead of
-  // dup()ing and close()ing.
-  // When we're not closing the source, we need to duplicate the handle and take
-  // ownership of that. The reason is that this function is often used to
-  // generate IPC messages, and the handle must remain valid until it's sent to
-  // the other process from the I/O thread. Without the dup, calling code might
-  // close the source handle before the message is sent, creating a race
-  // condition.
-  int fd = should_close_source ? handle : ::dup(handle);
-  out_handle = base::FileDescriptor(fd, true);
-#else
-  #error Not implemented.
-#endif
-  return out_handle;
+  return IPC::GetPlatformFileForTransit(handle, should_close_source);
 }
 
 }  // namespace content
