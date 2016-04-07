4da6301f1d7b879d550f1ec82111c2e276125270
lukasza <lukasza@chromium.org>
2016-04-07 07:46:13 -0700

    Fix UaF of BlinkTestRunner caused by incorrect base::Unretained.
    BUG=601089
    
    Review URL: https://codereview.chromium.org/1864313002
    
    Cr-Commit-Position: refs/heads/master@{#385754}
    
    Former-commit-id: 647f8addf5b416dfc0b1742e22c85d02ec2b8791

--- content/shell/renderer/layout_test/blink_test_runner.cc
+++ content/shell/renderer/layout_test/blink_test_runner.cc
@@ -903,10 +903,7 @@ void BlinkTestRunner::OnPixelsDumpCompleted(const SkBitmap& snapshot) {
 void BlinkTestRunner::CaptureDumpComplete() {
   render_view()->GetWebView()->mainFrame()->stopLoading();
 
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::Bind(base::IgnoreResult(&BlinkTestRunner::Send),
-                            base::Unretained(this),
-                            new ShellViewHostMsg_TestFinished(routing_id())));
+  Send(new ShellViewHostMsg_TestFinished(routing_id()));
 }
 
 mojom::LayoutTestBluetoothFakeAdapterSetter&
