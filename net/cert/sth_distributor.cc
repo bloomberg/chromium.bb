d21ccbd04551a3555fb3bd627c2a85aef976c0a4
eranm <eranm@chromium.org>
2016-04-08 04:09:04 -0700

    Certificate Transparency: Measure the age of the STH from the client
    This will allow us to make an informed decision about update frequency.
    Note this CL depends on https://codereview.chromium.org/1853753003/
    being submitted.
    
    BUG=506227
    
    Review URL: https://codereview.chromium.org/1864093002
    
    Cr-Commit-Position: refs/heads/master@{#386043}
    
    Former-commit-id: 8dee69f1ffc21bbd12934fac1af2040dc5ce6659

--- net/cert/sth_distributor.cc
+++ net/cert/sth_distributor.cc
@@ -4,7 +4,15 @@
 
 #include "net/cert/sth_distributor.h"
 
+#include "base/metrics/histogram_macros.h"
+#include "base/time/time.h"
 #include "net/cert/signed_tree_head.h"
+
+namespace {
+const char kPilotLogID[33] =
+    "\xa4\xb9\x09\x90\xb4\x18\x58\x14\x87\xbb\x13\xa2\xcc\x67\x70\x0a\x3c\x35"
+    "\x98\x04\xf9\x1b\xdf\xb8\xe3\x77\xcd\x0e\xc8\x0d\xdc\x10";
+}
 
 namespace net {
 
@@ -17,6 +25,15 @@ STHDistributor::~STHDistributor() {}
 
 void STHDistributor::NewSTHObserved(const SignedTreeHead& sth) {
   FOR_EACH_OBSERVER(STHObserver, observer_list_, NewSTHObserved(sth));
+
+  if (sth.log_id.compare(0, sth.log_id.size(), kPilotLogID,
+                         arraysize(kPilotLogID)) != 0)
+    return;
+
+  const base::TimeDelta sth_age = base::Time::Now() - sth.timestamp;
+  UMA_HISTOGRAM_CUSTOM_TIMES("Net.CertificateTransparency.PilotSTHAge", sth_age,
+                             base::TimeDelta::FromHours(1),
+                             base::TimeDelta::FromDays(4), 100);
 }
 
 void STHDistributor::RegisterObserver(STHObserver* observer) {
