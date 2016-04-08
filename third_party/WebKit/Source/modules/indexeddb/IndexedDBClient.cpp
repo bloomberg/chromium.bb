e7963869f7924d2eaa811ffc53cea62b86568040
sigbjornf <sigbjornf@opera.com>
2016-04-07 22:55:16 -0700

    Add setIndexedDBClientCreateFunction() explanatory comment.
    Follow up changes in r385772 and r385733 with a comment to
    try to explain why update atomicity matters here.
    
    R=
    BUG=598551, 599011
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/1862223005
    
    Cr-Commit-Position: refs/heads/master@{#386000}
    
    Former-commit-id: 4fa5dd8ee1e6f6c8719935152f3b48f82626d595

--- third_party/WebKit/Source/modules/indexeddb/IndexedDBClient.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IndexedDBClient.cpp
@@ -11,6 +11,10 @@ static void* idbClientCreateFunction = nullptr;
 
 void setIndexedDBClientCreateFunction(CreateIndexedDBClient createFunction)
 {
+    // See web/IndexedDBClientImpl.h comment for some context. As the initialization
+    // of this IndexedDB client constructor now happens as context is set up for
+    // threads, it is possible that setIndexedDBClientCreateFunction() will be
+    // called more than once. Hence update atomicity is needed.
 #if ENABLE(ASSERT)
     CreateIndexedDBClient* currentFunction = reinterpret_cast<CreateIndexedDBClient*>(acquireLoad(&idbClientCreateFunction));
     ASSERT(!currentFunction || currentFunction == createFunction);
