8c077a79bc8ab2e1d4c08dcca883a93160f39739
lushnikov <lushnikov@chromium.org>
2016-04-12 16:17:03 -0700

    DevTools: fix elements-panel-shadow-selection-on-refresh.html
    BUG=577870
    R=dgozman
    
    Review URL: https://codereview.chromium.org/1878123003
    
    Cr-Commit-Position: refs/heads/master@{#386850}
    
    Former-commit-id: 12f2b7ec4bc05357bdb313e1d1e7898e45724bc6

---
+++ third_party/WebKit/LayoutTests/inspector/elements/shadow/elements-panel-shadow-selection-on-refresh.js
@@ -0,0 +1,39 @@
+var initialize_ElementPanelShadowSelection = function() {
+
+InspectorTest.selectReloadAndDump = function(next, node)
+{
+    InspectorTest.selectNode(node).then(onSelected);
+    var reloaded = false;
+    var selected = false;
+
+    function onSelected()
+    {
+        InspectorTest.reloadPage(onReloaded);
+        InspectorTest.addSniffer(WebInspector.ElementsPanel.prototype, "_lastSelectedNodeSelectedForTest", onReSelected);
+    }
+
+    function onReloaded()
+    {
+        reloaded = true;
+        maybeDumpSelectedNode();
+    }
+
+    function onReSelected()
+    {
+        selected = true;
+        maybeDumpSelectedNode();
+    }
+
+    function maybeDumpSelectedNode()
+    {
+        if (!reloaded || !selected)
+            return;
+        var selectedElement = InspectorTest.firstElementsTreeOutline().selectedTreeElement;
+        var nodeName = selectedElement ? selectedElement.node().nodeNameInCorrectCase() : "null";
+        InspectorTest.addResult("Selected node: '" + nodeName + "'");
+        next();
+    }
+}
+
+}
+
