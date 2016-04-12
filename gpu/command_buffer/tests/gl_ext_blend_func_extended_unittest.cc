54c21229781ca818c64aa03f2135a834d5184ba8
zmo <zmo@chromium.org>
2016-04-12 16:21:32 -0700

    Use another mechanism to disable a test on AMD bot.
    GL_RENDERER is masked as "Chromium", so it cannot be used to identify the AMD bot.
    
    BUG=585132
    TEST=
    R=bajones@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1878393004
    
    Cr-Commit-Position: refs/heads/master@{#386856}
    
    Former-commit-id: d9ce28005c0071b2735d21ac161de0b35083a4d5

--- gpu/command_buffer/tests/gl_ext_blend_func_extended_unittest.cc
+++ gpu/command_buffer/tests/gl_ext_blend_func_extended_unittest.cc
@@ -13,6 +13,7 @@
 #include "gpu/command_buffer/service/gpu_switches.h"
 #include "gpu/command_buffer/tests/gl_manager.h"
 #include "gpu/command_buffer/tests/gl_test_utils.h"
+#include "gpu/config/gpu_test_config.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gl/gl_switches.h"
@@ -470,10 +471,11 @@ TEST_P(EXTBlendFuncExtendedES3DrawTest, ES3GettersArray) {
     return;
 
   // TODO(zmo): Figure out why this fails on AMD. crbug.com/585132.
-  std::string renderer(
-      reinterpret_cast<const char*>(glGetString(GL_RENDERER)));
-  if (renderer.find("AMD Radeon HD 6450") != std::string::npos)
+  gpu::GPUTestBotConfig bot_config;
+  if (bot_config.LoadCurrentConfig(nullptr) &&
+      bot_config.Matches("linux amd")) {
     return;
+  }
 
   const GLint kTestArraySize = 2;
   const GLint kFragData0Location = 2;
