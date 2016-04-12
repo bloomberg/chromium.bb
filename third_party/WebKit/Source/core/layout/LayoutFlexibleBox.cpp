59757d93834ea7ab60783c31d054040e01482e45
cbiesinger <cbiesinger@chromium.org>
2016-04-12 14:23:54 -0700

    [css-flexbox] Fix available space computations in the presence of flex factors < 1
    When our sum of flex factors is between 0 and 1, we don't have an accurate
    value of the remaining space. We do need to keep both the "real" value
    of remaining space (so centering works) as well as the adjusted value
    for remaining space (so flexing works correctly).
    
    This patch includes a helper struct to keep track of the three values
    we need.
    
    R=leviw@chromium.org
    CC=dgrogan@chromium.org
    BUG=576202
    
    Review URL: https://codereview.chromium.org/1879623003
    
    Cr-Commit-Position: refs/heads/master@{#386809}
    
    Former-commit-id: 6d813847e05b975678bd9a96b275e383fa5f26e8

--- third_party/WebKit/Source/core/layout/LayoutFlexibleBox.cpp
+++ third_party/WebKit/Source/core/layout/LayoutFlexibleBox.cpp
@@ -794,7 +794,7 @@ void LayoutFlexibleBox::layoutFlexItems(bool relayoutChildren, SubtreeLayoutScop
         // availableFreeSpace is the initial amount of free space in this flexbox.
         // remainingFreeSpace starts out at the same value but as we place and lay out
         // flex items we subtract from it. Note that both values can be negative.
-        LayoutUnit availableFreeSpace = containerMainInnerSize - sumFlexBaseSize;
+        const LayoutUnit availableFreeSpace = containerMainInnerSize - sumFlexBaseSize;
         LayoutUnit remainingFreeSpace = availableFreeSpace;
         FlexSign flexSign = (sumHypotheticalMainSize < containerMainInnerSize) ? PositiveFlexibility : NegativeFlexibility;
         InflexibleFlexItemSize inflexibleItems;
@@ -804,6 +804,16 @@ void LayoutFlexibleBox::layoutFlexItems(bool relayoutChildren, SubtreeLayoutScop
             ASSERT(inflexibleItems.size() > 0);
         }
 
+        // Recalculate the remaining free space. The adjustment for flex factors between 0..1 means we can't just
+        // use remainingFreeSpace here.
+        remainingFreeSpace = containerMainInnerSize;
+        for (size_t i = 0; i < orderedChildren.size(); ++i) {
+            LayoutBox* child = orderedChildren[i];
+            if (child->isOutOfFlowPositioned())
+                continue;
+            remainingFreeSpace -= (childSizes[i] + mainAxisBorderAndPaddingExtentForChild(*child)
+                + (isHorizontalFlow() ? child->marginWidth() : child->marginHeight()));
+        }
         layoutAndPlaceChildren(crossAxisOffset, orderedChildren, childSizes, remainingFreeSpace, relayoutChildren, layoutScope, lineContexts);
     }
     if (hasLineIfEmpty()) {
