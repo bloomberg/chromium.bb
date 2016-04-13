470ed4648ad820b92d05c56316307fa31b39cc59
gyzhou <gyzhou@chromium.org>
2016-04-13 08:44:56 -0700

    Enlarge window to hold media select dialog completely
    BUG=602813
    
    Review URL: https://codereview.chromium.org/1877073005
    
    Cr-Commit-Position: refs/heads/master@{#386999}
    
    Former-commit-id: 4925b57aa107ddb4231bb0974537fd3e47231a87

--- chrome/common/extensions/docs/examples/api/desktopCapture/background.js
+++ chrome/common/extensions/docs/examples/api/desktopCapture/background.js
@@ -6,7 +6,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('index.html', {
     bounds: {
       width: 1080,
-      height: 420
+      height: 550
     }
   });
 });
