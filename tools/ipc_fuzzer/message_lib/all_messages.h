4261ab1072dbe833cd3a5f9e52b199bbd62c9efa
mbarbella <mbarbella@chromium.org>
2016-04-13 09:45:53 -0700

    Remove an invalid IPC fuzzer import.
    This fixes a compile failure introduced by https://codereview.chromium.org/1877123003
    
    R=ochang@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1885043002
    
    Cr-Commit-Position: refs/heads/master@{#387006}
    
    Former-commit-id: f83d1aed812fcd08a443c8f15b78fa9ba6ee09a4

--- tools/ipc_fuzzer/message_lib/all_messages.h
+++ tools/ipc_fuzzer/message_lib/all_messages.h
@@ -19,7 +19,6 @@
 #include "components/content_settings/content/common/content_settings_message_generator.h"
 #include "components/nacl/common/nacl_host_messages.h"
 #include "components/network_hints/common/network_hints_message_generator.h"
-#include "components/password_manager/content/common/credential_manager_message_generator.h"
 #include "components/pdf/common/pdf_message_generator.h"
 #include "components/tracing/tracing_messages.h"
 #include "components/translate/content/common/translate_messages.h"
