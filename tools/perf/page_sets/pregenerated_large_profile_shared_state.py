1e3f9ecb6ab65a8401e90e6038ef32cff9ab3348
gurrrik <gurrrik@yandex-team.ru>
2016-04-13 02:14:37 -0700

    Move profile handling code to a child class of SharedPageState
    There will be a separate review for removing this logic from SharedPageState.
    More info: https://codereview.chromium.org/1871033002/
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1884493002
    
    Cr-Commit-Position: refs/heads/master@{#386943}
    
    Former-commit-id: 70390020b54d46b4d34f96429c1db62b1f0629c4

--- tools/perf/page_sets/pregenerated_large_profile_shared_state.py
+++ tools/perf/page_sets/pregenerated_large_profile_shared_state.py
@@ -3,17 +3,16 @@
 # found in the LICENSE file.
 import os
 
-from telemetry.page import shared_page_state
+from page_sets import pregenerated_profile_shared_state
 
 
-class PregeneratedLargeProfileSharedState(shared_page_state.SharedPageState):
+class PregeneratedLargeProfileSharedState(
+    pregenerated_profile_shared_state.PregeneratedProfileSharedState):
   def __init__(self, test, finder_options, story_set):
     super(PregeneratedLargeProfileSharedState, self).__init__(
         test, finder_options, story_set)
     perf_dir = os.path.normpath(
         os.path.join(os.path.dirname(__file__), os.path.pardir))
-    profile_archive_dir = os.path.join(
+    self._pregenerated_profile_archive_dir = os.path.join(
         perf_dir, 'generated_profiles', self._possible_browser.target_os,
         'large_profile.zip')
-
-    self.SetPregeneratedProfileArchiveDir(profile_archive_dir)
