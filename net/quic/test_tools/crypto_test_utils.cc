1b0ce7e0f81057b5af3a30cba63abed593f0c87e
zhongyi <zhongyi@chromium.org>
2016-04-13 10:59:22 -0700

    Fix inconsistent code in QUIC shared code
    BUG=
    
    Review URL: https://codereview.chromium.org/1882723002
    
    Cr-Commit-Position: refs/heads/master@{#387026}
    
    Former-commit-id: f74ee624e0ad5a197195c4c75650ab7521e0553b

--- net/quic/test_tools/crypto_test_utils.cc
+++ net/quic/test_tools/crypto_test_utils.cc
@@ -57,7 +57,7 @@ class CryptoFramerVisitor : public CryptoFramerVisitorInterface {
 // HexChar parses |c| as a hex character. If valid, it sets |*value| to the
 // value of the hex character and returns true. Otherwise it returns false.
 bool HexChar(char c, uint8_t* value) {
-  if (base::IsAsciiDigit(c)) {
+  if (c >= '0' && c <= '9') {
     *value = c - '0';
     return true;
   }
