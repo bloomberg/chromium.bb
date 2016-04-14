6797e51b19a24896b25686a25bdfa52816bc7016
senorblanco <senorblanco@chromium.org>
2016-04-14 15:40:19 -0700

    Add Animometer "Focus" test to tough_filters_cases, and update.
    Also update IE Pirates demo URL to use archive.org, since the site
    has been removed.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1890503002
    
    Cr-Commit-Position: refs/heads/master@{#387462}
    
    Former-commit-id: 45d9e5fc911867073607fcdf095a8be2fc1d022d

--- tools/perf/page_sets/tough_filters_cases.py
+++ tools/perf/page_sets/tough_filters_cases.py
@@ -32,6 +32,7 @@ class ToughFiltersCasesPageSet(story.StorySet):
       cloud_storage_bucket=story.PARTNER_BUCKET)
 
     urls_list = [
+      'http://rawgit.com/WebKit/webkit/master/PerformanceTests/Animometer/developer.html?test-interval=20&display=minimal&controller=fixed&frame-rate=50&kalman-process-error=1&kalman-measurement-error=4&time-measurement=performance&suite-name=Animometer&test-name=Focus&complexity=100', # pylint: disable=line-too-long
       'http://letmespellitoutforyou.com/samples/svg/filter_terrain.svg',
       'http://static.bobdo.net/Analog_Clock.svg',
     ]
@@ -40,4 +41,4 @@ class ToughFiltersCasesPageSet(story.StorySet):
       self.AddStory(ToughFiltersCasesPage(url, self))
 
     self.AddStory(PirateMarkPage(
-        'http://ie.microsoft.com/testdrive/Performance/Pirates/', self))
+        'http://web.archive.org/web/20150502135732/http://ie.microsoft.com/testdrive/Performance/Pirates/Default.html', self)) # pylint: disable=line-too-long
