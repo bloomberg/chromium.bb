ebd0ba46bd089c6a4026ea37091fd134f0ea1bdc
drott <drott@chromium.org>
2016-04-14 00:44:19 -0700

    Add OpenTypeCapsSupport class
    As preparation for implementing correct font-variant-caps we need to be
    able to check font files for their ability to support caps related
    OpenType features, such as 'smcp', 'c2sc' etc.
    
    The implementation files are split in two, one part Chromium-side
    implementation, one part Mozilla Public License'd code adapted from
    Firefox.
    
    Relanding this after revert in
    https://codereview.chromium.org/1891613002/ due to broken
    blink_platform_unittests. Splitting out the test into a separate CL
    in order to investigate a possible swarming/isolate step issue.
    
    BUG=587094
    TEST=OpenTypeCapsSupportTest.cpp
    
    Review URL: https://codereview.chromium.org/1886953002
    
    Cr-Commit-Position: refs/heads/master@{#387256}
    
    Former-commit-id: 15a403b49955e66c4b3abb8f3f93c5426d7923b1

---
+++ third_party/WebKit/Source/platform/fonts/opentype/OpenTypeCapsSupportMPL.cpp
@@ -0,0 +1,81 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * ***** END LICENSE BLOCK ***** */
+
+#include "platform/fonts/opentype/OpenTypeCapsSupport.h"
+
+#include <hb-ot.h>
+
+namespace blink  {
+
+bool OpenTypeCapsSupport::supportsOpenTypeFeature(
+    hb_script_t script,
+    uint32_t tag) const
+{
+
+    hb_face_t* face = m_harfBuzzFace->face();
+    ASSERT(face);
+
+    ASSERT((tag == HB_TAG('s', 'm', 'c', 'p')
+        || tag == HB_TAG('c', '2', 's', 'c')
+        || tag == HB_TAG('p', 'c', 'a', 'p')
+        || tag == HB_TAG('c', '2', 'p', 'c')
+        || tag == HB_TAG('s', 'u', 'p', 's')
+        || tag == HB_TAG('s', 'u', 'b', 's')
+        || tag == HB_TAG('t', 'i', 't', 'l')
+        || tag == HB_TAG('u', 'n', 'i', 'c')
+        || tag == HB_TAG('v', 'e', 'r', 't')));
+
+    bool result = false;
+
+    if (!hb_ot_layout_has_substitution(face))
+        return false;
+
+    // Get the OpenType tag(s) that match this script code
+    const size_t kMaxScriptTags = 4;
+    hb_tag_t scriptTags[kMaxScriptTags] = {
+        HB_TAG_NONE,
+        HB_TAG_NONE,
+        HB_TAG_NONE,
+        HB_TAG_NONE
+    };
+    hb_ot_tags_from_script(static_cast<hb_script_t>(script),
+        &scriptTags[0],
+        &scriptTags[1]);
+
+    // Replace the first remaining NONE with DEFAULT
+    for (size_t i = 0; i < kMaxScriptTags; ++i) {
+        if (scriptTags[i] == HB_TAG_NONE) {
+            scriptTags[i] = HB_OT_TAG_DEFAULT_SCRIPT;
+            break;
+        }
+    }
+
+    // Now check for 'smcp' under the first of those scripts that is present
+    const hb_tag_t kGSUB = HB_TAG('G', 'S', 'U', 'B');
+    for (size_t j = 0; j < kMaxScriptTags; ++j) {
+        if (scriptTags[j] == HB_TAG_NONE)
+            break;
+
+        unsigned scriptIndex;
+        if (hb_ot_layout_table_find_script(face,
+            kGSUB,
+            scriptTags[j],
+            &scriptIndex)) {
+            if (hb_ot_layout_language_find_feature(face, kGSUB,
+                scriptIndex,
+                HB_OT_LAYOUT_DEFAULT_LANGUAGE_INDEX,
+                tag, nullptr)) {
+                result = true;
+            }
+            break;
+        }
+    }
+    return result;
+}
+
+} // namespace blink
