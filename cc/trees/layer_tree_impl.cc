cb4151eb6dd8fde270661ea04a01ac57f78721e0
rjkroege <rjkroege@chromium.org>
2016-04-14 16:43:46 -0700

    Revert of cc: Add a main thread LayerListIterator (patchset #3 id:40001 of https://codereview.chromium.org/1887703002/ )
    Reason for revert:
    Per: https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29/builds/47790,
    
    I think that this CL breaks cc unit tests.
    
    [ RUN      ] LayerTreeHostCopyRequestTestLostOutputSurface.RunMultiThread_DirectRenderer
    
    c:\b\build\slave\win_builder__dbg_\build\src\cc\test\layer_tree_test.cc(899): error: Failed
    
    Test timed out
    
    [  FAILED  ] LayerTreeHostCopyRequestTestLostOutputSurface.RunMultiThread_DirectRenderer (5006 ms)
    
    Original issue's description:
    > cc: Add a main thread LayerListIterator
    >
    > Make LayerListIterator a template class for Layer and LayerImpl.
    >
    > Add tests for LayerListIterator<Layer>.
    >
    > BUG=603181
    > CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    >
    > Committed: https://crrev.com/112133622e4899d1abe5242a7bd638712bc6ee7f [formerly 7a1807b49236a338953fea5e7f7f6f6f9cbb0aa8]
    > Cr-Commit-Position: refs/heads/master@{#387422}
    
    TBR=ajuma@chromium.org,vollick@chromium.org,jaydasika@chromium.org,sunxd@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=603181
    
    Review URL: https://codereview.chromium.org/1888453006
    
    Cr-Commit-Position: refs/heads/master@{#387474}
    
    Former-commit-id: ad45a5f2de581606e585b21a120f61a159de4550

--- cc/trees/layer_tree_impl.cc
+++ cc/trees/layer_tree_impl.cc
@@ -441,20 +441,20 @@ void LayerTreeImpl::PushPropertiesTo(LayerTreeImpl* target_tree) {
   target_tree->has_ever_been_drawn_ = false;
 }
 
-LayerListIterator<LayerImpl> LayerTreeImpl::begin() {
-  return LayerListIterator<LayerImpl>(root_layer_);
+LayerListIterator LayerTreeImpl::begin() {
+  return LayerListIterator(root_layer_);
 }
 
-LayerListIterator<LayerImpl> LayerTreeImpl::end() {
-  return LayerListIterator<LayerImpl>(nullptr);
+LayerListIterator LayerTreeImpl::end() {
+  return LayerListIterator(nullptr);
 }
 
-LayerListReverseIterator<LayerImpl> LayerTreeImpl::rbegin() {
-  return LayerListReverseIterator<LayerImpl>(root_layer_);
+LayerListReverseIterator LayerTreeImpl::rbegin() {
+  return LayerListReverseIterator(root_layer_);
 }
 
-LayerListReverseIterator<LayerImpl> LayerTreeImpl::rend() {
-  return LayerListReverseIterator<LayerImpl>(nullptr);
+LayerListReverseIterator LayerTreeImpl::rend() {
+  return LayerListReverseIterator(nullptr);
 }
 
 void LayerTreeImpl::AddToElementMap(LayerImpl* layer) {
