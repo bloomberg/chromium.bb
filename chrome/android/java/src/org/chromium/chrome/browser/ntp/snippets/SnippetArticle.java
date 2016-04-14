bb017db543debe1c131a05cc02a55a3f8b3d0001
dgn <dgn@chromium.org>
2016-04-14 01:41:43 -0700

    [NTP Snippets] Add placeholders and start caching thumbnails
    Adds placeholders that are displayed while the thumbnail is being
    fetched from the network, and keeps the downloaded thumbnail while
    we stay on the NTP.
    
    BUG=601456
    
    Review URL: https://codereview.chromium.org/1871343006
    
    Cr-Commit-Position: refs/heads/master@{#387262}
    
    Former-commit-id: 204cf9c4eb767c7f8cfbd85267c4b9e87b62067d

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetArticle.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetArticle.java
@@ -3,27 +3,14 @@
 // found in the LICENSE file.
 package org.chromium.chrome.browser.ntp.snippets;
 
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
-import android.view.View;
-import android.widget.ImageView;
+import android.graphics.Bitmap;
 
-import org.chromium.base.Log;
-import org.chromium.base.StreamUtil;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageListItem;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 
 /**
  * Represents the data for an article card on the NTP.
  */
 public class SnippetArticle implements NewTabPageListItem {
-    private static final String TAG = "SnippetArticle";
-
-    private static final int FADE_IN_ANIMATION_TIME_MS = 300;
 
     public final String mTitle;
     public final String mPublisher;
@@ -33,54 +20,8 @@ public class SnippetArticle implements NewTabPageListItem {
     public final long mTimestamp;
     public final int mPosition;
 
-    private ThumbnailRenderingTask mThumbnailRenderingTask;
-
-    // Async task to create the thumbnail from a URL
-    // TODO(maybelle): This task to retrieve the thumbnail from the web is temporary while
-    // we are prototyping this feature for clank. For the real production feature, we
-    // will likely have to download/decode the thumbnail on the native side.
-    private static class ThumbnailRenderingTask extends AsyncTask<String, Void, Drawable> {
-        private ImageView mThumbnailView;
-
-        ThumbnailRenderingTask(ImageView thumbnailView) {
-            mThumbnailView = thumbnailView;
-
-            // The view might be already holding the thumbnail from another snippet, as the view
-            // is recycled. We start by hiding it to avoid having a stale image be displayed while
-            // the new one is being downloaded.
-            mThumbnailView.setVisibility(View.INVISIBLE);
-        }
-
-        @Override
-        protected Drawable doInBackground(String... params) {
-            if (params[0].isEmpty()) return null;
-            InputStream is = null;
-            try {
-                is = (InputStream) new URL(params[0]).getContent();
-                return Drawable.createFromStream(is, "thumbnail");
-            } catch (MalformedURLException e) {
-                Log.e(TAG, "Could not get image thumbnail due to malformed URL", e);
-            } catch (IOException e) {
-                Log.e(TAG, "Could not get image thumbnail", e);
-            } finally {
-                StreamUtil.closeQuietly(is);
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Drawable thumbnail) {
-            if (thumbnail == null) {
-                mThumbnailView.setVisibility(View.GONE);
-            } else {
-                // Fade in the image thumbnail
-                mThumbnailView.setImageDrawable(thumbnail);
-                mThumbnailView.setVisibility(View.VISIBLE);
-                mThumbnailView.setAlpha(0f);
-                mThumbnailView.animate().alpha(1f).setDuration(FADE_IN_ANIMATION_TIME_MS);
-            }
-        }
-    }
+    /** Bitmap of the thumbnail, fetched lazily, when the RecyclerView wants to show the snippet. */
+    private Bitmap mThumbnailBitmap;
 
     /**
      * Creates a SnippetArticle object that will hold the data
@@ -109,15 +50,15 @@ public class SnippetArticle implements NewTabPageListItem {
     }
 
     /**
-     * Retrieves this SnippetArticle's thumbnail asynchronously and sets it onto the given
-     * ImageView.
-     *
-     * @param view The ImageView to set the thumbnail onto.
+     * Returns this article's tumbnail as a {@link Bitmap}. Can return {@code null} as it is
+     * initially unset.
      */
-    public void setThumbnailOnView(ImageView view) {
-        if (mThumbnailRenderingTask != null) mThumbnailRenderingTask.cancel(true);
+    Bitmap getThumbnailBitmap() {
+        return mThumbnailBitmap;
+    }
 
-        mThumbnailRenderingTask = new ThumbnailRenderingTask(view);
-        mThumbnailRenderingTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, mThumbnailUrl);
+    /** Sets the tumbnail bitmap for this article. */
+    void setThumbnailBitmap(Bitmap bitmap) {
+        mThumbnailBitmap = bitmap;
     }
 }
--- chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetArticleViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/snippets/SnippetArticleViewHolder.java
@@ -4,6 +4,14 @@
 
 package org.chromium.chrome.browser.ntp.snippets;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.media.ThumbnailUtils;
+import android.os.AsyncTask;
 import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +19,8 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import org.chromium.base.Log;
+import org.chromium.base.StreamUtil;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
@@ -19,15 +29,24 @@ import org.chromium.chrome.browser.ntp.NewTabPageView.NewTabPageManager;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageListItem;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageViewHolder;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 /**
  * A class that represents the view for a single card snippet.
  */
 public class SnippetArticleViewHolder extends NewTabPageViewHolder implements View.OnClickListener {
+    private static final String TAG = "NtpSnippets";
+    private static final int FADE_IN_ANIMATION_TIME_MS = 300;
+
     private final NewTabPageManager mNewTabPageManager;
-    public TextView mHeadlineTextView;
-    public TextView mPublisherTextView;
-    public TextView mArticleSnippetTextView;
-    public ImageView mThumbnailView;
+    private final TextView mHeadlineTextView;
+    private final TextView mPublisherTextView;
+    private final TextView mArticleSnippetTextView;
+    private final ImageView mThumbnailView;
+
+    private AsyncTask<String, Void, Bitmap> mThumbnailFetchingTask;
     public String mUrl;
     public int mPosition;
 
@@ -90,7 +109,72 @@ public class SnippetArticleViewHolder extends NewTabPageViewHolder implements Vi
         mUrl = item.mUrl;
         mPosition = item.mPosition;
 
-        item.setThumbnailOnView(mThumbnailView);
+        updateThumbnail(item);
+    }
+
+    private void updateThumbnail(final SnippetArticle snippet) {
+        // If this view has a pending fetching task, it will display the stale thumbnail when it
+        // finishes, so we need to cancel that task.
+        if (mThumbnailFetchingTask != null) mThumbnailFetchingTask.cancel(true);
+
+        if (snippet.getThumbnailBitmap() != null) {
+            mThumbnailView.setImageBitmap(snippet.getThumbnailBitmap());
+            return;
+        }
+
+        mThumbnailView.setImageResource(R.drawable.ic_snippet_thumbnail_placeholder);
+
+        if (snippet.mThumbnailUrl.isEmpty()) {
+            Log.e(TAG, "Could not get image thumbnail due to empty URL");
+            return;
+        }
+
+        mThumbnailFetchingTask = new AsyncTask<String, Void, Bitmap>() {
+
+            @Override
+            protected Bitmap doInBackground(String... params) {
+                InputStream is = null;
+                try {
+                    is = new URL(params[0]).openStream();
+                    return BitmapFactory.decodeStream(is);
+                } catch (IOException e) {
+                    Log.e(TAG, "Could not get image thumbnail", e);
+                } finally {
+                    StreamUtil.closeQuietly(is);
+                }
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap result) {
+                if (result == null) return; // Nothing to do, we keep the placeholder.
+                fadeThumbnailIn(snippet, result);
+            }
+        };
+
+        mThumbnailFetchingTask.executeOnExecutor(
+                AsyncTask.THREAD_POOL_EXECUTOR, snippet.mThumbnailUrl);
+    }
+
+    private void fadeThumbnailIn(SnippetArticle snippet, Bitmap thumbnail) {
+        // We need to crop and scale the downloaded bitmap, as the ImageView we set it on won't be
+        // able to do so when using a TransitionDrawable (as opposed to the straight bitmap).
+        // That's a limitation of TransitionDrawable, which doesn't handle layers of varying sizes.
+        Resources res = mThumbnailView.getResources();
+        int targetSize = res.getDimensionPixelSize(R.dimen.snippets_thumbnail_size);
+        Bitmap scaledThumbnail = ThumbnailUtils.extractThumbnail(
+                thumbnail, targetSize, targetSize, ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+
+        // Store the bitmap to skip the download task next time we display this snippet.
+        snippet.setThumbnailBitmap(scaledThumbnail);
+
+        // Cross-fade between the placeholder and the thumbnail.
+        Drawable[] layers = {mThumbnailView.getDrawable(),
+                new BitmapDrawable(mThumbnailView.getResources(), scaledThumbnail)};
+        TransitionDrawable transitionDrawable = new TransitionDrawable(layers);
+        mThumbnailView.setImageDrawable(transitionDrawable);
+        transitionDrawable.startTransition(FADE_IN_ANIMATION_TIME_MS);
     }
 
     @Override
