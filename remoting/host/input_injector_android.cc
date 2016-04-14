ca7a707c43988106e65a43a500d0db7ab174cbb9
gabadie <gabadie@chromium.org>
2016-04-14 09:54:52 -0700

    Fixes clank down-stream compilation failures of input_injector_android.cc
    BUG=602355
    
    TBR=lambroslambrou@chromium.org, sergeyu@chromium.org,
    
    Review URL: https://codereview.chromium.org/1883153002
    
    Cr-Commit-Position: refs/heads/master@{#387340}
    
    Former-commit-id: f8563372f7ba26f49ea5b4f9fcff0ef4a5a5ac68

--- remoting/host/input_injector_android.cc
+++ remoting/host/input_injector_android.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "remoting/host/input_injector.h"
 
