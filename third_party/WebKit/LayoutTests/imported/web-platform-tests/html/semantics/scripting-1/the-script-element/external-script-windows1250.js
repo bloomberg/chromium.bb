946f4c9ae4047cc05ec1305ead26289fae0997b7
kochi <kochi@chromium.org>
2016-04-17 22:46:13 -0700

    Import web-platform-tests@2bd72173f4580563f51eaadd6c430465be4183be
    Using update-w3c-deps in Blink c5972fc41208fb767bf66eaca74392673405ca09 [formerly 367b26a9cdb1389d3e16c30527742f7cd33ef74d].
    
    This intends to include the massive cleanups in
    shadow-dom/untriaged directory that were made recently.
    
    - W3CImportExpectations changes
      - Exclude new dirs (generic-sensor, compat)
      - Exclude unsupported tests/scripts
        (for the same reason as those removed in
        https://codereview.chromium.org/1872233002,
        requiring running python script from wptserve)
    - TestExpectations changes
      - Removed web-animations/animation-node/idlharness.html
        entry (removed in upstream)
      - Removed mediacapture-streams/MediaDevices-getUserMedia.html
        (now passes the test)
      - Added html/rendering/replaced-elements/embedded-content-rendering-rules/canvas-fallback.html
        (new)
    
    BUG=490511, 505364, 600261
    
    Review URL: https://codereview.chromium.org/1899623002
    
    Cr-Commit-Position: refs/heads/master@{#387861}
    
    Former-commit-id: abf5e76789c4fd14f617d1cc04ddf3ee860dae41

--- third_party/WebKit/LayoutTests/imported/web-platform-tests/html/semantics/scripting-1/the-script-element/external-script-windows1250.js
+++
@@ -1,5 +0,0 @@
-(function() {
-  window.getSomeString = function() {
-    return "æ¹¿"; //<- these are five Polish letters, similar to scazz. It can be read correctly only with windows 1250 encoding.
-  };
-})();
