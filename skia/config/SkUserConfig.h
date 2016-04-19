c098777230f2f9b3316a84b0c66b57724a524eb4
reed <reed@google.com>
2016-04-19 14:13:00 -0700

    add SK_SUPPORT_LEGACY_IMAGEFILTER_CTM for future skia cl
    in support of this CL https://codereview.chromium.org/1899263002/
    
    Note: there is a related flag with a different name already in blink, which controls whether blink also tries to handle affine ctms and filters. I think that is somewhat orthogonal, as this new flag controls what skia does by default, and for rebaselining we may want to change these flags separately (or not). See https://codereview.chromium.org/1205413003
    
    BUG=
    
    Review URL: https://codereview.chromium.org/1898193005
    
    Cr-Commit-Position: refs/heads/master@{#388309}
    
    Former-commit-id: dafbff4e06b16d2005b0bdc077e5ea7f2642237a

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -242,6 +242,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_TYPEFACE_PTR
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_IMAGEFILTER_CTM
+#   define SK_SUPPORT_LEGACY_IMAGEFILTER_CTM
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
