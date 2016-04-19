10cd1de58adb5dd6330390174f540a799e1666c0
eugenebut <eugenebut@chromium.org>
2016-04-19 09:14:53 -0700

    [ios] Do not allow loading chrome:// URLs from regular http/https pages.
    BUG=604086
    
    Review URL: https://codereview.chromium.org/1900783003
    
    Cr-Commit-Position: refs/heads/master@{#388217}
    
    Former-commit-id: 4998fbe285b9c857b454b70a2bfeda562a92c3ae

--- ios/web/web_state/ui/crw_web_controller.mm
+++ ios/web/web_state/ui/crw_web_controller.mm
@@ -4842,9 +4842,15 @@ const NSTimeInterval kSnapshotOverlayTransition = 0.5;
     // TODO(crbug.com/546347): Extract necessary tasks for app specific URL
     // navigation rather than restarting the load.
     if (web::GetWebClient()->IsAppSpecificURL(webViewURL)) {
-      [self abortWebLoad];
-      NavigationManager::WebLoadParams params(webViewURL);
-      [self loadWithParams:params];
+      // Renderer-initiated loads of WebUI can be done only from other WebUI
+      // pages. WebUI pages may have increased power and using the same web
+      // process (which may potentially be controller by an attacker) is
+      // dangerous.
+      if (web::GetWebClient()->IsAppSpecificURL(_documentURL)) {
+        [self abortWebLoad];
+        NavigationManager::WebLoadParams params(webViewURL);
+        [self loadWithParams:params];
+      }
       return;
     } else {
       [self registerLoadRequest:webViewURL];
