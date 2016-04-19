900392f6a0bf2b50f55de3fa053f8e1df3d1392c
phajdan.jr@chromium.org <phajdan.jr@chromium.org>
2016-04-19 13:06:45 +0000

    depot_tools: expose path to gsutil.py
    This is needed for https://codereview.chromium.org/1896113002
    
    BUG=chromium:585401, chromium:593999
    
    Review URL: https://codereview.chromium.org/1897743004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@300011 0039d316-1c4b-4281-b951-d872f2087c98

--- recipe_modules/depot_tools/api.py
+++ recipe_modules/depot_tools/api.py
@@ -10,6 +10,10 @@ class DepotToolsApi(recipe_api.RecipeApi):
     return self.package_repo_resource('download_from_google_storage.py')
 
   @property
+  def gsutil_py_path(self):
+    return self.package_repo_resource('gsutil.py')
+
+  @property
   def ninja_path(self):
     ninja_exe = 'ninja.exe' if self.m.platform.is_win else 'ninja'
     return self.package_repo_resource(ninja_exe)
