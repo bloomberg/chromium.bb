ad2663603c91003a3faacb13159d46ce96f3c774
fs <fs@opera.com>
2016-04-19 11:23:18 -0700

    Improve default value handling for SVGSVGElement.width/height
    When a length attribute is either removed or set to an invalid value,
    it would get set to '0'. It should however be set to the initial value
    specified for the attribute (possibly presentation attribute) in
    question.
    
    The test svg/custom/disallow-non-lengths-in-attrs.html is tweaked to
    accommodate for this.
    
    BUG=604093
    
    Review URL: https://codereview.chromium.org/1901673003
    
    Cr-Commit-Position: refs/heads/master@{#388250}
    
    Former-commit-id: 6cd726f3f3ed5d5fecfe725bb3c85dc2cac39e85

--- third_party/WebKit/Source/core/svg/SVGSVGElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGSVGElement.cpp
@@ -193,6 +193,14 @@ void SVGSVGElement::parseAttribute(const QualifiedName& name, const AtomicString
     } else if (name == HTMLNames::onerrorAttr) {
         document().setWindowAttributeEventListener(EventTypeNames::error, createAttributeEventListener(document().frame(), name, value, eventParameterName()));
     } else if (SVGZoomAndPan::parseAttribute(name, value)) {
+    } else if (name == SVGNames::widthAttr || name == SVGNames::heightAttr) {
+        SVGAnimatedLength* property = name == SVGNames::widthAttr ? m_width : m_height;
+        SVGParsingError parseError;
+        if (!value.isNull())
+            parseError = property->setBaseValueAsString(value);
+        if (parseError != SVGParseStatus::NoError || value.isNull())
+            property->setDefaultValueAsString("100%");
+        reportAttributeParsingError(parseError, name, value);
     } else {
         SVGElement::parseAttribute(name, oldValue, value);
     }
