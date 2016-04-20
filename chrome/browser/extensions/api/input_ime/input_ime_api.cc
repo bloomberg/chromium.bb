e0d357cb6d6f3264d40e11c6d580e1b47230d5b6
azurewei <azurewei@chromium.org>
2016-04-19 22:39:06 -0700

    Instead of using ProfileCompare in InputImeEventRouterFactory::router_map_ when trying to remove profile, just compares the profile directly.
    BUG=602746
    TEST=Verfied not repo on local build.
    
    Review URL: https://codereview.chromium.org/1896993003
    
    Cr-Commit-Position: refs/heads/master@{#388431}
    
    Former-commit-id: 13814091e327ebc1792dfb329b88d2289568a78a

--- chrome/browser/extensions/api/input_ime/input_ime_api.cc
+++ chrome/browser/extensions/api/input_ime/input_ime_api.cc
@@ -264,7 +264,7 @@ void InputImeEventRouterFactory::RemoveProfile(Profile* profile) {
   if (!profile || router_map_.empty())
     return;
   auto it = router_map_.find(profile);
-  if (it != router_map_.end()) {
+  if (it != router_map_.end() && it->first == profile) {
     delete it->second;
     router_map_.erase(it);
   }
