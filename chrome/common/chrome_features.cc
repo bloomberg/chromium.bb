5a118ad67442437a0d588d10c342d395e1eeb037
azurewei <azurewei@chromium.org>
2016-04-19 17:28:52 -0700

    Turn off the opt-in IME menu feature flag.
    As this launch bug was moved to M52, we should disable the feature flag: OptInImeMenu by default.
    
    BUG=570761
    TEST=None
    
    Review URL: https://codereview.chromium.org/1903473002
    
    Cr-Commit-Position: refs/heads/master@{#388377}
    
    Former-commit-id: 7169294bab6add599edec7d1d739b9514ac0e610

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -72,7 +72,7 @@ const base::Feature kSyzyasanDeferredFree{"SyzyasanDeferredFree",
 #if defined(OS_CHROMEOS)
 // Enables or disables the opt-in IME menu in the language settings page.
 const base::Feature kOptInImeMenu{"OptInImeMenu",
-                                  base::FEATURE_ENABLED_BY_DEFAULT};
+                                  base::FEATURE_DISABLED_BY_DEFAULT};
 #endif  // defined(OS_CHROMEOS)
 
 }  // namespace features
