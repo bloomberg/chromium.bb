16b4a3a3d3296b39d2cbba3342b008944b357df9
mad <mad@chromium.org>
2016-04-20 12:39:45 -0700

    Explicitly use HTTPS to download the CCT binary
    TBR=mattm@chromium.org
    BUG=598752
    
    Review URL: https://codereview.chromium.org/1908613002
    
    Cr-Commit-Position: refs/heads/master@{#388555}
    
    Former-commit-id: ff7b8097333f0abd117606334dc925b09a2b247f

--- chrome/browser/safe_browsing/srt_field_trial_win.cc
+++ chrome/browser/safe_browsing/srt_field_trial_win.cc
@@ -25,10 +25,10 @@ const char kSRTReporterOffGroup[] = "Off";
 
 // The download links of the Software Removal Tool.
 const char kMainSRTDownloadURL[] =
-    "http://dl.google.com/dl"
+    "https://dl.google.com/dl"
     "/softwareremovaltool/win/chrome_cleanup_tool.exe?chrome-prompt=1";
 const char kCanarySRTDownloadURL[] =
-    "http://dl.google.com/dl"
+    "https://dl.google.com/dl"
     "/softwareremovaltool/win/c/chrome_cleanup_tool.exe?chrome-prompt=1";
 
 }  // namespace
