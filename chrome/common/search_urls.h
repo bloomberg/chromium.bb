b4a0261f4eee550068afd9467b83c23c8cdcfd35
treib <treib@chromium.org>
2016-04-20 08:16:45 -0700

    Add comment for search::MatchesOriginAndPath
    Also add an OWNERS entry for chrome/common/search_urls*
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/1891233002
    
    Cr-Commit-Position: refs/heads/master@{#388500}
    
    Former-commit-id: 63356ae5b201b39905d461c32e91b78b7552faf4

--- chrome/common/search_urls.h
+++ chrome/common/search_urls.h
@@ -9,7 +9,11 @@ class GURL;
 
 namespace search {
 
-// Returns true if |my_url| matches |other_url|.
+// Returns true if |my_url| matches |other_url| in terms of origin (i.e. host,
+// port, and scheme) and path. As a special case, this also matches if |my_url|
+// is "https://" and |other_url| is "http://" (and host, port, and path match).
+// This is so that "https://" URLs will be considered "Instant URLs" even if the
+// default search provider URL is "http://".
 bool MatchesOriginAndPath(const GURL& my_url, const GURL& other_url);
 
 }  // namespace search
