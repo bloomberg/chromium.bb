3d416933be4f1d658b64a6e43b7d6b5061c5cb87
jmadill <jmadill@chromium.org>
2016-04-21 09:22:10 -0700

    Revert of command_buffer_gles2: Implement EGL default Display as a global object (patchset #21 id:400001 of https://codereview.chromium.org/1714883002/ )
    Reason for revert:
    Seems to be failing the gles2_conform_test of all flavours on all bots.
    
    First fail:
    https://build.chromium.org/p/chromium.gpu.fyi/builders/Win7%20Release%20%28NVIDIA%29/builds/24198
    https://build.chromium.org/p/chromium.gpu.fyi/builders/Linux%20Release%20%28NVIDIA%20GeForce%20730%29/builds/840
    
    Original issue's description:
    > command_buffer_gles2: Implement EGL default Display as a global object
    >
    > Make EGL Display a global. eglGetDisplay(EGL_DEFAULT_DISPLAY) will
    > return the same object.
    >
    > Store EGL state in a new thread-local object (ThreadState). One
    > display, multiple surfaces, multiple contexts.
    >
    > Fix the currently implemented entry points to return specified errors
    > and behave as specified. Notable changes:
    > * eglGetDisplay() does not create new displays for each invocation
    > * eglTerminate() invalidates the created egl objects but leaves the
    >   current context intact as specified
    > * eglMakeCurrent() is implemented more correctly with refcount semantics
    > * eglQueryStrings() works without display for some names
    > * eglReleaseThread() is implemeted
    > * Config are now separate for pbuffers and windows, so that contexts
    >   can be created separate from surfaces
    > * Context can be made current with different surfaces
    > * Catches errors related to context being set current to multiple
    >   threads
    > * Tries to be thread-safe
    >
    > In the implementation, the Display contains a lock ensuring all thread
    > accesses are consistent. All functions using or changing thread-local
    > state have explicit egl::ThreadState parameter.
    >
    > BUG=581634
    > CQ_INCLUDE_TRYBOTS=tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Committed: https://crrev.com/35c41501620e7999a5fd78eb99c487f0d23bfa1f [formerly de9aa3be41bca094454babbd62907815fad7f5c2]
    > Cr-Commit-Position: refs/heads/master@{#377535}
    >
    > Committed: https://crrev.com/2151a975947d621a6677ca77a47c3eff0e3e0cfe [formerly 7b01a0867be0895009b120f37fcd4166a84f404b]
    > Cr-Commit-Position: refs/heads/master@{#386613}
    >
    > Committed: https://crrev.com/419038d648ca6e047fc8e1d43185c6d6a8031d99 [formerly f9d5c7e6fd53570c0decc63e8ec51d30e0701c89]
    > Cr-Commit-Position: refs/heads/master@{#387871}
    
    TBR=piman@chromium.org,thakis@chromium.org,vabr@chromium.org,kkinnunen@nvidia.com
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=581634
    
    Review URL: https://codereview.chromium.org/1900563007
    
    Cr-Commit-Position: refs/heads/master@{#388784}
    
    Former-commit-id: 50e5602ba08b92a495c9855f3118898591e33dce

--- gpu/gles2_conform_support/egl/test_support.h
+++ gpu/gles2_conform_support/egl/test_support.h
@@ -5,8 +5,6 @@
 #ifndef GPU_GLES2_CONFORM_SUPPORT_EGL_TEST_SUPPORT_H_
 #define GPU_GLES2_CONFORM_SUPPORT_EGL_TEST_SUPPORT_H_
 
-#include <EGL/egl.h>
-
 #if defined(COMPONENT_BUILD) && defined(COMMAND_BUFFER_GLES_LIB_SUPPORT_ONLY)
 // A variable used for communicating whether the app has initialized the global
 // variables.
@@ -18,15 +16,4 @@
 EGLAPI extern EGLAPIENTRY bool g_command_buffer_gles_has_atexit_manager;
 #endif
 
-extern "C" {
-// A function to support GTF windowless tests. gles2_conform_test_windowless and
-// khronos_glcts_test_windowless create "windowless" native windows and render
-// to those. The test runners do not at the moment implement creating said
-// windowless native windows. This call sets the system so that it will create a
-// pbuffer when eglCreateWindow is called.
-EGLAPI EGLAPIENTRY void
-CommandBufferGLESSetNextCreateWindowSurfaceCreatesPBuffer(EGLDisplay eglDisplay,
-                                                          EGLint width,
-                                                          EGLint height);
-}
 #endif
