ead4e30fbdcc83b4c30e5d9d678e242fe23d7f5f
mstensho <mstensho@opera.com>
2016-04-21 12:09:21 -0700

    Prioritize first-lines over orphans when deciding whether to propagate a strut.
    The orphans code piece simply blindly propagates the strut to the block if the
    orphans requirement isn't satisfied. It's incapable of inserting a break at a
    class C break point [1] (i.e. before the first line in the block), while the
    first-line code piece handles this just fine (since this is easy to do when at
    the first line, but not when at a later line).
    
    If we're at a non-first line at the time of breaking, there's no support for
    going back to insert a break before the first line. That has to be done when
    processing the first line, not later on. Flip the priority, so that orphans
    requirements aren't even checked if we're at the first line. There's no need
    then. We always try to avoid breaking before the first line anyway.
    
    [1] https://drafts.csswg.org/css-break/#possible-breaks
    
    Review URL: https://codereview.chromium.org/1908643003
    
    Cr-Commit-Position: refs/heads/master@{#388838}
    
    Former-commit-id: 26d004ea56c7db51b1dc19ba21670587604612d8

--- third_party/WebKit/Source/core/layout/LayoutBlockFlow.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlockFlow.cpp
@@ -798,26 +798,37 @@ LayoutUnit LayoutBlockFlow::adjustBlockChildForPagination(LayoutUnit logicalTop,
 
 static bool shouldSetStrutOnBlock(const LayoutBlockFlow& block, const RootInlineBox& lineBox, LayoutUnit lineLogicalOffset, int lineIndex, LayoutUnit pageLogicalHeight)
 {
-    bool wantsStrutOnBlock = false;
-    if (!block.style()->hasAutoOrphans() && block.style()->orphans() >= lineIndex) {
-        // Not enough orphans here. Push the entire block to the next column / page as an
-        // attempt to better satisfy the orphans requirement.
-        wantsStrutOnBlock = true;
-    } else if (lineBox == block.firstRootBox() && lineLogicalOffset == block.borderAndPaddingBefore()) {
+    if (lineBox == block.firstRootBox()) {
         // This is the first line in the block. We can take the whole block with us to the next page
         // or column, rather than keeping a content-less portion of it in the previous one. Only do
         // this if the line is flush with the content edge of the block, though. If it isn't, it
         // means that the line was pushed downwards by preceding floats that didn't fit beside the
         // line, and we don't want to move all that, since it has already been established that it
-        // fits nicely where it is.
+        // fits nicely where it is. In this case we have a class "C" break point [1] in front of
+        // this line.
+        //
+        // [1] https://drafts.csswg.org/css-break/#possible-breaks
+        if (lineLogicalOffset > block.borderAndPaddingBefore())
+            return false;
+
         LayoutUnit lineHeight = lineBox.lineBottomWithLeading() - lineBox.lineTopWithLeading();
         LayoutUnit totalLogicalHeight = lineHeight + lineLogicalOffset.clampNegativeToZero();
         // It's rather pointless to break before the block if the current line isn't going to
         // fit in the same column or page, so check that as well.
-        if (totalLogicalHeight <= pageLogicalHeight)
-            wantsStrutOnBlock = true;
+        if (totalLogicalHeight > pageLogicalHeight)
+            return false;
+    } else {
+        if (block.style()->hasAutoOrphans() || lineIndex > block.style()->orphans())
+            return false;
+
+        // Not enough orphans here. Push the entire block to the next column / page as an attempt to
+        // better satisfy the orphans requirement.
+        //
+        // Note that we should ideally check if the first line in the block is flush with the
+        // content edge of the block here, because if it isn't, we should break at the class "C"
+        // break point in front of the first line, rather than before the entire block.
     }
-    return wantsStrutOnBlock && block.allowsPaginationStrut();
+    return block.allowsPaginationStrut();
 }
 
 void LayoutBlockFlow::adjustLinePositionForPagination(RootInlineBox& lineBox, LayoutUnit& delta)
