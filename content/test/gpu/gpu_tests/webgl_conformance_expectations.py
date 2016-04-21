57b89c6be54393ca9a8d5a45948a4032f38a44e3
kbr <kbr@chromium.org>
2016-04-20 20:05:23 -0700

    Remove flaky timeout suppression for TexImage2D with RGB/UNSIGNED_BYTE.
    An unscientific sampling of linux_chromium_rel_ng try jobs shows that
    this test is not flakily failing any more. I suspect the fix for Issue
    586183 has fixed it, and am going to duplicate Issue 596622 into it.
    
    BUG=596622
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review URL: https://codereview.chromium.org/1904873002
    
    Cr-Commit-Position: refs/heads/master@{#388671}
    
    Former-commit-id: 33fe94fcfa642ddc4a8c02c5eebee5d9211dc0f1

--- content/test/gpu/gpu_tests/webgl_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl_conformance_expectations.py
@@ -197,9 +197,6 @@ class WebGLConformanceExpectations(GpuTestExpectations):
               ['linux', 'nvidia'], bug=544989) # Too flaky to retry
     self.Flaky('conformance/extensions/oes-element-index-uint.html',
                ['linux', 'nvidia'], bug=524144)
-    self.Flaky('conformance/textures/image/' +
-               'tex-2d-rgb-rgb-unsigned_byte.html',
-               ['linux', 'nvidia'], bug=596622)
     # AMD
     self.Flaky('conformance/more/functions/uniformi.html',
                ['linux', 'amd'], bug=550989)
