409ff3dd1bfeceb570685bc73f328fa9e583a2c2
fsamuel <fsamuel@chromium.org>
2016-04-22 12:14:07 -0700

    Introduce GpuServiceMus skeleton
    This is the first step of many to use the newly refactored gpu/ipc code
    in Mus. I'm introducing this skeleton to more easily parallelize further
    work here.
    
    BUG=586390
    
    patch from issue 1903243003 at patchset 120001 (http://crrev.com/1903243003#ps120001)
    TBR=piman@chromium.org, sky@chromium.org, rjkroege@chromium.org
    
    Review URL: https://codereview.chromium.org/1911043002
    
    Cr-Commit-Position: refs/heads/master@{#389203}
    
    Former-commit-id: b25f854150972b9961bee37cc76103ec911aed0a

---
+++ components/mus/gpu/gpu_service_mus.cc
@@ -0,0 +1,67 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/mus/gpu/gpu_service_mus.h"
+
+#include "gpu/ipc/common/surface_handle.h"
+
+namespace mus {
+
+GpuServiceMus::GpuServiceMus() {}
+
+GpuServiceMus::~GpuServiceMus() {}
+
+void GpuServiceMus::DidCreateOffscreenContext(const GURL& active_url) {
+  NOTIMPLEMENTED();
+}
+
+void GpuServiceMus::DidDestroyChannel(int client_id) {
+  NOTIMPLEMENTED();
+}
+
+void GpuServiceMus::DidDestroyOffscreenContext(const GURL& active_url) {
+  NOTIMPLEMENTED();
+}
+
+void GpuServiceMus::DidLoseContext(bool offscreen,
+                                   gpu::error::ContextLostReason reason,
+                                   const GURL& active_url) {
+  NOTIMPLEMENTED();
+}
+
+void GpuServiceMus::GpuMemoryUmaStats(const gpu::GPUMemoryUmaStats& params) {
+  NOTIMPLEMENTED();
+}
+
+void GpuServiceMus::StoreShaderToDisk(int32_t client_id,
+                                      const std::string& key,
+                                      const std::string& shader) {
+  NOTIMPLEMENTED();
+}
+
+#if defined(OS_MACOSX)
+void GpuServiceMus::SendAcceleratedSurfaceBuffersSwapped(
+    int32_t surface_id,
+    CAContextID ca_context_id,
+    const gfx::ScopedRefCountedIOSurfaceMachPort& io_surface,
+    const gfx::Size& size,
+    float scale_factor,
+    std::vector<ui::LatencyInfo> latency_info) {
+  NOTIMPLEMENTED();
+}
+#endif
+
+#if defined(OS_WIN)
+void GpuServiceMus::SendAcceleratedSurfaceCreatedChildWindow(
+    gpu::SurfaceHandle parent_window,
+    gpu::SurfaceHandle child_window) {
+  NOTIMPLEMENTED();
+}
+#endif
+
+void GpuServiceMus::SetActiveURL(const GURL& url) {
+  NOTIMPLEMENTED();
+}
+
+}  // namespace mus
