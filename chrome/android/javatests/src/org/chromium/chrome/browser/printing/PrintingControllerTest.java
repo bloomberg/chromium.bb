228e1d7043d61f2cb37d588b04815ebe193202c3
apacible <apacible@chromium.org>
2016-04-21 21:10:44 -0700

    Disable PrintingControllerTest.testPrintClosedWindow.
    This test has been flaky on the bots. We're unsure how long it's been flaky since it was reenabled, but it failed a few times on the linux_android_rel_ng trybot in the past few hours.
    
    BUG=532652
    TBR=dgn@chromium.org
    
    Review URL: https://codereview.chromium.org/1912963002
    
    Cr-Commit-Position: refs/heads/master@{#389027}
    
    Former-commit-id: 1628e3607dac39636d2b9fb0e0188ded7e8e588f

--- chrome/android/javatests/src/org/chromium/chrome/browser/printing/PrintingControllerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/printing/PrintingControllerTest.java
@@ -13,10 +13,10 @@ import android.print.PrintAttributes;
 import android.print.PrintDocumentAdapter;
 import android.print.PrintDocumentInfo;
 import android.test.suitebuilder.annotation.LargeTest;
-import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.TestFileUtil;
 import org.chromium.base.test.util.UrlUtils;
@@ -157,11 +157,14 @@ public class PrintingControllerTest extends ChromeActivityTestCaseBase<ChromeAct
 
     /**
      * Test for http://crbug.com/528909
+     *
+     * Bug: http://crbug.com/532652
+     * @SmallTest
+     * @Feature({"Printing"})
      */
-    @SmallTest
-    @Feature({"Printing"})
     @CommandLineFlags.Add(
             {ContentSwitches.DISABLE_POPUP_BLOCKING, ChromeSwitches.DISABLE_DOCUMENT_MODE})
+    @DisabledTest
     public void testPrintClosedWindow() throws Throwable {
         if (!ApiCompatibilityUtils.isPrintingSupported()) return;
 
