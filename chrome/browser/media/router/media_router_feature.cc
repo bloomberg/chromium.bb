f9c5f05e1c8d9b057892bafb97f3b84c988f5151
imcheng <imcheng@chromium.org>
2016-04-22 11:30:30 -0700

    [Media Router] Ensure the same FeatureSwitch is used for lifetime of the browser.
    We noticed that with Cast Extension enabled, the component action
    toolbar logic will end up using the EnableMediaRouterWithCastExtension
    field trial during the call to MediaRouterEnabled(). If so, the
    Cast Extension will be unloaded. Code executed subsequently use
    EnableMediaRouter field trial during the call to MediaRouterEnabled().
    This could be a problem for users with certain field trial group
    configurations and results in inconsistencies (e.g., has Cast button
    in toolbar, but opens error page when clicked due to WebUI not found).
    
    The decision is to no longer rely on the EMRWCE field trial. For one,
    the mechanism to switch on BrowserContext to return the correct field
    trial consistently require a complicated mechanism, and we will still
    end up with some corner cases that result in strange behavior. In
    addition, having both field trials make analyzing the data more
    difficult.
    
    BUG=605165
    
    Review URL: https://codereview.chromium.org/1907673002
    
    Cr-Commit-Position: refs/heads/master@{#389175}
    
    Former-commit-id: f735fb21eb9fdc1cade2b8a30217b8c5dc626b90

--- chrome/browser/media/router/media_router_feature.cc
+++ chrome/browser/media/router/media_router_feature.cc
@@ -8,9 +8,6 @@
 #include "content/public/browser/browser_context.h"
 
 #if defined(ENABLE_EXTENSIONS)
-#include "chrome/browser/extensions/api/tab_capture/tab_capture_api.h"
-#include "extensions/browser/extension_registry.h"
-#include "extensions/common/extension_set.h"
 #include "extensions/common/feature_switch.h"
 #endif
 
@@ -21,22 +18,6 @@ bool MediaRouterEnabled(content::BrowserContext* context) {
 #if defined(OS_ANDROID)
   return true;
 #elif defined(ENABLE_EXTENSIONS)
-  extensions::ExtensionRegistry* registry =
-      extensions::ExtensionRegistry::Get(context);
-  if (!registry) {
-    DLOG(ERROR) << "ExtensionRegistry is null. Assume no cast extension "
-                   "installed.";
-    return extensions::FeatureSwitch::media_router()->IsEnabled();
-  }
-
-  const extensions::ExtensionSet& extension_set =
-      registry->enabled_extensions();
-  if (extension_set.Contains(extensions::kStableChromecastExtensionId) ||
-      extension_set.Contains(extensions::kBetaChromecastExtensionId)) {
-    return extensions::FeatureSwitch::media_router_with_cast_extension()
-        ->IsEnabled();
-  }
-
   return extensions::FeatureSwitch::media_router()->IsEnabled();
 #else  // !defined(ENABLE_EXTENSIONS)
   return false;
