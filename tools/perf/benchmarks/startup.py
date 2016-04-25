894eddbce23ba8e6f3ba5eab77d1d95313c7cf85
fmeawad <fmeawad@chromium.org>
2016-04-25 11:48:52 -0700

    [PerfBotSherrif] Rename OS name for disabled startup benchmark
    I got the OS name wrong the first time in http://codereview.chromium.org/1914543003
    
    BUG=605485
    
    TBR=nednguyen@google.com,aiolos@chromium.org
    
    Disabling the extra trybots:
    tryserver.chromium.perf:android_s5_perf_cq;tryserver.chromium.perf:winx64_10_perf_cq;tryserver.chromium.perf:mac_retina_perf_cq;tryserver.chromium.perf:linux_perf_cq
    
    Review URL: https://codereview.chromium.org/1913903002
    
    Cr-Commit-Position: refs/heads/master@{#389526}
    
    Former-commit-id: 0735876b2703bd85e023d84193a69c0a64ae5c7d

--- tools/perf/benchmarks/startup.py
+++ tools/perf/benchmarks/startup.py
@@ -61,7 +61,7 @@ class StartupWarmBlankPage(_StartupWarm):
 
 @benchmark.Disabled('reference',                   # http://crbug.com/476882
                     'android',                     # http://crbug.com/481919
-                    'snowleopard',                 # http://crbug.com/605485
+                    'yosemite',                 # http://crbug.com/605485
                     'content-shell')               # No pregenerated profiles.
 class StartupLargeProfileColdBlankPage(_StartupCold):
   """Measures cold startup time with a large profile."""
@@ -82,7 +82,7 @@ class StartupLargeProfileColdBlankPage(_StartupCold):
 
 @benchmark.Disabled('reference',                   # http://crbug.com/476882
                     'android',                     # http://crbug.com/481919
-                    'snowleopard',                 # http://crbug.com/605485
+                    'yosemite',                 # http://crbug.com/605485
                     'content-shell')               # No pregenerated profiles.
 class StartupLargeProfileWarmBlankPage(_StartupWarm):
   """Measures warm startup time with a large profile."""
