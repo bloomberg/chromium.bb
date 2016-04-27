d904d6eb6e63225dcb43ed4810f7932be12ab946
ben <ben@chromium.org>
2016-04-26 17:15:16 -0700

    Strip comments from manifests prior to collating.
    The C++ json reader already eats comments.
    
    Adds some documentation to existing manifests.
    
    R=rockot@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/1919493002
    
    Cr-Commit-Position: refs/heads/master@{#389951}
    
    Former-commit-id: e13ba746a7e8aefeba6a487db65878424de7b356

--- mojo/public/tools/manifest/manifest_collator.py
+++ mojo/public/tools/manifest/manifest_collator.py
@@ -7,19 +7,26 @@
 
 import argparse
 import json
+import os
 import shutil
 import sys
 import urlparse
 
+eater_relative = '../../../../../tools/json_comment_eater'
+eater_relative = os.path.join(os.path.abspath(__file__), eater_relative)
+sys.path.insert(0, os.path.normpath(eater_relative))
+try:
+  import json_comment_eater
+finally:
+  sys.path.pop(0)
 
 def ParseJSONFile(filename):
   with open(filename) as json_file:
     try:
-      return json.load(json_file)
+      return json.loads(json_comment_eater.Nom(json_file.read()))
     except ValueError:
       print "%s is not a valid JSON document" % filename
       return None
-
 
 def MergeDicts(left, right):
   for k, v in right.iteritems():
