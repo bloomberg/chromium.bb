ed5d686469021b2a2a77e62834a8c0d6430abd02
rockot <rockot@chromium.org>
2016-04-28 07:47:16 -0700

    [mojo-edk] Add single-process message pipe perf test
    Adds a single-process pingpong test to
    mojo_message_pipe_perftests. This is useful for environments
    where multiprocess tests don't work properly.
    
    BUG=None
    R=amistry@chromium.org
    
    Review-Url: https://codereview.chromium.org/1926993002
    Cr-Commit-Position: refs/heads/master@{#390376}
    
    Former-commit-id: 38303ba5f2422e768896bec9e7371556df07988b

--- mojo/edk/system/message_pipe_perftest.cc
+++ mojo/edk/system/message_pipe_perftest.cc
@@ -5,12 +5,16 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <memory>
 #include <utility>
 
 #include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/logging.h"
+#include "base/macros.h"
 #include "base/strings/stringprintf.h"
 #include "base/test/perf_time_logger.h"
+#include "base/threading/thread.h"
 #include "mojo/edk/embedder/embedder.h"
 #include "mojo/edk/embedder/scoped_platform_handle.h"
 #include "mojo/edk/system/handle_signals_state.h"
@@ -25,11 +29,9 @@ namespace mojo {
 namespace edk {
 namespace {
 
-class MultiprocessMessagePipePerfTest : public test::MojoTestBase {
+class MessagePipePerfTest : public test::MojoTestBase {
  public:
-  MultiprocessMessagePipePerfTest()
-      : message_count_(0),
-        message_size_(0) {}
+  MessagePipePerfTest() : message_count_(0), message_size_(0) {}
 
   void SetUpMeasurement(int message_count, size_t message_size) {
     message_count_ = message_count;
@@ -76,50 +78,82 @@ class MultiprocessMessagePipePerfTest : public test::MojoTestBase {
     logger.Done();
   }
 
+ protected:
+  void RunPingPongServer(MojoHandle mp) {
+    // This values are set to align with one at ipc_pertests.cc for comparison.
+    const size_t kMsgSize[5] = {12, 144, 1728, 20736, 248832};
+    const int kMessageCount[5] = {50000, 50000, 50000, 12000, 1000};
+
+    for (size_t i = 0; i < 5; i++) {
+      SetUpMeasurement(kMessageCount[i], kMsgSize[i]);
+      Measure(mp);
+    }
+
+    SendQuitMessage(mp);
+  }
+
+  static int RunPingPongClient(MojoHandle mp) {
+    std::string buffer(1000000, '\0');
+    int rv = 0;
+    while (true) {
+      // Wait for our end of the message pipe to be readable.
+      HandleSignalsState hss;
+      MojoResult result =
+          MojoWait(mp, MOJO_HANDLE_SIGNAL_READABLE,
+                   MOJO_DEADLINE_INDEFINITE, &hss);
+      if (result != MOJO_RESULT_OK) {
+        rv = result;
+        break;
+      }
+
+      uint32_t read_size = static_cast<uint32_t>(buffer.size());
+      CHECK_EQ(MojoReadMessage(mp, &buffer[0],
+                               &read_size, nullptr,
+                               0, MOJO_READ_MESSAGE_FLAG_NONE),
+               MOJO_RESULT_OK);
+
+      // Empty message indicates quit.
+      if (read_size == 0)
+        break;
+
+      CHECK_EQ(MojoWriteMessage(mp, &buffer[0],
+                                read_size,
+                                nullptr, 0, MOJO_WRITE_MESSAGE_FLAG_NONE),
+               MOJO_RESULT_OK);
+    }
+
+    return rv;
+  }
+
  private:
   int message_count_;
   size_t message_size_;
   std::string payload_;
   std::string read_buffer_;
   std::unique_ptr<base::PerfTimeLogger> perf_logger_;
+
+  DISALLOW_COPY_AND_ASSIGN(MessagePipePerfTest);
 };
+
+TEST_F(MessagePipePerfTest, PingPong) {
+  MojoHandle server_handle, client_handle;
+  CreateMessagePipe(&server_handle, &client_handle);
+
+  base::Thread client_thread("PingPongClient");
+  client_thread.Start();
+  client_thread.task_runner()->PostTask(
+      FROM_HERE,
+      base::Bind(base::IgnoreResult(&RunPingPongClient), client_handle));
+
+  RunPingPongServer(server_handle);
+}
 
 // For each message received, sends a reply message with the same contents
 // repeated twice, until the other end is closed or it receives "quitquitquit"
 // (which it doesn't reply to). It'll return the number of messages received,
 // not including any "quitquitquit" message, modulo 100.
-DEFINE_TEST_CLIENT_WITH_PIPE(PingPongClient, MultiprocessMessagePipePerfTest,
-                             h) {
-  std::string buffer(1000000, '\0');
-  int rv = 0;
-  while (true) {
-    // Wait for our end of the message pipe to be readable.
-    HandleSignalsState hss;
-    MojoResult result =
-        MojoWait(h, MOJO_HANDLE_SIGNAL_READABLE,
-                 MOJO_DEADLINE_INDEFINITE, &hss);
-    if (result != MOJO_RESULT_OK) {
-      rv = result;
-      break;
-    }
-
-    uint32_t read_size = static_cast<uint32_t>(buffer.size());
-    CHECK_EQ(MojoReadMessage(h, &buffer[0],
-                             &read_size, nullptr,
-                             0, MOJO_READ_MESSAGE_FLAG_NONE),
-             MOJO_RESULT_OK);
-
-    // Empty message indicates quit.
-    if (read_size == 0)
-      break;
-
-    CHECK_EQ(MojoWriteMessage(h, &buffer[0],
-                              read_size,
-                              nullptr, 0, MOJO_WRITE_MESSAGE_FLAG_NONE),
-             MOJO_RESULT_OK);
-  }
-
-  return rv;
+DEFINE_TEST_CLIENT_WITH_PIPE(PingPongClient, MessagePipePerfTest, h) {
+  return RunPingPongClient(h);
 }
 
 // Repeatedly sends messages as previous one got replied by the child.
@@ -127,22 +161,13 @@ DEFINE_TEST_CLIENT_WITH_PIPE(PingPongClient, MultiprocessMessagePipePerfTest,
 // number of messages has been sent.
 #if defined(OS_ANDROID)
 // Android multi-process tests are not executing the new process. This is flaky.
-#define MAYBE_PingPong DISABLED_PingPong
+#define MAYBE_MultiprocessPingPong DISABLED_MultiprocessPingPong
 #else
-#define MAYBE_PingPong PingPong
+#define MAYBE_MultiprocessPingPong MultiprocessPingPong
 #endif  // defined(OS_ANDROID)
-TEST_F(MultiprocessMessagePipePerfTest, MAYBE_PingPong) {
+TEST_F(MessagePipePerfTest, MAYBE_MultiprocessPingPong) {
   RUN_CHILD_ON_PIPE(PingPongClient, h)
-    // This values are set to align with one at ipc_pertests.cc for comparison.
-    const size_t kMsgSize[5] = {12, 144, 1728, 20736, 248832};
-    const int kMessageCount[5] = {50000, 50000, 50000, 12000, 1000};
-
-    for (size_t i = 0; i < 5; i++) {
-      SetUpMeasurement(kMessageCount[i], kMsgSize[i]);
-      Measure(h);
-    }
-
-    SendQuitMessage(h);
+    RunPingPongServer(h);
   END_CHILD()
 }
 
