e13d16dfc782316edbcdc0ba8d9425e88f6d0c7d
gabadie <gabadie@chromium.org>
2016-04-29 04:11:29 -0700

    sandwich: Remove deprecated sub-command and rename `run-all` to `run`
    Sandwich has been migrated to the task manager that is going to let
    us do more complex operations than the deprecated sub-commands that
    required to be executed in a sequence of different processes.
    
    This CL simply remove the old sub-commands to reduce test coverage
    and release us from non necessary constrains on further sandwich
    improvments.
    
    BUG=582080
    
    Review-Url: https://codereview.chromium.org/1930023002
    Cr-Commit-Position: refs/heads/master@{#390616}
    
    Former-commit-id: bc8010bd66683defcad902d7b51868ed1c16538c

--- tools/android/loading/sandwich.py
+++ tools/android/loading/sandwich.py
@@ -50,15 +50,13 @@ _MEMORY_MEASUREMENT = 'memory'
 
 def _ArgumentParser():
   """Build a command line argument's parser."""
-  # Command parser when dealing with jobs.
-  common_job_parser = argparse.ArgumentParser(add_help=False)
-  common_job_parser.add_argument('--job', required=True,
-                                 help='JSON file with job description.')
-  common_job_parser.add_argument('--android', default=None, type=str,
-                                 dest='android_device_serial',
-                                 help='Android device\'s serial to use.')
-
   task_parser = task_manager.CommandLineParser()
+
+  # Command parser when dealing with SandwichRunner.
+  sandwich_runner_parser = argparse.ArgumentParser(add_help=False)
+  sandwich_runner_parser.add_argument('--android', default=None, type=str,
+                                      dest='android_device_serial',
+                                      help='Android device\'s serial to use.')
 
   # Plumbing parser to configure OPTIONS.
   plumbing_parser = OPTIONS.GetParentParser('plumbing options')
@@ -67,109 +65,12 @@ def _ArgumentParser():
   parser = argparse.ArgumentParser(parents=[plumbing_parser])
   subparsers = parser.add_subparsers(dest='subcommand', help='subcommand line')
 
-  # Record WPR subcommand.
-  record_wpr = subparsers.add_parser('record-wpr', parents=[common_job_parser],
-                                     help='Record WPR from sandwich job.')
-  record_wpr.add_argument('--wpr-archive', required=True, type=str,
-                          dest='wpr_archive_path',
-                          help='Web page replay archive to generate.')
-
-  # Patch WPR subcommand.
-  patch_wpr = subparsers.add_parser('patch-wpr',
-                                     help='Patch WPR response headers.')
-  patch_wpr.add_argument('--wpr-archive', required=True, type=str,
-                         dest='wpr_archive_path',
-                         help='Web page replay archive to patch.')
-
-  # Create cache subcommand.
-  create_cache_parser = subparsers.add_parser('create-cache',
-      parents=[common_job_parser],
-      help='Create cache from sandwich job.')
-  create_cache_parser.add_argument('--cache-archive', required=True, type=str,
-                                   dest='cache_archive_path',
-                                   help='Cache archive destination path.')
-  create_cache_parser.add_argument('--wpr-archive', default=None, type=str,
-                                   dest='wpr_archive_path',
-                                   help='Web page replay archive to create ' +
-                                       'the cache from.')
-
-  # Run subcommand.
-  run_parser = subparsers.add_parser('run', parents=[common_job_parser],
-                                     help='Run sandwich benchmark.')
-  run_parser.add_argument('--output', required=True, type=str,
-                          dest='trace_output_directory',
-                          help='Path of output directory to create.')
-  run_parser.add_argument('--cache-archive', type=str,
-                          dest='cache_archive_path',
-                          help='Cache archive destination path.')
-  run_parser.add_argument('--cache-op',
-                          choices=['clear', 'push', 'reload'],
-                          dest='cache_operation',
-                          default='clear',
-                          help='Configures cache operation to do before '
-                              +'launching Chrome. (Default is clear). The push'
-                              +' cache operation requires --cache-archive to '
-                              +'set.')
-  run_parser.add_argument('--disable-wpr-script-injection',
-                          action='store_true',
-                          help='Disable WPR default script injection such as ' +
-                              'overriding javascript\'s Math.random() and ' +
-                              'Date() with deterministic implementations.')
-  run_parser.add_argument('--network-condition', default=None,
-      choices=sorted(emulation.NETWORK_CONDITIONS.keys()),
-      help='Set a network profile.')
-  run_parser.add_argument('--network-emulator', default='browser',
-      choices=['browser', 'wpr'],
-      help='Set which component is emulating the network condition.' +
-          ' (Default to browser). Wpr network emulator requires --wpr-archive' +
-          ' to be set.')
-  run_parser.add_argument('--job-repeat', default=1, type=int,
-                          help='How many times to run the job.')
-  run_parser.add_argument('--record-video', action='store_true',
-                          help='Configures either to record or not a video of '
-                              +'chrome loading the web pages.')
-  run_parser.add_argument('--wpr-archive', default=None, type=str,
-                          dest='wpr_archive_path',
-                          help='Web page replay archive to load job\'s urls ' +
-                              'from.')
-
-  # Pull metrics subcommand.
-  create_cache_parser = subparsers.add_parser('extract-metrics',
-      help='Extracts metrics from a loading trace and saves as CSV.')
-  create_cache_parser.add_argument('--trace-directory', required=True,
-                                   dest='trace_output_directory', type=str,
-                                   help='Path of loading traces directory.')
-  create_cache_parser.add_argument('--out-metrics', default=None, type=str,
-                                   dest='metrics_csv_path',
-                                   help='Path where to save the metrics\'s '+
-                                      'CSV.')
-
-  # Filter cache subcommand.
-  filter_cache_parser = subparsers.add_parser('filter-cache',
-      help='Cache filtering that keeps only resources discoverable by the HTML'+
-          ' document parser.')
-  filter_cache_parser.add_argument('--cache-archive', type=str, required=True,
-                                   dest='cache_archive_path',
-                                   help='Path of the cache archive to filter.')
-  filter_cache_parser.add_argument('--subresource-discoverer', required=True,
-      help='Strategy for populating the cache with a subset of resources, '
-           'according to the way they can be discovered',
-      choices=sandwich_misc.SUBRESOURCE_DISCOVERERS)
-  filter_cache_parser.add_argument('--output', type=str, required=True,
-                                   dest='output_cache_archive_path',
-                                   help='Path of filtered cache archive.')
-  filter_cache_parser.add_argument('loading_trace_paths', type=str, nargs='+',
-      metavar='LOADING_TRACE',
-      help='A list of loading traces generated by a sandwich run for a given' +
-          ' url. This is used to have a resource dependency graph to white-' +
-          'list the ones discoverable by the HTML pre-scanner for that given ' +
-          'url.')
-
   # Record test trace subcommand.
   record_trace_parser = subparsers.add_parser('record-test-trace',
+      parents=[sandwich_runner_parser],
       help='Record a test trace using the trace_test.webserver_test.')
   record_trace_parser.add_argument('--source-dir', type=str, required=True,
-                                   help='Base path where the files are opened'
+                                   help='Base path where the files are opened '
                                         'by the web server.')
   record_trace_parser.add_argument('--page', type=str, required=True,
                                    help='Source page in source-dir to navigate '
@@ -177,93 +78,38 @@ def _ArgumentParser():
   record_trace_parser.add_argument('-o', '--output', type=str, required=True,
                                    help='Output path of the generated trace.')
 
-  # Run all subcommand.
-  run_all = subparsers.add_parser('run-all',
-                       parents=[common_job_parser, task_parser],
-                       help='Run all steps using the task manager '
-                            'infrastructure.')
-  run_all.add_argument('-m', '--measure', default=[], dest='optional_measures',
-                       choices=[_SPEED_INDEX_MEASUREMENT, _MEMORY_MEASUREMENT],
-                       nargs='+', help='Enable optional measurements.')
-  run_all.add_argument('-g', '--gen-full', action='store_true',
-                       help='Generate the full graph with all possible'
-                            'benchmarks.')
-  run_all.add_argument('--wpr-archive', default=None, type=str,
-                       dest='wpr_archive_path',
-                       help='WebPageReplay archive to use, instead of '
-                            'generating one.')
-  run_all.add_argument('--url-repeat', default=1, type=int,
-                       help='How many times to repeat the urls.')
+  # Run subcommand.
+  run_parser = subparsers.add_parser('run',
+      parents=[sandwich_runner_parser, task_parser],
+      help='Run all steps using the task manager infrastructure.')
+  run_parser.add_argument('-g', '--gen-full', action='store_true',
+                          help='Generate the full graph with all possible '
+                               'benchmarks.')
+  run_parser.add_argument('--job', required=True,
+      help='JSON file with job description such as in sandwich_jobs/.')
+  run_parser.add_argument('-m', '--measure', default=[], nargs='+',
+      choices=[_SPEED_INDEX_MEASUREMENT, _MEMORY_MEASUREMENT],
+      dest='optional_measures', help='Enable optional measurements.')
+  run_parser.add_argument('--wpr-archive', default=None, type=str,
+                          dest='wpr_archive_path',
+                          help='WebPageReplay archive to use, instead of '
+                               'generating one.')
+  run_parser.add_argument('--url-repeat', default=1, type=int,
+                          help='How many times to repeat the urls.')
 
   return parser
 
 
-def _CreateSandwichRunner(args):
-  sandwich_runner = SandwichRunner()
-  sandwich_runner.LoadJob(args.job)
-  sandwich_runner.PullConfigFromArgs(args)
-  if args.android_device_serial is not None:
-    sandwich_runner.android_device = \
-        device_setup.GetDeviceFromSerial(args.android_device_serial)
-  return sandwich_runner
-
-
-def _RecordWprMain(args):
-  sandwich_runner = _CreateSandwichRunner(args)
-  sandwich_runner.wpr_record = True
-  sandwich_runner.PrintConfig()
-  if not os.path.isdir(os.path.dirname(args.wpr_archive_path)):
-    os.makedirs(os.path.dirname(args.wpr_archive_path))
-  sandwich_runner.Run()
-  return 0
-
-
-def _CreateCacheMain(args):
-  sandwich_runner = _CreateSandwichRunner(args)
-  sandwich_runner.cache_operation = 'save'
-  sandwich_runner.PrintConfig()
-  if not os.path.isdir(os.path.dirname(args.cache_archive_path)):
-    os.makedirs(os.path.dirname(args.cache_archive_path))
-  sandwich_runner.Run()
-  return 0
-
-
-def _RunJobMain(args):
-  sandwich_runner = _CreateSandwichRunner(args)
-  sandwich_runner.PrintConfig()
-  sandwich_runner.Run()
-  return 0
-
-
-def _ExtractMetricsMain(args):
-  run_metrics_list = sandwich_metrics.ExtractMetricsFromRunnerOutputDirectory(
-      args.trace_output_directory)
-  run_metrics_list.sort(key=lambda e: e['repeat_id'])
-  with open(args.metrics_csv_path, 'w') as csv_file:
-    writer = csv.DictWriter(csv_file,
-                            fieldnames=sandwich_metrics.CSV_FIELD_NAMES)
-    writer.writeheader()
-    for run_metrics in run_metrics_list:
-      writer.writerow(run_metrics)
-  return 0
-
-
-def _FilterCacheMain(args):
-  whitelisted_urls = set()
-  for loading_trace_path in args.loading_trace_paths:
-    whitelisted_urls.update(sandwich_misc.ExtractDiscoverableUrls(
-        loading_trace_path, args.subresource_discoverer))
-  if not os.path.isdir(os.path.dirname(args.output_cache_archive_path)):
-    os.makedirs(os.path.dirname(args.output_cache_archive_path))
-  chrome_cache.ApplyUrlWhitelistToCacheArchive(args.cache_archive_path,
-                                               whitelisted_urls,
-                                               args.output_cache_archive_path)
-  return 0
+def _GetAndroidDeviceFromArgs(args):
+  if args.android_device_serial:
+    return device_setup.GetDeviceFromSerial(args.android_device_serial)
+  return None
 
 
 def _RecordWebServerTestTrace(args):
   with common_util.TemporaryDirectory() as out_path:
     sandwich_runner = SandwichRunner()
+    sandwich_runner.android_device = _GetAndroidDeviceFromArgs(args)
     # Reuse the WPR's forwarding to access the webpage from Android.
     sandwich_runner.wpr_record = True
     sandwich_runner.wpr_archive_path = os.path.join(out_path, 'wpr')
@@ -280,13 +126,9 @@ def _RecordWebServerTestTrace(args):
 
 
 def _RunAllMain(args):
-  android_device = None
-  if args.android_device_serial:
-    android_device = \
-        device_setup.GetDeviceFromSerial(args.android_device_serial)
   builder = sandwich_task_builder.SandwichTaskBuilder(
       output_directory=args.output,
-      android_device=android_device,
+      android_device=_GetAndroidDeviceFromArgs(args),
       job_path=args.job)
   if args.wpr_archive_path:
     builder.OverridePathToWprArchive(args.wpr_archive_path)
@@ -330,22 +172,9 @@ def main(command_line_args):
   args = _ArgumentParser().parse_args(command_line_args)
   OPTIONS.SetParsedArgs(args)
 
-  if args.subcommand == 'record-wpr':
-    return _RecordWprMain(args)
-  if args.subcommand == 'patch-wpr':
-    sandwich_misc.PatchWpr(args.wpr_archive_path)
-    return 0
-  if args.subcommand == 'create-cache':
-    return _CreateCacheMain(args)
-  if args.subcommand == 'run':
-    return _RunJobMain(args)
-  if args.subcommand == 'extract-metrics':
-    return _ExtractMetricsMain(args)
-  if args.subcommand == 'filter-cache':
-    return _FilterCacheMain(args)
   if args.subcommand == 'record-test-trace':
     return _RecordWebServerTestTrace(args)
-  if args.subcommand == 'run-all':
+  if args.subcommand == 'run':
     return _RunAllMain(args)
   assert False
 
