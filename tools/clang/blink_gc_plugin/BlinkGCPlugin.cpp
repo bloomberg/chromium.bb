dffcea3a03c51cb61f0aa9af06eca96a3149aa96
sigbjornf <sigbjornf@opera.com>
2016-05-01 14:54:52 -0700

    gc plugin: stop recognizing old and unused options.
    The options "enable-oilpan" and "warn-raw-ptr" are following r390631 no
    longer being passed in as options, hence we can finally stop recognizing
    them.
    
    R=
    BUG=585328
    
    Review-Url: https://codereview.chromium.org/1941603002
    Cr-Commit-Position: refs/heads/master@{#390875}
    
    Former-commit-id: 11d39c1b27d7c35d9196edbfc7e82a3551c9eec8

--- tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
+++ tools/clang/blink_gc_plugin/BlinkGCPlugin.cpp
@@ -29,30 +29,19 @@ class BlinkGCPluginAction : public PluginASTAction {
     return llvm::make_unique<BlinkGCPluginConsumer>(instance, options_);
   }
 
-  virtual bool ParseArgs(const CompilerInstance& instance,
+  virtual bool ParseArgs(const CompilerInstance&,
                          const std::vector<std::string>& args) {
-    bool parsed = true;
-
-    for (size_t i = 0; i < args.size() && parsed; ++i) {
-      // TODO(sof): remove this case once a version of the GC plugin
-      // has rolled which has enable-oilpan-always baked in _and_
-      // Blink no longer passes in the option (cf. Source/config.gyp)
-      if (args[i] == "enable-oilpan")
-        continue;
-      // TODO(sof): same for warn-raw-ptr
-      if (args[i] == "warn-raw-ptr")
-        continue;
-      if (args[i] == "dump-graph") {
+    for (const auto& arg : args) {
+      if (arg == "dump-graph") {
         options_.dump_graph = true;
-      } else if (args[i] == "warn-unneeded-finalizer") {
+      } else if (arg == "warn-unneeded-finalizer") {
         options_.warn_unneeded_finalizer = true;
       } else {
-        parsed = false;
-        llvm::errs() << "Unknown blink-gc-plugin argument: " << args[i] << "\n";
+        llvm::errs() << "Unknown blink-gc-plugin argument: " << arg << "\n";
+        return false;
       }
     }
-
-    return parsed;
+    return true;
   }
 
  private:
