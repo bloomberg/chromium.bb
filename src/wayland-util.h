479945b4f1c34207ad785f844eca3e422af1c6dd
Eric Engestrom <eric@engestrom.ch>
2016-05-02 09:49:35 +0100

    util: fix typo
    Signed-off-by: Eric Engestrom <eric@engestrom.ch>
    Reviewed-by: Armin Krezović <krezovic.armin@gmail.com>
    Reviewed-by: Bryce Harrington <bryce@osg.samsung.com>

--- src/wayland-util.h
+++ src/wayland-util.h
@@ -300,7 +300,7 @@ union wl_argument {
  * A dispatcher takes five arguments:  The first is the dispatcher-specific
  * implementation data associated with the target object.  The second is the
  * object on which the callback is being invoked (either wl_proxy or
- * wl_resource).  The third and fourth arguments are the opcode the wl_messsage
+ * wl_resource).  The third and fourth arguments are the opcode the wl_message
  * structure corresponding to the callback being emitted.  The final argument
  * is an array of arguments received from the other process via the wire
  * protocol.
