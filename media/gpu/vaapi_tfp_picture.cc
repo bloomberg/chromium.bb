929f11c8dd09a49f0ef7dc21cca0ce31518f3538
markdittmer <markdittmer@chromium.org>
2016-05-02 00:49:04 -0700

    Migrate content/common/gpu/media code to media/gpu
    This is part of a gpu & media refactor to enable proper layering for Mus. See bug for details.
    
    BUG=586386
    
    Committed: https://crrev.com/cda9cadaa3c9dd33df3f380a0affcf4196ac363b [formerly 6e70beb84fe0d0a3d27e4369c8b6aaae001afb81]
    Cr-Commit-Position: refs/heads/master@{#390896}
    
    Review-Url: https://codereview.chromium.org/1882373004
    Cr-Commit-Position: refs/heads/master@{#390897}
    
    Former-commit-id: 0f54920173bda1d13e9c89c51a1995a8542c5648

--- media/gpu/vaapi_tfp_picture.cc
+++ media/gpu/vaapi_tfp_picture.cc
@@ -1,9 +1,12 @@
 // Copyright 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+#
+#include "media/gpu/vaapi_tfp_picture.h"
+
+#include <X11/Xlib.h>
 
 #include "media/gpu/va_surface.h"
-#include "media/gpu/vaapi_tfp_picture.h"
 #include "media/gpu/vaapi_wrapper.h"
 #include "ui/gfx/x/x11_types.h"
 #include "ui/gl/gl_bindings.h"
