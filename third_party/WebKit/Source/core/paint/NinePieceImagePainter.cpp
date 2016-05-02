785e128a7ccc57b90ad2b5ea1e855ffab1ec4652
davve <davve@opera.com>
2016-05-02 04:57:49 -0700

    Revert "Straighten out zoom and border-image"
    This patch reverts https://codereview.chromium.org/1819083004 patchset
    #3.
    
    Fixing SVG zoom in border-image unfortunatly broke gradients
    instead. There is a difference in how scale it applied for
    zooming. For SVG a source rect (unzoomed) <-> destination rect
    (zoomed) scale factor is computed and used for scaling. For gradients,
    the source and destination rect are always the same (both zoomed). The
    individual gradients stops are zoomed instead.
    
    BUG=607414, 596075, 561519
    
    Review-Url: https://codereview.chromium.org/1934953002
    Cr-Commit-Position: refs/heads/master@{#390920}
    
    Former-commit-id: 6529aa5b970ab274de1ff4c0475f223930babff7

--- third_party/WebKit/Source/core/paint/NinePieceImagePainter.cpp
+++ third_party/WebKit/Source/core/paint/NinePieceImagePainter.cpp
@@ -42,15 +42,19 @@ bool NinePieceImagePainter::paint(GraphicsContext& graphicsContext, const Layout
 
     // NinePieceImage returns the image slices without effective zoom applied and thus we compute
     // the nine piece grid on top of the image in unzoomed coordinates.
-    LayoutSize defaultObjectSize = borderImageRect.size();
-    defaultObjectSize.scale(1 / style.effectiveZoom());
-    IntSize imageSize = roundedIntSize(styleImage->imageSize(m_layoutObject, 1, defaultObjectSize));
+    //
+    // FIXME: The default object size passed to imageSize() should be scaled by the zoom factor
+    // passed in. In this case it means that borderImageRect should be passed in compensated by
+    // effective zoom, since the scale factor is one. For generated images, the actual image data
+    // (gradient stops, etc.) are scaled to effective zoom instead so we must take care not to cause
+    // scale of them again.
+    IntSize imageSize = roundedIntSize(styleImage->imageSize(m_layoutObject, 1, borderImageRect.size()));
 
     IntRectOutsets borderWidths(style.borderTopWidth(), style.borderRightWidth(),
         style.borderBottomWidth(), style.borderLeftWidth());
     NinePieceImageGrid grid(ninePieceImage, imageSize, pixelSnappedIntRect(borderImageRect), borderWidths);
 
-    RefPtr<Image> image = styleImage->image(m_layoutObject, imageSize, 1);
+    RefPtr<Image> image = styleImage->image(m_layoutObject, imageSize, style.effectiveZoom());
 
     InterpolationQuality interpolationQuality = BoxPainter::chooseInterpolationQuality(m_layoutObject, image.get(), 0, rectWithOutsets.size());
     InterpolationQuality previousInterpolationQuality = graphicsContext.imageInterpolationQuality();
