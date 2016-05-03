2763b917dde592ee2dba685370d6885c7207ea3c
Clark-Q-Gao <qingzhu.gao@gmail.com>
2016-05-03 16:37:52 -0700

    changed psi caculation

--- visualmetrics.py
+++ visualmetrics.py
@@ -828,15 +828,16 @@ def calculate_perceptual_speed_index(progress, directory):
     dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), directory)
     target_frame = os.path.join(dir, "ms_{0:06d}.png".format(progress[x - 1]["time"]))
     logging.debug("Target image for perSI is %s" % target_frame)
+    ssim = 0
     for p in progress:
         elapsed = p['time'] - last_ms
         # Full Path of the Current Frame
         current_frame = os.path.join(dir, "ms_{0:06d}.png".format(p["time"]))
         logging.debug("Current Image is %s" % current_frame)
         # Takes full path of PNG frames to compute SSIM value
-        ssim = compute_ssim(current_frame, target_frame)
         per_si += elapsed * (1.0 - ssim)
         last_ms = p['time']
+        ssim = compute_ssim(current_frame, target_frame)
     return int(per_si)
 
 
