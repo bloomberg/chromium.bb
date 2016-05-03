9d3d760d99e218f35c5c7ce63a3c5325c343b1fc
Dongseong Hwang <dongseong.hwang@intel.com>
2016-05-03 13:43:29 +0300

    minigbm: remove GBM_BO_USE_WRITE from the drivers.
    We deprecated GBM_BO_USE_WRITE in Chrome OS, so we should remove it from the
    drivers.
    
    BUG=chromium:356871
    CQ-DEPEND=CL:342391
    Change-Id: I29c10817cf71ae51d12092f05228e92f342ca4e5
    Reviewed-on: https://chromium-review.googlesource.com/341911
    Commit-Ready: Dongseong Hwang <dongseong.hwang@intel.com>
    Tested-by: Dongseong Hwang <dongseong.hwang@intel.com>
    Reviewed-by: Dongseong Hwang <dongseong.hwang@intel.com>
    Reviewed-by: Zach Reizner <zachr@chromium.org>

--- gma500.c
+++ gma500.c
@@ -13,6 +13,6 @@ const struct gbm_driver gbm_driver_gma500 =
 	.bo_create = gbm_dumb_bo_create,
 	.bo_destroy = gbm_dumb_bo_destroy,
 	.format_list = {
-		{GBM_FORMAT_RGBX8888, GBM_BO_USE_SCANOUT | GBM_BO_USE_CURSOR | GBM_BO_USE_RENDERING | GBM_BO_USE_WRITE},
+		{GBM_FORMAT_RGBX8888, GBM_BO_USE_SCANOUT | GBM_BO_USE_CURSOR | GBM_BO_USE_RENDERING},
 	}
 };
