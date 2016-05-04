ef614ce5f884e37a5805cd832806e767fbbfa8b6
cylee <cylee@chromium.org>
2016-05-03 21:34:22 -0700

    TabManagerDelegate: Kill multiple processes on a single lowmemory event.
    1. The original kill speed is around one process per second, which is not enough
    to catch memory consuming speed when lowmemory event fires on CrOS.
    This CL calculated a target memory to freed to kill proesses until the target
    is met.
    Also working on another CrOS CL to fire lowmemory event earlier.
    
    2. Never kill top Android apps no matter it's on foreground or not, since the
    user experiment of directly closing a window is not good.
    
    BUG=b:24572007
    
    Review-Url: https://codereview.chromium.org/1912933002
    Cr-Commit-Position: refs/heads/master@{#391445}
    
    Former-commit-id: 80920a85b6d5709d99545b3fcb883ea821b87a05

--- chrome/browser/memory/tab_manager_delegate_chromeos_unittest.cc
+++ chrome/browser/memory/tab_manager_delegate_chromeos_unittest.cc
@@ -5,13 +5,17 @@
 #include "chrome/browser/memory/tab_manager_delegate_chromeos.h"
 
 #include <algorithm>
+#include <map>
 #include <string>
 #include <vector>
 
 #include "ash/shell.h"
 #include "ash/test/ash_test_base.h"
 #include "base/logging.h"
+#include "base/memory/weak_ptr.h"
+#include "base/process/process_handle.h"
 #include "chrome/browser/chromeos/arc/arc_process.h"
+#include "chrome/browser/memory/tab_manager.h"
 #include "chrome/browser/memory/tab_stats.h"
 #include "chrome/common/url_constants.h"
 #include "components/arc/common/process.mojom.h"
@@ -63,7 +67,7 @@ class TabManagerDelegateTest : public ash::test::AshTestBase {
   std::string application_id_;
 };
 
-TEST_F(TabManagerDelegateTest, KillCandidatesSorted) {
+TEST_F(TabManagerDelegateTest, CandidatesSorted) {
   std::vector<arc::ArcProcess> arc_processes = {
       {1, 10, "top", arc::mojom::ProcessState::TOP},
       {2, 20, "foreground", arc::mojom::ProcessState::FOREGROUND_SERVICE},
@@ -89,11 +93,11 @@ TEST_F(TabManagerDelegateTest, KillCandidatesSorted) {
     tab1, tab2, tab3, tab4, tab5
   };
 
-  std::vector<TabManagerDelegate::KillCandidate> candidates;
+  std::vector<TabManagerDelegate::Candidate> candidates;
 
   // Case 1: ARC window in the foreground.
   ActivateArcWindow();
-  candidates = TabManagerDelegate::GetSortedKillCandidates(
+  candidates = TabManagerDelegate::GetSortedCandidates(
           tab_list, arc_processes);
   EXPECT_EQ(8U, candidates.size());
 
@@ -114,7 +118,7 @@ TEST_F(TabManagerDelegateTest, KillCandidatesSorted) {
 
   // Case 2: ARC window in the background.
   DeactivateArcWindow();
-  candidates = TabManagerDelegate::GetSortedKillCandidates(
+  candidates = TabManagerDelegate::GetSortedCandidates(
           tab_list, arc_processes);
   EXPECT_EQ(8U, candidates.size());
 
@@ -146,11 +150,74 @@ class MockTabManagerDelegate : public TabManagerDelegate {
  public:
   MockTabManagerDelegate(): TabManagerDelegate(nullptr) {
   }
+
+  explicit MockTabManagerDelegate(TabManagerDelegate::MemoryStat* mem_stat)
+      : TabManagerDelegate(nullptr, mem_stat) {
+  }
+
+  // unit test.
+  std::vector<int> GetKilledArcProcesses() {
+    return killed_arc_processes_;
+  }
+
+  // unit test.
+  std::vector<int64_t> GetKilledTabs() {
+    return killed_tabs_;
+  }
+
+  // unit test.
+  void Clear() {
+    killed_arc_processes_.clear();
+    killed_tabs_.clear();
+  }
+
  protected:
   // Nullify the operation for unit test.
   void SetOomScoreAdjForTabs(
       const std::vector<std::pair<base::ProcessHandle, int>>& entries)
       override {}
+
+  bool KillArcProcess(const int nspid) override {
+    killed_arc_processes_.push_back(nspid);
+    return true;
+  }
+
+  bool KillTab(int64_t tab_id) override {
+    killed_tabs_.push_back(tab_id);
+    return true;
+  }
+
+ private:
+  std::vector<int> killed_arc_processes_;
+  std::vector<int64_t> killed_tabs_;
+};
+
+class MockMemoryStat : public TabManagerDelegate::MemoryStat {
+ public:
+  MockMemoryStat() {}
+  virtual ~MockMemoryStat() {}
+
+  int TargetMemoryToFreeKB() override {
+    return target_memory_to_free_kb_;
+  }
+
+  int EstimatedMemoryFreedKB(base::ProcessHandle pid) override {
+    return process_pss_[pid];
+  }
+
+  // unittest.
+  void SetTargetMemoryToFreeKB(const int target) {
+    target_memory_to_free_kb_ = target;
+  }
+
+  // unittest.
+  void SetProcessPss(base::ProcessHandle pid, int pss) {
+    process_pss_[pid] = pss;
+  }
+
+ private:
+  int target_memory_to_free_kb_;
+  std::map<base::ProcessHandle, int> process_pss_;
 };
 
 TEST_F(TabManagerDelegateTest, SetOomScoreAdj) {
@@ -203,6 +270,78 @@ TEST_F(TabManagerDelegateTest, SetOomScoreAdj) {
   // Lower priority part.
   EXPECT_EQ(650, oom_score_map[20]);
   EXPECT_EQ(825, oom_score_map[30]);
+}
+
+TEST_F(TabManagerDelegateTest, KillMultipleProcesses) {
+  arc::FakeArcBridgeService fake_arc_bridge_service;
+
+  // Not owned.
+  MockMemoryStat* memory_stat = new MockMemoryStat();
+
+  // Instantiate the mock instance.
+  MockTabManagerDelegate tab_manager_delegate(memory_stat);
+
+  ActivateArcWindow();
+
+  std::vector<arc::ArcProcess> arc_processes = {
+      {10001, 100, "top", arc::mojom::ProcessState::TOP},
+      {10002, 200, "foreground", arc::mojom::ProcessState::FOREGROUND_SERVICE},
+      {10003, 300, "service", arc::mojom::ProcessState::SERVICE}};
+
+  TabStats tab1, tab2, tab3, tab4, tab5;
+  tab1.is_pinned = true;
+  tab1.renderer_handle = 11;
+  tab1.tab_contents_id = 1;
+
+  tab2.is_internal_page = true;
+  tab2.renderer_handle = 11;
+  tab2.tab_contents_id = 2;
+
+  tab3.is_pinned = true;
+  tab3.is_media = true;
+  tab3.renderer_handle = 12;
+  tab3.tab_contents_id = 3;
+
+  tab4.is_media = true;
+  tab4.renderer_handle = 12;
+  tab4.tab_contents_id = 4;
+
+  tab5.is_app = true;
+  tab5.renderer_handle = 12;
+  tab5.tab_contents_id = 5;
+  TabStatsList tab_list = {tab1, tab2, tab3, tab4, tab5};
+
+  // Sorted order:
+  // app "service"     pid: 30
+  // app "foreground"  pid: 20
+  // tab2              pid: 11
+  // tab5              pid: 12
+  // tab1              pid: 11
+  // tab4              pid: 12
+  // tab3              pid: 12
+  // app "top"         pid: 10
+  memory_stat->SetTargetMemoryToFreeKB(250000);
+  // The 3 entities to be killed.
+  memory_stat->SetProcessPss(300, 10000);
+  memory_stat->SetProcessPss(200, 200000);
+  memory_stat->SetProcessPss(11, 50000);
+  // Should not be used.
+  memory_stat->SetProcessPss(100, 30000);
+  memory_stat->SetProcessPss(12, 100000);
+
+  tab_manager_delegate.LowMemoryKillImpl(tab_list, arc_processes);
+
+  auto killed_arc_processes = tab_manager_delegate.GetKilledArcProcesses();
+  auto killed_tabs = tab_manager_delegate.GetKilledTabs();
+
+  EXPECT_EQ(2U, killed_arc_processes.size());
+  EXPECT_EQ(1U, killed_tabs.size());
+
+  // nspid.
+  EXPECT_EQ(10003, killed_arc_processes[0]);
+  EXPECT_EQ(10002, killed_arc_processes[1]);
+  // tab content id.
+  EXPECT_EQ(2, killed_tabs[0]);
 }
 
 }  // namespace memory
