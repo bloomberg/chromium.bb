c527dbd4ced4f4f99924db31f9469c5761e1c427
rkc <rkc@chromium.org>
2016-05-05 23:34:36 -0700

    Expand register service test.
    Add a few more cases to the register services test.
    
    R=xiyuan@chromium.org
    BUG=None.
    
    Review-Url: https://codereview.chromium.org/1955733002
    Cr-Commit-Position: refs/heads/master@{#392015}
    
    Former-commit-id: 76e042dcd9e13894e2d6bad689fb746929504026

--- chrome/test/data/extensions/api_test/bluetooth_low_energy/register_service/runtest.js
+++ chrome/test/data/extensions/api_test/bluetooth_low_energy/register_service/runtest.js
@@ -18,20 +18,28 @@ function failOnSuccess() {
   return false;
 }
 
-var service = { uuid: '00001234-0000-1000-8000-00805f9b34fb', isPrimary: true }
-chrome.bluetoothLowEnergy.createService(service, function(serviceId) {
+var service1 = { uuid: '00001234-0000-1000-8000-00805f9b34fb', isPrimary: true }
+var service2 = { uuid: '00001234-0000-1000-8000-00805f9b34fb', isPrimary: true }
+chrome.bluetoothLowEnergy.createService(service1, function(serviceId) {
   if (failOnError(serviceId))
     return;
-
   chrome.bluetoothLowEnergy.registerService(serviceId, function() {
     if (failOnError('result'))
       return;
-
     // Registering again, this should fail.
     chrome.bluetoothLowEnergy.registerService(serviceId, function() {
       if (failOnSuccess())
         return;
-      chrome.test.succeed();
+      chrome.bluetoothLowEnergy.createService(service2, function(serviceId) {
+        if (failOnError(serviceId))
+          return;
+        // Registering another service, this should work.
+        chrome.bluetoothLowEnergy.registerService(serviceId, function() {
+          if (failOnError('result'))
+            return;
+          chrome.test.succeed();
+        });
+      });
     });
   });
 });
