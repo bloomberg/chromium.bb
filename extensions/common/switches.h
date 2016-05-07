95998f4bc51f2c6cd90548716863f9e430a18f52
Nico Weber <thakis@chromium.org>
2016-05-07 10:24:05 -0400

    Revert "Desktop Capture Picker New UI: Non Mac Structure Change" (https://codereview.chromium.org/1932413002/)
    This broke DesktopMediaPickerViewsDeprecatedTest on various main
    waterfall debug
    bots (e.g.
    https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%2...)
    
    [4208:2052:0506/162201:4424718:FATAL:l10n_util.cc(712)] Check failed:
    std::string::npos != pos (4294967295 vs. 4294967295) Didn't find a $1
    placeholder in Share your screen
    Backtrace:
            base::CancellationFlag::Set [0x0F7520F1+508917]
            base::CancellationFlag::Set [0x0F7AC6AB+879023]
            ui::AreTouchEventsEnabled [0x1424C6B2+469462]
            ui::AreTouchEventsEnabled [0x1424C0F4+467992]
            deprecated::DesktopMediaPickerDialogView::GetWindowTitle
    [0x06A160F9+41]
            views::TouchSelectionControllerImpl::GetSelectionHandle2Bounds
    [0x17A7D01D+1794589]
    
    BUG=602478
    TBR=qiangchen@chromium.org
    
    Review URL: https://codereview.chromium.org/1957063002 .
    
    Cr-Commit-Position: refs/heads/master@{#392261}
    
    Former-commit-id: c0f67d648495f568b4c034e9e5c0fe63d688fc4b

--- extensions/common/switches.h
+++ extensions/common/switches.h
@@ -14,7 +14,6 @@ namespace switches {
 extern const char kAllowHTTPBackgroundPage[];
 extern const char kAllowLegacyExtensionManifests[];
 extern const char kDisableDesktopCaptureAudio[];
-extern const char kDisableDesktopCapturePickerOldUI[];
 extern const char kDisableTabForDesktopShare[];
 extern const char kEmbeddedExtensionOptions[];
 extern const char kEnableAppsShowOnFirstPaint[];
