f8f60b638cb5daf5e26eb5bad864f1a21a5dd19f
oysteine <oysteine@chromium.org>
2016-05-09 15:03:34 -0700

    Disabled rasterize_and_record_micro.key_silk_cases for flakiness
    TBR=nednguyen
    BUG=610424
    
    Review-Url: https://codereview.chromium.org/1966433002
    Cr-Commit-Position: refs/heads/master@{#392432}
    
    Former-commit-id: c4f81162912dd035f78f9e696dd2338cfb93df98

--- tools/perf/benchmarks/rasterize_and_record_micro.py
+++ tools/perf/benchmarks/rasterize_and_record_micro.py
@@ -72,7 +72,7 @@ class RasterizeAndRecordMicroKeyMobileSites(_RasterizeAndRecordMicro):
     return 'rasterize_and_record_micro.key_mobile_sites_smooth'
 
 
-@benchmark.Disabled('mac', 'win')
+@benchmark.Disabled('mac', 'win', 'android') # http://crbug.com/610424
 class RasterizeAndRecordMicroKeySilkCases(_RasterizeAndRecordMicro):
   """Measures rasterize and record performance on the silk sites.
 
