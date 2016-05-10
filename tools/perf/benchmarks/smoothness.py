f98c202cbb55a85f31fd9f5534107ddaca616e0b
oysteine <oysteine@chromium.org>
2016-05-10 07:55:57 -0700

    Disable smoothness/v8.key_mobile_sites_smooth
    TBR=nednguyen
    BUG=610338
    
    Review-Url: https://codereview.chromium.org/1956413002
    Cr-Commit-Position: refs/heads/master@{#392602}
    
    Former-commit-id: 13a76e93830f9ffe6fd03074d01daf00641cc336

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -121,7 +121,7 @@ class SmoothnessKeyDesktopMoveCases(_Smoothness):
     return 'smoothness.key_desktop_move_cases'
 
 
-@benchmark.Enabled('android')
+@benchmark.Disabled('all') # crbug.com/610338
 class SmoothnessKeyMobileSites(_Smoothness):
   """Measures rendering statistics while scrolling down the key mobile sites.
 
