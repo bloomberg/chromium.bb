13d983c5d517d48b5092bbfdee5179fd99a43768
gab <gab@chromium.org>
2016-05-10 16:17:01 -0700

    Move base/thread_task_runner_handle.h to base/threading/thread_task_runner_handle.h
    Keeping a base/thread_task_runner_handle.h which
    merely does a direct mapping behind for now.
    
    Will then proceed to incrementally fixing >1500 files that
    include the old path (TBR'ing to component OWNERS) to eventually
    get rid of it.
    
    BUG=610438
    
    Review-Url: https://codereview.chromium.org/1966733002
    Cr-Commit-Position: refs/heads/master@{#392765}
    
    Former-commit-id: 0ac3fff261825d90beb0b8111fa7499d73bcac7c

--- base/threading/thread_task_runner_handle.cc
+++ base/threading/thread_task_runner_handle.cc
@@ -1,8 +1,8 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2016 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/thread_task_runner_handle.h"
+#include "base/threading/thread_task_runner_handle.h"
 
 #include <utility>
 
