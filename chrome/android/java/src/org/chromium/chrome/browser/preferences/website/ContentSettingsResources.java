5c525ce1b550c4c4682492462f26294661f76709
finnur <finnur@chromium.org>
2016-05-11 05:01:45 -0700

    Site Settings Android: A few polish items.
    From issue 601918:
    - Change "+ Add site" blue action link to be "+ Add site exception"
    - Change dialog text to be: "Allow [JavaScript/the permission in question etc.] for a specific site."
    
    From issue 467788:
    - Update the individual site settings page toggles with verbs that match the permissions bubbles
    
    From issue 604753 (partial fix):
    - Improve alignment of Site Settings list a bit (move icon a bit to the right).
    - Color the Site List heading blue.
    
    Also add an updated image for Protected Content (slightly larger) and the Media icon, which is for
    the container menu that will house Protected Content and Autoplay.
    
    BUG=601918, 467788, 604753
    
    Committed: https://crrev.com/e5175c121d53d2ebd5fed7a3b7ccab81724575c0 [formerly 2d4101ec17e44ff061d5e65bc52f386d9985e881]
    Cr-Commit-Position: refs/heads/master@{#392363}
    
    Review-Url: https://codereview.chromium.org/1947493002
    Cr-Commit-Position: refs/heads/master@{#392899}
    
    Former-commit-id: 91faabbb0fd92d636378508b4aa644b11bc4877b

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/ContentSettingsResources.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/ContentSettingsResources.java
@@ -107,19 +107,19 @@ public class ContentSettingsResources {
                                  R.string.website_settings_fullscreen,
                                  R.string.fullscreen_permission_title, ContentSetting.ALLOW,
                                  ContentSetting.ASK, 0,
-                                 R.string.website_settings_category_ask_first_recommended));
+                                 R.string.website_settings_category_fullscreen_ask));
             localMap.put(ContentSettingsType.CONTENT_SETTINGS_TYPE_GEOLOCATION,
                     new ResourceItem(R.drawable.permission_location,
                                  R.string.website_settings_device_location,
                                  R.string.geolocation_permission_title, ContentSetting.ASK,
                                  ContentSetting.BLOCK,
-                                 R.string.website_settings_category_ask_before_accessing, 0));
+                                 R.string.website_settings_category_location_ask, 0));
             localMap.put(ContentSettingsType.CONTENT_SETTINGS_TYPE_JAVASCRIPT,
                     new ResourceItem(R.drawable.permission_javascript,
                                  R.string.javascript_permission_title,
                                  R.string.javascript_permission_title, ContentSetting.ALLOW,
                                  ContentSetting.BLOCK,
-                                 R.string.website_settings_category_allowed_recommended, 0));
+                                 R.string.website_settings_category_javascript_allowed, 0));
             localMap.put(ContentSettingsType.CONTENT_SETTINGS_TYPE_KEYGEN,
                     new ResourceItem(R.drawable.permission_keygen,
                                  R.string.keygen_permission_title,
@@ -131,12 +131,12 @@ public class ContentSettingsResources {
                     new ResourceItem(R.drawable.permission_camera,
                             R.string.website_settings_use_camera, R.string.camera_permission_title,
                             ContentSetting.ASK, ContentSetting.BLOCK,
-                            R.string.website_settings_category_ask_before_accessing, 0));
+                            R.string.website_settings_category_camera_ask, 0));
             localMap.put(
                     ContentSettingsType.CONTENT_SETTINGS_TYPE_MEDIASTREAM_MIC,
                     new ResourceItem(R.drawable.permission_mic, R.string.website_settings_use_mic,
                             R.string.mic_permission_title, ContentSetting.ASK, ContentSetting.BLOCK,
-                            R.string.website_settings_category_ask_before_accessing, 0));
+                            R.string.website_settings_category_mic_ask, 0));
             localMap.put(ContentSettingsType.CONTENT_SETTINGS_TYPE_MIDI_SYSEX,
                     new ResourceItem(R.drawable.permission_midi, 0,
                                  R.string.midi_sysex_permission_title, null, null, 0, 0));
@@ -145,12 +145,12 @@ public class ContentSettingsResources {
                                  R.string.push_notifications_permission_title,
                                  R.string.push_notifications_permission_title, ContentSetting.ASK,
                                  ContentSetting.BLOCK,
-                                 R.string.website_settings_category_ask_before_sending, 0));
+                                 R.string.website_settings_category_notifications_ask, 0));
             localMap.put(ContentSettingsType.CONTENT_SETTINGS_TYPE_POPUPS,
                     new ResourceItem(R.drawable.permission_popups, R.string.popup_permission_title,
                                  R.string.popup_permission_title, ContentSetting.ALLOW,
                                  ContentSetting.BLOCK, 0,
-                                 R.string.website_settings_category_blocked_recommended));
+                                 R.string.website_settings_category_popups_blocked));
             localMap.put(ContentSettingsType.CONTENT_SETTINGS_TYPE_PROTECTED_MEDIA_IDENTIFIER,
                     new ResourceItem(R.drawable.permission_protected_media,
                                  org.chromium.chrome.R.string.protected_content,
