9926b323166e1d89d42908a59bc5bb6336d9394a
sigbjornf <sigbjornf@opera.com>
2016-05-11 07:39:31 -0700

    Simplify SVGSMILElement::notifyDependentsIntervalChanged loop breaker.
    To catch out recursive notifications, notifyDependentsIntervalChanged()
    keeps track of the SVGSMILElements that are on the stack and being
    notified, so as to bail early in case of loops.
    
    There's no need for that set of SVGSMILElements to be recorded using
    a persistent static local as the objects are stack reachable should
    a conservative GC be needed, so an 'ordinary' hash set will do.
    
    Not using a persistent reference also addresses a bad interaction with
    LSan (Blink has to release all static persistents before shutting
    down to prevent false leaks w/ LSan enabled), but SVGImages containing
    animations may end up in this code path as part of an image resource
    being finalized. Which would then encounter an empty persistent
    static reference and fail (see associated bug and stack trace.)
    
    R=haraken
    BUG=610855
    
    Review-Url: https://codereview.chromium.org/1968683003
    Cr-Commit-Position: refs/heads/master@{#392919}
    
    Former-commit-id: 6d735f78334721b46d37f97ad8efb12d6e329e75

--- third_party/WebKit/Source/core/svg/animation/SVGSMILElement.cpp
+++ third_party/WebKit/Source/core/svg/animation/SVGSMILElement.cpp
@@ -1222,8 +1222,11 @@ void SVGSMILElement::notifyDependentsIntervalChanged()
     ASSERT(m_interval.begin.isFinite());
     // |loopBreaker| is used to avoid infinite recursions which may be caused from:
     // |notifyDependentsIntervalChanged| -> |createInstanceTimesFromSyncbase| -> |add{Begin,End}Time| -> |{begin,end}TimeChanged| -> |notifyDependentsIntervalChanged|
-    // |loopBreaker| is defined as a Persistent<HeapHashSet<Member<SVGSMILElement>>>. This won't cause leaks because it is guaranteed to be empty after the root |notifyDependentsIntervalChanged| has exited.
-    DEFINE_STATIC_LOCAL(HeapHashSet<Member<SVGSMILElement>>, loopBreaker, (new HeapHashSet<Member<SVGSMILElement>>));
+    //
+    // As the set here records SVGSMILElements on the stack, it is acceptable to
+    // use a HashSet of untraced heap references -- any conservative GC which
+    // strikes before unwinding will find these elements on the stack.
+    DEFINE_STATIC_LOCAL(HashSet<UntracedMember<SVGSMILElement>>, loopBreaker, ());
     if (!loopBreaker.add(this).isNewEntry)
         return;
 
