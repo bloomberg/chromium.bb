3070ae39e4f79a0595e3770f2f557728b7dbbfde
liberato <liberato@chromium.org>
2016-05-11 12:21:59 -0700

    Blacklist more Samsung S3 devices from SurfaceView usage.
    SurfaceView for video playback doesn't work properly on Samsung
    Galaxy S3 devices due to slow playback.  This CL adds additional
    model numbers for the S3 blacklist.
    
    BUG=602870, 609345
    
    Review-Url: https://codereview.chromium.org/1968013003
    Cr-Commit-Position: refs/heads/master@{#393020}
    
    Former-commit-id: 7137c2419c2c9a67350122b6e22b053c69f5a58f

--- media/base/android/media_codec_util.cc
+++ media/base/android/media_codec_util.cc
@@ -225,8 +225,22 @@ bool MediaCodecUtil::IsSurfaceViewOutputSupported() {
   // Notably this is codec agnostic at present, so any devices added to
   // the blacklist will avoid trying to play any codecs on SurfaceView.  If
   // needed in the future this can be expanded to be codec specific.
-  return !base::StartsWith(base::android::BuildInfo::GetInstance()->model(),
-                           "GT-I9300", base::CompareCase::INSENSITIVE_ASCII);
+  const char* model_prefixes[] = {// Exynos 4 (Mali-400)
+                                  "GT-I9300", "GT-I9305", "SHV-E210",
+                                  // Snapdragon S4 (Adreno-225)
+                                  "SCH-I535", "SCH-J201", "SCH-R530",
+                                  "SCH-I960", "SCH-S968", "SGH-T999",
+                                  "SGH-I747", "SGH-N064", 0};
+
+  std::string model(base::android::BuildInfo::GetInstance()->model());
+  for (int i = 0; model_prefixes[i]; ++i) {
+    if (base::StartsWith(model, model_prefixes[i],
+                         base::CompareCase::INSENSITIVE_ASCII)) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 }  // namespace media
