e12baeb6d44d22924eb6728970c71055cc0de94b
melandory <melandory@chromium.org>
2016-05-11 04:22:30 -0700

    [Smart Lock] Adds UMA Metrics for buttons clicks in Auto-signin FRE.
    Adds UMA metrics for tracking "Turn Off" and "Ok, got it" buttons in Auto sign-in first run experience for the CM API.
    
    BUG=610975
    
    Review-Url: https://codereview.chromium.org/1968013002
    Cr-Commit-Position: refs/heads/master@{#392898}
    
    Former-commit-id: b53d8f6104063f23f369f0a2c90aecd0b048be1f

--- chrome/browser/password_manager/auto_signin_first_run_dialog_android.cc
+++ chrome/browser/password_manager/auto_signin_first_run_dialog_android.cc
@@ -14,6 +14,7 @@
 #include "components/browser_sync/browser/profile_sync_service.h"
 #include "components/password_manager/core/browser/password_bubble_experiment.h"
 #include "components/password_manager/core/browser/password_manager_constants.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "jni/AutoSigninFirstRunDialog_jni.h"
 #include "ui/android/window_android.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -80,11 +81,15 @@ void AutoSigninFirstRunDialogAndroid::Destroy(JNIEnv* env, jobject obj) {
 }
 
 void AutoSigninFirstRunDialogAndroid::OnOkClicked(JNIEnv* env, jobject obj) {
+  password_manager::metrics_util::LogAutoSigninPromoUserAction(
+      password_manager::metrics_util::AUTO_SIGNIN_OK_GOT_IT);
   MarkAutoSignInFirstRunExperienceShown(web_contents_);
 }
 
 void AutoSigninFirstRunDialogAndroid::OnTurnOffClicked(JNIEnv* env,
                                                        jobject obj) {
+  password_manager::metrics_util::LogAutoSigninPromoUserAction(
+      password_manager::metrics_util::AUTO_SIGNIN_TURN_OFF);
   Profile* profile =
       Profile::FromBrowserContext(web_contents_->GetBrowserContext());
   password_bubble_experiment::TurnOffAutoSignin(profile->GetPrefs());
--- chrome/browser/password_manager/auto_signin_first_run_dialog_android_unittest.cc
+++ chrome/browser/password_manager/auto_signin_first_run_dialog_android_unittest.cc
@@ -3,9 +3,11 @@
 // found in the LICENSE file.
 
 #include "base/macros.h"
+#include "base/test/histogram_tester.h"
 #include "chrome/browser/password_manager/auto_signin_first_run_dialog_android.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/common/password_manager_pref_names.h"
 #include "components/prefs/pref_service.h"
 #include "content/public/browser/web_contents.h"
@@ -52,6 +54,7 @@ TEST_F(AutoSigninFirstRunDialogAndroidTest,
 
 TEST_F(AutoSigninFirstRunDialogAndroidTest,
        CheckResetOfPrefAfterFirstRunMessageWasShownOnTurnOkClicked) {
+  base::HistogramTester histogram_tester;
   prefs()->SetBoolean(
       password_manager::prefs::kWasAutoSignInFirstRunExperienceShown, false);
   prefs()->SetBoolean(password_manager::prefs::kCredentialsEnableAutosignin,
@@ -63,10 +66,14 @@ TEST_F(AutoSigninFirstRunDialogAndroidTest,
       password_manager::prefs::kWasAutoSignInFirstRunExperienceShown));
   EXPECT_TRUE(prefs()->GetBoolean(
       password_manager::prefs::kCredentialsEnableAutosignin));
+  histogram_tester.ExpectUniqueSample(
+      "PasswordManager.AutoSigninFirstRunDialog",
+      password_manager::metrics_util::AUTO_SIGNIN_OK_GOT_IT, 1);
 }
 
 TEST_F(AutoSigninFirstRunDialogAndroidTest,
        CheckResetOfPrefAfterFirstRunMessageWasShownOnTurnOffClicked) {
+  base::HistogramTester histogram_tester;
   prefs()->SetBoolean(
       password_manager::prefs::kWasAutoSignInFirstRunExperienceShown, false);
   prefs()->SetBoolean(password_manager::prefs::kCredentialsEnableAutosignin,
@@ -78,4 +85,7 @@ TEST_F(AutoSigninFirstRunDialogAndroidTest,
       password_manager::prefs::kWasAutoSignInFirstRunExperienceShown));
   EXPECT_FALSE(prefs()->GetBoolean(
       password_manager::prefs::kCredentialsEnableAutosignin));
+  histogram_tester.ExpectUniqueSample(
+      "PasswordManager.AutoSigninFirstRunDialog",
+      password_manager::metrics_util::AUTO_SIGNIN_TURN_OFF, 1);
 }
