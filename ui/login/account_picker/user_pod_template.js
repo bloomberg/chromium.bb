cffe126ec47925e6b74a7d61ca70bfc953d55c1c
jdufault <jdufault@chromium.org>
2016-05-11 16:59:24 -0700

    Remove most of the loading delay for warning icon.
    This does not appear to cause any performance regressions when loading the lock
    screen.
    
    BUG=605893
    
    Review-Url: https://codereview.chromium.org/1925453005
    Cr-Commit-Position: refs/heads/master@{#393111}
    
    Former-commit-id: 637d3009b75b4837c9a151e84615f57b984a8fa2

--- ui/login/account_picker/user_pod_template.js
+++ ui/login/account_picker/user_pod_template.js
@@ -5,22 +5,24 @@
 (function() {
   "use strict";
 
-  var START_LOADING_DELAY = 1000;
-
-  function doLazyLoad() {
-    function lazyLoadUrl(url) {
-      var link = document.createElement('link');
-      link.rel = 'import';
-      link.href = url;
-      document.body.appendChild(link);
-    }
-
-    lazyLoadUrl('chrome://resources/polymer/v1_0/iron-icons/iron-icons.html');
-    lazyLoadUrl(
-        'chrome://resources/polymer/v1_0/paper-button/paper-button.html');
-  }
+  var RESOURCES_TO_LOAD = [
+    'chrome://resources/polymer/v1_0/iron-icons/iron-icons.html',
+    'chrome://resources/polymer/v1_0/paper-button/paper-button.html'
+  ];
+  /* const */ var IDLE_TIMEOUT_MS = 200;
 
   window.addEventListener('load', function() {
-    setTimeout(doLazyLoad, START_LOADING_DELAY);
+    // The user pod template gets cloned shortly after the load event to make
+    // the actual user pods. It then takes a few update cycles to style these
+    // elements. Loading polymer will block the DOM, so we try to load polymer
+    // after the user pods have been cloned and styled.
+    requestIdleCallback(function() {
+      for (var resourceUrl of RESOURCES_TO_LOAD) {
+        var link = document.createElement('link');
+        link.rel = 'import';
+        link.href = resourceUrl;
+        document.head.appendChild(link);
+      }
+    }, { timeout: IDLE_TIMEOUT_MS });
   });
 })();
