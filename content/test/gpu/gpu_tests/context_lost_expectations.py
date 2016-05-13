d8392fc6d09679ca3b8a94ab2a1b4fde63e8d785
dyen <dyen@chromium.org>
2016-05-12 23:14:26 -0700

    Tentatively enable flaky GpuCrash.GPUProcessCrashesExactlyOnce again.
    R=kbr@chromium.org
    BUG=608923, 606112
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/1971403002
    Cr-Commit-Position: refs/heads/master@{#393454}
    
    Former-commit-id: 889fa48ee6cbe56579a77382232eaef9293816c2

--- content/test/gpu/gpu_tests/context_lost_expectations.py
+++ content/test/gpu/gpu_tests/context_lost_expectations.py
@@ -31,12 +31,6 @@ class ContextLostExpectations(GpuTestExpectations):
     self.Skip('ContextLost.WebGLContextLostFromSelectElement',
               ['lion', 'debug'], bug=498149)
 
-    # Flaky on linux_chromium_rel_ng and mac_chromium_rel_ng bots.
-    self.Flaky('GpuCrash.GPUProcessCrashesExactlyOnce',
-               ['linux'], bug=608923)
-    self.Flaky('GpuCrash.GPUProcessCrashesExactlyOnce',
-               ['mac'], bug=608923)
-
     # 'Browser must support tab control' raised on Android
     self.Fail('GpuCrash.GPUProcessCrashesExactlyOnce',
               ['android'], bug=609629)
