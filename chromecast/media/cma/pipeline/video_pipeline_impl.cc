9ff149e9d225826ab698a50a9a04afcf399a60aa
jasonroberts <jasonroberts@google.com>
2016-05-13 06:46:06 -0700

    [Chromecast] Add metrics reporting for video resolution.
    BUG= internal b/28726662
    
    Review-Url: https://codereview.chromium.org/1966933005
    Cr-Commit-Position: refs/heads/master@{#393516}
    
    Former-commit-id: 3956a6dd16fee36fcd6a5ec2be46267cb35a1a3f

--- chromecast/media/cma/pipeline/video_pipeline_impl.cc
+++ chromecast/media/cma/pipeline/video_pipeline_impl.cc
@@ -8,6 +8,7 @@
 #include <utility>
 
 #include "base/bind.h"
+#include "chromecast/base/metrics/cast_metrics_helper.h"
 #include "chromecast/media/cdm/browser_cdm_cast.h"
 #include "chromecast/media/cma/base/buffering_defs.h"
 #include "chromecast/media/cma/base/cma_logging.h"
@@ -76,6 +77,12 @@ VideoPipelineImpl::~VideoPipelineImpl() {
 void VideoPipelineImpl::OnVideoResolutionChanged(const Size& size) {
   if (state() != kPlaying)
     return;
+
+  metrics::CastMetricsHelper* metrics_helper =
+      metrics::CastMetricsHelper::GetInstance();
+  int encoded_video_resolution = (size.width << 16) | size.height;
+  metrics_helper->RecordApplicationEventWithValue(
+      "Cast.Platform.VideoResolution", encoded_video_resolution);
 
   if (!natural_size_changed_cb_.is_null()) {
     natural_size_changed_cb_.Run(gfx::Size(size.width, size.height));
