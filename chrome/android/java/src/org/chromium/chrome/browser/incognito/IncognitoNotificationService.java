0215ec65aa2319a850553c348140f6f11c23454c
tedchoc <tedchoc@chromium.org>
2016-05-13 15:52:18 -0700

    Handle cleaning up Android recents snapshots for the incognito notification.
    BUG=597147
    
    Review-Url: https://codereview.chromium.org/1976163005
    Cr-Commit-Position: refs/heads/master@{#393665}
    
    Former-commit-id: d5c76083b577f9dbf7b8c5933661c82b4131dee6

--- chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/incognito/IncognitoNotificationService.java
@@ -4,19 +4,30 @@
 
 package org.chromium.chrome.browser.incognito;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityManager.AppTask;
+import android.app.ActivityManager.RecentTaskInfo;
 import android.app.IntentService;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.text.TextUtils;
 import android.util.Pair;
 
+import org.chromium.base.ActivityState;
 import org.chromium.base.ApplicationStatus;
+import org.chromium.base.ContextUtils;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.browser.ChromeApplication;
 import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.chrome.browser.TabState;
+import org.chromium.chrome.browser.document.ChromeLauncherActivity;
+import org.chromium.chrome.browser.document.DocumentUtils;
 import org.chromium.chrome.browser.tabmodel.TabPersistentStore;
 import org.chromium.chrome.browser.tabmodel.TabWindowManager;
 import org.chromium.chrome.browser.util.FeatureUtilities;
@@ -24,7 +35,9 @@ import org.chromium.chrome.browser.util.FeatureUtilities;
 import java.io.File;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 /**
@@ -90,8 +103,85 @@ public class IncognitoNotificationService extends IntentService {
                 if (incognitoCount == 0) {
                     IncognitoNotificationManager.dismissIncognitoNotification();
                 }
+
+                // Now ensure that the snapshots in recents are all cleared for Tabbed activities
+                // to remove any trace of incognito mode.
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                    focusChromeIfNecessary();
+                } else {
+                    removeNonVisibleChromeTabbedRecentEntries();
+                }
             }
         });
+    }
+
+    private void focusChromeIfNecessary() {
+        Set<Integer> visibleTaskIds = getTaskIdsForVisibleActivities();
+        int tabbedTaskId = -1;
+
+        List<WeakReference<Activity>> runningActivities =
+                ApplicationStatus.getRunningActivities();
+        for (int i = 0; i < runningActivities.size(); i++) {
+            Activity activity = runningActivities.get(i).get();
+            if (activity == null) continue;
+
+            if (activity instanceof ChromeTabbedActivity) {
+                tabbedTaskId = activity.getTaskId();
+                break;
+            }
+        }
+
+        // If the task containing the tabbed activity is visible, then do nothing as there is no
+        // snapshot that would need to be regenerated.
+        if (visibleTaskIds.contains(tabbedTaskId)) return;
+
+        Context context = ContextUtils.getApplicationContext();
+        Intent startIntent = new Intent(Intent.ACTION_MAIN);
+        startIntent.setPackage(context.getPackageName());
+        startIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(startIntent);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void removeNonVisibleChromeTabbedRecentEntries() {
+        Set<Integer> visibleTaskIds = getTaskIdsForVisibleActivities();
+
+        Context context = ContextUtils.getApplicationContext();
+        ActivityManager manager =
+                (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        PackageManager pm = getPackageManager();
+
+        for (AppTask task : manager.getAppTasks()) {
+            RecentTaskInfo info = DocumentUtils.getTaskInfoFromTask(task);
+            if (info == null) continue;
+            String className = DocumentUtils.getTaskClassName(task, pm);
+
+            // It is not easily possible to distinguish between tasks sitting on top of
+            // ChromeLauncherActivity, so we treat them all as likely ChromeTabbedActivities and
+            // close them to be on the cautious side of things.
+            if ((TextUtils.equals(className, ChromeTabbedActivity.class.getName())
+                    || TextUtils.equals(className, ChromeLauncherActivity.class.getName()))
+                    && !visibleTaskIds.contains(info.id)) {
+                task.finishAndRemoveTask();
+            }
+        }
+    }
+
+    private Set<Integer> getTaskIdsForVisibleActivities() {
+        List<WeakReference<Activity>> runningActivities =
+                ApplicationStatus.getRunningActivities();
+        Set<Integer> visibleTaskIds = new HashSet<>();
+        for (int i = 0; i < runningActivities.size(); i++) {
+            Activity activity = runningActivities.get(i).get();
+            if (activity == null) continue;
+
+            int activityState = ApplicationStatus.getStateForActivity(activity);
+            if (activityState != ActivityState.STOPPED
+                    && activityState != ActivityState.DESTROYED) {
+                visibleTaskIds.add(activity.getTaskId());
+            }
+        }
+        return visibleTaskIds;
     }
 
     /**
