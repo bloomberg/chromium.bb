90752bf24b9f53d94a6951d47bcc7f84bde546d5
jbudorick <jbudorick@chromium.org>
2016-05-16 11:56:15 -0700

    [Android] Back out UTC changes for pre-M in provision_devices.
    This also changes exit codes for two types of device errors.
    
    BUG=612150
    TBR=stip@chromium.org
    
    Review-Url: https://codereview.chromium.org/1986443002
    Cr-Commit-Position: refs/heads/master@{#393887}
    
    Former-commit-id: 78e09f919d473d2b5044ca53b990ee6ef650de42

--- build/android/provision_devices.py
+++ build/android/provision_devices.py
@@ -35,6 +35,7 @@ from devil.android import device_temp_file
 from devil.android import device_utils
 from devil.android.sdk import keyevent
 from devil.android.sdk import version_codes
+from devil.constants import exit_codes
 from devil.utils import run_tests_helper
 from devil.utils import timeout_retry
 from pylib import constants
@@ -370,16 +371,21 @@ def FinishProvisioning(device, options):
     if device.build_version_sdk >= version_codes.MARSHMALLOW:
       date_format = '%m%d%H%M%Y.%S'
       set_date_command = ['date', '-u']
+      get_date_command = ['date', '-u']
     else:
       date_format = '%Y%m%d.%H%M%S'
-      set_date_command = ['date', '-u', '-s']
+      set_date_command = ['date', '-s']
+      get_date_command = ['date']
+
+    # TODO(jbudorick): This is wrong on pre-M devices -- get/set are
+    # dealing in local time, but we're setting based on GMT.
     strgmtime = time.strftime(date_format, time.gmtime())
     set_date_command.append(strgmtime)
     device.RunShellCommand(set_date_command, as_root=True, check_return=True)
 
+    get_date_command.append('+"%Y%m%d.%H%M%S"')
     device_time = device.RunShellCommand(
-        ['date', '-u', '+"%Y%m%d.%H%M%S"'], as_root=True,
-        single_line=True).replace('"', '')
+        get_date_command, as_root=True, single_line=True).replace('"', '')
     device_time = datetime.datetime.strptime(device_time, "%Y%m%d.%H%M%S")
     correct_time = datetime.datetime.strptime(strgmtime, date_format)
     tdelta = (correct_time - device_time).seconds
@@ -549,7 +555,10 @@ def main():
 
   devil_chromium.Initialize(custom_deps=devil_custom_deps)
 
-  return ProvisionDevices(args)
+  try:
+    return ProvisionDevices(args)
+  except (device_errors.DeviceUnreachableError, device_errors.NoDevicesError):
+    return exit_codes.INFRA
 
 
 if __name__ == '__main__':
