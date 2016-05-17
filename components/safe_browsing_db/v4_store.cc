65ed2468582c2857e92f106c19bde84a0d056333
qyearsley <qyearsley@chromium.org>
2016-05-17 16:05:13 -0700

    Reland of land: Initialize and reset V4LocalDBManager. Instantiate V4Stores. (patchset #1 id:1 of https://codereview.chromium.org/1984283003/ )
    Reason for revert:
    Revert didn't help the problem (tests still failing as before on https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29?numbuilds=50)
    
    Original issue's description:
    > Revert of Reland: Initialize and reset V4LocalDBManager. Instantiate V4Stores. (patchset #6 id:100001 of https://codereview.chromium.org/1983723002/ )
    >
    > Reason for revert:
    > Speculative revert for http://crbug.com/612523; will re-land if that builder still fails the same tests after the revert
    >
    > Original issue's description:
    > > Reland: Initialize and reset V4LocalDBManager. Instantiate V4Stores.
    > > A rough sketch of how this works is here: https://goto.google.com/pver4-LoadUpdateDatabase-doc
    > >
    > > This relands https://codereview.chromium.org/1954393002 which was reverted by https://codereview.chromium.org/1983603002
    > >
    > > Reason for failure: StopOnIOThread ran first and reset v4_update_protocol_manager_, which we were later tried to dereference in DatabaseReady()
    > >
    > > --
    > > Original commit: http://crrev.com/c7057e745067d9aa08dacf142a185ae371c5f3a7 [formerly 32b9468ab5177534cb155b2d222611a6bca16c98]
    > > Which was reverted as: http://crrev.com/68bf37ebcb468449a300579942ff29579ca173f6 [formerly 517133dfc95a9ca2b599bfe7452205e5b5d81fd4]
    > >
    > > This CL reverts commit http://crrev.com/68bf37ebcb468449a300579942ff29579ca173f6 [formerly 517133dfc95a9ca2b599bfe7452205e5b5d81fd4]
    > > --
    > >
    > > BUG=543161
    > >
    > > Committed: https://crrev.com/a2003dc7334c2107f597ca3591874c9be7e92291 [formerly 26771182e06d28496aca4c73e095d3c9a43713ce]
    > > Cr-Commit-Position: refs/heads/master@{#393989}
    >
    > TBR=shess@chromium.org,nparker@chromium.org,vakh@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=543161
    >
    > Committed: https://crrev.com/47f090069b1acf2bdb7c2a9dc1bfde09fc50d0d6 [formerly ed2c1c1767e9eb657d804b32088ba141502b645e]
    > Cr-Commit-Position: refs/heads/master@{#394227}
    
    TBR=shess@chromium.org,nparker@chromium.org,vakh@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=543161
    
    Review-Url: https://codereview.chromium.org/1985263002
    Cr-Commit-Position: refs/heads/master@{#394257}
    
    Former-commit-id: 0c301ecdffa4dcd497a30661b716fb170a17c4d6

---
+++ components/safe_browsing_db/v4_store.cc
@@ -0,0 +1,19 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/safe_browsing_db/v4_store.h"
+
+namespace safe_browsing {
+
+V4Store::V4Store(const scoped_refptr<base::SequencedTaskRunner>& task_runner,
+                 const base::FilePath& store_path) {}
+
+V4Store::~V4Store() {}
+
+bool V4Store::Reset() {
+  // TODO(vakh): Implement skeleton.
+  return true;
+}
+
+}  // namespace safe_browsing
