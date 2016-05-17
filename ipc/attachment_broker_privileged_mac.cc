708e528356d431a9cab8c05ae911a7c187c481da
erikchen <erikchen@chromium.org>
2016-05-17 10:28:27 -0700

    Do not emit a useless UMA metric from AttachmentBrokerPrivilegedMac.
    The error DELAYED is not useful, since it is emitted each time the broker fails
    to run the processors or extractors for a source/destination, rather than for
    each message that cannot be brokered.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/1985083002
    Cr-Commit-Position: refs/heads/master@{#394155}
    
    Former-commit-id: f326b984b03a5d7a5242c9c0741e25d83cf6b40b

--- ipc/attachment_broker_privileged_mac.cc
+++ ipc/attachment_broker_privileged_mac.cc
@@ -243,15 +243,14 @@ void AttachmentBrokerPrivilegedMac::SendPrecursorsForProcess(
     AttachmentBrokerPrivileged::EndpointRunnerPair pair =
         GetSenderWithProcessId(pid);
     if (!pair.first) {
-      if (store_on_failure) {
-        // Try again later.
-        LogError(DELAYED);
-      } else {
-        // If there is no sender, then permanently fail.
-        LogError(DESTINATION_NOT_FOUND);
-        delete it->second;
-        precursors_.erase(it);
-      }
+      // Try again later.
+      if (store_on_failure)
+        return;
+
+      // If there is no sender, then permanently fail.
+      LogError(DESTINATION_NOT_FOUND);
+      delete it->second;
+      precursors_.erase(it);
       return;
     }
   }
@@ -340,16 +339,15 @@ void AttachmentBrokerPrivilegedMac::ProcessExtractorsForProcess(
     AttachmentBrokerPrivileged::EndpointRunnerPair pair =
         GetSenderWithProcessId(pid);
     if (!pair.first) {
-      if (store_on_failure) {
-        // If there is no sender, then the communication channel with the source
-        // process has not yet been established. Try again later.
-        LogError(DELAYED);
-      } else {
-        // There is no sender. Permanently fail.
-        LogError(ERROR_SOURCE_NOT_FOUND);
-        delete it->second;
-        extractors_.erase(it);
-      }
+      // If there is no sender, then the communication channel with the source
+      // process has not yet been established. Try again later.
+      if (store_on_failure)
+        return;
+
+      // There is no sender. Permanently fail.
+      LogError(ERROR_SOURCE_NOT_FOUND);
+      delete it->second;
+      extractors_.erase(it);
       return;
     }
   }
