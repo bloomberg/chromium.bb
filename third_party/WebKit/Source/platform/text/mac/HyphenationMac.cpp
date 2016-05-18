3b1056c4baaa7d17465030325c37c2b51ce936f6
kojii <kojii@chromium.org>
2016-05-18 04:58:12 -0700

    Support Mac system hyphenation dictionary
    This patch supports hyphenation dictionary for Mac using Core
    Foundation API.
    
    BUG=605840
    
    Review-Url: https://codereview.chromium.org/1994443002
    Cr-Commit-Position: refs/heads/master@{#394388}
    
    Former-commit-id: c1d8f02d2ebf6dbaeb7f593c6a75ee494a900ca1

--- third_party/WebKit/Source/platform/text/mac/HyphenationMac.cpp
+++ third_party/WebKit/Source/platform/text/mac/HyphenationMac.cpp
@@ -4,11 +4,38 @@
 
 #include "platform/text/Hyphenation.h"
 
+#include "wtf/RetainPtr.h"
+#include "wtf/text/StringView.h"
+
 namespace blink {
 
-PassRefPtr<Hyphenation> Hyphenation::platformGetHyphenation(const AtomicString&)
+class HyphenationCF : public Hyphenation {
+public:
+    HyphenationCF(RetainPtr<CFLocaleRef>& localeCF)
+        : m_localeCF(localeCF)
+    {
+        DCHECK(m_localeCF);
+    }
+
+    size_t lastHyphenLocation(const StringView& text, size_t beforeIndex) const override
+    {
+        CFIndex result = CFStringGetHyphenationLocationBeforeIndex(
+            text.toString()->createCFString().get(), beforeIndex,
+            CFRangeMake(0, text.length()), 0, m_localeCF.get(), 0);
+        return result == kCFNotFound ? 0 : result;
+    }
+
+private:
+    RetainPtr<CFLocaleRef> m_localeCF;
+};
+
+PassRefPtr<Hyphenation> Hyphenation::platformGetHyphenation(const AtomicString& locale)
 {
-    return nullptr;
+    RetainPtr<CFStringRef> localeCFString = locale.impl()->createCFString();
+    RetainPtr<CFLocaleRef> localeCF = adoptCF(CFLocaleCreate(kCFAllocatorDefault, localeCFString.get()));
+    if (!CFStringIsHyphenationAvailableForLocale(localeCF.get()))
+        return nullptr;
+    return adoptRef(new HyphenationCF(localeCF));
 }
 
 } // namespace blink
