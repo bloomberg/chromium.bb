585674028d3a6df56b1a35eafaafa059d977dd4b
krasin <krasin@google.com>
2016-05-18 08:17:36 -0700

    Reserve more RAM per Gold process.
    The current settings are too agressive even given the fact that
    all the bots have 200+ GB RAM, and there's not so many large targets.
    OOMs were observed:
    https://build.chromium.org/p/chromium.fyi/builders/LTO%20Linux/builds/500
    
    BUG=453195
    
    Review-Url: https://codereview.chromium.org/1989083003
    Cr-Commit-Position: refs/heads/master@{#394423}
    
    Former-commit-id: b1e7d762e134b94196f551667ed31535b701e76e

--- build/toolchain/get_concurrent_links.py
+++ build/toolchain/get_concurrent_links.py
@@ -55,7 +55,7 @@ def _GetDefaultConcurrentLinks(is_lto):
           if is_lto:
             mem_total_gb -= 10 # Reserve
             # For LTO builds the RAM requirements are even higher
-            mem_per_link_gb = 16
+            mem_per_link_gb = 24
           return int(max(1, mem_total_gb / mem_per_link_gb))
     return 1
   elif sys.platform == 'darwin':
