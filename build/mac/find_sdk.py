c3fde6756cdf71f5282967a04c56bb2df360f39f
thakis <thakis@chromium.org>
2016-05-18 15:00:06 -0700

    mac: Make `find_sdk.py --print_sdk_path` much faster.
    Use xcrun instead of xcodebuild if --print_sdk_path is passed.  Reduces time
    to run `gn gen` from 2.2s to 1.95s on my MacBook Pro.
    
    (In the gyp build, we don't pass --print_sdk_path to this script, so I didn't
    notice this script had this slow path until now.)
    
    While here, also stop pretending that we support Xcodes older than 4.3.
    
    BUG=609541
    
    Review-Url: https://codereview.chromium.org/1988023006
    Cr-Commit-Position: refs/heads/master@{#394565}
    
    Former-commit-id: d50140bae570f519cea29a8b20e58511032cbfd7

--- build/mac/find_sdk.py
+++ build/mac/find_sdk.py
@@ -52,16 +52,9 @@ def main():
   if job.returncode != 0:
     print >> sys.stderr, out
     print >> sys.stderr, err
-    raise Exception(('Error %d running xcode-select, you might have to run '
-      '|sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer| '
-      'if you are using Xcode 4.') % job.returncode)
-  # The Developer folder moved in Xcode 4.3.
-  xcode43_sdk_path = os.path.join(
+    raise Exception('Error %d running xcode-select' % job.returncode)
+  sdk_dir = os.path.join(
       out.rstrip(), 'Platforms/MacOSX.platform/Developer/SDKs')
-  if os.path.isdir(xcode43_sdk_path):
-    sdk_dir = xcode43_sdk_path
-  else:
-    sdk_dir = os.path.join(out.rstrip(), 'SDKs')
   sdks = [re.findall('^MacOSX(10\.\d+)\.sdk$', s) for s in os.listdir(sdk_dir)]
   sdks = [s[0] for s in sdks if s]  # [['10.5'], ['10.6']] => ['10.5', '10.6']
   sdks = [s for s in sdks  # ['10.5', '10.6'] => ['10.6']
@@ -85,8 +78,8 @@ def main():
     return min_sdk_version
 
   if options.print_sdk_path:
-    print subprocess.check_output(['xcodebuild', '-version', '-sdk',
-                                   'macosx' + best_sdk, 'Path']).strip()
+    print subprocess.check_output(
+        ['xcrun', '-sdk', 'macosx' + best_sdk, '--show-sdk-path']).strip()
 
   return best_sdk
 
