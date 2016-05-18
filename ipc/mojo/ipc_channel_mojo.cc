96e1d24e446a69f7296c503a3ce8537ce2005d10
rockot <rockot@chromium.org>
2016-05-18 15:03:37 -0700

    Disable thread-safe Send on ChannelMojo
    The world is not yet ready for thread-safe Send. D:
    
    BUG=612944
    TBR=jam@chromium.org
    
    Review-Url: https://codereview.chromium.org/1991913003
    Cr-Commit-Position: refs/heads/master@{#394568}
    
    Former-commit-id: ff0d8038dd81bb025313a393cb8a6fcde9a79603

--- ipc/mojo/ipc_channel_mojo.cc
+++ ipc/mojo/ipc_channel_mojo.cc
@@ -346,7 +346,7 @@ bool ChannelMojo::Send(Message* message) {
 }
 
 bool ChannelMojo::IsSendThreadSafe() const {
-  return true;
+  return false;
 }
 
 base::ProcessId ChannelMojo::GetPeerPID() const {
