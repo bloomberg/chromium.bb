6737032de2458eb9180851fb1afed497ded90c32
hlopko <hlopko@chromium.org>
2016-05-18 06:24:25 -0700

    Implement SetWrapperReferenceFrom idl attribute within traceWrappers for node lists
    SetWrapperReferenceFrom(param) idl attribute denotes that when a <param> object
    is alive, current object should be kept alive too. To implement the same logic
    using tracing, we need to find a path from <param> object to the current object.
    
    For the node lists, we are lucky because all classes inheriting from
    LiveNodeListBase (= all node lists) register themselves in their document in the
    constructor. Node lists are tested by fast/dom/gc-9.html layout test.
    
    Follow status of tracing SetWrapperReferenceFrom here:
    
    https://docs.google.com/a/google.com/spreadsheets/d/1HY7Flx2TTyWIi9S2efRWMG2nRl84E7UB2t7dx_b5K00/edit?usp=sharing
    
    LOG=no
    BUG=468240
    
    Review-Url: https://codereview.chromium.org/1986633003
    Cr-Commit-Position: refs/heads/master@{#394400}
    
    Former-commit-id: a43e2359d03497a2a8a7e8327470abcf18eaa7d0

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -6000,6 +6000,11 @@ DEFINE_TRACE_WRAPPERS(Document)
     visitor->traceWrappers(
         Supplementable<Document>::m_supplements.get(
             FontFaceSet::supplementName()));
+    for (int i = 0; i < numNodeListInvalidationTypes; ++i) {
+        for (auto list : m_nodeLists[i]) {
+            visitor->traceWrappers(list);
+        }
+    }
     ContainerNode::traceWrappers(visitor);
 }
 
