f7ccad45013347698f53fdefe996b93b298548f7
hiroshige <hiroshige@chromium.org>
2016-05-18 06:41:57 -0700

    Revert of Add RELEASE_ASSERT()s for |MemoryCache::m_liveSize/m_deadSize| (patchset #2 id:20001 of https://codereview.chromium.org/1959573002/ )
    Reason for revert:
    Reverting test code as planned before going to beta.
    
    Original issue's description:
    > Add RELEASE_ASSERT()s for |MemoryCache::m_liveSize/m_deadSize|
    >
    > To check whether Issue 570043 is fixed, this CL temporarily turns some
    > ASSERT()s for |MemoryCache::m_liveSize/m_deadSize| into RELEASE_ASSERT()s.
    >
    > BUG=570043
    >
    > Committed: https://crrev.com/d6c14909876a3d9478a7056a99b581752b7f6812 [formerly 4f35b4ca6678836a93dc4b3e0160b55f0895a4fe]
    > Cr-Commit-Position: refs/heads/master@{#392858}
    
    TBR=yhirano@chromium.org,junov@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=570043
    
    Review-Url: https://codereview.chromium.org/1993733002
    Cr-Commit-Position: refs/heads/master@{#394405}
    
    Former-commit-id: 0498a35847221a5b4cc22e70c95018d5bc962ea5

--- third_party/WebKit/Source/core/fetch/MemoryCache.cpp
+++ third_party/WebKit/Source/core/fetch/MemoryCache.cpp
@@ -514,9 +514,7 @@ void MemoryCache::makeLive(Resource* resource)
 {
     if (!contains(resource))
         return;
-    // TODO(hiroshige): RELEASE_ASSERT() is used here to check whether
-    // crbug.com/570043 was fixed and should be reverted before going to beta.
-    RELEASE_ASSERT(m_deadSize >= resource->size());
+    ASSERT(m_deadSize >= resource->size());
     m_liveSize += resource->size();
     m_deadSize -= resource->size();
 }
@@ -525,9 +523,6 @@ void MemoryCache::makeDead(Resource* resource)
 {
     if (!contains(resource))
         return;
-    // TODO(hiroshige): RELEASE_ASSERT() is used here to check whether
-    // crbug.com/570043 was fixed and should be reverted before going to beta.
-    RELEASE_ASSERT(m_liveSize >= resource->size());
     m_liveSize -= resource->size();
     m_deadSize += resource->size();
     removeFromLiveDecodedResourcesList(getEntryForResource(resource));
@@ -549,13 +544,11 @@ void MemoryCache::update(Resource* resource, size_t oldSize, size_t newSize, boo
         insertInLRUList(entry, lruListFor(entry->m_accessCount, newSize));
 
     ptrdiff_t delta = newSize - oldSize;
-    // TODO(hiroshige): RELEASE_ASSERT()s are used below to check whether
-    // crbug.com/570043 was fixed and should be reverted before going to beta.
     if (resource->hasClientsOrObservers()) {
-        RELEASE_ASSERT(delta >= 0 || m_liveSize >= static_cast<size_t>(-delta) );
+        ASSERT(delta >= 0 || m_liveSize >= static_cast<size_t>(-delta) );
         m_liveSize += delta;
     } else {
-        RELEASE_ASSERT(delta >= 0 || m_deadSize >= static_cast<size_t>(-delta) );
+        ASSERT(delta >= 0 || m_deadSize >= static_cast<size_t>(-delta) );
         m_deadSize += delta;
     }
 }
