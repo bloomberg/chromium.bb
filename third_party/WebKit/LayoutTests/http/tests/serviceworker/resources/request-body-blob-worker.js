d0353cc04e1ef891d6f74d59fd06209b06d5661b
falken <falken@chromium.org>
2016-05-19 02:45:02 -0700

    service worker: Deflake request-body-blob.html and request-body-blob-crash.html
    It wasn't actually waiting for the service worker to be activated.
    
    This patch only affects tests.
    
    BUG=608544
    
    Review-Url: https://codereview.chromium.org/1994083002
    Cr-Commit-Position: refs/heads/master@{#394723}
    
    Former-commit-id: 1150c234173b9f9390b42860710af3c98537dbe9

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/request-body-blob-worker.js
+++ third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/request-body-blob-worker.js
@@ -1,12 +1,11 @@
 self.addEventListener('fetch', event => {
     if (event.request.url.indexOf('?post-with-blob-body') == -1)
       return;
-    event.respondWith(
-     event.request.text().then(resp => {
-       if (resp.indexOf('it\'s me the blob') == -1)
-         return new Response('fail');
-       if (resp.indexOf('and more blob!') == -1)
-         return new Response('fail');
-       return new Response('Pass');
-     }));
+    event.respondWith(event.request.text().then(body => {
+        if (body.indexOf('it\'s me the blob') == -1)
+          return new Response('fail');
+        if (body.indexOf('and more blob!') == -1)
+          return new Response('fail');
+        return new Response('Pass');
+      }));
   });
