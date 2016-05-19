e976f5762b11dbcbf5b59d53fd868a4f94f9ab7b
eugenebut <eugenebut@chromium.org>
2016-05-19 07:49:49 -0700

    [ios Mojo] Allow loading WebUI resources in TestRequestContextGetter.
    ContextGetter is used by WebUI to load page resources.
    TestContextURLRequestContextGetter is a context getter used by web
    integration tests. This CL enabled loading WebUI resources in tests, which
    is necessary for Mojo integration test.
    
    Design doc:
    https://docs.google.com/document/d/1iUJXX_zoY2BbnecpfxjznOeasf29KUzl_9S_LG08Bes/edit#heading=h.yjqp5garsqf9
    
    BUG=567809
    
    Review-Url: https://codereview.chromium.org/1991893003
    Cr-Commit-Position: refs/heads/master@{#394767}
    
    Former-commit-id: 30a5fbbf891a859e48c669e4650bbf4b50c2a2f5

--- ios/web/public/test/test_browser_state.cc
+++ ios/web/public/test/test_browser_state.cc
@@ -5,35 +5,43 @@
 #include "ios/web/public/test/test_browser_state.h"
 
 #include "base/files/file_path.h"
-#include "base/test/null_task_runner.h"
+#include "ios/web/public/web_thread.h"
+#include "ios/web/test/test_url_constants.h"
+#include "ios/web/webui/url_data_manager_ios_backend.h"
 #include "net/url_request/url_request_context.h"
 #include "net/url_request/url_request_context_getter.h"
+#include "net/url_request/url_request_job_factory_impl.h"
 #include "net/url_request/url_request_test_util.h"
+
+namespace web {
 
 namespace {
 
 class TestContextURLRequestContextGetter : public net::URLRequestContextGetter {
  public:
-  TestContextURLRequestContextGetter()
-      : null_task_runner_(new base::NullTaskRunner) {}
+  TestContextURLRequestContextGetter(web::BrowserState* browser_state) {
+    job_factory_.SetProtocolHandler(
+        kTestWebUIScheme,
+        web::URLDataManagerIOSBackend::CreateProtocolHandler(browser_state));
+    context_.set_job_factory(&job_factory_);
+  }
 
   net::URLRequestContext* GetURLRequestContext() override { return &context_; }
 
   scoped_refptr<base::SingleThreadTaskRunner>
   GetNetworkTaskRunner() const override {
-    return null_task_runner_;
+    return web::WebThread::GetTaskRunnerForThread(web::WebThread::IO);
   }
 
  private:
   ~TestContextURLRequestContextGetter() override {}
 
   net::TestURLRequestContext context_;
-  scoped_refptr<base::SingleThreadTaskRunner> null_task_runner_;
+  net::URLRequestJobFactoryImpl job_factory_;
 };
 
 }  // namespace
 
-namespace web {
 TestBrowserState::TestBrowserState() : is_off_the_record_(false) {}
 
 TestBrowserState::~TestBrowserState() {
@@ -49,7 +57,7 @@ base::FilePath TestBrowserState::GetStatePath() const {
 
 net::URLRequestContextGetter* TestBrowserState::GetRequestContext() {
   if (!request_context_)
-    request_context_ = new TestContextURLRequestContextGetter();
+    request_context_ = new TestContextURLRequestContextGetter(this);
   return request_context_.get();
 }
 
