ea5c606f32c084dc18289f2b4520c6bc8189772e
sandersd <sandersd@chromium.org>
2016-05-20 14:05:44 -0700

    H264POC: Allow gaps in frame_num.
    This change only removes the restriction, it does not try to detect
    all the cases where doing so is likely to result in problems. In most
    cases (specifically, those where the number of missing frames is much
    less than max_frame_num), the result will be correct.
    
    In incorrect cases the reorder queue could get confused and result in
    strange playback. The existing TODOs for the reorder queue explain how
    to reduce that, but also inserting dummy frames for gaps would help as
    well.
    
    BUG=550854
    
    Review-Url: https://codereview.chromium.org/1967893002
    Cr-Commit-Position: refs/heads/master@{#395164}
    
    Former-commit-id: b6f4380493084007dc4237d28153c0dd3e5d520b

--- media/video/h264_poc_unittest.cc
+++ media/video/h264_poc_unittest.cc
@@ -52,11 +52,13 @@ TEST_F(H264POCTest, PicOrderCntType0) {
 
   // Initial IDR with POC 0.
   slice_hdr_.idr_pic_flag = true;
+  slice_hdr_.frame_num = 0;
   ASSERT_TRUE(ComputePOC());
   ASSERT_EQ(0, poc_);
 
   // Ref frame with POC lsb 8.
   slice_hdr_.idr_pic_flag = false;
+  slice_hdr_.frame_num = 1;
   slice_hdr_.pic_order_cnt_lsb = 8;
   ASSERT_TRUE(ComputePOC());
   ASSERT_EQ(8, poc_);
@@ -64,12 +66,14 @@ TEST_F(H264POCTest, PicOrderCntType0) {
   // Ref frame with POC lsb 0. This should be detected as wrapping, as the
   // (negative) gap is at least half the maximum.
   slice_hdr_.pic_order_cnt_lsb = 0;
+  slice_hdr_.frame_num = 2;
   ASSERT_TRUE(ComputePOC());
   ASSERT_EQ(16, poc_);
 
   // Ref frame with POC lsb 9. This should be detected as negative wrapping,
   // as the (positive) gap is more than half the maximum.
   slice_hdr_.pic_order_cnt_lsb = 9;
+  slice_hdr_.frame_num = 3;
   ASSERT_TRUE(ComputePOC());
   ASSERT_EQ(9, poc_);
 }
@@ -80,30 +84,36 @@ TEST_F(H264POCTest, PicOrderCntType0_WithMMCO5) {
 
   // Initial IDR with POC 0.
   slice_hdr_.idr_pic_flag = true;
+  slice_hdr_.frame_num = 0;
   ASSERT_TRUE(ComputePOC());
   ASSERT_EQ(0, poc_);
 
   // Skip ahead.
   slice_hdr_.idr_pic_flag = false;
+  slice_hdr_.frame_num = 1;
   slice_hdr_.pic_order_cnt_lsb = 8;
   ASSERT_TRUE(ComputePOC());
   ASSERT_EQ(8, poc_);
 
+  slice_hdr_.frame_num = 2;
   slice_hdr_.pic_order_cnt_lsb = 0;
   ASSERT_TRUE(ComputePOC());
   ASSERT_EQ(16, poc_);
 
+  slice_hdr_.frame_num = 3;
   slice_hdr_.pic_order_cnt_lsb = 8;
   ASSERT_TRUE(ComputePOC());
   ASSERT_EQ(24, poc_);
 
-  SetMMCO5();
+  slice_hdr_.frame_num = 4;
   slice_hdr_.pic_order_cnt_lsb = 0;
+  SetMMCO5();
   ASSERT_TRUE(ComputePOC());
   ASSERT_EQ(32, poc_);
 
   // Due to the MMCO5 above, this is relative to 0, but also detected as
   // positive wrapping.
+  slice_hdr_.frame_num = 5;
   slice_hdr_.pic_order_cnt_lsb = 8;
   ASSERT_TRUE(ComputePOC());
   ASSERT_EQ(24, poc_);
