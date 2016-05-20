9361804bee1d5d0697449734928a63ac98523071
afakhry <afakhry@chromium.org>
2016-05-20 12:09:31 -0700

    Fix the views Task Manager on Mac showing 0s for the Shared Memory
    R=nick@chromium.org
    BUG=613025
    
    Review-Url: https://codereview.chromium.org/1994653006
    Cr-Commit-Position: refs/heads/master@{#395129}
    
    Former-commit-id: 07a8e7b48eeff1b271e50ea371d8c7e722ee3891

--- chrome/browser/task_management/sampling/task_group_sampler.cc
+++ chrome/browser/task_management/sampling/task_group_sampler.cc
@@ -122,10 +122,18 @@ double TaskGroupSampler::RefreshCpuUsage() {
 MemoryUsageStats TaskGroupSampler::RefreshMemoryUsage() {
   DCHECK(worker_pool_sequenced_checker_.CalledOnValidSequencedThread());
 
-  // TODO(afakhry): Integrate Bruce's CL for faster retrieval of physical memory
-  // on Windows here.
-
   MemoryUsageStats memory_usage;
+#if defined(OS_MACOSX)
+  memory_usage.physical_bytes =
+      static_cast<int64_t>(process_metrics_->GetWorkingSetSize());
+
+  size_t private_bytes = 0;
+  size_t shared_bytes = 0;
+  if (process_metrics_->GetMemoryBytes(&private_bytes, &shared_bytes)) {
+    memory_usage.private_bytes = static_cast<int64_t>(private_bytes);
+    memory_usage.shared_bytes = static_cast<int64_t>(shared_bytes);
+  }
+#else
   // Refreshing the physical/private/shared memory at one shot.
   base::WorkingSetKBytes ws_usage;
   if (process_metrics_->GetWorkingSetKBytes(&ws_usage)) {
@@ -144,11 +152,13 @@ MemoryUsageStats TaskGroupSampler::RefreshMemoryUsage() {
         static_cast<int64_t>(process_metrics_->GetWorkingSetSize());
     memory_usage.physical_bytes -=
         static_cast<int64_t>(ws_usage.shareable * 1024);
-#endif
+#endif  // defined(OS_LINUX)
+
 #if defined(OS_CHROMEOS)
     memory_usage.swapped_bytes = ws_usage.swapped * 1024;
-#endif
+#endif  // defined(OS_CHROMEOS)
   }
+#endif  // defined(OS_MACOSX)
 
   return memory_usage;
 }
