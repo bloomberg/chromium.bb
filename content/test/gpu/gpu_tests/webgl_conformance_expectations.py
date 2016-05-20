9e8eae6b0dba4d9c2070f0882680c6a82fbb3b32
cblume <cblume@chromium.org>
2016-05-19 19:52:14 -0700

    Adding another test expectations for Android.
    Nexus 6:
    
    WebglConformance.conformance_glsl_misc_shader_with_257_character_identifier_frag
    
    BUG=611945
    R=kbr@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/1991163003
    Cr-Commit-Position: refs/heads/master@{#394969}
    
    Former-commit-id: b95e78206c21373c88e457608ef5ae4ceb8ef24e

--- content/test/gpu/gpu_tests/webgl_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl_conformance_expectations.py
@@ -339,6 +339,9 @@ class WebGLConformanceExpectations(GpuTestExpectations):
               ['android', ('qualcomm', 'Adreno (TM) 420')], bug=611945)
     self.Fail('conformance/glsl/misc/shader-with-257-character-define.html',
               ['android', ('qualcomm', 'Adreno (TM) 420')], bug=611945)
+    self.Fail('conformance/glsl/misc/' +
+              'shader-with-257-character-identifier.frag.html',
+              ['android', ('qualcomm', 'Adreno (TM) 420')], bug=611945)
 
     # Nexus 9
     self.Fail('WebglExtension.WEBGL_compressed_texture_atc',
