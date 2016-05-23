31c0c4164cd9b99c4a69e1658f8a22063f160ce4
jonathan.garbee <jonathan.garbee@chromium.org>
2016-05-23 09:43:52 -0700

    Improve console message about deferring long-running timers.
    BUG=574343
    
    Review-Url: https://codereview.chromium.org/1997413002
    Cr-Commit-Position: refs/heads/master@{#395341}
    
    Former-commit-id: 097f2cb6d4af73a27d0456a97bf5053f7f7db913

--- components/scheduler/renderer/renderer_scheduler_impl.cc
+++ components/scheduler/renderer/renderer_scheduler_impl.cc
@@ -1315,8 +1315,11 @@ void RendererSchedulerImpl::OnTriedToExecuteBlockedTask(
     MainThreadOnly().have_reported_blocking_intervention_since_navigation =
         true;
     BroadcastConsoleWarning(
-        "Deferred long-running timer task(s) to improve scrolling smoothness. "
-        "See crbug.com/574343.");
+        "Blink deferred a task in order to make scrolling smoother. "
+        "Your timer tasks should take less than 50ms to run to avoid this. "
+        "Please see "
+        "https://developers.google.com/web/tools/chrome-devtools/profile/evaluate-performance/rail"
+        " and https://crbug.com/574343#c40 for more information.");
   }
 }
 
