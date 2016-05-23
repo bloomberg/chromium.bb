91a44598a87678bcabaf5686c54972cab259fca1
michaeldo <michaeldo@chromium.org>
2016-05-23 16:21:31 -0700

    Fix web_state_delegate_bridge_unittest setter.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2004143003
    Cr-Commit-Position: refs/heads/master@{#395458}
    
    Former-commit-id: 13f1499722d29997190d9a3f29bfa8dee42de0f8

--- ios/web/web_state/web_state_delegate_bridge_unittest.mm
+++ ios/web/web_state/web_state_delegate_bridge_unittest.mm
@@ -48,23 +48,24 @@ TEST_F(WebStateDelegateBridgeTest, LoadProgressChanged) {
 // Tests |HandleContextMenu| forwarding.
 TEST_F(WebStateDelegateBridgeTest, HandleContextMenu) {
   EXPECT_EQ(nil, [delegate_ contextMenuParams]);
-  web::ContextMenuParams contextMenuParams;
-  contextMenuParams.menu_title.reset([@"Menu title" copy]);
-  contextMenuParams.link_url = GURL("http://www.url.com");
-  contextMenuParams.src_url = GURL("http://www.url.com/image.jpeg");
-  contextMenuParams.referrer_policy = web::ReferrerPolicyOrigin;
-  contextMenuParams.view = [UIView new];
-  contextMenuParams.location = CGPointMake(5.0, 5.0);
-  bridge_->HandleContextMenu(nullptr, contextMenuParams);
-  web::ContextMenuParams* resultParams = [delegate_ contextMenuParams];
-  EXPECT_NE(nullptr, resultParams);
-  EXPECT_EQ(contextMenuParams.menu_title, resultParams->menu_title);
-  EXPECT_EQ(contextMenuParams.link_url, resultParams->link_url);
-  EXPECT_EQ(contextMenuParams.src_url, resultParams->src_url);
-  EXPECT_EQ(contextMenuParams.referrer_policy, resultParams->referrer_policy);
-  EXPECT_EQ(contextMenuParams.view, resultParams->view);
-  EXPECT_EQ(contextMenuParams.location.x, resultParams->location.x);
-  EXPECT_EQ(contextMenuParams.location.y, resultParams->location.y);
+  web::ContextMenuParams context_menu_params;
+  context_menu_params.menu_title.reset([@"Menu title" copy]);
+  context_menu_params.link_url = GURL("http://www.url.com");
+  context_menu_params.src_url = GURL("http://www.url.com/image.jpeg");
+  context_menu_params.referrer_policy = web::ReferrerPolicyOrigin;
+  context_menu_params.view = [UIView new];
+  context_menu_params.location = CGPointMake(5.0, 5.0);
+  bridge_->HandleContextMenu(nullptr, context_menu_params);
+  web::ContextMenuParams* result_params = [delegate_ contextMenuParams];
+  EXPECT_NE(nullptr, result_params);
+  EXPECT_EQ(context_menu_params.menu_title, result_params->menu_title);
+  EXPECT_EQ(context_menu_params.link_url, result_params->link_url);
+  EXPECT_EQ(context_menu_params.src_url, result_params->src_url);
+  EXPECT_EQ(context_menu_params.referrer_policy,
+            result_params->referrer_policy);
+  EXPECT_EQ(context_menu_params.view, result_params->view);
+  EXPECT_EQ(context_menu_params.location.x, result_params->location.x);
+  EXPECT_EQ(context_menu_params.location.y, result_params->location.y);
 }
 
 }  // namespace web
