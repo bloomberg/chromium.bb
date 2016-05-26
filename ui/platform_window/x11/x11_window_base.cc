1204d5c8151bb322daf78ebfcab8a66e146bd385
sadrul <sadrul@chromium.org>
2016-05-26 10:02:23 -0700

    x11: Make sure X11WindowBase always computes bounds in screen-space.
    The X11 window-manager can install the window inside a parent window,
    and the parent window may not be located at (0, 0). So translate the
    bounds to screen-space and maintain that as the bounds for the
    X11Window instance. This makes sure hit-testing works correctly when
    such a parent window is involved.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2009363002
    Cr-Commit-Position: refs/heads/master@{#396203}
    
    Former-commit-id: 093812d76b36875bbaa30191e3bdd0e93c11378a

--- ui/platform_window/x11/x11_window_base.cc
+++ ui/platform_window/x11/x11_window_base.cc
@@ -254,7 +254,18 @@ void X11WindowBase::ProcessXWindowEvent(XEvent* xev) {
     case ConfigureNotify: {
       DCHECK_EQ(xwindow_, xev->xconfigure.event);
       DCHECK_EQ(xwindow_, xev->xconfigure.window);
-      gfx::Rect bounds(xev->xconfigure.x, xev->xconfigure.y,
+      // It's possible that the X window may be resized by some other means than
+      // from within aura (e.g. the X window manager can change the size). Make
+      // sure the root window size is maintained properly.
+      int translated_x_in_pixels = xev->xconfigure.x;
+      int translated_y_in_pixels = xev->xconfigure.y;
+      if (!xev->xconfigure.send_event && !xev->xconfigure.override_redirect) {
+        Window unused;
+        XTranslateCoordinates(xdisplay_, xwindow_, xroot_window_, 0, 0,
+                              &translated_x_in_pixels, &translated_y_in_pixels,
+                              &unused);
+      }
+      gfx::Rect bounds(translated_x_in_pixels, translated_y_in_pixels,
                        xev->xconfigure.width, xev->xconfigure.height);
       if (confirmed_bounds_ != bounds) {
         confirmed_bounds_ = bounds;
