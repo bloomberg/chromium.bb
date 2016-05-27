a2c966022f64afd14603ae4f83901cbacdf0c8d4
gogerald <gogerald@chromium.org>
2016-05-27 06:55:49 -0700

    Abort sign in if the sign in flow activity was set but no longer visible to the user
    The root cause of the below bug: onSaveInstanceState is called when Activity is stopped, after that we can not attach Fragment to it.
    
    BUG=615129
    
    Review-Url: https://codereview.chromium.org/2013283002
    Cr-Commit-Position: refs/heads/master@{#396456}
    
    Former-commit-id: 4ef6f1a97fcca34d88de124bda55e0ad4cee5277

--- chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/SigninManager.java
@@ -176,11 +176,13 @@ public class SigninManager implements AccountTrackerService.OnSystemAccountsSeed
         }
 
         /**
-         * Returns whether the sign-in flow activity was set but is no longer valid.
+         * Returns whether the sign-in flow activity was set but is no longer visible to the user.
          */
-        private boolean isActivityDestroyed() {
+        private boolean isActivityInvisible() {
             return activity != null
-                    && ApplicationStatus.getStateForActivity(activity) == ActivityState.DESTROYED;
+                    && (ApplicationStatus.getStateForActivity(activity) == ActivityState.STOPPED
+                               || ApplicationStatus.getStateForActivity(activity)
+                                       == ActivityState.DESTROYED);
         }
     }
 
@@ -390,7 +392,7 @@ public class SigninManager implements AccountTrackerService.OnSystemAccountsSeed
             return;
         }
 
-        if (mSignInState.isActivityDestroyed()) {
+        if (mSignInState.isActivityInvisible()) {
             abortSignIn();
             return;
         }
@@ -417,7 +419,7 @@ public class SigninManager implements AccountTrackerService.OnSystemAccountsSeed
             return;
         }
 
-        if (mSignInState.isActivityDestroyed()) {
+        if (mSignInState.isActivityInvisible()) {
             abortSignIn();
             return;
         }
