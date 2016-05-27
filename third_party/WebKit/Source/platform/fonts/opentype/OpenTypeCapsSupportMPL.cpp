49937fc6b8aa1421d336baf4f46c5f63e4a4e651
drott <drott@chromium.org>
2016-05-27 15:00:25 -0700

    Simplify OpenType caps feature detection logic
    We can rely on HarfBuzz API for finding the required OpenType
    features, instead of implementing our own find loops.
    
    Thanks to Behdad for the suggestion.
    
    TEST=LayoutTest/fast/text/font-features/*
    BUG=603137
    R=behdad,eae
    
    Review-Url: https://codereview.chromium.org/2017533002
    Cr-Commit-Position: refs/heads/master@{#396579}
    
    Former-commit-id: c51ee10d5afd2a657ed357cda9caf31c788790c1

--- third_party/WebKit/Source/platform/fonts/opentype/OpenTypeCapsSupportMPL.cpp
+++ third_party/WebKit/Source/platform/fonts/opentype/OpenTypeCapsSupportMPL.cpp
@@ -29,52 +29,37 @@ bool OpenTypeCapsSupport::supportsOpenTypeFeature(
         || tag == HB_TAG('u', 'n', 'i', 'c')
         || tag == HB_TAG('v', 'e', 'r', 't')));
 
-    bool result = false;
-
     if (!hb_ot_layout_has_substitution(face))
         return false;
 
     // Get the OpenType tag(s) that match this script code
-    const size_t kMaxScriptTags = 4;
-    hb_tag_t scriptTags[kMaxScriptTags] = {
+    hb_tag_t scriptTags[] = {
         HB_TAG_NONE,
         HB_TAG_NONE,
         HB_TAG_NONE,
-        HB_TAG_NONE
     };
     hb_ot_tags_from_script(static_cast<hb_script_t>(script),
         &scriptTags[0],
         &scriptTags[1]);
 
-    // Replace the first remaining NONE with DEFAULT
-    for (size_t i = 0; i < kMaxScriptTags; ++i) {
-        if (scriptTags[i] == HB_TAG_NONE) {
-            scriptTags[i] = HB_OT_TAG_DEFAULT_SCRIPT;
-            break;
-        }
-    }
-
-    // Now check for 'smcp' under the first of those scripts that is present
     const hb_tag_t kGSUB = HB_TAG('G', 'S', 'U', 'B');
-    for (size_t j = 0; j < kMaxScriptTags; ++j) {
-        if (scriptTags[j] == HB_TAG_NONE)
-            break;
-
-        unsigned scriptIndex;
-        if (hb_ot_layout_table_find_script(face,
-            kGSUB,
-            scriptTags[j],
-            &scriptIndex)) {
-            if (hb_ot_layout_language_find_feature(face, kGSUB,
-                scriptIndex,
-                HB_OT_LAYOUT_DEFAULT_LANGUAGE_INDEX,
-                tag, nullptr)) {
-                result = true;
-            }
-            break;
-        }
+    unsigned scriptIndex = 0;
+    // Identify for which script a GSUB table is available.
+    if (!hb_ot_layout_table_choose_script(face,
+        kGSUB,
+        scriptTags,
+        &scriptIndex,
+        nullptr)) {
+        return false;
     }
-    return result;
+
+    if (hb_ot_layout_language_find_feature(face, kGSUB,
+        scriptIndex,
+        HB_OT_LAYOUT_DEFAULT_LANGUAGE_INDEX,
+        tag, nullptr)) {
+        return true;
+    }
+    return false;
 }
 
 } // namespace blink
