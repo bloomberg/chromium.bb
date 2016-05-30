bf23eff330c788eb099bc6b5b7a2bd555f08346b
kjellander@chromium.org <kjellander@chromium.org>
2016-05-30 15:03:56 +0000

    Make WebRTC patches apply to Chromium checkouts on Windows.
    The change in https://codereview.chromium.org/1982243002
    didn't take into account that the script runs on the actual
    bot OS, so Windows will have backslashes as dir separators.
    
    BUG=438952
    
    Review-Url: https://codereview.chromium.org/2021783003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/tools/depot_tools@300672 0039d316-1c4b-4281-b951-d872f2087c98

--- recipe_modules/bot_update/resources/bot_update.py
+++ recipe_modules/bot_update/resources/bot_update.py
@@ -1127,7 +1127,7 @@ def apply_rietveld_issue(issue, patchset, root, server, _rev_map, _revision,
       cmd.extend(['--blacklist', item])
 
   # TODO(kjellander): Remove this hack when http://crbug.com/611808 is fixed.
-  if root == 'src/third_party/webrtc':
+  if root == path.join('src', 'third_party', 'webrtc'):
     cmd.extend(['--extra_patchlevel=1'])
 
   # Only try once, since subsequent failures hide the real failure.
