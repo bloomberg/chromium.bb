7eb9bb32be142bbc5cbd48adc88b4fecada810d1
tsepez <tsepez@chromium.org>
2016-05-31 14:13:53 -0700

    Apply ScrLike truncation to base href attribute's URL
    Otherwise, the same techniques can be applied to it as with
    other kinds of injected URLs.
    
    Review-Url: https://codereview.chromium.org/1998743002
    Cr-Commit-Position: refs/heads/master@{#396931}
    
    Former-commit-id: b60f49a81f8dc8d9b9b88cb2707a25751acd9682

--- third_party/WebKit/Source/core/html/parser/XSSAuditor.cpp
+++ third_party/WebKit/Source/core/html/parser/XSSAuditor.cpp
@@ -571,7 +571,7 @@ bool XSSAuditor::filterBaseToken(const FilterTokenRequest& request)
     ASSERT(request.token.type() == HTMLToken::StartTag);
     ASSERT(hasName(request.token, baseTag));
 
-    return eraseAttributeIfInjected(request, hrefAttr);
+    return eraseAttributeIfInjected(request, hrefAttr, String(), SrcLikeAttributeTruncation);
 }
 
 bool XSSAuditor::filterFormToken(const FilterTokenRequest& request)
