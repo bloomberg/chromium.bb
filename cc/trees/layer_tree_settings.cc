c5517b5a1ec4dbc412ea065fa678022676454f83
vmpstr <vmpstr@chromium.org>
2016-05-31 16:59:28 -0700

    cc: Make LayerTreeSettings have in-member initialization.
    This patch changes LayerTreeSettings from using an in-ctor
    initialization to in-class initialization.
    
    R=danakj@chromium.org
    CQ_INCLUDE_TRYBOTS=tryserver.blink:linux_blink_rel
    
    Review-Url: https://codereview.chromium.org/2021433005
    Cr-Commit-Position: refs/heads/master@{#396983}
    
    Former-commit-id: 51db06d62dc6a6a25f16be3ab52053f80abece9c

--- cc/trees/layer_tree_settings.cc
+++ cc/trees/layer_tree_settings.cc
@@ -4,17 +4,9 @@
 
 #include "cc/trees/layer_tree_settings.h"
 
-#include <GLES2/gl2.h>
-#include <stddef.h>
-#include <limits>
-
-#include "base/command_line.h"
-#include "base/logging.h"
-#include "base/strings/string_number_conversions.h"
 #include "cc/proto/gfx_conversions.h"
-#include "cc/proto/gpu_conversions.h"
 #include "cc/proto/layer_tree_settings.pb.h"
-#include "ui/gfx/buffer_types.h"
+#include "third_party/khronos/GLES2/gl2.h"
 
 namespace cc {
 
@@ -55,69 +47,18 @@ LayerTreeSettingsScrollbarAnimatorFromProto(
 }  // namespace
 
 LayerTreeSettings::LayerTreeSettings()
-    : single_thread_proxy_scheduler(true),
-      use_external_begin_frame_source(false),
-      use_output_surface_begin_frame_source(false),
-      main_frame_before_activation_enabled(false),
-      using_synchronous_renderer_compositor(false),
-      can_use_lcd_text(true),
-      use_distance_field_text(false),
-      gpu_rasterization_enabled(false),
-      gpu_rasterization_forced(false),
-      async_worker_context_enabled(false),
-      gpu_rasterization_msaa_sample_count(0),
-      gpu_rasterization_skewport_target_time_in_seconds(0.2f),
-      create_low_res_tiling(false),
-      scrollbar_animator(NO_ANIMATOR),
-      scrollbar_fade_delay_ms(0),
-      scrollbar_fade_resize_delay_ms(0),
-      scrollbar_fade_duration_ms(0),
-      solid_color_scrollbar_color(SK_ColorWHITE),
-      timeout_and_draw_when_animation_checkerboards(true),
-      layer_transforms_should_scale_layer_contents(false),
-      layers_always_allowed_lcd_text(false),
-      minimum_contents_scale(0.0625f),
-      low_res_contents_scale_factor(0.25f),
-      top_controls_show_threshold(0.5f),
-      top_controls_hide_threshold(0.5f),
-      background_animation_rate(1.0),
-      default_tile_size(gfx::Size(256, 256)),
+    : default_tile_size(gfx::Size(256, 256)),
       max_untiled_layer_size(gfx::Size(512, 512)),
       minimum_occlusion_tracking_size(gfx::Size(160, 160)),
-      // 3000 pixels should give sufficient area for prepainting.
-      // Note this value is specified with an ideal contents scale in mind. That
-      // is, the ideal tiling would use this value as the padding.
-      // TODO(vmpstr): Figure out a better number that doesn't depend on scale.
-      tiling_interest_area_padding(3000),
-      skewport_target_time_in_seconds(1.0f),
-      skewport_extrapolation_limit_in_screen_pixels(2000),
-      max_memory_for_prepaint_percentage(100),
-      use_zero_copy(false),
-      use_partial_raster(false),
-      enable_elastic_overscroll(false),
       use_image_texture_targets(
           static_cast<size_t>(gfx::BufferFormat::LAST) + 1,
           GL_TEXTURE_2D),
-      ignore_root_layer_flings(false),
-      scheduled_raster_task_limit(32),
-      use_occlusion_for_tile_prioritization(false),
-      verify_clip_tree_calculations(false),
-      image_decode_tasks_enabled(false),
-      wait_for_beginframe_interval(true),
-      abort_commit_before_output_surface_creation(true),
-      use_mouse_wheel_gestures(false),
-      use_layer_lists(false),
-      max_staging_buffer_usage_in_bytes(32 * 1024 * 1024),
       memory_policy_(64 * 1024 * 1024,
                      gpu::MemoryAllocation::CUTOFF_ALLOW_EVERYTHING,
-                     ManagedMemoryPolicy::kDefaultNumResourcesLimit),
-      gpu_decoded_image_budget_bytes(96 * 1024 * 1024),
-      software_decoded_image_budget_bytes(128 * 1024 * 1024),
-      use_cached_picture_raster(true) {}
+                     ManagedMemoryPolicy::kDefaultNumResourcesLimit) {}
 
 LayerTreeSettings::LayerTreeSettings(const LayerTreeSettings& other) = default;
-
-LayerTreeSettings::~LayerTreeSettings() {}
+LayerTreeSettings::~LayerTreeSettings() = default;
 
 bool LayerTreeSettings::operator==(const LayerTreeSettings& other) const {
   return renderer_settings == other.renderer_settings &&
