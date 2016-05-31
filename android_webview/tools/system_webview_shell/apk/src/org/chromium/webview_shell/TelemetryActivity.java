53156d8e78b54522111bb20086d730aab31959ab
yolandyan <yolandyan@google.com>
2016-05-31 13:04:15 -0700

    Mark trace data when launching WebView
    BUG=613419
    
    Review-Url: https://codereview.chromium.org/1994403002
    Cr-Commit-Position: refs/heads/master@{#396905}
    
    Former-commit-id: a9fa54ee41fb8ef334883c808123268bea188003

--- android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/TelemetryActivity.java
+++ android_webview/tools/system_webview_shell/apk/src/org/chromium/webview_shell/TelemetryActivity.java
@@ -1,12 +1,12 @@
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-
 package org.chromium.webview_shell;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Trace;
 import android.webkit.CookieManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -16,15 +16,24 @@ import android.webkit.WebViewClient;
  * This activity is designed for Telemetry testing of WebView.
  */
 public class TelemetryActivity extends Activity {
+    static final String DEFAULT_START_UP_TRACE_TAG = "WebViewStartupInterval";
+    static final String DEFAULT_LOAD_URL_TRACE_TAG = "WebViewBlankUrlLoadInterval";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setTitle(
                 getResources().getString(R.string.title_activity_telemetry));
-        setContentView(R.layout.activity_webview);
 
-        WebView webView = (WebView) findViewById(R.id.webview);
+        Intent intent = getIntent();
+        final String startUpTraceTag = intent.getStringExtra("WebViewStartUpTraceTag");
+        final String loadUrlTraceTag = intent.getStringExtra("WebViewLoadUrlTraceTag");
+
+        Trace.beginSection(startUpTraceTag == null ? DEFAULT_START_UP_TRACE_TAG : startUpTraceTag);
+        WebView webView = new WebView(this);
+        Trace.endSection();
+        setContentView(webView);
+
         CookieManager.setAcceptFileSchemeCookies(true);
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
@@ -32,7 +41,6 @@ public class TelemetryActivity extends Activity {
         settings.setLoadWithOverviewMode(true);
         settings.setDomStorageEnabled(true);
         settings.setMediaPlaybackRequiresUserGesture(false);
-        Intent intent = getIntent();
         String userAgentString = intent.getStringExtra("userAgent");
         if (userAgentString != null) {
             settings.setUserAgentString(userAgentString);
@@ -40,11 +48,18 @@ public class TelemetryActivity extends Activity {
 
         webView.setWebViewClient(new WebViewClient() {
                 @Override
-                public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
                     return false;
+                }
+
+                @Override
+                public void onPageFinished(WebView view, String url) {
+                    super.onPageFinished(view, url);
+                    Trace.endSection();
                 }
         });
 
+        Trace.beginSection(loadUrlTraceTag == null ? DEFAULT_LOAD_URL_TRACE_TAG : loadUrlTraceTag);
         webView.loadUrl("about:blank");
     }
 }
