b215b5bb68dc1d960fa10e7d6423505169b16f05
piman <piman@chromium.org>
2016-05-31 21:10:45 -0700

    Allow premultiply_alpha + unpremultiply_alpha in CopyTextureCHROMIUMResourceManager
    It's allowed by the spec (and by the implementation), so we shouldn't DCHECK.
    
    BUG=None
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2030483002
    Cr-Commit-Position: refs/heads/master@{#397050}
    
    Former-commit-id: a32c1564165a6daad637eb29f2a8a97449f49547

--- gpu/command_buffer/service/gles2_cmd_copy_texture_chromium.cc
+++ gpu/command_buffer/service/gles2_cmd_copy_texture_chromium.cc
@@ -37,9 +37,6 @@ enum FragmentShaderId {
 FragmentShaderId GetFragmentShaderId(bool premultiply_alpha,
                                      bool unpremultiply_alpha,
                                      GLenum target) {
-  // Only one alpha mode at a time makes sense.
-  DCHECK(!premultiply_alpha || !unpremultiply_alpha);
-
   enum {
     SAMPLER_2D,
     SAMPLER_RECTANGLE_ARB,
@@ -136,9 +133,6 @@ std::string GetVertexShaderSource() {
 std::string GetFragmentShaderSource(bool premultiply_alpha,
                                     bool unpremultiply_alpha,
                                     GLenum target) {
-  // Only one alpha mode at a time makes sense.
-  DCHECK(!premultiply_alpha || !unpremultiply_alpha);
-
   std::string source;
 
   // Preamble for core and compatibility mode.
