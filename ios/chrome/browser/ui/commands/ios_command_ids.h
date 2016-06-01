712f3d4c9b10e2bd9f3ed679ef5173c7c1c982c4
bzanotti <bzanotti@chromium.org>
2016-06-01 06:21:31 -0700

    Introduce Chrome command to show passphrase screen.
    BUG=616432
    
    Review-Url: https://codereview.chromium.org/2032503002
    Cr-Commit-Position: refs/heads/master@{#397122}
    
    Former-commit-id: 907dd0af8255b33f5981161ed571eb5cd7386e66

--- ios/chrome/browser/ui/commands/ios_command_ids.h
+++ ios/chrome/browser/ui/commands/ios_command_ids.h
@@ -75,6 +75,7 @@
 #define IDC_ADD_READING_LIST                           40949
 #define IDC_SHOW_READING_LIST                          40950
 #define IDC_SHOW_CLEAR_BROWSING_DATA_SETTINGS          40951
+#define IDC_SHOW_SYNC_PASSPHRASE_SETTINGS              40952
 // clang-format on
 
 #endif  // IOS_CHROME_BROWSER_UI_COMMANDS_IOS_COMMAND_IDS_H_
