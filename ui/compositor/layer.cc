d6bd51a5d446f7e502aaf3da0929af450984b4c4
jbauman <jbauman@chromium.org>
2016-06-02 15:22:03 -0700

    Clear ui::Layer damaged_region_ after commit.
    This was only being cleared for ui::Layers with PictureLayers and not
    those with TextureLayers, so all frames afterwards would have
    unnecessary damage.
    
    BUG=610086
    
    Committed: https://crrev.com/bdb53e1ab961c8b35f9784bd727ea34dd934600b [formerly 5ab42edc1361fee3b5255dd36bc8663dfb44cb5e]
    Cr-Commit-Position: refs/heads/master@{#396936}
    
    Review-Url: https://codereview.chromium.org/2018223002
    Cr-Commit-Position: refs/heads/master@{#397529}
    
    Former-commit-id: ac619ac038346cc2ea96c469ab0bcd23bd13d51c

--- ui/compositor/layer.cc
+++ ui/compositor/layer.cc
@@ -689,9 +689,9 @@ void Layer::SendDamagedRects() {
 
   for (cc::Region::Iterator iter(damaged_region_); iter.has_rect(); iter.next())
     cc_layer_->SetNeedsDisplayRect(iter.rect());
-}
 
-void Layer::ClearDamagedRects() {
+  if (content_layer_)
+    paint_region_.Union(damaged_region_);
   damaged_region_.Clear();
 }
 
@@ -756,8 +756,8 @@ scoped_refptr<cc::DisplayItemList> Layer::PaintContentsToDisplayList(
   TRACE_EVENT1("ui", "Layer::PaintContentsToDisplayList", "name", name_);
   gfx::Rect local_bounds(bounds().size());
   gfx::Rect invalidation(
-      gfx::IntersectRects(damaged_region_.bounds(), local_bounds));
-  ClearDamagedRects();
+      gfx::IntersectRects(paint_region_.bounds(), local_bounds));
+  paint_region_.Clear();
   cc::DisplayItemListSettings settings;
   settings.use_cached_picture = false;
   scoped_refptr<cc::DisplayItemList> display_list =
