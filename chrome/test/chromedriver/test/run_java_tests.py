19fa0dbcaf6398b2a95b68baba642b27c78d57ea
samuong <samuong@chromium.org>
2016-06-02 14:54:43 -0700

    [chromedriver] Pass --no-sandbox to chrome for java tests on linux32 bot.
    BUG=611886
    TBR=stgao@chromium.org
    
    Review-Url: https://codereview.chromium.org/2036793003
    Cr-Commit-Position: refs/heads/master@{#397521}
    
    Former-commit-id: 775ae9da9a7371d726f634c9d0333b530b1b1b16

--- chrome/test/chromedriver/test/run_java_tests.py
+++ chrome/test/chromedriver/test/run_java_tests.py
@@ -14,6 +14,7 @@ For ChromeDriver documentation, refer to http://code.google.com/p/chromedriver.
 import optparse
 import os
 import shutil
+import stat
 import sys
 import xml.dom.minidom as minidom
 
@@ -97,10 +98,21 @@ def _Run(java_tests_src_dir, test_filter,
   shutil.copyfile(os.path.join(java_tests_src_dir, test_jar),
                   os.path.join(test_dir, test_jar))
 
+  if util.IsLinux() and not util.Is64Bit():
+    # Workaround for crbug.com/611886
+    chrome_wrapper_path = os.path.join(test_dir, 'chrome-wrapper-no-sandbox')
+    with open(chrome_wrapper_path, 'w') as f:
+      f.write('#!/bin/sh\n')
+      f.write('exec %s --no-sandbox "$@"\n' % os.path.abspath(chrome_path))
+    st = os.stat(chrome_wrapper_path)
+    os.chmod(chrome_wrapper_path, st.st_mode | stat.S_IEXEC)
+  else:
+    chrome_wrapper_path = os.path.abspath(chrome_path)
+
   sys_props = ['selenium.browser=chrome',
                'webdriver.chrome.driver=' + os.path.abspath(chromedriver_path)]
   if chrome_path:
-    sys_props += ['webdriver.chrome.binary=' + os.path.abspath(chrome_path)]
+    sys_props += ['webdriver.chrome.binary=' + chrome_wrapper_path]
   if log_path:
     sys_props += ['webdriver.chrome.logfile=' + log_path]
   if android_package_key:
