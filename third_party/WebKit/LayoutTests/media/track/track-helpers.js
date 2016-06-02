44fbaf4829eded55aa493590d33250d0d019691b
srirama.m <srirama.m@samsung.com>
2016-06-02 06:59:38 -0700

    Convert track-webvtt-tc[019-021] tests to testharness.js
    Cleaning up track-webvtt-tc[019-021] tests in media/track
    to use testharness.js instead of video-test.js.
    This will enable to upstream these tests to web-platform-tests.
    
    BUG=588956
    
    Review-Url: https://codereview.chromium.org/2029663002
    Cr-Commit-Position: refs/heads/master@{#397389}
    
    Former-commit-id: 97ae3bc8dd59a5e1a6243460b94ed12dbe66e4f7

--- third_party/WebKit/LayoutTests/media/track/track-helpers.js
+++ third_party/WebKit/LayoutTests/media/track/track-helpers.js
@@ -16,12 +16,26 @@ function assert_cues_equal(cues, expected) {
     }
 }
 
-function assert_cues_match(cues, expected, properties) {
+function assert_cues_match(cues, expected) {
     assert_equals(cues.length, expected.length);
     for (var i = 0; i < cues.length; i++) {
         var cue = cues[i];
         var expectedItem = expected[i];
-        for (var property of properties)
+        for (var property of Object.getOwnPropertyNames(expectedItem))
             assert_equals(cue[property], expectedItem[property]);
     }
+}
+
+function check_cues_from_track(src, func) {
+    async_test(function(t) {
+        var video = document.createElement("video");
+        var trackElement = document.createElement("track");
+        trackElement.src = src;
+        trackElement.default = true;
+        video.appendChild(trackElement);
+
+        trackElement.onload = t.step_func_done(function() {
+            func(trackElement.track);
+        });
+    }, "Check cues from " + src);
 }
