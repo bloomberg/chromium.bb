87c0c2b6a4c4a716d6730f918d55fad27deaf67e
rdevlin.cronin <rdevlin.cronin@chromium.org>
2016-06-02 08:30:57 -0700

    Add metrics for Google Docs Offline extension
    BUG=616572
    
    Review-Url: https://codereview.chromium.org/2027723004
    Cr-Commit-Position: refs/heads/master@{#397412}
    
    Former-commit-id: 67890d35a6daad41ec99b939dac3a8b009a4a005

--- chrome/browser/ui/toolbar/toolbar_actions_model.cc
+++ chrome/browser/ui/toolbar/toolbar_actions_model.cc
@@ -555,6 +555,35 @@ void ToolbarActionsModel::Populate() {
   UMA_HISTOGRAM_COUNTS_100("Toolbar.ActionsModel.OverallActionsCount",
                            toolbar_items_.size());
 
+  const char kDocsOfflineExtensionId[] = "ghbmnnjooekpmoecnnnilnnbdlolhkhi";
+  if (extension_registry_->GetExtensionById(
+          kDocsOfflineExtensionId,
+          extensions::ExtensionRegistry::ENABLED |
+              extensions::ExtensionRegistry::DISABLED) != nullptr) {
+    // Note: This enum is used in UMA (directly below). Don't renumber.
+    enum ExtensionState {
+      DISABLED   = 0,
+      VISIBLE    = 1,
+      OVERFLOWED = 2,
+      BOUNDARY   = 3,
+    };
+    ExtensionState doc_state = DISABLED;
+    if (extensions.GetByID(kDocsOfflineExtensionId)) {  // In the enabled set.
+      auto current_pos = std::find_if(
+          toolbar_items_.begin(), toolbar_items_.end(),
+          [&kDocsOfflineExtensionId](const ToolbarItem& item) {
+        return item.id == kDocsOfflineExtensionId;
+      });
+      doc_state =
+          current_pos - toolbar_items_.begin() <
+              static_cast<int>(visible_icon_count()) ||
+          all_icons_visible() ?
+              VISIBLE : OVERFLOWED;
+    }
+    UMA_HISTOGRAM_ENUMERATION("Extensions.DocsOfflineIconState",
+                              doc_state, BOUNDARY);
+  }
+
   if (!toolbar_items_.empty()) {
     // Visible count can be -1, meaning: 'show all'. Since UMA converts negative
     // values to 0, this would be counted as 'show none' unless we convert it to
