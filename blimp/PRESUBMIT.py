b123621776246c12599f5abbf5b1804cf8c49260
bgoldman <bgoldman@chromium.org>
2016-06-03 12:17:10 -0700

    Blimp presubmit to check for unit tests on new .cc files.
    BUG=612498,543268
    TEST=Added the files blimp/science.cc, blimp/science_unittest.cc, blimp/science2.cc, blimp/science2_unittest.cc, blimp/science_browsertest.cc, and science.cc. Verified that blimp/science.cc was flagged for missing unit tests.
    
    Review-Url: https://codereview.chromium.org/2005023002
    Cr-Commit-Position: refs/heads/master@{#397773}
    
    Former-commit-id: 18b2942b8f1890e8307cde644b448830a7a89411

--- blimp/PRESUBMIT.py
+++ blimp/PRESUBMIT.py
@@ -19,7 +19,33 @@ def CheckChangeLintsClean(input_api, output_api):
   return input_api.canned_checks.CheckChangeLintsClean(
       input_api, output_api, source_filter, lint_filters=[], verbose_level=1)
 
+def CheckNewFilesHaveTests(input_api, output_api):
+  unittest_files = set()
+  files_needing_unittest = set()
+
+  for source_file in input_api.AffectedFiles():
+    if source_file.Action() == 'A':
+      name = source_file.LocalPath()
+      if name.endswith('_unittest.cc'):
+        unittest_files.add(name)
+      elif name.endswith('.cc') and not name.endswith('test.cc'):
+        files_needing_unittest.add(name)
+
+  missing_unittest_files = []
+
+  for name in files_needing_unittest:
+    unittest_name = re.sub(r'\.cc$', '_unittest.cc', name)
+    if unittest_name not in unittest_files:
+      missing_unittest_files.append(name)
+
+  if missing_unittest_files:
+    message = 'The following new files are missing unit tests:'
+    return [output_api.PresubmitPromptWarning(message, missing_unittest_files)]
+  else:
+    return []
+
 def CheckChangeOnUpload(input_api, output_api):
   results = []
   results += CheckChangeLintsClean(input_api, output_api)
+  results += CheckNewFilesHaveTests(input_api, output_api)
   return results
