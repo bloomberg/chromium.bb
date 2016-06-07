58270d6c42c2e5881137f44f12727cf446940476
jochen <jochen@chromium.org>
2016-06-07 02:46:15 -0700

    Sandbox detached iframes a bit more
    Disallow cross-origin wrapper creation from them
    
    BUG=616225
    R=haraken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2042743002
    Cr-Commit-Position: refs/heads/master@{#398260}
    
    Former-commit-id: c0f67c5511dacb91d9484a737e162c02d9f53269

--- third_party/WebKit/Source/bindings/core/v8/V8DOMWrapper.cpp
+++ third_party/WebKit/Source/bindings/core/v8/V8DOMWrapper.cpp
@@ -37,6 +37,8 @@
 #include "bindings/core/v8/V8PerIsolateData.h"
 #include "bindings/core/v8/V8ScriptRunner.h"
 #include "bindings/core/v8/V8Window.h"
+#include "core/dom/Document.h"
+#include "core/frame/LocalDOMWindow.h"
 
 namespace blink {
 
@@ -107,8 +109,20 @@ void V8WrapperInstantiationScope::securityCheck(v8::Isolate* isolate, v8::Local<
     // If the context is different, we need to make sure that the current
     // context has access to the creation context.
     Frame* frame = toFrameIfNotDetached(contextForWrapper);
-    if (!frame)
+    if (!frame) {
+        // Sandbox detached frames - they can't create cross origin objects.
+        LocalDOMWindow* callingWindow = currentDOMWindow(isolate);
+        DOMWindow* targetWindow = toDOMWindow(contextForWrapper);
+        if (callingWindow->document()->getSecurityOrigin()->canAccessCheckSuborigins(targetWindow->document()->getSecurityOrigin()))
+            return;
+
+        // TODO(jochen): Currently, Location is the only object for which we can reach this code path. Should be generalized.
+        ExceptionState exceptionState(ExceptionState::ConstructionContext, "Location", contextForWrapper->Global(), isolate);
+        // We can't create a better message for a detached frame.
+        exceptionState.throwSecurityError(String(), String());
+        exceptionState.throwIfNeeded();
         return;
+    }
     const DOMWrapperWorld& currentWorld = DOMWrapperWorld::world(m_context);
     RELEASE_ASSERT(currentWorld.worldId() == DOMWrapperWorld::world(contextForWrapper).worldId());
     if (currentWorld.isMainWorld()) {
