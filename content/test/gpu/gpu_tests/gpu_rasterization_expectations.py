b6336e4a3d44d9cb8955f914b21edf50f3525118
kbr <kbr@chromium.org>
2016-06-07 11:08:45 -0700

    Suppress GpuRasterization.ConcavePaths on Nexus 9.
    The test's failing because of a legitimate bug. This suppression should
    be removed after the bug's fixed.
    
    BUG=617805
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    TBR=vmiura@chromium.org
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2046523006
    Cr-Commit-Position: refs/heads/master@{#398339}
    
    Former-commit-id: 737f0b91ba323760e33dc2b5b7d8a7be765cb979

--- content/test/gpu/gpu_tests/gpu_rasterization_expectations.py
+++ content/test/gpu/gpu_tests/gpu_rasterization_expectations.py
@@ -15,3 +15,7 @@ class GpuRasterizationExpectations(GpuTestExpectations):
     # Failing on Nexus 5 and 6
     self.Fail('GpuRasterization.ConcavePaths',
               ['android', 'qualcomm'], bug=499555)
+
+    # Failing on Nexus 9
+    self.Fail('GpuRasterization.ConcavePaths',
+              ['android', 'nvidia'], bug=617805)
