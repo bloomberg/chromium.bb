e53b789d7f88b996b970ba36b739e79aa1abab79
mythria <mythria@chromium.org>
2016-06-08 06:03:05 -0700

    Adds benchmarks to measure code size reductions when using ignition.
    Adds two benchmarks to measure the code size reductions in ignition.
    The benchmarks run the same set of pages, once with ignition flag enabled
    and once without ignition flag. This allows us to measure the code
    size measurements in ignition.
    
    BUG=v8:5019
    LOG=N
    CQ_EXTRA_TRYBOTS=tryserver.chromium.perf:android_s5_perf_cq;tryserver.chromium.perf:mac_retina_perf_cq
    
    Review-Url: https://codereview.chromium.org/1982393002
    Cr-Commit-Position: refs/heads/master@{#398533}
    
    Former-commit-id: 75fcb546047b211d6bd71df5b82d30a5f09130bf

--- tools/perf/benchmarks/v8_helper.py
+++ tools/perf/benchmarks/v8_helper.py
@@ -2,16 +2,26 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import shlex
+_JS_FLAGS_SWITCH = '--js-flags='
 
 
 def EnableIgnition(options):
-  existing_js_flags = []
+  AppendJSFlags(options, '--ignition')
+
+
+def AppendJSFlags(options, js_flags):
+  existing_js_flags = ''
+  # There should be only one occurence of --js-flags in the browser flags. When
+  # there are multiple flags, only one of them would be used. Append any
+  # additional js_flags to the existing flags (if present).
   for extra_arg in options.extra_browser_args:
-    if extra_arg.startswith('--js-flags='):
-      existing_js_flags.extend(shlex.split(extra_arg[len('--js-flags='):]))
+    if extra_arg.startswith(_JS_FLAGS_SWITCH):
+      # Find and remove the set of existing js_flags.
+      existing_js_flags = extra_arg[len(_JS_FLAGS_SWITCH):]
+      options.extra_browser_args.remove(extra_arg)
+      break
+
   options.AppendExtraBrowserArgs([
-      # This overrides any existing --js-flags, hence we have to include the
-      # previous flags as well.
-      '--js-flags=--ignition %s' % (' '.join(existing_js_flags))
+      # Add a new --js-flags which includes previous flags.
+      '%s%s %s' %  (_JS_FLAGS_SWITCH, js_flags, existing_js_flags)
   ])
--- tools/perf/page_sets/top_10_mobile.py
+++ tools/perf/page_sets/top_10_mobile.py
@@ -41,28 +41,51 @@ URL_LIST = [
 
 class Top10MobilePage(page_module.Page):
 
-  def __init__(self, url, page_set, run_no_page_interactions):
+  def __init__(self, url, page_set, run_no_page_interactions,
+               collect_memory_dumps):
     super(Top10MobilePage, self).__init__(
         url=url, page_set=page_set, credentials_path = 'data/credentials.json',
         shared_page_state_class=shared_page_state.SharedMobilePageState)
     self.archive_data_file = 'data/top_10_mobile.json'
     self._run_no_page_interactions = run_no_page_interactions
+    self._collect_memory_dumps = collect_memory_dumps
 
   def RunPageInteractions(self, action_runner):
+    if self._collect_memory_dumps:
+      action_runner.tab.browser.DumpMemory()
     if self._run_no_page_interactions:
       return
     with action_runner.CreateGestureInteraction('ScrollAction'):
       action_runner.ScrollPage()
+    if self._collect_memory_dumps:
+      action_runner.tab.browser.DumpMemory()
 
 
-class Top10MobilePageSet(story.StorySet):
+class _Top10MobilePageSet(story.StorySet):
+  """ Base class for Top 10 mobile sites """
 
-  """ Top 10 mobile sites """
-
-  def __init__(self, run_no_page_interactions=False):
-    super(Top10MobilePageSet, self).__init__(
+  def __init__(self, run_no_page_interactions=False,
+               collect_memory_dumps=False):
+    super(_Top10MobilePageSet, self).__init__(
       archive_data_file='data/top_10_mobile.json',
       cloud_storage_bucket=story.PARTNER_BUCKET)
 
     for url in URL_LIST:
-      self.AddStory(Top10MobilePage(url, self, run_no_page_interactions))
+      self.AddStory(Top10MobilePage(url, self, run_no_page_interactions,
+        collect_memory_dumps))
+
+
+class Top10MobilePageSet(_Top10MobilePageSet):
+  """ Top 10 mobile sites """
+
+  def __init__(self, run_no_page_interactions=False):
+    super(Top10MobilePageSet, self).__init__(run_no_page_interactions)
+
+
+class Top10MobileMemoryPageSet(_Top10MobilePageSet):
+  """ Top 10 mobile sites for measuring memory"""
+
+  def __init__(self):
+    super(Top10MobileMemoryPageSet, self).__init__(
+        run_no_page_interactions=False, collect_memory_dumps=True)
+
