b036afe4675ac23885e313ee247a04396cdcb677
wangxianzhu <wangxianzhu@chromium.org>
2016-06-09 13:50:17 -0700

    Convert fast/repaint/window-resize*.html into reftests
    to make rect under-invalidations more visible, and show
    painting/rasterization errors if any.
    
    BUG=600008
    
    Review-Url: https://codereview.chromium.org/2039943003
    Cr-Commit-Position: refs/heads/master@{#399007}
    
    Former-commit-id: c612643f5283dc92d261ca2b359e918b8b9343e4

--- third_party/WebKit/LayoutTests/fast/repaint/resources/window-resize-repaint.js
+++ third_party/WebKit/LayoutTests/fast/repaint/resources/window-resize-repaint.js
@@ -1,39 +1,36 @@
 var testSizes = [
-    { width: 200, height: 200 }, // initial size
-    { width: 200, height: 300 }, // height increase
-    { width: 300, height: 300 }, // width increase
-    { width: 300, height: 250 }, // height decrease
-    { width: 250, height: 250 }  // width decrease
-    // Tests can add more testSizes.
+    { width: 600, height: 500 }, // initial size
+    { width: 600, height: 250 }, // height decrease
+    { width: 400, height: 250 }, // width decrease
+    { width: 400, height: 600 }, // height increase
+    { width: 800, height: 600 }  // width increase
 ];
 
 var sizeIndex = 0;
+var repaintRects = "";
 
-function repaintTest() {
-    window.resizeTo(testSizes[sizeIndex].width, testSizes[sizeIndex].height);
-}
+if (window.internals)
+    internals.settings.setUseDefaultImageInterpolationQuality(true);
 
-if (window.internals) {
-    internals.settings.setOverlayScrollbarsEnabled(true);
-    internals.settings.setMockScrollbarsEnabled(true);
+function doTest() {
+    if (sizeIndex) {
+        repaintRects += internals.layerTreeAsText(document, window.internals.LAYER_TREE_INCLUDES_PAINT_INVALIDATIONS);
+        internals.stopTrackingRepaints(document);
+    }
+    ++sizeIndex;
+    if (sizeIndex < testSizes.length) {
+        internals.startTrackingRepaints(document);
+        window.resizeTo(testSizes[sizeIndex].width, testSizes[sizeIndex].height);
+        testRunner.layoutAndPaintAsyncThen(doTest);
+    } else if (window.testRunner) {
+        testRunner.setCustomTextOutput(repaintRects);
+        testRunner.notifyDone();
+    }
 }
 
 if (window.testRunner) {
     testRunner.useUnfortunateSynchronousResizeMode();
-    testRunner.dumpAsText();
-
-    window.onload = function() {
-        window.resizeTo(testSizes[0].width, testSizes[0].height);
-
-        var repaintRects = "";
-        for (sizeIndex = 1; sizeIndex < testSizes.length; ++sizeIndex) {
-            document.body.offsetTop;
-            internals.startTrackingRepaints(document);
-            repaintTest();
-            document.body.offsetTop;
-            repaintRects += internals.layerTreeAsText(document, window.internals.LAYER_TREE_INCLUDES_PAINT_INVALIDATIONS);
-            internals.stopTrackingRepaints(document);
-        }
-        testRunner.setCustomTextOutput(repaintRects);
-    }
+    testRunner.waitUntilDone();
+    window.resizeTo(testSizes[0].width, testSizes[0].height);
+    testRunner.layoutAndPaintAsyncThen(doTest);
 }
