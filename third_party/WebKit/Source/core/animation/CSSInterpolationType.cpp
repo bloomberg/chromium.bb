0daf58925c803f03e6f6c393e5c75bdd872f23a4
sashab <sashab@chromium.org>
2016-06-09 00:22:35 -0700

    Make ResolvedVariableChecker store const CSSValues
    Make ResolvedVariableChecker store const CSSValues instead of regular
    CSSValues. This is an entirely mechanical change that is pre-work for
    making CSSPropertyParser::parseSingleValue return a const CSSValue*.
    
    R=timloh@chromium.org
    BUG=526586
    
    Review-Url: https://codereview.chromium.org/2040813002
    Cr-Commit-Position: refs/heads/master@{#398804}
    
    Former-commit-id: d117a6d7fc8d83cf449c237fa84ae89285de36f3

--- third_party/WebKit/Source/core/animation/CSSInterpolationType.cpp
+++ third_party/WebKit/Source/core/animation/CSSInterpolationType.cpp
@@ -15,13 +15,13 @@ namespace blink {
 
 class ResolvedVariableChecker : public InterpolationType::ConversionChecker {
 public:
-    static PassOwnPtr<ResolvedVariableChecker> create(CSSPropertyID property, CSSVariableReferenceValue* variableReference, CSSValue* resolvedValue)
+    static PassOwnPtr<ResolvedVariableChecker> create(CSSPropertyID property, const CSSVariableReferenceValue* variableReference, const CSSValue* resolvedValue)
     {
         return adoptPtr(new ResolvedVariableChecker(property, variableReference, resolvedValue));
     }
 
 private:
-    ResolvedVariableChecker(CSSPropertyID property, CSSVariableReferenceValue* variableReference, CSSValue* resolvedValue)
+    ResolvedVariableChecker(CSSPropertyID property, const CSSVariableReferenceValue* variableReference, const CSSValue* resolvedValue)
         : m_property(property)
         , m_variableReference(variableReference)
         , m_resolvedValue(resolvedValue)
@@ -35,8 +35,8 @@ private:
     }
 
     CSSPropertyID m_property;
-    Persistent<CSSVariableReferenceValue> m_variableReference;
-    Persistent<CSSValue> m_resolvedValue;
+    Persistent<const CSSVariableReferenceValue> m_variableReference;
+    Persistent<const CSSValue> m_resolvedValue;
 };
 
 InterpolationValue CSSInterpolationType::maybeConvertSingle(const PropertySpecificKeyframe& keyframe, const InterpolationEnvironment& environment, const InterpolationValue& underlying, ConversionCheckers& conversionCheckers) const
@@ -49,7 +49,7 @@ InterpolationValue CSSInterpolationType::maybeConvertSingle(const PropertySpecif
 
     if (value->isVariableReferenceValue() && !isShorthandProperty(cssProperty())) {
         resolvedCSSValueOwner = CSSVariableResolver::resolveVariableReferences(environment.state().style()->variables(), cssProperty(), toCSSVariableReferenceValue(*value));
-        conversionCheckers.append(ResolvedVariableChecker::create(cssProperty(), toCSSVariableReferenceValue(const_cast<CSSValue*>(value)), resolvedCSSValueOwner));
+        conversionCheckers.append(ResolvedVariableChecker::create(cssProperty(), toCSSVariableReferenceValue(value), resolvedCSSValueOwner));
         value = resolvedCSSValueOwner;
     }
 
