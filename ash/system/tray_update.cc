a6d3295d66c654592fc1a4444ef2a19525c7ced0
jamescook <jamescook@chromium.org>
2016-06-08 17:05:13 -0700

    ash: Remove broken OS update system tray "nagger" popup
    See bug for details. We used to show a "nag" popup if you had an update
    pending and your shelf had been hidden for > 24 hours. The feature has been
    broken for a while and after discussing with abodenha@ we've decided to
    remove it.
    
    This is a manual revert of http://crrev.com/134812
    
    BUG=122641
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2051643002
    Cr-Commit-Position: refs/heads/master@{#398740}
    
    Former-commit-id: 5314658a6e5aad26636d9d2bd20890b241ead672

--- ash/system/tray_update.cc
+++ ash/system/tray_update.cc
@@ -4,25 +4,16 @@
 
 #include "ash/system/tray_update.h"
 
-#include "ash/root_window_controller.h"
-#include "ash/shelf/shelf_layout_manager.h"
-#include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
-#include "ash/system/status_area_widget.h"
 #include "ash/system/tray/fixed_sized_image_view.h"
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/system_tray_delegate.h"
 #include "ash/system/tray/system_tray_notifier.h"
 #include "ash/system/tray/tray_constants.h"
-#include "base/time/time.h"
-#include "base/timer/timer.h"
 #include "grit/ash_resources.h"
 #include "grit/ash_strings.h"
 #include "ui/aura/window.h"
 #include "ui/base/resource/resource_bundle.h"
-#include "ui/compositor/layer.h"
-#include "ui/compositor/layer_animation_observer.h"
-#include "ui/compositor/layer_animation_sequence.h"
 #include "ui/gfx/image/image.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
@@ -30,12 +21,6 @@
 #include "ui/views/widget/widget.h"
 
 namespace {
-
-// How many seconds should we wait before showing the nag reminder?
-const int kUpdateNaggingTimeSeconds = 24 * 60 * 60;
-
-// How long should the nag reminder be displayed?
-const int kShowUpdateNaggerForSeconds = 15;
 
 int DecideResource(ash::UpdateInfo::UpdateSeverity severity, bool dark) {
   switch (severity) {
@@ -101,62 +86,6 @@ class UpdateView : public ash::ActionableView {
 }
 
 namespace ash {
-namespace tray {
-
-class UpdateNagger : public ui::LayerAnimationObserver {
- public:
-  explicit UpdateNagger(SystemTrayItem* owner)
-      : owner_(owner) {
-    RestartTimer();
-    owner_->system_tray()->GetWidget()->GetNativeView()->layer()->
-        GetAnimator()->AddObserver(this);
-  }
-
-  ~UpdateNagger() override {
-    StatusAreaWidget* status_area = Shell::GetPrimaryRootWindowController()
-                                        ->shelf_widget()
-                                        ->status_area_widget();
-    if (status_area) {
-      status_area->system_tray()->GetWidget()->GetNativeView()->layer()->
-          GetAnimator()->RemoveObserver(this);
-    }
-  }
-
-  void RestartTimer() {
-    timer_.Stop();
-    timer_.Start(FROM_HERE,
-                 base::TimeDelta::FromSeconds(kUpdateNaggingTimeSeconds),
-                 this,
-                 &UpdateNagger::Nag);
-  }
-
- private:
-  void Nag() {
-    owner_->PopupDetailedView(kShowUpdateNaggerForSeconds, false);
-  }
-
-  // Overridden from ui::LayerAnimationObserver.
-  void OnLayerAnimationEnded(ui::LayerAnimationSequence* sequence) override {
-    // TODO(oshima): Find out if the updator will be shown on non
-    // primary display.
-    if (Shell::GetPrimaryRootWindowController()->shelf_widget()->IsVisible())
-      timer_.Stop();
-    else if (!timer_.IsRunning())
-      RestartTimer();
-  }
-
-  void OnLayerAnimationAborted(ui::LayerAnimationSequence* sequence) override {}
-
-  void OnLayerAnimationScheduled(
-      ui::LayerAnimationSequence* sequence) override {}
-
-  SystemTrayItem* owner_;
-  base::OneShotTimer timer_;
-
-  DISALLOW_COPY_AND_ASSIGN(UpdateNagger);
-};
-
-}  // namespace tray
 
 TrayUpdate::TrayUpdate(SystemTray* system_tray)
     : TrayImageItem(system_tray, IDR_AURA_UBER_TRAY_UPDATE) {
@@ -179,28 +108,9 @@ views::View* TrayUpdate::CreateDefaultView(LoginStatus status) {
   return info.update_required ? new UpdateView(info) : nullptr;
 }
 
-views::View* TrayUpdate::CreateDetailedView(LoginStatus status) {
-  return CreateDefaultView(status);
-}
-
-void TrayUpdate::DestroyDetailedView() {
-  if (nagger_) {
-    // The nagger was being displayed. Now that the detailed view is being
-    // closed, that means either the user clicks on it to restart, or the user
-    // didn't click on it to restart. In either case, start the timer to show
-    // the nag reminder again after the specified time.
-    nagger_->RestartTimer();
-  }
-}
-
 void TrayUpdate::OnUpdateRecommended(const UpdateInfo& info) {
   SetImageFromResourceId(DecideResource(info.severity, false));
   tray_view()->SetVisible(true);
-  if (!Shell::GetPrimaryRootWindowController()->shelf_widget()->IsVisible() &&
-      !nagger_.get()) {
-    // The shelf is not visible, and there is no nagger scheduled.
-    nagger_.reset(new tray::UpdateNagger(this));
-  }
 }
 
 }  // namespace ash
--- ash/system/tray_update.h
+++ ash/system/tray_update.h
@@ -5,9 +5,6 @@
 #ifndef ASH_SYSTEM_TRAY_UPDATE_H_
 #define ASH_SYSTEM_TRAY_UPDATE_H_
 
-#include <memory>
-
-#include "ash/system/tray/system_tray_delegate.h"
 #include "ash/system/tray/tray_image_item.h"
 #include "ash/system/user/update_observer.h"
 #include "base/macros.h"
@@ -17,10 +14,6 @@ class View;
 }
 
 namespace ash {
-
-namespace tray {
-class UpdateNagger;
-}
 
 class TrayUpdate : public TrayImageItem,
                    public UpdateObserver {
@@ -32,15 +25,9 @@ class TrayUpdate : public TrayImageItem,
   // Overridden from TrayImageItem.
   bool GetInitialVisibility() override;
   views::View* CreateDefaultView(LoginStatus status) override;
-  views::View* CreateDetailedView(LoginStatus status) override;
-  void DestroyDetailedView() override;
 
   // Overridden from UpdateObserver.
   void OnUpdateRecommended(const UpdateInfo& info) override;
-
-  // Used to nag the user in case the tray has been hidden too long with an
-  // unseen update notification.
-  std::unique_ptr<tray::UpdateNagger> nagger_;
 
   DISALLOW_COPY_AND_ASSIGN(TrayUpdate);
 };
