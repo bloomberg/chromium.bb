9463b5d135179078eba08ca891cd48d7dd822aa5
fdoray <fdoray@chromium.org>
2016-06-09 07:27:16 -0700

    Remove MessageLoop::current()->task_runner() in device.
    This CL replaces MessageLoop::current()->task_runner() with
    ThreadTaskRunnerHandle::Get() in device.
    
    In files where this replacement is made, it adds these include:
      #include "base/threading/thread_task_runner_handle.h"
      #include "base/location.h" (if required)
      #include "base/single_thread_task_runner.h" (if required)
    And removes this include if it is no longer required:
      #include "base/message_loop/message_loop.h"
    
    Why?
    The fact that there's a MessageLoop on the thread is an
    unnecessary implementation detail. When browser threads
    are migrated to base/task_scheduler, tasks will no longer
    have access to a MessageLoop but they will have access to
    a ThreadTaskRunnerHandle.
    
    Steps to generate this patch:
    1. Run message_loop_cleanup_2.py (see code on the bug).
    2. Run tools/sort-headers.py on modified files.
    3. Run git cl format.
    
    BUG=616447
    R=reillyg@chromium.org
    
    Review-Url: https://codereview.chromium.org/2046843006
    Cr-Commit-Position: refs/heads/master@{#398871}
    
    Former-commit-id: 015ccc150ccbd15d52306d38a2521f1b4f86ee68

--- device/battery/battery_status_service.cc
+++ device/battery/battery_status_service.cc
@@ -7,15 +7,16 @@
 #include <utility>
 
 #include "base/bind.h"
-#include "base/message_loop/message_loop.h"
+#include "base/location.h"
 #include "base/single_thread_task_runner.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "device/battery/battery_monitor_impl.h"
 #include "device/battery/battery_status_manager.h"
 
 namespace device {
 
 BatteryStatusService::BatteryStatusService()
-    : main_thread_task_runner_(base::MessageLoop::current()->task_runner()),
+    : main_thread_task_runner_(base::ThreadTaskRunnerHandle::Get()),
       update_callback_(base::Bind(&BatteryStatusService::NotifyConsumers,
                                   base::Unretained(this))),
       status_updated_(false),
