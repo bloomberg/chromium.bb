a0fab119655c0d9e15fda275fd73100881551a82
simonhatch <simonhatch@chromium.org>
2016-06-09 09:43:27 -0700

    Disable latimes.com from power.tough_ad_cases.
    Failing on android.
    
    TBR=sullivan@chromium.org
    BUG=618708
    
    Review-Url: https://codereview.chromium.org/2057623002
    Cr-Commit-Position: refs/heads/master@{#398915}
    
    Former-commit-id: adc16e92f89daf2cbb407dc01f6ea3e33826a8b0

--- tools/perf/page_sets/tough_ad_cases.py
+++ tools/perf/page_sets/tough_ad_cases.py
@@ -179,8 +179,9 @@ class ToughAdCasesPageSet(story.StorySet):
     self.AddStory(AdPage('file://tough_ad_cases/'
         'swiffy_webgl_collection.html',
         self, make_javascript_deterministic=False, scroll=scroll))
-    self.AddStory(AdPage('http://www.latimes.com', self, scroll=scroll,
-        wait_for_interactive_or_better=True))
+    # crbug.com/618708
+    # self.AddStory(AdPage('http://www.latimes.com', self, scroll=scroll,
+    #     wait_for_interactive_or_better=True))
     self.AddStory(ForbesAdPage('http://www.forbes.com/sites/parmyolson/'
         '2015/07/29/jana-mobile-data-facebook-internet-org/',
         self, scroll=scroll))
