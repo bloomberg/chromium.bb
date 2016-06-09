48de0181118961b42de25dd4eb4660a86de4c6ea
ki.stfu <ki.stfu@gmail.com>
2016-06-09 14:04:56 -0700

    Cleanup: Initialize Value from tools/gn by moving std::string
    Previously it used string::swap, but std::move is more effective in this case.
    
    BUG=367418
    TEST=
    R=brettw@chromium.org
    
    Review-Url: https://codereview.chromium.org/1350043004
    Cr-Commit-Position: refs/heads/master@{#399012}
    
    Former-commit-id: 0b310e43469a5ef54d76661ec36e4a7e1ca09d3f

--- tools/gn/value.cc
+++ tools/gn/value.cc
@@ -41,12 +41,10 @@ Value::Value(const ParseNode* origin, int64_t int_val)
 
 Value::Value(const ParseNode* origin, std::string str_val)
     : type_(STRING),
-      string_value_(),
+      string_value_(std::move(str_val)),
       boolean_value_(false),
       int_value_(0),
-      origin_(origin) {
-  string_value_.swap(str_val);
-}
+      origin_(origin) {}
 
 Value::Value(const ParseNode* origin, const char* str_val)
     : type_(STRING),
