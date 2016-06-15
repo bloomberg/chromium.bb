e35abc11bb6a92e6bcc2dfe6c4ec27a17f248fea
peconn <peconn@chromium.org>
2016-06-15 09:22:27 -0700

    Save RadioButtonWithDescription state explicitly.
    BUG=601005, 605472
    
    Review-Url: https://codereview.chromium.org/2068693002
    Cr-Commit-Position: refs/heads/master@{#399918}
    
    Former-commit-id: 7adb0f63c605f7c6c3cdb9dd78fcc022a5ad0116

--- chrome/android/java/src/org/chromium/chrome/browser/widget/RadioButtonWithDescription.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/RadioButtonWithDescription.java
@@ -6,7 +6,10 @@ package org.chromium.chrome.browser.widget;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.AttributeSet;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -27,6 +30,9 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
     private TextView mDescription;
 
     private List<RadioButtonWithDescription> mGroup;
+
+    private static final String SUPER_STATE_KEY = "superState";
+    private static final String CHECKED_KEY = "isChecked";
 
     /**
      * Constructor for inflating via XML.
@@ -102,6 +108,60 @@ public class RadioButtonWithDescription extends RelativeLayout implements OnClic
      */
     public void setRadioButtonGroup(List<RadioButtonWithDescription> group) {
         mGroup = group;
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        // Since this View is designed to be used multiple times in the same layout and contains
+        // children with ids, Android gets confused. This is because it will see two Views in the
+        // hierarchy with the same id (eg, the RadioButton that makes up this View).
+        //
+        // eg:
+        // LinearLayout (no id):
+        // |-> RadioButtonWithDescription (id=sync_confirm_import_choice)
+        // |   |-> RadioButton            (id=radio_button)
+        // |   |-> TextView               (id=title)
+        // |   \-> TextView               (id=description)
+        // \-> RadioButtonWithDescription (id=sync_keep_separate_choice)
+        //     |-> RadioButton            (id=radio_button)
+        //     |-> TextView               (id=title)
+        //     \-> TextView               (id=description)
+        //
+        // This causes the automagic state saving and recovery to do the wrong thing and restore all
+        // of these Views to the state of the last one it saved.
+        // Therefore we manually save the state of the child Views here so the state can be
+        // associated with the id of the RadioButtonWithDescription, which should be unique and
+        // not the id of the RadioButtons, which will be duplicated.
+        //
+        // Note: We disable Activity recreation on many config changes (such as orientation),
+        // but not for all of them (eg, locale or font scale change). So this code will only be
+        // called on the latter ones, or when the Activity is destroyed due to memory constraints.
+        Bundle saveState = new Bundle();
+        saveState.putParcelable(SUPER_STATE_KEY, super.onSaveInstanceState());
+        saveState.putBoolean(CHECKED_KEY, isChecked());
+        return saveState;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        if (state instanceof Bundle) {
+            super.onRestoreInstanceState(((Bundle) state).getParcelable(SUPER_STATE_KEY));
+            setChecked(((Bundle) state).getBoolean(CHECKED_KEY));
+        } else {
+            super.onRestoreInstanceState(state);
+        }
+    }
+
+    @Override
+    protected void dispatchSaveInstanceState(SparseArray<Parcelable> container) {
+        // This method and dispatchRestoreInstanceState prevent the Android automagic state save
+        // and restore from touching this View's children.
+        dispatchFreezeSelfOnly(container);
+    }
+
+    @Override
+    protected void dispatchRestoreInstanceState(SparseArray<Parcelable> container) {
+        dispatchThawSelfOnly(container);
     }
 }
 
