2fe0353b7a4d77aab80c94cc518a6a0223a4b5fd
shrike <shrike@chromium.org>
2016-06-16 08:40:16 -0700

    [Mac][Material Design] Fix problem with tab close button redraw.
    With a theme installed the close button's "x" can have a different
    color for the tab active and inactive states. This cl forces the close
    button to redraw on tab state change so that it displays the "x" in the
    correct color.
    
    R=tapted@chromium.org
    BUG=616008
    
    Review-Url: https://codereview.chromium.org/2073523002
    Cr-Commit-Position: refs/heads/master@{#400162}
    
    Former-commit-id: 29bd2eed90a5c548ce737aa09225952bfab22461

--- chrome/browser/ui/cocoa/tabs/tab_view.mm
+++ chrome/browser/ui/cocoa/tabs/tab_view.mm
@@ -603,6 +603,7 @@ CGFloat LineWidthFromContext(CGContextRef context) {
     return;
   state_ = state;
   [self setNeedsDisplay:YES];
+  [closeButton_ setNeedsDisplay:YES];
 }
 
 - (void)setClosing:(BOOL)closing {
