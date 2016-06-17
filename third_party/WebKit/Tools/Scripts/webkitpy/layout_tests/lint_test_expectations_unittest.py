bf4607b374412fdf168d72708b13dd04bd37f9f1
Xianzhu Wang <wangxianzhu@chromium.org>
2016-06-16 21:15:05 -0700

    Enable NeedsRebaseline
    BUG=574272
    TBR=wkorman@chromium.org
    
    Review URL: https://codereview.chromium.org/2078593005 .
    
    Cr-Commit-Position: refs/heads/master@{#400351}
    
    Former-commit-id: c12157ef6b1fac0f935af79e2fc9028354d59769

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/lint_test_expectations_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/lint_test_expectations_unittest.py
@@ -101,8 +101,7 @@ class LintTest(unittest.TestCase):
         self.assertEqual(res, [])
         self.assertEqual(host.ports_parsed, ['a', 'b', 'b-win'])
 
-    # TODO(crbug.com/574272) - Reenable this after the bug is fixed.
-    def disabled_test_lint_test_files(self):
+    def test_lint_test_files(self):
         logging_stream = StringIO.StringIO()
         options = optparse.Values({'platform': 'test-mac-mac10.10'})
         host = MockHost()
