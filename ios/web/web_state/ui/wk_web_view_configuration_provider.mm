66e5a8848e9c3dbd701fe0ec220e0bb04e008c02
michaeldo <michaeldo@chromium.org>
2016-06-16 20:26:02 -0700

    Enable WKWebView phone number detection.
    Set dataDetectorTypes on the WKWebView configuration to enable Phone
    Numbers on web pages to be automatically clickable.
    
    BUG=467213
    
    Review-Url: https://codereview.chromium.org/2076753002
    Cr-Commit-Position: refs/heads/master@{#400340}
    
    Former-commit-id: f7cc3350fd1c5e4055b3fb52394137c8b61d49f2

--- ios/web/web_state/ui/wk_web_view_configuration_provider.mm
+++ ios/web/web_state/ui/wk_web_view_configuration_provider.mm
@@ -63,6 +63,12 @@ WKWebViewConfigurationProvider::GetWebViewConfiguration() {
       [configuration_
           setWebsiteDataStore:[WKWebsiteDataStore nonPersistentDataStore]];
     }
+// TODO(crbug.com/620878) Remove these guards after moving to iOS10 SDK.
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+    if (base::ios::IsRunningOnIOS10OrLater()) {
+      [configuration_ setDataDetectorTypes:WKDataDetectorTypePhoneNumber];
+    }
+#endif
     // setJavaScriptCanOpenWindowsAutomatically is required to support popups.
     [[configuration_ preferences] setJavaScriptCanOpenWindowsAutomatically:YES];
     [[configuration_ userContentController]
