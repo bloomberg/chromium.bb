e5701f1d88fd1dac984b06c4edddae4fa1444822
rsesek <rsesek@chromium.org>
2016-06-17 18:58:07 -0700

    [Mac/iOS/GN] Use rsync in copy_bundle_data instead of shutil.copytree.
    As the TODO this fixes states, copytree preserves mtimes. This can cause
    overbuild when building bundled products. Switch to using rsync to have more
    control over copy parameters.
    
    BUG=297668,620950
    R=dpranke@chromium.org
    
    Originally Committed: https://crrev.com/b62d6f0b0b80bf6ce624694f308f551b58044368 [formerly 0cc137a639e9eb687b0fbde47983d672bcc8ef9f]
    Reverted: https://crrev.com/a7e693b7afab61d939d323692879d1d95e1b7850 [formerly e127bb46966c5d7eec116a9612502e1cd0ea20cf]
    
    Review-Url: https://codereview.chromium.org/2072713005
    Cr-Commit-Position: refs/heads/master@{#400570}
    
    Former-commit-id: e10a42e0d13e56e035a290b66a873442a7f71cf7

--- build/toolchain/mac/copy_bundle_data.py
+++ build/toolchain/mac/copy_bundle_data.py
@@ -3,10 +3,22 @@
 # found in the LICENSE file.
 
 import argparse
+import errno
 import os
 import shutil
+import subprocess
 import sys
 
+# Copies a macOS or iOS resource into a bundle. Special handling is given to
+# .strings files.
+#
+# Usage: python copy_bundle_data.py path/to/source path/of/destination
+#
+# Note that if |source| is a directory, its contents will be copied to |dest|,
+# rather than copying the |source| directory itself. Example:
+#   copy_bundle_data.py out/Release/Foo.framework out/Release/App/Foo.framework
+# The contents of Foo.framwork will be copied into the destination path, which
+# includes the name of the destination framework, which is also Foo.framework.
 
 def DetectEncoding(data, default_encoding='UTF-8'):
   """Detects the encoding used by |data| from the Byte-Order-Mark if present.
@@ -80,25 +92,28 @@ def CopyFile(source, dest, strings_format):
     source: string, path to the source file
     dest: string, path to the destination file
   """
-  if os.path.isdir(source):
-    if os.path.exists(dest):
-      shutil.rmtree(dest)
-    # Copy tree.
-    # TODO(thakis): This copies file attributes like mtime, while the
-    # single-file branch below doesn't. This should probably be changed to
-    # be consistent with the single-file branch.
-    shutil.copytree(source, dest, symlinks=True)
-    return
-
-  if os.path.exists(dest):
-    os.unlink(dest)
+  try:
+    shutil.rmtree(dest)
+  except OSError as e:
+    if e.errno == errno.ENOENT:
+      pass
+    elif e.errno == errno.ENOTDIR:
+      os.unlink(dest)
+    else:
+      raise
 
   _, extension = os.path.splitext(source)
   if extension == '.strings':
     CopyStringsFile(source, dest, strings_format)
     return
 
-  shutil.copy(source, dest)
+  # If the source is a directory, add a trailing slash so its contents get
+  # copied, rather than copying the directory itself.
+  if os.path.isdir(source) and not source.endswith('/'):
+    source += '/'
+
+  subprocess.check_call(
+      ['rsync', '--recursive', '--perms', '--links', source, dest])
 
 
 def Main():
