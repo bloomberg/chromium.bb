04bdf98fe4ac6efaedc89dd4969d5b68143ea98e
fgorski <fgorski@chromium.org>
2016-06-20 15:15:54 -0700

    [Offline pages] Renaming the file with request queue store tests
    This patch moves out file renaming from issue 2053163002
    
    BUG=607221
    
    Review-Url: https://codereview.chromium.org/2083783002
    Cr-Commit-Position: refs/heads/master@{#400810}
    
    Former-commit-id: c2c55ab11fee910d1426e0b59de55a88c5f36420

--- components/offline_pages/background/request_queue_store_unittest.cc
+++ components/offline_pages/background/request_queue_store_unittest.cc
@@ -2,14 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/offline_pages/background/request_queue_in_memory_store.h"
+#include "components/offline_pages/background/request_queue_store.h"
 
 #include <memory>
 
 #include "base/bind.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "components/offline_pages/background/request_queue_store.h"
+#include "components/offline_pages/background/request_queue_in_memory_store.h"
 #include "components/offline_pages/background/save_page_request.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
