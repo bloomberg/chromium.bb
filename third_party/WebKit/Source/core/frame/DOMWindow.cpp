daa7ab701fd9ebd8fc738e942f66307d94522142
yukishiino <yukishiino@chromium.org>
2016-06-21 09:14:39 -0700

    binding: Window-returning attributes return null if a Window is detached.
    Makes the attributes that return a WindowProxy return null if the
    browsing context of the Window is detached in order to avoid crashes
    that are caused by accessing to a ScriptState for the detached browsing
    context.
    
    This is a kind of first aid, and not the right fix.  The spec requires
    DOM attributes such as Window.parent or top to return the WindowProxy
    of the browsing context if it's detached.  I.e. we need to fully support
    detached frames.
    
    BUG=621730,621577
    
    Review-Url: https://codereview.chromium.org/2085983002
    Cr-Commit-Position: refs/heads/master@{#401007}
    
    Former-commit-id: eb73fb7bd81b3253a36171851da62e15e5f88b41

--- third_party/WebKit/Source/core/frame/DOMWindow.cpp
+++ third_party/WebKit/Source/core/frame/DOMWindow.cpp
@@ -103,7 +103,10 @@ DOMWindow* DOMWindow::opener() const
 
 DOMWindow* DOMWindow::parent() const
 {
-    if (!frame())
+    // TODO(yukishiino): The 'parent' attribute must return |this|
+    // (the WindowProxy object of the browsing context itself) when it's
+    // top-level or detached.
+    if (!isCurrentlyDisplayedInFrame())
         return nullptr;
 
     Frame* parent = frame()->tree().parent();
@@ -112,7 +115,10 @@ DOMWindow* DOMWindow::parent() const
 
 DOMWindow* DOMWindow::top() const
 {
-    if (!frame())
+    // TODO(yukishiino): The 'top' attribute must return |this|
+    // (the WindowProxy object of the browsing context itself) when it's
+    // top-level or detached.
+    if (!isCurrentlyDisplayedInFrame())
         return nullptr;
 
     return frame()->tree().top()->domWindow();
