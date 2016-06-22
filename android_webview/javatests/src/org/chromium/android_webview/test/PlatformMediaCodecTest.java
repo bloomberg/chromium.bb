d6ca8bfe63f9b88096ac6a3b5fa39fc3d719db9b
boliu <boliu@chromium.org>
2016-06-21 18:57:20 -0700

    aw: Disable testCanPlayPlatformMediaCodecs
    Test is flaky. See bug for details.
    
    BUG=620890
    TBR=dalecurtis
    
    Review-Url: https://codereview.chromium.org/2086173002
    Cr-Commit-Position: refs/heads/master@{#401158}
    
    Former-commit-id: 355585715cb19b5fea8cd1db2c2682e4867617c3

--- android_webview/javatests/src/org/chromium/android_webview/test/PlatformMediaCodecTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/PlatformMediaCodecTest.java
@@ -4,9 +4,7 @@
 
 package org.chromium.android_webview.test;
 
-import android.test.suitebuilder.annotation.MediumTest;
-
-import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.browser.test.util.DOMUtils;
 import org.chromium.content_public.browser.WebContents;
@@ -30,8 +28,11 @@ public class PlatformMediaCodecTest extends AwTestBase {
         enableJavaScriptOnUiThread(mTestContainerView.getAwContents());
     }
 
+    /*
     @MediumTest
     @Feature({"AndroidWebView"})
+    */
+    @DisabledTest(message = "crbug.com/620890")
     public void testCanPlayPlatformMediaCodecs() throws Throwable {
         loadUrlSync(mTestContainerView.getAwContents(), mContentsClient.getOnPageFinishedHelper(),
                 "file:///android_asset/platform-media-codec-test.html");
