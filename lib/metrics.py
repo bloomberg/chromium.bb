61fb5ca8994ec10c131719e77d535c2ba6fc1d7a
Aviv Keshet <akeshet@chromium.org>
2016-06-22 14:08:45 -0700

    metrics: add support for String, Boolean, and Float metrics
    BUG=None
    TEST=Unit test
    
    Change-Id: I05de576555e6ab2e5e60021e034666c248ee4cb8
    Reviewed-on: https://chromium-review.googlesource.com/355180
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>
    Trybot-Ready: Aviv Keshet <akeshet@chromium.org>
    Tested-by: Aviv Keshet <akeshet@chromium.org>

--- lib/metrics.py
+++ lib/metrics.py
@@ -1,4 +1,4 @@
-# Copyright 2014 The Chromium OS Authors. All rights reserved.
+# Copyright 2016 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -29,17 +29,55 @@ class MockMetric(object):
     return self._mock_method
 
 
+def _ImportSafe(fn):
+  """Decorator which causes |fn| to return MockMetric if ts_mon not imported."""
+  def wrapper(*args, **kwargs):
+    if metrics:
+      return fn(*args, **kwargs)
+    else:
+      return MockMetric()
+
+  return wrapper
+
+
+def _GetOrConstructMetric(name, constructor):
+  """Returns an existing metric handle or constructs a new one.
+
+  Args:
+    name: Name of the metric to construct.
+    constructor: Callable constructor object, if that metric doesn't exist.
+
+  Returns:
+    A metric handle, or a MockMetric if ts_mon was not imported.
+  """
+  return interface.state.metrics.get(name) or constructor(name)
+
+
+@_ImportSafe
 def Counter(name):
-  """Returns a counter handle for counter named |name|."""
-  if metrics:
-    return interface.state.metrics.get(name) or metrics.CounterMetric(name)
-  else:
-    return MockMetric()
+  """Returns a metric handle for a counter named |name|."""
+  return _GetOrConstructMetric(name, metrics.CounterMetric)
 
 
+@_ImportSafe
 def Gauge(name):
-  """Returns a gauge handle for gauge named |name|."""
-  if metrics:
-    return interface.state.metrics.get(name) or metrics.GaugeMetric(name)
-  else:
-    return MockMetric()
+  """Returns a metric handle for a gauge named |name|."""
+  return _GetOrConstructMetric(name, metrics.GaugeMetric)
+
+
+@_ImportSafe
+def String(name):
+  """Returns a metric handle for a string named |name|."""
+  return _GetOrConstructMetric(name, metrics.StringMetric)
+
+
+@_ImportSafe
+def Boolean(name):
+  """Returns a metric handle for a boolean named |name|."""
+  return _GetOrConstructMetric(name, metrics.BooleanMetric)
+
+
+@_ImportSafe
+def Float(name):
+  """Returns a metric handle for a float named |name|."""
+  return _GetOrConstructMetric(name, metrics.FloatMetric)
