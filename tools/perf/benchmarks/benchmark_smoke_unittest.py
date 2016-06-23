d7df0dc1fb738f68147d90a63984702b81510f67
nednguyen <nednguyen@google.com>
2016-06-23 05:36:29 -0700

    [tools/perf] Remove ANDROID_BLACK_LIST from benchmark_smoke_unittest
    Since telemetry_perf_unittest is sharded & swarmed, cycle time shouldn't be a
    problem.
    
    BUG=548652
    
    Review-Url: https://codereview.chromium.org/2096563002
    Cr-Commit-Position: refs/heads/master@{#401589}
    
    Former-commit-id: 223ef77c15c12d7f032f43f80186c6f241773f57

--- tools/perf/benchmarks/benchmark_smoke_unittest.py
+++ tools/perf/benchmarks/benchmark_smoke_unittest.py
@@ -23,16 +23,13 @@ from telemetry.testing import progress_reporter
 from benchmarks import image_decoding
 from benchmarks import indexeddb_perf
 from benchmarks import jetstream
-from benchmarks import kraken
 from benchmarks import memory
 from benchmarks import octane
 from benchmarks import rasterize_and_record_micro
 from benchmarks import repaint
 from benchmarks import spaceport
 from benchmarks import speedometer
-from benchmarks import sunspider
 from benchmarks import text_selection
-from benchmarks import tracing
 
 
 def SmokeTestGenerator(benchmark):
@@ -97,14 +94,6 @@ _BLACK_LIST_TEST_MODULES = {
     memory  # Flaky on bots, crbug.com/513767.
 }
 
-# Some smoke benchmark tests that run quickly on desktop platform can be very
-# slow on Android. So we create a separate set of black list only for Android.
-_ANDROID_BLACK_LIST_MODULES = {
-    kraken,  # Takes 275 seconds on Android.
-    sunspider,  # Takes 163 seconds on Android.
-    tracing,  # Failed on Android bot, crbug.com/600989.
-}
-
 
 def load_tests(loader, standard_tests, pattern):
   del loader, standard_tests, pattern  # unused
@@ -149,14 +138,6 @@ def load_tests(loader, standard_tests, pattern):
           getattr(benchmark, attribute, set()))
       if merged_attributes:
         setattr(method, attribute, merged_attributes)
-
-    # Disable some tests on android platform only.
-    if sys.modules[benchmark.__module__] in _ANDROID_BLACK_LIST_MODULES:
-      method._disabled_strings.add('android')
-
-    # TODO(bashi): Remove once crrev.com/1266833004 is landed.
-    if benchmark.Name() == 'memory.blink_memory_mobile':
-      method._disabled_strings.add('android')
 
     setattr(BenchmarkSmokeTest, benchmark.Name(), method)
 
