ae4f2dede1edc2045042132bf15caa6aa2a33286
yosin <yosin@chromium.org>
2016-06-23 02:41:03 -0700

    Make default constructor of Font class to initialize all member variables
    This patch makes default constructor of |Font| class to initialize
    |m_canShapeWordByWord| and |m_shapeWordByWordComputed| member variables to
    make MSAN happy.
    
    This patch is a preparation of re-landing http://crrev.com/2082893005, which
    is revered by uninitialized member variables of |Font|.
    
    BUG=618237, 622566
    TEST=n/a; MSAN will check this
    
    Review-Url: https://codereview.chromium.org/2091633002
    Cr-Commit-Position: refs/heads/master@{#401567}
    
    Former-commit-id: 2217e8c563575ead5c9450408529340ec93ccef7

--- third_party/WebKit/Source/platform/fonts/Font.cpp
+++ third_party/WebKit/Source/platform/fonts/Font.cpp
@@ -57,6 +57,8 @@ using namespace Unicode;
 namespace blink {
 
 Font::Font()
+    : m_canShapeWordByWord(0)
+    , m_shapeWordByWordComputed(0)
 {
 }
 
@@ -70,6 +72,9 @@ Font::Font(const FontDescription& fd)
 Font::Font(const Font& other)
     : m_fontDescription(other.m_fontDescription)
     , m_fontFallbackList(other.m_fontFallbackList)
+    // TODO(yosin): We should have a comment the reason why don't we copy
+    // |m_canShapeWordByWord| and |m_shapeWordByWordComputed| from |other|,
+    // since |operator=()| copies them from |other|.
     , m_canShapeWordByWord(0)
     , m_shapeWordByWordComputed(0)
 {
