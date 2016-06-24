2a142968dd8581a564526e850ca5665e06207356
j.isorce <j.isorce@samsung.com>
2016-06-23 23:47:44 -0700

    Make DesktopWindowTreeHostX11::ShouldWindowContentsBeTransparent() relies on argb visual flag
    If depth is 32 and if there is a compositing manager then transparency is supported.
    
    Do not force transparency if Widget::InitParams::TRANSLUCENT_WINDOW is set whereas transparency is not supported.
    Which is the same behavior as on OS_WIN.
    
    Also add a new test WidgetTest.Transparency_DesktopWidget to widget_unittest.cc which ensures that transparency state of the native window matches what returns ShouldWindowContentsBeTransparent().
    
    BUG=589509
    
    R=erg@chromium.org, mgiuca@chromium.org, sadrul@chromium.org
    
    TEST=views_unittests --gtest_filter=*WidgetTest.Transparency*
    
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_optional_gpu_tests_rel;tryserver.chromium.mac:mac_optional_gpu_tests_rel;tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/1998653002
    Cr-Commit-Position: refs/heads/master@{#401826}
    
    Former-commit-id: 29d8c51452a83dcbc359679e8ce36af41af64e36

--- ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_x11.cc
@@ -784,7 +784,7 @@ bool DesktopWindowTreeHostX11::ShouldUseNativeFrame() const {
 }
 
 bool DesktopWindowTreeHostX11::ShouldWindowContentsBeTransparent() const {
-  return false;
+  return IsTranslucentWindowOpacitySupported();
 }
 
 void DesktopWindowTreeHostX11::FrameTypeChanged() {
@@ -940,7 +940,7 @@ bool DesktopWindowTreeHostX11::IsAnimatingClosed() const {
 }
 
 bool DesktopWindowTreeHostX11::IsTranslucentWindowOpacitySupported() const {
-  return false;
+  return use_argb_visual_;
 }
 
 void DesktopWindowTreeHostX11::SizeConstraintsChanged() {
@@ -1148,7 +1148,10 @@ void DesktopWindowTreeHostX11::InitX11Window(
     attribute_mask |= CWBorderPixel;
     swa.border_pixel = 0;
 
-    use_argb_visual_ = true;
+    // A compositing manager is required to support transparency.
+    use_argb_visual_ =
+        XGetSelectionOwner(xdisplay_, atom_cache_.GetAtom("_NET_WM_CM_S0")) !=
+        None;
   }
 
   bounds_in_pixels_ = ToPixelRect(params.bounds);
