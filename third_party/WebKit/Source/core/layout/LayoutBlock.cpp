6feec8387a47a48e0b0fb315adeba62c911a289f
wangxianzhu <wangxianzhu@chromium.org>
2016-06-28 14:36:58 -0700

    Update containing block's positioned descendants list when an object becomes out-of-flow-positioned
    This is required when there are descendants needing overflow recalc.
    Otherwise during overflow recalc because the containing block's
    positioned descendant list didn't contain the new out-of-flow-positioned
    object we would not reach the descendants needing overflow recalc.
    
    BUG=623867
    TEST=fast/overflow/add-visual-overflow-and-change-container-position.html
    
    Review-Url: https://codereview.chromium.org/2103843003
    Cr-Commit-Position: refs/heads/master@{#402550}
    
    Former-commit-id: fd08a658d68598dce7886abf88fbc71ed6659f41

--- third_party/WebKit/Source/core/layout/LayoutBlock.cpp
+++ third_party/WebKit/Source/core/layout/LayoutBlock.cpp
@@ -202,8 +202,15 @@ void LayoutBlock::styleDidChange(StyleDifference diff, const ComputedStyle* oldS
             // block's list during layout. However the positioned descendant layout logic assumes
             // layout objects to obey parent-child order in the list. Remove our descendants here
             // so they will be re-inserted after us.
-            if (LayoutBlock* cb = containingBlock())
+            if (LayoutBlock* cb = containingBlock()) {
                 cb->removePositionedObjects(this, NewContainingBlock);
+                if (isOutOfFlowPositioned()) {
+                    // Insert this object into containing block's positioned descendants list
+                    // in case the parent won't layout. This is needed especially there are
+                    // descendants scheduled for overflow recalc.
+                    cb->insertPositionedObject(this);
+                }
+            }
         }
     }
 
