98c474dbd66f5098672743e098f2d9bd56455eb7
jaepark <jaepark@google.com>
2016-06-28 20:00:24 -0700

    Remove redundant variable declaration in pdf/basic_plugin_test.js.
    Review-Url: https://codereview.chromium.org/2102883004
    Cr-Commit-Position: refs/heads/master@{#402695}
    
    Former-commit-id: 625e2c8105ebb18772ac3cd939d9bbeef48cde56

--- chrome/test/data/pdf/basic_plugin_test.js
+++ chrome/test/data/pdf/basic_plugin_test.js
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var scriptingAPI;
-
 /**
  * These tests require that the PDF plugin be available to run correctly.
  */
