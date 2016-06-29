01f2b28237d6add6fb140e0e04b4ca1b517ad168
scroggo <scroggo@google.com>
2016-06-29 15:36:08 -0700

    Fix dead-lock issue attempting to decode
    During serialization, if DecodingImageGenerator::onRefEncodedData()
    returns nullptr, this may require the serialization code to decode so
    it can have something to then encode and serialize.
    
    This can cause a deadlock if the DecodingImageGenerator is using
    discardable memory, which cannot be used from certain threads (where
    serialization may be occurring).
    
    Currently, onRefEncodedData() returns nullptr if !m_allDataReceived.
    This was to fix crbug.com/568016, but that has been further fixed by
    crrev.com/1970773002, which avoids calling onRefEncodedData() to pass
    it to the GPU, since even if m_allDataReceived is true, we do not yet
    support any GPU texture formats.
    
    This change returns the encoded data even if the data has not all been
    received, except if the GrContext argument is not NULL for the future
    when the GPU starts calling it again.
    
    This will result in serializing the existing data and avoid the
    deadlock.
    
    BUG=607227,623475
    
    Review-Url: https://codereview.chromium.org/2108163003
    Cr-Commit-Position: refs/heads/master@{#402963}
    
    Former-commit-id: 7ba0aa90179d42ac4806386a88d50ed3778ae30b

--- third_party/WebKit/Source/platform/graphics/DecodingImageGenerator.cpp
+++ third_party/WebKit/Source/platform/graphics/DecodingImageGenerator.cpp
@@ -54,7 +54,19 @@ SkData* DecodingImageGenerator::onRefEncodedData(GrContext* ctx)
 {
     TRACE_EVENT0("blink", "DecodingImageGenerator::refEncodedData");
 
-    return m_allDataReceived ? m_data->getAsSkData().leakRef() : nullptr;
+    // The GPU only wants the data if it has all been received, since the GPU
+    // only wants a complete texture. getAsSkData() may require copying, so
+    // skip it and just return nullptr to avoid a slowdown. (See
+    // crbug.com/568016 for details about such a slowdown.)
+    // TODO (scroggo): Stop relying on the internal knowledge of how Skia uses
+    // this. skbug.com/5485
+    if (ctx && !m_allDataReceived)
+        return nullptr;
+
+    // Other clients are serializers, which want the data even if it requires
+    // copying, and even if the data is incomplete. (Otherwise they would
+    // potentially need to decode the partial image in order to re-encode it.)
+    return m_data->getAsSkData().leakRef();
 }
 
 bool DecodingImageGenerator::onGetPixels(const SkImageInfo& info, void* pixels, size_t rowBytes, SkPMColor table[], int* tableCount)
