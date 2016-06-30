4ccd1205d255235669837eaad3dcb69d7db043b3
tfarina <tfarina@chromium.org>
2016-06-30 00:13:09 -0700

    content: convert QuotaPolicyCookieStoreTest to use CanonicalCookie's Create() method.
    This patch converts the usage of CanonicalCookie's constructor in
    QuotaPolicyCookieStoreTest tests to use CanonicalCookie::Create() method.
    
    We are doing this because the Create() method validates the parameters
    and we thus we can test the return value to see if it has passed or not.
    Whereas we can not do this when using the constructor.
    
    BUG=57061
    TEST=content_unittests
    R=mmenke@chromium.org,avi@chromium.org
    
    Review-Url: https://codereview.chromium.org/2111533002
    Cr-Commit-Position: refs/heads/master@{#403109}
    
    Former-commit-id: 8c79919c81d99412193ad6274e5b818ef3f04c5d

--- content/browser/net/quota_policy_cookie_store_unittest.cc
+++ content/browser/net/quota_policy_cookie_store_unittest.cc
@@ -21,6 +21,7 @@
 #include "net/test/test_data_directory.h"
 #include "sql/statement.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
 
 namespace {
 const base::FilePath::CharType kTestCookiesFilename[] =
@@ -92,14 +93,15 @@ class QuotaPolicyCookieStoreTest : public testing::Test {
   }
 
   // Adds a persistent cookie to store_.
-  void AddCookie(const std::string& name,
+  void AddCookie(const GURL& url,
+                 const std::string& name,
                  const std::string& value,
                  const std::string& domain,
                  const std::string& path,
                  const base::Time& creation) {
-    store_->AddCookie(net::CanonicalCookie(
-        GURL(), name, value, domain, path, creation, creation, creation, false,
-        false, net::CookieSameSite::DEFAULT_MODE,
+    store_->AddCookie(*net::CanonicalCookie::Create(
+        url, name, value, domain, path, creation, creation, false, false,
+        net::CookieSameSite::DEFAULT_MODE, false,
         net::COOKIE_PRIORITY_DEFAULT));
   }
 
@@ -137,9 +139,9 @@ TEST_F(QuotaPolicyCookieStoreTest, TestPersistence) {
   ASSERT_EQ(0U, cookies.size());
 
   base::Time t = base::Time::Now();
-  AddCookie("A", "B", "foo.com", "/", t);
+  AddCookie(GURL("http://foo.com"), "A", "B", std::string(), "/", t);
   t += base::TimeDelta::FromInternalValue(10);
-  AddCookie("A", "B", "persistent.com", "/", t);
+  AddCookie(GURL("http://persistent.com"), "A", "B", std::string(), "/", t);
 
   // Replace the store, which forces the current store to flush data to
   // disk. Then, after reloading the store, confirm that the data was flushed by
@@ -181,11 +183,11 @@ TEST_F(QuotaPolicyCookieStoreTest, TestPolicy) {
   ASSERT_EQ(0U, cookies.size());
 
   base::Time t = base::Time::Now();
-  AddCookie("A", "B", "foo.com", "/", t);
+  AddCookie(GURL("http://foo.com"), "A", "B", std::string(), "/", t);
   t += base::TimeDelta::FromInternalValue(10);
-  AddCookie("A", "B", "persistent.com", "/", t);
+  AddCookie(GURL("http://persistent.com"), "A", "B", std::string(), "/", t);
   t += base::TimeDelta::FromInternalValue(10);
-  AddCookie("A", "B", "nonpersistent.com", "/", t);
+  AddCookie(GURL("http://nonpersistent.com"), "A", "B", std::string(), "/", t);
 
   // Replace the store, which forces the current store to flush data to
   // disk. Then, after reloading the store, confirm that the data was flushed by
@@ -204,7 +206,8 @@ TEST_F(QuotaPolicyCookieStoreTest, TestPolicy) {
   EXPECT_EQ(3U, cookies.size());
 
   t += base::TimeDelta::FromInternalValue(10);
-  AddCookie("A", "B", "nonpersistent.com", "/second", t);
+  AddCookie(GURL("http://nonpersistent.com"), "A", "B", std::string(),
+            "/second", t);
 
   // Now close the store, and "nonpersistent.com" should be deleted according to
   // policy.
@@ -225,7 +228,7 @@ TEST_F(QuotaPolicyCookieStoreTest, ForceKeepSessionState) {
   ASSERT_EQ(0U, cookies.size());
 
   base::Time t = base::Time::Now();
-  AddCookie("A", "B", "foo.com", "/", t);
+  AddCookie(GURL("http://foo.com"), "A", "B", std::string(), "/", t);
 
   // Recreate |store_| with a storage policy that makes "nonpersistent.com"
   // session only, but then instruct the store to forcibly keep all cookies.
@@ -241,9 +244,9 @@ TEST_F(QuotaPolicyCookieStoreTest, ForceKeepSessionState) {
   EXPECT_EQ(1U, cookies.size());
 
   t += base::TimeDelta::FromInternalValue(10);
-  AddCookie("A", "B", "persistent.com", "/", t);
+  AddCookie(GURL("http://persistent.com"), "A", "B", std::string(), "/", t);
   t += base::TimeDelta::FromInternalValue(10);
-  AddCookie("A", "B", "nonpersistent.com", "/", t);
+  AddCookie(GURL("http://nonpersistent.com"), "A", "B", std::string(), "/", t);
 
   // Now close the store, but the "nonpersistent.com" cookie should not be
   // deleted.
@@ -270,7 +273,7 @@ TEST_F(QuotaPolicyCookieStoreTest, TestDestroyOnBackgroundThread) {
   ASSERT_EQ(0U, cookies.size());
 
   base::Time t = base::Time::Now();
-  AddCookie("A", "B", "nonpersistent.com", "/", t);
+  AddCookie(GURL("http://nonpersistent.com"), "A", "B", std::string(), "/", t);
 
   // Replace the store, which forces the current store to flush data to
   // disk. Then, after reloading the store, confirm that the data was flushed by
