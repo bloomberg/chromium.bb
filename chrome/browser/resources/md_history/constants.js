10798535e73cf00217dce36178d18ff04a7849ff
tsergeant <tsergeant@chromium.org>
2016-06-30 16:53:46 -0700

    MD History: Trim long item titles to reduce effect on performance
    History items with extremely long titles are very slow to render,
    due to needing to layout the entire title in order to determine where to
    cut it off with ellipses. This greatly degrades the performance of MD
    History in a profile which has visited several of these items.
    
    This CL trims titles down to at most 300 characters before they are
    rendered, which has no visible effect, but greatly improves performance.
    On a profile that has visited crashsafari[dot]com, load time is reduced
    from ~5000ms to ~1800ms on my Z620. Scrolling performance is also
    greatly improved.
    
    BUG=621347
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2112803002
    Cr-Commit-Position: refs/heads/master@{#403351}
    
    Former-commit-id: 24e3e3772122abf6a01683c9b412256228428fd7

--- chrome/browser/resources/md_history/constants.js
+++ chrome/browser/resources/md_history/constants.js
@@ -13,6 +13,14 @@
 var BROWSING_GAP_TIME = 15 * 60 * 1000;
 
 /**
+ * Maximum length of a history item title. Anything longer than this will be
+ * cropped to fit within this limit. This value is large enough that it will not
+ * be noticeable in a 960px wide history-item.
+ * @const
+ */
+var TITLE_MAX_LENGTH = 300;
+
+/**
  * @enum {number}
  */
 var HistoryRange = {
