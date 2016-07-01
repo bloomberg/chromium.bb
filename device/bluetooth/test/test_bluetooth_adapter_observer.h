123b9f8693c827728a34265726502a8fc6c834ba
ortuno <ortuno@chromium.org>
2016-06-30 18:09:38 -0700

    bluetooth: Mark TestBluetoothAdapterObserver accessor methods const
    There is no reason why the methods should be non-const. More importantly
    it allows us to pass the observer as a const reference to lambdas or
    callbacks that need to check its properties.
    
    BUG=580406
    
    Review-Url: https://codereview.chromium.org/2096043002
    Cr-Commit-Position: refs/heads/master@{#403380}
    
    Former-commit-id: 084aec7f79d92169449f435c51aa6f562413b323

--- device/bluetooth/test/test_bluetooth_adapter_observer.h
+++ device/bluetooth/test/test_bluetooth_adapter_observer.h
@@ -78,64 +78,78 @@ class TestBluetoothAdapterObserver : public BluetoothAdapter::Observer {
                                   const std::vector<uint8_t>& value) override;
 
   // Adapter related:
-  int present_changed_count() { return present_changed_count_; }
-  int powered_changed_count() { return powered_changed_count_; }
-  int discoverable_changed_count() { return discoverable_changed_count_; }
-  int discovering_changed_count() { return discovering_changed_count_; }
-  bool last_present() { return last_present_; }
-  bool last_powered() { return last_powered_; }
-  bool last_discovering() { return last_discovering_; }
+  int present_changed_count() const { return present_changed_count_; }
+  int powered_changed_count() const { return powered_changed_count_; }
+  int discoverable_changed_count() const { return discoverable_changed_count_; }
+  int discovering_changed_count() const { return discovering_changed_count_; }
+  bool last_present() const { return last_present_; }
+  bool last_powered() const { return last_powered_; }
+  bool last_discovering() const { return last_discovering_; }
 
   // Device related:
-  int device_added_count() { return device_added_count_; }
-  int device_changed_count() { return device_changed_count_; }
-  int device_address_changed_count() { return device_address_changed_count_; }
+  int device_added_count() const { return device_added_count_; }
+  int device_changed_count() const { return device_changed_count_; }
+  int device_address_changed_count() const {
+    return device_address_changed_count_;
+  }
 #if defined(OS_CHROMEOS) || defined(OS_LINUX)
-  int device_paired_changed_count() { return device_paired_changed_count_; }
-  bool device_new_paired_status() { return device_new_paired_status_; }
+  int device_paired_changed_count() const {
+    return device_paired_changed_count_;
+  }
+  bool device_new_paired_status() const { return device_new_paired_status_; }
 #endif
-  int device_removed_count() { return device_removed_count_; }
-  BluetoothDevice* last_device() { return last_device_; }
-  std::string last_device_address() { return last_device_address_; }
+  int device_removed_count() const { return device_removed_count_; }
+  BluetoothDevice* last_device() const { return last_device_; }
+  std::string last_device_address() const { return last_device_address_; }
 
   // GATT related:
-  int gatt_service_added_count() { return gatt_service_added_count_; }
-  int gatt_service_removed_count() { return gatt_service_removed_count_; }
-  int gatt_services_discovered_count() {
+  int gatt_service_added_count() const { return gatt_service_added_count_; }
+  int gatt_service_removed_count() const { return gatt_service_removed_count_; }
+  int gatt_services_discovered_count() const {
     return gatt_services_discovered_count_;
   }
-  int gatt_service_changed_count() { return gatt_service_changed_count_; }
-  int gatt_discovery_complete_count() { return gatt_discovery_complete_count_; }
-  int gatt_characteristic_added_count() {
+  int gatt_service_changed_count() const { return gatt_service_changed_count_; }
+  int gatt_discovery_complete_count() const {
+    return gatt_discovery_complete_count_;
+  }
+  int gatt_characteristic_added_count() const {
     return gatt_characteristic_added_count_;
   }
-  int gatt_characteristic_removed_count() {
+  int gatt_characteristic_removed_count() const {
     return gatt_characteristic_removed_count_;
   }
   int gatt_characteristic_value_changed_count() const {
     return gatt_characteristic_value_changed_count_;
   }
-  int gatt_descriptor_added_count() { return gatt_descriptor_added_count_; }
-  int gatt_descriptor_removed_count() { return gatt_descriptor_removed_count_; }
-  int gatt_descriptor_value_changed_count() {
+  int gatt_descriptor_added_count() const {
+    return gatt_descriptor_added_count_;
+  }
+  int gatt_descriptor_removed_count() const {
+    return gatt_descriptor_removed_count_;
+  }
+  int gatt_descriptor_value_changed_count() const {
     return gatt_descriptor_value_changed_count_;
   }
-  std::string last_gatt_service_id() { return last_gatt_service_id_; }
-  BluetoothUUID last_gatt_service_uuid() { return last_gatt_service_uuid_; }
-  std::string last_gatt_characteristic_id() {
+  std::string last_gatt_service_id() const { return last_gatt_service_id_; }
+  BluetoothUUID last_gatt_service_uuid() const {
+    return last_gatt_service_uuid_;
+  }
+  std::string last_gatt_characteristic_id() const {
     return last_gatt_characteristic_id_;
   }
-  BluetoothUUID last_gatt_characteristic_uuid() {
+  BluetoothUUID last_gatt_characteristic_uuid() const {
     return last_gatt_characteristic_uuid_;
   }
-  std::vector<uint8_t> last_changed_characteristic_value() {
+  std::vector<uint8_t> last_changed_characteristic_value() const {
     return last_changed_characteristic_value_;
   }
-  std::string last_gatt_descriptor_id() { return last_gatt_descriptor_id_; }
-  BluetoothUUID last_gatt_descriptor_uuid() {
+  std::string last_gatt_descriptor_id() const {
+    return last_gatt_descriptor_id_;
+  }
+  BluetoothUUID last_gatt_descriptor_uuid() const {
     return last_gatt_descriptor_uuid_;
   }
-  std::vector<uint8_t> last_changed_descriptor_value() {
+  std::vector<uint8_t> last_changed_descriptor_value() const {
     return last_changed_descriptor_value_;
   }
 
