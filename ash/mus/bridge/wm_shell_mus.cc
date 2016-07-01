baa2dbff98374cec05fecfc4f497c1a3a92c7564
sky <sky@chromium.org>
2016-07-01 16:44:33 -0700

    Fixes mash
    Turns out maximize controller uses dbus, which we aren't yet
    creating. Temporarily disable mojo:ash from creating maximize
    controller until sorted out.
    
    BUG=612629
    TEST=covered by tests
    R=jamescook@chromium.org
    TBR=jamescook@chromium.org
    
    Review-Url: https://codereview.chromium.org/2120003002
    Cr-Commit-Position: refs/heads/master@{#403563}
    
    Former-commit-id: 3469a6d1b37bed86a1f08ada6c3e350a236158ba

--- ash/mus/bridge/wm_shell_mus.cc
+++ ash/mus/bridge/wm_shell_mus.cc
@@ -97,8 +97,6 @@ WmShellMus::WmShellMus(::mus::WindowTreeClient* client)
   client_->AddObserver(this);
   WmShell::Set(this);
 
-  CreateMaximizeModeController();
-
   CreateMruWindowTracker();
 
   accessibility_delegate_.reset(new DefaultAccessibilityDelegate);
@@ -108,9 +106,6 @@ WmShellMus::WmShellMus(::mus::WindowTreeClient* client)
 WmShellMus::~WmShellMus() {
   // This order mirrors that of Shell.
 
-  // Destroy maximize mode controller early on since it has some observers which
-  // need to be removed.
-  DeleteMaximizeModeController();
   DeleteSystemTrayDelegate();
   DeleteWindowSelectorController();
   DeleteMruWindowTracker();
