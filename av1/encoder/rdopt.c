8234d97a403edd94557a63e51adc308c5408f95c
Jingning Han <jingning@google.com>
2016-07-01 10:04:57 -0700

    Use the actual inter prediction filter buffer in DRL
    This avoids an encoding segmentation fault in speed 5, due to the
    use of uninitialized dummy inter prediction filter buffer in the
    dynamic motion vector referencing scheme.
    
    Change-Id: Icd888d46623e8abf34267838135eed8656d552e4

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -4115,11 +4115,8 @@ void av1_rd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
           clamp_mv2(&cur_mv.as_mv, xd);
 
           if (!mv_check_bounds(x, &cur_mv.as_mv)) {
-            InterpFilter
-                dummy_single_inter_filter[MB_MODE_COUNT][MAX_REF_FRAMES];
             int dummy_single_skippable[MB_MODE_COUNT][MAX_REF_FRAMES];
             int_mv dummy_single_newmv[MAX_REF_FRAMES] = { { 0 } };
-
             frame_mv[NEARMV][ref_frame] = cur_mv;
             tmp_alt_rd = handle_inter_mode(
                 cpi, x, bsize, &tmp_rate, &tmp_dist, &tmp_skip, &tmp_rate_y,
@@ -4127,8 +4124,8 @@ void av1_rd_pick_inter_mode_sb(AV1_COMP *cpi, TileDataEnc *tile_data,
 #if CONFIG_MOTION_VAR
                 dst_buf1, dst_stride1, dst_buf2, dst_stride2,
 #endif  // CONFIG_MOTION_VAR
-                dummy_single_newmv, dummy_single_inter_filter,
-                dummy_single_skippable, &tmp_sse, best_rd);
+                dummy_single_newmv, single_inter_filter, dummy_single_skippable,
+                &tmp_sse, best_rd);
           }
 
           for (i = 0; i < mbmi->ref_mv_idx; ++i) {
