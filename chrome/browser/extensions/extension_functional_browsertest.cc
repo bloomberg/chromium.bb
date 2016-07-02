da11a52611ad14f5ca67fb96c5873ac1ed56dc68
thestig <thestig@chromium.org>
2016-07-01 19:03:18 -0700

    Re-enable ExtensionFunctionalTest.TestSetExtensionsState.
    It was disabled on Windows because of a flaky failure on Windows XP.
    
    BUG=389545
    
    Review-Url: https://codereview.chromium.org/2115913002
    Cr-Commit-Position: refs/heads/master@{#403589}
    
    Former-commit-id: 199d81f26b4aee6eeb7020a3036807f3670886f7

--- chrome/browser/extensions/extension_functional_browsertest.cc
+++ chrome/browser/extensions/extension_functional_browsertest.cc
@@ -30,19 +30,17 @@ class ExtensionFunctionalTest : public ExtensionBrowserTest {
 
     base::FilePath path = test_data_dir_.AppendASCII(filename);
 
-    extensions::TestExtensionRegistryObserver extension_observer(registry);
+    TestExtensionRegistryObserver extension_observer(registry);
 
-    scoped_refptr<extensions::CrxInstaller> installer(
-        extensions::CrxInstaller::CreateSilent(service));
+    scoped_refptr<CrxInstaller> installer(CrxInstaller::CreateSilent(service));
     installer->set_is_gallery_install(false);
     installer->set_allow_silent_install(true);
     installer->set_install_source(Manifest::INTERNAL);
     installer->set_off_store_install_allow_reason(
-        extensions::CrxInstaller::OffStoreInstallAllowedInTest);
+        CrxInstaller::OffStoreInstallAllowedInTest);
 
-    observer_->Watch(
-        extensions::NOTIFICATION_CRX_INSTALLER_DONE,
-        content::Source<extensions::CrxInstaller>(installer.get()));
+    observer_->Watch(NOTIFICATION_CRX_INSTALLER_DONE,
+                     content::Source<CrxInstaller>(installer.get()));
 
     installer->InstallCrx(path);
     observer_->Wait();
@@ -77,13 +75,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionFunctionalTest,
   EXPECT_FALSE(util::IsIncognitoEnabled(last_loaded_extension_id(), profile()));
 }
 
-// Failing on XP: http://crbug.com/389545
-#if defined(OS_WIN)
-#define MAYBE_TestSetExtensionsState DISABLED_TestSetExtensionsState
-#else
-#define MAYBE_TestSetExtensionsState TestSetExtensionsState
-#endif
-IN_PROC_BROWSER_TEST_F(ExtensionFunctionalTest, MAYBE_TestSetExtensionsState) {
+IN_PROC_BROWSER_TEST_F(ExtensionFunctionalTest, TestSetExtensionsState) {
   InstallExtensionSilently(extension_service(), "google_talk.crx");
 
   // Disable the extension and verify.
@@ -108,4 +100,5 @@ IN_PROC_BROWSER_TEST_F(ExtensionFunctionalTest, MAYBE_TestSetExtensionsState) {
   util::SetIsIncognitoEnabled(last_loaded_extension_id(), profile(), false);
   EXPECT_FALSE(util::IsIncognitoEnabled(last_loaded_extension_id(), profile()));
 }
+
 }  // namespace extensions
