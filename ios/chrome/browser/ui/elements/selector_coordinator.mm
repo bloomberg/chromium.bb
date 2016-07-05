6fba75a4d423d3800eaf7f1c01180d67ee7f8890
jyquinn <jyquinn@chromium.org>
2016-07-05 07:30:08 -0700

    SelectorCoordinator for presenting a picker view from an infobar
    Add coordinator for presenting a picker view from an infobar, as needed
    in update passwords where there are multiple accounts, or in the
    translate infobar for language selection. Starting the coordinator
    animates the picker view in from the bottom edge of the
    baseViewController's view, and stopping animates it out.
    
    BUG=622244
    
    Review-Url: https://codereview.chromium.org/2105253004
    Cr-Commit-Position: refs/heads/master@{#403780}
    
    Former-commit-id: b7a60b6c46e6a3c703a714d64b1786ad130c76c8

---
+++ ios/chrome/browser/ui/elements/selector_coordinator.mm
@@ -0,0 +1,70 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/elements/selector_coordinator.h"
+
+#import "base/mac/objc_property_releaser.h"
+#import "ios/chrome/browser/ui/elements/selector_picker_view_controller.h"
+#import "ios/chrome/browser/ui/elements/selector_view_controller_delegate.h"
+
+@interface SelectorCoordinator ()<SelectorViewControllerDelegate> {
+  base::mac::ObjCPropertyReleaser _propertyReleaser_SelectorCoordinator;
+  __unsafe_unretained id<SelectorCoordinatorDelegate> _delegate;
+  __unsafe_unretained NSOrderedSet<NSString*>* _options;
+}
+
+// Redeclaration of infoBarPickerController as readwrite.
+@property(nonatomic, nullable, retain)
+    SelectorPickerViewController* selectorPickerViewController;
+
+@end
+
+@implementation SelectorCoordinator
+
+@synthesize options = _options;
+@synthesize defaultOption = _defaultOption;
+@synthesize delegate = _delegate;
+@synthesize selectorPickerViewController = _selectorPickerViewController;
+
+- (nullable instancetype)initWithBaseViewController:
+    (nullable UIViewController*)viewController {
+  self = [super initWithBaseViewController:viewController];
+  if (self) {
+    _propertyReleaser_SelectorCoordinator.Init(self,
+                                               [SelectorCoordinator class]);
+  }
+  return self;
+}
+
+- (void)start {
+  self.selectorPickerViewController =
+      [[SelectorPickerViewController alloc] initWithOptions:self.options
+                                                    default:self.defaultOption];
+  self.selectorPickerViewController.delegate = self;
+  // TODO(crbug.com/622244): Display via custom UIPresentionController to
+  // show as a bottom sheet.
+  self.selectorPickerViewController.modalTransitionStyle =
+      UIModalTransitionStyleCoverVertical;
+  self.selectorPickerViewController.modalPresentationStyle =
+      UIModalPresentationFormSheet;
+  [self.baseViewController
+      presentViewController:self.selectorPickerViewController
+                   animated:YES
+                 completion:nil];
+}
+
+- (void)stop {
+  [self.selectorPickerViewController dismissViewControllerAnimated:YES
+                                                        completion:nil];
+}
+
+#pragma mark SelectorViewControllerDelegate
+
+- (void)selectorViewController:(UIViewController*)viewController
+               didSelectOption:(NSString*)option {
+  [self.delegate selectorCoordinator:self didCompleteWithSelection:option];
+  [self stop];
+}
+
+@end
