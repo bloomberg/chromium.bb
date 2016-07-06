bac4680ec9a5c55ab692490b6732999648ecf1e9
Sylvain Defresne <sdefresne@chromium.org>
2016-07-06 17:11:17 +0200

    Only call CopyStringsFile if convert_to_binary is False.
    When convert_to_binary is True, there is no point converting the .strings
    file encoding to UTF-16 as the conversion will be performed by "plutil".
    This fix compilation failure when the input files are already in "binary1"
    property list format.
    
    BUG=625578
    R=justincohen@chromium.org
    
    Review URL: https://codereview.chromium.org/2117353002 .

--- pylib/gyp/mac_tool.py
+++ pylib/gyp/mac_tool.py
@@ -49,6 +49,7 @@ class MacTool(object):
   def ExecCopyBundleResource(self, source, dest, convert_to_binary):
     """Copies a resource file to the bundle/Resources directory, performing any
     necessary compilation on each resource."""
+    convert_to_binary = convert_to_binary == 'True'
     extension = os.path.splitext(source)[1].lower()
     if os.path.isdir(source):
       # Copy tree.
@@ -62,14 +63,14 @@ class MacTool(object):
       return self._CopyXIBFile(source, dest)
     elif extension == '.storyboard':
       return self._CopyXIBFile(source, dest)
-    elif extension == '.strings':
+    elif extension == '.strings' and not convert_to_binary:
       self._CopyStringsFile(source, dest)
     else:
       if os.path.exists(dest):
         os.unlink(dest)
       shutil.copy(source, dest)
 
-    if extension in ('.plist', '.strings') and convert_to_binary == 'True':
+    if convert_to_binary and extension in ('.plist', '.strings'):
       self._ConvertToBinary(dest)
 
   def _CopyXIBFile(self, source, dest):
