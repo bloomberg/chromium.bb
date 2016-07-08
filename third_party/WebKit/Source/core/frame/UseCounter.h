61d0b04c1469ab3b3eb34d9c2b927dcf0ed3f313
foolip <foolip@chromium.org>
2016-07-08 07:07:31 -0700

    Measure usage of MediaStream.prototype.active
    The active/inactive events have been removed from the spec, but the
    active attribute has not, so removing just the events wouldn't reduce
    complexity by much. Gecko doesn't have the attribute either, so measure
    its usage before deciding on the next steps.
    
    Related spec issue:
    https://github.com/w3c/mediacapture-main/issues/370
    
    R=guidou@chromium.org,isherman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2128593003
    Cr-Commit-Position: refs/heads/master@{#404352}
    
    Former-commit-id: 851f0291106fd0d444553c079e3004b815b8f957

--- third_party/WebKit/Source/core/frame/UseCounter.h
+++ third_party/WebKit/Source/core/frame/UseCounter.h
@@ -1234,10 +1234,13 @@ public:
         MediaSourceAbortRemove = 1428,
         MediaSourceDurationTruncatingBuffered = 1429,
         AudioContextCrossOriginIframe = 1430,
+        // The above items are available in M53 branch
+
         PointerEventSetCapture = 1431,
         PointerEventDispatch = 1432,
         MIDIMessageEventReceivedTime = 1433,
         SummaryElementWithDisplayBlockAuthorRule = 1434,
+        V8MediaStream_Active_AttributeGetter = 1435,
 
         // Add new features immediately above this line. Don't change assigned
         // numbers of any item, and don't reuse removed slots.
