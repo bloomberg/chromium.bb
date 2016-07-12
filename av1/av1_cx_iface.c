8a8b770790378041d3486e0dee004bdd91b77b0b
Yaowu Xu <yaowu@google.com>
2016-07-12 11:57:51 -0700

    Correct data size estimation for odd size video
    Given the largest transform size is 32x32, this commmit changes size
    estiiation based on the size rounding up to 32 multiples to avoid
    insufficient buffer allocations.
    
    BUG=https://bugs.chromium.org/p/aomedia/issues/detail?id=36
    
    Change-Id: I6eab09dc6acdc0f5a6bcadb918d62c4852aae21f

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -905,8 +905,9 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
     if (res == AOM_CODEC_OK && cpi != NULL) {
       // There's no codec control for multiple alt-refs so check the encoder
       // instance for its status to determine the compressed data size.
-      data_sz = ctx->cfg.g_w * ctx->cfg.g_h * get_image_bps(img) / 8 *
-                (cpi->multi_arf_allowed ? 8 : 2);
+      data_sz = ALIGN_POWER_OF_TWO(ctx->cfg.g_w, 5) *
+          ALIGN_POWER_OF_TWO(ctx->cfg.g_h, 5) * get_image_bps(img) / 8 *
+          (cpi->multi_arf_allowed ? 8 : 2);
       if (data_sz < kMinCompressedSize) data_sz = kMinCompressedSize;
       if (ctx->cx_data == NULL || ctx->cx_data_sz < data_sz) {
         ctx->cx_data_sz = data_sz;
