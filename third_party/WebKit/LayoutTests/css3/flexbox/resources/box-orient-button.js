448aa0d9654397412c9c3956f266c14fa81f9629
cbiesinger <cbiesinger@chromium.org>
2016-07-12 14:19:41 -0700

    Convert some flexbox tests to testharness.js
    R=eae@chromium.org,dgrogan@chromium.org
    
    Review-Url: https://codereview.chromium.org/2130543003
    Cr-Commit-Position: refs/heads/master@{#404835}
    
    Former-commit-id: e2276c4abe46c25480470c3f6d6b968671a8a4df

--- third_party/WebKit/LayoutTests/css3/flexbox/resources/box-orient-button.js
+++ third_party/WebKit/LayoutTests/css3/flexbox/resources/box-orient-button.js
@@ -1,4 +1,3 @@
-description("Check if box-orient is working in &lt;button&gt;. See https://bugs.webkit.org/show_bug.cgi?id=25406");
 
 function setFlexDirection(element, flexDirection) {
     element.style.webkitFlexDirection = flexDirection;
@@ -14,12 +13,8 @@ setFlexDirection(gebi("toHorizontal"), "row");
 
 var referenceHorizontalHeight = gebi("reference_horizontal").clientHeight;
 var referenceVerticalHeight = gebi("reference_vertical").clientHeight;
-shouldBe("gebi('default').clientHeight", "referenceHorizontalHeight");
-shouldBe("gebi('horizontal').clientHeight", "referenceHorizontalHeight");
-shouldBe("gebi('vertical').clientHeight", "referenceVerticalHeight");
-shouldBe("gebi('toHorizontal').clientHeight", "referenceHorizontalHeight");
-shouldBe("gebi('toVertical').clientHeight", "referenceVerticalHeight");
-
-// If we are in DRT, we don't need meaningless messages.
-if (window.testRunner)
-    document.getElementById("main").innerHTML = "";
+test(function() { assert_equals(gebi('default').clientHeight, referenceHorizontalHeight); }, "default height");
+test(function() { assert_equals(gebi('horizontal').clientHeight, referenceHorizontalHeight); }, "horizontal height");
+test(function() { assert_equals(gebi('vertical').clientHeight, referenceVerticalHeight); }, "vertical height");
+test(function() { assert_equals(gebi('toHorizontal').clientHeight, referenceHorizontalHeight); }, "toHorizontal height");
+test(function() { assert_equals(gebi('toVertical').clientHeight, referenceVerticalHeight); }, "toVertical height");
