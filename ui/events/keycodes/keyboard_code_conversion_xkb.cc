be9dc242b3da3350a1cdcf754bebb4a1a049d2d0
hychao <hychao@chromium.org>
2016-07-12 20:44:37 -0700

    Convert XKB_KEY_XF86AudioPlay to MEDIA_PLAY_PAUSE
    This change makes the media play/pause key on logitech keyboard
    to send MEDIA_PLAY_PAUSE event, so it can control the built-in
    media player on Chromebook.
    
    BUG=597901
    TEST=test with logitech mk520r keyboard
    
    Review-Url: https://codereview.chromium.org/1835493003
    Cr-Commit-Position: refs/heads/master@{#404976}
    
    Former-commit-id: 7fbc859e5011519563167b607b3f13515553e7c6

--- ui/events/keycodes/keyboard_code_conversion_xkb.cc
+++ ui/events/keycodes/keyboard_code_conversion_xkb.cc
@@ -238,7 +238,7 @@ DomKey NonPrintableXKeySymToDomKey(xkb_keysym_t keysym) {
     case XKB_KEY_XF86AudioRaiseVolume:
       return DomKey::AUDIO_VOLUME_UP;
     case XKB_KEY_XF86AudioPlay:
-      return DomKey::MEDIA_PLAY;
+      return DomKey::MEDIA_PLAY_PAUSE;
     case XKB_KEY_XF86AudioStop:
       return DomKey::MEDIA_STOP;
     case XKB_KEY_XF86AudioPrev:
