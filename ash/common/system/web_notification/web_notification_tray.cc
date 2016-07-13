1314442fbed1cb0583aba2f17d51e881041a9b66
yoshiki <yoshiki@chromium.org>
2016-07-13 12:52:46 -0700

    Revert "Hide web notification tray when LoginStatus is locked"
    This reverts commit f08013cfb3b040a52ca34ef9889ba75483ff0e8a [formerly 198e264eb577cab76a6cb13d1d8b4b5d0135cb3c].
    
    The root cause of the crash has been fixed and we don't need this patch now.
    
    BUG=625357
    TBR=warx@chromium.org
    
    Review-Url: https://codereview.chromium.org/2151613002
    Cr-Commit-Position: refs/heads/master@{#405239}
    
    Former-commit-id: 4f78cb96761fe87d72220e08744a5c203c7f5a26

--- ash/common/system/web_notification/web_notification_tray.cc
+++ ash/common/system/web_notification/web_notification_tray.cc
@@ -516,9 +516,8 @@ message_center::MessageCenter* WebNotificationTray::message_center() const {
 
 bool WebNotificationTray::IsLoggedIn() const {
   WmShell* shell = WmShell::Get();
-  LoginStatus status = shell->system_tray_delegate()->GetUserLoginStatus();
-  return status != LoginStatus::NOT_LOGGED_IN &&
-         status != LoginStatus::LOCKED &&
+  return shell->system_tray_delegate()->GetUserLoginStatus() !=
+             LoginStatus::NOT_LOGGED_IN &&
          !shell->GetSessionStateDelegate()->IsInSecondaryLoginScreen();
 }
 
