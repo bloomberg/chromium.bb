ca855f078928772096c0538274309619d980b3a5
kbr <kbr@chromium.org>
2016-07-15 13:59:36 -0700

    Disable angle_end2end_tests on old generation Nexuses
    Taking over ynovikov's https://codereview.chromium.org/2159463002 to
    fix a bug in the generation script.
    
    BUG=angleproject:1362
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2157543003
    Cr-Commit-Position: refs/heads/master@{#405857}
    
    Former-commit-id: 71109d591c253da9181e01301f0a8974f1479a13

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -624,6 +624,16 @@ COMMON_GTESTS = {
         'run_on_optional': True,
       },
     ],
+    'disabled_tester_configs': [
+      {
+        'names': [
+          # TODO(ynovikov) Investigate why the test breaks on older devices.
+          'Android Release (Nexus 5)',
+          'Android Release (Nexus 6)',
+          'Android Release (Nexus 9)',
+        ],
+      },
+    ],
     'desktop_args': ['--use-gpu-in-tests']
   },
   'angle_unittests': {
@@ -1011,6 +1021,9 @@ def tester_config_matches_tester(tester_name, tester_config, tc, is_fyi,
       return False
 
   if 'names' in tc:
+    # Give priority to matching the tester_name.
+    if tester_name in tc['names']:
+      return True
     if not tester_name in tc['names']:
       return False
   if 'os_types' in tc:
@@ -1055,6 +1068,9 @@ def generate_gtest(tester_name, tester_config, test, test_config, is_fyi):
   if 'tester_configs' in result:
     # Don't print the tester_configs in the JSON.
     result.pop('tester_configs')
+  if 'disabled_tester_configs' in result:
+    # Don't print the disabled_tester_configs in the JSON.
+    result.pop('disabled_tester_configs')
   if 'test' in result:
     result['name'] = test
   else:
