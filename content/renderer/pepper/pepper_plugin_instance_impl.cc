9d1c16fb39a59975adb3516f9c87dcee5b6ffcbe
dmazzoni <dmazzoni@chromium.org>
2016-07-19 00:32:31 -0700

    Null-check render_frame_ in PepperPluginInstanceImpl::Initialize
    BUG=627206
    
    Review-Url: https://codereview.chromium.org/2160973002
    Cr-Commit-Position: refs/heads/master@{#406229}
    
    Former-commit-id: d7411acd92d04753e83b4eef6e3a4021a9c6edb7

--- content/renderer/pepper/pepper_plugin_instance_impl.cc
+++ content/renderer/pepper/pepper_plugin_instance_impl.cc
@@ -890,8 +890,12 @@ bool PepperPluginInstanceImpl::Initialize(
       message_channel_->Start();
   }
 
-  if (render_frame_->render_accessibility() && LoadPdfInterface())
+  if (success &&
+      render_frame_ &&
+      render_frame_->render_accessibility() &&
+      LoadPdfInterface()) {
     plugin_pdf_interface_->EnableAccessibility(pp_instance());
+  }
 
   initialized_ = success;
   return success;
