75c2cc859d3547a02edff5387ec75d47ed41afd5
agable <agable@chromium.org>
2016-07-21 12:45:10 -0700

    Update make_docs.sh to use github asciidoc
    R=iannucci@chromium.org
    BUG=630312
    
    Review-Url: https://codereview.chromium.org/2165633007

--- man/src/make_docs.sh
+++ man/src/make_docs.sh
@@ -23,7 +23,7 @@ ensure_in_path() {
 }
 
 ensure_in_path xmlto
-ensure_in_path hg
+ensure_in_path git
 
 DFLT_CATALOG_PATH="/usr/local/etc/xml/catalog"
 if [[ ! $XML_CATALOG_FILES && -f "$DFLT_CATALOG_PATH" ]]
@@ -35,12 +35,12 @@ fi
 
 # We pull asciidoc to get the right version
 BRANCH=8.6.9
-ASCIIDOC_HASH=7fed0aff1b30
-if [[ ! -d asciidoc || $(cd asciidoc && hg id -i) != $ASCIIDOC_HASH ]]
+ASCIIDOC_HASH=2ff8681
+if [[ ! -d asciidoc || $(cd asciidoc && git rev-parse --short HEAD) != $ASCIIDOC_HASH ]]
 then
   echo Cloning asciidoc
   rm -rf asciidoc
-  hg clone -r $BRANCH https://asciidoc.googlecode.com/hg/ asciidoc
+  git clone --branch $BRANCH https://github.com/asciidoc/asciidoc asciidoc
   (cd asciidoc && ln -s asciidoc.py asciidoc)
 fi
 echo Asciidoc up to date at $ASCIIDOC_HASH \($BRANCH\)
