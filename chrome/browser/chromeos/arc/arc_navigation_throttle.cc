1bed4556f692f30a3bc5c65123692f81d4dae18a
kenobi <kenobi@chromium.org>
2016-07-22 13:49:28 -0700

    [arc-intents] Adjust link-click detection code.
    Use the page transition, which seems more reliable, rather than the
    HasUserGesture method, to detect whether a given navigation event was
    spawned by the user clicking on a link.
    
    BUG=630072
    
    Review-Url: https://codereview.chromium.org/2165193002
    Cr-Commit-Position: refs/heads/master@{#407256}
    
    Former-commit-id: ef206ad2f2bd182205b4adb5580c00e7967c4fd8

--- chrome/browser/chromeos/arc/arc_navigation_throttle.cc
+++ chrome/browser/chromeos/arc/arc_navigation_throttle.cc
@@ -16,6 +16,7 @@
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
+#include "ui/base/page_transition_types.h"
 
 namespace arc {
 
@@ -63,8 +64,22 @@ ArcNavigationThrottle::~ArcNavigationThrottle() = default;
 content::NavigationThrottle::ThrottleCheckResult
 ArcNavigationThrottle::WillStartRequest() {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  if (!navigation_handle()->HasUserGesture())
+
+  const ui::PageTransition transition =
+      navigation_handle()->GetPageTransition();
+
+  if (!ui::PageTransitionCoreTypeIs(transition, ui::PAGE_TRANSITION_LINK)) {
+    // If this navigation event wasn't spawned by the user clicking on a link.
     return content::NavigationThrottle::PROCEED;
+  }
+
+  if (ui::PageTransitionGetQualifier(transition) != 0) {
+    // Qualifiers indicate that this navigation was the result of a click on a
+    // forward/back button, or a redirect, or typing in the URL bar, etc.  Don't
+    // pass any of those types of navigations to the intent helper (see
+    // crbug.com/630072).
+    return content::NavigationThrottle::PROCEED;
+  }
 
   if (!ShouldOverrideUrlLoading(navigation_handle()))
     return content::NavigationThrottle::PROCEED;
