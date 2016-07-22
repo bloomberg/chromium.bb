ddf75ba291565de0acb8e704e4a88cb0928048b2
tedchoc <tedchoc@chromium.org>
2016-07-22 10:49:24 -0700

    Disable PaymentRequestDynamicShippingSingleAddressTest#testAddAddressAndPay on svelte
    Has never passed since it was introduced.  Disabling on low end
    for now.
    
    BUG=626289
    
    Review-Url: https://codereview.chromium.org/2170183004
    Cr-Commit-Position: refs/heads/master@{#407196}
    
    Former-commit-id: d577494f52c4d5a5347b5eee564769de34dffeda

--- chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestDynamicShippingSingleAddressTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/payments/PaymentRequestDynamicShippingSingleAddressTest.java
@@ -4,10 +4,13 @@
 
 package org.chromium.chrome.browser.payments;
 
+import static org.chromium.base.test.util.Restriction.RESTRICTION_TYPE_NON_LOW_END_DEVICE;
+
 import android.content.DialogInterface;
 import android.test.suitebuilder.annotation.MediumTest;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.autofill.AutofillTestHelper;
 import org.chromium.chrome.browser.autofill.PersonalDataManager.AutofillProfile;
@@ -77,6 +80,7 @@ public class PaymentRequestDynamicShippingSingleAddressTest extends PaymentReque
 
     /** Add a valid address and complete the transaction. */
     @MediumTest
+    @Restriction(RESTRICTION_TYPE_NON_LOW_END_DEVICE) // crbug.com/626289
     public void testAddAddressAndPay()
             throws InterruptedException, ExecutionException, TimeoutException {
         triggerUIAndWait(mReadyForInput);
