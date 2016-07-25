f2a9f832954449b926a96cc71640cb691dd1e609
dozsa <dozsa@google.com>
2016-07-25 03:07:18 -0700

    Reland: Redirect syncing users without sync passphrase to passwords.google.com
    This CL redirects syncing users without a sync passphrase to https://passwords.google.com instead of natively displaying passwords. The changes created in this CL are hidden behind the ViewPasswords feature.
    
    This is a reland of https://codereview.chromium.org/2092723002/
    
    BUG=623488
    
    Review-Url: https://codereview.chromium.org/2173713002
    Cr-Commit-Position: refs/heads/master@{#407440}
    
    Former-commit-id: 86fa8831dcb7154bd60f77a532953517797d4f10

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/MainPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/MainPreferences.java
@@ -4,24 +4,32 @@
 
 package org.chromium.chrome.browser.preferences;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
+import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.PasswordUIView;
 import org.chromium.chrome.browser.autofill.PersonalDataManager;
 import org.chromium.chrome.browser.net.spdyproxy.DataReductionProxySettings;
 import org.chromium.chrome.browser.partnercustomizations.HomepageManager;
 import org.chromium.chrome.browser.preferences.datareduction.DataReductionPreferences;
+import org.chromium.chrome.browser.preferences.password.SavePasswordsPreferences;
 import org.chromium.chrome.browser.signin.SigninManager;
 import org.chromium.chrome.browser.signin.SigninManager.SignInStateObserver;
+import org.chromium.chrome.browser.sync.ProfileSyncService;
+import org.chromium.sync.AndroidSyncSettings;
 
 /**
  * The main settings screen, shown when the user first opens Settings.
  */
-public class MainPreferences extends PreferenceFragment implements SignInStateObserver {
+public class MainPreferences extends PreferenceFragment implements SignInStateObserver,
+        Preference.OnPreferenceClickListener {
 
     public static final String PREF_SIGN_IN = "sign_in";
     public static final String PREF_SEARCH_ENGINE = "search_engine";
@@ -33,6 +41,11 @@ public class MainPreferences extends PreferenceFragment implements SignInStateOb
 
     public static final String ACCOUNT_PICKER_DIALOG_TAG = "account_picker_dialog_tag";
     public static final String EXTRA_SHOW_SEARCH_ENGINE_PICKER = "show_search_engine_picker";
+
+    public static final String PREF_MANAGE_ACCOUNT_LINK = "manage_account_link";
+
+    @VisibleForTesting
+    public static final String VIEW_PASSWORDS = "view-passwords";
 
     private SignInPreference mSignInPreference;
     private ManagedPreferenceDelegate mManagedPreferenceDelegate;
@@ -77,6 +90,16 @@ public class MainPreferences extends PreferenceFragment implements SignInStateOb
         clearSignInPref();
     }
 
+    @Override
+    public boolean onPreferenceClick(Preference preference) {
+        Intent intent = new Intent(
+                  Intent.ACTION_VIEW,
+                  Uri.parse(PasswordUIView.getAccountDashboardURL()));
+        intent.setPackage(getActivity().getPackageName());
+        getActivity().startActivity(intent);
+        return true;
+    }
+
     private void updatePreferences() {
         if (getPreferenceScreen() != null) getPreferenceScreen().removeAll();
         addPreferencesFromResource(R.xml.main_preferences);
@@ -88,13 +111,31 @@ public class MainPreferences extends PreferenceFragment implements SignInStateOb
 
         ChromeBasePreference passwordsPref =
                 (ChromeBasePreference) findPreference(PREF_SAVED_PASSWORDS);
-        if (PasswordUIView.shouldUseSmartLockBranding()) {
-            passwordsPref.setTitle(getResources().getString(
-                    R.string.prefs_smart_lock_for_passwords));
+
+        ProfileSyncService syncService = ProfileSyncService.get();
+
+        if (AndroidSyncSettings.isSyncEnabled(getActivity().getApplicationContext())
+                  && syncService.isBackendInitialized()
+                  && !syncService.isUsingSecondaryPassphrase()
+                  && ChromeFeatureList.isEnabled(VIEW_PASSWORDS)) {
+            passwordsPref.setKey(PREF_MANAGE_ACCOUNT_LINK);
+            passwordsPref.setTitle(R.string.redirect_to_passwords_text);
+            passwordsPref.setSummary(R.string.redirect_to_passwords_link);
+            passwordsPref.setOnPreferenceClickListener(this);
+            passwordsPref.setManagedPreferenceDelegate(null);
+        } else {
+            if (PasswordUIView.shouldUseSmartLockBranding()) {
+                passwordsPref.setTitle(getResources().getString(
+                         R.string.prefs_smart_lock_for_passwords));
+            } else {
+                passwordsPref.setTitle(getResources().getString(
+                          R.string.prefs_saved_passwords));
+            }
+            passwordsPref.setFragment(SavePasswordsPreferences.class.getCanonicalName());
+            setOnOffSummary(passwordsPref,
+                    PrefServiceBridge.getInstance().isRememberPasswordsEnabled());
+            passwordsPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
         }
-        setOnOffSummary(passwordsPref,
-                PrefServiceBridge.getInstance().isRememberPasswordsEnabled());
-        passwordsPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
 
         Preference homepagePref = findPreference(PREF_HOMEPAGE);
         if (HomepageManager.shouldShowHomepageSetting()) {
---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/preferences/PasswordViewingTypeTest.java
@@ -0,0 +1,165 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.preferences;
+
+import android.accounts.Account;
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.Feature;
+import org.chromium.chrome.browser.preferences.password.SavePasswordsPreferences;
+import org.chromium.chrome.browser.sync.ProfileSyncService;
+import org.chromium.content.browser.test.NativeLibraryTestBase;
+import org.chromium.sync.AndroidSyncSettings;
+import org.chromium.sync.signin.AccountManagerHelper;
+import org.chromium.sync.test.util.AccountHolder;
+import org.chromium.sync.test.util.MockAccountManager;
+import org.chromium.sync.test.util.MockSyncContentResolverDelegate;
+
+/**
+ * Tests for verifying whether users are presented with the correct option of viewing
+ * passwords according to the user group they belong to (syncing with sync passphrase,
+ * syncing without sync passsphrase, non-syncing).
+ */
+
+public class PasswordViewingTypeTest extends NativeLibraryTestBase {
+
+    private MainPreferences mMainPreferences;
+    private ChromeBasePreference mPasswordsPref;
+    private static final String DEFAULT_ACCOUNT = "test@gmail.com";
+    private Context mContext;
+    private MockSyncContentResolverDelegate mSyncContentResolverDelegate;
+    private String mAuthority;
+    private Account mAccount;
+    private MockAccountManager mAccountManager;
+
+    @Override
+    protected void setUp() throws Exception {
+
+        mSyncContentResolverDelegate = new MockSyncContentResolverDelegate();
+        mContext = getInstrumentation().getTargetContext();
+        mMainPreferences = (MainPreferences) startMainPreferences(getInstrumentation(),
+            mContext).getFragmentForTest();
+        mPasswordsPref = (ChromeBasePreference) mMainPreferences.findPreference(
+                MainPreferences.PREF_SAVED_PASSWORDS);
+        setupTestAccount(mContext);
+        AndroidSyncSettings.overrideForTests(mContext, mSyncContentResolverDelegate);
+        mAuthority = AndroidSyncSettings.getContractAuthority(mContext);
+        AndroidSyncSettings.updateAccount(mContext, mAccount);
+        super.setUp();
+        loadNativeLibraryAndInitBrowserProcess();
+    }
+
+    private void setupTestAccount(Context context) {
+        mAccountManager = new MockAccountManager(context, context);
+        AccountManagerHelper.overrideAccountManagerHelperForTests(context, mAccountManager);
+        mAccount = AccountManagerHelper.createAccountFromName("account@example.com");
+        AccountHolder.Builder accountHolder =
+                AccountHolder.create().account(mAccount).password("password").alwaysAccept(true);
+        mAccountManager.addAccountHolderExplicitly(accountHolder.build());
+    }
+
+    /**
+     * Launches the main preferences.
+     */
+    private static Preferences startMainPreferences(Instrumentation instrumentation,
+            final Context mContext) {
+        Intent intent = PreferencesLauncher.createIntentForSettingsPage(mContext,
+                MainPreferences.class.getName());
+        Activity activity = (Preferences) instrumentation.startActivitySync(intent);
+        return (Preferences) activity;
+    }
+
+    /**
+     * Override ProfileSyncService using FakeProfileSyncService.
+     */
+    private void overrideProfileSyncService(final boolean usingPassphrase) {
+        class FakeProfileSyncService extends ProfileSyncService {
+
+            @Override
+            public boolean isUsingSecondaryPassphrase() {
+                return usingPassphrase;
+            }
+
+            @Override
+            public boolean isBackendInitialized() {
+                return true;
+            }
+        }
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                ProfileSyncService.overrideForTests(new FakeProfileSyncService());
+            }
+        });
+    }
+
+    /**
+     * Turn syncability on/off.
+     */
+    private void setSyncability(boolean syncState) throws InterruptedException {
+
+        // Turn on syncability
+        mSyncContentResolverDelegate.setMasterSyncAutomatically(syncState);
+        mSyncContentResolverDelegate.waitForLastNotificationCompleted();
+
+        // First sync
+        mSyncContentResolverDelegate.setIsSyncable(mAccount, mAuthority, (syncState) ? 1 : 0);
+        mSyncContentResolverDelegate.waitForLastNotificationCompleted();
+
+        if (syncState) {
+            mSyncContentResolverDelegate.setSyncAutomatically(mAccount, mAuthority, syncState);
+            mSyncContentResolverDelegate.waitForLastNotificationCompleted();
+        }
+    }
+
+    /**
+     * Verifies that sync settings are being set up correctly in the case of redirecting users.
+     */
+    @SmallTest
+    @CommandLineFlags.Add("enable-features=" + MainPreferences.VIEW_PASSWORDS)
+    @Feature({"Sync"})
+    public void testUserRedirectSyncSettings() throws InterruptedException {
+        setSyncability(true);
+        overrideProfileSyncService(false);
+        assertTrue(AndroidSyncSettings.isSyncEnabled(mContext));
+        ThreadUtils.runOnUiThreadBlocking(new Runnable() {
+            @Override
+            public void run() {
+                assertTrue(ProfileSyncService.get().isBackendInitialized());
+                assertFalse(ProfileSyncService.get().isUsingSecondaryPassphrase());
+            }
+        });
+    }
+
+    /**
+     * Verifies that syncing users with a custom passphrase are allowed to
+     * natively view passwords.
+     */
+    @SmallTest
+    public void testSyncingNativePasswordView() throws InterruptedException {
+        setSyncability(true);
+        overrideProfileSyncService(true);
+        assertEquals(SavePasswordsPreferences.class.getCanonicalName(),
+                mPasswordsPref.getFragment());
+        assertNotNull(mMainPreferences.getActivity().getIntent());
+    }
+
+    /**
+     * Verifies that non-syncing users are allowed to natively view passwords.
+     */
+    @SmallTest
+    public void testNonSyncingNativePasswordView() throws InterruptedException {
+        setSyncability(false);
+        assertEquals(SavePasswordsPreferences.class.getCanonicalName(),
+                mPasswordsPref.getFragment());
+    }
+
+}
