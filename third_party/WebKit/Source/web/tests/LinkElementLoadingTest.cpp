36640cc1697e519de99fb4777f4965d30ecc8984
hayato <hayato@chromium.org>
2016-07-25 02:39:30 -0700

    Add a test for canceling a pending stylesheet loading of a link element
    This test should be added together with https://codereview.chromium.org/2171293002
    
    BUG=630141
    
    Review-Url: https://codereview.chromium.org/2173413002
    Cr-Commit-Position: refs/heads/master@{#407439}
    
    Former-commit-id: 0d90f310e3bdfc51391a7c34d1982c8f23845090

---
+++ third_party/WebKit/Source/web/tests/LinkElementLoadingTest.cpp
@@ -0,0 +1,42 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/dom/Document.h"
+#include "core/html/HTMLLinkElement.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "web/tests/sim/SimRequest.h"
+#include "web/tests/sim/SimTest.h"
+
+namespace blink {
+
+class LinkElementLoadingTest : public SimTest {
+};
+
+TEST_F(LinkElementLoadingTest, ShouldCancelLoadingStyleSheetIfLinkElementIsDisconnected)
+{
+    SimRequest mainResource("https://example.com/test.html", "text/html");
+    SimRequest cssResource("https://example.com/test.css", "text/css");
+
+    loadURL("https://example.com/test.html");
+
+    mainResource.start();
+    mainResource.write("<!DOCTYPE html><link id=link rel=stylesheet href=test.css>");
+
+    // Sheet is streaming in, but not ready yet.
+    cssResource.start();
+
+    // Remove a link element from a document
+    HTMLLinkElement* link = toHTMLLinkElement(document().getElementById("link"));
+    EXPECT_NE(nullptr, link);
+    link->remove();
+
+    // Finish the load.
+    cssResource.complete();
+    mainResource.finish();
+
+    // Link element's sheet loading should be canceled.
+    EXPECT_EQ(nullptr, link->sheet());
+}
+
+} // namespace blink
