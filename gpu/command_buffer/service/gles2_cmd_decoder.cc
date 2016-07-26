cc2ced6be249a1efcfdd60ec9541ade48eb43709
mpearson <mpearson@chromium.org>
2016-07-25 21:43:48 -0700

    Revert of current program can be null in ES2/ES3 contexts. (patchset #2 id:20001 of https://codereview.chromium.org/2174173002/ )
    Reason for revert:
    Caused webkit test failure on WebKit Win7 (dbg)
    https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Win7%20%28dbg%29/builds/6624
    
    unexpected_failures:
    printing/webgl-oversized-printing.html
    virtual/threaded/printing/webgl-oversized-printing.html
    
    https://storage.googleapis.com/chromium-layout-test-archives/WebKit_Win7__dbg_/6624/layout-test-results/results.html
    
    ---
    --- E:\b\rr\tmpymgexl\w\layout-test-results\printing/webgl-oversized-printing-expected.txt
    +++ E:\b\rr\tmpymgexl\w\layout-test-results\printing/webgl-oversized-printing-actual.txt
    @@ -1,5 +1,5 @@
    -PASS successfullyParsed is true
    +CONSOLE ERROR: line 13: Uncaught TypeError: Cannot read property 'clearColor' of null
    +FAIL successfullyParsed should be true. Was false.
    
     TEST COMPLETE
    -PASS Printed without crashing.
    ---
    
    ---
    --- E:\b\rr\tmpymgexl\w\layout-test-results\virtual/threaded/printing/webgl-oversized-printing-expected.txt
    +++ E:\b\rr\tmpymgexl\w\layout-test-results\virtual/threaded/printing/webgl-oversized-printing-actual.txt
    @@ -1,5 +1,5 @@
    -PASS successfullyParsed is true
    +CONSOLE ERROR: line 13: Uncaught TypeError: Cannot read property 'clearColor' of null
    +FAIL successfullyParsed should be true. Was false.
    
     TEST COMPLETE
    -PASS Printed without crashing.
    ---
    
    Original issue's description:
    > current program can be null in ES2/ES3 contexts.
    >
    > They are only required to be non null in WebGL.
    >
    > This also did some clean up and a minor optimization.
    >
    > BUG=630802
    > TEST=fuzzer case in the bug
    > R=piman@chromium.org
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    >
    > Committed: https://crrev.com/ae4fa44db9da743c1b0a69c7fbf9cceedbc8df8d [formerly 14ef40159115ca5059907835a34bcd1667af7631]
    > Cr-Commit-Position: refs/heads/master@{#407659}
    
    TBR=piman@chromium.org,yunchao.he@intel.com,zmo@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=630802
    
    Review-Url: https://codereview.chromium.org/2181193002
    Cr-Commit-Position: refs/heads/master@{#407711}
    
    Former-commit-id: e9c2cd979c8114b75bbc2640a43a1371d64f502b

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -3086,7 +3086,8 @@ bool GLES2DecoderImpl::Initialize(
   state_.indexed_uniform_buffer_bindings = new IndexedBufferBindingHost(
       group_->max_uniform_buffer_bindings(), needs_emulation);
 
-  state_.InitGenericAttribs(group_->max_vertex_attribs());
+  state_.InitGenericAttribBaseType(group_->max_vertex_attribs());
+  state_.attrib_values.resize(group_->max_vertex_attribs());
   vertex_array_manager_.reset(new VertexArrayManager());
 
   GLuint default_vertex_attrib_service_id = 0;
@@ -9144,34 +9145,21 @@ void GLES2DecoderImpl::RestoreStateForSimulatedFixedAttribs() {
 }
 
 bool GLES2DecoderImpl::AttribsTypeMatch() {
-  if (!state_.current_program.get())
-    return true;
-  const std::vector<uint32_t>& shader_attrib_active_mask =
-      state_.current_program->vertex_input_active_mask();
-  const std::vector<uint32_t>& shader_attrib_type_mask =
-      state_.current_program->vertex_input_base_type_mask();
-  const std::vector<uint32_t>& generic_vertex_attrib_type_mask =
-      state_.generic_attrib_base_type_mask();
-  const std::vector<uint32_t>& vertex_attrib_array_enabled_mask =
-      state_.vertex_attrib_manager->attrib_enabled_mask();
-  const std::vector<uint32_t>& vertex_attrib_array_type_mask =
-      state_.vertex_attrib_manager->attrib_base_type_mask();
-  DCHECK_EQ(shader_attrib_active_mask.size(),
-            shader_attrib_type_mask.size());
-  DCHECK_EQ(shader_attrib_active_mask.size(),
-            generic_vertex_attrib_type_mask.size());
-  DCHECK_EQ(shader_attrib_active_mask.size(),
-            vertex_attrib_array_enabled_mask.size());
-  DCHECK_EQ(shader_attrib_active_mask.size(),
-            vertex_attrib_array_type_mask.size());
-  for (size_t ii = 0; ii < shader_attrib_active_mask.size(); ++ii) {
-    uint32_t vertex_attrib_source_type_mask =
-        (~vertex_attrib_array_enabled_mask[ii] &
-         generic_vertex_attrib_type_mask[ii]) |
-        (vertex_attrib_array_enabled_mask[ii] &
-         vertex_attrib_array_type_mask[ii]);
-    if ((shader_attrib_type_mask[ii] & shader_attrib_active_mask[ii]) !=
-        (vertex_attrib_source_type_mask & shader_attrib_active_mask[ii])) {
+  for (uint32_t index = 0; index < group_->max_vertex_attribs(); index += 16) {
+    uint32_t shader_attrib_written_mask =
+        state_.current_program->vertex_input_type_written_mask(index);
+    uint32_t vao_attrib_enabled_mask =
+        state_.vertex_attrib_manager->attrib_enabled_mask(index);
+
+    uint32_t vertex_attrib_base_type_mask =
+        (~vao_attrib_enabled_mask &
+         state_.GetGenericVertexAttribBaseTypeMask(index)) |
+        (vao_attrib_enabled_mask &
+         state_.vertex_attrib_manager->attrib_base_type_mask(index));
+
+    if ((state_.current_program->vertex_input_base_type_mask(index)
+         & shader_attrib_written_mask) !=
+         (vertex_attrib_base_type_mask & shader_attrib_written_mask)) {
       return false;
     }
   }
