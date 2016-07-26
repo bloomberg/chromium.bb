b8b8e28c933013e7bf59969a11cc2c43f84c7b24
chcunningham <chcunningham@chromium.org>
2016-07-26 15:29:46 -0700

    Add gpu accelerated config changes to SmokeTests.
    SmokeTests are the subset of layout tests run on the android bots. This change
    adds coverage to several historically problematic config-change scenarios.
    
    These tests use a virtual suite that sets the flag "--use-gpu-in-tests". This
    flag overrides the default test behavior of using MesaGL, which is not capable
    of creating surfaces to use with gpu accelerated decoding (at least on Android).
    
    This change also fixes some test/expectations rot.
    
    BUG=555703
    TESTS=Config change LayoutTests.
    
    Review-Url: https://codereview.chromium.org/1644763002
    Cr-Commit-Position: refs/heads/master@{#407938}
    
    Former-commit-id: b8bf5eba6b6cd3623f16404c746251bf662d141d

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1511,6 +1511,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     // This flag needs to be propagated to the renderer process for
     // --in-process-webgl.
     switches::kUseGL,
+    switches::kUseGpuInTests,
     switches::kUseMobileUserAgent,
     switches::kUseRemoteCompositing,
     switches::kV,
