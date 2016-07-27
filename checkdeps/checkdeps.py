9b645dbe0bbc540ad62a877988c35f53ba8af0fa
John Budorick <jbudorick@google.com>
2016-07-26 17:55:58 -0700

    Run checkdeps for Java code as part of PRESUBMIT.py
    Add CheckLine, IsJavaFile in java_checker.py
    
    Linked wit CL https://codereview.chromium.org/2036163003
    
    BUG=611752
    R=brettw@chromium.org, jbudorick@chromium.org, nick@chromium.org
    
    Review URL: https://codereview.chromium.org/2036903003 .

--- checkdeps/checkdeps.py
+++ checkdeps/checkdeps.py
@@ -95,6 +95,39 @@ class DepsChecker(DepsBuilder):
         if file_status.HasViolations():
           self.results_formatter.AddError(file_status)
 
+  def CheckIncludesAndImports(self, added_lines, checker):
+    """Check new import/#include statements added in the change
+    being presubmit checked.
+
+    Args:
+      added_lines: ((file_path, (changed_line, changed_line, ...), ...)
+      checker: CppChecker/JavaChecker checker instance
+
+    Return:
+      A list of tuples, (bad_file_path, rule_type, rule_description)
+      where rule_type is one of Rule.DISALLOW or Rule.TEMP_ALLOW and
+      rule_description is human-readable. Empty if no problems.
+    """
+    problems = []
+    for file_path, changed_lines in added_lines:
+      if not checker.ShouldCheck(file_path):
+        continue
+      rules_for_file = self.GetDirectoryRules(os.path.dirname(file_path))
+      if not rules_for_file:
+        continue
+      for line in changed_lines:
+        is_include, violation = checker.CheckLine(
+            rules_for_file, line, file_path, True)
+        if not violation:
+          continue
+        rule_type = violation.violated_rule.allow
+        if rule_type == Rule.ALLOW:
+          continue
+        violation_text = results.NormalResultsFormatter.FormatViolation(
+            violation, self.verbose)
+        problems.append((file_path, rule_type, violation_text))
+    return problems
+
   def CheckAddedCppIncludes(self, added_includes):
     """This is used from PRESUBMIT.py to check new #include statements added in
     the change being presubmit checked.
@@ -107,27 +140,24 @@ class DepsChecker(DepsBuilder):
       where rule_type is one of Rule.DISALLOW or Rule.TEMP_ALLOW and
       rule_description is human-readable. Empty if no problems.
     """
-    cpp = cpp_checker.CppChecker(self.verbose, self._resolve_dotdot)
-    problems = []
-    for file_path, include_lines in added_includes:
-      if not cpp.IsCppFile(file_path):
-        continue
-      rules_for_file = self.GetDirectoryRules(os.path.dirname(file_path))
-      if not rules_for_file:
-        continue
-      for line in include_lines:
-        is_include, violation = cpp.CheckLine(
-            rules_for_file, line, file_path, True)
-        if not violation:
-          continue
-        rule_type = violation.violated_rule.allow
-        if rule_type == Rule.ALLOW:
-          continue
-        violation_text = results.NormalResultsFormatter.FormatViolation(
-            violation, self.verbose)
-        problems.append((file_path, rule_type, violation_text))
-    return problems
+    return self.CheckIncludesAndImports(
+        added_includes, cpp_checker.CppChecker(self.verbose))
 
+  def CheckAddedJavaImports(self, added_imports):
+    """This is used from PRESUBMIT.py to check new import statements added in
+    the change being presubmit checked.
+
+    Args:
+      added_imports: ((file_path, (import_line, import_line, ...), ...)
+
+    Return:
+      A list of tuples, (bad_file_path, rule_type, rule_description)
+      where rule_type is one of Rule.DISALLOW or Rule.TEMP_ALLOW and
+      rule_description is human-readable. Empty if no problems.
+    """
+    return self.CheckIncludesAndImports(
+        added_imports,
+        java_checker.JavaChecker(self.base_directory, self.verbose))
 
 def PrintUsage():
   print """Usage: python checkdeps.py [--root <root>] [tocheck]
