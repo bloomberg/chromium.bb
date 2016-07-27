fbca56956c84b92ba4fc8103fa7bbff9d6a943cb
sdefresne <sdefresne@chromium.org>
2016-07-27 01:45:57 -0700

    [GN] Prefer explicit pool to automatic link_pool for link targets.
    In preparation of refactoring the concurrent_links code to use
    explicit pools, prefer to use an explicit pool if defined over
    the automatic link_pool for link targets.
    
    BUG=612786
    
    Review-Url: https://codereview.chromium.org/2182093002
    Cr-Commit-Position: refs/heads/master@{#408077}
    
    Former-commit-id: 1c27131e1bf77b059ef4c4f444bdccecdddeb0f6

--- tools/gn/ninja_toolchain_writer.cc
+++ tools/gn/ninja_toolchain_writer.cc
@@ -105,16 +105,17 @@ void NinjaToolchainWriter::WriteToolRule(const Toolchain::ToolType type,
     out_ << kIndent << "deps = msvc" << std::endl;
   }
 
-  // The link pool applies to linker tools. Don't count TYPE_ALINK since
-  // static libraries are not generally intensive to write.
-  if (type == Toolchain::TYPE_SOLINK ||
-      type == Toolchain::TYPE_SOLINK_MODULE ||
-      type == Toolchain::TYPE_LINK) {
-    out_ << kIndent << "pool = link_pool\n";
-  } else if (tool->pool().ptr) {
+  // Use pool is specified.
+  if (tool->pool().ptr) {
     std::string pool_name =
         tool->pool().ptr->GetNinjaName(settings_->default_toolchain_label());
     out_ << kIndent << "pool = " << pool_name << std::endl;
+  } else if (type == Toolchain::TYPE_SOLINK ||
+      type == Toolchain::TYPE_SOLINK_MODULE ||
+      type == Toolchain::TYPE_LINK) {
+    // The link pool applies to linker tools. Don't count TYPE_ALINK since
+    // static libraries are not generally intensive to write.
+    out_ << kIndent << "pool = link_pool\n";
   }
 
   if (tool->restat())
