ac6aff18b0d056fa2e59e893c2af86c5f6b5e26a
robliao <robliao@chromium.org>
2016-07-27 12:54:22 -0700

    Audit and Deprecate DPI Functions in display::win
    SetDefaultDeviceScaleFactor is deprecated in favor of --force-device-scale-factor
    
    GetDPIScale and GetSystemMetricsInDIP are deprecated in favor of their equivalent per-HWND DPI aware replacements in ScreenWin.
    
    BUG=426656
    
    Review-Url: https://codereview.chromium.org/2182333005
    Cr-Commit-Position: refs/heads/master@{#408218}
    
    Former-commit-id: 34a04bf6f2c6969f4d038034b259adc6c08bae60

--- ui/display/win/dpi.h
+++ ui/display/win/dpi.h
@@ -11,14 +11,16 @@
 namespace display {
 namespace win {
 
+// Deprecated. Use --force-device-scale-factor instead.
+//
 // Sets the device scale factor that will be used unless overridden on the
-// command line by --force-device-scale-factor.  If this is not called, and that
-// flag is not used, the scale factor used by the DIP conversion functions below
-// will be that returned by GetDPIScale().
+// command line by --force-device-scale-factor.
 DISPLAY_EXPORT void SetDefaultDeviceScaleFactor(float scale);
 
-// Gets the scale factor of the display. For example, if the display DPI is
-// 96 then the scale factor is 1.0.
+// Deprecated. Use display::win::ScreenWin::GetScaleFactorForHWND instead.
+//
+// Gets the system's scale factor. For example, if the system DPI is 96 then the
+// scale factor is 1.0. This does not handle per-monitor DPI.
 DISPLAY_EXPORT float GetDPIScale();
 
 // Returns the equivalent DPI for |device_scaling_factor|.
@@ -27,8 +29,11 @@ DISPLAY_EXPORT int GetDPIFromScalingFactor(float device_scaling_factor);
 // Returns the equivalent scaling factor for |dpi|.
 DISPLAY_EXPORT float GetScalingFactorFromDPI(int dpi);
 
+// Deprecated. Use display::win::ScreenWin::GetSystemMetricsForHwnd instead.
+//
 // Win32's GetSystemMetrics uses pixel measures. This function calls
-// GetSystemMetrics for the given |metric|, then converts the result to DIP.
+// GetSystemMetrics for the given |metric|, then converts the result using the
+// system's DPI to DIP. This does not handle per-monitor DPI.
 DISPLAY_EXPORT int GetSystemMetricsInDIP(int metric);
 
 }  // namespace win
