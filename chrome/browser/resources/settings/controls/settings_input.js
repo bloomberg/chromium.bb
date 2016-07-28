7a0b61b5e1a695fc6e1900fdbbd6eeb344721d68
dschuyler <dschuyler@chromium.org>
2016-07-28 12:14:56 -0700

    [MD settings] propagate stopKeyboardEventPropagation to paper-input
    This CL passes stopKeyboardEventPropagation from settings-input to the
    underlying paper-input and then uses that property to keep the focus
    in the home page url setting.
    
    BUG=621480
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2187713003
    Cr-Commit-Position: refs/heads/master@{#408451}
    
    Former-commit-id: 0ba8786cfbc72edbdb38e37bcc9b29d0ac0696ee

--- chrome/browser/resources/settings/controls/settings_input.js
+++ chrome/browser/resources/settings/controls/settings_input.js
@@ -22,43 +22,29 @@ Polymer({
       observer: 'prefChanged_'
     },
 
-    /**
-     * The current value of the input, reflected to/from |pref|.
-     */
+    /* The current value of the input, reflected to/from |pref|. */
     value: {
       type: String,
       value: '',
       notify: true,
     },
 
-    /**
-     * Set to true to disable editing the input.
-     */
+    /* Set to true to disable editing the input. */
     disabled: {
       type: Boolean,
       value: false,
       reflectToAttribute: true
     },
 
-    /** Propagate the errorMessage property. */
+    /* Properties for paper-input. This is not strictly necessary.
+     * Though it does define the types for the closure compiler. */
     errorMessage: { type: String },
-
-    /** Propagate the label property. */
     label: { type: String },
-
-    /** Propagate the no-label-float property. */
     noLabelFloat: { type: Boolean, value: false },
-
-    /** Propagate the pattern property. */
     pattern: { type: String },
-
-    /** Propagate the readonly property. */
     readonly: { type: Boolean, value: false },
-
-    /** Propagate the required property. */
     required: { type: Boolean, value: false },
-
-    /** Propagate the type property. */
+    stopKeyboardEventPropagation: { type: Boolean, value: false },
     type: { type: String },
   },
 
