4f046b6c6680feef19aaddd38dbc5d3c168675a0
yukishiino <yukishiino@chromium.org>
2016-07-28 05:14:34 -0700

    binding: Disallows to run a function if its context is detached.
    There are two contexts when about to run a ScheduledAction.
    |this|'s context and the context of the function to be run.
    If the latter context no longer has a valid V8PerContextData,
    |self| returns null.
        function() { self.testRunner; } // self will be null
    
    This issue has been there for a long time, but
    https://crrev.com/2049493005 made it easy to happen.
    
    This CL disallows to run a function if the function's context is
    detached.  This is not the right fix, but close to the behavior
    before https://crrev.com/2049493005 .
    
    The right fix is that, per spec, |self| always returns the global
    proxy object that never be null.  I.e. we need to support detached
    windows.
    
    BUG=630996
    
    Review-Url: https://codereview.chromium.org/2191543002
    Cr-Commit-Position: refs/heads/master@{#408376}
    
    Former-commit-id: 981ce039703c0350086568b8c922bee8b8ca2761

--- third_party/WebKit/Source/bindings/core/v8/ScheduledAction.cpp
+++ third_party/WebKit/Source/bindings/core/v8/ScheduledAction.cpp
@@ -114,9 +114,15 @@ void ScheduledAction::execute(LocalFrame* frame)
     ScriptState::Scope scope(m_scriptState.get());
     if (!m_function.isEmpty()) {
         DVLOG(1) << "ScheduledAction::execute " << this << ": have function";
+        v8::Local<v8::Function> function = m_function.newLocal(m_scriptState->isolate());
+        ScriptState* scriptStateForFunc = ScriptState::from(function->CreationContext());
+        if (!scriptStateForFunc->contextIsValid()) {
+            DVLOG(1) << "ScheduledAction::execute " << this << ": function's context is empty";
+            return;
+        }
         Vector<v8::Local<v8::Value>> info;
         createLocalHandlesForArgs(&info);
-        V8ScriptRunner::callFunction(m_function.newLocal(m_scriptState->isolate()), frame->document(), m_scriptState->context()->Global(), info.size(), info.data(), m_scriptState->isolate());
+        V8ScriptRunner::callFunction(function, frame->document(), m_scriptState->context()->Global(), info.size(), info.data(), m_scriptState->isolate());
     } else {
         DVLOG(1) << "ScheduledAction::execute " << this << ": executing from source";
         frame->script().executeScriptAndReturnValue(m_scriptState->context(), ScriptSourceCode(m_code));
@@ -128,12 +134,23 @@ void ScheduledAction::execute(LocalFrame* frame)
 void ScheduledAction::execute(WorkerGlobalScope* worker)
 {
     ASSERT(worker->thread()->isCurrentThread());
-    ASSERT(m_scriptState->contextIsValid());
+
+    if (!m_scriptState->contextIsValid()) {
+        DVLOG(1) << "ScheduledAction::execute " << this << ": context is empty";
+        return;
+    }
+
     if (!m_function.isEmpty()) {
         ScriptState::Scope scope(m_scriptState.get());
+        v8::Local<v8::Function> function = m_function.newLocal(m_scriptState->isolate());
+        ScriptState* scriptStateForFunc = ScriptState::from(function->CreationContext());
+        if (!scriptStateForFunc->contextIsValid()) {
+            DVLOG(1) << "ScheduledAction::execute " << this << ": function's context is empty";
+            return;
+        }
         Vector<v8::Local<v8::Value>> info;
         createLocalHandlesForArgs(&info);
-        V8ScriptRunner::callFunction(m_function.newLocal(m_scriptState->isolate()), worker, m_scriptState->context()->Global(), info.size(), info.data(), m_scriptState->isolate());
+        V8ScriptRunner::callFunction(function, worker, m_scriptState->context()->Global(), info.size(), info.data(), m_scriptState->isolate());
     } else {
         worker->scriptController()->evaluate(m_code);
     }
