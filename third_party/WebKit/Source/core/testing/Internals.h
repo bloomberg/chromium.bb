717cc51049557e29b3004428cf4b547c334087f0
xiaochengh <xiaochengh@chromium.org>
2016-07-28 18:08:54 -0700

    Remove hasGrammarMarker from internals
    BUG=619452
    TEST=n/a; no behavior change
    
    Review-Url: https://codereview.chromium.org/2191803002
    Cr-Commit-Position: refs/heads/master@{#408541}
    
    Former-commit-id: bce93917093ec0d1acd2de4c769ffd18c7e3c075

--- third_party/WebKit/Source/core/testing/Internals.h
+++ third_party/WebKit/Source/core/testing/Internals.h
@@ -209,7 +209,6 @@ public:
         unsigned bottomPadding, unsigned leftPadding, bool ignoreClipping, bool allowChildFrameContent, ExceptionState&) const;
 
     bool hasSpellingMarker(Document*, int from, int length);
-    bool hasGrammarMarker(Document*, int from, int length);
     void setContinuousSpellCheckingEnabled(bool);
 
     bool canHyphenate(const AtomicString& locale);
