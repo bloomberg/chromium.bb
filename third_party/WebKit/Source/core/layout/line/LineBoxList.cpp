0069e8ec1e43dbe090d8fb5e3f584deee869e936
robhogan <robhogan@gmail.com>
2016-07-29 01:00:19 -0700

    Skip dirtying lines when attaching children prior to attaching oneself
    If a parent is about to be (re)attached then there is no point in keeping its
    dirty lines up to date as we attach children. The parent will get a full layout
    anyway and we end up having to dirty the lineboxes for every inlines we add - which
    can get expensive if there are a lot of inlines.
    
    BUG=345972
    
    Review-Url: https://codereview.chromium.org/1550513003
    Cr-Commit-Position: refs/heads/master@{#408601}
    
    Former-commit-id: dcb4562e8f888588dfe686b1b9d864e39b3dc5cb

--- third_party/WebKit/Source/core/layout/line/LineBoxList.cpp
+++ third_party/WebKit/Source/core/layout/line/LineBoxList.cpp
@@ -234,6 +234,12 @@ void LineBoxList::dirtyLinesFromChangedChild(LineLayoutItem container, LineLayou
         return;
 
     LineLayoutInline inlineContainer = container.isLayoutInline() ? LineLayoutInline(container) : LineLayoutInline();
+
+    // If we are attaching children dirtying lines is unnecessary as we will do a full layout
+    // of the inline's contents anyway.
+    if (inlineContainer && inlineContainer.node() && inlineContainer.node()->needsAttach())
+        return;
+
     InlineBox* firstBox = inlineContainer ? inlineContainer.firstLineBoxIncludingCulling() : firstLineBox();
 
     // If we have no first line box, then just bail early.
