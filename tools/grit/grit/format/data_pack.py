92584b4ec569f15dbd21fc3792b8a6e3ae252264
estevenson <estevenson@chromium.org>
2016-08-03 07:55:10 -0700

    Enable whitelist generation for official builds.
    Currently, all resources are included in PAK files when Chrome is
    built locally. Only official_buildbot.sh uses a resource whitelist. This CL
    enables local builds to use resource whitelisting by setting the
    enable_resource_whitelist_generation gn flag to true, or by building an
    official build.
    
    This will allow developers to more easily monitor the changes in APK size
    for each commit they make.
    
    However, a large amount of output is generated (_pragma is used to
    create warnings to allow whitelisted resources to be listed), so for now
    the whitelist will only be generated for official builds.
    
    Comparison of ChromePublic.apk_InstallSize APK size using resource_sizes.py:
    * Local build with this patch: 73338275 bytes
    * Local build without this patch: 75248223 bytes
    
    BUG=632385
    
    Review-Url: https://codereview.chromium.org/2175413004
    Cr-Commit-Position: refs/heads/master@{#409511}
    
    Former-commit-id: 3f3fa5fc415baab5a875839e6520c2cbb245ec38

--- tools/grit/grit/format/data_pack.py
+++ tools/grit/grit/format/data_pack.py
@@ -109,7 +109,8 @@ def WriteDataPack(resources, output_file, encoding):
     file.write(content)
 
 
-def RePack(output_file, input_files, whitelist_file=None):
+def RePack(output_file, input_files, whitelist_file=None,
+           suppress_removed_key_output=False):
   """Write a new data pack file by combining input pack files.
 
   Args:
@@ -118,6 +119,8 @@ def RePack(output_file, input_files, whitelist_file=None):
       whitelist_file: path to the file that contains the list of resource IDs
                       that should be kept in the output file or None to include
                       all resources.
+      suppress_removed_key_output: allows the caller to suppress the output from
+                                   RePackFromDataPackStrings.
 
   Raises:
       KeyError: if there are duplicate keys or resource encoding is
@@ -128,17 +131,20 @@ def RePack(output_file, input_files, whitelist_file=None):
   if whitelist_file:
     whitelist = util.ReadFile(whitelist_file, util.RAW_TEXT).strip().split('\n')
     whitelist = set(map(int, whitelist))
-  resources, encoding = RePackFromDataPackStrings(input_data_packs, whitelist)
+  resources, encoding = RePackFromDataPackStrings(
+      input_data_packs, whitelist, suppress_removed_key_output)
   WriteDataPack(resources, output_file, encoding)
 
 
-def RePackFromDataPackStrings(inputs, whitelist):
+def RePackFromDataPackStrings(inputs, whitelist,
+                              suppress_removed_key_output=False):
   """Returns a data pack string that combines the resources from inputs.
 
   Args:
       inputs: a list of data pack strings that need to be combined.
       whitelist: a list of resource IDs that should be kept in the output string
                  or None to include all resources.
+      suppress_removed_key_output: Do not print removed keys.
 
   Returns:
       DataPackContents: a tuple containing the new combined data pack and its
@@ -170,8 +176,9 @@ def RePackFromDataPackStrings(inputs, whitelist):
       resources.update(whitelisted_resources)
       removed_keys = [key for key in content.resources.keys()
                       if key not in whitelist]
-      for key in removed_keys:
-        print 'RePackFromDataPackStrings Removed Key:', key
+      if not suppress_removed_key_output:
+        for key in removed_keys:
+          print 'RePackFromDataPackStrings Removed Key:', key
     else:
       resources.update(content.resources)
 
