ae16636e3a01961fcc6020437278c80948345827
rtenneti <rtenneti@chromium.org>
2016-08-03 18:58:33 -0700

    QUIC - Race Cert Verification with host resolution if certs are availiable when race_cert_verification network session param is enabled.
    This experiment is not enabled in chromium.
    
    R=eroman@chromium.org, mef@chromium.org, rch@chromium.org,
    TBR=eroman@chromium.org (for net-internals changes)
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2120703003
    Cr-Commit-Position: refs/heads/master@{#409701}
    
    Former-commit-id: d073dd2ac386ae7893f03cd7659abb66f045e5fa

--- components/cronet/android/test/javatests/src/org/chromium/net/QuicTest.java
+++ components/cronet/android/test/javatests/src/org/chromium/net/QuicTest.java
@@ -51,7 +51,8 @@ public class QuicTest extends CronetTestBase {
                                         .put("idle_connection_timeout_seconds", 300)
                                         .put("close_sessions_on_ip_change", false)
                                         .put("migrate_sessions_on_network_change", true)
-                                        .put("migrate_sessions_early", true);
+                                        .put("migrate_sessions_early", true)
+                                        .put("race_cert_verification", true);
         JSONObject experimentalOptions = new JSONObject().put("QUIC", quicParams);
         mBuilder.setExperimentalOptions(experimentalOptions.toString());
         mBuilder.setMockCertVerifierForTesting(QuicTestServer.createMockCertVerifier());
