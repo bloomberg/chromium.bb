19c6b1f0326254d8d7ae6022e6a57df5f5accfc8
petrcermak <petrcermak@chromium.org>
2016-08-04 12:21:28 -0700

    [system-health] Update ID of bug blocking browse:news:cnn in desktop smoke tests
    The original bug that caused the story to fail is now fixed, but another
    one is still causing failures.
    
    BUG=622409,634389
    
    Review-Url: https://codereview.chromium.org/2210083003
    Cr-Commit-Position: refs/heads/master@{#409862}
    
    Former-commit-id: a44d109020cd88b093298afdbceacfe2a5f52502

--- tools/perf/benchmarks/system_health_smoke_test.py
+++ tools/perf/benchmarks/system_health_smoke_test.py
@@ -32,7 +32,7 @@ def GetSystemHealthBenchmarksToSmokeTest():
 
 
 _DISABLED_TESTS = frozenset({
-  # crbug.com/622409
+  # crbug.com/634389
   'benchmarks.system_health_smoke_test.SystemHealthBenchmarkSmokeTest.system_health.memory_desktop.browse:news:cnn',  # pylint: disable=line-too-long
   # crbug.com/629123
   'benchmarks.system_health_smoke_test.SystemHealthBenchmarkSmokeTest.system_health.memory_mobile.browse:news:hackernews',  # pylint: disable=line-too-long
