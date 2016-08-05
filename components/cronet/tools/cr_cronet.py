1f0560441d99f145101b6275787ee1f392e57fbf
mgersh <mgersh@chromium.org>
2016-08-05 08:22:40 -0700

    Add is_component_build=false to cronet gn args
    When debug builds were switched to component builds by default, cronet
    builds using the args in cr_cronet.py stopped working. This fixes it.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2217163002
    Cr-Commit-Position: refs/heads/master@{#410064}
    
    Former-commit-id: cbccecfd145776679aecf24c5b03bcd459a454f1

--- components/cronet/tools/cr_cronet.py
+++ components/cronet/tools/cr_cronet.py
@@ -99,7 +99,7 @@ def main():
   gn_args += 'target_os="' + target_os + '" enable_websockets=false '+ \
       'disable_file_support=true disable_ftp_support=true '+ \
       'use_platform_icu_alternatives=true '+ \
-      'disable_brotli_filter=true'
+      'disable_brotli_filter=true is_component_build=false'
 
   extra_options = ' '.join(extra_options_list)
   if options.gn:
