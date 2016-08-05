94c97ece687d9caf6062a37840e464c8d25745b9
beaufort.francois <beaufort.francois@gmail.com>
2016-08-05 15:37:46 -0700

    bluetooth: {start,stop}Notifications() return the characteristic
    See Spec PR at https://github.com/WebBluetoothCG/web-bluetooth/pull/264
    
    BUG=529560
    
    Review-Url: https://codereview.chromium.org/2216623002
    Cr-Commit-Position: refs/heads/master@{#410201}
    
    Former-commit-id: e1ce2f1c64c95ca852601407c15906b19ca26d03

--- third_party/WebKit/Source/modules/bluetooth/BluetoothRemoteGATTCharacteristic.cpp
+++ third_party/WebKit/Source/modules/bluetooth/BluetoothRemoteGATTCharacteristic.cpp
@@ -195,12 +195,36 @@ ScriptPromise BluetoothRemoteGATTCharacteristic::writeValue(ScriptState* scriptS
     return promise;
 }
 
+class NotificationsCallback : public WebBluetoothNotificationsCallbacks {
+public:
+    NotificationsCallback(BluetoothRemoteGATTCharacteristic* characteristic, ScriptPromiseResolver* resolver) : m_webCharacteristic(characteristic), m_resolver(resolver) {}
+
+    void onSuccess() override
+    {
+        if (!m_resolver->getExecutionContext() || m_resolver->getExecutionContext()->activeDOMObjectsAreStopped())
+            return;
+
+        m_resolver->resolve(m_webCharacteristic);
+    }
+
+    void onError(int32_t error /* Corresponds to WebBluetoothError in web_bluetooth.mojom */) override
+    {
+        if (!m_resolver->getExecutionContext() || m_resolver->getExecutionContext()->activeDOMObjectsAreStopped())
+            return;
+        m_resolver->reject(BluetoothError::take(m_resolver, error));
+    }
+
+private:
+    Persistent<BluetoothRemoteGATTCharacteristic> m_webCharacteristic;
+    Persistent<ScriptPromiseResolver> m_resolver;
+};
+
 ScriptPromise BluetoothRemoteGATTCharacteristic::startNotifications(ScriptState* scriptState)
 {
     WebBluetooth* webbluetooth = BluetoothSupplement::fromScriptState(scriptState);
     ScriptPromiseResolver* resolver = ScriptPromiseResolver::create(scriptState);
     ScriptPromise promise = resolver->promise();
-    webbluetooth->startNotifications(m_webCharacteristic->characteristicInstanceID, new CallbackPromiseAdapter<void, BluetoothError>(resolver));
+    webbluetooth->startNotifications(m_webCharacteristic->characteristicInstanceID, new NotificationsCallback(this, resolver));
     return promise;
 }
 
@@ -217,7 +241,7 @@ ScriptPromise BluetoothRemoteGATTCharacteristic::stopNotifications(ScriptState* 
     WebBluetooth* webbluetooth = BluetoothSupplement::fromScriptState(scriptState);
     ScriptPromiseResolver* resolver = ScriptPromiseResolver::create(scriptState);
     ScriptPromise promise = resolver->promise();
-    webbluetooth->stopNotifications(m_webCharacteristic->characteristicInstanceID, new CallbackPromiseAdapter<void, BluetoothError>(resolver));
+    webbluetooth->stopNotifications(m_webCharacteristic->characteristicInstanceID, new NotificationsCallback(this, resolver));
     return promise;
 }
 
