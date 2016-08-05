5c086ed54b86de4562f661a0bc457b6be1886b79
nainar <nainar@chromium.org>
2016-08-05 14:49:30 -0700

    Resize is being triggered too early in Print code.
    Currently the ResizeForPrinting() function is called too early in
    StartPrinting(). The resize causes transitions to be triggered. This
    change delays the call to ResizeForPrinting() till after the call to
    frame()->printBegin().
    
    BUG=567317
    
    Review-Url: https://codereview.chromium.org/2089373002
    Cr-Commit-Position: refs/heads/master@{#410175}
    
    Former-commit-id: a7760a764855fb5678f7e5e88e11b834416ffbb7

--- components/printing/renderer/print_web_view_helper.cc
+++ components/printing/renderer/print_web_view_helper.cc
@@ -703,11 +703,11 @@ void PrepareFrameAndViewForPrint::ResizeForPrinting() {
 }
 
 void PrepareFrameAndViewForPrint::StartPrinting() {
-  ResizeForPrinting();
   blink::WebView* web_view = frame_.view();
   web_view->settings()->setShouldPrintBackgrounds(should_print_backgrounds_);
   expected_pages_count_ =
       frame()->printBegin(web_print_params_, node_to_print_);
+  ResizeForPrinting();
   is_printing_started_ = true;
 }
 
