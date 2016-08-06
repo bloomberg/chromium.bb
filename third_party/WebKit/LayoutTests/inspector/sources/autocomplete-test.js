dd2478a728022eb477a6cc4dfac116e7c9b6ffb8
einbinder <einbinder@chromium.org>
2016-08-05 18:22:10 -0700

    DevTools: Hint text in sources autocomplete
    This is in preparation for CodeMirror console prompt.
    
    Review-Url: https://codereview.chromium.org/2189233002
    Cr-Commit-Position: refs/heads/master@{#410242}
    
    Former-commit-id: a74adcf14186edb9462bbb6cef7eca1b3867dcdd

--- third_party/WebKit/LayoutTests/inspector/sources/autocomplete-test.js
+++ third_party/WebKit/LayoutTests/inspector/sources/autocomplete-test.js
@@ -1,27 +1,33 @@
 function initialize_AutocompleteTest()
 {
 
-InspectorTest.dumpSuggestions = function(textEditor, lines) {
-    var lineNumber = -1, columnNumber;
-    for (var i = 0; i < lines.length; ++i) {
-        var columnNumber = lines[i].indexOf("|");
-        if (columnNumber !== -1) {
-            lineNumber = i;
-            break;
+    InspectorTest.dumpSuggestions = function(textEditor, lines)
+    {
+        var resolve;
+        var promise = new Promise(fulfill => resolve = fulfill);
+        var lineNumber = -1, columnNumber;
+        for (var i = 0; i < lines.length; ++i) {
+            var columnNumber = lines[i].indexOf("|");
+            if (columnNumber !== -1) {
+                lineNumber = i;
+                break;
+            }
         }
+        if (lineNumber === -1)
+            throw new Error("Test case is invalid: cursor position is not marked with '|' symbol.");
+        textEditor.setText(lines.join("\n").replace("|", ""));
+        textEditor.setSelection(WebInspector.TextRange.createFromLocation(lineNumber, columnNumber));
+        InspectorTest.addSniffer(WebInspector.TextEditorAutocompleteController.prototype, "_onSuggestionsShownForTest", suggestionsShown);
+        textEditor._autocompleteController.autocomplete();
+        function suggestionsShown(words)
+        {
+            InspectorTest.addResult("========= Selection In Editor =========");
+            InspectorTest.dumpTextWithSelection(textEditor);
+            InspectorTest.addResult("======= Autocomplete Suggestions =======");
+            InspectorTest.addResult("[" + words.map(item => item.title).join(", ") + "]");
+            resolve();
+        }
+        return promise;
     }
-    if (lineNumber === -1)
-        throw new Error("Test case is invalid: cursor position is not marked with '|' symbol.");
-    textEditor.setText(lines.join("\n").replace("|", ""));
-    textEditor.setSelection(WebInspector.TextRange.createFromLocation(lineNumber, columnNumber));
-    var suggestions = [];
-    InspectorTest.addSniffer(WebInspector.TextEditorAutocompleteController.prototype, "_onSuggestionsShownForTest", function (words) { suggestions = words; });
-    textEditor._autocompleteController.autocomplete();
-
-    InspectorTest.addResult("========= Selection In Editor =========");
-    InspectorTest.dumpTextWithSelection(textEditor);
-    InspectorTest.addResult("======= Autocomplete Suggestions =======");
-    InspectorTest.addResult("[" + suggestions.join(", ") + "]");
-}
 
 }
