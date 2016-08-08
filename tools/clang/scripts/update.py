c3eb5e8c568935aa98683786f01b0b1bba37b8e2
thakis <thakis@chromium.org>
2016-08-07 19:19:58 -0700

    Roll clang 274369:277962.
    Ran `tools/clang/scripts/upload_revision.py 277909`.
    
    New:
    * version number is now 4.0.0
    * much better debug info on Windows
    * asan/win runtime detects EXCEPTION_ILLEGAL_INSTRUCTION / EXCEPTION_BREAKPOINT
    * improved integrated assembler on arm
    * sanitizer coverage improvements
    * lto devirtualization fixes
    * …and over 3500 other changes
    
    This is the first clang roll with gn as default meta-buildsystem. With gyp,
    all .o files got automatically rebuilt after clang rolls. With gn, this doesn't
    always work (https://crbug.com/612340). If you're getting errors about
    having to rebuild pch files after this roll (on Mac and Windows), manually
    run `gn gen out/gn` (or whatever your build dir is) and then build again.
    
    BUG=629966
    TBR=hans
    
    Review-Url: https://codereview.chromium.org/2220793002
    Cr-Commit-Position: refs/heads/master@{#410289}
    
    Former-commit-id: 78f40fbff7fe6ea54b0de1492268f4f5f76f6eb6

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -27,7 +27,7 @@ import zipfile
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # https://chromium.googlesource.com/chromium/src/+/master/docs/updating_clang.md
 # Reverting problematic clang rolls is safe, though.
-CLANG_REVISION = '274369'
+CLANG_REVISION = '277962'
 
 use_head_revision = 'LLVM_FORCE_HEAD_REVISION' in os.environ
 if use_head_revision:
@@ -71,7 +71,7 @@ BINUTILS_BIN_DIR = os.path.join(BINUTILS_DIR, BINUTILS_DIR,
                                 'Linux_x64', 'Release', 'bin')
 BFD_PLUGINS_DIR = os.path.join(BINUTILS_DIR, 'Linux_x64', 'Release',
                                'lib', 'bfd-plugins')
-VERSION = '3.9.0'
+VERSION = '4.0.0'
 ANDROID_NDK_DIR = os.path.join(
     CHROMIUM_DIR, 'third_party', 'android_tools', 'ndk')
 
@@ -853,11 +853,6 @@ def main():
     if re.search(r'\b(make_clang_dir)=', os.environ.get('GYP_DEFINES', '')):
       print 'Skipping Clang update (make_clang_dir= was set in GYP_DEFINES).'
       return 0
-
-  if use_head_revision:
-    # TODO(hans): Remove after the next roll.
-    global VERSION
-    VERSION = '4.0.0'
 
   global CLANG_REVISION, PACKAGE_VERSION
   if args.print_revision:
