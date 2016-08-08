995d99d22d9f1e12b0f6ead4d79c1157f62b3ff2
lizeb <lizeb@chromium.org>
2016-08-08 13:36:39 -0700

    customtabs: Remove unused Broadcast receiver.
    Review-Url: https://codereview.chromium.org/2203333002
    Cr-Commit-Position: refs/heads/master@{#410444}
    
    Former-commit-id: 9c498fff4d6dd7a43f6a0918aff32e255d3e6ad5

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -12,7 +12,6 @@ import android.app.Instrumentation;
 import android.app.Instrumentation.ActivityMonitor;
 import android.app.Instrumentation.ActivityResult;
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -90,25 +89,6 @@ import java.util.concurrent.atomic.AtomicReference;
  * Instrumentation tests for app menu, context menu, and toolbar of a {@link CustomTabActivity}.
  */
 public class CustomTabActivityTest extends CustomTabActivityTestBase {
-
-    /**
-     * An empty {@link BroadcastReceiver} that exists only to make the PendingIntent to carry an
-     * explicit intent. Otherwise the framework will not send it after {@link PendingIntent#send()}.
-     */
-    public class DummyBroadcastReceiver extends BroadcastReceiver {
-        // The url has to be copied from the instrumentation class, because a BroadcastReceiver is
-        // deployed as a different package, and it cannot get access to data from the
-        // instrumentation package.
-        private static final String TEST_PAGE_COPY = "/chrome/test/data/android/google.html";
-
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            // Note: even if this assertion fails, the test might still pass, because
-            // BroadcastReceiver is not treated as part of the instrumentation test.
-            assertEquals(mTestServer.getURL(TEST_PAGE_COPY), intent.getDataString());
-        }
-    }
-
     private static final String READ_IT_LATER_FEATURE = "ReadItLaterInMenu";
     private static final int MAX_MENU_CUSTOM_ITEMS = 5;
     private static final int NUM_CHROME_MENU_ITEMS = 2;
@@ -182,11 +162,8 @@ public class CustomTabActivityTest extends CustomTabActivityTestBase {
      * @return The pending intent associated with the menu entry.
      */
     private PendingIntent addMenuEntriesToIntent(Intent intent, int numEntries) {
-        Intent menuIntent = new Intent();
-        menuIntent.setClass(getInstrumentation().getContext(), DummyBroadcastReceiver.class);
-        menuIntent.setAction(TEST_ACTION);
         PendingIntent pi = PendingIntent.getBroadcast(getInstrumentation().getTargetContext(), 0,
-                menuIntent, 0);
+                new Intent(), 0);
         ArrayList<Bundle> menuItems = new ArrayList<Bundle>();
         for (int i = 0; i < numEntries; i++) {
             Bundle bundle = new Bundle();
@@ -207,15 +184,11 @@ public class CustomTabActivityTest extends CustomTabActivityTestBase {
      * @return The {@link PendingIntent} that will be triggered when the action button is clicked.
      */
     private PendingIntent addActionButtonToIntent(Intent intent, Bitmap icon, String description) {
-        Intent actionIntent = new Intent();
-        actionIntent.setClass(getInstrumentation().getContext(), DummyBroadcastReceiver.class);
-        actionIntent.setAction(TEST_ACTION);
-
         Bundle bundle = new Bundle();
         bundle.putParcelable(CustomTabsIntent.KEY_ICON, icon);
         bundle.putString(CustomTabsIntent.KEY_DESCRIPTION, description);
         PendingIntent pi = PendingIntent.getBroadcast(getInstrumentation().getTargetContext(), 0,
-                actionIntent, 0);
+                new Intent(), 0);
         bundle.putParcelable(CustomTabsIntent.KEY_PENDING_INTENT, pi);
 
         intent.putExtra(CustomTabsIntent.EXTRA_ACTION_BUTTON_BUNDLE, bundle);
@@ -224,11 +197,8 @@ public class CustomTabActivityTest extends CustomTabActivityTestBase {
 
     private Bundle makeBottomBarBundle(int id, Bitmap icon, String description) {
         Bundle bundle = new Bundle();
-        Intent testIntent = new Intent();
-        testIntent.setClass(getInstrumentation().getContext(), DummyBroadcastReceiver.class);
-        testIntent.setAction(TEST_ACTION);
         PendingIntent pi = PendingIntent.getBroadcast(getInstrumentation().getTargetContext(), 0,
-                testIntent, 0);
+                new Intent(), 0);
 
         bundle.putInt(CustomTabsIntent.KEY_ID, sIdToIncrement++);
         bundle.putString(CustomTabsIntent.KEY_DESCRIPTION, description);
