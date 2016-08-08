2cbf4a9a43384d3129aa76b56b227844c2a51aa1
sullivan <sullivan@chromium.org>
2016-08-08 16:42:49 -0700

    Disable oortonline_tbmv2 on Windows
    BUG=635649
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.perf:android_s5_perf_cq;master.tryserver.chromium.perf:linux_perf_cq;master.tryserver.chromium.perf:mac_retina_perf_cq
    TBR=ulan@chromium.org
    
    Review-Url: https://codereview.chromium.org/2223233002
    Cr-Commit-Position: refs/heads/master@{#410501}
    
    Former-commit-id: 9d22c6c30a2a54dabb48643d8f8413cecbb52239

--- tools/perf/benchmarks/oortonline.py
+++ tools/perf/benchmarks/oortonline.py
@@ -52,6 +52,7 @@ class OortOnline(perf_benchmark.PerfBenchmark):
 # Tracing.requestMemoryDump DevTools API. See http://crbug.com/540022.
 @benchmark.Disabled('reference')
 @benchmark.Disabled('android')
+@benchmark.Disabled('win')
 class OortOnlineTBMv2(perf_benchmark.PerfBenchmark):
   """OortOnline benchmark that measures WebGL and V8 performance.
   URL: http://oortonline.gl/#run
