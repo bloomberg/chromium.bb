0a6478c2a30ef82cd70e573c7cc4e40827e12c14
cbiesinger <cbiesinger@chromium.org>
2016-08-09 13:54:44 -0700

    [LayoutNG] Very simple layout() implementation
    Just resolved width and height, and returns a fragment with that.
    
    R=eae@chromium.org,ikilpatrick@chromium.org,glebl@chromium.org
    BUG=635619
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2230563002
    Cr-Commit-Position: refs/heads/master@{#410814}
    
    Former-commit-id: 7872d0ed57157601145d7efe6f3c773750f38ee2

--- third_party/WebKit/Source/core/layout/ng/NGBlockLayoutAlgorithm.cpp
+++ third_party/WebKit/Source/core/layout/ng/NGBlockLayoutAlgorithm.cpp
@@ -4,10 +4,27 @@
 
 #include "core/layout/ng/NGBlockLayoutAlgorithm.h"
 
+#include "core/layout/LayoutBox.h"
+#include "core/layout/ng/NGConstraintSpace.h"
+#include "core/layout/ng/NGFragment.h"
+#include "core/style/ComputedStyle.h"
+#include "platform/LengthFunctions.h"
+
 namespace blink {
 
-NGBlockLayoutAlgorithm::NGBlockLayoutAlgorithm()
+NGBlockLayoutAlgorithm::NGBlockLayoutAlgorithm(const ComputedStyle* style)
+    : m_style(style)
 {
+}
+
+NGFragment* NGBlockLayoutAlgorithm::layout(
+    const NGConstraintSpace& constraintSpace)
+{
+    LayoutUnit inlineSize = valueForLength(m_style->logicalWidth(),
+        constraintSpace.inlineContainerSize());
+    LayoutUnit blockSize = valueForLength(m_style->logicalHeight(),
+        constraintSpace.blockContainerSize());
+    return new NGFragment(inlineSize, blockSize, inlineSize, blockSize);
 }
 
 } // namespace blink
--- third_party/WebKit/Source/core/layout/ng/NGBlockLayoutAlgorithm.h
+++ third_party/WebKit/Source/core/layout/ng/NGBlockLayoutAlgorithm.h
@@ -5,11 +5,29 @@
 #ifndef NGBlockLayoutAlgorithm_h
 #define NGBlockLayoutAlgorithm_h
 
+#include "wtf/RefPtr.h"
+
 namespace blink {
 
+class ComputedStyle;
+class LayoutBox;
+class NGConstraintSpace;
+class NGFragment;
+
+// A class for general block layout (e.g. a <div> with no special style).
+// Lays out the children in sequence.
 class NGBlockLayoutAlgorithm {
 public:
-    NGBlockLayoutAlgorithm();
+    NGBlockLayoutAlgorithm(const ComputedStyle*);
+
+    // Actual layout implementation. Lays out the children in sequence within
+    // the constraints given by the NGConstraintSpace. Returns a fragment
+    // with the resulting layout information.
+    // This function can not be const because for interruptible layout, we have
+    // to be able to store state information.
+    NGFragment* layout(const NGConstraintSpace&);
+private:
+    RefPtr<const ComputedStyle> m_style;
 };
 
 } // namespace blink
