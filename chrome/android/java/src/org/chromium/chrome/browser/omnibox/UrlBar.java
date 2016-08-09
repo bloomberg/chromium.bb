a7e3a00909d5e1dafdaa836aa18e9324999c2ba3
mgiuca <mgiuca@chromium.org>
2016-08-08 17:15:55 -0700

    Android Omnibox: Do not force LTR text direction when empty.
    Fixes a regression introduced in r400359: the "Search or type URL"
    placeholder text jumping back and forth from left and right alignment
    when in RTL languages.
    
    Also fixes incorrect text direction when restoring a closed tab.
    
    BUG=631422
    TEST=On a phone-sized Android device, with language set to
      Hebrew/Arabic, in landscape, open New Tab Page and scroll page up and
      down. Should see the grey placeholder text in Omnibox always
      right-aligned, not jumping from left to right.
    
    Review-Url: https://codereview.chromium.org/2182183008
    Cr-Commit-Position: refs/heads/master@{#410510}
    
    Former-commit-id: 1d266aa00775a9e9307c7d342fb6354ba7707012

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/UrlBar.java
@@ -444,16 +444,7 @@ public class UrlBar extends VerticallyFixedEditText {
 
         if (focused) StartupMetrics.getInstance().recordFocusedOmnibox();
 
-        // When unfocused, force left-to-right rendering at the paragraph level (which is desired
-        // for URLs). Right-to-left runs are still rendered RTL, but will not flip the whole URL
-        // around. This is consistent with OmniboxViewViews on desktop. When focused, render text
-        // normally (to allow users to make non-URL searches and to avoid showing Android's split
-        // insertion point when an RTL user enters RTL text).
-        if (focused) {
-            ApiCompatibilityUtils.setTextDirection(this, TEXT_DIRECTION_INHERIT);
-        } else {
-            ApiCompatibilityUtils.setTextDirection(this, TEXT_DIRECTION_LTR);
-        }
+        fixupTextDirection();
         // Always align to the same as the paragraph direction (LTR = left, RTL = right).
         ApiCompatibilityUtils.setTextAlignment(this, TEXT_ALIGNMENT_TEXT_START);
     }
@@ -474,6 +465,25 @@ public class UrlBar extends VerticallyFixedEditText {
         if (mFirstDrawComplete) {
             setFocusable(allowFocus);
             setFocusableInTouchMode(allowFocus);
+        }
+    }
+
+    /**
+     * Sets the {@link UrlBar}'s text direction based on focus and contents.
+     *
+     * Should be called whenever focus or text contents change.
+     */
+    private void fixupTextDirection() {
+        // When unfocused, force left-to-right rendering at the paragraph level (which is desired
+        // for URLs). Right-to-left runs are still rendered RTL, but will not flip the whole URL
+        // around. This is consistent with OmniboxViewViews on desktop. When focused, render text
+        // normally (to allow users to make non-URL searches and to avoid showing Android's split
+        // insertion point when an RTL user enters RTL text). Also render text normally when the
+        // text field is empty (because then it displays an instruction that is not a URL).
+        if (mFocused || length() == 0) {
+            ApiCompatibilityUtils.setTextDirection(this, TEXT_DIRECTION_INHERIT);
+        } else {
+            ApiCompatibilityUtils.setTextDirection(this, TEXT_DIRECTION_LTR);
         }
     }
 
@@ -865,6 +875,8 @@ public class UrlBar extends VerticallyFixedEditText {
                 clearAutocompleteSpanIfInvalid();
             }
         }
+
+        fixupTextDirection();
     }
 
     private void clearAutocompleteSpanIfInvalid() {
