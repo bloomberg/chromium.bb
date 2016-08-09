3cf0c4593356b871cdd47183d38bf740f5e75d35
erikchen <erikchen@chromium.org>
2016-08-09 05:14:21 -0700

    Add fullscreen video power test.
    BUG=616811
    
    Review-Url: https://codereview.chromium.org/2223123002
    Cr-Commit-Position: refs/heads/master@{#410639}
    
    Former-commit-id: 8bc0053763a01b684ec821798e152fd93091ec7b

--- tools/perf/page_sets/mac_gpu_sites.py
+++ tools/perf/page_sets/mac_gpu_sites.py
@@ -80,10 +80,24 @@ class TrivialBlurAnimationPage(page_module.Page):
         shared_page_state_class=shared_page_state_class)
 
 
+class TrivialFullscreenVideoPage(page_module.Page):
+
+  def __init__(self, page_set, shared_page_state_class):
+    super(TrivialFullscreenVideoPage, self).__init__(
+        url='file://trivial_sites/trivial_fullscreen_video.html',
+        page_set=page_set,
+        name=self.__class__.__name__ + shared_page_state_class.__name__,
+        shared_page_state_class=shared_page_state_class)
+
+  def RunPageInteractions(self, action_runner):
+    action_runner.PressKey("Return")
+
+
 class MacGpuTrivialPagesStorySet(story.StorySet):
 
   def __init__(self):
-    super(MacGpuTrivialPagesStorySet, self).__init__()
+    super(MacGpuTrivialPagesStorySet, self).__init__(
+        cloud_storage_bucket=story.PUBLIC_BUCKET)
     self.AddStory(TrivialScrollingPage(self, shared_page_state.SharedPageState))
     self.AddStory(TrivialBlinkingCursorPage(
         self, shared_page_state.SharedPageState))
@@ -91,18 +105,22 @@ class MacGpuTrivialPagesStorySet(story.StorySet):
     self.AddStory(TrivialWebGLPage(self, shared_page_state.SharedPageState))
     self.AddStory(TrivialBlurAnimationPage(
         self, shared_page_state.SharedPageState))
+    self.AddStory(TrivialFullscreenVideoPage(
+        self, shared_page_state.SharedPageState))
 
     self.AddStory(TrivialScrollingPage(self, _NoOverlaysSharedPageState))
     self.AddStory(TrivialBlinkingCursorPage(self, _NoOverlaysSharedPageState))
     self.AddStory(TrivialCanvasPage(self, _NoOverlaysSharedPageState))
     self.AddStory(TrivialWebGLPage(self, _NoOverlaysSharedPageState))
     self.AddStory(TrivialBlurAnimationPage(self, _NoOverlaysSharedPageState))
+    self.AddStory(TrivialFullscreenVideoPage(self, _NoOverlaysSharedPageState))
 
     self.AddStory(TrivialScrollingPage(self, _NoGpuSharedPageState))
     self.AddStory(TrivialBlinkingCursorPage(self, _NoGpuSharedPageState))
     self.AddStory(TrivialCanvasPage(self, _NoGpuSharedPageState))
     self.AddStory(TrivialWebGLPage(self, _NoWebGLImageChromiumSharedPageState))
     self.AddStory(TrivialBlurAnimationPage(self, _NoGpuSharedPageState))
+    self.AddStory(TrivialFullscreenVideoPage(self, _NoGpuSharedPageState))
 
   @property
   def allow_mixed_story_states(self):
