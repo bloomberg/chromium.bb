3a42b9d2a8cd278156dcccc49b47769f63379f5a
rnk <rnk@chromium.org>
2016-08-10 13:25:07 -0700

    Volatilize the global store in DummyExport to defeat Clang's optimizer and avoid ICF
    Otherwise this test crashes under ASan while running a corrupted at-exit handler.
    
    R=grt@chromium.org,thakis@chromium.org
    BUG=636157
    
    Review-Url: https://codereview.chromium.org/2228233002
    Cr-Commit-Position: refs/heads/master@{#411134}
    
    Former-commit-id: 98b87cf47c62669a1bcd67f0db868c31482605a1

--- chrome/browser/safe_browsing/incident_reporting/verifier_test/verifier_test_dll.cc
+++ chrome/browser/safe_browsing/incident_reporting/verifier_test/verifier_test_dll.cc
@@ -9,12 +9,16 @@
 namespace {
 
 void (*g_somestate)(int) = nullptr;
-int g_somevalue = 0;
+volatile int g_somevalue = 0;
 
 }  // namespace
 
 extern "C"
 void DummyExport(int foo) {
+  // The test will patch the code of this function. Do a volatile store to a
+  // global variable to defeat LLVM's dead store elimination and the linker's
+  // ICF. Otherwise the code patch affects code run during DLL unloading.
+  // http://crbug.com/636157
   g_somevalue = foo;
 }
 
