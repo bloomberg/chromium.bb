6a7faede2f5bfa9cc634bdb0b4c1ddd7b540cee4
michaelpg <michaelpg@chromium.org>
2016-08-10 16:11:48 -0700

    Update create_components_summary.py to allow other tree names in bower.json
    It's nice to be able to test a particular branch of a Polymer repo, by updating
    bower.json locally and running reproduce.sh, but doing so causes
    create_components_summary.py to fail loudly due to missing properties in the
    .bower.json.
    
    We should find the more general tree-ish name instead of the "tag" property.
    For release tags (what our committed bower.json uses) this is the same.
    
    R=dbeam@chromium.org
    
    Review-Url: https://codereview.chromium.org/2225513002
    Cr-Commit-Position: refs/heads/master@{#411176}
    
    Former-commit-id: b561abad688e9fca08fa7663e8f6e921f8b61e7b

--- third_party/polymer/v1_0/create_components_summary.py
+++ third_party/polymer/v1_0/create_components_summary.py
@@ -2,35 +2,59 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import os
 import json
+import os
+import re
+
 
 COMPONENTS_DIR = 'components'
 COMPONENT_SUMMARY =\
 """Name: %(name)s
-Version: %(version)s
 Repository: %(repository)s
-Tag: %(tag)s
+Tree: %(tree)s
 Revision: %(revision)s
-Tree link: %(tree)s
+Tree link: %(tree_link)s
 """
 
-for entry in sorted(os.listdir(COMPONENTS_DIR)):
-  component_path = os.path.join(COMPONENTS_DIR, entry)
-  if not os.path.isdir(component_path):
-    continue
-  bower_path = os.path.join(component_path, '.bower.json')
-  if not os.path.isfile(bower_path):
-    raise Exception('%s is not a file.' % bower_path)
-  with open(bower_path) as stream:
-    info = json.load(stream)
+
+def PrintSummary(info):
   repository = info['_source']
-  tree = 'https%s/tree/%s' % (repository[3:-4], info['_resolution']['tag'])
+  resolution = info['_resolution']
+  tree = GetTreeishName(resolution)
+  # Convert to web link.
+  repository_web = re.sub('^git:', 'https:', re.sub('\.git$', '',  repository))
+  # Specify tree to browse to.
+  tree_link = repository_web + '/tree/' + tree
   print COMPONENT_SUMMARY % {
     'name': info['name'],
-    'version': info['version'],
     'repository': repository,
-    'tag': info['_resolution']['tag'],
-    'revision': info['_resolution']['commit'],
-    'tree': tree
+    'tree': tree,
+    'revision': resolution['commit'],
+    'tree_link': tree_link
   }
+
+
+def GetTreeishName(resolution):
+  """Gets the name of the tree-ish (branch, tag or commit)."""
+  if resolution['type'] == 'branch':
+    return resolution['branch']
+  if resolution['type'] in ('version', 'tag'):
+    return resolution['tag']
+  return resolution['commit']
+
+
+def main():
+  for entry in sorted(os.listdir(COMPONENTS_DIR)):
+    component_path = os.path.join(COMPONENTS_DIR, entry)
+    if not os.path.isdir(component_path):
+      continue
+    bower_path = os.path.join(component_path, '.bower.json')
+    if not os.path.isfile(bower_path):
+      raise Exception('%s is not a file.' % bower_path)
+    with open(bower_path) as stream:
+      info = json.load(stream)
+    PrintSummary(info)
+
+
+if __name__ == '__main__':
+  main()
