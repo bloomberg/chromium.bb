10d34718fbad59d5974f51e3eabba43d253474db
hzl <hzl@google.com>
2016-08-09 18:56:09 -0700

    Created an intentionally crashing test for Android Instrumentation test.
    It is important for us to be sure that we capture all the
    right information if a testcase crashes, information such
    as tombstones and logcat. Most existing testcases crash unpredictably,
    so it is hard for us to check.
    
    In this cl, I created an intentionally crashing testcase.
    With this testcase, we are able to reliably check
    how our test runner behaves when a testcase crashes.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2216773002
    Cr-Commit-Position: refs/heads/master@{#410919}
    
    Former-commit-id: 22723935445be14da759893cdbc06de09ab1a7fa

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/CrashTest.java
@@ -0,0 +1,36 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.chromium.base.test.util.DisabledTest;
+import org.chromium.chrome.test.ChromeActivityTestCaseBase;
+
+/**
+ * The class contains a testcase that intentionally crashes.
+ */
+public class CrashTest extends ChromeActivityTestCaseBase<ChromeActivity> {
+
+    public CrashTest() {
+        super(ChromeActivity.class);
+    }
+
+    @Override
+    public void startMainActivity() {
+        // Don't launch activity automatically.
+    }
+
+    /**
+     * Intentionally crashing the test. The testcase should be
+     * disabled the majority of time.
+     */
+    @DisabledTest
+    @SmallTest
+    public void testIntentionalBrowserCrash() throws Exception {
+        startMainActivityFromLauncher();
+        loadUrl("chrome://inducebrowsercrashforrealz");
+    }
+}
