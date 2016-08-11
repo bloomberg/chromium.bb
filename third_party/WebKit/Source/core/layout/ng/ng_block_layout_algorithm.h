d572d750e689cf116d09f31f03e1360a2a7267f4
cbiesinger <cbiesinger@chromium.org>
2016-08-10 19:59:55 -0700

    [layoutng] Add a base class for layout algorithms
    R=ikilpatrick@chromium.org,eae@chromium.org,glebl@chromium.org
    BUG=635619
    
    Review-Url: https://codereview.chromium.org/2237643003
    Cr-Commit-Position: refs/heads/master@{#411240}
    
    Former-commit-id: 4447e60b8218f0fb2e16d30844ac129419bc95b5

--- third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.h
+++ third_party/WebKit/Source/core/layout/ng/ng_block_layout_algorithm.h
@@ -7,6 +7,7 @@
 
 #include "core/CoreExport.h"
 #include "core/layout/ng/ng_box.h"
+#include "core/layout/ng/ng_layout_algorithm.h"
 #include "wtf/RefPtr.h"
 
 namespace blink {
@@ -17,7 +18,7 @@ class NGFragment;
 
 // A class for general block layout (e.g. a <div> with no special style).
 // Lays out the children in sequence.
-class CORE_EXPORT NGBlockLayoutAlgorithm {
+class CORE_EXPORT NGBlockLayoutAlgorithm : public NGLayoutAlgorithm {
  public:
   NGBlockLayoutAlgorithm(PassRefPtr<const ComputedStyle>, NGBox);
 
@@ -26,7 +27,7 @@ class CORE_EXPORT NGBlockLayoutAlgorithm {
   // resulting layout information.
   // This function can not be const because for interruptible layout, we have
   // to be able to store state information.
-  NGFragment* layout(const NGConstraintSpace&);
+  NGFragment* layout(const NGConstraintSpace&) override;
 
  private:
   RefPtr<const ComputedStyle> m_style;
---
+++ third_party/WebKit/Source/core/layout/ng/ng_layout_algorithm.h
@@ -0,0 +1,35 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NGLayoutAlgorithm_h
+#define NGLayoutAlgorithm_h
+
+#include "core/CoreExport.h"
+#include "wtf/Allocator.h"
+#include "wtf/Noncopyable.h"
+
+namespace blink {
+
+class NGConstraintSpace;
+class NGFragment;
+
+// Base class for all LayoutNG algorithms.
+class CORE_EXPORT NGLayoutAlgorithm {
+  WTF_MAKE_NONCOPYABLE(NGLayoutAlgorithm);
+  USING_FAST_MALLOC(NGLayoutAlgorithm);
+
+ public:
+  NGLayoutAlgorithm() {}
+
+  // Actual layout function. Lays out the children and descendents within the
+  // constraints given by the NGConstraintSpace. Returns a fragment with the
+  // resulting layout information.
+  // This function can not be const because for interruptible layout, we have
+  // to be able to store state information.
+  virtual NGFragment* layout(const NGConstraintSpace&) = 0;
+};
+
+}  // namespace blink
+
+#endif  // NGLayoutAlgorithm_h
