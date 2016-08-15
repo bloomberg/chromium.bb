d0f549a56ab55c087009df02671fdfcddbb22e19
ianwen <ianwen@chromium.org>
2016-08-15 12:34:52 -0700

    Remove debug loggings in history report for privacy reasons
    There are complaints about history report spamming Android logcat, and
    leaking users history entirely in logcat.
    
    BUG=474708
    
    Review-Url: https://codereview.chromium.org/2247633004
    Cr-Commit-Position: refs/heads/master@{#412021}
    
    Former-commit-id: 11cf2f58ec87d91cae9eb9855f45f77604a263a5

--- chrome/android/java/src/org/chromium/chrome/browser/historyreport/HistoryReportJniBridge.java
+++ chrome/android/java/src/org/chromium/chrome/browser/historyreport/HistoryReportJniBridge.java
@@ -4,8 +4,6 @@
 
 package org.chromium.chrome.browser.historyreport;
 
-import android.text.TextUtils;
-
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.VisibleForTesting;
@@ -66,7 +64,6 @@ public class HistoryReportJniBridge implements SearchJniBridge {
         }
         Log.d(TAG, "query %d %d", lastSeqNo, limit);
         DeltaFileEntry[] result = nativeQuery(mNativeHistoryReportJniBridge, lastSeqNo, limit);
-        Log.d(TAG, "query result: %s", TextUtils.join("\n", result));
         return result;
     }
 
@@ -100,7 +97,6 @@ public class HistoryReportJniBridge implements SearchJniBridge {
         for (int i = 0; i < reports.length; ++i) {
             reportIds[i] = reports[i].reportId;
         }
-        Log.d(TAG, "removeUsageReports %s", TextUtils.join(",", reportIds));
         nativeRemoveUsageReports(mNativeHistoryReportJniBridge, reportIds);
     }
 
