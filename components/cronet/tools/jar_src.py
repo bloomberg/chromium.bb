5d0263afbc11be4b3d812ac2d416747aff24b278
bsep <bsep@chromium.org>
2016-08-15 10:58:53 -0700

    Revert of Include generated EffectiveConnectionType.java in cronet api jar and JavaDoc (patchset #5 id:120001 of https://codereview.chromium.org/2234113002/ )
    Reason for revert:
    compile failed on Android Arm64:
    
    FAILED: gen/components/cronet/android/generate_javadoc.d
    python ../../components/cronet/tools/generate_javadoc.py --output-dir cronet --input-dir ../../components/cronet --overview-file cronet/README.md.html --readme-file ../../components/cronet/README.md --depfile gen/components/cronet/android/generate_javadoc.d --lib-java-dir lib.java/components/cronet/android --input-src-jar cronet/cronet_api-src.jar
    Traceback (most recent call last):
      File "../../components/cronet/tools/generate_javadoc.py", line 85, in <module>
        sys.exit(main())
      File "../../components/cronet/tools/generate_javadoc.py", line 73, in main
        GenerateJavadoc(options, os.path.abspath(unzipped_jar_path))
      File "../../components/cronet/tools/generate_javadoc.py", line 48, in GenerateJavadoc
        raise build_utils.CalledProcessError(working_dir, javadoc_cmd, stdout)
    
    Original issue's description:
    > Include generated EffectiveConnectionType.java in cronet api jar and JavaDoc
    >
    > This CL includes the EffectiveConnectionType.java in cronet
    > api jar and in JavaDoc, since that is a part of Cronet API.
    >
    > BUG=634092
    >
    > Committed: https://crrev.com/65b2ae0980b8380978a676630b6e8305fd4fb09d [formerly cfb2180def7b0e17bd4dce2bc8300fb4ea02e4ba]
    > Cr-Commit-Position: refs/heads/master@{#411977}
    
    TBR=mef@chromium.org,kapishnikov@chromium.org,xunjieli@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=634092
    
    Review-Url: https://codereview.chromium.org/2242243002
    Cr-Commit-Position: refs/heads/master@{#411980}
    
    Former-commit-id: f84d14ad945d5cc3d945bf4df0d9ca9e82824ed7

--- components/cronet/tools/jar_src.py
+++ components/cronet/tools/jar_src.py
@@ -7,7 +7,6 @@
 import optparse
 import os
 import sys
-import tempfile
 
 REPOSITORY_ROOT = os.path.abspath(os.path.join(
     os.path.dirname(__file__), '..', '..', '..'))
@@ -29,43 +28,20 @@ def JarSources(src_dir, jar_path):
 
   build_utils.CheckOutput(jar_cmd, cwd=jar_cwd)
 
-# Uncompress source jars so that they can be combined with other sources
-def UnzipSourceJar(jar, unzipped_jar_path):
-  if os.path.exists(jar):
-    jar_cmd = ['jar', 'xf', os.path.abspath(jar)]
-    build_utils.CheckOutput(jar_cmd, cwd=unzipped_jar_path)
-  else:
-    raise Exception('Jar file does not exist %s' % jar)
-
 
 def main():
   parser = optparse.OptionParser()
   build_utils.AddDepfileOption(parser)
   parser.add_option('--src-dir', action="append",
       help='Directory containing .java files.')
-  parser.add_option('--src-jars', action="append",
-      help='A list of source jars to include in addition to source files.')
   parser.add_option('--jar-path', help='Jar output path.')
   parser.add_option('--stamp', help='Path to touch on success.')
 
   options, _ = parser.parse_args()
 
-  # A temporary directory to put the output of jar files.
-  unzipped_jar_path = None
-  if options.src_jars:
-    unzipped_jar_path = tempfile.mkdtemp(dir=os.path.dirname(options.jar_path))
-    jar_list = []
-    for gn_list in options.src_jars:
-      jar_list.extend(build_utils.ParseGnList(gn_list))
-
-    for jar in jar_list:
-      UnzipSourceJar(jar, unzipped_jar_path)
-
   src_dirs = []
   for src_dir in options.src_dir:
     src_dirs.extend(build_utils.ParseGypList(src_dir))
-  if unzipped_jar_path:
-    src_dirs += [unzipped_jar_path]
 
   for src_dir in src_dirs:
     JarSources(src_dir, options.jar_path)
@@ -77,9 +53,6 @@ def main():
         input_paths.extend(os.path.join(root, f) for f in filenames)
     build_utils.WriteDepfile(options.depfile,
                              input_paths + build_utils.GetPythonDependencies())
-  # Clean up temporary output directory.
-  if unzipped_jar_path:
-    build_utils.DeleteDirectory(unzipped_jar_path)
 
   if options.stamp:
     build_utils.Touch(options.stamp)
