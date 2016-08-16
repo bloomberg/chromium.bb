93c5bee831eb1839b89a9437039459d6def78d87
jbauman <jbauman@chromium.org>
2016-08-16 14:11:17 -0700

    Add histograms to keep track of how much committed memory is being used.
    This will help us keep track of slow leaks of memory that isn't used
    and is then paged out.
    
    Review-Url: https://codereview.chromium.org/2238413003
    Cr-Commit-Position: refs/heads/master@{#412337}
    
    Former-commit-id: 9114ac31b82529d145ba6fe3eda9def53df6fa06

--- chrome/browser/metrics/metrics_memory_details.cc
+++ chrome/browser/metrics/metrics_memory_details.cc
@@ -86,10 +86,15 @@ void MetricsMemoryDetails::UpdateHistograms() {
   int process_limit = content::RenderProcessHost::GetMaxRendererProcessCount();
   for (size_t index = 0; index < browser.processes.size(); index++) {
     int sample = static_cast<int>(browser.processes[index].working_set.priv);
+    size_t committed = browser.processes[index].committed.priv +
+                       browser.processes[index].committed.mapped +
+                       browser.processes[index].committed.image;
     aggregate_memory += sample;
     switch (browser.processes[index].process_type) {
       case content::PROCESS_TYPE_BROWSER:
         UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Browser.Large2", sample / 1024);
+        UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Browser.Committed",
+                                      committed / 1024);
         continue;
       case content::PROCESS_TYPE_RENDERER: {
         ProcessMemoryInformation::RendererProcessType renderer_type =
@@ -111,6 +116,8 @@ void MetricsMemoryDetails::UpdateHistograms() {
             // TODO(erikkay): Should we bother splitting out the other subtypes?
             UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Renderer.Large2",
                                           sample / 1024);
+            UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Renderer.Committed",
+                                          committed / 1024);
             int diff;
             if (memory_growth_tracker_ &&
                 memory_growth_tracker_->UpdateSample(
--- chrome/browser/metrics/metrics_memory_details_browsertest.cc
+++ chrome/browser/metrics/metrics_memory_details_browsertest.cc
@@ -52,11 +52,22 @@ IN_PROC_BROWSER_TEST_F(MetricsMemoryDetailsBrowserTest, TestMemoryDetails) {
   scoped_refptr<TestMemoryDetails> details(new TestMemoryDetails);
   details->StartFetchAndWait();
 
-  // Memory.Browser.Large2 histogram should have a single non-0 sample recorded.
+  // Memory.Browser.Large2 and Memory.Browser.Committed histograms should each
+  // have a single non-0 sample recorded.
   histogram_tester.ExpectTotalCount("Memory.Browser.Large2", 1);
   std::unique_ptr<base::HistogramSamples> samples(
       histogram_tester.GetHistogramSamplesSinceCreation(
           "Memory.Browser.Large2"));
   ASSERT_TRUE(samples);
   EXPECT_NE(0, samples->sum());
+
+  histogram_tester.ExpectTotalCount("Memory.Browser.Committed", 1);
+  std::unique_ptr<base::HistogramSamples> committed_samples(
+      histogram_tester.GetHistogramSamplesSinceCreation(
+          "Memory.Browser.Committed"));
+  ASSERT_TRUE(committed_samples);
+#if !defined(OS_LINUX)
+  // Committed memory isn't calculated on Linux.
+  EXPECT_NE(0, committed_samples->sum());
+#endif
 }
