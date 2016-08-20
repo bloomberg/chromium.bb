a4dd65aefb1d9132e76140a9eede974a6e73f8ad
takumif <takumif@chromium.org>
2016-08-20 08:34:43 -0700

    Revert of Fix MediaRouterContextualMenuUnitTest (patchset #1 id:1 of https://codereview.chromium.org/2253613003/ )
    Reason for revert:
    Reverting this CL as well as the other CL[1] this was meant to fix. They are causing crashes and need a redesign.
    
    [1] https://codereview.chromium.org/2155293002
    
    Original issue's description:
    > Fix MediaRouterContextualMenuUnitTest
    >
    > This CL resolves the compliation error in the unit test introduced
    > by https://codereview.chromium.org/2155293002.
    >
    > TBR=msw@chromium.org
    > BUG=638300
    >
    > Committed: https://crrev.com/b260dfb9842427f50a8fb1f8725d8132bc6701ab [formerly 0d3d4235ebcd37fa56b05517c9d97c018e8c0f5f]
    > Cr-Commit-Position: refs/heads/master@{#412332}
    
    TBR=msw@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=638300
    
    Review-Url: https://codereview.chromium.org/2260873003
    Cr-Commit-Position: refs/heads/master@{#413339}
    
    Former-commit-id: b61db4502430b2ed441ce4f1e75c1db593dd3691

--- chrome/browser/ui/toolbar/media_router_contextual_menu_unittest.cc
+++ chrome/browser/ui/toolbar/media_router_contextual_menu_unittest.cc
@@ -35,7 +35,6 @@ class MediaRouterContextualMenuUnitTest : public BrowserWithTestWindowTest {
 
   SigninManagerBase* signin_manager() { return signin_manager_; }
   ui::SimpleMenuModel* model() { return model_; }
-  MediaRouterAction* action() { return action_.get(); }
 
  private:
   std::unique_ptr<BrowserActionTestUtil> browser_action_test_util_;
@@ -97,7 +96,7 @@ TEST_F(MediaRouterContextualMenuUnitTest, Basic) {
 TEST_F(MediaRouterContextualMenuUnitTest, ToggleCloudServicesItem) {
   // The Media Router Action has a getter for the model, but not the delegate.
   // Create the MediaRouterContextualMenu ui::SimpleMenuModel::Delegate here.
-  MediaRouterContextualMenu menu(browser(), action());
+  MediaRouterContextualMenu menu(browser());
 
   // Set up an authenticated account such that the cloud services menu item is
   // surfaced. Whether or not it is surfaced is tested in the "Basic" test.
