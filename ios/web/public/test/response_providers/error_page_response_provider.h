496fcc6dfab847d79ca85fe713e6e73347184f27
baxley <baxley@chromium.org>
2016-08-21 13:26:06 -0700

    Upstream ErrorPageResponseProvider to be used in integration tests.
    A response provider for DNS failures.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2259393002
    Cr-Commit-Position: refs/heads/master@{#413378}
    
    Former-commit-id: cbe7b17289c80014528d418a99a1e10ae16fb89b

---
+++ ios/web/public/test/response_providers/error_page_response_provider.h
@@ -0,0 +1,37 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_PUBLIC_TEST_RESPONSE_PROVIDERS_ERROR_PAGE_RESPONSE_PROVIDER_H_
+#define IOS_WEB_PUBLIC_TEST_RESPONSE_PROVIDERS_ERROR_PAGE_RESPONSE_PROVIDER_H_
+
+#include <map>
+#include <string>
+
+#include "ios/web/public/test/response_providers/html_response_provider.h"
+#include "url/gurl.h"
+
+// A HtmlResponseProvider that supports the following additional URLs:
+// - GetRedirectToDnsFailureUrl - the response is a redirect to
+//   |GetDnsFailureUrl|.
+// - GetDnsFailureUrl - triggers a DNS error.
+class ErrorPageResponseProvider : public HtmlResponseProvider {
+ public:
+  ErrorPageResponseProvider() : HtmlResponseProvider() {}
+  explicit ErrorPageResponseProvider(
+      const std::map<GURL, std::string>& responses)
+      : HtmlResponseProvider(responses) {}
+  // Returns a URL that causes a DNS failure.
+  static GURL GetDnsFailureUrl();
+  // Returns a URL that redirects to a bad URL.
+  static GURL GetRedirectToDnsFailureUrl();
+
+  // HtmlResponseProvider implementation.
+  bool CanHandleRequest(const Request& request) override;
+  void GetResponseHeadersAndBody(
+      const Request& request,
+      scoped_refptr<net::HttpResponseHeaders>* headers,
+      std::string* response_body) override;
+};
+
+#endif  // IOS_WEB_PUBLIC_TEST_RESPONSE_PROVIDERS_ERROR_PAGE_RESPONSE_PROVIDER_H_
---
+++ ios/web/public/test/response_providers/error_page_response_provider.mm
@@ -0,0 +1,41 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/web/public/test/response_providers/error_page_response_provider.h"
+
+#include "base/logging.h"
+#import "ios/web/public/test/http_server.h"
+#include "net/http/http_status_code.h"
+
+// static
+GURL ErrorPageResponseProvider::GetDnsFailureUrl() {
+  return GURL("http://mock/bad");
+}
+
+// static
+GURL ErrorPageResponseProvider::GetRedirectToDnsFailureUrl() {
+  return web::test::HttpServer::MakeUrl("http://mock/redirect");
+}
+
+// Returns true for |RedirectToDnsFailureUrl|.
+bool ErrorPageResponseProvider::CanHandleRequest(const Request& request) {
+  return (HtmlResponseProvider::CanHandleRequest(request) ||
+          request.url ==
+              ErrorPageResponseProvider::GetRedirectToDnsFailureUrl());
+}
+
+void ErrorPageResponseProvider::GetResponseHeadersAndBody(
+    const Request& request,
+    scoped_refptr<net::HttpResponseHeaders>* headers,
+    std::string* response_body) {
+  if (HtmlResponseProvider::CanHandleRequest(request)) {
+    HtmlResponseProvider::GetResponseHeadersAndBody(request, headers,
+                                                    response_body);
+  } else if (request.url == GetRedirectToDnsFailureUrl()) {
+    *headers = web::ResponseProvider::GetRedirectResponseHeaders(
+        ErrorPageResponseProvider::GetDnsFailureUrl().spec(), net::HTTP_FOUND);
+  } else {
+    NOTREACHED();
+  }
+}
