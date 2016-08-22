9b2a31d6b390bfdc4cce3e1ff7bc15772732de3f
dschuff <dschuff@chromium.org>
2016-08-22 15:27:21 -0700

    PNaCl IRT shim: Fix IRT query wrapper
    Replace the local strcmp with a simpler (and more correct) comparison function.
    
    R=jvoung@chromium.org
    BUG=None
    TEST=NaClBrowserTestPnacl*
    
    Review-Url: https://codereview.chromium.org/2267873002
    Cr-Commit-Position: refs/heads/master@{#413554}
    
    Former-commit-id: 51b72e205d8f3a1ccad76986e8ae3af258b94454

--- ppapi/native_client/src/untrusted/pnacl_irt_shim/shim_ppapi.c
+++ ppapi/native_client/src/untrusted/pnacl_irt_shim/shim_ppapi.c
@@ -6,23 +6,26 @@
 
 #include "ppapi/native_client/src/untrusted/pnacl_irt_shim/shim_ppapi.h"
 
-#include <string.h>
 #include "native_client/src/untrusted/irt/irt.h"
 #include "ppapi/nacl_irt/public/irt_ppapi.h"
 #include "ppapi/native_client/src/shared/ppapi_proxy/ppruntime.h"
 #include "ppapi/native_client/src/untrusted/pnacl_irt_shim/irt_shim_ppapi.h"
 #include "ppapi/native_client/src/untrusted/pnacl_irt_shim/pnacl_shim.h"
 
-/* Use local strcmp to avoid dependency on libc. */
-static int mystrcmp(const char* s1, const char *s2) {
-  while((*s1 && *s2) && (*s1++ == *s2++));
-  return *(--s1) - *(--s2);
+#define ARRAY_SIZEOF(a) (sizeof(a) / sizeof(a[0]))
+
+/* Returns 1 if the strings s1 and s2 are equal, comparing only the first n
+   bytes of s1 and s2; 0 otherwise. */
+static int streq(const char *s1, const char *s2, size_t n) {
+  for(; n && *s1 && *s2 && *s1 == *s2; ++s1, ++s2, --n);
+  return n == 0 || *s1 == *s2;
 }
+
 
 TYPE_nacl_irt_query __pnacl_real_irt_query_func = NULL;
 
 size_t __pnacl_wrap_irt_query_func(const char *interface_ident,
-                                     void *table, size_t tablesize) {
+                                   void *table, size_t tablesize) {
   /*
    * Note there is a benign race in initializing the wrapper.
    * We build the "hook" structure by copying from the IRT's hook and then
@@ -30,7 +33,8 @@ size_t __pnacl_wrap_irt_query_func(const char *interface_ident,
    * attempting to do this simultaneously, which should not be a problem,
    * as they are writing the same values.
    */
-  if (0 != mystrcmp(interface_ident, NACL_IRT_PPAPIHOOK_v0_1)) {
+  if (!streq(interface_ident, NACL_IRT_PPAPIHOOK_v0_1,
+             ARRAY_SIZEOF(NACL_IRT_PPAPIHOOK_v0_1))) {
     /*
      * The interface is not wrapped, so use the real interface.
      */
