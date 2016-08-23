837746f4d2771dd5831d01eac9c378236cdefe32
Tom Finegan <tomfinegan@google.com>
2016-08-23 13:10:21 -0700

    mkvparser_tests: Add invalid block test.
    Add test that confirms expected failure when a block
    that ends beyond the current cluster is encountered.
    
    Change-Id: I27abcab6d00b78c14b7ca00f51c97e43c5cdd34c

--- testing/mkvparser_tests.cc
+++ testing/mkvparser_tests.cc
@@ -723,6 +723,16 @@ TEST_F(ParserTest, InvalidFixedLacingSize) {
             segment_->GetFirst()->GetFirst(block_entry));
 }
 
+TEST_F(ParserTest, InvalidBlockEndsBeyondCluster) {
+  ASSERT_NO_FATAL_FAILURE(
+      CreateSegmentNoHeaderChecks("invalid/block_ends_beyond_cluster.mkv"));
+  ASSERT_EQ(0, segment_->Load());
+  const mkvparser::BlockEntry* block_entry = NULL;
+  EXPECT_EQ(0, segment_->GetFirst()->GetFirst(block_entry));
+  EXPECT_EQ(mkvparser::E_FILE_FORMAT_INVALID,
+            segment_->GetFirst()->GetNext(block_entry, block_entry));
+}
+
 }  // namespace test
 
 int main(int argc, char* argv[]) {
