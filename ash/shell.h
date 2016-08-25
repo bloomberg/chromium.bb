bf6fd2a72526287491b9beac233b7971ad2baf6e
xiaoyinh <xiaoyinh@chromium.org>
2016-08-24 21:11:43 -0700

    Adding the following stylus metrics:
    1. Number of “down” events generated by stylus/touch/mouse
    2. Number of “down” events received by clamshell vs touchview
    
    BUG=630070
    
    Review-Url: https://codereview.chromium.org/2270173002
    Cr-Commit-Position: refs/heads/master@{#414304}
    
    Former-commit-id: 693c002fc07b010ab3ef17360a0557bd87be2832

--- ash/shell.h
+++ ash/shell.h
@@ -120,6 +120,7 @@ struct ShellInitParams;
 class SlowAnimationEventFilter;
 class StatusAreaWidget;
 class StickyKeysController;
+class StylusMetricsRecorder;
 class SystemGestureEventFilter;
 class SystemModalContainerEventFilter;
 class SystemTray;
@@ -576,6 +577,7 @@ class ASH_EXPORT Shell : public SystemModalContainerEventFilterDelegate,
 
   std::unique_ptr<ui::EventHandler> magnifier_key_scroll_handler_;
   std::unique_ptr<ui::EventHandler> speech_feedback_handler_;
+  std::unique_ptr<StylusMetricsRecorder> stylus_metrics_recorder_;
 #endif  // defined(OS_CHROMEOS)
 
   // |native_cursor_manager_| is owned by |cursor_manager_|, but we keep a
