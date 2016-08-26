16522efcadd5a532701b871bde20e282638f209a
sievers <sievers@chromium.org>
2016-08-26 12:31:31 -0700

    Android: Fix Thumbnail CreateSmallHolderBitmap()
    Need to alloc bitmap before creating canvas or it will create
    an empty bitmap instead of one in WHITE.
    
    BUG=636630
    
    Review-Url: https://codereview.chromium.org/2238173003
    Cr-Commit-Position: refs/heads/master@{#414778}
    
    Former-commit-id: df099f06101f2b01d5aa39d96661ab77ad4d6621

--- chrome/browser/android/thumbnail/thumbnail.cc
+++ chrome/browser/android/thumbnail/thumbnail.cc
@@ -18,9 +18,9 @@ namespace {
 
 SkBitmap CreateSmallHolderBitmap() {
   SkBitmap small_bitmap;
-  SkCanvas canvas(small_bitmap);
   small_bitmap.allocPixels(
       SkImageInfo::Make(1, 1, kRGBA_8888_SkColorType, kOpaque_SkAlphaType));
+  SkCanvas canvas(small_bitmap);
   canvas.drawColor(SK_ColorWHITE);
   small_bitmap.setImmutable();
   return small_bitmap;
