4e66ab0ecddbe7506e61448db14a6dc4a4a6fcf4
kcarattini <kcarattini@chromium.org>
2016-08-25 21:53:16 -0700

    Permission Action Reporting: Fix upload url.
    BUG=641220,613883
    
    TBR=nparker@chromium.org
    NOPRESUBMIT=true
    NOTRY=true
    
    Committed: https://crrev.com/4954648f0b202242b679f5f5e7b45e90535a1cbc [formerly ade822e48b63a78dbe5b812702f74fe831bd6a5c]
    Review-Url: https://codereview.chromium.org/2272793009
    Cr-Original-Commit-Position: refs/heads/master@{#414637}
    Cr-Commit-Position: refs/heads/master@{#414652}
    
    Former-commit-id: bdc8c79c3b29ee3299847cdb11e2c82d66138cff

--- chrome/browser/safe_browsing/permission_reporter_unittest.cc
+++ chrome/browser/safe_browsing/permission_reporter_unittest.cc
@@ -24,8 +24,8 @@ namespace {
 
 // URL to upload permission action reports.
 const char kPermissionActionReportingUploadUrl[] =
-    "http://safebrowsing.googleusercontent.com/safebrowsing/clientreport/"
-    "permission-action";
+    "https://safebrowsing.googleusercontent.com/safebrowsing/clientreport/"
+    "chrome-permissions";
 
 const int kMaximumReportsPerOriginPerPermissionPerMinute = 5;
 
