c2411a88c2250133d3000ac5477e9849def464a4
olivierrobin <olivierrobin@chromium.org>
2016-08-29 23:13:27 -0700

    Create a Delayed Response Provider for GCDWebServer.
    This provider will provide the response from another provider after a
    delay.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2282463002
    Cr-Commit-Position: refs/heads/master@{#415207}
    
    Former-commit-id: 6eeceba83633b457f6b5469457fbd69053deec54

---
+++ ios/web/public/test/response_providers/delayed_response_provider.h
@@ -0,0 +1,43 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_WEB_PUBLIC_TEST_RESPONSE_PROVIDERS_DELAYED_RESPONSE_PROVIDER_H_
+#define IOS_WEB_PUBLIC_TEST_RESPONSE_PROVIDERS_DELAYED_RESPONSE_PROVIDER_H_
+
+#include <memory>
+
+#include "ios/web/public/test/response_providers/response_provider.h"
+
+namespace web {
+
+// A Response provider that delays the response provided by another reponse
+// provider
+class DelayedResponseProvider : public ResponseProvider {
+ public:
+  // Creates a DelayedResponseProvider that delays the response from
+  // |delayed_provider| by |delay| seconds.
+  DelayedResponseProvider(
+      std::unique_ptr<web::ResponseProvider> delayed_provider,
+      double delay);
+  ~DelayedResponseProvider() override;
+
+  // Forwards to |delayed_provider_|.
+  bool CanHandleRequest(const Request& request) override;
+
+  // Creates a GCDWebServerResponse that will proxy the object returned by
+  // Forwards to |delayed_provider_->GetGCDWebServerResponse(request)|.
+  // The read operation will be delayed by |delay_| seconds.
+  GCDWebServerResponse* GetGCDWebServerResponse(
+      const Request& request) override;
+
+ private:
+  std::unique_ptr<web::ResponseProvider> delayed_provider_;
+  double delay_;
+
+  DISALLOW_COPY_AND_ASSIGN(DelayedResponseProvider);
+};
+
+}  // namespace web
+
+#endif  // IOS_WEB_PUBLIC_TEST_RESPONSE_PROVIDERS_DELAYED_RESPONSE_PROVIDER_H_
---
+++ ios/web/public/test/response_providers/delayed_response_provider.mm
@@ -0,0 +1,125 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ios/web/public/test/response_providers/delayed_response_provider.h"
+
+#import <Foundation/Foundation.h>
+
+#include "base/ios/weak_nsobject.h"
+#import "base/mac/scoped_nsobject.h"
+#import "base/mac/foundation_util.h"
+#import "ios/third_party/gcdwebserver/src/GCDWebServer/Responses/GCDWebServerDataResponse.h"
+
+// A proxy class that will forward all messages to the |_response|.
+// It will delay read operation  |_delay| seconds.
+@interface DelayedGCDWebServerResponse : NSProxy {
+  base::scoped_nsobject<GCDWebServerResponse> _response;
+  NSTimeInterval _delay;
+}
+
++ (instancetype)responseWithServerResponse:(GCDWebServerResponse*)response
+                                     delay:(NSTimeInterval)delay;
+- (instancetype)initWithServerResponse:(GCDWebServerResponse*)response
+                                 delay:(NSTimeInterval)delay;
+@end
+
+@implementation DelayedGCDWebServerResponse
++ (instancetype)responseWithServerResponse:(GCDWebServerResponse*)response
+                                     delay:(NSTimeInterval)delay {
+  return [[[DelayedGCDWebServerResponse alloc] initWithServerResponse:response
+                                                                delay:delay]
+      autorelease];
+}
+
+- (instancetype)initWithServerResponse:(GCDWebServerResponse*)response
+                                 delay:(NSTimeInterval)delay {
+  _response.reset([response retain]);
+  _delay = delay;
+  return self;
+}
+
+#pragma mark - GCDWebServerResponse
+
+- (void)performReadDataWithCompletion:
+    (GCDWebServerBodyReaderCompletionBlock)block {
+  [self asyncReadDataWithCompletion:block];
+}
+
+- (void)asyncReadDataWithCompletion:
+    (GCDWebServerBodyReaderCompletionBlock)block {
+  dispatch_async(dispatch_get_main_queue(), ^{
+    base::WeakNSObject<GCDWebServerResponse> weakResponse(_response);
+    if ([_response
+            respondsToSelector:@selector(asyncReadDataWithCompletion:)]) {
+      dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+                                   static_cast<int64_t>(_delay * NSEC_PER_SEC)),
+                     dispatch_get_main_queue(), ^{
+                       base::scoped_nsobject<GCDWebServerResponse> response(
+                           [weakResponse retain]);
+                       [response asyncReadDataWithCompletion:block];
+                     });
+    } else {
+      dispatch_after(dispatch_time(DISPATCH_TIME_NOW,
+                                   static_cast<int64_t>(_delay * NSEC_PER_SEC)),
+                     dispatch_get_main_queue(), ^{
+                       base::scoped_nsobject<GCDWebServerResponse> response(
+                           [weakResponse retain]);
+                       if (!response) {
+                         return;
+                       }
+                       NSError* error = nil;
+                       NSData* data = [response readData:&error];
+                       block(data, error);
+                     });
+    }
+  });
+}
+
+#pragma mark - NSProxy
+
+- (BOOL)conformsToProtocol:(Protocol*)protocol {
+  return [[_response class] conformsToProtocol:protocol];
+}
+
+- (BOOL)respondsToSelector:(SEL)selector {
+  return [[_response class] instancesRespondToSelector:selector];
+}
+
+- (NSMethodSignature*)methodSignatureForSelector:(SEL)selector {
+  return [[_response class] instanceMethodSignatureForSelector:selector];
+}
+
+- (void)forwardInvocation:(NSInvocation*)invocation {
+  SEL selector = [invocation selector];
+  if ([_response respondsToSelector:selector])
+    [invocation invokeWithTarget:_response];
+}
+
+@end
+
+namespace web {
+
+DelayedResponseProvider::DelayedResponseProvider(
+    std::unique_ptr<web::ResponseProvider> delayed_provider,
+    double delay)
+    : web::ResponseProvider(),
+      delayed_provider_(std::move(delayed_provider)),
+      delay_(delay) {}
+
+DelayedResponseProvider::~DelayedResponseProvider() {}
+
+bool DelayedResponseProvider::CanHandleRequest(const Request& request) {
+  return delayed_provider_->CanHandleRequest(request);
+}
+
+GCDWebServerResponse* DelayedResponseProvider::GetGCDWebServerResponse(
+    const Request& request) {
+  DelayedGCDWebServerResponse* response = [DelayedGCDWebServerResponse
+      responseWithServerResponse:delayed_provider_->GetGCDWebServerResponse(
+                                     request)
+                           delay:delay_];
+  return base::mac::ObjCCastStrict<GCDWebServerResponse>(response);
+}
+
+}  // namespace web
