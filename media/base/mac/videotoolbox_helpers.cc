1ba07e17159b32e5361149c1d506c5003e2f2f8b
emircan <emircan@chromium.org>
2016-08-30 11:51:24 -0700

    Fix AnnexB header bytes for VideoToolbox
    This CL addresses the bug where we see iOS HW decoder dropping most of the
    frames in a H264 connection with OSX HW encoder. There is a mismatch between
    the header bytes of these two implementations. iOS expect a 4 bytes header whereas
    currently 3 bytes are written by OSX encoder. Making them both based on 4 bytes
    fixes the dropped frames issue.
    
    [0] https://cs.chromium.org/chromium/src/third_party/webrtc/modules/video_coding/codecs/h264/h264_video_toolbox_nalu.cc?rcl=0&l=345
    
    BUG=webrtc:5931
    TEST= Tested H264 connection between Macbook Air and iOS.
    
    Review-Url: https://codereview.chromium.org/2292683002
    Cr-Commit-Position: refs/heads/master@{#415376}
    
    Former-commit-id: 885d82bc302b12ae7dc97db6a8c43fd5ffcf277c

--- media/base/mac/videotoolbox_helpers.cc
+++ media/base/mac/videotoolbox_helpers.cc
@@ -13,6 +13,10 @@ namespace media {
 
 namespace video_toolbox {
 
+namespace {
+static const char kAnnexBHeaderBytes[4] = {0, 0, 0, 1};
+}  // anonymous namespace
+
 base::ScopedCFTypeRef<CFDictionaryRef>
 DictionaryWithKeysAndValues(CFTypeRef* keys, CFTypeRef* values, size_t size) {
   return base::ScopedCFTypeRef<CFDictionaryRef>(CFDictionaryCreate(
@@ -113,7 +117,6 @@ void CopyNalsToAnnexB(char* avcc_buffer,
   static_assert(sizeof(NalSizeType) == 1 || sizeof(NalSizeType) == 2 ||
                     sizeof(NalSizeType) == 4,
                 "NAL size type has unsupported size");
-  static const char startcode_3[3] = {0, 0, 1};
   DCHECK(avcc_buffer);
   DCHECK(annexb_buffer);
   size_t bytes_left = avcc_size;
@@ -125,7 +128,7 @@ void CopyNalsToAnnexB(char* avcc_buffer,
     avcc_buffer += sizeof(NalSizeType);
 
     DCHECK_GE(bytes_left, nal_size);
-    annexb_buffer->Append(startcode_3, sizeof(startcode_3));
+    annexb_buffer->Append(kAnnexBHeaderBytes, sizeof(kAnnexBHeaderBytes));
     annexb_buffer->Append(avcc_buffer, nal_size);
     bytes_left -= nal_size;
     avcc_buffer += nal_size;
@@ -203,8 +206,7 @@ bool CopySampleBufferToAnnexBBuffer(CoreMediaGlue::CMSampleBufferRef sbuf,
             << status;
         return false;
       }
-      static const char startcode_4[4] = {0, 0, 0, 1};
-      annexb_buffer->Append(startcode_4, sizeof(startcode_4));
+      annexb_buffer->Append(kAnnexBHeaderBytes, sizeof(kAnnexBHeaderBytes));
       annexb_buffer->Append(reinterpret_cast<const char*>(pset), pset_size);
     }
   }
