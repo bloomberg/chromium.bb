7b921a10d248df9744e3a0b34ccb2f819e589779
charliea <charliea@chromium.org>
2016-08-30 21:40:17 -0700

    [system health] Add power system health benchmarks
    Even though the metrics that we have aren't ideal quite yet (total
    energy consumed, average power draw), we still want system health
    benchmarks running ASAP in order to prevent major regressions.
    
    We discussed having two runs of the system health benchmarks:
    memory and non-memory, with memory being isolated due to its high
    overhead. At least for the time being, that's not possible with power
    benchmarks because we need to decide in the benchmark constructor
    whether to enable BattOr tracing, even though we don't have any ability
    to know whether a BattOr is attached. Because of that, we're forced to
    disable the test on any devices without BattOrs in the @ShouldDisable
    predicate.
    
    BUG=637158,589726
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.perf:linux_perf_cq;master.tryserver.chromium.perf:mac_retina_perf_cq;master.tryserver.chromium.perf:winx64_10_perf_cq
    
    Review-Url: https://codereview.chromium.org/2239053003
    Cr-Commit-Position: refs/heads/master@{#415561}
    
    Former-commit-id: 71253911d21dfc93fdeccb6c46e518c2a078a3b9

--- tools/perf/benchmarks/system_health.py
+++ tools/perf/benchmarks/system_health.py
@@ -16,8 +16,69 @@ import page_sets
 _IGNORED_STATS_RE = re.compile(r'_(std|count|max|min|sum|pct_\d{4}(_\d+)?)$')
 
 
+class _CommonSystemHealthBenchmark(perf_benchmark.PerfBenchmark):
+  """Chrome Common System Health Benchmark.
+
+  This test suite contains system health benchmarks that can be collected
+  together due to the low overhead of the tracing agents required. If a
+  benchmark does have significant overhead, it should either:
+
+    1) Be rearchitected such that it doesn't. This is the most preferred option.
+    2) Be run in a separate test suite (e.g. memory).
+
+  https://goo.gl/Jek2NL.
+  """
+
+  def CreateTimelineBasedMeasurementOptions(self):
+    options = timeline_based_measurement.Options(
+        chrome_trace_category_filter.ChromeTraceCategoryFilter())
+    options.config.chrome_trace_config.category_filter.AddFilterString('rail')
+    options.config.enable_battor_trace = True
+    options.config.enable_chrome_trace = True
+    return options
+
+  def CreateStorySet(self, options):
+    return page_sets.SystemHealthStorySet(platform=self.PLATFORM)
+
+  @classmethod
+  def ShouldTearDownStateAfterEachStoryRun(cls):
+    return True
+
+  @classmethod
+  def Name(cls):
+    return 'system_health.common_%s' % cls.PLATFORM
+
+
+class DesktopCommonSystemHealth(_CommonSystemHealthBenchmark):
+  """Desktop Chrome Energy System Health Benchmark."""
+  PLATFORM = 'desktop'
+
+  @classmethod
+  def ShouldDisable(cls, possible_browser):
+    # http://crbug.com/624355 (reference builds).
+    return (possible_browser.platform.GetDeviceTypeName() != 'Desktop' or
+            possible_browser.browser_type == 'reference')
+
+
+class MobileCommonSystemHealth(_CommonSystemHealthBenchmark):
+  """Mobile Chrome Energy System Health Benchmark."""
+  PLATFORM = 'mobile'
+
+  @classmethod
+  def ShouldDisable(cls, possible_browser):
+    # http://crbug.com/612144
+    if (possible_browser.browser_type == 'reference' and
+        possible_browser.platform.GetDeviceTypeName() == 'Nexus 5X'):
+      return True
+
+    return possible_browser.platform.GetDeviceTypeName() == 'Desktop'
+
+
 class _MemorySystemHealthBenchmark(perf_benchmark.PerfBenchmark):
   """Chrome Memory System Health Benchmark.
+
+  This test suite is run separately from the common one due to the high overhead
+  of memory tracing.
 
   https://goo.gl/Jek2NL.
   """
