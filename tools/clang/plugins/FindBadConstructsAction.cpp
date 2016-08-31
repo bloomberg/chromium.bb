7d6df76c560c185b7db19b1acd3e62f6d5c0e3dd
thestig <thestig@chromium.org>
2016-08-31 12:18:56 -0700

    Remove old Clang plugin flags.
    These flags are on-by-default and no longer passed in as of
    https://codereview.chromium.org/2292173002
    
    Review-Url: https://codereview.chromium.org/2270433002
    Cr-Commit-Position: refs/heads/master@{#415719}
    
    Former-commit-id: f0563f0ccdfd6e74b0cd4d59b2b900cd69fff2c3

--- tools/clang/plugins/FindBadConstructsAction.cpp
+++ tools/clang/plugins/FindBadConstructsAction.cpp
@@ -47,21 +47,12 @@ bool FindBadConstructsAction::ParseArgs(const CompilerInstance& instance,
     if (args[i] == "check-base-classes") {
       // TODO(rsleevi): Remove this once http://crbug.com/123295 is fixed.
       options_.check_base_classes = true;
-    } else if (args[i] == "enforce-in-pdf") {
-      // TODO(thestig): This is now always on. Remove support for this flag once
-      // the build no longer passes it in.
     } else if (args[i] == "enforce-in-thirdparty-webkit") {
       options_.enforce_in_thirdparty_webkit = true;
     } else if (args[i] == "check-enum-last-value") {
       // TODO(tsepez): Enable this by default once http://crbug.com/356815
       // and http://crbug.com/356816 are fixed.
       options_.check_enum_last_value = true;
-    } else if (args[i] == "check-templates") {
-      // TODO(thakis): This is now always on. Remove support for this flag once
-      // the build no longer passes it in.
-    } else if (args[i] == "follow-macro-expansion") {
-      // TODO(thakis): This is now always on. Remove support for this flag once
-      // the build no longer passes it in.
     } else if (args[i] == "no-realpath") {
       options_.no_realpath = true;
     } else if (args[i] == "check-ipc") {
