99d7ba1d30580966d756bbccf79cd096970a76b1
sfiera <sfiera@chromium.org>
2016-09-01 07:08:10 -0700

    Revert of Support server categories in NTPSnippetsService. (patchset #4 id:60001 of https://codereview.chromium.org/2297183002/ )
    Reason for revert:
    Broke various tests again:
    
    https://build.chromium.org/p/chromium.mac/builders/Mac10.9%20Tests/builds/27680
    https://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20(1)/builds/25568
    https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20(1)/builds/56836
    
    Original issue's description:
    > Support server categories in NTPSnippetsService.
    >
    > Reland of:
    > https://crrev.com/bb3795733a4a3866f2f84319d749e0d44601cb5d [formerly 330958ddcdcd0084d724571eddaa66f9c364115f]
    >
    > While I haven't reproduced the specific failure that caused that CL to
    > be reverted, the failing test has been fixed up a lot, including some
    > mistakes that are definitely implicated in those failures.
    >
    > BUG=633613
    >
    > Committed: https://crrev.com/24b7fd846b14cbe7cc159ad1939044e5e62abe1e [formerly 2fcf9d5c5f6047250ec00a3ee4a51e750529932d]
    > Cr-Commit-Position: refs/heads/master@{#415938}
    
    TBR=treib@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=633613
    
    Review-Url: https://codereview.chromium.org/2305653002
    Cr-Commit-Position: refs/heads/master@{#415944}
    
    Former-commit-id: 86cf93821dd622ea3717dcf3e935b265b0b40162

--- components/ntp_snippets/ntp_snippets_service.h
+++ components/ntp_snippets/ntp_snippets_service.h
@@ -157,8 +157,8 @@ class NTPSnippetsService : public ContentSuggestionsProvider,
   static int GetMaxSnippetCountForTesting();
 
   // Available snippets, only for unit tests.
-  const NTPSnippet::PtrVector& GetSnippetsForTesting(Category category) const {
-    return categories_.find(category)->second.snippets;
+  const NTPSnippet::PtrVector& GetSnippetsForTesting() const {
+    return snippets_;
   }
 
  private:
@@ -226,7 +226,7 @@ class NTPSnippetsService : public ContentSuggestionsProvider,
   void OnFetchFinished(NTPSnippetsFetcher::OptionalSnippets snippets);
 
   // Merges newly available snippets with the previously available list.
-  void MergeSnippets(Category category, NTPSnippet::PtrVector new_snippets);
+  void MergeSnippets(NTPSnippet::PtrVector new_snippets);
 
   std::set<std::string> GetSnippetHostsFromPrefs() const;
   void StoreSnippetHostsToPrefs(const std::set<std::string>& hosts);
@@ -243,18 +243,18 @@ class NTPSnippetsService : public ContentSuggestionsProvider,
   void FinishInitialization();
 
   void OnSnippetImageFetchedFromDatabase(const ImageFetchedCallback& callback,
-                                         const std::string& suggestion_id,
+                                         const std::string& snippet_id,
                                          std::string data);
 
   void OnSnippetImageDecodedFromDatabase(const ImageFetchedCallback& callback,
-                                         const std::string& suggestion_id,
+                                         const std::string& snippet_id,
                                          const gfx::Image& image);
 
-  void FetchSnippetImageFromNetwork(const std::string& suggestion_id,
+  void FetchSnippetImageFromNetwork(const std::string& snippet_id,
                                     const ImageFetchedCallback& callback);
 
   void OnSnippetImageDecodedFromNetwork(const ImageFetchedCallback& callback,
-                                        const std::string& suggestion_id,
+                                        const std::string& snippet_id,
                                         const gfx::Image& image);
 
   // Triggers a state transition depending on the provided reason to be
@@ -265,7 +265,7 @@ class NTPSnippetsService : public ContentSuggestionsProvider,
   // Verifies state transitions (see |State|'s documentation) and applies them.
   // Also updates the provider status. Does nothing except updating the provider
   // status if called with the current state.
-  void EnterState(State state);
+  void EnterState(State state, CategoryStatus status);
 
   // Enables the service and triggers a fetch if required. Do not call directly,
   // use |EnterState| instead.
@@ -282,42 +282,24 @@ class NTPSnippetsService : public ContentSuggestionsProvider,
   // the observers.
   void NotifyNewSuggestions();
 
-  // Updates the internal status for |category| to |category_status_| and
-  // notifies the content suggestions observer if it changed.
-  void UpdateCategoryStatus(Category category, CategoryStatus status);
-  // Calls UpdateCategoryStatus() for all provided categories.
-  void UpdateAllCategoryStatus(CategoryStatus status);
+  // Updates the internal status |category_status_| and notifies the content
+  // suggestions observer if it changed.
+  void UpdateCategoryStatus(CategoryStatus status);
 
   State state_;
+
+  CategoryStatus category_status_;
 
   PrefService* pref_service_;
 
   suggestions::SuggestionsService* suggestions_service_;
 
-  const Category articles_category_;
+  // All current suggestions (i.e. not dismissed ones).
+  NTPSnippet::PtrVector snippets_;
 
-  struct CategoryContent {
-    CategoryStatus status = CategoryStatus::INITIALIZING;
-
-    // True iff the server returned results in this category in the last fetch.
-    // We never remove categories that the server still provides, but if the
-    // server stops providing a category, we won't yet report it as NOT_PROVIDED
-    // while we still have non-expired snippets in it.
-    bool provided_by_server = true;
-
-    // All current suggestions (i.e. not dismissed ones).
-    NTPSnippet::PtrVector snippets;
-
-    // Suggestions that the user dismissed. We keep these around until they
-    // expire so we won't re-add them on the next fetch.
-    NTPSnippet::PtrVector dismissed;
-
-    CategoryContent();
-    CategoryContent(CategoryContent&&);
-    ~CategoryContent();
-    CategoryContent& operator=(CategoryContent&&);
-  };
-  std::map<Category, CategoryContent, Category::CompareByID> categories_;
+  // Suggestions that the user dismissed. We keep these around until they expire
+  // so we won't re-add them on the next fetch.
+  NTPSnippet::PtrVector dismissed_snippets_;
 
   // The ISO 639-1 code of the language used by the application.
   const std::string application_language_code_;
@@ -359,6 +341,8 @@ class NTPSnippetsService : public ContentSuggestionsProvider,
   // Set to true if NukeAllSnippets is called before the database has been
   // loaded. The nuke will be executed after the database load finishes.
   bool nuke_after_load_;
+
+  const Category provided_category_;
 
   // Request throttler for limiting requests to thumbnail images.
   RequestThrottler thumbnail_requests_throttler_;
