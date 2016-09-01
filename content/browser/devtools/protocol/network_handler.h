712aa3b96c729bb9cda7b66fc8733f4f7d7d96a8
pfeldman <pfeldman@chromium.org>
2016-08-31 20:26:00 -0700

    Revert of DevTools: add Network.enable/disable handlers on the content side. (patchset #1 id:1 of https://codereview.chromium.org/2269343002/ )
    Reason for revert:
    We did not need it.
    
    Original issue's description:
    > DevTools: add Network.enable/disable handlers on the content side.
    >
    > R=jam
    >
    > Committed: https://crrev.com/0a01b01824b74502f80d1028561e2a40ba5c90be [formerly 4f455b2ceac18f9217d6143acd64a1d44f031f00]
    > Cr-Commit-Position: refs/heads/master@{#413933}
    
    TBR=jam@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    
    Review-Url: https://codereview.chromium.org/2302723002
    Cr-Commit-Position: refs/heads/master@{#415872}
    
    Former-commit-id: 31f837acae747ccc1475eaa0ff99e2558734c2eb

--- content/browser/devtools/protocol/network_handler.h
+++ content/browser/devtools/protocol/network_handler.h
@@ -26,9 +26,6 @@ class NetworkHandler {
   void SetRenderFrameHost(RenderFrameHostImpl* host);
   void SetClient(std::unique_ptr<Client> client);
 
-  Response Enable(const int* max_total_size,
-                  const int* max_resource_size);
-  Response Disable();
   Response ClearBrowserCache();
   Response ClearBrowserCookies();
   Response GetCookies(DevToolsCommandId command_id);
@@ -66,7 +63,6 @@ class NetworkHandler {
 
   RenderFrameHostImpl* host_;
   std::unique_ptr<Client> client_;
-  bool enabled_;
   base::WeakPtrFactory<NetworkHandler> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkHandler);
