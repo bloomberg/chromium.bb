f86e8512b2fb05cbf0979a92b538e9ed9a3bd8e0
krytarowski <krytarowski@gmail.com>
2016-09-01 04:35:13 -0700

    Fix unportable shell construct (detected with pkgsrc)
    BUG=
    
    Review-Url: https://codereview.chromium.org/2216023004
    Cr-Commit-Position: refs/heads/master@{#415930}
    
    Former-commit-id: 7f3292145c1bb84b0b6d72cc6e0c8a7467b11868

--- chrome/common/extensions/docs/examples/api/nativeMessaging/host/install_host.sh
+++ chrome/common/extensions/docs/examples/api/nativeMessaging/host/install_host.sh
@@ -6,14 +6,14 @@
 set -e
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-if [ "$(uname -s)" == "Darwin" ]; then
-  if [ "$(whoami)" == "root" ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
+  if [ "$(whoami)" = "root" ]; then
     TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
   else
     TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
   fi
 else
-  if [ "$(whoami)" == "root" ]; then
+  if [ "$(whoami)" = "root" ]; then
     TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
   else
     TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
--- chrome/common/extensions/docs/examples/api/nativeMessaging/host/uninstall_host.sh
+++ chrome/common/extensions/docs/examples/api/nativeMessaging/host/uninstall_host.sh
@@ -5,14 +5,14 @@
 
 set -e
 
-if [ "$(uname -s)" == "Darwin" ]; then
-  if [ "$(whoami)" == "root" ]; then
+if [ "$(uname -s)" = "Darwin" ]; then
+  if [ "$(whoami)" = "root" ]; then
     TARGET_DIR="/Library/Google/Chrome/NativeMessagingHosts"
   else
     TARGET_DIR="$HOME/Library/Application Support/Google/Chrome/NativeMessagingHosts"
   fi
 else
-  if [ "$(whoami)" == "root" ]; then
+  if [ "$(whoami)" = "root" ]; then
     TARGET_DIR="/etc/opt/chrome/native-messaging-hosts"
   else
     TARGET_DIR="$HOME/.config/google-chrome/NativeMessagingHosts"
