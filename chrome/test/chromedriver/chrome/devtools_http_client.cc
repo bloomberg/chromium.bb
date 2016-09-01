9ea5401e1e9074c846869e7786218bff44a3ba03
samuong <samuong@chromium.org>
2016-08-31 20:38:14 -0700

    [chromedriver] Do not identify extension background pages by url scheme.
    BUG=642902
    
    Review-Url: https://codereview.chromium.org/2304433002
    Cr-Commit-Position: refs/heads/master@{#415873}
    
    Former-commit-id: 329ded4ea587e22b0d2660bafdd3e8d98300a972

--- chrome/test/chromedriver/chrome/devtools_http_client.cc
+++ chrome/test/chromedriver/chrome/devtools_http_client.cc
@@ -160,9 +160,7 @@ const DeviceMetrics* DevToolsHttpClient::device_metrics() {
 bool DevToolsHttpClient::IsBrowserWindow(const WebViewInfo& view) const {
   return base::ContainsKey(*window_types_, view.type) ||
          (view.type == WebViewInfo::kOther &&
-          (base::StartsWith(view.url, "chrome-extension://",
-                            base::CompareCase::SENSITIVE) ||
-           view.url == "chrome://print/" ||
+          (view.url == "chrome://print/" ||
            view.url == "chrome://media-router/"));
 }
 
