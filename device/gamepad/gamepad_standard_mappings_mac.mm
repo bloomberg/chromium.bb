ab7bd143360882e79ff74f974e44a99c34ce7842
aicommander <aicommander@gmail.com>
2016-09-05 20:43:24 -0700

    Add a standard gamepad mapping for the new Xbox One S controller in Bluetooth mode on Mac.
    In this mode, the controller acts like a standard HID gamepad device,
    requiring only minor mapping fixups for proper functionality.
    
    BUG=644139
    TEST=http://html5gamepad.com/
    
    Review-Url: https://codereview.chromium.org/2314553002
    Cr-Commit-Position: refs/heads/master@{#416580}
    
    Former-commit-id: 6f9bf0317b8f29113d4d7c4f5742397c68c3e410

--- device/gamepad/gamepad_standard_mappings_mac.mm
+++ device/gamepad/gamepad_standard_mappings_mac.mm
@@ -31,6 +31,23 @@ void MapperXbox360Gamepad(const blink::WebGamepad& input,
   mapped->axesLength = AXIS_INDEX_COUNT;
 }
 
+void MapperXboxOneHidGamepad(const blink::WebGamepad& input,
+                             blink::WebGamepad* mapped) {
+  *mapped = input;
+  mapped->buttons[BUTTON_INDEX_LEFT_TRIGGER] = AxisToButton(input.axes[2]);
+  mapped->buttons[BUTTON_INDEX_RIGHT_TRIGGER] = AxisToButton(input.axes[5]);
+  mapped->buttons[BUTTON_INDEX_BACK_SELECT] = input.buttons[6];
+  mapped->buttons[BUTTON_INDEX_START] = input.buttons[7];
+  mapped->buttons[BUTTON_INDEX_LEFT_THUMBSTICK] = input.buttons[8];
+  mapped->buttons[BUTTON_INDEX_RIGHT_THUMBSTICK] = input.buttons[9];
+  mapped->axes[AXIS_INDEX_RIGHT_STICK_X] = input.axes[3];
+  mapped->axes[AXIS_INDEX_RIGHT_STICK_Y] = input.axes[4];
+  DpadFromAxis(mapped, input.axes[9]);
+
+  mapped->buttonsLength = BUTTON_INDEX_COUNT - 1; /* no meta */
+  mapped->axesLength = AXIS_INDEX_COUNT;
+}
+
 void MapperPlaystationSixAxis(const blink::WebGamepad& input,
                               blink::WebGamepad* mapped) {
   *mapped = input;
@@ -359,6 +376,7 @@ struct MappingData {
     {"0079", "0006", MapperDragonRiseGeneric},   // DragonRise Generic USB
     {"045e", "028e", MapperXbox360Gamepad},      // Xbox 360 Wired
     {"045e", "028f", MapperXbox360Gamepad},      // Xbox 360 Wireless
+    {"045e", "02e0", MapperXboxOneHidGamepad},   // Xbox One S (Bluetooth mode)
     {"045e", "0719", MapperXbox360Gamepad},      // Xbox 360 Wireless
     {"046d", "c216", MapperDirectInputStyle},    // Logitech F310, D mode
     {"046d", "c218", MapperDirectInputStyle},    // Logitech F510, D mode
