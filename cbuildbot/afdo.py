678330988cde54f041fdc4737696fb3ac973c14b
Ting-Yuan Huang <laszio@chromium.org>
2016-09-06 18:11:34 +0000

    AFDOGenerate is failing again. Need more time to fix it.
    Revert "Revert "AFDO: Increase the expiration days of old AFDO data.""
    
    This reverts commit 4ca44bbe26e2dc51b8a3f43efff9b556138de128.
    
    Change-Id: I0513263a0875afc217868822798c132c30c564a0
    Reviewed-on: https://chromium-review.googlesource.com/381202
    Commit-Ready: Ting-Yuan Huang <laszio@chromium.org>
    Tested-by: Ting-Yuan Huang <laszio@chromium.org>
    Reviewed-by: Yunlian Jiang <yunlian@chromium.org>

--- cbuildbot/afdo.py
+++ cbuildbot/afdo.py
@@ -53,7 +53,7 @@ CHROME_EBUILD_AFDO_EXP = r'^(?P<bef>AFDO_FILE\["%s"\]=")(?P<name>.*)(?P<aft>")'
 CHROME_EBUILD_AFDO_REPL = r'\g<bef>%s\g<aft>'
 
 # How old can the AFDO data be? (in days).
-AFDO_ALLOWED_STALE = 7
+AFDO_ALLOWED_STALE = 14
 
 # Set of boards that can generate the AFDO profile (can generate 'perf'
 # data with LBR events). Currently, it needs to be a device that has
