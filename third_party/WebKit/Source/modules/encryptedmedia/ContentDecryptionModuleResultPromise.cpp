08d6a9b37bba4b2c0f933e37ae13e6bffc46eae4
jrummell <jrummell@chromium.org>
2016-09-06 17:32:22 -0700

    Add check for null pointer
    When resources are freed, asynchronous tasks may fail later and cause
    promises to be rejected. However, since the resource is gone, this
    crashes. Add a check that the ExecutionContext still exists before
    using it.
    
    BUG=644269
    TEST=test case doesn't crash
    
    Review-Url: https://codereview.chromium.org/2314253002
    Cr-Commit-Position: refs/heads/master@{#416797}
    
    Former-commit-id: d0abaf6c13603474382963075424bf7586558d74

--- third_party/WebKit/Source/modules/encryptedmedia/ContentDecryptionModuleResultPromise.cpp
+++ third_party/WebKit/Source/modules/encryptedmedia/ContentDecryptionModuleResultPromise.cpp
@@ -96,7 +96,9 @@ void ContentDecryptionModuleResultPromise::reject(ExceptionCode code, const Stri
     // already posted that need to happen only after the promise is resolved.)
     // TODO(jrummell): Make resolving a promise asynchronous as well (including
     // making sure events still happen after the promise is resolved).
-    getExecutionContext()->postTask(BLINK_FROM_HERE, createSameThreadTask(&ContentDecryptionModuleResultPromise::rejectInternal, wrapPersistent(this), code, errorMessage));
+    if (getExecutionContext()) {
+        getExecutionContext()->postTask(BLINK_FROM_HERE, createSameThreadTask(&ContentDecryptionModuleResultPromise::rejectInternal, wrapPersistent(this), code, errorMessage));
+    }
 }
 
 void ContentDecryptionModuleResultPromise::rejectInternal(ExceptionCode code, const String& errorMessage)
