adfced92b70c21220a50b77ef7751586ae90da7f
creis <creis@chromium.org>
2016-09-07 15:00:08 -0700

    Fix null check to avoid RendererDidNavigateSamePage crash.
    We shouldn't treat a navigation as NAVIGATION_TYPE_SAME_PAGE if
    there is no last committed entry.
    
    BUG=640429
    TEST=No known repro steps.  Crashes should go away.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2319843002
    Cr-Commit-Position: refs/heads/master@{#417070}
    
    Former-commit-id: 08557a213c024dbc17f0f8e62aa31e8bae212711

--- content/browser/frame_host/navigation_controller_impl.cc
+++ content/browser/frame_host/navigation_controller_impl.cc
@@ -1007,7 +1007,7 @@ NavigationType NavigationControllerImpl::ClassifyNavigation(
     // make sure Blink didn't treat a new cross-process navigation as inert, and
     // thus set params.did_create_new_entry to false. In that case, we must
     // treat it as NEW since the SiteInstance doesn't match the entry.
-    if (GetLastCommittedEntry() &&
+    if (!GetLastCommittedEntry() ||
         GetLastCommittedEntry()->site_instance() != rfh->GetSiteInstance())
       return NAVIGATION_TYPE_NEW_PAGE;
 
