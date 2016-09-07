1f8658701c49b44b36359db70776e451588fd222
dtrainor <dtrainor@chromium.org>
2016-09-06 19:08:20 -0700

    Make sure to register ui/android in Blimp
    - The standalone Blimp app wasn't registering ui/android.
    BUG=644462
    
    Review-Url: https://codereview.chromium.org/2313723009
    Cr-Commit-Position: refs/heads/master@{#416818}
    
    Former-commit-id: b808795bda2466b9a66634841e45965d99b95aea

--- blimp/client/app/android/blimp_library_loader.cc
+++ blimp/client/app/android/blimp_library_loader.cc
@@ -17,6 +17,7 @@
 #include "blimp/client/public/android/blimp_jni_registrar.h"
 #include "jni/BlimpLibraryLoader_jni.h"
 #include "net/android/net_jni_registrar.h"
+#include "ui/android/ui_android_jni_registrar.h"
 #include "ui/gl/gl_surface.h"
 
 using base::android::JavaParamRef;
@@ -44,6 +45,9 @@ bool RegisterJni(JNIEnv* env) {
     return false;
 
   if (!blimp::client::RegisterBlimpAppJni(env))
+    return false;
+
+  if (!ui::RegisterUIAndroidJni(env))
     return false;
 
   return true;
