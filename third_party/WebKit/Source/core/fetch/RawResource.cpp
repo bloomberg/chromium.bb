400851b0c5304f01e725852a520c13dcaf692196
hiroshige <hiroshige@chromium.org>
2016-09-06 21:31:10 -0700

    Capture wrong states in RawResourceClientStateChecker::responseReceived()
    To investigate in which state RawResourceClientStateChecker::responseReceived()
    crashes, this CL adds separate assertions to each of wrong states.
    This CL should be reverted at least before going to beta.
    
    BUG=640960
    
    Review-Url: https://codereview.chromium.org/2314503003
    Cr-Commit-Position: refs/heads/master@{#416845}
    
    Former-commit-id: 068bdce3e08bb45e7fdb0b7377787f5946c64ec2

--- third_party/WebKit/Source/core/fetch/RawResource.cpp
+++ third_party/WebKit/Source/core/fetch/RawResource.cpp
@@ -299,9 +299,16 @@ NEVER_INLINE void RawResourceClientStateChecker::dataSent()
 
 NEVER_INLINE void RawResourceClientStateChecker::responseReceived()
 {
-    // TODO(hiroshige): Temporarily we use DCHECK() here instead of CHECK(),
-    // due to crbug.com/640960.
-    SECURITY_DCHECK(m_state == Started);
+    // TODO(hiroshige): Temporarily we insert the checks below to capture the
+    // wrong state. The checks should be reverted shortly. crbug.com/640960.
+    SECURITY_CHECK(m_state != NotAddedAsClient);
+    SECURITY_CHECK(m_state != RedirectBlocked);
+    SECURITY_CHECK(m_state != ResponseReceived);
+    SECURITY_CHECK(m_state != SetSerializedCachedMetadata);
+    SECURITY_CHECK(m_state != DataReceived);
+    SECURITY_CHECK(m_state != DataDownloaded);
+    SECURITY_CHECK(m_state != NotifyFinished);
+    SECURITY_CHECK(m_state == Started);
     m_state = ResponseReceived;
 }
 
