ebf63d46bd12140d2b82c317babf9bd43f8fa7c2
gogerald <gogerald@chromium.org>
2016-09-08 09:06:18 -0700

    Add Signin_Impression_FromSettings user action
    BUG=639860
    
    Review-Url: https://codereview.chromium.org/2321653002
    Cr-Commit-Position: refs/heads/master@{#417303}
    
    Former-commit-id: 8dafb06dc59a8b0ca7c42c79f4177a146e6ef30b

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/SignInPreference.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/SignInPreference.java
@@ -14,6 +14,7 @@ import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.View;
 
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.firstrun.FirstRunSignInProcessor;
 import org.chromium.chrome.browser.profiles.Profile;
@@ -141,6 +142,10 @@ public class SignInPreference extends Preference
                 return true;
             }
         });
+
+        if (account == null && enabled) {
+            RecordUserAction.record("Signin_Impression_FromSettings");
+        }
     }
 
     private void updateSyncStatusIcon() {
