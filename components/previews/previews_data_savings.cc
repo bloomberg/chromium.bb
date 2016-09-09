891965d91156fcb6a6747eeb47db4f7fc604ed5c
ryansturm <ryansturm@chromium.org>
2016-09-09 06:31:56 -0700

    Adding UMA to PreviewsDataSavings
    This UMA measures savings, inflation, content length, etc.
    
    BUG=615565
    
    Review-Url: https://codereview.chromium.org/2256603005
    Cr-Commit-Position: refs/heads/master@{#417572}
    
    Former-commit-id: 887c3eaec73e864e1b6e237d49912708de6ff89d

--- components/previews/previews_data_savings.cc
+++ components/previews/previews_data_savings.cc
@@ -26,8 +26,46 @@ void PreviewsDataSavings::RecordDataSavings(
     int64_t original_size) {
   DCHECK(thread_checker_.CalledOnValidThread());
 
-  data_savings_recorder_->UpdateDataSavings(fully_qualified_domain_name,
-                                            data_used, original_size);
+  // Only record savings when data saver is enabled.
+  if (!data_savings_recorder_->UpdateDataSavings(fully_qualified_domain_name,
+                                                 data_used, original_size)) {
+    // Record metrics in KB for previews with data saver disabled.
+    UMA_HISTOGRAM_COUNTS("Previews.OriginalContentLength.DataSaverDisabled",
+                         original_size >> 10);
+    UMA_HISTOGRAM_COUNTS("Previews.ContentLength.DataSaverDisabled",
+                         data_used >> 10);
+    if (original_size >= data_used) {
+      UMA_HISTOGRAM_COUNTS("Previews.DataSavings.DataSaverDisabled",
+                           (original_size - data_used) >> 10);
+      UMA_HISTOGRAM_PERCENTAGE(
+          "Previews.DataSavingsPercent.DataSaverDisabled",
+          (original_size - data_used) * 100 / original_size);
+    } else {
+      UMA_HISTOGRAM_COUNTS("Previews.DataInflation.DataSaverDisabled",
+                           (data_used - original_size) >> 10);
+      UMA_HISTOGRAM_PERCENTAGE(
+          "Previews.DataInflationPercent.DataSaverDisabled",
+          (data_used - original_size) * 100 / data_used);
+    }
+    return;
+  }
+
+  // Record metrics in KB for previews with data saver enabled.
+  UMA_HISTOGRAM_COUNTS("Previews.OriginalContentLength.DataSaverEnabled",
+                       original_size >> 10);
+  UMA_HISTOGRAM_COUNTS("Previews.ContentLength.DataSaverEnabled",
+                       data_used >> 10);
+  if (original_size >= data_used) {
+    UMA_HISTOGRAM_COUNTS("Previews.DataSavings.DataSaverEnabled",
+                         (original_size - data_used) >> 10);
+    UMA_HISTOGRAM_PERCENTAGE("Previews.DataSavingsPercent.DataSaverEnabled",
+                             (original_size - data_used) * 100 / original_size);
+  } else {
+    UMA_HISTOGRAM_COUNTS("Previews.DataInflation.DataSaverEnabled",
+                         (data_used - original_size) >> 10);
+    UMA_HISTOGRAM_PERCENTAGE("Previews.DataInflationPercent.DataSaverEnabled",
+                             (data_used - original_size) * 100 / data_used);
+  }
 }
 
 }  // namespace previews
--- components/previews/previews_data_savings_unittest.cc
+++ components/previews/previews_data_savings_unittest.cc
@@ -9,6 +9,7 @@
 #include <memory>
 #include <string>
 
+#include "base/test/histogram_tester.h"
 #include "components/data_reduction_proxy/core/common/data_savings_recorder.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -69,7 +70,11 @@ class PreviewsDataSavingsTest : public testing::Test {
     return test_data_savings_recorder_.get();
   }
 
+  base::HistogramTester* histogram_tester() { return &histogram_tester_; }
+
  private:
+  base::HistogramTester histogram_tester_;
+
   std::unique_ptr<TestDataSavingsRecorder> test_data_savings_recorder_;
   std::unique_ptr<PreviewsDataSavings> data_savings_;
 };
@@ -82,19 +87,80 @@ TEST_F(PreviewsDataSavingsTest, RecordDataSavings) {
   EXPECT_EQ(0, test_data_savings_recorder()->data_used());
   EXPECT_EQ(0, test_data_savings_recorder()->original_size());
   test_data_savings_recorder()->set_data_saver_enabled(false);
-
   data_savings()->RecordDataSavings(host, data_used, original_size);
 
   EXPECT_EQ(0, test_data_savings_recorder()->data_used());
   EXPECT_EQ(0, test_data_savings_recorder()->original_size());
   EXPECT_EQ(std::string(), test_data_savings_recorder()->last_host());
-  test_data_savings_recorder()->set_data_saver_enabled(true);
 
+  test_data_savings_recorder()->set_data_saver_enabled(true);
   data_savings()->RecordDataSavings(host, data_used, original_size);
 
   EXPECT_EQ(data_used, test_data_savings_recorder()->data_used());
   EXPECT_EQ(original_size, test_data_savings_recorder()->original_size());
   EXPECT_EQ(host, test_data_savings_recorder()->last_host());
+}
+
+TEST_F(PreviewsDataSavingsTest, RecordDataSavingsHistograms) {
+  int64_t twenty_kb = 20480;
+  int64_t twenty = twenty_kb >> 10;
+  int64_t ten_kb = 10240;
+  int64_t ten = ten_kb >> 10;
+  std::string host = "host";
+
+  test_data_savings_recorder()->set_data_saver_enabled(false);
+  data_savings()->RecordDataSavings(host, ten_kb, twenty_kb);
+
+  // The histograms record bytes in KB.
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.OriginalContentLength.DataSaverDisabled", twenty, 1);
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.ContentLength.DataSaverDisabled", ten, 1);
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.DataSavings.DataSaverDisabled", twenty - ten, 1);
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.DataSavingsPercent.DataSaverDisabled",
+      (twenty_kb - ten_kb) * 100 / twenty_kb, 1);
+
+  data_savings()->RecordDataSavings(host, twenty_kb, ten_kb);
+
+  // The histograms record bytes in KB.
+  histogram_tester()->ExpectBucketCount(
+      "Previews.OriginalContentLength.DataSaverDisabled", ten, 1);
+  histogram_tester()->ExpectBucketCount(
+      "Previews.ContentLength.DataSaverDisabled", twenty, 1);
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.DataInflation.DataSaverDisabled", twenty - ten, 1);
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.DataInflationPercent.DataSaverDisabled",
+      (twenty_kb - ten_kb) * 100 / twenty_kb, 1);
+
+  test_data_savings_recorder()->set_data_saver_enabled(true);
+  data_savings()->RecordDataSavings(host, ten_kb, twenty_kb);
+
+  // The histograms record bytes in KB.
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.OriginalContentLength.DataSaverEnabled", twenty, 1);
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.ContentLength.DataSaverEnabled", ten, 1);
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.DataSavings.DataSaverEnabled", twenty - ten, 1);
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.DataSavingsPercent.DataSaverEnabled",
+      (twenty_kb - ten_kb) * 100 / twenty_kb, 1);
+
+  data_savings()->RecordDataSavings(host, twenty_kb, ten_kb);
+
+  // The histograms record bytes in KB.
+  histogram_tester()->ExpectBucketCount(
+      "Previews.OriginalContentLength.DataSaverEnabled", ten, 1);
+  histogram_tester()->ExpectBucketCount(
+      "Previews.ContentLength.DataSaverEnabled", twenty, 1);
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.DataInflation.DataSaverEnabled", twenty - ten, 1);
+  histogram_tester()->ExpectUniqueSample(
+      "Previews.DataInflationPercent.DataSaverEnabled",
+      (twenty_kb - ten_kb) * 100 / twenty_kb, 1);
 }
 
 }  // namespace previews
