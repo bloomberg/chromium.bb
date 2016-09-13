a25c6c3ba675a7b0c1e4793b210b02d71cdb42c0
Steinar Midtskogen <stemidts@cisco.com>
2016-09-13 16:37:13 +0200

    Extend CLPF to chroma.
    Objective quality impact (low latency):
    
    PSNR YCbCr:      0.13%     -1.37%     -1.79%
       PSNRHVS:      0.03%
          SSIM:      0.24%
        MSSSIM:      0.10%
     CIEDE2000:     -0.83%
    
    Change-Id: I8ddf0def569286775f0f9d4d4005932766a7fc27

--- av1/encoder/bitstream.c
+++ av1/encoder/bitstream.c
@@ -1428,8 +1428,10 @@ static void encode_loopfilter(struct loopfilter *lf,
 
 #if CONFIG_CLPF
 static void encode_clpf(const AV1_COMMON *cm, struct aom_write_bit_buffer *wb) {
-  aom_wb_write_literal(wb, cm->clpf_strength, 2);
-  if (cm->clpf_strength) {
+  aom_wb_write_literal(wb, cm->clpf_strength_y, 2);
+  aom_wb_write_literal(wb, cm->clpf_strength_u, 2);
+  aom_wb_write_literal(wb, cm->clpf_strength_v, 2);
+  if (cm->clpf_strength_y) {
     aom_wb_write_literal(wb, cm->clpf_size, 2);
     if (cm->clpf_size) {
       int i;
