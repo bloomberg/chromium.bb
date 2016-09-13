81ff26648d3cbec123ef0b4c3a0c4495c2df5fe7
rch <rch@chromium.org>
2016-09-12 21:20:45 -0700

    Deprecate Net.QuicHttpStream::DoStreamRequest.IsNullSession as it never contained any data.
    Review-Url: https://codereview.chromium.org/2331203002
    Cr-Commit-Position: refs/heads/master@{#418169}
    
    Former-commit-id: ca6ea8de76b27a57c1f9ad58882ba6048a1c4094

--- net/quic/chromium/quic_http_stream.cc
+++ net/quic/chromium/quic_http_stream.cc
@@ -631,16 +631,6 @@ int QuicHttpStream::DoLoop(int rv) {
 }
 
 int QuicHttpStream::DoStreamRequest() {
-  // TODO(rtenneti) Bug: b/28676259 - a temporary fix until we find out why
-  // |session_| could be a nullptr. Delete |null_session| check and histogram if
-  // session is never a nullptr.
-  bool null_session = session_ == nullptr;
-  if (null_session) {
-    UMA_HISTOGRAM_BOOLEAN("Net.QuicHttpStream::DoStreamRequest.IsNullSession",
-                          null_session);
-    return was_handshake_confirmed_ ? ERR_CONNECTION_CLOSED
-                                    : ERR_QUIC_HANDSHAKE_FAILED;
-  }
   int rv = stream_request_.StartRequest(
       session_, &stream_,
       base::Bind(&QuicHttpStream::OnStreamReady, weak_factory_.GetWeakPtr()));
