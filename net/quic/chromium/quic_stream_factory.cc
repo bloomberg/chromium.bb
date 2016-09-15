7f5402669b26c3586ae610a2de39bb1eb838d3bc
xunjieli <xunjieli@chromium.org>
2016-09-15 13:09:37 -0700

    Implement QuicHttpStream::GetLoadTimingInfo
    This CL implements QuicHttpStream::GetLoadTimingInfo.
    
    |dns_start| and |dns_end| are obtained in
    QuicStreamFactory::Job::DoResolveHost and
    DoResolveHostComplete. These two values are passed to
    QuicChromiumClientSession's constructor.
    
    |connect_start| and |connect_end| are obtained in
    QuicChromiumClientSession::CrytoConnect() and when
    handshake is confirmed.
    
    |ssl_start| and |ssl_end| are the same as |connect_start|
    and |connect_end| because QUIC always does encryption.
    
    If a session is reused, the connect_timing fields should
    all be null. This CL also sets
    |socket_reused| field of LoadTimingInfo accordingly.
    
    BUG=637051
    
    Committed: https://crrev.com/fb26de92c8868acb6ec40b9f8b08d3651ad05bd1 [formerly 5608fc1bd4f0a80b6d1996f3f4059f2efa0a0777]
    Committed: https://crrev.com/65c76a7e32833a6cdfde885a0fb14ca480a53604 [formerly 83ac2f22700c563fef8a670ad4c5c57d34032e03]
    Review-Url: https://codereview.chromium.org/2324183002
    Cr-Original-Original-Commit-Position: refs/heads/master@{#417969}
    Cr-Original-Commit-Position: refs/heads/master@{#418567}
    Cr-Commit-Position: refs/heads/master@{#418946}
    
    Former-commit-id: 100937eb53048b5d2df84438ce09c9067d96ccfb

--- net/quic/chromium/quic_stream_factory.cc
+++ net/quic/chromium/quic_stream_factory.cc
@@ -343,6 +343,7 @@ class QuicStreamFactory::Job {
   QuicChromiumClientSession* session_;
   CompletionCallback callback_;
   AddressList address_list_;
+  base::TimeTicks dns_resolution_start_time_;
   base::TimeTicks dns_resolution_end_time_;
   base::WeakPtrFactory<Job> weak_factory_;
   DISALLOW_COPY_AND_ASSIGN(Job);
@@ -469,6 +470,7 @@ void QuicStreamFactory::Job::CancelWaitForDataReadyCallback() {
 }
 
 int QuicStreamFactory::Job::DoResolveHost() {
+  dns_resolution_start_time_ = base::TimeTicks::Now();
   // Start loading the data now, and wait for it after we resolve the host.
   if (server_info_)
     server_info_->Start();
@@ -561,9 +563,10 @@ int QuicStreamFactory::Job::DoLoadServerInfoComplete(int rv) {
 int QuicStreamFactory::Job::DoConnect() {
   io_state_ = STATE_CONNECT_COMPLETE;
 
-  int rv = factory_->CreateSession(
-      key_, cert_verify_flags_, std::move(server_info_), address_list_,
-      dns_resolution_end_time_, net_log_, &session_);
+  int rv =
+      factory_->CreateSession(key_, cert_verify_flags_, std::move(server_info_),
+                              address_list_, dns_resolution_start_time_,
+                              dns_resolution_end_time_, net_log_, &session_);
   if (rv != OK) {
     DCHECK(rv != ERR_IO_PENDING);
     DCHECK(!session_);
@@ -1592,6 +1595,7 @@ int QuicStreamFactory::CreateSession(
     int cert_verify_flags,
     std::unique_ptr<QuicServerInfo> server_info,
     const AddressList& address_list,
+    base::TimeTicks dns_resolution_start_time,
     base::TimeTicks dns_resolution_end_time,
     const BoundNetLog& net_log,
     QuicChromiumClientSession** session) {
@@ -1693,8 +1697,8 @@ int QuicStreamFactory::CreateSession(
       clock_.get(), transport_security_state_, std::move(server_info),
       server_id, yield_after_packets_, yield_after_duration_, cert_verify_flags,
       config, &crypto_config_, network_connection_.GetDescription(),
-      dns_resolution_end_time, &push_promise_index_, task_runner_,
-      std::move(socket_performance_watcher), net_log.net_log());
+      dns_resolution_start_time, dns_resolution_end_time, &push_promise_index_,
+      task_runner_, std::move(socket_performance_watcher), net_log.net_log());
 
   all_sessions_[*session] = key;  // owning pointer
   writer->set_delegate(*session);
