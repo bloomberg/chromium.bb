4955404f6962260e470b465e0b543de24b499959
ricea <ricea@chromium.org>
2016-09-16 00:47:03 -0700

    WebSocketExtensionParser: reject top-bit-set characters
    net::WebSocketExtensionParser would accept top-bit-set characters in
    tokens. Change it to use net::HttpUtil::IsTokenChar() so it accepts the same
    characters as everything else.
    
    Add some tests for top-bit-set characters and a regression test for issue
    647156.
    
    Also various minor changes to make the code more Chromey.
    
    BUG=647156
    
    Review-Url: https://codereview.chromium.org/2344873002
    Cr-Commit-Position: refs/heads/master@{#419125}
    
    Former-commit-id: 66318666019b16c7315c5e1747101bc7e8e9bab7

--- net/websockets/websocket_extension_parser_test.cc
+++ net/websockets/websocket_extension_parser_test.cc
@@ -131,10 +131,15 @@ TEST(WebSocketExtensionParserTest, InvalidPatterns) {
       "foo; bar=\"\"",        // quoted empty parameter value
       "foo; bar=\"baz",       // unterminated quoted string
       "foo; bar=\"baz \"",    // space in quoted string
-      "foo; bar baz",         // mising '='
+      "foo; bar baz",         // missing '='
       "foo; bar - baz",  // '-' instead of '=' (note: "foo; bar-baz" is valid).
       "foo; bar=\r\nbaz",   // CRNL not followed by a space
       "foo; bar=\r\n baz",  // CRNL followed by a space
+      "f\xFFpp",            // 8-bit character in extension name
+      "foo; b\xFFr=baz"     // 8-bit character in parameter name
+      "foo; bar=b\xFF"      // 8-bit character in parameter value
+      "foo; bar=\"b\xFF\""  // 8-bit character in quoted parameter value
+      "foo; bar=\"baz\\"    // ends with backslash
   };
 
   for (size_t i = 0; i < arraysize(patterns); ++i) {
@@ -153,6 +158,13 @@ TEST(WebSocketExtensionParserTest, QuotedParameterValue) {
 
   ASSERT_EQ(1U, parser.extensions().size());
   EXPECT_TRUE(expected.Equals(parser.extensions()[0]));
+}
+
+// This is a regression test for crbug.com/647156
+TEST(WebSocketExtensionParserTest, InvalidToken) {
+  static const char kInvalidInput[] = "\304;\304!*777\377=\377\254\377";
+  WebSocketExtensionParser parser;
+  EXPECT_FALSE(parser.Parse(kInvalidInput));
 }
 
 }  // namespace
