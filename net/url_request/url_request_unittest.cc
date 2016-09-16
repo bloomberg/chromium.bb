10445fa552f862a0d5886c8bf5426368c8ce1fe3
davidben <davidben@chromium.org>
2016-09-16 12:21:15 -0700

    Revert of CertVerifyProcMac: Add Keychain re-ordering hack, check CRLsets in path pruning loop. (patchset #11 id:300001 of https://codereview.chromium.org/2101303005/ )
    Reason for revert:
    This breaks verification on OS X 10.12 and probably needs some further investigation.
    
    Original issue's description:
    > CertVerifyProcMac: Add Keychain re-ordering hack, check CRLsets in path pruning loop.
    >
    > This also removes the native hostname checking workarounds.
    >
    > BUG=570909,588789,621684
    >
    > Committed: https://crrev.com/10965587ca08568e24f12fa274a7c8c0fbb92a6f [formerly 9cedf75377d817c6b32a01f1d30fbe10663b8bb8]
    > Cr-Commit-Position: refs/heads/master@{#418732}
    
    TBR=rsleevi@chromium.org,mattm@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=570909,588789,621684,647241
    
    Review-Url: https://codereview.chromium.org/2347893002
    Cr-Commit-Position: refs/heads/master@{#419245}
    
    Former-commit-id: 03a7397bd4ed8c059a7f5977a7672769fb783626

--- net/url_request/url_request_unittest.cc
+++ net/url_request/url_request_unittest.cc
@@ -9002,7 +9002,7 @@ class HTTPSOCSPTest : public HTTPSRequestTest {
 };
 
 static CertStatus ExpectedCertStatusForFailedOnlineRevocationCheck() {
-#if defined(OS_WIN) || defined(OS_MACOSX)
+#if defined(OS_WIN)
   // Windows can return CERT_STATUS_UNABLE_TO_CHECK_REVOCATION but we don't
   // have that ability on other platforms.
   return CERT_STATUS_UNABLE_TO_CHECK_REVOCATION;
@@ -9041,19 +9041,6 @@ static bool SystemUsesChromiumEVMetadata() {
 #else
   return true;
 #endif
-}
-
-// Returns the expected CertStatus for tests that expect an online revocation
-// check failure as a result of checking a test EV cert, which will not
-// actually trigger an online revocation check on some platforms.
-static CertStatus ExpectedCertStatusForFailedOnlineEVRevocationCheck() {
-  if (SystemUsesChromiumEVMetadata()) {
-    return ExpectedCertStatusForFailedOnlineRevocationCheck();
-  } else {
-    // If SystemUsesChromiumEVMetadata is false, revocation checking will not
-    // be enabled, and thus there will not be a revocation check to fail.
-    return 0u;
-  }
 }
 
 static bool SystemSupportsOCSP() {
@@ -9107,7 +9094,10 @@ TEST_F(HTTPSOCSPTest, Revoked) {
   CertStatus cert_status;
   DoConnection(ssl_options, &cert_status);
 
+#if !(defined(OS_MACOSX) && !defined(OS_IOS))
+  // Doesn't pass on OS X yet for reasons that need to be investigated.
   EXPECT_EQ(CERT_STATUS_REVOKED, cert_status & CERT_STATUS_ALL_ERRORS);
+#endif
   EXPECT_FALSE(cert_status & CERT_STATUS_IS_EV);
   EXPECT_TRUE(cert_status & CERT_STATUS_REV_CHECKING_ENABLED);
 }
@@ -9614,7 +9604,7 @@ TEST_F(HTTPSEVCRLSetTest, MissingCRLSetAndInvalidOCSP) {
   CertStatus cert_status;
   DoConnection(ssl_options, &cert_status);
 
-  EXPECT_EQ(ExpectedCertStatusForFailedOnlineEVRevocationCheck(),
+  EXPECT_EQ(ExpectedCertStatusForFailedOnlineRevocationCheck(),
             cert_status & CERT_STATUS_ALL_ERRORS);
 
   EXPECT_FALSE(cert_status & CERT_STATUS_IS_EV);
@@ -9688,7 +9678,7 @@ TEST_F(HTTPSEVCRLSetTest, ExpiredCRLSet) {
   CertStatus cert_status;
   DoConnection(ssl_options, &cert_status);
 
-  EXPECT_EQ(ExpectedCertStatusForFailedOnlineEVRevocationCheck(),
+  EXPECT_EQ(ExpectedCertStatusForFailedOnlineRevocationCheck(),
             cert_status & CERT_STATUS_ALL_ERRORS);
 
   EXPECT_FALSE(cert_status & CERT_STATUS_IS_EV);
@@ -9741,7 +9731,7 @@ TEST_F(HTTPSEVCRLSetTest, FreshCRLSetNotCovered) {
   // Even with a fresh CRLSet, we should still do online revocation checks when
   // the certificate chain isn't covered by the CRLSet, which it isn't in this
   // test.
-  EXPECT_EQ(ExpectedCertStatusForFailedOnlineEVRevocationCheck(),
+  EXPECT_EQ(ExpectedCertStatusForFailedOnlineRevocationCheck(),
             cert_status & CERT_STATUS_ALL_ERRORS);
 
   EXPECT_FALSE(cert_status & CERT_STATUS_IS_EV);
