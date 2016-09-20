4841567228db3868567af903264e461fd8eccb5f
jwd <jwd@chromium.org>
2016-09-20 15:57:13 -0700

    Removing dcheck in SetUploadConsentImpl.
    This DCHECK is very easy to trigger by developers, googlers specifically, without actually violating the intent of the DCHECK.
    
    BUG=648594
    
    Review-Url: https://codereview.chromium.org/2353133003
    Cr-Commit-Position: refs/heads/master@{#419882}
    
    Former-commit-id: 6bbba65a7074b6cdbe1dada8d51d0ba90ff944b6

--- components/crash/content/app/crashpad.cc
+++ components/crash/content/app/crashpad.cc
@@ -307,10 +307,11 @@ extern "C" {
 // This function is used in chrome_metrics_services_manager_client.cc to trigger
 // changes to the upload-enabled state. This is done when the metrics services
 // are initialized, and when the user changes their consent for uploads. See
-// crash_reporter::SetUploadConsent for effects.
+// crash_reporter::SetUploadConsent for effects. The given consent value should
+// be consistent with
+// crash_reporter::GetCrashReporterClient()->GetCollectStatsConsent(), but it's
+// not enforced to avoid blocking startup code on synchronizing them.
 void __declspec(dllexport) __cdecl SetUploadConsentImpl(bool consent) {
-  DCHECK_EQ(consent,
-            crash_reporter::GetCrashReporterClient()->GetCollectStatsConsent());
   crash_reporter::SetUploadConsent(consent);
 }
 
