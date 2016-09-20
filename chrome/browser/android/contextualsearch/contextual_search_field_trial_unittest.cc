329da7b0371246d3d74905b8fb50b09e5602ece3
donnd <donnd@chromium.org>
2016-09-20 16:19:38 -0700

    [TTS] Rename ContextualSearchFieldTrial unit tests.
    Updates unit tests for FieldTrial (which were misspelled FieldTrail).
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2351903004
    Cr-Commit-Position: refs/heads/master@{#419893}
    
    Former-commit-id: a130331129068c4210fd8675d2ccbc157042293a

--- chrome/browser/android/contextualsearch/contextual_search_field_trial_unittest.cc
+++ chrome/browser/android/contextualsearch/contextual_search_field_trial_unittest.cc
@@ -9,14 +9,14 @@
 #include "base/macros.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-// Tests ContextualSearchFieldTrail class.
-class ContextualSearchFieldTrailTest : public testing::Test {
+// Tests ContextualSearchFieldTrial class.
+class ContextualSearchFieldTrialTest : public testing::Test {
  public:
-  ContextualSearchFieldTrailTest() {}
-  ~ContextualSearchFieldTrailTest() override {}
+  ContextualSearchFieldTrialTest() {}
+  ~ContextualSearchFieldTrialTest() override {}
 
   // Inner class that stubs out access to Variations and command-line switches.
-  class ContextualSearchFieldTrailStubbed : public ContextualSearchFieldTrial {
+  class ContextualSearchFieldTrialStubbed : public ContextualSearchFieldTrial {
    public:
     // Use these to set a non-empty value to override return of a Get method.
     void SetSwitchValue(const std::string& value);
@@ -35,68 +35,68 @@ class ContextualSearchFieldTrailTest : public testing::Test {
   };
 
   // The class under test.
-  std::unique_ptr<ContextualSearchFieldTrailStubbed> field_trial_;
+  std::unique_ptr<ContextualSearchFieldTrialStubbed> field_trial_;
 
  protected:
   void SetUp() override {
-    field_trial_.reset(new ContextualSearchFieldTrailStubbed());
+    field_trial_.reset(new ContextualSearchFieldTrialStubbed());
   }
 
   void TearDown() override {}
 
  private:
-  DISALLOW_COPY_AND_ASSIGN(ContextualSearchFieldTrailTest);
+  DISALLOW_COPY_AND_ASSIGN(ContextualSearchFieldTrialTest);
 };
 
-bool ContextualSearchFieldTrailTest::ContextualSearchFieldTrailStubbed::
+bool ContextualSearchFieldTrialTest::ContextualSearchFieldTrialStubbed::
     HasSwitch(const std::string& name) {
   return does_have_switch_;
 }
 
-void ContextualSearchFieldTrailTest::ContextualSearchFieldTrailStubbed::
+void ContextualSearchFieldTrialTest::ContextualSearchFieldTrialStubbed::
     SetSwitchValue(const std::string& value) {
   switch_value_ = value;
   does_have_switch_ = true;
 }
 
 std::string
-ContextualSearchFieldTrailTest::ContextualSearchFieldTrailStubbed::GetSwitch(
+ContextualSearchFieldTrialTest::ContextualSearchFieldTrialStubbed::GetSwitch(
     const std::string& name) {
   return switch_value_;
 }
 
-void ContextualSearchFieldTrailTest::ContextualSearchFieldTrailStubbed::
+void ContextualSearchFieldTrialTest::ContextualSearchFieldTrialStubbed::
     SetParamValue(const std::string& value) {
   param_value_ = value;
 }
 
 std::string
-ContextualSearchFieldTrailTest::ContextualSearchFieldTrailStubbed::GetParam(
+ContextualSearchFieldTrialTest::ContextualSearchFieldTrialStubbed::GetParam(
     const std::string& name) {
   return param_value_;
 }
 
-TEST_F(ContextualSearchFieldTrailTest, IntegerDefaultValue) {
+TEST_F(ContextualSearchFieldTrialTest, IntegerDefaultValue) {
   // Should return this default value.
   EXPECT_EQ(
       ContextualSearchFieldTrial::kContextualSearchDefaultIcingSurroundingSize,
       field_trial_->GetIcingSurroundingSize());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, IntegerParamOverrides) {
+TEST_F(ContextualSearchFieldTrialTest, IntegerParamOverrides) {
   // Params override defaults.
   field_trial_->SetParamValue("500");
   EXPECT_EQ(500, field_trial_->GetIcingSurroundingSize());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, IntegerSwitchOverrides) {
+TEST_F(ContextualSearchFieldTrialTest, IntegerSwitchOverrides) {
   field_trial_->SetParamValue("500");
   // Switches override params.
   field_trial_->SetSwitchValue("200");
   EXPECT_EQ(200, field_trial_->GetIcingSurroundingSize());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, IntegerJunkIgnored) {
+TEST_F(ContextualSearchFieldTrialTest, IntegerJunkIgnored) {
   // A junk value effectively resets the switch.
   field_trial_->SetSwitchValue("foo");
   EXPECT_EQ(
@@ -104,47 +104,47 @@ TEST_F(ContextualSearchFieldTrailTest, IntegerJunkIgnored) {
       field_trial_->GetIcingSurroundingSize());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, BooleanDefaultValue) {
+TEST_F(ContextualSearchFieldTrialTest, BooleanDefaultValue) {
   // Should return this default value.
   EXPECT_FALSE(field_trial_->IsSendBasePageURLDisabled());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, BooleanParamOverrides) {
+TEST_F(ContextualSearchFieldTrialTest, BooleanParamOverrides) {
   // Params override defaults.
   field_trial_->SetParamValue("any");
   EXPECT_TRUE(field_trial_->IsSendBasePageURLDisabled());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, BooleanFalseParam) {
+TEST_F(ContextualSearchFieldTrialTest, BooleanFalseParam) {
   field_trial_->SetParamValue("false");
   EXPECT_FALSE(field_trial_->IsSendBasePageURLDisabled());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, BooleanSwitchOverrides) {
+TEST_F(ContextualSearchFieldTrialTest, BooleanSwitchOverrides) {
   field_trial_->SetParamValue("false");
   // Switches override params.
   field_trial_->SetSwitchValue("any");
   EXPECT_TRUE(field_trial_->IsSendBasePageURLDisabled());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, BooleanEmptySwitch) {
+TEST_F(ContextualSearchFieldTrialTest, BooleanEmptySwitch) {
   // An empty switch that's present should return true;
   field_trial_->SetSwitchValue("");
   EXPECT_TRUE(field_trial_->IsSendBasePageURLDisabled());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, StringDefaultEmpty) {
+TEST_F(ContextualSearchFieldTrialTest, StringDefaultEmpty) {
   // Default should return an empty string.
   EXPECT_TRUE(field_trial_->GetResolverURLPrefix().empty());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, StringParamOverrides) {
+TEST_F(ContextualSearchFieldTrialTest, StringParamOverrides) {
   // Params override.
   field_trial_->SetParamValue("param");
   EXPECT_EQ("param", field_trial_->GetResolverURLPrefix());
 }
 
-TEST_F(ContextualSearchFieldTrailTest, StringSwitchOverrides) {
+TEST_F(ContextualSearchFieldTrialTest, StringSwitchOverrides) {
   field_trial_->SetParamValue("param");
   // Switches override params.
   field_trial_->SetSwitchValue("switch");
