86736ef321ed1d7b8469fb1cab576f4657ddd81e
xiaochengh <xiaochengh@chromium.org>
2016-09-21 03:44:07 -0700

    Revert "Document::haveImportsLoaded() should return true when ignoring pending sheets"
    This is a manual revert of commit 42d1ae61cb99b0fd9e3abfa81d1316917ed5fd26 [formerly cd626f11b0ddb8fc655ac2fd808ca3930e3e29cf]
    for causing crbug.com/648547.
    
    It also marks editing/selection/modify_move/move_backward_line_import_crash.html
    as crashing, because this layout test is a repro case of crbug.com/646323.
    
    Original issue's description:
    > Document::haveImportsLoaded() should return true when ignoring pending sheets
    >
    > BUG=646323
    > TESTS=webkit_unit_tests DocumentLoadingRenderingTest.ShouldNotPerformRepeatedLayoutWithPendingImport DocumentLoadingRenderingTest.ShouldClearPlaceholderStyleWhenIgnoringPendingStylesheet
    >
    > Committed: https://crrev.com/42d1ae61cb99b0fd9e3abfa81d1316917ed5fd26 [formerly cd626f11b0ddb8fc655ac2fd808ca3930e3e29cf]
    > Cr-Commit-Position: refs/heads/master@{#419067}
    
    BUG=648547,646323
    TBR=esprehn@chromium.org,rune@chromium.org
    
    Review-Url: https://codereview.chromium.org/2353243002
    Cr-Commit-Position: refs/heads/master@{#420023}
    
    Former-commit-id: 593d84c9016da731e62991eec28d998104b121da

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -765,8 +765,6 @@ HTMLImportLoader* Document::importLoader() const
 
 bool Document::haveImportsLoaded() const
 {
-    if (m_styleEngine->ignoringPendingStylesheets())
-        return true;
     if (!m_importsController)
         return true;
     return !m_importsController->shouldBlockScriptExecution(*this);
