401d6d12784f33d2af8c94a6906fd8d93f6c461a
estade <estade@chromium.org>
2016-09-22 08:45:35 -0700

    Remove pre-MD code from bookmark bar (Views).
    One small behavioral change: the drag/drop indicator uses
    the text color, so it's actually visible on dark themes.
    
    BUG=648281
    
    Review-Url: https://codereview.chromium.org/2357353003
    Cr-Commit-Position: refs/heads/master@{#420354}
    
    Former-commit-id: f5a394b0e277189a4e7d57b68bb1ae3a0f4ee3ee

--- chrome/browser/ui/views/bookmarks/bookmark_bar_instructions_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_instructions_view.cc
@@ -13,7 +13,6 @@
 #include "chrome/grit/generated_resources.h"
 #include "ui/accessibility/ax_view_state.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "ui/base/material_design/material_design_controller.h"
 #include "ui/base/theme_provider.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/controls/label.h"
@@ -23,14 +22,9 @@ namespace {
 
 // Horizontal padding, in pixels, between the link and label.
 int GetViewPadding() {
-  if (ui::MaterialDesignController::IsModeMaterial()) {
-    static int space_width =
-        views::Label(base::ASCIIToUTF16(" ")).GetPreferredSize().width();
-    return space_width;
-  }
-
-  // Pre-MD is hard-coded to 6px.
-  return 6;
+  static int space_width =
+      views::Label(base::ASCIIToUTF16(" ")).GetPreferredSize().width();
+  return space_width;
 }
 
 }  // namespace
@@ -130,19 +124,15 @@ void BookmarkBarInstructionsView::UpdateColors() {
   if (!import_link_)
     return;
 
-  if (!ui::MaterialDesignController::IsModeMaterial()) {
-    import_link_->SetEnabledColor(text_color);
-  } else {
-    // For MD, use the default link color if it provides enough contrast. If
-    // contrast is too low, fall back to the bookmark text color and use an
-    // underline to make it obvious it's a link. The default color readability
-    // code (which only adjusts luminance) doesn't work well in this case.
-    SkColor bg = theme_provider->GetColor(ThemeProperties::COLOR_TOOLBAR);
-    SkColor link_color =
-        GetNativeTheme()->GetSystemColor(ui::NativeTheme::kColorId_LinkEnabled);
-    bool link_has_contrast = color_utils::GetContrastRatio(link_color, bg) >=
-                             color_utils::kMinimumReadableContrastRatio;
-    import_link_->SetUnderline(!link_has_contrast);
-    import_link_->SetEnabledColor(link_has_contrast ? link_color : text_color);
-  }
+  // Use the default link color if it provides enough contrast. If
+  // contrast is too low, fall back to the bookmark text color and use an
+  // underline to make it obvious it's a link. The default color readability
+  // code (which only adjusts luminance) doesn't work well in this case.
+  SkColor bg = theme_provider->GetColor(ThemeProperties::COLOR_TOOLBAR);
+  SkColor link_color =
+      GetNativeTheme()->GetSystemColor(ui::NativeTheme::kColorId_LinkEnabled);
+  bool link_has_contrast = color_utils::GetContrastRatio(link_color, bg) >=
+                           color_utils::kMinimumReadableContrastRatio;
+  import_link_->SetUnderline(!link_has_contrast);
+  import_link_->SetEnabledColor(link_has_contrast ? link_color : text_color);
 }
