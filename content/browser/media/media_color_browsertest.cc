e5eda2ac1296f83f3735449a6a88587fcce62c38
watk <watk@chromium.org>
2016-09-22 11:56:54 -0700

    Reland: Enable media browsertests on Android
    These have been disabled for a long time. Now that the unified media
    pipeline is enabled, they should be much more reliable.
    
    Original review: https://codereview.chromium.org/2346963004
    
    BUG=582293
    
    Review-Url: https://codereview.chromium.org/2365463003
    Cr-Commit-Position: refs/heads/master@{#420411}
    
    Former-commit-id: ff7248a2f2763f75cb4b7659d2faf9a6e78408dc

--- content/browser/media/media_color_browsertest.cc
+++ content/browser/media/media_color_browsertest.cc
@@ -19,6 +19,8 @@ class MediaColorTest : public MediaBrowserTest {
   }
 };
 
+// Android doesn't support Theora.
+#if !defined(OS_ANDROID)
 IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv420pTheora) {
   RunColorTest("yuv420p.ogv");
 }
@@ -30,6 +32,7 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv422pTheora) {
 IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv444pTheora) {
   RunColorTest("yuv444p.ogv");
 }
+#endif  // !defined(OS_ANDROID)
 
 IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv420pVp8) {
   RunColorTest("yuv420p.webm");
@@ -40,11 +43,19 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv444pVp9) {
 }
 
 #if defined(USE_PROPRIETARY_CODECS)
-IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv420pH264) {
+
+// This fails on some Android devices: http://crbug.com/649199,
+// http://crbug.com/649185.
+#if defined(OS_ANDROID)
+#define MAYBE_Yuv420pH264 DISABLED_Yuv420pH264
+#else
+#define MAYBE_Yuv420pH264 Yuv420pH264
+#endif
+IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv420pH264) {
   RunColorTest("yuv420p.mp4");
 }
 
-// This test fails on Android http://crbug.com/647818, and OSX
+// This test fails on Android: http://crbug.com/647818, and OSX:
 // http://crbug.com/647838.
 #if defined(OS_MACOSX) || defined(OS_ANDROID)
 #define MAYBE_Yuvj420pH264 DISABLED_Yuvj420pH264
@@ -55,9 +66,11 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuvj420pH264) {
   RunColorTest("yuvj420p.mp4");
 }
 
-// This fails on Linux & ChromeOS http://crbug.com/647400, and Windows
-// http://crbug.com/647842.
-#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_WIN)
+// This fails on Linux & ChromeOS: http://crbug.com/647400,
+// Windows: http://crbug.com/647842, and Android: http://crbug.com/649199,
+// http://crbug.com/649185.
+#if defined(OS_LINUX) || defined(OS_CHROMEOS) || defined(OS_WIN) || \
+    defined(OS_ANDROID)
 #define MAYBE_Yuv420pRec709H264 DISABLED_Yuv420pRec709H264
 #else
 #define MAYBE_Yuv420pRec709H264 Yuv420pRec709H264
@@ -66,6 +79,8 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, MAYBE_Yuv420pRec709H264) {
   RunColorTest("yuv420p_rec709.mp4");
 }
 
+// Android devices usually only support baseline, main and high.
+#if !defined(OS_ANDROID)
 IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv420pHighBitDepth) {
   RunColorTest("yuv420p_hi10p.mp4");
 }
@@ -77,6 +92,7 @@ IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv422pH264) {
 IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv444pH264) {
   RunColorTest("yuv444p.mp4");
 }
+#endif  // !defined(OS_ANDROID)
 
 #if defined(OS_CHROMEOS)
 IN_PROC_BROWSER_TEST_F(MediaColorTest, Yuv420pMpeg4) {
