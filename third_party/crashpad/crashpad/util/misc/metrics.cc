dc97227d53ae7a9e8013549dc23b7a23e4630458
scottmg <scottmg@chromium.org>
2016-09-22 12:31:23 -0700

    Update Crashpad to 0aeca5f12374fdbf3d4f6c656abf950ba2a96f1c
    b48e9bfbabf3 Add UMA to exception handler exception catching
    72a12e2e9497 Make UMA for exception code OS-specific
    17167a1e579a static_cast UMA 'enum class's to int
    007f790fe21c static const on const char[] for UMA string
    0aeca5f12374 UMA changes based on Chromium-side review
    
    Adds entries to histogram.xml for UMA_s added in Crashpad.
    
    Requires https://codereview.chromium.org/2358023003/.
    
    R=mark@chromium.org, asvitkine@chromium.org
    BUG=crashpad:100
    
    Review-Url: https://codereview.chromium.org/2350943003
    Cr-Commit-Position: refs/heads/master@{#420425}
    
    Former-commit-id: ca7b105dc952bd2d9b2fd5aaf9563f25f15aa752

--- third_party/crashpad/crashpad/util/misc/metrics.cc
+++ third_party/crashpad/crashpad/util/misc/metrics.cc
@@ -15,13 +15,60 @@
 #include "util/misc/metrics.h"
 
 #include "base/metrics/histogram_macros.h"
+#include "base/metrics/sparse_histogram.h"
+#include "build/build_config.h"
 
 namespace crashpad {
 
+namespace {
+
+//! \brief Metrics values used to track the start and completion of a crash
+//!     handling. These are used as metrics values directly, so
+//!     enumeration values so new values should always be added at the end.
+enum class ExceptionProcessingState {
+  //! \brief Logged when exception processing is started.
+  kStarted = 0,
+
+  //! \brief Logged when exception processing completes.
+  kFinished = 1,
+};
+
+void ExceptionProcessing(ExceptionProcessingState state) {
+  UMA_HISTOGRAM_COUNTS("Crashpad.ExceptionEncountered",
+                       static_cast<int>(state));
+}
+
+}  // namespace
+
+// static
 void Metrics::CrashReportSize(FileHandle file) {
   const FileOffset size = LoggingFileSizeByHandle(file);
   UMA_HISTOGRAM_CUSTOM_COUNTS(
-      "Crashpad.CrashReportSize", size, 0, 5 * 1024 * 1024, 50);
+      "Crashpad.CrashReportSize", size, 0, 20 * 1024 * 1024, 50);
+}
+
+// static
+void Metrics::ExceptionCaptureResult(CaptureResult result) {
+  ExceptionProcessing(ExceptionProcessingState::kFinished);
+  UMA_HISTOGRAM_ENUMERATION("Crashpad.ExceptionCaptureResult",
+                            static_cast<int32_t>(result),
+                            static_cast<int32_t>(CaptureResult::kMaxValue));
+}
+
+// static
+void Metrics::ExceptionCode(uint32_t exception_code) {
+#if defined(OS_WIN)
+  static const char kExceptionCodeString[] = "Crashpad.ExceptionCode.Win";
+#elif defined(OS_MACOSX)
+  static const char kExceptionCodeString[] = "Crashpad.ExceptionCode.Mac";
+#endif
+  UMA_HISTOGRAM_SPARSE_SLOWLY(kExceptionCodeString,
+                              static_cast<int32_t>(exception_code));
+}
+
+// static
+void Metrics::ExceptionEncountered() {
+  ExceptionProcessing(ExceptionProcessingState::kStarted);
 }
 
 }  // namespace crashpad
