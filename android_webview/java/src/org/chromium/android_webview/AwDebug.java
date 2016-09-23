d300c252ac802abc86b7eb63cd3cf144b8966b5f
tobiasjs <tobiasjs@chromium.org>
2016-09-23 06:32:54 -0700

    Remove the proguard keep rule for AwDebug now @UsedByReflection works.
    BUG=541543
    
    Review-Url: https://codereview.chromium.org/2364733004
    Cr-Commit-Position: refs/heads/master@{#420612}
    
    Former-commit-id: e8bb027218d9aaa48c29ef67bc989ca1ce69148e

--- android_webview/java/src/org/chromium/android_webview/AwDebug.java
+++ android_webview/java/src/org/chromium/android_webview/AwDebug.java
@@ -5,6 +5,7 @@
 package org.chromium.android_webview;
 
 import org.chromium.base.annotations.JNINamespace;
+import org.chromium.base.annotations.UsedByReflection;
 
 import java.io.File;
 import java.io.IOException;
@@ -16,6 +17,7 @@ import java.io.IOException;
  * the client of WebView.
  */
 @JNINamespace("android_webview")
+@UsedByReflection("")
 public class AwDebug {
     /**
      * Dump webview state (predominantly a minidump for all threads,
@@ -29,6 +31,7 @@ public class AwDebug {
      * be able to be re-opened for reading and writing via its
      * canonical path. The file will be truncated upon re-opening.
      */
+    @UsedByReflection("")
     public static boolean dumpWithoutCrashing(File dumpFile) {
         String dumpPath;
         try {
