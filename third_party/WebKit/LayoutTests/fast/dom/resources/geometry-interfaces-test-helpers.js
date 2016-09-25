fcb7d5221e7b5b9e5e6c35589fafb19f9ecf0b18
hs1217.lee <hs1217.lee@samsung.com>
2016-09-25 05:15:33 -0700

    assert_*d_matrix_equals fuction can accept array as expected parameter.
    when use it, it makes more simple test code.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2364393002
    Cr-Commit-Position: refs/heads/master@{#420857}
    
    Former-commit-id: c7a688377217a7a7fa65a4ace691a471e16c0231

--- third_party/WebKit/LayoutTests/fast/dom/resources/geometry-interfaces-test-helpers.js
+++ third_party/WebKit/LayoutTests/fast/dom/resources/geometry-interfaces-test-helpers.js
@@ -19,19 +19,44 @@ function assert_identity_3d_matrix(actual, description) {
 }
 
 function assert_2d_matrix_equals(actual, expected, description) {
-  var full_expected = {
-      m11: expected.m11, m12: expected.m12, m13: 0, m14: 0,
-      m21: expected.m21, m22: expected.m22, m23: 0, m24: 0,
-      m31: 0, m32: 0, m33: 1, m34: 0,
-      m41: expected.m41, m42: expected.m42, m43: 0, m44: 1,
-      is2D: true, isIdentity: expected.isIdentity
-  };
-  assert_matrix_equals(actual, full_expected, description);
+  if (Array.isArray(expected)) {
+    assert_equals(6, expected.length);
+    var full_expected = {
+        m11: expected[0], m12: expected[1], m13: 0, m14: 0,
+        m21: expected[2], m22: expected[3], m23: 0, m24: 0,
+        m31: 0, m32: 0, m33: 1, m34: 0,
+        m41: expected[4], m42: expected[5], m43: 0, m44: 1,
+        is2D: true, isIdentity: false
+    };
+    assert_matrix_equals(actual, full_expected, description);
+  } else {
+    var full_expected = {
+        m11: expected.m11, m12: expected.m12, m13: 0, m14: 0,
+        m21: expected.m21, m22: expected.m22, m23: 0, m24: 0,
+        m31: 0, m32: 0, m33: 1, m34: 0,
+        m41: expected.m41, m42: expected.m42, m43: 0, m44: 1,
+        is2D: true, isIdentity: false
+    };
+    assert_matrix_equals(actual, full_expected, description);
+  }
 }
 
 function assert_3d_matrix_equals(actual, expected, description){
-  expected['is2D'] = false;
-  assert_matrix_equals(actual, expected, description);
+  if (Array.isArray(expected)) {
+    assert_equals(16, expected.length);
+    var full_expected = {
+        m11: expected[0], m12: expected[1], m13: expected[2], m14: expected[3],
+        m21: expected[4], m22: expected[5], m23: expected[6], m24: expected[7],
+        m31: expected[8], m32: expected[9], m33: expected[10], m34: expected[11],
+        m41: expected[12], m42: expected[13], m43: expected[14], m44: expected[15],
+        is2D: false, isIdentity: false
+    };
+    assert_matrix_equals(actual, full_expected, description);
+  } else {
+    expected['is2D'] = false;
+    expected['isIdentity'] = false;
+    assert_matrix_equals(actual, expected, description);
+  }
 }
 
 function assert_matrix_equals(actual, expected, description) {
