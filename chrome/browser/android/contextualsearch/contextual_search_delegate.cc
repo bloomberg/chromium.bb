9d5a8c688bbceb97203ad1e4a597d4b68e41fe17
donnd <donnd@chromium.org>
2016-09-27 11:38:29 -0700

    [TTS] Cleanup a potential issue introduced in CL 2157793002
    Refactoring done in that CL looks like it could introduce potential problems
    because now there's a path through the affected method that does not
    reset the context.
    
    This CL refactors the affected method to always reset the context while
    preserving the refactoring done in 2157793002.
    
    BUG=626770
    
    Review-Url: https://codereview.chromium.org/2300753003
    Cr-Commit-Position: refs/heads/master@{#421279}
    
    Former-commit-id: e4fcb595ed939ec2f912d6fdd211ce70c73ee0ce

--- chrome/browser/android/contextualsearch/contextual_search_delegate.cc
+++ chrome/browser/android/contextualsearch/contextual_search_delegate.cc
@@ -248,21 +248,19 @@ void ContextualSearchDelegate::GatherSurroundingTextWithCallback(
     content::WebContents* web_contents,
     bool may_send_base_page_url,
     HandleSurroundingsCallback callback) {
-  DCHECK(web_contents);
-  DCHECK(!callback.is_null());
-  DCHECK(!selection.empty());
-  RenderFrameHost* focused_frame = web_contents->GetFocusedFrame();
-  if (!focused_frame) {
-    callback.Run(base::string16(), 0, 0);
-    return;
-  }
   // Immediately cancel any request that's in flight, since we're building a new
   // context (and the response disposes of any existing context).
   search_term_fetcher_.reset();
   BuildContext(selection, use_resolved_search_term, web_contents,
                may_send_base_page_url);
-  focused_frame->RequestTextSurroundingSelection(
-      callback, field_trial_->GetSurroundingSize());
+  DCHECK(web_contents);
+  RenderFrameHost* focused_frame = web_contents->GetFocusedFrame();
+  if (focused_frame) {
+    focused_frame->RequestTextSurroundingSelection(
+        callback, field_trial_->GetSurroundingSize());
+  } else {
+    callback.Run(base::string16(), 0, 0);
+  }
 }
 
 void ContextualSearchDelegate::BuildContext(
