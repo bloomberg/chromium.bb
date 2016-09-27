34696aa2c58c0340e336224e66d49e0540493305
gambard <gambard@chromium.org>
2016-09-27 04:37:38 -0700

    Add sequential dispatching for InitializationRunner
    This CL adds a new way do dispatch tasks at the beginning of the application.
    Before the tasks were run after an arbitrary delay. Now the tasks can be run
    sequentially with a small delay between each.
    It also deprecate the previous method.
    
    BUG=227553
    
    Review-Url: https://codereview.chromium.org/2217083002
    Cr-Commit-Position: refs/heads/master@{#421171}
    
    Former-commit-id: 61f96f5bf492c26145a1682ac42420c261eb1f23

--- ios/chrome/app/deferred_initialization_runner.h
+++ ios/chrome/app/deferred_initialization_runner.h
@@ -18,6 +18,7 @@
 // Returns singleton instance.
 + (DeferredInitializationRunner*)sharedInstance;
 
+// Deprecated, use |enqueueBlockNamed:block:| instead.
 // Schedules |block| to be run after |delaySeconds| on the current queue.
 // This |block| is stored as |name| so code can force this initialization to
 // be run synchronously if necessary. This method may be called more than
@@ -27,6 +28,12 @@
 - (void)runBlockNamed:(NSString*)name
                 after:(NSTimeInterval)delaySeconds
                 block:(ProceduralBlock)block;
+
+// Stores |block| under |name| to a queue of blocks to run. All blocks are run
+// sequentially with a small delay before the first block and between each
+// successive block. If a block is already registered under |name|, it is
+// replaced with |block| unless it has already been run.
+- (void)enqueueBlockNamed:(NSString*)name block:(ProceduralBlock)block;
 
 // Looks up a previously scheduled block of |name|. If block has not been
 // run yet, run it synchronously now.
@@ -39,6 +46,17 @@
 // Number of blocks that have been registered but not executed yet.
 // Exposed for testing.
 @property(nonatomic, readonly) NSUInteger numberOfBlocksRemaining;
+
+@end
+
+@interface DeferredInitializationRunner (ExposedForTesting)
+
+// Time interval between two blocks. Default value is 200ms.
+@property(nonatomic, assign) NSTimeInterval delayBetweenBlocks;
+
+// Time interval before running the first block. To override default value of
+// 3s, set this property before the first call to |-enqueueBlockNamed:block:|.
+@property(nonatomic, assign) NSTimeInterval delayBeforeFirstBlock;
 
 @end
 
