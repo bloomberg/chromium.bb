4202b7049c310b104f5dcd149c98f79dd7557c5d
xidachen <xidachen@chromium.org>
2016-09-26 19:08:03 -0700

    Remove some failure entry in GPU pixel test expectations
    Currently in pixel_expectations.py file, some of the newly added pixel
    tests are marked as failure. I have checked all GPU bots, and they have
    picked up the tests and generated the reference images. I also checked
    the reference images and they are as expected. So this CL removes those
    entries.
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2365383002
    Cr-Commit-Position: refs/heads/master@{#421081}
    
    Former-commit-id: 3663253ceb0e141151eee1046acb21d8a9375d40

--- content/test/gpu/gpu_tests/pixel_expectations.py
+++ content/test/gpu/gpu_tests/pixel_expectations.py
@@ -31,15 +31,5 @@ class PixelExpectations(GpuTestExpectations):
     self.Fail('Pixel.CSSFilterEffects', ['mac'], bug=581526)
     self.Fail('Pixel.CSSFilterEffects.NoOverlays', ['mac'], bug=581526)
 
-    # TODO(xidachen) check / generate reference images
-    self.Fail('Pixel.OffscreenCanvasWebGLRedBoxWorker', bug=563852)
-    self.Fail('Pixel.OffscreenCanvasWebGLRedBoxWorkerES3', ['mac'], bug=563852)
-
-    self.Fail('Pixel.OffscreenCanvasAccelerated2D', bug=563852)
-    self.Fail('Pixel.OffscreenCanvasAccelerated2DES3', ['mac'], bug=563852)
-    self.Fail('Pixel.OffscreenCanvasAccelerated2DWorker', bug=563852)
-    self.Fail('Pixel.OffscreenCanvasAccelerated2DWorkerES3', ['mac'],
-        bug=563852)
-
     # TODO(kbr): flakily timing out on this configuration.
     self.Flaky('*', ['linux', 'intel', 'debug'], bug=648369)
