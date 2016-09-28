5f9ef4520a7a38d2f8f2c33e1b01dccd26289478
timav <timav@chromium.org>
2016-09-28 13:03:33 -0700

    Fix initialization in MediaCodecAudioDecoder
    Move the call to ResetTimestampState() that sets several
    init parameters before we return from Initialize waiting
    for the media crypto.
    
    Should be part of https://codereview.chromium.org/2376513002/
    
    BUG=650735
    
    Review-Url: https://codereview.chromium.org/2378643003
    Cr-Commit-Position: refs/heads/master@{#421619}
    
    Former-commit-id: ae5d3869802f69c43a1c03790c9292453ea5e9fd

--- media/filters/android/media_codec_audio_decoder.cc
+++ media/filters/android/media_codec_audio_decoder.cc
@@ -80,6 +80,8 @@ void MediaCodecAudioDecoder::Initialize(const AudioDecoderConfig& config,
   config_ = config;
   output_cb_ = BindToCurrentLoop(output_cb);
 
+  ResetTimestampState();
+
   if (config_.is_encrypted()) {
     // Postpone initialization after MediaCrypto is available.
     // SetCdm uses init_cb in a method that's already bound to the current loop.
@@ -93,7 +95,6 @@ void MediaCodecAudioDecoder::Initialize(const AudioDecoderConfig& config,
     return;
   }
 
-  ResetTimestampState();
   SetState(STATE_READY);
   bound_init_cb.Run(true);
 }
