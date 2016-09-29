1fc4363afe9b686ce68b3f8b686f06d08ef1ea1e
skyostil <skyostil@chromium.org>
2016-09-29 12:06:53 -0700

    perf: Speculative fix for tough_webgl_ad_cases
    Review-Url: https://codereview.chromium.org/2379073002
    Cr-Commit-Position: refs/heads/master@{#421894}
    
    Former-commit-id: df8688b9fbb6140e0c9093aaa5240e6252fe9e00

--- tools/perf/page_sets/tough_ad_cases.py
+++ tools/perf/page_sets/tough_ad_cases.py
@@ -25,6 +25,8 @@ class SwiffyPage(page_module.Page):
         'var meta = document.createElement("meta");'
         'meta.name = "viewport";'
         'meta.content = "width=device-width";'
+        'if (!document.getElementsByTagName("head"))'
+        '  document.body.appendChild(document.createElement("head"));'
         'document.getElementsByTagName("head")[0].appendChild(meta);')
     action_runner.EvaluateJavaScript(viewport_js)
 
