13a24b6bc90fcfd39c47c8c40d66d46d9c98e750
Yaowu Xu <yaowu@google.com>
2016-09-29 08:25:20 -0700

    Remove unused buffer_size
    Manually cherry-picked from libvpx/nextgenv2:
    fa99c376e509ef52ed8370019538b23e76477ddb
    
    Change-Id: Ia614aaa784c405a3d0f815846a584dd358008b18

--- test/webm_video_source.h
+++ test/webm_video_source.h
@@ -56,7 +56,7 @@ class WebMVideoSource : public CompressedVideoSource {
 
   void FillFrame() {
     ASSERT_TRUE(aom_ctx_->file != NULL);
-    const int status = webm_read_frame(webm_ctx_, &buf_, &buf_sz_, &buf_sz_);
+    const int status = webm_read_frame(webm_ctx_, &buf_, &buf_sz_);
     ASSERT_GE(status, 0) << "webm_read_frame failed";
     if (status == 1) {
       end_of_file_ = true;
@@ -66,7 +66,7 @@ class WebMVideoSource : public CompressedVideoSource {
   void SeekToNextKeyFrame() {
     ASSERT_TRUE(aom_ctx_->file != NULL);
     do {
-      const int status = webm_read_frame(webm_ctx_, &buf_, &buf_sz_, &buf_sz_);
+      const int status = webm_read_frame(webm_ctx_, &buf_, &buf_sz_);
       ASSERT_GE(status, 0) << "webm_read_frame failed";
       ++frame_;
       if (status == 1) {
