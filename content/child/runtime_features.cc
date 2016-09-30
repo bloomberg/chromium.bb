288ed747b4036511e8b4c108b6c98033df763fe2
skobes <skobes@chromium.org>
2016-09-30 15:29:32 -0700

    Let features::kScrollAnchoring disable the Blink REF.
    BUG=651874
    
    Review-Url: https://codereview.chromium.org/2379053004
    Cr-Commit-Position: refs/heads/master@{#422244}
    
    Former-commit-id: 560680291035d12baa675c8fd9681b1c04a4b4ac

--- content/child/runtime_features.cc
+++ content/child/runtime_features.cc
@@ -230,8 +230,8 @@ void SetRuntimeFeaturesDefaultsAndUpdateFromArgs(
   if (command_line.HasSwitch(switches::kEnableWebFontsInterventionTrigger))
     WebRuntimeFeatures::enableWebFontsInterventionTrigger(true);
 
-  if (base::FeatureList::IsEnabled(features::kScrollAnchoring))
-    WebRuntimeFeatures::enableScrollAnchoring(true);
+  WebRuntimeFeatures::enableScrollAnchoring(
+      base::FeatureList::IsEnabled(features::kScrollAnchoring));
 
   if (command_line.HasSwitch(switches::kEnableSlimmingPaintV2))
     WebRuntimeFeatures::enableSlimmingPaintV2(true);
