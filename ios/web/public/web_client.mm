be2e633b74ad1a17db2d012fa9067b341464b8b9
stkhapugin <stkhapugin@chromium.org>
2016-10-03 07:55:37 -0700

    [ARC] Converts part of ios/web/public to ARC.
    Notable changes:none
    
    BUG=651816
    TEST=none
    
    Review-Url: https://codereview.chromium.org/2379363002
    Cr-Commit-Position: refs/heads/master@{#422421}
    
    Former-commit-id: a53c4c9498ad3636411fa1debcfc66a5d71cfec9

--- ios/web/public/web_client.mm
+++ ios/web/public/web_client.mm
@@ -6,6 +6,10 @@
 
 #include <Foundation/Foundation.h>
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace web {
 
 static WebClient* g_client;
