f994eae533debf254c23ad3155ea7394411de5a2
falken <falken@chromium.org>
2016-10-02 19:11:10 -0700

    Revert of Check that all shared workers are terminated. (patchset #2 id:170001 of https://c… (patchset #1 id:1 of https://codereview.chromium.org/2364113003/ )
    Reason for revert:
    These temporary CHECKs can be removed because they are passing on Canary.
    
    Original issue's description:
    > Reland of Check that all shared workers are terminated. (patchset #2 id:170001 of https://c… (patchset #1 id:1 of https://codereview.chromium.org/2338113002/ )
    >
    > Reason for revert:
    > Relanding after the attempted fix in https://crrev.com/5d42508bc0966b3eceb56abe6a72c8735685b490 [formerly 04a6912a1a2b77faf61e4b2514b9ecff74ee1f21]
    >
    
    TBR=horo@chromium.org,jam@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=636377
    
    Review-Url: https://codereview.chromium.org/2385053002
    Cr-Commit-Position: refs/heads/master@{#422359}
    
    Former-commit-id: eb3de882fc78dd4604a2dfb6b2a106da127c4ae1

--- content/browser/renderer_host/render_process_host_impl.h
+++ content/browser/renderer_host/render_process_host_impl.h
@@ -250,12 +250,6 @@ class CONTENT_EXPORT RenderProcessHostImpl
   static void RegisterRendererMainThreadFactory(
       RendererMainThreadFactoryFunction create);
 
-  // TODO(alokp): Remove after collecting crash data.
-  // Temporary checks to verify that all shared workers are terminated.
-  // It is suspected that shared workers prevent render process hosts
-  // from shutting down: crbug.com/608049
-  static void CheckAllWorkersTerminated();
-
   RenderFrameMessageFilter* render_frame_message_filter_for_testing() const {
     return render_frame_message_filter_.get();
   }
