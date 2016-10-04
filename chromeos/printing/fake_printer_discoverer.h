7ffe9f629f1b65c919a0af2b181dad1529dc9fa6
skau <skau@chromium.org>
2016-10-03 17:51:31 -0700

    Add a PrinterDiscoverer fake object.
    We will eventually support detecting printers using mDNS and autodetect
    USB devices.  This will take a little while to implement.  Providing a
    fake in the meantime since the system is behind a flag.  The fake may be
    useful for some tests in the future.
    
    BUG=631352
    
    Review-Url: https://codereview.chromium.org/2344393002
    Cr-Commit-Position: refs/heads/master@{#422641}
    
    Former-commit-id: 28fa7dc12fc5de5c75200ce40b666c078e836087

---
+++ chromeos/printing/fake_printer_discoverer.h
@@ -0,0 +1,41 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_PRINTING_FAKE_PRINTER_DISCOVERER_H_
+#define CHROMEOS_PRINTING_FAKE_PRINTER_DISCOVERER_H_
+
+#include <vector>
+
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "chromeos/printing/printer_configuration.h"
+#include "chromeos/printing/printer_discoverer.h"
+
+namespace chromeos {
+
+class FakePrinterDiscoverer : public PrinterDiscoverer {
+ public:
+  FakePrinterDiscoverer();
+  ~FakePrinterDiscoverer();
+
+  bool StartDiscovery() override;
+  bool StopDiscovery() override;
+  void AddObserver(PrinterDiscoverer::Observer* observer) override;
+  void RemoveObserver(PrinterDiscoverer::Observer* observer) override;
+
+ private:
+  void EmitPrinters(size_t start, size_t end);
+
+  std::vector<chromeos::Printer> printers_;
+  bool discovery_running_;
+  std::vector<PrinterDiscoverer::Observer*> observers_;
+
+  base::WeakPtrFactory<FakePrinterDiscoverer> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(FakePrinterDiscoverer);
+};
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_PRINTING_FAKE_PRINTER_DISCOVERER_H_
---
+++ chromeos/printing/printer_discoverer.h
@@ -0,0 +1,63 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_PRINTING_PRINTER_DISCOVERER_H_
+#define CHROMEOS_PRINTING_PRINTER_DISCOVERER_H_
+
+#include <memory>
+#include <vector>
+
+#include "chromeos/printing/printer_configuration.h"
+
+namespace chromeos {
+
+// Interface for printer discovery.  Constructs Printer objects from USB and
+// zeroconf (DNS-SD) printers.
+class PrinterDiscoverer {
+ public:
+  // Interface for objects interested in detected printers.
+  class Observer {
+   public:
+    // Called after discovery has started.
+    virtual void OnDiscoveryStarted() {}
+
+    // Called when discovery is stopping.  OnPrintersFound will not be
+    // called after this occurs.
+    virtual void OnDiscoveryStopping() {}
+
+    // Called after all printers have been discovered and the observers
+    // notified.
+    virtual void OnDiscoveryDone() {}
+
+    // Called with a collection of printers as they are discovered.  Printer
+    // objects must be copied if they are retained outside of the scope of this
+    // function.
+    virtual void OnPrintersFound(const std::vector<Printer>& printers) {}
+  };
+
+  // Static factory
+  static std::unique_ptr<PrinterDiscoverer> Create();
+
+  // Begin scanning for printers.  Found printers will be reported to the
+  // attached observer.
+  virtual bool StartDiscovery() = 0;
+
+  // Stop scanning for printers.  Returns false if scanning could not be stopped
+  // and new results will continue to be be sent to observers.  Returns true if
+  // scanning was stopped successfully.  No calls to the attached observer will
+  // be made after this returns if it returned true.
+  virtual bool StopDiscovery() = 0;
+
+  // Add an observer that will be notified of discovered printers.  Ownership of
+  // |observer| is not taken by the discoverer.  It is an error to add an
+  // oberserver more than once.
+  virtual void AddObserver(PrinterDiscoverer::Observer* observer) = 0;
+
+  // Remove an observer of printer discovery.
+  virtual void RemoveObserver(PrinterDiscoverer::Observer* observer) = 0;
+};
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_PRINTING_PRINTER_DISCOVERER_H_
