3d6e8f5a905601c683f0aac2d1757647bc93bf6c
keishi <keishi@chromium.org>
2016-10-05 02:57:56 -0700

    Revert of Enable per thread heap on SharedWorker (patchset #1 id:1 of https://codereview.chromium.org/2374693002/ )
    Reason for revert:
    Caused crashes
    crbug.com/652058
    crbug.com/651702
    crbug.com/652001
    crbug.com/652264
    crbug.com/652183
    crbug.com/652556
    
    Original issue's description:
    > Enable per thread heap on SharedWorker
    >
    > blink-dev PSA email: https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/wROZOp-YKSc
    >
    > BUG=591606
    >
    > Committed: https://crrev.com/16077edfb43642bbd80e387ae946b9970ff34c64 [formerly 62d4e2e8f5ca66516854cbdbd1402d344410ec75]
    > Cr-Commit-Position: refs/heads/master@{#421731}
    
    TBR=oilpan-reviews@chromium.org,haraken@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=591606
    
    Review-Url: https://codereview.chromium.org/2386263004
    Cr-Commit-Position: refs/heads/master@{#423122}
    
    Former-commit-id: e01a95ada6400bcba59f521d336cfe2909c7b929

--- third_party/WebKit/Source/core/workers/SharedWorkerThread.cpp
+++ third_party/WebKit/Source/core/workers/SharedWorkerThread.cpp
@@ -53,7 +53,7 @@ SharedWorkerThread::SharedWorkerThread(
     : WorkerThread(std::move(workerLoaderProxy), workerReportingProxy),
       m_workerBackingThread(
           WorkerBackingThread::create("SharedWorker Thread",
-                                      BlinkGC::PerThreadHeapMode)),
+                                      BlinkGC::MainThreadHeapMode)),
       m_name(name.isolatedCopy()) {}
 
 SharedWorkerThread::~SharedWorkerThread() {}
