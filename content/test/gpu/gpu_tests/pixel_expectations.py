f79a0d063536e87cea11411f5a30948102184c69
xlai <xlai@chromium.org>
2016-10-05 09:34:04 -0700

    Remove failed entries and skip Android tests for gpu pixel expectations
    The two unaccelerated 2d gpu integration tests are working
    fine in Win,Linux and Mac. So I'm removing their failed
    entries.
    
    Because software compositing is not supported on Android.
    These two tests should not be running on Android platforms.
    So I mark them as Skipped.
    
    TBR=kbr@chromium.org
    BUG=563852
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2388803002
    Cr-Commit-Position: refs/heads/master@{#423184}
    
    Former-commit-id: 5280614b9452793b73489cb103cd740ea24f638f

--- content/test/gpu/gpu_tests/pixel_expectations.py
+++ content/test/gpu/gpu_tests/pixel_expectations.py
@@ -12,6 +12,11 @@ class PixelExpectations(GpuTestExpectations):
     # self.Fail('Pixel_Canvas2DRedBox',
     #     ['mac', 'amd', ('nvidia', 0x1234)], bug=123)
 
+    # Software compositing is not supported on Android; so we skip these tests
+    # that disables gpu compositing on Android platforms.
+    self.Skip('Pixel_OffscreenCanvasUnaccelerated2D', ['android'])
+    self.Skip('Pixel_OffscreenCanvasUnaccelerated2DWorker', ['android'])
+
     self.Fail('Pixel_ScissorTestWithPreserveDrawingBuffer',
         ['android'], bug=521588)
 
@@ -31,9 +36,6 @@ class PixelExpectations(GpuTestExpectations):
     self.Fail('Pixel_CSSFilterEffects', ['mac'], bug=581526)
     self.Fail('Pixel_CSSFilterEffects_NoOverlays', ['mac'], bug=581526)
 
-    # TODO(xlai) check / generate reference images.
-    self.Fail('Pixel_OffscreenCanvasUnaccelerated2D', bug=563852)
-    self.Fail('Pixel_OffscreenCanvasUnaccelerated2DWorker', bug=563858)
     self.Fail('Pixel_OffscreenCanvasUnaccelerated2DGPUCompositingWorker',
               ['mac', ('nvidia', 0xfe9)], bug=652931)
 
