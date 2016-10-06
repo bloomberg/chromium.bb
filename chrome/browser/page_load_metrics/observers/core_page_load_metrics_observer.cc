b8c6db4b27268212a7492fca5210d523423a0fad
ksakamoto <ksakamoto@chromium.org>
2016-10-05 18:45:01 -0700

    Add RAPPOR metric for sites not reporting first meaningful paint
    First Meaningful Paint UMA is not recorded on >20% of page loads. This
    RAPPOR metric will help identify top sites that fail to report First
    Meaningful Paint.
    
    BUG=632081
    
    Review-Url: https://codereview.chromium.org/2398573002
    Cr-Commit-Position: refs/heads/master@{#423387}
    
    Former-commit-id: 76351a9a523c5d103a21901b0e13046e80447ad3

--- chrome/browser/page_load_metrics/observers/core_page_load_metrics_observer.cc
+++ chrome/browser/page_load_metrics/observers/core_page_load_metrics_observer.cc
@@ -194,6 +194,9 @@ const char kHistogramTotalRequestsParseStop[] =
 const char kRapporMetricsNameCoarseTiming[] =
     "PageLoad.CoarseTiming.NavigationToFirstContentfulPaint";
 
+const char kRapporMetricsNameFirstMeaningfulPaintNotRecorded[] =
+    "PageLoad.Experimental.PaintTiming.FirstMeaningfulPaintNotRecorded";
+
 const char kHistogramFirstContentfulPaintUserInitiated[] =
     "PageLoad.PaintTiming.NavigationToFirstContentfulPaint.UserInitiated";
 
@@ -624,23 +627,32 @@ void CorePageLoadMetricsObserver::RecordRappor(
   if (!info.time_to_commit)
     return;
   DCHECK(!info.committed_url.is_empty());
+
   // Log the eTLD+1 of sites that show poor loading performance.
-  if (!WasStartedInForegroundOptionalEventInForeground(
+  if (WasStartedInForegroundOptionalEventInForeground(
           timing.first_contentful_paint, info)) {
-    return;
+    std::unique_ptr<rappor::Sample> sample =
+        rappor_service->CreateSample(rappor::UMA_RAPPOR_TYPE);
+    sample->SetStringField(
+        "Domain",
+        rappor::GetDomainAndRegistrySampleFromGURL(info.committed_url));
+    uint64_t bucket_index =
+        RapporHistogramBucketIndex(timing.first_contentful_paint.value());
+    sample->SetFlagsField("Bucket", uint64_t(1) << bucket_index,
+                          kNumRapporHistogramBuckets);
+    // The IsSlow flag is just a one bit boolean if the first contentful paint
+    // was > 10s.
+    sample->SetFlagsField(
+        "IsSlow", timing.first_contentful_paint.value().InSecondsF() >= 10, 1);
+    rappor_service->RecordSampleObj(internal::kRapporMetricsNameCoarseTiming,
+                                    std::move(sample));
   }
-  std::unique_ptr<rappor::Sample> sample =
-      rappor_service->CreateSample(rappor::UMA_RAPPOR_TYPE);
-  sample->SetStringField(
-      "Domain", rappor::GetDomainAndRegistrySampleFromGURL(info.committed_url));
-  uint64_t bucket_index =
-      RapporHistogramBucketIndex(timing.first_contentful_paint.value());
-  sample->SetFlagsField("Bucket", uint64_t(1) << bucket_index,
-                        kNumRapporHistogramBuckets);
-  // The IsSlow flag is just a one bit boolean if the first contentful paint
-  // was > 10s.
-  sample->SetFlagsField(
-      "IsSlow", timing.first_contentful_paint.value().InSecondsF() >= 10, 1);
-  rappor_service->RecordSampleObj(internal::kRapporMetricsNameCoarseTiming,
-                                  std::move(sample));
+
+  // Log the eTLD+1 of sites that did not report first meaningful paint.
+  if (timing.first_paint && !timing.first_meaningful_paint) {
+    rappor::SampleDomainAndRegistryFromGURL(
+        rappor_service,
+        internal::kRapporMetricsNameFirstMeaningfulPaintNotRecorded,
+        info.committed_url);
+  }
 }
