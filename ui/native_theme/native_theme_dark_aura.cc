bloomberg/chromium.bb

commit 0343b0c161e34bc3478c49b701fa72908fe00003

Clean up NativeTheme (particularly CommonTheme).
- discard pre-MD colors
- consolidate color logic that didn't change from pre-MD to MD
- remove some colors that either aren't used, or are only used in one
  place, or otherwise don't make sense to be part of NativeTheme

BUG=648281

Review-Url: https://codereview.chromium.org/2382443007
Cr-Commit-Position: refs/heads/master@{#422665}

Former-commit-id: f02568a216b975130849079183cc0f7173d6b832

ui/native_theme/native_theme_dark_aura.cc
@@ -4,7 +4,6 @@
 
 #include "ui/native_theme/native_theme_dark_aura.h"
 
-#include "ui/base/material_design/material_design_controller.h"
 #include "ui/gfx/color_palette.h"
 
 namespace ui {
@@ -15,11 +14,7 @@ NativeThemeDarkAura* NativeThemeDarkAura::instance() {
 }
 
 SkColor NativeThemeDarkAura::GetSystemColor(ColorId color_id) const {
-  if (!ui::MaterialDesignController::IsModeMaterial())
-    return NativeThemeAura::GetSystemColor(color_id);
-
   static const SkColor kPrimaryTextColor = SK_ColorWHITE;
-  static const SkColor kDefaultDarkBackground = SkColorSetRGB(0x3C, 0x3C, 0x3E);
 
   static const SkColor kButtonEnabledColor = SK_ColorWHITE;
 
@@ -45,8 +40,6 @@ SkColor NativeThemeDarkAura::GetSystemColor(ColorId color_id) const {
     // Label
     case kColorId_LabelEnabledColor:
       return kPrimaryTextColor;
-    case kColorId_LabelBackgroundColor:
-      return kDefaultDarkBackground;
 
     // Link
     case kColorId_LinkEnabled:
@@ -102,11 +95,8 @@ SkColor NativeThemeDarkAura::GetSystemColor(ColorId color_id) const {
     case kColorId_DialogBackground:
     case kColorId_BubbleBackground:
     case kColorId_UnfocusedBorderColor:
-    case kColorId_ButtonBackgroundColor:
     case kColorId_ButtonDisabledColor:
-    case kColorId_ButtonHighlightColor:
     case kColorId_ButtonHoverColor:
-    case kColorId_ButtonHoverBackgroundColor:
     case kColorId_BlueButtonEnabledColor:
     case kColorId_BlueButtonDisabledColor:
     case kColorId_BlueButtonPressedColor:
@@ -114,10 +104,8 @@ SkColor NativeThemeDarkAura::GetSystemColor(ColorId color_id) const {
     case kColorId_BlueButtonShadowColor:
     case kColorId_EnabledMenuItemForegroundColor:
     case kColorId_DisabledMenuItemForegroundColor:
-    case kColorId_DisabledEmphasizedMenuItemForegroundColor:
     case kColorId_SelectedMenuItemForegroundColor:
     case kColorId_FocusedMenuItemBackgroundColor:
-    case kColorId_HoverMenuItemBackgroundColor:
     case kColorId_MenuSeparatorColor:
     case kColorId_MenuBackgroundColor:
     case kColorId_MenuBorderColor:

chrome/browser/ui/cocoa/profiles/profile_chooser_controller.mm
@@ -45,6 +45,7 @@
 #import "chrome/browser/ui/cocoa/info_bubble_window.h"
 #include "chrome/browser/ui/cocoa/profiles/signin_view_controller_delegate_mac.h"
 #import "chrome/browser/ui/cocoa/profiles/user_manager_mac.h"
+#include "chrome/browser/ui/profile_chooser_constants.h"
 #include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/user_manager.h"
 #include "chrome/browser/ui/webui/signin/login_ui_service.h"
@@ -848,11 +849,8 @@ class ActiveProfileObserverBridge : public AvatarMenuObserver,
     backgroundColor:(NSColor*)backgroundColor {
   if ((self = [super initWithFrame:frameRect])) {
     backgroundColor_.reset([backgroundColor retain]);
-    // Use a color from Aura, since this button is not trying to look like a
-    // native control.
-    SkColor hoverColor = ui::GetAuraColor(
-        ui::NativeTheme::kColorId_ButtonHoverBackgroundColor, nullptr);
-    hoverColor_.reset([skia::SkColorToSRGBNSColor(hoverColor) retain]);
+    hoverColor_.reset([skia::SkColorToSRGBNSColor(profiles::kHoverColor)
+                          retain]);
 
     [self setBordered:NO];
     [self setFont:[NSFont labelFontOfSize:kTextFontSize]];

chrome/browser/ui/libgtk2ui/gtk2_ui.cc
@@ -813,7 +813,7 @@ void Gtk2UI::LoadGtkValues() {
   NativeThemeGtk2* theme = NativeThemeGtk2::instance();
 
   SkColor toolbar_color =
-      theme->GetSystemColor(ui::NativeTheme::kColorId_LabelBackgroundColor);
+      theme->GetSystemColor(ui::NativeTheme::kColorId_DialogBackground);
   SkColor label_color =
       theme->GetSystemColor(ui::NativeTheme::kColorId_LabelEnabledColor);
 
@@ -1033,14 +1033,12 @@ void Gtk2UI::GetNormalButtonTintHSL(color_utils::HSL* tint) const {
   NativeThemeGtk2* theme = NativeThemeGtk2::instance();
 
   SkColor accent_color =
-      theme->GetSystemColor(
-          ui::NativeTheme::kColorId_ButtonHighlightColor);
+      theme->GetSystemColor(ui::NativeTheme::kColorId_ProminentButtonColor);
   SkColor text_color =
       theme->GetSystemColor(
           ui::NativeTheme::kColorId_LabelEnabledColor);
   SkColor base_color =
-      theme->GetSystemColor(
-          ui::NativeTheme::kColorId_LabelBackgroundColor);
+      theme->GetSystemColor(ui::NativeTheme::kColorId_DialogBackground);
 
   PickButtonTintFromColors(accent_color, text_color, base_color, tint);
 }
@@ -1049,8 +1047,7 @@ void Gtk2UI::GetNormalEntryForegroundHSL(color_utils::HSL* tint) const {
   NativeThemeGtk2* theme = NativeThemeGtk2::instance();
 
   SkColor accent_color =
-      theme->GetSystemColor(
-          ui::NativeTheme::kColorId_ButtonHighlightColor);
+      theme->GetSystemColor(ui::NativeTheme::kColorId_ProminentButtonColor);
   SkColor text_color =
       theme->GetSystemColor(
           ui::NativeTheme::kColorId_TextfieldDefaultColor);

chrome/browser/ui/libgtk2ui/native_theme_gtk2.cc
@@ -261,12 +261,9 @@ SkColor NativeThemeGtk2::GetSystemColor(ColorId color_id) const {
 #endif
 
     case kColorId_EnabledMenuItemForegroundColor:
-    case kColorId_DisabledEmphasizedMenuItemForegroundColor:
       return GetTextColor(GetMenuItem(), NORMAL);
     case kColorId_DisabledMenuItemForegroundColor:
       return GetTextColor(GetMenuItem(), INSENSITIVE);
-    case kColorId_HoverMenuItemBackgroundColor:
-      return GetBGColor(GetMenuItem(), PRELIGHT);
     case kColorId_FocusedMenuButtonBorderColor:
       return GetBGColor(GetEntry(), NORMAL);
     case kColorId_HoverMenuButtonBorderColor:
@@ -284,8 +281,6 @@ SkColor NativeThemeGtk2::GetSystemColor(ColorId color_id) const {
       return GetTextColor(GetEntry(), NORMAL);
     case kColorId_LabelDisabledColor:
       return GetTextColor(GetLabel(), INSENSITIVE);
-    case kColorId_LabelBackgroundColor:
-      return GetBGColor(GetWindow(), NORMAL);
 
     // Link
     case kColorId_LinkDisabled:
@@ -302,8 +297,6 @@ SkColor NativeThemeGtk2::GetSystemColor(ColorId color_id) const {
       return SK_ColorRED;
 
     // Button
-    case kColorId_ButtonBackgroundColor:
-      return GetBGColor(GetButton(), NORMAL);
     case kColorId_ButtonEnabledColor:
       return GetTextColor(GetButton(), NORMAL);
     case kColorId_BlueButtonEnabledColor:
@@ -312,19 +305,14 @@ SkColor NativeThemeGtk2::GetSystemColor(ColorId color_id) const {
       return GetTextColor(GetButton(), INSENSITIVE);
     case kColorId_BlueButtonDisabledColor:
       return GetTextColor(GetBlueButton(), INSENSITIVE);
-    case kColorId_ButtonHighlightColor:
-      return GetBaseColor(GetButton(), SELECTED);
     case kColorId_ButtonHoverColor:
       return GetTextColor(GetButton(), PRELIGHT);
     case kColorId_BlueButtonHoverColor:
       return GetTextColor(GetBlueButton(), PRELIGHT);
-    case kColorId_ButtonHoverBackgroundColor:
-      return GetBGColor(GetButton(), PRELIGHT);
     case kColorId_BlueButtonPressedColor:
       return GetTextColor(GetBlueButton(), ACTIVE);
     case kColorId_BlueButtonShadowColor:
       return SK_ColorTRANSPARENT;
-      // return GetTextColor(GetButton(), NORMAL);
     case kColorId_ProminentButtonColor:
       return GetSystemColor(kColorId_LinkEnabled);
     case kColorId_TextOnProminentButtonColor:

chrome/browser/ui/profile_chooser_constants.h
@@ -5,7 +5,11 @@
 #ifndef CHROME_BROWSER_UI_PROFILE_CHOOSER_CONSTANTS_H_
 #define CHROME_BROWSER_UI_PROFILE_CHOOSER_CONSTANTS_H_
 
+#include "third_party/skia/include/core/SkColor.h"
+
 namespace profiles {
+
+static const SkColor kHoverColor = SkColorSetRGB(0xEA, 0xEA, 0xEA);
 
 // Different views that can be displayed in the profile chooser bubble.
 enum BubbleViewMode {

chrome/browser/ui/views/passwords/manage_passwords_bubble_view.cc
@@ -11,6 +11,7 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/exclusive_access/fullscreen_controller.h"
+#include "chrome/browser/ui/passwords/password_dialog_prompts.h"
 #include "chrome/browser/ui/passwords/passwords_model_delegate.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
 #include "chrome/browser/ui/views/passwords/credentials_item_view.h"
@@ -218,13 +219,10 @@ ManagePasswordsBubbleView::AutoSigninView::AutoSigninView(
   SetLayoutManager(new views::FillLayout);
   const autofill::PasswordForm& form = parent_->model()->pending_password();
   CredentialsItemView* credential = new CredentialsItemView(
-      this,
-      base::string16(),
+      this, base::string16(),
       l10n_util::GetStringFUTF16(IDS_MANAGE_PASSWORDS_AUTO_SIGNIN_TITLE,
                                  form.username_value),
-      GetNativeTheme()->GetSystemColor(
-          ui::NativeTheme::kColorId_ButtonBackgroundColor),
-      &form,
+      kButtonHoverColor, &form,
       parent_->model()->GetProfile()->GetRequestContext());
   credential->SetEnabled(false);
   AddChildView(credential);

chrome/browser/ui/views/profiles/profile_chooser_view.cc
@@ -28,6 +28,7 @@
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/chrome_pages.h"
+#include "chrome/browser/ui/profile_chooser_constants.h"
 #include "chrome/browser/ui/singleton_tabs.h"
 #include "chrome/browser/ui/user_manager.h"
 #include "chrome/browser/ui/views/profiles/signin_view_controller_delegate_views.h"
@@ -220,8 +221,7 @@ class BackgroundColorHoverButton : public views::LabelButton {
   void OnPaint(gfx::Canvas* canvas) override {
     if ((state() == STATE_PRESSED) ||
         (state() == STATE_HOVERED)) {
-      canvas->DrawColor(GetNativeTheme()->GetSystemColor(
-          ui::NativeTheme::kColorId_ButtonHoverBackgroundColor));
+      canvas->DrawColor(profiles::kHoverColor);
     }
     LabelButton::OnPaint(canvas);
   }

chrome/browser/ui/views/toolbar/app_menu.cc
@@ -194,14 +194,12 @@ class InMenuButtonBackground : public views::Background {
                                  views::Button::ButtonState state) {
     const ui::NativeTheme* theme = view->GetNativeTheme();
     switch (state) {
-      case views::Button::STATE_HOVERED:
-        // Hovered should be handled in DrawBackground.
-        NOTREACHED();
-        return theme->GetSystemColor(
-            ui::NativeTheme::kColorId_HoverMenuItemBackgroundColor);
       case views::Button::STATE_PRESSED:
         return theme->GetSystemColor(
             ui::NativeTheme::kColorId_FocusedMenuItemBackgroundColor);
+      case views::Button::STATE_HOVERED:
+        // Hovered should be handled in DrawBackground.
+        NOTREACHED();
       default:
         return theme->GetSystemColor(
             ui::NativeTheme::kColorId_MenuBackgroundColor);
@@ -731,12 +729,6 @@ class AppMenu::RecentTabsMenuModelDelegate : public ui::MenuModelDelegate {
     return model_->GetLabelFontListAt(index);
   }
 
-  bool GetShouldUseDisabledEmphasizedForegroundColor(int index) const {
-    // The items for which we get a font list, should be shown in the bolded
-    // color.
-    return GetLabelFontListAt(index) ? true : false;
-  }
-
   // ui::MenuModelDelegate implementation:
 
   void OnIconChanged(int index) override {
@@ -862,14 +854,8 @@ const gfx::FontList* AppMenu::GetLabelFontList(int command_id) const {
   return NULL;
 }
 
-bool AppMenu::GetShouldUseDisabledEmphasizedForegroundColor(
-    int command_id) const {
-  if (IsRecentTabsCommand(command_id)) {
-    return recent_tabs_menu_model_delegate_->
-        GetShouldUseDisabledEmphasizedForegroundColor(
-            ModelIndexFromCommandId(command_id));
-  }
-  return false;
+bool AppMenu::GetShouldUseNormalForegroundColor(int command_id) const {
+  return IsRecentTabsCommand(command_id);
 }
 
 base::string16 AppMenu::GetTooltipText(int command_id,

commit b3d6d6f533c19d80ef353a786b070671e9dc0b01

Fix label colors for dark native themes (incognito, shelf).
Regressed in 0343b0c161e34bc3478c49b701fa72908fe00003 [formerly f02568a216b975130849079183cc0f7173d6b832]

BUG=653241

Review-Url: https://codereview.chromium.org/2394083002
Cr-Commit-Position: refs/heads/master@{#423556}

Former-commit-id: ec013bf782f54a4ef8f3897f390ca391c701bee2

ui/native_theme/native_theme_dark_aura.cc
@@ -31,6 +31,12 @@ SkColor NativeThemeDarkAura::GetSystemColor(ColorId color_id) const {
       SkColorSetA(kResultsTableText, 0x80);
 
   switch (color_id) {
+    // Window
+    case kColorId_WindowBackground:
+    case kColorId_DialogBackground:
+    case kColorId_BubbleBackground:
+      return SK_ColorBLACK;
+
     // Button
     case kColorId_ButtonEnabledColor:
       return kButtonEnabledColor;
@@ -91,9 +97,6 @@ SkColor NativeThemeDarkAura::GetSystemColor(ColorId color_id) const {
       return NativeThemeAura::GetSystemColor(color_id);
 
     // Any other color is not defined and shouldn't be used in a dark theme.
-    case kColorId_WindowBackground:
-    case kColorId_DialogBackground:
-    case kColorId_BubbleBackground:
     case kColorId_UnfocusedBorderColor:
     case kColorId_ButtonDisabledColor:
     case kColorId_ButtonHoverColor: