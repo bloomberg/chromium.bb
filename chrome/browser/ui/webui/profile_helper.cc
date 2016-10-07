c27344397b48789f83629670465467a3c9a76b96
zmin <zmin@chromium.org>
2016-10-07 09:29:47 -0700

    Display signin dialog with system profile after the last profile being deleted when force signin enabled.
    BUG=642059
    
    Review-Url: https://codereview.chromium.org/2392953002
    Cr-Commit-Position: refs/heads/master@{#423879}
    
    Former-commit-id: 438598fc3ff38423311a37341023690405bc34a0

--- chrome/browser/ui/webui/profile_helper.cc
+++ chrome/browser/ui/webui/profile_helper.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/ui/webui/profile_helper.h"
+
 #include "base/bind.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -9,21 +11,45 @@
 #include "chrome/browser/profiles/profile_window.h"
 #include "chrome/browser/profiles/profiles_state.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/webui/profile_helper.h"
+#include "chrome/browser/ui/user_manager.h"
+#include "chrome/browser/ui/webui/signin/signin_utils.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 #include "extensions/browser/app_window/app_window.h"
 #include "extensions/browser/app_window/app_window_registry.h"
 
 namespace webui {
+namespace {
+void ShowSigninDialog(base::FilePath signin_profile_path,
+                      Profile* system_profile,
+                      Profile::CreateStatus status) {
+  UserManager::ShowSigninDialog(system_profile, signin_profile_path);
+}
 
+}  // namespace
 void OpenNewWindowForProfile(Profile* profile, Profile::CreateStatus status) {
   if (status != Profile::CREATE_STATUS_INITIALIZED)
     return;
 
-  profiles::FindOrCreateNewWindowForProfile(
-      profile, chrome::startup::IS_PROCESS_STARTUP,
-      chrome::startup::IS_FIRST_RUN, false);
+  if (signin::IsForceSigninEnabled()) {
+// TODO(zmin): Remove the switcher once the UserManager API is finished on Mac.
+#if !defined(OS_MACOSX)
+    if (!UserManager::IsShowing()) {
+      UserManager::Show(base::FilePath(), profiles::USER_MANAGER_NO_TUTORIAL,
+                        profiles::USER_MANAGER_SELECT_PROFILE_NO_ACTION);
+    }
+
+    g_browser_process->profile_manager()->CreateProfileAsync(
+        ProfileManager::GetSystemProfilePath(),
+        base::Bind(&ShowSigninDialog, profile->GetPath()), base::string16(),
+        std::string(), std::string());
+
+#endif
+  } else {
+    profiles::FindOrCreateNewWindowForProfile(
+        profile, chrome::startup::IS_PROCESS_STARTUP,
+        chrome::startup::IS_FIRST_RUN, false);
+  }
 }
 
 void DeleteProfileAtPath(base::FilePath file_path,
