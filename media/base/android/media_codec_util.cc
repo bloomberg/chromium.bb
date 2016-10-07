d6e12383995b67ffc075bc430601b93478595f00
liberato <liberato@chromium.org>
2016-10-07 15:59:26 -0700

    Blacklist Spitzer for Lenovo A6600 / 6800 due to GL crashses.
    BUG=628059
    
    Review-Url: https://codereview.chromium.org/2406553002
    Cr-Commit-Position: refs/heads/master@{#424000}
    
    Former-commit-id: 1f4d4af144a152df25dbf7194c74368d89281a12

--- media/base/android/media_codec_util.cc
+++ media/base/android/media_codec_util.cc
@@ -91,11 +91,14 @@ static bool IsDecoderSupportedByDevice(const std::string& android_mime_type) {
 bool MediaCodecUtil::IsMediaCodecAvailable() {
   // Blacklist some devices on Jellybean as MediaCodec is buggy.
   // http://crbug.com/365494, http://crbug.com/615872
+  // Blacklist Lenovo A6600 / A6800 on KitKat, which tends to crash a lot.
+  // See crbug.com/628059 .  We include < K since they don't exist.
   if (base::android::BuildInfo::GetInstance()->sdk_int() <= 19) {
     std::string model(base::android::BuildInfo::GetInstance()->model());
     return model != "GT-I9100" && model != "GT-I9300" && model != "GT-N7000" &&
-           model != "GT-N7100";
+           model != "GT-N7100" && model != "A6600" && model != "A6800";
   }
+
   return true;
 }
 
