f1a81e51d57f78b03e4d935e838053b9fd12e250
tonikitoo <tonikitoo@igalia.com>
2016-10-09 21:07:02 -0700

    Remove some superfluous OS_CHROMEOS checks
    There is some code in ui/base/ime/input_method_factory.cc
    like below:
    
      #if defined(OS_CHROMEOS)
        ..
      #elif defined(BLEH) && !defined(OS_CHROMEOS)
        ..
      #endif
    
    The second OS_CHROMEOS check is unneeded, because if it was computed
    as true, flow would already had entered the first "#if".
    
    Review-Url: https://codereview.chromium.org/2393133006
    Cr-Commit-Position: refs/heads/master@{#424116}
    
    Former-commit-id: 78dfcd91c14b68d8f6fd4ad6efc4cba8762b7db2

--- ui/base/ime/input_method_factory.cc
+++ ui/base/ime/input_method_factory.cc
@@ -14,8 +14,7 @@
 #include "ui/base/ime/input_method_win.h"
 #elif defined(OS_MACOSX)
 #include "ui/base/ime/input_method_mac.h"
-#elif defined(USE_AURA) && defined(OS_LINUX) && defined(USE_X11) && \
-      !defined(OS_CHROMEOS)
+#elif defined(USE_AURA) && defined(OS_LINUX) && defined(USE_X11)
 #include "ui/base/ime/input_method_auralinux.h"
 #elif defined(OS_ANDROID)
 #include "ui/base/ime/input_method_android.h"
@@ -56,8 +55,7 @@ std::unique_ptr<InputMethod> CreateInputMethod(
   return base::MakeUnique<InputMethodWin>(delegate, widget);
 #elif defined(OS_MACOSX)
   return base::MakeUnique<InputMethodMac>(delegate);
-#elif defined(USE_AURA) && defined(OS_LINUX) && defined(USE_X11) && \
-      !defined(OS_CHROMEOS)
+#elif defined(USE_AURA) && defined(OS_LINUX) && defined(USE_X11)
   return base::MakeUnique<InputMethodAuraLinux>(delegate);
 #elif defined(OS_ANDROID)
   return base::MakeUnique<InputMethodAndroid>(delegate);
