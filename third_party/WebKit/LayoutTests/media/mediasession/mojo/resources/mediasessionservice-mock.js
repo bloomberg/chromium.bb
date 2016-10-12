588b509efcbce5c681c6102b35dd0d2b0bed81a0
zqzhang <zqzhang@chromium.org>
2016-10-12 02:58:34 -0700

    Make MediaSession per frame as an attribute of Navigator
    This CL makes MediaSession non user-constructible, and make it as
    an attribute of window.navigator, thus MediaSession is one object
    per frame. This CL also adds back the removed browser tests as
    layout tests using a mock blink::mojom::MediaSessionService in js.
    This CL also made MediaSession enabled in tests so no need for
    virtual tests anymore.
    
    Spec URL: https://wicg.github.io/mediasession/
    
    BUG=643190,646490,653894
    
    Review-Url: https://codereview.chromium.org/2401013002
    Cr-Commit-Position: refs/heads/master@{#424702}
    
    Former-commit-id: dbbe70b8977af46a3c33d2b1c42123f7e5d5b98b

---
+++ third_party/WebKit/LayoutTests/media/mediasession/mojo/resources/mediasessionservice-mock.js
@@ -0,0 +1,72 @@
+/*
+ * mediasessionservice-mock contains a mock implementation of MediaSessionService.
+ */
+
+"use strict";
+
+function mojoString16ToJS(mojoString16) {
+  return String.fromCharCode.apply(null, mojoString16.data);
+}
+
+function mojoImageToJS(mojoImage) {
+  var src = mojoImage.src.url;
+  var type = mojoString16ToJS(mojoImage.type);
+  var sizes = "";
+  for (var i = 0; i < mojoImage.sizes.length; i++) {
+    if (i > 0)
+      sizes += " ";
+
+    var mojoSize = mojoImage.sizes[i];
+    sizes += mojoSize.width.toString() + "x" + mojoSize.height.toString();
+  }
+  return { src: src, type: type, sizes: sizes };
+}
+
+function mojoMetadataToJS(mojoMetadata) {
+  if (mojoMetadata == null)
+    return null;
+
+  var title = mojoString16ToJS(mojoMetadata.title);
+  var artist = mojoString16ToJS(mojoMetadata.artist);
+  var album = mojoString16ToJS(mojoMetadata.album);
+  var artwork = [];
+  for (var i = 0; i < mojoMetadata.artwork.length; i++)
+    artwork.push(mojoImageToJS(mojoMetadata.artwork[i]));
+
+  return new MediaMetadata({title: title, artist: artist, album: album, artwork: artwork});
+}
+
+let mediaSessionServiceMock = loadMojoModules(
+    'mediaSessionServiceMock',
+    ['third_party/WebKit/public/platform/modules/mediasession/media_session.mojom',
+     'mojo/public/js/router',
+    ]).then(mojo => {
+      let [mediaSessionService, router] = mojo.modules;
+
+      class MediaSessionServiceMock {
+        constructor(interfaceProvider) {
+          interfaceProvider.addInterfaceOverrideForTesting(
+              mediaSessionService.MediaSessionService.name,
+              handle => this.connectMediaSessionService_(handle));
+          this.interfaceProvider_ = interfaceProvider;
+          this.pendingResponse_ = null;
+        }
+
+        connectMediaSessionService_(handle) {
+          this.mediaSessionServiceStub_ = new mediaSessionService.MediaSessionService.stubClass(this);
+          this.mediaSessionServiceRouter_ = new router.Router(handle);
+          this.mediaSessionServiceRouter_.setIncomingReceiver(this.mediaSessionServiceStub_);
+        }
+
+        setMetadata(metadata) {
+          if (!!this.metadataCallback_)
+            this.metadataCallback_(mojoMetadataToJS(metadata));
+        }
+
+        setMetadataCallback(callback) {
+          this.metadataCallback_ = callback;
+        }
+      }
+
+      return new MediaSessionServiceMock(mojo.frameInterfaces);
+    });
--- third_party/WebKit/LayoutTests/virtual/mediasession/media/mediasession/README.txt
+++
@@ -1,3 +0,0 @@
-# This suite runs the tests in media/mediasession with
-# --enable-blink-features=MediaSession
-# For details on MediaSession implementation plan, see: http://crbug.com/497735 .
