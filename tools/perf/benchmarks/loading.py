de18c997ae05f3962c4bd5eab814d894fd67bacb
nednguyen <nednguyen@google.com>
2016-10-12 10:33:47 -0700

    Create loading.cluster_telemetry benchmark
    This cluster telemetry benchmark supports these extra flags:
      --wait-time: how long to wait (second) after load
      --traffic-setting: which traffic setting to use
    
    This also refactor ct_page_set to allow parameterizing which actions to run after page navigation.
    
    Review-Url: https://codereview.chromium.org/2415483003
    Cr-Commit-Position: refs/heads/master@{#424778}
    
    Former-commit-id: 41c64c37933f5efcd670a7b2005d550b1d96b5fd

--- tools/perf/benchmarks/loading.py
+++ tools/perf/benchmarks/loading.py
@@ -5,9 +5,11 @@
 from core import perf_benchmark
 import page_sets
 
+import ct_benchmarks_util
 from benchmarks import page_cycler_v2
 from telemetry import benchmark
 from telemetry.page import cache_temperature
+from telemetry.page import traffic_setting
 
 
 @benchmark.Disabled('all')  # crbug.com/654215
@@ -26,3 +28,39 @@ class LoadingMobile(perf_benchmark.PerfBenchmark):
   def CreateStorySet(self, options):
     return page_sets.LoadingMobileStorySet(cache_temperatures=[
           cache_temperature.ANY])
+
+
+
+# Disabled because we do not plan on running CT benchmarks on the perf
+# waterfall any time soon.
+@benchmark.Disabled('all')
+class LoadingClusterTelemetry(perf_benchmark.PerfBenchmark):
+  _ALL_NET_CONFIGS = traffic_setting.NETWORK_CONFIGS.keys()
+
+  def CreateTimelineBasedMeasurementOptions(self):
+    return page_cycler_v2.TimelineBasedMeasurementOptionsForLoadingMetric()
+
+  @classmethod
+  def Name(cls):
+    return 'loading.cluster_telemetry'
+
+  @classmethod
+  def AddBenchmarkCommandLineArgs(cls, parser):
+    super(LoadingClusterTelemetry, cls).AddBenchmarkCommandLineArgs(parser)
+    ct_benchmarks_util.AddBenchmarkCommandLineArgs(parser)
+    parser.add_option(
+        '--wait-time',  action='store', type='int',
+        default=60, help='Number of seconds to wait for after navigation.')
+    parser.add_option(
+        '--traffic-setting',  choices=cls._ALL_NET_CONFIGS,
+        default=traffic_setting.REGULAR_4G,
+        help='Traffic condition (string). Default to "%%default". Can be: %s' %
+         ', '.join(cls._ALL_NET_CONFIGS))
+
+  def CreateStorySet(self, options):
+    def Wait(action_runner):
+      action_runner.Wait(options.wait_time)
+    return page_sets.CTPageSet(
+      options.urls_list, options.user_agent, options.archive_data_file,
+      traffic_setting=options.traffic_setting,
+      run_page_interaction_callback=Wait)
