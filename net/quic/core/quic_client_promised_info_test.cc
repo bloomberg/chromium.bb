6b1ab32b85ccd02724c7e108f2f443ed6d62d29e
ckrasic <ckrasic@chromium.org>
2016-10-11 18:05:20 -0700

    QUIC - minor fix for QuicClientPromisedInfoTest.
    Should be using SpdyHeaderBlock instead of BalsaHeaders.
    
    Merge internal change: 135849129
    
    BUG=654483
    
    Review-Url: https://codereview.chromium.org/2413533002
    Cr-Commit-Position: refs/heads/master@{#424622}
    
    Former-commit-id: 26fae96d2810cd0a55c71f5a6d99836f68ad4347

--- net/quic/core/quic_client_promised_info_test.cc
+++ net/quic/core/quic_client_promised_info_test.cc
@@ -11,7 +11,6 @@
 #include "net/quic/test_tools/crypto_test_utils.h"
 #include "net/test/gtest_util.h"
 #include "net/tools/quic/quic_client_session.h"
-#include "net/tools/quic/spdy_balsa_utils.h"
 
 using std::string;
 using testing::StrictMock;
@@ -72,9 +71,10 @@ class QuicClientPromisedInfoTest : public ::testing::Test {
         promise_id_(kServerDataStreamId1) {
     session_.Initialize();
 
-    headers_.SetResponseFirstlineFromStringPieces("HTTP/1.1", "200", "Ok");
-    headers_.ReplaceOrAppendHeader("content-length", "11");
-    headers_string_ = SpdyBalsaUtils::SerializeResponseHeaders(headers_);
+    headers_[":status"] = "200";
+    headers_["content-length"] = "11";
+
+    headers_string_ = SpdyUtils::SerializeUncompressedHeaders(headers_);
 
     stream_.reset(new QuicSpdyClientStream(kClientDataStreamId1, &session_));
     stream_visitor_.reset(new StreamVisitor());
@@ -142,7 +142,7 @@ class QuicClientPromisedInfoTest : public ::testing::Test {
   std::unique_ptr<QuicSpdyClientStream> stream_;
   std::unique_ptr<StreamVisitor> stream_visitor_;
   std::unique_ptr<QuicSpdyClientStream> promised_stream_;
-  BalsaHeaders headers_;
+  SpdyHeaderBlock headers_;
   string headers_string_;
   string body_;
   SpdyHeaderBlock push_promise_;
