6f7cf215a2d970a3b405554fedd45a8df8fe9ad0
jzern <jzern@chromium.org>
2016-10-13 10:56:44 -0700

    libwebp: disable SSE2 VerticalUnfilter
    causes a crash on x86 android
    
    BUG=654974
    
    Review-Url: https://codereview.chromium.org/2415883002
    Cr-Commit-Position: refs/heads/master@{#425083}
    
    Former-commit-id: 61fb879aaf2714b28f1837e98a673b3c98f3d6df

--- third_party/libwebp/dsp/filters_sse2.c
+++ third_party/libwebp/dsp/filters_sse2.c
@@ -315,7 +315,9 @@ extern void VP8FiltersInitSSE2(void);
 
 WEBP_TSAN_IGNORE_FUNCTION void VP8FiltersInitSSE2(void) {
   WebPUnfilters[WEBP_FILTER_HORIZONTAL] = HorizontalUnfilter;
-  WebPUnfilters[WEBP_FILTER_VERTICAL] = VerticalUnfilter;
+  // crbug.com/654974
+  // WebPUnfilters[WEBP_FILTER_VERTICAL] = VerticalUnfilter;
+  (void)VerticalUnfilter;
   WebPUnfilters[WEBP_FILTER_GRADIENT] = GradientUnfilter;
 
   WebPFilters[WEBP_FILTER_HORIZONTAL] = HorizontalFilter;
