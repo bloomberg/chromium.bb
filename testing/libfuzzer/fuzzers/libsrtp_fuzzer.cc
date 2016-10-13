463f7bdc67e499bb531ab155b090f292e78d2e5c
tnagel <tnagel@chromium.org>
2016-10-13 05:22:28 -0700

    Revert of Upgrade libsrtp to version 2.0 (patchset #4 id:60001 of https://codereview.chromium.org/2398073002/ )
    Reason for revert:
    Breaks the build. Reverting on behalf of Matthew.
    
    Original issue's description:
    > Upgrade libsrtp to version 2.0
    >
    > Roll DEPS for libsrtp to pick up version 2.0. Fix up remaining client.
    >
    > https://chromium.googlesource.com/chromium/deps/libsrtp.git/+log/b17c065a8a63..eccff9b52186
    >
    > $ git log b17c065a8..eccff9b52 --date=short --no-merges --format='%ad %ae %s'
    > 2016-10-12 kjellander Disable warning for signed/unsigned mismatch in MSVC
    > 2016-10-10 kjellander Make libsrtp configuration public
    > 2016-10-06 kjellander Update libsrtp to version 2.0
    >
    > BUG=webrtc:6376
    >
    > Committed: https://crrev.com/708f41098ef18e2a5644e1a893e90a758990c530 [formerly bf2e39f3e02a38491c70fa15897b0dcde1f77fba]
    > Cr-Commit-Position: refs/heads/master@{#425002}
    
    TBR=jochen@chromium.org,kjellander@chromium.org,mmoroz@chromium.org,mattdr@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=webrtc:6376
    
    Review-Url: https://codereview.chromium.org/2419733003
    Cr-Commit-Position: refs/heads/master@{#425006}
    
    Former-commit-id: 8d42c040b24a03c47e418911e3efb1901f76090b

--- testing/libfuzzer/fuzzers/libsrtp_fuzzer.cc
+++ testing/libfuzzer/fuzzers/libsrtp_fuzzer.cc
@@ -3,16 +3,15 @@
 // found in the LICENSE file.
 
 #include <assert.h>
-#include <netinet/in.h>
 #include <stddef.h>
 #include <stdint.h>
 
 #include <algorithm>
 #include <vector>
 
-#include "third_party/libsrtp/include/rtp.h"
-#include "third_party/libsrtp/include/rtp_priv.h"
-#include "third_party/libsrtp/include/srtp.h"
+#include "third_party/libsrtp/srtp/include/rtp.h"
+#include "third_party/libsrtp/srtp/include/rtp_priv.h"
+#include "third_party/libsrtp/srtp/include/srtp.h"
 
 // TODO(katrielc) Also test the authenticated path, which is what
 // WebRTC uses.  This is nontrivial because you need to bypass the MAC
@@ -36,21 +35,21 @@ struct Environment {
     switch (crypto_policy) {
       case LibSrtpFuzzer::NUMBER_OF_POLICIES:
       case LibSrtpFuzzer::NONE:
-        srtp_crypto_policy_set_null_cipher_null_auth(&policy.rtp);
-        srtp_crypto_policy_set_null_cipher_null_auth(&policy.rtcp);
+        crypto_policy_set_null_cipher_null_auth(&policy.rtp);
+        crypto_policy_set_null_cipher_null_auth(&policy.rtcp);
         break;
       case LibSrtpFuzzer::LIKE_WEBRTC:
-        srtp_crypto_policy_set_aes_cm_128_hmac_sha1_80(&policy.rtp);
-        srtp_crypto_policy_set_aes_cm_128_hmac_sha1_80(&policy.rtcp);
+        crypto_policy_set_aes_cm_128_hmac_sha1_80(&policy.rtp);
+        crypto_policy_set_aes_cm_128_hmac_sha1_80(&policy.rtcp);
         break;
       case LibSrtpFuzzer::LIKE_WEBRTC_WITHOUT_AUTH:
-        srtp_crypto_policy_set_aes_cm_128_null_auth(&policy.rtp);
-        srtp_crypto_policy_set_aes_cm_128_null_auth(&policy.rtcp);
+        crypto_policy_set_aes_cm_128_null_auth(&policy.rtp);
+        crypto_policy_set_aes_cm_128_null_auth(&policy.rtcp);
         break;
       case LibSrtpFuzzer::AES_GCM:
         // There was a security bug in the GCM mode in libsrtp 1.5.2.
-        srtp_crypto_policy_set_aes_gcm_128_8_auth(&policy.rtp);
-        srtp_crypto_policy_set_aes_gcm_128_8_auth(&policy.rtcp);
+        crypto_policy_set_aes_gcm_128_8_auth(&policy.rtp);
+        crypto_policy_set_aes_gcm_128_8_auth(&policy.rtcp);
         break;
     }
 
@@ -75,11 +74,10 @@ struct Environment {
   srtp_policy_t policy;
   unsigned char key[SRTP_MASTER_KEY_LEN] = {0};
 
-  static void srtp_crypto_policy_set_null_cipher_null_auth(
-      srtp_crypto_policy_t* p) {
-    p->cipher_type = SRTP_NULL_CIPHER;
+  static void crypto_policy_set_null_cipher_null_auth(crypto_policy_t* p) {
+    p->cipher_type = NULL_CIPHER;
     p->cipher_key_len = 0;
-    p->auth_type = SRTP_NULL_AUTH;
+    p->auth_type = NULL_AUTH;
     p->auth_key_len = 0;
     p->auth_tag_len = 0;
     p->sec_serv = sec_serv_none;
@@ -112,8 +110,8 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   size -= SRTP_MASTER_KEY_LEN;
 
   srtp_t session;
-  srtp_err_status_t error = srtp_create(&session, &srtp_policy);
-  if (error != srtp_err_status_ok) {
+  err_status_t error = srtp_create(&session, &srtp_policy);
+  if (error != err_status_ok) {
     assert(false);
     return 0;
   }
