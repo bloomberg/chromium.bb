9d3409c52930a36961f375b47a6e42069066735a
peconn <peconn@chromium.org>
2016-10-14 05:34:42 -0700

    Disable the contents of search_bar if it is being animated away.
    BUG=652615
    
    Review-Url: https://codereview.chromium.org/2418923004
    Cr-Commit-Position: refs/heads/master@{#425302}
    
    Former-commit-id: 57a54197b21499fd990e15d8edc097113b99ff5d

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/NewTabPageView.java
@@ -92,7 +92,7 @@ public class NewTabPageView extends FrameLayout
 
     private NewTabPageLayout mNewTabPageLayout;
     private LogoView mSearchProviderLogoView;
-    private View mSearchBoxView;
+    private ViewGroup mSearchBoxView;
     private ImageView mVoiceSearchButton;
     private MostVisitedLayout mMostVisitedLayout;
     private View mMostVisitedPlaceholder;
@@ -372,7 +372,7 @@ public class NewTabPageView extends FrameLayout
 
         mSearchProviderLogoView =
                 (LogoView) mNewTabPageLayout.findViewById(R.id.search_provider_logo);
-        mSearchBoxView = mNewTabPageLayout.findViewById(R.id.search_box);
+        mSearchBoxView = (ViewGroup) mNewTabPageLayout.findViewById(R.id.search_box);
         mNoSearchLogoSpacer = mNewTabPageLayout.findViewById(R.id.no_search_logo_spacer);
 
         initializeSearchBoxTextView();
@@ -491,6 +491,11 @@ public class NewTabPageView extends FrameLayout
         // the current page changes. We check it again to make sure we don't attempt to update the
         // wrong page.
         if (!mManager.isCurrentPage()) return;
+
+        // Disable the search box contents if it is the process of being animated away.
+        for (int i = 0; i < mSearchBoxView.getChildCount(); i++) {
+            mSearchBoxView.getChildAt(i).setEnabled(mSearchBoxView.getAlpha() == 1.0f);
+        }
 
         if (mSearchBoxScrollListener != null) {
             mSearchBoxScrollListener.onNtpScrollChanged(getToolbarTransitionPercentage());
