4a7caac58806c339020ab4ecd644a08bccf88f55
qiangchen <qiangchen@chromium.org>
2016-10-14 09:55:20 -0700

    Bug Fix: Initialize |disable_local_echo|
    This CL fixes the cause to a memory test failure. We did not initialize a field
    of StreamControls.
    
    BUG=655973
    
    Review-Url: https://codereview.chromium.org/2420903002
    Cr-Commit-Position: refs/heads/master@{#425362}
    
    Former-commit-id: 7098f1cafa2dc58dd43856c413987a5c25fa54fc

--- content/common/media/media_stream_options.cc
+++ content/common/media/media_stream_options.cc
@@ -24,10 +24,16 @@ TrackControls::TrackControls(const TrackControls& other) = default;
 TrackControls::~TrackControls() {}
 
 StreamControls::StreamControls()
-    : audio(false), video(false), hotword_enabled(false) {}
+    : audio(false),
+      video(false),
+      hotword_enabled(false),
+      disable_local_echo(false) {}
 
 StreamControls::StreamControls(bool request_audio, bool request_video)
-    : audio(request_audio), video(request_video), hotword_enabled(false) {}
+    : audio(request_audio),
+      video(request_video),
+      hotword_enabled(false),
+      disable_local_echo(false) {}
 
 StreamControls::~StreamControls() {}
 
