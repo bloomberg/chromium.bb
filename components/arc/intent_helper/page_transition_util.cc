8450474797ecc6dca8aab40f723e66e49c1be838
yusukes <yusukes@chromium.org>
2016-10-14 10:12:07 -0700

    Move page_transition_util to components/arc/intent_helper/
    The files have neither chrome nor content dependency, and
    hence can live in components/.
    
    BUG=None
    TEST=try
    
    Review-Url: https://codereview.chromium.org/2415983002
    Cr-Commit-Position: refs/heads/master@{#425367}
    
    Former-commit-id: 551b02d67966d8b68552da7c2b490e9bf0b9411a

--- components/arc/intent_helper/page_transition_util.cc
+++ components/arc/intent_helper/page_transition_util.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/chromeos/arc/page_transition_util.h"
+#include "components/arc/intent_helper/page_transition_util.h"
 
 namespace arc {
 
