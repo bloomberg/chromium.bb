74a46219906a212458add0666f4e440c976fb55c
Yaowu Xu <yaowu@google.com>
2016-10-14 11:05:18 -0700

    Use "av1" as codec name
    Change-Id: Iaa0795563ecb4fb8001ca4defccf540b3675a93d

--- test/aomenc.sh
+++ test/aomenc.sh
@@ -36,8 +36,8 @@ aomenc_verify_environment() {
   fi
 }
 
-aomenc_can_encode_aom() {
-  if [ "$(aom_encode_available)" = "yes" ]; then
+aomenc_can_encode_av1() {
+  if [ "$(av1_encode_available)" = "yes" ]; then
     echo yes
   fi
 }
@@ -58,9 +58,9 @@ y4m_input_720p() {
 }
 
 # Echo default aomenc real time encoding params. $1 is the codec, which defaults
-# to aom if unspecified.
+# to av1 if unspecified.
 aomenc_rt_params() {
-  local readonly codec="${1:-aom}"
+  local readonly codec="${1:-av1}"
   echo "--codec=${codec}
     --buf-initial-sz=500
     --buf-optimal-sz=600
