63fab008189a4a7bd6f5bd1b38e336ac6ad49a5b
mseaborn <mseaborn@chromium.org>
2016-10-14 18:02:34 -0700

    Re-enable tests for Non-SFI NaCl
    This reverts commit bf8d2a845673bfb8a26f79b441d5c1e188f5d85a [formerly 46cfa62f3881df61ce65e843868a6c4801c3d6b5]
    (https://codereview.chromium.org/1662283002), with an addition change
    to leave these tests disabled under ASan and other sanitizers since
    they don't pass there.
    
    The Non-SFI NaCl tests were disabled because of a one-off failure.
    It's not known what the cause of that failure was, but the tests don't
    fail at the moment.
    
    BUG=579804
    TEST=browser_tests
    
    Review-Url: https://codereview.chromium.org/1882753002
    Cr-Commit-Position: refs/heads/master@{#425521}
    
    Former-commit-id: 4a08b3a4f646f4a52d26af9b191d034b6534f048

--- chrome/test/nacl/nacl_browsertest_util.h
+++ chrome/test/nacl/nacl_browsertest_util.h
@@ -196,10 +196,11 @@ class NaClBrowserTestGLibcExtension : public NaClBrowserTestGLibc {
 
 // Similar to MAYBE_NONSFI, this is available only on x86-32, x86-64 or
 // ARM linux.
-// TODO(crbug.com/579804) -- tests disabled on Linux for flakiness.
-#if defined(OS_LINUX) && \
+#if defined(OS_LINUX) && !defined(ADDRESS_SANITIZER) && \
+    !defined(THREAD_SANITIZER) && !defined(MEMORY_SANITIZER) && \
+    !defined(LEAK_SANITIZER) && \
     (defined(ARCH_CPU_X86_FAMILY) || defined(ARCH_CPU_ARMEL))
-#  define MAYBE_PNACL_NONSFI(test_case) DISABLED_##test_case
+#  define MAYBE_PNACL_NONSFI(test_case) test_case
 #else
 #  define MAYBE_PNACL_NONSFI(test_case) DISABLED_##test_case
 #endif
