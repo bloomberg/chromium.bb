abcfeda0df0fb57c07698d1cbe3d28e4ff2a4978
jsbell <jsbell@chromium.org>
2016-10-17 14:39:59 -0700

    Indexed DB: More exception precedence tests
    For methods previously lacking such tests, add tests that cover which
    exception is thrown when a method is called with two invalid
    conditions at the same time.
    
    R=pwnall@chromium.org
    BUG=597303
    
    Review-Url: https://codereview.chromium.org/2415203002
    Cr-Commit-Position: refs/heads/master@{#425785}
    
    Former-commit-id: 1998b62c571a941c26fdc8bd1cda5fe2193d21d4

--- third_party/WebKit/LayoutTests/storage/indexeddb/resources/testharness-helpers.js
+++ third_party/WebKit/LayoutTests/storage/indexeddb/resources/testharness-helpers.js
@@ -1,23 +1,23 @@
 function indexeddb_test(upgrade_func, body_func, description) {
-    async_test(function(t) {
-        var dbName = 'db' + self.location.pathname + '-' + description;
-        var delete_request = indexedDB.deleteDatabase(dbName);
-        delete_request.onerror = t.unreached_func('deleteDatabase should not fail');
-        delete_request.onsuccess = t.step_func(function(e) {
-            var open_request = indexedDB.open(dbName);
-            open_request.onerror = t.unreached_func('open should not fail');
-            open_request.onupgradeneeded = t.step_func(function(e) {
-                upgrade_func(t, open_request.result);
-            });
-            open_request.onsuccess = t.step_func(function(e) {
-                body_func(t, open_request.result);
-            });
-        });
-    }, description);
+  async_test(function(t) {
+    var dbName = 'db' + self.location.pathname + '-' + description;
+    var delete_request = indexedDB.deleteDatabase(dbName);
+    delete_request.onerror = t.unreached_func('deleteDatabase should not fail');
+    delete_request.onsuccess = t.step_func(function(e) {
+      var open_request = indexedDB.open(dbName);
+      open_request.onerror = t.unreached_func('open should not fail');
+      open_request.onupgradeneeded = t.step_func(function(e) {
+        upgrade_func(t, open_request.result, open_request);
+      });
+      open_request.onsuccess = t.step_func(function(e) {
+        body_func(t, open_request.result);
+      });
+    });
+  }, description);
 }
 
 function assert_key_equals(a, b, message) {
-    assert_equals(indexedDB.cmp(a, b), 0, message);
+  assert_equals(indexedDB.cmp(a, b), 0, message);
 }
 
 // Call with a Test and an array of expected results in order. Returns
@@ -25,12 +25,12 @@ function assert_key_equals(a, b, message) {
 // expected number appear the order will be asserted and test
 // completed.
 function expect(t, expected) {
-    var results = [];
-    return result => {
-        results.push(result);
-        if (results.length === expected.length) {
-            assert_array_equals(results, expected);
-            t.done();
-        }
-    };
+  var results = [];
+  return result => {
+    results.push(result);
+    if (results.length === expected.length) {
+      assert_array_equals(results, expected);
+      t.done();
+    }
+  };
 }
