217c525a6ee14d6233dd71d8fc04615eededf091
brettw <brettw@chromium.org>
2016-10-17 15:08:05 -0700

    Remove space from omnibox test.
    The original bug for this test (603839) mentions a space in the host name. But
    removing the space from this test and reverting the patch still makes the test
    fail, so it seems the test itself is not dependent on the presence of the
    space.
    
    This is part of a patch to make some invalid characters like space and "*"
    invalid in URLs. Since this test seems not dependent on the presence of the
    space, I'm changing it in a separate pass.
    
    BUG=652808
    
    Review-Url: https://codereview.chromium.org/2423253002
    Cr-Commit-Position: refs/heads/master@{#425787}
    
    Former-commit-id: 8b1643a13acc2663941caf671282f23305d00775

--- components/omnibox/browser/omnibox_edit_unittest.cc
+++ components/omnibox/browser/omnibox_edit_unittest.cc
@@ -378,16 +378,16 @@ TEST_F(OmniboxEditTest, AlternateNavHasHTTP) {
   const AutocompleteMatch match(
       model()->autocomplete_controller()->search_provider(), 0, false,
       AutocompleteMatchType::SEARCH_WHAT_YOU_TYPED);
-  const GURL alternate_nav_url("http://ab%20cd/");
+  const GURL alternate_nav_url("http://abcd/");
 
   model()->OnSetFocus(false);  // Avoids DCHECK in OpenMatch().
-  model()->SetUserText(base::ASCIIToUTF16("http://ab cd"));
+  model()->SetUserText(base::ASCIIToUTF16("http://abcd"));
   model()->OpenMatch(match, WindowOpenDisposition::CURRENT_TAB,
                      alternate_nav_url, base::string16(), 0);
   EXPECT_TRUE(AutocompleteInput::HasHTTPScheme(
       client->alternate_nav_match().fill_into_edit));
 
-  model()->SetUserText(base::ASCIIToUTF16("ab cd"));
+  model()->SetUserText(base::ASCIIToUTF16("abcd"));
   model()->OpenMatch(match, WindowOpenDisposition::CURRENT_TAB,
                      alternate_nav_url, base::string16(), 0);
   EXPECT_TRUE(AutocompleteInput::HasHTTPScheme(
