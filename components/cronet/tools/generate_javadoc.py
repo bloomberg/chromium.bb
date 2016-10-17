83825f03d9922d7567edf0d12e424efa84a697f8
nednguyen <nednguyen@google.com>
2016-10-16 18:36:35 -0700

    Revert of Upgrade to new Android SDK. (patchset #16 id:300001 of https://codereview.chromium.org/2350013004/ )
    Reason for revert:
    This may have changed the adb used by adb_logcat_monitor.py  & creates a conflict with the one used by telemetry.
    
    BUG=656224
    
    Original issue's description:
    > Upgrade to new Android SDK.
    >
    > BUG=623989
    >
    > Committed: https://crrev.com/834a81847c11f4eb60c4ea551ae98e80567da5f5 [formerly e3c21739bcc8d5df70d2dd011c3f575570085b6d]
    > Cr-Commit-Position: refs/heads/master@{#425423}
    
    TBR=jbudorick@chromium.org,xunjieli@chromium.org,agrieve@chromium.org,stip@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=623989
    
    Review-Url: https://codereview.chromium.org/2425563002
    Cr-Commit-Position: refs/heads/master@{#425604}
    
    Former-commit-id: 3d5a0fb164db4c2c24e83fe859463aa4268918a4

--- components/cronet/tools/generate_javadoc.py
+++ components/cronet/tools/generate_javadoc.py
@@ -43,9 +43,7 @@ def GenerateJavadoc(options, src_dir):
              '-Dlib.java.dir=' + lib_java_dir, '-Doverview=' + overview_file,
              'doc']
   stdout = build_utils.CheckOutput(javadoc_cmd, cwd=working_dir)
-  # TODO(crbug.com/655666): add 'or "warning"' back once compiler
-  # situation is resolved.
-  if " error: " in stdout or "javadoc: error " in stdout:
+  if " error: " in stdout or "warning" in stdout or "javadoc: error " in stdout:
     build_utils.DeleteDirectory(output_dir)
     raise build_utils.CalledProcessError(working_dir, javadoc_cmd, stdout)
 
