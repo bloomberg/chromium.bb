d8a258add0ccd8b353da1b087b14b5601e3857ac
fgorski <fgorski@chromium.org>
2016-10-17 17:40:58 -0700

    Revert of [Offline pages] Ensuring verbose Offline status does not show next to the security padlock (patchset #2 id:40001 of https://codereview.chromium.org/2412273002/ )
    Reason for revert:
    Reverting per offline discussion with tedchoc@ as this patch introduced bug 655923
    
    Original issue's description:
    > [Offline pages] Ensuring verbose Offline status does not show next to the security padlock
    >
    > * Ensuring the verbose status is updated when the padlock animation ends
    > * Ensuring the URL is properly emphasized when that happens as well
    >
    > BUG=648129
    >
    > Committed: https://crrev.com/6342bbf86b4b5fc4f9483e01fbf866cfa89dc473 [formerly 083092ef6ec00129ebf7d755b323a1cb90cdb659]
    > Cr-Commit-Position: refs/heads/master@{#425140}
    
    TBR=tedchoc@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=648129
    
    Review-Url: https://codereview.chromium.org/2423153004
    Cr-Commit-Position: refs/heads/master@{#425836}
    
    Former-commit-id: 6eb43a4602315a47eb11f12ac6a302534f040070

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -674,12 +674,6 @@ public class LocationBarLayout extends FrameLayout implements OnClickListener,
                 } else if (animation == mNavigationIconShowAnimator) {
                     mSecurityButton.setVisibility(INVISIBLE);
                 }
-                // This is done specifically not to show offline page verbose status with the
-                // icon. It should be properly solved, when omnibox verbose status animation is
-                // implemented. See: http://crbug.com/648129
-                updateVerboseStatusVisibility();
-                // URL cleanup to not show not emphasized URL next to padlock.
-                setUrlToPageUrl();
             }
 
             @Override
@@ -689,8 +683,6 @@ public class LocationBarLayout extends FrameLayout implements OnClickListener,
                 } else if (animation == mNavigationIconShowAnimator) {
                     mNavigationButton.setVisibility(VISIBLE);
                 }
-                // As above. See: http://crbug.com/648129
-                updateVerboseStatusVisibility();
             }
         };
 
@@ -1397,10 +1389,8 @@ public class LocationBarLayout extends FrameLayout implements OnClickListener,
      * omnibox.
      */
     private void updateVerboseStatusVisibility() {
-        boolean verboseStatusVisible = !mUrlHasFocus
-                && mNavigationButtonType == NavigationButtonType.OFFLINE
-                && mNavigationButton.getVisibility() == VISIBLE
-                && mSecurityButton.getVisibility() == INVISIBLE;
+        boolean verboseStatusVisible =
+                mNavigationButtonType == NavigationButtonType.OFFLINE && !mUrlHasFocus;
 
         int verboseStatusVisibility = verboseStatusVisible ? VISIBLE : GONE;
 
