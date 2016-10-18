219a2be4503fdec4bb46c6366a93cd1613c3411b
mlippautz <mlippautz@chromium.org>
2016-10-18 11:57:42 -0700

    [wrapper-tracing] Add ContainerNode write barrier
    Make it an explicit call because we don't want to increase ContainerNode's
    memory footprint.
    
    BUG=chromium:468240
    R=haraken@chromium.org,hlopko@chromium.org
    
    Review-Url: https://chromiumcodereview.appspot.com/2423963003
    Cr-Commit-Position: refs/heads/master@{#426016}
    
    Former-commit-id: 171a611ec4b033ddeee36c897711f15f3adda75c

--- third_party/WebKit/Source/core/dom/ContainerNode.h
+++ third_party/WebKit/Source/core/dom/ContainerNode.h
@@ -26,6 +26,7 @@
 #define ContainerNode_h
 
 #include "bindings/core/v8/ExceptionStatePlaceholder.h"
+#include "bindings/core/v8/ScriptWrappableVisitor.h"
 #include "core/CoreExport.h"
 #include "core/dom/Node.h"
 #include "core/html/CollectionType.h"
@@ -311,8 +312,14 @@ class CORE_EXPORT ContainerNode : public Node {
       const QualifiedName* attrName = nullptr,
       Element* attributeOwnerElement = nullptr);
 
-  void setFirstChild(Node* child) { m_firstChild = child; }
-  void setLastChild(Node* child) { m_lastChild = child; }
+  void setFirstChild(Node* child) {
+    m_firstChild = child;
+    ScriptWrappableVisitor::writeBarrier(this, m_firstChild);
+  }
+  void setLastChild(Node* child) {
+    m_lastChild = child;
+    ScriptWrappableVisitor::writeBarrier(this, m_lastChild);
+  }
 
   // Utility functions for NodeListsNodeData API.
   template <typename Collection>
