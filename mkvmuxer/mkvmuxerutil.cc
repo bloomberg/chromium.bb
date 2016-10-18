9732ae991efb71aced4267d4794918279e362d99
James Zern <jzern@google.com>
2016-10-18 15:31:17 -0700

    EbmlElementSize: quiet uint64->int32 conv warning
    Change-Id: I189c3a3ee2f946034a5f7ad1bd5e1d0a301d24c0

--- mkvmuxer/mkvmuxerutil.cc
+++ mkvmuxer/mkvmuxerutil.cc
@@ -253,7 +253,7 @@ uint64 EbmlElementSize(uint64 type, uint64 value) {
 
 uint64 EbmlElementSize(uint64 type, uint64 value, uint64 fixed_size) {
   // Size of EBML ID
-  int32 ebml_size = GetUIntSize(type);
+  uint64 ebml_size = GetUIntSize(type);
 
   // Datasize
   ebml_size += (fixed_size > 0) ? fixed_size : GetUIntSize(value);
