da4fb94624ea9e469eb8713541b5cd490b46a03c
yoav <yoav@yoav.ws>
2016-10-19 07:17:16 -0700

    Refactor LinkStyle out of HTMLLinkElement
    BUG=
    
    Review-Url: https://chromiumcodereview.appspot.com/2426513003
    Cr-Commit-Position: refs/heads/master@{#426179}
    
    Former-commit-id: f37493aca7831b1acea7a8af6ca4996c55ab15b7

--- third_party/WebKit/Source/core/html/HTMLLinkElement.h
+++ third_party/WebKit/Source/core/html/HTMLLinkElement.h
@@ -25,17 +25,12 @@
 #define HTMLLinkElement_h
 
 #include "core/CoreExport.h"
-#include "core/css/CSSStyleSheet.h"
 #include "core/dom/DOMTokenList.h"
-#include "core/dom/IconURL.h"
 #include "core/dom/IncrementLoadEventDelayCount.h"
-#include "core/dom/StyleEngine.h"
-#include "core/fetch/ResourceOwner.h"
-#include "core/fetch/StyleSheetResource.h"
-#include "core/fetch/StyleSheetResourceClient.h"
 #include "core/html/HTMLElement.h"
 #include "core/html/LinkRelAttribute.h"
 #include "core/html/LinkResource.h"
+#include "core/html/LinkStyle.h"
 #include "core/html/RelList.h"
 #include "core/loader/LinkLoader.h"
 #include "core/loader/LinkLoaderClient.h"
@@ -43,87 +38,8 @@
 
 namespace blink {
 
-class HTMLLinkElement;
 class KURL;
 class LinkImport;
-
-//
-// LinkStyle handles dynamically change-able link resources, which is
-// typically @rel="stylesheet".
-//
-// It could be @rel="shortcut icon" or something else though. Each of
-// types might better be handled by a separate class, but dynamically
-// changing @rel makes it harder to move such a design so we are
-// sticking current way so far.
-//
-class LinkStyle final : public LinkResource, ResourceOwner<StyleSheetResource> {
-  USING_GARBAGE_COLLECTED_MIXIN(LinkStyle);
-
- public:
-  static LinkStyle* create(HTMLLinkElement* owner);
-
-  explicit LinkStyle(HTMLLinkElement* owner);
-  ~LinkStyle() override;
-
-  LinkResourceType type() const override { return Style; }
-  void process() override;
-  void ownerRemoved() override;
-  bool hasLoaded() const override { return m_loadedSheet; }
-  DECLARE_VIRTUAL_TRACE();
-
-  void startLoadingDynamicSheet();
-  void notifyLoadedSheetAndAllCriticalSubresources(
-      Node::LoadedSheetErrorStatus);
-  bool sheetLoaded();
-
-  void setDisabledState(bool);
-  void setSheetTitle(
-      const String&,
-      StyleEngine::ActiveSheetsUpdate = StyleEngine::DontUpdateActiveSheets);
-
-  bool styleSheetIsLoading() const;
-  bool hasSheet() const { return m_sheet; }
-  bool isDisabled() const { return m_disabledState == Disabled; }
-  bool isEnabledViaScript() const {
-    return m_disabledState == EnabledViaScript;
-  }
-  bool isUnset() const { return m_disabledState == Unset; }
-
-  CSSStyleSheet* sheet() const { return m_sheet.get(); }
-
- private:
-  // From StyleSheetResourceClient
-  void setCSSStyleSheet(const String& href,
-                        const KURL& baseURL,
-                        const String& charset,
-                        const CSSStyleSheetResource*) override;
-  String debugName() const override { return "LinkStyle"; }
-
-  enum DisabledState { Unset, EnabledViaScript, Disabled };
-
-  enum PendingSheetType { None, NonBlocking, Blocking };
-
-  void clearSheet();
-  void addPendingSheet(PendingSheetType);
-  void removePendingSheet();
-  Document& document();
-
-  void setCrossOriginStylesheetStatus(CSSStyleSheet*);
-  void setFetchFollowingCORS() {
-    DCHECK(!m_fetchFollowingCORS);
-    m_fetchFollowingCORS = true;
-  }
-  void clearFetchFollowingCORS() { m_fetchFollowingCORS = false; }
-
-  Member<CSSStyleSheet> m_sheet;
-  DisabledState m_disabledState;
-  PendingSheetType m_pendingSheetType;
-  StyleEngineContext m_styleEngineContext;
-  bool m_loading;
-  bool m_firedLoad;
-  bool m_loadedSheet;
-  bool m_fetchFollowingCORS;
-};
 
 class CORE_EXPORT HTMLLinkElement final : public HTMLElement,
                                           public LinkLoaderClient,
