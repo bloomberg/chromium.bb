13386a20bc15659d872722d5e4403237bd589c94
nhiroki <nhiroki@chromium.org>
2016-10-18 20:59:53 -0700

    Worker: Remove an outdated TODO comment from WorkerScriptLoader
    |m_finishedCallback| is now guaranteed to be invoked even if this loader is used
    from a worker context and the worker shuts down in the middle of an operation.
    In the case, the callback is invoked via WorkerScriptLoader::cancel() called
    from InProcessWorkerBase::contextDestroyed().
    
    BUG=650131
    
    Review-Url: https://chromiumcodereview.appspot.com/2431593002
    Cr-Commit-Position: refs/heads/master@{#426129}
    
    Former-commit-id: 5f28d4a21bb7fd0c2af45451c524dfb1bc62c94e

--- third_party/WebKit/Source/core/workers/WorkerScriptLoader.h
+++ third_party/WebKit/Source/core/workers/WorkerScriptLoader.h
@@ -64,9 +64,7 @@ class CORE_EXPORT WorkerScriptLoader final
                          const KURL&,
                          CrossOriginRequestPolicy,
                          WebAddressSpace);
-  // TODO: |finishedCallback| is not currently guaranteed to be invoked if
-  // used from worker context and the worker shuts down in the middle of an
-  // operation. This will cause leaks when we support nested workers.
+
   // Note that callbacks could be invoked before loadAsynchronously() returns.
   void loadAsynchronously(ExecutionContext&,
                           const KURL&,
