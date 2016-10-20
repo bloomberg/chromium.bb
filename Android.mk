d965b5bb880f6d09050a282bce50329a87e5a3d6
Joe Onorato <joeo@google.com>
2016-10-20 13:34:24 -0700

    Compile libjsoncpp for the host too.
    Change-Id: Id4f9d51267626f8c11c253cdb224ab019ff40139
    Test: External library. No tests.

--- Android.mk
+++ Android.mk
@@ -1,6 +1,9 @@
-BASE_PATH := $(call my-dir)
+
+
 LOCAL_PATH:= $(call my-dir)
 
+# Device library
+# ================================================
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= \
@@ -25,3 +28,32 @@ LOCAL_MODULE := \
 	libjsoncpp
 
 include $(BUILD_STATIC_LIBRARY)
+
+
+
+# Host library
+# ================================================
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+	src/lib_json/json_reader.cpp \
+	src/lib_json/json_value.cpp \
+	src/lib_json/json_writer.cpp
+
+LOCAL_C_INCLUDES:= \
+	$(LOCAL_PATH)/include \
+	$(LOCAL_PATH)/src/lib_json
+
+LOCAL_EXPORT_C_INCLUDE_DIRS := \
+	$(LOCAL_PATH)/include
+
+LOCAL_CFLAGS := \
+	-DJSON_USE_EXCEPTION=0
+
+LOCAL_MODULE := \
+	libjsoncpp
+
+include $(BUILD_HOST_STATIC_LIBRARY)
+
+
+
