ede33d03323cc8b9ebf9d7083f537e6b12c4c36a
brianosman <brianosman@google.com>
2016-10-21 14:54:39 -0700

    Enable deprecated SkColorSpace factories named "NewXXX"
    Factories are being renamed to "MakeXXX" to be consistent with other parts
    of Skia.
    
    See also: https://skia-review.googlesource.com/c/3822/
    
    BUG=
    
    Review-Url: https://chromiumcodereview.appspot.com/2442053002
    Cr-Commit-Position: refs/heads/master@{#426904}
    
    Former-commit-id: b07f20559996aa8154a3a67e742676a9799e37c4

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -205,6 +205,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_CLIP_REGIONOPS
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_COLOR_SPACE_FACTORIES
+#define SK_SUPPORT_LEGACY_COLOR_SPACE_FACTORIES
+#endif
+
 // Workaround for poor anisotropic mipmap quality,
 // pending Skia ripmap support.
 // (https://bugs.chromium.org/p/skia/issues/detail?id=4863)
