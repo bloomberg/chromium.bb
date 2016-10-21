80261debd6b354a845bf54bd0bcbf06c6880347b
xhwang <xhwang@chromium.org>
2016-10-20 17:53:38 -0700

    media: Reorganize media unittests
    - Put the current "media" unittest files in a source_set.
    - Add a "media_base_unittests" target which includes only "media"
      unittests.
    - Update "media_unittests" so that it includes "media" unittests for now
      but in the future it will also include tests for other subfolders,
      e.g. caputre, remoting, mojo etc.
    - Also rename "unittests" targets to "unit_tests" to be consistent to
      the rest of the codebase.
    
    BUG=656112
    TEST=This only reorganizes tests and doesn't change any existing tests.
    
    Review-Url: https://chromiumcodereview.appspot.com/2426773005
    Cr-Commit-Position: refs/heads/master@{#426665}
    
    Former-commit-id: f072c4c56b530b857771e3eea4358ba0daea3c19

--- media/test/run_all_unittests.cc
+++ media/test/run_all_unittests.cc
@@ -43,6 +43,7 @@ void TestSuiteNoAtExit::Initialize() {
   JNIEnv* env = base::android::AttachCurrentThread();
   // Needed for surface texture support.
   ui::gl::android::RegisterJni(env);
+
   media::RegisterJni(env);
 
   if (media::MediaCodecUtil::IsMediaCodecAvailable())
@@ -61,6 +62,6 @@ int main(int argc, char** argv) {
   TestSuiteNoAtExit test_suite(argc, argv);
 
   return base::LaunchUnitTests(
-      argc, argv, base::Bind(&TestSuiteNoAtExit::Run,
-                             base::Unretained(&test_suite)));
+      argc, argv,
+      base::Bind(&TestSuiteNoAtExit::Run, base::Unretained(&test_suite)));
 }
