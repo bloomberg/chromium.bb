0d9a73c4083d8089f46b3250df291e38abcc80d7
ericrk <ericrk@chromium.org>
2016-10-24 14:05:49 -0700

    Revert of Remove GpuRasterization.BlueBox suppression for Win/AMD. (patchset #1 id:1 of https://chromiumcodereview.appspot.com/2442963002/ )
    Reason for revert:
    Looks like the tests use --force-gpu-rasterization, which overrides the blacklist. I think we still need this suppression.
    
    See:
    https://build.chromium.org/p/tryserver.chromium.angle/builders/win_angle_rel_ng/builds/2567
    https://build.chromium.org/p/chromium.gpu.fyi/builders/Win7%20Debug%20%28ATI%29/builds/6394
    
    Original issue's description:
    > Remove GpuRasterization.BlueBox suppression for Win/AMD.
    >
    > GPU rasterization has been disabled on this configuration.
    >
    > BUG=653538
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    > TBR=zmo@chromium.org
    >
    > Committed: https://crrev.com/9a50668ad3254baec73741e44462ed87a8aabbb8 [formerly 516005ea9c2ffe59b5d468bf4673a549996010df]
    > Cr-Commit-Position: refs/heads/master@{#426972}
    
    TBR=zmo@chromium.org,kbr@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=653538
    
    Review-Url: https://codereview.chromium.org/2443283002
    Cr-Commit-Position: refs/heads/master@{#427149}
    
    Former-commit-id: 02503a0d49c9335c434730dc77e2d38c620f2310

--- content/test/gpu/gpu_tests/gpu_rasterization_expectations.py
+++ content/test/gpu/gpu_tests/gpu_rasterization_expectations.py
@@ -11,4 +11,7 @@ class GpuRasterizationExpectations(GpuTestExpectations):
     # Sample Usage:
     # self.Fail('GpuRasterization.BlueBox',
     #     ['mac', 'amd', ('nvidia', 0x1234)], bug=123)
-    pass
+
+    # Seems to be flaky on the new AMD R7 240 drivers.
+    self.Flaky('GpuRasterization.BlueBox',
+        ['win', ('amd', 0x6613)], bug=653538)
