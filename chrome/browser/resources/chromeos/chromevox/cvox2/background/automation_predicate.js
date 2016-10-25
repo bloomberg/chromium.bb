0254fdab974c519f4e9e0daacf3791be656a9637
dtseng <dtseng@chromium.org>
2016-10-25 11:45:00 -0700

    Treat landmark nodes like containers
    In object navigation, it is appropriate to move into landmark nodes, scanning for another node to visit.
    
    Notice that landmark nodes are not structural containers, so they won't be ignored even if we end up landing on the landmark node directly (in the case they are focusable).
    
    BUG=587654
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2446903004
    Cr-Commit-Position: refs/heads/master@{#427424}
    
    Former-commit-id: c616280803921c1b42ab89c0a6e52b2d7717c2be

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/automation_predicate.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/automation_predicate.js
@@ -235,16 +235,27 @@ AutomationPredicate.linebreak = function(first, second) {
  * @return {boolean}
  */
 AutomationPredicate.container = function(node) {
-  return AutomationPredicate.structuralContainer(node) ||
-      node.role == Role.div ||
-      node.role == Role.document ||
-      node.role == Role.group ||
-      node.role == Role.listItem ||
-      node.role == Role.toolbar ||
-      node.role == Role.window ||
-      // For example, crosh.
-      (node.role == Role.textField && node.state.readOnly) ||
-      (node.state.editable && node.parent && !node.parent.state.editable);
+  return AutomationPredicate.match({
+    anyRole: [
+      Role.div,
+      Role.document,
+      Role.group,
+      Role.listItem,
+      Role.toolbar,
+      Role.window],
+    anyPredicate: [
+      AutomationPredicate.landmark,
+      AutomationPredicate.structuralContainer,
+      function(node) {
+        // For example, crosh.
+        return (node.role == Role.textField && node.state.readOnly);
+      },
+      function(node) {
+        return (node.state.editable &&
+            node.parent &&
+            !node.parent.state.editable);
+      }]
+  })(node);
 };
 
 /**
