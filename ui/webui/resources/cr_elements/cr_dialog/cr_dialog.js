0a221661fa02fb82101654c6ae0169138c32511f
tommycli <tommycli@chromium.org>
2016-10-24 19:41:19 -0700

    MD Settings: Don't auto-close navigable dialogs on popstate.
    Previously, we made all cr-dialogs close on popstate. This is good,
    except for navigable dialogs.
    
    For navigable dialogs: It should open if the new route is the dialog's
    route, and close otherwise. All the navigable dialogs already have code
    that implements this behavior, but it conflicts with the popstate
    event handler on cr-dialog.
    
    This CL adds a parameter that disables this behavior for navigable
    dialogs.
    
    BUG=656918
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2445713004
    Cr-Commit-Position: refs/heads/master@{#427249}
    
    Former-commit-id: d263cf266480fb3472db7b56d54291466226c592

--- ui/webui/resources/cr_elements/cr_dialog/cr_dialog.js
+++ ui/webui/resources/cr_elements/cr_dialog/cr_dialog.js
@@ -15,12 +15,23 @@ Polymer({
   is: 'cr-dialog',
   extends: 'dialog',
 
+  properties: {
+    /**
+     * True if the dialog should remain open on 'popstate' events. This is used
+     * for navigable dialogs that have their separate navigation handling code.
+     */
+    ignorePopstate: {
+      type: Boolean,
+      value: false,
+    },
+  },
+
   /** @override */
-  created: function() {
+  ready: function() {
     // If the active history entry changes (i.e. user clicks back button),
     // all open dialogs should be cancelled.
     window.addEventListener('popstate', function() {
-      if (this.open)
+      if (!this.ignorePopstate && this.open)
         this.cancel();
     }.bind(this));
   },
