f7e56b70b57d030dbaf15e7db3e87c07966254f7
dongseong.hwang <dongseong.hwang@intel.com>
2016-10-25 14:16:41 -0700

    Reland of gpu: Clarify sized texture format is available (https://codereview.chromium.org/2117183006)
    Reason for revert:
    r425684 causes blank video of mediaStream playback in MacOSX, because MacOSX
    relies on the hack, which the CL removed. This CL remains the hack only in MacOSX.
    
    BUG=535198, 657532
    
    > Revert of gpu: Clarify sized texture format is available only if ES3 context or immutable texture is supported (patchset #7 id:160001 of https://codereview.chromium.org/2117183006/ )
    >
    > Reason for revert:
    > Broke playing back recorded data as detailed
    > in https://crbug.com/657532.  The bug has
    > step-by-step notes on how to repro (see also #4
    > in there).
    >
    > Locally reverting of this CL restores the functionality.
    >
    > Original issue's description:
    > > gpu: Clarify the sized texture format is available only if ES3 context or immutable texture is supported.
    > >
    > > This clarification makes it possible that gles2 decoder tracks true internal format of immutable texture. So this CL removes internal_format hack in GLES2DecoderImpl::TexStorageImpl()
    > >
    > > In addition, it makes CMAA skip redundant copy path because CMAA can know true internal format.
    > >
    > > BUG=535198
    > > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    > >
    > > Committed: https://crrev.com/5137f7099bd751e79b610f515777cb738973f12e [formerly 730f8089f791fa31a3ee2a7f5059a1052b129d21]
    > > Cr-Commit-Position: refs/heads/master@{#425684}
    >
    > TBR=dongseong.hwang@chromium.org,zmo@chromium.org,kbr@chromium.org,dongseong.hwang@intel.com
    > # Not skipping CQ checks because original CL landed more than 1 days ago.
    > BUG=535198
    >
    > Review-Url: https://codereview.chromium.org/2443123004
    > Cr-Commit-Position: refs/heads/master@{#427229}
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2448083003
    Cr-Commit-Position: refs/heads/master@{#427473}
    
    Former-commit-id: 460cac969e2e9ac38a2611be1a32db0361d88bfb

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -16545,22 +16545,24 @@ void GLES2DecoderImpl::TexStorageImpl(GLenum target,
     GLsizei level_width = width;
     GLsizei level_height = height;
     GLsizei level_depth = depth;
-    GLenum adjusted_format =
-        feature_info_->IsES3Enabled() ? internal_format : format;
+    GLenum adjusted_format = internal_format;
+#if defined(OS_MACOSX)
+    // TODO(dshwang): remove it after fixing the root cause. crbug.com/657532
+    adjusted_format = feature_info_->IsES3Enabled() ? internal_format : format;
+#endif
+
     for (int ii = 0; ii < levels; ++ii) {
       if (target == GL_TEXTURE_CUBE_MAP) {
         for (int jj = 0; jj < 6; ++jj) {
           GLenum face = GL_TEXTURE_CUBE_MAP_POSITIVE_X + jj;
-          texture_manager()->SetLevelInfo(texture_ref, face, ii,
-                                          adjusted_format,
-                                          level_width, level_height, 1,
-                                          0, format, type, gfx::Rect());
+          texture_manager()->SetLevelInfo(
+              texture_ref, face, ii, adjusted_format, level_width, level_height,
+              1, 0, format, type, gfx::Rect());
         }
       } else {
-        texture_manager()->SetLevelInfo(texture_ref, target, ii,
-                                        adjusted_format,
-                                        level_width, level_height, level_depth,
-                                        0, format, type, gfx::Rect());
+        texture_manager()->SetLevelInfo(
+            texture_ref, target, ii, adjusted_format, level_width, level_height,
+            level_depth, 0, format, type, gfx::Rect());
       }
       level_width = std::max(1, level_width >> 1);
       level_height = std::max(1, level_height >> 1);
