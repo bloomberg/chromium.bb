de9ae6aac6d9566c70b5c6ad516cf94fad5e4cbc
liaoyuke <liaoyuke@chromium.org>
2016-10-25 17:17:17 -0700

    Remove dead code path that was originally designed as workaround for pre-iOS8 system versions.
    BUG=659240
    
    Review-Url: https://codereview.chromium.org/2447243002
    Cr-Commit-Position: refs/heads/master@{#427543}
    
    Former-commit-id: e9f47a754a93f979627f28988032d19b75728b22

--- ios/chrome/browser/ui/UIView+SizeClassSupport.mm
+++ ios/chrome/browser/ui/UIView+SizeClassSupport.mm
@@ -27,48 +27,24 @@ bool IsSizeClassSpecified(UIUserInterfaceSizeClass size_class) {
   return size_class != UIUserInterfaceSizeClassUnspecified;
 }
 
-// The height of an iPhone 6 in portrait.  A UIWindow's size class idiom is
-// REGULAR if the frame's size is greater than this value in that dimension.
-const CGFloat kIPhone6PortraitHeight = 667.0f;
-
-// Returns the SizeClassIdiom for a UIWindow with a side of |side_length| points
-// in a given dimension.  This fallback approach is used when:
-// - |-traitCollection| is not implemented (i.e. pre-iOS8), or
-// - both the target and the UIApplication's |-keyWindow| have an unspecified
-//   size class.
-SizeClassIdiom SizeClassForSideLength(CGFloat side_length) {
-  return side_length > kIPhone6PortraitHeight ? REGULAR : COMPACT;
-}
-
 }  // namespace
 
 @implementation UIView (SizeClassSupport)
 
 - (SizeClassIdiom)cr_widthSizeClass {
   UIWindow* keyWindow = [UIApplication sharedApplication].keyWindow;
-  if ([self respondsToSelector:@selector(traitCollection)]) {
-    UIUserInterfaceSizeClass sizeClass =
-        self.traitCollection.horizontalSizeClass;
-    if (!IsSizeClassSpecified(sizeClass))
-      sizeClass = keyWindow.traitCollection.horizontalSizeClass;
-    if (IsSizeClassSpecified(sizeClass))
-      return GetSizeClassIdiom(sizeClass);
-    LOG(WARNING) << "Encountered UIWindow with unspecified width size class.";
-  }
-  return SizeClassForSideLength(CGRectGetWidth(keyWindow.frame));
+  UIUserInterfaceSizeClass sizeClass = self.traitCollection.horizontalSizeClass;
+  if (!IsSizeClassSpecified(sizeClass))
+    sizeClass = keyWindow.traitCollection.horizontalSizeClass;
+  return GetSizeClassIdiom(sizeClass);
 }
 
 - (SizeClassIdiom)cr_heightSizeClass {
   UIWindow* keyWindow = [UIApplication sharedApplication].keyWindow;
-  if ([self respondsToSelector:@selector(traitCollection)]) {
-    UIUserInterfaceSizeClass sizeClass = self.traitCollection.verticalSizeClass;
-    if (!IsSizeClassSpecified(sizeClass))
-      sizeClass = keyWindow.traitCollection.verticalSizeClass;
-    if (IsSizeClassSpecified(sizeClass))
-      return GetSizeClassIdiom(sizeClass);
-    LOG(WARNING) << "Encountered UIWindow with unspecified height size class.";
-  }
-  return SizeClassForSideLength(CGRectGetHeight(keyWindow.frame));
+  UIUserInterfaceSizeClass sizeClass = self.traitCollection.verticalSizeClass;
+  if (!IsSizeClassSpecified(sizeClass))
+    sizeClass = keyWindow.traitCollection.verticalSizeClass;
+  return GetSizeClassIdiom(sizeClass);
 }
 
 @end
