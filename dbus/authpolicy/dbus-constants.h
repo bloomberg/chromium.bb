6b679748bee1f2593d523dfb690e1f2d9c982f12
Roman Sorokin <rsorokin@chromium.org>
2016-10-26 18:44:33 +0200

    Add error codes for authpolicy
    This will be used by auth_policy_client on the Chrome side
    
    BUG=chromium:653509
    TEST=compiles
    
    Change-Id: I77c9f0efdb67f8863ccfd0ca4976104396206a31
    Reviewed-on: https://chromium-review.googlesource.com/403809
    Commit-Ready: Roman Sorokin <rsorokin@chromium.org>
    Tested-by: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- dbus/authpolicy/dbus-constants.h
+++ dbus/authpolicy/dbus-constants.h
@@ -14,6 +14,31 @@ const char kAuthPolicyAuthenticateUser[] = "AuthenticateUser";
 const char kAuthPolicyJoinADDomain[] = "JoinADDomain";
 const char kAuthPolicyRefreshUserPolicy[] = "RefreshUserPolicy";
 const char kAuthPolicyRefreshDevicePolicy[] = "RefreshDevicePolicy";
+
+// Enum values.
+enum ADJoinErrorType {
+  AD_JOIN_ERROR_NONE = 0,
+  AD_JOIN_ERROR_UNKNOWN = 1,
+  AD_JOIN_ERROR_DBUS_FAILURE = 2,
+};
+
+enum AuthUserErrorType {
+  AUTH_USER_ERROR_NONE = 0,
+  AUTH_USER_ERROR_UNKNOWN = 1,
+  AUTH_USER_ERROR_DBUS_FAILURE = 2,
+};
+
+enum RefreshUserPolicyErrorType {
+  REFRESH_USER_POLICY_ERROR_NONE = 0,
+  REFRESH_USER_POLICY_ERROR_UNKNOWN = 1,
+  REFRESH_USER_POLICY_ERROR_DBUS_FAILURE = 2,
+};
+
+enum RefreshDevicePolicyErrorType {
+  REFRESH_DEVICE_POLICY_ERROR_NONE = 0,
+  REFRESH_DEVICE_POLICY_ERROR_UNKNOWN = 1,
+  REFRESH_DEVICE_POLICY_ERROR_DBUS_FAILURE = 2,
+};
 }  // namespace authpolicy
 
 #endif  // SYSTEM_API_DBUS_AUTHPOLICY_DBUS_CONSTANTS_H_
