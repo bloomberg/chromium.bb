2b5f7aba986c30d9f9fe16090d8851d15a10e083
steimel <steimel@chromium.org>
2016-10-27 15:18:16 -0700

    Proto-based Syncable interface with LwwRegister
    Review-Url: https://codereview.chromium.org/2436253003
    Cr-Commit-Position: refs/heads/master@{#428163}
    
    Former-commit-id: 669bfc4d5ec1248ce1a374fd0e762d61d8dc3f7e

--- blimp/helium/sample/syncable_comparison_unittest.cc
+++ blimp/helium/sample/syncable_comparison_unittest.cc
@@ -12,6 +12,9 @@
 #include "base/memory/ptr_util.h"
 #include "blimp/helium/compound_syncable.h"
 #include "blimp/helium/lww_register.h"
+#include "blimp/helium/sample/proto_lww_register.h"
+#include "blimp/helium/sample/proto_syncable.h"
+#include "blimp/helium/sample/sample.pb.h"
 #include "blimp/helium/syncable_common.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/protobuf/src/google/protobuf/io/coded_stream.h"
@@ -154,6 +157,163 @@ class CodedStreamSyncableConfig : public SyncableConfig {
   DISALLOW_COPY_AND_ASSIGN(CodedStreamSyncableConfig);
 };
 
+class TemplatedSyncableConfig : public SyncableConfig {
+ public:
+  TemplatedSyncableConfig()
+      : client_syncable_(Peer::CLIENT), engine_syncable_(Peer::ENGINE) {}
+
+  ~TemplatedSyncableConfig() override {}
+
+  InputMethodState* GetClientSyncable() override { return &client_syncable_; }
+
+  InputMethodState* GetEngineSyncable() override { return &engine_syncable_; }
+
+  void Synchronize() override {
+    bytes_sent_ += SynchronizeOneWay(last_sync_client_, &client_syncable_,
+                                     &engine_syncable_);
+    bytes_received_ += SynchronizeOneWay(last_sync_engine_, &engine_syncable_,
+                                         &client_syncable_);
+
+    last_sync_client_ = RevisionGenerator::GetInstance()->current();
+    last_sync_engine_ = RevisionGenerator::GetInstance()->current();
+  }
+
+  size_t BytesSent() override { return bytes_sent_; }
+
+  size_t BytesReceived() override { return bytes_received_; }
+
+ private:
+  class InputMethodStateSyncable
+      : public ProtoSyncable<InputMethodChangesetMessage>,
+        public InputMethodState {
+   public:
+    explicit InputMethodStateSyncable(Peer running_as)
+        : input_generation_(
+              base::MakeUnique<ProtoLwwRegister<int>>(Peer::ENGINE,
+                                                      running_as)),
+          input_type_(base::MakeUnique<ProtoLwwRegister<int>>(Peer::ENGINE,
+                                                              running_as)),
+          response_generation_(
+              base::MakeUnique<ProtoLwwRegister<int>>(Peer::CLIENT,
+                                                      running_as)),
+          response_(
+              base::MakeUnique<ProtoLwwRegister<std::string>>(Peer::CLIENT,
+                                                              running_as)) {
+      members_.push_back(input_generation_.get());
+      members_.push_back(input_type_.get());
+      members_.push_back(response_generation_.get());
+      members_.push_back(response_.get());
+    }
+    ~InputMethodStateSyncable() override {}
+
+    // ProtoSyncable implementation.
+    std::unique_ptr<InputMethodChangesetMessage> CreateChangesetToCurrent(
+        Revision from) override {
+      std::unique_ptr<InputMethodChangesetMessage> changeset =
+          base::MakeUnique<InputMethodChangesetMessage>();
+      changeset->set_allocated_input_generation(
+          input_generation_->CreateChangesetToCurrent(from).release());
+      changeset->set_allocated_input_type(
+          input_type_->CreateChangesetToCurrent(from).release());
+      changeset->set_allocated_response_generation(
+          response_generation_->CreateChangesetToCurrent(from).release());
+      changeset->set_allocated_response(
+          response_->CreateChangesetToCurrent(from).release());
+      return changeset;
+    }
+
+    Result ApplyChangeset(
+        Revision to,
+        std::unique_ptr<InputMethodChangesetMessage> changeset) override {
+      std::vector<Result> results;
+      results.push_back(input_generation_->ApplyChangeset(
+          to, base::WrapUnique<LwwRegisterChangesetMessage>(
+                  changeset->release_input_generation())));
+      results.push_back(input_type_->ApplyChangeset(
+          to, base::WrapUnique<LwwRegisterChangesetMessage>(
+                  changeset->release_input_type())));
+      results.push_back(response_generation_->ApplyChangeset(
+          to, base::WrapUnique<LwwRegisterChangesetMessage>(
+                  changeset->release_response_generation())));
+      results.push_back(response_->ApplyChangeset(
+          to, base::WrapUnique<LwwRegisterChangesetMessage>(
+                  changeset->release_response())));
+      for (Result result : results) {
+        if (result != Result::SUCCESS) {
+          return result;
+        }
+      }
+      return Result::SUCCESS;
+    }
+
+    void ReleaseBefore(Revision checkpoint) override {
+      for (auto& member : members_) {
+        member->ReleaseBefore(checkpoint);
+      }
+    }
+
+    VersionVector GetVersionVector() const override {
+      VersionVector merged;
+      for (const auto& member : members_) {
+        merged = merged.MergeWith(member->GetVersionVector());
+      }
+      return merged;
+    }
+
+    void SetInputGeneration(int generation) override {
+      input_generation_->Set(generation);
+    }
+    int GetInputGeneration() const override { return input_generation_->Get(); }
+    void SetInputType(int type) override { input_type_->Set(type); }
+    int GetInputType() const override { return input_type_->Get(); }
+    void SetResponseGeneration(int generation) override {
+      response_generation_->Set(generation);
+    }
+    int GetResponseGeneration() const override {
+      return response_generation_->Get();
+    }
+    void SetResponse(const std::string& response) override {
+      response_->Set(response);
+    }
+    std::string GetResponse() const override { return response_->Get(); }
+
+   private:
+    std::unique_ptr<ProtoLwwRegister<int>> input_generation_;
+    std::unique_ptr<ProtoLwwRegister<int>> input_type_;
+    std::unique_ptr<ProtoLwwRegister<int>> response_generation_;
+    std::unique_ptr<ProtoLwwRegister<std::string>> response_;
+
+    std::vector<ProtoSyncable<LwwRegisterChangesetMessage>*> members_;
+
+    DISALLOW_COPY_AND_ASSIGN(InputMethodStateSyncable);
+  };
+
+  // Synchronizes changes from |source| to |dest|.
+  size_t SynchronizeOneWay(Revision last_sync,
+                           InputMethodStateSyncable* source,
+                           InputMethodStateSyncable* dest) {
+    if (source->GetVersionVector().local_revision() <= last_sync) {
+      return 0u;  // Changeset not computed; no bytes transferred.
+    }
+
+    std::unique_ptr<InputMethodChangesetMessage> changeset =
+        source->CreateChangesetToCurrent(last_sync_engine_);
+    size_t changeset_size = changeset->ByteSize();
+    CHECK_EQ(Result::SUCCESS,
+             dest->ApplyChangeset(last_sync_engine_, std::move(changeset)));
+    return changeset_size;
+  }
+
+  InputMethodStateSyncable client_syncable_;
+  InputMethodStateSyncable engine_syncable_;
+  Revision last_sync_client_ = 0u;
+  Revision last_sync_engine_ = 0u;
+  size_t bytes_sent_ = 0u;
+  size_t bytes_received_ = 0u;
+
+  DISALLOW_COPY_AND_ASSIGN(TemplatedSyncableConfig);
+};
+
 class InputMethodSampleTest
     : public testing::TestWithParam<SyncableConfig::SyncableType> {
  public:
@@ -165,7 +325,8 @@ class InputMethodSampleTest
     // test suite to use either templated proto or coded stream definitions.
     switch (GetParam()) {
       case SyncableConfig::TEMPLATED:
-      // TODO(steimel): Implement this.
+        config_ = base::MakeUnique<TemplatedSyncableConfig>();
+        break;
       case SyncableConfig::CODED_STREAM:
         config_ = base::MakeUnique<CodedStreamSyncableConfig>();
         break;
