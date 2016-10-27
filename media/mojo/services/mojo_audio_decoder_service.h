1447f56c9e7c6e90127116bca09901e48f42ff92
xhwang <xhwang@chromium.org>
2016-10-26 21:48:20 -0700

    media: Fix destruction order in MojoAudioDecoderService
    The CDM must outlive the decoder. Fix the declaration order to enforce
    this.
    
    BUG=659255
    
    Review-Url: https://codereview.chromium.org/2450403002
    Cr-Commit-Position: refs/heads/master@{#427944}
    
    Former-commit-id: abb0911aa8ea7d9c54b2643153f79fbfc077c414

--- media/mojo/services/mojo_audio_decoder_service.h
+++ media/mojo/services/mojo_audio_decoder_service.h
@@ -64,15 +64,18 @@ class MEDIA_MOJO_EXPORT MojoAudioDecoderService
   // A helper object required to get CDM from CDM id.
   base::WeakPtr<MojoCdmServiceContext> mojo_cdm_service_context_;
 
-  // The AudioDecoder that does actual decoding work.
-  std::unique_ptr<media::AudioDecoder> decoder_;
-
   // The destination for the decoded buffers.
   mojom::AudioDecoderClientAssociatedPtr client_;
 
   // Hold a reference to the CDM to keep it alive for the lifetime of the
   // |decoder_|. The |cdm_| owns the CdmContext which is passed to |decoder_|.
   scoped_refptr<MediaKeys> cdm_;
+
+  // The AudioDecoder that does actual decoding work.
+  // This MUST be declared after |cdm_| to maintain correct destruction order.
+  // The |decoder_| may need to access the CDM to do some clean up work in its
+  // own destructor.
+  std::unique_ptr<media::AudioDecoder> decoder_;
 
   base::WeakPtr<MojoAudioDecoderService> weak_this_;
   base::WeakPtrFactory<MojoAudioDecoderService> weak_factory_;
