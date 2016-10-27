834251156771d6dd9270cb09a1834d384fec5773
jww <jww@chromium.org>
2016-10-27 03:01:08 -0700

    Add suborigin test for document origin and simplify document.* tests
    This adds a LayoutTest for verifying the value of document.origin in a
    suborigin page. It also combines the other document.* tests into one
    test file to keep things simpler and cleaner.
    
    BUG=336894
    
    Review-Url: https://codereview.chromium.org/2455733002
    Cr-Commit-Position: refs/heads/master@{#427985}
    
    Former-commit-id: 0dfccfbdc3a54ae520ac87fe281f1cda0212a622

--- third_party/WebKit/LayoutTests/http/tests/security/suborigins/suborigin-document-properties.php
+++ third_party/WebKit/LayoutTests/http/tests/security/suborigins/suborigin-document-properties.php
@@ -5,13 +5,17 @@ header("Suborigin: foobar");
 <html>
 <head>
 <meta charset="utf-8">
-<title>Validate behavior of document.domain</title>
+<title>Validate behavior of document properties</title>
 <script src="/resources/testharness.js"></script>
 <script src="/resources/testharnessreport.js"></script>
 </head>
 <script>
 assert_equals(document.domain, '127.0.0.1',
     'document.domain should not be affected by the suborigin.');
+assert_equals(document.origin, 'http-so://foobar.127.0.0.1:8000',
+    'document.origin should reflect the suborigin serialized in the origin.');
+assert_equals(document.suborigin, 'foobar',
+    'document.suborigin should reflect the suborigin name.');
 done();
 </script>
 </html>
