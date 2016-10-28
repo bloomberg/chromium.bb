89d6df1ad0ddc4d593aae80c3d6301f97c72e142
estade <estade@chromium.org>
2016-10-28 11:42:45 -0700

    Use a standard message box for DesktopCastingWarningView.
    This is even simpler than what I did for TeleportWarningView, which
    means I should probably go back and re-rewrite that one.
    
    BUG=658410
    
    Review-Url: https://codereview.chromium.org/2441403003
    Cr-Commit-Position: refs/heads/master@{#428440}
    
    Former-commit-id: 5f1173f1e5a5678cc6c7cc9046939447398eaf19

--- chrome/browser/ui/ash/multi_user/user_switch_util_unittest.cc
+++ chrome/browser/ui/ash/multi_user/user_switch_util_unittest.cc
@@ -7,7 +7,10 @@
 #include "ash/common/wm/overview/window_selector_controller.h"
 #include "ash/common/wm_shell.h"
 #include "ash/test/ash_test_base.h"
+#include "base/run_loop.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/ui/ash/multi_user/user_switch_util.h"
+#include "chrome/browser/ui/simple_message_box.h"
 #include "ui/aura/window.h"
 
 namespace ash {
@@ -68,19 +71,11 @@ class TrySwitchingUserTest : public ash::test::AshTestBase {
   // The passed |action| type parameter defines the outcome (which will be
   // checked) and the action the user will choose.
   void SwitchUser(ActionType action) {
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE, base::Bind(&CloseMessageBox, action));
     TrySwitchingActiveUser(base::Bind(&TrySwitchingUserTest::SwitchCallback,
                                       base::Unretained(this)));
-    switch (action) {
-      case NO_DIALOG:
-        EXPECT_TRUE(!TestAndTerminateDesktopCastingWarningForTest(true));
-        return;
-      case ACCEPT_DIALOG:
-        EXPECT_TRUE(TestAndTerminateDesktopCastingWarningForTest(true));
-        return;
-      case DECLINE_DIALOG:
-        EXPECT_TRUE(TestAndTerminateDesktopCastingWarningForTest(false));
-        return;
-    }
+    base::RunLoop().RunUntilIdle();
   }
 
   // Called when the user will get actually switched.
@@ -110,6 +105,20 @@ class TrySwitchingUserTest : public ash::test::AshTestBase {
   int switch_callback_hit_count() const { return switch_callback_hit_count_; }
 
  private:
+  static void CloseMessageBox(ActionType action) {
+    switch (action) {
+      case NO_DIALOG:
+        EXPECT_FALSE(chrome::CloseMessageBoxForTest(true));
+        return;
+      case ACCEPT_DIALOG:
+        EXPECT_TRUE(chrome::CloseMessageBoxForTest(true));
+        return;
+      case DECLINE_DIALOG:
+        EXPECT_TRUE(chrome::CloseMessageBoxForTest(false));
+        return;
+    }
+  }
+
   // The two items from the SystemTray for the screen capture / share
   // functionality.
   ScreenTrayItem* capture_item_;
