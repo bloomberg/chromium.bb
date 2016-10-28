9b015257a264d0d51194db0bf6a173417360892a
robliao <robliao@chromium.org>
2016-10-28 10:12:16 -0700

    Add Browser Task Scheduler Flags to iOS
    This harmonizes flag support for the task scheduler between desktop,
    Android, and iOS.
    
    BUG=553459,633389
    
    Review-Url: https://codereview.chromium.org/2439383003
    Cr-Commit-Position: refs/heads/master@{#428401}
    
    Former-commit-id: 59eed1a47e068cd347c48177648e5bb4f2f66d47

--- ios/chrome/browser/about_flags.mm
+++ ios/chrome/browser/about_flags.mm
@@ -19,6 +19,7 @@
 #include "base/strings/stringprintf.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/sys_info.h"
+#include "base/task_scheduler/switches.h"
 #include "components/dom_distiller/core/dom_distiller_switches.h"
 #include "components/flags_ui/feature_entry.h"
 #include "components/flags_ui/feature_entry_macros.h"
@@ -58,6 +59,10 @@ const flags_ui::FeatureEntry kFeatureEntries[] = {
      IDS_IOS_FLAGS_PHYSICAL_WEB_DESCRIPTION, flags_ui::kOsIos,
      ENABLE_DISABLE_VALUE_TYPE(switches::kEnableIOSPhysicalWeb,
                                switches::kDisableIOSPhysicalWeb)},
+    {"browser-task-scheduler", IDS_IOS_FLAGS_BROWSER_TASK_SCHEDULER_NAME,
+     IDS_IOS_FLAGS_BROWSER_TASK_SCHEDULER_DESCRIPTION, flags_ui::kOsIos,
+     ENABLE_DISABLE_VALUE_TYPE(switches::kEnableBrowserTaskScheduler,
+                               switches::kDisableBrowserTaskScheduler)},
 };
 
 // Add all switches from experimental flags to |command_line|.
