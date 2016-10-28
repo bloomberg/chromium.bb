444f659649ec11bc194ab97775ae9379c47074e3
wychen <wychen@chromium.org>
2016-10-28 13:43:36 -0700

    Reland of Restrict flaky CustomTabActivityTest#testContextMenuEntriesForLink/Image
    This is a reland of https://codereview.chromium.org/2451883002/
    
    TBR=yusufo@chromium.org
    BUG=655970
    
    Review-Url: https://codereview.chromium.org/2454503006
    Cr-Commit-Position: refs/heads/master@{#428482}
    
    Former-commit-id: 1107bd576a9902d29d010cfb1c8edf512fb846a5

--- chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/customtabs/CustomTabActivityTest.java
@@ -45,7 +45,6 @@ import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.base.library_loader.LibraryProcessType;
 import org.chromium.base.test.util.CallbackHelper;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
@@ -240,13 +239,11 @@ public class CustomTabActivityTest extends CustomTabActivityTestBase {
 
     /**
      * Test the entries in the context menu shown when long clicking an image.
+     * Restricted to phone due to BUG=crbug.com/655970
      */
-    /*
-     * @SmallTest
-     * @RetryOnFailure
-     * BUG=crbug.com/655970
-     */
-    @DisabledTest
+    @SmallTest
+    @RetryOnFailure
+    @Restriction(ChromeRestriction.RESTRICTION_TYPE_PHONE)
     public void testContextMenuEntriesForImage() throws InterruptedException, TimeoutException {
         startCustomTabActivityWithIntent(createMinimalCustomTabIntent());
 
@@ -277,9 +274,11 @@ public class CustomTabActivityTest extends CustomTabActivityTestBase {
 
     /**
      * Test the entries in the context menu shown when long clicking an link.
+     * Restricted to phone due to BUG=crbug.com/655970
      */
     @SmallTest
     @RetryOnFailure
+    @Restriction(ChromeRestriction.RESTRICTION_TYPE_PHONE)
     public void testContextMenuEntriesForLink() throws InterruptedException, TimeoutException {
         startCustomTabActivityWithIntent(createMinimalCustomTabIntent());
 
