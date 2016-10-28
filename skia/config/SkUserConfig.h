cae72cc1c57a27afd46b9b7889301be1521e58ba
reed <reed@google.com>
2016-10-28 07:04:56 -0700

    add SK_SUPPORT_LEGACY_XFERMODE_PARAM flag for future skia CL
    BUG=
    TBR=fmalita
    NOTRY=True
    
    Review-Url: https://codereview.chromium.org/2446273007
    Cr-Commit-Position: refs/heads/master@{#428360}
    
    Former-commit-id: 323171b3f7ffd27ab489b5d099709bd9b0c29f6e

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -201,6 +201,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_GETDEVICE
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_XFERMODE_PARAM
+#   define SK_SUPPORT_LEGACY_XFERMODE_PARAM
+#endif
+
 #ifndef SK_SUPPORT_LEGACY_CLIP_REGIONOPS
 #define SK_SUPPORT_LEGACY_CLIP_REGIONOPS
 #endif
