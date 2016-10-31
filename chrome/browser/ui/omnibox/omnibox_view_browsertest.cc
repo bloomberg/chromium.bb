bloomberg/chromium.bb

commit 8bd9450171bbaf9c1ec3cd846fedb2f6f3848f69

Fixed a clipboard bug in views/omnibox. The content was always copied as pure text, never as hyperlink.
BUG=168583

Review URL: https://chromiumcodereview.appspot.com/11789003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176656 0039d316-1c4b-4281-b951-d872f2087c98

Former-commit-id: 8dc16851987d0b2092b2dfaa6188108f1b675888

chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
@@ -38,6 +38,7 @@
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/web_contents.h"
 #include "net/base/mock_host_resolver.h"
+#include "ui/base/clipboard/clipboard.h"
 #include "ui/base/events/event_constants.h"
 #include "ui/base/keycodes/keyboard_codes.h"
 #include "ui/gfx/point.h"
@@ -1686,3 +1687,41 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, DISABLED_SelectAllOnClick) {
   EXPECT_FALSE(omnibox_view->IsSelectAll());
 }
 #endif  // defined(USE_AURA)
+
+IN_PROC_BROWSER_TEST_F(OmniboxViewTest, CopyURLToClipboard) {
+  OmniboxView* omnibox_view = NULL;
+  ASSERT_NO_FATAL_FAILURE(GetOmniboxView(&omnibox_view));
+  const char* target_url = "http://www.google.com/calendar";
+  omnibox_view->SetUserText(ASCIIToUTF16(target_url));
+
+  // Set permanent text thus making sure that omnibox treats 'google.com'
+  // as URL (not as ordinary user input).
+  OmniboxEditModel* edit_model = omnibox_view->model();
+  ASSERT_NE(edit_model, static_cast<OmniboxEditModel*>(NULL));
+  edit_model->UpdatePermanentText(ASCIIToUTF16("http://www.google.com/"));
+
+  // Select full URL and copy it to clipboard. General text and html should
+  // be available.
+  omnibox_view->SelectAll(true);
+  EXPECT_TRUE(omnibox_view->IsSelectAll());
+  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+  clipboard->Clear(ui::Clipboard::BUFFER_STANDARD);
+  ASSERT_NO_FATAL_FAILURE(SendKey(ui::VKEY_C, kCtrlOrCmdMask));
+  EXPECT_TRUE(clipboard->IsFormatAvailable(
+      ui::Clipboard::GetPlainTextFormatType(), ui::Clipboard::BUFFER_STANDARD));
+
+  // MAC is the only platform which doesn't write html.
+#if !defined(OS_MACOSX)
+  EXPECT_TRUE(clipboard->IsFormatAvailable(
+      ui::Clipboard::GetHtmlFormatType(), ui::Clipboard::BUFFER_STANDARD));
+#endif
+
+  // These platforms should read bookmark format.
+#if defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_MACOSX)
+  string16 title;
+  std::string url;
+  clipboard->ReadBookmark(&title, &url);
+  EXPECT_EQ(target_url, url);
+  EXPECT_EQ(ASCIIToUTF16(target_url), title);
+#endif
+}

chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -115,21 +115,18 @@ int GetEditFontPixelSize(bool popup_window_mode) {
                              kAutocompleteEditFontPixelSize;
 }
 
-// Copies |selected_text| as text to the primary clipboard. If |write_url| is
-// true, this will also write |url| and |text| to the clipboard as a well-formed
-// URL.
-void DoCopy(const string16& selected_text,
-            bool write_url,
-            const GURL& url,
-            const string16& text) {
-  ui::Clipboard* cb = ui::Clipboard::GetForCurrentThread();
-  ui::ScopedClipboardWriter scw(cb, ui::Clipboard::BUFFER_STANDARD);
+// Copies |selected_text| as text to the primary clipboard.
+void DoCopyText(const string16& selected_text) {
+  ui::ScopedClipboardWriter scw(ui::Clipboard::GetForCurrentThread(),
+                                ui::Clipboard::BUFFER_STANDARD);
   scw.WriteText(selected_text);
-  if (write_url) {
-    BookmarkNodeData data;
-    data.ReadFromTuple(url, text);
-    data.WriteToClipboard(NULL);
-  }
+}
+
+// This will write |url| and |text| to the clipboard as a well-formed URL.
+void DoCopyURL(const GURL& url, const string16& text) {
+  BookmarkNodeData data;
+  data.ReadFromTuple(url, text);
+  data.WriteToClipboard(NULL);
 }
 
 }  // namespace
@@ -770,7 +767,10 @@ void OmniboxViewViews::OnAfterCutOrCopy() {
   bool write_url;
   model()->AdjustTextForCopy(selection_range.GetMin(), selected_text == text,
       &selected_text, &url, &write_url);
-  DoCopy(selected_text, write_url, url, text);
+  if (write_url)
+    DoCopyURL(url, selected_text);
+  else
+    DoCopyText(selected_text);
 }
 
 void OmniboxViewViews::OnWriteDragData(ui::OSExchangeData* data) {
@@ -952,8 +952,7 @@ string16 OmniboxViewViews::GetSelectedText() const {
 }
 
 void OmniboxViewViews::CopyURL() {
-  const string16& text = toolbar_model()->GetText(false);
-  DoCopy(text, true, toolbar_model()->GetURL(), text);
+  DoCopyURL(toolbar_model()->GetURL(), toolbar_model()->GetText(false));
 }
 
 void OmniboxViewViews::OnPaste() {

chrome/browser/ui/views/omnibox/omnibox_view_win.cc
@@ -28,6 +28,7 @@
 #include "chrome/browser/autocomplete/autocomplete_input.h"
 #include "chrome/browser/autocomplete/autocomplete_match.h"
 #include "chrome/browser/autocomplete/keyword_provider.h"
+#include "chrome/browser/bookmarks/bookmark_node_data.h"
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/net/url_fixer_upper.h"
 #include "chrome/browser/profiles/profile.h"
@@ -127,13 +128,18 @@ bool IsDrag(const POINT& origin, const POINT& current) {
       gfx::Point(current) - gfx::Point(origin));
 }
 
-// Write |text| and an optional |url| to the clipboard.
-void DoCopy(const string16& text, const GURL* url) {
+// Copies |selected_text| as text to the primary clipboard.
+void DoCopyText(const string16& selected_text) {
   ui::ScopedClipboardWriter scw(ui::Clipboard::GetForCurrentThread(),
                                 ui::Clipboard::BUFFER_STANDARD);
-  scw.WriteText(text);
-  if (url != NULL)
-    scw.WriteBookmark(text, url->spec());
+  scw.WriteText(selected_text);
+}
+
+// Writes |url| and |text| to the clipboard as a well-formed URL.
+void DoCopyURL(const GURL& url, const string16& text) {
+  BookmarkNodeData data;
+  data.ReadFromTuple(url, text);
+  data.WriteToClipboard(NULL);
 }
 
 }  // namespace
@@ -1093,7 +1099,7 @@ int OmniboxViewWin::OnPerformDropImpl(const ui::DropTargetEvent& event,
 }
 
 void OmniboxViewWin::CopyURL() {
-  DoCopy(toolbar_model()->GetText(false), &toolbar_model()->GetURL());
+  DoCopyURL(toolbar_model()->GetURL(), toolbar_model()->GetText(false));
 }
 
 bool OmniboxViewWin::SkipDefaultKeyEventProcessing(const ui::KeyEvent& event) {
@@ -1428,7 +1434,10 @@ void OmniboxViewWin::OnCopy() {
   // GetSel() doesn't preserve selection direction, so sel.cpMin will always be
   // the smaller value.
   model()->AdjustTextForCopy(sel.cpMin, IsSelectAll(), &text, &url, &write_url);
-  DoCopy(text, write_url ? &url : NULL);
+  if (write_url)
+    DoCopyURL(url, text);
+  else
+    DoCopyText(text);
 }
 
 LRESULT OmniboxViewWin::OnCreate(const CREATESTRUCTW* /*create_struct*/) {

commit f44fff2c4970de6fbf015ee308a2cf938f367131

Cut/Copy links from the omnibox as URLs instead of bookmarks
We currently copy URLs from the omnibox as bookmarks.  This is
incorrect because bookmarks have both a URL and a title, but we just
want to copy a URL.

This bug was introduced in 8bd9450171 [formerly 8dc1685198] where the intent appears to be
just to copy as a URL.

BUG=442253
R=pkasting@chromium.org

Review-Url: https://codereview.chromium.org/2257293002
Cr-Commit-Position: refs/heads/master@{#428760}

Former-commit-id: ec29ae19b4a7e4f5ff3123ebd0b4155a81799faf

chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
@@ -1746,14 +1746,15 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, CopyURLToClipboard) {
   EXPECT_FALSE(clipboard->IsFormatAvailable(
       ui::Clipboard::GetHtmlFormatType(), ui::CLIPBOARD_TYPE_COPY_PASTE));
 
-  // These platforms should read bookmark format.
-#if defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_MACOSX)
-  base::string16 title;
-  std::string url;
-  clipboard->ReadBookmark(&title, &url);
-  EXPECT_EQ(target_url, url);
-  EXPECT_EQ(ASCIIToUTF16(target_url), title);
+// Windows clipboard only supports text URLs.
+#if defined(OS_LINUX) || defined(OS_MACOSX)
+  EXPECT_TRUE(clipboard->IsFormatAvailable(ui::Clipboard::GetUrlFormatType(),
+                                           ui::CLIPBOARD_TYPE_COPY_PASTE));
 #endif
+
+  std::string url;
+  clipboard->ReadAsciiText(ui::CLIPBOARD_TYPE_COPY_PASTE, &url);
+  EXPECT_EQ(target_url, url);
 }
 
 IN_PROC_BROWSER_TEST_F(OmniboxViewTest, CutURLToClipboard) {
@@ -1792,14 +1793,15 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, CutURLToClipboard) {
   EXPECT_FALSE(clipboard->IsFormatAvailable(
       ui::Clipboard::GetHtmlFormatType(), ui::CLIPBOARD_TYPE_COPY_PASTE));
 
-  // These platforms should read bookmark format.
-#if defined(OS_WIN) || defined(OS_CHROMEOS) || defined(OS_MACOSX)
-  base::string16 title;
-  std::string url;
-  clipboard->ReadBookmark(&title, &url);
-  EXPECT_EQ(target_url, url);
-  EXPECT_EQ(ASCIIToUTF16(target_url), title);
+// Windows clipboard only supports text URLs.
+#if defined(OS_LINUX) || defined(OS_MACOSX)
+  EXPECT_TRUE(clipboard->IsFormatAvailable(ui::Clipboard::GetUrlFormatType(),
+                                           ui::CLIPBOARD_TYPE_COPY_PASTE));
 #endif
+
+  std::string url;
+  clipboard->ReadAsciiText(ui::CLIPBOARD_TYPE_COPY_PASTE, &url);
+  EXPECT_EQ(target_url, url);
 }
 
 IN_PROC_BROWSER_TEST_F(OmniboxViewTest, CopyTextToClipboard) {

chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -22,7 +22,6 @@
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.h"
 #include "chrome/grit/generated_resources.h"
-#include "components/bookmarks/browser/bookmark_node_data.h"
 #include "components/omnibox/browser/autocomplete_input.h"
 #include "components/omnibox/browser/autocomplete_match.h"
 #include "components/omnibox/browser/omnibox_edit_controller.h"
@@ -60,8 +59,6 @@
 #if defined(OS_WIN)
 #include "chrome/browser/browser_process.h"
 #endif
-
-using bookmarks::BookmarkNodeData;
 
 namespace {
 
@@ -1009,14 +1006,11 @@ void OmniboxViewViews::OnAfterCutOrCopy(ui::ClipboardType clipboard_type) {
   if (IsSelectAll())
     UMA_HISTOGRAM_COUNTS(OmniboxEditModel::kCutOrCopyAllTextHistogram, 1);
 
-  if (write_url) {
-    BookmarkNodeData data;
-    data.ReadFromTuple(url, selected_text);
-    data.WriteToClipboard(clipboard_type);
-  } else {
-    ui::ScopedClipboardWriter scoped_clipboard_writer(clipboard_type);
+  ui::ScopedClipboardWriter scoped_clipboard_writer(clipboard_type);
+  if (write_url)
+    scoped_clipboard_writer.WriteURL(selected_text);
+  else
     scoped_clipboard_writer.WriteText(selected_text);
-  }
 }
 
 void OmniboxViewViews::OnWriteDragData(ui::OSExchangeData* data) {