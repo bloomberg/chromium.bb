3f56f3f5875858244848ae8b9daca37da1d3a696
fukino <fukino@chromium.org>
2016-10-31 23:17:15 -0700

    Suppress mount notfications from a dock.
    The HP Elite USB-C Doc can cause repeated mount notifications about its internal storage.
    This CL suppresses the notifications from the device as a workaround.
    
    BUG=chrome-os-partner:58309
    TEST=manually tested with the dock device in question.
    
    Review-Url: https://codereview.chromium.org/2459623002
    Cr-Commit-Position: refs/heads/master@{#428945}
    
    Former-commit-id: 8412ab1cc9cb389e253f9dece859643b407e59b5

--- chrome/browser/chromeos/extensions/file_manager/event_router.cc
+++ chrome/browser/chromeos/extensions/file_manager/event_router.cc
@@ -288,6 +288,17 @@ bool ShouldShowNotificationForVolume(
     return false;
   }
 
+  // We suppress notifications about HP Elite USB-C Dock's internal storage.
+  // chrome-os-partner:58309.
+  // TODO(fukino): Remove this workaround when the root cause is fixed.
+  if (volume.type() == VOLUME_TYPE_REMOVABLE_DISK_PARTITION) {
+    const DiskMountManager::Disk* disk =
+        DiskMountManager::GetInstance()->FindDiskBySourcePath(
+            volume.source_path().AsUTF8Unsafe());
+    if (disk && disk->vendor_id() == "0ea0" && disk->product_id() == "2272")
+      return false;
+  }
+
   return true;
 }
 
