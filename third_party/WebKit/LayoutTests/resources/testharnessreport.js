71c29dc037eb1e97523f1b1d9cb8e82b10011262
tkent <tkent@chromium.org>
2016-10-31 23:54:17 -0700

    testharnessreport: Merge PASS lines for html/dom/reflection-*.html
    These tests are too large, and the sizes of some *-expected.txt were 1MB+.
    We can't upload a 1MB+ diff to codereview.chromium.org.
    
    So, this CL adds a special handling to testharnessreport.js for these tests. If
    "PASS element.attribute: " lines are repeated, we merge them into
    
        PASS element.attribute: N tests
    
    Review-Url: https://codereview.chromium.org/2466233002
    Cr-Commit-Position: refs/heads/master@{#428950}
    
    Former-commit-id: 8f18f240c870e0c64127085b4f5b86e238b3ca37

--- third_party/WebKit/LayoutTests/resources/testharnessreport.js
+++ third_party/WebKit/LayoutTests/resources/testharnessreport.js
@@ -130,12 +130,39 @@
                 harness_status.message +
                 "\n";
         }
-        // Iterate through tests array and build string that contains
-        // results for all tests.
-        for (var i = 0; i < tests.length; ++i) {
-            resultStr += convertResult(tests[i].status) + " " +
-                sanitize(tests[i].name) + " " +
-                sanitize(tests[i].message) + "\n";
+        // reflection tests contain huge number of tests, and Chromium code
+        // review tool has the 1MB diff size limit. We merge PASS lines.
+        if (document.URL.indexOf("/html/dom/reflection") >= 0) {
+            for (var i = 0; i < tests.length; ++i) {
+                if (tests[i].status == 0) {
+                    var colon = tests[i].name.indexOf(':');
+                    if (colon > 0) {
+                        var prefix = tests[i].name.substring(0, colon + 1);
+                        var j = i + 1;
+                        for (; j < tests.length; ++j) {
+                            if (!tests[j].name.startsWith(prefix) || tests[j].status != 0)
+                                break;
+                        }
+                        if ((j - i) > 1) {
+                            resultStr += convertResult(tests[i].status) +
+                                " " + sanitize(prefix) + " " + (j - i) + " tests\n"
+                            i = j - 1;
+                            continue;
+                        }
+                    }
+                }
+                resultStr += convertResult(tests[i].status) + " " +
+                    sanitize(tests[i].name) + " " +
+                    sanitize(tests[i].message) + "\n";
+            }
+        } else {
+            // Iterate through tests array and build string that contains
+            // results for all tests.
+            for (var i = 0; i < tests.length; ++i) {
+                resultStr += convertResult(tests[i].status) + " " +
+                    sanitize(tests[i].name) + " " +
+                    sanitize(tests[i].message) + "\n";
+            }
         }
 
         resultStr += "Harness: the test ran to completion.\n";
