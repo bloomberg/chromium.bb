4fbe4bdb3af32d7d4c3356e5412be92090780a69
zakerinasab <zakerinasab@chromium.org>
2016-11-01 11:59:53 -0700

    Removing self.Fails of browser pixel tests from pixel_expectations.py.
    As all the results on cloud storage bucket are as expected, we can
    remove the self.Fails lines from pixel_expectations.py.
    
    BUG=657946
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2459373002
    Cr-Commit-Position: refs/heads/master@{#429065}
    
    Former-commit-id: fefddce5c498187b4d9a2f721d489ab82c7154c5

--- content/test/gpu/gpu_tests/pixel_expectations.py
+++ content/test/gpu/gpu_tests/pixel_expectations.py
@@ -45,8 +45,3 @@ class PixelExpectations(GpuTestExpectations):
 
     # TODO(kbr): flakily timing out on this configuration.
     self.Flaky('*', ['linux', 'intel', 'debug'], bug=648369)
-
-    # TODO(zakerinasab): check / generate reference images.
-    self.Fail('Pixel_CanvasDisplayLinearRGBAccelerated2D', bug=657946)
-    self.Fail('Pixel_CanvasDisplayLinearRGBUnaccelerated2D', bug=657946)
-    self.Fail('Pixel_CanvasDisplayLinearRGBUnaccelerated2DGPUCompositing', bug=657946)
