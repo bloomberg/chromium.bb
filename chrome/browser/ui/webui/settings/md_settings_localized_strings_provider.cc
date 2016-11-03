56425ed5eca476dcb6e0f359abc811b80c932ffa
tommycli <tommycli@chromium.org>
2016-11-03 13:11:28 -0700

    MD Settings: Add Import Bookmarks Dialog Strings
    Straight copy of the strings from Options to Settings. The IDS
    constants have been renamed.
    
    BUG=659259
    
    Review-Url: https://codereview.chromium.org/2480473002
    Cr-Commit-Position: refs/heads/master@{#429681}
    
    Former-commit-id: 1287354883d7254210f53a13ab7fde856cad3661

--- chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
+++ chrome/browser/ui/webui/settings/md_settings_localized_strings_provider.cc
@@ -732,6 +732,26 @@ void AddEasyUnlockStrings(content::WebUIDataSource* html_source) {
                          chrome::kEasyUnlockLearnMoreUrl);
 }
 
+void AddImportDataStrings(content::WebUIDataSource* html_source) {
+  LocalizedString localized_strings[] = {
+    {"importFromLabel", IDS_SETTINGS_IMPORT_FROM_LABEL},
+    {"importDescription", IDS_SETTINGS_IMPORT_ITEMS_LABEL},
+    {"importLoading", IDS_SETTINGS_IMPORT_LOADING_PROFILES},
+    {"importHistory", IDS_SETTINGS_IMPORT_HISTORY_CHECKBOX},
+    {"importFavorites", IDS_SETTINGS_IMPORT_FAVORITES_CHECKBOX},
+    {"importPasswords", IDS_SETTINGS_IMPORT_PASSWORDS_CHECKBOX},
+    {"importSearch", IDS_SETTINGS_IMPORT_SEARCH_ENGINES_CHECKBOX},
+    {"importAutofillFormData", IDS_SETTINGS_IMPORT_AUTOFILL_FORM_DATA_CHECKBOX},
+    {"importSucceeded", IDS_SETTINGS_IMPORT_SUCCEEDED},
+    {"importChooseFile", IDS_SETTINGS_IMPORT_CHOOSE_FILE},
+    {"importCommit", IDS_SETTINGS_IMPORT_COMMIT},
+    {"noProfileFound", IDS_SETTINGS_IMPORT_NO_PROFILE_FOUND},
+    {"findYourImportedBookmarks", IDS_SETTINGS_IMPORT_FIND_YOUR_BOOKMARKS},
+  };
+  AddLocalizedStringsBulk(html_source, localized_strings,
+                          arraysize(localized_strings));
+}
+
 void AddInternetStrings(content::WebUIDataSource* html_source) {
   LocalizedString localized_strings[] = {
       {"internetAddConnection", IDS_SETTINGS_INTERNET_ADD_CONNECTION},
@@ -1745,6 +1765,7 @@ void AddLocalizedStrings(content::WebUIDataSource* html_source,
 
 #if defined(OS_CHROMEOS)
   AddEasyUnlockStrings(html_source);
+  AddImportDataStrings(html_source);
   AddInternetStrings(html_source);
   AddCrNetworkStrings(html_source);
 #endif
