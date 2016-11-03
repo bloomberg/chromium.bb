248594e7989240185efb1ef71e6621a6eefeee4e
primiano <primiano@chromium.org>
2016-11-03 08:22:03 -0700

    tracing: Bail out early in tracing tests when tracing is already enabled
    A bunch of tracing browsertests have recently become flaky.
    I strongly suspect this is because some other test leaves the
    startup tracing file (/data/local/chrome-trace-config.json)
    around. On Android the presence of that file is enough to
    initialize the tracing machinery and interfere with tests.
    Bail out early explicitly in that case.
    
    BUG=657628, 656729
    
    Review-Url: https://codereview.chromium.org/2479563002
    Cr-Commit-Position: refs/heads/master@{#429593}
    
    Former-commit-id: ea26a4f05c216cd7dd7d76ea19fc46665c300baa

--- content/browser/tracing/memory_tracing_browsertest.cc
+++ content/browser/tracing/memory_tracing_browsertest.cc
@@ -13,6 +13,7 @@
 #include "base/trace_event/memory_dump_provider.h"
 #include "base/trace_event/memory_dump_request_args.h"
 #include "base/trace_event/trace_config_memory_test_util.h"
+#include "base/trace_event/trace_log.h"
 #include "content/public/browser/tracing_controller.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test_utils.h"
@@ -109,6 +110,9 @@ class MemoryTracingTest : public ContentBrowserTest {
   }
 
   void EnableMemoryTracing() {
+    CHECK(!base::trace_event::TraceLog::GetInstance()->IsEnabled())
+        << "Tracing seems to be already enabled. Very likely this is because "
+           "the startup tracing file has been leaked from a previous test.";
     // Enable tracing without periodic dumps.
     base::trace_event::TraceConfig trace_config(
         base::trace_event::TraceConfigMemoryTestUtil::
