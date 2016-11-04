01bb2fa947e6b2c98f775d052d9926ccee3c6a81
glebl <glebl@chromium.org>
2016-11-04 06:43:19 -0700

    Add WTF::MakeUnique to mirror C++14's std::make_unique.
    This implementation is identical to base/memory/ptr_util.h.
    
    BUG=662304
    
    Review-Url: https://codereview.chromium.org/2455313003
    Cr-Commit-Position: refs/heads/master@{#429878}
    
    Former-commit-id: dc91022fc289e4bdfe0ca9a857775b91762d0b78

--- third_party/WebKit/Source/wtf/PtrUtil.h
+++ third_party/WebKit/Source/wtf/PtrUtil.h
@@ -5,6 +5,7 @@
 #ifndef PtrUtil_h
 #define PtrUtil_h
 
+#include "base/memory/ptr_util.h"
 #include "wtf/TypeTraits.h"
 
 #include <memory>
@@ -27,8 +28,27 @@ std::unique_ptr<T[]> wrapArrayUnique(T* ptr) {
   return std::unique_ptr<T[]>(ptr);
 }
 
+// WTF::makeUnique is base::MakeUnique. See base/ptr_util.h for documentation.
+template <typename T, typename... Args>
+auto makeUnique(Args&&... args)
+    -> decltype(base::MakeUnique<T>(std::forward<Args>(args)...)) {
+  static_assert(
+      !WTF::IsGarbageCollectedType<T>::value,
+      "Garbage collected types should not be stored in std::unique_ptr!");
+  return base::MakeUnique<T>(std::forward<Args>(args)...);
+}
+
+template <typename T>
+auto makeUnique(size_t size) -> decltype(base::MakeUnique<T>(size)) {
+  static_assert(
+      !WTF::IsGarbageCollectedType<T>::value,
+      "Garbage collected types should not be stored in std::unique_ptr!");
+  return base::MakeUnique<T>(size);
+}
+
 }  // namespace WTF
 
+using WTF::makeUnique;
 using WTF::wrapUnique;
 using WTF::wrapArrayUnique;
 
