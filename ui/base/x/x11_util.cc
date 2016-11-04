dc0d6c249de66633fbc53d578afafb702ada8790
thomasanderson <thomasanderson@google.com>
2016-11-04 15:55:14 -0700

    X11: Use CopyFromParent colormap when possible (Reland)
    This CL is a small performance optimization.  Previously,
    XVisualManager always allocated a new colormap for every used visual.
    However, on non-compositing WMs, on some drivers, this could result in
    an expensive copy-on-present for every frame, but this should not
    occur when using the root window's colormap.
    
    BUG=661697
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2471073002
    Cr-Commit-Position: refs/heads/master@{#430050}
    
    Former-commit-id: 66edd15df4d85fa3ee13e8dd911bee8ffcfbe885

--- ui/base/x/x11_util.cc
+++ ui/base/x/x11_util.cc
@@ -1422,6 +1422,7 @@ XVisualManager* XVisualManager::GetInstance() {
 
 XVisualManager::XVisualManager()
     : display_(gfx::GetXDisplay()),
+      default_visual_id_(0),
       system_visual_id_(0),
       transparent_visual_id_(0),
       using_software_rendering_(false),
@@ -1438,11 +1439,9 @@ XVisualManager::XVisualManager()
   using_compositing_wm_ = XGetSelectionOwner(display_, NET_WM_CM_S0) != None;
 
   // Choose the opaque visual.
-  XWindowAttributes attribs;
-  Window root = XDefaultRootWindow(display_);
-  Status status = XGetWindowAttributes(display_, root, &attribs);
-  DCHECK_NE(0, status);
-  system_visual_id_ = attribs.visual->visualid;
+  default_visual_id_ =
+      XVisualIDFromVisual(DefaultVisual(display_, DefaultScreen(display_)));
+  system_visual_id_ = default_visual_id_;
   DCHECK(system_visual_id_);
   DCHECK(visuals_.find(system_visual_id_) != visuals_.end());
 
@@ -1472,15 +1471,20 @@ void XVisualManager::ChooseVisualForWindow(bool want_argb_visual,
                                            bool* using_argb_visual) {
   bool use_argb = want_argb_visual && using_compositing_wm_ &&
                   (using_software_rendering_ || have_gpu_argb_visual_);
-  XVisualData& visual_data =
-      *visuals_[use_argb && transparent_visual_id_ ? transparent_visual_id_
-                                                   : system_visual_id_];
+  VisualID visual_id = use_argb && transparent_visual_id_
+                           ? transparent_visual_id_
+                           : system_visual_id_;
+  XVisualData& visual_data = *visuals_[visual_id];
+  const XVisualInfo& visual_info = visual_data.visual_info;
+
+  bool is_default_visual = visual_id == default_visual_id_;
+
   if (visual)
-    *visual = visual_data.visual_info.visual;
+    *visual = visual_info.visual;
   if (depth)
-    *depth = visual_data.visual_info.depth;
+    *depth = visual_info.depth;
   if (colormap)
-    *colormap = visual_data.GetColormap();
+    *colormap = is_default_visual ? CopyFromParent : visual_data.GetColormap();
   if (using_argb_visual)
     *using_argb_visual = use_argb;
 }
--- ui/base/x/x11_util_internal.h
+++ ui/base/x/x11_util_internal.h
@@ -63,6 +63,8 @@ class UI_BASE_X_EXPORT XVisualManager {
  public:
   static XVisualManager* GetInstance();
 
+  // Picks the best argb or opaque visual given |want_argb_visual|.  If the
+  // default visual is returned, |colormap| is set to CopyFromParent.
   void ChooseVisualForWindow(bool want_argb_visual,
                              Visual** visual,
                              int* depth,
@@ -101,6 +103,10 @@ class UI_BASE_X_EXPORT XVisualManager {
 
   XDisplay* display_;
 
+  VisualID default_visual_id_;
+
+  // The system visual is usually the same as the default visual, but
+  // may not be in general.
   VisualID system_visual_id_;
   VisualID transparent_visual_id_;
 
