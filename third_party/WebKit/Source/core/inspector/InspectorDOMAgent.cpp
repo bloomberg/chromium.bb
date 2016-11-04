ee5085bb4aa278833d151bd6d94506e0690d9a20
aboxhall <aboxhall@chromium.org>
2016-11-04 11:07:57 -0700

    [DevTools] Add backendNodeId to DOM Node protocol object and SDK object
    BUG=560525
    
    Review-Url: https://codereview.chromium.org/2471623006
    Cr-Commit-Position: refs/heads/master@{#429950}
    
    Former-commit-id: 137c2aee31639f9e72375b1e2ae68aabfe69ec1b

--- third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -1569,6 +1569,7 @@ std::unique_ptr<protocol::DOM::Node> InspectorDOMAgent::buildObjectForNode(
   std::unique_ptr<protocol::DOM::Node> value =
       protocol::DOM::Node::create()
           .setNodeId(id)
+          .setBackendNodeId(DOMNodeIds::idForNode(node))
           .setNodeType(static_cast<int>(node->getNodeType()))
           .setNodeName(node->nodeName())
           .setLocalName(localName)
