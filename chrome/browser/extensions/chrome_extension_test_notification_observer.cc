4363381a82af0a2a79a02abb71878f33f38f443b
tmoniuszko <tmoniuszko@opera.com>
2016-11-04 04:00:26 -0700

    Add missing web_contents.h include
    BUG=
    
    Review-Url: https://codereview.chromium.org/2473793002
    Cr-Commit-Position: refs/heads/master@{#429853}
    
    Former-commit-id: 4dc6d5c376e605b96a4513f5b1771dfdfbd63d1d

--- chrome/browser/extensions/chrome_extension_test_notification_observer.cc
+++ chrome/browser/extensions/chrome_extension_test_notification_observer.cc
@@ -10,6 +10,7 @@
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "content/public/browser/browser_context.h"
+#include "content/public/browser/web_contents.h"
 #include "extensions/browser/notification_types.h"
 #include "extensions/browser/process_manager.h"
 #include "extensions/common/extension.h"
