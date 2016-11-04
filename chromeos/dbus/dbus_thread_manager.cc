1ddeb7b5062d6d91819df7f609a3dc246ea0c406
thestig <thestig@chromium.org>
2016-11-04 11:37:06 -0700

    Prevent double DBusThreadManager initialization.
    BUG=660866
    
    Review-Url: https://codereview.chromium.org/2472203003
    Cr-Commit-Position: refs/heads/master@{#429958}
    
    Former-commit-id: 6d0239bd155271fd85434865a4aa52f52e5c938c

--- chromeos/dbus/dbus_thread_manager.cc
+++ chromeos/dbus/dbus_thread_manager.cc
@@ -42,7 +42,7 @@
 
 namespace chromeos {
 
-static DBusThreadManager* g_dbus_thread_manager = NULL;
+static DBusThreadManager* g_dbus_thread_manager = nullptr;
 static bool g_using_dbus_thread_manager_for_testing = false;
 
 DBusThreadManager::DBusThreadManager(ProcessMask process_mask,
@@ -91,9 +91,9 @@ DBusThreadManager::~DBusThreadManager() {
     return;  // Called form Shutdown() or local test instance.
 
   // There should never be both a global instance and a local instance.
-  CHECK(this == g_dbus_thread_manager);
+  CHECK_EQ(this, g_dbus_thread_manager);
   if (g_using_dbus_thread_manager_for_testing) {
-    g_dbus_thread_manager = NULL;
+    g_dbus_thread_manager = nullptr;
     g_using_dbus_thread_manager_for_testing = false;
     VLOG(1) << "DBusThreadManager destroyed";
   } else {
@@ -248,6 +248,7 @@ std::unique_ptr<DBusThreadManagerSetter>
 DBusThreadManager::GetSetterForTesting() {
   if (!g_using_dbus_thread_manager_for_testing) {
     g_using_dbus_thread_manager_for_testing = true;
+    CHECK(!g_dbus_thread_manager);
     // TODO(jamescook): Don't initialize clients as a side-effect of using a
     // test API. For now, assume the caller wants all clients.
     g_dbus_thread_manager =
@@ -260,7 +261,7 @@ DBusThreadManager::GetSetterForTesting() {
 
 // static
 bool DBusThreadManager::IsInitialized() {
-  return g_dbus_thread_manager != NULL;
+  return !!g_dbus_thread_manager;
 }
 
 // static
@@ -268,7 +269,7 @@ void DBusThreadManager::Shutdown() {
   // Ensure that we only shutdown DBusThreadManager once.
   CHECK(g_dbus_thread_manager);
   DBusThreadManager* dbus_thread_manager = g_dbus_thread_manager;
-  g_dbus_thread_manager = NULL;
+  g_dbus_thread_manager = nullptr;
   g_using_dbus_thread_manager_for_testing = false;
   delete dbus_thread_manager;
   VLOG(1) << "DBusThreadManager Shutdown completed";
