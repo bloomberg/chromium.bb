90dd9dd2eb473579eaf706fa62d1e5e155f55e4b
tapted <tapted@chromium.org>
2016-11-06 18:32:59 -0800

    Disable Flaky PolicyTest.Disable3DAPIs.
    BUG=662693
    TBR=bartfab@chromium.org, kainino@chromium.org
    
    Review-Url: https://codereview.chromium.org/2477233002
    Cr-Commit-Position: refs/heads/master@{#430210}
    
    Former-commit-id: a5d5e38d1e12f33effaa5300409ef5d299cd3a60

--- chrome/browser/policy/policy_browsertest.cc
+++ chrome/browser/policy/policy_browsertest.cc
@@ -1214,7 +1214,8 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, ForceGoogleSafeSearch) {
   }
 }
 
-IN_PROC_BROWSER_TEST_F(PolicyTest, Disable3DAPIs) {
+// Flaky. See http://crbug.com/662693.
+IN_PROC_BROWSER_TEST_F(PolicyTest, DISABLED_Disable3DAPIs) {
   // This test assumes Gpu access.
   if (!content::GpuDataManager::GetInstance()->GpuAccessAllowed(NULL))
     return;
