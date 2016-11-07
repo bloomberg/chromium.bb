498ee0ba34ccb6c3f6e7ee8fd226313a0df88d8f
hshi <hshi@chromium.org>
2016-11-07 12:54:03 -0800

    OzoneGpuTestHelper: post FakeGpuProcessHost::Init on IO thread.
    As the result of crrev.com/429173, the primary DRM device discovery now happens
    synchronously on the IO thread when GpuProcessHost::Init() gets called.
    
    We need to make the corresponding change in FakeGpuProcessHost to ensure that
    primary DRM device discovery happens on the same IO thread task runner too.
    
    BUG=chromium:662791
    TEST=build and run video_decode_accelerator_unittest on elm
    
    Review-Url: https://codereview.chromium.org/2484863002
    Cr-Commit-Position: refs/heads/master@{#430370}
    
    Former-commit-id: fcd83cd88ae65c63c34edab073cdbdc1165ffeb1

--- ui/ozone/public/ozone_gpu_test_helper.cc
+++ ui/ozone/public/ozone_gpu_test_helper.cc
@@ -139,7 +139,9 @@ bool OzoneGpuTestHelper::Initialize(
 
   fake_gpu_process_host_.reset(new FakeGpuProcessHost(
       gpu_task_runner, io_helper_thread_->task_runner()));
-  fake_gpu_process_host_->Init();
+  io_helper_thread_->task_runner()->PostTask(
+      FROM_HERE, base::Bind(&FakeGpuProcessHost::Init,
+                            base::Unretained(fake_gpu_process_host_.get())));
 
   return true;
 }
