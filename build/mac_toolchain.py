b5c5d70ba8bb31b67075a2bdecdeece696d0cbc5
sdefresne <sdefresne@chromium.org>
2016-11-08 01:10:50 -0800

    Revert of Turn on hermetic toolchain for all corp machines. (patchset #2 id:20001 of https://codereview.chromium.org/2445993004/ )
    Reason for revert:
    This break developer workflow for Googlers (as build/mac/should_use_hermetic_xcode.py returns 1) as the hermetic build does not contains support for iOS SDK, thus "gn gen" fails with the following error:
    
    $ gn gen --args='target_os="ios"' out/default
    ERROR at //build/config/ios/ios_sdk.gni:98:21: Script returned non-zero exit code.
      _ios_sdk_result = exec_script(script_name, ios_sdk_info_args, "scope")
                        ^----------
    Current dir: /Users/sdefresne/Developer/chromium/src/out/default/
    Command: python -- /Users/sdefresne/Developer/chromium/src/build/config/mac/sdk_info.py --developer_dir /Users/sdefresne/Developer/chromium/src/build/mac_files/Xcode.app iphonesimulator
    Returned 1.
    stderr:
    
    xcodebuild: error: SDK "iphonesimulator" cannot be located.
    xcodebuild: error: SDK "iphonesimulator" cannot be located.
    xcrun: error: unable to lookup item 'Path' in SDK 'iphonesimulator'
    
    The was not caught by the bots because they do not set FORCE_MAC_TOOLCHAIN and are not corporate machine.
    
    Original issue's description:
    > Turn on hermetic toolchain for all corp machines.
    >
    > BUG=659726
    >
    > Committed: https://crrev.com/54fae14cf7b4e9b5edd9b5624e7774a78814bc2b [formerly 7778e931dabc1fa85d0b77321e3fdef166e0b6a0]
    > Cr-Commit-Position: refs/heads/master@{#430376}
    
    TBR=thakis@chromium.org,dpranke@chromium.org,brettw@chromium.org,justincohen@chromium.org,erikchen@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=659726
    
    Review-Url: https://codereview.chromium.org/2487643002
    Cr-Commit-Position: refs/heads/master@{#430553}
    
    Former-commit-id: e62f1a5d8886fc1086a180f4da94e0a3d1ca03dc

--- build/mac_toolchain.py
+++ build/mac_toolchain.py
@@ -3,14 +3,19 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""
-If should_use_hermetic_xcode.py emits "1", and the current toolchain is out of
-date:
-  * Downloads the hermetic mac toolchain
-    * Requires gsutil to be configured.
-  * Accepts the license.
-    * If xcode-select and xcodebuild are not passwordless in sudoers, requires
-      user interaction.
+"""Download necessary mac toolchain files under certain conditions.  If
+xcode-select is already set and points to an external folder
+(e.g. /Application/Xcode.app), this script only runs if the GYP_DEFINE
+|force_mac_toolchain| is set.  To override the values in
+|TOOLCHAIN_REVISION|-|TOOLCHAIN_SUB_REVISION| below, GYP_DEFINE
+mac_toolchain_revision can be used instead.
+
+This script will only run on machines if /usr/bin/xcodebuild and
+/usr/bin/xcode-select has been added to the sudoers list so the license can be
+accepted.
+
+Otherwise, user input would be required to complete the script.  Perhaps future
+versions can be modified to allow for user input on developer machines.
 """
 
 import os
@@ -133,42 +138,23 @@ def AcceptLicense():
     subprocess.check_call(['sudo', '/usr/bin/xcode-select', '-s', old_path])
 
 
-def _UseHermeticToolchain():
-  current_dir = os.path.dirname(os.path.realpath(__file__))
-  script_path = os.path.join(current_dir, 'mac/should_use_hermetic_xcode.py')
-  proc = subprocess.Popen([script_path], stdout=subprocess.PIPE)
-  return '1' in proc.stdout.readline()
+def _UseLocalMacSDK():
+  force_pull = os.environ.has_key('FORCE_MAC_TOOLCHAIN')
 
-
-def RequestGsAuthentication():
-  """Requests that the user authenticate to be able to access gs://.
-  """
-  print 'Access to ' + TOOLCHAIN_URL + ' not configured.'
-  print '-----------------------------------------------------------------'
-  print
-  print 'You appear to be a Googler.'
-  print
-  print 'I\'m sorry for the hassle, but you need to do a one-time manual'
-  print 'authentication. Please run:'
-  print
-  print '    download_from_google_storage --config'
-  print
-  print 'and follow the instructions.'
-  print
-  print 'NOTE 1: Use your google.com credentials, not chromium.org.'
-  print 'NOTE 2: Enter 0 when asked for a "project-id".'
-  print
-  print '-----------------------------------------------------------------'
-  print
-  sys.stdout.flush()
-  sys.exit(1)
+  # Don't update the toolchain if there's already one installed outside of the
+  # expected location for a Chromium mac toolchain, unless |force_pull| is set.
+  proc = subprocess.Popen(['xcode-select', '-p'], stdout=subprocess.PIPE)
+  xcode_select_dir = proc.communicate()[0]
+  rc = proc.returncode
+  return (not force_pull and rc == 0 and
+          TOOLCHAIN_BUILD_DIR not in xcode_select_dir)
 
 
 def main():
   if sys.platform != 'darwin':
     return 0
 
-  if not _UseHermeticToolchain():
+  if _UseLocalMacSDK():
     print 'Using local toolchain.'
     return 0
 
@@ -180,8 +166,8 @@ def main():
     return 0
 
   if not CanAccessToolchainBucket():
-    RequestGsAuthentication()
-    return 1
+    print 'Cannot access toolchain bucket.'
+    return 0
 
   # Reset the stamp file in case the build is unsuccessful.
   WriteStampFile('')
