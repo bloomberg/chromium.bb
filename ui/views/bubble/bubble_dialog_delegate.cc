bloomberg/chromium.bb

commit ab32001923082396fc3eea5f58a2840e4d8dc13f

CrOS - Fix text background of shelf tooltips.
BUG=660510

Review-Url: https://codereview.chromium.org/2460103002
Cr-Commit-Position: refs/heads/master@{#428719}

Former-commit-id: 17ab8262e8e13c1ace5b8270dbf71a3c7af6a7aa

ui/views/bubble/bubble_dialog_delegate.cc
@@ -295,11 +295,6 @@ void BubbleDialogDelegateView::UpdateColorsFromTheme(
     const ui::NativeTheme* theme) {
   if (!color_explicitly_set_)
     color_ = theme->GetSystemColor(ui::NativeTheme::kColorId_BubbleBackground);
-  // The background color is handled by the BubbleFrameView, so it shouldn't be
-  // necessary to set the color on |this|. I am cowardly leaving it in place for
-  // pre-MD bubbles in case this is necessary for some reason.
-  if (!ui::MaterialDesignController::IsSecondaryUiMaterial())
-    set_background(Background::CreateSolidBackground(color()));
   BubbleFrameView* frame_view = GetBubbleFrameView();
   if (frame_view)
     frame_view->bubble_border()->set_background_color(color());

ash/common/shelf/shelf_tooltip_manager.cc
@@ -77,10 +77,9 @@ class ShelfTooltipManager::ShelfTooltipBubble
         theme->GetSystemColor(ui::NativeTheme::kColorId_TooltipBackground);
     set_color(background_color);
     label->SetBackgroundColor(background_color);
+    // The background is not opaque, so we can't do subpixel rendering.
+    label->SetSubpixelRenderingEnabled(false);
     AddChildView(label);
-    // The bubble border has its own background so the background created by the
-    // BubbleDialogDelegateView is redundant and would cause extra opacity.
-    set_background(nullptr);
 
     gfx::Insets insets(kArrowTopBottomOffset, kArrowLeftRightOffset);
     // Adjust the anchor location for asymmetrical borders of shelf item.

commit a9f87c062d39067c9c7044080683b17cc7dd7a6a

Fix IME tray bubble (and other tray bubble views).
This was broken by ab32001923082396fc3 [formerly 17ab8262e8e13c1ace5] which stopped explicitly painting
a background for bubble contents views.

The symptom I observed is a black background (due to the layer being
opaque and painting nothing). This should also fix the subpixel text
corruption shown in the bug.

BUG=663192

Review-Url: https://codereview.chromium.org/2485793002
Cr-Commit-Position: refs/heads/master@{#430500}

Former-commit-id: 03f052fcaf93364ce2136172b91137793e4f3458

ui/views/bubble/bubble_dialog_delegate.cc
@@ -298,6 +298,12 @@ void BubbleDialogDelegateView::UpdateColorsFromTheme(
   BubbleFrameView* frame_view = GetBubbleFrameView();
   if (frame_view)
     frame_view->bubble_border()->set_background_color(color());
+
+  // When there's an opaque layer, the bubble border background won't show
+  // through, so explicitly paint a background color.
+  set_background(layer() && layer()->fills_bounds_opaquely()
+                     ? Background::CreateSolidBackground(color())
+                     : nullptr);
 }
 
 void BubbleDialogDelegateView::HandleVisibilityChanged(Widget* widget,