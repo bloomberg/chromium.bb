e6885e50a85d403cc106e429b5e9f327fb2205c1
eugenebut <eugenebut@chromium.org>
2016-11-09 14:31:26 -0800

    [ios] Added new unittests for NavigationManager.
    New tests cover NavigationManager::GetPendingItemIndex method which will
    be changed to support pending navigation index.
    
    BUG=661858
    
    Review-Url: https://codereview.chromium.org/2489983003
    Cr-Commit-Position: refs/heads/master@{#431054}
    
    Former-commit-id: 741ee79d54e68e064b22392f5754acf5391c333c

--- ios/web/navigation/navigation_manager_impl_unittest.mm
+++ ios/web/navigation/navigation_manager_impl_unittest.mm
@@ -2,31 +2,76 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#import "ios/web/navigation/navigation_manager_impl.h"
+
 #include "base/logging.h"
-#include "ios/web/navigation/navigation_manager_impl.h"
+#import "base/mac/scoped_nsobject.h"
+#import "ios/web/navigation/crw_session_controller+private_constructors.h"
+#import "ios/web/navigation/navigation_manager_delegate.h"
 #include "ios/web/public/test/test_browser_state.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "testing/gtest_mac.h"
 #include "testing/platform_test.h"
 
 namespace web {
 namespace {
+// Stub class for NavigationManagerDelegate.
+class TestNavigationManagerDelegate : public NavigationManagerDelegate {
+  void GoToOffset(int offset) override {}
+  void GoToIndex(int index) override {}
+  void LoadURLWithParams(const NavigationManager::WebLoadParams&) override {}
+  void OnNavigationItemsPruned(size_t pruned_item_count) override {}
+  void OnNavigationItemChanged() override{};
+  void OnNavigationItemCommitted(const LoadCommittedDetails&) override {}
+  WebState* GetWebState() override { return nullptr; }
+};
+}  // namespace
 
+// Test fixture for NavigationManagerImpl testing.
 class NavigationManagerTest : public PlatformTest {
  protected:
-  void SetUp() override {
-    manager_.reset(new NavigationManagerImpl(NULL, &browser_state_));
+  NavigationManagerTest()
+      : manager_(new NavigationManagerImpl(&delegate_, &browser_state_)) {
+    controller_.reset([[CRWSessionController alloc]
+           initWithWindowName:nil
+                     openerId:nil
+                  openedByDOM:NO
+        openerNavigationIndex:0
+                 browserState:&browser_state_]);
+    manager_->SetSessionController(controller_.get());
   }
-  std::unique_ptr<NavigationManagerImpl> manager_;
+  CRWSessionController* session_controller() { return controller_.get(); }
+  NavigationManagerImpl* navigation_manager() { return manager_.get(); }
+
+ private:
   TestBrowserState browser_state_;
+  TestNavigationManagerDelegate delegate_;
+  std::unique_ptr<NavigationManagerImpl> manager_;
+  base::scoped_nsobject<CRWSessionController> controller_;
 };
 
-// TODO(stuartmorgan): Remove this once NavigationManager has actual
-// functionality to test, instead of just being pass-throughs.
-TEST_F(NavigationManagerTest, Dummy) {
-  EXPECT_FALSE(manager_->CanGoBack());
-  EXPECT_FALSE(manager_->CanGoForward());
+// Tests that GetPendingItemIndex() returns -1 if there is no pending entry.
+TEST_F(NavigationManagerTest, GetPendingItemIndexWithoutPendingEntry) {
+  [session_controller() addPendingEntry:GURL("http://www.url.com")
+                               referrer:Referrer()
+                             transition:ui::PAGE_TRANSITION_TYPED
+                      rendererInitiated:NO];
+  [session_controller() commitPendingEntry];
+  EXPECT_EQ(-1, navigation_manager()->GetPendingItemIndex());
 }
 
-}  // namespace
+// Tests that GetPendingItemIndex() returns current item index if there is a
+// pending entry.
+TEST_F(NavigationManagerTest, GetPendingItemIndexWithPendingEntry) {
+  [session_controller() addPendingEntry:GURL("http://www.url.com")
+                               referrer:Referrer()
+                             transition:ui::PAGE_TRANSITION_TYPED
+                      rendererInitiated:NO];
+  [session_controller() commitPendingEntry];
+  [session_controller() addPendingEntry:GURL("http://www.url.com/0")
+                               referrer:Referrer()
+                             transition:ui::PAGE_TRANSITION_TYPED
+                      rendererInitiated:NO];
+  EXPECT_EQ(0, navigation_manager()->GetPendingItemIndex());
+}
+
 }  // namespace web
