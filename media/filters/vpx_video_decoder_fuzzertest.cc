e31ff88aa9e4c4dcf83f7e7ef8dd6483c2306cb6
mmoroz <mmoroz@chromium.org>
2016-11-09 05:33:51 -0800

    Generate more valid configurations in media_vpx_video_decoder_fuzzer.
    That helps to bypass some DCHECK()'s and to make the Debug build work.
    Also that increases execution speed and helps to find new coverage.
    Speed increased ~5-10 times. Also I see lot of NEW testcases while
    running locally both Debug and Release builds.
    
    R=jrummell@chromium.org
    BUG=644672
    
    Review-Url: https://codereview.chromium.org/2324843004
    Cr-Commit-Position: refs/heads/master@{#430921}
    
    Former-commit-id: 29b3abb7b00ec613920892937199d39f684c9493

--- media/filters/vpx_video_decoder_fuzzertest.cc
+++ media/filters/vpx_video_decoder_fuzzertest.cc
@@ -55,18 +55,43 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   }
 
   // Compute randomized constants. Put all rng() usages here.
-  auto codec = static_cast<VideoCodec>(rng() % kVideoCodecMax);
+  // Use only values that pass DCHECK in VpxVideoDecoder::ConfigureDecoder().
+  VideoCodec codec;
+  VideoPixelFormat pixel_format;
+  if (rng() & 1) {
+    codec = kCodecVP8;
+    // PIXEL_FORMAT_YV12 disabled for kCodecVP8 on Linux.
+    pixel_format = PIXEL_FORMAT_YV12A;
+  } else {
+    codec = kCodecVP9;
+    switch (rng() % 3) {
+      case 0:
+        pixel_format = PIXEL_FORMAT_YV12;
+        break;
+      case 1:
+        pixel_format = PIXEL_FORMAT_YV12A;
+        break;
+      case 2:
+        pixel_format = PIXEL_FORMAT_YV24;
+        break;
+      default:
+        return 0;
+    }
+  }
+
   auto profile =
       static_cast<VideoCodecProfile>(rng() % VIDEO_CODEC_PROFILE_MAX);
-  auto pixel_format = static_cast<VideoPixelFormat>(rng() % PIXEL_FORMAT_MAX);
   auto color_space = static_cast<ColorSpace>(rng() % COLOR_SPACE_MAX);
-  auto coded_size = gfx::Size(rng() % 128, rng() % 128);
-  auto visible_rect = gfx::Rect(rng() % 128, rng() % 128);
-  auto natural_size = gfx::Size(rng() % 128, rng() % 128);
+  auto coded_size = gfx::Size(1 + (rng() % 127), 1 + (rng() % 127));
+  auto visible_rect = gfx::Rect(coded_size);
+  auto natural_size = gfx::Size(1 + (rng() % 127), 1 + (rng() % 127));
 
   VideoDecoderConfig config(codec, profile, pixel_format, color_space,
                             coded_size, visible_rect, natural_size,
                             EmptyExtraData(), Unencrypted());
+
+  if (!config.IsValidConfig())
+    return 0;
 
   VpxVideoDecoder decoder;
 
