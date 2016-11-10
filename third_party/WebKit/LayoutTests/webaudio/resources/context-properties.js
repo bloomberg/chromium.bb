db9a6c7265cc2db34b22df0385e498a9b22f351c
hongchan <hongchan@chromium.org>
2016-11-09 16:40:04 -0800

    Expose BaseAudioContext prototype
    Refactoring of BaseAudioContext (https://codereview.chromium.org/1865583002)
    changed the prototype structure of AudioContext/OfflineAudioContext and
    developers could not call hasOwnProperty() on BaseAudioContext because
    it was not exposed to the JS domain.
    
    This CL exposes BaseAudioContext's prototype. Also it adds 3 new layout
    tests for each audio context and removes the old context property test.
    
    BUG=555608, 661637
    TEST=
    LayoutTests/webaudio/baseaudiocontext-properties.html
    LayoutTests/webaudio/audiocontext-properties.html
    LayoutTests/webaudio/offlineaudiocontext-properties.html
    
    Review-Url: https://codereview.chromium.org/2386303002
    Cr-Commit-Position: refs/heads/master@{#431105}
    
    Former-commit-id: 5ec22676c144539f37f15740f4d712bb298ea02a

---
+++ third_party/WebKit/LayoutTests/webaudio/resources/context-properties.js
@@ -0,0 +1,115 @@
+// The list of the 'own' properties in various AudioContexts. These lists were
+// populated by running:
+//
+//   Object.getOwnPropertyNames(FooAudioContext.prototype);
+//
+// https://webaudio.github.io/web-audio-api/#BaseAudioContext
+
+
+let BaseAudioContextOwnProperties = [
+  'constructor',
+  'createAnalyser',
+  'createBiquadFilter',
+  'createBuffer',
+  'createBufferSource',
+  'createChannelMerger',
+  'createChannelSplitter',
+  'createConstantSource',
+  'createConvolver',
+  'createDelay',
+  'createDynamicsCompressor',
+  'createGain',
+  'createIIRFilter',
+  'createOscillator',
+  'createPanner',
+  'createPeriodicWave',
+  'createScriptProcessor',
+  'createStereoPanner',
+  'createWaveShaper',
+  'currentTime',
+  'decodeAudioData',
+  'destination',
+  'listener',
+  'onstatechange',
+  'resume',
+  'sampleRate',
+  'state',
+
+  // TODO(hongchan): these belong to AudioContext.
+  'createMediaElementSource',
+  'createMediaStreamDestination',
+  'createMediaStreamSource',
+
+  // TODO(hongchan): Not implemented yet.
+  // 'baseLatency'
+];
+
+
+let AudioContextOwnProperties = [
+  'close',
+  'constructor',
+  'suspend',
+
+  // TODO(hongchan): Not implemented yet.
+  // 'outputLatency',
+  // 'getOutputTimestamp'
+];
+
+
+let OfflineAudioContextOwnProperties = [
+  'constructor',
+  'length',
+  'oncomplete',
+  'startRendering',
+  'suspend',
+];
+
+
+/**
+ * Verify properties in the prototype with the pre-populated list. This is a
+ * 2-way comparison to detect the missing and the unexpected property at the
+ * same time.
+ * @param  {Object} targetPrototype           Target prototype.
+ * @param  {Array} populatedList              Property dictionary.
+ * @param  {Function} should                  |Should| assertion function.
+ * @return {Map}                              Verification result map.
+ */
+function verifyPrototypeOwnProperties (targetPrototype, populatedList, should) {
+  let propertyMap = new Map();
+  let generatedList = Object.getOwnPropertyNames(targetPrototype);
+
+  for (let index in populatedList) {
+    propertyMap.set(populatedList[index], {
+      actual: false,
+      expected: true
+    });
+  }
+
+  for (let index in generatedList) {
+    if (propertyMap.has(generatedList[index])) {
+      propertyMap.get(generatedList[index]).actual = true;
+    } else {
+      propertyMap.set(generatedList[index], {
+        actual: true,
+        expected: false
+      });
+    }
+  }
+
+  // TODO(hongchan): replace the Should assertion when the new test infra lands.
+  for (let [property, result] of propertyMap) {
+    if (result.expected && result.actual) {
+      // The test meets the expectation.
+      should('The property "' + property + '"')
+          ._testPassed('was expected and found successfully', false);
+    } else if (result.expected && !result.actual) {
+      // The expected property is missing.
+      should('The property "' + property + '" was expected but not found.')
+          ._testFailed('', false);
+    } else if (!result.expected && result.actual) {
+      // Something unexpected was found.
+      should('The property "' + property + '" was not expected but found.')
+          ._testFailed('', false);
+    }
+  }
+}
