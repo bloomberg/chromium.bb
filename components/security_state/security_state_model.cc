6e9c9b2e36ff8b2f7f8f5f2d9954cd51f88fb005
lshang <lshang@chromium.org>
2016-11-10 19:15:50 -0800

    HTTP Bad: Split out UMA metrics for password vs credit card "Not secure" warnings
    Split SecurityInfo::displayed_private_user_input_data_on_http into two booleans,
    one for passwords and one for credit cards, and split out the
    Security.HTTPBad.UserWarnedAboutSensitiveInput histogram into two
    histograms: Security.HTTPBad.UserWarnedAboutSensitiveInput.CreditCard and
    Security.HTTPBad.UserWarnedAboutSensitiveInput.Password depending on the boolean
    password/credit card flags in |security_info|.
    
    BUG=663389
    
    Review-Url: https://codereview.chromium.org/2483423002
    Cr-Commit-Position: refs/heads/master@{#431481}
    
    Former-commit-id: 4805cebc1656e9f99905537db6b369bec581ac28

--- components/security_state/security_state_model.cc
+++ components/security_state/security_state_model.cc
@@ -242,8 +242,9 @@ void SecurityInfoForRequest(
   security_info->fails_malware_check =
       visible_security_state.fails_malware_check;
 
-  security_info->displayed_private_user_data_input_on_http =
-      visible_security_state.displayed_password_field_on_http ||
+  security_info->displayed_password_field_on_http =
+      visible_security_state.displayed_password_field_on_http;
+  security_info->displayed_credit_card_field_on_http =
       visible_security_state.displayed_credit_card_field_on_http;
 
   security_info->security_level = GetSecurityLevelForRequest(
@@ -274,7 +275,8 @@ SecurityStateModel::SecurityInfo::SecurityInfo()
       key_exchange_group(0),
       obsolete_ssl_status(net::OBSOLETE_SSL_NONE),
       pkp_bypassed(false),
-      displayed_private_user_data_input_on_http(false) {}
+      displayed_password_field_on_http(false),
+      displayed_credit_card_field_on_http(false) {}
 
 SecurityStateModel::SecurityInfo::~SecurityInfo() {}
 
