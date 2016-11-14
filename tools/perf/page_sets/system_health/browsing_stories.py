f30877770c1d3e31e9efa9b5553967d6d36ee365
zhenw <zhenw@chromium.org>
2016-11-14 15:25:18 -0800

    Disable browse:social:twitter on Mac perf bots
    BUG=664661, 663025
    
    Review-Url: https://codereview.chromium.org/2501623002
    Cr-Commit-Position: refs/heads/master@{#431964}
    
    Former-commit-id: d4c02aa0d46a7fe3bf66c97e70921f653ff4913f

--- tools/perf/page_sets/system_health/browsing_stories.py
+++ tools/perf/page_sets/system_health/browsing_stories.py
@@ -194,7 +194,8 @@ class TwitterMobileStory(_NewsBrowsingStory):
   SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
 
 
-@decorators.Disabled('win')  # crbug.com/662971
+@decorators.Disabled('win',  # crbug.com/662971
+                     'mac')  # crbug.com/664661, crbug.com/663025
 class TwitterDesktopStory(_NewsBrowsingStory):
   NAME = 'browse:social:twitter'
   URL = 'https://www.twitter.com/nasa'
