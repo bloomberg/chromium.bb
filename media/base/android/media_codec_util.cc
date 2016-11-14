3ed52831cf8df4ca1c884cf859b5d1cdba47974a
liberato <liberato@chromium.org>
2016-11-14 13:35:21 -0800

    Blacklist Samsung Galaxy Star Pro (GT-S7262) for Spitzer.
    BUG=crbug.com/634920
    TBR=watk@chromium.org
    
    Review-Url: https://codereview.chromium.org/2409843002
    Cr-Commit-Position: refs/heads/master@{#431916}
    
    Former-commit-id: 184decd0afefe8f8b666b6923ab1d7c38865544a

--- media/base/android/media_codec_util.cc
+++ media/base/android/media_codec_util.cc
@@ -101,12 +101,13 @@ bool MediaCodecUtil::IsMediaCodecAvailable() {
   // http://crbug.com/365494, http://crbug.com/615872
   // Blacklist Lenovo A6600 / A6800 on KitKat, which tends to crash a lot.
   // See crbug.com/628059 .  We include < K since they don't exist.
+  // Blacklist Samsung Galaxy Star Pro (GT-S7262) (crbug.com/634920).
   // GT-S5282 and GT-I8552 are for crbug.com/634920 .
   if (base::android::BuildInfo::GetInstance()->sdk_int() <= 19) {
     std::string model(base::android::BuildInfo::GetInstance()->model());
     return model != "GT-I9100" && model != "GT-I9300" && model != "GT-N7000" &&
            model != "GT-N7100" && model != "A6600" && model != "A6800" &&
-           model != "GT-S5282" && model != "GT-I8552";
+           model != "GT-S7262" && model != "GT-S5282" && model != "GT-I8552";
   }
 
   return true;
