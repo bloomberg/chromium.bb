755a66f9b5c0d2bd7a74e9a05a325ec1e1cf4eb2
dcheng <dcheng@chromium.org>
2016-11-15 00:41:56 -0800

    Remove unused TypeConverter helper for WTFArray.
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2505523002
    Cr-Commit-Position: refs/heads/master@{#432146}
    
    Former-commit-id: fdbb56c4feb0e9fb9b457c427841c20e7aa985c2

--- third_party/WebKit/Source/platform/mojo/MojoHelper.h
+++ third_party/WebKit/Source/platform/mojo/MojoHelper.h
@@ -6,26 +6,6 @@
 #define MojoHelper_h
 
 #include "base/message_loop/message_loop.h"
-#include "mojo/public/cpp/bindings/wtf_array.h"
-#include "platform/heap/HeapAllocator.h"
-#include <utility>
-
-namespace mojo {
-
-// A |TypeConverter| that will create a |WTFArray<T>| containing a copy of the
-// contents of a |blink::HeapVector<E>|, using |TypeConverter<T, E>| to copy
-// each element. The returned array will always be non-null.
-template <typename T, typename E>
-struct TypeConverter<WTFArray<T>, blink::HeapVector<E>> {
-  static WTFArray<T> Convert(const blink::HeapVector<E>& input) {
-    WTFArray<T> result(input.size());
-    for (size_t i = 0; i < input.size(); ++i)
-      result[i] = TypeConverter<T, E>::Convert(input[i]);
-    return std::move(result);
-  }
-};
-
-}  // namespace mojo
 
 namespace blink {
 
