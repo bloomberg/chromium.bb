214423c811e960d3ef3546a2d1edab0decf9c50a
dmazzoni <dmazzoni@chromium.org>
2016-11-16 10:41:02 -0800

    Fix accessibility crash due to root being null.
    See stack trace from bug - basically on Windows we fire a HIDE event on a node
    being destroyed, but we also have logic to fire a queued-up load complete
    event any time any event is fired - but when we're firing the HIDE event on
    the old root, GetRoot() will return null. Add a null check to prevent the
    crash.
    
    BUG=613652
    
    Review-Url: https://codereview.chromium.org/2498323003
    Cr-Commit-Position: refs/heads/master@{#432570}
    
    Former-commit-id: 39befe481bd527fe1eb1e9cd216d52fb2b70c93a

--- content/browser/accessibility/browser_accessibility_manager_win.cc
+++ content/browser/accessibility/browser_accessibility_manager_win.cc
@@ -118,7 +118,7 @@ void BrowserAccessibilityManagerWin::NotifyAccessibilityEvent(
   if (event_type == ui::AX_EVENT_LOAD_COMPLETE && can_fire_events)
     load_complete_pending_ = false;
 
-  if (load_complete_pending_ && can_fire_events) {
+  if (load_complete_pending_ && can_fire_events && GetRoot()) {
     load_complete_pending_ = false;
     NotifyAccessibilityEvent(BrowserAccessibilityEvent::FromPendingLoadComplete,
                              ui::AX_EVENT_LOAD_COMPLETE,
@@ -128,6 +128,7 @@ void BrowserAccessibilityManagerWin::NotifyAccessibilityEvent(
   if (!can_fire_events &&
       !load_complete_pending_ &&
       event_type == ui::AX_EVENT_LOAD_COMPLETE &&
+      GetRoot() &&
       !GetRoot()->HasState(ui::AX_STATE_OFFSCREEN) &&
       GetRoot()->PlatformChildCount() > 0) {
     load_complete_pending_ = true;
