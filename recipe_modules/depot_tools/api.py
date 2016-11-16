6ecbb4ac0b4374feefad513056baf5ffe109e2b0
Dan Jacques <dnj@chromium.org>
2016-11-16 11:46:39 -0800

    Add gsutil recipe module.
    Add the "gsutil" recipe module to depot_tools. This module is copied
    from "tools/build"'s recipe module set at:
    d6205ab0c0e8e1920f4af1617b5b3767cd944492.
    
    Two Chromium-specific functions have been removed:
    - download_with_polling
    - download_latest_file
    
    It also adds a function, "gsutil_py_path", which mimics the function in
    the "depot_tools" recipe module. Ultimately, the latter will be phased
    out in favor of the module-specific function.
    
    BUG=chromium:662895
    TEST=expectations
    
    Change-Id: Ie049d637593b3f02b9ff029fbd20fd3f7170faa6
    Reviewed-on: https://chromium-review.googlesource.com/411970
    Commit-Queue: Daniel Jacques <dnj@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- recipe_modules/depot_tools/api.py
+++ recipe_modules/depot_tools/api.py
@@ -21,6 +21,8 @@ class DepotToolsApi(recipe_api.RecipeApi):
   def gn_py_path(self):
     return self.package_repo_resource('gn.py')
 
+  # TODO(dnj): Remove this once everything uses the "gsutil" recipe module
+  # version.
   @property
   def gsutil_py_path(self):
     return self.package_repo_resource('gsutil.py')
