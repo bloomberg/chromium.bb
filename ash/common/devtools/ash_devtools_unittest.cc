38d1b8d49eba7c58a09c6308442bd9efe58264de
mhashmi <mhashmi@chromium.org>
2016-11-15 20:46:02 -0800

    Revert of Fix bug where removed (but not deleted) windows are not reflected in the tree properly (patchset #2 id:20001 of https://codereview.chromium.org/2495533005/ )
    Reason for revert:
    SystemTrayTest.NullDefaultViewIsNotRecorded failing (https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%281%29/builds/60026)
    
    Original issue's description:
    > Fix bug where removed (but not deleted) windows are not reflected in the tree properly
    >
    > When there are windows which are removed as children of some parent, and
    > they still exist, OnWindowDestroying is not called because the child window
    > was never destroyed. However, OnWindowTreeChanging is called, if
    > the target has an old_parent, aka the parent it is being removed from.
    > OnWindowTreeChanged is called if the target has a new_parent, aka the
    > parent it is being added to.
    >
    > This CL removes nodes on OnWindowTreeChanging and adds them back in
    > OnWindowTreeChanged if necessary. Since we only want them to trigger once,
    > we check to see in either callback if the window that the callback is
    > being called on is the old_parent or the new_parent.
    >
    > BUG=648701
    >
    > Committed: https://crrev.com/802c932a380d0b02e020f26702355fab597731fe [formerly f555669c992a0bf22b8994d6a6f5a3f27e9f32c4]
    > Cr-Commit-Position: refs/heads/master@{#432366}
    
    TBR=sadrul@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=648701
    
    Review-Url: https://codereview.chromium.org/2506913002
    Cr-Commit-Position: refs/heads/master@{#432374}
    
    Former-commit-id: e75795096a81f406c92afce687ef4aa54900845f

--- ash/common/devtools/ash_devtools_unittest.cc
+++ ash/common/devtools/ash_devtools_unittest.cc
@@ -226,7 +226,7 @@ TEST_F(AshDevToolsTest, WindowDestroyedChildNodeRemoved) {
   ExpectChildNodeRemoved(parent_node->getNodeId(), child_node->getNodeId());
 }
 
-TEST_F(AshDevToolsTest, WindowReorganizedChildNodeRearranged) {
+TEST_F(AshDevToolsTest, WindowReorganizedChildNodeRemovedAndInserted) {
   // Initialize DOMAgent
   std::unique_ptr<ui::devtools::protocol::DOM::Node> root;
   dom_agent()->getDocument(&root);
@@ -245,35 +245,6 @@ TEST_F(AshDevToolsTest, WindowReorganizedChildNodeRearranged) {
 
   Compare(target_window, target_node);
   Compare(child_window, child_node);
-  target_window->AddChild(child_window);
-  ExpectChildNodeRemoved(parent_node->getNodeId(), child_node->getNodeId());
-  ExpectChildNodeInserted(target_node->getNodeId(), sibling_node->getNodeId());
-}
-
-TEST_F(AshDevToolsTest, WindowReorganizedChildNodeRemovedAndInserted) {
-  WmWindow* root_window = WmShell::Get()->GetPrimaryRootWindow();
-  WmWindow* target_window = root_window->GetChildren()[1];
-  WmWindow* parent_window = root_window->GetChildren()[0];
-  std::unique_ptr<WindowOwner> child_owner(CreateChildWindow(parent_window));
-  WmWindow* child_window = child_owner->window();
-
-  // Initialize DOMAgent
-  std::unique_ptr<ui::devtools::protocol::DOM::Node> root;
-  dom_agent()->getDocument(&root);
-  DOM::Node* root_node = root->getChildren(nullptr)->get(0);
-
-  DOM::Node* parent_node = root_node->getChildren(nullptr)->get(0);
-  DOM::Node* target_node = root_node->getChildren(nullptr)->get(1);
-  Array<DOM::Node>* target_node_children = target_node->getChildren(nullptr);
-  DOM::Node* sibling_node =
-      target_node_children->get(target_node_children->length() - 1);
-  Array<DOM::Node>* parent_node_children = parent_node->getChildren(nullptr);
-  DOM::Node* child_node =
-      parent_node_children->get(parent_node_children->length() - 1);
-
-  Compare(target_window, target_node);
-  Compare(child_window, child_node);
-  parent_window->RemoveChild(child_window);
   target_window->AddChild(child_window);
   ExpectChildNodeRemoved(parent_node->getNodeId(), child_node->getNodeId());
   ExpectChildNodeInserted(target_node->getNodeId(), sibling_node->getNodeId());
