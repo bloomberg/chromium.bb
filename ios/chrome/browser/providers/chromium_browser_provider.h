9b298e2d70f67df9ad69552cd3189034d20eafb8
rohitrao <rohitrao@chromium.org>
2016-11-17 10:04:58 -0800

    [ios] Implements more of ChromiumBrowserProvider.
    BUG=666375
    
    Review-Url: https://codereview.chromium.org/2507933003
    Cr-Commit-Position: refs/heads/master@{#432911}
    
    Former-commit-id: bf9201e200fcdb2d36f269dc0ddcd1a000265d1b

--- ios/chrome/browser/providers/chromium_browser_provider.h
+++ ios/chrome/browser/providers/chromium_browser_provider.h
@@ -12,13 +12,23 @@ class ChromiumBrowserProvider : public ios::ChromeBrowserProvider {
   ChromiumBrowserProvider();
   ~ChromiumBrowserProvider() override;
 
- private:
   // ChromeBrowserProvider implementation
+  ios::SigninErrorProvider* GetSigninErrorProvider() override;
+  ios::SigninResourcesProvider* GetSigninResourcesProvider() override;
+  void SetChromeIdentityServiceForTesting(
+      std::unique_ptr<ios::ChromeIdentityService> service) override;
+  ios::ChromeIdentityService* GetChromeIdentityService() override;
+  void InitializeCastService(id main_tab_model) const override;
+  void AttachTabHelpers(web::WebState* web_state, id tab) const override;
   VoiceSearchProvider* GetVoiceSearchProvider() const override;
   id<LogoVendor> CreateLogoVendor(ios::ChromeBrowserState* browser_state,
                                   id<UrlLoader> loader) const override
       NS_RETURNS_RETAINED;
 
+ private:
+  std::unique_ptr<ios::SigninErrorProvider> signin_error_provider_;
+  std::unique_ptr<ios::SigninResourcesProvider> signin_resources_provider_;
+  std::unique_ptr<ios::ChromeIdentityService> chrome_identity_service_;
   std::unique_ptr<VoiceSearchProvider> voice_search_provider_;
 };
 
--- ios/chrome/browser/providers/chromium_browser_provider.mm
+++ ios/chrome/browser/providers/chromium_browser_provider.mm
@@ -7,15 +7,48 @@
 #include "base/memory/ptr_util.h"
 #import "ios/chrome/browser/providers/chromium_logo_controller.h"
 #import "ios/chrome/browser/providers/chromium_voice_search_provider.h"
+#include "ios/public/provider/chrome/browser/signin/chrome_identity_service.h"
+#include "ios/public/provider/chrome/browser/signin/signin_error_provider.h"
+#include "ios/public/provider/chrome/browser/signin/signin_resources_provider.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
 ChromiumBrowserProvider::ChromiumBrowserProvider()
-    : voice_search_provider_(base::MakeUnique<ChromiumVoiceSearchProvider>()) {}
+    : signin_error_provider_(base::MakeUnique<ios::SigninErrorProvider>()),
+      signin_resources_provider_(
+          base::MakeUnique<ios::SigninResourcesProvider>()),
+      voice_search_provider_(base::MakeUnique<ChromiumVoiceSearchProvider>()) {}
 
 ChromiumBrowserProvider::~ChromiumBrowserProvider() {}
+
+ios::SigninErrorProvider* ChromiumBrowserProvider::GetSigninErrorProvider() {
+  return signin_error_provider_.get();
+}
+
+ios::SigninResourcesProvider*
+ChromiumBrowserProvider::GetSigninResourcesProvider() {
+  return signin_resources_provider_.get();
+}
+
+void ChromiumBrowserProvider::SetChromeIdentityServiceForTesting(
+    std::unique_ptr<ios::ChromeIdentityService> service) {
+  chrome_identity_service_ = std::move(service);
+}
+
+ios::ChromeIdentityService*
+ChromiumBrowserProvider::GetChromeIdentityService() {
+  if (!chrome_identity_service_) {
+    chrome_identity_service_ = base::MakeUnique<ios::ChromeIdentityService>();
+  }
+  return chrome_identity_service_.get();
+}
+
+void ChromiumBrowserProvider::InitializeCastService(id main_tab_model) const {}
+
+void ChromiumBrowserProvider::AttachTabHelpers(web::WebState* web_state,
+                                               id tab) const {}
 
 VoiceSearchProvider* ChromiumBrowserProvider::GetVoiceSearchProvider() const {
   return voice_search_provider_.get();
