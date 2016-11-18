3223d9ebe373100294df149fb6804cc8b5d2f5a5
stkhapugin <stkhapugin@chromium.org>
2016-11-18 10:25:47 -0800

    [ObjC ARC] Converts ios/chrome/browser/prefs:prefs to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2506153004
    Cr-Commit-Position: refs/heads/master@{#433243}
    
    Former-commit-id: dc7a6cd5cc6d8b07647c2876f1534a46fccb6aa1

--- ios/chrome/browser/prefs/pref_observer_bridge.mm
+++ ios/chrome/browser/prefs/pref_observer_bridge.mm
@@ -7,6 +7,10 @@
 #include "base/bind.h"
 #include "components/prefs/pref_change_registrar.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 PrefObserverBridge::PrefObserverBridge(id<PrefObserverDelegate> delegate)
     : delegate_(delegate) {
 }
