bloomberg/chromium.bb

commit 62401e0af104bb524fa9c73d93b2eff826816873

Add DeviceADPolicyManager to provide AD policy.
For Active Directory-managed devices, in BrowserPolicyConnectorChromeOS
replace the DeviceCloudPolicyManagerChromeOS ConfigurationPolicyProvider
by the new DeviceADPolicyManager ConfigurationPolicyProvider and disable
DeviceLocalAccountPolicyService and ServerBackedStateKeysBroker.  Note
that accessors to the disabled services may return nullptr now.

Policy refresh is not yet wired through to authpolicyd, instead it
triggers another load from session_manager.

BUG=656558
TBR=bartfab (enterprise_platform_keys_private_api.cc)
TBR=achuith (chrome/browser/ui/webui/options/chromeos)

Review-Url: https://codereview.chromium.org/2486813002
Cr-Commit-Position: refs/heads/master@{#433171}

Former-commit-id: 8d2791ecec5187ab2188b123ce50710d14f9b9e4

chrome/browser/chromeos/policy/browser_policy_connector_chromeos.cc
@@ -24,6 +24,7 @@
 #include "chrome/browser/chromeos/policy/affiliated_invalidation_service_provider.h"
 #include "chrome/browser/chromeos/policy/affiliated_invalidation_service_provider_impl.h"
 #include "chrome/browser/chromeos/policy/bluetooth_policy_handler.h"
+#include "chrome/browser/chromeos/policy/device_active_directory_policy_manager.h"
 #include "chrome/browser/chromeos/policy/device_cloud_policy_initializer.h"
 #include "chrome/browser/chromeos/policy/device_cloud_policy_store_chromeos.h"
 #include "chrome/browser/chromeos/policy/device_local_account.h"
@@ -80,9 +81,7 @@ scoped_refptr<base::SequencedTaskRunner> GetBackgroundTaskRunner() {
 }  // namespace
 
 BrowserPolicyConnectorChromeOS::BrowserPolicyConnectorChromeOS()
-    : device_cloud_policy_manager_(nullptr),
-      global_user_cloud_policy_provider_(nullptr),
-      weak_ptr_factory_(this) {
+    : weak_ptr_factory_(this) {
   if (g_testing_install_attributes) {
     install_attributes_.reset(g_testing_install_attributes);
     g_testing_install_attributes = nullptr;
@@ -94,18 +93,11 @@ BrowserPolicyConnectorChromeOS::BrowserPolicyConnectorChromeOS()
   // (removing it now breaks tests). crbug.com/141016.
   if (chromeos::SystemSaltGetter::IsInitialized() &&
       chromeos::DBusThreadManager::IsInitialized()) {
-    state_keys_broker_ = base::MakeUnique<ServerBackedStateKeysBroker>(
-        chromeos::DBusThreadManager::Get()->GetSessionManagerClient(),
-        base::ThreadTaskRunnerHandle::Get());
-
-    chromeos::CryptohomeClient* cryptohome_client =
-        chromeos::DBusThreadManager::Get()->GetCryptohomeClient();
-
     // Don't initialize install attributes if g_testing_install_attributes have
     // been injected.
     if (!install_attributes_) {
-      install_attributes_ =
-          base::MakeUnique<chromeos::InstallAttributes>(cryptohome_client);
+      install_attributes_ = base::MakeUnique<chromeos::InstallAttributes>(
+          chromeos::DBusThreadManager::Get()->GetCryptohomeClient());
       base::FilePath install_attrs_file;
       CHECK(PathService::Get(chromeos::FILE_INSTALL_ATTRIBUTES,
                              &install_attrs_file));
@@ -116,11 +108,24 @@ BrowserPolicyConnectorChromeOS::BrowserPolicyConnectorChromeOS()
         base::MakeUnique<DeviceCloudPolicyStoreChromeOS>(
             chromeos::DeviceSettingsService::Get(), install_attributes_.get(),
             GetBackgroundTaskRunner());
-    device_cloud_policy_manager_ = new DeviceCloudPolicyManagerChromeOS(
-        std::move(device_cloud_policy_store),
-        base::ThreadTaskRunnerHandle::Get(), state_keys_broker_.get());
-    AddPolicyProvider(std::unique_ptr<ConfigurationPolicyProvider>(
-        device_cloud_policy_manager_));
+
+    if (install_attributes_->IsActiveDirectoryManaged()) {
+      device_active_directory_policy_manager_ =
+          new DeviceActiveDirectoryPolicyManager(
+              std::move(device_cloud_policy_store));
+      AddPolicyProvider(base::WrapUnique<ConfigurationPolicyProvider>(
+          device_active_directory_policy_manager_));
+    } else {
+      state_keys_broker_ = base::MakeUnique<ServerBackedStateKeysBroker>(
+          chromeos::DBusThreadManager::Get()->GetSessionManagerClient(),
+          base::ThreadTaskRunnerHandle::Get());
+
+      device_cloud_policy_manager_ = new DeviceCloudPolicyManagerChromeOS(
+          std::move(device_cloud_policy_store),
+          base::ThreadTaskRunnerHandle::Get(), state_keys_broker_.get());
+      AddPolicyProvider(base::WrapUnique<ConfigurationPolicyProvider>(
+          device_cloud_policy_manager_));
+    }
   }
 
   global_user_cloud_policy_provider_ = new ProxyPolicyProvider();
@@ -150,17 +155,22 @@ void BrowserPolicyConnectorChromeOS::Init(
     RestartDeviceCloudPolicyInitializer();
   }
 
-  device_local_account_policy_service_ =
-      base::MakeUnique<DeviceLocalAccountPolicyService>(
-          chromeos::DBusThreadManager::Get()->GetSessionManagerClient(),
-          chromeos::DeviceSettingsService::Get(), chromeos::CrosSettings::Get(),
-          affiliated_invalidation_service_provider_.get(),
-          GetBackgroundTaskRunner(), GetBackgroundTaskRunner(),
-          GetBackgroundTaskRunner(),
-          content::BrowserThread::GetTaskRunnerForThread(
-              content::BrowserThread::IO),
-          request_context);
-  device_local_account_policy_service_->Connect(device_management_service());
+  DCHECK(install_attributes_);
+  if (install_attributes_->IsCloudManaged()) {
+    device_local_account_policy_service_ =
+        base::MakeUnique<DeviceLocalAccountPolicyService>(
+            chromeos::DBusThreadManager::Get()->GetSessionManagerClient(),
+            chromeos::DeviceSettingsService::Get(),
+            chromeos::CrosSettings::Get(),
+            affiliated_invalidation_service_provider_.get(),
+            GetBackgroundTaskRunner(), GetBackgroundTaskRunner(),
+            GetBackgroundTaskRunner(),
+            content::BrowserThread::GetTaskRunnerForThread(
+                content::BrowserThread::IO),
+            request_context);
+    device_local_account_policy_service_->Connect(device_management_service());
+  }
+
   if (device_cloud_policy_manager_) {
     device_cloud_policy_invalidator_ =
         base::MakeUnique<AffiliatedCloudPolicyInvalidator>(
@@ -212,7 +222,7 @@ void BrowserPolicyConnectorChromeOS::Shutdown() {
 }
 
 bool BrowserPolicyConnectorChromeOS::IsEnterpriseManaged() const {
-  return install_attributes_ && install_attributes_->IsEnterpriseDevice();
+  return install_attributes_ && install_attributes_->IsEnterpriseManaged();
 }
 
 std::string BrowserPolicyConnectorChromeOS::GetEnterpriseDomain() const {

chrome/browser/chromeos/BUILD.gn
@@ -1001,6 +1001,8 @@ source_set("chromeos") {
     "policy/cloud_external_data_store.h",
     "policy/configuration_policy_handler_chromeos.cc",
     "policy/configuration_policy_handler_chromeos.h",
+    "policy/device_active_directory_policy_manager.cc",
+    "policy/device_active_directory_policy_manager.h",
     "policy/device_cloud_policy_initializer.cc",
     "policy/device_cloud_policy_initializer.h",
     "policy/device_cloud_policy_manager_chromeos.cc",

chrome/browser/chromeos/login/screens/chrome_user_selection_screen.cc
@@ -40,11 +40,15 @@ ChromeUserSelectionScreen::ChromeUserSelectionScreen(
       weak_factory_(this) {
   device_local_account_policy_service_ = g_browser_process->platform_part()->
       browser_policy_connector_chromeos()->GetDeviceLocalAccountPolicyService();
-  device_local_account_policy_service_->AddObserver(this);
+  if (device_local_account_policy_service_) {
+    device_local_account_policy_service_->AddObserver(this);
+  }
 }
 
 ChromeUserSelectionScreen::~ChromeUserSelectionScreen() {
-  device_local_account_policy_service_->RemoveObserver(this);
+  if (device_local_account_policy_service_) {
+    device_local_account_policy_service_->RemoveObserver(this);
+  }
 }
 
 void ChromeUserSelectionScreen::Init(const user_manager::UserList& users,

chrome/browser/chromeos/login/users/avatar/user_image_manager_browsertest.cc
@@ -35,10 +35,12 @@
 #include "chrome/browser/chromeos/login/users/default_user_image/default_user_images.h"
 #include "chrome/browser/chromeos/login/users/mock_user_manager.h"
 #include "chrome/browser/chromeos/login/users/scoped_user_manager_enabler.h"
+#include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/policy/cloud_external_data_manager_base_test_util.h"
 #include "chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos.h"
 #include "chrome/browser/chromeos/policy/user_cloud_policy_manager_factory_chromeos.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
+#include "chrome/browser/chromeos/settings/stub_install_attributes.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_downloader.h"
 #include "chrome/browser/signin/account_tracker_service_factory.h"
@@ -112,6 +114,13 @@ class UserImageManagerTest : public LoginManagerTest,
 
   // LoginManagerTest overrides:
   void SetUpInProcessBrowserTestFixture() override {
+    // Set up fake install attributes.
+    std::unique_ptr<chromeos::StubInstallAttributes> attributes =
+        base::MakeUnique<chromeos::StubInstallAttributes>();
+    attributes->SetEnterprise("fake-domain", "fake-id");
+    policy::BrowserPolicyConnectorChromeOS::SetInstallAttributesForTesting(
+        attributes.release());
+
     LoginManagerTest::SetUpInProcessBrowserTestFixture();
 
     ASSERT_TRUE(PathService::Get(chrome::DIR_TEST_DATA, &test_data_dir_));

chrome/browser/chromeos/login/users/chrome_user_manager_impl.cc
@@ -219,20 +219,23 @@ ChromeUserManagerImpl::ChromeUserManagerImpl()
   multi_profile_user_controller_.reset(
       new MultiProfileUserController(this, GetLocalState()));
 
-  policy::BrowserPolicyConnectorChromeOS* connector =
-      g_browser_process->platform_part()->browser_policy_connector_chromeos();
-  avatar_policy_observer_.reset(new policy::CloudExternalDataPolicyObserver(
-      cros_settings_,
-      connector->GetDeviceLocalAccountPolicyService(),
-      policy::key::kUserAvatarImage,
-      this));
-  avatar_policy_observer_->Init();
+  policy::DeviceLocalAccountPolicyService* device_local_account_policy_service =
+      g_browser_process->platform_part()
+          ->browser_policy_connector_chromeos()
+          ->GetDeviceLocalAccountPolicyService();
+  if (!device_local_account_policy_service) {
+    return;
+  }
 
-  wallpaper_policy_observer_.reset(new policy::CloudExternalDataPolicyObserver(
-      cros_settings_,
-      connector->GetDeviceLocalAccountPolicyService(),
-      policy::key::kWallpaperImage,
-      this));
+  avatar_policy_observer_ =
+      base::MakeUnique<policy::CloudExternalDataPolicyObserver>(
+          cros_settings_, device_local_account_policy_service,
+          policy::key::kUserAvatarImage, this);
+  avatar_policy_observer_->Init();
+  wallpaper_policy_observer_ =
+      base::MakeUnique<policy::CloudExternalDataPolicyObserver>(
+          cros_settings_, device_local_account_policy_service,
+          policy::key::kWallpaperImage, this);
   wallpaper_policy_observer_->Init();
 }
 

chrome/browser/chromeos/login/users/wallpaper/wallpaper_manager_policy_browsertest.cc
@@ -23,10 +23,12 @@
 #include "chrome/browser/chromeos/login/startup_utils.h"
 #include "chrome/browser/chromeos/login/ui/login_display_host.h"
 #include "chrome/browser/chromeos/login/users/wallpaper/wallpaper_manager.h"
+#include "chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h"
 #include "chrome/browser/chromeos/policy/cloud_external_data_manager_base_test_util.h"
 #include "chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos.h"
 #include "chrome/browser/chromeos/policy/user_cloud_policy_manager_factory_chromeos.h"
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
+#include "chrome/browser/chromeos/settings/stub_install_attributes.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "chrome/common/chrome_paths.h"
@@ -175,6 +177,13 @@ class WallpaperManagerPolicyTest : public LoginManagerTest,
 
   // LoginManagerTest:
   void SetUpInProcessBrowserTestFixture() override {
+    // Set up fake install attributes.
+    std::unique_ptr<chromeos::StubInstallAttributes> attributes =
+        base::MakeUnique<chromeos::StubInstallAttributes>();
+    attributes->SetEnterprise("fake-domain", "fake-id");
+    policy::BrowserPolicyConnectorChromeOS::SetInstallAttributesForTesting(
+        attributes.release());
+
     DBusThreadManager::GetSetterForTesting()->SetSessionManagerClient(
         std::unique_ptr<SessionManagerClient>(fake_session_manager_client_));
 
@@ -410,6 +419,7 @@ IN_PROC_BROWSER_TEST_F(WallpaperManagerPolicyTest, PRE_PersistOverLogout) {
   // Run until wallpaper has changed.
   RunUntilWallpaperChangeCount(2);
   ASSERT_EQ(kRedImageColor, GetAverageWallpaperColor());
+  StartupUtils::MarkOobeCompleted();
 }
 
 IN_PROC_BROWSER_TEST_F(WallpaperManagerPolicyTest, PersistOverLogout) {

chrome/browser/chromeos/policy/browser_policy_connector_chromeos.h
@@ -39,6 +39,7 @@ class AffiliatedCloudPolicyInvalidator;
 class AffiliatedInvalidationServiceProvider;
 class AffiliatedRemoteCommandsInvalidator;
 class BluetoothPolicyHandler;
+class DeviceActiveDirectoryPolicyManager;
 class DeviceCloudPolicyInitializer;
 class DeviceLocalAccountPolicyService;
 class DeviceManagementService;
@@ -75,6 +76,8 @@ class BrowserPolicyConnectorChromeOS
   // Returns the enterprise domain if device is managed.
   std::string GetEnterpriseDomain() const;
 
+  // Returns the Kerberos realm (aka Windows Domain) if the device is managed by
+  // Active Directory.
   std::string GetRealm() const;
 
   // Returns the device asset ID if it is set.
@@ -94,14 +97,23 @@ class BrowserPolicyConnectorChromeOS
   // for details.
   EnrollmentConfig GetPrescribedEnrollmentConfig() const;
 
+  // May be nullptr, e.g. for devices managed by Active Directory.
   DeviceCloudPolicyManagerChromeOS* GetDeviceCloudPolicyManager() const {
     return device_cloud_policy_manager_;
   }
 
+  // May be nullptr, e.g. for cloud-managed devices.
+  DeviceActiveDirectoryPolicyManager* GetDeviceActiveDirectoryPolicyManager()
+      const {
+    return device_active_directory_policy_manager_;
+  }
+
+  // May be nullptr, e.g. for devices managed by Active Directory.
   DeviceCloudPolicyInitializer* GetDeviceCloudPolicyInitializer() const {
     return device_cloud_policy_initializer_.get();
   }
 
+  // May be nullptr, e.g. for devices managed by Active Directory.
   DeviceLocalAccountPolicyService* GetDeviceLocalAccountPolicyService() const {
     return device_local_account_policy_service_.get();
   }
@@ -110,6 +122,7 @@ class BrowserPolicyConnectorChromeOS
     return install_attributes_.get();
   }
 
+  // May be nullptr, e.g. for devices managed by Active Directory.
   ServerBackedStateKeysBroker* GetStateKeysBroker() const {
     return state_keys_broker_.get();
   }
@@ -163,8 +176,10 @@ class BrowserPolicyConnectorChromeOS
   std::unique_ptr<chromeos::InstallAttributes> install_attributes_;
   std::unique_ptr<AffiliatedInvalidationServiceProvider>
       affiliated_invalidation_service_provider_;
-  DeviceCloudPolicyManagerChromeOS* device_cloud_policy_manager_;
-  PrefService* local_state_;
+  DeviceCloudPolicyManagerChromeOS* device_cloud_policy_manager_ = nullptr;
+  DeviceActiveDirectoryPolicyManager* device_active_directory_policy_manager_ =
+      nullptr;
+  PrefService* local_state_ = nullptr;
   std::unique_ptr<DeviceCloudPolicyInitializer>
       device_cloud_policy_initializer_;
   std::unique_ptr<DeviceLocalAccountPolicyService>
@@ -182,7 +197,7 @@ class BrowserPolicyConnectorChromeOS
   // after login.
   // The provider is owned by the base class; this field is just a typed weak
   // pointer to get to the ProxyPolicyProvider at SetUserPolicyDelegate().
-  ProxyPolicyProvider* global_user_cloud_policy_provider_;
+  ProxyPolicyProvider* global_user_cloud_policy_provider_ = nullptr;
 
   std::unique_ptr<NetworkConfigurationUpdater> network_configuration_updater_;
 

chrome/browser/chromeos/policy/device_active_directory_policy_manager.cc
@@ -1,0 +1,80 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/policy/device_active_directory_policy_manager.h"
+
+#include <string>
+#include <utility>
+
+#include "base/logging.h"
+#include "base/memory/ptr_util.h"
+#include "components/policy/core/common/policy_bundle.h"
+#include "components/policy/core/common/policy_types.h"
+
+namespace policy {
+
+DeviceActiveDirectoryPolicyManager::DeviceActiveDirectoryPolicyManager(
+    std::unique_ptr<CloudPolicyStore> store)
+    : store_(std::move(store)) {}
+
+DeviceActiveDirectoryPolicyManager::~DeviceActiveDirectoryPolicyManager() {}
+
+void DeviceActiveDirectoryPolicyManager::Init(SchemaRegistry* registry) {
+  ConfigurationPolicyProvider::Init(registry);
+
+  store_->AddObserver(this);
+  PublishPolicy();
+  if (!store_->is_initialized()) {
+    store_->Load();
+  }
+}
+
+void DeviceActiveDirectoryPolicyManager::Shutdown() {
+  store_->RemoveObserver(this);
+  ConfigurationPolicyProvider::Shutdown();
+}
+
+bool DeviceActiveDirectoryPolicyManager::IsInitializationComplete(
+    PolicyDomain domain) const {
+  if (domain == POLICY_DOMAIN_CHROME)
+    return store_->is_initialized();
+  return true;
+}
+
+void DeviceActiveDirectoryPolicyManager::RefreshPolicies() {
+  store_->Load();
+}
+
+void DeviceActiveDirectoryPolicyManager::OnStoreLoaded(
+    CloudPolicyStore* cloud_policy_store) {
+  DCHECK_EQ(store_.get(), cloud_policy_store);
+  PublishPolicy();
+}
+
+void DeviceActiveDirectoryPolicyManager::OnStoreError(
+    CloudPolicyStore* cloud_policy_store) {
+  DCHECK_EQ(store_.get(), cloud_policy_store);
+  // Publish policy (even though it hasn't changed) in order to signal load
+  // complete on the ConfigurationPolicyProvider interface. Technically, this is
+  // only required on the first load, but doesn't hurt in any case.
+  PublishPolicy();
+}
+
+void DeviceActiveDirectoryPolicyManager::PublishPolicy() {
+  if (!store_->is_initialized()) {
+    return;
+  }
+  std::unique_ptr<PolicyBundle> bundle = base::MakeUnique<PolicyBundle>();
+  PolicyMap& policy_map =
+      bundle->Get(PolicyNamespace(POLICY_DOMAIN_CHROME, std::string()));
+  policy_map.CopyFrom(store_->policy_map());
+
+  // Overwrite the source which is POLICY_SOURCE_CLOUD by default.
+  // TODO(tnagel): Rename CloudPolicyStore to PolicyStore and make the source
+  // configurable, then drop PolicyMap::SetSourceForAll().
+  policy_map.SetSourceForAll(POLICY_SOURCE_ACTIVE_DIRECTORY);
+  UpdatePolicy(std::move(bundle));
+}
+
+}  // namespace policy

commit 4ee0a15ba542aa2dd54ab019b3d94607603eda43

Fix enrollment crash
Fix a crash when hitting the "Done" button after enrollment that was
introduced in 62401e0af104bb524fa9c73d93b2eff826816873 [formerly 8d2791ecec5187ab2188b123ce50710d14f9b9e4].

BUG=656558

Review-Url: https://codereview.chromium.org/2515673002
Cr-Commit-Position: refs/heads/master@{#433199}

Former-commit-id: dd095cf6bd65478ad5e09f63aa0d4a927e85fc89

chrome/browser/chromeos/policy/browser_policy_connector_chromeos.cc
@@ -156,7 +156,7 @@ void BrowserPolicyConnectorChromeOS::Init(
   }
 
   DCHECK(install_attributes_);
-  if (install_attributes_->IsCloudManaged()) {
+  if (!install_attributes_->IsActiveDirectoryManaged()) {
     device_local_account_policy_service_ =
         base::MakeUnique<DeviceLocalAccountPolicyService>(
             chromeos::DBusThreadManager::Get()->GetSessionManagerClient(),