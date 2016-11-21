6fcd518b387acb4027717aedc75c3dc5984f457d
kapishnikov <kapishnikov@chromium.org>
2016-11-20 20:28:26 -0800

    Revert of Move WTF::SpinLock to base::SpinLock. (patchset #4 id:60001 of https://codereview.chromium.org/2484803003/ )
    Reason for revert:
    Broke Android Cronet ARMv6 Builder:
    https://build.chromium.org/p/chromium.android/builders/Android%20Cronet%20ARMv6%20Builder/builds/3565
    
    Original issue's description:
    > Move WTF::SpinLock to base::SpinLock.
    >
    > This CL depends on https://codereview.chromium.org/2473153003 ("Move some
    > compiler intrinsic #defines to base/.").
    >
    > BUG=632441
    >
    > Committed: https://crrev.com/6363899c4d997f34780356708a63cf38de2364ac [formerly d9dbd59434791ddc998d2aeb24f7f1326431ed14]
    > Cr-Commit-Position: refs/heads/master@{#433378}
    
    TBR=esprehn@chromium.org,dcheng@chromium.org,hans@chromium.org,thakis@chromium.org,primiano@chromium.org,palmer@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=632441
    
    Review-Url: https://codereview.chromium.org/2517033002
    Cr-Commit-Position: refs/heads/master@{#433472}
    
    Former-commit-id: d0f9937ffbecc604e21893ee38f2385550daa8c5

--- third_party/WebKit/Source/wtf/SpinLock.cpp
+++ third_party/WebKit/Source/wtf/SpinLock.cpp
@@ -2,11 +2,15 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/synchronization/spin_lock.h"
+#include "wtf/SpinLock.h"
 
-#if defined(OS_WIN)
+#include "wtf/Atomics.h"
+#include "wtf/CPU.h"
+#include "wtf/Compiler.h"
+
+#if OS(WIN)
 #include <windows.h>
-#elif defined(OS_POSIX)
+#elif OS(POSIX)
 #include <sched.h>
 #endif
 
@@ -16,24 +20,23 @@
 // other hyper-thread on this core. See the following for context:
 // https://software.intel.com/en-us/articles/benefitting-power-and-performance-sleep-loops
 //
-// The YIELD_THREAD macro tells the OS to relinquish our quantum. This is
+// The YIELD_THREAD macro tells the OS to relinquish our quanta. This is
 // basically a worst-case fallback, and if you're hitting it with any frequency
-// you really should be using a proper lock (such as |base::Lock|)rather than
-// these spinlocks.
-#if defined(OS_WIN)
+// you really should be using proper lock rather than these spinlocks.
+#if OS(WIN)
 #define YIELD_PROCESSOR YieldProcessor()
 #define YIELD_THREAD SwitchToThread()
-#elif defined(COMPILER_GCC) || defined(__clang__)
-#if defined(ARCH_CPU_X86_64) || defined(ARCH_CPU_X86)
+#elif COMPILER(GCC) || COMPILER(CLANG)
+#if CPU(X86_64) || CPU(X86)
 #define YIELD_PROCESSOR __asm__ __volatile__("pause")
-#elif defined(ARCH_CPU_ARMEL) || defined(ARCH_CPU_ARM64)
+#elif CPU(ARM) || CPU(ARM64)
 #define YIELD_PROCESSOR __asm__ __volatile__("yield")
-#elif defined(ARCH_CPU_MIPSEL)
+#elif CPU(MIPS)
 // The MIPS32 docs state that the PAUSE instruction is a no-op on older
 // architectures (first added in MIPS32r2). To avoid assembler errors when
 // targeting pre-r2, we must encode the instruction manually.
 #define YIELD_PROCESSOR __asm__ __volatile__(".word 0x00000140")
-#elif defined(ARCH_CPU_MIPS64EL) && __mips_isa_rev >= 2
+#elif CPU(MIPS64) && __mips_isa_rev >= 2
 // Don't bother doing using .word here since r2 is the lowest supported mips64
 // that Chromium supports.
 #define YIELD_PROCESSOR __asm__ __volatile__("pause")
@@ -46,7 +49,7 @@
 #endif
 
 #ifndef YIELD_THREAD
-#if defined(OS_POSIX)
+#if OS(POSIX)
 #define YIELD_THREAD sched_yield()
 #else
 #warning "Thread yield not supported on this OS."
@@ -54,29 +57,27 @@
 #endif
 #endif
 
-namespace base {
-namespace subtle {
+namespace WTF {
 
-void SpinLock::LockSlow() {
-  // The value of |kYieldProcessorTries| is cargo culted from TCMalloc, Windows
+void SpinLock::lockSlow() {
+  // The value of kYieldProcessorTries is cargo culted from TCMalloc, Windows
   // critical section defaults, and various other recommendations.
   // TODO(jschuh): Further tuning may be warranted.
   static const int kYieldProcessorTries = 1000;
   do {
     do {
       for (int count = 0; count < kYieldProcessorTries; ++count) {
-        // Let the processor know we're spinning.
+        // Let the Processor know we're spinning.
         YIELD_PROCESSOR;
-        if (!lock_.load(std::memory_order_relaxed) &&
-            LIKELY(!lock_.exchange(true, std::memory_order_acquire)))
+        if (!m_lock.load(std::memory_order_relaxed) &&
+            LIKELY(!m_lock.exchange(true, std::memory_order_acquire)))
           return;
       }
 
       // Give the OS a chance to schedule something on this core.
       YIELD_THREAD;
-    } while (lock_.load(std::memory_order_relaxed));
-  } while (UNLIKELY(lock_.exchange(true, std::memory_order_acquire)));
+    } while (m_lock.load(std::memory_order_relaxed));
+  } while (UNLIKELY(m_lock.exchange(true, std::memory_order_acquire)));
 }
 
-}  // namespace subtle
-}  // namespace base
+}  // namespace WTF
