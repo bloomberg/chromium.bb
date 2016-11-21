81844c72fccc8f573a7c80bb98b0e6ab8ffc8dfa
piman <piman@chromium.org>
2016-11-21 14:29:44 -0800

    Reland of Use switch-case for CSS delimiter parsing (patchset #1 id:1 of https://codereview.chromium.org/2521043002/ )
    Reason for revert:
    Doesn't look like the revert helped in any way, so relanding
    
    Original issue's description:
    > Revert of Use switch-case for CSS delimiter parsing (patchset #2 id:20001 of https://codereview.chromium.org/2515633002/ )
    >
    > Reason for revert:
    > Suspected cause for fast/forms/relayout-shifts-inner-editor.html failures.
    >
    > https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Mac10.11%20%28dbg%29/builds/6029
    > https://bugs.chromium.org/p/chromium/issues/detail?id=667403
    >
    > Original issue's description:
    > > Use switch-case for CSS delimiter parsing
    > >
    > > This is style-only change and should not change any logic/behavior.
    > >
    > > Committed: https://crrev.com/4b70eff9c685d95060595f9492bd462057e4597f [formerly 87c9635abb27e45f913c174043fc115c088aead5]
    > > Cr-Commit-Position: refs/heads/master@{#433470}
    >
    > TBR=timloh@chromium.org,kochi@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    >
    > Committed: https://crrev.com/35828af16033a0ec7759494ed4a6f4467885b5d8 [formerly 749322111601d5aef7f4679bde7652535ccbc391]
    > Cr-Commit-Position: refs/heads/master@{#433599}
    
    TBR=timloh@chromium.org,kochi@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2519973003
    Cr-Commit-Position: refs/heads/master@{#433676}
    
    Former-commit-id: 93fbcb734e49ba6719be0b926f865b4f944ec665

--- third_party/WebKit/Source/core/css/parser/CSSSelectorParser.cpp
+++ third_party/WebKit/Source/core/css/parser/CSSSelectorParser.cpp
@@ -604,48 +604,53 @@ CSSSelector::RelationType CSSSelectorParser::consumeCombinator(
   if (range.peek().type() != DelimiterToken)
     return fallbackResult;
 
-  UChar delimiter = range.peek().delimiter();
-
-  if (delimiter == '+' || delimiter == '~') {
-    range.consumeIncludingWhitespace();
-    if (delimiter == '+')
-      return CSSSelector::DirectAdjacent;
-    return CSSSelector::IndirectAdjacent;
-  }
-
-  if (delimiter == '>') {
-    if (!RuntimeEnabledFeatures::shadowPiercingDescendantCombinatorEnabled() ||
-        m_context.isDynamicProfile() ||
-        range.peek(1).type() != DelimiterToken ||
-        range.peek(1).delimiter() != '>') {
+  switch (range.peek().delimiter()) {
+    case '+':
       range.consumeIncludingWhitespace();
-      return CSSSelector::Child;
-    }
-    range.consume();
+      return CSSSelector::DirectAdjacent;
 
-    // Check the 3rd '>'.
-    if (range.peek(1).type() != DelimiterToken ||
-        range.peek(1).delimiter() != '>') {
-      // TODO: Treat '>>' as a CSSSelector::Descendant here.
-      return CSSSelector::Child;
+    case '~':
+      range.consumeIncludingWhitespace();
+      return CSSSelector::IndirectAdjacent;
+
+    case '>':
+      if (!RuntimeEnabledFeatures::
+              shadowPiercingDescendantCombinatorEnabled() ||
+          m_context.isDynamicProfile() ||
+          range.peek(1).type() != DelimiterToken ||
+          range.peek(1).delimiter() != '>') {
+        range.consumeIncludingWhitespace();
+        return CSSSelector::Child;
+      }
+      range.consume();
+
+      // Check the 3rd '>'.
+      if (range.peek(1).type() != DelimiterToken ||
+          range.peek(1).delimiter() != '>') {
+        // TODO: Treat '>>' as a CSSSelector::Descendant here.
+        return CSSSelector::Child;
+      }
+      range.consume();
+      range.consumeIncludingWhitespace();
+      return CSSSelector::ShadowPiercingDescendant;
+
+    case '/': {
+      // Match /deep/
+      range.consume();
+      const CSSParserToken& ident = range.consume();
+      if (ident.type() != IdentToken ||
+          !equalIgnoringASCIICase(ident.value(), "deep"))
+        m_failedParsing = true;
+      const CSSParserToken& slash = range.consumeIncludingWhitespace();
+      if (slash.type() != DelimiterToken || slash.delimiter() != '/')
+        m_failedParsing = true;
+      return CSSSelector::ShadowDeep;
     }
-    range.consume();
-    range.consumeIncludingWhitespace();
-    return CSSSelector::ShadowPiercingDescendant;
+
+    default:
+      break;
   }
-
-  // Match /deep/
-  if (delimiter != '/')
-    return fallbackResult;
-  range.consume();
-  const CSSParserToken& ident = range.consume();
-  if (ident.type() != IdentToken ||
-      !equalIgnoringASCIICase(ident.value(), "deep"))
-    m_failedParsing = true;
-  const CSSParserToken& slash = range.consumeIncludingWhitespace();
-  if (slash.type() != DelimiterToken || slash.delimiter() != '/')
-    m_failedParsing = true;
-  return CSSSelector::ShadowDeep;
+  return fallbackResult;
 }
 
 CSSSelector::MatchType CSSSelectorParser::consumeAttributeMatch(
