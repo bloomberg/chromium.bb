e4667fe3c071eae91ae6f2f7dc6d85b6fb9624ea
stkhapugin <stkhapugin@chromium.org>
2016-11-21 06:17:46 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/webui:webui to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2518573002
    Cr-Commit-Position: refs/heads/master@{#433533}
    
    Former-commit-id: 1c39ac38482e62040d3ad2f4678cafae8f56c4e2

--- ios/chrome/browser/ui/webui/version_ui.mm
+++ ios/chrome/browser/ui/webui/version_ui.mm
@@ -26,6 +26,10 @@
 #include "ios/web/public/webui/web_ui_ios.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 web::WebUIIOSDataSource* CreateVersionUIDataSource() {
