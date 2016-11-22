498fe889bb7a735cab1ba4dfc89a9748f6da9eba
mbjorge <mbjorge@chromium.org>
2016-11-21 18:39:22 -0800

    [Buildfix] Fix enable_webrtc=false build.
    BUG=667527
    TEST=ninja content/renderer
    
    Review-Url: https://codereview.chromium.org/2522663003
    Cr-Commit-Position: refs/heads/master@{#433753}
    
    Former-commit-id: 53189bba54cc9bb057f9e35ede472088bed73a1d

--- content/renderer/pepper/pepper_media_device_manager.cc
+++ content/renderer/pepper/pepper_media_device_manager.cc
@@ -89,9 +89,9 @@ void PepperMediaDeviceManager::EnumerateDevices(
                  callback, ToMediaDeviceType(type)));
 #else
   base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE,
-      base::Bind(&PepperMediaDeviceManager::DevicesEnumerated, AsWeakPtr(),
-                 callback, ToMediaDeviceType(type), MediaDeviceInfoArray()));
+      FROM_HERE, base::Bind(&PepperMediaDeviceManager::DevicesEnumerated,
+                            AsWeakPtr(), callback, ToMediaDeviceType(type),
+                            std::vector<MediaDeviceInfoArray>()));
 #endif
 }
 
