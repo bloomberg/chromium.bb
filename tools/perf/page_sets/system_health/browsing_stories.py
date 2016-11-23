db9504d74ac3a47d7ec4a8b28d002a310a9bfc25
perezju <perezju@chromium.org>
2016-11-23 07:49:52 -0800

    [System Health] Disable browse:news:flipboard on Android
    Test has been failing on low end devices, and causes the whole of the
    benchmark to stop running. Disabling while issue is investigated.
    
    BUG=668097
    
    Review-Url: https://codereview.chromium.org/2525633005
    Cr-Commit-Position: refs/heads/master@{#434174}
    
    Former-commit-id: f1e0f5406659a87c17fc8dc29a0525194dc60bc5

--- tools/perf/page_sets/system_health/browsing_stories.py
+++ tools/perf/page_sets/system_health/browsing_stories.py
@@ -127,6 +127,10 @@ class FlipboardMobileStory(_NewsBrowsingStory):
   ITEM_SCROLL_REPEAT = 4
   SUPPORTED_PLATFORMS = platforms.MOBILE_ONLY
 
+  @classmethod
+  def ShouldDisable(cls, possible_browser):
+    return possible_browser.platform.IsSvelte()  # crbug.com/668097
+
 
 @decorators.Disabled('mac') # crbug.com/663025
 class FlipboardDesktopStory(_NewsBrowsingStory):
