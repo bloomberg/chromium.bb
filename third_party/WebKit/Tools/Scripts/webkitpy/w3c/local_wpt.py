71ab920ef368f279ebc30e7ed43f5b1f64c9eea9
qyearsley <qyearsley@chromium.org>
2016-11-22 17:04:15 -0800

    Edit comments in local_wpt.
    This should cause no behavior/functional changes; the purpose is just to clarify the code for future readers.
    
    Review-Url: https://codereview.chromium.org/2521113003
    Cr-Commit-Position: refs/heads/master@{#434054}
    
    Former-commit-id: a1437ec58be250c64565e192cd24bac5e4cdb966

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/local_wpt.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/local_wpt.py
@@ -2,7 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""A utility class for interacting with a local checkout of the web-platform-tests."""
+"""A utility class for interacting with a local checkout of the Web Platform Tests."""
 
 import logging
 
@@ -11,6 +11,7 @@ from webkitpy.w3c.chromium_commit import ChromiumCommit
 WPT_REPO_URL = 'https://chromium.googlesource.com/external/w3c/web-platform-tests.git'
 WPT_TMP_DIR = '/tmp/wpt'
 CHROMIUM_WPT_DIR = 'third_party/WebKit/LayoutTests/imported/wpt/'
+
 _log = logging.getLogger(__name__)
 
 
@@ -49,9 +50,7 @@ class LocalWPT(object):
         return self.host.executive.run_command(command, cwd=self.path, **kwargs)
 
     def most_recent_chromium_commit(self):
-        """Goes back in WPT commit history and gets the most recent commit
-        that contains 'Cr-Commit-Position:'
-        """
+        """Finds the most recent commit in WPT with a Chromium commit position."""
         sha = self.run(['git', 'rev-list', 'HEAD', '-n', '1', '--grep=Cr-Commit-Position'])
         if not sha:
             return None, None
@@ -70,9 +69,18 @@ class LocalWPT(object):
         self.run(['git', 'checkout', 'origin/master'])
 
     def all_branches(self):
+        """Returns a list of local and remote branches."""
         return self.run(['git', 'branch', '-a']).splitlines()
 
     def create_branch_with_patch(self, branch_name, message, patch):
+        """Commits the given patch and pushes to the upstream repo.
+
+        Args:
+            branch_name: A name that is used for both the local branch
+                and the remote branch on github.
+            message: Commit message string.
+            patch: A patch that can be applied by git apply.
+        """
         self.clean()
         all_branches = self.all_branches()
         remote_branch_name = 'remotes/github/%s' % branch_name
@@ -83,13 +91,13 @@ class LocalWPT(object):
 
         if remote_branch_name in all_branches:
             _log.info('Remote branch %s already exists, deleting', branch_name)
-            # TODO(jeffcarp): Investigate what happens when remote branch exists
+            # TODO(jeffcarp): Investigate what happens when remote branch exists.
             self.run(['git', 'push', 'github', ':{}'.format(branch_name)])
 
         _log.info('Creating local branch %s', branch_name)
         self.run(['git', 'checkout', '-b', branch_name])
 
-        # Remove Chromium WPT directory prefix
+        # Remove Chromium WPT directory prefix.
         patch = patch.replace(CHROMIUM_WPT_DIR, '')
 
         # TODO(jeffcarp): Use git am -p<n> where n is len(CHROMIUM_WPT_DIR.split(/'))
@@ -99,6 +107,11 @@ class LocalWPT(object):
         self.run(['git', 'push', 'github', branch_name])
 
     def commits_behind_master(self, commit):
+        """Returns the number of commits after the given commit on origin/master.
+
+        This doesn't include the given commit, and this assumes that the given
+        commit is on the the master branch.
+        """
         return len(self.run([
             'git', 'rev-list', '{}..origin/master'.format(commit)
         ]).splitlines())
