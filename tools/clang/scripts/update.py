1b05739a088fa80709107027aca208c1a7eb4d3d
thakis <thakis@chromium.org>
2016-11-28 13:13:47 -0800

    clang: Stop passing LLVM_ENABLE_TIMESTAMPS to cmake.
    It was removed upstream in a few months ago in rL268670
    (https://reviews.llvm.org/D19892)
    
    No behavior change.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2530413003
    Cr-Commit-Position: refs/heads/master@{#434720}
    
    Former-commit-id: 4af9c73ca32f2479e36756869120e41d108dbec7

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -496,7 +496,6 @@ def UpdateClang(args):
                      '-DCMAKE_BUILD_TYPE=Release',
                      '-DLLVM_ENABLE_ASSERTIONS=ON',
                      '-DLLVM_ENABLE_THREADS=OFF',
-                     '-DLLVM_ENABLE_TIMESTAMPS=OFF',
                      # Statically link MSVCRT to avoid DLL dependencies.
                      '-DLLVM_USE_CRT_RELEASE=MT',
                      ]
