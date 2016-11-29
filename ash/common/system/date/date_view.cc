12735cfd412b952004bca87a7311e86d0909b0e4
bruthig <bruthig@chromium.org>
2016-11-29 05:54:57 -0800

    [ash-md] Removed un-wanted right padding from the date label in the system menu.
    Removed 8dp padding from the right side of the date label in the system
    menu that was left over from non-md.
    
    BUG=669221
    
    Review-Url: https://codereview.chromium.org/2537033002
    Cr-Commit-Position: refs/heads/master@{#434980}
    
    Former-commit-id: bccf9329f520d11d88a04e43abb1632c4859ddfb

--- ash/common/system/date/date_view.cc
+++ ash/common/system/date/date_view.cc
@@ -168,7 +168,7 @@ DateView::DateView(SystemTrayItem* owner)
     // TODO(tdanderson): Tweak spacing and layout for material design.
     views::BoxLayout* box_layout =
         new views::BoxLayout(views::BoxLayout::kHorizontal, 0, 0, 0);
-    box_layout->set_inside_border_insets(gfx::Insets(0, 12, 0, 8));
+    box_layout->set_inside_border_insets(gfx::Insets(0, 12, 0, 0));
     box_layout->set_main_axis_alignment(
         views::BoxLayout::MAIN_AXIS_ALIGNMENT_START);
     box_layout->set_cross_axis_alignment(
