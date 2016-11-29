b076cc78a00cf611a59444c31e6c79c181636a9f
apacible <apacible@chromium.org>
2016-11-29 10:48:42 -0800

    Disable flaky SSLUITest.TestGoodFrameNavigation test.
    BUG=668360
    TBR=palmer@chromium.org
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2537193002
    Cr-Commit-Position: refs/heads/master@{#435042}
    
    Former-commit-id: 05d8c5e2f99b074e54f1011868147ef21c2a50ea

--- chrome/browser/ssl/ssl_browser_tests.cc
+++ chrome/browser/ssl/ssl_browser_tests.cc
@@ -2198,7 +2198,7 @@ IN_PROC_BROWSER_TEST_F(SSLUITest, TestConnectToBadPort) {
 // - navigate to a bad HTTPS (expect unsafe content and filtered frame), then
 //   back
 // - navigate to HTTP (expect insecure content), then back
-IN_PROC_BROWSER_TEST_F(SSLUITest, TestGoodFrameNavigation) {
+IN_PROC_BROWSER_TEST_F(SSLUITest, DISABLED_TestGoodFrameNavigation) {
   ASSERT_TRUE(embedded_test_server()->Start());
   ASSERT_TRUE(https_server_.Start());
   ASSERT_TRUE(https_server_expired_.Start());
