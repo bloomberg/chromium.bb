7986a0cea029e94122919b3656f42ce609dc4f70
georgesak <georgesak@chromium.org>
2016-11-29 09:21:13 -0800

    Add a crash key to record whether a windows machine is domain joined.
    BUG=660868
    
    Review-Url: https://codereview.chromium.org/2514483002
    Cr-Commit-Position: refs/heads/master@{#435018}
    
    Former-commit-id: bf33256aee8db58c30b9d36e991055f2a4ed62a4

--- chrome/common/crash_keys.cc
+++ chrome/common/crash_keys.cc
@@ -58,6 +58,8 @@ const char kHungRendererReason[] = "hung-reason";
 
 const char kThirdPartyModulesLoaded[] = "third-party-modules-loaded";
 const char kThirdPartyModulesNotLoaded[] = "third-party-modules-not-loaded";
+
+const char kEnrolledToDomain[] = "enrolled-to-domain";
 #endif
 
 const char kInputEventFilterSendFailure[] = "input-event-filter-send-failure";
@@ -143,6 +145,7 @@ size_t RegisterChromeCrashKeys() {
     { kHungRendererReason, kSmallSize },
     { kThirdPartyModulesLoaded, kSmallSize },
     { kThirdPartyModulesNotLoaded, kSmallSize },
+    { kEnrolledToDomain, kSmallSize },
 #endif
     { kInputEventFilterSendFailure, kSmallSize },
 #if defined(OS_CHROMEOS)
