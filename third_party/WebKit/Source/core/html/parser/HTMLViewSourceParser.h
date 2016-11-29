cfe4a0414d33fe0558132826d936e121cfa058da
battre <battre@chromium.org>
2016-11-29 05:21:42 -0800

    Reland of After detach, there's no document to notify parsing is finished. (patchset #1 id:1 of https://codereview.chromium.org/2538473004/ )
    Reason for revert:
    Reland, probably innocuous for crbug.com/669435
    
    Original issue's description:
    > Revert of After detach, there's no document to notify parsing is finished. (patchset #3 id:40001 of https://codereview.chromium.org/2537043002/ )
    >
    > Reason for revert:
    > Speculative revert for crbug.com/669435
    >
    > Original issue's description:
    > > After detach, there's no document to notify parsing is finished.
    > >
    > > HTMLViewSourceParser was tripping the DCHECK in
    > > DocumentParser::document when it recieved the last bytes of the
    > > document it was parsing after it was detached.
    > >
    > > BUG=664915
    > >
    > > Committed: https://crrev.com/b382deebcbbbdd887f586ceab92161594ef0c46f [formerly 8e59e2d6feaa38b37836837e996b9ad91c0b3745]
    > > Cr-Commit-Position: refs/heads/master@{#434937}
    >
    > TBR=tkent@chromium.org,yoichio@chromium.org,yosin@chromium.org,dominicc@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=664915
    >
    > Committed: https://crrev.com/9b9b30220ac8b34ee2cbb298ad5feddf97bfd8b0 [formerly ffe8e7b51df227590c9cadad11dd9e734f571129]
    > Cr-Commit-Position: refs/heads/master@{#434954}
    
    TBR=tkent@chromium.org,yoichio@chromium.org,yosin@chromium.org,dominicc@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=664915
    
    Review-Url: https://codereview.chromium.org/2535233002
    Cr-Commit-Position: refs/heads/master@{#434972}
    
    Former-commit-id: 53d659d792a55aa8244004a333fb5866a10048a2

--- third_party/WebKit/Source/core/html/parser/HTMLViewSourceParser.h
+++ third_party/WebKit/Source/core/html/parser/HTMLViewSourceParser.h
@@ -26,6 +26,7 @@
 #ifndef HTMLViewSourceParser_h
 #define HTMLViewSourceParser_h
 
+#include "core/CoreExport.h"
 #include "core/dom/DecodedDataDocumentParser.h"
 #include "core/html/HTMLViewSourceDocument.h"
 #include "core/html/parser/HTMLInputStream.h"
@@ -36,7 +37,8 @@
 
 namespace blink {
 
-class HTMLViewSourceParser final : public DecodedDataDocumentParser {
+class CORE_EXPORT HTMLViewSourceParser final
+    : public DecodedDataDocumentParser {
  public:
   static HTMLViewSourceParser* create(HTMLViewSourceDocument& document,
                                       const String& mimeType) {
