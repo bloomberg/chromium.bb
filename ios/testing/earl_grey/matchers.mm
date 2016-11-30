7a6f57a17596e422e4b56b119eabc636208607f7
stkhapugin <stkhapugin@chromium.org>
2016-11-30 05:31:00 -0800

    [ObjC ARC] Converts ios/testing/earl_grey:earl_grey_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2508973007
    Cr-Commit-Position: refs/heads/master@{#435241}
    
    Former-commit-id: fa554a4b832c0d1b826da1fca37c7dec2c20cce0

--- ios/testing/earl_grey/matchers.mm
+++ ios/testing/earl_grey/matchers.mm
@@ -4,6 +4,10 @@
 
 #import "ios/testing/earl_grey/matchers.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace testing {
 
 id<GREYMatcher> contextMenuItemWithText(NSString* text) {
