c030f5ac27833aabde48b56ff51e90a10431b6ac
eyaich <eyaich@chromium.org>
2016-11-30 09:27:38 -0800

    Returning correct failure code on swarmed perf test run.
    Currently the rc value is not set correctly and always
    returns 0.  Updating it to return 1 on failure so the
    step will turn red.
    
    Review-Url: https://codereview.chromium.org/2546443002
    Cr-Commit-Position: refs/heads/master@{#435313}
    
    Former-commit-id: a8d5dffdbf5fff11333a16599df5096736251423

--- testing/scripts/run_gtest_perf_test.py
+++ testing/scripts/run_gtest_perf_test.py
@@ -82,7 +82,6 @@ def main():
     assert xvfb_proc and openbox_proc and xcompmgr_proc, 'Failed to start xvfb'
 
   try:
-    valid = True
     rc = 0
     try:
       executable = rest_args[0]
@@ -94,8 +93,8 @@ def main():
       else:
         executable = './%s' % executable
       with common.temporary_file() as tempfile_path:
-        valid = (common.run_command_with_output([executable] + extra_flags,
-            env=env, stdoutfile=tempfile_path) == 0)
+        rc = common.run_command_with_output([executable] + extra_flags,
+            env=env, stdoutfile=tempfile_path)
 
         # Now get the correct json format from the stdout to write to the
         # perf results file
@@ -107,8 +106,9 @@ def main():
           f.write(charts)
     except Exception:
       traceback.print_exc()
-      valid = False
+      rc = 1
 
+    valid = (rc == 0)
     failures = [] if valid else ['(entire test suite)']
     with open(args.isolated_script_test_output, 'w') as fp:
       json.dump({
