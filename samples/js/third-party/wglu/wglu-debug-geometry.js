c0f56fb81cff925361bc4404b873f05da1bd99f0
Klaus Weidner <klausw@chromium.org>
2016-11-29 18:10:31 -0800

    Implement simple half-lambert shading for debug geometry
    This is mainly intended for the controller demo so that it looks
    a bit more three-dimensional.

--- samples/js/third-party/wglu/wglu-debug-geometry.js
+++ samples/js/third-party/wglu/wglu-debug-geometry.js
@@ -28,19 +28,29 @@ var WGLUDebugGeometry = (function() {
     "uniform mat4 projectionMat;",
     "uniform mat4 viewMat;",
     "uniform mat4 modelMat;",
+    "uniform mat3 normalMat;",
     "attribute vec3 position;",
+    "attribute vec3 normal;",
+    "varying vec3 v_normal;",
 
     "void main() {",
     "  gl_Position = projectionMat * viewMat * modelMat * vec4( position, 1.0 );",
+    "  v_normal = normalMat * normal;",
     "}",
   ].join("\n");
 
+  // Simple shading with a single light source, this uses half-lambert
+  // shading to keep things from getting too dark in the unlit areas.
+  // It's not physically based but looks ok.
   var debugGeomFS = [
     "precision mediump float;",
     "uniform vec4 color;",
+    "uniform vec3 light;",
+    "varying vec3 v_normal;",
 
     "void main() {",
-    "  gl_FragColor = color;",
+    "  vec3 normal = normalize(v_normal);",
+    "  gl_FragColor = ((dot(light, normal) * 0.5 + 0.5) * 0.8 + 0.2) * color;",
     "}",
   ].join("\n");
 
@@ -50,11 +60,13 @@ var WGLUDebugGeometry = (function() {
     this.projMat = mat4.create();
     this.viewMat = mat4.create();
     this.modelMat = mat4.create();
+    this.normalMat = mat3.create();
 
     this.program = new WGLUProgram(gl);
     this.program.attachShaderSource(debugGeomVS, gl.VERTEX_SHADER);
     this.program.attachShaderSource(debugGeomFS, gl.FRAGMENT_SHADER);
     this.program.bindAttribLocation({ position: 0 });
+    this.program.bindAttribLocation({ normal: 1 });
     this.program.link();
 
     var verts = [];
@@ -67,64 +79,64 @@ var WGLUDebugGeometry = (function() {
 
     var size = 0.5;
     // Bottom
-    var idx = verts.length / 3.0;
+    var idx = verts.length / 6.0;
     indices.push(idx, idx+1, idx+2);
     indices.push(idx, idx+2, idx+3);
 
-    verts.push(-size, -size, -size);
-    verts.push(+size, -size, -size);
-    verts.push(+size, -size, +size);
-    verts.push(-size, -size, +size);
+    verts.push(-size, -size, -size, 0, -1, 0);
+    verts.push(+size, -size, -size, 0, -1, 0);
+    verts.push(+size, -size, +size, 0, -1, 0);
+    verts.push(-size, -size, +size, 0, -1, 0);
 
     // Top
-    idx = verts.length / 3.0;
+    idx = verts.length / 6.0;
     indices.push(idx, idx+2, idx+1);
     indices.push(idx, idx+3, idx+2);
 
-    verts.push(-size, +size, -size);
-    verts.push(+size, +size, -size);
-    verts.push(+size, +size, +size);
-    verts.push(-size, +size, +size);
+    verts.push(-size, +size, -size, 0, 1, 0);
+    verts.push(+size, +size, -size, 0, 1, 0);
+    verts.push(+size, +size, +size, 0, 1, 0);
+    verts.push(-size, +size, +size, 0, 1, 0);
 
     // Left
-    idx = verts.length / 3.0;
+    idx = verts.length / 6.0;
     indices.push(idx, idx+2, idx+1);
     indices.push(idx, idx+3, idx+2);
 
-    verts.push(-size, -size, -size);
-    verts.push(-size, +size, -size);
-    verts.push(-size, +size, +size);
-    verts.push(-size, -size, +size);
+    verts.push(-size, -size, -size, -1, 0, 0);
+    verts.push(-size, +size, -size, -1, 0, 0);
+    verts.push(-size, +size, +size, -1, 0, 0);
+    verts.push(-size, -size, +size, -1, 0, 0);
 
     // Right
-    idx = verts.length / 3.0;
+    idx = verts.length / 6.0;
     indices.push(idx, idx+1, idx+2);
     indices.push(idx, idx+2, idx+3);
 
-    verts.push(+size, -size, -size);
-    verts.push(+size, +size, -size);
-    verts.push(+size, +size, +size);
-    verts.push(+size, -size, +size);
+    verts.push(+size, -size, -size, 1, 0, 0);
+    verts.push(+size, +size, -size, 1, 0, 0);
+    verts.push(+size, +size, +size, 1, 0, 0);
+    verts.push(+size, -size, +size, 1, 0, 0);
 
     // Back
-    idx = verts.length / 3.0;
+    idx = verts.length / 6.0;
     indices.push(idx, idx+2, idx+1);
     indices.push(idx, idx+3, idx+2);
 
-    verts.push(-size, -size, -size);
-    verts.push(+size, -size, -size);
-    verts.push(+size, +size, -size);
-    verts.push(-size, +size, -size);
+    verts.push(-size, -size, -size, 0, 0, -1);
+    verts.push(+size, -size, -size, 0, 0, -1);
+    verts.push(+size, +size, -size, 0, 0, -1);
+    verts.push(-size, +size, -size, 0, 0, -1);
 
     // Front
-    idx = verts.length / 3.0;
+    idx = verts.length / 6.0;
     indices.push(idx, idx+1, idx+2);
     indices.push(idx, idx+2, idx+3);
 
-    verts.push(-size, -size, +size);
-    verts.push(+size, -size, +size);
-    verts.push(+size, +size, +size);
-    verts.push(-size, +size, +size);
+    verts.push(-size, -size, +size, 0, 0, 1);
+    verts.push(+size, -size, +size, 0, 0, 1);
+    verts.push(+size, +size, +size, 0, 0, 1);
+    verts.push(-size, +size, +size, 0, 0, 1);
 
     this.cubeIndexCount = indices.length - this.cubeIndexOffset;
 
@@ -134,30 +146,36 @@ var WGLUDebugGeometry = (function() {
     this.coneIndexOffset = indices.length;
 
     var size = 0.5;
-    var conePointVertex = verts.length / 3.0;
+    var conePointVertex = verts.length / 6.0;
     var coneBaseVertex = conePointVertex+1;
-    var coneSegments = 16;
+    var coneSegments = 64;
 
-    // Point
-    verts.push(0, size, 0);
-
-    // Base Vertices
+    // Cone side vertices
     for (var i = 0; i < coneSegments; ++i) {
-        if (i > 0) {
-            idx = verts.length / 3.0;
-            indices.push(idx-1, conePointVertex, idx);
-        }
-
+        idx = verts.length / 6.0;
+        indices.push(idx, idx + 1, idx + 2);
         var rad = ((Math.PI * 2) / coneSegments) * i;
-        verts.push(Math.sin(rad) * (size / 2.0), -size, Math.cos(rad) * (size  / 2.0));
+        var rad2 = ((Math.PI * 2) / coneSegments) * (i + 1);
+        verts.push(Math.sin(rad) * (size / 2), -size, Math.cos(rad) * (size / 2),
+                   Math.sin(rad), 0.25, Math.cos(rad));
+
+        verts.push(Math.sin(rad2) * (size / 2), -size, Math.cos(rad2) * (size / 2),
+                   Math.sin(rad2), 0.25, Math.cos(rad2));
+
+        verts.push(0, size, 0,
+                   Math.sin((rad + rad2) / 2), 0.25, Math.cos((rad + rad2) / 2));
     }
 
-    // Last triangle to fill the gap
-    indices.push(idx, conePointVertex, coneBaseVertex);
-
     // Base triangles
-    for (var i = 2; i < coneSegments; ++i) {
-        indices.push(coneBaseVertex, coneBaseVertex+(i-1), coneBaseVertex+i);
+    var baseCenterIdx = verts.length / 6.0;
+    verts.push(0, -size, 0, 0, -1, 0);
+    for (var i = 0; i < coneSegments; ++i) {
+      idx = verts.length / 6.0;
+      indices.push(baseCenterIdx, idx, idx + 1);
+      var rad = ((Math.PI * 2) / coneSegments) * i;
+      var rad2 = ((Math.PI * 2) / coneSegments) * (i + 1);
+      verts.push(Math.sin(rad2) * (size / 2.0), -size, Math.cos(rad2) * (size  / 2.0), 0, -1, 0);
+      verts.push(Math.sin(rad) * (size / 2.0), -size, Math.cos(rad) * (size  / 2.0), 0, -1, 0);
     }
 
     this.coneIndexCount = indices.length - this.coneIndexOffset;
@@ -167,13 +185,13 @@ var WGLUDebugGeometry = (function() {
     //
     this.rectIndexOffset = indices.length;
 
-    idx = verts.length / 3.0;
+    idx = verts.length / 6.0;
     indices.push(idx, idx+1, idx+2, idx+3, idx);
 
-    verts.push(0, 0, 0);
-    verts.push(1, 0, 0);
-    verts.push(1, 1, 0);
-    verts.push(0, 1, 0);
+    verts.push(0, 0, 0, 0, 0, -1);
+    verts.push(1, 0, 0, 0, 0, -1);
+    verts.push(1, 1, 0, 0, 0, -1);
+    verts.push(0, 1, 0, 0, 0, -1);
 
     this.rectIndexCount = indices.length - this.rectIndexOffset;
 
@@ -200,7 +218,8 @@ var WGLUDebugGeometry = (function() {
 
     gl.enableVertexAttribArray(program.attrib.position);
 
-    gl.vertexAttribPointer(program.attrib.position, 3, gl.FLOAT, false, 12, 0);
+    gl.vertexAttribPointer(program.attrib.position, 3, gl.FLOAT, false, 24, 0);
+    gl.vertexAttribPointer(program.attrib.normal, 3, gl.FLOAT, false, 24, 12);
   };
 
   DebugGeometry.prototype.bindOrtho = function() {
@@ -209,15 +228,27 @@ var WGLUDebugGeometry = (function() {
     this.bind(this.projMat, this.viewMat);
   };
 
-  DebugGeometry.prototype._bindUniforms = function(orientation, position, scale, color) {
+  DebugGeometry.prototype._bindUniformsRaw = function(model, color, light) {
+    if (!color) { color = [1, 0, 0, 1]; }
+    if (!light) { light = [1, 1, 1]; }
+    var lightVec = vec3.fromValues(light[0], light[1], light[2]);
+    vec3.normalize(lightVec, lightVec);
+
+    mat3.normalFromMat4(this.normalMat, model);
+
+    this.gl.uniformMatrix4fv(this.program.uniform.modelMat, false, model);
+    this.gl.uniformMatrix3fv(this.program.uniform.normalMat, false, this.normalMat);
+    this.gl.uniform4fv(this.program.uniform.color, color);
+    this.gl.uniform3fv(this.program.uniform.light, lightVec);
+  };
+
+  DebugGeometry.prototype._bindUniforms = function(orientation, position, scale, color, light) {
     if (!position) { position = [0, 0, 0]; }
     if (!orientation) { orientation = [0, 0, 0, 1]; }
     if (!scale) { scale = [1, 1, 1]; }
-    if (!color) { color = [1, 0, 0, 1]; }
 
     mat4.fromRotationTranslationScale(this.modelMat, orientation, position, scale);
-    this.gl.uniformMatrix4fv(this.program.uniform.modelMat, false, this.modelMat);
-    this.gl.uniform4fv(this.program.uniform.color, color);
+    this._bindUniformsRaw(this.modelMat, color, light);
   };
 
   DebugGeometry.prototype.drawCube = function(orientation, position, size, color) {
@@ -238,8 +269,7 @@ var WGLUDebugGeometry = (function() {
   DebugGeometry.prototype.drawBoxWithMatrix = function(mat, color) {
     var gl = this.gl;
 
-    gl.uniformMatrix4fv(this.program.uniform.modelMat, false, mat);
-    gl.uniform4fv(this.program.uniform.color, color);
+    this._bindUniformsRaw(mat, color);
     gl.drawElements(gl.TRIANGLES, this.cubeIndexCount, gl.UNSIGNED_SHORT, this.cubeIndexOffset * 2.0);
   };
 
@@ -261,8 +291,7 @@ var WGLUDebugGeometry = (function() {
   DebugGeometry.prototype.drawConeWithMatrix = function(mat, color) {
     var gl = this.gl;
 
-    gl.uniformMatrix4fv(this.program.uniform.modelMat, false, mat);
-    gl.uniform4fv(this.program.uniform.color, color);
+    this._bindUniformsRaw(mat, color);
     gl.drawElements(gl.TRIANGLES, this.coneIndexCount, gl.UNSIGNED_SHORT, this.coneIndexOffset * 2.0);
   };
 
