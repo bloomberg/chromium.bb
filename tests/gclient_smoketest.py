e86f6de84296601c61a3d3e6d7655e596535c4d6
Robert Iannucci <iannucci@chromium.org>
2016-12-02 14:03:36 -0800

    Fix smoke test to not hard-code python executable location.
    BUG=670678
    
    Change-Id: If3104a6d666e84d7150ba805672dac3e34a1f9a1
    Reviewed-on: https://chromium-review.googlesource.com/416269
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>
    Reviewed-by: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Aaron Gable <agable@chromium.org>

--- tests/gclient_smoketest.py
+++ tests/gclient_smoketest.py
@@ -499,9 +499,9 @@ class GClientSmokeGIT(GClientSmokeBase):
         ('running', self.root_dir),                 # pre-deps hook
         ('running', self.root_dir),                 # pre-deps hook (fails)
     ]
-    expected_stderr = ("Error: Command '/usr/bin/python -c import sys; "
+    expected_stderr = ("Error: Command '%s -c import sys; "
                        "sys.exit(1)' returned non-zero exit status 1 in %s\n"
-                       % self.root_dir)
+                       % (sys.executable, self.root_dir))
     stdout, stderr, retcode = self.gclient(['sync', '--deps', 'mac', '--jobs=1',
                                             '--revision',
                                             'src@' + self.githash('repo_5', 3)])
