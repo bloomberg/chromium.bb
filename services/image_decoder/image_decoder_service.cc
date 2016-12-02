9341999fb870cc143336108a3e0350e75793bee2
rockot <rockot@chromium.org>
2016-12-01 17:43:32 -0800

    image_decoder: Delay shutdown on idle
    Instead of immediately tearing down the image_decoder service
    once it's idle, wait 5 seconds in case there are going to be
    more requests soon.
    
    This mirrors the behavior of utility process image decoding
    prior to servicification, and is intended to address latency
    issues with multiple image decodes happening in quick (but not
    quite immediate) succession.
    
    BUG=669835
    
    Review-Url: https://codereview.chromium.org/2538833004
    Cr-Commit-Position: refs/heads/master@{#435815}
    
    Former-commit-id: 0798a4585e6cb5df5e5596b8692656fca669cc28

--- services/image_decoder/image_decoder_service.cc
+++ services/image_decoder/image_decoder_service.cc
@@ -5,6 +5,8 @@
 #include "services/image_decoder/image_decoder_service.h"
 
 #include "base/macros.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "base/time/time.h"
 #include "mojo/public/cpp/bindings/strong_binding.h"
 #include "services/image_decoder/image_decoder_impl.h"
 #include "services/image_decoder/public/interfaces/image_decoder.mojom.h"
@@ -41,8 +43,8 @@ std::unique_ptr<service_manager::Service> ImageDecoderService::Create() {
 
 void ImageDecoderService::OnStart() {
   ref_factory_.reset(new service_manager::ServiceContextRefFactory(
-      base::Bind(&service_manager::ServiceContext::RequestQuit,
-                 base::Unretained(context()))));
+      base::Bind(&ImageDecoderService::MaybeRequestQuitDelayed,
+                 base::Unretained(this))));
 }
 
 bool ImageDecoderService::OnConnect(
@@ -61,6 +63,20 @@ bool ImageDecoderService::OnConnect(
 
 bool ImageDecoderService::OnStop() {
   return true;
+}
+
+void ImageDecoderService::MaybeRequestQuitDelayed() {
+  base::ThreadTaskRunnerHandle::Get()->PostDelayedTask(
+      FROM_HERE,
+      base::Bind(&ImageDecoderService::MaybeRequestQuit,
+                 base::Unretained(this)),
+      base::TimeDelta::FromSeconds(5));
+}
+
+void ImageDecoderService::MaybeRequestQuit() {
+  DCHECK(ref_factory_);
+  if (ref_factory_->HasNoRefs())
+    context()->RequestQuit();
 }
 
 }  // namespace image_decoder
--- services/image_decoder/image_decoder_service.h
+++ services/image_decoder/image_decoder_service.h
@@ -29,6 +29,9 @@ class ImageDecoderService : public service_manager::Service {
   bool OnStop() override;
 
  private:
+  void MaybeRequestQuitDelayed();
+  void MaybeRequestQuit();
+
   std::unique_ptr<service_manager::ServiceContextRefFactory> ref_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ImageDecoderService);
