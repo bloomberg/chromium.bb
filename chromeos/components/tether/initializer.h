04d932d4d1c5fc7339dbbd68ed8838db3684ddb2
khorimoto <khorimoto@chromium.org>
2016-12-03 18:37:11 -0800

    Create Tether Chrome OS component and add a skeleton Initializer class.
    Review-Url: https://codereview.chromium.org/2545923005
    Cr-Commit-Position: refs/heads/master@{#436184}
    
    Former-commit-id: 4c53995f0b8ea16729f63fc5e9c012335b3df302

---
+++ chromeos/components/tether/initializer.h
@@ -0,0 +1,29 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_COMPONENTS_TETHER_INITIALIZER_H_
+#define CHROMEOS_COMPONENTS_TETHER_INITIALIZER_H_
+
+#include "base/macros.h"
+
+namespace chromeos {
+
+namespace tether {
+
+// Initializes the Tether Chrome OS component.
+// TODO(khorimoto): Implement.
+class Initializer {
+ public:
+  Initializer();
+  ~Initializer();
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(Initializer);
+};
+
+}  // namespace tether
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_COMPONENTS_TETHER_INITIALIZER_H_
