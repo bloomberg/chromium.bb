fe1fe1851d1a6dee1414bea774697d28d9cc946a
stkhapugin <stkhapugin@chromium.org>
2016-12-05 06:32:50 -0800

    [ObjC ARC] Converts ios/testing:ios_test_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2517463004
    Cr-Commit-Position: refs/heads/master@{#436287}
    
    Former-commit-id: 1a6a279b85842250fd42dbccae2d7e0e695beebf

--- ios/testing/wait_util.mm
+++ ios/testing/wait_util.mm
@@ -6,6 +6,10 @@
 
 #include "base/test/ios/wait_util.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace testing {
 
 const NSTimeInterval kSpinDelaySeconds = 0.01;
