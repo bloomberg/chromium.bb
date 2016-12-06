2f0f7843df9621abbe2a240f9026bffe0315e043
rohitrao <rohitrao@chromium.org>
2016-12-06 07:33:50 -0800

    [ios] Adds test hooks to enable/disable functionality while testing.
    BUG=671589
    
    Review-Url: https://codereview.chromium.org/2557613004
    Cr-Commit-Position: refs/heads/master@{#436603}
    
    Former-commit-id: dfb38253441460834d4667833f7a25a884ac5a68

---
+++ ios/chrome/app/tests_fake_hook.mm
@@ -0,0 +1,24 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/app/tests_hook.h"
+
+namespace tests_hook {
+
+bool DisableContextualSearch() {
+  return false;
+}
+bool DisableFirstRun() {
+  return false;
+}
+bool DisableSigninRecallPromo() {
+  return false;
+}
+bool DisableUpdateService() {
+  return false;
+}
+void SetUpTestsIfPresent() {}
+void RunTestsIfPresent() {}
+
+}  // namespace tests_hook
---
+++ ios/chrome/app/tests_hook.h
@@ -0,0 +1,36 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IOS_CHROME_APP_TESTS_HOOK_H_
+#define IOS_CHROME_APP_TESTS_HOOK_H_
+
+namespace tests_hook {
+
+// Returns true if contextual search should be disabled to allow other tests
+// to run unimpeded.
+bool DisableContextualSearch();
+
+// Returns true if the first_run path should be disabled to allow other tests to
+// run unimpeded.
+bool DisableFirstRun();
+
+// Returns true if the signin recall promo should be disabled to allow other
+// tests to run unimpeded.
+bool DisableSigninRecallPromo();
+
+// Returns true if the update service should be disabled so that the update
+// infobar won't be shown during testing.
+bool DisableUpdateService();
+
+// Global integration tests setup.  This is not used by EarlGrey-based
+// integration tests.
+void SetUpTestsIfPresent();
+
+// Runs the integration tests.  This is not used by EarlGrey-based integration
+// tests.
+void RunTestsIfPresent();
+
+}  // namespace tests_hook
+
+#endif  // IOS_CHROME_APP_TESTS_HOOK_H_
