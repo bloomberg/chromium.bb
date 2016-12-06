8622f9398e7c89a664c4c2ceff9d35b89ff17bb5
Matt Wolenetz <wolenetz@chromium.org>
2016-12-06 12:54:23 -0800

    lavf/mov.c: Avoid heap allocation wraps and OOB in mov_read_{senc,saiz,udta_string}()
    Core of patch is from paul@paulmehta.com
    
    BUG=643952
    R=dalecurtis@chromium.org
    
    Change-Id: Ie464d4d0df044725fcb0a6d2fa49847580de2731
    Reviewed-on: https://chromium-review.googlesource.com/417161
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>

--- libavformat/mov.c
+++ libavformat/mov.c
@@ -404,7 +404,7 @@ retry:
                 return ret;
             } else if (!key && c->found_hdlr_mdta && c->meta_keys) {
                 uint32_t index = AV_RB32(&atom.type);
-                if (index < c->meta_keys_count) {
+                if (index < c->meta_keys_count && index > 0) {
                     key = c->meta_keys[index];
                 } else {
                     av_log(c->fc, AV_LOG_WARNING,
@@ -4502,8 +4502,8 @@ static int mov_read_senc(MOVContext *c, AVIOContext *pb, MOVAtom atom)
 
     avio_rb32(pb);        /* entries */
 
-    if (atom.size < 8) {
-        av_log(c->fc, AV_LOG_ERROR, "senc atom size %"PRId64" too small\n", atom.size);
+    if (atom.size < 8 || atom.size > UINT_MAX) {
+        av_log(c->fc, AV_LOG_ERROR, "senc atom size %"PRId64" invalid\n", atom.size);
         return AVERROR_INVALIDDATA;
     }
 
@@ -4569,6 +4569,11 @@ static int mov_read_saiz(MOVContext *c, AVIOContext *pb, MOVAtom atom)
 
     if (atom.size <= atom_header_size) {
         return 0;
+    }
+
+    if (atom.size > UINT_MAX) {
+        av_log(c->fc, AV_LOG_ERROR, "saiz atom auxiliary_info_sizes size %"PRId64" invalid\n", atom.size);
+        return AVERROR_INVALIDDATA;
     }
 
     /* save the auxiliary info sizes as is */
