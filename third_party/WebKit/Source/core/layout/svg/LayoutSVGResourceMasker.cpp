f0639d93143e96ecdaeca91d4bc5aca2eef192fe
fs <fs@opera.com>
2016-12-07 09:45:36 -0800

    Don't check 'visibility' in LayoutSVGResourceMasker
    Since 'visibility' does not work in the same way as 'display', it's not
    possible to "prune" subtrees based on non-'visible' values of the
    property. Remove the check from the two methods that use it, and leave
    to lower levels to handle it.
    
    BUG=672055
    
    Review-Url: https://codereview.chromium.org/2558793002
    Cr-Commit-Position: refs/heads/master@{#436996}
    
    Former-commit-id: 51a75fa55ba71462963ddcf085995cf3eca66104

--- third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceMasker.cpp
+++ third_party/WebKit/Source/core/layout/svg/LayoutSVGResourceMasker.cpp
@@ -85,17 +85,11 @@ sk_sp<const SkPicture> LayoutSVGResourceMasker::createContentPicture(
           : ColorFilterNone;
   pictureBuilder.context().setColorFilter(maskContentFilter);
 
-  for (SVGElement* childElement = Traversal<SVGElement>::firstChild(*element());
-       childElement;
-       childElement = Traversal<SVGElement>::nextSibling(*childElement)) {
-    LayoutObject* layoutObject = childElement->layoutObject();
-    if (!layoutObject)
+  for (const SVGElement& childElement :
+       Traversal<SVGElement>::childrenOf(*element())) {
+    const LayoutObject* layoutObject = childElement.layoutObject();
+    if (!layoutObject || layoutObject->styleRef().display() == EDisplay::None)
       continue;
-    const ComputedStyle* style = layoutObject->style();
-    if (!style || style->display() == EDisplay::None ||
-        style->visibility() != EVisibility::Visible)
-      continue;
-
     SVGPaintContext::paintSubtree(pictureBuilder.context(), layoutObject);
   }
 
@@ -104,15 +98,10 @@ sk_sp<const SkPicture> LayoutSVGResourceMasker::createContentPicture(
 }
 
 void LayoutSVGResourceMasker::calculateMaskContentVisualRect() {
-  for (SVGElement* childElement = Traversal<SVGElement>::firstChild(*element());
-       childElement;
-       childElement = Traversal<SVGElement>::nextSibling(*childElement)) {
-    LayoutObject* layoutObject = childElement->layoutObject();
-    if (!layoutObject)
-      continue;
-    const ComputedStyle* style = layoutObject->style();
-    if (!style || style->display() == EDisplay::None ||
-        style->visibility() != EVisibility::Visible)
+  for (const SVGElement& childElement :
+       Traversal<SVGElement>::childrenOf(*element())) {
+    const LayoutObject* layoutObject = childElement.layoutObject();
+    if (!layoutObject || layoutObject->styleRef().display() == EDisplay::None)
       continue;
     m_maskContentBoundaries.unite(
         layoutObject->localToSVGParentTransform().mapRect(
