fba6139dce71c5f2b2002bacb4175963d016a7c9
justincohen <justincohen@chromium.org>
2016-12-08 09:26:49 -0800

    Revert of Enable hermetic builds for iOS. (patchset #4 id:100001 of https://codereview.chromium.org/2546613004/ )
    Reason for revert:
    IndexError: list index out of range
    
    Original issue's description:
    > Enable hermetic builds for iOS.
    >
    > BUG=669094
    >
    > Committed: https://crrev.com/673b6870184dfb38f9b2dee2c8983ab5fbd172a6 [formerly 080de20fe53a35ec6b5ce89470261157f9389db3]
    > Cr-Commit-Position: refs/heads/master@{#437269}
    
    TBR=erikchen@chromium.org,kjellander@chromium.org,sdefresne@chromium.org,rsesek@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=669094
    
    Review-Url: https://codereview.chromium.org/2552383006
    Cr-Commit-Position: refs/heads/master@{#437273}
    
    Former-commit-id: 73c24cdb89cee4af2adb7021dbd05d4418525ef8

--- build/mac/should_use_hermetic_xcode.py
+++ build/mac/should_use_hermetic_xcode.py
@@ -4,11 +4,7 @@
 # found in the LICENSE file.
 
 """Prints "1" if Chrome targets should be built with hermetic xcode. Otherwise
-prints "0".
-
-Usage:
-  python should_use_hermetic_xcode.py <target_os>
-"""
+prints "0"."""
 
 import os
 import sys
@@ -19,8 +15,7 @@ def _IsCorpMachine():
 
 
 def main():
-  allow_corp = sys.argv[1] == 'mac' and _IsCorpMachine()
-  if os.environ.get('FORCE_MAC_TOOLCHAIN') or allow_corp:
+  if os.environ.get('FORCE_MAC_TOOLCHAIN') or _IsCorpMachine():
     return "1"
   else:
     return "0"
