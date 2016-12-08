16d03efb3dadc88f78cc3970a797f975c98d5bff
eroman <eroman@chromium.org>
2016-12-08 13:27:15 -0800

    Use UTC rather than local time for interpreting FTP listing dates.
    Both approaches are wrong, as the actual timezone is unknown.
    
    The main advantage of using UTC time is that it avoids a bug on Linux
    (issue 666535).
    
    BUG=641096
    
    Review-Url: https://codereview.chromium.org/2558143003
    Cr-Commit-Position: refs/heads/master@{#437346}
    
    Former-commit-id: 27f05072359d1862332f695ef9530f5f19da8b76

--- net/ftp/ftp_directory_listing_parser_unittest.h
+++ net/ftp/ftp_directory_listing_parser_unittest.h
@@ -49,7 +49,7 @@ class FtpDirectoryListingParserTest : public testing::Test {
     EXPECT_EQ(test_case.size, entry.size);
 
     base::Time::Exploded time_exploded;
-    entry.last_modified.LocalExplode(&time_exploded);
+    entry.last_modified.UTCExplode(&time_exploded);
 
     // Only test members displayed on the directory listing.
     EXPECT_EQ(test_case.year, time_exploded.year);
@@ -68,7 +68,7 @@ class FtpDirectoryListingParserTest : public testing::Test {
     mock_current_time_exploded.day_of_month = 15;
     mock_current_time_exploded.hour = 12;
     mock_current_time_exploded.minute = 45;
-    return base::Time::FromLocalExploded(mock_current_time_exploded);
+    return base::Time::FromUTCExploded(mock_current_time_exploded);
   }
 };
 
