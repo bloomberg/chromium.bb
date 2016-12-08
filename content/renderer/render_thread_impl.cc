27717dbf8ca786c287330db29879820dc0c27b95
tasak <tasak@google.com>
2016-12-07 23:16:27 -0800

    Fix crashes caused by GetMallocUsages().
    In GetMallocUsages, obtains number of heaps by invoking the first GetProcessHeaps(), and obtains heap handles by invoking the second GetProcessHeaps() with the number of heaps.
    However, the second GetProcessHeaps() may obtain smaller (or larger) number of heaps. So we need to see number of heaps returned by the second GetProcessHeaps().
    
    BUG=671459
    TBR=avi
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2562633002
    Cr-Commit-Position: refs/heads/master@{#437203}
    
    Former-commit-id: ac412ede3a8021afc3c34073d1cb2a9480f27cd1

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -1817,8 +1817,14 @@ static size_t GetMallocUsage() {
 
   size_t malloc_usage = 0;
   std::unique_ptr<HANDLE[]> heaps(new HANDLE[number_of_heaps]);
-  ::GetProcessHeaps(number_of_heaps, heaps.get());
-  for (size_t i = 0; i < number_of_heaps; i++) {
+  // If some heaps were gone between the first GetProcessHeaps and here,
+  // GetProcessHeaps obtains small number of heaps.
+  // If some new heaps were available between the first GetProcessHeaps and
+  // here, GetProcessHeaps returns larger number than number_of_heaps.
+  // So we need to see min(number_of_obtained_heaps, number_of_heaps).
+  DWORD number_of_obtained_heaps =
+      ::GetProcessHeaps(number_of_heaps, heaps.get());
+  for (size_t i = 0; i < number_of_heaps && i < number_of_obtained_heaps; i++) {
     PROCESS_HEAP_ENTRY heap_entry;
     ::HeapLock(heaps[i]);
     heap_entry.lpData = NULL;
