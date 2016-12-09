2869edab435e3820c2c3f4f33f34fb3257059f24
jschuh <jschuh@chromium.org>
2016-12-09 09:37:23 -0800

    Revert of Remove bad comment in HasSignBit (patchset #1 id:1 of https://codereview.chromium.org/2562643005/ )
    Reason for revert:
    The original change was not actually a performance improvement.
    
    Original issue's description:
    > Remove bad comment in HasSignBit
    >
    > Remove a bad comment left behind from crrev.com/437342
    > TBR=scottmg
    > R=scottmg
    > NOTRY=true
    >
    > Committed: https://crrev.com/5a38c30d00b8b6d59300ab7fe23cb4bab820c16f [formerly 6b15c686879f6747935a517f74f579f7ae46d797]
    > Cr-Commit-Position: refs/heads/master@{#437416}
    
    TBR=scottmg@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2559183004
    Cr-Commit-Position: refs/heads/master@{#437579}
    
    Former-commit-id: 28c208fcaf6848fc6b429bfeb362de83383302fd

--- base/numerics/safe_math_impl.h
+++ base/numerics/safe_math_impl.h
@@ -46,6 +46,7 @@ struct UnsignedOrFloatForSize<Numeric, false, true> {
 
 template <typename T>
 constexpr bool HasSignBit(T x) {
+  // Cast to unsigned since right shift on signed is undefined.
   return static_cast<typename std::make_signed<T>::type>(x) <
          static_cast<typename std::make_signed<T>::type>(0);
 }
