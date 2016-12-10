a2264e263614eeb956d440b84f75747f45da9c05
gmanikpure <gmanikpure@chromium.org>
2016-12-09 18:33:57 -0800

    [chromedriver] Drop support for M53 and add M56
    BUG=
    
    Review-Url: https://codereview.chromium.org/2515313003
    Cr-Commit-Position: refs/heads/master@{#437728}
    
    Former-commit-id: 807855a39ee75ed0b9f09bb025e07a831a1d34cb

--- chrome/test/chromedriver/chrome/version.cc
+++ chrome/test/chromedriver/chrome/version.cc
@@ -9,7 +9,7 @@
 namespace {
 
 // This variable must be able to be found and parsed by the upload script.
-const int kMinimumSupportedChromeVersion[] = {53, 0, 2785, 0};
+const int kMinimumSupportedChromeVersion[] = {54, 0, 2840, 0};
 
 }  // namespace
 
