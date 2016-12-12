0645648dd08942face3cad03bedcdd39e3a6f5fc
Emil Velikov <emil.l.velikov@gmail.com>
2016-12-12 17:50:46 +0000

    autogen.sh: set format.subjectPrefix and sendemail.to if needed
    Just set the rules automatically rather than asking each contributor to
    update thing locally.
    
    v2: Silence errors if run outside of git repo. (Eric)
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com
    Reviewed-by: Eric Engestrom <eric.engestrom@imgtec.com>

--- autogen.sh
+++ autogen.sh
@@ -9,6 +9,12 @@ cd "$srcdir"
 autoreconf --force --verbose --install || exit 1
 cd "$ORIGDIR" || exit $?
 
+git config --local --get format.subjectPrefix ||
+    git config --local format.subjectPrefix "PATCH libdrm" 2>/dev/null
+
+git config --local --get sendemail.to ||
+    git config --local sendemail.to "dri-devel@lists.freedesktop.org" 2>/dev/null
+
 if test -z "$NOCONFIGURE"; then
     "$srcdir"/configure "$@"
 fi
