ffcfd36c04cee28175b44d9f5fe6a910f124856d
dgn <dgn@chromium.org>
2016-12-13 15:10:59 -0800

    [gmscore] Stop commiting from the gmscore update script
    This was mostly inconvenient and not very comprehensible, as it
    would make the run fail because we need to update the config file
    first anyway. Remove the option completely.
    
    BUG=671699
    
    Review-Url: https://codereview.chromium.org/2570593003
    Cr-Commit-Position: refs/heads/master@{#438326}
    
    Former-commit-id: b11143e6a11e9f0ffdf755daac58d227b4461fd9

--- build/android/play_services/preprocess.py
+++ build/android/play_services/preprocess.py
@@ -38,7 +38,6 @@ import tempfile
 import zipfile
 
 sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
-import devil_chromium
 from play_services import utils
 from pylib.utils import argparse_utils
 
@@ -72,8 +71,6 @@ def main():
                       help='show the configuration file format help')
 
   args = parser.parse_args()
-
-  devil_chromium.Initialize()
 
   return ProcessGooglePlayServices(args.repository,
                                    args.out_dir,
