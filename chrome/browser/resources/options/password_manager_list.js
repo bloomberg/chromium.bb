f787cbbeeb76547960711b200c408d4ae5cd9753
kolos <kolos@chromium.org>
2016-12-14 00:56:39 -0800

    [Password Manager] Fix icons on chrome://settings/passwords
    Before this CL requests to the favicon service looked like the following:
    chrome://favicon/size/16@1x/origin/https://twitter.com/signup
    
    The "origin" parameter means that
    a) the path should be removed
    b) the "http" scheme should be added, if it is missed
    (see details here
    https://cs.chromium.org/chromium/src/chrome/browser/ui/webui/favicon_source.h).
    
    The favicon service fetches icons from the history backend. If the path has
    been removed from the URL, new URL might be absent in the history or even not
    exist on the given site. Therefore, we might fail to fetch icon.
    So, the removing the path doesn't make sense and the "origin/" parameter should
    be removed. The bookmarks manager also doesn't use this parameter.
    
    The parameter was added in vabr@'s CL
    (https://codereview.chromium.org/10079024) which has a comment about "origin/".
    The comment doesn't match to how the favicon service works.
    
    BUG=672483
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2568063002
    Cr-Commit-Position: refs/heads/master@{#438465}
    
    Former-commit-id: 6178699a5a0ba0c505e336e7caf449a32467cba0

--- chrome/browser/resources/options/password_manager_list.js
+++ chrome/browser/resources/options/password_manager_list.js
@@ -97,7 +97,7 @@ cr.define('options.passwordManager', function() {
     var urlDiv = cr.doc.createElement('div');
     urlDiv.className = 'favicon-cell url';
     urlDiv.setAttribute('title', getTitleForPasswordOrigin(item));
-    urlDiv.style.backgroundImage = cr.icon.getFavicon('origin/' + item.url);
+    urlDiv.style.backgroundImage = cr.icon.getFavicon(item.url);
 
     item.urlLink = createUrlLink(item, urlDiv);
     urlDiv.appendChild(item.urlLink);
