6d728a37ecd7a4a693d015e9d22dd71c84586a94
tkent <tkent@chromium.org>
2016-12-14 01:56:22 -0800

    Form validation: Do not show validation bubble if the page is invisible.
    BUG=673163
    
    Review-Url: https://codereview.chromium.org/2572813003
    Cr-Commit-Position: refs/heads/master@{#438476}
    
    Former-commit-id: a8e17a3031b6ad69c399e5e04dd0084e577097fc

--- third_party/WebKit/Source/core/html/HTMLFormControlElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLFormControlElement.cpp
@@ -452,7 +452,7 @@ void HTMLFormControlElement::findCustomValidationMessageTextDirection(
 
 void HTMLFormControlElement::updateVisibleValidationMessage() {
   Page* page = document().page();
-  if (!page)
+  if (!page || !page->isPageVisible())
     return;
   String message;
   if (layoutObject() && willValidate())
