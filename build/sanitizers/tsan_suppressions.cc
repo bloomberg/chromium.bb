a4da5e1a9df7f28affc04f6562c01474a3925209
gab <gab@chromium.org>
2016-12-14 21:00:33 -0800

    Suppress TSAN race in TimerTest while working on complex fix.
    Fix coming up @ https://codereview.chromium.org/2491613004/ but
    suppressing race in mean time to make TSAN bot green again
    (issue 672860).
    
    BUG=587199
    TEST=TimerTest.OneShotTimer_CustomTaskRunner no longer reported as failure by linux_chromium_tsan_rel_ng.
    NO_DEPENDENCY_CHECKS=true
    
    Review-Url: https://codereview.chromium.org/2576503002
    Cr-Commit-Position: refs/heads/master@{#438749}
    
    Former-commit-id: 243a13726f5baf1a3f5fc9dfa0f4ee651fd87ab4

--- build/sanitizers/tsan_suppressions.cc
+++ build/sanitizers/tsan_suppressions.cc
@@ -270,6 +270,9 @@ char kTSanDefaultSuppressions[] =
 // http://crbug.com/638583
 "race:webrtc/modules/audio_processing/aec/aec_rdft.cc\n"
 
+// http://crbug.com/587199
+"race:base::TimerTest_OneShotTimer_CustomTaskRunner_Test::TestBody\n"
+
 // End of suppressions.
 ;  // Please keep this semicolon.
 
