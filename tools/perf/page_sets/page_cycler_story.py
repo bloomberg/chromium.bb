7a8da476c2f9c65c55635ff8264e775067a99c7e
charliea <charliea@chromium.org>
2016-12-16 14:18:01 -0800

    Further increase the navigation timeout to 180s for page cycler stories
    The increase done in https://codereview.chromium.org/2577243002 was
    effective in decreasing the number of failing 3G/Android One stories
    from 23 to 1, and hopefully this increase gets that number to zero.
    
    BUG=673461
    
    Review-Url: https://codereview.chromium.org/2576403005
    Cr-Commit-Position: refs/heads/master@{#439214}
    
    Former-commit-id: 33dea85c0961db70473126d594815c5f1739bc84

--- tools/perf/page_sets/page_cycler_story.py
+++ tools/perf/page_sets/page_cycler_story.py
@@ -8,6 +8,7 @@ from telemetry.page import shared_page_state
 
 
 _TTI_WAIT_TIME = 10
+_NAVIGATION_TIMEOUT = 180
 _WEB_CONTENTS_TIMEOUT = 180
 
 class PageCyclerStory(page.Page):
@@ -25,7 +26,7 @@ class PageCyclerStory(page.Page):
     url = self.file_path_url_with_scheme if self.is_file else self.url
     action_runner.Navigate(url,
                            self.script_to_evaluate_on_commit,
-                           timeout_in_seconds = 120)
+                           timeout_in_seconds=_NAVIGATION_TIMEOUT)
 
   def RunPageInteractions(self, action_runner):
     action_runner.tab.WaitForDocumentReadyStateToBeComplete(
