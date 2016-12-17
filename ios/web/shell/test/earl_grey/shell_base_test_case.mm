1ca4b77f0b0e0a7eb11aee4bf7aa8cc860403438
baxley <baxley@chromium.org>
2016-12-16 16:59:49 -0800

    Don't run web shell EarlGrey tests if a system alert is present.
    This isn't a case in which the user can interact with the UI,
    witout dismissing/accepting the alert, so our tests shouldn't
    run either.
    
    BUG=654085
    
    Review-Url: https://codereview.chromium.org/2584743004
    Cr-Commit-Position: refs/heads/master@{#439267}
    
    Former-commit-id: f2b2a809eace73ef64e3f81ae4f1af5d8f44478e

--- ios/web/shell/test/earl_grey/shell_base_test_case.mm
+++ ios/web/shell/test/earl_grey/shell_base_test_case.mm
@@ -18,6 +18,24 @@ using web::webViewContainingText;
 
 @implementation ShellBaseTestCase
 
+// Overrides |testInvocations| to skip all tests if a system alert view is
+// shown, since this isn't a case a user would encounter (i.e. they would
+// dismiss the alert first).
++ (NSArray*)testInvocations {
+  // TODO(crbug.com/654085): Simply skipping all tests isn't the best way to
+  // handle this, it would be better to have something that is more obvious
+  // on the bots that this is wrong, without making it look like test flake.
+  NSError* error = nil;
+  [[EarlGrey selectElementWithMatcher:grey_systemAlertViewShown()]
+      assertWithMatcher:grey_nil()
+                  error:&error];
+  if (error != nil) {
+    NSLog(@"System alert view is present, so skipping all tests!");
+    return @[];
+  }
+  return [super testInvocations];
+}
+
 // Set up called once for the class.
 + (void)setUp {
   [super setUp];
