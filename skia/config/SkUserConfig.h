b1c02742c57bb1db679c097c2aa3b4581d8b76a1
reed <reed@google.com>
2016-12-19 11:23:39 -0800

    add flag for future skia CL
    see https://skia-review.googlesource.com/c/6206/
    
    BUG=
    NOTRY=True
    
    Review-Url: https://codereview.chromium.org/2583023003
    Cr-Commit-Position: refs/heads/master@{#439521}
    
    Former-commit-id: 4dcfb81a0424cb7642c5f44e80196b9c6fe1c2bf

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -246,6 +246,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_USE_LEGACY_DISTANCE_FIELDS
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_CLIPOP_EXOTIC_NAMES
+#   define SK_SUPPORT_LEGACY_CLIPOP_EXOTIC_NAMES
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
