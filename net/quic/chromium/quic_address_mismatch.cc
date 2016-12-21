90db57df71532bfd78206c230756ae67d890403d
rch <rch@chromium.org>
2016-12-20 22:21:28 -0800

    Move quic_address_mismatch from net/quic/core to net/quic/chromium and remove unused quic_stream_sequencer_buffer_interface.h.
    Review-Url: https://codereview.chromium.org/2593923002
    Cr-Commit-Position: refs/heads/master@{#440041}
    
    Former-commit-id: de02335ec30070bb473ec3d2360e2e1de8bfb0d5

--- net/quic/chromium/quic_address_mismatch.cc
+++ net/quic/chromium/quic_address_mismatch.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "net/quic/core/quic_address_mismatch.h"
+#include "net/quic/chromium/quic_address_mismatch.h"
 
 #include "base/logging.h"
 #include "net/base/ip_address.h"
--- net/quic/chromium/quic_address_mismatch_test.cc
+++ net/quic/chromium/quic_address_mismatch_test.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "net/quic/core/quic_address_mismatch.h"
+#include "net/quic/chromium/quic_address_mismatch.h"
 
 #include "testing/gtest/include/gtest/gtest.h"
 
