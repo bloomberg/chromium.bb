3a24353d93f16c6dc56b1b13292638d180cf9fcf
krasin <krasin@chromium.org>
2016-12-20 17:10:35 -0800

    Fix use-after-free issue in VideoDecoderResourceTest.
    Local variables responsible for shared memory go out of scope
    right after their initialization and then accessed by the saved
    references to the stack space which is already potentially reused
    for other local variables.
    
    The bug was found by AddressSanitizer with use-after-free check
    enabled. It's currently being rolled out into Chrome, and this CL
    is a part of a larger cleanup of existing failures.
    
    BUG=649897
    TBR=piman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2593873002
    Cr-Commit-Position: refs/heads/master@{#439961}
    
    Former-commit-id: e9416e5d3947a57e2217a8aa63535f9cf9c4ca15

--- ppapi/proxy/video_decoder_resource_unittest.cc
+++ ppapi/proxy/video_decoder_resource_unittest.cc
@@ -147,13 +147,15 @@ class VideoDecoderResourceTest : public PluginProxyTest {
         &sink(), PpapiHostMsg_VideoDecoder_GetShm::ID, PP_OK, shm_msg_reply);
     sink().AddFilter(&shm_msg_handler);
 
+    std::unique_ptr<base::SharedMemoryHandle> shm_handle;
+    std::unique_ptr<SerializedHandle> serialized_handle;
     base::SharedMemory shm;
     if (expected_shm_msg) {
       shm.CreateAnonymous(kShmSize);
-      base::SharedMemoryHandle shm_handle;
-      shm.ShareToProcess(base::GetCurrentProcessHandle(), &shm_handle);
-      SerializedHandle serialized_handle(shm_handle, kShmSize);
-      shm_msg_handler.set_serialized_handle(&serialized_handle);
+      shm_handle.reset(new base::SharedMemoryHandle());
+      shm.ShareToProcess(base::GetCurrentProcessHandle(), shm_handle.get());
+      serialized_handle.reset(new SerializedHandle(*shm_handle, kShmSize));
+      shm_msg_handler.set_serialized_handle(serialized_handle.get());
     }
 
     memset(decode_buffer_, 0x55, kDecodeBufferSize);
