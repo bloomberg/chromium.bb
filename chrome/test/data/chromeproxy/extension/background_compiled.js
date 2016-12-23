ce23b11525e977ddc332c8a80f2af633eb65464b
bustamante <bustamante@chromium.org>
2016-12-23 14:07:48 -0800

    Add data use ascription test case to new framework
    This CL updates the extension in chrome/test/data/... with the latest
    public version.  It's all generated code and doesn't need reviewing.
    
    The only code that does need reviewing is the data_use.py module.
    
    BUG=547967
    
    Review-Url: https://codereview.chromium.org/2598483003
    Cr-Commit-Position: refs/heads/master@{#440656}
    
    Former-commit-id: 2f63ebdd3fe91e94c29c35b15236f74a4b92cdd5

--- chrome/test/data/chromeproxy/extension/background_compiled.js
+++ chrome/test/data/chromeproxy/extension/background_compiled.js
@@ -1,43 +1,4 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-chrome.runtime.onInstalled.addListener(
-  function(a){
-    console.log("chrome.runtime.onInstalled details: "+JSON.stringify(a));
-    console.log("typeof chrome.dataReductionProxy: "+
-                typeof chrome.dataReductionProxy);
-    if("undefined"!=typeof chrome.dataReductionProxy)
-      if("install"==a.reason)chrome.dataReductionProxy.spdyProxyEnabled.set(
-                               {value:!0}),
-      "clearDataSavings"in chrome.dataReductionProxy&&
-        chrome.dataReductionProxy.clearDataSavings(),
-      showEnabledIcon();
-      else if("update"==a.reason||"chrome_update"==a.reason)
-        showEnabledIconIfProxyOn(),
-        chrome.storage.local.get("user_enabled_proxy",
-        function(a){"user_enabled_proxy"in a&&(
-          chrome.dataReductionProxy.spdyProxyEnabled.set({value:!0}),
-          showEnabledIcon(),
-          chrome.storage.local.remove("user_enabled_proxy"))})});
-      chrome.runtime.onStartup.addListener(function(){
-      showEnabledIconIfProxyOn()});chrome.tabs.onCreated.addListener(
-      function(a){setTimeout(function(){a.incognito&&
-      chrome.browserAction.setIcon({tabId:a.id,path:{
-        19:"./images/proxy-disabled19.png",
-        38:"./images/proxy-disabled38.png"}})},500)});
-function showEnabledIconIfProxyOn(){
-  "undefined"!=typeof chrome.dataReductionProxy&&(
-    console.log("Calling spdyProxyEnabled.get"),
-    chrome.dataReductionProxy.spdyProxyEnabled.get({},
-      function(a){
-        console.log("chrome.dataReductionProxy.spdyProxyEnabled.get: "
-                    +JSON.stringify(a));
-        (a="value"in a&&a.value)&&showEnabledIcon()
-      })
-  )
-};
-function showEnabledIcon(){
-  console.log("Calling chrome.browserAction.setIcon");
-  chrome.browserAction.setIcon({path:{19:"./images/proxy-enabled19.webp",
-                                      38:"./images/proxy-enabled38.webp"}})
-};
+"undefined"!=typeof chrome.dataReductionProxy&&"undefined"!=typeof chrome.dataReductionProxy.dataUsageReportingEnabled&&(chrome.dataReductionProxy.dataUsageReportingEnabled.set({value:!0}),b());
+chrome.runtime.onInstalled.addListener(function(a){console.log("chrome.runtime.onInstalled details: "+JSON.stringify(a));console.log("typeof chrome.dataReductionProxy: "+typeof chrome.dataReductionProxy);if("undefined"!=typeof chrome.dataReductionProxy&&"undefined"!=typeof chrome.dataReductionProxy.dataUsageReportingEnabled)if(chrome.dataReductionProxy.dataUsageReportingEnabled.set({value:!0}),"install"==a.reason)chrome.dataReductionProxy.spdyProxyEnabled.set({value:!0}),c();else if("update"==a.reason||
+"chrome_update"==a.reason)b(),chrome.storage.local.get("user_enabled_proxy",function(a){"user_enabled_proxy"in a&&(chrome.dataReductionProxy.spdyProxyEnabled.set({value:!0}),c(),chrome.storage.local.remove("user_enabled_proxy"))})});chrome.runtime.onStartup.addListener(function(){b()});
+function b(){"undefined"!=typeof chrome.dataReductionProxy&&(console.log("Calling spdyProxyEnabled.get"),chrome.dataReductionProxy.spdyProxyEnabled.get({},function(a){console.log("chrome.dataReductionProxy.spdyProxyEnabled.get: "+JSON.stringify(a));(a="value"in a&&a.value)&&c()}))}function c(){console.log("Calling chrome.browserAction.setIcon");chrome.browserAction.setIcon({path:{19:"./images/proxy-enabled19.webp",38:"./images/proxy-enabled38.webp"}})};
