e0e6d06310fe084012a9e8d49db9e153e939c68c
mikhail.pozdnyakov <mikhail.pozdnyakov@intel.com>
2016-12-23 10:58:20 -0800

    [Sensors] Reland: Reland: Align sensor reading updates and 'onchange' notification with rAF.
    For all sensors new reading values are read and 'onchange' notfication is send from rAF callbacks, thus avoiding possible Critical Rendering Path interruption.
    
    Before this change a timers were used and this could unnecessarily drain CPU and battery.
    
    BUG=668052
    BUG=606766
    
    Committed: https://crrev.com/367787adbe4e76b6be9b59c89e2c17fc7c7d6745 [formerly 6b071fe7dc3bd64a2914eadd5c67b483d064a6cb]
    Cr-Commit-Position: refs/heads/master@{#439467}
    
    patch from issue 2551223003 at patchset 140001 (http://crrev.com/2551223003#ps140001)
    
    Committed: https://crrev.com/3c94dc13b14e40ad0797ec4bf1cb38e21246410a [formerly 5f2cde61f6ff4a764b9b1c7d74a24ef6de815835]
    Cr-Commit-Position: refs/heads/master@{#440208}
    
    patch from issue 2589283003 at patchset 60001 (http://crrev.com/2589283003#ps60001)
    
    Review-Url: https://codereview.chromium.org/2604483004
    Cr-Commit-Position: refs/heads/master@{#440650}
    
    Former-commit-id: a14c24ea3dd1eb6c6bcb72d1ffa1b1cece4c3257

--- third_party/WebKit/LayoutTests/sensor/resources/sensor-helpers.js
+++ third_party/WebKit/LayoutTests/sensor/resources/sensor-helpers.js
@@ -39,6 +39,7 @@ function sensor_mocks(mojo) {
         this.reporting_mode_ = reportingMode;
         this.sensor_reading_timer_id_ = null;
         this.update_reading_function_ = null;
+        this.reading_updates_count_ = 0;
         this.suspend_called_ = null;
         this.resume_called_ = null;
         this.add_configuration_called_ = null;
@@ -62,6 +63,9 @@ function sensor_mocks(mojo) {
         return Promise.resolve({frequency: 5});
       }
 
+      reading_updates_count() {
+        return this.reading_updates_count_;
+      }
       // Adds configuration for the sensor and starts reporting fake data
       // through update_reading_function_ callback.
       addConfiguration(configuration) {
@@ -126,6 +130,7 @@ function sensor_mocks(mojo) {
         this.stopReading();
 
         this.expects_modified_reading_ = false;
+        this.reading_updates_count_ = 0;
         this.start_should_fail_ = false;
         this.update_reading_function_ = null;
         this.active_sensor_configurations_ = [];
@@ -193,13 +198,17 @@ function sensor_mocks(mojo) {
 
       startReading() {
         if (this.update_reading_function_ != null) {
+          this.stopReading();
           let max_frequency_used =
               this.active_sensor_configurations_[0].frequency;
           let timeout = (1 / max_frequency_used) * 1000;
           this.sensor_reading_timer_id_ = window.setInterval(() => {
-            if (this.update_reading_function_)
+            if (this.update_reading_function_) {
               this.update_reading_function_(this.buffer_,
-                                            this.expects_modified_reading_);
+                                            this.expects_modified_reading_,
+                                            this.reading_updates_count_);
+              this.reading_updates_count_++;
+            }
             if (this.reporting_mode_ === sensor.ReportingMode.ON_CHANGE) {
               this.client_.sensorReadingChanged();
             }
---
+++ third_party/WebKit/Source/modules/sensor/SensorReadingUpdater.h
@@ -0,0 +1,39 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SensorReadingUpdater_h
+#define SensorReadingUpdater_h
+
+#include "device/generic_sensor/public/interfaces/sensor_provider.mojom-blink.h"
+#include "platform/heap/Handle.h"
+
+namespace blink {
+
+class SensorProxy;
+
+// This class encapsulates sensor reading update notification logic.
+class SensorReadingUpdater : public GarbageCollected<SensorReadingUpdater> {
+ public:
+  static SensorReadingUpdater* create(SensorProxy*,
+                                      device::mojom::blink::ReportingMode);
+
+  virtual void start();
+
+  DECLARE_VIRTUAL_TRACE();
+
+ protected:
+  explicit SensorReadingUpdater(SensorProxy*);
+  void enqueueAnimationFrameTask();
+  virtual void onAnimationFrameInternal() = 0;
+
+  Member<SensorProxy> m_sensorProxy;
+  bool m_hasPendingAnimationFrameTask;
+
+ private:
+  void onAnimationFrame();
+};
+
+}  // namespace blink
+
+#endif  // SensorReadingUpdater_h
