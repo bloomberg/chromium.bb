786487e622b2d55630bda1de8884353c33814d10
dbeam <dbeam@chromium.org>
2016-12-22 18:46:48 -0800

    Disable flaky test OmniboxViewViewsTest.SelectionClipboard
    TBR=msw@chromium.org
    BUG=676746
    
    Review-Url: https://codereview.chromium.org/2597403002
    Cr-Commit-Position: refs/heads/master@{#440586}
    
    Former-commit-id: dea296de1bb5b61515994926d3ece3165274ffc2

--- chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_views_browsertest.cc
@@ -192,7 +192,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, SelectAllOnClick) {
 }
 
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, SelectionClipboard) {
+IN_PROC_BROWSER_TEST_F(OmniboxViewViewsTest, DISABLED_SelectionClipboard) {
   OmniboxView* omnibox_view = NULL;
   ASSERT_NO_FATAL_FAILURE(GetOmniboxViewForBrowser(browser(), &omnibox_view));
   omnibox_view->SetUserText(base::ASCIIToUTF16("http://www.google.com/"));
