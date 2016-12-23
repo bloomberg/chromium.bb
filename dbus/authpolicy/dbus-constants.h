05cee9da77790110786e6211f75c49d0c7e26ae8
Lutz Justen <ljusten@chromium.org>
2016-12-23 16:32:38 +0100

    authpolicy: Update error enum
    Also combine all 4 error enum types into one enum for simplicity.
    Keep the old enums for now until they are removed from Chrome.
    
    BUG=chromium:676648
    TEST=Compiles, tested with custom test code, ran unit test.
    
    Change-Id: I0402bf0ce341cb03b3156a6aedb1028522cecc28
    Reviewed-on: https://chromium-review.googlesource.com/422769
    Commit-Ready: Roman Sorokin <rsorokin@chromium.org>
    Tested-by: Lutz Justen <ljusten@chromium.org>
    Tested-by: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Roman Sorokin <rsorokin@chromium.org>

--- dbus/authpolicy/dbus-constants.h
+++ dbus/authpolicy/dbus-constants.h
@@ -6,6 +6,7 @@
 #define SYSTEM_API_DBUS_AUTHPOLICY_DBUS_CONSTANTS_H_
 
 namespace authpolicy {
+
 const char kAuthPolicyInterface[] = "org.chromium.AuthPolicy";
 const char kAuthPolicyServicePath[] = "/org/chromium/AuthPolicy";
 const char kAuthPolicyServiceName[] = "org.chromium.AuthPolicy";
@@ -14,6 +15,33 @@ const char kAuthPolicyAuthenticateUser[] = "AuthenticateUser";
 const char kAuthPolicyJoinADDomain[] = "JoinADDomain";
 const char kAuthPolicyRefreshUserPolicy[] = "RefreshUserPolicy";
 const char kAuthPolicyRefreshDevicePolicy[] = "RefreshDevicePolicy";
+
+// Enum values.
+enum ErrorType {
+  ERROR_NONE = 0,               // Everything is A-OK!
+  ERROR_UNKNOWN,                // Unspecified error.
+  ERROR_DBUS_FAILURE,           // Unspecified D-Bus error.
+  ERROR_PARSE_UPN_FAILED,       // Badly formatted user principal name.
+  ERROR_BAD_USER_NAME,          // Auth failed because of bad user name.
+  ERROR_BAD_PASSWORD,           // Auth failed because of bad password.
+  ERROR_PASSWORD_EXPIRED,       // Auth failed because of expired password.
+  ERROR_CANNOT_RESOLVE_KDC,     // Auth failed because of bad realm or network.
+  ERROR_KINIT_FAILED,           // kinit exited with unspecified error.
+  ERROR_NET_FAILED,             // net exited with unspecified error.
+  ERROR_SMBCLIENT_FAILED,       // smdclient exited with unspecified error.
+  ERROR_PARSE_FAILED,           // authpolicy_parser exited with unknown error.
+  ERROR_PARSE_PREG_FAILED,      // Parsing GPOs failed.
+  ERROR_BAD_GPOS,               // GPO data is bad.
+  ERROR_LOCAL_IO,               // Some local IO operation failed.
+  ERROR_NOT_JOINED,             // Machine is not joined to AD domain yet.
+  ERROR_NOT_LOGGED_IN,          // User is not logged in yet.
+  ERROR_STORE_POLICY_FAILED,    // Failed to send policy to Session Manager.
+  // Should be the last.
+  ERROR_COUNT,
+};
+
+// TODO(ljusten): 2016/12/26: Remove 4 enums below as soon as Chrome code has
+// been switched to new enum above.
 
 // Enum values.
 enum ADJoinErrorType {
@@ -47,6 +75,7 @@ enum RefreshDevicePolicyErrorType {
   // Should be the last.
   REFRESH_DEVICE_POLICY_ERROR_COUNT = 3,
 };
+
 }  // namespace authpolicy
 
 #endif  // SYSTEM_API_DBUS_AUTHPOLICY_DBUS_CONSTANTS_H_
