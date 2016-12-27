d998a007ad91ff9fec734926db9f8d891eec028f
Nathan E. Egge <negge@mozilla.com>
2016-12-27 14:50:48 -0500

    Delete unused laplace decoder functions.
    Delete the unused od_laplace_decode(), od_laplace_decode_vector(), and
     laplace_decode_vector_delta() functions.
    
    Change-Id: Iec581e8cdb0bc9cac9199c09486891500c707c03

--- av1/decoder/pvq_decoder.h
+++ av1/decoder/pvq_decoder.h
@@ -22,20 +22,11 @@ void aom_decode_band_pvq_splits(aom_reader *r, od_pvq_codeword_ctx *adapt,
 
 #if OD_ACCOUNTING
 # define laplace_decode_special(dec, decay, max, str) od_laplace_decode_special_(dec, decay, max, str)
-# define laplace_decode(dec, ex_q8, k, str) od_laplace_decode_(dec, ex_q8, k, str)
-#define laplace_decode_vector(dec, y, n, k, curr, means, str) od_laplace_decode_vector_(dec, y, n, k, curr, means, str)
 #else
 # define laplace_decode_special(dec, decay, max, str) od_laplace_decode_special_(dec, decay, max)
-# define laplace_decode(dec, ex_q8, k, str) od_laplace_decode_(dec, ex_q8, k)
-#define laplace_decode_vector(dec, y, n, k, curr, means, str) od_laplace_decode_vector_(dec, y, n, k, curr, means)
 #endif
 
 int od_laplace_decode_special_(od_ec_dec *dec, unsigned decay, int max OD_ACC_STR);
-int od_laplace_decode_(od_ec_dec *dec, unsigned ex_q8, int k OD_ACC_STR);
-void od_laplace_decode_vector_(od_ec_dec *dec, od_coeff *y, int n, int k,
-                                  int32_t *curr, const int32_t *means
-                                  OD_ACC_STR);
-
 
 void od_pvq_decode(daala_dec_ctx *dec, od_coeff *ref, od_coeff *out, int q0,
  int pli, int bs, const od_val16 *beta, int robust, int is_keyframe,
