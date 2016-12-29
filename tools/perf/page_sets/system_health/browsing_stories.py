7c277b14360e60f15e21740edf4df22f4651394d
nednguyen <nednguyen@google.com>
2016-12-29 10:41:54 -0800

    Reenable Flipboard story on Mac
    BUG=662021, 663025
    TBR=perejzu@chromium.org
    
    Review-Url: https://codereview.chromium.org/2602073002
    Cr-Commit-Position: refs/heads/master@{#440974}
    
    Former-commit-id: 889401456c5adbcb91f996f55b619413241a57d4

--- tools/perf/page_sets/system_health/browsing_stories.py
+++ tools/perf/page_sets/system_health/browsing_stories.py
@@ -129,7 +129,6 @@ class FlipboardMobileStory(_NewsBrowsingStory):
     return possible_browser.platform.IsSvelte()  # crbug.com/668097
 
 
-@decorators.Disabled('mac') # crbug.com/663025
 class FlipboardDesktopStory(_NewsBrowsingStory):
   NAME = 'browse:news:flipboard'
   URL = 'https://flipboard.com/explore'
