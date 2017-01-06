3c5f905f2408097f3f025711236ec4defb8ac5cc
sky <sky@chromium.org>
2017-01-06 13:53:27 -0800

    Makes mash use ChromeNativeAppWindowViewsAura
    Mash should really use ChromeNativeAppWindowViewsAuraAsh, but that
    depends upon a bunch of types in ash. So, make mash use
    ChromeNativeAppWindowViewsAura, which isn't perfect, but at least it
    doesn't crash.
    
    BUG=679028
    TEST=none
    R=msw@chromium.org
    
    Review-Url: https://codereview.chromium.org/2617923004
    Cr-Commit-Position: refs/heads/master@{#442067}
    
    Former-commit-id: 148ae8075bfdc22e122b3307d3344337db12d00c

--- chrome/browser/ui/views/apps/chrome_app_window_client_views_chromeos.cc
+++ chrome/browser/ui/views/apps/chrome_app_window_client_views_chromeos.cc
@@ -5,13 +5,18 @@
 #include "chrome/browser/ui/apps/chrome_app_window_client.h"
 
 #include "chrome/browser/ui/views/apps/chrome_native_app_window_views_aura_ash.h"
+#include "services/service_manager/runner/common/client_util.h"
 
 // static
 extensions::NativeAppWindow* ChromeAppWindowClient::CreateNativeAppWindowImpl(
     extensions::AppWindow* app_window,
     const extensions::AppWindow::CreateParams& params) {
-  ChromeNativeAppWindowViewsAuraAsh* window =
-      new ChromeNativeAppWindowViewsAuraAsh;
+  // TODO: Mash should use ChromeNativeAppWindowViewsAuraAsh, but
+  // it can't because of dependencies on ash. http://crbug.com/679028.
+  ChromeNativeAppWindowViewsAura* window =
+      service_manager::ServiceManagerIsRemote()
+          ? new ChromeNativeAppWindowViewsAura
+          : new ChromeNativeAppWindowViewsAuraAsh;
   window->Init(app_window, params);
   return window;
 }
