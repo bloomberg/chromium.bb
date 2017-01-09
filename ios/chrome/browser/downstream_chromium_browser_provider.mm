62417d0d059608a20950556ffa47291ab9225dfc
sdefresne <sdefresne@chromium.org>
2017-01-09 06:59:07 -0800

    Remove obsolete class DownstreamChromiumBrowserProvider.
    This class is no longer used nor the factory function, so remove
    them both and cleanup BUILD.gn files.
    
    Remove obsolete public_deps that were there for the upstreaming
    process and are no longer required.
    
    BUG=676278
    
    Review-Url: https://codereview.chromium.org/2613063002
    Cr-Commit-Position: refs/heads/master@{#442249}
    
    Former-commit-id: a87330e6389b26bdfe5623b677f79be4bccb7f92

--- ios/chrome/browser/downstream_chromium_browser_provider.mm
+++
@@ -1,9 +0,0 @@
-// Copyright 2016 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#import "ios/chrome/browser/downstream_chromium_browser_provider.h"
-
-DownstreamChromiumBrowserProvider::DownstreamChromiumBrowserProvider() {}
-
-DownstreamChromiumBrowserProvider::~DownstreamChromiumBrowserProvider() {}
