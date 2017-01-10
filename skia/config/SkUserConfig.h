e50000a2eebb6dac27f300f6a87073f7a2ad8b1a
liyuqian <liyuqian@google.com>
2017-01-10 08:26:23 -0800

    Define SK_SUPPORT_LEGACY_QUAD_SHIFT
    This guards against our improvement over quadratic edges in non-AA
    cases.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2613213004
    Cr-Commit-Position: refs/heads/master@{#442608}
    
    Former-commit-id: 2465bc703dd202541a42d07363568644f79a4a83

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -234,6 +234,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_CLIPOP_EXOTIC_NAMES
 #endif
 
+#ifndef    SK_SUPPORT_LEGACY_QUAD_SHIFT
+#   define SK_SUPPORT_LEGACY_QUAD_SHIFT
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
