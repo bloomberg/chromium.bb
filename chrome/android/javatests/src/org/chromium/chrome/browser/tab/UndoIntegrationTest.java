b164d96a53dfdd133553253f949e61ed36205d89
hanxi <hanxi@chromium.org>
2017-01-10 08:52:29 -0800

    Marking UndoIntegrationTest#testAddNewContentsFromClosingTab as flaky.
    TBR=dfalcantara,newt
    BUG=679480
    
    Review-Url: https://codereview.chromium.org/2623653004
    Cr-Commit-Position: refs/heads/master@{#442613}
    
    Former-commit-id: beb68480169299425f03363a52113903a0b07fa9

--- chrome/android/javatests/src/org/chromium/chrome/browser/tab/UndoIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/tab/UndoIntegrationTest.java
@@ -9,6 +9,7 @@ import static org.chromium.base.test.util.Restriction.RESTRICTION_TYPE_NON_LOW_E
 import android.support.test.filters.LargeTest;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.base.test.util.UrlUtils;
@@ -52,6 +53,7 @@ public class UndoIntegrationTest extends ChromeTabbedActivityTestBase {
      * @throws InterruptedException
      * @throws TimeoutException
      */
+    @FlakyTest(message = "https://crbug.com/679480")
     @LargeTest
     @Restriction(RESTRICTION_TYPE_NON_LOW_END_DEVICE)
     @RetryOnFailure
