655ab80380f7bd66afad610da18e52a9a1ed0fd5
yzshen <yzshen@chromium.org>
2017-01-11 14:29:34 -0800

    Change web_ui testing code to avoid using JS router/proxyClass directly.
    The previous code didn't validate incoming messages at all.
    
    BUG=579646
    
    Review-Url: https://codereview.chromium.org/2613813002
    Cr-Commit-Position: refs/heads/master@{#443020}
    
    Former-commit-id: b59d006f624c34344e9835ed99d8279d4aa5701e

--- content/test/data/web_ui_mojo.js
+++ content/test/data/web_ui_mojo.js
@@ -3,16 +3,15 @@
 // found in the LICENSE file.
 
 define('main', [
-    'mojo/public/js/router',
+    'mojo/public/js/bindings',
     'content/test/data/web_ui_test_mojo_bindings.mojom',
     'content/public/renderer/frame_interfaces',
-], function (router, bindings, interfaceProvider) {
+], function (bindings, webUITest, interfaceProvider) {
   var browserTarget;
 
   return function() {
-    browserTarget = new bindings.BrowserTarget.proxyClass(
-        new router.Router(
-            interfaceProvider.getInterface(bindings.BrowserTarget.name)));
+    browserTarget = new webUITest.BrowserTargetPtr(
+        interfaceProvider.getInterface(webUITest.BrowserTarget.name));
 
     browserTarget.start().then(function() {
       browserTarget.stop();
