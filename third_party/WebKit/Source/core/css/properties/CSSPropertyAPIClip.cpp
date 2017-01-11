39ce3afaa252e7fb262f334f4f0b240eeab3a81b
aazzam <aazzam@google.com>
2017-01-10 21:24:47 -0800

    Implements CSSPropertyAPI for the clip property.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API. This patch removes
    CSSPropertyClip from the switch statement in parseSingleValue,
    and calls the API instead.
    
    A function pointer to the parseSingleValue function from the API for the
    page property is stored in a CSSPropertyDescriptor, and is called from
    CSSPropertyParser.
    
    This patch:
    - Adds CSSPropertyAPIClip.cpp to the BUILD.gn file.
    - Adds api_class flag to CSSProperties.in, which indicates that
      CSSPropertyAPIClip.h is generated.
    - Moves the parsing logic for page from CSSPropertyParser.cpp to
      CSSPropertyAPIClip.cpp, which implements CSSPropertyAPI.h.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2614783003
    Cr-Commit-Position: refs/heads/master@{#442810}
    
    Former-commit-id: 3f6fb5a1899d4ad52e4997f0f309c221497fe561

---
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIClip.cpp
@@ -0,0 +1,58 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSPropertyAPIClip.h"
+
+#include "core/css/CSSQuadValue.h"
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+namespace blink {
+
+namespace {
+
+CSSValue* consumeClipComponent(CSSParserTokenRange& range,
+                               CSSParserMode cssParserMode) {
+  if (range.peek().id() == CSSValueAuto)
+    return CSSPropertyParserHelpers::consumeIdent(range);
+  return CSSPropertyParserHelpers::consumeLength(
+      range, cssParserMode, ValueRangeAll,
+      CSSPropertyParserHelpers::UnitlessQuirk::Allow);
+}
+}
+
+const CSSValue* CSSPropertyAPIClip::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext& context) {
+  if (range.peek().id() == CSSValueAuto)
+    return CSSPropertyParserHelpers::consumeIdent(range);
+
+  if (range.peek().functionId() != CSSValueRect)
+    return nullptr;
+
+  CSSParserTokenRange args = CSSPropertyParserHelpers::consumeFunction(range);
+  // rect(t, r, b, l) || rect(t r b l)
+  CSSValue* top = consumeClipComponent(args, context.mode());
+  if (!top)
+    return nullptr;
+  bool needsComma =
+      CSSPropertyParserHelpers::consumeCommaIncludingWhitespace(args);
+  CSSValue* right = consumeClipComponent(args, context.mode());
+  if (!right ||
+      (needsComma &&
+       !CSSPropertyParserHelpers::consumeCommaIncludingWhitespace(args)))
+    return nullptr;
+  CSSValue* bottom = consumeClipComponent(args, context.mode());
+  if (!bottom ||
+      (needsComma &&
+       !CSSPropertyParserHelpers::consumeCommaIncludingWhitespace(args)))
+    return nullptr;
+  CSSValue* left = consumeClipComponent(args, context.mode());
+  if (!left || !args.atEnd())
+    return nullptr;
+  return CSSQuadValue::create(top, right, bottom, left,
+                              CSSQuadValue::SerializeAsRect);
+}
+
+}  // namespace blink
