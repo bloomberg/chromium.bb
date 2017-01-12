04a08636134ec6f8607202019b78a8812c4e2a52
sdy <sdy@chromium.org>
2017-01-11 16:45:55 -0800

    Add a test for our workaround of an AppKit crash in SFChooseIdentityPanel.
    BUG=679992,653093
    
    Review-Url: https://codereview.chromium.org/2624273003
    Cr-Commit-Position: refs/heads/master@{#443090}
    
    Former-commit-id: 145b27b6e14ae6dd557c823e39680d62f16b1cf3

--- chrome/browser/ui/cocoa/ssl_client_certificate_selector_cocoa_browsertest.mm
+++ chrome/browser/ui/cocoa/ssl_client_certificate_selector_cocoa_browsertest.mm
@@ -119,3 +119,57 @@ IN_PROC_BROWSER_TEST_F(SSLClientCertificateSelectorCocoaTest, HideShow) {
   EXPECT_EQ(1.0, [sheetWindow alphaValue]);
   EXPECT_FALSE([[selector overlayWindow] ignoresMouseEvents]);
 }
+
+@interface DeallocTrackingSSLClientCertificateSelectorCocoa
+    : SSLClientCertificateSelectorCocoa
+@property(nonatomic) BOOL* wasDeallocated;
+@end
+
+@implementation DeallocTrackingSSLClientCertificateSelectorCocoa
+@synthesize wasDeallocated = wasDeallocated_;
+
+- (void)dealloc {
+  *wasDeallocated_ = true;
+  [super dealloc];
+}
+
+@end
+
+// Test that we can't trigger the crash from https://crbug.com/653093
+IN_PROC_BROWSER_TEST_F(SSLClientCertificateSelectorCocoaTest,
+                       WorkaroundCrashySierra) {
+  BOOL selector_was_deallocated = false;
+
+  @autoreleasepool {
+    content::WebContents* web_contents =
+        browser()->tab_strip_model()->GetActiveWebContents();
+    DeallocTrackingSSLClientCertificateSelectorCocoa* selector =
+        [[DeallocTrackingSSLClientCertificateSelectorCocoa alloc]
+            initWithBrowserContext:web_contents->GetBrowserContext()
+                   certRequestInfo:auth_requestor_->cert_request_info_.get()
+                          delegate:nil];
+    [selector displayForWebContents:web_contents];
+    content::RunAllPendingInMessageLoop();
+
+    selector.wasDeallocated = &selector_was_deallocated;
+
+    [selector.overlayWindow endSheet:selector.overlayWindow.attachedSheet];
+    content::RunAllPendingInMessageLoop();
+  }
+
+  EXPECT_TRUE(selector_was_deallocated);
+
+  // Without the workaround, this will crash on Sierra.
+  [[NSNotificationCenter defaultCenter]
+      postNotificationName:NSPreferredScrollerStyleDidChangeNotification
+                    object:nil
+                  userInfo:@{
+                    @"NSScrollerStyle" : @(NSScrollerStyleLegacy)
+                  }];
+  [[NSNotificationCenter defaultCenter]
+      postNotificationName:NSPreferredScrollerStyleDidChangeNotification
+                    object:nil
+                  userInfo:@{
+                    @"NSScrollerStyle" : @(NSScrollerStyleOverlay)
+                  }];
+}
