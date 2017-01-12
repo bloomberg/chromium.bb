5ededcb5f2d1dac9b6dd6346a72ca402b82e6670
Robert Iannucci <iannucci@chromium.org>
2017-01-11 19:59:08 -0800

    Clear Zone.Identifier to allow depot_tools.zip bootstrap
    BUG=680274
    
    Change-Id: I47fd8d7a5380aca49c873006c4f66732b21bee37
    Reviewed-on: https://chromium-review.googlesource.com/427323
    Reviewed-by: Vadim Shtayura <vadimsh@chromium.org>
    Commit-Queue: Robbie Iannucci <iannucci@chromium.org>

--- cipd.bat
+++ cipd.bat
@@ -3,4 +3,10 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
+:: To allow this powershell script to run if it was a byproduct of downloading
+:: and unzipping the depot_tools.zip distribution, we clear the Zone.Identifier
+:: alternate data stream. This is equivalent to clicking the "Unblock" button
+:: in the file's properties dialog.
+echo.>"%~dp0\cipd.ps1:Zone.Identifier"
+
 powershell -NoProfile -ExecutionPolicy RemoteSigned -File "%~dp0\cipd.ps1" %*
