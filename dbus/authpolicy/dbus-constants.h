eb397c32a942a2839b2aa3638b6c0da5c3bee146
Roman Sorokin <rsorokin@chromium.org>
2017-01-12 18:57:30 +0100

    authpolicy: Add new errors
    Also added explicit numbers to the enum values.
    
    BUG=chromium:680095
    TEST=manual
    
    Change-Id: I7d23286b1f9d83f2dcca562dc857a959e597a6c0
    Reviewed-on: https://chromium-review.googlesource.com/427839
    Commit-Ready: Lutz Justen <ljusten@chromium.org>
    Tested-by: Roman Sorokin <rsorokin@chromium.org>
    Reviewed-by: Lutz Justen <ljusten@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- dbus/authpolicy/dbus-constants.h
+++ dbus/authpolicy/dbus-constants.h
@@ -18,24 +18,52 @@ const char kAuthPolicyRefreshDevicePolicy[] = "RefreshDevicePolicy";
 
 // Enum values.
 enum ErrorType {
-  ERROR_NONE = 0,               // Everything is A-OK!
-  ERROR_UNKNOWN,                // Unspecified error.
-  ERROR_DBUS_FAILURE,           // Unspecified D-Bus error.
-  ERROR_PARSE_UPN_FAILED,       // Badly formatted user principal name.
-  ERROR_BAD_USER_NAME,          // Auth failed because of bad user name.
-  ERROR_BAD_PASSWORD,           // Auth failed because of bad password.
-  ERROR_PASSWORD_EXPIRED,       // Auth failed because of expired password.
-  ERROR_CANNOT_RESOLVE_KDC,     // Auth failed because of bad realm or network.
-  ERROR_KINIT_FAILED,           // kinit exited with unspecified error.
-  ERROR_NET_FAILED,             // net exited with unspecified error.
-  ERROR_SMBCLIENT_FAILED,       // smdclient exited with unspecified error.
-  ERROR_PARSE_FAILED,           // authpolicy_parser exited with unknown error.
-  ERROR_PARSE_PREG_FAILED,      // Parsing GPOs failed.
-  ERROR_BAD_GPOS,               // GPO data is bad.
-  ERROR_LOCAL_IO,               // Some local IO operation failed.
-  ERROR_NOT_JOINED,             // Machine is not joined to AD domain yet.
-  ERROR_NOT_LOGGED_IN,          // User is not logged in yet.
-  ERROR_STORE_POLICY_FAILED,    // Failed to send policy to Session Manager.
+  // Everything is A-OK!
+  ERROR_NONE = 0,
+  // Unspecified error.
+  ERROR_UNKNOWN = 1,
+  // Unspecified D-Bus error.
+  ERROR_DBUS_FAILURE = 2,
+  // Badly formatted user principal name.
+  ERROR_PARSE_UPN_FAILED = 3,
+  // Auth failed because of bad user name.
+  ERROR_BAD_USER_NAME = 4,
+  // Auth failed because of bad password.
+  ERROR_BAD_PASSWORD = 5,
+  // Auth failed because of expired password.
+  ERROR_PASSWORD_EXPIRED = 6,
+  // Auth failed because of bad realm or network.
+  ERROR_CANNOT_RESOLVE_KDC = 7,
+  // kinit exited with unspecified error.
+  ERROR_KINIT_FAILED = 8,
+  // net exited with unspecified error.
+  ERROR_NET_FAILED = 9,
+  // smdclient exited with unspecified error.
+  ERROR_SMBCLIENT_FAILED = 10,
+  // authpolicy_parser exited with unknown error.
+  ERROR_PARSE_FAILED = 11,
+  // Parsing GPOs failed.
+  ERROR_PARSE_PREG_FAILED = 12,
+  // GPO data is bad.
+  ERROR_BAD_GPOS = 13,
+  // Some local IO operation failed.
+  ERROR_LOCAL_IO = 14,
+  // Machine is not joined to AD domain yet.
+  ERROR_NOT_JOINED = 15,
+  // User is not logged in yet.
+  ERROR_NOT_LOGGED_IN = 16,
+  // Failed to send policy to Session Manager.
+  ERROR_STORE_POLICY_FAILED = 17,
+  // User doesn't have the right to join machines to the domain.
+  ERROR_JOIN_ACCESS_DENIED = 18,
+  // General network problem.
+  ERROR_NETWORK_PROBLEM = 19,
+  // Machine name contains restricted characters.
+  ERROR_BAD_MACHINE_NAME = 20,
+  // Machine name too long.
+  ERROR_MACHINE_NAME_TOO_LONG = 21,
+  // User joined maximum number of machines to the domain.
+  ERROR_USER_HIT_JOIN_QUOTA = 22,
   // Should be the last.
   ERROR_COUNT,
 };
