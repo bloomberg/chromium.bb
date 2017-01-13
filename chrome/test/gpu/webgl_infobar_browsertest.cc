4e40763b8d6869c6b6ad19b92d8a2b4fee5b35ec
kbr <kbr@chromium.org>
2017-01-12 17:40:34 -0800

    Delete flaky WebGLInfoBarTest.ContextLossInfoBarReload test.
    The aim is to delete this code path entirely and the test hasn't been
    run for years. Fixing it is not a priority.
    
    BUG=324555
    TBR=zmo@chromium.org
    
    Review-Url: https://codereview.chromium.org/2633473003
    Cr-Commit-Position: refs/heads/master@{#443443}
    
    Former-commit-id: 5294b308574741e970de525415d8aa59feb92345

--- chrome/test/gpu/webgl_infobar_browsertest.cc
+++ chrome/test/gpu/webgl_infobar_browsertest.cc
@@ -88,50 +88,6 @@ IN_PROC_BROWSER_TEST_F(WebGLInfoBarTest, DISABLED_ContextLossRaisesInfoBar) {
                     infobar_count());
 }
 
-// This test is flaky. http://crbug.com/324555
-IN_PROC_BROWSER_TEST_F(WebGLInfoBarTest, DISABLED_ContextLossInfoBarReload) {
-  if (gpu::GPUTestBotConfig::CurrentConfigMatches("XP"))
-    return;
-
-  content::DOMMessageQueue message_queue;
-
-  // Load page and wait for it to load.
-  content::WindowedNotificationObserver observer(
-      content::NOTIFICATION_LOAD_STOP,
-      content::NotificationService::AllSources());
-  ui_test_utils::NavigateToURL(
-      browser(),
-      content::GetFileUrlWithQuery(
-          gpu_test_dir_.AppendASCII("webgl.html"),
-          "query=kill_after_notification"));
-  observer.Wait();
-
-  std::string m;
-  ASSERT_TRUE(message_queue.WaitForMessage(&m));
-  EXPECT_EQ("\"LOADED\"", m);
-
-  message_queue.ClearQueue();
-
-  content::WindowedNotificationObserver infobar_added(
-        chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_ADDED,
-        content::NotificationService::AllSources());
-  SimulateGPUCrash(browser());
-  infobar_added.Wait();
-  InfoBarService* infobar_service = InfoBarService::FromWebContents(
-      browser()->tab_strip_model()->GetActiveWebContents());
-  ASSERT_EQ(1u, infobar_service->infobar_count());
-  infobars::InfoBarDelegate* delegate =
-      infobar_service->infobar_at(0)->delegate();
-  ASSERT_TRUE(delegate->AsThreeDAPIInfoBarDelegate());
-  delegate->AsConfirmInfoBarDelegate()->Cancel();
-
-  // The page should reload and another message sent to the
-  // DomAutomationController.
-  m.clear();
-  ASSERT_TRUE(message_queue.WaitForMessage(&m));
-  EXPECT_EQ("\"LOADED\"", m);
-}
-
 // There isn't any point in adding a test which calls Accept() on the
 // ThreeDAPIInfoBarDelegate; doing so doesn't remove the infobar, and
 // there's no concrete event that could be observed in response.
