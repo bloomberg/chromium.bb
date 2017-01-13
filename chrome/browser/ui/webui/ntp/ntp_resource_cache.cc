cf4e892bfbe712e5b9149d746ac5e6480d7ff871
zork <zork@chromium.org>
2017-01-13 15:00:42 -0800

    Update Guest session's Learn More link.
    BUG=680231
    
    Review-Url: https://codereview.chromium.org/2624903005
    Cr-Commit-Position: refs/heads/master@{#443710}
    
    Former-commit-id: f4747e7d46e184744730cf6096c12d775262c9e2

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -85,7 +85,7 @@ const char kLearnMoreIncognitoUrl[] =
 // The URL for the Learn More page shown on guest session new tab.
 const char kLearnMoreGuestSessionUrl[] =
 #if defined(OS_CHROMEOS)
-    "https://support.google.com/chromebook/answer/1057090";
+    "https://support.google.com/chromebook/?p=chromebook_guest";
 #else
     "https://support.google.com/chrome/?p=ui_guest";
 #endif
