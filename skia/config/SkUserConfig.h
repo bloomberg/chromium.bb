35b5b5b8a8e50929a3b3ce40e670fa171cc28e35
liyuqian <liyuqian@google.com>
2017-01-14 18:34:32 -0800

    Rebaseline to enable new quad shift
    BUG=
    
    Review-Url: https://codereview.chromium.org/2630593002
    Cr-Commit-Position: refs/heads/master@{#443807}
    
    Former-commit-id: 7d522d7829ed677010ffa071e60f43f47ebad3cf

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -227,10 +227,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_CLIPOP_EXOTIC_NAMES
 #endif
 
-#ifndef    SK_SUPPORT_LEGACY_QUAD_SHIFT
-#   define SK_SUPPORT_LEGACY_QUAD_SHIFT
-#endif
-
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
