9ecc6ab8eeaa236ce329a2a68bba0bbbaa6bd590
reed <reed@google.com>
2017-01-17 07:38:17 -0800

    replace use of getClipStack, which is deprecated
    Will enable this CL: https://skia-review.googlesource.com/c/7097/
    
    BUG=skia:6122
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2633913002
    Cr-Commit-Position: refs/heads/master@{#444057}
    
    Former-commit-id: dd8450ea557526a61dc45104947cbea01bd7ae76

--- cc/playback/raster_source.cc
+++ cc/playback/raster_source.cc
@@ -108,13 +108,27 @@ void RasterSource::PlaybackToCanvas(SkCanvas* raster_canvas,
   }
 }
 
+namespace {
+
+bool CanvasIsUnclipped(const SkCanvas* canvas) {
+  if (!canvas->isClipRect())
+    return false;
+
+  SkIRect bounds;
+  if (!canvas->getClipDeviceBounds(&bounds))
+    return false;
+
+  SkISize size = canvas->getBaseLayerSize();
+  return bounds.contains(0, 0, size.width(), size.height());
+}
+
+}  // namespace
+
 void RasterSource::PrepareForPlaybackToCanvas(SkCanvas* canvas) const {
   // TODO(hendrikw): See if we can split this up into separate functions.
 
-  if (canvas->getClipStack()->quickContains(
-          SkRect::MakeFromIRect(canvas->imageInfo().bounds()))) {
+  if (CanvasIsUnclipped(canvas))
     canvas->discard();
-  }
 
   // If this raster source has opaque contents, it is guaranteeing that it will
   // draw an opaque rect the size of the layer.  If it is not, then we must
