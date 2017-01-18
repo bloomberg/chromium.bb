a1aab6cfee0736a7a72664581bcaa8f404fd47a8
meade <meade@chromium.org>
2017-01-17 18:43:47 -0800

    Disable ImeTest testLongPressInputWhileComposingText due to flakiness
    BUG=682080
    TBR=changwan@chromium.org
    
    Review-Url: https://codereview.chromium.org/2631313004
    Cr-Commit-Position: refs/heads/master@{#444251}
    
    Former-commit-id: 00320b068a7e061eb3634293c19a0401357fa66a

--- content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/ImeTest.java
@@ -22,6 +22,7 @@ import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content.browser.ContentViewCore;
@@ -535,8 +536,9 @@ public class ImeTest extends ContentShellTestBase {
         assertWaitForSelectActionBarStatus(true);
     }
 
-    @SmallTest
-    @Feature({"TextInput"})
+    // @SmallTest
+    // @Feature({"TextInput"})
+    @DisabledTest
     public void testLongPressInputWhileComposingText() throws Exception {
         assertWaitForSelectActionBarStatus(false);
         setComposingText("Sample Text", 1);
