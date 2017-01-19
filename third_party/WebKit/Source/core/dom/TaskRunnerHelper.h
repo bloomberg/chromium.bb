5a27b8720b68a52307b1df19714c8bf01578afb5
dcheng <dcheng@chromium.org>
2017-01-19 02:17:33 -0800

    Add spec links and more descriptions of the different task types.
    BUG=624694
    
    Review-Url: https://codereview.chromium.org/2638403003
    Cr-Commit-Position: refs/heads/master@{#444697}
    
    Former-commit-id: bc507c8eff55131526e098b4ae8218ee735f9cc3

--- third_party/WebKit/Source/core/dom/TaskRunnerHelper.h
+++ third_party/WebKit/Source/core/dom/TaskRunnerHelper.h
@@ -20,22 +20,79 @@ class WebTaskRunner;
 enum class TaskType : unsigned {
   // Speced tasks and related internal tasks should be posted to one of
   // the following task runners. These task runners may be throttled.
+
+  // https://html.spec.whatwg.org/multipage/webappapis.html#generic-task-sources
+  //
+  // This task source is used for features that react to DOM manipulations, such
+  // as things that happen in a non-blocking fashion when an element is inserted
+  // into the document.
   DOMManipulation,
+  // This task source is used for features that react to user interaction, for
+  // example keyboard or mouse input. Events sent in response to user input
+  // (e.g. click events) must be fired using tasks queued with the user
+  // interaction task source.
   UserInteraction,
+  // This task source is used for features that trigger in response to network
+  // activity.
   Networking,
+  // This task source is used to queue calls to history.back() and similar APIs.
   HistoryTraversal,
+
+  // https://html.spec.whatwg.org/multipage/embedded-content.html#the-embed-element
+  // This task source is used for the embed element setup steps.
   Embed,
+
+  // https://html.spec.whatwg.org/multipage/embedded-content.html#media-elements
+  // This task source is used for all tasks queued in the [Media elements]
+  // section and subsections of the spec unless explicitly specified otherwise.
   MediaElementEvent,
+
+  // https://html.spec.whatwg.org/multipage/scripting.html#the-canvas-element
+  // This task source is used to invoke the result callback of
+  // HTMLCanvasElement.toBlob().
   CanvasBlobSerialization,
+
+  // https://html.spec.whatwg.org/multipage/webappapis.html#event-loop-processing-model
+  // This task source is used when an algorithm requires a microtask to be
+  // queued.
   Microtask,
+
+  // https://html.spec.whatwg.org/multipage/webappapis.html#timers
+  // This task source is used to queue tasks queued by setInterval() and similar
+  // APIs.
   Timer,
+
+  // https://html.spec.whatwg.org/multipage/comms.html#sse-processing-model
+  // This task source is used for any tasks that are queued by EventSource
+  // objects.
   RemoteEvent,
+
+  // https://html.spec.whatwg.org/multipage/comms.html#feedback-from-the-protocol
+  // The task source for all tasks queued in the [WebSocket] section of the
+  // spec.
   WebSocket,
+
+  // https://html.spec.whatwg.org/multipage/comms.html#web-messaging
+  // This task source is used for the tasks in cross-document messaging.
   PostedMessage,
+
+  // https://html.spec.whatwg.org/multipage/comms.html#message-ports
   UnshippedPortMessage,
+
+  // https://www.w3.org/TR/FileAPI/#blobreader-task-source
+  // This task source is used for all tasks queued in the FileAPI spec to read
+  // byte sequences associated with Blob and File objects.
   FileReading,
+
+  // https://www.w3.org/TR/IndexedDB/#request-api
   DatabaseAccess,
+
+  // https://w3c.github.io/presentation-api/#common-idioms
+  // This task source is used for all tasks in the Presentation API spec.
   Presentation,
+
+  // https://www.w3.org/TR/2016/WD-generic-sensor-20160830/#sensor-task-source
+  // This task source is used for all tasks in the Sensor API spec.
   Sensor,
 
   // Use MiscPlatformAPI for a task that is defined in the spec but is not yet
