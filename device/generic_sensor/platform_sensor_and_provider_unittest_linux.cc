3c38ff83940f9b7285d8262a9f95cd19e97bd4ff
jianli <jianli@chromium.org>
2017-01-19 15:36:29 -0800

    Revert of Fix unused lambda captures in //device and //services/media_capture. (patchset #1 id:1 of https://codereview.chromium.org/2649443002/ )
    Reason for revert:
    https://build.chromium.org/p/chromium/builders/Win/builds/51061
    
    Original issue's description:
    > Fix unused lambda captures in //device and //services/media_capture.
    >
    > Clang just got a new warning about unused lambda captures,
    > and that requires us to clean all places with this issue
    > across all the Chromium code base. This CL fixes all such
    > cases in //device and //services/media_capture.
    >
    > An example of a code that is no longer considered normal is
    >
    > auto g = [i, j]() { printf("%d\n", i); };
    >
    > in which j is never used in the lambda and therefore its capture is unnecessary.
    >
    > BUG=681136
    >
    > Review-Url: https://codereview.chromium.org/2649443002
    > Cr-Commit-Position: refs/heads/master@{#444873}
    > Committed: https://chromium.googlesource.com/chromium/src/+/2fd2c1601bad644f910eee55ff520e09faa06181 [formerly 3cc294f047397929621de8cb19c2e911c8f488a1]
    
    TBR=rockot@chromium.org,krasin@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=681136
    
    Review-Url: https://codereview.chromium.org/2643853006
    Cr-Commit-Position: refs/heads/master@{#444889}
    
    Former-commit-id: 7c9a1c6ec0f08e65ec3aa5e8a42a239c267ca611

--- device/generic_sensor/platform_sensor_and_provider_unittest_linux.cc
+++ device/generic_sensor/platform_sensor_and_provider_unittest_linux.cc
@@ -221,14 +221,15 @@ class PlatformSensorAndProviderLinuxTest : public ::testing::Test {
   // calling udev methods.
   void InitializeMockUdevMethods(const base::FilePath& sensor_dir) {
     ON_CALL(*manager_, GetUdevDeviceGetSubsystem(IsNull()))
-        .WillByDefault(Invoke([](udev_device* dev) { return "iio"; }));
+        .WillByDefault(Invoke([this](udev_device* dev) { return "iio"; }));
 
     ON_CALL(*manager_, GetUdevDeviceGetSyspath(IsNull()))
         .WillByDefault(Invoke(
             [sensor_dir](udev_device* dev) { return sensor_dir.value(); }));
 
     ON_CALL(*manager_, GetUdevDeviceGetDevnode(IsNull()))
-        .WillByDefault(Invoke([](udev_device* dev) { return "/dev/test"; }));
+        .WillByDefault(
+            Invoke([this](udev_device* dev) { return "/dev/test"; }));
 
     ON_CALL(*manager_, GetUdevDeviceGetSysattrValue(IsNull(), _))
         .WillByDefault(Invoke(
