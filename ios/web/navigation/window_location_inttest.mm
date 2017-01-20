c039a8240f5e5b3117007a32548f467c37c538b8
kkhorimoto <kkhorimoto@chromium.org>
2017-01-19 17:22:54 -0800

    Created test for window.location.assign() with unresolvable URLs.
    BUG=673022
    
    Review-Url: https://codereview.chromium.org/2600693002
    Cr-Commit-Position: refs/heads/master@{#444918}
    
    Former-commit-id: 274da537e7c6531cf15a4145c408607df06e6b8f

--- ios/web/navigation/window_location_inttest.mm
+++ ios/web/navigation/window_location_inttest.mm
@@ -121,6 +121,23 @@ TEST_F(WindowLocationTest, Assign) {
   EXPECT_EQ(NSNotFound, GetIndexOfNavigationItem(about_blank_item));
 }
 
+// Tests that calling window.location.assign() with an unresolvable URL loads
+// about:blank.
+TEST_F(WindowLocationTest, WindowLocationAssignUnresolvable) {
+  // Attempt to call window.location.assign() using an unresolvable URL.
+  GURL about_blank("about:blank");
+  GURL unresolvable_url("http:https:not a url");
+  SetWindowLocationUrl(unresolvable_url);
+  ExecuteBlockAndWaitForLoad(about_blank, ^{
+    ASSERT_TRUE(web::test::TapWebViewElementWithId(web_state(),
+                                                   kWindowLocationAssignID));
+  });
+
+  // Verify that about:blank was actually loaded.
+  EXPECT_EQ(about_blank,
+            navigation_manager()->GetLastCommittedItem()->GetURL());
+}
+
 // Tests that calling window.location.replace() doesn't create a new
 // NavigationItem.
 // TODO(crbug.com/307072): Enable test when location.replace is fixed.
