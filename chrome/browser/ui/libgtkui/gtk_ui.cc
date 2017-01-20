bloomberg/chromium.bb

commit 8a953b925378c84a1d8dd2414afa551d65f71733

Change Painter factory functions to unique_ptr
No behavioral changes.

BUG=678371

Review-Url: https://codereview.chromium.org/2637383003
Cr-Commit-Position: refs/heads/master@{#444783}

Former-commit-id: 477aca494328faf52dfb88b9926933e94241debe

chrome/browser/ui/libgtkui/gtk_ui.cc
@@ -687,17 +687,14 @@ std::unique_ptr<views::Border> GtkUi::CreateNativeBorder(
       owning_button->GetClassName() == views::BlueButton::kViewClassName;
 
   for (unsigned i = 0; i < arraysize(paintstate); i++) {
-    views::Painter* painter = nullptr;
-
     if (border->PaintsButtonState(paintstate[i].focus, paintstate[i].state)) {
       std::string idr = is_blue ? paintstate[i].idr_blue : paintstate[i].idr;
-      painter = new GtkButtonPainter(idr);
+      gtk_border->SetPainter(paintstate[i].focus, paintstate[i].state,
+                             base::MakeUnique<GtkButtonPainter>(idr));
     }
-
-    gtk_border->SetPainter(paintstate[i].focus, paintstate[i].state, painter);
   }
 
-  return std::move(gtk_border);
+  return gtk_border;
 }
 
 void GtkUi::AddWindowButtonOrderObserver(

ash/common/wm/window_cycle_list.cc
@@ -14,7 +14,6 @@
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/root_window_controller.h"
 #include "base/command_line.h"
-#include "base/memory/ptr_util.h"
 #include "ui/accessibility/ax_node_data.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/display/display.h"
@@ -247,9 +246,9 @@ class WindowCycleView : public views::WidgetDelegateView {
     // because the layer animates bounds changes but the View's bounds change
     // immediately.
     highlight_view_->set_background(new LayerFillBackgroundPainter(
-        base::WrapUnique(views::Painter::CreateRoundRectWith1PxBorderPainter(
+        views::Painter::CreateRoundRectWith1PxBorderPainter(
             SkColorSetA(SK_ColorWHITE, 0x4D), SkColorSetA(SK_ColorWHITE, 0x33),
-            kBackgroundCornerRadius))));
+            kBackgroundCornerRadius)));
     highlight_view_->SetPaintToLayer(true);
     highlight_view_->layer()->SetFillsBoundsOpaquely(false);
 

ash/common/wm/workspace/phantom_window_controller.cc
@@ -130,11 +130,9 @@ std::unique_ptr<views::Widget> PhantomWindowController::CreatePhantomWidget(
   }
 
   const int kImages[] = IMAGE_GRID(IDR_AURA_PHANTOM_WINDOW);
-  views::Painter* background_painter =
-      views::Painter::CreateImageGridPainter(kImages);
   views::View* content_view = new views::View;
-  content_view->set_background(
-      views::Background::CreateBackgroundPainter(true, background_painter));
+  content_view->set_background(views::Background::CreateBackgroundPainter(
+      views::Painter::CreateImageGridPainter(kImages)));
   phantom_widget->SetContentsView(content_view);
 
   // Show the widget after all the setups.

chrome/browser/ui/views/toolbar/browser_actions_container.cc
@@ -95,8 +95,8 @@ BrowserActionsContainer::BrowserActionsContainer(
     AddChildView(resize_area_);
 
     const int kWarningImages[] = IMAGE_GRID(IDR_DEVELOPER_MODE_HIGHLIGHT);
-    warning_highlight_painter_.reset(
-        views::Painter::CreateImageGridPainter(kWarningImages));
+    warning_highlight_painter_ =
+        views::Painter::CreateImageGridPainter(kWarningImages);
   }
 }
 

ui/views/background.cc
@@ -38,23 +38,19 @@ class SolidBackground : public Background {
 
 class BackgroundPainter : public Background {
  public:
-  BackgroundPainter(bool owns_painter, Painter* painter)
-      : owns_painter_(owns_painter), painter_(painter) {
-    DCHECK(painter);
+  explicit BackgroundPainter(std::unique_ptr<Painter> painter)
+      : painter_(std::move(painter)) {
+    DCHECK(painter_);
   }
 
-  ~BackgroundPainter() override {
-    if (owns_painter_)
-      delete painter_;
-  }
+  ~BackgroundPainter() override {}
 
   void Paint(gfx::Canvas* canvas, View* view) const override {
-    Painter::PaintPainterAt(canvas, painter_, view->GetLocalBounds());
+    Painter::PaintPainterAt(canvas, painter_.get(), view->GetLocalBounds());
   }
 
  private:
-  bool owns_painter_;
-  Painter* painter_;
+  std::unique_ptr<Painter> painter_;
 
   DISALLOW_COPY_AND_ASSIGN(BackgroundPainter);
 };
@@ -85,8 +81,8 @@ Background* Background::CreateStandardPanelBackground() {
 // static
 Background* Background::CreateVerticalGradientBackground(SkColor color1,
                                                          SkColor color2) {
-  Background* background = CreateBackgroundPainter(
-      true, Painter::CreateVerticalGradient(color1, color2));
+  Background* background =
+      CreateBackgroundPainter(Painter::CreateVerticalGradient(color1, color2));
   background->SetNativeControlColor(
       color_utils::AlphaBlend(color1, color2, 128));
 
@@ -94,22 +90,9 @@ Background* Background::CreateVerticalGradientBackground(SkColor color1,
 }
 
 // static
-Background* Background::CreateVerticalMultiColorGradientBackground(
-    SkColor* colors,
-    SkScalar* pos,
-    size_t count) {
-  Background* background = CreateBackgroundPainter(
-      true, Painter::CreateVerticalMultiColorGradient(colors, pos, count));
-  background->SetNativeControlColor(
-      color_utils::AlphaBlend(colors[0], colors[count-1], 128));
-
-  return background;
-}
-
-// static
-Background* Background::CreateBackgroundPainter(bool owns_painter,
-                                                Painter* painter) {
-  return new BackgroundPainter(owns_painter, painter);
+Background* Background::CreateBackgroundPainter(
+    std::unique_ptr<Painter> painter) {
+  return new BackgroundPainter(std::move(painter));
 }
 
 }  // namespace views

ui/views/background.h
@@ -7,6 +7,8 @@
 
 #include <stddef.h>
 
+#include <memory>
+
 #include "base/macros.h"
 #include "build/build_config.h"
 #include "third_party/skia/include/core/SkColor.h"
@@ -60,22 +62,11 @@ class VIEWS_EXPORT Background {
   static Background* CreateVerticalGradientBackground(SkColor color1,
                                                       SkColor color2);
 
-  // Creates a background that contains a vertical gradient. The gradient can
-  // have multiple |colors|. The |pos| array contains the relative positions of
-  // each corresponding color. |colors| and |pos| must be the same size. The
-  // first element in |pos| must be 0.0 and the last element must be 1.0.
-  // |count| contains the number of elements in |colors| and |pos|.
-  static Background* CreateVerticalMultiColorGradientBackground(SkColor* colors,
-                                                                SkScalar* pos,
-                                                                size_t count);
-
   // Creates Chrome's standard panel background
   static Background* CreateStandardPanelBackground();
 
-  // Creates a Background from the specified Painter. If owns_painter is
-  // true, the Painter is deleted when the Border is deleted.
-  static Background* CreateBackgroundPainter(bool owns_painter,
-                                             Painter* painter);
+  // Creates a Background from the specified Painter.
+  static Background* CreateBackgroundPainter(std::unique_ptr<Painter> painter);
 
   // Render the background for the provided view
   virtual void Paint(gfx::Canvas* canvas, View* view) const = 0;

ui/views/bubble/bubble_border.cc
@@ -42,7 +42,7 @@ BorderImages::BorderImages(const int border_image_ids[],
   if (!border_image_ids)
     return;
 
-  border_painter.reset(Painter::CreateImageGridPainter(border_image_ids));
+  border_painter = Painter::CreateImageGridPainter(border_image_ids);
   ResourceBundle& rb = ResourceBundle::GetSharedInstance();
   border_thickness = rb.GetImageSkiaNamed(border_image_ids[0])->width();
 

ui/views/controls/button/label_button_border.cc
@@ -183,8 +183,8 @@ Painter* LabelButtonAssetBorder::GetPainter(bool focused,
 
 void LabelButtonAssetBorder::SetPainter(bool focused,
                                         Button::ButtonState state,
-                                        Painter* painter) {
-  painters_[focused ? 1 : 0][state].reset(painter);
+                                        std::unique_ptr<Painter> painter) {
+  painters_[focused ? 1 : 0][state] = std::move(painter);
 }
 
 }  // namespace views

commit cead71c5724ba7d5ea97a97cccbf38ec562158cd

Fix gtk theme regression introduced in 8a953b925378c [formerly 477aca494328f]
Turns out we do need to explicitly clear the border images. This bug is
visible when you hover a toolbar button.

BUG=682940

Review-Url: https://codereview.chromium.org/2639423005
Cr-Commit-Position: refs/heads/master@{#445130}

Former-commit-id: 7e2b5c14fa70447b692fab4f7a9660729cf55b38

chrome/browser/ui/libgtkui/gtk_ui.cc
@@ -687,11 +687,12 @@ std::unique_ptr<views::Border> GtkUi::CreateNativeBorder(
       owning_button->GetClassName() == views::BlueButton::kViewClassName;
 
   for (unsigned i = 0; i < arraysize(paintstate); i++) {
-    if (border->PaintsButtonState(paintstate[i].focus, paintstate[i].state)) {
-      std::string idr = is_blue ? paintstate[i].idr_blue : paintstate[i].idr;
-      gtk_border->SetPainter(paintstate[i].focus, paintstate[i].state,
-                             base::MakeUnique<GtkButtonPainter>(idr));
-    }
+    std::string idr = is_blue ? paintstate[i].idr_blue : paintstate[i].idr;
+    gtk_border->SetPainter(
+        paintstate[i].focus, paintstate[i].state,
+        border->PaintsButtonState(paintstate[i].focus, paintstate[i].state)
+            ? base::MakeUnique<GtkButtonPainter>(idr)
+            : nullptr);
   }
 
   return gtk_border;