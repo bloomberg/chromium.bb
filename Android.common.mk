9c924e823543d24476d73b3f5878935552f31891
Emil Velikov <emil.l.velikov@gmail.com>
2017-01-22 17:43:13 +0000

    android: silence ~550 warnings
    Analogous to the autoconf build add the following to the build
    
       -Wno-unused-parameter
       -Wno-missing-field-initializers
    
    Cc: Chih-Wei Huang <cwhuang@android-x86.org>
    Cc: Rob Herring <robh@kernel.org>
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>
    Reviewed-by: Rob Herring <robh@kernel.org>

--- Android.common.mk
+++ Android.common.mk
@@ -3,6 +3,10 @@ LOCAL_CFLAGS += \
 	-DHAVE_VISIBILITY=1 \
 	-DHAVE_LIBDRM_ATOMIC_PRIMITIVES=1
 
+LOCAL_CFLAGS += \
+	-Wno-unused-parameter \
+	-Wno-missing-field-initializers
+
 # Quiet down the build system and remove any .h files from the sources
 LOCAL_SRC_FILES := $(patsubst %.h, , $(LOCAL_SRC_FILES))
 LOCAL_EXPORT_C_INCLUDE_DIRS += $(LOCAL_PATH)
