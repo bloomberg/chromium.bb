bloomberg/chromium.bb

commit 932c34b4cc3fb8f05c03afa5fd2827b738170bb6

[wrapper-tracing] Add heap snapshot generator infrastructure
Register a callback with V8's heap profiler that can be used to query all
RetainedObjectInfo_s for a given Isolate.

Basically, we create RetainedObjectInfo_s for DOM trees (attached and detached)
and pending activities and assign all found wrappers to these info objects.

BUG=chromium:679724

Review-Url: https://codereview.chromium.org/2625093002
Cr-Commit-Position: refs/heads/master@{#445372}

Former-commit-id: a34d185b3e31187fb8aa16b700e5837e9636cba9

third_party/WebKit/Source/bindings/core/v8/V8GCController.cpp
@@ -51,6 +51,8 @@
 #include "wtf/Vector.h"
 #include "wtf/allocator/Partitions.h"
 #include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
 
 namespace blink {
 
@@ -147,6 +149,140 @@ class MinorGCUnmodifiedWrapperVisitor : public v8::PersistentHandleVisitor {
  private:
   v8::Isolate* m_isolate;
 };
+
+class HeapSnaphotWrapperVisitor : public ScriptWrappableVisitor,
+                                  public v8::PersistentHandleVisitor {
+ public:
+  explicit HeapSnaphotWrapperVisitor(v8::Isolate* isolate)
+      : ScriptWrappableVisitor(isolate),
+        m_currentParent(nullptr),
+        m_onlyTraceSingleLevel(false),
+        m_firstScriptWrappableTraced(false) {}
+
+  // Collect interesting V8 roots for the heap snapshot. Currently these are
+  // DOM nodes.
+  void collectV8Roots() { m_isolate->VisitHandlesWithClassIds(this); }
+
+  void VisitPersistentHandle(v8::Persistent<v8::Value>* value,
+                             uint16_t classId) override {
+    if (classId != WrapperTypeInfo::NodeClassId)
+      return;
+
+    DCHECK(!value->IsIndependent());
+    v8::Local<v8::Object> wrapper = v8::Local<v8::Object>::New(
+        m_isolate, v8::Persistent<v8::Object>::Cast(*value));
+    DCHECK(V8Node::hasInstance(wrapper, m_isolate));
+    Node* node = V8Node::toImpl(wrapper);
+    Node* root = V8GCController::opaqueRootForGC(m_isolate, node);
+    m_nodesRequiringTracing[root].push_back(node);
+  }
+
+  // Trace through the blink heap to find all V8 wrappers reachable from
+  // ActiveScriptWrappables. Also collect retainer edges on the way.
+  void tracePendingActivities() {
+    CHECK(m_foundV8Wrappers.empty());
+    m_currentParent = nullptr;
+
+    TracePrologue();
+    ActiveScriptWrappableBase::traceActiveScriptWrappables(m_isolate, this);
+    AdvanceTracing(
+        0,
+        v8::EmbedderHeapTracer::AdvanceTracingActions(
+            v8::EmbedderHeapTracer::ForceCompletionAction::FORCE_COMPLETION));
+    // Abort instead of Epilogue as we want to finish synchronously.
+    AbortTracing();
+
+    m_groups.push_back(
+        std::make_pair(new SuspendableObjectsInfo(m_foundV8Wrappers.size()),
+                       std::move(m_foundV8Wrappers)));
+  }
+
+  // Trace through the blink heap to find all V8 wrappers reachable from any
+  // of the collected roots. Also collect retainer edges on the way.
+  void traceV8Roots() {
+    for (auto& groupPair : m_nodesRequiringTracing) {
+      v8::HeapProfiler::RetainerChildren groupChildren;
+      for (auto& node : groupPair.second) {
+        auto wrappers = findV8WrappersDirectlyReachableFrom(node);
+        groupChildren.insert(wrappers.begin(), wrappers.end());
+      }
+      m_groups.push_back(std::make_pair(new RetainedDOMInfo(groupPair.first),
+                                        std::move(groupChildren)));
+    }
+  }
+
+  v8::HeapProfiler::RetainerEdges edges() { return std::move(m_edges); }
+  v8::HeapProfiler::RetainerGroups groups() { return std::move(m_groups); }
+
+  void markWrapper(const v8::PersistentBase<v8::Value>* value) const override {
+    if (m_currentParent && m_currentParent != value)
+      m_edges.push_back(std::make_pair(m_currentParent, value));
+    m_foundV8Wrappers.insert(value);
+  }
+
+  void dispatchTraceWrappers(const TraceWrapperBase* traceable) const override {
+    if (!m_onlyTraceSingleLevel || !traceable->isScriptWrappable() ||
+        !reinterpret_cast<const ScriptWrappable*>(traceable)
+             ->containsWrapper() ||
+        !m_firstScriptWrappableTraced) {
+      m_firstScriptWrappableTraced = true;
+      traceable->traceWrappers(this);
+    }
+  }
+
+ private:
+  v8::HeapProfiler::RetainerChildren findV8WrappersDirectlyReachableFrom(
+      Node* traceable) {
+    CHECK(m_foundV8Wrappers.empty());
+    WTF::AutoReset<bool>(&m_onlyTraceSingleLevel, true);
+    m_firstScriptWrappableTraced = false;
+    m_currentParent =
+        &v8::Persistent<v8::Value>::Cast(*traceable->rawMainWorldWrapper());
+
+    TracePrologue();
+    traceable->wrapperTypeInfo()->traceWrappers(this, traceable);
+    AdvanceTracing(
+        0,
+        v8::EmbedderHeapTracer::AdvanceTracingActions(
+            v8::EmbedderHeapTracer::ForceCompletionAction::FORCE_COMPLETION));
+    // Abort instead of Epilogue as we want to finish synchronously.
+    AbortTracing();
+
+    return std::move(m_foundV8Wrappers);
+  }
+
+  // Input obtained from |VisitPersistentHandle|.
+  std::unordered_map<Node*, std::vector<Node*>> m_nodesRequiringTracing;
+
+  // Temporaries used for tracing a single Node.
+  const v8::PersistentBase<v8::Value>* m_currentParent;
+  bool m_onlyTraceSingleLevel;
+  mutable bool m_firstScriptWrappableTraced;
+  mutable v8::HeapProfiler::RetainerChildren m_foundV8Wrappers;
+
+  // Out variables
+  mutable v8::HeapProfiler::RetainerEdges m_edges;
+  mutable v8::HeapProfiler::RetainerGroups m_groups;
+};
+
+// The function |getRetainerInfos| processing all handles on the blink heap,
+// logically grouping together DOM trees (attached and detached) and pending
+// activities, while at the same time finding parent to child relationships
+// for non-Node wrappers. Since we are processing *all* handles there is no
+// way we miss out on a handle. V8 will figure out the liveness information
+// for the provided information itself.
+v8::HeapProfiler::RetainerInfos V8GCController::getRetainerInfos(
+    v8::Isolate* isolate) {
+  std::unique_ptr<HeapSnaphotWrapperVisitor> tracer(
+      new HeapSnaphotWrapperVisitor(isolate));
+  V8PerIsolateData::TemporaryScriptWrappableVisitorScope scope(
+      isolate, std::move(tracer));
+
+  tracer->collectV8Roots();
+  tracer->traceV8Roots();
+  tracer->tracePendingActivities();
+  return v8::HeapProfiler::RetainerInfos{tracer->groups(), tracer->edges()};
+}
 
 class MajorGCWrapperVisitor : public v8::PersistentHandleVisitor {
  public:

third_party/WebKit/Source/bindings/core/v8/V8PerIsolateData.cpp
@@ -406,4 +406,15 @@ void V8PerIsolateData::addActiveScriptWrappable(
   m_activeScriptWrappables->add(wrappable);
 }
 
+void V8PerIsolateData::TemporaryScriptWrappableVisitorScope::
+    swapWithV8PerIsolateDataVisitor(
+        std::unique_ptr<ScriptWrappableVisitor>& visitor) {
+  ScriptWrappableVisitor* current = currentVisitor(m_isolate);
+  if (current)
+    current->performCleanup();
+
+  V8PerIsolateData::from(m_isolate)->m_scriptWrappableVisitor.swap(visitor);
+  m_isolate->SetEmbedderHeapTracer(visitor.get());
+}
+
 }  // namespace blink

third_party/WebKit/Source/bindings/core/v8/V8PerIsolateData.h
@@ -167,6 +167,33 @@ class CORE_EXPORT V8PerIsolateData {
     return m_activeScriptWrappables.get();
   }
 
+  class TemporaryScriptWrappableVisitorScope {
+    WTF_MAKE_NONCOPYABLE(TemporaryScriptWrappableVisitorScope);
+    STACK_ALLOCATED();
+
+   public:
+    TemporaryScriptWrappableVisitorScope(
+        v8::Isolate* isolate,
+        std::unique_ptr<ScriptWrappableVisitor> visitor)
+        : m_isolate(isolate) {
+      swapWithV8PerIsolateDataVisitor(visitor);
+    }
+    ~TemporaryScriptWrappableVisitorScope() {
+      swapWithV8PerIsolateDataVisitor(m_savedVisitor);
+    }
+
+   private:
+    inline ScriptWrappableVisitor* currentVisitor(v8::Isolate* isolate) {
+      return V8PerIsolateData::from(m_isolate)->scriptWrappableVisitor();
+    }
+
+    void swapWithV8PerIsolateDataVisitor(
+        std::unique_ptr<ScriptWrappableVisitor>&);
+
+    v8::Isolate* m_isolate;
+    std::unique_ptr<ScriptWrappableVisitor> m_savedVisitor;
+  };
+
   void setScriptWrappableVisitor(
       std::unique_ptr<ScriptWrappableVisitor> visitor) {
     m_scriptWrappableVisitor = std::move(visitor);

third_party/WebKit/Source/bindings/core/v8/ScriptWrappable.h
@@ -46,6 +46,7 @@ class CORE_EXPORT TraceWrapperBase {
 
  public:
   TraceWrapperBase() = default;
+  virtual bool isScriptWrappable() const { return false; }
 
   DECLARE_VIRTUAL_TRACE_WRAPPERS(){};
 };
@@ -61,6 +62,8 @@ class CORE_EXPORT ScriptWrappable : public TraceWrapperBase {
 
  public:
   ScriptWrappable() {}
+
+  bool isScriptWrappable() const override { return true; }
 
   template <typename T>
   T* toImpl() {
@@ -166,12 +169,20 @@ class CORE_EXPORT ScriptWrappable : public TraceWrapperBase {
  private:
   // These classes are exceptionally allowed to use mainWorldWrapper().
   friend class DOMDataStore;
+  friend class HeapSnaphotWrapperVisitor;
   friend class V8HiddenValue;
   friend class V8PrivateProperty;
   friend class WebGLRenderingContextBase;
 
   v8::Local<v8::Object> mainWorldWrapper(v8::Isolate* isolate) const {
     return v8::Local<v8::Object>::New(isolate, m_mainWorldWrapper);
+  }
+
+  // Only use when really necessary, i.e., when passing over this
+  // ScriptWrappable's reference to V8. Should only be needed by GC
+  // infrastructure.
+  const v8::Persistent<v8::Object>* rawMainWorldWrapper() const {
+    return &m_mainWorldWrapper;
   }
 
   v8::Persistent<v8::Object> m_mainWorldWrapper;

third_party/WebKit/Source/bindings/core/v8/ScriptWrappableVisitor.cpp
@@ -80,6 +80,7 @@ void ScriptWrappableVisitor::performCleanup() {
   if (!m_shouldCleanup)
     return;
 
+  CHECK(!m_tracingInProgress);
   for (auto header : m_headersToUnmark) {
     // Dead objects residing in the marking deque may become invalid due to
     // minor garbage collections and are therefore set to nullptr. We have

third_party/WebKit/Source/bindings/core/v8/ScriptWrappableVisitor.h
@@ -169,6 +169,9 @@ class CORE_EXPORT ScriptWrappableVisitor : public v8::EmbedderHeapTracer,
     return &m_headersToUnmark;
   }
 
+  // Immediately cleans up all wrappers if necessary.
+  void performCleanup();
+
  protected:
   bool pushToMarkingDeque(
       void (*traceWrappersCallback)(const WrapperVisitor*, const void*),
@@ -189,7 +192,6 @@ class CORE_EXPORT ScriptWrappableVisitor : public v8::EmbedderHeapTracer,
     return true;
   }
 
- private:
   // Returns true if wrapper tracing is currently in progress, i.e.,
   // TracePrologue has been called, and TraceEpilogue has not yet been called.
   bool m_tracingInProgress = false;
@@ -207,9 +209,6 @@ class CORE_EXPORT ScriptWrappableVisitor : public v8::EmbedderHeapTracer,
   // Indicates whether cleanup should currently happen. The flag is used to
   // avoid cleaning up in the next GC cycle.
   bool m_shouldCleanup = false;
-
-  // Immediately cleans up all wrappers.
-  void performCleanup();
 
   // Schedule an idle task to perform a lazy (incremental) clean up of
   // wrappers.

third_party/WebKit/Source/bindings/core/v8/V8GCController.h
@@ -34,6 +34,7 @@
 #include "core/CoreExport.h"
 #include "platform/heap/Handle.h"
 #include "wtf/Allocator.h"
+#include <v8-profiler.h>
 #include <v8.h>
 
 namespace blink {
@@ -59,6 +60,8 @@ class CORE_EXPORT V8GCController {
 
   static void traceDOMWrappers(v8::Isolate*, Visitor*);
   static bool hasPendingActivity(v8::Isolate*, ExecutionContext*);
+
+  static v8::HeapProfiler::RetainerInfos getRetainerInfos(v8::Isolate*);
 };
 
 }  // namespace blink

third_party/WebKit/Source/bindings/core/v8/V8Initializer.cpp
@@ -421,9 +421,11 @@ void V8Initializer::initializeMainThread() {
 
   isolate->SetPromiseRejectCallback(promiseRejectHandlerInMainThread);
 
-  if (v8::HeapProfiler* profiler = isolate->GetHeapProfiler())
+  if (v8::HeapProfiler* profiler = isolate->GetHeapProfiler()) {
     profiler->SetWrapperClassInfoProvider(
         WrapperTypeInfo::NodeClassId, &RetainedDOMInfo::createRetainedDOMInfo);
+    profiler->SetGetRetainerInfosCallback(&V8GCController::getRetainerInfos);
+  }
 
   ASSERT(ThreadState::mainThreadState());
   ThreadState::mainThreadState()->addInterruptor(

commit 136b518f400b3ceaaa03add3ecffbbb2c8a100eb

[wrapper-tracing] Fix V8GCController::getRetainerInfos
Fix issues introduced in
  932c34b4cc3fb8f05c03afa5fd2827b738170bb6 [formerly a34d185b3e31187fb8aa16b700e5837e9636cba9]

BUG=chromium:679724
TBR=haraken@chromium.org,jochen@chromium.org

Review-Url: https://codereview.chromium.org/2650783002
Cr-Commit-Position: refs/heads/master@{#445489}

Former-commit-id: 124f15acfc2ed99a2e163191e6b24ed5dedb103a

third_party/WebKit/Source/bindings/core/v8/V8GCController.cpp
@@ -234,7 +234,7 @@ class HeapSnaphotWrapperVisitor : public ScriptWrappableVisitor,
   v8::HeapProfiler::RetainerChildren findV8WrappersDirectlyReachableFrom(
       Node* traceable) {
     CHECK(m_foundV8Wrappers.empty());
-    WTF::AutoReset<bool>(&m_onlyTraceSingleLevel, true);
+    WTF::AutoReset<bool> scope(&m_onlyTraceSingleLevel, true);
     m_firstScriptWrappableTraced = false;
     m_currentParent =
         &v8::Persistent<v8::Value>::Cast(*traceable->rawMainWorldWrapper());
@@ -273,11 +273,12 @@ class HeapSnaphotWrapperVisitor : public ScriptWrappableVisitor,
 // for the provided information itself.
 v8::HeapProfiler::RetainerInfos V8GCController::getRetainerInfos(
     v8::Isolate* isolate) {
-  std::unique_ptr<HeapSnaphotWrapperVisitor> tracer(
-      new HeapSnaphotWrapperVisitor(isolate));
   V8PerIsolateData::TemporaryScriptWrappableVisitorScope scope(
-      isolate, std::move(tracer));
+      isolate, std::unique_ptr<HeapSnaphotWrapperVisitor>(
+                   new HeapSnaphotWrapperVisitor(isolate)));
 
+  HeapSnaphotWrapperVisitor* tracer =
+      reinterpret_cast<HeapSnaphotWrapperVisitor*>(scope.currentVisitor());
   tracer->collectV8Roots();
   tracer->traceV8Roots();
   tracer->tracePendingActivities();

third_party/WebKit/Source/bindings/core/v8/V8PerIsolateData.cpp
@@ -409,12 +409,13 @@ void V8PerIsolateData::addActiveScriptWrappable(
 void V8PerIsolateData::TemporaryScriptWrappableVisitorScope::
     swapWithV8PerIsolateDataVisitor(
         std::unique_ptr<ScriptWrappableVisitor>& visitor) {
-  ScriptWrappableVisitor* current = currentVisitor(m_isolate);
+  ScriptWrappableVisitor* current = currentVisitor();
   if (current)
     current->performCleanup();
 
-  V8PerIsolateData::from(m_isolate)->m_scriptWrappableVisitor.swap(visitor);
-  m_isolate->SetEmbedderHeapTracer(visitor.get());
+  V8PerIsolateData::from(m_isolate)->m_scriptWrappableVisitor.swap(
+      m_savedVisitor);
+  m_isolate->SetEmbedderHeapTracer(currentVisitor());
 }
 
 }  // namespace blink

third_party/WebKit/Source/bindings/core/v8/V8PerIsolateData.h
@@ -175,18 +175,18 @@ class CORE_EXPORT V8PerIsolateData {
     TemporaryScriptWrappableVisitorScope(
         v8::Isolate* isolate,
         std::unique_ptr<ScriptWrappableVisitor> visitor)
-        : m_isolate(isolate) {
-      swapWithV8PerIsolateDataVisitor(visitor);
+        : m_isolate(isolate), m_savedVisitor(std::move(visitor)) {
+      swapWithV8PerIsolateDataVisitor(m_savedVisitor);
     }
     ~TemporaryScriptWrappableVisitorScope() {
       swapWithV8PerIsolateDataVisitor(m_savedVisitor);
     }
 
-   private:
-    inline ScriptWrappableVisitor* currentVisitor(v8::Isolate* isolate) {
+    inline ScriptWrappableVisitor* currentVisitor() {
       return V8PerIsolateData::from(m_isolate)->scriptWrappableVisitor();
     }
 
+   private:
     void swapWithV8PerIsolateDataVisitor(
         std::unique_ptr<ScriptWrappableVisitor>&);
 