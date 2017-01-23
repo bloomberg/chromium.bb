f237166b8a702642db69d7fcedc30c4ee195abc7
maxbogue <maxbogue@chromium.org>
2017-01-23 11:18:17 -0800

    [Sync] Integ tests can't compare progress markers during configuration.
    GetActiveDataTypes() returns an empty set during configuration, so
    ProgressMarkersMatch could return a false positive if either service was
    in the middle of configuration during a check.
    
    BUG=637023
    
    Review-Url: https://codereview.chromium.org/2643643011
    Cr-Commit-Position: refs/heads/master@{#445430}
    
    Former-commit-id: c4706b070fab0bee61323c702cb91e3405070cc5

--- chrome/browser/sync/test/integration/quiesce_status_change_checker.cc
+++ chrome/browser/sync/test/integration/quiesce_status_change_checker.cc
@@ -23,6 +23,12 @@ bool IsSyncDisabled(browser_sync::ProfileSyncService* service) {
 // Returns true if these services have matching progress markers.
 bool ProgressMarkersMatch(const browser_sync::ProfileSyncService* service1,
                           const browser_sync::ProfileSyncService* service2) {
+  // GetActiveDataTypes() is always empty during configuration, so progress
+  // markers cannot be compared.
+  if (!service1->ConfigurationDone() || !service2->ConfigurationDone()) {
+    return false;
+  }
+
   const syncer::ModelTypeSet common_types =
       Intersection(service1->GetActiveDataTypes(),
                    service2->GetActiveDataTypes());
