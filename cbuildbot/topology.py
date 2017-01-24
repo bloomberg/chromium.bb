9c199469c30d994e550273f0f688b8d0c84ca2e6
David Riley <davidriley@chromium.org>
2017-01-23 18:34:52 -0800

    topology: add keys for Logdog, MILO and Sheriff-o-Matic
    BUG=chromium:639901
    TEST=run_tests
    
    Change-Id: I52005d15f05ccb2c810d862d846f25b078272869
    Reviewed-on: https://chromium-review.googlesource.com/431539
    Commit-Ready: David Riley <davidriley@chromium.org>
    Tested-by: David Riley <davidriley@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>

--- cbuildbot/topology.py
+++ cbuildbot/topology.py
@@ -22,6 +22,9 @@ ELASTIC_SEARCH_HOST_KEY = '/statsd/es_host'
 ELASTIC_SEARCH_PORT_KEY = '/statsd/es_port'
 ELASTIC_SEARCH_UDP_PORT_KEY = '/statsd/es_udp_port'
 SWARMING_PROXY_HOST_KEY = '/swarming_proxy/host'
+LUCI_LOGDOG_HOST_KEY = '/luci-logdog/host'
+LUCI_MILO_HOST_KEY = '/luci-milo/host'
+SHERIFFOMATIC_HOST_KEY = '/sheriffomatic/host'
 
 TOPOLOGY_DEFAULTS = {
     STATSD_HOST_KEY : '146.148.70.158',
@@ -29,7 +32,10 @@ TOPOLOGY_DEFAULTS = {
     ELASTIC_SEARCH_HOST_KEY : '146.148.70.158',
     ELASTIC_SEARCH_PORT_KEY : '9200',
     ELASTIC_SEARCH_UDP_PORT_KEY : '9700',
-    SWARMING_PROXY_HOST_KEY: 'fake_swarming_server'
+    SWARMING_PROXY_HOST_KEY: 'fake_swarming_server',
+    LUCI_LOGDOG_HOST_KEY: 'luci-logdog.appspot.com',
+    LUCI_MILO_HOST_KEY: 'luci-milo.appspot.com',
+    SHERIFFOMATIC_HOST_KEY: 'sheriff-o-matic-staging.appspot.com',
 }
 
 
