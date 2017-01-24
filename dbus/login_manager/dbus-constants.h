9c74156d9ec2f4032acf0e193ac09c47b540df48
yusukes <yusukes@google.com>
2017-01-23 21:23:36 -0800

    dbus: login_manager: Remove PrioritizeArcInstance
    The interface is deprecated and is no longer used.
    
    BUG=chromium:684147
    TEST=None
    
    Change-Id: Ia6a884368cfbfe1ad6dbc83c14a498dcc6503937
    Reviewed-on: https://chromium-review.googlesource.com/431823
    Commit-Ready: Yusuke Sato <yusukes@chromium.org>
    Tested-by: Yusuke Sato <yusukes@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>

--- dbus/login_manager/dbus-constants.h
+++ dbus/login_manager/dbus-constants.h
@@ -43,7 +43,6 @@ const char kSessionManagerInitMachineInfo[] = "InitMachineInfo";
 const char kSessionManagerCheckArcAvailability[] = "CheckArcAvailability";
 const char kSessionManagerStartArcInstance[] = "StartArcInstance";
 const char kSessionManagerStopArcInstance[] = "StopArcInstance";
-const char kSessionManagerPrioritizeArcInstance[] = "PrioritizeArcInstance";
 const char kSessionManagerSetArcCpuRestriction[] = "SetArcCpuRestriction";
 const char kSessionManagerEmitArcBooted[] = "EmitArcBooted";
 const char kSessionManagerGetArcStartTimeTicks[] = "GetArcStartTimeTicks";
