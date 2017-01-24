4d6a16c01d89d20aef01ac104e21a86bb93a12e9
jeffcarp <jeffcarp@chromium.org>
2017-01-23 19:48:17 -0800

    Add license header to wpt_github_mock.py
    BUG=
    R=qyearsley@chromium.org
    
    Review-Url: https://codereview.chromium.org/2651683003
    Cr-Commit-Position: refs/heads/master@{#445632}
    
    Former-commit-id: 65d897bce4cf52cc82882453945dd93a6460d4cc

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/wpt_github_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/wpt_github_mock.py
@@ -1,3 +1,8 @@
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
 class MockWPTGitHub(object):
 
     def __init__(self, pull_requests, unsuccessful_merge=False):
