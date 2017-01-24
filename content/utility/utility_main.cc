d9c9f0c673c45c594afbb104c2733a8fad52a0c4
leon.han <leon.han@intel.com>
2017-01-23 20:41:55 -0800

    Do not create base::HighResolutionTimerManager in service utility process
    Before mojofication of power monitor IPCs, although service utility process
    would create base::PowerMonitor, it can't receive any IPC about power
    monitor changes because corresponding ServiceUtilityProcessHost does not create
    the PowerMonitorMessageBroadcaster instance sending power monitor changes. Then
    service utility process enables HiRes-Timer no matter on battery or not, this
    is supposed to be incorrect behavior, the expected behavior should be that
    base::HighResolutionTimerManager can enable/disable HiRes-Timer correctly.
    
    Currently power monitor has been mojofied and changed to be hosted in Device
    Service, because service utility process has no connection to service manager,
    it can not connect to Device Service, so base::PowerMonitor can not work, means
    base::HighResolutionTimerManager still can not work.
    This CL
      - Avoids creating base::HighResolutionTimerManager in service utility process,
        this will make HiRes-Timer always be disabled by default. As currently
        service utiltiy process does not really use HiRes-Timer, this change has no
        any bad effect.
      - Adds a TODO to enable service utility process to have a service manager
        connection, after that we can make base::HighResolutionTimerManager work
        correctly for future possible usage of HiRes-Timer in service utility
        process.
    
    BUG=680400
    
    Review-Url: https://codereview.chromium.org/2629873002
    Cr-Commit-Position: refs/heads/master@{#445646}
    
    Former-commit-id: 8f88196e6d6f9f81cb3bb05e3baca460a7df91b6

--- content/utility/utility_main.cc
+++ content/utility/utility_main.cc
@@ -5,6 +5,8 @@
 #include "base/command_line.h"
 #include "base/debug/leak_annotations.h"
 #include "base/message_loop/message_loop.h"
+#include "base/optional.h"
+#include "base/power_monitor/power_monitor.h"
 #include "base/run_loop.h"
 #include "base/threading/platform_thread.h"
 #include "base/timer/hi_res_timer_manager.h"
@@ -40,7 +42,22 @@ int UtilityMain(const MainFunctionParams& parameters) {
   ChildProcess utility_process;
   utility_process.set_main_thread(new UtilityThreadImpl());
 
-  base::HighResolutionTimerManager hi_res_timer_manager;
+  // Both utility process and service utility process would come
+  // here, but the later is launched without connection to service manager, so
+  // there has no base::PowerMonitor be created(See ChildThreadImpl::Init()).
+  // As base::PowerMonitor is necessary to base::HighResolutionTimerManager, for
+  // such case we just disable base::HighResolutionTimerManager for now.
+  // Note that disabling base::HighResolutionTimerManager means high resolution
+  // timer is always disabled no matter on battery or not, but it should have
+  // no any bad influence because currently service utility process is not using
+  // any high resolution timer.
+  // TODO(leonhsl): Once http://crbug.com/646833 got resolved, re-enable
+  // base::HighResolutionTimerManager here for future possible usage of high
+  // resolution timer in service utility process.
+  base::Optional<base::HighResolutionTimerManager> hi_res_timer_manager;
+  if (base::PowerMonitor::Get()) {
+    hi_res_timer_manager.emplace();
+  }
 
 #if defined(OS_WIN)
   bool no_sandbox = parameters.command_line.HasSwitch(switches::kNoSandbox);
