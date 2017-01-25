08cf2b54ae661ad6b06e4fb1ddcb7ab141d1bdad
tbarzic <tbarzic@chromium.org>
2017-01-25 15:51:01 -0800

    Restore auto-launched state on kiosk restart within session
    When kiosk app is restarted within a kiosk session - e.g. due to crash,
    app's auto-launched state has to be preserved. Previously auto-launched
    state was kept only in memory - if kiosk was restarted, the information
    would get lost.
    This CL is attempt to fix the problem by preserving auto-launched state
    by setting a user session scoped flag that indicates that the kiosk app
    is auto-launched (newly introduced app-auto-launched). The flag will be
    scoped to current used session, and will be passed to Chrome in a case
    Chrome restarts withing the user session. On restart, the flag can be
    used to deduce whether the kiosk app should be considered auto-launched
    or not.
    
    Note that when setting the auto-launched flag, it's important to
    consider current set of policy defined flags - they should be
    preserved when auto-launched flag is added to set of user flags.
    
    BUG=677340
    
    Review-Url: https://codereview.chromium.org/2639033002
    Cr-Commit-Position: refs/heads/master@{#446166}
    
    Former-commit-id: 663f7dfaa3b96b353ade5b9ce6b7616e51ba73bb

--- chrome/browser/chromeos/app_mode/kiosk_app_manager.h
+++ chrome/browser/chromeos/app_mode/kiosk_app_manager.h
@@ -26,6 +26,10 @@ class GURL;
 class PrefRegistrySimple;
 class Profile;
 
+namespace base {
+class CommandLine;
+}
+
 namespace extensions {
 class Extension;
 class ExternalLoader;
@@ -316,6 +320,15 @@ class KioskAppManager : public KioskAppDataDelegate,
 
   // Returns the auto launch delay.
   base::TimeDelta GetAutoLaunchDelay() const;
+
+  // Gets list of user switches that should be passed to Chrome in case current
+  // session has to be restored, e.g. in case of a crash. The switches will be
+  // returned as |switches| command line arguments.
+  // Returns whether the set of switches would have to be changed in respect to
+  // the current set of switches - if that is not the case |switches| might not
+  // get populated.
+  bool GetSwitchesForSessionRestore(const std::string& app_id,
+                                    base::CommandLine* switches);
 
   // True if machine ownership is already established.
   bool ownership_established_;
