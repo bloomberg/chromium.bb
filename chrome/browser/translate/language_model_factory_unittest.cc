d89521051ea202efddab7f2112021cad7e518e28
jkrcal <jkrcal@chromium.org>
2017-01-26 12:01:28 -0800

    [LanguageModel] Test that the model is not created in icognito
    Currently the language model is not created in incognito mode (as
    desired) due to generic code in BrowserContextKeyedServiceFactory.
    
    This CL adds an explicit unit-test for skipping incognito (data collected by the LanguageModel is privacy-sensitive as discussed in recent crbug.com/681876).
    
    Review-Url: https://codereview.chromium.org/2654873005
    Cr-Commit-Position: refs/heads/master@{#446414}
    
    Former-commit-id: 2be0a8b81da0eac3830b6f3d22bc2f0296c4035a

---
+++ chrome/browser/translate/language_model_factory_unittest.cc
@@ -0,0 +1,24 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/translate/language_model_factory.h"
+
+#include "chrome/test/base/testing_profile.h"
+#include "content/public/test/test_browser_thread_bundle.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+using testing::IsNull;
+using testing::Not;
+
+TEST(LanguageModelFactoryTest, NotCreatedInIncognito) {
+  content::TestBrowserThreadBundle thread_bundle;
+  TestingProfile profile;
+
+  EXPECT_THAT(LanguageModelFactory::GetForBrowserContext(&profile),
+              Not(IsNull()));
+
+  Profile* incognito = profile.GetOffTheRecordProfile();
+  ASSERT_THAT(incognito, Not(IsNull()));
+  EXPECT_THAT(LanguageModelFactory::GetForBrowserContext(incognito), IsNull());
+}
