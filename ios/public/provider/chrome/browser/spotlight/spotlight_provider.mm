0f5a885a2a160e91312365191d3b7b2ce50a3de1
stkhapugin <stkhapugin@chromium.org>
2017-01-30 02:43:05 -0800

    [ObjC ARC] Converts ios/public/provider/chrome/browser/spotlight:spotlight to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2655553005
    Cr-Commit-Position: refs/heads/master@{#446967}
    
    Former-commit-id: 7e16d275194300325492301d27e8bc51d800ccaf

--- ios/public/provider/chrome/browser/spotlight/spotlight_provider.mm
+++ ios/public/provider/chrome/browser/spotlight/spotlight_provider.mm
@@ -4,6 +4,10 @@
 
 #import "ios/public/provider/chrome/browser/spotlight/spotlight_provider.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 bool SpotlightProvider::IsSpotlightEnabled() {
   return false;
 }
