21358e4f2ffc8e01d9aba22b3b4d1533d225a56d
imcheng <imcheng@chromium.org>
2017-01-30 19:59:32 -0800

    [MR tests] Assert initial state / state change for reconnect case.
    Followup to https://codereview.chromium.org/2653683006/.
    
    Refactor logic that asserts initial state of a PresentationConnection
    into a helper method and also use it in the reconnect success case.
    
    BUG=684664
    
    Review-Url: https://codereview.chromium.org/2654003002
    Cr-Commit-Position: refs/heads/master@{#447184}
    
    Former-commit-id: 54e7828cdcb38fda68c7a5bbc7ea1d7896f96588

--- chrome/test/media_router/resources/common.js
+++ chrome/test/media_router/resources/common.js
@@ -71,24 +71,33 @@ function checkSession() {
       } else {
         // set the new session
         startedConnection = session;
-        console.log('connection state is "' + startedConnection.state + '"');
-        if (startedConnection.state == "connected") {
-          sendResult(true, '');
-        } else if (startedConnection.state == "connecting") {
-          startedConnection.onconnect = () => {
-            sendResult(true, '');
-          };
-        } else {
-          sendResult(false,
-            'Expect connection state to be "connecting" or "connected", ' +
-            'actual "' + startedConnection.state + '"');
-        }
+        waitForConnectedStateAndSendResult(startedConnection);
       }
     }).catch(function(e) {
       // terminate old session if exists
       startedConnection && startedConnection.terminate();
       sendResult(false, 'Failed to start session: encountered exception ' + e);
     })
+  }
+}
+
+/**
+ * Asserts the current state of the connection is 'connected' or 'connecting'.
+ * If the current state is connecting, waits for it to become 'connected'.
+ * @param {!PresentationConnection} connection
+ */
+function waitForConnectedStateAndSendResult(connection) {
+  console.log(`connection state is "${connection.state}"`);
+  if (connection.state == 'connected') {
+    sendResult(true, '');
+  } else if (connection.state == 'connecting') {
+    connection.onconnect = () => {
+      sendResult(true, '');
+    };
+  } else {
+    sendResult(false,
+      `Expect connection state to be "connecting" or "connected", actual: \
+      "${connection.state}"`);
   }
 }
 
@@ -184,7 +193,7 @@ function reconnectSession(sessionId) {
       sendResult(false, 'reconnectSession returned null session');
     } else {
       reconnectedSession = session;
-      sendResult(true, '');
+      waitForConnectedStateAndSendResult(reconnectedSession);
     }
   }).catch(function(error) {
     sendResult(false, 'reconnectSession failed: ' + error.message);
