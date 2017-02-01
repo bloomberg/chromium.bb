b8b04b4ce2f1d856ce470503fab8cd6d81de7a34
eakuefner <eakuefner@chromium.org>
2017-02-01 13:12:19 -0800

    [BotHealth] Disable speedometer benchmarks on Android
    BUG=687681
    
    Review-Url: https://codereview.chromium.org/2671463003
    Cr-Commit-Position: refs/heads/master@{#447609}
    
    Former-commit-id: 2dfbf027e6a3d4aaee892c9897c28bd18a39dabb

--- tools/perf/benchmarks/speedometer.py
+++ tools/perf/benchmarks/speedometer.py
@@ -91,6 +91,7 @@ class SpeedometerMeasurement(legacy_page_test.LegacyPageTest):
     keychain_metric.KeychainMetric().AddResults(tab, results)
 
 
+@benchmark.Disabled('android')  # crbug.com/687681
 class Speedometer(perf_benchmark.PerfBenchmark):
   test = SpeedometerMeasurement
 
@@ -109,7 +110,8 @@ class Speedometer(perf_benchmark.PerfBenchmark):
     return ps
 
 
-@benchmark.Disabled('reference')  # crbug.com/579546
+# crbug.com/579546 (ref), crbug.com/687681 (android)
+@benchmark.Disabled('reference', 'android')
 class SpeedometerIgnition(Speedometer):
   def SetExtraBrowserOptions(self, options):
     super(SpeedometerIgnition, self).SetExtraBrowserOptions(options)
@@ -120,6 +122,7 @@ class SpeedometerIgnition(Speedometer):
     return 'speedometer-ignition'
 
 
+@benchmark.Disabled('android')  # crbug.com/687681
 class SpeedometerTurbo(Speedometer):
   def SetExtraBrowserOptions(self, options):
     super(SpeedometerTurbo, self).SetExtraBrowserOptions(options)
