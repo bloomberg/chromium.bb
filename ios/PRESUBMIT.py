5cf2bfb06a079e20d38be304e41bfc2690e8f990
sdefresne <sdefresne@chromium.org>
2017-02-01 09:03:14 -0800

    Enfore "clang-format" for all of src/ios/.
    As we're adding more and more code outside of stc/ios/{web,chrome},
    add a presubmit check enforcing "clang-format" for all of src/ios/
    instead of limiting it to select sub-directories of src/ios/.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2665383002
    Cr-Commit-Position: refs/heads/master@{#447541}
    
    Former-commit-id: b742db2fa3101ae54b7de497497b0643cf723ad4

--- ios/PRESUBMIT.py
+++ ios/PRESUBMIT.py
@@ -1,8 +1,8 @@
-# Copyright 2015 The Chromium Authors. All rights reserved.
+# Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Presubmit script for ios/chrome.
+"""Presubmit script for ios.
 
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
