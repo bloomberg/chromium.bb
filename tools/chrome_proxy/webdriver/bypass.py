8b77026bcb640c1cbcf66a6b67c517bbae87a726
ryansturm <ryansturm@chromium.org>
2017-01-31 18:35:50 -0800

    Adding a CORS bypass integration test
    This test verifies that CORS requests are treated with block-once by the
    data reduction proxy server.
    
    BUG=680556
    
    Review-Url: https://codereview.chromium.org/2661363002
    Cr-Commit-Position: refs/heads/master@{#447422}
    
    Former-commit-id: 7d56868afb2fd253cfe318d2e39fe591a458f0db

--- tools/chrome_proxy/webdriver/bypass.py
+++ tools/chrome_proxy/webdriver/bypass.py
@@ -23,5 +23,31 @@ class Bypass(IntegrationTest):
       for response in t.GetHTTPResponses():
         self.assertNotHasChromeProxyViaHeader(response)
 
+  # Verify that CORS requests receive a block-once from the data reduction
+  # proxy by checking that those requests are retried without data reduction
+  # proxy.
+  def testCorsBypass(self):
+    with TestDriver() as test_driver:
+      test_driver.AddChromeArg('--enable-spdy-proxy-auth')
+      test_driver.LoadURL('http://www.gstatic.com/chrome/googlezip/cors/')
+
+      # Navigate to a different page to verify that later requests are not
+      # blocked.
+      test_driver.LoadURL('http://check.googlezip.net/test.html')
+
+      cors_requests = 0
+      same_origin_requests = 0
+      for response in test_driver.GetHTTPResponses():
+        # The origin header implies that |response| is a CORS request.
+        if ('origin' not in response.request_headers):
+          self.assertHasChromeProxyViaHeader(response)
+          same_origin_requests = same_origin_requests + 1
+        else:
+          self.assertNotHasChromeProxyViaHeader(response)
+          cors_requests = cors_requests + 1
+      # Verify that both CORS and same origin requests were seen.
+      self.assertNotEqual(0, same_origin_requests)
+      self.assertNotEqual(0, cors_requests)
+
 if __name__ == '__main__':
   IntegrationTest.RunAllTests()
