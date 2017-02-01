9039cfdfbbafa0c442ce97f4a90e9ad3af42e800
aazzam <aazzam@google.com>
2017-01-31 21:54:52 -0800

    Added api_methods flag to CSSProperties.json5.
    A part of Project Ribbon, separating the parsing logic for CSS
    properties from the parser into an API.
    
    This patch adds an api_methods=["method1", "method2"] flag to
    CSSProperties.json5, which specifies which methods have been
    implemented in the API implementation of that property. This is so
    that when API .h files are generated, only methods implemented for
    that property are defined.
    
    It also adds "parseSingleValue" to the list of api_methods for all
    existing properties with an API that implements parseSingleValue.
    
    Before, when a method was added to the API, this method had to be
    implemented in all API files - however, not every API should hold
    each method. In this new design, to add a method to the API:
    - Add the method name to the valid_values array in CSSProperties.json5.
    - Add the method to CSSPropertyAPIFiles.h.tmpl
    - Add the method name to the api_methods list in CSSProperties.json5
      for each property where it is implemented.
    
    BUG=668012
    
    Review-Url: https://codereview.chromium.org/2654403003
    Cr-Original-Commit-Position: refs/heads/master@{#447427}
    Committed: https://chromium.googlesource.com/chromium/src/+/36cd79e92e8439b3aead093bcf42184357e46f15 [formerly a252f0fa5f780f176eb3388df35ec67abaf8ca79]
    Review-Url: https://codereview.chromium.org/2654403003
    Cr-Commit-Position: refs/heads/master@{#447459}
    
    Former-commit-id: d965a330d470c634f1010dd90bc20cfb1e81971f

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPI.h
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPI.h
@@ -18,18 +18,21 @@ class CSSParserTokenRange;
 // logic inside the blink style engine. All specific properties are subclasses
 // of CSSPropertyAPI.
 //
-// To add a new property using this API:
-// - Make a class that implements CSSPropertyAPI, and implement the static
-//   methods.
-// - Update the cssPropertyDescriptors array in CSSPropertyDescriptor.cpp to
-//   call GET_DESCRIPTOR(classname).
+// To add a new implementation of this API for a property:
+// - Make a class that implements CSSPropertyAPI.
+// - For each method that you wish to implement in this class, add this method
+//   name to the api_methods flag in CSSProperties.json5.
+// - Implement these methods in the .cpp file.
 //
-// To add new functions using this API:
-// - New functions and static variables can be added in this class. A default
-//   implementation of functions can optionally be provided.
-// - When adding new functions, also add them to GET_DESCRIPTOR, and the get()
-//   method in CSSPropertyDescriptors.cpp, and the descriptor struct in
-//   CSSPropertyDescriptor.h.
+// To add new functions to this API:
+// - Add the function to the struct below.
+// - Add the function to CSSPropertyAPIFiles.h.tmpl template file, surrounded
+//   with an {% if "methodName" in implementedMethods %} check.
+// - Add the function name to the valid_methods array in
+//   make_css_property_apis.py.
+// - Add function to the initializer list in the cssPropertyDescriptors array
+//   in CSSPropertyDescriptor.cpp.tmpl, and add a nullptr to the invalid
+//   CSSPropertyDescriptor.
 class CSSPropertyAPI {
   STATIC_ONLY(CSSPropertyAPI);
 
@@ -37,7 +40,12 @@ class CSSPropertyAPI {
   // Parses a single CSS property and returns the corresponding CSSValue. If the
   // input is invalid it returns nullptr.
   static const CSSValue* parseSingleValue(CSSParserTokenRange&,
-                                          const CSSParserContext*);
+                                          const CSSParserContext*) {
+    // No code should reach here, since properties either have their own
+    // implementations of this method or store nullptr in their descriptor.
+    NOTREACHED();
+    return nullptr;
+  }
 };
 
 }  // namespace blink
