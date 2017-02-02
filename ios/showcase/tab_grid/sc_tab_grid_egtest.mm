716cd69561223ff23f5234bebfef535831cc6204
gambard <gambard@chromium.org>
2017-02-02 01:37:48 -0800

    Disable SCTabGridTestCase.testLaunchAndTappingCell on device
    SCTabGridTestCase.testLaunchAndTappingCell is flaky on iPhone, disabling it
    on devices.
    
    BUG=687865
    
    Review-Url: https://codereview.chromium.org/2670663004
    Cr-Commit-Position: refs/heads/master@{#447732}
    
    Former-commit-id: 0e38f6a240a50e71d3a280deacf36dfbb8c65ee1

--- ios/showcase/tab_grid/sc_tab_grid_egtest.mm
+++ ios/showcase/tab_grid/sc_tab_grid_egtest.mm
@@ -5,6 +5,7 @@
 #import <EarlGrey/EarlGrey.h>
 
 #import "ios/showcase/test/showcase_test_case.h"
+#import "ios/testing/earl_grey/disabled_test_macros.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -18,6 +19,11 @@
 
 // Tests launching TabGridViewController and tapping a cell.
 - (void)testLaunchAndTappingCell {
+// TODO(crbug.com/687865): enable the test. It was flaky on device.
+#if !TARGET_IPHONE_SIMULATOR
+  EARL_GREY_TEST_DISABLED(
+      @"Disabled for devices because it is flaky on iPhone");
+#endif
   [[EarlGrey selectElementWithMatcher:grey_text(@"TabGridViewController")]
       performAction:grey_tap()];
   [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"Tab 0_button")]
