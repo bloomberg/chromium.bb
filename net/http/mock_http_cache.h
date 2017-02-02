9d1bc3f5228e3bd60ae8c6184cd4ba820882fcd8
zhongyi <zhongyi@chromium.org>
2017-02-01 18:26:42 -0800

    Change MockHttpCache constructor to take boolean to set up quic server info factory.
    BUG=686632
    
    Review-Url: https://codereview.chromium.org/2668563002
    Cr-Commit-Position: refs/heads/master@{#447690}
    
    Former-commit-id: fcd1397490a75238764689ac720275a83721fa2d

--- net/http/mock_http_cache.h
+++ net/http/mock_http_cache.h
@@ -182,6 +182,12 @@ class MockHttpCache {
   MockHttpCache();
   explicit MockHttpCache(
       std::unique_ptr<HttpCache::BackendFactory> disk_cache_factory);
+  // |set_up_quic_server_info| if set, will set a quic server info factory
+  // adptor.
+  explicit MockHttpCache(bool set_up_quic_server_info);
+
+  MockHttpCache(std::unique_ptr<HttpCache::BackendFactory> disk_cache_factory,
+                bool set_up_quic_server_info);
 
   HttpCache* http_cache() { return &http_cache_; }
 
