wjdz99/aom

EC_MULTISYMBOL: Add block zero (CBP) counts and probs.
Preparation for merging EOB_TOKEN. The block_zero value
corresponds to the first EOB_TOKEN: other EOB_TOKEN values will
be merged with non-zero values.

Change-Id: I94036783ee240fa916a79c544ecd716a9c24fa59

av1/common/entropy.h
@@ -202,6 +202,8 @@ void av1_model_to_full_probs(const aom_prob *model, aom_prob *full);
 #if CONFIG_EC_MULTISYMBOL
 typedef aom_cdf_prob coeff_cdf_model[REF_TYPES][COEF_BANDS][COEFF_CONTEXTS]
                                     [ENTROPY_TOKENS];
+typedef aom_prob av1_cbp_probs_model[REF_TYPES][CBP_CONTEXTS];
+typedef unsigned int av1_cbp_count_model[REF_TYPES][CBP_CONTEXTS][2];
 extern const aom_cdf_prob av1_pareto8_token_probs[COEFF_PROB_MODELS]
                                                  [ENTROPY_TOKENS - 2];
 extern const aom_cdf_prob av1_pareto8_tail_cdfs[COEFF_PROB_MODELS]

av1/decoder/decodeframe.c
@@ -4607,6 +4607,10 @@ static void debug_check_frame_counts(const AV1_COMMON *const cm) {
   assert(!memcmp(cm->counts.coef, zero_counts.coef, sizeof(cm->counts.coef)));
   assert(!memcmp(cm->counts.eob_branch, zero_counts.eob_branch,
                  sizeof(cm->counts.eob_branch)));
+#if CONFIG_EC_MULTISYMBOL
+  assert(!memcmp(cm->counts.cbp_count, zero_counts.cbp_count,
+                 sizeof(cm->counts.cbp_count)));
+#endif
   assert(!memcmp(cm->counts.switchable_interp, zero_counts.switchable_interp,
                  sizeof(cm->counts.switchable_interp)));
   assert(!memcmp(cm->counts.inter_mode, zero_counts.inter_mode,

bloomberg/chromium.bb

av1/common/entropy.h
@@ -202,6 +202,8 @@ void av1_model_to_full_probs(const aom_prob *model, aom_prob *full);
 #if CONFIG_EC_MULTISYMBOL
 typedef aom_cdf_prob coeff_cdf_model[REF_TYPES][COEF_BANDS][COEFF_CONTEXTS]
                                     [ENTROPY_TOKENS];
+typedef aom_prob av1_blockz_probs_model[REF_TYPES][BLOCKZ_CONTEXTS];
+typedef unsigned int av1_blockz_count_model[REF_TYPES][BLOCKZ_CONTEXTS][2];
 extern const aom_cdf_prob av1_pareto8_token_probs[COEFF_PROB_MODELS]
                                                  [ENTROPY_TOKENS - 2];
 extern const aom_cdf_prob av1_pareto8_tail_cdfs[COEFF_PROB_MODELS]

av1/decoder/decodeframe.c
@@ -4622,6 +4622,10 @@ static void debug_check_frame_counts(const AV1_COMMON *const cm) {
   assert(!memcmp(cm->counts.coef, zero_counts.coef, sizeof(cm->counts.coef)));
   assert(!memcmp(cm->counts.eob_branch, zero_counts.eob_branch,
                  sizeof(cm->counts.eob_branch)));
+#if CONFIG_EC_MULTISYMBOL
+  assert(!memcmp(cm->counts.blockz_count, zero_counts.blockz_count,
+                 sizeof(cm->counts.blockz_count)));
+#endif
   assert(!memcmp(cm->counts.switchable_interp, zero_counts.switchable_interp,
                  sizeof(cm->counts.switchable_interp)));
   assert(!memcmp(cm->counts.inter_mode, zero_counts.inter_mode,