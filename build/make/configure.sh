9d3076cf9e4f9011de969fd79f83b79c39570940
Tom Finegan <tomfinegan@google.com>
2017-02-03 08:42:34 -0800

    Add macosx Sierra (v10.12) support to configure.
    Change-Id: Ib90be26f69c658a6be6e133097c41845db58b6e1

--- build/make/configure.sh
+++ build/make/configure.sh
@@ -733,6 +733,10 @@ process_common_toolchain() {
         tgt_isa=x86_64
         tgt_os=darwin15
         ;;
+       *darwin16*)
+        tgt_isa=x86_64
+        tgt_os=darwin16
+        ;;
       x86_64*mingw32*)
         tgt_os=win64
         ;;
@@ -850,6 +854,10 @@ process_common_toolchain() {
     *-darwin15-*)
       add_cflags  "-mmacosx-version-min=10.11"
       add_ldflags "-mmacosx-version-min=10.11"
+      ;;
+    *-darwin16-*)
+      add_cflags  "-mmacosx-version-min=10.12"
+      add_ldflags "-mmacosx-version-min=10.12"
       ;;
     *-iphonesimulator-*)
       add_cflags  "-miphoneos-version-min=${IOS_VERSION_MIN}"
