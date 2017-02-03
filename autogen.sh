a6cdfa5cd8cc953ffec4497cd8262059c4b14675
Eric Engestrom <eric.engestrom@imgtec.com>
2017-02-03 18:21:09 +0000

    autogen.sh: don't print old git-config values
    Old values are of no interest to the user, so let's reduce the spam
    a bit by hiding those.
    
    Signed-off-by: Eric Engestrom <eric.engestrom@imgtec.com>
    Reviewed-by: Chad Versace <chadversary@chromium.org>
    Reviewed-by: Emil Velikov <emil.velikov@collabora.com>

--- autogen.sh
+++ autogen.sh
@@ -9,10 +9,10 @@ cd "$srcdir"
 autoreconf --force --verbose --install || exit 1
 cd "$ORIGDIR" || exit $?
 
-git config --local --get format.subjectPrefix ||
+git config --local --get format.subjectPrefix >/dev/null ||
     git config --local format.subjectPrefix "PATCH libdrm" 2>/dev/null
 
-git config --local --get sendemail.to ||
+git config --local --get sendemail.to >/dev/null ||
     git config --local sendemail.to "dri-devel@lists.freedesktop.org" 2>/dev/null
 
 if test -z "$NOCONFIGURE"; then
