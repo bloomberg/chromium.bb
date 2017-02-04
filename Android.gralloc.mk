bc7dc78e795caa97ad26045b9247f1e0584e69ba
Kalyan Kondapally <kalyan.kondapally@intel.com>
2017-02-04 12:10:50 -0800

    Add support to build with Android.
    BUG=None
    TEST=Able to build Minigbm in a pure Android environment
         i.e. Android IA.
    
    Change-Id: Ib12e8411b4ba42b8dd83974fc372bf4884ed6e41
    Signed-off-by: Kalyan Kondapally <kalyan.kondapally@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/448841
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

---
+++ Android.gralloc.mk
@@ -0,0 +1,10 @@
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+LOCAL_CPP_EXTENSION := .cc
+
+LOCAL_SRC_FILES += \
+	cros_gralloc/cros_alloc_device.cc \
+	cros_gralloc/cros_gralloc_helpers.cc \
+	cros_gralloc/cros_gralloc_module.cc
