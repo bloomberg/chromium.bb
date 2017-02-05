717405e3df8c2679d00443117ecd5d797780eb7d
gogag2 <gogag2@gmail.com>
2017-02-05 07:33:02 -0800

    Move SetSinkIdResolver timer to frame-specific TaskRunnerTimer.
    Move SetSinkIdResolver timer m_timer to frame-specific TaskRunnerTimer.
    This associates it with the frame's MiscPlatformAPI timer task queue.
    It is a speced task, so we should use MiscPlatformAPI.[1]
    
    [1] The spec link: https://w3c.github.io/mediacapture-output/#htmlmediaelement-
    extensions
    
    This CL is migrating tasks/timers from a per-renderer scheduler to a per-frame
    scheduler:
    https://groups.google.com/a/chromium.org/forum/#!topic/blink-dev/80dsbEEOcWY
    
    BUG=624694
    
    Review-Url: https://codereview.chromium.org/2644153002
    Cr-Commit-Position: refs/heads/master@{#448184}
    
    Former-commit-id: 89d445684f181b98567d6181670b6f84de2c981d

--- third_party/WebKit/Source/modules/audio_output_devices/HTMLMediaElementAudioOutputDevice.cpp
+++ third_party/WebKit/Source/modules/audio_output_devices/HTMLMediaElementAudioOutputDevice.cpp
@@ -8,6 +8,7 @@
 #include "bindings/core/v8/ScriptState.h"
 #include "core/dom/DOMException.h"
 #include "core/dom/ExecutionContext.h"
+#include "core/dom/TaskRunnerHelper.h"
 #include "modules/audio_output_devices/AudioOutputDeviceClient.h"
 #include "modules/audio_output_devices/SetSinkIdCallbacks.h"
 #include "public/platform/WebSecurityOrigin.h"
@@ -36,7 +37,7 @@ class SetSinkIdResolver : public ScriptPromiseResolver {
 
   Member<HTMLMediaElement> m_element;
   String m_sinkId;
-  Timer<SetSinkIdResolver> m_timer;
+  TaskRunnerTimer<SetSinkIdResolver> m_timer;
 };
 
 SetSinkIdResolver* SetSinkIdResolver::create(ScriptState* scriptState,
@@ -55,7 +56,9 @@ SetSinkIdResolver::SetSinkIdResolver(ScriptState* scriptState,
     : ScriptPromiseResolver(scriptState),
       m_element(element),
       m_sinkId(sinkId),
-      m_timer(this, &SetSinkIdResolver::timerFired) {}
+      m_timer(TaskRunnerHelper::get(TaskType::MiscPlatformAPI, scriptState),
+              this,
+              &SetSinkIdResolver::timerFired) {}
 
 void SetSinkIdResolver::startAsync() {
   m_timer.startOneShot(0, BLINK_FROM_HERE);
