920465ca00e865bba56a807c93d42371951c491a
joedow <joedow@chromium.org>
2017-02-06 15:21:41 -0800

    Disabling SpellCheckTest on Windows due to flakiness
    TBR=tapted,groby
    BUG=689101
    
    Review-Url: https://codereview.chromium.org/2677333003
    Cr-Commit-Position: refs/heads/master@{#448440}
    
    Former-commit-id: 725955e7f9984305e32f9e0a58a51d8e48c46897

--- components/spellcheck/renderer/spellcheck_unittest.cc
+++ components/spellcheck/renderer/spellcheck_unittest.cc
@@ -489,7 +489,14 @@ TEST_F(SpellCheckTest, SpellCheckSuggestions_EN_US) {
 
 // This test verifies our spellchecker can split a text into words and check
 // the spelling of each word in the text.
-TEST_F(SpellCheckTest, SpellCheckText) {
+#if defined(OS_WIN)
+// SpellCheckTest.SpellCheckText fails on Windows.
+// See http://crbug.com/689101.
+#define MAYBE_SpellCheckText DISABLED_SpellCheckText
+#else
+#define MAYBE_SpellCheckText SpellCheckText
+#endif  // OS_WIN
+TEST_F(SpellCheckTest, MAYBE_SpellCheckText) {
   static const struct {
     const char* language;
     const wchar_t* input;
