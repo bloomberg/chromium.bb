d3ff283a611ddac7b0d3875a301a0766fcceb361
fdoray <fdoray@chromium.org>
2017-02-06 16:21:34 -0800

    Use TaskScheduler instead of blocking pool in device_storage_handler.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=fukino@chromium.org
    TBR=dbeam@chromium.org
    
    Review-Url: https://codereview.chromium.org/2674743003
    Cr-Commit-Position: refs/heads/master@{#448460}
    
    Former-commit-id: 69f89306d1b4f91d8810ef0aa2c59fef6903cb98

--- chrome/browser/ui/webui/settings/chromeos/device_storage_handler.cc
+++ chrome/browser/ui/webui/settings/chromeos/device_storage_handler.cc
@@ -10,6 +10,7 @@
 
 #include "base/files/file_util.h"
 #include "base/sys_info.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browsing_data/browsing_data_appcache_helper.h"
 #include "chrome/browser/browsing_data/browsing_data_cache_storage_helper.h"
@@ -43,9 +44,9 @@ namespace chromeos {
 namespace settings {
 namespace {
 
-void GetSizeStatOnBlockingPool(const base::FilePath& mount_path,
-                               int64_t* total_size,
-                               int64_t* available_size) {
+void GetSizeStatBlocking(const base::FilePath& mount_path,
+                         int64_t* total_size,
+                         int64_t* available_size) {
   int64_t size = base::SysInfo::AmountOfTotalDiskSpace(mount_path);
   if (size >= 0)
     *total_size = size;
@@ -144,15 +145,10 @@ void StorageHandler::UpdateSizeStat() {
   int64_t* total_size = new int64_t(0);
   int64_t* available_size = new int64_t(0);
   content::BrowserThread::PostBlockingPoolTaskAndReply(
-      FROM_HERE,
-      base::Bind(&GetSizeStatOnBlockingPool,
-                 downloads_path,
-                 total_size,
-                 available_size),
-      base::Bind(&StorageHandler::OnGetSizeStat,
-                 base::Unretained(this),
-                 base::Owned(total_size),
-                 base::Owned(available_size)));
+      FROM_HERE, base::Bind(&GetSizeStatBlocking, downloads_path, total_size,
+                            available_size),
+      base::Bind(&StorageHandler::OnGetSizeStat, base::Unretained(this),
+                 base::Owned(total_size), base::Owned(available_size)));
 }
 
 void StorageHandler::OnGetSizeStat(int64_t* total_size,
@@ -185,12 +181,11 @@ void StorageHandler::UpdateDownloadsSize() {
   const base::FilePath downloads_path =
       file_manager::util::GetDownloadsFolderForProfile(profile);
 
-  base::PostTaskAndReplyWithResult(
-      content::BrowserThread::GetBlockingPool(),
-      FROM_HERE,
+  base::PostTaskWithTraitsAndReplyWithResult(
+      FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                     base::TaskPriority::BACKGROUND),
       base::Bind(&base::ComputeDirectorySize, downloads_path),
-      base::Bind(&StorageHandler::OnGetDownloadsSize,
-                 base::Unretained(this)));
+      base::Bind(&StorageHandler::OnGetDownloadsSize, base::Unretained(this)));
 }
 
 void StorageHandler::OnGetDownloadsSize(int64_t size) {
