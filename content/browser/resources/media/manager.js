9ab8aae2e33bac3430e9d525f060b368f47dbc9c
xhwang <xhwang@chromium.org>
2017-02-07 14:56:51 -0800

    media: Add "Hide players" button in about://media-internals
    What this does is:
    - Adds a button to hide all current players on the page, but doesn't touch the C++ side to actually delete those players.
    - After a player is hidden, new logs for that player will be dropped and will not resurrect any hidden players.
    - If a user refreshes the media-internals page, or open a new one, we'll get all players back.
    
    BUG=688607
    
    Review-Url: https://codereview.chromium.org/2672193003
    Cr-Commit-Position: refs/heads/master@{#448764}
    
    Former-commit-id: 4ee2eeb27fb5cb49b1d5fa470528c16738d0274d

--- content/browser/resources/media/manager.js
+++ content/browser/resources/media/manager.js
@@ -16,6 +16,9 @@ var Manager = (function() {
     this.players_ = {};
     this.audioComponents_ = [];
     this.clientRenderer_ = clientRenderer;
+
+    this.hidePlayersButton = document.getElementById('hide-players-button');
+    this.hidePlayersButton.onclick = this.hidePlayers_.bind(this);
   }
 
   Manager.prototype = {
@@ -76,6 +79,12 @@ var Manager = (function() {
       var playerRemoved = this.players_[id];
       delete this.players_[id];
       this.clientRenderer_.playerRemoved(this.players_, playerRemoved);
+    },
+
+    hidePlayers_: function() {
+      util.object.forEach(this.players_, function(playerInfo, id) {
+        this.removePlayer(id);
+      }, this);
     },
 
     updatePlayerInfoNoRecord: function(id, timestamp, key, value) {
