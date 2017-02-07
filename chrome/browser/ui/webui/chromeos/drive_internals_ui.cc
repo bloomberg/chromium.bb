778ec04d13c5e6fe77322520c06f7d8e2273390f
fdoray <fdoray@chromium.org>
2017-02-06 17:43:16 -0800

    Use TaskScheduler instead of blocking pool in drive_internals_ui.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=kinaba@chromium.org
    
    Review-Url: https://codereview.chromium.org/2677143002
    Cr-Commit-Position: refs/heads/master@{#448503}
    
    Former-commit-id: f2b591f8f26308120c5ead9bd3352ef159ebcd07

--- chrome/browser/ui/webui/chromeos/drive_internals_ui.cc
+++ chrome/browser/ui/webui/chromeos/drive_internals_ui.cc
@@ -20,6 +20,7 @@
 #include "base/path_service.h"
 #include "base/strings/stringprintf.h"
 #include "base/sys_info.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/chromeos/drive/debug_info_collector.h"
 #include "chrome/browser/chromeos/drive/drive_integration_service.h"
 #include "chrome/browser/chromeos/drive/file_system_util.h"
@@ -669,15 +670,13 @@ void DriveInternalsWebUIHandler::UpdateGCacheContentsSection() {
   const base::FilePath root_path = drive::util::GetCacheRootPath(profile);
   base::ListValue* gcache_contents = new base::ListValue;
   base::DictionaryValue* gcache_summary = new base::DictionaryValue;
-  BrowserThread::PostBlockingPoolTaskAndReply(
-      FROM_HERE,
-      base::Bind(&GetGCacheContents,
-                 root_path,
-                 gcache_contents,
+  base::PostTaskWithTraitsAndReply(
+      FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                     base::TaskPriority::BACKGROUND),
+      base::Bind(&GetGCacheContents, root_path, gcache_contents,
                  gcache_summary),
       base::Bind(&DriveInternalsWebUIHandler::OnGetGCacheContents,
-                 weak_ptr_factory_.GetWeakPtr(),
-                 base::Owned(gcache_contents),
+                 weak_ptr_factory_.GetWeakPtr(), base::Owned(gcache_contents),
                  base::Owned(gcache_summary)));
 }
 
@@ -711,8 +710,9 @@ void DriveInternalsWebUIHandler::UpdateLocalStorageUsageSection() {
   base::FilePath home_path;
   if (PathService::Get(base::DIR_HOME, &home_path)) {
     base::DictionaryValue* local_storage_summary = new base::DictionaryValue;
-    BrowserThread::PostBlockingPoolTaskAndReply(
-        FROM_HERE,
+    base::PostTaskWithTraitsAndReply(
+        FROM_HERE, base::TaskTraits().MayBlock().WithPriority(
+                       base::TaskPriority::BACKGROUND),
         base::Bind(&GetFreeDiskSpace, home_path, local_storage_summary),
         base::Bind(&DriveInternalsWebUIHandler::OnGetFreeDiskSpace,
                    weak_ptr_factory_.GetWeakPtr(),
