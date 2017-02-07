d696c007ef53cc3dcb704cfceac7dc58c6ea5941
David Riley <davidriley@chromium.org>
2017-02-06 16:51:42 -0800

    logdog: Add methods to retrieve logs from logdog.
    BUG=chromium:639901
    TEST=logdog_unittest
    
    Change-Id: I4c7e6ddac4b54807352fb825e60c3ff9715e5cff
    Reviewed-on: https://chromium-review.googlesource.com/438775
    Commit-Ready: David Riley <davidriley@chromium.org>
    Tested-by: David Riley <davidriley@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>

--- lib/logdog.py
+++ lib/logdog.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 
 import base64
 import json
+import time
 import urllib
 
 from chromite.lib.protos import annotations_pb2
@@ -17,6 +18,10 @@ from chromite.cbuildbot import topology
 
 class LogdogResponseException(Exception):
   """Exception got from invalid Logdog response."""
+
+
+class LogdogTimeoutException(Exception):
+  """Exception got from Logdog request taking too long."""
 
 
 class LogdogClient(prpc.PRPCClient):
@@ -57,6 +62,130 @@ class LogdogClient(prpc.PRPCClient):
       return resp['streams'][0]['path']
     except KeyError:
       raise LogdogResponseException('Unable to retrieve logs path')
+
+  def LogsGet(self, project, path, state=True, index=0, byte_count=0,
+              log_count=0, dryrun=False):
+    """Logdog Logs.Get request to retrieve logs.
+
+    Args:
+      project: Logdog project of stream.
+      path: Logdog path of stream.
+      state: Boolean indicating if state descriptor should be returned.
+      index: The initial log stream index to retrieve.
+      byte_count: A suggested maximum number of bytes to return.  At least
+                  one full log line will be returned.
+      log_count: The maximum number of logs to return.
+      dryrun: Whether a dryrun.
+
+    Returns:
+      Full Logs.Get response protobuf.
+    """
+    body = json.dumps({
+        'project': project,
+        'path': path,
+        'state': bool(state),
+        'index': int(index),
+        'byte_count': int(byte_count),
+        'log_count': int(log_count),
+    })
+    return self.SendRequest('prpc/logdog.Logs', 'Get', body, dryrun=dryrun)
+
+  def LogsGetChunked(self, project, path, index=0, log_count=0,
+                     chunk_bytes=0, dryrun=False):
+    """Logdog Logs.Get wrapper to request logs in chunks.
+
+    Args:
+      project: Logdog project of stream.
+      path: Logdog path of stream.
+      index: The initial log stream index to retrieve.
+      log_count: The maximum number of logs to return.
+      chunk_bytes: A suggested maximum number of bytes to return per chunk.
+                   At least one full log line will be returned with each chunk.
+      dryrun: Whether a dryrun.
+
+    Returns:
+      Generator of Logs.Get response protobufs, one per chunk.
+    """
+    retry_count = 3
+    terminal_index = None
+    while True:
+      # Retrieve the next chunk.
+      resp = self.LogsGet(project, path, state=True,
+                          index=index, byte_count=chunk_bytes,
+                          log_count=log_count, dryrun=dryrun)
+
+      # Ensure logs came back.
+      logs = resp.get('logs', [])
+      count = len(logs)
+      if count == 0:
+        # Logs haven't yet be ingested, retry.
+        if retry_count:
+          time.sleep(5)
+          retry_count -= 1
+          continue
+        else:
+          terminal = '%d' % (terminal_index) if terminal_index else 'None'
+          raise LogdogTimeoutException(
+              'Request to retrieve %s %s took too long '
+              '(index=%d, byte_count=%d, log_count=%d, terminal=%s)' %
+              (project, path, index, chunk_bytes, log_count, terminal))
+
+      # Return the current chunk.
+      yield resp
+
+      # Track the total number of logs.
+      if log_count > 0:
+        log_count -= count
+        if log_count <= 0:
+          return
+
+      # Keep track of the next index to read from.
+      final_index = int(logs[-1].get('streamIndex', 0))
+
+      # Was this the last index in the stream?
+      state = resp.get('state', {})
+      terminal_index = int(state.get('terminalIndex', 0))
+      if terminal_index >= 0 and final_index >= terminal_index:
+        return
+
+      # Start reading at the next index.
+      index = final_index + 1
+
+  def ExtractLines(self, resp):
+    """Generate log lines from a Logs.Get request.
+
+    Args:
+      resp: Logs.Get response protobuf.
+
+    Returns:
+      Generator of delimited log lines.
+    """
+    for log in resp.get('logs', []):
+      if 'text' in log and 'lines' in log['text']:
+        for line in log['text']['lines']:
+          yield '%s%s' % (line.get('value', ''), line['delimiter'])
+
+  def GetLines(self, project, path, index=0, log_count=0, chunk_bytes=0,
+               dryrun=False):
+    """Retrieve a Logdog stream as lines of text.
+
+    Args:
+      project: Logdog project of stream.
+      path: Logdog path of stream.
+      index: The initial log stream index to retrieve.
+      log_count: The maximum number of logs to return.
+      chunk_bytes: A suggested maximum number of bytes to retrieve from the
+                   server each time.
+      dryrun: Whether a dryrun.
+
+    Returns:
+      Generator of delimited log lines.
+    """
+    for resp in self.LogsGetChunked(project, path, index=index,
+                                    log_count=log_count,
+                                    chunk_bytes=chunk_bytes, dryrun=dryrun):
+      for line in self.ExtractLines(resp):
+        yield line
 
   def LogsTail(self, project, path, dryrun=False):
     """Logdog Logs.Tail request to retrieve logs.
--- lib/logdog_unittest.py
+++ lib/logdog_unittest.py
@@ -8,6 +8,7 @@ from __future__ import print_function
 
 import mock
 import os
+import time
 
 from chromite.lib import auth
 from chromite.lib import cros_test_lib
@@ -23,7 +24,36 @@ class LogdogClientTest(cros_test_lib.MockTestCase):
     self.mock_request = self.PatchObject(logdog.LogdogClient, 'SendRequest')
     self.PatchObject(logdog.LogdogClient, '_GetHost', return_value='foo.com')
     self.PatchObject(auth, 'GetAccessToken', return_value='token')
+    self.PatchObject(time, 'sleep')
     self.client = logdog.LogdogClient()
+
+    text_dict = {
+        'lines': [
+            {'value': 'hello', 'delimiter': '\n'},
+            {'value': 'foo', 'delimiter': '\n'},
+            {'delimiter': '\n'},
+        ]
+    }
+
+    self.get_response = {
+        'logs': [
+            {'text': text_dict,},
+            {'streamIndex': 1, 'text': text_dict,},
+            {'streamIndex': 2,},
+        ],
+        'state': {'terminalIndex': 3,}
+    }
+
+    self.get_response2 = {
+        'logs': [
+            {'streamIndex': 3,},
+        ],
+        'state': {'terminalIndex': 3,}
+    }
+
+    self.logs_response = {
+        'logs': [{'text': text_dict}],
+    }
 
   def testLogsQueryAnnotations(self):
     """Test LogsQueryAnnotations."""
@@ -56,6 +86,101 @@ class LogdogClientTest(cros_test_lib.MockTestCase):
     self.assertRaises(logdog.LogdogResponseException,
                       self.client.LogsQueryAnnotations,
                       'chromeos', 'elm-paladin', 1535)
+
+  def testLogsGet(self):
+    """Test LogsGet."""
+    self.mock_request.return_value = 'foo'
+    resp = self.client.LogsGet('proj', 'path', index=237, byte_count=1982,
+                               log_count=88821)
+    self.assertEqual(resp, 'foo')
+    self.mock_request.assert_called_with('prpc/logdog.Logs', 'Get', mock.ANY,
+                                         dryrun=False)
+    body_json = self.mock_request.call_args[0][2]
+    self.assertIn('proj', body_json)
+    self.assertIn('path', body_json)
+    self.assertIn('237', body_json)
+    self.assertIn('88821', body_json)
+
+  def testLogsGetChunked(self):
+    """Test LogsGetChunked."""
+    self.mock_request.side_effect = [self.get_response, self.get_response2]
+    resp = self.client.LogsGetChunked('proj', 'path', index=0, log_count=3)
+    self.assertEqual(list(resp), [self.get_response])
+    self.mock_request.assert_called_with('prpc/logdog.Logs', 'Get', mock.ANY,
+                                         dryrun=False)
+    body_json = self.mock_request.call_args[0][2]
+    self.assertIn('proj', body_json)
+    self.assertIn('path', body_json)
+    self.assertEqual(1, self.mock_request.call_count)
+
+  def testLogsGetChunkedShort(self):
+    """Test LogsGetChunked with log_count being short of initial response."""
+    self.mock_request.side_effect = [self.get_response, self.get_response2]
+    resp = self.client.LogsGetChunked('proj', 'path', index=0, log_count=2)
+    self.assertEqual(list(resp), [self.get_response])
+    self.mock_request.assert_called_with('prpc/logdog.Logs', 'Get', mock.ANY,
+                                         dryrun=False)
+    body_json = self.mock_request.call_args[0][2]
+    self.assertIn('proj', body_json)
+    self.assertIn('path', body_json)
+    self.assertEqual(1, self.mock_request.call_count)
+
+  def testLogsGetChunkedMultiple(self):
+    """Test LogsGetChunked with multiple chunks."""
+    self.mock_request.side_effect = [self.get_response, self.get_response2]
+    resp = self.client.LogsGetChunked('proj', 'path', index=0, log_count=4)
+    self.assertEqual(list(resp), [self.get_response, self.get_response2])
+    self.mock_request.assert_called_with('prpc/logdog.Logs', 'Get', mock.ANY,
+                                         dryrun=False)
+    body_json = self.mock_request.call_args[0][2]
+    self.assertIn('proj', body_json)
+    self.assertIn('path', body_json)
+    self.assertEqual(2, self.mock_request.call_count)
+
+  def testLogsGetChunkedTerminal(self):
+    """Test LogsGetChunked stopping with terminal index."""
+    self.mock_request.side_effect = [self.get_response, self.get_response2]
+    resp = self.client.LogsGetChunked('proj', 'path', index=0, log_count=0)
+    self.assertEqual(list(resp), [self.get_response, self.get_response2])
+    self.mock_request.assert_called_with('prpc/logdog.Logs', 'Get', mock.ANY,
+                                         dryrun=False)
+    body_json = self.mock_request.call_args[0][2]
+    self.assertIn('proj', body_json)
+    self.assertIn('path', body_json)
+    self.assertEqual(2, self.mock_request.call_count)
+
+  def testLogsGetChunkedRetry(self):
+    """Test LogsGetChunked retry and timeout."""
+    empty_response = {
+        'logs': [
+        ],
+        'state': {'terminalIndex': 3,}
+    }
+    self.mock_request.return_value = empty_response
+    resp = self.client.LogsGetChunked('proj', 'path', index=0, log_count=0)
+    self.assertRaises(logdog.LogdogTimeoutException, list, resp)
+    self.mock_request.assert_called_with('prpc/logdog.Logs', 'Get', mock.ANY,
+                                         dryrun=False)
+    body_json = self.mock_request.call_args[0][2]
+    self.assertIn('proj', body_json)
+    self.assertIn('path', body_json)
+    self.assertEqual(4, self.mock_request.call_count)
+
+  def testExtractLines(self):
+    """Test ExtractLines."""
+    self.assertEquals(['hello\n', 'foo\n', '\n'],
+                      list(self.client.ExtractLines(self.logs_response)))
+
+  def testGetLines(self):
+    """Test GetLines."""
+    self.mock_request.side_effect = [self.get_response, self.get_response2]
+    resp = self.client.GetLines('proj', 'path', index=0, log_count=4)
+    self.assertEquals(['hello\n', 'foo\n', '\n', 'hello\n', 'foo\n', '\n'],
+                      list(resp))
+    body_json = self.mock_request.call_args[0][2]
+    self.assertIn('proj', body_json)
+    self.assertIn('path', body_json)
+    self.assertEqual(2, self.mock_request.call_count)
 
   def testLogsTail(self):
     """Test LogsTail."""
