cfea327235fb401b77dfd152b3d4071cb8b8d6c8
rtoy <rtoy@chromium.org>
2017-02-07 11:19:30 -0800

    Convert DelayNode tests to testharness
    Manually convert the tests to testharness and new Audit.
    
    BUG=688065
    TEST=delaynode-max-default-delay.html,
    delaynode-max-nondefault-delay.html, delaynode-maxdelay.html,
    delaynode-scheduling.html, delaynode.html
    
    Review-Url: https://codereview.chromium.org/2675823002
    Cr-Commit-Position: refs/heads/master@{#448689}
    
    Former-commit-id: 81a6df48520904f38fef52dd165dc24e5cbf95e4

--- third_party/WebKit/LayoutTests/webaudio/resources/delay-testing.js
+++ third_party/WebKit/LayoutTests/webaudio/resources/delay-testing.js
@@ -19,52 +19,50 @@ function createToneBuffer(context, frequency, numberOfCycles, sampleRate) {
     return audioBuffer;
 }
 
-function checkDelayedResult(toneBuffer) {
-    return function(event) {
-        var renderedBuffer = event.renderedBuffer;
+function checkDelayedResult(renderedBuffer, toneBuffer, should) {
+    var sourceData = toneBuffer.getChannelData(0);
+    var renderedData = renderedBuffer.getChannelData(0);
 
-        var sourceData = toneBuffer.getChannelData(0);
-        var renderedData = renderedBuffer.getChannelData(0);
-    
-        var delayTimeFrames = delayTimeSeconds * sampleRate;
-        var toneLengthFrames = toneLengthSeconds * sampleRate;
+    var delayTimeFrames = delayTimeSeconds * sampleRate;
+    var toneLengthFrames = toneLengthSeconds * sampleRate;
 
-        var success = true;
-    
-        var n = renderedBuffer.length;
-    
-        for (var i = 0; i < n; ++i) {
-            if (i < delayTimeFrames) {
-                // Check that initial portion is 0 (since signal is delayed).
-                if (renderedData[i] != 0) {
-                    testFailed("Initial portion is not 0 at frame " + i);
-                    success = false;
-                    break;
-                }
-            } else if (i >= delayTimeFrames && i < delayTimeFrames + toneLengthFrames) {
-                // Make sure that the tone data is delayed by exactly the expected number of frames.
-                var j = i - delayTimeFrames;
-                if (renderedData[i] != sourceData[j]) {
-                    testFailed("Actual data does not match expected data at frame " + i);
-                    success = false;
-                    break;
-                }
-            } else {
-                // Make sure we have silence after the delayed tone.
-                if (renderedData[i] != 0) {
-                    testFailed("Final portion is not 0 at frame " + i);
-                    success = false;
-                    break;
-                }
-            }    
-        }
+    var success = true;
 
-        if (success) {
-            testPassed("Test signal was correctly delayed by " + delayTimeSeconds + " sec.");
+    var n = renderedBuffer.length;
+
+    for (var i = 0; i < n; ++i) {
+        if (i < delayTimeFrames) {
+            // Check that initial portion is 0 (since signal is delayed).
+            if (renderedData[i] != 0) {
+                should(renderedData[i],
+                       "Initial portion expected to be 0 at frame " + i)
+                    .beEqualTo(0);
+                success = false;
+                break;
+            }
+        } else if (i >= delayTimeFrames && i < delayTimeFrames +
+            toneLengthFrames) {
+            // Make sure that the tone data is delayed by exactly the expected number of frames.
+            var j = i - delayTimeFrames;
+            if (renderedData[i] != sourceData[j]) {
+                should(renderedData[i],
+                     "Actual data at frame " + i)
+                  .beEqualTo(sourceData[j]);
+                success = false;
+                break;
+            }
         } else {
-            testFailed("Test signal was not correctly delayed by " + delayTimeSeconds + " sec.");
+            // Make sure we have silence after the delayed tone.
+            if (renderedData[i] != 0) {
+                should(renderedData[j],
+                     "Final portion at frame " + i)
+                  .beEqualTo(0);
+                success = false;
+                break;
+            }
         }
-
-        finishJSTest();
     }
+
+    should(success, "Delaying test signal by " + delayTimeSeconds + " sec was done")
+        .message("correctly", "incorrectly")
 }
