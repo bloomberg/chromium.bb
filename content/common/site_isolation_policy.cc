336692cfc7ddfb2ec85fe6de16274682ee7313d9
alexmos <alexmos@chromium.org>
2017-02-07 14:51:33 -0800

    Turn on AreCrossProcessFramesPossible by default.
    This turns on all of the plumbing required to create out-of-process
    iframes, without enabling any policies that create them in practice.
    This minimizes the difference between OOPIF and non-OOPIF modes.
    
    Behavior changes include:
     - Creating proxies for child frames and replicating state to them
     - Enabling cross-process policy checks for child frame navigations
     - More complex fullscreen toggling logic
     - Slightly different event routing and focus logic
    
    Split off from creis's http://crrev.com/2668013005.
    
    BUG=545200
    TEST=No visible behavior change.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2676823003
    Cr-Commit-Position: refs/heads/master@{#448761}
    
    Former-commit-id: 443064bb9932a98ad47c1cbbf8bba2493a31f888

--- content/common/site_isolation_policy.cc
+++ content/common/site_isolation_policy.cc
@@ -5,21 +5,13 @@
 #include "content/common/site_isolation_policy.h"
 
 #include "base/command_line.h"
-#include "base/feature_list.h"
-#include "base/lazy_instance.h"
-#include "content/public/common/browser_side_navigation_policy.h"
-#include "content/public/common/content_client.h"
-#include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 
 namespace content {
 
 // static
 bool SiteIsolationPolicy::AreCrossProcessFramesPossible() {
-  return UseDedicatedProcessesForAllSites() ||
-         IsTopDocumentIsolationEnabled() ||
-         GetContentClient()->IsSupplementarySiteIsolationModeEnabled() ||
-         base::FeatureList::IsEnabled(::features::kGuestViewCrossProcessFrames);
+  return true;
 }
 
 // static
