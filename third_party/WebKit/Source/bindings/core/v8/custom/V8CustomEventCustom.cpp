b703fc2db84f0f9aa68035d32ea79ad9b7c2d540
lpy <lpy@chromium.org>
2017-02-07 15:43:58 -0800

    Avoid setting CustomEvent.detail property in initCustomEvent while dispatch an event.
    BUG=684843
    
    Review-Url: https://codereview.chromium.org/2679933003
    Cr-Commit-Position: refs/heads/master@{#448794}
    
    Former-commit-id: aa57f762dfa15e283219c3ff3e9c8b9d611cae7c

--- third_party/WebKit/Source/bindings/core/v8/custom/V8CustomEventCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8CustomEventCustom.cpp
@@ -107,6 +107,8 @@ void V8CustomEvent::initCustomEventMethodEpilogueCustom(
     const v8::FunctionCallbackInfo<v8::Value>& info,
     CustomEvent* impl) {
   ASSERT(info.Length() >= 3);
+  if (impl->isBeingDispatched())
+    return;
   v8::Local<v8::Value> detail = info[3];
   if (!detail.IsEmpty())
     storeDetail(ScriptState::current(info.GetIsolate()), impl, info.Holder(),
