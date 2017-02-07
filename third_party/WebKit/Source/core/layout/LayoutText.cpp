7c984da9898dbf8d5f181965344dae40e6c67034
kojii <kojii@chromium.org>
2017-02-07 14:27:18 -0800

    Fix min-content sizing for 'word-break: break-word'
    This patch fixes min-content[1] sizing for 'word-break: break-word'.
    
    The fix for crbug.com/655590 computes min-content sizing for
    'break-word' the same as 'break-all', even though these two values have
    different break opportunities as per the spec[2].
    
    Due to this, when 'break-word' is applied to text which 'break-all' does
    not allow break, such as a long sequence of '.' (FULL STOP, a.k.a.
    period,) the min-content becomes too long.
    
    [1] https://drafts.csswg.org/css-sizing/#min-content
    [2] https://drafts.csswg.org/css-text-3/#word-break-property
    
    BUG=687338
    
    Review-Url: https://codereview.chromium.org/2681633002
    Cr-Commit-Position: refs/heads/master@{#448744}
    
    Former-commit-id: eb5a8c59e9f3121d5c0dc92b2539f86f847eb295

--- third_party/WebKit/Source/core/layout/LayoutText.cpp
+++ third_party/WebKit/Source/core/layout/LayoutText.cpp
@@ -998,15 +998,21 @@ static float minWordFragmentWidthForBreakAll(LayoutText* layoutText,
                                              const Font& font,
                                              TextDirection textDirection,
                                              int start,
-                                             int length) {
+                                             int length,
+                                             EWordBreak breakAllOrBreakWord) {
   DCHECK_GT(length, 0);
   LazyLineBreakIterator breakIterator(layoutText->text(), style.locale());
   int nextBreakable = -1;
   float min = std::numeric_limits<float>::max();
   int end = start + length;
   for (int i = start; i < end;) {
-    breakIterator.isBreakable(i + 1, nextBreakable, LineBreakType::BreakAll);
-    int fragmentLength = (nextBreakable > i ? nextBreakable : length) - i;
+    int fragmentLength;
+    if (breakAllOrBreakWord == EWordBreak::BreakAllWordBreak) {
+      breakIterator.isBreakable(i + 1, nextBreakable, LineBreakType::BreakAll);
+      fragmentLength = (nextBreakable > i ? nextBreakable : length) - i;
+    } else {
+      fragmentLength = U16_LENGTH(layoutText->codepointAt(i));
+    }
     // The correct behavior is to measure width without re-shaping, but we
     // reshape each fragment here because a) the current line breaker does not
     // support it, b) getCharacterRange() can reshape if the text is too long
@@ -1094,11 +1100,16 @@ void LayoutText::computePreferredLogicalWidths(
   int lastWordBoundary = 0;
   float cachedWordTrailingSpaceWidth[2] = {0, 0};  // LTR, RTL
 
-  bool breakAll = (styleToUse.wordBreak() == BreakAllWordBreak ||
-                   styleToUse.wordBreak() == BreakWordBreak) &&
-                  styleToUse.autoWrap();
-  bool keepAll =
-      styleToUse.wordBreak() == KeepAllWordBreak && styleToUse.autoWrap();
+  EWordBreak breakAllOrBreakWord = EWordBreak::NormalWordBreak;
+  LineBreakType lineBreakType = LineBreakType::Normal;
+  if (styleToUse.autoWrap()) {
+    if (styleToUse.wordBreak() == BreakAllWordBreak ||
+        styleToUse.wordBreak() == BreakWordBreak) {
+      breakAllOrBreakWord = styleToUse.wordBreak();
+    } else if (styleToUse.wordBreak() == KeepAllWordBreak) {
+      lineBreakType = LineBreakType::KeepAll;
+    }
+  }
 
   Hyphenation* hyphenation =
       styleToUse.autoWrap() ? styleToUse.getHyphenation() : nullptr;
@@ -1194,9 +1205,7 @@ void LayoutText::computePreferredLogicalWidths(
       continue;
     }
 
-    bool hasBreak = breakIterator.isBreakable(
-        i, nextBreakable,
-        keepAll ? LineBreakType::KeepAll : LineBreakType::Normal);
+    bool hasBreak = breakIterator.isBreakable(i, nextBreakable, lineBreakType);
     bool betweenWords = true;
     int j = i;
     while (c != newlineCharacter && c != spaceCharacter &&
@@ -1274,12 +1283,13 @@ void LayoutText::computePreferredLogicalWidths(
         }
       }
 
-      if (breakAll) {
+      if (breakAllOrBreakWord != EWordBreak::NormalWordBreak) {
         // Because sum of character widths may not be equal to the word width,
         // we need to measure twice; once with normal break for max width,
         // another with break-all for min width.
-        currMinWidth = minWordFragmentWidthForBreakAll(
-            this, styleToUse, f, textDirection, i, wordLen);
+        currMinWidth =
+            minWordFragmentWidthForBreakAll(this, styleToUse, f, textDirection,
+                                            i, wordLen, breakAllOrBreakWord);
       } else {
         currMinWidth += w;
       }
