76df63a403f17a787e6da21cf788ae1f3a069a94
jbroman <jbroman@chromium.org>
2017-02-07 21:37:59 -0800

    WTF: Remove variadic arguments from DEFINE_GLOBAL.
    They aren't used for anything.
    
    Review-Url: https://codereview.chromium.org/2680073002
    Cr-Commit-Position: refs/heads/master@{#448914}
    
    Former-commit-id: 19fb57b4a9b7c6764e1898b7b4bee6e785a38b00

--- third_party/WebKit/Source/wtf/StaticConstructors.h
+++ third_party/WebKit/Source/wtf/StaticConstructors.h
@@ -32,7 +32,7 @@
 
 // Use an array of pointers instead of an array of char in case there is some
 // alignment issue.
-#define DEFINE_GLOBAL(type, name, ...)                                     \
+#define DEFINE_GLOBAL(type, name)                                          \
   void* name##Storage[(sizeof(type) + sizeof(void*) - 1) / sizeof(void*)]; \
   const type& name = *reinterpret_cast<type*>(&name##Storage)
 
