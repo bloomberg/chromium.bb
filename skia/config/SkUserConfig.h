54b8a7a5445fa238df3a66df02940516e8171489
senorblanco <senorblanco@chromium.org>
2017-02-08 12:35:40 -0800

    Enable Skia's edge-antialiasing tessellating path renderer.
    This will affect all concave paths rendered in 2D canvas, and in SVG
    content with five concave paths or fewer.  SVG content with >5 five paths
    will continue to be vetoed to MSAA if available, or software
    rasterization if not.
    
    BUG=642376
    
    Review-Url: https://codereview.chromium.org/2680003002
    Cr-Commit-Position: refs/heads/master@{#449081}
    
    Former-commit-id: 4fa27c5c19dadb9b737cb004c8ad176e8cb45422

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -258,9 +258,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 // Updating to a correct SkPMColor lerp will require layout test rebaselines.
 #define SK_SUPPORT_LEGACY_BROKEN_LERP
 
-// Enabling the screenspace AA tessellating path renderer needs rebaselines.
-#define SK_DISABLE_SCREENSPACE_TESS_AA_PATH_RENDERER
-
 #ifndef    SK_SUPPORT_LEGACY_AAA
 #   define SK_SUPPORT_LEGACY_AAA
 #endif
