e7a804288b036aee2c51d3be03f22457de35adac
sdy <sdy@chromium.org>
2017-02-08 15:11:21 -0800

    Fix TabStripBackgroundView drawing the theme with the wrong phase after leaving fullscreen.
    Two hacks worked together to make this bug:
    
    1. TabStripBackgroundView is added to a window's NSThemeFrame (its
       contentView's superview), not its contentView.
    
    2. While a window is leaving fullscreen, it's kept at the full size of
       the screen during the transition. Only the contentView's frame
       changes.
    
    As a result, a window's TabStripBackgroundView stays at the top of the
    screen during the transition, hidden. At the end of the transition, the
    window's frame is changed and the TabStripBackgroundView is shown.
    
    The TabStripBackgroundView draws when the contentView's frame first
    changes, using a patternPhase that's based on the position of the
    contentView within the window, which is wrong. When the window's
    non-fullscreen width is smaller than the screen width, the final resize
    causes it to redraw again, correctly. But when the window's
    non-fullscreen width is the same as the screen width, it doesn't need to
    redraw and the misaligned theme image stays until the next draw (when
    the window loses main, is resized, etc.).
    
    This fix adjusts for the contentView being smaller than the window, so
    that the theme is drawn correctly the first time. It's intended to be
    temporary. The first hack should be going away soon, at least.
    
    BUG=623424
    
    Review-Url: https://codereview.chromium.org/2681923004
    Cr-Commit-Position: refs/heads/master@{#449128}
    
    Former-commit-id: 2d3743754422ae2662dfaca2f91cddb3ca587a2d

--- ui/base/cocoa/nsgraphics_context_additions.mm
+++ ui/base/cocoa/nsgraphics_context_additions.mm
@@ -4,10 +4,27 @@
 
 #import "ui/base/cocoa/nsgraphics_context_additions.h"
 
+#include "base/logging.h"
+
 @implementation NSGraphicsContext (CrAdditions)
 
 - (void)cr_setPatternPhase:(NSPoint)phase
                    forView:(NSView*)view {
+  // TODO(sdy): Remove once we no longer add the tab background view to
+  // [[window contentView] superview] *or* no longer make the content view
+  // smaller than the window while exiting fullscreen. These two things
+  // together can result in the tab background view being drawn while it's
+  // outside of contentView, with a pattern phase that assumes it's inside.
+  NSView* contentView = [[view window] contentView];
+  if (![view isDescendantOf:contentView]) {
+    NSView* frameView = [contentView superview];
+    DCHECK([view isDescendantOf:frameView]);
+    // Convert phase into an offset from the top left corner of contentView so
+    // that it will be aligned correctly at the end of the transition.
+    phase.x += NSMinX([frameView frame]) - NSMinX([contentView frame]);
+    phase.y += NSMaxY([frameView frame]) - NSMaxY([contentView frame]);
+  }
+
   NSView* ancestorWithLayer = view;
   while (ancestorWithLayer && ![ancestorWithLayer layer])
     ancestorWithLayer = [ancestorWithLayer superview];
