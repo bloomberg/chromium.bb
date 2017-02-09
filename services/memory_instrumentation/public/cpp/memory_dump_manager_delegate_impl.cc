8aa418d107cd58d80d0cc3ac8f161534fc95f9e3
chiniforooshan <chiniforooshan@chromium.org>
2017-02-09 11:31:34 -0800

    memory-infra: The client library
    This implements MemoryDumpManagerDelegate (MDMD). MDMD is just a
    bridge between MemoryDumpManager and the Coordinator service, hiding
    mojo details from MemoryDumpManager.
    
    The next CL will put everything together and delete the old code.
    
    A prototype of the end product: https://codereview.chromium.org/2571823002
    
    BUG=679830
    
    Review-Url: https://codereview.chromium.org/2681003006
    Cr-Commit-Position: refs/heads/master@{#449374}
    
    Former-commit-id: 7ca40bc859221e9ce128da52ca664d35d2781a26

---
+++ services/memory_instrumentation/public/cpp/memory_dump_manager_delegate_impl.cc
@@ -0,0 +1,48 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/memory_instrumentation/public/cpp/memory_dump_manager_delegate_impl.h"
+
+#include "base/trace_event/memory_dump_request_args.h"
+#include "mojo/public/cpp/bindings/interface_request.h"
+#include "services/memory_instrumentation/public/cpp/coordinator.h"
+#include "services/memory_instrumentation/public/interfaces/memory_instrumentation.mojom.h"
+#include "services/service_manager/public/cpp/interface_provider.h"
+
+namespace memory_instrumentation {
+
+MemoryDumpManagerDelegateImpl::MemoryDumpManagerDelegateImpl(
+    service_manager::InterfaceProvider* interface_provider)
+    : is_coordinator_(false), binding_(this) {
+  interface_provider->GetInterface(mojo::MakeRequest(&coordinator_));
+  coordinator_->RegisterProcessLocalDumpManager(
+      binding_.CreateInterfacePtrAndBind());
+}
+
+MemoryDumpManagerDelegateImpl::MemoryDumpManagerDelegateImpl(
+    Coordinator* coordinator) : is_coordinator_(true), binding_(this) {
+  coordinator->BindCoordinatorRequest(mojo::MakeRequest(&coordinator_));
+  coordinator_->RegisterProcessLocalDumpManager(
+      binding_.CreateInterfacePtrAndBind());
+}
+
+MemoryDumpManagerDelegateImpl::~MemoryDumpManagerDelegateImpl() {}
+
+bool MemoryDumpManagerDelegateImpl::IsCoordinator() const {
+  return is_coordinator_;
+}
+
+void MemoryDumpManagerDelegateImpl::RequestProcessMemoryDump(
+    const base::trace_event::MemoryDumpRequestArgs& args,
+    const RequestProcessMemoryDumpCallback& callback) {
+  MemoryDumpManagerDelegate::CreateProcessDump(args, callback);
+}
+
+void MemoryDumpManagerDelegateImpl::RequestGlobalMemoryDump(
+    const base::trace_event::MemoryDumpRequestArgs& args,
+    const base::trace_event::MemoryDumpCallback& callback) {
+  coordinator_->RequestGlobalMemoryDump(args, callback);
+}
+
+}  // namespace memory_instrumentation
---
+++ services/memory_instrumentation/public/cpp/memory_dump_manager_delegate_impl.h
@@ -0,0 +1,51 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_MEMORY_INFRA_PUBLIC_CPP_MEMORY_DUMP_MANAGER_DELEGATE_IMPL_H_
+#define SERVICES_MEMORY_INFRA_PUBLIC_CPP_MEMORY_DUMP_MANAGER_DELEGATE_IMPL_H_
+
+#include "base/trace_event/memory_dump_manager.h"
+#include "base/trace_event/memory_dump_request_args.h"
+#include "mojo/public/cpp/bindings/binding.h"
+#include "services/memory_instrumentation/public/cpp/coordinator.h"
+#include "services/memory_instrumentation/public/interfaces/memory_instrumentation.mojom.h"
+#include "services/service_manager/public/cpp/interface_provider.h"
+
+namespace memory_instrumentation {
+
+class MemoryDumpManagerDelegateImpl
+    : public base::trace_event::MemoryDumpManagerDelegate,
+      public mojom::ProcessLocalDumpManager {
+ public:
+  // Use to bind to a remote coordinator.
+  MemoryDumpManagerDelegateImpl(
+      service_manager::InterfaceProvider* interface_provider);
+
+  // Use to bind to a coordinator in the same process.
+  MemoryDumpManagerDelegateImpl(Coordinator* coordinator);
+  ~MemoryDumpManagerDelegateImpl() override;
+
+  bool IsCoordinator() const;
+
+  // The base::trace_event::MemoryDumpManager calls this.
+  void RequestGlobalMemoryDump(
+      const base::trace_event::MemoryDumpRequestArgs& args,
+      const base::trace_event::MemoryDumpCallback& callback) override;
+
+ private:
+  // The ProcessLocalDumpManager interface. The coordinator calls this.
+  void RequestProcessMemoryDump(
+      const base::trace_event::MemoryDumpRequestArgs& args,
+      const RequestProcessMemoryDumpCallback& callback) override;
+
+  bool is_coordinator_;
+  mojom::CoordinatorPtr coordinator_;
+  mojo::Binding<mojom::ProcessLocalDumpManager> binding_;
+
+  DISALLOW_COPY_AND_ASSIGN(MemoryDumpManagerDelegateImpl);
+};
+
+}  // namespace memory_instrumentation
+
+#endif  // SERVICES_MEMORY_INFRA_PUBLIC_CPP_MEMORY_DUMP_MANAGER_DELEGATE_IMPL_H_
