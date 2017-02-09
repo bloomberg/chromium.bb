a11f75458b4296ea56ab4979fdac863f2efbd5a9
dougarnett <dougarnett@chromium.org>
2017-02-09 09:28:37 -0800

    [Prerender] Ignores resources with unsupported schemes for Offline reqs
    For offlining a page with the prerender, this changes the policy when
    encountering a resource with an unsupported scheme - instread of failing
    the prerender (as is done with other prerender requests), it will only
    cancel the unsupported resource and attempt to continue with prerendering.
    
    This has been tested end-end on 5 articles linked from www.foxsports.com
    that have a "blob:" subresource and with this change, we are able to
    download and offline version of them.
    
    BUG=686192
    
    Review-Url: https://codereview.chromium.org/2661703002
    Cr-Commit-Position: refs/heads/master@{#449337}
    
    Former-commit-id: f1ba435af9338792a78fc422eb9cd1a51a14aa13

--- chrome/browser/prerender/prerender_browsertest.cc
+++ chrome/browser/prerender/prerender_browsertest.cc
@@ -2823,6 +2823,39 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
   PrerenderTestURL(replacement_path, FINAL_STATUS_UNSUPPORTED_SCHEME, 0);
 }
 
+// Checks that non-http/https/chrome-extension subresource does not cancel the
+// prerender.
+IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
+                       PrerenderSubresourceUnsupportedSchemeForOffline) {
+  // Set up a page with unsupported subresource.
+  GURL image_url = GURL("invalidscheme://www.google.com/test.jpg");
+  base::StringPairs replacement_text;
+  replacement_text.push_back(
+      std::make_pair("REPLACE_WITH_IMAGE_URL", image_url.spec()));
+  std::string replacement_path;
+  net::test_server::GetFilePathWithReplacements(
+      "/prerender/prerender_with_image.html", replacement_text,
+      &replacement_path);
+  const GURL url = src_server()->GetURL(MakeAbsolute(replacement_path));
+
+  // Navigate to about:blank to get the session storage namespace.
+  ui_test_utils::NavigateToURL(current_browser(), GURL(url::kAboutBlankURL));
+  content::SessionStorageNamespace* storage_namespace =
+      GetActiveWebContents()
+          ->GetController()
+          .GetDefaultSessionStorageNamespace();
+
+  std::unique_ptr<TestPrerender> test_prerender =
+      prerender_contents_factory()->ExpectPrerenderContents(
+          FINAL_STATUS_APP_TERMINATING);
+
+  std::unique_ptr<PrerenderHandle> prerender_handle(
+      GetPrerenderManager()->AddPrerenderForOffline(url, storage_namespace,
+                                                    gfx::Size(640, 480)));
+  ASSERT_EQ(prerender_handle->contents(), test_prerender->contents());
+  test_prerender->WaitForLoads(1);
+}
+
 // Ensure that about:blank is permitted for any subresource.
 IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
                        PrerenderAllowAboutBlankSubresource) {
