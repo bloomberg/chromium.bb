5028e66df72dd80ede701bd76f0c2c9c1423edd8
thakis <thakis@chromium.org>
2017-02-10 15:13:59 -0800

    Revert of Re-enable SecurityTest.NewOverFlow on Windows. (patchset #2 id:40001 of https://codereview.chromium.org/2679153002/ )
    Reason for revert:
    Broke all the clang/win bots (https://crbug.com/690271), and I'd like to have them clear for the weekend. Easy to reland.
    
    Original issue's description:
    > Re-enable SecurityTest.NewOverFlow on Windows.
    >
    > BUG=
    >
    > Review-Url: https://codereview.chromium.org/2679153002
    > Cr-Commit-Position: refs/heads/master@{#449041}
    > Committed: https://chromium.googlesource.com/chromium/src/+/ada873a455d749c4a775e7cf84eac6836154a472 [formerly 82681d8ead6aa059caf6a64e485dbc6b210339be]
    
    TBR=wfh@chromium.org,gab@chromium.org,siggi@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=
    
    Review-Url: https://codereview.chromium.org/2691543003
    Cr-Commit-Position: refs/heads/master@{#449778}
    
    Former-commit-id: 4f46a461e16b504b1b2105a8b717be39bd688c02

--- base/security_unittest.cc
+++ base/security_unittest.cc
@@ -87,13 +87,14 @@ void OverflowTestsSoftExpectTrue(bool overflow_detected) {
   }
 }
 
-#if defined(OS_IOS) || defined(OS_LINUX)
+#if defined(OS_IOS) || defined(OS_WIN) || defined(OS_LINUX)
 #define MAYBE_NewOverflow DISABLED_NewOverflow
 #else
 #define MAYBE_NewOverflow NewOverflow
 #endif
 // Test array[TooBig][X] and array[X][TooBig] allocations for int overflows.
 // IOS doesn't honor nothrow, so disable the test there.
+// Crashes on Windows Dbg builds, disable there as well.
 // Disabled on Linux because failing Linux Valgrind bot, and Valgrind exclusions
 // are not currently read. See http://crbug.com/582398
 TEST(SecurityTest, MAYBE_NewOverflow) {
