13dfe7e9ebec86f1cf34ec188fded904d70fcff2
lod <lod@chromium.org>
2017-02-10 06:30:25 -0800

    [ObjC ARC] Converts ios/chrome/browser/ui/tools_menu:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2680973009
    Cr-Commit-Position: refs/heads/master@{#449614}
    
    Former-commit-id: 86412cb8391076dbaf91ce14b6c9fefb7451c04c

--- ios/chrome/browser/ui/tools_menu/tools_menu_view_item_unittest.mm
+++ ios/chrome/browser/ui/tools_menu/tools_menu_view_item_unittest.mm
@@ -6,41 +6,40 @@
 
 #import <UIKit/UIKit.h>
 
-#include "base/mac/scoped_nsobject.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
 
 namespace {
 
 class ToolsMenuViewItemTest : public PlatformTest {
  protected:
   void SetUp() override {
-    toolsMenuViewItem_.reset([[ToolsMenuViewItem alloc] init]);
+    toolsMenuViewItem_ = [[ToolsMenuViewItem alloc] init];
   }
-  base::scoped_nsobject<ToolsMenuViewItem> toolsMenuViewItem_;
+  ToolsMenuViewItem* toolsMenuViewItem_;
 };
 
 TEST_F(ToolsMenuViewItemTest, CustomizeCellAccessibilityTrait) {
-  base::scoped_nsobject<ToolsMenuViewCell> cell1(
-      [[ToolsMenuViewCell alloc] init]);
-  base::scoped_nsobject<ToolsMenuViewCell> cell2(
-      [[ToolsMenuViewCell alloc] init]);
+  ToolsMenuViewCell* cell1 = [[ToolsMenuViewCell alloc] init];
+  ToolsMenuViewCell* cell2 = [[ToolsMenuViewCell alloc] init];
 
   [toolsMenuViewItem_ setActive:YES];
   [cell1 setAccessibilityTraits:UIAccessibilityTraitNotEnabled];
   [cell1 configureForMenuItem:toolsMenuViewItem_];
-  EXPECT_FALSE(cell1.get().accessibilityTraits &
-               UIAccessibilityTraitNotEnabled);
+  EXPECT_FALSE(cell1.accessibilityTraits & UIAccessibilityTraitNotEnabled);
 
   [toolsMenuViewItem_ setActive:NO];
   [cell2 configureForMenuItem:toolsMenuViewItem_];
-  EXPECT_TRUE(cell2.get().accessibilityTraits & UIAccessibilityTraitNotEnabled);
+  EXPECT_TRUE(cell2.accessibilityTraits & UIAccessibilityTraitNotEnabled);
 
   // Happens when cell2 is reused.
   [toolsMenuViewItem_ setActive:YES];
   [cell2 configureForMenuItem:toolsMenuViewItem_];
-  EXPECT_FALSE(cell2.get().accessibilityTraits &
-               UIAccessibilityTraitNotEnabled);
+  EXPECT_FALSE(cell2.accessibilityTraits & UIAccessibilityTraitNotEnabled);
 }
 
 }  // namespace
