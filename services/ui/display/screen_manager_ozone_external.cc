2cc3462b65783f0c0281cbf512104199f668befe
tonikitoo <tonikitoo@igalia.com>
2017-02-13 15:50:36 -0800

    Stub out ScreenManagerOzoneExternal
    This is a step forward in order to have mus_demo
    (//services/ui/demo/) running in "external window mode".
    
    In external window mode, the need for a ScreenManager and
    ScreenManagerDelegate (aka DisplayManager) is eased.
    CL adds an stub'ed out implementation to be used on ozone/non-chromeos.
    
    BUG=666958
    
    Review-Url: https://codereview.chromium.org/2697693002
    Cr-Commit-Position: refs/heads/master@{#450155}
    
    Former-commit-id: 5a9fe272cb758a4a60f251bd797144fa7c75e79f

---
+++ services/ui/display/screen_manager_ozone_external.cc
@@ -0,0 +1,34 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/ui/display/screen_manager_ozone_external.h"
+
+#include <memory>
+
+#include "services/service_manager/public/cpp/interface_registry.h"
+#include "ui/display/types/display_constants.h"
+
+namespace display {
+
+// static
+std::unique_ptr<ScreenManager> ScreenManager::Create() {
+  return base::MakeUnique<ScreenManagerOzoneExternal>();
+}
+
+ScreenManagerOzoneExternal::ScreenManagerOzoneExternal() {}
+
+ScreenManagerOzoneExternal::~ScreenManagerOzoneExternal() {}
+
+void ScreenManagerOzoneExternal::AddInterfaces(
+    service_manager::InterfaceRegistry* registry) {}
+
+void ScreenManagerOzoneExternal::Init(ScreenManagerDelegate* delegate) {}
+
+void ScreenManagerOzoneExternal::RequestCloseDisplay(int64_t display_id) {}
+
+int64_t ScreenManagerOzoneExternal::GetPrimaryDisplayId() const {
+  return kInvalidDisplayId;
+}
+
+}  // namespace display
