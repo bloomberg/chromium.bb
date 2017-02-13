da109fa76b5c15f33a32d5282d4b17296da3f8af
yhirano <yhirano@chromium.org>
2017-02-13 04:24:02 -0800

    Merge response-stream-construction.js to response.js
    The former one was decoupled because it was unstable. Now it's stable, so let's
    merge them.
    
    BUG=None
    R=tyoshino@chromium.org
    
    Review-Url: https://codereview.chromium.org/2675163003
    Cr-Commit-Position: refs/heads/master@{#449934}
    
    Former-commit-id: af3e6c08bbf5bebead8e7bd19a9b64b7e117efd7

--- third_party/WebKit/LayoutTests/http/tests/fetch/script-tests/response.js
+++ third_party/WebKit/LayoutTests/http/tests/fetch/script-tests/response.js
@@ -466,4 +466,90 @@ test(function() {
       });
   }, 'Response.redirect() with URLs with CR or LF');
 
+test(() => {
+    var controller;
+    var stream = new ReadableStream({start: c => controller = c});
+
+    var response = new Response(stream);
+    assert_equals(response.body, stream);
+  }, 'Response constructed with a stream');
+
+promise_test(() => {
+    var controller;
+    var stream = new ReadableStream({start: c => controller = c});
+    controller.enqueue(new Uint8Array([0x68, 0x65, 0x6c, 0x6c, 0x6f]));
+    controller.enqueue(new Uint8Array([0x77, 0x6f, 0x72, 0x6c, 0x64]));
+    controller.close();
+    assert_false(stream.locked);
+    var response = new Response(stream);
+    var p = response.text().then(t => {
+        assert_equals(t, 'helloworld');
+      });
+    assert_true(stream.locked);
+    return p;
+  }, 'Response constructed with a stream');
+
+promise_test(() => {
+    var controller;
+    var stream = new ReadableStream({start: c => controller = c});
+    controller.enqueue(new Uint8Array([0x68, 0x65, 0x6c, 0x6c, 0x6f]));
+    controller.enqueue(new Uint8Array([0x77, 0x6f, 0x72, 0x6c, 0x64]));
+    controller.close();
+
+    var response = new Response(stream);
+    return readableStreamToArray(response.body).then(chunks => {
+        var decoder = new TextDecoder('utf-8');
+        var r = '';
+        for (var chunk of chunks) {
+          r += decoder.decode(chunk, {stream: true});
+        }
+        r += decoder.decode();
+        assert_equals(r, 'helloworld');
+      });
+  }, 'Response constructed with a stream / Read from body stream');
+
+promise_test(t => {
+    var controller;
+    var stream = new ReadableStream({start: c => controller = c});
+    setTimeout(() => {
+        controller.enqueue(new Uint8Array([0x68, 0x65, 0x6c, 0x6c, 0x6f]));
+        controller.enqueue(new Uint8Array([0x77, 0x6f, 0x72, 0x6c, 0x64]));
+        controller.error();
+    }, 1);
+    var response = new Response(stream);
+    return promise_rejects(t, TypeError(), response.text());
+  }, 'Response constructed with an errored stream');
+
+promise_test(t => {
+    var controller;
+    var stream = new ReadableStream({start: c => controller = c});
+    stream.getReader();
+    var response = new Response(stream);
+    return promise_rejects(t, TypeError(), response.text());
+  }, 'Response constructed with a locked stream');
+
+promise_test(t => {
+    var controller;
+    var stream = new ReadableStream({start: c => controller = c});
+    setTimeout(() => controller.enqueue(), 1);
+    var response = new Response(stream);
+    return promise_rejects(t, TypeError(), response.text());
+  }, 'Response constructed stream with an undefined chunk');
+
+promise_test(t => {
+    var controller;
+    var stream = new ReadableStream({start: c => controller = c});
+    setTimeout(() => controller.enqueue(null), 1);
+    var response = new Response(stream);
+    return promise_rejects(t, TypeError(), response.text());
+  }, 'Response constructed stream with a null chunk');
+
+promise_test(t => {
+    var controller;
+    var stream = new ReadableStream({start: c => controller = c});
+    setTimeout(() => controller.enqueue('hello'), 1);
+    var response = new Response(stream);
+    return promise_rejects(t, TypeError(), response.text());
+  }, 'Response constructed stream with a string chunk');
+
 done();
