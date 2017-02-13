71e473b8f9ead72d08368c589bd74df5cb6ecc8e
ulan <ulan@chromium.org>
2017-02-13 09:13:31 -0800

    Enable smoothness.maps benchmark on all platforms.
    The benchmark is currently enabled only for android, but there seems to
    be no reason why it cannot be run on desktop.
    
    Locally it runs OK on my Linux machine.
    
    The android-only restriction was introduced in this CL:
    https://codereview.chromium.org/587833003
    
    BUG=691581
    
    Review-Url: https://codereview.chromium.org/2691993002
    Cr-Commit-Position: refs/heads/master@{#449985}
    
    Former-commit-id: c2e7e0a2c9fb07ddc5cb71ef91ac0c157330fb61

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -121,7 +121,6 @@ class SmoothnessToughWebGLCases(_Smoothness):
     return 'smoothness.tough_webgl_cases'
 
 
-@benchmark.Enabled('android')
 @benchmark.Disabled('android-webview')  # http://crbug.com/653933
 class SmoothnessMaps(perf_benchmark.PerfBenchmark):
   page_set = page_sets.MapsPageSet
