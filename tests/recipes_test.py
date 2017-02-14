014cb038b8e3a22c90864c842d2c049d3e9637d4
Stephen Martinis <martiniss@google.com>
2017-02-13 17:57:27 -0800

    recipes_test.py: Use recipe bootstrap
    This ensures coverage is installed, rather than depending on it being
    installed in your system.
    
    BUG=671189
    
    Change-Id: Id9d7c514b4db53963381c5269bee06c706b23751
    Reviewed-on: https://chromium-review.googlesource.com/442050
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>

--- tests/recipes_test.py
+++ tests/recipes_test.py
@@ -12,7 +12,8 @@ import subprocess
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def recipes_py(*args):
-  subprocess.check_call([os.path.join(ROOT_DIR, 'recipes.py')] + list(args))
+  subprocess.check_call([
+      os.path.join(ROOT_DIR, 'recipes.py'), '--use-bootstrap'] + list(args))
 
 recipes_py('simulation_test')
 recipes_py('lint')
