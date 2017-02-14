c4ecf35efb1347addfeefea53fbdbca09dda0438
tyoshino <tyoshino@chromium.org>
2017-02-14 01:34:38 -0800

    Disable WallpaperManagerBrowserTest.DisplayChange for Chrome OS with Ozone enabled
    BUG=691548
    TBR=bshe@chromium.org
    
    Review-Url: https://codereview.chromium.org/2695933002
    Cr-Commit-Position: refs/heads/master@{#450305}
    
    Former-commit-id: 1e1ddf1e55bff7ead27411e3ac5dfab57b6966e4

--- chrome/browser/chromeos/login/users/wallpaper/wallpaper_manager_browsertest.cc
+++ chrome/browser/chromeos/login/users/wallpaper/wallpaper_manager_browsertest.cc
@@ -627,7 +627,12 @@ class TestObserver : public WallpaperManager::Observer {
   DISALLOW_COPY_AND_ASSIGN(TestObserver);
 };
 
-IN_PROC_BROWSER_TEST_F(WallpaperManagerBrowserTest, DisplayChange) {
+#if defined(OS_CHROMEOS) && defined(USE_OZONE)
+#define MAYBE_DisplayChange DISABLED_DisplayChange
+#else
+#define MAYBE_DisplayChange DisplayChange
+#endif
+IN_PROC_BROWSER_TEST_F(WallpaperManagerBrowserTest, MAYBE_DisplayChange) {
   TestObserver observer(WallpaperManager::Get());
 
   // Set the wallpaper to ensure that UpdateWallpaper() will be called when the
