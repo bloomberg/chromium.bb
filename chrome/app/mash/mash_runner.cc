1d475229007f22809896d5c1bba379e6d049a83f
jamescook <jamescook@chromium.org>
2017-02-14 15:11:32 -0800

    mash: Exit main process if content_browser service crashes
    For the ash/browser and window-server/browser connections both sides cache
    information about the other side. For now, rather than trying to make the
    services restartable we just bring down the main chrome --mash root process.
    The Chrome OS session_manager will then restart everything.
    
    Also explicitly start content_browser on startup, like mash_session used to
    do. It provides interfaces needed by ash (prefs, login state) that we're
    going to need for a while.
    
    BUG=678683
    TEST=navigate to about:inducebrowsercrashforrealz, root process exits
    
    Review-Url: https://codereview.chromium.org/2686003002
    Cr-Commit-Position: refs/heads/master@{#450506}
    
    Former-commit-id: 896d1d57393c5a0d787210a3e24007e973fb2fe5

--- chrome/app/mash/mash_runner.cc
+++ chrome/app/mash/mash_runner.cc
@@ -132,7 +132,8 @@ void OnInstanceQuitInMain(base::RunLoop* run_loop,
   DCHECK(run_loop);
 
   if (identity.name() != mash::common::GetWindowManagerServiceName() &&
-      identity.name() != ui::mojom::kServiceName) {
+      identity.name() != ui::mojom::kServiceName &&
+      identity.name() != content::mojom::kBrowserServiceName) {
     return;
   }
 
@@ -207,6 +208,7 @@ int MashRunner::RunServiceManagerInMain() {
   context.connector()->Connect(ui::mojom::kServiceName);
   context.connector()->Connect(mash::common::GetWindowManagerServiceName());
   context.connector()->Connect(mash::quick_launch::mojom::kServiceName);
+  context.connector()->Connect(content::mojom::kBrowserServiceName);
 
   run_loop.Run();
 
