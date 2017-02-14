4fe3d45d5d5a5ff4e0d09417f901477b891ba65a
jdufault <jdufault@chromium.org>
2017-02-14 12:54:44 -0800

    cros: Add unit test for default policy value generation.
    BUG=680161
    
    Review-Url: https://codereview.chromium.org/2653393004
    Cr-Commit-Position: refs/heads/master@{#450462}
    
    Former-commit-id: 25446c1a63bcb440c61347e4e0978093a59430c3

---
+++ components/policy/tools/generate_policy_source_test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# Copyright 2016 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import unittest
+
+import generate_policy_source
+
+class CppGenerationTest(unittest.TestCase):
+  def testDefaultValueGeneration(self):
+    """Tests generation of default policy values."""
+    # Bools
+    stmts, expr = generate_policy_source._GenerateDefaultValue(True)
+    self.assertListEqual([], stmts)
+    self.assertEqual('base::MakeUnique<base::FundamentalValue>(true)', expr)
+    stmts, expr = generate_policy_source._GenerateDefaultValue(False)
+    self.assertListEqual([], stmts)
+    self.assertEqual('base::MakeUnique<base::FundamentalValue>(false)', expr)
+
+    # Ints
+    stmts, expr = generate_policy_source._GenerateDefaultValue(33)
+    self.assertListEqual([], stmts)
+    self.assertEqual('base::MakeUnique<base::FundamentalValue>(33)', expr)
+
+    # Strings
+    stmts, expr = generate_policy_source._GenerateDefaultValue('foo')
+    self.assertListEqual([], stmts)
+    self.assertEqual('base::MakeUnique<base::StringValue>("foo")', expr)
+
+    # Empty list
+    stmts, expr = generate_policy_source._GenerateDefaultValue([])
+    self.assertListEqual(
+        ['auto default_value = base::MakeUnique<base::ListValue>();'], stmts)
+    self.assertEqual('std::move(default_value)', expr)
+
+    # List with values
+    stmts, expr = generate_policy_source._GenerateDefaultValue([1, '2'])
+    self.assertListEqual([
+        'auto default_value = base::MakeUnique<base::ListValue>();',
+        'default_value->Append(base::MakeUnique<base::FundamentalValue>(1));',
+        'default_value->Append(base::MakeUnique<base::StringValue>("2"));'
+      ], stmts)
+    self.assertEqual('std::move(default_value)', expr)
+
+    # Recursive lists are not supported.
+    stmts, expr = generate_policy_source._GenerateDefaultValue([1, []])
+    self.assertListEqual([], stmts)
+    self.assertIsNone(expr)
+
+    # Arbitary types are not supported.
+    stmts, expr = generate_policy_source._GenerateDefaultValue(object())
+    self.assertListEqual([], stmts)
+    self.assertIsNone(expr)
+
+
+if __name__ == '__main__':
+  unittest.main()
