de6193c99b87723a59b5e04f3f167133ea08b605
wez <wez@chromium.org>
2017-02-14 17:19:26 -0800

    Log the error result when observing a registry key fails.
    Logging a warning on failure isn't very helpful without the error code.
    
    BUG=692259
    
    Review-Url: https://codereview.chromium.org/2693223002
    Cr-Commit-Position: refs/heads/master@{#450538}
    
    Former-commit-id: 2884d9bee4a26233c3195f8d05b68aae8277e5c8

--- chrome/browser/extensions/external_registry_loader_win.cc
+++ chrome/browser/extensions/external_registry_loader_win.cc
@@ -201,24 +201,26 @@ void ExternalRegistryLoader::CompleteLoadAndStartWatchingRegistry() {
   LoadFinished();
 
   // Start watching registry.
-  if (hklm_key_.Create(HKEY_LOCAL_MACHINE, kRegistryExtensions,
-                       KEY_NOTIFY | KEY_WOW64_32KEY) == ERROR_SUCCESS) {
+  LONG result = ERROR_SUCCESS;
+  if ((result = hklm_key_.Create(HKEY_LOCAL_MACHINE, kRegistryExtensions,
+                                 KEY_NOTIFY | KEY_WOW64_32KEY)) ==
+      ERROR_SUCCESS) {
     base::win::RegKey::ChangeCallback callback =
         base::Bind(&ExternalRegistryLoader::OnRegistryKeyChanged,
                    base::Unretained(this), base::Unretained(&hklm_key_));
     hklm_key_.StartWatching(callback);
   } else {
-    LOG(WARNING) << "Error observing HKLM.";
+    LOG(WARNING) << "Error observing HKLM: " << result;
   }
 
-  if (hkcu_key_.Create(HKEY_CURRENT_USER, kRegistryExtensions, KEY_NOTIFY) ==
-      ERROR_SUCCESS) {
+  if ((result = hkcu_key_.Create(HKEY_CURRENT_USER, kRegistryExtensions,
+                                 KEY_NOTIFY)) == ERROR_SUCCESS) {
     base::win::RegKey::ChangeCallback callback =
         base::Bind(&ExternalRegistryLoader::OnRegistryKeyChanged,
                    base::Unretained(this), base::Unretained(&hkcu_key_));
     hkcu_key_.StartWatching(callback);
   } else {
-    LOG(WARNING) << "Error observing HKCU.";
+    LOG(WARNING) << "Error observing HKCU: " << result;
   }
 }
 
