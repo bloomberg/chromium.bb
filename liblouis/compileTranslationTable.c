e0487d98117894e7460798d1e7c02e97fb30d244
Dave Mielke <dave@mielke.cc>
2017-02-15 18:05:18 -0500

    Numeric values were being parsed beyond the end of  a table line.

--- liblouis/compileTranslationTable.c
+++ liblouis/compileTranslationTable.c
@@ -2123,8 +2123,9 @@ passGetNumber ()
 {
   /*Convert a string of wide character digits to an integer */
   passHoldNumber = 0;
-  while (passLine.chars[passLinepos] >= '0'
-	 && passLine.chars[passLinepos] <= '9')
+  while ((passLinepos < passLine.length) &&
+         (passLine.chars[passLinepos] >= '0') &&
+	 (passLine.chars[passLinepos] <= '9'))
     passHoldNumber =
       10 * passHoldNumber + (passLine.chars[passLinepos++] - '0');
   return 1;
