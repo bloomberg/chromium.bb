2d16760bcc41057b83e13b6c2154b182d075a6b4
fhorschig <fhorschig@chromium.org>
2017-02-16 06:28:04 -0800

    Introduce a Doodle Fetcher for NTP
    This fetcher queries the standard Doodle endpoint [1] and
    converts the config into a struct.
    Attributes are checked for sanity and converted into
    fitting formats (e.g. expiry_date based on TTL value).
    
    [1] www.google.com/async/ddljson.
    
    BUG=690467
    
    Review-Url: https://codereview.chromium.org/2660883002
    Cr-Commit-Position: refs/heads/master@{#450954}
    
    Former-commit-id: 5ba57aa78c96c678de1cdf6c34ffe9fefae21a6e

---
+++ components/doodle/doodle_fetcher.cc
@@ -0,0 +1,240 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/doodle/doodle_fetcher.h"
+
+#include <utility>
+
+#include "base/strings/string_number_conversions.h"
+#include "base/time/clock.h"
+#include "base/time/default_clock.h"
+#include "base/time/time.h"
+#include "base/values.h"
+#include "components/google/core/browser/google_url_tracker.h"
+#include "components/google/core/browser/google_util.h"
+#include "net/base/load_flags.h"
+#include "net/http/http_status_code.h"
+#include "net/url_request/url_fetcher.h"
+
+using net::URLFetcher;
+
+namespace doodle {
+
+namespace {
+
+const double kMaxTimeToLiveMS = 30.0 * 24 * 60 * 60 * 1000;  // 30 days
+
+const char kDoodleConfigPath[] = "/async/ddljson";
+
+std::string StripSafetyPreamble(const std::string& json) {
+  // The response may start with )]}'. Ignore this.
+  const char kResponsePreamble[] = ")]}'";
+
+  base::StringPiece json_sp(json);
+  if (json_sp.starts_with(kResponsePreamble)) {
+    json_sp.remove_prefix(strlen(kResponsePreamble));
+  }
+
+  return json_sp.as_string();
+}
+
+DoodleType ParseDoodleType(const base::DictionaryValue& ddljson) {
+  std::string type_str;
+  ddljson.GetString("doodle_type", &type_str);
+  if (type_str == "SIMPLE") {
+    return DoodleType::SIMPLE;
+  }
+  if (type_str == "RANDOM") {
+    return DoodleType::RANDOM;
+  }
+  if (type_str == "VIDEO") {
+    return DoodleType::VIDEO;
+  }
+  if (type_str == "INTERACTIVE") {
+    return DoodleType::INTERACTIVE;
+  }
+  if (type_str == "INLINE_INTERACTIVE") {
+    return DoodleType::INLINE_INTERACTIVE;
+  }
+  if (type_str == "SLIDESHOW") {
+    return DoodleType::SLIDESHOW;
+  }
+  return DoodleType::UNKNOWN;
+}
+
+}  // namespace
+
+DoodleImage::DoodleImage()
+    : height(0), width(0), is_animated_gif(false), is_cta(false) {}
+DoodleImage::~DoodleImage() = default;
+
+DoodleConfig::DoodleConfig() : doodle_type(DoodleType::UNKNOWN) {}
+DoodleConfig::DoodleConfig(const DoodleConfig& config) = default;
+DoodleConfig::~DoodleConfig() = default;
+
+DoodleFetcher::DoodleFetcher(
+    scoped_refptr<net::URLRequestContextGetter> download_context,
+    GoogleURLTracker* google_url_tracker,
+    const ParseJSONCallback& json_parsing_callback)
+    : download_context_(download_context),
+      json_parsing_callback_(json_parsing_callback),
+      google_url_tracker_(google_url_tracker),
+      clock_(new base::DefaultClock()),
+      weak_ptr_factory_(this) {
+  DCHECK(google_url_tracker_);
+}
+
+DoodleFetcher::~DoodleFetcher() = default;
+
+void DoodleFetcher::FetchDoodle(FinishedCallback callback) {
+  if (IsFetchInProgress()) {
+    callbacks_.push_back(std::move(callback));
+    return;  // The callback will be called for the existing request's results.
+  }
+  DCHECK(!fetcher_.get());
+  callbacks_.push_back(std::move(callback));
+  fetcher_ = URLFetcher::Create(GetGoogleBaseUrl().Resolve(kDoodleConfigPath),
+                                URLFetcher::GET, this);
+  fetcher_->SetRequestContext(download_context_.get());
+  fetcher_->SetLoadFlags(net::LOAD_DO_NOT_SEND_COOKIES |
+                         net::LOAD_DO_NOT_SAVE_COOKIES |
+                         net::LOAD_DO_NOT_SEND_AUTH_DATA);
+  fetcher_->SetAutomaticallyRetryOnNetworkChanges(1);
+  fetcher_->Start();
+}
+
+void DoodleFetcher::OnURLFetchComplete(const URLFetcher* source) {
+  DCHECK_EQ(fetcher_.get(), source);
+  std::unique_ptr<net::URLFetcher> free_fetcher = std::move(fetcher_);
+
+  std::string json_string;
+  if (!(source->GetStatus().is_success() &&
+        source->GetResponseCode() == net::HTTP_OK &&
+        source->GetResponseAsString(&json_string))) {
+    RespondToAllCallbacks(DoodleState::DOWNLOAD_ERROR, base::nullopt);
+    return;
+  }
+
+  json_parsing_callback_.Run(
+      StripSafetyPreamble(std::move(json_string)),
+      base::Bind(&DoodleFetcher::OnJsonParsed, weak_ptr_factory_.GetWeakPtr()),
+      base::Bind(&DoodleFetcher::OnJsonParseFailed,
+                 weak_ptr_factory_.GetWeakPtr()));
+}
+
+void DoodleFetcher::OnJsonParsed(std::unique_ptr<base::Value> json) {
+  std::unique_ptr<base::DictionaryValue> config =
+      base::DictionaryValue::From(std::move(json));
+  if (!config.get()) {
+    DLOG(WARNING) << "Doodle JSON is not valid";
+    RespondToAllCallbacks(DoodleState::PARSING_ERROR, base::nullopt);
+    return;
+  }
+
+  const base::DictionaryValue* ddljson = nullptr;
+  if (!config->GetDictionary("ddljson", &ddljson)) {
+    DLOG(WARNING) << "Doodle JSON reponse did not contain 'ddljson' key.";
+    RespondToAllCallbacks(DoodleState::PARSING_ERROR, base::nullopt);
+    return;
+  }
+
+  base::Optional<DoodleConfig> doodle = ParseDoodle(*ddljson);
+  if (!doodle.has_value()) {
+    RespondToAllCallbacks(DoodleState::NO_DOODLE, base::nullopt);
+    return;
+  }
+
+  RespondToAllCallbacks(DoodleState::AVAILABLE, std::move(doodle));
+}
+
+void DoodleFetcher::OnJsonParseFailed(const std::string& error_message) {
+  DLOG(WARNING) << "JSON parsing failed: " << error_message;
+  RespondToAllCallbacks(DoodleState::PARSING_ERROR, base::nullopt);
+}
+
+base::Optional<DoodleConfig> DoodleFetcher::ParseDoodle(
+    const base::DictionaryValue& ddljson) const {
+  DoodleConfig doodle;
+  if (!ParseImage(ddljson, "large_image", &doodle.large_image)) {
+    return base::nullopt;
+  }
+  ParseImage(ddljson, "transparent_large_image",
+             &doodle.transparent_large_image);
+  ParseImage(ddljson, "large_cta_image", &doodle.large_cta_image);
+  ParseBaseInformation(ddljson, &doodle);
+  return doodle;
+}
+
+bool DoodleFetcher::ParseImage(const base::DictionaryValue& image_parent,
+                               const std::string& image_name,
+                               DoodleImage* image) const {
+  DCHECK(image);
+  const base::DictionaryValue* image_dict = nullptr;
+  if (!image_parent.GetDictionary(image_name, &image_dict)) {
+    return false;
+  }
+  image->url = ParseRelativeUrl(*image_dict, "url");
+  if (!image->url.is_valid()) {
+    DLOG(WARNING) << "Image URL for \"" << image_name << "\" is invalid.";
+    return false;
+  }
+  image_dict->GetInteger("height", &image->height);
+  image_dict->GetInteger("width", &image->width);
+  image_dict->GetBoolean("is_animated_gif", &image->is_animated_gif);
+  image_dict->GetBoolean("is_cta", &image->is_cta);
+  return true;
+}
+
+void DoodleFetcher::ParseBaseInformation(const base::DictionaryValue& ddljson,
+                                         DoodleConfig* config) const {
+  config->search_url = ParseRelativeUrl(ddljson, "search_url");
+  config->target_url = ParseRelativeUrl(ddljson, "target_url");
+  config->fullpage_interactive_url =
+      ParseRelativeUrl(ddljson, "fullpage_interactive_url");
+
+  config->doodle_type = ParseDoodleType(ddljson);
+  ddljson.GetString("alt_text", &config->alt_text);
+  ddljson.GetString("interactive_html", &config->interactive_html);
+
+  // The JSON doesn't guarantee the number to fit into an int.
+  double ttl = 0;  // Expires immediately if the parameter is missing.
+  if (!ddljson.GetDouble("time_to_live_ms", &ttl) || ttl < 0) {
+    DLOG(WARNING) << "No valid Doodle image TTL present in ddljson!";
+    ttl = 0;
+  }
+  if (ttl > kMaxTimeToLiveMS) {
+    ttl = kMaxTimeToLiveMS;
+    DLOG(WARNING) << "Clamping Doodle image TTL to 30 days!";
+  }
+  config->expiry_date = clock_->Now() + base::TimeDelta::FromMillisecondsD(ttl);
+}
+
+GURL DoodleFetcher::ParseRelativeUrl(const base::DictionaryValue& dict_value,
+                                     const std::string& key) const {
+  std::string str_url;
+  dict_value.GetString(key, &str_url);
+  if (str_url.empty()) {
+    return GURL();
+  }
+  return GetGoogleBaseUrl().Resolve(str_url);
+}
+
+void DoodleFetcher::RespondToAllCallbacks(
+    DoodleState state,
+    const base::Optional<DoodleConfig>& config) {
+  for (auto& callback : callbacks_) {
+    std::move(callback).Run(state, config);
+  }
+  callbacks_.clear();
+}
+
+GURL DoodleFetcher::GetGoogleBaseUrl() const {
+  GURL cmd_line_url = google_util::CommandLineGoogleBaseURL();
+  if (cmd_line_url.is_valid()) {
+    return cmd_line_url;
+  }
+  return google_url_tracker_->google_url();
+}
+
+}  // namespace doodle
---
+++ components/doodle/doodle_fetcher_unittest.cc
@@ -0,0 +1,425 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/doodle/doodle_fetcher.h"
+
+#include <string>
+#include <utility>
+
+#include "base/bind.h"
+#include "base/command_line.h"
+#include "base/json/json_reader.h"
+#include "base/memory/ptr_util.h"
+#include "base/message_loop/message_loop.h"
+#include "base/test/simple_test_clock.h"
+#include "base/values.h"
+#include "components/google/core/browser/google_switches.h"
+#include "components/google/core/browser/google_url_tracker.h"
+#include "net/http/http_status_code.h"
+#include "net/url_request/test_url_fetcher_factory.h"
+#include "net/url_request/url_request_status.h"
+#include "net/url_request/url_request_test_util.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using testing::Eq;
+
+namespace doodle {
+
+namespace {
+
+const char kDoodleConfigPath[] = "/async/ddljson";
+
+// Required to instantiate a GoogleUrlTracker in UNIT_TEST_MODE.
+class GoogleURLTrackerClientStub : public GoogleURLTrackerClient {
+ public:
+  GoogleURLTrackerClientStub() {}
+  ~GoogleURLTrackerClientStub() override {}
+
+  bool IsBackgroundNetworkingEnabled() override { return true; }
+
+  PrefService* GetPrefs() override { return nullptr; }
+
+  net::URLRequestContextGetter* GetRequestContext() override { return nullptr; }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(GoogleURLTrackerClientStub);
+};
+
+std::string Resolve(const std::string& relative_url) {
+  return GURL(GoogleURLTracker::kDefaultGoogleHomepage)
+      .Resolve(relative_url)
+      .spec();
+}
+
+void ParseJson(
+    const std::string& json,
+    const base::Callback<void(std::unique_ptr<base::Value> json)>& success,
+    const base::Callback<void(const std::string&)>& error) {
+  base::JSONReader json_reader;
+  std::unique_ptr<base::Value> value = json_reader.ReadToValue(json);
+  if (value) {
+    success.Run(std::move(value));
+  } else {
+    error.Run(json_reader.GetErrorMessage());
+  }
+}
+
+}  // namespace
+
+class DoodleFetcherTest : public testing::Test {
+ public:
+  DoodleFetcherTest()
+      : url_(GURL(GoogleURLTracker::kDefaultGoogleHomepage)),
+        google_url_tracker_(base::MakeUnique<GoogleURLTrackerClientStub>(),
+                            GoogleURLTracker::UNIT_TEST_MODE),
+        doodle_fetcher_(
+            new net::TestURLRequestContextGetter(message_loop_.task_runner()),
+            &google_url_tracker_,
+            base::Bind(ParseJson)) {
+    // Random difference to ensure that expiry_dates are really relative.
+    auto clock = base::MakeUnique<base::SimpleTestClock>();
+    clock_ = clock.get();
+    clock_->Advance(base::TimeDelta::FromMilliseconds(1123581321));
+    doodle_fetcher_.SetClockForTesting(std::move(clock));
+  }
+
+  void RespondWithData(const std::string& data) {
+    RespondToFetcherWithData(GetRunningFetcher(), data);
+  }
+
+  void RespondToFetcherWithData(net::TestURLFetcher* url_fetcher,
+                                const std::string& data) {
+    url_fetcher->set_status(net::URLRequestStatus());
+    url_fetcher->set_response_code(net::HTTP_OK);
+    url_fetcher->SetResponseString(data);
+    // Call the URLFetcher delegate to continue the test.
+    url_fetcher->delegate()->OnURLFetchComplete(url_fetcher);
+  }
+
+  void RespondWithError(int error_code) {
+    net::TestURLFetcher* url_fetcher = GetRunningFetcher();
+    url_fetcher->set_status(net::URLRequestStatus::FromError(error_code));
+    url_fetcher->SetResponseString("");
+    // Call the URLFetcher delegate to continue the test.
+    url_fetcher->delegate()->OnURLFetchComplete(url_fetcher);
+  }
+
+  net::TestURLFetcher* GetRunningFetcher() {
+    // All created TestURLFetchers have ID 0 by default.
+    net::TestURLFetcher* url_fetcher = url_fetcher_factory_.GetFetcherByID(0);
+    DCHECK(url_fetcher);
+    return url_fetcher;
+  }
+
+  DoodleFetcher::FinishedCallback CreateResponseSavingCallback(
+      DoodleState* state_out,
+      base::Optional<DoodleConfig>* config_out) {
+    return base::BindOnce(
+        [](DoodleState* state_out, base::Optional<DoodleConfig>* config_out,
+           DoodleState state, const base::Optional<DoodleConfig>& config) {
+          if (state_out) {
+            *state_out = state;
+          }
+          if (config_out) {
+            *config_out = config;
+          }
+        },
+        state_out, config_out);
+  }
+
+  DoodleFetcher* doodle_fetcher() { return &doodle_fetcher_; }
+
+  GURL GetGoogleBaseURL() { return google_url_tracker_.google_url(); }
+
+  base::Time TimeFromNow(uint64_t milliseconds) {
+    return clock_->Now() + base::TimeDelta::FromMilliseconds(milliseconds);
+  }
+
+ private:
+  base::MessageLoop message_loop_;
+  GURL url_;
+  net::TestURLFetcherFactory url_fetcher_factory_;
+  base::SimpleTestClock* clock_;  // Owned by the doodle_fetcher.
+  GoogleURLTracker google_url_tracker_;
+  DoodleFetcher doodle_fetcher_;
+};
+
+TEST_F(DoodleFetcherTest, ReturnsFromFetchWithoutError) {
+  DoodleState state(DoodleState::NO_DOODLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithData(R"json({"ddljson": {
+        "time_to_live_ms":55000,
+        "large_image": {"url":"/logos/doodles/2015/some.gif"}
+      }})json");
+
+  EXPECT_THAT(state, Eq(DoodleState::AVAILABLE));
+  EXPECT_TRUE(response.has_value());
+}
+
+TEST_F(DoodleFetcherTest, ReturnsFrom404FetchWithError) {
+  DoodleState state(DoodleState::NO_DOODLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithError(net::ERR_FILE_NOT_FOUND);
+
+  EXPECT_THAT(state, Eq(DoodleState::DOWNLOAD_ERROR));
+  EXPECT_FALSE(response.has_value());
+}
+
+TEST_F(DoodleFetcherTest, ReturnsErrorForInvalidJson) {
+  DoodleState state(DoodleState::NO_DOODLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithData("}");
+
+  EXPECT_THAT(state, Eq(DoodleState::PARSING_ERROR));
+  EXPECT_FALSE(response.has_value());
+}
+
+TEST_F(DoodleFetcherTest, ReturnsErrorForIncompleteJson) {
+  DoodleState state(DoodleState::NO_DOODLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithData("{}");
+
+  EXPECT_THAT(state, Eq(DoodleState::PARSING_ERROR));
+  EXPECT_FALSE(response.has_value());
+}
+
+TEST_F(DoodleFetcherTest, ResponseContainsValidBaseInformation) {
+  DoodleState state(DoodleState::NO_DOODLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithData(R"json()]}'{
+      "ddljson": {
+        "alt_text":"Mouseover Text",
+        "doodle_type":"SIMPLE",
+        "interactive_html":"\u003cstyle\u003e\u003c\/style\u003e",
+        "search_url":"/search?q\u003dtest",
+        "target_url":"/search?q\u003dtest\u0026sa\u003dX\u0026ved\u003d0ahUKEw",
+        "time_to_live_ms":55000,
+        "large_image": {
+          "url":"/logos/doodles/2015/new-years-eve-2015-59854387958251-hp.gif"
+        }
+      }})json");
+
+  EXPECT_THAT(state, Eq(DoodleState::AVAILABLE));
+  ASSERT_TRUE(response.has_value());
+  DoodleConfig config = response.value();
+
+  EXPECT_TRUE(config.search_url.is_valid());
+  EXPECT_THAT(config.search_url, Eq(Resolve("/search?q\u003dtest")));
+  EXPECT_TRUE(config.fullpage_interactive_url.is_empty());
+  EXPECT_TRUE(config.target_url.is_valid());
+  EXPECT_THAT(config.target_url,
+              Eq(Resolve("/search?q\u003dtest\u0026sa\u003dX\u0026ved\u003d"
+                         "0ahUKEw")));
+  EXPECT_THAT(config.doodle_type, Eq(DoodleType::SIMPLE));
+  EXPECT_THAT(config.alt_text, Eq("Mouseover Text"));
+  EXPECT_THAT(config.interactive_html,
+              Eq("\u003cstyle\u003e\u003c/style\u003e"));
+
+  EXPECT_THAT(config.expiry_date, Eq(TimeFromNow(55000)));
+}
+
+TEST_F(DoodleFetcherTest, DoodleExpiresWithinThirtyDaysForTooLargeTTL) {
+  DoodleState state(DoodleState::NO_DOODLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithData(R"json({"ddljson": {
+      "time_to_live_ms":5184000000,
+      "large_image": {"url":"/logos/doodles/2015/some.gif"}
+    }})json");  // 60 days
+
+  EXPECT_THAT(state, Eq(DoodleState::AVAILABLE));
+  ASSERT_TRUE(response.has_value());
+  EXPECT_THAT(response.value().expiry_date,
+              Eq(TimeFromNow(30ul * 24 * 60 * 60 * 1000 /* ms */)));  // 30 days
+}
+
+TEST_F(DoodleFetcherTest, DoodleExpiresNowWithNegativeTTL) {
+  DoodleState state(DoodleState::NO_DOODLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithData(R"json({"ddljson": {
+      "time_to_live_ms":-1,
+      "large_image": {"url":"/logos/doodles/2015/some.gif"}
+    }})json");
+
+  EXPECT_THAT(state, Eq(DoodleState::AVAILABLE));
+  ASSERT_TRUE(response.has_value());
+  EXPECT_THAT(response.value().expiry_date, Eq(TimeFromNow(0)));
+}
+
+TEST_F(DoodleFetcherTest, DoodleExpiresNowWithoutValidTTL) {
+  DoodleState state(DoodleState::NO_DOODLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithData(R"json({"ddljson": {
+        "large_image": {"url":"/logos/doodles/2015/some.gif"}
+      }})json");
+
+  EXPECT_THAT(state, Eq(DoodleState::AVAILABLE));
+  ASSERT_TRUE(response.has_value());
+  EXPECT_THAT(response.value().expiry_date, Eq(TimeFromNow(0)));
+}
+
+TEST_F(DoodleFetcherTest, ReturnsNoDoodleForMissingLargeImageUrl) {
+  DoodleState state(DoodleState::AVAILABLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithData(R"json({"ddljson": {
+        "time_to_live_ms":55000,
+        "large_image": {}
+      }})json");
+
+  EXPECT_THAT(state, Eq(DoodleState::NO_DOODLE));
+  EXPECT_FALSE(response.has_value());
+}
+
+TEST_F(DoodleFetcherTest, EmptyResponsesCausesNoDoodleState) {
+  DoodleState state(DoodleState::AVAILABLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithData("{\"ddljson\":{}}");
+
+  EXPECT_THAT(state, Eq(DoodleState::NO_DOODLE));
+  EXPECT_FALSE(response.has_value());
+}
+
+TEST_F(DoodleFetcherTest, ResponseContainsExactlyTheSampleImages) {
+  DoodleState state(DoodleState::NO_DOODLE);
+  base::Optional<DoodleConfig> response;
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state, &response));
+  RespondWithData(R"json()]}'{
+      "ddljson": {
+        "time_to_live_ms":55000,
+        "large_image": {
+          "height":225,
+          "is_animated_gif":true,
+          "is_cta":false,
+          "url":"/logos/doodles/2015/new-years-eve-2015-59854387958251-hp.gif",
+          "width":489
+        },
+        "large_cta_image": {
+          "height":225,
+          "is_animated_gif":true,
+          "is_cta":true,
+          "url":"/logos/doodles/2015/new-years-eve-2015-59854387958251-cta.gif",
+          "width":489
+        },
+        "transparent_large_image": {
+          "height":225,
+          "is_animated_gif":false,
+          "is_cta":false,
+          "url":"/logos/doodles/2015/new-years-eve-2015-59854387958251-thp.png",
+          "width":510
+        }
+      }})json");
+
+  EXPECT_THAT(state, Eq(DoodleState::AVAILABLE));
+  ASSERT_TRUE(response.has_value());
+  DoodleConfig config = response.value();
+
+  EXPECT_TRUE(config.transparent_large_image.url.is_valid());
+  EXPECT_THAT(config.transparent_large_image.url.spec(),
+              Eq(Resolve("/logos/doodles/2015/new-years-eve-2015-5985438795"
+                         "8251-thp.png")));
+  EXPECT_THAT(config.transparent_large_image.width, Eq(510));
+  EXPECT_THAT(config.transparent_large_image.height, Eq(225));
+  EXPECT_FALSE(config.transparent_large_image.is_animated_gif);
+  EXPECT_FALSE(config.transparent_large_image.is_cta);
+
+  EXPECT_TRUE(config.large_image.url.is_valid());
+  EXPECT_THAT(config.large_image.url.spec(),
+              Eq(Resolve("/logos/doodles/2015/new-years-eve-2015-5985438795"
+                         "8251-hp.gif")));
+  EXPECT_THAT(config.large_image.width, Eq(489));
+  EXPECT_THAT(config.large_image.height, Eq(225));
+  EXPECT_TRUE(config.large_image.is_animated_gif);
+  EXPECT_FALSE(config.large_image.is_cta);
+
+  EXPECT_TRUE(config.large_cta_image.url.is_valid());
+  EXPECT_THAT(config.large_cta_image.url.spec(),
+              Eq(Resolve("/logos/doodles/2015/new-years-eve-2015-5985438795"
+                         "8251-cta.gif")));
+  EXPECT_THAT(config.large_cta_image.width, Eq(489));
+  EXPECT_THAT(config.large_cta_image.height, Eq(225));
+  EXPECT_TRUE(config.large_cta_image.is_animated_gif);
+  EXPECT_TRUE(config.large_cta_image.is_cta);
+}
+
+TEST_F(DoodleFetcherTest, RespondsToMultipleRequestsWithSameFetcher) {
+  DoodleState state1(DoodleState::NO_DOODLE);
+  DoodleState state2(DoodleState::NO_DOODLE);
+  base::Optional<DoodleConfig> response1;
+  base::Optional<DoodleConfig> response2;
+
+  // Trigger two requests.
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state1, &response1));
+  net::URLFetcher* first_created_fetcher = GetRunningFetcher();
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(&state2, &response2));
+  net::URLFetcher* second_created_fetcher = GetRunningFetcher();
+
+  // Expect that only one fetcher handles both requests.
+  EXPECT_THAT(first_created_fetcher, Eq(second_created_fetcher));
+
+  RespondWithData(R"json({"ddljson": {
+        "time_to_live_ms":55000,
+        "large_image": {"url":"/logos/doodles/2015/some.gif"}
+      }})json");
+
+  // Ensure that both requests received a response.
+  EXPECT_THAT(state1, Eq(DoodleState::AVAILABLE));
+  EXPECT_TRUE(response1.has_value());
+  EXPECT_THAT(state2, Eq(DoodleState::AVAILABLE));
+  EXPECT_TRUE(response2.has_value());
+}
+
+TEST_F(DoodleFetcherTest, ReceivesBaseUrlFromTracker) {
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(/*state=*/nullptr, /*response=*/nullptr));
+
+  EXPECT_THAT(GetRunningFetcher()->GetOriginalURL(),
+              Eq(GetGoogleBaseURL().Resolve(kDoodleConfigPath)));
+}
+
+TEST_F(DoodleFetcherTest, OverridesBaseUrlWithCommandLineArgument) {
+  base::CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kGoogleBaseURL, "http://www.google.kz");
+
+  doodle_fetcher()->FetchDoodle(
+      CreateResponseSavingCallback(/*state=*/nullptr, /*response=*/nullptr));
+
+  EXPECT_THAT(GetRunningFetcher()->GetOriginalURL(),
+              Eq(GURL("http://www.google.kz").Resolve(kDoodleConfigPath)));
+}
+
+}  // namespace doodle
