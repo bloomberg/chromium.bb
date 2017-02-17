a590f73bd28f974be471651df2c9546eab6a1963
Ryan Tseng <hinoka@google.com>
2017-02-17 10:45:22 -0800

    Fix extra space in git env var
    BUG=635641
    
    Change-Id: I66f669ed45199107643fced174c0cd1b8eaa80ad
    Reviewed-on: https://chromium-review.googlesource.com/444449
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Ryan Tseng <hinoka@chromium.org>

--- recipe_modules/bot_update/api.py
+++ recipe_modules/bot_update/api.py
@@ -40,7 +40,7 @@ class BotUpdateApi(recipe_api.RecipeApi):
     # These are to prevent git from hanging.  If the git connection is slower
     # than 1KB/s for more than 5 minutes then git will kill the connection
     # and die with an error "error: RPC failed; curl 28 Operation too slow"
-    kwargs['env']['GIT_HTTP_LOW_SPEED_LIMIT '] = 1000
+    kwargs['env']['GIT_HTTP_LOW_SPEED_LIMIT'] = 1000
     kwargs['env']['GIT_HTTP_LOW_SPEED_TIME'] = 300
     return self.m.python(name, bot_update_path, cmd, **kwargs)
 
