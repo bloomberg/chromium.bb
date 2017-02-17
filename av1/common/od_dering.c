15dbc1a1495f57840e608911814ab0fdddd4ced9
Thomas Daede <tdaede@mozilla.com>
2017-02-17 11:21:55 -0800

    Avoid unused variable warnings when CLPF and dering are enabled.
    Change-Id: I7909756e6a7d2da817b3354d6a8cb341cdc5bf98

--- av1/common/od_dering.c
+++ av1/common/od_dering.c
@@ -300,13 +300,15 @@ void od_dering(int16_t *y, int16_t *in, int xdec,
   int bx;
   int by;
   int bsize;
-  int filter2_thresh[OD_DERING_NBLOCKS][OD_DERING_NBLOCKS];
   od_filter_dering_direction_func filter_dering_direction[OD_DERINGSIZES] = {
     od_filter_dering_direction_4x4, od_filter_dering_direction_8x8
   };
+#ifndef CONFIG_CLPF
+  int filter2_thresh[OD_DERING_NBLOCKS][OD_DERING_NBLOCKS];
   od_filter_dering_orthogonal_func filter_dering_orthogonal[OD_DERINGSIZES] = {
     od_filter_dering_orthogonal_4x4, od_filter_dering_orthogonal_8x8
   };
+#endif
   bsize = OD_DERING_SIZE_LOG2 - xdec;
   if (pli == 0) {
     for (bi = 0; bi < dering_count; bi++) {
@@ -315,27 +317,33 @@ void od_dering(int16_t *y, int16_t *in, int xdec,
       bx = dlist[bi].bx;
       dir[by][bx] = od_dir_find8(&in[8 * by * OD_FILT_BSTRIDE + 8 * bx],
                                  OD_FILT_BSTRIDE, &var, coeff_shift);
-      /* Deringing orthogonal to the direction uses a tighter threshold
-         because we want to be conservative. We've presumably already
-         achieved some deringing, so the amount of change is expected
-         to be low. Also, since we might be filtering across an edge, we
-         want to make sure not to blur it. That being said, we might want
-         to be a little bit more aggressive on pure horizontal/vertical
-         since the ringing there tends to be directional, so it doesn't
-         get removed by the directional filtering. */
-      filter2_thresh[by][bx] = (filter_dering_direction[bsize - OD_LOG_BSIZE0])(
-          &y[bi << 2 * bsize], 1 << bsize,
-          &in[(by * OD_FILT_BSTRIDE << bsize) + (bx << bsize)],
-          od_adjust_thresh(threshold, var), dir[by][bx]);
+/* Deringing orthogonal to the direction uses a tighter threshold
+   because we want to be conservative. We've presumably already
+   achieved some deringing, so the amount of change is expected
+   to be low. Also, since we might be filtering across an edge, we
+   want to make sure not to blur it. That being said, we might want
+   to be a little bit more aggressive on pure horizontal/vertical
+   since the ringing there tends to be directional, so it doesn't
+   get removed by the directional filtering. */
+#ifndef CONFIG_CLPF
+      filter2_thresh[by][bx] =
+#endif
+          (filter_dering_direction[bsize - OD_LOG_BSIZE0])(
+              &y[bi << 2 * bsize], 1 << bsize,
+              &in[(by * OD_FILT_BSTRIDE << bsize) + (bx << bsize)],
+              od_adjust_thresh(threshold, var), dir[by][bx]);
     }
   } else {
     for (bi = 0; bi < dering_count; bi++) {
       by = dlist[bi].by;
       bx = dlist[bi].bx;
-      filter2_thresh[by][bx] = (filter_dering_direction[bsize - OD_LOG_BSIZE0])(
-          &y[bi << 2 * bsize], 1 << bsize,
-          &in[(by * OD_FILT_BSTRIDE << bsize) + (bx << bsize)], threshold,
-          dir[by][bx]);
+#ifndef CONFIG_CLPF
+      filter2_thresh[by][bx] =
+#endif
+          (filter_dering_direction[bsize - OD_LOG_BSIZE0])(
+              &y[bi << 2 * bsize], 1 << bsize,
+              &in[(by * OD_FILT_BSTRIDE << bsize) + (bx << bsize)], threshold,
+              dir[by][bx]);
     }
   }
   copy_dering_16bit_to_16bit(in, OD_FILT_BSTRIDE, y, dlist, dering_count,
