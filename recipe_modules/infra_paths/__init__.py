cef0dcf97166ce022f5756361a0ee71ed89ede32
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2017-02-17 22:30:26 +0100

    Add DISABLE_STRICT_COVERAGE to modules which need it (round #2)
    For some reason some of these issues were only detected on
    recipe autoroller bot.
    
    TBR=iannucci
    
    BUG=693058
    
    Change-Id: I5d4a1243850a31d5430f2dc07be4a5138551ef82
    Reviewed-on: https://chromium-review.googlesource.com/444884
    Reviewed-by: Paweł Hajdan Jr. <phajdan.jr@chromium.org>
    Commit-Queue: Paweł Hajdan Jr. <phajdan.jr@chromium.org>

--- recipe_modules/infra_paths/__init__.py
+++ recipe_modules/infra_paths/__init__.py
@@ -2,3 +2,6 @@ DEPS = [
   'recipe_engine/path',
   'recipe_engine/properties',
 ]
+
+# TODO(phajdan): provide coverage (http://crbug.com/693058).
+DISABLE_STRICT_COVERAGE = True
--- recipe_modules/tryserver/__init__.py
+++ recipe_modules/tryserver/__init__.py
@@ -14,3 +14,7 @@ DEPS = [
   'rietveld',
   'recipe_engine/step',
 ]
+
+
+# TODO(phajdan): provide coverage (http://crbug.com/693058).
+DISABLE_STRICT_COVERAGE = True
