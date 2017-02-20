340e248a30c765d49441d8246c865aa1cfd2d6c6
erikchen <erikchen@chromium.org>
2017-02-20 05:04:50 -0800

    base: Add support for malloc zones to the allocator shim
    This CL has no functional change, but could cause performance
    regressions. It adds an additional void* context parameter to all
    allocator-shim related functions, as it will be necessary to support
    multiple malloc zones.
    
    BUG=693237
    TBR=thakis@chromium.org
    
    Review-Url: https://codereview.chromium.org/2697123007
    Cr-Commit-Position: refs/heads/master@{#451612}
    
    Former-commit-id: eff0ecbf12a6757ebb46438100fef60dff531e43

--- base/allocator/allocator_shim_default_dispatch_to_mac_zoned_malloc.cc
+++ base/allocator/allocator_shim_default_dispatch_to_mac_zoned_malloc.cc
@@ -20,49 +20,68 @@ namespace {
 // malloc zone.
 MallocZoneFunctions g_default_zone;
 
-void* MallocImpl(const AllocatorDispatch*, size_t size) {
-  return g_default_zone.malloc(malloc_default_zone(), size);
+void* MallocImpl(const AllocatorDispatch*, size_t size, void* context) {
+  return g_default_zone.malloc(
+      reinterpret_cast<struct _malloc_zone_t*>(context), size);
 }
 
-void* CallocImpl(const AllocatorDispatch*, size_t n, size_t size) {
-  return g_default_zone.calloc(malloc_default_zone(), n, size);
+void* CallocImpl(const AllocatorDispatch*,
+                 size_t n,
+                 size_t size,
+                 void* context) {
+  return g_default_zone.calloc(
+      reinterpret_cast<struct _malloc_zone_t*>(context), n, size);
 }
 
-void* MemalignImpl(const AllocatorDispatch*, size_t alignment, size_t size) {
-  return g_default_zone.memalign(malloc_default_zone(), alignment, size);
+void* MemalignImpl(const AllocatorDispatch*,
+                   size_t alignment,
+                   size_t size,
+                   void* context) {
+  return g_default_zone.memalign(
+      reinterpret_cast<struct _malloc_zone_t*>(context), alignment, size);
 }
 
-void* ReallocImpl(const AllocatorDispatch*, void* ptr, size_t size) {
-  return g_default_zone.realloc(malloc_default_zone(), ptr, size);
+void* ReallocImpl(const AllocatorDispatch*,
+                  void* ptr,
+                  size_t size,
+                  void* context) {
+  return g_default_zone.realloc(
+      reinterpret_cast<struct _malloc_zone_t*>(context), ptr, size);
 }
 
-void FreeImpl(const AllocatorDispatch*, void* ptr) {
-  g_default_zone.free(malloc_default_zone(), ptr);
+void FreeImpl(const AllocatorDispatch*, void* ptr, void* context) {
+  g_default_zone.free(reinterpret_cast<struct _malloc_zone_t*>(context), ptr);
 }
 
-size_t GetSizeEstimateImpl(const AllocatorDispatch*, void* ptr) {
-  return g_default_zone.size(malloc_default_zone(), ptr);
+size_t GetSizeEstimateImpl(const AllocatorDispatch*, void* ptr, void* context) {
+  return g_default_zone.size(reinterpret_cast<struct _malloc_zone_t*>(context),
+                             ptr);
 }
 
 unsigned BatchMallocImpl(const AllocatorDispatch* self,
                          size_t size,
                          void** results,
-                         unsigned num_requested) {
-  return g_default_zone.batch_malloc(malloc_default_zone(), size, results,
-                                     num_requested);
+                         unsigned num_requested,
+                         void* context) {
+  return g_default_zone.batch_malloc(
+      reinterpret_cast<struct _malloc_zone_t*>(context), size, results,
+      num_requested);
 }
 
 void BatchFreeImpl(const AllocatorDispatch* self,
                    void** to_be_freed,
-                   unsigned num_to_be_freed) {
-  g_default_zone.batch_free(malloc_default_zone(), to_be_freed,
-                            num_to_be_freed);
+                   unsigned num_to_be_freed,
+                   void* context) {
+  g_default_zone.batch_free(reinterpret_cast<struct _malloc_zone_t*>(context),
+                            to_be_freed, num_to_be_freed);
 }
 
 void FreeDefiniteSizeImpl(const AllocatorDispatch* self,
                           void* ptr,
-                          size_t size) {
-  g_default_zone.free_definite_size(malloc_default_zone(), ptr, size);
+                          size_t size,
+                          void* context) {
+  g_default_zone.free_definite_size(
+      reinterpret_cast<struct _malloc_zone_t*>(context), ptr, size);
 }
 
 }  // namespace
