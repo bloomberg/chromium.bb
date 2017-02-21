a3a30c484562ea45e1467e036400d61e42bf969f
Angie Chiang <angiebird@google.com>
2017-02-21 10:36:01 -0800

    No round in the second interp stage in conv_round
    Change-Id: I05377f3d06b3a863e0372d100bc78ea1fbee9b82

--- av1/common/convolve.h
+++ av1/common/convolve.h
@@ -64,7 +64,7 @@ static INLINE ConvolveParams get_conv_params_no_round(int ref, int plane,
   conv_params.ref = ref;
   conv_params.round = CONVOLVE_OPT_NO_ROUND;
   conv_params.round_0 = 5;
-  conv_params.round_1 = 1;
+  conv_params.round_1 = 0;
   conv_params.dst = dst;
   conv_params.dst_stride = dst_stride;
   conv_params.plane = plane;
