281a6de89aa75dfa3d53d414361cfa1914c55f3d
fmalita <fmalita@chromium.org>
2017-02-21 14:07:20 -0800

    Enable display list caching for record_time_partial_invalidation
    We're currently disabling display list caching when running
    rasterize_and_record_micro:record_time_partial_invalidation, due to the
    negated test.
    
    This seems unintentional - presumably the conditional was not updated in
    https://codereview.chromium.org/2380573003 when PartialInvalidation was
    added.
    
    Refactor to test for PaintingControlSetting modes which require
    disabling the cache, instead of the converse.
    
    R=chrishtr@chromium.org,wkorman@chromium.org
    
    Review-Url: https://codereview.chromium.org/2703413002
    Cr-Commit-Position: refs/heads/master@{#451845}
    
    Former-commit-id: b01fc99adf32befd7b61f43453960623bef35600

--- third_party/WebKit/Source/platform/graphics/ContentLayerDelegate.cpp
+++ third_party/WebKit/Source/platform/graphics/ContentLayerDelegate.cpp
@@ -69,9 +69,9 @@ void ContentLayerDelegate::paintContents(
   // We also disable caching when Painting or Construction are disabled. In both
   // cases we would like to compare assuming the full cost of recording, not the
   // cost of re-using cached content.
-  if (paintingControl != WebContentLayerClient::PaintDefaultBehavior &&
-      paintingControl != WebContentLayerClient::PaintDefaultBehaviorForTest &&
-      paintingControl != WebContentLayerClient::SubsequenceCachingDisabled)
+  if (paintingControl == WebContentLayerClient::DisplayListCachingDisabled ||
+      paintingControl == WebContentLayerClient::DisplayListPaintingDisabled ||
+      paintingControl == WebContentLayerClient::DisplayListConstructionDisabled)
     paintController.invalidateAll();
 
   GraphicsContext::DisabledMode disabledMode = GraphicsContext::NothingDisabled;
