3e303d56ce1f86a5aeb57e3a0f0b98964e28fea4
dominickn <dominickn@chromium.org>
2017-02-22 15:49:08 -0800

    Do not drop the final web app scope path component if it ends with a slash.
    This CL updates the web app scope computation. The current computation
    drops the final component of the path if there is more than one
    component. For web apps which have a start URL that ends in a folder,
    this removes the containing folder, resulting in an overly broad scope.
    For instance, https://example.com/today/ would end up with a scope
    over all of https://example.com under this scheme.
    
    Instead, we refine the computation by not dropping the final path
    component if the path ends with a slash. This indicates that the final
    component is a directory which should be retained. Otherwise, a lack of
    a slash at the end of the path signals a file, which we remove to
    extract the containing directory.
    
    BUG=688888
    
    Review-Url: https://codereview.chromium.org/2711563003
    Cr-Commit-Position: refs/heads/master@{#452279}
    
    Former-commit-id: 4141e5db756f115ae51c31f71d33d6c3a8422638

--- chrome/android/junit/src/org/chromium/chrome/browser/ShortcutHelperTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/ShortcutHelperTest.java
@@ -6,10 +6,11 @@ package org.chromium.chrome.browser;
 
 import static org.junit.Assert.assertEquals;
 
-import org.chromium.testing.local.LocalRobolectricTestRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.annotation.Config;
+
+import org.chromium.testing.local.LocalRobolectricTestRunner;
 
 /**
  * Unit tests for {@link org.chromium.chrome.browser.ShortcutHelper}.
@@ -25,7 +26,7 @@ public class ShortcutHelperTest {
     public void testGetScopeFromUrl() {
         String url1 = "https://www.google.com";
         String url2 = "https://www.google.com/";
-        String url3 = "https://www.google.com/maps";
+        String url3 = "https://www.google.com/maps.htm";
         String url4 = "https://www.google.com/maps/";
         String url5 = "https://www.google.com/index.html";
         String url6 = "https://www.google.com/index.html?q=maps";
@@ -35,22 +36,28 @@ public class ShortcutHelperTest {
         String url10 = "https://www.google.com/maps/au/north/";
         String url11 = "https://www.google.com/maps/au/index.html?q=maps#fragment/";
         String url12 = "http://www.google.com:8000/maps/au/index.html?q=maps#fragment/";
+        String url13 = "https://www.google.com/maps/au/north/?q=maps#fragment";
+        String url14 = "https://www.google.com/maps/au/north?q=maps#fragment";
 
         String url2_scope = "https://www.google.com/";
+        String url4_scope = "https://www.google.com/maps/";
         String url8_scope = "https://www.google.com/maps/au/";
+        String url10_scope = "https://www.google.com/maps/au/north/";
         String url12_scope = "http://www.google.com:8000/maps/au/";
 
         assertEquals(url2_scope, ShortcutHelper.getScopeFromUrl(url1));
         assertEquals(url2_scope, ShortcutHelper.getScopeFromUrl(url2));
         assertEquals(url2_scope, ShortcutHelper.getScopeFromUrl(url3));
-        assertEquals(url2_scope, ShortcutHelper.getScopeFromUrl(url4));
+        assertEquals(url4_scope, ShortcutHelper.getScopeFromUrl(url4));
         assertEquals(url2_scope, ShortcutHelper.getScopeFromUrl(url5));
         assertEquals(url2_scope, ShortcutHelper.getScopeFromUrl(url6));
         assertEquals(url2_scope, ShortcutHelper.getScopeFromUrl(url7));
         assertEquals(url8_scope, ShortcutHelper.getScopeFromUrl(url8));
         assertEquals(url8_scope, ShortcutHelper.getScopeFromUrl(url9));
-        assertEquals(url8_scope, ShortcutHelper.getScopeFromUrl(url10));
+        assertEquals(url10_scope, ShortcutHelper.getScopeFromUrl(url10));
         assertEquals(url8_scope, ShortcutHelper.getScopeFromUrl(url11));
         assertEquals(url12_scope, ShortcutHelper.getScopeFromUrl(url12));
+        assertEquals(url10_scope, ShortcutHelper.getScopeFromUrl(url13));
+        assertEquals(url8_scope, ShortcutHelper.getScopeFromUrl(url14));
     }
 }
