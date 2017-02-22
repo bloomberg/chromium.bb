0369899e3e416a75359b0c36a78355b69c532ca6
kkhorimoto <kkhorimoto@chromium.org>
2017-02-22 14:58:19 -0800

    Remove CRWSessionEntry usage from BrowserViewController.
    This CL also removes |-currentSessionEntry| from
    PreloadControllerDelegate, as it is unused.
    
    BUG=454984, 546348
    
    Review-Url: https://codereview.chromium.org/2690943006
    Cr-Commit-Position: refs/heads/master@{#452254}
    
    Former-commit-id: 8793cf8e2e26093c0e38af20be24864c0d1a3cff

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -171,7 +171,6 @@
 #include "ios/public/provider/chrome/browser/voice/voice_search_controller_delegate.h"
 #include "ios/public/provider/chrome/browser/voice/voice_search_provider.h"
 #import "ios/web/navigation/crw_session_controller.h"
-#import "ios/web/navigation/crw_session_entry.h"
 #include "ios/web/navigation/navigation_manager_impl.h"
 #include "ios/web/public/active_state_manager.h"
 #include "ios/web/public/navigation_item.h"
@@ -1917,11 +1916,10 @@ class BrowserBookmarkModelBridge : public bookmarks::BookmarkModelObserver {
 
   // Hide the toolbar if displaying phone NTP.
   if (!IsIPadIdiom()) {
-    CRWSessionEntry* entry =
-        [[tab navigationManager]->GetSessionController() currentEntry];
+    web::NavigationItem* item = [tab navigationManager]->GetVisibleItem();
     BOOL hideToolbar = NO;
-    if (entry) {
-      GURL url = [entry navigationItem]->GetURL();
+    if (item) {
+      GURL url = item->GetURL();
       BOOL isNTP = url.GetOrigin() == GURL(kChromeUINewTabURL);
       hideToolbar = isNTP && !_isOffTheRecord &&
                     ![_toolbarController isOmniboxFirstResponder] &&
@@ -4803,13 +4801,6 @@ class BrowserBookmarkModelBridge : public bookmarks::BookmarkModelObserver {
 
 - (BOOL)shouldUseDesktopUserAgent {
   return [_model currentTab].useDesktopUserAgent;
-}
-
-- (CRWSessionEntry*)currentSessionEntry {
-  Tab* tab = [_model currentTab];
-  if (![tab navigationManager])
-    return nil;
-  return [[tab navigationManager]->GetSessionController() currentEntry];
 }
 
 #pragma mark - BookmarkBridgeMethods
--- ios/chrome/browser/ui/preload_controller_delegate.h
+++ ios/chrome/browser/ui/preload_controller_delegate.h
@@ -7,16 +7,11 @@
 
 #import <UIKit/UIKit.h>
 
-@class CRWSessionEntry;
-
 // A protocol implemented by a delegate of PreloadController
 @protocol PreloadControllerDelegate
 
 // Should preload controller request a desktop site.
 - (BOOL)shouldUseDesktopUserAgent;
-// Return the current sessionEntry from the delegate.
-// TODO(crbug.com/546348): See if this can return a NavigationItem instead.
-- (CRWSessionEntry*)currentSessionEntry;
 @end
 
 #endif  // IOS_CHROME_BROWSER_UI_PRELOAD_CONTROLLER_DELEGATE_H_
