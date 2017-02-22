3f774a507ec82e3e5e7ff1536bb2ca6d2fac695e
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-02-21 17:12:27 -0800

    [Extensions] Add crash tracking
    See linked bug.
    
    BUG=692069
    
    Review-Url: https://codereview.chromium.org/2711513003
    Cr-Commit-Position: refs/heads/master@{#451864}
    
    Former-commit-id: 55264e89bb1d75126b5054844bec333ab0174234

--- extensions/browser/sandboxed_unpacker.cc
+++ extensions/browser/sandboxed_unpacker.cc
@@ -228,7 +228,12 @@ SandboxedUnpacker::SandboxedUnpacker(
       location_(location),
       creation_flags_(creation_flags),
       unpacker_io_task_runner_(unpacker_io_task_runner),
-      utility_wrapper_(new UtilityHostWrapper) {}
+      utility_wrapper_(new UtilityHostWrapper) {
+  // Tracking for crbug.com/692069. The location must be valid. If it's invalid,
+  // the utility process kills itself for a bad IPC.
+  CHECK_GT(location, Manifest::INVALID_LOCATION);
+  CHECK_LT(location, Manifest::NUM_LOCATIONS);
+}
 
 bool SandboxedUnpacker::CreateTempDirectory() {
   CHECK(unpacker_io_task_runner_->RunsTasksOnCurrentThread());
