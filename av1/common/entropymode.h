1d7db728ae0e298922bb19bbe8ad168494e36288
Thomas Davies <thdavies@cisco.com>
2017-02-22 11:15:00 +0000

    Use default CDF tables when initialising mode probs.
    No change in BDR.
    
    Change-Id: I77551120a2e94dcbf818b039154495f0f9b21755

--- av1/common/entropymode.h
+++ av1/common/entropymode.h
@@ -375,9 +375,10 @@ extern const aom_prob av1_kf_y_mode_prob[INTRA_MODES][INTRA_MODES]
                                         [INTRA_MODES - 1];
 #if CONFIG_EC_MULTISYMBOL
 // CDF version of 'av1_kf_y_mode_prob'.
-extern aom_cdf_prob av1_kf_y_mode_cdf[INTRA_MODES][INTRA_MODES]
-                                     [CDF_SIZE(INTRA_MODES)];
+extern const aom_cdf_prob av1_kf_y_mode_cdf[INTRA_MODES][INTRA_MODES]
+                                           [CDF_SIZE(INTRA_MODES)];
 #endif
+
 #if CONFIG_PALETTE
 extern const aom_prob av1_default_palette_y_mode_prob[PALETTE_BLOCK_SIZES]
                                                      [PALETTE_Y_MODE_CONTEXTS];
