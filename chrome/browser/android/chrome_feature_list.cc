1f6146fb070c455dc7dba28d6fe9bef0c87bfe3d
waltercacau <waltercacau@chromium.org>
2017-02-22 04:42:04 -0800

    Enabling AndroidPayIntegrationV2 by default.
    BUG=676694
    
    Review-Url: https://codereview.chromium.org/2709883006
    Cr-Commit-Position: refs/heads/master@{#452013}
    
    Former-commit-id: 1626534a64848c697e2be14ba95fbfb814779461

--- chrome/browser/android/chrome_feature_list.cc
+++ chrome/browser/android/chrome_feature_list.cc
@@ -95,7 +95,7 @@ const base::Feature kAndroidPayIntegrationV1{"AndroidPayIntegrationV1",
                                              base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kAndroidPayIntegrationV2{"AndroidPayIntegrationV2",
-                                             base::FEATURE_DISABLED_BY_DEFAULT};
+                                             base::FEATURE_ENABLED_BY_DEFAULT};
 
 const base::Feature kAndroidPaymentApps{"AndroidPaymentApps",
                                         base::FEATURE_DISABLED_BY_DEFAULT};
