e14dc0e24cba6e19cda7cb855a3db52e8b74c9a2
cliffordcheng <cliffordcheng@chromium.org>
2017-02-22 14:31:15 -0800

    Fix the telemetry errors when running all CPU/memory related tests.
    BUG=694666
    
    Review-Url: https://codereview.chromium.org/2705163006
    Cr-Commit-Position: refs/heads/master@{#452238}
    
    Former-commit-id: ad5c9b9b07e5b387c497d351c681d958e47c2b8f

--- chrome/test/media_router/telemetry/benchmarks/media_router_benchmark.py
+++ chrome/test/media_router/telemetry/benchmarks/media_router_benchmark.py
@@ -80,7 +80,7 @@ class HistogramCastBenckmark(_BaseCastBenchmark):
 class CPUMemoryCastBenckmark(_BaseCastBenchmark):
   """Benchmark for CPU and memory usage with Media Router."""
 
-  options = {'page_repeat': 1}
+  options = {'pageset_repeat': 1}
 
   page_set = media_router_perf_pages.MediaRouterCPUMemoryPageSet
 
@@ -95,7 +95,7 @@ class CPUMemoryCastBenckmark(_BaseCastBenchmark):
 class CPUMemoryBenckmark(perf_benchmark.PerfBenchmark):
   """Benchmark for CPU and memory usage without Media Router."""
 
-  options = {'page_repeat': 1}
+  options = {'pageset_repeat': 1}
 
   page_set = media_router_perf_pages.CPUMemoryPageSet
 
