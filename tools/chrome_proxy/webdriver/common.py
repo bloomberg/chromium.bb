cc5796a192a45b34e7237cfd411941c9b561d243
robertogden <robertogden@chromium.org>
2017-02-22 08:09:57 -0800

    Add platform specific test function decorators
    BUG=691690
    
    Review-Url: https://codereview.chromium.org/2701873002
    Cr-Commit-Position: refs/heads/master@{#452075}
    
    Former-commit-id: 43ac2e876ac8862c9dc65cdb9a74b749b14e62eb

--- tools/chrome_proxy/webdriver/common.py
+++ tools/chrome_proxy/webdriver/common.py
@@ -581,3 +581,71 @@ class IntegrationTest(unittest.TestCase):
     testRunner = unittest.runner.TextTestRunner(verbosity=2,
       failfast=flags.failfast, buffer=(not flags.disable_buffer))
     testRunner.run(tests)
+
+# Platform-specific decorators.
+# These decorators can be used to only run a test function for certain platforms
+# by annotating the function with them.
+
+def AndroidOnly(func):
+  def wrapper(*args, **kwargs):
+    if ParseFlags().android:
+      func(*args, **kwargs)
+    else:
+      args[0].skipTest('This test runs on Android only.')
+  return wrapper
+
+def NotAndroid(func):
+  def wrapper(*args, **kwargs):
+    if not ParseFlags().android:
+      func(*args, **kwargs)
+    else:
+      args[0].skipTest('This test does not run on Android.')
+  return wrapper
+
+def WindowsOnly(func):
+  def wrapper(*args, **kwargs):
+    if sys.platform == 'win32':
+      func(*args, **kwargs)
+    else:
+      args[0].skipTest('This test runs on Windows only.')
+  return wrapper
+
+def NotWindows(func):
+  def wrapper(*args, **kwargs):
+    if sys.platform != 'win32':
+      func(*args, **kwargs)
+    else:
+      args[0].skipTest('This test does not run on Windows.')
+  return wrapper
+
+def LinuxOnly(func):
+  def wrapper(*args, **kwargs):
+    if sys.platform.startswith('linux'):
+      func(*args, **kwargs)
+    else:
+      args[0].skipTest('This test runs on Linux only.')
+  return wrapper
+
+def NotLinux(func):
+  def wrapper(*args, **kwargs):
+    if sys.platform.startswith('linux'):
+      func(*args, **kwargs)
+    else:
+      args[0].skipTest('This test does not run on Linux.')
+  return wrapper
+
+def MacOnly(func):
+  def wrapper(*args, **kwargs):
+    if sys.platform == 'darwin':
+      func(*args, **kwargs)
+    else:
+      args[0].skipTest('This test runs on Mac OS only.')
+  return wrapper
+
+def NotMac(func):
+  def wrapper(*args, **kwargs):
+    if sys.platform == 'darwin':
+      func(*args, **kwargs)
+    else:
+      args[0].skipTest('This test does not run on Mac OS.')
+  return wrapper
