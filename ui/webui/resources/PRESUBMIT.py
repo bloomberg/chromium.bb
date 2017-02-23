f74c929222deb4c4c5065c364bd2be84b37d0b53
dbeam <dbeam@chromium.org>
2017-02-23 11:27:08 -0800

    Closure compiler: add try bots to CLs from more directories
    R=dpapad@chromium.org
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2713793002
    Cr-Commit-Position: refs/heads/master@{#452583}
    
    Former-commit-id: 3f7bdc0ab1aeeb97fa8fcdf0e400232d31b7adb2

--- ui/webui/resources/PRESUBMIT.py
+++ ui/webui/resources/PRESUBMIT.py
@@ -2,9 +2,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import re
-
-
 def PostUploadHook(cl, change, output_api):
   return output_api.EnsureCQIncludeTrybotsAreAdded(
     cl,
