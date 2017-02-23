59dd44295c1c8f3b06d629b0a2eb16a202c391f6
fukino <fukino@chromium.org>
2017-02-23 00:59:15 -0800

    Files app: Add presubmit check for JavaScript code format.
    BUG=none
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2715653002
    Cr-Commit-Position: refs/heads/master@{#452437}
    
    Former-commit-id: 0aab3ee4e050bd84d990cc0dfdac90afb05621e5

--- ui/file_manager/PRESUBMIT.py
+++ ui/file_manager/PRESUBMIT.py
@@ -9,3 +9,16 @@ def PostUploadHook(cl, change, output_api):
       'master.tryserver.chromium.linux:closure_compilation',
     ],
     'Automatically added optional Closure bots to run on CQ.')
+
+
+def CheckChangeOnUpload(input_api, output_api):
+  return _CommonChecks(input_api, output_api)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+  return _CommonChecks(input_api, output_api)
+
+
+def _CommonChecks(input_api, output_api):
+  return input_api.canned_checks.CheckPatchFormatted(input_api, output_api,
+                                                     check_js=True)
