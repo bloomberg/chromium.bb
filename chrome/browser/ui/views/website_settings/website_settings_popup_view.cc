cabdaa35c3cd3ac18cd0c6a11f60a6747a167e40
tapted <tapted@chromium.org>
2017-02-23 17:24:53 -0800

    Don't offset chrome:// page security bubbles.
    It breaks the alignment logic under Harmony, and makes the alignment
    inconsistent with regular bubbles under non-Harmony.
    
    Note under non-Harmony, the Chrome icon in the bubble will no longer be
    directly below the Chrome icon in the Omnibox. This is intentional.
    
    Under Harmony, the icon in the bubble is going away.
    
    BUG=691445
    
    Review-Url: https://codereview.chromium.org/2693833002
    Cr-Commit-Position: refs/heads/master@{#452707}
    
    Former-commit-id: 28ad54cb961a743161398564f4f1f97ded3b2350

--- chrome/browser/ui/views/website_settings/website_settings_popup_view.cc
+++ chrome/browser/ui/views/website_settings/website_settings_popup_view.cc
@@ -46,7 +46,6 @@
 #include "ui/gfx/image/image.h"
 #include "ui/resources/grit/ui_resources.h"
 #include "ui/views/border.h"
-#include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/button/md_text_button.h"
 #include "ui/views/controls/image_view.h"
@@ -170,8 +169,6 @@ class InternalPageInfoPopupView : public views::BubbleDialogDelegateView {
   ~InternalPageInfoPopupView() override;
 
   // views::BubbleDialogDelegateView:
-  views::NonClientFrameView* CreateNonClientFrameView(
-      views::Widget* widget) override;
   void OnWidgetDestroying(views::Widget* widget) override;
   int GetDialogButtons() const override;
 
@@ -324,16 +321,6 @@ InternalPageInfoPopupView::InternalPageInfoPopupView(
 }
 
 InternalPageInfoPopupView::~InternalPageInfoPopupView() {
-}
-
-views::NonClientFrameView* InternalPageInfoPopupView::CreateNonClientFrameView(
-    views::Widget* widget) {
-  views::BubbleFrameView* frame = static_cast<views::BubbleFrameView*>(
-      BubbleDialogDelegateView::CreateNonClientFrameView(widget));
-  // Padding around icon + half of icon width.
-  frame->bubble_border()->set_arrow_offset(
-      kSpacing + 8 + frame->bubble_border()->GetBorderThickness());
-  return frame;
 }
 
 void InternalPageInfoPopupView::OnWidgetDestroying(views::Widget* widget) {
