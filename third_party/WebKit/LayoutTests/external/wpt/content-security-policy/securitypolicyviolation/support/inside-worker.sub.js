a6943f442da20eb9b26e1dc3211ec18dca51d23c
mkwst <mkwst@chromium.org>
2017-02-24 01:31:19 -0800

    Upstream Blink's 'securitypolicyviolation' tests.
    Starting on the long slog to get our CSP layout tests more completely
    upstreamed. This patch leaves most upstream CSP tests disabled, as I
    still need to go through them in detail, but carves out the
    'securitypolicyviolation' directory, moves our local tests over, and
    ports them to `testharness.js`-style.
    
    BUG=695486
    
    Review-Url: https://codereview.chromium.org/2711913003
    Cr-Commit-Position: refs/heads/master@{#452780}
    
    Former-commit-id: 7e900589febbfc49da56f18b33750786c24ba14c

--- third_party/WebKit/LayoutTests/external/wpt/content-security-policy/securitypolicyviolation/support/inside-worker.sub.js
+++ third_party/WebKit/LayoutTests/external/wpt/content-security-policy/securitypolicyviolation/support/inside-worker.sub.js
@@ -1,9 +1,5 @@
-<?php
-  header("Content-Type: application/javascript");
-  header("Content-Security-Policy: connect-src http://127.0.0.1:8000");
-?>
-importScripts("http://127.0.0.1:8000/resources/testharness.js");
-importScripts("http://127.0.0.1:8000/security/contentSecurityPolicy/resources/testharness-helper.js");
+importScripts("{{location[scheme]}}://{{host}}:{{location[port]}}/resources/testharness.js");
+importScripts("{{location[scheme]}}://{{host}}:{{location[port]}}/content-security-policy/support/testharness-helper.js");
 
 var cspEventFiredInDocument = false;
 // ServiceWorker and Worker
@@ -20,7 +16,7 @@ self.addEventListener("connect", c => {
 });
 
 async_test(t => {
-  var url = "http://127.0.0.1:8000/security/resources/cors-hello.php";
+  var url = "{{location[scheme]}}://{{host}}:{{location[port]}}/content-security-policy/support/resource.py";
   assert_no_csp_event_for_url(t, url);
 
   fetch(url)
@@ -32,7 +28,7 @@ async_test(t => {
 }, "No SecurityPolicyViolation event fired for successful load.");
 
 async_test(t => {
-  var url = "http://1.example.test:8000/security/resources/cors-hello.php";
+  var url = "{{location[scheme]}}://{{domains[www2]}}:{{location[port]}}/content-security-policy/support/resource.py";
   waitUntilCSPEventForURL(t, url)
     .then(t.step_func_done(e => {
       assert_equals(e.blockedURI, url);
