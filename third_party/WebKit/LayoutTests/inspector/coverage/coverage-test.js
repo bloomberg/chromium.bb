e03a35ea84a94ec6b316ec6e11112573e9e0e896
caseq <caseq@chromium.org>
2017-02-27 19:42:03 -0800

    DevTools: make coverage start/stop an action, use standard record button rendering
    Drive-by: give coverage experiment a more apporpriate display name (but keep
    old setting name intact)
    
    Review-Url: https://codereview.chromium.org/2719133003
    Cr-Commit-Position: refs/heads/master@{#453486}
    
    Former-commit-id: 9c4b5c2e2031c57553ef03b7077d0accd83e65c5

--- third_party/WebKit/LayoutTests/inspector/coverage/coverage-test.js
+++ third_party/WebKit/LayoutTests/inspector/coverage/coverage-test.js
@@ -2,8 +2,21 @@ function initialize_CoverageTests() {
 
 InspectorTest.preloadModule("coverage");
 
-InspectorTest.sourceDecorated = async function(source) {
+InspectorTest.startCoverage = function()
+{
+    UI.viewManager.showView("coverage");
+    var coverageView = self.runtime.sharedInstance(Coverage.CoverageView);
+    coverageView._startRecording();
+}
 
+InspectorTest.stopCoverage = function()
+{
+    var coverageView = self.runtime.sharedInstance(Coverage.CoverageView);
+    return coverageView._stopRecording();
+}
+
+InspectorTest.sourceDecorated = async function(source)
+{
     await UI.inspectorView.showPanel("sources");
     var decoratePromise = InspectorTest.addSnifferPromise(Coverage.CoverageView.LineDecorator.prototype, "decorate");
     await new Promise(fulfill => InspectorTest.showScriptSource(source, fulfill));
