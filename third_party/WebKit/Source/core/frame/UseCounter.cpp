409de79ecaa2ea97c0fafd31bf6b3fcdd983e9aa
kojii <kojii@chromium.org>
2017-02-28 01:57:21 -0800

    Add unprefixed -webkit-line-break as an alias to import csswg-test
    This patch adds unprefixed line-break as an alias to -webkit-line-break.
    
    Currently, the -webkit-line-break is parsed but is not honored. This is
    a preparation step to import csswg-test for the 'line-break' property.
    
    To import csswg-test, adding unprefixed aliases is recommended in
    crbug.com/614955.
    
    BUG=306730, 614955
    
    Review-Url: https://codereview.chromium.org/2725543002
    Cr-Commit-Position: refs/heads/master@{#453559}
    
    Former-commit-id: c90e37c93ed9855f9f2e2d7a1b8729c6db5b3934

--- third_party/WebKit/Source/core/frame/UseCounter.cpp
+++ third_party/WebKit/Source/core/frame/UseCounter.cpp
@@ -46,7 +46,7 @@ int totalPagesMeasuredCSSSampleId() {
 }
 
 // Make sure update_use_counter_css.py was run which updates histograms.xml.
-constexpr int kMaximumCSSSampleId = 555;
+constexpr int kMaximumCSSSampleId = 556;
 
 }  // namespace
 
@@ -1079,6 +1079,8 @@ int UseCounter::mapCSSPropertyIdToCSSSampleIdForHistogram(
       return 554;
     case CSSPropertyMaxBlockSize:
       return 555;
+    case CSSPropertyAliasLineBreak:
+      return 556;
     // 1. Add new features above this line (don't change the assigned numbers of
     // the existing items).
     // 2. Update kMaximumCSSSampleId with the new maximum value.
