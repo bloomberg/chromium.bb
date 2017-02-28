48d30b25a8c0899186986f0edf4cf5c8c17b2739
jam <jam@chromium.org>
2017-02-28 14:30:37 -0800

    Revert of [Media Router] Remove binary messaging implementation. (patchset #1 id:1 of https://codereview.chromium.org/2720273003/ )
    Reason for revert:
    Breaks the build.
    
    Original issue's description:
    > [Media Router] Remove binary messaging implementation.
    >
    > This removes the binary messaging implementation for Media Router from Android,
    > as there are no plans to support it for Cast.
    >
    > BUG=524128
    > R=avayvod@chromium.org
    >
    > Review-Url: https://codereview.chromium.org/2720273003 .
    > Cr-Commit-Position: refs/heads/master@{#453709}
    > Committed: https://chromium.googlesource.com/chromium/src/+/2aa6659106a903e60ae9fb1b8b309d7d0bb80328 [formerly ebdab4190ceb21a9ba4a5d8e719df9ab94b2716e]
    
    TBR=avayvod@chromium.org,mfoltz@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=524128
    
    Review-Url: https://codereview.chromium.org/2720363002
    Cr-Commit-Position: refs/heads/master@{#453729}
    
    Former-commit-id: e9aeb1792103e88435b765da22ba37ff80960887

--- chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/CastMediaRouteProvider.java
+++ chrome/android/java/src/org/chromium/chrome/browser/media/router/cast/CastMediaRouteProvider.java
@@ -376,6 +376,15 @@ public class CastMediaRouteProvider implements MediaRouteProvider, DiscoveryDele
         mManager.onMessageSentResult(success, nativeCallbackId);
     }
 
+    @Override
+    public void sendBinaryMessage(String routeId, byte[] data, int nativeCallbackId) {
+        // TODO(crbug.com/524128): Cast API does not support sending binary message
+        // to receiver application. Binary data may be converted to String and send as
+        // an app_message within it's own message namespace, using the string version.
+        // Sending failure in the result callback for now.
+        mManager.onMessageSentResult(false, nativeCallbackId);
+    }
+
     private boolean handleClientConnectMessage(JSONObject jsonMessage) throws JSONException {
         String clientId = jsonMessage.getString("clientId");
         if (clientId == null) return false;
