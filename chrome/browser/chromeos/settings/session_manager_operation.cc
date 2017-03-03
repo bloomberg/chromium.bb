1424a3a74fd60621a092e0d344c23fa4a710ea1f
emaxx <emaxx@chromium.org>
2017-03-02 19:54:13 -0800

    Fix handling of device cloud signing policy key rotation
    This CL fixes issues with handling of the signing key rotation for the
    device policy. There were some issues with not loading the updated
    owner key right after the new policy is stored. Even though the key
    would eventually be loaded thanks to the OwnerKeySet signal emitted
    by the session manager, there may be some window during which the old
    key is still used on the Chrome side.
    
    Also the CL adds a browser end-to-end test for browser policy key
    rotation (using a local policy test server). Some fake testing-only
    stubs were also fixed in order to support changing the signing keys
    (similar to how this is handled by the session manager).
    
    BUG=671659,668716
    TEST=new browser test
    
    Review-Url: https://codereview.chromium.org/2558543003
    Cr-Commit-Position: refs/heads/master@{#454506}
    
    Former-commit-id: 6581fa97d9ac35a9c541856a0acc04c8be2cfc20

--- chrome/browser/chromeos/settings/session_manager_operation.cc
+++ chrome/browser/chromeos/settings/session_manager_operation.cc
@@ -47,7 +47,7 @@ void SessionManagerOperation::Start(
 
 void SessionManagerOperation::RestartLoad(bool key_changed) {
   if (key_changed)
-    public_key_ = NULL;
+    public_key_ = nullptr;
 
   if (!is_loading_)
     return;
@@ -84,20 +84,17 @@ void SessionManagerOperation::ReportResult(
 }
 
 void SessionManagerOperation::EnsurePublicKey(const base::Closure& callback) {
-  if (force_key_load_ || !public_key_.get() || !public_key_->is_loaded()) {
+  if (force_key_load_ || !public_key_ || !public_key_->is_loaded()) {
     scoped_refptr<base::TaskRunner> task_runner =
         content::BrowserThread::GetBlockingPool()
             ->GetTaskRunnerWithShutdownBehavior(
                 base::SequencedWorkerPool::SKIP_ON_SHUTDOWN);
     base::PostTaskAndReplyWithResult(
-        task_runner.get(),
-        FROM_HERE,
-        base::Bind(&SessionManagerOperation::LoadPublicKey,
-                   owner_key_util_,
-                   public_key_),
+        task_runner.get(), FROM_HERE,
+        base::Bind(&SessionManagerOperation::LoadPublicKey, owner_key_util_,
+                   force_key_load_ ? nullptr : public_key_),
         base::Bind(&SessionManagerOperation::StorePublicKey,
-                   weak_factory_.GetWeakPtr(),
-                   callback));
+                   weak_factory_.GetWeakPtr(), callback));
   } else {
     callback.Run();
   }
@@ -110,7 +107,7 @@ scoped_refptr<PublicKey> SessionManagerOperation::LoadPublicKey(
   scoped_refptr<PublicKey> public_key(new PublicKey());
 
   // Keep already-existing public key.
-  if (current_key.get() && current_key->is_loaded()) {
+  if (current_key && current_key->is_loaded()) {
     public_key->data() = current_key->data();
   }
   if (!public_key->is_loaded() && util->IsPublicKeyPresent()) {
@@ -126,7 +123,7 @@ void SessionManagerOperation::StorePublicKey(const base::Closure& callback,
   force_key_load_ = false;
   public_key_ = new_key;
 
-  if (!public_key_.get() || !public_key_->is_loaded()) {
+  if (!public_key_ || !public_key_->is_loaded()) {
     ReportResult(DeviceSettingsService::STORE_KEY_UNAVAILABLE);
     return;
   }
@@ -261,7 +258,10 @@ StoreSettingsOperation::StoreSettingsOperation(
     std::unique_ptr<em::PolicyFetchResponse> policy)
     : SessionManagerOperation(callback),
       policy_(std::move(policy)),
-      weak_factory_(this) {}
+      weak_factory_(this) {
+  if (policy_->has_new_public_key())
+    force_key_load_ = true;
+}
 
 StoreSettingsOperation::~StoreSettingsOperation() {}
 
