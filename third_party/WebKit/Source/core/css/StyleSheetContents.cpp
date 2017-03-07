522a3e13cee0257dc86b61fb42f7bcc774ebc3b2
horo <horo@chromium.org>
2017-03-07 08:39:43 -0800

    Skip the unnecessary check for the response of Service Worker in StyleSheetContents.
    BUG=696611
    
    Review-Url: https://codereview.chromium.org/2739473003
    Cr-Commit-Position: refs/heads/master@{#455095}
    
    Former-commit-id: 18bd51a489591d38431fdf40e69ae407b89c60a4

--- third_party/WebKit/Source/core/css/StyleSheetContents.cpp
+++ third_party/WebKit/Source/core/css/StyleSheetContents.cpp
@@ -347,7 +347,8 @@ void StyleSheetContents::parseAuthorStyleSheet(
   // not be same as the base URL.
   // TODO(horo): When we will use the original URL as the base URL, we can
   // remove this check. crbug.com/553535
-  if (cachedStyleSheet->response().wasFetchedViaServiceWorker()) {
+  if (isSameOriginRequest &&
+      cachedStyleSheet->response().wasFetchedViaServiceWorker()) {
     const KURL originalURL(
         cachedStyleSheet->response().originalURLViaServiceWorker());
     // |originalURL| is empty when the response is created in the SW.
