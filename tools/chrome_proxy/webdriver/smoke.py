d1d7ba55315534f52fe212870444bbc540a77a6e
dougarnett <dougarnett@chromium.org>
2017-03-08 08:55:29 -0800

    Adds proxy pingback chromedriver integration test (testPingback).
    BUG=680577
    
    Review-Url: https://codereview.chromium.org/2732133002
    Cr-Commit-Position: refs/heads/master@{#455474}
    
    Former-commit-id: ce6e8b3d591faa42d1044ae562cde091166e3ccc

--- tools/chrome_proxy/webdriver/smoke.py
+++ tools/chrome_proxy/webdriver/smoke.py
@@ -31,5 +31,19 @@ class Smoke(IntegrationTest):
       for response in responses:
         self.assertHasChromeProxyViaHeader(response)
 
+  # Ensure pageload metric pingback with DataSaver.
+  def testPingback(self):
+    with TestDriver() as t:
+      t.AddChromeArg('--enable-spdy-proxy-auth')
+      t.AddChromeArg('--enable-data-reduction-proxy-force-pingback')
+      t.LoadURL('http://check.googlezip.net/test.html')
+      t.LoadURL('http://check.googlezip.net/test.html')
+      t.SleepUntilHistogramHasEntry("DataReductionProxy.Pingback.Succeeded")
+      # Verify one pingback attempt that was successful.
+      attempted = t.GetHistogram('DataReductionProxy.Pingback.Attempted')
+      self.assertEqual(1, attempted['count'])
+      succeeded = t.GetHistogram('DataReductionProxy.Pingback.Succeeded')
+      self.assertEqual(1, succeeded['count'])
+
 if __name__ == '__main__':
   IntegrationTest.RunAllTests()
