1f195f18f556bf4a4c78f35e8d4bfbd88f2447a3
mstensho <mstensho@opera.com>
2017-03-08 01:45:16 -0800

    Search the entire subtree when looking for the end of an inline continuation chain.
    Inlines may be nested, so we may not find the last inline in the chain as a
    direct child of the anonymous blocks. We need to search the entire subtree.
    Don't do this with anonymous blocks that wrap block children (the block-level
    DOM children of the inline-level objects), though. We're not going to find
    anything interesting there.
    
    This fix is speculative; the original bug report didn't come with a test case.
    
    BUG=662767
    
    Review-Url: https://codereview.chromium.org/2738503004
    Cr-Commit-Position: refs/heads/master@{#455420}
    
    Former-commit-id: e72c8c06b956706b54648589f807086d17340831

--- third_party/WebKit/Source/core/layout/LayoutInline.cpp
+++ third_party/WebKit/Source/core/layout/LayoutInline.cpp
@@ -1140,11 +1140,17 @@ LayoutRect LayoutInline::absoluteVisualRect() const {
   for (LayoutBlock* currBlock = containingBlock();
        currBlock && currBlock->isAnonymousBlock();
        currBlock = toLayoutBlock(currBlock->nextSibling())) {
+    bool walkChildrenOnly = !currBlock->childrenInline();
     for (LayoutObject* curr = currBlock->firstChild(); curr;
          curr = curr->nextSibling()) {
       LayoutRect rect(curr->localVisualRect());
       context(FloatRect(rect));
-      if (curr == endContinuation) {
+      if (walkChildrenOnly)
+        continue;
+      for (LayoutObject* walker = curr; walker;
+           walker = walker->nextInPreOrder(curr)) {
+        if (walker != endContinuation)
+          continue;
         LayoutRect rect(enclosingIntRect(floatResult));
         mapToVisualRectInAncestorSpace(view(), rect);
         return rect;
