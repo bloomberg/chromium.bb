7b7c04376f2d5edca1592526e70fdf797a9a8d06
sashab <sashab@chromium.org>
2017-03-08 15:00:18 -0800

    Reland of Make EventHandlerRegistry store Page instead of FrameHost (patchset #1 id:1 of https://codereview.chromium.org/2737863005/ )
    Reason for revert:
    Patch unrelated to test failure; trying to revert-revert
    
    Original issue's description:
    > Revert of Make EventHandlerRegistry store Page instead of FrameHost (patchset #1 id:1 of https://codereview.chromium.org/2736753003/ )
    >
    > Reason for revert:
    > virtual/gpu-rasterization/images/cross-fade-background-size.html failed after this change.
    >
    > No idea why, perhaps it is too sensitive, or needs a concurrent rebaseline.
    >
    > Tracked in bug 699573
    >
    > Original issue's description:
    > > Make EventHandlerRegistry store Page instead of FrameHost
    > >
    > > This is pre-work to move FrameHost::m_eventHandlerRegistry to Page. Also
    > > renamed didMove{Into,OutOf}FrameHost to didMove{Into,OutOf}Page, which
    > > now makes more sense.
    > >
    > > BUG=691794
    > >
    > > Review-Url: https://codereview.chromium.org/2736753003
    > > Cr-Commit-Position: refs/heads/master@{#455379}
    > > Committed: https://chromium.googlesource.com/chromium/src/+/9df6f5c794c8b28513b3c5e86d7b53b80a8df724 [formerly 5c4056a61c3d24b5df4ebdba406467b96ff68866]
    >
    > TBR=joelhockey@chromium.org,sashab@chromium.org
    > # Skipping CQ checks because original CL landed less than 1 days ago.
    > NOPRESUBMIT=true
    > NOTREECHECKS=true
    > NOTRY=true
    > BUG=691794
    >
    > Review-Url: https://codereview.chromium.org/2737863005
    > Cr-Commit-Position: refs/heads/master@{#455462}
    > Committed: https://chromium.googlesource.com/chromium/src/+/11d8fd56c80a2b477ea5617d9f4c98e48c18950c [formerly c5a5f183a349f0088685acc2a063f9b84e9ec237]
    
    TBR=joelhockey@chromium.org,petermayo@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=691794
    
    Review-Url: https://codereview.chromium.org/2742543002
    Cr-Commit-Position: refs/heads/master@{#455575}
    
    Former-commit-id: 6fe37909237df848564e70b749eac2a3a8e32b95

--- third_party/WebKit/Source/core/frame/EventHandlerRegistry.h
+++ third_party/WebKit/Source/core/frame/EventHandlerRegistry.h
@@ -6,7 +6,8 @@
 #define EventHandlerRegistry_h
 
 #include "core/CoreExport.h"
-#include "core/frame/FrameHost.h"
+#include "core/frame/FrameHost.h"  // TODO(sashab): Remove this.
+#include "core/page/Page.h"
 #include "wtf/HashCountedSet.h"
 
 namespace blink {
@@ -20,11 +21,11 @@ typedef HashCountedSet<UntracedMember<EventTarget>> EventTargetSet;
 
 // Registry for keeping track of event handlers. Note that only handlers on
 // documents that can be rendered or can receive input (i.e., are attached to a
-// FrameHost) are registered here.
+// Page) are registered here.
 class CORE_EXPORT EventHandlerRegistry final
     : public GarbageCollectedFinalized<EventHandlerRegistry> {
  public:
-  explicit EventHandlerRegistry(FrameHost&);
+  explicit EventHandlerRegistry(Page&);
   virtual ~EventHandlerRegistry();
 
   // Supported event handler classes. Note that each one may correspond to
@@ -44,7 +45,7 @@ class CORE_EXPORT EventHandlerRegistry final
     EventHandlerClassCount,  // Must be the last entry.
   };
 
-  // Returns true if the FrameHost has event handlers of the specified class.
+  // Returns true if the Page has event handlers of the specified class.
   bool hasEventHandlers(EventHandlerClass) const;
 
   // Returns a set of EventTargets which have registered handlers of the given
@@ -62,11 +63,11 @@ class CORE_EXPORT EventHandlerRegistry final
   void didRemoveEventHandler(EventTarget&, EventHandlerClass);
   void didRemoveAllEventHandlers(EventTarget&);
 
-  void didMoveIntoFrameHost(EventTarget&);
-  void didMoveOutOfFrameHost(EventTarget&);
+  void didMoveIntoPage(EventTarget&);
+  void didMoveOutOfPage(EventTarget&);
 
-  // Either |documentDetached| or |didMove{Into,OutOf,Between}FrameHosts| must
-  // be called whenever the FrameHost that is associated with a registered event
+  // Either |documentDetached| or |didMove{Into,OutOf,Between}Pages| must
+  // be called whenever the Page that is associated with a registered event
   // target changes. This ensures the registry does not end up with stale
   // references to handlers that are no longer related to it.
   void documentDetached(Document&);
@@ -120,7 +121,7 @@ class CORE_EXPORT EventHandlerRegistry final
 
   void checkConsistency(EventHandlerClass) const;
 
-  Member<FrameHost> m_frameHost;
+  Member<Page> m_page;
   EventTargetSet m_targets[EventHandlerClassCount];
 };
 
