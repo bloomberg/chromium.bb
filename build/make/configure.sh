wjdz99/aom

Add analyzer to build system.
Change-Id: I93da53eae42d91419cc5bb4d55e7ce87eadd2551

build/make/configure.sh
@@ -1148,6 +1148,10 @@ EOF
             enabled pic && add_asflags --apcs=/fpic
             enabled shared && add_cflags --shared
           fi
+          if enabled analyzer; then
+            add_cflags ${WX_CXXFLAGS}
+            add_ldflags ${WX_LDFLAGS}
+          fi
           ;;
       esac
       ;;

bloomberg/chromium.bb

build/make/configure.sh
@@ -1148,6 +1148,10 @@ EOF
             enabled pic && add_asflags --apcs=/fpic
             enabled shared && add_cflags --shared
           fi
+          if enabled analyzer; then
+            add_cxxflags ${WX_CXXFLAGS}
+            add_extralibs ${WX_LDFLAGS}
+          fi
           ;;
       esac
       ;;