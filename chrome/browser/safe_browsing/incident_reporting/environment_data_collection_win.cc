ac1fc5c08e843edd8c07f8ac256e8a513292b00d
jialiul <jialiul@chromium.org>
2017-03-08 16:57:51 -0800

    Revert of Move chrome/common/safe_browsing/csd.proto to components/safe_browsing (patchset #2 id:20001 of https://codereview.chromium.org/2733343002/ )
    Reason for revert:
    revert this CL since it broke linux bots.
    
    Original issue's description:
    > Move chrome/common/safe_browsing/csd.proto to components/safe_browsing
    >
    > Move csd.proto to components/safe_browsing.
    > This CL serves 2 purpose:
    > (1) moving csd.proto to component enables further componentization of
    > safe browsing reporting functionality, that will be used by WebView
    > (2) It also allows PasswordProtectionService, and PasswordManager code
    > to use these proto directly.
    >
    > BUG=698899,688629
    >
    > Review-Url: https://codereview.chromium.org/2733343002
    > Cr-Commit-Position: refs/heads/master@{#455609}
    > Committed: https://chromium.googlesource.com/chromium/src/+/b2c2e65953c9142d7da9ffe8537d234dd6c2d62a [formerly f3aafdc5ac1a6caf52c6c41615118e381ad9e549]
    
    TBR=nparker@chromium.org,vakh@chromium.org,sky@chromium.org,pkasting@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=698899,688629
    
    Review-Url: https://codereview.chromium.org/2743483002
    Cr-Commit-Position: refs/heads/master@{#455617}
    
    Former-commit-id: 7fdf43c8251e77df8fba1933f71b9a40b924359e

--- chrome/browser/safe_browsing/incident_reporting/environment_data_collection_win.cc
+++ chrome/browser/safe_browsing/incident_reporting/environment_data_collection_win.cc
@@ -25,8 +25,8 @@
 #include "chrome/browser/safe_browsing/incident_reporting/module_integrity_verifier_win.h"
 #include "chrome/browser/safe_browsing/path_sanitizer.h"
 #include "chrome/common/safe_browsing/binary_feature_extractor.h"
+#include "chrome/common/safe_browsing/csd.pb.h"
 #include "chrome_elf/chrome_elf_constants.h"
-#include "components/safe_browsing/csd.pb.h"
 #include "components/variations/variations_associated_data.h"
 
 namespace safe_browsing {
