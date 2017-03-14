b5279098e3aee6c8ea62b94882ea896c34b72aec
sigbjornf <sigbjornf@opera.com>
2017-03-14 09:56:37 -0700

    Pull AudioDestinationConsumer off the Blink GC heap.
    The AudioDestinationConsumer interface and its single implementation
    does not meet the bar for being on the Blink GC heap. It doesn't cause
    harm to have them there, but in order to diagnose an unexplained
    failure, pull these objects off the heap.
    
    We may want to restore them to the Blink GC heap once the issue has
    been resolved.
    
    R=
    BUG=682945
    
    Review-Url: https://codereview.chromium.org/2748133003
    Cr-Commit-Position: refs/heads/master@{#456736}
    
    Former-commit-id: bb9b04fe0965eeb09f229f7a727fe7235add810f

--- third_party/WebKit/Source/platform/mediastream/MediaStreamSource.cpp
+++ third_party/WebKit/Source/platform/mediastream/MediaStreamSource.cpp
@@ -85,8 +85,7 @@ bool MediaStreamSource::removeAudioConsumer(
     AudioDestinationConsumer* consumer) {
   ASSERT(m_requiresConsumer);
   MutexLocker locker(m_audioConsumersLock);
-  HeapHashSet<Member<AudioDestinationConsumer>>::iterator it =
-      m_audioConsumers.find(consumer);
+  auto it = m_audioConsumers.find(consumer);
   if (it == m_audioConsumers.end())
     return false;
   m_audioConsumers.erase(it);
@@ -101,24 +100,19 @@ void MediaStreamSource::setAudioFormat(size_t numberOfChannels,
                                        float sampleRate) {
   ASSERT(m_requiresConsumer);
   MutexLocker locker(m_audioConsumersLock);
-  for (HeapHashSet<Member<AudioDestinationConsumer>>::iterator it =
-           m_audioConsumers.begin();
-       it != m_audioConsumers.end(); ++it)
-    (*it)->setFormat(numberOfChannels, sampleRate);
+  for (AudioDestinationConsumer* consumer : m_audioConsumers)
+    consumer->setFormat(numberOfChannels, sampleRate);
 }
 
 void MediaStreamSource::consumeAudio(AudioBus* bus, size_t numberOfFrames) {
   ASSERT(m_requiresConsumer);
   MutexLocker locker(m_audioConsumersLock);
-  for (HeapHashSet<Member<AudioDestinationConsumer>>::iterator it =
-           m_audioConsumers.begin();
-       it != m_audioConsumers.end(); ++it)
-    (*it)->consumeAudio(bus, numberOfFrames);
+  for (AudioDestinationConsumer* consumer : m_audioConsumers)
+    consumer->consumeAudio(bus, numberOfFrames);
 }
 
 DEFINE_TRACE(MediaStreamSource) {
   visitor->trace(m_observers);
-  visitor->trace(m_audioConsumers);
 }
 
 }  // namespace blink
