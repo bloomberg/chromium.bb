054739f314aef1f46311693f1bdbf1bda865c063
tkent <tkent@chromium.org>
2017-03-14 00:03:53 -0700

    INPUT element: Implicit form submission should reset setTextAsOfLastFormControlChangeEvent even if no <form>.
    BUG=700842
    
    Review-Url: https://codereview.chromium.org/2749513003
    Cr-Commit-Position: refs/heads/master@{#456644}
    
    Former-commit-id: 1eff0d16f548ffa09d45de590c17f780406ae05e

--- third_party/WebKit/Source/core/html/HTMLInputElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLInputElement.cpp
@@ -1266,12 +1266,12 @@ void HTMLInputElement::defaultEventHandler(Event* evt) {
     HTMLFormElement* formForSubmission = m_inputTypeView->formForSubmission();
     // Form may never have been present, or may have been destroyed by code
     // responding to the change event.
-    if (formForSubmission) {
+    if (formForSubmission)
       formForSubmission->submitImplicitly(evt, canTriggerImplicitSubmission());
-      // We treat implicit submission is something like blur()-then-focus(). So
-      // we reset the last value. crbug.com/695349.
-      setTextAsOfLastFormControlChangeEvent(value());
-    }
+
+    // We treat implicit submission is something like blur()-then-focus(). So
+    // we reset the last value. crbug.com/695349 and crbug.com/700842.
+    setTextAsOfLastFormControlChangeEvent(value());
 
     evt->setDefaultHandled();
     return;
