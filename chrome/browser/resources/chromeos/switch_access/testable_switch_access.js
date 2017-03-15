5e29fa21778c52f4d31ab046df493cf4c7767c87
elichtenberg <elichtenberg@google.com>
2017-03-14 18:57:31 -0700

    Added unit tests for switch_access.js.
    BUG=593885
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2748853002
    Cr-Commit-Position: refs/heads/master@{#456943}
    
    Former-commit-id: 0b2807a74e0055362506beb0cda0b8f3c9d0cd40

---
+++ chrome/browser/resources/chromeos/switch_access/testable_switch_access.js
@@ -0,0 +1,30 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Subclass of SwitchAccess that exposes SwitchAccess's methods for testing.
+ */
+let TestableSwitchAccess = function() {
+  SwitchAccess.call(this);
+};
+
+TestableSwitchAccess.prototype = {
+  __proto__: SwitchAccess.prototype,
+
+  getNextNode: function(node) {
+    return this.getNextNode_(node);
+  },
+
+  getPreviousNode: function(node) {
+    return this.getPreviousNode_(node);
+  },
+
+  getYoungestDescendant: function(node) {
+    return this.getYoungestDescendant_(node);
+  },
+
+  isInteresting: function(node) {
+    return this.isInteresting_(node);
+  }
+};
