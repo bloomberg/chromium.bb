f0e2d2051ae6cbc201322a0b4322b76418bb176b
yutak <yutak@chromium.org>
2017-03-16 20:42:01 -0700

    Move files in wtf/ to platform/wtf/ (Part 3).
    The last part for files with simple dependencies.
    
        NonCopyingSort.h
        NotFound.h
        Optional.h
        SizeAssertions.h
        StaticConstructors.h
        Time.h
        TriState.h
    
    Note that test files corresponding to those headers are still left in wtf/,
    because it seemed hard to have unit tests in two different compilation units.
    
    BUG=691465
    
    Review-Url: https://codereview.chromium.org/2755793004
    Cr-Commit-Position: refs/heads/master@{#457678}
    
    Former-commit-id: 16ddbc9a801d1689cf110d947ebd5f680a2701c8

--- third_party/WebKit/Source/wtf/Optional.h
+++ third_party/WebKit/Source/wtf/Optional.h
@@ -1,29 +1,9 @@
-// Copyright 2015 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef Optional_h
-#define Optional_h
+#include "platform/wtf/Optional.h"
 
-#include "base/optional.h"
-#include "wtf/TypeTraits.h"
-
-namespace WTF {
-
-// WTF::Optional is base::Optional. See base/optional.h for documentation.
-//
-// A clang plugin enforces that garbage collected types are not allocated
-// outside of the heap, similarly we enforce that one doesn't create garbage
-// collected types nested inside an Optional.
-template <typename T>
-using Optional = typename std::enable_if<!IsGarbageCollectedType<T>::value,
-                                         base::Optional<T>>::type;
-
-constexpr base::nullopt_t nullopt = base::nullopt;
-constexpr base::in_place_t in_place = base::in_place;
-
-}  // namespace WTF
-
-using WTF::Optional;
-
-#endif  // Optional_h
+// The contents of this header was moved to platform/wtf as part of
+// WTF migration project. See the following post for details:
+// https://groups.google.com/a/chromium.org/d/msg/blink-dev/tLdAZCTlcAA/bYXVT8gYCAAJ
--- third_party/WebKit/Source/wtf/SizeAssertions.h
+++ third_party/WebKit/Source/wtf/SizeAssertions.h
@@ -1,29 +1,9 @@
-// Copyright 2016 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef WTF_SizeAssertions_h
-#define WTF_SizeAssertions_h
+#include "platform/wtf/SizeAssertions.h"
 
-namespace WTF {
-
-// The ASSERT_SIZE macro can be used to check that a given struct is the same
-// size as a class. This is useful to visualize where the space is being used in
-// a class, as well as give a useful compile error message when the size doesn't
-// match the expected value.
-template <class T, class U>
-struct assert_size {
-  template <int ActualSize, int ExpectedSize>
-  struct assertSizeEqual {
-    static_assert(ActualSize == ExpectedSize, "Class should stay small");
-    static const bool innerValue = true;
-  };
-  static const bool value = assertSizeEqual<sizeof(T), sizeof(U)>::innerValue;
-};
-
-}  // namespace WTF
-
-#define ASSERT_SIZE(className, sameSizeAsClassName) \
-  static_assert(WTF::assert_size<className, sameSizeAsClassName>::value, "");
-
-#endif  // WTF_SizeAssertions_h
+// The contents of this header was moved to platform/wtf as part of
+// WTF migration project. See the following post for details:
+// https://groups.google.com/a/chromium.org/d/msg/blink-dev/tLdAZCTlcAA/bYXVT8gYCAAJ
