58967dc2948c91665717bce59132388dd127aa8a
dominicc <dominicc@chromium.org>
2017-03-16 22:16:02 -0700

    node-set($x), when $x is empty, should not halt XSLT processing
    node-set is polymorphic; if called with a string, it returns a
    node-set containing a text node with that content. The libxslt roll in
    r425586 broke this behavior because Blink's fork of the node-set
    extension returned a text node without an owner document. Fix that.
    
    BUG=689977
    
    Review-Url: https://codereview.chromium.org/2750943004
    Cr-Commit-Position: refs/heads/master@{#457692}
    
    Former-commit-id: 3f6e482b7f13c1fc5bd68227321ef6da0ddf9937

--- third_party/WebKit/Source/core/xml/XSLTExtensions.cpp
+++ third_party/WebKit/Source/core/xml/XSLTExtensions.cpp
@@ -52,14 +52,21 @@ static void exsltNodeSetFunction(xmlXPathParserContextPtr ctxt, int nargs) {
     return;
   }
 
-  strval = xmlXPathPopString(ctxt);
-  retNode = xmlNewDocText(0, strval);
-  ret = xmlXPathNewValueTree(retNode);
+  // node-set can also take a string and turn it into a singleton node
+  // set with one text node. This may null-deref if allocating the
+  // document, text node, etc. fails; that behavior is expected.
 
-  // FIXME: It might be helpful to push any errors from xmlXPathNewValueTree
-  // up to the Javascript Console.
-  if (ret)
-    ret->type = XPATH_NODESET;
+  // Create a document to hold the text node result.
+  xsltTransformContextPtr tctxt = xsltXPathGetTransformContext(ctxt);
+  xmlDocPtr fragment = xsltCreateRVT(tctxt);
+  xsltRegisterLocalRVT(tctxt, fragment);
+
+  // Create the text node and wrap it in a result set.
+  strval = xmlXPathPopString(ctxt);
+  retNode = xmlNewDocText(fragment, strval);
+  xmlAddChild(reinterpret_cast<xmlNodePtr>(fragment), retNode);
+  ret = xmlXPathNewNodeSet(retNode);
+  CHECK(ret);
 
   if (strval)
     xmlFree(strval);
