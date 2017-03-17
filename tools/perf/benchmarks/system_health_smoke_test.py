007134d7853b54bd9d2a533900ffc102fd46f965
nednguyen <nednguyen@google.com>
2017-03-17 11:03:51 -0700

    Prefetch all WPR archives used by system_health_smoke_test
    This also enables system_health_smoke_test on android.chromium bot (Android N5X Swarm Builder).
    
    BUG=700426
    
    Review-Url: https://codereview.chromium.org/2752033002
    Cr-Commit-Position: refs/heads/master@{#457813}
    
    Former-commit-id: cae53638598092a3a97ad4fd6fbe5addd9404d9a

--- tools/perf/benchmarks/system_health_smoke_test.py
+++ tools/perf/benchmarks/system_health_smoke_test.py
@@ -155,8 +155,14 @@ def load_tests(loader, standard_tests, pattern):
     # Since none of our system health benchmarks creates stories based on
     # command line options, it should be ok to pass options=None to
     # CreateStorySet.
-    for story_to_smoke_test in (
-        benchmark_class().CreateStorySet(options=None).stories):
+    stories_set = benchmark_class().CreateStorySet(options=None)
+
+    # Prefetch WPR archive needed by the stories set to avoid race condition
+    # when feching them when tests are run in parallel.
+    # See crbug.com/700426 for more details.
+    stories_set.wpr_archive_info.DownloadArchivesIfNeeded()
+
+    for story_to_smoke_test in stories_set.stories:
       suite.addTest(
           _GenerateSmokeTestCase(benchmark_class, story_to_smoke_test))
 
