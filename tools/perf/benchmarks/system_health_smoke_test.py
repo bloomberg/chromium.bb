d32ddddddb18a4779a833c1ea4cf63621d725e11
nednguyen <nednguyen@google.com>
2017-03-16 21:51:59 -0700

    Disable system_health.memory_desktop.browse:media:youtube smoke test
    BUG=702455
    TBR=perezju@chromium.org,
    
    Review-Url: https://codereview.chromium.org/2756073002
    Cr-Commit-Position: refs/heads/master@{#457686}
    
    Former-commit-id: cbed8980f896748f5c453fa3adfc08ab037af449

--- tools/perf/benchmarks/system_health_smoke_test.py
+++ tools/perf/benchmarks/system_health_smoke_test.py
@@ -32,6 +32,8 @@ def GetSystemHealthBenchmarksToSmokeTest():
 
 
 _DISABLED_TESTS = frozenset({
+  # crbug.com/702455
+  'benchmarks.system_health_smoke_test.SystemHealthBenchmarkSmokeTest.system_health.memory_desktop.browse:media:youtube',  # pylint: disable=line-too-long
   # crbug.com/637230
   'benchmarks.system_health_smoke_test.SystemHealthBenchmarkSmokeTest.system_health.memory_desktop.browse:news:cnn',  # pylint: disable=line-too-long
   # crbug.com/666293
