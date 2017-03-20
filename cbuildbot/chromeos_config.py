198399ff21082061fc1e17ae486e5b8477ed8486
Yunlian Jiang <yunlian@google.com>
2017-03-20 10:23:44 -0700

    toolchain_builder: remove test power_DarkResumeDisplay
    The power_DarkResumeDisplay is flaky,
    https://wmatrix.googleplex.com/unfiltered?hide_missing=True&releases=tot&tests=power_DarkResumeDisplay
    So we need to remove this test from our builder.
    BUG=None
    TEST=cbuildbot amd64-gcc-toolchain, the test is gone.
    
    Change-Id: Ib03e973409f75e15e4808e78b00d09e9f248079a
    Reviewed-on: https://chromium-review.googlesource.com/457221
    Commit-Ready: Yunlian Jiang <yunlian@chromium.org>
    Tested-by: Yunlian Jiang <yunlian@chromium.org>
    Reviewed-by: Caroline Tice <cmtice@chromium.org>

--- cbuildbot/chromeos_config.py
+++ cbuildbot/chromeos_config.py
@@ -1651,6 +1651,7 @@ def ToolchainBuilders(site_config, boards_dict, ge_build_config):
       # power_DarkResumeShutdownServer after issue crosbug/689598 is fixed.
       # According to crosbug/653496 security_OpenFDs will not work for
       # non-official builds, so we need to leave it permanently disabled.
+      # Need to reenable power_DarkResumeDisplay after crosbug/703250 is fixed.
       # Need to reenable cheets_SELinuxTest after crosbug/693308 is fixed.
       # Need to reenable security_SMMLocked when crosbug/654610 is fixed.
       useflags=append_useflags(['-cros-debug',
@@ -1659,6 +1660,7 @@ def ToolchainBuilders(site_config, boards_dict, ge_build_config):
                                 '-tests_network_VPNConnect.l2tpipsec_xauth',
                                 '-tests_network_VPNConnect.l2tpipsec_psk',
                                 '-tests_power_DarkResumeShutdownServer',
+                                '-tests_power_DarkResumeDisplay',
                                 '-tests_security_SMMLocked',
                                 '-tests_cheets_SELinuxTest']),
       afdo_use=True,
