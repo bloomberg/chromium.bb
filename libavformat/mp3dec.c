f058fa86bb0e7e43d4208eb05a71c8ce46487611
Thomas Guilbert <tguilbert@chromium.org>
2017-03-20 11:15:26 -0700

    Skip ID3v1 tags
    Recent changes to mp3dec.c caused the custom flag we added to the
    AVFormatContext to be overwritten.
    
    This CL adds an explicit check for our custom "skip_id3v1_tags"
    metadata value in mp3_read_header, before it is overwritten.
    
    BUG=
    TEST= ran media_unittests before/after
    
    Change-Id: Ia83e9ffcdff0775bacfa40c2eab633539cd9906c
    Reviewed-on: https://chromium-review.googlesource.com/457249
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>

--- libavformat/mp3dec.c
+++ libavformat/mp3dec.c
@@ -353,6 +353,10 @@ static int mp3_read_header(AVFormatContext *s)
     int ret;
     int i;
 
+    // Chromium: Disable ID3v1 tag reading to avoid costly seeks to end of file
+    // for data we don't use.
+    int skip_id3v1 = !!av_dict_get(s->metadata, "skip_id3v1_tags", NULL,
+                                   AV_DICT_IGNORE_SUFFIX);
     s->metadata = s->internal->id3v2_meta;
     s->internal->id3v2_meta = NULL;
 
@@ -371,7 +375,7 @@ static int mp3_read_header(AVFormatContext *s)
     s->pb->maxsize = -1;
     off = avio_tell(s->pb);
 
-    if (!av_dict_get(s->metadata, "", NULL, AV_DICT_IGNORE_SUFFIX))
+    if (!skip_id3v1 && !av_dict_get(s->metadata, "", NULL, AV_DICT_IGNORE_SUFFIX))
         ff_id3v1_read(s);
 
     if(s->pb->seekable)
