742adbd76f8d4c730866ba02d2e4b71524ce9049
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2017-03-20 20:46:18 +0100

    Run new recipes 'test' command
    BUG=chromium:699120
    
    Change-Id: Ie498bec2e2e38cbde8fac6409b13d2032b2e1a1b
    Reviewed-on: https://chromium-review.googlesource.com/457318
    Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    Commit-Queue: Paweł Hajdan Jr. <phajdan.jr@chromium.org>

--- tests/recipes_test.py
+++ tests/recipes_test.py
@@ -15,5 +15,9 @@ def recipes_py(*args):
   subprocess.check_call([
       os.path.join(ROOT_DIR, 'recipes.py'), '--use-bootstrap'] + list(args))
 
+# Run both current simulation test logic (simulation_test), and experimental
+# (test). Eventually the former will be removed.
 recipes_py('simulation_test')
+recipes_py('test', 'run')
+
 recipes_py('lint')
