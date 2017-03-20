43e8b83485fa3a5cbd5b76d6afb50e64e7cba235
aicommander <aicommander@gmail.com>
2017-03-20 15:27:57 -0700

    Add new Xbox 360 wireless gamepad product ID for Linux
    The Linux xpad driver was updated to report the same product ID for
    wireless Xbox 360 gamepads as Windows does. This commit was queued for
    stable too, so it will show up in LTS kernel updates too.
    
    Xpad change: https://git.kernel.org/cgit/linux/kernel/git/torvalds/linux.git/commit/drivers/input/joystick/xpad.c?id=b6fc513da50c5dbc457a8ad6b58b046a6a68fd9d
    
    BUG=700632
    TEST=http://html5gamepad.com/
    
    Review-Url: https://codereview.chromium.org/2743153002
    Cr-Commit-Position: refs/heads/master@{#458204}
    
    Former-commit-id: 8bf2c398ceb1cc65bbe89ccb359c38662fd22a38

--- device/gamepad/gamepad_standard_mappings_linux.cc
+++ device/gamepad/gamepad_standard_mappings_linux.cc
@@ -358,6 +358,7 @@ struct MappingData {
     {"0079", "0006", MapperDragonRiseGeneric},   // DragonRise Generic USB
     {"045e", "028e", MapperXInputStyleGamepad},  // Xbox 360 Wired
     {"045e", "028f", MapperXInputStyleGamepad},  // Xbox 360 Wireless
+    {"045e", "02a1", MapperXInputStyleGamepad},  // Xbox 360 Wireless
     {"045e", "02d1", MapperXInputStyleGamepad},  // Xbox One Wired
     {"045e", "02dd", MapperXInputStyleGamepad},  // Xbox One Wired (2015 FW)
     {"045e", "02e3", MapperXInputStyleGamepad},  // Xbox One Elite Wired
