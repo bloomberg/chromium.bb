5a418303e33eb7caa0c98e2547081094db31ab9a
Tom Finegan <tomfinegan@google.com>
2017-03-21 10:44:09 -0700

    mkvparser: Avoid double free when Chromaticity parse fails.
    PrimaryChromaticity::Parse never owns the PrimaryChromaticity
    it allocates-- avoid freeing it because doing so results in a
    double free when the MasteringMetadata dtor runs.
    
    BUG=webm:1381
    
    Change-Id: Ief0159f6ab667234e3fdc51c65f23ef5efb32a71

--- mkvparser/mkvparser.cc
+++ mkvparser/mkvparser.cc
@@ -4979,29 +4979,27 @@ bool PrimaryChromaticity::Parse(IMkvReader* reader, long long read_pos,
   if (!reader)
     return false;
 
-  std::auto_ptr<PrimaryChromaticity> chromaticity_ptr;
+  if (!*chromaticity)
+    *chromaticity = new PrimaryChromaticity();
 
-  if (!*chromaticity) {
-    chromaticity_ptr.reset(new PrimaryChromaticity());
-  } else {
-    chromaticity_ptr.reset(*chromaticity);
-  }
-
-  if (!chromaticity_ptr.get())
+  if (!*chromaticity)
     return false;
 
-  float* value = is_x ? &chromaticity_ptr->x : &chromaticity_ptr->y;
+  PrimaryChromaticity* pc = *chromaticity;
+  float* value = is_x ? &pc->x : &pc->y;
 
   double parser_value = 0;
-  const long long value_parse_status =
+  const long long parse_status =
       UnserializeFloat(reader, read_pos, value_size, parser_value);
+
+  if (parse_status < 0 || parser_value < FLT_MIN || parser_value > FLT_MAX)
+    return false;
 
   *value = static_cast<float>(parser_value);
 
-  if (value_parse_status < 0 || *value < 0.0 || *value > 1.0)
+  if (*value < 0.0 || *value > 1.0)
     return false;
 
-  *chromaticity = chromaticity_ptr.release();
   return true;
 }
 
