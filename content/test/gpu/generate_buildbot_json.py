9f823aec720993ad96e8870a36439ec2bf44051b
ynovikov <ynovikov@chromium.org>
2017-03-22 12:11:24 -0700

    Remove NVIDIA Shield GPU.FYI bot
    BUG=704187
    TBR=kbr@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2766323002
    Cr-Commit-Position: refs/heads/master@{#458827}
    
    Former-commit-id: 2d30e002c7613a18fed1c12da55a4a5527dd36aa

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -717,21 +717,6 @@ FYI_WATERFALL = {
       'swarming': False,
       'os_type': 'android',
     },
-    'Android Release (NVIDIA Shield)': {
-      'swarming_dimensions': [
-        {
-          # There are no PCI IDs on Android.
-          # This is a hack to get the script working.
-          'gpu': '0000:0000',
-          'os': 'Android'
-        },
-      ],
-      'build_config': 'android-chromium',
-      # This bot is a one-off and doesn't have similar slaves in the
-      # swarming pool.
-      'swarming': False,
-      'os_type': 'android',
-    },
 
     # The following "optional" testers don't actually exist on the
     # waterfall. They are present here merely to specify additional
