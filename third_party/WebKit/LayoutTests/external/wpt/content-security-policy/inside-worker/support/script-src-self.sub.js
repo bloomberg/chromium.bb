4b9462acc4ef8c3ac889077a0cd3029f8c52aef7
Blink W3C Test Autoroller <blink-w3c-test-autoroller@chromium.org>
2017-03-22 21:24:38 -0700

    Import wpt@bb66394e1ac8612e6d52b07223baae1c4f1520fe
    Using wpt-import in Chromium 5593d37e3b97a4f421c9491384cf16590ff32a81 [formerly dc118ac412a555cd36fc6f5fde5c37aaf261057b].
    
    Build: https://build.chromium.org/p/chromium.infra.cron/builders/w3c-test-autoroller/builds/8049
    
    Directory owners for changes in this CL:
    dom-dev@chromium.org:
      external/wpt/html
      external/wpt/dom
    hta@chromium.org:
      external/wpt/webrtc
    jsbell@chromium.org:
      external/wpt/IndexedDB
      external/wpt/encoding
      external/wpt/common
    mkwst@chromium.org:
      external/wpt/content-security-policy
    sunjian@chromium.org, panicker@chromium.org:
      external/wpt/navigation-timing
    suzyh@chromium.org:
      external/wpt/web-animations
    timvolodine@chromium.org, reillyg@chromium.org:
      external/wpt/orientation-event
    
    TBR=qyearsley@chromium.org
    NOEXPORT=true
    
    Change-Id: I0d254f140a3f68cd64efb3e0972f08a4521b2a53
    Reviewed-on: https://chromium-review.googlesource.com/458082
    Reviewed-by: Blink W3C Test Autoroller <blink-w3c-test-autoroller@chromium.org>
    Commit-Queue: Blink W3C Test Autoroller <blink-w3c-test-autoroller@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#459007}
    Former-commit-id: afd35b8d536a34cc4d0f23d3f84ada63906cd3c7

--- third_party/WebKit/LayoutTests/external/wpt/content-security-policy/inside-worker/support/script-src-self.sub.js
+++ third_party/WebKit/LayoutTests/external/wpt/content-security-policy/inside-worker/support/script-src-self.sub.js
@@ -2,14 +2,14 @@ importScripts("{{location[server]}}/resources/testharness.js");
 importScripts("{{location[server]}}/content-security-policy/support/testharness-helper.js");
 
 test(t => {
+  self.a = false;
   assert_throws("NetworkError",
-                _ => importScripts("http://{{domains[www]}}:{{ports[http][1]}}/content-security-policy/support/fail.js"),
+                _ => importScripts("http://{{domains[www]}}:{{ports[http][1]}}/content-security-policy/support/var-a.js"),
                 "importScripts should throw `NetworkError`");
+  assert_false(self.a);
 }, "Cross-origin `importScripts()` blocked in " + self.location.protocol + self.location.search);
 
 test(t => {
-  // TODO(mkwst): The error event isn't firing. :/
-
   assert_throws(EvalError(),
                 _ => eval("1 + 1"),
                 "`eval()` should throw 'EvalError'.");
@@ -21,11 +21,11 @@ test(t => {
 
 async_test(t => {
   waitUntilCSPEventForEval(t, 27)
-    .then(_ => t.done());
+    .then(t.step_func_done());
 
-  assert_equals(
-      setTimeout("assert_unreached('setTimeout([string]) should not execute.')", 0),
-      0);
+  self.setTimeoutTest = t;
+  var result = setTimeout("(self.setTimeoutTest.unreached_func('setTimeout([string]) should not execute.'))()", 1);
+  assert_equals(result, 0);
 }, "`setTimeout([string])` blocked in " + self.location.protocol + self.location.search);
 
 done();
