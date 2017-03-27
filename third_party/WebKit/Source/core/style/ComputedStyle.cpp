bloomberg/chromium.bb

commit 34e5163aff2a711580a4e435675b536af61b44a5

Support subpixel layout of borders.
This cl implements subpixel layout of borders.  Painting of subpixel
borders now uses rounding to nearest CSS pixel, and is aligned with
Edge.  Handling of device pixel ratios > 1 isn't implemented here,
and is better covered in a separate task.

This does not implement subpixel borders for tables, as that interacts
with table layout width calculations, which would also need to be
adapted to subpixels.  Again, that's better dealt with in a separate
task.

A number of test cases had to be rebaselined, due to minor changes in
layout and/or painting.  Most of these changes appear in tests that uses
zooming, and implicitly have fractional borders.

svg/zoom/page/zoom-replaced-intrinsic-ratio-001.htm now looks a bit
broken, but that's due to an unrelated issue with aspect ratio and
subpixels (in LayoutPart, if I recall correctly).

BUG=227192
CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2

Review-Url: https://codereview.chromium.org/2640143005
Cr-Commit-Position: refs/heads/master@{#449943}

Former-commit-id: 934becac5daa91ea979fb66e4ae21761ca11ebc9

third_party/WebKit/Source/core/style/ComputedStyle.cpp
@@ -63,7 +63,7 @@ namespace blink {
 
 struct SameSizeAsBorderValue {
   RGBA32 m_color;
-  unsigned m_width;
+  unsigned m_bitfield;
 };
 
 ASSERT_SIZE(BorderValue, SameSizeAsBorderValue);
@@ -1493,12 +1493,13 @@ FloatRoundedRect ComputedStyle::getRoundedInnerBorderFor(
   bool horizontal = isHorizontalWritingMode();
 
   int leftWidth =
-      (!horizontal || includeLogicalLeftEdge) ? borderLeftWidth() : 0;
+      (!horizontal || includeLogicalLeftEdge) ? roundf(borderLeftWidth()) : 0;
   int rightWidth =
-      (!horizontal || includeLogicalRightEdge) ? borderRightWidth() : 0;
-  int topWidth = (horizontal || includeLogicalLeftEdge) ? borderTopWidth() : 0;
+      (!horizontal || includeLogicalRightEdge) ? roundf(borderRightWidth()) : 0;
+  int topWidth =
+      (horizontal || includeLogicalLeftEdge) ? roundf(borderTopWidth()) : 0;
   int bottomWidth =
-      (horizontal || includeLogicalRightEdge) ? borderBottomWidth() : 0;
+      (horizontal || includeLogicalRightEdge) ? roundf(borderBottomWidth()) : 0;
 
   return getRoundedInnerBorderFor(
       borderRect,
@@ -2215,7 +2216,7 @@ const BorderValue& ComputedStyle::borderEnd() const {
   return isLeftToRightDirection() ? borderBottom() : borderTop();
 }
 
-int ComputedStyle::borderBeforeWidth() const {
+float ComputedStyle::borderBeforeWidth() const {
   switch (getWritingMode()) {
     case WritingMode::kHorizontalTb:
       return borderTopWidth();
@@ -2228,7 +2229,7 @@ int ComputedStyle::borderBeforeWidth() const {
   return borderTopWidth();
 }
 
-int ComputedStyle::borderAfterWidth() const {
+float ComputedStyle::borderAfterWidth() const {
   switch (getWritingMode()) {
     case WritingMode::kHorizontalTb:
       return borderBottomWidth();
@@ -2241,23 +2242,23 @@ int ComputedStyle::borderAfterWidth() const {
   return borderBottomWidth();
 }
 
-int ComputedStyle::borderStartWidth() const {
+float ComputedStyle::borderStartWidth() const {
   if (isHorizontalWritingMode())
     return isLeftToRightDirection() ? borderLeftWidth() : borderRightWidth();
   return isLeftToRightDirection() ? borderTopWidth() : borderBottomWidth();
 }
 
-int ComputedStyle::borderEndWidth() const {
+float ComputedStyle::borderEndWidth() const {
   if (isHorizontalWritingMode())
     return isLeftToRightDirection() ? borderRightWidth() : borderLeftWidth();
   return isLeftToRightDirection() ? borderBottomWidth() : borderTopWidth();
 }
 
-int ComputedStyle::borderOverWidth() const {
+float ComputedStyle::borderOverWidth() const {
   return isHorizontalWritingMode() ? borderTopWidth() : borderRightWidth();
 }
 
-int ComputedStyle::borderUnderWidth() const {
+float ComputedStyle::borderUnderWidth() const {
   return isHorizontalWritingMode() ? borderBottomWidth() : borderLeftWidth();
 }
 

commit 19351bb30c9a9b1e9d9b834c6807eb2f9570aa4f

Fix for performance regression on high dpi devices.
ComputedStyle::getRoundedInnerBorderFor() could generate negative
content boxes for boxes with no content and sub pixel borders that
round up (eg. 1.5px-1.99px).  Negative content boxes are illegal per
spec, and causes BoxBorderPainter::paintBorderFastPath() to bail,
and performance to be substantially degraded.

This issue would be evident on the paint-offset-changes perftest when
device pixel ratio was set to eg. 1.5.  The issue was introduced in
34e5163aff2a711580a4e435675b536af61b44a5 [formerly 934becac5daa91ea979fb66e4ae21761ca11ebc9]

BUG=692955

Review-Url: https://codereview.chromium.org/2771093003
Cr-Commit-Position: refs/heads/master@{#459770}

Former-commit-id: dd547f46cfb46ea92f554aa842d06bde19ceef6e

third_party/WebKit/Source/core/style/ComputedStyle.cpp
@@ -1505,6 +1505,7 @@ FloatRoundedRect ComputedStyle::getRoundedInnerBorderFor(
     bool includeLogicalRightEdge) const {
   LayoutRect innerRect(borderRect);
   innerRect.expand(insets);
+  innerRect.size().clampNegativeToZero();
 
   FloatRoundedRect roundedRect(pixelSnappedIntRect(innerRect));
 