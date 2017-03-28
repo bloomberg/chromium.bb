3b24bb4460bba741ca4c1f47badf56c189dd173c
dpapad <dpapad@chromium.org>
2017-03-28 13:47:50 -0700

    MD Settings: Fix case where 'REMOVE ALL SHOWN' button is erroneously shown.
    BUG=705441
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2781703002
    Cr-Commit-Position: refs/heads/master@{#460214}
    
    Former-commit-id: ff5026c59eda509f6f73caee29a4b36042d2e4f9

--- chrome/test/data/webui/settings/site_data_test.js
+++ chrome/test/data/webui/settings/site_data_test.js
@@ -26,4 +26,32 @@ suite('<site-data>', function() {
       assertEquals(GOOGLE_ID, path);
     });
   });
+
+  test('remove button hidden when no search results', function() {
+    siteData.sites = [
+      {site: 'Hello', id: '1', localData: 'Cookiez!'},
+      {site: 'World', id: '2', localData: 'Cookiez!'},
+    ];
+
+    Polymer.dom.flush();
+    assertEquals(
+        siteData.sites.length,
+        siteData.shadowRoot.querySelectorAll('.list-item').length);
+
+    // Expecting one result, so the button should be shown.
+    siteData.$.filter.dispatchEvent(
+        new CustomEvent('search-changed', {detail: 'Hello'}));
+    Polymer.dom.flush();
+    assertEquals(
+        1, siteData.shadowRoot.querySelectorAll('.list-item').length);
+    assertFalse(siteData.$.removeShowingSites.hidden);
+
+    // Expecting no results, so the button should be hidden.
+    siteData.$.filter.dispatchEvent(
+        new CustomEvent('search-changed', {detail: 'foo'}));
+    Polymer.dom.flush();
+    assertEquals(
+        0, siteData.shadowRoot.querySelectorAll('.list-item').length);
+    assertTrue(siteData.$.removeShowingSites.hidden);
+  });
 });
