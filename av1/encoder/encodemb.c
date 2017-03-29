7faea436534f47ebbd30d265b8e54f5f03d0ccab
Luc Trudeau <ltrudeau@mozilla.com>
2017-03-29 21:03:00 +0000

    Revert "[PVQ] Don't transform if block skipped"
    This reverts commit c538d6815eb22f851880400ddd8bcc72e53d8d0a.
    
    Reason for revert: should be av1_fwd_txfm(), instead of fwd_txfm()
    
    Change-Id: I58657e54eb0a1c20c930b32cd53b6d05493eb8f4

--- av1/encoder/encodemb.c
+++ av1/encoder/encodemb.c
@@ -673,10 +673,6 @@ void av1_xform_quant(const AV1_COMMON *cm, MACROBLOCK *x, int plane, int block,
 
   // PVQ for inter mode block
   if (!x->skip_block) {
-    (void)xform_quant_idx;
-    fwd_txfm(src_int16, coeff, diff_stride, &fwd_txfm_param);
-    fwd_txfm(pred, ref_coeff, diff_stride, &fwd_txfm_param);
-
     PVQ_SKIP_TYPE ac_dc_coded =
         av1_pvq_encode_helper(x,
                               coeff,        // target original vector
