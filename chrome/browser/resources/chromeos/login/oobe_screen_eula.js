ce37f455603ec993d9c7fd782e9c2473347c99b8
glevin <glevin@chromium.org>
2017-03-30 14:54:27 -0700

    Remove broken focusout listener for System Security Setting OOBE dialog
    BUG=690726
    TEST=In new MD OOBE, advance to Chrome OS Terms page, click "System
    security setting".  Turn on ChromeVox, check that CV nav keys can
    highlight and read dialog contents.
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2787033002
    Cr-Commit-Position: refs/heads/master@{#460899}
    
    Former-commit-id: f9baf94fc70de8725932d3421fafa85db3d3cfbf

--- chrome/browser/resources/chromeos/login/oobe_screen_eula.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_eula.js
@@ -27,15 +27,6 @@ login.createScreen('EulaScreen', 'eula', function() {
           'click', function(event) {
             $('popup-overlay').hidden = true;
           });
-      // Do not allow focus leaving the overlay.
-      $('popup-overlay').addEventListener('focusout', function(event) {
-        // WebKit does not allow immediate focus return.
-        window.setTimeout(function() {
-          // TODO(ivankr): focus cycling.
-          $('installation-settings-ok-button').focus();
-        }, 0);
-        event.preventDefault();
-      });
 
       var self = this;
       $('usage-stats').addEventListener('click', function(event) {
