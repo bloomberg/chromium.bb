3f8bddab0b28d3acfd05bf73c5a1f7306704af2e
arthursonzogni <arthursonzogni@chromium.org>
2017-03-31 00:58:15 -0700

    Fix flaky TEST PaymentAppBrowserTest.PaymentAppInvocation.
    The test PaymentAppBrowserTest.PaymentAppInvocation was flaky. I was able to
    make it fails several times. On the other side, when --browser-side-navigation
    is enabled, this test fails very often.
    
    This is due to a race condition:
    
    payment_app.js                 ¦  payment_app_window.html
    -------------------------------¦----------------------------------------
    client.openWindow              ¦
            |      |_______________¦_______
            |                      ¦       |
     (promise onFullfiled called)  ¦       |
            |                      ¦       |
           ...                     ¦       |
            |                      ¦       |
            v                      ¦       |
      AddEventListener(...)        ¦ postMessage('payment_app_window_ready')
    
    * postMessage('payment_app_window_ready') could be sended/received before
      the event listener is registered on the service worker. In this case
      the 'message' event was not handled. It causes the test to fails.
    
    * A little bit surprising, it could also be send before the promise is
      resolved. That's why I had to use the maybeSendPaymentRequest().
    
    BUG=703602
    
    Review-Url: https://codereview.chromium.org/2785093003
    Cr-Commit-Position: refs/heads/master@{#461074}
    
    Former-commit-id: 007c7242b1ac8aad353e2eac5279071ac59936c0

--- content/test/data/payments/payment_app.js
+++ content/test/data/payments/payment_app.js
@@ -2,44 +2,38 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function sendResultToTest(data) {
-  clients.matchAll({includeUncontrolled: true}).then(clients => {
-    clients.forEach(client => {
-      if (client.url.indexOf('payment_app_invocation.html') != -1) {
-        client.postMessage(data);
-      }
-    });
-  });
-}
-
-function getMessageFromPaymentAppWindow() {
-  return new Promise((resolve, reject) => {
-    var listener = self.addEventListener('message', e => {
-      resolve(e.data);
-      self.removeEventListener(listener);
-    });
-  });
-}
-
 self.addEventListener('paymentrequest', e => {
-  var payment_app_window;
-
   // SW -------------------- openWindow() ------------------> payment_app_window
   // SW <----- postMessage('payment_app_window_ready') ------ payment_app_window
   // SW -------- postMessage('payment_app_request') --------> payment_app_window
   // SW <-- postMessage({methodName: 'test', details: {}}) -- payment_app_window
   e.respondWith(new Promise(resolve => {
+    let payment_app_window = undefined;
+    let window_ready = false;
+
+    let maybeSendPaymentRequest = function() {
+      if (payment_app_window && window_ready)
+        payment_app_window.postMessage('payment_app_request');
+    };
+
+    self.addEventListener('message', e => {
+      if (e.data == "payment_app_window_ready") {
+        window_ready = true;
+        maybeSendPaymentRequest();
+        return;
+      }
+
+      if (e.data.methodName) {
+        resolve(e.data);
+        return;
+      }
+    });
+
+    // Open a window
     clients.openWindow('payment_app_window.html')
-        .then(window_client => {
-          payment_app_window = window_client;
-          return getMessageFromPaymentAppWindow();
-        })
-        .then(message => {
-          payment_app_window.postMessage('payment_app_request');
-          return getMessageFromPaymentAppWindow();
-        })
-        .then(message => {
-          resolve(message);
-        });
+      .then(window_client => {
+        payment_app_window = window_client;
+        maybeSendPaymentRequest();
+      })
   }));
 });
