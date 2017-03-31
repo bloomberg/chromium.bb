122e6692b42d627466c590628436019fa46981e2
yutak <yutak@chromium.org>
2017-03-31 00:55:13 -0700

    Reland: Move wtf_unittests to platform/wtf/.
    This relands the exact same patch as the previous commit
    https://codereview.chromium.org/2771783003 did. This patch was reverted
    because the offending CL was dependent on it.
    
    TBRing since the exact same change was approved before.
    
    > This patch moves all the cpp files for wtf_unittests to their new home
    > platform/wtf. The target "wtf_unittests" is removed from wtf/BUILD.gn, and
    > it's added in platform/wtf/BUILD.gn. Due to this change, a few other files
    > need to be updated, too.
    
    BUG=691465
    TBR=haraken@chromium.org,thakis@chromium.org
    
    Review-Url: https://codereview.chromium.org/2787293002
    Cr-Commit-Position: refs/heads/master@{#461072}
    
    Former-commit-id: 6fbccafc5d50e35d8f8360559829a8b5ece100dc

---
+++ third_party/WebKit/Source/platform/wtf/text/IntegerToStringConversionTest.cpp
@@ -0,0 +1,18 @@
+// Copyright 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/wtf/text/IntegerToStringConversion.h"
+
+#include "platform/wtf/text/StringView.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace WTF {
+
+TEST(IntegerToStringConversionTest, SimpleIntConversion) {
+  const IntegerToStringConverter<int> conv(100500);
+  EXPECT_EQ(StringView(conv.characters8(), conv.length()),
+            StringView("100500"));
+}
+
+}  // namespace WTF
