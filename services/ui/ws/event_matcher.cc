d15242d9e8a6b2c5c485f0dacb3b1e0ac86c3a6c
penghuang <penghuang@chromium.org>
2017-03-31 08:03:32 -0700

    services/ui: make autorepeat work with accelerator.
    BUG=706571
    
    Review-Url: https://codereview.chromium.org/2788753002
    Cr-Commit-Position: refs/heads/master@{#461122}
    
    Former-commit-id: 16dd65423c4063f423238456001c672464474a99

--- services/ui/ws/event_matcher.cc
+++ services/ui/ws/event_matcher.cc
@@ -76,7 +76,9 @@ EventMatcher::~EventMatcher() {}
 bool EventMatcher::MatchesEvent(const ui::Event& event) const {
   if ((fields_to_match_ & TYPE) && event.type() != event_type_)
     return false;
-  int flags = event.flags() & ~ignore_event_flags_;
+  // Synthetic flags should never be matched against.
+  constexpr int kSyntheticFlags = EF_IS_SYNTHESIZED | EF_IS_REPEAT;
+  int flags = event.flags() & ~(ignore_event_flags_ | kSyntheticFlags);
   if ((fields_to_match_ & FLAGS) && flags != event_flags_)
     return false;
   if (fields_to_match_ & KEYBOARD_CODE) {
