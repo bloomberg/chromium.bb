b7bb75ebfdba8d38a8bd82d0c7d6da1b87e6015d
dcastagna <dcastagna@chromium.org>
2017-04-03 09:39:41 -0700

    drm: Don't fill modeset buffer if the src might have modifiers.
    When modesetting, we try to reuse previous content associated with the
    CRTC to fill the modeset buffer.
    
    The content is read back from the previously set framebufferid and the
    assumption is that the format would be linear RGBX.
    
    Lately we added AFBC, that means we can't always read the content back.
    
    This CL avoids reading back the content when the CRTC supports any modifier.
    
    BUG=b/35774940,b/35647845
    
    Review-Url: https://codereview.chromium.org/2771413007
    Cr-Commit-Position: refs/heads/master@{#461451}
    
    Former-commit-id: 9b767bfca91f4fe675e6a5ece749e416201142f6

--- ui/ozone/platform/drm/gpu/screen_manager.cc
+++ ui/ozone/platform/drm/gpu/screen_manager.cc
@@ -45,6 +45,19 @@ void FillModesetBuffer(const scoped_refptr<DrmDevice>& drm,
     return;
   }
 
+  uint32_t fourcc_format = buffer->GetFramebufferPixelFormat();
+  const auto& modifiers = controller->GetFormatModifiers(fourcc_format);
+  for (const uint64_t modifier : modifiers) {
+    // A value of 0 means DRM_FORMAT_MOD_NONE. If the CRTC has any other
+    // modifier (tiling, compression, etc.) we can't read the fb and assume it's
+    // a linear buffer.
+    if (modifier) {
+      VLOG(2) << "Crtc has a modifier and we might not know how to interpret "
+                 "the fb.";
+      return;
+    }
+  }
+
   // If the display controller is in mirror mode, the CRTCs should be sharing
   // the same framebuffer.
   DrmConsoleBuffer saved_buffer(drm, saved_crtc->buffer_id);
