db3c6e00966bcebd3be62c74085e6db9b91d1f79
riesa <riesa@chromium.org>
2017-04-03 15:34:47 -0700

    Sets is_reliable for CLD3 if below a minimum byte threshold of 50 bytes.
    BUG=706606
    
    Review-Url: https://codereview.chromium.org/2780323002
    Cr-Commit-Position: refs/heads/master@{#461560}
    
    Former-commit-id: 88953d9c7663f797a6c92b8b764812d0e84ef8d9

--- extensions/renderer/i18n_custom_bindings.cc
+++ extensions/renderer/i18n_custom_bindings.cc
@@ -41,6 +41,10 @@ namespace {
 // Max number of languages to detect.
 const int kCldNumLangs = 3;
 
+// CLD3 minimum reliable byte threshold. Predictions for inputs below this size
+// in bytes will be considered unreliable.
+const int kCld3MinimumByteThreshold = 50;
+
 struct DetectedLanguage {
   DetectedLanguage(const std::string& language, int percentage)
       : language(language), percentage(percentage) {}
@@ -310,8 +314,17 @@ void I18NCustomBindings::DetectTextLanguage(
 #elif BUILDFLAG(CLD_VERSION) == 3
   chrome_lang_id::NNetLanguageIdentifier nnet_lang_id(/*min_num_bytes=*/0,
                                                       /*max_num_bytes=*/512);
-  const std::vector<chrome_lang_id::NNetLanguageIdentifier::Result>
-      lang_results = nnet_lang_id.FindTopNMostFreqLangs(text, kCldNumLangs);
+  std::vector<chrome_lang_id::NNetLanguageIdentifier::Result> lang_results =
+      nnet_lang_id.FindTopNMostFreqLangs(text, kCldNumLangs);
+
+  // is_reliable is set to false if we believe the input is too short to be
+  // accurately identified by the current model.
+  if (text.size() < kCld3MinimumByteThreshold) {
+    for (auto& result : lang_results) {
+      result.is_reliable = false;
+    }
+  }
+
   LanguageDetectionResult result;
 
   // Populate LanguageDetectionResult with prediction reliability, languages,
