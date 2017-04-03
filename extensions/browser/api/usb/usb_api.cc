75028e9208f5dc1f940ac7ecab09a242d09bd4c5
jdoerrie <jdoerrie@chromium.org>
2017-04-03 12:43:06 -0700

    Avoid extra buffer copy in |OnCompleted|
    As pointed out in http://crbug.com/646113#c11 http://crrev.com/2689673002
    introduced an extra buffer copy in |OnCompleted| that is not necessary. This
    change addresses this issue by turning the copy into a move.
    
    R=reillyg@chromium.org
    BUG=646113
    
    Review-Url: https://codereview.chromium.org/2788863002
    Cr-Commit-Position: refs/heads/master@{#461505}
    
    Former-commit-id: e75bef3da890d51a40dd4919d5b26baffd29c4a2

--- extensions/browser/api/usb/usb_api.cc
+++ extensions/browser/api/usb/usb_api.cc
@@ -1217,11 +1217,11 @@ void UsbIsochronousTransferFunction::OnCompleted(
       [](const size_t& a, const UsbDeviceHandle::IsochronousPacket& packet) {
         return a + packet.transferred_length;
       });
-  std::unique_ptr<char[]> buffer(new char[length]);
+  std::vector<char> buffer;
+  buffer.reserve(length);
 
   UsbTransferStatus status = device::USB_TRANSFER_COMPLETED;
-  size_t buffer_offset = 0;
-  size_t data_offset = 0;
+  const char* data_ptr = data ? data->data() : nullptr;
   for (const auto& packet : packets) {
     // Capture the error status of the first unsuccessful packet.
     if (status == device::USB_TRANSFER_COMPLETED &&
@@ -1230,18 +1230,16 @@ void UsbIsochronousTransferFunction::OnCompleted(
     }
 
     if (data) {
-      memcpy(&buffer[buffer_offset], data->data() + data_offset,
-             packet.transferred_length);
+      buffer.insert(buffer.end(), data_ptr,
+                    data_ptr + packet.transferred_length);
+      data_ptr += packet.length;
     }
-    buffer_offset += packet.transferred_length;
-    data_offset += packet.length;
   }
 
   std::unique_ptr<base::DictionaryValue> transfer_info(
       new base::DictionaryValue());
   transfer_info->SetInteger(kResultCodeKey, status);
-  transfer_info->Set(kDataKey, new base::BinaryValue(std::vector<char>(
-                                   buffer.get(), buffer.get() + length)));
+  transfer_info->Set(kDataKey, new base::BinaryValue(std::move(buffer)));
   if (status == device::USB_TRANSFER_COMPLETED) {
     Respond(OneArgument(std::move(transfer_info)));
   } else {
