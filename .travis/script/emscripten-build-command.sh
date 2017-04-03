7ddff9e0f0208c487352c7a9c116653efee872a4
Reiner Dolp <reiner-dolp@users.noreply.github.com>
2017-04-03 19:51:29 +0100

    build command as function

--- .travis/script/emscripten-build-command.sh
+++ .travis/script/emscripten-build-command.sh
@@ -1,25 +1,30 @@
-echo $(pwd);
+function buildjs {
 
-if [ $0 != "32" -a $0 != "16" ]; then
-	echo "argument 0 must either be 32 for UTF32 builts or 16 for UTF16 builds"
-	exit 1
-fi
+	if [ $1 != "32" -a $1 != "16" ]; then
+		echo "argument 1 must either be 32 for UTF32 builts or 16 for UTF16 builds"
+		exit 1
+	fi
+	
+	if [ -z $2 ]; then
+		echo "argument 2 must be a valid filename"
+		exit 1
+	fi
+	
+	set -x
 
-if [ -z $1 ]; then
-	echo "argument 1 must be a valid filename"
-	exit 1
-fi
+	emcc ./liblouis/.libs/liblouis.a -s RESERVED_FUNCTION_POINTERS=1\
+	 -s EXPORTED_FUNCTIONS="['_lou_version', '_lou_translateString', '_lou_translate',\
+	'_lou_backTranslateString', '_lou_backTranslate', '_lou_hyphenate',\
+	'_lou_compileString', '_lou_getTypeformForEmphClass', '_lou_dotsToChar',\
+	'_lou_charToDots', '_lou_registerLogCallback', '_lou_setLogLevel',\
+	'_lou_logFile', '_lou_logPrint', '_lou_logEnd', '_lou_setDataPath',\
+	'_lou_getDataPath', '_lou_getTable', '_lou_checkTable',\
+	'_lou_readCharFromFile', '_lou_free', '_lou_charSize']" -s MODULARIZE=1\
+	 -s EXPORT_NAME="'liblouisBuild'" -s EXTRA_EXPORTED_RUNTIME_METHODS="['FS',\
+	'Runtime', 'stringToUTF${1}', 'Pointer_Stringify']" --pre-js ./liblouis-js/inc/pre.js\
+	 --post-js ./liblouis-js/inc/post.js ${3} -o ./out/${2} &&
+	
+	cat ./liblouis-js/inc/append.js >> ./out/${2}
 
-emcc ./liblouis/.libs/liblouis.a -s RESERVED_FUNCTION_POINTERS=1\
- -s EXPORTED_FUNCTIONS="['_lou_version', '_lou_translateString', '_lou_translate',\
-'_lou_backTranslateString', '_lou_backTranslate', '_lou_hyphenate',\
-'_lou_compileString', '_lou_getTypeformForEmphClass', '_lou_dotsToChar',\
-'_lou_charToDots', '_lou_registerLogCallback', '_lou_setLogLevel',\
-'_lou_logFile', '_lou_logPrint', '_lou_logEnd', '_lou_setDataPath',\
-'_lou_getDataPath', '_lou_getTable', '_lou_checkTable',\
-'_lou_readCharFromFile', '_lou_free', '_lou_charSize']" -s MODULARIZE=1\
- -s EXPORT_NAME="'liblouisBuild'" -s EXTRA_EXPORTED_RUNTIME_METHODS="['FS',\
-'Runtime', 'stringToUTF${0}', 'Pointer_Stringify']" --pre-js ./liblouis-js/inc/pre.js\
- --post-js ./liblouis-js/inc/post.js ${2} -o ./out/${1} &&
-
-cat ./liblouis-js/inc/append.js >> ./out/${1}
+	set +x
+}
--- .travis/script/emscripten-build.sh
+++ .travis/script/emscripten-build.sh
@@ -1,3 +1,4 @@
+source ./.travis/script/emscripten-build-command.sh &&
 mkdir out &&
 
 echo "[liblouis-js] starting build process in docker image..." &&
@@ -8,15 +9,15 @@ echo "[liblouis-js] configuring and making UTF-16 builds..." &&
 emconfigure ./configure --disable-shared &&
 emmake make &&
 
-./.travis/script/emscripten-build-command.sh 16 build-no-tables-utf16.js
-./.travis/script/emscripten-build-command.sh 16 build-tables-embeded-utf16.js "--embed-files tables@/"
+buildjs "16" "build-no-tables-utf16.js" &&
+buildjs "16" "build-tables-embeded-utf16.js" "--embed-files tables@/" &&
 
 echo "[liblouis-js] configuring and making UTF-32 builds..." &&
 emconfigure ./configure --enable-ucs4 --disable-shared &&
 emmake make &&
 
 echo "[liblouis-js] building UTF-32 with no tables..." &&
-./.travis/script/emscripten-build-command.sh 32 build-no-tables-utf32.js
-./.travis/script/emscripten-build-command.sh 32 build-tables-embeded-utf32.js "--embed-files tables@/"
+buildjs "32" "build-no-tables-utf32.js" &&
+buildjs "32" "build-tables-embeded-utf32.js" "--embed-files tables@/" &&
 
 echo "[liblouis-js] done building in docker image..."
