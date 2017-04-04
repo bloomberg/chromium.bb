bloomberg/chromium.bb

commit 5fff9d84bf716cf55f6778c000f34692a3ab998a

Several fixes for UEB back-translation.
* Don't backtranslate the dot 4, dot 5 and dot 5 6 indicators as punctuation when not followed by another cell. These indicators prefix various contractions and symbols, but should not have any meaning by themselves. This is needed to improve cell by cell backtranslation of input from a braille keyboard.
* Exclamation mark at the end of a word no longer produces "ff".
* Question mark at the end of a word no longer produces "his".
* Open and close quotes no longer produce "his" and "was".
* Colon at the end of a word no longer produces "cc".
* Semi-colon at the end of a word no longer produces "bb".
* The gg contraction in the middle of a word no longer produces "were".

tables/en-ueb-chardefs.uti
@@ -78,14 +78,18 @@ litdigit 9 24
 
 display ! 2346
 punctuation ! 235
+nofor postpunc ! 235
 
 #TODO:  why?!
 display " 5
-punctuation " 5
+noback punctuation " 5
 #punctuation " 236
-#punctuation " 356
-#prepunc " 236
-#postpunc " 356
+# The "?" symbol is mostly handled below,
+# but the pattern needs to be defined before prepunc and postpunc can be used.
+nofor punctuation ? 236
+nofor punctuation " 356
+nofor prepunc " 236
+nofor postpunc " 356
 match %[^_~]%<* " %[_.$]*%[a#] 236
 match %[a#]%[_.$]* " %>*%[^_~] 356
 
@@ -118,10 +122,11 @@ math / 456-34
 
 display : 156
 punctuation : 25
+postpunc : 25
 #TODO:  this is unnecessarily necessary
 display ; 56
 punctuation ; 23
-punctuation ; 56
+noback punctuation ; 56
 display < 126
 math < 4-126
 display = 123456
@@ -145,7 +150,7 @@ display ^ 457
 sign ^ 4-26
 display _ 456
 sign _ 46-36
-punctuation ` 4
+noback punctuation ` 4
 
 display } 12456
 punctuation } 456-345

tests/Makefile.am
@@ -135,7 +135,9 @@ dist_yaml_TESTS =					\
 	yaml/en-GB-g2_backward.yaml			\
 	yaml/en-GB-g2_harness.yaml			\
 	yaml/en-gb-g1_harness.yaml			\
+	yaml/en-ueb-g1_backward.yaml			\
 	yaml/en-ueb-g1_harness.yaml			\
+	yaml/en-ueb-g2_backward.yaml			\
 	yaml/en-ueb-g2-dictionary_harness.yaml		\
 	yaml/en-ueb-symbols_harness.yaml		\
 	yaml/en-ueb-02-stand_alone.yaml			\

tests/yaml/Makefile.am
@@ -17,7 +17,9 @@ EXTRA_DIST =						\
 	en-ueb-09-typeforms.yaml			\
 	en-ueb-10-07-contractions.yaml			\
 	en-ueb-10-13-contractions.yaml			\
+	en-ueb-g1_backward.yaml				\
 	en-ueb-g1_harness.yaml				\
+	en-ueb-g2_backward.yaml				\
 	en-ueb-g2-dictionary_harness.yaml		\
 	en-ueb-math.yaml				\
 	en-ueb-symbols_harness.yaml			\

tests/yaml/en-ueb-g2_backward.yaml
@@ -1,0 +1,29 @@
+tables: [tables/unicode.dis, tables/en-ueb-g2.ctb]
+flags: {testmode: backward}
+tests:
+  # Patterns used for both punctuation and contractions.
+  - [⠁⠖, a!]
+  - [⠁⠖⠁, affa]
+  - [⠦, his]
+  - [⠁⠦, "a?"]
+  - [⠦⠁, '"a']
+  - [⠴, was]
+  - [⠁⠴, 'a"']
+  - [⠦⠁⠴, '"a"']
+  - [⠶, were]
+  - [⠁⠶⠁, agga]
+  - [⠆, be]
+  - [⠆⠁, bea]
+  - [⠁⠆⠁, abba]
+  - [⠁⠆, a;]
+  - [⠒, ":"]
+  - [⠒⠁, cona]
+  - [⠁⠒, "a:"]
+  - [⠁⠒⠁, acca]
+  # Contractions prefixed by an indicator which means nothing on its own.
+  # These indicators are tested by themselves with no following cell in en-ueb-g1.
+  - [⠘⠺, word]
+  - [⠸⠍, many]
+  - [⠐⠝, name]
+  - [⠰⠃, \\56/b]
+  - [⠁⠰⠝, ation]

tables/en-ueb-g2.ctb
@@ -500,7 +500,7 @@ always enough 26-1256-126
 
 # his
 match %[^_]|%[^_~]%<*[([{] his %[^_]|[;:)}\\]]%>*%[^_~] 236
-nofor always his 236
+nofor lowword his 236
 
 # in
 match %[^_]!%[_1]* in !%[_1]*%[^_] 35
@@ -508,11 +508,11 @@ nofor always in 35
 
 # was
 match %[^_]|%[^_~]%<*[([{] was %[^_]|[;:)}\\]]%>*%[^_~] 356
-nofor always was 356
+nofor lowword was 356
 
 # were
 match %[^_]|%[^_~]%<*[([{] were %[^_]|[;:)}\\]]%>*%[^_~] 2356
-nofor always were 2356
+nofor lowword were 2356
 
 
 #   10.6   Lower Groupsigns
@@ -575,12 +575,12 @@ nofor sufword be 23
 # bb   10.6.5   10.6.6
 match %a bb %a 23
 
-nofor always bb 23
+nofor midword bb 23
 
 # cc   10.6.5   10.6.6
 match %a cc %a 25
 
-nofor always cc 25
+nofor midword cc 25
 
 sufword arccosine 345-14-14-135-234-35-15
 sufword bacchanal 12-1-14-16-1-1345-1-123
@@ -675,7 +675,7 @@ sufword turtleneck 2345-136-1235-2345-123-15-1345-15-14-13
 # ff   10.6.5   10.6.6
 match %a ff %a 235
 
-nofor always ff 235
+nofor midword ff 235
 
 sufword afford 1-124-123456-145
 sufword afforest 1-124-123456-15-34
@@ -686,7 +686,7 @@ sufword stafford 34-1-124-123456-145
 # gg   10.6.5   10.6.6
 match %a gg %a 2356
 
-nofor always gg 2356
+nofor midword gg 2356
 
 # in   10.6.8
 match !%[_1]%1* in - 35

tests/yaml/en-ueb-g1_backward.yaml
@@ -1,0 +1,18 @@
+tables: [tables/unicode.dis, tables/en-ueb-g1.ctb]
+flags: {testmode: backward}
+tests:
+  # Numbers.
+  - [⠼⠁, 1]
+  - [⠼⠁⠁, 11]
+  # Indicators which prefix other cells and don't mean anything by themselves.
+  - [⠈, \\4/]
+  - [⠈⠁, "@"]
+  - [⠘, \\45/]
+  - [⠸, \\456/]
+  - [⠨, \\46/]
+  - [⠐, \\5/]
+  - [⠰, \\56/]
+  - [⠠, ""]
+  - [⠠⠁, A]
+  - [⠠⠠, ""]
+  - [⠠⠠⠁⠁, AA]

commit 8212d1544c2148cf469052c92c351907cff92d93

en-ueb-g2.ctb: Fix back-translation of "?" to "this".
"?" was incorrectly being back-translated to "his". This occurred because the display opcode for "?" was after the punctuation opcode for "?".
Regression introduced in 5fff9d84 (#211).
Fixes #309.

tables/en-ueb-chardefs.uti
@@ -84,6 +84,7 @@ display " 5
 punctuation " 6-2356
 # The "?" symbol is mostly handled below,
 # but the pattern needs to be defined before prepunc and postpunc can be used.
+display ? 1456
 nofor punctuation ? 236
 nofor punctuation " 356
 nofor prepunc " 236
@@ -132,7 +133,6 @@ math = 5-2356
 display > 345
 punctuation > 4-345
 #   requires grade one indicator when by itself
-display ? 1456
 punctuation ? 56-236
 postpunc ? 236
 display @ 47

tests/Makefile.am
@@ -165,6 +165,7 @@ dist_yaml_TESTS =				\
 	yaml/en-ueb-g1_backward.yaml		\
 	yaml/en-ueb-g1_harness.yaml		\
 	yaml/en-ueb-g2_backward.yaml		\
+	yaml/en-ueb-g2_backward_no_dis.yaml	\
 	yaml/en-ueb-g2-dictionary_harness.yaml	\
 	yaml/en-ueb-math.yaml			\
 	yaml/en-ueb-symbols_harness.yaml	\

tests/yaml/Makefile.am
@@ -34,6 +34,7 @@ EXTRA_DIST =					\
 	en-ueb-g1_backward.yaml			\
 	en-ueb-g1_harness.yaml			\
 	en-ueb-g2_backward.yaml			\
+	en-ueb-g2_backward_no_dis.yaml	\
 	en-ueb-g2-dictionary_harness.yaml	\
 	en-ueb-math.yaml			\
 	en-ueb-symbols_harness.yaml		\

tests/yaml/en-ueb-g2_backward.yaml
@@ -27,3 +27,5 @@ tests:
   - [⠐⠝, name]
   - [⠰⠃, \\56/b]
   - [⠁⠰⠝, ation]
+  # #309: Ensure correct back-translation to "this".
+  - [⠹, this]

tests/yaml/en-ueb-g2_backward_no_dis.yaml
@@ -1,0 +1,7 @@
+# Tests for en-ueb-g2.ctb back-translation with no additional display table.
+table: [tables/en-ueb-g2.ctb]
+flags: {testmode: backward}
+tests:
+  # #309: Ensure the display rule is used for "?",
+  # rather than a subsequent rule.
+  - ["?", this]