1e93f61621e584b6a0c84aadf54b0eb30b896563
findit-for-me <findit-for-me@appspot.gserviceaccount.com>
2017-04-04 15:52:36 -0700

    Revert of mac: Periodically shim new malloc zones immediately after startup. (patchset #9 id:160001 of https://codereview.chromium.org/2713553002/ )
    Reason for revert:
    
    Findit identified CL at revision 461836 as the culprit for
    failures in the build cycles as shown on:
    https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzI0NWYxNzU1OWY5NzNiYjVhMjA3YzA2M2Q5ODJjMGJhYjBlYzFjMGIM
    
    Original issue's description:
    > mac: Periodically shim new malloc zones immediately after startup.
    >
    > On macOS, libraries lazily make malloc zones. There are no notifications when
    > this happens. This CL adds polling with an exponential backoff to check for the
    > existence of new malloc zones, stopping after 1 minute.
    >
    > This CL only has an affect when heap profiling is enabled.
    >
    > BUG=693237
    >
    > Review-Url: https://codereview.chromium.org/2713553002
    > Cr-Commit-Position: refs/heads/master@{#461836}
    > Committed: https://chromium.googlesource.com/chromium/src/+/f74dd2f5f75a72d8d19dc25ca0456b6aee3780c9 [formerly 245f17559f973bb5a207c063d982c0bab0ec1c0b]
    
    TBR=primiano@chromium.org,mark@chromium.org,avi@chromium.org,erikchen@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=693237
    
    Review-Url: https://codereview.chromium.org/2802493002
    Cr-Commit-Position: refs/heads/master@{#461877}
    
    Former-commit-id: ecc1d96e6789d172b45dd155ccdb26b631f1ecb9

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -10,7 +10,6 @@
 #include <utility>
 #include <vector>
 
-#include "base/base_switches.h"
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/feature_list.h"
@@ -135,7 +134,6 @@
 #endif
 
 #if defined(OS_MACOSX)
-#include "base/allocator/allocator_interception_mac.h"
 #include "base/memory/memory_pressure_monitor_mac.h"
 #include "content/browser/bootstrap_sandbox_manager_mac.h"
 #include "content/browser/cocoa/system_hotkey_helper_mac.h"
@@ -844,14 +842,6 @@ int BrowserMainLoop::PreCreateThreads() {
 
   if (base::FeatureList::IsEnabled(features::kMemoryCoordinator))
     MemoryCoordinatorImpl::GetInstance()->Start();
-
-#if defined(OS_MACOSX)
-  if (base::CommandLine::InitializedForCurrentProcess() &&
-      base::CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableHeapProfiling)) {
-    base::allocator::PeriodicallyShimNewMallocZones();
-  }
-#endif
 
 #if BUILDFLAG(ENABLE_PLUGINS)
   // Prior to any processing happening on the IO thread, we create the
