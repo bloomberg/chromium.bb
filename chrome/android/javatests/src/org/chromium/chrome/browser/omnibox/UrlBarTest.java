e7794b6981800cf8e92fe765e0e28c756b7fb5c2
maxmorin <maxmorin@chromium.org>
2017-04-05 07:26:21 -0700

    Add RetryOnFailure annotation to omnibox.UrlBarTest#testCutHuge.
    BUG=708501
    TBR=tedchoc@chromium.org
    
    Review-Url: https://codereview.chromium.org/2799653002
    Cr-Commit-Position: refs/heads/master@{#462063}
    
    Former-commit-id: d8b12e10d8ad68dda1a2b19b42e2366541e61f70

--- chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/UrlBarTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/omnibox/UrlBarTest.java
@@ -783,6 +783,7 @@ public class UrlBarTest extends ChromeActivityTestCaseBase<ChromeActivity> {
 
     @SmallTest
     @Feature({"Omnibox"})
+    @RetryOnFailure
     public void testCutHuge() throws InterruptedException {
         startMainActivityWithURL(HUGE_URL);
         OmniboxTestUtils.toggleUrlBarFocus(getUrlBar(), true);
