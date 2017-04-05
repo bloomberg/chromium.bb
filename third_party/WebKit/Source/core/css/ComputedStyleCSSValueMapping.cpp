e70c35f5430c01e5b7ac123efa61e76aba00f96c
bratell <bratell@opera.com>
2017-04-05 16:04:34 -0700

    Removing duplicate functions and symbol names in core/css
    In the unity build experiment I encountered a few colliding duplicate
    symbol names in core/css and this patch removes them by replacing
    duplicated symbols with a single code and by doing some symbol
    renames.
    
    Review-Url: https://codereview.chromium.org/2797973002
    Cr-Commit-Position: refs/heads/master@{#462256}
    
    Former-commit-id: 5fa4cb0d7a1a784fec7991eaeb1a80e43f9156e5

--- third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.cpp
+++ third_party/WebKit/Source/core/css/ComputedStyleCSSValueMapping.cpp
@@ -57,6 +57,7 @@
 #include "core/css/CSSValueList.h"
 #include "core/css/CSSValuePair.h"
 #include "core/css/PropertyRegistry.h"
+#include "core/css/zoomAdjustedPixelValue.h"
 #include "core/layout/LayoutBlock.h"
 #include "core/layout/LayoutBox.h"
 #include "core/layout/LayoutGrid.h"
@@ -76,13 +77,6 @@ using namespace cssvalue;
 
 inline static bool isFlexOrGrid(const ComputedStyle* style) {
   return style && style->isDisplayFlexibleOrGridBox();
-}
-
-inline static CSSPrimitiveValue* zoomAdjustedPixelValue(
-    double value,
-    const ComputedStyle& style) {
-  return CSSPrimitiveValue::create(adjustFloatForAbsoluteZoom(value, style),
-                                   CSSPrimitiveValue::UnitType::Pixels);
 }
 
 inline static CSSValue* zoomAdjustedPixelValueOrAuto(
