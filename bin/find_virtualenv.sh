d78e3d638d6a6807037bb810e5de25b666198c06
Allen Li <ayatane@chromium.org>
2017-04-05 18:01:03 -0700

    Replace non-versioned venv with versioned venv
    BUG=chromium:706660
    TEST=Run sysmon locally and check venv setup works
    
    Change-Id: I045393d1e021361b06588a9c08cd50a59e7afd07
    Reviewed-on: https://chromium-review.googlesource.com/469110
    Commit-Ready: Allen Li <ayatane@chromium.org>
    Tested-by: Allen Li <ayatane@chromium.org>
    Reviewed-by: Aviv Keshet <akeshet@chromium.org>

--- bin/find_virtualenv.sh
+++ bin/find_virtualenv.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# Copyright 2016 The Chromium OS Authors. All rights reserved.
+#!/bin/bash
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
@@ -9,19 +9,20 @@
 # sourcing script should cd into the directory containing this script
 # first.  This script defines functions for performing common tasks:
 #
-# exec_python -- Execute Python module inside of virtualenv
+# exec_python_module -- Execute Python module inside of virtualenv
 set -eu
 
-readonly virtualenv_dir=../../infra_virtualenv
-readonly venv_dir=../venv/.venv
-readonly reqs_file=../venv/requirements.txt
-
-_create_venv() {
-    "$virtualenv_dir/create_venv" "$venv_dir" "$reqs_file" >&2
+realpath() {
+    readlink -f -- "$1"
 }
 
+readonly venv_repo=$(realpath ../../infra_virtualenv)
+readonly create_script=$(realpath "${venv_repo}/bin/create_venv")
+readonly venv_home=$(realpath ../venv)
+readonly reqs_file=$(realpath "${venv_home}/requirements.txt")
+
 exec_python_module() {
-    _create_venv
-    export PYTHONPATH='../venv'
-    exec "$venv_dir/bin/python" -m "$@"
+    venvdir=$("${create_script}" "$reqs_file")
+    export PYTHONPATH=${venv_home}
+    exec "${venvdir}/bin/python" -m "$@"
 }
