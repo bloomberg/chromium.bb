cef2b6bf0d520fd7449ff64ce6ef281d0d452b59
lukasza <lukasza@chromium.org>
2017-04-07 11:35:32 -0700

    Disable flaky ContentHashFetcherTest.MissingVerifiedContents on Linux.
    BUG=702300
    TBR=lazyboy@chromium.org
    
    Review-Url: https://codereview.chromium.org/2805443005
    Cr-Commit-Position: refs/heads/master@{#462936}
    
    Former-commit-id: d821806d4441025512588c514e66756323eb1408

--- extensions/browser/content_hash_fetcher_unittest.cc
+++ extensions/browser/content_hash_fetcher_unittest.cc
@@ -186,9 +186,15 @@ class ContentHashFetcherTest : public ExtensionsTest {
   base::ScopedTempDir temp_dir_;
 };
 
+// Flaky on Linux and ChromeOS. https://crbug.com/702300
+#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+#define MAYBE_MissingVerifiedContents DISABLED_MissingVerifiedContents
+#else
+#define MAYBE_MissingVerifiedContents MissingVerifiedContents
+#endif
 // This tests our ability to successfully fetch, parse, and validate a missing
 // verified_contents.json file for an extension.
-TEST_F(ContentHashFetcherTest, MissingVerifiedContents) {
+TEST_F(ContentHashFetcherTest, MAYBE_MissingVerifiedContents) {
   // We unzip the extension source to a temp directory to simulate it being
   // installed there, because the ContentHashFetcher will create the _metadata/
   // directory within the extension install dir and write the fetched
@@ -227,9 +233,7 @@ TEST_F(ContentHashFetcherTest, MissingVerifiedContents) {
       base::PathExists(file_util::GetVerifiedContentsPath(extension->path())));
 }
 
-// Similar to MissingVerifiedContents, but tests the case where the extension
-// actually has corruption.
-// Flaky on Linux and ChromeOS. crbug.com/
+// Flaky on Linux and ChromeOS. https://crbug.com/702300
 #if defined(OS_LINUX) || defined(OS_CHROMEOS)
 #define MAYBE_MissingVerifiedContentsAndCorrupt \
   DISABLED_MissingVerifiedContentsAndCorrupt
@@ -237,6 +241,8 @@ TEST_F(ContentHashFetcherTest, MissingVerifiedContents) {
 #define MAYBE_MissingVerifiedContentsAndCorrupt \
   MissingVerifiedContentsAndCorrupt
 #endif
+// Similar to MissingVerifiedContents, but tests the case where the extension
+// actually has corruption.
 TEST_F(ContentHashFetcherTest, MAYBE_MissingVerifiedContentsAndCorrupt) {
   base::FilePath test_dir_base =
       GetTestPath(base::FilePath()).AppendASCII("missing_verified_contents");
