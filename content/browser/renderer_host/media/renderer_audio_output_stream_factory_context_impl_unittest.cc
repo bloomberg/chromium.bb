556abcb05e9c041a69e0972a1713daf3191fc8fd
foolip <foolip@chromium.org>
2017-04-07 03:44:00 -0700

    Disable RendererAudioOutputStreamFactoryIntegrationTest.StreamIntegrationTest on Win
    BUG=709394
    TBR=maxmorin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2804953006
    Cr-Commit-Position: refs/heads/master@{#462832}
    
    Former-commit-id: ec5dca5c2345b87104ef47fee11ac9fce6b1f0cc

--- content/browser/renderer_host/media/renderer_audio_output_stream_factory_context_impl_unittest.cc
+++ content/browser/renderer_host/media/renderer_audio_output_stream_factory_context_impl_unittest.cc
@@ -327,7 +327,14 @@ class RendererAudioOutputStreamFactoryIntegrationTest : public Test {
       factory_context_;
 };
 
-TEST_F(RendererAudioOutputStreamFactoryIntegrationTest, StreamIntegrationTest) {
+// Flaky on Win x64. https://crbug.com/709394
+#if defined(OS_WIN)
+#define MAYBE_StreamIntegrationTest DISABLED_StreamIntegrationTest
+#else
+#define MAYBE_StreamIntegrationTest StreamIntegrationTest
+#endif
+TEST_F(RendererAudioOutputStreamFactoryIntegrationTest,
+       MAYBE_StreamIntegrationTest) {
   // Sets up the factory on the IO thread and runs client code on the UI thread.
   // Send a sine wave from the client and makes sure it's received by the output
   // stream.
