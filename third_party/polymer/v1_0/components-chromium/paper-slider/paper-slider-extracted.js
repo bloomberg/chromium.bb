e72745fa5c18bba88af78bfb75ab7b17dcf91233
dschuyler <dschuyler@chromium.org>
2017-04-07 12:17:09 -0700

    [Polymer] update paper-slider for RTL support
    This CL applies a pull request submitted to Polymer to add RTL support
    for paper-sliders.
    
    BUG=696437
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2801193002
    Cr-Commit-Position: refs/heads/master@{#462953}
    
    Former-commit-id: 6a6df4ec7e64dfc890c6450cd4a86e39f6754da1

--- third_party/polymer/v1_0/components-chromium/paper-slider/paper-slider-extracted.js
+++ third_party/polymer/v1_0/components-chromium/paper-slider/paper-slider-extracted.js
@@ -114,8 +114,10 @@ Polymer({
       },
 
       keyBindings: {
-        'left down pagedown home': '_decrementKey',
-        'right up pageup end': '_incrementKey'
+        'left': '_leftKey',
+        'right': '_rightKey',
+        'down pagedown home': '_decrementKey',
+        'up pageup end': '_incrementKey'
       },
 
       /**
@@ -213,7 +215,9 @@ Polymer({
           this._trackStart(event);
         }
 
-        var dx = Math.min(this._maxx, Math.max(this._minx, event.detail.dx));
+        var direction = this._isRTL ? -1 : 1;
+        var dx = Math.min(
+            this._maxx, Math.max(this._minx, event.detail.dx * direction));
         this._x = this._startx + dx;
 
         var immediateValue = this._calcStep(this._calcKnobPosition(this._x / this._w));
@@ -251,6 +255,9 @@ Polymer({
         this._w = this.$.sliderBar.offsetWidth;
         var rect = this.$.sliderBar.getBoundingClientRect();
         var ratio = (event.detail.x - rect.left) / this._w;
+        if (this._isRTL) {
+          ratio = 1 - ratio;
+        }
         var prevRatio = this.ratio;
 
         this._setTransiting(true);
@@ -292,6 +299,9 @@ Polymer({
         if (steps > maxMarkers) {
           steps = maxMarkers;
         }
+        if (steps < 0 || !isFinite(steps)) {
+          steps = 0;
+        }
         this._setMarkers(new Array(steps));
       },
 
@@ -313,6 +323,27 @@ Polymer({
           transiting: this.transiting,
           editable: this.editable
         });
+      },
+
+      get _isRTL() {
+        if (this.__isRTL === undefined) {
+          this.__isRTL = window.getComputedStyle(this)['direction'] === 'rtl';
+        }
+        return this.__isRTL;
+      },
+
+      _leftKey: function(event) {
+        if (this._isRTL)
+          this._incrementKey(event);
+        else
+          this._decrementKey(event);
+      },
+
+      _rightKey: function(event) {
+        if (this._isRTL)
+          this._decrementKey(event);
+        else
+          this._incrementKey(event);
       },
 
       _incrementKey: function(event) {
