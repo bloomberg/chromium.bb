e2b3a0605aee0d9bde24c8167a5adeea1975a869
Gurchetan Singh <gurchetansingh@google.com>
2017-04-07 17:15:34 -0700

    minigbm: virtio-gpu: add more supported formats
    There is a desire to use virtio-gpu + Android with kernel v4.4. Let's
    make it like the vgem backend, which aligns widths and heights to
    64-pixel boundaries to support LLVM requirements.
    
    BUG=chromium:616275
    TEST=no crashes when running Android with amd64-generic-cheets
    
    Change-Id: I5640a0f9cc84260d30eecddd84307170fa41d2ce
    Reviewed-on: https://chromium-review.googlesource.com/471879
    Commit-Ready: Gurchetan Singh <gurchetansingh@chromium.org>
    Tested-by: Gurchetan Singh <gurchetansingh@chromium.org>
    Reviewed-by: Gurchetan Singh <gurchetansingh@chromium.org>

--- virtio_gpu.c
+++ virtio_gpu.c
@@ -8,18 +8,46 @@
 #include "helpers.h"
 #include "util.h"
 
-static const uint32_t supported_formats[] = { DRM_FORMAT_ARGB8888, DRM_FORMAT_XRGB8888 };
+#define MESA_LLVMPIPE_TILE_ORDER 6
+#define MESA_LLVMPIPE_TILE_SIZE (1 << MESA_LLVMPIPE_TILE_ORDER)
+
+static const uint32_t supported_formats[] = { DRM_FORMAT_ABGR8888,      DRM_FORMAT_ARGB8888,
+					      DRM_FORMAT_RGB565,	DRM_FORMAT_XBGR8888,
+					      DRM_FORMAT_XRGB8888,      DRM_FORMAT_YVU420,
+					      DRM_FORMAT_YVU420_ANDROID };
 
 static int virtio_gpu_init(struct driver *drv)
 {
 	return drv_add_linear_combinations(drv, supported_formats, ARRAY_SIZE(supported_formats));
 }
 
+static int virtio_gpu_bo_create(struct bo *bo, uint32_t width, uint32_t height, uint32_t format,
+				uint32_t flags)
+{
+	int ret = drv_dumb_bo_create(bo, ALIGN(width, MESA_LLVMPIPE_TILE_SIZE),
+				     ALIGN(height, MESA_LLVMPIPE_TILE_SIZE), format, flags);
+	return ret;
+}
+
+static uint32_t virtio_gpu_resolve_format(uint32_t format)
+{
+	switch (format) {
+	case DRM_FORMAT_FLEX_IMPLEMENTATION_DEFINED:
+		/*HACK: See b/28671744 */
+		return DRM_FORMAT_XBGR8888;
+	case DRM_FORMAT_FLEX_YCbCr_420_888:
+		return DRM_FORMAT_YVU420_ANDROID;
+	default:
+		return format;
+	}
+}
+
 struct backend backend_virtio_gpu = {
 	.name = "virtio_gpu",
 	.init = virtio_gpu_init,
-	.bo_create = drv_dumb_bo_create,
+	.bo_create = virtio_gpu_bo_create,
 	.bo_destroy = drv_dumb_bo_destroy,
 	.bo_import = drv_prime_bo_import,
 	.bo_map = drv_dumb_bo_map,
+	.resolve_format = virtio_gpu_resolve_format,
 };
