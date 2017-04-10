4dc92021ed60894ef54f343c22774af965a719bd
pavor <pavor@yandex-team.ru>
2017-04-10 03:29:26 -0700

    Fix PlatformInfo variation for mime util IsCodecSupportedOnAndroidTest
    The parameterized template function RunCodecSupportTest is supposed to
    invoke tested callback with different combinations of PlatformInfo settings,
    but it actually used only initial values of PlatformInfo fields.
    
    Also it seems that H264 is always supported for non-encrypted video, so the
    test IsCodecSupportedOnAndroidTest.ClearCodecBehavior should be corrected.
    This has been revealed only after fixing parameter variation.
    
    R=chcunningham, dalecurtis
    BUG=709428
    
    Review-Url: https://codereview.chromium.org/2804883007
    Cr-Commit-Position: refs/heads/master@{#463213}
    
    Former-commit-id: 1b75c4d80c9ac27428c760c727c39500d6e8c27b

--- media/base/mime_util_unittest.cc
+++ media/base/mime_util_unittest.cc
@@ -65,8 +65,8 @@ static void RunCodecSupportTest(const MimeUtil::PlatformInfo& states_to_vary,
   MimeUtil::PlatformInfo info;
 
 #define RUN_TEST_VECTOR(name)                   \
-  size_t name##_index = 0;                      \
-  for (info.name = name##_states[name##_index]; \
+  for (size_t name##_index = 0;                 \
+       info.name = name##_states[name##_index], \
        name##_index < name##_states.size(); ++name##_index)
 
   RUN_TEST_VECTOR(has_platform_decoders) {
@@ -333,6 +333,7 @@ TEST(IsCodecSupportedOnAndroidTest, ClearCodecBehavior) {
 
           // These codecs are always supported with the unified pipeline.
           case MimeUtil::FLAC:
+          case MimeUtil::H264:
           case MimeUtil::PCM:
           case MimeUtil::MPEG2_AAC:
           case MimeUtil::MP3:
@@ -345,10 +346,6 @@ TEST(IsCodecSupportedOnAndroidTest, ClearCodecBehavior) {
             break;
 
           // These codecs are only supported if platform decoders are supported.
-          case MimeUtil::H264:
-            EXPECT_EQ(info.has_platform_decoders, result);
-            break;
-
           case MimeUtil::HEVC:
             EXPECT_EQ(HasHevcSupport() && info.has_platform_decoders, result);
             break;
