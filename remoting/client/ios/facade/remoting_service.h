4db79a47c26234f0f62abbab83efd95f7c422346
nicholss <nicholss@chromium.org>
2017-04-10 12:46:11 -0700

    CRD iOS: Updating remoting service to use oauth and host list fetcher.
    This intergrates some work that has landed in remoting base in the last month,
    remoting/client/ios now as access to common oauth classes in remoting/base. This
    is the integration with those classes and it's use. The app is still in
    development so there are still mocked methods and functions but this is a working
    first pass.
    
    R=joedow@chromium.org
    
    BUG=652781
    
    Review-Url: https://codereview.chromium.org/2794013005
    Cr-Commit-Position: refs/heads/master@{#463369}
    
    Former-commit-id: 18f139664640da5a1b9fa73225ecbde332d5f39e

--- remoting/client/ios/facade/remoting_service.h
+++ remoting/client/ios/facade/remoting_service.h
@@ -2,21 +2,71 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef REMOTING_CLIENT_IOS_FACADE_REMOTING_SERCICE_H_
-#define REMOTING_CLIENT_IOS_FACADE_REMOTING_SERCICE_H_
+#ifndef REMOTING_CLIENT_IOS_FACADE_REMOTING_SERVICE_H_
+#define REMOTING_CLIENT_IOS_FACADE_REMOTING_SERVICE_H_
 
+#import "remoting/client/chromoting_client_runtime.h"
 #import "remoting/client/ios/domain/host_info.h"
 #import "remoting/client/ios/domain/user_info.h"
 
+#include "base/memory/weak_ptr.h"
+
+// |RemotingAuthenticationDelegate|s are interested in authentication related
+// notifications.
+@protocol RemotingAuthenticationDelegate<NSObject>
+
+// Notifies the delegate that the authentication status of the current user has
+// changed to a new state.
+- (void)nowAuthenticated:(BOOL)authenticated;
+
+@end
+
+// |RemotingHostListDelegate|s are interested in notifications related to host
+// list.
+@protocol RemotingHostListDelegate<NSObject>
+
+- (void)hostListUpdated;
+
+@end
+
+// |RemotingService| is the centralized place to ask for information about
+// authentication or query the remote services. It also helps deal with the
+// runtime and threading used in the application. |RemotingService| is a
+// singleton and should only be accessed via the |SharedInstance| method.
 @interface RemotingService : NSObject
 
-+ (RemotingService*)sharedInstance;
+// Access to the singleton shared instance from this method.
++ (RemotingService*)SharedInstance;
 
+// Access to the current |ChromotingClientRuntime| from this method.
+- (remoting::ChromotingClientRuntime*)runtime;
+
+// Register to be a |RemotingAuthenticationDelegate|.
+- (void)setAuthenticationDelegate:(id<RemotingAuthenticationDelegate>)delegate;
+
+// A cached answer if there is a currently authenticated user.
 - (BOOL)isAuthenticated;
+
+// Provide an |authorizationCode| to authenticate a user as the first time user
+// of the application or OAuth Flow.
 - (void)authenticateWithAuthorizationCode:(NSString*)authorizationCode;
+
+// Provide the |refreshToken| and |email| to authenticate a user as a returning
+// user of the application.
+- (void)authenticateWithRefreshToken:(NSString*)refreshToken
+                               email:(NSString*)email;
+
+// Returns the currently logged in user info from cache, or nil if no
+// currently authenticated user.
 - (UserInfo*)getUser;
+
+// Register to be a |RemotingHostListDelegate|. Side effect of setting this
+// delegate is the application will attempt to fetch a fresh host list.
+- (void)setHostListDelegate:(id<RemotingHostListDelegate>)delegate;
+
+// Returns the currently cached host list or nil if none exist.
 - (NSArray<HostInfo*>*)getHosts;
 
 @end
 
-#endif  // REMOTING_CLIENT_IOS_FACADE_REMOTING_SERCICE_H_
+#endif  // REMOTING_CLIENT_IOS_FACADE_REMOTING_SERVICE_H_
