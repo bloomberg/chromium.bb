88b13c54aab021c37cc64647a0f0c02c127245c5
hbos <hbos@chromium.org>
2017-04-10 02:17:20 -0700

    RTCRtpReceiver.getContributingSources() added.
    Adds RTCRtpReceiver.getContributingSources(),
    https://w3c.github.io/webrtc-pc/#dom-rtcrtpreceiver-getcontributingsources,
    and RTCRtpContributingSource,
    https://w3c.github.io/webrtc-pc/#dom-rtcrtpcontributingsource,
    behind flag "RuntimeEnabled=RTCRtpReceiver".
    
    Contributing sources come in two flavors, SSRCs and CSRCs. This
    interface represents both. For now we ignore SSRCs and only return
    CSRCs. It was recently decided that SSRCs would be returned in a
    separate method to distinguish between the two cases.
    
    A LayoutTest is used to thoroughly test all blink functionality with
    regards to CSRCs and the caching of contributing sources. This uses
    mocking to update contributing sources in a predictable way.
    
    The browsertest verifies that no contributing sources are used in a
    normal peerconnection call. The multiple CSRCs case is currently not
    possible to test in a browsertest so we rely on the LayoutTest (and
    webrtc layer testing of contributing sources).
    
    BUG=703122
    
    Review-Url: https://codereview.chromium.org/2803693002
    Cr-Commit-Position: refs/heads/master@{#463199}
    
    Former-commit-id: 7a16f508ad712bc38ff72e979dd5155a4015c320

---
+++ third_party/WebKit/Source/modules/peerconnection/RTCRtpContributingSource.h
@@ -0,0 +1,42 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef RTCRtpContributingSource_h
+#define RTCRtpContributingSource_h
+
+#include "bindings/core/v8/ScriptWrappable.h"
+#include "platform/heap/GarbageCollected.h"
+#include "platform/heap/Member.h"
+#include "platform/heap/Visitor.h"
+
+namespace blink {
+
+class RTCRtpReceiver;
+class WebRTCRtpContributingSource;
+
+// https://w3c.github.io/webrtc-pc/#dom-rtcrtpcontributingsource
+class RTCRtpContributingSource final
+    : public GarbageCollectedFinalized<RTCRtpContributingSource>,
+      public ScriptWrappable {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  RTCRtpContributingSource(RTCRtpReceiver*, const WebRTCRtpContributingSource&);
+  // The source's source ID must match |source|.
+  void UpdateMembers(const WebRTCRtpContributingSource&);
+
+  double timestamp();
+  uint32_t source() const;
+
+  DECLARE_VIRTUAL_TRACE();
+
+ private:
+  Member<RTCRtpReceiver> receiver_;
+  double timestamp_ms_;
+  const uint32_t source_;
+};
+
+}  // namespace blink
+
+#endif  // RTCRtpContributingSource_h
