0e16b6dd0ad71730a8d0276da9891a580353bc54
tdanderson <tdanderson@chromium.org>
2017-04-11 12:50:14 -0700

    Add gesture support in views::ResizeArea
    Add gesture (touch) support in views::ResizeArea, which
    is used for resizing the location bar. This area is located
    directly to the right of the location bar.
    
    BUG=320544
    TEST=manual
    
    Review-Url: https://codereview.chromium.org/2796303006
    Cr-Commit-Position: refs/heads/master@{#463733}
    
    Former-commit-id: 6db93d5b7ac95549bea2a1ec2d82cf6c1e8ed58d

--- ui/views/controls/resize_area.h
+++ ui/views/controls/resize_area.h
@@ -14,11 +14,7 @@ namespace views {
 
 class ResizeAreaDelegate;
 
-////////////////////////////////////////////////////////////////////////////////
-//
 // An invisible area that acts like a horizontal resizer.
-//
-////////////////////////////////////////////////////////////////////////////////
 class VIEWS_EXPORT ResizeArea : public View {
  public:
   static const char kViewClassName[];
@@ -26,9 +22,10 @@ class VIEWS_EXPORT ResizeArea : public View {
   explicit ResizeArea(ResizeAreaDelegate* delegate);
   ~ResizeArea() override;
 
-  // Overridden from views::View:
+  // views::View:
   const char* GetClassName() const override;
   gfx::NativeCursor GetCursor(const ui::MouseEvent& event) override;
+  void OnGestureEvent(ui::GestureEvent* event) override;
   bool OnMousePressed(const ui::MouseEvent& event) override;
   bool OnMouseDragged(const ui::MouseEvent& event) override;
   void OnMouseReleased(const ui::MouseEvent& event) override;
@@ -40,10 +37,16 @@ class VIEWS_EXPORT ResizeArea : public View {
   // directionality.
   void ReportResizeAmount(int resize_amount, bool last_update);
 
+  // Converts |event_x| to screen coordinates and sets |initial_position_| to
+  // this value.
+  void SetInitialPosition(int event_x);
+
   // The delegate to notify when we have updates.
   ResizeAreaDelegate* delegate_;
 
-  // The mouse position at start (in screen coordinates).
+  // The event's x-position at the start of the resize operation. The resize
+  // area will move while being dragged, so |initial_position_| is represented
+  // in screen coordinates so that we don't lose our bearings.
   int initial_position_;
 
   DISALLOW_COPY_AND_ASSIGN(ResizeArea);
