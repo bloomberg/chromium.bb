c4f49b3662f6f404c97f4b96e1f1a054e88d285d
mkwst <mkwst@chromium.org>
2017-04-11 03:59:17 -0700

    Revert of Using multi-tab story in TabSwitching Benchmark (patchset #12 id:220001 of https://codereview.chromium.org/2766533002/ )
    Reason for revert:
    It looks like this caused some breakage on Mac telemetry_perf_unittests: https://build.chromium.org/p/chromium.mac/builders/Mac10.11%20Tests/builds/10856
    
    Original issue's description:
    > Using multi-tab story in TabSwitching Benchmark and rewrite measurement
    > to fit the new multi-tab story
    >
    > BUG=689388
    >
    > Review-Url: https://codereview.chromium.org/2766533002
    > Cr-Commit-Position: refs/heads/master@{#463563}
    > Committed: https://chromium.googlesource.com/chromium/src/+/9f6769141c688eacc68512144384f459ba127bc8 [formerly cd01f90472cd2aa9fa824c902f39d0b9e50e4fba]
    
    TBR=nednguyen@google.com,deanliao@chromium.org,achuith@chromium.org,bccheng@chromium.org,perezju@chromium.org,vovoy@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=689388
    
    Review-Url: https://codereview.chromium.org/2809813003
    Cr-Commit-Position: refs/heads/master@{#463596}
    
    Former-commit-id: c11680219c62c4276972d72d6cb0da91cb96b80e

--- tools/perf/page_sets/system_health/multi_tab_stories.py
+++ tools/perf/page_sets/system_health/multi_tab_stories.py
@@ -12,7 +12,7 @@ from page_sets.system_health import platforms
 from telemetry import benchmark
 
 
-class MultiTabStory(system_health_story.SystemHealthStory):
+class _MultiTabStory(system_health_story.SystemHealthStory):
   ABSTRACT_STORY = True
 
   def RunNavigateSteps(self, action_runner):
@@ -40,7 +40,7 @@ class MultiTabStory(system_health_story.SystemHealthStory):
 
 
 @benchmark.Disabled('all')  # crbug.com/704197
-class MultiTabTypical24Story(MultiTabStory):
+class MultiTabTypical24Story(_MultiTabStory):
   NAME = 'multitab:misc:typical24'
   TAGS = [story_tags.TABS_SWITCHING]
   URL_LIST = [
