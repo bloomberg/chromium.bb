e632a1e8cebc48bf27410c03dff8fb334d843988
ananta <ananta@chromium.org>
2017-04-11 20:48:16 -0700

    Fix an issue with the bookmarks bar in Harmony where in the focus ring would paint outside the scrollview.
    The proposed fix here is to scroll the parent scrollview by the row bounds to ensure that the ring does not
    paint outside the content area. There is an underlying problem here with the focus ring not getting clipped
    when it tries to paint outside the parent.
    
    The correct fix there is to notify the parents about the layer being used the focus ring which would require
    them to create layers to host the child layer. Clipping can be done then.
    
    The plan is to work on that in a subsequent patch.
    
    BUG=665412
    
    Review-Url: https://codereview.chromium.org/2813103002
    Cr-Commit-Position: refs/heads/master@{#463923}
    
    Former-commit-id: 7720633b71d3f5d8d3c912b93d2466a0b2972c24

--- ui/views/controls/tree/tree_view.cc
+++ ui/views/controls/tree/tree_view.cc
@@ -729,6 +729,8 @@ void TreeView::LayoutEditor() {
                    -(empty_editor_size_.height() - font_list_.GetHeight()) / 2);
   // Give a little extra space for editing.
   row_bounds.set_width(row_bounds.width() + 50);
+  // Scroll as necessary to ensure that the editor is visible.
+  ScrollRectToVisible(row_bounds);
   editor_->SetBoundsRect(row_bounds);
   editor_->Layout();
 }
