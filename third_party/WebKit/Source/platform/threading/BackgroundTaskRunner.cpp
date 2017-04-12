853bda7270f486ffeddf0e954f41088909061b0d
fdoray <fdoray@chromium.org>
2017-04-12 12:58:13 -0700

    Revert of Use TaskScheduler instead of WorkerPool in BackgroundTaskRunner.cpp (patchset #1 id:1 of https://codereview.chromium.org/2685673005/ )
    Reason for revert:
    Reverting to see the effect on memory benchmarks.
    BUG=691618
    
    Original issue's description:
    > Use TaskScheduler instead of WorkerPool in BackgroundTaskRunner.cpp
    >
    > This CL replaces base::WorkerPool::PostTask() with
    > base::PostTaskWithTraits(). The following traits are used:
    >
    > Priority: Inherited (default)
    >   The priority is inherited from the calling context (i.e. TaskTraits
    >   are initialized with the priority of the current task).
    >
    > Shutdown behavior: CONTINUE_ON_SHUTDOWN
    >   Tasks posted with this mode which have not started executing before
    >   shutdown is initiated will never run. Tasks with this mode running at
    >   shutdown will be ignored (the worker will not be joined).
    >
    >   Note: Tasks that were previously posted to base::WorkerPool should
    >   use this shutdown behavior because this is how base::WorkerPool
    >   handles all its tasks.
    >
    > *No* May Block (default):
    >   Tasks may no block on synchronous file I/O operations or on sync
    >   primitives.
    >
    > BUG=659191
    >
    > Review-Url: https://codereview.chromium.org/2685673005
    > Cr-Commit-Position: refs/heads/master@{#449633}
    > Committed: https://chromium.googlesource.com/chromium/src/+/ea35dbb4d69a01ad8a5eaacbf641753746964e9a [formerly 99e56574537a8cf27af4c86f34e42fcad65e21c8]
    
    TBR=kinuko@chromium.org,haraken@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=659191
    
    Review-Url: https://codereview.chromium.org/2812263003
    Cr-Commit-Position: refs/heads/master@{#464121}
    
    Former-commit-id: 24e5d1ddee9fa61587b6f0279a25792d0208b9f1

--- third_party/WebKit/Source/platform/threading/BackgroundTaskRunner.cpp
+++ third_party/WebKit/Source/platform/threading/BackgroundTaskRunner.cpp
@@ -5,7 +5,7 @@
 #include "platform/threading/BackgroundTaskRunner.h"
 
 #include "base/location.h"
-#include "base/task_scheduler/post_task.h"
+#include "base/threading/worker_pool.h"
 #include "public/platform/WebTraceLocation.h"
 
 namespace blink {
@@ -13,11 +13,8 @@ namespace blink {
 void BackgroundTaskRunner::PostOnBackgroundThread(
     const WebTraceLocation& location,
     std::unique_ptr<CrossThreadClosure> closure) {
-  base::PostTaskWithTraits(
-      location,
-      base::TaskTraits().WithShutdownBehavior(
-          base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN),
-      ConvertToBaseCallback(std::move(closure)));
+  base::WorkerPool::PostTask(
+      location, ConvertToBaseCallback(std::move(closure)), false /* ignored */);
 }
 
 }  // namespace blink
