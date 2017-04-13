b4c119ccdb8d09ba3ad4ea2241f62afdf1701abc
stevenjb <stevenjb@chromium.org>
2017-04-13 15:56:02 -0700

    MD Settings: Internet: Enable WiFi on row tap
    BUG=709074
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2820543002
    Cr-Commit-Position: refs/heads/master@{#464589}
    
    Former-commit-id: 7cda776b17ae612ddc45b948e4a350a5295a1b5b

--- chrome/browser/resources/settings/internet_page/network_summary_item.js
+++ chrome/browser/resources/settings/internet_page/network_summary_item.js
@@ -159,12 +159,17 @@ Polymer({
    * @private
    */
   onShowDetailsTap_: function(event) {
-    if (this.shouldShowList_())
+    if (!this.deviceIsEnabled_(this.deviceState)) {
+      this.fire(
+          'device-enabled-toggled',
+          {enabled: true, type: this.deviceState.Type});
+    } else if (this.shouldShowList_()) {
       this.fire('show-networks', this.deviceState);
-    else if (this.activeNetworkState.GUID)
+    } else if (this.activeNetworkState.GUID) {
       this.fire('show-detail', this.activeNetworkState);
-    else if (this.networkStateList.length > 0)
+    } else if (this.networkStateList.length > 0) {
       this.fire('show-detail', this.networkStateList[0]);
+    }
     event.stopPropagation();
   },
 
