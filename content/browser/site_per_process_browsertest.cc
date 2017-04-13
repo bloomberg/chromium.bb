6c73b2309bc3b077834e28b68438c1f0bb9f1b90
wjmaclean <wjmaclean@chromium.org>
2017-04-13 12:55:18 -0700

    Revert of Re-enable OOPIF scroll bubbling test. (patchset #1 id:1 of https://codereview.chromium.org/2818733002/ )
    Reason for revert:
    We need to land the DCHECK fix in https://codereview.chromium.org/2814043004/ first.
    
    Original issue's description:
    > Re-enable OOPIF scroll bubbling test.
    >
    > Scroll animation breaks this test, so re-enable by setting flag on
    > mouse wheel events that disable the animation pathway. We'll need a
    > separate test for the animation pathways at some point.
    >
    > BUG=627238
    > NO_DEPENDENCY_CHECKS=true
    >
    > Review-Url: https://codereview.chromium.org/2818733002
    > Cr-Commit-Position: refs/heads/master@{#464434}
    > Committed: https://chromium.googlesource.com/chromium/src/+/e0f1dc460ef09ae749fa98f5dbd806ffdc233827 [formerly 1ed97c2a3c31e85cadfc73b5e775b17571f0656e]
    
    TBR=avi@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=627238
    
    Review-Url: https://codereview.chromium.org/2815383002
    Cr-Commit-Position: refs/heads/master@{#464515}
    
    Former-commit-id: 75b40615b6c4f91c40120a7b79ae22c5980a05c3

--- content/browser/site_per_process_browsertest.cc
+++ content/browser/site_per_process_browsertest.cc
@@ -1090,13 +1090,9 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessBrowserTest,
 
 // Test that scrolling a nested out-of-process iframe bubbles unused scroll
 // delta to a parent frame.
-#if defined(OS_ANDROID)
-#define MAYBE_ScrollBubblingFromOOPIFTest DISABLED_ScrollBubblingFromOOPIFTest
-#else
-#define MAYBE_ScrollBubblingFromOOPIFTest ScrollBubblingFromOOPIFTest
-#endif
+// Flaky: https://crbug.com/627238
 IN_PROC_BROWSER_TEST_F(SitePerProcessBrowserTest,
-                       MAYBE_ScrollBubblingFromOOPIFTest) {
+                       DISABLED_ScrollBubblingFromOOPIFTest) {
   GURL main_url(embedded_test_server()->GetURL(
       "a.com", "/cross_site_iframe_factory.html?a(b)"));
   EXPECT_TRUE(NavigateToURL(shell(), main_url));
@@ -1164,10 +1160,6 @@ IN_PROC_BROWSER_TEST_F(SitePerProcessBrowserTest,
   scroll_event.SetPositionInWidget(1, 1);
   scroll_event.delta_x = 0.0f;
   scroll_event.delta_y = -5.0f;
-  // Set has_precise_scroll_deltas to keep these events off the animated scroll
-  // pathways, which currently break this test.
-  // https://bugs.chromium.org/p/chromium/issues/detail?id=710513
-  scroll_event.has_precise_scrolling_deltas = true;
   rwhv_parent->ProcessMouseWheelEvent(scroll_event, ui::LatencyInfo());
 
   // Ensure that the view position is propagated to the child properly.
