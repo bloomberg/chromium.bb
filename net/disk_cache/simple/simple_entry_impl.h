128ce33e38c159808a6ec17c9cb8537d604b9dad
tzik <tzik@chromium.org>
2017-04-13 21:03:27 -0700

    Avoid cross thread malloc/free pair of IOBuffer on the simple cache
    This CL removes a cross thread malloc/free pair from simple disk cache backend.
    
    After a PostTaskAndReply change in http://crrev.com/87037e099c0f0891 [formerly e5de8d551e80115b], it
    destroys |task| part of the given tasks on the target thread, and that
    introduced a number of cross thread malloc/free pairs around net::IOBuffer.
    The cross thread malloc/free pair increased the apparent size of memory
    usage on some Android perf bots by 2~3%, that is likely due to thread
    specific free list caches.
    
    BUG=708644
    
    Review-Url: https://codereview.chromium.org/2815563002
    Cr-Original-Commit-Position: refs/heads/master@{#464338}
    Committed: https://chromium.googlesource.com/chromium/src/+/9570bb13c9f2c20d45b62e9a7d791e4128b11dac [formerly 1714b61635cc8207647b1a9857e8042fbb833025]
    Review-Url: https://codereview.chromium.org/2815563002
    Cr-Commit-Position: refs/heads/master@{#464673}
    
    Former-commit-id: 248782b9cda664a1ea6d347dd508354d23160a5a

--- net/disk_cache/simple/simple_entry_impl.h
+++ net/disk_cache/simple/simple_entry_impl.h
@@ -253,10 +253,14 @@ class NET_EXPORT_PRIVATE SimpleEntryImpl : public Entry,
                              std::unique_ptr<int> result);
 
   // Called after an asynchronous write completes.
+  // |buf| parameter brings back a reference to net::IOBuffer to the original
+  // thread, so that we can reduce cross thread malloc/free pair.
+  // See http://crbug.com/708644 for details.
   void WriteOperationComplete(int stream_index,
                               const CompletionCallback& completion_callback,
                               std::unique_ptr<SimpleEntryStat> entry_stat,
-                              std::unique_ptr<int> result);
+                              std::unique_ptr<int> result,
+                              net::IOBuffer* buf);
 
   void ReadSparseOperationComplete(
       const CompletionCallback& completion_callback,
