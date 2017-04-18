0f0338f423af22d816f86e8e99cf8ee3b4df0596
estade <estade@chromium.org>
2017-04-18 13:43:25 -0700

    Use MD settings icon in ash shelf unconditionally.
    The shelf is already fully materialized. The icon appears on the shelf.
    No reason to use a different icon for pre-MD settings.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2794333003
    Cr-Commit-Position: refs/heads/master@{#465356}
    
    Former-commit-id: cbf212b54620d1c1cb11ea1072fd698dd47ba26b

--- chrome/browser/ui/ash/launcher/settings_window_observer.cc
+++ chrome/browser/ui/ash/launcher/settings_window_observer.cc
@@ -7,12 +7,10 @@
 #include "ash/public/cpp/shelf_item.h"
 #include "ash/public/cpp/window_properties.h"
 #include "ash/resources/grit/ash_resources.h"
-#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/settings_window_manager.h"
-#include "chrome/common/chrome_features.h"
 #include "components/strings/grit/components_strings.h"
 #include "services/ui/public/interfaces/window_manager.mojom.h"
 #include "ui/aura/client/aura_constants.h"
@@ -56,11 +54,9 @@ void SettingsWindowObserver::OnNewSettingsWindow(Browser* settings_browser) {
   window->SetTitle(l10n_util::GetStringUTF16(IDS_SETTINGS_TITLE));
   window->SetProperty<int>(ash::kShelfItemTypeKey, ash::TYPE_DIALOG);
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
-  gfx::ImageSkia* icon =
-      base::FeatureList::IsEnabled(features::kMaterialDesignSettings)
-          ? rb.GetImageSkiaNamed(IDR_ASH_SHELF_ICON_MD_SETTINGS)
-          : rb.GetImageSkiaNamed(IDR_ASH_SHELF_ICON_SETTINGS);
   // The new gfx::ImageSkia instance is owned by the window itself.
-  window->SetProperty(aura::client::kWindowIconKey, new gfx::ImageSkia(*icon));
+  window->SetProperty(
+      aura::client::kWindowIconKey,
+      new gfx::ImageSkia(*rb.GetImageSkiaNamed(IDR_ASH_SHELF_ICON_SETTINGS)));
   aura_window_tracker_->Add(window);
 }
