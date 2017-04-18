269549633dc5f0a30df7c5a20e8fff200ee3a90a
Sunny Sachanandani <sunnyps@chromium.org>
2017-04-17 19:30:05 -0700

    gpu: Automatically add optional gpu tests to ui/gl changes.
    ui/gl changes are likely to affect gpu tests so we should run these
    trybots.
    
    R=kbr
    BUG=712303
    
    Change-Id: Id8b89b4647b1b1926184ff55a8746adddc3b345b
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Change-Id: Id8b89b4647b1b1926184ff55a8746adddc3b345b
    Reviewed-on: https://chromium-review.googlesource.com/479802
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Sunny Sachanandani <sunnyps@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#465347}
    Former-commit-id: 4b2894f1267ae935781d02057ab0ac01feae8d7e

--- ui/gl/PRESUBMIT.py
+++ ui/gl/PRESUBMIT.py
@@ -1,8 +1,8 @@
-# Copyright (c) 2016 The Chromium Authors. All rights reserved.
+# Copyright (c) 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Top-level presubmit script for gpu.
+"""Presubmit script for //ui/gl.
 
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details about the presubmit API built into depot_tools.
