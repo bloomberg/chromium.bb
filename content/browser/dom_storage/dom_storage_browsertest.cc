f3c9ec455c46516e0ff2a8f040a84277d84876f9
mek <mek@chromium.org>
2017-04-19 10:08:57 -0700

    Disable several flaky mojo localstorage tests on Mac.
    Somehow sometimes browser shutdown crashes on shutting down
    the DB thread for these tests on mac.
    
    BUG=712872
    
    Review-Url: https://codereview.chromium.org/2820343003
    Cr-Commit-Position: refs/heads/master@{#465644}
    
    Former-commit-id: 17e49b087759887c2ebe54950ab5efdf45472fee

--- content/browser/dom_storage/dom_storage_browsertest.cc
+++ content/browser/dom_storage/dom_storage_browsertest.cc
@@ -98,7 +98,13 @@ IN_PROC_BROWSER_TEST_F(DOMStorageBrowserTest, MAYBE_DataPersists) {
   SimpleTest(GetTestUrl("dom_storage", "verify_data.html"), kNotIncognito);
 }
 
-IN_PROC_BROWSER_TEST_F(MojoDOMStorageBrowserTest, SanityCheck) {
+// http://crbug.com/712872 All the mojo tests are flaky on Mac.
+#if defined(OS_MACOSX)
+#define MAYBE_SanityCheck DISABLED_SanityCheck
+#else
+#define MAYBE_SanityCheck SanityCheck
+#endif
+IN_PROC_BROWSER_TEST_F(MojoDOMStorageBrowserTest, MAYBE_SanityCheck) {
   SimpleTest(GetTestUrl("dom_storage", "sanity_check.html"), kNotIncognito);
 }
 
@@ -112,6 +118,11 @@ IN_PROC_BROWSER_TEST_F(MojoDOMStorageBrowserTest, PRE_DataPersists) {
   Flush();
 }
 
+// http://crbug.com/712872 All the mojo tests are flaky on Mac.
+#if defined(OS_MACOSX)
+#undef MAYBE_DataPersists
+#define MAYBE_DataPersists DISABLED_DataPersists
+#endif
 IN_PROC_BROWSER_TEST_F(MojoDOMStorageBrowserTest, MAYBE_DataPersists) {
   SimpleTest(GetTestUrl("dom_storage", "verify_data.html"), kNotIncognito);
 }
@@ -134,6 +145,9 @@ IN_PROC_BROWSER_TEST_F(DOMStorageMigrationBrowserTest, PRE_DataMigrates) {
 
 // http://crbug.com/654704 PRE_ tests aren't supported on Android.
 #if defined(OS_ANDROID)
+#define MAYBE_DataMigrates DISABLED_DataMigrates
+#elif defined(OS_MACOSX)
+// http://crbug.com/712872 All the mojo tests are flaky on Mac.
 #define MAYBE_DataMigrates DISABLED_DataMigrates
 #else
 #define MAYBE_DataMigrates DataMigrates
