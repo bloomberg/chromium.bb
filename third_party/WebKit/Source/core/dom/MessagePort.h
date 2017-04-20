d4da8e729f3745ee6ac322ebb4bea2c9dfd5024a
nhiroki <nhiroki@chromium.org>
2017-04-19 20:21:04 -0700

    Messaging: Avoid calling non-thread-safe functions in MessagePort::messageAvailable()
    As implementation comments in MessagePort::messageAvailable(), it can be called
    from another thread and should not call non-thread-functions. However, the
    current implementation wrongly calls ContextLifecycleObserver's
    GetExecutionContext() that is not a thread-safe function to post a task to the
    context thread.
    
    To avoid that, this CL replaces ExecutionContext::PostTask() with
    WebTaskRunner::PostTask(). The task runner is captured in the ctor of
    MessagePort called on the context thread.
    
    BUG=694925
    
    Review-Url: https://codereview.chromium.org/2818073002
    Cr-Commit-Position: refs/heads/master@{#465886}
    
    Former-commit-id: 829907ac0d3d2ed39d8752d22a6d0e45df1e8325

--- third_party/WebKit/Source/core/dom/MessagePort.h
+++ third_party/WebKit/Source/core/dom/MessagePort.h
@@ -34,6 +34,7 @@
 #include "core/dom/ContextLifecycleObserver.h"
 #include "core/events/EventListener.h"
 #include "core/events/EventTarget.h"
+#include "platform/WebTaskRunner.h"
 #include "platform/wtf/PassRefPtr.h"
 #include "platform/wtf/RefPtr.h"
 #include "platform/wtf/Vector.h"
@@ -140,9 +141,11 @@ class CORE_EXPORT MessagePort : public EventTargetWithInlineData,
 
   std::unique_ptr<WebMessagePortChannel> entangled_channel_;
 
-  int pending_dispatch_task_;
-  bool started_;
-  bool closed_;
+  int pending_dispatch_task_ = 0;
+  bool started_ = false;
+  bool closed_ = false;
+
+  RefPtr<WebTaskRunner> task_runner_;
 };
 
 }  // namespace blink
