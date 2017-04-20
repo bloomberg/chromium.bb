f01655c9c830628dc9ab76f2acb1ac8000b4dc6d
sgurun <sgurun@chromium.org>
2017-04-20 16:00:27 -0700

    Revert of Address some TODO(GYP_GONE)s (patchset #6 id:100001 of https://codereview.chromium.org/2569333002/ )
    Reason for revert:
    breaking android builder, see crbug.com/713915
    
    Original issue's description:
    > Address some TODO(GYP_GONE)s
    >
    > No intended behavior change. If this breaks your bot because it's
    > trying to build a foo_run target, make it build the corresponding
    > foo target instead (see diff).
    >
    > Depends on https://chromium-review.googlesource.com/#/c/419296/
    >
    > BUG=673897
    > CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel;master.tryserver.chromium.android:android_optional_gpu_tests_rel
    > TBR=brettw
    >
    > Review-Url: https://codereview.chromium.org/2569333002
    > Cr-Commit-Position: refs/heads/master@{#466154}
    > Committed: https://chromium.googlesource.com/chromium/src/+/ced3b21aa31c52f3997726193804fbaf4e854b99 [formerly 1d66d232a08315927b4ffb6212c1516c6ddfbe6d]
    
    TBR=dpranke@chromium.org,kbr@chromium.org,thakis@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=673897
    
    Review-Url: https://codereview.chromium.org/2831243002
    Cr-Commit-Position: refs/heads/master@{#466177}
    
    Former-commit-id: 637c7353077c92bbf627b602c57f0e3604525129

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -1470,6 +1470,9 @@ COMMON_GTESTS = {
         'os_types': ['android'],
       },
     ],
+    'override_compile_targets': [
+      'tab_capture_end2end_tests_run',
+    ],
   },
   'video_decode_accelerator_d3d11_unittest': {
     'tester_configs': [
@@ -2310,7 +2313,7 @@ def generate_telemetry_test(tester_name, tester_config,
   return generate_isolated_test(tester_name, tester_config, test,
                                 test_config, extra_browser_args,
                                 'telemetry_gpu_integration_test',
-                                ['telemetry_gpu_integration_test'],
+                                ['telemetry_gpu_integration_test_run'],
                                 prefix_args)
 
 def generate_telemetry_tests(tester_name, tester_config,
