16738cf5c78383455ef20284c4742e1e35d79389
gambard <gambard@chromium.org>
2017-04-20 05:45:46 -0700

    [ObjC ARC] Converts ios/chrome/browser/ui/side_swipe:unit_tests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2827963005
    Cr-Commit-Position: refs/heads/master@{#465985}
    
    Former-commit-id: a47740436146174ce74fca4fcbcf9b2b92c8e355

--- ios/chrome/browser/ui/side_swipe/side_swipe_controller_unittest.mm
+++ ios/chrome/browser/ui/side_swipe/side_swipe_controller_unittest.mm
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/mac/scoped_nsobject.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
 #import "ios/chrome/browser/ui/side_swipe/side_swipe_controller.h"
 #include "ios/web/public/test/test_web_thread_bundle.h"
@@ -11,35 +10,39 @@
 #import "third_party/ocmock/gtest_support.h"
 #include "third_party/ocmock/ocmock_extensions.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 class SideSwipeControllerTest : public PlatformTest {
  public:
   void SetUp() override {
     // Create a mock for the TabModel that owns the object under test.
-    tab_model_.reset([[OCMockObject niceMockForClass:[TabModel class]] retain]);
+    tab_model_ = [OCMockObject niceMockForClass:[TabModel class]];
 
     TestChromeBrowserState::Builder builder;
     browser_state_ = builder.Build();
     // Create the object to test.
-    side_swipe_controller_.reset([[SideSwipeController alloc]
-        initWithTabModel:tab_model_
-            browserState:browser_state_.get()]);
+    side_swipe_controller_ =
+        [[SideSwipeController alloc] initWithTabModel:tab_model_
+                                         browserState:browser_state_.get()];
 
-    view_.reset([[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 240)]);
+    view_ = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 240)];
 
     [side_swipe_controller_ addHorizontalGesturesToView:view_];
   };
 
   web::TestWebThreadBundle thread_bundle_;
   std::unique_ptr<TestChromeBrowserState> browser_state_;
-  base::scoped_nsobject<UIView> view_;
-  base::scoped_nsobject<TabModel> tab_model_;
-  base::scoped_nsobject<SideSwipeController> side_swipe_controller_;
+  UIView* view_;
+  TabModel* tab_model_;
+  SideSwipeController* side_swipe_controller_;
 };
 
 TEST_F(SideSwipeControllerTest, TestConstructor) {
-  EXPECT_TRUE(side_swipe_controller_.get());
+  EXPECT_TRUE(side_swipe_controller_);
 }
 
 TEST_F(SideSwipeControllerTest, TestSwipeRecognizers) {
