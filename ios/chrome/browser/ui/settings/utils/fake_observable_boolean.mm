e414b3dce6a5d974cceee5fed6df1b8d439c23e3
gambard <gambard@chromium.org>
2017-04-20 06:25:32 -0700

    [ObjC ARC] Converts ios/chrome/browser/ui/settings/utils:test_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2834623002
    Cr-Commit-Position: refs/heads/master@{#465990}
    
    Former-commit-id: 45d637552defb050205b9a1301b6c540a39dda0c

--- ios/chrome/browser/ui/settings/utils/fake_observable_boolean.mm
+++ ios/chrome/browser/ui/settings/utils/fake_observable_boolean.mm
@@ -4,6 +4,10 @@
 
 #import "ios/chrome/browser/ui/settings/utils/fake_observable_boolean.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 @implementation FakeObservableBoolean
 
 @synthesize value = _value;
