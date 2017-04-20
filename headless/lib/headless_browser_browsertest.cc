f029710994c70137c5bf0b5b8d612f0851d8f866
aboxhall <aboxhall@chromium.org>
2017-04-19 21:05:03 -0700

    Revert of Reland Enable crashpad for Mac. (patchset #1 id:1 of https://codereview.chromium.org/2820453003/ )
    Reason for revert:
    Sorry, looks like this is still causing some test failures:
    https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Fchromium.mac%2FMac10.10_Tests%2F16868%2F%2B%2Frecipes%2Fsteps%2Fheadless_browsertests_on_Mac-10.10%2F0%2Flogs%2FCrashReporterTest.GenerateMinidump%2F0
    https://uberchromegw.corp.google.com/i/chromium.mac/builders/Mac10.10%20Tests/builds/16868
    
    Original issue's description:
    > Reland Enable crashpad for Mac.
    > -- Add :mac_helpers dependendency to headless_lib
    >
    >
    >
    > Original reason for revert: Reason for revert:
    > mac_chromium10.10_rel_ng buildbot failing in browsertests
    > https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Fchromium.mac%2FMac10.10_Tests%2F16541%2F%2B%2Frecipes%2Fsteps%2Fheadless_browsertests_on_Mac-10.10%2F0%2Flogs%2FCrashReporterTest.GenerateMinidump%2F0
    >
    >
    > This follows the same logic as using breakpad in linux with flags --enable_crash-reporter and --crash-dumps-dir folders.
    >
    > For headless_shell target, we need to include the Helper crashpad_handler in the build. For --headless, this is already available in the framework bundle.
    >
    > BUG=687407
    >
    > Review-Url: https://codereview.chromium.org/2816593006
    > Cr-Commit-Position: refs/heads/master@{#464289}
    > Committed: https://chromium.googlesource.com/chromium/src/+/5d7aac285c32b1dd59cb8a48c1db823e0e786fd8 [formerly ecb2ad24952b077e71e56ba9d3edfc83c522f32a]
    >
    > patch from issue 2816593006 at patchset 60001 (http://crrev.com/2816593006#ps60001)
    >
    > Review-Url: https://codereview.chromium.org/2820453003
    > Cr-Commit-Position: refs/heads/master@{#465869}
    > Committed: https://chromium.googlesource.com/chromium/src/+/cb109ab96496476c1a2d66916952c425f2a63992 [formerly df51c3e48120bc5f68e0fc2cdc92d91592249e98]
    
    TBR=skyostil@chromium.org,dvallet@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=687407
    
    Review-Url: https://codereview.chromium.org/2826193003
    Cr-Commit-Position: refs/heads/master@{#465895}
    
    Former-commit-id: 70a619651df1aa66a84743d770b1736e5f696d95

--- headless/lib/headless_browser_browsertest.cc
+++ headless/lib/headless_browser_browsertest.cc
@@ -717,13 +717,12 @@ class CrashReporterTest : public HeadlessBrowserTest,
   base::FilePath crash_dumps_dir_;
 };
 
-// TODO(skyostil): Minidump generation currently is only supported on Linux and
-// Mac.
-#if defined(HEADLESS_USE_BREAKPAD) || defined(OS_MACOSX)
+// TODO(skyostil): Minidump generation currently is only supported on Linux.
+#if defined(HEADLESS_USE_BREAKPAD)
 #define MAYBE_GenerateMinidump GenerateMinidump
 #else
 #define MAYBE_GenerateMinidump DISABLED_GenerateMinidump
-#endif  // defined(HEADLESS_USE_BREAKPAD) || defined(OS_MACOSX)
+#endif  // defined(HEADLESS_USE_BREAKPAD)
 IN_PROC_BROWSER_TEST_F(CrashReporterTest, MAYBE_GenerateMinidump) {
   // Navigates a tab to chrome://crash and checks that a minidump is generated.
   // Note that we only test renderer crashes here -- browser crashes need to be
@@ -745,10 +744,6 @@ IN_PROC_BROWSER_TEST_F(CrashReporterTest, MAYBE_GenerateMinidump) {
 
   // Check that one minidump got created.
   {
-#if defined(OS_MACOSX)
-    // Mac outputs dumps in the 'completed' directory.
-    crash_dumps_dir_ = crash_dumps_dir_.Append("completed");
-#endif
     base::ThreadRestrictions::SetIOAllowed(true);
     base::FileEnumerator it(crash_dumps_dir_, /* recursive */ false,
                             base::FileEnumerator::FILES);
