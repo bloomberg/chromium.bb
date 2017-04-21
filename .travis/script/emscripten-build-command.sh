e8afe3c145fe4be1073df74feff5bd3420be1475
Reiner Dolp <reiner-dolp@users.noreply.github.com>
2017-04-21 09:51:05 +0100

    automatically export liblouis c-api in js builds

--- .travis/script/emscripten-build-command.sh
+++ .travis/script/emscripten-build-command.sh
@@ -12,14 +12,7 @@ function buildjs {
 	
 	set -x
 
-	emcc ./liblouis/.libs/liblouis.a -s RESERVED_FUNCTION_POINTERS=1\
-	 -s EXPORTED_FUNCTIONS="['_lou_version', '_lou_translateString', '_lou_translate',\
-	'_lou_backTranslateString', '_lou_backTranslate', '_lou_hyphenate',\
-	'_lou_compileString', '_lou_getTypeformForEmphClass', '_lou_dotsToChar',\
-	'_lou_charToDots', '_lou_registerLogCallback', '_lou_setLogLevel',\
-	'_lou_logFile', '_lou_logPrint', '_lou_logEnd', '_lou_setDataPath',\
-	'_lou_getDataPath', '_lou_getTable', '_lou_checkTable',\
-	'_lou_readCharFromFile', '_lou_free', '_lou_charSize']" -s MODULARIZE=1\
+	emcc ./liblouis/.libs/liblouis.a -s RESERVED_FUNCTION_POINTERS=1 -s MODULARIZE=1\
 	 -s EXPORT_NAME="'liblouisBuild'" -s EXTRA_EXPORTED_RUNTIME_METHODS="['FS',\
 	'Runtime', 'stringToUTF${1}', 'Pointer_Stringify']" --pre-js ./liblouis-js/inc/pre.js\
 	 --post-js ./liblouis-js/inc/post.js ${3} -o ./out/${2} &&
