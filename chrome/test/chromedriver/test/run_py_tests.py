c808901a710fd0feee0fd4dbbcc37362fae3ab6f
gmanikpure <gmanikpure@chromium.org>
2017-04-24 09:25:50 -0700

    [Chromedriver] Disable testWindowMaximize on Mac.
    BUG=chromedriver:1779
    
    Review-Url: https://codereview.chromium.org/2836743002
    Cr-Commit-Position: refs/heads/master@{#466647}
    
    Former-commit-id: c6cf016ecb53fa978f663d100c11e3aaa03997cc

--- chrome/test/chromedriver/test/run_py_tests.py
+++ chrome/test/chromedriver/test/run_py_tests.py
@@ -101,7 +101,10 @@ _OS_SPECIFIC_FILTER['linux'] = [
     # Xvfb doesn't support maximization.
     'ChromeDriverTest.testWindowMaximize',
 ]
-_OS_SPECIFIC_FILTER['mac'] = []
+_OS_SPECIFIC_FILTER['mac'] = [
+    # https://bugs.chromium.org/p/chromedriver/issues/detail?id=1779
+    'ChromeDriverTest.testWindowMaximize',
+]
 
 _DESKTOP_NEGATIVE_FILTER = [
     # Desktop doesn't support touch (without --touch-events).
