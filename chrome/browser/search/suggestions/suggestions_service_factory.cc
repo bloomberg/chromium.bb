81bcd37e209f83293edc5e55c1b97efeaf834099
fdoray <fdoray@chromium.org>
2017-04-24 12:57:57 -0700

    Use TaskScheduler instead of blocking pool in suggestions_service_factory.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=mathp@chromium.org
    
    Review-Url: https://codereview.chromium.org/2835273002
    Cr-Commit-Position: refs/heads/master@{#466724}
    
    Former-commit-id: 89b968cfda432441c0575f63710b8d3247c0cca5

--- chrome/browser/search/suggestions/suggestions_service_factory.cc
+++ chrome/browser/search/suggestions/suggestions_service_factory.cc
@@ -8,6 +8,7 @@
 #include <utility>
 
 #include "base/memory/ptr_util.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/suggestions/image_decoder_impl.h"
@@ -62,8 +63,9 @@ SuggestionsServiceFactory::~SuggestionsServiceFactory() {}
 KeyedService* SuggestionsServiceFactory::BuildServiceInstanceFor(
     content::BrowserContext* context) const {
   scoped_refptr<base::SequencedTaskRunner> background_task_runner =
-      BrowserThread::GetBlockingPool()->GetSequencedTaskRunner(
-          base::SequencedWorkerPool::GetSequenceToken());
+      base::CreateSequencedTaskRunnerWithTraits(
+          base::TaskTraits().MayBlock().WithPriority(
+              base::TaskPriority::BACKGROUND));
 
   Profile* profile = static_cast<Profile*>(context);
 
