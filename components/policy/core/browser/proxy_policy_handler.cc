84bcfb3720c72d255e7b41a316636e01e9b8fbc3
vabr <vabr@chromium.org>
2017-04-25 00:12:00 -0700

    Remove #include of ptr_util.h in proxy_policy_handler.cc
    After recent changes, proxy_policy_handler.cc no longer needs to include
    base/memory/ptr_util.h. This has been pointed in
    https://codereview.chromium.org/2785883003/diff/80001/components/policy/core/browser/proxy_policy_handler.cc#newcode11.
    Because that comment happened after landing of the original CL, that follow-up
    is done here.
    
    R=bartfab@chromium.org
    BUG=697817
    
    Review-Url: https://codereview.chromium.org/2793093003
    Cr-Commit-Position: refs/heads/master@{#466911}
    
    Former-commit-id: 967cb822e5eb8f4dc2245d2369f12a8f18a66b0e

--- components/policy/core/browser/proxy_policy_handler.cc
+++ components/policy/core/browser/proxy_policy_handler.cc
@@ -8,7 +8,6 @@
 
 #include "base/logging.h"
 #include "base/macros.h"
-#include "base/memory/ptr_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
 #include "components/policy/core/browser/configuration_policy_handler.h"
