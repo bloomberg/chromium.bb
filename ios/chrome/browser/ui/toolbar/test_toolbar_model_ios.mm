dcf839ec67b6c32be20f40f530e7895393c1a81a
gambard <gambard@chromium.org>
2017-04-25 05:49:35 -0700

    [ObjC ARC] Converts ios/chrome/browser/ui/toolbar:test_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2826073003
    Cr-Commit-Position: refs/heads/master@{#466957}
    
    Former-commit-id: bfbf87d97979e6a32080320bd5ade308b7a1844a

--- ios/chrome/browser/ui/toolbar/test_toolbar_model_ios.mm
+++ ios/chrome/browser/ui/toolbar/test_toolbar_model_ios.mm
@@ -7,14 +7,18 @@
 #include "components/toolbar/toolbar_model_impl.h"
 #import "ios/chrome/browser/tabs/tab.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 TestToolbarModelIOS::TestToolbarModelIOS()
     : ToolbarModelIOS(),
-      is_loading_(FALSE),
+      is_loading_(false),
       load_progress_fraction_(0.0),
-      can_go_back_(FALSE),
-      can_go_forward_(FALSE),
-      is_current_tab_native_page_(FALSE),
-      is_current_tab_bookmarked_(FALSE) {
+      can_go_back_(false),
+      can_go_forward_(false),
+      is_current_tab_native_page_(false),
+      is_current_tab_bookmarked_(false) {
   test_toolbar_model_.reset(new TestToolbarModel());
 }
 
