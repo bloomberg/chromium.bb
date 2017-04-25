c8a76b938e39180b30c288fa348e40183c143b85
kenrb <kenrb@chromium.org>
2017-04-25 15:00:36 -0700

    Set GPU rasterization trigger for OOPIFs
    Currently the GPU rasterization trigger is never set on the compositor
    for an out-of-process iframe. This patch changes it so that it is always
    set.
    
    BUG=712794
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2828023002
    Cr-Commit-Position: refs/heads/master@{#467130}
    
    Former-commit-id: 1931faea0d5ada216fca3c11add153ceaa82ba6d

--- third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
+++ third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
@@ -1074,6 +1074,14 @@ void WebFrameWidgetImpl::InitializeLayerTreeView() {
   if (layer_tree_view_) {
     GetPage()->LayerTreeViewInitialized(*layer_tree_view_,
                                         local_root_->GetFrame()->View());
+
+    // TODO(kenrb): Currently GPU rasterization is always enabled for OOPIFs.
+    // This is okay because it is only necessarily to set the trigger to false
+    // for certain cases that affect the top-level frame, but it would be better
+    // to be consistent with the top-level frame. Ideally the logic should
+    // be moved from WebViewImpl into WebFrameWidget and used for all local
+    // frame roots. https://crbug.com/712794
+    layer_tree_view_->HeuristicsForGpuRasterizationUpdated(true);
   }
 
   // FIXME: only unittests, click to play, Android priting, and printing (for
