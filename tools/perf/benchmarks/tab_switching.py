2dab2a8f0c91e5201b536642973ad233a2cc1f67
vovoy <vovoy@chromium.org>
2017-04-24 18:14:13 -0700

    Support flag --tabset-repeat on benchmark tab_switching
    flag --tabset-repeat multiply the tab count of benchmark tab_switching
    to measure metrics on different tab count. ex: add flag --tabset-repeat=2
    to double the tab count.
    
    BUG=689388
    
    Review-Url: https://codereview.chromium.org/2819423002
    Cr-Commit-Position: refs/heads/master@{#466852}
    
    Former-commit-id: ef1307664210fb58e9b9fb85586704ca1d5e59bc

--- tools/perf/benchmarks/tab_switching.py
+++ tools/perf/benchmarks/tab_switching.py
@@ -2,11 +2,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import os
+
 from core import perf_benchmark
 
 from measurements import tab_switching
-import page_sets
+from page_sets.system_health import multi_tab_stories
 from telemetry import benchmark
+from telemetry import story
 
 
 @benchmark.Owner(emails=['vovoy@chromium.org'],
@@ -24,9 +27,19 @@ class TabSwitchingTypical25(perf_benchmark.PerfBenchmark):
   """
   test = tab_switching.TabSwitching
 
+  @classmethod
+  def AddBenchmarkCommandLineArgs(cls, parser):
+    parser.add_option('--tabset-repeat', type='int', default=1,
+                      help='repeat tab page set')
+
   def CreateStorySet(self, options):
-    return page_sets.SystemHealthStorySet(platform='desktop',
-                                          case='multitab:misc')
+    story_set = story.StorySet(
+        archive_data_file='../page_sets/data/system_health_desktop.json',
+        base_dir=os.path.dirname(os.path.abspath(__file__)),
+        cloud_storage_bucket=story.PARTNER_BUCKET)
+    story_set.AddStory(multi_tab_stories.MultiTabTypical24Story(
+        story_set, False, options.tabset_repeat))
+    return story_set
 
   @classmethod
   def Name(cls):
--- tools/perf/page_sets/system_health/multi_tab_stories.py
+++ tools/perf/page_sets/system_health/multi_tab_stories.py
@@ -15,18 +15,23 @@ from telemetry import benchmark
 class MultiTabStory(system_health_story.SystemHealthStory):
   ABSTRACT_STORY = True
 
+  def __init__(self, story_set, take_memory_measurement, tabset_repeat=1):
+    super(MultiTabStory, self).__init__(story_set, take_memory_measurement)
+    self._tabset_repeat = tabset_repeat
+
   def RunNavigateSteps(self, action_runner):
     tabs = action_runner.tab.browser.tabs
 
     # No need to create the first tab as there is already one
     # when the browser is ready,
-    if self.URL_LIST:
-      action_runner.Navigate(self.URL_LIST[0])
-    for url in self.URL_LIST[1:]:
+    url_list = self.URL_LIST * self._tabset_repeat
+    if url_list:
+      action_runner.Navigate(url_list[0])
+    for url in url_list[1:]:
       new_tab = tabs.New()
       new_tab.action_runner.Navigate(url)
 
-    for i, url in enumerate(self.URL_LIST):
+    for i, url in enumerate(url_list):
       try:
         tabs[i].action_runner.WaitForNetworkQuiescence()
       except py_utils.TimeoutException:
