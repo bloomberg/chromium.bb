5434b4f495f0dce7ce47cbe9224e7369036b32b3
wolenetz <wolenetz@chromium.org>
2017-04-26 11:33:57 -0700

    Fix audio keyframe comment in MSE MP4 parser
    Trivial change to reflect the actual logic and intent already in the
    code.
    
    BUG=710692
    R=chcunningham@chromium.org
    
    Review-Url: https://codereview.chromium.org/2835163004
    Cr-Commit-Position: refs/heads/master@{#467388}
    
    Former-commit-id: 8ab986c77a7fae2cbe9c63a50c08246ceb2f3e04

--- media/formats/mp4/track_run_iterator.cc
+++ media/formats/mp4/track_run_iterator.cc
@@ -169,11 +169,11 @@ static bool PopulateSampleInfo(const TrackExtends& trex,
   // that marks non-key video frames as sync samples (http://crbug.com/507916
   // and http://crbug.com/310712). Hence, for video we additionally check that
   // the sample does not depend on others (FFmpeg does too, see mov_read_trun).
-  // Sample dependency is not ignored for audio because encoded audio samples
-  // can depend on other samples and still be used for random access. Generally
-  // all audio samples are expected to be sync samples, but we  prefer to check
-  // the flags to catch badly muxed audio (for now anyway ;P). History of
-  // attempts to get this right discussed in http://crrev.com/1319813002
+  // Sample dependency is ignored for audio because encoded audio samples can
+  // depend on other samples and still be used for random access. Generally all
+  // audio samples are expected to be sync samples, but we  prefer to check the
+  // flags to catch badly muxed audio (for now anyway ;P). History of attempts
+  // to get this right discussed in http://crrev.com/1319813002
   bool sample_is_sync_sample = !(flags & kSampleIsNonSyncSample);
   bool sample_depends_on_others = sample_depends_on == kSampleDependsOnOthers;
   sample_info->is_keyframe = sample_is_sync_sample &&
