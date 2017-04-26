b7ce373c47b3d8f4209a1171d260a443e6b62ead
erg <erg@chromium.org>
2017-04-26 10:16:39 -0700

    Delete orphaned uma metric MPArch.RWH_DidUpdateBackingStore.
    This isn't in histograms.xml so nothing is done with it.
    
    BUG=642284
    
    Review-Url: https://codereview.chromium.org/2836413002
    Cr-Commit-Position: refs/heads/master@{#467359}
    
    Former-commit-id: fd927edcf1208e1ddd26faed7dfdcba6daca133c

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1988,7 +1988,6 @@ void RenderWidgetHostImpl::DidUpdateBackingStore(
     const ViewHostMsg_UpdateRect_Params& params,
     const TimeTicks& paint_start) {
   TRACE_EVENT0("renderer_host", "RenderWidgetHostImpl::DidUpdateBackingStore");
-  TimeTicks update_start = TimeTicks::Now();
 
   NotificationService::current()->Notify(
       NOTIFICATION_RENDER_WIDGET_HOST_DID_UPDATE_BACKING_STORE,
@@ -2006,11 +2005,6 @@ void RenderWidgetHostImpl::DidUpdateBackingStore(
       ViewHostMsg_UpdateRect_Flags::is_resize_ack(params.flags);
   if (is_resize_ack)
     WasResized();
-
-  // Log the time delta for processing a paint message.
-  TimeTicks now = TimeTicks::Now();
-  TimeDelta delta = now - update_start;
-  UMA_HISTOGRAM_TIMES("MPArch.RWH_DidUpdateBackingStore", delta);
 }
 
 void RenderWidgetHostImpl::OnQueueSyntheticGesture(
