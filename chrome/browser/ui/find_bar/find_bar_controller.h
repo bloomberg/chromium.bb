fe4187bec0f038c1afd208ddff10a9192623db75
siggi <siggi@chromium.org>
2017-04-26 12:41:45 -0700

    Fix extraneous beeps on empty page search.
    As of https://crrev.com/d5ca2524c54035fd979bccf35cec094dc69647ee [formerly 3ac612d592cd42355a432c8d307051d299d79768] a
    find-in-page can issue multiple "final updates" if the document changes.
    Each time a final update with zero results occurs, there is an audbible
    notification which is a nuisance beyond the first one. Particularly
    bothersome is that most or all pages cause a ding as you close them if
    they have an active find in page with zero results.
    
    BUG=682299
    
    Review-Url: https://codereview.chromium.org/2839713002
    Cr-Commit-Position: refs/heads/master@{#467417}
    
    Former-commit-id: 59192dd2dd5d23372ca3ace498327f1c690e61ac

--- chrome/browser/ui/find_bar/find_bar_controller.h
+++ chrome/browser/ui/find_bar/find_bar_controller.h
@@ -99,6 +99,12 @@ class FindBarController : public content::NotificationObserver {
   // UpdateFindBarForCurrentResult to avoid flickering.
   int last_reported_matchcount_;
 
+  // Used to keep track of whether the user has been notified that the find came
+  // up empty. A single find session can result in multiple final updates, if
+  // the document contents change dynamically. It's a nuisance to notify the
+  // user more than once that a search came up empty, however.
+  base::string16 alerted_search_;
+
   DISALLOW_COPY_AND_ASSIGN(FindBarController);
 };
 
