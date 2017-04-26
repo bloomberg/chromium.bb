715b670773d31675cd940dbb2f33e28c3a4274dd
boliu <boliu@chromium.org>
2017-04-26 14:26:20 -0700

    Remove post_launch_on_client_thread_called
    This is a follow up to refs/heads/master@{#465155}. Android now always
    calls OnChildProcessStarted on the launcher thread, which means the
    complexity to deal with the call directly happening on the client thread
    can be removed.
    
    BUG=689758
    
    Review-Url: https://codereview.chromium.org/2843063002
    Cr-Commit-Position: refs/heads/master@{#467463}
    
    Former-commit-id: 213830d7d68eefddedc6199bf699dfac3d5481e8

--- content/browser/child_process_launcher_helper.h
+++ content/browser/child_process_launcher_helper.h
@@ -122,14 +122,10 @@ class ChildProcessLauncherHelper :
       const base::LaunchOptions& options);
 
   // Called once the process has been created, successfully or not.
-  // If |post_launch_on_client_thread_called| is false,
-  // this calls PostLaunchOnClientThread on the client thread.
   void PostLaunchOnLauncherThread(ChildProcessLauncherHelper::Process process,
-                                  int launch_result,
-                                  bool post_launch_on_client_thread_called);
+                                  int launch_result);
 
-  // Note that this could be called before PostLaunchOnLauncherThread() is
-  // called.
+  // Posted by PostLaunchOnLauncherThread onto the client thread.
   void PostLaunchOnClientThread(ChildProcessLauncherHelper::Process process,
                                 int error_code);
 
