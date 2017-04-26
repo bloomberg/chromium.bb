8c7d61e75eb47f36261a16bd2845a9c3dff72ec4
thomasanderson <thomasanderson@google.com>
2017-04-26 01:50:15 -0700

    Linux sandbox: handle ENOSPC clone failure
    Linux 4.9 adds /proc/sys/user/max_user_namespaces to limit the number
    of concurrent user namespaces.  Sometimes the limit is set to 0 in
    which case clone() will return ENOSPC.  This CL avoids PCHECK()ing in
    that case.
    
    BUG=715138
    R=mdempsky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2842033002
    Cr-Commit-Position: refs/heads/master@{#467271}
    
    Former-commit-id: aebec0b4680564dfd5184cb288c07a2fd0ca50bd

--- sandbox/linux/services/credentials.cc
+++ sandbox/linux/services/credentials.cc
@@ -132,9 +132,10 @@ bool ChrootToSafeEmptyDir() {
 void CheckCloneNewUserErrno(int error) {
   // EPERM can happen if already in a chroot. EUSERS if too many nested
   // namespaces are used. EINVAL for kernels that don't support the feature.
-  // Valgrind will ENOSYS unshare().
+  // Valgrind will ENOSYS unshare().  ENOSPC can occur when the system has
+  // reached its maximum configured number of user namespaces.
   PCHECK(error == EPERM || error == EUSERS || error == EINVAL ||
-         error == ENOSYS);
+         error == ENOSYS || error == ENOSPC);
 }
 
 // Converts a Capability to the corresponding Linux CAP_XXX value.
