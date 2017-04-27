e328cab47f91872270ae5b236978dc8f635dcfb5
moshayedi <moshayedi@chromium.org>
2017-04-27 08:31:23 -0700

    IME for Mus: Structs and StructTraits for IME Candidate Window.
    This CL adds ui.mojom.CandidateWindowProperties and
    ui.mojom.CandidateWindowEntry and corresponding struct traits.
    
    BUG=637416, 672587
    
    Review-Url: https://codereview.chromium.org/2839243002
    Cr-Commit-Position: refs/heads/master@{#467686}
    
    Former-commit-id: b69ccf6f92a78f8dc097466d8132b7ccfd000950

--- services/ui/public/interfaces/ime/ime_struct_traits.h
+++ services/ui/public/interfaces/ime/ime_struct_traits.h
@@ -7,12 +7,71 @@
 
 #include "base/strings/utf_string_conversions.h"
 #include "services/ui/public/interfaces/ime/ime.mojom-shared.h"
+#include "ui/base/ime/candidate_window.h"
 #include "ui/base/ime/composition_text.h"
 #include "ui/base/ime/composition_underline.h"
 #include "ui/base/ime/text_input_mode.h"
 #include "ui/base/ime/text_input_type.h"
 
 namespace mojo {
+
+template <>
+struct StructTraits<ui::mojom::CandidateWindowPropertiesDataView,
+                    ui::CandidateWindow::CandidateWindowProperty> {
+  static int32_t page_size(
+      const ui::CandidateWindow::CandidateWindowProperty& p) {
+    return p.page_size;
+  }
+  static bool vertical(const ui::CandidateWindow::CandidateWindowProperty& p) {
+    return p.is_vertical;
+  }
+  static std::string auxiliary_text(
+      const ui::CandidateWindow::CandidateWindowProperty& p) {
+    return p.auxiliary_text;
+  }
+  static bool auxiliary_text_visible(
+      const ui::CandidateWindow::CandidateWindowProperty& p) {
+    return p.is_auxiliary_text_visible;
+  }
+  static int32_t cursor_position(
+      const ui::CandidateWindow::CandidateWindowProperty& p) {
+    return p.cursor_position;
+  }
+  static bool cursor_visible(
+      const ui::CandidateWindow::CandidateWindowProperty& p) {
+    return p.is_cursor_visible;
+  }
+  static ui::mojom::CandidateWindowPosition window_position(
+      const ui::CandidateWindow::CandidateWindowProperty& p) {
+    return p.show_window_at_composition
+               ? ui::mojom::CandidateWindowPosition::kComposition
+               : ui::mojom::CandidateWindowPosition::kCursor;
+  }
+  static bool Read(ui::mojom::CandidateWindowPropertiesDataView data,
+                   ui::CandidateWindow::CandidateWindowProperty* out);
+};
+
+template <>
+struct StructTraits<ui::mojom::CandidateWindowEntryDataView,
+                    ui::CandidateWindow::Entry> {
+  static std::string value(const ui::CandidateWindow::Entry& e) {
+    return base::UTF16ToUTF8(e.value);
+  }
+  static std::string label(const ui::CandidateWindow::Entry& e) {
+    return base::UTF16ToUTF8(e.label);
+  }
+  static std::string annotation(const ui::CandidateWindow::Entry& e) {
+    return base::UTF16ToUTF8(e.annotation);
+  }
+  static std::string description_title(const ui::CandidateWindow::Entry& e) {
+    return base::UTF16ToUTF8(e.description_title);
+  }
+  static std::string description_body(const ui::CandidateWindow::Entry& e) {
+    return base::UTF16ToUTF8(e.description_body);
+  }
+  static bool Read(ui::mojom::CandidateWindowEntryDataView data,
+                   ui::CandidateWindow::Entry* out);
+};
 
 template <>
 struct StructTraits<ui::mojom::CompositionUnderlineDataView,
