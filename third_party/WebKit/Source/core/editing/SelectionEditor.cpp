8a6c47565a6d3e06b6b3dfb98d0a83a79a34e7fb
xiaochengh <xiaochengh@chromium.org>
2017-04-27 08:04:10 -0700

    Stop flat tree selection canonicalization from using invalid positions
    There are some valid DOM positions (*) that do not have corresponding
    valid flat tree positions. This patch adds special handling of such
    DOM positions, so that when computing VisibleSelectionInFlatTree from
    SelectionInDOMTree, such positions are converted to NULL instead of
    invalid flat tree positions, so that the renderer does not crash.
    
    (*) If NODE is a direct child of a shadow host but is not distributed
    into the flat tree, NODE@BeforeAnchor and NODE@AfterAnchor are valid
    Position but invalid PositionInFlatTree. This patch handles these two
    kind of positions.
    
    BUG=702756, 709872, 712984
    TEST=FrameSelectionTest.SelectInvalidPositionInFlatTreeDoesntCrash
    
    Review-Url: https://codereview.chromium.org/2850443002
    Cr-Commit-Position: refs/heads/master@{#467676}
    
    Former-commit-id: 0cbb9fc2999813a837b8ee0483f77e3879cc25e0

--- third_party/WebKit/Source/core/editing/SelectionEditor.cpp
+++ third_party/WebKit/Source/core/editing/SelectionEditor.cpp
@@ -405,15 +405,21 @@ void SelectionEditor::UpdateCachedVisibleSelectionInFlatTreeIfNeeded() const {
     return;
   style_version_for_flat_tree_ = GetDocument().StyleVersion();
   cached_visible_selection_in_flat_tree_is_dirty_ = false;
-  cached_visible_selection_in_flat_tree_ = CreateVisibleSelection(
-      SelectionInFlatTree::Builder()
-          .SetBaseAndExtent(ToPositionInFlatTree(selection_.Base()),
-                            ToPositionInFlatTree(selection_.Extent()))
-          .SetAffinity(selection_.Affinity())
-          .SetHasTrailingWhitespace(selection_.HasTrailingWhitespace())
-          .SetGranularity(selection_.Granularity())
-          .SetIsDirectional(selection_.IsDirectional())
-          .Build());
+  SelectionInFlatTree::Builder builder;
+  const PositionInFlatTree& base = ToPositionInFlatTree(selection_.Base());
+  const PositionInFlatTree& extent = ToPositionInFlatTree(selection_.Extent());
+  if (base.IsNotNull() && extent.IsNotNull())
+    builder.SetBaseAndExtent(base, extent);
+  else if (base.IsNotNull())
+    builder.Collapse(base);
+  else if (extent.IsNotNull())
+    builder.Collapse(extent);
+  builder.SetAffinity(selection_.Affinity())
+      .SetHasTrailingWhitespace(selection_.HasTrailingWhitespace())
+      .SetGranularity(selection_.Granularity())
+      .SetIsDirectional(selection_.IsDirectional());
+  cached_visible_selection_in_flat_tree_ =
+      CreateVisibleSelection(builder.Build());
   if (!cached_visible_selection_in_flat_tree_.IsNone())
     return;
   style_version_for_dom_tree_ = GetDocument().StyleVersion();
