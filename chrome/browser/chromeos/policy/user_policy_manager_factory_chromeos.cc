5741a926ebb112c949ba33115ac171294d9b85f1
fdoray <fdoray@chromium.org>
2017-04-27 10:50:41 -0700

    Use TaskScheduler instead of blocking pool in user_policy_manager_factory_chromeos.cc.
    The blocking pool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=bartfab@chromium.org
    
    Review-Url: https://codereview.chromium.org/2834413003
    Cr-Commit-Position: refs/heads/master@{#467728}
    
    Former-commit-id: a9e5023d954f033d7720a91cea36ec15aea73a7c

--- chrome/browser/chromeos/policy/user_policy_manager_factory_chromeos.cc
+++ chrome/browser/chromeos/policy/user_policy_manager_factory_chromeos.cc
@@ -14,7 +14,7 @@
 #include "base/memory/ref_counted.h"
 #include "base/path_service.h"
 #include "base/sequenced_task_runner.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "chrome/browser/browser_process.h"
@@ -240,8 +240,12 @@ UserPolicyManagerFactoryChromeOS::CreateManagerForProfile(
           is_active_directory);
 
   scoped_refptr<base::SequencedTaskRunner> backend_task_runner =
-      content::BrowserThread::GetBlockingPool()->GetSequencedTaskRunner(
-          content::BrowserThread::GetBlockingPool()->GetSequenceToken());
+      base::CreateSequencedTaskRunnerWithTraits(
+          base::TaskTraits()
+              .MayBlock()
+              .WithPriority(base::TaskPriority::BACKGROUND)
+              .WithShutdownBehavior(
+                  base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN));
   scoped_refptr<base::SequencedTaskRunner> io_task_runner =
       content::BrowserThread::GetTaskRunnerForThread(
           content::BrowserThread::IO);
