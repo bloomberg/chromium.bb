b5c11decda9ee555eef3388f0e3704a81c6c103d
zijiehe <zijiehe@chromium.org>
2017-04-27 19:16:05 -0700

    [Chromoting] Retrieve process resource usage (ProcessStats) and its tests
    ProcessStatsStub is an interface to receive process resource usage information
    from a ProcessStats.
    
    ProcessResourceUsage is a structure to store the resource usage of one process.
    AggregatedProcessResourceUsage is a structure to store the resource usgaes of
    several processes.
    
    ProcessStatsAgent is an interface to retrieve resource usage of one process.
    CurrentProcessStatsAgent is a ProcessStatsAgent implementation to retrieve
    resource usage from current process.
    ForwardProcessStatsAgent receives resource usage from a third-party and forwards
    the latest usage as a ProcessStatsAgent.
    
    ProcessStatsSender is a class to regularly report resource usages from various
    ProcessStatsAgent implementations to a ProcessStatsStub. It merges several
    non-empty ProcessResourceUsage into one AggregatedProcessResourceUsage.
    This is typical useful on Windows: we need to calculate the resource usages from
    both network process and desktop process.
    
    R=sergeyu@chromium.org, jamiewalch@chromium.org
    BUG=650926
    
    Review-Url: https://codereview.chromium.org/2775983003
    Cr-Original-Commit-Position: refs/heads/master@{#467753}
    Committed: https://chromium.googlesource.com/chromium/src/+/a581643d33c5eb832b724fe47c6720fb49f4f20e [formerly 5928f440f84065ab45f848dcd135e3c2f52ca5bb]
    Review-Url: https://codereview.chromium.org/2775983003
    Cr-Commit-Position: refs/heads/master@{#467844}
    
    Former-commit-id: 4f3f095f04100e1e443cfdcba1813bdaa625bc77

---
+++ remoting/host/process_stats_sender.cc
@@ -0,0 +1,51 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/process_stats_sender.h"
+
+#include <utility>
+
+#include "base/location.h"
+#include "base/logging.h"
+#include "remoting/host/process_stats_agent.h"
+#include "remoting/host/process_stats_util.h"
+
+namespace remoting {
+
+ProcessStatsSender::ProcessStatsSender(
+    protocol::ProcessStatsStub* host_stats_stub,
+    base::TimeDelta interval,
+    std::initializer_list<ProcessStatsAgent*> agents)
+    : host_stats_stub_(host_stats_stub),
+      agents_(agents),
+      thread_checker_() {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  DCHECK(host_stats_stub_);
+  DCHECK(!interval.is_zero());
+  DCHECK(!agents_.empty());
+
+  timer_.Start(FROM_HERE, interval, this, &ProcessStatsSender::ReportUsage);
+}
+
+ProcessStatsSender::~ProcessStatsSender() {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  timer_.Stop();
+}
+
+void ProcessStatsSender::ReportUsage() {
+  DCHECK(thread_checker_.CalledOnValidThread());
+
+  std::vector<protocol::ProcessResourceUsage> usages;
+  for (auto* const agent : agents_) {
+    DCHECK(agent);
+    protocol::ProcessResourceUsage usage = agent->GetResourceUsage();
+    if (!IsEmptyProcessResourceUsage(usage)) {
+      usages.push_back(std::move(usage));
+    }
+  }
+
+  host_stats_stub_->OnProcessStats(AggregateProcessResourceUsage(usages));
+}
+
+}  // namespace remoting
