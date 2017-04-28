502348fae17bd16550173d6ca08136dff1a5941f
Chris Kuiper <ckuiper@google.com>
2017-04-28 13:59:25 -0700

    [Chromecast] Initial commit of libcastmedia support for Android
    This is the first set of changes to add Android support for
    cast_media. It consists of files copied from the alsa/ folder and
    tweaked for Android. Stub functions are used at the end of the
    pipeline, so nothing is actually played out yet. The next change set
    will contain the plumbing to Android's AudioTrack.
    
    The easiest way to review this is to checkout the change and diff the
    files against their alsa/ counterparts. Very little was changed
    actually, so the diffs are easy to follow. At some point we should
    consider sharing this code between alsa/ and android/.
    
    Bug: internal b/37624516
    Test: fling to device, verifying logs
    
    Change-Id: Ic923a0242111730aa50a7526513d83834dd85884
    Reviewed-on: https://chromium-review.googlesource.com/518551
    Commit-Queue: Chris Kuiper <ckuiper@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Dan Sanders <sandersd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477028}
    Former-commit-id: 636fa0bcfb72e8e7ec0486bcbabe4b6f818cd130

---
+++ chromecast/media/cma/backend/android/audio_sink_android.cc
@@ -0,0 +1,45 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromecast/media/cma/backend/android/audio_sink_android.h"
+
+#include <string>
+#include <utility>
+
+#include "chromecast/media/cma/backend/android/audio_sink_android_dummy_impl.h"
+
+namespace chromecast {
+namespace media {
+
+AudioSinkAndroid::AudioSinkAndroid(Delegate* delegate,
+                                   int samples_per_second,
+                                   bool primary,
+                                   const std::string& device_id,
+                                   AudioContentType content_type) {
+  impl_.reset(new AudioSinkAndroidDummyImpl(delegate, samples_per_second,
+                                            primary, device_id, content_type));
+}
+
+AudioSinkAndroid::~AudioSinkAndroid() {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  impl_->PreventDelegateCalls();
+}
+
+void AudioSinkAndroid::WritePcm(const scoped_refptr<DecoderBufferBase>& data) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  impl_->WritePcm(data);
+}
+
+void AudioSinkAndroid::SetPaused(bool paused) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  impl_->SetPaused(paused);
+}
+
+void AudioSinkAndroid::SetVolumeMultiplier(float multiplier) {
+  DCHECK(thread_checker_.CalledOnValidThread());
+  impl_->SetVolumeMultiplier(multiplier);
+}
+
+}  // namespace media
+}  // namespace chromecast
---
+++ chromecast/media/cma/backend/android/audio_sink_android.h
@@ -0,0 +1,83 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_MEDIA_CMA_BACKEND_ANDROID_AUDIO_SINK_ANDROID_H_
+#define CHROMECAST_MEDIA_CMA_BACKEND_ANDROID_AUDIO_SINK_ANDROID_H_
+
+#include <memory>
+#include <string>
+
+#include "base/macros.h"
+#include "base/memory/ref_counted.h"
+#include "base/threading/thread_checker.h"
+#include "chromecast/media/cma/backend/android/media_pipeline_backend_android.h"
+#include "chromecast/public/media/media_pipeline_device_params.h"
+#include "chromecast/public/volume_control.h"
+
+namespace chromecast {
+namespace media {
+
+class DecoderBufferBase;
+class AudioSinkAndroidDummyImpl;
+
+// Input handle to the sink. All methods (including constructor and destructor)
+// must be called on the same thread.
+class AudioSinkAndroid {
+ public:
+  enum class SinkError {
+    // This input is being ignored due to a sample rate changed.
+    kInputIgnored,
+    // An internal sink error occurred. The input is no longer usable.
+    kInternalError,
+  };
+
+  class Delegate {
+   public:
+    using SinkError = AudioSinkAndroid::SinkError;
+
+    // Called when the last data passed to WritePcm() has been successfully
+    // added to the queue.
+    virtual void OnWritePcmCompletion(
+        MediaPipelineBackendAndroid::BufferStatus status,
+        const MediaPipelineBackendAndroid::RenderingDelay& delay) = 0;
+
+    // Called when a sink error occurs. No further data should be written.
+    virtual void OnSinkError(SinkError error) = 0;
+
+   protected:
+    virtual ~Delegate() {}
+  };
+
+  AudioSinkAndroid(Delegate* delegate,
+                   int samples_per_second,
+                   bool primary,
+                   const std::string& device_id,
+                   AudioContentType content_type);
+  ~AudioSinkAndroid();
+
+  // Writes some PCM data to the sink. |data| must be in planar float format.
+  // Once the data has been written, the delegate's OnWritePcmCompletion()
+  // method will be called on the same thread that the AudioSinkAndroid was
+  // created on. Note that no further calls to WritePcm() should be made until
+  // OnWritePcmCompletion() has been called.
+  void WritePcm(const scoped_refptr<DecoderBufferBase>& data);
+
+  // Pauses/unpauses this input.
+  void SetPaused(bool paused);
+
+  // Sets the volume multiplier for this input. If |multiplier| is outside the
+  // range [0.0, 1.0], it is clamped to that range.
+  void SetVolumeMultiplier(float multiplier);
+
+ private:
+  std::unique_ptr<AudioSinkAndroidDummyImpl> impl_;
+  base::ThreadChecker thread_checker_;
+
+  DISALLOW_COPY_AND_ASSIGN(AudioSinkAndroid);
+};
+
+}  // namespace media
+}  // namespace chromecast
+
+#endif  // CHROMECAST_MEDIA_CMA_BACKEND_ANDROID_AUDIO_SINK_ANDROID_H_
