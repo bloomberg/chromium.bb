5d22b752ea78a6b7dc07956950b02ab245856e5a
dtapuska <dtapuska@chromium.org>
2017-04-28 10:34:06 -0700

    Cleanup determining ibeam for node.
    Change
    https://chromium.googlesource.com/chromium/src/+/ee2abfad5f8d82b8ec2079df077084efaf0352fa [formerly d892f9592860691ae9a782c12260c94ed6bd1a63]%5E%21/#F30
    
    caused the layout object to possibly be invalidated. Collapse the code
    so that LayoutObject is correclty scoped so we don't run this risk.
    
    BUG=712459
    
    Review-Url: https://codereview.chromium.org/2849883002
    Cr-Commit-Position: refs/heads/master@{#468045}
    
    Former-commit-id: 9d6b6c40b933564260a0a70567b90ac30656222a

--- third_party/WebKit/Source/core/input/EventHandler.cpp
+++ third_party/WebKit/Source/core/input/EventHandler.cpp
@@ -118,6 +118,26 @@ bool ShouldRefetchEventTarget(const MouseEventWithHitTestResults& mev) {
          isHTMLInputElement(ToShadowRoot(target_node)->host());
 }
 
+bool ShouldShowIBeamForNode(const Node* node, const HitTestResult& result) {
+  if (!node)
+    return false;
+
+  bool layout_object_selectable = false;
+  if (LayoutObject* layout_object = node->GetLayoutObject()) {
+    PaintLayer* layer = layout_object->EnclosingLayer();
+    if (layer->GetScrollableArea() &&
+        layer->GetScrollableArea()->IsPointInResizeControl(
+            result.RoundedPointInMainFrame(), kResizerForPointer)) {
+      return false;
+    }
+
+    layout_object_selectable =
+        layout_object->IsText() && node->CanStartSelection();
+  }
+
+  return HasEditableStyle(*node) || layout_object_selectable;
+}
+
 }  // namespace
 
 using namespace HTMLNames;
@@ -537,26 +557,16 @@ OptionalCursor EventHandler::SelectAutoCursor(const HitTestResult& result,
     return PointerCursor();
   }
 
-  bool editable = (node && HasEditableStyle(*node));
-
   const bool is_over_link =
       !GetSelectionController().MouseDownMayStartSelect() &&
       result.IsOverLink();
   if (UseHandCursor(node, is_over_link))
     return HandCursor();
 
-  bool in_resizer = false;
-  LayoutObject* layout_object = node ? node->GetLayoutObject() : nullptr;
-  if (layout_object && frame_->View()) {
-    PaintLayer* layer = layout_object->EnclosingLayer();
-    in_resizer = layer->GetScrollableArea() &&
-                 layer->GetScrollableArea()->IsPointInResizeControl(
-                     result.RoundedPointInMainFrame(), kResizerForPointer);
-  }
-
   // During selection, use an I-beam no matter what we're over.
   // If a drag may be starting or we're capturing mouse events for a particular
-  // node, don't treat this as a selection.
+  // node, don't treat this as a selection. Note calling
+  // ComputeVisibleSelectionInDOMTreeDeprecated may update layout.
   if (mouse_event_manager_->MousePressed() &&
       GetSelectionController().MouseDownMayStartSelect() &&
       !mouse_event_manager_->MouseDownMayStartDrag() &&
@@ -567,9 +577,7 @@ OptionalCursor EventHandler::SelectAutoCursor(const HitTestResult& result,
     return i_beam;
   }
 
-  if ((editable || (layout_object && layout_object->IsText() &&
-                    node->CanStartSelection())) &&
-      !in_resizer && !result.GetScrollbar())
+  if (ShouldShowIBeamForNode(node, result))
     return i_beam;
   return PointerCursor();
 }
