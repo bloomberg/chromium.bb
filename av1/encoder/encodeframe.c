bf6ad90073fecb7bbcb789f8ced34ad1c30e934a
Yaowu Xu <yaowu@google.com>
2017-04-28 11:03:33 -0700

    Align tile_data to 32 byte boundary
    Change-Id: I3cd7a4598535023b97f2df772959805a9b13dd62

--- av1/encoder/encodeframe.c
+++ av1/encoder/encodeframe.c
@@ -4894,8 +4894,9 @@ void av1_init_tile_data(AV1_COMP *cpi) {
 
   if (cpi->tile_data == NULL || cpi->allocated_tiles < tile_cols * tile_rows) {
     if (cpi->tile_data != NULL) aom_free(cpi->tile_data);
-    CHECK_MEM_ERROR(cm, cpi->tile_data, aom_malloc(tile_cols * tile_rows *
-                                                   sizeof(*cpi->tile_data)));
+    CHECK_MEM_ERROR(
+        cm, cpi->tile_data,
+        aom_memalign(32, tile_cols * tile_rows * sizeof(*cpi->tile_data)));
     cpi->allocated_tiles = tile_cols * tile_rows;
 
     for (tile_row = 0; tile_row < tile_rows; ++tile_row)
