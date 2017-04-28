805f4cb188b1d600dddb05e0075a9ea576b666a8
slan <slan@chromium.org>
2017-04-28 08:09:38 -0700

    Reland "[Chromecast] Use base::FeatureList to control features."
    This feature was reverted due to the new browsertest being flaky on
    internal Cast infrastructure: crrev.com/2838813003
    
    === Original Commit Message ===
    In Chromium, Finch-enabled features are controlled through base::FeatureList,
    a class which abstracts the experiment framework and developer overrides
    from client code. Though Chromecast's experiment framework is fundamentally
    different (in that it is server-driven) Cast builds can still make use of
    this class. Introduce some utilities to help.
    
    At boot-up, the pref store will be queried for experiment configs, which
    were cached to disk on the most recent config fetch from the last boot
    cycle. If a developer overrides these features from the command line,
    that value takes precedence. These features will be used to initialize
    base::FeatureList, which can then be statically queried from any client
    code that depends on //base.
    
    This patch does not actually introduce or convert any existing features
    to use this framework.
    
    BUG=714291
    BUG= internal b/35424335
    
    Review-Url: https://codereview.chromium.org/2836263003
    Cr-Commit-Position: refs/heads/master@{#467998}
    
    Former-commit-id: aab6bf24d32d931359b2d0c25fb39b3ed51fbc86

---
+++ chromecast/base/cast_features.h
@@ -0,0 +1,52 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMECAST_BASE_CAST_FEATURES_H_
+#define CHROMECAST_BASE_CAST_FEATURES_H_
+
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <unordered_set>
+
+#include "base/feature_list.h"
+#include "base/macros.h"
+
+namespace base {
+class DictionaryValue;
+class ListValue;
+}
+
+namespace chromecast {
+
+// Initialize the global base::FeatureList instance, taking into account
+// overrides from DCS and the command line. |dcs_features| and
+// |dcs_experiment_ids| are read from the PrefService in the browser process.
+// |cmd_line_enable_features| and |cmd_line_disable_features| should be passed
+// to this function, unmodified from the command line.
+//
+// This function should be called before the browser's main loop. After this is
+// called, the other functions in this file may be called on any thread.
+void InitializeFeatureList(const base::DictionaryValue& dcs_features,
+                           const base::ListValue& dcs_experiment_ids,
+                           const std::string& cmd_line_enable_features,
+                           const std::string& cmd_line_disable_features);
+
+// Given a dictionary of features, create a copy that is ready to be persisted
+// to disk. Encodes all values as strings,  which is how the FieldTrial
+// classes expect the param data.
+base::DictionaryValue GetOverriddenFeaturesForStorage(
+    const base::DictionaryValue& features);
+
+// Query the set of experiment ids set for this run. Intended only for metrics
+// reporting. Must be called after InitializeFeatureList(). May be called on any
+// thread.
+const std::unordered_set<int32_t>& GetDCSExperimentIds();
+
+// Reset static state to ensure clean unittests. For tests only.
+void ResetCastFeaturesForTesting();
+
+}  // namespace chromecast
+
+#endif  // CHROMECAST_BASE_CAST_FEATURES_H_
