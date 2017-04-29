42b8b9c656bf1b4e6895b2eb4ba4ecda0985039b
eseckler <eseckler@chromium.org>
2017-04-29 00:07:59 -0700

    [headless] Expose DevToolsTarget via HeadlessBrowser + add tracing test.
    BUG=546953
    
    Review-Url: https://codereview.chromium.org/2840993003
    Cr-Commit-Position: refs/heads/master@{#468229}
    
    Former-commit-id: 32fdf2ad9ef36269d8c5369c4ce667a80c50d0d9

--- headless/lib/browser/headless_browser_impl.cc
+++ headless/lib/browser/headless_browser_impl.cc
@@ -13,11 +13,13 @@
 #include "base/threading/thread_task_runner_handle.h"
 #include "content/public/app/content_main.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/devtools_agent_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_switches.h"
 #include "headless/app/headless_shell_switches.h"
 #include "headless/lib/browser/headless_browser_context_impl.h"
 #include "headless/lib/browser/headless_browser_main_parts.h"
+#include "headless/lib/browser/headless_devtools_client_impl.h"
 #include "headless/lib/browser/headless_web_contents_impl.h"
 #include "headless/lib/headless_content_main_delegate.h"
 #include "net/http/http_util.h"
@@ -31,6 +33,10 @@
 #include "content/public/app/sandbox_helper_win.h"
 #include "sandbox/win/src/sandbox_types.h"
 #endif
+
+namespace content {
+class DevToolsAgentHost;
+}
 
 namespace headless {
 namespace {
@@ -67,6 +73,7 @@ HeadlessBrowserImpl::HeadlessBrowserImpl(
       options_(std::move(options)),
       browser_main_parts_(nullptr),
       default_browser_context_(nullptr),
+      agent_host_(nullptr),
       weak_ptr_factory_(this) {}
 
 HeadlessBrowserImpl::~HeadlessBrowserImpl() {}
@@ -133,6 +140,10 @@ void HeadlessBrowserImpl::set_browser_main_parts(
 }
 
 void HeadlessBrowserImpl::RunOnStartCallback() {
+  // We don't support the tethering domain on this agent host.
+  agent_host_ = content::DevToolsAgentHost::CreateForBrowser(
+      nullptr, content::DevToolsAgentHost::CreateServerSocketCallback());
+
   PlatformCreateWindow();
   on_start_callback_.Run(this);
   on_start_callback_ = base::Callback<void(HeadlessBrowser*)>();
@@ -198,6 +209,32 @@ HeadlessBrowserContext* HeadlessBrowserImpl::GetBrowserContextForId(
   if (find_it == browser_contexts_.end())
     return nullptr;
   return find_it->second.get();
+}
+
+HeadlessDevToolsTarget* HeadlessBrowserImpl::GetDevToolsTarget() {
+  return agent_host_ ? this : nullptr;
+}
+
+bool HeadlessBrowserImpl::AttachClient(HeadlessDevToolsClient* client) {
+  DCHECK(agent_host_);
+  return HeadlessDevToolsClientImpl::From(client)->AttachToHost(
+      agent_host_.get());
+}
+
+void HeadlessBrowserImpl::ForceAttachClient(HeadlessDevToolsClient* client) {
+  DCHECK(agent_host_);
+  HeadlessDevToolsClientImpl::From(client)->ForceAttachToHost(
+      agent_host_.get());
+}
+
+void HeadlessBrowserImpl::DetachClient(HeadlessDevToolsClient* client) {
+  DCHECK(agent_host_);
+  HeadlessDevToolsClientImpl::From(client)->DetachFromHost(agent_host_.get());
+}
+
+bool HeadlessBrowserImpl::IsAttached() {
+  DCHECK(agent_host_);
+  return agent_host_->IsAttached();
 }
 
 void RunChildProcessIfNeeded(int argc, const char** argv) {
