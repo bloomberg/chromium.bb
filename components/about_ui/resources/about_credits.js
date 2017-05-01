263b6d17f7ae39d71892ff29861060bb48c2dc5d
Yipeng Wang <yipengw@chromium.org>
2017-05-01 13:05:43 -0400

    Reland: [about:credits] Improve compression by sorting licenses
    Reverted in: 6eb41d1a7381f53efba5273e435f7b92d900510b [formerly ba3fb344a4ad428165507ebbd82be1b2ffe48c6c].
    
    Reason for reland: Fixed sort order when two licenses of same length exist.
    
    Restore the alphabetical order in javascript.
    
    Saves around 100k when gzip is used, but only 2k for brotli.
    Intention is to have android_webview to use the same copy
    of about:credits in a follow-up, in which case this optimization
    becomes relevant.
    
    TBR=dbeam@chromium.org
    BUG=688077
    Change-Id: I085626bc2c81933ae973fb9762b6c3694318855b
    
    Change-Id: I085626bc2c81933ae973fb9762b6c3694318855b
    Reviewed-on: https://chromium-review.googlesource.com/490718
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#468402}
    Former-commit-id: 0534ad76ecae8af1ea088bec38536de6d736c887

--- components/about_ui/resources/about_credits.js
+++ components/about_ui/resources/about_credits.js
@@ -23,6 +23,24 @@ function toggle(o) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
+  var licenseEls = [].slice.call(document.getElementsByClassName('product'));
+
+  licenseEls.sort(function(a, b) {
+    var nameA = a.getElementsByClassName('title')[0].textContent;
+    var nameB = b.getElementsByClassName('title')[0].textContent;
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+  });
+
+  var parentEl = licenseEls[0].parentNode;
+  parentEl.innerHTML = '';
+  for (var i = 0; i < licenseEls.length; i++) {
+    parentEl.appendChild(licenseEls[i]);
+  }
+
+  document.body.hidden = false;
+
   if (cr.isChromeOS) {
     var keyboardUtils = document.createElement('script');
     keyboardUtils.src = 'chrome://credits/keyboard_utils.js';
