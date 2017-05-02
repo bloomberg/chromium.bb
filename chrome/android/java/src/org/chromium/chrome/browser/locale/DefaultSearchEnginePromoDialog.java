93b290d81d560c3c987d96b71306716d737287c6
dfalcantara <dfalcantara@chromium.org>
2017-05-02 15:30:21 -0700

    🔍 Pull out default search engine dialog logic
    Refactors the DefaultSearchEnginePromoDialog so that logic for
    handling the search engine choice can be reused for First Run.
    Also adds a test suite for it.
    
    BUG=712836,712833
    
    Review-Url: https://codereview.chromium.org/2851303004
    Cr-Commit-Position: refs/heads/master@{#468797}
    
    Former-commit-id: c12d684be36eddb9dfd7022e08da1667975e6e7e

--- chrome/android/java/src/org/chromium/chrome/browser/locale/DefaultSearchEnginePromoDialog.java
+++ chrome/android/java/src/org/chromium/chrome/browser/locale/DefaultSearchEnginePromoDialog.java
@@ -8,26 +8,18 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.view.View;
 import android.widget.Button;
-import android.widget.RadioGroup;
-import android.widget.RadioGroup.OnCheckedChangeListener;
 
 import org.chromium.base.Callback;
 import org.chromium.base.library_loader.LibraryLoader;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.locale.LocaleManager.SearchEnginePromoType;
 import org.chromium.chrome.browser.search_engines.TemplateUrlService;
-import org.chromium.chrome.browser.search_engines.TemplateUrlService.TemplateUrl;
 import org.chromium.chrome.browser.widget.PromoDialog;
 import org.chromium.chrome.browser.widget.RadioButtonLayout;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 /** A dialog that forces the user to choose a default search engine. */
-public class DefaultSearchEnginePromoDialog extends PromoDialog implements OnCheckedChangeListener {
+public class DefaultSearchEnginePromoDialog extends PromoDialog {
     /** Used to determine the promo dialog contents. */
     @SearchEnginePromoType
     private final int mDialogType;
@@ -35,8 +27,8 @@ public class DefaultSearchEnginePromoDialog extends PromoDialog implements OnChe
     /** Called when the dialog is dismissed after the user has chosen a search engine. */
     private final Callback<Boolean> mOnDismissed;
 
-    private RadioGroup mRadioGroup;
-    private String mSelectedKeyword;
+    /** Encapsulates most of the logic for filling the dialog and handling clicks. */
+    private DefaultSearchEngineDialogHelper mHelper;
 
     /**
      * Construct and show the dialog.  Will be asynchronous if the TemplateUrlService has not yet
@@ -87,75 +79,30 @@ public class DefaultSearchEnginePromoDialog extends PromoDialog implements OnChe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Determine what search engines will be listed.
-        TemplateUrlService instance = TemplateUrlService.getInstance();
-        assert instance.isLoaded();
-        List<TemplateUrl> engines = null;
-        if (mDialogType == LocaleManager.SEARCH_ENGINE_PROMO_SHOW_EXISTING) {
-            engines = instance.getSearchEngines();
-        } else {
-            // TODO(dfalcantara): Handle the new user case.
-            assert false;
-        }
-
-        // Shuffle up the engines.
-        List<CharSequence> engineNames = new ArrayList<>();
-        List<String> engineKeywords = new ArrayList<>();
-        Collections.shuffle(engines);
-        for (int i = 0; i < engines.size(); i++) {
-            TemplateUrl engine = engines.get(i);
-            engineNames.add(engine.getShortName());
-            engineKeywords.add(engine.getKeyword());
-        }
-
-        // Add the search engines to the dialog.
+        Button okButton = (Button) findViewById(R.id.button_primary);
         RadioButtonLayout radioButtons = new RadioButtonLayout(getContext());
-        radioButtons.addOptions(engineNames, engineKeywords);
-        radioButtons.setOnCheckedChangeListener(this);
         addControl(radioButtons);
 
-        Button button = (Button) findViewById(R.id.button_primary);
-        button.setOnClickListener(this);
-        updateButtonState();
-    }
-
-    @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        mSelectedKeyword = (String) group.findViewById(checkedId).getTag();
-        updateButtonState();
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.button_primary) {
-            if (mSelectedKeyword == null) {
-                updateButtonState();
-                return;
+        Runnable dismissRunnable = new Runnable() {
+            @Override
+            public void run() {
+                dismiss();
             }
-
-            dismiss();
-        } else {
-            assert false : "Unhandled click.";
-        }
-
-        // Don't propagate the click to the parent to prevent circumventing the dialog.
+        };
+        mHelper = new DefaultSearchEngineDialogHelper(
+                mDialogType, radioButtons, okButton, dismissRunnable);
     }
 
     @Override
     public void onDismiss(DialogInterface dialog) {
-        if (mSelectedKeyword == null) {
+        if (mHelper.getCurrentlySelectedKeyword() == null) {
             // This shouldn't happen, but in case it does, finish the Activity so that the user has
             // to respond to the dialog next time.
             if (getOwnerActivity() != null) getOwnerActivity().finish();
-        } else {
-            TemplateUrlService.getInstance().setSearchEngine(mSelectedKeyword.toString());
-            // TODO(dfalcantara): Prevent the dialog from appearing again.
         }
 
-        if (mOnDismissed != null) mOnDismissed.onResult(mSelectedKeyword != null);
-    }
-
-    private void updateButtonState() {
-        findViewById(R.id.button_primary).setEnabled(mSelectedKeyword != null);
+        if (mOnDismissed != null) {
+            mOnDismissed.onResult(mHelper.getCurrentlySelectedKeyword() != null);
+        }
     }
 }
