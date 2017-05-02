3d92eb3686a55eff1e3bb650b5cdb4d44683b346
fmalita <fmalita@chromium.org>
2017-05-02 07:33:16 -0700

    More missing Skia includes
    We're removing some indirect includes from SkCanvas.h; updating clients
    to explicitly include required headers.
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    TBR=enne@chromium.org
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2855863002
    Cr-Commit-Position: refs/heads/master@{#468629}
    
    Former-commit-id: 0bfbbd44d9d10e86fff9ed75141ad09d65ec42ed

--- cc/paint/paint_flags.h
+++ cc/paint/paint_flags.h
@@ -14,6 +14,7 @@
 #include "third_party/skia/include/core/SkMaskFilter.h"
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkPathEffect.h"
+#include "third_party/skia/include/core/SkShader.h"
 #include "third_party/skia/include/core/SkTypeface.h"
 
 namespace cc {
