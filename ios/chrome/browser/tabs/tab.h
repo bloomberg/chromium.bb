7d115f19167ab4e87817558a1514aafd8bfaf938
sdefresne <sdefresne@chromium.org>
2017-05-02 09:11:06 -0700

    [ios] ARCMigrate ios/chrome/browser/tabs/tab.mm to ARC.
    Semi-automatic conversion of ios/chrome/browser/tabs/tab.mm
    to ARC (Automatic Reference Counting).
    
    Manual changes:
    -   audit raw Objective-C pointers and mark them as __weak if
        needed (were all correctly annotated with weak comments);
    -   convert base::scoped_nsobject<> & base::WeakNSObject<> to
        annotations for the compiler (e.g. __weak);
    -   remove setter/getter for properties that can be synthesized;
    -   remove [[... retain] autorelease] as the CRWWebController
        has been converted to ARC already and thus the object will
        be autorelease and the hack is no longer required.
    
    BUG=None
    
    Review-Url: https://codereview.chromium.org/2846233002
    Cr-Commit-Position: refs/heads/master@{#468660}
    
    Former-commit-id: 376186504c93feac078435f1620005d15609974f

--- ios/chrome/browser/tabs/tab.h
+++ ios/chrome/browser/tabs/tab.h
@@ -113,7 +113,7 @@ extern NSString* const kProxyPassthroughHeaderValue;
 @property(nonatomic, readonly) const GURL& visibleURL;
 
 // The Passkit Dialog provider used to show the UI to download a passkit object.
-@property(nonatomic, assign) id<PassKitDialogProvider> passKitDialogProvider;
+@property(nonatomic, weak) id<PassKitDialogProvider> passKitDialogProvider;
 
 // The current title of the tab.
 @property(nonatomic, readonly) NSString* title;
@@ -133,27 +133,27 @@ extern NSString* const kProxyPassthroughHeaderValue;
 @property(nonatomic, readonly) web::WebState* webState;
 
 @property(nonatomic, readonly) CRWWebController* webController;
+
+// Handles saving and autofill of passwords.
 @property(nonatomic, readonly) PasswordController* passwordController;
 @property(nonatomic, readonly) BOOL canGoBack;
 @property(nonatomic, readonly) BOOL canGoForward;
-@property(nonatomic, assign) id<TabDelegate> delegate;
-@property(nonatomic, assign) id<TabHeadersDelegate> tabHeadersDelegate;
-@property(nonatomic, assign) id<TabSnapshottingDelegate>
-    tabSnapshottingDelegate;
+@property(nonatomic, weak) id<TabDelegate> delegate;
+@property(nonatomic, weak) id<TabHeadersDelegate> tabHeadersDelegate;
+@property(nonatomic, weak) id<TabSnapshottingDelegate> tabSnapshottingDelegate;
 @property(nonatomic, readonly) id<FindInPageControllerDelegate>
     findInPageControllerDelegate;
 
 // Whether or not desktop user agent is used for the currently visible page.
 @property(nonatomic, readonly) BOOL usesDesktopUserAgent;
 
-@property(nonatomic, assign) id<FullScreenControllerDelegate>
+@property(nonatomic, weak) id<FullScreenControllerDelegate>
     fullScreenControllerDelegate;
 @property(nonatomic, readonly)
     OverscrollActionsController* overscrollActionsController;
-@property(nonatomic, assign) id<OverscrollActionsControllerDelegate>
+@property(nonatomic, weak) id<OverscrollActionsControllerDelegate>
     overscrollActionsControllerDelegate;
-@property(nonatomic, assign) id<SnapshotOverlayProvider>
-    snapshotOverlayProvider;
+@property(nonatomic, weak) id<SnapshotOverlayProvider> snapshotOverlayProvider;
 
 // Delegate used to show HTTP Authentication dialogs.
 @property(nonatomic, weak) id<TabDialogDelegate> dialogDelegate;
