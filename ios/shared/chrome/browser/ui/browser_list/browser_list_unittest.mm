1e6dacb464db9d62365bf59ef60ae2458b89e7a9
sdefresne <sdefresne@chromium.org>
2017-05-03 03:33:36 -0700

    [ios] Add a BrowserListObserver interface.
    In order to serialise the state of the application, it is necessary
    to track creation and destruction of WebStateLists. This is done
    by adding a BrowserListObserver to track the creation of owning
    Browsers.
    
    BUG=710848
    
    Review-Url: https://codereview.chromium.org/2831363002
    Cr-Commit-Position: refs/heads/master@{#468930}
    
    Former-commit-id: b65a25e558ff3d3f3e411e3aa2fb09cab0fe2ca1

--- ios/shared/chrome/browser/ui/browser_list/browser_list_unittest.mm
+++ ios/shared/chrome/browser/ui/browser_list/browser_list_unittest.mm
@@ -7,13 +7,49 @@
 #include <memory>
 
 #include "base/macros.h"
+#include "base/scoped_observer.h"
 #include "ios/chrome/browser/browser_state/test_chrome_browser_state.h"
+#import "ios/shared/chrome/browser/ui/browser_list/browser_list_observer.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+
+class BrowserListTestObserver : public BrowserListObserver {
+ public:
+  BrowserListTestObserver() = default;
+
+  // Reset statistics whether events has been called.
+  void ResetStatistics() {
+    browser_created_called_ = false;
+    browser_removed_called_ = false;
+  }
+
+  // Returns whether OnBrowserCreated() was invoked.
+  bool browser_created_called() const { return browser_created_called_; }
+
+  // Returns whether OnBrowserRemoved() was invoked.
+  bool browser_removed_called() const { return browser_removed_called_; }
+
+  // BrowserListObserver implementation.
+  void OnBrowserCreated(BrowserList* browser_list, Browser* browser) override {
+    browser_created_called_ = true;
+  }
+
+  void OnBrowserRemoved(BrowserList* browser_list, Browser* browser) override {
+    browser_removed_called_ = true;
+  }
+
+ private:
+  bool browser_created_called_ = false;
+  bool browser_removed_called_ = false;
+};
+
+}  // namespace
 
 class BrowserListTest : public PlatformTest {
  public:
@@ -90,4 +126,28 @@ TEST_F(BrowserListTest, GetIndexOfBrowser) {
             browser_list->GetIndexOfBrowser(browser_0));
   EXPECT_EQ(0, browser_list->GetIndexOfBrowser(browser_1));
   EXPECT_EQ(browser_1, browser_list->GetBrowserAtIndex(0));
+}
+
+TEST_F(BrowserListTest, Observer) {
+  BrowserList* browser_list = BrowserList::FromBrowserState(browser_state());
+
+  BrowserListTestObserver observer;
+  ScopedObserver<BrowserList, BrowserListObserver> scoped_observer(&observer);
+  scoped_observer.Add(browser_list);
+
+  observer.ResetStatistics();
+  ASSERT_FALSE(observer.browser_created_called());
+  ASSERT_FALSE(observer.browser_removed_called());
+
+  browser_list->CreateNewBrowser();
+  EXPECT_TRUE(observer.browser_created_called());
+  EXPECT_FALSE(observer.browser_removed_called());
+
+  observer.ResetStatistics();
+  ASSERT_FALSE(observer.browser_created_called());
+  ASSERT_FALSE(observer.browser_removed_called());
+
+  browser_list->CloseBrowserAtIndex(0);
+  EXPECT_FALSE(observer.browser_created_called());
+  EXPECT_TRUE(observer.browser_removed_called());
 }
