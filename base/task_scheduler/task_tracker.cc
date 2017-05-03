8c52e009593591b96aa09a96fbbacf9da49bb750
fdoray <fdoray@chromium.org>
2017-05-03 10:03:34 -0700

    Synchronize memory in TaskTracker::Flush().
    To prevent races, make sure that code that runs after
    TaskTracker::Flush() sees memory changed by all tasks it was waiting
    for.
    
    BUG=717527
    
    Review-Url: https://codereview.chromium.org/2857613002
    Cr-Commit-Position: refs/heads/master@{#469008}
    
    Former-commit-id: 9888c32790d9934ce4a4446349c9a691536561cd

--- base/task_scheduler/task_tracker.cc
+++ base/task_scheduler/task_tracker.cc
@@ -215,7 +215,7 @@ void TaskTracker::Shutdown() {
 
 void TaskTracker::Flush() {
   AutoSchedulerLock auto_lock(flush_lock_);
-  while (subtle::NoBarrier_Load(&num_pending_undelayed_tasks_) != 0 &&
+  while (subtle::Acquire_Load(&num_pending_undelayed_tasks_) != 0 &&
          !IsShutdownComplete()) {
     flush_cv_->Wait();
   }
@@ -484,7 +484,7 @@ void TaskTracker::OnBlockingShutdownTasksComplete() {
 
 void TaskTracker::DecrementNumPendingUndelayedTasks() {
   const auto new_num_pending_undelayed_tasks =
-      subtle::NoBarrier_AtomicIncrement(&num_pending_undelayed_tasks_, -1);
+      subtle::Barrier_AtomicIncrement(&num_pending_undelayed_tasks_, -1);
   DCHECK_GE(new_num_pending_undelayed_tasks, 0);
   if (new_num_pending_undelayed_tasks == 0) {
     AutoSchedulerLock auto_lock(flush_lock_);
