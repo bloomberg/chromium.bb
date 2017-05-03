60927ae7b8f15c1ef99f4050ee5563932e8120ef
nednguyen <nednguyen@google.com>
2017-05-03 11:22:15 -0700

    Remove usage of bot_utils.GetDeviceAffinity in perf_data_generator
    |benchmark_sharding_map| is always non None, hence the branching to use
    |bot_utils.GetDeviceAffinity| when it's None is now dead code.
    
    BUG=713357
    TBR=eyaich@chromium.org
    
    Review-Url: https://codereview.chromium.org/2854413002
    Cr-Commit-Position: refs/heads/master@{#469037}
    
    Former-commit-id: ce159dee32a6cf18669950198af0efd9218393e8

--- tools/perf/core/perf_data_generator_unittest.py
+++ tools/perf/core/perf_data_generator_unittest.py
@@ -134,9 +134,10 @@ class PerfDataGeneratorTest(unittest.TestCase):
         'platform': 'android',
         'swarming_dimensions': swarming_dimensions,
     }
+    sharding_map = {'1': {'blacklisted': 0, 'not_blacklisted': 0}}
     benchmarks = [BlacklistedBenchmark, NotBlacklistedBenchmark]
     tests = perf_data_generator.generate_telemetry_tests(
-        test_config, benchmarks, None, ['blacklisted'])
+        test_config, benchmarks, sharding_map, ['blacklisted'])
 
     generated_test_names = set(t['name'] for t in tests)
     self.assertEquals(
