9ce7cb56ac8a4564c17b5a2cb164d0b091a2b1a2
robliao <robliao@chromium.org>
2017-05-04 13:53:37 -0700

    Exempt the Service Thread from BLOCK_SHUTDOWN DCHECKs
    In production, FileDescriptorWatchers can be leaky and post
    BLOCK_SHUTDOWN tasks in response to their notification.
    
    Stopping the service thread requires some non-trivial task lifetime
    fixes to do it correctly, so this was deemed the next best quick fix
    for redirection.
    
    BUG=717380
    
    Review-Url: https://codereview.chromium.org/2857103005
    Cr-Commit-Position: refs/heads/master@{#469461}
    
    Former-commit-id: f10490bd9e23a11a75977d6c53e2571248a61900

--- base/task_scheduler/task_scheduler_impl.cc
+++ base/task_scheduler/task_scheduler_impl.cc
@@ -104,7 +104,11 @@ void TaskSchedulerImpl::Start(const TaskScheduler::InitParams& init_params) {
   // message_loop().
   task_tracker_.set_watch_file_descriptor_message_loop(
       static_cast<MessageLoopForIO*>(service_thread_.message_loop()));
-#endif
+
+#if DCHECK_IS_ON()
+  task_tracker_.set_service_thread_handle(service_thread_.GetThreadHandle());
+#endif  // DCHECK_IS_ON()
+#endif  // defined(OS_POSIX) && !defined(OS_NACL_SFI)
 
   // Needs to happen after starting the service thread to get its task_runner().
   delayed_task_manager_.Start(service_thread_.task_runner());
