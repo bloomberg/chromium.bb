7d7c2da8a2ce6f0108e5e5b3f36c89ee532fb7d5
Don Garrett <dgarrett@google.com>
2017-05-03 18:31:08 -0700

    cros tryjob: Create a new command.
    This is intended as an evnentual replacement for "cbuildbot --remote
    <args>".
    
    Does not (yet) support local changes, or local tryjobs. Also needs
    improved help messages, and improved unittests.
    
    BUG=chromium:735720
    TEST=run_tests + cros tryjob -g 544630 kevin-compile-only-pre-cq
    
    Change-Id: Id0da3f34cc3724fbb8c1ed676a0a9f9ee904b7e8
    Reviewed-on: https://chromium-review.googlesource.com/544630
    Commit-Ready: Don Garrett <dgarrett@chromium.org>
    Tested-by: Don Garrett <dgarrett@chromium.org>
    Reviewed-by: Paul Hobbs <phobbs@google.com>

---
+++ cli/cros/cros_tryjob.py
@@ -0,0 +1,240 @@
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""cros tryjob: Schedule a tryjob."""
+
+from __future__ import print_function
+
+import argparse
+
+from chromite.lib import constants
+from chromite.cli import command
+from chromite.lib import cros_build_lib
+from chromite.lib import cros_logging as logging
+from chromite.lib import path_util
+
+from chromite.cbuildbot import remote_try
+from chromite.cbuildbot import trybot_patch_pool
+
+
+def CbuildbotArgs(options):
+  """Function to generate cbuidlbot command line args.
+
+  This are pre-api version filtering.
+
+  Args:
+    options: Parsed cros tryjob tryjob arguments.
+
+  Returns:
+    List of strings in ['arg1', 'arg2'] format.
+  """
+  args = []
+
+  if options.production:
+    args.append('--buildbot')
+  else:
+    # TODO: Remove from remote_try.py after cbuildbot --remote removed.
+    args.append('--remote-trybot')
+
+  if options.branch:
+    args.extend(('-b', options.branch))
+
+  if options.hwtest:
+    args.append('--hwtest')
+
+  for g in options.gerrit_patches:
+    args.extend(('-g', g))
+
+  for r in options.rietveld_patches:
+    args.extend(('-G', r))
+
+  # TODO: local_patches ???
+
+  # Specialty options.
+  if options.version:
+    args.extend(('--version', options.version))
+
+  for c in options.channels:
+    args.extend(('--channel', c))
+
+  # Tack on the pass through options.
+  args.extend(options.passthrough)
+
+  return args
+
+
+def RunLocal(options):
+  """Run a local tryjob."""
+  if len(options.build_configs) != 1:
+    cros_build_lib.Die('Local tryjobs only support a single config. Got: %s',
+                       ', '.join(options.build_configs))
+
+  build_config = options.build_configs[0]
+  logging.info('Running local tryjob: %s', build_config)
+  assert False, 'TODO: Local tryjobs aren\'t yet supported.'
+
+
+def RunRemote(options, patch_pool):
+  """Schedule remote tryjobs."""
+  logging.info('Scheduling remote tryjob(s): %s',
+               ', '.join(options.build_configs))
+
+  # Figure out the cbuildbot command line to pass in.
+  args = CbuildbotArgs(options)
+
+  # Figure out the tryjob description.
+  description = options.remote_description
+  if description is None:
+    description = remote_try.DefaultDescription(
+        options.branch,
+        options.gerrit_patches+options.local_patches)
+
+  print('Submitting tryjob...')
+  tryjob = remote_try.RemoteTryJob(options.build_configs,
+                                   patch_pool.local_patches,
+                                   args,
+                                   path_util.GetCacheDir(),
+                                   description,
+                                   options.committer_email)
+  tryjob.Submit(dryrun=False)
+  print('Tryjob submitted!')
+  print(('Go to %s to view the status of your job.'
+         % tryjob.GetTrybotWaterfallLink()))
+
+
+@command.CommandDecorator('tryjob')
+class TryjobCommand(command.CliCommand):
+  """Schedule a tryjob."""
+
+  EPILOG = """
+Examples:
+  cros tryjob -g 123 lumpy-compile-only-pre-cq
+  cros tryjob -g 123 -g 456 lumpy-compile-only-pre-cq daisy-pre-cq
+  cros tryjob -g 123 --hwtest daisy-paladin
+  cros tryjob --branch release-R61-9765.B asuka-paladin
+  cros tryjob --version 9795.0.0 --channel canary  lumpy-payloads
+"""
+
+  @classmethod
+  def AddParser(cls, parser):
+    """Adds a parser."""
+    super(cls, TryjobCommand).AddParser(parser)
+    parser.add_argument(
+        'build_configs', nargs='+',
+        help='One or more configs to build.')
+    parser.add_argument(
+        '-b', '--branch',
+        help='The manifest branch to test.  The branch to '
+             'check the buildroot out to.')
+    parser.add_argument(
+        '--hwtest', action='store_true', default=False,
+        help='Enable hwlab testing. Default false, except for production.')
+    parser.add_argument(
+        '--yes', action='store_true', default=False,
+        help='Never prompt to confirm.')
+    parser.add_argument(
+        '--production', action='store_true', default=False,
+        help='This is a production build, NOT a test build. Use with care.')
+    parser.add_argument(
+        '--passthrough', nargs=argparse.REMAINDER, default=[],
+        help='Arguments to pass to cbuildbot.')
+
+    # Do we build locally, on on a trybot builder?
+    where_group = parser.add_option_group(
+        'Where',
+        description='Where do we run the tryjob?')
+    where_ex = where_group.add_mutually_exclusive_group()
+    where_ex.add_argument(
+        '--local', action='store_false', dest='remote',
+        help='Run the tryjob on your local machine.')
+    where_ex.add_argument(
+        '--remote', action='store_true', default=True,
+        help='Run the tryjob on a remote builder. (default)')
+    where_group.add_argument(
+        '-r', '--buildroot', type='path', dest='buildroot',
+        help='Root directory to use for the local tryjob. '
+             'NOT the current checkout.')
+
+    # What patches do we include in the build?
+    what_group = parser.add_option_group(
+        'Patch',
+        description='Which patches should be included with the tryjob?')
+    what_group.add_argument(
+        '-g', '--gerrit-patches', action='append', default=[],
+        # metavar='Id1 *int_Id2...IdN',
+        help='Space-separated list of short-form Gerrit '
+             "Change-Id's or change numbers to patch. "
+             "Please prepend '*' to internal Change-Id's")
+    what_group.add_argument(
+        '-G', '--rietveld-patches', action='append', default=[],
+        # metavar='id1[:subdir1]...idN[:subdirN]',
+        help='Space-separated list of short-form Rietveld '
+             'issue numbers to patch. If no subdir is '
+             'specified, the src directory is used.')
+    what_group.add_argument(
+        '-p', '--local-patches', action='append', default=[],
+        # metavar="'<project1>[:<branch1>]...<projectN>[:<branchN>]'",
+        help='Space-separated list of project branches with '
+             'patches to apply.  Projects are specified by name. '
+             'If no branch is specified the current branch of the '
+             'project will be used.')
+
+    # Identifing the request.
+    who_group = parser.add_option_group(
+        'Requestor',
+        description='Who is submitting the jobs?')
+    who_group.add_argument(
+        '--committer-email',
+        help='Override default git committer email.')
+    who_group.add_argument(
+        '--remote-description',
+        help='Attach an optional description to a --remote run '
+             'to make it easier to identify the results when it '
+             'finishes')
+
+    # Specialized tryjob options.
+    special_group = parser.add_option_group(
+        'Specialty',
+        description='Options only used by specific tryjobs.')
+    special_group.add_argument(
+        '--version', dest='version', default=None,
+        help='Specify the release version for payload regeneration. '
+             'Ex: 9799.0.0')
+    special_group.add_argument(
+        '--channel', dest='channels', action='append', default=[],
+        help='Specify a channel for a payloads trybot. Can '
+             'be specified multiple times. No valid for '
+             'non-payloads configs.')
+
+  def Run(self):
+    """Runs `cros chroot`."""
+    self.options.Freeze()
+
+    # Ask for confirmation if there are no patches to test.
+    if (not self.options.yes and
+        not self.options.production and
+        not self.options.gerrit_patches and
+        not self.options.local_patches and
+        not self.options.rietveld_patches):
+      prompt = ('No patches were provided; are you sure you want to just '
+                'run a remote build of %s?' % (
+                    self.options.branch if self.options.branch else 'ToT'))
+      if not cros_build_lib.BooleanPrompt(prompt=prompt, default=False):
+        cros_build_lib.Die('No confirmation.')
+
+    # TODO: Local patch support.
+    assert not self.options.local_patches, 'TODO: Local patches.'
+
+    # Verify gerrit patches are valid.
+    print('Verifying patches...')
+    patch_pool = trybot_patch_pool.TrybotPatchPool.FromOptions(
+        gerrit_patches=self.options.gerrit_patches,
+        local_patches=[],
+        sourceroot=constants.SOURCE_ROOT,
+        remote_patches=[])
+
+    if not self.options.remote:
+      RunLocal(self.options)
+    else:
+      RunRemote(self.options, patch_pool)
---
+++ cli/cros/cros_tryjob_unittest.py
@@ -0,0 +1,111 @@
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Tests the `cros chroot` command."""
+
+from __future__ import print_function
+
+from chromite.cli import command_unittest
+from chromite.cli.cros import cros_tryjob
+from chromite.lib import cros_test_lib
+
+
+class MockTryjobCommand(command_unittest.MockCommand):
+  """Mock out the `cros tryjob` command."""
+  TARGET = 'chromite.cli.cros.cros_tryjob.TryjobCommand'
+  TARGET_CLASS = cros_tryjob.TryjobCommand
+  COMMAND = 'tryjob'
+
+
+class TryjobTest(cros_test_lib.MockTestCase):
+  """Test the ChrootCommand."""
+
+  def setUp(self):
+    self.cmd_mock = None
+
+  def SetupCommandMock(self, cmd_args):
+    """Sets up the `cros chroot` command mock."""
+    self.cmd_mock = MockTryjobCommand(cmd_args)
+    self.StartPatcher(self.cmd_mock)
+
+  def testMinimalParsing(self):
+    """Tests flow for an interactive session."""
+    self.SetupCommandMock(['lumpy-paladin'])
+    options = self.cmd_mock.inst.options
+
+    self.assertEqual(options.build_configs, ['lumpy-paladin'])
+    self.assertTrue(options.remote)
+    self.assertIsNone(options.branch)
+    self.assertFalse(options.hwtest)
+    self.assertFalse(options.production)
+    self.assertEqual(options.passthrough, [])
+    self.assertFalse(options.yes)
+    self.assertEqual(options.gerrit_patches, [])
+    self.assertEqual(options.rietveld_patches, [])
+    self.assertEqual(options.local_patches, [])
+
+  def testComplexParsing(self):
+    """Tests flow for an interactive session."""
+    self.SetupCommandMock([
+        '--yes', '--hwtest',
+        '--local', '--buildroot', '/buildroot',
+        '--gerrit-patches', '123', '-g', '*123', '-g', '123..456',
+        '--rietveld-patches', '12345', '-G', '12345[:/sub]',
+        '--local-patches', 'chromiumos/chromite:tryjob', '-p', 'other:other',
+        'lumpy-paladin', 'lumpy-release',
+        '--passthrough', 'foo', '--cbuild-arg', '--b-arg', 'bar',
+    ])
+    options = self.cmd_mock.inst.options
+
+    self.assertEqual(options.build_configs, ['lumpy-paladin', 'lumpy-release'])
+    self.assertFalse(options.remote)
+    self.assertEqual(options.buildroot, '/buildroot')
+    self.assertIsNone(options.branch)
+    self.assertTrue(options.hwtest)
+    self.assertFalse(options.production)
+    self.assertEqual(options.passthrough,
+                     ['foo', '--cbuild-arg', '--b-arg', 'bar'])
+    self.assertTrue(options.yes)
+    self.assertEqual(options.gerrit_patches, ['123', '*123', '123..456'])
+    self.assertEqual(options.rietveld_patches, ['12345', '12345[:/sub]'])
+    self.assertEqual(options.local_patches,
+                     ['chromiumos/chromite:tryjob', 'other:other'])
+
+  def testPayloadsParsing(self):
+    """Tests flow for an interactive session."""
+    self.SetupCommandMock([
+        '--version', '9795.0.0', '--channel', 'canary', 'lumpy-payloads'
+    ])
+    options = self.cmd_mock.inst.options
+
+    self.assertEqual(options.build_configs, ['lumpy-payloads'])
+    self.assertTrue(options.remote)
+    self.assertIsNone(options.branch)
+    self.assertFalse(options.hwtest)
+    self.assertFalse(options.production)
+    self.assertEqual(options.passthrough, [])
+    self.assertFalse(options.yes)
+    self.assertEqual(options.gerrit_patches, [])
+    self.assertEqual(options.rietveld_patches, [])
+    self.assertEqual(options.local_patches, [])
+
+  def testDashDashParsing(self):
+    """Tests flow for an interactive session."""
+    self.SetupCommandMock([
+        '--passthrough', 'foo', '--cbuild-arg', '--b-arg', 'bar',
+        '--', 'lumpy-paladin', 'lumpy-release',
+    ])
+    options = self.cmd_mock.inst.options
+
+    self.assertEqual(options.build_configs, ['lumpy-paladin', 'lumpy-release'])
+    self.assertTrue(options.remote)
+    self.assertIsNone(options.branch)
+    self.assertFalse(options.hwtest)
+    self.assertFalse(options.production)
+    self.assertEqual(options.passthrough,
+                     ['foo', '--cbuild-arg', '--b-arg', 'bar'])
+    self.assertFalse(options.yes)
+    self.assertEqual(options.gerrit_patches, [])
+    self.assertEqual(options.rietveld_patches, [])
+    self.assertEqual(options.local_patches, [])
