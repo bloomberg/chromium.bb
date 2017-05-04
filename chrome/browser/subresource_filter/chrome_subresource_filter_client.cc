d6bc0f303ed78e3245633a4a2d66920091773a28
csharrison <csharrison@chromium.org>
2017-05-04 11:23:10 -0700

    [subresource_filter] Move throttle insertion into the client
    This patch also  makes the SB throttle pass-through if there isn't a valid
    DB manager.
    
    BUG=717590
    
    Review-Url: https://codereview.chromium.org/2858483003
    Cr-Commit-Position: refs/heads/master@{#469388}
    
    Former-commit-id: 77bf83460738f6b1465d016ccfc26e930babfc00

--- chrome/browser/subresource_filter/chrome_subresource_filter_client.cc
+++ chrome/browser/subresource_filter/chrome_subresource_filter_client.cc
@@ -5,19 +5,28 @@
 #include "chrome/browser/subresource_filter/chrome_subresource_filter_client.h"
 
 #include <string>
+#include <utility>
 
+#include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/content_settings/tab_specific_content_settings.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/subresource_filter/subresource_filter_profile_context_factory.h"
 #include "chrome/browser/ui/android/content_settings/subresource_filter_infobar_delegate.h"
 #include "components/content_settings/core/browser/host_content_settings_map.h"
 #include "components/content_settings/core/common/content_settings_types.h"
+#include "components/safe_browsing_db/database_manager.h"
+#include "components/safe_browsing_db/v4_feature_list.h"
 #include "components/subresource_filter/content/browser/content_ruleset_service.h"
 #include "components/subresource_filter/content/browser/content_subresource_filter_driver_factory.h"
+#include "components/subresource_filter/content/browser/subresource_filter_safe_browsing_activation_throttle.h"
+#include "components/subresource_filter/core/browser/subresource_filter_features.h"
+#include "components/subresource_filter/core/common/activation_scope.h"
+#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
 
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(ChromeSubresourceFilterClient);
@@ -34,6 +43,43 @@ ChromeSubresourceFilterClient::ChromeSubresourceFilterClient(
 }
 
 ChromeSubresourceFilterClient::~ChromeSubresourceFilterClient() {}
+
+void ChromeSubresourceFilterClient::MaybeAppendNavigationThrottles(
+    content::NavigationHandle* navigation_handle,
+    std::vector<std::unique_ptr<content::NavigationThrottle>>* throttles) {
+  // Don't add any throttles if the feature isn't enabled at all.
+  if (subresource_filter::GetActiveConfigurations()
+          ->the_one_and_only()
+          .activation_scope == subresource_filter::ActivationScope::NO_SITES) {
+    return;
+  }
+
+  if (navigation_handle->IsInMainFrame()) {
+    safe_browsing::SafeBrowsingService* safe_browsing_service =
+        g_browser_process->safe_browsing_service();
+    scoped_refptr<safe_browsing::SafeBrowsingDatabaseManager> database_manager;
+    if (safe_browsing_service &&
+        safe_browsing_service->database_manager()->IsSupported() &&
+        safe_browsing::V4FeatureList::GetV4UsageStatus() ==
+            safe_browsing::V4FeatureList::V4UsageStatus::V4_ONLY) {
+      database_manager = safe_browsing_service->database_manager();
+    }
+
+    throttles->push_back(
+        base::MakeUnique<subresource_filter::
+                             SubresourceFilterSafeBrowsingActivationThrottle>(
+            navigation_handle,
+            content::BrowserThread::GetTaskRunnerForThread(
+                content::BrowserThread::IO),
+            std::move(database_manager)));
+  }
+
+  auto* driver_factory =
+      subresource_filter::ContentSubresourceFilterDriverFactory::
+          FromWebContents(navigation_handle->GetWebContents());
+  driver_factory->throttle_manager()->MaybeAppendNavigationThrottles(
+      navigation_handle, throttles);
+}
 
 void ChromeSubresourceFilterClient::ToggleNotificationVisibility(
     bool visibility) {
