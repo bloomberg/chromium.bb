9fd359a418657ab443517f83821963d156911d46
lpromero <lpromero@chromium.org>
2017-05-04 09:03:15 -0700

    Update the Speak answers back switch item.
    BUG=717483
    R=sczs@chromium.org
    TEST=See steps in bug.
    
    Review-Url: https://codereview.chromium.org/2862583005
    Cr-Commit-Position: refs/heads/master@{#469348}
    
    Former-commit-id: 644d16e85e314eea64abdb2ebd30b11ed731f5c9

--- ios/chrome/browser/ui/settings/voicesearch_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/voicesearch_collection_view_controller.mm
@@ -237,6 +237,12 @@ typedef NS_ENUM(NSInteger, ItemType) {
   if (on != switchView.isOn) {
     [switchView setOn:on animated:YES];
   }
+  // Also update the switch item.
+  CollectionViewSwitchItem* switchItem =
+      base::mac::ObjCCastStrict<CollectionViewSwitchItem>(
+          [self.collectionViewModel itemAtIndexPath:switchPath]);
+  switchItem.enabled = enabled;
+  switchItem.on = on;
 
   [self reconfigureCellsForItems:modifiedItems];
 }
