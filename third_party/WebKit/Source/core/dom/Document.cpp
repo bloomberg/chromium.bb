2ad0ec7bc285f99eff1edc0821d6688f98ddae3a
kouhei <kouhei@chromium.org>
2017-05-04 12:53:50 -0700

    [ES6 modules] accessing document.currentScript on module scripts should not crash
    Before this CL, accessing document.currentScript caused crash. It dereferenced nullptr
    pushed to Document::current_script_stack_ by ScriptLoader::DoExecuteScript() for
    script == ScriptType::kModule.
    
    This CL adds null check in Document::currentScriptForBinding so that it is aware that
    the item on the stack may be nullptr.
    
    TEST=wpt/html/semantics/scripting-1/the-script-element/module/currentScript-null.html
    BUG=716951
    
    Review-Url: https://codereview.chromium.org/2860913002
    Cr-Commit-Position: refs/heads/master@{#469435}
    
    Former-commit-id: 3d379f511131f8692bc7f46e617794d685935a0b

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -5289,8 +5289,10 @@ KURL Document::OpenSearchDescriptionURL() {
 
 void Document::currentScriptForBinding(
     HTMLScriptElementOrSVGScriptElement& script_element) const {
-  if (!current_script_stack_.IsEmpty())
-    current_script_stack_.back()->SetScriptElementForBinding(script_element);
+  if (!current_script_stack_.IsEmpty()) {
+    if (ScriptElementBase* script_element_base = current_script_stack_.back())
+      script_element_base->SetScriptElementForBinding(script_element);
+  }
 }
 
 void Document::PushCurrentScript(ScriptElementBase* new_current_script) {
