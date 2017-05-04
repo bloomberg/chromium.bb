945e1d27621900c477370867643a77a5ae442f92
paulmeyer <paulmeyer@chromium.org>
2017-05-04 13:59:30 -0700

    Reenable WebViewTests/WebViewTest.Shim_TestFindAPI_findupdate.
    BUG=710486
    TBR=wjmaclean@chromium.org
    
    Review-Url: https://codereview.chromium.org/2863633003
    Cr-Commit-Position: refs/heads/master@{#469465}
    
    Former-commit-id: 2a3f50f3684ccd28c4eb5160909a9cb3d64c9187

--- chrome/browser/apps/guest_view/web_view_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_browsertest.cc
@@ -3139,13 +3139,7 @@ IN_PROC_BROWSER_TEST_P(WebViewTest, Shim_TestFindAPI) {
   TestHelper("testFindAPI", "web_view/shim", NO_TEST_SERVER);
 }
 
-// crbug.com/710486
-#if defined(MEMORY_SANITIZER)
-#define MAYBE_Shim_TestFindAPI_findupdate DISABLED_Shim_TestFindAPI_findupdate
-#else
-#define MAYBE_Shim_TestFindAPI_findupdate Shim_TestFindAPI_findupdate
-#endif
-IN_PROC_BROWSER_TEST_P(WebViewTest, MAYBE_Shim_TestFindAPI_findupdate) {
+IN_PROC_BROWSER_TEST_P(WebViewTest, Shim_TestFindAPI_findupdate) {
   TestHelper("testFindAPI_findupdate", "web_view/shim", NO_TEST_SERVER);
 }
 
