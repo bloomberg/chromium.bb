3b35e50fd4b60fc21b9362022ba739f2ad11fc1a
tsergeant <tsergeant@chromium.org>
2017-05-04 21:42:09 -0700

    MD Bookmarks: Add keyboard shortcut support to bookmarks-command-manager
    This adds keyboard shortcuts for Edit, Copy and Delete.
    
    BUG=692837
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2843903002
    Cr-Commit-Position: refs/heads/master@{#469595}
    
    Former-commit-id: fad224fbb037adf1a6f37633708732f758f39497

--- chrome/test/data/webui/md_bookmarks/command_manager_test.js
+++ chrome/test/data/webui/md_bookmarks/command_manager_test.js
@@ -5,6 +5,23 @@
 suite('<bookmarks-command-manager>', function() {
   var commandManager;
   var store;
+  var lastCommand;
+  var lastCommandIds;
+
+  function assertLastCommand(command, ids) {
+    assertEquals(lastCommand, command);
+    if (ids)
+      assertDeepEquals(normalizeSet(lastCommandIds), ids);
+    lastCommand = null;
+    lastCommandIds = null;
+  }
+
+  suiteSetup(function() {
+    // Overwrite bookmarkManagerPrivate APIs which will crash if called with
+    // fake data.
+    chrome.bookmarkManagerPrivate.copy = function() {};
+    chrome.bookmarkManagerPrivate.removeTrees = function() {};
+  });
 
   setup(function() {
     store = new bookmarks.TestStore({
@@ -19,6 +36,13 @@ suite('<bookmarks-command-manager>', function() {
     bookmarks.Store.instance_ = store;
 
     commandManager = document.createElement('bookmarks-command-manager');
+
+    var realHandle = commandManager.handle.bind(commandManager);
+    commandManager.handle = function(command, itemIds) {
+      lastCommand = command;
+      lastCommandIds = itemIds;
+      realHandle(command, itemIds);
+    };
     replaceBody(commandManager);
   });
 
@@ -43,5 +67,48 @@ suite('<bookmarks-command-manager>', function() {
     assertTrue(commandHidden['edit']);
     assertTrue(commandHidden['copy']);
     assertFalse(commandHidden['delete']);
+  });
+
+  test('keyboard shortcuts trigger when valid', function() {
+    var modifier = cr.isMac ? 'meta' : 'ctrl';
+
+    store.data.selection.items = new Set(['13']);
+    store.notifyObservers();
+
+    MockInteractions.pressAndReleaseKeyOn(document, 67, modifier, 'c');
+    assertLastCommand('copy', ['13']);
+
+    // Doesn't trigger when a folder is selected.
+    store.data.selection.items = new Set(['11']);
+    store.notifyObservers();
+
+    MockInteractions.pressAndReleaseKeyOn(document, 67, modifier, 'c');
+    assertLastCommand(null);
+
+    // Doesn't trigger when nothing is selected.
+    store.data.selection.items = new Set();
+    store.notifyObservers();
+
+    MockInteractions.pressAndReleaseKeyOn(document, 67, modifier, 'c');
+    assertLastCommand(null);
+  });
+
+  test('delete command triggers', function() {
+    store.data.selection.items = new Set(['12', '13']);
+    store.notifyObservers();
+
+    MockInteractions.pressAndReleaseKeyOn(document, 46, '', 'Delete');
+    assertLastCommand('delete', ['12', '13']);
+  });
+
+  test('edit command triggers', function() {
+    var key = cr.isMac ? 'Enter' : 'F2';
+    var keyCode = cr.isMac ? 13 : 113;
+
+    store.data.selection.items = new Set(['11']);
+    store.notifyObservers();
+
+    MockInteractions.pressAndReleaseKeyOn(document, keyCode, '', key);
+    assertLastCommand('edit', ['11']);
   });
 });
