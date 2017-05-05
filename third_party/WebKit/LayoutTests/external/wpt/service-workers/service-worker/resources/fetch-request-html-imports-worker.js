9644b41cb273e4e163df3cda856dc9578bfbc5ff
mike <mike@mikepennisi.com>
2017-05-05 11:19:49 -0700

    Upstream service worker `fetch` test to WPT
    **fetch-request-html-imports**
    
    Use HTTPS version of absolute URLs and schedule iframe element to be
    removed at the completion of the test.
    
    **fetch-request-resources**
    
    Extend the WPT version of this test with the novel aspects of the
    Chromium version:
    
    - assertions for the `redirect` attribute of the Request object
    - 1 additional test case for image resource requests
    - 2 additional test cases for stylesheet resource requests
    - 15 test cases for "fetch" requests
    - 6 test cases for audio resource requests
    
    Note that while the Chromium version also contains test cases for image
    resources loaded via CSS, equivalent tests are already present upstream
    (under the name `fetch-request-css-images.https.html`) and therefore do
    not need to be migrated.
    
    Chromium continues to fail the upstream version due to its additional
    assertions for the `integrity` attribute of the Request object. Due to
    this, it cannot be used as a substitute for the Chromium-specific
    version. Persist the Chromium-specific version under a new file name
    documenting its deprecated status.
    
    BUG=688116
    R=mek@chromium.org
    
    Review-Url: https://codereview.chromium.org/2858093002
    Cr-Commit-Position: refs/heads/master@{#469726}
    
    Former-commit-id: be8f71f78b95b5d953e1f385120f9aa7f797ed0d

--- third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-request-html-imports-worker.js
+++ third_party/WebKit/LayoutTests/external/wpt/service-workers/service-worker/resources/fetch-request-html-imports-worker.js
@@ -1,4 +1,4 @@
-importScripts('../../resources/get-host-info.js');
+importScripts('/common/get-host-info.sub.js');
 var host_info = get_host_info();
 
 self.addEventListener('fetch', function(event) {
@@ -8,21 +8,21 @@ self.addEventListener('fetch', function(event) {
     }
     var result = 'mode=' + event.request.mode +
       ' credentials=' + event.request.credentials;
-    if (url == host_info.HTTP_ORIGIN + '/dummy-dir/same.html') {
+    if (url == host_info.HTTPS_ORIGIN + '/dummy-dir/same.html') {
       event.respondWith(new Response(
         result +
         '<link id="same-same" rel="import" ' +
-        'href="' + host_info.HTTP_ORIGIN + '/dummy-dir/same-same.html">' +
+        'href="' + host_info.HTTPS_ORIGIN + '/dummy-dir/same-same.html">' +
         '<link id="same-other" rel="import" ' +
-        ' href="' + host_info.HTTP_REMOTE_ORIGIN +
+        ' href="' + host_info.HTTPS_REMOTE_ORIGIN +
         '/dummy-dir/same-other.html">'));
-    } else if (url == host_info.HTTP_REMOTE_ORIGIN + '/dummy-dir/other.html') {
+    } else if (url == host_info.HTTPS_REMOTE_ORIGIN + '/dummy-dir/other.html') {
       event.respondWith(new Response(
         result +
         '<link id="other-same" rel="import" ' +
-        ' href="' + host_info.HTTP_ORIGIN + '/dummy-dir/other-same.html">' +
+        ' href="' + host_info.HTTPS_ORIGIN + '/dummy-dir/other-same.html">' +
         '<link id="other-other" rel="import" ' +
-        ' href="' + host_info.HTTP_REMOTE_ORIGIN +
+        ' href="' + host_info.HTTPS_REMOTE_ORIGIN +
         '/dummy-dir/other-other.html">'));
     } else {
       event.respondWith(new Response(result));
