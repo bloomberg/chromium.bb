c70b3b1c8e3ff2da6449549fd658d67682eae793
oysteine <oysteine@chromium.org>
2017-05-05 10:52:44 -0700

    Reland: Global Resource Coordinator: Basic service internals
    This adds the basic internals of the GRC; CoordinationUnits which can form a DAG, receive events, and send back updated resource usage policies.
    
    A full prototype of GRC usage can be seen here: https://codereview.chromium.org/2710823003
    
    Service architecture doc: https://docs.google.com/document/d/1qec4DNDM2pLLIFfCBtnNQTxlNXQzjml69yC8SGU9bzI/edit#
    
    GRC parent doc (internal only): https://docs.google.com/document/d/1dx4KDbDFvP-GWwwrSPg8Gxx4kboIoPi8kDKTSXoTbC4/edit#heading=h.td4yhfm12fe3
    
    R=primiano@chromium.org,skyostil@chromium.org,fmeawad@chromium.org,rockot@chromium.org
    BUG=691886
    
    Review-Url: https://codereview.chromium.org/2798713002
    Cr-Original-Original-Commit-Position: refs/heads/master@{#468002}
    Committed: https://chromium.googlesource.com/chromium/src/+/d114341877c9285b79d8db78d76cb7e1d29d5a9b [formerly de0f39d2a625433e60b5e22e1d2d3803f3d219e5]
    Review-Url: https://codereview.chromium.org/2798713002
    Cr-Original-Commit-Position: refs/heads/master@{#469107}
    Committed: https://chromium.googlesource.com/chromium/src/+/ef25e7f7a89617cc2bbf1b4ca97765a17b8b88af [formerly 8b47351be59f0d590d8d5fcffe1c07350ba110f2]
    Review-Url: https://codereview.chromium.org/2798713002
    Cr-Commit-Position: refs/heads/master@{#469713}
    
    Former-commit-id: ea57f11806604d841b31336cb4da7dda36c8b1b7

---
+++ services/resource_coordinator/coordination_unit/coordination_unit_impl.h
@@ -0,0 +1,72 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_RESOURCE_COORDINATOR_COORDINATION_UNIT_COORDINATION_UNIT_IMPL_H_
+#define SERVICES_RESOURCE_COORDINATOR_COORDINATION_UNIT_COORDINATION_UNIT_IMPL_H_
+
+#include <list>
+
+#include "base/optional.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
+#include "mojo/public/cpp/bindings/interface_request.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
+#include "services/resource_coordinator/public/cpp/coordination_unit_id.h"
+#include "services/resource_coordinator/public/interfaces/coordination_unit.mojom.h"
+#include "services/resource_coordinator/public/interfaces/coordination_unit_provider.mojom.h"
+#include "services/service_manager/public/cpp/service_context_ref.h"
+
+namespace resource_coordinator {
+
+class CoordinationUnitImpl : public mojom::CoordinationUnit {
+ public:
+  CoordinationUnitImpl(
+      const CoordinationUnitID& id,
+      std::unique_ptr<service_manager::ServiceContextRef> service_ref);
+  ~CoordinationUnitImpl() override;
+
+  // Overridden from mojom::CoordinationUnit:
+  void SendEvent(mojom::EventPtr event) override;
+  void GetID(const GetIDCallback& callback) override;
+  void Duplicate(mojom::CoordinationUnitRequest request) override;
+  void AddChild(const CoordinationUnitID& child_id) override;
+  void SetCoordinationPolicyCallback(
+      mojom::CoordinationPolicyCallbackPtr callback) override;
+
+ private:
+  bool AddChild(CoordinationUnitImpl* child);
+  void RemoveChild(CoordinationUnitImpl* child);
+  void AddParent(CoordinationUnitImpl* parent);
+  void RemoveParent(CoordinationUnitImpl* parent);
+  bool HasParent(CoordinationUnitImpl* unit);
+  bool HasChild(CoordinationUnitImpl* unit);
+
+  void RecalcCoordinationPolicy();
+  void UnregisterCoordinationPolicyCallback();
+
+  enum StateFlags : uint8_t {
+    kTestState,
+    kTabVisible,
+    kAudioPlaying,
+    kNumStateFlags
+  };
+  bool SelfOrParentHasFlagSet(StateFlags state);
+
+  std::unique_ptr<service_manager::ServiceContextRef> service_ref_;
+  mojo::BindingSet<mojom::CoordinationUnit> bindings_;
+  CoordinationUnitID id_;
+
+  std::set<CoordinationUnitImpl*> children_;
+  std::set<CoordinationUnitImpl*> parents_;
+
+  mojom::CoordinationPolicyCallbackPtr policy_callback_;
+  mojom::CoordinationPolicyPtr current_policy_;
+
+  base::Optional<bool> state_flags_[kNumStateFlags];
+
+  DISALLOW_COPY_AND_ASSIGN(CoordinationUnitImpl);
+};
+
+}  // namespace resource_coordinator
+
+#endif  // SERVICES_RESOURCE_COORDINATOR_COORDINATION_UNIT_COORDINATION_UNIT_IMPL_H_
