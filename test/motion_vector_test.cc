23aaaa9c3e01cd7bec04d2ac286110a84717d30d
Yunqing Wang <yunqingwang@google.com>
2017-05-04 17:24:25 -0700

    Use 2k resolution in motion vector tests on 32-bit system
    Reduced the test clip's resolution while testing on 32-bit system.
    
    BUG=aomedia:510
    
    Change-Id: Icea7a9d3affe1ea9e0842424db72bdc5bdf91390

--- test/motion_vector_test.cc
+++ test/motion_vector_test.cc
@@ -17,12 +17,6 @@
 #include "test/yuv_video_source.h"
 
 namespace {
-#if defined(__has_feature)
-#if __has_feature(address_sanitizer)
-#define BUILDING_WITH_ASAN
-#endif
-#endif
-
 #define MAX_EXTREME_MV 1
 #define MIN_EXTREME_MV 2
 
@@ -85,16 +79,11 @@ TEST_P(MotionVectorTestLarge, OverallTest) {
   int width = 3840;
   int height = 2160;
 
-#ifdef BUILDING_WITH_ASAN
-  // On the 32-bit system, if using 4k test clip, an "out of memory" error
-  // occurs because of the AddressSanitizer instrumentation memory overhead.
-  // Here, reduce the test clip's resolution while testing on 32-bit system
-  // and AddressSanitizer is enabled.
+  // Reduce the test clip's resolution while testing on 32-bit system.
   if (sizeof(void *) == 4) {
     width = 2048;
     height = 1080;
   }
-#endif
 
   cfg_.rc_target_bitrate = 24000;
   cfg_.g_profile = 0;
