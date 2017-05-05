wjdz99/aom

Add SMOOTH_PRED to interintra mode list
Change-Id: Id490c57df5dca13c59a62b0a542779784842944c

av1/common/entropymode.c
@@ -833,6 +833,15 @@ static const aom_prob default_interintra_prob[BLOCK_SIZE_GROUPS] = {
   208, 208, 208, 208,
 };
 
+#if CONFIG_ALT_INTRA
+static const aom_prob
+    default_interintra_mode_prob[BLOCK_SIZE_GROUPS][INTERINTRA_MODES - 1] = {
+      { 88, 16, 47, 133, 143, 150, 70, 48, 84, 122 },  // block_size < 8x8
+      { 75, 26, 51, 120, 158, 157, 44, 45, 56, 102 },  // block_size < 16x16
+      { 73, 24, 60, 115, 184, 164, 26, 36, 32, 63 },   // block_size < 32x32
+      { 96, 27, 50, 107, 221, 148, 16, 22, 14, 39 },   // block_size >= 32x32
+    };
+#else
 static const aom_prob
     default_interintra_mode_prob[BLOCK_SIZE_GROUPS][INTERINTRA_MODES - 1] = {
       { 65, 32, 18, 144, 162, 194, 41, 51, 98 },   // block_size < 8x8
@@ -840,6 +849,7 @@ static const aom_prob
       { 173, 80, 19, 176, 240, 193, 64, 35, 46 },  // block_size < 32x32
       { 221, 135, 38, 194, 248, 121, 96, 85, 29 }  // block_size >= 32x32
     };
+#endif  // CONFIG_ALT_INTRA
 
 static const aom_prob default_wedge_interintra_prob[BLOCK_SIZES] = {
 #if CONFIG_CB4X4
@@ -981,6 +991,20 @@ const aom_tree_index av1_inter_mode_tree[TREE_SIZE(INTER_MODES)] = {
 
 #if CONFIG_EXT_INTER
 /* clang-format off */
+#if CONFIG_ALT_INTRA
+const aom_tree_index av1_interintra_mode_tree[TREE_SIZE(INTERINTRA_MODES)] = {
+  -II_DC_PRED,   2,            /* 0 = DC_NODE */
+  -II_TM_PRED,   4,            /* 1 = TM_NODE */
+  -II_V_PRED,    6,            /* 2 = V_NODE */
+  8,          12,           /* 3 = COM_NODE */
+  -II_H_PRED,    10,           /* 4 = H_NODE */
+  -II_D135_PRED, -II_D117_PRED,   /* 5 = D135_NODE */
+  -II_D45_PRED,  14,           /* 6 = D45_NODE */
+  -II_D63_PRED,  16,           /* 7 = D63_NODE */
+  -II_D153_PRED, 18,           /* 8 = D153_NODE */
+  -II_D207_PRED, -II_SMOOTH_PRED, /* 9 = D207_NODE */
+};
+#else
 const aom_tree_index av1_interintra_mode_tree[TREE_SIZE(INTERINTRA_MODES)] = {
   -II_DC_PRED, 2,                   /* 0 = II_DC_NODE     */
   -II_TM_PRED, 4,                   /* 1 = II_TM_NODE     */
@@ -992,6 +1016,7 @@ const aom_tree_index av1_interintra_mode_tree[TREE_SIZE(INTERINTRA_MODES)] = {
   -II_D63_PRED, 16,                 /* 7 = II_D63_NODE    */
   -II_D153_PRED, -II_D207_PRED      /* 8 = II_D153_NODE   */
 };
+#endif
 
 const aom_tree_index av1_inter_compound_mode_tree
     [TREE_SIZE(INTER_COMPOUND_MODES)] = {

bloomberg/chromium.bb

av1/common/entropymode.c
@@ -831,6 +831,15 @@ static const aom_prob default_interintra_prob[BLOCK_SIZE_GROUPS] = {
   208, 208, 208, 208,
 };
 
+#if CONFIG_ALT_INTRA
+static const aom_prob
+    default_interintra_mode_prob[BLOCK_SIZE_GROUPS][INTERINTRA_MODES - 1] = {
+      { 88, 16, 47, 133, 143, 150, 70, 48, 84, 122 },  // block_size < 8x8
+      { 75, 26, 51, 120, 158, 157, 44, 45, 56, 102 },  // block_size < 16x16
+      { 73, 24, 60, 115, 184, 164, 26, 36, 32, 63 },   // block_size < 32x32
+      { 96, 27, 50, 107, 221, 148, 16, 22, 14, 39 },   // block_size >= 32x32
+    };
+#else
 static const aom_prob
     default_interintra_mode_prob[BLOCK_SIZE_GROUPS][INTERINTRA_MODES - 1] = {
       { 65, 32, 18, 144, 162, 194, 41, 51, 98 },   // block_size < 8x8
@@ -838,6 +847,7 @@ static const aom_prob
       { 173, 80, 19, 176, 240, 193, 64, 35, 46 },  // block_size < 32x32
       { 221, 135, 38, 194, 248, 121, 96, 85, 29 }  // block_size >= 32x32
     };
+#endif  // CONFIG_ALT_INTRA
 
 static const aom_prob default_wedge_interintra_prob[BLOCK_SIZES] = {
 #if CONFIG_CB4X4
@@ -979,6 +989,20 @@ const aom_tree_index av1_inter_mode_tree[TREE_SIZE(INTER_MODES)] = {
 
 #if CONFIG_EXT_INTER
 /* clang-format off */
+#if CONFIG_ALT_INTRA
+const aom_tree_index av1_interintra_mode_tree[TREE_SIZE(INTERINTRA_MODES)] = {
+  -II_DC_PRED,   2,               /* 0 = II_DC_NODE   */
+  -II_TM_PRED,   4,               /* 1 = II_TM_NODE   */
+  -II_V_PRED,    6,               /* 2 = II_V_NODE    */
+  8,            12,               /* 3 = II_COM_NODE  */
+  -II_H_PRED,    10,              /* 4 = II_H_NODE    */
+  -II_D135_PRED, -II_D117_PRED,   /* 5 = II_D135_NODE */
+  -II_D45_PRED,  14,              /* 6 = II_D45_NODE  */
+  -II_D63_PRED,  16,              /* 7 = II_D63_NODE  */
+  -II_D153_PRED, 18,              /* 8 = II_D153_NODE */
+  -II_D207_PRED, -II_SMOOTH_PRED, /* 9 = II_D207_NODE */
+};
+#else
 const aom_tree_index av1_interintra_mode_tree[TREE_SIZE(INTERINTRA_MODES)] = {
   -II_DC_PRED, 2,                   /* 0 = II_DC_NODE     */
   -II_TM_PRED, 4,                   /* 1 = II_TM_NODE     */
@@ -990,6 +1014,7 @@ const aom_tree_index av1_interintra_mode_tree[TREE_SIZE(INTERINTRA_MODES)] = {
   -II_D63_PRED, 16,                 /* 7 = II_D63_NODE    */
   -II_D153_PRED, -II_D207_PRED      /* 8 = II_D153_NODE   */
 };
+#endif
 
 const aom_tree_index av1_inter_compound_mode_tree
     [TREE_SIZE(INTER_COMPOUND_MODES)] = {