ba75bd35c44f2f454d07a613062d09dd98816804
eae <eae@chromium.org>
2017-05-08 10:09:17 -0700

    [LayoutNG] Add Font::GetTextIntercepts for LayoutNG
    Add a version of the Font::GetTextIntercepts method that takes fragments
    for LayoutNG. Breaks out intercepts logic into GetTextInterceptsInternal
    helper function that is shared between the TextRun & TextFragment impls.
    
    R=drott@chromium.org
    BUG=714962
    
    Review-Url: https://codereview.chromium.org/2869433002
    Cr-Commit-Position: refs/heads/master@{#470025}
    
    Former-commit-id: 9dc9a99a5f4e79fbccbb7c1a41bc0d32cd6bff96

--- third_party/WebKit/Source/platform/fonts/Font.h
+++ third_party/WebKit/Source/platform/fonts/Font.h
@@ -135,6 +135,11 @@ class PLATFORM_EXPORT Font {
                          const PaintFlags&,
                          const std::tuple<float, float>& bounds,
                          Vector<TextIntercept>&) const;
+  void GetTextIntercepts(const TextFragmentPaintInfo&,
+                         float device_scale_factor,
+                         const PaintFlags&,
+                         const std::tuple<float, float>& bounds,
+                         Vector<TextIntercept>&) const;
 
   // Glyph bounds will be the minimum rect containing all glyph strokes, in
   // coordinates using (<text run x position>, <baseline position>) as the
