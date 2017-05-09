1f3cfb8d657c82e96a73a39a5097f416684f5736
vmiura <vmiura@chromium.org>
2017-05-09 15:23:28 -0700

    Bump revision number for _DirectComposition_Video_VP9 test.
    The VP9 decoded image result slighly changed, which seems to be related
    to a recent third_party/libvpx roll.
    
    Bump the revision to regenerate the reference image.
    
    TBR=kbr@chromium.org,jbauman@chromium.org
    NOTRY=true
    BUG=720017
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2873733002
    Cr-Commit-Position: refs/heads/master@{#470430}
    
    Former-commit-id: 6d54e02b9d97dd583267a6591e63558a8e8be852

--- content/test/gpu/gpu_tests/pixel_test_pages.py
+++ content/test/gpu/gpu_tests/pixel_test_pages.py
@@ -508,6 +508,6 @@ def DirectCompositionPages(base_name):
       'pixel_video_vp9.html',
       base_name + '_DirectComposition_Video_VP9',
       test_rect=[0, 0, 300, 300],
-      revision=2,
+      revision=3,
       browser_args=browser_args),
   ]
