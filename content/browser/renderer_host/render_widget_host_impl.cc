c72247e1f5bfe2c987ba5aefeea73070911df663
ccameron <ccameron@chromium.org>
2017-05-09 07:55:47 -0700

    Wait for a frame before white-out for 167 instead of 50 msec
    This timeout came in originally with
    https://chromiumcodereview.appspot.com/9980016
    In that patch, it was suggested that we may want to increase the timeout
    to 60 or 100 msec.
    
    The moment for that change has come. Use 167 msec (10 frames at 60 Hz).
    
    BUG=718923
    
    Review-Url: https://codereview.chromium.org/2869943002
    Cr-Commit-Position: refs/heads/master@{#470330}
    
    Former-commit-id: 0226fbeb21522a77293fa4f956ec8c8165ee7584

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -884,7 +884,7 @@ void RenderWidgetHostImpl::WaitForSurface() {
   // How long to (synchronously) wait for the renderer to respond with a
   // new frame when our current frame doesn't exist or is the wrong size.
   // This timeout impacts the "choppiness" of our window resize.
-  const int kPaintMsgTimeoutMS = 50;
+  const int kPaintMsgTimeoutMS = 167;
 
   if (!view_)
     return;
