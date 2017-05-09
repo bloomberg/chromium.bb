c27ef0c8357e18139cc84d022605b1f52e711e76
Nathan E. Egge <negge@mozilla.com>
2017-05-09 07:35:57 -0400

    Fix ifdef guard on av1_set_mv_cdfs() declaration.
    Change-Id: I253a3dfdc4329c19fd8c9aca32971c62cea864b3

--- av1/common/entropymv.h
+++ av1/common/entropymv.h
@@ -138,7 +138,7 @@ void av1_inc_mv(const MV *mv, nmv_context_counts *mvctx, const int usehp);
 extern const aom_tree_index
     av1_global_motion_types_tree[TREE_SIZE(GLOBAL_TRANS_TYPES)];
 #endif  // CONFIG_GLOBAL_MOTION
-#if CONFIG_EC_MULTISYMBOL
+#if CONFIG_EC_MULTISYMBOL && !CONFIG_EC_ADAPT
 void av1_set_mv_cdfs(nmv_context *ctx);
 #endif
 
