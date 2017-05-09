5d7117f487acd93917cd783811b6d4687296f579
gambard <gambard@chromium.org>
2017-05-09 00:55:55 -0700

    Add Most Visited tiles to ContentSuggestions
    This CL creates and add the Most Visited tiles to the ContentSuggestions.
    
    BUG=707754, 708491, 713183
    
    Review-Url: https://codereview.chromium.org/2815023003
    Cr-Commit-Position: refs/heads/master@{#470264}
    
    Former-commit-id: 29067c254b36f05d476335066744e3f6782c7e98

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_updater.mm
@@ -14,6 +14,8 @@
 #import "ios/chrome/browser/ui/collection_view/collection_view_model.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_article_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_footer_item.h"
+#import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_most_visited.h"
+#import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_most_visited_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_reading_list_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_text_item.h"
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestion.h"
@@ -236,16 +238,19 @@ SectionIdentifier SectionIdentifierForInfo(
     return [NSArray array];
   }
 
+  NSMutableArray<ContentSuggestionsMostVisited*>* mostVisitedToAdd =
+      [NSMutableArray array];
+
+  CSCollectionViewModel* model =
+      self.collectionViewController.collectionViewModel;
   NSMutableArray<NSIndexPath*>* indexPaths = [NSMutableArray array];
   for (ContentSuggestion* suggestion in suggestions) {
     ContentSuggestionsSectionInformation* sectionInfo =
         suggestion.suggestionIdentifier.sectionInfo;
     NSInteger sectionIdentifier = SectionIdentifierForInfo(sectionInfo);
-    CSCollectionViewModel* model =
-        self.collectionViewController.collectionViewModel;
 
     if (![model hasSectionForSectionIdentifier:sectionIdentifier])
-      return [NSArray array];
+      continue;
 
     NSInteger section = [model sectionForSectionIdentifier:sectionIdentifier];
     NSIndexPath* indexPath = [NSIndexPath indexPathForItem:0 inSection:section];
@@ -288,10 +293,35 @@ SectionIdentifier SectionIdentifierForInfo(
         break;
       }
       case ContentSuggestionTypeMostVisited: {
-        // TODO(crbug.com/707754): Add the most visited item.
+        NSInteger section =
+            [model sectionForSectionIdentifier:SectionIdentifierMostVisited];
+        NSIndexPath* indexPath =
+            [NSIndexPath indexPathForItem:0 inSection:section];
+
+        if ([model numberOfItemsInSection:section] == 0) {
+          [model addItem:[[ContentSuggestionsMostVisitedItem alloc]
+                             initWithType:ItemTypeMostVisited]
+              toSectionWithIdentifier:SectionIdentifierMostVisited];
+          [indexPaths addObject:indexPath];
+        }
+
+        ContentSuggestionsMostVisited* mostVisited =
+            [ContentSuggestionsMostVisited mostVisitedWithTitle:suggestion.title
+                                                     attributes:nil];
+        [mostVisitedToAdd addObject:mostVisited];
         break;
       }
     }
+  }
+
+  if ([model hasSectionForSectionIdentifier:SectionIdentifierMostVisited]) {
+    NSInteger section =
+        [model sectionForSectionIdentifier:SectionIdentifierMostVisited];
+    NSIndexPath* indexPath = [NSIndexPath indexPathForItem:0 inSection:section];
+    ContentSuggestionsMostVisitedItem* item =
+        base::mac::ObjCCast<ContentSuggestionsMostVisitedItem>(
+            [model itemAtIndexPath:indexPath]);
+    item.suggestions = mostVisitedToAdd;
   }
 
   return indexPaths;
