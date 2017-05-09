bloomberg/chromium.bb

commit 1e34d438dd5cf12644b67ee642e10b0f38589112

supersize: Fix --toolprefix="" and normalize cpu metadata
--toolprefix="" was triggering autodetection when it shouldn't.
x64 ELFs were being reported as "Advanced" rather than "x64"

BUG=681694

Review-Url: https://codereview.chromium.org/2869793002
Cr-Commit-Position: refs/heads/master@{#470043}

Former-commit-id: de4ca3bfeed1d86c3f9b5bbd4d895a4393650a0b

tools/binary_size/libsupersize/archive.py
@@ -549,7 +549,16 @@ def _SectionSizesFromElf(elf_path, tool_prefix):
 def _ArchFromElf(elf_path, tool_prefix):
   args = [tool_prefix + 'readelf', '-h', elf_path]
   stdout = subprocess.check_output(args)
-  return re.search('Machine:\s*(\S+)', stdout).group(1)
+  machine = re.search('Machine:\s*(.+)', stdout).group(1)
+  if machine == 'Intel 80386':
+    return 'x86'
+  if machine == 'Advanced Micro Devices X86-64':
+    return 'x64'
+  elif machine == 'ARM':
+    return 'arm'
+  elif machine == 'AArch64':
+    return 'arm64'
+  return machine
 
 
 def _ParseGnArgs(args_path):
@@ -664,9 +673,10 @@ def Run(args, parser):
 
       packed_section_name = None
       architecture = metadata[models.METADATA_ELF_ARCHITECTURE]
-      if architecture == 'ARM':
+      # Packing occurs enabled only arm32 & arm64.
+      if architecture == 'arm':
         packed_section_name = '.rel.dyn'
-      elif architecture == 'AArch64':
+      elif architecture == 'arm64':
         packed_section_name = '.rela.dyn'
 
       if packed_section_name:

tools/binary_size/libsupersize/paths.py
@@ -18,8 +18,10 @@ class LazyPaths(object):
     self._tool_prefix = tool_prefix
     self._output_directory = output_directory
     self._any_path_within_output_directory = any_path_within_output_directory
-    self._output_directory_status = _STATUS_DETECTED if output_directory else 0
-    self._tool_prefix_status = _STATUS_DETECTED if tool_prefix else 0
+    self._output_directory_status = (
+        _STATUS_DETECTED if output_directory is not None else 0)
+    self._tool_prefix_status = (
+        _STATUS_DETECTED if tool_prefix is not None else 0)
 
   @property
   def tool_prefix(self):

tools/binary_size/libsupersize/testdata/Archive_Elf.golden
@@ -1,4 +1,4 @@
-elf_arch=ARM
+elf_arch=arm
 elf_build_id=WhatAnAmazingBuildId
 elf_file_name=elf
 elf_mtime={redacted}

tools/binary_size/libsupersize/testdata/Console.golden
@@ -47,7 +47,7 @@ Print(c[-1].GroupByPath(depth=2).Sorted())
 # For even more inspiration, look at canned_queries.py
 # (and feel free to add your own!).
 Metadata:
-    elf_arch=ARM
+    elf_arch=arm
     elf_build_id=WhatAnAmazingBuildId
     elf_file_name=elf
     elf_mtime={redacted}

tools/binary_size/libsupersize/testdata/Diff_NullDiff.golden
@@ -1,5 +1,5 @@
 Common Metadata:
-    elf_arch=ARM
+    elf_arch=arm
     elf_build_id=WhatAnAmazingBuildId
     elf_file_name=elf
     elf_mtime={redacted}

tools/binary_size/libsupersize/testdata/FullDescription.golden
@@ -1,5 +1,5 @@
 Metadata:
-    elf_arch=ARM
+    elf_arch=arm
     elf_build_id=WhatAnAmazingBuildId
     elf_file_name=elf
     elf_mtime={redacted}

commit 74bb0871832f9072a6fd5b6d8a0a3f93307a4b1c

supersize: Fix --tool-prefix autodetection
Broken by 1e34d438dd5cf12644b67ee642e10b0f38589112 [formerly de4ca3bfeed1d86c3f9b5bbd4d895a4393650a0b]

BUG=681694

Review-Url: https://codereview.chromium.org/2866103003
Cr-Commit-Position: refs/heads/master@{#470341}

Former-commit-id: adccc21bf05c55215cec58b6d19cb4d39cfa69b6

tools/binary_size/libsupersize/archive.py
@@ -323,6 +323,11 @@ def _AddSymbolAliases(raw_symbols, aliases_by_address):
       replacements.append((i, name_list))
       num_new_symbols += len(name_list) - 1
 
+  if float(num_new_symbols) / len(raw_symbols) < .1:
+    logging.warning('Number of aliases is oddly low (%.0f%%). It should '
+                    'usually be around 25%%. Ensure --tool-prefix is correct.',
+                    float(num_new_symbols) / len(raw_symbols) * 100)
+
   # Step 2: Create new symbols as siblings to each existing one.
   logging.debug('Creating %d aliases', num_new_symbols)
   src_cursor_end = len(raw_symbols)
@@ -601,8 +606,8 @@ def AddArguments(parser):
   parser.add_argument('--no-source-paths', action='store_true',
                       help='Do not use .ninja files to map '
                            'object_path -> source_path')
-  parser.add_argument('--tool-prefix', default='',
-                      help='Path prefix for c++filt.')
+  parser.add_argument('--tool-prefix',
+                      help='Path prefix for c++filt, nm, readelf.')
   parser.add_argument('--output-directory',
                       help='Path to the root build directory.')
 

tools/binary_size/libsupersize/console.py
@@ -259,7 +259,7 @@ def AddArguments(parser):
   parser.add_argument('--query',
                       help='Execute the given snippet. '
                            'Example: Print(size_info)')
-  parser.add_argument('--tool-prefix', default='',
+  parser.add_argument('--tool-prefix',
                       help='Path prefix for objdump. Required only for '
                            'Disassemble().')
   parser.add_argument('--output-directory',