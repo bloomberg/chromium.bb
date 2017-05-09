939fe9cc8b9baf82da30fb275b324c0fe5e2996c
rtoy <rtoy@chromium.org>
2017-05-09 12:13:33 -0700

    Rename Distance.* file to DistanceEffect.*
    The class is named DistanceEffect, but the implementation is in the
    files Distance.{h,cpp}.  Rename the file to match the class name.
    
    No other changes.
    
    BUG=718967
    TEST=no changes to tests
    
    Review-Url: https://codereview.chromium.org/2864383002
    Cr-Commit-Position: refs/heads/master@{#470401}
    
    Former-commit-id: 27ea3538fa4f74eef95f7f53834231aa6c0892c9

--- third_party/WebKit/Source/platform/audio/DistanceEffect.cpp
+++ third_party/WebKit/Source/platform/audio/DistanceEffect.cpp
@@ -26,7 +26,7 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "platform/audio/Distance.h"
+#include "platform/audio/DistanceEffect.h"
 
 #include <math.h>
 #include <algorithm>
