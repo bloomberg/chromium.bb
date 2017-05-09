80be2bc1a8600a81d24b7822fd68f283a08d0fff
thomasanderson <thomasanderson@google.com>
2017-05-08 22:33:03 -0700

    Linux native notifications: use image_path instead of image-path for spec 1.1
    You're supposed to use 'image_path' for notification servers that only
    support the FDO Notification spec version 1.1, and 'image-path' for
    versions >= 1.2.  ¯\_(ツ)_/¯
    
    Also, ban servers that have a spec version <= 1.0 because these won't
    support notification icons at all.
    
    BUG=676220
    R=thestig@chromium.org
    
    Review-Url: https://codereview.chromium.org/2868433004
    Cr-Commit-Position: refs/heads/master@{#470237}
    
    Former-commit-id: c3b7ae8000d7222d74e2d0942d1d8823ecc0fd52

--- chrome/browser/notifications/notification_platform_bridge_linux_unittest.cc
+++ chrome/browser/notifications/notification_platform_bridge_linux_unittest.cc
@@ -44,6 +44,16 @@ ACTION_P(OnGetCapabilities, capabilities) {
   return response;
 }
 
+ACTION_P(OnGetServerInformation, spec_version) {
+  dbus::Response* response = dbus::Response::CreateEmpty().release();
+  dbus::MessageWriter writer(response);
+  writer.AppendString("");  // name
+  writer.AppendString("");  // vendor
+  writer.AppendString("");  // version
+  writer.AppendString(spec_version);
+  return response;
+}
+
 ACTION_P(OnNotify, id) {
   // The "Notify" message must have type (susssasa{sv}i).
   // https://developer.gnome.org/notification-spec/#command-notify
@@ -141,6 +151,10 @@ class NotificationPlatformBridgeLinuxTest : public testing::Test {
     EXPECT_CALL(*mock_notification_proxy_.get(),
                 MockCallMethodAndBlock(Calls("GetCapabilities"), _))
         .WillOnce(OnGetCapabilities(std::vector<std::string>()));
+
+    EXPECT_CALL(*mock_notification_proxy_.get(),
+                MockCallMethodAndBlock(Calls("GetServerInformation"), _))
+        .WillOnce(OnGetServerInformation("1.2"));
 
     EXPECT_CALL(
         *mock_notification_proxy_.get(),
