4c30c2fc6d20f428baa0e9e708f6ede5f23eef4a
mpearson <mpearson@chromium.org>
2017-05-08 23:00:17 -0700

    Omnibox - HistoryQuick Provider - Don't Run Duplicate Searches
    (when splitting on cursor)
    
    If the cursor is next to a space, there's no need to insert a space
    where the cursor is.
    
    Not tested.  Please give this change a thorough review.
    
    BUG=670003
    
    Review-Url: https://codereview.chromium.org/2842513006
    Cr-Commit-Position: refs/heads/master@{#470242}
    
    Former-commit-id: 7d8bbddd8416f906955738a2f70cb402527d079c

--- components/omnibox/browser/url_index_private_data.cc
+++ components/omnibox/browser/url_index_private_data.cc
@@ -10,6 +10,7 @@
 #include <iterator>
 #include <limits>
 #include <numeric>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -155,7 +156,10 @@ ScoredHistoryMatches URLIndexPrivateData::HistoryItemsForTerms(
       (cursor_position < original_search_string.length()) &&
       (cursor_position > 0)) {
     // The original search_string broken at cursor position. This is one type of
-    // transformation.
+    // transformation.  It's possible this transformation doesn't actually
+    // break any words.  There's no harm in adding the transformation in this
+    // case because the searching code below prevents running duplicate
+    // searches.
     base::string16 transformed_search_string(original_search_string);
     transformed_search_string.insert(cursor_position, base::ASCIIToUTF16(" "));
     search_strings.push_back(transformed_search_string);
@@ -173,6 +177,9 @@ ScoredHistoryMatches URLIndexPrivateData::HistoryItemsForTerms(
   ResetSearchTermCache();
 
   bool history_ids_were_trimmed = false;
+  // A set containing the list of words extracted from each search string,
+  // used to prevent running duplicate searches.
+  std::set<String16Vector> search_string_words;
   for (const base::string16& search_string : search_strings) {
     // The search string we receive may contain escaped characters. For reducing
     // the index we need individual, lower-cased words, ignoring escapings. For
@@ -192,6 +199,10 @@ ScoredHistoryMatches URLIndexPrivateData::HistoryItemsForTerms(
         String16VectorFromString16(lower_unescaped_string, false, nullptr));
     if (lower_words.empty())
       continue;
+    // If we've already searched for this list of words, don't do it again.
+    if (search_string_words.find(lower_words) != search_string_words.end())
+      continue;
+    search_string_words.insert(lower_words);
 
     HistoryIDVector history_ids = HistoryIDsFromWords(lower_words);
     history_ids_were_trimmed |= TrimHistoryIdsPool(&history_ids);
