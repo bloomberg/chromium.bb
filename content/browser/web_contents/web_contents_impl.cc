f4dccf4231d950b37d72db65c36bb85aae727eed
creis <creis@chromium.org>
2017-05-09 15:07:50 -0700

    Diagnose another possibly bad WebContentsObserver in RenderFrameDeleted.
    Similar to r451344, we're hoping to catch which WebContentsObserver
    might be deleting the WebContents while we're iterating over the
    observer list.  If this CL has more luck catching a culprit than the
    last attempt, we can generalize the approach.
    
    BUG=717650
    TEST=More useful crash reports.
    
    Review-Url: https://codereview.chromium.org/2867793003
    Cr-Commit-Position: refs/heads/master@{#470426}
    
    Former-commit-id: 36388974520e22c554b2f57e38ede28a6260293d

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -4339,8 +4339,10 @@ void WebContentsImpl::RenderFrameCreated(RenderFrameHost* render_frame_host) {
 }
 
 void WebContentsImpl::RenderFrameDeleted(RenderFrameHost* render_frame_host) {
+  is_notifying_observers_ = true;
   for (auto& observer : observers_)
     observer.RenderFrameDeleted(render_frame_host);
+  is_notifying_observers_ = false;
 #if BUILDFLAG(ENABLE_PLUGINS)
   pepper_playback_observer_->RenderFrameDeleted(render_frame_host);
 #endif
