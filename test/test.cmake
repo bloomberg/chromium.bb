b7336fb936582f79bac4453f934715abae7a0dc7
Tom Finegan <tomfinegan@google.com>
2017-05-09 16:24:52 -0700

    Enable FilterIntra tests in the CMake build.
    BUG=https://bugs.chromium.org/p/aomedia/issues/detail?id=76
    
    Change-Id: I6b51219ff28758b38379033dc3ab11bf288ac3ce

--- test/test.cmake
+++ test/test.cmake
@@ -103,9 +103,7 @@ if (CONFIG_AV1)
     if (HAVE_SSE4_1)
       set(AOM_UNIT_TEST_COMMON_SOURCES
           ${AOM_UNIT_TEST_COMMON_SOURCES}
-          # TODO: not sure if this intrinsics or a wrapper calling intrin/asm.
-          #"${AOM_ROOT}/test/filterintra_predictors_test.cc")
-          )
+          "${AOM_ROOT}/test/filterintra_predictors_test.cc")
     endif ()
   endif ()
 
