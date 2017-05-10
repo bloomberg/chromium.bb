ffe3e478657dc7126fca6329dfcedc49f4c726d9
Lars Volker <lv@cloudera.com>
2017-05-10 22:50:32 +0200

    Only use O_CLOEXEC on platforms that support it
    Change a9fca58 made use of the O_CLOEXEC flag, which is not supported on
    older Linux kernels. This change makes the use contingent on kernel
    support.
    
    Testing: I manually compiled breakpad on CentOS 5.8 running kernel
    2.6.18-308.8.2.el5.centos.plusxen.
    
    Bug: 730
    Change-Id: I21dff928cfba3c156a56708913f65a0c7b5396a6
    Reviewed-on: https://chromium-review.googlesource.com/498528
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- src/common/linux/guid_creator.cc
+++ src/common/linux/guid_creator.cc
@@ -27,6 +27,7 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include "config.h"
 #include "common/linux/eintr_wrapper.h"
 #include "common/linux/guid_creator.h"
 
