8f5c2357a714c792f6f292222d6db11b6f7d6f97
tbarzic <tbarzic@chromium.org>
2017-05-10 10:11:21 -0700

    Implement tray item for note taking lock screen app
    The item is displayed in system tray status area when lock screen note
    action is available - the lock screen note creation tray item is
    updated by observing TrayAction state for lock screen notes.
    
    The item is visible only in two states:
    kAvailable - the item is actionable
    kLaunching - the item is not actionable
    
    The item is only visible on lock screen (given that it represents an
    action handler for new note action on lock screen), regardless of the
    TrayAction state - even though Chrome should eventually ensure that the
    state is properly updated when user session is unlocked.
    
    When the tray item is clicked, a request will be sent to Chrome to handle
    new lock screen note using mojom::TrayActionClient interface.
    
    BUG=716081
    
    Review-Url: https://codereview.chromium.org/2864843002
    Cr-Commit-Position: refs/heads/master@{#470621}
    
    Former-commit-id: 011afda700511a9c30c7256cda8d5bc2e4b62fe8

---
+++ ash/system/lock_screen_action/lock_screen_action_tray_unittest.cc
@@ -0,0 +1,230 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/system/lock_screen_action/lock_screen_action_tray.h"
+
+#include "ash/resources/vector_icons/vector_icons.h"
+#include "ash/shelf/shelf_constants.h"
+#include "ash/shell.h"
+#include "ash/system/status_area_widget.h"
+#include "ash/test/ash_test_base.h"
+#include "ash/test/status_area_widget_test_helper.h"
+#include "ash/test/test_session_controller_client.h"
+#include "ash/tray_action/tray_action.h"
+#include "base/macros.h"
+#include "components/session_manager/session_manager_types.h"
+#include "ui/events/test/event_generator.h"
+#include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/paint_vector_icon.h"
+
+namespace ash {
+
+namespace {
+
+LockScreenActionTray* GetTray() {
+  return StatusAreaWidgetTestHelper::GetStatusAreaWidget()
+      ->lock_screen_action_tray_for_testing();
+}
+
+class TestTrayActionClient : public mojom::TrayActionClient {
+ public:
+  TestTrayActionClient() : binding_(this) {}
+
+  ~TestTrayActionClient() override = default;
+
+  mojom::TrayActionClientPtr CreateInterfacePtrAndBind() {
+    return binding_.CreateInterfacePtrAndBind();
+  }
+
+  void RequestNewLockScreenNote() override { action_requests_count_++; }
+
+  int action_requests_count() const { return action_requests_count_; }
+
+  void ResetActionRequestsCount() { action_requests_count_ = 0; }
+
+ private:
+  mojo::Binding<ash::mojom::TrayActionClient> binding_;
+
+  int action_requests_count_ = 0;
+
+  DISALLOW_COPY_AND_ASSIGN(TestTrayActionClient);
+};
+
+class LockScreenActionTrayTest : public test::AshTestBase {
+ public:
+  LockScreenActionTrayTest() = default;
+  ~LockScreenActionTrayTest() override = default;
+
+  void ClickOnTray() {
+    // Perform click on the tray view.
+    ui::test::EventGenerator& generator = GetEventGenerator();
+    gfx::Point center = GetTray()->GetBoundsInScreen().CenterPoint();
+    generator.MoveMouseTo(center.x(), center.y());
+    generator.ClickLeftButton();
+
+    RunAllPendingInMessageLoop();
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(LockScreenActionTrayTest);
+};
+
+}  // namespace
+
+TEST_F(LockScreenActionTrayTest, NoClient) {
+  SetUserLoggedIn(true);
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOCKED);
+
+  Shell::Get()->tray_action()->UpdateLockScreenNoteState(
+      mojom::TrayActionState::kAvailable);
+
+  EXPECT_FALSE(GetTray()->visible());
+}
+
+TEST_F(LockScreenActionTrayTest, NotAvailableState) {
+  SetUserLoggedIn(true);
+
+  TrayAction* tray_action = Shell::Get()->tray_action();
+  TestTrayActionClient tray_action_client;
+  tray_action->SetClient(tray_action_client.CreateInterfacePtrAndBind(),
+                         mojom::TrayActionState::kNotAvailable);
+
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOCKED);
+  EXPECT_FALSE(GetTray()->visible());
+}
+
+TEST_F(LockScreenActionTrayTest, ActionAvailableState) {
+  SetUserLoggedIn(true);
+
+  TrayAction* tray_action = Shell::Get()->tray_action();
+  TestTrayActionClient tray_action_client;
+  tray_action->SetClient(tray_action_client.CreateInterfacePtrAndBind(),
+                         mojom::TrayActionState::kNotAvailable);
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kAvailable);
+
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOCKED);
+  EXPECT_TRUE(GetTray()->visible());
+
+  ASSERT_FALSE(GetTray()->GetImageForTesting().isNull());
+
+  EXPECT_EQ(0, tray_action_client.action_requests_count());
+  ClickOnTray();
+  ASSERT_EQ(1, tray_action_client.action_requests_count());
+  EXPECT_TRUE(GetTray()->visible());
+  EXPECT_FALSE(GetTray()->GetImageForTesting().isNull());
+
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kActive);
+  EXPECT_FALSE(GetTray()->visible());
+}
+
+TEST_F(LockScreenActionTrayTest, ActionAvailableStateSetWithClient) {
+  SetUserLoggedIn(true);
+
+  TrayAction* tray_action = Shell::Get()->tray_action();
+  TestTrayActionClient tray_action_client;
+  tray_action->SetClient(tray_action_client.CreateInterfacePtrAndBind(),
+                         mojom::TrayActionState::kAvailable);
+
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOCKED);
+  EXPECT_TRUE(GetTray()->visible());
+  ASSERT_FALSE(GetTray()->GetImageForTesting().isNull());
+
+  EXPECT_EQ(0, tray_action_client.action_requests_count());
+  ClickOnTray();
+  ASSERT_EQ(1, tray_action_client.action_requests_count());
+  EXPECT_TRUE(GetTray()->visible());
+  EXPECT_FALSE(GetTray()->GetImageForTesting().isNull());
+
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kActive);
+  EXPECT_FALSE(GetTray()->visible());
+}
+
+TEST_F(LockScreenActionTrayTest, ActionActiveState) {
+  SetUserLoggedIn(true);
+
+  TrayAction* tray_action = Shell::Get()->tray_action();
+  TestTrayActionClient tray_action_client;
+  tray_action->SetClient(tray_action_client.CreateInterfacePtrAndBind(),
+                         mojom::TrayActionState::kNotAvailable);
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOCKED);
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kActive);
+  EXPECT_FALSE(GetTray()->visible());
+}
+
+TEST_F(LockScreenActionTrayTest, ActionBackgroundState) {
+  SetUserLoggedIn(true);
+
+  TrayAction* tray_action = Shell::Get()->tray_action();
+  TestTrayActionClient tray_action_client;
+  tray_action->SetClient(tray_action_client.CreateInterfacePtrAndBind(),
+                         mojom::TrayActionState::kNotAvailable);
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOCKED);
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kBackground);
+  EXPECT_FALSE(GetTray()->visible());
+}
+
+TEST_F(LockScreenActionTrayTest, LaunchingState) {
+  SetUserLoggedIn(true);
+
+  TrayAction* tray_action = Shell::Get()->tray_action();
+  TestTrayActionClient tray_action_client;
+  tray_action->SetClient(tray_action_client.CreateInterfacePtrAndBind(),
+                         mojom::TrayActionState::kNotAvailable);
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOCKED);
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kLaunching);
+  EXPECT_TRUE(GetTray()->visible());
+  ASSERT_FALSE(GetTray()->GetImageForTesting().isNull());
+
+  EXPECT_EQ(0, tray_action_client.action_requests_count());
+  ClickOnTray();
+  // Clicking on the item while the action is launching should not repeat action
+  // request.
+  ASSERT_EQ(0, tray_action_client.action_requests_count());
+  EXPECT_TRUE(GetTray()->visible());
+  EXPECT_FALSE(GetTray()->GetImageForTesting().isNull());
+
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kActive);
+  EXPECT_FALSE(GetTray()->visible());
+}
+
+TEST_F(LockScreenActionTrayTest, TrayNotVisibleWhenSessionNotLocked) {
+  TrayAction* tray_action = Shell::Get()->tray_action();
+  TestTrayActionClient tray_action_client;
+  tray_action->SetClient(tray_action_client.CreateInterfacePtrAndBind(),
+                         mojom::TrayActionState::kAvailable);
+  EXPECT_FALSE(GetTray()->visible());
+
+  SetUserLoggedIn(true);
+  EXPECT_FALSE(GetTray()->visible());
+
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOCKED);
+  EXPECT_TRUE(GetTray()->visible());
+
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::ACTIVE);
+  EXPECT_FALSE(GetTray()->visible());
+
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kAvailable);
+  EXPECT_FALSE(GetTray()->visible());
+
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kLaunching);
+  EXPECT_FALSE(GetTray()->visible());
+
+  GetSessionControllerClient()->SetSessionState(
+      session_manager::SessionState::LOCKED);
+  EXPECT_TRUE(GetTray()->visible());
+
+  SetUserLoggedIn(false);
+  EXPECT_FALSE(GetTray()->visible());
+}
+
+}  // namespace ash
