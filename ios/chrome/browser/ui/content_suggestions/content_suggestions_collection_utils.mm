ce2b9a54654ba24f8e8c85e04fdc07527d9752c9
gambard <gambard@chromium.org>
2017-05-10 08:41:53 -0700

    Move the fake omnibox constraints out of GoogleLandingViewController
    In order to refactor GoogleLandingViewController, the configuration of the
    elements displayed in the fake omnibox are moved to a helper.
    
    BUG=717974
    
    Review-Url: https://codereview.chromium.org/2855113003
    Cr-Commit-Position: refs/heads/master@{#470583}
    
    Former-commit-id: 02b01701047edf186f7d4164af8bcc6a0c78594f

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.mm
@@ -4,9 +4,16 @@
 
 #import "ios/chrome/browser/ui/content_suggestions/content_suggestions_collection_utils.h"
 
+#include "base/i18n/rtl.h"
 #include "base/logging.h"
+#include "components/strings/grit/components_strings.h"
+#include "ios/chrome/browser/ui/commands/ios_command_ids.h"
 #import "ios/chrome/browser/ui/content_suggestions/cells/content_suggestions_most_visited_item.h"
+#import "ios/chrome/browser/ui/toolbar/web_toolbar_controller.h"
 #include "ios/chrome/browser/ui/ui_util.h"
+#include "ios/chrome/grit/ios_strings.h"
+#import "ios/third_party/material_components_ios/src/components/Typography/src/MaterialTypography.h"
+#include "ui/base/l10n/l10n_util.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -20,6 +27,8 @@ const CGFloat kMaxSearchFieldFrameMargin = 200;
 const CGFloat kDoodleTopMarginIPadPortrait = 82;
 const CGFloat kDoodleTopMarginIPadLandscape = 82;
 const CGFloat kNTPSearchFieldBottomPadding = 16;
+
+const CGFloat kVoiceSearchButtonWidth = 48;
 
 // Height for the doodle frame when Google is not the default search engine.
 const CGFloat kNonGoogleSearchDoodleHeight = 60;
@@ -132,6 +141,57 @@ CGFloat heightForLogoHeader(CGFloat width,
   }
 
   return headerHeight;
+}
+
+void configureSearchHintLabel(UILabel* searchHintLabel,
+                              UIButton* searchTapTarget,
+                              CGFloat searchFieldWidth) {
+  CGRect hintFrame = CGRectInset(searchTapTarget.bounds, 12, 3);
+  const CGFloat kVoiceSearchOffset = 48;
+  hintFrame.size.width = searchFieldWidth - kVoiceSearchOffset;
+  [searchHintLabel setTranslatesAutoresizingMaskIntoConstraints:NO];
+
+  [searchTapTarget addSubview:searchHintLabel];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [searchHintLabel.heightAnchor
+        constraintEqualToConstant:hintFrame.size.height],
+    [searchHintLabel.centerYAnchor
+        constraintEqualToAnchor:searchTapTarget.centerYAnchor]
+  ]];
+
+  [searchHintLabel setText:l10n_util::GetNSString(IDS_OMNIBOX_EMPTY_HINT)];
+  if (base::i18n::IsRTL()) {
+    [searchHintLabel setTextAlignment:NSTextAlignmentRight];
+  }
+  [searchHintLabel
+      setTextColor:[UIColor
+                       colorWithWhite:kiPhoneOmniboxPlaceholderColorBrightness
+                                alpha:1.0]];
+  [searchHintLabel setFont:[MDCTypography subheadFont]];
+}
+
+void configureVoiceSearchButton(UIButton* voiceSearchButton,
+                                UIButton* searchTapTarget) {
+  UIImage* micImage = [UIImage imageNamed:@"voice_icon"];
+  [voiceSearchButton setTranslatesAutoresizingMaskIntoConstraints:NO];
+  [searchTapTarget addSubview:voiceSearchButton];
+
+  [NSLayoutConstraint activateConstraints:@[
+    [voiceSearchButton.centerYAnchor
+        constraintEqualToAnchor:searchTapTarget.centerYAnchor],
+    [voiceSearchButton.widthAnchor
+        constraintEqualToConstant:kVoiceSearchButtonWidth],
+    [voiceSearchButton.heightAnchor
+        constraintEqualToAnchor:voiceSearchButton.widthAnchor],
+  ]];
+
+  [voiceSearchButton setAdjustsImageWhenHighlighted:NO];
+  [voiceSearchButton setImage:micImage forState:UIControlStateNormal];
+  [voiceSearchButton setTag:IDC_VOICE_SEARCH];
+  [voiceSearchButton setAccessibilityLabel:l10n_util::GetNSString(
+                                               IDS_IOS_ACCNAME_VOICE_SEARCH)];
+  [voiceSearchButton setAccessibilityIdentifier:@"Voice Search"];
 }
 
 }  // namespace content_suggestions
