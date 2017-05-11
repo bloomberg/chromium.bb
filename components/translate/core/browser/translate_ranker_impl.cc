b95462c47bd4a0c5193ac238112e4b27de5d6d21
rogerm <rogerm@chromium.org>
2017-05-11 15:16:40 -0700

    [translate] Defer start of translate ranker model load to first use.
    This CL is in response to perf regression (see referenced bugs).
    
    BUG=697947, 697665
    
    Review-Url: https://codereview.chromium.org/2871843003
    Cr-Commit-Position: refs/heads/master@{#471086}
    
    Former-commit-id: d2876f4ee1c1a01ee262ca55752503ba3a30a6d5

--- components/translate/core/browser/translate_ranker_impl.cc
+++ components/translate/core/browser/translate_ranker_impl.cc
@@ -152,8 +152,6 @@ TranslateRankerImpl::TranslateRankerImpl(const base::FilePath& model_path,
         base::Bind(&TranslateRankerImpl::OnModelAvailable,
                    weak_ptr_factory_.GetWeakPtr()),
         model_path, model_url, kUmaPrefix);
-    // Kick off the initial load from cache.
-    model_loader_->NotifyOfRankerActivity();
   }
 }
 
