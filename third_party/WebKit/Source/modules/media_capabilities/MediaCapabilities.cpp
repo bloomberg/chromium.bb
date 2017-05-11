6c6f01a909864072a1c1172380b89920e6496a11
mlamouri <mlamouri@chromium.org>
2017-05-11 07:25:20 -0700

    Media Capabilities: stricter checks for the media configuration inputs.
    This is checking the framerate value, the content type, etc.
    
    Spec change:
    https://github.com/WICG/media-capabilities/commit/741d312207bac0bf5a61dba539c0ee7890d98138
    
    BUG=690380
    
    Review-Url: https://codereview.chromium.org/2852563002
    Cr-Commit-Position: refs/heads/master@{#470946}
    
    Former-commit-id: b99daebfd25fdf8346fcef1110afd26bd971308a

--- third_party/WebKit/Source/modules/media_capabilities/MediaCapabilities.cpp
+++ third_party/WebKit/Source/modules/media_capabilities/MediaCapabilities.cpp
@@ -24,6 +24,62 @@ namespace blink {
 
 namespace {
 
+constexpr const char* kApplicationMimeTypePrefix = "application/";
+constexpr const char* kAudioMimeTypePrefix = "audio/";
+constexpr const char* kVideoMimeTypePrefix = "video/";
+constexpr const char* kCodecsMimeTypeParam = "codecs";
+
+bool IsValidMimeType(const String& content_type, const String& prefix) {
+  ParsedContentType parsed_content_type(content_type,
+                                        ParsedContentType::Mode::kStrict);
+  if (!parsed_content_type.IsValid())
+    return false;
+
+  if (!parsed_content_type.MimeType().StartsWith(prefix) &&
+      !parsed_content_type.MimeType().StartsWith(kApplicationMimeTypePrefix)) {
+    return false;
+  }
+
+  if (parsed_content_type.ParameterCount() > 1)
+    return false;
+
+  if (parsed_content_type.ParameterCount() == 1 &&
+      parsed_content_type.ParameterValueForName(kCodecsMimeTypeParam)
+          .IsNull()) {
+    return false;
+  }
+
+  return true;
+}
+
+bool IsValidMediaConfiguration(const MediaConfiguration& configuration) {
+  return configuration.hasAudio() || configuration.hasVideo();
+}
+
+bool IsValidVideoConfiguration(const VideoConfiguration& configuration) {
+  DCHECK(configuration.hasContentType());
+
+  if (!IsValidMimeType(configuration.contentType(), kVideoMimeTypePrefix))
+    return false;
+
+  DCHECK(configuration.hasFramerate());
+  if (!std::isfinite(configuration.framerate()) ||
+      configuration.framerate() <= 0) {
+    return false;
+  }
+
+  return true;
+}
+
+bool IsValidAudioConfiguration(const AudioConfiguration& configuration) {
+  DCHECK(configuration.hasContentType());
+
+  if (!IsValidMimeType(configuration.contentType(), kAudioMimeTypePrefix))
+    return false;
+
+  return true;
+}
+
 WebAudioConfiguration ToWebAudioConfiguration(
     const AudioConfiguration& configuration) {
   WebAudioConfiguration web_configuration;
@@ -32,9 +88,7 @@ WebAudioConfiguration ToWebAudioConfiguration(
   DCHECK(configuration.hasContentType());
   ParsedContentType parsed_content_type(configuration.contentType(),
                                         ParsedContentType::Mode::kStrict);
-
-  // TODO(chcunningham): Throw TypeError for invalid input.
-  // DCHECK(parsed_content_type.IsValid());
+  DCHECK(parsed_content_type.IsValid());
 
   DEFINE_STATIC_LOCAL(const String, codecs, ("codecs"));
   web_configuration.mime_type = parsed_content_type.MimeType().LowerASCII();
@@ -62,9 +116,7 @@ WebVideoConfiguration ToWebVideoConfiguration(
   DCHECK(configuration.hasContentType());
   ParsedContentType parsed_content_type(configuration.contentType(),
                                         ParsedContentType::Mode::kStrict);
-
-  // TODO(chcunningham): Throw TypeError for invalid input.
-  // DCHECK(parsed_content_type.IsValid());
+  DCHECK(parsed_content_type.IsValid());
 
   DEFINE_STATIC_LOCAL(const String, codecs, ("codecs"));
   web_configuration.mime_type = parsed_content_type.MimeType().LowerASCII();
@@ -86,8 +138,36 @@ WebVideoConfiguration ToWebVideoConfiguration(
 }
 
 WebMediaConfiguration ToWebMediaConfiguration(
-    const MediaConfiguration& configuration) {
+    const MediaDecodingConfiguration& configuration) {
   WebMediaConfiguration web_configuration;
+
+  // |type| is mandatory.
+  DCHECK(configuration.hasType());
+  if (configuration.type() == "file")
+    web_configuration.type = MediaConfigurationType::kFile;
+  else if (configuration.type() == "media-source")
+    web_configuration.type = MediaConfigurationType::kMediaSource;
+  else
+    NOTREACHED();
+
+  if (configuration.hasAudio()) {
+    web_configuration.audio_configuration =
+        ToWebAudioConfiguration(configuration.audio());
+  }
+
+  if (configuration.hasVideo()) {
+    web_configuration.video_configuration =
+        ToWebVideoConfiguration(configuration.video());
+  }
+
+  return web_configuration;
+}
+
+WebMediaConfiguration ToWebMediaConfiguration(
+    const MediaEncodingConfiguration& configuration) {
+  WebMediaConfiguration web_configuration;
+
+  // TODO(mcasas): parse and set the type for encoding.
 
   if (configuration.hasAudio()) {
     web_configuration.audio_configuration =
@@ -112,8 +192,29 @@ ScriptPromise MediaCapabilities::decodingInfo(
   ScriptPromiseResolver* resolver = ScriptPromiseResolver::Create(script_state);
   ScriptPromise promise = resolver->Promise();
 
-  // |type| is mandatory.
-  DCHECK(configuration.hasType());
+  if (!IsValidMediaConfiguration(configuration)) {
+    resolver->Reject(V8ThrowException::CreateTypeError(
+        script_state->GetIsolate(),
+        "The configuration dictionary has neither |video| nor |audio| "
+        "specified and needs at least one of them."));
+    return promise;
+  }
+
+  if (configuration.hasVideo() &&
+      !IsValidVideoConfiguration(configuration.video())) {
+    resolver->Reject(V8ThrowException::CreateTypeError(
+        script_state->GetIsolate(),
+        "The video configuration dictionary is not valid."));
+    return promise;
+  }
+
+  if (configuration.hasAudio() &&
+      !IsValidAudioConfiguration(configuration.audio())) {
+    resolver->Reject(V8ThrowException::CreateTypeError(
+        script_state->GetIsolate(),
+        "The audio configuration dictionary is not valid."));
+    return promise;
+  }
 
   Platform::Current()->MediaCapabilitiesClient()->DecodingInfo(
       ToWebMediaConfiguration(configuration),
@@ -129,11 +230,27 @@ ScriptPromise MediaCapabilities::encodingInfo(
   ScriptPromiseResolver* resolver = ScriptPromiseResolver::Create(script_state);
   ScriptPromise promise = resolver->Promise();
 
-  if (!configuration.hasVideo() && !configuration.hasAudio()) {
-    resolver->Reject(DOMException::Create(
-        kSyntaxError,
+  if (!IsValidMediaConfiguration(configuration)) {
+    resolver->Reject(V8ThrowException::CreateTypeError(
+        script_state->GetIsolate(),
         "The configuration dictionary has neither |video| nor |audio| "
         "specified and needs at least one of them."));
+    return promise;
+  }
+
+  if (configuration.hasVideo() &&
+      !IsValidVideoConfiguration(configuration.video())) {
+    resolver->Reject(V8ThrowException::CreateTypeError(
+        script_state->GetIsolate(),
+        "The video configuration dictionary is not valid."));
+    return promise;
+  }
+
+  if (configuration.hasAudio() &&
+      !IsValidAudioConfiguration(configuration.audio())) {
+    resolver->Reject(V8ThrowException::CreateTypeError(
+        script_state->GetIsolate(),
+        "The audio configuration dictionary is not valid."));
     return promise;
   }
 
