f071141cc19efd48bad074dc445c93b38dbbe63c
emx <emx@chromium.org>
2017-05-11 03:46:03 -0700

    Rename new_avatar_button.* to avatar_button in preparation for CL 2851543002
    BUG=635699
    
    Review-Url: https://codereview.chromium.org/2868293002
    Cr-Commit-Position: refs/heads/master@{#470908}
    
    Former-commit-id: defdfbc63904f49b29947f3a51babfe1f00f23b8

--- chrome/browser/ui/views/profiles/avatar_button.cc
+++ chrome/browser/ui/views/profiles/avatar_button.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/views/profiles/new_avatar_button.h"
+#include "chrome/browser/ui/views/profiles/avatar_button.h"
 
 #include <utility>
 
