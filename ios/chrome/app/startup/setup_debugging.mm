caa929e33c33719e56360828c4ebd8c0aa888855
jif <jif@chromium.org>
2017-05-11 02:22:49 -0700

    DCHECK when loading images that do not exist.
    Enabled only in debug mode.
    This works by swizzling [UIImage imageNamed:] and making sure that it returns
    a non-nil UIImage*.
    
    BUG=718850
    
    Review-Url: https://codereview.chromium.org/2863983002
    Cr-Commit-Position: refs/heads/master@{#470887}
    
    Former-commit-id: b75611794c4ee0538c0f62aaf75abaa409d8963d

--- ios/chrome/app/startup/setup_debugging.mm
+++ ios/chrome/app/startup/setup_debugging.mm
@@ -4,12 +4,61 @@
 
 #include "ios/chrome/app/startup/setup_debugging.h"
 
+#include <objc/runtime.h>
+
 #include "base/logging.h"
+#include "base/strings/sys_string_conversions.h"
 #include "components/crash/core/common/objc_zombie.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+namespace {
+
+#ifndef NDEBUG
+
+// Swizzles [UIImage imageNamed:] to trigger a DCHECK if an invalid image is
+// attempted to be loaded.
+void swizzleUIImageImageNamed() {
+  // Retained by the swizzle block.
+  NSMutableSet* whiteList = [NSMutableSet set];
+
+  // TODO(crbug.com/720337): Add missing image.
+  [whiteList addObject:@"card_close_button_pressed_incognito"];
+  // TODO(crbug.com/720355): Add missing image.
+  [whiteList addObject:@"find_close_pressed_incognito"];
+  // TODO(crbug.com/720338): Add missing images.
+  [whiteList addObject:@"glif-mic-to-dots-large_37"];
+  [whiteList addObject:@"glif-google-to-dots_28"];
+
+  // The original implementation of [UIImage imageNamed:].
+  // Called by the new implementation.
+  static IMP originalImp;
+  IMP* originalImpPtr = &originalImp;
+
+  id swizzleBlock = ^(id self, NSString* imageName) {
+    // Call the original [UIImage imageNamed:] method.
+    IMP imp = *originalImpPtr;
+    Class aClass = objc_getClass("UIImage");
+    UIImage* image = imp(aClass, @selector(imageNamed:), imageName);
+
+    if (![whiteList containsObject:imageName]) {
+      DCHECK(image) << "Missing image: " << base::SysNSStringToUTF8(imageName);
+    }
+    return image;
+  };
+
+  Method method = class_getClassMethod([UIImage class], @selector(imageNamed:));
+  DCHECK(method);
+
+  IMP blockImp = imp_implementationWithBlock(swizzleBlock);
+  originalImp = method_setImplementation(method, blockImp);
+}
+
+#endif
+
+}  // namespace
 
 @implementation SetupDebugging
 
@@ -18,6 +67,11 @@
 // TODO(crbug.com/663390): Consider enabling this on device builds too.
 #if TARGET_IPHONE_SIMULATOR
   DCHECK(ObjcEvilDoers::ZombieEnable(true, 10000));
+#endif
+
+// Enable the detection of missing image assets.
+#ifndef NDEBUG
+  swizzleUIImageImageNamed();
 #endif
 }
 
