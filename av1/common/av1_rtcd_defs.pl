ea1668708405cd0568d5bf2daa43e9ebaa288716
Alex Converse <aconverse@google.com>
2017-05-11 09:49:13 -0700

    Fix build with global motion disabled
    Change-Id: I1c00925f83c6a858b0e799ddd90f241570a40575

--- av1/common/av1_rtcd_defs.pl
+++ av1/common/av1_rtcd_defs.pl
@@ -617,7 +617,7 @@ if ((aom_config("CONFIG_WARPED_MOTION") eq "yes") ||
   }
 }
 
-if (aom_config("CONFIG_WARPED_MOTION") eq "yes" &&
+if (aom_config("CONFIG_GLOBAL_MOTION") eq "yes" &&
     aom_config("CONFIG_AV1_ENCODER") eq "yes") {
   add_proto qw/double compute_cross_correlation/, "unsigned char *im1, int stride1, int x1, int y1, unsigned char *im2, int stride2, int x2, int y2";
   specialize qw/compute_cross_correlation sse4_1/;
