4baeee38cfad564fa89dfdcfe08581290314b4f5
avayvod <avayvod@chromium.org>
2017-05-12 14:47:19 -0700

    [Android] Mark testExitOnBack as flaky
    BUG=721906
    NOTRY=true
    NOTREECHECKS=true
    TBR=peconn@chromium.org
    
    Review-Url: https://codereview.chromium.org/2883663002
    Cr-Commit-Position: refs/heads/master@{#471451}
    
    Former-commit-id: 2a59964048e9267ba29e710e5b719d9bdfee830e

--- chrome/android/javatests/src/org/chromium/chrome/browser/FullscreenWebContentsActivityTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/FullscreenWebContentsActivityTest.java
@@ -19,6 +19,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.ApplicationStatus;
 import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.FlakyTest;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.test.ChromeActivityTestRule;
 import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
@@ -137,6 +138,7 @@ public class FullscreenWebContentsActivityTest {
 
     @Test
     @MediumTest
+    @FlakyTest(message = "https://crbug.com/721906")
     public void testExitOnBack() throws Throwable {
         final FullscreenWebContentsActivity fullscreenActivity = enterFullscreen();
         mUiThreadTestRule.runOnUiThread(new Runnable() {
