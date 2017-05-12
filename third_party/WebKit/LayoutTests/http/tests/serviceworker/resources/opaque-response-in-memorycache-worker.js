a9da33a9bb5aab14eaaef1d2c9768803d8f79d37
mike <mike@mikepennisi.com>
2017-05-12 09:58:31 -0700

    Upstream service worker opaque-response-preloaded.https.html test to WPT
    - Re-locate file for eventual submission to the Web Platform Tests
      project
    - Add "clean up" logic to remove iframe from document
    - Remove implementation-specific details from in-line documentation
    
    BUG=688116, 625575
    R=falken@chromium.org
    
    Review-Url: https://codereview.chromium.org/2877673004
    Cr-Commit-Position: refs/heads/master@{#471341}
    
    Former-commit-id: 7794609a04bdf904491f8d9501f6cd5ecf39adde

--- third_party/WebKit/LayoutTests/http/tests/serviceworker/resources/opaque-response-in-memorycache-worker.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('fetch', event => {
-    if (!event.request.url.match(/opaque-response$/))
-      return;
-    event.respondWith(fetch("http://localhost:8000/serviceworker/resources/simple.txt", {mode: 'no-cors'}));
-  });
