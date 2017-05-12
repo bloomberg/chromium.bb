8fddebe980f3a8126ec5c81238d71c953199f478
wzang <wzang@chromium.org>
2017-05-12 14:36:22 -0700

    Fork PIN keyboard assets and create additional flag for lock screen
    Fork relevant assets (as follow-up to CL 2855883005) so that the new
    design of lock screen can be implemented in tandem with login screen.
    
    The recently added flag (kShowMdLogin) is reserved for views-based login
    and lock screen as they are being implemented.
    
    The new flag added by this CL (kShowNonViewMdLogin) is a temporary flag
    used until the views-based screens become available.
    
    The two flags should not be used together.
    
    BUG=718174
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2874303003
    Cr-Commit-Position: refs/heads/master@{#471449}
    
    Former-commit-id: be738df8f289bf20423865f1bc555f2976a2e921

--- chromeos/chromeos_switches.h
+++ chromeos/chromeos_switches.h
@@ -129,6 +129,7 @@ CHROMEOS_EXPORT extern const char kOobeGuestSession[];
 CHROMEOS_EXPORT extern const char kOobeSkipPostLogin[];
 CHROMEOS_EXPORT extern const char kOobeTimerInterval[];
 CHROMEOS_EXPORT extern const char kShowMdLogin[];
+CHROMEOS_EXPORT extern const char kShowNonViewMdLogin[];
 CHROMEOS_EXPORT extern const char kPowerStub[];
 CHROMEOS_EXPORT extern const char kShillStub[];
 CHROMEOS_EXPORT extern const char kSmsTestMessages[];
