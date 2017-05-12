1d25c047565a2abf8f2784d4b7f2877183671e01
rjkroege <rjkroege@chromium.org>
2017-05-12 16:30:11 -0700

    Update ozone/drm proxy_helpers.h to support move-only types.
    As a helpful prerequisite for the use of mojo IPC in ozone/drm,
    support move only types in proxy_helpers.h callback sequences.
    
    BUG=620927
    
    Review-Url: https://codereview.chromium.org/2873963004
    Cr-Original-Commit-Position: refs/heads/master@{#470715}
    Committed: https://chromium.googlesource.com/chromium/src/+/cc8579da161fb3d2cd96e3a50fff1cbfe2a32695 [formerly a82a006f8420ecc7c75deab4f1e9d23c43616e1f]
    Review-Url: https://codereview.chromium.org/2873963004
    Cr-Commit-Position: refs/heads/master@{#471486}
    
    Former-commit-id: 2ae4e4bee29bcb52bc0dd21a2dd59551003cd678

--- ui/ozone/platform/drm/gpu/proxy_helpers.h
+++ ui/ozone/platform/drm/gpu/proxy_helpers.h
@@ -22,6 +22,15 @@ void PostAsyncTask(
   task_runner->PostTask(FROM_HERE, base::Bind(callback, args...));
 }
 
+template <typename... Args>
+void PostAsyncTaskOnce(
+    const scoped_refptr<base::SingleThreadTaskRunner>& task_runner,
+    base::OnceCallback<void(Args...)> callback,
+    Args... args) {
+  auto closure = base::BindOnce(std::move(callback), std::move(args)...);
+  task_runner->PostTask(FROM_HERE, std::move(closure));
+}
+
 }  // namespace internal
 
 // Posts a task to a different thread and blocks waiting for the task to finish
@@ -38,6 +47,17 @@ base::Callback<void(Args...)> CreateSafeCallback(
     const base::Callback<void(Args...)>& callback) {
   return base::Bind(&internal::PostAsyncTask<Args...>,
                     base::ThreadTaskRunnerHandle::Get(), callback);
+}
+
+// Creates a OnceCallback that will run |callback| on the calling thread. Useful
+// when posting a task on a different thread and expecting a callback when the
+// task finished (and the callback needs to run on the original thread).
+template <typename... Args>
+base::OnceCallback<void(Args...)> CreateSafeOnceCallback(
+    base::OnceCallback<void(Args...)> callback) {
+  return base::BindOnce(&internal::PostAsyncTaskOnce<Args...>,
+                        base::ThreadTaskRunnerHandle::Get(),
+                        std::move(callback));
 }
 
 }  // namespace ui
