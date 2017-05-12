694b7cc88e967110cbf70f4a24dae0f5202c9e1f
Rouslan Solomakhin <rouslan@chromium.org>
2017-05-12 10:46:11 -0400

    Prevent screenshots of the card editor views.
    Before this patch, users could take screenshots of the local credit card
    editor views, which show the full card number in plain text. (Chrome
    does not show the full number of the server cards.)
    
    This patch adds a FLAG_SECURE to the window of the local card editors
    for PaymentRequest and autofill. The flag is added for Chrome Beta and
    Stable builds to protect regular users. Dev, Canary, and developer
    builds do not have the FLAG_SECURE set.
    
    After this patch, users cannot take screenshots of the local card
    editor views in Chrome Beta and Stable.
    
    Bug: 721579
    Change-Id: I82dcd7c83cec85fb3f8dd58aa7ab25c2826641d0
    Reviewed-on: https://chromium-review.googlesource.com/504787
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#471807}
    Former-commit-id: 416588f2ea472b4f9273cbd0e40c1d18f71d1cba

--- chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
+++ chrome/android/java/src/org/chromium/chrome/browser/payments/ui/PaymentRequestUI.java
@@ -42,6 +42,7 @@ import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.Callback;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeVersionInfo;
 import org.chromium.chrome.browser.payments.ShippingStrings;
 import org.chromium.chrome.browser.payments.ui.PaymentRequestSection.LineItemBreakdownSection;
 import org.chromium.chrome.browser.payments.ui.PaymentRequestSection.OptionSection;
@@ -436,6 +437,11 @@ public class PaymentRequestUI implements DialogInterface.OnDismissListener, View
 
         mEditorView = new EditorView(activity, sObserverForTest);
         mCardEditorView = new EditorView(activity, sObserverForTest);
+
+        // Allow screenshots of the credit card number in Canary, Dev, and developer builds.
+        if (ChromeVersionInfo.isBetaBuild() || ChromeVersionInfo.isStableBuild()) {
+            mCardEditorView.disableScreenshots();
+        }
 
         // Set up the dialog.
         mDialog = new AlwaysDismissedDialog(activity, R.style.DialogWhenLarge);
