178e075bfda1919f5097b74c472294fa2b4fd9ff
xiaochengh <xiaochengh@chromium.org>
2017-05-12 02:03:01 -0700

    Make DragController::DragIsMove return false when selection does not have focus
    This patch makes the above change so that when dragging with an unfocused
    selection, the selection is not considered as the dragging source.
    
    BUG=717453
    TEST=editing/selection/drag_with_unfocused_selection.html
    
    Review-Url: https://codereview.chromium.org/2877623005
    Cr-Commit-Position: refs/heads/master@{#471253}
    
    Former-commit-id: 5fb678a9ee8b5e8e303ea0d66ae5efe979e6fcd2

--- third_party/WebKit/Source/core/page/DragController.cpp
+++ third_party/WebKit/Source/core/page/DragController.cpp
@@ -200,6 +200,7 @@ static DocumentFragment* DocumentFragmentFromDragData(
 bool DragController::DragIsMove(FrameSelection& selection,
                                 DragData* drag_data) {
   return document_under_mouse_ == drag_initiator_ &&
+         selection.SelectionHasFocus() &&
          selection.ComputeVisibleSelectionInDOMTreeDeprecated()
              .IsContentEditable() &&
          selection.ComputeVisibleSelectionInDOMTreeDeprecated().IsRange() &&
