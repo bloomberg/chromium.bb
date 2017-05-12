67f3015747d99eb6f4d89d105564225c3b3cd9e0
msarett <msarett@google.com>
2017-05-12 08:50:06 -0700

    Add flag to guard legacy image generator API
    NOTRY=true
    TBR=reed@google.com
    
    BUG=skia:6620
    
    Review-Url: https://codereview.chromium.org/2876163002
    Cr-Commit-Position: refs/heads/master@{#471317}
    
    Former-commit-id: 4ae6ee83354580dbf7fd4eeb2c0e9b030c6b3547

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -208,6 +208,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_USE_LEGACY_DISTANCE_FIELDS
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_IMGEN_API
+#define SK_SUPPORT_LEGACY_IMGEN_API
+#endif
+
 #ifndef SK_DISABLE_DEFERRED_PROXIES
 #define SK_DISABLE_DEFERRED_PROXIES
 #endif
