cdd332c7a2d6bb1edef68074ef6ff1ecb5d0ad6e
nednguyen <nednguyen@google.com>
2017-05-15 08:33:58 -0700

    Remove v8.top_25_smooth & v8.key_mobile_sites_smooth benchmarks
    These real world v8 benchmarks are now better covered by
    v8.browsing_desktop & v8.browsing_mobile.
    
    BUG=721413
    
    Review-Url: https://codereview.chromium.org/2877803003
    Cr-Commit-Position: refs/heads/master@{#471772}
    
    Former-commit-id: 12b214a95fb98f5ce9eb412d28cda654f49b4d08

--- tools/perf/benchmarks/v8.py
+++ tools/perf/benchmarks/v8.py
@@ -10,7 +10,6 @@ from page_sets import google_pages
 from benchmarks import v8_helper
 
 from measurements import v8_detached_context_age_in_gc
-from measurements import v8_gc_times
 import page_sets
 from telemetry import benchmark
 from telemetry import story
@@ -27,34 +26,6 @@ def CreateV8TimelineBasedMeasurementOptions():
   options = timeline_based_measurement.Options(category_filter)
   options.SetTimelineBasedMetrics(['executionMetric'])
   return options
-
-
-@benchmark.Disabled('win')        # crbug.com/416502
-@benchmark.Owner(emails=['hpayer@chromium.org', 'rmcilroy@chromium.org'])
-class V8Top25(perf_benchmark.PerfBenchmark):
-  """Measures V8 GC metrics on the while scrolling down the top 25 web pages.
-
-  http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
-  test = v8_gc_times.V8GCTimes
-  page_set = page_sets.V8Top25SmoothPageSet
-
-  @classmethod
-  def Name(cls):
-    return 'v8.top_25_smooth'
-
-
-@benchmark.Enabled('android')
-@benchmark.Owner(emails=['hpayer@chromium.org', 'rmcilroy@chromium.org'])
-class V8KeyMobileSites(perf_benchmark.PerfBenchmark):
-  """Measures V8 GC metrics on the while scrolling down key mobile sites.
-
-  http://www.chromium.org/developers/design-documents/rendering-benchmarks"""
-  test = v8_gc_times.V8GCTimes
-  page_set = page_sets.KeyMobileSitesSmoothPageSet
-
-  @classmethod
-  def Name(cls):
-    return 'v8.key_mobile_sites_smooth'
 
 
 @benchmark.Owner(emails=['ulan@chromium.org'])
