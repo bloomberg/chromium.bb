a283372bd383962b9108c10056c9cbc8fbb76201
nainar <nainar@chromium.org>
2017-05-14 21:17:38 -0700

    Move code from from ComputedStyleBase.cpp.tmpl to .h.tmpl
    This patch moves code from ComputedStyleBase.cpp.tmpl to the h.tmpl
    file in preparation to make it a template so that we can make
    ComputedStyle a templated argument and access both functions on
    ComputedStyleBase and ComputedStyle in the base class.
    
    Diff: https://gist.github.com/nainar/f7cb9953674378504ac6795be365bb1f/revisions
    
    BUG=710938
    
    Review-Url: https://codereview.chromium.org/2875233002
    Cr-Commit-Position: refs/heads/master@{#471664}
    
    Former-commit-id: 6f28abba06b92fafe20371b82d81b13e6a8b6d6c

--- third_party/WebKit/Source/build/scripts/make_computed_style_base.py
+++ third_party/WebKit/Source/build/scripts/make_computed_style_base.py
@@ -377,7 +377,6 @@ class ComputedStyleBaseWriter(make_style_builder.StyleBuilderWriter):
         self._include_paths = _get_include_paths(all_properties)
         self._outputs = {
             'ComputedStyleBase.h': self.generate_base_computed_style_h,
-            'ComputedStyleBase.cpp': self.generate_base_computed_style_cpp,
             'ComputedStyleBaseConstants.h': self.generate_base_computed_style_constants,
         }
 
@@ -387,14 +386,6 @@ class ComputedStyleBaseWriter(make_style_builder.StyleBuilderWriter):
             'properties': self._properties,
             'enums': self._generated_enums,
             'include_paths': self._include_paths,
-            'computed_style': self._root_group,
-        }
-
-    @template_expander.use_jinja('ComputedStyleBase.cpp.tmpl', tests={'in': lambda a, b: a in b})
-    def generate_base_computed_style_cpp(self):
-        return {
-            'properties': self._properties,
-            'enums': self._generated_enums,
             'computed_style': self._root_group,
         }
 
