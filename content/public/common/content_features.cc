18c16bc1c4d6ec8002d7374196afd99e514f5749
amp <amp@chromium.org>
2017-05-15 09:35:53 -0700

    Enable VrShell by default on Android.
    BUG=715612
    
    Review-Url: https://codereview.chromium.org/2871053002
    Cr-Commit-Position: refs/heads/master@{#471789}
    
    Former-commit-id: 4f5fc1b7bd67d34a07bdeb5c69cd8ae101d65083

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -244,7 +244,14 @@ const base::Feature kVibrateRequiresUserGesture{
     "VibrateRequiresUserGesture", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Enables VR UI.
-const base::Feature kVrShell{"VrShell", base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kVrShell {
+  "VrShell",
+#if defined(OS_ANDROID)
+      base::FEATURE_ENABLED_BY_DEFAULT
+#else
+      base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+};
 
 // Enable WebAssembly structured cloning.
 // http://webassembly.org/
