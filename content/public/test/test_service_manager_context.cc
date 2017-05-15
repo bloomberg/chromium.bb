3bd0ce233a134a4d8717f14b0e23d7f737a6f626
olegmax <olegmax@yandex-team.ru>
2017-05-15 01:26:12 -0700

    Isolate tests that initialize ServiceManagerConnection
    Some unit tests initialize ServiceManagerConnection and leave it initialized
    (e.g. in RenderProcessHostImpl::InitializeChannelProxy). On the other hand,
    tests that use TestServiceManagerContext/ServiceManagerContext expect
    ServiceManagerConnection to be unitialized on test start or
    DCHECK(!g_connection_for_process.Get()) fails.
    Change to explicitly uninitialize ServiceManagerConnection on creation of
    TestServiceManagerContext.
    
    BUG=717928,697217
    
    Review-Url: https://codereview.chromium.org/2861503003
    Cr-Commit-Position: refs/heads/master@{#471696}
    
    Former-commit-id: 5ffddc43be38516f89c6c631872285324778eb39

--- content/public/test/test_service_manager_context.cc
+++ content/public/test/test_service_manager_context.cc
@@ -6,11 +6,17 @@
 
 #include "content/browser/child_process_launcher.h"
 #include "content/browser/service_manager/service_manager_context.h"
+#include "content/public/common/service_manager_connection.h"
 
 namespace content {
 
-TestServiceManagerContext::TestServiceManagerContext()
-    : context_(new ServiceManagerContext) {}
+TestServiceManagerContext::TestServiceManagerContext() {
+  // Isolate from previous tests that may have already initialized
+  // ServiceManagerConnection (e.g. in
+  // RenderProcessHostImpl::InitializeChannelProxy()).
+  ServiceManagerConnection::DestroyForProcess();
+  context_.reset(new ServiceManagerContext);
+}
 
 TestServiceManagerContext::~TestServiceManagerContext() {
   ChildProcessLauncher::ResetRegisteredFilesForTesting();
