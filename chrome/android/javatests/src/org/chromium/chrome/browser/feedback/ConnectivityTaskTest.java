401543197998a23512c8693e7cf9ab0694351167
yolandyan <yolandyan@chromium.org>
2017-05-15 15:37:01 -0700

    Convert ConnectivityCheckerTestBase children to JUnit4
    For more on JUnit4 migration, please check src/testing/android/docs/junit4.md
    
    BUG=640116
    
    Review-Url: https://codereview.chromium.org/2878203002
    Cr-Commit-Position: refs/heads/master@{#471930}
    
    Former-commit-id: 1a2e9429dd31e0929621a54e7728dcfe3a58ddab

--- chrome/android/javatests/src/org/chromium/chrome/browser/feedback/ConnectivityTaskTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/feedback/ConnectivityTaskTest.java
@@ -4,10 +4,18 @@
 
 package org.chromium.chrome.browser.feedback;
 
+import static org.chromium.chrome.browser.feedback.ConnectivityCheckerTestRule.TIMEOUT_MS;
+
 import android.support.test.filters.MediumTest;
 import android.support.test.filters.SmallTest;
 
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import org.chromium.base.ThreadUtils;
+import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.Feature;
 import org.chromium.chrome.browser.feedback.ConnectivityTask.FeedbackData;
 import org.chromium.chrome.browser.feedback.ConnectivityTask.Type;
@@ -26,9 +34,15 @@ import java.util.concurrent.atomic.AtomicReference;
 /**
  * Tests for {@link ConnectivityTask}.
  */
-public class ConnectivityTaskTest extends ConnectivityCheckerTestBase {
+@RunWith(BaseJUnit4ClassRunner.class)
+public class ConnectivityTaskTest {
+    @Rule
+    public ConnectivityCheckerTestRule mConnectivityCheckerTestRule =
+            new ConnectivityCheckerTestRule();
+
     private static final int RESULT_CHECK_INTERVAL_MS = 10;
 
+    @Test
     @MediumTest
     @Feature({"Feedback"})
     public void testNormalCaseShouldWork() {
@@ -38,7 +52,9 @@ public class ConnectivityTaskTest extends ConnectivityCheckerTestBase {
                     public ConnectivityTask call() {
                         // Intentionally make HTTPS-connection fail which should result in
                         // NOT_CONNECTED.
-                        ConnectivityChecker.overrideUrlsForTest(mGenerate204Url, mGenerate404Url);
+                        ConnectivityChecker.overrideUrlsForTest(
+                                mConnectivityCheckerTestRule.getGenerated204Url(),
+                                mConnectivityCheckerTestRule.getGenerated404Url());
                         return ConnectivityTask.create(Profile.getLastUsedProfile(), TIMEOUT_MS,
                                 null);
                     }
@@ -52,12 +68,12 @@ public class ConnectivityTaskTest extends ConnectivityCheckerTestBase {
         }, TIMEOUT_MS, RESULT_CHECK_INTERVAL_MS);
         FeedbackData feedback = getResult(task);
         verifyConnections(feedback, ConnectivityCheckResult.NOT_CONNECTED);
-        assertEquals("The timeout value is wrong.", TIMEOUT_MS, feedback.getTimeoutMs());
+        Assert.assertEquals("The timeout value is wrong.", TIMEOUT_MS, feedback.getTimeoutMs());
     }
 
     private static void verifyConnections(FeedbackData feedback, int expectedHttpsValue) {
         Map<Type, Integer> results = feedback.getConnections();
-        assertEquals("Should have 4 results.", 4, results.size());
+        Assert.assertEquals("Should have 4 results.", 4, results.size());
         for (Map.Entry<Type, Integer> result : results.entrySet()) {
             switch (result.getKey()) {
                 case CHROME_HTTP:
@@ -69,18 +85,20 @@ public class ConnectivityTaskTest extends ConnectivityCheckerTestBase {
                     assertResult(expectedHttpsValue, result);
                     break;
                 default:
-                    fail("Failed to recognize type " + result.getKey());
+                    Assert.fail("Failed to recognize type " + result.getKey());
             }
         }
-        assertTrue("The elapsed time should be non-negative.", feedback.getElapsedTimeMs() >= 0);
+        Assert.assertTrue(
+                "The elapsed time should be non-negative.", feedback.getElapsedTimeMs() >= 0);
     }
 
     private static void assertResult(int expectedValue, Map.Entry<Type, Integer> actualEntry) {
-        assertEquals("Wrong result for " + actualEntry.getKey(),
+        Assert.assertEquals("Wrong result for " + actualEntry.getKey(),
                 ConnectivityTask.getHumanReadableResult(expectedValue),
                 ConnectivityTask.getHumanReadableResult(actualEntry.getValue()));
     }
 
+    @Test
     @SmallTest
     @Feature({"Feedback"})
     public void testCallbackNormalCaseShouldWork() throws InterruptedException {
@@ -98,18 +116,21 @@ public class ConnectivityTaskTest extends ConnectivityCheckerTestBase {
             @Override
             public void run() {
                 // Intentionally make HTTPS-connection fail which should result in NOT_CONNECTED.
-                ConnectivityChecker.overrideUrlsForTest(mGenerate204Url, mGenerate404Url);
+                ConnectivityChecker.overrideUrlsForTest(
+                        mConnectivityCheckerTestRule.getGenerated204Url(),
+                        mConnectivityCheckerTestRule.getGenerated404Url());
                 ConnectivityTask.create(Profile.getLastUsedProfile(), TIMEOUT_MS, callback);
             }
         });
         if (!semaphore.tryAcquire(TIMEOUT_MS, TimeUnit.MILLISECONDS)) {
-            fail("Failed to acquire semaphore.");
+            Assert.fail("Failed to acquire semaphore.");
         }
         FeedbackData feedback = feedbackRef.get();
         verifyConnections(feedback, ConnectivityCheckResult.NOT_CONNECTED);
-        assertEquals("The timeout value is wrong.", TIMEOUT_MS, feedback.getTimeoutMs());
+        Assert.assertEquals("The timeout value is wrong.", TIMEOUT_MS, feedback.getTimeoutMs());
     }
 
+    @Test
     @MediumTest
     @Feature({"Feedback"})
     public void testCallbackTwoTimeouts() throws InterruptedException {
@@ -128,20 +149,23 @@ public class ConnectivityTaskTest extends ConnectivityCheckerTestBase {
             @Override
             public void run() {
                 // Intentionally make HTTPS connections slow which should result in TIMEOUT.
-                ConnectivityChecker.overrideUrlsForTest(mGenerate204Url, mGenerateSlowUrl);
+                ConnectivityChecker.overrideUrlsForTest(
+                        mConnectivityCheckerTestRule.getGenerated204Url(),
+                        mConnectivityCheckerTestRule.getGeneratedSlowUrl());
                 ConnectivityTask.create(Profile.getLastUsedProfile(), checkTimeoutMs, callback);
             }
         });
         if (!semaphore.tryAcquire(TIMEOUT_MS, TimeUnit.MILLISECONDS)) {
-            fail("Failed to acquire semaphore.");
+            Assert.fail("Failed to acquire semaphore.");
         }
         FeedbackData feedback = feedbackRef.get();
         // In the case of a timeout when using callbacks, the result will be TIMEOUT instead
         // of UNKNOWN.
         verifyConnections(feedback, ConnectivityCheckResult.TIMEOUT);
-        assertEquals("The timeout value is wrong.", checkTimeoutMs, feedback.getTimeoutMs());
+        Assert.assertEquals("The timeout value is wrong.", checkTimeoutMs, feedback.getTimeoutMs());
     }
 
+    @Test
     @MediumTest
     @Feature({"Feedback"})
     public void testTwoTimeoutsShouldFillInTheRest() {
@@ -151,8 +175,9 @@ public class ConnectivityTaskTest extends ConnectivityCheckerTestBase {
                     public ConnectivityTask call() {
                         // Intentionally make HTTPS connections slow which should result in
                         // UNKNOWN.
-                        ConnectivityChecker.overrideUrlsForTest(mGenerate204Url,
-                                mGenerateSlowUrl);
+                        ConnectivityChecker.overrideUrlsForTest(
+                                mConnectivityCheckerTestRule.getGenerated204Url(),
+                                mConnectivityCheckerTestRule.getGeneratedSlowUrl());
                         return ConnectivityTask.create(Profile.getLastUsedProfile(), TIMEOUT_MS,
                                 null);
                     }
@@ -164,15 +189,16 @@ public class ConnectivityTaskTest extends ConnectivityCheckerTestBase {
                     return task.isDone();
                 }
             }, TIMEOUT_MS / 5, RESULT_CHECK_INTERVAL_MS);
-            fail("Should not be finished by now.");
+            Assert.fail("Should not be finished by now.");
         } catch (AssertionError e) {
             // TODO(tedchoc): This is horrible and should never timeout to determine success.
         }
         FeedbackData feedback = getResult(task);
         verifyConnections(feedback, ConnectivityCheckResult.UNKNOWN);
-        assertEquals("The timeout value is wrong.", TIMEOUT_MS, feedback.getTimeoutMs());
+        Assert.assertEquals("The timeout value is wrong.", TIMEOUT_MS, feedback.getTimeoutMs());
     }
 
+    @Test
     @SmallTest
     @Feature({"Feedback"})
     public void testFeedbackDataConversion() {
@@ -186,19 +212,19 @@ public class ConnectivityTaskTest extends ConnectivityCheckerTestBase {
                 new FeedbackData(connectionMap, 42, 21, ConnectionType.CONNECTION_WIFI);
         Map<String, String> map = feedback.toMap();
 
-        assertEquals("Should have 6 entries.", 6, map.size());
-        assertTrue(map.containsKey(ConnectivityTask.CHROME_HTTP_KEY));
-        assertEquals("NOT_CONNECTED", map.get(ConnectivityTask.CHROME_HTTP_KEY));
-        assertTrue(map.containsKey(ConnectivityTask.CHROME_HTTPS_KEY));
-        assertEquals("CONNECTED", map.get(ConnectivityTask.CHROME_HTTPS_KEY));
-        assertTrue(map.containsKey(ConnectivityTask.SYSTEM_HTTP_KEY));
-        assertEquals("UNKNOWN", map.get(ConnectivityTask.SYSTEM_HTTP_KEY));
-        assertTrue(map.containsKey(ConnectivityTask.SYSTEM_HTTPS_KEY));
-        assertEquals("CONNECTED", map.get(ConnectivityTask.SYSTEM_HTTPS_KEY));
-        assertTrue(map.containsKey(ConnectivityTask.CONNECTION_CHECK_ELAPSED_KEY));
-        assertEquals("21", map.get(ConnectivityTask.CONNECTION_CHECK_ELAPSED_KEY));
-        assertTrue(map.containsKey(ConnectivityTask.CONNECTION_TYPE_KEY));
-        assertEquals("WiFi", map.get(ConnectivityTask.CONNECTION_TYPE_KEY));
+        Assert.assertEquals("Should have 6 entries.", 6, map.size());
+        Assert.assertTrue(map.containsKey(ConnectivityTask.CHROME_HTTP_KEY));
+        Assert.assertEquals("NOT_CONNECTED", map.get(ConnectivityTask.CHROME_HTTP_KEY));
+        Assert.assertTrue(map.containsKey(ConnectivityTask.CHROME_HTTPS_KEY));
+        Assert.assertEquals("CONNECTED", map.get(ConnectivityTask.CHROME_HTTPS_KEY));
+        Assert.assertTrue(map.containsKey(ConnectivityTask.SYSTEM_HTTP_KEY));
+        Assert.assertEquals("UNKNOWN", map.get(ConnectivityTask.SYSTEM_HTTP_KEY));
+        Assert.assertTrue(map.containsKey(ConnectivityTask.SYSTEM_HTTPS_KEY));
+        Assert.assertEquals("CONNECTED", map.get(ConnectivityTask.SYSTEM_HTTPS_KEY));
+        Assert.assertTrue(map.containsKey(ConnectivityTask.CONNECTION_CHECK_ELAPSED_KEY));
+        Assert.assertEquals("21", map.get(ConnectivityTask.CONNECTION_CHECK_ELAPSED_KEY));
+        Assert.assertTrue(map.containsKey(ConnectivityTask.CONNECTION_TYPE_KEY));
+        Assert.assertEquals("WiFi", map.get(ConnectivityTask.CONNECTION_TYPE_KEY));
     }
 
     private static FeedbackData getResult(final ConnectivityTask task) {
