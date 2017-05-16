027bd88f7e829cfb79288d055e32b69e4ab0290e
fdoray <fdoray@chromium.org>
2017-05-16 02:31:14 -0700

    Use TaskScheduler instead of SequencedWorkerPool in snapshot_cache.mm.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=jif@chromium.org
    
    Review-Url: https://codereview.chromium.org/2881873002
    Cr-Commit-Position: refs/heads/master@{#472052}
    
    Former-commit-id: aa5299ae23a57ca5dce00a11da3ac8e73849351f

--- ios/chrome/browser/snapshots/snapshot_cache.mm
+++ ios/chrome/browser/snapshots/snapshot_cache.mm
@@ -18,6 +18,7 @@
 #include "base/mac/scoped_nsobject.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/task_runner_util.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_restrictions.h"
 #include "ios/chrome/browser/experimental_flags.h"
 #import "ios/chrome/browser/snapshots/lru_cache.h"
@@ -574,9 +575,10 @@ void ConvertAndSaveGreyImage(
     }
   }
 
-  web::WebThread::PostBlockingPoolTask(
-      FROM_HERE, base::Bind(&ConvertAndSaveGreyImage, colorImagePath,
-                            greyImagePath, backgroundingColorImage_));
+  base::PostTaskWithTraits(
+      FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_VISIBLE},
+      base::BindOnce(&ConvertAndSaveGreyImage, colorImagePath, greyImagePath,
+                     backgroundingColorImage_));
 }
 
 @end
