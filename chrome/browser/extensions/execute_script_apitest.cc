403a202f42ef5307a75e0d074370278e2ed3d3d1
bmcquade <bmcquade@chromium.org>
2017-05-16 09:12:01 -0700

    Revert of Disable flaky DestructiveScriptTest.MacrotaskRemoval (patchset #1 id:1 of https://codereview.chromium.org/2887683002/ )
    Reason for revert:
    Revert https://codereview.chromium.org/2885773002/ should address the flake, so this test can now be re-enabled.
    
    Original issue's description:
    > Disable flaky DestructiveScriptTest.MacrotaskRemoval
    >
    > TBR=jochen@chromium.org,zhenw@chromium.org
    > BUG=722703
    >
    > Review-Url: https://codereview.chromium.org/2887683002
    > Cr-Commit-Position: refs/heads/master@{#472103}
    > Committed: https://chromium.googlesource.com/chromium/src/+/4ccf5510aa46556c2e722574979bbce0a32d02a7 [formerly 28e83a520012c28bb95b0b41cac7a03071001a4f]
    
    TBR=kolos@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=722703
    
    Review-Url: https://codereview.chromium.org/2889633002
    Cr-Commit-Position: refs/heads/master@{#472123}
    
    Former-commit-id: e56b2dd082428125bac359c3511a2912d5ca3229

--- chrome/browser/extensions/execute_script_apitest.cc
+++ chrome/browser/extensions/execute_script_apitest.cc
@@ -135,8 +135,7 @@ IN_PROC_BROWSER_TEST_P(DestructiveScriptTest, MicrotaskRemoval) {
 }
 
 // Removes the frame at the frame's first scheduled macrotask.
-// Test is flaky. crbug.com/722703
-IN_PROC_BROWSER_TEST_P(DestructiveScriptTest, DISABLED_MacrotaskRemoval) {
+IN_PROC_BROWSER_TEST_P(DestructiveScriptTest, MacrotaskRemoval) {
   ASSERT_TRUE(RunSubtest("macrotask")) << message_;
 }
 
