bd14f76af58438c1604dac45d2d42cd35a5a4b5a
bsheedy <bsheedy@chromium.org>
2017-05-15 18:12:33 -0700

    Convert Vr tests to JUnit4
    patch from issue 2857583005 (http://crrev.com/2857583005)
    
    Convert Vr tests in chrome_public_test_vr_apk to JUnit4. For more detail,
    please check src/testing/android/docs/junit4.md
    
    Also removes the image tests from VrShellTest.java since:
    1. They provide no actual test coverage since Render Tests don't actually fail currently AND the way they captured screenshots is pretty much useless for VR (just captures a black screen)
    2. The move to JUnit4 seems to have messed with them - each call to renderAndCompare was taking an absurd amount of time (30+ seconds each).
    
    BUG=640116
    
    Review-Url: https://codereview.chromium.org/2876223004
    Cr-Commit-Position: refs/heads/master@{#471965}
    
    Former-commit-id: 64baf01809ec9265017ce344d306497b2f855d38

--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/VrShellTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/VrShellTest.java
@@ -4,31 +4,39 @@
 
 package org.chromium.chrome.browser.vr_shell;
 
+import static org.chromium.chrome.browser.vr_shell.VrTestRule.PAGE_LOAD_TIMEOUT_S;
 import static org.chromium.chrome.browser.vr_shell.VrUtils.POLL_CHECK_INTERVAL_LONG_MS;
 import static org.chromium.chrome.browser.vr_shell.VrUtils.POLL_TIMEOUT_LONG_MS;
 import static org.chromium.chrome.test.util.ChromeRestriction.RESTRICTION_TYPE_DEVICE_DAYDREAM;
 import static org.chromium.chrome.test.util.ChromeRestriction.RESTRICTION_TYPE_DEVICE_NON_DAYDREAM;
 import static org.chromium.chrome.test.util.ChromeRestriction.RESTRICTION_TYPE_VIEWER_DAYDREAM;
 
-import android.content.pm.ActivityInfo;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
 import android.support.test.filters.SmallTest;
 import android.view.ViewGroup;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.test.util.CommandLineFlags;
-import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.compositor.CompositorViewHolder;
 import org.chromium.chrome.browser.tabmodel.TabModelSelector;
-import org.chromium.chrome.test.util.RenderUtils.ViewRenderer;
+import org.chromium.chrome.test.ChromeActivityTestRule;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.chrome.test.ChromeTabbedActivityTestRule;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
 
-import java.io.IOException;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
@@ -38,25 +46,20 @@ import java.util.concurrent.atomic.AtomicReference;
  * interacting with WebVR in addition to the VR browser, so inherit from
  * VrTestBase for the WebVR test framework.
  */
-@CommandLineFlags.Add("enable-features=VrShell")
-public class VrShellTest extends VrTestBase {
-    private static final String GOLDEN_DIR =
-            "chrome/test/data/android/render_tests";
+@RunWith(ChromeJUnit4ClassRunner.class)
+
+@CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE,
+        ChromeActivityTestRule.DISABLE_NETWORK_PREDICTION_FLAG, "enable-features=VrShell"})
+public class VrShellTest {
+    @Rule
+    public ChromeTabbedActivityTestRule mActivityTestRule = new ChromeTabbedActivityTestRule();
 
     private VrShellDelegate mDelegate;
-    private ViewRenderer mViewRenderer;
 
-    @Override
-    protected void setUp() throws Exception {
-        super.setUp();
+    @Before
+    public void setUp() throws Exception {
+        mActivityTestRule.startMainActivityOnBlankPage();
         mDelegate = VrUtils.getVrShellDelegateInstance();
-    }
-
-    @Override
-    public void startMainActivity() throws InterruptedException {
-        startMainActivityOnBlankPage();
-        mViewRenderer = new ViewRenderer(getActivity(),
-                GOLDEN_DIR, this.getClass().getSimpleName());
     }
 
     private void enterExitVrMode(boolean supported) {
@@ -67,61 +70,23 @@ public class VrShellTest extends VrTestBase {
         VrUtils.forceEnterVr();
         if (supported) {
             VrUtils.waitForVrSupported(POLL_TIMEOUT_LONG_MS);
-            assertTrue(VrShellDelegate.isInVr());
+            Assert.assertTrue(VrShellDelegate.isInVr());
         } else {
-            assertFalse(mockApi.getLaunchInVrCalled());
-            assertFalse(VrShellDelegate.isInVr());
+            Assert.assertFalse(mockApi.getLaunchInVrCalled());
+            Assert.assertFalse(VrShellDelegate.isInVr());
         }
         VrUtils.forceExitVr(mDelegate);
-        assertFalse(VrShellDelegate.isInVr());
-    }
-
-    private void enterExitVrModeImage(boolean supported) throws IOException {
-        int prevOrientation = getActivity().getRequestedOrientation();
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        getInstrumentation().waitForIdleSync();
-        mViewRenderer.renderAndCompare(
-                getActivity().getWindow().getDecorView().getRootView(),
-                "blank_page");
-
-        VrUtils.forceEnterVr();
-        // Currently, screenshots only show the static UI overlay, not the
-        // actual content. Thus, 1:1 pixel checking is reliable until a
-        // way to take screenshots of VR content is added, in which case
-        // % similarity or some other method will need to be used. We're
-        // assuming that if the UI overlay is visible, then the device has
-        // successfully entered VR mode.
-        if (supported) {
-            VrUtils.waitForVrSupported(POLL_TIMEOUT_LONG_MS);
-            mViewRenderer.renderAndCompare(
-                    getActivity().getWindow().getDecorView().getRootView(),
-                    "vr_entered");
-        } else {
-            // TODO(bsheedy): Find a good way to wait before taking a screenshot
-            // when running on an unsupported device
-            mViewRenderer.renderAndCompare(
-                    getActivity().getWindow().getDecorView().getRootView(),
-                    "blank_page");
-        }
-
-        VrUtils.forceExitVr(mDelegate);
-        getActivity().setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        getInstrumentation().waitForIdleSync();
-        mViewRenderer.renderAndCompare(
-                getActivity().getWindow().getDecorView().getRootView(),
-                "blank_page");
-
-        getActivity().setRequestedOrientation(prevOrientation);
+        Assert.assertFalse(VrShellDelegate.isInVr());
     }
 
     private void enterVrModeNfc(boolean supported) {
-        getInstrumentation().waitForIdleSync();
-        VrUtils.simNfc(getActivity());
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+        VrUtils.simNfc(mActivityTestRule.getActivity());
         if (supported) {
             VrUtils.waitForVrSupported(POLL_TIMEOUT_LONG_MS);
-            assertTrue(VrShellDelegate.isInVr());
+            Assert.assertTrue(VrShellDelegate.isInVr());
         } else {
-            assertFalse(VrShellDelegate.isInVr());
+            Assert.assertFalse(VrShellDelegate.isInVr());
         }
         VrUtils.forceExitVr(mDelegate);
         // TODO(bsheedy): Figure out why NFC tests cause the next test to fail
@@ -134,6 +99,7 @@ public class VrShellTest extends VrTestBase {
      * NFC tag is scanned on a Daydream-ready device. Requires that the phone
      * is unlocked.
      */
+    @Test
     @Restriction({RESTRICTION_TYPE_DEVICE_DAYDREAM, RESTRICTION_TYPE_VIEWER_DAYDREAM})
     @MediumTest
     public void testSimNfcSupported() {
@@ -144,6 +110,7 @@ public class VrShellTest extends VrTestBase {
      * Verifies that the browser does not enter VR mode on Non-Daydream-ready
      * devices when the Daydream headset NFC tag is scanned.
      */
+    @Test
     @Restriction(RESTRICTION_TYPE_DEVICE_NON_DAYDREAM)
     @SmallTest
     public void testSimNfcUnsupported() {
@@ -154,6 +121,7 @@ public class VrShellTest extends VrTestBase {
      * Verifies that browser successfully enters and exits VR mode when told to
      * on Daydream-ready devices. Requires that the phone is unlocked.
      */
+    @Test
     @Restriction(RESTRICTION_TYPE_DEVICE_DAYDREAM)
     @SmallTest
     public void testEnterExitVrModeSupported() {
@@ -163,6 +131,7 @@ public class VrShellTest extends VrTestBase {
     /**
      * Verifies that browser does not enter VR mode on Non-Daydream-ready devices.
      */
+    @Test
     @Restriction(RESTRICTION_TYPE_DEVICE_NON_DAYDREAM)
     @SmallTest
     public void testEnterExitVrModeUnsupported() {
@@ -170,41 +139,20 @@ public class VrShellTest extends VrTestBase {
     }
 
     /**
-     * Verifies that browser successfully enters and exits VR mode when told to
-     * on Daydream-ready devices via a screendiffing check.
-     * Requires that the phone is unlocked.
-     */
-    @Restriction(RESTRICTION_TYPE_DEVICE_DAYDREAM)
-    @Feature("RenderTest")
-    @MediumTest
-    public void testEnterExitVrModeSupportedImage() throws IOException {
-        enterExitVrModeImage(true);
-    }
-
-    /**
-     * Verifies that browser does not enter VR mode on Non-Daydream-ready devices
-     * via a screendiffing check. Requires that the phone is unlocked.
-     */
-    @Restriction(RESTRICTION_TYPE_DEVICE_NON_DAYDREAM)
-    @Feature("RenderTest")
-    @MediumTest
-    public void testEnterExitVrModeUnsupportedImage() throws IOException {
-        enterExitVrModeImage(false);
-    }
-
-    /**
      * Verifies that swiping up/down on the Daydream controller's touchpad scrolls
      * the webpage while in the VR browser.
      */
+    @Test
     @Restriction({RESTRICTION_TYPE_DEVICE_DAYDREAM, RESTRICTION_TYPE_VIEWER_DAYDREAM})
     @MediumTest
     public void testControllerScrolling() throws InterruptedException {
         // Load page in VR and make sure the controller is pointed at the content quad
-        loadUrl("chrome://credits", PAGE_LOAD_TIMEOUT_S);
+        mActivityTestRule.loadUrl("chrome://credits", PAGE_LOAD_TIMEOUT_S);
         VrUtils.forceEnterVr();
         VrUtils.waitForVrSupported(POLL_TIMEOUT_LONG_MS);
-        EmulatedVrController controller = new EmulatedVrController(getActivity());
-        final ContentViewCore cvc = getActivity().getActivityTab().getActiveContentViewCore();
+        EmulatedVrController controller = new EmulatedVrController(mActivityTestRule.getActivity());
+        final ContentViewCore cvc =
+                mActivityTestRule.getActivity().getActivityTab().getActiveContentViewCore();
         controller.recenterView();
 
         // Wait for the page to be scrollable
@@ -222,13 +170,13 @@ public class VrShellTest extends VrTestBase {
         int scrollSpeed = 60;
         controller.scrollDown(scrollSteps, scrollSpeed);
         int endScrollY = cvc.getNativeScrollYForTest();
-        assertTrue("Controller was able to scroll down", startScrollY < endScrollY);
+        Assert.assertTrue("Controller was able to scroll down", startScrollY < endScrollY);
 
         // Test that scrolling up works
         startScrollY = endScrollY;
         controller.scrollUp(scrollSteps, scrollSpeed);
         endScrollY = cvc.getNativeScrollYForTest();
-        assertTrue("Controller was able to scroll up", startScrollY > endScrollY);
+        Assert.assertTrue("Controller was able to scroll up", startScrollY > endScrollY);
     }
 
     /**
@@ -237,6 +185,7 @@ public class VrShellTest extends VrTestBase {
      * @throws InterruptedException
      * @throws TimeoutException
      */
+    @Test
     @SmallTest
     @RetryOnFailure
     public void testResizeWithCompositorViewHolderDetached()
@@ -244,13 +193,15 @@ public class VrShellTest extends VrTestBase {
         final AtomicReference<TabModelSelector> selector = new AtomicReference<>();
         final AtomicReference<Integer> oldWidth = new AtomicReference<>();
         final int testWidth = 123;
-        final ContentViewCore cvc = getActivity().getActivityTab().getActiveContentViewCore();
+        final ContentViewCore cvc =
+                mActivityTestRule.getActivity().getActivityTab().getActiveContentViewCore();
 
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
-                CompositorViewHolder compositorViewHolder = (CompositorViewHolder)
-                        getActivity().findViewById(R.id.compositor_view_holder);
+                CompositorViewHolder compositorViewHolder =
+                        (CompositorViewHolder) mActivityTestRule.getActivity().findViewById(
+                                R.id.compositor_view_holder);
                 selector.set(compositorViewHolder.detachForVr());
                 oldWidth.set(cvc.getViewportWidthPix());
 
@@ -263,20 +214,22 @@ public class VrShellTest extends VrTestBase {
         CriteriaHelper.pollUiThread(Criteria.equals(testWidth, new Callable<Integer>() {
             @Override
             public Integer call() {
-                return getActivity().findViewById(R.id.compositor_view_holder).getMeasuredWidth();
+                return mActivityTestRule.getActivity()
+                        .findViewById(R.id.compositor_view_holder)
+                        .getMeasuredWidth();
             }
         }));
 
-        assertEquals("Viewport width should not have changed when resizing a detached "
-                + "CompositorViewHolder",
-                cvc.getViewportWidthPix(),
-                oldWidth.get().intValue());
+        Assert.assertEquals("Viewport width should not have changed when resizing a detached "
+                        + "CompositorViewHolder",
+                cvc.getViewportWidthPix(), oldWidth.get().intValue());
 
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
-                CompositorViewHolder compositorViewHolder = (CompositorViewHolder) getActivity()
-                        .findViewById(R.id.compositor_view_holder);
+                CompositorViewHolder compositorViewHolder =
+                        (CompositorViewHolder) mActivityTestRule.getActivity().findViewById(
+                                R.id.compositor_view_holder);
                 compositorViewHolder.onExitVr(selector.get());
             }
         });
--- chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/VrTestRule.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/vr_shell/VrTestRule.java
@@ -9,9 +9,17 @@ import static org.chromium.chrome.browser.vr_shell.VrUtils.POLL_CHECK_INTERVAL_S
 import static org.chromium.chrome.browser.vr_shell.VrUtils.POLL_TIMEOUT_LONG_MS;
 import static org.chromium.chrome.browser.vr_shell.VrUtils.POLL_TIMEOUT_SHORT_MS;
 
+import android.app.Activity;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.Assert;
+import org.junit.rules.TestRule;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
+
 import org.chromium.base.Log;
 import org.chromium.base.test.util.UrlUtils;
-import org.chromium.chrome.test.ChromeTabbedActivityTestBase;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
 import org.chromium.content.browser.test.util.ClickUtils;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
@@ -42,22 +50,14 @@ import java.util.concurrent.TimeoutException;
  * are processed, the JavaScript code will automatically signal the Java code,
  * which can then grab the results and pass/fail the instrumentation test.
  */
-public class VrTestBase extends ChromeTabbedActivityTestBase {
-    private static final String TAG = "VrTestBase";
-    protected static final String TEST_DIR = "chrome/test/data/android/webvr_instrumentation";
-    protected static final int PAGE_LOAD_TIMEOUT_S = 10;
-
-    protected WebContents mWebContents;
+public class VrTestRule implements TestRule {
+    private static final String TAG = "VrTestRule";
+    static final String TEST_DIR = "chrome/test/data/android/webvr_instrumentation";
+    static final int PAGE_LOAD_TIMEOUT_S = 10;
 
     @Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        mWebContents = getActivity().getActivityTab().getWebContents();
-    }
-
-    @Override
-    public void startMainActivity() throws InterruptedException {
-        startMainActivityOnBlankPage();
+    public Statement apply(final Statement base, Description desc) {
+        return base;
     }
 
     /**
@@ -65,7 +65,7 @@ public class VrTestBase extends ChromeTabbedActivityTestBase {
      * @param testName The name of the test whose file will be retrieved
      * @return The file:// URL to the specified test file
      */
-    protected static String getHtmlTestFile(String testName) {
+    public static String getHtmlTestFile(String testName) {
         return "file://" + UrlUtils.getIsolatedTestFilePath(TEST_DIR) + "/html/" + testName
                 + ".html";
     }
@@ -76,7 +76,7 @@ public class VrTestBase extends ChromeTabbedActivityTestBase {
      * @param webContents The WebContents to run the JavaScript through
      * @return Whether a VRDisplay was found
      */
-    protected boolean vrDisplayFound(WebContents webContents) {
+    public boolean vrDisplayFound(WebContents webContents) {
         pollJavaScriptBoolean("vrDisplayPromiseDone", POLL_TIMEOUT_SHORT_MS, webContents);
         return !runJavaScriptOrFail("vrDisplay", POLL_TIMEOUT_SHORT_MS, webContents).equals("null");
     }
@@ -85,25 +85,25 @@ public class VrTestBase extends ChromeTabbedActivityTestBase {
      * Use to tap in the middle of the screen, triggering the canvas' onclick
      * to fulfil WebVR's gesture requirement for presenting.
      */
-    protected void enterVrTap() {
-        ClickUtils.mouseSingleClickView(
-                getInstrumentation(), getActivity().getWindow().getDecorView().getRootView());
+    public void enterVrTap(Activity activity) {
+        ClickUtils.mouseSingleClickView(InstrumentationRegistry.getInstrumentation(),
+                activity.getWindow().getDecorView().getRootView());
     }
 
     /**
      * Taps in the middle of the screen then waits for the JavaScript step to finish.
      * @param webContents The WebContents for the tab the JavaScript step is in
      */
-    protected void enterVrTapAndWait(WebContents webContents) {
-        enterVrTap();
+    public void enterVrTapAndWait(Activity activity, WebContents webContents) {
+        enterVrTap(activity);
         waitOnJavaScriptStep(webContents);
     }
 
     /**
      * Use to simulate a Daydream View NFC scan without blocking afterwards
      */
-    protected void simNfcScan() {
-        VrUtils.simNfc(getActivity());
+    public void simNfcScan(ChromeTabbedActivity activity) {
+        VrUtils.simNfc(activity);
     }
 
     /**
@@ -111,8 +111,8 @@ public class VrTestBase extends ChromeTabbedActivityTestBase {
      * WebContents to signal that it is done with the step.
      * @param webContents The WebContents for the JavaScript that will be polled
      */
-    protected void simNfcScanAndWait(WebContents webContents) {
-        simNfcScan();
+    public void simNfcScanAndWait(ChromeTabbedActivity activity, WebContents webContents) {
+        simNfcScan(activity);
         waitOnJavaScriptStep(webContents);
     }
 
@@ -125,12 +125,12 @@ public class VrTestBase extends ChromeTabbedActivityTestBase {
      * @param webContents The WebContents object to run the JavaScript in
      * @return The return value of the JavaScript
      */
-    protected String runJavaScriptOrFail(String js, int timeout, WebContents webContents) {
+    public String runJavaScriptOrFail(String js, int timeout, WebContents webContents) {
         try {
             return JavaScriptUtils.executeJavaScriptAndWaitForResult(
                     webContents, js, timeout, TimeUnit.MILLISECONDS);
         } catch (InterruptedException | TimeoutException e) {
-            fail("Fatal interruption or timeout running JavaScript: " + js);
+            Assert.fail("Fatal interruption or timeout running JavaScript: " + js);
         }
         return "Not reached";
     }
@@ -140,7 +140,7 @@ public class VrTestBase extends ChromeTabbedActivityTestBase {
      * @param webContents The WebContents for the tab to check results in
      * @return "Passed" if test passed, String with failure reason otherwise
      */
-    protected String checkResults(WebContents webContents) {
+    public String checkResults(WebContents webContents) {
         if (runJavaScriptOrFail("testPassed", POLL_TIMEOUT_SHORT_MS, webContents).equals("true")) {
             return "Passed";
         }
@@ -152,8 +152,8 @@ public class VrTestBase extends ChromeTabbedActivityTestBase {
      * setting the failure reason as the description if it didn't.
      * @param webContents The WebContents for the tab to check test results in
      */
-    protected void endTest(WebContents webContents) {
-        assertEquals("Passed", checkResults(webContents));
+    public void endTest(WebContents webContents) {
+        Assert.assertEquals("Passed", checkResults(webContents));
     }
 
     /**
@@ -164,7 +164,7 @@ public class VrTestBase extends ChromeTabbedActivityTestBase {
      * @param webContents The WebContents to run the JavaScript through
      * @return True if the boolean evaluated to true, false if timed out
      */
-    protected boolean pollJavaScriptBoolean(
+    public boolean pollJavaScriptBoolean(
             final String boolName, int timeoutMs, final WebContents webContents) {
         try {
             CriteriaHelper.pollInstrumentationThread(Criteria.equals(true, new Callable<Boolean>() {
@@ -192,8 +192,8 @@ public class VrTestBase extends ChromeTabbedActivityTestBase {
      * instead of timing out.
      * @param webContents The WebContents for the tab the JavaScript step is in
      */
-    protected void waitOnJavaScriptStep(WebContents webContents) {
-        assertTrue("Polling JavaScript boolean javascriptDone timed out",
+    public void waitOnJavaScriptStep(WebContents webContents) {
+        Assert.assertTrue("Polling JavaScript boolean javascriptDone timed out",
                 pollJavaScriptBoolean("javascriptDone", POLL_TIMEOUT_LONG_MS, webContents));
         // Reset the synchronization boolean
         runJavaScriptOrFail("javascriptDone = false", POLL_TIMEOUT_SHORT_MS, webContents);
@@ -204,7 +204,7 @@ public class VrTestBase extends ChromeTabbedActivityTestBase {
      * @param stepFunction The JavaScript step function to call
      * @param webContents The WebContents for the tab the JavaScript is in
      */
-    protected void executeStepAndWait(String stepFunction, WebContents webContents) {
+    public void executeStepAndWait(String stepFunction, WebContents webContents) {
         // Run the step and block
         JavaScriptUtils.executeJavaScript(webContents, stepFunction);
         waitOnJavaScriptStep(webContents);
