b9ec23ce4a3b97e344b9bafdcd7a7747ac7251c9
fserb <fserb@chromium.org>
2017-05-16 11:00:49 -0700

    Add FontFaceSource interface and make it available on Document
    Move document.fonts to a mixin instead of partial interface.
    
    Per spec: https://drafts.csswg.org/css-font-loading/#font-face-source
    
    BUG=722511
    
    Review-Url: https://codereview.chromium.org/2884053002
    Cr-Commit-Position: refs/heads/master@{#472154}
    
    Former-commit-id: 32dbbeab5cdae35fb757924d339b2d83bc4ea487

---
+++ third_party/WebKit/Source/core/css/FontFaceSource.cpp
@@ -0,0 +1,15 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/FontFaceSource.h"
+
+#include "core/css/FontFaceSet.h"
+
+namespace blink {
+
+FontFaceSet* FontFaceSource::fonts(Document& document) {
+  return FontFaceSet::From(document);
+}
+
+}  // namespace blink
