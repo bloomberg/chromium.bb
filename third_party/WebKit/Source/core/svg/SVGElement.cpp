1123cfc36d2d33c37a51ed6c90bd174690db6914
fs <fs@opera.com>
2017-05-15 17:22:06 -0700

    Use CorrespondingUseElement() in SVGElement::TreeScopeForIdResolution
    For elements sourced non-locally, CorrespondingElement() will give the
    TreeScope of the document it was sourced from rather than the TreeScope
    of the <use> element. Until we are able to resolve references at
    ComputedStyle resolution, attempt to use the host of the shadow tree,
    i.e the (outermost) <use> element. (This will not work when external
    paint servers are supported, or with a paint server defined in the
    referenced document.)
    
    BUG=721167
    
    Review-Url: https://codereview.chromium.org/2877973002
    Cr-Commit-Position: refs/heads/master@{#471955}
    
    Former-commit-id: 6d8df7c57b4cc1beb9816b7379d88a48650dd507

--- third_party/WebKit/Source/core/svg/SVGElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGElement.cpp
@@ -88,7 +88,7 @@ void SVGElement::AttachLayoutTree(const AttachContext& context) {
 
 TreeScope& SVGElement::TreeScopeForIdResolution() const {
   const SVGElement* tree_scope_element = this;
-  if (const SVGElement* element = CorrespondingElement())
+  if (const SVGElement* element = CorrespondingUseElement())
     tree_scope_element = element;
   return tree_scope_element->GetTreeScope();
 }
