f37a9db20c35341bada43e059b795fa92118f849
fdoray <fdoray@chromium.org>
2017-05-17 02:23:25 -0700

    Use TaskScheduler instead of SequencedWorkerPool in module_load_analyzer_win.cc.
    SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    
    BUG=667892
    R=proberge@chromium.org
    
    Review-Url: https://codereview.chromium.org/2875353004
    Cr-Commit-Position: refs/heads/master@{#472394}
    
    Former-commit-id: f83ea6fa55b3a7d2c8ee80003ec1e1b2d9fcddfc

--- chrome/browser/safe_browsing/incident_reporting/module_load_analyzer_win.cc
+++ chrome/browser/safe_browsing/incident_reporting/module_load_analyzer_win.cc
@@ -15,7 +15,7 @@
 #include "base/metrics/histogram_macros.h"
 #include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "chrome/browser/install_verification/win/module_info.h"
 #include "chrome/browser/install_verification/win/module_verification_common.h"
 #include "chrome/browser/safe_browsing/incident_reporting/incident_receiver.h"
@@ -111,12 +111,13 @@ void CheckModuleWhitelistOnIOThread(
                        suspicious_paths->size());
 
   if (!suspicious_paths->empty()) {
-    content::BrowserThread::GetBlockingPool()
-        ->PostWorkerTaskWithShutdownBehavior(
-            FROM_HERE, base::Bind(&ReportIncidentsForSuspiciousModules,
-                                  base::Passed(std::move(suspicious_paths)),
-                                  base::Passed(std::move(incident_receiver))),
-            base::SequencedWorkerPool::CONTINUE_ON_SHUTDOWN);
+    base::PostTaskWithTraits(
+        FROM_HERE,
+        {base::MayBlock(), base::TaskPriority::BACKGROUND,
+         base::TaskShutdownBehavior::CONTINUE_ON_SHUTDOWN},
+        base::BindOnce(&ReportIncidentsForSuspiciousModules,
+                       base::Passed(std::move(suspicious_paths)),
+                       base::Passed(std::move(incident_receiver))));
   }
 }
 
