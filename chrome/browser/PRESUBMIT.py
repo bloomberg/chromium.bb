306cd8ceebd8ef8bb04c95a45028c0adf0d12b26
dbeam <dbeam@chromium.org>
2017-05-17 15:24:29 -0700

    Move chrome/browser/web_dev_style/ to tools/ to use from more places
    There has always been code that should abide by the web development
    style guide[1] in ui/webui/ but hasn't been automatedly checked.
    
    Additionally, code from chrome/browser/ was moved to
    components/flags_ui/, components/version_ui/, components/about_ui/ a
    while ago to be used on iOS and was previously checked by presubmits but
    is no longer.
    
    This CL moves web_dev_style to a more generic location and hopefully
    makes it easier to check a whole CL's web resources with a single method
    call.
    
    R=tsergeant@chromium.org
    BUG=723234
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2890513003
    Cr-Commit-Position: refs/heads/master@{#472588}
    
    Former-commit-id: ccf7fefad401e05a493e59eabf3c99801fe5fd8a

--- chrome/browser/PRESUBMIT.py
+++ chrome/browser/PRESUBMIT.py
@@ -2,22 +2,7 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Presubmit script for Chromium browser code.
-
-This script currently checks HTML/CSS/JS files in resources/ and ui/webui/.
-
-See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
-for more details about the presubmit API built into depot_tools, and see
-https://chromium.googlesource.com/chromium/src/+/master/styleguide/web/web.md
-for the rules checked for here.
-"""
-
-def CheckChangeOnUpload(input_api, output_api):
-  return _CommonChecks(input_api, output_api)
-
-
-def CheckChangeOnCommit(input_api, output_api):
-  return _CommonChecks(input_api, output_api)
+"""Presubmit script for Chromium browser code."""
 
 def _RunHistogramChecks(input_api, output_api, histogram_name):
   try:
@@ -47,61 +32,12 @@ def _RunHistogramChecks(input_api, output_api, histogram_name):
 
 def _CommonChecks(input_api, output_api):
   """Checks common to both upload and commit."""
-  results = []
+  return _RunHistogramChecks(input_api, output_api, "BadMessageReasonChrome")
 
-  path = input_api.os_path
-  cwd = input_api.PresubmitLocalPath()
-  resources = path.join(cwd, 'resources')
-  webui = path.join(cwd, 'ui', 'webui')
 
-  affected_files = (f.AbsoluteLocalPath() for f in input_api.AffectedFiles())
+def CheckChangeOnUpload(input_api, output_api):
+  return _CommonChecks(input_api, output_api)
 
-  would_affect_tests = [
-      path.join(cwd, 'PRESUBMIT.py'),
-      path.join(cwd, 'test_presubmit.py'),
-  ]
-  would_affect_tests += input_api.glob(path.join(cwd, 'web_dev_style', '*.py'))
 
-  if any(f for f in affected_files if f in would_affect_tests):
-    tests = [path.join(cwd, 'test_presubmit.py')]
-    results.extend(
-        input_api.canned_checks.RunUnitTests(input_api, output_api, tests))
-
-  import sys
-  old_path = sys.path
-
-  try:
-    sys.path = [cwd] + old_path
-    from web_dev_style import (resource_checker, css_checker, html_checker,
-                               js_checker)
-
-    search_dirs = (resources, webui)
-    def _html_css_js_resource(p):
-      return p.endswith(('.html', '.css', '.js')) and p.startswith(search_dirs)
-
-    def _vulcanized_resource(p):
-      return p.endswith(('vulcanized.html', 'crisper.js'))
-
-    BLACKLIST = [
-        'chrome/browser/resources/pdf/index.html',
-        'chrome/browser/resources/pdf/index.js'
-    ]
-    def is_resource(maybe_resource):
-      return (maybe_resource.LocalPath() not in BLACKLIST and
-          not _vulcanized_resource(maybe_resource.LocalPath()) and
-          _html_css_js_resource(maybe_resource.AbsoluteLocalPath()))
-
-    results.extend(resource_checker.ResourceChecker(
-        input_api, output_api, file_filter=is_resource).RunChecks())
-    results.extend(css_checker.CSSChecker(
-        input_api, output_api, file_filter=is_resource).RunChecks())
-    results.extend(html_checker.HtmlChecker(
-        input_api, output_api, file_filter=is_resource).RunChecks())
-    results.extend(js_checker.JSChecker(
-        input_api, output_api, file_filter=is_resource).RunChecks())
-    results.extend(_RunHistogramChecks(input_api, output_api,
-      "BadMessageReasonChrome"))
-  finally:
-    sys.path = old_path
-
-  return results
+def CheckChangeOnCommit(input_api, output_api):
+  return _CommonChecks(input_api, output_api)
