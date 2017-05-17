8f50fec10b73855c28e449a116b0b99fc7923336
samans <samans@chromium.org>
2017-05-17 11:27:21 -0700

    Factor FrameData out of RendererCompositorFrameSink
    The same struct that holds surface information in
    RendererCompositorFrameSink will be used in the browser and sent over
    IPC in future CLs. This CL factors it out and also adds IPC param traits.
    
    Review-Url: https://codereview.chromium.org/2882303002
    Cr-Commit-Position: refs/heads/master@{#472506}
    
    Former-commit-id: a1d159d88e6b302061a629bdc368609ba50b08b6

--- content/common/common_param_traits_unittest.cc
+++ content/common/common_param_traits_unittest.cc
@@ -255,3 +255,38 @@ TEST(IPCMessageTest, SSLInfo) {
   ASSERT_EQ(in.ct_cert_policy_compliance, out.ct_cert_policy_compliance);
   ASSERT_EQ(in.ocsp_result, out.ocsp_result);
 }
+
+TEST(IPCMessageTest, RenderWidgetSurfaceProperties) {
+  content::RenderWidgetSurfaceProperties input;
+  input.size = gfx::Size(23, 45);
+  input.device_scale_factor = 0.8;
+#ifdef OS_ANDROID
+  input.top_controls_height = 16.5;
+  input.top_controls_shown_ratio = 0.4;
+  input.bottom_controls_height = 23.4;
+  input.bottom_controls_shown_ratio = 0.8;
+  input.selection.start.set_type(gfx::SelectionBound::Type::CENTER);
+  input.has_transparent_background = true;
+#endif
+
+  IPC::Message msg(1, 2, IPC::Message::PRIORITY_NORMAL);
+  IPC::ParamTraits<content::RenderWidgetSurfaceProperties>::Write(&msg, input);
+
+  content::RenderWidgetSurfaceProperties output;
+  base::PickleIterator iter(msg);
+  EXPECT_TRUE(IPC::ParamTraits<content::RenderWidgetSurfaceProperties>::Read(
+      &msg, &iter, &output));
+
+  EXPECT_EQ(input.size, output.size);
+  EXPECT_EQ(input.device_scale_factor, output.device_scale_factor);
+#ifdef OS_ANDROID
+  EXPECT_EQ(input.top_controls_height, output.top_controls_height);
+  EXPECT_EQ(input.top_controls_shown_ratio, output.top_controls_shown_ratio);
+  EXPECT_EQ(input.bottom_controls_height, output.bottom_controls_height);
+  EXPECT_EQ(input.bottom_controls_shown_ratio,
+            output.bottom_controls_shown_ratio);
+  EXPECT_EQ(input.selection, output.selection);
+  EXPECT_EQ(input.has_transparent_background,
+            output.has_transparent_background);
+#endif
+}
