41ec112253aebb1651c25a20875d354dcac4c315
samans <samans@chromium.org>
2017-05-17 16:26:52 -0700

    Send an empty frame in HardwareRenderer to force returning of resources
    After https://codereview.chromium.org/2856913002 where we got rid of
    sending an empty frame in HardwareRenderer resources are leaking
    because of an issue in cc. This CL switches back to submitting an
    "empty" frame to stop the leaks until we fix the issue in cc. This is
    not an exact revert of the culprit because that would break some CLs
    that landed after it.
    
    BUG=719379
    
    Review-Url: https://codereview.chromium.org/2887073003
    Cr-Commit-Position: refs/heads/master@{#472600}
    
    Former-commit-id: ff1bc521f21c078f2f8574eac0641edd8450367c

--- android_webview/browser/hardware_renderer.cc
+++ android_webview/browser/hardware_renderer.cc
@@ -166,6 +166,17 @@ void HardwareRenderer::AllocateSurface() {
 void HardwareRenderer::DestroySurface() {
   DCHECK(child_id_.is_valid());
 
+  // Submit an empty frame to force any existing resources to be returned.
+  gfx::Rect rect(frame_size_);
+  std::unique_ptr<cc::RenderPass> render_pass = cc::RenderPass::Create();
+  render_pass->SetNew(1, rect, rect, gfx::Transform());
+  cc::CompositorFrame frame;
+  frame.render_pass_list.push_back(std::move(render_pass));
+  // We submit without a prior BeginFrame, so acknowledge a manual BeginFrame.
+  frame.metadata.begin_frame_ack =
+      cc::BeginFrameAck::CreateManualAckWithDamage();
+  support_->SubmitCompositorFrame(child_id_, std::move(frame));
+
   surfaces_->RemoveChildId(cc::SurfaceId(frame_sink_id_, child_id_));
   support_->EvictCurrentSurface();
   child_id_ = cc::LocalSurfaceId();
