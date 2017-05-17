81e8a08646166750433fcb215d00deaf77c2ac37
liaoyuke <liaoyuke@chromium.org>
2017-05-17 13:15:43 -0700

    [ObjC ARC] Converts ios/chrome/browser/web:test_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2879423002
    Cr-Commit-Position: refs/heads/master@{#472545}
    
    Former-commit-id: 45371c8b1e01bac440bc20f29782ceec2580bbc7

--- ios/chrome/browser/web/chrome_web_test.mm
+++ ios/chrome/browser/web/chrome_web_test.mm
@@ -11,6 +11,10 @@
 #import "ios/chrome/browser/ui/fullscreen_controller.h"
 #import "ios/web/public/web_state/web_state.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 ChromeWebTest::~ChromeWebTest() {}
 
 ChromeWebTest::ChromeWebTest() : web::WebTestWithWebState() {
