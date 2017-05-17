c0c32dcf874d07166df609c8f632119e348d9e1c
Sami Kyostila <skyostil@chromium.org>
2017-05-17 14:21:55 +0100

    scheduler: Don't run Javascript idle tasks when there is pending input
    If there is pending input, deadline.timeRemaining will return zero when
    queried from within a JavaScript idle task. Because of this it's not
    helpful to even start running these types of idle tasks if we already
    know that there is pending input.
    
    BUG=721773
    
    Change-Id: I5576b1081f1efe61ad3901f68692b950163ea9e9
    Reviewed-on: https://chromium-review.googlesource.com/506157
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#473209}
    Former-commit-id: 2e545f253b342d81a9c2c27a83a0d08a1a0ab3ed

---
+++ third_party/WebKit/Source/core/dom/ScriptedIdleTaskControllerTest.cpp
@@ -0,0 +1,144 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/dom/ScriptedIdleTaskController.h"
+
+#include "core/dom/IdleRequestCallback.h"
+#include "core/dom/IdleRequestOptions.h"
+#include "core/testing/NullExecutionContext.h"
+#include "platform/testing/TestingPlatformSupport.h"
+#include "platform/wtf/CurrentTime.h"
+#include "public/platform/Platform.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+namespace {
+
+class MockScheduler final : public WebScheduler {
+ public:
+  MockScheduler(bool should_yield) : should_yield_(should_yield) {}
+  ~MockScheduler() override {}
+
+  // WebScheduler implementation:
+  WebTaskRunner* LoadingTaskRunner() override { return nullptr; }
+  WebTaskRunner* TimerTaskRunner() override { return nullptr; }
+  WebTaskRunner* CompositorTaskRunner() override { return nullptr; }
+  void Shutdown() override {}
+  bool ShouldYieldForHighPriorityWork() override { return should_yield_; }
+  bool CanExceedIdleDeadlineIfRequired() override { return false; }
+  void PostIdleTask(const WebTraceLocation&,
+                    WebThread::IdleTask* idle_task) override {
+    idle_task_.reset(idle_task);
+  }
+  void PostNonNestableIdleTask(const WebTraceLocation&,
+                               WebThread::IdleTask*) override {}
+  std::unique_ptr<WebViewScheduler> CreateWebViewScheduler(
+      InterventionReporter*,
+      WebViewScheduler::WebViewSchedulerSettings*) override {
+    return nullptr;
+  }
+  void SuspendTimerQueue() override {}
+  void ResumeTimerQueue() override {}
+  void AddPendingNavigation(
+      scheduler::RendererScheduler::NavigatingFrameType) override {}
+  void RemovePendingNavigation(
+      scheduler::RendererScheduler::NavigatingFrameType) override {}
+
+  void RunIdleTask() {
+    auto idle_task = std::move(idle_task_);
+    idle_task->Run(0);
+  }
+  bool HasIdleTask() const { return idle_task_.get(); }
+
+ private:
+  bool should_yield_;
+  std::unique_ptr<WebThread::IdleTask> idle_task_;
+
+  DISALLOW_COPY_AND_ASSIGN(MockScheduler);
+};
+
+class MockThread final : public WebThread {
+ public:
+  MockThread(bool should_yield) : scheduler_(should_yield) {}
+  ~MockThread() override {}
+  bool IsCurrentThread() const override { return true; }
+  WebScheduler* Scheduler() const override { return &scheduler_; }
+
+  void RunIdleTask() { scheduler_.RunIdleTask(); }
+  bool HasIdleTask() const { return scheduler_.HasIdleTask(); }
+
+ private:
+  mutable MockScheduler scheduler_;
+  DISALLOW_COPY_AND_ASSIGN(MockThread);
+};
+
+class MockPlatform : public TestingPlatformSupport {
+ public:
+  MockPlatform(bool should_yield) : thread_(should_yield) {}
+  ~MockPlatform() override {}
+  WebThread* CurrentThread() override { return &thread_; }
+
+  void RunIdleTask() { thread_.RunIdleTask(); }
+  bool HasIdleTask() const { return thread_.HasIdleTask(); }
+
+ private:
+  MockThread thread_;
+  DISALLOW_COPY_AND_ASSIGN(MockPlatform);
+};
+
+class MockIdleRequestCallback : public IdleRequestCallback {
+ public:
+  MOCK_METHOD1(handleEvent, void(IdleDeadline*));
+};
+
+}  // namespace
+
+class ScriptedIdleTaskControllerTest : public testing::Test {
+ public:
+  void SetUp() override { execution_context_ = new NullExecutionContext(); }
+
+ protected:
+  Persistent<ExecutionContext> execution_context_;
+};
+
+TEST_F(ScriptedIdleTaskControllerTest, RunCallback) {
+  ScopedTestingPlatformSupport<MockPlatform, bool> platform(false);
+  NullExecutionContext execution_context;
+  ScriptedIdleTaskController* controller =
+      ScriptedIdleTaskController::Create(execution_context_);
+
+  Persistent<MockIdleRequestCallback> callback(new MockIdleRequestCallback());
+  IdleRequestOptions options;
+  EXPECT_FALSE(platform->HasIdleTask());
+  int id = controller->RegisterCallback(callback, options);
+  EXPECT_TRUE(platform->HasIdleTask());
+  EXPECT_NE(0, id);
+
+  EXPECT_CALL(*callback, handleEvent(testing::_));
+  platform->RunIdleTask();
+  testing::Mock::VerifyAndClearExpectations(callback);
+  EXPECT_FALSE(platform->HasIdleTask());
+}
+
+TEST_F(ScriptedIdleTaskControllerTest, DontRunCallbackWhenAskedToYield) {
+  ScopedTestingPlatformSupport<MockPlatform, bool> platform(true);
+  NullExecutionContext execution_context;
+  ScriptedIdleTaskController* controller =
+      ScriptedIdleTaskController::Create(execution_context_);
+
+  Persistent<MockIdleRequestCallback> callback(new MockIdleRequestCallback());
+  IdleRequestOptions options;
+  int id = controller->RegisterCallback(callback, options);
+  EXPECT_NE(0, id);
+
+  EXPECT_CALL(*callback, handleEvent(testing::_)).Times(0);
+  platform->RunIdleTask();
+  testing::Mock::VerifyAndClearExpectations(callback);
+
+  // The idle task should have been reposted.
+  EXPECT_TRUE(platform->HasIdleTask());
+}
+
+}  // namespace blink
