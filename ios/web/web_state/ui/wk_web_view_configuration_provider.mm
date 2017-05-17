2c89390926b78623d2d4c8614c8f32f68fb58046
gab <gab@chromium.org>
2017-05-17 07:01:05 -0700

    Fix DCHECK_IS_ON() iOS build
    Extracted from https://codereview.chromium.org/2886803002/
    
    BUG=722827
    TBR=sdefresne
    
    Review-Url: https://codereview.chromium.org/2885403002
    Cr-Commit-Position: refs/heads/master@{#472445}
    
    Former-commit-id: ab611961b4bf7919c37a26491cc79376a4b07f7b

--- ios/web/web_state/ui/wk_web_view_configuration_provider.mm
+++ ios/web/web_state/ui/wk_web_view_configuration_provider.mm
@@ -87,18 +87,20 @@ WKWebViewConfigurationProvider::GetScriptMessageRouter() {
 
 void WKWebViewConfigurationProvider::Purge() {
   DCHECK([NSThread isMainThread]);
-#if !defined(NDEBUG) || !defined(DCHECK_ALWAYS_ON)  // Matches DCHECK_IS_ON.
+#if DCHECK_IS_ON()
   base::WeakNSObject<id> weak_configuration(configuration_);
   base::WeakNSObject<id> weak_router(router_);
   base::WeakNSObject<id> weak_process_pool([configuration_ processPool]);
-#endif  // !defined(NDEBUG) || defined(DCHECK_ALWAYS_ON)
+#endif  // DCHECK_IS_ON()
   configuration_.reset();
   router_.reset();
   // Make sure that no one retains configuration, router, processPool.
+#if DCHECK_IS_ON()
   DCHECK(!weak_configuration);
   DCHECK(!weak_router);
   // TODO(crbug.com/522672): Enable this DCHECK.
   // DCHECK(!weak_process_pool);
+#endif  // DCHECK_IS_ON()
 }
 
 }  // namespace web
