5e770e8cc1c89bf4f400a0fd926a67f950684a15
treib <treib@chromium.org>
2017-05-18 03:37:02 -0700

    Disable flaky test BackgroundTracingThrottleTimeElapsed on Linux
    TBR=dsinclair@chromium.org
    BUG=723933
    
    Review-Url: https://codereview.chromium.org/2891043003
    Cr-Commit-Position: refs/heads/master@{#472761}
    
    Former-commit-id: e9399f008302fcb62a257e67951497290bd7e93e

--- chrome/browser/tracing/chrome_tracing_delegate_browsertest.cc
+++ chrome/browser/tracing/chrome_tracing_delegate_browsertest.cc
@@ -151,8 +151,16 @@ IN_PROC_BROWSER_TEST_F(ChromeTracingDelegateBrowserTest,
       base::Closure(), content::BackgroundTracingManager::NO_DATA_FILTERING));
 }
 
+// Flaky on Linux. See https://crbug.com/723933.
+#if defined(OS_LINUX)
+#define MAYBE_BackgroundTracingThrottleTimeElapsed \
+  DISABLED_BackgroundTracingThrottleTimeElapsed
+#else
+#define MAYBE_BackgroundTracingThrottleTimeElapsed \
+  BackgroundTracingThrottleTimeElapsed
+#endif
 IN_PROC_BROWSER_TEST_F(ChromeTracingDelegateBrowserTest,
-                       BackgroundTracingThrottleTimeElapsed) {
+                       MAYBE_BackgroundTracingThrottleTimeElapsed) {
   base::RunLoop wait_for_upload;
 
   EXPECT_TRUE(StartPreemptiveScenario(
