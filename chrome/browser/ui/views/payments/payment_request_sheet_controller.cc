c78868e82a6d056d6cc8c79c404d438b1572ccd8
Anthony Vallee-Dubois <anthonyvd@chromium.org>
2017-05-18 09:59:46 -0400

    [Web Payments] Add separator to scroll view.
    Bug: 718548
    Change-Id: I02a89040c13fcb5b9e05846ef0fdc4b05e88d414
    Reviewed-on: https://chromium-review.googlesource.com/508252
    Commit-Queue: Anthony Vallee-Dubois <anthonyvd@chromium.org>
    Reviewed-by: Mathieu Perreault <mathp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#472809}
    Former-commit-id: c694a817a95ab321d824235acacebaebc6277e1b

--- chrome/browser/ui/views/payments/payment_request_sheet_controller.cc
+++ chrome/browser/ui/views/payments/payment_request_sheet_controller.cc
@@ -9,6 +9,7 @@
 #include "components/payments/content/payment_request.h"
 #include "components/strings/grit/components_strings.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/gfx/canvas.h"
 #include "ui/views/background.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/button/md_text_button.h"
@@ -17,6 +18,7 @@
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/fill_layout.h"
 #include "ui/views/layout/grid_layout.h"
+#include "ui/views/painter.h"
 
 namespace payments {
 
@@ -103,6 +105,69 @@ class SheetView : public views::View, public views::FocusTraversable {
   DISALLOW_COPY_AND_ASSIGN(SheetView);
 };
 
+// A scroll view that displays a separator on the bounds where content is
+// scrolled out of view. For example, if the view can be scrolled up to reveal
+// more content, the top of the content area will display a separator.
+class BorderedScrollView : public views::ScrollView {
+ public:
+  // The painter used by the scroll view to display the border.
+  class BorderedScrollViewBorderPainter : public views::Painter {
+   public:
+    BorderedScrollViewBorderPainter(SkColor color,
+                                    BorderedScrollView* scroll_view)
+        : color_(color), scroll_view_(scroll_view) {}
+    ~BorderedScrollViewBorderPainter() override {}
+
+   private:
+    // views::Painter:
+    gfx::Size GetMinimumSize() const override { return gfx::Size(0, 2); }
+
+    void Paint(gfx::Canvas* canvas, const gfx::Size& size) override {
+      if (scroll_view_->HasTopBorder()) {
+        canvas->Draw1pxLine(gfx::PointF(0, 0), gfx::PointF(size.width(), 0),
+                            color_);
+      }
+
+      if (scroll_view_->HasBottomBorder()) {
+        canvas->Draw1pxLine(gfx::PointF(0, size.height() - 1),
+                            gfx::PointF(size.width(), size.height() - 1),
+                            color_);
+      }
+    }
+
+   private:
+    SkColor color_;
+    // The scroll view that owns the border that owns this painter.
+    BorderedScrollView* scroll_view_;
+    DISALLOW_COPY_AND_ASSIGN(BorderedScrollViewBorderPainter);
+  };
+
+  BorderedScrollView() : views::ScrollView() {
+    SetBorder(views::CreateBorderPainter(
+        base::MakeUnique<BorderedScrollViewBorderPainter>(
+            GetNativeTheme()->GetSystemColor(
+                ui::NativeTheme::kColorId_SeparatorColor),
+            this),
+        gfx::Insets(1, 0, 1, 0)));
+  }
+
+  bool HasTopBorder() const {
+    gfx::Rect visible_rect = GetVisibleRect();
+    return visible_rect.y() > 0;
+  }
+
+  bool HasBottomBorder() const {
+    gfx::Rect visible_rect = GetVisibleRect();
+    return visible_rect.y() + visible_rect.height() < contents()->height();
+  }
+
+  // views::ScrollView:
+  void ScrollToPosition(views::ScrollBar* source, int position) override {
+    views::ScrollView::ScrollToPosition(source, position);
+    SchedulePaint();
+  }
+};
+
 }  // namespace
 
 PaymentRequestSheetController::PaymentRequestSheetController(
@@ -167,7 +232,7 @@ std::unique_ptr<views::View> PaymentRequestSheetController::CreateView() {
   pane_layout->AddView(content_view_);
   pane_->SizeToPreferredSize();
 
-  scroll_ = base::MakeUnique<views::ScrollView>();
+  scroll_ = base::MakeUnique<BorderedScrollView>();
   scroll_->set_owned_by_client();
   scroll_->EnableViewPortLayer();
   scroll_->set_hide_horizontal_scrollbar(true);
