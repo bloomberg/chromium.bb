dbfd248da4ccfffde461de856a4d8504e33d91a9
Derek Foreman <derekf@osg.samsung.com>
2017-05-19 10:42:07 -0500

    libweston: Allow compositor-wayland to use wl_surface_damage_buffer
    wl_surface_damage_buffer landed ages ago, but in order for GL to
    use it the client must bind a wl_compositor version >= 4 (the
    version where damage_buffer was introduced).
    
    This patch updates the bind version and allows
    eglSwapBuffersWithDamage to actually use the provided damage
    rectangles instead of performing full surface damage.
    
    Reviewed-by: Daniel Stone <daniels@collabora.com>

--- libweston/compositor-wayland.c
+++ libweston/compositor-wayland.c
@@ -2319,7 +2319,8 @@ registry_handle_global(void *data, struct wl_registry *registry, uint32_t name,
 	if (strcmp(interface, "wl_compositor") == 0) {
 		b->parent.compositor =
 			wl_registry_bind(registry, name,
-					 &wl_compositor_interface, 1);
+					 &wl_compositor_interface,
+					 MIN(version, 4));
 	} else if (strcmp(interface, "zxdg_shell_v6") == 0) {
 		b->parent.xdg_shell =
 			wl_registry_bind(registry, name,
