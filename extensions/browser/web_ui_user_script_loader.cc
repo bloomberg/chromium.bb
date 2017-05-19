712b92342b4817ad75b4deb43c7d2c776ff8fc3c
pmonette <pmonette@chromium.org>
2017-05-19 16:59:14 -0700

    Remove the usage of BrowserThread::FILE in web_ui_user_script_loader.cc
    The usage of BrowserThread::FILE is deprecated in favor of the
    post_task.h API.
    
    BUG=689520
    
    Review-Url: https://codereview.chromium.org/2898523002
    Cr-Commit-Position: refs/heads/master@{#473387}
    
    Former-commit-id: 40c99bcdd1a0c0bcd3dbcfd2c49733621eb40614

--- extensions/browser/web_ui_user_script_loader.cc
+++ extensions/browser/web_ui_user_script_loader.cc
@@ -4,25 +4,32 @@
 
 #include "extensions/browser/web_ui_user_script_loader.h"
 
+#include <set>
+#include <string>
 #include <utility>
 
 #include "base/bind.h"
+#include "base/memory/ref_counted.h"
+#include "base/sequenced_task_runner.h"
 #include "base/strings/string_util.h"
+#include "base/task_scheduler/post_task.h"
+#include "base/threading/sequenced_task_runner_handle.h"
 #include "content/public/browser/browser_context.h"
-#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_process_host.h"
 #include "extensions/browser/guest_view/web_view/web_ui/web_ui_url_fetcher.h"
 
 namespace {
 
-void SerializeOnFileThread(
+void SerializeOnBlockingTask(
+    scoped_refptr<base::SequencedTaskRunner> task_runner,
     std::unique_ptr<extensions::UserScriptList> user_scripts,
     extensions::UserScriptLoader::LoadScriptsCallback callback) {
   std::unique_ptr<base::SharedMemory> memory =
       extensions::UserScriptLoader::Serialize(*user_scripts);
-  content::BrowserThread::PostTask(
-      content::BrowserThread::UI, FROM_HERE,
-      base::Bind(callback, base::Passed(&user_scripts), base::Passed(&memory)));
+
+  task_runner->PostTask(
+      FROM_HERE, base::BindOnce(std::move(callback), std::move(user_scripts),
+                                std::move(memory)));
 }
 
 }  // namespace
@@ -68,7 +75,7 @@ void WebUIUserScriptLoader::LoadScripts(
     LoadScriptsCallback callback) {
   DCHECK(!user_scripts_cache_) << "Loading scripts in flight.";
   user_scripts_cache_.swap(user_scripts);
-  scripts_loaded_callback_ = callback;
+  scripts_loaded_callback_ = std::move(callback);
 
   // The total number of the tasks is used to trace whether all the fetches
   // are complete. Therefore, we store all the fetcher pointers in |fetchers_|
@@ -156,10 +163,9 @@ void WebUIUserScriptLoader::OnSingleWebUIURLFetchComplete(
 }
 
 void WebUIUserScriptLoader::OnWebUIURLFetchComplete() {
-  content::BrowserThread::PostTask(
-      content::BrowserThread::FILE, FROM_HERE,
-      base::Bind(&SerializeOnFileThread, base::Passed(&user_scripts_cache_),
-                 scripts_loaded_callback_));
-  scripts_loaded_callback_.Reset();
-  user_scripts_cache_.reset();
+  base::PostTaskWithTraits(
+      FROM_HERE, {base::MayBlock()},
+      base::BindOnce(
+          &SerializeOnBlockingTask, base::SequencedTaskRunnerHandle::Get(),
+          std::move(user_scripts_cache_), std::move(scripts_loaded_callback_)));
 }
