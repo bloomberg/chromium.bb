267f5ba8dbdc3b3607bcde80341f96b4744ae0c2
miguelg <miguelg@chromium.org>
2017-05-20 01:34:35 -0700

    Minimize the delegate dependencies for native extension notifications.
    Support for events like click, close etc is now fully handled by the
    ExtensionNotificationHandler like persistent web notifications.
    
    The delegate is now just a little shim to hold the delegate id since it
    is still required by the Notification class.
    
    BUG=720345
    
    Review-Url: https://codereview.chromium.org/2875673002
    Cr-Commit-Position: refs/heads/master@{#473427}
    
    Former-commit-id: dcc699ff151393a7d2fa29314befd57d194b0f60

--- chrome/browser/extensions/api/notifications/extension_notification_handler.cc
+++ chrome/browser/extensions/api/notifications/extension_notification_handler.cc
@@ -5,12 +5,119 @@
 #include "chrome/browser/extensions/api/notifications/extension_notification_handler.h"
 
 #include "base/logging.h"
+#include "base/strings/nullable_string16.h"
+#include "base/strings/string_piece.h"
+#include "chrome/browser/extensions/api/notifications/extension_notification_display_helper.h"
+#include "chrome/browser/extensions/api/notifications/extension_notification_display_helper_factory.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/common/extensions/api/notifications.h"
+#include "extensions/common/constants.h"
+#include "url/gurl.h"
+
+namespace extensions {
+
+namespace {
+
+std::string GetExtensionId(const std::string& extension_url) {
+  GURL url(extension_url);
+  if (!url.is_valid() || !url.SchemeIs(extensions::kExtensionScheme))
+    return "";
+  return url.GetOrigin().host_piece().as_string();
+}
+
+std::unique_ptr<base::ListValue> CreateBaseEventArgs(
+    const std::string& extension_id,
+    const std::string& scoped_notification_id) {
+  // Unscope the notification id before returning it.
+  size_t index_of_separator = extension_id.length() + 1;
+  DCHECK_LT(index_of_separator, scoped_notification_id.length());
+  std::string unscoped_notification_id =
+      scoped_notification_id.substr(index_of_separator);
+
+  std::unique_ptr<base::ListValue> args(new base::ListValue());
+  args->AppendString(unscoped_notification_id);
+  return args;
+}
+
+}  // namespace
 
 ExtensionNotificationHandler::ExtensionNotificationHandler() = default;
 
 ExtensionNotificationHandler::~ExtensionNotificationHandler() = default;
 
+void ExtensionNotificationHandler::OnClose(Profile* profile,
+                                           const std::string& origin,
+                                           const std::string& notification_id,
+                                           bool by_user) {
+  EventRouter::UserGestureState gesture =
+      by_user ? EventRouter::USER_GESTURE_ENABLED
+              : EventRouter::USER_GESTURE_NOT_ENABLED;
+  std::string extension_id(GetExtensionId(origin));
+  DCHECK(!extension_id.empty());
+
+  std::unique_ptr<base::ListValue> args(
+      CreateBaseEventArgs(extension_id, notification_id));
+  args->AppendBoolean(by_user);
+  SendEvent(profile, extension_id, events::NOTIFICATIONS_ON_CLOSED,
+            api::notifications::OnClosed::kEventName, gesture, std::move(args));
+
+  ExtensionNotificationDisplayHelper* display_helper =
+      ExtensionNotificationDisplayHelperFactory::GetForProfile(profile);
+  if (display_helper)
+    display_helper->EraseDataForNotificationId(notification_id);
+}
+
+void ExtensionNotificationHandler::OnClick(
+    Profile* profile,
+    const std::string& origin,
+    const std::string& notification_id,
+    int action_index,
+    const base::NullableString16& reply) {
+  DCHECK(reply.is_null());
+
+  std::string extension_id(GetExtensionId(origin));
+  std::unique_ptr<base::ListValue> args(
+      CreateBaseEventArgs(extension_id, notification_id));
+  if (action_index > -1)
+    args->AppendInteger(action_index);
+  events::HistogramValue histogram_value =
+      action_index > -1 ? events::NOTIFICATIONS_ON_BUTTON_CLICKED
+                        : events::NOTIFICATIONS_ON_CLICKED;
+  const std::string& event_name =
+      action_index > -1 ? api::notifications::OnButtonClicked::kEventName
+                        : api::notifications::OnClicked::kEventName;
+
+  SendEvent(profile, extension_id, histogram_value, event_name,
+            EventRouter::USER_GESTURE_ENABLED, std::move(args));
+}
+
 void ExtensionNotificationHandler::OpenSettings(Profile* profile) {
   // Extension notifications don't display a settings button.
   NOTREACHED();
 }
+
+void ExtensionNotificationHandler::RegisterNotification(
+    const std::string& notification_id,
+    NotificationDelegate* delegate) {}
+
+void ExtensionNotificationHandler::SendEvent(
+    Profile* profile,
+    const std::string& extension_id,
+    events::HistogramValue histogram_value,
+    const std::string& event_name,
+    EventRouter::UserGestureState user_gesture,
+    std::unique_ptr<base::ListValue> args) {
+  if (extension_id.empty())
+    return;
+
+  EventRouter* event_router = EventRouter::Get(profile);
+  if (!event_router)
+    return;
+
+  std::unique_ptr<Event> event(
+      new Event(histogram_value, event_name, std::move(args)));
+  event->user_gesture = user_gesture;
+  event_router->DispatchEventToExtension(extension_id, std::move(event));
+}
+
+}  // namespace extensions
--- chrome/browser/extensions/api/notifications/notifications_api.cc
+++ chrome/browser/extensions/api/notifications/notifications_api.cc
@@ -25,7 +25,9 @@
 #include "chrome/browser/notifications/notifier_state_tracker.h"
 #include "chrome/browser/notifications/notifier_state_tracker_factory.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_features.h"
 #include "chrome/common/extensions/api/notifications/notification_style.h"
+#include "chrome/common/features.h"
 #include "components/keyed_service/content/browser_context_keyed_service_shutdown_notifier_factory.h"
 #include "components/keyed_service/core/keyed_service_shutdown_notifier.h"
 #include "content/public/browser/render_process_host.h"
@@ -204,12 +206,38 @@ class ShutdownNotifierFactory
   DISALLOW_COPY_AND_ASSIGN(ShutdownNotifierFactory);
 };
 
-class NotificationsApiDelegate : public NotificationDelegate {
+// Temporary native notification api delagate, it is only used
+// to extract the delegate id.
+// This is an interim state until the work in
+// https://bugs.chromium.org/p/chromium/issues/detail?id=720345
+// is completed. We need a small delegate shim since the
+// Notification object has a non virtual method (delegate_id) that is
+// used all over the place whose implementation returns delegate->id()
+#if BUILDFLAG(ENABLE_NATIVE_NOTIFICATIONS)
+class NativeNotificationApiDelegate : public NotificationDelegate {
  public:
-  NotificationsApiDelegate(ChromeAsyncExtensionFunction* api_function,
-                           Profile* profile,
-                           const std::string& extension_id,
-                           const std::string& id)
+  NativeNotificationApiDelegate(const std::string& extension_id,
+                                const std::string& notification_id)
+      : scoped_notification_id_(
+            CreateScopedIdentifier(extension_id, notification_id)) {}
+
+  std::string id() const override { return scoped_notification_id_; }
+
+ private:
+  ~NativeNotificationApiDelegate() override = default;
+  const std::string scoped_notification_id_;
+
+  DISALLOW_COPY_AND_ASSIGN(NativeNotificationApiDelegate);
+};
+#endif  // BUILDFLAG(ENABLE_NATIVE_NOTIFICATIONS)
+
+// Message center based notification delegate with all the functionality.
+class NotificationApiDelegate : public NotificationDelegate {
+ public:
+  NotificationApiDelegate(ChromeAsyncExtensionFunction* api_function,
+                          Profile* profile,
+                          const std::string& extension_id,
+                          const std::string& id)
       : api_function_(api_function),
         event_router_(EventRouter::Get(profile)),
         display_helper_(
@@ -221,7 +249,7 @@ class NotificationsApiDelegate : public NotificationDelegate {
 
     shutdown_notifier_subscription_ =
         ShutdownNotifierFactory::GetInstance()->Get(profile)->Subscribe(
-            base::Bind(&NotificationsApiDelegate::Shutdown,
+            base::Bind(&NotificationApiDelegate::Shutdown,
                        base::Unretained(this)));
   }
 
@@ -292,7 +320,7 @@ class NotificationsApiDelegate : public NotificationDelegate {
   }
 
  private:
-  ~NotificationsApiDelegate() override {}
+  ~NotificationApiDelegate() override {}
 
   void SendEvent(events::HistogramValue histogram_value,
                  const std::string& name,
@@ -333,7 +361,7 @@ class NotificationsApiDelegate : public NotificationDelegate {
   std::unique_ptr<KeyedServiceShutdownNotifier::Subscription>
       shutdown_notifier_subscription_;
 
-  DISALLOW_COPY_AND_ASSIGN(NotificationsApiDelegate);
+  DISALLOW_COPY_AND_ASSIGN(NotificationApiDelegate);
 };
 
 }  // namespace
@@ -489,9 +517,20 @@ bool NotificationsApiFunction::CreateNotification(
   if (options->is_clickable.get())
     optional_fields.clickable = *options->is_clickable;
 
-  NotificationsApiDelegate* api_delegate(new NotificationsApiDelegate(
-      this, GetProfile(), extension_->id(), id));  // ownership is passed to
-                                                   // Notification
+  // Create the notification api delegate. Ownership passed to the notification.
+  NotificationDelegate* api_delegate;
+#if BUILDFLAG(ENABLE_NATIVE_NOTIFICATIONS)
+  if (base::FeatureList::IsEnabled(features::kNativeNotifications)) {
+    api_delegate = new NativeNotificationApiDelegate(extension_->id(), id);
+  } else {
+    api_delegate =
+        new NotificationApiDelegate(this, GetProfile(), extension_->id(), id);
+  }
+#else
+  api_delegate =
+      new NotificationApiDelegate(this, GetProfile(), extension_->id(), id);
+#endif  // BUILDFLAG(ENABLE_NATIVE_NOTIFICATIONS)
+
   Notification notification(
       type, title, message, icon,
       message_center::NotifierId(message_center::NotifierId::APPLICATION,
