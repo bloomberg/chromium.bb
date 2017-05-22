4a78cbd53bbe9d7f7f04af4be853f4fc70175809
Klaus Weidner <klausw@chromium.org>
2017-05-22 16:17:00 -0700

    Support using hash parameters for URL arguments
    Add support for using '#key=value" hash parameters as an
    alternative to the currently supported '?key=value' parameters.
    This simplifies local testing. Note that applications that
    only read parameters once on startup won't see new values
    until they reload.

--- samples/js/third-party/wglu/wglu-url.js
+++ samples/js/third-party/wglu/wglu-url.js
@@ -37,10 +37,15 @@ var WGLUUrl = (function() {
 
   var urlArgs = null;
 
+  window.onhashchange = function() {
+    // Force re-parsing on next access
+    urlArgs = null;
+  };
+
   function ensureArgsCached() {
     if (!urlArgs) {
       urlArgs = {};
-      var query = window.location.search.substring(1);
+      var query = window.location.search.substring(1) || window.location.hash.substring(1);
       var vars = query.split("&");
       for (var i = 0; i < vars.length; i++) {
         var pair = vars[i].split("=");
