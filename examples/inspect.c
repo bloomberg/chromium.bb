fea5f401ab4c54768ceffae38353c83742289c9e
Urvang Joshi <urvang@google.com>
2017-05-22 14:46:10 -0700

    inspect.c: Add enums for SMOOTH_{H,V}_PRED
    Change-Id: I3eebf3811964c682309784d9bbdfb00790fde871

--- examples/inspect.c
+++ examples/inspect.c
@@ -193,7 +193,10 @@ const map_entry prediction_mode_map[] = {
   ENUM(D63_PRED),
 #if CONFIG_ALT_INTRA
   ENUM(SMOOTH_PRED),
-#endif
+#if CONFIG_SMOOTH_HV
+  ENUM(SMOOTH_V_PRED),  ENUM(SMOOTH_H_PRED),
+#endif  // CONFIG_SMOOTH_HV
+#endif  // CONFIG_ALT_INTRA
   ENUM(TM_PRED),        ENUM(NEARESTMV),
   ENUM(NEARMV),         ENUM(ZEROMV),
   ENUM(NEWMV),
