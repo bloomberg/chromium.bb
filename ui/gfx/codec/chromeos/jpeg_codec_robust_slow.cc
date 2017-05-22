e50626a46e8600a6147763f27f42dfb18ca03165
thestig <thestig@chromium.org>
2017-05-22 13:04:45 -0700

    Fix includes with double slashes.
    Review-Url: https://codereview.chromium.org/2894793004
    Cr-Commit-Position: refs/heads/master@{#473669}
    
    Former-commit-id: d570301027a991639be4e1c4ecfc7353e98ad012

--- ui/gfx/codec/chromeos/jpeg_codec_robust_slow.cc
+++ ui/gfx/codec/chromeos/jpeg_codec_robust_slow.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/gfx//codec/chromeos/jpeg_codec_robust_slow.h"
+#include "ui/gfx/codec/chromeos/jpeg_codec_robust_slow.h"
 
 #include <setjmp.h>
 
