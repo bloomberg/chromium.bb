fd899e6d340a197a6f38fed05ff33a3f8992daca
glebl <glebl@chromium.org>
2017-05-22 12:18:43 -0700

    Add a basic support of text-align:right property to NGInlineLayoutAlgorithm.
    BUG=635619
    TEST=This patch fixes 74 tests
    
    Review-Url: https://codereview.chromium.org/2892293002
    Cr-Commit-Position: refs/heads/master@{#473652}
    
    Former-commit-id: ba4f9ac96ff6456cc2fab42b7bd3a05f80119c97

--- third_party/WebKit/Source/core/layout/ng/inline/ng_line_box_fragment_builder.cc
+++ third_party/WebKit/Source/core/layout/ng/inline/ng_line_box_fragment_builder.cc
@@ -42,6 +42,18 @@ void NGLineBoxFragmentBuilder::MoveChildrenInBlockDirection(LayoutUnit delta) {
     offset.block_offset += delta;
 }
 
+void NGLineBoxFragmentBuilder::MoveChildrenInInlineDirection(LayoutUnit delta) {
+  NGWritingMode writing_mode(
+      FromPlatformWritingMode(node_->Style().GetWritingMode()));
+  LayoutUnit child_inline_size;
+  for (size_t i = 0; i < children_.size(); ++i) {
+    offsets_[i].inline_offset = delta + child_inline_size;
+    NGPhysicalFragment* child = children_[i].Get();
+    child_inline_size +=
+        child->Size().ConvertToLogical(writing_mode).inline_size;
+  }
+}
+
 void NGLineBoxFragmentBuilder::MoveChildrenInBlockDirection(LayoutUnit delta,
                                                             unsigned start,
                                                             unsigned end) {
