7c416b75ca996ebe71fcda33ed2b08d56c45b7fa
samans <samans@chromium.org>
2017-05-23 14:11:22 -0700

    Remove ClientCompositorFrameSinkBinding
    ClientCompositorFrameSinkBinding simply holds
    MojoCompositorFrameSinkRequest and MojoCompositorFrameSinkClientPtr and I
    don't think it adds a lot of value.
    
    TBR=sadrul@chromium.org
    
    Review-Url: https://codereview.chromium.org/2899063002
    Cr-Commit-Position: refs/heads/master@{#474052}
    
    Former-commit-id: ac50ea80f469353abd39abf4bea74a9b14065877

--- services/ui/public/cpp/client_compositor_frame_sink.h
+++ services/ui/public/cpp/client_compositor_frame_sink.h
@@ -16,19 +16,16 @@
 
 namespace ui {
 
-class ClientCompositorFrameSinkBinding;
-
 class ClientCompositorFrameSink
     : public cc::CompositorFrameSink,
       public cc::mojom::MojoCompositorFrameSinkClient,
       public cc::ExternalBeginFrameSourceClient {
  public:
-  // static
-  static std::unique_ptr<ClientCompositorFrameSink> Create(
+  ClientCompositorFrameSink(
       scoped_refptr<cc::ContextProvider> context_provider,
       gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager,
-      std::unique_ptr<ClientCompositorFrameSinkBinding>*
-          compositor_frame_sink_binding,
+      cc::mojom::MojoCompositorFrameSinkPtrInfo compositor_frame_sink_info,
+      cc::mojom::MojoCompositorFrameSinkClientRequest client_request,
       bool enable_surface_synchronization);
 
   ~ClientCompositorFrameSink() override;
@@ -41,13 +38,6 @@ class ClientCompositorFrameSink
   void DidNotProduceFrame(const cc::BeginFrameAck& ack) override;
 
  private:
-  ClientCompositorFrameSink(
-      scoped_refptr<cc::ContextProvider> context_provider,
-      gpu::GpuMemoryBufferManager* gpu_memory_buffer_manager,
-      cc::mojom::MojoCompositorFrameSinkPtrInfo compositor_frame_sink_info,
-      cc::mojom::MojoCompositorFrameSinkClientRequest client_request,
-      bool enable_surface_synchronization);
-
   // cc::mojom::MojoCompositorFrameSinkClient implementation:
   void DidReceiveCompositorFrameAck(
       const cc::ReturnedResourceArray& resources) override;
@@ -72,32 +62,6 @@ class ClientCompositorFrameSink
   DISALLOW_COPY_AND_ASSIGN(ClientCompositorFrameSink);
 };
 
-// A ClientCompositorFrameSinkBinding is a bundle of mojo interfaces that is
-// created by ClientCompositorFrameSink::Create and is used by or implemented by
-// Mus when a window is attached to a frame-sink..
-// ClientCompositorFrameSinkBinding has no standalone functionality. Its purpose
-// is to allow safely creating and attaching a CompositorFrameSink on one
-// thread and using it on another.
-class ClientCompositorFrameSinkBinding {
- public:
-  ~ClientCompositorFrameSinkBinding();
-
-  cc::mojom::MojoCompositorFrameSinkRequest TakeFrameSinkRequest();
-  cc::mojom::MojoCompositorFrameSinkClientPtrInfo TakeFrameSinkClient();
-
- private:
-  friend class ClientCompositorFrameSink;
-
-  ClientCompositorFrameSinkBinding(
-      cc::mojom::MojoCompositorFrameSinkRequest compositor_frame_sink_request,
-      cc::mojom::MojoCompositorFrameSinkClientPtrInfo
-          compositor_frame_sink_client);
-
-  cc::mojom::MojoCompositorFrameSinkRequest compositor_frame_sink_request_;
-  cc::mojom::MojoCompositorFrameSinkClientPtrInfo compositor_frame_sink_client_;
-
-  DISALLOW_COPY_AND_ASSIGN(ClientCompositorFrameSinkBinding);
-};
 }  // namespace ui
 
 #endif  // SERVICES_UI_PUBLIC_CPP_WINDOW_COMPOSITOR_FRAME_SINK_H_
