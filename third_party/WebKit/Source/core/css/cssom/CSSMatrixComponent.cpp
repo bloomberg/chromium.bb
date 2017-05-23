efc875c97dbfe3675eec0283ab4d5c3c5988bd4f
hs1217.lee <hs1217.lee@samsung.com>
2017-05-23 06:48:31 -0700

    [css-typed-om] replace all attribute with DOMMatrix in cssMatrixComponent
    replace all attribute with DOMMatrix in cssMatrixComponent.
    
    https://drafts.css-houdini.org/css-typed-om-1/#cssmatrixcomponent
    
    BUG=545318
    
    Review-Url: https://codereview.chromium.org/2888403004
    Cr-Commit-Position: refs/heads/master@{#473880}
    
    Former-commit-id: d094fefe4ebce2a81b28a9e0b1e7d2ed553a3b75

--- third_party/WebKit/Source/core/css/cssom/CSSMatrixComponent.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSMatrixComponent.cpp
@@ -11,20 +11,27 @@
 
 namespace blink {
 
+CSSMatrixComponent* CSSMatrixComponent::Create(DOMMatrixReadOnly* matrix) {
+  return new CSSMatrixComponent(matrix);
+}
+
 CSSFunctionValue* CSSMatrixComponent::ToCSSValue() const {
   CSSFunctionValue* result =
       CSSFunctionValue::Create(is2d_ ? CSSValueMatrix : CSSValueMatrix3d);
 
   if (is2d_) {
-    double values[6] = {a(), b(), c(), d(), e(), f()};
+    double values[6] = {matrix()->a(), matrix()->b(), matrix()->c(),
+                        matrix()->d(), matrix()->e(), matrix()->f()};
     for (double value : values) {
       result->Append(*CSSPrimitiveValue::Create(
           value, CSSPrimitiveValue::UnitType::kNumber));
     }
   } else {
-    double values[16] = {m11(), m12(), m13(), m14(), m21(), m22(),
-                         m23(), m24(), m31(), m32(), m33(), m34(),
-                         m41(), m42(), m43(), m44()};
+    double values[16] = {
+        matrix()->m11(), matrix()->m12(), matrix()->m13(), matrix()->m14(),
+        matrix()->m21(), matrix()->m22(), matrix()->m23(), matrix()->m24(),
+        matrix()->m31(), matrix()->m32(), matrix()->m33(), matrix()->m34(),
+        matrix()->m41(), matrix()->m42(), matrix()->m43(), matrix()->m44()};
     for (double value : values) {
       result->Append(*CSSPrimitiveValue::Create(
           value, CSSPrimitiveValue::UnitType::kNumber));
@@ -35,67 +42,96 @@ CSSFunctionValue* CSSMatrixComponent::ToCSSValue() const {
 }
 
 CSSMatrixComponent* CSSMatrixComponent::Perspective(double length) {
-  std::unique_ptr<TransformationMatrix> matrix = TransformationMatrix::Create();
+  DOMMatrixInit init;
+  DOMMatrix* matrix = DOMMatrix::fromMatrix(init, ASSERT_NO_EXCEPTION);
+
   if (length != 0)
-    matrix->SetM34(-1 / length);
-  return new CSSMatrixComponent(std::move(matrix), kPerspectiveType);
+    matrix->setM34(-1 / length);
+  return new CSSMatrixComponent(matrix, kPerspectiveType);
 }
 
 CSSMatrixComponent* CSSMatrixComponent::Rotate(double angle) {
-  std::unique_ptr<TransformationMatrix> matrix = TransformationMatrix::Create();
-  matrix->Rotate(angle);
-  return new CSSMatrixComponent(std::move(matrix), kRotationType);
+  DOMMatrixInit init;
+  DOMMatrix* matrix = DOMMatrix::fromMatrix(init, ASSERT_NO_EXCEPTION);
+
+  matrix->rotateSelf(angle);
+  return new CSSMatrixComponent(matrix, kRotationType);
 }
 
 CSSMatrixComponent* CSSMatrixComponent::Rotate3d(double angle,
                                                  double x,
                                                  double y,
                                                  double z) {
-  std::unique_ptr<TransformationMatrix> matrix = TransformationMatrix::Create();
-  matrix->Rotate3d(x, y, z, angle);
-  return new CSSMatrixComponent(std::move(matrix), kRotation3DType);
+  DOMMatrixInit init;
+  DOMMatrix* matrix = DOMMatrix::fromMatrix(init, ASSERT_NO_EXCEPTION);
+
+  matrix->rotateAxisAngleSelf(x, y, z, angle);
+  return new CSSMatrixComponent(matrix, kRotation3DType);
 }
 
 CSSMatrixComponent* CSSMatrixComponent::Scale(double x, double y) {
-  std::unique_ptr<TransformationMatrix> matrix = TransformationMatrix::Create();
-  matrix->SetM11(x);
-  matrix->SetM22(y);
-  return new CSSMatrixComponent(std::move(matrix), kScaleType);
+  DOMMatrixInit init;
+  DOMMatrix* matrix = DOMMatrix::fromMatrix(init, ASSERT_NO_EXCEPTION);
+
+  matrix->setM11(x);
+  matrix->setM22(y);
+  return new CSSMatrixComponent(matrix, kScaleType);
 }
 
 CSSMatrixComponent* CSSMatrixComponent::Scale3d(double x, double y, double z) {
-  std::unique_ptr<TransformationMatrix> matrix = TransformationMatrix::Create();
-  matrix->SetM11(x);
-  matrix->SetM22(y);
-  matrix->SetM33(z);
-  return new CSSMatrixComponent(std::move(matrix), kScale3DType);
+  DOMMatrixInit init;
+  DOMMatrix* matrix = DOMMatrix::fromMatrix(init, ASSERT_NO_EXCEPTION);
+
+  matrix->setM11(x);
+  matrix->setM22(y);
+  matrix->setM33(z);
+  return new CSSMatrixComponent(matrix, kScale3DType);
 }
 
 CSSMatrixComponent* CSSMatrixComponent::Skew(double ax, double ay) {
   double tan_ax = std::tan(deg2rad(ax));
   double tan_ay = std::tan(deg2rad(ay));
 
-  std::unique_ptr<TransformationMatrix> matrix = TransformationMatrix::Create();
-  matrix->SetM12(tan_ay);
-  matrix->SetM21(tan_ax);
-  return new CSSMatrixComponent(std::move(matrix), kSkewType);
+  DOMMatrixInit init;
+  DOMMatrix* matrix = DOMMatrix::fromMatrix(init, ASSERT_NO_EXCEPTION);
+
+  matrix->setM12(tan_ay);
+  matrix->setM21(tan_ax);
+  return new CSSMatrixComponent(matrix, kSkewType);
 }
 
 CSSMatrixComponent* CSSMatrixComponent::Translate(double x, double y) {
-  std::unique_ptr<TransformationMatrix> matrix = TransformationMatrix::Create();
-  matrix->SetM41(x);
-  matrix->SetM42(y);
-  return new CSSMatrixComponent(std::move(matrix), kTranslationType);
+  DOMMatrixInit init;
+  DOMMatrix* matrix = DOMMatrix::fromMatrix(init, ASSERT_NO_EXCEPTION);
+
+  matrix->setM41(x);
+  matrix->setM42(y);
+  return new CSSMatrixComponent(matrix, kTranslationType);
 }
 
 CSSMatrixComponent* CSSMatrixComponent::Translate3d(double x,
                                                     double y,
                                                     double z) {
-  std::unique_ptr<TransformationMatrix> matrix = TransformationMatrix::Create();
-  matrix->SetM41(x);
-  matrix->SetM42(y);
-  matrix->SetM43(z);
-  return new CSSMatrixComponent(std::move(matrix), kTranslation3DType);
+  DOMMatrixInit init;
+  DOMMatrix* matrix = DOMMatrix::fromMatrix(init, ASSERT_NO_EXCEPTION);
+
+  matrix->setM41(x);
+  matrix->setM42(y);
+  matrix->setM43(z);
+  return new CSSMatrixComponent(matrix, kTranslation3DType);
+}
+
+CSSMatrixComponent::CSSMatrixComponent(DOMMatrixReadOnly* matrix)
+    : CSSTransformComponent() {
+  matrix_ = DOMMatrix::Create(matrix);
+  is2d_ = matrix->is2D();
+}
+
+CSSMatrixComponent::CSSMatrixComponent(DOMMatrixReadOnly* matrix,
+                                       TransformComponentType from_type)
+    : CSSTransformComponent() {
+  matrix_ = DOMMatrix::Create(matrix);
+  is2d_ = Is2DComponentType(from_type);
 }
 
 }  // namespace blink
--- third_party/WebKit/Source/core/css/cssom/CSSMatrixComponent.h
+++ third_party/WebKit/Source/core/css/cssom/CSSMatrixComponent.h
@@ -7,73 +7,27 @@
 
 #include <memory>
 #include "core/css/cssom/CSSTransformComponent.h"
+#include "core/geometry/DOMMatrix.h"
 #include "platform/transforms/TransformationMatrix.h"
 
 namespace blink {
+
+class DOMMatrixReadOnly;
 
 class CORE_EXPORT CSSMatrixComponent final : public CSSTransformComponent {
   WTF_MAKE_NONCOPYABLE(CSSMatrixComponent);
   DEFINE_WRAPPERTYPEINFO();
 
  public:
-  static CSSMatrixComponent* Create(double a,
-                                    double b,
-                                    double c,
-                                    double d,
-                                    double e,
-                                    double f) {
-    return new CSSMatrixComponent(a, b, c, d, e, f);
-  }
-
-  static CSSMatrixComponent* Create(double m11,
-                                    double m12,
-                                    double m13,
-                                    double m14,
-                                    double m21,
-                                    double m22,
-                                    double m23,
-                                    double m24,
-                                    double m31,
-                                    double m32,
-                                    double m33,
-                                    double m34,
-                                    double m41,
-                                    double m42,
-                                    double m43,
-                                    double m44) {
-    return new CSSMatrixComponent(m11, m12, m13, m14, m21, m22, m23, m24, m31,
-                                  m32, m33, m34, m41, m42, m43, m44);
-  }
+  static CSSMatrixComponent* Create(DOMMatrixReadOnly*);
 
   static CSSMatrixComponent* FromCSSValue(const CSSFunctionValue& value) {
     return nullptr;
   }
 
-  // 2D matrix attributes
-  double a() const { return matrix_->A(); }
-  double b() const { return matrix_->B(); }
-  double c() const { return matrix_->C(); }
-  double d() const { return matrix_->D(); }
-  double e() const { return matrix_->E(); }
-  double f() const { return matrix_->F(); }
+  DOMMatrix* matrix() const { return matrix_; }
 
-  // 3D matrix attributes
-  double m11() const { return matrix_->M11(); }
-  double m12() const { return matrix_->M12(); }
-  double m13() const { return matrix_->M13(); }
-  double m14() const { return matrix_->M14(); }
-  double m21() const { return matrix_->M21(); }
-  double m22() const { return matrix_->M22(); }
-  double m23() const { return matrix_->M23(); }
-  double m24() const { return matrix_->M24(); }
-  double m31() const { return matrix_->M31(); }
-  double m32() const { return matrix_->M32(); }
-  double m33() const { return matrix_->M33(); }
-  double m34() const { return matrix_->M34(); }
-  double m41() const { return matrix_->M41(); }
-  double m42() const { return matrix_->M42(); }
-  double m43() const { return matrix_->M43(); }
-  double m44() const { return matrix_->M44(); }
+  void setMatrix(DOMMatrix* matrix) { matrix_ = matrix; }
 
   TransformComponentType GetType() const override {
     return is2d_ ? kMatrixType : kMatrix3DType;
@@ -102,58 +56,16 @@ class CORE_EXPORT CSSMatrixComponent final : public CSSTransformComponent {
   static CSSMatrixComponent* Translate(double x, double y);
   static CSSMatrixComponent* Translate3d(double x, double y, double z);
 
+  DEFINE_INLINE_VIRTUAL_TRACE() {
+    visitor->Trace(matrix_);
+    CSSTransformComponent::Trace(visitor);
+  }
+
  private:
-  CSSMatrixComponent(double a, double b, double c, double d, double e, double f)
-      : CSSTransformComponent(),
-        matrix_(TransformationMatrix::Create(a, b, c, d, e, f)),
-        is2d_(true) {}
+  CSSMatrixComponent(DOMMatrixReadOnly*);
+  CSSMatrixComponent(DOMMatrixReadOnly*, TransformComponentType);
 
-  CSSMatrixComponent(double m11,
-                     double m12,
-                     double m13,
-                     double m14,
-                     double m21,
-                     double m22,
-                     double m23,
-                     double m24,
-                     double m31,
-                     double m32,
-                     double m33,
-                     double m34,
-                     double m41,
-                     double m42,
-                     double m43,
-                     double m44)
-      : CSSTransformComponent(),
-        matrix_(TransformationMatrix::Create(m11,
-                                             m12,
-                                             m13,
-                                             m14,
-                                             m21,
-                                             m22,
-                                             m23,
-                                             m24,
-                                             m31,
-                                             m32,
-                                             m33,
-                                             m34,
-                                             m41,
-                                             m42,
-                                             m43,
-                                             m44)),
-        is2d_(false) {}
-
-  CSSMatrixComponent(std::unique_ptr<const TransformationMatrix> matrix,
-                     TransformComponentType from_type)
-      : CSSTransformComponent(),
-        matrix_(std::move(matrix)),
-        is2d_(Is2DComponentType(from_type)) {}
-
-  // TransformationMatrix needs to be 16-byte aligned. PartitionAlloc
-  // supports 16-byte alignment but Oilpan doesn't. So we use an std::unique_ptr
-  // to allocate TransformationMatrix on PartitionAlloc.
-  // TODO(oilpan): Oilpan should support 16-byte aligned allocations.
-  std::unique_ptr<const TransformationMatrix> matrix_;
+  Member<DOMMatrix> matrix_;
   bool is2d_;
 };
 
