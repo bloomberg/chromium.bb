7a7d49938ecb3e85f2868ba7abef231a15533e91
emircan <emircan@chromium.org>
2017-05-22 17:21:53 -0700

    Revert of Disable WebRtcVideoQualityBrowserTest.TestVideoQualityH264/0 on Win bots. (patchset #2 id:20001 of https://codereview.chromium.org/2893523004/ )
    Reason for revert:
    This test passes after the fixes on issue webrtc:7532. See the explanation https://bugs.chromium.org/p/chromium/issues/detail?id=722746#c21 for how they are related.
    
    Original issue's description:
    > Disable WebRtcVideoQualityBrowserTest.TestVideoQualityH264/0 on Win bots.
    >
    > BUG=chromium:722746
    >
    > Review-Url: https://codereview.chromium.org/2893523004
    > Cr-Commit-Position: refs/heads/master@{#472824}
    > Committed: https://chromium.googlesource.com/chromium/src/+/5d6d5274e8bdd667fe295dbcbcb1f834ce6313cf [formerly 7498e9b31a8ed8fd1d1fac071916cf824e48e2c5]
    
    TBR=tommi@chromium.org,philipel@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=chromium:722746
    
    Review-Url: https://codereview.chromium.org/2903463002
    Cr-Commit-Position: refs/heads/master@{#473760}
    
    Former-commit-id: 7197acdffac9a256fb1eed080469b2f383b97897

--- chrome/browser/media/webrtc/webrtc_video_quality_browsertest.cc
+++ chrome/browser/media/webrtc/webrtc_video_quality_browsertest.cc
@@ -357,15 +357,8 @@ IN_PROC_BROWSER_TEST_P(WebRtcVideoQualityBrowserTest,
 
 #if BUILDFLAG(RTC_USE_H264)
 
-// TODO(philipel): Remove disable condition when H264 flakyness has been
-//                 investigated (crbug.com/722746).
-#if defined(OS_WIN)
-#define MAYBE_TestVideoQualityH264 DISABLED_TestVideoQualityH264
-#else
-#define MAYBE_TestVideoQualityH264 MANUAL_TestVideoQualityH264
-#endif
 IN_PROC_BROWSER_TEST_P(WebRtcVideoQualityBrowserTest,
-                       MAYBE_TestVideoQualityH264) {
+                       MANUAL_TestVideoQualityH264) {
   base::ThreadRestrictions::ScopedAllowIO allow_io;
   // Only run test if run-time feature corresponding to |rtc_use_h264| is on.
   if (!base::FeatureList::IsEnabled(content::kWebRtcH264WithOpenH264FFmpeg)) {
