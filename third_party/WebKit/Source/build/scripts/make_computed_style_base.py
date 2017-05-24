277b5d2d01164dccd2a86ea08b78b08650f403f0
nainar <nainar@chromium.org>
2017-05-24 02:40:28 -0700

    Move LayoutUnit to correct alignment
    LayoutUnit only contains an int so it alignment belongs on the same
    line as an int
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2904843002
    Cr-Commit-Position: refs/heads/master@{#474225}
    
    Former-commit-id: b69b3599eedc856b722158672add9095a518440c

--- third_party/WebKit/Source/build/scripts/make_computed_style_base.py
+++ third_party/WebKit/Source/build/scripts/make_computed_style_base.py
@@ -25,9 +25,9 @@ from itertools import chain
 # TODO(shend): Put alignment sizes into code form, rather than linking to a CL which may disappear.
 ALIGNMENT_ORDER = [
     'double',
-    'Font', 'FillLayer', 'NinePieceImage', 'LayoutUnit',  # Aligns like a void * (can be 32 or 64 bits)
+    'Font', 'FillLayer', 'NinePieceImage',  # Aligns like a void * (can be 32 or 64 bits)
     'LengthBox', 'LengthSize', 'Length', 'float',
-    'StyleColor', 'Color', 'unsigned', 'int',
+    'StyleColor', 'Color', 'LayoutUnit', 'unsigned', 'int',
     'short',
     'char',
     'bool'
