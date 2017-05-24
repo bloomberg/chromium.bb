d653cbacc56469c6f24edf516f55ba1591cbf6d6
lpromero <lpromero@chromium.org>
2017-05-24 04:46:02 -0700

    Clean up: remove a non-required @required.
    BUG=675355
    R=sdefresne@chromium.org
    
    Review-Url: https://codereview.chromium.org/2896403003
    Cr-Commit-Position: refs/heads/master@{#474252}
    
    Former-commit-id: 5265ca6a9e1d5baf1f0384a1bf6b2f2c0d90c077

--- ios/chrome/browser/ui/stack_view/stack_view_controller.h
+++ ios/chrome/browser/ui/stack_view/stack_view_controller.h
@@ -16,7 +16,6 @@ class GURL;
 
 // A protocol used by the StackViewController for test purposes.
 @protocol StackViewControllerTestDelegate
-@required
 // Informs the delegate that the show tab animation starts.
 - (void)stackViewControllerShowWithSelectedTabAnimationDidStart;
 // Informs the delegate that the show tab animation finished.
