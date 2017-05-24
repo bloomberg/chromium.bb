7fd1777b09ccb0d760d4547b15e75f8dd99021f3
cwallez <cwallez@chromium.org>
2017-05-24 09:59:19 -0700

    Remove references to GClientKeywords.FromImpl from roll scripts
    This class was removed because it was no longer used but some roll
    scripts were still referencing it. Also removes a dummy FromImpl in
    PRESUBMIT.py as it shouldn't be needed anymore.
    
    TBR=dpranke@chromium.org
    
    BUG=661382
    
    Review-Url: https://codereview.chromium.org/2898403003
    Cr-Commit-Position: refs/heads/master@{#474329}
    
    Former-commit-id: c3a2fa143c3b2b867d74738b0978faf8818388a1

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -1140,12 +1140,6 @@ def _ExtractAddRulesFromParsedDeps(parsed_deps):
 def _ParseDeps(contents):
   """Simple helper for parsing DEPS files."""
   # Stubs for handling special syntax in the root DEPS file.
-  def FromImpl(*_):
-    pass  # NOP function so "From" doesn't fail.
-
-  def FileImpl(_):
-    pass  # NOP function so "File" doesn't fail.
-
   class _VarImpl:
 
     def __init__(self, local_scope):
@@ -1160,8 +1154,6 @@ def _ParseDeps(contents):
 
   local_scope = {}
   global_scope = {
-      'File': FileImpl,
-      'From': FromImpl,
       'Var': _VarImpl(local_scope).Lookup,
   }
   exec contents in global_scope, local_scope
