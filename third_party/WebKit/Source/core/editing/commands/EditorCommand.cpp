ad4e560a7c2de25e365fafb4cc61f99c4d3743db
Xiaocheng Hu <xiaochengh@chromium.org>
2017-05-24 12:12:48 -0700

    Make EnabledVisibleSelectionAndMark return false for use-triggered command and unfocused selection
    Certain editing commands shouldn't be enabled when they are user-triggered and
    the selection doesn't have focus. This patch adds checking of
    FrameSelection::SelectionHasFocus() in EnabledVisibleSelectionAndMark() to fix
    the behavior of the following commands:
    - SelectToMark
    - SwapWithMark
    
    Bug: 722925
    Change-Id: Ib5e2c9fde9fec1f73421166d04d40daeecfd46d6
    Reviewed-on: https://chromium-review.googlesource.com/514164
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474581}
    Former-commit-id: 5172eedc999c1828d7051d3a9c8a210d8370f0ad

--- third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
+++ third_party/WebKit/Source/core/editing/commands/EditorCommand.cpp
@@ -2024,8 +2024,12 @@ static bool EnabledVisibleSelection(LocalFrame& frame,
 
 static bool EnabledVisibleSelectionAndMark(LocalFrame& frame,
                                            Event* event,
-                                           EditorCommandSource) {
+                                           EditorCommandSource source) {
   frame.GetDocument()->UpdateStyleAndLayoutIgnorePendingStylesheets();
+
+  if (source == kCommandFromMenuOrKeyBinding &&
+      !frame.Selection().SelectionHasFocus())
+    return false;
 
   const VisibleSelection& selection =
       frame.GetEditor().SelectionForCommand(event);
