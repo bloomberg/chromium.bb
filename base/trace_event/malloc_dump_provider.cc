8daac49b76099d6fe20ccb2a7b28b4ed9f9bf160
etienneb <etienneb@chromium.org>
2017-05-24 10:49:09 -0700

    Cleanup malloc dump provider after previous refactoring
    This CL is a code cleanup after this refactoring:
      https://codereview.chromium.org/2890363003
      https://chromium.googlesource.com/chromium/src/+/df9c9b78abdd69ab167d109c13e682bd6ab8979a [formerly bd599af5d33647f0b498311eab05be79fac98e63]
    
    CC=erikchen@chromium.org
    R=primiano@chromium.org
    
    BUG=724651
    
    Review-Url: https://codereview.chromium.org/2898943004
    Cr-Commit-Position: refs/heads/master@{#474345}
    
    Former-commit-id: 7de5d92d63eab0410c6e679510ca65941d59587b

--- base/trace_event/malloc_dump_provider.cc
+++ base/trace_event/malloc_dump_provider.cc
@@ -298,26 +298,22 @@ bool MallocDumpProvider::OnMemoryDump(const MemoryDumpArgs& args,
   // Enclosing all the temporary data structures in a scope, so that the heap
   // profiler does not see unbalanced malloc/free calls from these containers.
   {
-    size_t shim_allocated_objects_size = 0;
-    size_t shim_allocated_objects_count = 0;
     TraceEventMemoryOverhead overhead;
     std::unordered_map<AllocationContext, AllocationMetrics> metrics_by_context;
     if (args.level_of_detail == MemoryDumpLevelOfDetail::DETAILED) {
-      ShardedAllocationRegister::OutputMetrics metrics =
+      ShardedAllocationRegister::OutputMetrics shim_metrics =
           allocation_register_.UpdateAndReturnsMetrics(metrics_by_context);
 
       // Aggregate data for objects allocated through the shim.
-      shim_allocated_objects_size += metrics.size;
-      shim_allocated_objects_count += metrics.count;
+      inner_dump->AddScalar("shim_allocated_objects_size",
+                            MemoryAllocatorDump::kUnitsBytes,
+                            shim_metrics.size);
+      inner_dump->AddScalar("shim_allocator_object_count",
+                            MemoryAllocatorDump::kUnitsObjects,
+                            shim_metrics.count);
     }
     allocation_register_.EstimateTraceMemoryOverhead(&overhead);
 
-    inner_dump->AddScalar("shim_allocated_objects_size",
-                          MemoryAllocatorDump::kUnitsBytes,
-                          shim_allocated_objects_size);
-    inner_dump->AddScalar("shim_allocator_object_count",
-                          MemoryAllocatorDump::kUnitsObjects,
-                          shim_allocated_objects_count);
     pmd->DumpHeapUsage(metrics_by_context, overhead, "malloc");
   }
   tid_dumping_heap_ = kInvalidThreadId;
