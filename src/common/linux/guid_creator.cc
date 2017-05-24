77305c3ae6c975753cd0d696d4921cd767a4a44a
John Budorick <jbudorick@chromium.org>
2017-05-24 12:36:40 -0700

    Wrap config.h include in HAVE_CONFIG_H.
    Bug: breakpad:730
    Change-Id: I5a24b96258e1114378061512239d3e18f3f753f0
    Reviewed-on: https://chromium-review.googlesource.com/514283
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Reviewed-by: Mike Frysinger <vapier@chromium.org>

--- src/common/linux/guid_creator.cc
+++ src/common/linux/guid_creator.cc
@@ -27,7 +27,10 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "config.h"
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include "common/linux/eintr_wrapper.h"
 #include "common/linux/guid_creator.h"
 
