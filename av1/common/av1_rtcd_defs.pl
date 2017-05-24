bf8af7e62411bf98ed9c7f2aab4231f814b95b82
Yi Luo <luoyi@google.com>
2017-05-24 15:11:05 -0700

    Add HBD build to av1_quantize_fp_sse2
    - This change turns on low bit depth data path for
      this function under default HBD build.
    - Encoder user level encoding time reduces ~12%
      on i7-6700.
    
    Change-Id: I7ce21e8db1a379f972e51c3b4ab305ca10e41efb

--- av1/common/av1_rtcd_defs.pl
+++ av1/common/av1_rtcd_defs.pl
@@ -312,6 +312,7 @@ if (aom_config("CONFIG_AOM_QM") eq "yes") {
     add_proto qw/int64_t av1_block_error/, "const tran_low_t *coeff, const tran_low_t *dqcoeff, intptr_t block_size, int64_t *ssz";
 
     add_proto qw/void av1_quantize_fp/, "const tran_low_t *coeff_ptr, intptr_t n_coeffs, int skip_block, const int16_t *zbin_ptr, const int16_t *round_ptr, const int16_t *quant_ptr, const int16_t *quant_shift_ptr, tran_low_t *qcoeff_ptr, tran_low_t *dqcoeff_ptr, const int16_t *dequant_ptr, uint16_t *eob_ptr, const int16_t *scan, const int16_t *iscan";
+    specialize qw/av1_quantize_fp sse2/;
 
     add_proto qw/void av1_quantize_fp_32x32/, "const tran_low_t *coeff_ptr, intptr_t n_coeffs, int skip_block, const int16_t *zbin_ptr, const int16_t *round_ptr, const int16_t *quant_ptr, const int16_t *quant_shift_ptr, tran_low_t *qcoeff_ptr, tran_low_t *dqcoeff_ptr, const int16_t *dequant_ptr, uint16_t *eob_ptr, const int16_t *scan, const int16_t *iscan";
 
