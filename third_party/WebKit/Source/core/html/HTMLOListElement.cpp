a56b53454b81c82b911d5de76535675259bb98d9
shanmuga.m <shanmuga.m@samsung.com>
2017-05-25 02:22:43 -0700

    Measure the usage of <ol>.start with |reversed| and no |start| attribute.
    Add usecounter to measure the usage of <ol>.start which has
    |reversed| attribute and no |start| content attribute.
    
    BUG=657972
    
    Review-Url: https://codereview.chromium.org/2902723006
    Cr-Commit-Position: refs/heads/master@{#474614}
    
    Former-commit-id: 846987251c417048faef232c657c83da59715eae

--- third_party/WebKit/Source/core/html/HTMLOListElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLOListElement.cpp
@@ -96,6 +96,20 @@ void HTMLOListElement::ParseAttribute(
   }
 }
 
+int HTMLOListElement::start() const {
+  if (has_explicit_start_)
+    return start_;
+
+  if (is_reversed_) {
+    UseCounter::Count(
+        GetDocument(),
+        UseCounter::kHTMLOListElementStartGetterReversedWithoutStartAttribute);
+    return ItemCount();
+  }
+
+  return 1;
+}
+
 void HTMLOListElement::setStart(int start) {
   SetIntegralAttribute(startAttr, start);
 }
