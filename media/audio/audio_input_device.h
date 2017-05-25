ba004affce03adc50811984145a6fcd96d7dcba4
grunell <grunell@chromium.org>
2017-05-25 04:30:27 -0700

    Re-land: Stop source and fire MediaStreamTrack ended event if missing audio input callbacks are detected.
    This way the javascript application is informed when there's a problem with the input device. Since the detection is in the renderer process we would also catch any other issues in the pipeline, including on the process border (i.e. the socket pair), should it render the same type of symptom.
    
    * Detection is done in AudioInputDevice as this corresponds to a physical device which is what we want to detect missing callbacks for.
    * When detected we call CaptureError() on the capturer, which stops the source and puts the track in ended state and fires the ended event.
    * The time with missing callbacks before detecting is chosen to 12 seconds, so that other components down the stack have a chance to defer start if needed and report startup success stats. Specifically the Mac implementation. See also code comment in the CL.
    * UMA stats is added; reporting a boolean when stopping.
    
    Tested manually by altering a Linux build with https://codereview.chromium.org/2891303002/ and using a WebRTC test page with the MediaStreamTrack.onended set to log that the event was fired.
    
    Original patch (1) from issue 2888383002 at patchset 300001 (http://crrev.com/2888383002#ps300001)
    
    BUG=722335
    TEST=See above.
    TBR=ossu@chromium.org, tommi@chromium.org, isherman@chromium.org
    
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2899413004
    Cr-Commit-Position: refs/heads/master@{#474628}
    
    Former-commit-id: 021d9e05dc8384ba3d5f7c1c1834ee0bf9fd3e5c

--- media/audio/audio_input_device.h
+++ media/audio/audio_input_device.h
@@ -59,6 +59,8 @@
 #include "base/compiler_specific.h"
 #include "base/macros.h"
 #include "base/memory/shared_memory.h"
+#include "base/time/time.h"
+#include "base/timer/timer.h"
 #include "media/audio/audio_device_thread.h"
 #include "media/audio/audio_input_ipc.h"
 #include "media/audio/scoped_task_runner_observer.h"
@@ -128,6 +130,19 @@ class MEDIA_EXPORT AudioInputDevice
   // If the IO loop dies before we do, we shut down the audio thread from here.
   void WillDestroyCurrentMessageLoop() override;
 
+  // Checks if we have gotten callbacks within a certain time period. If no
+  // callbacks have been received, we report a capture error to the capture
+  // callback. Must be called on IO thread.
+  void CheckIfInputStreamIsAlive();
+
+  // Sets the last callback time |last_callback_time_ms_| to the current time.
+  // SetLastCallbackTimeToNow() posts a task on the IO thread to run
+  // SetLastCallbackTimeToNowOnIOThread() which actually sets the variable. We
+  // don't need high precision so we don't have to care about the delay added
+  // with posting the task.
+  void SetLastCallbackTimeToNow();
+  void SetLastCallbackTimeToNowOnIOThread();
+
   AudioParameters audio_parameters_;
 
   CaptureCallback* callback_;
@@ -165,6 +180,22 @@ class MEDIA_EXPORT AudioInputDevice
   // TODO(miu): Replace this by changing AudioCapturerSource to accept the
   // callback via Start(). See http://crbug.com/151051 for details.
   bool stopping_hack_;
+
+  // Mechanism for detecting if we don't get callbacks for some period of time.
+  // |check_alive_timer_| runs the check regularly.
+  // |last_callback_time_| stores the time for the last callback.
+  // Both must only be accessed on the IO thread.
+  // TODO(grunell): Change from TimeTicks to Atomic32 and remove the task
+  // posting in SetLastCallbackTimeToNow(). The Atomic32 variable would have to
+  // represent some time in seconds or tenths of seconds to be able to span over
+  // enough time. Atomic64 cannot be used since it's not supported on 32-bit
+  // platforms.
+  std::unique_ptr<base::RepeatingTimer> check_alive_timer_;
+  base::TimeTicks last_callback_time_;
+
+  // Flags that missing callbacks has been detected. Used for statistics,
+  // reported when stopping. Must only be accessed on the IO thread.
+  bool missing_callbacks_detected_;
 
   DISALLOW_IMPLICIT_CONSTRUCTORS(AudioInputDevice);
 };
