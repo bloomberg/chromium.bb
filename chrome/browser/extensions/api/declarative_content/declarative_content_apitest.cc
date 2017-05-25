73489c5e3fed0c9a186afe31bb2b74a70546e5fb
rdevlin.cronin <rdevlin.cronin@chromium.org>
2017-05-25 14:55:11 -0700

    [Extensions Bindings] Update declarativeContent CSS validation test
    Update the declarativeContent CSS validation test to accept errors from
    either the JS bindings system or the native bindings system.
    
    BUG=653596
    
    Review-Url: https://codereview.chromium.org/2909523003
    Cr-Commit-Position: refs/heads/master@{#474817}
    
    Former-commit-id: 3137d1a8be97dbc4fe14075d61458ce9a5e6219e

--- chrome/browser/extensions/api/declarative_content/declarative_content_apitest.cc
+++ chrome/browser/extensions/api/declarative_content/declarative_content_apitest.cc
@@ -790,7 +790,7 @@ IN_PROC_BROWSER_TEST_F(DeclarativeContentApiTest,
                   "} catch (e) {\n"
                   "  Return(e.message);\n"
                   "}\n"),
-              testing::ContainsRegex("css.*Expected 'array'"));
+              testing::ContainsRegex("css.*xpected '?array'?"));
   EXPECT_THAT(ExecuteScriptInBackgroundPage(
                   extension->id(),
                   "try {\n"
@@ -799,7 +799,7 @@ IN_PROC_BROWSER_TEST_F(DeclarativeContentApiTest,
                   "} catch (e) {\n"
                   "  Return(e.message);\n"
                   "}\n"),
-              testing::ContainsRegex("css\\.0.*Expected 'string'"));
+              testing::ContainsRegex("css.*0.*xpected '?string'?"));
   EXPECT_THAT(ExecuteScriptInBackgroundPage(
                   extension->id(),
                   "try {\n"
@@ -819,7 +819,7 @@ IN_PROC_BROWSER_TEST_F(DeclarativeContentApiTest,
                   "} catch (e) {\n"
                   "  Return(e.message);\n"
                   "}\n"),
-              testing::ContainsRegex("compound selector.*: div input$"));
+              testing::ContainsRegex("selector.*: div input$"));
 }
 
 // Tests that the rules with isBookmarked: true are evaluated when handling
