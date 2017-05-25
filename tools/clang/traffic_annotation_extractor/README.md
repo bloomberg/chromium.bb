79885340cea14dde4f34f6c8eb42ede7a0323792
rhalavati <rhalavati@chromium.org>
2017-05-25 00:53:01 -0700

    Partial Protobuf for Traffic Annotation.
    In many use cases, it's better to define some part of a network traffic
    annotation in one function and the reset of it in another one. like when a
    function makes the initial request and controls policies, and then passes it
    to another one to set/unset the cookies and perform the actual request. This CL
    adds required definitions to make partial annotations.
    
    BUG=656607
    
    patch from issue 2815293002 at patchset 160001 (http://crrev.com/2815293002#ps160001)
    
    Review-Url: https://codereview.chromium.org/2869373002
    Cr-Commit-Position: refs/heads/master@{#474602}
    
    Former-commit-id: 99c6fcef9cff05b0df3a536623f8d8251c2f2a6b

--- tools/clang/traffic_annotation_extractor/README.md
+++ tools/clang/traffic_annotation_extractor/README.md
@@ -1,7 +1,7 @@
 # Traffic Annotation Extrator
 This is a clang tool to extract network traffic annotations. The tool is run by
-`tools/traffic_annotation/auditor/traffic_annotaion_auditor.py`. Refer to it for
-help on how to use.
+`tools/traffic_annotation/auditor/traffic_annotation_auditor.py`. Refer to it
+for help on how to use.
 
 ## Build on Linux
 `tools/clang/scripts/update.py --bootstrap --force-local-build
@@ -16,11 +16,25 @@ help on how to use.
 
 ## Usage
 Run `traffic_annotation_extractor --help` for parameters help.
+
+Example for direct call:
+  `third_party/llvm-build/Release+Asserts/bin/traffic_annotation_extractor
+     -p=out/Debug components/spellcheck/browser/spelling_service_client.cc`
+
+Example for call using run_tool.py:
+  `tools/clang/scripts/run_tool.py --tool=traffic_annotation_extractor
+     --generate-compdb -p=out/Debug components/spellcheck/browser`
+
 The executable extracts network traffic annotations from given file paths based
   on build parameters in build path, and writes them to llvm::outs.
   Each output will have the following format:
   - Line 1: File path.
   - Line 2: Name of the function in which annotation is defined.
   - Line 3: Line number of annotation.
-  - Line 4: Unique id of annotation.
-  - Line 5-: Serialized protobuf of the annotation.
+  - Line 4: Function type ("Definition", "Partial", "Completing",
+            "BranchedCompleting").
+  - Line 5: Unique id of annotation.
+  - Line 6: Completing id or group id, when applicable, empty otherwise.
+  - Line 7-: Serialized protobuf of the annotation.
+Outputs are enclosed by "==== NEW ANNOTATION ====" and
+  "==== ANNOTATION ENDS ===="
--- tools/clang/traffic_annotation_extractor/traffic_annotation_extractor.cpp
+++ tools/clang/traffic_annotation_extractor/traffic_annotation_extractor.cpp
@@ -2,11 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This clang tool finds all instances of net::DefineNetworkTrafficAnnotation in
-// given source code, extracts the location info and content of annotation tags
-// (unique id and annotation text), and stores them in separate text files
-// (per instance) in the given output directory. Please refer to README.md for
-// build and usage instructions.
+// This clang tool finds all instances of the following functions:
+//   - net::DefineNetworkTrafficAnnotation
+//   - net::DefinePartialNetworkTrafficAnnotation
+//   - net::CompleteNetworkTrafficAnnotation
+//   - net::BranchedCompleteNetworkTrafficAnnotation
+// It extracts the location info and content of annotation tags, and outputs
+// them to llvm::outs. Please refer to README.md for build and usage
+// instructions.
 
 #include <memory>
 #include <vector>
@@ -25,32 +28,66 @@ using namespace clang::ast_matchers;
 
 namespace {
 
-// An instance of a call to the net::DefineNetworkTrafficAnnotation function.
+// An instance of a call to either of the 4 network traffic annotation
+// definition functions.
 struct NetworkAnnotationInstance {
   // Information about where the call has happened.
   struct Location {
     std::string file_path;
     int line_number = -1;
 
-    // Name of the function calling net::DefineNetworkTrafficAnnotation. E.g.,
-    // in the following code, |function_name| will be 'foo':
-    // void foo() { NetworkTrafficAnnotationTag bar =
-    //                  net::DefineNetworkTrafficAnnotation(...); }
+    // Name of the function including this line. E.g., in the following code,
+    // |function_name| will be 'foo' for all |line_number| values 101-103.
+    //
+    // 100 void foo() {
+    // 101   NetworkTrafficAnnotationTag baz =
+    // 102       net::DefineNetworkTrafficAnnotation(...); }
+    // 103   bar(baz);
+    // 104 }
     // If no function is found, 'Global Namespace' will be returned.
     std::string function_name;
   };
 
-  // Annotation content. These are the parameters of the call to
-  // net::DefineNetworkTrafficAnnotation. The unique_id is an identifier for the
-  // annotation that has to be unique across the entire code base. The |text|
-  // stores a raw string with the annotation that should be extracted.
+  // Annotation content. These are the arguments of the call to either of the 4
+  // network traffic annotation definition functions.
   struct Annotation {
     std::string unique_id;
     std::string text;
+
+    // |extra_id| will have |completing_id| for
+    // net::DefinePartialNetworkTrafficAnnotation and |group_id| for
+    // net::BranchedCompleteNetworkTrafficAnnotation. It will be empty in other
+    // cases.
+    std::string extra_id;
   };
 
   Location location;
   Annotation annotation;
+
+  // Specifying the function type.
+  enum FunctionType {
+    kDefinition,         // net::DefineNetworkTrafficAnnotation
+    kPartial,            // net::DefinePartialNetworkTrafficAnnotation
+    kCompleting,         // net::CompleteNetworkTrafficAnnotation
+    kBranchedCompleting  // net::BranchedCompleteNetworkTrafficAnnotation
+  };
+
+  FunctionType function_type;
+
+  const char* GetTypeName() const {
+    switch (function_type) {
+      case kDefinition:
+        return "Definition";
+      case kPartial:
+        return "Partial";
+      case kCompleting:
+        return "Completing";
+      case kBranchedCompleting:
+        return "BranchedCompleting";
+    }
+    assert(false);
+    return "";
+  }
 };
 
 using Collector = std::vector<NetworkAnnotationInstance>;
@@ -68,18 +105,41 @@ class NetworkAnnotationTagCallback : public MatchFinder::MatchCallback {
   // Is called on any pattern found by ASTMathers that are defined in RunMathers
   // function.
   virtual void run(const MatchFinder::MatchResult& result) override {
-    const clang::CallExpr* call_expr =
-        result.Nodes.getNodeAs<clang::CallExpr>("definition_function");
+    NetworkAnnotationInstance instance;
+
     const clang::StringLiteral* unique_id =
         result.Nodes.getNodeAs<clang::StringLiteral>("unique_id");
     const clang::StringLiteral* annotation_text =
         result.Nodes.getNodeAs<clang::StringLiteral>("annotation_text");
     const clang::FunctionDecl* ancestor =
         result.Nodes.getNodeAs<clang::FunctionDecl>("function_context");
+    const clang::StringLiteral* group_id =
+        result.Nodes.getNodeAs<clang::StringLiteral>("group_id");
+    const clang::StringLiteral* completing_id =
+        result.Nodes.getNodeAs<clang::StringLiteral>("completing_id");
 
-    assert(call_expr && unique_id && annotation_text);
+    const clang::CallExpr* call_expr = nullptr;
+    if ((call_expr =
+             result.Nodes.getNodeAs<clang::CallExpr>("definition_function"))) {
+      instance.function_type = NetworkAnnotationInstance::kDefinition;
+    } else if ((call_expr = result.Nodes.getNodeAs<clang::CallExpr>(
+                    "partial_function"))) {
+      instance.function_type = NetworkAnnotationInstance::kPartial;
+      assert(completing_id);
+      instance.annotation.extra_id = completing_id->getString();
+    } else if ((call_expr = result.Nodes.getNodeAs<clang::CallExpr>(
+                    "completing_function"))) {
+      instance.function_type = NetworkAnnotationInstance::kCompleting;
+    } else if ((call_expr = result.Nodes.getNodeAs<clang::CallExpr>(
+                    "branched_completing_function"))) {
+      instance.function_type = NetworkAnnotationInstance::kBranchedCompleting;
+      assert(group_id);
+      instance.annotation.extra_id = group_id->getString();
+    } else {
+      assert(false);
+    }
 
-    NetworkAnnotationInstance instance;
+    assert(unique_id && annotation_text);
     instance.annotation.unique_id = unique_id->getString();
     instance.annotation.text = annotation_text->getString();
 
@@ -120,10 +180,8 @@ int RunMatchers(clang::tooling::ClangTool* clang_tool, Collector* collector) {
   NetworkAnnotationTagCallback callback(collector);
   MatchFinder match_finder;
 
-  // Set up a pattern to find functions that are named
-  // [net::]DefineNetworkTrafficAnnotation and have 2 arguments of string
-  // literal type. If pattern has a function declaration as ancestor, it is
-  // marked.
+  // Set up patterns to find network traffic annotation definition functions,
+  // their arguments, and their ancestor function (when possible).
   match_finder.addMatcher(
       callExpr(hasDeclaration(functionDecl(
                    anyOf(hasName("DefineNetworkTrafficAnnotation"),
@@ -133,6 +191,38 @@ int RunMatchers(clang::tooling::ClangTool* clang_tool, Collector* collector) {
                anyOf(hasAncestor(functionDecl().bind("function_context")),
                      unless(hasAncestor(functionDecl()))))
           .bind("definition_function"),
+      &callback);
+  match_finder.addMatcher(
+      callExpr(hasDeclaration(functionDecl(anyOf(
+                   hasName("DefinePartialNetworkTrafficAnnotation"),
+                   hasName("net::DefinePartialNetworkTrafficAnnotation")))),
+               hasArgument(0, stringLiteral().bind("unique_id")),
+               hasArgument(1, stringLiteral().bind("completing_id")),
+               hasArgument(2, stringLiteral().bind("annotation_text")),
+               anyOf(hasAncestor(functionDecl().bind("function_context")),
+                     unless(hasAncestor(functionDecl()))))
+          .bind("partial_function"),
+      &callback);
+  match_finder.addMatcher(
+      callExpr(hasDeclaration(functionDecl(
+                   anyOf(hasName("CompleteNetworkTrafficAnnotation"),
+                         hasName("net::CompleteNetworkTrafficAnnotation")))),
+               hasArgument(0, stringLiteral().bind("unique_id")),
+               hasArgument(2, stringLiteral().bind("annotation_text")),
+               anyOf(hasAncestor(functionDecl().bind("function_context")),
+                     unless(hasAncestor(functionDecl()))))
+          .bind("completing_function"),
+      &callback);
+  match_finder.addMatcher(
+      callExpr(hasDeclaration(functionDecl(anyOf(
+                   hasName("BranchedCompleteNetworkTrafficAnnotation"),
+                   hasName("net::BranchedCompleteNetworkTrafficAnnotation")))),
+               hasArgument(0, stringLiteral().bind("unique_id")),
+               hasArgument(1, stringLiteral().bind("group_id")),
+               hasArgument(3, stringLiteral().bind("annotation_text")),
+               anyOf(hasAncestor(functionDecl().bind("function_context")),
+                     unless(hasAncestor(functionDecl()))))
+          .bind("branched_completing_function"),
       &callback);
   std::unique_ptr<clang::tooling::FrontendActionFactory> frontend_factory =
       clang::tooling::newFrontendActionFactory(&match_finder);
@@ -157,16 +247,16 @@ int main(int argc, const char* argv[]) {
   if (result != 0)
     return result;
 
-  // For each call to "DefineNetworkTrafficAnnotation", write annotation text
-  // and relevant meta data into a separate file. The filename is uniquely
-  // generated using the file path and filename of the code including the call
-  // and its line number.
+  // For each call to any of the functions that define a network traffic
+  // annotation, write annotation text and relevant meta data into llvm::outs().
   for (const NetworkAnnotationInstance& call : collector) {
     llvm::outs() << "==== NEW ANNOTATION ====\n";
     llvm::outs() << call.location.file_path << "\n";
     llvm::outs() << call.location.function_name << "\n";
     llvm::outs() << call.location.line_number << "\n";
+    llvm::outs() << call.GetTypeName() << "\n";
     llvm::outs() << call.annotation.unique_id << "\n";
+    llvm::outs() << call.annotation.extra_id << "\n";
     llvm::outs() << call.annotation.text << "\n";
     llvm::outs() << "==== ANNOTATION ENDS ====\n";
   }
