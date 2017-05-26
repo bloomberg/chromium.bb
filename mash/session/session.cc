d87d22e7e0031f1a4e9cededb06c58cd63378ae8
sky <sky@chromium.org>
2017-05-26 11:25:39 -0700

    chromeos: make mash session not launch chrome
    Doing this causes problems with ash_standalone as well as tests. In
    particular it means ash_standalone attempts to connect to chrome and
    then chrome tries to connect as the window manager. For tests it can
    cause hangs.
    
    BUG=721901
    TEST=none
    R=msw@chromium.org
    
    Review-Url: https://codereview.chromium.org/2908703003
    Cr-Commit-Position: refs/heads/master@{#475064}
    
    Former-commit-id: e7d345c67a7b4bb98e8c7c0fed1f37d316acd867

--- mash/session/session.cc
+++ mash/session/session.cc
@@ -7,7 +7,6 @@
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/message_loop/message_loop.h"
-#include "content/public/common/service_names.mojom.h"
 #include "mash/common/config.h"
 #include "mash/quick_launch/public/interfaces/constants.mojom.h"
 #include "services/service_manager/public/cpp/connector.h"
@@ -22,10 +21,6 @@ Session::~Session() {}
 void Session::OnStart() {
   StartWindowManager();
   StartQuickLaunch();
-
-  // Launch a chrome window for dev convience; don't do this in the long term.
-  context()->connector()->StartService(
-      content::mojom::kPackagedServicesServiceName);
 }
 
 void Session::StartWindowManager() {
