832310901b7c2d2f1875f8c989117ef9eeaf24da
hajimehoshi <hajimehoshi@chromium.org>
2017-05-25 22:49:36 -0700

    Recode shared memory usage on Windows, macOS and NaCl
    Now that shared memory's unique ids are portable, shared memory usages
    can be counted. This CL inserts incrementing and decrementing shared
    memory usages when map/unmapped.
    
    BUG=604726
    
    Review-Url: https://codereview.chromium.org/2867193004
    Cr-Commit-Position: refs/heads/master@{#474930}
    
    Former-commit-id: e80c576c7bdcd567ba96a4e1ceb9f845dcf1168e

--- base/memory/shared_memory_posix.cc
+++ base/memory/shared_memory_posix.cc
@@ -298,8 +298,8 @@ bool SharedMemory::Unmap() {
   if (memory_ == NULL)
     return false;
 
-  munmap(memory_, mapped_size_);
   SharedMemoryTracker::GetInstance()->DecrementMemoryUsage(*this);
+  munmap(memory_, mapped_size_);
   memory_ = NULL;
   mapped_size_ = 0;
   return true;
