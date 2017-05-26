e38a5e896f7a7c27b7c727ceeae2e30791fb48ce
jdufault <jdufault@chromium.org>
2017-05-26 14:25:55 -0700

    cros: Pull TestLockScreenClient into a separate MockLockScreenClient file.
    - converted the class to a mock.
    - made it easy for tests to create an instance by defining a helper method.
    
    BUG=719015
    
    Review-Url: https://codereview.chromium.org/2894193002
    Cr-Commit-Position: refs/heads/master@{#475135}
    
    Former-commit-id: 87469729cfc60bc4cc139dc55b2410bda8f65e5f

---
+++ ash/login/mock_lock_screen_client.cc
@@ -0,0 +1,29 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ash/login/mock_lock_screen_client.h"
+
+#include "ash/login/lock_screen_controller.h"
+#include "ash/shell.h"
+
+namespace ash {
+
+MockLockScreenClient::MockLockScreenClient() : binding_(this) {}
+
+MockLockScreenClient::~MockLockScreenClient() = default;
+
+mojom::LockScreenClientPtr MockLockScreenClient::CreateInterfacePtrAndBind() {
+  return binding_.CreateInterfacePtrAndBind();
+}
+
+std::unique_ptr<MockLockScreenClient> BindMockLockScreenClient() {
+  LockScreenController* lock_screen_controller =
+      Shell::Get()->lock_screen_controller();
+  auto lock_screen_client = base::MakeUnique<MockLockScreenClient>();
+  lock_screen_controller->SetClient(
+      lock_screen_client->CreateInterfacePtrAndBind());
+  return lock_screen_client;
+}
+
+}  // namespace ash
---
+++ ash/login/mock_lock_screen_client.h
@@ -0,0 +1,38 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_LOGIN_MOCK_LOCK_SCREEN_CLIENT_H_
+#define ASH_LOGIN_MOCK_LOCK_SCREEN_CLIENT_H_
+
+#include "ash/public/interfaces/lock_screen.mojom.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace ash {
+
+class MockLockScreenClient : public mojom::LockScreenClient {
+ public:
+  MockLockScreenClient();
+  ~MockLockScreenClient() override;
+
+  mojom::LockScreenClientPtr CreateInterfacePtrAndBind();
+
+  // mojom::LockScreenClient:
+  MOCK_METHOD3(AuthenticateUser,
+               void(const AccountId& account_id,
+                    const std::string& password,
+                    bool authenticated_by_pin));
+
+ private:
+  mojo::Binding<ash::mojom::LockScreenClient> binding_;
+
+  DISALLOW_COPY_AND_ASSIGN(MockLockScreenClient);
+};
+
+// Helper method to bind a lock screen client so it receives all mojo calls.
+std::unique_ptr<MockLockScreenClient> BindMockLockScreenClient();
+
+}  // namespace ash
+
+#endif  // ASH_LOGIN_MOCK_LOCK_SCREEN_CLIENT_H_
