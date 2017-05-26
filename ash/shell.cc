2782892649587552d98c3fe2138a7ac07db7c2ca
Sam McNally <sammc@chromium.org>
2017-05-26 16:42:41 +1000

    Change prefs::ConnectToPrefService() to take local pref stores.
    Currently, ConnectToPrefService() takes a list of pref store types that
    are to be provided locally, but does not expose a way to register them
    with the created PrefService object. This replaces that with a map of
    pref stores that are local.
    
    Bug: 654988
    Change-Id: I187bf1a148e3e2e041e6f3b01b7eefc4deaabefd
    Reviewed-on: https://chromium-review.googlesource.com/514942
    Commit-Queue: Sam McNally <sammc@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Johan Tibell <tibell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#474964}
    Former-commit-id: 74bb3b85224a01473a0efabf331ebe4e8541ccb8

--- ash/shell.cc
+++ ash/shell.cc
@@ -807,8 +807,7 @@ void Shell::Init(const ShellInitParams& init_params) {
     auto pref_registry = base::MakeRefCounted<PrefRegistrySimple>();
     Shell::RegisterPrefs(pref_registry.get());
     prefs::ConnectToPrefService(
-        shell_delegate_->GetShellConnector(), std::move(pref_registry),
-        std::vector<PrefValueStore::PrefStoreType>(),
+        shell_delegate_->GetShellConnector(), std::move(pref_registry), {},
         base::Bind(&Shell::OnPrefServiceInitialized, base::Unretained(this)),
         prefs::mojom::kForwarderServiceName);
   }
