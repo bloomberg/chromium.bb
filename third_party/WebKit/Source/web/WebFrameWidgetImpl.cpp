b26f496a6491afefbf3a926116b83a1e485c9177
Xiaocheng Hu <xiaochengh@chromium.org>
2017-05-28 13:33:16 -0700

    Prune ComputeVisibleSelectionInDOMTreeDeprecated from WebFrameWidgetImpl::SelectionTextDirection
    This patch removes a call site of the deprecated function, which is safe
    because layout has been updated just before the call site in the same
    function.
    
    Bug: 698633
    Change-Id: I71624bc7d827a729d3e63e427d0067ba15a67315
    Reviewed-on: https://chromium-review.googlesource.com/518022
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475271}
    Former-commit-id: 7eb6069bf8afee2521bcc856488c45b1f43ae6e6

--- third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
+++ third_party/WebKit/Source/web/WebFrameWidgetImpl.cpp
@@ -650,14 +650,10 @@ bool WebFrameWidgetImpl::SelectionTextDirection(WebTextDirection& start,
           .ToNormalizedEphemeralRange()
           .IsNull())
     return false;
-  start = ToWebTextDirection(
-      PrimaryDirectionOf(*selection.ComputeVisibleSelectionInDOMTreeDeprecated()
-                              .Start()
-                              .AnchorNode()));
-  end = ToWebTextDirection(
-      PrimaryDirectionOf(*selection.ComputeVisibleSelectionInDOMTreeDeprecated()
-                              .end()
-                              .AnchorNode()));
+  start = ToWebTextDirection(PrimaryDirectionOf(
+      *selection.ComputeVisibleSelectionInDOMTree().Start().AnchorNode()));
+  end = ToWebTextDirection(PrimaryDirectionOf(
+      *selection.ComputeVisibleSelectionInDOMTree().end().AnchorNode()));
   return true;
 }
 
