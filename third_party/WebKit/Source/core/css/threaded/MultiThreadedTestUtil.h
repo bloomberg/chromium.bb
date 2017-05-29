cae0f797ee99bdc0c7e0adff9e4afbebdf9364eb
Fernando Serboncini <fserb@google.com>
2017-05-29 15:42:13 -0400

    Makes MultiThreadedTestUtil useful
    - Adds new TSAN_TEST and TSAN_TEST_F macros for threaded tests
    - Replace WebThread with WebThreadSupportingGC
    
    Bug: 724565
    Change-Id: Ic8a64d268049c56f5e70611e28ecad9ab07e7ec9
    Reviewed-on: https://chromium-review.googlesource.com/518262
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475385}
    Former-commit-id: 335f92a2a16c9a041c693938b9faa66516f0713e

--- third_party/WebKit/Source/core/css/threaded/MultiThreadedTestUtil.h
+++ third_party/WebKit/Source/core/css/threaded/MultiThreadedTestUtil.h
@@ -7,6 +7,7 @@
 #include "platform/CrossThreadFunctional.h"
 #include "platform/WaitableEvent.h"
 #include "platform/WebTaskRunner.h"
+#include "platform/WebThreadSupportingGC.h"
 #include "platform/scheduler/child/web_scheduler.h"
 #include "platform/wtf/Functional.h"
 #include "platform/wtf/RefCounted.h"
@@ -15,6 +16,32 @@
 
 namespace blink {
 
+// This creates 2 macros for TSAN: TSAN_TEST and TSAN_TEST_F.
+// Those tests are automatically disabled on non-tsan builds and should be used
+// instead of the normal gtest macros for MultiThreadedTests.
+// It guarantees that those tests are only run on Thread Sanitizer enabled
+// builds.
+// Also, TSAN_TEST subclasses MultiThreadTest instead of testing::Test.
+#if defined(THREAD_SANITIZER)
+
+#define TSAN_TEST(test_case_name, test_name)                         \
+  GTEST_TEST_(test_case_name, test_name, ::blink::MultiThreadedTest, \
+              ::testing::internal::GetTypeId<::blink::MultiThreadedTest>())
+
+#define TSAN_TEST_F(test_fixture, test_name) TEST_F(test_fixture, test_name)
+
+#else
+
+#define TSAN_TEST(test_case_name, test_name)        \
+  GTEST_TEST_(test_case_name, DISABLED_##test_name, \
+              ::blink::MultiThreadedTest,           \
+              ::testing::internal::GetTypeId<::blink::MultiThreadedTest>())
+
+#define TSAN_TEST_F(test_fixture, test_name) \
+  TEST_F(test_fixture, DISABLED_##test_name)
+
+#endif
+
 class MultiThreadedTest : public testing::Test {
  public:
   // RunOnThreads run a closure num_threads * callbacks_per_thread times.
@@ -22,16 +49,23 @@ class MultiThreadedTest : public testing::Test {
 
   template <typename FunctionType, typename... Ps>
   void RunOnThreads(FunctionType function, Ps&&... parameters) {
-    Vector<std::unique_ptr<WebThread>> threads;
+    Vector<std::unique_ptr<WebThreadSupportingGC>> threads;
     Vector<std::unique_ptr<WaitableEvent>> waits;
 
     for (int i = 0; i < num_threads_; ++i) {
-      threads.push_back(Platform::Current()->CreateThread(""));
+      threads.push_back(WebThreadSupportingGC::Create(""));
       waits.push_back(WTF::MakeUnique<WaitableEvent>());
     }
 
     for (int i = 0; i < num_threads_; ++i) {
-      WebTaskRunner* task_runner = threads[i]->GetWebTaskRunner();
+      WebTaskRunner* task_runner =
+          threads[i]->PlatformThread().GetWebTaskRunner();
+
+      task_runner->PostTask(
+          FROM_HERE,
+          CrossThreadBind(
+              [](WebThreadSupportingGC* thread) { thread->Initialize(); },
+              CrossThreadUnretained(threads[i].get())));
 
       for (int j = 0; j < callbacks_per_thread_; ++j) {
         task_runner->PostTask(FROM_HERE,
@@ -39,8 +73,13 @@ class MultiThreadedTest : public testing::Test {
       }
 
       task_runner->PostTask(
-          FROM_HERE, CrossThreadBind([](WaitableEvent* w) { w->Signal(); },
-                                     CrossThreadUnretained(waits[i].get())));
+          FROM_HERE, CrossThreadBind(
+                         [](WebThreadSupportingGC* thread, WaitableEvent* w) {
+                           thread->Shutdown();
+                           w->Signal();
+                         },
+                         CrossThreadUnretained(threads[i].get()),
+                         CrossThreadUnretained(waits[i].get())));
     }
 
     for (int i = 0; i < num_threads_; ++i) {
