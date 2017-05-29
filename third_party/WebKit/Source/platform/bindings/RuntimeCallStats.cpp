102a031bd15de378103dab9cc38c17c7b72638e0
Adithya Srinivasan <adithyas@chromium.org>
2017-05-29 13:11:21 -0400

    Add test counters to RuntimeCallStats
    - Adds counters to RuntimeCallStats
    - Updates Enter/Leave to use CounterId instead of RuntimeCallCounter*
    - Add << overload for RuntimeCallStats
    
    Bug: 724543
    Change-Id: I238f362a726984a0651975fc0a2a9d2b0a57889c
    Reviewed-on: https://chromium-review.googlesource.com/516222
    Commit-Queue: Adithya Srinivasan <adithyas@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475374}
    Former-commit-id: 8b83adb8cd1eec1b0db28353b9cd106613160915

--- third_party/WebKit/Source/platform/bindings/RuntimeCallStats.cpp
+++ third_party/WebKit/Source/platform/bindings/RuntimeCallStats.cpp
@@ -4,6 +4,10 @@
 
 #include "platform/bindings/RuntimeCallStats.h"
 
+#include <inttypes.h>
+#include <algorithm>
+#include "platform/wtf/text/StringBuilder.h"
+
 namespace blink {
 
 void RuntimeCallTimer::Start(RuntimeCallCounter* counter,
@@ -25,6 +29,37 @@ RuntimeCallTimer* RuntimeCallTimer::Stop() {
   if (parent_)
     parent_->Resume(now);
   return parent_;
+}
+
+RuntimeCallStats::RuntimeCallStats() {
+  static const char* const names[] = {
+#define COUNTER_NAME_ENTRY(name) #name,
+      FOR_EACH_COUNTER(COUNTER_NAME_ENTRY)
+#undef COUNTER_NAME_ENTRY
+  };
+
+  for (int i = 0; i < number_of_counters_; i++) {
+    counters_[i] = RuntimeCallCounter(names[i]);
+  }
+}
+
+void RuntimeCallStats::Reset() {
+  for (int i = 0; i < number_of_counters_; i++) {
+    counters_[i].Reset();
+  }
+}
+
+String RuntimeCallStats::ToString() const {
+  StringBuilder builder;
+  builder.Append("Runtime Call Stats for Blink \n");
+  builder.Append("Name                              Count     Time (ms)\n\n");
+  for (int i = 0; i < number_of_counters_; i++) {
+    const RuntimeCallCounter* counter = &counters_[i];
+    builder.Append(String::Format("%-32s  %8" PRIu64 "%8.3f\n",
+                                  counter->GetName(), counter->GetCount(),
+                                  counter->GetTime().InMillisecondsF()));
+  }
+  return builder.ToString();
 }
 
 }  // namespace blink
--- third_party/WebKit/Source/platform/bindings/RuntimeCallStats.h
+++ third_party/WebKit/Source/platform/bindings/RuntimeCallStats.h
@@ -8,6 +8,7 @@
 #include "platform/PlatformExport.h"
 #include "platform/wtf/Allocator.h"
 #include "platform/wtf/Time.h"
+#include "platform/wtf/text/WTFString.h"
 
 namespace blink {
 
@@ -15,7 +16,7 @@ namespace blink {
 // function/scope.
 class PLATFORM_EXPORT RuntimeCallCounter {
  public:
-  RuntimeCallCounter(const char* name) : count_(0), name_(name) {}
+  explicit RuntimeCallCounter(const char* name) : count_(0), name_(name) {}
 
   void IncrementAndAddTime(TimeDelta time) {
     count_++;
@@ -26,10 +27,19 @@ class PLATFORM_EXPORT RuntimeCallCounter {
   TimeDelta GetTime() const { return time_; }
   const char* GetName() const { return name_; }
 
+  void Reset() {
+    time_ = TimeDelta();
+    count_ = 0;
+  }
+
  private:
+  RuntimeCallCounter() {}
+
   uint64_t count_;
   TimeDelta time_;
   const char* name_;
+
+  friend class RuntimeCallStats;
 };
 
 // Used to track elapsed time for a counter.
@@ -79,10 +89,24 @@ class PLATFORM_EXPORT RuntimeCallTimer {
 // scope.
 class PLATFORM_EXPORT RuntimeCallStats {
  public:
+  RuntimeCallStats();
+
+// Counters
+#define FOR_EACH_COUNTER(V) \
+  V(TestCounter1)           \
+  V(TestCounter2)
+
+  enum class CounterId : uint16_t {
+#define ADD_ENUM_VALUE(counter) k##counter,
+    FOR_EACH_COUNTER(ADD_ENUM_VALUE)
+#undef ADD_ENUM_VALUE
+        kNumberOfCounters
+  };
+
   // Enters a new recording scope by pausing the currently running timer that
   // was started by the current instance, and starting <timer>.
-  void Enter(RuntimeCallTimer* timer, RuntimeCallCounter* counter) {
-    timer->Start(counter, current_timer_);
+  void Enter(RuntimeCallTimer* timer, CounterId id) {
+    timer->Start(GetCounter(id), current_timer_);
     current_timer_ = timer;
   }
 
@@ -94,8 +118,20 @@ class PLATFORM_EXPORT RuntimeCallStats {
     current_timer_ = timer->Stop();
   }
 
+  // Reset all the counters.
+  void Reset();
+
+  RuntimeCallCounter* GetCounter(CounterId id) {
+    return &(counters_[static_cast<uint16_t>(id)]);
+  }
+
+  String ToString() const;
+
  private:
   RuntimeCallTimer* current_timer_ = nullptr;
+  RuntimeCallCounter counters_[static_cast<int>(CounterId::kNumberOfCounters)];
+  static const int number_of_counters_ =
+      static_cast<int>(CounterId::kNumberOfCounters);
 };
 
 // A utility class that creates a RuntimeCallTimer and uses it with
@@ -104,7 +140,8 @@ class PLATFORM_EXPORT RuntimeCallTimerScope {
   STATIC_ONLY(RuntimeCallTimerScope);
 
  public:
-  RuntimeCallTimerScope(RuntimeCallStats* stats, RuntimeCallCounter* counter)
+  RuntimeCallTimerScope(RuntimeCallStats* stats,
+                        RuntimeCallStats::CounterId counter)
       : call_stats_(stats) {
     call_stats_->Enter(&timer_, counter);
   }
