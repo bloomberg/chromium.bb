678f255564d061ae69a5e1de34a28404275cdaa9
sashab <sashab@chromium.org>
2017-05-30 00:16:21 -0700

    Introduced WebFactory and WebFactoryImpl for constructing web/ classes
    Introduced WebFactory and WebFactoryImpl for constructing web/ classes
    outside of core/.
    
    This is a temporary class that will be removed once web/ is removed.
    
    BUG=712963
    
    Review-Url: https://codereview.chromium.org/2887523003
    Cr-Commit-Position: refs/heads/master@{#475451}
    
    Former-commit-id: 8d376e7d11c6d2efe83f46fa043f036b0f6246fe

---
+++ third_party/WebKit/Source/core/exported/WebFactory.h
@@ -0,0 +1,37 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebFactory_h
+#define WebFactory_h
+
+#include "core/CoreExport.h"
+#include "public/platform/WebPageVisibilityState.h"
+
+namespace blink {
+
+class ChromeClient;
+class WebViewBase;
+class WebViewClient;
+
+// WebFactory is a temporary class implemented in web/ that allows classes to
+// construct interfaces that are being moved out of web/.
+// This class will be removed once all implementations are in core/ or modules/.
+class CORE_EXPORT WebFactory {
+ public:
+  // Takes ownership of |factory|.
+  // TODO(sashab): Make this method private to WebKit.cpp.
+  static void SetInstance(WebFactory&);
+  static WebFactory& GetInstance();
+
+  virtual ChromeClient* CreateChromeClient(WebViewBase*) const = 0;
+  virtual WebViewBase* CreateWebViewBase(WebViewClient*,
+                                         WebPageVisibilityState) const = 0;
+
+ private:
+  static WebFactory* factory_instance_;
+};
+
+}  // namespace blink
+
+#endif
---
+++ third_party/WebKit/Source/web/WebFactoryImpl.h
@@ -0,0 +1,24 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WebFactoryImpl_h
+#define WebFactoryImpl_h
+
+#include "core/exported/WebFactory.h"
+
+namespace blink {
+
+class WebFactoryImpl : public WebFactory {
+ public:
+  WebFactoryImpl() {}
+  ~WebFactoryImpl() {}
+
+  ChromeClient* CreateChromeClient(WebViewBase*) const override;
+  WebViewBase* CreateWebViewBase(WebViewClient*,
+                                 WebPageVisibilityState) const override;
+};
+
+}  // namespace blink
+
+#endif
