567ac2ddf3b387c9f6b0b57e002722a1dbfaf34e
Jay Civelli <jcivelli@google.com>
2017-05-30 13:15:25 -0700

    Fix for ChildProcessLauncher's warm-up connection.
    My previous CL https://chromium-review.googlesource.com/c/514424/
    change the warm-up connection erroneously to use
    bindAsExternalService in place of bindToCallerCheck.
    Fixing it and adding a test.
    
    BUG=726901
    
    Change-Id: I8b2dfc7ccc8dbf6db22af45714d7777c437e8f16
    Reviewed-on: https://chromium-review.googlesource.com/518289
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475811}
    Former-commit-id: 580410917ea90ebc1f040380a82d51392a6b88e6

--- content/public/android/javatests/src/org/chromium/content/browser/ChildProcessLauncherTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ChildProcessLauncherTest.java
@@ -408,10 +408,11 @@ public class ChildProcessLauncherTest {
         });
     }
 
-    @Test
-    @MediumTest
-    @Feature({"ProcessManagement"})
-    public void testWarmUp() {
+    private void testWarmUpWithCreationParams(ChildProcessCreationParams creationParams) {
+        if (creationParams != null) {
+            ChildProcessCreationParams.registerDefault(creationParams);
+        }
+
         Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
         warmUpOnUiThreadBlocking(context);
 
@@ -419,7 +420,7 @@ public class ChildProcessLauncherTest {
 
         ChildProcessLauncherHelper launcherHelper = ChildProcessLauncherTestUtils.startForTesting(
                 context, true /* sandboxed */, false /* alwaysInForeground */, new String[0],
-                new FileDescriptorInfo[0], null);
+                new FileDescriptorInfo[0], creationParams /* creationParams */);
 
         final ChildProcessConnection conn = retrieveConnection(launcherHelper);
 
@@ -440,6 +441,25 @@ public class ChildProcessLauncherTest {
                         return allocatedChromeSandboxedConnectionsCount() == 0;
                     }
                 });
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"ProcessManagement"})
+    public void testWarmUp() {
+        // Use the default creation parameters.
+        testWarmUpWithCreationParams(null /* creationParams */);
+    }
+
+    @Test
+    @MediumTest
+    @Feature({"ProcessManagement"})
+    public void testWarmUpWithBindToCaller() {
+        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        ChildProcessCreationParams creationParams = new ChildProcessCreationParams(
+                context.getPackageName(), false /* isExternalService */,
+                LibraryProcessType.PROCESS_CHILD, true /* bindToCallerCheck */);
+        testWarmUpWithCreationParams(creationParams);
     }
 
     // Tests that the warm-up connection is freed from its allocator if it crashes.
