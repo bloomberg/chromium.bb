b9e93b3b88b53c7022d6426bea5ae5a139628441
Chris Watkins <watk@chromium.org>
2017-05-30 15:19:32 -0700

    media: Add CodecWrapper for threadsafe MediaCodec access
    MediaCodecVideoDecoder needs to decode using a MediaCodec on the MCVD
    thread while dequeued buffers are released back to the codec on the GPU
    thread. CodecWrapper supports this use case by vending CodecOutputBuffers
    which keep a reference to their backing wrapper, and a unique id so that
    CodecWrapper can keep track of which buffers are valid.
    
    Bug: 660942
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I798df44a7639071f9eb3f2803d018ced4b121137
    Reviewed-on: https://chromium-review.googlesource.com/517351
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Chris Watkins <watk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475740}
    Former-commit-id: 4a732e3be418d4c3fda70486571f68d24cd3a5ba

---
+++ media/gpu/android/codec_wrapper.h
@@ -0,0 +1,118 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef MEDIA_GPU_ANDROID_CODEC_WRAPPER_H_
+#define MEDIA_GPU_ANDROID_CODEC_WRAPPER_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "base/containers/flat_map.h"
+#include "base/memory/ref_counted.h"
+#include "base/synchronization/lock.h"
+#include "media/base/android/media_codec_bridge.h"
+#include "media/gpu/media_gpu_export.h"
+#include "media/gpu/surface_texture_gl_owner.h"
+
+namespace media {
+class CodecWrapperImpl;
+
+// A MediaCodec output buffer that can be released on any thread.
+class MEDIA_GPU_EXPORT CodecOutputBuffer {
+ public:
+  // Releases the buffer without rendering it.
+  ~CodecOutputBuffer();
+
+  // Releases this buffer and renders it to the surface.
+  bool ReleaseToSurface();
+
+  // The size of the image.
+  gfx::Size size() const { return size_; }
+
+ private:
+  // Let CodecWrapperImpl call the constructor.
+  friend class CodecWrapperImpl;
+  CodecOutputBuffer(scoped_refptr<CodecWrapperImpl> codec,
+                    int64_t id,
+                    gfx::Size size);
+
+  scoped_refptr<CodecWrapperImpl> codec_;
+  int64_t id_;
+  gfx::Size size_;
+  DISALLOW_COPY_AND_ASSIGN(CodecOutputBuffer);
+};
+
+// This wraps a MediaCodecBridge and provides a pared down version of its
+// interface. It also adds the following features:
+// * It outputs CodecOutputBuffers from DequeueOutputBuffer() which can be
+//   safely rendered on any thread, and that will release their buffers on
+//   destruction. This lets us decode on one thread while rendering on another.
+// * It maintains codec specific state like whether an error has occurred.
+//
+// CodecWrapper is not threadsafe, but the CodecOutputBuffers it outputs
+// can be released on any thread.
+class MEDIA_GPU_EXPORT CodecWrapper {
+ public:
+  // |codec| should be in the state referred to by the MediaCodec docs as
+  // "Flushed", i.e., freshly configured or after a Flush() call.
+  CodecWrapper(std::unique_ptr<MediaCodecBridge> codec);
+  ~CodecWrapper();
+
+  // Takes the backing codec and discards all outstanding codec buffers. This
+  // lets you tear down the codec while there are still CodecOutputBuffers
+  // referencing |this|.
+  std::unique_ptr<MediaCodecBridge> TakeCodec();
+
+  // Whether there are any valid CodecOutputBuffers that have not been released.
+  bool HasValidCodecOutputBuffers() const;
+
+  // Releases currently dequeued codec buffers back to the codec without
+  // rendering.
+  void DiscardCodecOutputBuffers();
+
+  // Whether the codec supports Flush().
+  bool SupportsFlush() const;
+
+  // See MediaCodecBridge documentation for the following.
+  bool Flush();
+  MediaCodecStatus QueueInputBuffer(int index,
+                                    const uint8_t* data,
+                                    size_t data_size,
+                                    base::TimeDelta presentation_time);
+  MediaCodecStatus QueueSecureInputBuffer(
+      int index,
+      const uint8_t* data,
+      size_t data_size,
+      const std::string& key_id,
+      const std::string& iv,
+      const std::vector<SubsampleEntry>& subsamples,
+      const EncryptionScheme& encryption_scheme,
+      base::TimeDelta presentation_time);
+  void QueueEOS(int input_buffer_index);
+  MediaCodecStatus DequeueInputBuffer(base::TimeDelta timeout, int* index);
+
+  // Like MediaCodecBridge::DequeueOutputBuffer() but it outputs a
+  // CodecOutputBuffer instead of an index. And it's guaranteed to not return
+  // either of MEDIA_CODEC_OUTPUT_BUFFERS_CHANGED or
+  // MEDIA_CODEC_OUTPUT_FORMAT_CHANGED. It will try to dequeue another
+  // buffer instead. |*codec_buffer| must be null.
+  MediaCodecStatus DequeueOutputBuffer(
+      base::TimeDelta timeout,
+      base::TimeDelta* presentation_time,
+      bool* end_of_stream,
+      std::unique_ptr<CodecOutputBuffer>* codec_buffer);
+  bool SetSurface(jobject surface);
+
+ private:
+  scoped_refptr<CodecWrapperImpl> impl_;
+  DISALLOW_COPY_AND_ASSIGN(CodecWrapper);
+};
+
+}  // namespace media
+
+#endif  // MEDIA_GPU_ANDROID_CODEC_WRAPPER_H_
