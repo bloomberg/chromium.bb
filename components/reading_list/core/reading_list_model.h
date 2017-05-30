1ebe46c49151cbfbded0bddcd7a9af06d58ba4bc
gab <gab@chromium.org>
2017-05-30 01:56:55 -0700

    Replace deprecated base::NonThreadSafe in components/reading_list in favor of SequenceChecker.
    Note to crash team: This CL is a refactor and has no intended behavior change.
    
    This change was scripted by https://crbug.com/676387#c8.
    
    Note-worthy for the reviewer:
     * SequenceChecker enforces thread-safety but not thread-affinity!
       If the classes that were updated are thread-affine (use thread local
       storage or a third-party API that does) they should be migrated to
       ThreadChecker instead.
     * ~NonThreadSafe() used to implcitly check in its destructor
       ~Sequence/ThreadChecker() doesn't by design. To keep this CL a
       no-op, an explicit check was added to the destructor of migrated
       classes.
     * NonThreadSafe used to provide access to subclasses, as such
       the |sequence_checker_| member was made protected rather than
       private where necessary.
    
    BUG=676387
    This CL was uploaded by git cl split.
    
    R=olivierrobin@chromium.org
    
    Review-Url: https://codereview.chromium.org/2914583002
    Cr-Commit-Position: refs/heads/master@{#475471}
    
    Former-commit-id: 3184531f72e00e50b4e79bf0a3fe799bde9c87f6

--- components/reading_list/core/reading_list_model.h
+++ components/reading_list/core/reading_list_model.h
@@ -11,7 +11,7 @@
 
 #include "base/callback.h"
 #include "base/observer_list.h"
-#include "base/threading/non_thread_safe.h"
+#include "base/sequence_checker.h"
 #include "components/reading_list/core/reading_list_entry.h"
 #include "components/reading_list/core/reading_list_model_observer.h"
 
@@ -27,7 +27,7 @@ class ModelTypeSyncBridge;
 // other of read ones. This object should only be accessed from one thread
 // (Usually the main thread). The observers callbacks are also sent on the main
 // thread.
-class ReadingListModel : public base::NonThreadSafe {
+class ReadingListModel {
  public:
   class ScopedReadingListBatchUpdate;
 
@@ -169,6 +169,8 @@ class ReadingListModel : public base::NonThreadSafe {
 
   // Called when model is leaving batch update mode.
   virtual void LeavingBatchUpdates();
+
+  SEQUENCE_CHECKER(sequence_checker_);
 
  private:
   unsigned int current_batch_updates_count_;
