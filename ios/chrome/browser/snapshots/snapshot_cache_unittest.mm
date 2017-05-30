7ceaa1428fc9d5ce43e913ac44a4908530a41634
sdefresne <sdefresne@chromium.org>
2017-05-30 07:01:39 -0700

    Revert of [reland] Use TaskScheduler instead of SequencedWorkerPool in snapshot_cache.mm. (patchset #8 id:140001 of https://codereview.chromium.org/2881873002/ )
    Reason for revert:
    This CL is incorrect. SnapshotCache is a singleton that is indirectly accessed by multiple unit tests (for example MainController unit tests).
    
    CreateSequencedTaskRunnerWithTraits() returns an object that reference TaskScheduler::GetInstance(). This TaskScheduler object is detroyed between each test, thus we have a singleton object pointing to de-allocated memory.
    
    SnapshotCache need to be changed to no longer be a singleton, this is tracked by https://bugs.chromium.org/p/chromium/issues/detail?id=727597.
    
    Original issue's description:
    > [reland] Use TaskScheduler instead of SequencedWorkerPool in snapshot_cache.mm.
    >
    > Reland with fix in test.
    >
    > SequencedWorkerPool is being deprecated in favor of TaskScheduler.
    >
    > BUG=667892
    > R=jif@chromium.org
    >
    > Review-Url: https://codereview.chromium.org/2881873002
    > Cr-Original-Commit-Position: refs/heads/master@{#472052}
    > Committed: https://chromium.googlesource.com/chromium/src/+/b22993924a105a780ed8f1c934e5155499125621 [formerly aa5299ae23a57ca5dce00a11da3ac8e73849351f]
    > Review-Url: https://codereview.chromium.org/2881873002
    > Cr-Commit-Position: refs/heads/master@{#475107}
    > Committed: https://chromium.googlesource.com/chromium/src/+/59173282a328bb14804ddcc065acedca655ebd85 [formerly a11be0d57a4e4f214588bbd6fab9398824802b7f]
    
    TBR=jif@chromium.org,fdoray@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=667892
    
    Review-Url: https://codereview.chromium.org/2910183002
    Cr-Commit-Position: refs/heads/master@{#475518}
    
    Former-commit-id: cf9d12f1999e34f40e9e3ee9e40829d4529cb55b

--- ios/chrome/browser/snapshots/snapshot_cache_unittest.mm
+++ ios/chrome/browser/snapshots/snapshot_cache_unittest.mm
@@ -16,10 +16,11 @@
 #include "base/mac/scoped_nsobject.h"
 #include "base/run_loop.h"
 #include "base/strings/sys_string_conversions.h"
-#include "base/task_scheduler/task_scheduler.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/time/time.h"
 #import "ios/chrome/browser/snapshots/snapshot_cache_internal.h"
 #include "ios/web/public/test/test_web_thread_bundle.h"
+#include "ios/web/public/web_thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/gtest_mac.h"
 #include "testing/platform_test.h"
@@ -83,7 +84,7 @@ class SnapshotCacheTest : public PlatformTest {
   // Flushes all the runloops internally used by the snapshot cache.
   void FlushRunLoops() {
     base::RunLoop().RunUntilIdle();
-    base::TaskScheduler::GetInstance()->FlushForTesting();
+    web::WebThread::GetBlockingPool()->FlushForTesting();
     base::RunLoop().RunUntilIdle();
   }
 
