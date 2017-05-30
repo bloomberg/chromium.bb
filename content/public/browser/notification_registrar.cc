70b09e55a09b7c3ea7670918de4c0d429106e78e
gab <gab@chromium.org>
2017-05-30 08:04:53 -0700

    Replace deprecated base::NonThreadSafe in content in favor of SequenceChecker.
    Note to crash team: This CL is a refactor and has no intended behavior change.
    
    This change was scripted by https://crbug.com/676387#c8.
    
    Note-worthy for the reviewer:
     * SequenceChecker enforces thread-safety but not thread-affinity!
       If the classes that were updated are thread-affine (use thread local
       storage or a third-party API that does) they should be migrated to
       ThreadChecker instead.
     * ~NonThreadSafe() used to implcitly check in its destructor
       ~Sequence/ThreadChecker() doesn't by design. To keep this CL a
       no-op, an explicit check was added to the destructor of migrated
       classes.
     * NonThreadSafe used to provide access to subclasses, as such
       the |sequence_checker_| member was made protected rather than
       private where necessary.
    
    BUG=676387
    This CL was uploaded by git cl split.
    
    R=nasko@chromium.org
    
    Review-Url: https://codereview.chromium.org/2909053003
    Cr-Commit-Position: refs/heads/master@{#475526}
    
    Former-commit-id: a05726ef77312a57e1c392b8dac943abb0d97dc1

--- content/public/browser/notification_registrar.cc
+++ content/public/browser/notification_registrar.cc
@@ -36,17 +36,18 @@ NotificationRegistrar::NotificationRegistrar() {
   NotificationServiceImpl::current();
   // It is OK to create a NotificationRegistrar instance on one thread and then
   // use it (exclusively) on another, so we detach from the initial thread.
-  DetachFromThread();
+  DETACH_FROM_SEQUENCE(sequence_checker_);
 }
 
 NotificationRegistrar::~NotificationRegistrar() {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   RemoveAll();
 }
 
 void NotificationRegistrar::Add(NotificationObserver* observer,
                                 int type,
                                 const NotificationSource& source) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(!IsRegistered(observer, type, source)) << "Duplicate registration.";
 
   Record record = { observer, type, source };
@@ -58,7 +59,7 @@ void NotificationRegistrar::Add(NotificationObserver* observer,
 void NotificationRegistrar::Remove(NotificationObserver* observer,
                                    int type,
                                    const NotificationSource& source) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 
   Record record = { observer, type, source };
   RecordVector::iterator found = std::find(
@@ -75,7 +76,7 @@ void NotificationRegistrar::Remove(NotificationObserver* observer,
 }
 
 void NotificationRegistrar::RemoveAll() {
-  CHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   // Early-exit if no registrations, to avoid calling
   // NotificationService::current.  If we've constructed an object with a
   // NotificationRegistrar member, but haven't actually used the notification
@@ -99,14 +100,14 @@ void NotificationRegistrar::RemoveAll() {
 }
 
 bool NotificationRegistrar::IsEmpty() const {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   return registered_.empty();
 }
 
 bool NotificationRegistrar::IsRegistered(NotificationObserver* observer,
                                          int type,
                                          const NotificationSource& source) {
-  DCHECK(CalledOnValidThread());
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   Record record = { observer, type, source };
   return std::find(registered_.begin(), registered_.end(), record) !=
       registered_.end();
