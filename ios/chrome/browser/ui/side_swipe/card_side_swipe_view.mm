b1dd44c3ab3742e546804302d6a093fbadd47a7b
kkhorimoto <kkhorimoto@chromium.org>
2017-05-31 15:27:57 -0700

    Remove usage of Tab's |url| property from CardSideSwipeView.
    BUG=546208
    
    Review-Url: https://codereview.chromium.org/2822743002
    Cr-Commit-Position: refs/heads/master@{#476062}
    
    Former-commit-id: baa0132eb52a6aece13847b9b2c3b7dd0dce8e4f

--- ios/chrome/browser/ui/side_swipe/card_side_swipe_view.mm
+++ ios/chrome/browser/ui/side_swipe/card_side_swipe_view.mm
@@ -227,7 +227,7 @@ const CGFloat kResizeFactor = 4;
   [card setHidden:NO];
 
   Tab* tab = [model_ tabAtIndex:index];
-  BOOL isNTP = tab.url.host() == kChromeUINewTabHost;
+  BOOL isNTP = tab.lastCommittedURL.host() == kChromeUINewTabHost;
   [toolbarController updateToolbarForSideSwipeSnapshot:tab];
   UIImage* toolbarView = CaptureViewWithOption([toolbarController view],
                                                [[UIScreen mainScreen] scale],
