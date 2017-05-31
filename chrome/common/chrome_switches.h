1e8af64b2f9ed74358cb6037e3c19bf0062f6050
mmenke <mmenke@chromium.org>
2017-05-30 17:29:38 -0700

    Remove --host-resolver-retry-attempts.
    This doesn't seem to have seen a whole lot of use.
    
    BUG=727894
    
    Review-Url: https://codereview.chromium.org/2912163002
    Cr-Commit-Position: refs/heads/master@{#475704}
    
    Former-commit-id: 9a6e17b873b960f3ec952187af2700b718e64580

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -156,7 +156,6 @@ extern const char kForceFirstRun[];
 extern const char kForceLocalNtp[];
 extern const char kForceVariationIds[];
 extern const char kHomePage[];
-extern const char kHostResolverRetryAttempts[];
 extern const char kHostRules[];
 extern const char kIgnoreCertificateErrorsSPKIList[];
 extern const char kIgnoreUrlFetcherCertRequests[];
