5a88b5a884d9e52179b811c6bc0e1f6348ed2b69
xiaohuic <xiaohuic@chromium.org>
2017-05-30 18:29:53 -0700

    Refactor dbus SessionManagerInterface.StartArcInterface
    Now we use a proto message as parameter for the dbus interface
    to start ARC instances.
    
    BUG=b:37989086
    TEST=locally build and flash, start ARC
    
    Review-Url: https://codereview.chromium.org/2869163002
    Cr-Commit-Position: refs/heads/master@{#475722}
    
    Former-commit-id: 2f57e3f7ccd26bba9a22394f5bf2741a0682b510

--- chromeos/dbus/session_manager_client.cc
+++ chromeos/dbus/session_manager_client.cc
@@ -25,6 +25,7 @@
 #include "chromeos/cryptohome/cryptohome_parameters.h"
 #include "chromeos/dbus/blocking_method_caller.h"
 #include "chromeos/dbus/cryptohome_client.h"
+#include "chromeos/dbus/login_manager/arc.pb.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "crypto/sha2.h"
 #include "dbus/bus.h"
@@ -402,16 +403,20 @@ class SessionManagerClientImpl : public SessionManagerClient {
   }
 
   void StartArcInstance(const cryptohome::Identification& cryptohome_id,
-                        bool disable_boot_completed_broadcast,
-                        bool enable_vendor_privileged,
+                        bool skip_boot_completed_broadcast,
+                        bool scan_vendor_priv_app,
                         const StartArcInstanceCallback& callback) override {
     dbus::MethodCall method_call(
         login_manager::kSessionManagerInterface,
         login_manager::kSessionManagerStartArcInstance);
     dbus::MessageWriter writer(&method_call);
-    writer.AppendString(cryptohome_id.id());
-    writer.AppendBool(disable_boot_completed_broadcast);
-    writer.AppendBool(enable_vendor_privileged);
+
+    login_manager::StartArcInstanceRequest request;
+    request.set_account_id(cryptohome_id.id());
+    request.set_skip_boot_completed_broadcast(skip_boot_completed_broadcast);
+    request.set_scan_vendor_priv_app(scan_vendor_priv_app);
+    writer.AppendProtoAsArrayOfBytes(request);
+
     session_manager_proxy_->CallMethodWithErrorCallback(
         &method_call, dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
         base::Bind(&SessionManagerClientImpl::OnStartArcInstanceSucceeded,
