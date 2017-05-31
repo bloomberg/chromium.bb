2554db34ebab1c2f2a66655c4c90c86b6d783f04
Jochen Eisinger <jochen@chromium.org>
2017-05-31 10:10:11 +0200

    Disallow access to plugin instances from non-main world
    The entire system is not setup to handle multiple worlds and UMA
    suggests that it's also hardly used. So instead of teaching the plugin
    system about multiple world, restrict access to the main world
    
    BUG=708237
    R=haraken@chromium.org
    
    Change-Id: I3fed56c149b2d70593bedb99dff1664a819b3ae4
    Reviewed-on: https://chromium-review.googlesource.com/518902
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#475874}
    Former-commit-id: 1e196e33687bd22a33c8b2116956754b90b29bde

--- third_party/WebKit/Source/bindings/core/v8/custom/V8HTMLPlugInElementCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8HTMLPlugInElementCustom.cpp
@@ -47,12 +47,23 @@ template <typename ElementType>
 void GetScriptableObjectProperty(
     const AtomicString& name,
     const v8::PropertyCallbackInfo<v8::Value>& info) {
+  ScriptState* state = ScriptState::Current(info.GetIsolate());
+  if (!state->World().IsMainWorld()) {
+    if (state->World().IsIsolatedWorld()) {
+      UseCounter::Count(CurrentExecutionContext(info.GetIsolate()),
+                        UseCounter::kPluginInstanceAccessFromIsolatedWorld);
+    }
+    // The plugin system cannot deal with multiple worlds, so block any
+    // non-main world access.
+    return;
+  }
+  UseCounter::Count(CurrentExecutionContext(info.GetIsolate()),
+                    UseCounter::kPluginInstanceAccessFromMainWorld);
+
   HTMLPlugInElement* impl = ElementType::toImpl(info.Holder());
   v8::Local<v8::Object> instance = impl->PluginWrapper();
   if (instance.IsEmpty())
     return;
-
-  ScriptState* state = ScriptState::Current(info.GetIsolate());
 
   v8::Local<v8::String> v8_name = V8String(info.GetIsolate(), name);
   if (!V8CallBoolean(instance->HasOwnProperty(state->GetContext(), v8_name)))
@@ -61,14 +72,6 @@ void GetScriptableObjectProperty(
   v8::Local<v8::Value> value;
   if (!instance->Get(state->GetContext(), v8_name).ToLocal(&value))
     return;
-
-  if (state->World().IsIsolatedWorld()) {
-    UseCounter::Count(CurrentExecutionContext(info.GetIsolate()),
-                      UseCounter::kPluginInstanceAccessFromIsolatedWorld);
-  } else if (state->World().IsMainWorld()) {
-    UseCounter::Count(CurrentExecutionContext(info.GetIsolate()),
-                      UseCounter::kPluginInstanceAccessFromMainWorld);
-  }
 
   V8SetReturnValue(info, value);
 }
@@ -79,6 +82,12 @@ void SetScriptableObjectProperty(
     v8::Local<v8::Value> value,
     const v8::PropertyCallbackInfo<v8::Value>& info) {
   DCHECK(!value.IsEmpty());
+  ScriptState* state = ScriptState::Current(info.GetIsolate());
+  if (!state->World().IsMainWorld()) {
+    // The plugin system cannot deal with multiple worlds, so block any
+    // non-main world access.
+    return;
+  }
 
   HTMLPlugInElement* impl = ElementType::toImpl(info.Holder());
   v8::Local<v8::Object> instance = impl->PluginWrapper();
@@ -87,10 +96,8 @@ void SetScriptableObjectProperty(
 
   // Don't intercept any of the properties of the HTMLPluginElement.
   v8::Local<v8::String> v8_name = V8String(info.GetIsolate(), name);
-  if (!V8CallBoolean(instance->HasOwnProperty(
-          info.GetIsolate()->GetCurrentContext(), v8_name)) &&
-      V8CallBoolean(info.Holder()->Has(info.GetIsolate()->GetCurrentContext(),
-                                       v8_name))) {
+  if (!V8CallBoolean(instance->HasOwnProperty(state->GetContext(), v8_name)) &&
+      V8CallBoolean(info.Holder()->Has(state->GetContext(), v8_name))) {
     return;
   }
 
@@ -103,8 +110,8 @@ void SetScriptableObjectProperty(
   // DOM element will also be set. For plugin's that don't intercept the call
   // (all except gTalk) this makes no difference at all. For gTalk the fact
   // that the property on the DOM element also gets set is inconsequential.
-  V8CallBoolean(instance->CreateDataProperty(
-      info.GetIsolate()->GetCurrentContext(), v8_name, value));
+  V8CallBoolean(
+      instance->CreateDataProperty(state->GetContext(), v8_name, value));
   V8SetReturnValue(info, value);
 }
 
