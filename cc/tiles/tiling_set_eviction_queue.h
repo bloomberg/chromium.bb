c81d261f8766d0092744e9a5a0496aa49712fb46
khushalsagar <khushalsagar@chromium.org>
2017-05-31 13:39:59 -0700

    cc: Keep tile eviction prioritization consistent with raster priorities.
    We prioritize rasterization of drawing layers over non-drawing layers.
    Ensure tile eviction also respects this policy.
    
    BUG=724591
    CQ_INCLUDE_TRYBOTS=master.tryserver.blink:linux_trusty_blink_rel
    
    Review-Url: https://codereview.chromium.org/2910303002
    Cr-Commit-Position: refs/heads/master@{#476005}
    
    Former-commit-id: 2f65325fc076e6e17958c2e4880c1d0d745db5f4

--- cc/tiles/tiling_set_eviction_queue.h
+++ cc/tiles/tiling_set_eviction_queue.h
@@ -67,12 +67,14 @@ namespace cc {
 // with low priority on one tree, but high combined priority.
 class CC_EXPORT TilingSetEvictionQueue {
  public:
-  explicit TilingSetEvictionQueue(PictureLayerTilingSet* tiling_set);
+  explicit TilingSetEvictionQueue(PictureLayerTilingSet* tiling_set,
+                                  bool is_drawing_layer);
   ~TilingSetEvictionQueue();
 
   const PrioritizedTile& Top() const;
   void Pop();
   bool IsEmpty() const;
+  bool is_drawing_layer() const { return is_drawing_layer_; }
 
  private:
   enum Phase {
@@ -198,6 +200,7 @@ class CC_EXPORT TilingSetEvictionQueue {
   SkewportTilingIterator skewport_iterator_;
   PendingVisibleTilingIterator pending_visible_iterator_;
   VisibleTilingIterator visible_iterator_;
+  bool is_drawing_layer_;
 };
 
 }  // namespace cc
