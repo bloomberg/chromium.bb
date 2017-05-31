2a9518842b573864d4ac0447f99f7c646458b22d
maxmorin <maxmorin@chromium.org>
2017-05-30 23:18:55 -0700

    Add a mojo implementation of AudioOutputIPC.
    A couple of tricky points:
    * The requirement to always call CloseStream is added to
      media/audio/audio_output_ipc.h. This requirement isn't new, but it
      wasn't documented before.
    * If the RendererAudioOutputStreamFactory used by MojoAudioOutputIPC is
      destroyed prior to receiving authorization (e.g. because of frame
      destruction), we need to signal the AudioOutputDevice anyways to
      avoid waiting forever. We pass a ScopedClosureRunner into the
      authorization callback to make sure we get a callback even if the
      factory is destroyed early.
    * Code for intergrating this into the AudioDeviceFactory is in
      https://codereview.chromium.org/2890753003
    * Browser tests (using this and the dependent CL) in
      https://codereview.chromium.org/2812883003/#ps380001.
    
    BUG=425368
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Review-Url: https://codereview.chromium.org/2821203005
    Cr-Commit-Position: refs/heads/master@{#475827}
    
    Former-commit-id: aa9020e577bcd1f459094168b00a07aaba063a9f

--- media/audio/audio_output_ipc.h
+++ media/audio/audio_output_ipc.h
@@ -97,7 +97,10 @@ class MEDIA_EXPORT AudioOutputIPC {
   virtual void PauseStream() = 0;
 
   // Closes the audio stream which should shut down the corresponding
-  // AudioOutputController in the peer process.
+  // AudioOutputController in the peer process. Usage of an AudioOutputIPC must
+  // always end with a call to CloseStream(), and the |delegate| passed to other
+  // method must remain valid until then. An exception is if OnIPCClosed is
+  // called first.
   virtual void CloseStream() = 0;
 
   // Sets the volume of the audio stream.
