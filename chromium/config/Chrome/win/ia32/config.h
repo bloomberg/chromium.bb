cccdbd512d897db539541c684f0b9a0824698d83
John Rummell <jrummell@chromium.org>
2017-05-31 14:44:00 -0700

    Remove extra ^M character from compiler version
    This is producing problems compiling on Windows.
    
    BUG=718142
    
    Change-Id: Iee5d7743ec5bf19d22381779f8be7291588464e2
    Reviewed-on: https://chromium-review.googlesource.com/520362
    Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>

--- chromium/config/Chrome/win/ia32/config.h
+++ chromium/config/Chrome/win/ia32/config.h
@@ -6,7 +6,7 @@
 #define CONFIG_THIS_YEAR 2017
 #define FFMPEG_DATADIR "/usr/local/share/ffmpeg"
 #define AVCONV_DATADIR "/usr/local/share/ffmpeg"
-#define CC_IDENT "Microsoft (R) C/C++ Optimizing Compiler Version 19.00.24213.1 for x86"
+#define CC_IDENT "Microsoft (R) C/C++ Optimizing Compiler Version 19.00.24213.1 for x86"
 #define av_restrict __restrict
 #define EXTERN_PREFIX "_"
 #define EXTERN_ASM _
