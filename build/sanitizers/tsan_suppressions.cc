ea70103ed220c565bc25b81f04627fa9e588ba80
gab <gab@chromium.org>
2017-06-01 11:21:49 -0700

    Suppress one more TimerSequenceTest race per known pre-existing race.
    Missed one in https://codereview.chromium.org/2491613004/ (CQ didn't catch it?)
    
    Caught by findit @ https://findit-for-me.appspot.com/waterfall/culprit?key=ag9zfmZpbmRpdC1mb3ItbWVyRAsSDVdmU3VzcGVjdGVkQ0wiMWNocm9taXVtLzRlODQ0YmI1ZjY4NmRiZDNhMDYyYzZhYWY4M2M4YTFkZjE3MWEwOGEM
    
    BUG=587199, 552633
    TBR=danakj
    NOTRY=True
    
    Review-Url: https://codereview.chromium.org/2921463003
    Cr-Commit-Position: refs/heads/master@{#476357}
    
    Former-commit-id: 4dc3eafa33faa138cf508bdcea933813c4a042dd

--- build/sanitizers/tsan_suppressions.cc
+++ build/sanitizers/tsan_suppressions.cc
@@ -246,6 +246,8 @@ char kTSanDefaultSuppressions[] =
     // http://crbug.com/587199
     "race:base::TimerTest_OneShotTimer_CustomTaskRunner_Test::TestBody\n"
     "race:base::TimerSequenceTest_OneShotTimerTaskOnPoolThread_Test::TestBody\n"
+    "race:base::TimerSequenceTest_OneShotTimerUsedAndTaskedOnDifferentPools_"
+    "Test::TestBody\n"
 
     // http://crbug.com/v8/6065
     "race:net::(anonymous namespace)::ProxyResolverV8TracingImpl::RequestImpl"
