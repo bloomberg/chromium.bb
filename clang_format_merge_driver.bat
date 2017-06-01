96fa295ab453feee177e3b6cf0992d576cf87af5
Daniel Jacques <dnj@chromium.org>
2017-06-01 20:43:09 +0000

    Revert "[.bat] Remove "depot_tools" override."
    This reverts commit 2f5f0b7a99c9df653b090e3ef00d13a2cab00a8d.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > [.bat] Remove "depot_tools" override.
    > 
    > Currently, all ".bat" entry points use "~dp0python" to ensure that the
    > Python that is used to execute the tool is the one in depot_tools. This
    > prevents any sort of system override.
    > 
    > Remove this override so that PATH solely determines which Python is
    > used. To accommodate users who invoked these tools without Python on the
    > PATH, we still still add "depot_tools" as a catch-all PATH suffix.
    > 
    > Some tools were also not using DOS-style line endings. This CL fixes
    > this.
    > 
    > BUG=chromium:714293, chromium:724902
    > TEST=None
    > 
    > Change-Id: I06e9583a668c767196a2a335547aded868f2a2b5
    > Reviewed-on: https://chromium-review.googlesource.com/517236
    > Commit-Queue: Daniel Jacques <dnj@chromium.org>
    > Reviewed-by: Robbie Iannucci <iannucci@chromium.org>
    > Reviewed-by: Marc-Antoine Ruel <maruel@chromium.org>
    > 
    
    TBR=maruel@chromium.org,iannucci@chromium.org,vadimsh@chromium.org,dnj@chromium.org
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    BUG=chromium:714293, chromium:724902
    
    Change-Id: I822abdd4e02abd32d2f4789fb16d5a7f78fdd02d
    Reviewed-on: https://chromium-review.googlesource.com/521867
    Reviewed-by: Daniel Jacques <dnj@chromium.org>
    Commit-Queue: Daniel Jacques <dnj@chromium.org>

--- clang_format_merge_driver.bat
+++ clang_format_merge_driver.bat
@@ -4,9 +4,5 @@
 :: found in the LICENSE file.
 setlocal
 
-:: Ensure that "depot_tools" is somewhere in PATH so this tool can be used
-:: standalone, but allow other PATH manipulations to take priority.
-set PATH=%PATH%;%~dp0
-
 :: Defer control.
-python "%~dp0\clang_format_merge_driver.py" %*
+%~dp0python "%~dp0\clang_format_merge_driver.py" %*
