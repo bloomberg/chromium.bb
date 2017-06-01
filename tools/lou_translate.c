9af1b3f5495ce4bf6788e96f66076be396f718f0
Christian Egli <christian.egli@sbs.ch>
2017-06-01 15:20:11 +0200

    Enhance documentation on usage of display tables
    Fixes #306

--- tools/lou_translate.c
+++ tools/lou_translate.c
@@ -108,12 +108,28 @@ output. It is intended for large-scale testing of the accuracy of\n\
 Braille translation and back-translation.\n\n", stdout);
 
   fputs ("\
+Options:\n\
   -h, --help          display this help and exit\n\
   -v, --version       display version information and exit\n\
   -f, --forward       forward translation using the given table\n\
   -b, --backward      backward translation using the given table\n\
                       If neither -f nor -b are specified forward translation\n\
                       is assumed\n", stdout);
+  fputs ("\
+Examples:\n\
+  lou_translate --forward en-us-g2.ctb < input.txt\n\
+  \n\
+  Do a forward translation with table en-us-g2.ctb. The resulting braille is\n\
+  ASCII encoded.\n\
+  \n\
+  lou_translate unicode.dis,en-us-g2.ctb < input.txt\n\
+  \n\
+  Do a forward translation with table en-us-g2.ctb. The resulting braille is\n\
+  encoded as Unicode dot patterns.\n\
+  \n\
+  echo \",! qk br{n fox\" | lou_translate --backward en-us-g2.ctb\n\
+  \n\
+  Do a backward translation with table en-us-g2.ctb.\n", stdout);
   printf ("\n");
   printf ("Report bugs to %s.\n", PACKAGE_BUGREPORT);
 
