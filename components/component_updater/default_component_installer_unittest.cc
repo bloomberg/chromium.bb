0f0ed9ad7b8ae0bf689d7c858794f4bd6a317fe0
Sorin Jianu <sorin@chromium.org>
2017-06-01 16:42:32 -0700

    Remove DefaultComponentInstaller::manifest_ member.
    This change simplifies the scope and lifetime of the manifest_.
    
    Bug: 728667
    Change-Id: Ia234f9cf6e56dac45c5f358f6b3cd8bdc6652ba3
    Reviewed-on: https://chromium-review.googlesource.com/522202
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Joshua Pawlicki <waffles@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476512}
    Former-commit-id: 0bf4bd3f8a3111af1a7194607248a3649a1b7baf

--- components/component_updater/default_component_installer_unittest.cc
+++ components/component_updater/default_component_installer_unittest.cc
@@ -244,6 +244,8 @@ TEST_F(DefaultComponentInstallerTest, RegisterComponent) {
     base::Closure quit_closure_;
   };
 
+  base::ScopedPathOverride scoped_path_override(DIR_COMPONENT_USER);
+
   const std::string id("jebgalgnebhfojomionfpkfelancnnkf");
 
   // Quit after one update check has been fired.
