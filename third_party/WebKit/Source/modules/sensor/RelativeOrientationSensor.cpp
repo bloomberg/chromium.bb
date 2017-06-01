bb160a4729f20a2aed15c873345315667ba37504
mikhail.pozdnyakov <mikhail.pozdnyakov@intel.com>
2017-06-01 09:34:09 -0700

    [Sensors] Bindings for RelativeOrientationSensor
    BUG=661478
    
    Review-Url: https://codereview.chromium.org/2908393002
    Cr-Commit-Position: refs/heads/master@{#476320}
    
    Former-commit-id: 5e98276d1f29fbb44d414f20630a2dfb7c376727

---
+++ third_party/WebKit/Source/modules/sensor/RelativeOrientationSensor.cpp
@@ -0,0 +1,39 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/sensor/RelativeOrientationSensor.h"
+
+using device::mojom::blink::SensorType;
+
+namespace blink {
+
+RelativeOrientationSensor* RelativeOrientationSensor::Create(
+    ExecutionContext* execution_context,
+    const SensorOptions& options,
+    ExceptionState& exception_state) {
+  return new RelativeOrientationSensor(execution_context, options,
+                                       exception_state);
+}
+
+// static
+RelativeOrientationSensor* RelativeOrientationSensor::Create(
+    ExecutionContext* execution_context,
+    ExceptionState& exception_state) {
+  return Create(execution_context, SensorOptions(), exception_state);
+}
+
+RelativeOrientationSensor::RelativeOrientationSensor(
+    ExecutionContext* execution_context,
+    const SensorOptions& options,
+    ExceptionState& exception_state)
+    : OrientationSensor(execution_context,
+                        options,
+                        exception_state,
+                        SensorType::RELATIVE_ORIENTATION) {}
+
+DEFINE_TRACE(RelativeOrientationSensor) {
+  OrientationSensor::Trace(visitor);
+}
+
+}  // namespace blink
---
+++ third_party/WebKit/Source/modules/sensor/RelativeOrientationSensor.h
@@ -0,0 +1,31 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef RelativeOrientationSensor_h
+#define RelativeOrientationSensor_h
+
+#include "modules/sensor/OrientationSensor.h"
+
+namespace blink {
+
+class RelativeOrientationSensor final : public OrientationSensor {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  static RelativeOrientationSensor* Create(ExecutionContext*,
+                                           const SensorOptions&,
+                                           ExceptionState&);
+  static RelativeOrientationSensor* Create(ExecutionContext*, ExceptionState&);
+
+  DECLARE_VIRTUAL_TRACE();
+
+ private:
+  RelativeOrientationSensor(ExecutionContext*,
+                            const SensorOptions&,
+                            ExceptionState&);
+};
+
+}  // namespace blink
+
+#endif  // RelativeOrientationSensor_h
