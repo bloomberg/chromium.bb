1fecf635b66a2708f07b05c93d8b3a97d40e5208
John Budorick <jbudorick@chromium.org>
2017-06-01 00:05:24 +0000

    Revert "[Offline Pages] Ensure the archive folder exists when saving a page."
    This reverts commit e30a3782d4a422ecee2daeaedd52cfb0212f692e [formerly 9cbc9e00f731772c85ab54f9dad713bafd0d7ee4].
    
    Reason for revert: https://bugs.chromium.org/p/chromium/issues/detail?id=728433
    
    Original change's description:
    > [Offline Pages] Ensure the archive folder exists when saving a page.
    > 
    > Move the logic ensuring archives directory is created from the startup of
    > OfflinePageModel to every time saving a page. This change will also handle
    > the directory creation gracefully to not crash Chrome.
    > 
    > Bug: 725266
    > Change-Id: Iec46b007e51c33d90c3e282919a6313d7a5e3f85
    > Reviewed-on: https://chromium-review.googlesource.com/513984
    > Commit-Queue: Yafei Duan <romax@chromium.org>
    > Reviewed-by: Filip Gorski <fgorski@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#475992}
    
    TBR=fgorski@chromium.org,romax@chromium.org
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 725266
    
    Change-Id: Ic9da92f529b68f3b472c072cecec2b9fc1059802
    Reviewed-on: https://chromium-review.googlesource.com/520484
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476138}
    Former-commit-id: 04b1442d27ae436d542e0e2ad0a072f7d4a5ec12

--- components/offline_pages/core/archive_manager.cc
+++ components/offline_pages/core/archive_manager.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "components/offline_pages/core/archive_manager.h"
-
 #include "base/bind.h"
 #include "base/callback.h"
 #include "base/files/file_enumerator.h"
@@ -13,8 +11,8 @@
 #include "base/memory/ref_counted.h"
 #include "base/sequenced_task_runner.h"
 #include "base/sys_info.h"
-#include "base/task_runner_util.h"
 #include "base/threading/thread_task_runner_handle.h"
+#include "components/offline_pages/core/archive_manager.h"
 
 namespace offline_pages {
 
@@ -23,13 +21,8 @@ namespace {
 using StorageStatsCallback =
     base::Callback<void(const ArchiveManager::StorageStats& storage_stats)>;
 
-ArchiveManager::ArchivesDirCreationResult EnsureArchivesDirCreatedImpl(
-    const base::FilePath& archives_dir) {
-  if (base::PathExists(archives_dir))
-    return ArchiveManager::ArchivesDirCreationResult::ALREADY_EXISTS;
-  if (base::CreateDirectory(archives_dir))
-    return ArchiveManager::ArchivesDirCreationResult::SUCCESS;
-  return ArchiveManager::ArchivesDirCreationResult::FAILURE;
+void EnsureArchivesDirCreatedImpl(const base::FilePath& archives_dir) {
+  CHECK(base::CreateDirectory(archives_dir));
 }
 
 void ExistsArchiveImpl(const base::FilePath& file_path,
@@ -86,11 +79,10 @@ ArchiveManager::ArchiveManager(
 
 ArchiveManager::~ArchiveManager() {}
 
-void ArchiveManager::EnsureArchivesDirCreated(
-    const base::Callback<void(ArchivesDirCreationResult)>& callback) {
-  base::PostTaskAndReplyWithResult(
-      task_runner_.get(), FROM_HERE,
-      base::Bind(EnsureArchivesDirCreatedImpl, archives_dir_), callback);
+void ArchiveManager::EnsureArchivesDirCreated(const base::Closure& callback) {
+  task_runner_->PostTaskAndReply(
+      FROM_HERE, base::Bind(EnsureArchivesDirCreatedImpl, archives_dir_),
+      callback);
 }
 
 void ArchiveManager::ExistsArchive(const base::FilePath& archive_path,
