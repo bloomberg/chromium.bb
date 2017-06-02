8d66b74dcb7fa62c198ff370e95335d74d55f570
tzik <tzik@chromium.org>
2017-06-01 22:01:32 -0700

    Use OnceCallback on Mojo interfaces in //device/generic_sensor
    This CL flips `use_once_calback` flag on the Mojo code generator, and
    fixes all compile errors after it. After this CL, Mojo interfaces in
    //device/generic_sensor starts using base::OnceCallback instead of
    base::Callback on its return value handling.
    
    The migration recipe was:
     - Convert pass-by-ref callback objects to pass-by-value.
     - Use std::move() to forward it to other consumer, or to invoke it
       with Callback::Run().
     - Handle wherever copies are required manually.
     - Check if the conversion doesn't change the semantics. As the transfer
       and invocation clobber the callback object, care about use-after-move.
       It's considered safe to consume almost scoped-out callback.
    
    BUG=714018
    
    Review-Url: https://codereview.chromium.org/2865013003
    Cr-Commit-Position: refs/heads/master@{#476574}
    
    Former-commit-id: fd68f9c8dce64243427e2e5ae65272708e852b54

--- device/generic_sensor/sensor_impl.cc
+++ device/generic_sensor/sensor_impl.cc
@@ -25,21 +25,21 @@ mojom::SensorClientRequest SensorImpl::GetClient() {
 
 void SensorImpl::AddConfiguration(
     const PlatformSensorConfiguration& configuration,
-    const AddConfigurationCallback& callback) {
+    AddConfigurationCallback callback) {
   // TODO(Mikhail): To avoid overflowing browser by repeated AddConfigs
   // (maybe limit the number of configs per client).
-  callback.Run(sensor_->StartListening(this, configuration));
+  std::move(callback).Run(sensor_->StartListening(this, configuration));
 }
 
 void SensorImpl::GetDefaultConfiguration(
-    const GetDefaultConfigurationCallback& callback) {
-  callback.Run(sensor_->GetDefaultConfiguration());
+    GetDefaultConfigurationCallback callback) {
+  std::move(callback).Run(sensor_->GetDefaultConfiguration());
 }
 
 void SensorImpl::RemoveConfiguration(
     const PlatformSensorConfiguration& configuration,
-    const RemoveConfigurationCallback& callback) {
-  callback.Run(sensor_->StopListening(this, configuration));
+    RemoveConfigurationCallback callback) {
+  std::move(callback).Run(sensor_->StopListening(this, configuration));
 }
 
 void SensorImpl::Suspend() {
--- device/generic_sensor/sensor_provider_impl.h
+++ device/generic_sensor/sensor_provider_impl.h
@@ -33,13 +33,13 @@ class DEVICE_GENERIC_SENSOR_EXPORT SensorProviderImpl final
   // SensorProvider implementation.
   void GetSensor(mojom::SensorType type,
                  mojom::SensorRequest sensor_request,
-                 const GetSensorCallback& callback) override;
+                 GetSensorCallback callback) override;
 
   // Helper callback method to return created sensors.
   void SensorCreated(mojom::SensorType type,
                      mojo::ScopedSharedBufferHandle cloned_handle,
                      mojom::SensorRequest sensor_request,
-                     const GetSensorCallback& callback,
+                     GetSensorCallback callback,
                      scoped_refptr<PlatformSensor> sensor);
 
   PlatformSensorProvider* provider_;
