db89687c3129b51182fe56d115d484c4ac01d567
bmcquade <bmcquade@chromium.org>
2017-06-02 10:42:19 -0700

    Ignore download bytes in page load metrics.
    We currently end up ignoring download bytes by chance.
    This change makes us explicitly ignore download bytes, and
    adds a test.
    
    BUG=728696
    
    Review-Url: https://codereview.chromium.org/2917873003
    Cr-Commit-Position: refs/heads/master@{#476710}
    
    Former-commit-id: 71a51d537b4bf80c9a30223bc916c05f373462a2

--- chrome/browser/loader/chrome_resource_dispatcher_host_delegate.cc
+++ chrome/browser/loader/chrome_resource_dispatcher_host_delegate.cc
@@ -350,19 +350,21 @@ void NotifyUIThreadOfRequestStarted(
     const content::ResourceRequestInfo::WebContentsGetter& web_contents_getter,
     const content::GlobalRequestID& request_id,
     ResourceType resource_type,
+    bool is_download,
     base::TimeTicks request_creation_time) {
   content::WebContents* web_contents = web_contents_getter.Run();
-
   if (!web_contents)
     return;
 
-  page_load_metrics::MetricsWebContentsObserver* metrics_observer =
-      page_load_metrics::MetricsWebContentsObserver::FromWebContents(
-          web_contents);
+  if (!is_download) {
+    page_load_metrics::MetricsWebContentsObserver* metrics_observer =
+        page_load_metrics::MetricsWebContentsObserver::FromWebContents(
+            web_contents);
 
-  if (metrics_observer) {
-    metrics_observer->OnRequestStarted(request_id, resource_type,
-                                       request_creation_time);
+    if (metrics_observer) {
+      metrics_observer->OnRequestStarted(request_id, resource_type,
+                                         request_creation_time);
+    }
   }
 }
 
@@ -373,6 +375,7 @@ void NotifyUIThreadOfRequestComplete(
     const GURL& url,
     const content::GlobalRequestID& request_id,
     ResourceType resource_type,
+    bool is_download,
     bool was_cached,
     std::unique_ptr<data_reduction_proxy::DataReductionProxyData>
         data_reduction_proxy_data,
@@ -400,14 +403,16 @@ void NotifyUIThreadOfRequestComplete(
       background_loader->OnNetworkBytesChanged(total_received_bytes);
 #endif  // OS_ANDROID
   }
-  page_load_metrics::MetricsWebContentsObserver* metrics_observer =
-      page_load_metrics::MetricsWebContentsObserver::FromWebContents(
-          web_contents);
-  if (metrics_observer) {
-    metrics_observer->OnRequestComplete(
-        url, frame_tree_node_id_getter.Run(), request_id, resource_type,
-        was_cached, std::move(data_reduction_proxy_data), raw_body_bytes,
-        original_content_length, request_creation_time);
+  if (!is_download) {
+    page_load_metrics::MetricsWebContentsObserver* metrics_observer =
+        page_load_metrics::MetricsWebContentsObserver::FromWebContents(
+            web_contents);
+    if (metrics_observer) {
+      metrics_observer->OnRequestComplete(
+          url, frame_tree_node_id_getter.Run(), request_id, resource_type,
+          was_cached, std::move(data_reduction_proxy_data), raw_body_bytes,
+          original_content_length, request_creation_time);
+    }
   }
 }
 
@@ -478,7 +483,7 @@ void ChromeResourceDispatcherHostDelegate::RequestBeginning(
       base::Bind(&NotifyUIThreadOfRequestStarted,
                  info->GetWebContentsGetterForRequest(),
                  info->GetGlobalRequestID(), info->GetResourceType(),
-                 request->creation_time()));
+                 info->IsDownload(), request->creation_time()));
 
   ProfileIOData* io_data = ProfileIOData::FromResourceContext(
       resource_context);
@@ -873,16 +878,16 @@ void ChromeResourceDispatcherHostDelegate::RequestComplete(
 
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
-      base::BindOnce(&NotifyUIThreadOfRequestComplete,
-                     info->GetWebContentsGetterForRequest(),
-                     info->GetFrameTreeNodeIdGetterForRequest(),
-                     url_request->url(), info->GetGlobalRequestID(),
-                     info->GetResourceType(), url_request->was_cached(),
-                     base::Passed(&data_reduction_proxy_data), net_error,
-                     url_request->GetTotalReceivedBytes(),
-                     url_request->GetRawBodyBytes(), original_content_length,
-                     url_request->creation_time(),
-                     base::TimeTicks::Now() - url_request->creation_time()));
+      base::BindOnce(
+          &NotifyUIThreadOfRequestComplete,
+          info->GetWebContentsGetterForRequest(),
+          info->GetFrameTreeNodeIdGetterForRequest(), url_request->url(),
+          info->GetGlobalRequestID(), info->GetResourceType(),
+          info->IsDownload(), url_request->was_cached(),
+          base::Passed(&data_reduction_proxy_data), net_error,
+          url_request->GetTotalReceivedBytes(), url_request->GetRawBodyBytes(),
+          original_content_length, url_request->creation_time(),
+          base::TimeTicks::Now() - url_request->creation_time()));
 }
 
 content::PreviewsState ChromeResourceDispatcherHostDelegate::GetPreviewsState(
--- chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
+++ chrome/browser/page_load_metrics/page_load_metrics_browsertest.cc
@@ -1111,6 +1111,33 @@ IN_PROC_BROWSER_TEST_P(PageLoadMetricsBrowserTest, PayloadSize) {
   histogram_tester_.ExpectBucketCount(internal::kHistogramTotalBytes, 10, 1);
 }
 
+IN_PROC_BROWSER_TEST_P(PageLoadMetricsBrowserTest,
+                       PayloadSizeIgnoresDownloads) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+
+  bool prev_io_allowed = base::ThreadRestrictions::SetIOAllowed(true);
+  {
+    base::ScopedTempDir downloads_directory;
+    ASSERT_TRUE(downloads_directory.CreateUniqueTempDir());
+    browser()->profile()->GetPrefs()->SetFilePath(
+        prefs::kDownloadDefaultDirectory, downloads_directory.GetPath());
+    content::DownloadTestObserverTerminal downloads_observer(
+        content::BrowserContext::GetDownloadManager(browser()->profile()),
+        1,  // == wait_count (only waiting for "download-test1.lib").
+        content::DownloadTestObserver::ON_DANGEROUS_DOWNLOAD_FAIL);
+
+    ui_test_utils::NavigateToURL(
+        browser(), embedded_test_server()->GetURL(
+                       "/page_load_metrics/download_anchor_click.html"));
+    downloads_observer.WaitForFinished();
+  }
+  base::ThreadRestrictions::SetIOAllowed(prev_io_allowed);
+
+  NavigateToUntrackedUrl();
+
+  histogram_tester_.ExpectUniqueSample(internal::kHistogramTotalBytes, 0, 1);
+}
+
 INSTANTIATE_TEST_CASE_P(
     /* no prefix */,
     PageLoadMetricsBrowserTest,
