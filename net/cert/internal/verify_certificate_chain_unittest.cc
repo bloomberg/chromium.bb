2160ddfe4917e1aed853153bef46470860721dba
yoichio <yoichio@chromium.org>
2017-06-01 21:29:42 -0700

    Revert of Add policies support to VerifyCertificateChain(). (patchset #9 id:160001 of https://codereview.chromium.org/2903283002/ )
    Reason for revert:
    This patch causes VerifyCertificateChain test failure on linux:
    https://uberchromegw.corp.google.com/i/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%28dbg%29%281%29/builds/26805
    
    Original issue's description:
    > Add policies support to VerifyCertificateChain().
    >
    > Support is compliant with RFC 5280 and supports all the policy
    > extensions specified therein:
    >
    >  * Inhibit Any Policy
    >  * Policy Constraints
    >  * Policies
    >  * Policy Mappings
    >
    > Testing is done solely using the PKITS test suite, which has fairly good
    > coverage of these extensions:
    >
    >   4.8 (Certificate Policies)
    >   4.9 (Require Explicit Policy)
    >   4.10 (Policy Mappings)
    >   4.11 (Inhibit Policy Mapping)
    >   4.12 (Inhibit Any Policy)
    >
    > BUG=634456,634453,634452
    >
    > Review-Url: https://codereview.chromium.org/2903283002
    > Cr-Commit-Position: refs/heads/master@{#476513}
    > Committed: https://chromium.googlesource.com/chromium/src/+/2346e8f6e665a91ac2df54cce954211e039d73be [formerly bcca0368ac116828cecc6c68a381bfc5147f8c98]
    
    TBR=mattm@chromium.org,eroman@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=634456,634453,634452
    
    Review-Url: https://codereview.chromium.org/2918063002
    Cr-Commit-Position: refs/heads/master@{#476562}
    
    Former-commit-id: 854d102a9172d20548a8e8a1b5310e02dfeb9759

--- net/cert/internal/verify_certificate_chain_unittest.cc
+++ net/cert/internal/verify_certificate_chain_unittest.cc
@@ -19,13 +19,8 @@ class VerifyCertificateChainDelegate {
     SimpleSignaturePolicy signature_policy(1024);
 
     CertPathErrors errors;
-    // TODO(eroman): Check user_constrained_policy_set.
-    VerifyCertificateChain(
-        test.chain, test.last_cert_trust, &signature_policy, test.time,
-        test.key_purpose, test.initial_explicit_policy,
-        test.user_initial_policy_set, test.initial_policy_mapping_inhibit,
-        test.initial_any_policy_inhibit,
-        nullptr /*user_constrained_policy_set*/, &errors);
+    VerifyCertificateChain(test.chain, test.last_cert_trust, &signature_policy,
+                           test.time, test.key_purpose, &errors);
     EXPECT_EQ(test.expected_errors, errors.ToDebugString(test.chain))
         << "Test file: " << test_file_path;
   }
