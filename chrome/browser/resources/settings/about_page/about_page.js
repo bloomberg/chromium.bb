2ed5b135944f3e0b90b7c2760d381379a50fff4e
stevenjb <stevenjb@chromium.org>
2017-06-02 19:28:51 -0700

    Settings: About: Fix restart and powerwash button visibility
    Currently if |currentChannel_| changes after |targetChannel_|
    computeShowRelaunchAndPowerwash_() will not get called and the
    button visibility may be incorrect. Instead of fixing the
    computeShowRelaunchAndPowerwash_() params, update visibity for
    both in updateShowRelaunch_() so that they stay in sync.
    
    BUG=724518
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.linux:closure_compilation
    
    Review-Url: https://codereview.chromium.org/2916383002
    Cr-Commit-Position: refs/heads/master@{#476867}
    
    Former-commit-id: 038795765171270a268fde65453f5af66d914061

--- chrome/browser/resources/settings/about_page/about_page.js
+++ chrome/browser/resources/settings/about_page/about_page.js
@@ -66,7 +66,7 @@ Polymer({
     showRelaunchAndPowerwash_: {
       type: Boolean,
       computed: 'computeShowRelaunchAndPowerwash_(' +
-          'currentUpdateStatusEvent_, targetChannel_)',
+          'currentUpdateStatusEvent_, targetChannel_, currentChannel_)',
     },
 
     /** @private */
