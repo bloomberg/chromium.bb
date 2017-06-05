8baa848d2b14439864f2c311fb32bbeed1c6e682
yfriedman <yfriedman@chromium.org>
2017-06-05 07:13:21 -0700

    [Notifications] Only set defaults and vibration pattern when explicitly set.
    Cleans up a poor assumption about channel definitions.
    BUG=729111
    
    Review-Url: https://codereview.chromium.org/2916243005
    Cr-Commit-Position: refs/heads/master@{#476980}
    
    Former-commit-id: 3357f82e8c79423a4446984c048f7ed3ad4048db

--- chrome/android/java/src/org/chromium/chrome/browser/notifications/StandardNotificationBuilder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/notifications/StandardNotificationBuilder.java
@@ -59,11 +59,8 @@ public class StandardNotificationBuilder extends NotificationBuilderBase {
             addActionToBuilder(builder, mSettingsAction);
         }
         builder.setPriority(mPriority);
-        // Browser channel uses silent notifications to avoid disrupting the user.
-        if (!ChannelDefinitions.CHANNEL_ID_BROWSER.equals(mChannelId)) {
-            builder.setDefaults(mDefaults);
-            builder.setVibrate(mVibratePattern);
-        }
+        builder.setDefaults(mDefaults);
+        if (mVibratePattern != null) builder.setVibrate(mVibratePattern);
         builder.setWhen(mTimestamp);
         builder.setOnlyAlertOnce(!mRenotify);
         setGroupOnBuilder(builder, mOrigin);
