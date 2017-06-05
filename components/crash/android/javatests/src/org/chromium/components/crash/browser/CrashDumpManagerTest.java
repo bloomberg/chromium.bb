8ffec83675881d43b5b7395fccd5c79d0ea9a649
Colin Blundell <blundell@chromium.org>
2017-06-05 11:08:17 +0200

    Disable remaining CrashDumpManager tests due to flake
    At least one of these tests continues to exhibit flake, and there's no
    reason to suppose that the other isn't subject to the same problems as
    the rest of the tests in this suite, as it looks like it's going
    through the same basic flow.
    
    TBR=isherman@chromium.org
    
    Bug: 725379
    Change-Id: I1fa0747d9554e64b67f4c6ee11777d9096ff5572
    Reviewed-on: https://chromium-review.googlesource.com/524042
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476964}
    Former-commit-id: 119740b795871b7c4fc374c3e13d15ebf20ced4f

--- components/crash/android/javatests/src/org/chromium/components/crash/browser/CrashDumpManagerTest.java
+++ components/crash/android/javatests/src/org/chromium/components/crash/browser/CrashDumpManagerTest.java
@@ -68,7 +68,7 @@ public class CrashDumpManagerTest extends InstrumentationTestCase {
 
     // @SmallTest
     // @Feature({"Android-AppBase"})
-    @DisabledTest // Flaky, crbug.com/726976.
+    @DisabledTest // Flaky, crbug.com/725379.
     public void testUploadMinidump_FileDoesntExist() {
         registerUploadCallback(new CrashDumpManager.UploadMinidumpCallback() {
             @Override
@@ -83,6 +83,7 @@ public class CrashDumpManagerTest extends InstrumentationTestCase {
 
     @SmallTest
     @Feature({"Android-AppBase"})
+    @DisabledTest // Flaky, crbug.com/725379.
     public void testUploadMinidump_MinidumpPathIsDirectory() throws IOException {
         File directory = new File(mTempDir, "a_directory");
         assertTrue(directory.mkdir());
@@ -99,6 +100,7 @@ public class CrashDumpManagerTest extends InstrumentationTestCase {
 
     @SmallTest
     @Feature({"Android-AppBase"})
+    @DisabledTest // Flaky, crbug.com/725379.
     public void testUploadMinidump_MinidumpPathIsValid() throws IOException {
         final File minidump = new File(mTempDir, "mini.dmp");
         assertTrue(minidump.createNewFile());
