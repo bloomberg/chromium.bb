3233b7a81622f4ae7063509fa47488071c088f9f
Makoto Shimazu <shimazu@chromium.org>
2017-06-05 11:13:31 +0900

    ServiceWorker: detach when event dispatcher is disconnected unexpectedly
    Currently there are two pipes connected between the browser and the renderer:
    EmbeddedWorkerInstanceClient owned by EmbeddedWorkerInstance and
    ServiceWorkerEventDispatcher owned by ServiceWorkerVersion. When the renderer
    dies unexpectedly, these two endpoints on the browser process are closed
    individually. If worker is started for dispatching event and the renderer
    unfortunately crashes, it's possible to reach a message to notify disconnection
    of ServiceWorkerEventDispatcher and later another message of "OnStarted". In
    that case, ServiceWorkerVersion::event_dispatcher() might return null.
    
    This patch is to solve the issue by calling EmbeddedWorkerInstance::Detach()
    when the message pipe for ServiceWorkerEventDispatcher is closed unexpectedly.
    Detach will close the pipe connected to the EmbeddedWorkerInstanceClient, so
    EmbeddedWorkerInstance::OnStarted won't be invoked anymore.
    
    Bug: 719311
    Change-Id: I24a81c7fb843d9ee41522adee25d8dc319a00c55
    R: falken
    Reviewed-on: https://chromium-review.googlesource.com/518645
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476938}
    Former-commit-id: 683485cf448a4b1e083be8e6a9d02bacb21275a4

--- content/browser/service_worker/service_worker_version.cc
+++ content/browser/service_worker/service_worker_version.cc
@@ -180,6 +180,25 @@ int NextTraceId() {
   return trace_id;
 }
 
+void OnEventDispatcherConnectionError(
+    base::WeakPtr<EmbeddedWorkerInstance> embedded_worker) {
+  if (!embedded_worker)
+    return;
+
+  switch (embedded_worker->status()) {
+    case EmbeddedWorkerStatus::STARTING:
+    case EmbeddedWorkerStatus::RUNNING:
+      // In this case the disconnection might be happening because of sudden
+      // renderer shutdown like crash.
+      embedded_worker->Detach();
+      break;
+    case EmbeddedWorkerStatus::STOPPING:
+    case EmbeddedWorkerStatus::STOPPED:
+      // Do nothing
+      break;
+  }
+}
+
 }  // namespace
 
 const int ServiceWorkerVersion::kTimeoutTimerDelaySeconds = 30;
@@ -1440,6 +1459,8 @@ void ServiceWorkerVersion::StartWorkerInternal() {
       std::move(params), mojo::MakeRequest(&event_dispatcher_),
       base::Bind(&ServiceWorkerVersion::OnStartSentAndScriptEvaluated,
                  weak_factory_.GetWeakPtr()));
+  event_dispatcher_.set_connection_error_handler(base::Bind(
+      &OnEventDispatcherConnectionError, embedded_worker_->AsWeakPtr()));
 }
 
 void ServiceWorkerVersion::StartTimeoutTimer() {
