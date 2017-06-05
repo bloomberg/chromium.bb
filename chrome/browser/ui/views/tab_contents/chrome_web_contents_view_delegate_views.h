d63a35677b700e86e3fe5d19839db660d628bb3d
Scott Violet <sky@chromium.org>
2017-06-05 08:37:24 -0700

    views: convert ViewStorage to ViewTracker
    BrowserView didn't need it at all.
    
    BUG=727934
    TEST=covered by tests
    R=msw@chromium.org
    
    Change-Id: Ia5d3a44a0d0e9f6ea7d152a0c42cabd253636712
    Reviewed-on: https://chromium-review.googlesource.com/522243
    Commit-Queue: Scott Violet <sky@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#476999}
    Former-commit-id: 44b004ca4a75dfdca74aa2605f4ef027f1434299

--- chrome/browser/ui/views/tab_contents/chrome_web_contents_view_delegate_views.h
+++ chrome/browser/ui/views/tab_contents/chrome_web_contents_view_delegate_views.h
@@ -26,6 +26,7 @@ class RenderFrameHost;
 
 namespace views {
 class FocusManager;
+class ViewTracker;
 class Widget;
 }
 
@@ -62,8 +63,8 @@ class ChromeWebContentsViewDelegateViews
   views::FocusManager* GetFocusManager();
   void SetInitialFocus();
 
-  // The id used in the ViewStorage to store the last focused view.
-  int last_focused_view_storage_id_;
+  // Used to store the last focused view.
+  std::unique_ptr<views::ViewTracker> last_focused_view_tracker_;
 
   // The context menu is reset every time we show it, but we keep a pointer to
   // between uses so that it won't go out of scope before we're done with it.
