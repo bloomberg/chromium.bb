0fb36dfe11aa7a2d3cb499e903339ac224775d80
Jungshik Shin <jshin@chromium.org>
2017-06-05 14:30:27 -0700

    Add a few timezones and accept-language items
    Also fix the indentation.
    
    Bug: None
    Test: CrOS timezone selection menu shows America/Tegucigalpa, America/Santiago
    Change-Id: Id3702694b5c3a2ccf65e05a507f401a1d3ed589d
    Reviewed-on: https://chromium-review.googlesource.com/523678
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Reviewed-by: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477153}
    Former-commit-id: e50c5169c9edc323d2192bf5c79a1bcf455849b0

--- ui/base/l10n/l10n_util.cc
+++ ui/base/l10n/l10n_util.cc
@@ -49,152 +49,159 @@
 namespace {
 
 static const char* const kAcceptLanguageList[] = {
-  "af",     // Afrikaans
-  "am",     // Amharic
-  "an",     // Aragonese
-  "ar",     // Arabic
-  "ast",    // Asturian
-  "az",     // Azerbaijani
-  "be",     // Belarusian
-  "bg",     // Bulgarian
-  "bh",     // Bihari
-  "bn",     // Bengali
-  "br",     // Breton
-  "bs",     // Bosnian
-  "ca",     // Catalan
-  "ckb",    // Kurdish (Arabci),  Sorani
-  "co",     // Corsican
-  "cs",     // Czech
-  "cy",     // Welsh
-  "da",     // Danish
-  "de",     // German
-  "de-AT",  // German (Austria)
-  "de-CH",  // German (Switzerland)
-  "de-DE",  // German (Germany)
-  "de-LI",  // German (Liechtenstein)
-  "el",     // Greek
-  "en",     // English
-  "en-AU",  // English (Australia)
-  "en-CA",  // English (Canada)
-  "en-GB",  // English (UK)
-  "en-IN",  // English (India)
-  "en-NZ",  // English (New Zealand)
-  "en-US",  // English (US)
-  "en-ZA",  // English (South Africa)
-  "eo",     // Esperanto
-  // TODO(jungshik) : Do we want to list all es-Foo for Latin-American
-  // Spanish speaking countries?
-  "es",     // Spanish
-  "es-419", // Spanish (Latin America)
-  "es-AR",  // Spanish (Argentina)
-  "es-ES",  // Spanish (Spain)
-  "es-MX",  // Spanish (Mexico)
-  "es-US",  // Spanish (US)
-  "et",     // Estonian
-  "eu",     // Basque
-  "fa",     // Persian
-  "fi",     // Finnish
-  "fil",    // Filipino
-  "fo",     // Faroese
-  "fr",     // French
-  "fr-CA",  // French (Canada)
-  "fr-CH",  // French (Switzerland)
-  "fr-FR",  // French (France)
-  "fy",     // Frisian
-  "ga",     // Irish
-  "gd",     // Scots Gaelic
-  "gl",     // Galician
-  "gn",     // Guarani
-  "gu",     // Gujarati
-  "ha",     // Hausa
-  "haw",    // Hawaiian
-  "he",     // Hebrew
-  "hi",     // Hindi
-  "hr",     // Croatian
-  "hu",     // Hungarian
-  "hy",     // Armenian
-  "ia",     // Interlingua
-  "id",     // Indonesian
-  "is",     // Icelandic
-  "it",     // Italian
-  "it-CH",  // Italian (Switzerland)
-  "it-IT",  // Italian (Italy)
-  "ja",     // Japanese
-  "jw",     // Javanese
-  "ka",     // Georgian
-  "kk",     // Kazakh
-  "km",     // Cambodian
-  "kn",     // Kannada
-  "ko",     // Korean
-  "ku",     // Kurdish
-  "ky",     // Kyrgyz
-  "la",     // Latin
-  "ln",     // Lingala
-  "lo",     // Laothian
-  "lt",     // Lithuanian
-  "lv",     // Latvian
-  "mk",     // Macedonian
-  "ml",     // Malayalam
-  "mn",     // Mongolian
-  "mo",     // Moldavian
-  "mr",     // Marathi
-  "ms",     // Malay
-  "mt",     // Maltese
-  "nb",     // Norwegian (Bokmal)
-  "ne",     // Nepali
-  "nl",     // Dutch
-  "nn",     // Norwegian (Nynorsk)
-  "no",     // Norwegian
-  "oc",     // Occitan
-  "om",     // Oromo
-  "or",     // Oriya
-  "pa",     // Punjabi
-  "pl",     // Polish
-  "ps",     // Pashto
-  "pt",     // Portuguese
-  "pt-BR",  // Portuguese (Brazil)
-  "pt-PT",  // Portuguese (Portugal)
-  "qu",     // Quechua
-  "rm",     // Romansh
-  "ro",     // Romanian
-  "ru",     // Russian
-  "sd",     // Sindhi
-  "sh",     // Serbo-Croatian
-  "si",     // Sinhalese
-  "sk",     // Slovak
-  "sl",     // Slovenian
-  "sn",     // Shona
-  "so",     // Somali
-  "sq",     // Albanian
-  "sr",     // Serbian
-  "st",     // Sesotho
-  "su",     // Sundanese
-  "sv",     // Swedish
-  "sw",     // Swahili
-  "ta",     // Tamil
-  "te",     // Telugu
-  "tg",     // Tajik
-  "th",     // Thai
-  "ti",     // Tigrinya
-  "tk",     // Turkmen
-  "to",     // Tonga
-  "tr",     // Turkish
-  "tt",     // Tatar
-  "tw",     // Twi
-  "ug",     // Uighur
-  "uk",     // Ukrainian
-  "ur",     // Urdu
-  "uz",     // Uzbek
-  "vi",     // Vietnamese
-  "wa",     // Walloon
-  "xh",     // Xhosa
-  "yi",     // Yiddish
-  "yo",     // Yoruba
-  "zh",     // Chinese
-  "zh-CN",  // Chinese (China)
-  "zh-HK",  // Chinese (Hong Kong)
-  "zh-TW",  // Chinese (Taiwan)
-  "zu",     // Zulu
+    "af",     // Afrikaans
+    "am",     // Amharic
+    "an",     // Aragonese
+    "ar",     // Arabic
+    "ast",    // Asturian
+    "az",     // Azerbaijani
+    "be",     // Belarusian
+    "bg",     // Bulgarian
+    "bh",     // Bihari
+    "bn",     // Bengali
+    "br",     // Breton
+    "bs",     // Bosnian
+    "ca",     // Catalan
+    "ckb",    // Kurdish (Arabci),  Sorani
+    "co",     // Corsican
+    "cs",     // Czech
+    "cy",     // Welsh
+    "da",     // Danish
+    "de",     // German
+    "de-AT",  // German (Austria)
+    "de-CH",  // German (Switzerland)
+    "de-DE",  // German (Germany)
+    "de-LI",  // German (Liechtenstein)
+    "el",     // Greek
+    "en",     // English
+    "en-AU",  // English (Australia)
+    "en-CA",  // English (Canada)
+    "en-GB",  // English (UK)
+    "en-IN",  // English (India)
+    "en-NZ",  // English (New Zealand)
+    "en-US",  // English (US)
+    "en-ZA",  // English (South Africa)
+    "eo",     // Esperanto
+    // TODO(jungshik) : Do we want to list all es-Foo for Latin-American
+    // Spanish speaking countries?
+    "es",      // Spanish
+    "es-419",  // Spanish (Latin America)
+    "es-AR",   // Spanish (Argentina)
+    "es-CL",   // Spanish (Chile)
+    "es-CO",   // Spanish (Colombia)
+    "es-CR",   // Spanish (Costa Rica)
+    "es-ES",   // Spanish (Spain)
+    "es-HN",   // Spanish (Honduras)
+    "es-MX",   // Spanish (Mexico)
+    "es-PE",   // Spanish (Peru)
+    "es-US",   // Spanish (US)
+    "es-UY",   // Spanish (Uruguay)
+    "es-VE",   // Spanish (Venezuela)
+    "et",      // Estonian
+    "eu",      // Basque
+    "fa",      // Persian
+    "fi",      // Finnish
+    "fil",     // Filipino
+    "fo",      // Faroese
+    "fr",      // French
+    "fr-CA",   // French (Canada)
+    "fr-CH",   // French (Switzerland)
+    "fr-FR",   // French (France)
+    "fy",      // Frisian
+    "ga",      // Irish
+    "gd",      // Scots Gaelic
+    "gl",      // Galician
+    "gn",      // Guarani
+    "gu",      // Gujarati
+    "ha",      // Hausa
+    "haw",     // Hawaiian
+    "he",      // Hebrew
+    "hi",      // Hindi
+    "hr",      // Croatian
+    "hu",      // Hungarian
+    "hy",      // Armenian
+    "ia",      // Interlingua
+    "id",      // Indonesian
+    "is",      // Icelandic
+    "it",      // Italian
+    "it-CH",   // Italian (Switzerland)
+    "it-IT",   // Italian (Italy)
+    "ja",      // Japanese
+    "jw",      // Javanese
+    "ka",      // Georgian
+    "kk",      // Kazakh
+    "km",      // Cambodian
+    "kn",      // Kannada
+    "ko",      // Korean
+    "ku",      // Kurdish
+    "ky",      // Kyrgyz
+    "la",      // Latin
+    "ln",      // Lingala
+    "lo",      // Laothian
+    "lt",      // Lithuanian
+    "lv",      // Latvian
+    "mk",      // Macedonian
+    "ml",      // Malayalam
+    "mn",      // Mongolian
+    "mo",      // Moldavian
+    "mr",      // Marathi
+    "ms",      // Malay
+    "mt",      // Maltese
+    "nb",      // Norwegian (Bokmal)
+    "ne",      // Nepali
+    "nl",      // Dutch
+    "nn",      // Norwegian (Nynorsk)
+    "no",      // Norwegian
+    "oc",      // Occitan
+    "om",      // Oromo
+    "or",      // Oriya
+    "pa",      // Punjabi
+    "pl",      // Polish
+    "ps",      // Pashto
+    "pt",      // Portuguese
+    "pt-BR",   // Portuguese (Brazil)
+    "pt-PT",   // Portuguese (Portugal)
+    "qu",      // Quechua
+    "rm",      // Romansh
+    "ro",      // Romanian
+    "ru",      // Russian
+    "sd",      // Sindhi
+    "sh",      // Serbo-Croatian
+    "si",      // Sinhalese
+    "sk",      // Slovak
+    "sl",      // Slovenian
+    "sn",      // Shona
+    "so",      // Somali
+    "sq",      // Albanian
+    "sr",      // Serbian
+    "st",      // Sesotho
+    "su",      // Sundanese
+    "sv",      // Swedish
+    "sw",      // Swahili
+    "ta",      // Tamil
+    "te",      // Telugu
+    "tg",      // Tajik
+    "th",      // Thai
+    "ti",      // Tigrinya
+    "tk",      // Turkmen
+    "to",      // Tonga
+    "tr",      // Turkish
+    "tt",      // Tatar
+    "tw",      // Twi
+    "ug",      // Uighur
+    "uk",      // Ukrainian
+    "ur",      // Urdu
+    "uz",      // Uzbek
+    "vi",      // Vietnamese
+    "wa",      // Walloon
+    "xh",      // Xhosa
+    "yi",      // Yiddish
+    "yo",      // Yoruba
+    "zh",      // Chinese
+    "zh-CN",   // Chinese (China)
+    "zh-HK",   // Chinese (Hong Kong)
+    "zh-TW",   // Chinese (Taiwan)
+    "zu",      // Zulu
 };
 
 // Returns true if |locale_name| has an alias in the ICU data file.
