6f0ee6de3a0bc6c17eb6d030d61495876214c5d6
yhanada <yhanada@chromium.org>
2017-06-06 16:53:22 +0900

    Partial revert of "Initial implementation of new-style notification".
    This reverts cc0fd07fe236a03b10c0a120f5e39cd6031dbbff [formerly f14ac82cdb0d6e6953e59558e0f2884ca8bf89c8] partially.
    The reverted commit includes the change for crbug.com/727288 accidentally.
    That part is reverted by this commit for making the merge to M-60 easy.
    
    BUG=727288
    
    Change-Id: Ib0c8fb53d6f34068bdf3920b76ec88ace5da7c5a
    Reviewed-on: https://chromium-review.googlesource.com/525197
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477236}
    Former-commit-id: 001d4558116ec81cbf9c432b715b12128aaeb7bb

--- ui/arc/notification/arc_notification_content_view.cc
+++ ui/arc/notification/arc_notification_content_view.cc
@@ -10,7 +10,6 @@
 #include "components/exo/notification_surface.h"
 #include "components/exo/surface.h"
 #include "ui/accessibility/ax_action_data.h"
-#include "ui/accessibility/ax_node_data.h"
 #include "ui/arc/notification/arc_notification_view.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -253,7 +252,6 @@ ArcNotificationContentView::ArcNotificationContentView(
   // Create a layer as an anchor to insert surface copy during a slide.
   SetPaintToLayer();
   UpdatePreferredSize();
-  UpdateAccessibleName();
 }
 
 ArcNotificationContentView::~ArcNotificationContentView() {
@@ -487,14 +485,6 @@ bool ArcNotificationContentView::ShouldUpdateControlButtonsColor() const {
   return false;
 }
 
-void ArcNotificationContentView::UpdateAccessibleName() {
-  // Don't update the accessible name when we are about to be destroyed.
-  if (!item_)
-    return;
-
-  accessible_name_ = item_->GetAccessibleName();
-}
-
 void ArcNotificationContentView::ViewHierarchyChanged(
     const views::View::ViewHierarchyChangedDetails& details) {
   views::Widget* widget = GetWidget();
@@ -635,12 +625,6 @@ bool ArcNotificationContentView::HandleAccessibleAction(
   return false;
 }
 
-void ArcNotificationContentView::GetAccessibleNodeData(
-    ui::AXNodeData* node_data) {
-  node_data->role = ui::AX_ROLE_BUTTON;
-  node_data->SetName(accessible_name_);
-}
-
 void ArcNotificationContentView::ButtonPressed(views::Button* sender,
                                                const ui::Event& event) {
   if (item_ && !item_->GetPinned() && sender == close_button_.get()) {
@@ -677,7 +661,6 @@ void ArcNotificationContentView::OnItemDestroying() {
 }
 
 void ArcNotificationContentView::OnItemUpdated() {
-  UpdateAccessibleName();
   UpdatePinnedState();
   UpdateSnapshot();
   if (ShouldUpdateControlButtonsColor())
