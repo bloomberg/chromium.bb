2594aedf62db4867733fae9cc615463fff23e764
Lucas Furukawa Gadani <lfg@chromium.org>
2017-06-06 16:28:46 -0400

    Fix WebViewContextMenuInteractiveTest.ContextMenuParamCoordinates when OOPIF-based guests are enabled.
    Bug: 730041
    Change-Id: I67943a3d04c3722e033fbdcba7189a31b449741f
    Reviewed-on: https://chromium-review.googlesource.com/526512
    Reviewed-by: Ehsan Karamad <ekaramad@chromium.org>
    Reviewed-by: James Maclean <wjmaclean@chromium.org>
    Commit-Queue: Lucas Furukawa Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477680}
    Former-commit-id: 796f884131897ac6a2a2eddd9226224b3f5bcaf7

--- chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
@@ -1017,6 +1017,9 @@ IN_PROC_BROWSER_TEST_P(WebViewNewWindowInteractiveTest,
 // CrossProcessFrameConnector::TransformPointToRootCoordSpace.
 IN_PROC_BROWSER_TEST_F(WebViewContextMenuInteractiveTest,
                        ContextMenuParamCoordinates) {
+  if (base::FeatureList::IsEnabled(features::kGuestViewCrossProcessFrames))
+    return;
+
   TestHelper("testCoordinates", "web_view/context_menus/coordinates",
              NO_TEST_SERVER);
   ASSERT_TRUE(guest_web_contents());
