722240c9337bf70c142074dc7131251b105f2c92
iclelland <iclelland@chromium.org>
2017-06-05 21:11:58 -0700

    Feature Policy: Only add container policy once
    A poorly-positioned closing parenthesis meant that the container policy
    was being applied to newly-created policy objects once per supported
    feature. (Adding the container policy is idempotent, so this doesn't
    represent anything other than an inefficiency at policy creation time.)
    
    Review-Url: https://codereview.chromium.org/2848443005
    Cr-Commit-Position: refs/heads/master@{#477191}
    
    Former-commit-id: 07fadbc84d1cfaac39addc39c2a64b4eafa89030

--- content/common/feature_policy/feature_policy.cc
+++ content/common/feature_policy/feature_policy.cc
@@ -170,9 +170,9 @@ std::unique_ptr<FeaturePolicy> FeaturePolicy::CreateFromParentPolicy(
     } else {
       new_policy->inherited_policies_[feature.first] = false;
     }
-    if (parent_policy && !container_policy.empty())
-      new_policy->AddContainerPolicy(container_policy, parent_policy);
   }
+  if (parent_policy && !container_policy.empty())
+    new_policy->AddContainerPolicy(container_policy, parent_policy);
   return new_policy;
 }
 
