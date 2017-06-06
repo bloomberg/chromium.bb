fef1eede6d0524442fe76d98dcccc54be70afae9
Darren Shen <shend@chromium.org>
2017-06-06 16:58:25 +1000

    Generate methods for SubtreeWillChangeContents in ComputedStyleBase.
    This patch generates getters/setters for SubtreeWillChangeContents.
    
    Diff of generated files:
    https://gist.github.com/46ddf3dfedfbbc8a990e1c3acff6507d/revisions
    
    Bug: 628043
    Change-Id: I411be4511953bb15e9b626439179a8ea8ca1684a
    Reviewed-on: https://chromium-review.googlesource.com/525214
    Commit-Queue: Darren Shen <shend@chromium.org>
    Reviewed-by: Naina Raisinghani <nainar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477437}
    Former-commit-id: c5ccd757d6645405106fffc4c79f9697a7ec4da3

--- third_party/WebKit/Source/core/style/ComputedStyle.h
+++ third_party/WebKit/Source/core/style/ComputedStyle.h
@@ -1547,9 +1547,6 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase<ComputedStyle>,
     return rare_non_inherited_data_->will_change_data_
         ->will_change_scroll_position_;
   }
-  bool SubtreeWillChangeContents() const {
-    return SubtreeWillChangeContentsInternal();
-  }
   void SetWillChangeProperties(const Vector<CSSPropertyID>& properties) {
     SET_NESTED_VAR(rare_non_inherited_data_, will_change_data_,
                    will_change_properties_, properties);
@@ -1561,9 +1558,6 @@ class CORE_EXPORT ComputedStyle : public ComputedStyleBase<ComputedStyle>,
   void SetWillChangeScrollPosition(bool b) {
     SET_NESTED_VAR(rare_non_inherited_data_, will_change_data_,
                    will_change_scroll_position_, b);
-  }
-  void SetSubtreeWillChangeContents(bool b) {
-    SetSubtreeWillChangeContentsInternal(b);
   }
 
   // z-index
