6bb8c2ec30d48597956803f0e461661bddf4962a
Michael Giuffrida <michaelpg@chromium.org>
2017-06-07 10:54:50 -0700

    Move path_util to //extensions
    R=rdevlin.cronin@chromium.org
    
    Bug: 729713
    Change-Id: Iee0ecb436217d769933002dcc2befbd82dc95c79
    Reviewed-on: https://chromium-review.googlesource.com/524989
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477895}
    Former-commit-id: 26c5026c6f179f4e4e98a723d3dd9f1a80e6b38e

--- extensions/browser/path_util.h
+++ extensions/browser/path_util.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_PATH_UTIL_H_
-#define CHROME_BROWSER_EXTENSIONS_PATH_UTIL_H_
+#ifndef EXTENSIONS_BROWSER_PATH_UTIL_H_
+#define EXTENSIONS_BROWSER_PATH_UTIL_H_
 
 #include "base/files/file_path.h"
 
@@ -15,12 +15,9 @@ namespace path_util {
 // For OS X, prettifies |source_path| by localizing every component of the
 // path. Additionally, if the path is inside the user's home directory, then
 // replace the home directory component with "~".
-//
-// This function is tested in
-// chrome/browser/extensions/extension_path_util_unittest.cc.
 base::FilePath PrettifyPath(const base::FilePath& source_path);
 
 }  // namespace path_util
 }  // namespace extensions
 
-#endif  // CHROME_BROWSER_EXTENSIONS_PATH_UTIL_H_
+#endif  // EXTENSIONS_BROWSER_PATH_UTIL_H_
