555964d7e768b613ff3cd073c562ac8cfc34f640
Adam Parker <amp@google.com>
2017-06-07 11:40:13 -0700

    [VrShell] Fix incorrect loading indicator colors.
    This also reorganizes the names to always have foreground first.
    
    Bug: 730712
    Change-Id: Iad914ead69489069f5f07d6d7cf6ef7adae17053
    Reviewed-on: https://chromium-review.googlesource.com/527533
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Christopher Grant <cjgrant@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477750}
    Former-commit-id: 9576b7bd92fc9c368cd0200a0105b8244396bd3f

--- chrome/browser/android/vr_shell/color_scheme.cc
+++ chrome/browser/android/vr_shell/color_scheme.cc
@@ -32,28 +32,28 @@ void InitializeColorSchemes() {
       normal_scheme.element_background_hover;
   normal_scheme.close_button_background_down =
       normal_scheme.element_background_down;
-  normal_scheme.loading_indicator_background = normal_scheme.element_foreground;
-  normal_scheme.loading_indicator_foreground = normal_scheme.floor;
-  normal_scheme.exit_warning_background = 0xCC1A1A1A;
+  normal_scheme.loading_indicator_foreground = normal_scheme.element_foreground;
+  normal_scheme.loading_indicator_background = normal_scheme.floor;
   normal_scheme.exit_warning_foreground = SK_ColorWHITE;
-  normal_scheme.transient_warning_background =
-      normal_scheme.exit_warning_background;
+  normal_scheme.exit_warning_background = 0xCC1A1A1A;
   normal_scheme.transient_warning_foreground =
       normal_scheme.exit_warning_foreground;
-  normal_scheme.permanent_warning_background = SK_ColorWHITE;
+  normal_scheme.transient_warning_background =
+      normal_scheme.exit_warning_background;
   normal_scheme.permanent_warning_foreground = 0xFF444444;
-  normal_scheme.system_indicator_background =
-      normal_scheme.permanent_warning_background;
+  normal_scheme.permanent_warning_background = SK_ColorWHITE;
   normal_scheme.system_indicator_foreground =
       normal_scheme.permanent_warning_foreground;
+  normal_scheme.system_indicator_background =
+      normal_scheme.permanent_warning_background;
   normal_scheme.separator = 0xFF9E9E9E;
   normal_scheme.prompt_foreground = 0xCC000000;
-  normal_scheme.prompt_primary_button_background = 0xBFFFFFFF;
-  normal_scheme.prompt_secondary_button_background = 0x66FFFFFF;
   normal_scheme.prompt_primary_button_forground = 0xA6000000;
   normal_scheme.prompt_secondary_button_foreground = 0xA6000000;
-  normal_scheme.prompt_button_background_down = 0xE6FFFFFF;
+  normal_scheme.prompt_primary_button_background = 0xBFFFFFFF;
+  normal_scheme.prompt_secondary_button_background = 0x66FFFFFF;
   normal_scheme.prompt_button_background_hover = 0xFFFFFFFF;
+  normal_scheme.prompt_button_background_down = 0xE6FFFFFF;
   normal_scheme.secure = gfx::kGoogleGreen700;
   normal_scheme.insecure = gfx::kGoogleRed700;
   normal_scheme.warning = 0xFF5A5A5A;
@@ -84,8 +84,8 @@ void InitializeColorSchemes() {
   incognito_scheme.element_background = 0xCC454545;
   incognito_scheme.element_background_hover = 0xCC505050;
   incognito_scheme.element_background_down = 0xCC888888;
-  incognito_scheme.loading_indicator_foreground = 0xFF454545;
-  incognito_scheme.loading_indicator_background = 0xFF8A8A8A;
+  incognito_scheme.loading_indicator_foreground = 0xFF8A8A8A;
+  incognito_scheme.loading_indicator_background = 0xFF454545;
   incognito_scheme.separator = 0xFF474747;
   incognito_scheme.secure = 0xFFEDEDED;
   incognito_scheme.insecure = incognito_scheme.secure;
@@ -93,10 +93,10 @@ void InitializeColorSchemes() {
   incognito_scheme.url_emphasized = incognito_scheme.secure;
   incognito_scheme.url_deemphasized = 0xFF878787;
   incognito_scheme.prompt_foreground = 0xCCFFFFFF;
-  incognito_scheme.prompt_primary_button_background = 0xD9FFFFFF;
-  incognito_scheme.prompt_secondary_button_background = 0x80FFFFFF;
   incognito_scheme.prompt_primary_button_forground = 0xD9000000;
   incognito_scheme.prompt_secondary_button_foreground = 0xD9000000;
+  incognito_scheme.prompt_primary_button_background = 0xD9FFFFFF;
+  incognito_scheme.prompt_secondary_button_background = 0x80FFFFFF;
   incognito_scheme.prompt_button_background_hover = 0xFF8C8C8C;
   incognito_scheme.prompt_button_background_down = 0xE6FFFFFF;
   incognito_scheme.disabled = 0x33E6E6E6;
