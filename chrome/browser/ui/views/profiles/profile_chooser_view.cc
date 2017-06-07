b6914a5f402bf5ed1b2cc50b8618e887134aaf88
Allen Bauer <kylixrd@chromium.org>
2017-06-07 06:58:39 -0700

    use INSETS_DIALOG_CONTENTS instead of the individual DISTANCE_XXX metrics.
    Bug: 723924
    Change-Id: Ia0bb3b9aa1bd5d759ae515a8e89a0224fb479e27
    Reviewed-on: https://chromium-review.googlesource.com/526279
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#477664}
    Former-commit-id: 7f135c9f1fc53033408152e5024c1fe30c3bb3bb

--- chrome/browser/ui/views/profiles/profile_chooser_view.cc
+++ chrome/browser/ui/views/profiles/profile_chooser_view.cc
@@ -481,9 +481,9 @@ class TitleCard : public views::View {
     int back_button_width = back_button_->GetPreferredSize().width();
     back_button_->SetBounds(0, 0, back_button_width, height());
     int label_padding =
-        back_button_width +
-        ChromeLayoutProvider::Get()->GetDistanceMetric(
-            views::DISTANCE_DIALOG_CONTENTS_HORIZONTAL_MARGIN);
+        back_button_width + ChromeLayoutProvider::Get()
+                                ->GetInsetsMetric(views::INSETS_DIALOG_CONTENTS)
+                                .left();
     int label_width = width() - 2 * label_padding;
     DCHECK_GT(label_width, 0);
     title_label_->SetBounds(label_padding, 0, label_width, height());
