5dc63cbbfb35e38a1a8a6496dd46a456f7a31a85
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-06-07 16:28:35 -0700

    Input type check comparision should be an ASCII case-insensitive match.
    Currently we use unicode-careless matching,which is wrong.
    so ASCII -caseless matching is used now.
    
    BUG=728515
    
    Review-Url: https://codereview.chromium.org/2918033003
    Cr-Commit-Position: refs/heads/master@{#477786}
    
    Former-commit-id: b1bf7d6b39cc77b38b433873a3db686dc3a0ca73

--- third_party/WebKit/Source/core/html/forms/InputType.cpp
+++ third_party/WebKit/Source/core/html/forms/InputType.cpp
@@ -82,8 +82,7 @@ using blink::WebLocalizedString;
 using namespace HTMLNames;
 
 using InputTypeFactoryFunction = InputType* (*)(HTMLInputElement&);
-using InputTypeFactoryMap =
-    HashMap<AtomicString, InputTypeFactoryFunction, CaseFoldingHash>;
+using InputTypeFactoryMap = HashMap<AtomicString, InputTypeFactoryFunction>;
 
 static std::unique_ptr<InputTypeFactoryMap> CreateInputTypeFactoryMap() {
   std::unique_ptr<InputTypeFactoryMap> map =
@@ -136,7 +135,8 @@ const AtomicString& InputType::NormalizeTypeName(
     const AtomicString& type_name) {
   if (type_name.IsEmpty())
     return InputTypeNames::text;
-  InputTypeFactoryMap::const_iterator it = FactoryMap()->find(type_name);
+  InputTypeFactoryMap::const_iterator it =
+      FactoryMap()->find(type_name.LowerASCII());
   return it == FactoryMap()->end() ? InputTypeNames::text : it->key;
 }
 
