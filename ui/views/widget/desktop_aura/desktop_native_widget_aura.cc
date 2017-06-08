69c3e60bd066273f6fa5f1912842291ec6cce0e6
thomasanderson <thomasanderson@chromium.org>
2017-06-07 20:26:12 -0700

    Linux Aura: Use Separate profile key specifically for theme usage
    This CL is a revert to patch set 1 of CL [1].  This is necessary
    because window position save/restore for the task manager relies on
    the regular profile key being null for the task manager widget.
    
    [1] https://codereview.chromium.org/2862113002/#ps1
    
    BUG=729405
    R=sky@chromium.org
    
    Review-Url: https://codereview.chromium.org/2923323002
    Cr-Commit-Position: refs/heads/master@{#477872}
    
    Former-commit-id: 2e3beeadb7a48af2c3dd58e0ec22085fc988636b

--- ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
+++ ui/views/widget/desktop_aura/desktop_native_widget_aura.cc
@@ -400,12 +400,6 @@ gfx::NativeWindow DesktopNativeWidgetAura::GetNativeWindow() const {
   return content_window_;
 }
 
-void DesktopNativeWidgetAura::SetNativeWindowProperty(const char* name,
-                                                      void* value) {
-  if (content_window_)
-    content_window_->SetNativeWindowProperty(name, value);
-}
-
 ////////////////////////////////////////////////////////////////////////////////
 // DesktopNativeWidgetAura, internal::NativeWidgetPrivate implementation:
 
@@ -608,6 +602,12 @@ void DesktopNativeWidgetAura::ReorderNativeViews() {
 void DesktopNativeWidgetAura::ViewRemoved(View* view) {
   DCHECK(drop_helper_.get() != NULL);
   drop_helper_->ResetTargetViewIfEquals(view);
+}
+
+void DesktopNativeWidgetAura::SetNativeWindowProperty(const char* name,
+                                                      void* value) {
+  if (content_window_)
+    content_window_->SetNativeWindowProperty(name, value);
 }
 
 void* DesktopNativeWidgetAura::GetNativeWindowProperty(const char* name) const {
