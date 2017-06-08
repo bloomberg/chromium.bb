52e1f41ef61bca8c490771a66ad14ccb4a85a870
oshima <oshima@chromium.org>
2017-06-08 11:29:42 -0700

    Do not adjust subpixel if the window's position is controlled by client.
    BUG=730903
    TEST=manual. ARC++ apps looks crisp on 1.25 and 1.6 dsf regardless of its position.
    
    Review-Url: https://codereview.chromium.org/2927003002
    Cr-Commit-Position: refs/heads/master@{#478034}
    
    Former-commit-id: a9393413782287bde14d32f64410fed64d683a96

--- ash/wm/window_state.cc
+++ ash/wm/window_state.cc
@@ -452,7 +452,8 @@ void WindowState::SetBoundsDirect(const gfx::Rect& bounds) {
         std::max(min_size.height(), actual_new_bounds.height()));
   }
   BoundsSetter().SetBounds(window_, actual_new_bounds);
-  wm::SnapWindowToPixelBoundary(window_);
+  if (!allow_set_bounds_direct())
+    wm::SnapWindowToPixelBoundary(window_);
 }
 
 void WindowState::SetBoundsConstrained(const gfx::Rect& bounds) {
