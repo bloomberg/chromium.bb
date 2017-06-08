ba3a2f54280d05c4c750503b24dc2a425e800498
kkhorimoto <kkhorimoto@chromium.org>
2017-06-07 20:08:31 -0700

    [iOS Clean] Moved nsurl_protection_space_util into ios/shared/
    Its functionality is needed in both the old and new architecture.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2923963002
    Cr-Commit-Position: refs/heads/master@{#477870}
    
    Former-commit-id: aa2a07d8232fba92dadf65375d6327adce7a9a28

--- ios/shared/chrome/browser/ui/dialogs/nsurl_protection_space_util_unittest.mm
+++ ios/shared/chrome/browser/ui/dialogs/nsurl_protection_space_util_unittest.mm
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#import "ios/chrome/browser/ui/dialogs/nsurl_protection_space_util.h"
+#import "ios/shared/chrome/browser/ui/dialogs/nsurl_protection_space_util.h"
 
 #include "base/ios/ios_util.h"
 #include "base/strings/sys_string_conversions.h"
