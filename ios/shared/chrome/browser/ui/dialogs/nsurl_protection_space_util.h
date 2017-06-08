1191e5fe869ba6842d5dfd78584f516406f70de4
kkhorimoto <kkhorimoto@chromium.org>
2017-06-07 20:08:31 -0700

    [iOS Clean] Moved nsurl_protection_space_util into ios/shared/
    Its functionality is needed in both the old and new architecture.
    
    BUG=none
    
    Review-Url: https://codereview.chromium.org/2923963002
    Cr-Commit-Position: refs/heads/master@{#477870}
    
    Former-commit-id: aa2a07d8232fba92dadf65375d6327adce7a9a28

--- ios/shared/chrome/browser/ui/dialogs/nsurl_protection_space_util.h
+++ ios/shared/chrome/browser/ui/dialogs/nsurl_protection_space_util.h
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef IOS_CHROME_BROWSER_UI_DIALOGS_NSURL_PROTECTION_SPACE_UTIL_H_
-#define IOS_CHROME_BROWSER_UI_DIALOGS_NSURL_PROTECTION_SPACE_UTIL_H_
+#ifndef IOS_SHARED_CHROME_BROWSER_UI_DIALOGS_NSURL_PROTECTION_SPACE_UTIL_H_
+#define IOS_SHARED_CHROME_BROWSER_UI_DIALOGS_NSURL_PROTECTION_SPACE_UTIL_H_
 
 #import <Foundation/Foundation.h>
 
@@ -27,4 +27,4 @@ GURL RequesterOrigin(NSURLProtectionSpace* protectionSpace);
 }  // namespace nsurlprotectionspace_util
 }  // namespace ios_internal
 
-#endif  // IOS_CHROME_BROWSER_UI_DIALOGS_NSURL_PROTECTION_SPACE_UTIL_H_
+#endif  // IOS_SHARED_CHROME_BROWSER_UI_DIALOGS_NSURL_PROTECTION_SPACE_UTIL_H_
