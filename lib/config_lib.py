a50254e9fbaed9983e58ba854c588a02a310473e
Hidehiko Abe <hidehiko@chromium.org>
2017-06-09 16:27:01 +0900

    Enable goma for TestSimpleChrome step.
    BUG=chromium:279618
    TEST=Ran try.
    
    Change-Id: Iebac3fe06eb88444bd979fa44e7118ea8fd3ac8e
    Reviewed-on: https://chromium-review.googlesource.com/527779
    Commit-Ready: Hidehiko Abe <hidehiko@chromium.org>
    Tested-by: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Don Garrett <dgarrett@chromium.org>

--- lib/config_lib.py
+++ lib/config_lib.py
@@ -933,7 +933,7 @@ def DefaultSettings():
 
       # If chrome_sdk is set to True, this determines whether we use goma to
       # build chrome.
-      chrome_sdk_goma=False,
+      chrome_sdk_goma=True,
 
       # Run image tests. This should only be set if 'base' is in our list of
       # images.
