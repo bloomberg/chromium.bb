8f834bcf7d82c989a57dae18cca2807c980a55ee
Pavel Feldman <pfeldman@chromium.org>
2017-06-08 17:48:54 -0700

    DevTools: remove stray error messages while devtools on devtools (2)
    Change-Id: I6694d9fd998ceabf72ff81050fd577d0630c3c5c
    Reviewed-on: https://chromium-review.googlesource.com/528489
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478143}
    Former-commit-id: 19ba322284f000480a980cdb1a6452bdba9e07c3

--- chrome/browser/devtools/devtools_file_helper.cc
+++ chrome/browser/devtools/devtools_file_helper.cc
@@ -211,7 +211,6 @@ DevToolsFileHelper::DevToolsFileHelper(WebContents* web_contents,
       profile_(profile),
       delegate_(delegate),
       weak_factory_(this) {
-  pref_change_registrar_.Init(profile_->GetPrefs());
 }
 
 DevToolsFileHelper::~DevToolsFileHelper() {
@@ -389,6 +388,7 @@ DevToolsFileHelper::GetFileSystems() {
   if (!file_watcher_) {
     file_watcher_.reset(new DevToolsFileWatcher(base::Bind(
         &DevToolsFileHelper::FilePathsChanged, weak_factory_.GetWeakPtr())));
+    pref_change_registrar_.Init(profile_->GetPrefs());
     pref_change_registrar_.Add(
         prefs::kDevToolsFileSystemPaths,
         base::Bind(&DevToolsFileHelper::FileSystemPathsSettingChanged,
