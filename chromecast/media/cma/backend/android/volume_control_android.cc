d5f446849c1c30c73a2448f087895a736a2ab271
Chris Kuiper <ckuiper@google.com>
2017-06-09 22:12:27 -0700

    [Chromecast] Move VolumeControlAndroid class declaration into a header file.
    This is in preparation for tweaking the volume control to use
    Android's volume settings for type volume and muting.
    
    No logic changed, just code was moved and renamed.
    
    Bug: internal b/62846729
    Test: verified volume control worked as before
    
    Change-Id: Ide4cf53c3c680bdda7db7840d31b5ce732d5fc3f
    Reviewed-on: https://chromium-review.googlesource.com/544110
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Chris Kuiper <ckuiper@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481979}
    Former-commit-id: f144797cfdf6792d1d3f18a23cf7e626f2b3c1e5

--- chromecast/media/cma/backend/android/volume_control_android.cc
+++ chromecast/media/cma/backend/android/volume_control_android.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chromecast/public/volume_control.h"
+#include "chromecast/media/cma/backend/android/volume_control_android.h"
 
 #include <algorithm>
 #include <cmath>
@@ -50,16 +50,11 @@ constexpr char kKeyVolumeMap[] = "volume_map";
 constexpr char kKeyLevel[] = "level";
 constexpr char kKeyDb[] = "db";
 
-struct LevelToDb {
-  float level;
-  float db;
-};
-
-const LevelToDb kDefaultVolumeMap[] = {{0.0f, kMinDbFS},
-                                       {0.01f, -58.0f},
-                                       {0.090909f, -48.0f},
-                                       {0.818182f, -8.0f},
-                                       {1.0f, 0.0f}};
+const VolumeMap::LevelToDb kDefaultVolumeMap[] = {{0.0f, kMinDbFS},
+                                                  {0.01f, -58.0f},
+                                                  {0.090909f, -48.0f},
+                                                  {0.818182f, -8.0f},
+                                                  {1.0f, 0.0f}};
 
 float DbFsToScale(float db) {
   if (db <= kMinDbFS) {
@@ -79,272 +74,248 @@ std::string ContentTypeToDbFSKey(AudioContentType type) {
   }
 }
 
-// TODO(ckuiper): This logic is identical to the one in alsa/volume_control.cc.
-// Consolidate it.
-class VolumeMap {
- public:
-  VolumeMap() {
-    auto cast_audio_config =
-        DeserializeJsonFromFile(base::FilePath(kCastAudioConfigFilePath));
-    const base::DictionaryValue* cast_audio_dict;
-    if (!cast_audio_config ||
-        !cast_audio_config->GetAsDictionary(&cast_audio_dict)) {
-      LOG(WARNING) << "No cast audio config found; using default volume map.";
-      volume_map_.insert(volume_map_.end(), kDefaultVolumeMap,
-                         kDefaultVolumeMap + arraysize(kDefaultVolumeMap));
-      return;
-    }
-
-    const base::ListValue* volume_map_list;
-    if (!cast_audio_dict->GetList(kKeyVolumeMap, &volume_map_list)) {
-      LOG(WARNING) << "No volume map found; using default volume map.";
-      volume_map_.insert(volume_map_.end(), kDefaultVolumeMap,
-                         kDefaultVolumeMap + arraysize(kDefaultVolumeMap));
-      return;
-    }
-
-    double prev_level = -1.0;
-    for (size_t i = 0; i < volume_map_list->GetSize(); ++i) {
-      const base::DictionaryValue* volume_map_entry;
-      CHECK(volume_map_list->GetDictionary(i, &volume_map_entry));
-
-      double level;
-      CHECK(volume_map_entry->GetDouble(kKeyLevel, &level));
-      CHECK_GE(level, 0.0);
-      CHECK_LE(level, 1.0);
-      CHECK_GT(level, prev_level);
-      prev_level = level;
-
-      double db;
-      CHECK(volume_map_entry->GetDouble(kKeyDb, &db));
-      CHECK_LE(db, 0.0);
-      if (level == 1.0) {
-        CHECK_EQ(db, 0.0);
-      }
-
-      volume_map_.push_back({level, db});
-    }
-
-    if (volume_map_.empty()) {
-      LOG(FATAL) << "No entries in volume map.";
-      return;
-    }
-
-    if (volume_map_[0].level > 0.0) {
-      volume_map_.insert(volume_map_.begin(), {0.0, kMinDbFS});
-    }
-
-    if (volume_map_.rbegin()->level < 1.0) {
-      volume_map_.push_back({1.0, 0.0});
-    }
-  }
-
-  float VolumeToDbFS(float volume) {
-    if (volume <= volume_map_[0].level) {
-      return volume_map_[0].db;
-    }
-    for (size_t i = 1; i < volume_map_.size(); ++i) {
-      if (volume < volume_map_[i].level) {
-        const float x_range = volume_map_[i].level - volume_map_[i - 1].level;
-        const float y_range = volume_map_[i].db - volume_map_[i - 1].db;
-        const float x_pos = volume - volume_map_[i - 1].level;
-
-        return volume_map_[i - 1].db + x_pos * y_range / x_range;
-      }
-    }
-    return volume_map_[volume_map_.size() - 1].db;
-  }
-
-  // static
-  float DbFSToVolume(float db) {
-    if (db <= volume_map_[0].db) {
-      return volume_map_[0].level;
-    }
-    for (size_t i = 1; i < volume_map_.size(); ++i) {
-      if (db < volume_map_[i].db) {
-        const float x_range = volume_map_[i].db - volume_map_[i - 1].db;
-        const float y_range = volume_map_[i].level - volume_map_[i - 1].level;
-        const float x_pos = db - volume_map_[i - 1].db;
-
-        return volume_map_[i - 1].level + x_pos * y_range / x_range;
-      }
-    }
-    return volume_map_[volume_map_.size() - 1].level;
-  }
-
- private:
-  std::vector<LevelToDb> volume_map_;
-
-  DISALLOW_COPY_AND_ASSIGN(VolumeMap);
-};
-
 base::LazyInstance<VolumeMap>::Leaky g_volume_map = LAZY_INSTANCE_INITIALIZER;
 
-// TODO(ckuiper): This logic is similar to the one in alsa/volume_control.cc.
-// Consolidate it.
-class VolumeControlInternal {
- public:
-  VolumeControlInternal()
-      : thread_("VolumeControl"),
-        initialize_complete_event_(
-            base::WaitableEvent::ResetPolicy::MANUAL,
-            base::WaitableEvent::InitialState::NOT_SIGNALED) {
-    // Load volume map to check that the config file is correct.
-    g_volume_map.Get();
-
-    stored_values_.SetDouble(kKeyMediaDbFS, kDefaultMediaDbFS);
-    stored_values_.SetDouble(kKeyAlarmDbFS, kDefaultAlarmDbFS);
-    stored_values_.SetDouble(kKeyCommunicationDbFS, kDefaultCommunicationDbFS);
-
-    auto types = {AudioContentType::kMedia, AudioContentType::kAlarm,
-                  AudioContentType::kCommunication};
-    double volume;
-
-    storage_path_ = base::GetHomeDir().Append("saved_volumes");
-    auto old_stored_data = DeserializeJsonFromFile(storage_path_);
-    base::DictionaryValue* old_stored_dict;
-    if (old_stored_data && old_stored_data->GetAsDictionary(&old_stored_dict)) {
-      for (auto type : types) {
-        if (old_stored_dict->GetDouble(ContentTypeToDbFSKey(type), &volume)) {
-          stored_values_.SetDouble(ContentTypeToDbFSKey(type), volume);
-        }
-      }
-    }
-
-    base::Thread::Options options;
-    options.message_loop_type = base::MessageLoop::TYPE_IO;
-    thread_.StartWithOptions(options);
-
-    thread_.task_runner()->PostTask(
-        FROM_HERE, base::Bind(&VolumeControlInternal::InitializeOnThread,
-                              base::Unretained(this)));
-    initialize_complete_event_.Wait();
-  }
-
-  ~VolumeControlInternal() {}
-
-  void AddVolumeObserver(VolumeObserver* observer) {
-    base::AutoLock lock(observer_lock_);
-    volume_observers_.push_back(observer);
-  }
-
-  void RemoveVolumeObserver(VolumeObserver* observer) {
-    base::AutoLock lock(observer_lock_);
-    volume_observers_.erase(std::remove(volume_observers_.begin(),
-                                        volume_observers_.end(), observer),
-                            volume_observers_.end());
-  }
-
-  float GetVolume(AudioContentType type) {
-    base::AutoLock lock(volume_lock_);
-    return volumes_[type];
-  }
-
-  void SetVolume(AudioContentType type, float level) {
-    level = std::max(0.0f, std::min(level, 1.0f));
-    thread_.task_runner()->PostTask(
-        FROM_HERE, base::Bind(&VolumeControlInternal::SetVolumeOnThread,
-                              base::Unretained(this), type, level));
-  }
-
-  bool IsMuted(AudioContentType type) {
-    base::AutoLock lock(volume_lock_);
-    return muted_[type];
-  }
-
-  void SetMuted(AudioContentType type, bool muted) {
-    thread_.task_runner()->PostTask(
-        FROM_HERE, base::Bind(&VolumeControlInternal::SetMutedOnThread,
-                              base::Unretained(this), type, muted));
-  }
-
-  void SetOutputLimit(AudioContentType type, float limit) {
-    limit = std::max(0.0f, std::min(limit, 1.0f));
-    AudioSinkManager::Get()->SetOutputLimit(
-        type, DbFsToScale(VolumeControl::VolumeToDbFS(limit)));
-  }
-
- private:
-  void InitializeOnThread() {
-    DCHECK(thread_.task_runner()->BelongsToCurrentThread());
-
-    double dbfs;
-    for (auto type : {AudioContentType::kMedia, AudioContentType::kAlarm,
-                      AudioContentType::kCommunication}) {
-      CHECK(stored_values_.GetDouble(ContentTypeToDbFSKey(type), &dbfs));
-      volumes_[type] = VolumeControl::DbFSToVolume(dbfs);
-      AudioSinkManager::Get()->SetVolume(type, DbFsToScale(dbfs));
-
-      // Note that mute state is not persisted across reboots.
-      muted_[type] = false;
-    }
-    initialize_complete_event_.Signal();
-  }
-
-  void SetVolumeOnThread(AudioContentType type, float level) {
-    DCHECK(thread_.task_runner()->BelongsToCurrentThread());
-    {
-      base::AutoLock lock(volume_lock_);
-      if (level == volumes_[type]) {
-        return;
-      }
-      volumes_[type] = level;
-    }
-
-    float dbfs = VolumeControl::VolumeToDbFS(level);
-    AudioSinkManager::Get()->SetVolume(type, DbFsToScale(dbfs));
-
-    {
-      base::AutoLock lock(observer_lock_);
-      for (VolumeObserver* observer : volume_observers_) {
-        observer->OnVolumeChange(type, level);
-      }
-    }
-
-    stored_values_.SetDouble(ContentTypeToDbFSKey(type), dbfs);
-    SerializeJsonToFile(storage_path_, stored_values_);
-  }
-
-  void SetMutedOnThread(AudioContentType type, bool muted) {
-    DCHECK(thread_.task_runner()->BelongsToCurrentThread());
-    {
-      base::AutoLock lock(volume_lock_);
-      if (muted == muted_[type]) {
-        return;
-      }
-      muted_[type] = muted;
-    }
-
-    AudioSinkManager::Get()->SetMuted(type, muted);
-
-    {
-      base::AutoLock lock(observer_lock_);
-      for (VolumeObserver* observer : volume_observers_) {
-        observer->OnMuteChange(type, muted);
-      }
-    }
-  }
-
-  base::FilePath storage_path_;
-  base::DictionaryValue stored_values_;
-
-  base::Lock volume_lock_;
-  std::map<AudioContentType, float> volumes_;
-  std::map<AudioContentType, bool> muted_;
-
-  base::Lock observer_lock_;
-  std::vector<VolumeObserver*> volume_observers_;
-
-  base::Thread thread_;
-  base::WaitableEvent initialize_complete_event_;
-
-  DISALLOW_COPY_AND_ASSIGN(VolumeControlInternal);
-};
-
-base::LazyInstance<VolumeControlInternal>::Leaky g_volume_control =
+base::LazyInstance<VolumeControlAndroid>::Leaky g_volume_control =
     LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
+
+VolumeMap::VolumeMap() {
+  auto cast_audio_config =
+      DeserializeJsonFromFile(base::FilePath(kCastAudioConfigFilePath));
+  const base::DictionaryValue* cast_audio_dict;
+  if (!cast_audio_config ||
+      !cast_audio_config->GetAsDictionary(&cast_audio_dict)) {
+    LOG(WARNING) << "No cast audio config found; using default volume map.";
+    volume_map_.insert(volume_map_.end(), kDefaultVolumeMap,
+                       kDefaultVolumeMap + arraysize(kDefaultVolumeMap));
+    return;
+  }
+
+  const base::ListValue* volume_map_list;
+  if (!cast_audio_dict->GetList(kKeyVolumeMap, &volume_map_list)) {
+    LOG(WARNING) << "No volume map found; using default volume map.";
+    volume_map_.insert(volume_map_.end(), kDefaultVolumeMap,
+                       kDefaultVolumeMap + arraysize(kDefaultVolumeMap));
+    return;
+  }
+
+  double prev_level = -1.0;
+  for (size_t i = 0; i < volume_map_list->GetSize(); ++i) {
+    const base::DictionaryValue* volume_map_entry;
+    CHECK(volume_map_list->GetDictionary(i, &volume_map_entry));
+
+    double level;
+    CHECK(volume_map_entry->GetDouble(kKeyLevel, &level));
+    CHECK_GE(level, 0.0);
+    CHECK_LE(level, 1.0);
+    CHECK_GT(level, prev_level);
+    prev_level = level;
+
+    double db;
+    CHECK(volume_map_entry->GetDouble(kKeyDb, &db));
+    CHECK_LE(db, 0.0);
+    if (level == 1.0) {
+      CHECK_EQ(db, 0.0);
+    }
+
+    volume_map_.push_back({level, db});
+  }
+
+  if (volume_map_.empty()) {
+    LOG(FATAL) << "No entries in volume map.";
+    return;
+  }
+
+  if (volume_map_[0].level > 0.0) {
+    volume_map_.insert(volume_map_.begin(), {0.0, kMinDbFS});
+  }
+
+  if (volume_map_.rbegin()->level < 1.0) {
+    volume_map_.push_back({1.0, 0.0});
+  }
+}
+
+VolumeMap::~VolumeMap() {}
+
+float VolumeMap::VolumeToDbFS(float volume) {
+  if (volume <= volume_map_[0].level) {
+    return volume_map_[0].db;
+  }
+  for (size_t i = 1; i < volume_map_.size(); ++i) {
+    if (volume < volume_map_[i].level) {
+      const float x_range = volume_map_[i].level - volume_map_[i - 1].level;
+      const float y_range = volume_map_[i].db - volume_map_[i - 1].db;
+      const float x_pos = volume - volume_map_[i - 1].level;
+
+      return volume_map_[i - 1].db + x_pos * y_range / x_range;
+    }
+  }
+  return volume_map_[volume_map_.size() - 1].db;
+}
+
+float VolumeMap::DbFSToVolume(float db) {
+  if (db <= volume_map_[0].db) {
+    return volume_map_[0].level;
+  }
+  for (size_t i = 1; i < volume_map_.size(); ++i) {
+    if (db < volume_map_[i].db) {
+      const float x_range = volume_map_[i].db - volume_map_[i - 1].db;
+      const float y_range = volume_map_[i].level - volume_map_[i - 1].level;
+      const float x_pos = db - volume_map_[i - 1].db;
+
+      return volume_map_[i - 1].level + x_pos * y_range / x_range;
+    }
+  }
+  return volume_map_[volume_map_.size() - 1].level;
+}
+
+VolumeControlAndroid::VolumeControlAndroid()
+    : thread_("VolumeControl"),
+      initialize_complete_event_(
+          base::WaitableEvent::ResetPolicy::MANUAL,
+          base::WaitableEvent::InitialState::NOT_SIGNALED) {
+  // Load volume map to check that the config file is correct.
+  g_volume_map.Get();
+
+  stored_values_.SetDouble(kKeyMediaDbFS, kDefaultMediaDbFS);
+  stored_values_.SetDouble(kKeyAlarmDbFS, kDefaultAlarmDbFS);
+  stored_values_.SetDouble(kKeyCommunicationDbFS, kDefaultCommunicationDbFS);
+
+  auto types = {AudioContentType::kMedia, AudioContentType::kAlarm,
+                AudioContentType::kCommunication};
+  double volume;
+
+  storage_path_ = base::GetHomeDir().Append("saved_volumes");
+  auto old_stored_data = DeserializeJsonFromFile(storage_path_);
+  base::DictionaryValue* old_stored_dict;
+  if (old_stored_data && old_stored_data->GetAsDictionary(&old_stored_dict)) {
+    for (auto type : types) {
+      if (old_stored_dict->GetDouble(ContentTypeToDbFSKey(type), &volume)) {
+        stored_values_.SetDouble(ContentTypeToDbFSKey(type), volume);
+      }
+    }
+  }
+
+  base::Thread::Options options;
+  options.message_loop_type = base::MessageLoop::TYPE_IO;
+  thread_.StartWithOptions(options);
+
+  thread_.task_runner()->PostTask(
+      FROM_HERE, base::Bind(&VolumeControlAndroid::InitializeOnThread,
+                            base::Unretained(this)));
+  initialize_complete_event_.Wait();
+}
+
+VolumeControlAndroid::~VolumeControlAndroid() {}
+
+void VolumeControlAndroid::AddVolumeObserver(VolumeObserver* observer) {
+  base::AutoLock lock(observer_lock_);
+  volume_observers_.push_back(observer);
+}
+
+void VolumeControlAndroid::RemoveVolumeObserver(VolumeObserver* observer) {
+  base::AutoLock lock(observer_lock_);
+  volume_observers_.erase(
+      std::remove(volume_observers_.begin(), volume_observers_.end(), observer),
+      volume_observers_.end());
+}
+
+float VolumeControlAndroid::GetVolume(AudioContentType type) {
+  base::AutoLock lock(volume_lock_);
+  return volumes_[type];
+}
+
+void VolumeControlAndroid::SetVolume(AudioContentType type, float level) {
+  level = std::max(0.0f, std::min(level, 1.0f));
+  thread_.task_runner()->PostTask(
+      FROM_HERE, base::Bind(&VolumeControlAndroid::SetVolumeOnThread,
+                            base::Unretained(this), type, level));
+}
+
+bool VolumeControlAndroid::IsMuted(AudioContentType type) {
+  base::AutoLock lock(volume_lock_);
+  return muted_[type];
+}
+
+void VolumeControlAndroid::SetMuted(AudioContentType type, bool muted) {
+  thread_.task_runner()->PostTask(
+      FROM_HERE, base::Bind(&VolumeControlAndroid::SetMutedOnThread,
+                            base::Unretained(this), type, muted));
+}
+
+void VolumeControlAndroid::SetOutputLimit(AudioContentType type, float limit) {
+  limit = std::max(0.0f, std::min(limit, 1.0f));
+  AudioSinkManager::Get()->SetOutputLimit(
+      type, DbFsToScale(VolumeControl::VolumeToDbFS(limit)));
+}
+
+void VolumeControlAndroid::InitializeOnThread() {
+  DCHECK(thread_.task_runner()->BelongsToCurrentThread());
+
+  double dbfs;
+  for (auto type : {AudioContentType::kMedia, AudioContentType::kAlarm,
+                    AudioContentType::kCommunication}) {
+    CHECK(stored_values_.GetDouble(ContentTypeToDbFSKey(type), &dbfs));
+    volumes_[type] = VolumeControl::DbFSToVolume(dbfs);
+    AudioSinkManager::Get()->SetVolume(type, DbFsToScale(dbfs));
+
+    // Note that mute state is not persisted across reboots.
+    muted_[type] = false;
+  }
+  initialize_complete_event_.Signal();
+}
+
+void VolumeControlAndroid::SetVolumeOnThread(AudioContentType type,
+                                             float level) {
+  DCHECK(thread_.task_runner()->BelongsToCurrentThread());
+  {
+    base::AutoLock lock(volume_lock_);
+    if (level == volumes_[type]) {
+      return;
+    }
+    volumes_[type] = level;
+  }
+
+  float dbfs = VolumeControl::VolumeToDbFS(level);
+  LOG(INFO) << __func__ << ": level=" << level << " -> dbfs=" << dbfs;
+  AudioSinkManager::Get()->SetVolume(type, DbFsToScale(dbfs));
+
+  {
+    base::AutoLock lock(observer_lock_);
+    for (VolumeObserver* observer : volume_observers_) {
+      observer->OnVolumeChange(type, level);
+    }
+  }
+
+  stored_values_.SetDouble(ContentTypeToDbFSKey(type), dbfs);
+  SerializeJsonToFile(storage_path_, stored_values_);
+}
+
+void VolumeControlAndroid::SetMutedOnThread(AudioContentType type, bool muted) {
+  DCHECK(thread_.task_runner()->BelongsToCurrentThread());
+  {
+    base::AutoLock lock(volume_lock_);
+    if (muted == muted_[type]) {
+      return;
+    }
+    muted_[type] = muted;
+  }
+
+  AudioSinkManager::Get()->SetMuted(type, muted);
+
+  {
+    base::AutoLock lock(observer_lock_);
+    for (VolumeObserver* observer : volume_observers_) {
+      observer->OnMuteChange(type, muted);
+    }
+  }
+}
+
+//
+// Implementation of VolumeControl as defined in public/volume_control.h
+//
 
 // static
 void VolumeControl::Initialize(const std::vector<std::string>& argv) {
