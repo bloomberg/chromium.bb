34af7f798a57ab0b7c52d08ea7bf42f83dd3694e
Samuel Huang <huangs@chromium.org>
2017-06-12 14:44:13 +0000

    Revert "Generate experimental domain commands, fix method case and stray whitespace."
    This reverts commit c278a00bb65329f822adf76e765e7b57e143e5ba [formerly 0a8c2fcd5f0ca9236744f956610f8e0ad5d7f3c7].
    
    Reason for revert: Prime suspect for Linux Test failure for {headless_browsertests, browser_side_navigation_headless_browsertests} in:
    
    SimpleEventJsBindingsTest.RunAsyncTest
    ExperimentalCommandJsBindingsTest.RunAsyncTest
    
    
    Original change's description:
    > Generate experimental domain commands, fix method case and stray whitespace.
    > 
    > An experimental class is now generated as well as the regular one, and is linked within 
    > the main class.  Just like in the C++ bindings the experimental class contains everything.
    > The class methods generated now start with a lowercase letter (same as in the protocol).
    > Some stray white space has been removed from the template.
    > 
    > Bug: 
    > Change-Id: I4e0d8e0f04990e9f635cab71fea18c1f21111754
    > Reviewed-on: https://chromium-review.googlesource.com/529070
    > Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    > Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#478601}
    
    TBR=skyostil@chromium.org,alexclarke@chromium.org
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 
    Change-Id: I21a4e119fd4f0b568789c72608553f530a88adc5
    Reviewed-on: https://chromium-review.googlesource.com/531304
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#478617}
    Former-commit-id: 804e16e2b137546476881afd0c3034582072ddcd

--- headless/test/bindings_test.js
+++ headless/test/bindings_test.js
@@ -8,7 +8,6 @@ goog.module.declareLegacyNamespace();
 
 const Connection = goog.require('chromium.DevTools.Connection');
 const DOM = goog.require('chromium.DevTools.DOM');
-const Page = goog.require('chromium.DevTools.Page');
 const Runtime = goog.require('chromium.DevTools.Runtime');
 
 /**
@@ -23,7 +22,7 @@ class BindingsTest {
   evalOneAddOne() {
     let connection = new Connection(window.TabSocket);
     let runtime = new Runtime(connection);
-    runtime.evaluate({'expression': '1+1'}).then(function(message) {
+    runtime.Evaluate({'expression': '1+1'}).then(function(message) {
       connection.sendDevToolsMessage(
           '__Result',
           {'result': JSON.stringify(message.result.value)});
@@ -36,36 +35,16 @@ class BindingsTest {
   listenForChildNodeCountUpdated() {
     let connection = new Connection(window.TabSocket);
     let dom = new DOM(connection);
-    dom.onChildNodeCountUpdated(function(params) {
+    dom.OnChildNodeCountUpdated(function(params) {
       connection.sendDevToolsMessage('__Result',
                                      {'result': JSON.stringify(params)});
     });
-    dom.enable().then(function() {
-      return dom.getDocument({});
+    dom.Enable().then(function() {
+      return dom.GetDocument({});
     }).then(function() {
       // Create a new div which should trigger the event.
       let div = document.createElement('div');
       document.body.appendChild(div);
-    });
-  }
-
-  /**
-   * Uses an experimental command to obtain the resource tree.
-   */
-  getResourceTreeUrls() {
-    let connection = new Connection(window.TabSocket);
-    let page = new Page(connection);
-    page.experimental.getResourceTree().then(function(result) {
-      var urls = [];
-      for (let i = 0; i < result.frameTree.resources.length; i++) {
-        // Remove the host and port which is random.
-        urls.push(
-            result.frameTree.resources[i].url.substring(
-                result.frameTree.resources[i].url.lastIndexOf('/')));
-      }
-      urls.sort();
-      connection.sendDevToolsMessage(
-          '__Result', {'result': JSON.stringify(urls)});
     });
   }
 }
