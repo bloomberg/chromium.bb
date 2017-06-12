ea85e304bf49b44d21f3800f3f1527d24e69aae3
hbos <hbos@chromium.org>
2017-06-12 03:17:09 -0700

    WebRtcMediaStreamTrackMap added.
    This maps id <-> WebRtcMediaStreamTrackAdapter and handles creating
    track adapters if they don't already exist. Reference counting
    ensures adapters exists for as long as they are referenced, and that
    they are disposed and removed from the map when no longer needed.
    
    This can in a follow-up be used by WebRtcMediaStreamAdapter ("local
    streams") and RemoteMediaStreamImpl ("remote streams") to get
    references to track adapters "owned" by the map instead of directly
    creating and owning the sinks/adapters that a
    WebRtcMediaStreamTrackAdapter consists of.
    
    This would allow local and remote streams to reference tracks that
    already exist. The map can also be used to create and own tracks that
    are not associated with any streams.
    
    BUG=705901, 700916
    
    Review-Url: https://codereview.chromium.org/2887403003
    Cr-Commit-Position: refs/heads/master@{#478580}
    
    Former-commit-id: ebab8e6156335da0a2f87cfaea1ad8da7f5e47e1

---
+++ content/renderer/media/webrtc/webrtc_media_stream_track_adapter_map.h
@@ -0,0 +1,145 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_RENDERER_MEDIA_WEBRTC_WEBRTC_MEDIA_STREAM_TRACK_COLLECTION_H_
+#define CONTENT_RENDERER_MEDIA_WEBRTC_WEBRTC_MEDIA_STREAM_TRACK_COLLECTION_H_
+
+#include <map>
+#include <string>
+
+#include "base/memory/ref_counted.h"
+#include "base/single_thread_task_runner.h"
+#include "content/common/content_export.h"
+#include "content/renderer/media/webrtc/webrtc_media_stream_track_adapter.h"
+#include "third_party/WebKit/public/platform/WebMediaStreamTrack.h"
+#include "third_party/webrtc/api/mediastreaminterface.h"
+
+namespace content {
+
+class PeerConnectionDependencyFactory;
+
+// A map and owner of |WebRtcMediaStreamTrackAdapter|s. It takes care of
+// creating, initializing and disposing track adapters independently of media
+// streams. Adapters are accessed via |AdapterRef|s, when all references to an
+// adapter are destroyed it is disposed and removed from the map.
+class CONTENT_EXPORT WebRtcMediaStreamTrackAdapterMap
+    : public base::RefCountedThreadSafe<WebRtcMediaStreamTrackAdapterMap> {
+ private:
+  // The map's entries are reference counted in order to |Dispose| the adapter
+  // when all |AdapterRef|s referencing an entry are destroyed.
+  // Private section needed here due to |AdapterRef|'s usage of |AdapterEntry|.
+  struct AdapterEntry {
+    AdapterEntry(const scoped_refptr<WebRtcMediaStreamTrackAdapter>& adapter);
+    AdapterEntry(AdapterEntry&& other);
+    ~AdapterEntry();
+
+    AdapterEntry(const AdapterEntry&) = delete;
+    AdapterEntry& operator=(const AdapterEntry&) = delete;
+
+    scoped_refptr<WebRtcMediaStreamTrackAdapter> adapter;
+  };
+
+ public:
+  // Acts as an accessor to adapter members without leaking a reference to the
+  // adapter. When the last |AdapterRef| is destroyed, the corresponding adapter
+  // is |Dispose|d and removed from the map.
+  class CONTENT_EXPORT AdapterRef {
+   public:
+    // Must be invoked on the main thread. If this was the last reference to the
+    // adapter it will be disposed and removed from the map.
+    ~AdapterRef();
+
+    bool is_initialized() const { return adapter_->is_initialized(); }
+    const blink::WebMediaStreamTrack& web_track() const {
+      return adapter_->web_track();
+    }
+    webrtc::MediaStreamTrackInterface* webrtc_track() const {
+      return adapter_->webrtc_track();
+    }
+
+    // Warning: Holding an external reference to the adapter will prevent
+    // |~AdapterRef| from disposing the adapter.
+    WebRtcMediaStreamTrackAdapter* GetAdapterForTesting() const {
+      return adapter_.get();
+    }
+
+   private:
+    friend class WebRtcMediaStreamTrackAdapterMap;
+
+    using MapEntryIterator = std::map<std::string, AdapterEntry>::iterator;
+    enum class Type { kLocal, kRemote };
+
+    // Increments the |AdapterEntry::ref_count|. Assumes map's |lock_| is held.
+    AdapterRef(const scoped_refptr<WebRtcMediaStreamTrackAdapterMap>& map,
+               Type type,
+               const MapEntryIterator& it);
+
+    AdapterEntry* entry() { return &it_->second; }
+
+    scoped_refptr<WebRtcMediaStreamTrackAdapterMap> map_;
+    Type type_;
+    MapEntryIterator it_;
+    // A reference to the entry's adapter, ensures that |HasOneRef| is false.
+    scoped_refptr<WebRtcMediaStreamTrackAdapter> adapter_;
+  };
+
+  WebRtcMediaStreamTrackAdapterMap(
+      PeerConnectionDependencyFactory* const factory,
+      const scoped_refptr<base::SingleThreadTaskRunner>& main_thread);
+
+  // Gets the new reference to the local track adapter by ID, or null if no such
+  // adapter was found. When all references are destroyed the adapter is
+  // disposed and removed from the map. This method can be called from any
+  // thread, but references must be destroyed on the main thread.
+  std::unique_ptr<AdapterRef> GetLocalTrackAdapter(const std::string& id);
+  // Invoke on the main thread. Gets a new reference to the local track adapter
+  // for the web track. If no adapter exists for the track one is created and
+  // initialized. When all references are destroyed the adapter is disposed and
+  // removed from the map. References must be destroyed on the main thread.
+  std::unique_ptr<AdapterRef> GetOrCreateLocalTrackAdapter(
+      const blink::WebMediaStreamTrack& web_track);
+  size_t GetLocalTrackCount() const;
+
+  // Gets the new reference to the remote track adapter by ID, or null if no
+  // such adapter was found. When all references are destroyed the adapter is
+  // disposed and removed from the map. This method can be called from any
+  // thread, but references must be destroyed on the main thread.
+  std::unique_ptr<AdapterRef> GetRemoteTrackAdapter(const std::string& id);
+  // Invoke on the webrtc signaling thread. Gets a new reference to the remote
+  // track adapter for the webrtc track. If no adapter exists for the track one
+  // is created and initialization completes on the main thread in a post. When
+  // all references are destroyed the adapter is disposed and removed from the
+  // map. References must be destroyed on the main thread.
+  std::unique_ptr<AdapterRef> GetOrCreateRemoteTrackAdapter(
+      webrtc::MediaStreamTrackInterface* webrtc_track);
+  size_t GetRemoteTrackCount() const;
+
+ protected:
+  friend class base::RefCountedThreadSafe<WebRtcMediaStreamTrackAdapterMap>;
+
+  // Invoke on the main thread.
+  virtual ~WebRtcMediaStreamTrackAdapterMap();
+
+ private:
+  std::unique_ptr<AdapterRef> GetTrackAdapter(AdapterRef::Type type,
+                                              const std::string& id);
+  std::unique_ptr<AdapterRef> GetOrCreateTrackAdapter(
+      AdapterRef::Type type,
+      base::Callback<scoped_refptr<WebRtcMediaStreamTrackAdapter>()>
+          create_adapter_callback,
+      const std::string& id);
+
+  // Pointer to a |PeerConnectionDependencyFactory| owned by the |RenderThread|.
+  // It's valid for the lifetime of |RenderThread|.
+  PeerConnectionDependencyFactory* const factory_;
+  scoped_refptr<base::SingleThreadTaskRunner> main_thread_;
+
+  mutable base::Lock lock_;
+  std::map<std::string, AdapterEntry> local_track_adapters_;
+  std::map<std::string, AdapterEntry> remote_track_adapters_;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_RENDERER_MEDIA_WEBRTC_WEBRTC_MEDIA_STREAM_TRACK_COLLECTION_H_
---
+++ content/renderer/media/webrtc/webrtc_media_stream_track_adapter_map_unittest.cc
@@ -0,0 +1,210 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/renderer/media/webrtc/webrtc_media_stream_track_adapter_map.h"
+
+#include <memory>
+
+#include "base/memory/ref_counted.h"
+#include "base/message_loop/message_loop.h"
+#include "base/run_loop.h"
+#include "base/single_thread_task_runner.h"
+#include "content/child/child_process.h"
+#include "content/renderer/media/media_stream_audio_source.h"
+#include "content/renderer/media/webrtc/mock_peer_connection_dependency_factory.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/WebKit/public/platform/WebMediaStreamSource.h"
+#include "third_party/WebKit/public/platform/WebMediaStreamTrack.h"
+#include "third_party/WebKit/public/platform/WebString.h"
+#include "third_party/WebKit/public/web/WebHeap.h"
+
+namespace content {
+
+class WebRtcMediaStreamTrackAdapterMapTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    dependency_factory_.reset(new MockPeerConnectionDependencyFactory());
+    main_thread_ = base::ThreadTaskRunnerHandle::Get();
+    map_ = new WebRtcMediaStreamTrackAdapterMap(dependency_factory_.get(),
+                                                main_thread_);
+  }
+
+  void TearDown() override { blink::WebHeap::CollectAllGarbageForTesting(); }
+
+  blink::WebMediaStreamTrack CreateLocalTrack(const std::string& id) {
+    blink::WebMediaStreamSource web_source;
+    web_source.Initialize(
+        blink::WebString::FromUTF8(id), blink::WebMediaStreamSource::kTypeAudio,
+        blink::WebString::FromUTF8("local_audio_track"), false);
+    MediaStreamAudioSource* audio_source = new MediaStreamAudioSource(true);
+    // Takes ownership of |audio_source|.
+    web_source.SetExtraData(audio_source);
+
+    blink::WebMediaStreamTrack web_track;
+    web_track.Initialize(web_source.Id(), web_source);
+    audio_source->ConnectToTrack(web_track);
+    return web_track;
+  }
+
+  std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef>
+  GetOrCreateRemoteTrackAdapter(
+      webrtc::MediaStreamTrackInterface* webrtc_track) {
+    DCHECK(main_thread_->BelongsToCurrentThread());
+    std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> adapter;
+    dependency_factory_->GetWebRtcSignalingThread()->PostTask(
+        FROM_HERE,
+        base::Bind(&WebRtcMediaStreamTrackAdapterMapTest::
+                       GetOrCreateRemoteTrackAdapterOnSignalingThread,
+                   base::Unretained(this), webrtc_track, &adapter));
+    RunMessageLoopsUntilIdle();
+    return adapter;
+  }
+
+  void GetOrCreateRemoteTrackAdapterOnSignalingThread(
+      webrtc::MediaStreamTrackInterface* webrtc_track,
+      std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef>* adapter) {
+    DCHECK(dependency_factory_->GetWebRtcSignalingThread()
+               ->BelongsToCurrentThread());
+    *adapter = map_->GetOrCreateRemoteTrackAdapter(webrtc_track);
+  }
+
+  // Runs message loops on the webrtc signaling thread and the main thread until
+  // idle.
+  void RunMessageLoopsUntilIdle() {
+    DCHECK(main_thread_->BelongsToCurrentThread());
+    base::WaitableEvent waitable_event(
+        base::WaitableEvent::ResetPolicy::MANUAL,
+        base::WaitableEvent::InitialState::NOT_SIGNALED);
+    dependency_factory_->GetWebRtcSignalingThread()->PostTask(
+        FROM_HERE, base::Bind(&WebRtcMediaStreamTrackAdapterMapTest::
+                                  RunMessageLoopUntilIdleOnSignalingThread,
+                              base::Unretained(this), &waitable_event));
+    waitable_event.Wait();
+    base::RunLoop().RunUntilIdle();
+  }
+
+  void RunMessageLoopUntilIdleOnSignalingThread(
+      base::WaitableEvent* waitable_event) {
+    DCHECK(dependency_factory_->GetWebRtcSignalingThread()
+               ->BelongsToCurrentThread());
+    base::RunLoop().RunUntilIdle();
+    waitable_event->Signal();
+  }
+
+ protected:
+  // Message loop and child processes is needed for task queues and threading to
+  // work, as is necessary to create tracks and adapters.
+  base::MessageLoop message_loop_;
+  ChildProcess child_process_;
+
+  std::unique_ptr<MockPeerConnectionDependencyFactory> dependency_factory_;
+  scoped_refptr<base::SingleThreadTaskRunner> main_thread_;
+  scoped_refptr<WebRtcMediaStreamTrackAdapterMap> map_;
+};
+
+TEST_F(WebRtcMediaStreamTrackAdapterMapTest, AddAndRemoveLocalTrackAdapter) {
+  blink::WebMediaStreamTrack web_track = CreateLocalTrack("local_track");
+  std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> adapter_ref =
+      map_->GetOrCreateLocalTrackAdapter(web_track);
+  EXPECT_TRUE(adapter_ref->is_initialized());
+  EXPECT_EQ(adapter_ref->GetAdapterForTesting(),
+            map_->GetLocalTrackAdapter(web_track.Id().Utf8())
+                ->GetAdapterForTesting());
+  EXPECT_EQ(1u, map_->GetLocalTrackCount());
+
+  // "GetOrCreate" for already existing track.
+  std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> adapter_ref2 =
+      map_->GetOrCreateLocalTrackAdapter(web_track);
+  EXPECT_EQ(adapter_ref->GetAdapterForTesting(),
+            adapter_ref2->GetAdapterForTesting());
+  EXPECT_EQ(1u, map_->GetLocalTrackCount());
+
+  adapter_ref2.reset();  // Not the last reference.
+  EXPECT_TRUE(adapter_ref->GetAdapterForTesting()->is_initialized());
+  EXPECT_EQ(1u, map_->GetLocalTrackCount());
+
+  // Destroying all references to the adapter should remove it from the map and
+  // dispose it.
+  adapter_ref.reset();
+  EXPECT_EQ(0u, map_->GetLocalTrackCount());
+  EXPECT_EQ(nullptr, map_->GetLocalTrackAdapter(web_track.Id().Utf8()));
+  // Allow the disposing of track to occur.
+  RunMessageLoopsUntilIdle();
+}
+
+TEST_F(WebRtcMediaStreamTrackAdapterMapTest, AddAndRemoveRemoteTrackAdapter) {
+  scoped_refptr<MockWebRtcAudioTrack> webrtc_track =
+      MockWebRtcAudioTrack::Create("remote_track");
+  std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> adapter_ref =
+      GetOrCreateRemoteTrackAdapter(webrtc_track.get());
+  EXPECT_TRUE(adapter_ref->is_initialized());
+  EXPECT_EQ(
+      adapter_ref->GetAdapterForTesting(),
+      map_->GetRemoteTrackAdapter(webrtc_track->id())->GetAdapterForTesting());
+  EXPECT_EQ(1u, map_->GetRemoteTrackCount());
+
+  // "GetOrCreate" for already existing track.
+  std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> adapter_ref2 =
+      GetOrCreateRemoteTrackAdapter(webrtc_track.get());
+  EXPECT_EQ(adapter_ref->GetAdapterForTesting(),
+            adapter_ref2->GetAdapterForTesting());
+  EXPECT_EQ(1u, map_->GetRemoteTrackCount());
+
+  adapter_ref2.reset();  // Not the last reference.
+  EXPECT_TRUE(adapter_ref->GetAdapterForTesting()->is_initialized());
+  EXPECT_EQ(1u, map_->GetRemoteTrackCount());
+
+  // Destroying all references to the adapter should remove it from the map and
+  // dispose it.
+  adapter_ref.reset();
+  EXPECT_EQ(0u, map_->GetRemoteTrackCount());
+  EXPECT_EQ(nullptr, map_->GetRemoteTrackAdapter(webrtc_track->id()));
+  // Allow the disposing of track to occur.
+  RunMessageLoopsUntilIdle();
+}
+
+TEST_F(WebRtcMediaStreamTrackAdapterMapTest,
+       LocalAndRemoteTrackAdaptersWithSameID) {
+  // Local and remote tracks should be able to use the same id without conflict.
+  const char* id = "id";
+
+  blink::WebMediaStreamTrack local_web_track = CreateLocalTrack(id);
+  std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> local_adapter =
+      map_->GetOrCreateLocalTrackAdapter(local_web_track);
+  EXPECT_TRUE(local_adapter->is_initialized());
+  EXPECT_EQ(local_adapter->GetAdapterForTesting(),
+            map_->GetLocalTrackAdapter(id)->GetAdapterForTesting());
+  EXPECT_EQ(1u, map_->GetLocalTrackCount());
+
+  scoped_refptr<MockWebRtcAudioTrack> remote_webrtc_track =
+      MockWebRtcAudioTrack::Create(id);
+  std::unique_ptr<WebRtcMediaStreamTrackAdapterMap::AdapterRef> remote_adapter =
+      GetOrCreateRemoteTrackAdapter(remote_webrtc_track.get());
+  EXPECT_TRUE(remote_adapter->is_initialized());
+  EXPECT_EQ(remote_adapter->GetAdapterForTesting(),
+            map_->GetRemoteTrackAdapter(id)->GetAdapterForTesting());
+  EXPECT_NE(local_adapter->GetAdapterForTesting(),
+            remote_adapter->GetAdapterForTesting());
+  EXPECT_EQ(1u, map_->GetRemoteTrackCount());
+
+  // Destroying all references to the adapters should remove them from the map.
+  local_adapter.reset();
+  remote_adapter.reset();
+  EXPECT_EQ(0u, map_->GetLocalTrackCount());
+  EXPECT_EQ(0u, map_->GetRemoteTrackCount());
+  EXPECT_EQ(nullptr, map_->GetLocalTrackAdapter(id));
+  EXPECT_EQ(nullptr, map_->GetRemoteTrackAdapter(id));
+  // Allow the disposing of tracks to occur.
+  RunMessageLoopsUntilIdle();
+}
+
+TEST_F(WebRtcMediaStreamTrackAdapterMapTest, GetLocalTrackAdapterInvalidID) {
+  EXPECT_EQ(nullptr, map_->GetLocalTrackAdapter("invalid"));
+}
+
+TEST_F(WebRtcMediaStreamTrackAdapterMapTest, GetRemoteTrackAdapterInvalidID) {
+  EXPECT_EQ(nullptr, map_->GetRemoteTrackAdapter("invalid"));
+}
+
+}  // namespace content
