91b635f0acd0753da3a4892b9bc7e30e8d6df084
kkhorimoto <kkhorimoto@chromium.org>
2017-06-12 12:56:31 -0700

    Cancel dialogs for Tabs when they are removed from their TabModels.
    A WebState attempts to cancel its dialogs in its destructor.  However,
    the BVC uninstalls itself as the WebStateDelegate before the WebState
    is deallocated, so the WebStateImpl doesn't have access to its
    JavaScriptDialogPresenter to handle the cancellation.
    
    This CL adds a call to |-cancelDialogForWebState:| when the delegates
    are uninstalled for a Tab to ensure that alerts aren't queued in the
    DialogPresenter for WebStates that have been deallocated.
    
    BUG=720778
    
    Review-Url: https://codereview.chromium.org/2926413004
    Cr-Commit-Position: refs/heads/master@{#478729}
    
    Former-commit-id: 496fdd75e503c044babb47801e772c7255da94fa

--- ios/chrome/browser/ui/browser_view_controller.mm
+++ ios/chrome/browser/ui/browser_view_controller.mm
@@ -4683,6 +4683,9 @@ class BrowserBookmarkModelBridge : public bookmarks::BookmarkModelObserver {
          atIndex:(NSUInteger)index {
   [self uninstallDelegatesForTab:tab];
 
+  // Cancel dialogs for |tab|'s WebState.
+  [self.dialogPresenter cancelDialogForWebState:tab.webState];
+
   // Remove stored native controllers for the tab.
   [_nativeControllersForTabIDs removeObjectForKey:tab.tabId];
 
