86b71185700d440164e67b16112f654bce53beb2
shivanisha <shivanisha@chromium.org>
2017-06-12 10:06:35 -0700

    Changes subresource filtering icon in the site settings for Android.
    Screenshot attached in the bug.
    
    BUG=689992
    
    Review-Url: https://codereview.chromium.org/2930753002
    Cr-Commit-Position: refs/heads/master@{#478666}
    
    Former-commit-id: d442e58e5f8625d8baa062a9d43f8391b1c0927e

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/website/ContentSettingsResources.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/website/ContentSettingsResources.java
@@ -148,9 +148,8 @@ public class ContentSettingsResources {
                                  org.chromium.chrome.R.string.protected_content,
                                  org.chromium.chrome.R.string.protected_content,
                                  ContentSetting.ASK, ContentSetting.BLOCK, 0, 0));
-            // TODO(csharrison): Swap out the placeholder icon once it is ready.
             localMap.put(ContentSettingsType.CONTENT_SETTINGS_TYPE_SUBRESOURCE_FILTER,
-                    new ResourceItem(R.drawable.permission_popups,
+                    new ResourceItem(R.drawable.ic_ad_24dp_grey600,
                             R.string.subresource_filter_permission_title,
                             R.string.subresource_filter_permission_title, ContentSetting.ALLOW,
                             ContentSetting.BLOCK, 0, 0));
