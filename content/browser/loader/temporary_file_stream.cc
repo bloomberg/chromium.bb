3dab261d8b753b3c88cf0d37b2e575a807a046f7
sebmarchand <sebmarchand@chromium.org>
2017-06-12 09:24:05 -0700

    Switch temporary_file_stream to the TaskScheduler API.
    BUG=689520
    
    Review-Url: https://codereview.chromium.org/2888743002
    Cr-Commit-Position: refs/heads/master@{#478652}
    
    Former-commit-id: 4ee99e970e98366c707549fa52a1e2c4ac6b3d4d

--- content/browser/loader/temporary_file_stream.cc
+++ content/browser/loader/temporary_file_stream.cc
@@ -10,6 +10,7 @@
 #include "base/callback.h"
 #include "base/files/file_proxy.h"
 #include "base/memory/ref_counted.h"
+#include "base/task_scheduler/post_task.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/file_stream.h"
 #include "storage/browser/blob/shareable_file_reference.h"
@@ -20,19 +21,18 @@ namespace content {
 
 namespace {
 
-void DidCreateTemporaryFile(const CreateTemporaryFileStreamCallback& callback,
-                            std::unique_ptr<base::FileProxy> file_proxy,
-                            base::File::Error error_code,
-                            const base::FilePath& file_path) {
+void DidCreateTemporaryFile(
+    const CreateTemporaryFileStreamCallback& callback,
+    std::unique_ptr<base::FileProxy> file_proxy,
+    scoped_refptr<base::SequencedTaskRunner> task_runner,
+    base::File::Error error_code,
+    const base::FilePath& file_path) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
   if (!file_proxy->IsValid()) {
     callback.Run(error_code, std::unique_ptr<net::FileStream>(), NULL);
     return;
   }
-
-  scoped_refptr<base::TaskRunner> task_runner =
-      BrowserThread::GetTaskRunnerForThread(BrowserThread::FILE);
 
   // Cancelled or not, create the deletable_file so the temporary is cleaned up.
   scoped_refptr<ShareableFileReference> deletable_file =
@@ -53,12 +53,18 @@ void CreateTemporaryFileStream(
     const CreateTemporaryFileStreamCallback& callback) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
-  std::unique_ptr<base::FileProxy> file_proxy(new base::FileProxy(
-      BrowserThread::GetTaskRunnerForThread(BrowserThread::FILE).get()));
+  scoped_refptr<base::SequencedTaskRunner> task_runner =
+      base::CreateSequencedTaskRunnerWithTraits(
+          {base::MayBlock(), base::TaskPriority::USER_VISIBLE,
+           base::TaskShutdownBehavior::BLOCK_SHUTDOWN});
+
+  std::unique_ptr<base::FileProxy> file_proxy(
+      new base::FileProxy(task_runner.get()));
   base::FileProxy* proxy = file_proxy.get();
   proxy->CreateTemporary(
       base::File::FLAG_ASYNC,
-      base::Bind(&DidCreateTemporaryFile, callback, Passed(&file_proxy)));
+      base::Bind(&DidCreateTemporaryFile, callback, Passed(&file_proxy),
+                 std::move(task_runner)));
 }
 
 }  // namespace content
