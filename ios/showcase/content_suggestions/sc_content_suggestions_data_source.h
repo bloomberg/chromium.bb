21d9ccd2884ad6219c51c87caedb1c59add31d04
Gauthier Ambard <gambard@chromium.org>
2017-06-13 16:25:27 +0000

    Revert "Add EG test for SCContentSuggestions"
    This reverts commit 6e3030487c53bc0bce18d4218f1a9348c374a142 [formerly 2be5085c9c7797ebadf40df25c25e54449743406].
    
    Reason for revert: Breaks on iPhone 5
    
    Original change's description:
    > Add EG test for SCContentSuggestions
    > 
    > This CL adds Earl Grey tests testing swipe to dismiss, tap and long press on
    > suggested content.
    > 
    > BUG=704493
    > 
    > Change-Id: Ibd3d9e8030fcaf4069a2cf632791bbb0387a7301
    > Reviewed-on: https://chromium-review.googlesource.com/529248
    > Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    > Reviewed-by: Louis Romero <lpromero@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#478984}
    
    TBR=lpromero@chromium.org,edchin@chromium.org,gambard@chromium.org
    
    Change-Id: I8677cffaf92abd123f6b0a215886416f76fca175
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 704493
    Reviewed-on: https://chromium-review.googlesource.com/533338
    Reviewed-by: Gauthier Ambard <gambard@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479040}
    Former-commit-id: 9252a3b5bb4c21ab80082b5a82530dee524bdca7

--- ios/showcase/content_suggestions/sc_content_suggestions_data_source.h
+++ ios/showcase/content_suggestions/sc_content_suggestions_data_source.h
@@ -13,12 +13,6 @@
 @interface SCContentSuggestionsDataSource
     : NSObject<ContentSuggestionsDataSource>
 
-// Title of the first suggested article.
-+ (NSString*)titleFirstSuggestion;
-
-// Title of the Reading List item.
-+ (NSString*)titleReadingListItem;
-
 @end
 
 #endif  // IOS_SHOWCASE_CONTENT_SUGGESTIONS_SC_CONTENT_SUGGESTIONS_DATA_SOURCE_H_
