bdaf4d97edbe68889b42cec28c5928ecbf6f2c9a
rohitrao <rohitrao@chromium.org>
2017-06-13 05:50:30 -0700

    [ios] Disables WindowLocationTest.WindowLocationSetToDOMString on device.
    This test occasionally times out on iOS 9.3.2 devices.
    
    BUG=731740
    
    Review-Url: https://codereview.chromium.org/2937693002
    Cr-Commit-Position: refs/heads/master@{#478987}
    
    Former-commit-id: 4fc8601f5e8bc4bcefed93f3edefbb70dc163fbd

--- ios/web/navigation/window_location_inttest.mm
+++ ios/web/navigation/window_location_inttest.mm
@@ -222,7 +222,14 @@ TEST_F(WindowLocationTest, MAYBE_WindowLocationReload) {
 }
 
 // Tests that calling window.location.assign() creates a new NavigationItem.
-TEST_F(WindowLocationTest, WindowLocationSetToDOMString) {
+#if TARGET_IPHONE_SIMULATOR
+#define MAYBE_WindowLocationSetToDOMString WindowLocationSetToDOMString
+#else
+#define MAYBE_WindowLocationSetToDOMString DISABLED_WindowLocationSetToDOMString
+#endif
+// TODO(crbug.com/731740): This test is disabled because it occasionally times
+// out on device.
+TEST_F(WindowLocationTest, MAYBE_WindowLocationSetToDOMString) {
   // Navigate to about:blank so there is a forward entry to prune.
   GURL about_blank("about:blank");
   LoadUrl(about_blank);
