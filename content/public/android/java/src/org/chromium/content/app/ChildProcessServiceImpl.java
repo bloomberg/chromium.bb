2745540dc6c5fb35e2730b98605912aef3550acd
Jay Civelli <jcivelli@google.com>
2017-06-13 08:59:37 -0700

    ChildProcessServiceImpl: using a specific lock for library init var.
    Using a specific lock for the library initialized member variable in
    ChildProcessServiceImpl instead of using the main thread member.
    
    Bug: 732591
    Change-Id: I2973f250aaa1d03eebea75810a06ef4f6925537a
    Reviewed-on: https://chromium-review.googlesource.com/532313
    Reviewed-by: Bo Liu <boliu@chromium.org>
    Commit-Queue: Jay Civelli <jcivelli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479104}
    Former-commit-id: a8874ee752dc6a9e8250640952bfb3a8055c2764

--- content/public/android/java/src/org/chromium/content/app/ChildProcessServiceImpl.java
+++ content/public/android/java/src/org/chromium/content/app/ChildProcessServiceImpl.java
@@ -59,6 +59,7 @@ public class ChildProcessServiceImpl {
     private static boolean sCreateCalled;
 
     private final Object mBinderLock = new Object();
+    private final Object mLibraryInitializedLock = new Object();
 
     @GuardedBy("mBinderLock")
     private boolean mBindToCallerCheck;
@@ -81,6 +82,7 @@ public class ChildProcessServiceImpl {
     // Child library process type.
     private int mLibraryProcessType;
 
+    @GuardedBy("mLibraryInitializedLock")
     private boolean mLibraryInitialized;
 
     /**
@@ -253,9 +255,11 @@ public class ChildProcessServiceImpl {
                             .registerRendererProcessHistogram(requestedSharedRelro,
                                     loadAtFixedAddressFailed);
                     LibraryLoader.get(mLibraryProcessType).initialize();
-                    synchronized (mMainThread) {
+                    synchronized (mLibraryInitializedLock) {
                         mLibraryInitialized = true;
-                        mMainThread.notifyAll();
+                        mLibraryInitializedLock.notifyAll();
+                    }
+                    synchronized (mMainThread) {
                         while (mFdInfos == null) {
                             mMainThread.wait();
                         }
@@ -300,12 +304,12 @@ public class ChildProcessServiceImpl {
             System.exit(0);
             return;
         }
-        synchronized (mMainThread) {
+        synchronized (mLibraryInitializedLock) {
             try {
                 while (!mLibraryInitialized) {
                     // Avoid a potential race in calling through to native code before the library
                     // has loaded.
-                    mMainThread.wait();
+                    mLibraryInitializedLock.wait();
                 }
             } catch (InterruptedException e) {
                 // Ignore
