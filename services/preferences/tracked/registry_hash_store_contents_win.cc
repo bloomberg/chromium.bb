fc950d1227fbd17b58d30ab7a120c72a19937cec
dalecurtis <dalecurtis@chromium.org>
2017-06-13 09:19:07 -0700

    Revert of Fix a race condition in ~RegistryHashStoreContentsWin (patchset #9 id:180001 of https://codereview.chromium.org/2926453002/ )
    Reason for revert:
    temped_scoped_dir_cleaner is failing, see issue http://crbug.com/732835
    
    Original issue's description:
    > Fix a race condition in ~RegistryHashStoreContentsWin
    >
    > Enabling mojo-ification of Tracked Preference code highlighted a race
    > condition in integration tests: two instances of
    > RegistryHashStoreContentsWin for the same profile can destructed at the
    > same time on different threads, causing one of the registry operations
    > initiated by the destructor to fail.
    >
    > BUG=727282
    >
    > Review-Url: https://codereview.chromium.org/2926453002
    > Cr-Commit-Position: refs/heads/master@{#479020}
    > Committed: https://chromium.googlesource.com/chromium/src/+/46aaa3fc703b95a29ee74c549c254a58eab90e99 [formerly 7c8c48226fd78ad01858284bd2ee91bed27b513c]
    
    TBR=gab@chromium.org,proberge@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=727282
    
    Review-Url: https://codereview.chromium.org/2937633003
    Cr-Commit-Position: refs/heads/master@{#479037}
    
    Former-commit-id: a3a1cec34a434ca7c0ef4ac4f52b9bd8540873af

--- services/preferences/tracked/registry_hash_store_contents_win.cc
+++ services/preferences/tracked/registry_hash_store_contents_win.cc
@@ -6,10 +6,12 @@
 
 #include <windows.h>
 
+#include "base/files/scoped_temp_dir.h"
 #include "base/logging.h"
 #include "base/memory/ptr_util.h"
 #include "base/numerics/safe_conversions.h"
 #include "base/strings/string_split.h"
+#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
@@ -66,43 +68,21 @@ bool ClearSplitMac(const base::string16& reg_key_name,
   return false;
 }
 
-// Deletes |reg_key_name| if it exists.
-void DeleteRegistryKey(const base::string16& reg_key_name) {
-  base::win::RegKey key;
-  if (key.Open(HKEY_CURRENT_USER, reg_key_name.c_str(),
-               KEY_SET_VALUE | KEY_WOW64_32KEY) == ERROR_SUCCESS) {
-    LONG result = key.DeleteKey(L"");
-    DCHECK(result == ERROR_SUCCESS || result == ERROR_FILE_NOT_FOUND) << result;
-  }
-}
-
 }  // namespace
-
-void TempScopedDirRegistryCleaner::SetRegistryPath(
-    const base::string16& registry_path) {
-  if (registry_path_.empty())
-    registry_path_ = registry_path;
-  else
-    DCHECK_EQ(registry_path_, registry_path);
-}
-
-TempScopedDirRegistryCleaner::~TempScopedDirRegistryCleaner() {
-  DCHECK(!registry_path_.empty());
-  DeleteRegistryKey(registry_path_);
-}
 
 RegistryHashStoreContentsWin::RegistryHashStoreContentsWin(
     const base::string16& registry_path,
-    const base::string16& store_key,
-    scoped_refptr<TempScopedDirCleaner> temp_dir_cleaner)
+    const base::string16& store_key)
     : preference_key_name_(registry_path + L"\\PreferenceMACs\\" + store_key),
-      temp_dir_cleaner_(std::move(temp_dir_cleaner)) {
-  if (temp_dir_cleaner_)
-    static_cast<TempScopedDirRegistryCleaner*>(temp_dir_cleaner_.get())
-        ->SetRegistryPath(preference_key_name_);
+      reset_on_delete_(base::StartsWith(store_key,
+                                        base::ScopedTempDir::GetTempDirPrefix(),
+                                        base::CompareCase::INSENSITIVE_ASCII)) {
 }
 
-RegistryHashStoreContentsWin::~RegistryHashStoreContentsWin() = default;
+RegistryHashStoreContentsWin::~RegistryHashStoreContentsWin() {
+  if (reset_on_delete_)
+    Reset();
+}
 
 RegistryHashStoreContentsWin::RegistryHashStoreContentsWin(
     const RegistryHashStoreContentsWin& other) = default;
@@ -121,7 +101,14 @@ base::StringPiece RegistryHashStoreContentsWin::GetUMASuffix() const {
 }
 
 void RegistryHashStoreContentsWin::Reset() {
-  DeleteRegistryKey(preference_key_name_);
+  base::win::RegKey key;
+  if (key.Open(HKEY_CURRENT_USER, preference_key_name_.c_str(),
+               KEY_SET_VALUE | KEY_WOW64_32KEY) == ERROR_SUCCESS) {
+    LONG result = key.DeleteKey(L"");
+    DCHECK(result == ERROR_SUCCESS || result == ERROR_FILE_NOT_FOUND ||
+           result == ERROR_ACCESS_DENIED || result == ERROR_KEY_DELETED)
+        << result;
+  }
 }
 
 bool RegistryHashStoreContentsWin::GetMac(const std::string& path,
