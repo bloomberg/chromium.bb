d3e9eee6690282dd097f3b97b8ec270ce416d6ba
shend <shend@chromium.org>
2017-06-13 01:22:46 -0700

    Make RareNonInheritedData use MemberCopy and DataEquivalent.
    When we generate RareNonInheritedData, the generated code will use
    MemberCopy and DataEquivalent on all pointer fields. This patch first
    changes the handwritten RareNonInheritedData to use these functions
    in preparation for generating RareNonInheritedData.
    
    Note that we have temporary overloads for DataRef. These will no longer
    be needed once all subgroups of RareNonInheritedData are generated.
    
    BUG=628043
    
    Review-Url: https://codereview.chromium.org/2926053002
    Cr-Commit-Position: refs/heads/master@{#478934}
    
    Former-commit-id: b8a5e4f08664ade94cc12c72cec6e208e03fad2d

--- third_party/WebKit/Source/core/style/DataEquivalency.h
+++ third_party/WebKit/Source/core/style/DataEquivalency.h
@@ -14,6 +14,10 @@ template <typename T>
 class Persistent;
 template <typename T>
 class Member;
+template <typename T>
+class DataRef;
+template <typename T>
+class DataPersistent;
 
 template <typename T>
 bool DataEquivalent(const T* a, const T* b) {
@@ -42,6 +46,18 @@ bool DataEquivalent(const Member<T>& a, const Member<T>& b) {
 template <typename T>
 bool DataEquivalent(const std::unique_ptr<T>& a, const std::unique_ptr<T>& b) {
   return DataEquivalent(a.get(), b.get());
+}
+
+// TODO(shend): Remove this once all subgroups of StyleRareNonInheritedData are
+// generated
+template <typename T>
+bool DataEquivalent(const DataRef<T>& a, const DataRef<T>& b) {
+  return DataEquivalent(a.Get(), b.Get());
+}
+
+template <typename T>
+bool DataEquivalent(const DataPersistent<T>& a, const DataPersistent<T>& b) {
+  return DataEquivalent(a.Get(), b.Get());
 }
 
 }  // namespace blink
--- third_party/WebKit/Source/core/style/StyleRareNonInheritedData.cpp
+++ third_party/WebKit/Source/core/style/StyleRareNonInheritedData.cpp
@@ -23,9 +23,10 @@
 
 #include "core/animation/css/CSSAnimationData.h"
 #include "core/animation/css/CSSTransitionData.h"
+#include "core/style/ComputedStyle.h"
 #include "core/style/ContentData.h"
 #include "core/style/DataEquivalency.h"
-#include "core/style/ComputedStyle.h"
+#include "core/style/MemberCopy.h"
 #include "core/style/ShadowList.h"
 #include "core/style/StyleFilterData.h"
 #include "core/style/StyleNonInheritedVariables.h"
@@ -143,25 +144,25 @@ StyleRareNonInheritedData::StyleRareNonInheritedData(
       object_position_(o.object_position_),
       line_clamp_(o.line_clamp_),
       draggable_region_mode_(o.draggable_region_mode_),
-      deprecated_flexible_box_data_(o.deprecated_flexible_box_data_),
-      flexible_box_data_(o.flexible_box_data_),
-      multi_col_data_(o.multi_col_data_),
-      transform_data_(o.transform_data_),
-      will_change_data_(o.will_change_data_),
-      filter_(o.filter_),
-      backdrop_filter_(o.backdrop_filter_),
-      grid_data_(o.grid_data_),
-      grid_item_data_(o.grid_item_data_),
-      scroll_snap_data_(o.scroll_snap_data_),
-      content_(o.content_ ? o.content_->Clone() : nullptr),
-      counter_directives_(o.counter_directives_ ? o.counter_directives_->Clone()
-                                                : nullptr),
-      animations_(o.animations_ ? o.animations_->Clone() : nullptr),
-      transitions_(o.transitions_ ? o.transitions_->Clone() : nullptr),
-      box_shadow_(o.box_shadow_),
-      box_reflect_(o.box_reflect_),
+      deprecated_flexible_box_data_(
+          MemberCopy(o.deprecated_flexible_box_data_)),
+      flexible_box_data_(MemberCopy(o.flexible_box_data_)),
+      multi_col_data_(MemberCopy(o.multi_col_data_)),
+      transform_data_(MemberCopy(o.transform_data_)),
+      will_change_data_(MemberCopy(o.will_change_data_)),
+      filter_(MemberCopy(o.filter_)),
+      backdrop_filter_(MemberCopy(o.backdrop_filter_)),
+      grid_data_(MemberCopy(o.grid_data_)),
+      grid_item_data_(MemberCopy(o.grid_item_data_)),
+      scroll_snap_data_(MemberCopy(o.scroll_snap_data_)),
+      content_(MemberCopy(o.content_)),
+      counter_directives_(MemberCopy(o.counter_directives_)),
+      animations_(MemberCopy(o.animations_)),
+      transitions_(MemberCopy(o.transitions_)),
+      box_shadow_(MemberCopy(o.box_shadow_)),
+      box_reflect_(MemberCopy(o.box_reflect_)),
       shape_outside_(o.shape_outside_),
-      clip_path_(o.clip_path_),
+      clip_path_(MemberCopy(o.clip_path_)),
       mask_(o.mask_),
       mask_box_image_(o.mask_box_image_),
       page_size_(o.page_size_),
@@ -177,11 +178,8 @@ StyleRareNonInheritedData::StyleRareNonInheritedData(
       visited_link_border_top_color_(o.visited_link_border_top_color_),
       visited_link_border_bottom_color_(o.visited_link_border_bottom_color_),
       callback_selectors_(o.callback_selectors_),
-      paint_images_(o.paint_images_ ? new PaintImages(*o.paint_images_)
-                                    : nullptr),
-      non_inherited_variables_(o.non_inherited_variables_
-                                   ? o.non_inherited_variables_->Clone()
-                                   : nullptr),
+      paint_images_(MemberCopy(o.paint_images_)),
+      non_inherited_variables_(MemberCopy(o.non_inherited_variables_)),
       align_content_(o.align_content_),
       align_items_(o.align_items_),
       align_self_(o.align_self_),
@@ -236,14 +234,17 @@ bool StyleRareNonInheritedData::operator==(
          object_position_ == o.object_position_ &&
          line_clamp_ == o.line_clamp_ &&
          draggable_region_mode_ == o.draggable_region_mode_ &&
-         deprecated_flexible_box_data_ == o.deprecated_flexible_box_data_ &&
-         flexible_box_data_ == o.flexible_box_data_ &&
-         multi_col_data_ == o.multi_col_data_ &&
-         transform_data_ == o.transform_data_ &&
-         will_change_data_ == o.will_change_data_ && filter_ == o.filter_ &&
-         backdrop_filter_ == o.backdrop_filter_ && grid_data_ == o.grid_data_ &&
-         grid_item_data_ == o.grid_item_data_ &&
-         scroll_snap_data_ == o.scroll_snap_data_ &&
+         DataEquivalent(deprecated_flexible_box_data_,
+                        o.deprecated_flexible_box_data_) &&
+         DataEquivalent(flexible_box_data_, o.flexible_box_data_) &&
+         DataEquivalent(multi_col_data_, o.multi_col_data_) &&
+         DataEquivalent(transform_data_, o.transform_data_) &&
+         DataEquivalent(will_change_data_, o.will_change_data_) &&
+         DataEquivalent(filter_, o.filter_) &&
+         DataEquivalent(backdrop_filter_, o.backdrop_filter_) &&
+         DataEquivalent(grid_data_, o.grid_data_) &&
+         DataEquivalent(grid_item_data_, o.grid_item_data_) &&
+         DataEquivalent(scroll_snap_data_, o.scroll_snap_data_) &&
          DataEquivalent(content_, o.content_) &&
          DataEquivalent(counter_directives_, o.counter_directives_) &&
          DataEquivalent(box_shadow_, o.box_shadow_) &&
