5a0a8df8dccad8e9e925c12b8a5d2ffd1824b85e
vadimt <vadimt@chromium.org>
2017-06-13 11:40:33 -0700

    Adding a flag for Playstore search.
    Bug=712331
    
    Review-Url: https://codereview.chromium.org/2939603002
    Cr-Commit-Position: refs/heads/master@{#479076}
    
    Former-commit-id: d2c0221f76a7f3bbda235dab39211838eb5c2e17

--- ui/app_list/app_list_features.h
+++ ui/app_list/app_list_features.h
@@ -32,6 +32,7 @@ bool APP_LIST_EXPORT IsAnswerCardEnabled();
 bool APP_LIST_EXPORT IsAnswerCardDarkRunEnabled();
 bool APP_LIST_EXPORT IsFullscreenAppListEnabled();
 bool APP_LIST_EXPORT IsSearchResultsNewDesignEnabled();
+bool APP_LIST_EXPORT IsPlaystoreAppSearchEnabled();
 int APP_LIST_EXPORT AnswerCardMaxWidth();
 int APP_LIST_EXPORT AnswerCardMaxHeight();
 std::string APP_LIST_EXPORT AnswerServerUrl();
