50b3d4195555de2d4b86bb9fd1b358cdaa9520c2
Hector Dearman <hjd@google.com>
2017-06-14 14:29:47 +0100

    memory-infra: Re-enable MemoryDumpSchedulerTest.MultipleTriggers
    De-flakes and re-enables MemoryDumpSchedulerTest.MultipleTriggers.
    This test was flaky because we scheduled the next task before calling
    the callback. When calling the callback takes a long time (due to TSAN)
    the timing (as seen by the tests) is off. Instead we can run the
    callback before scheduling the next tick avoiding this issue.
    
    Bug: 732861
    Change-Id: I39c8c63c2c6589e7319f48bbf15e26acea9b6295
    Reviewed-on: https://chromium-review.googlesource.com/535541
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Hector Dearman <hjd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479395}
    Former-commit-id: 52d98977cc8d59dfa5fdd9b37858fe35af6737ad

--- base/trace_event/memory_dump_scheduler_unittest.cc
+++ base/trace_event/memory_dump_scheduler_unittest.cc
@@ -88,13 +88,7 @@ TEST_F(MemoryDumpSchedulerTest, SingleTrigger) {
   scheduler_->Stop();
 }
 
-// Flaky on Linux. http://crbug.com/732861
-#if defined(OS_LINUX)
-#define MAYBE_MultipleTriggers DISABLED_MultipleTriggers
-#else
-#define MAYBE_MultipleTriggers MultipleTriggers
-#endif
-TEST_F(MemoryDumpSchedulerTest, MAYBE_MultipleTriggers) {
+TEST_F(MemoryDumpSchedulerTest, MultipleTriggers) {
   const uint32_t kPeriodLightMs = 3;
   const uint32_t kPeriodDetailedMs = 9;
   WaitableEvent evt(WaitableEvent::ResetPolicy::MANUAL,
