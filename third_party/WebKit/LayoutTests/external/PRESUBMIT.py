ff939ba849f808c0ebb8a7519282bd4b6690be62
Quinten Yearsley <qyearsley@google.com>
2017-06-14 16:04:16 -0700

    Fix wpt lint presubmit script.
    Specifically, this changes the the --ignore-glob flag in the command.
    
    This CL also makes a couple minor style/comment changes
    and makes the presubmit skip the linting if no files in
    wpt were changed.
    
    Bug: 686927
    Change-Id: I17f55fdaa30b3ecdf82cae1884b332a0ccff8afe
    Reviewed-on: https://chromium-review.googlesource.com/536234
    Reviewed-by: Jeff Carpenter <jeffcarp@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479755}
    Former-commit-id: 295132a293eadbdee96c0a728c811b4d0c2a481b

--- third_party/WebKit/LayoutTests/external/PRESUBMIT.py
+++ third_party/WebKit/LayoutTests/external/PRESUBMIT.py
@@ -2,46 +2,53 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Lint functionality duplicated from WPT upstream.
+"""Lint functionality duplicated from web-platform-tests upstream.
 
 This is to catch lint errors that would otherwise be caught in WPT CI.
+See http://web-platform-tests.org/writing-tests/lint-tool.html for more
+information about the lint tool.
 """
+
 
 def _LintWPT(input_api, output_api):
     wpt_path = input_api.os_path.join(input_api.PresubmitLocalPath(), 'wpt')
-    linter_path = input_api.os_path.join(input_api.PresubmitLocalPath(),
-        '..', '..', 'Tools', 'Scripts', 'webkitpy', 'thirdparty', 'wpt', 'wpt', 'lint')
+    linter_path = input_api.os_path.join(
+        input_api.PresubmitLocalPath(), '..', '..', 'Tools', 'Scripts',
+        'webkitpy', 'thirdparty', 'wpt', 'wpt', 'lint')
 
     paths_in_wpt = []
-    for file in input_api.AffectedFiles():
-        abs_path = file.AbsoluteLocalPath()
+    for f in input_api.AffectedFiles():
+        abs_path = f.AbsoluteLocalPath()
         if abs_path.startswith(wpt_path):
             paths_in_wpt.append(abs_path[len(wpt_path) + 1:])
+
+    # If there are changes in LayoutTests/external that aren't in wpt, e.g.
+    # changes to wpt_automation or this presubmit script, then we can return
+    # to avoid running the linter on all files in wpt (which is slow).
+    if not paths_in_wpt:
+        return []
 
     args = [
         input_api.python_executable,
         linter_path,
-        '--repo-root', wpt_path,
-        '-ignore-glob' '"*-expected.txt"'
+        '--repo-root=%s' % wpt_path,
+        '--ignore-glob=*-expected.txt',
     ] + paths_in_wpt
 
-    stdout_data, _ = input_api.subprocess.Popen(
+    proc = input_api.subprocess.Popen(
         args,
         stdout=input_api.subprocess.PIPE,
-        stderr=input_api.subprocess.PIPE).communicate()
+        stderr=input_api.subprocess.PIPE)
+    stdout, _ = proc.communicate()
 
-    if stdout_data:
-        return [output_api.PresubmitError(stdout_data)]
+    if stdout:
+        return [output_api.PresubmitError(stdout)]
     return []
 
 
 def CheckChangeOnUpload(input_api, output_api):
-    results = []
-    results.extend(_LintWPT(input_api, output_api))
-    return results
+    return _LintWPT(input_api, output_api)
 
 
 def CheckChangeOnCommit(input_api, output_api):
-    results = []
-    results.extend(_LintWPT(input_api, output_api))
-    return results
+    return _LintWPT(input_api, output_api)
