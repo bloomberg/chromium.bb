5064388b597991b023c6646ac917c95f4523a24f
mastiz <mastiz@chromium.org>
2017-06-13 22:02:55 -0700

    ntp_tiles: Extend chrome://ntp-tiles-internals with favicon data
    For each suggestion, the page URL is used to issue a lookup in the
    favicon cache, and the results are displayed in the table.
    
    BUG=655622
    
    Review-Url: https://codereview.chromium.org/2936793002
    Cr-Commit-Position: refs/heads/master@{#479288}
    
    Former-commit-id: fbc3cb683e35004ca90dd649076e77ad3ff8e47e

--- ios/chrome/browser/ui/webui/ntp_tiles_internals_ui.cc
+++ ios/chrome/browser/ui/webui/ntp_tiles_internals_ui.cc
@@ -6,12 +6,14 @@
 
 #include "base/memory/ptr_util.h"
 #include "components/grit/components_resources.h"
+#include "components/keyed_service/core/service_access_type.h"
 #include "components/ntp_tiles/field_trial.h"
 #include "components/ntp_tiles/most_visited_sites.h"
 #include "components/ntp_tiles/webui/ntp_tiles_internals_message_handler.h"
 #include "components/ntp_tiles/webui/ntp_tiles_internals_message_handler_client.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
+#include "ios/chrome/browser/favicon/favicon_service_factory.h"
 #include "ios/chrome/browser/ntp_tiles/ios_most_visited_sites_factory.h"
 #include "ios/chrome/browser/ntp_tiles/ios_popular_sites_factory.h"
 #include "ios/web/public/web_thread.h"
@@ -26,7 +28,10 @@ class IOSNTPTilesInternalsMessageHandlerBridge
     : public web::WebUIIOSMessageHandler,
       public ntp_tiles::NTPTilesInternalsMessageHandlerClient {
  public:
-  IOSNTPTilesInternalsMessageHandlerBridge() {}
+  // |favicon_service| must not be null and must outlive this object.
+  explicit IOSNTPTilesInternalsMessageHandlerBridge(
+      favicon::FaviconService* favicon_service)
+      : handler_(favicon_service) {}
 
  private:
   // web::WebUIIOSMessageHandler:
@@ -109,10 +114,14 @@ web::WebUIIOSDataSource* CreateNTPTilesInternalsHTMLSource() {
 
 NTPTilesInternalsUI::NTPTilesInternalsUI(web::WebUIIOS* web_ui)
     : web::WebUIIOSController(web_ui) {
-  web::WebUIIOSDataSource::Add(ios::ChromeBrowserState::FromWebUIIOS(web_ui),
+  ios::ChromeBrowserState* browser_state =
+      ios::ChromeBrowserState::FromWebUIIOS(web_ui);
+  web::WebUIIOSDataSource::Add(browser_state,
                                CreateNTPTilesInternalsHTMLSource());
   web_ui->AddMessageHandler(
-      base::MakeUnique<IOSNTPTilesInternalsMessageHandlerBridge>());
+      base::MakeUnique<IOSNTPTilesInternalsMessageHandlerBridge>(
+          ios::FaviconServiceFactory::GetForBrowserState(
+              browser_state, ServiceAccessType::EXPLICIT_ACCESS)));
 }
 
 NTPTilesInternalsUI::~NTPTilesInternalsUI() {}
