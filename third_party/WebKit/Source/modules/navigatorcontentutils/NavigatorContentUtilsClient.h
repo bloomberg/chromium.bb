a921e87885085b5020112c448dc460582a3bb3c8
Sasha Bermeister <sashab@chromium.org>
2017-06-14 14:11:19 +1000

    Merged NavigatorContentUtilsClient and NavigatorContentUtilsClientImpl
    The NavigatorContentUtilsClientImpl class is an artifact left over from
    when NavigatorContentUtilsClient was in modules/ and
    NavigatorContentUtilsClientImpl was in web/. Now that they are both in
    modules/, there's no need to have these as two separate classes.
    
    Bug: 731490
    Change-Id: I63d18671e6ebfadd8fefe8c52617918474f5cbc0
    Reviewed-on: https://chromium-review.googlesource.com/535195
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Sasha Morrissey <sashab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479315}
    Former-commit-id: b52f16be8c5dfc01e33afae295b5ed78c9764262

--- third_party/WebKit/Source/modules/navigatorcontentutils/NavigatorContentUtilsClient.h
+++ third_party/WebKit/Source/modules/navigatorcontentutils/NavigatorContentUtilsClient.h
@@ -1,27 +1,6 @@
-/*
- * Copyright (C) 2012 Samsung Electronics. All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY APPLE INC. ``AS IS'' AND ANY
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL APPLE INC. OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
- * OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef NavigatorContentUtilsClient_h
 #define NavigatorContentUtilsClient_h
@@ -34,13 +13,17 @@
 
 namespace blink {
 
-class NavigatorContentUtilsClient
+class WebLocalFrameBase;
+
+class MODULES_EXPORT NavigatorContentUtilsClient
     : public GarbageCollectedFinalized<NavigatorContentUtilsClient> {
  public:
+  static NavigatorContentUtilsClient* Create(WebLocalFrameBase*);
   virtual ~NavigatorContentUtilsClient() {}
+
   virtual void RegisterProtocolHandler(const String& scheme,
                                        const KURL&,
-                                       const String& title) = 0;
+                                       const String& title);
 
   enum CustomHandlersState {
     kCustomHandlersNew,
@@ -49,10 +32,16 @@ class NavigatorContentUtilsClient
   };
 
   virtual CustomHandlersState IsProtocolHandlerRegistered(const String& scheme,
-                                                          const KURL&) = 0;
-  virtual void UnregisterProtocolHandler(const String& scheme, const KURL&) = 0;
+                                                          const KURL&);
+  virtual void UnregisterProtocolHandler(const String& scheme, const KURL&);
 
-  DEFINE_INLINE_VIRTUAL_TRACE() {}
+  DECLARE_VIRTUAL_TRACE();
+
+ protected:
+  explicit NavigatorContentUtilsClient(WebLocalFrameBase*);
+
+ private:
+  Member<WebLocalFrameBase> web_frame_;
 };
 
 }  // namespace blink
