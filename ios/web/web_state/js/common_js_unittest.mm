cf227f65091d08e2e3e207d542a07c3a2d8208c8
marq <marq@chromium.org>
2017-06-14 11:36:43 -0700

    [ObjC ARC] Converts ios/web:ios_web_web_state_js_unittests to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2933373002
    Cr-Commit-Position: refs/heads/master@{#479453}
    
    Former-commit-id: fda6759463dc44e5c0f5b0c46e6eb23bcd6fb349

--- ios/web/web_state/js/common_js_unittest.mm
+++ ios/web/web_state/js/common_js_unittest.mm
@@ -11,6 +11,10 @@
 #include "testing/gtest/include/gtest/gtest.h"
 #import "testing/gtest_mac.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace {
 
 // Struct for isTextField() test data.
