e1e61a6c04bfddf3bc63a4f9ae2d9a3950a13748
jiameng <jiameng@chromium.org>
2017-06-14 00:52:51 -0700

    Implement API for shorthand property "flex"
    This cl implements parseShorthand method for CSSShorthandPropertyAPIFlex API. This cl is the 2nd batch of the split from cl (crrev.com/2932663002)
    
    The cl contains the following changes
    (i). A new CSSShorthandPropertyAPIFlex class and its parseShorthand method impl.
    
    (ii). Updated CSSPropertyParser's ParseShorthand so that it'll use flex's API.
    
    (iii). Renamed CSSPropertyAPITextDecoration.[cpp|h] to CSSShorthandPropertyAPITextDecoration.[cpp|h]
    - text-decoration is a shorthand property, so its API class name should have 'Shorthand' prefix.
    - this class has no impl, so there's no impact from the naming change.
    - cl (crrev.com/2932403003) removed the old generated .h file, this cl adds the new generated header file. This cl also replaces the old .cpp with the new .cpp.
    
    Diff: https://gist.github.com/95c9d6865aa31b2fdc824cb87b812d14/revisions
    Review-Url: https://codereview.chromium.org/2935753003
    Cr-Commit-Position: refs/heads/master@{#479321}
    
    Former-commit-id: 8c5e4fb1fced9af3d19e718e54e07ef8b8d2d1da

--- third_party/WebKit/Source/core/css/properties/CSSShorthandPropertyAPITextDecoration.cpp
+++ third_party/WebKit/Source/core/css/properties/CSSShorthandPropertyAPITextDecoration.cpp
@@ -2,6 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "core/css/properties/CSSPropertyAPITextDecoration.h"
+#include "core/css/properties/CSSShorthandPropertyAPITextDecoration.h"
 
 namespace blink {}  // namespace blink
