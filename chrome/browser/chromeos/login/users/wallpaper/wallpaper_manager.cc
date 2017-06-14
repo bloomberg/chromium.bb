df5ff95936bdd5f824831de2a62718448dbacdfb
xdai <xdai@chromium.org>
2017-06-14 13:33:28 -0700

    Fix the crash that happens if the default wallpaper image decoding failed.
    BUG=725667
    
    Review-Url: https://codereview.chromium.org/2931063003
    Cr-Commit-Position: refs/heads/master@{#479490}
    
    Former-commit-id: 26f7ab8456c7e21751bd55ef1112a063dfc25203

--- chrome/browser/chromeos/login/users/wallpaper/wallpaper_manager.cc
+++ chrome/browser/chromeos/login/users/wallpaper/wallpaper_manager.cc
@@ -1380,6 +1380,11 @@ void WallpaperManager::OnDefaultWallpaperDecoded(
     std::unique_ptr<user_manager::UserImage>* result_out,
     MovableOnDestroyCallbackHolder on_finish,
     std::unique_ptr<user_manager::UserImage> user_image) {
+  if (user_image->image().isNull()) {
+    LOG(ERROR) << "Failed to decode default wallpaper. ";
+    return;
+  }
+
   *result_out = std::move(user_image);
   SetWallpaper((*result_out)->image(), layout);
 }
