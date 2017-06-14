422f9418f714335e15ec7c0f2e07bb5102297c43
Xida Chen <xidachen@chromium.org>
2017-06-14 16:49:47 -0400

    Change the bounds in PaintGeneratedImage::Draw
    I did bisect for this bug, and it is pointing to this CL:
    https://codereview.chromium.org/2914863003.
    I realized that the above CL loses the origin when it sets the bound
    in PaintGeneratedImage. So in this CL, I change the bound to be src_rect.
    
    The change seems to pass major try bots. A new layout test is added.
    
    Bug: 731942
    Change-Id: If44e698b024f7bfb72d71e41932ad3b2cbd22852
    Reviewed-on: https://chromium-review.googlesource.com/533473
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Reviewed-by: Stephen Chenney <schenney@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479532}
    Former-commit-id: be46e03b22ee635ddd23a08c4a07e3551d211966

--- third_party/WebKit/Source/platform/graphics/PaintGeneratedImage.cpp
+++ third_party/WebKit/Source/platform/graphics/PaintGeneratedImage.cpp
@@ -23,7 +23,7 @@ void PaintGeneratedImage::Draw(PaintCanvas* canvas,
     canvas->scale(dest_rect.Width() / src_rect.Width(),
                   dest_rect.Height() / src_rect.Height());
   canvas->translate(-src_rect.X(), -src_rect.Y());
-  SkRect bounds = SkRect::MakeWH(src_rect.Width(), src_rect.Height());
+  SkRect bounds = src_rect;
   canvas->saveLayer(&bounds, &flags);
   canvas->drawPicture(record_);
 }
