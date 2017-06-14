2d4c176220d03f92ae43426f49cac36c24e023ec
Jeffrey He <jeffreyhe@google.com>
2017-06-14 13:16:48 -0400

    Integrate SequenceLocalStorageMap into MessageLoop
    MessageLoop::BindToCurrentThread() now sets up a SequenceLocalStorageMap
    on the thread running the MessageLoop. This allows SequenceLocalStorage
    to be used by tasks running on a MessageLoop as well as by code within
    the scope of a MessageLoop.
    
    Bug: 695727
    Change-Id: If0d1eb06cd936e1d032d8bb70bbdb994f080a3fe
    Reviewed-on: https://chromium-review.googlesource.com/533394
    Commit-Queue: Jeffrey He <jeffreyhe@google.com>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Reviewed-by: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479458}
    Former-commit-id: ebe0c685c86dd20cf520e7a76d9139ba891745ba

--- base/threading/sequence_local_storage_map.h
+++ base/threading/sequence_local_storage_map.h
@@ -73,8 +73,9 @@ class BASE_EXPORT SequenceLocalStorageMap {
 // Within the scope of this object,
 // SequenceLocalStorageMap::GetForCurrentThread() will return a reference to
 // the SequenceLocalStorageMap object passed to the constructor.
-// There can only be one ScopedSetSequenceLocalStorageMapForCurrentThread
-// instance per thread at a time.
+// TODO(jeffreyhe): Remove |old_sequence_local_storage_| and only allow one
+// ScopedSetSequenceLocalStorageMapForCurrentThread per thread at a time
+// once ScopedTaskScheduler is removed.
 class BASE_EXPORT ScopedSetSequenceLocalStorageMapForCurrentThread {
  public:
   ScopedSetSequenceLocalStorageMapForCurrentThread(
@@ -83,6 +84,8 @@ class BASE_EXPORT ScopedSetSequenceLocalStorageMapForCurrentThread {
   ~ScopedSetSequenceLocalStorageMapForCurrentThread();
 
  private:
+  SequenceLocalStorageMap* const old_sequence_local_storage_;
+
   DISALLOW_COPY_AND_ASSIGN(ScopedSetSequenceLocalStorageMapForCurrentThread);
 };
 }  // namespace internal
