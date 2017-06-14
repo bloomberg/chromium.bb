af6ee928aa4575080d6266975f4b044884e6aea8
mark a. foltz <mfoltz@chromium.org>
2017-06-14 11:40:47 -0700

    Revert "Revert "[chrome.cast.channel] Remove logging.proto from Cast channel.""
    This reverts commit ab928565ab8102daf28bfec7f692c457a7a79e2f [formerly 7fe21b23107abb9852e497c2ccbed97fd1ba3db1].
    
    Reason for revert: Remove #include that broke build.
    
    Original change's description:
    > Revert "[chrome.cast.channel] Remove logging.proto from Cast channel."
    > 
    > This reverts commit 98ea458dc1b7d7e3362f6d39f19f970ae76ff80a [formerly 0a656de983cf9717acef57c64a999a90b96d531d].
    > 
    > Reason for revert: Broke the tree,
    > https://build.chromium.org/p/chromium/builders/Mac/builds/28833
    > https://build.chromium.org/p/chromium.chrome/builders/Google%20Chrome%20ChromeOS/builds/35155
    > 
    > Original change's description:
    > > [chrome.cast.channel] Remove logging.proto from Cast channel.
    > > 
    > > This protocol message is no longer necessary as we are no longer
    > > serializing logging events. This patch also converts the enums still
    > > in use to scoped enums, consolidates redundant enums elsewhere, and
    > > makes the following renamings:
    > > 
    > > LastErrors => LastError
    > > EventType => ChannelEvent
    > > ChallengeAuthErrorType => ChallengeAuthError
    > > 
    > > Bug: 513801
    > > Change-Id: If6e690c94854739f9861219871a048164924126b
    > > Reviewed-on: https://chromium-review.googlesource.com/533683
    > > Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    > > Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#479405}
    > 
    > TBR=mfoltz@chromium.org,kmarshall@chromium.org,marshallk@google.com,zhaobin@chromium.org
    > 
    > Change-Id: Icd83d69ddf8b24f87d8aac08e66646456b5a6f8f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 513801
    > Reviewed-on: https://chromium-review.googlesource.com/534903
    > Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#479408}
    
    TBR=dalecurtis@chromium.org,mfoltz@chromium.org,kmarshall@chromium.org,marshallk@google.com,zhaobin@chromium.org
    
    Change-Id: I81681feec9499a96fdc302e12863e6c882ab0251
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 513801
    Reviewed-on: https://chromium-review.googlesource.com/535242
    Commit-Queue: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479455}
    Former-commit-id: ae7702ad350761014f8daf0df3deea65f61ef54b

--- components/cast_channel/logger_unittest.cc
+++ components/cast_channel/logger_unittest.cc
@@ -14,54 +14,49 @@
 
 namespace cast_channel {
 
-using proto::EventType;
-using proto::Log;
-using proto::SocketEvent;
-
 TEST(CastChannelLoggerTest, LogLastErrorEvents) {
   scoped_refptr<Logger> logger(new Logger());
 
   // Net return value is set to an error
-  logger->LogSocketEventWithRv(1, EventType::TCP_SOCKET_CONNECT,
+  logger->LogSocketEventWithRv(1, ChannelEvent::TCP_SOCKET_CONNECT,
                                net::ERR_CONNECTION_FAILED);
 
-  LastErrors last_errors = logger->GetLastErrors(1);
-  EXPECT_EQ(last_errors.event_type, proto::TCP_SOCKET_CONNECT);
-  EXPECT_EQ(last_errors.net_return_value, net::ERR_CONNECTION_FAILED);
+  LastError last_error = logger->GetLastError(1);
+  EXPECT_EQ(last_error.channel_event, ChannelEvent::TCP_SOCKET_CONNECT);
+  EXPECT_EQ(last_error.net_return_value, net::ERR_CONNECTION_FAILED);
 
   // Challenge reply error set
   AuthResult auth_result = AuthResult::CreateWithParseError(
       "Some error", AuthResult::ErrorType::ERROR_PEER_CERT_EMPTY);
 
   logger->LogSocketChallengeReplyEvent(2, auth_result);
-  last_errors = logger->GetLastErrors(2);
-  EXPECT_EQ(last_errors.event_type, proto::AUTH_CHALLENGE_REPLY);
-  EXPECT_EQ(last_errors.challenge_reply_error_type,
-            proto::CHALLENGE_REPLY_ERROR_PEER_CERT_EMPTY);
+  last_error = logger->GetLastError(2);
+  EXPECT_EQ(last_error.channel_event, ChannelEvent::AUTH_CHALLENGE_REPLY);
+  EXPECT_EQ(last_error.challenge_reply_error,
+            ChallengeReplyError::PEER_CERT_EMPTY);
 
-  // Logging a non-error event does not set the LastErrors for the channel.
-  logger->LogSocketEventWithRv(3, EventType::TCP_SOCKET_CONNECT, net::OK);
-  last_errors = logger->GetLastErrors(3);
-  EXPECT_EQ(last_errors.event_type, proto::EVENT_TYPE_UNKNOWN);
-  EXPECT_EQ(last_errors.net_return_value, net::OK);
-  EXPECT_EQ(last_errors.challenge_reply_error_type,
-            proto::CHALLENGE_REPLY_ERROR_NONE);
+  // Logging a non-error event does not set the LastError for the channel.
+  logger->LogSocketEventWithRv(3, ChannelEvent::TCP_SOCKET_CONNECT, net::OK);
+  last_error = logger->GetLastError(3);
+  EXPECT_EQ(last_error.channel_event, ChannelEvent::UNKNOWN);
+  EXPECT_EQ(last_error.net_return_value, net::OK);
+  EXPECT_EQ(last_error.challenge_reply_error, ChallengeReplyError::NONE);
 
-  // Now log a challenge reply error.  LastErrors will be set.
+  // Now log a challenge reply error.  LastError will be set.
   auth_result =
       AuthResult("Some error failed", AuthResult::ERROR_WRONG_PAYLOAD_TYPE);
   logger->LogSocketChallengeReplyEvent(3, auth_result);
-  last_errors = logger->GetLastErrors(3);
-  EXPECT_EQ(last_errors.event_type, proto::AUTH_CHALLENGE_REPLY);
-  EXPECT_EQ(last_errors.challenge_reply_error_type,
-            proto::CHALLENGE_REPLY_ERROR_WRONG_PAYLOAD_TYPE);
+  last_error = logger->GetLastError(3);
+  EXPECT_EQ(last_error.channel_event, ChannelEvent::AUTH_CHALLENGE_REPLY);
+  EXPECT_EQ(last_error.challenge_reply_error,
+            ChallengeReplyError::WRONG_PAYLOAD_TYPE);
 
-  // Logging a non-error event does not change the LastErrors for the channel.
-  logger->LogSocketEventWithRv(3, EventType::TCP_SOCKET_CONNECT, net::OK);
-  last_errors = logger->GetLastErrors(3);
-  EXPECT_EQ(last_errors.event_type, proto::AUTH_CHALLENGE_REPLY);
-  EXPECT_EQ(last_errors.challenge_reply_error_type,
-            proto::CHALLENGE_REPLY_ERROR_WRONG_PAYLOAD_TYPE);
+  // Logging a non-error event does not change the LastError for the channel.
+  logger->LogSocketEventWithRv(3, ChannelEvent::TCP_SOCKET_CONNECT, net::OK);
+  last_error = logger->GetLastError(3);
+  EXPECT_EQ(last_error.channel_event, ChannelEvent::AUTH_CHALLENGE_REPLY);
+  EXPECT_EQ(last_error.challenge_reply_error,
+            ChallengeReplyError::WRONG_PAYLOAD_TYPE);
 }
 
 }  // namespace cast_channel
