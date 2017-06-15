1f6778ccaabce22d6341db1ff56563c1135e9a13
wychen <wychen@chromium.org>
2017-06-15 14:35:48 -0700

    Record user action MobileHelpAndFeedback and MobileCustomFeedback
    MobileCustomFeedback is in downstream.
    
    BUG=731206
    
    Review-Url: https://codereview.chromium.org/2939893002
    Cr-Commit-Position: refs/heads/master@{#479847}
    
    Former-commit-id: 5235b8e3a5ef55769b4cb47335beca851a70528b

--- chrome/android/java/src/org/chromium/chrome/browser/help/HelpAndFeedback.java
+++ chrome/android/java/src/org/chromium/chrome/browser/help/HelpAndFeedback.java
@@ -14,6 +14,7 @@ import android.text.TextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.ThreadUtils;
 import org.chromium.base.annotations.SuppressFBWarnings;
+import org.chromium.base.metrics.RecordUserAction;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.AppHooks;
 import org.chromium.chrome.browser.UrlConstants;
@@ -85,6 +86,7 @@ public class HelpAndFeedback {
      */
     public void show(final Activity activity, final String helpContext, Profile profile,
             @Nullable String url) {
+        RecordUserAction.record("MobileHelpAndFeedback");
         FeedbackCollector.create(activity, profile, url, true /* takeScreenshot */,
                 new FeedbackCollector.FeedbackResult() {
                     @Override
