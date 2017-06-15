f60f6ec74488a9dbc5978db25da423628b977ca7
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-06-16 08:40:14 +1000

    bluetooth: Implement AddFakeService and convert tests
    Adds a fake GATT Service with |uuid| to be discovered when discovering
    the peripheral's GATT Attributes. Returns a FakeRemoteGATTService
    corresponding to this service. |uuid| should be a BluetoothServiceUUIDs
    https://webbluetoothcg.github.io/web-bluetooth/#typedefdef-bluetoothserviceuuid
    
    Bug: 719816
    Change-Id: I7192509b94fcc3921c4d9e8d7117dfd325a79c7e
    Reviewed-on: https://chromium-review.googlesource.com/527719
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Vincent Scheib <scheib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479943}
    Former-commit-id: 3b57fad232eae8f1cdc8868cb858b60f908091b7

--- third_party/WebKit/LayoutTests/bluetooth/script-tests/server/garbage-collection-ran-during-success.js
+++ third_party/WebKit/LayoutTests/bluetooth/script-tests/server/garbage-collection-ran-during-success.js
@@ -1,25 +1,21 @@
 'use strict';
 promise_test(() => {
   let promise;
-  return setBluetoothFakeAdapter('DisconnectingHealthThermometerAdapter')
-      .then(
-          () => requestDeviceWithKeyDown(
-              {filters: [{services: ['health_thermometer']}]}))
-      .then(device => device.gatt.connect())
-      .then(gattServer => {
-        promise = assert_promise_rejects_with_message(
-            gattServer.CALLS(
-                [getPrimaryService('health_thermometer') |
-                 getPrimaryServices() |
-                 getPrimaryServices('health_thermometer')[UUID]]),
-            new DOMException(
-                'GATT Server is disconnected. ' +
-                    'Cannot retrieve services. ' +
-                    '(Re)connect first with `device.gatt.connect`.',
-                'NetworkError'));
-        gattServer.disconnect();
-      })
-      .then(runGarbageCollection)
-      .then(() => promise);
+  return getHealthThermometerDevice({
+      filters: [{services: ['health_thermometer']}]})
+    .then(([device]) => {
+      promise = assert_promise_rejects_with_message(
+        device.gatt.CALLS([
+          getPrimaryService('health_thermometer') |
+          getPrimaryServices() |
+          getPrimaryServices('health_thermometer')[UUID]]),
+        new DOMException(
+          'GATT Server is disconnected. Cannot retrieve services. ' +
+          '(Re)connect first with `device.gatt.connect`.',
+          'NetworkError'));
+      device.gatt.disconnect();
+    })
+    .then(runGarbageCollection)
+    .then(() => promise);
 }, 'Garbage Collection ran during a FUNCTION_NAME call that succeeds. ' +
    'Should not crash.');
