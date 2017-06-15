f1eb3b1d84dcfee63723684f3888c41acfb1ee37
Takeshi Yoshino <tyoshino@chromium.org>
2017-06-15 20:06:41 +0900

    Inline CSSFontFaceSrcValue::SetCrossOriginAccessControl()
    Its contents are very short. It's better inlined.
    
    Bug: 
    Change-Id: Ib084c886c66ff17e4914a1c3a97e9d6d459852d2
    Reviewed-on: https://chromium-review.googlesource.com/536221
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Takeshi Yoshino <tyoshino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479678}
    Former-commit-id: 4a38d5b2e4769b67d5faf7b91da149fe0c92768e

--- third_party/WebKit/Source/core/css/CSSFontFaceSrcValue.cpp
+++ third_party/WebKit/Source/core/css/CSSFontFaceSrcValue.cpp
@@ -77,17 +77,6 @@ bool CSSFontFaceSrcValue::HasFailedOrCanceledSubresources() const {
   return fetched_ && fetched_->GetResource()->LoadFailedOrCanceled();
 }
 
-static void SetCrossOriginAccessControl(FetchParameters& params,
-                                        SecurityOrigin* security_origin) {
-  // Local fonts are accessible from file: URLs even when
-  // allowFileAccessFromFileURLs is false.
-  if (params.Url().IsLocalFile())
-    return;
-
-  params.SetCrossOriginAccessControl(security_origin,
-                                     kCrossOriginAttributeAnonymous);
-}
-
 FontResource* CSSFontFaceSrcValue::Fetch(Document* document) const {
   if (!fetched_) {
     ResourceRequest resource_request(absolute_resource_);
@@ -101,7 +90,14 @@ FontResource* CSSFontFaceSrcValue::Fetch(Document* document) const {
       params.SetCacheAwareLoadingEnabled(kIsCacheAwareLoadingEnabled);
     params.SetContentSecurityCheck(should_check_content_security_policy_);
     SecurityOrigin* security_origin = document->GetSecurityOrigin();
-    SetCrossOriginAccessControl(params, security_origin);
+
+    // Local fonts are accessible from file: URLs even when
+    // allowFileAccessFromFileURLs is false.
+    if (!params.Url().IsLocalFile()) {
+      params.SetCrossOriginAccessControl(security_origin,
+                                         kCrossOriginAttributeAnonymous);
+    }
+
     FontResource* resource = FontResource::Fetch(params, document->Fetcher());
     if (!resource)
       return nullptr;
