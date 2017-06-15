56b76482b25b419a71f347b80312e73aa74531bf
Steve Kobes <skobes@chromium.org>
2017-06-15 11:49:09 -0700

    Fix handling of scrollbar removal on viewport setting change.
    When the viewportEnabled and viewportMetaEnabled settings are turned on, we
    remove existing scrollbars on the LocalFrameView because the visual viewport
    will supply them instead.  But we weren't calling ScrollbarExistenceDidChange,
    so PaintLayerCompositor wasn't notified.  This meant the scrollbar and scroll
    corner layers continued to exist when they should have been destroyed, causing
    the paint code to crash.
    
    Bug: 729317
    Change-Id: I2d02a6d4f5b1cab96c510f143434a9d5c868f9a5
    Reviewed-on: https://chromium-review.googlesource.com/537335
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480100}
    Former-commit-id: 52dd5c4fd4a98a209ed3bfb8eadf4caf9e40fed6

--- third_party/WebKit/Source/core/frame/LocalFrameView.cpp
+++ third_party/WebKit/Source/core/frame/LocalFrameView.cpp
@@ -4399,8 +4399,12 @@ void LocalFrameView::UpdateScrollbars() {
 
   // Avoid drawing two sets of scrollbars when visual viewport is enabled.
   if (VisualViewportSuppliesScrollbars()) {
-    scrollbar_manager_.SetHasHorizontalScrollbar(false);
-    scrollbar_manager_.SetHasVerticalScrollbar(false);
+    if (scrollbar_manager_.HasHorizontalScrollbar() ||
+        scrollbar_manager_.HasVerticalScrollbar()) {
+      scrollbar_manager_.SetHasHorizontalScrollbar(false);
+      scrollbar_manager_.SetHasVerticalScrollbar(false);
+      ScrollbarExistenceDidChange();
+    }
     AdjustScrollOffsetFromUpdateScrollbars();
     return;
   }
