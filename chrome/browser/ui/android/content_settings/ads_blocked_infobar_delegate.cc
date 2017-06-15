e9636465a27748af5fb1f8bf61e150c8ac37277a
Charles Harrison <csharrison@chromium.org>
2017-06-15 16:40:02 -0400

    [subresource_filter] Replace the infobar with the experimental infobar
    This CL replaces the subresource filter infobar with an "AdsBlockedInfobar"
    and updates strings accordingly. This infobar will be used on master for
    filtering on pages just after a safe browsing interstitial. Other uses
    (e.g. for blocking more generally on sites which show intrusive ads)
    is still behind a flag
    
    Bug: 689992
    Change-Id: I62bf99e3f67e4a32fec8820f5bdad0b93da3d17e
    Reviewed-on: https://chromium-review.googlesource.com/526092
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Dan Alcantara <dfalcantara@chromium.org>
    Reviewed-by: Bryan McQuade <bmcquade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479864}
    Former-commit-id: a18f38c607db22eed956aff833f70ad246bc6ea6

---
+++ chrome/browser/ui/android/content_settings/ads_blocked_infobar_delegate.cc
@@ -0,0 +1,79 @@
+// Copyright (c) 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/android/content_settings/ads_blocked_infobar_delegate.h"
+
+#include "base/memory/ptr_util.h"
+#include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/android/android_theme_resources.h"
+#include "chrome/browser/infobars/infobar_service.h"
+#include "chrome/browser/subresource_filter/chrome_subresource_filter_client.h"
+#include "chrome/browser/ui/android/infobars/ads_blocked_infobar.h"
+#include "chrome/grit/generated_resources.h"
+#include "components/infobars/core/infobar.h"
+#include "components/strings/grit/components_strings.h"
+#include "components/subresource_filter/core/browser/subresource_filter_features.h"
+#include "content/public/browser/web_contents.h"
+#include "ui/base/l10n/l10n_util.h"
+
+// static
+void AdsBlockedInfobarDelegate::Create(InfoBarService* infobar_service) {
+  infobar_service->AddInfoBar(base::MakeUnique<AdsBlockedInfoBar>(
+      base::WrapUnique(new AdsBlockedInfobarDelegate())));
+}
+
+AdsBlockedInfobarDelegate::AdsBlockedInfobarDelegate()
+    : ConfirmInfoBarDelegate() {}
+
+AdsBlockedInfobarDelegate::~AdsBlockedInfobarDelegate() {}
+
+base::string16 AdsBlockedInfobarDelegate::GetExplanationText() const {
+  return l10n_util::GetStringUTF16(IDS_BLOCKED_ADS_PROMPT_EXPLANATION);
+}
+
+// The experimental UI includes the permission and its associated UI. Without it
+// we can't say "Always". Allowing ads will only be scoped to the tab.
+base::string16 AdsBlockedInfobarDelegate::GetToggleText() const {
+  return base::FeatureList::IsEnabled(
+             subresource_filter::kSafeBrowsingSubresourceFilterExperimentalUI)
+             ? l10n_util::GetStringUTF16(IDS_ALWAYS_SHOW_ADS)
+             : l10n_util::GetStringUTF16(IDS_SHOW_ADS);
+}
+
+infobars::InfoBarDelegate::InfoBarIdentifier
+AdsBlockedInfobarDelegate::GetIdentifier() const {
+  return ADS_BLOCKED_INFOBAR_DELEGATE_ANDROID;
+}
+
+int AdsBlockedInfobarDelegate::GetIconId() const {
+  return IDR_ANDROID_INFOBAR_SUBRESOURCE_FILTERING;
+}
+
+base::string16 AdsBlockedInfobarDelegate::GetMessageText() const {
+  return l10n_util::GetStringUTF16(IDS_BLOCKED_ADS_PROMPT_TITLE);
+}
+
+int AdsBlockedInfobarDelegate::GetButtons() const {
+  return BUTTON_OK | BUTTON_CANCEL;
+}
+
+base::string16 AdsBlockedInfobarDelegate::GetButtonLabel(
+    InfoBarButton button) const {
+  if (button == BUTTON_OK)
+    return l10n_util::GetStringUTF16(IDS_OK);
+  return l10n_util::GetStringUTF16(IDS_APP_MENU_RELOAD);
+}
+
+bool AdsBlockedInfobarDelegate::Cancel() {
+  content::WebContents* web_contents =
+      InfoBarService::WebContentsFromInfoBar(infobar());
+  ChromeSubresourceFilterClient::FromWebContents(web_contents)
+      ->OnReloadRequested();
+  return true;
+}
+
+bool AdsBlockedInfobarDelegate::LinkClicked(WindowOpenDisposition disposition) {
+  ChromeSubresourceFilterClient::LogAction(kActionDetailsShown);
+  return false;
+}
