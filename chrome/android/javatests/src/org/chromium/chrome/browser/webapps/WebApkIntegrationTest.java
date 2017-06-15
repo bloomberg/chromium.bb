f4984e7880c96405e1c5fe43bc5c916886f97a3f
pkotwicz <pkotwicz@chromium.org>
2017-06-15 08:22:08 -0700

    [Android] Remove RetryOnFailure from WebApkIntegrationTest
    It is likely that the source of flakiness was fixed by
    https://codereview.chromium.org/2859053004
    
    BUG=708523
    
    Review-Url: https://codereview.chromium.org/2935903002
    Cr-Commit-Position: refs/heads/master@{#479710}
    
    Former-commit-id: 3ac2c353dc23284cca32db9ca76a21567f77e088

--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkIntegrationTest.java
@@ -16,7 +16,6 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
-import org.chromium.base.test.util.RetryOnFailure;
 import org.chromium.base.test.util.ScalableTimeout;
 import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
@@ -100,7 +99,6 @@ public class WebApkIntegrationTest {
     @Test
     @LargeTest
     @Feature({"WebApk"})
-    @RetryOnFailure
     public void testLaunchAndNavigateOffOrigin() throws Exception {
         startWebApkActivity("org.chromium.webapk.test",
                 mTestServer.getURL("/chrome/test/data/android/test.html"));
