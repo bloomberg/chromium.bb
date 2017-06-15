0cb46996924ef137f9f796f0a6e52b27fa5e0866
mmenke <mmenke@chromium.org>
2017-06-15 10:37:24 -0700

    Fix QuicHttpStream reporting negative sent bytes.
    It was treating error codes as number of header bytes sent.
    This may have affected metrics slightly, not sure if it had any
    had any web-visible ordevtools-visible effects.
    
    BUG=731819
    
    Review-Url: https://codereview.chromium.org/2937113002
    Cr-Commit-Position: refs/heads/master@{#479749}
    
    Former-commit-id: ffff364f06b3fa261dcf249ded47675c2d0d48a3

--- net/quic/chromium/quic_http_stream_test.cc
+++ net/quic/chromium/quic_http_stream_test.cc
@@ -1530,6 +1530,9 @@ TEST_P(QuicHttpStreamTest, SessionClosedBeforeSendHeadersComplete) {
                                       net_log_.bound(), callback_.callback()));
   ASSERT_EQ(ERR_QUIC_PROTOCOL_ERROR,
             stream_->SendRequest(headers_, &response_, callback_.callback()));
+
+  EXPECT_LE(0, stream_->GetTotalSentBytes());
+  EXPECT_EQ(0, stream_->GetTotalReceivedBytes());
 }
 
 TEST_P(QuicHttpStreamTest, SessionClosedBeforeSendBodyComplete) {
