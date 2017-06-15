49b8d2c4667be875ed3683962b062c6062543f46
Hiroki Nakagawa <nhiroki@chromium.org>
2017-06-15 11:08:24 +0900

    ServiceWorker: Merge ServiceWorkerGlobalScopeClientImpl into ServiceWorkerGlobalScopeClient
    This is a follow-up for [1] that moved
    ServiceWorkerGlobalScopeClientImpl.{cpp,h} from web/ to modules/serviceworkers/.
    After the move, these classes don't have to be separated anymore.
    
    [1] https://chromium-review.googlesource.com/c/535302/
    
    Bug: n/a
    Change-Id: I9a299fa255649b199ca0d9564bfa054987247262
    Reviewed-on: https://chromium-review.googlesource.com/536674
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#479626}
    Former-commit-id: 4aa51ba80f162e442ed23e846b1a6135ab889d6b

--- third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerGlobalScopeClient.cpp
+++ third_party/WebKit/Source/modules/serviceworkers/ServiceWorkerGlobalScopeClient.cpp
@@ -30,10 +30,225 @@
 
 #include "modules/serviceworkers/ServiceWorkerGlobalScopeClient.h"
 
+#include <memory>
+#include <utility>
 #include "core/dom/ExecutionContext.h"
 #include "core/workers/WorkerGlobalScope.h"
+#include "modules/fetch/Response.h"
+#include "public/platform/WebURL.h"
+#include "public/platform/modules/payments/WebPaymentAppResponse.h"
+#include "public/platform/modules/serviceworker/WebServiceWorkerResponse.h"
+#include "public/web/modules/serviceworker/WebServiceWorkerContextClient.h"
 
 namespace blink {
+
+ServiceWorkerGlobalScopeClient::ServiceWorkerGlobalScopeClient(
+    WebServiceWorkerContextClient& client)
+    : client_(client) {}
+
+void ServiceWorkerGlobalScopeClient::GetClient(
+    const WebString& id,
+    std::unique_ptr<WebServiceWorkerClientCallbacks> callbacks) {
+  client_.GetClient(id, std::move(callbacks));
+}
+
+void ServiceWorkerGlobalScopeClient::GetClients(
+    const WebServiceWorkerClientQueryOptions& options,
+    std::unique_ptr<WebServiceWorkerClientsCallbacks> callbacks) {
+  client_.GetClients(options, std::move(callbacks));
+}
+
+void ServiceWorkerGlobalScopeClient::OpenWindowForClients(
+    const WebURL& url,
+    std::unique_ptr<WebServiceWorkerClientCallbacks> callbacks) {
+  client_.OpenNewTab(url, std::move(callbacks));
+}
+
+void ServiceWorkerGlobalScopeClient::OpenWindowForPaymentHandler(
+    const WebURL& url,
+    std::unique_ptr<WebServiceWorkerClientCallbacks> callbacks) {
+  client_.OpenNewPopup(url, std::move(callbacks));
+}
+
+void ServiceWorkerGlobalScopeClient::SetCachedMetadata(const WebURL& url,
+                                                       const char* data,
+                                                       size_t size) {
+  client_.SetCachedMetadata(url, data, size);
+}
+
+void ServiceWorkerGlobalScopeClient::ClearCachedMetadata(const WebURL& url) {
+  client_.ClearCachedMetadata(url);
+}
+
+WebURL ServiceWorkerGlobalScopeClient::Scope() const {
+  return client_.Scope();
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleActivateEvent(
+    int event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleActivateEvent(event_id, result, event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleBackgroundFetchAbortEvent(
+    int event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleBackgroundFetchAbortEvent(event_id, result,
+                                             event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleBackgroundFetchClickEvent(
+    int event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleBackgroundFetchClickEvent(event_id, result,
+                                             event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleBackgroundFetchFailEvent(
+    int event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleBackgroundFetchFailEvent(event_id, result,
+                                            event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleBackgroundFetchedEvent(
+    int event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleBackgroundFetchedEvent(event_id, result,
+                                          event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleExtendableMessageEvent(
+    int event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleExtendableMessageEvent(event_id, result,
+                                          event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::RespondToFetchEventWithNoResponse(
+    int fetch_event_id,
+    double event_dispatch_time) {
+  client_.RespondToFetchEventWithNoResponse(fetch_event_id,
+                                            event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::RespondToFetchEvent(
+    int fetch_event_id,
+    const WebServiceWorkerResponse& response,
+    double event_dispatch_time) {
+  client_.RespondToFetchEvent(fetch_event_id, response, event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::RespondToFetchEventWithResponseStream(
+    int fetch_event_id,
+    const WebServiceWorkerResponse& response,
+    WebServiceWorkerStreamHandle* stream_handle,
+    double event_dispatch_time) {
+  client_.RespondToFetchEventWithResponseStream(
+      fetch_event_id, response, stream_handle, event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::RespondToPaymentRequestEvent(
+    int event_id,
+    const WebPaymentAppResponse& response,
+    double event_dispatch_time) {
+  client_.RespondToPaymentRequestEvent(event_id, response, event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleFetchEvent(
+    int fetch_event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleFetchEvent(fetch_event_id, result, event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleInstallEvent(
+    int install_event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleInstallEvent(install_event_id, result, event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleNotificationClickEvent(
+    int event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleNotificationClickEvent(event_id, result,
+                                          event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleNotificationCloseEvent(
+    int event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleNotificationCloseEvent(event_id, result,
+                                          event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandlePushEvent(
+    int push_event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandlePushEvent(push_event_id, result, event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandleSyncEvent(
+    int sync_event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandleSyncEvent(sync_event_id, result, event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::DidHandlePaymentRequestEvent(
+    int payment_request_event_id,
+    WebServiceWorkerEventResult result,
+    double event_dispatch_time) {
+  client_.DidHandlePaymentRequestEvent(payment_request_event_id, result,
+                                       event_dispatch_time);
+}
+
+void ServiceWorkerGlobalScopeClient::PostMessageToClient(
+    const WebString& client_uuid,
+    const WebString& message,
+    WebMessagePortChannelArray web_channels) {
+  client_.PostMessageToClient(client_uuid, message, std::move(web_channels));
+}
+
+void ServiceWorkerGlobalScopeClient::SkipWaiting(
+    std::unique_ptr<WebServiceWorkerSkipWaitingCallbacks> callbacks) {
+  client_.SkipWaiting(std::move(callbacks));
+}
+
+void ServiceWorkerGlobalScopeClient::Claim(
+    std::unique_ptr<WebServiceWorkerClientsClaimCallbacks> callbacks) {
+  client_.Claim(std::move(callbacks));
+}
+
+void ServiceWorkerGlobalScopeClient::Focus(
+    const WebString& client_uuid,
+    std::unique_ptr<WebServiceWorkerClientCallbacks> callback) {
+  client_.Focus(client_uuid, std::move(callback));
+}
+
+void ServiceWorkerGlobalScopeClient::Navigate(
+    const WebString& client_uuid,
+    const WebURL& url,
+    std::unique_ptr<WebServiceWorkerClientCallbacks> callback) {
+  client_.Navigate(client_uuid, url, std::move(callback));
+}
+
+void ServiceWorkerGlobalScopeClient::RegisterForeignFetchScopes(
+    int install_event_id,
+    const WebVector<WebURL>& sub_scopes,
+    const WebVector<WebSecurityOrigin>& origins) {
+  client_.RegisterForeignFetchScopes(install_event_id, sub_scopes, origins);
+}
 
 const char* ServiceWorkerGlobalScopeClient::SupplementName() {
   return "ServiceWorkerGlobalScopeClient";
@@ -45,6 +260,10 @@ ServiceWorkerGlobalScopeClient* ServiceWorkerGlobalScopeClient::From(
   DCHECK(clients);
   return static_cast<ServiceWorkerGlobalScopeClient*>(
       Supplement<WorkerClients>::From(clients, SupplementName()));
+}
+
+DEFINE_TRACE(ServiceWorkerGlobalScopeClient) {
+  Supplement<WorkerClients>::Trace(visitor);
 }
 
 void ProvideServiceWorkerGlobalScopeClientToWorker(
