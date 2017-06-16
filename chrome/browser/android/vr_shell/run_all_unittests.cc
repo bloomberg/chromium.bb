58eacbd383a59804ac0d2ad4796a62c3b1d995f0
cjgrant <cjgrant@chromium.org>
2017-06-16 12:01:38 -0700

    VR: Support loading string resources in unit tests.
    To this point, we haven't needed to test code that relies on translated
    strings.  The URL bar is about to require such a string, so we need to
    include resource capabilities in the test target.
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2937263002
    Cr-Commit-Position: refs/heads/master@{#480115}
    
    Former-commit-id: 4d456ff29175aef972c8f24554ec1529afbfe2a7

---
+++ chrome/browser/android/vr_shell/run_all_unittests.cc
@@ -0,0 +1,52 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/bind.h"
+#include "base/files/file_util.h"
+#include "base/path_service.h"
+#include "base/test/launcher/unit_test_launcher.h"
+#include "base/test/test_suite.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/base/ui_base_paths.h"
+
+namespace {
+
+class VrCommonTestSuite : public base::TestSuite {
+ public:
+  VrCommonTestSuite(int argc, char** argv) : base::TestSuite(argc, argv) {}
+
+ protected:
+  void Initialize() override {
+    base::TestSuite::Initialize();
+
+    ui::RegisterPathProvider();
+
+    base::FilePath pak_path;
+#if defined(OS_ANDROID)
+    PathService::Get(ui::DIR_RESOURCE_PAKS_ANDROID, &pak_path);
+#else
+    PathService::Get(base::DIR_MODULE, &pak_path);
+#endif
+    ui::ResourceBundle::InitSharedInstanceWithPakPath(
+        pak_path.AppendASCII("components_tests_resources.pak"));
+  }
+
+  void Shutdown() override {
+    ui::ResourceBundle::CleanupSharedInstance();
+    base::TestSuite::Shutdown();
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(VrCommonTestSuite);
+};
+
+}  // namespace
+
+int main(int argc, char** argv) {
+  VrCommonTestSuite test_suite(argc, argv);
+
+  return base::LaunchUnitTests(
+      argc, argv,
+      base::Bind(&VrCommonTestSuite::Run, base::Unretained(&test_suite)));
+}
