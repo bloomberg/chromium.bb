2f2bc12dac9851d44c2ec156924198152b383a1d
yosin <yosin@chromium.org>
2017-06-15 18:49:24 -0700

    Get rid of redundant block scope from ComputeInlineBoxPositionForTextNode()
    This patch gets rid of redundant block scope from
    |ComputeInlineBoxPositionForTextNode()| for improving code health.
    
    BUG=707656
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2938923004
    Cr-Commit-Position: refs/heads/master@{#479899}
    
    Former-commit-id: e7b173d3f09519dccd5cd6fbc1d55bd300a895ae

--- third_party/WebKit/Source/core/editing/VisibleUnits.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnits.cpp
@@ -839,41 +839,37 @@ static InlineBoxPosition ComputeInlineBoxPositionForTextNode(
     int caret_offset,
     TextAffinity affinity,
     TextDirection primary_direction) {
-  // TODO(yosin): We should move this code fragment to another function to
-  // remove the scope.
   InlineBox* inline_box = nullptr;
-  {
-    LayoutText* text_layout_object = ToLayoutText(layout_object);
+  LayoutText* text_layout_object = ToLayoutText(layout_object);
 
-    InlineTextBox* candidate = nullptr;
+  InlineTextBox* candidate = nullptr;
 
-    for (InlineTextBox* box : InlineTextBoxesOf(*text_layout_object)) {
-      int caret_min_offset = box->CaretMinOffset();
-      int caret_max_offset = box->CaretMaxOffset();
+  for (InlineTextBox* box : InlineTextBoxesOf(*text_layout_object)) {
+    int caret_min_offset = box->CaretMinOffset();
+    int caret_max_offset = box->CaretMaxOffset();
 
-      if (caret_offset < caret_min_offset || caret_offset > caret_max_offset ||
-          (caret_offset == caret_max_offset && box->IsLineBreak()))
-        continue;
+    if (caret_offset < caret_min_offset || caret_offset > caret_max_offset ||
+        (caret_offset == caret_max_offset && box->IsLineBreak()))
+      continue;
 
-      if (caret_offset > caret_min_offset && caret_offset < caret_max_offset)
-        return InlineBoxPosition(box, caret_offset);
+    if (caret_offset > caret_min_offset && caret_offset < caret_max_offset)
+      return InlineBoxPosition(box, caret_offset);
 
-      if (IsCaretAtEdgeOfInlineTextBox(caret_offset, *box, affinity)) {
-        inline_box = box;
-        break;
-      }
-
-      candidate = box;
+    if (IsCaretAtEdgeOfInlineTextBox(caret_offset, *box, affinity)) {
+      inline_box = box;
+      break;
     }
-    if (candidate && candidate == text_layout_object->LastTextBox() &&
-        affinity == TextAffinity::kDownstream) {
-      inline_box = SearchAheadForBetterMatch(text_layout_object);
-      if (inline_box)
-        caret_offset = inline_box->CaretMinOffset();
-    }
-    if (!inline_box)
-      inline_box = candidate;
+
+    candidate = box;
   }
+  if (candidate && candidate == text_layout_object->LastTextBox() &&
+      affinity == TextAffinity::kDownstream) {
+    inline_box = SearchAheadForBetterMatch(text_layout_object);
+    if (inline_box)
+      caret_offset = inline_box->CaretMinOffset();
+  }
+  if (!inline_box)
+    inline_box = candidate;
 
   if (!inline_box)
     return InlineBoxPosition();
