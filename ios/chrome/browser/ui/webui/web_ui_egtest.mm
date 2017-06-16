3fa194dc52652446e42220aba7d652327aa1ebcf
Elodie Banel <lod@google.com>
2017-06-16 17:08:59 +0200

    Disable webui egtest testPhysicalWeb and testVersion
    The iOS implementation oh webUI does not support i18n source stream
    filtering. As a result, https://codereview.chromium.org/2908353003
    breaks the version page in iOS. Because this is not a CL from the iOS
    team, it's not being reverted, but the tests being disabled is a
    temporary measure; either the CL must be reverted or the i18n change
    added to the ios webui implementation.
    
    (see linked bug for more information)
    
    Bug: 734079
    Change-Id: Ie3a2a3896baf59d065b398d056ca7f8a7b5bb901
    TBR: michaeldo@chromium.org
    Reviewed-on: https://chromium-review.googlesource.com/538755
    Commit-Queue: Elodie Banel <lod@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480049}
    Former-commit-id: 2b7bf9311f65ccdc06f992be97dddbacbc71475c

--- ios/chrome/browser/ui/webui/web_ui_egtest.mm
+++ ios/chrome/browser/ui/webui/web_ui_egtest.mm
@@ -81,7 +81,8 @@ id<GREYMatcher> WaitForOmniboxText(std::string text) {
 
 // Tests that chrome://version renders and contains correct version number and
 // user agent string.
-- (void)testVersion {
+// TODO(crbug.com/734079): Re-enable this test.
+- (void)DISABLED_testVersion {
   LoadWebUIUrl(kChromeUIVersionHost);
 
   // Verify that app version is present on the page.
@@ -95,7 +96,8 @@ id<GREYMatcher> WaitForOmniboxText(std::string text) {
 }
 
 // Tests that chrome://physical-web renders and the page title is present.
-- (void)testPhysicalWeb {
+// TODO(crbug.com/734079): Re-enable this test.
+- (void)DISABLED_testPhysicalWeb {
   // Enable the Physical Web via Chrome variation.
   base::FieldTrialList::CreateFieldTrial("PhysicalWebEnabled", "Enabled");
 
