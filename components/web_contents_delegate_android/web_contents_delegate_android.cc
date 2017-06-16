f2890ba5428c2543cf140b8bced827c977be2bd6
thildebr <thildebr@chromium.org>
2017-06-16 12:38:37 -0700

    Pass user gesture flag when opening a new tab on Android.
    What was thought to be a bug related to rel=noopener turned out to be a bug
    with opening new tabs after all (oops.) User gestures were not being passed
    through via LoadURLParams on Android, which causes issues with launching apps
    when necessary via links with target=_blank.
    
    BUG=729187,728346
    
    Review-Url: https://codereview.chromium.org/2942163003
    Cr-Commit-Position: refs/heads/master@{#480124}
    
    Former-commit-id: b0d04772edfcaf49e8bfc909f8ed1596e8d46184

--- components/web_contents_delegate_android/web_contents_delegate_android.cc
+++ components/web_contents_delegate_android/web_contents_delegate_android.cc
@@ -110,6 +110,7 @@ WebContents* WebContentsDelegateAndroid::OpenURLFromTab(
   load_params.should_replace_current_entry =
       params.should_replace_current_entry;
   load_params.is_renderer_initiated = params.is_renderer_initiated;
+  load_params.has_user_gesture = params.user_gesture;
 
   if (params.uses_post) {
     load_params.load_type = content::NavigationController::LOAD_TYPE_HTTP_POST;
