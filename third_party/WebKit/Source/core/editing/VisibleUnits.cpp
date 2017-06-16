3631575224b36ffd2750a7fb9aea0e3e50a40103
yosin <yosin@chromium.org>
2017-06-16 00:56:42 -0700

    2941213002: Get rid of forward declaration of ComputeInlineBoxPositionForTextNode()
    This patch moves implementation of |ComputeInlineBoxPositionForTextNode()| at
    forward declaration to get rid of forward declaration for improving code health.
    
    This is follow-up the patch[1] which inttroduces it.
    
    [1] http://crrev.com/2941013003 Introduce ComputeInlineBoxPositionForTextNode()
    
    BUG=707656
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2941213002
    Cr-Commit-Position: refs/heads/master@{#479986}
    
    Former-commit-id: f27177df9fa6d949bd33fd6095f8c8a1481b1890

--- third_party/WebKit/Source/core/editing/VisibleUnits.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnits.cpp
@@ -770,12 +770,49 @@ static bool IsCaretAtEdgeOfInlineTextBox(int caret_offset,
   return box.NextLeafChild() && box.NextLeafChild()->IsLineBreak();
 }
 
-// TODO(yosin): We have a forward declaration here to reduce patch size. We'll
-// replace this with an implementation once review finished.
-static InlineBoxPosition ComputeInlineBoxPositionForTextNode(LayoutObject*,
-                                                             int caret_offset,
-                                                             TextAffinity,
-                                                             TextDirection);
+static InlineBoxPosition ComputeInlineBoxPositionForTextNode(
+    LayoutObject* layout_object,
+    int caret_offset,
+    TextAffinity affinity,
+    TextDirection primary_direction) {
+  InlineBox* inline_box = nullptr;
+  LayoutText* text_layout_object = ToLayoutText(layout_object);
+
+  InlineTextBox* candidate = nullptr;
+
+  for (InlineTextBox* box : InlineTextBoxesOf(*text_layout_object)) {
+    int caret_min_offset = box->CaretMinOffset();
+    int caret_max_offset = box->CaretMaxOffset();
+
+    if (caret_offset < caret_min_offset || caret_offset > caret_max_offset ||
+        (caret_offset == caret_max_offset && box->IsLineBreak()))
+      continue;
+
+    if (caret_offset > caret_min_offset && caret_offset < caret_max_offset)
+      return InlineBoxPosition(box, caret_offset);
+
+    if (IsCaretAtEdgeOfInlineTextBox(caret_offset, *box, affinity)) {
+      inline_box = box;
+      break;
+    }
+
+    candidate = box;
+  }
+  if (candidate && candidate == text_layout_object->LastTextBox() &&
+      affinity == TextAffinity::kDownstream) {
+    inline_box = SearchAheadForBetterMatch(text_layout_object);
+    if (inline_box)
+      caret_offset = inline_box->CaretMinOffset();
+  }
+  if (!inline_box)
+    inline_box = candidate;
+
+  if (!inline_box)
+    return InlineBoxPosition();
+  return AdjustInlineBoxPositionForTextDirection(
+      inline_box, caret_offset, layout_object->Style()->GetUnicodeBidi(),
+      primary_direction);
+}
 
 template <typename Strategy>
 static InlineBoxPosition ComputeInlineBoxPositionTemplate(
@@ -831,52 +868,6 @@ static InlineBoxPosition ComputeInlineBoxPositionTemplate(
 
   return ComputeInlineBoxPositionForTextNode(layout_object, caret_offset,
                                              affinity, primary_direction);
-}
-
-// TODO(yosin): We should make |ComputeInlineBoxPositionForTextNode()| to take
-// |const LayoutText&|.
-static InlineBoxPosition ComputeInlineBoxPositionForTextNode(
-    LayoutObject* layout_object,
-    int caret_offset,
-    TextAffinity affinity,
-    TextDirection primary_direction) {
-  InlineBox* inline_box = nullptr;
-  LayoutText* text_layout_object = ToLayoutText(layout_object);
-
-  InlineTextBox* candidate = nullptr;
-
-  for (InlineTextBox* box : InlineTextBoxesOf(*text_layout_object)) {
-    int caret_min_offset = box->CaretMinOffset();
-    int caret_max_offset = box->CaretMaxOffset();
-
-    if (caret_offset < caret_min_offset || caret_offset > caret_max_offset ||
-        (caret_offset == caret_max_offset && box->IsLineBreak()))
-      continue;
-
-    if (caret_offset > caret_min_offset && caret_offset < caret_max_offset)
-      return InlineBoxPosition(box, caret_offset);
-
-    if (IsCaretAtEdgeOfInlineTextBox(caret_offset, *box, affinity)) {
-      inline_box = box;
-      break;
-    }
-
-    candidate = box;
-  }
-  if (candidate && candidate == text_layout_object->LastTextBox() &&
-      affinity == TextAffinity::kDownstream) {
-    inline_box = SearchAheadForBetterMatch(text_layout_object);
-    if (inline_box)
-      caret_offset = inline_box->CaretMinOffset();
-  }
-  if (!inline_box)
-    inline_box = candidate;
-
-  if (!inline_box)
-    return InlineBoxPosition();
-  return AdjustInlineBoxPositionForTextDirection(
-      inline_box, caret_offset, layout_object->Style()->GetUnicodeBidi(),
-      primary_direction);
 }
 
 static InlineBoxPosition AdjustInlineBoxPositionForTextDirection(
