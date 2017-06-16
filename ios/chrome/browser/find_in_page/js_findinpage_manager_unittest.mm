adc56d382c5f2fe93cdc90cebb7245e1ad210633
marq <marq@chromium.org>
2017-06-16 06:27:08 -0700

    [ObjC ARC] Converts ios/chrome/browser/find_in_page:unit_tests_nonarc to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2941053002
    Cr-Commit-Position: refs/heads/master@{#480027}
    
    Former-commit-id: 918180286a5a098ab0bc590d38720bbde3de53f3

--- ios/chrome/browser/find_in_page/js_findinpage_manager_unittest.mm
+++ ios/chrome/browser/find_in_page/js_findinpage_manager_unittest.mm
@@ -12,6 +12,10 @@
 #import "ios/web/public/web_state/web_state.h"
 #import "testing/gtest_mac.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 // Test fixture to test Find In Page JS.
 class JsFindinpageManagerTest : public ChromeWebTest {
  protected:
