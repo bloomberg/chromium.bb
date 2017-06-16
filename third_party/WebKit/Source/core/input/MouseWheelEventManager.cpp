a4d39bc17f78ace187db1906a31d31ca9e02356a
sahel <sahel@chromium.org>
2017-06-16 08:32:22 -0700

    Re-target wheel events only when a new scroll sequence has started.
    In RouteMouseWheelEvent, when a wheel event with phase began arrives,
    find the target for wheel event. While no wheel with phase end has
    arrived (during a scroll sequence) send the wheel events to the saved
    target.
    
    In EventHandler, do the same targetting to dispatch the events to js.
    
    BUG=526463
    TEST=WheelScrollLatchingBrowserTest.WheelEventTarget,
         WheelScrollLatchingDisabledBrowserTest.WheelEventTarget
    
    Review-Url: https://codereview.chromium.org/2928793003
    Cr-Commit-Position: refs/heads/master@{#480052}
    
    Former-commit-id: 8aa8c38005ee36bc441138c81a5cf165fb6514a5

--- third_party/WebKit/Source/core/input/MouseWheelEventManager.cpp
+++ third_party/WebKit/Source/core/input/MouseWheelEventManager.cpp
@@ -18,37 +18,100 @@
 namespace blink {
 MouseWheelEventManager::MouseWheelEventManager(LocalFrame& frame,
                                                ScrollManager& scroll_manager)
-    : frame_(frame), scroll_manager_(scroll_manager) {}
+    : frame_(frame), wheel_target_(nullptr), scroll_manager_(scroll_manager) {}
 
 DEFINE_TRACE(MouseWheelEventManager) {
   visitor->Trace(frame_);
+  visitor->Trace(wheel_target_);
   visitor->Trace(scroll_manager_);
+}
+
+void MouseWheelEventManager::Clear() {
+  wheel_target_ = nullptr;
 }
 
 WebInputEventResult MouseWheelEventManager::HandleWheelEvent(
     const WebMouseWheelEvent& event) {
-#if OS(MACOSX)
-  // Filter Mac OS specific phases, usually with a zero-delta.
-  // https://crbug.com/553732
-  // TODO(chongz): EventSender sends events with
-  // |WebMouseWheelEvent::PhaseNone|,
-  // but it shouldn't.
-  const int kWheelEventPhaseNoEventMask = WebMouseWheelEvent::kPhaseEnded |
-                                          WebMouseWheelEvent::kPhaseCancelled |
-                                          WebMouseWheelEvent::kPhaseMayBegin;
-  if ((event.phase & kWheelEventPhaseNoEventMask) ||
-      (event.momentum_phase & kWheelEventPhaseNoEventMask))
-    return WebInputEventResult::kNotHandled;
-#endif
-  Document* doc = frame_->GetDocument();
+  bool wheel_scroll_latching =
+      RuntimeEnabledFeatures::TouchpadAndWheelScrollLatchingEnabled();
 
-  if (doc->GetLayoutViewItem().IsNull())
+  Document* doc = frame_->GetDocument();
+  if (!doc || doc->GetLayoutViewItem().IsNull())
     return WebInputEventResult::kNotHandled;
 
   LocalFrameView* view = frame_->View();
   if (!view)
     return WebInputEventResult::kNotHandled;
 
+  if (wheel_scroll_latching) {
+    const int kWheelEventPhaseEndedEventMask =
+        WebMouseWheelEvent::kPhaseEnded | WebMouseWheelEvent::kPhaseCancelled;
+    const int kWheelEventPhaseNoEventMask =
+        kWheelEventPhaseEndedEventMask | WebMouseWheelEvent::kPhaseMayBegin;
+
+    if ((event.phase & kWheelEventPhaseEndedEventMask) ||
+        (event.momentum_phase & kWheelEventPhaseEndedEventMask)) {
+      wheel_target_ = nullptr;
+    }
+
+    if ((event.phase & kWheelEventPhaseNoEventMask) ||
+        (event.momentum_phase & kWheelEventPhaseNoEventMask)) {
+      // Filter wheel events with zero deltas and reset the wheel_target_ node.
+      DCHECK(!event.delta_x && !event.delta_y);
+      return WebInputEventResult::kNotHandled;
+    }
+
+    if (event.phase == WebMouseWheelEvent::kPhaseBegan) {
+      // Find and save the wheel_target_, this target will be used for the rest
+      // of the current scrolling sequence.
+      DCHECK(!wheel_target_);
+      wheel_target_ = FindTargetNode(event, doc, view);
+    }
+  } else {  // !wheel_scroll_latching, wheel_target_ will be updated for each
+            // wheel event.
+#if OS(MACOSX)
+    // Filter Mac OS specific phases, usually with a zero-delta.
+    // https://crbug.com/553732
+    // TODO(chongz): EventSender sends events with
+    // |WebMouseWheelEvent::PhaseNone|,
+    // but it shouldn't.
+    const int kWheelEventPhaseNoEventMask =
+        WebMouseWheelEvent::kPhaseEnded | WebMouseWheelEvent::kPhaseCancelled |
+        WebMouseWheelEvent::kPhaseMayBegin;
+    if ((event.phase & kWheelEventPhaseNoEventMask) ||
+        (event.momentum_phase & kWheelEventPhaseNoEventMask))
+      return WebInputEventResult::kNotHandled;
+#endif
+
+    wheel_target_ = FindTargetNode(event, doc, view);
+  }
+
+  LocalFrame* subframe =
+      EventHandlingUtil::SubframeForTargetNode(wheel_target_.Get());
+  if (subframe) {
+    WebInputEventResult result =
+        subframe->GetEventHandler().HandleWheelEvent(event);
+    if (result != WebInputEventResult::kNotHandled)
+      scroll_manager_->SetFrameWasScrolledByUser();
+    return result;
+  }
+
+  if (wheel_target_) {
+    WheelEvent* dom_event =
+        WheelEvent::Create(event, wheel_target_->GetDocument().domWindow());
+    DispatchEventResult dom_event_result =
+        wheel_target_->DispatchEvent(dom_event);
+    if (dom_event_result != DispatchEventResult::kNotCanceled)
+      return EventHandlingUtil::ToWebInputEventResult(dom_event_result);
+  }
+
+  return WebInputEventResult::kNotHandled;
+}
+
+Node* MouseWheelEventManager::FindTargetNode(const WebMouseWheelEvent& event,
+                                             const Document* doc,
+                                             const LocalFrameView* view) {
+  DCHECK(doc && !doc->GetLayoutViewItem().IsNull() && view);
   LayoutPoint v_point =
       view->RootFrameToContents(FlooredIntPoint(event.PositionInRootFrame()));
 
@@ -65,24 +128,7 @@ WebInputEventResult MouseWheelEventManager::HandleWheelEvent(
   if (!node && result.GetScrollbar())
     node = doc->documentElement();
 
-  LocalFrame* subframe = EventHandlingUtil::SubframeForTargetNode(node);
-  if (subframe) {
-    WebInputEventResult result =
-        subframe->GetEventHandler().HandleWheelEvent(event);
-    if (result != WebInputEventResult::kNotHandled)
-      scroll_manager_->SetFrameWasScrolledByUser();
-    return result;
-  }
-
-  if (node) {
-    WheelEvent* dom_event =
-        WheelEvent::Create(event, node->GetDocument().domWindow());
-    DispatchEventResult dom_event_result = node->DispatchEvent(dom_event);
-    if (dom_event_result != DispatchEventResult::kNotCanceled)
-      return EventHandlingUtil::ToWebInputEventResult(dom_event_result);
-  }
-
-  return WebInputEventResult::kNotHandled;
+  return node;
 }
 
 }  // namespace blink
