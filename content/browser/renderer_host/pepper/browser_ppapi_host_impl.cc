66d6a3915d937511498678b8be7f4b1417592b8c
Bill Budge <bbudge@chromium.org>
2017-06-19 16:14:57 -0700

    Validate in-process plugin instance messages.
    Bug: 733548, 733549
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ie5572c7bcafa05399b09c44425ddd5ce9b9e4cba
    Reviewed-on: https://chromium-review.googlesource.com/538908
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480696}
    Former-commit-id: 66b99f3fe60dce77f079cc9c07164f6a34dbea37

--- content/browser/renderer_host/pepper/browser_ppapi_host_impl.cc
+++ content/browser/renderer_host/pepper/browser_ppapi_host_impl.cc
@@ -148,24 +148,37 @@ bool BrowserPpapiHostImpl::IsPotentiallySecurePluginContext(
 void BrowserPpapiHostImpl::AddInstance(
     PP_Instance instance,
     const PepperRendererInstanceData& renderer_instance_data) {
-  DCHECK(instance_map_.find(instance) == instance_map_.end());
-  instance_map_[instance] =
-      base::MakeUnique<InstanceData>(renderer_instance_data);
+  // NOTE: 'instance' may be coming from a compromised renderer process. We
+  // take care here to make sure an attacker can't overwrite data for an
+  // existing plugin instance.
+  // See http://crbug.com/733548.
+  if (instance_map_.find(instance) == instance_map_.end()) {
+    instance_map_[instance] =
+        base::MakeUnique<InstanceData>(renderer_instance_data);
+  } else {
+    NOTREACHED();
+  }
 }
 
 void BrowserPpapiHostImpl::DeleteInstance(PP_Instance instance) {
+  // NOTE: 'instance' may be coming from a compromised renderer process. We
+  // take care here to make sure an attacker can't cause a UAF by deleting a
+  // non-existent plugin instance.
+  // See http://crbug.com/733548.
   auto it = instance_map_.find(instance);
-  DCHECK(it != instance_map_.end());
+  if (it != instance_map_.end()) {
+    // We need to tell the observers for that instance that we are destroyed
+    // because we won't have the opportunity to once we remove them from the
+    // |instance_map_|. If the instance was deleted, observers for those
+    // instances should never call back into the host anyway, so it is safe to
+    // tell them that the host is destroyed.
+    for (auto& observer : it->second->observer_list)
+      observer.OnHostDestroyed();
 
-  // We need to tell the observers for that instance that we are destroyed
-  // because we won't have the opportunity to once we remove them from the
-  // |instance_map_|. If the instance was deleted, observers for those instances
-  // should never call back into the host anyway, so it is safe to tell them
-  // that the host is destroyed.
-  for (auto& observer : it->second->observer_list)
-    observer.OnHostDestroyed();
-
-  instance_map_.erase(it);
+    instance_map_.erase(it);
+  } else {
+    NOTREACHED();
+  }
 }
 
 void BrowserPpapiHostImpl::AddInstanceObserver(PP_Instance instance,
