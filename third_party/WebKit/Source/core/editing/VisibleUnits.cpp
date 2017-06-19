b7dcdb25041c7dc12454dcce4d656b70e130dee3
yosin <yosin@chromium.org>
2017-06-18 20:28:45 -0700

    Reduce indentation in ComputeInlineBoxPositionTemplate()
    This patch reduces indentation in |ComputeInlineBoxPositionTemplate()| by
    switching if-condition to move large then-clause out of if-statement for
    improving of readability.
    
    BUG=707656
    TEST=n/a; no behavior changes
    
    Review-Url: https://codereview.chromium.org/2943623002
    Cr-Commit-Position: refs/heads/master@{#480337}
    
    Former-commit-id: 38c57515aad29dfce4bd77c885ba937c5b93371b

--- third_party/WebKit/Source/core/editing/VisibleUnits.cpp
+++ third_party/WebKit/Source/core/editing/VisibleUnits.cpp
@@ -828,46 +828,44 @@ static InlineBoxPosition ComputeInlineBoxPositionTemplate(
 
   DCHECK(layout_object) << position;
 
-  if (!layout_object->IsText()) {
-    if (CanHaveChildrenForEditing(anchor_node) &&
-        layout_object->IsLayoutBlockFlow() &&
-        HasRenderedNonAnonymousDescendantsWithHeight(layout_object)) {
-      // Try a visually equivalent position with possibly opposite
-      // editability. This helps in case |this| is in an editable block
-      // but surrounded by non-editable positions. It acts to negate the
-      // logic at the beginning of
-      // |LayoutObject::createPositionWithAffinity()|.
-      const PositionTemplate<Strategy>& downstream_equivalent =
-          DownstreamIgnoringEditingBoundaries(position);
-      if (downstream_equivalent != position) {
-        return ComputeInlineBoxPosition(
-            downstream_equivalent, TextAffinity::kUpstream, primary_direction);
-      }
-      const PositionTemplate<Strategy>& upstream_equivalent =
-          UpstreamIgnoringEditingBoundaries(position);
-      if (upstream_equivalent == position ||
-          DownstreamIgnoringEditingBoundaries(upstream_equivalent) == position)
-        return InlineBoxPosition();
-
-      return ComputeInlineBoxPosition(
-          upstream_equivalent, TextAffinity::kUpstream, primary_direction);
-    }
-    if (!layout_object->IsBox())
-      return InlineBoxPosition();
-    InlineBox* const inline_box =
-        ToLayoutBox(layout_object)->InlineBoxWrapper();
-    if (!inline_box)
-      return InlineBoxPosition();
-    if ((caret_offset > inline_box->CaretMinOffset() &&
-         caret_offset < inline_box->CaretMaxOffset()))
-      return InlineBoxPosition(inline_box, caret_offset);
-    return AdjustInlineBoxPositionForTextDirection(
-        inline_box, caret_offset, layout_object->Style()->GetUnicodeBidi(),
-        primary_direction);
+  if (layout_object->IsText()) {
+    return ComputeInlineBoxPositionForTextNode(layout_object, caret_offset,
+                                               affinity, primary_direction);
   }
+  if (CanHaveChildrenForEditing(anchor_node) &&
+      layout_object->IsLayoutBlockFlow() &&
+      HasRenderedNonAnonymousDescendantsWithHeight(layout_object)) {
+    // Try a visually equivalent position with possibly opposite
+    // editability. This helps in case |this| is in an editable block
+    // but surrounded by non-editable positions. It acts to negate the
+    // logic at the beginning of
+    // |LayoutObject::createPositionWithAffinity()|.
+    const PositionTemplate<Strategy>& downstream_equivalent =
+        DownstreamIgnoringEditingBoundaries(position);
+    if (downstream_equivalent != position) {
+      return ComputeInlineBoxPosition(
+          downstream_equivalent, TextAffinity::kUpstream, primary_direction);
+    }
+    const PositionTemplate<Strategy>& upstream_equivalent =
+        UpstreamIgnoringEditingBoundaries(position);
+    if (upstream_equivalent == position ||
+        DownstreamIgnoringEditingBoundaries(upstream_equivalent) == position)
+      return InlineBoxPosition();
 
-  return ComputeInlineBoxPositionForTextNode(layout_object, caret_offset,
-                                             affinity, primary_direction);
+    return ComputeInlineBoxPosition(upstream_equivalent,
+                                    TextAffinity::kUpstream, primary_direction);
+  }
+  if (!layout_object->IsBox())
+    return InlineBoxPosition();
+  InlineBox* const inline_box = ToLayoutBox(layout_object)->InlineBoxWrapper();
+  if (!inline_box)
+    return InlineBoxPosition();
+  if ((caret_offset > inline_box->CaretMinOffset() &&
+       caret_offset < inline_box->CaretMaxOffset()))
+    return InlineBoxPosition(inline_box, caret_offset);
+  return AdjustInlineBoxPositionForTextDirection(
+      inline_box, caret_offset, layout_object->Style()->GetUnicodeBidi(),
+      primary_direction);
 }
 
 static InlineBoxPosition AdjustInlineBoxPositionForTextDirection(
