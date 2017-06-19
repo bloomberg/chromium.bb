7f8a69b3b39fd4bdbb1a99940ea85142b5efeb9b
marq <marq@chromium.org>
2017-06-19 04:23:18 -0700

    [ObjC ARC] Converts ios/web/test:test_support to ARC.
    Automatically generated ARCMigrate commit
    Notable issues:None
    BUG=624363
    TEST=None
    
    Review-Url: https://codereview.chromium.org/2940303002
    Cr-Commit-Position: refs/heads/master@{#480403}
    
    Former-commit-id: 1941ee92a0406d17fb00d5b14e51e4918f7e2e20

--- ios/web/test/web_test_with_web_controller.mm
+++ ios/web/test/web_test_with_web_controller.mm
@@ -6,6 +6,10 @@
 
 #import "ios/web/web_state/web_state_impl.h"
 
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
 namespace web {
 
 WebTestWithWebController::WebTestWithWebController() {}
