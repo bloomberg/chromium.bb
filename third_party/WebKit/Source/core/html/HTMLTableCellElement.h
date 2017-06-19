81c36c2820d572f244807231a844688286e994ea
Vadym Doroshenko <dvadym@chromium.org>
2017-06-19 08:59:53 +0000

    Revert "Update MaxColSpan to 1000."
    This reverts commit 8240462943e280393ae2dc64c8fec1f40ebd2295 [formerly dfc8e26b984ef15da906c1303bc8a17bf1e6fef1].
    
    Reason for revert: this CL makes that a test external/wpt/html/semantics/tabular-data/processing-model-1/span-limits.html fails on multiple bots (eg. https://uberchromegw.corp.google.com/i/chromium.webkit/builders/WebKit%20Mac10.11/builds/20873 )
    
    Original change's description:
    > Update MaxColSpan to 1000.
    > 
    > Update MaxColspan value from 8190 to 1000 for below.
    > a) colspan attribute of td and th elements.
    > b) span attribute of colgroup and col elements.
    > 
    > Spec change:
    > https://github.com/whatwg/html/pull/1993
    > https://github.com/whatwg/html/pull/2734
    > 
    > Spec:
    > https://html.spec.whatwg.org/#dom-tdth-colspan
    > https://html.spec.whatwg.org/#dom-col-span
    > https://html.spec.whatwg.org/#dom-colgroup-span
    > 
    > BUG=715559
    > 
    > Change-Id: I8f5e0e22336ad90cf118ac3ac6ebbf9a7c4b149d
    > Reviewed-on: https://chromium-review.googlesource.com/538455
    > Commit-Queue: Shanmuga Pandi <shanmuga.m@samsung.com>
    > Reviewed-by: David Grogan <dgrogan@chromium.org>
    > Reviewed-by: Fredrik Söderquist <fs@opera.com>
    > Reviewed-by: Kent Tamura <tkent@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#480359}
    
    TBR=dgrogan@chromium.org,fs@opera.com,tkent@chromium.org,shanmuga.m@samsung.com
    
    Change-Id: Ie8c59dc21fc8d3e8b1b6aad6a673d07008a2fb22
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 715559
    Reviewed-on: https://chromium-review.googlesource.com/538640
    Reviewed-by: Vadym Doroshenko <dvadym@chromium.org>
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480378}
    Former-commit-id: 2eae763de8c2ce4c4e7585723120c0f8afd6f40f

--- third_party/WebKit/Source/core/html/HTMLTableCellElement.h
+++ third_party/WebKit/Source/core/html/HTMLTableCellElement.h
@@ -50,11 +50,15 @@ class CORE_EXPORT HTMLTableCellElement final : public HTMLTablePartElement {
   const AtomicString& Headers() const;
   void setRowSpan(unsigned);
 
-  // Public so that HTMLTableColElement can use MaxColSpan. MaxRowSpan is only
-  // used by this class but keeping them together seems desirable.
-  // https://html.spec.whatwg.org/#dom-tdth-colspan
-  static unsigned MaxColSpan() { return 1000u; }
-  // https://html.spec.whatwg.org/#dom-tdth-rowspan
+  // Rowspan: match Firefox's limit of 65,534. Edge has a higher limit, at
+  // least 2^17.
+  // Colspan: Firefox uses a limit of 1,000 for colspan and resets the value to
+  // 1.
+  // TODO(dgrogan): Change these to HTML's new specified behavior when
+  // https://github.com/whatwg/html/issues/1198 is resolved.
+  // Public so that HTMLColElement can use maxColSpan. maxRowSpan is only used
+  // by this class but keeping them together seems desirable.
+  static unsigned MaxColSpan() { return 8190u; }
   static unsigned MaxRowSpan() { return 65534u; }
 
  private:
