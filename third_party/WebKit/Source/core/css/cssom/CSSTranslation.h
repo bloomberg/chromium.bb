79dba711ceae7a7ef0108dab2481d93bc1ee743e
meade <meade@chromium.org>
2017-06-19 03:07:19 -0700

    [Typed OM] Add comments and consistent structure to all the CSSTransformComponent subclasses
    BUG=545318
    
    Review-Url: https://codereview.chromium.org/2935413002
    Cr-Commit-Position: refs/heads/master@{#480385}
    
    Former-commit-id: 4c46599615d3bb4af1c914bc583b5cf0ee1a08af

--- third_party/WebKit/Source/core/css/cssom/CSSTranslation.h
+++ third_party/WebKit/Source/core/css/cssom/CSSTranslation.h
@@ -13,11 +13,15 @@ class CSSNumericValue;
 class DOMMatrix;
 class ExceptionState;
 
+// Represents a translation value in a CSSTransformValue used for properties
+// like "transform".
+// See CSSTranslation.idl for more information about this class.
 class CORE_EXPORT CSSTranslation final : public CSSTransformComponent {
   WTF_MAKE_NONCOPYABLE(CSSTranslation);
   DEFINE_WRAPPERTYPEINFO();
 
  public:
+  // Constructors defined in the IDL.
   static CSSTranslation* Create(CSSNumericValue* x,
                                 CSSNumericValue* y,
                                 ExceptionState&) {
@@ -28,21 +32,22 @@ class CORE_EXPORT CSSTranslation final : public CSSTransformComponent {
                                 CSSNumericValue* z,
                                 ExceptionState&);
 
+  // Blink-internal ways of creating CSSTranslations.
   static CSSTranslation* FromCSSValue(const CSSFunctionValue& value) {
     return nullptr;
   }
 
+  // Getters and setters for attributes defined in the IDL.
   CSSNumericValue* x() const { return x_; }
   CSSNumericValue* y() const { return y_; }
   CSSNumericValue* z() const { return z_; }
 
+  // Internal methods - from CSSTransformComponent.
   TransformComponentType GetType() const override {
     return Is2D() ? kTranslationType : kTranslation3DType;
   }
-
   // TODO: Implement AsMatrix for CSSTranslation.
   DOMMatrix* AsMatrix() const override { return nullptr; }
-
   CSSFunctionValue* ToCSSValue() const override;
 
   DEFINE_INLINE_VIRTUAL_TRACE() {
