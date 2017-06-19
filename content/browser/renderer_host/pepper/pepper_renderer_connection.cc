c122fb732edeee9192e735029807e693708aee42
Bill Budge <bbudge@chromium.org>
2017-06-19 16:14:57 -0700

    Validate in-process plugin instance messages.
    Bug: 733548, 733549
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Change-Id: Ie5572c7bcafa05399b09c44425ddd5ce9b9e4cba
    Reviewed-on: https://chromium-review.googlesource.com/538908
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480696}
    Former-commit-id: 66b99f3fe60dce77f079cc9c07164f6a34dbea37

--- content/browser/renderer_host/pepper/pepper_renderer_connection.cc
+++ content/browser/renderer_host/pepper/pepper_renderer_connection.cc
@@ -232,12 +232,16 @@ void PepperRendererConnection::OnMsgDidCreateInProcessInstance(
     PP_Instance instance,
     const PepperRendererInstanceData& instance_data) {
   PepperRendererInstanceData data = instance_data;
+  // It's important that we supply the render process ID ourselves since the
+  // message may be coming from a compromised renderer.
   data.render_process_id = render_process_id_;
+  // 'instance' is possibly invalid. The host must be careful not to trust it.
   in_process_host_->AddInstance(instance, data);
 }
 
 void PepperRendererConnection::OnMsgDidDeleteInProcessInstance(
     PP_Instance instance) {
+  // 'instance' is possibly invalid. The host must be careful not to trust it.
   in_process_host_->DeleteInstance(instance);
 }
 
