a74d4752bea1e3aa1a90992173a2afa7e15fbefd
jif <jif@chromium.org>
2017-06-19 03:25:40 -0700

    [iOS] Iteration on the new experimental keyboard accessory view.
    This CL iterates over the new keyboard accessory view:
    -Final assets were added.
    -The design was updated slightly (no buttons are centered aligned).
    -Limit the new keyboard accessory view to iPhones.
    
    BUG=642702
    
    Review-Url: https://codereview.chromium.org/2939093002
    Cr-Commit-Position: refs/heads/master@{#480388}
    
    Former-commit-id: 49e90e207a5e77ae327efe0712a24e42e35d5877

--- ios/chrome/browser/ui/toolbar/new_keyboard_accessory_view.mm
+++ ios/chrome/browser/ui/toolbar/new_keyboard_accessory_view.mm
@@ -21,6 +21,7 @@
 
 @interface NewKeyboardAccessoryView ()
 
+@property(nonatomic, retain) NSArray<NSString*>* buttonTitles;
 @property(nonatomic, weak) id<KeyboardAccessoryViewDelegate> delegate;
 
 // Called when a keyboard shortcut button is pressed.
@@ -37,136 +38,123 @@
 // Unused by this implementation of |KeyboardAccessoryViewProtocol|.
 @synthesize mode = _mode;
 
+@synthesize buttonTitles = _buttonTitles;
 @synthesize delegate = _delegate;
 
 - (instancetype)initWithButtons:(NSArray<NSString*>*)buttonTitles
                        delegate:(id<KeyboardAccessoryViewDelegate>)delegate {
-  const CGFloat kButtonMinSizeX = 61.0;
-  const CGFloat kButtonMinSizeXCompact = 32.0;
-  const CGFloat kButtonSizeY = 62.0;
-  const CGFloat kButtonSizeYCompact = 30.0;
-  const CGFloat kBetweenButtonSpacing = 15.0;
-  const CGFloat kBetweenButtonSpacingCompact = 6.0;
-  const CGFloat kSeparatorAlpha = 0.1;
-  const CGFloat kViewHeight = 70.0;
-  const CGFloat kViewHeightCompact = 43.0;
-  const BOOL isCompact = IsCompact();
+  DCHECK(!IsIPadIdiom());
 
+  const CGFloat kViewHeight = 44.0;
   CGFloat width = [[UIScreen mainScreen] bounds].size.width;
-  CGFloat height = isCompact ? kViewHeightCompact : kViewHeight;
-  CGRect frame = CGRectMake(0.0, 0.0, width, height);
+  // TODO(734512): Have the creator of the view define the size.
+  CGRect frame = CGRectMake(0.0, 0.0, width, kViewHeight);
 
   self = [super initWithFrame:frame inputViewStyle:UIInputViewStyleKeyboard];
   if (self) {
+    _buttonTitles = buttonTitles;
     _delegate = delegate;
 
-    // Create and add stackview filled with the shortcut buttons.
-    UIStackView* stackView = [[UIStackView alloc] init];
-    [stackView setTranslatesAutoresizingMaskIntoConstraints:NO];
-    stackView.spacing =
-        isCompact ? kBetweenButtonSpacingCompact : kBetweenButtonSpacing;
-    for (NSString* title in buttonTitles) {
-      UIView* button = [self shortcutButtonWithTitle:title];
-      [button setTranslatesAutoresizingMaskIntoConstraints:NO];
-      CGFloat buttonMinWidth =
-          isCompact ? kButtonMinSizeXCompact : kButtonMinSizeX;
-      [button.widthAnchor constraintGreaterThanOrEqualToConstant:buttonMinWidth]
-          .active = YES;
-      CGFloat buttonHeight = isCompact ? kButtonSizeYCompact : kButtonSizeY;
-      [button.heightAnchor constraintEqualToConstant:buttonHeight].active = YES;
-      [stackView addArrangedSubview:button];
-    }
-    [self addSubview:stackView];
-
-    // Create and add buttons for voice and camera search.
-    UIButton* cameraButton = [self iconButton:@"qr_scanner_keyboard_accessory"];
-    [cameraButton
-               addTarget:_delegate
-                  action:@selector(keyboardAccessoryCameraSearchTouchUpInside)
-        forControlEvents:UIControlEventTouchUpInside];
-    [self addSubview:cameraButton];
-
-    UIButton* voiceSearchButton =
-        [self iconButton:@"voice_icon_keyboard_accessory"];
-    [self addSubview:voiceSearchButton];
-    [voiceSearchButton
-               addTarget:_delegate
-                  action:@selector(keyboardAccessoryVoiceSearchTouchDown)
-        forControlEvents:UIControlEventTouchDown];
-    [voiceSearchButton
-               addTarget:_delegate
-                  action:@selector(keyboardAccessoryVoiceSearchTouchUpInside)
-        forControlEvents:UIControlEventTouchUpInside];
-
-    // Create and add 1 pixel high separator view.
-    UIView* separator = [[UIView alloc] init];
-    [separator setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [separator
-        setBackgroundColor:[UIColor colorWithWhite:0 alpha:kSeparatorAlpha]];
-    [separator.heightAnchor
-        constraintEqualToConstant:1.0 / [[UIScreen mainScreen] scale]]
-        .active = YES;
-    [self addSubview:separator];
-
-    // Position all the views.
-    NSDictionary* viewsDictionary = @{
-      @"cameraButton" : cameraButton,
-      @"voiceSearch" : voiceSearchButton,
-      @"stackView" : stackView,
-      @"separator" : separator,
-    };
-    NSArray* constraints = @[
-      @"H:|-8-[cameraButton]-8-[voiceSearch]-(>=16)-[stackView]",
-      @"H:|-0-[separator]-0-|",
-      @"V:[separator]-0-|",
-    ];
-    ApplyVisualConstraintsWithOptions(constraints, viewsDictionary,
-                                      LayoutOptionForRTLSupport(), self);
-    AddSameCenterYConstraint(self, cameraButton);
-    AddSameCenterYConstraint(self, voiceSearchButton);
-    AddSameCenterYConstraint(self, stackView);
-    // The following constraint is supposed to break if there's not enough
-    // space to center the stack view.
-    NSLayoutConstraint* horizontallyCenterStackViewConstraint =
-        [self.centerXAnchor constraintEqualToAnchor:stackView.centerXAnchor];
-    horizontallyCenterStackViewConstraint.priority =
-        UILayoutPriorityDefaultHigh;
-    horizontallyCenterStackViewConstraint.active = YES;
   }
   return self;
 }
 
+- (void)willMoveToSuperview:(UIView*)newSuperview {
+  if (!self.subviews.count)
+    return;
+
+  const CGFloat kButtonMinWidth = 36.0;
+  const CGFloat kButtonHeight = 34.0;
+  const CGFloat kBetweenShortcutButtonSpacing = 5.0;
+  const CGFloat kBetweenSearchButtonSpacing = 12.0;
+  const CGFloat kMarginFromBottom = 2.0;
+  const CGFloat kHorizontalMargin = 12.0;
+
+  // Create and add stackview filled with the shortcut buttons.
+  UIStackView* shortcutStackView = [[UIStackView alloc] init];
+  shortcutStackView.translatesAutoresizingMaskIntoConstraints = NO;
+  shortcutStackView.spacing = kBetweenShortcutButtonSpacing;
+  for (NSString* title in self.buttonTitles) {
+    UIView* button = [self shortcutButtonWithTitle:title];
+    [button setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [button.widthAnchor constraintGreaterThanOrEqualToConstant:kButtonMinWidth]
+        .active = YES;
+    [button.heightAnchor constraintEqualToConstant:kButtonHeight].active = YES;
+    [shortcutStackView addArrangedSubview:button];
+  }
+  [self addSubview:shortcutStackView];
+
+  // Create buttons for voice search and camera search.
+  UIButton* voiceSearchButton =
+      [self iconButton:@"keyboard_accessory_voice_search"];
+  [voiceSearchButton addTarget:_delegate
+                        action:@selector(keyboardAccessoryVoiceSearchTouchDown)
+              forControlEvents:UIControlEventTouchDown];
+  [voiceSearchButton
+             addTarget:_delegate
+                action:@selector(keyboardAccessoryVoiceSearchTouchUpInside)
+      forControlEvents:UIControlEventTouchUpInside];
+  UIButton* cameraButton = [self iconButton:@"keyboard_accessory_qr_scanner"];
+  [cameraButton addTarget:_delegate
+                   action:@selector(keyboardAccessoryCameraSearchTouchUpInside)
+         forControlEvents:UIControlEventTouchUpInside];
+
+  // Create a stackview containing containing the buttons for voice search
+  // and camera search.
+  UIStackView* searchStackView = [[UIStackView alloc] init];
+  searchStackView.translatesAutoresizingMaskIntoConstraints = NO;
+  searchStackView.spacing = kBetweenSearchButtonSpacing;
+  [searchStackView addArrangedSubview:voiceSearchButton];
+  [searchStackView addArrangedSubview:cameraButton];
+  [self addSubview:searchStackView];
+
+  // Position the stack views.
+  NSArray* constraints = @[
+    @"H:|-horizontalMargin-[searchStackView]-(>=0)-[shortcutStackView]",
+    @"[shortcutStackView]-horizontalMargin-|",
+    @"V:[searchStackView]-bottomMargin-|",
+    @"V:[shortcutStackView]-bottomMargin-|"
+  ];
+  NSDictionary* viewsDictionary = @{
+    @"searchStackView" : searchStackView,
+    @"shortcutStackView" : shortcutStackView,
+  };
+  NSDictionary* metrics = @{
+    @"bottomMargin" : @(kMarginFromBottom),
+    @"horizontalMargin" : @(kHorizontalMargin)
+  };
+  ApplyVisualConstraintsWithMetrics(constraints, viewsDictionary, metrics);
+}
+
 - (UIView*)shortcutButtonWithTitle:(NSString*)title {
-  const CGFloat kCornerRadius = 4.0;
-  const CGFloat kAlphaStateNormal = 0.1;
-  const CGFloat kAlphaStateHighlighted = 0.2;
+  const CGFloat kCornerRadius = 5.0;
+  const CGFloat kAlphaStateNormal = 0.3;
+  const CGFloat kAlphaStateHighlighted = 0.6;
   const CGFloat kHorizontalEdgeInset = 8;
-  const CGFloat kIpadButtonTitleFontSize = 20.0;
-  const CGFloat kIphoneButtonTitleFontSize = 15.0;
+  const CGFloat kButtonTitleFontSize = 20.0;
+  const UIColor* kButtonBackgroundColor =
+      [UIColor colorWithRed:0.507 green:0.534 blue:0.57 alpha:1.0];
 
   ColoredButton* button = [ColoredButton buttonWithType:UIButtonTypeCustom];
-  UIFont* font = nil;
+
+  UIColor* stateNormalBackgroundColor =
+      [kButtonBackgroundColor colorWithAlphaComponent:kAlphaStateNormal];
+  UIColor* stateHighlightedBackgroundColor =
+      [kButtonBackgroundColor colorWithAlphaComponent:kAlphaStateHighlighted];
+
+  [button setBackgroundColor:stateNormalBackgroundColor
+                    forState:UIControlStateNormal];
+  [button setBackgroundColor:stateHighlightedBackgroundColor
+                    forState:UIControlStateHighlighted];
 
   [button setTitle:title forState:UIControlStateNormal];
   [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-
-  [button setBackgroundColor:UIColorFromRGB(0, kAlphaStateNormal)
-                    forState:UIControlStateNormal];
-  [button setBackgroundColor:UIColorFromRGB(0, kAlphaStateHighlighted)
-                    forState:UIControlStateHighlighted];
-
   button.layer.cornerRadius = kCornerRadius;
   button.contentEdgeInsets =
       UIEdgeInsetsMake(0, kHorizontalEdgeInset, 0, kHorizontalEdgeInset);
   button.clipsToBounds = YES;
-
-  if (IsIPadIdiom()) {
-    font = [UIFont systemFontOfSize:kIpadButtonTitleFontSize];
-  } else {
-    font = [UIFont boldSystemFontOfSize:kIphoneButtonTitleFontSize];
-  }
-
-  [button.titleLabel setFont:font];
+  [button.titleLabel setFont:[UIFont systemFontOfSize:kButtonTitleFontSize
+                                               weight:UIFontWeightMedium]];
 
   [button addTarget:self
                 action:@selector(keyboardButtonPressed:)
@@ -177,21 +165,9 @@
 }
 
 - (UIButton*)iconButton:(NSString*)iconName {
-  const CGFloat kIconTintAlphaStateNormal = 0.45;
-  const CGFloat kIconTintAlphaStateHighlighted = 0.6;
-
-  UIColor* iconTintStateNormal = UIColorFromRGB(0, kIconTintAlphaStateNormal);
-  UIColor* iconTintStateHighlighted =
-      UIColorFromRGB(0, kIconTintAlphaStateHighlighted);
-
-  ColoredButton* button = [ColoredButton buttonWithType:UIButtonTypeCustom];
-
-  [button setTintColor:iconTintStateNormal forState:UIControlStateNormal];
-  [button setTintColor:iconTintStateHighlighted
-              forState:UIControlStateHighlighted];
+  UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
   [button setTranslatesAutoresizingMaskIntoConstraints:NO];
-  UIImage* icon = [[UIImage imageNamed:iconName]
-      imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+  UIImage* icon = [UIImage imageNamed:iconName];
   [button setImage:icon forState:UIControlStateNormal];
   return button;
 }
