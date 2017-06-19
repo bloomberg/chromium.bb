5db86824f70e30fd9bfd9987cc3d2eb3d7cdb8f4
Florin Malita <fmalita@chromium.org>
2017-06-19 10:01:36 -0400

    Add guard for upcoming Skia change
    SK_SUPPORT_LEGACY_DITHERING, for
    https://skia-review.googlesource.com/c/19880/
    Change-Id: I89cd24c3d52c49b22a94c2d0391040c2b3e1dda9
    
    TBR=
    NOTRY=true
    
    Change-Id: I89cd24c3d52c49b22a94c2d0391040c2b3e1dda9
    Reviewed-on: https://chromium-review.googlesource.com/539695
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480431}
    Former-commit-id: 9cfe883cc11b74989f6119c4090b2cf7538e694b

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -216,6 +216,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_GRADIENT_ALPHATRUNC
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_DITHERING
+#define SK_SUPPORT_LEGACY_DITHERING
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
