380606f29ab60f56faa8832407a3d175a87c2f05
Kenneth Russell <kbr@chromium.org>
2017-06-20 16:22:33 -0700

    Add CHECKs during sampler state restoration.
    It's difficult to figure out why the crashes are being seen, so add
    explicit checks that will help track down the cause.
    
    These will be removed before the next branch point.
    
    BUG=733096
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ic073fb288ff4084614142ff802e9a93457241c6a
    Reviewed-on: https://chromium-review.googlesource.com/540781
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481068}
    Former-commit-id: 96a04c90505930f286a37b8beb1c6852d0644fcd

--- gpu/command_buffer/service/context_state.cc
+++ gpu/command_buffer/service/context_state.cc
@@ -285,10 +285,12 @@ void ContextState::RestoreSamplerBinding(GLuint unit,
                                          const ContextState* prev_state) const {
   if (!feature_info_->IsES3Capable())
     return;
+  CHECK_LT(unit, sampler_units.size());
   const scoped_refptr<Sampler>& cur_sampler = sampler_units[unit];
   GLuint cur_id = cur_sampler ? cur_sampler->service_id() : 0;
   GLuint prev_id = 0;
   if (prev_state) {
+    CHECK_LT(unit, prev_state->sampler_units.size());
     const scoped_refptr<Sampler>& prev_sampler =
         prev_state->sampler_units[unit];
     prev_id = prev_sampler ? prev_sampler->service_id() : 0;
