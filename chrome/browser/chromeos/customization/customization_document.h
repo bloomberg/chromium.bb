5f89be0661614eed01d1b3fc558e407912a4fe3b
Xiyuan Xia <xiyuan@chromium.org>
2017-06-20 16:12:21 -0700

    cros: Replace BrowserThread::FILE in ServicesCustomizationDocument
    BrowserThread::FILE is deprecated in favor of the post_task API.
    
    BUG=689520
    
    Change-Id: Ia3cee112af64b7e5e1208fb523f508c1444e2436
    Reviewed-on: https://chromium-review.googlesource.com/541623
    Commit-Queue: Xiyuan Xia <xiyuan@chromium.org>
    Reviewed-by: Achuith Bhandarkar <achuith@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481218}
    Former-commit-id: e9294ad21604f461226df94577094c361f1cdc49

--- chrome/browser/chromeos/customization/customization_document.h
+++ chrome/browser/chromeos/customization/customization_document.h
@@ -236,13 +236,8 @@ class ServicesCustomizationDocument : public CustomizationDocument,
   // Initiate file fetching. Don't wait for online status.
   void DoStartFileFetch();
 
-  // Executes on FILE thread and reads file to string.
-  static void ReadFileInBackground(
-      base::WeakPtr<ServicesCustomizationDocument> self,
-      const base::FilePath& file);
-
   // Called on UI thread with results of ReadFileInBackground.
-  void OnManifesteRead(const std::string& manifest);
+  void OnManifestRead(const std::string& manifest);
 
   // Method called when manifest was successfully loaded.
   void OnManifestLoaded();
