a40ef5e9e6baa8d4e39b988b2579b9d374c7274c
Fernando Serboncini <fserb@chromium.org>
2017-06-20 13:45:32 -0400

    Creates FontStyleResolver for off-main thread font resolution
    This still shares code with StyleBuilderConverter and FontBuilder.
    For that, we create a StyleBuilderConverterBase that is independent of
    ComputedStyle.
    
    Bug: 730692
    Change-Id: I7792ef340a5f95782f57a94e093c3c7d73ad11b1
    Reviewed-on: https://chromium-review.googlesource.com/531655
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480971}
    Former-commit-id: 4d8733fe8c6e16b9bd8605bb15b0003df39d11e4

---
+++ third_party/WebKit/Source/core/css/resolver/FontStyleResolver.cpp
@@ -0,0 +1,58 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/resolver/FontStyleResolver.h"
+
+#include "core/css/CSSToLengthConversionData.h"
+#include "core/css/resolver/FontBuilder.h"
+#include "core/css/resolver/StyleBuilderConverter.h"
+#include "platform/fonts/FontDescription.h"
+
+namespace blink {
+
+FontDescription FontStyleResolver::ComputeFont(
+    const StylePropertySet& property_set) {
+  FontBuilder builder(nullptr);
+
+  FontDescription fontDescription;
+  Font font(fontDescription);
+  CSSToLengthConversionData::FontSizes fontSizes(16, 16, &font);
+  CSSToLengthConversionData::ViewportSize viewportSize(0, 0);
+  CSSToLengthConversionData conversionData(nullptr, fontSizes, viewportSize, 1);
+
+  // CSSPropertyFontSize
+  builder.SetSize(StyleBuilderConverterBase::ConvertFontSize(
+      *property_set.GetPropertyCSSValue(CSSPropertyFontSize), conversionData,
+      FontDescription::Size(0, 0.0f, false)));
+
+  // CSSPropertyFontFamily
+  builder.SetFamilyDescription(StyleBuilderConverterBase::ConvertFontFamily(
+      *property_set.GetPropertyCSSValue(CSSPropertyFontFamily), &builder,
+      nullptr));
+
+  // CSSPropertyFontStretch
+  builder.SetStretch(ToCSSIdentifierValue(*property_set.GetPropertyCSSValue(
+                                              CSSPropertyFontStretch))
+                         .ConvertTo<FontStretch>());
+
+  // CSSPropertyFontStyle
+  builder.SetStyle(ToCSSIdentifierValue(
+                       *property_set.GetPropertyCSSValue(CSSPropertyFontStyle))
+                       .ConvertTo<FontStyle>());
+
+  // CSSPropertyFontVariantCaps
+  builder.SetVariantCaps(StyleBuilderConverterBase::ConvertFontVariantCaps(
+      *property_set.GetPropertyCSSValue(CSSPropertyFontVariantCaps)));
+
+  // CSSPropertyFontWeight
+  builder.SetWeight(StyleBuilderConverterBase::ConvertFontWeight(
+      *property_set.GetPropertyCSSValue(CSSPropertyFontWeight),
+      FontBuilder::InitialWeight()));
+
+  builder.UpdateFontDescription(fontDescription);
+
+  return fontDescription;
+}
+
+}  // namespace blink
