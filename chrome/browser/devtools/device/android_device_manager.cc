23b0516c4bd7d070051da4b19eba37e8304d1ae3
Andrey Kosyakov <caseq@chromium.org>
2017-06-20 16:38:12 -0700

    Do not use BrowserThread::FILE in android_device_manager.cc
    ... just post a task with MayBlock traits.
    
    Bug: 689520
    Change-Id: I44a5faccb5d3f46c263de355169473b597e64f0c
    Reviewed-on: https://chromium-review.googlesource.com/541958
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481316}
    Former-commit-id: 5c4bbafa0d3cee8b39639b62e3990832a3df7522

--- chrome/browser/devtools/device/android_device_manager.cc
+++ chrome/browser/devtools/device/android_device_manager.cc
@@ -15,6 +15,7 @@
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
@@ -498,7 +499,6 @@ AndroidDeviceManager::HandlerThread::message_loop() {
 // static
 void AndroidDeviceManager::HandlerThread::StopThread(
     base::Thread* thread) {
-  thread->Stop();
   delete thread;
 }
 
@@ -507,10 +507,10 @@ AndroidDeviceManager::HandlerThread::~HandlerThread() {
   instance_ = nullptr;
   if (!thread_)
     return;
-  // Shut down thread on FILE thread to join into IO.
-  content::BrowserThread::PostTask(
-      content::BrowserThread::FILE, FROM_HERE,
-      base::BindOnce(&HandlerThread::StopThread, thread_));
+  // Shut down thread on a thread other than UI so it can join a thread.
+  base::PostTaskWithTraits(FROM_HERE,
+                           {base::MayBlock(), base::TaskPriority::BACKGROUND},
+                           base::BindOnce(&HandlerThread::StopThread, thread_));
 }
 
 // static
