d94551367baa53a817bfc744378aeff04bde011a
Hiroki Nakagawa <nhiroki@chromium.org>
2017-06-20 12:42:35 +0900

    Worklet: Move ParseCredentialsOption() to anonymous namespace for cleanup
    This is a follow-up CL for
    https://chromium.googlesource.com/chromium/src/+/963e6d1c7bafcf9f2a8e5826ed1b88af877cda55 [formerly 8f9e4bf115f530b797a615fdecd5711845fec39f]
    
    Worklet::ParseCredentialsOption() was called from both MainThreadWorklet and
    ThreadedWorklet, but after the merge of them, the function is used only from
    Worklet. Therefore, it's no longer necessary that the function is a static
    member of Worklet.
    
    Bug: 727194
    Change-Id: Ic9a7c90e61ec1a6a619096500a2e30064199d09f
    Reviewed-on: https://chromium-review.googlesource.com/540958
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#480749}
    Former-commit-id: 17b0d447e8382efbc891bd5c519b484a325ec8c3

--- third_party/WebKit/Source/core/workers/Worklet.h
+++ third_party/WebKit/Source/core/workers/Worklet.h
@@ -12,7 +12,6 @@
 #include "core/workers/WorkletOptions.h"
 #include "platform/bindings/ScriptWrappable.h"
 #include "platform/heap/Handle.h"
-#include "public/platform/WebURLRequest.h"
 
 namespace blink {
 
@@ -50,9 +49,6 @@ class CORE_EXPORT Worklet : public GarbageCollectedFinalized<Worklet>,
  protected:
   // The Worklet inherits the url and userAgent from the frame->document().
   explicit Worklet(LocalFrame*);
-
-  static WebURLRequest::FetchCredentialsMode ParseCredentialsOption(
-      const String& credentials_option);
 
   // Returns one of available global scopes.
   WorkletGlobalScopeProxy* FindAvailableGlobalScope() const;
