a505e21b6faa2ffd7d2f974011117da190411eb9
Fernando Serboncini <fserb@chromium.org>
2017-06-21 15:29:04 -0400

    FontObjectThreadedTests to tests font creation/usage on threaded
    Those tests exercise:
      - FontStyleResolver CSS resolution to FontDescription
      - CreateTestFont (FontSelector + test Font loading)
    
    Bug: 730692
    Change-Id: I3c68144eefeadda5568ab4977809dd36b6640d63
    Reviewed-on: https://chromium-review.googlesource.com/541497
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481335}
    Former-commit-id: 5e2330c58ace31c217a12891436089e7084169e9

---
+++ third_party/WebKit/Source/core/css/threaded/FontObjectThreadedTest.cpp
@@ -0,0 +1,45 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/resolver/FilterOperationResolver.h"
+
+#include "core/css/parser/CSSParser.h"
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/resolver/FontStyleResolver.h"
+#include "core/css/threaded/MultiThreadedTestUtil.h"
+#include "platform/fonts/Font.h"
+#include "platform/fonts/FontCustomPlatformData.h"
+#include "platform/fonts/FontDescription.h"
+#include "platform/fonts/FontSelector.h"
+#include "platform/testing/FontTestHelpers.h"
+#include "platform/testing/UnitTestHelpers.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using blink::testing::CreateTestFont;
+
+namespace blink {
+
+TSAN_TEST(FontObjectThreadedTest, GetFontDefinition) {
+  RunOnThreads([]() {
+    MutableStylePropertySet* style =
+        MutableStylePropertySet::Create(kHTMLStandardMode);
+    CSSParser::ParseValue(style, CSSPropertyFont, "15px Ahem", true);
+
+    FontDescription desc = FontStyleResolver::ComputeFont(*style);
+
+    EXPECT_EQ(desc.SpecifiedSize(), 15);
+    EXPECT_EQ(desc.ComputedSize(), 15);
+    EXPECT_EQ(desc.Family().Family(), "Ahem");
+  });
+}
+
+// This test passes by not crashing TSAN.
+TSAN_TEST(FontObjectThreadedTest, FontSelector) {
+  RunOnThreads([]() {
+    Font font =
+        CreateTestFont("Ahem", testing::PlatformTestDataPath("Ahem.woff"), 16);
+  });
+}
+
+}  // namespace blink
