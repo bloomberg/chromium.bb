bbe711642cc2686bbcb0d415d8a60f99c5f8299a
Jochen Eisinger <jochen@chromium.org>
2017-06-21 10:48:22 +0200

    Temporarily disable script forbidden scope checks
    While fixing the V8 side to invoke the callback correctly, I temporarily
    disable the check on the blink side until we've correctly cleaned up all
    callsites that execute script, and can re-enable then check
    
    BUG=728583
    R=haraken@chromium.org
    
    Change-Id: Ia32abee6485124766d73f69cdb6cc3350bffe3b4
    Reviewed-on: https://chromium-review.googlesource.com/542815
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481211}
    Former-commit-id: 2865ab16e98fa7ac6892ea0a753ec2f6213c1710

--- third_party/WebKit/Source/platform/bindings/V8PerIsolateData.cpp
+++ third_party/WebKit/Source/platform/bindings/V8PerIsolateData.cpp
@@ -44,7 +44,8 @@ namespace blink {
 static V8PerIsolateData* g_main_thread_per_isolate_data = 0;
 
 static void BeforeCallEnteredCallback(v8::Isolate* isolate) {
-  CHECK(!ScriptForbiddenScope::IsScriptForbidden());
+  // TODO(jochen): Re-enable this once https://crbug.com/728583
+  // CHECK(!ScriptForbiddenScope::IsScriptForbidden());
 }
 
 static void MicrotasksCompletedCallback(v8::Isolate* isolate) {
