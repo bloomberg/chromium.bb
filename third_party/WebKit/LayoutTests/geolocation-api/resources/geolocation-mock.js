e865975f4251ad4c17fd43b991680d2a0dbe77e5
Miguel Casas-Sanchez <mcasas@chromium.org>
2017-06-21 12:23:37 -0700

    Geolocation: extract struct Geoposition out of geolocation.mojom
    This CL extracts the struct Geoposition out of geolocation.mojom 
    into a file on its own. No new code.
    
    
    Bug: 
    Change-Id: I6350537ccd19d1da0b02f3c8489fa39a06740376
    Reviewed-on: https://chromium-review.googlesource.com/543577
    Reviewed-by: Colin Blundell <blundell@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Miguel Casas <mcasas@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481777}
    Former-commit-id: a5015e84b97fe94e43c3d46f26c2b0480345bb80

--- third_party/WebKit/LayoutTests/geolocation-api/resources/geolocation-mock.js
+++ third_party/WebKit/LayoutTests/geolocation-api/resources/geolocation-mock.js
@@ -8,11 +8,12 @@
 let geolocationServiceMock = loadMojoModules(
     'geolocationServiceMock',
     ['device/geolocation/public/interfaces/geolocation.mojom',
+     'device/geolocation/public/interfaces/geoposition.mojom',
      'third_party/WebKit/public/platform/modules/permissions/permission.mojom',
      'third_party/WebKit/public/platform/modules/permissions/permission_status.mojom',
      'mojo/public/js/bindings',
     ]).then(mojo => {
-  let [geolocation, permission, permissionStatus, bindings] =
+  let [geolocation, geoposition, permission, permissionStatus, bindings] =
       mojo.modules;
 
   class GeolocationServiceMock {
@@ -101,7 +102,7 @@ let geolocationServiceMock = loadMojoModules(
      */
     setGeolocationPosition(latitude, longitude, accuracy, altitude,
                            altitudeAccuracy, heading, speed) {
-      this.geoposition_ = new geolocation.Geoposition();
+      this.geoposition_ = new geoposition.Geoposition();
       this.geoposition_.latitude = latitude;
       this.geoposition_.longitude = longitude;
       this.geoposition_.accuracy = accuracy;
@@ -120,11 +121,11 @@ let geolocationServiceMock = loadMojoModules(
      * the error set by this call.
      */
     setGeolocationPositionUnavailableError(message) {
-      this.geoposition_ = new geolocation.Geoposition();
+      this.geoposition_ = new geoposition.Geoposition();
       this.geoposition_.valid = false;
       this.geoposition_.error_message = message;
       this.geoposition_.error_code =
-          geolocation.Geoposition.ErrorCode.POSITION_UNAVAILABLE;
+          geoposition.Geoposition.ErrorCode.POSITION_UNAVAILABLE;
     }
 
     /**
