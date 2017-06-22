5b8d2f623597abc566c9656a95e22380351c0c64
bashi <bashi@chromium.org>
2017-06-21 18:15:18 -0700

    Update default policy for memory coordinator
    For now, memory coordinator is going to focus on how to detect memory
    pressure and when to ask TabManager to schedule tab discarding. To
    make things simpler, remove all actions other than tab discarding from
    the default policy.
    
    BUG=696844
    
    Review-Url: https://codereview.chromium.org/2943283002
    Cr-Commit-Position: refs/heads/master@{#481390}
    
    Former-commit-id: c8af805863f40f75271df364e73ad02c2f6a18f2

--- content/browser/memory/memory_coordinator_default_policy.cc
+++ content/browser/memory/memory_coordinator_default_policy.cc
@@ -6,18 +6,6 @@
 
 namespace content {
 
-namespace {
-
-const int kDefaultBackgroundChildPurgeCandidatePeriodSeconds = 30;
-
-MemoryState CalculateMemoryStateForProcess(MemoryCondition condition) {
-  if (condition == MemoryCondition::CRITICAL)
-    return MemoryState::THROTTLED;
-  return MemoryState::NORMAL;
-}
-
-}  // namespace
-
 MemoryCoordinatorDefaultPolicy::MemoryCoordinatorDefaultPolicy(
     MemoryCoordinatorImpl* coordinator)
     : coordinator_(coordinator) {
@@ -27,69 +15,15 @@ MemoryCoordinatorDefaultPolicy::MemoryCoordinatorDefaultPolicy(
 MemoryCoordinatorDefaultPolicy::~MemoryCoordinatorDefaultPolicy() {}
 
 void MemoryCoordinatorDefaultPolicy::OnCriticalCondition() {
+  // Just trigger tab discarding for now.
   coordinator_->DiscardTab();
-
-  // Prefer to purge memory from child processes than browser process because
-  // we prioritize the brower process.
-  if (TryToPurgeMemoryFromChildren(PurgeTarget::ALL))
-    return;
-
-  coordinator_->TryToPurgeMemoryFromBrowser();
 }
 
 void MemoryCoordinatorDefaultPolicy::OnConditionChanged(MemoryCondition prev,
-                                                        MemoryCondition next) {
-  DCHECK(prev != next);
-  if (next == MemoryCondition::NORMAL) {
-    // Set NORMAL state to all clients/processes.
-    coordinator_->SetBrowserMemoryState(MemoryState::NORMAL);
-    SetMemoryStateToAllChildren(MemoryState::NORMAL);
-  } else if (next == MemoryCondition::CRITICAL) {
-    // Set THROTTLED state to all clients/processes.
-    coordinator_->SetBrowserMemoryState(MemoryState::THROTTLED);
-    SetMemoryStateToAllChildren(MemoryState::THROTTLED);
-  }
-}
+                                                        MemoryCondition next) {}
 
 void MemoryCoordinatorDefaultPolicy::OnChildVisibilityChanged(
     int render_process_id,
-    bool is_visible) {
-  auto iter = coordinator_->children().find(render_process_id);
-  if (iter == coordinator_->children().end())
-    return;
-
-  iter->second.is_visible = is_visible;
-  if (!is_visible) {
-    // A backgrounded process becomes a candidate for purging memory when
-    // the process remains backgrounded for a certian period of time.
-    iter->second.can_purge_after =
-        coordinator_->NowTicks() +
-        base::TimeDelta::FromSeconds(
-            kDefaultBackgroundChildPurgeCandidatePeriodSeconds);
-  }
-  MemoryState new_state =
-      CalculateMemoryStateForProcess(coordinator_->GetMemoryCondition());
-  coordinator_->SetChildMemoryState(render_process_id, new_state);
-}
-
-void MemoryCoordinatorDefaultPolicy::SetMemoryStateToAllChildren(
-    MemoryState state) {
-  // It's OK to call SetChildMemoryState() unconditionally because it checks
-  // whether this state transition is valid.
-  for (auto& iter : coordinator_->children())
-    coordinator_->SetChildMemoryState(iter.first, state);
-}
-
-bool MemoryCoordinatorDefaultPolicy::TryToPurgeMemoryFromChildren(
-    PurgeTarget target) {
-  // TODO(bashi): Better to sort child processes based on their priorities.
-  for (auto& iter : coordinator_->children()) {
-    if (iter.second.is_visible && target == PurgeTarget::BACKGROUNDED)
-      continue;
-    if (coordinator_->TryToPurgeMemoryFromChild(iter.first))
-      return true;
-  }
-  return false;
-}
+    bool is_visible) {}
 
 }  // namespace content
--- content/browser/memory/memory_coordinator_impl.h
+++ content/browser/memory/memory_coordinator_impl.h
@@ -163,7 +163,11 @@ class CONTENT_EXPORT MemoryCoordinatorImpl : public base::MemoryCoordinator,
   void SetDelegateForTesting(
       std::unique_ptr<MemoryCoordinatorDelegate> delegate);
 
+  // Sets a policy for testing.
+  void SetPolicyForTesting(std::unique_ptr<Policy> policy);
+
   MemoryCoordinatorDelegate* delegate() { return delegate_.get(); }
+  Policy* policy() { return policy_.get(); }
 
   // Adds the given ChildMemoryCoordinator as a child of this coordinator.
   void AddChildForTesting(int dummy_render_process_id,
@@ -182,14 +186,9 @@ class CONTENT_EXPORT MemoryCoordinatorImpl : public base::MemoryCoordinator,
 #if !defined(OS_MACOSX)
   FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplBrowserTest, HandleAdded);
 #endif
-  FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, OnChildVisibilityChanged);
   FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, CalculateNextCondition);
-  FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, UpdateCondition);
-  FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, SetMemoryStateForTesting);
   FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, ForceSetMemoryCondition);
   FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, DiscardTabUnderCritical);
-  FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, OnWarningCondition);
-  FRIEND_TEST_ALL_PREFIXES(MemoryCoordinatorImplTest, OnCriticalCondition);
 
   friend class MemoryCoordinatorHandleImpl;
 
