e705d093de5569d4cf92cd6aa961ad05c3d4c8ee
Hidehiko Abe <hidehiko@chromium.org>
2017-06-22 14:05:45 +0900

    Remove the usage of BrowserThread::FILE in ArcPrintService.
    The usage of BrowserThread::FILE is deprecated in favor of the
    post_task.h API.
    
    Cancels the inflight operation, if the ArcPrintService is destroyed.
    
    BUG=689520
    TEST=Ran try.
    
    Change-Id: I894034a24e7e5ced38901d6a6cbaa669f672548c
    Reviewed-on: https://chromium-review.googlesource.com/544417
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Sergey Poromov <poromov@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483044}
    Former-commit-id: 456c704844c441dbaad088a1af8805916ddfde29

--- chrome/browser/chromeos/arc/print/arc_print_service.h
+++ chrome/browser/chromeos/arc/print/arc_print_service.h
@@ -5,7 +5,11 @@
 #ifndef CHROME_BROWSER_CHROMEOS_ARC_PRINT_ARC_PRINT_SERVICE_H_
 #define CHROME_BROWSER_CHROMEOS_ARC_PRINT_ARC_PRINT_SERVICE_H_
 
+#include "base/files/file.h"
 #include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "base/optional.h"
+#include "base/threading/thread_checker.h"
 #include "components/arc/arc_service.h"
 #include "components/arc/common/print.mojom.h"
 #include "components/arc/instance_holder.h"
@@ -22,14 +26,21 @@ class ArcPrintService : public ArcService,
   explicit ArcPrintService(ArcBridgeService* bridge_service);
   ~ArcPrintService() override;
 
-  // InstanceHolder<mojom::PrintInstance>::Observer:
+  // InstanceHolder<mojom::PrintInstance>::Observer override:
   void OnInstanceReady() override;
 
+  // mojom::PrintHost override:
   void Print(mojo::ScopedHandle pdf_data) override;
 
  private:
+  // Opens the pdf file at |file_path|.
+  // If given |file_path| is nullopt, do nothing.
+  void OpenPdf(base::Optional<base::FilePath> file_path) const;
+
+  THREAD_CHECKER(thread_checker_);
   mojo::Binding<mojom::PrintHost> binding_;
 
+  base::WeakPtrFactory<ArcPrintService> weak_ptr_factory_;
   DISALLOW_COPY_AND_ASSIGN(ArcPrintService);
 };
 
