2177b79db31bd3703e00264f6664fe62c3ad22fe
Kenichi Ishibashi <bashi@chromium.org>
2017-06-22 16:16:28 +0900

    Remove PPAPI related code from tools/idl_parser
    It seems there was an intention of merging lexer/parser for PPAPI IDL
    and WebIDL but no one has been working on it recently. PPAPI has its own
    lexer/parser in ppapi/generators. Blink is the only user of
    tools/idl_parser in the code base with some custom extensions (See
    third_party/WebKit/Source/bindings/scripts/blink_idl_parser.py).
    
    The grammar of WebIDL has been actively changing and maintaining both
    idl_parser.py and blink_idl_parser.py is troublesome. Let's remove PPAPI
    support from tools/idl_parser so that we can easily keep the parser
    up-to-date.
    
    BUG=617899
    
    Change-Id: Ia4b8c7918245cce57c7a14326e5fc5c48ba42127
    Reviewed-on: https://chromium-review.googlesource.com/544401
    Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Kenichi Ishibashi <bashi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481704}
    Former-commit-id: 23e996b5055fc9de59db10ea8246a2ff386753c2

--- tools/idl_parser/idl_lexer.py
+++ tools/idl_parser/idl_lexer.py
@@ -3,12 +3,12 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-""" Lexer for PPAPI IDL
+""" Lexer for Web IDL
 
-The lexer uses the PLY library to build a tokenizer which understands both
-WebIDL and Pepper tokens.
+The lexer uses the PLY library to build a tokenizer which understands
+Web IDL tokens.
 
-WebIDL, and WebIDL regular expressions can be found at:
+Web IDL, and Web IDL regular expressions can be found at:
    http://heycam.github.io/webidl/
 PLY can be found at:
    http://www.dabeaz.com/ply/
