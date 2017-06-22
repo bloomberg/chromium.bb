3430844fd1f3dd94e08c465415dbc6bb754940da
Charles Harrison <csharrison@chromium.org>
2017-06-22 12:02:34 -0400

    [subresource_filter] Reset smart UI on manual permission change
    This patch also updates the smart UI time-to-reset to 24 hours,
    from 30 minutes. The shorter time is not necessary since all the
    permission work is completed.
    
    Bug: 689487
    Change-Id: I42624747400078c8329881f9cc0326609ce36e7b
    Reviewed-on: https://chromium-review.googlesource.com/544316
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Martin Šrámek <msramek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481595}
    Former-commit-id: 8c0554a8ee2573e6ffc77203b00d657757d2ae8c

--- chrome/browser/subresource_filter/subresource_filter_content_settings_manager.h
+++ chrome/browser/subresource_filter/subresource_filter_content_settings_manager.h
@@ -55,6 +55,9 @@ class SubresourceFilterContentSettingsManager
   void OnDidShowUI(const GURL& url);
   bool ShouldShowUIForSite(const GURL& url) const;
   bool should_use_smart_ui() const { return should_use_smart_ui_; }
+  void set_should_use_smart_ui_for_testing(bool should_use_smart_ui) {
+    should_use_smart_ui_ = should_use_smart_ui;
+  }
 
   void ClearSiteMetadata(const GURL& url);
 
@@ -65,7 +68,7 @@ class SubresourceFilterContentSettingsManager
   // Time before showing the UI again on a domain.
   // TODO(csharrison): Consider setting this via a finch param.
   static constexpr base::TimeDelta kDelayBeforeShowingInfobarAgain =
-      base::TimeDelta::FromMinutes(30);
+      base::TimeDelta::FromHours(24);
 
  private:
   // content_settings::Observer:
@@ -101,7 +104,7 @@ class SubresourceFilterContentSettingsManager
   // user initiated through the settings UI.
   bool ignore_settings_changes_ = false;
 
-  const bool should_use_smart_ui_ = false;
+  bool should_use_smart_ui_ = false;
 
   DISALLOW_COPY_AND_ASSIGN(SubresourceFilterContentSettingsManager);
 };
