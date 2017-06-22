4c841ab059fc1a21d663993d8d8a91ac1ef4e41d
meade <meade@chromium.org>
2017-06-21 22:46:40 -0700

    [CSS Typed OM] Add missing error handling for CSSRotation constructor
    Note: breaks current is2D handling, but it isn't consistent with the
    spec right now anyway (https://drafts.css-houdini.org/css-typed-om/#dom-csstransformcomponent-is2d)
    
    BUG=545318
    
    Review-Url: https://codereview.chromium.org/2938353002
    Cr-Commit-Position: refs/heads/master@{#481455}
    
    Former-commit-id: c736555ad42dd36b986506e05bc532529bf5f808

--- third_party/WebKit/Source/core/css/cssom/CSSRotation.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSRotation.cpp
@@ -4,6 +4,7 @@
 
 #include "core/css/cssom/CSSRotation.h"
 
+#include "bindings/core/v8/ExceptionState.h"
 #include "core/css/CSSFunctionValue.h"
 #include "core/css/CSSPrimitiveValue.h"
 
@@ -59,6 +60,26 @@ CSSRotation* FromCSSRotateXYZ(const CSSFunctionValue& value) {
 }
 
 }  // namespace
+
+CSSRotation* CSSRotation::Create(double x,
+                                 double y,
+                                 double z,
+                                 CSSNumericValue* angle,
+                                 ExceptionState& exception_state) {
+  if (angle->GetType() != CSSStyleValue::StyleValueType::kAngleType) {
+    exception_state.ThrowTypeError("Must pass an angle to CSSRotation");
+    return nullptr;
+  }
+  return new CSSRotation(x, y, z, angle);
+}
+
+CSSRotation* CSSRotation::Create(double x,
+                                 double y,
+                                 double z,
+                                 CSSNumericValue* angle) {
+  DCHECK_EQ(angle->GetType(), CSSStyleValue::StyleValueType::kAngleType);
+  return new CSSRotation(x, y, z, angle);
+}
 
 CSSRotation* CSSRotation::FromCSSValue(const CSSFunctionValue& value) {
   switch (value.FunctionType()) {
