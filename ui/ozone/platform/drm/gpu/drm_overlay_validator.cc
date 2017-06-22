101c8eb7a0a718c1e35165624fcae694ddab4e77
Dongseong Hwang <dongseong.hwang@intel.com>
2017-06-21 20:08:29 -0700

    cros, gbm: scale a overlay plane by native scaler, instead of VPP
    Currently, for Intel, VPP (video processing hardware)[1] scales a overlay plane,
    instead of display controller [2].
    
    [1] https://software.intel.com/en-us/node/628415
    [2] https://01.org/sites/default/files/documentation/intel-gfx-prm-osrc-skl-vol12-display.pdf
    
    VPP scaler was introduced to support hardware overlay for old Intel SoCs.
    However VPP scaling doesn't make sense because it consumes more bandwidth
    (results in power) than GPU compositing. VPP needs to read the given buffer and
    then write to new temp buffer. It already consumes similar bandwidth to GPU
    compositing, but hardware overlay path still needs to send the overlay plane
    to display controller. This CL removes this incorrect legacy, and make
    scaling rely on the native scaler which atomic modeset API natively supports,
    which finally display controller scales the plane by itself. For Intel, Skylake
    or newer SoCs support it. [2]
    
    In the code side, only Intel sets OverlayPlane::ProcessBufferCallback,
    which points out VPP scaler.
    HardwareDisplayPlaneManager already uses native scaler (i.e. display controller
    scaler) via |crtc_w| and |crtc_h| value on drmModeAtomicAddProperty() call,
    only if OverlayPlane::ProcessBufferCallback isn't set.
    
    ARM and Rockchip don't set the callback and rely on native scaler already. cc
    checks if display controller can support scale via OverlayCandidateValidator.
    If not, cc composites the layer by GPU.
    
    BUG=683347
    TEST=run https://www.quirksmode.org/html5/tests/video.html on reef and eve
    using cros kernel intel-graphics/syncfence_ams_npf_mst_607_r2 branch
    
    Change-Id: Id492c0317313fe6a905ee96797ceefbb9746d798
    Reviewed-on: https://chromium-review.googlesource.com/544617
    Commit-Queue: Dongseong Hwang <dongseong.hwang@intel.com>
    Reviewed-by: Daniel Nicoara <dnicoara@chromium.org>
    Reviewed-by: Dongseong Hwang <dongseong.hwang@intel.com>
    Cr-Commit-Position: refs/heads/master@{#483086}
    Former-commit-id: a9bb336f1ec642759d6c1348cbff82f706d6e598

--- ui/ozone/platform/drm/gpu/drm_overlay_validator.cc
+++ ui/ozone/platform/drm/gpu/drm_overlay_validator.cc
@@ -17,18 +17,6 @@ namespace ui {
 
 namespace {
 
-const size_t kMaxCacheSize = 200;
-
-bool NeedsAlphaComposition(uint32_t format) {
-  switch (format) {
-    case DRM_FORMAT_XRGB8888:
-    case DRM_FORMAT_UYVY:
-      return false;
-    default:
-      return true;
-  }
-}
-
 scoped_refptr<ScanoutBuffer> GetBufferForPageFlipTest(
     const scoped_refptr<DrmDevice>& drm_device,
     const gfx::Size& size,
@@ -51,56 +39,12 @@ scoped_refptr<ScanoutBuffer> GetBufferForPageFlipTest(
   return scanout_buffer;
 }
 
-gfx::Size GetScaledSize(const gfx::Size original_size,
-                        const gfx::Rect display_rect,
-                        const gfx::RectF crop_rect) {
-  if (!crop_rect.IsEmpty()) {
-    return gfx::ToCeiledSize(
-        gfx::SizeF(display_rect.width() / crop_rect.width(),
-                   display_rect.height() / crop_rect.height()));
-  }
-
-  return original_size;
-}
-
-uint32_t FindOptimalBufferFormat(uint32_t original_format,
-                                 uint32_t plane_z_order,
-                                 const gfx::Rect& plane_bounds,
-                                 const gfx::Rect& window_bounds,
-                                 HardwareDisplayController* controller) {
-  uint32_t z_order = plane_z_order;
-  // If Overlay completely covers primary and isn't transparent, try to find
-  // optimal format w.r.t primary plane. This guarantees that optimal format
-  // would not fail page flip when plane manager/CC collapses planes.
-  if (plane_bounds == window_bounds &&
-      !NeedsAlphaComposition(original_format)) {
-    z_order = 0;
-  }
-
-  // YUV is preferable format if supported.
-  if (controller->IsFormatSupported(DRM_FORMAT_UYVY, z_order)) {
-    return DRM_FORMAT_UYVY;
-  } else if (controller->IsFormatSupported(DRM_FORMAT_XRGB8888, z_order)) {
-    return DRM_FORMAT_XRGB8888;
-  }
-
-  return original_format;
-}
-
 }  // namespace
-
-DrmOverlayValidator::OverlayHints::OverlayHints(uint32_t format,
-                                                bool scale_buffer)
-    : optimal_format(format), handle_scaling(scale_buffer) {}
-
-DrmOverlayValidator::OverlayHints::~OverlayHints() {}
 
 DrmOverlayValidator::DrmOverlayValidator(
     DrmWindow* window,
     ScanoutBufferGenerator* buffer_generator)
-    : window_(window),
-      buffer_generator_(buffer_generator),
-      overlay_hints_cache_(kMaxCacheSize) {}
+    : window_(window), buffer_generator_(buffer_generator) {}
 
 DrmOverlayValidator::~DrmOverlayValidator() {}
 
@@ -130,13 +74,16 @@ std::vector<OverlayCheckReturn_Params> DrmOverlayValidator::TestPageFlip(
       continue;
     }
 
-    gfx::Size scaled_buffer_size = GetScaledSize(
-        params[i].buffer_size, params[i].display_rect, params[i].crop_rect);
-
     uint32_t original_format =
         params[i].plane_z_order
             ? GetFourCCFormatFromBufferFormat(params[i].format)
             : GetFourCCFormatForOpaqueFramebuffer(params[i].format);
+    if (!controller->IsFormatSupported(original_format,
+                                       params[i].plane_z_order)) {
+      returns[i].status = OverlayCheckReturn_Params::Status::NOT;
+      continue;
+    }
+
     scoped_refptr<ScanoutBuffer> buffer =
         GetBufferForPageFlipTest(drm, params[i].buffer_size, original_format,
                                  buffer_generator_, &reusable_buffers);
@@ -147,38 +94,6 @@ std::vector<OverlayCheckReturn_Params> DrmOverlayValidator::TestPageFlip(
 
     if (buffer && controller->TestPageFlip(test_list)) {
       returns[i].status = OverlayCheckReturn_Params::Status::ABLE;
-
-      // If size scaling is needed, find an optimal format.
-      if (params[i].plane_z_order &&
-          scaled_buffer_size != params[i].buffer_size) {
-        uint32_t optimal_format = FindOptimalBufferFormat(
-            original_format, params[i].plane_z_order, params[i].display_rect,
-            window_->bounds(), controller);
-
-        if (original_format != optimal_format) {
-          OverlayPlane original_plain = test_list.back();
-          test_list.pop_back();
-          scoped_refptr<ScanoutBuffer> optimal_buffer =
-              GetBufferForPageFlipTest(drm, scaled_buffer_size, optimal_format,
-                                       buffer_generator_, &reusable_buffers);
-          DCHECK(optimal_buffer);
-
-          OverlayPlane optimal_plane(
-              optimal_buffer, params[i].plane_z_order, params[i].transform,
-              params[i].display_rect, params[i].crop_rect);
-          test_list.push_back(optimal_plane);
-
-          // If test failed here, it means even though optimal_format is
-          // supported, platform cannot support it with current combination of
-          // layers. This is usually the case when optimal_format needs certain
-          // capabilites (i.e. conversion, scaling etc) and needed hardware
-          // resources might be already in use. Fall back to original format.
-          if (!controller->TestPageFlip(test_list)) {
-            test_list.pop_back();
-            test_list.push_back(original_plain);
-          }
-        }
-      }
     } else {
       // If test failed here, platform cannot support this configuration
       // with current combination of layers. This is usually the case when this
@@ -191,86 +106,7 @@ std::vector<OverlayCheckReturn_Params> DrmOverlayValidator::TestPageFlip(
     }
   }
 
-  UpdateOverlayHintsCache(test_list);
-
   return returns;
-}
-
-OverlayPlaneList DrmOverlayValidator::PrepareBuffersForPageFlip(
-    const OverlayPlaneList& planes) {
-  if (planes.size() <= 1)
-    return planes;
-
-  HardwareDisplayController* controller = window_->GetController();
-  if (!controller)
-    return planes;
-
-  OverlayPlaneList pending_planes = planes;
-  const auto& overlay_hints = overlay_hints_cache_.Get(planes);
-
-  size_t size = planes.size();
-  bool use_hints = overlay_hints != overlay_hints_cache_.end();
-
-  for (size_t i = 0; i < size; i++) {
-    auto& plane = pending_planes.at(i);
-    if (plane.processing_callback.is_null())
-      continue;
-
-    uint32_t original_format = plane.buffer->GetFramebufferPixelFormat();
-    uint32_t target_format = original_format;
-
-    const gfx::Size& original_size = plane.buffer->GetSize();
-    gfx::Size target_size =
-        GetScaledSize(original_size, plane.display_bounds, plane.crop_rect);
-
-    if (use_hints) {
-      DCHECK(size == overlay_hints->second.size());
-      const OverlayHints& hints = overlay_hints->second.at(i);
-      target_format = hints.optimal_format;
-
-      // We can handle plane scaling, avoid scaling buffer here.
-      if (!hints.handle_scaling)
-        target_size = original_size;
-    }
-
-    // The size scaling piggybacks the format conversion.
-    if (original_size != target_size) {
-      scoped_refptr<ScanoutBuffer> processed_buffer =
-          plane.processing_callback.Run(target_size, target_format);
-
-      if (processed_buffer)
-        plane.buffer = processed_buffer;
-    }
-  }
-
-  return pending_planes;
-}
-
-void DrmOverlayValidator::ClearCache() {
-  overlay_hints_cache_.Clear();
-}
-
-void DrmOverlayValidator::UpdateOverlayHintsCache(
-    const OverlayPlaneList& plane_list) {
-  const auto& iter = overlay_hints_cache_.Get(plane_list);
-  if (iter != overlay_hints_cache_.end())
-    return;
-
-  OverlayPlaneList hints_plane_list = plane_list;
-  OverlayHintsList overlay_hints;
-  for (auto& plane : hints_plane_list) {
-    uint32_t format = plane.buffer->GetFramebufferPixelFormat();
-    // TODO(kalyank): We always request scaling to be done by 3D engine, VPP
-    // etc. We should use them only if downscaling is needed and let display
-    // controller handle up-scaling on platforms which support it.
-    overlay_hints.push_back(OverlayHints(format, true /* scaling */));
-
-    // Make sure we dont hold reference to buffer when caching this plane list.
-    plane.buffer = nullptr;
-  }
-
-  DCHECK(hints_plane_list.size() == overlay_hints.size());
-  overlay_hints_cache_.Put(hints_plane_list, overlay_hints);
 }
 
 }  // namespace ui
--- ui/ozone/platform/drm/gpu/drm_overlay_validator.h
+++ ui/ozone/platform/drm/gpu/drm_overlay_validator.h
@@ -28,44 +28,9 @@ class DrmOverlayValidator {
       const std::vector<OverlayCheck_Params>& params,
       const OverlayPlaneList& last_used_planes);
 
-  // Checks if buffers bound to planes can be optimized (i.e. format, scaling)to
-  // reduce Display Read bandwidth. This should be called before an actual page
-  // flip. Expects |planes| to be sorted by Z order.
-  OverlayPlaneList PrepareBuffersForPageFlip(const OverlayPlaneList& planes);
-
-  // Clears internal cache of validated overlay configurations. This should be
-  // usually called when |window_| size has changed or moved controller.
-  void ClearCache();
-
  private:
-  // Contains hints which can be used to reduce display read memory bandwidth,
-  // for a given OverlayPlane. These are useful in case of video to determine
-  // if converting the buffer storage format before composition could lead to
-  // any potential bandwidth savings. Other useful hint is to determine if
-  // scaling needs to be done before page flip or can be handled by plane.
-  struct OverlayHints {
-    OverlayHints(uint32_t optimal_format, bool handle_scaling);
-    ~OverlayHints();
-    // Optimal buffer storage format supported by hardware for a given
-    // OverlayPlane. This hint can be ignored and still compositing an
-    // OverlayPlane should not fail page flip or cause any visual artifacts.
-    uint32_t optimal_format;
-    // Hints if buffer scaling needs to be done before page flip as plane cannot
-    // support it. Ignoring this hint may result in displaying buffer with wrong
-    // resolution.
-    bool handle_scaling;
-  };
-
-  using OverlayHintsList = std::vector<OverlayHints>;
-
-  // Update hints cache.
-  void UpdateOverlayHintsCache(const OverlayPlaneList& plane_list);
-
   DrmWindow* window_;  // Not owned.
   ScanoutBufferGenerator* buffer_generator_;  // Not owned.
-
-  // List of all configurations which have been validated.
-  base::MRUCache<OverlayPlaneList, OverlayHintsList> overlay_hints_cache_;
 
   DISALLOW_COPY_AND_ASSIGN(DrmOverlayValidator);
 };
