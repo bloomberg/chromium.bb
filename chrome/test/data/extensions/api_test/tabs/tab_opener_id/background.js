8fb845cb40242b5fb1d9fa6ed309d06f64dff924
Catherine Mullings <catmullings@chromium.org>
2017-06-22 11:45:25 -0700

    Prevent openerTabId to be set to the tab opened by the tab opener
    Setting the openerTabId param of any tab to its own tab id via
    chrome.tabs API should not be allowed. For example, if tab 1 opened
    tab 2, tab 2's openerTabId value should not be allowed to be 2.
    
    This can lead to crashes, for example, when if openerTabId equals the
    tab's id and the tab is closed.
    
    This patch prevents the aforementioned from occurring and throws a
    console error to inform the developer.
    
    Bug: 709961
    Change-Id: I8513091b11088447477f4709ec605b78dafe7b0a
    Reviewed-on: https://chromium-review.googlesource.com/544224
    Commit-Queue: catmullings <catmullings@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481658}
    Former-commit-id: f125703b5fedcaa29123407147abdb568e0542f5

--- chrome/test/data/extensions/api_test/tabs/tab_opener_id/background.js
+++ chrome/test/data/extensions/api_test/tabs/tab_opener_id/background.js
@@ -5,14 +5,15 @@
 // Add a listener just so we dispatch onUpdated events.
 chrome.tabs.onUpdated.addListener(function(update) {});
 
+var port;
+var getUrl = function(url) {
+  return 'http://localhost:' + port + '/' + url;
+};
+
 // Test a series of crazy events where we can set a tab's opener, then close it,
 // and hope the browser does the right thing.
 // Regression test for crbug.com/698681.
-chrome.test.getConfig((config) => {
-  var getUrl = function(url) {
-    return 'http://localhost:' + config.testServer.port + '/' + url;
-  };
-
+function testSetOpenerOutsideOfWindow() {
   var url1 = getUrl('title1.html');
   var url2 = getUrl('title2.html');
   var url3 = getUrl('title3.html');
@@ -32,8 +33,8 @@ chrome.test.getConfig((config) => {
       chrome.test.assertTrue(!!secondWin.tabs);
       chrome.test.assertEq(1, secondWin.tabs.length);
 
-      // Try to update the tab in the second window to have an opener of a tab
-      // in the first window. This *should* fail.
+      // Try to update the tab in the second window to have an opener of a
+      // tab in the first window. This *should* fail.
       chrome.tabs.update(secondWin.tabs[0].id, {openerTabId: openerId}, () => {
         chrome.test.assertLastError(
             'Tab opener must be in the same window as the updated tab.');
@@ -51,4 +52,33 @@ chrome.test.getConfig((config) => {
       });
     });
   });
+}
+
+// Tests that the tab in a window cannot be set to have an opener of itself.
+// Regression test for crbug.com/709961
+function testSetOpenerToSelf() {
+  var url1 = getUrl('title1.html');
+
+  // create a new window with one tab.
+  chrome.windows.create({url: [url1]}, (win) => {
+    chrome.test.assertNoLastError();
+    chrome.test.assertTrue(!!win);
+    chrome.test.assertTrue(!!win.tabs);
+    chrome.test.assertEq(1, win.tabs.length);
+    var openerId = win.tabs[0].id;
+
+    // Try to update the tab in the window to have an opener of itself.
+    // This *should* fail.
+    chrome.tabs.update(openerId, {openerTabId: openerId}, () => {
+      chrome.test.assertLastError(
+          'Cannot set a tab\'s opener to itself.');
+      chrome.test.succeed();
+    });
+  });
+}
+
+chrome.test.getConfig((config) => {
+  port = config.testServer.port;
+
+  chrome.test.runTests([testSetOpenerOutsideOfWindow, testSetOpenerToSelf]);
 });
