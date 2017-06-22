2ce4bfdb12cc01b33ff3c3c4900ffde5a2815dbf
dgozman <dgozman@chromium.org>
2017-06-22 15:58:13 -0700

    [DevTools] Migrate inspector-protocol/animation tests to new harness
    BUG=734762
    
    Review-Url: https://codereview.chromium.org/2950193003
    Cr-Commit-Position: refs/heads/master@{#481702}
    
    Former-commit-id: e530921fff950ad4e2da1c99c1d61aa1e4eb5ef9

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/animation/animation-empty-transition-cancel.js
@@ -0,0 +1,19 @@
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startHTML(`
+    <div id='node' style='background-color: red; width: 100px'></div>
+  `, '');
+
+  dp.Animation.onAnimationCreated(() => testRunner.log('Animation created'));
+  dp.Animation.onAnimationCanceled(() => testRunner.log('Animation canceled'));
+  dp.Animation.enable();
+  await session.evaluate(`
+    node.offsetTop;
+    node.style.transition = "1s";
+    node.offsetTop;
+    node.style.width = "200px";
+    node.offsetTop;
+    node.style.transition = "";
+    node.offsetTop;
+  `);
+  testRunner.completeTest();
+})
