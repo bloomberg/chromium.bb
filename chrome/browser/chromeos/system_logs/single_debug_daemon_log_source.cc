bea2b372f5745d2079041ef5a770c44153b2e0f8
sque <sque@chromium.org>
2017-06-23 07:44:54 -0700

    Add log source to read from one DebugDaemon log
    DebugDaemon has a function, GetLog(), that returns the output of one
    log command. This patch adds a new log source class to fetch the output
    of GetLog().
    
    BUG=733846
    
    Review-Url: https://codereview.chromium.org/2955463002
    Cr-Commit-Position: refs/heads/master@{#481884}
    
    Former-commit-id: f1f0b837c9b55453209f84a4fb8dbecec1f383da

---
+++ chrome/browser/chromeos/system_logs/single_debug_daemon_log_source.cc
@@ -0,0 +1,68 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/system_logs/single_debug_daemon_log_source.h"
+
+#include "base/bind.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/dbus/debug_daemon_client.h"
+#include "components/feedback/anonymizer_tool.h"
+#include "content/public/browser/browser_thread.h"
+
+namespace system_logs {
+
+namespace {
+
+using SupportedSource = SingleDebugDaemonLogSource::SupportedSource;
+
+// Converts a logs source type to the corresponding debugd log name.
+std::string GetLogName(SupportedSource source_type) {
+  switch (source_type) {
+    case SupportedSource::kModetest:
+      return "modetest";
+    case SupportedSource::kLsusb:
+      return "lsusb";
+  }
+  NOTREACHED();
+  return "";
+}
+
+}  // namespace
+
+SingleDebugDaemonLogSource::SingleDebugDaemonLogSource(
+    SupportedSource source_type)
+    : SystemLogsSource(GetLogName(source_type)), weak_ptr_factory_(this) {}
+
+SingleDebugDaemonLogSource::~SingleDebugDaemonLogSource() {}
+
+void SingleDebugDaemonLogSource::Fetch(const SysLogsSourceCallback& callback) {
+  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK(!callback.is_null());
+
+  chromeos::DebugDaemonClient* client =
+      chromeos::DBusThreadManager::Get()->GetDebugDaemonClient();
+
+  client->GetLog(
+      source_name(),
+      base::Bind(&SingleDebugDaemonLogSource::OnFetchComplete,
+                 weak_ptr_factory_.GetWeakPtr(), source_name(), callback));
+}
+
+void SingleDebugDaemonLogSource::OnFetchComplete(
+    const std::string& log_name,
+    const SysLogsSourceCallback& callback,
+    bool success,
+    const std::string& result) const {
+  // |result| and |response| are the same type, but |result| is passed in from
+  // DebugDaemonClient, which does not use the SystemLogsResponse alias.
+  SystemLogsResponse response;
+  // Return an empty result if the call to GetLog() failed.
+  std::string final_result;
+  if (success)
+    response.emplace(log_name, feedback::AnonymizerTool().Anonymize(result));
+
+  callback.Run(&response);
+}
+
+}  // namespace system_logs
---
+++ chrome/browser/chromeos/system_logs/single_debug_daemon_log_source.h
@@ -0,0 +1,46 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_SYSTEM_LOGS_SINGLE_DEBUG_DAEMON_LOG_SOURCE_H_
+#define CHROME_BROWSER_CHROMEOS_SYSTEM_LOGS_SINGLE_DEBUG_DAEMON_LOG_SOURCE_H_
+
+#include <stddef.h>
+
+#include "base/macros.h"
+#include "chrome/browser/feedback/system_logs/system_logs_fetcher.h"
+
+namespace system_logs {
+
+// Gathers log data from a single debugd log source, via DebugDaemonClient.
+class SingleDebugDaemonLogSource : public SystemLogsSource {
+ public:
+  enum class SupportedSource {
+    // For "modetest" command.
+    kModetest,
+
+    // For "lsusb" command.
+    kLsusb,
+  };
+
+  explicit SingleDebugDaemonLogSource(SupportedSource source);
+  ~SingleDebugDaemonLogSource() override;
+
+  // system_logs::SystemLogsSource:
+  void Fetch(const SysLogsSourceCallback& callback) override;
+
+ private:
+  // Callback for handling response from DebugDaemonClient.
+  void OnFetchComplete(const std::string& log_name,
+                       const SysLogsSourceCallback& callback,
+                       bool success,
+                       const std::string& result) const;
+
+  base::WeakPtrFactory<SingleDebugDaemonLogSource> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(SingleDebugDaemonLogSource);
+};
+
+}  // namespace system_logs
+
+#endif  // CHROME_BROWSER_CHROMEOS_SYSTEM_LOGS_SINGLE_DEBUG_DAEMON_LOG_SOURCE_H_
