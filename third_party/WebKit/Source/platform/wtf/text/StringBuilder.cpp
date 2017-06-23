585f307d8596a2a1a59a86c5cac16dc64abb938a
kojii <kojii@chromium.org>
2017-06-23 09:31:23 -0700

    Add StringBuilder::erase(index)
    The function name follows the WTF Vector convention.
    
    Needed in LayoutNG.
    
    BUG=636993
    
    Review-Url: https://codereview.chromium.org/2956603002
    Cr-Commit-Position: refs/heads/master@{#481917}
    
    Former-commit-id: 37fd3477f0a335abbc3f5ebf6b8121e0be19edba

--- third_party/WebKit/Source/platform/wtf/text/StringBuilder.cpp
+++ third_party/WebKit/Source/platform/wtf/text/StringBuilder.cpp
@@ -230,4 +230,18 @@ void StringBuilder::AppendNumber(double number, unsigned precision) {
   Append(NumberToFixedPrecisionString(number, precision, buffer));
 }
 
+void StringBuilder::erase(unsigned index) {
+  if (index >= length_)
+    return;
+
+  if (is8_bit_) {
+    EnsureBuffer8(0);
+    buffer8_->erase(index);
+  } else {
+    EnsureBuffer16(0);
+    buffer16_->erase(index);
+  }
+  --length_;
+}
+
 }  // namespace WTF
