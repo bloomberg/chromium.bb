fccf5462d6d41a19a47562b425f780750e2c2fc4
pcc <pcc@chromium.org>
2017-06-23 14:11:52 -0700

    Remove support for downloading gold plugin.
    Now that https://codereview.chromium.org/2957523002 has landed it
    is an error to use the gold plugin (except with ChromeOS, which has
    its own copy of the plugin), so we no longer need to be able to
    download it. The only other user of the download_gold_plugin.py
    script that I am aware of was removed from the build repo in
    https://chromium-review.googlesource.com/c/545085/ .
    
    This doesn't remove support for building the plugin because v8 still
    uses it I think.
    
    BUG=607968
    R=thakis@chromium.org
    
    Review-Url: https://codereview.chromium.org/2953283002
    Cr-Commit-Position: refs/heads/master@{#482031}
    
    Former-commit-id: 1bb8cb7bc4944ce1687c7d3980bb10f3547fce1c

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -396,15 +396,9 @@ def VeryifyVersionOfBuiltClangMatchesVERSION():
 def UpdateClang(args):
   print 'Updating Clang to %s...' % PACKAGE_VERSION
 
-  need_gold_plugin = 'LLVM_DOWNLOAD_GOLD_PLUGIN' in os.environ or (
-      sys.platform.startswith('linux') and
-      'buildtype=Official' in os.environ.get('GYP_DEFINES', ''))
-
   if ReadStampFile() == PACKAGE_VERSION and not args.force_local_build:
     print 'Clang is already up to date.'
-    if not need_gold_plugin or os.path.exists(
-        os.path.join(LLVM_BUILD_DIR, "lib/LLVMgold.so")):
-      return 0
+    return 0
 
   # Reset the stamp file in case the build is unsuccessful.
   WriteStampFile('')
@@ -427,11 +421,6 @@ def UpdateClang(args):
       print 'clang %s unpacked' % PACKAGE_VERSION
       if sys.platform == 'win32':
         CopyDiaDllTo(os.path.join(LLVM_BUILD_DIR, 'bin'))
-      # Download the gold plugin if requested to by an environment variable.
-      # This is used by the CFI ClusterFuzz bot, and it's required for official
-      # builds on linux.
-      if need_gold_plugin:
-        RunCommand(['python', CHROMIUM_DIR+'/build/download_gold_plugin.py'])
       WriteStampFile(PACKAGE_VERSION)
       return 0
     except urllib2.URLError:
