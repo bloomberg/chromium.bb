69b775a9bef822eb55ad1112d37f465250f506e9
Xianzhu Wang <wangxianzhu@chromium.org>
2017-06-23 14:19:31 -0700

    Trigger SPv2 trybot on spv2 test expectation change
    BUG=731850
    
    Change-Id: I2b9ca36110a2889e8234378a16cd6b11f1fbfb7b
    Reviewed-on: https://chromium-review.googlesource.com/546837
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482347}
    Former-commit-id: 016f42f92248b8d6c37894f247b6e4e7957fbe5e

--- third_party/WebKit/PRESUBMIT.py
+++ third_party/WebKit/PRESUBMIT.py
@@ -286,6 +286,10 @@ def _ArePaintOrCompositingDirectoriesModified(change):  # pylint: disable=C0103
         os.path.join('third_party', 'WebKit', 'Source', 'core', 'layout',
                      'compositing'),
         os.path.join('third_party', 'WebKit', 'Source', 'core', 'paint'),
+        os.path.join('third_party', 'WebKit', 'LayoutTests', 'FlagExpectations',
+                     'enable-slimming-paint-v2'),
+        os.path.join('third_party', 'WebKit', 'LayoutTests', 'flag-specific',
+                     'enable-slimming-paint-v2'),
     ]
     for affected_file in change.AffectedFiles():
         file_path = affected_file.LocalPath()
