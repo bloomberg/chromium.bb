73b7b91049762a299829c34ab55f561f79723e95
Anita Woodruff <awdf@chromium.org>
2017-06-23 15:41:02 +0100

    [Android Tests] SiteChannelsManagerTest: use custom matcher in all cases
    - Should have been using matchesChannelStatus in all cases where channel
    statuses are checked, for better error messaging on fail.
    
    Bug: 
    Change-Id: If36da189c1707e3e788bbe48cfc9ccf8eccbe71e
    Reviewed-on: https://chromium-review.googlesource.com/545716
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#481897}
    Former-commit-id: 0fa061bae1492a8ea56b07f34368443f58635cca

--- chrome/android/javatests/src/org/chromium/chrome/browser/notifications/channels/SiteChannelsManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/notifications/channels/SiteChannelsManagerTest.java
@@ -71,7 +71,7 @@ public class SiteChannelsManagerTest {
         assertThat(Arrays.asList(mSiteChannelsManager.getSiteChannels()), hasSize(1));
         NotificationSettingsBridge.SiteChannel channel = mSiteChannelsManager.getSiteChannels()[0];
         assertThat(channel.getOrigin(), is("https://chromium.org"));
-        assertThat(channel.getStatus(), is(NotificationChannelStatus.ENABLED));
+        assertThat(channel.getStatus(), matchesChannelStatus(NotificationChannelStatus.ENABLED));
     }
 
     @Test
@@ -82,7 +82,7 @@ public class SiteChannelsManagerTest {
         assertThat(Arrays.asList(mSiteChannelsManager.getSiteChannels()), hasSize(1));
         NotificationSettingsBridge.SiteChannel channel = mSiteChannelsManager.getSiteChannels()[0];
         assertThat(channel.getOrigin(), is("https://example.com"));
-        assertThat(channel.getStatus(), is(NotificationChannelStatus.BLOCKED));
+        assertThat(channel.getStatus(), matchesChannelStatus(NotificationChannelStatus.BLOCKED));
     }
 
     @Test
