43547b67e8c8fd6a0c26344d704f7b367a2df159
Bryan Henry <bryanhenry@google.com>
2017-06-23 12:23:00 -0700

    [Chromecast] Don't override command line switches with defaults
    On Android, the command line is initialized in Java before
    CastBrowserMainParts is created and has a chance to add the default
    switches specified here. Make sure we don't override a switch value
    added in Java with the default.
    
    BUG=internal b/62928634
    TEST=Verified that default values no longer override other values.
    
    Change-Id: Ib09719d166ed13168f866fb7b066145ddd166536
    Reviewed-on: https://chromium-review.googlesource.com/545224
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Commit-Queue: Bryan Henry <bryanhenry@google.com>
    Cr-Commit-Position: refs/heads/master@{#482012}
    Former-commit-id: c6d26b7f1cf8f663456fac11013e73f6b763988d

--- chromecast/browser/cast_browser_main_parts.cc
+++ chromecast/browser/cast_browser_main_parts.cc
@@ -11,6 +11,7 @@
 
 #include "base/command_line.h"
 #include "base/files/file_util.h"
+#include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/message_loop/message_loop.h"
@@ -216,7 +217,7 @@ struct DefaultCommandLineSwitch {
   const char* const switch_value;
 };
 
-DefaultCommandLineSwitch g_default_switches[] = {
+const DefaultCommandLineSwitch kDefaultSwitches[] = {
 #if defined(OS_ANDROID)
     // TODO(714676): this should probably set the no restrictions autoplay
     // policy instead.
@@ -261,16 +262,21 @@ DefaultCommandLineSwitch g_default_switches[] = {
     {switches::kEnableNetworkInformation, ""},
     // TODO(halliwell): Remove after fixing b/35422666.
     {switches::kEnableUseZoomForDSF, "false"},
-    {nullptr, nullptr},  // Termination
 };
 
 void AddDefaultCommandLineSwitches(base::CommandLine* command_line) {
-  int i = 0;
-  while (g_default_switches[i].switch_name != NULL) {
-    command_line->AppendSwitchASCII(
-        std::string(g_default_switches[i].switch_name),
-        std::string(g_default_switches[i].switch_value));
-    ++i;
+  for (const auto& default_switch : kDefaultSwitches) {
+    // Don't override existing command line switch values with these defaults.
+    // This could occur primarily (or only) on Android, where the command line
+    // is initialized in Java first.
+    std::string name(default_switch.switch_name);
+    if (!command_line->HasSwitch(name)) {
+      std::string value(default_switch.switch_value);
+      VLOG(2) << "Set default switch '" << name << "' = '" << value << "'";
+      command_line->AppendSwitchASCII(name, value);
+    } else {
+      VLOG(2) << "Skip setting default switch '" << name << "', already set";
+    }
   }
 }
 
