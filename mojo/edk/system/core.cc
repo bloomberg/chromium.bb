688ea9337763b26c881d780501e5317aeb5d2c34
Ken Rockot <rockot@chromium.org>
2017-06-24 13:35:43 -0700

    Mojo EDK: Add missing RequestContexts
    It's possible for MojoSerializeMessage and
    MojoGetSerializedMessageContents to elicit handle watcher notifications.
    
    This ensures that they have a valid RequestContext in TLS in case that
    happens. 
    
    BUG=725321
    TBR=jcivelli@chromium.org
    
    Change-Id: Id5960cbff0cf7ee12a578f0b066291f11e95bc51
    Reviewed-on: https://chromium-review.googlesource.com/547521
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482163}
    Former-commit-id: 8555f372bec6701b15e143866d4c9a88970df336

--- mojo/edk/system/core.cc
+++ mojo/edk/system/core.cc
@@ -495,6 +495,7 @@ MojoResult Core::DestroyMessage(MojoMessageHandle message_handle) {
 MojoResult Core::SerializeMessage(MojoMessageHandle message_handle) {
   if (!message_handle)
     return MOJO_RESULT_INVALID_ARGUMENT;
+  RequestContext request_context;
   return reinterpret_cast<ports::UserMessageEvent*>(message_handle)
       ->GetMessage<UserMessageImpl>()
       ->SerializeIfNecessary();
@@ -540,6 +541,7 @@ MojoResult Core::GetSerializedMessageContents(
     return MOJO_RESULT_RESOURCE_EXHAUSTED;
   }
 
+  RequestContext request_context;
   return message->ExtractSerializedHandles(
       UserMessageImpl::ExtractBadHandlePolicy::kAbort, handles);
 }
