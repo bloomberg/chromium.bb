45c2a8423e027364423d48bd9f1d2b9a26ac2027
Kenneth Russell <kbr@chromium.org>
2017-06-26 11:08:17 -0700

    Try running context_lost tests on Linux TSAN GPU bot again.
    Suppressions were added a while back; let's see if they've helped.
    
    BUG=691029
    TBR=zmo@chromium.org
    NOTRY=true
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I9aa633145871c3e279b2967e8942831562336cea
    Reviewed-on: https://chromium-review.googlesource.com/548958
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482324}
    Former-commit-id: a2c0592a18bd438cedc03892bdaaf54132f18898

--- content/test/gpu/generate_buildbot_json.py
+++ content/test/gpu/generate_buildbot_json.py
@@ -1584,7 +1584,6 @@ TELEMETRY_GPU_INTEGRATION_TESTS = {
     'tester_configs': [
       {
         'predicate': Predicates.DEFAULT_PLUS_V8,
-        'disabled_instrumentation_types': ['tsan'],
       },
     ],
     'disabled_tester_configs': [
