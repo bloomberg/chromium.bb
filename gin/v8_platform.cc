c9df7664688e6b414b07e2baff910bdd9786b6c4
Jochen Eisinger <jochen@chromium.org>
2017-06-26 17:05:45 +0200

    Implement v8::TracingController
    BUG=v8:6511
    R=fmeawad@chromium.org
    
    Change-Id: I98409f03d252a9e08d5403ec236a5863ef7eeb74
    Reviewed-on: https://chromium-review.googlesource.com/543055
    Commit-Queue: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482286}
    Former-commit-id: 7845aae2e9a7f3a21f6eb79ac8e8f338e8a90aed

--- gin/v8_platform.cc
+++ gin/v8_platform.cc
@@ -54,12 +54,144 @@ void PrintStackTrace() {
   trace.Print();
 }
 
+class ConvertableToTraceFormatWrapper final
+    : public base::trace_event::ConvertableToTraceFormat {
+ public:
+  explicit ConvertableToTraceFormatWrapper(
+      std::unique_ptr<v8::ConvertableToTraceFormat>& inner)
+      : inner_(std::move(inner)) {}
+  ~ConvertableToTraceFormatWrapper() override = default;
+  void AppendAsTraceFormat(std::string* out) const final {
+    inner_->AppendAsTraceFormat(out);
+  }
+
+ private:
+  std::unique_ptr<v8::ConvertableToTraceFormat> inner_;
+
+  DISALLOW_COPY_AND_ASSIGN(ConvertableToTraceFormatWrapper);
+};
+
+class EnabledStateObserverImpl final
+    : public base::trace_event::TraceLog::EnabledStateObserver {
+ public:
+  EnabledStateObserverImpl() = default;
+
+  void OnTraceLogEnabled() final {
+    base::AutoLock lock(mutex_);
+    for (auto* o : observers_) {
+      o->OnTraceEnabled();
+    }
+  }
+
+  void OnTraceLogDisabled() final {
+    base::AutoLock lock(mutex_);
+    for (auto* o : observers_) {
+      o->OnTraceDisabled();
+    }
+  }
+
+  void AddObserver(v8::TracingController::TraceStateObserver* observer) {
+    {
+      base::AutoLock lock(mutex_);
+      DCHECK(!observers_.count(observer));
+      if (observers_.empty()) {
+        base::trace_event::TraceLog::GetInstance()->AddEnabledStateObserver(
+            this);
+      }
+      observers_.insert(observer);
+    }
+    // Fire the observer if recording is already in progress.
+    if (base::trace_event::TraceLog::GetInstance()->IsEnabled())
+      observer->OnTraceEnabled();
+  }
+
+  void RemoveObserver(v8::TracingController::TraceStateObserver* observer) {
+    base::AutoLock lock(mutex_);
+    DCHECK(observers_.count(observer) == 1);
+    observers_.erase(observer);
+    if (observers_.empty()) {
+      base::trace_event::TraceLog::GetInstance()->RemoveEnabledStateObserver(
+          this);
+    }
+  }
+
+ private:
+  base::Lock mutex_;
+  std::unordered_set<v8::TracingController::TraceStateObserver*> observers_;
+
+  DISALLOW_COPY_AND_ASSIGN(EnabledStateObserverImpl);
+};
+
+base::LazyInstance<EnabledStateObserverImpl>::Leaky g_trace_state_dispatcher =
+    LAZY_INSTANCE_INITIALIZER;
+
 }  // namespace
+
+class V8Platform::TracingControllerImpl : public v8::TracingController {
+ public:
+  TracingControllerImpl() = default;
+  ~TracingControllerImpl() override = default;
+
+  // TracingController implementation.
+  const uint8_t* GetCategoryGroupEnabled(const char* name) override {
+    return TRACE_EVENT_API_GET_CATEGORY_GROUP_ENABLED(name);
+  }
+  uint64_t AddTraceEvent(
+      char phase,
+      const uint8_t* category_enabled_flag,
+      const char* name,
+      const char* scope,
+      uint64_t id,
+      uint64_t bind_id,
+      int32_t num_args,
+      const char** arg_names,
+      const uint8_t* arg_types,
+      const uint64_t* arg_values,
+      std::unique_ptr<v8::ConvertableToTraceFormat>* arg_convertables,
+      unsigned int flags) override {
+    std::unique_ptr<base::trace_event::ConvertableToTraceFormat>
+        convertables[2];
+    if (num_args > 0 && arg_types[0] == TRACE_VALUE_TYPE_CONVERTABLE) {
+      convertables[0].reset(
+          new ConvertableToTraceFormatWrapper(arg_convertables[0]));
+    }
+    if (num_args > 1 && arg_types[1] == TRACE_VALUE_TYPE_CONVERTABLE) {
+      convertables[1].reset(
+          new ConvertableToTraceFormatWrapper(arg_convertables[1]));
+    }
+    DCHECK_LE(num_args, 2);
+    base::trace_event::TraceEventHandle handle =
+        TRACE_EVENT_API_ADD_TRACE_EVENT_WITH_BIND_ID(
+            phase, category_enabled_flag, name, scope, id, bind_id, num_args,
+            arg_names, arg_types, (const long long unsigned int*)arg_values,
+            convertables, flags);
+    uint64_t result;
+    memcpy(&result, &handle, sizeof(result));
+    return result;
+  }
+  void UpdateTraceEventDuration(const uint8_t* category_enabled_flag,
+                                const char* name,
+                                uint64_t handle) override {
+    base::trace_event::TraceEventHandle traceEventHandle;
+    memcpy(&traceEventHandle, &handle, sizeof(handle));
+    TRACE_EVENT_API_UPDATE_TRACE_EVENT_DURATION(category_enabled_flag, name,
+                                                traceEventHandle);
+  }
+  void AddTraceStateObserver(TraceStateObserver* observer) override {
+    g_trace_state_dispatcher.Get().AddObserver(observer);
+  }
+  void RemoveTraceStateObserver(TraceStateObserver* observer) override {
+    g_trace_state_dispatcher.Get().RemoveObserver(observer);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(TracingControllerImpl);
+};
 
 // static
 V8Platform* V8Platform::Get() { return g_v8_platform.Pointer(); }
 
-V8Platform::V8Platform() {}
+V8Platform::V8Platform() : tracing_controller_(new TracingControllerImpl) {}
 
 V8Platform::~V8Platform() {}
 
@@ -125,8 +257,12 @@ double V8Platform::MonotonicallyIncreasingTime() {
       static_cast<double>(base::Time::kMicrosecondsPerSecond);
 }
 
+v8::TracingController* V8Platform::GetTracingController() {
+  return tracing_controller_.get();
+}
+
 const uint8_t* V8Platform::GetCategoryGroupEnabled(const char* name) {
-  return TRACE_EVENT_API_GET_CATEGORY_GROUP_ENABLED(name);
+  return tracing_controller_->GetCategoryGroupEnabled(name);
 }
 
 const char* V8Platform::GetCategoryGroupName(
@@ -134,27 +270,6 @@ const char* V8Platform::GetCategoryGroupName(
   return base::trace_event::TraceLog::GetCategoryGroupName(
       category_enabled_flag);
 }
-
-namespace {
-
-class ConvertableToTraceFormatWrapper
-    : public base::trace_event::ConvertableToTraceFormat {
- public:
-  explicit ConvertableToTraceFormatWrapper(
-      std::unique_ptr<v8::ConvertableToTraceFormat>& inner)
-      : inner_(std::move(inner)) {}
-  ~ConvertableToTraceFormatWrapper() override = default;
-  void AppendAsTraceFormat(std::string* out) const final {
-    inner_->AppendAsTraceFormat(out);
-  }
-
- private:
-  std::unique_ptr<v8::ConvertableToTraceFormat> inner_;
-
-  DISALLOW_COPY_AND_ASSIGN(ConvertableToTraceFormatWrapper);
-};
-
-}  // namespace
 
 uint64_t V8Platform::AddTraceEvent(
     char phase,
@@ -169,101 +284,26 @@ uint64_t V8Platform::AddTraceEvent(
     const uint64_t* arg_values,
     std::unique_ptr<v8::ConvertableToTraceFormat>* arg_convertables,
     unsigned int flags) {
-  std::unique_ptr<base::trace_event::ConvertableToTraceFormat> convertables[2];
-  if (num_args > 0 && arg_types[0] == TRACE_VALUE_TYPE_CONVERTABLE) {
-    convertables[0].reset(
-        new ConvertableToTraceFormatWrapper(arg_convertables[0]));
-  }
-  if (num_args > 1 && arg_types[1] == TRACE_VALUE_TYPE_CONVERTABLE) {
-    convertables[1].reset(
-        new ConvertableToTraceFormatWrapper(arg_convertables[1]));
-  }
-  DCHECK_LE(num_args, 2);
-  base::trace_event::TraceEventHandle handle =
-      TRACE_EVENT_API_ADD_TRACE_EVENT_WITH_BIND_ID(
-          phase, category_enabled_flag, name, scope, id, bind_id, num_args,
-          arg_names, arg_types, (const long long unsigned int*)arg_values,
-          convertables, flags);
-  uint64_t result;
-  memcpy(&result, &handle, sizeof(result));
-  return result;
+  return tracing_controller_->AddTraceEvent(
+      phase, category_enabled_flag, name, scope, id, bind_id, num_args,
+      arg_names, arg_types, arg_values, arg_convertables, flags);
 }
 
 void V8Platform::UpdateTraceEventDuration(const uint8_t* category_enabled_flag,
                                           const char* name,
                                           uint64_t handle) {
-  base::trace_event::TraceEventHandle traceEventHandle;
-  memcpy(&traceEventHandle, &handle, sizeof(handle));
-  TRACE_EVENT_API_UPDATE_TRACE_EVENT_DURATION(category_enabled_flag, name,
-                                              traceEventHandle);
+  tracing_controller_->UpdateTraceEventDuration(category_enabled_flag, name,
+                                                handle);
 }
-
-namespace {
-
-class EnabledStateObserverImpl final
-    : public base::trace_event::TraceLog::EnabledStateObserver {
- public:
-  EnabledStateObserverImpl() = default;
-
-  void OnTraceLogEnabled() final {
-    base::AutoLock lock(mutex_);
-    for (auto* o : observers_) {
-      o->OnTraceEnabled();
-    }
-  }
-
-  void OnTraceLogDisabled() final {
-    base::AutoLock lock(mutex_);
-    for (auto* o : observers_) {
-      o->OnTraceDisabled();
-    }
-  }
-
-  void AddObserver(v8::Platform::TraceStateObserver* observer) {
-    {
-      base::AutoLock lock(mutex_);
-      DCHECK(!observers_.count(observer));
-      if (observers_.empty()) {
-        base::trace_event::TraceLog::GetInstance()->AddEnabledStateObserver(
-            this);
-      }
-      observers_.insert(observer);
-    }
-    // Fire the observer if recording is already in progress.
-    if (base::trace_event::TraceLog::GetInstance()->IsEnabled())
-      observer->OnTraceEnabled();
-  }
-
-  void RemoveObserver(v8::Platform::TraceStateObserver* observer) {
-    base::AutoLock lock(mutex_);
-    DCHECK(observers_.count(observer) == 1);
-    observers_.erase(observer);
-    if (observers_.empty()) {
-      base::trace_event::TraceLog::GetInstance()->RemoveEnabledStateObserver(
-          this);
-    }
-  }
-
- private:
-  base::Lock mutex_;
-  std::unordered_set<v8::Platform::TraceStateObserver*> observers_;
-
-  DISALLOW_COPY_AND_ASSIGN(EnabledStateObserverImpl);
-};
-
-base::LazyInstance<EnabledStateObserverImpl>::Leaky g_trace_state_dispatcher =
-    LAZY_INSTANCE_INITIALIZER;
-
-}  // namespace
 
 void V8Platform::AddTraceStateObserver(
     v8::Platform::TraceStateObserver* observer) {
-  g_trace_state_dispatcher.Get().AddObserver(observer);
+  tracing_controller_->AddTraceStateObserver(observer);
 }
 
 void V8Platform::RemoveTraceStateObserver(
     v8::Platform::TraceStateObserver* observer) {
-  g_trace_state_dispatcher.Get().RemoveObserver(observer);
+  tracing_controller_->RemoveTraceStateObserver(observer);
 }
 
 v8::Platform::StackTracePrinter V8Platform::GetStackTracePrinter() {
