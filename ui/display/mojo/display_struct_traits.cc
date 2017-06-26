1afa476cf172022f6962f975855454e6bd89a66a
kylechar <kylechar@chromium.org>
2017-06-26 10:20:41 -0400

    Add AccelerometerSupport to Display StructTraits.
    Also small cleanup in DisplayStructTraitsTests.
    
    Bug: 704929
    Change-Id: I00c69a7fb7209554489409c5d016688da386cbc6
    Reviewed-on: https://chromium-review.googlesource.com/546619
    Commit-Queue: kylechar <kylechar@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482291}
    Former-commit-id: f642949fc45c792e93e4e9ad1f1f489097ae73a0

--- ui/display/mojo/display_struct_traits.cc
+++ ui/display/mojo/display_struct_traits.cc
@@ -77,6 +77,41 @@ bool EnumTraits<display::mojom::TouchSupport, display::Display::TouchSupport>::
   return false;
 }
 
+display::mojom::AccelerometerSupport
+EnumTraits<display::mojom::AccelerometerSupport,
+           display::Display::AccelerometerSupport>::
+    ToMojom(display::Display::AccelerometerSupport accelerometer_support) {
+  switch (accelerometer_support) {
+    case display::Display::ACCELEROMETER_SUPPORT_UNKNOWN:
+      return display::mojom::AccelerometerSupport::UNKNOWN;
+    case display::Display::ACCELEROMETER_SUPPORT_AVAILABLE:
+      return display::mojom::AccelerometerSupport::AVAILABLE;
+    case display::Display::ACCELEROMETER_SUPPORT_UNAVAILABLE:
+      return display::mojom::AccelerometerSupport::UNAVAILABLE;
+  }
+  NOTREACHED();
+  return display::mojom::AccelerometerSupport::UNKNOWN;
+}
+
+bool EnumTraits<display::mojom::AccelerometerSupport,
+                display::Display::AccelerometerSupport>::
+    FromMojom(display::mojom::AccelerometerSupport accelerometer_support,
+              display::Display::AccelerometerSupport* out) {
+  switch (accelerometer_support) {
+    case display::mojom::AccelerometerSupport::UNKNOWN:
+      *out = display::Display::ACCELEROMETER_SUPPORT_UNKNOWN;
+      return true;
+    case display::mojom::AccelerometerSupport::AVAILABLE:
+      *out = display::Display::ACCELEROMETER_SUPPORT_AVAILABLE;
+      return true;
+    case display::mojom::AccelerometerSupport::UNAVAILABLE:
+      *out = display::Display::ACCELEROMETER_SUPPORT_UNAVAILABLE;
+      return true;
+  }
+  NOTREACHED();
+  return false;
+}
+
 bool StructTraits<display::mojom::DisplayDataView, display::Display>::Read(
     display::mojom::DisplayDataView data,
     display::Display* out) {
@@ -97,6 +132,9 @@ bool StructTraits<display::mojom::DisplayDataView, display::Display>::Read(
     return false;
 
   if (!data.ReadTouchSupport(&out->touch_support_))
+    return false;
+
+  if (!data.ReadAccelerometerSupport(&out->accelerometer_support_))
     return false;
 
   if (!data.ReadMaximumCursorSize(&out->maximum_cursor_size_))
