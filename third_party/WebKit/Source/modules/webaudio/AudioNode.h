d6a13bd505f6b501e5abb1bf7880bf3e81bca2fb
Hongchan Choi <hongchan@chromium.org>
2017-06-26 10:27:52 -0700

    Add ENUM categories for AudioWorkletNode and its AudioParam object
    This is a simple addition to the ENUM category for:
    - AudioWorkletNode
    - AudioParam objects in AudioWorkletNode
    
    Bug: 691782, 469639
    Change-Id: If8499382f2aed8157f55c419c3fdc2f71c666196
    Reviewed-on: https://chromium-review.googlesource.com/548580
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482348}
    Former-commit-id: f6242a5f445d0bf60387d7070ea4b0e7eb09b6b5

--- third_party/WebKit/Source/modules/webaudio/AudioNode.h
+++ third_party/WebKit/Source/modules/webaudio/AudioNode.h
@@ -95,7 +95,8 @@ class MODULES_EXPORT AudioHandler : public ThreadSafeRefCounted<AudioHandler> {
     kNodeTypeWaveShaper = 18,
     kNodeTypeIIRFilter = 19,
     kNodeTypeConstantSource = 20,
-    kNodeTypeEnd = 21
+    kNoteTypeAudioWorklet = 21,
+    kNodeTypeEnd = 22
   };
 
   AudioHandler(NodeType, AudioNode&, float sample_rate);
