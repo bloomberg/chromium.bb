c7c5e83cf95ebaafd7f8abb08f94ef6a6940841a
Jialiu Lin <jialiul@chromium.org>
2017-06-26 09:52:43 -0700

    Sanitize referrer chain in ClientDownloadRequest if it is a sampled ping.
    Also shorten all data url in referrer chain to save some bytes.
    
    Bug: 726493
    Change-Id: I6b33ad6d9144ea2244922f254f2a1188249f0658
    Reviewed-on: https://chromium-review.googlesource.com/530158
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Nathan Parker <nparker@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482322}
    Former-commit-id: 1004a7a4a4f6aca1422caa28aa9e26741e6760f9

---
+++ components/safe_browsing/common/utils.cc
@@ -0,0 +1,25 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "components/safe_browsing/common/utils.h"
+
+#include "base/strings/string_number_conversions.h"
+#include "crypto/sha2.h"
+
+namespace safe_browsing {
+
+std::string ShortURLForReporting(const GURL& url) {
+  std::string spec(url.spec());
+  if (url.SchemeIs(url::kDataScheme)) {
+    size_t comma_pos = spec.find(',');
+    if (comma_pos != std::string::npos && comma_pos != spec.size() - 1) {
+      std::string hash_value = crypto::SHA256HashString(spec);
+      spec.erase(comma_pos + 1);
+      spec += base::HexEncode(hash_value.data(), hash_value.size());
+    }
+  }
+  return spec;
+}
+
+}  // namespace safe_browsing
