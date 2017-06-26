38283f0d59a9ebded02d97556b4f960df918edd5
suzyh <suzyh@chromium.org>
2017-06-25 20:00:44 -0700

    Expose getAnimations as function on Document
    The document-level getAnimations function was previously defined on
    AnimationTimeline, requiring callers to specify
    document.timeline.getAnimations(). The Web Animations spec exposes this
    function on Document directly: document.getAnimations(). This patch
    moves the function to the appropriate place.
    
    Spec link:
    http://w3c.github.io/web-animations/#extensions-to-the-document-interface
    
    BUG=624639
    
    Review-Url: https://codereview.chromium.org/2947103002
    Cr-Commit-Position: refs/heads/master@{#482188}
    
    Former-commit-id: da913699c72d5f97405db96ddaa4040ed621a0bc

--- third_party/WebKit/Source/core/animation/DocumentAnimation.h
+++ third_party/WebKit/Source/core/animation/DocumentAnimation.h
@@ -5,6 +5,8 @@
 #ifndef DocumentAnimation_h
 #define DocumentAnimation_h
 
+#include "core/animation/Animation.h"
+#include "core/animation/DocumentTimeline.h"
 #include "core/dom/Document.h"
 #include "platform/wtf/Allocator.h"
 
@@ -16,6 +18,10 @@ class DocumentAnimation {
  public:
   static DocumentTimeline* timeline(Document& document) {
     return &document.Timeline();
+  }
+
+  static HeapVector<Member<Animation>> getAnimations(Document& document) {
+    return document.Timeline().getAnimations();
   }
 };
 
