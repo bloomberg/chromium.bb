a2925e1b2764971ec1e783597b21a864839b4483
Derek Cheng <imcheng@chromium.org>
2017-06-26 23:13:47 +0000

    Revert "Fix thread safety issues with safe browsing DB."
    This reverts commit ef0a30e165828627641590f67b875a6018d03e89 [formerly 0ac8d89ee822c431ea66e0a7da762bb8c28b1877].
    
    Reason for revert: Seems to have broken RemoteDatabaseManagerTest on android - crbug.com/736924
    
    Original change's description:
    > Fix thread safety issues with safe browsing DB.
    > 
    > Fix thread safety issues with safe browsing DB.
    > The Safe Browsing DB has some sequence consistency issues, which
    > is caught by the stricter WeakPtr semantics detailed on CL 2908073007.
    > 
    > * Delete SafeBrowsingDatabaseManager on IO thread.
    > * Remove illegal dereferencing of IO-thread WeakPtr from DB thread in
    >   V4Database::VerifyChecksumOnTaskRunner
    > * Remove non-threadsafe accesses of |io_thread_| resident members from
    >   the DB thread in V4LocalDatabaseManager.
    > * Add IO-thread runloops to unit tests to handle database teardown.
    > 
    > 
    > R=​nparker@chromium.org
    > CC=wez@chromium.org
    > 
    > Bug: 729716
    > Change-Id: I1bc620d42aca2f1cc99e482b7776a628d783d390
    > Reviewed-on: https://chromium-review.googlesource.com/523983
    > Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    > Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    > Reviewed-by: Greg Thompson <grt@chromium.org>
    > Reviewed-by: Varun Khaneja <vakh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#481735}
    
    TBR=kmarshall@chromium.org,nparker@chromium.org,csharrison@chromium.org,vakh@chromium.org,grt@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 729716
    Change-Id: Ia10145f404619bdde8f0348d5ab764fd29a59fbc
    Reviewed-on: https://chromium-review.googlesource.com/549073
    Reviewed-by: Derek Cheng <imcheng@chromium.org>
    Commit-Queue: Derek Cheng <imcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482489}
    Former-commit-id: 1f4730efcd8f4c7152b69665bc96ad7f7c464667

--- components/safe_browsing_db/database_manager.h
+++ components/safe_browsing_db/database_manager.h
@@ -15,7 +15,7 @@
 
 #include "base/gtest_prod_util.h"
 #include "base/macros.h"
-#include "base/memory/ref_counted_delete_on_sequence.h"
+#include "base/memory/ref_counted.h"
 #include "components/safe_browsing_db/hit_report.h"
 #include "components/safe_browsing_db/util.h"
 #include "content/public/common/resource_type.h"
@@ -40,7 +40,7 @@ class V4GetHashProtocolManager;
 
 // Base class to either the locally-managed or a remotely-managed database.
 class SafeBrowsingDatabaseManager
-    : public base::RefCountedDeleteOnSequence<SafeBrowsingDatabaseManager> {
+    : public base::RefCountedThreadSafe<SafeBrowsingDatabaseManager> {
  public:
   // Callers requesting a result should derive from this class.
   // The destructor should call db_manager->CancelCheck(client) if a
@@ -259,8 +259,7 @@ class SafeBrowsingDatabaseManager
 
   virtual ~SafeBrowsingDatabaseManager();
 
-  friend class base::RefCountedDeleteOnSequence<SafeBrowsingDatabaseManager>;
-  friend class base::DeleteHelper<SafeBrowsingDatabaseManager>;
+  friend class base::RefCountedThreadSafe<SafeBrowsingDatabaseManager>;
 
   FRIEND_TEST_ALL_PREFIXES(SafeBrowsingDatabaseManagerTest,
                            CheckApiBlacklistUrlPrefixes);
