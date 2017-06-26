9d0e4cab1d300e05b608897146c090adcd9be683
Colin Blundell <blundell@chromium.org>
2017-06-26 13:39:30 +0200

    [Services] Add echo service for use in testing contexts
    This CL adds a test-only echo service that can be used as a simple
    example service to connect to in end-to-end testing contexts.
    
    This service will shortly be used in an end-to-end test of the
    embedding of the Service Manager in //ios/web. The addition of the service
    is split out into this CL to avoid that CL growing further.
    
    Bug: 731588
    Change-Id: I4949685f12e23fc482e33ebfdc71f45a396ef482
    Reviewed-on: https://chromium-review.googlesource.com/548395
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482575}
    Former-commit-id: 29d682c92068cd88424d3e3a9a27efe65575d8b0

---
+++ services/test/echo/echo_service.cc
@@ -0,0 +1,43 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/test/echo/echo_service.h"
+
+#include "services/service_manager/public/cpp/service_context.h"
+
+namespace echo {
+
+std::unique_ptr<service_manager::Service> CreateEchoService() {
+  return base::MakeUnique<EchoService>();
+}
+
+EchoService::EchoService() {
+  registry_.AddInterface<mojom::Echo>(
+      base::Bind(&EchoService::BindEchoRequest, base::Unretained(this)));
+}
+
+EchoService::~EchoService() {}
+
+void EchoService::OnStart() {}
+
+void EchoService::OnBindInterface(
+    const service_manager::BindSourceInfo& source_info,
+    const std::string& interface_name,
+    mojo::ScopedMessagePipeHandle interface_pipe) {
+  registry_.BindInterface(source_info, interface_name,
+                          std::move(interface_pipe));
+}
+
+void EchoService::BindEchoRequest(
+    const service_manager::BindSourceInfo& source_info,
+    mojom::EchoRequest request) {
+  bindings_.AddBinding(this, std::move(request));
+}
+
+void EchoService::EchoString(const std::string& input,
+                             EchoStringCallback callback) {
+  std::move(callback).Run(input);
+}
+
+}  // namespace echo
