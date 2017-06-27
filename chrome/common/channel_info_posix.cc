2982b16681d1fedec41c6c06514aa25b688ac65b
Steve Kobes <skobes@chromium.org>
2017-06-27 04:20:52 +0000

    Revert "Include channel suffix in default user data directory on Linux."
    This reverts commit 052abb38f43b [formerly 2f7ff6f11ae5] (http://crrev.com/482531).
    
    Reason for revert: Broke ChromeOS compile:
    https://build.chromium.org/p/chromium.chrome/builders/Google%20Chrome%20ChromeOS/builds/35706
    
    TBR=jamiewalch@chromium.org,thestig@chromium.org,skobes@chromium.org
    
    Change-Id: Ibd825677ec17f81b9a108d260480bd7295b4937d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 726016
    Reviewed-on: https://chromium-review.googlesource.com/549585
    Reviewed-by: Steve Kobes <skobes@chromium.org>
    Commit-Queue: Steve Kobes <skobes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482535}
    Former-commit-id: 9f0da5d0c526cddcb7b4d6fdf11fa4ec54ba84c9

--- chrome/common/channel_info_posix.cc
+++ chrome/common/channel_info_posix.cc
@@ -15,11 +15,9 @@ namespace {
 // Helper function to return both the channel enum and modifier string.
 // Implements both together to prevent their behavior from diverging, which has
 // happened multiple times in the past.
-version_info::Channel GetChannelImpl(std::string* modifier_out,
-                                     std::string* data_dir_suffix_out) {
+version_info::Channel GetChannelImpl(std::string* modifier_out) {
   version_info::Channel channel = version_info::Channel::UNKNOWN;
   std::string modifier;
-  std::string data_dir_suffix;
 
   char* env = getenv("CHROME_VERSION_EXTRA");
   if (env)
@@ -34,10 +32,8 @@ version_info::Channel GetChannelImpl(std::string* modifier_out,
     modifier = "";
   } else if (modifier == "dev") {
     channel = version_info::Channel::DEV;
-    data_dir_suffix = "-unstable";
   } else if (modifier == "beta") {
     channel = version_info::Channel::BETA;
-    data_dir_suffix = "-beta";
   } else {
     modifier = "unknown";
   }
@@ -45,8 +41,6 @@ version_info::Channel GetChannelImpl(std::string* modifier_out,
 
   if (modifier_out)
     modifier_out->swap(modifier);
-  if (data_dir_suffix_out)
-    data_dir_suffix_out->swap(data_dir_suffix);
 
   return channel;
 }
@@ -55,20 +49,12 @@ version_info::Channel GetChannelImpl(std::string* modifier_out,
 
 std::string GetChannelString() {
   std::string modifier;
-  GetChannelImpl(&modifier, nullptr);
+  GetChannelImpl(&modifier);
   return modifier;
 }
 
-#if defined(GOOGLE_CHROME_BUILD)
-std::string GetChannelSuffixForDataDir() {
-  std::string data_dir_suffix;
-  GetChannelImpl(nullptr, &data_dir_suffix);
-  return data_dir_suffix;
-}
-#endif  // defined(GOOGLE_CHROME_BUILD)
-
 version_info::Channel GetChannel() {
-  return GetChannelImpl(nullptr, nullptr);
+  return GetChannelImpl(nullptr);
 }
 
 }  // namespace chrome
