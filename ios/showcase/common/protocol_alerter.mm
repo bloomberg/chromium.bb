f00674247bab26bbe2cae636f7275460f0390d97
gambard <gambard@chromium.org>
2017-06-27 14:54:27 +0200

    Possibility to add exceptions in Protocol Alerter
    The ProtocolAlerter logs all the methods of a protocol. In some cases,
    a showcase example does not want to log all methods, because their are
    called too often or are not relevant.
    This CL adds a way to remove the logging for some specific methods.
    
    BUG=none
    
    Change-Id: I7956911e6f472271a57e72288c7ee04d86623084
    Reviewed-on: https://chromium-review.googlesource.com/549898
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482610}
    Former-commit-id: 47642f05bc6abb99b8df5f3900e5c89a5e88ef9a

--- ios/showcase/common/protocol_alerter.mm
+++ ios/showcase/common/protocol_alerter.mm
@@ -26,6 +26,8 @@ char kAssociatedProtocolNameKey;
 
 @interface ProtocolAlerter () {
   NSSet<Protocol*>* _protocols;
+  // Selectors for which no logging should be done.
+  NSMutableSet<NSValue*>* _ignoredSelectors;
 }
 @end
 
@@ -39,7 +41,12 @@ char kAssociatedProtocolNameKey;
   // NSProxy isn't a subclass of NSObject, and has no superclass, so
   // there's no [super init] to call.
   _protocols = [[NSSet<Protocol*> alloc] initWithArray:protocols];
+  _ignoredSelectors = [NSMutableSet set];
   return self;
+}
+
+- (void)ignoreSelector:(SEL)sel {
+  [_ignoredSelectors addObject:[NSValue valueWithPointer:sel]];
 }
 
 #pragma mark - NSProxy
@@ -68,6 +75,10 @@ char kAssociatedProtocolNameKey;
 }
 
 - (void)forwardInvocation:(NSInvocation*)invocation {
+  if ([_ignoredSelectors
+          containsObject:[NSValue valueWithPointer:invocation.selector]]) {
+    return;
+  }
   // Instead of actually doing anything the protocol method would normally
   // do, instead just generate a title and description and display an alert or
   // log a message.
