4ba5750eae2c5a915959bbd4b1e634f20e2257f2
Christopher Benson <chrisbenson@google.com>
2017-06-27 15:49:52 -0700

    New Tab Button Refactor
    Goal is to refactor the NewTabButton out of the Tabstrip code so that it will be more accessible to the NewTabPromo. Not intended to change functionality at all.
    
    Change-Id: I1a0ad75eee5e2c865509c147950fb74a69559d61
    Reviewed-on: https://chromium-review.googlesource.com/550376
    Commit-Queue: Christopher Benson <chrisbenson@google.com>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482866}
    Former-commit-id: a870f3a000f480d9dea3399f4e011c080ac669aa

---
+++ chrome/browser/ui/views/tabs/new_tab_button.h
@@ -0,0 +1,76 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_VIEWS_TABS_NEW_TAB_BUTTON_H_
+#define CHROME_BROWSER_UI_VIEWS_TABS_NEW_TAB_BUTTON_H_
+
+#include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/tabs/tab_strip.h"
+#include "ui/views/controls/button/image_button.h"
+#include "ui/views/view.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// NewTabButton
+//
+//  A subclass of ImageButton that hit-tests to the shape of the new tab button
+//  and does custom drawing.
+//
+///////////////////////////////////////////////////////////////////////////////
+class NewTabButton : public views::ImageButton,
+                     public views::MaskedTargeterDelegate {
+ public:
+  NewTabButton(TabStrip* tab_strip, views::ButtonListener* listener);
+  ~NewTabButton() override;
+
+  // Set the background offset used to match the background image to the frame
+  // image.
+  void set_background_offset(const gfx::Point& offset) {
+    background_offset_ = offset;
+  }
+
+  // Returns the offset from the top of the tabstrip at which the new tab
+  // button's visible region begins.
+  static int GetTopOffset();
+
+ private:
+// views::ImageButton:
+#if defined(OS_WIN)
+  void OnMouseReleased(const ui::MouseEvent& event) override;
+#endif
+  void OnGestureEvent(ui::GestureEvent* event) override;
+  void PaintButtonContents(gfx::Canvas* canvas) override;
+
+  // views::MaskedTargeterDelegate:
+  bool GetHitTestMask(gfx::Path* mask) const override;
+
+  // Computes a path corresponding to the button's outer border for a given
+  // |scale| and stores it in |path|.  |button_y| is used as the y-coordinate
+  // for the top of the button.  If |extend_to_top| is true, the path is
+  // extended vertically to y = 0.  The caller uses this for Fitts' Law purposes
+  // in maximized/fullscreen mode.
+  void GetBorderPath(float button_y,
+                     float scale,
+                     bool extend_to_top,
+                     SkPath* path) const;
+
+  // Paints the fill region of the button into |canvas|, according to the
+  // supplied values from GetImage() and the given |fill| path.
+  void PaintFill(bool pressed,
+                 float scale,
+                 const SkPath& fill,
+                 gfx::Canvas* canvas) const;
+
+  // Tab strip that contains this button.
+  TabStrip* tab_strip_;
+
+  // The offset used to paint the background image.
+  gfx::Point background_offset_;
+
+  // were we destroyed?
+  bool* destroyed_;
+
+  DISALLOW_COPY_AND_ASSIGN(NewTabButton);
+};
+
+#endif  // CHROME_BROWSER_UI_VIEWS_TABS_NEW_TAB_BUTTON_H_
--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -25,6 +25,7 @@
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/view_ids.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
+#include "chrome/browser/ui/views/tabs/new_tab_button.h"
 #include "chrome/browser/ui/views/tabs/stacked_tab_strip_layout.h"
 #include "chrome/browser/ui/views/tabs/tab.h"
 #include "chrome/browser/ui/views/tabs/tab_drag_controller.h"
@@ -111,38 +112,10 @@ const int kPinnedToNonPinnedOffset = 2;
 const int kPinnedToNonPinnedOffset = 3;
 #endif
 
-// Returns the offset from the top of the tabstrip at which the new tab button's
-// visible region begins.
-int GetNewTabButtonTopOffset() {
-  // The vertical distance between the bottom of the new tab button and the
-  // bottom of the tabstrip.
-  const int kNewTabButtonBottomOffset = 4;
-  return Tab::GetMinimumInactiveSize().height() - kNewTabButtonBottomOffset -
-      GetLayoutSize(NEW_TAB_BUTTON).height();
-}
-
 // Returns the width needed for the new tab button (and padding).
 int GetNewTabButtonWidth() {
   return GetLayoutSize(NEW_TAB_BUTTON).width() -
       GetLayoutConstant(TABSTRIP_NEW_TAB_BUTTON_OVERLAP);
-}
-
-sk_sp<SkDrawLooper> CreateShadowDrawLooper(SkColor color) {
-  SkLayerDrawLooper::Builder looper_builder;
-  looper_builder.addLayer();
-
-  SkLayerDrawLooper::LayerInfo layer_info;
-  layer_info.fPaintBits |= SkLayerDrawLooper::kMaskFilter_Bit;
-  layer_info.fPaintBits |= SkLayerDrawLooper::kColorFilter_Bit;
-  layer_info.fColorMode = SkBlendMode::kDst;
-  layer_info.fOffset.set(0, 1);
-  SkPaint* layer_paint = looper_builder.addLayer(layer_info);
-  layer_paint->setMaskFilter(SkBlurMaskFilter::Make(
-      kNormal_SkBlurStyle, 0.5, SkBlurMaskFilter::kHighQuality_BlurFlag));
-  layer_paint->setColorFilter(
-      SkColorFilter::MakeModeFilter(color, SkBlendMode::kSrcIn));
-
-  return looper_builder.detach();
 }
 
 // Animation delegate used for any automatic tab movement.  Hides the tab if it
@@ -257,278 +230,6 @@ const TabSizeInfo& GetTabSizeInfo() {
 
 }  // namespace
 
-///////////////////////////////////////////////////////////////////////////////
-// NewTabButton
-//
-//  A subclass of ImageButton that hit-tests to the shape of the new tab button
-//  and does custom drawing.
-
-class NewTabButton : public views::ImageButton,
-                     public views::MaskedTargeterDelegate {
- public:
-  NewTabButton(TabStrip* tab_strip, views::ButtonListener* listener);
-  ~NewTabButton() override;
-
-  // Set the background offset used to match the background image to the frame
-  // image.
-  void set_background_offset(const gfx::Point& offset) {
-    background_offset_ = offset;
-  }
-
- private:
-  // views::ImageButton:
-#if defined(OS_WIN)
-  void OnMouseReleased(const ui::MouseEvent& event) override;
-#endif
-  void OnGestureEvent(ui::GestureEvent* event) override;
-  void PaintButtonContents(gfx::Canvas* canvas) override;
-
-  // views::MaskedTargeterDelegate:
-  bool GetHitTestMask(gfx::Path* mask) const override;
-
-  // Computes a path corresponding to the button's outer border for a given
-  // |scale| and stores it in |path|.  |button_y| is used as the y-coordinate
-  // for the top of the button.  If |extend_to_top| is true, the path is
-  // extended vertically to y = 0.  The caller uses this for Fitts' Law purposes
-  // in maximized/fullscreen mode.
-  void GetBorderPath(float button_y,
-                     float scale,
-                     bool extend_to_top,
-                     SkPath* path) const;
-
-  // Paints the fill region of the button into |canvas|, according to the
-  // supplied values from GetImage() and the given |fill| path.
-  void PaintFill(bool pressed,
-                 float scale,
-                 const SkPath& fill,
-                 gfx::Canvas* canvas) const;
-
-  // Tab strip that contains this button.
-  TabStrip* tab_strip_;
-
-  // The offset used to paint the background image.
-  gfx::Point background_offset_;
-
-  // were we destroyed?
-  bool* destroyed_;
-
-  DISALLOW_COPY_AND_ASSIGN(NewTabButton);
-};
-
-NewTabButton::NewTabButton(TabStrip* tab_strip, views::ButtonListener* listener)
-    : views::ImageButton(listener),
-      tab_strip_(tab_strip),
-      destroyed_(NULL) {
-  set_animate_on_state_change(true);
-#if defined(OS_LINUX) && !defined(OS_CHROMEOS)
-  set_triggerable_event_flags(triggerable_event_flags() |
-                              ui::EF_MIDDLE_MOUSE_BUTTON);
-#endif
-}
-
-NewTabButton::~NewTabButton() {
-  if (destroyed_)
-    *destroyed_ = true;
-}
-
-#if defined(OS_WIN)
-void NewTabButton::OnMouseReleased(const ui::MouseEvent& event) {
-  if (event.IsOnlyRightMouseButton()) {
-    gfx::Point point = event.location();
-    views::View::ConvertPointToScreen(this, &point);
-    point = display::win::ScreenWin::DIPToScreenPoint(point);
-    bool destroyed = false;
-    destroyed_ = &destroyed;
-    gfx::ShowSystemMenuAtPoint(views::HWNDForView(this), point);
-    if (destroyed)
-      return;
-
-    destroyed_ = NULL;
-    SetState(views::CustomButton::STATE_NORMAL);
-    return;
-  }
-  views::ImageButton::OnMouseReleased(event);
-}
-#endif
-
-void NewTabButton::OnGestureEvent(ui::GestureEvent* event) {
-  // Consume all gesture events here so that the parent (Tab) does not
-  // start consuming gestures.
-  views::ImageButton::OnGestureEvent(event);
-  event->SetHandled();
-}
-
-void NewTabButton::PaintButtonContents(gfx::Canvas* canvas) {
-  gfx::ScopedCanvas scoped_canvas(canvas);
-  const int visible_height = GetLayoutSize(NEW_TAB_BUTTON).height();
-  canvas->Translate(gfx::Vector2d(0, height() - visible_height));
-
-  const bool pressed = state() == views::CustomButton::STATE_PRESSED;
-  const float scale = canvas->image_scale();
-
-  // Fill.
-  SkPath fill;
-  const float fill_bottom = (visible_height - 2) * scale;
-  const float diag_height = fill_bottom - 3.5 * scale;
-  const float diag_width = diag_height * Tab::GetInverseDiagonalSlope();
-  fill.moveTo(diag_width + 4 * scale, fill_bottom);
-  fill.rCubicTo(-0.75 * scale, 0, -1.625 * scale, -0.5 * scale, -2 * scale,
-                -1.5 * scale);
-  fill.rLineTo(-diag_width, -diag_height);
-  fill.rCubicTo(0, -0.5 * scale, 0.25 * scale, -scale, scale, -scale);
-  fill.lineTo((width() - 4) * scale - diag_width, scale);
-  fill.rCubicTo(0.75 * scale, 0, 1.625 * scale, 0.5 * scale, 2 * scale,
-                1.5 * scale);
-  fill.rLineTo(diag_width, diag_height);
-  fill.rCubicTo(0, 0.5 * scale, -0.25 * scale, scale, -scale, scale);
-  fill.close();
-  PaintFill(pressed, scale, fill, canvas);
-
-  // Stroke.
-  canvas->UndoDeviceScaleFactor();
-  SkPath stroke;
-  GetBorderPath(0, scale, false, &stroke);
-  // We want to draw a drop shadow either inside or outside the stroke,
-  // depending on whether we're pressed; so, either clip out what's outside
-  // the stroke, or clip out the fill inside it.
-  if (pressed)
-    canvas->ClipPath(stroke, true);
-  Op(stroke, fill, kDifference_SkPathOp, &stroke);
-  if (!pressed)
-    canvas->sk_canvas()->clipPath(fill, SkClipOp::kDifference, true);
-  // Now draw the stroke and shadow; the stroke will always be visible, while
-  // the shadow will be affected by the clip we set above.
-  cc::PaintFlags flags;
-  flags.setAntiAlias(true);
-  const SkColor stroke_color = tab_strip_->GetToolbarTopSeparatorColor();
-  const float alpha = SkColorGetA(stroke_color);
-  const SkAlpha shadow_alpha =
-      base::saturated_cast<SkAlpha>(std::round(2.1875f * alpha));
-  flags.setLooper(
-      CreateShadowDrawLooper(SkColorSetA(stroke_color, shadow_alpha)));
-  const SkAlpha path_alpha = static_cast<SkAlpha>(
-      std::round((pressed ? 0.875f : 0.609375f) * alpha));
-  flags.setColor(SkColorSetA(stroke_color, path_alpha));
-  canvas->DrawPath(stroke, flags);
-}
-
-bool NewTabButton::GetHitTestMask(gfx::Path* mask) const {
-  DCHECK(mask);
-
-  SkPath border;
-  const float scale = GetWidget()->GetCompositor()->device_scale_factor();
-  GetBorderPath(GetNewTabButtonTopOffset() * scale, scale,
-                tab_strip_->SizeTabButtonToTopOfTabStrip(), &border);
-  mask->addPath(border, SkMatrix::MakeScale(1 / scale));
-  return true;
-}
-
-void NewTabButton::GetBorderPath(float button_y,
-                                 float scale,
-                                 bool extend_to_top,
-                                 SkPath* path) const {
-  const float inverse_slope = Tab::GetInverseDiagonalSlope();
-  const float fill_bottom =
-      (GetLayoutSize(NEW_TAB_BUTTON).height() - 2) * scale;
-  const float stroke_bottom = button_y + fill_bottom + 1;
-  const float diag_height = fill_bottom - 3.5 * scale;
-  const float diag_width = diag_height * inverse_slope;
-  path->moveTo(diag_width + 4 * scale - 1, stroke_bottom);
-  path->rCubicTo(-0.75 * scale, 0, -1.625 * scale, -0.5 * scale, -2 * scale,
-                 -1.5 * scale);
-  path->rLineTo(-diag_width, -diag_height);
-  if (extend_to_top) {
-    // Create the vertical extension by extending the side diagonals at the
-    // upper left and lower right corners until they reach the top and bottom of
-    // the border, respectively (in other words, "un-round-off" those corners
-    // and turn them into sharp points).  Then extend upward from the corner
-    // points to the top of the bounds.
-    const float dy = scale + 2;
-    const float dx = inverse_slope * dy;
-    path->rLineTo(-dx, -dy);
-    path->rLineTo(0, -button_y - scale + 1);
-    path->lineTo((width() - 2) * scale + 1 + dx, 0);
-    path->rLineTo(0, stroke_bottom);
-  } else {
-    path->rCubicTo(-0.5 * scale, -1.125 * scale, 0.5 * scale, -scale - 2, scale,
-                   -scale - 2);
-    path->lineTo((width() - 4) * scale - diag_width + 1, button_y + scale - 1);
-    path->rCubicTo(0.75 * scale, 0, 1.625 * scale, 0.5 * scale, 2 * scale,
-                   1.5 * scale);
-    path->rLineTo(diag_width, diag_height);
-    path->rCubicTo(0.5 * scale, 1.125 * scale, -0.5 * scale, scale + 2, -scale,
-                   scale + 2);
-  }
-  path->close();
-}
-
-void NewTabButton::PaintFill(bool pressed,
-                             float scale,
-                             const SkPath& fill,
-                             gfx::Canvas* canvas) const {
-  gfx::ScopedCanvas scoped_canvas(canvas);
-  canvas->UndoDeviceScaleFactor();
-  cc::PaintFlags flags;
-  flags.setAntiAlias(true);
-
-  // For unpressed buttons, draw the fill and its shadow.
-  if (!pressed) {
-    // First we compute the background image coordinates and scale, in case we
-    // need to draw a custom background image.
-    const ui::ThemeProvider* tp = GetThemeProvider();
-    bool custom_image;
-    const int bg_id = tab_strip_->GetBackgroundResourceId(&custom_image);
-    if (custom_image) {
-      // For custom tab backgrounds the background starts at the top of the tab
-      // strip. Otherwise the background starts at the top of the frame.
-      const int offset_y =
-          tp->HasCustomImage(bg_id) ? 0 : background_offset_.y();
-      // The new tab background is mirrored in RTL mode, but the theme
-      // background should never be mirrored. Mirror it here to compensate.
-      float x_scale = 1.0f;
-      int x = GetMirroredX() + background_offset_.x();
-      const gfx::Size size(GetLayoutSize(NEW_TAB_BUTTON));
-      if (base::i18n::IsRTL()) {
-        x_scale = -1.0f;
-        // Offset by |width| such that the same region is painted as if there
-        // was no flip.
-        x += size.width();
-      }
-
-      const bool succeeded =
-          canvas->InitPaintFlagsForTiling(*tp->GetImageSkiaNamed(bg_id), x,
-                                          GetNewTabButtonTopOffset() + offset_y,
-                                          x_scale * scale, scale, 0, 0, &flags);
-      DCHECK(succeeded);
-    } else {
-      flags.setColor(tp->GetColor(ThemeProperties::COLOR_BACKGROUND_TAB));
-    }
-    const SkColor stroke_color = tab_strip_->GetToolbarTopSeparatorColor();
-    const SkAlpha alpha = static_cast<SkAlpha>(
-        std::round(SkColorGetA(stroke_color) * 0.59375f));
-    cc::PaintFlags shadow_flags = flags;
-    shadow_flags.setLooper(
-        CreateShadowDrawLooper(SkColorSetA(stroke_color, alpha)));
-    canvas->DrawPath(fill, shadow_flags);
-  }
-
-  // Draw a white highlight on hover.
-  const SkAlpha hover_alpha = static_cast<SkAlpha>(
-      hover_animation().CurrentValueBetween(0x00, 0x4D));
-  if (hover_alpha != SK_AlphaTRANSPARENT) {
-    flags.setColor(SkColorSetA(SK_ColorWHITE, hover_alpha));
-    canvas->DrawPath(fill, flags);
-  }
-
-  // Most states' opacities are adjusted using an opacity recorder in
-  // TabStrip::PaintChildren(), but the pressed state is excluded there and
-  // instead rendered using a dark overlay here.  Avoiding the use of the
-  // opacity recorder keeps the stroke more visible in this state.
-  if (pressed) {
-    flags.setColor(SkColorSetA(SK_ColorBLACK, 0x14));
-    canvas->DrawPath(fill, flags);
-  }
-}
 
 ///////////////////////////////////////////////////////////////////////////////
 // TabStrip::RemoveTabDelegate
@@ -1604,7 +1305,7 @@ void TabStrip::Init() {
   set_notify_enter_exit_on_child(true);
 
   newtab_button_bounds_.set_size(GetLayoutSize(NEW_TAB_BUTTON));
-  newtab_button_bounds_.Inset(0, 0, 0, -GetNewTabButtonTopOffset());
+  newtab_button_bounds_.Inset(0, 0, 0, -NewTabButton::GetTopOffset());
   newtab_button_ = new NewTabButton(this, this);
   newtab_button_->SetTooltipText(
       l10n_util::GetStringUTF16(IDS_TOOLTIP_NEW_TAB));
