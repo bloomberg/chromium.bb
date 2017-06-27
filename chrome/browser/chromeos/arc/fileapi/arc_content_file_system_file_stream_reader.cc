31b72c370c2ae1f7b421e1519a14670c41e17693
Ryo Hashimoto <hashimoto@chromium.org>
2017-06-27 15:53:08 +0900

    arc: Switch from BlockingPool to TaskScheduler
    BUG=667892
    
    Change-Id: Ic1113e7da8020f4529a554a9d4483a147ebc59be
    Reviewed-on: https://chromium-review.googlesource.com/549521
    Commit-Queue: Ryo Hashimoto <hashimoto@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482567}
    Former-commit-id: 587eef11c3078e0706b54247efccb8bde6b4b359

--- chrome/browser/chromeos/arc/fileapi/arc_content_file_system_file_stream_reader.cc
+++ chrome/browser/chromeos/arc/fileapi/arc_content_file_system_file_stream_reader.cc
@@ -8,7 +8,7 @@
 #include <unistd.h>
 
 #include "base/files/file.h"
-#include "base/threading/sequenced_worker_pool.h"
+#include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/chromeos/arc/fileapi/arc_file_system_operation_runner_util.h"
 #include "content/public/browser/browser_thread.h"
@@ -41,10 +41,8 @@ ArcContentFileSystemFileStreamReader::ArcContentFileSystemFileStreamReader(
     const GURL& arc_url,
     int64_t offset)
     : arc_url_(arc_url), offset_(offset), weak_ptr_factory_(this) {
-  auto* blocking_pool = content::BrowserThread::GetBlockingPool();
-  task_runner_ = blocking_pool->GetSequencedTaskRunnerWithShutdownBehavior(
-      blocking_pool->GetSequenceToken(),
-      base::SequencedWorkerPool::SKIP_ON_SHUTDOWN);
+  task_runner_ = base::CreateSequencedTaskRunnerWithTraits(
+      {base::MayBlock(), base::TaskShutdownBehavior::SKIP_ON_SHUTDOWN});
 }
 
 ArcContentFileSystemFileStreamReader::~ArcContentFileSystemFileStreamReader() {
