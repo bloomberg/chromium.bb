596dbbad398e0ef37cb2fbdefc52f2fca35bd533
Jungshik Shin <jshin@chromium.org>
2017-06-27 08:41:20 -0700

    Detect the OS timezone using JNI
    ICU timezone detection on Android does not work properly for most
    timezones. [1]
    
    Use JNI to detect the OS timezone and set the ICU default timezone
    accordingly both on ICU initialization and on timezone change.
    
    [1] http://bugs.icu-project.org/trac/ticket/13208
    
    BUG=chromium:722821
    TEST=Change the timezone on Android to a few different zones (e.g.
    Australia/Sydney, Asia/Seoul, Europe/Amstredam). At
    match what's chosen in Android settings.
    
    http: //jungshik.github.io/cr/bugs/722821.html , local tz should
    Change-Id: I70b98f976c289fb3e717431c9c85bfd66d85b19c
    Reviewed-on: https://chromium-review.googlesource.com/512282
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Mark Mentovai <mark@chromium.org>
    Commit-Queue: Jungshik Shin <jshin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482748}
    Former-commit-id: 46cea8687bbbee19b4c48a341c5814ef01dcbb38

--- services/device/time_zone_monitor/time_zone_monitor.cc
+++ services/device/time_zone_monitor/time_zone_monitor.cc
@@ -9,6 +9,10 @@
 #include "third_party/icu/source/common/unicode/unistr.h"
 #include "third_party/icu/source/i18n/unicode/timezone.h"
 
+#if defined(OS_ANDROID)
+#include "base/android/timezone_utils.h"  // nogncheck
+#endif
+
 namespace device {
 
 TimeZoneMonitor::TimeZoneMonitor() {
@@ -30,23 +34,31 @@ void TimeZoneMonitor::NotifyClients() {
   // need to redetect it with detectHostTimeZone().
   std::unique_ptr<icu::TimeZone> new_zone(icu::TimeZone::createDefault());
 #else
-  icu::TimeZone* new_zone = icu::TimeZone::detectHostTimeZone();
+#if defined(OS_ANDROID)
+  base::string16 timezone_id = base::android::GetDefaultTimeZoneId();
+  std::unique_ptr<icu::TimeZone> new_zone(icu::TimeZone::createTimeZone(
+      icu::UnicodeString(FALSE, timezone_id.data(), timezone_id.length())));
+#else
+  std::unique_ptr<icu::TimeZone> new_zone(icu::TimeZone::detectHostTimeZone());
+#endif
 #if defined(OS_LINUX)
   // We get here multiple times on Linux per a single tz change, but
   // want to update the ICU default zone and notify renderer only once.
   std::unique_ptr<icu::TimeZone> current_zone(icu::TimeZone::createDefault());
   if (*current_zone == *new_zone) {
     VLOG(1) << "timezone already updated";
-    delete new_zone;
     return;
   }
 #endif
-  icu::TimeZone::adoptDefault(new_zone);
-#endif
+#endif  // OS_CHROMEOS
   icu::UnicodeString zone_id;
   std::string zone_id_str;
   new_zone->getID(zone_id).toUTF8String(zone_id_str);
   VLOG(1) << "timezone reset to " << zone_id_str;
+
+#if !defined(OS_CHROMEOS)
+  icu::TimeZone::adoptDefault(new_zone.release());
+#endif
 
   clients_.ForAllPtrs(
       [&zone_id_str](device::mojom::TimeZoneMonitorClient* client) {
