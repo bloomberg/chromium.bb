8a3a5b94501615d4592f5879ebf90fb2352b7454
Ricky Liang <jcliang@chromium.org>
2017-06-27 13:33:24 +0800

    media: add ARC++ camera HAL v3 Mojo service
    The ARC++ camera HAL v3 Mojo service serves as a proxy to establish the
    Mojo connections between the camera clients (i.e. VideoCaptureDeviceFactory
    in Chrome and HAL v3 shim in cameraserver process in the Android container)
    and the camera HALv3 process in Chrome OS.
    
    BUG=b:32690003
    TEST=Make sure camera preview works in hangout.
    TEST=unit tests
    
    Change-Id: I9e4e35a49b8efdc158b2c3ef30de352e32198064
    Reviewed-on: https://chromium-review.googlesource.com/540956
    Commit-Queue: Ricky Liang <jcliang@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Christian Fremerey <chfremer@chromium.org>
    Reviewed-by: Kuang-che Wu <kcwu@chromium.org>
    Reviewed-by: Wu-cheng Li <wuchengli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482587}
    Former-commit-id: 6b4ec7b883c9758c427a883a6fe5a461c170b3bf

---
+++ media/capture/video/chromeos/camera_hal_dispatcher_impl_unittest.cc
@@ -0,0 +1,217 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/capture/video/chromeos/camera_hal_dispatcher_impl.h"
+
+#include <memory>
+#include <utility>
+
+#include "base/memory/ptr_util.h"
+#include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
+#include "media/capture/video/chromeos/mojo/arc_camera3_service.mojom.h"
+#include "mojo/public/cpp/bindings/strong_binding.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+using testing::_;
+using testing::InvokeWithoutArgs;
+
+namespace media {
+namespace {
+
+class MockCameraHalServer : public arc::mojom::CameraHalServer {
+ public:
+  MockCameraHalServer() : binding_(this) {}
+
+  ~MockCameraHalServer() {}
+
+  void CreateChannel(
+      arc::mojom::CameraModuleRequest camera_module_request) override {
+    DoCreateChannel(camera_module_request);
+  }
+  MOCK_METHOD1(DoCreateChannel,
+               void(arc::mojom::CameraModuleRequest& camera_module_request));
+
+  arc::mojom::CameraHalServerPtr GetInterfacePtr(
+      scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
+    arc::mojom::CameraHalServerPtr camera_hal_server_ptr;
+    arc::mojom::CameraHalServerRequest camera_hal_server_request =
+        mojo::MakeRequest(&camera_hal_server_ptr, std::move(task_runner));
+    binding_.Bind(std::move(camera_hal_server_request));
+    return camera_hal_server_ptr;
+  }
+
+ private:
+  mojo::Binding<arc::mojom::CameraHalServer> binding_;
+  DISALLOW_COPY_AND_ASSIGN(MockCameraHalServer);
+};
+
+class MockCameraHalClient : public arc::mojom::CameraHalClient {
+ public:
+  MockCameraHalClient() : binding_(this) {}
+
+  ~MockCameraHalClient() {}
+
+  void SetUpChannel(arc::mojom::CameraModulePtr camera_module_ptr) override {
+    DoSetUpChannel(camera_module_ptr);
+  }
+  MOCK_METHOD1(DoSetUpChannel,
+               void(arc::mojom::CameraModulePtr& camera_module_ptr));
+
+  arc::mojom::CameraHalClientPtr GetInterfacePtr(
+      scoped_refptr<base::SingleThreadTaskRunner> task_runner) {
+    arc::mojom::CameraHalClientPtr camera_hal_client_ptr;
+    arc::mojom::CameraHalClientRequest camera_hal_client_request =
+        mojo::MakeRequest(&camera_hal_client_ptr, std::move(task_runner));
+    binding_.Bind(std::move(camera_hal_client_request));
+    return camera_hal_client_ptr;
+  }
+
+ private:
+  mojo::Binding<arc::mojom::CameraHalClient> binding_;
+  DISALLOW_COPY_AND_ASSIGN(MockCameraHalClient);
+};
+
+}  // namespace
+
+class CameraHalDispatcherImplTest : public ::testing::Test {
+ public:
+  CameraHalDispatcherImplTest() {}
+
+  ~CameraHalDispatcherImplTest() override {}
+
+  void SetUp() override {
+    dispatcher_ = new CameraHalDispatcherImpl();
+    EXPECT_TRUE(dispatcher_->StartThreads());
+  }
+
+  void TearDown() override { delete dispatcher_; }
+
+  scoped_refptr<base::SingleThreadTaskRunner> GetProxyTaskRunner() {
+    return dispatcher_->proxy_task_runner_;
+  }
+
+  void DoLoop() {
+    run_loop_.reset(new base::RunLoop());
+    run_loop_->Run();
+  }
+
+  void QuitRunLoop() {
+    VLOG(2) << "quit!";
+    if (run_loop_) {
+      run_loop_->Quit();
+    }
+  }
+
+ protected:
+  // We can't use std::unique_ptr here because the constructor and destructor of
+  // CameraHalDispatcherImpl are private.
+  CameraHalDispatcherImpl* dispatcher_;
+
+ private:
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
+  std::unique_ptr<base::RunLoop> run_loop_;
+  DISALLOW_COPY_AND_ASSIGN(CameraHalDispatcherImplTest);
+};
+
+// Test that the CameraHalDisptcherImpl correctly re-establishes a Mojo channel
+// for the client when the server crashes.
+TEST_F(CameraHalDispatcherImplTest, ServerConnectionError) {
+  // First verify that a the CameraHalDispatcherImpl establishes a Mojo channel
+  // between the server and the client.
+  auto mock_server = base::MakeUnique<MockCameraHalServer>();
+  auto mock_client = base::MakeUnique<MockCameraHalClient>();
+
+  EXPECT_CALL(*mock_server, DoCreateChannel(_)).Times(1);
+  EXPECT_CALL(*mock_client, DoSetUpChannel(_))
+      .Times(1)
+      .WillOnce(
+          InvokeWithoutArgs(this, &CameraHalDispatcherImplTest::QuitRunLoop));
+
+  auto server_ptr = mock_server->GetInterfacePtr(GetProxyTaskRunner());
+  GetProxyTaskRunner()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&CameraHalDispatcherImpl::RegisterServer,
+                     base::Unretained(dispatcher_), base::Passed(&server_ptr)));
+  auto client_ptr = mock_client->GetInterfacePtr(GetProxyTaskRunner());
+  GetProxyTaskRunner()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&CameraHalDispatcherImpl::RegisterClient,
+                     base::Unretained(dispatcher_), base::Passed(&client_ptr)));
+
+  // Wait until the client gets the established Mojo channel.
+  DoLoop();
+
+  // Re-create a new server to simulate a server crash.
+  mock_server = base::MakeUnique<MockCameraHalServer>();
+
+  // Make sure we creates a new Mojo channel from the new server to the same
+  // client.
+  EXPECT_CALL(*mock_server, DoCreateChannel(_)).Times(1);
+  EXPECT_CALL(*mock_client, DoSetUpChannel(_))
+      .Times(1)
+      .WillOnce(
+          InvokeWithoutArgs(this, &CameraHalDispatcherImplTest::QuitRunLoop));
+
+  server_ptr = mock_server->GetInterfacePtr(GetProxyTaskRunner());
+  GetProxyTaskRunner()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&CameraHalDispatcherImpl::RegisterServer,
+                     base::Unretained(dispatcher_), base::Passed(&server_ptr)));
+
+  // Wait until the clients gets the newly established Mojo channel.
+  DoLoop();
+};
+
+// Test that the CameraHalDisptcherImpl correctly re-establishes a Mojo channel
+// for the client when the client reconnects after crash.
+TEST_F(CameraHalDispatcherImplTest, ClientConnectionError) {
+  // First verify that a the CameraHalDispatcherImpl establishes a Mojo channel
+  // between the server and the client.
+  auto mock_server = base::MakeUnique<MockCameraHalServer>();
+  auto mock_client = base::MakeUnique<MockCameraHalClient>();
+
+  EXPECT_CALL(*mock_server, DoCreateChannel(_)).Times(1);
+  EXPECT_CALL(*mock_client, DoSetUpChannel(_))
+      .Times(1)
+      .WillOnce(
+          InvokeWithoutArgs(this, &CameraHalDispatcherImplTest::QuitRunLoop));
+
+  auto server_ptr = mock_server->GetInterfacePtr(GetProxyTaskRunner());
+  GetProxyTaskRunner()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&CameraHalDispatcherImpl::RegisterServer,
+                     base::Unretained(dispatcher_), base::Passed(&server_ptr)));
+  auto client_ptr = mock_client->GetInterfacePtr(GetProxyTaskRunner());
+  GetProxyTaskRunner()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&CameraHalDispatcherImpl::RegisterClient,
+                     base::Unretained(dispatcher_), base::Passed(&client_ptr)));
+
+  // Wait until the client gets the established Mojo channel.
+  DoLoop();
+
+  // Re-create a new server to simulate a server crash.
+  mock_client = base::MakeUnique<MockCameraHalClient>();
+
+  // Make sure we re-create the Mojo channel from the same server to the new
+  // client.
+  EXPECT_CALL(*mock_server, DoCreateChannel(_)).Times(1);
+  EXPECT_CALL(*mock_client, DoSetUpChannel(_))
+      .Times(1)
+      .WillOnce(
+          InvokeWithoutArgs(this, &CameraHalDispatcherImplTest::QuitRunLoop));
+
+  client_ptr = mock_client->GetInterfacePtr(GetProxyTaskRunner());
+  GetProxyTaskRunner()->PostTask(
+      FROM_HERE,
+      base::BindOnce(&CameraHalDispatcherImpl::RegisterClient,
+                     base::Unretained(dispatcher_), base::Passed(&client_ptr)));
+
+  // Wait until the clients gets the newly established Mojo channel.
+  DoLoop();
+};
+
+}  // namespace media
