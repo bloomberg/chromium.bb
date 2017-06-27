fc9015e61636ac2b5ec5fe5a020fc4514579046a
Rouslan Solomakhin <rouslan@chromium.org>
2017-06-27 12:38:07 +0000

    Revert "chromeos: Flags to ignore accidental power button presses."
    This reverts commit 1bdc8d70c6c01be3701c469ee08baab0ea0b5458 [formerly d42ad28d76f64fd6c8e790a8ba74f4f56fd6b08a].
    
    Reason for revert: Broke the following tests on "Linux Chromium OS ASan LSan" bot.
    TabletPowerButtonControllerTest.EnableOnAccelerometerUpdate
    TabletPowerButtonControllerTest.SyncTouchscreenStatus
    
    https://findit-for-me.appspot.com/waterfall/build-failure?url=https://build.chromium.org/p/chromium.memory/builders/Linux%20Chromium%20OS%20ASan%20LSan%20Tests%20%281%29/builds/21984
    
    Original change's description:
    > chromeos: Flags to ignore accidental power button presses.
    > 
    > Add a bunch of experimental flags to chrome://flags to
    > configure the TabletPowerButtonController class to acquire
    > accelerometer data and use it to disregard power button
    > events that it determines to be accidental/spurious.
    > 
    > BUG=736936
    > 
    > Change-Id: Id6573364af65affb1a91fb9c18753a35c18452c5
    > Reviewed-on: https://chromium-review.googlesource.com/549476
    > Commit-Queue: Dan Erat <derat@chromium.org>
    > Reviewed-by: Qiang(Joe) Xu <warx@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#482543}
    
    TBR=derat@chromium.org,warx@chromium.org
    
    Change-Id: I324f92de6a8b38d086a46309c2727599a44b1222
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 736936
    Reviewed-on: https://chromium-review.googlesource.com/549120
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482604}
    Former-commit-id: 982475f97f725663af0cfd0687d92cf17491712b

--- ash/system/power/tablet_power_button_controller.h
+++ ash/system/power/tablet_power_button_controller.h
@@ -6,26 +6,17 @@
 #define ASH_SYSTEM_POWER_TABLET_POWER_BUTTON_CONTROLLER_H_
 
 #include <memory>
-#include <utility>
 
 #include "ash/ash_export.h"
 #include "ash/shell_observer.h"
 #include "base/macros.h"
 #include "base/memory/weak_ptr.h"
-#include "base/scoped_observer.h"
 #include "base/time/tick_clock.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
-#include "chromeos/accelerometer/accelerometer_reader.h"
-#include "chromeos/accelerometer/accelerometer_types.h"
 #include "chromeos/dbus/power_manager_client.h"
 #include "ui/events/devices/input_device_event_observer.h"
 #include "ui/events/event_handler.h"
-#include "ui/gfx/geometry/vector3d_f.h"
-
-namespace base {
-class CommandLine;
-}  // namespace base
 
 namespace ash {
 
@@ -34,8 +25,7 @@ class LockStateController;
 // Handles power button events on convertible/tablet device. This class is
 // instantiated and used in PowerButtonController.
 class ASH_EXPORT TabletPowerButtonController
-    : public chromeos::AccelerometerReader::Observer,
-      public chromeos::PowerManagerClient::Observer,
+    : public chromeos::PowerManagerClient::Observer,
       public ShellObserver,
       public ui::EventHandler,
       public ui::InputDeviceEventObserver {
@@ -52,25 +42,11 @@ class ASH_EXPORT TabletPowerButtonController
     // Emulates |shutdown_timer_| timeout.
     void TriggerShutdownTimeout();
 
-    // Returns true if |controller_| is observing |reader|.
-    bool IsObservingAccelerometerReader(
-        chromeos::AccelerometerReader* reader) const;
-
-    // Calls |controller_|'s ParseSpuriousPowerButtonSwitches() method.
-    void ParseSpuriousPowerButtonSwitches(
-        const base::CommandLine& command_line);
-
-    // Calls |controller_|'s IsSpuriousPowerButtonEvent() method.
-    bool IsSpuriousPowerButtonEvent() const;
-
    private:
     TabletPowerButtonController* controller_;  // Not owned.
 
     DISALLOW_COPY_AND_ASSIGN(TestApi);
   };
-
-  // Public for tests.
-  static constexpr float kGravity = 9.80665f;
 
   explicit TabletPowerButtonController(LockStateController* controller);
   ~TabletPowerButtonController() override;
@@ -81,10 +57,6 @@ class ASH_EXPORT TabletPowerButtonController
 
   // Handles a power button event.
   void OnPowerButtonEvent(bool down, const base::TimeTicks& timestamp);
-
-  // Overridden from chromeos::AccelerometerReader::Observer:
-  void OnAccelerometerUpdated(
-      scoped_refptr<const chromeos::AccelerometerUpdate> update) override;
 
   // Overridden from chromeos::PowerManagerClient::Observer:
   void PowerManagerRestarted() override;
@@ -108,15 +80,6 @@ class ASH_EXPORT TabletPowerButtonController
   void SetTickClockForTesting(std::unique_ptr<base::TickClock> tick_clock);
 
  private:
-  // Parses command-line switches that provide settings used to attempt to
-  // ignore accidental power button presses by looking at accelerometer data.
-  void ParseSpuriousPowerButtonSwitches(const base::CommandLine& command_line);
-
-  // Returns true if the device's accelerometers have reported enough recent
-  // movement that we should consider a power button event that was just
-  // received to be accidental and ignore it.
-  bool IsSpuriousPowerButtonEvent() const;
-
   // Updates the power manager's backlights-forced-off state and enables or
   // disables the touchscreen. No-op if |backlights_forced_off_| already equals
   // |forced_off|.
@@ -153,10 +116,6 @@ class ASH_EXPORT TabletPowerButtonController
   // True if the screen was off when the power button was pressed.
   bool screen_off_when_power_button_down_ = false;
 
-  // True if the last power button down event was deemed spurious and ignored as
-  // a result.
-  bool power_button_down_was_spurious_ = false;
-
   // Time source for performed action times.
   std::unique_ptr<base::TickClock> tick_clock_;
 
@@ -175,30 +134,6 @@ class ASH_EXPORT TabletPowerButtonController
   base::OneShotTimer shutdown_timer_;
 
   LockStateController* controller_;  // Not owned.
-
-  ScopedObserver<chromeos::AccelerometerReader, TabletPowerButtonController>
-      accelerometer_scoped_observer_;
-
-  // Number of recent screen and keyboard accelerometer samples to retain.
-  size_t max_accelerometer_samples_ = 0;
-
-  // Circular buffer of recent (screen, keyboard) accelerometer samples.
-  std::vector<std::pair<gfx::Vector3dF, gfx::Vector3dF>> accelerometer_samples_;
-  size_t last_accelerometer_sample_index_ = 0;
-
-  // Number of acceleration readings in |accelerometer_samples_| that must
-  // exceed the threshold in order for a power button event to be considered
-  // spurious.
-  size_t spurious_accel_count_ = 0;
-
-  // Thresholds for screen and keyboard accelerations (excluding gravity). See
-  // |spurious_accel_count_|.
-  float spurious_screen_accel_ = 0;
-  float spurious_keyboard_accel_ = 0;
-
-  // Threshold for the lid angle change seen within |accelerometer_samples_|
-  // in order for a power button event to be considered spurious.
-  float spurious_lid_angle_change_ = 0;
 
   base::WeakPtrFactory<TabletPowerButtonController> weak_ptr_factory_;
 
