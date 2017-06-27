70e6a2f9d37411b272c5ed05c99976905c061961
dougt <dougt@chromium.org>
2017-06-27 14:36:46 -0700

    Forward BrowserAccessibility get_accState to AXPlatformNode.
    This is another migration from BrowserAccessibility to AXPlatformNodeWin.
    In this installment, we're converting get_accState.
    
    We attempted to land this CL in https://codereview.chromium.org/2913553002/.
    The CL was reverted due to lack of adequate testing. We since have improved
    our testing in bug 733437.
    
    BUG=703369
    
    Review-Url: https://codereview.chromium.org/2943243002
    Cr-Commit-Position: refs/heads/master@{#482751}
    
    Former-commit-id: 57c6dff1214ac4d327d0705d89534412a1743f3c

--- content/browser/accessibility/browser_accessibility.h
+++ content/browser/accessibility/browser_accessibility.h
@@ -393,6 +393,7 @@ class CONTENT_EXPORT BrowserAccessibility : public ui::AXPlatformNodeDelegate {
   gfx::NativeViewAccessible GetFocus() override;
   gfx::AcceleratedWidget GetTargetForNativeAccessibilityEvent() override;
   bool AccessibilityPerformAction(const ui::AXActionData& data) override;
+  bool ShouldIgnoreHoveredStateForTesting() override;
 
  protected:
   using AXPlatformPositionInstance = AXPlatformPosition::AXPositionInstance;
