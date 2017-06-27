c5a1230ecba2665d6b5c5f03899f60e149b44940
mmenke <mmenke@chromium.org>
2017-06-27 14:15:01 -0700

    NetworkService: Destroy NetworkContexts on NetworkService teardown.
    Also fix NetworkContexts potentially not being destroyed on connection
    error / consumer teardown, due to calling str::move twice on the same
    RequestPtr.
    
    BUG=736472
    
    Review-Url: https://codereview.chromium.org/2962693002
    Cr-Commit-Position: refs/heads/master@{#482745}
    
    Former-commit-id: cfbf85b0c37adf3acccead282bf5c9b794ba4009

--- content/network/network_context.h
+++ content/network/network_context.h
@@ -22,11 +22,13 @@ class URLRequestContext;
 }
 
 namespace content {
+class NetworkService;
 class URLLoaderImpl;
 
 class NetworkContext : public mojom::NetworkContext {
  public:
-  NetworkContext(mojom::NetworkContextRequest request,
+  NetworkContext(NetworkService* network_service,
+                 mojom::NetworkContextRequest request,
                  mojom::NetworkContextParamsPtr params);
   ~NetworkContext() override;
 
@@ -47,8 +49,17 @@ class NetworkContext : public mojom::NetworkContext {
   void HandleViewCacheRequest(const GURL& url,
                               mojom::URLLoaderClientPtr client) override;
 
+  // Called when the associated NetworkService is going away. Guaranteed to
+  // destroy NetworkContext's URLRequestContext.
+  void Cleanup();
+
  private:
   NetworkContext();
+
+  // On connection errors the NetworkContext destroys itself.
+  void OnConnectionError();
+
+  NetworkService* const network_service_;
 
   std::unique_ptr<net::URLRequestContext> url_request_context_;
 
