5bf71ebec26a240d6a74eda475375dd57df94c9d
Greg Thompson <grt@chromium.org>
2017-06-28 18:21:20 +0200

    Removing some dead code left behind after r313626.
    BUG=297647
    R=huangs@chromium.org
    
    Change-Id: I94bc4cd8075581cc3d0d91061131562e0d194d76
    Reviewed-on: https://chromium-review.googlesource.com/552555
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483040}
    Former-commit-id: cf092b655e25bce6e63092fe28066b8d252b0cb4

--- chrome/installer/util/google_update_util.cc
+++ chrome/installer/util/google_update_util.cc
@@ -8,75 +8,19 @@
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
 #include "base/logging.h"
-#include "base/process/kill.h"
 #include "base/process/launch.h"
 #include "base/strings/string16.h"
 #include "base/time/time.h"
-#include "base/win/registry.h"
-#include "base/win/scoped_handle.h"
 #include "base/win/win_util.h"
-#include "chrome/installer/util/google_update_constants.h"
 #include "chrome/installer/util/google_update_settings.h"
 #include "chrome/installer/util/install_util.h"
 #include "chrome/installer/util/installation_state.h"
-#include "chrome/installer/util/product.h"
-
-using base::win::RegKey;
 
 namespace google_update {
 
 namespace {
 
 const int kGoogleUpdateTimeoutMs = 20 * 1000;
-
-// Returns true if Google Update is present at the given level.
-bool IsGoogleUpdatePresent(bool system_install) {
-  // Using the existence of version key in the registry to decide.
-  return GoogleUpdateSettings::GetGoogleUpdateVersion(system_install).IsValid();
-}
-
-// Returns GoogleUpdateSetup.exe's executable path at specified level.
-// or an empty path if none is found.
-base::FilePath GetGoogleUpdateSetupExe(bool system_install) {
-  const HKEY root_key = system_install ? HKEY_LOCAL_MACHINE : HKEY_CURRENT_USER;
-  RegKey update_key;
-
-  if (update_key.Open(root_key, kRegPathGoogleUpdate, KEY_QUERY_VALUE) ==
-          ERROR_SUCCESS) {
-    base::string16 path_str;
-    base::string16 version_str;
-    if ((update_key.ReadValue(kRegPathField, &path_str) == ERROR_SUCCESS) &&
-        (update_key.ReadValue(kRegGoogleUpdateVersion, &version_str) ==
-             ERROR_SUCCESS)) {
-      return base::FilePath(path_str).DirName().Append(version_str).
-          Append(kGoogleUpdateSetupExe);
-    }
-  }
-  return base::FilePath();
-}
-
-// If Google Update is present at system-level, sets |cmd_string| to the command
-// line to install Google Update at user-level and returns true.
-// Otherwise, clears |cmd_string| and returns false.
-bool GetUserLevelGoogleUpdateInstallCommandLine(base::string16* cmd_string) {
-  cmd_string->clear();
-  base::FilePath google_update_setup(
-      GetGoogleUpdateSetupExe(true));  // system-level.
-  if (!google_update_setup.empty()) {
-    base::CommandLine cmd(google_update_setup);
-    // Appends "/install runtime=true&needsadmin=false /silent /nomitag".
-    // NB: /nomitag needs to be at the end.
-    // Constants are found in code.google.com/p/omaha/common/const_cmd_line.h.
-    cmd.AppendArg("/install");
-    // The "&" can be used in base::LaunchProcess() without quotation
-    // (this is problematic only if run from command prompt).
-    cmd.AppendArg("runtime=true&needsadmin=false");
-    cmd.AppendArg("/silent");
-    cmd.AppendArg("/nomitag");
-    *cmd_string = cmd.GetCommandLineString();
-  }
-  return !cmd_string->empty();
-}
 
 // Launches command |cmd_string|, and waits for |timeout| milliseconds before
 // timing out.  To wait indefinitely, one can set
@@ -106,29 +50,6 @@ bool LaunchProcessAndWaitWithTimeout(const base::string16& cmd_string,
 }
 
 }  // namespace
-
-bool EnsureUserLevelGoogleUpdatePresent() {
-  VLOG(0) << "Ensuring Google Update is present at user-level.";
-
-  bool success = false;
-  if (IsGoogleUpdatePresent(false)) {
-    success = true;
-  } else {
-    base::string16 cmd_string;
-    if (!GetUserLevelGoogleUpdateInstallCommandLine(&cmd_string)) {
-      LOG(ERROR) << "Cannot find Google Update at system-level.";
-      // Ideally we should return false. However, this case should not be
-      // encountered by regular users, and developers (who often installs
-      // Chrome without Google Update) may be unduly impeded by this case.
-      // Therefore we return true.
-      success = true;
-    } else {
-      success = LaunchProcessAndWaitWithTimeout(cmd_string,
-          base::TimeDelta::FromMilliseconds(INFINITE));
-    }
-  }
-  return success;
-}
 
 bool UninstallGoogleUpdate(bool system_install) {
   bool success = false;
