2521901af6381895772c6f88ed4e6e3cc92edac9
Ken Rockot <rockot@chromium.org>
2017-06-27 17:04:40 -0700

    Mojo C++ Bindings: Move bits of InterfacePtrState out-of-line
    BUG=737372
    
    Change-Id: I84b735900c0f065d80b927868696688b46ca7caa
    Reviewed-on: https://chromium-review.googlesource.com/551086
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482960}
    Former-commit-id: 7ccd5748af65f98edbb5fbd44e60b7e61f8cec3f

--- mojo/public/cpp/bindings/lib/interface_ptr_state.h
+++ mojo/public/cpp/bindings/lib/interface_ptr_state.h
@@ -20,6 +20,7 @@
 #include "base/memory/ref_counted.h"
 #include "base/sequenced_task_runner.h"
 #include "mojo/public/cpp/bindings/associated_group.h"
+#include "mojo/public/cpp/bindings/bindings_export.h"
 #include "mojo/public/cpp/bindings/connection_error_callback.h"
 #include "mojo/public/cpp/bindings/filter_chain.h"
 #include "mojo/public/cpp/bindings/interface_endpoint_client.h"
@@ -32,17 +33,79 @@
 namespace mojo {
 namespace internal {
 
-template <typename Interface>
-class InterfacePtrState {
+class MOJO_CPP_BINDINGS_EXPORT InterfacePtrStateBase {
  public:
-  InterfacePtrState() : version_(0u) {}
+  InterfacePtrStateBase();
+  ~InterfacePtrStateBase();
 
-  ~InterfacePtrState() {
-    endpoint_client_.reset();
-    proxy_.reset();
-    if (router_)
-      router_->CloseMessagePipe();
+  MessagePipeHandle handle() const {
+    return router_ ? router_->handle() : handle_.get();
   }
+
+  uint32_t version() const { return version_; }
+
+  bool is_bound() const { return handle_.is_valid() || endpoint_client_; }
+
+  bool encountered_error() const {
+    return endpoint_client_ ? endpoint_client_->encountered_error() : false;
+  }
+
+  bool HasAssociatedInterfaces() const {
+    return router_ ? router_->HasAssociatedEndpoints() : false;
+  }
+
+  // Returns true if bound and awaiting a response to a message.
+  bool has_pending_callbacks() const {
+    return endpoint_client_ && endpoint_client_->has_pending_responders();
+  }
+
+ protected:
+  InterfaceEndpointClient* endpoint_client() const {
+    return endpoint_client_.get();
+  }
+  MultiplexRouter* router() const { return router_.get(); }
+
+  void QueryVersion(const base::Callback<void(uint32_t)>& callback);
+  void RequireVersion(uint32_t version);
+  void Swap(InterfacePtrStateBase* other);
+  void Bind(ScopedMessagePipeHandle handle,
+            uint32_t version,
+            scoped_refptr<base::SequencedTaskRunner> task_runner);
+
+  ScopedMessagePipeHandle PassMessagePipe() {
+    endpoint_client_.reset();
+    return router_ ? router_->PassMessagePipe() : std::move(handle_);
+  }
+
+  bool InitializeEndpointClient(
+      bool passes_associated_kinds,
+      bool has_sync_methods,
+      std::unique_ptr<MessageReceiver> payload_validator);
+
+ private:
+  void OnQueryVersion(const base::Callback<void(uint32_t)>& callback,
+                      uint32_t version);
+
+  scoped_refptr<MultiplexRouter> router_;
+
+  std::unique_ptr<InterfaceEndpointClient> endpoint_client_;
+
+  // |router_| (as well as other members above) is not initialized until
+  // read/write with the message pipe handle is needed. |handle_| is valid
+  // between the Bind() call and the initialization of |router_|.
+  ScopedMessagePipeHandle handle_;
+  scoped_refptr<base::SequencedTaskRunner> runner_;
+
+  uint32_t version_ = 0;
+
+  DISALLOW_COPY_AND_ASSIGN(InterfacePtrStateBase);
+};
+
+template <typename Interface>
+class InterfacePtrState : public InterfacePtrStateBase {
+ public:
+  InterfacePtrState() = default;
+  ~InterfacePtrState() = default;
 
   Interface* instance() {
     ConfigureProxyIfNecessary();
@@ -51,120 +114,81 @@ class InterfacePtrState {
     return proxy_.get();
   }
 
-  uint32_t version() const { return version_; }
-
   void QueryVersion(const base::Callback<void(uint32_t)>& callback) {
     ConfigureProxyIfNecessary();
-
-    // It is safe to capture |this| because the callback won't be run after this
-    // object goes away.
-    endpoint_client_->QueryVersion(base::Bind(
-        &InterfacePtrState::OnQueryVersion, base::Unretained(this), callback));
+    InterfacePtrStateBase::QueryVersion(callback);
   }
 
   void RequireVersion(uint32_t version) {
     ConfigureProxyIfNecessary();
-
-    if (version <= version_)
-      return;
-
-    version_ = version;
-    endpoint_client_->RequireVersion(version);
+    InterfacePtrStateBase::RequireVersion(version);
   }
 
   void FlushForTesting() {
     ConfigureProxyIfNecessary();
-    endpoint_client_->FlushForTesting();
+    endpoint_client()->FlushForTesting();
   }
 
   void CloseWithReason(uint32_t custom_reason, const std::string& description) {
     ConfigureProxyIfNecessary();
-    endpoint_client_->CloseWithReason(custom_reason, description);
+    endpoint_client()->CloseWithReason(custom_reason, description);
   }
 
   void Swap(InterfacePtrState* other) {
     using std::swap;
-    swap(other->router_, router_);
-    swap(other->endpoint_client_, endpoint_client_);
     swap(other->proxy_, proxy_);
-    handle_.swap(other->handle_);
-    runner_.swap(other->runner_);
-    swap(other->version_, version_);
+    InterfacePtrStateBase::Swap(other);
   }
 
   void Bind(InterfacePtrInfo<Interface> info,
             scoped_refptr<base::SequencedTaskRunner> runner) {
-    DCHECK(!router_);
-    DCHECK(!endpoint_client_);
     DCHECK(!proxy_);
-    DCHECK(!handle_.is_valid());
-    DCHECK_EQ(0u, version_);
-    DCHECK(info.is_valid());
-
-    handle_ = info.PassHandle();
-    version_ = info.version();
-    runner_ = std::move(runner);
-  }
-
-  bool HasAssociatedInterfaces() const {
-    return router_ ? router_->HasAssociatedEndpoints() : false;
+    InterfacePtrStateBase::Bind(info.PassHandle(), info.version(),
+                                std::move(runner));
   }
 
   // After this method is called, the object is in an invalid state and
   // shouldn't be reused.
   InterfacePtrInfo<Interface> PassInterface() {
-    endpoint_client_.reset();
     proxy_.reset();
-    return InterfacePtrInfo<Interface>(
-        router_ ? router_->PassMessagePipe() : std::move(handle_), version_);
-  }
-
-  bool is_bound() const { return handle_.is_valid() || endpoint_client_; }
-
-  bool encountered_error() const {
-    return endpoint_client_ ? endpoint_client_->encountered_error() : false;
+    return InterfacePtrInfo<Interface>(PassMessagePipe(), version());
   }
 
   void set_connection_error_handler(base::OnceClosure error_handler) {
     ConfigureProxyIfNecessary();
 
-    DCHECK(endpoint_client_);
-    endpoint_client_->set_connection_error_handler(std::move(error_handler));
+    DCHECK(endpoint_client());
+    endpoint_client()->set_connection_error_handler(std::move(error_handler));
   }
 
   void set_connection_error_with_reason_handler(
       ConnectionErrorWithReasonCallback error_handler) {
     ConfigureProxyIfNecessary();
 
-    DCHECK(endpoint_client_);
-    endpoint_client_->set_connection_error_with_reason_handler(
+    DCHECK(endpoint_client());
+    endpoint_client()->set_connection_error_with_reason_handler(
         std::move(error_handler));
-  }
-
-  // Returns true if bound and awaiting a response to a message.
-  bool has_pending_callbacks() const {
-    return endpoint_client_ && endpoint_client_->has_pending_responders();
   }
 
   AssociatedGroup* associated_group() {
     ConfigureProxyIfNecessary();
-    return endpoint_client_->associated_group();
+    return endpoint_client()->associated_group();
   }
 
   void EnableTestingMode() {
     ConfigureProxyIfNecessary();
-    router_->EnableTestingMode();
+    router()->EnableTestingMode();
   }
 
   void ForwardMessage(Message message) {
     ConfigureProxyIfNecessary();
-    endpoint_client_->Accept(&message);
+    endpoint_client()->Accept(&message);
   }
 
   void ForwardMessageWithResponder(Message message,
                                    std::unique_ptr<MessageReceiver> responder) {
     ConfigureProxyIfNecessary();
-    endpoint_client_->AcceptWithResponder(&message, std::move(responder));
+    endpoint_client()->AcceptWithResponder(&message, std::move(responder));
   }
 
  private:
@@ -173,50 +197,20 @@ class InterfacePtrState {
   void ConfigureProxyIfNecessary() {
     // The proxy has been configured.
     if (proxy_) {
-      DCHECK(router_);
-      DCHECK(endpoint_client_);
+      DCHECK(router());
+      DCHECK(endpoint_client());
       return;
     }
-    // The object hasn't been bound.
-    if (!handle_.is_valid())
-      return;
 
-    MultiplexRouter::Config config =
-        Interface::PassesAssociatedKinds_
-            ? MultiplexRouter::MULTI_INTERFACE
-            : (Interface::HasSyncMethods_
-                   ? MultiplexRouter::SINGLE_INTERFACE_WITH_SYNC_METHODS
-                   : MultiplexRouter::SINGLE_INTERFACE);
-    router_ = new MultiplexRouter(std::move(handle_), config, true, runner_);
-    router_->SetMasterInterfaceName(Interface::Name_);
-    endpoint_client_.reset(new InterfaceEndpointClient(
-        router_->CreateLocalEndpointHandle(kMasterInterfaceId), nullptr,
-        base::WrapUnique(new typename Interface::ResponseValidator_()), false,
-        std::move(runner_),
-        // The version is only queried from the client so the value passed here
-        // will not be used.
-        0u));
-    proxy_.reset(new Proxy(endpoint_client_.get()));
+    if (InitializeEndpointClient(
+            Interface::PassesAssociatedKinds_, Interface::HasSyncMethods_,
+            base::MakeUnique<typename Interface::ResponseValidator_>())) {
+      router()->SetMasterInterfaceName(Interface::Name_);
+      proxy_ = base::MakeUnique<Proxy>(endpoint_client());
+    }
   }
 
-  void OnQueryVersion(const base::Callback<void(uint32_t)>& callback,
-                      uint32_t version) {
-    version_ = version;
-    callback.Run(version);
-  }
-
-  scoped_refptr<MultiplexRouter> router_;
-
-  std::unique_ptr<InterfaceEndpointClient> endpoint_client_;
   std::unique_ptr<Proxy> proxy_;
-
-  // |router_| (as well as other members above) is not initialized until
-  // read/write with the message pipe handle is needed. |handle_| is valid
-  // between the Bind() call and the initialization of |router_|.
-  ScopedMessagePipeHandle handle_;
-  scoped_refptr<base::SequencedTaskRunner> runner_;
-
-  uint32_t version_;
 
   DISALLOW_COPY_AND_ASSIGN(InterfacePtrState);
 };
