b639225f221a14253f1409dc28193ce47ebab3dc
Daniel Vogelheim <vogelheim@chromium.org>
2017-06-28 12:44:05 +0200

    Ensure FrameFetchContext::Url() works if !document_.
    When created via CreateFetcherFromDocumentLoader, the context will have neither
    a document_ nor a frozen_state_. Url() needs to return something vaguely
    useful in this case.
    
    Bug: 733147
    Change-Id: I86532eb2234d74cf6fc9055ed0fc331a939b75ce
    Reviewed-on: https://chromium-review.googlesource.com/549638
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482959}
    Former-commit-id: 47f802227d3d74f8535df2d17f46d6cce75228c8

--- third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
+++ third_party/WebKit/Source/core/loader/FrameFetchContext.cpp
@@ -988,6 +988,8 @@ String FrameFetchContext::GetOutgoingReferrer() const {
 const KURL& FrameFetchContext::Url() const {
   if (IsDetached())
     return frozen_state_->url;
+  if (!document_)
+    return NullURL();
   return document_->Url();
 }
 
