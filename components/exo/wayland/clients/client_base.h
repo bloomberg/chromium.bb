081a714b41b20b79ab4873674df394040c8e1a4c
Daniele Castagna <dcastagna@chromium.org>
2017-06-27 20:34:39 -0400

    exo: Add MessageLoopForUI in wayland clients.
    This is needed to initialize Ozone, otherwise the clients
    crash when --use-drm is passed.
    
    Bug: 
    Change-Id: Ic10131c7e61af03951f9791d4987b412e63bf6c1
    Reviewed-on: https://chromium-review.googlesource.com/551295
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482824}
    Former-commit-id: d113d16354bdf66f1471a131cac316e4ddb68e4d

--- components/exo/wayland/clients/client_base.h
+++ components/exo/wayland/clients/client_base.h
@@ -19,6 +19,7 @@
 
 namespace base {
 class CommandLine;
+class MessageLoopForUI;
 }
 
 namespace exo {
@@ -92,6 +93,7 @@ class ClientBase {
   std::unique_ptr<wl_shell_surface> shell_surface_;
   Globals globals_;
 #if defined(OZONE_PLATFORM_GBM)
+  std::unique_ptr<base::MessageLoopForUI> ui_loop_;
   base::ScopedFD drm_fd_;
   std::unique_ptr<gbm_device> device_;
 #endif
