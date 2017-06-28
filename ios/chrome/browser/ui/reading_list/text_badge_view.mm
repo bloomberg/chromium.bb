ca2df45e4df1844e46eb4c7a3751a84d24188dde
helenlyang <helenlyang@google.com>
2017-06-28 16:58:53 -0700

    [ios] TextBadgeView interface, implementation, and unit test stubs.
    Added header, implementation, and unit test files for TextBadgeView.
    Updated the BUILD.gn file in the reading_list directory to include the
    header and implementation files in the reading_list_ui source set, and
    the unit test file in the unit_tests source set. The header,
    implementation, and unit test files are stub implementations.
    
    For context, the TextBadgeView is part of the User Education project.
    
    BUG=none.
    
    Review-Url: https://codereview.chromium.org/2954853004
    Cr-Commit-Position: refs/heads/master@{#483205}
    
    Former-commit-id: e28c24b9848609678128c3644f752bcf8d5832d7

---
+++ ios/chrome/browser/ui/reading_list/text_badge_view.mm
@@ -0,0 +1,18 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/reading_list/text_badge_view.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+@implementation TextBadgeView
+
+- (instancetype)initWithText:(NSString*)text {
+  self = [super initWithFrame:CGRectZero];
+  return self;
+}
+
+@end
