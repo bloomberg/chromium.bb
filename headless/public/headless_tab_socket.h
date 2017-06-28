b40abe55179f8fcd5a485369f9314c8a592d6d82
Alex Clarke <alexclarke@chromium.org>
2017-06-28 13:03:00 +0100

    Headless: Support multiple TabSockets
    TabSockets are intended to either be installed into the main world or
    a isolated world created by Page.createIsolatedWorld.  Previously we
    only supported a single TabSocket per WebContents but that's unfortunate
    with OOPIF.  This patch lets us support multiple TabSockets up to one
    per world per frame.
    
    The mojoms have changed a bit with
    TabSocket::AwaitNextMessageFromEmbedder moving to 
    HeadlessRenderFrameController::SendMessageToTabSocket which simplifies
    recieveing a message from the embedder since a hanging IPC is no
    longer needed.
    
    Bug: 546953
    Change-Id: I835ffcdf3bf9acd5be687074977346fb19f69d12
    Reviewed-on: https://chromium-review.googlesource.com/543160
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Mike West <mkwst@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#482990}
    Former-commit-id: c5b3efa9a0ea0b0d2e4aa28784e6d7029b7f1289

--- headless/public/headless_tab_socket.h
+++ headless/public/headless_tab_socket.h
@@ -8,6 +8,7 @@
 #include <string>
 
 #include "base/macros.h"
+#include "base/optional.h"
 #include "headless/public/headless_export.h"
 
 namespace headless {
@@ -20,15 +21,34 @@ class HEADLESS_EXPORT HeadlessTabSocket {
     Listener() {}
     virtual ~Listener() {}
 
-    // The |message| may be potentially sent by untrusted web content so it
-    // should be validated carefully.
-    virtual void OnMessageFromTab(const std::string& message) = 0;
+    // The |message| and |v8_execution_context_id| may be potentially sent by
+    // untrusted web content so it should be validated carefully.
+    virtual void OnMessageFromContext(const std::string& message,
+                                      int v8_execution_context_id) = 0;
 
    private:
     DISALLOW_COPY_AND_ASSIGN(Listener);
   };
 
-  virtual void SendMessageToTab(const std::string& message) = 0;
+  // Installs headless tab socket bindings into the specified execution context.
+  // If the bindings are successfully installed then the |callback| is run with
+  // |success| = true, otherwise with |success| = false.
+  virtual void InstallHeadlessTabSocketBindings(
+      int v8_execution_context_id,
+      base::Callback<void(bool success)> callback) = 0;
+
+  // Installs headless tab socket bindings into the main frame main world. If
+  // the bindings were installed correctly then |callback| is run with a
+  // non-empty base::Optional<int> containing the main world
+  // v8_execution_context_id, otherwise |callback| is run with an empty
+  // base::Optional<int>.
+  virtual void InstallMainFrameMainWorldHeadlessTabSocketBindings(
+      base::Callback<void(base::Optional<int> v8_execution_context_id)>
+          callback) = 0;
+
+  // Note this will fail unless the bindings have been installed.
+  virtual void SendMessageToContext(const std::string& message,
+                                    int v8_execution_context_id) = 0;
 
   virtual void SetListener(Listener* listener) = 0;
 
