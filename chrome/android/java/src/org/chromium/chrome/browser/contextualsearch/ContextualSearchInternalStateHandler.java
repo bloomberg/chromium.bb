e2526d46f2d761d33b3884b574d62ae905d2b24b
donnd <donnd@chromium.org>
2017-06-29 15:08:19 -0700

    [TTS] Fix tap on a previous tap-selection.
    When a tap-select is later tapped upon, it should always show
    the selection pins and the CS Bar.  However this isn't working
    when the later tap happens very quickly after the first tap.
    
    The problem is that the second tap happens before we get notification
    that the selection pins have been shown.  TTS starts the process
    of handling the tap unless it's fully handled the previous one,
    which is flawed logic.  This flawed logic may have been responsible
    for some taps being ignored.
    
    This CL introduces a new state that's used to wait for the tap
    gesture to be converted to a long-press gesture.  Unless we're
    in the IDLE state when a tap is recognized we'll enter this new
    wait state and usually timeout and advance our normal tap
    processing.  In the case of the user doing a second tap the
    recognition of the long-press abandons the previous tap processing.
    
    Also clean up our @see directives in CSInternalStateHandler.java.
    
    BUG=736237,717942
    
    Review-Url: https://codereview.chromium.org/2963013002
    Cr-Commit-Position: refs/heads/master@{#483510}
    
    Former-commit-id: 04a6e962e89fbab7754fc4c796b25b6ddc134985

--- chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchInternalStateHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextualsearch/ContextualSearchInternalStateHandler.java
@@ -13,51 +13,60 @@ import org.chromium.chrome.browser.compositor.bottombar.OverlayPanel.StateChange
 public interface ContextualSearchInternalStateHandler {
     /**
      * Hides the Contextual Search user interface.
-     * {@See ContextualSearchInternalStateController#InternalState#IDLE}.
+     * @see ContextualSearchInternalStateController.InternalState#IDLE
      */
     void hideContextualSearchUi(StateChangeReason reason);
 
     /**
      * Shows the Contextual Search user interface for a Tap.
-     * {@See ContextualSearchInternalStateController#InternalState#SHOW_FULL_TAP_UI}.
+     * @see ContextualSearchInternalStateController.InternalState#SHOW_FULL_TAP_UI
      */
     void showContextualSearchTapUi();
 
     /**
      * Shows the Contextual Search user interface for a Long-press.
-     * {@See ContextualSearchInternalStateController#InternalState#SHOWING_LONGPRESS_SEARCH}.
+     * @see ContextualSearchInternalStateController.InternalState#SHOWING_LONGPRESS_SEARCH
      */
     void showContextualSearchLongpressUi();
 
     /**
      * Gathers text surrounding the current selection, which may have been created by either a Tap
      * or a Long-press gesture.
-     * {@See ContextualSearchInternalStateController#InternalState#GATHERING_SURROUNDINGS}.
+     * @see ContextualSearchInternalStateController.InternalState#GATHERING_SURROUNDINGS
      */
     void gatherSurroundingText();
 
     /**
      * Starts the process of deciding if we'll suppress the current gesture or not.
-     * {@See ContextualSearchInternalStateController#InternalState#DECIDING_SUPPRESSION}.
+     * @see ContextualSearchInternalStateController.InternalState#DECIDING_SUPPRESSION
      */
     void decideSuppression();
 
     /**
      * Starts the process of selecting a word around the current caret.
-     * {@See ContextualSearchInternalStateController#InternalState#START_SHOWING_TAP_UI}.
+     * @see ContextualSearchInternalStateController.InternalState#START_SHOWING_TAP_UI
      */
     void startShowingTapUi();
 
     /**
-     * Waits to see if a Tap gesture will be made when a previous Tap was recognized.
-     * {@See
-     * ContextualSearchInternalStateController#InternalState#WAITING_FOR_POSSIBLE_TAP_NEAR_PREVIOUS}
+     * Waits to see if a Tap gesture will be made when the selection has been cleared, which allows
+     * a Tap near a previous Tap to be handled without a hide/show of the UI.
+     * @see
+     * ContextualSearchInternalStateController.InternalState#WAITING_FOR_POSSIBLE_TAP_NEAR_PREVIOUS
      */
     void waitForPossibleTapNearPrevious();
 
     /**
+     * Waits to see if a Tap gesture will be made on a previous Tap-selection when a Tap was
+     * recognized.
+     * @see ContextualSearchInternalStateController.InternalState#
+     * WAITING_FOR_POSSIBLE_TAP_ON_TAP_SELECTION
+     */
+    void waitForPossibleTapOnTapSelection();
+
+    /**
      * Starts a Resolve request to our server for the best Search Term.
-     * {@See ContextualSearchInternalStateController#InternalState#RESOLVING}.
+     * @see ContextualSearchInternalStateController.InternalState#RESOLVING
      */
     void resolveSearchTerm();
 }
