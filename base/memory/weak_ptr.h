80f1842563f29a4e99bf341f5c78c09b6c4790ba
hans <hans@chromium.org>
2017-06-29 11:22:36 -0700

    Make base::WeakPtr::Get() fast
    It was previously calling WeakReference::is_valid() which was an out-of-line
    method. That means code like
    
      if (my_weak_ptr)
        my_weak_ptr->foo();
    
    would do two calls to is_valid(), plus tests and branching.
    
    The is_valid() method showed up as one of the most frequently called non-inline
    functions during browser start-up on Windows (about 1M calls).
    
    is_valid() was also inefficient because it had to do a null-pointer check on
    flag_, as well as checking if the flag was marked valid.
    
    This patch removes the null-pointer check by using a sentinel object instead of
    a nullptr. And instead of storing a bool in the flag, it stores a pointer-sized
    bitmask 0 or ~0, so it can be AND'ed with the pointer value and conveniently
    setting EFLAGS so that the code above just becomes a few loads, AND, branch,
    and call. (The size of Flag is unchanged; it grows into the padding only.)
    
    This is expected to reduce the binary size by ~48KB on Android, and increase it
    by 79KB on x64 Linux -- something that's paid for by the split-out refactorings
    to WeakPtr and WeakPtrFactory.
    
    Exposing the SequenceChecker calls in inline functions caused the
    MessageLoopTestType*.Nesting test to overflow on Win64 dcheck release builds,
    which is why this patch also lowers the recursion depth there.
    
    BUG=728324
    
    Review-Url: https://codereview.chromium.org/2963623002
    Cr-Commit-Position: refs/heads/master@{#483427}
    
    Former-commit-id: 526f714c9ae172c3b16581b7e11eb035fd14274e

--- base/memory/weak_ptr.h
+++ base/memory/weak_ptr.h
@@ -96,16 +96,59 @@ class BASE_EXPORT WeakReference {
    public:
     Flag();
 
-    void Invalidate();
-    bool IsValid() const;
+    // Get a pointer to the "Null Flag", a sentinel object used by WeakReference
+    // objects that don't point to a valid Flag, either because they're default
+    // constructed or because they have been invalidated. This can be used like
+    // any other Flag object, but it is invalidated already from the start, and
+    // its refcount will never reach zero.
+    static Flag* NullFlag();
+
+    void Invalidate() {
+#if DCHECK_IS_ON()
+      if (this == NullFlag()) {
+        // The Null Flag does not participate in the sequence checks below.
+        // Since its state never changes, it can be accessed from any thread.
+        DCHECK(!is_valid_);
+        return;
+      }
+      // The flag being invalidated with a single ref implies that there are no
+      // weak pointers in existence. Allow deletion on other thread in this
+      // case.
+      DCHECK(sequence_checker_.CalledOnValidSequence() || HasOneRef())
+          << "WeakPtrs must be invalidated on the same sequenced thread.";
+#endif
+      is_valid_ = 0;
+    }
+
+    // Returns a pointer-sized bitmask of all 1s if valid or all 0s otherwise.
+    uintptr_t IsValid() const {
+#if DCHECK_IS_ON()
+      if (this == NullFlag()) {
+        // The Null Flag does not participate in the sequence checks below.
+        // Since its state never changes, it can be accessed from any thread.
+        DCHECK(!is_valid_);
+        return 0;
+      }
+      DCHECK(sequence_checker_.CalledOnValidSequence())
+          << "WeakPtrs must be checked on the same sequenced thread.";
+#endif
+      return is_valid_;
+    }
 
    private:
     friend class base::RefCountedThreadSafe<Flag>;
 
+    enum NullFlagTag { kNullFlagTag };
+    Flag(NullFlagTag);
+
     ~Flag();
 
+    uintptr_t is_valid_;
+#if DCHECK_IS_ON()
+    // Even if SequenceChecker is an empty class in non-dcheck builds, it still
+    // takes up space in the class.
     SequenceChecker sequence_checker_;
-    bool is_valid_;
+#endif
   };
 
   WeakReference();
@@ -117,9 +160,11 @@ class BASE_EXPORT WeakReference {
   WeakReference& operator=(WeakReference&& other) = default;
   WeakReference& operator=(const WeakReference& other) = default;
 
-  bool is_valid() const;
+  uintptr_t is_valid() const { return flag_->IsValid(); }
 
  private:
+  // Note: To avoid null-checks, flag_ always points to either Flag::NullFlag()
+  // or some other object.
   scoped_refptr<const Flag> flag_;
 };
 
@@ -131,7 +176,7 @@ class BASE_EXPORT WeakReferenceOwner {
   WeakReference GetRef() const;
 
   bool HasRefs() const {
-    return flag_.get() && !flag_->HasOneRef();
+    return flag_ != WeakReference::Flag::NullFlag() && !flag_->HasOneRef();
   }
 
   void Invalidate();
@@ -222,7 +267,13 @@ class WeakPtr : public internal::WeakPtrBase {
   WeakPtr(WeakPtr<U>&& other)
       : WeakPtrBase(std::move(other)), ptr_(other.ptr_) {}
 
-  T* get() const { return ref_.is_valid() ? ptr_ : nullptr; }
+  T* get() const {
+    // Intentionally bitwise and; see command on Flag::IsValid(). This provides
+    // a fast way of conditionally retrieving the pointer, and conveniently sets
+    // EFLAGS for any null-check performed by the caller.
+    return reinterpret_cast<T*>(ref_.is_valid() &
+                                reinterpret_cast<uintptr_t>(ptr_));
+  }
 
   T& operator*() const {
     DCHECK(get() != nullptr);
