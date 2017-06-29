2e1a8ea72eb4303dff657e2a2ef5c2a046e33820
Jeremy Roman <jbroman@chromium.org>
2017-06-29 21:24:02 +0000

    Build WTF::ThreadSafeRefCounted using base::AtomicRefCount.
    It is now built on std::atomic, using acquire/release semantics.
    This reduces the code needed in WTF, and improves the generated
    code on ARM.
    
    Bug: 736037
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I3e3d1725e81d4f809df2b1cdf2c3def7847daac1
    Reviewed-on: https://chromium-review.googlesource.com/521822
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483490}
    Former-commit-id: 7cab7ac9f9bd90e440812fe5f4266ebd4b73b3b9

--- third_party/WebKit/Source/core/inspector/InspectorTraceEvents.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorTraceEvents.cpp
@@ -38,6 +38,7 @@
 #include "platform/loader/fetch/ResourceRequest.h"
 #include "platform/loader/fetch/ResourceResponse.h"
 #include "platform/weborigin/KURL.h"
+#include "platform/wtf/DynamicAnnotations.h"
 #include "platform/wtf/Vector.h"
 #include "platform/wtf/text/TextPosition.h"
 #include "v8/include/v8-profiler.h"
