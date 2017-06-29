734be37d55e275a4ebd002b97e88f7e3eb227863
mmenke <mmenke@chromium.org>
2017-06-28 17:36:29 -0700

    NetworkService: Destroy NetworkContexts on NetworkService teardown.
    Also fix NetworkContexts potentially not being destroyed on connection
    error / consumer teardown, due to calling str::move twice on the same
    RequestPtr.
    
    This is a reland of https://codereview.chromium.org/2962693002,
    which was reverted in https://codereview.chromium.org/2960843005/ due
    to a conflict with https://codereview.chromium.org/2951813002/. The
    solution was to add a base::test::ScopedTaskEnvironment to the tests.
    
    BUG=736472
    
    Review-Url: https://codereview.chromium.org/2963823002
    Cr-Commit-Position: refs/heads/master@{#483225}
    
    Former-commit-id: 502a6133a5974683b23edab936d6b480dcd7684e

---
+++ content/network/network_service_unittest.cc
@@ -0,0 +1,71 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+
+#include "base/run_loop.h"
+#include "base/test/scoped_task_environment.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "content/common/network_service.mojom.h"
+#include "content/network/network_context.h"
+#include "content/network/network_service.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace content {
+
+namespace {
+
+class NetworkServiceTest : public testing::Test {
+ public:
+  NetworkServiceTest()
+      : scoped_task_environment_(
+            base::test::ScopedTaskEnvironment::MainThreadType::IO),
+        service_(NetworkService::CreateForTesting()) {}
+  ~NetworkServiceTest() override {}
+
+  NetworkService* service() const { return service_.get(); }
+
+  void DestroyService() { service_.reset(); }
+
+ private:
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
+  std::unique_ptr<NetworkService> service_;
+};
+
+// Test shutdown in the case a NetworkContext is destroyed before the
+// NetworkService.
+TEST_F(NetworkServiceTest, CreateAndDestroyContext) {
+  mojom::NetworkContextPtr network_context;
+  mojom::NetworkContextParamsPtr context_params =
+      mojom::NetworkContextParams::New();
+
+  service()->CreateNetworkContext(mojo::MakeRequest(&network_context),
+                                  std::move(context_params));
+  network_context.reset();
+  // Make sure the NetworkContext is destroyed.
+  base::RunLoop().RunUntilIdle();
+}
+
+// Test shutdown in the case there is still a live NetworkContext when the
+// NetworkService is destroyed. The service should destroy the NetworkContext
+// itself.
+TEST_F(NetworkServiceTest, DestroyingServiceDestroysContext) {
+  mojom::NetworkContextPtr network_context;
+  mojom::NetworkContextParamsPtr context_params =
+      mojom::NetworkContextParams::New();
+
+  service()->CreateNetworkContext(mojo::MakeRequest(&network_context),
+                                  std::move(context_params));
+  base::RunLoop run_loop;
+  network_context.set_connection_error_handler(run_loop.QuitClosure());
+  DestroyService();
+
+  // Destroying the service should destroy the context, causing a connection
+  // error.
+  run_loop.Run();
+}
+
+}  // namespace
+
+}  // namespace content
