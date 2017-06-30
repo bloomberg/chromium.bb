4cf6b908a6e4913f9cf91a64d70d0a73f7f2ba01
Elly Fong-Jones <ellyjones@chromium.org>
2017-06-30 21:58:58 +0000

    [macviews] wire the import lock dialog
    The import lock dialog is shown during profile import if firefox is running.
    
    BUG=728155
    
    Change-Id: I4937ab02a7660d7e02482a42d113b7b52df33cb3
    Reviewed-on: https://chromium-review.googlesource.com/556112
    Commit-Queue: Elly Fong-Jones <ellyjones@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483837}
    Former-commit-id: d4c0b8ab59d72e7414652ddac075c88d033d66d2

--- chrome/browser/ui/cocoa/importer/import_lock_dialog_cocoa.mm
+++ chrome/browser/ui/cocoa/importer/import_lock_dialog_cocoa.mm
@@ -10,9 +10,11 @@
 #include "base/metrics/user_metrics.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "chrome/browser/importer/importer_lock_dialog.h"
+#include "chrome/browser/ui/cocoa/browser_dialogs_views_mac.h"
 #include "chrome/grit/chromium_strings.h"
 #include "chrome/grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util_mac.h"
+#include "ui/base/material_design/material_design_controller.h"
 
 using base::UserMetricsAction;
 
@@ -20,6 +22,9 @@ namespace importer {
 
 void ShowImportLockDialog(gfx::NativeWindow parent,
                           const base::Callback<void(bool)>& callback) {
+  if (ui::MaterialDesignController::IsSecondaryUiMaterial())
+    return chrome::ShowImportLockDialogViews(parent, callback);
+
   base::scoped_nsobject<NSAlert> lock_alert([[NSAlert alloc] init]);
   [lock_alert addButtonWithTitle:l10n_util::GetNSStringWithFixup(
       IDS_IMPORTER_LOCK_OK)];
