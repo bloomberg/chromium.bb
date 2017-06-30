496b7ac9fdceab59705d9b1fd31251e42359a8e8
Sorin Jianu <sorin@chromium.org>
2017-06-30 16:06:37 +0000

    Define a UMA stat to count the number of component updater BITS jobs.
    We'd like to have an idea how effective the component updater is at
    cleaning up orphaned jobs in the BITS queue. The component updater has
    a mechanism to clean up jobs that are older than 7 days. There could be
    several reasons why jobs are left behind, such as browser restarts,
    crashes, system restarts, or simply code bugs.
    
    TBR=waffles
    
    Bug: 737730
    Change-Id: I7eee79a1f9126846e0620b670c91c72bda774786
    Reviewed-on: https://chromium-review.googlesource.com/556479
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483718}
    Former-commit-id: 4782af57f14cd7acf259b381d4b47f1d6974d56c

--- components/update_client/background_downloader_win.cc
+++ components/update_client/background_downloader_win.cc
@@ -22,6 +22,7 @@
 #include "base/files/file_util.h"
 #include "base/location.h"
 #include "base/macros.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/sequenced_task_runner.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/win/scoped_co_mem.h"
@@ -423,6 +424,20 @@ HRESULT CleanupStaleJobs(
   return S_OK;
 }
 
+// Returns the number of jobs in the BITS queue which were created by this
+// downloader.
+HRESULT GetBackgroundDownloaderJobCount(
+    const ScopedComPtr<IBackgroundCopyManager>& bits_manager,
+    size_t* num_jobs) {
+  std::vector<ScopedComPtr<IBackgroundCopyJob>> jobs;
+  const HRESULT hr = FindBitsJobIf([](IBackgroundCopyJob*) { return true; },
+                                   bits_manager.Get(), &jobs);
+  if (FAILED(hr))
+    return hr;
+  *num_jobs = jobs.size();
+  return S_OK;
+}
+
 }  // namespace
 
 BackgroundDownloader::BackgroundDownloader(
@@ -714,6 +729,10 @@ bool BackgroundDownloader::OnStateTransferring() {
 HRESULT BackgroundDownloader::QueueBitsJob(const GURL& url,
                                            IBackgroundCopyJob** job) {
   DCHECK(task_runner()->RunsTasksInCurrentSequence());
+
+  size_t num_jobs = 0;
+  GetBackgroundDownloaderJobCount(bits_manager_, &num_jobs);
+  UMA_HISTOGRAM_COUNTS_100("UpdateClient.BackgroundDownloaderJobs", num_jobs);
 
   ScopedComPtr<IBackgroundCopyJob> p;
   HRESULT hr = CreateOrOpenJob(url, p.GetAddressOf());
