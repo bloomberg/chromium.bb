9dde60734ee6eaf097ab9f86741cc402d8b71a5d
timloh <timloh@chromium.org>
2017-06-29 20:08:59 -0700

    Revert of Added a system caret used for accessibility on Windows. (patchset #28 id:540001 of https://codereview.chromium.org/2852763002/ )
    Reason for revert:
    The added test AXSystemCaretWinTest.TestMovingWindow is failing on the bots, e.g. https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%281%29/builds/61190
    
    Original issue's description:
    > Added a system caret used for accessibility on Windows.
    >
    > Screen magnifiers need to know where the caret is located so that they can show a magnified caret allowing the user to more easily track it.
    > Existing screen magnifiers, such as the built-in Windows Magnifier, require a system caret to be present in order to track it. However, Chrome doesn't use the system caret.
    > This is an object that implements the same accessibility interface that would have been returned by the system if the system caret had been used.
    > R=dmazzoni@chromium.org, shuchen@chromium.org, ellyjones@chromium.org, ananta@chromium.org, sky@chromium.org, sadrul@chromium.org
    >
    > Review-Url: https://codereview.chromium.org/2852763002
    > Cr-Commit-Position: refs/heads/master@{#483558}
    > Committed: https://chromium.googlesource.com/chromium/src/+/cfcdc3083345c9350b8b2de4da5ded4370d2ae1f [formerly 3fc0df1e01ab7e17318ce50724890f939bc00fa4]
    
    TBR=ananta@chromium.org,dmazzoni@chromium.org,ellyjones@chromium.org,sadrul@chromium.org,shuchen@chromium.org,sky@chromium.org,nektar@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Review-Url: https://codereview.chromium.org/2962273002
    Cr-Commit-Position: refs/heads/master@{#483611}
    
    Former-commit-id: 8620053cfda9ac0f2dd0c2e0231231ccb5033fe9

--- ui/accessibility/platform/ax_fake_caret_win.cc
+++ ui/accessibility/platform/ax_fake_caret_win.cc
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "ui/accessibility/platform/ax_system_caret_win.h"
+#include "ui/accessibility/platform/ax_fake_caret_win.h"
 
 #include <windows.h>
 
@@ -15,24 +15,22 @@
 
 namespace ui {
 
-AXSystemCaretWin::AXSystemCaretWin(gfx::AcceleratedWidget event_target)
+AXFakeCaretWin::AXFakeCaretWin(gfx::AcceleratedWidget event_target)
     : event_target_(event_target) {
   caret_ = static_cast<AXPlatformNodeWin*>(AXPlatformNodeWin::Create(this));
   data_.id = GetNextAXPlatformNodeUniqueId();
   data_.role = AX_ROLE_CARET;
-  // |get_accState| should return 0 which means that the caret is visible.
   data_.state = 0;
-  // According to MSDN, "Edit" should be the name of the caret object.
-  data_.SetName(L"Edit");
+  data_.SetName(L"caret");
   data_.offset_container_id = -1;
 }
 
-AXSystemCaretWin::~AXSystemCaretWin() {
+AXFakeCaretWin::~AXFakeCaretWin() {
   caret_->Destroy();
   caret_ = nullptr;
 }
 
-base::win::ScopedComPtr<IAccessible> AXSystemCaretWin::GetCaret() const {
+base::win::ScopedComPtr<IAccessible> AXFakeCaretWin::GetCaret() const {
   base::win::ScopedComPtr<IAccessible> caret_accessible;
   HRESULT hr = caret_->QueryInterface(
       IID_IAccessible,
@@ -41,7 +39,7 @@ base::win::ScopedComPtr<IAccessible> AXSystemCaretWin::GetCaret() const {
   return caret_accessible;
 }
 
-void AXSystemCaretWin::MoveCaretTo(const gfx::Rect& bounds) {
+void AXFakeCaretWin::MoveCaretTo(const gfx::Rect& bounds) {
   if (bounds.IsEmpty())
     return;
   data_.location = gfx::RectF(bounds);
@@ -51,20 +49,20 @@ void AXSystemCaretWin::MoveCaretTo(const gfx::Rect& bounds) {
   }
 }
 
-const AXNodeData& AXSystemCaretWin::GetData() const {
+const AXNodeData& AXFakeCaretWin::GetData() const {
   return data_;
 }
 
-const ui::AXTreeData& AXSystemCaretWin::GetTreeData() const {
+const ui::AXTreeData& AXFakeCaretWin::GetTreeData() const {
   CR_DEFINE_STATIC_LOCAL(ui::AXTreeData, empty_data, ());
   return empty_data;
 }
 
-gfx::NativeWindow AXSystemCaretWin::GetTopLevelWidget() {
+gfx::NativeWindow AXFakeCaretWin::GetTopLevelWidget() {
   return nullptr;
 }
 
-gfx::NativeViewAccessible AXSystemCaretWin::GetParent() {
+gfx::NativeViewAccessible AXFakeCaretWin::GetParent() {
   if (!event_target_)
     return nullptr;
 
@@ -77,39 +75,37 @@ gfx::NativeViewAccessible AXSystemCaretWin::GetParent() {
   return nullptr;
 }
 
-int AXSystemCaretWin::GetChildCount() {
+int AXFakeCaretWin::GetChildCount() {
   return 0;
 }
 
-gfx::NativeViewAccessible AXSystemCaretWin::ChildAtIndex(int index) {
+gfx::NativeViewAccessible AXFakeCaretWin::ChildAtIndex(int index) {
   return nullptr;
 }
 
-gfx::Rect AXSystemCaretWin::GetScreenBoundsRect() const {
+gfx::Rect AXFakeCaretWin::GetScreenBoundsRect() const {
   gfx::Rect bounds = ToEnclosingRect(data_.location);
   return bounds;
 }
 
-gfx::NativeViewAccessible AXSystemCaretWin::HitTestSync(int x, int y) {
+gfx::NativeViewAccessible AXFakeCaretWin::HitTestSync(int x, int y) {
   return nullptr;
 }
 
-gfx::NativeViewAccessible AXSystemCaretWin::GetFocus() {
+gfx::NativeViewAccessible AXFakeCaretWin::GetFocus() {
   return nullptr;
 }
 
-gfx::AcceleratedWidget
-AXSystemCaretWin::GetTargetForNativeAccessibilityEvent() {
+gfx::AcceleratedWidget AXFakeCaretWin::GetTargetForNativeAccessibilityEvent() {
   return event_target_;
 }
 
-bool AXSystemCaretWin::AccessibilityPerformAction(
-    const ui::AXActionData& data) {
+bool AXFakeCaretWin::AccessibilityPerformAction(const ui::AXActionData& data) {
   return false;
 }
 
-bool AXSystemCaretWin::ShouldIgnoreHoveredStateForTesting() {
-  return false;
+bool AXFakeCaretWin::ShouldIgnoreHoveredStateForTesting() {
+  return true;
 }
 
 }  // namespace ui
