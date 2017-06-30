bloomberg/chromium.bb

commit c98618a28e31e4324b15c6b45926a98b27a29969

Fix about:flags information not showing up in chrome://net-export/ logs
Pass the command-line and channel as dependencies to StartNetLog() rather than saving them in the ChromeNetLog constructor.

The issue is that the CommandLine singleton may be mutated after ChromeNetLog has been initialized (it gets re-written to include synthesized switches for about:flags).

BUG=711432

Review-Url: https://codereview.chromium.org/2965623002
Cr-Commit-Position: refs/heads/master@{#483754}

Former-commit-id: 30355da69277622a7f6bb2129c0435b1e1223f49

chrome/browser/ui/webui/net_export_ui.cc
@@ -24,6 +24,7 @@
 #include "chrome/browser/platform_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/chrome_select_file_policy.h"
+#include "chrome/common/channel_info.h"
 #include "chrome/common/url_constants.h"
 #include "components/grit/components_resources.h"
 #include "components/net_log/chrome_net_log.h"
@@ -110,6 +111,8 @@ class NetExportMessageHandler
 
   // Send NetLog data via email.
   static void SendEmail(const base::FilePath& file_to_send);
+
+  void StartNetLog(const base::FilePath& path);
 
   // Reveal |path| in the shell on desktop platforms.
   void ShowFileInShell(const base::FilePath& path);
@@ -222,8 +225,7 @@ void NetExportMessageHandler::OnStartNetLog(const base::ListValue* list) {
       net_log::NetLogFileWriter::CaptureModeFromString(capture_mode_string);
 
   if (UsingMobileUI()) {
-    file_writer_->StartNetLog(base::FilePath(), capture_mode_,
-                              GetURLRequestContexts());
+    StartNetLog(base::FilePath());
   } else {
     base::FilePath initial_dir = last_save_dir.Pointer()->empty() ?
         DownloadPrefs::FromBrowserContext(
@@ -280,7 +282,7 @@ void NetExportMessageHandler::FileSelected(const base::FilePath& path,
   DCHECK(select_file_dialog_);
   *last_save_dir.Pointer() = path.DirName();
 
-  file_writer_->StartNetLog(path, capture_mode_, GetURLRequestContexts());
+  StartNetLog(path);
 
   // IMPORTANT: resetting the dialog may lead to the deletion of |path|, so keep
   // this line last.
@@ -313,6 +315,15 @@ void NetExportMessageHandler::SendEmail(const base::FilePath& file_to_send) {
       base::UTF8ToUTF16(body), base::UTF8ToUTF16(title),
       base::UTF8ToUTF16(file_to_attach));
 #endif
+}
+
+void NetExportMessageHandler::StartNetLog(const base::FilePath& path) {
+  DCHECK_CURRENTLY_ON(BrowserThread::UI);
+
+  file_writer_->StartNetLog(
+      base::FilePath(), capture_mode_,
+      base::CommandLine::ForCurrentProcess()->GetCommandLineString(),
+      chrome::GetChannelString(), GetURLRequestContexts());
 }
 
 void NetExportMessageHandler::ShowFileInShell(const base::FilePath& path) {

ios/chrome/browser/ui/webui/net_export/net_export_ui.cc
@@ -23,6 +23,7 @@
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
 #include "ios/chrome/browser/ui/show_mail_composer_util.h"
+#include "ios/chrome/common/channel_info.h"
 #include "ios/chrome/grit/ios_strings.h"
 #include "ios/web/public/web_thread.h"
 #include "ios/web/public/web_ui_ios_data_source.h"
@@ -141,6 +142,8 @@ void NetExportMessageHandler::OnStartNetLog(const base::ListValue* list) {
       net_log::NetLogFileWriter::CaptureModeFromString(capture_mode_string);
   file_writer_->StartNetLog(
       base::FilePath(), capture_mode,
+      base::CommandLine::ForCurrentProcess()->GetCommandLineString(),
+      GetChannelString(),
       {GetApplicationContext()->GetSystemURLRequestContext()});
 }
 

components/net_log/chrome_net_log.cc
@@ -29,8 +29,7 @@ ChromeNetLog::ChromeNetLog(
     net::NetLogCaptureMode log_file_mode,
     const base::CommandLine::StringType& command_line_string,
     const std::string& channel_string)
-    : net_log_file_writer_(
-          new NetLogFileWriter(this, command_line_string, channel_string)) {
+    : net_log_file_writer_(new NetLogFileWriter(this)) {
   if (!log_file.empty()) {
     // Much like logging.h, bypass threading restrictions by using fopen
     // directly.  Have to write on a thread that's shutdown to handle events on
@@ -71,6 +70,12 @@ ChromeNetLog::~ChromeNetLog() {
     write_to_file_observer_->StopObserving(nullptr);
   if (trace_net_log_observer_)
     trace_net_log_observer_->StopWatchForTraceStart();
+}
+
+NetLogFileWriter* ChromeNetLog::net_log_file_writer() {
+  if (!net_log_file_writer_)
+    net_log_file_writer_ = base::WrapUnique(new NetLogFileWriter(this));
+  return net_log_file_writer_.get();
 }
 
 // static

components/net_log/chrome_net_log.h
@@ -30,6 +30,12 @@ class NetLogFileWriter;
 // as its observers.
 class ChromeNetLog : public net::NetLog {
  public:
+  // The parameters to the constructor are only used for command-line based
+  // NetLog writing (which starts immediately after construction).
+  //
+  // TODO(eroman): This would be clearer as a separate method to configure and
+  //               start this logging mode.
+  //
   // The log is saved to |log_file|.
   // |log_file_mode| is the mode used to log in |log_file|.
   // If |log_file| is empty, only a temporary log is created, and
@@ -40,7 +46,9 @@ class ChromeNetLog : public net::NetLog {
                const std::string& channel_string);
   ~ChromeNetLog() override;
 
-  NetLogFileWriter* net_log_file_writer() { return net_log_file_writer_.get(); }
+  // TODO(eroman): Rename this to something clearer. Perhaps
+  //               |net_export_file_writer()|.
+  NetLogFileWriter* net_log_file_writer();
 
   // Returns a Value containing constants needed to load a log file.
   // Safe to call on any thread.

components/net_log/net_log_file_writer.cc
@@ -109,17 +109,12 @@ base::FilePath GetPathWithAllPermissions(const base::FilePath& path) {
 
 }  // namespace
 
-NetLogFileWriter::NetLogFileWriter(
-    ChromeNetLog* chrome_net_log,
-    const base::CommandLine::StringType& command_line_string,
-    const std::string& channel_string)
+NetLogFileWriter::NetLogFileWriter(ChromeNetLog* chrome_net_log)
     : state_(STATE_UNINITIALIZED),
       log_exists_(false),
       log_capture_mode_known_(false),
       log_capture_mode_(net::NetLogCaptureMode::Default()),
       chrome_net_log_(chrome_net_log),
-      command_line_string_(command_line_string),
-      channel_string_(channel_string),
       default_log_base_dir_getter_(base::Bind(&base::GetTempDir)),
       weak_ptr_factory_(this) {}
 
@@ -169,6 +164,8 @@ void NetLogFileWriter::Initialize(
 void NetLogFileWriter::StartNetLog(
     const base::FilePath& log_path,
     net::NetLogCaptureMode capture_mode,
+    const base::CommandLine::StringType& command_line_string,
+    const std::string& channel_string,
     const URLRequestContextGetterList& context_getters) {
   DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK(file_task_runner_);
@@ -186,7 +183,7 @@ void NetLogFileWriter::StartNetLog(
   NotifyStateObserversAsync();
 
   std::unique_ptr<base::Value> constants(
-      ChromeNetLog::GetConstants(command_line_string_, channel_string_));
+      ChromeNetLog::GetConstants(command_line_string, channel_string));
   // Instantiate a FileNetLogObserver in unbounded mode.
   file_net_log_observer_ = net::FileNetLogObserver::CreateUnbounded(
       file_task_runner_, log_path_, std::move(constants));

components/net_log/net_log_file_writer.h
@@ -105,6 +105,8 @@ class NetLogFileWriter {
   // thread.
   void StartNetLog(const base::FilePath& log_path,
                    net::NetLogCaptureMode capture_mode,
+                   const base::CommandLine::StringType& command_line_string,
+                   const std::string& channel_string,
                    const URLRequestContextGetterList& context_getters);
 
   // Stops collecting NetLog data into the file. It is a no-op if
@@ -149,9 +151,7 @@ class NetLogFileWriter {
  protected:
   // Constructs a NetLogFileWriter. Only one instance is created in browser
   // process.
-  NetLogFileWriter(ChromeNetLog* chrome_net_log,
-                   const base::CommandLine::StringType& command_line_string,
-                   const std::string& channel_string);
+  explicit NetLogFileWriter(ChromeNetLog* chrome_net_log);
 
  private:
   friend class ChromeNetLog;
@@ -227,9 +227,6 @@ class NetLogFileWriter {
 
   // List of StateObservers to notify on state changes.
   base::ObserverList<StateObserver, true> state_observer_list_;
-
-  const base::CommandLine::StringType command_line_string_;
-  const std::string channel_string_;
 
   // Used by unit tests to override the default log base directory retrieved
   // during initialization. This getter is initialized to base::GetTempDir().

components/net_log/net_log_file_writer_unittest.cc
@@ -257,12 +257,9 @@ class NetLogFileWriterTest : public ::testing::Test {
   NetLogFileWriterTest()
       : net_log_(base::FilePath(),
                  net::NetLogCaptureMode::Default(),
-                 base::CommandLine::ForCurrentProcess()->GetCommandLineString(),
+                 base::CommandLine::StringType(),
                  kChannelString),
-        net_log_file_writer_(
-            &net_log_,
-            base::CommandLine::ForCurrentProcess()->GetCommandLineString(),
-            kChannelString),
+        net_log_file_writer_(&net_log_),
         file_thread_("NetLogFileWriter file thread"),
         net_thread_("NetLogFileWriter net thread") {}
 
@@ -337,7 +334,8 @@ class NetLogFileWriterTest : public ::testing::Test {
       const std::string& expected_capture_mode_string,
       const URLRequestContextGetterList& context_getters) {
     net_log_file_writer_.StartNetLog(custom_log_path, capture_mode,
-                                     context_getters);
+                                     base::CommandLine::StringType(),
+                                     kChannelString, context_getters);
     std::unique_ptr<base::DictionaryValue> state =
         test_state_observer_.WaitForNewState();
     ::testing::AssertionResult result =
@@ -503,14 +501,17 @@ TEST_F(NetLogFileWriterTest, StartAndStopWithAllCaptureModes) {
     // Calling StartNetLog() again should be a no-op. Try doing StartNetLog()
     // with various capture modes; they should all be ignored and result in no
     // state change.
-    net_log_file_writer_.StartNetLog(base::FilePath(), capture_modes[i],
-                                     URLRequestContextGetterList());
-    net_log_file_writer_.StartNetLog(base::FilePath(),
-                                     capture_modes[(i + 1) % 3],
-                                     URLRequestContextGetterList());
-    net_log_file_writer_.StartNetLog(base::FilePath(),
-                                     capture_modes[(i + 2) % 3],
-                                     URLRequestContextGetterList());
+    net_log_file_writer_.StartNetLog(
+        base::FilePath(), capture_modes[i], base::CommandLine::StringType(),
+        kChannelString, URLRequestContextGetterList());
+    net_log_file_writer_.StartNetLog(
+        base::FilePath(), capture_modes[(i + 1) % 3],
+        base::CommandLine::StringType(), kChannelString,
+        URLRequestContextGetterList());
+    net_log_file_writer_.StartNetLog(
+        base::FilePath(), capture_modes[(i + 2) % 3],
+        base::CommandLine::StringType(), kChannelString,
+        URLRequestContextGetterList());
 
     // StopNetLog(), should result in state change. The capture mode should
     // match that of the first StartNetLog() call (called by
@@ -748,9 +749,10 @@ TEST_F(NetLogFileWriterTest, ReceiveStartWhileInitializing) {
   // process from completing, so this ensures that StartNetLog() is received
   // before |net_log_file_writer_| finishes initialization, which means this
   // should be a no-op.
-  net_log_file_writer_.StartNetLog(base::FilePath(),
-                                   net::NetLogCaptureMode::Default(),
-                                   URLRequestContextGetterList());
+  net_log_file_writer_.StartNetLog(
+      base::FilePath(), net::NetLogCaptureMode::Default(),
+      base::CommandLine::StringType(), kChannelString,
+      URLRequestContextGetterList());
 
   // Now run the main message loop. Make sure StartNetLog() was ignored by
   // checking that the next two states are "initializing" followed by
@@ -779,9 +781,10 @@ TEST_F(NetLogFileWriterTest, ReceiveStartWhileStoppingLog) {
   // from completing, so this ensures StartNetLog() is received before
   // |net_log_file_writer_| finishes stopping, which means this should be a
   // no-op.
-  net_log_file_writer_.StartNetLog(base::FilePath(),
-                                   net::NetLogCaptureMode::Default(),
-                                   URLRequestContextGetterList());
+  net_log_file_writer_.StartNetLog(
+      base::FilePath(), net::NetLogCaptureMode::Default(),
+      base::CommandLine::StringType(), kChannelString,
+      URLRequestContextGetterList());
 
   // Now run the main message loop. Make sure the last StartNetLog() was
   // ignored by checking that the next two states are "stopping-log" followed by

ios/chrome/browser/ui/webui/net_export/BUILD.gn
@@ -15,6 +15,7 @@ source_set("net_export") {
     "//ios/chrome/browser",
     "//ios/chrome/browser/browser_state",
     "//ios/chrome/browser/ui",
+    "//ios/chrome/common:common",
     "//ios/web",
     "//net:net",
   ]

commit f174973e152a4097e3632632c33869a426aacf35

Fix net-export file path that is used on desktop.
This bug was introduced by c98618a28e31e4324b15c6b45926a98b27a29969 [formerly 30355da69277622a7f6bb2129c0435b1e1223f49].

BUG=711432

Review-Url: https://codereview.chromium.org/2968723003
Cr-Commit-Position: refs/heads/master@{#483817}

Former-commit-id: 144002e61ba9773f3375290854e4dcaee320a218

chrome/browser/ui/webui/net_export_ui.cc
@@ -321,7 +321,7 @@ void NetExportMessageHandler::StartNetLog(const base::FilePath& path) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   file_writer_->StartNetLog(
-      base::FilePath(), capture_mode_,
+      path, capture_mode_,
       base::CommandLine::ForCurrentProcess()->GetCommandLineString(),
       chrome::GetChannelString(), GetURLRequestContexts());
 }