ee11d40645c4e8c72116d23052bb55e7a8079292
Florin Malita <fmalita@chromium.org>
2017-06-30 17:54:17 +0000

    Use SkRasterPipeline for 2-point radial gradients
    (remove SK_SUPPORT_LEGACY_2PTCONICAL_GRADIENT)
    
    Change-Id: I6d4deac487066132b4f09e34951cd871a6e0b1b3
    Reviewed-on: https://chromium-review.googlesource.com/556362
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Mike Reed <reed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483753}
    Former-commit-id: 62a057db405d02f0ae5f775a1b9a081ba7202e1e

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -216,10 +216,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_TILED_BITMAPS
 #endif
 
-#ifndef SK_SUPPORT_LEGACY_2PTCONICAL_GRADIENT
-#define SK_SUPPORT_LEGACY_2PTCONICAL_GRADIENT
-#endif
-
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
