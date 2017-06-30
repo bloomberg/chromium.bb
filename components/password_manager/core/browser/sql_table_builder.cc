8c7dc5ca6c2d46d96370a6f40e999af35fd131e4
jdoerrie <jdoerrie@chromium.org>
2017-06-30 14:28:45 +0000

    Add Index Methods to SQL Table Builder
    This change extends the SQL Table Builder API by adding methods for
    index manipulation. The Index API mirrors the already existing Column
    API.
    
    Bug: 628988
    Change-Id: Ib17ace2ecc60af4543ae101420aa12578a3854f9
    Reviewed-on: https://chromium-review.googlesource.com/555510
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Commit-Queue: Vaclav Brozek <vabr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483697}
    Former-commit-id: bb5308235b7770af9a2ee8502099e39bd45a3282

--- components/password_manager/core/browser/sql_table_builder.cc
+++ components/password_manager/core/browser/sql_table_builder.cc
@@ -5,10 +5,12 @@
 #include "components/password_manager/core/browser/sql_table_builder.h"
 
 #include <algorithm>
+#include <set>
 #include <utility>
-#include <vector>
 
 #include "base/numerics/safe_conversions.h"
+#include "base/stl_util.h"
+#include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "sql/connection.h"
 #include "sql/transaction.h"
@@ -26,6 +28,9 @@ void Append(const std::string& name, std::string* list_of_names) {
 }
 
 }  // namespace
+
+// static
+unsigned constexpr SQLTableBuilder::kInvalidVersion;
 
 struct SQLTableBuilder::Column {
   std::string name;
@@ -45,34 +50,49 @@ struct SQLTableBuilder::Column {
   bool gets_previous_data;
 };
 
+struct SQLTableBuilder::Index {
+  // The name of this index.
+  std::string name;
+  // The names of columns this index is built from.
+  std::vector<std::string> columns;
+  // The first version this index is part of.
+  unsigned min_version;
+  // The last version this index is part of. The value of kInvalidVersion
+  // means that it is part of all versions since |min_version|.
+  unsigned max_version;
+};
+
 SQLTableBuilder::SQLTableBuilder(const std::string& table_name)
     : table_name_(table_name) {}
 
 SQLTableBuilder::~SQLTableBuilder() = default;
 
-void SQLTableBuilder::AddColumn(const std::string& name,
-                                const std::string& type) {
-  DCHECK(FindLastByName(name) == columns_.rend());
-  Column column = {name, type, false, sealed_version_ + 1, kInvalidVersion,
-                   false};
-  columns_.push_back(std::move(column));
+void SQLTableBuilder::AddColumn(std::string name, std::string type) {
+  DCHECK(FindLastColumnByName(name) == columns_.rend());
+  columns_.push_back({std::move(name), std::move(type), false,
+                      sealed_version_ + 1, kInvalidVersion, false});
 }
 
-void SQLTableBuilder::AddColumnToUniqueKey(const std::string& name,
-                                           const std::string& type) {
-  AddColumn(name, type);
+void SQLTableBuilder::AddColumnToUniqueKey(std::string name, std::string type) {
+  AddColumn(std::move(name), std::move(type));
   columns_.back().part_of_unique_key = true;
 }
 
 void SQLTableBuilder::RenameColumn(const std::string& old_name,
                                    const std::string& new_name) {
-  auto old_column = FindLastByName(old_name);
+  auto old_column = FindLastColumnByName(old_name);
   DCHECK(old_column != columns_.rend());
-
   if (old_name == new_name)  // The easy case.
     return;
 
-  DCHECK_NE("signon_realm", old_name);
+  DCHECK(FindLastColumnByName(new_name) == columns_.rend());
+  // Check there is no index in the current version that references |old_name|.
+  DCHECK(std::none_of(indices_.begin(), indices_.end(),
+                      [this, &old_name](const Index& index) {
+                        return index.max_version == kInvalidVersion &&
+                               base::ContainsValue(index.columns, old_name);
+                      }));
+
   if (sealed_version_ != kInvalidVersion &&
       old_column->min_version <= sealed_version_) {
     // This column exists in the last sealed version. Therefore it cannot be
@@ -96,9 +116,14 @@ void SQLTableBuilder::RenameColumn(const std::string& old_name,
 
 // Removes column |name|. |name| must have been added in the past.
 void SQLTableBuilder::DropColumn(const std::string& name) {
-  auto column = FindLastByName(name);
+  auto column = FindLastColumnByName(name);
   DCHECK(column != columns_.rend());
-  DCHECK_NE("signon_realm", name);
+  // Check there is no index in the current version that references |old_name|.
+  DCHECK(std::none_of(indices_.begin(), indices_.end(),
+                      [this, &name](const Index& index) {
+                        return index.max_version == kInvalidVersion &&
+                               base::ContainsValue(index.columns, name);
+                      }));
   if (sealed_version_ != kInvalidVersion &&
       column->min_version <= sealed_version_) {
     // This column exists in the last sealed version. Therefore it cannot be
@@ -112,8 +137,73 @@ void SQLTableBuilder::DropColumn(const std::string& name) {
   }
 }
 
+void SQLTableBuilder::AddIndex(std::string name,
+                               std::vector<std::string> columns) {
+  DCHECK(!columns.empty());
+  // Check if all entries of |columns| are unique.
+  DCHECK_EQ(std::set<std::string>(columns.begin(), columns.end()).size(),
+            columns.size());
+  // |name| must not have been added before.
+  DCHECK(FindLastIndexByName(name) == indices_.rend());
+  // Check that all referenced columns are present in the last version by making
+  // sure that the inner predicate applies to all columns names in |columns|.
+  DCHECK(std::all_of(
+      columns.begin(), columns.end(), [this](const std::string& column_name) {
+        // Check if there is any column with the required name which is also
+        // present in the latest version. Note that we don't require the last
+        // version to be sealed.
+        return std::any_of(columns_.begin(), columns_.end(),
+                           [&column_name](const Column& column) {
+                             return column.name == column_name &&
+                                    column.max_version == kInvalidVersion;
+                           });
+      }));
+  indices_.push_back({std::move(name), std::move(columns), sealed_version_ + 1,
+                      kInvalidVersion});
+}
+
+void SQLTableBuilder::RenameIndex(const std::string& old_name,
+                                  const std::string& new_name) {
+  auto old_index = FindLastIndexByName(old_name);
+  // Check that there is an index with the old name.
+  DCHECK(old_index != indices_.rend());
+  if (old_name == new_name)  // The easy case.
+    return;
+
+  // Check that there is no index with the new name.
+  DCHECK(FindLastIndexByName(new_name) == indices_.rend());
+  // Check that there is at least one sealed version.
+  DCHECK_NE(sealed_version_, kInvalidVersion);
+  // Check that the old index has been added before the last version was sealed.
+  DCHECK_LE(old_index->min_version, sealed_version_);
+  // Check that the old index has not been renamed or deleted yet.
+  DCHECK_EQ(old_index->max_version, kInvalidVersion);
+  // This index exists in the last sealed version. Therefore it cannot be
+  // just replaced, it needs to be kept for generating the migration code.
+  old_index->max_version = sealed_version_;
+  // Add the new index.
+  indices_.push_back(
+      {new_name, old_index->columns, sealed_version_ + 1, kInvalidVersion});
+}
+
+void SQLTableBuilder::DropIndex(const std::string& name) {
+  auto index = FindLastIndexByName(name);
+  // Check that an index with the name exists.
+  DCHECK(index != indices_.rend());
+  // Check that this index exists in the last sealed version and hasn't been
+  // renamed or deleted yet.
+  // Check that there is at least one sealed version.
+  DCHECK_NE(sealed_version_, kInvalidVersion);
+  // Check that the index has been added before the last version was sealed.
+  DCHECK_LE(index->min_version, sealed_version_);
+  // Check that the index has not been renamed or deleted yet.
+  DCHECK_EQ(index->max_version, kInvalidVersion);
+  // This index exists in the last sealed version. Therefore it cannot be
+  // just deleted, it needs to be kept for generating the migration code.
+  index->max_version = sealed_version_;
+}
+
 unsigned SQLTableBuilder::SealVersion() {
-  DCHECK(FindLastByName("signon_realm") != columns_.rend());
   if (sealed_version_ == kInvalidVersion) {
     DCHECK_EQ(std::string(), unique_constraint_);
     // First sealed version, time to compute the UNIQUE string.
@@ -147,8 +237,18 @@ bool SQLTableBuilder::CreateTable(sql::Connection* db) {
 
   std::string names;  // Names and types of the current columns.
   for (const Column& column : columns_) {
-    if (IsInLastVersion(column))
+    if (IsColumnInLastVersion(column))
       Append(column.name + " " + column.type, &names);
+  }
+
+  std::vector<std::string>
+      create_index_sqls;  // CREATE INDEX statements for the current indices.
+  for (const Index& index : indices_) {
+    if (IsIndexInLastVersion(index)) {
+      create_index_sqls.push_back(base::StringPrintf(
+          "CREATE INDEX %s ON %s (%s)", index.name.c_str(), table_name_.c_str(),
+          base::JoinString(index.columns, ", ").c_str()));
+    }
   }
 
   sql::Transaction transaction(db);
@@ -157,10 +257,10 @@ bool SQLTableBuilder::CreateTable(sql::Connection* db) {
                                         table_name_.c_str(), names.c_str(),
                                         unique_constraint_.c_str())
                          .c_str()) &&
-         db->Execute(
-             base::StringPrintf("CREATE INDEX %s_signon ON %s (signon_realm)",
-                                table_name_.c_str(), table_name_.c_str())
-                 .c_str()) &&
+         std::all_of(create_index_sqls.begin(), create_index_sqls.end(),
+                     [&db](const std::string& sql) {
+                       return db->Execute(sql.c_str());
+                     }) &&
          transaction.Commit();
 }
 
@@ -168,7 +268,7 @@ std::string SQLTableBuilder::ListAllColumnNames() const {
   DCHECK(IsVersionLastAndSealed(sealed_version_));
   std::string result;
   for (const Column& column : columns_) {
-    if (IsInLastVersion(column))
+    if (IsColumnInLastVersion(column))
       Append(column.name, &result);
   }
   return result;
@@ -178,7 +278,7 @@ std::string SQLTableBuilder::ListAllNonuniqueKeyNames() const {
   DCHECK(IsVersionLastAndSealed(sealed_version_));
   std::string result;
   for (const Column& column : columns_) {
-    if (IsInLastVersion(column) && !column.part_of_unique_key)
+    if (IsColumnInLastVersion(column) && !column.part_of_unique_key)
       Append(column.name + "=?", &result);
   }
   return result;
@@ -188,7 +288,7 @@ std::string SQLTableBuilder::ListAllUniqueKeyNames() const {
   DCHECK(IsVersionLastAndSealed(sealed_version_));
   std::string result;
   for (const Column& column : columns_) {
-    if (IsInLastVersion(column) && column.part_of_unique_key) {
+    if (IsColumnInLastVersion(column) && column.part_of_unique_key) {
       if (!result.empty())
         result += " AND ";
       result += column.name + "=?";
@@ -197,11 +297,28 @@ std::string SQLTableBuilder::ListAllUniqueKeyNames() const {
   return result;
 }
 
+std::string SQLTableBuilder::ListAllIndexNames() const {
+  DCHECK(IsVersionLastAndSealed(sealed_version_));
+  std::string result;
+  for (const Index& index : indices_) {
+    if (IsIndexInLastVersion(index))
+      Append(index.name, &result);
+  }
+  return result;
+}
+
 size_t SQLTableBuilder::NumberOfColumns() const {
   DCHECK(IsVersionLastAndSealed(sealed_version_));
   return base::checked_cast<size_t>(std::count_if(
       columns_.begin(), columns_.end(),
-      [this](const Column& column) { return (IsInLastVersion(column)); }));
+      [this](const Column& column) { return IsColumnInLastVersion(column); }));
+}
+
+size_t SQLTableBuilder::NumberOfIndices() const {
+  DCHECK(IsVersionLastAndSealed(sealed_version_));
+  return base::checked_cast<size_t>(std::count_if(
+      indices_.begin(), indices_.end(),
+      [this](const Index& index) { return IsIndexInLastVersion(index); }));
 }
 
 bool SQLTableBuilder::MigrateToNextFrom(unsigned old_version,
@@ -226,8 +343,7 @@ bool SQLTableBuilder::MigrateToNextFrom(unsigned old_version,
       DCHECK(!column->part_of_unique_key);
       // This column was deleted after |old_version|. It can have two reasons:
       needs_temp_table = true;
-      auto next_column = column;
-      ++next_column;
+      auto next_column = std::next(column);
       if (next_column != columns_.end() && next_column->gets_previous_data) {
         // (1) The column is being renamed.
         DCHECK_EQ(column->type, next_column->type);
@@ -281,10 +397,6 @@ bool SQLTableBuilder::MigrateToNextFrom(unsigned old_version,
                                          temp_table_name.c_str(),
                                          table_name_.c_str())
                           .c_str()) &&
-          db->Execute(
-              base::StringPrintf("CREATE INDEX %s_signon ON %s (signon_realm)",
-                                 table_name_.c_str(), table_name_.c_str())
-                  .c_str()) &&
           transaction.Commit())) {
       return false;
     }
@@ -306,14 +418,50 @@ bool SQLTableBuilder::MigrateToNextFrom(unsigned old_version,
     }
   }
 
-  return true;
+  return MigrateIndicesToNextFrom(old_version, db);
 }
 
-std::list<SQLTableBuilder::Column>::reverse_iterator
-SQLTableBuilder::FindLastByName(const std::string& name) {
+bool SQLTableBuilder::MigrateIndicesToNextFrom(unsigned old_version,
+                                               sql::Connection* db) {
+  DCHECK_LT(old_version, sealed_version_);
+  DCHECK(IsVersionLastAndSealed(sealed_version_));
+  sql::Transaction transaction(db);
+  if (!transaction.Begin())
+    return false;
+
+  for (const auto& index : indices_) {
+    std::string sql;
+    if (index.max_version <= old_version) {
+      // Index is not supposed to exist in the new version.
+      sql = base::StringPrintf("DROP INDEX IF EXISTS %s", index.name.c_str());
+    } else if (index.min_version <= old_version + 1) {
+      // Index is supposed to exist in the new version.
+      sql = base::StringPrintf("CREATE INDEX IF NOT EXISTS %s ON %s (%s)",
+                               index.name.c_str(), table_name_.c_str(),
+                               base::JoinString(index.columns, ", ").c_str());
+    } else {
+      continue;
+    }
+
+    if (!db->Execute(sql.c_str()))
+      return false;
+  }
+
+  return transaction.Commit();
+}
+
+std::vector<SQLTableBuilder::Column>::reverse_iterator
+SQLTableBuilder::FindLastColumnByName(const std::string& name) {
   return std::find_if(
       columns_.rbegin(), columns_.rend(),
       [&name](const Column& column) { return name == column.name; });
+}
+
+std::vector<SQLTableBuilder::Index>::reverse_iterator
+SQLTableBuilder::FindLastIndexByName(const std::string& name) {
+  return std::find_if(
+      indices_.rbegin(), indices_.rend(),
+      [&name](const Index& index) { return name == index.name; });
 }
 
 bool SQLTableBuilder::IsVersionLastAndSealed(unsigned version) const {
@@ -321,21 +469,32 @@ bool SQLTableBuilder::IsVersionLastAndSealed(unsigned version) const {
   if (sealed_version_ != version)
     return false;
   // Is the current version the last sealed one? In other words, is there
-  // either a column added past the sealed version (min_version > sealed) or
-  // deleted one version after the sealed (max_version == sealed)?
-  return columns_.end() ==
-         std::find_if(columns_.begin(), columns_.end(),
+  // neither a column or index added past the sealed version (min_version >
+  // sealed) nor deleted one version after the sealed (max_version == sealed)?
+  return std::none_of(columns_.begin(), columns_.end(),
                       [this](const Column& column) {
                         return column.min_version > sealed_version_ ||
                                column.max_version == sealed_version_;
+                      }) &&
+         std::none_of(indices_.begin(), indices_.end(),
+                      [this](const Index& index) {
+                        return index.min_version > sealed_version_ ||
+                               index.max_version == sealed_version_;
                       });
 }
 
-bool SQLTableBuilder::IsInLastVersion(const Column& column) const {
+bool SQLTableBuilder::IsColumnInLastVersion(const Column& column) const {
   DCHECK(IsVersionLastAndSealed(sealed_version_));
   return (column.min_version <= sealed_version_ &&
           (column.max_version == kInvalidVersion ||
            column.max_version >= sealed_version_));
+}
+
+bool SQLTableBuilder::IsIndexInLastVersion(const Index& index) const {
+  DCHECK(IsVersionLastAndSealed(sealed_version_));
+  return (index.min_version <= sealed_version_ &&
+          (index.max_version == kInvalidVersion ||
+           index.max_version >= sealed_version_));
 }
 
 }  // namespace password_manager
--- components/password_manager/core/browser/sql_table_builder_unittest.cc
+++ components/password_manager/core/browser/sql_table_builder_unittest.cc
@@ -68,7 +68,6 @@ TEST_F(SQLTableBuilderTest, SealVersion_0) {
   EXPECT_TRUE(builder()->MigrateFrom(0, db()));
   EXPECT_TRUE(builder()->CreateTable(db()));
   EXPECT_TRUE(db()->DoesTableExist("my_logins_table"));
-  EXPECT_TRUE(db()->DoesIndexExist("my_logins_table_signon"));
   EXPECT_TRUE(db()->DoesColumnExist("my_logins_table", "signon_realm"));
   EXPECT_TRUE(IsColumnOfType("signon_realm", "VARCHAR NOT NULL"));
 }
@@ -79,10 +78,31 @@ TEST_F(SQLTableBuilderTest, AddColumn) {
   EXPECT_TRUE(builder()->MigrateFrom(0, db()));
   EXPECT_TRUE(builder()->CreateTable(db()));
   EXPECT_TRUE(db()->DoesTableExist("my_logins_table"));
-  EXPECT_TRUE(db()->DoesIndexExist("my_logins_table_signon"));
   EXPECT_TRUE(db()->DoesColumnExist("my_logins_table", "signon_realm"));
   EXPECT_TRUE(db()->DoesColumnExist("my_logins_table", "password_value"));
   EXPECT_TRUE(IsColumnOfType("password_value", "BLOB"));
+}
+
+TEST_F(SQLTableBuilderTest, AddIndex) {
+  builder()->AddIndex("my_logins_table_signon", {"signon_realm"});
+  EXPECT_EQ(0u, builder()->SealVersion());
+  EXPECT_TRUE(builder()->MigrateFrom(0, db()));
+  EXPECT_TRUE(builder()->CreateTable(db()));
+  EXPECT_TRUE(db()->DoesTableExist("my_logins_table"));
+  EXPECT_TRUE(db()->DoesIndexExist("my_logins_table_signon"));
+}
+
+TEST_F(SQLTableBuilderTest, AddIndexOnMultipleColumns) {
+  builder()->AddColumn("column_1", "BLOB");
+  builder()->AddColumn("column_2", "BLOB");
+  builder()->AddIndex("my_index", {"column_1", "column_2"});
+  EXPECT_EQ(0u, builder()->SealVersion());
+  EXPECT_TRUE(builder()->MigrateFrom(0, db()));
+  EXPECT_TRUE(builder()->CreateTable(db()));
+  EXPECT_TRUE(db()->DoesTableExist("my_logins_table"));
+  EXPECT_TRUE(db()->DoesColumnExist("my_logins_table", "column_1"));
+  EXPECT_TRUE(db()->DoesColumnExist("my_logins_table", "column_2"));
+  EXPECT_TRUE(db()->DoesIndexExist("my_index"));
 }
 
 TEST_F(SQLTableBuilderTest, RenameColumn_InSameVersion) {
@@ -108,6 +128,22 @@ TEST_F(SQLTableBuilderTest, RenameColumn_InNextVersion) {
   EXPECT_TRUE(IsColumnOfType("password_value", "BLOB"));
 }
 
+// There is no test for renaming an index in the same version, as this is a
+// misuse of the API. Instead of invoking |builder()->AddIndex("foo", ...)| and
+// |builder->RenameIndex("foo", "bar")| callers should simply use
+// |builder->AddIndex("bar", ...)|.
+
+TEST_F(SQLTableBuilderTest, RenameIndex_InNextVersion) {
+  builder()->AddIndex("old_index", {"signon_realm"});
+  EXPECT_EQ(0u, builder()->SealVersion());
+  builder()->RenameIndex("old_index", "new_index");
+  EXPECT_EQ(1u, builder()->SealVersion());
+  EXPECT_TRUE(builder()->CreateTable(db()));
+  EXPECT_TRUE(db()->DoesTableExist("my_logins_table"));
+  EXPECT_FALSE(db()->DoesIndexExist("old_index"));
+  EXPECT_TRUE(db()->DoesIndexExist("new_index"));
+}
+
 TEST_F(SQLTableBuilderTest, RenameColumn_SameNameInSameVersion) {
   builder()->AddColumn("name", "BLOB");
   builder()->RenameColumn("name", "name");
@@ -129,6 +165,16 @@ TEST_F(SQLTableBuilderTest, RenameColumn_SameNameInNextVersion) {
   EXPECT_TRUE(IsColumnOfType("name", "BLOB"));
 }
 
+TEST_F(SQLTableBuilderTest, RenameIndex_SameNameInNextVersion) {
+  builder()->AddIndex("my_index", {"signon_realm"});
+  EXPECT_EQ(0u, builder()->SealVersion());
+  builder()->RenameIndex("my_index", "my_index");
+  EXPECT_EQ(1u, builder()->SealVersion());
+  EXPECT_TRUE(builder()->CreateTable(db()));
+  EXPECT_TRUE(db()->DoesTableExist("my_logins_table"));
+  EXPECT_TRUE(db()->DoesIndexExist("my_index"));
+}
+
 TEST_F(SQLTableBuilderTest, DropColumn_InSameVersion) {
   builder()->AddColumn("password_value", "BLOB");
   builder()->DropColumn("password_value");
@@ -148,15 +194,29 @@ TEST_F(SQLTableBuilderTest, DropColumn_InNextVersion) {
   EXPECT_FALSE(db()->DoesColumnExist("my_logins_table", "password_value"));
 }
 
+TEST_F(SQLTableBuilderTest, DropIndex_InNextVersion) {
+  builder()->AddIndex("my_index", {"signon_realm"});
+  EXPECT_EQ(0u, builder()->SealVersion());
+  builder()->DropIndex("my_index");
+  EXPECT_EQ(1u, builder()->SealVersion());
+  EXPECT_TRUE(builder()->CreateTable(db()));
+  EXPECT_TRUE(db()->DoesTableExist("my_logins_table"));
+  EXPECT_FALSE(db()->DoesIndexExist("my_index"));
+}
+
 TEST_F(SQLTableBuilderTest, MigrateFrom) {
   // First, create a table at version 0, with some columns.
   builder()->AddColumn("for_renaming", "INTEGER DEFAULT 100");
   builder()->AddColumn("for_deletion", "INTEGER");
+  builder()->AddIndex("my_signon_index", {"signon_realm"});
+  builder()->AddIndex("my_changing_index_v0", {"for_renaming", "for_deletion"});
   EXPECT_EQ(0u, builder()->SealVersion());
   EXPECT_TRUE(builder()->CreateTable(db()));
   EXPECT_TRUE(db()->DoesTableExist("my_logins_table"));
   EXPECT_TRUE(db()->DoesColumnExist("my_logins_table", "for_renaming"));
   EXPECT_TRUE(db()->DoesColumnExist("my_logins_table", "for_deletion"));
+  EXPECT_TRUE(db()->DoesIndexExist("my_signon_index"));
+  EXPECT_TRUE(db()->DoesIndexExist("my_changing_index_v0"));
   EXPECT_TRUE(
       db()->Execute("INSERT INTO my_logins_table (signon_realm, for_renaming, "
                     "for_deletion) VALUES ('abc', 123, 456)"));
@@ -172,21 +232,28 @@ TEST_F(SQLTableBuilderTest, MigrateFrom) {
   EXPECT_TRUE(first_check.Succeeded());
 
   // Now, specify some modifications for version 1.
+  builder()->DropIndex("my_changing_index_v0");
   builder()->RenameColumn("for_renaming", "renamed");
   builder()->DropColumn("for_deletion");
   builder()->AddColumn("new_column", "INTEGER DEFAULT 789");
+  builder()->AddIndex("my_changing_index_v1", {"renamed", "new_column"});
   EXPECT_EQ(1u, builder()->SealVersion());
 
   // The migration should have the following effect:
   // * The renamed column should keep its non-default value.
   // * The succession of column removal and addition should not result in the
   //   values from the deleted column to be copied to the added one.
+  // * Only the signon index and the second version of the changing index should
+  //   be present in the last version.
   EXPECT_TRUE(builder()->MigrateFrom(0, db()));
   EXPECT_FALSE(db()->DoesColumnExist("my_logins_table", "for_renaming"));
   EXPECT_FALSE(db()->DoesColumnExist("my_logins_table", "for_deletion"));
   EXPECT_TRUE(db()->DoesColumnExist("my_logins_table", "renamed"));
   EXPECT_TRUE(IsColumnOfType("renamed", "INTEGER"));
   EXPECT_TRUE(db()->DoesColumnExist("my_logins_table", "new_column"));
+  EXPECT_TRUE(db()->DoesIndexExist("my_signon_index"));
+  EXPECT_FALSE(db()->DoesIndexExist("my_changing_index_v0"));
+  EXPECT_TRUE(db()->DoesIndexExist("my_changing_index_v1"));
   sql::Statement second_check(
       db()->GetCachedStatement(SQL_FROM_HERE, retrieval));
   EXPECT_TRUE(second_check.Step());
@@ -198,7 +265,7 @@ TEST_F(SQLTableBuilderTest, MigrateFrom) {
   EXPECT_TRUE(second_check.Succeeded());
 }
 
-TEST_F(SQLTableBuilderTest, MigrateFrom_RenameAndAdd) {
+TEST_F(SQLTableBuilderTest, MigrateFrom_RenameAndAddColumns) {
   builder()->AddColumn("old_name", "INTEGER");
   EXPECT_EQ(0u, builder()->SealVersion());
 
@@ -222,7 +289,27 @@ TEST_F(SQLTableBuilderTest, MigrateFrom_RenameAndAdd) {
   EXPECT_EQ("signon_realm=?", builder()->ListAllUniqueKeyNames());
 }
 
-TEST_F(SQLTableBuilderTest, MigrateFrom_RenameAndAddAndDrop) {
+TEST_F(SQLTableBuilderTest, MigrateFrom_RenameAndAddIndices) {
+  builder()->AddIndex("old_name", {"signon_realm"});
+  EXPECT_EQ(0u, builder()->SealVersion());
+
+  EXPECT_TRUE(builder()->CreateTable(db()));
+
+  builder()->RenameIndex("old_name", "new_name");
+  EXPECT_EQ(1u, builder()->SealVersion());
+
+  builder()->AddIndex("added", {"signon_realm"});
+  EXPECT_EQ(2u, builder()->SealVersion());
+
+  EXPECT_TRUE(builder()->MigrateFrom(0, db()));
+  EXPECT_FALSE(db()->DoesIndexExist("old_name"));
+  EXPECT_TRUE(db()->DoesIndexExist("added"));
+  EXPECT_TRUE(db()->DoesIndexExist("new_name"));
+  EXPECT_EQ(2u, builder()->NumberOfIndices());
+  EXPECT_EQ("new_name, added", builder()->ListAllIndexNames());
+}
+
+TEST_F(SQLTableBuilderTest, MigrateFrom_RenameAndAddAndDropColumns) {
   builder()->AddColumnToUniqueKey("uni", "VARCHAR NOT NULL");
   builder()->AddColumn("old_name", "INTEGER");
   EXPECT_EQ(0u, builder()->SealVersion());
@@ -248,6 +335,29 @@ TEST_F(SQLTableBuilderTest, MigrateFrom_RenameAndAddAndDrop) {
   EXPECT_EQ("signon_realm, uni, new_name", builder()->ListAllColumnNames());
   EXPECT_EQ("new_name=?", builder()->ListAllNonuniqueKeyNames());
   EXPECT_EQ("signon_realm=? AND uni=?", builder()->ListAllUniqueKeyNames());
+}
+
+TEST_F(SQLTableBuilderTest, MigrateFrom_RenameAndAddAndDropIndices) {
+  builder()->AddIndex("old_name", {"signon_realm"});
+  EXPECT_EQ(0u, builder()->SealVersion());
+
+  EXPECT_TRUE(builder()->CreateTable(db()));
+
+  builder()->RenameIndex("old_name", "new_name");
+  EXPECT_EQ(1u, builder()->SealVersion());
+
+  builder()->AddIndex("added", {"signon_realm"});
+  EXPECT_EQ(2u, builder()->SealVersion());
+
+  builder()->DropIndex("added");
+  EXPECT_EQ(3u, builder()->SealVersion());
+
+  EXPECT_TRUE(builder()->MigrateFrom(0, db()));
+  EXPECT_FALSE(db()->DoesIndexExist("old_name"));
+  EXPECT_FALSE(db()->DoesIndexExist("added"));
+  EXPECT_TRUE(db()->DoesIndexExist("new_name"));
+  EXPECT_EQ(1u, builder()->NumberOfColumns());
+  EXPECT_EQ("new_name", builder()->ListAllIndexNames());
 }
 
 }  // namespace password_manager
