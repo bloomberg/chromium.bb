3bc72dd6ef85b2f7548411df76cb99c956d6e84c
Bence Béky <bnc@chromium.org>
2017-06-30 19:52:52 +0000

    Remove is_secure from SpdySession.
    Remove is_secure_ private member from SpdySession, and is_secure
    argument from SpdySession::InitializeWithSocket(), since Chromium does
    not support unencrypted HTTP/2 connections.
    
    Remove unused SpdySession::GetServer().
    
    Subclass FuzzedSocketFactory so that SpdySessionFuzzer works with secure
    SpdySessions.  AddSSLSocketDataProvider() and CreateSSLClientSocket()
    method definitions are mostly copied over from MockClientSocketFactory.
    
    Remove obsolete
    AltSvcFrameTest.DoNotProcessAltSvcFrameOnInsecureSession.
    
    Remove now unused CreateInsecureSpdySession() test-only function.
    
    Rename CreateSecureSpdySession() test-only function
    to CreateSpdySession().
    
    BUG=737143
    
    Change-Id: I785c7e25170423294e4ed71fe7cdfba7d7e00808
    Reviewed-on: https://chromium-review.googlesource.com/558146
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#483794}
    Former-commit-id: 0ef1556e374c51f1ffc344e88cb1d9e6659b03a2

--- net/http/http_stream_factory_impl_job.cc
+++ net/http/http_stream_factory_impl_job.cc
@@ -1216,8 +1216,7 @@ int HttpStreamFactoryImpl::Job::DoCreateStream() {
 
   base::WeakPtr<SpdySession> spdy_session =
       session_->spdy_session_pool()->CreateAvailableSessionFromSocket(
-          spdy_session_key_, std::move(connection_), net_log_,
-          /* is_secure = */ true);
+          spdy_session_key_, std::move(connection_), net_log_);
 
   if (!spdy_session->HasAcceptableTransportSecurity()) {
     spdy_session->CloseSessionOnError(
