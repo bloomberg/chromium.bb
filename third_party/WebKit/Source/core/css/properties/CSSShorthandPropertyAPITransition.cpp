8172d543b302cc39bf3e47857bb1e8c466b16ecc
Jia <jiameng@chromium.org>
2017-07-04 06:08:23 +0000

    Implement parseShorthand for shorthand properties "animation" and "transition".
    This cl contains the following changes
    1. Refactored ConsumeAnimationValue from CSSPropertyParser and broke it into
    ConsumeAnimationValue and ConsumeTransitionValue.
    - The old version is used by both "animation" and "transition" properties, now
    the two properties are parsed by respective Consume<Property>Value functions.
    - Moved the new ConsumeAnimationValue into animation property API as an anonymous 
    function.
    - Moved the new ConsumeTransitionValue into transition property API as an anonymous 
    function.
    
    2. Refactored ConsumeAnimationShorthand from CSSPropertyParser and moved it to
    a new util file (CSSPropertyAnimationUtils) as ConsumeAnimationShorthand.
    - The old version directly calls the old ConsumeAnimationValue. Since we are 
    breaking up ConsumeAnimationValue, the new ConsumeAnimationShorthand receives a callback 
    func as a param and forwards respective args to the appropriate callback functions. 
    The expected callback functions are ConsumeAnimationValue and 
    ConsumeTransitionValue.
    
    3. Added CSSPropertyAnimationTimingFunctionUtils file.
    - Moved ConsumeAnimationTimingFunction from CSSPropertyParser to this util file.
    - It's currently being used by "animation" and "transition" shorthands. It will 
    later be used by animation-timing-function longhand API.
    
    4. Added API impl for "animation" property.
    - In addition to parseShorthand, it has an anonymous ConsumeAnimationValue func.
    
    5. Added API impl of "transition" property.
    - In addition to parseShorthand, it has an anonymous ConsumeTransitionValue func.
    
    6. Changed CSSPropertyParser to use the new API for these two shorthands.
    - Also removed functions that won't be used.
    
    7. Build files and json to include the new API.
    
    Bug: 668012
    Diff: https://gist.github.com/429b73fec6e926f58897301b51856465/revisions
    Change-Id: Ic20898fbd603c5760da4b5afab86aa48edcff600
    Reviewed-on: https://chromium-review.googlesource.com/549675
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484058}
    Former-commit-id: e9f0923be4be20773b0138e070a2dbf35bdb11e6

---
+++ third_party/WebKit/Source/core/css/properties/CSSShorthandPropertyAPITransition.cpp
@@ -0,0 +1,73 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSShorthandPropertyAPITransition.h"
+
+#include "core/StylePropertyShorthand.h"
+#include "core/css/CSSIdentifierValue.h"
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSParserLocalContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "core/css/properties/CSSPropertyAnimationTimingFunctionUtils.h"
+#include "core/css/properties/CSSPropertyAnimationUtils.h"
+#include "core/css/properties/CSSPropertyTransitionPropertyUtils.h"
+
+namespace blink {
+
+namespace {
+
+CSSValue* ConsumeTransitionValue(CSSPropertyID property,
+                                 CSSParserTokenRange& range) {
+  switch (property) {
+    case CSSPropertyTransitionDelay:
+      return CSSPropertyParserHelpers::ConsumeTime(range, kValueRangeAll);
+    case CSSPropertyTransitionDuration:
+      return CSSPropertyParserHelpers::ConsumeTime(range,
+                                                   kValueRangeNonNegative);
+    case CSSPropertyTransitionProperty:
+      return CSSPropertyTransitionPropertyUtils::ConsumeTransitionProperty(
+          range);
+    case CSSPropertyTransitionTimingFunction:
+      return CSSPropertyAnimationTimingFunctionUtils::
+          ConsumeAnimationTimingFunction(range);
+    default:
+      NOTREACHED();
+      return nullptr;
+  }
+}
+
+}  // namespace
+
+bool CSSShorthandPropertyAPITransition::parseShorthand(
+    bool important,
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    const CSSParserLocalContext&,
+    HeapVector<CSSProperty, 256>& properties) {
+  const StylePropertyShorthand shorthand = transitionShorthandForParsing();
+  const unsigned longhand_count = shorthand.length();
+
+  HeapVector<CSSValueList*, kMaxNumAnimationLonghands> longhands(
+      longhand_count);
+  if (!CSSPropertyAnimationUtils::ConsumeAnimationShorthand(
+          shorthand, longhands, ConsumeTransitionValue, range)) {
+    return false;
+  }
+
+  for (size_t i = 0; i < longhand_count; ++i) {
+    if (shorthand.properties()[i] == CSSPropertyTransitionProperty &&
+        !CSSPropertyTransitionPropertyUtils::IsValidPropertyList(*longhands[i]))
+      return false;
+  }
+
+  for (size_t i = 0; i < longhand_count; ++i) {
+    CSSPropertyParserHelpers::AddProperty(
+        shorthand.properties()[i], shorthand.id(), *longhands[i], important,
+        CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+  }
+
+  return range.AtEnd();
+}
+
+}  // namespace blink
