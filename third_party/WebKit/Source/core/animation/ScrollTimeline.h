3c106a61e4bb3402ac0a28c7eedc25f1802ccfc3
smcgruer <smcgruer@chromium.org>
2017-07-04 08:48:41 -0700

    Basic ScrollTimeline implementation for Animation Worklet
    This is based on the WICG spec[0], but simplified for the Animation Worklet
    origin trial. Specifically, the following things were changed from the spec:
    
      * ScrollDirection only has two values: "block" and "inline".
      * Dropped startScrollOffset.
      * Dropped endScrollOffset.
      * timeRange is not allowed to be "auto".
      * Dropped fill.
    
    [0] https://wicg.github.io/scroll-animations/#scrolltimeline-interface
    
    BUG=719517
    
    Review-Url: https://codereview.chromium.org/2873493002
    Cr-Commit-Position: refs/heads/master@{#484117}
    
    Former-commit-id: a5a47f2f0ae0b0ec8e114f0023d1bae7290475cd

---
+++ third_party/WebKit/Source/core/animation/ScrollTimeline.h
@@ -0,0 +1,59 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ScrollTimeline_h
+#define ScrollTimeline_h
+
+#include "core/CoreExport.h"
+#include "core/animation/AnimationTimeline.h"
+#include "core/animation/ScrollTimelineOptions.h"
+#include "core/dom/Element.h"
+#include "platform/bindings/ScriptWrappable.h"
+#include "platform/wtf/text/WTFString.h"
+
+namespace blink {
+
+// Implements the ScrollTimeline concept from the Scroll-linked Animations spec.
+//
+// A ScrollTimeline is a special form of AnimationTimeline whose time values are
+// not determined by wall-clock time but instead the progress of scrolling in a
+// scroll container. The user is able to specify which scroll container to
+// track, the direction of scroll they care about, and various attributes to
+// control the conversion of scroll amount to time output.
+//
+// Spec: https://wicg.github.io/scroll-animations/#scroll-timelines
+class CORE_EXPORT ScrollTimeline final : public AnimationTimeline {
+  DEFINE_WRAPPERTYPEINFO();
+
+ public:
+  enum ScrollDirection {
+    Block,
+    Inline,
+  };
+
+  static ScrollTimeline* Create(Document&,
+                                ScrollTimelineOptions,
+                                ExceptionState&);
+
+  // AnimationTimeline implementation.
+  double currentTime(bool& is_null) final;
+
+  // IDL API implementation.
+  Element* scrollSource();
+  String orientation();
+  void timeRange(DoubleOrScrollTimelineAutoKeyword&);
+
+  DECLARE_VIRTUAL_TRACE();
+
+ private:
+  ScrollTimeline(const Document&, Element*, ScrollDirection, double);
+
+  Member<Element> scroll_source_;
+  ScrollDirection orientation_;
+  double time_range_;
+};
+
+}  // namespace blink
+
+#endif
