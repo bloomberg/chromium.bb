77fc49518c4f258fbed936dd6ab2b3677eb1c871
Becca Hughes <beccahughes@chromium.org>
2017-07-04 16:28:17 +0000

    Add a MediaEngagementScore class to manage storing the score.
    Add a MediaEngagementScore class that manages calculating the total
    score as well as storing/reading the score for an origin from the
    content settings store. Register the setting to be synced across
    devices.
    
    BUG=731750
    
    Change-Id: I30e1ceb815c252d5f0395a8d10b905e7a6c9914b
    Reviewed-on: https://chromium-review.googlesource.com/529087
    Commit-Queue: Becca Hughes <beccahughes@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484127}
    Former-commit-id: e06e83a8e6999472d4b9d6efa8ccc3b79ca8c9b7

---
+++ chrome/browser/media/media_engagement_score.cc
@@ -0,0 +1,110 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/media/media_engagement_score.h"
+
+#include "chrome/browser/engagement/site_engagement_metrics.h"
+#include "components/content_settings/core/browser/host_content_settings_map.h"
+#include "components/content_settings/core/common/content_settings.h"
+#include "components/content_settings/core/common/content_settings_types.h"
+
+const char* MediaEngagementScore::kVisitsKey = "visits";
+const char* MediaEngagementScore::kMediaPlaybacksKey = "mediaPlaybacks";
+const char* MediaEngagementScore::kLastMediaPlaybackTimeKey =
+    "lastMediaPlaybackTime";
+
+const int MediaEngagementScore::kScoreMinVisits = 5;
+
+namespace {
+
+std::unique_ptr<base::DictionaryValue> GetScoreDictForSettings(
+    const HostContentSettingsMap* settings,
+    const GURL& origin_url) {
+  if (!settings)
+    return base::MakeUnique<base::DictionaryValue>();
+
+  std::unique_ptr<base::DictionaryValue> value =
+      base::DictionaryValue::From(settings->GetWebsiteSetting(
+          origin_url, origin_url, CONTENT_SETTINGS_TYPE_MEDIA_ENGAGEMENT,
+          content_settings::ResourceIdentifier(), nullptr));
+
+  if (value.get())
+    return value;
+  return base::MakeUnique<base::DictionaryValue>();
+}
+
+}  // namespace
+
+MediaEngagementScore::MediaEngagementScore(base::Clock* clock,
+                                           const GURL& origin,
+                                           HostContentSettingsMap* settings)
+    : MediaEngagementScore(clock,
+                           origin,
+                           GetScoreDictForSettings(settings, origin)) {
+  settings_map_ = settings;
+}
+
+MediaEngagementScore::MediaEngagementScore(
+    base::Clock* clock,
+    const GURL& origin,
+    std::unique_ptr<base::DictionaryValue> score_dict)
+    : origin_(origin), clock_(clock), score_dict_(score_dict.release()) {
+  if (!score_dict_)
+    return;
+
+  score_dict_->GetInteger(kVisitsKey, &visits_);
+  score_dict_->GetInteger(kMediaPlaybacksKey, &media_playbacks_);
+
+  double internal_time;
+  if (score_dict_->GetDouble(kLastMediaPlaybackTimeKey, &internal_time))
+    last_media_playback_time_ = base::Time::FromInternalValue(internal_time);
+}
+
+MediaEngagementScore::~MediaEngagementScore() = default;
+
+double MediaEngagementScore::GetTotalScore() const {
+  if (visits_ < kScoreMinVisits)
+    return 0;
+  return static_cast<double>(media_playbacks_) / static_cast<double>(visits_);
+}
+
+void MediaEngagementScore::Commit() {
+  DCHECK(settings_map_);
+  if (!UpdateScoreDict())
+    return;
+
+  settings_map_->SetWebsiteSettingDefaultScope(
+      origin_, GURL(), CONTENT_SETTINGS_TYPE_MEDIA_ENGAGEMENT,
+      content_settings::ResourceIdentifier(), std::move(score_dict_));
+}
+
+void MediaEngagementScore::IncrementMediaPlaybacks() {
+  media_playbacks_++;
+  last_media_playback_time_ = clock_->Now();
+}
+
+bool MediaEngagementScore::UpdateScoreDict() {
+  int stored_visits = 0;
+  int stored_media_playbacks = 0;
+  double stored_last_media_playback_internal = 0;
+
+  score_dict_->GetInteger(kVisitsKey, &stored_visits);
+  score_dict_->GetInteger(kMediaPlaybacksKey, &stored_media_playbacks);
+  score_dict_->GetDouble(kLastMediaPlaybackTimeKey,
+                         &stored_last_media_playback_internal);
+
+  bool changed = stored_visits != visits() ||
+                 stored_media_playbacks != media_playbacks() ||
+                 stored_last_media_playback_internal !=
+                     last_media_playback_time_.ToInternalValue();
+  if (!changed)
+    return false;
+
+  score_dict_->SetInteger(kVisitsKey, visits_);
+  score_dict_->SetInteger(kMediaPlaybacksKey, media_playbacks_);
+  score_dict_->SetDouble(kLastMediaPlaybackTimeKey,
+                         last_media_playback_time_.ToInternalValue());
+
+  return true;
+}
---
+++ chrome/browser/media/media_engagement_score.h
@@ -0,0 +1,96 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_SCORE_H_
+#define CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_SCORE_H_
+
+#include "base/macros.h"
+#include "base/memory/ptr_util.h"
+#include "base/time/clock.h"
+#include "base/values.h"
+#include "url/gurl.h"
+
+class HostContentSettingsMap;
+
+// Calculates and stores the Media Engagement Index score for a certain origin.
+class MediaEngagementScore final {
+ public:
+  // The dictionary keys to store individual metrics. kVisitsKey will
+  // store the number of visits to an origin and kMediaPlaybacksKey
+  // will store the number of media playbacks on an origin.
+  // kLastMediaPlaybackTimeKey will store the timestamp of the last
+  // media playback on an origin.
+  static const char* kVisitsKey;
+  static const char* kMediaPlaybacksKey;
+  static const char* kLastMediaPlaybackTimeKey;
+
+  // Origins with a number of visits less than this number will recieve
+  // a score of zero.
+  static const int kScoreMinVisits;
+
+  MediaEngagementScore(base::Clock* clock,
+                       const GURL& origin,
+                       HostContentSettingsMap* settings);
+  ~MediaEngagementScore();
+
+  // Returns the total score, as per the formula.
+  double GetTotalScore() const;
+
+  // Writes the values in this score into |settings_map_|. If there are multiple
+  // instances of a score object for an origin, this could result in stale data
+  // being stored.
+  void Commit();
+
+  // Get/increment the number of visits this origin has had.
+  int visits() const { return visits_; }
+  void IncrementVisits() { visits_++; }
+
+  // Get/increment the number of media playbacks this origin has had.
+  int media_playbacks() const { return media_playbacks_; }
+  void IncrementMediaPlaybacks();
+
+  // Get the last time media was played on this origin.
+  base::Time last_media_playback_time() const {
+    return last_media_playback_time_;
+  }
+
+ private:
+  friend class MediaEngagementScoreTest;
+
+  // Used for tests.
+  MediaEngagementScore(base::Clock* clock,
+                       const GURL& origin,
+                       std::unique_ptr<base::DictionaryValue> score_dict);
+
+  // Update the dictionary continaing the latest score values and return whether
+  // they have changed or not (since what was last retrieved from content
+  // settings).
+  bool UpdateScoreDict();
+
+  // The number of media playbacks this origin has had.
+  int media_playbacks_ = 0;
+
+  // The number of visits this origin has had.
+  int visits_ = 0;
+
+  // The last time media was played back on this origin.
+  base::Time last_media_playback_time_;
+
+  // The origin this score represents.
+  GURL origin_;
+
+  // A clock that can be used for testing, owned by the service.
+  base::Clock* clock_;
+
+  // The dictionary that represents this engagement score.
+  std::unique_ptr<base::DictionaryValue> score_dict_;
+
+  // The content settings map that will persist the score,
+  // has a lifetime of the Profile like the service which owns |this|.
+  HostContentSettingsMap* settings_map_;
+
+  DISALLOW_COPY_AND_ASSIGN(MediaEngagementScore);
+};
+
+#endif  // CHROME_BROWSER_MEDIA_MEDIA_ENGAGEMENT_SCORE_H_
