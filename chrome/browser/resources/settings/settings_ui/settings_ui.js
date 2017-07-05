8ccdf84846657c68dc30db9e462b161d7156dc9d
Dan Elphick <delphick@chromium.org>
2017-07-05 16:01:35 +0000

    Fix potential assertion failure in settings
    Remove IntersectionObserver callback assertion that there be only one
    entry. Since it's only checking if the element is visible or not just
    ignore all but the last entry.
    
    The assertion failure causes CrSettingsRouteDynamicParametersTest to fail in
    combination with another uncommitted change to increase the priority of the
    compositor task queue.
    
    Bug: 703608
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I3c162153e1ed32c81a02dd9eddcd3762974c118a
    Reviewed-on: https://chromium-review.googlesource.com/558247
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Commit-Queue: Dan Elphick <delphick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484287}
    Former-commit-id: c4374fd8312a91467b300590ad4f65512b393441

--- chrome/browser/resources/settings/settings_ui/settings_ui.js
+++ chrome/browser/resources/settings/settings_ui/settings_ui.js
@@ -159,9 +159,8 @@ Polymer({
 
     // Setup drop shadow logic.
     var callback = function(entries) {
-      assert(entries.length == 1);
       this.$.dropShadow.classList.toggle(
-          'has-shadow', entries[0].intersectionRatio == 0);
+          'has-shadow', entries[entries.length - 1].intersectionRatio == 0);
     }.bind(this);
 
     this.intersectionObserver_ = new IntersectionObserver(
