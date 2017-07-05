3ad61179f68d1eec4a3aca4bdfcfa0715b72f5be
sadrul <sadrul@chromium.org>
2017-07-05 11:17:54 -0700

    vr/gpu: BrowserGpuChannelHostFactory should be called on the same thread.
    MailboxToSurfaceBridge in vr code lives in the vr-gl thread. It requests
    a cc::ContextProvider from content::Compositor, which in turn gets a gpu
    channel from BrowserGpuChannelHostFactory. However, the factory lives in
    the UI thread, and is not thread-safe. So, impose a restriction on the
    factory and content::Compositor that the relevant functions can only be
    called from the UI thread. Update the vr code to do the necessary thread
    hopping to make sure it calls the functions on the right threads.
    
    BUG=609316
    
    Review-Url: https://codereview.chromium.org/2969153002
    Cr-Commit-Position: refs/heads/master@{#484311}
    
    Former-commit-id: a4447c621719211e9d8624de2fe62c59f1e1384e

--- chrome/browser/android/vr_shell/mailbox_to_surface_bridge.h
+++ chrome/browser/android/vr_shell/mailbox_to_surface_bridge.h
@@ -8,6 +8,7 @@
 #include "base/memory/weak_ptr.h"
 
 namespace gl {
+class ScopedJavaSurface;
 class SurfaceTexture;
 }
 
@@ -39,7 +40,8 @@ class MailboxToSurfaceBridge {
   bool CopyMailboxToSurfaceAndSwap(const gpu::MailboxHolder& mailbox);
 
  private:
-  void OnContextAvailable(scoped_refptr<cc::ContextProvider>);
+  void OnContextAvailable(std::unique_ptr<gl::ScopedJavaSurface> surface,
+                          scoped_refptr<cc::ContextProvider>);
   void InitializeRenderer();
   void DestroyContext();
   void DrawQuad(unsigned int textureHandle);
