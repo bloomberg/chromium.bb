0d1a9c68a36e52e1634a26908bba083b545cdf04
hansberry <hansberry@chromium.org>
2017-07-05 13:02:22 -0700

    Tether: Add metrics for host connection results, and a wrapper around recording them.
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2969503002
    Cr-Commit-Position: refs/heads/master@{#484337}
    
    Former-commit-id: 739d014309b133de81f42e777eb8435e75410035

---
+++ chromeos/components/tether/host_connection_metrics_logger.cc
@@ -0,0 +1,116 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/tether/host_connection_metrics_logger.h"
+
+#include "base/metrics/histogram_macros.h"
+
+namespace chromeos {
+
+namespace tether {
+
+HostConnectionMetricsLogger::HostConnectionMetricsLogger() {}
+
+HostConnectionMetricsLogger::~HostConnectionMetricsLogger() {}
+
+void HostConnectionMetricsLogger::RecordConnectionToHostResult(
+    ConnectionToHostResult result) {
+  switch (result) {
+    case ConnectionToHostResult::CONNECTION_RESULT_PROVISIONING_FAILED:
+      RecordConnectionResultProvisioningFailure(
+          ConnectionToHostResult_ProvisioningFailureEventType::
+              PROVISIONING_FAILED);
+      break;
+    case ConnectionToHostResult::CONNECTION_RESULT_SUCCESS:
+      RecordConnectionResultSuccess(
+          ConnectionToHostResult_SuccessEventType::SUCCESS);
+      break;
+    case ConnectionToHostResult::CONNECTION_RESULT_FAILURE_UNKNOWN_ERROR:
+      RecordConnectionResultFailure(
+          ConnectionToHostResult_FailureEventType::UNKNOWN_ERROR);
+      break;
+    case ConnectionToHostResult::
+        CONNECTION_RESULT_FAILURE_CLIENT_CONNECTION_TIMEOUT:
+      RecordConnectionResultFailureClientConnection(
+          ConnectionToHostResult_FailureClientConnectionEventType::TIMEOUT);
+      break;
+    case ConnectionToHostResult::
+        CONNECTION_RESULT_FAILURE_CLIENT_CONNECTION_CANCELED_BY_NEW_ATTEMPT:
+      RecordConnectionResultFailureClientConnection(
+          ConnectionToHostResult_FailureClientConnectionEventType::
+              CANCELED_BY_NEW_ATTEMPT);
+      break;
+    case ConnectionToHostResult::
+        CONNECTION_RESULT_FAILURE_TETHERING_TIMED_OUT_FIRST_TIME_SETUP_WAS_REQUIRED:
+      RecordConnectionResultFailureTetheringTimeout(
+          ConnectionToHostResult_FailureTetheringTimeoutEventType::
+              FIRST_TIME_SETUP_WAS_REQUIRED);
+      break;
+    case ConnectionToHostResult::
+        CONNECTION_RESULT_FAILURE_TETHERING_TIMED_OUT_FIRST_TIME_SETUP_WAS_NOT_REQUIRED:
+      RecordConnectionResultFailureTetheringTimeout(
+          ConnectionToHostResult_FailureTetheringTimeoutEventType::
+              FIRST_TIME_SETUP_WAS_NOT_REQUIRED);
+      break;
+    default:
+      NOTREACHED();
+  };
+}
+
+void HostConnectionMetricsLogger::RecordConnectionResultProvisioningFailure(
+    ConnectionToHostResult_ProvisioningFailureEventType event_type) {
+  UMA_HISTOGRAM_ENUMERATION(
+      "InstantTethering.ConnectionToHostResult.ProvisioningFailureRate",
+      event_type,
+      ConnectionToHostResult_ProvisioningFailureEventType::
+          PROVISIONING_FAILURE_MAX);
+}
+
+void HostConnectionMetricsLogger::RecordConnectionResultSuccess(
+    ConnectionToHostResult_SuccessEventType event_type) {
+  UMA_HISTOGRAM_ENUMERATION(
+      "InstantTethering.ConnectionToHostResult.SuccessRate", event_type,
+      ConnectionToHostResult_SuccessEventType::SUCCESS_MAX);
+
+  RecordConnectionResultProvisioningFailure(
+      ConnectionToHostResult_ProvisioningFailureEventType::OTHER);
+}
+
+void HostConnectionMetricsLogger::RecordConnectionResultFailure(
+    ConnectionToHostResult_FailureEventType event_type) {
+  UMA_HISTOGRAM_ENUMERATION(
+      "InstantTethering.ConnectionToHostResult.Failure", event_type,
+      ConnectionToHostResult_FailureEventType::FAILURE_MAX);
+
+  RecordConnectionResultSuccess(
+      ConnectionToHostResult_SuccessEventType::FAILURE);
+}
+
+void HostConnectionMetricsLogger::RecordConnectionResultFailureClientConnection(
+    ConnectionToHostResult_FailureClientConnectionEventType event_type) {
+  UMA_HISTOGRAM_ENUMERATION(
+      "InstantTethering.ConnectionToHostResult.Failure.ClientConnection",
+      event_type,
+      ConnectionToHostResult_FailureClientConnectionEventType::
+          FAILURE_CLIENT_CONNECTION_MAX);
+
+  RecordConnectionResultFailure(
+      ConnectionToHostResult_FailureEventType::CLIENT_CONNECTION_ERROR);
+}
+
+void HostConnectionMetricsLogger::RecordConnectionResultFailureTetheringTimeout(
+    ConnectionToHostResult_FailureTetheringTimeoutEventType event_type) {
+  UMA_HISTOGRAM_ENUMERATION(
+      "InstantTethering.ConnectionToHostResult.Failure.TetheringTimeout",
+      event_type,
+      ConnectionToHostResult_FailureTetheringTimeoutEventType::
+          FAILURE_TETHERING_TIMEOUT_MAX);
+
+  RecordConnectionResultFailure(
+      ConnectionToHostResult_FailureEventType::TETHERING_TIMED_OUT);
+}
+
+}  // namespace tether
+
+}  // namespace chromeos
---
+++ chromeos/components/tether/host_connection_metrics_logger_unittest.cc
@@ -0,0 +1,189 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/components/tether/host_connection_metrics_logger.h"
+
+#include "base/memory/ptr_util.h"
+#include "base/test/histogram_tester.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+
+namespace tether {
+
+class HostConnectionMetricsLoggerTest : public testing::Test {
+ protected:
+  HostConnectionMetricsLoggerTest() {}
+
+  void SetUp() override {
+    metrics_logger_ = base::MakeUnique<HostConnectionMetricsLogger>();
+  }
+
+  void VerifyProvisioningFailure(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_ProvisioningFailureEventType event_type) {
+    histogram_tester_.ExpectUniqueSample(
+        "InstantTethering.ConnectionToHostResult.ProvisioningFailureRate",
+        event_type, 1);
+  }
+
+  void VerifySuccess(
+      HostConnectionMetricsLogger::ConnectionToHostResult_SuccessEventType
+          event_type) {
+    histogram_tester_.ExpectUniqueSample(
+        "InstantTethering.ConnectionToHostResult.SuccessRate", event_type, 1);
+  }
+
+  void VerifyFailure(
+      HostConnectionMetricsLogger::ConnectionToHostResult_FailureEventType
+          event_type) {
+    histogram_tester_.ExpectUniqueSample(
+        "InstantTethering.ConnectionToHostResult.Failure", event_type, 1);
+  }
+
+  void VerifyFailure_ClientConnection(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_FailureClientConnectionEventType event_type) {
+    histogram_tester_.ExpectUniqueSample(
+        "InstantTethering.ConnectionToHostResult.Failure.ClientConnection",
+        event_type, 1);
+  }
+
+  void VerifyFailure_TetheringTimeout(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_FailureTetheringTimeoutEventType event_type) {
+    histogram_tester_.ExpectUniqueSample(
+        "InstantTethering.ConnectionToHostResult.Failure.TetheringTimeout",
+        event_type, 1);
+  }
+
+  std::unique_ptr<HostConnectionMetricsLogger> metrics_logger_;
+
+  base::HistogramTester histogram_tester_;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(HostConnectionMetricsLoggerTest);
+};
+
+TEST_F(HostConnectionMetricsLoggerTest,
+       RecordConnectionResultProvisioningFailure) {
+  metrics_logger_->RecordConnectionToHostResult(
+      HostConnectionMetricsLogger::ConnectionToHostResult::
+          CONNECTION_RESULT_PROVISIONING_FAILED);
+
+  VerifyProvisioningFailure(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_ProvisioningFailureEventType::
+              PROVISIONING_FAILED);
+}
+
+TEST_F(HostConnectionMetricsLoggerTest, RecordConnectionResultSuccess) {
+  metrics_logger_->RecordConnectionToHostResult(
+      HostConnectionMetricsLogger::ConnectionToHostResult::
+          CONNECTION_RESULT_SUCCESS);
+
+  VerifySuccess(HostConnectionMetricsLogger::
+                    ConnectionToHostResult_SuccessEventType::SUCCESS);
+  VerifyProvisioningFailure(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_ProvisioningFailureEventType::OTHER);
+}
+
+TEST_F(HostConnectionMetricsLoggerTest, RecordConnectionResultFailure) {
+  metrics_logger_->RecordConnectionToHostResult(
+      HostConnectionMetricsLogger::ConnectionToHostResult::
+          CONNECTION_RESULT_FAILURE_UNKNOWN_ERROR);
+
+  VerifyFailure(HostConnectionMetricsLogger::
+                    ConnectionToHostResult_FailureEventType::UNKNOWN_ERROR);
+
+  VerifySuccess(HostConnectionMetricsLogger::
+                    ConnectionToHostResult_SuccessEventType::FAILURE);
+  VerifyProvisioningFailure(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_ProvisioningFailureEventType::OTHER);
+}
+
+TEST_F(HostConnectionMetricsLoggerTest,
+       RecordConnectionResultFailureClientConnection_Timeout) {
+  metrics_logger_->RecordConnectionToHostResult(
+      HostConnectionMetricsLogger::ConnectionToHostResult::
+          CONNECTION_RESULT_FAILURE_CLIENT_CONNECTION_TIMEOUT);
+
+  VerifyFailure_ClientConnection(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_FailureClientConnectionEventType::TIMEOUT);
+  VerifyFailure(
+      HostConnectionMetricsLogger::ConnectionToHostResult_FailureEventType::
+          CLIENT_CONNECTION_ERROR);
+  VerifySuccess(HostConnectionMetricsLogger::
+                    ConnectionToHostResult_SuccessEventType::FAILURE);
+  VerifyProvisioningFailure(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_ProvisioningFailureEventType::OTHER);
+}
+
+TEST_F(HostConnectionMetricsLoggerTest,
+       RecordConnectionResultFailureClientConnection_CanceledByNewAttempt) {
+  metrics_logger_->RecordConnectionToHostResult(
+      HostConnectionMetricsLogger::ConnectionToHostResult::
+          CONNECTION_RESULT_FAILURE_CLIENT_CONNECTION_CANCELED_BY_NEW_ATTEMPT);
+
+  VerifyFailure_ClientConnection(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_FailureClientConnectionEventType::
+              CANCELED_BY_NEW_ATTEMPT);
+  VerifyFailure(
+      HostConnectionMetricsLogger::ConnectionToHostResult_FailureEventType::
+          CLIENT_CONNECTION_ERROR);
+  VerifySuccess(HostConnectionMetricsLogger::
+                    ConnectionToHostResult_SuccessEventType::FAILURE);
+  VerifyProvisioningFailure(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_ProvisioningFailureEventType::OTHER);
+}
+
+TEST_F(HostConnectionMetricsLoggerTest,
+       RecordConnectionResultFailureTetheringTimeout_SetupRequired) {
+  metrics_logger_->RecordConnectionToHostResult(
+      HostConnectionMetricsLogger::ConnectionToHostResult::
+          CONNECTION_RESULT_FAILURE_TETHERING_TIMED_OUT_FIRST_TIME_SETUP_WAS_REQUIRED);
+
+  VerifyFailure_TetheringTimeout(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_FailureTetheringTimeoutEventType::
+              FIRST_TIME_SETUP_WAS_REQUIRED);
+  VerifyFailure(
+      HostConnectionMetricsLogger::ConnectionToHostResult_FailureEventType::
+          TETHERING_TIMED_OUT);
+  VerifySuccess(HostConnectionMetricsLogger::
+                    ConnectionToHostResult_SuccessEventType::FAILURE);
+  VerifyProvisioningFailure(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_ProvisioningFailureEventType::OTHER);
+}
+
+TEST_F(HostConnectionMetricsLoggerTest,
+       RecordConnectionResultFailureTetheringTimeout_SetupNotRequired) {
+  metrics_logger_->RecordConnectionToHostResult(
+      HostConnectionMetricsLogger::ConnectionToHostResult::
+          CONNECTION_RESULT_FAILURE_TETHERING_TIMED_OUT_FIRST_TIME_SETUP_WAS_NOT_REQUIRED);
+
+  VerifyFailure_TetheringTimeout(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_FailureTetheringTimeoutEventType::
+              FIRST_TIME_SETUP_WAS_NOT_REQUIRED);
+  VerifyFailure(
+      HostConnectionMetricsLogger::ConnectionToHostResult_FailureEventType::
+          TETHERING_TIMED_OUT);
+  VerifySuccess(HostConnectionMetricsLogger::
+                    ConnectionToHostResult_SuccessEventType::FAILURE);
+  VerifyProvisioningFailure(
+      HostConnectionMetricsLogger::
+          ConnectionToHostResult_ProvisioningFailureEventType::OTHER);
+}
+
+}  // namespace tether
+
+}  // namespace chromeos
