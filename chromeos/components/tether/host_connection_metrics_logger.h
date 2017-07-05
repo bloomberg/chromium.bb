0bca10edc2d2ec0c6d1ae076b3e40616b567d9ce
hansberry <hansberry@chromium.org>
2017-07-05 13:02:22 -0700

    Tether: Add metrics for host connection results, and a wrapper around recording them.
    BUG=672263
    
    Review-Url: https://codereview.chromium.org/2969503002
    Cr-Commit-Position: refs/heads/master@{#484337}
    
    Former-commit-id: 739d014309b133de81f42e777eb8435e75410035

---
+++ chromeos/components/tether/host_connection_metrics_logger.h
@@ -0,0 +1,141 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_COMPONENTS_TETHER_HOST_CONNECTION_METRICS_LOGGER_H_
+#define CHROMEOS_COMPONENTS_TETHER_HOST_CONNECTION_METRICS_LOGGER_H_
+
+#include "base/gtest_prod_util.h"
+#include "base/macros.h"
+
+namespace chromeos {
+
+namespace tether {
+
+// Wrapper around metrics reporting for host connection results. Clients are
+// expected to report the result of a host connection attempt once it has
+// concluded.
+class HostConnectionMetricsLogger {
+ public:
+  enum ConnectionToHostResult {
+    CONNECTION_RESULT_PROVISIONING_FAILED,
+    CONNECTION_RESULT_SUCCESS,
+    CONNECTION_RESULT_FAILURE_UNKNOWN_ERROR,
+    CONNECTION_RESULT_FAILURE_CLIENT_CONNECTION_TIMEOUT,
+    CONNECTION_RESULT_FAILURE_CLIENT_CONNECTION_CANCELED_BY_NEW_ATTEMPT,
+    CONNECTION_RESULT_FAILURE_TETHERING_TIMED_OUT_FIRST_TIME_SETUP_WAS_REQUIRED,
+    CONNECTION_RESULT_FAILURE_TETHERING_TIMED_OUT_FIRST_TIME_SETUP_WAS_NOT_REQUIRED
+  };
+
+  // Record the result of an attempted host connection.
+  virtual void RecordConnectionToHostResult(ConnectionToHostResult result);
+
+  HostConnectionMetricsLogger();
+  virtual ~HostConnectionMetricsLogger();
+
+ private:
+  friend class HostConnectionMetricsLoggerTest;
+  FRIEND_TEST_ALL_PREFIXES(HostConnectionMetricsLoggerTest,
+                           RecordConnectionResultProvisioningFailure);
+  FRIEND_TEST_ALL_PREFIXES(HostConnectionMetricsLoggerTest,
+                           RecordConnectionResultSuccess);
+  FRIEND_TEST_ALL_PREFIXES(HostConnectionMetricsLoggerTest,
+                           RecordConnectionResultFailure);
+  FRIEND_TEST_ALL_PREFIXES(
+      HostConnectionMetricsLoggerTest,
+      RecordConnectionResultFailureClientConnection_Timeout);
+  FRIEND_TEST_ALL_PREFIXES(
+      HostConnectionMetricsLoggerTest,
+      RecordConnectionResultFailureClientConnection_CanceledByNewAttempt);
+  FRIEND_TEST_ALL_PREFIXES(
+      HostConnectionMetricsLoggerTest,
+      RecordConnectionResultFailureTetheringTimeout_SetupRequired);
+  FRIEND_TEST_ALL_PREFIXES(
+      HostConnectionMetricsLoggerTest,
+      RecordConnectionResultFailureTetheringTimeout_SetupNotRequired);
+
+  // An Instant Tethering connection can fail for several different reasons.
+  // Though traditionally success and each failure case would be logged to a
+  // single enum, we have chosen to start at a top-level of view of simply
+  // "success vs failure", and then iteratively breaking down the failure count
+  // into its types (and possibly sub-types). Because of this cascading nature,
+  // when a failure sub-type occurs, the code path in question must report that
+  // sub-type as well as all the super-types above it. This would be cumbersome,
+  // and thus HostConnectionMetricsLogger exists to provide a simple API which
+  // handles all the other metrics that need to be reported.
+  //
+  // The most top-level metric is
+  // InstantTethering.ConnectionToHostResult.ProvisioningFailureRate. Its
+  // breakdown, and the breakdown of its subsquent metrics, is described in
+  // tools/metrics/histograms/histograms.xml.
+  enum ConnectionToHostResult_ProvisioningFailureEventType {
+    PROVISIONING_FAILED = 0,
+    OTHER = 1,
+    PROVISIONING_FAILURE_MAX
+  };
+
+  enum ConnectionToHostResult_SuccessEventType {
+    SUCCESS = 0,
+    FAILURE = 1,
+    SUCCESS_MAX
+  };
+
+  enum ConnectionToHostResult_FailureEventType {
+    UNKNOWN_ERROR = 0,
+    TETHERING_TIMED_OUT = 1,
+    CLIENT_CONNECTION_ERROR = 2,
+    FAILURE_MAX
+  };
+
+  enum ConnectionToHostResult_FailureClientConnectionEventType {
+    TIMEOUT = 0,
+    CANCELED_BY_NEW_ATTEMPT = 1,
+    FAILURE_CLIENT_CONNECTION_MAX
+  };
+
+  enum ConnectionToHostResult_FailureTetheringTimeoutEventType {
+    FIRST_TIME_SETUP_WAS_REQUIRED = 0,
+    FIRST_TIME_SETUP_WAS_NOT_REQUIRED = 1,
+    FAILURE_TETHERING_TIMEOUT_MAX
+  };
+
+  // Record if a host connection attempt never went through due to provisioning
+  // failure, or otherwise continued. Should only be publicly called with
+  // an argument of ConnectionToHostResult_ProvisioningFailureEventType::OTHER.
+  void RecordConnectionResultProvisioningFailure(
+      ConnectionToHostResult_ProvisioningFailureEventType event_type);
+
+  // Record if a host connection attempt succeeded or failed. Should only be
+  // publicly called with
+  // an argument of ConnectionToHostResult_SuccessEventType::SUCCESS. Failure is
+  // covered by the RecordConnectionResultFailure() method.
+  void RecordConnectionResultSuccess(
+      ConnectionToHostResult_SuccessEventType event_type);
+
+  // Record how a host connection attempt failed. Should only be
+  // publicly called with an argument of
+  // ConnectionToHostResult_FailureEventType::UNKNOWN_ERROR. Failure due to
+  // client error or tethering
+  // timeout is covered
+  // by theRecordConnectionResultFailureClientConnection() or
+  // RecordConnectionResultFailureTetheringTimeout() methods, respectively.
+  void RecordConnectionResultFailure(
+      ConnectionToHostResult_FailureEventType event_type);
+
+  // Record how a host connection attempt failed due to a client error.
+  void RecordConnectionResultFailureClientConnection(
+      ConnectionToHostResult_FailureClientConnectionEventType event_type);
+
+  // Record the conditions of when host connection attempt failed due to
+  // the host timing out during tethering.
+  void RecordConnectionResultFailureTetheringTimeout(
+      ConnectionToHostResult_FailureTetheringTimeoutEventType event_type);
+
+  DISALLOW_COPY_AND_ASSIGN(HostConnectionMetricsLogger);
+};
+
+}  // namespace tether
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_COMPONENTS_TETHER_HOST_CONNECTION_METRICS_LOGGER_H_
