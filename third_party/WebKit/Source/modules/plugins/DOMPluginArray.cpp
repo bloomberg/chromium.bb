4ae336491b4bc35177c434cd8afc0223cafa51d3
Lucas Furukawa Gadani <lfg@chromium.org>
2017-07-05 00:21:26 +0000

    Remove temporary debugging checks used to track bug 731239.
    Bug: 731239
    Change-Id: I9291b6e6bb22a7e5da52eb92740db5dae7a620ae
    Reviewed-on: https://chromium-review.googlesource.com/558770
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Lucas Gadani <lfg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484170}
    Former-commit-id: 431955525b0565a0be65ba3e9bb7072f41ec6108

--- third_party/WebKit/Source/modules/plugins/DOMPluginArray.cpp
+++ third_party/WebKit/Source/modules/plugins/DOMPluginArray.cpp
@@ -55,27 +55,6 @@ DOMPlugin* DOMPluginArray::item(unsigned index) {
   if (index >= dom_plugins_.size())
     return nullptr;
 
-  // TODO(lfg): Temporary to track down https://crbug.com/731239.
-  if (!GetPluginData()) {
-    LocalFrame* frame = GetFrame();
-    LocalFrameClient* client = GetFrame()->Client();
-    Settings* settings = GetFrame()->GetSettings();
-    ExecutionContext* context = GetExecutionContext();
-    Page* page = GetFrame()->GetPage();
-    bool allow_plugins =
-        frame->Loader().AllowPlugins(kNotAboutToInstantiatePlugin);
-
-    CHECK(false);
-
-    WTF::debug::Alias(frame);
-    WTF::debug::Alias(client);
-    WTF::debug::Alias(settings);
-    WTF::debug::Alias(context);
-    WTF::debug::Alias(page);
-    WTF::debug::Alias(&allow_plugins);
-  }
-  CHECK(main_frame_origin_->IsSameSchemeHostPort(GetPluginData()->Origin()));
-
   if (!dom_plugins_[index]) {
     dom_plugins_[index] =
         DOMPlugin::Create(GetFrame(), *GetPluginData()->Plugins()[index]);
@@ -133,8 +112,6 @@ void DOMPluginArray::UpdatePluginData() {
     dom_plugins_.clear();
     return;
   }
-
-  main_frame_origin_ = data->Origin();
 
   HeapVector<Member<DOMPlugin>> old_dom_plugins(std::move(dom_plugins_));
   dom_plugins_.clear();
