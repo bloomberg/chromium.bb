4beea65bfdd149ce80ddcfd7cc493b4d1af34be4
danielpark <danielpark@chromium.org>
2017-07-05 15:18:04 -0700

    Adding smarter resizing logic for the context menu on orientation changes
    * Renamed CONTEXT_MENU_MIN_PADDING_PX to mContextMenuMinimumPaddingPx
    
    * Grouped context_menu_min_vertical_padding and context_menu_min_side_padding into
    context_menu_min_padding.
    
    * Added more comments in TabularContextMenuViewPager#onMeasure
    
    * Made padding into a global variable called CONTEXT_MENU_MIN_PADDING_PX.
    
    * Renamed context_menu_height_padding to context_menu_min_vertical_padding with half
    the value, but multiplied all usages by 2 so the total values are the same.
    
    * Added context_menu_min_side_padding.
    
    * Rewrote sizing logic so width is the minimum of 500 dp or total width - 2 * side_padding
    
    * Added a max height to context menu row
    
    * Added a max height to the context menu so it no longer leaks over the edge.
    
    * The list view now scrolls when the context menu reaches max height.
    
    Previous issue link: https://codereview.chromium.org/2956943002/
    I had to recreate it after fixing the owner issue.
    
    BUG=731173
    
    Review-Url: https://codereview.chromium.org/2959853002
    Cr-Commit-Position: refs/heads/master@{#484364}
    
    Former-commit-id: c76fe2d15d81e2e125ad5efc57a70ab143870a6e

--- chrome/android/java/src/org/chromium/chrome/browser/contextmenu/TabularContextMenuViewPager.java
+++ chrome/android/java/src/org/chromium/chrome/browser/contextmenu/TabularContextMenuViewPager.java
@@ -16,7 +16,8 @@ import org.chromium.chrome.R;
  * pager.
  */
 public class TabularContextMenuViewPager extends ViewPager {
-    private static final double MAX_WIDTH_PROPORTION = 0.75;
+    private final int mContextMenuMinimumPaddingPx =
+            getResources().getDimensionPixelSize(R.dimen.context_menu_min_padding);
 
     public TabularContextMenuViewPager(Context context) {
         super(context);
@@ -38,13 +39,19 @@ public class TabularContextMenuViewPager extends ViewPager {
         // getCurrentItem() does not take into account the tab layout unlike getChildCount().
         int currentItemsIndex = getCurrentItem() + 1;
 
-        // TODO(injae): Fix sizing on orientation changes (crbug.com/731173)
-        int contextMenuWidth = (int) Math.min(
-                getResources().getDisplayMetrics().widthPixels * MAX_WIDTH_PROPORTION,
+        // The width of the context menu is defined so that it leaves space between itself and the
+        // screen's edges. It is also bounded to a max size to prevent the menu from stretching
+        // across a large display (e.g. a tablet screen).
+        int deviceWidthPx = getResources().getDisplayMetrics().widthPixels;
+        int contextMenuWidth = Math.min(deviceWidthPx - 2 * mContextMenuMinimumPaddingPx,
                 getResources().getDimensionPixelSize(R.dimen.context_menu_max_width));
 
         widthMeasureSpec = MeasureSpec.makeMeasureSpec(contextMenuWidth, MeasureSpec.EXACTLY);
 
+        // The height of the context menu is calculated as the sum of:
+        // 1. The tab bar's height, which is only visible when the context menu requires it
+        //    (i.e. an ImageLink is clicked)
+        // 2. The height of the View being displayed for the current tab.
         for (int i = 0; i < getChildCount(); i++) {
             View child = getChildAt(i);
 
@@ -61,7 +68,12 @@ public class TabularContextMenuViewPager extends ViewPager {
                 break;
             }
         }
+
+        // Cap the height of the context menu so that it fits on the screen without touching the
+        // screen's edges.
         int fullHeight = menuHeight + tabHeight;
+        int deviceHeightPx = getResources().getDisplayMetrics().heightPixels;
+        fullHeight = Math.min(fullHeight, deviceHeightPx - 2 * mContextMenuMinimumPaddingPx);
 
         heightMeasureSpec = MeasureSpec.makeMeasureSpec(fullHeight, MeasureSpec.EXACTLY);
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
