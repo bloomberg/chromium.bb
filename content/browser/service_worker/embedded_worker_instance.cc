f5255fe74368496ef5482fd956317bc6adad0141
Yuki Yamada <yukiy@google.com>
2017-07-06 03:11:27 +0000

    remove ServiceWorkerUtills::IsMojoForServiceWorkerEnabled()
    This method always returns true, and used only for DCHECK so it can be removed.
    
    Bug: 738959
    Change-Id: Ic76255ce1be93e875c9ea8c4e8c74fc029a52d12
    Reviewed-on: https://chromium-review.googlesource.com/558338
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Yuki Yamada <yukiy@google.com>
    Cr-Commit-Position: refs/heads/master@{#484451}
    Former-commit-id: 468c6ca853b5f75fc61897c87ee815c18605d407

--- content/browser/service_worker/embedded_worker_instance.cc
+++ content/browser/service_worker/embedded_worker_instance.cc
@@ -21,7 +21,6 @@
 #include "content/common/service_worker/embedded_worker_settings.h"
 #include "content/common/service_worker/embedded_worker_start_params.h"
 #include "content/common/service_worker/service_worker_types.h"
-#include "content/common/service_worker/service_worker_utils.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/render_process_host.h"
@@ -109,10 +108,8 @@ void CallDetach(EmbeddedWorkerInstance* instance) {
   // This could be called on the UI thread if |client_| still be valid when the
   // message loop on the UI thread gets destructed.
   // TODO(shimazu): Remove this after https://crbug.com/604762 is fixed
-  if (!BrowserThread::CurrentlyOn(BrowserThread::IO)) {
-    DCHECK(ServiceWorkerUtils::IsMojoForServiceWorkerEnabled());
+  if (!BrowserThread::CurrentlyOn(BrowserThread::IO))
     return;
-  }
   instance->Detach();
 }
 
