79287b59400e61ae779445f4679a94765446b7b9
Kent Tamura <tkent@chromium.org>
2017-07-06 08:37:03 +0000

    Remove ENABLE() macro.
    This macro requires an undefined-but-common behavior of C preprocessor,
    and is not used widely.
    
    Bug: 667238
    Change-Id: Ie6b500527f0ff4102becd9883ebcbf83a8406406
    Reviewed-on: https://chromium-review.googlesource.com/560932
    Reviewed-by: Yuta Kitamura <yutak@chromium.org>
    Commit-Queue: Yuta Kitamura <yutak@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484498}
    Former-commit-id: 518be28a99692aca461ce04d92b14baa396cfadc

--- third_party/WebKit/Source/core/page/ChromeClient.h
+++ third_party/WebKit/Source/core/page/ChromeClient.h
@@ -220,12 +220,12 @@ class CORE_EXPORT ChromeClient : public PlatformChromeClient {
                                          const Color&) = 0;
 
   // This function is used for:
-  //  - Mandatory date/time choosers if !ENABLE(INPUT_MULTIPLE_FIELDS_UI)
+  //  - Mandatory date/time choosers if InputMultipleFieldsUI flag is not set
   //  - Date/time choosers for types for which
-  //    LayoutTheme::supportsCalendarPicker returns true, if
-  //    ENABLE(INPUT_MULTIPLE_FIELDS_UI)
+  //    LayoutTheme::SupportsCalendarPicker returns true, if
+  //    InputMultipleFieldsUI flag is set
   //  - <datalist> UI for date/time input types regardless of
-  //    ENABLE(INPUT_MULTIPLE_FIELDS_UI)
+  //    InputMultipleFieldsUI flag
   virtual DateTimeChooser* OpenDateTimeChooser(
       DateTimeChooserClient*,
       const DateTimeChooserParameters&) = 0;
