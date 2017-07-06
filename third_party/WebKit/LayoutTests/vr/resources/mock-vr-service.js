f76636beb12347fe4d8854319e336344bc26f7dd
Reilly Grant <reillyg@chromium.org>
2017-07-06 23:35:46 +0000

    Switch the WebVR LayoutTests to the new Mojo JS bindings
    This change switches the WebVR Mojo interface JS bindings to the new
    style and updates the LayoutTests accordingly. Mojoms that this
    interface depends on are switched to generating both styles of bindings
    while some users have not yet been updated.
    
    Bug: 699569
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: If9171cf4b764a39ebfeb00e21b2c039e7f3e6812
    Reviewed-on: https://chromium-review.googlesource.com/560578
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Oliver Chang (OOO until July 10) <ochang@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484766}
    Former-commit-id: e7b1af385b62029ed042d59e93112ea9f7fed8ef

--- third_party/WebKit/LayoutTests/vr/resources/mock-vr-service.js
+++ third_party/WebKit/LayoutTests/vr/resources/mock-vr-service.js
@@ -1,181 +1,169 @@
 'use strict';
 
-let mockVRService = loadMojoModules(
-    'mockVRService',
-    ['mojo/public/js/bindings',
-     'device/vr/vr_service.mojom',
-    ]).then(mojo => {
-  let [bindings, vr_service] = mojo.modules;
+class MockVRDisplay {
+  constructor(displayInfo, service) {
+    this.bindingSet_ = new mojo.BindingSet(device.mojom.VRDisplay);
+    this.displayClient_ = new device.mojom.VRDisplayClientPtr();
+    this.displayInfo_ = displayInfo;
+    this.service_ = service;
+    this.presentation_provider_ = new MockVRPresentationProvider();
 
-  class MockVRDisplay {
-    constructor(interfaceProvider, displayInfo, service) {
-      this.bindingSet_ = new bindings.BindingSet(vr_service.VRDisplay);
-      this.displayClient_ = new vr_service.VRDisplayClientPtr();
-      this.displayInfo_ = displayInfo;
-      this.service_ = service;
-      this.presentation_provider_ = new MockVRPresentationProvider();
-
-      interfaceProvider.addInterfaceOverrideForTesting(
-          vr_service.VRDisplay.name,
-          handle => this.bindingSet_.addBinding(this, handle));
-
-      if (service.client_) {
-        this.notifyClientOfDisplay();
-      }
-    }
-
-    requestPresent(secureOrigin, submitFrameClient, request) {
-      this.presentation_provider_.bind(submitFrameClient, request);
-      return Promise.resolve({success: true});
-    }
-
-    setPose(pose) {
-      if (pose == null) {
-        this.presentation_provider_.pose_ = null;
-      } else {
-        this.presentation_provider_.initPose();
-        this.presentation_provider_.fillPose(pose);
-      }
-    }
-
-    getNextMagicWindowPose() {
-      return Promise.resolve({
-        pose: this.presentation_provider_.pose_,
-      });
-    }
-
-    forceActivate(reason) {
-      this.displayClient_.onActivate(reason);
-    }
-
-    notifyClientOfDisplay() {
-      let displayPtr = new vr_service.VRDisplayPtr();
-      let request = bindings.makeRequest(displayPtr);
-      let binding = new bindings.Binding(
-          vr_service.VRDisplay,
-          this, request);
-      let clientRequest = bindings.makeRequest(this.displayClient_);
-      this.service_.client_.onDisplayConnected(displayPtr, clientRequest,
-          this.displayInfo_);
+    if (service.client_) {
+      this.notifyClientOfDisplay();
     }
   }
 
-  class MockVRPresentationProvider {
-    constructor() {
-      this.binding_ = new bindings.Binding(vr_service.VRPresentationProvider,
-          this);
-      this.pose_ = null;
-    }
-    bind(client, request) {
-      this.submitFrameClient_ = client;
-      this.binding_.close();
-      this.binding_.bind(request);
-    }
-    submitFrame(frameId, mailboxHolder) {
-      // Trigger the submit completion callbacks here. WARNING: The
-      // Javascript-based mojo mocks are *not* re-entrant.  In the current
-      // default implementation, Javascript calls display.submitFrame, and the
-      // corresponding C++ code uses a reentrant mojo call that waits for
-      // onSubmitFrameTransferred to indicate completion. This never finishes
-      // when using the mocks since the incoming calls are queued until the
-      // current execution context finishes. As a workaround, use the alternate
-      // "WebVRExperimentalRendering" mode which works without reentrant calls,
-      // the code only checks for completion on the *next* frame, see the
-      // corresponding option setting in RuntimeEnabledFeatures.json5.
-      this.submitFrameClient_.onSubmitFrameTransferred();
-      this.submitFrameClient_.onSubmitFrameRendered();
-    }
-    getVSync() {
-      if (this.pose_) {
-        this.pose_.poseIndex++;
-      }
+  requestPresent(secureOrigin, submitFrameClient, request) {
+    this.presentation_provider_.bind(submitFrameClient, request);
+    return Promise.resolve({success: true});
+  }
 
-      // Convert current document time to monotonic time.
-      var now = window.performance.now() / 1000.0;
-      var diff =
-          now - window.internals.monotonicTimeToZeroBasedDocumentTime(now);
-      now += diff;
-      now *= 1000000;
-
-      let retval = Promise.resolve({
-        pose: this.pose_,
-        time: {
-          microseconds: now,
-        },
-        frame_id: 0,
-        status: vr_service.VRPresentationProvider.VSyncStatus.SUCCESS,
-      });
-
-      return retval;
-    }
-    initPose() {
-      this.pose_ = {
-        orientation: null,
-        position: null,
-        angularVelocity: null,
-        linearVelocity: null,
-        angularAcceleration: null,
-        linearAcceleration: null,
-        poseIndex: 0
-      };
-    }
-    fillPose(pose) {
-      for (var field in pose) {
-        if (this.pose_.hasOwnProperty(field)) {
-          this.pose_[field] = pose[field];
-        }
-      }
+  setPose(pose) {
+    if (pose == null) {
+      this.presentation_provider_.pose_ = null;
+    } else {
+      this.presentation_provider_.initPose();
+      this.presentation_provider_.fillPose(pose);
     }
   }
 
-  class MockVRService {
-    constructor(interfaceProvider) {
-      this.bindingSet_ = new bindings.BindingSet(vr_service.VRService);
-      this.mockVRDisplays_ = [];
+  getNextMagicWindowPose() {
+    return Promise.resolve({
+      pose: this.presentation_provider_.pose_,
+    });
+  }
 
-      interfaceProvider.addInterfaceOverrideForTesting(
-          vr_service.VRService.name,
-          handle => this.bindingSet_.addBinding(this, handle));
+  forceActivate(reason) {
+    this.displayClient_.onActivate(reason);
+  }
+
+  notifyClientOfDisplay() {
+    let displayPtr = new device.mojom.VRDisplayPtr();
+    let request = mojo.makeRequest(displayPtr);
+    let binding = new mojo.Binding(device.mojom.VRDisplay, this, request);
+    let clientRequest = mojo.makeRequest(this.displayClient_);
+    this.service_.client_.onDisplayConnected(displayPtr, clientRequest,
+        this.displayInfo_);
+  }
+}
+
+class MockVRPresentationProvider {
+  constructor() {
+    this.binding_ = new mojo.Binding(device.mojom.VRPresentationProvider, this);
+    this.pose_ = null;
+  }
+
+  bind(client, request) {
+    this.submitFrameClient_ = client;
+    this.binding_.close();
+    this.binding_.bind(request);
+  }
+
+  submitFrame(frameId, mailboxHolder) {
+    // Trigger the submit completion callbacks here. WARNING: The
+    // Javascript-based mojo mocks are *not* re-entrant.  In the current
+    // default implementation, Javascript calls display.submitFrame, and the
+    // corresponding C++ code uses a reentrant mojo call that waits for
+    // onSubmitFrameTransferred to indicate completion. This never finishes
+    // when using the mocks since the incoming calls are queued until the
+    // current execution context finishes. As a workaround, use the alternate
+    // "WebVRExperimentalRendering" mode which works without reentrant calls,
+    // the code only checks for completion on the *next* frame, see the
+    // corresponding option setting in RuntimeEnabledFeatures.json5.
+    this.submitFrameClient_.onSubmitFrameTransferred();
+    this.submitFrameClient_.onSubmitFrameRendered();
+  }
+
+  getVSync() {
+    if (this.pose_) {
+      this.pose_.poseIndex++;
     }
 
-    setVRDisplays(displays) {
-      this.mockVRDisplays_ = [];
-      for (let i = 0; i < displays.length; i++) {
-        displays[i].index = i;
-        this.mockVRDisplays_.push(new MockVRDisplay(mojo.frameInterfaces,
-              displays[i], this));
+    // Convert current document time to monotonic time.
+    var now = window.performance.now() / 1000.0;
+    var diff =
+        now - window.internals.monotonicTimeToZeroBasedDocumentTime(now);
+    now += diff;
+    now *= 1000000;
+
+    let retval = Promise.resolve({
+      pose: this.pose_,
+      time: {
+        microseconds: now,
+      },
+      frameId: 0,
+      status: device.mojom.VRPresentationProvider.VSyncStatus.SUCCESS,
+    });
+
+    return retval;
+  }
+
+  initPose() {
+    this.pose_ = {
+      orientation: null,
+      position: null,
+      angularVelocity: null,
+      linearVelocity: null,
+      angularAcceleration: null,
+      linearAcceleration: null,
+      poseIndex: 0
+    };
+  }
+
+  fillPose(pose) {
+    for (var field in pose) {
+      if (this.pose_.hasOwnProperty(field)) {
+        this.pose_[field] = pose[field];
       }
     }
+  }
+}
 
-    addVRDisplay(display) {
-      if (this.mockVRDisplays_.length) {
-        display.index =
-            this.mockVRDisplays_[this.mockVRDisplays_.length - 1] + 1;
-      } else {
-        display.index = 0;
-      }
-      this.mockVRDisplays_.push(new MockVRDisplay(mojo.frameInterfaces,
-            display, this));
-    }
+class MockVRService {
+  constructor() {
+    this.bindingSet_ = new mojo.BindingSet(device.mojom.VRService);
+    this.mockVRDisplays_ = [];
 
-    setClient(client) {
-      this.client_ = client;
-      for (let i = 0; i < this.mockVRDisplays_.length; i++) {
-        this.mockVRDisplays_[i].notifyClientOfDisplay();
-      }
+    this.interceptor_ =
+        new MojoInterfaceInterceptor(device.mojom.VRService.name);
+    this.interceptor_.oninterfacerequest =
+        e => this.bindingSet_.addBinding(this, e.handle);
+    this.interceptor_.start();
+  }
 
-      let device_number = this.mockVRDisplays_.length;
-      return Promise.resolve({number_of_connected_devices: device_number});
+  setVRDisplays(displays) {
+    this.mockVRDisplays_ = [];
+    for (let i = 0; i < displays.length; i++) {
+      displays[i].index = i;
+      this.mockVRDisplays_.push(new MockVRDisplay(displays[i], this));
     }
   }
 
-  return new MockVRService(mojo.frameInterfaces);
-});
+  addVRDisplay(display) {
+    if (this.mockVRDisplays_.length) {
+      display.index =
+          this.mockVRDisplays_[this.mockVRDisplays_.length - 1] + 1;
+    } else {
+      display.index = 0;
+    }
+    this.mockVRDisplays_.push(new MockVRDisplay(display, this));
+  }
+
+  setClient(client) {
+    this.client_ = client;
+    for (let i = 0; i < this.mockVRDisplays_.length; i++) {
+      this.mockVRDisplays_[i].notifyClientOfDisplay();
+    }
+
+    let device_number = this.mockVRDisplays_.length;
+    return Promise.resolve({numberOfConnectedDevices: device_number});
+  }
+}
+
+let mockVRService = new MockVRService(mojo.frameInterfaces);
 
 function vr_test(func, vrDisplays, name, properties) {
-  mockVRService.then( (service) => {
-    service.setVRDisplays(vrDisplays);
-    let t = async_test(name, properties);
-    func(t, service);
-  });
+  mockVRService.setVRDisplays(vrDisplays);
+  let t = async_test(name, properties);
+  func(t, mockVRService);
 }
