271011a5945a3809119ea2db2771647a67e89ea4
jinho.bang <jinho.bang@samsung.com>
2017-07-06 17:19:44 +0000

    PaymentHandler: Add Interfaces for CanMakePaymentEvent.
    This is an initial implementation of CanMakePaymentEvent in WebIDL level.
    
    Related spec change:
      https://github.com/w3c/payment-handler/pull/170
    
    See the other CLs in this series:
      [1/4] This patch.
      [2/4] https://chromium-review.googlesource.com/c/558888
      [3/4] https://chromium-review.googlesource.com/c/558889
      [4/4] https://chromium-review.googlesource.com/c/558890
    
    Bug: 736745
    Change-Id: I4fbb83d5c7ee014bfe773e4b246d1bf56e4dd0f8
    Reviewed-on: https://chromium-review.googlesource.com/558887
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484646}
    Former-commit-id: 871c0a37b06b2c7ef0ba708eebf62d06d32d7d9e

---
+++ third_party/WebKit/Source/modules/payments/CanMakePaymentEvent.cpp
@@ -0,0 +1,86 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/payments/CanMakePaymentEvent.h"
+
+#include "bindings/core/v8/ScriptPromiseResolver.h"
+#include "core/dom/DOMException.h"
+#include "core/workers/WorkerGlobalScope.h"
+#include "core/workers/WorkerLocation.h"
+#include "modules/serviceworkers/RespondWithObserver.h"
+#include "modules/serviceworkers/ServiceWorkerGlobalScopeClient.h"
+#include "modules/serviceworkers/ServiceWorkerWindowClientCallback.h"
+#include "platform/bindings/ScriptState.h"
+#include "platform/wtf/PtrUtil.h"
+#include "platform/wtf/text/AtomicString.h"
+
+namespace blink {
+
+CanMakePaymentEvent* CanMakePaymentEvent::Create(
+    const AtomicString& type,
+    const CanMakePaymentEventInit& initializer) {
+  return new CanMakePaymentEvent(type, initializer, nullptr, nullptr);
+}
+
+CanMakePaymentEvent* CanMakePaymentEvent::Create(
+    const AtomicString& type,
+    const CanMakePaymentEventInit& initializer,
+    RespondWithObserver* respond_with_observer,
+    WaitUntilObserver* wait_until_observer) {
+  return new CanMakePaymentEvent(type, initializer, respond_with_observer,
+                                 wait_until_observer);
+}
+
+CanMakePaymentEvent::~CanMakePaymentEvent() {}
+
+const AtomicString& CanMakePaymentEvent::InterfaceName() const {
+  return EventNames::CanMakePaymentEvent;
+}
+
+const String& CanMakePaymentEvent::topLevelOrigin() const {
+  return top_level_origin_;
+}
+
+const String& CanMakePaymentEvent::paymentRequestOrigin() const {
+  return payment_request_origin_;
+}
+
+const HeapVector<PaymentMethodData>& CanMakePaymentEvent::methodData() const {
+  return method_data_;
+}
+
+const HeapVector<PaymentDetailsModifier>& CanMakePaymentEvent::modifiers()
+    const {
+  return modifiers_;
+}
+
+void CanMakePaymentEvent::respondWith(ScriptState* script_state,
+                                      ScriptPromise script_promise,
+                                      ExceptionState& exception_state) {
+  stopImmediatePropagation();
+  if (observer_) {
+    observer_->RespondWith(script_state, script_promise, exception_state);
+  }
+}
+
+DEFINE_TRACE(CanMakePaymentEvent) {
+  visitor->Trace(method_data_);
+  visitor->Trace(modifiers_);
+  visitor->Trace(observer_);
+  ExtendableEvent::Trace(visitor);
+}
+
+CanMakePaymentEvent::CanMakePaymentEvent(
+    const AtomicString& type,
+    const CanMakePaymentEventInit& initializer,
+    RespondWithObserver* respond_with_observer,
+    WaitUntilObserver* wait_until_observer)
+    : ExtendableEvent(type, initializer, wait_until_observer),
+      top_level_origin_(initializer.topLevelOrigin()),
+      payment_request_origin_(initializer.paymentRequestOrigin()),
+      method_data_(std::move(initializer.methodData())),
+      modifiers_(initializer.modifiers()),
+      observer_(respond_with_observer) {}
+
+}  // namespace blink
