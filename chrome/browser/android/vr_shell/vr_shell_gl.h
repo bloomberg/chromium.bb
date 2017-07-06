36f0b272d072d8f1366fb21798dd9658ca7735a7
Tibor Goldschwendt <tiborg@chromium.org>
2017-07-06 18:38:10 +0000

    Move UI element input handling into UiInputManager.
    Change-Id: I163ff956a0929f2055a753940f3e7a435411f6f1
    Reviewed-on: https://chromium-review.googlesource.com/558387
    Reviewed-by: Michael Thiessen <mthiesse@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Commit-Queue: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484687}
    Former-commit-id: 4772c8e6b053dfb70cfc21db41ca46b07eb31027

--- chrome/browser/android/vr_shell/vr_shell_gl.h
+++ chrome/browser/android/vr_shell/vr_shell_gl.h
@@ -5,6 +5,7 @@
 #ifndef CHROME_BROWSER_ANDROID_VR_SHELL_VR_SHELL_GL_H_
 #define CHROME_BROWSER_ANDROID_VR_SHELL_VR_SHELL_GL_H_
 
+#include <chrome/browser/android/vr_shell/ui_input_manager.h>
 #include <memory>
 #include <queue>
 #include <utility>
@@ -66,7 +67,8 @@ struct WebVrBounds {
 
 // This class manages all GLThread owned objects and GL rendering for VrShell.
 // It is not threadsafe and must only be used on the GL thread.
-class VrShellGl : public device::mojom::VRPresentationProvider {
+class VrShellGl : public device::mojom::VRPresentationProvider,
+                  public UiInputManagerDelegate {
  public:
   VrShellGl(GlBrowserInterface* browser,
             gvr_context* gvr_api,
@@ -140,40 +142,30 @@ class VrShellGl : public device::mojom::VRPresentationProvider {
 
   void UpdateController(const gfx::Vector3dF& head_direction);
   void HandleWebVrCompatibilityClick();
-  void SendFlingCancel(GestureList& gesture_list);
-  void SendScrollEnd(GestureList& gesture_list);
-  bool SendScrollBegin(UiElement* target, GestureList& gesture_list);
-  void SendScrollUpdate(GestureList& gesture_list);
-  void SendHoverLeave(UiElement* target);
-  bool SendHoverEnter(UiElement* target,
-                      const gfx::PointF& target_point,
-                      const gfx::Point& local_point_pixels);
-  void SendHoverMove(const gfx::PointF& target_point,
-                     const gfx::Point& local_point_pixels);
-  void SendButtonDown(UiElement* target,
-                      const gfx::PointF& target_point,
-                      const gfx::Point& local_point_pixels);
-  bool SendButtonUp(UiElement* target,
-                    const gfx::PointF& target_point,
-                    const gfx::Point& local_point_pixels);
-  void SendTap(UiElement* target,
-               const gfx::PointF& target_point,
-               const gfx::Point& local_point_pixels);
   std::unique_ptr<blink::WebMouseEvent> MakeMouseEvent(
       blink::WebInputEvent::Type type,
-      const gfx::Point& location);
+      const gfx::PointF& normalized_web_content_location);
+  void UpdateGesture(const gfx::PointF& normalized_content_hit_point,
+                     blink::WebGestureEvent& gesture);
+
+  // UiInputManagerDelegate.
+  void OnContentEnter(const gfx::PointF& normalized_hit_point) override;
+  void OnContentLeave() override;
+  void OnContentMove(const gfx::PointF& normalized_hit_point) override;
+  void OnContentDown(const gfx::PointF& normalized_hit_point) override;
+  void OnContentUp(const gfx::PointF& normalized_hit_point) override;
+  void OnContentFlingBegin(std::unique_ptr<blink::WebGestureEvent> gesture,
+                           const gfx::PointF& normalized_hit_point) override;
+  void OnContentFlingCancel(std::unique_ptr<blink::WebGestureEvent> gesture,
+                            const gfx::PointF& normalized_hit_point) override;
+  void OnContentScrollBegin(std::unique_ptr<blink::WebGestureEvent> gesture,
+                            const gfx::PointF& normalized_hit_point) override;
+  void OnContentScrollUpdate(std::unique_ptr<blink::WebGestureEvent> gesture,
+                             const gfx::PointF& normalized_hit_point) override;
+  void OnContentScrollEnd(std::unique_ptr<blink::WebGestureEvent> gesture,
+                          const gfx::PointF& normalized_hit_point) override;
+
   void SendImmediateExitRequestIfNecessary();
-  void GetVisualTargetElement(const gfx::Vector3dF& controller_direction,
-                              gfx::Vector3dF& eye_to_target,
-                              gfx::Point3F& target_point,
-                              UiElement** target_element,
-                              gfx::PointF& target_local_point) const;
-  bool GetTargetLocalPoint(const gfx::Vector3dF& eye_to_target,
-                           const UiElement& element,
-                           float max_distance_to_plane,
-                           gfx::PointF& target_local_point,
-                           gfx::Point3F& target_point,
-                           float& distance_to_plane) const;
   void HandleControllerInput(const gfx::Vector3dF& head_direction);
   void HandleControllerAppButtonActivity(
       const gfx::Vector3dF& controller_direction);
@@ -240,18 +232,7 @@ class VrShellGl : public device::mojom::VRPresentationProvider {
   gfx::Quaternion controller_quat_;
 
   gfx::Point3F target_point_;
-
-  // TODO(mthiesse): We need to handle elements being removed, and update this
-  // state appropriately.
   UiElement* reticle_render_target_ = nullptr;
-  UiElement* hover_target_ = nullptr;
-  // TODO(mthiesse): We shouldn't have a fling target. Elements should fling
-  // independently and we should only cancel flings on the relevant element
-  // when we do cancel flings.
-  UiElement* fling_target_ = nullptr;
-  UiElement* input_locked_element_ = nullptr;
-  bool in_scroll_ = false;
-  bool in_click_ = false;
 
   int content_tex_css_width_ = 0;
   int content_tex_css_height_ = 0;
@@ -299,6 +280,8 @@ class VrShellGl : public device::mojom::VRPresentationProvider {
   std::unique_ptr<SlidingAverage> webvr_render_time_;
 
   gfx::Point3F pointer_start_;
+
+  std::unique_ptr<UiInputManager> input_manager_;
 
   base::WeakPtrFactory<VrShellGl> weak_ptr_factory_;
 
