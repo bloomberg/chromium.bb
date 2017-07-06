eaf594dbf049e482092f8769eb67cf98d390886b
Ehsan Chiniforooshan <chiniforooshan@chromium.org>
2017-07-06 17:35:51 +0000

    tracing: reland the coordinator implementation
    The original CL (crrev.com/3a1994e) was reverted in crrev.com/3a8e71c,
    because the tests were failing on Win7:
    
    https://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%281%29/builds/68107
    
    I could not reproduce the failure on a Win7 machine. I have some
    speculative fixes here and a little bit more logging so that if the
    tests fail again, maybe I have a little bit more information.
    
    Summary of changes:
    - The tracing service clears the callback before running it. This way,
      the callback can call service methods successfully. Before, we would
      clear the callback after the callback was finished and so we could
      not call service methods during the callback because the service
      would think the previous call is not finalized yet.
    
    - StopAndFlush* tests check if flushing is already done before running
      the RunLoop. This is for the hypothetical case that mojo calls do
      not generate tasks and everything is running directly. Then, by the
      time StopAndFlush is finished the events are already flushed and the
      task queue is empty. So, running the RunLoop will time out.
    
    - RequestBufferUsage verifies that all disconnect closures are indeed
      cleared up after the RunLoop quits. I added this so that if the test
      fails again at least I know whether RequestBufferUsage was actually
      finalized or there is a pending disconnect closure for some reason.
    
    Original CL description:
    
    The coordinator is the main part of the tracing service. It provides
    an API for trace collection to be used by the tracing UI, devtools,
    etc. It coordinates tracing agents through the agent registry.
    
    A prototype of the final product (a little bit stale and incomplete):
    https://codereview.chromium.org/2833873003/
    
    Issuing clock sync markers is not implemented in this CL. I think it
    deserves to be sent and reviewed in a separate CL.
    
    Design doc (use @chromium.org account):
    https://docs.google.com/document/d/1osLqctK_rTiioKFOG9wDLkrCQ9DLJZmm4j-S335u-vM
    
    BUG=640235
    
    Change-Id: I5904946171851da0a90313c3a8164134fb275c01
    Reviewed-on: https://chromium-review.googlesource.com/541556
    Commit-Queue: Ehsan Chiniforooshan <chiniforooshan@chromium.org>
    Reviewed-by: Ken Buchanan <kenrb@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484653}
    Former-commit-id: 425a4c83df470ab67739d7766f2bf09c7fe9b58c

--- services/resource_coordinator/tracing/recorder.h
+++ services/resource_coordinator/tracing/recorder.h
@@ -9,7 +9,6 @@
 #include "base/logging.h"
 #include "base/macros.h"
 #include "base/memory/ref_counted.h"
-#include "base/memory/weak_ptr.h"
 #include "base/sequenced_task_runner.h"
 #include "base/values.h"
 #include "mojo/public/cpp/bindings/binding.h"
@@ -76,7 +75,6 @@ class Recorder : public mojom::Recorder {
   // final stream is done on a background thread.
   scoped_refptr<base::SequencedTaskRunner> background_task_runner_;
   mojo::Binding<mojom::Recorder> binding_;
-  base::WeakPtrFactory<Recorder> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(Recorder);
 };
