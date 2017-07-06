0803ae261b952c5b95084b08eb9a6b28e7b4179c
nrpeter <nrpeter@google.com>
2017-07-06 13:54:41 -0700

    Don't display "Allow access to file URLs" when the request will be denied.
    Extensions force installed by admin policy aren't modifiable by the user.
    This means when a user on chrome://extensions tries to check the
    "Allow access to file URLs" checkbox on a force installed extension the
    change is denied.
    
    An error isn't displayed to the user, and the checkbox stays checked. The
    user then thinks their change was applied successfully until they refresh
    the chrome://extensions page in which case the setting seems to disappear.
    
    This change hides the "Allow access to file URLs" for extensions the user
    can't modify.
    
    BUG=173640
    
    Review-Url: https://codereview.chromium.org/2949973003
    Cr-Commit-Position: refs/heads/master@{#484739}
    
    Former-commit-id: a15e92ad1b93e1cef860f7a2f4247f110277e946

--- chrome/browser/extensions/api/developer_private/extension_info_generator_unittest.cc
+++ chrome/browser/extensions/api/developer_private/extension_info_generator_unittest.cc
@@ -121,7 +121,8 @@ class ExtensionInfoGeneratorUnitTest : public ExtensionServiceTestBase {
 
   const scoped_refptr<const Extension> CreateExtension(
       const std::string& name,
-      std::unique_ptr<base::ListValue> permissions) {
+      std::unique_ptr<base::ListValue> permissions,
+      Manifest::Location location) {
     const std::string kId = crx_file::id_util::GenerateId(name);
     scoped_refptr<const Extension> extension =
         ExtensionBuilder()
@@ -132,7 +133,7 @@ class ExtensionInfoGeneratorUnitTest : public ExtensionServiceTestBase {
                              .Set("version", "1.0.0")
                              .Set("permissions", std::move(permissions))
                              .Build())
-            .SetLocation(Manifest::INTERNAL)
+            .SetLocation(location)
             .SetID(kId)
             .Build();
 
@@ -383,9 +384,10 @@ TEST_F(ExtensionInfoGeneratorUnitTest, ExtensionInfoRunOnAllUrls) {
                                         true));
   // Two extensions - one with all urls, one without.
   scoped_refptr<const Extension> all_urls_extension = CreateExtension(
-      "all_urls", ListBuilder().Append(kAllHostsPermission).Build());
+      "all_urls", ListBuilder().Append(kAllHostsPermission).Build(),
+      Manifest::INTERNAL);
   scoped_refptr<const Extension> no_urls_extension =
-      CreateExtension("no urls", ListBuilder().Build());
+      CreateExtension("no urls", ListBuilder().Build(), Manifest::INTERNAL);
 
   std::unique_ptr<developer::ExtensionInfo> info =
       GenerateExtensionInfo(all_urls_extension->id());
@@ -431,7 +433,8 @@ TEST_F(ExtensionInfoGeneratorUnitTest, ExtensionInfoRunOnAllUrls) {
 
   // Load another extension with all urls (so permissions get re-init'd).
   all_urls_extension = CreateExtension(
-      "all_urls_II", ListBuilder().Append(kAllHostsPermission).Build());
+      "all_urls_II", ListBuilder().Append(kAllHostsPermission).Build(),
+      Manifest::INTERNAL);
 
   // Even though the extension has all_urls permission, the checkbox shouldn't
   // show up without the switch.
@@ -440,12 +443,30 @@ TEST_F(ExtensionInfoGeneratorUnitTest, ExtensionInfoRunOnAllUrls) {
   EXPECT_TRUE(info->run_on_all_urls.is_active);
 }
 
+// Test that file:// access checkbox does not show up when the user can't
+// modify an extension's settings. https://crbug.com/173640.
+TEST_F(ExtensionInfoGeneratorUnitTest, ExtensionInfoLockedAllUrls) {
+  // Force installed extensions aren't user modifyable.
+  scoped_refptr<const Extension> locked_extension =
+      CreateExtension("locked", ListBuilder().Append("file://*/*").Build(),
+                      Manifest::EXTERNAL_POLICY_DOWNLOAD);
+
+  std::unique_ptr<developer::ExtensionInfo> info =
+      GenerateExtensionInfo(locked_extension->id());
+
+  // Extension wants file:// access but the checkbox will not appear
+  // in chrome://extensions.
+  EXPECT_TRUE(locked_extension->wants_file_access());
+  EXPECT_FALSE(info->file_access.is_enabled);
+  EXPECT_FALSE(info->file_access.is_active);
+}
+
 // Tests that blacklisted extensions are returned by the ExtensionInfoGenerator.
 TEST_F(ExtensionInfoGeneratorUnitTest, Blacklisted) {
-  const scoped_refptr<const Extension> extension1 =
-      CreateExtension("test1", base::WrapUnique(new base::ListValue()));
-  const scoped_refptr<const Extension> extension2 =
-      CreateExtension("test2", base::WrapUnique(new base::ListValue()));
+  const scoped_refptr<const Extension> extension1 = CreateExtension(
+      "test1", base::WrapUnique(new base::ListValue()), Manifest::INTERNAL);
+  const scoped_refptr<const Extension> extension2 = CreateExtension(
+      "test2", base::WrapUnique(new base::ListValue()), Manifest::INTERNAL);
 
   std::string id1 = extension1->id();
   std::string id2 = extension2->id();
