d7acf402c3ed9a7df8e61f8ef9fc425a2557f9d6
Quinten Yearsley <qyearsley@google.com>
2017-07-06 00:19:57 +0000

    Add method pr_for_chromium_commit to WPTGitHub, and use it in importer
    Reason:
    While the is_exportable function was using both Change Id and commit
    position to find the PR, test_importer was using commit position
    to find the PR. Because of https://crbug.com/737178, that sometimes
    returns the wrong PR number, which caused some confusion.
    
    This CL extracts the logic used to get the PR for a commit to avoid
    repetition, and makes it so it's used in both is_exportable and the
    importer.
    
    Bug: 727923
    Change-Id: I9e5dfbdc2f8805693e1aa01760cb64951a6dd908
    Reviewed-on: https://chromium-review.googlesource.com/558200
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Jeff Carpenter <jeffcarp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484402}
    Former-commit-id: 995f3e6f520b491f9489a8842e1b3beb592a993d

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/common.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/common.py
@@ -94,11 +94,7 @@ def is_exportable(chromium_commit, local_wpt, wpt_github):
     # If there's a corresponding closed PR, then this commit should not
     # be considered exportable; the PR might have been merged and reverted,
     # or it might have been closed manually without merging.
-    pull_request = wpt_github.pr_with_change_id(chromium_commit.change_id())
-    if not pull_request:
-        # The Change ID can't be used for commits made via Rietveld.
-        # TODO(qyearsley): Remove this fallback after Gerrit migration.
-        pull_request = wpt_github.pr_with_position(chromium_commit.position)
+    pull_request = wpt_github.pr_for_chromium_commit(chromium_commit)
     if pull_request and pull_request.state == 'closed':
         return False
     return True
