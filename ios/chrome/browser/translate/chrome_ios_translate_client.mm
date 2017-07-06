8aa3418631a5f1738c15dcc89c4227dbe5860e21
renjieliu <renjieliu@chromium.org>
2017-07-05 17:29:21 -0700

    Implement ios language detection logging.
    BUG=722679
    
    Review-Url: https://codereview.chromium.org/2962783003
    Cr-Commit-Position: refs/heads/master@{#484417}
    
    Former-commit-id: 4a1c9af8a6715969608c575157685203742d777d

--- ios/chrome/browser/translate/chrome_ios_translate_client.mm
+++ ios/chrome/browser/translate/chrome_ios_translate_client.mm
@@ -23,6 +23,7 @@
 #include "components/translate/core/browser/translate_prefs.h"
 #include "components/translate/core/browser/translate_step.h"
 #include "components/translate/core/common/language_detection_details.h"
+#include "components/translate/core/common/language_detection_logging_helper.h"
 #include "components/translate/core/common/translation_logging_helper.h"
 #include "ios/chrome/browser/browser_state/chrome_browser_state.h"
 #include "ios/chrome/browser/infobars/infobar.h"
@@ -193,7 +194,25 @@ int ChromeIOSTranslateClient::GetInfobarIconID() const {
 
 void ChromeIOSTranslateClient::RecordLanguageDetectionEvent(
     const translate::LanguageDetectionDetails& details) const {
-  // TODO(crbug.com/728491): Implement this.
+  if (!base::FeatureList::IsEnabled(switches::kSyncUserLanguageDetectionEvents))
+    return;
+
+  syncer::UserEventService* const user_event_service =
+      IOSUserEventServiceFactory::GetForBrowserState(
+          ios::ChromeBrowserState::FromBrowserState(
+              web_state()->GetBrowserState()));
+  const auto* const item =
+      web_state()->GetNavigationManager()->GetLastCommittedItem();
+
+  // If entry is null, we don't record the page.
+  // The navigation entry can be null in situations like download or initial
+  // blank page.
+  if (item != nullptr &&
+      TranslateServiceIOS::IsTranslatableURL(item->GetVirtualURL())) {
+    user_event_service->RecordUserEvent(
+        translate::ConstructLanguageDetectionEvent(
+            item->GetTimestamp().ToInternalValue(), details));
+  }
 }
 
 bool ChromeIOSTranslateClient::IsTranslatableURL(const GURL& url) {
