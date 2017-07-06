4ba8726c7ff3cfdd97bd2f07cad3482065131e53
tapted <tapted@chromium.org>
2017-07-05 17:34:12 -0700

    Mac: Fix up location bar decoration bubble anchoring.
    Content settings bubbles currently anchor wrong both with and without
    --secondary-ui-md.
    
    Without --secondary-ui-md the y-offset just needs to match the bookmark
    star. Expose StarDecoration::GetStarBubblePointInFrame() for this.
    
    With --secondary-ui-md the bubble should anchor to the corner of the
    location bar frame. This was also slightly off for the bookmark star,
    which currently anchors to the location icon decoration hover
    background. Fix by consolidating codepaths for obtaining the anchor from
    LocationBarView.
    
    Screenshots: http://crbug.com/77157#c18
    
    BUG=77157, 635172
    
    Review-Url: https://codereview.chromium.org/2972593002
    Cr-Commit-Position: refs/heads/master@{#484420}
    
    Former-commit-id: c06fedb0d6a4463cc87ffd6e67375124f9c8426a

--- chrome/browser/ui/cocoa/location_bar/content_setting_decoration.mm
+++ chrome/browser/ui/cocoa/location_bar/content_setting_decoration.mm
@@ -17,6 +17,7 @@
 #import "chrome/browser/ui/cocoa/l10n_util.h"
 #include "chrome/browser/ui/cocoa/last_active_browser_cocoa.h"
 #import "chrome/browser/ui/cocoa/location_bar/location_bar_view_mac.h"
+#import "chrome/browser/ui/cocoa/location_bar/star_decoration.h"
 #import "chrome/browser/ui/cocoa/themed_window.h"
 #include "chrome/browser/ui/content_settings/content_setting_bubble_model.h"
 #include "chrome/browser/ui/content_settings/content_setting_image_model.h"
@@ -36,10 +37,6 @@
 using content::WebContents;
 
 namespace {
-
-// The bubble point should look like it points to the bottom of the respective
-// icon. The offset should be 2px.
-const CGFloat kPageBubblePointYOffset = 2.0;
 
 // Duration of animation, 3 seconds. The ContentSettingAnimationState breaks
 // this up into different states of varying lengths.
@@ -269,9 +266,9 @@ NSPoint ContentSettingDecoration::GetBubblePointInFrame(NSRect frame) {
   if (!cocoa_l10n_util::ShouldDoExperimentalRTLLayout())
     frame.origin.x += frame.size.width - image_size.width;
   frame.size = image_size;
-  const NSRect draw_frame = GetDrawRectInFrame(frame);
-  return NSMakePoint(NSMidX(draw_frame),
-                     NSMaxY(draw_frame) + kPageBubblePointYOffset);
+
+  // Position the same way as the bookmark star.
+  return StarDecoration::GetStarBubblePointInFrame(GetDrawRectInFrame(frame));
 }
 
 bool ContentSettingDecoration::AcceptsMousePress() {
@@ -285,14 +282,8 @@ bool ContentSettingDecoration::OnMousePressed(NSRect frame, NSPoint location) {
   if (!web_contents)
     return true;
 
-  // Find point for bubble's arrow in screen coordinates.
-  // TODO(shess): |owner_| is only being used to fetch |field|.
-  // Consider passing in |control_view|.  Or refactoring to be
-  // consistent with other decorations (which don't currently bring up
-  // their bubble directly).
   AutocompleteTextField* field = owner_->GetAutocompleteTextField();
-  NSPoint anchor = GetBubblePointInFrame(frame);
-  anchor = [field convertPoint:anchor toView:nil];
+  NSPoint anchor = [field bubblePointForDecoration:this];
   anchor = ui::ConvertPointFromWindowToScreen([field window], anchor);
 
   // Open bubble.
