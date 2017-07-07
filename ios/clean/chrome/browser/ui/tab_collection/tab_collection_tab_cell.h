0ff6fafd24ed8131d0fcbf62fcc23671e87972ad
edchin <edchin@chromium.org>
2017-07-07 16:27:10 -0700

    [ios] TabCollectionTabCell to use SnapshotCache.
    TabCollectionTabCell lazily retrieves snapshot images from SnapshotCache.
    This CL adds this capability and unit tests. This CL does not plumb the cache
    into the tab cell. This CL does not modify SnapshotCache to handle multiple
    image sizes.
    
    BUG=686770
    
    Review-Url: https://codereview.chromium.org/2967083002
    Cr-Commit-Position: refs/heads/master@{#485098}
    
    Former-commit-id: 4f216c72a3c1ab9cb60d747eae7bdeafbdd73af1

--- ios/clean/chrome/browser/ui/tab_collection/tab_collection_tab_cell.h
+++ ios/clean/chrome/browser/ui/tab_collection/tab_collection_tab_cell.h
@@ -5,10 +5,9 @@
 #ifndef IOS_CLEAN_CHROME_BROWSER_UI_TAB_COLLECTION_TAB_COLLECTION_TAB_CELL_H_
 #define IOS_CLEAN_CHROME_BROWSER_UI_TAB_COLLECTION_TAB_COLLECTION_TAB_CELL_H_
 
-#import <UIKit/UIKit.h>
-
 #import "ios/chrome/browser/ui/tab_switcher/tab_switcher_panel_cell.h"
 
+@class SnapshotCache;
 @class TabCollectionItem;
 
 // Cell represents a tab for use in the tab collection. It has a title, favicon,
@@ -16,7 +15,8 @@
 // highlight in the tintColor.
 // PLACEHOLDER: Create custom implemementation rather than subclassing.
 @interface TabCollectionTabCell : TabSwitcherLocalSessionCell
-@property(nonatomic, strong) TabCollectionItem* item;
+- (void)configureCell:(TabCollectionItem*)item
+        snapshotCache:(SnapshotCache*)snapshotCache;
 @end
 
 #endif  // IOS_CLEAN_CHROME_BROWSER_UI_TAB_COLLECTION_TAB_COLLECTION_TAB_CELL_H_
