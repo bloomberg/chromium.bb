b411d619b99582880eb687d1924906fc0a164e1c
Matthew Halpern <matthalp@google.com>
2017-07-07 20:13:43 +0000

    [GRC] Add Tab-level CPU Usage Attribution Tests
    This CL adds tab-level CPU usage attribution tests for GRC. Tab-level CPU usage attribution is tested under a variety of different coordination unit graph topologies.
    
    A set of mock coordination unit graph topologies has also been added to the code base as a common set of coordination unit graph topologies has emerged across the coordination unit unittests. 
    
    Bug: 691886
    Change-Id: I2e9178ca5454846320add15f9f004ba0d9df2a9f
    Reviewed-on: https://chromium-review.googlesource.com/562549
    Commit-Queue: Matthew Halpern <matthalp@google.com>
    Reviewed-by: Zhen Wang <zhenw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485024}
    Former-commit-id: b3c9a82477256a0f8dd9ee8f803f340cf194cc17

---
+++ services/resource_coordinator/coordination_unit/mock_coordination_unit_graphs.cc
@@ -0,0 +1,79 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/resource_coordinator/coordination_unit/mock_coordination_unit_graphs.h"
+
+#include <string>
+
+#include "services/resource_coordinator/coordination_unit/coordination_unit_factory.h"
+#include "services/resource_coordinator/public/cpp/coordination_unit_id.h"
+#include "services/resource_coordinator/public/cpp/coordination_unit_types.h"
+
+namespace service_manager {
+class ServiceContextRef;
+}
+
+namespace resource_coordinator {
+
+namespace {
+
+std::unique_ptr<CoordinationUnitImpl> CreateCoordinationUnit(
+    CoordinationUnitType type) {
+  CoordinationUnitID cu_id(type, std::string());
+  return coordination_unit_factory::CreateCoordinationUnit(
+      cu_id, std::unique_ptr<service_manager::ServiceContextRef>(nullptr));
+}  // namespace
+
+}  // namespace
+
+MockSingleTabInSingleProcessCoordinationUnitGraph::
+    MockSingleTabInSingleProcessCoordinationUnitGraph() {
+  frame = CreateCoordinationUnit(CoordinationUnitType::kFrame);
+  process = CreateCoordinationUnit(CoordinationUnitType::kProcess);
+  tab = CreateCoordinationUnit(CoordinationUnitType::kWebContents);
+
+  tab->AddChild(frame->id());
+  process->AddChild(frame->id());
+}
+
+MockSingleTabInSingleProcessCoordinationUnitGraph::
+    ~MockSingleTabInSingleProcessCoordinationUnitGraph() = default;
+
+MockMultipleTabsInSingleProcessCoordinationUnitGraph::
+    MockMultipleTabsInSingleProcessCoordinationUnitGraph() {
+  other_frame = CreateCoordinationUnit(CoordinationUnitType::kFrame);
+  other_tab = CreateCoordinationUnit(CoordinationUnitType::kWebContents);
+
+  other_tab->AddChild(other_frame->id());
+  process->AddChild(other_frame->id());
+}
+
+MockMultipleTabsInSingleProcessCoordinationUnitGraph::
+    ~MockMultipleTabsInSingleProcessCoordinationUnitGraph() = default;
+
+MockSingleTabWithMultipleProcessesCoordinationUnitGraph::
+    MockSingleTabWithMultipleProcessesCoordinationUnitGraph() {
+  child_frame = CreateCoordinationUnit(CoordinationUnitType::kFrame);
+  other_process = CreateCoordinationUnit(CoordinationUnitType::kProcess);
+
+  frame->AddChild(child_frame->id());
+  other_process->AddChild(child_frame->id());
+}
+
+MockSingleTabWithMultipleProcessesCoordinationUnitGraph::
+    ~MockSingleTabWithMultipleProcessesCoordinationUnitGraph() = default;
+
+MockMultipleTabsWithMultipleProcessesCoordinationUnitGraph::
+    MockMultipleTabsWithMultipleProcessesCoordinationUnitGraph() {
+  child_frame = CreateCoordinationUnit(CoordinationUnitType::kFrame);
+  other_process = CreateCoordinationUnit(CoordinationUnitType::kProcess);
+
+  other_frame->AddChild(child_frame->id());
+  other_process->AddChild(child_frame->id());
+}
+
+MockMultipleTabsWithMultipleProcessesCoordinationUnitGraph::
+    ~MockMultipleTabsWithMultipleProcessesCoordinationUnitGraph() = default;
+
+}  // namespace resource_coordinator
