bb89500af97f3cc2efe360f4fd5daa051af8f96b
Reilly Grant <reillyg@chromium.org>
2017-07-07 21:17:26 +0000

    Switch the Shape Detection LayoutTests to the new Mojo JS bindings
    This change switches the Shape Detection Mojo interface JS bindings to
    the new style and updates the LayoutTests accordingly. The Skia Mojo
    interfaces are also switched to generating the new binding style while
    the ui/gfx/geometry interfaces must still generated both as a
    temporary step while some code has not yet been updated.
    
    These tests demonstrate how the DCHECK in ~MojoWatcher is invalid and
    so it is removed.
    
    Bug: 699569
    Cq-Include-Trybots: master.tryserver.chromium.mac:mac_optional_gpu_tests_rel
    Change-Id: Ic83ac9bd50072eae8461edc0b85cd4bcae147358
    Reviewed-on: https://chromium-review.googlesource.com/560646
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Miguel Casas <mcasas@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485049}
    Former-commit-id: 47d872925a8504668a2b2c65f1d0572264d08398

--- third_party/WebKit/LayoutTests/shapedetection/resources/mock-textdetection.js
+++ third_party/WebKit/LayoutTests/shapedetection/resources/mock-textdetection.js
@@ -1,46 +1,37 @@
 "use strict";
 
-let mockTextDetectionReady = define(
-  'mockTextDetection',
-  ['services/shape_detection/public/interfaces/textdetection.mojom',
-   'mojo/public/js/bindings',
-   'mojo/public/js/core',
-   'content/public/renderer/frame_interfaces',
-   'content/public/renderer/interfaces',
-  ], (textDetection, bindings, mojo, frameInterfaces, processInterfaces) => {
+class MockTextDetection {
+  constructor() {
+    this.bindingSet_ =
+        new mojo.BindingSet(shapeDetection.mojom.TextDetection);
 
-  class MockTextDetection {
-    constructor() {
-      this.bindingSet_ = new bindings.BindingSet(textDetection.TextDetection);
-
-      frameInterfaces.addInterfaceOverrideForTesting(
-          textDetection.TextDetection.name,
-          handle => this.bindingSet_.addBinding(this, handle));
-      processInterfaces.addInterfaceOverrideForTesting(
-          textDetection.TextDetection.name,
-          handle => this.bindingSet_.addBinding(this, handle));
-    }
-
-    detect(bitmap_data) {
-      let receivedStruct = new Uint8Array(bitmap_data.pixel_data);
-      this.buffer_data_ = new Uint32Array(receivedStruct.buffer);
-      return Promise.resolve({
-        results: [
-          {
-            raw_value : "cats",
-            bounding_box: { x: 1.0, y: 1.0, width: 100.0, height: 100.0 }
-          },
-          {
-            raw_value : "dogs",
-            bounding_box: { x: 2.0, y: 2.0, width: 50.0, height: 50.0 }
-          },
-        ],
-      });
-    }
-
-    getFrameData() {
-      return this.buffer_data_;
-    }
+    this.interceptor_ = new MojoInterfaceInterceptor(
+        shapeDetection.mojom.TextDetection.name);
+    this.interceptor_.oninterfacerequest =
+        e => this.bindingSet_.addBinding(this, e.handle);
+    this.interceptor_.start();
   }
-  return new MockTextDetection();
-});
+
+  detect(bitmap_data) {
+    let receivedStruct = new Uint8Array(bitmap_data.pixel_data);
+    this.buffer_data_ = new Uint32Array(receivedStruct.buffer);
+    return Promise.resolve({
+      results: [
+        {
+          rawValue : "cats",
+          boundingBox: { x: 1.0, y: 1.0, width: 100.0, height: 100.0 }
+        },
+        {
+          rawValue : "dogs",
+          boundingBox: { x: 2.0, y: 2.0, width: 50.0, height: 50.0 }
+        },
+      ],
+    });
+  }
+
+  getFrameData() {
+    return this.buffer_data_;
+  }
+}
+
+let mockTextDetection = new MockTextDetection();
