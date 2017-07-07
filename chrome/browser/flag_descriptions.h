f54424418c65483e6f65e75edcd2a93f1d815377
Nicolas Dossou-gbete <dgn@chromium.org>
2017-07-07 12:40:00 +0000

    Revert "[Suggestions] Add flag to guard the Categories UI"
    This reverts commit e858bdc3db9825bf2b5d9a7e0d587608bb3d6d21 [formerly c92e3ffe214321528fe911957e2926123fc08ef2].
    
    Bug: 725887
    Change-Id: I507da712dc10681b15ca2590cadb2cf8735d3e8f
    Reviewed-on: https://chromium-review.googlesource.com/561719
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484897}
    Former-commit-id: ee4dcaf867b8a64b597070463fe50e0745573646

--- chrome/browser/flag_descriptions.h
+++ chrome/browser/flag_descriptions.h
@@ -907,9 +907,6 @@ extern const char kEnableConsistentOmniboxGeolocationDescription[];
 extern const char kEnableContentSuggestionsNewFaviconServerName[];
 extern const char kEnableContentSuggestionsNewFaviconServerDescription[];
 
-extern const char kEnableContentSuggestionsCategoriesName[];
-extern const char kEnableContentSuggestionsCategoriesDescription[];
-
 extern const char kEnableContentSuggestionsLargeThumbnailName[];
 extern const char kEnableContentSuggestionsLargeThumbnailDescription[];
 
