bae61f5506f52520b0c541d77da1f532b0cd9c21
Sergey Ulanov <sergeyu@google.com>
2017-07-07 21:44:18 +0000

    Fixes to compile //gpu:gpu on Fuchsia
    Several simple fixes to make //gpu compile on Fuchsia:
     1. Updated surface_handle.h to declare SurfaceHandle.
     2. gpu/ipc/common/memory_stats.h updated to convert int32_t
        between ProcessId.
     3. Updated eglplatform.h in third_party/khronos to define
        types for Fuchsia
     4. Enabled use_egl in ui/gl
    
    Bug: 737802
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Iad46d340ebdd3916ac43a3528cdaa37c9641d2be
    Reviewed-on: https://chromium-review.googlesource.com/563764
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485058}
    Former-commit-id: 147af9e4a6f1b9ffc46eaff28010093b7dbe11fe

--- third_party/khronos/EGL/eglplatform.h
+++ third_party/khronos/EGL/eglplatform.h
@@ -77,7 +77,7 @@ typedef HDC     EGLNativeDisplayType;
 typedef HBITMAP EGLNativePixmapType;
 typedef HWND    EGLNativeWindowType;
 
-#elif defined(__WINSCW__) || defined(__SYMBIAN32__)  /* Symbian */
+#elif defined(__WINSCW__) || defined(__SYMBIAN32__) || defined(__Fuchsia__)
 
 typedef int   EGLNativeDisplayType;
 typedef void *EGLNativeWindowType;
