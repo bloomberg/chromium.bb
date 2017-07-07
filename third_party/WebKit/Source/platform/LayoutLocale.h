c197878948bce12cc1b62c6060ba222ef5e60d06
Fernando Serboncini <fserb@chromium.org>
2017-07-07 14:43:58 +0000

    Make LayoutLocale thread safe
    moves Locale related static to FontGlobalContext
    
    Bug: 730692
    Change-Id: I0cf60421fbe07c13eacd1b8742920c052ccf255e
    Reviewed-on: https://chromium-review.googlesource.com/544909
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484919}
    Former-commit-id: 2f98242f3b670cafd1f6712143ed9306364376fc

--- third_party/WebKit/Source/platform/LayoutLocale.h
+++ third_party/WebKit/Source/platform/LayoutLocale.h
@@ -54,7 +54,6 @@ class PLATFORM_EXPORT LayoutLocale : public RefCounted<LayoutLocale> {
   UScriptCode GetScriptForHan() const;
   bool HasScriptForHan() const;
   static const LayoutLocale* LocaleForHan(const LayoutLocale*);
-  static void InvalidateLocaleForHan() { default_for_han_computed_ = false; }
   const char* LocaleForHanForSkFontMgr() const;
 
   Hyphenation* GetHyphenation() const;
@@ -62,7 +61,6 @@ class PLATFORM_EXPORT LayoutLocale : public RefCounted<LayoutLocale> {
   AtomicString LocaleWithBreakKeyword(LineBreakIteratorMode) const;
 
   static PassRefPtr<LayoutLocale> CreateForTesting(const AtomicString&);
-  static void ClearForTesting();
   static void SetHyphenationForTesting(const AtomicString&,
                                        PassRefPtr<Hyphenation>);
 
@@ -70,7 +68,6 @@ class PLATFORM_EXPORT LayoutLocale : public RefCounted<LayoutLocale> {
   explicit LayoutLocale(const AtomicString&);
 
   void ComputeScriptForHan() const;
-  static void ComputeLocaleForHan();
 
   AtomicString string_;
   mutable CString string_for_sk_font_mgr_;
@@ -84,11 +81,6 @@ class PLATFORM_EXPORT LayoutLocale : public RefCounted<LayoutLocale> {
 
   mutable unsigned has_script_for_han_ : 1;
   mutable unsigned hyphenation_computed_ : 1;
-
-  static const LayoutLocale* default_;
-  static const LayoutLocale* system_;
-  static const LayoutLocale* default_for_han_;
-  static bool default_for_han_computed_;
 };
 
 }  // namespace blink
