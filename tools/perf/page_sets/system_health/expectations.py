7e26da99818c0698abdba75834524f5717f2c1db
Ned Nguyen <nednguyen@google.com>
2017-07-07 22:09:18 +0000

    [system_health] Reenable browse:media:flickr_infinite_scroll story
    Bug:728785
    Change-Id: Ia7e9a7c1739be25c9f32ab88262b075d553ec535
    
    After the fix to scrolling retry logic in https://chromium-review.googlesource.com/c/563340/, I
    can run this test multiple time on my Mac without flakes. So this is probably fixed.
    
    TBR=ulan@chromium.org, perezju@chromium.org
    
    Change-Id: Ia7e9a7c1739be25c9f32ab88262b075d553ec535
    Reviewed-on: https://chromium-review.googlesource.com/563527
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#485066}
    Former-commit-id: e1466a707d3d4bc35f3bfa38169820edf9ee6060

--- tools/perf/page_sets/system_health/expectations.py
+++ tools/perf/page_sets/system_health/expectations.py
@@ -28,9 +28,6 @@ class SystemHealthDesktopCommonExpectations(expectations.StoryExpectations):
                       [expectations.ALL_WIN], 'crbug.com/728152')
     self.DisableStory('browse:news:cnn',
                       [expectations.ALL_MAC], 'crbug.com/728576')
-    self.DisableStory('browse:media:flickr_infinite_scroll',
-                      [expectations.ALL],
-                      'crbug.com/728785')
 
 
 class SystemHealthDesktopMemoryExpectations(expectations.StoryExpectations):
@@ -59,9 +56,6 @@ class SystemHealthDesktopMemoryExpectations(expectations.StoryExpectations):
                       [expectations.ALL_WIN], 'crbug.com/728152')
     self.DisableStory('browse:news:cnn',
                       [expectations.ALL_MAC], 'crbug.com/728576')
-    self.DisableStory('browse:media:flickr_infinite_scroll',
-                      [expectations.ALL],
-                      'crbug.com/728785')
 
 
 class SystemHealthMobileCommonExpectations(expectations.StoryExpectations):
@@ -82,8 +76,6 @@ class SystemHealthMobileCommonExpectations(expectations.StoryExpectations):
                       'crbug.com/728081')
     self.DisableStory('browse:social:facebook_infinite_scroll',
                       [expectations.ALL], 'crbug.com/728152')
-    self.DisableStory('browse:media:flickr_infinite_scroll',
-                      [expectations.ALL], 'crbug.com/728785')
     self.DisableStory(
         'load:tools:drive',
         [expectations.ANDROID_NEXUS5X, expectations.ANDROID_WEBVIEW],
@@ -122,8 +114,6 @@ class SystemHealthMobileMemoryExpectations(expectations.StoryExpectations):
                       'crbug.com/728081')
     self.DisableStory('browse:social:facebook_infinite_scroll',
                       [expectations.ALL], 'crbug.com/728152')
-    self.DisableStory('browse:media:flickr_infinite_scroll',
-                      [expectations.ALL], 'crbug.com/728785')
     self.DisableStory(
         'load:tools:drive',
         [expectations.ANDROID_NEXUS5X, expectations.ANDROID_WEBVIEW],
@@ -163,9 +153,6 @@ class V8BrowsingDesktopExpecations(expectations.StoryExpectations):
                       [expectations.ALL_MAC], 'crbug.com/728576')
     self.DisableStory('browse:social:twitter_infinite_scroll',
                       [expectations.ALL], 'crbug.com/728464')
-    self.DisableStory('browse:media:flickr_infinite_scroll',
-                      [expectations.ALL, expectations.ALL_WIN],
-                      'crbug.com/728785')
 
 # Should only include browse:*:* stories.
 class V8BrowsingMobileExpecations(expectations.StoryExpectations):
@@ -178,8 +165,6 @@ class V8BrowsingMobileExpecations(expectations.StoryExpectations):
                       'crbug.com/728081')
     self.DisableStory('browse:social:facebook_infinite_scroll',
                       [expectations.ALL], 'crbug.com/728152')
-    self.DisableStory('browse:media:flickr_infinite_scroll',
-                      [expectations.ALL], 'crbug.com/728785')
     # TODO(rnephew): This disabling should move to CanRunOnBrowser.
     self.DisableStory('browse:chrome:omnibox',
                       [expectations.ANDROID_WEBVIEW],
