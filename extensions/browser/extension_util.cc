3b9f848d7005e1485a8e326150d11b3c99f5d00a
Karan Bhatia <karandeepb@chromium.org>
2017-07-07 20:12:19 +0000

    Extensions: Remove unused extensions::util::HasIsolatedStorage.
    This CL removes extensions::util::HasIsolatedStorage, which is unused. The last
    usage seems to have been removed in r277972.
    
    BUG=None
    
    Change-Id: I6905ca9f09c1f9047c1978a73af139196a0c4a45
    Reviewed-on: https://chromium-review.googlesource.com/562675
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485023}
    Former-commit-id: 0bafc424d12de01bfbe72732a594b66f8e697cb8

--- extensions/browser/extension_util.cc
+++ extensions/browser/extension_util.cc
@@ -29,23 +29,6 @@ bool IsWhitelistedForIncognito(const Extension* extension) {
 
 }  // namespace
 
-bool HasIsolatedStorage(const ExtensionInfo& info) {
-  if (!info.extension_manifest.get())
-    return false;
-
-  std::string error;
-  scoped_refptr<const Extension> extension(Extension::Create(
-      info.extension_path,
-      info.extension_location,
-      *info.extension_manifest,
-      Extension::NO_FLAGS,
-      info.extension_id,
-      &error));
-
-  return extension.get() &&
-         AppIsolationInfo::HasIsolatedStorage(extension.get());
-}
-
 bool SiteHasIsolatedStorage(const GURL& extension_site_url,
                             content::BrowserContext* context) {
   const Extension* extension = ExtensionRegistry::Get(context)->
