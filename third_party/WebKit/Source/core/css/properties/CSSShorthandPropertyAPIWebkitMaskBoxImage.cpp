25f858817557c806ef30eaff454f4888f6082676
Jia <jiameng@chromium.org>
2017-07-07 07:06:17 +0000

    Implement parseShorthand API for "border-image" and "-webkit-mask-box-image".
    This cl contains the following changes
    * Implemented parseShorthand for CSSShorthandPropertyAPIBorderImage and CSSShorthandPropertyAPIWebkitMaskBoxImage.
    * Broke up ConsumeBorderImage from CSSPropertyParser to two parts, one for each shorthand property.
    - This function was used to serve both shorthands using a switch statement.
    * Removed ConsumeBorderImage from CSSPropertyParser. Also changed CSSPropertyParser to use API for these two properties.
    * Changed BUILD and json5 files to use the APIs.
    
    Bug: 668012
    Change-Id: I7fda459f423f684e52729f9ada0dc24e3b753d78
    Reviewed-on: https://chromium-review.googlesource.com/558328
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Alan Cutter <alancutter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#484844}
    Former-commit-id: 0b176f8a5bdbd7cbff9f554940277b9e27a21ae9

---
+++ third_party/WebKit/Source/core/css/properties/CSSShorthandPropertyAPIWebkitMaskBoxImage.cpp
@@ -0,0 +1,55 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSShorthandPropertyAPIWebkitMaskBoxImage.h"
+
+#include "core/css/CSSInitialValue.h"
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "core/css/properties/CSSPropertyBorderImageUtils.h"
+
+namespace blink {
+
+bool CSSShorthandPropertyAPIWebkitMaskBoxImage::parseShorthand(
+    bool important,
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    const CSSParserLocalContext&,
+    HeapVector<CSSProperty, 256>& properties) {
+  CSSValue* source = nullptr;
+  CSSValue* slice = nullptr;
+  CSSValue* width = nullptr;
+  CSSValue* outset = nullptr;
+  CSSValue* repeat = nullptr;
+
+  if (!CSSPropertyBorderImageUtils::ConsumeBorderImageComponents(
+          range, &context, source, slice, width, outset, repeat, true)) {
+    return false;
+  }
+
+  CSSPropertyParserHelpers::AddProperty(
+      CSSPropertyWebkitMaskBoxImageSource, CSSPropertyWebkitMaskBoxImage,
+      source ? *source : *CSSInitialValue::Create(), important,
+      CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+  CSSPropertyParserHelpers::AddProperty(
+      CSSPropertyWebkitMaskBoxImageSlice, CSSPropertyWebkitMaskBoxImage,
+      slice ? *slice : *CSSInitialValue::Create(), important,
+      CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+  CSSPropertyParserHelpers::AddProperty(
+      CSSPropertyWebkitMaskBoxImageWidth, CSSPropertyWebkitMaskBoxImage,
+      width ? *width : *CSSInitialValue::Create(), important,
+      CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+  CSSPropertyParserHelpers::AddProperty(
+      CSSPropertyWebkitMaskBoxImageOutset, CSSPropertyWebkitMaskBoxImage,
+      outset ? *outset : *CSSInitialValue::Create(), important,
+      CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+  CSSPropertyParserHelpers::AddProperty(
+      CSSPropertyWebkitMaskBoxImageRepeat, CSSPropertyWebkitMaskBoxImage,
+      repeat ? *repeat : *CSSInitialValue::Create(), important,
+      CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+
+  return true;
+}
+
+}  // namespace blink
