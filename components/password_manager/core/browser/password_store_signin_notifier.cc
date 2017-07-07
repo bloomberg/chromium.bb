4affd93f7a4afe63fad1b59903642096bea9545a
dvadym <dvadym@chromium.org>
2017-07-07 03:14:47 -0700

    UMA for saving/clearing sync pasword hash.
    BUG=657041
    
    Review-Url: https://codereview.chromium.org/2969943003
    Cr-Commit-Position: refs/heads/master@{#484873}
    
    Former-commit-id: 9d84260fdb5d8600a123b461f228c830ab594bb4

--- components/password_manager/core/browser/password_store_signin_notifier.cc
+++ components/password_manager/core/browser/password_store_signin_notifier.cc
@@ -4,6 +4,7 @@
 
 #include "components/password_manager/core/browser/password_store_signin_notifier.h"
 
+#include "components/password_manager/core/browser/password_manager_metrics_util.h"
 #include "components/password_manager/core/browser/password_store.h"
 
 namespace password_manager {
@@ -13,11 +14,15 @@ PasswordStoreSigninNotifier::PasswordStoreSigninNotifier() {}
 PasswordStoreSigninNotifier::~PasswordStoreSigninNotifier() {}
 
 void PasswordStoreSigninNotifier::NotifySignin(const std::string& password) {
+  metrics_util::LogSyncPasswordHashChange(
+      metrics_util::SyncPasswordHashChange::SAVED_ON_CHROME_SIGNIN);
   if (store_)
     store_->SaveSyncPasswordHash(base::UTF8ToUTF16(password));
 }
 
 void PasswordStoreSigninNotifier::NotifySignedOut() {
+  metrics_util::LogSyncPasswordHashChange(
+      metrics_util::SyncPasswordHashChange::CLEARED_ON_CHROME_SIGNOUT);
   if (store_)
     store_->ClearSyncPasswordHash();
 }
