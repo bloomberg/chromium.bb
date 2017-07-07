0bd8051bc8ef2de484673d40bf29ee059ca0d5ec
Hiroshige Hayashizaki <hiroshige@chromium.org>
2017-07-07 21:46:24 +0000

    Do not fire load events for async/dynamic inline module scripts
    This CL stops firing load events for inline module scripts controlled
    by ScriptRunner, by checking IsExternal() in Execute().
    
    This CL doesn't affect classic scripts, because all classic scripts
    controlled by ScriptRunner are external scripts.
    
    Bug: 715369
    Test: external/wpt/html/semantics/scripting-1/the-script-element/module/load-error-events-inline.html
    Change-Id: I1157b7e2d1f4f6e64077fde901cc6a6b95c102ae
    Reviewed-on: https://chromium-review.googlesource.com/557978
    Commit-Queue: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485059}
    Former-commit-id: ed0d20f7c347cc2ec55621918941556e141d8341

--- third_party/WebKit/Source/core/dom/ScriptLoader.cpp
+++ third_party/WebKit/Source/core/dom/ScriptLoader.cpp
@@ -910,16 +910,19 @@ void ScriptLoader::Execute() {
   DCHECK(!will_be_parser_executed_);
   DCHECK(async_exec_type_ != ScriptRunner::kNone);
   DCHECK(pending_script_->IsExternalOrModule());
+  DCHECK_EQ(pending_script_->IsExternal(), is_external_script_);
   bool error_occurred = false;
   Script* script = pending_script_->GetSource(NullURL(), error_occurred);
   const bool wasCanceled = pending_script_->WasCanceled();
+  const bool is_external = pending_script_->IsExternal();
   DetachPendingScript();
   if (error_occurred) {
     DispatchErrorEvent();
   } else if (!wasCanceled) {
     switch (ExecuteScript(script)) {
       case ExecuteScriptResult::kShouldFireLoadEvent:
-        DispatchLoadEvent();
+        if (is_external)
+          DispatchLoadEvent();
         break;
       case ExecuteScriptResult::kShouldFireErrorEvent:
         DispatchErrorEvent();
