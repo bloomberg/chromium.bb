de1542b7c6534774080ca71d67fdcafc9fa824a1
Sorin Jianu <sorin@chromium.org>
2017-07-07 22:19:06 +0000

    Limit the number of BITS jobs created by the component updater.
    Also, make the clean up of stale jobs more aggressive.
    
    TBR=waffles
    
    Bug: 737730
    Change-Id: I61bf9831aa5f753003c10ef50169a81a2545792a
    Reviewed-on: https://chromium-review.googlesource.com/563765
    Commit-Queue: Sorin Jianu <sorin@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485071}
    Former-commit-id: cc3f9c3f3c57b1a4e0c506828932c0c46170e723

--- components/update_client/update_client_errors.h
+++ components/update_client/update_client_errors.h
@@ -37,6 +37,9 @@ enum class CrxDownloaderError {
   NO_URL = 10,
   NO_HASH = 11,
   BAD_HASH = 12,  // The downloaded file fails the hash verification.
+  // The Windows BITS queue contains to many update client jobs. The value is
+  // chosen so that it can be reported as a custom COM error on this platform.
+  BITS_TOO_MANY_JOBS = 0x0200,
   GENERIC_ERROR = -1
 };
 
