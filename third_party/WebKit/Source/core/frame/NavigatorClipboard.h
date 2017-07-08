4ff28c41cdf15df77f651d12728f521657a56216
garykac <garykac@chromium.org>
2017-07-07 17:53:09 -0700

    Initial version of Async Clipboard API
    Includes basic support for plaintext read/write.
    
    See https://w3c.github.io/clipboard-apis/
    
    BUG=677564
    
    Review-Url: https://codereview.chromium.org/2695593006
    Cr-Commit-Position: refs/heads/master@{#485121}
    
    Former-commit-id: ac319a7ed300e2d8b7de3e6dcdfce2acf3363122

---
+++ third_party/WebKit/Source/core/frame/NavigatorClipboard.h
@@ -0,0 +1,38 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NavigatorClipboard_h
+#define NavigatorClipboard_h
+
+#include "core/CoreExport.h"
+#include "core/frame/Navigator.h"
+#include "platform/Supplementable.h"
+#include "platform/heap/Handle.h"
+#include "platform/wtf/Noncopyable.h"
+
+namespace blink {
+
+class Clipboard;
+class ScriptState;
+
+class NavigatorClipboard final : public GarbageCollected<NavigatorClipboard>,
+                                 public Supplement<Navigator> {
+  USING_GARBAGE_COLLECTED_MIXIN(NavigatorClipboard);
+  WTF_MAKE_NONCOPYABLE(NavigatorClipboard);
+
+ public:
+  static Clipboard* clipboard(ScriptState*, Navigator&);
+
+  DECLARE_TRACE();
+
+ private:
+  explicit NavigatorClipboard(Navigator&);
+  static const char* SupplementName();
+
+  Member<Clipboard> clipboard_;
+};
+
+}  // namespace blink
+
+#endif  // NavigatorClipboard.h
