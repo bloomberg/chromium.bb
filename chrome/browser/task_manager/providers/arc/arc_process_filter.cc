791b1a20f4d8732f6cbe7541b65fb87492769e43
Eliot Courtney <edcourtney@chromium.org>
2017-07-10 04:36:00 +0000

    [Task Manager] Implicitly white list more process states.
    If you minimise Gmail, it disappears from the task manager, so allow the
    LAST_ACTIVITY process state to be displayed for ARC processes.
    
    Additionally allow FOREGROUND_SERVICE since foreground services are user
    visible.
    
    BUG=738651
    
    Change-Id: I403b9946fbbb29b115fe1877f63bb64e3fe3b2c7
    Reviewed-on: https://chromium-review.googlesource.com/562906
    Reviewed-by: Nick Carter <nick@chromium.org>
    Reviewed-by: Yusuke Sato <yusukes@chromium.org>
    Commit-Queue: Eliot Courtney <edcourtney@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485183}
    Former-commit-id: 8a22a574d88c8a87f4854b8306dff5d4d776f94a

--- chrome/browser/task_manager/providers/arc/arc_process_filter.cc
+++ chrome/browser/task_manager/providers/arc/arc_process_filter.cc
@@ -6,6 +6,7 @@
 
 #include <utility>
 
+#include "base/logging.h"
 #include "components/arc/common/process.mojom.h"
 
 namespace task_manager {
@@ -35,14 +36,32 @@ bool ArcProcessFilter::ShouldDisplayProcess(
   // ProcessState::TOP: Process is hosting the current top activities
   // ProcessState::IMPORTANT_FOREGROUND: Process is important to the user
   // ProcessState::TOP_SLEEPING: Same as TOP, but while the device is sleeping
+  // ProcessState::LAST_ACTIVITY: The last activity - see crbug.com/738651.
+  // ProcessState::FOREGROUND_SERVICE: Foreground service - in Android, the user
+  //   is made aware of these via a notification.
   switch (process.process_state()) {
     case arc::mojom::ProcessState::TOP:  // Fallthrough.
     case arc::mojom::ProcessState::IMPORTANT_FOREGROUND:
     case arc::mojom::ProcessState::TOP_SLEEPING:
+    case arc::mojom::ProcessState::LAST_ACTIVITY:
+    case arc::mojom::ProcessState::FOREGROUND_SERVICE:
       return true;
-    default:
-      break;
+    case arc::mojom::ProcessState::NONEXISTENT:  // Fallthrough.
+    case arc::mojom::ProcessState::PERSISTENT:
+    case arc::mojom::ProcessState::PERSISTENT_UI:
+    case arc::mojom::ProcessState::BOUND_FOREGROUND_SERVICE:
+    case arc::mojom::ProcessState::IMPORTANT_BACKGROUND:
+    case arc::mojom::ProcessState::BACKUP:
+    case arc::mojom::ProcessState::HEAVY_WEIGHT:
+    case arc::mojom::ProcessState::SERVICE:
+    case arc::mojom::ProcessState::RECEIVER:
+    case arc::mojom::ProcessState::HOME:
+    case arc::mojom::ProcessState::CACHED_ACTIVITY:
+    case arc::mojom::ProcessState::CACHED_ACTIVITY_CLIENT:
+    case arc::mojom::ProcessState::CACHED_EMPTY:
+      return false;
   }
+  NOTREACHED();
   return false;
 }
 
