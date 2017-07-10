d6d227218d931a2812a4759223e6ae7f4e6bb6ff
Steven Bennetts <stevenjb@chromium.org>
2017-07-10 18:23:51 +0000

    ONC: Always set shill::kOpenVPNRemoteCertKUProperty
    If no ONC values is provided for OpenVPN.RemoteCertKU we need to
    set the Shill property to an empty string value.
    
    Bug: 738119
    Change-Id: I3266e0c9f81fea42734e9da7d68df9e268487423
    Reviewed-on: https://chromium-review.googlesource.com/558203
    Commit-Queue: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Kevin Cernekee <cernekee@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485318}
    Former-commit-id: 74fe242b94d0e1a024f644287cf8f462bcfd0705

--- chromeos/network/onc/onc_translator_onc_to_shill.cc
+++ chromeos/network/onc/onc_translator_onc_to_shill.cc
@@ -156,16 +156,17 @@ void LocalTranslator::TranslateOpenVPN() {
   if (user_auth_type == ::onc::openvpn_user_auth_type::kOTP)
     CopyFieldFromONCToShill(::onc::openvpn::kOTP, shill::kOpenVPNTokenProperty);
 
-  // Shill supports only one RemoteCertKU but ONC a list.
-  // Copy only the first entry if existing.
-  const base::ListValue* cert_kus = NULL;
+  // Shill supports only one RemoteCertKU but ONC specifies a list, so copy only
+  // the first entry if the lists exists. Otherwise copy an empty string to
+  // reset any previous configuration.
+  const base::ListValue* cert_kus = nullptr;
   std::string cert_ku;
   if (onc_object_->GetListWithoutPathExpansion(::onc::openvpn::kRemoteCertKU,
-                                               &cert_kus) &&
-      cert_kus->GetString(0, &cert_ku)) {
-    shill_dictionary_->SetStringWithoutPathExpansion(
-        shill::kOpenVPNRemoteCertKUProperty, cert_ku);
+                                               &cert_kus)) {
+    cert_kus->GetString(0, &cert_ku);
   }
+  shill_dictionary_->SetStringWithoutPathExpansion(
+      shill::kOpenVPNRemoteCertKUProperty, cert_ku);
 
   for (base::DictionaryValue::Iterator it(*onc_object_); !it.IsAtEnd();
        it.Advance()) {
