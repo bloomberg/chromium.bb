ad1254c5311c2e24466e8a1e794815fa19e6063f
Colin Blundell <blundell@chromium.org>
2017-07-10 13:25:47 +0000

    [ProfileOAuth2TokenService] Expose whether refresh tokens are loaded
    Consumers of ProfileOAuth2TokenService currently have the ability to load
    credentials but no way to query whether all credentilas have been loaded.
    This functionality will be needed by the Identity Service internal
    implementation in order to delay responding to consumer requests until
    all refresh tokens have been loaded (listening to the observer event
    isn't enough because the Identity Service might be started after the
    event has fired).
    
    This CL adds this functionality.
    
    Bug: 740117
    Change-Id: Ia2d61aefa9310f05bbc58c2884a68bc19d647c08
    Reviewed-on: https://chromium-review.googlesource.com/563616
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485253}
    Former-commit-id: 0156251a5c6b3374b41bfc8e1ef8642f469e8d70

--- components/signin/core/browser/profile_oauth2_token_service.cc
+++ components/signin/core/browser/profile_oauth2_token_service.cc
@@ -6,7 +6,7 @@
 
 ProfileOAuth2TokenService::ProfileOAuth2TokenService(
     std::unique_ptr<OAuth2TokenServiceDelegate> delegate)
-    : OAuth2TokenService(std::move(delegate)) {
+    : OAuth2TokenService(std::move(delegate)), all_credentials_loaded_(false) {
   AddObserver(this);
 }
 
@@ -22,6 +22,10 @@ void ProfileOAuth2TokenService::Shutdown() {
 void ProfileOAuth2TokenService::LoadCredentials(
     const std::string& primary_account_id) {
   GetDelegate()->LoadCredentials(primary_account_id);
+}
+
+bool ProfileOAuth2TokenService::AreAllCredentialsLoaded() {
+  return all_credentials_loaded_;
 }
 
 void ProfileOAuth2TokenService::UpdateCredentials(
@@ -49,4 +53,8 @@ void ProfileOAuth2TokenService::OnRefreshTokenRevoked(
     const std::string& account_id) {
   CancelRequestsForAccount(account_id);
   ClearCacheForAccount(account_id);
+}
+
+void ProfileOAuth2TokenService::OnRefreshTokensLoaded() {
+  all_credentials_loaded_ = true;
 }
