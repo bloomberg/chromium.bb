40e942da0376a73fa43ea6f4689a650c3c66d6e8
edchin <edchin@chromium.org>
2017-07-10 23:51:43 +0000

    [ios] Tab grid clean up.
    1) Uses the proper title for tab grid tab cells.
    2) Properly sets snapshot image. It was previously 
    setting the button's image rather than the imageView.
    
    Bug: 686770
    Change-Id: I1f400f9b543528355f3014f3e1503e50166b33e9
    Reviewed-on: https://chromium-review.googlesource.com/564346
    Commit-Queue: Ed Chin <edchin@chromium.org>
    Reviewed-by: Ed Chin <edchin@chromium.org>
    Reviewed-by: Jean-François Geyelin <jif@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485449}
    Former-commit-id: 9cc98cde8d35ec12992a2e91135ce6bd5096a9bf

--- ios/clean/chrome/browser/ui/tab_collection/tab_collection_item.mm
+++ ios/clean/chrome/browser/ui/tab_collection/tab_collection_item.mm
@@ -4,6 +4,10 @@
 
 #import "ios/clean/chrome/browser/ui/tab_collection/tab_collection_item.h"
 
+#include "base/logging.h"
+#include "components/strings/grit/components_strings.h"
+#include "ui/base/l10n/l10n_util.h"
+
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
@@ -11,4 +15,19 @@
 @implementation TabCollectionItem
 @synthesize tabID = _tabID;
 @synthesize title = _title;
+
+#pragma mark - Public properties
+
+- (void)setTabID:(NSString*)tabID {
+  DCHECK_GE(tabID.length, 0U);
+  _tabID = tabID;
+}
+
+- (void)setTitle:(NSString*)title {
+  _title = title;
+  if (title.length == 0) {
+    _title = l10n_util::GetNSString(IDS_DEFAULT_TAB_TITLE);
+  }
+}
+
 @end
