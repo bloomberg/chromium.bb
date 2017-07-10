e0f3a756dfb35a8a8f5ae3c80dcc29ea51c961b9
Sebastien Alaiwan <sebastien.alaiwan@allegrodvt.com>
2017-07-10 12:27:15 +0200

    inv_txfm accuracy test: use right cos_bit precision
    Match cos_bit precision actually used in the decoder (max: 13).
    
    Change-Id: I609f59a9653d74b569420bb0d402ba4f5352aa76

--- test/av1_inv_txfm1d_test.cc
+++ test/av1_inv_txfm1d_test.cc
@@ -42,7 +42,7 @@ const TxfmFunc inv_txfm_func_ls[][2] = {
 };
 
 // the maximum stage number of fwd/inv 1d dct/adst txfm is 12
-const int8_t cos_bit[12] = { 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14, 14 };
+const int8_t cos_bit[12] = { 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13 };
 const int8_t range_bit[12] = { 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32, 32 };
 
 TEST(av1_inv_txfm1d, round_trip) {
