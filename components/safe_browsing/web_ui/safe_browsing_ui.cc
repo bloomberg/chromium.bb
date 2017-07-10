db8eb64b40ac41ebb8d70d9dec46f83e8ac09974
hkamila <hkamila@google.com>
2017-07-10 14:00:50 -0700

    -Added the experimental features section as part of the chrome://safe-browsing. For the experiments not running at 100%.
    -Exported the list of SafeBrowsing features (base::Feature) to have them in the future in a central location.
    
    -Fixed the message dialogue for the WebUI.
    
    -Added some extra css.
    
    Note: Tags for the experiments are not added in this issue.
    Review-Url: https://codereview.chromium.org/2964693003
    Cr-Commit-Position: refs/heads/master@{#485380}
    
    Former-commit-id: fdacb19a2600b6ca47c7d58752a97bc075fd89f5

--- components/safe_browsing/web_ui/safe_browsing_ui.cc
+++ components/safe_browsing/web_ui/safe_browsing_ui.cc
@@ -1,21 +1,33 @@
-// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #include "components/safe_browsing/web_ui/safe_browsing_ui.h"
+
+#include <algorithm>
+#include <utility>
+#include "base/macros.h"
+#include "base/values.h"
 #include "components/grit/components_resources.h"
 #include "components/grit/components_scaled_resources.h"
+#include "components/safe_browsing/features.h"
 #include "components/safe_browsing/web_ui/constants.h"
 #include "components/strings/grit/components_strings.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
-#include "content/public/browser/web_ui_data_source.h"
-
+#include "content/public/browser/web_ui_message_handler.h"
+namespace safe_browsing {
 SafeBrowsingUI::SafeBrowsingUI(content::WebUI* web_ui)
     : content::WebUIController(web_ui) {
   // Set up the chrome://safe-browsing source.
+
   content::WebUIDataSource* html_source = content::WebUIDataSource::Create(
       safe_browsing::kChromeUISafeBrowsingHost);
+
+  // Register callback handler.
+  // Handles messages from JavaScript to C++ via chrome.send().
+  web_ui->AddMessageHandler(base::MakeUnique<SafeBrowsingUIHandler>());
 
   // Add localized string resources.
   html_source->AddLocalizedString("sbUnderConstruction",
@@ -23,6 +35,7 @@ SafeBrowsingUI::SafeBrowsingUI(content::WebUI* web_ui)
 
   // Add required resources.
   html_source->AddResourcePath("safe_browsing.css", IDR_SAFE_BROWSING_CSS);
+  html_source->AddResourcePath("safe_browsing.js", IDR_SAFE_BROWSING_JS);
   html_source->SetDefaultResource(IDR_SAFE_BROWSING_HTML);
 
   content::BrowserContext* browser_context =
@@ -31,3 +44,19 @@ SafeBrowsingUI::SafeBrowsingUI(content::WebUI* web_ui)
 }
 
 SafeBrowsingUI::~SafeBrowsingUI() {}
+
+SafeBrowsingUIHandler::SafeBrowsingUIHandler(){};
+
+void SafeBrowsingUIHandler::ExpParamList(const base::ListValue* unused) {
+  AllowJavascript();
+  CallJavascriptFunction("safe_browsing.addExperiment", GetFeatureStatusList());
+}
+
+void SafeBrowsingUIHandler::RegisterMessages() {
+  web_ui()->RegisterMessageCallback(
+      "expParamList",
+      base::Bind(&SafeBrowsingUIHandler::ExpParamList, base::Unretained(this)));
+}
+
+SafeBrowsingUIHandler::~SafeBrowsingUIHandler() {}
+}  // namespace safe_browsing
--- components/safe_browsing/web_ui/safe_browsing_ui.h
+++ components/safe_browsing/web_ui/safe_browsing_ui.h
@@ -1,13 +1,32 @@
-// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Copyright 2017 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_WEBUI_SAFE_BROWSING_UI_H_
-#define CHROME_BROWSER_UI_WEBUI_SAFE_BROWSING_UI_H_
+#ifndef COMPONENTS_SAFE_BROWSING_WEBUI_SAFE_BROWSING_UI_H_
+#define COMPONENTS_SAFE_BROWSING_WEBUI_SAFE_BROWSING_UI_H_
 
+#include "base/bind.h"
+#include "base/callback.h"
 #include "base/macros.h"
+#include "base/values.h"
 #include "content/public/browser/url_data_source.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_controller.h"
+#include "content/public/browser/web_ui_data_source.h"
+#include "content/public/browser/web_ui_message_handler.h"
+
+namespace safe_browsing {
+class SafeBrowsingUIHandler : public content::WebUIMessageHandler {
+ public:
+  SafeBrowsingUIHandler();
+  ~SafeBrowsingUIHandler() override;
+  void ExpParamList(const base::ListValue* args);
+  void RegisterMessages() override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SafeBrowsingUIHandler);
+};
 
 // The WebUI for chrome://safe-browsing
 class SafeBrowsingUI : public content::WebUIController {
@@ -18,5 +37,6 @@ class SafeBrowsingUI : public content::WebUIController {
  private:
   DISALLOW_COPY_AND_ASSIGN(SafeBrowsingUI);
 };
+}  // namespace safe_browsing
 
-#endif  // CHROME_BROWSER_UI_WEBUI_SAFE_BROWSING_UI_H_
+#endif  // COMPONENTS_SAFE_BROWSING_WEBUI_SAFE_BROWSING_UI_H_
