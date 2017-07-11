873b4218f605f15492557da2898e51672c6b7901
Eric Roman <eroman@chromium.org>
2017-07-11 19:05:05 +0000

    Use FileNetLogObserver in headless builds.
    This replaces use of the deprecated WriteToFileNetLogObserver.
    It has the advantage of moving file I/O off the calling thread.
    
    Bug: 716570
    Change-Id: I757c276b67c845bbcade777547b6a38342127460
    Reviewed-on: https://chromium-review.googlesource.com/564198
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485688}
    Former-commit-id: 6fedbf806dbb068d8cf0ae6bc1eae1ed8c56bac4

--- headless/lib/browser/headless_net_log.cc
+++ headless/lib/browser/headless_net_log.cc
@@ -4,24 +4,22 @@
 
 #include "headless/lib/browser/headless_net_log.h"
 
-#include <stdio.h>
-
 #include <utility>
 
+#include "base/bind.h"
 #include "base/command_line.h"
 #include "base/files/file_path.h"
-#include "base/files/scoped_file.h"
 #include "base/memory/ptr_util.h"
 #include "base/values.h"
 #include "build/build_config.h"
 #include "content/public/common/content_switches.h"
+#include "net/log/file_net_log_observer.h"
 #include "net/log/net_log_util.h"
-#include "net/log/write_to_file_net_log_observer.h"
 
 namespace headless {
 namespace {
 
-std::unique_ptr<base::Value> GetHeadlessConstants() {
+std::unique_ptr<base::DictionaryValue> GetHeadlessConstants() {
   std::unique_ptr<base::DictionaryValue> constants_dict =
       net::GetNetConstants();
 
@@ -35,43 +33,24 @@ std::unique_ptr<base::Value> GetHeadlessConstants() {
 
   constants_dict->Set("clientInfo", std::move(dict));
 
-  return std::move(constants_dict);
+  return constants_dict;
 }
 
 }  // namespace
 
 HeadlessNetLog::HeadlessNetLog(const base::FilePath& log_path) {
-  // TODO(mmenke):  Other than a different set of constants, this code is
-  //     identical to code in ChromeNetLog.  Consider merging the code.
-
-  // Much like logging.h, bypass threading restrictions by using fopen
-  // directly.  Have to write on a thread that's shutdown to handle events on
-  // shutdown properly, and posting events to another thread as they occur
-  // would result in an unbounded buffer size, so not much can be gained by
-  // doing this on another thread.  It's only used when debugging, so
-  // performance is not a big concern.
-  base::ScopedFILE file;
-#if defined(OS_WIN)
-  file.reset(_wfopen(log_path.value().c_str(), L"w"));
-#elif defined(OS_POSIX)
-  file.reset(fopen(log_path.value().c_str(), "w"));
-#endif
-
-  if (!file) {
-    LOG(ERROR) << "Could not open file " << log_path.value()
-               << " for net logging";
-  } else {
-    std::unique_ptr<base::Value> constants(GetHeadlessConstants());
-    write_to_file_observer_.reset(new net::WriteToFileNetLogObserver());
-    write_to_file_observer_->StartObserving(this, std::move(file),
-                                            constants.get(), nullptr);
+  if (!log_path.empty()) {
+    net::NetLogCaptureMode capture_mode = net::NetLogCaptureMode::Default();
+    file_net_log_observer_ = net::FileNetLogObserver::CreateUnbounded(
+        log_path, GetHeadlessConstants());
+    file_net_log_observer_->StartObserving(this, capture_mode);
   }
 }
 
 HeadlessNetLog::~HeadlessNetLog() {
   // Remove the observer we own before we're destroyed.
-  if (write_to_file_observer_)
-    write_to_file_observer_->StopObserving(nullptr);
+  if (file_net_log_observer_)
+    file_net_log_observer_->StopObserving(nullptr, base::OnceClosure());
 }
 
 }  // namespace headless
