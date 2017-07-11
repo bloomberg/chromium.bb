0231d22b130749a1e640b249b4096f12bab35b2c
keishi <keishi@chromium.org>
2017-07-11 03:34:20 -0700

    Revert of Remove all of the CrOS Ozone configs from MB. (patchset #8 id:130001 of https://codereview.chromium.org/2949093004/ )
    Reason for revert:
    ash_unittests are failing on ChromeOS AsanLsan bot
    crbug.com/740859
    Regression range is 485493 - 485513 so suspecting this.
    
    Original issue's description:
    > Remove all of the CrOS Ozone configs from MB.
    >
    > Ozone is now on by default on all CrOS builds, and we don't
    > need to test the old x11 config any more. This CL flips
    > all of the CrOS builders to (implicitly) be building for Ozone,
    > so that we can then remove the explicit Ozone builders in a
    > follow-up CL.
    >
    > This builder also removes the
    > linux_chromium_chromeos_ozone_rel_ng builder from the CQ
    > since would otherwise be redundant with the
    > linux_chromium_chromeos_rel_ng builder.
    >
    > This CL also removes a couple of commented-out CQ entries for
    > builders that were removed as part of the work in bug 669297
    > (linux_chromium_chromeos_compile_dbg_ng and linux_chromium_clobber_rel_ng);
    > at the time I had planned to re-add them once the capacity
    > issues were resolved, but at this point it's unclear how
    > important those configs are and we may want to use the
    > capacity for other things, and so these comments were misleading.
    >
    > R=kylechar@chromium.org, kbr@chromium.org
    > TBR=achuith@chromium.org, xdai@chromium.org
    > BUG=671355
    >
    > Review-Url: https://codereview.chromium.org/2949093004
    > Cr-Commit-Position: refs/heads/master@{#485495}
    > Committed: https://chromium.googlesource.com/chromium/src/+/9e741f85584902e40fcbcdc023f9c52265bb0488 [formerly 7385c63ec2b3606a67bb192bf96311ada07aa1c6]
    
    TBR=kylechar@chromium.org,kbr@chromium.org,fjhenigman@chromium.org,dpranke@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=671355
    
    Review-Url: https://codereview.chromium.org/2974923002
    Cr-Commit-Position: refs/heads/master@{#485579}
    
    Former-commit-id: 9dfa3c95d67ac0348f208a8cb631c606b5cfa54f

--- chrome/browser/chromeos/login/bluetooth_host_pairing_browsertest.cc
+++ chrome/browser/chromeos/login/bluetooth_host_pairing_browsertest.cc
@@ -161,7 +161,7 @@ class BluetoothHostPairingNoInputTest : public OobeBaseTest {
   pairing_chromeos::BluetoothHostPairingController* controller_ = nullptr;
 
   bluez::FakeBluetoothDeviceClient* fake_bluetooth_device_client_ = nullptr;
-  base::MessageLoopForUI message_loop_;
+  base::MessageLoop message_loop_;
 
   DISALLOW_COPY_AND_ASSIGN(BluetoothHostPairingNoInputTest);
 };
