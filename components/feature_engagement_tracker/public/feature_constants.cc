2d733cba2738e3ff51a53687a378f4d7ac893751
Cooper Knaak <cooperknaak@google.com>
2017-07-11 18:01:08 +0000

    [iOS] FeatureEngagementTracker Feature Constants
    Added base::Feature constants for FeatureEngagementTracker IPH events.
    
    Bug: 738164
    Change-Id: I26120c392dc721451d13ecfd9dc9652ad87b5b8d
    Reviewed-on: https://chromium-review.googlesource.com/556355
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Reviewed-by: Gregory Chatzinoff <gchatz@chromium.org>
    Reviewed-by: Ed Chin <edchin@chromium.org>
    Commit-Queue: Cooper Knaak <cooperknaak@google.com>
    Cr-Commit-Position: refs/heads/master@{#485676}
    Former-commit-id: 3580a13d6b6aedde1e26c9d1850505760587c736

--- components/feature_engagement_tracker/public/feature_constants.cc
+++ components/feature_engagement_tracker/public/feature_constants.cc
@@ -32,4 +32,13 @@ const base::Feature kIPHNewTabFeature{"IPH_NewTab",
                                       base::FEATURE_DISABLED_BY_DEFAULT};
 #endif  // defined(OS_WIN) || defined(OS_LINUX)
 
+#if defined(OS_IOS)
+const base::Feature kIPHNewTabTipFeature{"IPH_NewTabTip",
+                                         base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kIPHNewIncognitoTabTipFeature{
+    "IPH_NewIncognitoTabTip", base::FEATURE_DISABLED_BY_DEFAULT};
+const base::Feature kIPHBadgedReadingListFeature{
+    "IPH_BadgedReadingList", base::FEATURE_DISABLED_BY_DEFAULT};
+#endif  // defined(OS_IOS)
+
 }  // namespace feature_engagement_tracker
