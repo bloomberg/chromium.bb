94b4ff9eff3ef04cf30cb765c2d864a505fc6a39
Florin Malita <fmalita@chromium.org>
2017-07-12 20:02:25 +0000

    Remove flag to stop relying on Skia's bilerp-hack
    This brings the raster backend into line with Ganesh, which never had this hack.
    
    Bug=skia:6836
    
    Change-Id: I58581139bddb959e45f50b7549e857c8967ed691
    Reviewed-on: https://chromium-review.googlesource.com/567267
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Mike Reed <reed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486068}
    Former-commit-id: 4d3622e28d891b3a445275d132a6817bb83973ed

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -224,10 +224,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_NONSTD_BLENDMODES
 #endif
 
-#ifndef SK_SUPPORT_LEGACY_BILERP_IGNORING_HACK
-#define SK_SUPPORT_LEGACY_BILERP_IGNORING_HACK
-#endif
-
 #ifndef SK_SUPPORT_LEGACY_MASK_BLUR
 #define SK_SUPPORT_LEGACY_MASK_BLUR
 #endif
