4c901816f0fbd5703a92bc878581e1b7edbcce2d
Sergey Ulanov <sergeyu@google.com>
2017-07-12 01:22:52 +0000

    Update StackUtil.cpp for Fuchsia
    Fuchsia implements pthread_getattr_np(), so the GNU version of StackUtil
    internals can be used there as well.
    
    Bug: 740791
    Change-Id: I3bbc7de2d5ebf1124452ed6bb6901a0dcc142457
    Reviewed-on: https://chromium-review.googlesource.com/566140
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485762}
    Former-commit-id: 50fb6a87199997a56b1662e71bc39861320de31a

--- third_party/WebKit/Source/platform/wtf/StackUtil.cpp
+++ third_party/WebKit/Source/platform/wtf/StackUtil.cpp
@@ -28,7 +28,8 @@ size_t GetUnderestimatedStackSize() {
 // FIXME: On Mac OSX and Linux, this method cannot estimate stack size
 // correctly for the main thread.
 
-#if defined(__GLIBC__) || defined(OS_ANDROID) || defined(OS_FREEBSD)
+#if defined(__GLIBC__) || defined(OS_ANDROID) || defined(OS_FREEBSD) || \
+    defined(OS_FUCHSIA)
   // pthread_getattr_np() can fail if the thread is not invoked by
   // pthread_create() (e.g., the main thread of webkit_unit_tests).
   // If so, a conservative size estimate is returned.
@@ -95,7 +96,8 @@ size_t GetUnderestimatedStackSize() {
 }
 
 void* GetStackStart() {
-#if defined(__GLIBC__) || defined(OS_ANDROID) || defined(OS_FREEBSD)
+#if defined(__GLIBC__) || defined(OS_ANDROID) || defined(OS_FREEBSD) || \
+    defined(OS_FUCHSIA)
   pthread_attr_t attr;
   int error;
 #if defined(OS_FREEBSD)
