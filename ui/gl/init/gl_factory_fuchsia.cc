d0b8c83867c4bc8197ab0bca6b37e7bff2a35448
Sergey Ulanov <sergeyu@google.com>
2017-07-12 01:30:10 +0000

    Add stubs for GL initialization functions on Fuchsia.
    Bug: 737802
    Change-Id: Ia39269fef6a67e109b731f0036ec650c0d92f522
    Reviewed-on: https://chromium-review.googlesource.com/566158
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485772}
    Former-commit-id: 392d1944ac11cf5900c99677c9b1d8074e6b7fec

---
+++ ui/gl/init/gl_factory_fuchsia.cc
@@ -0,0 +1,45 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/gl/init/gl_factory.h"
+
+#include "ui/gl/gl_context.h"
+#include "ui/gl/gl_surface.h"
+
+namespace gl {
+namespace init {
+
+// TODO(fuchsia): Implement these functions.
+
+std::vector<GLImplementation> GetAllowedGLImplementations() {
+  NOTIMPLEMENTED();
+  return std::vector<GLImplementation>();
+}
+
+bool GetGLWindowSystemBindingInfo(GLWindowSystemBindingInfo* info) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
+scoped_refptr<GLContext> CreateGLContext(GLShareGroup* share_group,
+                                         GLSurface* compatible_surface,
+                                         const GLContextAttribs& attribs) {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+scoped_refptr<GLSurface> CreateViewGLSurface(gfx::AcceleratedWidget window) {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+scoped_refptr<GLSurface> CreateOffscreenGLSurfaceWithFormat(
+    const gfx::Size& size,
+    GLSurfaceFormat format) {
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+}  // namespace init
+}  // namespace gl
