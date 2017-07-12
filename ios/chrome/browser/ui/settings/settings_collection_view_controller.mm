bd413807f783bdcecdfef5bac3d1790da7f33d47
Jérôme Lebel <jlebel@chromium.org>
2017-07-12 15:56:13 +0000

    Avoid -[SettingsCollectionViewController dealloc]
    MobileSignInPromo.SettingsManager.ImpressionsTilDismiss histogram should
    be send in |settingsWillBeDismissed| instead of |dealloc|.
    
    Bug: 709286
    Change-Id: I11a4299ab1cdf911c63a2ca7e0a097954a7f513e
    Reviewed-on: https://chromium-review.googlesource.com/568304
    Reviewed-by: Louis Romero <lpromero@chromium.org>
    Commit-Queue: Louis Romero <lpromero@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485977}
    Former-commit-id: 4418fa10448f9363de7c60f19f07a90468d3738e

--- ios/chrome/browser/ui/settings/settings_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/settings_collection_view_controller.mm
@@ -292,14 +292,6 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
 
 - (void)dealloc {
   [self stopBrowserStateServiceObservers];
-  if (!_signinStarted && _signinPromoViewMediator) {
-    PrefService* prefs = _browserState->GetPrefs();
-    int displayedCount =
-        prefs->GetInteger(prefs::kIosSettingsSigninPromoDisplayedCount);
-    UMA_HISTOGRAM_COUNTS_100(
-        "MobileSignInPromo.SettingsManager.ImpressionsTilDismiss",
-        displayedCount);
-  }
 }
 
 - (void)stopBrowserStateServiceObservers {
@@ -1034,6 +1026,14 @@ void SigninObserverBridge::GoogleSignedOut(const std::string& account_id,
 #pragma mark SettingsControllerProtocol
 
 - (void)settingsWillBeDismissed {
+  if (!_signinStarted && _signinPromoViewMediator) {
+    PrefService* prefs = _browserState->GetPrefs();
+    int displayedCount =
+        prefs->GetInteger(prefs::kIosSettingsSigninPromoDisplayedCount);
+    UMA_HISTOGRAM_COUNTS_100(
+        "MobileSignInPromo.SettingsManager.ImpressionsTilDismiss",
+        displayedCount);
+  }
   [_signinInteractionController cancel];
   [self stopBrowserStateServiceObservers];
 }
