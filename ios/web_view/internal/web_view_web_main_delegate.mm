db60fd155154323e34f1abcaa5f2b5e178782aa9
jzw <jzw@chromium.org>
2017-07-12 14:47:20 -0700

    Fix dispatch_once deadlock BUG=
    Review-Url: https://codereview.chromium.org/2977833002
    Cr-Commit-Position: refs/heads/master@{#486099}
    
    Former-commit-id: 4153aff036de400191be6373e81b2bd6b9f195bc

--- ios/web_view/internal/web_view_web_main_delegate.mm
+++ ios/web_view/internal/web_view_web_main_delegate.mm
@@ -5,7 +5,7 @@
 #import "ios/web_view/internal/web_view_web_main_delegate.h"
 
 #import "base/mac/bundle_locations.h"
-#import "ios/web_view/public/cwv_web_view.h"
+#import "ios/web_view/public/cwv_html_element.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -18,8 +18,11 @@ WebViewWebMainDelegate::WebViewWebMainDelegate() {}
 WebViewWebMainDelegate::~WebViewWebMainDelegate() = default;
 
 void WebViewWebMainDelegate::BasicStartupComplete() {
+  // Use CWVHTMLElement instead of CWVWebView and CWVWebViewConfiguration
+  // because the latter two classes' +intialize calls in to this method and may
+  // cause a deadlock.
   base::mac::SetOverrideFrameworkBundle(
-      [NSBundle bundleForClass:[CWVWebView class]]);
+      [NSBundle bundleForClass:[CWVHTMLElement class]]);
 }
 
 }  // namespace ios_web_view
