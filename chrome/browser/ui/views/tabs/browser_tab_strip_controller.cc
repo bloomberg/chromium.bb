28638ab5bf964c3d2ca088cfefa16dc8e5e40c95
Evan Stade <estade@chromium.org>
2017-07-12 16:18:38 +0000

    Reland "Views: Don't show throbber for very brief resource loads in tabs post initial load."
    This time, land behind a command line flag (the same one that controls
    the stop/reload icon toggles). Also, it was noted that this change could
    make certain actions feel sluggish, such as pressing "Save Changes" in
    monorail, because the throbber feedback was no longer immediate. To
    address this, only suppress switches to the throbber that occur right
    after the throbber has stopped showing. That way, as long as you press
    "Save Changes" a few seconds after the page has finished loading, the
    throbber feedback will be immediate, but we'll still get the suppression
    of flicker at the end of a page load (visible, for example, on yahoo.com,
    cyclingnews.com, or when navigating to an extension/app/etc. in the
    Chrome Web Store).
    
    Bug: 734104
    Change-Id: I5a59fc16785e3a5f30b40a72f03c5c6a67e66337
    Reviewed-on: https://chromium-review.googlesource.com/567800
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485985}
    Former-commit-id: 896032108c97ec8e014f5b221afd5b945a4c0389

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
@@ -313,16 +313,8 @@ void BrowserTabStripController::ShowContextMenuForTab(
 }
 
 void BrowserTabStripController::UpdateLoadingAnimations() {
-  // Don't use the model count here as it's possible for this to be invoked
-  // before we've applied an update from the model (Browser::TabInsertedAt may
-  // be processed before us and invokes this).
-  for (int i = 0, tab_count = tabstrip_->tab_count(); i < tab_count; ++i) {
-    if (model_->ContainsIndex(i)) {
-      Tab* tab = tabstrip_->tab_at(i);
-      WebContents* contents = model_->GetWebContentsAt(i);
-      tab->UpdateLoadingAnimation(TabContentsNetworkState(contents));
-    }
-  }
+  for (int i = 0, tab_count = tabstrip_->tab_count(); i < tab_count; ++i)
+    tabstrip_->tab_at(i)->StepLoadingAnimation();
 }
 
 int BrowserTabStripController::HasAvailableDragActions() const {
