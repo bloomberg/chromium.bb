37ba8fdbea40a27d89691ab1bc0a29050a1c314f
Peter E Conn <peconn@chromium.org>
2017-07-12 10:39:40 +0000

    📺 Use video aspect ratio to determine PiP params.
    Use the bounds of the fullscreen video to determine the starting
    bounds for the PiP transformation and the aspect ratio of the PiP
    window.
    
    Change-Id: Ia2227c9d277b5ae2a1dedddb899082e2947137fe
    Reviewed-on: https://chromium-review.googlesource.com/536974
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485916}
    Former-commit-id: 164f881f06faea76c87b246e2cdd17ab531416e5

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -845,7 +845,7 @@ class CONTENT_EXPORT WebContentsImpl
                     const WebContentsObserver::MediaPlayerId& id);
 
   int GetCurrentlyPlayingVideoCount() override;
-  const VideoSizeMap& GetCurrentlyPlayingVideoSizes() override;
+  base::Optional<gfx::Size> GetFullscreenVideoSize() override;
   bool IsFullscreen() override;
 
   MediaWebContentsObserver* media_web_contents_observer() {
