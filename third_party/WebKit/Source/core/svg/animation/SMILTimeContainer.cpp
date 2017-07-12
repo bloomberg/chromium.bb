657d27591f6869f96b5ff2f528298dd27bdec01b
Fredrik Söderqvist <fs@opera.com>
2017-07-12 21:55:27 +0000

    Remove kInitialFrameDelay from SMILTimeContainer::Start
    This is an old hack to preserve behavior of certain tests. These tests
    have since been fixed to not rely on this. Get rid of it and just call
    UpdateAnimationsAndScheduleFrameIfNeeded(...) instead to schedule the
    first frame just like any other.
    
    BUG=726362
    
    Change-Id: I1ff2673a8d4b71ea237a2902f10599e08bcfb7d6
    Reviewed-on: https://chromium-review.googlesource.com/564608
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Fredrik Söderquist <fs@opera.com>
    Cr-Commit-Position: refs/heads/master@{#486115}
    Former-commit-id: 75f6502efdaf80adbc9ddce8f039ee2e9b58c227

--- third_party/WebKit/Source/core/svg/animation/SMILTimeContainer.cpp
+++ third_party/WebKit/Source/core/svg/animation/SMILTimeContainer.cpp
@@ -38,7 +38,6 @@
 
 namespace blink {
 
-static const double kInitialFrameDelay = 0.025;
 static const double kAnimationPolicyOnceDuration = 3.000;
 
 SMILTimeContainer::SMILTimeContainer(SVGSVGElement& owner)
@@ -176,18 +175,10 @@ void SMILTimeContainer::Start() {
   started_ = true;
 
   // If the "presentation time" is non-zero, the timeline was modified via
-  // setElapsed() before the document began.  In this case pass on
-  // 'seekToTime=true' to updateAnimations() to issue a seek.
-  SMILTime earliest_fire_time =
-      UpdateAnimations(presentation_time_, presentation_time_ ? true : false);
-  if (!CanScheduleFrame(earliest_fire_time))
-    return;
-  // If the timeline is running, and there are pending animation updates,
-  // always perform the first update after the timeline was started using
-  // the wake-up mechanism.
-  double delay_time = earliest_fire_time.Value() - presentation_time_;
-  ScheduleWakeUp(std::max(kInitialFrameDelay, delay_time),
-                 kSynchronizeAnimations);
+  // SetElapsed() before the document began. In this case pass on
+  // seek_to_time=true to issue a seek.
+  UpdateAnimationsAndScheduleFrameIfNeeded(presentation_time_,
+                                           presentation_time_ ? true : false);
 }
 
 void SMILTimeContainer::Pause() {
@@ -530,6 +521,7 @@ SMILTime SMILTimeContainer::UpdateAnimations(double elapsed,
 }
 
 void SMILTimeContainer::AdvanceFrameForTesting() {
+  const double kInitialFrameDelay = 0.025;
   SetElapsed(Elapsed() + kInitialFrameDelay);
 }
 
