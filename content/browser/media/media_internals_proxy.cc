8e4654de481397d1196de5a1d17e5ebeafc0b5ac
Dan Sanders <sandersd@chromium.org>
2017-07-12 23:51:30 +0000

    media: Don't remove players from chrome://media-internals on process exit.
    As long as chrome://media-internals is open, it should preserve log
    entries. Log entries are still removed from the backing store, so these
    players will be removed if the page is reloaded.
    
    This CL also changes the trigger for backing store removal to include
    tab crashes. Previously there was no reliable way to remove a logs for a
    player from a crashed tab, which is problematic if the tab is incognito.
    
    Bug: 741132
    Change-Id: I92b152fa6578c3728ab22a45786e188d93fb87f2
    Reviewed-on: https://chromium-review.googlesource.com/567644
    Commit-Queue: Dan Sanders <sandersd@chromium.org>
    Reviewed-by: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486159}
    Former-commit-id: b26585d80259af0dec30ac33a4cc76664e10a6ef

--- content/browser/media/media_internals_proxy.cc
+++ content/browser/media/media_internals_proxy.cc
@@ -4,40 +4,18 @@
 
 #include "content/browser/media/media_internals_proxy.h"
 
-#include <stddef.h>
-
-#include <utility>
-
 #include "base/bind.h"
-#include "base/bind_helpers.h"
 #include "base/location.h"
-#include "base/macros.h"
-#include "base/single_thread_task_runner.h"
-#include "base/threading/thread_task_runner_handle.h"
+#include "content/browser/media/media_internals.h"
 #include "content/browser/media/media_internals_handler.h"
-#include "content/public/browser/content_browser_client.h"
-#include "content/public/browser/notification_service.h"
-#include "content/public/browser/notification_types.h"
-#include "content/public/browser/render_process_host.h"
-#include "content/public/browser/web_ui.h"
+#include "content/public/browser/browser_thread.h"
 
 namespace content {
 
 MediaInternalsProxy::MediaInternalsProxy() {
-  registrar_.Add(this, NOTIFICATION_RENDERER_PROCESS_TERMINATED,
-                 NotificationService::AllBrowserContextsAndSources());
 }
 
-void MediaInternalsProxy::Observe(int type,
-                                  const NotificationSource& source,
-                                  const NotificationDetails& details) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  DCHECK_EQ(type, NOTIFICATION_RENDERER_PROCESS_TERMINATED);
-  RenderProcessHost* process = Source<RenderProcessHost>(source).ptr();
-  CallJavaScriptFunctionOnUIThread(
-      "media.onRendererTerminated",
-      base::MakeUnique<base::Value>(process->GetID()));
-}
+MediaInternalsProxy::~MediaInternalsProxy() {}
 
 void MediaInternalsProxy::Attach(MediaInternalsMessageHandler* handler) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
@@ -65,8 +43,6 @@ void MediaInternalsProxy::GetEverything() {
       base::Bind(&MediaInternalsProxy::GetEverythingOnIOThread, this));
 }
 
-MediaInternalsProxy::~MediaInternalsProxy() {}
-
 void MediaInternalsProxy::GetEverythingOnIOThread() {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
   // TODO(xhwang): Investigate whether we can update on UI thread directly.
@@ -79,16 +55,6 @@ void MediaInternalsProxy::UpdateUIOnUIThread(const base::string16& update) {
   // Don't forward updates to a destructed UI.
   if (handler_)
     handler_->OnUpdate(update);
-}
-
-void MediaInternalsProxy::CallJavaScriptFunctionOnUIThread(
-    const std::string& function,
-    std::unique_ptr<base::Value> args) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  std::vector<const base::Value*> args_vector;
-  args_vector.push_back(args.get());
-  base::string16 update = WebUI::GetJavascriptCall(function, args_vector);
-  UpdateUIOnUIThread(update);
 }
 
 }  // namespace content
