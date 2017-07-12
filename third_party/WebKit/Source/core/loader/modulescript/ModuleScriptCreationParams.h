c45fecabac2511a9534fa2531ba08e205ca5a07e
Hiroki Nakagawa <nhiroki@chromium.org>
2017-07-12 04:23:10 +0000

    Worklet: Implement the "module responses map" concept
    This CL implements the "module responses map" concept defined in the Worklet
    spec. This map caches module scripts to ensure that WorkletGlobalScopes created
    at different times contain the same set of script source text and have the same
    behaviour.
    
    The map is not used yet in this CL. Following CLs will wire it up with
    ModuleScriptLoader via WorkletModuleResponsesMapProxy (see the design doc for
    details).
    
    Worklet spec: https://drafts.css-houdini.org/worklets/#module-responses-map
    Design doc: https://docs.google.com/document/d/1cgLcrua7H_7x_o5GlzYrAi2qt-TqTzgtOeixFAugR6g/edit?usp=sharing
    
    Bug: 726576
    Change-Id: Ib2af974e4b48eaf23ed2a38c74da5f2a503568d9
    Reviewed-on: https://chromium-review.googlesource.com/549601
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485859}
    Former-commit-id: e56293066201e2f371cbf9e91eb79292d4a32748

---
+++ third_party/WebKit/Source/core/loader/modulescript/ModuleScriptCreationParams.h
@@ -0,0 +1,49 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ModuleScriptCreationParams_h
+#define ModuleScriptCreationParams_h
+
+#include "platform/CrossThreadCopier.h"
+#include "platform/loader/fetch/AccessControlStatus.h"
+#include "platform/weborigin/KURL.h"
+#include "platform/wtf/Optional.h"
+#include "platform/wtf/text/WTFString.h"
+#include "public/platform/WebURLRequest.h"
+
+namespace blink {
+
+// A ModuleScriptCreationParams carries parameters for creating ModuleScript.
+class ModuleScriptCreationParams {
+ public:
+  ModuleScriptCreationParams(
+      const KURL& response_url,
+      const String& source_text,
+      WebURLRequest::FetchCredentialsMode fetch_credentials_mode,
+      AccessControlStatus access_control_status)
+      : response_url_(response_url),
+        source_text_(source_text),
+        fetch_credentials_mode_(fetch_credentials_mode),
+        access_control_status_(access_control_status) {}
+  ~ModuleScriptCreationParams() = default;
+
+  const KURL& GetResponseUrl() const { return response_url_; };
+  const String& GetSourceText() const { return source_text_; }
+  WebURLRequest::FetchCredentialsMode GetFetchCredentialsMode() const {
+    return fetch_credentials_mode_;
+  }
+  AccessControlStatus GetAccessControlStatus() const {
+    return access_control_status_;
+  }
+
+ private:
+  const KURL response_url_;
+  const String source_text_;
+  const WebURLRequest::FetchCredentialsMode fetch_credentials_mode_;
+  const AccessControlStatus access_control_status_;
+};
+
+}  // namespace blink
+
+#endif  // ModuleScriptCreationParams_h
---
+++ third_party/WebKit/Source/core/workers/WorkletModuleResponsesMapTest.cpp
@@ -0,0 +1,158 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/loader/modulescript/ModuleScriptCreationParams.h"
+#include "core/workers/WorkletModuleResponsesMap.h"
+#include "platform/weborigin/KURL.h"
+#include "platform/wtf/Functional.h"
+#include "platform/wtf/Optional.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+
+namespace {
+
+class ClientImpl final : public GarbageCollectedFinalized<ClientImpl>,
+                         public WorkletModuleResponsesMap::Client {
+  USING_GARBAGE_COLLECTED_MIXIN(ClientImpl);
+
+ public:
+  enum class Result { kInitial, kOK, kNeedsFetching, kFailed };
+
+  void OnRead(const ModuleScriptCreationParams& params) override {
+    ASSERT_EQ(Result::kInitial, result_);
+    result_ = Result::kOK;
+    params_.emplace(params);
+  }
+
+  void OnFetchNeeded() override {
+    ASSERT_EQ(Result::kInitial, result_);
+    result_ = Result::kNeedsFetching;
+  }
+
+  void OnFailed() override {
+    ASSERT_EQ(Result::kInitial, result_);
+    result_ = Result::kFailed;
+  }
+
+  Result GetResult() const { return result_; }
+  WTF::Optional<ModuleScriptCreationParams> GetParams() const {
+    return params_;
+  }
+
+ private:
+  Result result_ = Result::kInitial;
+  WTF::Optional<ModuleScriptCreationParams> params_;
+};
+
+}  // namespace
+
+TEST(WorkletModuleResponsesMapTest, Basic) {
+  WorkletModuleResponsesMap* map = new WorkletModuleResponsesMap;
+  const KURL kUrl(kParsedURLString, "https://example.com/foo.js");
+
+  // An initial read call creates a placeholder entry and asks the client to
+  // fetch a module script.
+  ClientImpl* client1 = new ClientImpl;
+  map->ReadOrCreateEntry(kUrl, client1);
+  EXPECT_EQ(ClientImpl::Result::kNeedsFetching, client1->GetResult());
+  EXPECT_FALSE(client1->GetParams().has_value());
+
+  // The entry is now being fetched. Following read calls should wait for the
+  // completion.
+  ClientImpl* client2 = new ClientImpl;
+  map->ReadOrCreateEntry(kUrl, client2);
+  EXPECT_EQ(ClientImpl::Result::kInitial, client2->GetResult());
+
+  ClientImpl* client3 = new ClientImpl;
+  map->ReadOrCreateEntry(kUrl, client3);
+  EXPECT_EQ(ClientImpl::Result::kInitial, client3->GetResult());
+
+  // An update call should notify the waiting clients.
+  ModuleScriptCreationParams params(kUrl, "// dummy script",
+                                    WebURLRequest::kFetchCredentialsModeOmit,
+                                    kNotSharableCrossOrigin);
+  map->UpdateEntry(kUrl, params);
+  EXPECT_EQ(ClientImpl::Result::kOK, client2->GetResult());
+  EXPECT_TRUE(client2->GetParams().has_value());
+  EXPECT_EQ(ClientImpl::Result::kOK, client3->GetResult());
+  EXPECT_TRUE(client3->GetParams().has_value());
+}
+
+TEST(WorkletModuleResponsesMapTest, Failure) {
+  WorkletModuleResponsesMap* map = new WorkletModuleResponsesMap;
+  const KURL kUrl(kParsedURLString, "https://example.com/foo.js");
+
+  // An initial read call creates a placeholder entry and asks the client to
+  // fetch a module script.
+  ClientImpl* client1 = new ClientImpl;
+  map->ReadOrCreateEntry(kUrl, client1);
+  EXPECT_EQ(ClientImpl::Result::kNeedsFetching, client1->GetResult());
+  EXPECT_FALSE(client1->GetParams().has_value());
+
+  // The entry is now being fetched. Following read calls should wait for the
+  // completion.
+  ClientImpl* client2 = new ClientImpl;
+  map->ReadOrCreateEntry(kUrl, client2);
+  EXPECT_EQ(ClientImpl::Result::kInitial, client2->GetResult());
+
+  ClientImpl* client3 = new ClientImpl;
+  map->ReadOrCreateEntry(kUrl, client3);
+  EXPECT_EQ(ClientImpl::Result::kInitial, client3->GetResult());
+
+  // An invalidation call should notify the waiting clients.
+  map->InvalidateEntry(kUrl);
+  EXPECT_EQ(ClientImpl::Result::kFailed, client2->GetResult());
+  EXPECT_FALSE(client2->GetParams().has_value());
+  EXPECT_EQ(ClientImpl::Result::kFailed, client3->GetResult());
+  EXPECT_FALSE(client3->GetParams().has_value());
+}
+
+TEST(WorkletModuleResponsesMapTest, Isolation) {
+  WorkletModuleResponsesMap* map = new WorkletModuleResponsesMap;
+  const KURL kUrl1(kParsedURLString, "https://example.com/foo.js");
+  const KURL kUrl2(kParsedURLString, "https://example.com/bar.js");
+
+  // An initial read call for |kUrl1| creates a placeholder entry and asks the
+  // client to fetch a module script.
+  ClientImpl* client1 = new ClientImpl;
+  map->ReadOrCreateEntry(kUrl1, client1);
+  EXPECT_EQ(ClientImpl::Result::kNeedsFetching, client1->GetResult());
+  EXPECT_FALSE(client1->GetParams().has_value());
+
+  // The entry is now being fetched. Following read calls for |kUrl1| should
+  // wait for the completion.
+  ClientImpl* client2 = new ClientImpl;
+  map->ReadOrCreateEntry(kUrl1, client2);
+  EXPECT_EQ(ClientImpl::Result::kInitial, client2->GetResult());
+
+  // An initial read call for |kUrl2| also creates a placeholder entry and asks
+  // the client to fetch a module script.
+  ClientImpl* client3 = new ClientImpl;
+  map->ReadOrCreateEntry(kUrl2, client3);
+  EXPECT_EQ(ClientImpl::Result::kNeedsFetching, client3->GetResult());
+  EXPECT_FALSE(client3->GetParams().has_value());
+
+  // The entry is now being fetched. Following read calls for |kUrl2| should
+  // wait for the completion.
+  ClientImpl* client4 = new ClientImpl;
+  map->ReadOrCreateEntry(kUrl2, client4);
+  EXPECT_EQ(ClientImpl::Result::kInitial, client4->GetResult());
+
+  // The read call for |kUrl2| should not affect the other entry for |kUrl1|.
+  EXPECT_EQ(ClientImpl::Result::kInitial, client2->GetResult());
+
+  // An update call for |kUrl2| should notify the waiting clients for |kUrl2|.
+  ModuleScriptCreationParams params(kUrl2, "// dummy script",
+                                    WebURLRequest::kFetchCredentialsModeOmit,
+                                    kNotSharableCrossOrigin);
+  map->UpdateEntry(kUrl2, params);
+  EXPECT_EQ(ClientImpl::Result::kOK, client4->GetResult());
+  EXPECT_TRUE(client4->GetParams().has_value());
+
+  // ... but should not notify the waiting clients for |kUrl1|.
+  EXPECT_EQ(ClientImpl::Result::kInitial, client2->GetResult());
+}
+
+}  // namespace blink
