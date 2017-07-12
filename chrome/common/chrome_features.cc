1d72bf0bca15ae6fe7d01413a8297edd68c980c8
Wei-Yin Chen (陳威尹) <wychen@chromium.org>
2017-07-12 15:39:00 +0000

    Fully launch CopylessPaste
    Set CopylessPaste to be enabled by default.
    
    BUG=682077
    
    Change-Id: Ia51f540b39689e1b7b8b4c819163787b4c95378f
    Reviewed-on: https://chromium-review.googlesource.com/567555
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#485967}
    Former-commit-id: 3c9b1cb60a0f85032fb67540f0c584e401c72479

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -134,7 +134,7 @@ const base::Feature kConsistentOmniboxGeolocation{
 #if defined(OS_ANDROID)
 // Experiment to extract structured metadata for app indexing.
 const base::Feature kCopylessPaste{"CopylessPaste",
-                                   base::FEATURE_DISABLED_BY_DEFAULT};
+                                   base::FEATURE_ENABLED_BY_DEFAULT};
 #endif
 
 #if defined(OS_WIN)
