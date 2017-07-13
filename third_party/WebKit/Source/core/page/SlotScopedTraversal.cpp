8b55e15afe8d2471d065d9a1496903e1e1a7fb16
Hayato Ito <hayato@chromium.org>
2017-07-13 07:27:15 +0000

    Move focus related files from core/dom to core/page
    This is one of the efforts to organize core/dom files.
    See https://docs.google.com/spreadsheets/d/1OydPU6r8CTj8HC4D9_gVkriJETu1Egcw2RlajYcw3FM/edit?usp=sharing
    for details.
    
    Bug: 738794
    Change-Id: Icbc8173000fbd2e81c83a2e8802a95d8945f95af
    Reviewed-on: https://chromium-review.googlesource.com/567905
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Takayoshi Kochi <kochi@chromium.org>
    Commit-Queue: Hayato Ito <hayato@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486304}
    Former-commit-id: 6048b896826a1cf55096995d89ef4c72ae72ddd9

--- third_party/WebKit/Source/core/page/SlotScopedTraversal.cpp
+++ third_party/WebKit/Source/core/page/SlotScopedTraversal.cpp
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "core/dom/SlotScopedTraversal.h"
+#include "core/page/SlotScopedTraversal.h"
 
 #include "core/dom/Element.h"
 #include "core/dom/ElementTraversal.h"
