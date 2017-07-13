20e6a01b30651b4b47b3789f74fc03c9feb87e8e
Sergey Ulanov <sergeyu@google.com>
2017-07-13 01:54:20 +0000

    Define ParamTraits<long>::Log() on Fuchsia
    Previously the function was declared, but not defined.
    
    Bug: 734791
    Change-Id: I848bca19b1e79f33c8f4b796f5b340fdade315f9
    Reviewed-on: https://chromium-review.googlesource.com/565796
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486216}
    Former-commit-id: a98ff6b50930e5fda046942cce1d8d3ff35f6e2e

--- ipc/ipc_message_utils.cc
+++ ipc/ipc_message_utils.cc
@@ -423,7 +423,7 @@ void ParamTraits<unsigned int>::Log(const param_type& p, std::string* l) {
   l->append(base::UintToString(p));
 }
 
-#if defined(OS_WIN) || defined(OS_LINUX) || \
+#if defined(OS_WIN) || defined(OS_LINUX) || defined(OS_FUCHSIA) || \
     (defined(OS_ANDROID) && defined(ARCH_CPU_64_BITS))
 void ParamTraits<long>::Log(const param_type& p, std::string* l) {
   l->append(base::Int64ToString(static_cast<int64_t>(p)));
