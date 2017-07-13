539a7e6b89193b9b5736c48f61a2bf5a91ada1ba
David Trainor <dtrainor@chromium.org>
2017-07-13 00:17:19 +0000

    Add a recovery method to FileMonitor
    Add a method that resets FileMonitor in a hard recovery attempt.  This
    CL will, with subsequent CLs that add similar recovery methods to other
    components, allow recovering (or at least cleaning up) a failed
    DownloadService Controller.
    
    The cleanup steps are:
    1. Try to erase all files in the directory.
    2. Attempt to re-initialize the component (make sure the directory is
    created and accessed).
    3. Return the result of the re-initialization to the caller.
    
    BUG=736222
    
    Change-Id: Ic5a8ad63c836eedd37aebaa817fb201a9a36663f
    Reviewed-on: https://chromium-review.googlesource.com/567865
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Shakti Sahu <shaktisahu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486176}
    Former-commit-id: c6358cc30ae742c0da6289da16c8ee4925f38827

--- components/download/internal/file_monitor_unittest.cc
+++ components/download/internal/file_monitor_unittest.cc
@@ -10,6 +10,7 @@
 #include "base/files/scoped_temp_dir.h"
 #include "base/guid.h"
 #include "base/memory/ptr_util.h"
+#include "base/optional.h"
 #include "base/test/test_simple_task_runner.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "components/download/internal/driver_entry.h"
@@ -35,6 +36,8 @@ class FileMonitorTest : public testing::Test {
   }
   ~FileMonitorTest() override = default;
 
+  void HardRecoveryResponse(bool result);
+
  protected:
   base::FilePath CreateTemporaryFile(std::string file_name);
 
@@ -43,6 +46,8 @@ class FileMonitorTest : public testing::Test {
   base::ThreadTaskRunnerHandle handle_;
   base::FilePath download_dir_;
   std::unique_ptr<FileMonitor> monitor_;
+
+  base::Optional<bool> hard_recovery_result_;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(FileMonitorTest);
@@ -55,6 +60,10 @@ base::FilePath FileMonitorTest::CreateTemporaryFile(std::string file_name) {
   file.Close();
 
   return file_path;
+}
+
+void FileMonitorTest::HardRecoveryResponse(bool result) {
+  hard_recovery_result_ = result;
 }
 
 TEST_F(FileMonitorTest, TestDeleteUnknownFiles) {
@@ -133,6 +142,25 @@ TEST_F(FileMonitorTest, TestCleanupFilesForCompletedEntries) {
   EXPECT_EQ(1u, entries_to_remove.size());
   EXPECT_FALSE(base::PathExists(entry1.target_file_path));
   EXPECT_TRUE(base::PathExists(entry2.target_file_path));
+}
+
+TEST_F(FileMonitorTest, TestHardRecovery) {
+  base::FilePath temp_file1 = CreateTemporaryFile("temp1");
+  base::FilePath temp_file2 = CreateTemporaryFile("temp2");
+
+  auto callback = base::Bind(&FileMonitorTest::HardRecoveryResponse,
+                             base::Unretained(this));
+
+  EXPECT_TRUE(base::PathExists(temp_file1));
+  EXPECT_TRUE(base::PathExists(temp_file2));
+
+  monitor_->HardRecover(callback);
+  task_runner_->RunUntilIdle();
+
+  EXPECT_TRUE(hard_recovery_result_.has_value());
+  EXPECT_TRUE(hard_recovery_result_.value());
+  EXPECT_FALSE(base::PathExists(temp_file1));
+  EXPECT_FALSE(base::PathExists(temp_file2));
 }
 
 }  // namespace download
