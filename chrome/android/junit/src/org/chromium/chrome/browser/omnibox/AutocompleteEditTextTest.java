49a24832d67ef25e6e5442631294403890a266b4
Changwan Ryu <changwan@chromium.org>
2017-07-13 00:09:20 +0000

    Avoid flicker in the new omnibox inline autocomplete
    The user may see flickering of autocomplete text if onDraw() is called
    inside keyboard app's batch edited IME operations.
    
    For example, consider the following sequence of events:
    
    1) beginBatchEdit()
    2) commitText()
    3) onDraw()
    4) endBatchEdit()
    5) onDraw()
    
    With current implementation, onDraw() at step 3) and onDraw() at step 5)
    may see different getText() because we remove span at beginBatchEdit()
    and does not add a new one until endBatchEdit().
    
    In order to ensure that span is added at the end of every IME operation,
    even including beginBatchEdit(), the current internal calls to
    beginBatchEdit() and endBatchEdit() have been separated out as
    onBeginImeCommand() and onEndImeCommand(), respectively. And then
    beginBatchEdit() and endBatchEdit() will simply increment / decrement
    batch edit count inside these new IME command guards.
    
    And we remove / add span in the IME command guards even when they are
    not the outermost ones, to ensure that onDraw() can see the span when
    necessary.
    
    Note that notification is not affected thanks to the count guard.
    
    BUG=539536
    
    Change-Id: Ia1f044dd3c2669658dd363e4375f841c9c448f37
    Reviewed-on: https://chromium-review.googlesource.com/568914
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Alexandre Elias <aelias@chromium.org>
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486169}
    Former-commit-id: 880dcbe6bd3deb9a1cabf8dcbcb223a2cfa93bd9

--- chrome/android/junit/src/org/chromium/chrome/browser/omnibox/AutocompleteEditTextTest.java
+++ chrome/android/junit/src/org/chromium/chrome/browser/omnibox/AutocompleteEditTextTest.java
@@ -191,8 +191,13 @@ public class AutocompleteEditTextTest {
 
         // User types a space.
         assertTrue(mInputConnection.beginBatchEdit());
+        // We should still show the intermediate autocomplete text to the user even in the middle of
+        // a batch edit. Otherwise, the user may see flickering of autocomplete text.
+        assertEquals("hello world", mAutocomplete.getText().toString());
         assertTrue(mInputConnection.commitText(" ", 1));
+        assertEquals("hello world", mAutocomplete.getText().toString());
         assertFalse(mAutocomplete.shouldAutocomplete());
+        assertEquals("hello world", mAutocomplete.getText().toString());
 
         mInOrder.verifyNoMoreInteractions();
         assertTrue(mInputConnection.endBatchEdit());
@@ -281,8 +286,19 @@ public class AutocompleteEditTextTest {
 
         // User types a space.
         assertTrue(mInputConnection.beginBatchEdit());
+        // We should still show the intermediate autocomplete text to the user even in the middle of
+        // a batch edit. Otherwise, the user may see flickering of autocomplete text.
+        if (isUsingSpannableModel()) {
+            assertEquals("hello world", mAutocomplete.getText().toString());
+        }
         assertTrue(mInputConnection.finishComposingText());
+        if (isUsingSpannableModel()) {
+            assertEquals("hello world", mAutocomplete.getText().toString());
+        }
         assertTrue(mInputConnection.commitText(" ", 1));
+        if (isUsingSpannableModel()) {
+            assertEquals("hello world", mAutocomplete.getText().toString());
+        }
 
         mInOrder.verifyNoMoreInteractions();
         assertTrue(mInputConnection.endBatchEdit());
