060e84c9a949f6a861a47cc0916170892c94bb16
Allen Li <ayatane@chromium.org>
2017-07-12 18:42:48 -0700

    sysmon: Add sysmon.git_metrics tests
    BUG=chromium:736911
    TEST=None
    
    Change-Id: I846b74e6f7707709b92c96fc32b130dca5497350
    Reviewed-on: https://chromium-review.googlesource.com/570635
    Commit-Ready: Allen Li <ayatane@chromium.org>
    Tested-by: Allen Li <ayatane@chromium.org>
    Reviewed-by: Allen Li <ayatane@chromium.org>

--- scripts/sysmon/git_metrics_unittest.py
+++ scripts/sysmon/git_metrics_unittest.py
@@ -4,66 +4,56 @@
 
 """Unit tests for git_metrics."""
 
+# pylint: disable=protected-access
+
 from __future__ import absolute_import
 from __future__ import print_function
 
 import mock
 import os
+import subprocess
 
-from chromite.lib import cros_test_lib, osutils
+from chromite.lib import cros_test_lib
+from chromite.lib import osutils
 from chromite.scripts.sysmon import git_metrics
 
-# pylint: disable=protected-access
-# pylint: disable=attribute-defined-outside-init
 
-class TestGitMetricsWithTempdir(cros_test_lib.TempDirTestCase):
-  """Tests for git metrics using a Git fixture."""
+class TestGitMetricCollector(cros_test_lib.TestCase):
+  """Tests for _GitMetricCollector."""
 
   def setUp(self):
-    self.git_dir = os.path.join(self.tempdir, '.git')
-    with osutils.ChdirContext(self.tempdir):
-      self.git_repo = git_metrics._GitRepo(self.git_dir)
-      self.git_repo._check_output(['init'])
-      self.git_repo._check_output(['commit', '--allow-empty', '-m', 'hi'])
+    patcher = mock.patch('infra_libs.ts_mon.common.interface.state.store',
+                         autospec=True)
+    self.store = patcher.start()
+    self.addCleanup(patcher.stop)
 
-  def test_collect_git_hash_types_are_correct(self):
-    """Tests that collecting the git hash doesn't have type conflicts."""
-    collector = git_metrics._GitMetricCollector(self.git_dir, '/foo/bar')
+  def test_collect(self):
+    with mock.patch.object(git_metrics, '_GitRepo', autospec=True) as _GitRepo:
+      instance = _GitRepo('dummy')
+      instance.get_commit_hash.return_value = \
+          '2b1ce059425edc91e013c260e59019195f927a07'
+      instance.get_commit_time.return_value = 1483257600
+      instance.get_unstaged_changes.return_value = (0, 3)
 
-    # This has the side-effect of checking the types are correct.
-    collector._collect_commit_hash_metric()
+      collector = git_metrics._GitMetricCollector('~/solciel', 'dummy')
+      collector.collect()
 
-  def test_collect_git_time_types_are_correct(self):
-    """Tests that collecting the git commit time works."""
-    collector = git_metrics._GitMetricCollector(self.git_dir, '/foo/bar')
-
-    # This has the side-effect of checking the types are correct.
-    collector._collect_timestamp_metric()
-
-  def test_collect_git_hash_calls_set(self):
-    collector = git_metrics._GitMetricCollector(self.git_dir, '/foo/bar')
-
-    with mock.patch.object(git_metrics._GitMetricCollector,
-                           '_commit_hash_metric',
-                           autospec=True) as hash_metric:
-      collector._collect_commit_hash_metric()
-
-    commit_hash = self.git_repo.get_commit_hash()
-    self.assertEqual(hash_metric.set.call_args_list, [
-        mock.call(commit_hash, {'repo': self.git_dir})
-    ])
-
-  def test_collect_git_time_calls_set(self):
-    collector = git_metrics._GitMetricCollector(self.git_dir, '/foo/bar')
-    with mock.patch.object(git_metrics._GitMetricCollector,
-                           '_timestamp_metric',
-                           autospec=True) as time_metric:
-      collector._collect_timestamp_metric()
-
-    timestamp = self.git_repo.get_commit_time()
-    self.assertEqual(time_metric.set.call_args_list, [
-        mock.call(timestamp, {'repo': self.git_dir})
-    ])
+    setter = self.store.set
+    calls = [
+        mock.call('git/hash', ('~/solciel',), None,
+                  '2b1ce059425edc91e013c260e59019195f927a07',
+                  enforce_ge=mock.ANY),
+        mock.call('git/timestamp', ('~/solciel',), None,
+                  1483257600, enforce_ge=mock.ANY),
+        mock.call('git/unstaged_changes',
+                  ('added', '~/solciel'), None,
+                  0, enforce_ge=mock.ANY),
+        mock.call('git/unstaged_changes',
+                  ('deleted', '~/solciel'), None,
+                  3, enforce_ge=mock.ANY),
+    ]
+    setter.assert_has_calls(calls)
+    self.assertEqual(len(setter.mock_calls), len(calls))
 
 
 class TestGitRepoWithTempdir(cros_test_lib.TempDirTestCase):
@@ -71,16 +61,39 @@ class TestGitRepoWithTempdir(cros_test_lib.TempDirTestCase):
 
   def setUp(self):
     self.git_dir = os.path.join(self.tempdir, '.git')
+
+    devnull = open(os.devnull, 'w')
+    self.addCleanup(devnull.close)
+
+    def call(args, **kwargs):
+      subprocess.check_call(args, stdout=devnull, stderr=devnull, **kwargs)
+
     with osutils.ChdirContext(self.tempdir):
-      self.git_repo = git_metrics._GitRepo(self.git_dir)
-      self.git_repo._check_output(['init'])
+      call(['git', 'init'])
+      call(['git', 'config', 'user.name', 'John Doe'])
+      call(['git', 'config', 'user.email', 'john@example.com'])
       with open('foo', 'w') as f:
         f.write('a\nb\nc\n')
-      self.git_repo._check_output(['add', 'foo'])
-      self.git_repo._check_output(['commit', '-m', 'hi'])
+      call(['git', 'add', 'foo'])
+      env = os.environ.copy()
+      env['GIT_AUTHOR_DATE'] = '2017-01-01T00:00:00Z'
+      env['GIT_COMMITTER_DATE'] = '2017-01-01T00:00:00Z'
+      call(['git', 'commit', '-m', 'Initial commit'], env=env)
+
+  def test_get_commit_hash(self):
+    """Test get_commit_hash()."""
+    repo = git_metrics._GitRepo(self.git_dir)
+    got = repo.get_commit_hash()
+    self.assertEqual(got, '7c88f131e520e8455e2403b88ff4f723758c5dd6')
+
+  def test_get_commit_time(self):
+    """Test get_commit_time()."""
+    repo = git_metrics._GitRepo(self.git_dir)
+    got = repo.get_commit_time()
+    self.assertEqual(got, 1483228800)
 
   def test_get_unstaged_changes(self):
-    """Test get_unstaged_changes."""
+    """Test get_unstaged_changes()."""
     with open(os.path.join(self.tempdir, 'spam'), 'w') as f:
       f.write('a\n')
     os.remove(os.path.join(self.tempdir, 'foo'))
@@ -88,14 +101,3 @@ class TestGitRepoWithTempdir(cros_test_lib.TempDirTestCase):
     added, removed = repo.get_unstaged_changes()
     self.assertEqual(added, 0)  # Does not count untracked files
     self.assertEqual(removed, 3)
-
-
-class TestMetricCollector(cros_test_lib.TestCase):
-  """Tests for _GitMetricCollector."""
-
-  def test__gitdir_expand_user(self):
-    """Test that _gitdir is expanded for user."""
-    with mock.patch.dict(os.environ, HOME='/home/arciel'):
-      collector = git_metrics._GitMetricCollector('~/solciel', 'dummy')
-    self.assertEqual(collector._gitdir, '~/solciel')
-    self.assertEqual(collector._gitrepo._gitdir, '/home/arciel/solciel')
