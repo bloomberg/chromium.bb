ac9c8a0b02a8fdd5f4fcff395936b637fdc64f9b
Mikel Astiz <mastiz@chromium.org>
2017-07-13 11:54:15 +0000

    Add browser tests for redirect-like cases that currently work well
    The goal is to verify that such pages would display a nice icon in UIs
    like the NTP, which typically use the page URL prior to client-side
    redirects.
    
    Examples that couldn't be added because they are not supported well:
    - Client-side redirect with meta refresh tags.
    - history.pushState().
    - history.replaceState().
    
    Bug: 741340
    Change-Id: Ia37ccf59c5da3322f9fb34b3f8e05b75d1d51414
    Reviewed-on: https://chromium-review.googlesource.com/567502
    Commit-Queue: Mikel Astiz <mastiz@chromium.org>
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486349}
    Former-commit-id: eccf32f192883a884c16575ac7c5c2d733747a61

--- chrome/browser/favicon/content_favicon_driver_browsertest.cc
+++ chrome/browser/favicon/content_favicon_driver_browsertest.cc
@@ -295,6 +295,53 @@ IN_PROC_BROWSER_TEST_F(ContentFaviconDriverTest,
             GetFaviconForPageURL(url, favicon_base::FAVICON).bitmap_data);
 }
 
+// Test that a page which uses JavaScript to override document.location.hash
+// gets associated favicons.
+IN_PROC_BROWSER_TEST_F(ContentFaviconDriverTest,
+                       AssociateIconWithInitialPageDespiteHashOverride) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  GURL url =
+      embedded_test_server()->GetURL("/favicon/page_with_hash_override.html");
+  GURL landing_url = embedded_test_server()->GetURL(
+      "/favicon/page_with_hash_override.html#foo");
+
+  PendingTaskWaiter waiter(web_contents(), landing_url);
+  ui_test_utils::NavigateToURLWithDisposition(
+      browser(), url, WindowOpenDisposition::CURRENT_TAB,
+      ui_test_utils::BROWSER_TEST_NONE);
+  waiter.Wait();
+
+  EXPECT_NE(nullptr,
+            GetFaviconForPageURL(url, favicon_base::FAVICON).bitmap_data);
+  EXPECT_NE(
+      nullptr,
+      GetFaviconForPageURL(landing_url, favicon_base::FAVICON).bitmap_data);
+}
+
+// Test that a page which uses JavaScript document.location.replace() to
+// navigate within the page gets associated favicons.
+IN_PROC_BROWSER_TEST_F(
+    ContentFaviconDriverTest,
+    AssociateIconWithInitialPageDespiteLocationOverrideWithinPage) {
+  ASSERT_TRUE(embedded_test_server()->Start());
+  GURL url = embedded_test_server()->GetURL(
+      "/favicon/page_with_location_override_within_page.html");
+  GURL landing_url = embedded_test_server()->GetURL(
+      "/favicon/page_with_location_override_within_page.html#foo");
+
+  PendingTaskWaiter waiter(web_contents(), landing_url);
+  ui_test_utils::NavigateToURLWithDisposition(
+      browser(), url, WindowOpenDisposition::CURRENT_TAB,
+      ui_test_utils::BROWSER_TEST_NONE);
+  waiter.Wait();
+
+  EXPECT_NE(nullptr,
+            GetFaviconForPageURL(url, favicon_base::FAVICON).bitmap_data);
+  EXPECT_NE(
+      nullptr,
+      GetFaviconForPageURL(landing_url, favicon_base::FAVICON).bitmap_data);
+}
+
 #if defined(OS_ANDROID)
 IN_PROC_BROWSER_TEST_F(ContentFaviconDriverTest,
                        LoadIconFromWebManifestDespitePushState) {
