ab002eae213d9519fa048635b18d3894aac0c219
Marti Wong <martiw@chromium.org>
2017-07-13 02:02:11 +0000

    Move synced_sessions_bridge.mm from ui/ntp/recent_tabs/ to ui/sync/
    So that this class could be reused by bookmark_collection_view for
    keeping track of the syncing status. (and in later CL, it will show
    a loading spinner when bookmark is syncing.)
    
    Bug: 696893
    Change-Id: I370c91ad1f9815c6d3c70021939140478beaebcc
    Reviewed-on: https://chromium-review.googlesource.com/567786
    Commit-Queue: Marti Wong <martiw@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Reviewed-by: Jean-François Geyelin <jif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486218}
    Former-commit-id: 927b8799d354775dd8735ac760c66c423c82059b

--- ios/chrome/browser/ui/tab_switcher/tab_switcher_model.h
+++ ios/chrome/browser/ui/tab_switcher/tab_switcher_model.h
@@ -12,7 +12,7 @@
 #include "base/mac/scoped_nsobject.h"
 #import "ios/chrome/browser/tabs/tab_model_observer.h"
 #import "ios/chrome/browser/ui/ntp/recent_tabs/synced_sessions.h"
-#import "ios/chrome/browser/ui/ntp/recent_tabs/synced_sessions_bridge.h"
+#import "ios/chrome/browser/ui/sync/synced_sessions_bridge.h"
 #import "ios/chrome/browser/ui/tab_switcher/tab_model_snapshot.h"
 
 class ChromeBrowserState;
