f9e00e1fc8d923fc533efe1053efb735bb628051
Mark Cogan <marq@google.com>
2017-07-13 11:40:05 +0000

    [ios] Remove seven unused IDC command-ids.
    This CL removes the following command-ids:
    
    IDC_FULLSCREEN: Only usage was a NOTIMPLEMENTED() case in the BVC.
    
    IDC_SWITCH_BROWSER_MODES: Unused.
    
    IDC_SETUP_FOR_TESTING: Only used in SetUpForTestingCommand, which in
    turn wasn't used anywhere.
    
    IDC_RESET_ALL_WEBVIEWS: Handled in MainController, but never sent.
    
    IDC_SHOW_PRIVACY_SETTINGS: Unused.
    
    IDC_HIDE_SETTINGS_AND_SHOW_PRIVACY_SETTINGS: Unused.
    
    IDC_CLOSE_SETTINGS_AND_OPEN_NEW_INCOGNITO_TAB: Handled in the settings
    nav controller, but never sent.
    
    Bug: 738881
    Change-Id: Id931882add017306dc8a191616cef4006d9b9421
    Reviewed-on: https://chromium-review.googlesource.com/569959
    Reviewed-by: Eric Noyau <noyau@chromium.org>
    Commit-Queue: Mark Cogan <marq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486345}
    Former-commit-id: 3a4d5851ee0f5f7d8d0e0141dda1afb87f34189e

--- ios/chrome/browser/ui/commands/ios_command_ids.h
+++ ios/chrome/browser/ui/commands/ios_command_ids.h
@@ -14,7 +14,6 @@
 
 // clang-format off
 #define IDC_NEW_TAB                                    34014
-#define IDC_FULLSCREEN                                 34030
 #define IDC_VIEW_SOURCE                                35002
 #define IDC_PRINT                                      35003
 #define IDC_FIND                                       37000
@@ -28,7 +27,6 @@
 #define IDC_NEW_INCOGNITO_TAB                          40903
 #define IDC_CLOSE_ALL_TABS                             40904
 #define IDC_SHOW_SIGNIN_IOS                            40905
-#define IDC_SWITCH_BROWSER_MODES                       40906
 #define IDC_FIND_CLOSE                                 40907
 #define IDC_FIND_UPDATE                                40908
 #define IDC_CLOSE_MODALS                               40909
@@ -38,7 +36,6 @@
 #define IDC_SHOW_SECURITY_HELP                         40913
 #define IDC_SHOW_SYNC_SETTINGS                         40914
 #define IDC_OPEN_URL                                   40915
-#define IDC_SETUP_FOR_TESTING                          40916
 #define IDC_SHOW_OTHER_DEVICES                         40917
 #define IDC_CLOSE_ALL_INCOGNITO_TABS                   40918
 #define IDC_SHOW_CONTEXTUAL_SEARCH_SETTINGS            40919
@@ -47,15 +44,11 @@
 #define IDC_REQUEST_MOBILE_SITE                        40922
 #define IDC_CLEAR_BROWSING_DATA_IOS                    40924
 #define IDC_SHOW_MAIL_COMPOSER                         40926
-#define IDC_RESET_ALL_WEBVIEWS                         40928
 #define IDC_BACK_FORWARD_IN_TAB_HISTORY                40930
-#define IDC_SHOW_PRIVACY_SETTINGS                      40934
-#define IDC_HIDE_SETTINGS_AND_SHOW_PRIVACY_SETTINGS    40935
 #define IDC_REPORT_AN_ISSUE                            40936
 #define IDC_PRELOAD_VOICE_SEARCH                       40937
 #define IDC_SHOW_BACK_HISTORY                          40938
 #define IDC_SHOW_FORWARD_HISTORY                       40939
-#define IDC_CLOSE_SETTINGS_AND_OPEN_NEW_INCOGNITO_TAB  40942
 #define IDC_SHOW_ACCOUNTS_SETTINGS                     40943
 #define IDC_CLOSE_SETTINGS                             40944
 #define IDC_SHOW_SAVE_PASSWORDS_SETTINGS               40945
