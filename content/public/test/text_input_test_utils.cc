03a77d2524e560039685d97ca0c6fefaad25ecaf
Ehsan Karamad <ekaramad@chromium.org>
2017-07-13 17:23:52 +0000

    Fix an issue in WebViewImeInteractiveTest.CompositionRangeUpdates.
    We need to properly clear CompositionRangeUpdateObserver used in the test body
    and do not rely on tear down process. There seems to be a bug in the internals
    of TextInputManagerTester where the internal observer is not properly removed
    from TextInputManager's observer list.
    
    Synthetic local testing shows that if we repeatedly pump
    RenderWidget::UpdateCompositionInfo(true), the test crashes in non-OOPIF mode. The
    reasons seems to be that TextInputManagerTester::observer_ is not removed from
    TextInputManager's observer list and a late arriving IPC for IME composition range
    during shutdown causes the test process to crash (UaF).
    
    BUG=736752, 602723
    
    Change-Id: Iebf1f20dccddae63ad587ec6f03bf6951b89c5ee
    Reviewed-on: https://chromium-review.googlesource.com/545223
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Ehsan Karamad <ekaramad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486425}
    Former-commit-id: b933c4942b1f9f74cef4d563c49fb82b3c4795da

--- content/public/test/text_input_test_utils.cc
+++ content/public/test/text_input_test_utils.cc
@@ -123,7 +123,11 @@ class TextInputManagerTester::InternalObserver
   }
 
   // WebContentsObserver implementation.
-  void WebContentsDestroyed() override { text_input_manager_ = nullptr; }
+  void WebContentsDestroyed() override {
+    DCHECK(text_input_manager_);
+    text_input_manager_->RemoveObserver(this);
+    text_input_manager_ = nullptr;
+  }
 
  private:
   TextInputManager* text_input_manager_;
