98cbde928242d1f3b1cb2ce6c1579eba96db26c3
Hector Dearman <hjd@google.com>
2017-07-14 12:07:01 +0000

    memory-infra: Stop double counting rss_anon_bytes when computing private footprint
    A refactor (crrev.com/c/555497) I made to ProcessMetricsMemoryDumpProvider
    which split logic out by platform introduced a bug. The bug caused us to
    double count rss anon bytes and never count vm swap bytes when computing
    the 'private footprint' UMA on all processes on Linux and Android. This
    produced a large increase in those UMA metrics.
    
    This fixes the bug.
    
    Bug: 742958
    Change-Id: I893774e95154f50be0f30698b17c01ec4bfbcb1d
    Reviewed-on: https://chromium-review.googlesource.com/571804
    Commit-Queue: Hector Dearman <hjd@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486742}
    Former-commit-id: bd87bf3514c2d887d21c0e21ef466b560c976ed7

--- services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_linux.cc
+++ services/resource_coordinator/public/cpp/memory_instrumentation/os_metrics_linux.cc
@@ -201,7 +201,7 @@ bool OSMetrics::FillOSMemoryDump(base::ProcessId pid,
 
   const static size_t page_size = base::GetPageSize();
   uint64_t rss_anon_bytes = (resident_pages - shared_pages) * page_size;
-  uint64_t vm_swap_bytes = (resident_pages - shared_pages) * page_size;
+  uint64_t vm_swap_bytes = process_metrics->GetVmSwapBytes();
 
   dump->platform_private_footprint.rss_anon_bytes = rss_anon_bytes;
   dump->platform_private_footprint.vm_swap_bytes = vm_swap_bytes;
