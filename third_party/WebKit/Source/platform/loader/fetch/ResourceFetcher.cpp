7dfabdbce29672dfaa760d42418cac897c607c3a
arthursonzogni <arthursonzogni@chromium.org>
2017-07-14 10:36:17 -0700

    Fix: Loading cid: resources in WebView.
    A bug has been introduced in: https://crrev.com/2834013002.
    The assumption that Content-ID URLs can't be loaded without an
    MHTMLArchive doesn't hold. It is possible for instance with the Yahoo
    Mail app. It embeds chrome as a Webview. The webview load attached image
    with the Content-ID scheme.
    
    This CL allows the load instead of blocking it.
    
    BUG=739658
    TBR=japhet@chromium.org
    
    Review-Url: https://codereview.chromium.org/2975623002
    Cr-Commit-Position: refs/heads/master@{#486796}
    
    Former-commit-id: 2cccbe128c2425ab5d68c74af8760fe6a7af72f4

--- third_party/WebKit/Source/platform/loader/fetch/ResourceFetcher.cpp
+++ third_party/WebKit/Source/platform/loader/fetch/ResourceFetcher.cpp
@@ -633,10 +633,15 @@ Resource* ResourceFetcher::RequestResource(
                                     params.Options().initiator_info.name);
   }
 
-  // An URL with the "cid" scheme can only be handled by an MHTML Archive.
-  // Abort the request when there is none.
-  if (!archive_ && resource_request.Url().ProtocolIs(kContentIdScheme))
+  // A main resource request with the "cid" scheme can only be handled by an
+  // MHTML Archive. Abort the request when there is none.
+  // Note: There are some embedders of WebView that are using Content-ID
+  // URLs for sub-resources, even without any MHTMLArchive. Please see
+  // https://crbug.com/739658.
+  if (!archive_ && factory.GetType() == Resource::kMainResource &&
+      resource_request.Url().ProtocolIs(kContentIdScheme)) {
     return nullptr;
+  }
 
   bool is_data_url = resource_request.Url().ProtocolIsData();
   bool is_static_data = is_data_url || substitute_data.IsValid() || archive_;
