10d44090fb00298fce724c564770b3fbac19c8a5
Jialiu Lin <jialiul@chromium.org>
2017-07-14 20:46:06 +0000

    Fix crash in CleanUpExpiredVerdicts
    In https://codereview.chromium.org/2895323002, I slightly changed the 
    way how verdict is cached, which makes previous cached entries no
    longer valid. Therefore, CleanUpExpiredVerdicts should also remove
    any invalid verdict entries together with the expired ones.
    
    TBR=vakh@chromium.org
    
    Bug: 742713
    Change-Id: Id9e162d485e436e4992cb83c5a4378eea3df9771
    Reviewed-on: https://chromium-review.googlesource.com/572086
    Commit-Queue: Jialiu Lin <jialiul@chromium.org>
    Reviewed-by: Luke Z <lpz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486877}
    Former-commit-id: 3a77f5146ff9049d6212ae333281a21251e72037

--- components/safe_browsing/password_protection/password_protection_service.cc
+++ components/safe_browsing/password_protection/password_protection_service.cc
@@ -180,11 +180,14 @@ PasswordProtectionService::GetCachedVerdict(
       continue;
     }
     base::DictionaryValue* verdict_entry = nullptr;
-    CHECK(verdict_dictionary->GetDictionaryWithoutPathExpansion(
-        it.key() /* cache_expression */, &verdict_entry));
+    verdict_dictionary->GetDictionaryWithoutPathExpansion(
+        it.key() /* cache_expression */, &verdict_entry);
     int verdict_received_time;
     LoginReputationClientResponse verdict;
-    CHECK(ParseVerdictEntry(verdict_entry, &verdict_received_time, &verdict));
+    // Ignore any entry that we cannot parse. These invalid entries will be
+    // cleaned up during shutdown.
+    if (!ParseVerdictEntry(verdict_entry, &verdict_received_time, &verdict))
+      continue;
     // Since password protection content settings are keyed by origin, we only
     // need to compare the path part of the cache_expression and the given url.
     std::string cache_expression_path =
@@ -609,13 +612,13 @@ bool PasswordProtectionService::RemoveExpiredVerdicts(
       continue;
 
     base::DictionaryValue* verdict_entry = nullptr;
-    CHECK(verdict_dictionary->GetDictionaryWithoutPathExpansion(
-        it.key(), &verdict_entry));
+    verdict_dictionary->GetDictionaryWithoutPathExpansion(it.key(),
+                                                          &verdict_entry);
     int verdict_received_time;
     LoginReputationClientResponse verdict;
-    CHECK(ParseVerdictEntry(verdict_entry, &verdict_received_time, &verdict));
 
-    if (IsCacheExpired(verdict_received_time, verdict.cache_duration_sec())) {
+    if (!ParseVerdictEntry(verdict_entry, &verdict_received_time, &verdict) ||
+        IsCacheExpired(verdict_received_time, verdict.cache_duration_sec())) {
       // Since DictionaryValue::Iterator cannot be used to modify the
       // dictionary, we record the keys of expired verdicts in |expired_keys|
       // and remove them in the next for-loop.
