06cda8581f4812edf0017c63a5a6aeeaf9f39346
Bruce Dawson <brucedawson@chromium.org>
2017-07-14 19:37:59 +0000

    Use constexpr to avoid kInvalidSequenceNum duplicates
    Running tools\win\ShowGlobals on Canary PDBs shows that the variable
    mojo::edk::ports::kInvalidSequenceNum shows up 21 times in both
    chrome.dll and chrome_child.dll. This appears to be due to a VC++
    quirk in how it handles integer constants that are initialized by
    some expression types. Tagging the definition as constexpr should
    ensure that this doesn't happen.
    
    Bug: 
    Change-Id: I5d72acc494f14880cb7cd5a0107e7d6eefbf7c7f
    Reviewed-on: https://chromium-review.googlesource.com/571241
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486849}
    Former-commit-id: c29dde9a01968c1f9246f7d3fa776377f874e805

--- mojo/edk/system/ports/message_queue.h
+++ mojo/edk/system/ports/message_queue.h
@@ -18,8 +18,8 @@ namespace mojo {
 namespace edk {
 namespace ports {
 
-const uint64_t kInitialSequenceNum = 1;
-const uint64_t kInvalidSequenceNum = std::numeric_limits<uint64_t>::max();
+constexpr uint64_t kInitialSequenceNum = 1;
+constexpr uint64_t kInvalidSequenceNum = std::numeric_limits<uint64_t>::max();
 
 class MessageFilter;
 
