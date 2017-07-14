07bc3e9ec2f33d8db678702b9703caf63a4922ca
Andrey Kosyakov <caseq@chromium.org>
2017-07-14 23:18:29 +0000

    DevTools: do not enable temp file system in incognito
    This effectively reverts https://codereview.chromium.org/2556803003/
    as we're now using a different approach since
    https://chromium-review.googlesource.com/569119
    The original fix did not work anyway, see
    https://bugs.chromium.org/p/chromium/issues/detail?id=490148#c17 for details.
    
    Bug: 490148, 553246
    Change-Id: Id8c395eeaf3ed1c9e8404f11705ab147869a4ff1
    Reviewed-on: https://chromium-review.googlesource.com/570197
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486930}
    Former-commit-id: 8c1ea7c7b7fc0559e586b8e8744a18855758c3a1

--- chrome/browser/ui/webui/devtools_ui.cc
+++ chrome/browser/ui/webui/devtools_ui.cc
@@ -15,8 +15,6 @@
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/devtools_frontend_host.h"
-#include "content/public/browser/site_instance.h"
-#include "content/public/browser/storage_partition.h"
 #include "content/public/browser/url_data_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -27,7 +25,6 @@
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_fetcher_delegate.h"
 #include "net/url_request/url_request_context_getter.h"
-#include "storage/browser/fileapi/file_system_context.h"
 #include "third_party/WebKit/public/public_features.h"
 
 using content::BrowserThread;
@@ -351,13 +348,6 @@ DevToolsUI::DevToolsUI(content::WebUI* web_ui)
   content::URLDataSource::Add(
       profile,
       new DevToolsDataSource(profile->GetRequestContext()));
-
-  if (!profile->IsOffTheRecord())
-    return;
-  GURL url = web_ui->GetWebContents()->GetVisibleURL();
-  GURL site = content::SiteInstance::GetSiteForURL(profile, url);
-  content::BrowserContext::GetStoragePartitionForSite(profile, site)->
-      GetFileSystemContext()->EnableTemporaryFileSystemInIncognito();
 }
 
 DevToolsUI::~DevToolsUI() {
