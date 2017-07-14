355f8c0eeea8c0ae8bdc791439c7f428e4b94db4
John Budorick <jbudorick@chromium.org>
2017-07-14 00:58:29 +0000

    [android] Add @RetryOnFailure to UrlOverridingTest.
    Failing flakily on KitKat Tablet Tester.
    
    TBR=hartmanng@chromium.org
    
    Bug: 742430
    Change-Id: Ideccbdb16ae9e5d8e9a25e44cbc07102b21076f1
    Reviewed-on: https://chromium-review.googlesource.com/570186
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486564}
    Former-commit-id: 8aee8166f3e2d569971a5866734cc017b52483eb

--- chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/externalnav/UrlOverridingTest.java
@@ -361,6 +361,7 @@ public class UrlOverridingTest {
     @Test
     @SmallTest
     @Restriction(ChromeRestriction.RESTRICTION_TYPE_TABLET)
+    @RetryOnFailure
     public void testOpenWindowFromUserGesture() throws InterruptedException {
         loadUrlAndWaitForIntentUrl(mTestServer.getURL(OPEN_WINDOW_FROM_USER_GESTURE_PAGE),
                 true, 1, true, null, true);
