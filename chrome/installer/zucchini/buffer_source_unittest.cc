21a73cf626f465bb42ebdf9cf7f322a530ba4f3e
Etienne Pierre-Doray <etiennep@chromium.org>
2017-07-14 20:48:04 +0000

    Reland "[Zucchini] Introduce buffer_source and buffer_sink."
    This is a reland of f3ed44acef2f4b3b9ba4914545f640a12d112545 [formerly 9d704fa8d77d2e9b7a9ae4d70b0d125891ef963a]
    Original change's description:
    > [Zucchini] Introduce buffer_source and buffer_sink.
    > 
    > Cursors with convenient read and write methods to ease manipulation of
    > raw data.
    > 
    > Bug: 729154
    > Change-Id: I8a7303964b02b9df7ef033de8756b09716a91eae
    > Reviewed-on: https://chromium-review.googlesource.com/560657
    > Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    > Reviewed-by: Samuel Huang <huangs@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#486238}
    
    Bug: 729154
    Change-Id: I97552e41b452081bd8421d2a5be61a2f3b59e6b2
    Reviewed-on: https://chromium-review.googlesource.com/571177
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486878}
    Former-commit-id: e08252371b4d48a1ad1d44faa8f9b062d00efe70

---
+++ chrome/installer/zucchini/buffer_source_unittest.cc
@@ -0,0 +1,170 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/installer/zucchini/buffer_source.h"
+
+#include <cstdint>
+#include <iterator>
+#include <vector>
+
+#include "base/test/gtest_util.h"
+
+namespace zucchini {
+
+using vec = std::vector<uint8_t>;
+
+static constexpr size_t kLen = 10;
+constexpr const uint8_t bytes[kLen] = {0x10, 0x32, 0x54, 0x76, 0x98,
+                                       0xBA, 0xDC, 0xFE, 0x10, 0x00};
+
+class BufferSourceTest : public testing::Test {
+ protected:
+  BufferSource source_ = {std::begin(bytes), kLen};
+};
+
+TEST_F(BufferSourceTest, Skip) {
+  EXPECT_EQ(kLen, source_.Remaining());
+  source_.Skip(2);
+  EXPECT_EQ(kLen - 2, source_.Remaining());
+  source_.Skip(10);  // Skipping past end just moves cursor to end.
+  EXPECT_EQ(size_t(0), source_.Remaining());
+}
+
+TEST_F(BufferSourceTest, CheckNextBytes) {
+  EXPECT_TRUE(source_.CheckNextBytes({0x10, 0x32, 0x54, 0x76}));
+  source_.Skip(4);
+  EXPECT_TRUE(source_.CheckNextBytes({0x98, 0xBA, 0xDC, 0xFE}));
+
+  // Cursor has not advanced, so check fails.
+  EXPECT_FALSE(source_.CheckNextBytes({0x10, 0x00}));
+
+  source_.Skip(4);
+  EXPECT_EQ(size_t(2), source_.Remaining());
+
+  // Goes beyond end by 2 bytes.
+  EXPECT_FALSE(source_.CheckNextBytes({0x10, 0x00, 0x00, 0x00}));
+  EXPECT_EQ(size_t(2), source_.Remaining());
+}
+
+TEST_F(BufferSourceTest, ConsumeBytes) {
+  EXPECT_FALSE(source_.ConsumeBytes({0x10, 0x00}));
+  EXPECT_EQ(kLen, source_.Remaining());
+  EXPECT_TRUE(source_.ConsumeBytes({0x10, 0x32, 0x54, 0x76}));
+  EXPECT_EQ(size_t(6), source_.Remaining());
+  EXPECT_TRUE(source_.ConsumeBytes({0x98, 0xBA, 0xDC, 0xFE}));
+  EXPECT_EQ(size_t(2), source_.Remaining());
+
+  // Goes beyond end by 2 bytes.
+  EXPECT_FALSE(source_.ConsumeBytes({0x10, 0x00, 0x00, 0x00}));
+  EXPECT_EQ(size_t(2), source_.Remaining());
+}
+
+TEST_F(BufferSourceTest, CheckNextValue) {
+  EXPECT_TRUE(source_.CheckNextValue(uint32_t(0x76543210)));
+  EXPECT_FALSE(source_.CheckNextValue(uint32_t(0x0)));
+  EXPECT_TRUE(source_.CheckNextValue(uint64_t(0xFEDCBA9876543210)));
+  EXPECT_FALSE(source_.CheckNextValue(uint64_t(0x0)));
+
+  source_.Skip(8);
+  EXPECT_EQ(size_t(2), source_.Remaining());
+
+  // Goes beyond end by 2 bytes.
+  EXPECT_FALSE(source_.CheckNextValue(uint32_t(0x1000)));
+}
+
+// Supported by MSVC, g++, and clang++.
+// Ensures no gaps in packing.
+#pragma pack(push, 1)
+struct ValueType {
+  uint32_t a;
+  uint16_t b;
+};
+#pragma pack(pop)
+
+TEST_F(BufferSourceTest, GetValueIntegral) {
+  uint32_t value = 0;
+  EXPECT_TRUE(source_.GetValue(&value));
+  EXPECT_EQ(uint32_t(0x76543210), value);
+  EXPECT_EQ(size_t(6), source_.Remaining());
+
+  EXPECT_TRUE(source_.GetValue(&value));
+  EXPECT_EQ(uint32_t(0xFEDCBA98), value);
+  EXPECT_EQ(size_t(2), source_.Remaining());
+
+  EXPECT_FALSE(source_.GetValue(&value));
+  EXPECT_EQ(size_t(2), source_.Remaining());
+}
+
+TEST_F(BufferSourceTest, GetValueAggregate) {
+  ValueType value = {};
+  EXPECT_TRUE(source_.GetValue(&value));
+  EXPECT_EQ(uint32_t(0x76543210), value.a);
+  EXPECT_EQ(uint32_t(0xBA98), value.b);
+  EXPECT_EQ(size_t(4), source_.Remaining());
+}
+
+TEST_F(BufferSourceTest, GetRegion) {
+  ConstBufferView region;
+  EXPECT_TRUE(source_.GetRegion(0, &region));
+  EXPECT_EQ(kLen, source_.Remaining());
+  EXPECT_TRUE(region.empty());
+
+  EXPECT_TRUE(source_.GetRegion(2, &region));
+  EXPECT_EQ(size_t(2), region.size());
+  EXPECT_EQ(vec({0x10, 0x32}), vec(region.begin(), region.end()));
+  EXPECT_EQ(size_t(8), source_.Remaining());
+
+  EXPECT_FALSE(source_.GetRegion(kLen, &region));
+  EXPECT_EQ(size_t(8), source_.Remaining());
+  // |region| is left untouched.
+  EXPECT_EQ(vec({0x10, 0x32}), vec(region.begin(), region.end()));
+  EXPECT_EQ(size_t(2), region.size());
+}
+
+TEST_F(BufferSourceTest, GetPointerIntegral) {
+  const uint32_t* ptr = source_.GetPointer<uint32_t>();
+  EXPECT_NE(nullptr, ptr);
+  EXPECT_EQ(uint32_t(0x76543210), *ptr);
+  EXPECT_EQ(size_t(6), source_.Remaining());
+
+  ptr = source_.GetPointer<uint32_t>();
+  EXPECT_NE(nullptr, ptr);
+  EXPECT_EQ(uint32_t(0xFEDCBA98), *ptr);
+  EXPECT_EQ(size_t(2), source_.Remaining());
+
+  EXPECT_EQ(nullptr, source_.GetPointer<uint32_t>());
+  EXPECT_EQ(size_t(2), source_.Remaining());
+}
+
+TEST_F(BufferSourceTest, GetPointerAggregate) {
+  const ValueType* ptr = source_.GetPointer<ValueType>();
+  EXPECT_NE(nullptr, ptr);
+  EXPECT_EQ(uint32_t(0x76543210), ptr->a);
+  EXPECT_EQ(uint32_t(0xBA98), ptr->b);
+  EXPECT_EQ(size_t(4), source_.Remaining());
+}
+
+TEST_F(BufferSourceTest, GetArrayIntegral) {
+  EXPECT_EQ(nullptr, source_.GetArray<uint32_t>(3));
+
+  const uint32_t* ptr = source_.GetArray<uint32_t>(2);
+  EXPECT_NE(nullptr, ptr);
+  EXPECT_EQ(uint32_t(0x76543210), ptr[0]);
+  EXPECT_EQ(uint32_t(0xFEDCBA98), ptr[1]);
+  EXPECT_EQ(size_t(2), source_.Remaining());
+}
+
+TEST_F(BufferSourceTest, GetArrayAggregate) {
+  const ValueType* ptr = source_.GetArray<ValueType>(2);
+  EXPECT_EQ(nullptr, ptr);
+
+  ptr = source_.GetArray<ValueType>(1);
+
+  EXPECT_NE(nullptr, ptr);
+  EXPECT_EQ(uint32_t(0x76543210), ptr[0].a);
+  EXPECT_EQ(uint32_t(0xBA98), ptr[0].b);
+  EXPECT_EQ(size_t(4), source_.Remaining());
+}
+
+}  // namespace zucchini
