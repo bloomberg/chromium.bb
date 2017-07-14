8937b8c5af7a06b2c727d41949dde6f8b33e5f3a
jzw <jzw@chromium.org>
2017-07-14 13:23:31 -0700

    Use dummy class to locate NSBundle.
    This is the safest way to avoid potential dead lock issues with
    dispatch_once and ios_web_view::InitializeGlobalState().
    
    BUG=
    
    Review-Url: https://codereview.chromium.org/2980983002
    Cr-Commit-Position: refs/heads/master@{#486867}
    
    Former-commit-id: fcc9f6614289d1e710fb49bd4e116fbdc08cb966

--- ios/web_view/internal/web_view_web_main_delegate.mm
+++ ios/web_view/internal/web_view_web_main_delegate.mm
@@ -5,11 +5,17 @@
 #import "ios/web_view/internal/web_view_web_main_delegate.h"
 
 #import "base/mac/bundle_locations.h"
-#import "ios/web_view/public/cwv_html_element.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
+
+// Dummy class used to locate the containing NSBundle.
+@interface CWVBundleLocator : NSObject
+@end
+
+@implementation CWVBundleLocator
+@end
 
 namespace ios_web_view {
 
@@ -18,11 +24,8 @@ WebViewWebMainDelegate::WebViewWebMainDelegate() {}
 WebViewWebMainDelegate::~WebViewWebMainDelegate() = default;
 
 void WebViewWebMainDelegate::BasicStartupComplete() {
-  // Use CWVHTMLElement instead of CWVWebView and CWVWebViewConfiguration
-  // because the latter two classes' +intialize calls in to this method and may
-  // cause a deadlock.
   base::mac::SetOverrideFrameworkBundle(
-      [NSBundle bundleForClass:[CWVHTMLElement class]]);
+      [NSBundle bundleForClass:[CWVBundleLocator class]]);
 }
 
 }  // namespace ios_web_view
