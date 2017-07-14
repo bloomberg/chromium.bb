b930e58cfe70bc57332640bc17ab685dba9021d7
Jean-François Geyelin <jif@chromium.org>
2017-07-14 01:53:33 +0000

    [iOS] Factor out Keyboard Accessory View button creation.
    Those buttons will eventually be used both in a Keyboard Accessory View (KAV)
    (on iPhone) and as UITextInputAssistantItems (on iPad).
    
    The toolbar_assistive_keyboard_views.* files are in their own directory.
    A follow up CL will move all the code for the KAV in that directory.
    
    Bug: None
    Change-Id: I430791e16aaab98e1dce92f311e85f600370f420
    Reviewed-on: https://chromium-review.googlesource.com/567509
    Commit-Queue: Jean-François Geyelin <jif@chromium.org>
    Reviewed-by: Justin Cohen <justincohen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486622}
    Former-commit-id: 3765c417749915217a559702e2171e9af42c8e2b

---
+++ ios/chrome/browser/ui/toolbar/keyboard_assist/toolbar_assistive_keyboard_views.mm
@@ -0,0 +1,59 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/toolbar/keyboard_assist/toolbar_assistive_keyboard_views.h"
+
+#import "ios/chrome/browser/ui/toolbar/keyboard_accessory_view_delegate.h"
+#import "ios/chrome/browser/ui/uikit_ui_util.h"
+#include "ios/chrome/grit/ios_strings.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+namespace {
+
+UIButton* ButtonWitIcon(NSString* iconName) {
+  const CGFloat kButtonShadowOpacity = 0.35;
+  const CGFloat kButtonShadowRadius = 1.0;
+  const CGFloat kButtonShadowVerticalOffset = 1.0;
+
+  UIButton* button = [UIButton buttonWithType:UIButtonTypeCustom];
+  [button setTranslatesAutoresizingMaskIntoConstraints:NO];
+  UIImage* icon = [UIImage imageNamed:iconName];
+  [button setImage:icon forState:UIControlStateNormal];
+  button.layer.shadowColor = [UIColor blackColor].CGColor;
+  button.layer.shadowOffset = CGSizeMake(0, kButtonShadowVerticalOffset);
+  button.layer.shadowOpacity = kButtonShadowOpacity;
+  button.layer.shadowRadius = kButtonShadowRadius;
+  return button;
+}
+
+}  // namespace
+
+NSArray<UIButton*>* ToolbarAssistiveKeyboardLeadingButtons(
+    id<KeyboardAccessoryViewDelegate> delegate) {
+  UIButton* voiceSearchButton =
+      ButtonWitIcon(@"keyboard_accessory_voice_search");
+  [voiceSearchButton addTarget:delegate
+                        action:@selector(keyboardAccessoryVoiceSearchTouchDown:)
+              forControlEvents:UIControlEventTouchDown];
+  SetA11yLabelAndUiAutomationName(voiceSearchButton,
+                                  IDS_IOS_KEYBOARD_ACCESSORY_VIEW_VOICE_SEARCH,
+                                  @"Voice Search");
+  [voiceSearchButton
+             addTarget:delegate
+                action:@selector(keyboardAccessoryVoiceSearchTouchUpInside:)
+      forControlEvents:UIControlEventTouchUpInside];
+
+  UIButton* cameraButton = ButtonWitIcon(@"keyboard_accessory_qr_scanner");
+  [cameraButton addTarget:delegate
+                   action:@selector(keyboardAccessoryCameraSearchTouchUpInside:)
+         forControlEvents:UIControlEventTouchUpInside];
+  SetA11yLabelAndUiAutomationName(
+      cameraButton, IDS_IOS_KEYBOARD_ACCESSORY_VIEW_QR_CODE_SEARCH,
+      @"QR code Search");
+
+  return @[ voiceSearchButton, cameraButton ];
+}
