615c5ee5622fcaf8da26622d3270d1c84e0ddd55
gklassen <gklassen@chromium.org>
2017-07-14 12:42:33 -0700

    Implement HitTestAggregator
    Implements a HitTestAggregator component to facilitate cross process
    hit testing with minimal process hops.
    
    The HitTestAggregator maintains a list of HitTestData objects that will
    be created and sent with each CompositorFrame.  These are aggregated
    into a single DisplayHitTest data structure that can be made available
    in shared memory to facilitate hit testing with minimal process hops
    for any Display.
    
    HitTestAggregator listens as surfaces are added to the current
    CompositorFrame to ensure that the list only includes information that
    matches what has been rendered in the current frame.
    
    Aggregation occurs on the BeginFrame notification.
    
    This change includes the implementaiton of the HitTestAggregator and
    unit test cases to verify the intended behaviour.
    
    Next steps will include completion of the creation and transport of
    HitTestData objects, implementation of shared memory and using this
    component to target incoming events.
    
    BUG=732399
    
    Review-Url: https://codereview.chromium.org/2938953002
    Cr-Commit-Position: refs/heads/master@{#486852}
    
    Former-commit-id: 57cb4a50cf2c5e3a2c7015a4d335aa894c982338

---
+++ components/viz/common/hit_test/aggregated_hit_test_region.h
@@ -0,0 +1,49 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_VIZ_COMMON_HIT_TEST_AGGREGATED_HIT_TEST_REGION_H_
+#define COMPONENTS_VIZ_COMMON_HIT_TEST_AGGREGATED_HIT_TEST_REGION_H_
+
+#include <stdint.h>
+
+#include "components/viz/common/surfaces/surface_id.h"
+#include "ui/gfx/geometry/rect.h"
+#include "ui/gfx/transform.h"
+
+namespace viz {
+
+// A AggregatedHitTestRegion element with child_count of kEndOfList indicates
+// the last element and end of the list.
+constexpr int kEndOfList = -1;
+
+// An array of AggregatedHitTestRegion elements is used to define the
+// aggregated hit-test data for the Display.
+//
+// It is designed to be in shared memory so that the viz service can
+// write the hit_test data, and the viz host can read without
+// process hops.
+struct AggregatedHitTestRegion {
+  // The FrameSinkId corresponding to this region.  Events that match
+  // are routed to this surface.
+  FrameSinkId frame_sink_id;
+
+  // Flags to indicate the type of region as defined for
+  // mojom::HitTestRegion
+  uint32_t flags;
+
+  // The rectangle that defines the region in parent region's coordinate space.
+  gfx::Rect rect;
+
+  // The transform applied to the rect in parent region's coordinate space.
+  gfx::Transform transform;
+
+  // The number of children including their children below this entry.
+  // If this element is not matched then child_count elements can be skipped
+  // to move to the next entry.
+  int child_count;
+};
+
+}  // namespace viz
+
+#endif  // COMPONENTS_VIZ_COMMON_HIT_TEST_AGGREGATED_HIT_TEST_REGION_H_
