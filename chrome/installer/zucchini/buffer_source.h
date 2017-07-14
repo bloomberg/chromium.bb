b25bbb1eedaed8a4a79bc38421ce1bfa3d229f0d
Etienne Pierre-Doray <etiennep@chromium.org>
2017-07-14 20:48:04 +0000

    Reland "[Zucchini] Introduce buffer_source and buffer_sink."
    This is a reland of c80ccc04aa20cae447f9cce19bb3fb34db4ef6ce [formerly 9d704fa8d77d2e9b7a9ae4d70b0d125891ef963a]
    Original change's description:
    > [Zucchini] Introduce buffer_source and buffer_sink.
    > 
    > Cursors with convenient read and write methods to ease manipulation of
    > raw data.
    > 
    > Bug: 729154
    > Change-Id: I8a7303964b02b9df7ef033de8756b09716a91eae
    > Reviewed-on: https://chromium-review.googlesource.com/560657
    > Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    > Reviewed-by: Samuel Huang <huangs@chromium.org>
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#486238}
    
    Bug: 729154
    Change-Id: I97552e41b452081bd8421d2a5be61a2f3b59e6b2
    Reviewed-on: https://chromium-review.googlesource.com/571177
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486878}
    Former-commit-id: e08252371b4d48a1ad1d44faa8f9b062d00efe70

---
+++ chrome/installer/zucchini/buffer_source.h
@@ -0,0 +1,125 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_INSTALLER_ZUCCHINI_BUFFER_SOURCE_H_
+#define CHROME_INSTALLER_ZUCCHINI_BUFFER_SOURCE_H_
+
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <initializer_list>
+#include <type_traits>
+
+#include "chrome/installer/zucchini/buffer_view.h"
+
+namespace zucchini {
+
+// BufferSource acts like an input stream with convenience methods to parse data
+// from a contiguous sequence of raw data. The underlying ConstBufferView
+// emulates a cursor to track current read position, and guards against buffer
+// overrun. Where applicable, BufferSource should be passed by pointer to
+// maintain cursor progress across reads.
+class BufferSource : public ConstBufferView {
+ public:
+  static BufferSource FromRange(const_iterator first, const_iterator last) {
+    return BufferSource(ConstBufferView::FromRange(first, last));
+  }
+
+  using ConstBufferView::ConstBufferView;
+  BufferSource() = default;
+  explicit BufferSource(ConstBufferView buffer);
+  BufferSource(const BufferSource&) = default;
+
+  BufferSource& operator=(BufferSource&&) = default;
+
+  // Moves cursor forward by |n| bytes, or until end if data is exhausted.
+  // Returns a reference to *this, to allow chaining, e.g.:
+  //   if (!buffer_source.Skip(1024).GetValue<uint32_t>(&value)) {
+  //      ... // Handle error.
+  //   }
+  // Notice that Skip() defers error handling to GetValue().
+  BufferSource& Skip(size_type n);
+
+  // Returns true if |value| matches data starting at cursor when reinterpreted
+  // as the integral type |T|.
+  template <class T>
+  bool CheckNextValue(const T& value) const {
+    static_assert(std::is_integral<T>::value,
+                  "Value type must be an integral type");
+
+    DCHECK_NE(begin(), nullptr);
+    if (Remaining() < sizeof(T))
+      return false;
+    return value == *reinterpret_cast<const T*>(begin());
+  }
+
+  // Returns true if the next bytes.size() bytes at the cursor match those in
+  // |bytes|.
+  bool CheckNextBytes(std::initializer_list<uint8_t> bytes) const;
+
+  // Same as CheckNextBytes(), but moves the cursor by bytes.size() if read is
+  // successfull.
+  bool ConsumeBytes(std::initializer_list<uint8_t> bytes);
+
+  // Tries to reinterpret data as type |T|, starting at cursor and to write the
+  // result into |value|, while moving the cursor forward by sizeof(T). Returns
+  // true if sufficient data is available, and false otherwise.
+  template <class T>
+  bool GetValue(T* value) {
+    static_assert(std::is_standard_layout<T>::value,
+                  "Value type must be a standard layout type");
+
+    DCHECK_NE(begin(), nullptr);
+    if (Remaining() < sizeof(T))
+      return false;
+    *value = *reinterpret_cast<const T*>(begin());
+    remove_prefix(sizeof(T));
+    return true;
+  }
+
+  // Tries to reinterpret data as type |T| at cursor and to return a
+  // reinterpreted pointer of type |T| pointing into the underlying data, while
+  // moving the cursor forward by sizeof(T). Returns nullptr if insufficient
+  // data is available.
+  template <class T>
+  const T* GetPointer() {
+    static_assert(std::is_standard_layout<T>::value,
+                  "Value type must be a standard layout type");
+
+    DCHECK_NE(begin(), nullptr);
+    if (Remaining() < sizeof(T))
+      return nullptr;
+    const T* ptr = reinterpret_cast<const T*>(begin());
+    remove_prefix(sizeof(T));
+    return ptr;
+  }
+
+  // Tries to reinterpret data as an array of type |T| with |count| elements,
+  // starting at cursor, and to return a reinterpreted pointer of type |T|
+  // pointing into the underlying data, while advancing the cursor beyond the
+  // array. Returns nullptr if insufficient data is available.
+  template <class T>
+  const T* GetArray(size_t count) {
+    static_assert(std::is_standard_layout<T>::value,
+                  "Value type must be a standard layout type");
+
+    if (Remaining() / sizeof(T) < count)
+      return nullptr;
+    const T* array = reinterpret_cast<const T*>(begin());
+    remove_prefix(count * sizeof(T));
+    return array;
+  }
+
+  // If sufficient data is available, assigns |buffer| to point to a region of
+  // |size| bytes starting at cursor, while advancing the cursor beyond the
+  // region, and returns true. Otherwise returns false.
+  bool GetRegion(size_type size, ConstBufferView* buffer);
+
+  // Returns the number of bytes remaining from cursor until end.
+  size_type Remaining() const { return size(); }
+};
+
+}  // namespace zucchini
+
+#endif  // CHROME_INSTALLER_ZUCCHINI_BUFFER_SOURCE_H_
--- chrome/installer/zucchini/buffer_view.h
+++ chrome/installer/zucchini/buffer_view.h
@@ -28,7 +28,7 @@ class BufferViewBase {
   using difference_type = std::ptrdiff_t;
 
   static BufferViewBase FromRange(iterator first, iterator last) {
-    DCHECK(last >= first);
+    DCHECK_GE(last, first);
     BufferViewBase ret;
     ret.first_ = first;
     ret.last_ = last;
@@ -39,7 +39,7 @@ class BufferViewBase {
 
   BufferViewBase(iterator first, size_type size)
       : first_(first), last_(first_ + size) {
-    DCHECK(last_ >= first_);
+    DCHECK_GE(last_, first_);
   }
 
   BufferViewBase(const BufferViewBase&) = default;
@@ -57,7 +57,7 @@ class BufferViewBase {
   // Returns the raw value at specified location |pos|.
   // If |pos| is not within the range of the buffer, the process is terminated.
   reference operator[](size_type pos) const {
-    CHECK(first_ + pos < last_);
+    CHECK_LT(first_ + pos, last_);
     return first_[pos];
   }
 
@@ -69,8 +69,14 @@ class BufferViewBase {
   // Modifiers
 
   void shrink(size_type new_size) {
-    DCHECK(first_ + new_size <= last_);
+    DCHECK_LE(first_ + new_size, last_);
     last_ = first_ + new_size;
+  }
+
+  // Moves the start of the view forward by n bytes.
+  void remove_prefix(size_type n) {
+    DCHECK_LE(n, size());
+    first_ += n;
   }
 
  private:
