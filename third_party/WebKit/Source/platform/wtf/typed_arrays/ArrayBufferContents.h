1077ebcb3ee6e687ecacb438bfd49297c8808db4
Eric Seckler <eseckler@chromium.org>
2017-07-14 15:46:13 +0000

    [blink] Fix gcc compilation in ArrayBufferContents.h
    Bug: 
    Change-Id: I9b060df69df9893e65d90a58676d77bd3ec0da42
    Reviewed-on: https://chromium-review.googlesource.com/571745
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486767}
    Former-commit-id: 0bd0353af40ee9d30b2ff0e697d761d505222683

--- third_party/WebKit/Source/platform/wtf/typed_arrays/ArrayBufferContents.h
+++ third_party/WebKit/Source/platform/wtf/typed_arrays/ArrayBufferContents.h
@@ -122,7 +122,7 @@ class WTF_EXPORT ArrayBufferContents {
     void* Data() const { return data_; }
     size_t DataLength() const { return data_length_; }
 
-    AllocationKind AllocationKind() const { return kind_; }
+    ArrayBufferContents::AllocationKind AllocationKind() const { return kind_; }
 
     operator bool() const { return allocation_base_; }
 
