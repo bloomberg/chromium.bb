4cabb0caf0094e36131de674bc9339ae7a622632
hubbe <hubbe@chromium.org>
2017-07-14 16:10:41 -0700

    media: Allow suspend on HTTP 200 videos
    Currently, connections are kept open indefinitely, which
    can block future loads from a server if there are 6 or more
    <video> tags on the same page.  This should fix that in most
    cases.
    
    BUG=716748
    
    Review-Url: https://codereview.chromium.org/2979893002
    Cr-Commit-Position: refs/heads/master@{#486919}
    
    Former-commit-id: e2cc88c09afab5f066c83032548c1180d99c5ae7

--- media/blink/webmediaplayer_impl.cc
+++ media/blink/webmediaplayer_impl.cc
@@ -1820,6 +1820,11 @@ void WebMediaPlayerImpl::DataSourceInitialized(bool success) {
     return;
   }
 
+  // No point in preloading data as we'll probably just throw it away anyways.
+  if (IsStreaming() && preload_ > MultibufferDataSource::METADATA) {
+    data_source_->SetPreload(MultibufferDataSource::METADATA);
+  }
+
   StartPipeline();
 }
 
@@ -2100,7 +2105,18 @@ void WebMediaPlayerImpl::UpdatePlayState() {
   bool can_auto_suspend = true;
 #else
   bool is_remote = false;
-  bool can_auto_suspend = !disable_pipeline_auto_suspend_ && !IsStreaming();
+  bool can_auto_suspend = !disable_pipeline_auto_suspend_;
+  // For streaming videos, we only allow suspending at the very beginning of the
+  // video, and only if we know the length of the video. (If we don't know
+  // the length, it might be a dynamically generated video, and suspending
+  // will not work at all.)
+  if (IsStreaming()) {
+    bool at_beginning =
+        ready_state_ == WebMediaPlayer::kReadyStateHaveNothing ||
+        CurrentTime() == 0.0;
+    if (!at_beginning || GetPipelineMediaDuration() == kInfiniteDuration)
+      can_auto_suspend = false;
+  }
 #endif
 
   bool is_suspended = pipeline_controller_.IsSuspended();
