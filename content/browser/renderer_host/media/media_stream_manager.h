4202b0180019958f8414c511966e662f256708b2
Mark Pilgrim <pilgrim@chromium.org>
2017-07-14 19:43:24 +0000

    Speech recognition: BindOnce refactoring
    Use BindOnce in RecognitionAllowedCallback for MediaRequestPermissionCallback
    
    Bug: 714018
    Change-Id: I01e4eed7b75cd8828bd0dba80ca1fc07015a517f
    Reviewed-on: https://chromium-review.googlesource.com/570510
    Commit-Queue: Mark Pilgrim <pilgrim@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Tommi <tommi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486853}
    Former-commit-id: 35434205f102024c1aa53b64b2ba99438bdb4703

--- content/browser/renderer_host/media/media_stream_manager.h
+++ content/browser/renderer_host/media/media_stream_manager.h
@@ -73,8 +73,8 @@ class CONTENT_EXPORT MediaStreamManager
       public base::PowerObserver {
  public:
   // Callback to deliver the result of a media request.
-  typedef base::Callback<void(const MediaStreamDevices& devices,
-                              std::unique_ptr<MediaStreamUIProxy> ui)>
+  typedef base::OnceCallback<void(const MediaStreamDevices& devices,
+                                  std::unique_ptr<MediaStreamUIProxy> ui)>
       MediaRequestResponseCallback;
 
   // Callback for testing.
@@ -124,13 +124,12 @@ class CONTENT_EXPORT MediaStreamManager
   // used to determine where the infobar will appear to the user. |callback| is
   // used to send the selected device to the clients. An empty list of device
   // will be returned if the users deny the access.
-  std::string MakeMediaAccessRequest(
-      int render_process_id,
-      int render_frame_id,
-      int page_request_id,
-      const StreamControls& controls,
-      const url::Origin& security_origin,
-      const MediaRequestResponseCallback& callback);
+  std::string MakeMediaAccessRequest(int render_process_id,
+                                     int render_frame_id,
+                                     int page_request_id,
+                                     const StreamControls& controls,
+                                     const url::Origin& security_origin,
+                                     MediaRequestResponseCallback callback);
 
   // GenerateStream opens new media devices according to |components|.  It
   // creates a new request which is identified by a unique string that's
