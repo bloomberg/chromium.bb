7811d5c523f762477ff2cfa951b7764978d4e110
Maajid <maajid@chromium.org>
2017-07-14 22:00:07 +0000

    Reland: Use the magnifying glass icon instead of the Google icon for the search box when the default search provider is not Google.
    Bug:736038
    TEST:unit tested, device tested with --enable-features=EnableFullcreenAppList, ASAN and LSAN local test.
    
    The fix for the ASAN failure was to use unique_ptr for the search_box_view pointer in the unit test.
    
    Change-Id: Id6026f6193a87f48d44064315ddde5e436d7d333
    Reviewed-on: https://chromium-review.googlesource.com/571310
    Commit-Queue: Maajid <maajid@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486901}
    Former-commit-id: 455a5fcddd64b29d81b626cafca1200a0c8e0473

--- ui/app_list/views/search_box_view_unittest.cc
+++ ui/app_list/views/search_box_view_unittest.cc
@@ -10,10 +10,16 @@
 #include "base/macros.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/scoped_feature_list.h"
+#include "ui/app_list/app_list_constants.h"
 #include "ui/app_list/app_list_features.h"
 #include "ui/app_list/test/app_list_test_view_delegate.h"
+#include "ui/app_list/vector_icons/vector_icons.h"
 #include "ui/app_list/views/app_list_view.h"
 #include "ui/app_list/views/search_box_view_delegate.h"
+#include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/image/image_unittest_util.h"
+#include "ui/gfx/paint_vector_icon.h"
+#include "ui/views/controls/image_view.h"
 #include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/test/widget_test.h"
 
@@ -45,6 +51,8 @@ class KeyPressCounterView : public views::View {
   DISALLOW_COPY_AND_ASSIGN(KeyPressCounterView);
 };
 
+// These tests run with both FullscreenAppList enabled and disabled.
+// TODO(crbug.com/743113) Unify the two test classes.
 class SearchBoxViewTest : public views::test::WidgetTest,
                           public SearchBoxViewDelegate,
                           public testing::WithParamInterface<bool> {
@@ -67,24 +75,26 @@ class SearchBoxViewTest : public views::test::WidgetTest,
 
     gfx::NativeView parent = GetContext();
     app_list_view_ = new AppListView(&view_delegate_);
-    app_list_view_->Initialize(parent, 0, false, false);
+    app_list_view()->Initialize(parent, 0, false, false);
 
     widget_ = CreateTopLevelPlatformWidget();
-    view_ = new SearchBoxView(this, &view_delegate_, app_list_view_);
+    view_.reset(new SearchBoxView(this, &view_delegate_, app_list_view()));
     counter_view_ = new KeyPressCounterView();
-    widget_->GetContentsView()->AddChildView(view_);
+    widget_->GetContentsView()->AddChildView(view());
     widget_->GetContentsView()->AddChildView(counter_view_);
-    view_->set_contents_view(counter_view_);
+    view()->set_contents_view(counter_view_);
   }
 
   void TearDown() override {
+    view_.reset();
     app_list_view_->GetWidget()->Close();
     widget_->CloseNow();
     views::test::WidgetTest::TearDown();
   }
 
  protected:
-  SearchBoxView* view() { return view_; }
+  SearchBoxView* view() { return view_.get(); }
+  AppListView* app_list_view() { return app_list_view_; }
 
   bool test_with_fullscreen() { return test_with_fullscreen_; }
 
@@ -107,11 +117,11 @@ class SearchBoxViewTest : public views::test::WidgetTest,
 
   void KeyPress(ui::KeyboardCode key_code) {
     ui::KeyEvent event(ui::ET_KEY_PRESSED, key_code, ui::EF_NONE);
-    view_->search_box()->OnKeyEvent(&event);
+    view()->search_box()->OnKeyEvent(&event);
     // Emulates the input method.
     if (::isalnum(static_cast<int>(key_code))) {
       base::char16 character = ::tolower(static_cast<int>(key_code));
-      view_->search_box()->InsertText(base::string16(1, character));
+      view()->search_box()->InsertText(base::string16(1, character));
     }
   }
 
@@ -140,8 +150,8 @@ class SearchBoxViewTest : public views::test::WidgetTest,
 
   AppListTestViewDelegate view_delegate_;
   views::Widget* widget_;
-  SearchBoxView* view_;
   AppListView* app_list_view_ = nullptr;
+  std::unique_ptr<SearchBoxView> view_;
   KeyPressCounterView* counter_view_;
   base::string16 last_query_;
   int query_changed_count_ = 0;
@@ -149,6 +159,71 @@ class SearchBoxViewTest : public views::test::WidgetTest,
   base::test::ScopedFeatureList scoped_feature_list_;
 
   DISALLOW_COPY_AND_ASSIGN(SearchBoxViewTest);
+};
+
+class SearchBoxViewFullscreenTest : public views::test::WidgetTest,
+                                    public SearchBoxViewDelegate {
+ public:
+  SearchBoxViewFullscreenTest() {}
+  ~SearchBoxViewFullscreenTest() override {}
+
+  // Overridden from testing::Test:
+  void SetUp() override {
+    views::test::WidgetTest::SetUp();
+    scoped_feature_list_.InitAndEnableFeature(
+        app_list::features::kEnableFullscreenAppList);
+
+    gfx::NativeView parent = GetContext();
+    app_list_view_ = new AppListView(&view_delegate_);
+    app_list_view_->Initialize(parent, 0, false, false);
+
+    widget_ = CreateTopLevelPlatformWidget();
+    view_.reset(new SearchBoxView(this, &view_delegate_, app_list_view()));
+    widget_->SetBounds(gfx::Rect(0, 0, 300, 200));
+    widget_->GetContentsView()->AddChildView(view());
+  }
+
+  void TearDown() override {
+    view_.reset();
+    app_list_view_->GetWidget()->Close();
+    widget_->CloseNow();
+    views::test::WidgetTest::TearDown();
+  }
+
+ protected:
+  SearchBoxView* view() { return view_.get(); }
+  AppListView* app_list_view() { return app_list_view_; }
+
+  void SetSearchEngineIsGoogle(bool is_google) {
+    view_delegate_.SetSearchEngineIsGoogle(is_google);
+  }
+
+  void KeyPress(ui::KeyboardCode key_code) {
+    ui::KeyEvent event(ui::ET_KEY_PRESSED, key_code, ui::EF_NONE);
+    view()->search_box()->OnKeyEvent(&event);
+    // Emulates the input method.
+    if (::isalnum(static_cast<int>(key_code))) {
+      base::char16 character = ::tolower(static_cast<int>(key_code));
+      view()->search_box()->InsertText(base::string16(1, character));
+    }
+  }
+
+ private:
+  // Overridden from SearchBoxViewDelegate:
+  void QueryChanged(SearchBoxView* sender) override {}
+
+  void BackButtonPressed() override {}
+
+  void SetSearchResultSelection(bool select) override {}
+
+  base::test::ScopedFeatureList scoped_feature_list_;
+
+  AppListTestViewDelegate view_delegate_;
+  views::Widget* widget_;
+  AppListView* app_list_view_ = nullptr;
+  std::unique_ptr<SearchBoxView> view_;
+
+  DISALLOW_COPY_AND_ASSIGN(SearchBoxViewFullscreenTest);
 };
 
 // Instantiate the Boolean which is used to toggle the Fullscreen app list in
@@ -192,15 +267,36 @@ TEST_P(SearchBoxViewTest, CancelAutoLaunch) {
   EXPECT_EQ(base::TimeDelta(), GetAutoLaunchTimeout());
 }
 
-TEST_P(SearchBoxViewTest, CloseButtonTest) {
-  if (!test_with_fullscreen())
-    return;
-
+TEST_F(SearchBoxViewFullscreenTest, CloseButtonTest) {
   KeyPress(ui::VKEY_A);
   EXPECT_TRUE(view()->IsCloseButtonVisible());
 
   view()->ClearSearch();
   EXPECT_FALSE(view()->IsCloseButtonVisible());
+}
+
+TEST_F(SearchBoxViewFullscreenTest, SearchEngineGoogle) {
+  SetSearchEngineIsGoogle(true);
+  gfx::ImageSkia expected_icon = gfx::CreateVectorIcon(
+      kIcGoogleBlackIcon, kSearchIconSize, kDefaultSearchboxColor);
+  view()->ModelChanged();
+
+  gfx::ImageSkia actual_icon = view()->get_search_icon_for_test()->GetImage();
+
+  EXPECT_TRUE(gfx::test::AreBitmapsEqual(*expected_icon.bitmap(),
+                                         *actual_icon.bitmap()));
+}
+
+TEST_F(SearchBoxViewFullscreenTest, SearchEngineNotGoogle) {
+  SetSearchEngineIsGoogle(false);
+  gfx::ImageSkia expected_icon = gfx::CreateVectorIcon(
+      kIcSearchEngineNotGoogleIcon, kSearchIconSize, kDefaultSearchboxColor);
+  view()->ModelChanged();
+
+  gfx::ImageSkia actual_icon = view()->get_search_icon_for_test()->GetImage();
+
+  EXPECT_TRUE(gfx::test::AreBitmapsEqual(*expected_icon.bitmap(),
+                                         *actual_icon.bitmap()));
 }
 
 }  // namespace test
