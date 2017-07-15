6cb56827c74cc3e1c9bf08d97ac10cdb37e132ff
Amirhossein Simjour <asimjour@chromium.org>
2017-07-15 13:05:51 +0000

    Fix Screen Share icon
    screen_share.icon is moved back to ash/resources/vector_icons.
    As a result VrShell is no longer using the 1x version of this icon.
    
    BUG=736350
    
    Change-Id: Iaa9934fa99345e58acd60d7e3250b58b80ba170f
    Reviewed-on: https://chromium-review.googlesource.com/565546
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Commit-Queue: Amirhossein Simjour <asimjour@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486991}
    Former-commit-id: dbf508f8180fc7dff9c5cf0aad349bc5be6d3f7e

--- ash/system/screen_security/screen_tray_item.cc
+++ ash/system/screen_security/screen_tray_item.cc
@@ -9,7 +9,6 @@
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_popup_item_style.h"
 #include "ash/system/tray/tray_popup_utils.h"
-#include "components/vector_icons/vector_icons.h"
 #include "ui/gfx/paint_vector_icon.h"
 #include "ui/message_center/message_center.h"
 #include "ui/views/controls/button/label_button.h"
@@ -25,7 +24,7 @@ ScreenTrayView::ScreenTrayView(ScreenTrayItem* screen_tray_item)
     : TrayItemView(screen_tray_item), screen_tray_item_(screen_tray_item) {
   CreateImageView();
   image_view()->SetImage(
-      gfx::CreateVectorIcon(vector_icons::kScreenShareIcon, kTrayIconColor));
+      gfx::CreateVectorIcon(kSystemTrayScreenShareIcon, kTrayIconColor));
   Update();
 }
 
