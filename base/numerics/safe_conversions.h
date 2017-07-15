5c962e886aef95154365c97d4f381ee0a41b922e
Justin Schuh <jschuh@chromium.org>
2017-07-15 21:54:53 +0000

    Fix template bug in base::saturated_cast<>()
    Perform template check with UnderlyingType and clamp shift
    value for SaturateFastAsmOp<>;
    
    TBR=servolk@chromium.org
    
    Change-Id: I8828b06a211515ee06d65c7dd1989b7cd84a841c
    Reviewed-on: https://chromium-review.googlesource.com/572870
    Commit-Queue: Justin Schuh <jschuh@chromium.org>
    Reviewed-by: Justin Schuh <jschuh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486998}
    Former-commit-id: c77d482ba4a8815117ccd6f7f9a5796d3bbe7908

--- base/numerics/safe_conversions.h
+++ base/numerics/safe_conversions.h
@@ -13,9 +13,7 @@
 
 #include "base/numerics/safe_conversions_impl.h"
 
-// TODO(jschuh): Investigate why these were failing to build for ios.
-#if !defined(__APPLE__) && !defined(__native_client__) && \
-    (defined(__ARMEL__) || defined(__arch64__))
+#if !defined(__native_client__) && (defined(__ARMEL__) || defined(__arch64__))
 #include "base/numerics/safe_conversions_arm_impl.h"
 #define BASE_HAS_OPTIMIZED_SAFE_CONVERSIONS (1)
 #else
@@ -117,7 +115,7 @@ template <typename Dst,
 constexpr Dst saturated_cast(Src value) {
   using SrcType = typename UnderlyingType<Src>::type;
   return !IsCompileTimeConstant(value) &&
-                 SaturateFastAsmOp<Dst, Src>::is_supported &&
+                 SaturateFastAsmOp<Dst, SrcType>::is_supported &&
                  std::is_same<SaturationHandler<Dst>,
                               SaturationDefaultLimits<Dst>>::value
              ? SaturateFastAsmOp<Dst, SrcType>::Do(value)
