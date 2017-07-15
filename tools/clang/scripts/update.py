2fc2d91dba91738733ce8b60dfa57fc847de57ae
Bruce Dawson <brucedawson@chromium.org>
2017-07-15 01:51:41 +0000

    Always build clang with symbols on Windows
    If we always build clang with symbols on Windows then it makes debugging
    and profiling easier. The build time costs shouldn't be too bad, and the
    generated code will be identical. This would have saved a bit of time in
    the investigation of crbug.com/727447.
    
    BUG=727447
    
    Change-Id: Ib065be626db5d6b0b9c27792a0141c5fb696e208
    Reviewed-on: https://chromium-review.googlesource.com/572660
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Commit-Queue: Bruce Dawson <brucedawson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486962}
    Former-commit-id: 4c3d6a648ac28bcb24ab3537cc393f5467679b98

--- tools/clang/scripts/update.py
+++ tools/clang/scripts/update.py
@@ -588,7 +588,7 @@ def UpdateClang(args):
 
   # Build PDBs for archival on Windows.  Don't use RelWithDebInfo since it
   # has different optimization defaults than Release.
-  if sys.platform == 'win32' and args.bootstrap:
+  if sys.platform == 'win32':
     cflags += ['/Zi']
     cxxflags += ['/Zi']
     ldflags += ['/DEBUG', '/OPT:REF', '/OPT:ICF']
