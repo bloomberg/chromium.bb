33f6408e2043752001fc0a336f3d3a989c7ef53b
Tom Anderson <thomasanderson@chromium.org>
2017-07-15 02:29:42 +0000

    Cleanup x11_atom_cache
    BUG=730889
    R=sadrul@chromium.org
    
    Change-Id: I4b28a1e0a70236e2410eed9d442481db5fb0c898
    Reviewed-on: https://chromium-review.googlesource.com/572054
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#486968}
    Former-commit-id: e43049df31f1516f0b2202e7f7684942dc991d99

--- ui/gfx/x/x11_atom_cache.cc
+++ ui/gfx/x/x11_atom_cache.cc
@@ -14,6 +14,80 @@
 
 namespace {
 
+struct {
+  const char* atom_name;
+  Atom atom_value;
+} const kPredefinedAtoms[] = {
+    // {"PRIMARY", XA_PRIMARY},
+    // {"SECONDARY", XA_SECONDARY},
+    // {"ARC", XA_ARC},
+    {"ATOM", XA_ATOM},
+    // {"BITMAP", XA_BITMAP},
+    {"CARDINAL", XA_CARDINAL},
+    // {"COLORMAP", XA_COLORMAP},
+    // {"CURSOR", XA_CURSOR},
+    // {"CUT_BUFFER0", XA_CUT_BUFFER0},
+    // {"CUT_BUFFER1", XA_CUT_BUFFER1},
+    // {"CUT_BUFFER2", XA_CUT_BUFFER2},
+    // {"CUT_BUFFER3", XA_CUT_BUFFER3},
+    // {"CUT_BUFFER4", XA_CUT_BUFFER4},
+    // {"CUT_BUFFER5", XA_CUT_BUFFER5},
+    // {"CUT_BUFFER6", XA_CUT_BUFFER6},
+    // {"CUT_BUFFER7", XA_CUT_BUFFER7},
+    // {"DRAWABLE", XA_DRAWABLE},
+    // {"FONT", XA_FONT},
+    // {"INTEGER", XA_INTEGER},
+    // {"PIXMAP", XA_PIXMAP},
+    // {"POINT", XA_POINT},
+    // {"RECTANGLE", XA_RECTANGLE},
+    // {"RESOURCE_MANAGER", XA_RESOURCE_MANAGER},
+    // {"RGB_COLOR_MAP", XA_RGB_COLOR_MAP},
+    // {"RGB_BEST_MAP", XA_RGB_BEST_MAP},
+    // {"RGB_BLUE_MAP", XA_RGB_BLUE_MAP},
+    // {"RGB_DEFAULT_MAP", XA_RGB_DEFAULT_MAP},
+    // {"RGB_GRAY_MAP", XA_RGB_GRAY_MAP},
+    // {"RGB_GREEN_MAP", XA_RGB_GREEN_MAP},
+    // {"RGB_RED_MAP", XA_RGB_RED_MAP},
+    {"STRING", XA_STRING},
+    // {"VISUALID", XA_VISUALID},
+    // {"WINDOW", XA_WINDOW},
+    // {"WM_COMMAND", XA_WM_COMMAND},
+    // {"WM_HINTS", XA_WM_HINTS},
+    // {"WM_CLIENT_MACHINE", XA_WM_CLIENT_MACHINE},
+    // {"WM_ICON_NAME", XA_WM_ICON_NAME},
+    // {"WM_ICON_SIZE", XA_WM_ICON_SIZE},
+    // {"WM_NAME", XA_WM_NAME},
+    // {"WM_NORMAL_HINTS", XA_WM_NORMAL_HINTS},
+    // {"WM_SIZE_HINTS", XA_WM_SIZE_HINTS},
+    // {"WM_ZOOM_HINTS", XA_WM_ZOOM_HINTS},
+    // {"MIN_SPACE", XA_MIN_SPACE},
+    // {"NORM_SPACE", XA_NORM_SPACE},
+    // {"MAX_SPACE", XA_MAX_SPACE},
+    // {"END_SPACE", XA_END_SPACE},
+    // {"SUPERSCRIPT_X", XA_SUPERSCRIPT_X},
+    // {"SUPERSCRIPT_Y", XA_SUPERSCRIPT_Y},
+    // {"SUBSCRIPT_X", XA_SUBSCRIPT_X},
+    // {"SUBSCRIPT_Y", XA_SUBSCRIPT_Y},
+    // {"UNDERLINE_POSITION", XA_UNDERLINE_POSITION},
+    // {"UNDERLINE_THICKNESS", XA_UNDERLINE_THICKNESS},
+    // {"STRIKEOUT_ASCENT", XA_STRIKEOUT_ASCENT},
+    // {"STRIKEOUT_DESCENT", XA_STRIKEOUT_DESCENT},
+    // {"ITALIC_ANGLE", XA_ITALIC_ANGLE},
+    // {"X_HEIGHT", XA_X_HEIGHT},
+    // {"QUAD_WIDTH", XA_QUAD_WIDTH},
+    // {"WEIGHT", XA_WEIGHT},
+    // {"POINT_SIZE", XA_POINT_SIZE},
+    // {"RESOLUTION", XA_RESOLUTION},
+    // {"COPYRIGHT", XA_COPYRIGHT},
+    // {"NOTICE", XA_NOTICE},
+    // {"FONT_NAME", XA_FONT_NAME},
+    // {"FAMILY_NAME", XA_FAMILY_NAME},
+    // {"FULL_NAME", XA_FULL_NAME},
+    // {"CAP_HEIGHT", XA_CAP_HEIGHT},
+    {"WM_CLASS", XA_WM_CLASS},
+    // {"WM_TRANSIENT_FOR", XA_WM_TRANSIENT_FOR},
+};
+
 const char* kAtomsToCache[] = {"Abs Dbl End Timestamp",
                                "Abs Dbl Fling X Velocity",
                                "Abs Dbl Fling Y Velocity",
@@ -34,9 +108,7 @@ const char* kAtomsToCache[] = {"Abs Dbl End Timestamp",
                                "Abs MT Tracking ID",
                                "application/octet-stream",
                                "application/vnd.chromium.test",
-                               "ATOM",
                                "ATOM_PAIR",
-                               "CARDINAL",
                                "CHECK",
                                "_CHROME_DISPLAY_INTERNAL",
                                "_CHROME_DISPLAY_ROTATION",
@@ -112,7 +184,6 @@ const char* kAtomsToCache[] = {"Abs Dbl End Timestamp",
                                "_SCREENSAVER_VERSION",
                                "scaling mode",
                                "SELECTION_STRING",
-                               "STRING",
                                "Tap Paused",
                                "TARGETS",
                                "TARGET1",
@@ -130,7 +201,6 @@ const char* kAtomsToCache[] = {"Abs Dbl End Timestamp",
                                "Touch Timestamp",
                                "Undesired",
                                "UTF8_STRING",
-                               "WM_CLASS",
                                "WM_DELETE_WINDOW",
                                "WM_PROTOCOLS",
                                "WM_WINDOW_ROLE",
@@ -167,18 +237,19 @@ X11AtomCache* X11AtomCache::GetInstance() {
 }
 
 X11AtomCache::X11AtomCache() : xdisplay_(gfx::GetXDisplay()) {
-  int cache_count = 0;
-  for (const char* const* i = kAtomsToCache; *i; ++i)
-    ++cache_count;
+  for (const auto& predefined_atom : kPredefinedAtoms)
+    cached_atoms_[predefined_atom.atom_name] = predefined_atom.atom_value;
 
-  std::unique_ptr<XAtom[]> cached_atoms(new XAtom[cache_count]);
+  const int kCacheCount = arraysize(kAtomsToCache) - 1;
+
+  std::unique_ptr<XAtom[]> cached_atoms(new XAtom[kCacheCount]);
 
   // Grab all the atoms we need now to minimize roundtrips to the X11 server.
-  XInternAtoms(xdisplay_, const_cast<char**>(kAtomsToCache), cache_count, False,
+  XInternAtoms(xdisplay_, const_cast<char**>(kAtomsToCache), kCacheCount, False,
                cached_atoms.get());
 
-  for (int i = 0; i < cache_count; ++i)
-    cached_atoms_.insert(std::make_pair(kAtomsToCache[i], cached_atoms[i]));
+  for (int i = 0; i < kCacheCount; ++i)
+    cached_atoms_[kAtomsToCache[i]] = cached_atoms[i];
 }
 
 X11AtomCache::~X11AtomCache() {}
