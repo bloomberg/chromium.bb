b4031d2b766596b7c006e376268c9de49af5d8a9
Hidehiko Abe <hidehiko@chromium.org>
2017-07-16 17:45:27 +0000

    Migrate ArcService to BrowserContextKeyedService part 15.
    This CL migrates ArcPowerBridge.
    
    BUG=672829
    TEST=Ran try.
    
    Change-Id: I02dac942cbd31635120c62734032ca7611edae4b
    Reviewed-on: https://chromium-review.googlesource.com/572881
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Reviewed-by: Yusuke Sato (in China Mon-Thurs, may be offline) <yusukes@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487013}
    Former-commit-id: 0a4decaa857c7d142bd94762d51963973b9ace61

--- components/arc/power/arc_power_bridge.h
+++ components/arc/power/arc_power_bridge.h
@@ -9,11 +9,15 @@
 
 #include "base/macros.h"
 #include "chromeos/dbus/power_manager_client.h"
-#include "components/arc/arc_service.h"
 #include "components/arc/common/power.mojom.h"
 #include "components/arc/instance_holder.h"
+#include "components/keyed_service/core/keyed_service.h"
 #include "mojo/public/cpp/bindings/binding.h"
 #include "ui/display/manager/chromeos/display_configurator.h"
+
+namespace content {
+class BrowserContext;
+}  // namespace content
 
 namespace arc {
 
@@ -21,13 +25,18 @@ class ArcBridgeService;
 
 // ARC Power Client sets power management policy based on requests from
 // ARC instances.
-class ArcPowerBridge : public ArcService,
+class ArcPowerBridge : public KeyedService,
                        public InstanceHolder<mojom::PowerInstance>::Observer,
                        public chromeos::PowerManagerClient::Observer,
                        public display::DisplayConfigurator::Observer,
                        public mojom::PowerHost {
  public:
-  explicit ArcPowerBridge(ArcBridgeService* bridge_service);
+  // Returns singleton instance for the given BrowserContext,
+  // or nullptr if the browser |context| is not allowed to use ARC.
+  static ArcPowerBridge* GetForBrowserContext(content::BrowserContext* context);
+
+  ArcPowerBridge(content::BrowserContext* context,
+                 ArcBridgeService* bridge_service);
   ~ArcPowerBridge() override;
 
   // InstanceHolder<mojom::PowerInstance>::Observer overrides.
@@ -52,6 +61,7 @@ class ArcPowerBridge : public ArcService,
   void ReleaseAllDisplayWakeLocks();
   void UpdateAndroidScreenBrightness(double percent);
 
+  ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
   mojo::Binding<mojom::PowerHost> binding_;
 
   // Stores a mapping of type -> wake lock ID for all wake locks
