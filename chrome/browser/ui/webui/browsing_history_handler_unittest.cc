2a116ac06a75b99a8645c8238d5a38560c6b5740
Sky Malice <skym@chromium.org>
2017-07-17 17:34:15 +0000

    Remove dead offset/range code from BrowsingHistoryHandler.
    Bug: 
    Change-Id: I2d45ac2eafaf6c838056e6070b4830f05a25f1cc
    Reviewed-on: https://chromium-review.googlesource.com/569846
    Reviewed-by: calamity <calamity@chromium.org>
    Commit-Queue: Sky Malice <skym@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487138}
    Former-commit-id: 883ea33ba3a92df4d43e2a643e28f6de65a876d3

--- chrome/browser/ui/webui/browsing_history_handler_unittest.cc
+++ chrome/browser/ui/webui/browsing_history_handler_unittest.cc
@@ -88,7 +88,6 @@ class BrowsingHistoryHandlerWithWebUIForTesting
     set_clock(base::WrapUnique(test_clock_));
     set_web_ui(web_ui);
     test_clock_->SetNow(PretendNow());
-
   }
 
   base::SimpleTestClock* test_clock() { return test_clock_; }
@@ -165,67 +164,6 @@ class BrowsingHistoryHandlerTest : public ::testing::Test {
   std::unique_ptr<content::TestWebUI> web_ui_;
   std::unique_ptr<content::WebContents> web_contents_;
 };
-
-TEST_F(BrowsingHistoryHandlerTest, SetQueryTimeInWeeks) {
-  BrowsingHistoryHandlerWithWebUIForTesting handler(web_ui());
-  history::QueryOptions options;
-
-  // Querying this week should result in end time being midnight tonight and
-  // begin time being midnight a week ago.
-  handler.SetQueryTimeInWeeks(0, &options);
-  base::Time midnight_tonight =
-      PretendNow().LocalMidnight() + base::TimeDelta::FromDays(1);
-  EXPECT_EQ(midnight_tonight, options.end_time);
-  base::Time midnight_a_week_ago =
-      midnight_tonight - base::TimeDelta::FromDays(7);
-  EXPECT_EQ(midnight_a_week_ago, options.begin_time);
-
-  // Querying 4 weeks ago should result in end time being midnight 4 weeks ago
-  // and begin time being midnight 5 weeks ago.
-  handler.SetQueryTimeInWeeks(4, &options);
-  base::Time midnight_4_weeks_ago =
-      PretendNow().LocalMidnight() - base::TimeDelta::FromDays(27);
-  EXPECT_EQ(midnight_4_weeks_ago, options.end_time);
-  base::Time midnight_5_weeks_ago =
-      midnight_4_weeks_ago - base::TimeDelta::FromDays(7);
-  EXPECT_EQ(midnight_5_weeks_ago, options.begin_time);
-}
-
-TEST_F(BrowsingHistoryHandlerTest, SetQueryTimeInMonths) {
-  BrowsingHistoryHandlerWithWebUIForTesting handler(web_ui());
-  history::QueryOptions options;
-
-  base::Time::Exploded exploded_expected_time;
-  PretendNow().LocalExplode(&exploded_expected_time);
-
-  // Querying this month should result in end time being unset and begin time
-  // being midnight of the first.
-  handler.SetQueryTimeInMonths(0, &options);
-  EXPECT_EQ(base::Time(), options.end_time);
-
-  exploded_expected_time.day_of_month = 1;
-  exploded_expected_time.hour = 0;
-  base::Time first_jan_2015_midnight;
-  EXPECT_TRUE(base::Time::FromLocalExploded(exploded_expected_time,
-                                            &first_jan_2015_midnight));
-  EXPECT_EQ(first_jan_2015_midnight, options.begin_time);
-
-  // Querying 6 months ago should result in end time being 2014-08-01 and begin
-  // time being 2014-07-01.
-  handler.SetQueryTimeInMonths(6, &options);
-
-  exploded_expected_time.year = 2014;
-  exploded_expected_time.month = 8;
-  base::Time first_aug_2014_midnight;
-  EXPECT_TRUE(base::Time::FromLocalExploded(exploded_expected_time,
-                                            &first_aug_2014_midnight));
-  EXPECT_EQ(first_aug_2014_midnight, options.end_time);
-  exploded_expected_time.month = 7;
-  base::Time first_jul_2014_midnight;
-  EXPECT_TRUE(base::Time::FromLocalExploded(exploded_expected_time,
-                                            &first_jul_2014_midnight));
-  EXPECT_EQ(first_jul_2014_midnight, options.begin_time);
-}
 
 // Tests that BrowsingHistoryHandler is informed about WebHistoryService
 // deletions.
