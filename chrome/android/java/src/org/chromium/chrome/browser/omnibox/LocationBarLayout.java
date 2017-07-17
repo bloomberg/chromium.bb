dde8686c59dc0dbaf9bd9901216d8545a66f3441
Matthew Jones <mdjones@chromium.org>
2017-07-17 16:52:50 +0000

    [Home] Allow omnibox suggestions to be scrolled up
    Previously, omnibox suggestions could be scrolled down, but when
    scrolling back up, the bottom sheet would intercept the scroll and
    defocus the omnibox. This change adds a special case to the bottom
    sheet event handler. If the suggestions are showing and scrolled
    any amount, the sheet will not take the scroll event (unless it is
    in the toolbar area). This allows the suggestions to be freely
    scrolled and only when they reach the top can the sheet be pulled
    down.
    
    BUG=735319
    
    Change-Id: Icaf373d573b8c2380452d2b5e272aa1ed578f4ca
    Reviewed-on: https://chromium-review.googlesource.com/562667
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487120}
    Former-commit-id: f2cfd5c921e1c558f9a1922f62592e5032252a49

--- chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
+++ chrome/android/java/src/org/chromium/chrome/browser/omnibox/LocationBarLayout.java
@@ -646,6 +646,13 @@ public class LocationBarLayout extends FrameLayout
                 ApiCompatibilityUtils.setLayoutDirection(childView, layoutDirection);
             }
         }
+
+        /**
+         * @return Whether or not the suggestions list is scrolled any amount.
+         */
+        private boolean isScrolled() {
+            return computeVerticalScrollOffset() > 0;
+        }
     }
 
     public LocationBarLayout(Context context, AttributeSet attrs) {
@@ -688,6 +695,17 @@ public class LocationBarLayout extends FrameLayout
         mSuggestionListAdapter = new OmniboxResultsAdapter(getContext(), this, mSuggestionItems);
 
         mMicButton = (TintedImageButton) findViewById(R.id.mic_button);
+    }
+
+    @Override
+    public boolean isSuggestionsListShown() {
+        return mSuggestionsShown;
+    }
+
+    @Override
+    public boolean isSuggestionsListScrolled() {
+        if (!mSuggestionsShown || mSuggestionList == null) return false;
+        return mSuggestionList.isScrolled();
     }
 
     @Override
