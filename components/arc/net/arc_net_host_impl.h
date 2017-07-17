0323f8cd83b6566ed4c1c3102144a1bdb45bd997
Hidehiko Abe <hidehiko@chromium.org>
2017-07-17 15:50:03 +0000

    Migrate ArcService to BrowserContextKeyedService part 12.
    This CL migrates ArcNetHostImpl.
    
    BUG=672829
    TEST=Ran try.
    
    Change-Id: I4236b93a52b45b4e0ef34fe0fc72ceb124aac063
    Reviewed-on: https://chromium-review.googlesource.com/572671
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Commit-Queue: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487093}
    Former-commit-id: 99937de5cdbc9e276572083c992ba8a832b0a44c

--- components/arc/net/arc_net_host_impl.h
+++ components/arc/net/arc_net_host_impl.h
@@ -15,29 +15,36 @@
 #include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
 #include "chromeos/network/network_state_handler_observer.h"
-#include "components/arc/arc_service.h"
 #include "components/arc/common/net.mojom.h"
 #include "components/arc/instance_holder.h"
+#include "components/keyed_service/core/keyed_service.h"
 #include "mojo/public/cpp/bindings/binding.h"
 
 namespace base {
-
 class DictionaryValue;
-
 }  // namespace base
+
+namespace content {
+class BrowserContext;
+}  // namespace content
 
 namespace arc {
 
 class ArcBridgeService;
 
 // Private implementation of ArcNetHost.
-class ArcNetHostImpl : public ArcService,
+class ArcNetHostImpl : public KeyedService,
                        public InstanceHolder<mojom::NetInstance>::Observer,
                        public chromeos::NetworkStateHandlerObserver,
                        public mojom::NetHost {
  public:
+  // Returns singleton instance for the given BrowserContext,
+  // or nullptr if the browser |context| is not allowed to use ARC.
+  static ArcNetHostImpl* GetForBrowserContext(content::BrowserContext* context);
+
   // The constructor will register an Observer with ArcBridgeService.
-  explicit ArcNetHostImpl(ArcBridgeService* arc_bridge_service);
+  ArcNetHostImpl(content::BrowserContext* context,
+                 ArcBridgeService* arc_bridge_service);
   ~ArcNetHostImpl() override;
 
   // ARC -> Chrome calls:
@@ -103,6 +110,8 @@ class ArcNetHostImpl : public ArcService,
       const mojom::NetHost::CreateNetworkCallback& mojo_callback,
       const std::string& error_name,
       std::unique_ptr<base::DictionaryValue> error_data);
+
+  ArcBridgeService* const arc_bridge_service_;  // Owned by ArcServiceManager.
 
   // True if the chrome::NetworkStateHandler is currently being observed for
   // state changes.
