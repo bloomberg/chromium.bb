0356fd0164efbe41c4d05880ae6eefae6a987d52
proberge <proberge@chromium.org>
2017-07-17 21:19:03 +0000

    Chrome Cleanup WebUI: add conditional "Powered by" element
    Chrome Cleanup's functionality can be powered by an external company.
    When this is the case, we want to show attribution in the WebUI. This is
    done through the text "Powered by" followed by the company's logo inlined
    in the text, then the registered trademark symbol.
    
    Also fixes a line-split issue for the file paths to be removed and makes
    some static chrome_cleaner_controller methods into class methods.
    
    Bug: 690020
    Change-Id: I87acfeb4a6ee95cbdd197318b3596f93f819c520
    Reviewed-on: https://chromium-review.googlesource.com/568399
    Commit-Queue: proberge <proberge@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Chris Sharp <csharp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487246}
    Former-commit-id: adfdef9802da20158d0235d909ad6adb65ab647f

--- chrome/browser/resources/settings/chrome_cleanup_page/chrome_cleanup_page.js
+++ chrome/browser/resources/settings/chrome_cleanup_page/chrome_cleanup_page.js
@@ -102,6 +102,15 @@ Polymer({
         return /** @type {chrome.settingsPrivate.PrefObject} */ ({});
       },
     },
+
+    /** @private */
+    isPartnerPowered_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.valueExists('cleanupPoweredByPartner') &&
+            loadTimeData.getBoolean('cleanupPoweredByPartner');
+      },
+    },
   },
 
   /** @private {?settings.ChromeCleanupProxy} */
@@ -153,6 +162,14 @@ Polymer({
    */
   showFiles_: function() {
     this.showFilesToRemove_ = true;
+  },
+
+  /**
+   * @return {boolean}
+   * @private
+   */
+  showPoweredBy_: function() {
+    return this.showFilesToRemove_ && this.isPartnerPowered_;
   },
 
   /**
