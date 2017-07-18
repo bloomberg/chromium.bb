82beecbb18400bab1a7fe4a0c0d1761e066c793f
Cooper Knaak <cooperknaak@google.com>
2017-07-18 17:15:03 +0000

    [iOS] Notify FeatureEngagementTracker of Cleared Browsing Data Event
    This CL sends a notification to FeatureEngagementTracker when the
    user clears their browsing data. The notification was added to
    -[ClearBrowsingDataCollectionViewController clearDataForDataTypes:]
    because both entry points (History and Settings page) use this code
    path. The notification was not added to
    -[MainController chromeExecuteCommand:], despite it ultimately being
    the place that executes the action, because chromeExecuteCommand also
    is used for non-user initiated actions (such as clearing the browser
    data on sign out). Non-user initiated actions should not be logged
    by the FeatureEngagementTracker because that would interfere with the
    ability to present in-product help promotions to users who haven't
    used a given feature.
    
    Bug: 738164
    Change-Id: I7f79b724757fc990f93cdaff95c5bfa44f10c36f
    Reviewed-on: https://chromium-review.googlesource.com/560721
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Ed Chin <edchin@chromium.org>
    Reviewed-by: Gregory Chatzinoff <gchatz@chromium.org>
    Commit-Queue: Cooper Knaak <cooperknaak@google.com>
    Cr-Commit-Position: refs/heads/master@{#487510}
    Former-commit-id: 4af07e745f40672da0cca6f665b7c89750208010

--- ios/chrome/browser/ui/settings/clear_browsing_data_collection_view_controller.mm
+++ ios/chrome/browser/ui/settings/clear_browsing_data_collection_view_controller.mm
@@ -20,6 +20,8 @@
 #include "components/browsing_data/core/counters/browsing_data_counter.h"
 #include "components/browsing_data/core/history_notice_utils.h"
 #include "components/browsing_data/core/pref_names.h"
+#include "components/feature_engagement_tracker/public/event_constants.h"
+#include "components/feature_engagement_tracker/public/feature_engagement_tracker.h"
 #include "components/google/core/browser/google_util.h"
 #include "components/history/core/browser/web_history_service.h"
 #include "components/prefs/pref_service.h"
@@ -32,6 +34,7 @@
 #include "ios/chrome/browser/browsing_data/ios_chrome_browsing_data_remover.h"
 #include "ios/chrome/browser/chrome_url_constants.h"
 #include "ios/chrome/browser/experimental_flags.h"
+#include "ios/chrome/browser/feature_engagement_tracker/feature_engagement_tracker_factory.h"
 #include "ios/chrome/browser/history/web_history_service_factory.h"
 #include "ios/chrome/browser/signin/signin_manager_factory.h"
 #include "ios/chrome/browser/sync/ios_chrome_profile_sync_service_factory.h"
@@ -635,6 +638,12 @@ const int kMaxTimesHistoryNoticeShown = 1;
                                                         mask:dataTypeMask
                                                   timePeriod:_timePeriod];
   [self chromeExecuteCommand:command];
+
+  // Send the "Cleared Browsing Data" event to the FeatureEngagementTracker
+  // when the user initiates a clear browsing data action. No event is sent if
+  // the browsing data is cleared without the user's input.
+  FeatureEngagementTrackerFactory::GetForBrowserState(_browserState)
+      ->NotifyEvent(feature_engagement_tracker::events::kClearedBrowsingData);
 
   if (!!(dataTypeMask && IOSChromeBrowsingDataRemover::REMOVE_HISTORY)) {
     [self showBrowsingHistoryRemovedDialog];
