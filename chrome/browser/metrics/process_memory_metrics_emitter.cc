fe3ab47273779aee39f3c16b68f6f1e7b506f94c
erikchen <erikchen@chromium.org>
2017-07-18 21:21:13 +0000

    Make duplicates of UMA memmory metrics without Experimental in the name.
    Requested by the speed releasing team.
    
    Bug: 
    Change-Id: I87228f82856a0fce7034023e90ca18d524af0db1
    Reviewed-on: https://chromium-review.googlesource.com/576237
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Alexei Svitkine (slow) <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487609}
    Former-commit-id: 3ccadc41ccf8d6ed6c035350894c096a0282b085

--- chrome/browser/metrics/process_memory_metrics_emitter.cc
+++ chrome/browser/metrics/process_memory_metrics_emitter.cc
@@ -43,6 +43,8 @@ void EmitBrowserMemoryMetrics(const ProcessMemoryDumpPtr& pmd,
   UMA_HISTOGRAM_MEMORY_LARGE_MB(
       "Memory.Experimental.Browser2.PrivateMemoryFootprint",
       pmd->os_dump->private_footprint_kb / 1024);
+  UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Browser.PrivateMemoryFootprint",
+                                pmd->os_dump->private_footprint_kb / 1024);
   TryAddMetric(builder, "PrivateMemoryFootprint",
                pmd->os_dump->private_footprint_kb / 1024);
 }
@@ -64,6 +66,8 @@ void EmitRendererMemoryMetrics(const ProcessMemoryDumpPtr& pmd,
   UMA_HISTOGRAM_MEMORY_LARGE_MB(
       "Memory.Experimental.Renderer2.PrivateMemoryFootprint",
       pmd->os_dump->private_footprint_kb / 1024);
+  UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Renderer.PrivateMemoryFootprint",
+                                pmd->os_dump->private_footprint_kb / 1024);
   TryAddMetric(builder, "PrivateMemoryFootprint",
                pmd->os_dump->private_footprint_kb / 1024);
 
@@ -105,6 +109,8 @@ void EmitGpuMemoryMetrics(const ProcessMemoryDumpPtr& pmd,
   UMA_HISTOGRAM_MEMORY_LARGE_MB(
       "Memory.Experimental.Gpu2.PrivateMemoryFootprint",
       pmd->os_dump->private_footprint_kb / 1024);
+  UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Gpu.PrivateMemoryFootprint",
+                                pmd->os_dump->private_footprint_kb / 1024);
   TryAddMetric(builder, "PrivateMemoryFootprint",
                pmd->os_dump->private_footprint_kb / 1024);
 }
@@ -180,6 +186,8 @@ void ProcessMemoryMetricsEmitter::ReceivedMemoryDump(
   UMA_HISTOGRAM_MEMORY_LARGE_MB(
       "Memory.Experimental.Total2.PrivateMemoryFootprint",
       private_footprint_total_kb / 1024);
+  UMA_HISTOGRAM_MEMORY_LARGE_MB("Memory.Total.PrivateMemoryFootprint",
+                                private_footprint_total_kb / 1024);
 
   std::unique_ptr<ukm::UkmEntryBuilder> builder =
       CreateUkmBuilder("Memory.Experimental");
