b24c3d6329a8ed1c8423e446621f52abaf2aeabc
Jinho Bang <jinho.bang@samsung.com>
2017-07-18 14:05:53 +0000

    PaymentHandler: Add Interfaces for AbortPaymentEvent.
    This is an initial implementation of AbortPaymentEvent in WebIDL level.
    
    Related spec change:
      https://github.com/w3c/payment-handler/pull/170
    
    Bug: 736745
    Change-Id: I5cdf6b8e6a8019121aed4e10d33e884076bb519b
    Reviewed-on: https://chromium-review.googlesource.com/571345
    Commit-Queue: Jinho Bang <jinho.bang@samsung.com>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487463}
    Former-commit-id: 2d1d3d0ab6b476eff0d2b77c11426211d1fad63e

---
+++ third_party/WebKit/Source/modules/payments/AbortPaymentEvent.cpp
@@ -0,0 +1,61 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "modules/payments/AbortPaymentEvent.h"
+
+#include "bindings/core/v8/ExceptionState.h"
+#include "bindings/core/v8/ScriptPromise.h"
+#include "bindings/core/v8/ScriptPromiseResolver.h"
+#include "modules/EventModules.h"
+#include "modules/serviceworkers/ExtendableEventInit.h"
+#include "modules/serviceworkers/RespondWithObserver.h"
+#include "modules/serviceworkers/WaitUntilObserver.h"
+#include "platform/bindings/ScriptState.h"
+#include "platform/wtf/text/AtomicString.h"
+
+namespace blink {
+
+AbortPaymentEvent* AbortPaymentEvent::Create(
+    const AtomicString& type,
+    const ExtendableEventInit& initializer) {
+  return new AbortPaymentEvent(type, initializer, nullptr, nullptr);
+}
+
+AbortPaymentEvent* AbortPaymentEvent::Create(
+    const AtomicString& type,
+    const ExtendableEventInit& initializer,
+    RespondWithObserver* respond_with_observer,
+    WaitUntilObserver* wait_until_observer) {
+  return new AbortPaymentEvent(type, initializer, respond_with_observer,
+                               wait_until_observer);
+}
+
+AbortPaymentEvent::~AbortPaymentEvent() {}
+
+const AtomicString& AbortPaymentEvent::InterfaceName() const {
+  return EventNames::AbortPaymentEvent;
+}
+
+void AbortPaymentEvent::respondWith(ScriptState* script_state,
+                                    ScriptPromise script_promise,
+                                    ExceptionState& exception_state) {
+  stopImmediatePropagation();
+  if (observer_) {
+    observer_->RespondWith(script_state, script_promise, exception_state);
+  }
+}
+
+DEFINE_TRACE(AbortPaymentEvent) {
+  visitor->Trace(observer_);
+  ExtendableEvent::Trace(visitor);
+}
+
+AbortPaymentEvent::AbortPaymentEvent(const AtomicString& type,
+                                     const ExtendableEventInit& initializer,
+                                     RespondWithObserver* respond_with_observer,
+                                     WaitUntilObserver* wait_until_observer)
+    : ExtendableEvent(type, initializer, wait_until_observer),
+      observer_(respond_with_observer) {}
+
+}  // namespace blink
