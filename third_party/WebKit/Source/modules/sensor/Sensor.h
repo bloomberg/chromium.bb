020e5a7cc707e8f5dd8a9b60c4306af0769b26fd
Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
2017-07-18 13:09:59 +0000

    [Sensor] Improvements in Sensor events notifications
    This patch brings following improvements:
    - Same call chain is used for actual Sensor state updating and for firing the events
      (i.e. no implicit Sensor state change on JS side)
    - All the pending notification is suppressed after Sensor has stopped (or an error has occurred)
    - Notification for initial reading
    
    Bug: 
    Change-Id: Ia0505a4f51e17fbfaa8faa07602fff5618b8dc3d
    Reviewed-on: https://chromium-review.googlesource.com/559344
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com>
    Cr-Commit-Position: refs/heads/master@{#487451}
    Former-commit-id: 8fdeaa3887eeb93be6094c8cf577737f6c444500

--- third_party/WebKit/Source/modules/sensor/Sensor.h
+++ third_party/WebKit/Source/modules/sensor/Sensor.h
@@ -78,6 +78,7 @@ class Sensor : public EventTargetWithInlineData,
   double ReadingValueUnchecked(int index) const;
   bool CanReturnReadings() const;
   bool IsActivated() const { return state_ == SensorState::kActivated; }
+  bool IsIdleOrErrored() const;
 
   // SensorProxy::Observer overrides.
   void OnSensorInitialized() override;
@@ -94,18 +95,17 @@ class Sensor : public EventTargetWithInlineData,
 
   void OnAddConfigurationRequestCompleted(bool);
 
-  void StartListening();
-  void StopListening();
+  void Activate();
+  void Deactivate();
 
   void RequestAddConfiguration();
 
-  void UpdateState(SensorState new_state);
   void HandleError(ExceptionCode = kUnknownError,
                    const String& sanitized_message = String(),
                    const String& unsanitized_message = String());
 
   void NotifyReading();
-  void NotifyActivate();
+  void NotifyActivated();
   void NotifyError(DOMException* error);
 
  private:
@@ -115,7 +115,9 @@ class Sensor : public EventTargetWithInlineData,
   Member<SensorProxy> sensor_proxy_;
   double last_reported_timestamp_;
   SensorConfigurationPtr configuration_;
-  TaskHandle pending_reading_update_;
+  TaskHandle pending_reading_notification_;
+  TaskHandle pending_activated_notification_;
+  TaskHandle pending_error_notification_;
 };
 
 }  // namespace blink
