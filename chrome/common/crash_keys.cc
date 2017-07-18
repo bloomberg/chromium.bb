4afc29e9eb5486660bf05c3cb0716cd4a7c1f10b
Alexei Svitkine <asvitkine@chromium.org>
2017-07-18 00:08:39 +0000

    Add crash key for crbug.com/736675.
    The previous CL https://chromium-review.googlesource.com/570458
    added a CHECK, but the messages are not preserved in production
    builds. This adds the same info to a crash key, so we can get
    info from the wild.
    
    BUG=736675,744734
    
    Change-Id: I97106eec9a6f4a4f2ab00d2a28e23f891ebe5e50
    Reviewed-on: https://chromium-review.googlesource.com/575188
    Commit-Queue: Alexei Svitkine (slow) <asvitkine@chromium.org>
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487302}
    Former-commit-id: e73f80a0567b8c52776b70882ed094abf4e881d6

--- chrome/common/crash_keys.cc
+++ chrome/common/crash_keys.cc
@@ -231,6 +231,9 @@ size_t RegisterChromeCrashKeys() {
     // TEMPORARY: Compositor state for debugging BeginMainFrame renderer hang.
     // TODO(sunnyps): Remove after fixing https://crbug.com/622080
     {kBeginMainFrameHangCompositorState, crash_keys::kSmallSize},
+
+    // TODO(asvitkine): Remove after fixing https://crbug.com/736675
+    {"bad_histogram", kMediumSize},
   };
 
   // This dynamic set of keys is used for sets of key value pairs when gathering
