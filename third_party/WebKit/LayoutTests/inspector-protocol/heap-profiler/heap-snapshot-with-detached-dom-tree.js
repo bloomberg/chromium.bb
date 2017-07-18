028f0e82becdaa9d88c23d2502b8e2a1cc9df87d
Dmitry Gozman <dgozman@chromium.org>
2017-07-18 00:46:03 +0000

    [DevTools] Migrate inspector-protocol/heap-profiler tests to new harness
    BUG=734762
    
    Change-Id: I679f42eec506a033b9ab53799db4043f57a52293
    Reviewed-on: https://chromium-review.googlesource.com/572328
    Commit-Queue: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487320}
    Former-commit-id: 9aec2b4072c1fbe035af372206ae125899497ff1

---
+++ third_party/WebKit/LayoutTests/inspector-protocol/heap-profiler/heap-snapshot-with-detached-dom-tree.js
@@ -0,0 +1,61 @@
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startBlank(
+      `Test that all nodes from the detached DOM tree will get into one group in the heap snapshot. Bug 107819.`);
+
+  await session.evaluate(`
+    if (window.gc)
+      window.gc();
+    window.retaining_wrapper = document.createElement('div');
+    var t = document.createElement('div');
+    retaining_wrapper.appendChild(t);
+    t.appendChild(document.createElement('div'));
+  `);
+
+  function checkDetachedDOMTreeNodes(treeNode) {
+    var divCount = 0;
+    for (var iter = treeNode.edges(); iter.hasNext(); iter.next()) {
+      var node = iter.edge.node();
+      if (node.name() === 'HTMLDivElement')
+        ++divCount;
+      else
+        return testRunner.fail('unexpected DOM wrapper: ' + node.name());
+    }
+    if (divCount === 3)
+      testRunner.log('SUCCESS: found ' + divCount + ' DIVs in ' + treeNode.name());
+    else
+      return testRunner.fail('unexpected DIV count: ' + divCount);
+  }
+
+  var Helper = await testRunner.loadScript('resources/heap-snapshot-common.js');
+  var helper = await Helper(testRunner, session);
+
+  var snapshot = await helper.takeHeapSnapshot();
+  var node;
+  for (var it = snapshot._allNodes(); it.hasNext(); it.next()) {
+    if (it.node.name() === '(Detached DOM trees)') {
+      node = it.node;
+      break;
+    }
+  }
+  if (node)
+    testRunner.log('SUCCESS: found ' + node.name());
+  else
+    return testRunner.fail('cannot find detached DOM trees root');
+
+  var detachedDOMTreeRE = /^Detached DOM tree/;
+  var detachedDomTreeFound = false;
+  for (var iter = node.edges(); iter.hasNext(); iter.next()) {
+    var node = iter.edge.node();
+    if (detachedDOMTreeRE.test(node.className())) {
+      if ('Detached DOM tree / 3 entries' === node.name()) {
+        if (detachedDomTreeFound)
+          return testRunner.fail('second ' + node.name());
+        detachedDomTreeFound = true;
+        testRunner.log('SUCCESS: found ' + node.name());
+        checkDetachedDOMTreeNodes(node);
+      } else
+        return testRunner.fail('unexpected detached DOM tree: ' + node.name());
+    }
+  }
+  testRunner.completeTest();
+})
---
+++ third_party/WebKit/LayoutTests/inspector-protocol/heap-profiler/heap-snapshot-with-event-listener.js
@@ -0,0 +1,49 @@
+(async function(testRunner) {
+  let {page, session, dp} = await testRunner.startBlank(
+      `Test that all nodes from the detached DOM tree will get into one group in the heap snapshot. Bug 107819.`);
+
+  await session.evaluate(`
+    function addEventListenerAndRunTest() {
+      function myEventListener(e) {
+        console.log('myEventListener');
+      }
+      document.body.addEventListener('click', myEventListener, true);
+    }
+    addEventListenerAndRunTest();
+  `);
+
+  var Helper = await testRunner.loadScript('resources/heap-snapshot-common.js');
+  var helper = await Helper(testRunner, session);
+
+  var snapshot = await helper.takeHeapSnapshot();
+  var node;
+  for (var it = snapshot._allNodes(); it.hasNext(); it.next()) {
+    if (it.node.type() === 'closure' && it.node.name() === 'myEventListener') {
+      node = it.node;
+      break;
+    }
+  }
+  if (node)
+    testRunner.log('SUCCESS: found ' + node.name());
+  else
+    return testRunner.fail('cannot find detached DOM trees root');
+
+  var nativeRetainers = 0;
+  for (var iter = node.retainers(); iter.hasNext(); iter.next()) {
+    var retainingEdge = iter.retainer;
+    if (retainingEdge.isInternal() && retainingEdge.name() === 'native') {
+      if (++nativeRetainers === 1) {
+        var retainerName = retainingEdge.node().name();
+        if (retainerName === 'HTMLBodyElement')
+          testRunner.log('SUCCESS: found link from HTMLBodyElement to ' + node.name());
+        else
+          return testRunner.fail('unexpected retainer of ' + node.name() + ': ' + retainerName);
+      } else
+        return testRunner.fail('too many retainers of ' + node.name());
+    } else if (!retainingEdge.isWeak())
+      return testRunner.fail('unexpected retaining edge of ' + node.name() + ' type: ' + retainingEdge.type() + ', name: ' + retainingEdge.name());
+  }
+  if (!nativeRetainers)
+    return testRunner.fail('cannot find HTMLBodyElement among retainers of ' + node.name());
+  testRunner.completeTest();
+})
