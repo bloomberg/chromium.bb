cef60602043da7761c6269d850a4cbff7a8ff8e9
Hector Dearman <hjd@google.com>
2017-07-18 11:15:09 +0000

    memory-infra: Use new OnServicePIDReceived API
    Bug: 739710
    Change-Id: Ib926ae1ec013b33b7a90dcedb78960713cfa97e0
    Reviewed-on: https://chromium-review.googlesource.com/565510
    Reviewed-by: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Hector Dearman <hjd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487442}
    Former-commit-id: 6a576a58c36bcaf95478b30392ae6ecf2cfc5af6

--- services/resource_coordinator/memory_instrumentation/process_map.h
+++ services/resource_coordinator/memory_instrumentation/process_map.h
@@ -37,9 +37,7 @@ class ProcessMap : public service_manager::mojom::ServiceManagerListener {
  private:
   FRIEND_TEST_ALL_PREFIXES(ProcessMapTest, TypicalCase);
   FRIEND_TEST_ALL_PREFIXES(ProcessMapTest, PresentInInit);
-  FRIEND_TEST_ALL_PREFIXES(ProcessMapTest, MissedOnServiceCreated);
-  FRIEND_TEST_ALL_PREFIXES(ProcessMapTest, ZeroPidOnCreate_NonZeroOnStart);
-  FRIEND_TEST_ALL_PREFIXES(ProcessMapTest, NonZeroPidOnCreate_ZeroOnStart);
+  FRIEND_TEST_ALL_PREFIXES(ProcessMapTest, NullsInInitIgnored);
 
   using Identity = service_manager::Identity;
   using RunningServiceInfoPtr = service_manager::mojom::RunningServiceInfoPtr;
