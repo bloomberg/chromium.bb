cf43dd2adda54b4b29251fe06f0c982dc9de941d
Pete Williamson <petewil@chromium.org>
2017-07-19 00:42:36 +0000

    Fix for crashing bug in NotifyUIThreadOfRequestStarted
    It appears that sometimes the callback object passed into
    NotifyUIThreadOfRequestStarted doesn't always get initialized.
    If it has not been initialized, we return immediately instead of
    dereferencing the callback.
    
    Additionally, the async callsite uses base::Bind instead of 
    base::BindOnce, which we've changed since the callback only needs to be
    run a single time.
    
    Bug: 742370
    Change-Id: I73b8a49e9293408b8ab1f728358d0063ef82a4ab
    Reviewed-on: https://chromium-review.googlesource.com/574892
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Commit-Queue: Peter Williamson <petewil@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487693}
    Former-commit-id: 25b75fea13d119002a1e4551fc7aee197b25973f

--- chrome/browser/loader/chrome_resource_dispatcher_host_delegate.cc
+++ chrome/browser/loader/chrome_resource_dispatcher_host_delegate.cc
@@ -374,6 +374,7 @@ void NotifyUIThreadOfRequestStarted(
     const content::ResourceRequestInfo::WebContentsGetter& web_contents_getter,
     ResourceType resource_type) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  CHECK(!web_contents_getter.is_null());
   content::WebContents* web_contents = web_contents_getter.Run();
   if (!web_contents)
     return;
@@ -514,15 +515,18 @@ void ChromeResourceDispatcherHostDelegate::RequestBeginning(
     safe_browsing_->OnResourceRequest(request);
 
   const ResourceRequestInfo* info = ResourceRequestInfo::ForRequest(request);
+  // TODO(742370): remove when bug is resolved.
+  CHECK(info);
+  CHECK(!info->GetWebContentsGetterForRequest().is_null());
 
 #if BUILDFLAG(ENABLE_OFFLINE_PAGES)
   // TODO(petewil): Unify the safe browsing request and the metrics observer
   // request if possible so we only have to cross to the main thread once.
   // http://crbug.com/712312.
   BrowserThread::PostTask(BrowserThread::UI, FROM_HERE,
-                          base::Bind(&NotifyUIThreadOfRequestStarted,
-                                     info->GetWebContentsGetterForRequest(),
-                                     info->GetResourceType()));
+                          base::BindOnce(&NotifyUIThreadOfRequestStarted,
+                                         info->GetWebContentsGetterForRequest(),
+                                         info->GetResourceType()));
 #endif  // BUILDFLAG(ENABLE_OFFLINE_PAGES)
 
   ProfileIOData* io_data = ProfileIOData::FromResourceContext(
