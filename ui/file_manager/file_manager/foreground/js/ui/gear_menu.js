357186c0ebe0c9bbf269c4f77d2172130a883b24
Klemen Kozjek <klemenko@google.com>
2017-07-19 09:27:27 +0000

    Files app: Prevent to 'available Drive space' triggers the onMouseOver event for MenuItem
    This CL stops propagation of events on 'available Drive space' on mouse over event.
    As a result, when mouse goes over the 'available Drive space' it does not trigger onMouseOver for MenuItem and therefore error does not appear.
    
    Bug: 740890
    Change-Id: I4bec80aad0c4c9155fec5668d5bd8cef8e8a0e7b
    Reviewed-on: https://chromium-review.googlesource.com/577490
    Commit-Queue: Klemen Kozjek <klemenko@google.com>
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487801}
    Former-commit-id: 5c9dc07aa48293f68e8ab93a735efcd132b6b4d4

--- ui/file_manager/file_manager/foreground/js/ui/gear_menu.js
+++ ui/file_manager/file_manager/foreground/js/ui/gear_menu.js
@@ -25,9 +25,8 @@ function GearMenu(element) {
   /**
    * @type {!HTMLElement}
    * @const
-   * @private
    */
-  this.volumeSpaceInfo_ = queryRequiredElement('#volume-space-info', element);
+  this.volumeSpaceInfo = queryRequiredElement('#volume-space-info', element);
 
   /**
    * @type {!HTMLElement}
@@ -84,12 +83,12 @@ GearMenu.prototype.setSpaceInfo = function(
   this.spaceInfoPromise_ = spaceInfoPromise;
 
   if (!spaceInfoPromise) {
-    this.volumeSpaceInfo_.hidden = true;
+    this.volumeSpaceInfo.hidden = true;
     this.volumeSpaceInfoSeparator_.hidden = true;
     return;
   }
 
-  this.volumeSpaceInfo_.hidden = false;
+  this.volumeSpaceInfo.hidden = false;
   this.volumeSpaceInfoSeparator_.hidden = false;
   this.volumeSpaceInnerBar_.setAttribute('pending', '');
   if (showLoadingCaption) {
