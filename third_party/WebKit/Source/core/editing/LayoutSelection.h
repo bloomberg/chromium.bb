1fd1af7477aa1984702e02093515ae6fcd374669
Yoichi Osato <yoichio@chromium.org>
2017-07-19 08:11:06 +0000

    Move the end of traversing LayoutObject to SelectionPaintRange member
    Instead of calculating the stop LayoutObject, compute it in 
    SelectionPaintRange constructor.
    
    This is preparation for splitting SelectionState marking and
     invalidation.
    
    Bug: 739062
    Change-Id: I97083643ca9beb010f879b5c5048d71ce1783ed8
    Reviewed-on: https://chromium-review.googlesource.com/577327
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Commit-Queue: Yoichi Osato <yoichio@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487789}
    Former-commit-id: cb183afb3c6af0261c3aaa27f2f85d9a31168ec4

--- third_party/WebKit/Source/core/editing/LayoutSelection.h
+++ third_party/WebKit/Source/core/editing/LayoutSelection.h
@@ -57,9 +57,8 @@ class SelectionPaintRange {
     LayoutObject* operator*() const;
 
    private:
+    const SelectionPaintRange* range_;
     LayoutObject* current_;
-    const LayoutObject* included_end_;
-    const LayoutObject* stop_;
   };
   Iterator begin() const { return Iterator(this); };
   Iterator end() const { return Iterator(nullptr); };
@@ -84,6 +83,7 @@ class SelectionPaintRange {
   int start_offset_ = -1;
   LayoutObject* end_layout_object_ = nullptr;
   int end_offset_ = -1;
+  LayoutObject* traverse_stop_ = nullptr;
 };
 
 class LayoutSelection final : public GarbageCollected<LayoutSelection> {
