2facc33325b1440e8081b3625c4c9f182bfe09f9
Naina Raisinghani <nainar@chromium.org>
2017-07-19 07:02:54 +0000

    Add testcases to PerformanceTests/CSS to test changing style using selectors
    This CL amends the utils file to add a DOM tree generator function.
    
    We then change the style of all elements in the tree and benchmark the
    time spent. This allows us to set up a benchmark for style manipulation
    done using selectors.
    
    Bug: 741970
    Change-Id: Ic95054f5a849519713c60192c245e75dac73d341
    Reviewed-on: https://chromium-review.googlesource.com/572801
    Commit-Queue: nainar <nainar@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487774}
    Former-commit-id: ad0ccd37620cfc7e6cd1030ae323a6df051c1e7c

--- third_party/WebKit/PerformanceTests/CSS/resources/utils.js
+++ third_party/WebKit/PerformanceTests/CSS/resources/utils.js
@@ -9,9 +9,25 @@ function createDOMTree(node, siblings, depth) {
 }
 
 function createDeepDOMTree() {
-        createDOMTree(document.body, 2, 10);
+    createDOMTree(document.body, 2, 10);
+}
+
+function forceStyleRecalc(node) {
+    node.offsetTop; // forces style recalc
 }
 
 function createShallowDOMTree() {
-        createDOMTree(document.body, 10, 2);
+    createDOMTree(document.body, 10, 2);
+}
+
+function createRegularDOMTree() {
+    createDOMTree(document.body, 6, 6);
+}
+
+function applyCSSRule(rule) {
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = rule;
+    document.body.appendChild(css);
+    return css;
 }
