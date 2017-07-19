6f498ed797adca08f8a572a06a9609b367c98221
Primiano Tucci <primiano@chromium.org>
2017-07-19 11:58:49 +0000

    tracing: string.reserve() before serializing to JSON
    After the switch to libc++, flush time of tracing regressed.
    A cachegrind analysis suggests most time is spent in
    std::string::push_back().
    Pre-reserving the string capacity seems to alleviate the
    regression and bring us back to the old values (although
    libstdc++ would be still faster after this CL.
    
    Running on a Z620
    TraceEventPerfTest.Submit_10000_TRACE_EVENT0_in_traceable_tasks
    
    libstdc++ before this CL: 129ms
    libcxx    before this CL: 135ms
    
    libstdc++ after this CL: 123ms
    libcxx    after this CL: 129ms
    
    
    BUG=744555
    
    Change-Id: I9ebcbf432a983307e508327da0de8c4b82c41208
    Reviewed-on: https://chromium-review.googlesource.com/576176
    Reviewed-by: Hector Dearman <hjd@chromium.org>
    Commit-Queue: Primiano Tucci <primiano@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487827}
    Former-commit-id: f0d3f8eb3974e36ebb8116c15df45fccacb580ff

--- base/trace_event/trace_log.cc
+++ base/trace_event/trace_log.cc
@@ -888,12 +888,15 @@ void TraceLog::ConvertTraceEventsToTraceFormat(
   // The callback need to be called at least once even if there is no events
   // to let the caller know the completion of flush.
   scoped_refptr<RefCountedString> json_events_str_ptr = new RefCountedString();
+  const size_t kReserveCapacity = kTraceEventBufferSizeInBytes * 5 / 4;
+  json_events_str_ptr->data().reserve(kReserveCapacity);
   while (const TraceBufferChunk* chunk = logged_events->NextChunk()) {
     for (size_t j = 0; j < chunk->size(); ++j) {
       size_t size = json_events_str_ptr->size();
       if (size > kTraceEventBufferSizeInBytes) {
         flush_output_callback.Run(json_events_str_ptr, true);
         json_events_str_ptr = new RefCountedString();
+        json_events_str_ptr->data().reserve(kReserveCapacity);
       } else if (size) {
         json_events_str_ptr->data().append(",\n");
       }
