7a283fe89d63144f05d9732f104d940791b7881e
Eddy Mead <meade@chromium.org>
2017-07-19 04:34:57 +0000

    [CSS Typed OM] Update CSSPositionValue to match changed spec
    - x and y are now settable
    - Add missing exception handling in the constructor
    
    Spec: https://drafts.css-houdini.org/css-typed-om/#positionvalue-objects
    
    Bug: 545318
    Change-Id: I73fef9ed11eccb5ed156d6279f69c373fe1ad7a3
    Reviewed-on: https://chromium-review.googlesource.com/554654
    Commit-Queue: meade_UTC10 <meade@chromium.org>
    Reviewed-by: Eric Willigers <ericwilligers@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487747}
    Former-commit-id: 03e228d75abf51d33a1d2eed5a6e4caee4141e44

--- third_party/WebKit/Source/core/css/cssom/CSSPositionValue.cpp
+++ third_party/WebKit/Source/core/css/cssom/CSSPositionValue.cpp
@@ -4,10 +4,51 @@
 
 #include "core/css/cssom/CSSPositionValue.h"
 
+#include "bindings/core/v8/ExceptionState.h"
 #include "core/css/CSSValuePair.h"
 #include "core/css/cssom/CSSNumericValue.h"
 
 namespace blink {
+
+CSSPositionValue* CSSPositionValue::Create(CSSNumericValue* x,
+                                           CSSNumericValue* y,
+                                           ExceptionState& exception_state) {
+  if (x->GetType() != CSSStyleValue::StyleValueType::kLengthType &&
+      x->GetType() != CSSStyleValue::StyleValueType::kPercentType) {
+    exception_state.ThrowTypeError(
+        "Must pass length or percentage to x in CSSPositionValue");
+    return nullptr;
+  }
+  if (y->GetType() != CSSStyleValue::StyleValueType::kLengthType &&
+      y->GetType() != CSSStyleValue::StyleValueType::kPercentType) {
+    exception_state.ThrowTypeError(
+        "Must pass length or percentage to y in CSSPositionValue");
+    return nullptr;
+  }
+  return new CSSPositionValue(x, y);
+}
+
+void CSSPositionValue::setX(CSSNumericValue* x,
+                            ExceptionState& exception_state) {
+  if (x->GetType() != CSSStyleValue::StyleValueType::kLengthType &&
+      x->GetType() != CSSStyleValue::StyleValueType::kPercentType) {
+    exception_state.ThrowTypeError(
+        "Must pass length or percentage to x in CSSPositionValue");
+    return;
+  }
+  x_ = x;
+}
+
+void CSSPositionValue::setY(CSSNumericValue* y,
+                            ExceptionState& exception_state) {
+  if (y->GetType() != CSSStyleValue::StyleValueType::kLengthType &&
+      y->GetType() != CSSStyleValue::StyleValueType::kPercentType) {
+    exception_state.ThrowTypeError(
+        "Must pass length or percentage to y in CSSPositionValue");
+    return;
+  }
+  y_ = y;
+}
 
 CSSValue* CSSPositionValue::ToCSSValue() const {
   return CSSValuePair::Create(x_->ToCSSValue(), y_->ToCSSValue(),
