8346fc13c6f76a55d67c1948dc051fd418751fe9
Vladislav Kaznacheev <kaznacheev@google.com>
2017-07-19 22:54:50 +0000

    Extract reusable base from LaserPointerView
    The new FastInkView class contains everything
    required for low-latency rendering of pointer-like
    objects.
    
    Bug: 743083
    Change-Id: I0d31451899078875acd906396dda945bb45f2189
    Reviewed-on: https://chromium-review.googlesource.com/576389
    Commit-Queue: Vladislav Kaznacheev <kaznacheev@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488020}
    Former-commit-id: 04424852895156ea1aff5a02e72dc2fcb5f569ff

---
+++ ash/fast_ink/fast_ink_view.h
@@ -0,0 +1,86 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_FAST_INK_FAST_INK_VIEW_H_
+#define ASH_FAST_INK_FAST_INK_VIEW_H_
+
+#include <memory>
+#include <vector>
+
+#include "base/containers/flat_map.h"
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "ui/views/view.h"
+
+namespace aura {
+class Window;
+}
+
+namespace cc {
+struct ReturnedResource;
+}
+
+namespace gfx {
+class GpuMemoryBuffer;
+}
+
+namespace views {
+class Widget;
+}
+
+namespace ash {
+class FastInkLayerTreeFrameSinkHolder;
+struct FastInkResource;
+
+// FastInkView is a view supporting low-latency rendering.
+class FastInkView : public views::View {
+ public:
+  // Creates a FastInkView filling the bounds of |root_window|.
+  // If |root_window| is resized (e.g. due to a screen size change),
+  // a new instance of FastInkView should be created.
+  explicit FastInkView(aura::Window* root_window);
+  ~FastInkView() override;
+
+ protected:
+  // Unions |rect| with the current damage rect.
+  void UpdateDamageRect(const gfx::Rect& rect);
+
+  void RequestRedraw();
+
+  // Draw the contents of the view in the provided canvas.
+  virtual void OnRedraw(gfx::Canvas& canvas, const gfx::Vector2d& offset) = 0;
+
+ private:
+  friend class FastInkLayerTreeFrameSinkHolder;
+
+  // Call this to indicate that the previous frame has been processed.
+  void DidReceiveCompositorFrameAck();
+
+  // Call this to return resources so they can be reused or freed.
+  void ReclaimResources(const std::vector<cc::ReturnedResource>& resources);
+
+  void UpdateBuffer();
+  void UpdateSurface();
+  void OnDidDrawSurface();
+
+  std::unique_ptr<views::Widget> widget_;
+  float scale_factor_ = 1.0f;
+  std::unique_ptr<gfx::GpuMemoryBuffer> gpu_memory_buffer_;
+  gfx::Rect buffer_damage_rect_;
+  bool pending_update_buffer_ = false;
+  gfx::Rect surface_damage_rect_;
+  bool needs_update_surface_ = false;
+  bool pending_draw_surface_ = false;
+  std::unique_ptr<FastInkLayerTreeFrameSinkHolder> frame_sink_holder_;
+  int next_resource_id_ = 1;
+  base::flat_map<int, std::unique_ptr<FastInkResource>> resources_;
+  std::vector<std::unique_ptr<FastInkResource>> returned_resources_;
+  base::WeakPtrFactory<FastInkView> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(FastInkView);
+};
+
+}  // namespace ash
+
+#endif  // ASH_FAST_INK_FAST_INK_VIEW_H_
--- ash/laser/laser_pointer_view.h
+++ ash/laser/laser_pointer_view.h
@@ -5,41 +5,20 @@
 #ifndef ASH_LASER_LASER_POINTER_VIEW_H_
 #define ASH_LASER_LASER_POINTER_VIEW_H_
 
-#include <memory>
-#include <vector>
-
+#include "ash/fast_ink/fast_ink_view.h"
 #include "ash/laser/laser_pointer_points.h"
-#include "base/containers/flat_map.h"
-#include "base/macros.h"
-#include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
-#include "ui/views/view.h"
-
-namespace aura {
-class Window;
-}
-
-namespace cc {
-struct ReturnedResource;
-}
 
 namespace gfx {
-class GpuMemoryBuffer;
 class PointF;
 }
 
-namespace views {
-class Widget;
-}
-
 namespace ash {
-class LaserLayerTreeFrameSinkHolder;
-struct LaserResource;
 
 // LaserPointerView displays the palette tool laser pointer. It draws the laser,
 // which consists of a point where the mouse cursor should be, as well as a
 // trail of lines to help users track.
-class LaserPointerView : public views::View {
+class LaserPointerView : public FastInkView {
  public:
   LaserPointerView(base::TimeDelta life_duration,
                    base::TimeDelta presentation_delay,
@@ -51,38 +30,16 @@ class LaserPointerView : public views::View {
   void UpdateTime();
   void Stop();
 
-  // Call this to indicate that the previous frame has been processed.
-  void DidReceiveCompositorFrameAck();
-
-  // Call this to return resources so they can be reused or freed.
-  void ReclaimResources(const std::vector<cc::ReturnedResource>& resources);
-
  private:
   friend class LaserPointerControllerTestApi;
 
   gfx::Rect GetBoundingBox();
-  void OnPointsUpdated();
-  void UpdateBuffer();
-  void OnBufferUpdated();
-  void UpdateSurface();
-  void OnDidDrawSurface();
+
+  void OnRedraw(gfx::Canvas& canvas, const gfx::Vector2d& offset) override;
 
   LaserPointerPoints laser_points_;
   LaserPointerPoints predicted_laser_points_;
-  std::unique_ptr<views::Widget> widget_;
-  float scale_factor_ = 1.0f;
   const base::TimeDelta presentation_delay_;
-  std::unique_ptr<gfx::GpuMemoryBuffer> gpu_memory_buffer_;
-  gfx::Rect buffer_damage_rect_;
-  bool pending_update_buffer_ = false;
-  gfx::Rect surface_damage_rect_;
-  bool needs_update_surface_ = false;
-  bool pending_draw_surface_ = false;
-  std::unique_ptr<LaserLayerTreeFrameSinkHolder> frame_sink_holder_;
-  int next_resource_id_ = 1;
-  base::flat_map<int, std::unique_ptr<LaserResource>> resources_;
-  std::vector<std::unique_ptr<LaserResource>> returned_resources_;
-  base::WeakPtrFactory<LaserPointerView> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(LaserPointerView);
 };
