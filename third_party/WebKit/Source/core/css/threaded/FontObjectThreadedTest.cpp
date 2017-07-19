ea9754f90d168926e9e25fca47821c54a4952b36
Fernando Serboncini <fserb@chromium.org>
2017-07-19 20:24:57 +0000

    Adds TextIntercepts multi threaded tests
    Also, disabes HarfBuzz's atomic refptr TSan warning
    
    Bug: 730692
    Change-Id: I69c7fd5ab20896b6abd864506ae394732f8aa6e1
    Reviewed-on: https://chromium-review.googlesource.com/544625
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487953}
    Former-commit-id: de5a958df5a58664d436ad931d23e6cd25c71f9e

--- third_party/WebKit/Source/core/css/threaded/FontObjectThreadedTest.cpp
+++ third_party/WebKit/Source/core/css/threaded/FontObjectThreadedTest.cpp
@@ -13,6 +13,8 @@
 #include "platform/fonts/FontCustomPlatformData.h"
 #include "platform/fonts/FontDescription.h"
 #include "platform/fonts/FontSelector.h"
+#include "platform/fonts/shaping/CachingWordShapeIterator.h"
+#include "platform/fonts/shaping/HarfBuzzShaper.h"
 #include "platform/testing/FontTestHelpers.h"
 #include "platform/testing/UnitTestHelpers.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -39,11 +41,106 @@ TSAN_TEST(FontObjectThreadedTest, GetFontDefinition) {
   });
 }
 
+TSAN_TEST(FontObjectThreadedTest, GetDefaultFontData) {
+  callbacks_per_thread_ = 30;
+  num_threads_ = 5;
+  RunOnThreads([]() {
+    for (FontDescription::GenericFamilyType family_type :
+         {FontDescription::kStandardFamily, FontDescription::kSerifFamily,
+          FontDescription::kSansSerifFamily, FontDescription::kMonospaceFamily,
+          FontDescription::kCursiveFamily, FontDescription::kFantasyFamily,
+          FontDescription::kPictographFamily}) {
+      FontDescription font_description;
+      font_description.SetComputedSize(12.0);
+      font_description.SetLocale(LayoutLocale::Get("en"));
+      ASSERT_EQ(USCRIPT_LATIN, font_description.GetScript());
+      font_description.SetGenericFamily(family_type);
+
+      Font font = Font(font_description);
+      font.Update(nullptr);
+
+      ASSERT_TRUE(font.PrimaryFont());
+    }
+  });
+}
+
 // This test passes by not crashing TSAN.
 TSAN_TEST(FontObjectThreadedTest, FontSelector) {
   RunOnThreads([]() {
     Font font =
-        CreateTestFont("Ahem", testing::PlatformTestDataPath("Ahem.woff"), 16);
+        CreateTestFont("Ahem", testing::CoreTestDataPath("Ahem.ttf"), 16);
+  });
+}
+
+TSAN_TEST(FontObjectThreadedTest, TextIntercepts) {
+  callbacks_per_thread_ = 10;
+  RunOnThreads([]() {
+    Font font =
+        CreateTestFont("Ahem", testing::CoreTestDataPath("Ahem.ttf"), 16);
+    // A sequence of LATIN CAPITAL LETTER E WITH ACUTE and LATIN SMALL LETTER P
+    // characters. E ACUTES are squares above the baseline in Ahem, while p's
+    // are rectangles below the baseline.
+    UChar ahem_above_below_baseline_string[] = {0xc9, 0x70, 0xc9, 0x70, 0xc9,
+                                                0x70, 0xc9, 0x70, 0xc9};
+    TextRun ahem_above_below_baseline(ahem_above_below_baseline_string, 9);
+    TextRunPaintInfo text_run_paint_info(ahem_above_below_baseline);
+    PaintFlags default_paint;
+    float device_scale_factor = 1;
+    std::tuple<float, float> below_baseline_bounds = std::make_tuple(2, 4);
+    Vector<Font::TextIntercept> text_intercepts;
+
+    // 4 intercept ranges for below baseline p glyphs in the test string
+    font.GetTextIntercepts(text_run_paint_info, device_scale_factor,
+                           default_paint, below_baseline_bounds,
+                           text_intercepts);
+    EXPECT_EQ(text_intercepts.size(), 4u);
+    for (auto text_intercept : text_intercepts) {
+      EXPECT_GT(text_intercept.end_, text_intercept.begin_);
+    }
+
+    std::tuple<float, float> above_baseline_bounds = std::make_tuple(-4, -2);
+    // 5 intercept ranges for the above baseline E ACUTE glyphs
+    font.GetTextIntercepts(text_run_paint_info, device_scale_factor,
+                           default_paint, above_baseline_bounds,
+                           text_intercepts);
+    EXPECT_EQ(text_intercepts.size(), 5u);
+    for (auto text_intercept : text_intercepts) {
+      EXPECT_GT(text_intercept.end_, text_intercept.begin_);
+    }
+  });
+}
+
+TSAN_TEST(FontObjectThreadedTest, WordShaperTest) {
+  RunOnThreads([]() {
+    FontDescription font_description;
+    font_description.SetComputedSize(12.0);
+    font_description.SetLocale(LayoutLocale::Get("en"));
+    ASSERT_EQ(USCRIPT_LATIN, font_description.GetScript());
+    font_description.SetGenericFamily(FontDescription::kStandardFamily);
+
+    Font font = Font(font_description);
+    font.Update(nullptr);
+    ASSERT_TRUE(font.CanShapeWordByWord());
+    ShapeCache cache;
+
+    TextRun text_run(reinterpret_cast<const LChar*>("ABC DEF."), 8);
+
+    RefPtr<const ShapeResult> result;
+    CachingWordShapeIterator iter(&cache, text_run, &font);
+
+    ASSERT_TRUE(iter.Next(&result));
+    EXPECT_EQ(0u, result->StartIndexForResult());
+    EXPECT_EQ(3u, result->EndIndexForResult());
+
+    ASSERT_TRUE(iter.Next(&result));
+    EXPECT_EQ(0u, result->StartIndexForResult());
+    EXPECT_EQ(1u, result->EndIndexForResult());
+
+    ASSERT_TRUE(iter.Next(&result));
+    EXPECT_EQ(0u, result->StartIndexForResult());
+    EXPECT_EQ(4u, result->EndIndexForResult());
+
+    ASSERT_FALSE(iter.Next(&result));
   });
 }
 
