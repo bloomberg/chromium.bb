f1b40f1838e013f69c192843e01012b4fd54fe9b
jdoerrie <jdoerrie@chromium.org>
2017-07-19 15:54:51 +0000

    Disable Flaky WebNavigationApiTest.Failures on Mac
    TBR=jochen@chromium.org
    
    Bug: 746407
    Change-Id: I91caa2f48a47c8225d764f76355828dfa636ec2e
    Reviewed-on: https://chromium-review.googlesource.com/575246
    Reviewed-by: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Commit-Queue: Jan Wilken Dörrie <jdoerrie@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487873}
    Former-commit-id: 817d2624f59e3b6fa96486804c40fb8d3e396c05

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -487,7 +487,9 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, SimpleLoad) {
   ASSERT_TRUE(RunExtensionTest("webnavigation/simpleLoad")) << message_;
 }
 
-#if defined(OS_WIN)  // http://crbug.com/477840
+// Flaky on Windows and Mac. See http://crbug.com/477480 (Windows) and
+// https://crbug.com/746407 (Mac).
+#if defined(OS_WIN) || defined(OS_MACOSX)
 #define MAYBE_Failures DISABLED_Failures
 #else
 #define MAYBE_Failures Failures
