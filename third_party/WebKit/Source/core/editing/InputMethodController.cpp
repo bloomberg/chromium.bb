584cf0fc500e1df34f475f3860f12446169886a5
Changwan Ryu <changwan@chromium.org>
2017-07-19 05:51:13 +0000

    Clamp composition text when text is too long
    input and textarea element can set maxlength attribute to limit the text
    length. The desired behavior is to let the IME compose text as much as
    they want, and then clamp the text at FinishComposingText() because if
    we clamp the text at composition, IME can get confused. Most IMEs do not
    seem to care about the change at the finishing stage.
    
    By calling ReplaceComposition(), we get
    
    1) an additional compositionupdate event before compositionend event
    2) additional selectionchange events
    3) a call to HTML{Input,TextArea}Element::HandleBeforeTextInsertedEvent
       which clamps the text length
    
    Note that this is partially reverting http://crrev.com/2664603002,
    and reviving the autofill issue when the text is longer than maxlength.
    Autofill clearing + IME is essentially a general IME-editor conflict
    issue and may need an additional change to fix it correctly.
    
    BUG=721666
    
    Change-Id: I96f24c57e32357248834c756482ffffd4626f29b
    Reviewed-on: https://chromium-review.googlesource.com/576362
    Commit-Queue: Changwan Ryu <changwan@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Alexandre Elias <aelias@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487768}
    Former-commit-id: 7407bf52fde39b6dcb8f41271d0f664a3abcd683

--- third_party/WebKit/Source/core/editing/InputMethodController.cpp
+++ third_party/WebKit/Source/core/editing/InputMethodController.cpp
@@ -344,10 +344,23 @@ void InputMethodController::SelectComposition() const {
       SelectionInDOMTree::Builder().SetBaseAndExtent(range).Build(), 0);
 }
 
+bool IsCompositionTooLong(const Element& element) {
+  if (isHTMLInputElement(element))
+    return toHTMLInputElement(element).TooLong();
+  if (isHTMLTextAreaElement(element))
+    return toHTMLTextAreaElement(element).TooLong();
+  return false;
+}
+
 bool InputMethodController::FinishComposingText(
     ConfirmCompositionBehavior confirm_behavior) {
   if (!HasComposition())
     return false;
+
+  // If text is longer than maxlength, give input/textarea's handler a chance to
+  // clamp the text by replacing the composition with the same value.
+  const bool is_too_long =
+      IsCompositionTooLong(*GetDocument().FocusedElement());
 
   const String& composing = ComposingText();
 
@@ -359,8 +372,12 @@ bool InputMethodController::FinishComposingText(
     const PlainTextRange& old_offsets = GetSelectionOffsets();
     Editor::RevealSelectionScope reveal_selection_scope(&GetEditor());
 
-    Clear();
-    DispatchCompositionEndEvent(GetFrame(), composing);
+    if (is_too_long) {
+      ReplaceComposition(ComposingText());
+    } else {
+      Clear();
+      DispatchCompositionEndEvent(GetFrame(), composing);
+    }
 
     // TODO(editing-dev): Use of updateStyleAndLayoutIgnorePendingStylesheets
     // needs to be audited. see http://crbug.com/590369 for more details.
@@ -392,7 +409,11 @@ bool InputMethodController::FinishComposingText(
   if (composition_range.IsNull())
     return false;
 
-  Clear();
+  if (is_too_long) {
+    ReplaceComposition(ComposingText());
+  } else {
+    Clear();
+  }
 
   if (!MoveCaret(composition_range.End()))
     return false;
