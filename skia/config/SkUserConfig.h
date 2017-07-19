a12c72facc259a039d4ec00f79956ad72e6685a7
Florin Malita <fmalita@chromium.org>
2017-07-19 14:42:01 +0000

    Add guard for upcoming Skia change
    SK_SUPPORT_LEGACY_RADIAL_GRADIENT for
    https://skia-review.googlesource.com/c/24283.
    
    NOTRY=true
    
    Change-Id: Ia467184416526b870eca8b97f55ab06d7c792256
    Reviewed-on: https://chromium-review.googlesource.com/577594
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#487852}
    Former-commit-id: 467651aafd393af50da1e41f725926e7235207da

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -208,6 +208,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_USE_LEGACY_DISTANCE_FIELDS
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_RADIAL_GRADIENT
+#define SK_SUPPORT_LEGACY_RADIAL_GRADIENT
+#endif
+
 #ifndef SK_DISABLE_DEFERRED_PROXIES
 #define SK_DISABLE_DEFERRED_PROXIES
 #endif
