b3301923232fa45b02e177c83c158ff3410de006
Toni Barzic <tbarzic@google.com>
2017-07-20 19:37:09 +0000

    Make LockScreenActionTray item visible only with show-md-login flag
    The entry point for note taking on lock screen has been added to the
    lock screen UI for webui lock screen implementation - this means that
    the item in the system tray is not longer needed in this case.
    
    Keep the tray item around for views based lock screen (i.e.
    when show-md-login flag is set) - in this case the lock screen UI
    has yet to be updated, so there is still a need for an entry point in
    the tray.
    
    BUG=737067
    
    Change-Id: I53c53c7154c1dfec165c6a9898359b29b366db3f
    Reviewed-on: https://chromium-review.googlesource.com/578647
    Commit-Queue: Toni Barzic <tbarzic@chromium.org>
    Reviewed-by: Terry Anderson <tdanderson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488355}
    Former-commit-id: bed3c3d19f3fc96066b269a7c3bd2454f2e531a8

--- ash/system/lock_screen_action/lock_screen_action_tray_unittest.cc
+++ ash/system/lock_screen_action/lock_screen_action_tray_unittest.cc
@@ -12,7 +12,9 @@
 #include "ash/system/status_area_widget_test_helper.h"
 #include "ash/test/ash_test_base.h"
 #include "ash/tray_action/tray_action.h"
+#include "base/command_line.h"
 #include "base/macros.h"
+#include "chromeos/chromeos_switches.h"
 #include "components/session_manager/session_manager_types.h"
 #include "ui/events/test/event_generator.h"
 #include "ui/gfx/image/image_skia.h"
@@ -58,6 +60,12 @@ class LockScreenActionTrayTest : public AshTestBase {
   LockScreenActionTrayTest() = default;
   ~LockScreenActionTrayTest() override = default;
 
+  void SetUp() override {
+    base::CommandLine::ForCurrentProcess()->AppendSwitch(
+        chromeos::switches::kShowMdLogin);
+    AshTestBase::SetUp();
+  }
+
   void ClickOnTray() {
     // Perform click on the tray view.
     ui::test::EventGenerator& generator = GetEventGenerator();
@@ -71,6 +79,8 @@ class LockScreenActionTrayTest : public AshTestBase {
  private:
   DISALLOW_COPY_AND_ASSIGN(LockScreenActionTrayTest);
 };
+
+using LockScreenActionTrayWithoutMdLoginTest = AshTestBase;
 
 }  // namespace
 
@@ -226,6 +236,29 @@ TEST_F(LockScreenActionTrayTest, TrayNotVisibleWhenSessionNotLocked) {
   EXPECT_TRUE(GetTray()->visible());
 
   SetUserLoggedIn(false);
+  EXPECT_FALSE(GetTray()->visible());
+}
+
+TEST_F(LockScreenActionTrayWithoutMdLoginTest, NotVisible) {
+  SetUserLoggedIn(true);
+
+  TrayAction* tray_action = Shell::Get()->tray_action();
+  TestTrayActionClient tray_action_client;
+  tray_action->SetClient(tray_action_client.CreateInterfacePtrAndBind(),
+                         mojom::TrayActionState::kNotAvailable);
+
+  EXPECT_FALSE(GetTray()->visible());
+
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kAvailable);
+  EXPECT_FALSE(GetTray()->visible());
+
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kLaunching);
+  EXPECT_FALSE(GetTray()->visible());
+
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kActive);
+  EXPECT_FALSE(GetTray()->visible());
+
+  tray_action->UpdateLockScreenNoteState(mojom::TrayActionState::kBackground);
   EXPECT_FALSE(GetTray()->visible());
 }
 
