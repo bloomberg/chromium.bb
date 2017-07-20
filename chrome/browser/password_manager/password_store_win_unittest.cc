a5bb275901c4a67432fcae95f672fb3dfbebe915
Peter Kasting <pkasting@chromium.org>
2017-07-20 21:43:42 +0000

    Don't assume UI/DB threads unnecessarily.
    Some code that currently assumes it's running on the UI or DB threads can simply
    use "the current thread" or "the background thread" or similar.
    
    Bug: 689520
    Change-Id: Ide84528d7abea223939bef247c1f4d9c47bb8752
    Reviewed-on: https://chromium-review.googlesource.com/578144
    Commit-Queue: Peter Kasting <pkasting@chromium.org>
    Reviewed-by: Vaclav Brozek <vabr@chromium.org>
    Reviewed-by: Selim Gurun <sgurun@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488419}
    Former-commit-id: 13ea31c035bb660596a5d15af2aed54c5e565059

--- chrome/browser/password_manager/password_store_win_unittest.cc
+++ chrome/browser/password_manager/password_store_win_unittest.cc
@@ -22,6 +22,7 @@
 #include "base/synchronization/waitable_event.h"
 #include "base/test/histogram_tester.h"
 #include "base/threading/sequenced_task_runner_handle.h"
+#include "base/threading/thread_task_runner_handle.h"
 #include "base/time/time.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/os_crypt/ie7_password_win.h"
@@ -31,8 +32,6 @@
 #include "components/password_manager/core/browser/password_store_consumer.h"
 #include "components/password_manager/core/browser/webdata/logins_table.h"
 #include "components/password_manager/core/browser/webdata/password_web_data_service_win.h"
-#include "components/password_manager/core/common/password_manager_pref_names.h"
-#include "components/prefs/pref_service.h"
 #include "components/webdata/common/web_database_service.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/test_browser_thread_bundle.h"
@@ -138,14 +137,14 @@ class PasswordStoreWinTest : public testing::Test {
 
     base::FilePath path = temp_dir_.GetPath().AppendASCII("web_data_test");
     wdbs_ = new WebDatabaseService(
-        path, BrowserThread::GetTaskRunnerForThread(BrowserThread::UI),
+        path, base::ThreadTaskRunnerHandle::Get(),
         BrowserThread::GetTaskRunnerForThread(BrowserThread::DB));
     // Need to add at least one table so the database gets created.
     wdbs_->AddTable(std::unique_ptr<WebDatabaseTable>(new LoginsTable()));
     wdbs_->LoadDatabase();
-    wds_ = new PasswordWebDataService(
-        wdbs_, BrowserThread::GetTaskRunnerForThread(BrowserThread::UI),
-        WebDataServiceBase::ProfileErrorCallback());
+    wds_ =
+        new PasswordWebDataService(wdbs_, base::ThreadTaskRunnerHandle::Get(),
+                                   WebDataServiceBase::ProfileErrorCallback());
     wds_->Init();
   }
 
@@ -189,7 +188,6 @@ class PasswordStoreWinTest : public testing::Test {
 };
 
 ACTION(QuitUIMessageLoop) {
-  DCHECK_CURRENTLY_ON(BrowserThread::UI);
   base::MessageLoop::current()->QuitWhenIdle();
 }
 
