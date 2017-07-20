85b45ce4e310f5ccbc3f50189ac6d61f225f8499
danielpark@chromium.org <danielpark@chromium.org>
2017-07-20 23:20:50 +0000

    Hides keyboard after search
    Adds a call to hideKeyboard when the sheet is closed
    
    Bug: 746803
    Change-Id: Ib9d7a6a4696e7bf8f1864d0aaf3d6d3bb734f8d5
    Reviewed-on: https://chromium-review.googlesource.com/579790
    Commit-Queue: Daniel Park <danielpark@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488478}
    Former-commit-id: bade448099339e25ede870210d8230e4e1810fbe

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetContentController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetContentController.java
@@ -37,6 +37,7 @@ import org.chromium.chrome.browser.tabmodel.TabModelSelector;
 import org.chromium.chrome.browser.tabmodel.TabModelSelectorObserver;
 import org.chromium.chrome.browser.util.MathUtils;
 import org.chromium.chrome.browser.widget.bottomsheet.BottomSheet.BottomSheetContent;
+import org.chromium.ui.UiUtils;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -106,6 +107,9 @@ public class BottomSheetContentController extends BottomNavigationView
             } else {
                 clearBottomSheetContents(false);
             }
+            // The keyboard should be hidden when the sheet is closed in case it was made visible by
+            // sheet content.
+            UiUtils.hideKeyboard((View) BottomSheetContentController.this);
             // TODO(twellington): determine a policy for destroying the
             //                    SuggestionsBottomSheetContent.
         }
