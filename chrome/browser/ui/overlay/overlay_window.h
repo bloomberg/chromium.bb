a91dc0be1e251806652f519ea6f1fdbcc709eea8
apacible <apacible@chromium.org>
2017-07-19 23:31:27 -0700

    [OverlayWindow] Add platform-independent window and views implementation.
    This change adds a general window that overlays other windows, initially for the use case of picture in picture. This includes a partially stubbed views implementation. Cocoa work will be done in a separate CL.
    
    BUG=726621
    
    Review-Url: https://codereview.chromium.org/2905833004
    Cr-Commit-Position: refs/heads/master@{#488150}
    
    Former-commit-id: 36debc58c81094f47e733cf951afbd96c1ef4070

---
+++ chrome/browser/ui/overlay/overlay_window.h
@@ -0,0 +1,47 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_OVERLAY_OVERLAY_WINDOW_H_
+#define CHROME_BROWSER_UI_OVERLAY_OVERLAY_WINDOW_H_
+
+#include "base/memory/ptr_util.h"
+#include "ui/gfx/native_widget_types.h"
+
+namespace gfx {
+class Rect;
+}
+
+namespace ui {
+class Layer;
+}
+
+// This window will always float above other windows. The intention is to show
+// content perpetually while the user is still interacting with the other
+// browser windows.
+class OverlayWindow {
+ public:
+  OverlayWindow() = default;
+  virtual ~OverlayWindow() = default;
+
+  // Returns a created OverlayWindow. This is defined in the platform-specific
+  // implementation for the class.
+  static std::unique_ptr<OverlayWindow> Create();
+
+  virtual void Init() = 0;
+  virtual bool IsActive() const = 0;
+  virtual void Show() = 0;
+  virtual void Hide() = 0;
+  virtual void Close() = 0;
+  virtual void Activate() = 0;
+  virtual bool IsAlwaysOnTop() const = 0;
+  virtual ui::Layer* GetLayer() = 0;
+  virtual gfx::NativeWindow GetNativeWindow() const = 0;
+  // Retrieves the window's current bounds, including its window.
+  virtual gfx::Rect GetBounds() const = 0;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(OverlayWindow);
+};
+
+#endif  // CHROME_BROWSER_UI_OVERLAY_OVERLAY_WINDOW_H_
