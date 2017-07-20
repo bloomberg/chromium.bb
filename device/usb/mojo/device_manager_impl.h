8e158310d0473f38e521820876279f7ca41617d1
Vincent Scheib <scheib@chromium.org>
2017-07-20 23:37:36 +0000

    USB/WebUSB documentation, adding README.md files in several locations.
    Bug: 740070
    Change-Id: I9d0a7371c22856a641ad88910d55ba844aca4191
    Reviewed-on: https://chromium-review.googlesource.com/572332
    Commit-Queue: Vincent Scheib <scheib@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488481}
    Former-commit-id: 42d2353c55521340b8aff55cca1e9646617abd03

--- device/usb/mojo/device_manager_impl.h
+++ device/usb/mojo/device_manager_impl.h
@@ -26,8 +26,8 @@ namespace usb {
 
 class PermissionProvider;
 
-// Implementation of the public DeviceManager interface. This interface can be
-// requested from the devices app located at "devices", if available.
+// Implements the public Mojo UsbDeviceManager interface by wrapping the
+// UsbService instance.
 class DeviceManagerImpl : public mojom::UsbDeviceManager,
                           public UsbService::Observer {
  public:
