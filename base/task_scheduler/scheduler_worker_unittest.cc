1c5242c77f4081b8f4f2b635794326f103482a29
robliao <robliao@chromium.org>
2017-07-19 18:47:41 -0700

    Revert of Disable COM Initialization on SchedulerWorkers When COM_INIT_CHECK_HOOK_ENABLED() (patchset #1 id:1 of https://codereview.chromium.org/2978253002/ )
    Reason for revert:
    Causes
    
    base/task_scheduler/scheduler_worker.h(199,40):  error: private field 'backward_compatibility_' is not used [-Werror,-Wunused-private-field]
      const SchedulerBackwardCompatibility backward_compatibility_;
    
    Original issue's description:
    > Disable COM Initialization on SchedulerWorkers When COM_INIT_CHECK_HOOK_ENABLED()
    >
    > This change will allow for the detection of tasks running in
    > TaskScheduler that fail to post their task in a COM STA Task Runner
    > instead.
    >
    > BUG=706512
    > TBR=gab@chromium.org
    > Previously Reviewed at https://chromium-review.googlesource.com/c/549223/
    >
    > Review-Url: https://codereview.chromium.org/2978253002
    > Cr-Commit-Position: refs/heads/master@{#487625}
    > Committed: https://chromium.googlesource.com/chromium/src/+/2a58e233f8b6a41489aba0631300b6a5ea9d8a36 [formerly 122a24f59d90a1b225e00b3ab6c9fd37fd365b8f]
    
    TBR=gab@chromium.org,rnk@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=706512
    
    Review-Url: https://codereview.chromium.org/2983143002
    Cr-Commit-Position: refs/heads/master@{#488089}
    
    Former-commit-id: 9d02b58c2721f40f9b1f89af4fde464b6531360d

--- base/task_scheduler/scheduler_worker_unittest.cc
+++ base/task_scheduler/scheduler_worker_unittest.cc
@@ -30,8 +30,6 @@
 
 #if defined(OS_WIN)
 #include <objbase.h>
-
-#include "base/win/com_init_check_hook.h"
 #endif
 
 using testing::_;
@@ -933,13 +931,7 @@ TEST(TaskSchedulerWorkerTest, BackwardCompatibilityEnabled) {
 
   // The call to CoInitializeEx() should have returned S_FALSE to indicate that
   // the COM library was already initialized on the thread.
-  // See SchedulerWorker::Thread::ThreadMain for why we expect two different
-  // results here.
-#if defined(COM_INIT_CHECK_HOOK_ENABLED)
-  EXPECT_EQ(S_OK, delegate_raw->coinitialize_hresult());
-#else
   EXPECT_EQ(S_FALSE, delegate_raw->coinitialize_hresult());
-#endif
 
   worker->JoinForTesting();
 }
