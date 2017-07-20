b16dc006c4181987bfc7368c9b232d6be0e8d5c2
piotrs <piotrs@chromium.org>
2017-07-20 06:41:20 -0700

    Fixes the redirect to external apps when navigating from PWA.
    This is done with avoiding explicit chrome package name (using
    component name instead) and using
    EXTRA_SEND_TO_EXTERNAL_DEFAULT_HANDLER on the intent.
    
    With this patch the redirect works, however it takes a long time for CCT to
    consult ExternalNavigationHandler. This results in suboptimal UX. Same redirect
    in ChromeTabbedActivity is almost instantaneous. Result is that
    for _blank links CCT shows up briefly before redirecting to an external app.
    In a follow up it should be investigated why it is the case and if it cannot
    be improved, we might need to bypass CCT in such case, which would diverge
    from existing navigation paths for _blank links in Clank.
    
    BUG=740402
    
    Review-Url: https://codereview.chromium.org/2969143002
    Cr-Original-Original-Commit-Position: refs/heads/master@{#485831}
    Committed: https://chromium.googlesource.com/chromium/src/+/d58c6b4d6aef807362d7a156514d21bf1e31e01f [formerly 727468b41b2fe48bf73983ab0c3aa2632b47d89e]
    Review-Url: https://codereview.chromium.org/2969143002
    Cr-Original-Commit-Position: refs/heads/master@{#486275}
    Committed: https://chromium.googlesource.com/chromium/src/+/77d257a0fa1f42fef481b9b4dbf3fd885f4d52c8 [formerly 4dbcecfb630ac1a2dba1910f0e429b82bf3a0b5f]
    Review-Url: https://codereview.chromium.org/2969143002
    Cr-Commit-Position: refs/heads/master@{#488228}
    
    Former-commit-id: 471df3926c09fda0af15d9157eb29bd326b0f9fc

--- chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappTabDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/webapps/WebappTabDelegate.java
@@ -3,9 +3,16 @@
 // found in the LICENSE file.
 package org.chromium.chrome.browser.webapps;
 
+import android.content.Intent;
 import android.net.Uri;
+import android.os.StrictMode;
 import android.support.customtabs.CustomTabsIntent;
 
+import org.chromium.base.ContextUtils;
+import org.chromium.base.Log;
+import org.chromium.chrome.browser.IntentHandler;
+import org.chromium.chrome.browser.customtabs.CustomTabIntentDataProvider;
+import org.chromium.chrome.browser.externalnav.ExternalNavigationDelegateImpl;
 import org.chromium.chrome.browser.tab.Tab;
 import org.chromium.chrome.browser.tab.TabIdManager;
 import org.chromium.chrome.browser.tabmodel.AsyncTabParamsManager;
@@ -13,30 +20,62 @@ import org.chromium.chrome.browser.tabmodel.TabModel.TabLaunchType;
 import org.chromium.chrome.browser.tabmodel.document.AsyncTabCreationParams;
 import org.chromium.chrome.browser.tabmodel.document.TabDelegate;
 
+import java.net.URISyntaxException;
+
 /**
  * Asynchronously creates Tabs for navigation originating from an installed PWA.
  *
- * This is the same as the parent class with exception of opening a Custom Tab for
- * {@code _blank} links and {@code window.open(url)} calls instead of creating a new tab in Chrome.
+ * This is the same as the parent class with exception of checking for a specialized native handlers
+ * first, and if none are found opening a Custom Tab instead of creating a new tab in Chrome.
  */
 public class WebappTabDelegate extends TabDelegate {
-    private final WebappActivity mActivity;
+    private static final String TAG = "WebappTabDelegate";
 
-    public WebappTabDelegate(WebappActivity activity, boolean incognito) {
+    public WebappTabDelegate(boolean incognito) {
         super(incognito);
-        this.mActivity = activity;
     }
 
     @Override
     public void createNewTab(AsyncTabCreationParams asyncParams, TabLaunchType type, int parentId) {
+        String url = asyncParams.getLoadUrlParams().getUrl();
+        if (maybeStartExternalActivity(url)) {
+            return;
+        }
+
         int assignedTabId = TabIdManager.getInstance().generateValidId(Tab.INVALID_TAB_ID);
         AsyncTabParamsManager.add(assignedTabId, asyncParams);
 
-        CustomTabsIntent customTabIntent =
-                new CustomTabsIntent.Builder().setShowTitle(true).build();
+        Intent intent = new CustomTabsIntent.Builder().setShowTitle(true).build().intent;
+        intent.setData(Uri.parse(url));
+        intent.putExtra(CustomTabIntentDataProvider.EXTRA_SEND_TO_EXTERNAL_DEFAULT_HANDLER, true);
+        intent.putExtra(CustomTabIntentDataProvider.EXTRA_IS_OPENED_BY_CHROME, true);
+        addAsyncTabExtras(asyncParams, parentId, false /* isChromeUI */, assignedTabId, intent);
 
-        customTabIntent.intent.setPackage(mActivity.getPackageName());
-        addAsyncTabExtras(asyncParams, parentId, true, assignedTabId, customTabIntent.intent);
-        customTabIntent.launchUrl(mActivity, Uri.parse(asyncParams.getLoadUrlParams().getUrl()));
+        IntentHandler.startActivityForTrustedIntent(intent);
+    }
+
+    private boolean maybeStartExternalActivity(String url) {
+        Intent intent;
+        try {
+            intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME);
+        } catch (URISyntaxException ex) {
+            Log.w(TAG, "Bad URI %s", url, ex);
+            return false;
+        }
+
+        // See http://crbug.com/613977 for more context.
+        StrictMode.ThreadPolicy oldPolicy = StrictMode.allowThreadDiskWrites();
+        try {
+            // Launch a native app iff there is a specialized handler for a given URL.
+            if (ExternalNavigationDelegateImpl.isPackageSpecializedHandler(null, intent)) {
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                ContextUtils.getApplicationContext().startActivity(intent);
+                return true;
+            }
+        } finally {
+            StrictMode.setThreadPolicy(oldPolicy);
+        }
+
+        return false;
     }
 }
--- chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkIntegrationTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebApkIntegrationTest.java
@@ -4,6 +4,7 @@
 
 package org.chromium.chrome.browser.webapps;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.LargeTest;
@@ -15,11 +16,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.chromium.base.ApplicationStatus;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.base.test.util.CommandLineFlags;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.ScalableTimeout;
-import org.chromium.chrome.browser.ChromeActivity;
 import org.chromium.chrome.browser.ChromeSwitches;
 import org.chromium.chrome.browser.DeferredStartupHandler;
 import org.chromium.chrome.browser.ShortcutHelper;
@@ -44,9 +45,6 @@ public class WebApkIntegrationTest {
 
     @Rule
     public final NativeLibraryTestRule mNativeLibraryTestRule = new NativeLibraryTestRule();
-
-    @Rule
-    public final TopActivityListener activityListener = new TopActivityListener();
 
     private static final long STARTUP_TIMEOUT = ScalableTimeout.scaleTimeout(10000);
 
@@ -126,11 +124,14 @@ public class WebApkIntegrationTest {
         CriteriaHelper.pollUiThread(new Criteria() {
             @Override
             public boolean isSatisfied() {
-                ChromeActivity activity = (ChromeActivity) activityListener.getMostRecentActivity();
-                return activity instanceof CustomTabActivity
-                        && activity.getActivityTab() != null
+                Activity activity = ApplicationStatus.getLastTrackedFocusedActivity();
+                if (!(activity instanceof CustomTabActivity)) {
+                    return false;
+                }
+                CustomTabActivity customTab = (CustomTabActivity) activity;
+                return customTab.getActivityTab() != null
                         // Dropping the TLD as Google can redirect to a local site.
-                        && activity.getActivityTab().getUrl().startsWith("https://www.google.");
+                        && customTab.getActivityTab().getUrl().startsWith("https://www.google.");
             }
         });
     }
