b955825d91fd2dbb3586147b9d5610397343f0c9
Adam Rice <ricea@chromium.org>
2017-07-20 02:42:36 +0000

    Add UMA metrics for WebSocket SafeBrowsing lookups
    Add the following metrics for WebSocket SafeBrowsing lookups:
    
    * SafeBrowsing.WebSocket.Result is recorded for every WebSocket handshake that
      is checked.
    * - SafeBrowsing.WebSocket.Elapsed.Safe
      - SafeBrowsing.WebSocket.Elapsed.Blocked
      - SafeBrowsing.WebSocket.Elapsed.Abandoned
      Record the time in milliseconds spent performing the SafeBrowsing lookup,
      including time spent on the interstitial page if applicable, broken down by
      result.
    
    Because clicking "Back to safety" on the interstitial results in navigating away
    from the page, it is recorded as "Abandoned", not "Blocked" as might be
    expected. I haven't found a circumstance in which "Blocked" is recorded.
    
    Tested manually with about:histograms.
    
    Bug: 644744
    Change-Id: I4ec902e122f48482c6c4d54537ce8035bbac92fc
    Reviewed-on: https://chromium-review.googlesource.com/569942
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Commit-Queue: Adam Rice <ricea@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488107}
    Former-commit-id: 709344e608a6d7b82ba93e14cd49cbc08f3d590f

--- components/safe_browsing/renderer/websocket_sb_handshake_throttle.cc
+++ components/safe_browsing/renderer/websocket_sb_handshake_throttle.cc
@@ -8,6 +8,7 @@
 
 #include "base/bind.h"
 #include "base/logging.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/stringprintf.h"
 #include "content/public/common/resource_type.h"
 #include "content/public/renderer/render_frame.h"
@@ -20,9 +21,25 @@ namespace safe_browsing {
 
 WebSocketSBHandshakeThrottle::WebSocketSBHandshakeThrottle(
     mojom::SafeBrowsing* safe_browsing)
-    : callbacks_(nullptr), safe_browsing_(safe_browsing), weak_factory_(this) {}
+    : callbacks_(nullptr),
+      safe_browsing_(safe_browsing),
+      result_(Result::UNKNOWN),
+      weak_factory_(this) {}
 
-WebSocketSBHandshakeThrottle::~WebSocketSBHandshakeThrottle() {}
+WebSocketSBHandshakeThrottle::~WebSocketSBHandshakeThrottle() {
+  // ThrottleHandshake() should always be called, but since that is done all the
+  // way over in Blink, just avoid logging if it is not called rather than
+  // DCHECK()ing.
+  if (start_time_.is_null())
+    return;
+  if (result_ == Result::UNKNOWN) {
+    result_ = Result::ABANDONED;
+    UMA_HISTOGRAM_TIMES("SafeBrowsing.WebSocket.Elapsed.Abandoned",
+                        base::TimeTicks::Now() - start_time_);
+  }
+  UMA_HISTOGRAM_ENUMERATION("SafeBrowsing.WebSocket.Result", result_,
+                            Result::RESULT_COUNT);
+}
 
 void WebSocketSBHandshakeThrottle::ThrottleHandshake(
     const blink::WebURL& url,
@@ -38,6 +55,7 @@ void WebSocketSBHandshakeThrottle::ThrottleHandshake(
         content::RenderFrame::FromWebFrame(web_local_frame)->GetRoutingID();
   }
   int load_flags = 0;
+  start_time_ = base::TimeTicks::Now();
   safe_browsing_->CreateCheckerAndCheck(
       render_frame_id, mojo::MakeRequest(&url_checker_), url, load_flags,
       content::RESOURCE_TYPE_SUB_RESOURCE,
@@ -46,9 +64,17 @@ void WebSocketSBHandshakeThrottle::ThrottleHandshake(
 }
 
 void WebSocketSBHandshakeThrottle::OnCheckResult(bool safe) {
+  DCHECK(!start_time_.is_null());
+  base::TimeDelta elapsed = base::TimeTicks::Now() - start_time_;
   if (safe) {
+    result_ = Result::SAFE;
+    UMA_HISTOGRAM_TIMES("SafeBrowsing.WebSocket.Elapsed.Safe", elapsed);
     callbacks_->OnSuccess();
   } else {
+    // When the insterstitial is dismissed the page is navigated and this object
+    // is destroyed before reaching here.
+    result_ = Result::BLOCKED;
+    UMA_HISTOGRAM_TIMES("SafeBrowsing.WebSocket.Elapsed.Blocked", elapsed);
     callbacks_->OnError(blink::WebString::FromUTF8(base::StringPrintf(
         "WebSocket connection to %s failed safe browsing check",
         url_.spec().c_str())));
