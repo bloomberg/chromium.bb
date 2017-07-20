ff4c3fedd71ef79e0a07afa75e725029cf2690e3
Colin Blundell <blundell@chromium.org>
2017-07-20 09:44:54 +0000

    Convert chrome.identity.getAccounts() impl away from AccountTracker
    As a precursor to changing the implementation of
    chrome.identity.getAccounts() to use the Identity Service, this CL
    moves that implementation away from using AccountTracker::GetAccounts()
    to using OAuth2TokenService::GetAccounts(). The latter is the function
    that we intend to back the upcoming Identity Service API.
    
    AccountTracker::GetAccounts() is fundamentally based on
    OAuth2TokenService::GetAccounts(): they both have the basic behavior
    of returning the informations of all accounts for which a refresh
    token is available. However, AccountTracker::GetAccounts() layers
    some extra information on top relating to the primary account (a
    concept of which the OAuth2TokenService is not aware). (AccountTracker
    itself gets this information via IdentityProvider; in the context of
    the chrome.identity.getAccounts() impl, this is ProfileIdentityProvider,
    which is backed by SigninManager).
    
    To avoid any behavioral changes, this CL ports the particular semantics
    of AccountTracker::GetAccounts() to live directly in the implementation
    of chrome.identity.getAccounts():
    - Returns an empty list if there is no primary account or there is no
      refresh token available for the primary account
    - Otherwise, puts the primary account first in the returned list
    
    Bug: 729536
    Change-Id: Ib4f6cff0afd2f751ec3a7409d24b797b1be0ec79
    Reviewed-on: https://chromium-review.googlesource.com/574709
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Colin Blundell <blundell@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488177}
    Former-commit-id: 2e73f36bd9cc84ffe7db1b15fc12bc486b21ccbd

--- chrome/browser/extensions/api/identity/identity_get_accounts_function.cc
+++ chrome/browser/extensions/api/identity/identity_get_accounts_function.cc
@@ -7,7 +7,13 @@
 #include "chrome/browser/extensions/api/identity/identity_api.h"
 #include "chrome/browser/extensions/api/identity/identity_constants.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/signin/account_tracker_service_factory.h"
+#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
+#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/common/extensions/api/identity.h"
+#include "components/signin/core/browser/account_tracker_service.h"
+#include "components/signin/core/browser/profile_oauth2_token_service.h"
+#include "components/signin/core/browser/signin_manager.h"
 #include "components/signin/core/common/profile_management_switches.h"
 
 namespace extensions {
@@ -23,17 +29,44 @@ ExtensionFunction::ResponseAction IdentityGetAccountsFunction::Run() {
     return RespondNow(Error(identity_constants::kOffTheRecord));
   }
 
-  std::vector<std::string> gaia_ids =
-      IdentityAPI::GetFactoryInstance()->Get(GetProfile())->GetAccounts();
-  DCHECK(gaia_ids.size() < 2 || switches::IsExtensionsMultiAccount());
+  ProfileOAuth2TokenService* token_service =
+      ProfileOAuth2TokenServiceFactory::GetForProfile(GetProfile());
+  AccountTrackerService* account_tracker =
+      AccountTrackerServiceFactory::GetForProfile(GetProfile());
 
   std::unique_ptr<base::ListValue> infos(new base::ListValue());
+  std::string primary_account =
+      SigninManagerFactory::GetForProfile(GetProfile())
+          ->GetAuthenticatedAccountId();
 
-  for (std::vector<std::string>::const_iterator it = gaia_ids.begin();
-       it != gaia_ids.end();
-       ++it) {
+  // If there is no primary account, short-circuit out.
+  if (primary_account.empty() ||
+      !token_service->RefreshTokenIsAvailable(primary_account))
+    return RespondNow(OneArgument(std::move(infos)));
+
+  // Set the primary account as the first entry.
+  std::string primary_gaia_id =
+      account_tracker->GetAccountInfo(primary_account).gaia;
+  api::identity::AccountInfo primary_account_info;
+  primary_account_info.id = primary_gaia_id;
+  infos->Append(primary_account_info.ToValue());
+
+  // If extensions are not multi-account, ignore any other accounts.
+  if (!switches::IsExtensionsMultiAccount())
+    return RespondNow(OneArgument(std::move(infos)));
+
+  // Otherwise, add the other accounts.
+  std::vector<std::string> accounts = token_service->GetAccounts();
+  for (std::vector<std::string>::const_iterator it = accounts.begin();
+       it != accounts.end(); ++it) {
+    std::string gaia_id = account_tracker->GetAccountInfo(*it).gaia;
+    if (gaia_id.empty())
+      continue;
+    if (gaia_id == primary_gaia_id)
+      continue;
+
     api::identity::AccountInfo account_info;
-    account_info.id = *it;
+    account_info.id = gaia_id;
     infos->Append(account_info.ToValue());
   }
 
