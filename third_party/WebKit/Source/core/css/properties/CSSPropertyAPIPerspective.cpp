13959fced8440f179e401b595a790bebde61933a
Rob Buis <rob.buis@samsung.com>
2017-07-20 22:24:02 +0000

    Implements CSSPropertyAPI for -webkit-perspective/perspective
    As part of Project Ribbon, separate the parsing logic for CSS
    properties -webkit-perspective and perspective from
    the parser into an API.
    
    Bug: 668012
    
    Signed-off-by: Rob Buis <rob.buis@samsung.com>
    Change-Id: I504248e8561a9f97f741a5b5e3338b29d6caa395
    Reviewed-on: https://chromium-review.googlesource.com/578213
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488440}
    Former-commit-id: 5275eb16f93c821123d826f3dc5744b8af57b0e5

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPIPerspective.cpp
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIPerspective.cpp
@@ -4,4 +4,34 @@
 
 #include "core/css/properties/CSSPropertyAPIPerspective.h"
 
-namespace blink {}  // namespace blink
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSParserLocalContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "core/frame/UseCounter.h"
+
+namespace blink {
+
+const CSSValue* CSSPropertyAPIPerspective::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    const CSSParserLocalContext& localContext) {
+  if (range.Peek().Id() == CSSValueNone)
+    return CSSPropertyParserHelpers::ConsumeIdent(range);
+  CSSPrimitiveValue* parsed_value = CSSPropertyParserHelpers::ConsumeLength(
+      range, context.Mode(), kValueRangeAll);
+  bool use_legacy_parsing = localContext.UseAliasParsing();
+  if (!parsed_value && use_legacy_parsing) {
+    double perspective;
+    if (!CSSPropertyParserHelpers::ConsumeNumberRaw(range, perspective))
+      return nullptr;
+    context.Count(WebFeature::kUnitlessPerspectiveInPerspectiveProperty);
+    parsed_value = CSSPrimitiveValue::Create(
+        perspective, CSSPrimitiveValue::UnitType::kPixels);
+  }
+  if (parsed_value &&
+      (parsed_value->IsCalculated() || parsed_value->GetDoubleValue() > 0))
+    return parsed_value;
+  return nullptr;
+}
+
+}  // namespace blink
