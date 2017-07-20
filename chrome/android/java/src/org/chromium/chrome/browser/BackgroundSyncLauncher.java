89f71fe13032ec39f958a450b4af2867fe3c8cb9
estevenson <estevenson@chromium.org>
2017-07-20 13:36:03 -0700

    Revert of Handle Java assert using a customized function in base (patchset #11 id:470001 of https://codereview.chromium.org/2971063003/ )
    Reason for revert:
    Reland changes weren't reviewed
    
    Original issue's description:
    > Create java_assertion_handler, a Java binary that takes in a JAR file
    > and modifies the bytecode of classes.
    >
    > Replace the Java ASSERT with JavaExceptionReporter.assertFailureHandler
    > that throws Exception.
    >
    > Output is written to a new JAR file.
    >
    >
    > BUG=672945
    >
    > Review-Url: https://codereview.chromium.org/2971063003
    > Cr-Original-Commit-Position: refs/heads/master@{#487903}
    > Committed: https://chromium.googlesource.com/chromium/src/+/78e973f3891bb0793dd02b45e5344633fa75ab30 [formerly 33133e5767a9c1ca1f7e9defad2d7b3347065589]
    > Review-Url: https://codereview.chromium.org/2971063003
    > Cr-Commit-Position: refs/heads/master@{#488381}
    > Committed: https://chromium.googlesource.com/chromium/src/+/69fa5999d8932618b5a53520df070d28e6f5a356 [formerly bef3daf02ae6035f166782025fd7e4e54d465271]
    
    TBR=yfriedman@chromium.org,agrieve@chromium.org,zpeng@chromium.org,ranj@chromium.org
    # Skipping CQ checks because original CL landed less than 1 days ago.
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=672945
    
    Review-Url: https://codereview.chromium.org/2985613002
    Cr-Commit-Position: refs/heads/master@{#488390}
    
    Former-commit-id: 83b7815399e50c9f9da3434669eba2829c2857e0

--- chrome/android/java/src/org/chromium/chrome/browser/BackgroundSyncLauncher.java
+++ chrome/android/java/src/org/chromium/chrome/browser/BackgroundSyncLauncher.java
@@ -17,7 +17,6 @@ import org.chromium.base.ContextUtils;
 import org.chromium.base.Log;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.annotations.CalledByNative;
-import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.base.metrics.RecordHistogram;
 import org.chromium.chrome.browser.externalauth.ExternalAuthUtils;
 import org.chromium.chrome.browser.externalauth.UserRecoverableErrorHandler;
@@ -71,7 +70,6 @@ public class BackgroundSyncLauncher {
     /**
      * Called when the C++ counterpart is deleted.
      */
-    @SuppressFBWarnings("ST_WRITE_TO_STATIC_FROM_INSTANCE_METHOD")
     @VisibleForTesting
     @CalledByNative
     protected void destroy() {
