32909223a84c5767bc122d504a5b1889fe9c7a31
khmel <khmel@google.com>
2017-07-21 18:06:00 +0000

    arc: Hide default Play Store icon on shelf for AOSP images.
    This prevents appearing default Play Store icon on the shelf for AOSP
    images.
    
    Test: Unit test + manually
    Bug: b/36133646
    Change-Id: I90d05513d98c57a7ec514b66a8b468255434e841
    Reviewed-on: https://chromium-review.googlesource.com/580646
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Yury Khmel <khmel@google.com>
    Cr-Commit-Position: refs/heads/master@{#488705}
    Former-commit-id: d702c3905dbe9ef36bfda544228b53bf073702f4

--- chrome/browser/ui/ash/launcher/launcher_controller_helper.cc
+++ chrome/browser/ui/ash/launcher/launcher_controller_helper.cc
@@ -24,6 +24,7 @@
 #include "chrome/browser/ui/extensions/extension_enable_flow.h"
 #include "chrome/browser/web_applications/web_app.h"
 #include "chrome/common/extensions/manifest_handlers/app_launch_info.h"
+#include "components/arc/arc_util.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/web_contents.h"
 #include "extensions/browser/extension_registry.h"
@@ -149,10 +150,11 @@ bool LauncherControllerHelper::IsValidIDForCurrentUser(
   const ArcAppListPrefs* arc_prefs = GetArcAppListPrefs();
   if (arc_prefs && arc_prefs->IsRegistered(id))
     return true;
+
   if (!GetExtensionByID(profile_, id))
     return false;
   if (id == arc::kPlayStoreAppId) {
-    if (!arc::IsArcAllowedForProfile(profile()))
+    if (!arc::IsArcAllowedForProfile(profile()) || !arc::IsPlayStoreAvailable())
       return false;
     const arc::ArcSessionManager* arc_session_manager =
         arc::ArcSessionManager::Get();
