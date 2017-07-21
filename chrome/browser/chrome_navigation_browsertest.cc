dd2bcafae66ec1a07abd5b95b786e273c89b67cb
Alex Moshchuk <alexmos@chromium.org>
2017-07-21 01:06:23 +0000

    Add synthetic field trial for sign-in isolation.
    This will allow filtering results for the sign-in process isolation
    trial to users that have actually browsed to accounts.google.com.
    
    Bug: 739418
    Change-Id: I31c9b36614cbcf1440c692d29450a851a2e29aa3
    Reviewed-on: https://chromium-review.googlesource.com/579629
    Commit-Queue: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Reviewed-by: Lukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488532}
    Former-commit-id: 95e8906de1c8c086fee109c0de0b3a322b1faf12

--- chrome/browser/chrome_navigation_browsertest.cc
+++ chrome/browser/chrome_navigation_browsertest.cc
@@ -14,6 +14,8 @@
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/network_session_configurator/common/network_switches.h"
 #include "components/url_formatter/url_formatter.h"
+#include "components/variations/active_field_trials.h"
+#include "components/variations/metrics_util.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_handle.h"
 #include "content/public/browser/notification_service.h"
@@ -447,8 +449,12 @@ class SignInIsolationBrowserTest : public ChromeNavigationBrowserTest {
       : https_server_(net::EmbeddedTestServer::TYPE_HTTPS) {}
   ~SignInIsolationBrowserTest() override {}
 
-  void SetUp() override {
+  virtual void InitFeatureList() {
     feature_list_.InitAndEnableFeature(features::kSignInProcessIsolation);
+  }
+
+  void SetUp() override {
+    InitFeatureList();
     https_server_.ServeFilesFromSourceDirectory("chrome/test/data");
     ASSERT_TRUE(https_server_.InitializeAndListen());
     ChromeNavigationBrowserTest::SetUp();
@@ -477,8 +483,41 @@ class SignInIsolationBrowserTest : public ChromeNavigationBrowserTest {
 
   net::EmbeddedTestServer* https_server() { return &https_server_; }
 
- private:
+  bool HasSyntheticTrial(const std::string& trial_name) {
+    std::vector<std::string> synthetic_trials;
+    variations::GetSyntheticTrialGroupIdsAsString(&synthetic_trials);
+    std::string trial_hash =
+        base::StringPrintf("%x", metrics::HashName(trial_name));
+
+    for (auto entry : synthetic_trials) {
+      if (base::StartsWith(entry, trial_hash, base::CompareCase::SENSITIVE))
+        return true;
+    }
+
+    return false;
+  }
+
+  bool IsInSyntheticTrialGroup(const std::string& trial_name,
+                               const std::string& trial_group) {
+    std::vector<std::string> synthetic_trials;
+    variations::GetSyntheticTrialGroupIdsAsString(&synthetic_trials);
+    std::string expected_entry = base::StringPrintf(
+        "%x-%x", metrics::HashName(trial_name), metrics::HashName(trial_group));
+
+    for (auto entry : synthetic_trials) {
+      if (entry == expected_entry)
+        return true;
+    }
+
+    return false;
+  }
+
+  const std::string kSyntheticTrialName = "SignInProcessIsolationActive";
+
+ protected:
   base::test::ScopedFeatureList feature_list_;
+
+ private:
   net::EmbeddedTestServer https_server_;
 
   DISALLOW_COPY_AND_ASSIGN(SignInIsolationBrowserTest);
@@ -506,4 +545,137 @@ IN_PROC_BROWSER_TEST_F(SignInIsolationBrowserTest, NavigateToSignInPage) {
       ExecuteScript(web_contents, "location = '" + signin_url.spec() + "';"));
   manager.WaitForNavigationFinished();
   EXPECT_NE(web_contents->GetMainFrame()->GetSiteInstance(), first_instance);
+}
+
+// The next four tests verify that the synthetic field trial is set correctly
+// for sign-in process isolation.  The synthetic field trial should be created
+// when browsing to the sign-in URL for the first time, and it should reflect
+// whether or not the sign-in isolation base::Feature is enabled, and whether
+// or not it is force-enabled from the command line.
+IN_PROC_BROWSER_TEST_F(SignInIsolationBrowserTest, SyntheticTrial) {
+  EXPECT_FALSE(HasSyntheticTrial(kSyntheticTrialName));
+
+  ui_test_utils::NavigateToURL(
+      browser(), https_server()->GetURL("foo.com", "/title1.html"));
+  EXPECT_FALSE(HasSyntheticTrial(kSyntheticTrialName));
+
+  GURL signin_url(
+      https_server()->GetURL("accounts.google.com", "/title1.html"));
+
+  // This test class uses InitAndEnableFeature, which overrides the feature
+  // settings as if it came from the command line, so by default, browsing to
+  // the sign-in URL should create the synthetic trial with ForceEnabled.
+  ui_test_utils::NavigateToURL(browser(), signin_url);
+  EXPECT_TRUE(IsInSyntheticTrialGroup(kSyntheticTrialName, "ForceEnabled"));
+}
+
+// This test class is used to check the synthetic sign-in trial for the Enabled
+// group. It creates a new field trial (with 100% probability of being in the
+// group), and initializes the test class's ScopedFeatureList using it, being
+// careful to not override it using the command line (which corresponds to
+// ForceEnabled).
+class EnabledSignInIsolationBrowserTest : public SignInIsolationBrowserTest {
+ public:
+  EnabledSignInIsolationBrowserTest() {}
+  ~EnabledSignInIsolationBrowserTest() override {}
+
+  void InitFeatureList() override {}
+
+  void SetUpOnMainThread() override {
+    const std::string kTrialName = "SignInProcessIsolation";
+    const std::string kGroupName = "FooGroup";  // unused
+    scoped_refptr<base::FieldTrial> trial =
+        base::FieldTrialList::CreateFieldTrial(kTrialName, kGroupName);
+
+    std::unique_ptr<base::FeatureList> feature_list(new base::FeatureList);
+    feature_list->RegisterFieldTrialOverride(
+        features::kSignInProcessIsolation.name,
+        base::FeatureList::OverrideState::OVERRIDE_ENABLE_FEATURE, trial.get());
+
+    feature_list_.InitWithFeatureList(std::move(feature_list));
+    SignInIsolationBrowserTest::SetUpOnMainThread();
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(EnabledSignInIsolationBrowserTest);
+};
+
+IN_PROC_BROWSER_TEST_F(EnabledSignInIsolationBrowserTest, SyntheticTrial) {
+  EXPECT_FALSE(HasSyntheticTrial(kSyntheticTrialName));
+  EXPECT_FALSE(IsInSyntheticTrialGroup(kSyntheticTrialName, "Enabled"));
+
+  GURL signin_url =
+      https_server()->GetURL("accounts.google.com", "/title1.html");
+  ui_test_utils::NavigateToURL(browser(), signin_url);
+  EXPECT_TRUE(IsInSyntheticTrialGroup(kSyntheticTrialName, "Enabled"));
+
+  // A repeat navigation shouldn't change the synthetic trial.
+  ui_test_utils::NavigateToURL(
+      browser(), https_server()->GetURL("accounts.google.com", "/title2.html"));
+  EXPECT_TRUE(IsInSyntheticTrialGroup(kSyntheticTrialName, "Enabled"));
+}
+
+// This test class is similar to EnabledSignInIsolationBrowserTest, but for the
+// Disabled group of the synthetic sign-in trial.
+class DisabledSignInIsolationBrowserTest : public SignInIsolationBrowserTest {
+ public:
+  DisabledSignInIsolationBrowserTest() {}
+  ~DisabledSignInIsolationBrowserTest() override {}
+
+  void InitFeatureList() override {}
+
+  void SetUpOnMainThread() override {
+    const std::string kTrialName = "SignInProcessIsolation";
+    const std::string kGroupName = "FooGroup";  // unused
+    scoped_refptr<base::FieldTrial> trial =
+        base::FieldTrialList::CreateFieldTrial(kTrialName, kGroupName);
+
+    std::unique_ptr<base::FeatureList> feature_list(new base::FeatureList);
+    feature_list->RegisterFieldTrialOverride(
+        features::kSignInProcessIsolation.name,
+        base::FeatureList::OverrideState::OVERRIDE_DISABLE_FEATURE,
+        trial.get());
+
+    feature_list_.InitWithFeatureList(std::move(feature_list));
+    SignInIsolationBrowserTest::SetUpOnMainThread();
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(DisabledSignInIsolationBrowserTest);
+};
+
+IN_PROC_BROWSER_TEST_F(DisabledSignInIsolationBrowserTest, SyntheticTrial) {
+  EXPECT_FALSE(IsInSyntheticTrialGroup(kSyntheticTrialName, "Disabled"));
+  GURL signin_url =
+      https_server()->GetURL("accounts.google.com", "/title1.html");
+  ui_test_utils::NavigateToURL(browser(), signin_url);
+  EXPECT_TRUE(IsInSyntheticTrialGroup(kSyntheticTrialName, "Disabled"));
+}
+
+// This test class is similar to EnabledSignInIsolationBrowserTest, but for the
+// ForceDisabled group of the synthetic sign-in trial.
+class ForceDisabledSignInIsolationBrowserTest
+    : public SignInIsolationBrowserTest {
+ public:
+  ForceDisabledSignInIsolationBrowserTest() {}
+  ~ForceDisabledSignInIsolationBrowserTest() override {}
+
+  void InitFeatureList() override {
+    feature_list_.InitAndDisableFeature(features::kSignInProcessIsolation);
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(ForceDisabledSignInIsolationBrowserTest);
+};
+
+IN_PROC_BROWSER_TEST_F(ForceDisabledSignInIsolationBrowserTest,
+                       SyntheticTrial) {
+  // Test subframe navigation in this case, since that should also trigger
+  // synthetic trial creation.
+  ui_test_utils::NavigateToURL(browser(),
+                               https_server()->GetURL("a.com", "/iframe.html"));
+  content::WebContents* web_contents =
+      browser()->tab_strip_model()->GetActiveWebContents();
+  EXPECT_FALSE(IsInSyntheticTrialGroup(kSyntheticTrialName, "ForceDisabled"));
+  GURL signin_url =
+      https_server()->GetURL("accounts.google.com", "/title1.html");
+  EXPECT_TRUE(NavigateIframeToURL(web_contents, "test", signin_url));
+  EXPECT_TRUE(IsInSyntheticTrialGroup(kSyntheticTrialName, "ForceDisabled"));
 }
