da9eb279098c038e1afdf8202dbe9c5327872f94
Fredrik Hubinette <hubbe@google.com>
2017-07-21 00:55:31 +0000

    media: speed up H264Parser::FindStartCode
    10000 runs of H264ParserTest goes from 3327ms to 2370ms.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Ia64f90f5f1f89e2fc8ccfdec26040cc179112d25
    Reviewed-on: https://chromium-review.googlesource.com/580447
    Commit-Queue: Fredrik Hubinette <hubbe@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488521}
    Former-commit-id: bfc6ec6735cf439c2971eb2129ae357e5b57e0de

--- media/filters/h264_parser.cc
+++ media/filters/h264_parser.cc
@@ -287,6 +287,19 @@ bool H264Parser::FindStartCode(const uint8_t* data,
   off_t bytes_left = data_size;
 
   while (bytes_left >= 3) {
+    // The start code is "\0\0\1", ones are more unusual than zeroes, so let's
+    // search for it first.
+    const uint8_t* tmp =
+        reinterpret_cast<const uint8_t*>(memchr(data + 2, 1, bytes_left - 2));
+    if (!tmp) {
+      data += bytes_left - 2;
+      bytes_left = 2;
+      break;
+    }
+    tmp -= 2;
+    bytes_left -= tmp - data;
+    data = tmp;
+
     if (IsStartCode(data)) {
       // Found three-byte start code, set pointer at its beginning.
       *offset = data_size - bytes_left;
