3bd9514b696cf33506133fcdda6e52ab1c579d9b
Alex Zhang <staraz@chromium.org>
2017-07-21 17:25:27 +0000

    ResourceSettings struct traits
    To plumb RendererSettings through between browser and viz, we need
    struct traits for it and its members. ResourceSettings is a member of
    RendererSettings.
    
    This CL adds struct traits for ResourceSettings and a unit test for
    the struct traits.
    
    Bug: 732825
    Change-Id: I03010130b3614cc316f6fc234dea4b5bfbf3fbf5
    Reviewed-on: https://chromium-review.googlesource.com/575209
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xingyu Zhang <staraz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488689}
    Former-commit-id: de2037a5b5e0d6ffd72547f3438a3790e46ebb08

---
+++ services/viz/public/interfaces/compositing/resource_settings_struct_traits.cc
@@ -0,0 +1,42 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/viz/public/interfaces/compositing/resource_settings_struct_traits.h"
+
+namespace mojo {
+
+// static
+bool StructTraits<viz::mojom::BufferToTextureTargetKeyDataView,
+                  viz::BufferToTextureTargetKey>::
+    Read(viz::mojom::BufferToTextureTargetKeyDataView data,
+         viz::BufferToTextureTargetKey* out) {
+  return data.ReadUsage(&out->first) && data.ReadFormat(&out->second);
+}
+
+// static
+bool StructTraits<viz::mojom::ResourceSettingsDataView, viz::ResourceSettings>::
+    Read(viz::mojom::ResourceSettingsDataView data,
+         viz::ResourceSettings* out) {
+  out->texture_id_allocation_chunk_size =
+      data.texture_id_allocation_chunk_size();
+  out->use_gpu_memory_buffer_resources = data.use_gpu_memory_buffer_resources();
+
+  mojo::ArrayDataView<viz::mojom::BufferToTextureTargetPairDataView>
+      buffer_to_texture_target_array;
+  data.GetBufferToTextureTargetMapDataView(&buffer_to_texture_target_array);
+  for (size_t i = 0; i < buffer_to_texture_target_array.size(); ++i) {
+    viz::mojom::BufferToTextureTargetPairDataView buffer_to_texture_target_pair;
+    buffer_to_texture_target_array.GetDataView(i,
+                                               &buffer_to_texture_target_pair);
+    viz::BufferToTextureTargetKey key;
+    if (!buffer_to_texture_target_pair.ReadKey(&key))
+      return false;
+
+    auto& value = out->buffer_to_texture_target_map[key];
+    value = buffer_to_texture_target_pair.value();
+  }
+  return true;
+}
+
+}  // namespace mojo
---
+++ services/viz/public/interfaces/compositing/resource_settings_struct_traits.h
@@ -0,0 +1,92 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_VIZ_PUBLIC_INTERFACES_COMPOSITING_RESOURCE_SETTINGS_STRUCT_TRAITS_H_
+#define SERVICES_VIZ_PUBLIC_INTERFACES_COMPOSITING_RESOURCE_SETTINGS_STRUCT_TRAITS_H_
+
+#include "components/viz/common/resources/resource_settings.h"
+#include "services/viz/public/interfaces/compositing/resource_settings.mojom.h"
+#include "ui/gfx/mojo/buffer_types.mojom.h"
+#include "ui/gfx/mojo/buffer_types_struct_traits.h"
+
+namespace mojo {
+
+template <>
+struct ArrayTraits<viz::BufferToTextureTargetMap> {
+  using Element = viz::BufferToTextureTargetMap::value_type;
+  using Iterator = viz::BufferToTextureTargetMap::iterator;
+  using ConstIterator = viz::BufferToTextureTargetMap::const_iterator;
+
+  static ConstIterator GetBegin(const viz::BufferToTextureTargetMap& input) {
+    return input.begin();
+  }
+  static Iterator GetBegin(viz::BufferToTextureTargetMap& input) {
+    return input.begin();
+  }
+
+  static void AdvanceIterator(ConstIterator& iterator) { iterator++; }
+  static void AdvanceIterator(Iterator& iterator) { iterator++; }
+
+  static const Element& GetValue(ConstIterator& iterator) { return *iterator; }
+  static Element& GetValue(Iterator& iterator) { return *iterator; }
+
+  static size_t GetSize(const viz::BufferToTextureTargetMap& input) {
+    return input.size();
+  }
+};
+
+template <>
+struct StructTraits<viz::mojom::BufferToTextureTargetKeyDataView,
+                    std::pair<gfx::BufferUsage, gfx::BufferFormat>> {
+  static gfx::BufferUsage usage(const viz::BufferToTextureTargetKey& input) {
+    return input.first;
+  }
+
+  static gfx::BufferFormat format(const viz::BufferToTextureTargetKey& input) {
+    return input.second;
+  }
+
+  static bool Read(viz::mojom::BufferToTextureTargetKeyDataView data,
+                   viz::BufferToTextureTargetKey* out);
+};
+
+template <>
+struct StructTraits<viz::mojom::BufferToTextureTargetPairDataView,
+                    viz::BufferToTextureTargetMap::value_type> {
+  static const std::pair<gfx::BufferUsage, gfx::BufferFormat>& key(
+      const viz::BufferToTextureTargetMap::value_type& input) {
+    return input.first;
+  }
+
+  static uint32_t value(
+      const viz::BufferToTextureTargetMap::value_type& input) {
+    return input.second;
+  }
+};
+
+template <>
+struct StructTraits<viz::mojom::ResourceSettingsDataView,
+                    viz::ResourceSettings> {
+  static size_t texture_id_allocation_chunk_size(
+      const viz::ResourceSettings& input) {
+    return input.texture_id_allocation_chunk_size;
+  }
+
+  static bool use_gpu_memory_buffer_resources(
+      const viz::ResourceSettings& input) {
+    return input.use_gpu_memory_buffer_resources;
+  }
+
+  static const viz::BufferToTextureTargetMap& buffer_to_texture_target_map(
+      const viz::ResourceSettings& input) {
+    return input.buffer_to_texture_target_map;
+  }
+
+  static bool Read(viz::mojom::ResourceSettingsDataView data,
+                   viz::ResourceSettings* out);
+};
+
+}  // namespace mojo
+
+#endif  // SERVICES_VIZ_PUBLIC_INTERFACES_COMPOSITING_RESOURCE_SETTINGS_STRUCT_TRAITS_H_
