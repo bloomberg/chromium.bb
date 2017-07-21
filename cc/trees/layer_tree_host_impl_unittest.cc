cfd115cf8ad9acdcec94dec8e0c58d5aa0b8b437
Philip Rogers <pdr@chromium.org>
2017-07-21 19:36:50 +0000

    Revert "Remove LayerImpl dependency from LayerTreeImpl::DidUpdateScrollOffset"
    This reverts commit cc6f12e3a25fa2e3ab180b3c2758368679780436 [formerly 80d5a3ec9a3f7a2a94bfaa36aacc62979322125a].
    
    Reason for revert: May be the cause of scroll offset crashes (crbug.com/746718)
    
    Original change's description:
    > Remove LayerImpl dependency from LayerTreeImpl::DidUpdateScrollOffset
    > 
    > This patch removes the LayerImpl references from DidUpdateScrollOffset.
    > Instead of looking up scroll and transform property nodes with Layers,
    > ElementId is used.
    > 
    > Tests have been modified to ensure property trees exist before updating
    > scroll offset. This should be true in all real cases but is not for
    > synthetic testing scenarios.
    > 
    > A bug has been fixed in UpdateScrollChildPosition where the scroll layer
    > was not actually scrollable.
    > 
    > This is a reland of [1] which was rolled out because the active tree can
    > have a scroll offset for an element id that is not present in the pending
    > tree. The comment in DidUpdateScrollOffset has been updated to reflect
    > this. This is tested in PushPropertiesToMirrorsCurrentScrollOffset.
    > [1] https://chromium-review.googlesource.com/c/560584/
    > 
    > Bug: 743241, 742039
    > Change-Id: Ia13380d749843064f30de76ccfeeb5e72502e6b2
    > Reviewed-on: https://chromium-review.googlesource.com/572426
    > Reviewed-by: enne <enne@chromium.org>
    > Commit-Queue: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#487183}
    
    TBR=pdr@chromium.org,enne@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 743241, 746718
    Change-Id: I3a02c7a22c6db2d2e3eafcb83eace9e37db8c1d6
    Reviewed-on: https://chromium-review.googlesource.com/581629
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488737}
    Former-commit-id: 4131a1c1ef8bdde8e752353ad6e24771d3b866da

--- cc/trees/layer_tree_host_impl_unittest.cc
+++ cc/trees/layer_tree_host_impl_unittest.cc
@@ -4880,12 +4880,10 @@ TEST_F(LayerTreeHostImplBrowserControlsTest,
   child->SetDrawsContent(true);
   child->test_properties()->is_container_for_fixed_position_layers = true;
 
-  LayerImpl* child_ptr = child.get();
+  // scroll child to limit
+  SetScrollOffsetDelta(child.get(), gfx::Vector2dF(0, 100.f));
   outer_viewport_scroll_layer->test_properties()->AddChild(std::move(child));
   host_impl_->active_tree()->BuildPropertyTreesForTesting();
-
-  // Scroll child to the limit.
-  SetScrollOffsetDelta(child_ptr, gfx::Vector2dF(0, 100.f));
 
   // Scroll 25px to hide browser controls
   gfx::Vector2dF scroll_delta(0.f, 25.f);
