865ff795c0f5b16ae8e87d52a13e890c29a33197
tengs <tengs@chromium.org>
2017-07-20 17:55:55 -0700

    [EasyUnlock] Introduce EasyUnlockNotificationController
    This class is responsible for displaying and handling all EasyUnlock notifications.
    
    Originally, EasyUnlock notifications were shown by a component app, but this app is now deprecated and we are migrating EasyUnlock natively for v2.
    
    BUG=726791
    
    Review-Url: https://codereview.chromium.org/2968323002
    Cr-Commit-Position: refs/heads/master@{#488522}
    
    Former-commit-id: b8c14e087eb455c3e712753d44eb8a2c62cba9f8

---
+++ chrome/browser/signin/easy_unlock_notification_controller.cc
@@ -0,0 +1,46 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/signin/easy_unlock_notification_controller.h"
+
+#include "base/memory/ptr_util.h"
+#include "build/build_config.h"
+
+#if defined(OS_CHROMEOS)
+#include "chrome/browser/signin/easy_unlock_notification_controller_chromeos.h"
+#include "ui/message_center/message_center.h"
+#endif
+
+namespace {
+
+// Stub implementation of EasyUnlockNotificationController for non-ChromeOS
+// platforms.
+class EasyUnlockNotificationControllerStub
+    : public EasyUnlockNotificationController {
+ public:
+  EasyUnlockNotificationControllerStub() {}
+  ~EasyUnlockNotificationControllerStub() override {}
+
+  // EasyUnlockNotificationController:
+  void ShowChromebookAddedNotification() override {}
+  void ShowPairingChangeNotification() override {}
+  void ShowPairingChangeAppliedNotification(
+      const std::string& phone_name) override {}
+  void ShowPromotionNotification() override {}
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(EasyUnlockNotificationControllerStub);
+};
+
+}  // namespace
+
+std::unique_ptr<EasyUnlockNotificationController>
+EasyUnlockNotificationController::Create(Profile* profile) {
+#if defined(OS_CHROMEOS)
+  return base::MakeUnique<EasyUnlockNotificationControllerChromeOS>(
+      profile, message_center::MessageCenter::Get());
+#else
+  return base::MakeUnique<EasyUnlockNotificationControllerStub>();
+#endif
+}
