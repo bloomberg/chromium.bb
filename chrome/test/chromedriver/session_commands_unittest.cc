8330d09b541b07ef28e7808da231e334f3549007
Jonathon Kereliuk <kereliuk@chromium.org>
2017-07-21 15:41:39 +0000

    [Chromedriver] Add ability to merge capabilities to make chromedriver closer to being w3c compliant.
    This is built on yash's previous work.
    w3c standard: https://w3c.github.io/webdriver/webdriver-spec.html#processing-capabilities
    
    Change-Id: I9b2f49c0f2ebff59fd5b053c2d8b9b99a95aabe5
    
    BUG=chromedriver:1740
    
    Change-Id: I9b2f49c0f2ebff59fd5b053c2d8b9b99a95aabe5
    Reviewed-on: https://chromium-review.googlesource.com/562377
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Commit-Queue: Jonathon Kereliuk <kereliuk@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488663}
    Former-commit-id: b2900345e0ae38c962add4226241fe784be2d280

--- chrome/test/chromedriver/session_commands_unittest.cc
+++ chrome/test/chromedriver/session_commands_unittest.cc
@@ -21,6 +21,30 @@
 #include "chrome/test/chromedriver/session.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+TEST(SessionCommandsTest, MergeCapabilities) {
+  base::DictionaryValue primary;
+  primary.SetString("strawberry", "velociraptor");
+  primary.SetString("pear", "unicorn");
+
+  base::DictionaryValue secondary;
+  secondary.SetString("broccoli", "giraffe");
+  secondary.SetString("celery", "hippo");
+  secondary.SetString("eggplant", "elephant");
+
+  base::DictionaryValue merged;
+
+  // key collision should return false
+  ASSERT_FALSE(MergeCapabilities(&primary, &primary, &merged));
+  // non key collision should return true
+  ASSERT_TRUE(MergeCapabilities(&primary, &secondary, &merged));
+
+  merged.Clear();
+  MergeCapabilities(&primary, &secondary, &merged);
+  primary.MergeDictionary(&secondary);
+
+  ASSERT_EQ(primary, merged);
+}
+
 TEST(SessionCommandsTest, FileUpload) {
   Session session("id");
   base::DictionaryValue params;
