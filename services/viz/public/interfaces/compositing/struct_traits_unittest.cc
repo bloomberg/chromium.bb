301c8ab595d335fd04dad3790ccc968eb11e5c04
Alex Zhang <staraz@chromium.org>
2017-07-21 17:25:27 +0000

    ResourceSettings struct traits
    To plumb RendererSettings through between browser and viz, we need
    struct traits for it and its members. ResourceSettings is a member of
    RendererSettings.
    
    This CL adds struct traits for ResourceSettings and a unit test for
    the struct traits.
    
    Bug: 732825
    Change-Id: I03010130b3614cc316f6fc234dea4b5bfbf3fbf5
    Reviewed-on: https://chromium-review.googlesource.com/575209
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Xingyu Zhang <staraz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488689}
    Former-commit-id: de2037a5b5e0d6ffd72547f3438a3790e46ebb08

---
+++ services/viz/public/interfaces/compositing/struct_traits_unittest.cc
@@ -0,0 +1,41 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <utility>
+
+#include "base/message_loop/message_loop.h"
+#include "components/viz/common/resources/resource_settings.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
+#include "services/viz/public/interfaces/compositing/resource_settings_struct_traits.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace viz {
+
+namespace {
+
+using StructTraitsTest = testing::Test;
+
+TEST_F(StructTraitsTest, ResourceSettings) {
+  constexpr size_t kArbitrarySize = 32;
+  constexpr bool kArbitraryBool = true;
+  ResourceSettings input;
+  input.texture_id_allocation_chunk_size = kArbitrarySize;
+  input.use_gpu_memory_buffer_resources = kArbitraryBool;
+  input.buffer_to_texture_target_map =
+      DefaultBufferToTextureTargetMapForTesting();
+
+  ResourceSettings output;
+  mojom::ResourceSettings::Deserialize(
+      mojom::ResourceSettings::Serialize(&input), &output);
+  EXPECT_EQ(input.texture_id_allocation_chunk_size,
+            output.texture_id_allocation_chunk_size);
+  EXPECT_EQ(input.use_gpu_memory_buffer_resources,
+            output.use_gpu_memory_buffer_resources);
+  EXPECT_EQ(input.buffer_to_texture_target_map,
+            output.buffer_to_texture_target_map);
+}
+
+}  // namespace
+
+}  // namespace viz
