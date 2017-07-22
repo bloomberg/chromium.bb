0a312e0b4400b1db4b4ede865e3dea43f9ffcdf6
Michael Giuffrida <michaelpg@chromium.org>
2017-07-22 03:16:57 +0000

    Split MessageService::MessagePort into own file
    extensions::MessageService is rather large. To simplify removing its
    dependencies on Chrome, spin off MessagePort into its own file.
    
    Bug: 742641
    Change-Id: I4c7ff4fcaa15e34bec4f1292e1738c711f9a590c
    Reviewed-on: https://chromium-review.googlesource.com/578312
    Reviewed-by: Toni Barzic <tbarzic@chromium.org>
    Commit-Queue: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488848}
    Former-commit-id: 2e59602847f8073dc1f2a58d15697c470a710327

--- chrome/browser/extensions/api/messaging/native_message_port.h
+++ chrome/browser/extensions/api/messaging/native_message_port.h
@@ -5,22 +5,29 @@
 #ifndef CHROME_BROWSER_EXTENSIONS_API_MESSAGING_NATIVE_MESSAGE_PORT_H_
 #define CHROME_BROWSER_EXTENSIONS_API_MESSAGING_NATIVE_MESSAGE_PORT_H_
 
+#include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "base/threading/thread_checker.h"
-#include "chrome/browser/extensions/api/messaging/message_service.h"
+#include "chrome/browser/extensions/api/messaging/message_port.h"
 #include "extensions/common/api/messaging/port_id.h"
 
+namespace base {
+class SingleThreadTaskRunner;
+}
+
 namespace extensions {
+class NativeMessageHost;
 
 // A port that manages communication with a native application.
 // All methods must be called on the UI Thread of the browser process.
-class NativeMessagePort : public MessageService::MessagePort {
+class NativeMessagePort : public MessagePort {
  public:
-  NativeMessagePort(base::WeakPtr<MessageService> message_service,
+  NativeMessagePort(base::WeakPtr<ChannelDelegate> channel_delegate,
                     const PortId& port_id,
                     std::unique_ptr<NativeMessageHost> native_message_host);
   ~NativeMessagePort() override;
 
-  // MessageService::MessagePort implementation.
+  // MessagePort implementation.
   bool IsValidPort() override;
   void DispatchOnMessage(const Message& message) override;
 
@@ -30,7 +37,7 @@ class NativeMessagePort : public MessageService::MessagePort {
   void CloseChannel(const std::string& error_message);
 
   base::ThreadChecker thread_checker_;
-  base::WeakPtr<MessageService> weak_message_service_;
+  base::WeakPtr<ChannelDelegate> weak_channel_delegate_;
   scoped_refptr<base::SingleThreadTaskRunner> host_task_runner_;
   const PortId port_id_;
   std::unique_ptr<Core> core_;
