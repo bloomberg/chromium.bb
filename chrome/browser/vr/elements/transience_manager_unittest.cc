5e8539017f4b0ca5e3d902d6e12f69afc7a0c8f9
Ian Vollick <vollick@chromium.org>
2017-07-22 01:37:03 +0000

    Reland "[vr] Restore opacity when transient elements are shown again."
    This is a reland of bbe912de34ff7462b313c8a7486e64acb27e5fea [formerly c140538983d4575003bb31ee4f522da635e1a3c9]
    Original change's description:
    > [vr] Restore opacity when transient elements are shown again.
    > 
    > We'd previously set opacity to 0.0 when hiding elements in the
    > transience manager which makes it tricky to see them when they come
    > back. With this change, we inform the manager of the desired "opacity
    > when visible" and it will restore elements to that opacity when
    > shown again.
    > 
    > Bug: None
    > Change-Id: I1a941c3032c35adab5afed17a8afc60bd41230f5
    > Reviewed-on: https://chromium-review.googlesource.com/582069
    > Commit-Queue: Ian Vollick <vollick@chromium.org>
    > Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#488788}
    
    TBR=cjgrant@chromium.org
    
    Bug: 747633
    Change-Id: I340ed94f61f016243dc4495d687cab1d58ea2e67
    Reviewed-on: https://chromium-review.googlesource.com/582529
    Reviewed-by: Ian Vollick <vollick@chromium.org>
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488833}
    Former-commit-id: c2485c414664902ebdc0b297b65ef52e0a6cc080

--- chrome/browser/vr/elements/transience_manager_unittest.cc
+++ chrome/browser/vr/elements/transience_manager_unittest.cc
@@ -18,42 +18,57 @@ TEST(TransienceManager, Visibility) {
   base::ScopedMockTimeMessageLoopTaskRunner task_runner_;
 
   UiElement element;
+  float opacity_when_enabled = 0.8;
+  element.SetOpacity(0.0f);
 
-  TransienceManager transience(&element, base::TimeDelta::FromSeconds(10));
-  EXPECT_EQ(element.visible(), false);
+  TransienceManager transience(&element, opacity_when_enabled,
+                               base::TimeDelta::FromSeconds(10));
+  EXPECT_EQ(false, element.visible());
+  EXPECT_EQ(0.0f, element.opacity());
 
   transience.KickVisibilityIfEnabled();
-  EXPECT_EQ(element.visible(), false);
+  EXPECT_EQ(false, element.visible());
+  EXPECT_EQ(0.0f, element.opacity());
 
   // Enable and disable, making sure the element appears and disappears.
   transience.SetEnabled(true);
-  EXPECT_EQ(element.visible(), true);
+  EXPECT_EQ(true, element.visible());
+  EXPECT_EQ(opacity_when_enabled, element.opacity());
   transience.SetEnabled(false);
-  EXPECT_EQ(element.visible(), false);
+  EXPECT_EQ(false, element.visible());
+  EXPECT_EQ(0.0f, element.opacity());
 
   // Enable, and ensure that the element transiently disappears.
   transience.SetEnabled(true);
-  EXPECT_EQ(element.visible(), true);
+  EXPECT_EQ(true, element.visible());
+  EXPECT_EQ(opacity_when_enabled, element.opacity());
   task_runner_->FastForwardUntilNoTasksRemain();
-  EXPECT_EQ(element.visible(), false);
+  EXPECT_EQ(false, element.visible());
+  EXPECT_EQ(0.0f, element.opacity());
 
   // Kick visibility, and ensure that the element transiently disappears.
   transience.KickVisibilityIfEnabled();
-  EXPECT_EQ(element.visible(), true);
+  EXPECT_EQ(true, element.visible());
+  EXPECT_EQ(opacity_when_enabled, element.opacity());
   task_runner_->FastForwardUntilNoTasksRemain();
-  EXPECT_EQ(element.visible(), false);
+  EXPECT_EQ(false, element.visible());
+  EXPECT_EQ(0.0f, element.opacity());
 
   // Kick visibility, and ensure that ending visibility hides the element.
   transience.KickVisibilityIfEnabled();
-  EXPECT_EQ(element.visible(), true);
+  EXPECT_EQ(true, element.visible());
+  EXPECT_EQ(opacity_when_enabled, element.opacity());
   transience.EndVisibilityIfEnabled();
-  EXPECT_EQ(element.visible(), false);
+  EXPECT_EQ(false, element.visible());
+  EXPECT_EQ(0.0f, element.opacity());
 
   // Kick visibility, and ensure that disabling hides the element.
   transience.KickVisibilityIfEnabled();
-  EXPECT_EQ(element.visible(), true);
+  EXPECT_EQ(true, element.visible());
+  EXPECT_EQ(opacity_when_enabled, element.opacity());
   transience.SetEnabled(false);
-  EXPECT_EQ(element.visible(), false);
+  EXPECT_EQ(false, element.visible());
+  EXPECT_EQ(0.0f, element.opacity());
 }
 
 }  // namespace vr
--- chrome/browser/vr/elements/transient_url_bar.cc
+++ chrome/browser/vr/elements/transient_url_bar.cc
@@ -19,7 +19,7 @@ TransientUrlBar::TransientUrlBar(
     const base::Callback<void(UiUnsupportedMode)>& failure_callback)
     : TexturedElement(preferred_width),
       texture_(base::MakeUnique<UrlBarTexture>(true, failure_callback)),
-      transience_(this, timeout) {}
+      transience_(this, 1.0f, timeout) {}
 
 TransientUrlBar::~TransientUrlBar() = default;
 
