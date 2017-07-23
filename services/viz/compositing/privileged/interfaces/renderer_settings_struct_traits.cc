06bab95f82d90aeda8087ec7f72d87d8fd18da9a
Alex Zhang <staraz@chromium.org>
2017-07-23 14:29:48 +0000

    RendererSettings Struct Traits
    Add struct traits for RendererSettings and a unit test. The struct traits will
    be used to pass RendererSettings from browser to viz.
    
    Bug: 732825
    Change-Id: I9b1d4b12e479d26dd3794ef2c5c134e7a23ac529
    Reviewed-on: https://chromium-review.googlesource.com/581508
    Commit-Queue: Xingyu Zhang <staraz@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488886}
    Former-commit-id: 563abfa78490bec1a1dea4e9f93394d87dfd2147

---
+++ services/viz/compositing/privileged/interfaces/renderer_settings_struct_traits.cc
@@ -0,0 +1,32 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "services/viz/compositing/privileged/interfaces/renderer_settings_struct_traits.h"
+#include "services/viz/public/interfaces/compositing/resource_settings_struct_traits.h"
+
+namespace mojo {
+
+// static
+bool StructTraits<viz::mojom::RendererSettingsDataView, viz::RendererSettings>::
+    Read(viz::mojom::RendererSettingsDataView data,
+         viz::RendererSettings* out) {
+  out->allow_antialiasing = data.allow_antialiasing();
+  out->force_antialiasing = data.force_antialiasing();
+  out->force_blending_with_shaders = data.force_blending_with_shaders();
+  out->partial_swap_enabled = data.partial_swap_enabled();
+  out->finish_rendering_on_resize = data.finish_rendering_on_resize();
+  out->should_clear_root_render_pass = data.should_clear_root_render_pass();
+  out->release_overlay_resources_after_gpu_query =
+      data.release_overlay_resources_after_gpu_query();
+  out->gl_composited_overlay_candidate_quad_border =
+      data.gl_composited_overlay_candidate_quad_border();
+  out->show_overdraw_feedback = data.show_overdraw_feedback();
+  out->enable_color_correct_rendering = data.enable_color_correct_rendering();
+  out->highp_threshold_min = data.highp_threshold_min();
+  out->disallow_non_exact_resource_reuse =
+      data.disallow_non_exact_resource_reuse();
+  return data.ReadResourceSettings(&out->resource_settings);
+}
+
+}  // namespace mojo
