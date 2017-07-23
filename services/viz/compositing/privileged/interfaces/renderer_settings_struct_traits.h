06ac1d3be473fc52cabe46fd8917ba7c868e3ae2
Alex Zhang <staraz@chromium.org>
2017-07-23 14:29:48 +0000

    RendererSettings Struct Traits
    Add struct traits for RendererSettings and a unit test. The struct traits will
    be used to pass RendererSettings from browser to viz.
    
    Bug: 732825
    Change-Id: I9b1d4b12e479d26dd3794ef2c5c134e7a23ac529
    Reviewed-on: https://chromium-review.googlesource.com/581508
    Commit-Queue: Xingyu Zhang <staraz@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488886}
    Former-commit-id: 563abfa78490bec1a1dea4e9f93394d87dfd2147

---
+++ services/viz/compositing/privileged/interfaces/renderer_settings_struct_traits.h
@@ -0,0 +1,79 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef SERVICES_VIZ_COMPOSITING_PRIVILEGED_INTERFACES_RENDERER_SETTINGS_STRUCT_TRAITS_H_
+#define SERVICES_VIZ_COMPOSITING_PRIVILEGED_INTERFACES_RENDERER_SETTINGS_STRUCT_TRAITS_H_
+
+#include "components/viz/common/display/renderer_settings.h"
+#include "services/viz/compositing/privileged/interfaces/renderer_settings.mojom.h"
+#include "services/viz/compositing/privileged/interfaces/renderer_settings_struct_traits.h"
+
+namespace mojo {
+template <>
+struct StructTraits<viz::mojom::RendererSettingsDataView,
+                    viz::RendererSettings> {
+  static const viz::ResourceSettings& resource_settings(
+      const viz::RendererSettings& input) {
+    return input.resource_settings;
+  }
+
+  static bool allow_antialiasing(const viz::RendererSettings& input) {
+    return input.allow_antialiasing;
+  }
+
+  static bool force_antialiasing(const viz::RendererSettings& input) {
+    return input.force_antialiasing;
+  }
+
+  static bool force_blending_with_shaders(const viz::RendererSettings& input) {
+    return input.force_blending_with_shaders;
+  }
+
+  static bool partial_swap_enabled(const viz::RendererSettings& input) {
+    return input.partial_swap_enabled;
+  }
+
+  static bool finish_rendering_on_resize(const viz::RendererSettings& input) {
+    return input.finish_rendering_on_resize;
+  }
+
+  static bool should_clear_root_render_pass(
+      const viz::RendererSettings& input) {
+    return input.should_clear_root_render_pass;
+  }
+
+  static bool release_overlay_resources_after_gpu_query(
+      const viz::RendererSettings& input) {
+    return input.release_overlay_resources_after_gpu_query;
+  }
+
+  static bool gl_composited_overlay_candidate_quad_border(
+      const viz::RendererSettings& input) {
+    return input.gl_composited_overlay_candidate_quad_border;
+  }
+
+  static bool show_overdraw_feedback(const viz::RendererSettings& input) {
+    return input.show_overdraw_feedback;
+  }
+
+  static bool enable_color_correct_rendering(
+      const viz::RendererSettings& input) {
+    return input.enable_color_correct_rendering;
+  }
+
+  static int highp_threshold_min(const viz::RendererSettings& input) {
+    return input.highp_threshold_min;
+  }
+
+  static bool disallow_non_exact_resource_reuse(
+      const viz::RendererSettings& input) {
+    return input.disallow_non_exact_resource_reuse;
+  }
+
+  static bool Read(viz::mojom::RendererSettingsDataView data,
+                   viz::RendererSettings* out);
+};
+
+}  // namespace mojo
+
+#endif  // SERVICES_VIZ_COMPOSITING_PRIVILEGED_INTERFACES_RENDERER_SETTINGS_STRUCT_TRAITS_H_
---
+++ services/viz/compositing/privileged/interfaces/struct_traits_unittest.cc
@@ -0,0 +1,79 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <utility>
+
+#include "base/message_loop/message_loop.h"
+#include "components/viz/common/display/renderer_settings.h"
+#include "components/viz/common/resources/resource_settings.h"
+#include "mojo/public/cpp/bindings/binding_set.h"
+#include "services/viz/compositing/privileged/interfaces/renderer_settings_struct_traits.h"
+#include "services/viz/public/interfaces/compositing/resource_settings_struct_traits.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace viz {
+
+namespace {
+
+using StructTraitsTest = testing::Test;
+
+constexpr size_t kArbitrarySize = 32;
+constexpr bool kArbitraryBool = true;
+
+TEST_F(StructTraitsTest, RendererSettings) {
+  ResourceSettings arbitrary_resource_settings;
+  arbitrary_resource_settings.texture_id_allocation_chunk_size = kArbitrarySize;
+  arbitrary_resource_settings.use_gpu_memory_buffer_resources = kArbitraryBool;
+  arbitrary_resource_settings.buffer_to_texture_target_map =
+      DefaultBufferToTextureTargetMapForTesting();
+  RendererSettings input;
+
+  // Set |input| to non-default values.
+  input.resource_settings = arbitrary_resource_settings;
+  input.allow_antialiasing = false;
+  input.force_antialiasing = true;
+  input.force_blending_with_shaders = true;
+  input.partial_swap_enabled = true;
+  input.finish_rendering_on_resize = true;
+  input.should_clear_root_render_pass = false;
+  input.release_overlay_resources_after_gpu_query = true;
+  input.gl_composited_overlay_candidate_quad_border = true;
+  input.show_overdraw_feedback = true;
+  input.enable_color_correct_rendering = true;
+  input.highp_threshold_min = -1;
+  input.disallow_non_exact_resource_reuse = true;
+
+  RendererSettings output;
+  mojom::RendererSettings::Deserialize(
+      mojom::RendererSettings::Serialize(&input), &output);
+  EXPECT_EQ(input.resource_settings.texture_id_allocation_chunk_size,
+            output.resource_settings.texture_id_allocation_chunk_size);
+  EXPECT_EQ(input.resource_settings.use_gpu_memory_buffer_resources,
+            output.resource_settings.use_gpu_memory_buffer_resources);
+  EXPECT_EQ(input.resource_settings.buffer_to_texture_target_map,
+            output.resource_settings.buffer_to_texture_target_map);
+  EXPECT_EQ(input.allow_antialiasing, output.allow_antialiasing);
+  EXPECT_EQ(input.force_antialiasing, output.force_antialiasing);
+  EXPECT_EQ(input.force_blending_with_shaders,
+            output.force_blending_with_shaders);
+  EXPECT_EQ(input.partial_swap_enabled, output.partial_swap_enabled);
+  EXPECT_EQ(input.finish_rendering_on_resize,
+            output.finish_rendering_on_resize);
+  EXPECT_EQ(input.should_clear_root_render_pass,
+            output.should_clear_root_render_pass);
+  EXPECT_EQ(input.release_overlay_resources_after_gpu_query,
+            output.release_overlay_resources_after_gpu_query);
+  EXPECT_EQ(input.gl_composited_overlay_candidate_quad_border,
+            output.gl_composited_overlay_candidate_quad_border);
+  EXPECT_EQ(input.show_overdraw_feedback, output.show_overdraw_feedback);
+  EXPECT_EQ(input.enable_color_correct_rendering,
+            output.enable_color_correct_rendering);
+  EXPECT_EQ(input.highp_threshold_min, output.highp_threshold_min);
+  EXPECT_EQ(input.disallow_non_exact_resource_reuse,
+            output.disallow_non_exact_resource_reuse);
+}
+
+}  // namespace
+
+}  // namespace viz
