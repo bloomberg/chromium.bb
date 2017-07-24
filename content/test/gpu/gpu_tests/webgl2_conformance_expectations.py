b8384eb8ff973a12edd873bd20ed6865a460addb
Kenneth Russell <kbr@chromium.org>
2017-07-24 21:51:57 +0000

    Skip texture-switch-performance test everywhere.
    It needs to be rewritten to measure its expected performance. It's
    currently too flaky.
    
    BUG=735483
    TBR=zmo@chromium.org
    NOTRY=true
    
    Change-Id: I23b5e7eca97a29700caeb9fe0dcffc6d664f4766
    Reviewed-on: https://chromium-review.googlesource.com/583893
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489088}
    Former-commit-id: 65ef72c3a4b993acb333666b007ceea095b98aa4

--- content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
+++ content/test/gpu/gpu_tests/webgl2_conformance_expectations.py
@@ -44,6 +44,13 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
     self.Flaky('conformance2/query/occlusion-query.html', bug=603168)
     self.Fail('conformance2/glsl3/tricky-loop-conditions.html', bug=483282)
 
+    # This test needs to be rewritten to measure its expected
+    # performance; it's currently too flaky even on release bots.
+    self.Skip('conformance/rendering/texture-switch-performance.html',
+        bug=735483)
+    self.Skip('conformance2/rendering/texture-switch-performance.html',
+        bug=735483)
+
     self.Fail('conformance2/rendering/depth-stencil-feedback-loop.html',
         bug=660844) # WebGL 2.0.1
     self.Fail('conformance2/rendering/rendering-sampling-feedback-loop.html',
@@ -476,10 +483,12 @@ class WebGL2ConformanceExpectations(WebGLConformanceExpectations):
         ['mac', ('nvidia', 0xfe9)], bug=654187)
 
     # Mac AMD
-    self.Fail('conformance/rendering/texture-switch-performance.html',
-        ['mac', 'amd'], bug=735483)
-    self.Fail('conformance2/rendering/texture-switch-performance.html',
-        ['mac', 'amd'], bug=735483)
+    # TODO(kbr): uncomment the following two exepectations after test
+    # has been made more robust.
+    # self.Fail('conformance/rendering/texture-switch-performance.html',
+    #     ['mac', 'amd'], bug=735483)
+    # self.Fail('conformance2/rendering/texture-switch-performance.html',
+    #     ['mac', 'amd'], bug=735483)
     self.Fail('deqp/functional/gles3/transformfeedback/' +
         'array_interleaved_lines.html',
         ['mac', 'amd'], bug=483282)
