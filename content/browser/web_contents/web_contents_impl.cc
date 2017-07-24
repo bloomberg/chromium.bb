87550a9d88809946de433dc318310eefd9e190e4
Sebastien Marchand <sebmarchand@chromium.org>
2017-07-24 23:50:34 +0000

    Disable the FindReply beeps during a tab's shutdown.
    Fix the system beep that happen when closing a tab in some situations 
    (see this repro: https://bugs.chromium.org/p/chromium/issues/detail?id=682299#c56)
    
    This is probably not the best way to fix this bug but it's probably a
    sufficient workaround until we find a better fix.
    
    Bug: 682299
    Change-Id: I3a6c08cb01d3c2f6de1a8a2fee53a43eefde1e4d
    Reviewed-on: https://chromium-review.googlesource.com/581571
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Paul Meyer <paulmeyer@chromium.org>
    Commit-Queue: Sébastien Marchand <sebmarchand@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489146}
    Former-commit-id: cf97ed5bc86f3ec5ede7392671b7cc2e6d9a85dd

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -5643,7 +5643,8 @@ void WebContentsImpl::NotifyFindReply(int request_id,
                                       const gfx::Rect& selection_rect,
                                       int active_match_ordinal,
                                       bool final_update) {
-  if (delegate_ && !is_being_destroyed_) {
+  if (delegate_ && !is_being_destroyed_ &&
+      !GetRenderProcessHost()->FastShutdownStarted()) {
     delegate_->FindReply(this, request_id, number_of_matches, selection_rect,
                          active_match_ordinal, final_update);
   }
