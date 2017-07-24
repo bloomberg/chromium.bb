4a9413434bf284afa540775c91bbab6472a0ce6f
proberge <proberge@chromium.org>
2017-07-24 16:17:24 +0000

    Chrome Cleanup WebUI: polishing fixes from implementation review
    Resize spinner, standardize color use, add learn more to error state,
    change reboot required icon
    
    Also makes chrome://settings/help use the right colors.
    
    Bug: 747124
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ifae6b61cc4ffa668dbe095333f3590a2d97277a4
    Reviewed-on: https://chromium-review.googlesource.com/580360
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Commit-Queue: proberge <proberge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#488992}
    Former-commit-id: 435ed1848ee5bf092fcbfd818b9f49e98c1485a1

--- chrome/browser/resources/settings/chrome_cleanup_page/chrome_cleanup_page.js
+++ chrome/browser/resources/settings/chrome_cleanup_page/chrome_cleanup_page.js
@@ -65,6 +65,16 @@ Polymer({
       value: false,
     },
 
+    /**
+     * Learn more should only be visible for the infected, cleaning and error
+     * states.
+     * @private
+     */
+    showLearnMore_: {
+      type: Boolean,
+      value: false,
+    },
+
     /** @private */
     showLogsPermission_: {
       type: Boolean,
@@ -118,12 +128,6 @@ Polymer({
 
   /** @private {?function()} */
   doAction_: null,
-
-  /**
-   * If true, this settings page is waiting for cleanup results.
-   * @private {boolean}
-   */
-  waitingForCleanupResults_: false,
 
   /** @override */
   attached: function() {
@@ -196,6 +200,7 @@ Polymer({
       this.enableActionButton_(
           this.i18n('chromeCleanupDoneButtonLabel'), this.dismiss_.bind(this));
       this.setIconDone_();
+      this.showLearnMore_ = false;
     } else if (idleReason == settings.ChromeCleanupIdleReason.INITIAL) {
       this.dismiss_();
     } else {
@@ -205,11 +210,11 @@ Polymer({
       this.enableActionButton_(
           this.i18n('chromeCleanupDoneButtonLabel'), this.dismiss_.bind(this));
       this.setIconWarning_();
+      this.showLearnMore_ = true;
     }
 
     this.isRemoving_ = false;
     this.disableDetails_();
-    this.waitingForCleanupResults_ = false;
   },
 
   /**
@@ -249,7 +254,6 @@ Polymer({
    * @private
    */
   onCleaning_: function(files) {
-    this.waitingForCleanupResults_ = true;
     this.title_ = this.i18n('chromeCleanupTitleRemoving');
     this.isRemoving_ = true;
     this.resetIcon_();
@@ -267,7 +271,8 @@ Polymer({
   onRebootRequired_: function() {
     this.title_ = this.i18n('chromeCleanupTitleRestart');
     this.isRemoving_ = false;
-    this.setIconWarning_();
+    this.showLearnMore_ = false;
+    this.setIconDone_();
     this.enableActionButton_(
         this.i18n('chromeCleanupRestartButtonLabel'),
         this.restartComputer_.bind(this));
@@ -354,6 +359,7 @@ Polymer({
   enableDetails_: function(files) {
     this.showDetails_ = true;
     this.showLogsPermission_ = true;
+    this.showLearnMore_ = true;
     // Note: doesn't change the state of this.showFilesToRemove_.
     this.filesToRemove_ = files;
   },
@@ -385,8 +391,7 @@ Polymer({
   },
 
   /**
-   * Sets the card's icon as a warning (in case of computer restart required
-   * or failure).
+   * Sets the card's icon as a warning (in case of failure).
    * @private
    */
   setIconWarning_: function() {
@@ -395,7 +400,7 @@ Polymer({
   },
 
   /**
-   * Sets the card's icon as a completed indication.
+   * Sets the card's icon as a completed or reboot required indication.
    * @private
    */
   setIconDone_: function() {
