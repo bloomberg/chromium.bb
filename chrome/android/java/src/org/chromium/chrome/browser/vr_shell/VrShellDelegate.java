38fe2fc7f7955ab0354f23ab2efe65c103a6ac93
Biao She <bshe@chromium.org>
2017-07-25 22:22:06 +0000

    Fix crash when back button is pressed at Doff
    Bug: 748272
    Change-Id: I25d416132dfc9dccca2205bbe485291933eaeb2f
    Reviewed-on: https://chromium-review.googlesource.com/585590
    Commit-Queue: Biao She <bshe@chromium.org>
    Reviewed-by: Yash Malik <ymalik@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489462}
    Former-commit-id: f71b90f3e2816bd84bd35bf02d918995c99ddad2

--- chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
+++ chrome/android/java/src/org/chromium/chrome/browser/vr_shell/VrShellDelegate.java
@@ -1286,10 +1286,12 @@ public class VrShellDelegate
     private void cancelPendingVrEntry() {
         // Ensure we can't asynchronously enter VR after trying to exit it.
         mEnterVrHandler.removeCallbacksAndMessages(null);
-        mDonSucceeded = false;
         removeBlackOverlayView();
-        mVrClassesWrapper.setVrModeEnabled(mActivity, false);
-        restoreWindowMode();
+        mDonSucceeded = false;
+        if (!mShowingDaydreamDoff) {
+            mVrClassesWrapper.setVrModeEnabled(mActivity, false);
+            restoreWindowMode();
+        }
     }
 
     /**
