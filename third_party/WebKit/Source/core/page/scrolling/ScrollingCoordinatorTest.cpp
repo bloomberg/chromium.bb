87655e7cdd717836f228e39915d1d8b80cf7cdc1
sunxd <sunxd@chromium.org>
2017-07-25 23:17:07 +0000

    Revert "cc: Enable composited border-radius scrolling."
    This reverts commit 6fce9680bff1f045603b55a5a9c0d837d002668a [formerly 06adb5e74eb341b5a1fa68fed4e489f6f62ec5e8].
    
    Reason for revert: Composited border-radius scrolling triggers two regression bugs: 1) crbug.com/740720, it results in high memory usage probably due to mask tiling not saving enough memory; 2) crbug.com/745114, compositor's scroll snapping can sometimes result in blurry text.
    
    Original change's description:
    > cc: Enable composited border-radius scrolling.
    > 
    > Enable composited border-radius scrolling as mask tiling is implemented.
    > We'll revert this CL if anything breaks.
    > 
    > Bug: 567293, 567296
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > Change-Id: I70bea4c542e00d244a597d1a36857dd120fc96cd
    > Reviewed-on: https://chromium-review.googlesource.com/543397
    > Commit-Queue: Xianda Sun <sunxd@chromium.org>
    > Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    > Reviewed-by: Robert Flack <flackr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#485040}
    
    TBR=flackr@chromium.org,trchen@chromium.org,chrishtr@chromium.org,enne@chromium.org,sunxd@chromium.org,schenney@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: 567293, 567296, 740720, 745114
    Change-Id: Ia185a9ce4310aeaff0175f0e9f0a9080dccf22e5
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Reviewed-on: https://chromium-review.googlesource.com/585227
    Commit-Queue: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Reviewed-by: Xianda Sun <sunxd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489477}
    Former-commit-id: 2397e783b2b5a59a174d25d26f66d467a8b0a88f

--- third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinatorTest.cpp
+++ third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinatorTest.cpp
@@ -1122,6 +1122,11 @@ TEST_P(NonCompositedMainThreadScrollingReasonTest, BackgroundNotOpaqueTest) {
       MainThreadScrollingReason::kBackgroundNotOpaqueInRectAndLCDText);
 }
 
+TEST_P(NonCompositedMainThreadScrollingReasonTest, BorderRadiusTest) {
+  TestNonCompositedReasons("border-radius",
+                           MainThreadScrollingReason::kHasBorderRadius);
+}
+
 TEST_P(NonCompositedMainThreadScrollingReasonTest, ClipTest) {
   TestNonCompositedReasons("clip",
                            MainThreadScrollingReason::kHasClipRelatedProperty);
@@ -1177,8 +1182,9 @@ TEST_P(NonCompositedMainThreadScrollingReasonTest, ClipPathTest) {
 }
 
 TEST_P(NonCompositedMainThreadScrollingReasonTest, LCDTextEnabledTest) {
-  TestNonCompositedReasons("transparent",
-                           MainThreadScrollingReason::kHasOpacityAndLCDText);
+  TestNonCompositedReasons("transparent border-radius",
+                           MainThreadScrollingReason::kHasOpacityAndLCDText |
+                               MainThreadScrollingReason::kHasBorderRadius);
 }
 
 TEST_P(NonCompositedMainThreadScrollingReasonTest, BoxShadowTest) {
@@ -1243,7 +1249,8 @@ TEST_P(NonCompositedMainThreadScrollingReasonTest,
       ToLayoutBoxModelObject(container2->GetLayoutObject())
           ->GetScrollableArea();
   ASSERT_TRUE(scrollable_area2);
-  ASSERT_TRUE(scrollable_area2->UsesCompositedScrolling());
+  EXPECT_TRUE(scrollable_area2->GetNonCompositedMainThreadScrollingReasons() &
+              MainThreadScrollingReason::kHasBorderRadius);
 }
 
 }  // namespace blink
