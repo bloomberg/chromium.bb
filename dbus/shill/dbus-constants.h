b9fd1dfb3857c51887d4dd8dfa1781f907a8f5ef
Eric Caruso <ejcaruso@chromium.org>
2017-07-25 15:36:24 -0700

    shill: add new MAC address randomization constants
    We want to expose MAC address randomization support as a
    separate device property so that we don't have to go digging
    in wiphy flags and NL80211 packets to see if it's supported
    in tests.
    
    BUG=chromium:579598
    TEST=emerge shill
    
    Change-Id: Iad2755f0b4ed952aad119e69ef06704e01cf5882
    Reviewed-on: https://chromium-review.googlesource.com/585490
    Commit-Ready: Eric Caruso <ejcaruso@chromium.org>
    Tested-by: Eric Caruso <ejcaruso@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Ben Chan <benchan@chromium.org>

--- dbus/shill/dbus-constants.h
+++ dbus/shill/dbus-constants.h
@@ -178,7 +178,12 @@ const char kForceWakeToScanTimerProperty[] = "ForceWakeToScanTimer";
 const char kInterfaceProperty[] = "Interface";
 const char kSelectedServiceProperty[] = "SelectedService";
 const char kIPConfigsProperty[] = "IPConfigs";
+// TODO(crbug.com/749161): remove this after switching
 const char kMACAddressRandomizationProperty[] = "MACAddressRandomization";
+const char kMACAddressRandomizationSupportedProperty[] =
+    "MACAddressRandomizationSupported";
+const char kMACAddressRandomizationEnabledProperty[] =
+    "MACAddressRandomizationEnabled";
 
 // Flimflam Cellular Device property names.
 const char kCarrierProperty[] = "Cellular.Carrier";
