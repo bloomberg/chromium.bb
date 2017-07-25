46c1fdf32dca099f33d221e74b9069eb323fd43c
David Trainor <dtrainor@chromium.org>
2017-07-25 21:44:36 +0000

    Migrate ThumbnailProvider to TaskScheduler
    Change how ThumbnailProvider works to remove BrowserThread::FILE
    references.  There is a slightly larger refactor surrounding this which
    changes the thumbnail flow from:
    
    Request (UI) -> Load (File) -> Decode Helper (File) -> ...
    ... -> Decode Magic (?) -> Decode Finished (File) -> Scale (File) -> ...
    ... -> Respond (UI)
    
    to:
    
    Request (UI) -> Load (Pool) -> Decode Helper (UI) -> ...
    ... -> Decode Magic (?) -> Decode Finished (UI) -> Scale (Pool) -> ...
    ... -> Respond (UI)
    
    This makes a bit more sense because it uses the trait pool as a pool and
    not as a long running task_runner that can have tasks and replies posted
    to it.  It also cleans up the logic and flow a bit.
    
    BUG=689520
    
    Change-Id: I18f2600100cddec9d910933d80ef74c5a58502a0
    Reviewed-on: https://chromium-review.googlesource.com/581674
    Commit-Queue: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489448}
    Former-commit-id: 846b42d1db6f42a57766e35d92595b5a4b96e62b

--- chrome/browser/android/download/ui/thumbnail_provider.cc
+++ chrome/browser/android/download/ui/thumbnail_provider.cc
@@ -7,7 +7,10 @@
 #include "base/android/jni_string.h"
 #include "base/files/file_path.h"
 #include "base/files/file_util.h"
+#include "base/memory/ptr_util.h"
 #include "base/memory/weak_ptr.h"
+#include "base/task_scheduler/post_task.h"
+#include "base/threading/thread_restrictions.h"
 #include "chrome/browser/image_decoder.h"
 #include "content/public/browser/browser_thread.h"
 #include "jni/ThumbnailProviderImpl_jni.h"
@@ -25,71 +28,92 @@ namespace {
 // Ignore image files that are too large to avoid long delays.
 const int64_t kMaxImageSize = 10 * 1024 * 1024;  // 10 MB
 
+std::string LoadImageData(const base::FilePath& path) {
+  base::ThreadRestrictions::AssertIOAllowed();
+
+  // Confirm that the file's size is within our threshold.
+  int64_t file_size;
+  if (!base::GetFileSize(path, &file_size) || file_size > kMaxImageSize) {
+    LOG(ERROR) << "Unexpected file size: " << path.MaybeAsASCII() << ", "
+               << file_size;
+    return std::string();
+  }
+
+  std::string data;
+  bool success = base::ReadFileToString(path, &data);
+
+  // Make sure the file isn't empty.
+  if (!success || data.empty()) {
+    LOG(ERROR) << "Failed to read file: " << path.MaybeAsASCII();
+    return std::string();
+  }
+
+  return data;
+}
+
+SkBitmap ScaleDownBitmap(int icon_size, const SkBitmap& decoded_image) {
+  DCHECK(!content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+
+  if (decoded_image.drawsNothing())
+    return decoded_image;
+
+  // Shrink the image down so that its smallest dimension is equal to or
+  // smaller than the requested size.
+  int min_dimension = std::min(decoded_image.width(), decoded_image.height());
+
+  if (min_dimension <= icon_size)
+    return decoded_image;
+
+  uint64_t width = static_cast<uint64_t>(decoded_image.width());
+  uint64_t height = static_cast<uint64_t>(decoded_image.height());
+  return skia::ImageOperations::Resize(
+      decoded_image, skia::ImageOperations::RESIZE_BEST,
+      width * icon_size / min_dimension, height * icon_size / min_dimension);
+}
+
 class ImageThumbnailRequest : public ImageDecoder::ImageRequest {
  public:
-  static void Create(int icon_size,
-                     const std::string& file_path,
-                     base::WeakPtr<ThumbnailProvider> weak_provider) {
-    DCHECK_CURRENTLY_ON(content::BrowserThread::FILE);
-    ImageThumbnailRequest* request =
-        new ImageThumbnailRequest(icon_size, file_path, weak_provider);
-    request->Start();
+  ImageThumbnailRequest(int icon_size,
+                        base::OnceCallback<void(const SkBitmap&)> callback)
+      : icon_size_(icon_size),
+        callback_(std::move(callback)),
+        weak_ptr_factory_(this) {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   }
 
- protected:
-  void OnImageDecoded(const SkBitmap& decoded_image) override {
-    DCHECK_CURRENTLY_ON(content::BrowserThread::FILE);
-
-    SkBitmap thumbnail = decoded_image;
-    if (!decoded_image.drawsNothing()) {
-      // Shrink the image down so that its smallest dimension is equal to or
-      // smaller than the requested size.
-      int min_dimension =
-          std::min(decoded_image.width(), decoded_image.height());
-
-      if (min_dimension > icon_size_) {
-        uint64_t width = static_cast<uint64_t>(decoded_image.width());
-        uint64_t height = static_cast<uint64_t>(decoded_image.height());
-        thumbnail = skia::ImageOperations::Resize(
-            decoded_image, skia::ImageOperations::RESIZE_BEST,
-            width * icon_size_ / min_dimension,
-            height * icon_size_ / min_dimension);
-      }
-    }
-
-    FinishRequest(thumbnail);
+  ~ImageThumbnailRequest() override {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   }
 
-  void OnDecodeImageFailed() override {
-    LOG(ERROR) << "Failed to decode image: " << file_path_;
-    FinishRequest(SkBitmap());
+  void Start(const base::FilePath& path) {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+    base::PostTaskWithTraitsAndReplyWithResult(
+        FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_BLOCKING},
+        base::BindOnce(&LoadImageData, path),
+        base::BindOnce(&ImageThumbnailRequest::OnLoadComplete,
+                       weak_ptr_factory_.GetWeakPtr()));
   }
 
  private:
-  ImageThumbnailRequest(int icon_size,
-                        const std::string& file_path,
-                        base::WeakPtr<ThumbnailProvider> weak_provider)
-      : icon_size_(icon_size),
-        file_path_(file_path),
-        weak_provider_(weak_provider) {}
+  // ImageDecoder::ImageRequest implementation.
+  void OnImageDecoded(const SkBitmap& decoded_image) override {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+    base::PostTaskWithTraitsAndReplyWithResult(
+        FROM_HERE, {base::MayBlock(), base::TaskPriority::USER_BLOCKING},
+        base::BindOnce(&ScaleDownBitmap, icon_size_, decoded_image),
+        base::BindOnce(&ImageThumbnailRequest::FinishRequest,
+                       weak_ptr_factory_.GetWeakPtr()));
+  }
 
-  void Start() {
-    DCHECK_CURRENTLY_ON(content::BrowserThread::FILE);
+  void OnDecodeImageFailed() override {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+    LOG(ERROR) << "Failed to decode image.";
+    FinishRequest(SkBitmap());
+  }
 
-    // Confirm that the file's size is within our threshold.
-    int64_t file_size;
-    base::FilePath path = base::FilePath::FromUTF8Unsafe(file_path_);
-    if (!base::GetFileSize(path, &file_size) || file_size > kMaxImageSize) {
-      LOG(ERROR) << "Unexpected file size: " << file_path_ << ", " << file_size;
-      FinishRequest(SkBitmap());
-      return;
-    }
-
-    // Make sure the file isn't empty.
-    std::string data;
-    bool success = base::ReadFileToString(path, &data);
-    if (!success || data.empty()) {
-      LOG(ERROR) << "Failed to read file: " << file_path_;
+  void OnLoadComplete(const std::string& data) {
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+    if (data.empty()) {
       FinishRequest(SkBitmap());
       return;
     }
@@ -98,20 +122,18 @@ class ImageThumbnailRequest : public ImageDecoder::ImageRequest {
   }
 
   void FinishRequest(const SkBitmap& thumbnail) {
-    DCHECK_CURRENTLY_ON(content::BrowserThread::FILE);
-
+    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
     content::BrowserThread::PostTask(
         content::BrowserThread::UI, FROM_HERE,
-        base::Bind(&ThumbnailProvider::OnThumbnailRetrieved,
-                   weak_provider_, file_path_, thumbnail));
-    task_runner()->DeleteSoon(FROM_HERE, this);
+        base::BindOnce(std::move(callback_), thumbnail));
+    delete this;
   }
 
   const int icon_size_;
-  std::string file_path_;
-  base::WeakPtr<ThumbnailProvider> weak_provider_;
+  base::OnceCallback<void(const SkBitmap&)> callback_;
+  base::WeakPtrFactory<ImageThumbnailRequest> weak_ptr_factory_;
 
-  DISALLOW_IMPLICIT_CONSTRUCTORS(ImageThumbnailRequest);
+  DISALLOW_COPY_AND_ASSIGN(ImageThumbnailRequest);
 };
 
 }  // namespace
@@ -151,13 +173,17 @@ void ThumbnailProvider::RetrieveThumbnail(JNIEnv* env,
                                           jint icon_size) {
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
 
-  // The ImageThumbnailRequest deletes itself on completion.  Don't track it
-  // because we don't (currently) cancel it.
-  std::string path = base::android::ConvertJavaStringToUTF8(env, jfile_path);
-  content::BrowserThread::PostTask(
-      content::BrowserThread::FILE, FROM_HERE,
-      base::Bind(&ImageThumbnailRequest::Create, icon_size, path,
-                 weak_factory_.GetWeakPtr()));
+  std::string file_path =
+      base::android::ConvertJavaStringToUTF8(env, jfile_path);
+
+  auto request = base::MakeUnique<ImageThumbnailRequest>(
+      icon_size, base::BindOnce(&ThumbnailProvider::OnThumbnailRetrieved,
+                                weak_factory_.GetWeakPtr(), file_path));
+  request->Start(base::FilePath::FromUTF8Unsafe(file_path));
+
+  // Dropping ownership of |request| here because it will clean itself up once
+  // the started request finishes.
+  request.release();
 }
 
 // static
