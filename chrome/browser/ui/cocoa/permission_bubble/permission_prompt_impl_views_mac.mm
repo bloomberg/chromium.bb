77b3aaf0a1e294f0937bb9fdc12c91c06ce120da
tapted <tapted@chromium.org>
2017-07-25 00:07:28 -0700

    Use MacViews for Permissions bubbles on Mac
    The Cocoa permissions bubble code uses a button style that isn't used
    anywhere in top chrome. (It once matched chrome://settings buttons, but
    that's changing). The MacViews dialogs already use a button style that
    matches the downloads bar, and have slightly improved layout.
    
    Views becomes the default behaviour. To use Cocoa, pass
    
    --enable-features=CocoaPermissionBubbles
    
    BUG=740827
    TEST=Go to https://permissions.site .
    0. Click padlock, ensure everything is set to 'Ask'. Reload.
    1. Click buttons on the site to trigger prompts: should work the same as previously. Repeat Step 0 (every time).
    2. Enter fullscreen using the small green round fullscreen button on the window frame. Repeat Step 1.
    3. Enter fullscreen using the big white square button on the site. Repeat Step 1.
    4. Click the "Open https://permissions.site button." at https://jsfiddle.net/tapted/c9dpjf0g/ . Repeat Steps 1., 2. and 3 using the popup window.
    
    Review-Url: https://codereview.chromium.org/2975853002
    Cr-Commit-Position: refs/heads/master@{#489245}
    
    Former-commit-id: e75db8e47e1b5e84a2f05afbd33f254fe2879f1c

--- chrome/browser/ui/cocoa/permission_bubble/permission_prompt_impl_views_mac.mm
+++ chrome/browser/ui/cocoa/permission_bubble/permission_prompt_impl_views_mac.mm
@@ -2,20 +2,31 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
 #include "chrome/browser/ui/browser_finder.h"
 #import "chrome/browser/ui/cocoa/permission_bubble/permission_bubble_cocoa.h"
 #include "chrome/browser/ui/views/permission_bubble/permission_prompt_impl.h"
 #include "ui/base/material_design/material_design_controller.h"
 
+namespace {
+
+constexpr base::Feature kCocoaPermissionBubbles = {
+    "CocoaPermissionBubbles", base::FEATURE_DISABLED_BY_DEFAULT};
+
+bool UseViewsBubbles() {
+  return ui::MaterialDesignController::IsSecondaryUiMaterial() ||
+         !base::FeatureList::IsEnabled(kCocoaPermissionBubbles);
+}
+
+}  // namespace
+
 // static
 std::unique_ptr<PermissionPrompt> PermissionPrompt::Create(
     content::WebContents* web_contents,
     Delegate* delegate) {
-  if (ui::MaterialDesignController::IsSecondaryUiMaterial()) {
-    return base::WrapUnique(new PermissionPromptImpl(
-        chrome::FindBrowserWithWebContents(web_contents), delegate));
-  }
-  return base::MakeUnique<PermissionBubbleCocoa>(
-      chrome::FindBrowserWithWebContents(web_contents), delegate);
+  Browser* browser = chrome::FindBrowserWithWebContents(web_contents);
+  if (UseViewsBubbles())
+    return base::MakeUnique<PermissionPromptImpl>(browser, delegate);
+  return base::MakeUnique<PermissionBubbleCocoa>(browser, delegate);
 }
