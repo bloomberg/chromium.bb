301c3897c8277e8fbe5daede15e5e58ece1ffc04
Jia <jiameng@chromium.org>
2017-07-25 04:28:34 +0000

    Implement API for scroll-boundary-behavior.
    This cl also removes Consume2Values from CSSPropertyParser as it's no
    longer used.
    
    Bug: 668012
    Change-Id: Idabf30e1f196a79c16cdb9f67e76ccb569791196
    Reviewed-on: https://chromium-review.googlesource.com/582261
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489230}
    Former-commit-id: de69f63156bddb2e3807e6aeb4ab1a2be1622cfb

--- third_party/WebKit/Source/core/css/parser/CSSPropertyParserHelpers.h
+++ third_party/WebKit/Source/core/css/parser/CSSPropertyParserHelpers.h
@@ -131,26 +131,6 @@ const CSSValue* ParseLonghandViaAPI(CSSPropertyID unresolved_property,
                                     CSSParserTokenRange&,
                                     bool& needs_legacy_parsing);
 
-// ConsumeShorthandVia2LonghandAPIs is based on CSSPropertyParsers'
-// Consume2Values.
-// They both delegate parsing of a shorthand property to its respective longhand
-// components. The difference is the functions in this Helpers file expect
-// component longhands to have API implementations already because each
-// shorthand will call its component longhand APIs' parseShorthand method.
-// Consume2Values will be removed soon, when shorthand properties are ribbonised
-// (i.e. have their own APIs). Until then, there is a slight code duplication
-// between the two versions for the following reasons:
-// 1. An alternative to code duplicate is to have the old Consume*
-//    (e.g. Consume2Values) call the new Consume*
-//    (e.g. ConsumeShorthandVia2LonghandAPIs). However, the
-//    new Consume* expects ALL component longhands to have APIs and will parse
-///   all longhands via their APIs. In order to parse shorthands, where some
-//    component longhands do not have APIs, the new Consume* will need to return
-//    to the old Consume* which longhands have no APIs and thus are not parsed.
-//    The old Consume* will then have to parse these longhands separately.
-//    Hence there's added code complexity with little code reduction.
-// 2. All longhand & shorthand properties will have APIs soon, hence such code
-//    duplication is temporary only.
 bool ConsumeShorthandVia2LonghandAPIs(const StylePropertyShorthand&,
                                       bool important,
                                       const CSSParserContext&,
---
+++ third_party/WebKit/Source/core/css/properties/CSSShorthandPropertyAPIScrollBoundaryBehavior.cpp
@@ -0,0 +1,21 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSShorthandPropertyAPIScrollBoundaryBehavior.h"
+
+#include "core/StylePropertyShorthand.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+namespace blink {
+
+bool CSSShorthandPropertyAPIScrollBoundaryBehavior::parseShorthand(
+    bool important,
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    bool,
+    HeapVector<CSSProperty, 256>& properties) {
+  return CSSPropertyParserHelpers::ConsumeShorthandVia2LonghandAPIs(
+      scrollBoundaryBehaviorShorthand(), important, context, range, properties);
+}
+}  // namespace blink
