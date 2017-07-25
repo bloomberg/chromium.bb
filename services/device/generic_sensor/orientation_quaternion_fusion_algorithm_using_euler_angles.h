957ecc927782af002911518775aa7aa35c0e62d1
Jun Cai <juncai@chromium.org>
2017-07-25 19:03:43 +0000

    Implement RELATIVE_ORIENTATION_QUATERNION using euler angles on macOS
    This CL implements the RELATIVE_ORIENTATION_QUATERNION sensor type
    using data from RELATIVE_ORIENTATION_EULER_ANGLES on macOS. It just
    needs a conversion from euler angles to quaternion.
    
    Bug: 733707
    Change-Id: I621c6fe208ced6a0c904965985f3b8519eb24f50
    Reviewed-on: https://chromium-review.googlesource.com/583484
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489393}
    Former-commit-id: 1baa52d60dca0277379ba9dd79aae6dfc915a84b

---
+++ services/device/generic_sensor/orientation_quaternion_fusion_algorithm_using_euler_angles.h
@@ -0,0 +1,31 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SERVICES_DEVICE_ORIENTATION_QUATERNION_FUSION_ALGORITHM_USING_EULER_ANGLES_H_
+#define SERVICES_DEVICE_ORIENTATION_QUATERNION_FUSION_ALGORITHM_USING_EULER_ANGLES_H_
+
+#include "base/macros.h"
+#include "services/device/generic_sensor/platform_sensor_fusion_algorithm.h"
+
+namespace device {
+
+// Sensor fusion algorithm for implementing *_ORIENTATION_QUATERNION
+// using *_ORIENTATION_EULER_ANGLES.
+class OrientationQuaternionFusionAlgorithmUsingEulerAngles
+    : public PlatformSensorFusionAlgorithm {
+ public:
+  OrientationQuaternionFusionAlgorithmUsingEulerAngles();
+  ~OrientationQuaternionFusionAlgorithmUsingEulerAngles() override;
+
+  void GetFusedData(const std::vector<SensorReading>& readings,
+                    SensorReading* fused_reading) override;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(
+      OrientationQuaternionFusionAlgorithmUsingEulerAngles);
+};
+
+}  // namespace device
+
+#endif  // SERVICES_DEVICE_ORIENTATION_QUATERNION_FUSION_ALGORITHM_USING_EULER_ANGLES_H_
