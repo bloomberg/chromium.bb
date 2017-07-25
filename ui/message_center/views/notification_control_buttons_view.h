3ac13e26cb37011aad4b10fcfcc1639823f157a8
Yoshiki Iguchi <yoshiki@chromium.org>
2017-07-25 10:28:24 +0000

    Revert "Move the control buttons from dedicate widget to view"
    This reverts commit ed552f966fd23c3618b82f128afbcfc8238e93e6 [formerly 084388c254c2cde590a4e3df8601ac1e1ed84c88].
    
    Reason for revert: This change breaks some layout in notification.
    
    Original change's description:
    > Move the control buttons from dedicate widget to view
    > 
    > Previously, the buttons are on the dedicate widget to show them above
    > the ARC notification (NativeViewHost). This CL moves them to a view.
    > 
    > In addition, this CL adds a hacks for ARC notification which manipulates
    > the hierarycy of layers to show the buttons at top most.
    > 
    > Bug: b/62771497
    > Bug: 717455
    > Bug: 747217
    > Change-Id: I876c653e01d4a4237055bd4f763aa55017ba38ef
    > Reviewed-on: https://chromium-review.googlesource.com/579107
    > Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    > Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#488599}
    
    TBR=yoshiki@chromium.org,yhanada@chromium.org
    
    # Not skipping CQ checks because original CL landed > 1 day ago.
    
    Bug: b/62771497, 717455, 747217
    Change-Id: I2a4cbf639c1a13c910a1424589074d739fbc259b
    Reviewed-on: https://chromium-review.googlesource.com/584215
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489271}
    Former-commit-id: b14f20f949abe14f4317b525f279e95004512b4f

--- ui/message_center/views/notification_control_buttons_view.h
+++ ui/message_center/views/notification_control_buttons_view.h
@@ -6,7 +6,6 @@
 #define UI_MESSAGE_CENTER_VIEWS_NOTIFICATION_CONTROL_BUTTONS_VIEW_H_
 
 #include "base/macros.h"
-#include "build/build_config.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/animation/animation_delegate.h"
 #include "ui/message_center/message_center_export.h"
@@ -45,10 +44,6 @@ class MESSAGE_CENTER_EXPORT NotificationControlButtonsView
   // Set the background color of the view.
   void SetBackgroundColor(const SkColor& target_bgcolor);
 
-  // Set and get the visi8blity of buttons.
-  void SetButtonsVisible(bool visible);
-  bool GetButtonsVisible() const;
-
   // Request the focus on the close button.
   void RequestFocusOnCloseButton();
 
@@ -63,18 +58,9 @@ class MESSAGE_CENTER_EXPORT NotificationControlButtonsView
   message_center::PaddedButton* close_button_for_testing() const;
   message_center::PaddedButton* settings_button_for_testing() const;
 
-#if defined(OS_CHROMEOS)
-  void ReparentToWidgetLayer();
-  void AdjustLayerBounds();
-#endif  // defined(OS_CHROMEOS)
-
   // views::View
   const char* GetClassName() const override;
-#if defined(OS_CHROMEOS)
-  void ReorderChildLayers(ui::Layer* parent_layer) override;
-  gfx::Vector2d CalculateOffsetToAncestorWithLayer(
-      ui::Layer** layer_parent) override;
-#endif  // defined(OS_CHROMEOS)
+  void SetVisible(bool visible) override;
 
   // views::ButtonListener
   void ButtonPressed(views::Button* sender, const ui::Event& event) override;
@@ -85,23 +71,14 @@ class MESSAGE_CENTER_EXPORT NotificationControlButtonsView
   void AnimationCanceled(const gfx::Animation* animation) override;
 
  private:
-  // InnerView is the container for buttons. NotificationControlButtonsView has
-  // only one child which is InnerView.
-  class InnerView;
-
   MessageView* message_view_;
 
-  std::unique_ptr<InnerView> buttons_container_;
   std::unique_ptr<message_center::PaddedButton> close_button_;
   std::unique_ptr<message_center::PaddedButton> settings_button_;
 
   std::unique_ptr<gfx::LinearAnimation> bgcolor_animation_;
   SkColor bgcolor_origin_;
   SkColor bgcolor_target_;
-
-#if defined(OS_CHROMEOS)
-  bool is_layer_parent_widget_ = false;
-#endif  // defined(OS_CHROMEOS)
 
   DISALLOW_COPY_AND_ASSIGN(NotificationControlButtonsView);
 };
