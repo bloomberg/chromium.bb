84f16588a3cef7c0e71abea075193f8ffec8f52d
Jiaquan He <hejq@google.com>
2017-07-25 03:22:34 +0000

    Add tests for ArcPlayStoreAppSearchProvider.
    Bug=736552
    
    Change-Id: I6a9da9ce619f302824ef63c447ebce6738de6cf0
    Reviewed-on: https://chromium-review.googlesource.com/576291
    Commit-Queue: Jiaquan He <hejq@google.com>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Yury Khmel <khmel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489211}
    Former-commit-id: cc2fbf55db543c262459dd9ae07bd7753f66e486

--- components/arc/test/fake_app_instance.cc
+++ components/arc/test/fake_app_instance.cc
@@ -15,6 +15,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/path_service.h"
 #include "base/run_loop.h"
+#include "base/strings/stringprintf.h"
 
 namespace mojo {
 
@@ -277,7 +278,27 @@ void FakeAppInstance::GetRecentAndSuggestedAppsFromPlayStore(
     const std::string& query,
     int32_t max_results,
     const GetRecentAndSuggestedAppsFromPlayStoreCallback& callback) {
-  callback.Run(std::vector<arc::mojom::AppDiscoveryResultPtr>());
+  // Fake Play Store app info
+  std::vector<arc::mojom::AppDiscoveryResultPtr> fake_apps;
+  for (int i = 0; i < max_results; ++i) {
+    // Fake icon data
+    std::string png_data_as_string;
+    GetFakeIcon(mojom::ScaleFactor::SCALE_FACTOR_100P, &png_data_as_string);
+    std::vector<uint8_t> fake_icon_png_data(png_data_as_string.begin(),
+                                            png_data_as_string.end());
+
+    fake_apps.push_back(mojom::AppDiscoveryResult::New(
+        base::StringPrintf("LauncherIntentUri %d", i),  // launch_intent_uri
+        base::StringPrintf("InstallIntentUri %d", i),   // install_intent_uri
+        base::StringPrintf("%s %d", query.c_str(), i),  // label
+        i % 2 == 0,                                     // is_instant_app
+        i % 4 == 0,                                     // is_recent
+        base::StringPrintf("Publisher %d", i),          // publisher_name
+        base::StringPrintf("$%d.22", i),                // formatted_price
+        i,                                              // review_score
+        fake_icon_png_data));                           // icon_png_data
+  }
+  callback.Run(std::move(fake_apps));
 }
 
 void FakeAppInstance::StartPaiFlow() {
