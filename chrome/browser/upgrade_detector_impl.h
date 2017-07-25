a4000e92c4f5f3d5870f8102fa6a36af226d7d24
Greg Thompson <grt@chromium.org>
2017-07-25 15:49:37 +0000

    Migrate the UpgradeDetector to the task scheduler.
    Also:
    - removes the use of base::Singleton, which isn't needed
    - fixes races (potentially causing browser crashes) in the way
      detection was done on Windows.
    
    BUG=366647,689520
    
    Change-Id: I856a3d472225bb4678d89042b9997751935ba88f
    Reviewed-on: https://chromium-review.googlesource.com/581087
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Gabriel Charette <gab@chromium.org>
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489314}
    Former-commit-id: 6899098c691dd693391f5fa19456e571009c0e47

--- chrome/browser/upgrade_detector_impl.h
+++ chrome/browser/upgrade_detector_impl.h
@@ -5,15 +5,20 @@
 #ifndef CHROME_BROWSER_UPGRADE_DETECTOR_IMPL_H_
 #define CHROME_BROWSER_UPGRADE_DETECTOR_IMPL_H_
 
+#include "base/callback.h"
 #include "base/macros.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
+#include "base/sequence_checker.h"
 #include "base/timer/timer.h"
 #include "base/version.h"
+#include "build/build_config.h"
 #include "chrome/browser/upgrade_detector.h"
 #include "components/variations/service/variations_service.h"
 
 namespace base {
-template <typename T> struct DefaultSingletonTraits;
+class SequencedTaskRunner;
+class TaskRunner;
 }
 
 class UpgradeDetectorImpl : public UpgradeDetector,
@@ -23,10 +28,10 @@ class UpgradeDetectorImpl : public UpgradeDetector,
 
   // Returns the currently installed Chrome version, which may be newer than the
   // one currently running. Not supported on Android, iOS or ChromeOS. Must be
-  // run on a thread where I/O operations are allowed (e.g. FILE thread).
+  // run on a thread where I/O operations are allowed.
   static base::Version GetCurrentlyInstalledVersion();
 
-  // Returns the singleton instance.
+  // Returns the global instance.
   static UpgradeDetectorImpl* GetInstance();
 
  protected:
@@ -40,12 +45,19 @@ class UpgradeDetectorImpl : public UpgradeDetector,
   void NotifyOnUpgradeWithTimePassed(base::TimeDelta time_passed);
 
  private:
-  friend struct base::DefaultSingletonTraits<UpgradeDetectorImpl>;
+  // A callback that receives the results of |DetectUpgradeTask|.
+  using UpgradeDetectedCallback = base::OnceCallback<void(UpgradeAvailable)>;
+
+#if defined(OS_WIN) && defined(GOOGLE_CHROME_BUILD)
+  // Receives the results of AreAutoupdatesEnabled and starts the upgrade check
+  // timer.
+  void OnAutoupdatesEnabledResult(bool auto_updates_enabled);
+#endif
 
   // Start the timer that will call |CheckForUpgrade()|.
   void StartTimerForUpgradeCheck();
 
-  // Launches a task on the file thread to check if we have the latest version.
+  // Launches a background task to check if we have the latest version.
   void CheckForUpgrade();
 
   // Starts the upgrade notification timer that will check periodically whether
@@ -65,14 +77,16 @@ class UpgradeDetectorImpl : public UpgradeDetector,
   // user that a new version is available.
   void NotifyOnUpgrade();
 
-  // Called on the FILE thread to detect an upgrade. Calls back UpgradeDetected
-  // on the UI thread if so. Although it looks weird, this needs to be a static
-  // method receiving a WeakPtr<> to this object so that we can interrupt
-  // the UpgradeDetected callback before it runs. Having this method non-static
-  // and using |this| directly wouldn't be thread safe. And keeping it as a
-  // non-class function would prevent it from calling UpgradeDetected.
+  // Determines whether or not an update is available, posting |callback| with
+  // the result to |callback_task_runner| if so.
   static void DetectUpgradeTask(
-      base::WeakPtr<UpgradeDetectorImpl> upgrade_detector);
+      scoped_refptr<base::TaskRunner> callback_task_runner,
+      UpgradeDetectedCallback callback);
+
+  SEQUENCE_CHECKER(sequence_checker_);
+
+  // A sequenced task runner on which blocking tasks run.
+  scoped_refptr<base::SequencedTaskRunner> blocking_task_runner_;
 
   // We periodically check to see if Chrome has been upgraded.
   base::RepeatingTimer detect_upgrade_timer_;
--- chrome/browser/upgrade_detector_impl_unittest.cc
+++ chrome/browser/upgrade_detector_impl_unittest.cc
@@ -4,19 +4,17 @@
 
 #include "chrome/browser/upgrade_detector_impl.h"
 
-#include <vector>
-
 #include "base/macros.h"
+#include "base/test/scoped_task_environment.h"
 #include "chrome/browser/upgrade_observer.h"
-#include "content/public/browser/browser_thread.h"
-#include "content/public/test/test_browser_thread_bundle.h"
-#include "content/public/test/test_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+namespace {
 
 class TestUpgradeDetectorImpl : public UpgradeDetectorImpl {
  public:
-  TestUpgradeDetectorImpl() : trigger_critical_update_call_count_(0) {}
-  ~TestUpgradeDetectorImpl() override {}
+  TestUpgradeDetectorImpl() = default;
+  ~TestUpgradeDetectorImpl() override = default;
 
   // Methods exposed for testing.
   using UpgradeDetectorImpl::OnExperimentChangesDetected;
@@ -24,7 +22,7 @@ class TestUpgradeDetectorImpl : public UpgradeDetectorImpl {
 
   // UpgradeDetector:
   void TriggerCriticalUpdate() override {
-    trigger_critical_update_call_count_++;
+    ++trigger_critical_update_call_count_;
   }
 
   int trigger_critical_update_call_count() const {
@@ -34,7 +32,7 @@ class TestUpgradeDetectorImpl : public UpgradeDetectorImpl {
  private:
   // How many times TriggerCriticalUpdate() has been called. Expected to either
   // be 0 or 1.
-  int trigger_critical_update_call_count_;
+  int trigger_critical_update_call_count_ = 0;
 
   DISALLOW_COPY_AND_ASSIGN(TestUpgradeDetectorImpl);
 };
@@ -43,21 +41,20 @@ class TestUpgradeNotificationListener : public UpgradeObserver {
  public:
   explicit TestUpgradeNotificationListener(UpgradeDetector* detector)
       : notifications_count_(0), detector_(detector) {
+    DCHECK(detector_);
     detector_->AddObserver(this);
   }
   ~TestUpgradeNotificationListener() override {
-    if (detector_)
-      detector_->RemoveObserver(this);
+    detector_->RemoveObserver(this);
   }
 
-  int get_notifications_count() const { return notifications_count_; }
+  int notification_count() const { return notifications_count_; }
 
  private:
-  // UpgradeObserver implementation.
-  void OnUpgradeRecommended() override { notifications_count_ += 1; }
+  // UpgradeObserver:
+  void OnUpgradeRecommended() override { ++notifications_count_; }
 
-  // Keeps track of the number of upgrade recommended notifications that were
-  // received.
+  // The number of upgrade recommended notifications received.
   int notifications_count_;
 
   UpgradeDetector* detector_;
@@ -65,48 +62,50 @@ class TestUpgradeNotificationListener : public UpgradeObserver {
   DISALLOW_COPY_AND_ASSIGN(TestUpgradeNotificationListener);
 };
 
+}  // namespace
+
 TEST(UpgradeDetectorImplTest, VariationsChanges) {
-  content::TestBrowserThreadBundle bundle;
+  base::test::ScopedTaskEnvironment task_environment;
 
   TestUpgradeDetectorImpl detector;
   TestUpgradeNotificationListener notifications_listener(&detector);
   EXPECT_FALSE(detector.notify_upgrade());
-  EXPECT_EQ(0, notifications_listener.get_notifications_count());
+  EXPECT_EQ(0, notifications_listener.notification_count());
 
   detector.OnExperimentChangesDetected(
       variations::VariationsService::Observer::BEST_EFFORT);
   EXPECT_FALSE(detector.notify_upgrade());
-  EXPECT_EQ(0, notifications_listener.get_notifications_count());
+  EXPECT_EQ(0, notifications_listener.notification_count());
 
   detector.NotifyOnUpgradeWithTimePassed(base::TimeDelta::FromDays(30));
   EXPECT_TRUE(detector.notify_upgrade());
-  EXPECT_EQ(1, notifications_listener.get_notifications_count());
+  EXPECT_EQ(1, notifications_listener.notification_count());
   EXPECT_EQ(0, detector.trigger_critical_update_call_count());
 
-  // Execute tasks sent to FILE thread by |detector| referencing it
-  // while it's still in scope.
-  content::RunAllPendingInMessageLoop(content::BrowserThread::FILE);
+  // Execute tasks posted by |detector| referencing it while it's still in
+  // scope.
+  task_environment.RunUntilIdle();
 }
 
 TEST(UpgradeDetectorImplTest, VariationsCriticalChanges) {
-  content::TestBrowserThreadBundle bundle;
+  base::test::ScopedTaskEnvironment task_environment;
 
   TestUpgradeDetectorImpl detector;
   TestUpgradeNotificationListener notifications_listener(&detector);
   EXPECT_FALSE(detector.notify_upgrade());
-  EXPECT_EQ(0, notifications_listener.get_notifications_count());
+  EXPECT_EQ(0, notifications_listener.notification_count());
 
   detector.OnExperimentChangesDetected(
       variations::VariationsService::Observer::CRITICAL);
   EXPECT_FALSE(detector.notify_upgrade());
-  EXPECT_EQ(0, notifications_listener.get_notifications_count());
+  EXPECT_EQ(0, notifications_listener.notification_count());
 
   detector.NotifyOnUpgradeWithTimePassed(base::TimeDelta::FromDays(30));
   EXPECT_TRUE(detector.notify_upgrade());
-  EXPECT_EQ(1, notifications_listener.get_notifications_count());
+  EXPECT_EQ(1, notifications_listener.notification_count());
   EXPECT_EQ(1, detector.trigger_critical_update_call_count());
 
-  // Execute tasks sent to FILE thread by |detector| referencing it
-  // while it's still in scope.
-  content::RunAllPendingInMessageLoop(content::BrowserThread::FILE);
+  // Execute tasks posted by |detector| referencing it while it's still in
+  // scope.
+  task_environment.RunUntilIdle();
 }
