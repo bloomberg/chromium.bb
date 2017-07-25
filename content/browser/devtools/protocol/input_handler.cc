06d6835154cc6c39a050be4b6d742348862d590e
Blaise <allada@chromium.org>
2017-07-25 00:19:49 +0000

    [Reland #1] ChromeDriver: Handle key events properly on Mac
    Fixes devtool's emulation of key input events to emulate
    os key event for OSX.
    
    Original CL: https://codereview.chromium.org/2656903005
    Only major difference between two CLs is we now bypass browser
    if native_key_code was not set.
    
    667387, 732053
    
    Change-Id: If99f002491ef55caadbb7c82bcf55a2d1dbff8e6
    Reviewed-on: https://chromium-review.googlesource.com/562583
    Commit-Queue: Blaise Bruer <allada@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489156}
    Former-commit-id: bcad00c0fbd632e939b0d154f657138287c733de

--- content/browser/devtools/protocol/input_handler.cc
+++ content/browser/devtools/protocol/input_handler.cc
@@ -12,6 +12,7 @@
 #include "base/trace_event/trace_event.h"
 #include "cc/output/compositor_frame_metadata.h"
 #include "content/browser/devtools/devtools_session.h"
+#include "content/browser/devtools/protocol/native_input_event_builder.h"
 #include "content/browser/frame_host/render_frame_host_impl.h"
 #include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/common/input/synthetic_pinch_gesture_params.h"
@@ -300,7 +301,7 @@ void InputHandler::DispatchKeyEvent(
                         auto_repeat.fromMaybe(false),
                         is_keypad.fromMaybe(false)),
       GetEventTimeTicks(std::move(timestamp)));
-  event.skip_in_browser = true;
+
   if (!SetKeyboardEventText(event.text, std::move(text))) {
     callback->sendFailure(Response::InvalidParams("Invalid 'text' parameter"));
     return;
@@ -333,6 +334,13 @@ void InputHandler::DispatchKeyEvent(
     callback->sendFailure(Response::InternalError());
     return;
   }
+
+  // We do not pass events to browser if there is no native key event
+  // due to Mac needing the actual os_event.
+  if (event.native_key_code)
+    event.os_event = NativeInputEventBuilder::CreateEvent(event);
+  else
+    event.skip_in_browser = true;
 
   host_->GetRenderWidgetHost()->Focus();
   input_queued_ = false;
