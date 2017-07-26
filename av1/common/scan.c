963b86d324ed1a86fcdc2cebefacb55cfb22318d
Angie Chiang <angiebird@google.com>
2017-07-25 17:21:00 -0700

    Use ADAPT_SCAN_PROB_PRECISION to init prob
    Change-Id: I94d66c65d78235e1025703caf79ccca43208d604

--- av1/common/scan.c
+++ av1/common/scan.c
@@ -6824,7 +6824,8 @@ void av1_init_scan_order(AV1_COMMON *cm) {
       int i;
       SCAN_ORDER *sc = &cm->fc->sc[tx_size][tx_type];
       for (i = 0; i < tx2d_size; ++i) {
-        non_zero_prob[i] = (1 << 16) / 2;  // init non_zero_prob to 0.5
+        non_zero_prob[i] =
+            (1 << ADAPT_SCAN_PROB_PRECISION) / 2;  // init non_zero_prob to 0.5
       }
       update_scan_order_facade(cm, tx_size, tx_type);
       sc->scan = get_adapt_scan(cm->fc, tx_size, tx_type);
