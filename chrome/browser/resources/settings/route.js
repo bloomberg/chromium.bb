5a0a86d82f22b848a7ccc9ba11d1fa01efbc9ba0
Dave Schuyler <dschuyler@chromium.org>
2017-07-26 02:18:20 +0000

    [MD settings] change route for startupUrls to startupPages
    This CL changes the route name for the OnStartup settings sub-page. The
    new route is chrome://settings/startupPages.
    
    Bug: 747626
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I1d13908fb6e2ca9a4e09e6032caa05c796bfddc6
    Reviewed-on: https://chromium-review.googlesource.com/582467
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489530}
    Former-commit-id: 2dc1b0f5d089cd78bf907b38be2aeb53f40648fe

--- chrome/browser/resources/settings/route.js
+++ chrome/browser/resources/settings/route.js
@@ -236,7 +236,7 @@ cr.define('settings', function() {
 
     if (pageVisibility.onStartup !== false) {
       r.ON_STARTUP = r.BASIC.createSection('/onStartup', 'onStartup');
-      r.STARTUP_URLS = r.ON_STARTUP.createChild('/startupUrls');
+      r.STARTUP_PAGES = r.ON_STARTUP.createChild('/startupPages');
     }
 
     if (pageVisibility.people !== false) {
