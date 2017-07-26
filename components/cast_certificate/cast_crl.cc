416e7e3c8bef4171d2d15a4a38142b9532d6fe2f
Eric Roman <eroman@chromium.org>
2017-07-26 01:58:18 +0000

    Add delegate interfaces for CertPathBuilder and VerifyCertificateChain.
    The delegates subsume the functionality of SignaturePolicy, as well as adding a hook to check candidate paths built by CertPathBuilder.
    
    Bug: 634476
    Change-Id: I7ed337001e93b5de187a1780562558acf54dc73a
    Reviewed-on: https://chromium-review.googlesource.com/582127
    Commit-Queue: Eric Roman <eroman@chromium.org>
    Reviewed-by: Luke Halliwell <halliwell@chromium.org>
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489526}
    Former-commit-id: 5431d706fcb9b68710ef24e075d67bd19c0bc406

--- components/cast_certificate/cast_crl.cc
+++ components/cast_certificate/cast_crl.cc
@@ -17,7 +17,7 @@
 #include "net/cert/internal/parsed_certificate.h"
 #include "net/cert/internal/path_builder.h"
 #include "net/cert/internal/signature_algorithm.h"
-#include "net/cert/internal/signature_policy.h"
+#include "net/cert/internal/simple_path_builder_delegate.h"
 #include "net/cert/internal/trust_store_in_memory.h"
 #include "net/cert/internal/verify_certificate_chain.h"
 #include "net/cert/internal/verify_signed_data.h"
@@ -87,13 +87,6 @@ bool ConvertTimeSeconds(uint64_t seconds,
                                                generalized_time);
 }
 
-// Specifies the signature verification policy.
-// The required algorithms are:
-// RSASSA PKCS#1 v1.5 with SHA-256, using RSA keys 2048-bits or longer.
-std::unique_ptr<net::SignaturePolicy> CreateCastSignaturePolicy() {
-  return base::MakeUnique<net::SimpleSignaturePolicy>(2048);
-}
-
 // Verifies the CRL is signed by a trusted CRL authority at the time the CRL
 // was issued. Verifies the signature of |tbs_crl| is valid based on the
 // certificate and signature in |crl|. The validity of |tbs_crl| is verified
@@ -122,16 +115,12 @@ bool VerifyCRL(const Crl& crl,
       net::der::Input(base::StringPiece(crl.signature())), 0);
 
   // Verify the signature.
-  auto signature_policy = CreateCastSignaturePolicy();
   std::unique_ptr<net::SignatureAlgorithm> signature_algorithm_type =
       net::SignatureAlgorithm::CreateRsaPkcs1(net::DigestAlgorithm::Sha256);
-  net::CertErrors verify_errors;
-  if (!VerifySignedData(*signature_algorithm_type,
-                        net::der::Input(&crl.tbs_crl()),
-                        signature_value_bit_string, parsed_cert->tbs().spki_tlv,
-                        signature_policy.get(), &verify_errors)) {
-    VLOG(2) << "CRL - Signature verification failed:\n"
-            << verify_errors.ToDebugString();
+  if (!VerifySignedData(
+          *signature_algorithm_type, net::der::Input(&crl.tbs_crl()),
+          signature_value_bit_string, parsed_cert->tbs().spki_tlv)) {
+    VLOG(2) << "CRL - Signature verification failed";
     return false;
   }
 
@@ -141,9 +130,14 @@ bool VerifyCRL(const Crl& crl,
     VLOG(2) << "CRL - Unable to parse verification time.";
     return false;
   }
+
+  // SimplePathBuilderDelegate will enforce required signature algorithms of
+  // RSASSA PKCS#1 v1.5 with SHA-256, and RSA keys 2048-bits or longer.
+  net::SimplePathBuilderDelegate path_builder_delegate(2048);
+
   net::CertPathBuilder::Result result;
   net::CertPathBuilder path_builder(
-      parsed_cert.get(), trust_store, signature_policy.get(), verification_time,
+      parsed_cert.get(), trust_store, &path_builder_delegate, verification_time,
       net::KeyPurpose::ANY_EKU, net::InitialExplicitPolicy::kFalse,
       {net::AnyPolicy()}, net::InitialPolicyMappingInhibit::kFalse,
       net::InitialAnyPolicyInhibit::kFalse, &result);
