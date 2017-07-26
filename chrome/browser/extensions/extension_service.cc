1c8337f3cf23e259509132d6bc1c4cb66b2e5955
Takumi Fujimoto <takumif@chromium.org>
2017-07-26 17:29:59 +0000

    Cleanup: Remove check for a deprecated flag in ExtensionService::Init()
    Remove the check for a deprecated flag --load-component-extension.
    
    Change-Id: I761ec421903c0213050227c91295d1e36537e9b1
    Reviewed-on: https://chromium-review.googlesource.com/585335
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489681}
    Former-commit-id: d5cf965f1732453a445a9b0b33a719ddfa6c7d5c

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -144,10 +144,6 @@ namespace {
 // Wait this many seconds after an extensions becomes idle before updating it.
 const int kUpdateIdleDelay = 5;
 
-// Comma-separated list of directories with extensions to load.
-// TODO(samuong): Remove this in M58 (see comment in ExtensionService::Init).
-const char kDeprecatedLoadComponentExtension[] = "load-component-extension";
-
 void DoNothingWithExtensionHost(extensions::ExtensionHost* host) {}
 
 }  // namespace
@@ -471,20 +467,6 @@ void ExtensionService::Init() {
   LoadExtensionsFromCommandLineFlag(switches::kDisableExtensionsExcept);
   if (load_command_line_extensions)
     LoadExtensionsFromCommandLineFlag(extensions::switches::kLoadExtension);
-  // ChromeDriver has no way of determining the Chrome version until after
-  // launch, so it needs to continue passing load-component-extension until it
-  // stops supporting Chrome 56 (when M58 is released). These extensions are
-  // loaded as regular extensions, not component extensions, and are thus safe.
-  // TODO(samuong): Remove this when we release Chrome 58 to stable channel.
-  if (command_line_->HasSwitch(switches::kEnableAutomation) &&
-      command_line_->HasSwitch(kDeprecatedLoadComponentExtension)) {
-    LOG(WARNING) << "Loading extension specified by "
-                    "--load-component-extension as a regular extension. "
-                    "Extensions specified by --load-component-extension will "
-                    "not be loaded starting in M58. Use --load-extension or "
-                    "--disable-extensions-except.";
-    LoadExtensionsFromCommandLineFlag(kDeprecatedLoadComponentExtension);
-  }
   EnabledReloadableExtensions();
   MaybeFinishShutdownDelayed();
   SetReadyAndNotifyListeners();
