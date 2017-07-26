bdf42c12e3747e8bbd3b881c3fe2a62019105faf
Gabriel Charette <gab@chromium.org>
2017-07-26 12:02:04 +0000

    Introduce RunLoop::QuitCurrent*Deprecated() to be favored over MessageLoop::Quit*()
    This is required to get rid of direct calls to MessageLoop in tests
    and allow for other abstractions to drive RunLoop (since it's impossible
    to migrate every test to RunLoop).
    
    Using message_loop_test.cc to test this. Those tests will go away when
    the deprecated methods can finally be removed but by then a lot of
    callers will be using the RunLoop versions and I don't think it will be
    possible to break them.
    
    R=danakj@chromium.org
    TBR=dpranke@chromium.org (PRESUBMIT.py)
    
    Bug: 748715
    Change-Id: Ia0b996850a47bf24626a99c17b5985e8da826beb
    Reviewed-on: https://chromium-review.googlesource.com/585650
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489611}
    Former-commit-id: 0592c3a469ac181a07925cf6488d9b4112e17006

--- base/message_loop/message_loop_test.cc
+++ base/message_loop/message_loop_test.cc
@@ -75,7 +75,7 @@ class Foo : public RefCounted<Foo> {
 void SlowFunc(TimeDelta pause, int* quit_counter) {
     PlatformThread::Sleep(pause);
     if (--(*quit_counter) == 0)
-      MessageLoop::current()->QuitWhenIdle();
+      base::RunLoop::QuitCurrentWhenIdleDeprecated();
 }
 
 // This function records the time when Run was called in a Time object, which is
@@ -112,8 +112,7 @@ void RunTest_PostTask(MessagePumpFactory factory) {
       FROM_HERE, BindOnce(&Foo::Test2Mixed, foo, a, &d));
   // After all tests, post a message that will shut down the message loop
   ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE,
-      BindOnce(&MessageLoop::QuitWhenIdle, Unretained(MessageLoop::current())));
+      FROM_HERE, BindOnce(&RunLoop::QuitCurrentWhenIdleDeprecated));
 
   // Now kick things off
   RunLoop().Run();
@@ -360,7 +359,7 @@ void NestingFunc(int* depth) {
     MessageLoop::current()->SetNestableTasksAllowed(true);
     RunLoop().Run();
   }
-  MessageLoop::current()->QuitWhenIdle();
+  base::RunLoop::QuitCurrentWhenIdleDeprecated();
 }
 
 void RunTest_Nesting(MessagePumpFactory factory) {
@@ -468,7 +467,7 @@ void RecursiveFunc(TaskList* order, int cookie, int depth,
 
 void QuitFunc(TaskList* order, int cookie) {
   order->RecordStart(QUITMESSAGELOOP, cookie);
-  MessageLoop::current()->QuitWhenIdle();
+  base::RunLoop::QuitCurrentWhenIdleDeprecated();
   order->RecordEnd(QUITMESSAGELOOP, cookie);
 }
 void RunTest_RecursiveDenial1(MessagePumpFactory factory) {
@@ -675,7 +674,7 @@ void FuncThatRuns(TaskList* order, int cookie, RunLoop* run_loop) {
 }
 
 void FuncThatQuitsNow() {
-  MessageLoop::current()->QuitNow();
+  base::RunLoop::QuitCurrentDeprecated();
 }
 // Tests RunLoopQuit only quits the corresponding MessageLoop::Run.
 void RunTest_QuitNow(MessagePumpFactory factory) {
@@ -969,7 +968,7 @@ void PostNTasksThenQuit(int posts_remaining) {
     ThreadTaskRunnerHandle::Get()->PostTask(
         FROM_HERE, BindOnce(&PostNTasksThenQuit, posts_remaining - 1));
   } else {
-    MessageLoop::current()->QuitWhenIdle();
+    base::RunLoop::QuitCurrentWhenIdleDeprecated();
   }
 }
 
