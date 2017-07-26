be0b2b1d21879fcfde1b7f501c1db43c0968f5d5
Yuqian Li <liyuqian@google.com>
2017-07-26 18:11:00 +0000

    Add guard flag for Delta AA
    Bug: 
    Change-Id: Iee7f142e0497a7660a8be1f01699c0f0f5fd27a6
    Reviewed-on: https://chromium-review.googlesource.com/586947
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Yuqian Li <liyuqian@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489697}
    Former-commit-id: 55dc05579b639b9f71112aa8956c3b589d3b2342

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -212,6 +212,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_GRADIENT_MATRIX_MATH
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_DELTA_AA
+#define SK_SUPPORT_LEGACY_DELTA_AA
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
