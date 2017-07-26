59920f22f025b368d2a540516f26ae5f1b120974
Yi Gu <yigu@chromium.org>
2017-07-26 23:28:29 +0000

    Record GPU memory for tilings
    This patch records GPU memory usage for tilings on cc.
    
    Bug: 684629
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I8e480ed872b4c3d54edf139aca08153c341bd921
    Reviewed-on: https://chromium-review.googlesource.com/548875
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Commit-Queue: Yi Gu <yigu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489795}
    Former-commit-id: 9c16783426d192604ddac0831fcfb1a8544a14f8

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -1105,16 +1105,19 @@ DrawResult LayerTreeHostImpl::PrepareToDraw(FrameData* frame) {
     input_handler_client_->ReconcileElasticOverscrollAndRootScroll();
 
   if (const char* client_name = GetClientNameForMetrics()) {
-    size_t total_memory = 0;
-    for (const PictureLayerImpl* layer : active_tree()->picture_layers())
-      total_memory += layer->GetRasterSource()->GetMemoryUsage();
-    if (total_memory != 0) {
+    size_t total_memory_in_bytes = 0;
+    size_t total_gpu_memory_for_tilings_in_bytes = 0;
+    for (const PictureLayerImpl* layer : active_tree()->picture_layers()) {
+      total_memory_in_bytes += layer->GetRasterSource()->GetMemoryUsage();
+      total_gpu_memory_for_tilings_in_bytes += layer->GPUMemoryUsageInBytes();
+    }
+    if (total_memory_in_bytes != 0) {
       // GetClientNameForMetrics only returns one non-null value over the
       // lifetime of the process, so this histogram name is runtime constant.
       UMA_HISTOGRAM_COUNTS(
           base::StringPrintf("Compositing.%s.PictureMemoryUsageKb",
                              client_name),
-          base::saturated_cast<int>(total_memory / 1024));
+          base::saturated_cast<int>(total_memory_in_bytes / 1024));
     }
 
     // GetClientNameForMetrics only returns one non-null value over the lifetime
@@ -1128,6 +1131,18 @@ DrawResult LayerTreeHostImpl::PrepareToDraw(FrameData* frame) {
                            client_name),
         base::saturated_cast<int>(active_tree_->picture_layers().size()), 1,
         400, 20);
+
+    // TODO(yigu): Maybe we should use the same check above. Need to figure out
+    // why exactly we skip 0.
+    if (!active_tree()->picture_layers().empty()) {
+      UMA_HISTOGRAM_CUSTOM_COUNTS(
+          base::StringPrintf("Compositing.%s.GPUMemoryForTilingsInKb",
+                             client_name),
+          base::saturated_cast<int>(total_gpu_memory_for_tilings_in_bytes /
+                                    1024),
+          1, kGPUMemoryForTilingsLargestBucketKb,
+          kGPUMemoryForTilingsBucketCount);
+    }
   }
 
   bool ok = active_tree_->UpdateDrawProperties();
