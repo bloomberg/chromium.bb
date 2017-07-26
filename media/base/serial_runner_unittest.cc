4a5a52ea07fd9c1807d71794aeb276bf14fcc39e
Gabriel Charette <gab@chromium.org>
2017-07-26 17:57:03 +0000

    Manually migrate away from deprecated MessageLoop::Quit*() methods in /media.
    This follows an automated pass for simpler use cases @
    https://chromium-review.googlesource.com/c/585732/.
    
    Minimal cleanup was made when trivial.
    
    (some calls were even removed as they merely added a redundant QuitWhenIdle()
    to a RunLoop().RunUntilIdle() call)
    
    When too complex I defaulted to the lesser bad alternative of
    RunLoop::QuitCurrent*Deprecated() methods.
    
    I will not engage in further cleanups as part of this massive
    change -- owners are welcome to follow suit with better usage
    of RunLoop if unsatisfied with this).
    
    This CL was uploaded by git cl split.
    
    R=liberato@chromium.org
    
    Bug: 748715
    Change-Id: I995174fb611b7a1f4cd6d4a7b3c0f5cfde8bd1c0
    Reviewed-on: https://chromium-review.googlesource.com/586851
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Gabriel Charette <gab@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489689}
    Former-commit-id: fe80709efc0c017e1fada9e7040ef47f221b7fa4

--- media/base/serial_runner_unittest.cc
+++ media/base/serial_runner_unittest.cc
@@ -115,7 +115,6 @@ class SerialRunnerTest : public ::testing::Test {
 
     done_called_ = true;
     done_status_ = status;
-    message_loop_.QuitWhenIdle();
   }
 
   void CancelSerialRunner(const PipelineStatusCB& status_cb) {
--- media/test/pipeline_integration_test_base.cc
+++ media/test/pipeline_integration_test_base.cc
@@ -7,6 +7,7 @@
 #include <utility>
 
 #include "base/bind.h"
+#include "base/callback.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/run_loop.h"
@@ -347,17 +348,19 @@ void PipelineIntegrationTestBase::FailTest(PipelineStatus status) {
 }
 
 void PipelineIntegrationTestBase::QuitAfterCurrentTimeTask(
-    const base::TimeDelta& quit_time) {
+    base::TimeDelta quit_time,
+    base::OnceClosure quit_closure) {
   if (pipeline_->GetMediaTime() >= quit_time ||
       pipeline_status_ != PIPELINE_OK) {
-    message_loop_.QuitWhenIdle();
+    std::move(quit_closure).Run();
     return;
   }
 
   message_loop_.task_runner()->PostDelayedTask(
       FROM_HERE,
-      base::Bind(&PipelineIntegrationTestBase::QuitAfterCurrentTimeTask,
-                 base::Unretained(this), quit_time),
+      base::BindOnce(&PipelineIntegrationTestBase::QuitAfterCurrentTimeTask,
+                     base::Unretained(this), quit_time,
+                     std::move(quit_closure)),
       base::TimeDelta::FromMilliseconds(10));
 }
 
@@ -367,12 +370,14 @@ bool PipelineIntegrationTestBase::WaitUntilCurrentTimeIsAfter(
   DCHECK_GT(pipeline_->GetPlaybackRate(), 0);
   DCHECK(wait_time <= pipeline_->GetMediaDuration());
 
+  base::RunLoop run_loop;
   message_loop_.task_runner()->PostDelayedTask(
       FROM_HERE,
-      base::Bind(&PipelineIntegrationTestBase::QuitAfterCurrentTimeTask,
-                 base::Unretained(this), wait_time),
+      base::BindOnce(&PipelineIntegrationTestBase::QuitAfterCurrentTimeTask,
+                     base::Unretained(this), wait_time,
+                     run_loop.QuitWhenIdleClosure()),
       base::TimeDelta::FromMilliseconds(10));
-  base::RunLoop().Run();
+  run_loop.Run();
   return (pipeline_status_ == PIPELINE_OK);
 }
 
