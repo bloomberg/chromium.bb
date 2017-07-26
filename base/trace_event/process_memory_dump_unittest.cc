979b3c4f4e75c751b4e63fef732391dd0d5b8920
gambard <gambard@chromium.org>
2017-07-26 22:02:07 +0000

    Disable ProcessMemoryDumpTest.CountResidentBytesInSharedMemory for iOS 9
    TBR=primiano
    
    Bug: 748410
    Change-Id: Ie1f5e37772e918b9986faf1f3feb4e1bb2eebc97
    Reviewed-on: https://chromium-review.googlesource.com/584843
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Reviewed-by: Gregory Chatzinoff <gchatz@chromium.org>
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489770}
    Former-commit-id: 9dd9c24af2feb7ef0222326ba6b3f794711b4b9b

--- base/trace_event/process_memory_dump_unittest.cc
+++ base/trace_event/process_memory_dump_unittest.cc
@@ -23,6 +23,10 @@
 #include <sys/mman.h>
 #endif
 
+#if defined(OS_IOS)
+#include "base/ios/ios_util.h"
+#endif
+
 namespace base {
 namespace trace_event {
 
@@ -428,6 +432,13 @@ TEST(ProcessMemoryDumpTest, CountResidentBytes) {
 }
 
 TEST(ProcessMemoryDumpTest, CountResidentBytesInSharedMemory) {
+#if defined(OS_IOS)
+  // TODO(crbug.com/748410): Reenable this test.
+  if (!base::ios::IsRunningOnIOS10OrLater()) {
+    return;
+  }
+#endif
+
   const size_t page_size = ProcessMemoryDump::GetSystemPageSize();
 
   // Allocate few page of dirty memory and check if it is resident.
