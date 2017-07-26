ac215f1f05c8adc834fb5373f1f2af4ab8e79a3b
Robert Sesek <rsesek@chromium.org>
2017-07-26 23:00:33 +0000

    [Android] Permit PR_SET_TIMERSLACK option to prctl.
    The _PID option that set the timerslack on a specific pid are already
    allowed, so permit this option that targets the current pid.
    
    Bug: 740825
    Change-Id: Ie051b46b0685babb33f73259a67f96669dab717f
    Reviewed-on: https://chromium-review.googlesource.com/585593
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Commit-Queue: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489787}
    Former-commit-id: b5c1a79e3b946ec5e5c8304139157de3c744e8f6

--- sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.cc
+++ sandbox/linux/seccomp-bpf-helpers/syscall_parameters_restrictions.cc
@@ -42,6 +42,10 @@
 #define F_DUPFD_CLOEXEC (F_LINUX_SPECIFIC_BASE + 6)
 #endif
 
+#if !defined(PR_SET_TIMERSLACK)
+#define PR_SET_TIMERSLACK 29
+#endif
+
 // https://android.googlesource.com/platform/bionic/+/lollipop-release/libc/private/bionic_prctl.h
 #if !defined(PR_SET_VMA)
 #define PR_SET_VMA 0x53564d41
@@ -153,7 +157,7 @@ ResultExpr RestrictPrctl() {
   return Switch(option)
       .CASES((PR_GET_NAME, PR_SET_NAME, PR_GET_DUMPABLE, PR_SET_DUMPABLE
 #if defined(OS_ANDROID)
-              , PR_SET_VMA, PR_SET_PTRACER
+              , PR_SET_VMA, PR_SET_PTRACER, PR_SET_TIMERSLACK
 
 // Enable PR_SET_TIMERSLACK_PID, an Android custom prctl which is used in:
 // https://android.googlesource.com/platform/system/core/+/lollipop-release/libcutils/sched_policy.c.
