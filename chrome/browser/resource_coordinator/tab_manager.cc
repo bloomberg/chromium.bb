08232812a87cd6d4eb399cce5c782b650356b4e0
Zhen Wang <zhenw@chromium.org>
2017-07-27 17:13:08 +0000

    Add loading slots to TabManager for background tab opening
    This CL adds loading slots to TabManager to control how many background tabs
    can load in parallel. There is one loading slot currently.
    
    Bug: 730098
    Change-Id: I3748356c1f46cf553c9e6b0f04f970efc572602a
    Reviewed-on: https://chromium-review.googlesource.com/587434
    Commit-Queue: Zhen Wang <zhenw@chromium.org>
    Reviewed-by: Chris Hamilton <chrisha@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489996}
    Former-commit-id: 700d01a85c01dafab99da154d7480eac1a0d2bf0

--- chrome/browser/resource_coordinator/tab_manager.cc
+++ chrome/browser/resource_coordinator/tab_manager.cc
@@ -79,6 +79,10 @@ namespace {
 // a dynamic threshold under different scenarios.
 const TimeDelta kBackgroundTabLoadTimeout = TimeDelta::FromSeconds(10);
 
+// The number of loading slots for background tabs. TabManager will start to
+// load the next background tab when the loading slots free up.
+const size_t kNumOfLoadingSlots = 1;
+
 // The default interval in seconds after which to adjust the oom_score_adj
 // value.
 const int kAdjustmentIntervalSeconds = 10;
@@ -156,6 +160,7 @@ TabManager::TabManager()
       test_tick_clock_(nullptr),
       is_session_restore_loading_tabs_(false),
       force_load_timer_(base::MakeUnique<base::OneShotTimer>()),
+      loading_slots_(kNumOfLoadingSlots),
       weak_ptr_factory_(this) {
 #if defined(OS_CHROMEOS)
   delegate_.reset(new TabManagerDelegate(weak_ptr_factory_.GetWeakPtr()));
@@ -1043,7 +1048,7 @@ std::vector<TabManager::BrowserInfo> TabManager::GetBrowserInfoList() const {
 content::NavigationThrottle::ThrottleCheckResult
 TabManager::MaybeThrottleNavigation(BackgroundTabNavigationThrottle* throttle) {
   content::NavigationHandle* navigation_handle = throttle->navigation_handle();
-  if (!ShouldDelayNavigation(navigation_handle)) {
+  if (CanLoadNextTab()) {
     loading_contents_.insert(navigation_handle->GetWebContents());
     return content::NavigationThrottle::PROCEED;
   }
@@ -1058,13 +1063,14 @@ TabManager::MaybeThrottleNavigation(BackgroundTabNavigationThrottle* throttle) {
   return content::NavigationThrottle::DEFER;
 }
 
-bool TabManager::ShouldDelayNavigation(
-    content::NavigationHandle* navigation_handle) const {
-  // Do not delay the navigation if no tab is currently loading.
-  if (loading_contents_.empty())
-    return false;
+bool TabManager::CanLoadNextTab() const {
+  // TabManager can only load the next tab when the loading slots free up. The
+  // loading slot limit can be exceeded when |force_load_timer_| fires or when
+  // the user selects a background tab.
+  if (loading_contents_.size() < loading_slots_)
+    return true;
 
-  return true;
+  return false;
 }
 
 void TabManager::OnDidFinishNavigation(
@@ -1099,10 +1105,10 @@ void TabManager::StartForceLoadTimer() {
 }
 
 void TabManager::LoadNextBackgroundTabIfNeeded() {
-  // Do not load more background tabs until all currently loading tabs have
-  // finished. Ignore this constraint if the timer fires to force loading the
-  // next background tab.
-  if (force_load_timer_->IsRunning() && !loading_contents_.empty())
+  // Do not load more background tabs until TabManager can load the next tab.
+  // Ignore this constraint if the timer fires to force loading the next
+  // background tab.
+  if (force_load_timer_->IsRunning() && !CanLoadNextTab())
     return;
 
   if (pending_navigations_.empty())
