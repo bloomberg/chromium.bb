492074555a3d37deb097ac11aaeca27b6617a8cf
Quinten Yearsley <qyearsley@google.com>
2017-07-27 22:48:26 +0000

    Remove unused methods from Git
    Specifically, this removes:
      _commit_position_regex_for_timestamp
      timestamp_of_revision
      blame
      _branch_tracking_remote_master
      ensure_cleanly_tracking_remote_master
    
    Most of these are unused since removing "webkit-patch auto-rebaseline".
    
    Change-Id: I7422f7f32370b4348753439fdce47a35287866a5
    Reviewed-on: https://chromium-review.googlesource.com/587341
    Reviewed-by: Jeff Carpenter <jeffcarp@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490035}
    Former-commit-id: c124e9fd2ca4f21782895c6d699b1b5e4f19c6d1

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/git_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/git_unittest.py
@@ -181,35 +181,12 @@ Date:   Mon Sep 28 19:10:30 2015 -0700
         git = self.tracking_git
         self.assertEqual(git._commit_position_from_git_log(git_log), 1234567)
 
-    def test_timestamp_of_revision(self):
-        # This tests a protected method. pylint: disable=protected-access
-        self._chdir(self.tracking_git_checkout_path)
-        git = self.tracking_git
-        position_regex = git._commit_position_regex_for_timestamp()
-        git.most_recent_log_matching(position_regex, git.checkout_root)
-
 
 class GitTestWithMock(unittest.TestCase):
 
     def make_git(self):
         git = Git(cwd='.', executive=MockExecutive(), filesystem=MockFileSystem())
-        git.read_git_config = lambda *args, **kw: 'MOCKKEY:MOCKVALUE'
         return git
-
-    def _assert_timestamp_of_revision(self, canned_git_output, expected):
-        git = self.make_git()
-        git.find_checkout_root = lambda path: ''
-        git.run = lambda args: canned_git_output
-        self.assertEqual(git.timestamp_of_revision('some-path', '12345'), expected)
-
-    def test_timestamp_of_revision_utc(self):
-        self._assert_timestamp_of_revision('Date: 2013-02-08 08:05:49 +0000', '2013-02-08T08:05:49Z')
-
-    def test_timestamp_of_revision_positive_timezone(self):
-        self._assert_timestamp_of_revision('Date: 2013-02-08 01:02:03 +0130', '2013-02-07T23:32:03Z')
-
-    def test_timestamp_of_revision_pacific_timezone(self):
-        self._assert_timestamp_of_revision('Date: 2013-02-08 01:55:21 -0800', '2013-02-08T09:55:21Z')
 
     def test_unstaged_files(self):
         git = self.make_git()
