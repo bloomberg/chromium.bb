96c3c048a135971c9b5d7fdd7ec14b8094a14b50
danielpark@chromium.org <danielpark@chromium.org>
2017-07-27 00:42:56 +0000

    Reverting fix for 747783
    Bug: 749254
    Change-Id: I8d89b2bf8cf44b8b4501cceb04b0524f101039a2
    Reviewed-on: https://chromium-review.googlesource.com/587401
    Reviewed-by: Matthew Jones <mdjones@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489816}
    Former-commit-id: 73b3808e9815243ed3c19cb35ec1cb7eb11c52cf

--- chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetContentController.java
+++ chrome/android/java/src/org/chromium/chrome/browser/widget/bottomsheet/BottomSheetContentController.java
@@ -127,10 +127,6 @@ public class BottomSheetContentController extends BottomNavigationView
             }
 
             if (mBottomSheet.getSheetState() == BottomSheet.SHEET_STATE_PEEK) {
-                if (mSelectedItemId != R.id.action_home) {
-                    showBottomSheetContent(R.id.action_home);
-                    mBottomSheet.endAnimations();
-                }
                 clearBottomSheetContents(false);
             }
         }
