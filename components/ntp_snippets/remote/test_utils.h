9ddc18190b222f916769bc10af43af9c809ed48f
Vitalii Iarko <vitaliii@chromium.org>
2017-07-27 16:14:23 +0000

    [NTP::Tests::Cleanup] Move category/suggestion builders into utils.
    This CL moves RemoteSuggestionBuilder and FetchedCategoryBuilder into
    remote/test_utils.h. This allows using them in other tests.
    
    Bug: 
    Change-Id: I8fd6c1a4099de5fcc23d6fab2f8a444964298c70
    Reviewed-on: https://chromium-review.googlesource.com/586601
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Reviewed-by: Chris Pickel <sfiera@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489957}
    Former-commit-id: e4d0b0ee68c673de3b680bcc492596ca42413d6e

--- components/ntp_snippets/remote/test_utils.h
+++ components/ntp_snippets/remote/test_utils.h
@@ -6,8 +6,13 @@
 #define COMPONENTS_NTP_SNIPPETS_REMOTE_TEST_UTILS_H_
 
 #include <memory>
+#include <string>
+#include <vector>
 
+#include "base/optional.h"
 #include "build/build_config.h"
+#include "components/ntp_snippets/remote/json_to_categories.h"
+#include "components/ntp_snippets/remote/remote_suggestion.h"
 #include "components/signin/core/browser/fake_signin_manager.h"
 #include "components/sync/driver/fake_sync_service.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
@@ -27,6 +32,7 @@ using SigninManagerForTest = FakeSigninManager;
 #endif  // OS_CHROMEOS
 
 namespace ntp_snippets {
+
 namespace test {
 
 class FakeSyncService : public syncer::FakeSyncService {
@@ -74,7 +80,76 @@ class RemoteSuggestionsTestUtils {
   std::unique_ptr<AccountTrackerService> account_tracker_;
 };
 
+class RemoteSuggestionBuilder {
+ public:
+  RemoteSuggestionBuilder();
+  RemoteSuggestionBuilder(const RemoteSuggestionBuilder& other);
+  ~RemoteSuggestionBuilder();
+
+  RemoteSuggestionBuilder& AddId(const std::string& id);
+  RemoteSuggestionBuilder& SetTitle(const std::string& title);
+  RemoteSuggestionBuilder& SetSnippet(const std::string& snippet);
+  RemoteSuggestionBuilder& SetImageUrl(const std::string& image_url);
+  RemoteSuggestionBuilder& SetPublishDate(const base::Time& publish_date);
+  RemoteSuggestionBuilder& SetExpiryDate(const base::Time& expiry_date);
+  RemoteSuggestionBuilder& SetScore(double score);
+  RemoteSuggestionBuilder& SetIsDismissed(bool is_dismissed);
+  RemoteSuggestionBuilder& SetRemoteCategoryId(int remote_category_id);
+  RemoteSuggestionBuilder& SetUrl(const std::string& url);
+  RemoteSuggestionBuilder& SetPublisher(const std::string& publisher);
+  RemoteSuggestionBuilder& SetAmpUrl(const std::string& amp_url);
+  RemoteSuggestionBuilder& SetFetchDate(const base::Time& fetch_date);
+
+  std::unique_ptr<RemoteSuggestion> Build() const;
+
+ private:
+  base::Optional<std::vector<std::string>> ids_;
+  base::Optional<std::string> title_;
+  base::Optional<std::string> snippet_;
+  base::Optional<std::string> salient_image_url_;
+  base::Optional<base::Time> publish_date_;
+  base::Optional<base::Time> expiry_date_;
+  base::Optional<double> score_;
+  base::Optional<bool> is_dismissed_;
+  base::Optional<int> remote_category_id_;
+  base::Optional<std::string> url_;
+  base::Optional<std::string> publisher_name_;
+  base::Optional<std::string> amp_url_;
+  base::Optional<base::Time> fetch_date_;
+};
+
+class FetchedCategoryBuilder {
+ public:
+  FetchedCategoryBuilder();
+  FetchedCategoryBuilder(const FetchedCategoryBuilder& other);
+  ~FetchedCategoryBuilder();
+
+  FetchedCategoryBuilder& SetCategory(Category category);
+  FetchedCategoryBuilder& SetTitle(const std::string& title);
+  FetchedCategoryBuilder& SetCardLayout(
+      ContentSuggestionsCardLayout card_layout);
+  FetchedCategoryBuilder& SetAdditionalAction(
+      ContentSuggestionsAdditionalAction additional_action);
+  FetchedCategoryBuilder& SetShowIfEmpty(bool show_if_empty);
+  FetchedCategoryBuilder& SetNoSuggestionsMessage(
+      const std::string& no_suggestions_message);
+  FetchedCategoryBuilder& AddSuggestionViaBuilder(
+      const RemoteSuggestionBuilder& builder);
+
+  FetchedCategory Build() const;
+
+ private:
+  base::Optional<Category> category_;
+  base::Optional<base::string16> title_;
+  base::Optional<ContentSuggestionsCardLayout> card_layout_;
+  base::Optional<ContentSuggestionsAdditionalAction> additional_action_;
+  base::Optional<bool> show_if_empty_;
+  base::Optional<base::string16> no_suggestions_message_;
+  base::Optional<std::vector<RemoteSuggestionBuilder>> suggestion_builders_;
+};
+
 }  // namespace test
+
 }  // namespace ntp_snippets
 
 #endif  // COMPONENTS_NTP_SNIPPETS_REMOTE_TEST_UTILS_H_
