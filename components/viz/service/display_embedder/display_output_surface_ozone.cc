97c869718768c51c0061f943af2e49c34b3d06f0
Daniele Castagna <dcastagna@chromium.org>
2017-07-27 17:15:53 +0000

    viz: Align OutputSurface and BufferQueue in flight buffers.
    In crrev.com/2829543003 we landed support for empty swap on surfaceless
    output surfaces.
    
    When the damage rect of the swap buffer was empty, the OutputSurface
    would avoid calling BindFramebuffer and SwapBuffers on the BufferQueue.
    It would still call BufferQueue::PageFlipComplete though, causing
    an inconsistency between the BufferQueue::in_flight_surfaces_ and
    the actual in flight surfaces scheduled by the GLRenderer.
    
    In particular, in case of an empty damage rect, the renderer would
    reschedule the same surface, without calling BufferQueue::SwapBuffers,
    BufferQueue::in_flight_surfaces_ would contain only one surface, when
    the actual scheduled surfaces for scanout would be two (even if the
    same buffer).
    
    After PageFlipComplete, BufferQueue would think there were no more
    in_flight_surfaces_ and it would soon recycle the surface, and the
    renderer would end up drawing to the surface while still in use for
    scanout by the display controller (crbug.com/743277 for symptoms).
    
    This CL makes sure that the number of time we call
    Additionally, it makes the buffer queue advance by a surface every
    time a current surface is needed and is not available. In this way we
    can call SwapBuffers, GetCurrentTextureIdtwice followed by another
    SwapBuffers without having to call BindFramebuffer to have a current
    surface.
     
    
    BufferQueue: :SwapBuffers matches BufferQueue::PageFlipComplete.
    Bug: 743277
    Change-Id: Ibc970c8beea9edc3d901d165f0c0479718c6e43e
    Reviewed-on: https://chromium-review.googlesource.com/587461
    Reviewed-by: Dongseong Hwang <dongseong.hwang@intel.com>
    Reviewed-by: Robert Kroeger <rjkroege@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: John Bauman <jbauman@chromium.org>
    Commit-Queue: Daniele Castagna <dcastagna@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490001}
    Former-commit-id: b5168e0d1731a1e4b8e6da22bdac6d08f6ab41a2

--- components/viz/service/display_embedder/display_output_surface_ozone.cc
+++ components/viz/service/display_embedder/display_output_surface_ozone.cc
@@ -83,11 +83,7 @@ void DisplayOutputSurfaceOzone::SwapBuffers(cc::OutputSurfaceFrame frame) {
 
   gfx::Rect damage_rect =
       frame.sub_buffer_rect ? *frame.sub_buffer_rect : gfx::Rect(swap_size_);
-  // Use previous buffer when damage rect is empty. This avoids unnecessary
-  // partial swap work and makes it possible to support empty swaps on devices
-  // where partial swaps are disabled.
-  if (!damage_rect.IsEmpty())
-    buffer_queue_->SwapBuffers(damage_rect);
+  buffer_queue_->SwapBuffers(damage_rect);
 
   DisplayOutputSurface::SwapBuffers(std::move(frame));
 }
