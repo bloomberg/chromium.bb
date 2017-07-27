e5ce5c6807a85187dd9cede54e9144f3bcc3ee47
Chih-Wei Huang <cwhuang@android-x86.org>
2017-07-27 16:20:51 +0800

    android: add rules to build amdgpu.ids
    Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>

---
+++ data/Android.mk
@@ -0,0 +1,9 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := amdgpu.ids
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/hwdata
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
