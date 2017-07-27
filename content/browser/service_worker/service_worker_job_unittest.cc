1244abcf884557fba948443fcdcc42fab867bac0
xzhan96 <xiaofeng.zhang@intel.com>
2017-07-27 11:47:19 +0000

    Pass the WPT: register-wait-forever-in-install-worker.https.html
    This test failure is because of spec changes. Current version requires
    that algorithm execution should hold if the job queue still contains
    the previously-scheduled "register" job.
    
    This patch actually reverts crrev.com/817643002, but adds a timer to
    control the front job of each queue, in case somewhere gets stuck,
    like reading from the db, so that can't register/update/unregister a
    new service worker at that scope.
    
    BUG=723037
    
    Change-Id: I5c64314628fb025b190ea03a42d85dbacc3a6d94
    Reviewed-on: https://chromium-review.googlesource.com/560633
    Commit-Queue: Xiaofeng Zhang <xiaofeng.zhang@intel.com>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489901}
    Former-commit-id: ddf5acab903eaca3a63d65b0558c6bf7680864d8

--- content/browser/service_worker/service_worker_job_unittest.cc
+++ content/browser/service_worker/service_worker_job_unittest.cc
@@ -199,6 +199,13 @@ class ServiceWorkerJobTest : public testing::Test {
   ServiceWorkerJobCoordinator* job_coordinator() const {
     return context()->job_coordinator();
   }
+  std::map<GURL, ServiceWorkerJobCoordinator::JobQueue>* job_queues() const {
+    return &(job_coordinator()->job_queues_);
+  }
+  bool is_job_timer_running() {
+    return job_coordinator()->job_timeout_timer_.IsRunning();
+  }
+
   ServiceWorkerStorage* storage() const { return context()->storage(); }
 
  protected:
@@ -213,6 +220,7 @@ class ServiceWorkerJobTest : public testing::Test {
       const GURL& pattern,
       ServiceWorkerStatusCode expected_status = SERVICE_WORKER_OK);
   std::unique_ptr<ServiceWorkerProviderHost> CreateControllee();
+  void TimeOutFirstJob();
 
   TestBrowserThreadBundle browser_thread_bundle_;
   std::unique_ptr<EmbeddedWorkerTestHelper> helper_;
@@ -228,9 +236,11 @@ scoped_refptr<ServiceWorkerRegistration> ServiceWorkerJobTest::RunRegisterJob(
   job_coordinator()->Register(
       script_url, ServiceWorkerRegistrationOptions(pattern), nullptr,
       SaveRegistration(expected_status, &called, &registration));
+  EXPECT_TRUE(is_job_timer_running());
   EXPECT_FALSE(called);
   base::RunLoop().RunUntilIdle();
   EXPECT_TRUE(called);
+  EXPECT_FALSE(is_job_timer_running());
   return registration;
 }
 
@@ -269,6 +279,14 @@ ServiceWorkerJobTest::CreateControllee() {
       true /* is_parent_frame_secure */, helper_->context()->AsWeakPtr(),
       &remote_endpoints_.back());
   return host;
+}
+
+void ServiceWorkerJobTest::TimeOutFirstJob() {
+  ServiceWorkerRegisterJob* job = static_cast<ServiceWorkerRegisterJob*>(
+      job_queues()->begin()->second.front());
+  base::TimeDelta duration = base::TimeDelta::FromMinutes(30);
+  job->set_start_time_for_test(base::TimeTicks::Now() - duration);
+  job_coordinator()->MaybeTimeoutJobs();
 }
 
 TEST_F(ServiceWorkerJobTest, SameDocumentSameRegistration) {
@@ -394,6 +412,43 @@ TEST_F(ServiceWorkerJobTest, Register) {
   EXPECT_EQ(valid_scope_2, version_->foreign_fetch_scopes_[1]);
   EXPECT_EQ(1u, version_->foreign_fetch_origins_.size());
   EXPECT_EQ(valid_origin, version_->foreign_fetch_origins_[0]);
+}
+
+// Make sure job timeout timer is working.
+TEST_F(ServiceWorkerJobTest, RegistrationTimeout) {
+  bool called1;
+  scoped_refptr<ServiceWorkerRegistration> registration1;
+  job_coordinator()->Register(
+      GURL("http://www.example.com/service_worker.js"),
+      ServiceWorkerRegistrationOptions(GURL("http://www.example.com/one/")),
+      nullptr,
+      SaveRegistration(SERVICE_WORKER_ERROR_ABORT, &called1, &registration1));
+
+  bool called2;
+  scoped_refptr<ServiceWorkerRegistration> registration2;
+  job_coordinator()->Register(
+      GURL("http://www.example.com/service_worker.js"),
+      ServiceWorkerRegistrationOptions(GURL("http://www.example.com/two/")),
+      nullptr,
+      SaveRegistration(SERVICE_WORKER_ERROR_ABORT, &called2, &registration2));
+
+  EXPECT_EQ(2u, job_queues()->size());
+  EXPECT_FALSE(called1);
+  EXPECT_FALSE(called2);
+
+  // Timeout the first job. The timer should still be running.
+  TimeOutFirstJob();
+  EXPECT_EQ(1u, job_queues()->size());
+  EXPECT_TRUE(called1);
+  EXPECT_FALSE(called2);
+  EXPECT_TRUE(is_job_timer_running());
+
+  // Timeout the second job. The queue should be empty and the timer should be
+  // stopped.
+  TimeOutFirstJob();
+  EXPECT_TRUE(called2);
+  EXPECT_TRUE(job_queues()->empty());
+  EXPECT_FALSE(is_job_timer_running());
 }
 
 // Make sure registrations are cleaned up when they are unregistered.
