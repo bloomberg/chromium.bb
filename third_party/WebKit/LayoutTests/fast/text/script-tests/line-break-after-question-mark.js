d596b0429709086b32d19288775b8acf04c81e7d
Koji Ishii <kojii@chromium.org>
2017-07-27 05:54:47 +0000

    Avoid using dumpAsText() in line breaker tests
    This helps running these tests in LayoutNG, before TextIterator is
    fully supported.
    
    TBR=eae@chromium.org
    
    Bug: 636993, 425345
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I405064cf58a48fe6a285f0c5927f13170306ef14
    Reviewed-on: https://chromium-review.googlesource.com/585514
    Reviewed-by: Koji Ishii <kojii@chromium.org>
    Commit-Queue: Koji Ishii <kojii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489867}
    Former-commit-id: d25864d82dabec0856d9305adba79f165cda3580

--- third_party/WebKit/LayoutTests/fast/text/script-tests/line-break-after-question-mark.js
+++ third_party/WebKit/LayoutTests/fast/text/script-tests/line-break-after-question-mark.js
@@ -1,5 +1,3 @@
-description("Tests when line breaking is allowed after a question mark character.");
-
 var div = document.body.appendChild(document.createElement("div"));
 div.style.width = "1px";
 div.style.fontFamily = "Ahem";
@@ -11,9 +9,10 @@ function allowsBreakBefore(i)
     return div.offsetHeight > 25;
 }
 
-var unallowedBreaks = [0x21, 0x22, 0x27, 0x29, 0x2c, 0x2e, 0x2f, 0x3a, 0x3b, 0x3f, 0x5d, 0x7d];
+var unallowedBreaks = [0x21, 0x22, 0x29, 0x2c, 0x2e, 0x2f, 0x3a, 0x3b, 0x3f, 0x5d, 0x7d];
 
-for (var i = 33; i < 128; ++i)
-    shouldBe("allowsBreakBefore(" + i + ")", unallowedBreaks.indexOf(i) >= 0 ? "false" : "true");
-
-document.body.removeChild(div);
+for (var i = 33; i < 128; ++i) {
+  test(() => {
+    assert_equals(allowsBreakBefore(i), unallowedBreaks.indexOf(i) < 0);
+  }, "allowsBreakBefore(" + i + ")");
+}
