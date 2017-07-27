e8fc9b924ef3ef7b41d869bb98fcb4f0521b9d84
Rob Buis <rob.buis@samsung.com>
2017-07-27 16:13:19 +0000

    Implements CSSPropertyAPI for -webkit-filter/filter/background-filter
    As part of Project Ribbon, separate the parsing logic for CSS
    properties -webkit-filter, filter and background-filter from
    the parser into an API.
    
    Bug: 668012
    Change-Id: I28c68caef886d955cfcd50daa3a29839b145709a
    Reviewed-on: https://chromium-review.googlesource.com/581250
    Commit-Queue: Rob Buis <rob.buis@samsung.com>
    Reviewed-by: meade_UTC10 <meade@chromium.org>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489956}
    Former-commit-id: 9999dfe43c2cafcca50ca2c2bf1110b09386177b

--- third_party/WebKit/Source/core/css/properties/CSSPropertyAPIFilter.cpp
+++ third_party/WebKit/Source/core/css/properties/CSSPropertyAPIFilter.cpp
@@ -4,4 +4,99 @@
 
 #include "core/css/properties/CSSPropertyAPIFilter.h"
 
-namespace blink {}  // namespace blink
+#include "core/css/CSSFunctionValue.h"
+#include "core/css/CSSShadowValue.h"
+#include "core/css/CSSURIValue.h"
+#include "core/css/parser/CSSParserContext.h"
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+#include "core/css/properties/CSSPropertyBoxShadowUtils.h"
+#include "core/frame/UseCounter.h"
+
+namespace blink {
+class CSSParserLocalContext;
+
+namespace {
+
+static CSSFunctionValue* ConsumeFilterFunction(
+    CSSParserTokenRange& range,
+    const CSSParserContext& context) {
+  CSSValueID filter_type = range.Peek().FunctionId();
+  if (filter_type < CSSValueInvert || filter_type > CSSValueDropShadow)
+    return nullptr;
+  CSSParserTokenRange args = CSSPropertyParserHelpers::ConsumeFunction(range);
+  CSSFunctionValue* filter_value = CSSFunctionValue::Create(filter_type);
+  CSSValue* parsed_value = nullptr;
+
+  if (filter_type == CSSValueDropShadow) {
+    parsed_value = CSSPropertyBoxShadowUtils::ParseSingleShadow(
+        args, context.Mode(), AllowInsetAndSpread::kForbid);
+  } else {
+    if (args.AtEnd()) {
+      context.Count(WebFeature::kCSSFilterFunctionNoArguments);
+      return filter_value;
+    }
+    if (filter_type == CSSValueBrightness) {
+      // FIXME (crbug.com/397061): Support calc expressions like calc(10% + 0.5)
+      parsed_value =
+          CSSPropertyParserHelpers::ConsumePercent(args, kValueRangeAll);
+      if (!parsed_value) {
+        parsed_value =
+            CSSPropertyParserHelpers::ConsumeNumber(args, kValueRangeAll);
+      }
+    } else if (filter_type == CSSValueHueRotate) {
+      parsed_value = CSSPropertyParserHelpers::ConsumeAngle(
+          args, context, WebFeature::kUnitlessZeroAngleFilter);
+    } else if (filter_type == CSSValueBlur) {
+      parsed_value = CSSPropertyParserHelpers::ConsumeLength(
+          args, kHTMLStandardMode, kValueRangeNonNegative);
+    } else {
+      // FIXME (crbug.com/397061): Support calc expressions like calc(10% + 0.5)
+      parsed_value = CSSPropertyParserHelpers::ConsumePercent(
+          args, kValueRangeNonNegative);
+      if (!parsed_value) {
+        parsed_value = CSSPropertyParserHelpers::ConsumeNumber(
+            args, kValueRangeNonNegative);
+      }
+      if (parsed_value && filter_type != CSSValueSaturate &&
+          filter_type != CSSValueContrast) {
+        bool is_percentage = ToCSSPrimitiveValue(parsed_value)->IsPercentage();
+        double max_allowed = is_percentage ? 100.0 : 1.0;
+        if (ToCSSPrimitiveValue(parsed_value)->GetDoubleValue() > max_allowed) {
+          parsed_value = CSSPrimitiveValue::Create(
+              max_allowed, is_percentage
+                               ? CSSPrimitiveValue::UnitType::kPercentage
+                               : CSSPrimitiveValue::UnitType::kNumber);
+        }
+      }
+    }
+  }
+  if (!parsed_value || !args.AtEnd())
+    return nullptr;
+  filter_value->Append(*parsed_value);
+  return filter_value;
+}
+
+}  // namespace
+
+const CSSValue* CSSPropertyAPIFilter::parseSingleValue(
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    const CSSParserLocalContext&) {
+  if (range.Peek().Id() == CSSValueNone)
+    return CSSPropertyParserHelpers::ConsumeIdent(range);
+
+  CSSValueList* list = CSSValueList::CreateSpaceSeparated();
+  do {
+    CSSValue* filter_value =
+        CSSPropertyParserHelpers::ConsumeUrl(range, &context);
+    if (!filter_value) {
+      filter_value = ConsumeFilterFunction(range, context);
+      if (!filter_value)
+        return nullptr;
+    }
+    list->Append(*filter_value);
+  } while (!range.AtEnd());
+  return list;
+}
+
+}  // namespace blink
