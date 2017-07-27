49828cd7f9f11bc7120bead0f49501997b2ccd47
Ben Wagner <bungeman@chromium.org>
2017-07-27 17:16:29 +0000

    Use efficient ftsystem.c when possible.
    Some builds of Chromium include FontConfig as another user of FreeType.
    Some of these builds will use FontConfig to scan a number of font files
    to build an FcConfig. Using an optimized version of ftsystem.c can speed
    this process significantly.
    
    Change-Id: I6b0efc4969a1d23a4683662c246eafd9ab7760de
    Reviewed-on: https://chromium-review.googlesource.com/585389
    Commit-Queue: Ben Wagner <bungeman@chromium.org>
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490002}
    Former-commit-id: 64078272a13528bc845f73d333334faf0db8aa69

--- third_party/freetype/roll-freetype.sh
+++ third_party/freetype/roll-freetype.sh
@@ -9,6 +9,12 @@ git merge-file third_party/freetype/include/freetype-custom-config/ftoption.h ${
 rm ${TMPFILE} &&
 git add third_party/freetype/include/freetype-custom-config/ftoption.h &&
 
+TMPFILE=`mktemp` &&
+git -C third_party/freetype/src/ cat-file blob HEAD@{1}:include/freetype/config/ftconfig.h >> ${TMPFILE} &&
+git merge-file third_party/freetype/include/freetype-custom-config/ftconfig.h ${TMPFILE} third_party/freetype/src/include/freetype/config/ftconfig.h &&
+rm ${TMPFILE} &&
+git add third_party/freetype/include/freetype-custom-config/ftconfig.h &&
+
 FTVERSION=`git -C third_party/freetype/src/ describe` &&
 FTCOMMIT=`git -C third_party/freetype/src/ rev-parse HEAD` &&
 sed -i "s/^Version: .*\$/Version: ${FTVERSION%-*}/" third_party/freetype/README.chromium &&
