460cd8a23466d156db0633bdbbcc5acaa825dc91
Ningning Xia <nxia@chromium.org>
2017-07-27 14:42:21 -0700

    Add Insert and Get operations for buildRequestTable.
    BUG=chromium:748171
    TEST=cidb_integration_test
    
    Change-Id: If08175441f55d053fb27c91d18c232604fc3fe06
    Reviewed-on: https://chromium-review.googlesource.com/590855
    Commit-Ready: Ningning Xia <nxia@chromium.org>
    Tested-by: Ningning Xia <nxia@chromium.org>
    Reviewed-by: Ningning Xia <nxia@chromium.org>

---
+++ lib/build_requests.py
@@ -0,0 +1,22 @@
+# Copyright 2017 The Chromium OS Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Classes to manage build requests."""
+
+from __future__ import print_function
+
+import collections
+
+
+REASON_SANITY_PRE_CQ = 'sanity-pre-cq'
+
+# Valid build request reasons
+BUILD_REQUEST_REASONS = (REASON_SANITY_PRE_CQ,)
+
+BUILD_REQUEST_COLUMNS = (
+    'id', 'build_id', 'request_build_config', 'request_build_args',
+    'request_buildbucket_id', 'request_reason', 'timestamp')
+
+# A namedtuple representing a BuildRequest record as recorded in cidb.
+BuildRequest = collections.namedtuple('BuildRequest', BUILD_REQUEST_COLUMNS)
