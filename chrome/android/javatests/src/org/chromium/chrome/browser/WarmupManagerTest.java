e313b3fefcb8e812d4e9706351a664ca33c419e3
Benoit Lize <lizeb@chromium.org>
2017-07-27 15:43:47 +0000

    android: Test that preconnects can be issued from the Java side.
    Preconnects can be initiated from WarmupManager. This adds the testing
    infrastructure and a basic test checking that these preconnects are
    actually issued. This is required to test the redirect preconnection
    handling for Custom Tabs.
    Forthcoming CLs will add tests to check that the sockets are re-used,
    and end-to-end tests of redirect preconnection.
    
    In terms of implementation:
    Native:
      - Sets an EmbeddedTestServerConnectionListener on the native side.
      - Have this listener relay connection data to the Java side.
    
    In Java:
      - Add a callback AIDL interface to relay callbacks from the server to
        the test APK.
      - Have the client-side callback post a task to the UI thread, as AIDL
        backed callbacks are delivered to the binder thread pool.
    
    Bug: 739165
    Change-Id: Ifa461aa614354eaa5cb5d7b1aec5ba84b8d1b8f7
    Reviewed-on: https://chromium-review.googlesource.com/583607
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Reviewed-by: Misha Efimov <mef@chromium.org>
    Reviewed-by: Ted Choc <tedchoc@chromium.org>
    Commit-Queue: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#489937}
    Former-commit-id: 6c5197808ef235ba93e0376b6d8288b12a7e26be

--- chrome/android/javatests/src/org/chromium/chrome/browser/WarmupManagerTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/WarmupManagerTest.java
@@ -26,12 +26,17 @@ import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.MetricsUtils;
 import org.chromium.base.test.util.Restriction;
 import org.chromium.chrome.R;
+import org.chromium.chrome.browser.init.ChromeBrowserInitializer;
+import org.chromium.chrome.browser.profiles.Profile;
 import org.chromium.chrome.browser.test.ChromeBrowserTestRule;
 import org.chromium.content.browser.test.util.Criteria;
 import org.chromium.content.browser.test.util.CriteriaHelper;
 import org.chromium.content_public.browser.WebContents;
 import org.chromium.content_public.browser.WebContentsObserver;
+import org.chromium.net.test.EmbeddedTestServer;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -43,12 +48,21 @@ public class WarmupManagerTest {
             RuleChain.outerRule(new ChromeBrowserTestRule()).around(new UiThreadTestRule());
 
     private WarmupManager mWarmupManager;
+    private Context mContext;
 
     @Before
     public void setUp() throws Exception {
+        mContext = InstrumentationRegistry.getInstrumentation()
+                           .getTargetContext()
+                           .getApplicationContext();
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
+                try {
+                    ChromeBrowserInitializer.getInstance(mContext).handleSynchronousStartup();
+                } catch (Exception e) {
+                    Assert.fail();
+                }
                 mWarmupManager = WarmupManager.getInstance();
             }
         });
@@ -194,8 +208,47 @@ public class WarmupManagerTest {
     public void testInflateLayout() throws Throwable {
         int layoutId = R.layout.custom_tabs_control_container;
         int toolbarId = R.layout.custom_tabs_toolbar;
-        Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        mWarmupManager.initializeViewHierarchy(context, layoutId, toolbarId);
+        mWarmupManager.initializeViewHierarchy(mContext, layoutId, toolbarId);
         Assert.assertTrue(mWarmupManager.hasViewHierarchyWithToolbar(layoutId));
+    }
+
+    /** Tests that preconnects can be initiated from the Java side. */
+    @Test
+    @SmallTest
+    public void testPreconnect() throws Exception {
+        EmbeddedTestServer server = new EmbeddedTestServer();
+        try {
+            // The predictor prepares 2 connections when asked to preconnect. Initializes the
+            // semaphore to be unlocked after 2 connections.
+            final Semaphore connectionsSemaphore = new Semaphore(1 - 2);
+
+            // Cannot use EmbeddedTestServer#createAndStartServer(), as we need to add the
+            // connection listener.
+            server.initializeNative(mContext);
+            server.addDefaultHandlers("");
+            server.setConnectionListener(new EmbeddedTestServer.ConnectionListener() {
+                @Override
+                public void acceptedSocket(long socketId) {
+                    connectionsSemaphore.release();
+                }
+            });
+            server.start();
+
+            final String url = server.getURL("/hello_world.html");
+            ThreadUtils.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mWarmupManager.maybePreconnectUrlAndSubResources(
+                            Profile.getLastUsedProfile(), url);
+                }
+            });
+            if (!connectionsSemaphore.tryAcquire(5, TimeUnit.SECONDS)) {
+                // Starts at -1.
+                int actualConnections = connectionsSemaphore.availablePermits() + 1;
+                Assert.fail("Expected 2 connections, got " + actualConnections);
+            }
+        } finally {
+            server.stopAndDestroyServer();
+        }
     }
 }
