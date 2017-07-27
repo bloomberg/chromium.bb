565d0d21d88b1e6578f85d1af7cbe5c876c52ff7
ananta <ananta@chromium.org>
2017-07-27 15:41:41 -0700

    The Appcache subresource URL factory needs to inform the URLLoaderClient if there is a failure.
    Relanding this with the browser test fixes.
    
    Failure cases include, an invalid AppCacheHost instance, failure to create a request handler, etc.
    We already notify the client about the latter case above. Need to do this for an invalid host as well.
    This caused the w3schools AppCache test page to spin indefinitely if we pass in an invalid URL.
    
    Additionally on the renderer side we need to reset the cached URLLoaderFactory pointer if a null factory
    is passed in the CommitNavigation IPC. This ensures that the request hits the default network loader.
    
    BUG=715632
    TEST=Covered by browser test. AppCacheNetworkServiceBrowserTest.VerifySubresourceFactoryClearedOnNewNavigation
    
    This reverts commit b769da9087b9dc9deef15c71db2d4785bf4e2f41 [formerly a08d0fc015c42ce95a104c0f4e3de9448b7721e1].
    
    Review-Url: https://codereview.chromium.org/2986043002
    Cr-Commit-Position: refs/heads/master@{#490025}
    
    Former-commit-id: aad9e6266f96e08ae80b9af238a9e6121a28c76e

--- content/browser/appcache/appcache_subresource_url_factory.cc
+++ content/browser/appcache/appcache_subresource_url_factory.cc
@@ -63,17 +63,17 @@ void AppCacheSubresourceURLFactory::CreateLoaderAndStart(
 
   // If the host is invalid, it means that the renderer has probably died.
   // (Frame has navigated elsewhere?)
-  if (!appcache_host_.get())
+  if (!appcache_host_.get()) {
+    NotifyError(std::move(client), net::ERR_FAILED);
     return;
+  }
 
   std::unique_ptr<AppCacheRequestHandler> handler =
       appcache_host_->CreateRequestHandler(
           AppCacheURLLoaderRequest::Create(request), request.resource_type,
           request.should_reset_appcache);
   if (!handler) {
-    ResourceRequestCompletionStatus request_result;
-    request_result.error_code = net::ERR_FAILED;
-    client->OnComplete(request_result);
+    NotifyError(std::move(client), net::ERR_FAILED);
     return;
   }
 
@@ -107,6 +107,14 @@ void AppCacheSubresourceURLFactory::SyncLoad(int32_t routing_id,
 
 void AppCacheSubresourceURLFactory::OnConnectionError() {
   base::ThreadTaskRunnerHandle::Get()->DeleteSoon(FROM_HERE, this);
+}
+
+void AppCacheSubresourceURLFactory::NotifyError(
+    mojom::URLLoaderClientPtr client,
+    int error_code) {
+  ResourceRequestCompletionStatus request_result;
+  request_result.error_code = error_code;
+  client->OnComplete(request_result);
 }
 
 }  // namespace content
