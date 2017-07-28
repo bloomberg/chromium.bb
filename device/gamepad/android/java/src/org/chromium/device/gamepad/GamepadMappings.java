b1550dd82d0084cff3f48da43607293760dbd4e6
Matt Reynolds <mattreynolds@google.com>
2017-07-28 23:24:37 +0000

    Restore mappings for older Xbox One S Bluetooth gamepads on Android
    Microsoft released a firmware update for Xbox One S gamepads that changed
    the button and axis assignments when connected over Bluetooth, requiring a
    new mapping in Chrome. In a previous CL, the mappings for the old firmware
    were removed under the assumption that soon all gamepads would use the new
    firmware mappings. However, this was incorrect as the new mappings are not
    exposed on all platforms. Furthermore, Xbox One S gamepads are still
    shipping with the old FW and users may not have the means to update.
    
    On Android, the default mapping is correct for the new FW but a custom
    mapping is required to support the old FW. This CL restores the mapping
    for devices with the old FW and uses the default mapping for devices with
    the new FW.
    
    BUG=705506
    
    Change-Id: Iec721fc46935c40787aba904bf72db9e50a17e8e
    Reviewed-on: https://chromium-review.googlesource.com/592243
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Matt Reynolds <mattreynolds@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490538}
    Former-commit-id: 4196f898978a551e070a9274af7a26728db2f959

--- device/gamepad/android/java/src/org/chromium/device/gamepad/GamepadMappings.java
+++ device/gamepad/android/java/src/org/chromium/device/gamepad/GamepadMappings.java
@@ -36,6 +36,11 @@ abstract class GamepadMappings {
     @VisibleForTesting
     static final int PS_DUALSHOCK_4_SLIM_PRODUCT_ID = 2508;
 
+    @VisibleForTesting
+    static final int XBOX_ONE_S_2016_FIRMWARE_VENDOR_ID = 0x045e;
+    @VisibleForTesting
+    static final int XBOX_ONE_S_2016_FIRMWARE_PRODUCT_ID = 0x02e0;
+
     public static GamepadMappings getMappings(InputDevice device, int[] axes) {
         GamepadMappings mappings = null;
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -55,6 +60,15 @@ abstract class GamepadMappings {
                 && (productId == PS_DUALSHOCK_4_PRODUCT_ID
                            || productId == PS_DUALSHOCK_4_SLIM_PRODUCT_ID)) {
             return new PS4GamepadMappings();
+        }
+        // Microsoft released a firmware update for the Xbox One S gamepad that modified the button
+        // and axis assignments. With the new firmware, these gamepads work correctly in Android
+        // using the default mapping, but a custom mapping is still required for the old firmware.
+        // Both gamepads return the same device name, so we must compare hardware IDs to distinguish
+        // them.
+        if (vendorId == XBOX_ONE_S_2016_FIRMWARE_VENDOR_ID
+                && productId == XBOX_ONE_S_2016_FIRMWARE_PRODUCT_ID) {
+            return new XboxOneS2016FirmwareMappings();
         }
         return null;
     }
@@ -249,6 +263,68 @@ abstract class GamepadMappings {
 
             mapXYAxes(mappedAxes, rawAxes);
             mapZAndRZAxesToRightStick(mappedAxes, rawAxes);
+        }
+    }
+
+    private static class XboxOneS2016FirmwareMappings extends GamepadMappings {
+        private boolean mLeftTriggerActivated = false;
+        private boolean mRightTriggerActivated = false;
+
+        /**
+         * Method for mapping Xbox One S controller (in Bluetooth mode) to
+         * standard gamepad button and axes values.
+         */
+        @Override
+        public void mapToStandardGamepad(
+                float[] mappedAxes, float[] mappedButtons, float[] rawAxes, float[] rawButtons) {
+            mappedButtons[CanonicalButtonIndex.PRIMARY] = rawButtons[KeyEvent.KEYCODE_BUTTON_A];
+            mappedButtons[CanonicalButtonIndex.SECONDARY] = rawButtons[KeyEvent.KEYCODE_BUTTON_B];
+            mappedButtons[CanonicalButtonIndex.TERTIARY] = rawButtons[KeyEvent.KEYCODE_BUTTON_C];
+            mappedButtons[CanonicalButtonIndex.QUATERNARY] = rawButtons[KeyEvent.KEYCODE_BUTTON_X];
+
+            mappedButtons[CanonicalButtonIndex.LEFT_SHOULDER] =
+                    rawButtons[KeyEvent.KEYCODE_BUTTON_Y];
+            mappedButtons[CanonicalButtonIndex.RIGHT_SHOULDER] =
+                    rawButtons[KeyEvent.KEYCODE_BUTTON_Z];
+
+            mappedButtons[CanonicalButtonIndex.BACK_SELECT] =
+                    rawButtons[KeyEvent.KEYCODE_BUTTON_L1];
+            mappedButtons[CanonicalButtonIndex.START] = rawButtons[KeyEvent.KEYCODE_BUTTON_R1];
+
+            mappedButtons[CanonicalButtonIndex.LEFT_THUMBSTICK] =
+                    rawButtons[KeyEvent.KEYCODE_BUTTON_L2];
+            mappedButtons[CanonicalButtonIndex.RIGHT_THUMBSTICK] =
+                    rawButtons[KeyEvent.KEYCODE_BUTTON_R2];
+
+            // The left and right triggers on the Xbox One S controller
+            // are exposed as AXIS_Z and AXIS_RZ respectively. However,
+            // these nominally idle at -1 rather than 0, like other triggers.
+            // Unfortunately, the -1 value is only reported upon the first
+            // activation of each trigger axis. In order to prevent idling at
+            // 0.5 before trigger activation, we only expose trigger values
+            // when we've seen them report a non-zero value at least once.
+            if (rawAxes[MotionEvent.AXIS_Z] != 0) {
+                mLeftTriggerActivated = true;
+            }
+            if (rawAxes[MotionEvent.AXIS_RZ] != 0) {
+                mRightTriggerActivated = true;
+            }
+            if (mLeftTriggerActivated) {
+                mappedButtons[CanonicalButtonIndex.LEFT_TRIGGER] =
+                        (rawAxes[MotionEvent.AXIS_Z] + 1) / 2;
+            } else {
+                mappedButtons[CanonicalButtonIndex.LEFT_TRIGGER] = 0.f;
+            }
+            if (mRightTriggerActivated) {
+                mappedButtons[CanonicalButtonIndex.RIGHT_TRIGGER] =
+                        (rawAxes[MotionEvent.AXIS_RZ] + 1) / 2;
+            } else {
+                mappedButtons[CanonicalButtonIndex.RIGHT_TRIGGER] = 0.f;
+            }
+
+            mapHatAxisToDpadButtons(mappedButtons, rawAxes);
+            mapXYAxes(mappedAxes, rawAxes);
+            mapRXAndRYAxesToRightStick(mappedAxes, rawAxes);
         }
     }
 
