06365918b76842feb1e8e5dd2170c71c32f8652d
Ken Rockot <rockot@chromium.org>
2017-07-28 18:22:38 +0000

    Reland "Reland "Mojo bindings: API to serialize struct -> Message""
    This is a reland of 7dd82d35fb83c709d11e4197727e0227a79bb744 [formerly a6a0615dfc0f68a2cad6ec8e1a60a775f0efa9c6]
    Original change's description:
    > Reland "Mojo bindings: API to serialize struct -> Message"
    > 
    > Reland of https://chromium-review.googlesource.com/c/584004, which
    > was reverted due to a failure on a Win7 builder still using VC.
    > 
    > The problem was an invalid call to std::vector::front() on a
    > potentially empty vector, which triggers an assertion in VC debug
    > builds but is silently ignored in clang builds.
    > 
    > This reland fixes the problem by avoiding said call when the
    > vector in question is empty.
    > 
    > BUG=747525
    > 
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: Icc098223b125260a944146729d128f6b6e80ffde
    > 
    > TBR=danakj@chromium.org
    > TBR=yzshen@chromium.org
    > 
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: Icc098223b125260a944146729d128f6b6e80ffde
    > Reviewed-on: https://chromium-review.googlesource.com/587406
    > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > Reviewed-by: Ken Rockot <rockot@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#490011}
    
    Bug: 747525
    Change-Id: Iee153883aff3f7285f6dc54912b4c360acd184a0
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    
    TBR=danakj@chromium.org
    TBR=yzshen@chromium.org
    
    Change-Id: Iee153883aff3f7285f6dc54912b4c360acd184a0
    Reviewed-on: https://chromium-review.googlesource.com/591787
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490470}
    Former-commit-id: 152536c49f55b8be4758892c0f44f336145cca67

--- cc/ipc/cc_serialization_perftest.cc
+++ cc/ipc/cc_serialization_perftest.cc
@@ -22,6 +22,7 @@
 #include "gpu/ipc/common/mailbox_struct_traits.h"
 #include "gpu/ipc/common/sync_token_struct_traits.h"
 #include "ipc/ipc_message.h"
+#include "mojo/public/cpp/bindings/message.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/perf/perf_test.h"
 #include "third_party/skia/include/effects/SkBlurImageFilter.h"
@@ -140,11 +141,11 @@ class CCSerializationPerfTest : public testing::Test {
       const std::string& test_name,
       const CompositorFrame& frame,
       UseSingleSharedQuadState single_sqs) {
-    auto data = mojom::CompositorFrame::Serialize(&frame);
-    DCHECK_GT(data.size(), 0u);
+    mojo::Message message = mojom::CompositorFrame::SerializeAsMessage(&frame);
     for (int i = 0; i < kNumWarmupRuns; ++i) {
       CompositorFrame compositor_frame;
-      mojom::CompositorFrame::Deserialize(data, &compositor_frame);
+      mojom::CompositorFrame::Deserialize(
+          message.payload(), message.payload_num_bytes(), &compositor_frame);
     }
 
     base::TimeTicks start = base::TimeTicks::Now();
@@ -156,7 +157,8 @@ class CCSerializationPerfTest : public testing::Test {
     while (start < end) {
       for (int i = 0; i < kTimeCheckInterval; ++i) {
         CompositorFrame compositor_frame;
-        mojom::CompositorFrame::Deserialize(data, &compositor_frame);
+        mojom::CompositorFrame::Deserialize(
+            message.payload(), message.payload_num_bytes(), &compositor_frame);
         now = base::TimeTicks::Now();
         // We don't count iterations after the end time.
         if (now < end)
@@ -188,8 +190,8 @@ class CCSerializationPerfTest : public testing::Test {
       const CompositorFrame& frame,
       UseSingleSharedQuadState single_sqs) {
     for (int i = 0; i < kNumWarmupRuns; ++i) {
-      auto data = mojom::CompositorFrame::Serialize(&frame);
-      DCHECK_GT(data.size(), 0u);
+      mojo::Message message =
+          mojom::CompositorFrame::SerializeAsMessage(&frame);
     }
 
     base::TimeTicks start = base::TimeTicks::Now();
@@ -200,8 +202,8 @@ class CCSerializationPerfTest : public testing::Test {
     size_t count = 0;
     while (start < end) {
       for (int i = 0; i < kTimeCheckInterval; ++i) {
-        auto data = mojom::CompositorFrame::Serialize(&frame);
-        DCHECK_GT(data.size(), 0u);
+        mojo::Message message =
+            mojom::CompositorFrame::SerializeAsMessage(&frame);
         now = base::TimeTicks::Now();
         // We don't count iterations after the end time.
         if (now < end)
