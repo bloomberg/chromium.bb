aa8e9ecd05ff007e21b261f2739ebce03a908d51
Francois Doray <fdoray@chromium.org>
2017-07-28 15:38:41 +0000

    Remove usage of ScopedTaskScheduler from translate_script_unittest.cc.
    ScopedTaskScheduler is deprecated.
    
    Bug: 708584
    Change-Id: I7e5e3aeaccdf1bcaea4bfe72421cb667d427e5ef
    Reviewed-on: https://chromium-review.googlesource.com/587008
    Reviewed-by: Rachel Blum <groby@chromium.org>
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490403}
    Former-commit-id: d5aa3b99e65fa36659b334db88da1dfb149fe22d

--- components/translate/core/browser/translate_script_unittest.cc
+++ components/translate/core/browser/translate_script_unittest.cc
@@ -8,7 +8,7 @@
 #include "base/command_line.h"
 #include "base/macros.h"
 #include "base/strings/stringprintf.h"
-#include "base/test/scoped_task_scheduler.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "build/build_config.h"
 #include "components/translate/core/browser/translate_download_manager.h"
@@ -53,7 +53,7 @@ class TranslateScriptTest : public testing::Test {
   }
 
   // Sets up the task scheduling/task-runner environment for each test.
-  base::test::ScopedTaskScheduler scoped_task_scheduler_;
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
   // The translate script.
   std::unique_ptr<TranslateScript> script_;
