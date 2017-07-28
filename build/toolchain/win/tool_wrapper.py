3bf1e169d07e9c73e17708fbe70822ad1785bfb5
Nico Weber <thakis@chromium.org>
2017-07-28 04:06:18 +0000

    win: Let linker append .manifest files instead of calling mt.exe
    This has a bunch of minor advantages:
    1. It's less code
    2. It's a build step less per windows_manifest
    3. rc.exe is no longer explicitly required in the build, which means
       just distributing lld-link is enough on non-Windows hosts,
       we don't need to distribute an mt-like program (assuming lld-link
       internally doesn't need to shell out to mt).
    No intended behavior change.
    
    Bug: 495204,749768
    Change-Id: Ie079b82bc6217bc85942827f2c2843bb893d7186
    Reviewed-on: https://chromium-review.googlesource.com/588387
    Commit-Queue: Nico Weber <thakis@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490255}
    Former-commit-id: 9ad0916c64be00ef8ba7f7f6a9b3c78813e17f21

--- build/toolchain/win/tool_wrapper.py
+++ build/toolchain/win/tool_wrapper.py
@@ -142,19 +142,6 @@ class WinTool(object):
         print line,
     return link.wait()
 
-  def ExecManifestWrapper(self, arch, *args):
-    """Run manifest tool with environment set. Strip out undesirable warning
-    (some XML blocks are recognized by the OS loader, but not the manifest
-    tool)."""
-    env = self._GetEnv(arch)
-    popen = subprocess.Popen(args, shell=True, env=env,
-                             stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    out, _ = popen.communicate()
-    for line in out.splitlines():
-      if line and 'manifest authoring warning 81010002' not in line:
-        print line
-    return popen.returncode
-
   def ExecMidlWrapper(self, arch, outdir, tlb, h, dlldata, iid, proxy, idl,
                       *flags):
     """Filter noisy filenames output from MIDL compile step that isn't
