6a4b22f2140224fac440fbca24f96a76dfaffd36
Gregory Chatzinoff <gchatz@chromium.org>
2017-07-28 05:29:06 +0000

    Add integration of metrics collection to dispatcher.
    This CL adds to the dispatcher the ability to register and deregister
    MetricsRecorders with a selector. Then, when that selector is invoked
    on the dispatcher, the dispatcher will notify the MetricsRecorder to
    record a metric. Since MetricsRecorders require an NSInvocation to
    record a metric, the dispatcher uses |forwardInvocation| for metrics
    registered selectors.
    
    Bug: none
    Change-Id: I3acd464251b8c6d6450a4f24dda2455d91b517d3
    Reviewed-on: https://chromium-review.googlesource.com/580641
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490284}
    Former-commit-id: 09aa06294d58a7e9e2a496a233f6ff96f1d44436

--- ios/shared/chrome/browser/ui/commands/command_dispatcher.mm
+++ ios/shared/chrome/browser/ui/commands/command_dispatcher.mm
@@ -10,6 +10,7 @@
 
 #include "base/logging.h"
 #include "base/strings/sys_string_conversions.h"
+#import "ios/shared/chrome/browser/ui/metrics/metrics_recorder.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
@@ -18,10 +19,13 @@
 @implementation CommandDispatcher {
   // Stores which target to forward to for a given selector.
   std::unordered_map<SEL, __weak id> _forwardingTargets;
+
+  // Stores which MetricsRecorder to notify for a given selector.
+  std::unordered_map<SEL, __weak id<MetricsRecorder>> _metricsRecorders;
 }
 
 - (void)startDispatchingToTarget:(id)target forSelector:(SEL)selector {
-  DCHECK(_forwardingTargets.find(selector) == _forwardingTargets.end());
+  DCHECK(![self targetForSelector:selector]);
 
   _forwardingTargets[selector] = target;
 }
@@ -69,24 +73,89 @@
   }
 }
 
+- (void)registerMetricsRecorder:(id<MetricsRecorder>)recorder
+                    forSelector:(SEL)selector {
+  DCHECK(![self metricsRecorderForSelector:selector]);
+
+  _metricsRecorders[selector] = recorder;
+}
+
+- (void)deregisterMetricsRecordingForSelector:(SEL)selector {
+  _metricsRecorders.erase(selector);
+}
+
 #pragma mark - NSObject
 
 // Overridden to forward messages to registered handlers.
 - (id)forwardingTargetForSelector:(SEL)selector {
-  auto target = _forwardingTargets.find(selector);
-  if (target != _forwardingTargets.end()) {
-    return target->second;
-  }
+  // If the selector is registered with a MetricsRecorder, return nil to force
+  // |forwardInvocation| to handle message forwarding. |forwardInvocation|
+  // provides an NSInvocation that is required by the MetricsRecorders.
+  if ([self metricsRecorderForSelector:selector])
+    return nil;
+
+  id target = [self targetForSelector:selector];
+  if (target)
+    return target;
+
   return [super forwardingTargetForSelector:selector];
 }
 
 // Overriden to return YES for any registered method.
 - (BOOL)respondsToSelector:(SEL)selector {
-  auto target = _forwardingTargets.find(selector);
-  if (target != _forwardingTargets.end()) {
+  if ([self targetForSelector:selector])
     return YES;
-  }
   return [super respondsToSelector:selector];
+}
+
+// Overriden to forward messages to registered handlers when an NSInvocation is
+// required.
+- (void)forwardInvocation:(NSInvocation*)anInvocation {
+  SEL selector = anInvocation.selector;
+
+  id<MetricsRecorder> recorder = [self metricsRecorderForSelector:selector];
+  if (recorder) {
+    [recorder recordMetricForInvocation:anInvocation];
+  }
+
+  id target = [self targetForSelector:selector];
+  if ([target respondsToSelector:selector]) {
+    [anInvocation invokeWithTarget:target];
+    return;
+  }
+
+  [super forwardInvocation:anInvocation];
+}
+
+// Overriden because overrides of |forwardInvocation| also require an override
+// of |methodSignatureForSelector|, as the method signature is needed to
+// construct NSInvocations.
+- (NSMethodSignature*)methodSignatureForSelector:(SEL)aSelector {
+  NSMethodSignature* signature = [super methodSignatureForSelector:aSelector];
+  if (signature)
+    return signature;
+
+  id target = [self targetForSelector:aSelector];
+  return [target methodSignatureForSelector:aSelector];
+}
+
+#pragma mark - Private
+
+// Returns the target registered to receive messeages for |selector|.
+- (id)targetForSelector:(SEL)selector {
+  auto target = _forwardingTargets.find(selector);
+  if (target == _forwardingTargets.end())
+    return nil;
+  return target->second;
+}
+
+// Returns the MetricsRecorder registered to be notified when |selector| is
+// invoked on the dispatcher.
+- (id<MetricsRecorder>)metricsRecorderForSelector:(SEL)selector {
+  auto recorder = _metricsRecorders.find(selector);
+  if (recorder == _metricsRecorders.end())
+    return nil;
+  return recorder->second;
 }
 
 @end
