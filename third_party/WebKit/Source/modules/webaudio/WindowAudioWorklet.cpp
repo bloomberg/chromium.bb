67e4e68860c94505016356675006e63d675c202b
Hongchan Choi <hongchan@chromium.org>
2017-07-29 02:19:46 +0000

    Add storage in AudioWorklet to manage BaseAudioContexts
    Add a storage in AudioWorklet to manage all the available
    BaseAudioContexts in Window. This is necessary because AudioWorklet
    notifies AudioContexts when the script is loaded into
    AudioWorkletGlobalScope.
    
    Change-Id: I36cd1b5e93f538008fb9cad006ecdd0f7c5d88e7
    Reviewed-on: https://chromium-review.googlesource.com/572329
    Commit-Queue: Hongchan Choi <hongchan@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490584}
    Former-commit-id: a41d41e9c64f6d1c68a54da7600aad2124d49cab

--- third_party/WebKit/Source/modules/webaudio/WindowAudioWorklet.cpp
+++ third_party/WebKit/Source/modules/webaudio/WindowAudioWorklet.cpp
@@ -7,6 +7,7 @@
 #include "core/dom/Document.h"
 #include "core/frame/LocalDOMWindow.h"
 #include "core/frame/LocalFrame.h"
+#include "modules/webaudio/BaseAudioContext.h"
 
 namespace blink {
 
@@ -14,6 +15,13 @@ AudioWorklet* WindowAudioWorklet::audioWorklet(LocalDOMWindow& window) {
   if (!window.GetFrame())
     return nullptr;
   return From(window).audio_worklet_.Get();
+}
+
+AudioWorklet* WindowAudioWorklet::audioWorklet(BaseAudioContext* context) {
+  LocalDOMWindow* window = context->GetExecutionContext()->ExecutingWindow();
+  if (!window->GetFrame())
+    return nullptr;
+  return From(*window).audio_worklet_.Get();
 }
 
 // Break the following cycle when the context gets detached.
