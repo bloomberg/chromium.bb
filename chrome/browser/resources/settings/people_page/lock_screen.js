7d7ba8da9361eda4d827b361d7b242616ba99450
Sammie Quon <sammiequon@google.com>
2017-07-29 01:20:10 +0000

    settings: Fix password prompt not displaying after timeout.
    Password prompt would not display because of a old function causing a console error which would then skip the show password prompt function.
    
    Test: manual
    Bug: 749866
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I235fa5de68b6c37b76f21bfb1d2262860b7543ed
    Reviewed-on: https://chromium-review.googlesource.com/590761
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490572}
    Former-commit-id: 626e1fa012bf38c990bbcb37fa028bbd9ec6266f

--- chrome/browser/resources/settings/people_page/lock_screen.js
+++ chrome/browser/resources/settings/people_page/lock_screen.js
@@ -235,7 +235,7 @@ Polymer({
   /** @private */
   onSetModesChanged_: function() {
     if (this.shouldAskForPassword_(settings.getCurrentRoute())) {
-      this.$.setupPin.close();
+      this.showSetupPinDialog_ = false;
       this.openPasswordPromptDialog_();
     }
   },
