1727bb862942046df90276d1083303a23cfabded
Toni Barzic <tbarzic@google.com>
2017-07-29 04:09:48 +0000

    Introduce swipe gesture launch for lock screen note taking
    Adds SwipeDetector to MD TopHeaderBar, and hooks it up to observe touch
    events originating on new-note action launcher element and detect
    swipe gesture. If the SwipeDetector detects a swipe originating from
    the new-note action element, lock UI requests lock screen note taking
    flow.
    
    Bug:737067
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I560eef22365a37bfd3c5cbb8cb56f48eb44e62cf
    Reviewed-on: https://chromium-review.googlesource.com/582211
    Reviewed-by: Alexander Alekseev <alemate@chromium.org>
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490620}
    Former-commit-id: a540de62c987e3024774901e975a418aa30ea0b7

--- chrome/browser/resources/chromeos/login/md_top_header_bar.js
+++ chrome/browser/resources/chromeos/login/md_top_header_bar.js
@@ -11,13 +11,6 @@
  */
 
 cr.define('login', function() {
-  /**
-   * Creates a header bar element shown at the top of the login screen.
-   *
-   * @constructor
-   * @extends {HTMLDivElement}
-   */
-  var TopHeaderBar = cr.ui.define('div');
 
   /**
    * Calculates diagonal length of a rectangle with the provided sides.
@@ -29,6 +22,310 @@ cr.define('login', function() {
     return Math.sqrt(x * x + y * y);
   }
 
+  /**
+   * Returns Manhattan distance between two touch events.
+   * @param {!TouchInfo} a
+   * @param {!TouchInfo} b
+   * @return !number The distance between two touch event (client) coordinates.
+   */
+  function manhattanDistance(a, b) {
+    return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+  }
+
+  /**
+   * The minimal Manhattan distance between two touch events for the touch to
+   * be considered as moved.
+   * @const {!number}
+   */
+  var TOUCH_MOVE_THRESHOLD = 1;
+
+  /**
+   * The time in milliseconds after which the current touch should be considered
+   * stopped.
+   * @const {!number}
+   */
+  var TOUCH_STOP_TIMEOUT_MS = 300;
+
+  /**
+   * Minimal velocity in px per ms for a swipe movement to be considered
+   * actionable.
+   * @const {!number}
+   */
+  var MIN_SWIPE_VELOCITY = 0.4;
+
+  /**
+   * Contains data from a touch event relevant for swipe detection.
+   * @param {!Touch} touch The wrapped touch event.
+   */
+  function TouchInfo(touch, timeStamp) {
+    /**
+     * The touch event horizontal client coordinate.
+     * @type {!number}
+     */
+    this.x = touch.clientX;
+
+    /**
+     * The touch event vertical client coordinate.
+     * @type {!number}
+     */
+    this.y = touch.clientY;
+
+    /**
+     * The time the touch event was recorded (and the TouchInfo was created).
+     * @type {!number}
+     */
+    this.time = Date.now();
+  }
+
+  /**
+   * Used to detect potential swipe gestures originating from the provided HTML
+   * element. It tracks touch events on the element and remembers the last
+   * touch move longer than TOUCH_MOVE_THRESHOLD. On touch gesture end, it
+   * invokes the callback with the velocity vector of the last movement that was
+   * long enough - the vector can then be used to determine if the last movement
+   * should be considered a swipe or not.
+   *
+   * @param  {!HTMLElement} element The element whose touch events should be
+   *     tracked.
+   * @param  {!function(!{x: !number, y: !number})} swipeCallback Called when
+   *     a swipe has been detected - the argument will be an object representing
+   *     2-D swipe movement velocity vector.
+   */
+  function SwipeDetector(element, swipeCallback) {
+    element.addEventListener('touchstart', this.onTouchMove_.bind(this));
+    element.addEventListener('touchend', this.onTouchEnd_.bind(this));
+    element.addEventListener('touchcancel', this.clearSwipe_.bind(this));
+    element.addEventListener('touchmove', this.onTouchMove_.bind(this));
+
+    /** @private {!function(!{x: !number, y: !number})} */
+    this.swipeCallback_ = swipeCallback;
+  }
+
+  SwipeDetector.prototype = {
+    /**
+     * The last detected movement of the tracked touch point between two points
+     * (start and end) that are more than <code>TOUCH_MOVE_THRESHOLD</code>
+     * apart. When the touch event sequence ends (on touchend event), this
+     * movement will define the vector that represents the final swipe gesture.
+     * @private {?{start: !TouchInfo, end: !TouchInfo}}
+     */
+    current_: null,
+
+    /**
+     * The last detected touch point. It is a candidate for start of the next
+     * touch movement (which is defined by this.current_). When the next touch
+     * event is received, if the received event is far enough from this touch,
+     * the vector defined by these two events will become the new this.current_
+     * value.
+     * @private {?TouchInfo}
+     */
+    nextStart_: null,
+
+    /**
+     * The tracked touch event ID. <code>undefined</code> if a touch is not
+     * being tracked.
+     * @private {number|undefined}
+     */
+    touchId_: undefined,
+
+    /**
+     * Reference to the timeout posted to invalidate current swipe touch data.
+     * If a timeout was not posted, it will be set to <code>undefined</code>.
+     * @private {number|undefined}
+     */
+    swipeTimeout_: undefined,
+
+    /**
+     * Whether swipe detector is enabled.
+     * @private {boolean}
+     */
+    enabled_: false,
+
+    /**
+     * @param {boolean} enabled Whether the swipe detector should be enabled.
+     */
+    setEnabled: function(enabled) {
+      if (this.enabled_ == enabled)
+        return;
+      if (!enabled)
+        this.clearSwipe_();
+      this.enabled_ = enabled;
+    },
+
+    /**
+     * Whether the swipe detector started tracking a touch event, and at least
+     * one (valid) touch event was detected.
+     * @return {boolean}
+     */
+    started_: function() {
+      return !!this.nextStart_;
+    },
+
+    /**
+     * Processes a detected touch start/move event, updating current swipe
+     * detector state - the last detected touch movement (i.e. the last two
+     * touch events that are distant enough to be considered a movement) and the
+     * last detected touch event.
+     * @param {!TouchInfo} touch The relevant touch event information.
+     */
+    addTouch_: function(touch) {
+      // If the new touch is distant enough from the last recorded touch event,
+      // update the current movement (potential swipe).
+      if (this.nextStart_ &&
+          manhattanDistance(this.nextStart_, touch) >= TOUCH_MOVE_THRESHOLD) {
+        this.current_ = {start: this.nextStart_, end: touch};
+      }
+      // Use the touch as the potential start of the next movement.
+      this.nextStart_ = touch;
+    },
+
+    /**
+     * Handles swipe timeout - if there is a current detected swipe, it gets
+     * cleared if it did not start within the TOUCH_STOP_TIMEOUT_MS amount of
+     * time from now. Similarly, if recorded potential start of the next is not
+     * within the required time, it gets cleared as well.
+     * @private
+     */
+    handleSwipeTimeout_: function() {
+      this.swipeTimeout_ = undefined;
+
+      if (!this.started_())
+        return;
+
+      /**
+       * @param {!number} The time stamp of touch event.
+       * @param {!number} The current time stamp.
+       * @return Whether touchTime is within the valid time interval from now.
+       */
+      function isTouchTimeValid(touchTime, now) {
+        var timeDelta = now - touchTime;
+        return timeDelta >= 0 && timeDelta < TOUCH_STOP_TIMEOUT_MS;
+      }
+
+      // Detect whether detected events are within valid time interval, and
+      // record the latest detected event timestamp.
+      var now = Date.now();
+      var timeSinceEvent = -1;
+      if (this.current_ && isTouchTimeValid(this.current_.start.time, now)) {
+        timeSinceEvent = now - this.current_.start.time;
+      } else if (isTouchTimeValid(this.nextStart_.time, now)) {
+        this.current_ = null;
+        timeSinceEvent = now - this.nextStart_.time;
+      }
+
+      if (timeSinceEvent < 0 || timeSinceEvent > TOUCH_STOP_TIMEOUT_MS) {
+        this.clearSwipe_();
+        return;
+      }
+
+      // Schedule the next interval to be |TOUCH_STOP_TIMEOUT_MS| from the
+      // latest detected touch event.
+      this.swipeTimeout_ = setTimeout(
+          this.handleSwipeTimeout_.bind(this),
+          TOUCH_STOP_TIMEOUT_MS - timeSinceEvent);
+    },
+
+    /**
+     * Cancels the currently tracked movement, and resets cached touch data.
+     * @private
+     */
+    clearSwipe_: function() {
+      if (this.swipeTimeout_ !== undefined)
+        clearTimeout(this.swipeTimeout_);
+      this.swipeTimeout_ = undefined;
+      this.touchId_ = undefined;
+      this.current_ = null;
+      this.nextStart_ = null;
+    },
+
+    /**
+     * Handler for touch move and start events.
+     * @param {TouchEvent} e
+     * @private
+     */
+    onTouchMove_: function(e) {
+      if (!this.enabled_)
+        return;
+
+      // Cancel touch movement tracking if there is more than one touch point,
+      // or if the current touch point is not associated with the tracked
+      // element.
+      if (e.touches.length != 1 || e.targetTouches.length != 1) {
+        this.clearSwipe_();
+        return;
+      }
+
+      var touch = e.touches.item(0);
+      // Start/restart movement tracking if the touch event sequence is not
+      // being tracked, or the touch pointer that should be tracked changed.
+      if (!this.started_() || touch.identifier != this.touchId_) {
+        this.clearSwipe_();
+
+        // Invalidate the current detected swipe after a timeout - for example
+        // to handle situation where the touch stops moving immediately after
+        // a swipe, and the touch is ended without further movement after some
+        // time.
+        this.swipeTimeout_ = setTimeout(
+            this.handleSwipeTimeout_.bind(this), TOUCH_STOP_TIMEOUT_MS);
+
+        this.touchId_ = touch.identifier;
+      }
+
+      this.addTouch_(new TouchInfo(touch));
+    },
+
+    /**
+     * Handler for touch end event.
+     * @param {TouchEvent} e
+     * @private
+     */
+    onTouchEnd_: function(e) {
+      if (!this.enabled_)
+        return;
+
+      // Cancel the swipe if there are still other touch contact points when the
+      // touch ends, or if the finished touch is not associated with the
+      // currently tracked touch sequence.
+      if (e.touches.length != 0 || e.changedTouches.length > 1 ||
+          e.changedTouches.item(0).identifier != this.touchId_) {
+        this.clearSwipe_();
+        return;
+      }
+
+      // Process the final touch event.
+      this.addTouch_(new TouchInfo(e.changedTouches.item(0)));
+
+      // Remember the last potential swipe movement, and clear the tracked
+      // touch data.
+      var current = this.current_;
+      this.clearSwipe_();
+
+      if (!current)
+        return;
+
+      var velocity = {
+        x: current.end.x - current.start.x,
+        y: current.end.y - current.start.y
+      };
+      if (!velocity.x && !velocity.y)
+        return;
+
+      var time = current.end.time - current.start.time;
+      if (!time)
+        return;
+
+      this.swipeCallback_({x: velocity.x / time, y: velocity.y / time});
+    }
+  };
+
+  /**
+   * Creates a header bar element shown at the top of the login screen.
+   *
+   * @constructor
+   * @extends {HTMLDivElement}
+   */
+  var TopHeaderBar = cr.ui.define('div');
+
   TopHeaderBar.prototype = {
     __proto__: HTMLDivElement.prototype,
 
@@ -37,6 +334,9 @@ cr.define('login', function() {
      * @private {!LockScreenAppsState}
      */
     lockScreenAppsState_: LOCK_SCREEN_APPS_STATE.NONE,
+
+    /** @private {SwipeDetector} */
+    swipeDetector_: null,
 
     set lockScreenAppsState(state) {
       if (this.lockScreenAppsState_ == state)
@@ -53,6 +353,8 @@ cr.define('login', function() {
       $('new-note-action')
           .addEventListener(
               'keydown', this.handleNewNoteActionKeyDown_.bind(this));
+      this.swipeDetector_ =
+          new SwipeDetector($('new-note-action'), this.handleSwipe_.bind(this));
     },
 
     /**
@@ -74,6 +376,9 @@ cr.define('login', function() {
               .classList.toggle('new-note-action-short-transition', false);
         });
       }
+
+      this.swipeDetector_.setEnabled(
+          this.lockScreenAppsState_ == LOCK_SCREEN_APPS_STATE.AVAILABLE);
 
       $('top-header-bar')
           .classList.toggle(
@@ -119,6 +424,25 @@ cr.define('login', function() {
     handleNewNoteActionKeyDown_: function(evt) {
       if (evt.code != 'Enter')
         return;
+      this.activateNoteAction_();
+    },
+
+    /**
+     * Handles a detected touch movement event that might be considered a swipe.
+     * @param {!{x: !number, y: number}} velocity The touch movement velocity
+     *     vector.
+     * @private
+     */
+    handleSwipe_: function(velocity) {
+      // Ignore swipes in direction other than down left (in ltr world).
+      var rtlAdjustmentForX = isRTL() ? -1 : 1;
+      if ((rtlAdjustmentForX * velocity.x) > 0 || velocity.y < 0)
+        return;
+
+      // If not fast enough, ignore the swipe.
+      if (diag(velocity.x, velocity.y) < MIN_SWIPE_VELOCITY)
+        return;
+
       this.activateNoteAction_();
     },
 
