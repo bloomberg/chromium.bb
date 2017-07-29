5a0133f7891e5c2b11ae647a890752c108d991a8
Kenneth Russell <kbr@chromium.org>
2017-07-29 14:15:40 +0000

    Remove disable_multimonitor_multisampling driver bug workaround.
    The associated code has been removed for some time and its continued
    presence is confusing.
    
    BUG=237931
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I4c2ed46f7e8882958b6e533633a60f9afe04ce36
    Reviewed-on: https://chromium-review.googlesource.com/592713
    Reviewed-by: Victor Miura <vmiura@chromium.org>
    Commit-Queue: Kenneth Russell <kbr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490652}
    Former-commit-id: 86bac785154c489aa49f9e7037b19d3b294ec3a4

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -73,8 +73,6 @@
          disable_gl_rgb_format)                              \
   GPU_OP(DISABLE_LARGER_THAN_SCREEN_OVERLAYS,                \
          disable_larger_than_screen_overlays)                \
-  GPU_OP(DISABLE_MULTIMONITOR_MULTISAMPLING,                 \
-         disable_multimonitor_multisampling)                 \
   GPU_OP(DISABLE_NON_EMPTY_POST_SUB_BUFFERS_FOR_ONSCREEN_SURFACES, \
          disable_non_empty_post_sub_buffers_for_onscreen_surfaces) \
   GPU_OP(DISABLE_NV12_DXGI_VIDEO,                            \
