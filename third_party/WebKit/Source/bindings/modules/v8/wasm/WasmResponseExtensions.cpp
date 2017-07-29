fa515e6539bd78a980f258fd37b86c2111452fc4
Mircea Trofin <mtrofin@chromium.org>
2017-07-29 16:50:06 +0000

    Revert "Revert "[wasm] Response APIs: use promise-based module building API""
    This reverts commit d35b92f20f84f451f16883e74983211cb6aaf877 [formerly 0278717e4820176809cdc1fedd1c336167aba442].
    
    Reason for revert: Fixed leak by moving ownership of promise (when
    streaming has started) on the V8 side. Blink is still responsible, and
    owns, promises in setup error cases (like invalid MIME).
    
    Original change's description:
    > Revert "[wasm] Response APIs: use promise-based module building API"
    > 
    > This reverts commit 6e479b952ab9979497824e4e082d9fd73f2847bd [formerly 2f3d31477fc9b4e0f41242e7a63f0be4ee55d310].
    > 
    > Reason for revert: Suspected cause of failures on WebKit Linux Trusty Leak bot :
    > Unexpected Failures:
    > * http/tests/wasm_streaming/wasm_response_apis.html
    > * virtual/enable_wasm_streaming/http/tests/wasm_streaming/wasm_response_apis.html
    > * virtual/mojo-loading/http/tests/wasm_streaming/wasm_response_apis.html
    > 
    > First failure:
    > https://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%20Trusty%20Leak/builds/7364
    > 
    > Original change's description:
    > > [wasm] Response APIs: use promise-based module building API
    > > 
    > > The first implementation of the response-based APIs used
    > > sync compile as an implementation detail. As we're getting close
    > > to having a true streaming implementation, we're switching to
    > > a promise-based API, closer in shape and implementation to the
    > > final design.
    > > 
    > > What differs from true streaming is that the implementation, just
    > > like before this change, still pre buffers data before starting
    > > compilation. The difference in this CL is that compilation is
    > > asynchronous, and is responsible for resolving/rejecting the promise.
    > > 
    > > The case of network error is where we expect differences in the final
    > > implementation. Currently, no compilation is started, and the
    > > blink side rejects the promise. When we move to true streaming, 
    > > we'll need to explicitly signal to the v8 side when the download is 
    > > dropped, and probably let it (v8 side) reject the promise.
    > > 
    > > Also added a regression test for v8:6619, which is implicitly fixed by
    > > the new design.
    > >  
    > > 
    > > Bug: chromium:747396
    > > Bug: v8:6619
    > > Change-Id: Ic47c948a7cd001e07926ee4d0ae70669bab680bb
    > > Reviewed-on: https://chromium-review.googlesource.com/580788
    > > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > > Commit-Queue: Mircea Trofin <mtrofin@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#490111}
    > 
    > TBR=bradnelson@chromium.org,haraken@chromium.org,mtrofin@chromium.org,ahaas@chromium.org
    > 
    > Change-Id: I6a7fdb9b3d49d2fb26bc070f99ba6d4247f0f04f
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: chromium:747396, v8:6619
    > Reviewed-on: https://chromium-review.googlesource.com/590389
    > Commit-Queue: meade_UTC10 <meade@chromium.org>
    > Reviewed-by: meade_UTC10 <meade@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#490175}
    
    TBR=bradnelson@chromium.org,haraken@chromium.org,mtrofin@chromium.org,ahaas@chromium.org,meade@chromium.org
    
    Change-Id: I45d883fabf294efdcab87e5eb18ea7879a054aa9
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:747396, v8:6619
    Reviewed-on: https://chromium-review.googlesource.com/591053
    Commit-Queue: Mircea Trofin <mtrofin@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490663}
    Former-commit-id: d36bea9f66b9bcb2543f4f541c929da1c49b99cc

--- third_party/WebKit/Source/bindings/modules/v8/wasm/WasmResponseExtensions.cpp
+++ third_party/WebKit/Source/bindings/modules/v8/wasm/WasmResponseExtensions.cpp
@@ -24,11 +24,8 @@ class FetchDataLoaderAsWasmModule final : public FetchDataLoader,
   USING_GARBAGE_COLLECTED_MIXIN(FetchDataLoaderAsWasmModule);
 
  public:
-  FetchDataLoaderAsWasmModule(ScriptPromiseResolver* resolver,
-                              ScriptState* script_state)
-      : resolver_(resolver),
-        builder_(script_state->GetIsolate()),
-        script_state_(script_state) {}
+  FetchDataLoaderAsWasmModule(ScriptState* script_state)
+      : builder_(script_state->GetIsolate()), script_state_(script_state) {}
 
   void Start(BytesConsumer* consumer,
              FetchDataLoader::Client* client) override {
@@ -39,6 +36,8 @@ class FetchDataLoaderAsWasmModule final : public FetchDataLoader,
     consumer_->SetClient(this);
     OnStateChange();
   }
+
+  v8::Local<v8::Promise> GetPromise() { return builder_.GetPromise(); }
 
   void OnStateChange() override {
     while (true) {
@@ -65,32 +64,13 @@ class FetchDataLoaderAsWasmModule final : public FetchDataLoader,
           break;
         }
         case BytesConsumer::Result::kDone: {
-          v8::Isolate* isolate = script_state_->GetIsolate();
           ScriptState::Scope scope(script_state_.Get());
-
-          {
-            // The TryCatch destructor will clear the exception. We
-            // scope the block here to ensure tight control over the
-            // lifetime of the exception.
-            v8::TryCatch trycatch(isolate);
-            v8::Local<v8::WasmCompiledModule> module;
-            if (builder_.Finish().ToLocal(&module)) {
-              DCHECK(!trycatch.HasCaught());
-              ScriptValue script_value(script_state_.Get(), module);
-              resolver_->Resolve(script_value);
-            } else {
-              DCHECK(trycatch.HasCaught());
-              resolver_->Reject(trycatch.Exception());
-            }
-          }
-
+          builder_.Finish();
           client_->DidFetchDataLoadedCustomFormat();
           return;
         }
         case BytesConsumer::Result::kError: {
-          // TODO(mtrofin): do we need an abort on the wasm side?
-          // Something like "m_outStream->abort()" maybe?
-          return RejectPromise();
+          return AbortCompilation();
         }
       }
     }
@@ -98,12 +78,11 @@ class FetchDataLoaderAsWasmModule final : public FetchDataLoader,
 
   void Cancel() override {
     consumer_->Cancel();
-    return RejectPromise();
+    return AbortCompilation();
   }
 
   DEFINE_INLINE_TRACE() {
     visitor->Trace(consumer_);
-    visitor->Trace(resolver_);
     visitor->Trace(client_);
     FetchDataLoader::Trace(visitor);
     BytesConsumer::Client::Trace(visitor);
@@ -112,14 +91,13 @@ class FetchDataLoaderAsWasmModule final : public FetchDataLoader,
  private:
   // TODO(mtrofin): replace with spec-ed error types, once spec clarifies
   // what they are.
-  void RejectPromise() {
-    resolver_->Reject(V8ThrowException::CreateTypeError(
+  void AbortCompilation() {
+    builder_.Abort(V8ThrowException::CreateTypeError(
         script_state_->GetIsolate(), "Could not download wasm module"));
   }
   Member<BytesConsumer> consumer_;
-  Member<ScriptPromiseResolver> resolver_;
   Member<FetchDataLoader::Client> client_;
-  v8::WasmModuleObjectBuilder builder_;
+  v8::WasmModuleObjectBuilderStreaming builder_;
   const RefPtr<ScriptState> script_state_;
 };
 
@@ -180,29 +158,30 @@ void CompileFromResponseCallback(
             .V8Value());
     return;
   }
-  ScriptPromise promise;
+  v8::Local<v8::Value> promise;
   if (response->IsBodyLocked() || response->bodyUsed()) {
-    promise = ScriptPromise::Reject(
-        script_state,
-        V8ThrowException::CreateTypeError(
-            script_state->GetIsolate(),
-            "Cannot compile WebAssembly.Module from an already read Response"));
+    promise = ScriptPromise::Reject(script_state,
+                                    V8ThrowException::CreateTypeError(
+                                        script_state->GetIsolate(),
+                                        "Cannot compile WebAssembly.Module "
+                                        "from an already read Response"))
+                  .V8Value();
   } else {
-    ScriptPromiseResolver* resolver =
-        ScriptPromiseResolver::Create(script_state);
     if (response->BodyBuffer()) {
-      promise = resolver->Promise();
-      response->BodyBuffer()->StartLoading(
-          new FetchDataLoaderAsWasmModule(resolver, script_state),
-          new WasmDataLoaderClient());
+      FetchDataLoaderAsWasmModule* loader =
+          new FetchDataLoaderAsWasmModule(script_state);
+
+      promise = loader->GetPromise();
+      response->BodyBuffer()->StartLoading(loader, new WasmDataLoaderClient());
     } else {
-      promise = ScriptPromise::Reject(
-          script_state,
-          V8ThrowException::CreateTypeError(
-              script_state->GetIsolate(), "Response object has a null body."));
+      promise = ScriptPromise::Reject(script_state,
+                                      V8ThrowException::CreateTypeError(
+                                          script_state->GetIsolate(),
+                                          "Response object has a null body."))
+                    .V8Value();
     }
   }
-  V8SetReturnValue(args, promise.V8Value());
+  V8SetReturnValue(args, promise);
 }
 
 // See https://crbug.com/708238 for tracking avoiding the hand-generated code.
