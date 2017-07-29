b48986885a1a707405109c51fb3252c1c8e276f0
Alexander Alekseev <alemate@chromium.org>
2017-07-29 06:37:07 +0000

    ChromeOS OOBE: Implement Advanced options menu screen
    This Cl adds Advanced options menu screen to ChromeOS OOBE.
    
    Bug: 709263
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I8c759981d525f15c8c9b90cb11f5c29d9b813c7d
    Reviewed-on: https://chromium-review.googlesource.com/591049
    Commit-Queue: Alexander Alekseev <alemate@chromium.org>
    Reviewed-by: Michael Giuffrida <michaelpg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490638}
    Former-commit-id: 3fc306c49007119cb9582cbb5ce4b147de0867c9

--- chrome/browser/resources/chromeos/login/oobe_welcome_dialog.js
+++ chrome/browser/resources/chromeos/login/oobe_welcome_dialog.js
@@ -2,86 +2,158 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-Polymer({
-  is: 'oobe-welcome-dialog',
-  properties: {
+{
+  var LONG_TOUCH_TIME_MS = 1000;
+
+  function LongTouchDetector(element, callback) {
+    this.callback_ = callback;
+
+    element.addEventListener('touchstart', this.onTouchStart_.bind(this));
+    element.addEventListener('touchend', this.killTimer_.bind(this));
+    element.addEventListener('touchcancel', this.killTimer_.bind(this));
+
+    element.addEventListener('mousedown', this.onTouchStart_.bind(this));
+    element.addEventListener('mouseup', this.killTimer_.bind(this));
+    element.addEventListener('mouseleave', this.killTimer_.bind(this));
+  }
+
+  LongTouchDetector.prototype = {
     /**
-     * Currently selected system language (display name).
+     * This is timeout ID used to kill window timeout that fires "detected"
+     * callback if touch event was cancelled.
+     *
+     * @private {number|null}
      */
-    currentLanguage: {
-      type: String,
-      value: '',
+    timeoutId_: null,
+
+    /**
+     *  window.setTimeout() callback.
+     *
+     * @private
+     */
+    onTimeout_: function() {
+      this.killTimer_();
+      this.callback_();
     },
 
     /**
-     * Controls visibility of "Timezone" button.
+     * @private
      */
-    timezoneButtonVisible: {
-      type: Boolean,
-      value: false,
+    onTouchStart_: function() {
+      this.killTimer_();
+      this.timeoutId_ = window.setTimeout(
+          this.onTimeout_.bind(this, this.attempt_), LONG_TOUCH_TIME_MS);
     },
 
     /**
-     * Controls displaying of "Enable debugging features" link.
+     * @private
      */
-    debuggingLinkVisible: Boolean,
-  },
+    killTimer_: function() {
+      if (this.timeoutId_ === null)
+        return;
 
-  /**
-   * This is stored ID of currently focused element to restore id on returns
-   * to this dialog from Language / Timezone Selection dialogs.
-   */
-  focusedElement_: 'languageSelectionButton',
+      window.clearTimeout(this.timeoutId_);
+      this.timeoutId_ = null;
+    },
+  };
 
-  onLanguageClicked_: function() {
-    this.focusedElement_ = 'languageSelectionButton';
-    this.fire('language-button-clicked');
-  },
+  Polymer({
+    is: 'oobe-welcome-dialog',
+    properties: {
+      /**
+       * Currently selected system language (display name).
+       */
+      currentLanguage: {
+        type: String,
+        value: '',
+      },
 
-  onAccessibilityClicked_: function() {
-    this.focusedElement_ = 'accessibilitySettingsButton';
-    this.fire('accessibility-button-clicked');
-  },
+      /**
+       * Controls visibility of "Timezone" button.
+       */
+      timezoneButtonVisible: {
+        type: Boolean,
+        value: false,
+      },
 
-  onTimezoneClicked_: function() {
-    this.focusedElement_ = 'timezoneSettingsButton';
-    this.fire('timezone-button-clicked');
-  },
+      /**
+       * Controls displaying of "Enable debugging features" link.
+       */
+      debuggingLinkVisible: Boolean,
+    },
 
-  onNextClicked_: function() {
-    this.focusedElement_ = 'welcomeNextButton';
-    this.fire('next-button-clicked');
-  },
+    /**
+     * @private {LongTouchDetector}
+     */
+    titleLongTouchDetector_: null,
 
-  onDebuggingLinkClicked_: function() {
-    chrome.send(
-        'login.NetworkScreen.userActed', ['connect-debugging-features']);
-  },
+    /**
+     * This is stored ID of currently focused element to restore id on returns
+     * to this dialog from Language / Timezone Selection dialogs.
+     */
+    focusedElement_: 'languageSelectionButton',
 
-  attached: function() {
-    this.focus();
-  },
+    onLanguageClicked_: function() {
+      this.focusedElement_ = 'languageSelectionButton';
+      this.fire('language-button-clicked');
+    },
 
-  focus: function() {
-    var focusedElement = this.$[this.focusedElement_];
-    if (focusedElement)
-      focusedElement.focus();
-  },
+    onAccessibilityClicked_: function() {
+      this.focusedElement_ = 'accessibilitySettingsButton';
+      this.fire('accessibility-button-clicked');
+    },
 
-  /**
-   * This is called from oobe_welcome when this dialog is shown.
-   */
-  show: function() {
-    this.focus();
-  },
+    onTimezoneClicked_: function() {
+      this.focusedElement_ = 'timezoneSettingsButton';
+      this.fire('timezone-button-clicked');
+    },
 
-  /**
-   * This function formats message for labels.
-   * @param String label i18n string ID.
-   * @param String parameter i18n string parameter.
-   * @private
-   */
-  formatMessage_: function(label, parameter) {
-    return loadTimeData.getStringF(label, parameter);
-  },
-});
+    onNextClicked_: function() {
+      this.focusedElement_ = 'welcomeNextButton';
+      this.fire('next-button-clicked');
+    },
+
+    onDebuggingLinkClicked_: function() {
+      chrome.send(
+          'login.NetworkScreen.userActed', ['connect-debugging-features']);
+    },
+
+    /*
+     * This is called from titleLongTouchDetector_ when long touch is detected.
+     *
+     * @private
+     */
+    onTitleLongTouch_: function() {
+      this.fire('launch-advanced-options');
+    },
+
+    attached: function() {
+      this.titleLongTouchDetector_ = new LongTouchDetector(
+          this.$.title, this.onTitleLongTouch_.bind(this));
+      this.focus();
+    },
+
+    focus: function() {
+      var focusedElement = this.$[this.focusedElement_];
+      if (focusedElement)
+        focusedElement.focus();
+    },
+
+    /**
+     * This is called from oobe_welcome when this dialog is shown.
+     */
+    show: function() {
+      this.focus();
+    },
+
+    /**
+     * This function formats message for labels.
+     * @param String label i18n string ID.
+     * @param String parameter i18n string parameter.
+     * @private
+     */
+    formatMessage_: function(label, parameter) {
+      return loadTimeData.getStringF(label, parameter);
+    },
+  });
+}
