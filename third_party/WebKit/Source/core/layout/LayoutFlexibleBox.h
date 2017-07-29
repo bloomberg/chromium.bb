b95a6f896c7c7fbdc7f993bd42941a4248a22ae8
Christian Biesinger <cbiesinger@chromium.org>
2017-07-29 07:26:19 +0000

    [css-flexbox] Move ComputeLineItemsPosition to FlexLine
    This should now be sufficient to allow an initial version of Flexbox-in-LayoutNG!
    
    Change-Id: I0c9c5f330e90e85705cdbc3eb0cfb26b24d0fbf3
    Reviewed-on: https://chromium-review.googlesource.com/592691
    Commit-Queue: Christian Biesinger <cbiesinger@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490644}
    Former-commit-id: 65b35725c48bc658fe53911f3d248a68393ecf0e

--- third_party/WebKit/Source/core/layout/LayoutFlexibleBox.h
+++ third_party/WebKit/Source/core/layout/LayoutFlexibleBox.h
@@ -156,8 +156,6 @@ class CORE_EXPORT LayoutFlexibleBox : public LayoutBlock {
   void LayoutFlexItems(bool relayout_children, SubtreeLayoutScope&);
   LayoutUnit AutoMarginOffsetInMainAxis(const Vector<FlexItem>&,
                                         LayoutUnit& available_free_space);
-  void UpdateAutoMarginsInMainAxis(LayoutBox& child,
-                                   LayoutUnit auto_margin_offset);
   bool HasAutoMarginsInCrossAxis(const LayoutBox& child) const;
   bool UpdateAutoMarginsInCrossAxis(LayoutBox& child,
                                     LayoutUnit available_alignment_space);
@@ -181,9 +179,6 @@ class CORE_EXPORT LayoutFlexibleBox : public LayoutBlock {
                                               LayoutUnit child_preferred_size);
   void PrepareChildForPositionedLayout(LayoutBox& child);
   void LayoutLineItems(FlexLine*, bool relayout_children, SubtreeLayoutScope&);
-  void ComputeLineItemsPosition(LayoutUnit& cross_axis_offset,
-                                FlexLine*,
-                                LayoutUnit main_axis_offset);
   void ApplyLineItemsPosition(FlexLine*);
   void LayoutColumnReverse(Vector<FlexItem>&,
                            LayoutUnit cross_axis_offset,
