e7ca49228e5e442385e3456719c8f285c49266d6
Alex Clarke <alexclarke@chromium.org>
2017-07-31 09:39:50 +0000

    DOMSnapshot.getSnapshot to expose the document character encoding format
    Bug: 
    Change-Id: I44f80c51d5e96816bf937eb46f000c0fdcfe271f
    Reviewed-on: https://chromium-review.googlesource.com/591670
    Commit-Queue: Alex Clarke <alexclarke@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490736}
    Former-commit-id: 7f1ee9842fd803152fba630c747e5ccf6999d714

--- third_party/WebKit/Source/core/inspector/InspectorDOMSnapshotAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorDOMSnapshotAgent.cpp
@@ -246,6 +246,8 @@ int InspectorDOMSnapshotAgent::VisitNode(Node* node) {
     value->setBaseURL(InspectorDOMAgent::DocumentBaseURLString(document));
     if (document->ContentLanguage())
       value->setContentLanguage(document->ContentLanguage().Utf8().data());
+    if (document->EncodingName())
+      value->setDocumentEncoding(document->EncodingName().Utf8().data());
   } else if (node->IsDocumentTypeNode()) {
     DocumentType* doc_type = ToDocumentType(node);
     value->setPublicId(doc_type->publicId());
