1e6dbebf5923a3f07535f5953a201d8f0bcad577
Kunihiko Sakamoto <ksakamoto@chromium.org>
2017-07-31 03:19:53 +0000

    Add UMA to track the time yielded between content script runs
    This will be used to understand the effect of YieldBetweenContentScriptRuns
    experiment on page load time.
    
    Bug: 636655
    Change-Id: I7978daa7361c73ba5b835dbc21402b8cb7d73ae3
    Reviewed-on: https://chromium-review.googlesource.com/571246
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Kunihiko Sakamoto <ksakamoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490701}
    Former-commit-id: 7d386bf120192ae5aaeea88209f9267edc4be6f8

--- extensions/renderer/script_injection_manager.cc
+++ extensions/renderer/script_injection_manager.cc
@@ -21,6 +21,7 @@
 #include "extensions/common/extension_messages.h"
 #include "extensions/common/extension_set.h"
 #include "extensions/common/feature_switch.h"
+#include "extensions/renderer/async_scripts_run_info.h"
 #include "extensions/renderer/extension_frame_helper.h"
 #include "extensions/renderer/extension_injection_host.h"
 #include "extensions/renderer/programmatic_script_injector.h"
@@ -415,6 +416,10 @@ void ScriptInjectionManager::InjectScripts(
   active_injection_frames_.insert(frame);
 
   ScriptsRunInfo scripts_run_info(frame, run_location);
+  scoped_refptr<AsyncScriptsRunInfo> async_run_info;
+  if (FeatureSwitch::yield_between_content_script_runs()->IsEnabled())
+    async_run_info = base::MakeRefCounted<AsyncScriptsRunInfo>(run_location);
+
   for (auto iter = frame_injections.begin(); iter != frame_injections.end();) {
     // It's possible for the frame to be invalidated in the course of injection
     // (if a script removes its own frame, for example). If this happens, abort.
@@ -422,7 +427,8 @@ void ScriptInjectionManager::InjectScripts(
       break;
     std::unique_ptr<ScriptInjection> injection(std::move(*iter));
     iter = frame_injections.erase(iter);
-    TryToInject(std::move(injection), run_location, &scripts_run_info);
+    TryToInject(std::move(injection), run_location, &scripts_run_info,
+                async_run_info);
   }
 
   // We are done running in the frame.
@@ -434,15 +440,15 @@ void ScriptInjectionManager::InjectScripts(
 void ScriptInjectionManager::TryToInject(
     std::unique_ptr<ScriptInjection> injection,
     UserScript::RunLocation run_location,
-    ScriptsRunInfo* scripts_run_info) {
+    ScriptsRunInfo* scripts_run_info,
+    scoped_refptr<AsyncScriptsRunInfo> async_run_info) {
   // Try to inject the script. If the injection is waiting (i.e., for
   // permission), add it to the list of pending injections. If the injection
   // has blocked, add it to the list of running injections.
   // The Unretained below is safe because this object owns all the
   // ScriptInjections, so is guaranteed to outlive them.
   switch (injection->TryToInject(
-      run_location,
-      scripts_run_info,
+      run_location, scripts_run_info, std::move(async_run_info),
       base::Bind(&ScriptInjectionManager::OnInjectionFinished,
                  base::Unretained(this)))) {
     case ScriptInjection::INJECTION_WAITING:
@@ -480,7 +486,7 @@ void ScriptInjectionManager::HandleExecuteCode(
       iter == frame_statuses_.end() ? UserScript::UNDEFINED : iter->second;
 
   ScriptsRunInfo scripts_run_info(render_frame, run_location);
-  TryToInject(std::move(injection), run_location, &scripts_run_info);
+  TryToInject(std::move(injection), run_location, &scripts_run_info, nullptr);
 }
 
 void ScriptInjectionManager::HandleExecuteDeclarativeScript(
@@ -496,7 +502,7 @@ void ScriptInjectionManager::HandleExecuteDeclarativeScript(
     ScriptsRunInfo scripts_run_info(render_frame, UserScript::BROWSER_DRIVEN);
     // TODO(markdittmer): Use return value of TryToInject for error handling.
     TryToInject(std::move(injection), UserScript::BROWSER_DRIVEN,
-                &scripts_run_info);
+                &scripts_run_info, nullptr);
 
     scripts_run_info.LogRun(activity_logging_enabled_);
   }
