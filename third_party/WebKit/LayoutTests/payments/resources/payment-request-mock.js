de1555343ed1f8a6831a18b910eb5e0fe8fa6381
Reilly Grant <reillyg@chromium.org>
2017-08-01 19:28:11 +0000

    Switch Payments LayoutTest to the new Mojo JS bindings
    This change rewrites the single Payments API layout test that uses Mojo
    mock services to use the new Mojo JS bindings.
    
    Bug: 699569
    Change-Id: I6fa74bfb2e926d35ccf3232c87f7a0114592ce9a
    Reviewed-on: https://chromium-review.googlesource.com/595246
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491080}
    Former-commit-id: e36efa6b8d50503c355010612dab64fe421cbc35

--- third_party/WebKit/LayoutTests/payments/resources/payment-request-mock.js
+++ third_party/WebKit/LayoutTests/payments/resources/payment-request-mock.js
@@ -4,49 +4,44 @@
 
 "use strict";
 
-let paymentRequestMock = loadMojoModules(
-  'paymentRequestMock', ['third_party/WebKit/public/platform/modules/payments/payment_request.mojom',
-    'mojo/public/js/bindings',
-  ]).then(mojo => {
-  let [paymentRequest, bindings] = mojo.modules;
+class PaymentRequestMock {
+  constructor() {
+    this.pendingResponse_ = null;
+    this.bindings_ = new mojo.BindingSet(payments.mojom.PaymentRequest);
 
-  class PaymentRequestMock {
-    constructor(interfaceProvider) {
-      interfaceProvider.addInterfaceOverrideForTesting(
-        paymentRequest.PaymentRequest.name,
-        handle => this.bindings_.addBinding(this, handle));
+    this.interceptor_ = new MojoInterfaceInterceptor(
+        payments.mojom.PaymentRequest.name);
+    this.interceptor_.oninterfacerequest =
+        e => this.bindings_.addBinding(this, e.handle);
+    this.interceptor_.start();
+  }
 
-      this.interfaceProvider_ = interfaceProvider;
+  init(client, supportedMethods, details, options) {
+    this.client_ = client;
+    if (this.pendingResponse_) {
+      let response = this.pendingResponse_;
       this.pendingResponse_ = null;
-      this.bindings_ = new bindings.BindingSet(paymentRequest.PaymentRequest);
-    }
-
-    init(client, supportedMethods, details, options) {
-      this.client_ = client;
-      if (this.pendingResponse_) {
-        let response = this.pendingResponse_;
-        this.pendingResponse_ = null;
-        this.onPaymentResponse(response);
-      }
-    }
-
-    show() {}
-
-    updateWith(details) {}
-
-    complete(success) {}
-
-    onPaymentResponse(data) {
-      if (!this.client_) {
-        this.pendingResponse_ = data;
-        return;
-      }
-      this.client_.onPaymentResponse(new paymentRequest.PaymentResponse(data));
-    }
-
-    onComplete() {
-      this.client_.onComplete();
+      this.onPaymentResponse(response);
     }
   }
-  return new PaymentRequestMock(mojo.frameInterfaces);
-});
+
+  show() {}
+
+  updateWith(details) {}
+
+  complete(success) {}
+
+  onPaymentResponse(data) {
+    if (!this.client_) {
+      this.pendingResponse_ = data;
+      return;
+    }
+    this.client_.onPaymentResponse(new payments.mojom.PaymentResponse(data));
+  }
+
+  onComplete() {
+    this.client_.onComplete();
+  }
+}
+
+let paymentRequestMock = new PaymentRequestMock(mojo.frameInterfaces);
