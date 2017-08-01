95c63568d325894200f5009cb3141e293d63b351
Tom Sepez <tsepez@chromium.org>
2017-08-01 17:32:52 +0000

    Tidy IPC message fuzzer
    Removed stray semicolons from message files.
    Removed conflicting ParamTraits<net::IPEndPoint> as it is no longer
    used in the chromoting file.
    Added missing message files to ipc fuzzer.
    
    NOTE: If an IPC fuzzer report bisects to this CL, then it is
    likely that there is a pre-existing condition that was noticed
    due to improved fuzzing coverage, rather than an issue in this
    CL itself.
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Id424a3178401ec4ecc7af1701aa4c04c621b3e51
    Reviewed-on: https://chromium-review.googlesource.com/584857
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Reviewed-by: Selim Gurun <sgurun@chromium.org>
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Reviewed-by: Luis Hector Chavez <lhchavez@chromium.org>
    Reviewed-by: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
    Reviewed-by: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Adam Rice <ricea@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Heng-ruey Hsu <henryhsu@chromium.org>
    Reviewed-by: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Martin Barbella <mbarbella@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491042}
    Former-commit-id: 4ec751610d0f687db6fff1046ae7b799466a9354

--- remoting/host/chromoting_param_traits.h
+++ remoting/host/chromoting_param_traits.h
@@ -72,17 +72,6 @@ struct ParamTraits<remoting::ScreenResolution> {
 };
 
 template <>
-struct ParamTraits<net::IPEndPoint> {
-  typedef net::IPEndPoint param_type;
-  static void GetSize(base::PickleSizer* s, const param_type& p);
-  static void Write(base::Pickle* m, const param_type& p);
-  static bool Read(const base::Pickle* m,
-                   base::PickleIterator* iter,
-                   param_type* p);
-  static void Log(const param_type& p, std::string* l);
-};
-
-template <>
 struct ParamTraits<remoting::DesktopEnvironmentOptions> {
   typedef remoting::DesktopEnvironmentOptions param_type;
   static void Write(base::Pickle* m, const param_type& p);
