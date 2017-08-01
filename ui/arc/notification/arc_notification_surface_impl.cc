4ee4bf9a97e5242df81e8372b4eec458a4008929
yoshiki iguchi <yoshiki@chromium.org>
2017-08-01 05:59:26 +0000

    ui/arc: Return the host_window() for ArcNotificationSurfaceImpl::GetContentWindow()
    After
    https://chromium.googlesource.com/chromium/src/+/cbad051f332e1a9080bc1a99f665d743a1d7b6e9 [formerly 583c9dc6aebca1961320efab48bea42ce2e78fd4]
    all exo sub surfaces will be composited into one compositor frame and
    submit to host_window(), so to scale the content of an exo surface, we
    should set the transform on host_window.
    
    This CL also adds a native_view_ in ArcNotificationSurfaceImpl, and set
    the exo NotificationSurface::host_window() as a child of it, and attaches
    it on native_view_host_. It is because the native_view_host will changes
    the size of the attached window, but we don't want keep the size
    of host_window() unchanged.
    
    Bug: 750720
    Change-Id: I3240483487addbec2397a01d6c6d4ef1fbdcd70a
    Reviewed-on: https://chromium-review.googlesource.com/595349
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490867}
    Former-commit-id: 86e225106fac1561fd44db7e9ab4588644fc2720

--- ui/arc/notification/arc_notification_surface_impl.cc
+++ ui/arc/notification/arc_notification_surface_impl.cc
@@ -17,7 +17,15 @@ ArcNotificationSurfaceImpl::ArcNotificationSurfaceImpl(
     exo::NotificationSurface* surface)
     : surface_(surface) {
   DCHECK(surface);
+  native_view_ = base::MakeUnique<aura::Window>(nullptr);
+  native_view_->SetType(aura::client::WINDOW_TYPE_CONTROL);
+  native_view_->set_owned_by_parent(false);
+  native_view_->Init(ui::LAYER_NOT_DRAWN);
+  native_view_->AddChild(surface_->host_window());
+  native_view_->Show();
 }
+
+ArcNotificationSurfaceImpl::~ArcNotificationSurfaceImpl() = default;
 
 gfx::Size ArcNotificationSurfaceImpl::GetSize() const {
   return surface_->GetContentSize();
@@ -28,12 +36,12 @@ void ArcNotificationSurfaceImpl::Attach(
   DCHECK(!native_view_host_);
   DCHECK(native_view_host);
   native_view_host_ = native_view_host;
-  native_view_host->Attach(surface_->host_window());
+  native_view_host->Attach(native_view_.get());
 }
 
 void ArcNotificationSurfaceImpl::Detach() {
   DCHECK(native_view_host_);
-  DCHECK_EQ(surface_->host_window(), native_view_host_->native_view());
+  DCHECK_EQ(native_view_.get(), native_view_host_->native_view());
   native_view_host_->Detach();
   native_view_host_ = nullptr;
 }
@@ -47,17 +55,12 @@ views::NativeViewHost* ArcNotificationSurfaceImpl::GetAttachedHost() const {
 }
 
 aura::Window* ArcNotificationSurfaceImpl::GetWindow() const {
-  return surface_->host_window();
+  return native_view_.get();
 }
 
 aura::Window* ArcNotificationSurfaceImpl::GetContentWindow() const {
   DCHECK(surface_->host_window());
-  DCHECK(!surface_->host_window()->children().empty());
-
-  aura::Window* content_window = surface_->host_window()->children()[0];
-
-  DCHECK(content_window);
-  return content_window;
+  return surface_->host_window();
 }
 
 const std::string& ArcNotificationSurfaceImpl::GetNotificationKey() const {
