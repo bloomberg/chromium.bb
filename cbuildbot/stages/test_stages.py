7615f6b9fcebfdf85c3fe067d1eae5300476daa0
Po-Hsien Wang <pwang@chromium.org>
2017-08-01 12:05:14 -0700

    test_stages: fix float priority value
    See https://luci-logdog.appspot.com/v/?s=chromeos%2Fbb%2Fchromeos%2Fkevin-release%2F1368%2F%2B%2Frecipes%2Fsteps%2FASyncHWTest__arc-gts-qual_%2F0%2Fstdout
    
    cbuildbot is current failing to launch cts due to float number is not acceptable to
    run_suite.
    So, cast the priority to int in test_stages for arc-cts/gts-qual.
    
    BUG=None
    TEST=cbuildbot kevin-release (done)
    https://uberchromegw.corp.google.com/i/chromiumos.tryserver/builders/release/builds/13394
    
    TEST=cbuildbot samus-release (done)
    https://uberchromegw.corp.google.com/i/chromiumos.tryserver/builders/release/builds/13395
    
    TEST=run_tests
    
    TEST=run_suite --priority 11 ... and see if there is any problem
    
    Change-Id: Ib8798b0527c2e4d4be8a8072b01bef6b39463b21
    Reviewed-on: https://chromium-review.googlesource.com/596674
    Commit-Ready: Po-Hsien Wang <pwang@chromium.org>
    Tested-by: Po-Hsien Wang <pwang@chromium.org>
    Reviewed-by: Rohit Makasana <rohitbm@chromium.org>

--- cbuildbot/stages/test_stages.py
+++ cbuildbot/stages/test_stages.py
@@ -444,7 +444,7 @@ class HWTestStage(generic_stages.BoardSpecificBuilderStage,
         # We add 1/10 of the branch version to the priority. This results in a
         # modest priority bump the older the branch is. Typically beta priority
         # would be dev + [1..4] and stable priority dev + [5..9].
-        self.suite_config.priority += math.ceil(float(cros_vers[1]) / 10.0)
+        self.suite_config.priority += int(math.ceil(float(cros_vers[1]) / 10.0))
       except cbuildbot_run.VersionNotSetError:
         logging.debug('Could not obtain version info. %s will use initial '
                       'priority value: %s', self.suite_config.suite,
