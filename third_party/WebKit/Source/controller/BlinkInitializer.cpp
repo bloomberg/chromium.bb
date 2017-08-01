88175c145989690c045ffba7e329dee90b612454
Jan Krcal <jkrcal@chromium.org>
2017-08-01 13:50:48 +0000

    Revert "(Reland)[Bindings] Create and use V8 context snapshots."
    This reverts commit 986f1d3bb4aaa9fa1a275944eb8775b8941ad4f8 [formerly 59784d15a14347cd2e36769429ea0178f5c464b6].
    
    Reason for revert: it breaks compilation (linking of v8_context_snapshot_generator), see https://build.chromium.org/p/chromium.chrome/builders/Google%20Chrome%20Linux%20x64/builds/19741
    
    Original change's description:
    > (Reland)[Bindings] Create and use V8 context snapshots.
    > 
    > This CL does two things.
    > 
    > 1. In compile time, creates a snapshot file, which consists of V8 contexts.
    > 2. Creates v8::Context from the snapshot in LocalWindowProxy::CreateContext().
    > 
    > We expect this speeds up context creation for 3 times faster on Android.
    > Detailed information is described in the design doc [1].
    > 
    > 
    > [1] Design doc: https://docs.google.com/document/d/1jpQQX0piaxcHJPWakp_Kr_03g5Gnma5h5-Kdlqu7jVQ/edit#heading=h.k6iklq6rvd30
    > 
    > 
    > This CL is a re-land of https://codereview.chromium.org/2841443005 (and the 1st patch is same with it)
    > Test expectations are changed due to http://crbug.com/705364
    > 
    > BUG=588893, 617892, 705364
    > TBR=rkc, jochen, dchen, kinuko, eroman, thakis
    > 
    > Change-Id: If85e68a6498f7d35a0c59f4af9323ba72fc36d5f
    > Reviewed-on: https://chromium-review.googlesource.com/594608
    > Reviewed-by: Hitoshi Yoshida <peria@chromium.org>
    > Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    > Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#490955}
    
    TBR=rkc@chromium.org,dcheng@chromium.org,peria@chromium.org,kinuko@chromium.org,thakis@chromium.org,eroman@chromium.org,yukishiino@chromium.org,haraken@chromium.org,jochen@chromium.org
    
    Change-Id: I67f166ae37b2103100c066334586a3b49a3a21e1
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 588893, 617892, 705364
    Reviewed-on: https://chromium-review.googlesource.com/596087
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490973}
    Former-commit-id: 9f043c3f6c2e9ea2e3903964a3b917276661085d

--- third_party/WebKit/Source/controller/BlinkInitializer.cpp
+++ third_party/WebKit/Source/controller/BlinkInitializer.cpp
@@ -29,7 +29,6 @@
  */
 
 #include "bindings/core/v8/V8Initializer.h"
-#include "bindings/modules/v8/V8ContextSnapshotExternalReferences.h"
 #include "core/animation/AnimationClock.h"
 #include "modules/ModulesInitializer.h"
 #include "platform/bindings/Microtask.h"
@@ -69,8 +68,8 @@ static ModulesInitializer& GetModulesInitializer() {
 void Initialize(Platform* platform) {
   Platform::Initialize(platform);
 
-  V8Initializer::InitializeMainThread(
-      V8ContextSnapshotExternalReferences::GetTable());
+  V8Initializer::InitializeMainThread();
+
   GetModulesInitializer().Initialize();
 
   // currentThread is null if we are running on a thread without a message loop.
