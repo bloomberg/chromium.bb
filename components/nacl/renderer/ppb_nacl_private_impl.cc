fe08b2e3560113db204b4221ecee3f9426c5e93a
DongJun Kim <djmix.kim@samsung.com>
2017-08-01 18:59:54 +0000

    Add return functions for preventing crash
    Previous codes do not return directly even if
    plugin_instance and load_manager are null.
    It can occur crash whenever accessing each variables.
    This patch add return codes for avoiding crash
    if variable is null.
    
    BUG=NONE
    
    Change-Id: Ia5c59a9473124d6ad1ff76ee909d9396af35e96c
    Reviewed-on: https://chromium-review.googlesource.com/586488
    Reviewed-by: Raymes Khoury <raymes@chromium.org>
    Reviewed-by: Bill Budge <bbudge@chromium.org>
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491067}
    Former-commit-id: 94b8a70aa914dca36f0cb03f29265cfcca2ff5eb

--- components/nacl/renderer/ppb_nacl_private_impl.cc
+++ components/nacl/renderer/ppb_nacl_private_impl.cc
@@ -1024,6 +1024,7 @@ void DownloadManifestToBuffer(PP_Instance instance,
         FROM_HERE,
         base::Bind(callback.func, callback.user_data,
                    static_cast<int32_t>(PP_ERROR_FAILED)));
+    return;
   }
   const blink::WebDocument& document =
       plugin_instance->GetContainer()->GetDocument();
@@ -1379,6 +1380,7 @@ void PPBNaClPrivate::DownloadNexe(PP_Instance instance,
         FROM_HERE,
         base::Bind(callback.func, callback.user_data,
                    static_cast<int32_t>(PP_ERROR_FAILED)));
+    return;
   }
   const blink::WebDocument& document =
       plugin_instance->GetContainer()->GetDocument();
@@ -1531,6 +1533,7 @@ void DownloadFile(PP_Instance instance,
     base::ThreadTaskRunnerHandle::Get()->PostTask(
         FROM_HERE, base::Bind(callback, static_cast<int32_t>(PP_ERROR_FAILED),
                               kInvalidNaClFileInfo));
+    return;
   }
   const blink::WebDocument& document =
       plugin_instance->GetContainer()->GetDocument();
