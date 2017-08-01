6cb2f0a36ebecb79fe460e99c343e3cdcfa00e8f
Reilly Grant <reillyg@chromium.org>
2017-08-01 19:28:03 +0000

    Switch WebShare LayoutTests to the new Mojo JS bindings
    This change rewrites the WebShare API layout tests to use the new Mojo
    JS bindings.
    
    Bug: 699569
    Change-Id: Id95a016765c5b101ee347f760e7cec8b74ec4b7e
    Reviewed-on: https://chromium-review.googlesource.com/587412
    Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    Commit-Queue: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491079}
    Former-commit-id: b5bb5a2b5fabce64044a786d4bfaef26770c6523

--- third_party/WebKit/LayoutTests/webshare/resources/mock-share-service.js
+++ third_party/WebKit/LayoutTests/webshare/resources/mock-share-service.js
@@ -1,67 +1,60 @@
 'use strict';
 
-let mockShareService = loadMojoModules(
-    'mockShareService',
-    ['mojo/public/js/bindings',
-     'third_party/WebKit/public/platform/modules/webshare/webshare.mojom',
-    ]).then(mojo => {
-  let [bindings, webshare] = mojo.modules;
+class MockShareService {
+  constructor(interfaceProvider) {
+    this.bindingSet_ = new mojo.BindingSet(blink.mojom.ShareService);
+    this.interceptor_ = new MojoInterfaceInterceptor(
+        blink.mojom.ShareService.name);
+    this.interceptor_.oninterfacerequest =
+        e => this.bindingSet_.addBinding(this, e.handle);
+    this.interceptor_.start();
+  }
 
-  class MockShareService {
-    constructor(interfaceProvider) {
-      this.webshare_ = webshare;
-      this.bindingSet_ = new bindings.BindingSet(webshare.ShareService);
+  // Returns a Promise that gets rejected if the test should fail.
+  init_() {
+    // sequence of [expectedTitle, expectedText, result].
+    this.shareResultQueue_ = [];
 
-      interfaceProvider.addInterfaceOverrideForTesting(
-          webshare.ShareService.name,
-          handle => this.bindingSet_.addBinding(this, handle));
-    }
+    return new Promise((resolve, reject) => {this.reject_ = reject});
+  }
 
-    // Returns a Promise that gets rejected if the test should fail.
-    init_() {
-      // sequence of [expectedTitle, expectedText, result].
-      this.shareResultQueue_ = [];
+  share(title, text, url) {
+    let callback = null;
+    let result = new Promise(resolve => {callback = resolve;});
 
-      return new Promise((resolve, reject) => {this.reject_ = reject});
-    }
-
-    share(title, text, url) {
-      let callback = null;
-      let result = new Promise(resolve => {callback = resolve;});
-
-      if (!this.shareResultQueue_.length) {
-        this.reject_('Unexpected call to mojo share method');
-        return result;
-      }
-
-      let [expectedTitle, expectedText, expectedUrl, error] =
-          this.shareResultQueue_.shift();
-      try {
-        assert_equals(title, expectedTitle);
-        assert_equals(text, expectedText);
-        assert_equals(url.url, expectedUrl);
-      } catch (e) {
-        this.reject_(e);
-        return result;
-      }
-      callback({error: error});
-
+    if (!this.shareResultQueue_.length) {
+      this.reject_('Unexpected call to mojo share method');
       return result;
     }
 
-    pushShareResult(expectedTitle, expectedText, expectedUrl, result) {
-      this.shareResultQueue_.push(
-          [expectedTitle, expectedText, expectedUrl, result]);
+    let [expectedTitle, expectedText, expectedUrl, error] =
+        this.shareResultQueue_.shift();
+    try {
+      assert_equals(title, expectedTitle);
+      assert_equals(text, expectedText);
+      assert_equals(url.url, expectedUrl);
+    } catch (e) {
+      this.reject_(e);
+      return result;
     }
+    callback({error: error});
+
+    return result;
   }
-  return new MockShareService(mojo.frameInterfaces);
-});
+
+  pushShareResult(expectedTitle, expectedText, expectedUrl, result) {
+    this.shareResultQueue_.push(
+        [expectedTitle, expectedText, expectedUrl, result]);
+  }
+}
+
+let mockShareService = new MockShareService();
 
 function share_test(func, name, properties) {
-  promise_test(t => mockShareService.then(mock => {
-    let mockPromise = mock.init_();
-    return Promise.race([func(t, mock.webshare_, mock), mockPromise]);
-  }), name, properties);
+  promise_test(() => {
+    let mockPromise = mockShareService.init_();
+    return Promise.race([func(mockShareService), mockPromise]);
+  }, name, properties);
 }
 
 // Copied from resources/bluetooth/bluetooth-helpers.js.
