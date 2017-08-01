b0fd928ac457aa3fc4e7f76a561f9e7e2c07075e
Jan Krcal <jkrcal@chromium.org>
2017-08-01 09:22:09 +0000

    [Sheriff] Disabling SyncFileSystemTest.AuthorizationTest on ChromiumOS
    This test is flaky.
    
    TBR=tzik@chromium.org, yutak@chromium.org
    NOTRY=true
    
    Bug: 750800
    Change-Id: Ie46050f53dab1c328a795658d935b02ac573e1ee
    Reviewed-on: https://chromium-review.googlesource.com/595469
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Commit-Queue: Jan Krcal <jkrcal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#490925}
    Former-commit-id: de0ef9cf92d3d3454b1781e97cc65ad478306d47

--- chrome/browser/extensions/api/sync_file_system/sync_file_system_browsertest.cc
+++ chrome/browser/extensions/api/sync_file_system/sync_file_system_browsertest.cc
@@ -147,7 +147,14 @@ class SyncFileSystemTest : public extensions::PlatformAppBrowserTest,
   DISALLOW_COPY_AND_ASSIGN(SyncFileSystemTest);
 };
 
-IN_PROC_BROWSER_TEST_F(SyncFileSystemTest, AuthorizationTest) {
+#if defined(OS_CHROMEOS)
+// Flaky on ChromiumOS, http://crbug.com/750800.
+#define MAYBE_AuthorizationTest DISABLED_AuthorizationTest
+#else
+#define MAYBE_AuthorizationTest AuthorizationTest
+#endif
+
+IN_PROC_BROWSER_TEST_F(SyncFileSystemTest, MAYBE_AuthorizationTest) {
   ExtensionTestMessageListener open_failure(
       "checkpoint: Failed to get syncfs", true);
   ExtensionTestMessageListener bar_created(
