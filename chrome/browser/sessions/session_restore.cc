446be38e0d601f0b14d4ee16040b309db0ebfff0
Antonio Gomes <tonikitoo@igalia.com>
2017-08-01 04:11:29 +0000

    Factor out some duplicated logic in SessionRestoreImpl::RestoreTabsToBrowser
    The method SessionRestoreImpl::RestoreTabsToBrowser executes in two ways:
    
    - user is recovering a previous session and creating a new browser session
    for that.
    - user is recovering a previous session and (re)using an existing browser
    session for that.
    
    Both paths have similar logic, with some peculiarities, that is now is shared
    with this PR.
    
    This is a preparation step to fix Bug 749506:
    https://chromium-review.googlesource.com/c/593868/
    
    TEST=no behavior change, so no new tests needed.
    
    BUG=749506
    
    Change-Id: I6974d24defb8681a7a2e736f1ee98861d9ee316b
    Reviewed-on: https://chromium-review.googlesource.com/593867
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Antonio Gomes <tonikitoo@igalia.com>
    Cr-Commit-Position: refs/heads/master@{#490847}
    Former-commit-id: 284943d5dc070f507b5ffaacce110afc7411bf82

--- chrome/browser/sessions/session_restore.cc
+++ chrome/browser/sessions/session_restore.cc
@@ -540,47 +540,22 @@ class SessionRestoreImpl : public content::NotificationObserver {
         // Loads are scheduled for each restored tab unless the tab is going to
         // be selected as ShowBrowser() will load the selected tab.
         bool is_selected_tab = (i == selected_tab_index);
-        WebContents* contents = RestoreTab(tab, i, browser, is_selected_tab);
-
-        // RestoreTab can return nullptr if |tab| doesn't have valid data.
-        if (!contents)
-          continue;
-
-        // Sanitize the last active time.
-        base::TimeDelta delta = highest_time - tab.last_active_time;
-        contents->SetLastActiveTime(now - delta);
-
-        RestoredTab restored_tab(contents, is_selected_tab,
-                                 tab.extension_app_id.empty(), tab.pinned);
-        created_contents->push_back(restored_tab);
-
-        // If this isn't the selected tab, there's nothing else to do.
-        if (!is_selected_tab)
-          continue;
-
-        ShowBrowser(browser, browser->tab_strip_model()->GetIndexOfWebContents(
-                                 contents));
-        // TODO(sky): remove. For debugging 368236.
-        CHECK_EQ(browser->tab_strip_model()->GetActiveWebContents(), contents);
+        RestoreTab(tab, browser, created_contents, i, is_selected_tab, now,
+                   highest_time);
       }
     } else {
       // If the browser already has tabs, we want to restore the new ones after
       // the existing ones. E.g. this happens in Win8 Metro where we merge
       // windows or when launching a hosted app from the app launcher.
       int tab_index_offset = initial_tab_count;
+
+      // Always schedule loads as we will not be calling ShowBrowser().
+      bool is_selected_tab = false;
+
       for (int i = 0; i < static_cast<int>(window.tabs.size()); ++i) {
         const sessions::SessionTab& tab = *(window.tabs[i]);
-        // Always schedule loads as we will not be calling ShowBrowser().
-        WebContents* contents =
-            RestoreTab(tab, tab_index_offset + i, browser, false);
-        if (contents) {
-          // Sanitize the last active time.
-          base::TimeDelta delta = highest_time - tab.last_active_time;
-          contents->SetLastActiveTime(now - delta);
-          RestoredTab restored_tab(contents, false,
-                                   tab.extension_app_id.empty(), tab.pinned);
-          created_contents->push_back(restored_tab);
-        }
+        RestoreTab(tab, browser, created_contents, tab_index_offset + i,
+                   is_selected_tab, now, highest_time);
       }
     }
   }
@@ -589,15 +564,18 @@ class SessionRestoreImpl : public content::NotificationObserver {
   // the last existing pinned tab.
   // |tab_loader_| will schedule this tab for loading if |is_selected_tab| is
   // false.
-  WebContents* RestoreTab(const sessions::SessionTab& tab,
-                          const int tab_index,
-                          Browser* browser,
-                          bool is_selected_tab) {
+  void RestoreTab(const sessions::SessionTab& tab,
+                  Browser* browser,
+                  std::vector<RestoredTab>* created_contents,
+                  const int tab_index,
+                  bool is_selected_tab,
+                  base::TimeTicks now,
+                  base::TimeTicks highest_time) {
     // It's possible (particularly for foreign sessions) to receive a tab
     // without valid navigations. In that case, just skip it.
     // See crbug.com/154129.
     if (tab.navigations.empty())
-      return nullptr;
+      return;
 
     SessionRestore::NotifySessionRestoreStartedLoadingTabs();
     int selected_index = GetNavigationIndexToSelect(tab);
@@ -622,7 +600,26 @@ class SessionRestoreImpl : public content::NotificationObserver {
     // focused tab will be loaded by Browser, and TabLoader will load the rest.
     DCHECK(is_selected_tab || web_contents->GetController().NeedsReload());
 
-    return web_contents;
+    // RestoreTab can return nullptr if |tab| doesn't have valid data.
+    if (!web_contents)
+      return;
+
+    // Sanitize the last active time.
+    base::TimeDelta delta = highest_time - tab.last_active_time;
+    web_contents->SetLastActiveTime(now - delta);
+
+    RestoredTab restored_tab(web_contents, is_selected_tab,
+                             tab.extension_app_id.empty(), tab.pinned);
+    created_contents->push_back(restored_tab);
+
+    // If this isn't the selected tab, there's nothing else to do.
+    if (!is_selected_tab)
+      return;
+
+    ShowBrowser(browser, browser->tab_strip_model()->GetIndexOfWebContents(
+                             web_contents));
+    // TODO(sky): remove. For debugging 368236.
+    CHECK_EQ(browser->tab_strip_model()->GetActiveWebContents(), web_contents);
   }
 
   Browser* CreateRestoredBrowser(Browser::Type type,
