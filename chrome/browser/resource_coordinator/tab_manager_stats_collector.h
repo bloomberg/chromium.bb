42c219314e66a3b51e682e16aa300da9190ed184
Scott Haseley <shaseley@google.com>
2017-08-02 03:44:57 +0000

    Add session restore swap metrics UMAs to TabManager.
    This CL adds UMAs for system swap metrics during session restore. The
    implementation uses SwapMetricsDriver to collect the swap metrics during
    session restore, and computes rates (e.g. swaps in/second) as an average over
    the entire session restore interval. Swap metrics are platform-specific.
    Currently, swap in/out rate is available on Linux and Mac, and page
    compression/decompression rates are available on Mac only.
    
    Bug: 740625
    Change-Id: I3dfb4268759e6900e768756b8939ad64de603a06
    Reviewed-on: https://chromium-review.googlesource.com/585405
    Commit-Queue: Scott Haseley <shaseley@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Fadi Meawad <fmeawad@chromium.org>
    Reviewed-by: Zhen Wang <zhenw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491254}
    Former-commit-id: d450cb9e0664b13ae630bfeb488b69c98cc30423

--- chrome/browser/resource_coordinator/tab_manager_stats_collector.h
+++ chrome/browser/resource_coordinator/tab_manager_stats_collector.h
@@ -5,7 +5,17 @@
 #ifndef CHROME_BROWSER_RESOURCE_COORDINATOR_TAB_MANAGER_STATS_COLLECTOR_H_
 #define CHROME_BROWSER_RESOURCE_COORDINATOR_TAB_MANAGER_STATS_COLLECTOR_H_
 
+#include <cstdint>
+#include <memory>
+
+#include "chrome/browser/sessions/session_restore_observer.h"
+
+namespace base {
+class TimeDelta;
+}
+
 namespace content {
+class SwapMetricsDriver;
 class WebContents;
 }  // namespace content
 
@@ -15,7 +25,7 @@ class TabManager;
 
 // TabManagerStatsCollector records UMAs on behalf of TabManager for tab and
 // system-related events and properties during session restore.
-class TabManagerStatsCollector {
+class TabManagerStatsCollector : public SessionRestoreObserver {
  public:
   explicit TabManagerStatsCollector(TabManager* tab_manager);
   ~TabManagerStatsCollector();
@@ -24,8 +34,27 @@ class TabManagerStatsCollector {
   // during session restore.
   void RecordSwitchToTab(content::WebContents* contents) const;
 
+  // SessionRestoreObserver
+  void OnSessionRestoreStartedLoadingTabs() override;
+  void OnSessionRestoreFinishedLoadingTabs() override;
+
+  // The following record UMA histograms for system swap metrics during session
+  // restore.
+  void OnSessionRestoreSwapInCount(uint64_t count, base::TimeDelta interval);
+  void OnSessionRestoreSwapOutCount(uint64_t count, base::TimeDelta interval);
+  void OnSessionRestoreDecompressedPageCount(uint64_t count,
+                                             base::TimeDelta interval);
+  void OnSessionRestoreCompressedPageCount(uint64_t count,
+                                           base::TimeDelta interval);
+  void OnSessionRestoreUpdateMetricsFailed();
+
  private:
+  class SessionRestoreSwapMetricsDelegate;
+
   TabManager* tab_manager_;
+  std::unique_ptr<content::SwapMetricsDriver>
+      session_restore_swap_metrics_driver_;
+  bool is_session_restore_loading_tabs_;
 };
 
 }  // namespace resource_coordinator
