f5d72af1c21d46157f10bb049cc9c2f39a9178de
Yuwei Huang <yuweih@chromium.org>
2017-08-02 21:59:34 +0000

    [CRD iOS] Rework the HostView keyboard layout
    When rotating the screen on iPad with the keyboard showing, for some
    reason the host view will become improperly laid out. Part of the view
    gets overlaid by the keyboard.
    
    My suspicion is the view frame becomes inconsistent when the rotation
    happens.
    
    This CL fixes the problem by reworking the keyboard layout using
    constraints. It also makes GLRenderer cache the view size before the
    canvas is created.
    
    Bug: 751273
    Change-Id: I5146ff6bf602eef1857259e5e50ca6be1d952a68
    Reviewed-on: https://chromium-review.googlesource.com/596913
    Commit-Queue: Yuwei Huang <yuweih@chromium.org>
    Reviewed-by: Scott Nichols <nicholss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491519}
    Former-commit-id: 0ad8bf2ffab118707fc7ca4f6bf25dd676c70c7f

--- remoting/client/display/gl_renderer.cc
+++ remoting/client/display/gl_renderer.cc
@@ -106,6 +106,9 @@ void GlRenderer::OnCursorShapeChanged(const protocol::CursorShapeInfo& shape) {
 void GlRenderer::OnSurfaceCreated(std::unique_ptr<Canvas> canvas) {
   DCHECK(thread_checker_.CalledOnValidThread());
   canvas_ = std::move(canvas);
+  if (view_width_ > 0 && view_height_ > 0) {
+    canvas_->SetViewSize(view_width_, view_height_);
+  }
   for (auto& drawable : drawables_) {
     drawable->SetCanvas(canvas_->GetWeakPtr());
   }
@@ -113,10 +116,13 @@ void GlRenderer::OnSurfaceCreated(std::unique_ptr<Canvas> canvas) {
 
 void GlRenderer::OnSurfaceChanged(int view_width, int view_height) {
   DCHECK(thread_checker_.CalledOnValidThread());
+  view_width_ = view_width;
+  view_height_ = view_height;
+
   if (!canvas_) {
-    LOG(WARNING) << "Trying to set the view size when the canvas is not ready.";
     return;
   }
+
   canvas_->SetViewSize(view_width, view_height);
   RequestRender();
 }
