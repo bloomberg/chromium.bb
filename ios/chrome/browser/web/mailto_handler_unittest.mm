d824995913d5374880f215a04df86211bbb23566
Peter K. Lee <pkl@chromium.org>
2017-08-02 06:04:36 +0000

    Handles mailto: URLs with equal sign in body= parameter
    Original method expects only a single equal sign in each
    query parameter and that the query string is only in lower
    case. However, body may contain a URL with may have an
    unescaped equal sign. And query string keys may be in mixed
    case.
    
    As part of this fix, also removed -supportedHeaders from
    public API and use GURL for query parameter processing.
    
    Bug: 747949
    Change-Id: Ia157983e946d7301300d22f41e007bd10ce8b786
    Reviewed-on: https://chromium-review.googlesource.com/594873
    Commit-Queue: Peter Lee <pkl@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491280}
    Former-commit-id: 466ef2d5cc8088752de6713ff1dd20447ba4f0ad

--- ios/chrome/browser/web/mailto_handler_unittest.mm
+++ ios/chrome/browser/web/mailto_handler_unittest.mm
@@ -8,38 +8,60 @@
 #include "testing/gtest_mac.h"
 #include "url/gurl.h"
 
+// Tests constructor.
 TEST(MailtoHandlerTest, TestConstructor) {
   MailtoHandler* handler =
       [[MailtoHandler alloc] initWithName:@"Some App" appStoreID:@"12345"];
   EXPECT_NSEQ(@"Some App", [handler appName]);
   EXPECT_NSEQ(@"12345", [handler appStoreID]);
   EXPECT_NSEQ(@"mailtohandler:/co?", [handler beginningScheme]);
-  EXPECT_GT([[handler supportedHeaders] count], 0U);
 }
 
+// Tests mailto URL with and without a subject.
 TEST(MailtoHandlerTest, TestRewriteGood) {
-  MailtoHandler* handler = [[MailtoHandler alloc] init];
-  // Tests mailto URL without a subject.
+  MailtoHandler* handler =
+      [[MailtoHandler alloc] initWithName:@"Some App" appStoreID:@"12345"];
   NSString* result = [handler rewriteMailtoURL:GURL("mailto:user@domain.com")];
   EXPECT_NSEQ(@"mailtohandler:/co?to=user@domain.com", result);
   // Tests mailto URL with a subject.
   result =
       [handler rewriteMailtoURL:GURL("mailto:user@domain.com?subject=hello")];
   EXPECT_NSEQ(@"mailtohandler:/co?to=user@domain.com&subject=hello", result);
-  // Tests mailto URL with unrecognized query parameters.
-  result = [handler
+}
+
+// Tests mailto URL with unrecognized query parameters.
+TEST(MailtoHandlerTest, TestRewriteUnrecognizedParams) {
+  MailtoHandler* handler =
+      [[MailtoHandler alloc] initWithName:@"Some App" appStoreID:@"12345"];
+  NSString* result = [handler
       rewriteMailtoURL:
           GURL("mailto:someone@there.com?garbage=in&garbageOut&subject=trash")];
   EXPECT_NSEQ(@"mailtohandler:/co?to=someone@there.com&subject=trash", result);
 }
 
-TEST(MailtoHandlerTest, TestRewriteBad) {
-  MailtoHandler* handler = [[MailtoHandler alloc] init];
+// Tests mailto URL with a body that includes a = sign.
+TEST(MailtoHandlerTest, TestRewriteBodyWithUrl) {
+  MailtoHandler* handler =
+      [[MailtoHandler alloc] initWithName:@"Some App" appStoreID:@"12345"];
+  NSString* result = [handler
+      rewriteMailtoURL:GURL("mailto:user@domain.com?body=http://foo.bar?x=y")];
+  EXPECT_NSEQ(@"mailtohandler:/co?to=user@domain.com&body=http://foo.bar?x=y",
+              result);
+}
+
+// Tests mailto URL with parameters that are mixed upper/lower cases.
+TEST(MailtoHandlerTest, TestRewriteWithMixedCase) {
+  MailtoHandler* handler =
+      [[MailtoHandler alloc] initWithName:@"Some App" appStoreID:@"12345"];
+  NSString* result =
+      [handler rewriteMailtoURL:GURL("mailto:?Subject=Blah&BODY=stuff")];
+  EXPECT_NSEQ(@"mailtohandler:/co?subject=Blah&body=stuff", result);
+}
+
+// Tests that non-mailto URLs returns nil.
+TEST(MailtoHandlerTest, TestRewriteNotMailto) {
+  MailtoHandler* handler =
+      [[MailtoHandler alloc] initWithName:@"Some App" appStoreID:@"12345"];
   NSString* result = [handler rewriteMailtoURL:GURL("http://www.google.com")];
   EXPECT_FALSE(result);
-  result = [handler
-      rewriteMailtoURL:
-          GURL("mailto:user@domain.com?foo=bar&cc=someone@somewhere.com")];
-  EXPECT_NSEQ(@"mailtohandler:/co?to=user@domain.com&cc=someone@somewhere.com",
-              result);
 }
