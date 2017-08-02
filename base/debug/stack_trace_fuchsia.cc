d407a841068aad31c28ee462125d07c345c12df2
Kevin Marshall <kmarshall@chromium.org>
2017-08-02 19:41:09 +0000

    Emit symbolizable backtraces on DCHECK/LOG(FATAL) under Fuchsia.
    This adds code to query the process' debug symbol maps for offsets
    and produces an externally symbolizable backtrace using that data.
    
    R: thakis@chromium.org,wez@chromium.org
    CC: jamesr@chromium.org
    Bug: 706592
    Change-Id: I7b3257ff154671b2eb1c90f7ab91947f55cfa873
    Reviewed-on: https://chromium-review.googlesource.com/575290
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491470}
    Former-commit-id: abf2e85ada60eb318e91976a8845288d0cec959f

--- base/debug/stack_trace_fuchsia.cc
+++ base/debug/stack_trace_fuchsia.cc
@@ -4,14 +4,20 @@
 
 #include "base/debug/stack_trace.h"
 
+#include <link.h>
+#include <magenta/crashlogger.h>
 #include <magenta/process.h>
 #include <magenta/syscalls.h>
+#include <magenta/syscalls/definitions.h>
 #include <magenta/syscalls/port.h>
 #include <magenta/types.h>
+#include <stddef.h>
+#include <string.h>
 #include <threads.h>
 #include <unwind.h>
 
 #include <algorithm>
+#include <iomanip>
 #include <iostream>
 
 #include "base/logging.h"
@@ -20,6 +26,9 @@ namespace base {
 namespace debug {
 
 namespace {
+
+const char kProcessNamePrefix[] = "app:";
+const size_t kProcessNamePrefixLen = arraysize(kProcessNamePrefix) - 1;
 
 struct BacktraceData {
   void** trace_array;
@@ -36,6 +45,110 @@ _Unwind_Reason_Code UnwindStore(struct _Unwind_Context* context,
   if (*data->count == data->max)
     return _URC_END_OF_STACK;
   return _URC_NO_REASON;
+}
+
+// Stores and queries debugging symbol map info for the current process.
+class SymbolMap {
+ public:
+  struct Entry {
+    void* addr;
+    char name[MX_MAX_NAME_LEN + 1];
+  };
+
+  SymbolMap();
+  ~SymbolMap() = default;
+
+  // Gets the symbol map entry for |address|. Returns null if no entry could be
+  // found for the address, or if the symbol map could not be queried.
+  Entry* GetForAddress(void* address);
+
+ private:
+  static const size_t kMaxMapEntries = 64;
+
+  void Populate();
+
+  // Sorted in descending order by address, for lookup purposes.
+  Entry entries_[kMaxMapEntries];
+
+  size_t count_ = 0;
+  bool valid_ = false;
+
+  DISALLOW_COPY_AND_ASSIGN(SymbolMap);
+};
+
+SymbolMap::SymbolMap() {
+  Populate();
+}
+
+SymbolMap::Entry* SymbolMap::GetForAddress(void* address) {
+  if (!valid_) {
+    return nullptr;
+  }
+
+  // Working backwards in the address space, return the first map entry whose
+  // address comes before |address| (thereby enclosing it.)
+  for (size_t i = 0; i < count_; ++i) {
+    if (address >= entries_[i].addr) {
+      return &entries_[i];
+    }
+  }
+  return nullptr;
+}
+
+void SymbolMap::Populate() {
+  mx_handle_t process = mx_process_self();
+
+  // Get the process' name.
+  char app_name[MX_MAX_NAME_LEN + kProcessNamePrefixLen];
+  strcpy(app_name, kProcessNamePrefix);
+  auto status = mx_object_get_property(
+      process, MX_PROP_NAME, app_name + kProcessNamePrefixLen,
+      sizeof(app_name) - kProcessNamePrefixLen);
+  if (status != MX_OK) {
+    DPLOG(WARNING)
+        << "Couldn't get name, falling back to 'app' for program name: "
+        << status;
+    strlcpy(app_name, "app", sizeof(app_name));
+  }
+
+  // Retrieve the debug info struct.
+  constexpr size_t map_capacity = sizeof(entries_);
+  uintptr_t debug_addr;
+  status = mx_object_get_property(process, MX_PROP_PROCESS_DEBUG_ADDR,
+                                  &debug_addr, sizeof(debug_addr));
+  if (status != MX_OK) {
+    DPLOG(ERROR) << "Couldn't get symbol map for process: " << status;
+    return;
+  }
+  r_debug* debug_info = reinterpret_cast<r_debug*>(debug_addr);
+
+  // Get the link map from the debug info struct.
+  link_map* lmap = reinterpret_cast<link_map*>(debug_info->r_map);
+  if (!lmap) {
+    DPLOG(ERROR) << "Null link_map for process.";
+    return;
+  }
+
+  // Copy the contents of the link map linked list to |entries_|.
+  while (lmap != nullptr) {
+    if (count_ == map_capacity) {
+      break;
+    }
+    SymbolMap::Entry* next_entry = &entries_[count_];
+    count_++;
+
+    next_entry->addr = reinterpret_cast<void*>(lmap->l_addr);
+    char* name_to_use = lmap->l_name[0] ? lmap->l_name : app_name;
+    size_t name_len = strnlen(name_to_use, MX_MAX_NAME_LEN);
+    strncpy(next_entry->name, name_to_use, name_len + 1);
+    lmap = lmap->l_next;
+  }
+
+  std::sort(
+      &entries_[0], &entries_[count_ - 1],
+      [](const Entry& a, const Entry& b) -> bool { return a.addr >= b.addr; });
+
+  valid_ = true;
 }
 
 }  // namespace
@@ -60,12 +173,36 @@ void StackTrace::Print() const {
   OutputToStream(&std::cerr);
 }
 
+// Sample stack trace output:
+// #00 0x1527a058aa00 app:/system/base_unittests+0x18bda00
+// #01 0x1527a0254b5c app:/system/base_unittests+0x1587b5c
+// #02 0x15279f446ece app:/system/base_unittests+0x779ece
+// ...
+// #21 0x1527a05b51b4 app:/system/base_unittests+0x18e81b4
+// #22 0x54fdbf3593de libc.so+0x1c3de
+// #23 end
 void StackTrace::OutputToStream(std::ostream* os) const {
-  // TODO(fuchsia): Consider doing symbol resolution here. See
-  // https://crbug.com/706592.
-  for (size_t i = 0; (i < count_) && os->good(); ++i) {
-    (*os) << "\t" << trace_[i] << "\n";
+  SymbolMap map;
+
+  size_t i = 0;
+  for (; (i < count_) && os->good(); ++i) {
+    auto entry = map.GetForAddress(trace_[i]);
+    if (entry) {
+      size_t offset = reinterpret_cast<uintptr_t>(trace_[i]) -
+                      reinterpret_cast<uintptr_t>(entry->addr);
+      *os << "#" << std::setw(2) << std::setfill('0') << i << std::setw(0)
+          << " " << trace_[i] << " " << entry->name << "+0x" << std::hex
+          << offset << std::dec << std::setw(0) << "\n";
+    } else {
+      // Fallback if the DSO map isn't available.
+      // Logged PC values are absolute memory addresses, and the shared object
+      // name is not emitted.
+      *os << "#" << std::setw(2) << std::setfill('0') << i << std::setw(0)
+          << trace_[i] << "\n";
+    }
   }
+
+  (*os) << "#" << std::setw(2) << i << " end\n";
 }
 
 }  // namespace debug
