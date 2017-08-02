cac8ec0b8112d93d35ff08b5d42fd020696fcdd6
Dale Curtis <dalecurtis@chromium.org>
2017-08-02 02:09:13 +0000

    Reland "Refactor watch time reporting into a mojo service."
    This is a reland of 8bdc0b277570c9d2c9f7ee92d8f600560ac3e2bd [formerly 1b5b36fd2dcb4fa11d6453da1189be00e9cc892d]
    Original change's description:
    > Refactor watch time reporting into a mojo service.
    > 
    > It's not sustainable to continue to stuff metrics into the MediaInternals
    > reporter. Since these values aren't even displayed in the internals page
    > they should go into their own mojo service. Which has the benefit of
    > being a much simpler implementation to boot (~250 lines less).
    > 
    > We now have a mojo WatchTimeRecorder interface that the WatchTimeReporter
    > can send watch time updates too. It can also finalize individual keys with
    > this new interface. As with the old interface if the process dies the mojo
    > channel will close and we'll finalize all held values at destruction.
    > 
    > BUG=716643, 735238
    > TEST=new/existing unittests all pass
    > 
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    > Change-Id: I0565422fbf70eba676dc22e08ea22cec88f09778
    > Reviewed-on: https://chromium-review.googlesource.com/546903
    > Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    > Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    > Reviewed-by: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Mounir Lamouri <mlamouri@chromium.org>
    > Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    > Reviewed-by: Yuzhu Shen <yzshen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#490187}
    
    TBR=xhwang,nasko,rkaplow,yzshen,mlamouri
    
    Bug: 716643, 735238
    Change-Id: Ife0d1287dc4d8655ccbc98cdcbead48659eb685a
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_site_isolation
    Reviewed-on: https://chromium-review.googlesource.com/593855
    Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491219}
    Former-commit-id: 1adbe6aa7c5c005de36840c3f78834ef49798a66

---
+++ media/mojo/services/watch_time_recorder_unittest.cc
@@ -0,0 +1,231 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "media/mojo/services/watch_time_recorder.h"
+
+#include <stddef.h>
+
+#include "base/bind.h"
+#include "base/bind_helpers.h"
+#include "base/test/histogram_tester.h"
+#include "base/test/test_message_loop.h"
+#include "base/threading/thread_task_runner_handle.h"
+#include "components/ukm/test_ukm_recorder.h"
+#include "media/base/watch_time_keys.h"
+#include "testing/gmock/include/gmock/gmock.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "url/gurl.h"
+
+namespace media {
+
+constexpr char kTestOrigin[] = "https://test.google.com/";
+
+class WatchTimeRecorderTest : public testing::Test {
+ public:
+  WatchTimeRecorderTest()
+      : computation_keys_(
+            {WatchTimeKey::kAudioSrc, WatchTimeKey::kAudioMse,
+             WatchTimeKey::kAudioEme, WatchTimeKey::kAudioVideoSrc,
+             WatchTimeKey::kAudioVideoMse, WatchTimeKey::kAudioVideoEme}),
+        mtbr_keys_({kMeanTimeBetweenRebuffersAudioSrc,
+                    kMeanTimeBetweenRebuffersAudioMse,
+                    kMeanTimeBetweenRebuffersAudioEme,
+                    kMeanTimeBetweenRebuffersAudioVideoSrc,
+                    kMeanTimeBetweenRebuffersAudioVideoMse,
+                    kMeanTimeBetweenRebuffersAudioVideoEme}),
+        smooth_keys_({kRebuffersCountAudioSrc, kRebuffersCountAudioMse,
+                      kRebuffersCountAudioEme, kRebuffersCountAudioVideoSrc,
+                      kRebuffersCountAudioVideoMse,
+                      kRebuffersCountAudioVideoEme}) {
+    ResetMetricRecorders();
+    WatchTimeRecorder::CreateWatchTimeRecorderProvider(
+        mojo::MakeRequest(&provider_));
+  }
+
+  ~WatchTimeRecorderTest() override { base::RunLoop().RunUntilIdle(); }
+
+  void Initialize(bool has_audio,
+                  bool has_video,
+                  bool is_mse,
+                  bool is_encrypted) {
+    provider_->AcquireWatchTimeRecorder(
+        mojom::PlaybackProperties::New(kUnknownAudioCodec, kUnknownVideoCodec,
+                                       has_audio, has_video, is_mse,
+                                       is_encrypted, false, gfx::Size(800, 600),
+                                       url::Origin(GURL(kTestOrigin))),
+        mojo::MakeRequest(&wtr_));
+  }
+
+  void ExpectWatchTime(const std::vector<base::StringPiece>& keys,
+                       base::TimeDelta value) {
+    for (int i = 0; i <= static_cast<int>(WatchTimeKey::kWatchTimeKeyMax);
+         ++i) {
+      const base::StringPiece test_key =
+          WatchTimeKeyToString(static_cast<WatchTimeKey>(i));
+      auto it = std::find(keys.begin(), keys.end(), test_key);
+      if (it == keys.end()) {
+        histogram_tester_->ExpectTotalCount(test_key.as_string(), 0);
+      } else {
+        histogram_tester_->ExpectUniqueSample(test_key.as_string(),
+                                              value.InMilliseconds(), 1);
+      }
+    }
+  }
+
+  void ExpectHelper(const std::vector<base::StringPiece>& full_key_list,
+                    const std::vector<base::StringPiece>& keys,
+                    int64_t value) {
+    for (auto key : full_key_list) {
+      auto it = std::find(keys.begin(), keys.end(), key);
+      if (it == keys.end())
+        histogram_tester_->ExpectTotalCount(key.as_string(), 0);
+      else
+        histogram_tester_->ExpectUniqueSample(key.as_string(), value, 1);
+    }
+  }
+
+  void ExpectMtbrTime(const std::vector<base::StringPiece>& keys,
+                      base::TimeDelta value) {
+    ExpectHelper(mtbr_keys_, keys, value.InMilliseconds());
+  }
+
+  void ExpectZeroRebuffers(const std::vector<base::StringPiece>& keys) {
+    ExpectHelper(smooth_keys_, keys, 0);
+  }
+
+  void ExpectRebuffers(const std::vector<base::StringPiece>& keys, int count) {
+    ExpectHelper(smooth_keys_, keys, count);
+  }
+
+  void ExpectUkmWatchTime(const std::vector<base::StringPiece>& keys,
+                          base::TimeDelta value) {
+    if (keys.empty()) {
+      ASSERT_EQ(0u, test_recorder_->sources_count());
+      ASSERT_EQ(0u, test_recorder_->entries_count());
+      return;
+    }
+
+    ASSERT_EQ(1u, test_recorder_->sources_count());
+
+    const auto* source = test_recorder_->GetSourceForUrl(kTestOrigin);
+    ASSERT_TRUE(source);
+
+    for (auto key : keys) {
+      test_recorder_->ExpectMetric(
+          *source, WatchTimeRecorder::kWatchTimeUkmEvent,
+          key.substr(strlen(WatchTimeRecorder::kWatchTimeUkmEvent) + 1).data(),
+          value.InMilliseconds());
+    }
+  }
+
+  void ResetMetricRecorders() {
+    histogram_tester_.reset(new base::HistogramTester());
+    // Ensure cleared global before attempting to create a new TestUkmReporter.
+    test_recorder_.reset();
+    test_recorder_.reset(new ukm::TestAutoSetUkmRecorder());
+  }
+
+  MOCK_METHOD0(GetCurrentMediaTime, base::TimeDelta());
+
+ protected:
+  base::MessageLoop message_loop_;
+  mojom::WatchTimeRecorderProviderPtr provider_;
+  std::unique_ptr<base::HistogramTester> histogram_tester_;
+  std::unique_ptr<ukm::TestAutoSetUkmRecorder> test_recorder_;
+  mojom::WatchTimeRecorderPtr wtr_;
+  const std::vector<WatchTimeKey> computation_keys_;
+  const std::vector<base::StringPiece> mtbr_keys_;
+  const std::vector<base::StringPiece> smooth_keys_;
+
+  DISALLOW_COPY_AND_ASSIGN(WatchTimeRecorderTest);
+};
+
+TEST_F(WatchTimeRecorderTest, TestBasicReporting) {
+  constexpr base::TimeDelta kWatchTime1 = base::TimeDelta::FromSeconds(25);
+  constexpr base::TimeDelta kWatchTime2 = base::TimeDelta::FromSeconds(50);
+
+  // Don't include kWatchTimeKeyMax, since we'll use that as a placeholder to
+  // ensure only the keys requested for finalize are finalized.
+  const base::StringPiece kLastKey =
+      WatchTimeKeyToString(WatchTimeKey::kWatchTimeKeyMax);
+  for (int i = 0; i < static_cast<int>(WatchTimeKey::kWatchTimeKeyMax); ++i) {
+    const WatchTimeKey key = static_cast<WatchTimeKey>(i);
+
+    Initialize(true, false, true, true);
+    wtr_->RecordWatchTime(WatchTimeKey::kWatchTimeKeyMax, kWatchTime1);
+    wtr_->RecordWatchTime(key, kWatchTime1);
+    wtr_->RecordWatchTime(key, kWatchTime2);
+    base::RunLoop().RunUntilIdle();
+
+    // Nothing should be recorded yet since we haven't finalized.
+    ExpectWatchTime({}, base::TimeDelta());
+
+    // Only the requested key should be finalized.
+    wtr_->FinalizeWatchTime({key});
+    base::RunLoop().RunUntilIdle();
+
+    const base::StringPiece key_str = WatchTimeKeyToString(key);
+    ExpectWatchTime({key_str}, kWatchTime2);
+
+    // These keys are only reported for a full finalize.
+    ExpectMtbrTime({}, base::TimeDelta());
+    ExpectZeroRebuffers({});
+    if (!key_str.ends_with("EmbeddedExperience"))
+      ExpectUkmWatchTime({key_str}, kWatchTime2);
+
+    // Verify our sentinel key is recorded properly at player destruction.
+    ResetMetricRecorders();
+    wtr_.reset();
+    base::RunLoop().RunUntilIdle();
+
+    ExpectWatchTime({kLastKey}, kWatchTime1);
+    ExpectMtbrTime({}, base::TimeDelta());
+    ExpectZeroRebuffers({});
+    ExpectUkmWatchTime({}, base::TimeDelta());
+    ResetMetricRecorders();
+  }
+}
+
+TEST_F(WatchTimeRecorderTest, FinalizeWithoutWatchTime) {
+  Initialize(true, true, false, true);
+  wtr_.reset();
+  base::RunLoop().RunUntilIdle();
+
+  // No watch time should have been recorded even though a finalize event will
+  // be sent.
+  ExpectWatchTime({}, base::TimeDelta());
+  ExpectMtbrTime({}, base::TimeDelta());
+  ExpectZeroRebuffers({});
+  ExpectUkmWatchTime({}, base::TimeDelta());
+  ASSERT_EQ(0U, test_recorder_->sources_count());
+}
+
+TEST_F(WatchTimeRecorderTest, TestRebufferingMetrics) {
+  Initialize(true, false, true, true);
+
+  constexpr base::TimeDelta kWatchTime = base::TimeDelta::FromSeconds(50);
+  for (auto key : computation_keys_)
+    wtr_->RecordWatchTime(key, kWatchTime);
+  wtr_->UpdateUnderflowCount(1);
+  wtr_->UpdateUnderflowCount(2);
+
+  // Trigger finalization of everything.
+  wtr_->FinalizeWatchTime({});
+  base::RunLoop().RunUntilIdle();
+
+  ExpectMtbrTime(mtbr_keys_, kWatchTime / 2);
+  ExpectRebuffers(smooth_keys_, 2);
+
+  // Now rerun the test without any rebuffering.
+  ResetMetricRecorders();
+  for (auto key : computation_keys_)
+    wtr_->RecordWatchTime(key, kWatchTime);
+  wtr_->FinalizeWatchTime({});
+  base::RunLoop().RunUntilIdle();
+
+  ExpectMtbrTime({}, base::TimeDelta());
+  ExpectZeroRebuffers(smooth_keys_);
+}
+
+}  // namespace media
