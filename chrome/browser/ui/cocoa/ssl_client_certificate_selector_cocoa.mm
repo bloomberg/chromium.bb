d41a5146f0372bfd38845bea60eb63c67c3a9827
Aaron Gable <agable@chromium.org>
2017-08-02 17:01:52 +0000

    Revert "Bump macOS SDK to 10.12."
    This reverts commit fd0388bbb5077a65c2debeca60cb568b394ec40f [formerly f5345de13668f0176b11126a7ae391dad5b4ad0f].
    
    Reason for revert: Breaks bots which run layout_tests
    not via swarming: https://crbug.com/751421
    
    Original change's description:
    > Bump macOS SDK to 10.12.
    > 
    > Bug: 669240
    > Change-Id: I97cf582b97fac7aec2aa92bbdda02f5c17482c1d
    > Reviewed-on: https://chromium-review.googlesource.com/570459
    > Reviewed-by: Erik Chen <erikchen@chromium.org>
    > Reviewed-by: Nico Weber <thakis@chromium.org>
    > Commit-Queue: Erik Chen <erikchen@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#491060}
    
    TBR=thakis@chromium.org,erikchen@chromium.org
    
    Change-Id: I3b30bd3e18fa60f736078ed889a6c574b15c6467
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 669240, 751421
    Reviewed-on: https://chromium-review.googlesource.com/598449
    Commit-Queue: Aaron Gable <agable@chromium.org>
    Reviewed-by: Aaron Gable <agable@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491408}
    Former-commit-id: 00068b7e6365c9ac4994e8f3ee94b8c8a275ab5a

--- chrome/browser/ui/cocoa/ssl_client_certificate_selector_cocoa.mm
+++ chrome/browser/ui/cocoa/ssl_client_certificate_selector_cocoa.mm
@@ -115,12 +115,13 @@ namespace {
 // an NSTableView. Future events may make cause the table view to query its
 // dataSource, which will have been deallocated.
 //
-// Linking against the 10.12 SDK does not "fix" this issue, since
-// NSTableView.dataSource is a "weak" reference, which in non-ARC land still
-// translates to "raw pointer".
+// NSTableView.dataSource becomes a zeroing weak reference starting in 10.11,
+// so this workaround can be removed once we're on the 10.11 SDK.
 //
-// See https://crbug.com/653093, https://crbug.com/750242 and rdar://29409207
-// for more information.
+// See https://crbug.com/653093 and rdar://29409207 for more information.
+
+#if !defined(MAC_OS_X_VERSION_10_11) || \
+    MAC_OS_X_VERSION_MAX_ALLOWED < MAC_OS_X_VERSION_10_11
 
 void ClearTableViewDataSources(NSView* view) {
   if (auto table_view = base::mac::ObjCCast<NSTableView>(view)) {
@@ -149,6 +150,12 @@ void ClearTableViewDataSourcesIfNeeded(NSWindow* leaked_window) {
       FROM_HERE, base::Bind(ClearTableViewDataSourcesIfWindowStillExists,
                             base::Unretained(leaked_window)));
 }
+
+#else
+
+void ClearTableViewDataSourcesIfNeeded(NSWindow*) {}
+
+#endif  // MAC_OS_X_VERSION_10_11
 
 }  // namespace
 
