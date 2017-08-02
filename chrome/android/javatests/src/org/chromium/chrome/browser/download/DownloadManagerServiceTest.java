d468ef1952cf3bd13558220538b1f5e011267aca
Guido Urdaneta <guidou@chromium.org>
2017-08-02 13:22:09 +0000

    Add @RetryOnFailure to DownloadManagerServiceTest#testDownloadCompletedIsCalled
    This test has flaked sometimes on Android bots.
    
    BUG=750566
    TBR=jming@chromium.org
    
    Change-Id: I2b347ba3d055abe8b93a5a2df6a63e32d72f1a24
    Reviewed-on: https://chromium-review.googlesource.com/597851
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491355}
    Former-commit-id: 74ab9f06477c5ffd681c6e6e9d16f17551db0dc0

--- chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadManagerServiceTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/download/DownloadManagerServiceTest.java
@@ -406,6 +406,7 @@ public class DownloadManagerServiceTest {
     @Test
     @MediumTest
     @Feature({"Download"})
+    @RetryOnFailure
     public void testDownloadCompletedIsCalled() throws InterruptedException {
         MockDownloadNotifier notifier = new MockDownloadNotifier(getTestContext());
         MockDownloadSnackbarController snackbarController = new MockDownloadSnackbarController();
