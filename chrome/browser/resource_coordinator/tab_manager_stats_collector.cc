1e8f8cb9f728fe67dbe06da30683f9055a6988d9
Scott Haseley <shaseley@google.com>
2017-08-02 03:44:57 +0000

    Add session restore swap metrics UMAs to TabManager.
    This CL adds UMAs for system swap metrics during session restore. The
    implementation uses SwapMetricsDriver to collect the swap metrics during
    session restore, and computes rates (e.g. swaps in/second) as an average over
    the entire session restore interval. Swap metrics are platform-specific.
    Currently, swap in/out rate is available on Linux and Mac, and page
    compression/decompression rates are available on Mac only.
    
    Bug: 740625
    Change-Id: I3dfb4268759e6900e768756b8939ad64de603a06
    Reviewed-on: https://chromium-review.googlesource.com/585405
    Commit-Queue: Scott Haseley <shaseley@google.com>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Fadi Meawad <fmeawad@chromium.org>
    Reviewed-by: Zhen Wang <zhenw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491254}
    Former-commit-id: d450cb9e0664b13ae630bfeb488b69c98cc30423

--- chrome/browser/resource_coordinator/tab_manager_stats_collector.cc
+++ chrome/browser/resource_coordinator/tab_manager_stats_collector.cc
@@ -4,16 +4,70 @@
 
 #include "chrome/browser/resource_coordinator/tab_manager_stats_collector.h"
 
+#include <cstdint>
+#include <memory>
+#include <utility>
+
+#include "base/memory/ptr_util.h"
 #include "base/metrics/histogram_macros.h"
+#include "base/time/time.h"
 #include "chrome/browser/resource_coordinator/tab_manager.h"
 #include "chrome/browser/resource_coordinator/tab_manager_web_contents_data.h"
+#include "chrome/browser/sessions/session_restore.h"
+#include "content/public/browser/swap_metrics_driver.h"
 
 namespace resource_coordinator {
 
-TabManagerStatsCollector::TabManagerStatsCollector(TabManager* tab_manager)
-    : tab_manager_(tab_manager) {}
+class TabManagerStatsCollector::SessionRestoreSwapMetricsDelegate
+    : public content::SwapMetricsDriver::Delegate {
+ public:
+  explicit SessionRestoreSwapMetricsDelegate(
+      TabManagerStatsCollector* tab_manager_stats_collector)
+      : tab_manager_stats_collector_(tab_manager_stats_collector) {}
 
-TabManagerStatsCollector::~TabManagerStatsCollector() = default;
+  ~SessionRestoreSwapMetricsDelegate() override = default;
+
+  void OnSwapInCount(uint64_t count, base::TimeDelta interval) override {
+    tab_manager_stats_collector_->OnSessionRestoreSwapInCount(count, interval);
+  }
+
+  void OnSwapOutCount(uint64_t count, base::TimeDelta interval) override {
+    tab_manager_stats_collector_->OnSessionRestoreSwapOutCount(count, interval);
+  }
+
+  void OnDecompressedPageCount(uint64_t count,
+                               base::TimeDelta interval) override {
+    tab_manager_stats_collector_->OnSessionRestoreDecompressedPageCount(
+        count, interval);
+  }
+
+  void OnCompressedPageCount(uint64_t count,
+                             base::TimeDelta interval) override {
+    tab_manager_stats_collector_->OnSessionRestoreCompressedPageCount(count,
+                                                                      interval);
+  }
+
+  void OnSessionRestoreUpdateMetricsFailed() {
+    tab_manager_stats_collector_->OnSessionRestoreUpdateMetricsFailed();
+  }
+
+ private:
+  TabManagerStatsCollector* tab_manager_stats_collector_;
+};
+
+TabManagerStatsCollector::TabManagerStatsCollector(TabManager* tab_manager)
+    : tab_manager_(tab_manager), is_session_restore_loading_tabs_(false) {
+  std::unique_ptr<content::SwapMetricsDriver::Delegate> delegate(
+      base::WrapUnique<content::SwapMetricsDriver::Delegate>(
+          new SessionRestoreSwapMetricsDelegate(this)));
+  session_restore_swap_metrics_driver_ = content::SwapMetricsDriver::Create(
+      std::move(delegate), base::TimeDelta::FromSeconds(0));
+  SessionRestore::AddObserver(this);
+}
+
+TabManagerStatsCollector::~TabManagerStatsCollector() {
+  SessionRestore::RemoveObserver(this);
+}
 
 void TabManagerStatsCollector::RecordSwitchToTab(
     content::WebContents* contents) const {
@@ -23,6 +77,63 @@ void TabManagerStatsCollector::RecordSwitchToTab(
     UMA_HISTOGRAM_ENUMERATION("TabManager.SessionRestore.SwitchToTab",
                               data->tab_loading_state(), TAB_LOADING_STATE_MAX);
   }
+}
+
+void TabManagerStatsCollector::OnSessionRestoreStartedLoadingTabs() {
+  DCHECK(!is_session_restore_loading_tabs_);
+  if (session_restore_swap_metrics_driver_)
+    session_restore_swap_metrics_driver_->InitializeMetrics();
+  is_session_restore_loading_tabs_ = true;
+}
+
+void TabManagerStatsCollector::OnSessionRestoreFinishedLoadingTabs() {
+  DCHECK(is_session_restore_loading_tabs_);
+  if (session_restore_swap_metrics_driver_)
+    session_restore_swap_metrics_driver_->UpdateMetrics();
+  is_session_restore_loading_tabs_ = false;
+}
+
+void TabManagerStatsCollector::OnSessionRestoreSwapInCount(
+    uint64_t count,
+    base::TimeDelta interval) {
+  DCHECK(is_session_restore_loading_tabs_);
+  UMA_HISTOGRAM_COUNTS_10000(
+      "TabManager.SessionRestore.SwapInPerSecond",
+      static_cast<double>(count) / interval.InSecondsF());
+}
+
+void TabManagerStatsCollector::OnSessionRestoreSwapOutCount(
+    uint64_t count,
+    base::TimeDelta interval) {
+  DCHECK(is_session_restore_loading_tabs_);
+  UMA_HISTOGRAM_COUNTS_10000(
+      "TabManager.SessionRestore.SwapOutPerSecond",
+      static_cast<double>(count) / interval.InSecondsF());
+}
+
+void TabManagerStatsCollector::OnSessionRestoreDecompressedPageCount(
+    uint64_t count,
+    base::TimeDelta interval) {
+  DCHECK(is_session_restore_loading_tabs_);
+  UMA_HISTOGRAM_COUNTS_10000(
+      "TabManager.SessionRestore.DecompressedPagesPerSecond",
+      static_cast<double>(count) / interval.InSecondsF());
+}
+
+void TabManagerStatsCollector::OnSessionRestoreCompressedPageCount(
+    uint64_t count,
+    base::TimeDelta interval) {
+  DCHECK(is_session_restore_loading_tabs_);
+  UMA_HISTOGRAM_COUNTS_10000(
+      "TabManager.SessionRestore.CompressedPagesPerSecond",
+      static_cast<double>(count) / interval.InSecondsF());
+}
+
+void TabManagerStatsCollector::OnSessionRestoreUpdateMetricsFailed() {
+  // If either InitializeMetrics() or UpdateMetrics() fails, it's unlikely an
+  // error that can be recovered from, in which case we don't collect swap
+  // metrics for session restore.
+  session_restore_swap_metrics_driver_.reset();
 }
 
 }  // namespace resource_coordinator
