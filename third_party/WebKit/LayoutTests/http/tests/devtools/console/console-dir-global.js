b6de0208358fee1d2aed4a6f65e6105d1b154d5b
Will Chen <chenwilliam@chromium.org>
2017-08-02 21:15:51 +0000

    DevTools: transform console tests (part 2)
    F/u from https://chromium-review.googlesource.com/c/595238
    
    Bug: 667560
    Change-Id: I4e387fae79ab4de0965f84a045c4fd04631b9e7d
    Reviewed-on: https://chromium-review.googlesource.com/597312
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491501}
    Former-commit-id: 9869d2f1f8b6eeff130e26652e3075c43f279fc8

--- third_party/WebKit/LayoutTests/http/tests/devtools/console/console-dir-global.js
+++ third_party/WebKit/LayoutTests/http/tests/devtools/console/console-dir-global.js
@@ -1,53 +1,58 @@
-<html>
-<head>
-<script src="../../http/tests/inspector/inspector-test.js"></script>
-<script src="../../http/tests/inspector/console-test.js"></script>
-<script>
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
-function doit()
-{
-    console.dir(window);
-    runTest();
-}
+(async function() {
+  TestRunner.addResult(`Tests that console dumps global object with properties.\n`);
 
-function test()
-{
-    InspectorTest.RuntimeAgent.evaluate("window", "console", false).then(evalCallback);
+  await TestRunner.loadModule('console_test_runner');
+  await TestRunner.showPanel('console');
 
-    function evalCallback(result)
+  await TestRunner.evaluateInPagePromise(`
+    function doit()
     {
-        if (!result) {
-            testController.notifyDone("Exception");
-            return;
-        }
-        if (result.type === "error")
-            testController.notifyDone("Exception:" + result);
-        var objectProxy = InspectorTest.runtimeModel.createRemoteObject(result);
-        objectProxy.getOwnProperties(false, getPropertiesCallback);
+        console.dir(window);
+    };
+  `);
+
+  TestRunner.RuntimeAgent.evaluate('window', 'console', false).then(evalCallback);
+
+  function evalCallback(result) {
+    if (!result) {
+      testController.notifyDone('Exception');
+      return;
     }
 
-    function getPropertiesCallback(properties)
-    {
-        properties.sort(ObjectUI.ObjectPropertiesSection.CompareProperties);
-        var golden = { "window": 1, "document": 1, "eval": 1, "console": 1, "frames": 1, "Array": 1, "doit": 1 }; 
-        var result = {};
-        for (var i = 0; i < properties.length; ++i) {
-            var name = properties[i].name;
-            if (golden[name])
-                result[name] = 1;
-        }
-        InspectorTest.addObject(result);
-        InspectorTest.completeTest();
+    if (result.type === 'error')
+      testController.notifyDone('Exception:' + result);
+
+    var objectProxy = TestRunner.runtimeModel.createRemoteObject(result);
+    objectProxy.getOwnProperties(false, getPropertiesCallback);
+  }
+
+  function getPropertiesCallback(properties) {
+    properties.sort(ObjectUI.ObjectPropertiesSection.CompareProperties);
+
+    var golden = {
+      'window': 1,
+      'document': 1,
+      'eval': 1,
+      'console': 1,
+      'frames': 1,
+      'Array': 1,
+      'doit': 1
+    };
+
+    var result = {};
+
+    for (var i = 0; i < properties.length; ++i) {
+      var name = properties[i].name;
+
+      if (golden[name])
+        result[name] = 1;
     }
-}
 
-</script>
-</head>
-
-<body onload="doit()">
-<p>
-Tests that console dumps global object with properties.
-</p>
-
-</body>
-</html>
+    TestRunner.addObject(result);
+    TestRunner.completeTest();
+  }
+})();
