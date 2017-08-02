3bb057cff239829f4cbf6e356069c80a76ca5a96
Ella Ge <eirage@chromium.org>
2017-08-02 17:40:21 +0000

    cleanup a line for remove PointerEvent flag
    Remove a line missed when removing enable-pointer-event
    flag. Related cl:
    
    https: //codereview.chromium.org/2921923002
    Bug: 687976
    Change-Id: I1d129b05fce79a565ff9a92493d2e16c2d383cf8
    Reviewed-on: https://chromium-review.googlesource.com/596689
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Ella Ge <eirage@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491426}
    Former-commit-id: 83e2cb3b273b884e384be7abeef3fc72c4bd82c8

--- content/public/common/content_features.h
+++ content/public/common/content_features.h
@@ -59,7 +59,6 @@ CONTENT_EXPORT extern const base::Feature kParallelDownloading;
 CONTENT_EXPORT extern const base::Feature kPassiveDocumentEventListeners;
 CONTENT_EXPORT extern const base::Feature kPassiveEventListenersDueToFling;
 CONTENT_EXPORT extern const base::Feature kPepper3DImageChromium;
-CONTENT_EXPORT extern const base::Feature kPointerEvents;
 CONTENT_EXPORT extern const base::Feature kPurgeAndSuspend;
 CONTENT_EXPORT extern const base::Feature kRafAlignedMouseInputEvents;
 CONTENT_EXPORT extern const base::Feature kRafAlignedTouchInputEvents;
