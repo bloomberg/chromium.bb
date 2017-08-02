aefd8b5c85ebac1e94ffb5ffced7b594783b0f91
Allen Bauer <kylixrd@chromium.org>
2017-08-02 18:47:58 +0000

    Fix disabled stroke color processing to ensure the non-Harmony disabled color is properly lightened.
    Bug: 737271
    Change-Id: Ie6bb767b8138472037ffb685091cab8c8986b662
    Reviewed-on: https://chromium-review.googlesource.com/581630
    Commit-Queue: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Reviewed-by: Allen Bauer <kylixrd@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491451}
    Former-commit-id: 867e878f0a6a6aa940e3851f3df93cff54d4ea37

--- ui/views/controls/button/md_text_button.cc
+++ ui/views/controls/button/md_text_button.cc
@@ -243,6 +243,7 @@ void MdTextButton::UpdatePadding() {
 
 void MdTextButton::UpdateColors() {
   ui::NativeTheme* theme = GetNativeTheme();
+  bool is_disabled = state() == STATE_DISABLED;
   SkColor enabled_text_color = style::GetColor(
       label()->text_context(),
       is_prominent_ ? style::STYLE_DIALOG_BUTTON_DEFAULT : style::STYLE_PRIMARY,
@@ -250,10 +251,15 @@ void MdTextButton::UpdateColors() {
   if (!explicitly_set_normal_color()) {
     const auto colors = explicitly_set_colors();
     LabelButton::SetEnabledTextColors(enabled_text_color);
-    if (state() == STATE_DISABLED && !is_prominent_)
+    // Non-prominent, disabled buttons need the disabled color explicitly set.
+    // This ensures that label()->enabled_color() returns the correct color as
+    // the basis for calculating the stroke color. enabled_text_color isn't used
+    // since a descendant could have overridden the label enabled color.
+    if (is_disabled && !is_prominent_) {
       LabelButton::SetTextColor(STATE_DISABLED,
                                 style::GetColor(label()->text_context(),
                                                 style::STYLE_DISABLED, theme));
+    }
     set_explicitly_set_colors(colors);
   }
 
@@ -271,9 +277,10 @@ void MdTextButton::UpdateColors() {
   } else if (is_prominent_) {
     bg_color = theme->GetSystemColor(
         ui::NativeTheme::kColorId_ProminentButtonColor);
-    if (state() == STATE_DISABLED)
+    if (is_disabled) {
       bg_color = color_utils::BlendTowardOppositeLuma(
           bg_color, gfx::kDisabledControlAlpha);
+    }
   }
 
   if (state() == STATE_PRESSED) {
@@ -282,24 +289,27 @@ void MdTextButton::UpdateColors() {
     bg_color = color_utils::GetResultingPaintColor(shade, bg_color);
   }
 
-  // Specified text color: 5a5a5a @ 1.0 alpha, 757575 @ 1.0 alpha for Harmony
-  // Specified stroke color: 000000 @ 0.2 alpha, Harmony and non-Harmony
-  // 000000 @ 0.2 is very close to 5a5a5a @ 0.308 (== 0x4e) or 757575 @ 0.37
-  // (== 0x5f); both are cccccc @ 1.0, and this way if NativeTheme changes the
-  // button color, the button stroke will also change colors to match.
-  SkColor stroke_color =
-      is_prominent_ ? SK_ColorTRANSPARENT
-                    : SkColorSetA(text_color,
-                                  UseMaterialSecondaryButtons() ? 0x5f : 0x4e);
-
-  // Disabled, non-prominent buttons need their stroke lightened. The Harmony
-  // spec asks for 000000 @ 0.1 alpha or e6e6e6 @ 1.0 alpha. The stroke color
-  // has already been translated above to cccccc @ 1.0 or 000000 @ 0.2.
-  // cccccc @ 0.25 (== 0x41) translates to e6e6e6 @ 1.0. To simplify the code,
-  // 000000 @ 0.1 is being used for both Harmony and non-Harmony. Prominent
-  // buttons need it left at SK_ColorTRANSPARENT from above.
-  if (state() == STATE_DISABLED && !is_prominent_)
-    stroke_color = SkColorSetA(stroke_color, 0x41);
+  SkColor stroke_color;
+  if (is_prominent_) {
+    stroke_color = SK_ColorTRANSPARENT;
+  } else {
+    int stroke_alpha;
+    if (is_disabled) {
+      // Disabled, non-prominent buttons need a lighter stroke. This alpha
+      // value will take the disabled button colors, a1a192 @ 1.0 alpha for
+      // non-Harmony, 9e9e9e @ 1.0 alpha for Harmony and turn it into
+      // e6e6e6 @ 1.0 alpha (or very close to it) or an effective 000000 @ 0.1
+      // alpha for the stroke color. The same alpha value will work with both
+      // Harmony and non-Harmony colors.
+      stroke_alpha = 0x43;
+    } else {
+      // These alpha values will take the enabled button colors, 5a5a5a @ 1.0
+      // alpha for non-Harmony, 757575 @ 1.0 alpha for Harmony and turn it into
+      // an effective cccccc @ 1.0 alpha or 000000 @ 0.2 for the stroke_color.
+      stroke_alpha = UseMaterialSecondaryButtons() ? 0x5f : 0x4e;
+    }
+    stroke_color = SkColorSetA(text_color, stroke_alpha);
+  }
 
   DCHECK_EQ(SK_AlphaOPAQUE, static_cast<int>(SkColorGetA(bg_color)));
   SetBackground(
