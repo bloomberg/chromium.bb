1c4e83dd340b718ad19142c3d4e8d1b17434bf8f
James Cook <jamescook@chromium.org>
2017-08-02 18:48:36 +0000

    cros: Fix invisible shelf when external display attached at lock screen
    When an external display is attached a new RootWindowController and new
    shelf are created for that display. Shelf alignment is loaded from prefs.
    However, if the external display is marked as primary then the internal
    and external displays are swapped. This means that the (new) shelf now
    on the internal display never reloads its alignment from prefs. Since
    the screen was locked at attach time, the shelf stays in the
    bottom_locked state and never becomes visible.
    
    Fix this by reloaded shelf alignment from prefs on display swap.
    
    Bug: 748291
    Test: added to ash_unittests
    Change-Id: I5780dd3ca3b796b6f46fea9bc46fe875d123701e
    Reviewed-on: https://chromium-review.googlesource.com/597410
    Commit-Queue: James Cook <jamescook@chromium.org>
    Reviewed-by: Michael Wasserman <msw@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491452}
    Former-commit-id: 9a6ef135259507439b954aeaef887cf0ebc74a10

--- ash/shelf/shelf.h
+++ ash/shelf/shelf.h
@@ -8,6 +8,7 @@
 #include <memory>
 
 #include "ash/ash_export.h"
+#include "ash/display/window_tree_host_manager.h"
 #include "ash/public/cpp/shelf_types.h"
 #include "ash/shelf/shelf_layout_manager_observer.h"
 #include "ash/shelf/shelf_locking_manager.h"
@@ -40,7 +41,8 @@ class ShelfObserver;
 // Controller for the shelf state. One per display, because each display might
 // have different shelf alignment, autohide, etc. Exists for the lifetime of the
 // root window controller.
-class ASH_EXPORT Shelf : public ShelfLayoutManagerObserver {
+class ASH_EXPORT Shelf : public ShelfLayoutManagerObserver,
+                         public WindowTreeHostManager::Observer {
  public:
   Shelf();
   ~Shelf() override;
@@ -145,6 +147,13 @@ class ASH_EXPORT Shelf : public ShelfLayoutManagerObserver {
   void OnAutoHideStateChanged(ShelfAutoHideState new_state) override;
   void OnBackgroundUpdated(ShelfBackgroundType background_type,
                            AnimationChangeType change_type) override;
+
+  // WindowTreeHostManager::Observer:
+  void OnWindowTreeHostReusedForDisplay(
+      AshWindowTreeHost* window_tree_host,
+      const display::Display& display) override;
+  void OnWindowTreeHostsSwappedDisplays(AshWindowTreeHost* host1,
+                                        AshWindowTreeHost* host2) override;
 
  private:
   class AutoHideEventHandler;
