0f9cf2d1fe4c167ac37bff4c2966c4c8425aa6da
Jerome Jiang <jianj@google.com>
2017-08-02 03:27:51 +0000

    Roll src/third_party/libvpx/source/libvpx/ 8836e46ff..c22b17dce (20 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/8836e46ffd84..c22b17dcefe0
    
    $ git log 8836e46ff..c22b17dce --date=short --no-merges --format='%ad %ae %s'
    2017-07-31 marpan vp9: Adjust noise estimation for 360p.
    2017-07-28 marpan vp9: Fix denoising condition when pickmode partition is used.
    2017-07-30 jianj Revert "Revert "vp9: Speed feature to adapt partition based on source_sad.""
    2017-07-30 jzern highbd_inv_txfm_sse4: make << of neg. val a multiply
    2017-07-29 jzern Revert "vp9: Speed feature to adapt partition based on source_sad."
    2017-07-29 jzern Revert "Rewrite vpx_highbd_idct8x8_{12,64}_add_sse2"
    2017-07-28 jianj vp9: Adjust logic in source sad for screen content.
    2017-07-28 marpan vp9: Speed feature to adapt partition based on source_sad.
    2017-07-28 urvang Remove the DP version of vp9_optimize_b().
    2017-07-28 jzern Revert "quantize ssse3: declare all variables"
    2017-07-21 linfengz Update vpx_idct16x16_10_add_sse2()
    2017-07-20 linfengz Add vpx_idct16x16_38_add_sse2()
    2017-06-30 linfengz Rewrite vpx_highbd_idct8x8_{12,64}_add_sse2
    2017-06-30 linfengz Refactor highbd idct 4x4 and 8x8 x86 functions
    2017-07-19 jianj vp8: Remove isolated skin & non skin blocks.
    2017-07-25 jzern inv_txfm_{sse2,ssse3}: clear conversion warnings
    2017-07-25 jzern highbd_idct*_sse*.c: clear conversion warnings
    2017-07-25 jzern vpx_variance16x16_sse2: correct cast order
    2017-07-24 jzern vpx_get16x16var_avx2: correct cast order
    2017-07-18 johannkoenig quantize ssse3: declare all variables
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=johannkoenig@google.com
    
    Bug: 
    Change-Id: I8b26cd0a399df47482300a0c8d9395f2b28669f2
    Reviewed-on: https://chromium-review.googlesource.com/596499
    Commit-Queue: Jerome Jiang <jianj@google.com>
    Reviewed-by: Johann Koenig <johannkoenig@google.com>
    Cr-Commit-Position: refs/heads/master@{#491249}
    Former-commit-id: 3ddb612ca94702a4fc17dae3d04487e6b6a58763

--- third_party/libvpx/source/config/win/x64/vpx_dsp_rtcd.h
+++ third_party/libvpx/source/config/win/x64/vpx_dsp_rtcd.h
@@ -1348,8 +1348,8 @@ void vpx_idct16x16_256_add_sse2(const tran_low_t *input, uint8_t *dest, int stri
 #define vpx_idct16x16_256_add vpx_idct16x16_256_add_sse2
 
 void vpx_idct16x16_38_add_c(const tran_low_t *input, uint8_t *dest, int stride);
-void vpx_idct16x16_256_add_sse2(const tran_low_t *input, uint8_t *dest, int stride);
-#define vpx_idct16x16_38_add vpx_idct16x16_256_add_sse2
+void vpx_idct16x16_38_add_sse2(const tran_low_t *input, uint8_t *dest, int stride);
+#define vpx_idct16x16_38_add vpx_idct16x16_38_add_sse2
 
 void vpx_idct32x32_1024_add_c(const tran_low_t *input, uint8_t *dest, int stride);
 void vpx_idct32x32_1024_add_sse2(const tran_low_t *input, uint8_t *dest, int stride);
