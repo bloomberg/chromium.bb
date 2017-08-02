2eaf887aa6b50cdec5018fe263ce6fb7f9723719
Matt Falkenhagen <falken@chromium.org>
2017-08-02 05:30:14 +0000

    Documentation: For service worker "providers"
    Bug: 740070
    Change-Id: Ida0ce2080e724784cfb775d30004805cc70812f4
    Reviewed-on: https://chromium-review.googlesource.com/595301
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491275}
    Former-commit-id: 472df758deea89c7c5e55141dc5d6cf8b038ce28

--- third_party/WebKit/public/platform/modules/serviceworker/WebServiceWorkerProviderClient.h
+++ third_party/WebKit/public/platform/modules/serviceworker/WebServiceWorkerProviderClient.h
@@ -42,8 +42,11 @@ namespace blink {
 class WebServiceWorker;
 class WebString;
 
-// This class is the interface for embedders to talk to
-// ServiceWorkerContainer.
+// See WebServiceWorkerProvider for full documentation.
+//
+// WebServiceWorkerProviderClient is implemented by ServiceWorkerContainer.
+// We probably wouldn't need this abstract class, except we also make a
+// MockServiceWorkerProviderClient for unit tests.
 class WebServiceWorkerProviderClient {
  public:
   virtual ~WebServiceWorkerProviderClient() {}
