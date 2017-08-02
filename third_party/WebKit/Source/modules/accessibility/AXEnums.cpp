e3661155964c38bbfb060a993c89d6e2a00afe85
Morten Stenshorne <mstensho@opera.com>
2017-08-02 16:10:06 +0000

    Don't #undef STATIC_ASSERT_ENUM.
    Causes problems for Jumbo builds.
    
    TBR=nverne@chromium.org
    
    Change-Id: If4a37cff9ae787ec0c750b652663fd2d8d6b1a66
    Reviewed-on: https://chromium-review.googlesource.com/598090
    Reviewed-by: Morten Stenshorne <mstensho@opera.com>
    Reviewed-by: Nicholas Verne <nverne@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@opera.com>
    Commit-Queue: Nicholas Verne <nverne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491391}
    Former-commit-id: c7ad64f17a94278beac3d64c449d243b76d9ce48

--- third_party/WebKit/Source/modules/accessibility/AXEnums.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXEnums.cpp
@@ -229,5 +229,4 @@ STATIC_ASSERT_ENUM(WebAXObjectAttribute::kAriaDetails,
                    AXObjectAttribute::kAriaDetails);
 STATIC_ASSERT_ENUM(WebAXObjectVectorAttribute::kAriaFlowTo,
                    AXObjectVectorAttribute::kAriaFlowTo);
-#undef STATIC_ASSERT_ENUM
 }  // namespace blink
