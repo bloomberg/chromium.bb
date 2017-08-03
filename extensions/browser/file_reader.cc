7467dd39aaec59a488395e6314188ce81395dea2
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-08-03 00:23:33 +0000

    [Task Migration][Extensions] Update FileReader
    Update the FileReader class to use the new task scheduling API, as well
    as the dedicated Extensions-related file task runner.
    
    Bug: 689520
    Bug: 750122
    Change-Id: I3dd119e9c6bc368ab0186e599a4a67df02b872d6
    Reviewed-on: https://chromium-review.googlesource.com/594010
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Karan Bhatia <karandeepb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491566}
    Former-commit-id: 06ba081e89937dea18efa31eeecf8162dafd7434

--- extensions/browser/file_reader.cc
+++ extensions/browser/file_reader.cc
@@ -8,37 +8,36 @@
 #include "base/callback_helpers.h"
 #include "base/files/file_util.h"
 #include "base/threading/thread_task_runner_handle.h"
-#include "content/public/browser/browser_thread.h"
-
-using content::BrowserThread;
+#include "extensions/browser/extension_file_task_runner.h"
 
 FileReader::FileReader(
     const extensions::ExtensionResource& resource,
-    const OptionalFileThreadTaskCallback& optional_file_thread_task_callback,
+    const OptionalFileSequenceTask& optional_file_sequence_task,
     const DoneCallback& done_callback)
     : resource_(resource),
-      optional_file_thread_task_callback_(optional_file_thread_task_callback),
+      optional_file_sequence_task_(optional_file_sequence_task),
       done_callback_(done_callback),
       origin_task_runner_(base::ThreadTaskRunnerHandle::Get()) {}
 
 void FileReader::Start() {
-  BrowserThread::PostTask(
-      BrowserThread::FILE, FROM_HERE,
-      base::Bind(&FileReader::ReadFileOnBackgroundThread, this));
+  extensions::GetExtensionFileTaskRunner()->PostTask(
+      FROM_HERE, base::Bind(&FileReader::ReadFileOnFileSequence, this));
 }
 
 FileReader::~FileReader() {}
 
-void FileReader::ReadFileOnBackgroundThread() {
+void FileReader::ReadFileOnFileSequence() {
+  DCHECK(
+      extensions::GetExtensionFileTaskRunner()->RunsTasksInCurrentSequence());
+
   std::unique_ptr<std::string> data(new std::string());
   bool success = base::ReadFileToString(resource_.GetFilePath(), data.get());
 
-  if (!optional_file_thread_task_callback_.is_null()) {
+  if (!optional_file_sequence_task_.is_null()) {
     if (success) {
-      base::ResetAndReturn(&optional_file_thread_task_callback_)
-          .Run(data.get());
+      base::ResetAndReturn(&optional_file_sequence_task_).Run(data.get());
     } else {
-      optional_file_thread_task_callback_.Reset();
+      optional_file_sequence_task_.Reset();
     }
   }
 
--- extensions/browser/file_reader.h
+++ extensions/browser/file_reader.h
@@ -13,37 +13,40 @@
 #include "extensions/common/extension_resource.h"
 
 // This file defines an interface for reading a file asynchronously on a
-// background thread.
+// background sequence.
 // Consider abstracting out a FilePathProvider (ExtensionResource) and moving
 // back to chrome/browser/net if other subsystems want to use it.
 class FileReader : public base::RefCountedThreadSafe<FileReader> {
  public:
+  // TODO(devlin): Use base::OnceCallback here.
   // Reports success or failure and the data of the file upon success.
   using DoneCallback = base::Callback<void(bool, std::unique_ptr<std::string>)>;
   // Lets the caller accomplish tasks on the file data, after the file content
   // has been read.
   // If the file reading doesn't succeed, this will be ignored.
-  using OptionalFileThreadTaskCallback = base::Callback<void(std::string*)>;
+  using OptionalFileSequenceTask = base::Callback<void(std::string*)>;
 
   FileReader(const extensions::ExtensionResource& resource,
-             const OptionalFileThreadTaskCallback& file_thread_task_callback,
+             const OptionalFileSequenceTask& file_sequence_task,
              const DoneCallback& done_callback);
 
-  // Called to start reading the file on a background thread.  Upon completion,
+  // Called to start reading the file on a background sequence. Upon completion,
   // the callback will be notified of the results.
   void Start();
 
  private:
   friend class base::RefCountedThreadSafe<FileReader>;
 
-  virtual ~FileReader();
+  ~FileReader();
 
-  void ReadFileOnBackgroundThread();
+  void ReadFileOnFileSequence();
 
   extensions::ExtensionResource resource_;
-  OptionalFileThreadTaskCallback optional_file_thread_task_callback_;
+  OptionalFileSequenceTask optional_file_sequence_task_;
   DoneCallback done_callback_;
   const scoped_refptr<base::SingleThreadTaskRunner> origin_task_runner_;
+
+  DISALLOW_COPY_AND_ASSIGN(FileReader);
 };
 
 #endif  // EXTENSIONS_BROWSER_FILE_READER_H_
