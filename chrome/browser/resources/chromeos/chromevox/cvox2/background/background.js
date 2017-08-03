48a9e71641f722875d76a67bdea914f67d77bfff
David Tseng <dtseng@chromium.org>
2017-08-03 02:02:54 +0000

    Allow braille cursor routing at the end of the displayed braille content
    Test: press braille cursor routing key one characer beyond the last character of the displayed text. See action applied to the end of the content such as selection.
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I50a6e5f1f1d708595ad52d026bf279267c96a233
    Reviewed-on: https://chromium-review.googlesource.com/599108
    Commit-Queue: David Tseng <dtseng@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491610}
    Former-commit-id: 58fab5d0b933d5264c85bcba614857aa84357b20

--- chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
+++ chrome/browser/resources/chromeos/chromevox/cvox2/background/background.js
@@ -634,7 +634,14 @@ Background.prototype = {
   brailleRoutingCommand_: function(text, position) {
     var actionNodeSpan = null;
     var selectionSpan = null;
-    text.getSpans(position).forEach(function(span) {
+
+    // For a routing key press one cell beyond the last displayed character, use
+    // the node span for the last character. This enables routing selection to
+    // the length of the text, which is valid.
+    var nodePosition = position;
+    if (position == text.length && position > 0)
+      nodePosition--;
+    text.getSpans(nodePosition).forEach(function(span) {
       if (span instanceof Output.SelectionSpan) {
         selectionSpan = span;
       } else if (span instanceof Output.NodeSpan) {
