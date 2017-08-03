06fabbf8a2cb80b4cc62514dd7004cb530b3068e
Francois Doray <fdoray@chromium.org>
2017-08-03 18:49:22 +0000

    Remove usage of ScopedAsyncTaskScheduler from chrome_component_updater_configurator_unittest.cc.
    The ScopedAsyncTaskScheduler in 
    chrome_component_updater_configurator_unittest.cc was useless. This CL
    simply removes it.
    
    Bug: 708584
    Change-Id: Ia1cb9bfb343ddc5810421dc655227128c3a40979
    Reviewed-on: https://chromium-review.googlesource.com/591949
    Commit-Queue: Francois Doray <fdoray@chromium.org>
    Reviewed-by: Sorin Jianu <sorin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491806}
    Former-commit-id: 850e8aa588a8bec900bfb533dad9a1c63fb06091

--- chrome/browser/component_updater/chrome_component_updater_configurator_unittest.cc
+++ chrome/browser/component_updater/chrome_component_updater_configurator_unittest.cc
@@ -9,7 +9,6 @@
 #include "base/command_line.h"
 #include "base/memory/ptr_util.h"
 #include "base/memory/ref_counted.h"
-#include "base/test/scoped_async_task_scheduler.h"
 #include "chrome/browser/component_updater/chrome_component_updater_configurator.h"
 #include "components/component_updater/component_updater_switches.h"
 #include "components/component_updater/component_updater_url_constants.h"
@@ -34,7 +33,6 @@ class ChromeComponentUpdaterConfiguratorTest : public testing::Test {
   TestingPrefServiceSimple* pref_service() { return pref_service_.get(); }
 
  private:
-  base::test::ScopedAsyncTaskScheduler scoped_async_task_scheduler_;
   std::unique_ptr<TestingPrefServiceSimple> pref_service_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeComponentUpdaterConfiguratorTest);
