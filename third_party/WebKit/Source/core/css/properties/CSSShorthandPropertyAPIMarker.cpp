576d3510c5799bd4596403a72a6841c8ea00eb42
Jia <jiameng@chromium.org>
2017-08-03 09:55:14 +0000

    Implement parseShorthand API for marker and grid-gap.
    For marker property, CSSPropertyInvalid was used as the 
    "current_shorthand" in the old/non-API parsing method 
    because it's not used. In its API, we use 
    CSSPropertyMarker just to avoid confusion.  There is no 
    real difference.
    
    Bug: 668012
    Change-Id: I528f7da64d6dc6537a248e8b6a08b3940c0f3b90
    Reviewed-on: https://chromium-review.googlesource.com/597092
    Commit-Queue: Jia Meng <jiameng@chromium.org>
    Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491688}
    Former-commit-id: bf4bb81bf023901d629086e8eb7fd1f252da3ae7

---
+++ third_party/WebKit/Source/core/css/properties/CSSShorthandPropertyAPIMarker.cpp
@@ -0,0 +1,38 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/css/properties/CSSShorthandPropertyAPIMarker.h"
+
+#include "core/css/parser/CSSPropertyParserHelpers.h"
+
+namespace blink {
+
+bool CSSShorthandPropertyAPIMarker::parseShorthand(
+    bool important,
+    CSSParserTokenRange& range,
+    const CSSParserContext& context,
+    bool use_legacy_parsing,
+    HeapVector<CSSProperty, 256>& properties) {
+  bool needs_legacy_parsing = false;
+  const CSSValue* marker = CSSPropertyParserHelpers::ParseLonghandViaAPI(
+      CSSPropertyMarkerStart, CSSPropertyMarker, context, range,
+      needs_legacy_parsing);
+  if (!marker || !range.AtEnd())
+    return false;
+
+  DCHECK(!needs_legacy_parsing);
+
+  CSSPropertyParserHelpers::AddProperty(
+      CSSPropertyMarkerStart, CSSPropertyMarker, *marker, important,
+      CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+  CSSPropertyParserHelpers::AddProperty(
+      CSSPropertyMarkerMid, CSSPropertyMarker, *marker, important,
+      CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+  CSSPropertyParserHelpers::AddProperty(
+      CSSPropertyMarkerEnd, CSSPropertyMarker, *marker, important,
+      CSSPropertyParserHelpers::IsImplicitProperty::kNotImplicit, properties);
+  return true;
+}
+
+}  // namespace blink
