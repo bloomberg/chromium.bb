8263038a5d8a786b5c5783222e0688f5887153a9
Wez <wez@chromium.org>
2017-08-03 18:36:32 +0000

    Re-enable CheckStreamsAreLazy and Dcheck on iOS.
    Change-Id: I310dcfaa28be9c06c9bcd4524ceae32a38362ce7
    Reviewed-on: https://chromium-review.googlesource.com/572502
    Reviewed-by: Hector Dearman <hjd@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Commit-Queue: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491801}
    Former-commit-id: 303841d28a97141aca8b488bc58304efc50aef49

--- base/logging_unittest.cc
+++ base/logging_unittest.cc
@@ -198,13 +198,7 @@ TEST_F(LoggingTest, LoggingIsLazyByDestination) {
 // Official builds have CHECKs directly call BreakDebugger.
 #if !defined(OFFICIAL_BUILD)
 
-// https://crbug.com/709067 tracks test flakiness on iOS.
-#if defined(OS_IOS)
-#define MAYBE_CheckStreamsAreLazy DISABLED_CheckStreamsAreLazy
-#else
-#define MAYBE_CheckStreamsAreLazy CheckStreamsAreLazy
-#endif
-TEST_F(LoggingTest, MAYBE_CheckStreamsAreLazy) {
+TEST_F(LoggingTest, CheckStreamsAreLazy) {
   MockLogSource mock_log_source, uncalled_mock_log_source;
   EXPECT_CALL(mock_log_source, Log()).Times(8).
       WillRepeatedly(Return("check message"));
@@ -220,7 +214,7 @@ TEST_F(LoggingTest, MAYBE_CheckStreamsAreLazy) {
       << mock_log_source.Log();
 }
 
-#endif
+#endif  // !defined(OFFICIAL_BUILD)
 
 #if defined(OFFICIAL_BUILD) && defined(OS_WIN)
 NOINLINE void CheckContainingFunc(int death_location) {
@@ -405,13 +399,7 @@ void DcheckEmptyFunction1() {
 }
 void DcheckEmptyFunction2() {}
 
-// https://crbug.com/709067 tracks test flakiness on iOS.
-#if defined(OS_IOS)
-#define MAYBE_Dcheck DISABLED_Dcheck
-#else
-#define MAYBE_Dcheck Dcheck
-#endif
-TEST_F(LoggingTest, MAYBE_Dcheck) {
+TEST_F(LoggingTest, Dcheck) {
 #if defined(NDEBUG) && !defined(DCHECK_ALWAYS_ON)
   // Release build.
   EXPECT_FALSE(DCHECK_IS_ON());
