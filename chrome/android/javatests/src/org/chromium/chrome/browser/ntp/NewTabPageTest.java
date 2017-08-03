bf3d6229f5419ed0476b5c1514832621ed9773fc
Michael van Ouwerkerk <mvanouwerkerk@google.com>
2017-08-03 10:03:23 +0000

    Add entry to test config for NTPCondensedLayout.
    Bug: 715998
    Change-Id: I0066c9f3e87c2e3327ece2d1b357de1c4bd9c191
    Reviewed-on: https://chromium-review.googlesource.com/595981
    Commit-Queue: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491690}
    Former-commit-id: 8567a59d3283d7e0a6d0d4f818d83fbfba8d0770

--- chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/browser/ntp/NewTabPageTest.java
@@ -136,6 +136,7 @@ public class NewTabPageTest {
     @Test
     @MediumTest
     @Feature({"NewTabPage", "RenderTest"})
+    @CommandLineFlags.Add("disable-features=NTPCondensedLayout")
     public void testRender() throws IOException {
         mActivityTestRule.getInstrumentation().waitForIdleSync();
         mRenderTestRule.render(mTileGridLayout, "most_visited");
