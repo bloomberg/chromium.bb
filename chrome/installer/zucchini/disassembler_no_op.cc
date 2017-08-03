a7640ca1bba242ef1692f07f444b5f33bd8642b4
Etienne Pierre-Doray <etiennep@chromium.org>
2017-08-03 00:34:37 +0000

    [Zucchini] Implement DisassemblerNoOp.
    DisassemblerNoOp is a Disassembler that does not look for reference, and
    used as fallback for patching Elements not recognised as executable.
    
    Bug: 729154
    Change-Id: I4cbf355fb59bcf7df12b9e6d81029c771c52f215
    Reviewed-on: https://chromium-review.googlesource.com/599023
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491569}
    Former-commit-id: c97e0cf1d8c1d1b961de77505b9abf9c8beed7fc

---
+++ chrome/installer/zucchini/disassembler_no_op.cc
@@ -0,0 +1,36 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/installer/zucchini/disassembler_no_op.h"
+
+namespace zucchini {
+
+std::unique_ptr<DisassemblerNoOp> DisassemblerNoOp::Make(
+    ConstBufferView image) {
+  std::unique_ptr<DisassemblerNoOp> disasm(new DisassemblerNoOp());
+  if (!disasm->Parse(image))
+    return nullptr;
+  return disasm;
+}
+
+DisassemblerNoOp::DisassemblerNoOp() = default;
+DisassemblerNoOp::~DisassemblerNoOp() = default;
+
+ExecutableType DisassemblerNoOp::GetExeType() const {
+  return kExeTypeNoOp;
+}
+
+std::string DisassemblerNoOp::GetExeTypeString() const {
+  return "(Unknown)";
+}
+
+std::vector<ReferenceGroup> DisassemblerNoOp::GetReferenceGroups() const {
+  return std::vector<ReferenceGroup>();
+}
+
+bool DisassemblerNoOp::Parse(ConstBufferView image) {
+  return true;
+}
+
+}  // namespace zucchini
