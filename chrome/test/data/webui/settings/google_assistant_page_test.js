5443929e66021cf1bfaea7c1ac4950cfb5fd0870
Ryan Cui <rcui@chromium.org>
2017-08-03 03:33:27 +0000

    Browser tests for Google Assistant Web UI settings
    Bug: 727873
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ieca4aab7371e7208d49c78795f8da076bd237838
    Reviewed-on: https://chromium-review.googlesource.com/587991
    Commit-Queue: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: Demetrios Papadopoulos <dpapad@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491632}
    Former-commit-id: 538419f311c7ede9de97b4107d9aacb33a964286

---
+++ chrome/test/data/webui/settings/google_assistant_page_test.js
@@ -0,0 +1,111 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @implements {settings.GoogleAssistantBrowserProxy}
+ */
+class TestGoogleAssistantBrowserProxy extends TestBrowserProxy {
+  constructor() {
+    super([
+      'setGoogleAssistantEnabled',
+      'setGoogleAssistantContextEnabled',
+      'showGoogleAssistantSettings',
+    ]);
+  }
+
+  /** @override */
+  setGoogleAssistantEnabled(enabled) {
+    this.methodCalled('setGoogleAssistantEnabled', enabled);
+  }
+
+  /** @override */
+  setGoogleAssistantContextEnabled(enabled) {
+    this.methodCalled('setGoogleAssistantContextEnabled', enabled);
+  }
+
+  /** @override */
+  showGoogleAssistantSettings() {
+    this.methodCalled('showGoogleAssistantSettings');
+  }
+}
+
+suite('GoogleAssistantHandler', function() {
+
+  /** @type {SettingsGoogleAssistantPageElement} */
+  var page = null;
+
+  /** @type {?TestGoogleAssistantBrowserProxy} */
+  var browserProxy = null;
+
+  setup(function() {
+    browserProxy = new TestGoogleAssistantBrowserProxy();
+    settings.GoogleAssistantBrowserProxyImpl.instance_ = browserProxy;
+
+    PolymerTest.clearBody();
+
+    var prefElement = document.createElement('settings-prefs');
+    document.body.appendChild(prefElement);
+
+    return CrSettingsPrefs.initialized.then(function() {
+      page = document.createElement('settings-google-assistant-page');
+      page.prefs = prefElement.prefs;
+      document.body.appendChild(page);
+    });
+  });
+
+  teardown(function() {
+    page.remove();
+  });
+
+  test('toggleAssistant', function() {
+    Polymer.dom.flush();
+    var button = page.$$('#googleAssistantEnable');
+    assertTrue(!!button);
+    assertFalse(button.disabled);
+    assertFalse(button.checked);
+
+    // Tap the enable toggle button and ensure the state becomes enabled.
+    MockInteractions.tap(button.$.control);
+    Polymer.dom.flush();
+    assertTrue(button.checked);
+    return browserProxy.whenCalled('setGoogleAssistantEnabled')
+        .then(function(arg) {
+          assertTrue(arg);
+        });
+  });
+
+  test('toggleAssistantContext', function() {
+    var button = page.$$('#googleAssistantContextEnable');
+    assertFalse(!!button);
+    page.setPrefValue('settings.voice_interaction.enabled', true);
+    Polymer.dom.flush();
+    button = page.$$('#googleAssistantContextEnable');
+    assertTrue(!!button);
+    assertFalse(button.disabled);
+    assertFalse(button.checked);
+
+    MockInteractions.tap(button.$.control);
+    Polymer.dom.flush();
+    assertTrue(button.checked);
+    return browserProxy.whenCalled('setGoogleAssistantContextEnabled')
+        .then(function(arg) {
+          assertTrue(arg);
+        });
+    ;
+  });
+
+  test('tapOnAssistantSettings', function() {
+    var button = page.$$('#googleAssistantSettings');
+    assertFalse(!!button);
+    page.setPrefValue('settings.voice_interaction.enabled', true);
+    Polymer.dom.flush();
+    button = page.$$('#googleAssistantSettings');
+    assertTrue(!!button);
+
+    MockInteractions.tap(button);
+    Polymer.dom.flush();
+    return browserProxy.whenCalled('showGoogleAssistantSettings');
+  });
+
+});
