3812fb53ed76dc0abf216c9a47142d56ed610378
Nasko Oskov <nasko@chromium.org>
2017-08-03 20:44:23 +0000

    Enable WebNavigationApiTest.Failures on Windows and Mac.
    Bug: 477840, 746407
    Change-Id: Id10d8f38903bf64c67d0a8ca8dc0f09142907355
    Reviewed-on: https://chromium-review.googlesource.com/600949
    Commit-Queue: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491837}
    Former-commit-id: 6f408527d1887f3a3ed57a001c35fc61c91c6aef

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -486,14 +486,7 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, SimpleLoad) {
   ASSERT_TRUE(RunExtensionTest("webnavigation/simpleLoad")) << message_;
 }
 
-// Flaky on Windows and Mac. See http://crbug.com/477480 (Windows) and
-// https://crbug.com/746407 (Mac).
-#if defined(OS_WIN) || defined(OS_MACOSX)
-#define MAYBE_Failures DISABLED_Failures
-#else
-#define MAYBE_Failures Failures
-#endif
-IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, MAYBE_Failures) {
+IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, Failures) {
   ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunExtensionTest("webnavigation/failures")) << message_;
 }
