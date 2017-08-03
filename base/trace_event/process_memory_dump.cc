7486e6a3651f723d698fa7028cdd7fa10939a17e
Hajime Hoshi <hajimehoshi@chromium.org>
2017-08-03 12:04:24 +0000

    Remove MemoryAllocatorDumpGuid::UseSharedMemoryBasedGUIDs
    As we confirmed the new oenership edges works correctly, let's remove
    the logic to switch back to the old ownership edges
    
    Bug: 604726
    Change-Id: I16845d41a5aecd0d2e565157e88cf67eb9322d15
    Reviewed-on: https://chromium-review.googlesource.com/593667
    Reviewed-by: Primiano Tucci <primiano@chromium.org>
    Commit-Queue: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491705}
    Former-commit-id: bd605e7e22a8d62582509fb8aac443330c8126c3

--- base/trace_event/process_memory_dump.cc
+++ base/trace_event/process_memory_dump.cc
@@ -430,42 +430,31 @@ void ProcessMemoryDump::CreateSharedMemoryOwnershipEdgeInternal(
     const UnguessableToken& shared_memory_guid,
     int importance,
     bool is_weak) {
-  if (MemoryAllocatorDumpGuid::UseSharedMemoryBasedGUIDs()) {
-    DCHECK(!shared_memory_guid.is_empty());
-    // New model where the global dumps created by SharedMemoryTracker are used
-    // for the clients.
+  DCHECK(!shared_memory_guid.is_empty());
+  // New model where the global dumps created by SharedMemoryTracker are used
+  // for the clients.
 
-    // The guid of the local dump created by SharedMemoryTracker for the memory
-    // segment.
-    auto local_shm_guid =
-        SharedMemoryTracker::GetDumpIdForTracing(shared_memory_guid);
+  // The guid of the local dump created by SharedMemoryTracker for the memory
+  // segment.
+  auto local_shm_guid =
+      SharedMemoryTracker::GetDumpIdForTracing(shared_memory_guid);
 
-    // The dump guid of the global dump created by the tracker for the memory
-    // segment.
-    auto global_shm_guid =
-        SharedMemoryTracker::GetGlobalDumpIdForTracing(shared_memory_guid);
+  // The dump guid of the global dump created by the tracker for the memory
+  // segment.
+  auto global_shm_guid =
+      SharedMemoryTracker::GetGlobalDumpIdForTracing(shared_memory_guid);
 
-    // Create an edge between local dump of the client and the local dump of the
-    // SharedMemoryTracker. Do not need to create the dumps here since the
-    // tracker would create them. The importance is also required here for the
-    // case of single process mode.
-    AddOwnershipEdge(client_local_dump_guid, local_shm_guid, importance);
+  // Create an edge between local dump of the client and the local dump of the
+  // SharedMemoryTracker. Do not need to create the dumps here since the tracker
+  // would create them. The importance is also required here for the case of
+  // single process mode.
+  AddOwnershipEdge(client_local_dump_guid, local_shm_guid, importance);
 
-    // TODO(ssid): Handle the case of weak dumps here. This needs a new function
-    // GetOrCreaetGlobalDump() in PMD since we need to change the behavior of
-    // the created global dump.
-    // Create an edge that overrides the edge created by SharedMemoryTracker.
-    AddOwnershipEdge(local_shm_guid, global_shm_guid, importance);
-  } else {
-    // This is the old model where the clients create global dumps for
-    // themselves.
-    if (is_weak)
-      CreateWeakSharedGlobalAllocatorDump(client_global_dump_guid);
-    else
-      CreateSharedGlobalAllocatorDump(client_global_dump_guid);
-    AddOwnershipEdge(client_local_dump_guid, client_global_dump_guid,
-                     importance);
-  }
+  // TODO(ssid): Handle the case of weak dumps here. This needs a new function
+  // GetOrCreaetGlobalDump() in PMD since we need to change the behavior of the
+  // created global dump.
+  // Create an edge that overrides the edge created by SharedMemoryTracker.
+  AddOwnershipEdge(local_shm_guid, global_shm_guid, importance);
 }
 
 void ProcessMemoryDump::AddSuballocation(const MemoryAllocatorDumpGuid& source,
