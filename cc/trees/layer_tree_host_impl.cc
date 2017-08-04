12a0039a8f2f7f64f3a357a4dadee926ac04a0c0
sohan <sohan.jyoti@huawei.com>
2017-08-04 20:41:20 +0000

    cc: Temporarily avoid gpu raster drawing in HUD.
    Drawing HUD with gpu raster is causing some frame drops and
    stuttering. Fix that before re-eanbling.
    
    BUG=751732
    
    Change-Id: Ia62cfe8c1aa6e16bdd4737e9c8bb2de5b4b66fca
    Reviewed-on: https://chromium-review.googlesource.com/602349
    Commit-Queue: Sohan Jyoti Ghosh <sohan.jyoti@huawei.com>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Reviewed-by: Sohan Jyoti Ghosh <sohan.jyoti@huawei.com>
    Cr-Commit-Position: refs/heads/master@{#492117}
    Former-commit-id: 5eb08fbc487c137d4b6072adf0b4c74d87647b9f

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -1749,9 +1749,11 @@ bool LayerTreeHostImpl::DrawLayers(FrameData* frame) {
   // drawn.
   if (active_tree_->hud_layer()) {
     TRACE_EVENT0("cc", "DrawLayers.UpdateHudTexture");
+    // TODO(sohanjg): Temporarily do not pass context provider, so that SW
+    // raster path is chosen till we fix the FPS drop issue with gpu raster
+    // crbug.com/751732
     active_tree_->hud_layer()->UpdateHudTexture(
-        draw_mode, resource_provider_.get(),
-        layer_tree_frame_sink_->context_provider(), frame->render_passes);
+        draw_mode, resource_provider_.get(), nullptr, frame->render_passes);
   }
 
   CompositorFrameMetadata metadata = MakeCompositorFrameMetadata();
