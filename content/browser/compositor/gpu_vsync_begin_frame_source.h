1f1ee0ede19dd692b8f8ebacedf4f92501f415f7
Stanislav Chiknavaryan <stanisc@chromium.org>
2017-08-04 21:31:31 +0000

    Reland "Fix Omnibox.CharTypedToRepaintLatency regression with D3DVsync experiment"
    This reverts commit e5e8a960d878e87fcba19b4e886cf3887523dfdb [formerly d76957c122db1f7cf7d60f5663b6926a6dc34285].
    
    The previous fix had to be reverted due to occasionally hitting a DCHECK
    in ExternalBeginFrameSource::GetMissedBeginFrameArgs which turned out to
    be unrelated to my change and was addressed here:
    https://chromium-review.googlesource.com/c/569448/
    
    This change is pretty much the same as the original one except that some of
    the code has been moved to viz and some merge conflicts had to be resolved.
    
    Here is the original description from https://codereview.chromium.org/2897263003:
    
    The regression was caused by a subtle difference in how MISSED
    BeginFrameArgs is generated between DelayBasedBeginFrameSource and
    GpuVSyncBeginFrameSource (which is based on external BFS).
    
    External BFS would only generate a MISSED args if it already has a
    previous args (which is the case only when it already has at least
    one other observer). So in many real cases it would skip issuing a
    MISSED args.
    
    In contrast, DelayBasedBeginFrameSource uses its DelayBasedTimeSource
    to find the last missed frame time regardless of whether it had
    another observer before. 
    
    I've slightly refactored ExternalBeginFrameSource to make it 
    possible to override generation of a MISSED args in a derived class.
    Now GpuVSyncBeginFrameSource is able to project the last missed
    frame time based on a previous args even if it was issued some time
    ago.
    
    And because GpuVSyncBeginFrameSource is now more complex I included
    a unit test to cover all the special logic that it adds on top of
    ExternalBeginFrameSource.
    
    BUG=723935
    
    Change-Id: I2336407b6e51bd74aef6bf4a080eee1be50988c8
    Reviewed-on: https://chromium-review.googlesource.com/586358
    Commit-Queue: Stanislav Chiknavaryan <stanisc@chromium.org>
    Reviewed-by: Sunny Sachanandani <sunnyps@chromium.org>
    Reviewed-by: danakj <danakj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492126}
    Former-commit-id: 47e6660d4a2e273a3735582976088c5c34eb752d

--- content/browser/compositor/gpu_vsync_begin_frame_source.h
+++ content/browser/compositor/gpu_vsync_begin_frame_source.h
@@ -8,19 +8,21 @@
 #include "base/memory/weak_ptr.h"
 #include "base/time/time.h"
 #include "components/viz/common/frame_sinks/begin_frame_source.h"
+#include "content/common/content_export.h"
 
 namespace content {
 
 // This class is used to control VSync production on GPU side.
-class GpuVSyncControl {
+class CONTENT_EXPORT GpuVSyncControl {
  public:
   virtual void SetNeedsVSync(bool needs_vsync) = 0;
 };
 
 // This is a type of ExternalBeginFrameSource where VSync signals are
 // generated externally on GPU side.
-class GpuVSyncBeginFrameSource : public viz::ExternalBeginFrameSource,
-                                 viz::ExternalBeginFrameSourceClient {
+class CONTENT_EXPORT GpuVSyncBeginFrameSource
+    : public viz::ExternalBeginFrameSource,
+      public viz::ExternalBeginFrameSourceClient {
  public:
   explicit GpuVSyncBeginFrameSource(GpuVSyncControl* vsync_control);
   ~GpuVSyncBeginFrameSource() override;
@@ -30,7 +32,14 @@ class GpuVSyncBeginFrameSource : public viz::ExternalBeginFrameSource,
 
   void OnVSync(base::TimeTicks timestamp, base::TimeDelta interval);
 
+ protected:
+  // Virtual for testing.
+  virtual base::TimeTicks Now() const;
+
  private:
+  viz::BeginFrameArgs GetMissedBeginFrameArgs(
+      viz::BeginFrameObserver* obs) override;
+
   GpuVSyncControl* const vsync_control_;
   bool needs_begin_frames_;
   uint64_t next_sequence_number_;
