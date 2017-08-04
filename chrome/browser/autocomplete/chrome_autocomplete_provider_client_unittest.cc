aeac34d70332a3604421f5e8d0132c01e8f43afa
Justin Donnelly <jdonnelly@chromium.org>
2017-08-04 23:31:18 +0000

    Test disabling service worker startup when suggest is disabled.
    Actually disabling startup is done in http://crrev.com/c/598933 which is
    kept simple for merging to M61. This CL moves the test to a better
    location and tests the behavior.
    
    Bug: 751813
    Change-Id: Ic8b57af1586599fe73fe642eba759c3c1d678287
    Reviewed-on: https://chromium-review.googlesource.com/599011
    Reviewed-by: Mark Pearson <mpearson@chromium.org>
    Commit-Queue: Justin Donnelly <jdonnelly@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492173}
    Former-commit-id: fbc1f06772af27fab08b74064b914cbdc5fc7418

--- chrome/browser/autocomplete/chrome_autocomplete_provider_client_unittest.cc
+++ chrome/browser/autocomplete/chrome_autocomplete_provider_client_unittest.cc
@@ -5,7 +5,9 @@
 #include "chrome/browser/autocomplete/chrome_autocomplete_provider_client.h"
 
 #include "base/memory/ptr_util.h"
+#include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_profile.h"
+#include "components/prefs/pref_service.h"
 #include "content/public/test/mock_service_worker_context.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "content/public/test/test_storage_partition.h"
@@ -18,7 +20,9 @@ using ::testing::_;
 class ChromeAutocompleteProviderClientTest : public testing::Test {
  public:
   void SetUp() override {
-    client_ = base::MakeUnique<ChromeAutocompleteProviderClient>(&profile_);
+    profile_ = base::MakeUnique<TestingProfile>();
+    client_ =
+        base::MakeUnique<ChromeAutocompleteProviderClient>(profile_.get());
     storage_partition_.set_service_worker_context(&service_worker_context_);
     client_->set_storage_partition(&storage_partition_);
   }
@@ -28,16 +32,17 @@ class ChromeAutocompleteProviderClientTest : public testing::Test {
   // |client_| will be off the record.
   void GoOffTheRecord() {
     client_ = base::MakeUnique<ChromeAutocompleteProviderClient>(
-        profile_.GetOffTheRecordProfile());
+        profile_->GetOffTheRecordProfile());
   }
 
  protected:
   content::TestBrowserThreadBundle test_browser_thread_bundle_;
+
+  std::unique_ptr<TestingProfile> profile_;
   std::unique_ptr<ChromeAutocompleteProviderClient> client_;
   content::MockServiceWorkerContext service_worker_context_;
 
  private:
-  TestingProfile profile_;
   content::TestStoragePartition storage_partition_;
 };
 
@@ -55,6 +60,17 @@ TEST_F(ChromeAutocompleteProviderClientTest,
   GURL destination_url("https://google.com/search?q=puppies");
 
   GoOffTheRecord();
+  EXPECT_CALL(service_worker_context_,
+              StartServiceWorkerForNavigationHint(destination_url, _))
+      .Times(0);
+  client_->StartServiceWorker(destination_url);
+}
+
+TEST_F(ChromeAutocompleteProviderClientTest,
+       DontStartServiceWorkerIfSuggestDisabled) {
+  GURL destination_url("https://google.com/search?q=puppies");
+
+  profile_->GetPrefs()->SetBoolean(prefs::kSearchSuggestEnabled, false);
   EXPECT_CALL(service_worker_context_,
               StartServiceWorkerForNavigationHint(destination_url, _))
       .Times(0);
