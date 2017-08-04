a39b425b5f4c8941c52ecf93e3204390fa0a37c3
carloschilazo <carloschilazo@gmail.com>
2017-08-04 05:49:15 -0700

    Mac:Dont always enable findbar buttons when restoring focus with search string
    Avoids enabling findbar buttons when there is no search match.
    
    TEST=On mac: Search for something not in page, verify next/prev buttons
    disabled due to no results, focus away from findbar, do ctrl-f and verify
    buttons remain disabled.
    
    BUG=634289
    
    Review-Url: https://codereview.chromium.org/2928333002
    Cr-Commit-Position: refs/heads/master@{#492002}
    
    Former-commit-id: 91d01230acfaf11ce9ef59d1af6e9ce232cc9e01

--- chrome/browser/ui/cocoa/find_bar/find_bar_cocoa_controller.mm
+++ chrome/browser/ui/cocoa/find_bar/find_bar_cocoa_controller.mm
@@ -41,6 +41,7 @@ const float kFindBarCloseDuration = 0.15;
 const float kFindBarMoveDuration = 0.15;
 const float kRightEdgeOffset = 25;
 const int kMaxCharacters = 4000;
+const int kUndefinedResultCount = -1;
 
 @interface FindBarCocoaController (PrivateMethods) <NSAnimationDelegate>
 // Returns the appropriate frame for a hidden find bar.
@@ -75,6 +76,10 @@ const int kMaxCharacters = 4000;
 - (void)clearFindResultsForCurrentBrowser;
 
 - (BrowserWindowController*)browserWindowController;
+
+// Returns the number of matches from the last find results of the active
+// web contents. Returns kUndefinedResultCount if unable to determine the count.
+- (int)lastNumberOfMatchesForActiveWebContents;
 @end
 
 @implementation FindBarCocoaController
@@ -362,9 +367,8 @@ const int kMaxCharacters = 4000;
 
 - (void)setFocusAndSelection {
   [[findText_ window] makeFirstResponder:findText_];
+  BOOL buttonsEnabled = ([self lastNumberOfMatchesForActiveWebContents] != 0);
 
-  // Enable the buttons if the find text is non-empty.
-  BOOL buttonsEnabled = ([[findText_ stringValue] length] > 0) ? YES : NO;
   [previousButton_ setEnabled:buttonsEnabled];
   [nextButton_ setEnabled:buttonsEnabled];
 }
@@ -668,4 +672,13 @@ const int kMaxCharacters = 4000;
       browserWindowControllerForWindow:browser_->window()->GetNativeWindow()];
 }
 
+- (int)lastNumberOfMatchesForActiveWebContents {
+  if (!browser_)
+    return kUndefinedResultCount;
+
+  content::WebContents* contents =
+      findBarBridge_->GetFindBarController()->web_contents();
+  FindTabHelper* findTabHelper = FindTabHelper::FromWebContents(contents);
+  return findTabHelper->find_result().number_of_matches();
+}
 @end
