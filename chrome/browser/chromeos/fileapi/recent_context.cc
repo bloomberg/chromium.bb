a3a17b924b3af58c3a36fce4fad648bd873cc227
Shuhei Takahashi <nya@chromium.org>
2017-08-04 08:54:58 +0000

    recent: Introduce RecentModel.
    This change introduces core concepts of Recent file system:
    
    RecentModel:
    
      The most important class owning RecentSources.
    
      The only API it provides is GetRecentFiles() which queries
      RecentSources for recently modified files and build a list of
      recently modified files across file systems, and also caches the
      result for better file system operation performance.
    
      Files are represented as FileSystemURL.
    
    RecentSource:
    
      Interface class for a source of recent files.
    
      This interface will be implemented for several sources such as local
      storages and cloud storages so that they appear in Recent file
      system.
    
    RecentContext:
    
      Holds several "context" objects necessary for operations in
      RecentModel and RecentSource.
    
    Bug: 742722
    Test: unit_tests
    Change-Id: I2a2ff1fa28e8cf5c705ad1ee6bc75a39b49b2f0b
    Reviewed-on: https://chromium-review.googlesource.com/588950
    Reviewed-by: Tomasz Mikolajewski <mtomasz@chromium.org>
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491968}
    Former-commit-id: 975f86a4b46dc26540d66770e7bcf1dc58f5be5e

---
+++ chrome/browser/chromeos/fileapi/recent_context.cc
@@ -0,0 +1,25 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/fileapi/recent_context.h"
+
+#include <utility>
+
+namespace chromeos {
+
+RecentContext::RecentContext() : is_valid_(false) {}
+
+RecentContext::RecentContext(storage::FileSystemContext* file_system_context,
+                             const GURL& origin)
+    : is_valid_(true),
+      file_system_context_(file_system_context),
+      origin_(origin) {}
+
+RecentContext::RecentContext(const RecentContext& other) = default;
+
+RecentContext::~RecentContext() = default;
+
+RecentContext& RecentContext::operator=(const RecentContext& other) = default;
+
+}  // namespace chromeos
---
+++ chrome/browser/chromeos/fileapi/recent_model_unittest.cc
@@ -0,0 +1,90 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <algorithm>
+#include <string>
+#include <utility>
+
+#include "base/files/file_path.h"
+#include "base/memory/ptr_util.h"
+#include "chrome/browser/chromeos/fileapi/recent_context.h"
+#include "chrome/browser/chromeos/fileapi/recent_model.h"
+#include "chrome/browser/chromeos/fileapi/recent_model_factory.h"
+#include "chrome/browser/chromeos/fileapi/test/fake_recent_source.h"
+#include "chrome/test/base/testing_profile.h"
+#include "content/public/test/test_browser_thread_bundle.h"
+#include "storage/browser/fileapi/file_system_url.h"
+#include "storage/common/fileapi/file_system_types.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace chromeos {
+namespace {
+
+using RecentFileList = RecentModel::RecentFileList;
+
+class RecentModelTest : public testing::Test {
+ public:
+  RecentModelTest() = default;
+
+ protected:
+  RecentFileList BuildModelAndGetRecentFiles(
+      std::vector<std::unique_ptr<RecentSource>> sources) {
+    RecentModel* model = RecentModelFactory::SetForProfileAndUseForTest(
+        &profile_, RecentModel::CreateForTest(std::move(sources)));
+
+    RecentFileList files_out;
+
+    base::RunLoop run_loop;
+
+    model->GetRecentFiles(
+        RecentContext(),
+        base::BindOnce(
+            [](base::RunLoop* run_loop, RecentModel::RecentFileList* files_out,
+               const RecentModel::RecentFileList& files) {
+              *files_out = files;
+              run_loop->Quit();
+            },
+            &run_loop, &files_out));
+
+    run_loop.Run();
+
+    return files_out;
+  }
+
+  content::TestBrowserThreadBundle thread_bundle_;
+  TestingProfile profile_;
+};
+
+storage::FileSystemURL MakeFileSystemURL(const std::string& name) {
+  return storage::FileSystemURL::CreateForTest(
+      GURL(),  // origin
+      storage::kFileSystemTypeNativeLocal, base::FilePath(name));
+}
+
+TEST_F(RecentModelTest, GetRecentFiles) {
+  auto source1 = base::MakeUnique<FakeRecentSource>();
+  source1->AddFile(MakeFileSystemURL("aaa.jpg"));
+  source1->AddFile(MakeFileSystemURL("ccc.jpg"));
+
+  auto source2 = base::MakeUnique<FakeRecentSource>();
+  source2->AddFile(MakeFileSystemURL("bbb.jpg"));
+  source2->AddFile(MakeFileSystemURL("ddd.jpg"));
+
+  std::vector<std::unique_ptr<RecentSource>> sources;
+  sources.emplace_back(std::move(source1));
+  sources.emplace_back(std::move(source2));
+
+  RecentFileList files = BuildModelAndGetRecentFiles(std::move(sources));
+
+  std::sort(files.begin(), files.end(), storage::FileSystemURL::Comparator());
+
+  ASSERT_EQ(4u, files.size());
+  EXPECT_EQ("aaa.jpg", files[0].path().value());
+  EXPECT_EQ("bbb.jpg", files[1].path().value());
+  EXPECT_EQ("ccc.jpg", files[2].path().value());
+  EXPECT_EQ("ddd.jpg", files[3].path().value());
+}
+
+}  // namespace
+}  // namespace chromeos
