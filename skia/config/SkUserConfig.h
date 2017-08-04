a9453863a45efc9fb9486910301047cf1558b49b
Mike Klein <mtklein@chromium.org>
2017-08-04 02:01:29 +0000

    lift SK_JUMPER_LEGACY_LOWP
    ... and rebaseline
    
    Change-Id: I8023f1a57efb3536b38a912f9b6b9d8917fe3dc2
    Reviewed-on: https://chromium-review.googlesource.com/600734
    Commit-Queue: Mike Klein <mtklein@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#491916}
    Former-commit-id: cf1563247887651c4db9509be7cf064c8449423e

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -216,10 +216,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_DELTA_AA
 #endif
 
-#ifndef SK_JUMPER_LEGACY_LOWP
-#define SK_JUMPER_LEGACY_LOWP
-#endif
-
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
