58c77e941d3d005d00b48499145241d505ef3617
David Benjamin <davidben@chromium.org>
2017-08-04 19:17:00 +0000

    Partially rearrange x509_util* organization.
    x509_util_openssl.cc was historically both implementations of things in
    x509_util_openssl.h and things in x509_util.h that depended on OpenSSL.
    The latter distinction is no longer useful. This moves some things over
    to x509_util.cc.
    
    CreateSelfSignedCert is still in the old file because we'd otherwise
    need to duplicate or export DerEncodeCert. Later work to decouple it
    from X509* will allow us to complete the move, and then
    x509_util_openssl.h will be one of the files removed when use_byte_certs
    is everywhere.
    
    Bug: 706445
    Change-Id: I1ea1e3d80fdde397932aa9065901aabe23ad977a
    Reviewed-on: https://chromium-review.googlesource.com/601093
    Reviewed-by: Matt Mueller <mattm@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492096}
    Former-commit-id: f349dc4bf8d1c4f56a54722ceb116103f28350c3

--- net/cert/x509_util.cc
+++ net/cert/x509_util.cc
@@ -6,7 +6,9 @@
 
 #include <memory>
 
+#include "base/lazy_instance.h"
 #include "base/time/time.h"
+#include "build/build_config.h"
 #include "crypto/ec_private_key.h"
 #include "crypto/rsa_private_key.h"
 #include "net/base/hash_value.h"
@@ -18,8 +20,13 @@
 #include "net/cert/x509_certificate.h"
 #include "net/der/input.h"
 #include "net/der/parse_values.h"
+#include "third_party/boringssl/src/include/openssl/digest.h"
+#include "third_party/boringssl/src/include/openssl/pool.h"
+#include "third_party/boringssl/src/include/openssl/stack.h"
 
 namespace net {
+
+namespace x509_util {
 
 namespace {
 
@@ -50,9 +57,80 @@ bool DecodeTime(const der::GeneralizedTime& generalized_time,
   return base::Time::FromUTCExploded(exploded, time);
 }
 
+class BufferPoolSingleton {
+ public:
+  BufferPoolSingleton() : pool_(CRYPTO_BUFFER_POOL_new()) {}
+  CRYPTO_BUFFER_POOL* pool() { return pool_; }
+
+ private:
+  // The singleton is leaky, so there is no need to use a smart pointer.
+  CRYPTO_BUFFER_POOL* pool_;
+};
+
+base::LazyInstance<BufferPoolSingleton>::Leaky g_buffer_pool_singleton =
+    LAZY_INSTANCE_INITIALIZER;
+
 }  // namespace
 
-namespace x509_util {
+bool GetTLSServerEndPointChannelBinding(const X509Certificate& certificate,
+                                        std::string* token) {
+  static const char kChannelBindingPrefix[] = "tls-server-end-point:";
+
+  std::string der_encoded_certificate;
+  if (!X509Certificate::GetDEREncoded(certificate.os_cert_handle(),
+                                      &der_encoded_certificate))
+    return false;
+
+  der::Input tbs_certificate_tlv;
+  der::Input signature_algorithm_tlv;
+  der::BitString signature_value;
+  if (!ParseCertificate(der::Input(&der_encoded_certificate),
+                        &tbs_certificate_tlv, &signature_algorithm_tlv,
+                        &signature_value, nullptr))
+    return false;
+
+  std::unique_ptr<SignatureAlgorithm> signature_algorithm =
+      SignatureAlgorithm::Create(signature_algorithm_tlv, nullptr);
+  if (!signature_algorithm)
+    return false;
+
+  const EVP_MD* digest_evp_md = nullptr;
+  switch (signature_algorithm->digest()) {
+    case net::DigestAlgorithm::Md2:
+    case net::DigestAlgorithm::Md4:
+      // Shouldn't be reachable.
+      digest_evp_md = nullptr;
+      break;
+
+    // Per RFC 5929 section 4.1, MD5 and SHA1 map to SHA256.
+    case net::DigestAlgorithm::Md5:
+    case net::DigestAlgorithm::Sha1:
+    case net::DigestAlgorithm::Sha256:
+      digest_evp_md = EVP_sha256();
+      break;
+
+    case net::DigestAlgorithm::Sha384:
+      digest_evp_md = EVP_sha384();
+      break;
+
+    case net::DigestAlgorithm::Sha512:
+      digest_evp_md = EVP_sha512();
+      break;
+  }
+  if (!digest_evp_md)
+    return false;
+
+  uint8_t digest[EVP_MAX_MD_SIZE];
+  unsigned int out_size;
+  if (!EVP_Digest(der_encoded_certificate.data(),
+                  der_encoded_certificate.size(), digest, &out_size,
+                  digest_evp_md, nullptr))
+    return false;
+
+  token->assign(kChannelBindingPrefix);
+  token->append(digest, digest + out_size);
+  return true;
+}
 
 // RSA keys created by CreateKeyAndSelfSignedCert will be of this length.
 static const uint16_t kRSAKeyLength = 1024;
@@ -137,6 +215,50 @@ bool ParseCertificateSandboxed(const base::StringPiece& certificate,
   }
 
   return true;
+}
+
+CRYPTO_BUFFER_POOL* GetBufferPool() {
+  return g_buffer_pool_singleton.Get().pool();
+}
+
+bssl::UniquePtr<CRYPTO_BUFFER> CreateCryptoBuffer(const uint8_t* data,
+                                                  size_t length) {
+  return bssl::UniquePtr<CRYPTO_BUFFER>(
+      CRYPTO_BUFFER_new(data, length, GetBufferPool()));
+}
+
+bssl::UniquePtr<CRYPTO_BUFFER> CreateCryptoBuffer(
+    const base::StringPiece& data) {
+  return bssl::UniquePtr<CRYPTO_BUFFER>(
+      CRYPTO_BUFFER_new(reinterpret_cast<const uint8_t*>(data.data()),
+                        data.size(), GetBufferPool()));
+}
+
+scoped_refptr<X509Certificate> CreateX509CertificateFromBuffers(
+    STACK_OF(CRYPTO_BUFFER) * buffers) {
+  if (sk_CRYPTO_BUFFER_num(buffers) == 0) {
+    NOTREACHED();
+    return nullptr;
+  }
+
+#if BUILDFLAG(USE_BYTE_CERTS)
+  std::vector<CRYPTO_BUFFER*> intermediate_chain;
+  for (size_t i = 1; i < sk_CRYPTO_BUFFER_num(buffers); ++i)
+    intermediate_chain.push_back(sk_CRYPTO_BUFFER_value(buffers, i));
+  return X509Certificate::CreateFromHandle(sk_CRYPTO_BUFFER_value(buffers, 0),
+                                           intermediate_chain);
+#else
+  // Convert the certificate chains to a platform certificate handle.
+  std::vector<base::StringPiece> der_chain;
+  der_chain.reserve(sk_CRYPTO_BUFFER_num(buffers));
+  for (size_t i = 0; i < sk_CRYPTO_BUFFER_num(buffers); ++i) {
+    const CRYPTO_BUFFER* cert = sk_CRYPTO_BUFFER_value(buffers, i);
+    der_chain.push_back(base::StringPiece(
+        reinterpret_cast<const char*>(CRYPTO_BUFFER_data(cert)),
+        CRYPTO_BUFFER_len(cert)));
+  }
+  return X509Certificate::CreateFromDERCertChain(der_chain);
+#endif
 }
 
 }  // namespace x509_util
