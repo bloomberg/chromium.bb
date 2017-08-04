d8d23187211de96075a2a05942ee6f93ebb64e0d
Vasilii Sukhanov <vasilii@chromium.org>
2017-08-04 14:47:48 +0000

    Revert "Enable WebNavigationApiTest.Failures on Windows and Mac."
    This reverts commit a130d0389c90be061f455f9921d35cca80ba4a50 [formerly 6f408527d1887f3a3ed57a001c35fc61c91c6aef].
    
    Reason for revert: timeout on Win. It's not a false alarm. I can see from the flakiness dashboard that the test fails on all the platforms.
    
    Original change's description:
    > Enable WebNavigationApiTest.Failures on Windows and Mac.
    > 
    > Bug: 477840, 746407
    > Change-Id: Id10d8f38903bf64c67d0a8ca8dc0f09142907355
    > Reviewed-on: https://chromium-review.googlesource.com/600949
    > Commit-Queue: Nasko Oskov <nasko@chromium.org>
    > Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#491837}
    
    TBR=nasko@chromium.org,alexmos@chromium.org
    
    Change-Id: I9117c27ef3e419743646b31ecd9bcadb89754e2f
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 477840, 746407
    Reviewed-on: https://chromium-review.googlesource.com/602211
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Commit-Queue: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492026}
    Former-commit-id: a0d7943088eddeb4e73b0e25e721ef6baa9b99dd

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -487,7 +487,14 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, SimpleLoad) {
   ASSERT_TRUE(RunExtensionTest("webnavigation/simpleLoad")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, Failures) {
+// Flaky on Windows and Mac. See http://crbug.com/477480 (Windows) and
+// https://crbug.com/746407 (Mac).
+#if defined(OS_WIN) || defined(OS_MACOSX)
+#define MAYBE_Failures DISABLED_Failures
+#else
+#define MAYBE_Failures Failures
+#endif
+IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, MAYBE_Failures) {
   ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunExtensionTest("webnavigation/failures")) << message_;
 }
