79ae1c4ecfd9fa17b0c71a5675b35d00543cc820
Kevin Marshall <kmarshall@chromium.org>
2017-08-05 01:22:40 +0000

    Add Fuchsia platform font and text stubs for Blink.
    Add Fuchsia platform font and text stubs for Blink.
    These stubs are just placeholders used to get a headless build up and
    running. The bodies will be filled once the porting effort is ready
    for UI code.
    
    Bug: 750946
    Change-Id: I73b1415f6e66b7cc8e291c2cb4ff486a92c7d4a9
    Reviewed-on: https://chromium-review.googlesource.com/601127
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492207}
    Former-commit-id: b07077bc0f186f3f7a4f1a5bffc210d076ee956c

---
+++ third_party/WebKit/Source/platform/fonts/fuchsia/FontCacheFuchsia.cpp
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2017 Google Inc. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1.  Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ * 2.  Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY APPLE INC. AND ITS CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL APPLE INC. OR ITS CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "platform/fonts/FontCache.h"
+
+#include "platform/fonts/FontPlatformData.h"
+
+namespace blink {
+
+// static
+const AtomicString& FontCache::SystemFontFamily() {
+  // TODO(fuchsia): Implement this when UI support is ready. crbug.com/750946
+  NOTIMPLEMENTED();
+
+  DEFINE_THREAD_SAFE_STATIC_LOCAL(AtomicString, font_family, ());
+  return font_family;
+}
+
+void FontCache::GetFontForCharacter(
+    UChar32 c,
+    const char* preferred_locale,
+    FontCache::PlatformFallbackFont* fallback_font) {
+  // TODO(fuchsia): Implement this when UI support is ready. crbug.com/750946
+  NOTIMPLEMENTED();
+}
+
+PassRefPtr<SimpleFontData> FontCache::FallbackFontForCharacter(
+    const FontDescription&,
+    UChar32,
+    const SimpleFontData* font_data_to_substitute,
+    FontFallbackPriority priority) {
+  // TODO(fuchsia): Implement this when UI support is ready. crbug.com/750946
+  NOTIMPLEMENTED();
+  return nullptr;
+}
+
+}  // namespace blink
