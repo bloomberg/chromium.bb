1810b2c9e9039c550a1303077d356b0341c69989
Catherine Chung <catherinechung@google.com>
2017-08-05 18:12:10 +0000

    Adds promo bubble to teach users about bookmarks
    - BookmarkPromoBubbleView inherits from FeaturePromoBubbleView
    
    - The FeaturePromoBubbleView class can be found in this CL:
      https://crrev.com/b0aa59a6f15995da6652de4639a5dc6374b413a4 [formerly 4087aace354687df0b199dda4443975d9aa64524]
    
    Bug: 734132
    Change-Id: Idbff0f2cb5386b225d0f25ad03d25b473e349c2a
    Reviewed-on: https://chromium-review.googlesource.com/598192
    Commit-Queue: Catherine Chung <catherinechung@google.com>
    Reviewed-by: Robert Liao <robliao@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492244}
    Former-commit-id: 835f34d3bbc24ade1cb71289b73771274399ba42

--- chrome/browser/ui/views/location_bar/star_view.h
+++ chrome/browser/ui/views/location_bar/star_view.h
@@ -6,10 +6,12 @@
 #define CHROME_BROWSER_UI_VIEWS_LOCATION_BAR_STAR_VIEW_H_
 
 #include "base/macros.h"
+#include "base/scoped_observer.h"
 #include "chrome/browser/ui/views/location_bar/bubble_icon_view.h"
 
 class Browser;
 class CommandUpdater;
+class WidgetObserver;
 
 // The star icon to show a bookmark bubble.
 class StarView : public BubbleIconView {
@@ -20,15 +22,24 @@ class StarView : public BubbleIconView {
   // Toggles the star on or off.
   void SetToggled(bool on);
 
+  // Shows the BookmarkPromoBubbleView when the BookmarkTracker calls for it.
+  void ShowPromo();
+
  protected:
   // BubbleIconView:
   void OnExecuting(BubbleIconView::ExecuteSource execute_source) override;
+  void OnWidgetDestroying(views::Widget* widget) override;
   void ExecuteCommand(ExecuteSource source) override;
   views::BubbleDialogDelegateView* GetBubble() const override;
+  SkColor GetInkDropBaseColor() const override;
   const gfx::VectorIcon& GetVectorIcon() const override;
 
  private:
   Browser* browser_;
+
+  // Observes the BookmarkPromoBubbleView's widget. Used to tell whether the
+  // promo is open and gets called back when it closes.
+  ScopedObserver<views::Widget, WidgetObserver> bookmark_promo_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(StarView);
 };
