cb78349c9714513a79b6ae16f829f2e447eb4aaf
Kevin Marshall <kmarshall@chromium.org>
2017-08-05 00:49:00 +0000

    Add Fuchsia stub implementation for RenderView.
    Add Fuchsia stub implementation for RenderView.
    
    Bug: 750946
    Change-Id: Ie097e996e16d15a7603469f7a5a814afd4ca027b
    Reviewed-on: https://chromium-review.googlesource.com/601129
    Reviewed-by: Nico Weber <thakis@chromium.org>
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492200}
    Former-commit-id: d2dd1ab90fc5c08e2cc4cf96ed124e0ab07e76d8

---
+++ content/renderer/render_view_fuchsia.cc
@@ -0,0 +1,14 @@
+// Copyright (c) 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/renderer/render_view_impl.h"
+
+namespace content {
+
+void RenderViewImpl::UpdateFontRenderingFromRendererPrefs() {
+  // TODO(fuchsia): Implement this. See crbug.com/750946
+  NOTIMPLEMENTED();
+}
+
+}  // namespace content
