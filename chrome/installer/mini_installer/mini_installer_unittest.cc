9ed9a0d783684a181b476ddc36b0dd12d21b6b9e
Greg Thompson <grt@chromium.org>
2017-08-07 15:17:08 +0000

    Unconditionally quote the path to setup.exe in command lines.
    With this change, the mini_installer always follows best practices for
    use of CreateProcess:
    - lpApplicationName is never null.
    - Quotation marks are always used around the executable path in
      lpCommandLine.
    
    With these changes, CreateProcess does not need to do any probing to
    find the .exe, and CommandLineToArgvW always properly finds argv[0].
    Previously, quotes were left off of some setup.exe invocations, leading
    to odd command line parsing. It didn't seem to be problematic aside from
    odd entries in the installer's log.
    
    BUG=none
    
    Change-Id: I2e382f31c7d2e1d358f287893b5b188cc00bf99b
    Reviewed-on: https://chromium-review.googlesource.com/601789
    Commit-Queue: Greg Thompson <grt@chromium.org>
    Reviewed-by: Sigurður Ásgeirsson <siggi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492325}
    Former-commit-id: 818432fb1a24697db020f971b38ac94eed025d61

--- chrome/installer/mini_installer/mini_installer_unittest.cc
+++ chrome/installer/mini_installer/mini_installer_unittest.cc
@@ -4,10 +4,27 @@
 
 #include "chrome/installer/mini_installer/mini_installer.h"
 
+#include "base/test/test_reg_util_win.h"
+#include "base/win/registry.h"
+#include "chrome/install_static/install_details.h"
 #include "chrome/installer/mini_installer/configuration.h"
+#include "chrome/installer/mini_installer/mini_installer.h"
+#include "chrome/installer/util/util_constants.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace mini_installer {
+
+namespace {
+
+#define PREVIOUS_VERSION L"62.0.1234.0"
+constexpr wchar_t kPreviousVersion[] = PREVIOUS_VERSION;
+
+class FakeConfiguration : public Configuration {
+ public:
+  FakeConfiguration() { previous_version_ = kPreviousVersion; }
+};
+
+}  // namespace
 
 TEST(MiniInstallerTest, AppendCommandLineFlags) {
   static constexpr struct {
@@ -34,6 +51,68 @@ TEST(MiniInstallerTest, AppendCommandLineFlags) {
     AppendCommandLineFlags(data.command_line, &buffer);
     EXPECT_STREQ(data.args, buffer.get()) << data.command_line;
   }
+}
+
+// A test harness for GetPreviousSetupExePath.
+class GetPreviousSetupExePathTest : public ::testing::Test {
+ protected:
+  GetPreviousSetupExePathTest() = default;
+  ~GetPreviousSetupExePathTest() override = default;
+
+  void SetUp() override {
+    ASSERT_NO_FATAL_FAILURE(
+        registry_override_manager_.OverrideRegistry(HKEY_CURRENT_USER));
+  }
+
+  const Configuration& configuration() const { return configuration_; }
+
+  // Writes |path| to the registry in Chrome's ClientState...UninstallString
+  // value.
+  void SetPreviousSetup(const wchar_t* path) {
+    base::win::RegKey key;
+    const install_static::InstallDetails& details =
+        install_static::InstallDetails::Get();
+    ASSERT_EQ(
+        key.Create(HKEY_CURRENT_USER, details.GetClientStateKeyPath().c_str(),
+                   KEY_SET_VALUE | KEY_WOW64_32KEY),
+        ERROR_SUCCESS);
+    ASSERT_EQ(key.WriteValue(installer::kUninstallStringField, path),
+              ERROR_SUCCESS);
+  }
+
+ private:
+  registry_util::RegistryOverrideManager registry_override_manager_;
+  FakeConfiguration configuration_;
+  DISALLOW_COPY_AND_ASSIGN(GetPreviousSetupExePathTest);
+};
+
+// Tests that the path is returned.
+TEST_F(GetPreviousSetupExePathTest, SimpleTest) {
+  static constexpr wchar_t kSetupExePath[] =
+      L"C:\\SomePath\\To\\" PREVIOUS_VERSION L"\\setup.exe";
+  ASSERT_NO_FATAL_FAILURE(SetPreviousSetup(kSetupExePath));
+
+  StackString<MAX_PATH> path;
+  ProcessExitResult result =
+      GetPreviousSetupExePath(configuration(), path.get(), path.capacity());
+  ASSERT_TRUE(result.IsSuccess());
+  EXPECT_STREQ(path.get(), kSetupExePath);
+}
+
+// Tests that quotes are removed, if present.
+TEST_F(GetPreviousSetupExePathTest, QuoteStripping) {
+  static constexpr wchar_t kSetupExePath[] =
+      L"C:\\SomePath\\To\\" PREVIOUS_VERSION L"\\setup.exe";
+  base::string16 quoted_path(L"\"");
+  quoted_path += kSetupExePath;
+  quoted_path += L"\"";
+  ASSERT_NO_FATAL_FAILURE(SetPreviousSetup(quoted_path.c_str()));
+
+  StackString<MAX_PATH> path;
+  ProcessExitResult result =
+      GetPreviousSetupExePath(configuration(), path.get(), path.capacity());
+  ASSERT_TRUE(result.IsSuccess());
+  EXPECT_STREQ(path.get(), kSetupExePath);
 }
 
 }  // namespace mini_installer
