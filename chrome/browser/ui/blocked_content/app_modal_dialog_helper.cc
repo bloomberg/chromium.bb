dd94b5d177d8e59e75f41c6459dd4974bac3dcba
Avi Drissman <avi@chromium.org>
2017-08-07 21:10:30 +0000

    Expand the power of the popunder preventer.
    If *any* WebContents in the opener chain of a popup
    attempts to bring itself forward in front of the
    popup, block that attempt to create a popunder.
    
    BUG=752824
    
    Change-Id: I4178f3872eb7af2f10d993bc8b54485b340c8355
    Reviewed-on: https://chromium-review.googlesource.com/603149
    Commit-Queue: Avi Drissman <avi@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492407}
    Former-commit-id: 31c61a8abb57358c5ce00f67e183f351cad04efa

--- chrome/browser/ui/blocked_content/app_modal_dialog_helper.cc
+++ chrome/browser/ui/blocked_content/app_modal_dialog_helper.cc
@@ -18,6 +18,20 @@
 
 AppModalDialogHelper::AppModalDialogHelper(content::WebContents* dialog_host)
     : popup_(nullptr) {
+  // If a popup is the active window, and the WebContents that is going to be
+  // activated is in the opener chain of that popup, then we suspect that
+  // WebContents to be trying to create a popunder. Store the popup window so
+  // that it can be re-activated once the dialog (or whatever is causing the
+  // activation) is closed.
+  Browser* active_browser = BrowserList::GetInstance()->GetLastActive();
+  if (!active_browser || !active_browser->is_type_popup())
+    return;
+
+  content::WebContents* active_popup =
+      active_browser->tab_strip_model()->GetActiveWebContents();
+  if (!active_popup)
+    return;
+
   content::WebContents* actual_host = dialog_host;
 #if BUILDFLAG(ENABLE_EXTENSIONS)
   // If the dialog was triggered via an PDF, get the actual web contents that
@@ -28,24 +42,20 @@ AppModalDialogHelper::AppModalDialogHelper(content::WebContents* dialog_host)
     actual_host = guest->embedder_web_contents();
 #endif
 
-  // If the WebContents that triggered this dialog is not currently focused, we
-  // want to store a potential popup here to restore it after the dialog was
-  // closed.
-  Browser* active_browser = BrowserList::GetInstance()->GetLastActive();
-  if (active_browser) {
-    content::WebContents* active_web_contents =
-        active_browser->tab_strip_model()->GetActiveWebContents();
-    if (active_browser->is_type_popup() && active_web_contents) {
-      content::WebContents* original_opener =
-          content::WebContents::FromRenderFrameHost(
-              active_web_contents->GetOriginalOpener());
-      if (original_opener == actual_host) {
-        // It's indeed a popup from the dialog opening WebContents. Store it, so
-        // we can focus it later.
-        popup_ = active_web_contents;
-        Observe(popup_);
-      }
+  content::WebContents* original_opener =
+      content::WebContents::FromRenderFrameHost(
+          active_popup->GetOriginalOpener());
+  while (original_opener) {
+    if (original_opener == actual_host) {
+      // It's indeed a popup from the dialog opening WebContents. Store it, so
+      // we can focus it later.
+      popup_ = active_popup;
+      Observe(popup_);
+      return;
     }
+
+    original_opener = content::WebContents::FromRenderFrameHost(
+        original_opener->GetOriginalOpener());
   }
 }
 
