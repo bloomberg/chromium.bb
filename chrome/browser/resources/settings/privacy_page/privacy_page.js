482f0303e1d963702116735469d81815c950a93e
Christian Dullweber <dullweber@chromium.org>
2017-08-07 12:22:33 +0000

    Create CBD dialog fork
    Create a copy of the Clear Browsing Data dialog in order to develop
    the new UI behind a flag.
    
    Bug: 725401
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I2180d15bb3522b4e03c736a34a0c899a10e1d848
    Reviewed-on: https://chromium-review.googlesource.com/597852
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Commit-Queue: Christian Dullweber <dullweber@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492303}
    Former-commit-id: 7c07598be0299431d37794a3d34b9e2cd0dc5866

--- chrome/browser/resources/settings/privacy_page/privacy_page.js
+++ chrome/browser/resources/settings/privacy_page/privacy_page.js
@@ -75,6 +75,14 @@ Polymer({
     showClearBrowsingDataDialog_: Boolean,
 
     /** @private */
+    tabsInCbd_: {
+      type: Boolean,
+      value: function() {
+        return loadTimeData.getBoolean('tabsInCbd');
+      }
+    },
+
+    /** @private */
     showDoNotTrackDialog_: {
       type: Boolean,
       value: false,
