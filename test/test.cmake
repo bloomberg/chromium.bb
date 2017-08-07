92d50233161606aefce24780745acb2721bff42e
Tom Finegan <tomfinegan@google.com>
2017-08-07 16:53:32 -0700

    Fix CONFIG_AOM_QM builds with CMake.
    Exclude test/quantize_func_test.cc from the build when
    CONFIG_AOM_QM or CONFIG_NEW_QUANT are enabled to mimic the
    configure build.
    
    BUG=aomedia:683
    
    Change-Id: Ib1358d466aedad4a784b4693f74babacf86cda7a

--- test/test.cmake
+++ test/test.cmake
@@ -215,10 +215,15 @@ if (CONFIG_AV1_ENCODER)
         "${AOM_ROOT}/test/fdct8x8_test.cc"
         "${AOM_ROOT}/test/hadamard_test.cc"
         "${AOM_ROOT}/test/minmax_test.cc"
-        "${AOM_ROOT}/test/quantize_func_test.cc"
         "${AOM_ROOT}/test/subtract_test.cc"
         "${AOM_ROOT}/test/sum_squares_test.cc"
         "${AOM_ROOT}/test/variance_test.cc")
+
+    if (NOT CONFIG_AOM_QM AND NOT CONFIG_NEW_QUANT)
+      set(AOM_UNIT_TEST_ENCODER_SOURCES
+          ${AOM_UNIT_TEST_ENCODER_SOURCES}
+          "${AOM_ROOT}/test/quantize_func_test.cc")
+    endif ()
 
     if (CONFIG_CONVOLVE_ROUND)
       set(AOM_UNIT_TEST_ENCODER_SOURCES
