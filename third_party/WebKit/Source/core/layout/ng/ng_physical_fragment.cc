b15a0d42491f43e5861f6c506a34c3ba3c414d01
Morten Stenshorne <mstensho@opera.com>
2017-08-08 09:11:26 +0000

    [LayoutNG] Physical fragment tree dump support.
    Call NGPhysicalFragment::ShowFragmentTree() to dump the fragment tree.
    The solution is inspired by NGLayoutInputNode::ShowNodeTree().
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: Ie256d74bb80f2be5f372ec57791b693e4f5bdeaf
    Reviewed-on: https://chromium-review.googlesource.com/602227
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Commit-Queue: Morten Stenshorne <mstensho@opera.com>
    Cr-Commit-Position: refs/heads/master@{#492575}
    Former-commit-id: b4cb66d270ea96052ac6b23f500a5b39f252e17f

--- third_party/WebKit/Source/core/layout/ng/ng_physical_fragment.cc
+++ third_party/WebKit/Source/core/layout/ng/ng_physical_fragment.cc
@@ -12,6 +12,74 @@
 #include "core/layout/ng/ng_unpositioned_float.h"
 
 namespace blink {
+namespace {
+
+#ifndef NDEBUG
+void AppendFragmentOffsetAndSize(const NGPhysicalFragment* fragment,
+                                 StringBuilder* string_builder) {
+  string_builder->Append("offset:");
+  if (fragment->IsPlaced())
+    string_builder->Append(fragment->Offset().ToString());
+  else
+    string_builder->Append("unplaced");
+  string_builder->Append(" size:");
+  string_builder->Append(fragment->Size().ToString());
+}
+
+void AppendFragmentToString(const NGPhysicalFragment* fragment,
+                            StringBuilder* string_builder,
+                            unsigned indent = 2) {
+  for (unsigned i = 0; i < indent; i++)
+    string_builder->Append(" ");
+
+  if (fragment->IsBox()) {
+    string_builder->Append("PhysicalBoxFragment ");
+    AppendFragmentOffsetAndSize(fragment, string_builder);
+
+    const auto* box = ToNGPhysicalBoxFragment(fragment);
+    string_builder->Append(" overflow:");
+    string_builder->Append(box->OverflowSize().ToString());
+    string_builder->Append("\n");
+
+    const auto& children = box->Children();
+    for (unsigned i = 0; i < children.size(); i++)
+      AppendFragmentToString(children[i].Get(), string_builder, indent + 2);
+    return;
+  }
+
+  if (fragment->IsLineBox()) {
+    string_builder->Append("NGPhysicalLineBoxFragment ");
+    AppendFragmentOffsetAndSize(fragment, string_builder);
+
+    const auto* line_box = ToNGPhysicalLineBoxFragment(fragment);
+    string_builder->Append("\n");
+
+    const auto& children = line_box->Children();
+    for (unsigned i = 0; i < children.size(); i++)
+      AppendFragmentToString(children[i].Get(), string_builder, indent + 2);
+    return;
+  }
+
+  if (fragment->IsText()) {
+    string_builder->Append("PhysicalTextFragment ");
+    AppendFragmentOffsetAndSize(fragment, string_builder);
+
+    const auto* text = ToNGPhysicalTextFragment(fragment);
+    string_builder->Append(" start: ");
+    string_builder->Append(String::Format("%u", text->StartOffset()));
+    string_builder->Append(" end: ");
+    string_builder->Append(String::Format("%u", text->EndOffset()));
+    string_builder->Append("\n");
+    return;
+  }
+
+  string_builder->Append("Unknown fragment type ");
+  AppendFragmentOffsetAndSize(fragment, string_builder);
+  string_builder->Append("\n");
+}
+#endif  // !NDEBUG
+
+}  // namespace
 
 NGPhysicalFragment::NGPhysicalFragment(LayoutObject* layout_object,
                                        const ComputedStyle& style,
@@ -87,5 +155,14 @@ String NGPhysicalFragment::ToString() const {
                         Type(), Size().ToString().Ascii().data(),
                         Offset().ToString().Ascii().data(), IsPlaced());
 }
+
+#ifndef NDEBUG
+void NGPhysicalFragment::ShowFragmentTree() const {
+  StringBuilder string_builder;
+  string_builder.Append(".:: LayoutNG Physical Fragment Tree ::.\n");
+  AppendFragmentToString(this, &string_builder);
+  fprintf(stderr, "%s\n", string_builder.ToString().Utf8().data());
+}
+#endif  // !NDEBUG
 
 }  // namespace blink
