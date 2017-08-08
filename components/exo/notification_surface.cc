aa6af8735743c7efcfe81ceab01c37c8bfe7bf5b
yoshiki iguchi <yoshiki@chromium.org>
2017-08-08 19:09:08 +0000

    Fix the focus traversal by tab-key on ARC notification
    The tab-key didn't move the focus on ARC notification. This was a regression of crrev.com/c/582932. This patch fixes it by moving the window delegate to the top level window of ARC notification.
    
    Bug: 739293
    Change-Id: Ica993313d70d1ba05d20494bcbe81430315e9485
    Reviewed-on: https://chromium-review.googlesource.com/604815
    Reviewed-by: David Reveman <reveman@chromium.org>
    Reviewed-by: Peng Huang <penghuang@chromium.org>
    Commit-Queue: Yoshiki Iguchi <yoshiki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492719}
    Former-commit-id: 20b6e16ec5c80b4e34aa8fddd665da99f4fc823f

--- components/exo/notification_surface.cc
+++ components/exo/notification_surface.cc
@@ -7,73 +7,14 @@
 #include "components/exo/notification_surface_manager.h"
 #include "components/exo/surface.h"
 #include "ui/aura/window.h"
-#include "ui/aura/window_delegate.h"
-#include "ui/base/cursor/cursor.h"
-#include "ui/base/hit_test.h"
 #include "ui/gfx/geometry/rect.h"
-#include "ui/views/widget/widget.h"
 
 namespace exo {
-
-namespace {
-
-class CustomWindowDelegate : public aura::WindowDelegate {
- public:
-  explicit CustomWindowDelegate(NotificationSurface* notification_surface)
-      : notification_surface_(notification_surface) {}
-  ~CustomWindowDelegate() override {}
-
-  // Overridden from aura::WindowDelegate:
-  gfx::Size GetMinimumSize() const override { return gfx::Size(); }
-  gfx::Size GetMaximumSize() const override { return gfx::Size(); }
-  void OnBoundsChanged(const gfx::Rect& old_bounds,
-                       const gfx::Rect& new_bounds) override {}
-  gfx::NativeCursor GetCursor(const gfx::Point& point) override {
-    return notification_surface_->GetCursor(point);
-  }
-  int GetNonClientComponent(const gfx::Point& point) const override {
-    return HTNOWHERE;
-  }
-  bool ShouldDescendIntoChildForEventHandling(
-      aura::Window* child,
-      const gfx::Point& location) override {
-    return true;
-  }
-  bool CanFocus() override { return true; }
-  void OnCaptureLost() override {}
-  void OnPaint(const ui::PaintContext& context) override {}
-  void OnDeviceScaleFactorChanged(float device_scale_factor) override {}
-  void OnWindowDestroying(aura::Window* window) override {}
-  void OnWindowDestroyed(aura::Window* window) override { delete this; }
-  void OnWindowTargetVisibilityChanged(bool visible) override {}
-  bool HasHitTestMask() const override {
-    return notification_surface_->HasHitTestMask();
-  }
-  void GetHitTestMask(gfx::Path* mask) const override {
-    notification_surface_->GetHitTestMask(mask);
-  }
-  void OnKeyEvent(ui::KeyEvent* event) override {
-    // Propagates the key event upto the top-level views Widget so that we can
-    // trigger proper events in the views/ash level there. Event handling for
-    // Surfaces is done in a post event handler in keyboard.cc.
-    views::Widget* widget = views::Widget::GetTopLevelWidgetForNativeView(
-        notification_surface_->host_window());
-    if (widget)
-      widget->OnKeyEvent(event);
-  }
-
- private:
-  NotificationSurface* const notification_surface_;
-
-  DISALLOW_COPY_AND_ASSIGN(CustomWindowDelegate);
-};
-
-}  // namespace
 
 NotificationSurface::NotificationSurface(NotificationSurfaceManager* manager,
                                          Surface* surface,
                                          const std::string& notification_key)
-    : SurfaceTreeHost("ExoNotificationSurface", new CustomWindowDelegate(this)),
+    : SurfaceTreeHost("ExoNotificationSurface", nullptr),
       manager_(manager),
       notification_key_(notification_key) {
   surface->AddSurfaceObserver(this);
