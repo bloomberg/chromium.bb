337afc5837ccc99cfb1499f53a4d970c50faf018
Etienne Bergeron <etienneb@chromium.org>
2017-08-08 18:15:54 +0000

    Call garbage collector when running with LSAN
    LSAN is reporting objects that are allocated with PartitionAlloc
    (replaced by Malloc when using LSAN). These object are kept alive
    because some Blink (oilpan) objects are retaining them.
    
    In this case, StyleRule is a oilpan garbage collected object but has
    a member of type |CSSSelectorList| which is not garbage collected.
    
      see: https://cs.chromium.org/chromium/src/third_party/WebKit/Source/core/css/StyleRule.h?q=StyleRule&sq=package:chromium&dr=CSs&l=146
    
    
    This CSSSelector is retaining the object reported as a leak:
      see: https://cs.chromium.org/chromium/src/third_party/WebKit/Source/core/css/CSSSelectorList.h?sq=package:chromium&dr=CSs&l=130
    
    
    By calling the GC, the StyleRule are freed and it is releasing the memory allocated by PartitionAlloc.
    
    Bug: 743134
    Change-Id: Iea84298e4b087c6007124b436243c08bb381a4a9
    Reviewed-on: https://chromium-review.googlesource.com/602070
    Reviewed-by: Keishi Hattori <keishi@chromium.org>
    Commit-Queue: Etienne Bergeron <etienneb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492701}
    Former-commit-id: b6018356b662d5a88cd30f906e4f041c68c8c3f6

--- third_party/WebKit/Source/core/css/StyleSheetContentsFuzzer.cpp
+++ third_party/WebKit/Source/core/css/StyleSheetContentsFuzzer.cpp
@@ -14,8 +14,17 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
       blink::CSSParserContext::Create(blink::kHTMLStandardMode);
   blink::StyleSheetContents* styleSheet =
       blink::StyleSheetContents::Create(context);
+
   styleSheet->ParseString(String::FromUTF8WithLatin1Fallback(
       reinterpret_cast<const char*>(data), size));
+
+#if defined(ADDRESS_SANITIZER)
+  // LSAN needs unreachable objects to be released to avoid reporting them
+  // incorrectly as a memory leak.
+  blink::ThreadState* currentThreadState = blink::ThreadState::Current();
+  currentThreadState->CollectAllGarbage();
+#endif
+
   return 0;
 }
 
