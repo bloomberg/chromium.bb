97d73e6b6d70c267c631537e1563927e11de7718
Sam Clegg <sbc@chromium.org>
2017-08-08 18:57:26 +0000

    Fix library dependencies of sel_ldr_arm
    Chromium recently switched to a builtin libc++ for linux
    binaries which changes to dependencies of sel_ldr_arm.
    
    Bug: 
    Change-Id: Ic96218746c30911eaf0aa43397c9ad0f245bd46f
    Reviewed-on: https://chromium-review.googlesource.com/606610
    Reviewed-by: Ben Smith <binji@chromium.org>
    Commit-Queue: Sam Clegg <sbc@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492716}
    Former-commit-id: af6af98ffdf664fe2c7aa292eeb1f6563c52a5bc

--- native_client_sdk/src/build_tools/build_sdk.py
+++ native_client_sdk/src/build_tools/build_sdk.py
@@ -813,12 +813,12 @@ def main(args):
     # needed to be able to run sel_ldr_arm under qemu.
     arm_libs = [
       'lib/arm-linux-gnueabihf/librt.so.1',
+      'lib/arm-linux-gnueabihf/libdl.so.2',
       'lib/arm-linux-gnueabihf/libpthread.so.0',
       'lib/arm-linux-gnueabihf/libgcc_s.so.1',
       'lib/arm-linux-gnueabihf/libc.so.6',
       'lib/arm-linux-gnueabihf/ld-linux-armhf.so.3',
       'lib/arm-linux-gnueabihf/libm.so.6',
-      'usr/lib/arm-linux-gnueabihf/libstdc++.so.6'
     ]
     arm_lib_dir = os.path.join(pepperdir, 'tools', 'lib', 'arm_trusted', 'lib')
     buildbot_common.MakeDir(arm_lib_dir)
