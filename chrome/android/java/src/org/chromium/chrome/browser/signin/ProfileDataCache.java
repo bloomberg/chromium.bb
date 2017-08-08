2306713118a2041f71484c5fa342b878e3d6c005
Boris Sazonov <bsazonov@chromium.org>
2017-08-08 10:54:25 +0000

    [Android] Unify profile data handling
    This CL unifies profile info handling in UI-related code on Android:
    1. Add avatar badge support to ProfileDataCache.
    2. Remove code for processing avatars and static cache for profile info from
       AccountManagementFragment and use ProfileDataCache instead.
    
    Bug: 746519
    Change-Id: I7d12bc5e8b9e3fc6bd47e4f4e249d0c5a82d700f
    Reviewed-on: https://chromium-review.googlesource.com/593617
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492590}
    Former-commit-id: a6e7f8db46921a734d3aa7ab367322b936aeee0f

--- chrome/android/java/src/org/chromium/chrome/browser/signin/ProfileDataCache.java
+++ chrome/android/java/src/org/chromium/chrome/browser/signin/ProfileDataCache.java
@@ -5,17 +5,21 @@
 package org.chromium.chrome.browser.signin;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.graphics.PorterDuff;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.MainThread;
+import android.support.annotation.Nullable;
 import android.support.v7.content.res.AppCompatResources;
 
 import org.chromium.base.ObserverList;
@@ -45,6 +49,44 @@ public class ProfileDataCache implements ProfileDownloader.Observer {
         void onProfileDataUpdated(String accountId);
     }
 
+    /**
+     * Encapsulates info necessary to overlay a circular badge (e.g., child account icon) on top of
+     * a user avatar.
+     */
+    public static class BadgeConfig {
+        private final Bitmap mBadge;
+        private final Point mPosition;
+        private final int mBorderSize;
+
+        /**
+         * @param badge Square badge bitmap to overlay on user avatar. Will be cropped to a circular
+         *         one while overlaying.
+         * @param position Position of top left corner of a badge relative to top left corner of
+         *         avatar.
+         * @param borderSize Size of a transparent border around badge.
+         */
+        public BadgeConfig(Bitmap badge, Point position, int borderSize) {
+            assert badge.getHeight() == badge.getWidth();
+            assert position != null;
+
+            mBadge = badge;
+            mPosition = position;
+            mBorderSize = borderSize;
+        }
+
+        Bitmap getBadge() {
+            return mBadge;
+        }
+
+        Point getPosition() {
+            return mPosition;
+        }
+
+        int getBorderSize() {
+            return mBorderSize;
+        }
+    }
+
     private static class CacheEntry {
         public CacheEntry(Drawable picture, String fullName, String givenName) {
             this.picture = picture;
@@ -60,14 +102,22 @@ public class ProfileDataCache implements ProfileDownloader.Observer {
     private final Context mContext;
     private final Profile mProfile;
     private final int mImageSize;
+    @Nullable
+    private final BadgeConfig mBadgeConfig;
     private final Drawable mPlaceholderImage;
     private final ObserverList<Observer> mObservers = new ObserverList<>();
     private final HashMap<String, CacheEntry> mCacheEntries = new HashMap<>();
 
     public ProfileDataCache(Context context, Profile profile, int imageSize) {
+        this(context, profile, imageSize, null);
+    }
+
+    public ProfileDataCache(
+            Context context, Profile profile, int imageSize, @Nullable BadgeConfig badgeConfig) {
         mContext = context;
         mProfile = profile;
         mImageSize = imageSize;
+        mBadgeConfig = badgeConfig;
 
         mPlaceholderImage =
                 AppCompatResources.getDrawable(context, R.drawable.logo_avatar_anonymous);
@@ -149,14 +199,18 @@ public class ProfileDataCache implements ProfileDownloader.Observer {
     public void onProfileDownloaded(String accountId, String fullName, String givenName,
             Bitmap bitmap) {
         ThreadUtils.assertOnUiThread();
-        Drawable drawable = bitmap != null ? getCroppedAvatar(bitmap) : mPlaceholderImage;
-        mCacheEntries.put(accountId, new CacheEntry(drawable, fullName, givenName));
+        mCacheEntries.put(accountId, new CacheEntry(prepareAvatar(bitmap), fullName, givenName));
         for (Observer observer : mObservers) {
             observer.onProfileDataUpdated(accountId);
         }
     }
 
-    private Drawable getCroppedAvatar(Bitmap bitmap) {
+    /**
+     * Crops avatar image into a circle.
+     */
+    public static Drawable makeRoundAvatar(Resources resources, Bitmap bitmap) {
+        if (bitmap == null) return null;
+
         Bitmap output = Bitmap.createBitmap(
                 bitmap.getWidth(), bitmap.getHeight(), Config.ARGB_8888);
         Canvas canvas = new Canvas(output);
@@ -173,6 +227,42 @@ public class ProfileDataCache implements ProfileDownloader.Observer {
         paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
         canvas.drawBitmap(bitmap, rect, rect, paint);
 
-        return new BitmapDrawable(mContext.getResources(), output);
+        return new BitmapDrawable(resources, output);
+    }
+
+    private Drawable prepareAvatar(Bitmap bitmap) {
+        Drawable croppedAvatar = bitmap != null ? makeRoundAvatar(mContext.getResources(), bitmap)
+                                                : mPlaceholderImage;
+        if (mBadgeConfig == null) {
+            return croppedAvatar;
+        }
+        return overlayBadgeOnUserPicture(croppedAvatar);
+    }
+
+    private Drawable overlayBadgeOnUserPicture(Drawable userPicture) {
+        int badgeSize = mBadgeConfig.getBadge().getHeight();
+        int badgedPictureWidth = Math.max(mBadgeConfig.getPosition().x + badgeSize, mImageSize);
+        int badgedPictureHeight = Math.max(mBadgeConfig.getPosition().y + badgeSize, mImageSize);
+        Bitmap badgedPicture = Bitmap.createBitmap(
+                badgedPictureWidth, badgedPictureHeight, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(badgedPicture);
+        userPicture.setBounds(0, 0, mImageSize, mImageSize);
+        userPicture.draw(canvas);
+
+        // Cut a transparent hole through the background image.
+        // This will serve as a border to the badge being overlaid.
+        Paint paint = new Paint();
+        paint.setAntiAlias(true);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
+        int badgeRadius = badgeSize / 2;
+        int badgeCenterX = mBadgeConfig.getPosition().x + badgeRadius;
+        int badgeCenterY = mBadgeConfig.getPosition().y + badgeRadius;
+        canvas.drawCircle(
+                badgeCenterX, badgeCenterY, badgeRadius + mBadgeConfig.getBorderSize(), paint);
+
+        // Draw the badge
+        canvas.drawBitmap(mBadgeConfig.getBadge(), mBadgeConfig.getPosition().x,
+                mBadgeConfig.getPosition().y, null);
+        return new BitmapDrawable(mContext.getResources(), badgedPicture);
     }
 }
