84f43e00ac2cc8fe624e9dbafbd5ff60288d71a2
Bence Béky <bnc@chromium.org>
2017-08-08 15:52:22 +0000

    Delete SpdyFramerDecoderAdapter interface layer.
    This is another step towards cleaning up SpdyFramer.
    
    This CL lands server change 164303660 by dahollings.
    
    BUG=488484
    
    Change-Id: I6e20c30dab80500682905d54a568b450d12efbcd
    Reviewed-on: https://chromium-review.googlesource.com/603780
    Commit-Queue: Bence Béky <bnc@chromium.org>
    Reviewed-by: Helen Li <xunjieli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492638}
    Former-commit-id: f88f1829c4c981a7c4044195ad762fe18919b483

--- net/spdy/core/http2_frame_decoder_adapter.h
+++ net/spdy/core/http2_frame_decoder_adapter.h
@@ -5,15 +5,13 @@
 #ifndef NET_SPDY_CORE_HTTP2_FRAME_DECODER_ADAPTER_H_
 #define NET_SPDY_CORE_HTTP2_FRAME_DECODER_ADAPTER_H_
 
-// Provides a SpdyFramerDecoderAdapter that uses Http2FrameDecoder for decoding
-// HTTP/2 frames. The adapter does not directly decode HPACK, but instead calls
-// SpdyFramer::GetHpackDecoderAdapter() to get the decoder to be used.
-
 #include <stddef.h>
 
 #include <cstdint>
 #include <memory>
 
+#include "base/optional.h"
+#include "net/http2/decoder/http2_frame_decoder.h"
 #include "net/spdy/core/hpack/hpack_header_table.h"
 #include "net/spdy/core/spdy_alt_svc_wire_format.h"
 #include "net/spdy/core/spdy_framer.h"
@@ -23,7 +21,7 @@
 
 namespace net {
 
-// SpdyFramerDecoderAdapter will use the given visitor implementing this
+// Http2DecoderAdapter will use the given visitor implementing this
 // interface to deliver event callbacks as frames are decoded.
 //
 // Control frames that contain HTTP2 header blocks (HEADER, and PUSH_PROMISE)
@@ -182,42 +180,42 @@ class SPDY_EXPORT_PRIVATE SpdyFramerVisitorInterface {
   virtual bool OnUnknownFrame(SpdyStreamId stream_id, uint8_t frame_type) = 0;
 };
 
-// Abstract base class for an HTTP/2 decoder to be called from SpdyFramer.
-class SpdyFramerDecoderAdapter {
+// Adapts SpdyFramer interface to use Http2FrameDecoder.
+class SPDY_EXPORT_PRIVATE Http2DecoderAdapter
+    : public Http2FrameDecoderListener {
  public:
-  SpdyFramerDecoderAdapter();
-  virtual ~SpdyFramerDecoderAdapter();
+  Http2DecoderAdapter();
+  ~Http2DecoderAdapter() override;
 
   // Set callbacks to be called from the framer.  A visitor must be set, or
   // else the framer will likely crash.  It is acceptable for the visitor
   // to do nothing.  If this is called multiple times, only the last visitor
   // will be used.
-  virtual void set_visitor(SpdyFramerVisitorInterface* visitor);
+  void set_visitor(SpdyFramerVisitorInterface* visitor);
   SpdyFramerVisitorInterface* visitor() const { return visitor_; }
 
   // Set extension callbacks to be called from the framer or decoder. Optional.
   // If called multiple times, only the last visitor will be used.
-  virtual void set_extension_visitor(ExtensionVisitorInterface* visitor) = 0;
+  void set_extension_visitor(ExtensionVisitorInterface* visitor);
 
   // Set debug callbacks to be called from the framer. The debug visitor is
   // completely optional and need not be set in order for normal operation.
   // If this is called multiple times, only the last visitor will be used.
-  virtual void set_debug_visitor(
-      SpdyFramerDebugVisitorInterface* debug_visitor);
+  void set_debug_visitor(SpdyFramerDebugVisitorInterface* debug_visitor);
   SpdyFramerDebugVisitorInterface* debug_visitor() const {
     return debug_visitor_;
   }
 
   // Set debug callbacks to be called from the HPACK decoder.
-  virtual void SetDecoderHeaderTableDebugVisitor(
-      std::unique_ptr<HpackHeaderTable::DebugVisitorInterface> visitor) = 0;
+  void SetDecoderHeaderTableDebugVisitor(
+      std::unique_ptr<HpackHeaderTable::DebugVisitorInterface> visitor);
 
   // Sets whether or not ProcessInput returns after finishing a frame, or
   // continues processing additional frames. Normally ProcessInput processes
   // all input, but this method enables the caller (and visitor) to work with
   // a single frame at a time (or that portion of the frame which is provided
   // as input). Reset() does not change the value of this flag.
-  virtual void set_process_single_input_frame(bool v);
+  void set_process_single_input_frame(bool v);
   bool process_single_input_frame() const {
     return process_single_input_frame_;
   }
@@ -226,35 +224,206 @@ class SpdyFramerDecoderAdapter {
   // the number of bytes consumed. It is safe to pass more bytes in than
   // may be consumed. Should process (or otherwise buffer) as much as
   // available, unless process_single_input_frame is true.
-  virtual size_t ProcessInput(const char* data, size_t len) = 0;
+  size_t ProcessInput(const char* data, size_t len);
 
   // Reset the decoder (used just for tests at this time).
-  virtual void Reset() = 0;
+  void Reset();
 
   // Current state of the decoder.
-  virtual SpdyFramer::SpdyState state() const = 0;
+  SpdyFramer::SpdyState state() const;
 
   // Current error code (NO_ERROR if state != ERROR).
-  virtual SpdyFramer::SpdyFramerError spdy_framer_error() const = 0;
+  SpdyFramer::SpdyFramerError spdy_framer_error() const;
 
   // Has any frame header looked like the start of an HTTP/1.1 (or earlier)
   // response? Used to detect if a backend/server that we sent a request to
   // has responded with an HTTP/1.1 (or earlier) response.
-  virtual bool probable_http_response() const = 0;
+  bool probable_http_response() const;
 
   // Returns the estimate of dynamically allocated memory in bytes.
-  virtual size_t EstimateMemoryUsage() const = 0;
+  size_t EstimateMemoryUsage() const;
 
-  // Get (and lazily initialize) the HPACK decoder state.
-  virtual HpackDecoderAdapter* GetHpackDecoder() = 0;
+  HpackDecoderAdapter* GetHpackDecoder();
 
  private:
+  bool OnFrameHeader(const Http2FrameHeader& header) override;
+  void OnDataStart(const Http2FrameHeader& header) override;
+  void OnDataPayload(const char* data, size_t len) override;
+  void OnDataEnd() override;
+  void OnHeadersStart(const Http2FrameHeader& header) override;
+  void OnHeadersPriority(const Http2PriorityFields& priority) override;
+  void OnHpackFragment(const char* data, size_t len) override;
+  void OnHeadersEnd() override;
+  void OnPriorityFrame(const Http2FrameHeader& header,
+                       const Http2PriorityFields& priority) override;
+  void OnContinuationStart(const Http2FrameHeader& header) override;
+  void OnContinuationEnd() override;
+  void OnPadLength(size_t trailing_length) override;
+  void OnPadding(const char* padding, size_t skipped_length) override;
+  void OnRstStream(const Http2FrameHeader& header,
+                   Http2ErrorCode http2_error_code) override;
+  void OnSettingsStart(const Http2FrameHeader& header) override;
+  void OnSetting(const Http2SettingFields& setting_fields) override;
+  void OnSettingsEnd() override;
+  void OnSettingsAck(const Http2FrameHeader& header) override;
+  void OnPushPromiseStart(const Http2FrameHeader& header,
+                          const Http2PushPromiseFields& promise,
+                          size_t total_padding_length) override;
+  void OnPushPromiseEnd() override;
+  void OnPing(const Http2FrameHeader& header,
+              const Http2PingFields& ping) override;
+  void OnPingAck(const Http2FrameHeader& header,
+                 const Http2PingFields& ping) override;
+  void OnGoAwayStart(const Http2FrameHeader& header,
+                     const Http2GoAwayFields& goaway) override;
+  void OnGoAwayOpaqueData(const char* data, size_t len) override;
+  void OnGoAwayEnd() override;
+  void OnWindowUpdate(const Http2FrameHeader& header,
+                      uint32_t increment) override;
+  void OnAltSvcStart(const Http2FrameHeader& header,
+                     size_t origin_length,
+                     size_t value_length) override;
+  void OnAltSvcOriginData(const char* data, size_t len) override;
+  void OnAltSvcValueData(const char* data, size_t len) override;
+  void OnAltSvcEnd() override;
+  void OnUnknownStart(const Http2FrameHeader& header) override;
+  void OnUnknownPayload(const char* data, size_t len) override;
+  void OnUnknownEnd() override;
+  void OnPaddingTooLong(const Http2FrameHeader& header,
+                        size_t missing_length) override;
+  void OnFrameSizeError(const Http2FrameHeader& header) override;
+
+  size_t ProcessInputFrame(const char* data, size_t len);
+
+  void DetermineSpdyState(DecodeStatus status);
+  void ResetBetweenFrames();
+
+  // ResetInternal is called from the constructor, and during tests, but not
+  // otherwise (i.e. not between every frame).
+  void ResetInternal();
+
+  void set_spdy_state(SpdyFramer::SpdyState v);
+
+  void SetSpdyErrorAndNotify(SpdyFramer::SpdyFramerError error);
+
+  bool HasError() const;
+  const Http2FrameHeader& frame_header() const;
+
+  uint32_t stream_id() const;
+  Http2FrameType frame_type() const;
+
+  size_t remaining_total_payload() const;
+
+  bool IsReadingPaddingLength();
+  bool IsSkippingPadding();
+  bool IsDiscardingPayload();
+  // Called from OnXyz or OnXyzStart methods to decide whether it is OK to
+  // handle the callback.
+  bool IsOkToStartFrame(const Http2FrameHeader& header);
+  bool HasRequiredStreamId(uint32_t stream_id);
+
+  bool HasRequiredStreamId(const Http2FrameHeader& header);
+
+  bool HasRequiredStreamIdZero(uint32_t stream_id);
+
+  bool HasRequiredStreamIdZero(const Http2FrameHeader& header);
+
+  void ReportReceiveCompressedFrame(const Http2FrameHeader& header);
+
+  void CommonStartHpackBlock();
+
+  // SpdyFramer calls HandleControlFrameHeadersData even if there are zero
+  // fragment bytes in the first frame, so do the same.
+  void MaybeAnnounceEmptyFirstHpackFragment();
+  void CommonHpackFragmentEnd();
+
+  // The most recently decoded frame header; invalid after we reached the end
+  // of that frame.
+  Http2FrameHeader frame_header_;
+
+  // If decoding an HPACK block that is split across multiple frames, this holds
+  // the frame header of the HEADERS or PUSH_PROMISE that started the block.
+  Http2FrameHeader hpack_first_frame_header_;
+
+  // Amount of trailing padding. Currently used just as an indicator of whether
+  // OnPadLength has been called.
+  base::Optional<size_t> opt_pad_length_;
+
+  // Temporary buffers for the AltSvc fields.
+  Http2String alt_svc_origin_;
+  Http2String alt_svc_value_;
+
+  // Listener used if we transition to an error state; the listener ignores all
+  // the callbacks.
+  Http2FrameDecoderNoOpListener no_op_listener_;
+
   SpdyFramerVisitorInterface* visitor_ = nullptr;
   SpdyFramerDebugVisitorInterface* debug_visitor_ = nullptr;
+
+  // If non-null, unknown frames and settings are passed to the extension.
+  ExtensionVisitorInterface* extension_ = nullptr;
+
+  // The HPACK decoder to be used for this adapter. User is responsible for
+  // clearing if the adapter is to be used for another connection.
+  std::unique_ptr<HpackDecoderAdapter> hpack_decoder_ = nullptr;
+
+  // The HTTP/2 frame decoder. Accessed via a unique_ptr to allow replacement
+  // (e.g. in tests) when Reset() is called.
+  std::unique_ptr<Http2FrameDecoder> frame_decoder_;
+
+  // Next frame type expected. Currently only used for CONTINUATION frames,
+  // but could be used for detecting whether the first frame is a SETTINGS
+  // frame.
+  // TODO(jamessyng): Provide means to indicate that decoder should require
+  // SETTINGS frame as the first frame.
+  Http2FrameType expected_frame_type_;
+
+  // Attempt to duplicate the SpdyState and SpdyFramerError values that
+  // SpdyFramer sets. Values determined by getting tests to pass.
+  SpdyFramer::SpdyState spdy_state_;
+  SpdyFramer::SpdyFramerError spdy_framer_error_;
+
+  // The limit on the size of received HTTP/2 payloads as specified in the
+  // SETTINGS_MAX_FRAME_SIZE advertised to peer.
+  size_t recv_frame_size_limit_ = kSpdyInitialFrameSizeLimit;
+
+  // Has OnFrameHeader been called?
+  bool decoded_frame_header_ = false;
+
+  // Have we recorded an Http2FrameHeader for the current frame?
+  // We only do so if the decoder will make multiple callbacks for
+  // the frame; for example, for PING frames we don't make record
+  // the frame header, but for ALTSVC we do.
+  bool has_frame_header_ = false;
+
+  // Have we recorded an Http2FrameHeader for the current HPACK block?
+  // True only for multi-frame HPACK blocks.
+  bool has_hpack_first_frame_header_ = false;
+
+  // Has OnHeaders() already been called for current HEADERS block? Only
+  // meaningful between OnHeadersStart and OnHeadersPriority.
+  bool on_headers_called_;
+
+  // Has OnHpackFragment() already been called for current HPACK block?
+  // SpdyFramer will pass an empty buffer to the HPACK decoder if a HEADERS
+  // or PUSH_PROMISE has no HPACK data in it (e.g. a HEADERS frame with only
+  // padding). Detect that condition and replicate the behavior using this
+  // field.
+  bool on_hpack_fragment_called_;
+
+  // Have we seen a frame header that appears to be an HTTP/1 response?
+  bool latched_probable_http_response_ = false;
+
+  // Is expected_frame_type_ set?
+  bool has_expected_frame_type_ = false;
+
+  // Is the current frame payload destined for |extension_|?
+  bool handling_extension_payload_ = false;
+
   bool process_single_input_frame_ = false;
 };
 
-std::unique_ptr<SpdyFramerDecoderAdapter> CreateHttp2FrameDecoderAdapter();
+std::unique_ptr<Http2DecoderAdapter> CreateHttp2FrameDecoderAdapter();
 
 }  // namespace net
 
