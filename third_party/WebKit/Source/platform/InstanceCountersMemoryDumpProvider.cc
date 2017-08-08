5b6c15e0237a552315ee16c4c0c16ee5aca24769
Alexei Filippov <alph@chromium.org>
2017-08-08 02:44:00 +0000

    Refactor instance counters.
    Change-Id: I717fb657e0dba984bd69df20f12b0698f90b3042
    Reviewed-on: https://chromium-review.googlesource.com/601424
    Reviewed-by: Pavel Feldman <pfeldman@chromium.org>
    Commit-Queue: Alexei Filippov <alph@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492512}
    Former-commit-id: 8b916016dd37b9312f77854bec61d1c1fc1696d7

--- third_party/WebKit/Source/platform/InstanceCountersMemoryDumpProvider.cc
+++ third_party/WebKit/Source/platform/InstanceCountersMemoryDumpProvider.cc
@@ -20,51 +20,12 @@ bool InstanceCountersMemoryDumpProvider::OnMemoryDump(
     const base::trace_event::MemoryDumpArgs& args,
     base::trace_event::ProcessMemoryDump* memory_dump) {
   using base::trace_event::MemoryAllocatorDump;
-#define DUMP_COUNTER(CounterType)                                            \
-  {                                                                          \
-    MemoryAllocatorDump* counter_dump =                                      \
-        memory_dump->CreateAllocatorDump("counter/" #CounterType);           \
-    counter_dump->AddScalar("object_count",                                  \
-                            MemoryAllocatorDump::kUnitsObjects,              \
-                            InstanceCounters::CounterValue(                  \
-                                InstanceCounters::k##CounterType##Counter)); \
-    break;                                                                   \
-  }
-
-  for (int i = 0; i < InstanceCounters::kCounterTypeLength; ++i) {
-    InstanceCounters::CounterType type =
-        static_cast<InstanceCounters::CounterType>(i);
-    switch (type) {
-      case InstanceCounters::kAudioHandlerCounter:
-        DUMP_COUNTER(AudioHandler)
-      case InstanceCounters::kDocumentCounter:
-        DUMP_COUNTER(Document)
-      case InstanceCounters::kFrameCounter:
-        DUMP_COUNTER(Frame)
-      case InstanceCounters::kJSEventListenerCounter:
-        DUMP_COUNTER(JSEventListener)
-      case InstanceCounters::kLayoutObjectCounter:
-        DUMP_COUNTER(LayoutObject)
-      case InstanceCounters::kMediaKeySessionCounter:
-        DUMP_COUNTER(MediaKeySession)
-      case InstanceCounters::kMediaKeysCounter:
-        DUMP_COUNTER(MediaKeys)
-      case InstanceCounters::kNodeCounter:
-        DUMP_COUNTER(Node)
-      case InstanceCounters::kResourceCounter:
-        DUMP_COUNTER(Resource)
-      case InstanceCounters::kScriptPromiseCounter:
-        DUMP_COUNTER(ScriptPromise)
-      case InstanceCounters::kSuspendableObjectCounter:
-        DUMP_COUNTER(SuspendableObject)
-      case InstanceCounters::kV8PerContextDataCounter:
-        DUMP_COUNTER(V8PerContextData)
-      case InstanceCounters::kWorkerGlobalScopeCounter:
-        DUMP_COUNTER(WorkerGlobalScope)
-      case InstanceCounters::kCounterTypeLength:
-        break;
-    }
-  }
+#define DUMP_COUNTER(CounterType)                                     \
+  memory_dump->CreateAllocatorDump("counter/" #CounterType)           \
+      ->AddScalar("object_count", MemoryAllocatorDump::kUnitsObjects, \
+                  InstanceCounters::CounterValue(                     \
+                      InstanceCounters::k##CounterType##Counter));
+  INSTANCE_COUNTERS_LIST(DUMP_COUNTER)
   return true;
 }
 
