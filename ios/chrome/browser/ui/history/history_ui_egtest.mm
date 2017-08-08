f5405a04906e0f9fbbb271eb206ee6ed4ded6c55
Yuke Liao <liaoyuke@chromium.org>
2017-08-08 17:22:49 +0000

    Fix and re-enable testHistoryEntriesStatusCell on iOS 11.
    This CL fixes and re-enables the failing testHistoryEntriesStatusCell
    test on iOS 11.
    
    Bug: 750229
    Change-Id: I2e919536e2970b2c5eac94e5f7ee387fd258d338
    Reviewed-on: https://chromium-review.googlesource.com/602587
    Reviewed-by: Mike Baxley <baxley@chromium.org>
    Reviewed-by: Sergio Collazos <sczs@chromium.org>
    Commit-Queue: Yuke Liao <liaoyuke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492679}
    Former-commit-id: 25259b5629d9b6459a49e0555af6b1f79d170976

--- ios/chrome/browser/ui/history/history_ui_egtest.mm
+++ ios/chrome/browser/ui/history/history_ui_egtest.mm
@@ -133,36 +133,6 @@ id<GREYMatcher> ClearBrowsingDataButton() {
 id<GREYMatcher> ConfirmClearBrowsingDataButton() {
   return ButtonWithAccessibilityLabelId(IDS_IOS_CONFIRM_CLEAR_BUTTON);
 }
-
-// Sign in with a mock identity.
-void MockSignIn() {
-  // Set up a mock identity.
-  ChromeIdentity* identity =
-      [FakeChromeIdentity identityWithEmail:@"foo@gmail.com"
-                                     gaiaID:@"fooID"
-                                       name:@"Fake Foo"];
-  ios::FakeChromeIdentityService::GetInstanceFromChromeProvider()->AddIdentity(
-      identity);
-
-  [ChromeEarlGreyUI openSettingsMenu];
-  [[EarlGrey
-      selectElementWithMatcher:grey_accessibilityID(kSettingsSignInCellId)]
-      performAction:grey_tap()];
-  [[EarlGrey
-      selectElementWithMatcher:chrome_test_util::ButtonWithAccessibilityLabel(
-                                   identity.userEmail)]
-      performAction:grey_tap()];
-  [[EarlGrey selectElementWithMatcher:
-                 chrome_test_util::ButtonWithAccessibilityLabelId(
-                     IDS_IOS_ACCOUNT_CONSISTENCY_SETUP_SIGNIN_BUTTON)]
-      performAction:grey_tap()];
-  [[EarlGrey selectElementWithMatcher:
-                 chrome_test_util::ButtonWithAccessibilityLabelId(
-                     IDS_IOS_ACCOUNT_CONSISTENCY_CONFIRMATION_OK_BUTTON)]
-      performAction:grey_tap()];
-  [[EarlGrey selectElementWithMatcher:NavigationBarDoneButton()]
-      performAction:grey_tap()];
-}
 }  // namespace
 
 // History UI tests.
@@ -276,12 +246,6 @@ void MockSignIn() {
 // in, and that tapping on the link in the message opens a new tab with the sync
 // help page.
 - (void)testHistoryEntriesStatusCell {
-  // TODO(crbug.com/747445): Re-enable this test on iOS 11 once the sign in UI
-  // is fixed.
-  if (base::ios::IsRunningOnIOS11OrLater()) {
-    EARL_GREY_TEST_DISABLED(@"Disabled on iOS 11.");
-  }
-
   [self loadTestURLs];
   [self openHistoryPanel];
   // Assert that no message is shown when the user is not signed in.
@@ -293,9 +257,21 @@ void MockSignIn() {
   [[EarlGrey selectElementWithMatcher:NavigationBarDoneButton()]
       performAction:grey_tap()];
 
-  // Sign in and assert that the page indicates what type of history entries
-  // are shown.
-  MockSignIn();
+  // Mock sign in and assert that the page indicates what type of history
+  // entries are shown.
+  ChromeIdentity* identity =
+      [FakeChromeIdentity identityWithEmail:@"foo@gmail.com"
+                                     gaiaID:@"fooID"
+                                       name:@"Fake Foo"];
+  ios::FakeChromeIdentityService::GetInstanceFromChromeProvider()->AddIdentity(
+      identity);
+  [ChromeEarlGreyUI openSettingsMenu];
+  [ChromeEarlGreyUI tapSettingsMenuButton:chrome_test_util::SignInMenuButton()];
+  [ChromeEarlGreyUI signInToIdentityByEmail:identity.userEmail];
+  [ChromeEarlGreyUI confirmSigninConfirmationDialog];
+  [[EarlGrey selectElementWithMatcher:NavigationBarDoneButton()]
+      performAction:grey_tap()];
+
   [self openHistoryPanel];
   // Assert that message about entries is shown. The "history is showing local
   // entries" message will be shown because sync is not set up for this test.
