6999a42dd963a38fd89e24a687e40108da91f2a4
Makoto Shimazu <shimazu@chromium.org>
2017-08-08 03:25:35 +0000

    Disabled flaky test: SingleProcessMemoryTracingTest.QueuedDumps
    According to the flakiness dashboard, the test is sometimes failing.
    
    TBR=primiano@chromium.org
    
    Bug: 752613
    Change-Id: I57a4028d259a33f7b228a2e7f9dad6da78d06939
    Reviewed-on: https://chromium-review.googlesource.com/604630
    Reviewed-by: Makoto Shimazu <shimazu@chromium.org>
    Commit-Queue: Makoto Shimazu <shimazu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492518}
    Former-commit-id: 9652ffa58183a00262d59b87d88d1eda8226c26b

--- content/browser/tracing/memory_tracing_browsertest.cc
+++ content/browser/tracing/memory_tracing_browsertest.cc
@@ -236,7 +236,8 @@ IN_PROC_BROWSER_TEST_F(SingleProcessMemoryTracingTest, ManyInterleavedDumps) {
 // dump requests are queued and carried out after it's finished. Also checks
 // that periodic dump requests fail in case there is already a request in the
 // queue with the same level of detail.
-IN_PROC_BROWSER_TEST_F(SingleProcessMemoryTracingTest, QueuedDumps) {
+// Flaky failures on all platforms. https://crbug.com/752613
+IN_PROC_BROWSER_TEST_F(SingleProcessMemoryTracingTest, DISABLED_QueuedDumps) {
   Navigate(shell());
 
   EnableMemoryTracing();
