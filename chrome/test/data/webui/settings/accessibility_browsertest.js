afd7fb1ccf48c035eb9fe0a0b4412aea9649af73
Tina Quach <quacht@google.com>
2017-08-08 23:07:48 +0000

    Add AccessibilityTest abstraction for defining tests.
    In order to make it easier to write accessibility tests, this CL
    adds the AccessibilityTest namespace, which groups all audit and mocha
    test related code so it may be reused to test other Chrome WebUI.
    
    Bug: 737739
    Change-Id: I87cc0db0670a5a17d84406d6d2e3fdd619372774
    Reviewed-on: https://chromium-review.googlesource.com/600894
    Commit-Queue: Tina Quach <quacht@google.com>
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Reviewed-by: Hector Carmona <hcarmona@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492767}
    Former-commit-id: 26fb268e5ed0a275a750f3a895f8eda6dc2cfb6e

--- chrome/test/data/webui/settings/accessibility_browsertest.js
+++ chrome/test/data/webui/settings/accessibility_browsertest.js
@@ -14,19 +14,9 @@ var ROOT_PATH = '../../../../../';
 GEN_INCLUDE([
   ROOT_PATH + 'chrome/test/data/webui/polymer_browser_test_base.js',
   ROOT_PATH + 'chrome/test/data/webui/settings/accessibility_audit_rules.js',
+  ROOT_PATH + 'chrome/test/data/webui/settings/accessibility_audit.js',
   ROOT_PATH + 'third_party/axe-core/axe.js',
 ]);
-
-/**
- * @typedef {{
- *   rules: {
- *     'color_contrast': ({ enabled: boolean} | undefined),
- *     'aria_valid_attr': ({ enabled: boolean} | undefined),
- *   }
- * }}
- * @see https://github.com/dequelabs/axe-core/blob/develop/doc/API.md#options-parameter
- */
-function AccessibilityAuditConfig() {}
 
 /**
  * Test fixture for Accessibility of Chrome Settings.
@@ -34,37 +24,6 @@ function AccessibilityAuditConfig() {}
  * @extends {PolymerTest}
  */
 function SettingsAccessibilityTest() {}
-
-/**
- * Run aXe-core accessibility audit, print console-friendly representation
- * of violations to console, and fail the test if there are audit failures.
- * @param {AccessibilityAuditConfig} options Dictionary disabling specific
- *    audit rules.
- * @return {Promise} A promise resolved when the accessibility audit completes
- *    with no issues, or rejected if the audit finds any accessibility issues.
- */
-SettingsAccessibilityTest.runAudit = function(options) {
-  // Ignore iron-iconset-svg elements that have duplicate ids and result in
-  // false postives from the audit.
-  var context = {exclude: ['iron-iconset-svg']};
-  options = options || {};
-
-  return new Promise(function(resolve, reject) {
-    axe.run(context, options, function(err, results) {
-      if (err)
-        reject(err);
-
-      var violationCount = results.violations.length;
-      if (violationCount) {
-        // Pretty print out the violations detected by the audit.
-        console.log(JSON.stringify(results.violations, null, 4));
-        reject('Found ' + violationCount + ' accessibility violations.');
-      } else {
-        resolve();
-      }
-    });
-  });
-};
 
 SettingsAccessibilityTest.prototype = {
   __proto__: PolymerTest.prototype,
@@ -74,8 +33,9 @@ SettingsAccessibilityTest.prototype = {
 
   // Include files that define the mocha tests.
   extraLibraries: PolymerTest.getLibraries(ROOT_PATH).concat([
-    'ensure_lazy_loaded.js', 'passwords_and_autofill_fake_data.js',
-    'passwords_a11y_test.js'
+    'ensure_lazy_loaded.js',
+    'passwords_and_autofill_fake_data.js',
+    'passwords_a11y_test.js',
   ]),
 
   // TODO(hcarmona): Remove once ADT is not longer in the testing infrastructure
@@ -92,18 +52,21 @@ SettingsAccessibilityTest.prototype = {
 // http://crbug.com/748608
 // http://crbug.com/748632
 var rulesToSkip = ['aria-valid-attr', 'color-contrast', 'region', 'skip-link'];
+// Disable rules flaky for CFI build.
+var flakyRules = ['meta-viewpoint', 'list', 'frame-title', 'label',
+    'hidden_content', 'aria-valid-attr-value', 'button-name'];
+rulesToSkip.concat(flakyRules);
 
 // Define a unit test for every audit rule.
-AccessibilityAudit.ruleIds.forEach(function(ruleId) {
+AccessibilityTest.ruleIds.forEach((ruleId) => {
   if (rulesToSkip.indexOf(ruleId) == -1) {
     // Replace hyphens, which break the build.
     var ruleName = ruleId.replace(new RegExp('-', 'g'), '_');
-    TEST_F(
-        'SettingsAccessibilityTest', 'MANAGE_PASSWORDS_' + ruleName,
-        function() {
+    TEST_F('SettingsAccessibilityTest', 'MANAGE_PASSWORDS_' + ruleName,
+        () => {
           mocha.grep('MANAGE_PASSWORDS_' + ruleId).run();
         });
   }
-})
+});
 
 GEN('#endif  // defined(NDEBUG)');
