4942832e70e45447e9f555579498694801d4432d
Helen Yang <helenlyang@google.com>
2017-08-09 06:00:50 +0000

    [ios] Implement BubbleView and BubbleViewController
    This CL implements BubbleView and BubbleViewController. It adds
    logic to BubbleView to properly layout subviews and calculate its
    optimal size. Additionally, it adds animation logic to
    BubbleViewController's display and dismissal methods.
    
    This CL also adds unit tests for BubbleView, modifies the Showcase
    implementation to display the bubble, and changes the alignment
    offset value in bubble_util_unittest.mm to be consistent with the
    offset value defined in BubbleView.
    
    Screenshots and video of the animation can be found in this folder:
    https://drive.google.com/drive/folders/0B3YZmy_OdK4sTERILW15Q2NHN00?usp=sharing
    
    Bug: 740145
    Change-Id: I175eefbf3b739d48931ef8d8e77130892c3fa27f
    Reviewed-on: https://chromium-review.googlesource.com/598591
    Commit-Queue: Gregory Chatzinoff <gchatz@chromium.org>
    Reviewed-by: Ed Chin <edchin@chromium.org>
    Reviewed-by: Gregory Chatzinoff <gchatz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492844}
    Former-commit-id: 0903eeca7ba94a61b7658db77e390877481c297d

--- ios/chrome/browser/ui/bubble/bubble_view_controller.mm
+++ ios/chrome/browser/ui/bubble/bubble_view_controller.mm
@@ -5,19 +5,26 @@
 #import "ios/chrome/browser/ui/bubble/bubble_view_controller.h"
 
 #include "base/logging.h"
+#include "ios/chrome/browser/ui/animation_util.h"
+#import "ios/chrome/common/material_timing.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
+namespace {
+const CGFloat kAnimationDuration = ios::material::kDuration3;
+// The vertical offset distance used in the sink-down animation.
+const CGFloat kVerticalOffset = 8.0f;
+}  // namespace
+
 @interface BubbleViewController ()
-@property(nonatomic, readonly) NSString* text;
-@property(nonatomic, readonly) BubbleArrowDirection arrowDirection;
-@property(nonatomic, readonly) BubbleAlignment alignment;
+@property(nonatomic, copy, readonly) NSString* text;
+@property(nonatomic, assign, readonly) BubbleArrowDirection arrowDirection;
+@property(nonatomic, assign, readonly) BubbleAlignment alignment;
 @end
 
 @implementation BubbleViewController
-
 @synthesize text = _text;
 @synthesize arrowDirection = _arrowDirection;
 @synthesize alignment = _alignment;
@@ -26,9 +33,11 @@
               arrowDirection:(BubbleArrowDirection)direction
                    alignment:(BubbleAlignment)alignment {
   self = [super initWithNibName:nil bundle:nil];
-  _text = text;
-  _arrowDirection = direction;
-  _alignment = alignment;
+  if (self) {
+    _text = text;
+    _arrowDirection = direction;
+    _alignment = alignment;
+  }
   return self;
 }
 
@@ -36,14 +45,44 @@
   self.view = [[BubbleView alloc] initWithText:self.text
                                 arrowDirection:self.arrowDirection
                                      alignment:self.alignment];
+  // Begin hidden.
+  [self.view setAlpha:0.0f];
+  [self.view setHidden:YES];
 }
 
+// Animate the bubble view in with a fade-in and sink-down animation.
 - (void)animateContentIn {
-  NOTIMPLEMENTED();
+  // Set the frame's origin to be slightly higher on the screen, so that the
+  // view will be properly positioned once it sinks down.
+  CGRect frame = self.view.frame;
+  frame.origin.y = frame.origin.y - kVerticalOffset;
+  [self.view setFrame:frame];
+  [self.view setHidden:NO];
+
+  // Set the y-coordinate of |frame.origin| to its final value.
+  frame.origin.y = frame.origin.y + kVerticalOffset;
+  [UIView animateWithDuration:kAnimationDuration
+                        delay:0.0
+                      options:UIViewAnimationOptionCurveEaseOut
+                   animations:^{
+                     [self.view setFrame:frame];
+                     [self.view setAlpha:1.0f];
+                   }
+                   completion:nil];
 }
 
 - (void)dismissAnimated:(BOOL)animated {
-  NOTIMPLEMENTED();
+  NSTimeInterval duration = (animated ? kAnimationDuration : 0.0);
+  [UIView animateWithDuration:duration
+      animations:^{
+        [self.view setAlpha:0.0f];
+      }
+      completion:^(BOOL finished) {
+        [self.view setHidden:YES];
+        [self willMoveToParentViewController:nil];
+        [self.view removeFromSuperview];
+        [self removeFromParentViewController];
+      }];
 }
 
 @end
