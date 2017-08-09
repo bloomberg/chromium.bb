9e3af3659acccb4e9635e08aef8b985e02fa3336
Walter Korman <wkorman@chromium.org>
2017-08-09 15:24:29 +0000

    Extract FakeScrollableArea from GraphicsLayerTest.
    Intend to use to add unit tests for
    ScrollAnimatorCompositorCoordinator in a separate patch. Currently
    ScrollAnimatorCompositorCoordinator appears to have no direct unit
    tests.
    
    Bug: 709137
    Change-Id: I9f5201c3a1e7aa1a7ef88f9ee88fa4c7dc8ecadd
    Reviewed-on: https://chromium-review.googlesource.com/607048
    Commit-Queue: Walter Korman <wkorman@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492991}
    Former-commit-id: a167bd8be975d40364d6cb142f9825122415d441

--- third_party/WebKit/Source/platform/graphics/GraphicsLayerTest.cpp
+++ third_party/WebKit/Source/platform/graphics/GraphicsLayerTest.cpp
@@ -35,6 +35,7 @@
 #include "platform/animation/CompositorFloatAnimationCurve.h"
 #include "platform/animation/CompositorTargetProperty.h"
 #include "platform/graphics/CompositorElementId.h"
+#include "platform/graphics/test/FakeScrollableArea.h"
 #include "platform/scheduler/child/web_scheduler.h"
 #include "platform/scroll/ScrollableArea.h"
 #include "platform/testing/FakeGraphicsLayer.h"
@@ -165,54 +166,5 @@ TEST_F(GraphicsLayerTest, updateLayerShouldFlattenTransformWithAnimations) {
   compositor_timeline->PlayerDestroyed(player);
   host.RemoveTimeline(*compositor_timeline.get());
 }
-
-class FakeScrollableArea : public GarbageCollectedFinalized<FakeScrollableArea>,
-                           public ScrollableArea {
-  USING_GARBAGE_COLLECTED_MIXIN(FakeScrollableArea);
-
- public:
-  static FakeScrollableArea* Create() { return new FakeScrollableArea; }
-
-  CompositorElementId GetCompositorElementId() const override {
-    return CompositorElementId();
-  }
-  bool IsActive() const override { return false; }
-  int ScrollSize(ScrollbarOrientation) const override { return 100; }
-  bool IsScrollCornerVisible() const override { return false; }
-  IntRect ScrollCornerRect() const override { return IntRect(); }
-  IntRect VisibleContentRect(
-      IncludeScrollbarsInRect = kExcludeScrollbars) const override {
-    return IntRect(ScrollOffsetInt().Width(), ScrollOffsetInt().Height(), 10,
-                   10);
-  }
-  IntSize ContentsSize() const override { return IntSize(100, 100); }
-  bool ScrollbarsCanBeActive() const override { return false; }
-  IntRect ScrollableAreaBoundingBox() const override { return IntRect(); }
-  void ScrollControlWasSetNeedsPaintInvalidation() override {}
-  bool UserInputScrollable(ScrollbarOrientation) const override { return true; }
-  bool ShouldPlaceVerticalScrollbarOnLeft() const override { return false; }
-  int PageStep(ScrollbarOrientation) const override { return 0; }
-  IntSize MinimumScrollOffsetInt() const override { return IntSize(); }
-  IntSize MaximumScrollOffsetInt() const override {
-    return ContentsSize() - IntSize(VisibleWidth(), VisibleHeight());
-  }
-
-  void UpdateScrollOffset(const ScrollOffset& offset, ScrollType) override {
-    scroll_offset_ = offset;
-  }
-  ScrollOffset GetScrollOffset() const override { return scroll_offset_; }
-  IntSize ScrollOffsetInt() const override {
-    return FlooredIntSize(scroll_offset_);
-  }
-
-  RefPtr<WebTaskRunner> GetTimerTaskRunner() const final {
-    return Platform::Current()->CurrentThread()->Scheduler()->TimerTaskRunner();
-  }
-
-  DEFINE_INLINE_VIRTUAL_TRACE() { ScrollableArea::Trace(visitor); }
-
- private:
-  ScrollOffset scroll_offset_;
-};
 
 }  // namespace blink
