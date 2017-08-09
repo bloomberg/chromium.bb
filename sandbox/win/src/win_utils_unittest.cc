8305e25fecc0864295ac2d5cb5f051302bb60fa2
Penny MacNeil <pennymac@chromium.org>
2017-08-09 17:16:41 +0000

    [Windows Sandbox] Possible short/8.3 form native file paths prevent startup.
    win_utils.cc, GetProcessBaseAddress() ends up comparing file paths from two
    system APIs: ::QueryFullProcessImageName() and ::GetMappedFileName().  Both APIs return
    native file paths.  However, depending on filesystem, these paths can be in
    short/8.3 form.  Path comparison in GetProcessBaseAddress can fail, preventing
    process startup.
    
    This CL does the following:
     - GetProcessBaseAddress() now uses sandbox::ConvertToLongPath().
     - sandbox::ConvertToLongPath() now handles extending native device paths.
    
    TESTS=sbox_unittests.exe
    BUG=744638
    
    Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: I86cb8b4c8da13f4405aba16e127c512a1eec539d
    Reviewed-on: https://chromium-review.googlesource.com/580629
    Reviewed-by: James Forshaw <forshaw@chromium.org>
    Commit-Queue: Penny MacNeil <pennymac@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493034}
    Former-commit-id: 0a29c3285746c1a80ca039e7887180fc3f3e159c

--- sandbox/win/src/win_utils_unittest.cc
+++ sandbox/win/src/win_utils_unittest.cc
@@ -7,7 +7,10 @@
 
 #include <vector>
 
+#include "base/files/file_path.h"
+#include "base/files/file_util.h"
 #include "base/numerics/safe_conversions.h"
+#include "base/path_service.h"
 #include "base/win/scoped_handle.h"
 #include "base/win/scoped_process_information.h"
 #include "sandbox/win/src/nt_internals.h"
@@ -208,4 +211,59 @@ TEST(WinUtils, GetProcessBaseAddress) {
   }
   EXPECT_EQ(base_address,
             GetProcessBaseAddress(scoped_proc_info.process_handle()));
+}
+
+// This test requires an elevated prompt to setup.
+TEST(WinUtils, ConvertToLongPath) {
+  // Test setup.
+  base::FilePath orig_path;
+  ASSERT_TRUE(base::PathService::Get(base::DIR_SYSTEM, &orig_path));
+  orig_path = orig_path.Append(L"calc.exe");
+
+  base::FilePath temp_path;
+  ASSERT_TRUE(base::PathService::Get(base::DIR_PROGRAM_FILES, &temp_path));
+  temp_path = temp_path.Append(L"test_calc.exe");
+
+  ASSERT_TRUE(base::CopyFile(orig_path, temp_path));
+  // No more asserts until cleanup.
+
+  // WIN32 long path: "c:\Program Files\test_calc.exe"
+  wchar_t short_path[MAX_PATH] = {};
+  DWORD size =
+      ::GetShortPathNameW(temp_path.value().c_str(), short_path, MAX_PATH);
+  EXPECT_TRUE(size > 0 && size < MAX_PATH);
+  // WIN32 short path: "C:\PROGRA~1\TEST_C~1.exe"
+
+  // Sanity check that we actually got a short path above!  Small chance
+  // it was disabled in the filesystem setup.
+  EXPECT_NE(temp_path.value().length(), ::wcslen(short_path));
+
+  base::string16 short_form_native_path;
+  EXPECT_TRUE(sandbox::GetNtPathFromWin32Path(base::string16(short_path),
+                                              &short_form_native_path));
+  // NT short path: "\Device\HarddiskVolume4\PROGRA~1\TEST_C~1.EXE"
+
+  // Test 1: convert win32 short path to long:
+  base::string16 test1(short_path);
+  EXPECT_TRUE(sandbox::ConvertToLongPath(&test1));
+  EXPECT_TRUE(::wcsicmp(temp_path.value().c_str(), test1.c_str()) == 0);
+  // Expected result: "c:\Program Files\test_calc.exe"
+
+  // Test 2: convert native short path to long:
+  base::string16 drive_letter = temp_path.value().substr(0, 3);
+  base::string16 test2(short_form_native_path);
+  EXPECT_TRUE(sandbox::ConvertToLongPath(&test2, &drive_letter));
+
+  size_t index = short_form_native_path.find_first_of(
+      L'\\', ::wcslen(L"\\Device\\HarddiskVolume"));
+  EXPECT_TRUE(index != base::string16::npos);
+  base::string16 expected_result = short_form_native_path.substr(0, index + 1);
+  expected_result.append(temp_path.value().substr(3));
+  EXPECT_TRUE(::wcsicmp(expected_result.c_str(), test2.c_str()) == 0);
+  // Expected result: "\Device\HarddiskVolumeX\Program Files\test_calc.exe"
+
+  // clean up
+  EXPECT_TRUE(base::DeleteFileW(temp_path, false));
+
+  return;
 }
