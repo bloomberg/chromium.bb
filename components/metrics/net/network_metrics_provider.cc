8359ecb458450443fe428d00d9fce259528df80d
Tarun Bansal <tbansal@chromium.org>
2017-08-09 22:11:34 +0000

    Do not report min_ect as UNKNOWN if the connection type changes to OFFLINE
    Bug: 752962
    Change-Id: I285effcafd51c243cfe5f1fa80310138087a8606
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Reviewed-on: https://chromium-review.googlesource.com/603074
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Reviewed-by: Alexei Svitkine (very slow) <asvitkine@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493159}
    Former-commit-id: 3d1340e2e352246f61b93994e99175fd35c6090d

--- components/metrics/net/network_metrics_provider.cc
+++ components/metrics/net/network_metrics_provider.cc
@@ -449,6 +449,15 @@ void NetworkMetricsProvider::OnEffectiveConnectionTypeChanged(
     return;
   }
 
+  if (min_effective_connection_type_ ==
+          net::EFFECTIVE_CONNECTION_TYPE_OFFLINE &&
+      max_effective_connection_type_ ==
+          net::EFFECTIVE_CONNECTION_TYPE_OFFLINE) {
+    min_effective_connection_type_ = type;
+    max_effective_connection_type_ = type;
+    return;
+  }
+
   min_effective_connection_type_ =
       std::min(min_effective_connection_type_, effective_connection_type_);
   max_effective_connection_type_ =
@@ -457,6 +466,9 @@ void NetworkMetricsProvider::OnEffectiveConnectionTypeChanged(
   DCHECK_EQ(
       min_effective_connection_type_ == net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN,
       max_effective_connection_type_ == net::EFFECTIVE_CONNECTION_TYPE_UNKNOWN);
+  DCHECK_EQ(
+      min_effective_connection_type_ == net::EFFECTIVE_CONNECTION_TYPE_OFFLINE,
+      max_effective_connection_type_ == net::EFFECTIVE_CONNECTION_TYPE_OFFLINE);
 }
 
 }  // namespace metrics
