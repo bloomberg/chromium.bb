a1b7808c2721fc03189d30262f847f8b723f616c
Dave Tapuska <dtapuska@chromium.org>
2017-08-09 04:12:39 +0000

    Make pen inputs behave like touch inputs
    We should send touch events and pointer events instead of mouse events
    when we receive WM_POINTER message from pen input devices. The touch
    events are sent when the pen presses the tablet or screen, otherwise
    send the pointer events when pen is hovering in the range of the tablet.
    
    This breaks the text selection,
    https://bugs.chromium.org/p/chromium/issues/detail?id=749119
    
    This CL was originally written as
    https://chromium-review.googlesource.com/c/575720
    
    Bug: 746581
    Change-Id: If4d4dd3a8e83ac7b2200e48cfd38034edaf0ba71
    Reviewed-on: https://chromium-review.googlesource.com/606947
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492820}
    Former-commit-id: 3704d0e52397cbe9f22c4bc56400a85b4c8873b5

--- ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_window_tree_host_win.cc
@@ -843,6 +843,11 @@ bool DesktopWindowTreeHostWin::HandleMouseEvent(const ui::MouseEvent& event) {
   return event.handled();
 }
 
+bool DesktopWindowTreeHostWin::HandlePointerEvent(ui::PointerEvent* event) {
+  SendEventToSink(event);
+  return event->handled();
+}
+
 void DesktopWindowTreeHostWin::HandleKeyEvent(ui::KeyEvent* event) {
   SendEventToSink(event);
 }
