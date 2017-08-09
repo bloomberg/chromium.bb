eca29bac8d3158e45350b5817746f49a6e2929e6
Giovanni Ortuño Urquidi <ortuno@chromium.org>
2017-08-09 04:08:05 +0000

    desktop-pwas: Only open app for link navigations.
    Only PAGE_TRANSITION_LINK will open a new App window. The remaining
    types of PageTransitions[1] won't.
    
    [1] https://cs.chromium.org/chromium/src/ui/base/page_transition_types.h
    
    Bug: 740783, 753240, 753239
    Change-Id: I4230137cdbaf9b0f8b909c5d8cf8457e03884b07
    Reviewed-on: https://chromium-review.googlesource.com/593447
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Reviewed-by: Ben Wells <benwells@chromium.org>
    Commit-Queue: Giovanni Ortuño Urquidi <ortuno@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#492819}
    Former-commit-id: fa8f9f6c31328ec15f0a34cff57e3dbbfc7f2394

--- chrome/browser/apps/app_url_redirector.cc
+++ chrome/browser/apps/app_url_redirector.cc
@@ -38,6 +38,15 @@ bool ShouldOverrideNavigation(
     const Extension* app,
     content::WebContents* source,
     const navigation_interception::NavigationParams& params) {
+  DVLOG(1) << "ShouldOverrideNavigation called for: " << params.url();
+
+  ui::PageTransition transition_type = params.transition_type();
+  if (!(PageTransitionCoreTypeIs(transition_type, ui::PAGE_TRANSITION_LINK))) {
+    DVLOG(1) << "Don't override: Transition type is "
+             << PageTransitionGetCoreTransitionString(transition_type);
+    return false;
+  }
+
   Browser* browser = chrome::FindBrowserWithWebContents(source);
   if (browser == nullptr) {
     DVLOG(1) << "Don't override: No browser, can't know if already in app.";
