5e3a84eb292b5db3a14cd90b77e84c53a933172b
Siddhartha <ssid@chromium.org>
2017-08-10 05:31:28 +0000

    Make storage of values in DOMStorageMap optional
    The DOMStorageMap takes a bool |has_only_keys| and stores only keys and
    sizes instead of the actual values. This will be used in next CLs when
    browser process does not have to store the values.
    No functional change, just adding support.
    
    BUG=748218
    
    Change-Id: I23c78e5320caf9684c8e7372e5532f67863262c3
    Reviewed-on: https://chromium-review.googlesource.com/589818
    Commit-Queue: Siddhartha S <ssid@chromium.org>
    Reviewed-by: Michael Nordman <michaeln@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493276}
    Former-commit-id: 41acb7c6d5661509b65a90a2a48f93166c0efa9d

--- content/common/dom_storage/dom_storage_map.cc
+++ content/common/dom_storage/dom_storage_map.cc
@@ -10,57 +10,157 @@ namespace content {
 
 namespace {
 
-size_t size_of_item(const base::string16& key, const base::string16& value) {
-  return (key.length() + value.length()) * sizeof(base::char16);
+size_t size_of_item(const base::string16& key, const size_t value) {
+  return key.length() * sizeof(base::char16) + value;
+}
+
+size_t size_of_item(const base::string16& key,
+                    const base::NullableString16& value) {
+  // Null value indicates deletion. So, key size is not counted.
+  return value.is_null()
+             ? 0
+             : (key.length() + value.string().length()) * sizeof(base::char16);
 }
 
 }  // namespace
 
-DOMStorageMap::DOMStorageMap(size_t quota)
-    : bytes_used_(0),
-      quota_(quota) {
+DOMStorageMap::DOMStorageMap(size_t quota) : DOMStorageMap(quota, false) {}
+
+DOMStorageMap::DOMStorageMap(size_t quota, bool has_only_keys)
+    : bytes_used_(0), quota_(quota), has_only_keys_(has_only_keys) {
   ResetKeyIterator();
 }
 
 DOMStorageMap::~DOMStorageMap() {}
 
 unsigned DOMStorageMap::Length() const {
-  return values_.size();
+  return has_only_keys_ ? keys_only_.size() : keys_values_.size();
 }
 
 base::NullableString16 DOMStorageMap::Key(unsigned index) {
-  if (index >= values_.size())
+  if (index >= Length())
     return base::NullableString16();
   while (last_key_index_ != index) {
     if (last_key_index_ > index) {
-      --key_iterator_;
+      if (has_only_keys_)
+        --keys_only_iterator_;
+      else
+        --keys_values_iterator_;
       --last_key_index_;
     } else {
-      ++key_iterator_;
+      if (has_only_keys_)
+        ++keys_only_iterator_;
+      else
+        ++keys_values_iterator_;
       ++last_key_index_;
     }
   }
-  return base::NullableString16(key_iterator_->first, false);
-}
-
-base::NullableString16 DOMStorageMap::GetItem(const base::string16& key) const {
-  DOMStorageValuesMap::const_iterator found = values_.find(key);
-  if (found == values_.end())
-    return base::NullableString16();
-  return found->second;
+  return base::NullableString16(has_only_keys_ ? keys_only_iterator_->first
+                                               : keys_values_iterator_->first,
+                                false);
 }
 
 bool DOMStorageMap::SetItem(
     const base::string16& key, const base::string16& value,
     base::NullableString16* old_value) {
-  DOMStorageValuesMap::const_iterator found = values_.find(key);
-  if (found == values_.end())
+  if (has_only_keys_) {
+    size_t unused;
+    size_t value_size = value.length() * sizeof(base::char16);
+    return SetItemInternal<KeysMap>(&keys_only_, key, value_size, &unused);
+  } else {
+    base::NullableString16 new_value(value, false);
     *old_value = base::NullableString16();
-  else
-    *old_value = found->second;
+    return SetItemInternal<DOMStorageValuesMap>(&keys_values_, key, new_value,
+                                                old_value);
+  }
+}
 
-  size_t old_item_size = old_value->is_null() ?
-      0 : size_of_item(key, old_value->string());
+bool DOMStorageMap::RemoveItem(
+    const base::string16& key,
+    base::string16* old_value) {
+  if (has_only_keys_) {
+    size_t unused;
+    return RemoveItemInternal<KeysMap>(&keys_only_, key, &unused);
+  } else {
+    base::NullableString16 nullable_old;
+    bool success = RemoveItemInternal<DOMStorageValuesMap>(&keys_values_, key,
+                                                           &nullable_old);
+    if (success)
+      *old_value = nullable_old.string();
+    return success;
+  }
+}
+
+base::NullableString16 DOMStorageMap::GetItem(const base::string16& key) const {
+  DCHECK(!has_only_keys_);
+  DOMStorageValuesMap::const_iterator found = keys_values_.find(key);
+  if (found == keys_values_.end())
+    return base::NullableString16();
+  return found->second;
+}
+
+void DOMStorageMap::ExtractValues(DOMStorageValuesMap* map) const {
+  DCHECK(!has_only_keys_);
+  *map = keys_values_;
+}
+
+void DOMStorageMap::SwapValues(DOMStorageValuesMap* values) {
+  // Note: A pre-existing file may be over the quota budget.
+  DCHECK(!has_only_keys_);
+  keys_values_.swap(*values);
+  bytes_used_ = CountBytes(keys_values_);
+  ResetKeyIterator();
+}
+
+void DOMStorageMap::TakeKeysFrom(const DOMStorageValuesMap* values) {
+  // Note: A pre-existing file may be over the quota budget.
+  DCHECK(has_only_keys_);
+  keys_only_.clear();
+  for (const auto& item : *values) {
+    keys_only_[item.first] =
+        item.second.string().length() * sizeof(base::char16);
+  }
+  bytes_used_ = CountBytes(*values);
+  ResetKeyIterator();
+}
+
+DOMStorageMap* DOMStorageMap::DeepCopy() const {
+  DOMStorageMap* copy = new DOMStorageMap(quota_, has_only_keys_);
+  copy->keys_values_ = keys_values_;
+  copy->keys_only_ = keys_only_;
+  copy->bytes_used_ = bytes_used_;
+  copy->ResetKeyIterator();
+  return copy;
+}
+
+void DOMStorageMap::ResetKeyIterator() {
+  keys_only_iterator_ = keys_only_.begin();
+  keys_values_iterator_ = keys_values_.begin();
+  last_key_index_ = 0;
+}
+
+// static
+size_t DOMStorageMap::CountBytes(const DOMStorageValuesMap& values) {
+  if (values.empty())
+    return 0;
+
+  size_t count = 0;
+  for (const auto& pair : values)
+    count += size_of_item(pair.first, pair.second);
+  return count;
+}
+
+template <typename MapType>
+bool DOMStorageMap::SetItemInternal(MapType* map_type,
+                                    const base::string16& key,
+                                    typename MapType::mapped_type value,
+                                    typename MapType::mapped_type* old_value) {
+  const auto found = map_type->find(key);
+  size_t old_item_size = 0;
+  if (found != map_type->end()) {
+    *old_value = found->second;
+    old_item_size = size_of_item(key, *old_value);
+  }
   size_t new_item_size = size_of_item(key, value);
   size_t new_bytes_used = bytes_used_ - old_item_size + new_item_size;
 
@@ -69,53 +169,25 @@ bool DOMStorageMap::SetItem(
   if (new_item_size > old_item_size && new_bytes_used > quota_)
     return false;
 
-  values_[key] = base::NullableString16(value, false);
+  (*map_type)[key] = value;
   ResetKeyIterator();
   bytes_used_ = new_bytes_used;
   return true;
 }
 
-bool DOMStorageMap::RemoveItem(
+template <typename MapType>
+bool DOMStorageMap::RemoveItemInternal(
+    MapType* map_type,
     const base::string16& key,
-    base::string16* old_value) {
-  DOMStorageValuesMap::iterator found = values_.find(key);
-  if (found == values_.end())
+    typename MapType::mapped_type* old_value) {
+  const auto found = map_type->find(key);
+  if (found == map_type->end())
     return false;
-  *old_value = found->second.string();
-  values_.erase(found);
+  *old_value = found->second;
+  map_type->erase(found);
   ResetKeyIterator();
   bytes_used_ -= size_of_item(key, *old_value);
   return true;
-}
-
-void DOMStorageMap::SwapValues(DOMStorageValuesMap* values) {
-  // Note: A pre-existing file may be over the quota budget.
-  values_.swap(*values);
-  bytes_used_ = CountBytes(values_);
-  ResetKeyIterator();
-}
-
-DOMStorageMap* DOMStorageMap::DeepCopy() const {
-  DOMStorageMap* copy = new DOMStorageMap(quota_);
-  copy->values_ = values_;
-  copy->bytes_used_ = bytes_used_;
-  copy->ResetKeyIterator();
-  return copy;
-}
-
-void DOMStorageMap::ResetKeyIterator() {
-  key_iterator_ = values_.begin();
-  last_key_index_ = 0;
-}
-
-size_t DOMStorageMap::CountBytes(const DOMStorageValuesMap& values) {
-  if (values.empty())
-    return 0;
-
-  size_t count = 0;
-  for (const auto& pair : values)
-    count += size_of_item(pair.first, pair.second.string());
-  return count;
 }
 
 }  // namespace content
--- content/common/dom_storage/dom_storage_map.h
+++ content/common/dom_storage/dom_storage_map.h
@@ -9,6 +9,7 @@
 
 #include <map>
 
+#include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/strings/nullable_string16.h"
 #include "base/strings/string16.h"
@@ -23,44 +24,78 @@ namespace content {
 class CONTENT_EXPORT DOMStorageMap
     : public base::RefCountedThreadSafe<DOMStorageMap> {
  public:
+  DOMStorageMap(size_t quota, bool only_keys);
   explicit DOMStorageMap(size_t quota);
 
   unsigned Length() const;
   base::NullableString16 Key(unsigned index);
-  base::NullableString16 GetItem(const base::string16& key) const;
+
+  // Sets and removes items from the storage. |old_value| is only valid if
+  // |has_only_keys| is false. |old_value| is not accessed or modified when
+  // |has_only_keys|.
   bool SetItem(const base::string16& key, const base::string16& value,
                base::NullableString16* old_value);
   bool RemoveItem(const base::string16& key, base::string16* old_value);
 
-  // Swaps this instances values_ with |map|.
+  // Returns value for the given |key|. Use only when |has_only_keys| is false.
+  base::NullableString16 GetItem(const base::string16& key) const;
+
+  // Writes a copy of the current set of map_ to the |map|. Use only when
+  // |has_only_keys| is false.
+  void ExtractValues(DOMStorageValuesMap* map) const;
+
+  // Swaps the values from |map| to |keys_values_| in this instances,
   // Note: to grandfather in pre-existing files that are overbudget,
-  // this method does not do quota checking.
+  // this method does not do quota checking. Use only when |has_only_keys| is
+  // false.
   void SwapValues(DOMStorageValuesMap* map);
 
-  // Writes a copy of the current set of values_ to the |map|.
-  void ExtractValues(DOMStorageValuesMap* map) const { *map = values_; }
+  // Stores the keys and sizes of values from |map| to |keys_only_|. Use only
+  // when |has_only_keys| is true. This method does not do quota checking.
+  void TakeKeysFrom(const DOMStorageValuesMap* map);
 
   // Creates a new instance of DOMStorageMap containing
-  // a deep copy of values_.
+  // a deep copy of the map.
   DOMStorageMap* DeepCopy() const;
 
   size_t bytes_used() const { return bytes_used_; }
   size_t quota() const { return quota_; }
   void set_quota(size_t quota) { quota_ = quota; }
+  bool has_only_keys() const { return has_only_keys_; }
 
   static size_t CountBytes(const DOMStorageValuesMap& values);
 
  private:
   friend class base::RefCountedThreadSafe<DOMStorageMap>;
+  FRIEND_TEST_ALL_PREFIXES(DOMStorageMapParamTest, EnforcesQuota);
+
+  using KeysMap = std::map<base::string16, size_t>;
+
   ~DOMStorageMap();
+
+  template <typename MapType>
+  bool SetItemInternal(MapType* map_type,
+                       const base::string16& key,
+                       typename MapType::mapped_type value,
+                       typename MapType::mapped_type* old_value);
+  template <typename MapType>
+  bool RemoveItemInternal(MapType* map_type,
+                          const base::string16& key,
+                          typename MapType::mapped_type* old_value);
 
   void ResetKeyIterator();
 
-  DOMStorageValuesMap values_;
-  DOMStorageValuesMap::const_iterator key_iterator_;
+  // Used when |has_only_keys_| is false.
+  DOMStorageValuesMap keys_values_;
+  // Used when |has_only_keys_| is true.
+  KeysMap keys_only_;
+
+  DOMStorageValuesMap::const_iterator keys_values_iterator_;
+  KeysMap::const_iterator keys_only_iterator_;
   unsigned last_key_index_;
   size_t bytes_used_;
   size_t quota_;
+  const bool has_only_keys_;
 };
 
 }  // namespace content
