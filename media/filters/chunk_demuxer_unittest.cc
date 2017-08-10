bloomberg/chromium.bb

commit f59ff74c5056ca546867b44fb765b90587790b10

fix canplaythrough
This makes canplaythrough follow the spec.
It fires when we estimate that we have enough data to play to the
end, or when we stop downloading because the buffer is full.

We do this by keeping a time series in BufferedDataSourceHostImpl
which is used to estimate the download speed over the last 10 seconds.

BUG=73609

Review-Url: https://codereview.chromium.org/2796193002
Cr-Commit-Position: refs/heads/master@{#469797}

Former-commit-id: b2d3efd2ec18ffd52c3dacfece9862366b565a3d

media/blink/webmediaplayer_impl.cc
@@ -219,6 +219,10 @@ WebMediaPlayerImpl::WebMediaPlayerImpl(
       last_reported_memory_usage_(0),
       supports_save_(true),
       chunk_demuxer_(NULL),
+      tick_clock_(new base::DefaultTickClock()),
+      buffered_data_source_host_(
+          base::Bind(&WebMediaPlayerImpl::OnProgress, AsWeakPtr()),
+          tick_clock_.get()),
       url_index_(url_index),
       // Threaded compositing isn't enabled universally yet.
       compositor_task_runner_(params->compositor_task_runner()
@@ -251,8 +255,6 @@ WebMediaPlayerImpl::WebMediaPlayerImpl(
   DCHECK(renderer_factory_selector_);
   DCHECK(client_);
   DCHECK(delegate_);
-
-  tick_clock_.reset(new base::DefaultTickClock());
 
   force_video_overlays_ = base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kForceVideoOverlays);
@@ -855,24 +857,7 @@ bool WebMediaPlayerImpl::DidLoadingProgress() {
   // Note: Separate variables used to ensure both methods are called every time.
   const bool pipeline_progress = pipeline_controller_.DidLoadingProgress();
   const bool data_progress = buffered_data_source_host_.DidLoadingProgress();
-  const bool did_loading_progress = pipeline_progress || data_progress;
-
-  if (did_loading_progress &&
-      highest_ready_state_ < ReadyState::kReadyStateHaveFutureData) {
-    // Reset the preroll attempt clock.
-    preroll_attempt_pending_ = true;
-    preroll_attempt_start_time_ = base::TimeTicks();
-
-    // Clear any 'stale' flag and give the pipeline a chance to resume. If we
-    // are already resumed, this will cause |preroll_attempt_start_time_| to be
-    // set.
-    // TODO(sandersd): Should this be on the same stack? It might be surprising
-    // that didLoadingProgress() can synchronously change state.
-    delegate_->ClearStaleFlag(delegate_id_);
-    UpdatePlayState();
-  }
-
-  return did_loading_progress;
+  return pipeline_progress || data_progress;
 }
 
 void WebMediaPlayerImpl::Paint(blink::WebCanvas* canvas,
@@ -1296,6 +1281,41 @@ void WebMediaPlayerImpl::OnMetadata(PipelineMetadata metadata) {
   UpdatePlayState();
 }
 
+void WebMediaPlayerImpl::OnProgress() {
+  DVLOG(1) << __func__;
+  if (highest_ready_state_ < ReadyState::kReadyStateHaveFutureData) {
+    // Reset the preroll attempt clock.
+    preroll_attempt_pending_ = true;
+    preroll_attempt_start_time_ = base::TimeTicks();
+
+    // Clear any 'stale' flag and give the pipeline a chance to resume. If we
+    // are already resumed, this will cause |preroll_attempt_start_time_| to
+    // be set.
+    delegate_->ClearStaleFlag(delegate_id_);
+    UpdatePlayState();
+  } else if (ready_state_ == ReadyState::kReadyStateHaveFutureData &&
+             CanPlayThrough()) {
+    SetReadyState(WebMediaPlayer::kReadyStateHaveEnoughData);
+  }
+}
+
+bool WebMediaPlayerImpl::CanPlayThrough() {
+  if (!base::FeatureList::IsEnabled(kSpecCompliantCanPlayThrough))
+    return true;
+  if (chunk_demuxer_)
+    return true;
+  if (data_source_ && data_source_->assume_fully_buffered())
+    return true;
+  // If we're not currently downloading, we have as much buffer as
+  // we're ever going to get, which means we say we can play through.
+  if (network_state_ == WebMediaPlayer::kNetworkStateIdle)
+    return true;
+  return buffered_data_source_host_.CanPlayThrough(
+      base::TimeDelta::FromSecondsD(CurrentTime()),
+      base::TimeDelta::FromSecondsD(Duration()),
+      playback_rate_ == 0.0 ? 1.0 : playback_rate_);
+}
+
 void WebMediaPlayerImpl::OnBufferingStateChange(BufferingState state) {
   DVLOG(1) << __func__ << "(" << state << ")";
   DCHECK(main_task_runner_->BelongsToCurrentThread());
@@ -1315,9 +1335,8 @@ void WebMediaPlayerImpl::OnBufferingStateChange(BufferingState state) {
       RecordUnderflowDuration(base::TimeDelta());
     }
 
-    // TODO(chcunningham): Monitor playback position vs buffered. Potentially
-    // transition to HAVE_FUTURE_DATA here if not enough is buffered.
-    SetReadyState(WebMediaPlayer::kReadyStateHaveEnoughData);
+    SetReadyState(CanPlayThrough() ? WebMediaPlayer::kReadyStateHaveEnoughData
+                                   : WebMediaPlayer::kReadyStateHaveFutureData);
 
     // Let the DataSource know we have enough data. It may use this information
     // to release unused network connections.
@@ -1650,12 +1669,14 @@ void WebMediaPlayerImpl::DataSourceInitialized(bool success) {
 }
 
 void WebMediaPlayerImpl::NotifyDownloading(bool is_downloading) {
-  DVLOG(1) << __func__;
+  DVLOG(1) << __func__ << "(" << is_downloading << ")";
   if (!is_downloading && network_state_ == WebMediaPlayer::kNetworkStateLoading)
     SetNetworkState(WebMediaPlayer::kNetworkStateIdle);
   else if (is_downloading &&
            network_state_ == WebMediaPlayer::kNetworkStateIdle)
     SetNetworkState(WebMediaPlayer::kNetworkStateLoading);
+  if (ready_state_ == ReadyState::kReadyStateHaveFutureData && !is_downloading)
+    SetReadyState(WebMediaPlayer::kReadyStateHaveEnoughData);
   media_log_->AddEvent(
       media_log_->CreateBooleanEvent(MediaLogEvent::NETWORK_ACTIVITY_SET,
                                      "is_downloading_data", is_downloading));
@@ -2400,5 +2421,10 @@ void WebMediaPlayerImpl::RecordVideoNaturalSize(const gfx::Size& natural_size) {
 }
 
 #undef UMA_HISTOGRAM_VIDEO_HEIGHT
+
+void WebMediaPlayerImpl::SetTickClockForTest(base::TickClock* tick_clock) {
+  tick_clock_.reset(tick_clock);
+  buffered_data_source_host_.SetTickClockForTest(tick_clock);
+}
 
 }  // namespace media

media/blink/webmediaplayer_impl.h
@@ -457,8 +457,19 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
   // handling a src= or MSE based playback.
   void RecordUnderflowDuration(base::TimeDelta duration);
 
+  // Called by the data source when loading progresses.
+  // Can be called quite often.
+  void OnProgress();
+
+  // Returns true when we estimate that we can play the rest of the media
+  // without buffering.
+  bool CanPlayThrough();
+
   // Records |natural_size| to MediaLog and video height to UMA.
   void RecordVideoNaturalSize(const gfx::Size& natural_size);
+
+  // Takes ownership of |tick_clock|
+  void SetTickClockForTest(base::TickClock* tick_clock);
 
   blink::WebLocalFrame* frame_;
 
@@ -584,6 +595,8 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
 
   std::unique_ptr<base::MemoryPressureListener> memory_pressure_listener_;
 
+  std::unique_ptr<base::TickClock> tick_clock_;
+
   BufferedDataSourceHostImpl buffered_data_source_host_;
   linked_ptr<UrlIndex> url_index_;
 
@@ -684,8 +697,6 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
   // when a preroll attempt began.
   bool preroll_attempt_pending_;
   base::TimeTicks preroll_attempt_start_time_;
-
-  std::unique_ptr<base::TickClock> tick_clock_;
 
   // Monitors the player events.
   base::WeakPtr<MediaObserver> observer_;

media/base/media_switches.cc
@@ -200,6 +200,10 @@ const base::Feature kMemoryPressureBasedSourceBufferGC{
 const base::Feature kNewRemotePlaybackPipeline{
     "NewRemotePlaybackPipeline", base::FEATURE_DISABLED_BY_DEFAULT};
 
+// CanPlayThrough issued according to standard.
+const base::Feature kSpecCompliantCanPlayThrough{
+    "CanPlayThrough", base::FEATURE_DISABLED_BY_DEFAULT};
+
 // Use shared block-based buffering for media.
 const base::Feature kUseNewMediaCache{"use-new-media-cache",
                                       base::FEATURE_ENABLED_BY_DEFAULT};

media/base/media_switches.h
@@ -95,17 +95,18 @@ namespace media {
 // alongside the definition of their values in the .cc file.
 
 MEDIA_EXPORT extern const base::Feature kAutoplayPolicy;
+MEDIA_EXPORT extern const base::Feature kBackgroundVideoPauseOptimization;
+MEDIA_EXPORT extern const base::Feature kBackgroundVideoTrackOptimization;
+MEDIA_EXPORT extern const base::Feature kExternalClearKeyForTesting;
+MEDIA_EXPORT extern const base::Feature kMemoryPressureBasedSourceBufferGC;
 MEDIA_EXPORT extern const base::Feature kNewAudioRenderingMixingStrategy;
+MEDIA_EXPORT extern const base::Feature kNewRemotePlaybackPipeline;
 MEDIA_EXPORT extern const base::Feature kOverlayFullscreenVideo;
 MEDIA_EXPORT extern const base::Feature kResumeBackgroundVideo;
+MEDIA_EXPORT extern const base::Feature kSpecCompliantCanPlayThrough;
 MEDIA_EXPORT extern const base::Feature kUseNewMediaCache;
-MEDIA_EXPORT extern const base::Feature kVideoColorManagement;
 MEDIA_EXPORT extern const base::Feature kVideoBlitColorAccuracy;
-MEDIA_EXPORT extern const base::Feature kExternalClearKeyForTesting;
-MEDIA_EXPORT extern const base::Feature kBackgroundVideoTrackOptimization;
-MEDIA_EXPORT extern const base::Feature kBackgroundVideoPauseOptimization;
-MEDIA_EXPORT extern const base::Feature kMemoryPressureBasedSourceBufferGC;
-MEDIA_EXPORT extern const base::Feature kNewRemotePlaybackPipeline;
+MEDIA_EXPORT extern const base::Feature kVideoColorManagement;
 
 #if defined(OS_ANDROID)
 MEDIA_EXPORT extern const base::Feature kAndroidMediaPlayerRenderer;

media/blink/buffered_data_source_host_impl.cc
@@ -8,9 +8,28 @@
 
 namespace media {
 
-BufferedDataSourceHostImpl::BufferedDataSourceHostImpl()
+// We want a relatively small window for estimating bandwidth,
+// that way we don't need to worry too much about seeks and pause
+// throwing off the estimates.
+constexpr base::TimeDelta kDownloadHistoryWindowSeconds =
+    base::TimeDelta::FromSecondsD(10.0);
+
+// Limit the number of entries in the rate estimator queue.
+// 1024 entries should be more than enough.
+constexpr size_t kDownloadHistoryMaxEntries = 1024;
+
+// Just in case someone gives progress one byte at a time,
+// let's aggregate progress updates together until we reach
+// at least this many bytes.
+constexpr int64_t kDownloadHistoryMinBytesPerEntry = 1000;
+
+BufferedDataSourceHostImpl::BufferedDataSourceHostImpl(
+    base::RepeatingClosure progress_cb,
+    base::TickClock* tick_clock)
     : total_bytes_(0),
-      did_loading_progress_(false) { }
+      did_loading_progress_(false),
+      progress_cb_(std::move(progress_cb)),
+      tick_clock_(tick_clock) {}
 
 BufferedDataSourceHostImpl::~BufferedDataSourceHostImpl() { }
 
@@ -18,15 +37,56 @@ void BufferedDataSourceHostImpl::SetTotalBytes(int64_t total_bytes) {
   total_bytes_ = total_bytes;
 }
 
+int64_t BufferedDataSourceHostImpl::UnloadedBytesInInterval(
+    const Interval<int64_t>& interval) const {
+  int64_t bytes = 0;
+  auto i = buffered_byte_ranges_.find(interval.begin);
+  while (i != buffered_byte_ranges_.end()) {
+    if (i.interval_begin() >= interval.end)
+      break;
+    if (!i.value()) {
+      Interval<int64_t> intersection = i.interval().Intersect(interval);
+      if (!intersection.Empty())
+        bytes += intersection.end - intersection.begin;
+    }
+    ++i;
+  }
+  return bytes;
+}
+
 void BufferedDataSourceHostImpl::AddBufferedByteRange(int64_t start,
                                                       int64_t end) {
-  const auto i = buffered_byte_ranges_.find(start);
-  if (i.value() && i.interval_end() >= end) {
+  int64_t new_bytes = UnloadedBytesInInterval(Interval<int64_t>(start, end));
+  if (new_bytes == 0) {
     // No change
     return;
   }
   buffered_byte_ranges_.SetInterval(start, end, 1);
   did_loading_progress_ = true;
+
+  base::TimeTicks now = tick_clock_->NowTicks();
+  int64_t bytes_so_far = 0;
+  if (!download_history_.empty())
+    bytes_so_far = download_history_.back().second;
+  bytes_so_far += new_bytes;
+
+  // If the difference between the last entry and the second to last entry is
+  // less than kDownloadHistoryMinBytesPerEntry, just overwrite the last entry.
+  if (download_history_.size() > 1 &&
+      download_history_.back().second - (download_history_.end() - 2)->second <
+          kDownloadHistoryMinBytesPerEntry) {
+    download_history_.back() = std::make_pair(now, bytes_so_far);
+  } else {
+    download_history_.emplace_back(now, bytes_so_far);
+  }
+  DCHECK_GE(download_history_.size(), 1u);
+  // Drop entries that are too old.
+  while (download_history_.size() > kDownloadHistoryMaxEntries ||
+         download_history_.back().first - download_history_.front().first >
+             kDownloadHistoryWindowSeconds) {
+    download_history_.pop_front();
+  }
+  progress_cb_.Run();
 }
 
 static base::TimeDelta TimeForByteOffset(int64_t byte_offset,
@@ -64,6 +124,67 @@ bool BufferedDataSourceHostImpl::DidLoadingProgress() {
   bool ret = did_loading_progress_;
   did_loading_progress_ = false;
   return ret;
+}
+
+double BufferedDataSourceHostImpl::DownloadRate() const {
+  // If the download history is really small, any estimate we make is going to
+  // be wildly inaccurate, so let's not make any estimates until we have more
+  // data.
+  if (download_history_.size() < 5)
+    return 0.0;
+
+  // The data we get is bursty, so we get multiple measuring points very close
+  // together. These bursts will often lead us to over-estimate the download
+  // rate. By iterating over the beginning of the time series and picking the
+  // data point that has the lowest download rate, we avoid over-estimating.
+  const double kVeryLargeRate = 1.0E20;
+  double download_rate = kVeryLargeRate;
+  for (int i = 0; i < std::min<int>(20, download_history_.size() - 3); i++) {
+    int64_t downloaded_bytes =
+        download_history_.back().second - download_history_[i].second;
+    base::TimeTicks now = tick_clock_->NowTicks();
+    base::TimeDelta downloaded_seconds = now - download_history_[i].first;
+    if (downloaded_seconds <= base::TimeDelta())
+      continue;
+    download_rate = std::min(
+        download_rate, downloaded_bytes / downloaded_seconds.InSecondsF());
+  }
+  return download_rate == kVeryLargeRate ? 0.0 : download_rate;
+}
+
+bool BufferedDataSourceHostImpl::CanPlayThrough(
+    base::TimeDelta current_position,
+    base::TimeDelta media_duration,
+    double playback_rate) const {
+  DCHECK_GE(playback_rate, 0);
+  if (!total_bytes_ || media_duration <= base::TimeDelta() ||
+      media_duration == kInfiniteDuration) {
+    return false;
+  }
+  if (current_position > media_duration)
+    return true;
+  double fraction = current_position.InSecondsF() / media_duration.InSecondsF();
+  int64_t byte_pos = total_bytes_ * fraction;
+  if (byte_pos < 0)
+    byte_pos = 0;
+
+  int64_t unloaded_bytes =
+      UnloadedBytesInInterval(Interval<int64_t>(byte_pos, total_bytes_));
+
+  if (unloaded_bytes == 0)
+    return true;
+
+  double download_rate = DownloadRate();
+  if (download_rate <= 0.0)
+    return false;
+
+  return unloaded_bytes / download_rate <
+         (media_duration - current_position).InSecondsF() / playback_rate;
+}
+
+void BufferedDataSourceHostImpl::SetTickClockForTest(
+    base::TickClock* tick_clock) {
+  tick_clock_ = tick_clock;
 }
 
 }  // namespace media

media/blink/buffered_data_source_host_impl.h
@@ -6,8 +6,12 @@
 #define MEDIA_BLINK_BUFFERED_DATA_SOURCE_HOST_IMPL_H_
 
 #include <stdint.h>
+#include <deque>
 
+#include "base/callback.h"
+#include "base/gtest_prod_util.h"
 #include "base/macros.h"
+#include "base/time/tick_clock.h"
 #include "base/time/time.h"
 #include "media/base/ranges.h"
 #include "media/blink/interval_map.h"
@@ -35,7 +39,8 @@ class MEDIA_BLINK_EXPORT BufferedDataSourceHost {
 class MEDIA_BLINK_EXPORT BufferedDataSourceHostImpl
     : public BufferedDataSourceHost {
  public:
-  BufferedDataSourceHostImpl();
+  BufferedDataSourceHostImpl(base::Closure progress_cb,
+                             base::TickClock* tick_clock);
   ~BufferedDataSourceHostImpl() override;
 
   // BufferedDataSourceHost implementation.
@@ -50,7 +55,23 @@ class MEDIA_BLINK_EXPORT BufferedDataSourceHostImpl
 
   bool DidLoadingProgress();
 
+  // Returns true if we have enough buffered bytes to play from now
+  // until the end of media
+  bool CanPlayThrough(base::TimeDelta current_position,
+                      base::TimeDelta media_duration,
+                      double playback_rate) const;
+
+  // Caller must make sure |tick_clock| is valid for lifetime of this object.
+  void SetTickClockForTest(base::TickClock* tick_clock);
+
  private:
+  // Returns number of bytes not yet loaded in the given interval.
+  int64_t UnloadedBytesInInterval(const Interval<int64_t>& interval) const;
+
+  // Returns an estimate of the download rate.
+  // Returns 0.0 if an estimate cannot be made.
+  double DownloadRate() const;
+
   // Total size of the data source.
   int64_t total_bytes_;
 
@@ -62,6 +83,16 @@ class MEDIA_BLINK_EXPORT BufferedDataSourceHostImpl
   // DidLoadingProgress().
   bool did_loading_progress_;
 
+  // Contains how much we had downloaded at a given time.
+  // Pruned to contain roughly the last 10 seconds of data.
+  std::deque<std::pair<base::TimeTicks, uint64_t>> download_history_;
+  base::Closure progress_cb_;
+
+  base::TickClock* tick_clock_;
+
+  FRIEND_TEST_ALL_PREFIXES(BufferedDataSourceHostImplTest, CanPlayThrough);
+  FRIEND_TEST_ALL_PREFIXES(BufferedDataSourceHostImplTest,
+                           CanPlayThroughSmallAdvances);
   DISALLOW_COPY_AND_ASSIGN(BufferedDataSourceHostImpl);
 };
 

media/blink/buffered_data_source_host_impl_unittest.cc
@@ -2,23 +2,33 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/macros.h"
 #include "media/blink/buffered_data_source_host_impl.h"
+
+#include "base/bind.h"
+#include "base/macros.h"
+#include "base/test/simple_test_tick_clock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace media {
 
 class BufferedDataSourceHostImplTest : public testing::Test {
  public:
-  BufferedDataSourceHostImplTest() {}
+  BufferedDataSourceHostImplTest()
+      : host_(base::Bind(&BufferedDataSourceHostImplTest::ProgressCallback,
+                         base::Unretained(this)),
+              &clock_) {}
 
   void Add() {
     host_.AddBufferedTimeRanges(&ranges_, base::TimeDelta::FromSeconds(10));
   }
 
+  void ProgressCallback() { progress_callback_calls_++; }
+
  protected:
+  int progress_callback_calls_ = 0;
   BufferedDataSourceHostImpl host_;
   Ranges<base::TimeDelta> ranges_;
+  base::SimpleTestTickClock clock_;
 
   DISALLOW_COPY_AND_ASSIGN(BufferedDataSourceHostImplTest);
 };
@@ -71,6 +81,74 @@ TEST_F(BufferedDataSourceHostImplTest, DidLoadingProgress) {
   host_.AddBufferedByteRange(10, 20);
   EXPECT_TRUE(host_.DidLoadingProgress());
   EXPECT_FALSE(host_.DidLoadingProgress());
+}
+
+TEST_F(BufferedDataSourceHostImplTest, CanPlayThrough) {
+  host_.SetTotalBytes(100000);
+  EXPECT_EQ(100000,
+            host_.UnloadedBytesInInterval(Interval<int64_t>(0, 100000)));
+  host_.AddBufferedByteRange(0, 10000);
+  clock_.Advance(base::TimeDelta::FromSeconds(1));
+  host_.AddBufferedByteRange(10000, 20000);
+  clock_.Advance(base::TimeDelta::FromSeconds(1));
+  host_.AddBufferedByteRange(20000, 30000);
+  clock_.Advance(base::TimeDelta::FromSeconds(1));
+  host_.AddBufferedByteRange(30000, 40000);
+  clock_.Advance(base::TimeDelta::FromSeconds(1));
+  host_.AddBufferedByteRange(40000, 50000);
+  clock_.Advance(base::TimeDelta::FromSeconds(1));
+  EXPECT_EQ(50000, host_.UnloadedBytesInInterval(Interval<int64_t>(0, 100000)));
+  host_.AddBufferedByteRange(50000, 60000);
+  clock_.Advance(base::TimeDelta::FromSeconds(1));
+  host_.AddBufferedByteRange(60000, 70000);
+  clock_.Advance(base::TimeDelta::FromSeconds(1));
+  host_.AddBufferedByteRange(70000, 80000);
+  clock_.Advance(base::TimeDelta::FromSeconds(1));
+  host_.AddBufferedByteRange(80000, 90000);
+  // Download rate is allowed to be estimated low, but not high.
+  EXPECT_LE(host_.DownloadRate(), 10000.0f);
+  EXPECT_GE(host_.DownloadRate(), 9000.0f);
+  EXPECT_EQ(10000, host_.UnloadedBytesInInterval(Interval<int64_t>(0, 100000)));
+  EXPECT_EQ(9, progress_callback_calls_);
+  // If the video is 0.1s we can't play through.
+  EXPECT_FALSE(host_.CanPlayThrough(base::TimeDelta(),
+                                    base::TimeDelta::FromSecondsD(0.01), 1.0));
+  // If the video is 1000s we can play through.
+  EXPECT_TRUE(host_.CanPlayThrough(base::TimeDelta(),
+                                   base::TimeDelta::FromSecondsD(1000.0), 1.0));
+  // No more downloads for 1000 seconds...
+  clock_.Advance(base::TimeDelta::FromSeconds(1000));
+  // Can't play through..
+  EXPECT_FALSE(host_.CanPlayThrough(base::TimeDelta(),
+                                    base::TimeDelta::FromSecondsD(100.0), 1.0));
+  host_.AddBufferedByteRange(90000, 100000);
+  clock_.Advance(base::TimeDelta::FromSeconds(1));
+  EXPECT_EQ(0, host_.UnloadedBytesInInterval(Interval<int64_t>(0, 100000)));
+
+  // Media is fully downloaded, so we can certainly play through, even if
+  // we only have 0.01 seconds to do it.
+  EXPECT_TRUE(host_.CanPlayThrough(base::TimeDelta(),
+                                   base::TimeDelta::FromSecondsD(0.01), 1.0));
+}
+
+TEST_F(BufferedDataSourceHostImplTest, CanPlayThroughSmallAdvances) {
+  host_.SetTotalBytes(20000);
+  EXPECT_EQ(20000, host_.UnloadedBytesInInterval(Interval<int64_t>(0, 20000)));
+  for (int j = 1; j <= 100; j++) {
+    host_.AddBufferedByteRange(0, j * 100);
+    clock_.Advance(base::TimeDelta::FromSecondsD(0.01));
+  }
+  // Download rate is allowed to be estimated low, but not high.
+  EXPECT_LE(host_.DownloadRate(), 10000.0f);
+  EXPECT_GE(host_.DownloadRate(), 9000.0f);
+  EXPECT_EQ(10000, host_.UnloadedBytesInInterval(Interval<int64_t>(0, 20000)));
+  EXPECT_EQ(100, progress_callback_calls_);
+  // If the video is 0.1s we can't play through.
+  EXPECT_FALSE(host_.CanPlayThrough(base::TimeDelta(),
+                                    base::TimeDelta::FromSecondsD(0.01), 1.0));
+  // If the video is 1000s we can play through.
+  EXPECT_TRUE(host_.CanPlayThrough(base::TimeDelta(),
+                                   base::TimeDelta::FromSecondsD(1000.0), 1.0));
 }
 
 }  // namespace media

commit 3701347f1bddab10de5d13fdabe23039987725c9

[media] Enable ChunkDemuxer to signal OnProgress events.
This adds a callback to ChunkDemuxer to signal WMPI::OnProgress() when
data is appended, as is already done by the data source for src=.

This fixes an issue introduced by commit
f59ff74c5056ca546867b44fb765b90587790b10 [formerly b2d3efd2ec18ffd52c3dacfece9862366b565a3d], where MSE sources that do not
have enough data to complete preroll may be suspended forever.

Bug: 754043
Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
Change-Id: Ie36658a678a22ed394a8d29e2d3316df9aa8d60b
Reviewed-on: https://chromium-review.googlesource.com/611002
Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
Reviewed-by: Fredrik Hubinette <hubbe@chromium.org>
Commit-Queue: Dan Sanders <sandersd@chromium.org>
Cr-Commit-Position: refs/heads/master@{#493600}
Former-commit-id: 42311b57823ef216105dd588160bcaffd75a3f14

media/blink/webmediaplayer_impl.cc
@@ -2021,6 +2021,8 @@ void WebMediaPlayerImpl::StartPipeline() {
     chunk_demuxer_ = new ChunkDemuxer(
         BindToCurrentLoop(
             base::Bind(&WebMediaPlayerImpl::OnDemuxerOpened, AsWeakPtr())),
+        BindToCurrentLoop(
+            base::Bind(&WebMediaPlayerImpl::OnProgress, AsWeakPtr())),
         encrypted_media_init_data_cb, media_log_.get());
     demuxer_.reset(chunk_demuxer_);
 

media/blink/webmediaplayer_impl.h
@@ -484,7 +484,8 @@ class MEDIA_BLINK_EXPORT WebMediaPlayerImpl
   // handling a src= or MSE based playback.
   void RecordUnderflowDuration(base::TimeDelta duration);
 
-  // Called by the data source when loading progresses.
+  // Called by the data source (for src=) or demuxer (for mse) when loading
+  // progresses.
   // Can be called quite often.
   void OnProgress();
 

media/filters/chunk_demuxer.cc
@@ -424,12 +424,14 @@ void ChunkDemuxerStream::CompletePendingReadIfPossible_Locked() {
 
 ChunkDemuxer::ChunkDemuxer(
     const base::Closure& open_cb,
+    const base::Closure& progress_cb,
     const EncryptedMediaInitDataCB& encrypted_media_init_data_cb,
     MediaLog* media_log)
     : state_(WAITING_FOR_INIT),
       cancel_next_seek_(false),
       host_(NULL),
       open_cb_(open_cb),
+      progress_cb_(progress_cb),
       encrypted_media_init_data_cb_(encrypted_media_init_data_cb),
       enable_text_(false),
       media_log_(media_log),
@@ -884,6 +886,7 @@ bool ChunkDemuxer::AppendData(const std::string& id,
   }
 
   host_->OnBufferedTimeRangesChanged(ranges);
+  progress_cb_.Run();
   return true;
 }
 

media/filters/chunk_demuxer.h
@@ -176,11 +176,13 @@ class MEDIA_EXPORT ChunkDemuxer : public Demuxer {
   };
 
   // |open_cb| Run when Initialize() is called to signal that the demuxer
-  //   is ready to receive media data via AppenData().
+  //   is ready to receive media data via AppendData().
+  // |progress_cb| Run each time data is appended.
   // |encrypted_media_init_data_cb| Run when the demuxer determines that an
   //   encryption key is needed to decrypt the content.
   // |media_log| Used to report content and engine debug messages.
   ChunkDemuxer(const base::Closure& open_cb,
+               const base::Closure& progress_cb,
                const EncryptedMediaInitDataCB& encrypted_media_init_data_cb,
                MediaLog* media_log);
   ~ChunkDemuxer() override;
@@ -409,6 +411,7 @@ class MEDIA_EXPORT ChunkDemuxer : public Demuxer {
 
   DemuxerHost* host_;
   base::Closure open_cb_;
+  base::Closure progress_cb_;
   EncryptedMediaInitDataCB encrypted_media_init_data_cb_;
   bool enable_text_;
 

media/filters/chunk_demuxer_unittest.cc
@@ -10,6 +10,7 @@
 #include <utility>
 
 #include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/command_line.h"
 #include "base/macros.h"
 #include "base/message_loop/message_loop.h"
@@ -178,7 +179,9 @@ class ChunkDemuxerTest : public ::testing::Test {
     return GenerateCluster(46, 66, 5);
   }
 
-  ChunkDemuxerTest() : append_window_end_for_next_append_(kInfiniteDuration) {
+  ChunkDemuxerTest()
+      : did_progress_(false),
+        append_window_end_for_next_append_(kInfiniteDuration) {
     init_segment_received_cb_ = base::Bind(
         &ChunkDemuxerTest::InitSegmentReceived, base::Unretained(this));
     CreateNewDemuxer();
@@ -187,10 +190,12 @@ class ChunkDemuxerTest : public ::testing::Test {
   void CreateNewDemuxer() {
     base::Closure open_cb =
         base::Bind(&ChunkDemuxerTest::DemuxerOpened, base::Unretained(this));
+    base::Closure progress_cb =
+        base::Bind(&ChunkDemuxerTest::OnProgress, base::Unretained(this));
     Demuxer::EncryptedMediaInitDataCB encrypted_media_init_data_cb = base::Bind(
         &ChunkDemuxerTest::OnEncryptedMediaInitData, base::Unretained(this));
-    demuxer_.reset(
-        new ChunkDemuxer(open_cb, encrypted_media_init_data_cb, &media_log_));
+    demuxer_.reset(new ChunkDemuxer(open_cb, progress_cb,
+                                    encrypted_media_init_data_cb, &media_log_));
   }
 
   virtual ~ChunkDemuxerTest() {
@@ -1279,6 +1284,14 @@ class ChunkDemuxerTest : public ::testing::Test {
   MOCK_METHOD1(InitSegmentReceivedMock, void(std::unique_ptr<MediaTracks>&));
   MOCK_METHOD1(OnParseWarningMock, void(const SourceBufferParseWarning));
 
+  void OnProgress() { did_progress_ = true; }
+
+  bool DidProgress() {
+    bool result = did_progress_;
+    did_progress_ = false;
+    return result;
+  }
+
   void Seek(base::TimeDelta seek_time) {
     demuxer_->StartWaitingForSeek(seek_time);
     demuxer_->Seek(seek_time, NewExpectedStatusCB(PIPELINE_OK));
@@ -1306,6 +1319,8 @@ class ChunkDemuxerTest : public ::testing::Test {
 
   std::unique_ptr<ChunkDemuxer> demuxer_;
   Demuxer::MediaTracksUpdatedCB init_segment_received_cb_;
+
+  bool did_progress_;
 
   base::TimeDelta append_window_start_for_next_append_;
   base::TimeDelta append_window_end_for_next_append_;
@@ -2153,7 +2168,9 @@ TEST_F(ChunkDemuxerTest, AppendingInPieces) {
 
   ExpectInitMediaLogs(HAS_AUDIO | HAS_VIDEO);
   EXPECT_CALL(*this, InitSegmentReceivedMock(_));
+  EXPECT_FALSE(DidProgress());
   ASSERT_TRUE(AppendDataInPieces(buffer.get(), buffer_size));
+  EXPECT_TRUE(DidProgress());
 
   GenerateExpectedReads(0, 9);
 }

media/test/mock_media_source.cc
@@ -4,6 +4,8 @@
 
 #include "media/test/mock_media_source.h"
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "media/base/test_data_util.h"
 #include "media/base/timestamp_constants.h"
@@ -20,6 +22,7 @@ MockMediaSource::MockMediaSource(const std::string& filename,
       mimetype_(mimetype),
       chunk_demuxer_(new ChunkDemuxer(
           base::Bind(&MockMediaSource::DemuxerOpened, base::Unretained(this)),
+          base::Bind(&base::DoNothing),
           base::Bind(&MockMediaSource::OnEncryptedMediaInitData,
                      base::Unretained(this)),
           &media_log_)),