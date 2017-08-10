30c93d6ff0e5f618af7d7e5d0d32e79aab7c0bab
Ryan Landay <rlanday@chromium.org>
2017-08-10 22:51:28 +0000

    Add SuggestionMarkerReplacementScope and related behavior
    This CL adds a class SuggestionMarkerReplacementScope for controlling how
    suggestion DocumentMarkers are updated in response to edits. When a scope object
    is active, the markers are updated to properly respond to a suggestion being
    applied. When a scope object is not active, the markers are updated to properly
    respond to any other sort of edit.
    
    Bug: 672259
    Change-Id: I60aca1cafbcc93658c04c4f867196514a30fe56f
    Reviewed-on: https://chromium-review.googlesource.com/609097
    Commit-Queue: Ryan Landay <rlanday@chromium.org>
    Reviewed-by: Changwan Ryu <changwan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493578}
    Former-commit-id: bd3df550c43a23c45e2b7381a564447655739dd7

--- third_party/WebKit/Source/core/editing/markers/SuggestionMarkerListImpl.cpp
+++ third_party/WebKit/Source/core/editing/markers/SuggestionMarkerListImpl.cpp
@@ -5,8 +5,54 @@
 #include "core/editing/markers/SuggestionMarkerListImpl.h"
 
 #include "core/editing/markers/DocumentMarkerListEditor.h"
+#include "core/editing/markers/SuggestionMarker.h"
+#include "core/editing/markers/SuggestionMarkerReplacementScope.h"
 
 namespace blink {
+
+namespace {
+
+UChar32 GetCodePointAt(const String& text, size_t index) {
+  UChar32 c;
+  U16_GET(text, 0, index, text.length(), c);
+  return c;
+}
+
+Optional<DocumentMarker::MarkerOffsets>
+ComputeOffsetsAfterNonSuggestionEditingOperating(const DocumentMarker& marker,
+                                                 const String& node_text,
+                                                 unsigned offset,
+                                                 unsigned old_length,
+                                                 unsigned new_length) {
+  unsigned marker_start = marker.StartOffset();
+  unsigned marker_end = marker.EndOffset();
+
+  // Marked text was modified
+  if (offset < marker_end && offset + old_length > marker_start)
+    return {};
+
+  // Text inserted/replaced immediately after the marker, remove marker if first
+  // character is a (Unicode) letter or digit
+  if (offset == marker_end && new_length > 0) {
+    if (WTF::Unicode::IsAlphanumeric(GetCodePointAt(node_text, offset)))
+      return {};
+    return marker.ComputeOffsetsAfterShift(offset, old_length, new_length);
+  }
+
+  // Text inserted/replaced immediately before the marker, remove marker if
+  // first character is a (Unicode) letter or digit
+  if (offset == marker_start && new_length > 0) {
+    if (WTF::Unicode::IsAlphanumeric(
+            GetCodePointAt(node_text, offset + new_length - 1)))
+      return {};
+    return marker.ComputeOffsetsAfterShift(offset, old_length, new_length);
+  }
+
+  // Don't care if text was deleted immediately before or after the marker
+  return marker.ComputeOffsetsAfterShift(offset, old_length, new_length);
+}
+
+}  // namespace
 
 DocumentMarker::MarkerType SuggestionMarkerListImpl::MarkerType() const {
   return DocumentMarker::kSuggestion;
@@ -107,10 +153,67 @@ bool SuggestionMarkerListImpl::RemoveMarkers(unsigned start_offset,
   return did_remove_marker;
 }
 
-bool SuggestionMarkerListImpl::ShiftMarkers(const String&,
+bool SuggestionMarkerListImpl::ShiftMarkers(const String& node_text,
                                             unsigned offset,
                                             unsigned old_length,
                                             unsigned new_length) {
+  if (SuggestionMarkerReplacementScope::CurrentlyInScope())
+    return ShiftMarkersForSuggestionReplacement(offset, old_length, new_length);
+
+  return ShiftMarkersForNonSuggestionEditingOperation(node_text, offset,
+                                                      old_length, new_length);
+}
+
+bool SuggestionMarkerListImpl::ShiftMarkersForSuggestionReplacement(
+    unsigned offset,
+    unsigned old_length,
+    unsigned new_length) {
+  // Since suggestion markers are stored unsorted, the quickest way to perform
+  // this operation is to build a new list with the markers not removed by the
+  // shift.
+  bool did_shift_marker = false;
+  unsigned end_offset = offset + old_length;
+  HeapVector<Member<DocumentMarker>> unremoved_markers;
+  for (const Member<DocumentMarker>& marker : markers_) {
+    // Markers that intersect the replacement range, but do not fully contain
+    // it, should be removed.
+    const bool marker_intersects_replacement_range =
+        marker->StartOffset() < end_offset && marker->EndOffset() > offset;
+    const bool marker_contains_replacement_range =
+        marker->StartOffset() <= offset && marker->EndOffset() >= end_offset;
+
+    if (marker_intersects_replacement_range &&
+        !marker_contains_replacement_range) {
+      did_shift_marker = true;
+      continue;
+    }
+
+    Optional<DocumentMarker::MarkerOffsets> result =
+        marker->ComputeOffsetsAfterShift(offset, old_length, new_length);
+    if (result == WTF::nullopt) {
+      did_shift_marker = true;
+      continue;
+    }
+
+    if (marker->StartOffset() != result.value().start_offset ||
+        marker->EndOffset() != result.value().end_offset) {
+      marker->SetStartOffset(result.value().start_offset);
+      marker->SetEndOffset(result.value().end_offset);
+      did_shift_marker = true;
+    }
+
+    unremoved_markers.push_back(marker);
+  }
+
+  markers_ = std::move(unremoved_markers);
+  return did_shift_marker;
+}
+
+bool SuggestionMarkerListImpl::ShiftMarkersForNonSuggestionEditingOperation(
+    const String& node_text,
+    unsigned offset,
+    unsigned old_length,
+    unsigned new_length) {
   // Since suggestion markers are stored unsorted, the quickest way to perform
   // this operation is to build a new list with the markers not removed by the
   // shift.
@@ -118,8 +221,9 @@ bool SuggestionMarkerListImpl::ShiftMarkers(const String&,
   HeapVector<Member<DocumentMarker>> unremoved_markers;
   for (const Member<DocumentMarker>& marker : markers_) {
     Optional<DocumentMarker::MarkerOffsets> result =
-        marker->ComputeOffsetsAfterShift(offset, old_length, new_length);
-    if (result == WTF::nullopt) {
+        ComputeOffsetsAfterNonSuggestionEditingOperating(
+            *marker, node_text, offset, old_length, new_length);
+    if (!result) {
       did_shift_marker = true;
       continue;
     }
