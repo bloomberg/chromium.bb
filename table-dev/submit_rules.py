liblouis/liblouis

Extensive revision of table development tools
The model is built around the `nocross` and `syllable*` opcodes. By
using both of these opcodes instead of only nocross we allow a certain
chunk to have two different contractions depending on the context. I
expect this will give even better results.

The individual scripts from before are now combined in a single make
file. Most of what could be automated is now automated. In particular
the automatic generation of hyphenation patterns is important. The
script can compute how each word needs to be chunked up in order to be
translated correctly, and then invokes patgen using a non-interactive
wrapper script.

The process of table development now consists of running a simple
command

    ./make suggestions

then editing a file, running the command again, etc. All the work is
supposed to be done in a single working file, both

- adding/removing translation rules, and
- adding/correcting dictionary entries (text + expected braille +
  expected hyphenation).

After each change (at each run of ./make suggestions), the working file
is parsed, the dictionary and tables are automatically updated, and a
new working file is generated. A newly generated working file suggests a
number of words that need work. Several hints are presented at each
word, such as the set of rules that are currently applied and a set of
other existing rules that are possibly relevant.

table-dev/submit_rules.py
@@ -1,0 +1,45 @@
+import argparse
+import fileinput
+import re
+from utils import *
+
+def main():
+    parser = argparse.ArgumentParser(description="Add contraction rules to contraction table")
+    parser.add_argument('FILE', default=['-'],
+                        help="file with unprocessed contraction rules")
+    parser.add_argument('CONTRACTION_TABLE', help="contraction table")
+    parser.add_argument('-t', '--table', required=True,
+                        help="translation table for converting to dot patterns", dest="TABLE")
+    args = parser.parse_args()
+    load_table(args.TABLE)
+    p = re.compile(r"^#.*|[ \t]*([+-])?(nocross|syllable\*|word|begword|endword)[ \t]+([^ \t]+)[ \t]+([^ \t\n]+)([ \t]+(.*))?\n?$")
+    rules = []
+    for line in fileinput.FileInput(args.CONTRACTION_TABLE, openhook=fileinput.hook_encoded("utf-8")):
+        m = p.match(line)
+        exit_if_not(m and not m.group(1))
+        _, opcode, text, braille, _, comment = m.groups()
+        if opcode:
+            rule = {"opcode": opcode, "text": text, "braille": braille, "comment": comment}
+            rules.append(rule)
+    for line in fileinput.FileInput(args.FILE, openhook=fileinput.hook_encoded("utf-8")):
+        m = p.match(line)
+        exit_if_not(m)
+        add_or_delete, opcode, text, braille, _, _ = m.groups()
+        if opcode:
+            comment = braille + " "
+            braille = to_dot_pattern(braille)
+            rules, rule = partition(lambda rule: rule["opcode"] == opcode and
+                                                 rule["text"] == text and
+                                                 rule["braille"] == braille,
+                                    rules)
+            if add_or_delete == '-':
+                exit_if_not(rule)
+            else:
+                exit_if_not(not rule)
+                rule = {"opcode": opcode, "text": text, "braille": braille, "comment": comment}
+                rules.append(rule)
+    opcode_order = {"word": 1, "syllable*": 2, "nocross": 3, "begword": 4, "endword": 5}
+    for rule in sorted(rules, key=lambda rule: (rule["text"], opcode_order[rule["opcode"]])):
+        println(u"{opcode:<10} {text:<10} {braille:<30} {comment}".format(**rule))
+
+if __name__ == "__main__": main()

table-dev/utils.py
@@ -1,49 +1,156 @@
-from ctypes import c_wchar, c_char_p, c_wchar_p
-from louis import _loader, liblouis
-import os
+from ctypes import *
+from itertools import takewhile, izip_longest, chain, tee
+from louis import _loader, liblouis, outlenMultiplier                    , createStr
+import re
 import sqlite3
 import sys
 
-assert liblouis.lou_charSize() == 4
+def exit_if_not(expression):
+    if not expression:
+        raise RuntimeError()
+
+exit_if_not(liblouis.lou_charSize() == 4)
 
 liblouis_dev = _loader["liblouis-table-dev.so.0"]
-liblouis_dev.loadTable.argtypes = (c_char_p,)
-liblouis_dev.toLowercase.restype = c_wchar
+
+liblouis_dev.isLetter.argtypes = (c_wchar,)
 liblouis_dev.toLowercase.argtypes = (c_wchar,)
-liblouis_dev.suggestChunks.argtypes = (c_wchar_p,c_wchar_p)
+liblouis_dev.toLowercase.restype = c_wchar
+
+def println(line=""):
+    sys.stdout.write(("%s\n" % line).encode("utf-8"))
 
 def validate_chunks(chunked_text):
-    in_nocross = False
+    return re.search(r"^([^)(|]+|\([^)(|][^)(|]+\))(\|?([^)(|]+|\([^)(|][^)(|]+\)))*$", chunked_text) != None
+
+def print_chunks(text, hyphen_string):
+    exit_if_not(len(hyphen_string) == len(text) - 1 and re.search("^[01x]+$", hyphen_string))
+    chunked_text = []
+    k = 0
     prev_c = None
-    for c in chunked_text:
-        if c == '[':
-            if in_nocross or prev_c == '|':
-                return False
-            in_nocross = True
-        elif c == ']':
-            if not in_nocross:
-                return False
-            in_nocross = False
-        elif c == '|':
-            if in_nocross or prev_c == None or prev_c == ']' or prev_c == '|':
-                return False
+    for c in hyphen_string:
+        if c != prev_c and c == "0":
+            chunked_text.append("(")
+        chunked_text.append(text[k])
+        if c != prev_c and prev_c == "0":
+            chunked_text.append(")")
+        if c == "1":
+            chunked_text.append("|")
         prev_c = c
-    if in_nocross or prev_c == '|':
-        return False
-    return True
+        k += 1
+    chunked_text.append(text[k])
+    if (prev_c == "0"):
+        chunked_text.append(")")
+    return "".join(chunked_text)
 
-def normalize_text(text):
-    return "".join([liblouis_dev.toLowercase(c) for c in text])
+def parse_chunks(chunked_text):
+    exit_if_not(validate_chunks(chunked_text))
+    text, _ = read_text(chunked_text)
+    hyphen_string = ["x"] * (len(text) - 1)
+    k = 0
+    for c in chunked_text:
+        if c == "(":
+            hyphen_string[k:] = ["0"] * (len(text) - 1 - k)
+        elif c == ")":
+            hyphen_string[k-1:] =  ["x"] * (len(text) - 1 - (k-1))
+        elif c == "|":
+            hyphen_string[k-1] = "1"
+        else:
+            k += 1
+            if k > len(text):
+                break
+    return text, "".join(hyphen_string)
 
-def open_dictionary():
-    dirname, _ = os.path.split(os.path.abspath(sys.argv[0]))
-    dictionary = os.path.join(dirname, "dictionary.sqlite")
-    conn = sqlite3.connect(dictionary)
-    c = conn.cursor()
-    return conn, c
+def read_text(maybe_chunked_text):
+    if re.search("[)(|]", maybe_chunked_text) != None:
+        text = re.sub("[)(|]", "", maybe_chunked_text)
+        chunked_text = maybe_chunked_text
+    else:
+        text = maybe_chunked_text
+        chunked_text = None
+    return text, chunked_text
+
+def compare_chunks(expected_hyphen_string, real_hyphen_string, text):
+    exit_if_not(len(expected_hyphen_string) == len(text) - 1 and re.search("^[01x]+$", expected_hyphen_string))
+    exit_if_not(len(real_hyphen_string) == len(text) - 1 and re.search("^[01]+$", real_hyphen_string))
+    chunk_errors = "".join([x for x in chain(*izip_longest(text, map(lambda e, r: "*" if e in "1x" and r == "1" else
+                                                                                  "." if r == "1" else
+                                                                                  "-" if e == "1" else None,
+                                                                     expected_hyphen_string, real_hyphen_string)))
+                            if x is not None])
+    return chunk_errors if re.search(r"[-\.]", chunk_errors) else None
 
 def load_table(table):
     liblouis_dev.loadTable(table);
 
+def is_letter(text):
+    return all([liblouis_dev.isLetter(c) for c in text])
+
+def to_lowercase(text):
+    return "".join([liblouis_dev.toLowercase(c) for c in text])
+
+def to_dot_pattern(braille):
+    c_braille = create_unicode_buffer(braille)
+    c_dots = create_string_buffer(9 * len(braille))
+    liblouis_dev.toDotPattern(c_braille, c_dots)
+    return c_dots.value
+
 def hyphenate(table, text):
-    return text
+    c_text = create_unicode_buffer(text)
+    c_text_len = c_int(len(text))
+    c_hyphen_string = create_string_buffer(len(text) + 1)
+    exit_if_not(liblouis.lou_hyphenate(table, c_text, c_text_len, c_hyphen_string, 0))
+    return "".join(['1' if int(p) % 2 else '0' for p in c_hyphen_string.value[1:]])
+
+def translate(table, text):
+    c_text = create_unicode_buffer(text)
+    c_text_len = c_int(len(text))
+    braille_len = len(text) * outlenMultiplier
+    c_braille = create_unicode_buffer(braille_len)
+    c_braille_len = c_int(braille_len)
+    max_rules = 16
+    c_rules = (c_void_p * max_rules)()
+    c_rules_len = c_int(max_rules)
+    exit_if_not(liblouis.trace_translate(table, c_text, byref(c_text_len), c_braille, byref(c_braille_len),
+                                         None, None, None, None, None, c_rules, byref(c_rules_len), 0))
+    return c_braille.value, c_rules[0:c_rules_len.value]
+
+def get_rule(c_rule_pointer):
+    c_rule_string = create_unicode_buffer(u"", 128)
+    if not liblouis_dev.printRule(cast(c_rule_pointer, c_void_p), c_rule_string):
+        return None
+    return tuple(c_rule_string.value.split(" "))
+
+def suggest_chunks(text, braille):
+    c_text = create_unicode_buffer(text)
+    c_braille = create_unicode_buffer(braille)
+    c_hyphen_string = create_string_buffer(len(text) + 1)
+    if not liblouis_dev.suggestChunks(c_text, c_braille, c_hyphen_string):
+        return None;
+    hyphen_string = "".join(c_hyphen_string.value[1:len(c_hyphen_string.value)-1])
+    assert len(hyphen_string) == len(text) - 1 and re.search("^[01x]+$", hyphen_string)
+    return hyphen_string
+
+def find_possible_matches(text, braille):
+    c_text = create_unicode_buffer(text)
+    c_braille = create_unicode_buffer(braille)
+    max_rules = 16
+    c_matches = [u""] * max_rules + [None]
+    for i in range(0, max_rules):
+        c_matches[i] = create_unicode_buffer(c_matches[i], 128)
+        c_matches[i] = cast(c_matches[i], c_wchar_p)
+    c_matches = (c_wchar_p * (max_rules + 1))(*c_matches)
+    liblouis_dev.findPossibleMatches(c_text, c_braille, c_matches)
+    return map(lambda x: tuple(x.split(" ")), takewhile(lambda x: x, c_matches))
+
+def open_dictionary(dictionary):
+    conn = sqlite3.connect(dictionary)
+    c = conn.cursor()
+    return conn, c
+
+def filterfalse(predicate, iterable):
+    return [x for x in iterable if not predicate(x)]
+
+def partition(pred, iterable):
+    t1, t2 = tee(iterable)
+    return filterfalse(pred, t1), filter(pred, t2)

bloomberg/chromium.bb

table-dev/submit_rules.py
@@ -1,0 +1,47 @@
+import argparse
+import fileinput
+import re
+from utils import *
+
+def main():
+    parser = argparse.ArgumentParser(description="Add contraction rules to contraction table")
+    parser.add_argument('FILE', default=['-'],
+                        help="file with unprocessed contraction rules")
+    parser.add_argument('CONTRACTION_TABLE', help="contraction table")
+    parser.add_argument('-t', '--table', required=True,
+                        help="translation table for converting to dot patterns", dest="TABLE")
+    args = parser.parse_args()
+    load_table(args.TABLE)
+    p = re.compile(r"^#.*|[ \t]*([+-])?(nocross|syllable\*|word|begword|endword)[ \t]+([^ \t]+)[ \t]+([^ \t\n]+)([ \t]+(.*))?\n?$")
+    rules = []
+    for line in fileinput.FileInput(args.CONTRACTION_TABLE, openhook=fileinput.hook_encoded("utf-8")):
+        m = p.match(line)
+        exit_if_not(m and not m.group(1))
+        _, opcode, text, braille, _, comment = m.groups()
+        if opcode:
+            rule = {"opcode": opcode, "text": text, "braille": braille, "comment": comment}
+            rules.append(rule)
+    for line in fileinput.FileInput(args.FILE, openhook=fileinput.hook_encoded("utf-8")):
+        m = p.match(line)
+        exit_if_not(m)
+        add_or_delete, opcode, text, braille, _, _ = m.groups()
+        if opcode:
+            comment = braille
+            if comment.endswith('\\'):
+                comment = comment + " "
+            braille = to_dot_pattern(braille)
+            rules, rule = partition(lambda rule: rule["opcode"] == opcode and
+                                                 rule["text"] == text and
+                                                 rule["braille"] == braille,
+                                    rules)
+            if add_or_delete == '-':
+                exit_if_not(rule)
+            else:
+                exit_if_not(not rule)
+                rule = {"opcode": opcode, "text": text, "braille": braille, "comment": comment}
+                rules.append(rule)
+    opcode_order = {"word": 1, "syllable*": 2, "nocross": 3, "begword": 4, "endword": 5}
+    for rule in sorted(rules, key=lambda rule: (rule["text"], opcode_order[rule["opcode"]])):
+        println(u"{opcode:<10} {text:<10} {braille:<30} {comment}".format(**rule))
+
+if __name__ == "__main__": main()

table-dev/utils.py
@@ -1,49 +1,156 @@
-from ctypes import c_wchar, c_char_p, c_wchar_p
-from louis import _loader, liblouis
-import os
+from ctypes import *
+from itertools import takewhile, izip_longest, chain, tee
+from louis import _loader, liblouis, outlenMultiplier                    , createStr
+import re
 import sqlite3
 import sys
 
-assert liblouis.lou_charSize() == 4
+def exit_if_not(expression):
+    if not expression:
+        raise RuntimeError()
+
+exit_if_not(liblouis.lou_charSize() == 4)
 
 liblouis_dev = _loader["liblouis-table-dev.so.0"]
-liblouis_dev.loadTable.argtypes = (c_char_p,)
-liblouis_dev.toLowercase.restype = c_wchar
+
+liblouis_dev.isLetter.argtypes = (c_wchar,)
 liblouis_dev.toLowercase.argtypes = (c_wchar,)
-liblouis_dev.suggestChunks.argtypes = (c_wchar_p,c_wchar_p)
+liblouis_dev.toLowercase.restype = c_wchar
+
+def println(line=""):
+    sys.stdout.write(("%s\n" % line).encode("utf-8"))
 
 def validate_chunks(chunked_text):
-    in_nocross = False
+    return re.search(r"^([^)(|]+|\([^)(|][^)(|]+\))(\|?([^)(|]+|\([^)(|][^)(|]+\)))*$", chunked_text) != None
+
+def print_chunks(text, hyphen_string):
+    exit_if_not(len(hyphen_string) == len(text) - 1 and re.search("^[01x]+$", hyphen_string))
+    chunked_text = []
+    k = 0
     prev_c = None
-    for c in chunked_text:
-        if c == '[':
-            if in_nocross or prev_c == '|':
-                return False
-            in_nocross = True
-        elif c == ']':
-            if not in_nocross:
-                return False
-            in_nocross = False
-        elif c == '|':
-            if in_nocross or prev_c == None or prev_c == ']' or prev_c == '|':
-                return False
+    for c in hyphen_string:
+        if c != prev_c and c == "0":
+            chunked_text.append("(")
+        chunked_text.append(text[k])
+        if c != prev_c and prev_c == "0":
+            chunked_text.append(")")
+        if c == "1":
+            chunked_text.append("|")
         prev_c = c
-    if in_nocross or prev_c == '|':
-        return False
-    return True
+        k += 1
+    chunked_text.append(text[k])
+    if (prev_c == "0"):
+        chunked_text.append(")")
+    return "".join(chunked_text)
 
-def normalize_text(text):
-    return "".join([liblouis_dev.toLowercase(c) for c in text])
+def parse_chunks(chunked_text):
+    exit_if_not(validate_chunks(chunked_text))
+    text, _ = read_text(chunked_text)
+    hyphen_string = ["x"] * (len(text) - 1)
+    k = 0
+    for c in chunked_text:
+        if c == "(":
+            hyphen_string[k:] = ["0"] * (len(text) - 1 - k)
+        elif c == ")":
+            hyphen_string[k-1:] =  ["x"] * (len(text) - 1 - (k-1))
+        elif c == "|":
+            hyphen_string[k-1] = "1"
+        else:
+            k += 1
+            if k > len(text):
+                break
+    return text, "".join(hyphen_string)
 
-def open_dictionary():
-    dirname, _ = os.path.split(os.path.abspath(sys.argv[0]))
-    dictionary = os.path.join(dirname, "dictionary.sqlite")
-    conn = sqlite3.connect(dictionary)
-    c = conn.cursor()
-    return conn, c
+def read_text(maybe_chunked_text):
+    if re.search("[)(|]", maybe_chunked_text) != None:
+        text = re.sub("[)(|]", "", maybe_chunked_text)
+        chunked_text = maybe_chunked_text
+    else:
+        text = maybe_chunked_text
+        chunked_text = None
+    return text, chunked_text
+
+def compare_chunks(expected_hyphen_string, real_hyphen_string, text):
+    exit_if_not(len(expected_hyphen_string) == len(text) - 1 and re.search("^[01x]+$", expected_hyphen_string))
+    exit_if_not(len(real_hyphen_string) == len(text) - 1 and re.search("^[01]+$", real_hyphen_string))
+    chunk_errors = "".join([x for x in chain(*izip_longest(text, map(lambda e, r: "*" if e in "1x" and r == "1" else
+                                                                                  "." if r == "1" else
+                                                                                  "-" if e == "1" else None,
+                                                                     expected_hyphen_string, real_hyphen_string)))
+                            if x is not None])
+    return chunk_errors if re.search(r"[-\.]", chunk_errors) else None
 
 def load_table(table):
     liblouis_dev.loadTable(table);
 
+def is_letter(text):
+    return all([liblouis_dev.isLetter(c) for c in text])
+
+def to_lowercase(text):
+    return "".join([liblouis_dev.toLowercase(c) for c in text])
+
+def to_dot_pattern(braille):
+    c_braille = create_unicode_buffer(braille)
+    c_dots = create_string_buffer(9 * len(braille))
+    liblouis_dev.toDotPattern(c_braille, c_dots)
+    return c_dots.value
+
 def hyphenate(table, text):
-    return text
+    c_text = create_unicode_buffer(text)
+    c_text_len = c_int(len(text))
+    c_hyphen_string = create_string_buffer(len(text) + 1)
+    exit_if_not(liblouis.lou_hyphenate(table, c_text, c_text_len, c_hyphen_string, 0))
+    return "".join(['1' if int(p) % 2 else '0' for p in c_hyphen_string.value[1:]])
+
+def translate(table, text):
+    c_text = create_unicode_buffer(text)
+    c_text_len = c_int(len(text))
+    braille_len = len(text) * outlenMultiplier
+    c_braille = create_unicode_buffer(braille_len)
+    c_braille_len = c_int(braille_len)
+    max_rules = 16
+    c_rules = (c_void_p * max_rules)()
+    c_rules_len = c_int(max_rules)
+    exit_if_not(liblouis._lou_translateWithTracing(table, c_text, byref(c_text_len), c_braille, byref(c_braille_len),
+                                                   None, None, None, None, None, 0, c_rules, byref(c_rules_len)))
+    return c_braille.value, c_rules[0:c_rules_len.value]
+
+def get_rule(c_rule_pointer):
+    c_rule_string = create_unicode_buffer(u"", 128)
+    if not liblouis_dev.printRule(cast(c_rule_pointer, c_void_p), c_rule_string):
+        return None
+    return tuple(c_rule_string.value.split(" "))
+
+def suggest_chunks(text, braille):
+    c_text = create_unicode_buffer(text)
+    c_braille = create_unicode_buffer(braille)
+    c_hyphen_string = create_string_buffer(len(text) + 1)
+    if not liblouis_dev.suggestChunks(c_text, c_braille, c_hyphen_string):
+        return None;
+    hyphen_string = "".join(c_hyphen_string.value[1:len(c_hyphen_string.value)-1])
+    assert len(hyphen_string) == len(text) - 1 and re.search("^[01x]+$", hyphen_string)
+    return hyphen_string
+
+def find_possible_matches(text, braille):
+    c_text = create_unicode_buffer(text)
+    c_braille = create_unicode_buffer(braille)
+    max_rules = 16
+    c_matches = [u""] * max_rules + [None]
+    for i in range(0, max_rules):
+        c_matches[i] = create_unicode_buffer(c_matches[i], 128)
+        c_matches[i] = cast(c_matches[i], c_wchar_p)
+    c_matches = (c_wchar_p * (max_rules + 1))(*c_matches)
+    liblouis_dev.findPossibleMatches(c_text, c_braille, c_matches)
+    return map(lambda x: tuple(x.split(" ")), takewhile(lambda x: x, c_matches))
+
+def open_dictionary(dictionary):
+    conn = sqlite3.connect(dictionary)
+    c = conn.cursor()
+    return conn, c
+
+def filterfalse(predicate, iterable):
+    return [x for x in iterable if not predicate(x)]
+
+def partition(pred, iterable):
+    t1, t2 = tee(iterable)
+    return filterfalse(pred, t1), filter(pred, t2)