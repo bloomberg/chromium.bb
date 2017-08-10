f62a06c1178999fae10df03583e111e401d69aa7
Helen Yang <helenlyang@google.com>
2017-08-10 18:14:17 +0000

    [ios] Refactor NumberBadgeView and fix TextBadgeView bug
    This CL refactors NumberBadgeView by replacing the background view
    and label properties with a TextBadgeView property.
    Since NumberBadgeView and TextBadgeView are similar in appearance,
    this reduces the amount of repeated code. The refactoring also moves
    calls to instance methods and property accessors from the
    NumberBadgeView constructor to -willMoveToSuperview.
    
    Secondly, this CL fixes a bug that caused TextBadgeView to stretch
    across the reading list menu cell. It modifies constraints defined
    in TextBadgeView and ReadingListMenuViewCell. Additionally, in
    order to allow NumberBadgeView to set the width of the badge, this
    CL adds a |labelHorizontalMargin| parameter to the designated
    initializer. This is necessary because NumberBadgeView has a
    smaller label margin than TextBadgeView. Finally, this CL moves calls
    to instance methods and accessors from the constructor to
    -willMoveToSuperview.
    
    Bug: 740133
    Change-Id: Idc45cdf510aa1f5a362b40f494cfb9464c202662
    Reviewed-on: https://chromium-review.googlesource.com/607571
    Commit-Queue: Helen Yang <helenlyang@google.com>
    Reviewed-by: Ed Chin <edchin@chromium.org>
    Reviewed-by: Gregory Chatzinoff <gchatz@chromium.org>
    Reviewed-by: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493463}
    Former-commit-id: ccebb7a4df587ae86fa4dabaa3e032c4d75a8fec

--- ios/chrome/browser/ui/reading_list/text_badge_view.h
+++ ios/chrome/browser/ui/reading_list/text_badge_view.h
@@ -13,8 +13,13 @@
 // Text displayed on the badge.
 @property(nonatomic, copy) NSString* text;
 
-// Initialize the text badge with the given display text.
-- (instancetype)initWithText:(NSString*)text NS_DESIGNATED_INITIALIZER;
+// Initialize the text badge with the given display text and horizontal label
+// margin.
+- (instancetype)initWithText:(NSString*)text
+       labelHorizontalMargin:(CGFloat)margin NS_DESIGNATED_INITIALIZER;
+
+// Convenience initializer that uses a default horizontal label margin value.
+- (instancetype)initWithText:(NSString*)text;
 
 - (instancetype)initWithFrame:(CGRect)frame NS_UNAVAILABLE;
 
