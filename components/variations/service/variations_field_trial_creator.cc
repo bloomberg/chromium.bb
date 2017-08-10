d06788f8b8f5d9b568066309eba28424b1a4edb0
Kyle Milka <kmilka@google.com>
2017-08-10 02:16:48 +0000

    Allow overriding of the platform that experiments are filtered for
    This CL adds the OverrideVariationsPlatform() function to the
    VariationsFieldTrialCreator class. Now platforms such as WebView can
    correctly filter their list of active experiments.
    
    Bug=678288
    
    Change-Id: I91f89457475775d1ddece81ecc3519b8461d0e8c
    Reviewed-on: https://chromium-review.googlesource.com/607040
    Commit-Queue: Kyle Milka <kmilka@google.com>
    Reviewed-by: Alexei Svitkine (very slow) <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493231}
    Former-commit-id: b819b69d7cf653564e51d39b794ba6d9137441d2

--- components/variations/service/variations_field_trial_creator.cc
+++ components/variations/service/variations_field_trial_creator.cc
@@ -126,7 +126,9 @@ VariationsFieldTrialCreator::VariationsFieldTrialCreator(
     : client_(client),
       ui_string_overrider_(ui_string_overrider),
       seed_store_(local_state),
-      create_trials_from_seed_called_(false) {}
+      create_trials_from_seed_called_(false),
+      has_platform_override_(false),
+      platform_override_(Study::PLATFORM_WINDOWS) {}
 
 VariationsFieldTrialCreator::~VariationsFieldTrialCreator() {}
 
@@ -218,7 +220,9 @@ VariationsFieldTrialCreator::GetClientFilterableStateForVersion(
   state->version = version;
   state->channel = GetChannelForVariations(client_->GetChannel());
   state->form_factor = GetCurrentFormFactor();
-  state->platform = ClientFilterableState::GetCurrentPlatform();
+  state->platform = (has_platform_override_)
+                        ? platform_override_
+                        : ClientFilterableState::GetCurrentPlatform();
   state->hardware_class = GetHardwareClass();
 #if defined(OS_ANDROID)
   // This is set on Android only currently, because the IsLowEndDevice() API
@@ -327,6 +331,12 @@ void VariationsFieldTrialCreator::RecordLastFetchTime() {
 
 bool VariationsFieldTrialCreator::LoadSeed(VariationsSeed* seed) {
   return seed_store_.LoadSeed(seed);
+}
+
+void VariationsFieldTrialCreator::OverrideVariationsPlatform(
+    Study::Platform platform_override) {
+  has_platform_override_ = true;
+  platform_override_ = platform_override;
 }
 
 bool VariationsFieldTrialCreator::SetupFieldTrials(
