2bbe139d7a3e0de3626d60bdc346aa9c78e662f7
Mike Reed <reed@google.com>
2017-08-10 20:32:14 +0000

    add flag for future Skia CL
    see https://skia-review.googlesource.com/c/33180
    
    Bug: 
    Change-Id: I0d2453fa01907ca3827c12d48d4c6e7503b03d5e
    Reviewed-on: https://chromium-review.googlesource.com/610823
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Mike Reed <reed@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493521}
    Former-commit-id: 77b82ff0d7f2e8cdb60429564eedfb3e28b40c0b

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -208,6 +208,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_STREAM_API
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_LOCAL_ROTATE_SHADER
+#define SK_SUPPORT_LEGACY_LOCAL_ROTATE_SHADER
+#endif
+
 #ifndef SK_DISABLE_DEFERRED_PROXIES
 #define SK_DISABLE_DEFERRED_PROXIES
 #endif
