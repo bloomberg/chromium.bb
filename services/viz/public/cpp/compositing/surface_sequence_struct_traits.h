887c2cd808d9a8d9769c161e1cc97c040e005051
Saman Sami <samans@chromium.org>
2017-08-10 00:05:47 +0000

    viz: Move SurfaceSequence's mojom from cc/ipc to services/viz
    TBR=aelias@chromium.org
    
    Bug: 722935
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: Ibaff586055b80c63b6666efe010b3f8a5dfae4a5
    Reviewed-on: https://chromium-review.googlesource.com/602485
    Commit-Queue: Saman Sami <samans@chromium.org>
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493184}
    Former-commit-id: 95955cbc765c0eadc75d3e74c38ee4e4b034462a

--- services/viz/public/cpp/compositing/surface_sequence_struct_traits.h
+++ services/viz/public/cpp/compositing/surface_sequence_struct_traits.h
@@ -2,16 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CC_IPC_SURFACE_SEQUENCE_STRUCT_TRAITS_H_
-#define CC_IPC_SURFACE_SEQUENCE_STRUCT_TRAITS_H_
+#ifndef SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_SURFACE_SEQUENCE_STRUCT_TRAITS_H_
+#define SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_SURFACE_SEQUENCE_STRUCT_TRAITS_H_
 
-#include "cc/ipc/surface_sequence.mojom-shared.h"
 #include "components/viz/common/surfaces/surface_sequence.h"
+#include "services/viz/public/interfaces/compositing/surface_sequence.mojom-shared.h"
 
 namespace mojo {
 
 template <>
-struct StructTraits<cc::mojom::SurfaceSequenceDataView, viz::SurfaceSequence> {
+struct StructTraits<viz::mojom::SurfaceSequenceDataView, viz::SurfaceSequence> {
   static const viz::FrameSinkId& frame_sink_id(const viz::SurfaceSequence& id) {
     return id.frame_sink_id;
   }
@@ -20,7 +20,7 @@ struct StructTraits<cc::mojom::SurfaceSequenceDataView, viz::SurfaceSequence> {
     return id.sequence;
   }
 
-  static bool Read(cc::mojom::SurfaceSequenceDataView data,
+  static bool Read(viz::mojom::SurfaceSequenceDataView data,
                    viz::SurfaceSequence* out) {
     viz::FrameSinkId frame_sink_id;
     if (!data.ReadFrameSinkId(&frame_sink_id))
@@ -32,4 +32,4 @@ struct StructTraits<cc::mojom::SurfaceSequenceDataView, viz::SurfaceSequence> {
 
 }  // namespace mojo
 
-#endif  // CC_IPC_SURFACE_SEQUENCE_STRUCT_TRAITS_H_
+#endif  // SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_SURFACE_SEQUENCE_STRUCT_TRAITS_H_
