f19b6e6ab17d35625d74f694ea4f020de9ea0be2
Saman Sami <samans@chromium.org>
2017-08-10 20:21:57 +0000

    Add presubmit checks for services/viz
    TBR=fsamuel@chromium.org
    
    Change-Id: I54a1ea2bb73457751f261035d21246fb9af8cdb8
    Reviewed-on: https://chromium-review.googlesource.com/602481
    Commit-Queue: Saman Sami <samans@chromium.org>
    Reviewed-by: kylechar <kylechar@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493505}
    Former-commit-id: 6be8b1fc0d2601e573aeb624d716add5f95167ff

--- services/viz/PRESUBMIT.py
+++ services/viz/PRESUBMIT.py
@@ -2,15 +2,15 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Top-level presubmit script for components/viz."""
+"""Top-level presubmit script for services/viz."""
 
 def CheckChangeOnUpload(input_api, output_api):
   import sys
   original_sys_path = sys.path
   sys.path = sys.path + [input_api.os_path.join(
-    input_api.change.RepositoryRoot(),
-    'components', 'viz')]
+      input_api.change.RepositoryRoot(),
+      'components', 'viz')]
 
   import presubmit_checks as ps
-  white_list=(r'^components[\\/]viz[\\/].*\.(cc|h)$',)
+  white_list=(r'^services[\\/]viz[\\/].*\.(cc|h)$',)
   return ps.RunAllChecks(input_api, output_api, white_list)
