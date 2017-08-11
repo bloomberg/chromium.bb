1a8e9edaecc59546a8a0d0e6e8a3f482378d997c
Florin Malita <fmalita@chromium.org>
2017-08-11 22:49:29 +0000

    Add guard for upcoming Skia changes
    SK_SUPPORT_LEGACY_RP_BLENDS
    
    TBR=
    NOTRY=true
    
    Change-Id: I028351886928903c6424fe52f5be96fa8ad4ec31
    Reviewed-on: https://chromium-review.googlesource.com/612049
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493875}
    Former-commit-id: 1c182ec4825effa2c72648480f7b5b4f5f5ee4e0

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -220,6 +220,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_DELTA_AA
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_RP_BLENDS
+#define SK_SUPPORT_LEGACY_RP_BLENDS
+#endif
+
 ///////////////////////// Imported from BUILD.gn and skia_common.gypi
 
 /* In some places Skia can use static initializers for global initialization,
