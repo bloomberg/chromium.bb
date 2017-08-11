33042b7c69e2bc8c298d63a701e37b70b0f29d14
Olga Sharonova <olka@chromium.org>
2017-08-11 09:57:47 +0000

    Disabling flaky ChromeCleanerPromptUserTest.OnInfectedBrowserNotAvailable again
    TBR=robertshield@chromium.org,jjlopezjaimez@google.com
    
    Bug: 753632
    Change-Id: Ie4353a529e12e54e57f35a52549ff2fe96eacd15
    Reviewed-on: https://chromium-review.googlesource.com/612165
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493694}
    Former-commit-id: 245af5b2c2210ea85e08b754abde4e4e1e9ba2ab

--- chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_dialog_controller_impl_browsertest_win.cc
+++ chrome/browser/safe_browsing/chrome_cleaner/chrome_cleaner_dialog_controller_impl_browsertest_win.cc
@@ -80,7 +80,7 @@ IN_PROC_BROWSER_TEST_F(ChromeCleanerPromptUserTest,
 }
 
 IN_PROC_BROWSER_TEST_F(ChromeCleanerPromptUserTest,
-                       OnInfectedBrowserNotAvailable) {
+                       DISABLED_OnInfectedBrowserNotAvailable) {
   browser()->window()->Minimize();
   base::RunLoop().RunUntilIdle();
   dialog_controller_->OnInfected(std::set<base::FilePath>());
