a1e9dfaeddb805d4e95b0eef20ae99f438f7f6ad
Evan Stade <estade@chromium.org>
2017-08-11 17:44:17 +0000

    Count a WebContents as "no longer waiting for response" after receiving a DidGetResourceResponseStart callback.
    Bug: 752326
    Change-Id: I27c8eb9a26dccd5d5578aa34d575eb4bd46d0f24
    Reviewed-on: https://chromium-review.googlesource.com/601254
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493789}
    Former-commit-id: ad2949ac9cb3919d1f3866f10dd2e03cc1856e2a

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -3403,6 +3403,7 @@ void WebContentsImpl::LoadStateChanged(
 
 void WebContentsImpl::DidGetResourceResponseStart(
   const ResourceRequestDetails& details) {
+  SetNotWaitingForResponse();
   controller_.ssl_manager()->DidStartResourceResponse(
       details.url, details.has_certificate, details.ssl_cert_status);
 
