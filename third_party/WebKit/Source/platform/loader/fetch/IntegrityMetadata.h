133070c608437a49dcbeab85d1e94b51ace9afa9
Daniel Vogelheim <vogelheim@chromium.org>
2017-08-11 09:04:06 +0000

    Use a separate enum for IntegrityMetadata::algorithm_.
    This is a preparatory change for 607930. This separates the notion
    of the integrity algorithm from a crypto hash function. The intent
    is to introduce a signature-based scheme, and thus an integrity
    method not based on a hash function.
    
    
    This is a re-land of https://chromium-review.googlesource.com/c/608974
    This CL broke because (one of) the Windows compiler(s) through a
    variable might be uninitialized. Fix is a one-line in patch set 2.
    
    Bug: 753349
    Change-Id: I6d9e86e30ea248f1cb0aec3fb8f594e2a298dcab
    Reviewed-on: https://chromium-review.googlesource.com/610021
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Daniel Vogelheim <vogelheim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493691}
    Former-commit-id: 550aff53e740d03e24205c08425d1b002f13ce13

--- third_party/WebKit/Source/platform/loader/fetch/IntegrityMetadata.h
+++ third_party/WebKit/Source/platform/loader/fetch/IntegrityMetadata.h
@@ -5,7 +5,6 @@
 #ifndef IntegrityMetadata_h
 #define IntegrityMetadata_h
 
-#include "platform/Crypto.h"
 #include "platform/PlatformExport.h"
 #include "platform/wtf/HashSet.h"
 #include "platform/wtf/StringHasher.h"
@@ -15,20 +14,21 @@
 namespace blink {
 
 class IntegrityMetadata;
+enum class IntegrityAlgorithm : uint8_t;
 
-using IntegrityMetadataPair = std::pair<WTF::String, HashAlgorithm>;
+using IntegrityMetadataPair = std::pair<String, IntegrityAlgorithm>;
 using IntegrityMetadataSet = WTF::HashSet<IntegrityMetadataPair>;
 
 class PLATFORM_EXPORT IntegrityMetadata {
  public:
   IntegrityMetadata() {}
-  IntegrityMetadata(WTF::String digest, HashAlgorithm);
-  IntegrityMetadata(std::pair<WTF::String, HashAlgorithm>);
+  IntegrityMetadata(String digest, IntegrityAlgorithm);
+  IntegrityMetadata(IntegrityMetadataPair);
 
-  WTF::String Digest() const { return digest_; }
-  void SetDigest(const WTF::String& digest) { digest_ = digest; }
-  HashAlgorithm Algorithm() const { return algorithm_; }
-  void SetAlgorithm(HashAlgorithm algorithm) { algorithm_ = algorithm; }
+  String Digest() const { return digest_; }
+  void SetDigest(const String& digest) { digest_ = digest; }
+  IntegrityAlgorithm Algorithm() const { return algorithm_; }
+  void SetAlgorithm(IntegrityAlgorithm algorithm) { algorithm_ = algorithm; }
 
   IntegrityMetadataPair ToPair() const;
 
@@ -36,8 +36,8 @@ class PLATFORM_EXPORT IntegrityMetadata {
                         const IntegrityMetadataSet& set2);
 
  private:
-  WTF::String digest_;
-  HashAlgorithm algorithm_;
+  String digest_;
+  IntegrityAlgorithm algorithm_;
 };
 
 enum class ResourceIntegrityDisposition : uint8_t {
@@ -46,6 +46,24 @@ enum class ResourceIntegrityDisposition : uint8_t {
   kPassed
 };
 
+enum class IntegrityAlgorithm : uint8_t { kSha256, kSha384, kSha512 };
+
 }  // namespace blink
+
+namespace WTF {
+
+template <>
+struct DefaultHash<blink::IntegrityAlgorithm> {
+  STATIC_ONLY(DefaultHash);
+  typedef IntHash<blink::IntegrityAlgorithm> Hash;
+};
+
+template <>
+struct HashTraits<blink::IntegrityAlgorithm>
+    : UnsignedWithZeroKeyHashTraits<blink::IntegrityAlgorithm> {
+  STATIC_ONLY(HashTraits);
+};
+
+}  // namespace WTF
 
 #endif
