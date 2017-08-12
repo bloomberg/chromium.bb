4d6f96f74f8cef7c8843e4141430e154357268ad
Jeremy Roman <jbroman@chromium.org>
2017-08-12 05:51:39 +0000

    Do not open a Local<Context> in ScriptState::From.
    We can directly compare the local handle to the persistent handle for equality,
    which is substantially cheaper than opening a new Local<Context> to compare the
    two.
    
    This turns out to have been 2/3 of the cost of blink::ScriptState::From.
    
    Change-Id: I4fc47da794dd6458b7d8b0981403f7126156f899
    Reviewed-on: https://chromium-review.googlesource.com/611671
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493961}
    Former-commit-id: 67c1c1d0d891728fa12d75479ca3072364e4bb8d

--- third_party/WebKit/Source/platform/bindings/ScriptState.h
+++ third_party/WebKit/Source/platform/bindings/ScriptState.h
@@ -125,7 +125,7 @@ class PLATFORM_EXPORT ScriptState : public RefCounted<ScriptState> {
     // ScriptState::from() must not be called for a context that does not have
     // valid embedder data in the embedder field.
     SECURITY_CHECK(script_state);
-    SECURITY_CHECK(script_state->GetContext() == context);
+    SECURITY_CHECK(script_state->context_ == context);
     return script_state;
   }
 
