05c25ed4f86067a8bd9d26f57ba289a1bbb37b06
Joseph Arhar <jarhar@google.com>
2017-08-12 00:58:57 +0000

    Add FileProxyWrapper
    FileProxyWrapper is an interface to be used by FileTransferMessageHandler
    for doing file IO on one file. A linux implementation which runs in the
    same process will be implemented in a subsequent change, and a windows
    implementation may be written in the future which communicates
    operations to a different process.
    
    Bug: 
    Change-Id: Idc33788eff266e755ed70a92bf59aa051c311c45
    Reviewed-on: https://chromium-review.googlesource.com/606547
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joseph Arhar <jarhar@google.com>
    Cr-Commit-Position: refs/heads/master@{#493915}
    Former-commit-id: b5a0cf9aeee03498cc7d582dc59a5dee670e0fc2

---
+++ remoting/host/file_proxy_wrapper.h
@@ -0,0 +1,39 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_FILE_PROXY_WRAPPER_H_
+#define REMOTING_HOST_FILE_PROXY_WRAPPER_H_
+
+#include "base/callback.h"
+#include "base/files/file_proxy.h"
+#include "remoting/proto/file_transfer.pb.h"
+
+namespace remoting {
+
+class CompoundBuffer;
+
+// FileProxyWrapper is an interface for implementing platform-specific file
+// writers for file transfers. Each operation is posted to a separate file IO
+// thread, and possibly a different process depending on the platform.
+class FileProxyWrapper {
+ public:
+  typedef base::Callback<void(protocol::FileTransferResponse_ErrorCode)>
+      ErrorCallback;
+  typedef base::Callback<void()> SuccessCallback;
+
+  FileProxyWrapper();
+  virtual ~FileProxyWrapper();
+
+  virtual void Init(const ErrorCallback& error_callback) = 0;
+  virtual void CreateFile(const std::string& filename,
+                          uint64_t filesize,
+                          const SuccessCallback& success_callback) = 0;
+  virtual void WriteChunk(std::unique_ptr<CompoundBuffer> buffer) = 0;
+  virtual void Close(const SuccessCallback& success_callback) = 0;
+  virtual void Cancel() = 0;
+};
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_FILE_PROXY_WRAPPER_H_
