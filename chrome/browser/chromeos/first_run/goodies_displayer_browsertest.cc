bfd7c101eaa106dcac057b32282dcd6791996379
Scott Violet <sky@chromium.org>
2017-08-12 01:05:20 +0000

    Updates GoodiesDisplayerBrowserTest to work with mus
    It resets the command line. It needs to pass along the mus config,
    otherwise we get into a weird state (aura is configured with Mus, but
    not Shell tries to create classic).
    
    BUG=none
    TEST=none
    
    Change-Id: I2e351ac043b74cacd83161871df14f46dc77e5aa
    Reviewed-on: https://chromium-review.googlesource.com/611788
    Reviewed-by: James Cook <jamescook@chromium.org>
    Commit-Queue: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#493920}
    Former-commit-id: d1547e989afdb29e19ba8e5a6d86b8fc5d765d90

--- chrome/browser/chromeos/first_run/goodies_displayer_browsertest.cc
+++ chrome/browser/chromeos/first_run/goodies_displayer_browsertest.cc
@@ -2,9 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/chromeos/first_run/goodies_displayer.h"
+
 #include "base/command_line.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/chromeos/first_run/goodies_displayer.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
@@ -84,6 +85,13 @@ class GoodiesDisplayerBrowserTest : public InProcessBrowserTest,
   // InProcessBrowserTest overrides.
   void SetUpDefaultCommandLine(base::CommandLine* command_line) override {
     base::CommandLine default_command_line(base::CommandLine::NO_PROGRAM);
+    if (command_line->HasSwitch(switches::kMus)) {
+      default_command_line.AppendSwitch(switches::kMus);
+    } else if (command_line->HasSwitch(switches::kMusConfig)) {
+      default_command_line.AppendSwitchASCII(
+          switches::kMusConfig,
+          command_line->GetSwitchValueASCII(switches::kMusConfig));
+    }
     InProcessBrowserTest::SetUpDefaultCommandLine(&default_command_line);
     if (NoFirstRunSpecified()) {  // --no-first-run is present by default.
       *command_line = default_command_line;
