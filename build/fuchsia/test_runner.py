e9e0051c59f0426bdedcdfea399c634a925167b6
Sergey Ulanov <sergeyu@google.com>
2017-08-14 20:22:53 +0000

    Add testserver support in fuchsia/test_runner.py.
    net_unittests depends on a test server for some unittests. That server
    is written in Python, so currently it's not possible to run it under
    Fuchsia. But it's possible to run it outside qemu.
     1. Enabled user network interface when starting qemu.
     2. Enabled chrome_test_server_spawner in the test_runner.py for
        net_unittests.
    
    Bug: 738275
    Change-Id: I36eaea2cfec5ad6c45c18f41c1afd1d00b4a3fde
    Reviewed-on: https://chromium-review.googlesource.com/611461
    Commit-Queue: Sergey Ulanov <sergeyu@chromium.org>
    Reviewed-by: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494165}
    Former-commit-id: 241f1d55ac1233c8028c45f767a882cdd4be31f3

--- build/fuchsia/test_runner.py
+++ build/fuchsia/test_runner.py
@@ -10,10 +10,69 @@ to run, or starts the bootserver to allow running on a hardware device."""
 
 import argparse
 import os
+import socket
 import sys
+import tempfile
+import time
 
-from runner_common import MakeTargetImageName, RunFuchsia, BuildBootfs, \
-    ReadRuntimeDeps
+from runner_common import RunFuchsia, BuildBootfs, ReadRuntimeDeps
+
+DIR_SOURCE_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), os.pardir, os.pardir))
+sys.path.append(os.path.join(DIR_SOURCE_ROOT, 'build', 'util', 'lib', 'common'))
+import chrome_test_server_spawner
+
+TEST_SERVER_PORT = 5000
+
+def IsLocalPortAvailable(port):
+  s = socket.socket()
+  try:
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('127.0.0.1', port))
+    return True
+  except socket.error:
+    return False
+  finally:
+    s.close()
+
+
+def WaitUntil(predicate, max_attempts=5):
+  """Blocks until the provided predicate (function) is true.
+
+  Returns:
+    Whether the provided predicate was satisfied once (before the timeout).
+  """
+  sleep_time_sec = 0.025
+  for _ in xrange(1, max_attempts):
+    if predicate():
+      return True
+    time.sleep(sleep_time_sec)
+    sleep_time_sec = min(1, sleep_time_sec * 2)  # Don't wait more than 1 sec.
+  return False
+
+
+# Implementation of chrome_test_server_spawner.PortForwarder that doesn't
+# forward ports. Instead the tests are expected to connect to the host IP
+# address inside the virtual network provided by qemu. qemu will forward
+# these connections to the corresponding localhost ports.
+class PortForwarderNoop(chrome_test_server_spawner.PortForwarder):
+  def Map(self, port_pairs):
+    pass
+
+  def GetDevicePortForHostPort(self, host_port):
+    return host_port
+
+  def WaitHostPortAvailable(self, port):
+    return WaitUntil(lambda: IsLocalPortAvailable(port))
+
+  def WaitPortNotAvailable(self, port):
+    return WaitUntil(lambda: not IsLocalPortAvailable(port))
+
+  def WaitDevicePortReady(self, port):
+    return self.WaitPortNotAvailable(port)
+
+  def Unmap(self, device_port):
+    pass
 
 
 def main():
@@ -30,6 +89,9 @@ def main():
   parser.add_argument('--exe-name',
                       type=os.path.realpath,
                       help='Name of the the test')
+  parser.add_argument('--enable-test-server', action='store_true',
+                      default=False,
+                      help='Enable testserver spawner.')
   parser.add_argument('--gtest_filter',
                       help='GTest filter to use in place of any default.')
   parser.add_argument('--gtest_repeat',
@@ -83,18 +145,25 @@ def main():
 
   runtime_deps = ReadRuntimeDeps(args.runtime_deps_path, args.output_directory)
 
+  # Start test server spawner for tests that need it.
+  if args.enable_test_server:
+    spawning_server = chrome_test_server_spawner.SpawningServer(
+          TEST_SERVER_PORT, PortForwarderNoop())
+    spawning_server.Start()
+
+    # Generate test server config.
+    config_file = tempfile.NamedTemporaryFile()
+    config_file.write(str(TEST_SERVER_PORT) + ':' + str(TEST_SERVER_PORT + 1))
+    config_file.flush()
+    runtime_deps.append(('net-test-server-ports', config_file.name))
+
   if args.test_launcher_filter_file:
     # Bundle the filter file in the runtime deps and compose the command-line
     # flag which references it.
     test_launcher_filter_file = os.path.normpath(
         os.path.join(args.output_directory, args.test_launcher_filter_file))
-    common_prefix = os.path.commonprefix(runtime_deps)
-    filter_device_path = MakeTargetImageName(common_prefix,
-                                             args.output_directory,
-                                             test_launcher_filter_file)
-    runtime_deps.append(args.test_launcher_filter_file)
-    child_args.append('--test-launcher-filter-file=/system/' +
-                      filter_device_path)
+    runtime_deps.append(('test_filter_file', test_launcher_filter_file))
+    child_args.append('--test-launcher-filter-file=/system/test_filter_file')
 
   bootfs = BuildBootfs(args.output_directory, runtime_deps, args.exe_name,
                        child_args, args.device, args.dry_run)
