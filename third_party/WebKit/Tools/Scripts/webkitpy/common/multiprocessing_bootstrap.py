4b63fffabc5299e1c2c0f354b1b302ee8b9585d2
Quinten Yearsley <qyearsley@google.com>
2017-08-14 22:41:17 +0000

    Revert "More debug logging for run-webkit-tests on the windows bot."
    This reverts commit d10e5a138426d542e0cf3ca1ec77946d9e474881 [formerly b1b6145a2ebdca0d88d3d260558482d87329fd10].
    
    This temporary special case logging was added in 2015 for
    crbug.com/522396, which has been closed for some time.
    
    Change-Id: I7c0efc266b342a5ceb6f027c13128c30208d3966
    Reviewed-on: https://chromium-review.googlesource.com/610899
    Reviewed-by: Jeff Carpenter <jeffcarp@chromium.org>
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494219}
    Former-commit-id: 5ed4a8112fa6ce7ed9c40e578bdebe2d063f7bcf

--- third_party/WebKit/Tools/Scripts/webkitpy/common/multiprocessing_bootstrap.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/multiprocessing_bootstrap.py
@@ -57,20 +57,9 @@ def run(*parts):
     proc = subprocess.Popen(cmd, env=env)
     try:
         proc.wait()
-        # Temporary logging to try and diagnose hangs on the Windows bots.
-        if 'win_chromium_rel_ng' in sys.argv:
-            print 'proc.wait completed.'
     except KeyboardInterrupt:
         # We need a second wait in order to make sure the subprocess exits fully.
         # FIXME: It would be nice if we could put a timeout on this.
         proc.wait()
 
-    try:
-        # Temporary logging to try and diagnose hangs on the Windows bots.
-        if 'win_chromium_rel_ng' in sys.argv:
-            print 'sys.exit starting'
-        sys.exit(proc.returncode)
-    finally:
-        # Temporary logging to try and diagnose hangs on the Windows bots.
-        if 'win_chromium_rel_ng' in sys.argv:
-            print 'sys.exit completed'
+    sys.exit(proc.returncode)
