17ef576fdf0e7ff20dfa382aaf2c0906bd9f836b
Ethan Kuefner <eakuefner@chromium.org>
2017-08-14 20:56:42 +0000

    Add --isolated-script-test-perf-output flag to test harnesses
    In preparation for renaming the isolated-script-test-chartjson-output flag
    that Swarming passes to test harnesses, we add this argument to all harnesses
    in src/. This is a two-sided patch, so the next piece will rename the arg on
    the swarming side, and then we can remove all references on the src side to
    the old name.
    
    Doing the rename on the swarming side may be blocked on reconciling a few
    cases that this CL leaves (like chartjson being required in cases where it
    should not be, or harnesses needing to temporarily check for the presence of
    at most one arg), but in the interest of simplicity, this CL only does the
    add.
    
    Bug: 744736
    Change-Id: Ic37a53a8f82e33ab1c04f291f42408ab048dbb4e
    Reviewed-on: https://chromium-review.googlesource.com/587291
    Commit-Queue: Ethan Kuefner <eakuefner@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Emily Hanley <eyaich@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494183}
    Former-commit-id: 9d00951723b85c2085613c6655023f5a094812d5

--- testing/scripts/run_telemetry_benchmark_as_googletest.py
+++ testing/scripts/run_telemetry_benchmark_as_googletest.py
@@ -46,6 +46,8 @@ def main():
       required=True)
   parser.add_argument(
       '--isolated-script-test-chartjson-output', required=False)
+  parser.add_argument(
+      '--isolated-script-test-perf-output', required=False)
   parser.add_argument('--xvfb', help='Start xvfb.', action='store_true')
   args, rest_args = parser.parse_known_args()
   env = os.environ.copy()
