f80c63860c877d20399441989e0a07a51f98ec72
Eric Stevenson <estevenson@chromium.org>
2017-08-14 19:27:59 +0000

    Revert "Upload component versions to Crash and UMA."
    This reverts commit 7bece1aebb0ef054f08943a9d091b3aebabd8d30 [formerly 404c74d7436c836356f0803afad8c9c63fa3c0a1].
    
    Reason for revert: Adds static initializer on Android (crbug/755254).
    
    Original change's description:
    > Upload component versions to Crash and UMA.
    > 
    > Bug: 751787
    > Change-Id: Ie3f3c78a009edbdabda09b73ee3b03754ff60a8e
    > Reviewed-on: https://chromium-review.googlesource.com/598649
    > Commit-Queue: Joshua Pawlicki <waffles@chromium.org>
    > Reviewed-by: Alexei Svitkine (very slow) <asvitkine@chromium.org>
    > Reviewed-by: Sorin Jianu <sorin@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#494091}
    
    TBR=asvitkine@chromium.org,sorin@chromium.org,waffles@chromium.org
    
    Change-Id: Ib3b9b8c350dab8f6e2c1104d5f77fd8e64f5c041
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 751787
    Reviewed-on: https://chromium-review.googlesource.com/614241
    Reviewed-by: Eric Stevenson <estevenson@chromium.org>
    Commit-Queue: Eric Stevenson <estevenson@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494132}
    Former-commit-id: 8d7260b2352d7e5539160a015e62d828e4b3278e

--- components/component_updater/component_updater_service_internal.h
+++ components/component_updater/component_updater_service_internal.h
@@ -48,7 +48,6 @@ class CrxUpdateService : public ComponentUpdateService,
   std::vector<std::string> GetComponentIDs() const override;
   std::unique_ptr<ComponentInfo> GetComponentForMimeType(
       const std::string& id) const override;
-  std::vector<ComponentInfo> GetComponents() const override;
   OnDemandUpdater& GetOnDemandUpdater() override;
   void MaybeThrottle(const std::string& id,
                      const base::Closure& callback) override;
