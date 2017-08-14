a5ec734ec2fb34dc367278e512f364a3c22cf0b6
Maks Orlovich <morlovich@chromium.org>
2017-08-14 19:59:56 +0000

    Migrate blob storage away from deprecated CreateCacheBackend variant
    It is no longer necessary to pass in a thread (and it was not used for
    anything when using net::MEMORY_CACHE anyway), and I plan to remove
    the variant that permits one to.
    
    Bug: 747020
    Change-Id: I6f6cc3e5907a2da625fa3b1d7fce565dadec71c7
    Reviewed-on: https://chromium-review.googlesource.com/613960
    Reviewed-by: Daniel Murphy <dmurph@chromium.org>
    Commit-Queue: Maks Orlovich <morlovich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494149}
    Former-commit-id: 8342b9d1a124b1990b6b94cc41bf7d9d3afe0d34

--- storage/browser/blob/blob_storage_context_unittest.cc
+++ storage/browser/blob/blob_storage_context_unittest.cc
@@ -63,11 +63,9 @@ class EmptyDataHandle : public storage::BlobDataBuilder::DataHandle {
 std::unique_ptr<disk_cache::Backend> CreateInMemoryDiskCache() {
   std::unique_ptr<disk_cache::Backend> cache;
   net::TestCompletionCallback callback;
-  int rv = disk_cache::CreateCacheBackend(net::MEMORY_CACHE,
-                                          net::CACHE_BACKEND_DEFAULT,
-                                          base::FilePath(), 0,
-                                          false, nullptr, nullptr, &cache,
-                                          callback.callback());
+  int rv = disk_cache::CreateCacheBackend(
+      net::MEMORY_CACHE, net::CACHE_BACKEND_DEFAULT, base::FilePath(), 0, false,
+      nullptr, &cache, callback.callback());
   EXPECT_EQ(net::OK, callback.GetResult(rv));
 
   return cache;
