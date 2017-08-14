924cfc68240174ed513c0ffd57da81b1f224aa6a
Sadrul Habib Chowdhury <sadrul@chromium.org>
2017-08-14 22:05:10 +0000

    win osk: Make sure to remove observer before it is destroyed.
    It is possible for a new WinScreenKeyboardObserver to be created and
    an old observer to be destroyed, without removing the old observer
    from OnScreenKeyboardDisplayManager. Fix that by always removing the
    old observer first.
    
    BUG=755098
    
    Change-Id: Iefae1cf344be85fd5147e8c11d66b7df8336545f
    Reviewed-on: https://chromium-review.googlesource.com/613520
    Reviewed-by: Ananta Iyengar <ananta@chromium.org>
    Commit-Queue: Sadrul Chowdhury <sadrul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494193}
    Former-commit-id: 16206fe916d4dad7985eb06c83b408742ce362c8

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -773,6 +773,8 @@ void RenderWidgetHostViewAura::FocusedNodeTouched(
       ui::OnScreenKeyboardDisplayManager::GetInstance();
   DCHECK(osk_display_manager);
   if (editable && host_->GetView() && host_->delegate()) {
+    if (keyboard_observer_)
+      osk_display_manager->RemoveObserver(keyboard_observer_.get());
     keyboard_observer_.reset(new WinScreenKeyboardObserver(
         this, location_dips_screen, device_scale_factor_, window_));
     virtual_keyboard_requested_ =
