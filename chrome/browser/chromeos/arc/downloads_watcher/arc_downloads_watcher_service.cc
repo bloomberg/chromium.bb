f4a0cf7abe78cae6d5ec6eaf8fa9ba40d168e791
Shuhei Takahashi <nya@chromium.org>
2017-08-15 10:43:50 +0000

    Update the extension list to watch for Android N.
    Raw images and QuickTime images are added.
    
    Bug: 753524
    Test: unit_tests
    Test: Downloaded .nef image is shown in Media View
    Change-Id: Ifc9b0f5a4fe8ff5280e26a3b565010815ce403e6
    Reviewed-on: https://chromium-review.googlesource.com/615216
    Commit-Queue: Shuhei Takahashi <nya@chromium.org>
    Reviewed-by: Hidehiko Abe <hidehiko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494356}
    Former-commit-id: fc55a9d866626a697c16375a9b2084e309f85a51

--- chrome/browser/chromeos/arc/downloads_watcher/arc_downloads_watcher_service.cc
+++ chrome/browser/chromeos/arc/downloads_watcher/arc_downloads_watcher_service.cc
@@ -7,7 +7,6 @@
 #include <string.h>
 
 #include <algorithm>
-#include <iterator>
 #include <map>
 #include <memory>
 #include <utility>
@@ -47,56 +46,6 @@ const base::FilePath::CharType kAndroidDownloadDir[] =
 // map.
 const base::TimeDelta kBuildTimestampMapDelay =
     base::TimeDelta::FromMilliseconds(1000);
-
-// The set of media file extensions supported by Android MediaScanner.
-// Entries must be lower-case and sorted by lexicographical order for
-// binary search.
-// The current list was taken from aosp-marshmallow version of
-// frameworks/base/media/java/android/media/MediaFile.java.
-const char* kAndroidSupportedMediaExtensions[] = {
-    ".3g2",    // FILE_TYPE_3GPP2, video/3gpp2
-    ".3gp",    // FILE_TYPE_3GPP, video/3gpp
-    ".3gpp",   // FILE_TYPE_3GPP, video/3gpp
-    ".3gpp2",  // FILE_TYPE_3GPP2, video/3gpp2
-    ".aac",    // FILE_TYPE_AAC, audio/aac, audio/aac-adts
-    ".amr",    // FILE_TYPE_AMR, audio/amr
-    ".asf",    // FILE_TYPE_ASF, video/x-ms-asf
-    ".avi",    // FILE_TYPE_AVI, video/avi
-    ".awb",    // FILE_TYPE_AWB, audio/amr-wb
-    ".bmp",    // FILE_TYPE_BMP, image/x-ms-bmp
-    ".fl",     // FILE_TYPE_FL, application/x-android-drm-fl
-    ".gif",    // FILE_TYPE_GIF, image/gif
-    ".imy",    // FILE_TYPE_IMY, audio/imelody
-    ".jpeg",   // FILE_TYPE_JPEG, image/jpeg
-    ".jpg",    // FILE_TYPE_JPEG, image/jpeg
-    ".m4a",    // FILE_TYPE_M4A, audio/mp4
-    ".m4v",    // FILE_TYPE_M4V, video/mp4
-    ".mid",    // FILE_TYPE_MID, audio/midi
-    ".midi",   // FILE_TYPE_MID, audio/midi
-    ".mka",    // FILE_TYPE_MKA, audio/x-matroska
-    ".mkv",    // FILE_TYPE_MKV, video/x-matroska
-    ".mp3",    // FILE_TYPE_MP3, audio/mpeg
-    ".mp4",    // FILE_TYPE_MP4, video/mp4
-    ".mpeg",   // FILE_TYPE_MP4, video/mpeg, video/mp2p
-    ".mpg",    // FILE_TYPE_MP4, video/mpeg, video/mp2p
-    ".mpga",   // FILE_TYPE_MP3, audio/mpeg
-    ".mxmf",   // FILE_TYPE_MID, audio/midi
-    ".oga",    // FILE_TYPE_OGG, application/ogg
-    ".ogg",    // FILE_TYPE_OGG, audio/ogg, application/ogg
-    ".ota",    // FILE_TYPE_MID, audio/midi
-    ".png",    // FILE_TYPE_PNG, image/png
-    ".rtttl",  // FILE_TYPE_MID, audio/midi
-    ".rtx",    // FILE_TYPE_MID, audio/midi
-    ".smf",    // FILE_TYPE_SMF, audio/sp-midi
-    ".ts",     // FILE_TYPE_MP2TS, video/mp2ts
-    ".wav",    // FILE_TYPE_WAV, audio/x-wav
-    ".wbmp",   // FILE_TYPE_WBMP, image/vnd.wap.wbmp
-    ".webm",   // FILE_TYPE_WEBM, video/webm
-    ".webp",   // FILE_TYPE_WEBP, image/webp
-    ".wma",    // FILE_TYPE_WMA, audio/x-ms-wma
-    ".wmv",    // FILE_TYPE_WMV, video/x-ms-wmv
-    ".xmf",    // FILE_TYPE_MID, audio/midi
-};
 
 // Compares two TimestampMaps and returns the list of file paths added/removed
 // or whose timestamp have changed.
@@ -190,19 +139,89 @@ class ArcDownloadsWatcherServiceFactory
 
 }  // namespace
 
+// The set of media file extensions supported by Android MediaScanner.
+// Entries must be lower-case and sorted by lexicographical order for
+// binary search.
+//
+// The current list was taken from aosp-nougat version of
+// frameworks/base/media/java/android/media/MediaFile.java, and included
+// file type categories are:
+// - Audio
+// - MIDI
+// - Video
+// - Image
+// - Raw image
+const char* kAndroidSupportedMediaExtensions[] = {
+    ".3g2",    // FILE_TYPE_3GPP2, video/3gpp2
+    ".3gp",    // FILE_TYPE_3GPP, video/3gpp
+    ".3gpp",   // FILE_TYPE_3GPP, video/3gpp
+    ".3gpp2",  // FILE_TYPE_3GPP2, video/3gpp2
+    ".aac",    // FILE_TYPE_AAC, audio/aac, audio/aac-adts
+    ".amr",    // FILE_TYPE_AMR, audio/amr
+    ".arw",    // FILE_TYPE_ARW, image/x-sony-arw
+    ".asf",    // FILE_TYPE_ASF, video/x-ms-asf
+    ".avi",    // FILE_TYPE_AVI, video/avi
+    ".awb",    // FILE_TYPE_AWB, audio/amr-wb
+    ".bmp",    // FILE_TYPE_BMP, image/x-ms-bmp
+    ".cr2",    // FILE_TYPE_CR2, image/x-canon-cr2
+    ".dng",    // FILE_TYPE_DNG, image/x-adobe-dng
+    ".fl",     // FILE_TYPE_FL, application/x-android-drm-fl
+    ".gif",    // FILE_TYPE_GIF, image/gif
+    ".imy",    // FILE_TYPE_IMY, audio/imelody
+    ".jpeg",   // FILE_TYPE_JPEG, image/jpeg
+    ".jpg",    // FILE_TYPE_JPEG, image/jpeg
+    ".m4a",    // FILE_TYPE_M4A, audio/mp4
+    ".m4v",    // FILE_TYPE_M4V, video/mp4
+    ".mid",    // FILE_TYPE_MID, audio/midi
+    ".midi",   // FILE_TYPE_MID, audio/midi
+    ".mka",    // FILE_TYPE_MKA, audio/x-matroska
+    ".mkv",    // FILE_TYPE_MKV, video/x-matroska
+    ".mov",    // FILE_TYPE_QT, video/quicktime
+    ".mp3",    // FILE_TYPE_MP3, audio/mpeg
+    ".mp4",    // FILE_TYPE_MP4, video/mp4
+    ".mpeg",   // FILE_TYPE_MP4, video/mpeg, video/mp2p
+    ".mpg",    // FILE_TYPE_MP4, video/mpeg, video/mp2p
+    ".mpga",   // FILE_TYPE_MP3, audio/mpeg
+    ".mxmf",   // FILE_TYPE_MID, audio/midi
+    ".nef",    // FILE_TYPE_NEF, image/x-nikon-nef
+    ".nrw",    // FILE_TYPE_NRW, image/x-nikon-nrw
+    ".oga",    // FILE_TYPE_OGG, application/ogg
+    ".ogg",    // FILE_TYPE_OGG, audio/ogg, application/ogg
+    ".orf",    // FILE_TYPE_ORF, image/x-olympus-orf
+    ".ota",    // FILE_TYPE_MID, audio/midi
+    ".pef",    // FILE_TYPE_PEF, image/x-pentax-pef
+    ".png",    // FILE_TYPE_PNG, image/png
+    ".raf",    // FILE_TYPE_RAF, image/x-fuji-raf
+    ".rtttl",  // FILE_TYPE_MID, audio/midi
+    ".rtx",    // FILE_TYPE_MID, audio/midi
+    ".rw2",    // FILE_TYPE_RW2, image/x-panasonic-rw2
+    ".smf",    // FILE_TYPE_SMF, audio/sp-midi
+    ".srw",    // FILE_TYPE_SRW, image/x-samsung-srw
+    ".ts",     // FILE_TYPE_MP2TS, video/mp2ts
+    ".wav",    // FILE_TYPE_WAV, audio/x-wav
+    ".wbmp",   // FILE_TYPE_WBMP, image/vnd.wap.wbmp
+    ".webm",   // FILE_TYPE_WEBM, video/webm
+    ".webp",   // FILE_TYPE_WEBP, image/webp
+    ".wma",    // FILE_TYPE_WMA, audio/x-ms-wma
+    ".wmv",    // FILE_TYPE_WMV, video/x-ms-wmv
+    ".xmf",    // FILE_TYPE_MID, audio/midi
+};
+const int kAndroidSupportedMediaExtensionsSize =
+    arraysize(kAndroidSupportedMediaExtensions);
+
 bool HasAndroidSupportedMediaExtension(const base::FilePath& path) {
   const std::string extension = base::ToLowerASCII(path.Extension());
   const auto less_comparator = [](const char* a, const char* b) {
     return strcmp(a, b) < 0;
   };
-  DCHECK(std::is_sorted(std::begin(kAndroidSupportedMediaExtensions),
-                        std::end(kAndroidSupportedMediaExtensions),
-                        less_comparator));
-  auto** iter = std::lower_bound(std::begin(kAndroidSupportedMediaExtensions),
-                                 std::end(kAndroidSupportedMediaExtensions),
-                                 extension.c_str(), less_comparator);
-  return iter != std::end(kAndroidSupportedMediaExtensions) &&
-         strcmp(*iter, extension.c_str()) == 0;
+  DCHECK(std::is_sorted(
+      kAndroidSupportedMediaExtensions,
+      kAndroidSupportedMediaExtensions + kAndroidSupportedMediaExtensionsSize,
+      less_comparator));
+  return std::binary_search(
+      kAndroidSupportedMediaExtensions,
+      kAndroidSupportedMediaExtensions + kAndroidSupportedMediaExtensionsSize,
+      extension.c_str(), less_comparator);
 }
 
 // The core part of ArcDownloadsWatcherService to watch for file changes in
