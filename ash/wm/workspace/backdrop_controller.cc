0aa333324ff4a653263045f9d37ce589fcc24480
MinChen <minch@chromium.org>
2017-08-15 17:48:03 +0000

    Disable backdrop if fullscreen app list is opened in tablet mode.
    If fullscreen app list is opened, the shelf should change to be transparent.
    But since there is a fullscreen backdrop window in tablet mode. The shelf area
    is still black after app list is opened.
    
    Change:
    Add OnAppListVisibilityChanged(*) in BackdropController. Hide backdrop if app list
    is opened, update it if app list is dismissed.
    
    Bug: 752383
    Change-Id: Ic1be66eb5cb9b0a4d126d8e8d78aa2f229b824b8
    Reviewed-on: https://chromium-review.googlesource.com/606706
    Commit-Queue: min c <minch@chromium.org>
    Reviewed-by: Robert Flack <flackr@chromium.org>
    Reviewed-by: Alex Newcomer <newcomer@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494437}
    Former-commit-id: 55709dd24edad7c026e0ba7ecdb6a9b4b196894c

--- ash/wm/workspace/backdrop_controller.cc
+++ ash/wm/workspace/backdrop_controller.cc
@@ -9,11 +9,13 @@
 #include "ash/public/cpp/shell_window_ids.h"
 #include "ash/shell.h"
 #include "ash/system/tray/system_tray_notifier.h"
+#include "ash/wm/tablet_mode/tablet_mode_controller.h"
 #include "ash/wm/window_util.h"
 #include "ash/wm/workspace/backdrop_delegate.h"
 #include "base/auto_reset.h"
 #include "base/memory/ptr_util.h"
 #include "chromeos/audio/chromeos_sounds.h"
+#include "ui/app_list/app_list_features.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
@@ -106,7 +108,7 @@ void BackdropController::SetBackdropDelegate(
 
 void BackdropController::UpdateBackdrop() {
   // Avoid recursive calls.
-  if (in_restacking_ || force_hidden_)
+  if (in_restacking_ || force_hidden_counter_)
     return;
 
   aura::Window* window = GetTopmostWindowWithBackdrop();
@@ -136,26 +138,39 @@ void BackdropController::UpdateBackdrop() {
 }
 
 void BackdropController::OnOverviewModeStarting() {
-  force_hidden_ = true;
-  Hide();
+  AddForceHidden();
 }
 
 void BackdropController::OnOverviewModeEnded() {
-  if (Shell::Get()->IsSplitViewModeActive())
-    return;
-
-  force_hidden_ = false;
-  UpdateBackdrop();
+  RemoveForceHidden();
 }
 
 void BackdropController::OnSplitViewModeStarting() {
-  force_hidden_ = true;
-  Hide();
+  AddForceHidden();
 }
 
 void BackdropController::OnSplitViewModeEnded() {
-  force_hidden_ = false;
-  UpdateBackdrop();
+  RemoveForceHidden();
+}
+
+void BackdropController::OnAppListVisibilityChanged(bool shown,
+                                                    aura::Window* root_window) {
+  // Ignore the notification if it is not for this display.
+  if (container_->GetRootWindow() != root_window)
+    return;
+
+  // Hide or update backdrop only for fullscreen app list in tablet mode.
+  if (!app_list::features::IsFullscreenAppListEnabled() ||
+      !Shell::Get()
+           ->tablet_mode_controller()
+           ->IsTabletModeWindowManagerEnabled()) {
+    return;
+  }
+
+  if (shown)
+    AddForceHidden();
+  else
+    RemoveForceHidden();
 }
 
 void BackdropController::OnAccessibilityModeChanged(
@@ -249,6 +264,24 @@ void BackdropController::Hide() {
   backdrop_window_ = nullptr;
   original_event_handler_ = nullptr;
   backdrop_event_handler_.reset();
+}
+
+void BackdropController::AddForceHidden() {
+  force_hidden_counter_++;
+  CHECK_GE(force_hidden_counter_, 0);
+  if (force_hidden_counter_)
+    Hide();
+  else
+    UpdateBackdrop();
+}
+
+void BackdropController::RemoveForceHidden() {
+  force_hidden_counter_--;
+  CHECK_GE(force_hidden_counter_, 0);
+  if (force_hidden_counter_)
+    Hide();
+  else
+    UpdateBackdrop();
 }
 
 }  // namespace ash
