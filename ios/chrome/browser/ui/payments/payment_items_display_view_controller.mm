0c7f142ebcf0e9d15e1428c0714c9e389b397c33
Mohamad Ahmadi <mahmadi@chromium.org>
2017-08-15 16:45:28 +0000

    [Payment Request] Fixes the black background bug for the MDC buttons.
    - The documentation for the MDCButton states that if the backgroundColor
      is unset, it gets a "blue" default background color. The implementation
      was later updated to use "black" without updating the documentation:
      https://github.com/material-components/material-components-ios/pull/1609
    
    Bug: 602666
    Change-Id: Idbe07388591541c692a630371ac40c1608f349f0
    Reviewed-on: https://chromium-review.googlesource.com/614581
    Reviewed-by: Rouslan Solomakhin <rouslan@chromium.org>
    Commit-Queue: mahmadi (Moe) <mahmadi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494412}
    Former-commit-id: f4d90f1e136d6def1ea88f0cc8e3c8eb4c924927

--- ios/chrome/browser/ui/payments/payment_items_display_view_controller.mm
+++ ios/chrome/browser/ui/payments/payment_items_display_view_controller.mm
@@ -10,6 +10,7 @@
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_item+collection_view_controller.h"
 #import "ios/chrome/browser/ui/collection_view/cells/collection_view_item.h"
 #import "ios/chrome/browser/ui/collection_view/collection_view_model.h"
+#import "ios/chrome/browser/ui/colors/MDCPalette+CrAdditions.h"
 #import "ios/chrome/browser/ui/icons/chrome_icon.h"
 #import "ios/chrome/browser/ui/payments/cells/price_item.h"
 #import "ios/chrome/browser/ui/payments/payment_items_display_view_controller_actions.h"
@@ -74,6 +75,7 @@ typedef NS_ENUM(NSInteger, ItemType) {
     _payButton = [[MDCButton alloc] init];
     [_payButton setTitle:l10n_util::GetNSString(IDS_PAYMENTS_PAY_BUTTON)
                 forState:UIControlStateNormal];
+    [_payButton setBackgroundColor:[[MDCPalette cr_bluePalette] tint500]];
     [_payButton setTitleColor:[UIColor whiteColor]
                      forState:UIControlStateNormal];
     [_payButton setInkColor:[UIColor colorWithWhite:1 alpha:0.2]];
