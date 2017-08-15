e2cfbcfebfe6bef0ce03be4a14db9604f1bd8603
Andrew Grieve <agrieve@chromium.org>
2017-08-15 19:45:08 +0000

    Delete CrashTest.java since IntentionalCrashTest.java already exists
    Change-Id: I5446ef3814e48ab8f35078a2a9ac13886b6ffd1d
    Reviewed-on: https://chromium-review.googlesource.com/614140
    Reviewed-by: Yaron Friedman <yfriedman@chromium.org>
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494503}
    Former-commit-id: e11da440da1af1e639d5a24ac046426e14c474f4

--- chrome/android/javatests/src/org/chromium/chrome/test/crash/IntentionalCrashTest.java
+++ chrome/android/javatests/src/org/chromium/chrome/test/crash/IntentionalCrashTest.java
@@ -4,6 +4,8 @@
 
 package org.chromium.chrome.test.crash;
 
+import android.support.test.filters.SmallTest;
+
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
@@ -28,6 +30,7 @@ public class IntentionalCrashTest {
             new ChromeActivityTestRule<>(ChromeActivity.class);
 
     @DisabledTest
+    @SmallTest
     @Test
     public void testRendererCrash() {
         try {
@@ -38,6 +41,7 @@ public class IntentionalCrashTest {
     }
 
     @DisabledTest
+    @SmallTest
     @Test
     public void testBrowserCrash() {
         try {
@@ -48,6 +52,7 @@ public class IntentionalCrashTest {
     }
 
     @DisabledTest
+    @SmallTest
     @Test
     public void testJavaCrash() {
         try {
@@ -58,6 +63,7 @@ public class IntentionalCrashTest {
     }
 
     @DisabledTest
+    @SmallTest
     @Test
     public void testGpuCrash() {
         try {
