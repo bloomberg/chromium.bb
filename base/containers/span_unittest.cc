0414c0a00834a3f80b633dce22e67703f5359b81
Daniel Cheng <dcheng@chromium.org>
2017-08-15 19:04:26 +0000

    Add type-deducing MakeSpan helper.
    Also implements:
    - iterator, begin(), end()
    - const_iterator, cbegin(), cend()
    - operator[](size_t)
    - operator==(const Span<T>&, const Span<T>&) and
      operator!=(const Span<T>&, const Span<T>&)
    to make it less painful to write tests.
    
    Bug: 754077
    Change-Id: Ie1165566177b865f0b9e829d3dfea99105e5476b
    Reviewed-on: https://chromium-review.googlesource.com/611852
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494476}
    Former-commit-id: 14923dda9c11a13645cc35e0efaf61ead5f87a1a

--- base/containers/span_unittest.cc
+++ base/containers/span_unittest.cc
@@ -7,7 +7,10 @@
 #include <vector>
 
 #include "base/macros.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+using ::testing::ElementsAre;
 
 namespace base {
 
@@ -20,9 +23,8 @@ TEST(SpanTest, ConstructFromDataAndSize) {
   EXPECT_EQ(vector.data(), span.data());
   EXPECT_EQ(vector.size(), span.size());
 
-  // TODO(dcheng): Use operator[] when implemented.
   for (size_t i = 0; i < span.size(); ++i)
-    EXPECT_EQ(vector[i], span.data()[i]);
+    EXPECT_EQ(vector[i], span[i]);
 }
 
 TEST(SpanTest, ConstructFromConstexprArray) {
@@ -32,9 +34,8 @@ TEST(SpanTest, ConstructFromConstexprArray) {
   EXPECT_EQ(kArray, span.data());
   EXPECT_EQ(arraysize(kArray), span.size());
 
-  // TODO(dcheng): Use operator[] when implemented.
   for (size_t i = 0; i < span.size(); ++i)
-    EXPECT_EQ(kArray[i], span.data()[i]);
+    EXPECT_EQ(kArray[i], span[i]);
 }
 
 TEST(SpanTest, ConstructFromArray) {
@@ -43,16 +44,14 @@ TEST(SpanTest, ConstructFromArray) {
   Span<const int> const_span(array);
   EXPECT_EQ(array, const_span.data());
   EXPECT_EQ(arraysize(array), const_span.size());
-  // TODO(dcheng): Use operator[] when implemented.
   for (size_t i = 0; i < const_span.size(); ++i)
-    EXPECT_EQ(array[i], const_span.data()[i]);
+    EXPECT_EQ(array[i], const_span[i]);
 
   Span<int> span(array);
   EXPECT_EQ(array, span.data());
   EXPECT_EQ(arraysize(array), span.size());
-  // TODO(dcheng): Use operator[] when implemented.
   for (size_t i = 0; i < span.size(); ++i)
-    EXPECT_EQ(array[i], span.data()[i]);
+    EXPECT_EQ(array[i], span[i]);
 }
 
 TEST(SpanTest, Subspan) {
@@ -81,47 +80,97 @@ TEST(SpanTest, Subspan) {
     auto subspan = span.subspan(0, 1);
     EXPECT_EQ(span.data(), subspan.data());
     EXPECT_EQ(1u, subspan.size());
-    EXPECT_EQ(1, subspan.data()[0]);
+    EXPECT_EQ(1, subspan[0]);
   }
 
   {
     auto subspan = span.subspan(1, 1);
     EXPECT_EQ(span.data() + 1, subspan.data());
     EXPECT_EQ(1u, subspan.size());
-    EXPECT_EQ(2, subspan.data()[0]);
+    EXPECT_EQ(2, subspan[0]);
   }
 
   {
     auto subspan = span.subspan(2, 1);
     EXPECT_EQ(span.data() + 2, subspan.data());
     EXPECT_EQ(1u, subspan.size());
-    EXPECT_EQ(3, subspan.data()[0]);
+    EXPECT_EQ(3, subspan[0]);
   }
 
   {
     auto subspan = span.subspan(0, 2);
     EXPECT_EQ(span.data(), subspan.data());
     EXPECT_EQ(2u, subspan.size());
-    EXPECT_EQ(1, subspan.data()[0]);
-    EXPECT_EQ(2, subspan.data()[1]);
+    EXPECT_EQ(1, subspan[0]);
+    EXPECT_EQ(2, subspan[1]);
   }
 
   {
     auto subspan = span.subspan(1, 2);
     EXPECT_EQ(span.data() + 1, subspan.data());
     EXPECT_EQ(2u, subspan.size());
-    EXPECT_EQ(2, subspan.data()[0]);
-    EXPECT_EQ(3, subspan.data()[1]);
+    EXPECT_EQ(2, subspan[0]);
+    EXPECT_EQ(3, subspan[1]);
   }
 
   {
     auto subspan = span.subspan(0, 3);
     EXPECT_EQ(span.data(), subspan.data());
     EXPECT_EQ(span.size(), subspan.size());
-    EXPECT_EQ(1, subspan.data()[0]);
-    EXPECT_EQ(2, subspan.data()[1]);
-    EXPECT_EQ(3, subspan.data()[2]);
+    EXPECT_EQ(1, subspan[0]);
+    EXPECT_EQ(2, subspan[1]);
+    EXPECT_EQ(3, subspan[2]);
   }
+}
+
+TEST(SpanTest, Iterator) {
+  static constexpr int kArray[] = {1, 6, 1, 8, 0};
+  constexpr Span<const int> span(kArray);
+
+  std::vector<int> results;
+  for (int i : span)
+    results.emplace_back(i);
+  EXPECT_THAT(results, ElementsAre(1, 6, 1, 8, 0));
+}
+
+TEST(SpanTest, Equality) {
+  static constexpr int kArray1[] = {3, 1, 4, 1, 5};
+  static constexpr int kArray2[] = {3, 1, 4, 1, 5};
+  constexpr Span<const int> span1(kArray1);
+  constexpr Span<const int> span2(kArray2);
+
+  EXPECT_EQ(span1, span2);
+
+  static constexpr int kArray3[] = {2, 7, 1, 8, 3};
+  constexpr Span<const int> span3(kArray3);
+
+  EXPECT_FALSE(span1 == span3);
+}
+
+TEST(SpanTest, Inequality) {
+  static constexpr int kArray1[] = {2, 3, 5, 7, 11};
+  static constexpr int kArray2[] = {1, 4, 6, 8, 9};
+  constexpr Span<const int> span1(kArray1);
+  constexpr Span<const int> span2(kArray2);
+
+  EXPECT_NE(span1, span2);
+
+  static constexpr int kArray3[] = {2, 3, 5, 7, 11};
+  constexpr Span<const int> span3(kArray3);
+
+  EXPECT_FALSE(span1 != span3);
+}
+
+TEST(SpanTest, MakeSpanFromDataAndSize) {
+  std::vector<int> vector = {1, 1, 2, 3, 5, 8};
+  Span<int> span(vector.data(), vector.size());
+  EXPECT_EQ(span, MakeSpan(vector.data(), vector.size()));
+}
+
+TEST(SpanTest, MakeSpanFromConstexprArray) {
+  static constexpr int kArray[] = {1, 2, 3, 4, 5};
+  constexpr Span<const int> span(kArray);
+  EXPECT_EQ(span, MakeSpan(kArray));
 }
 
 }  // namespace base
