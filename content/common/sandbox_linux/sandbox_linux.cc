6f3bb1ba3094ea805755d87998469ae6d77e278b
Tom Sepez <tsepez@chromium.org>
2017-08-15 23:12:23 +0000

    Use common SandboxTypeFromCommandLine() on linux
    Split off from https://chromium-review.googlesource.com/c/583586
    
    TBR=jam@chromium.org
    
    Change-Id: I439784931ec3cbbd2892e9161970e8e9e147af02
    Reviewed-on: https://chromium-review.googlesource.com/616101
    Reviewed-by: Jorge Lucangeli Obes <jorgelo@chromium.org>
    Reviewed-by: Tom Sepez <tsepez@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494597}
    Former-commit-id: 36e13fc549366c4e1efbff556c4e5071d980a047

--- content/common/sandbox_linux/sandbox_linux.cc
+++ content/common/sandbox_linux/sandbox_linux.cc
@@ -37,6 +37,7 @@
 #include "content/public/common/content_features.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/sandbox_linux.h"
+#include "content/public/common/sandbox_type.h"
 #include "sandbox/linux/services/credentials.h"
 #include "sandbox/linux/services/namespace_sandbox.h"
 #include "sandbox/linux/services/proc_util.h"
@@ -383,9 +384,8 @@ bool LinuxSandbox::LimitAddressSpace(const std::string& process_type) {
   (void) process_type;
 #if !defined(ANY_OF_AMTLU_SANITIZER)
   base::CommandLine* command_line = base::CommandLine::ForCurrentProcess();
-  if (command_line->HasSwitch(switches::kNoSandbox)) {
+  if (SandboxTypeFromCommandLine(*command_line) == SANDBOX_TYPE_NO_SANDBOX)
     return false;
-  }
 
   // Limit the address space to 4GB.
   // This is in the hope of making some kernel exploits more complex and less
