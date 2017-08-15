cacc930a3fa000cac5ecf4e520499ee435aa5505
Chris Mumford <cmumford@chromium.org>
2017-08-15 21:43:00 +0000

    Revert "[tracing] Memory dump provider for leveldb_proto"
    Reporting to memory-infra was added when DBTracker was introduced
    in #483507. Reverting tracing to eliminate double-counting.
    
    This reverts commit ebde066d6b0829d4ae51ab227a61d3e412c8508f [formerly e4aba36676c616489620442d8ae6aa88931e680c].
    
    Bug: 735269
    Change-Id: Ie20015f628a393dc3e540864006c622ea8d2c12c
    Reviewed-on: https://chromium-review.googlesource.com/612361
    Reviewed-by: Tommy Nyquist <nyquist@chromium.org>
    Commit-Queue: Chris Mumford <cmumford@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494559}
    Former-commit-id: ae996c1b408b38064eb44b61f5f41b79cb0c6942

--- components/leveldb_proto/leveldb_database.h
+++ components/leveldb_proto/leveldb_database.h
@@ -13,7 +13,6 @@
 #include "base/macros.h"
 #include "base/strings/string_split.h"
 #include "base/threading/thread_collision_warner.h"
-#include "base/trace_event/memory_dump_provider.h"
 #include "components/leveldb_proto/options.h"
 #include "third_party/leveldatabase/env_chromium.h"
 
@@ -34,14 +33,14 @@ namespace leveldb_proto {
 // Interacts with the LevelDB third party module.
 // Once constructed, function calls and destruction should all occur on the
 // same thread (not necessarily the same as the constructor).
-class LevelDB : public base::trace_event::MemoryDumpProvider {
+class LevelDB {
  public:
   // Constructor. Does *not* open a leveldb - only initialize this class.
   // |client_name| is the name of the "client" that owns this instance. Used
   // for UMA statics as so: LevelDB.<value>.<client name>. It is best to not
   // change once shipped.
   explicit LevelDB(const char* client_name);
-  ~LevelDB() override;
+  virtual ~LevelDB();
 
   // Initializes a leveldb with the given options. If |options.database_dir| is
   // empty, this opens an in-memory db.
@@ -54,10 +53,6 @@ class LevelDB : public base::trace_event::MemoryDumpProvider {
   virtual bool Get(const std::string& key, bool* found, std::string* entry);
 
   static bool Destroy(const base::FilePath& database_dir);
-
-  // base::trace_event::MemoryDumpProvider implementation.
-  bool OnMemoryDump(const base::trace_event::MemoryDumpArgs& dump_args,
-                    base::trace_event::ProcessMemoryDump* pmd) override;
 
  protected:
   virtual bool InitWithOptions(const base::FilePath& database_dir,
@@ -74,8 +69,6 @@ class LevelDB : public base::trace_event::MemoryDumpProvider {
   std::unique_ptr<leveldb::Cache> custom_block_cache_;
   std::unique_ptr<leveldb::DB> db_;
   base::HistogramBase* open_histogram_;
-  // Name of the client shown in chrome://tracing.
-  std::string client_name_;
 
   DISALLOW_COPY_AND_ASSIGN(LevelDB);
 };
