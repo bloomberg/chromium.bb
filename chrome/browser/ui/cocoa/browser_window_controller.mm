9a708b40abb091f7ea11910a4ce84b35ee9a437b
spqchan <spqchan@chromium.org>
2017-08-15 01:29:28 +0000

    [Mac] Remove Excessive Touch Bar Invalidates
    The touch bar is updated with every page load, so that
    the stop/reload button would have the proper state.
    
    However, this causes the search button to blink frequently
    in some hardware. To fix this issue, remove the excessive
    invalidates and update the stop/reload item independently.
    
    Bug: 749424
    Change-Id: I02c5dd3ea8ddf978ecab6a3e5297df8d578939c2
    Reviewed-on: https://chromium-review.googlesource.com/612694
    Commit-Queue: Sarah Chan <spqchan@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494292}
    Former-commit-id: ccfaf09a251e8a7c752638b07ae9dd1a37156889

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -1064,8 +1064,10 @@ bool IsTabDetachingInFullscreenEnabled() {
 - (void)setStarredState:(BOOL)isStarred {
   [toolbarController_ setStarredState:isStarred];
 
-  [touchBar_ setIsStarred:isStarred];
-  [self invalidateTouchBar];
+  if ([touchBar_ isStarred] != isStarred) {
+    [touchBar_ setIsStarred:isStarred];
+    [self invalidateTouchBar];
+  }
 }
 
 - (void)setCurrentPageIsTranslated:(BOOL)on {
@@ -1206,7 +1208,6 @@ bool IsTabDetachingInFullscreenEnabled() {
 - (void)setIsLoading:(BOOL)isLoading force:(BOOL)force {
   [toolbarController_ setIsLoading:isLoading force:force];
   [touchBar_ setIsPageLoading:isLoading];
-  [self invalidateTouchBar];
 }
 
 // Make the location bar the first responder, if possible.
