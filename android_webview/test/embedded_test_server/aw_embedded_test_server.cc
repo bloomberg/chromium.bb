7d5b81e653ad1d3445a26979ff6aa32785e7a392
Shimi Zhang <ctzsm@chromium.org>
2017-08-15 20:49:18 +0000

    aw: Cleanup after removal of RegisterNativesImpl()
    Since we removed RegisterNativesImpl() calls in aw, code
    under /test/embedded_test_server/ changed a lot:
    
    aw_test_jni_onload.{cc,h} contains only one function calling
    base::android::OnJNIOnLoadInit(), so delete these files and
    call OnJNIOnLoadInit() from aw_test_entry_point.cc.
    
    aw_embedded_test_server.h is unnecessary now, so remove it.
    
    Dependencies of net/test/android/net_test_jni_onload.{cc,h}
    are obsoleted, remove them from BUILD.gn.
    
    Bug: 749244
    Change-Id: I6bca95557a8874865404af29aac8b633afca9a85
    Reviewed-on: https://chromium-review.googlesource.com/614862
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Shimi Zhang <ctzsm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494533}
    Former-commit-id: 3818f4da92f57db7212f9e9dc38b470cf47b605b

--- android_webview/test/embedded_test_server/aw_embedded_test_server.cc
+++ android_webview/test/embedded_test_server/aw_embedded_test_server.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "android_webview/test/embedded_test_server/aw_embedded_test_server.h"
-
 #include <jni.h>
 
 #include "android_webview/test/jni/AwEmbeddedTestServerImpl_jni.h"
--- android_webview/test/embedded_test_server/aw_test_entry_point.cc
+++ android_webview/test/embedded_test_server/aw_test_entry_point.cc
@@ -2,14 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "android_webview/test/embedded_test_server/aw_test_jni_onload.h"
+#include "base/android/base_jni_onload.h"
 #include "base/android/jni_android.h"
 
 // This is called by the VM when the shared library is first loaded.
 JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   base::android::InitVM(vm);
-  if (!android_webview::test::OnJNIOnLoadInit()) {
+  if (!base::android::OnJNIOnLoadInit())
     return -1;
-  }
   return JNI_VERSION_1_4;
 }
