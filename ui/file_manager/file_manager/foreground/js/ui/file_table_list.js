447dd5b1d10db7ed8df32607c13fcff16201bc3e
Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
2017-08-15 10:33:06 +0000

    Reset selection and set the right lead index upon long-pressing.
    The lead index must be set so that commands referring it, like rename,
    can work successfully.
    This change also clears the selection before selecting the item, so as
    to make sure existing single selection (which is old lead item) will
    be cleared.
    
    Bug: 754272,754283,754290
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: Ieb051b3b517cc06cd021d9b0feaa56c1e2a73b38
    Reviewed-on: https://chromium-review.googlesource.com/610341
    Reviewed-by: Naoki Fukino <fukino@chromium.org>
    Commit-Queue: Tatsuhisa Yamaguchi <yamaguchi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494354}
    Former-commit-id: e6b3ed78d0c53ca28e9959363132e9d8f4252933

--- ui/file_manager/file_manager/foreground/js/ui/file_table_list.js
+++ ui/file_manager/file_manager/foreground/js/ui/file_table_list.js
@@ -248,9 +248,14 @@ filelist.handleTap = function(e, index, eventType) {
     return true;
   } else if (sm.multiple && eventType == FileTapHandler.TapEvent.LONG_PRESS) {
     sm.beginChange();
-    if (!sm.getIndexSelected(index))
-      sm.setIndexSelected(index, true);
-    sm.setCheckSelectMode(true);
+    if (!sm.getCheckSelectMode()) {
+      // Make sure to unselect the leading item that was not the touch target.
+      sm.unselectAll();
+      sm.setCheckSelectMode(true);
+    }
+    sm.setIndexSelected(index, true);
+    sm.leadIndex = index;
+    sm.anchorIndex = index;
     sm.endChange();
     return true;
     // Do not toggle selection yet, so as to avoid unselecting before drag.
