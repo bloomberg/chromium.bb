79e2a1bb4ee70eaf5725c6a7a78788756fd45de6
Jared Saul <jsaul@google.com>
2017-08-16 20:48:12 +0000

    Upload save UI changes requested by UX targeting M62
    M62 Payments UI copy deck:
    https://docs.google.com/document/d/1e3yVuW6PFFu-2L1hvubN_9cbebywyUXaHqZn9AHH04w
    
    Bug: 736944
    Change-Id: If1481bfd9fc8abfb5f9ec5a1a4cd4ec8250a5694
    Reviewed-on: https://chromium-review.googlesource.com/617266
    Commit-Queue: Jared Saul <jsaul@google.com>
    Reviewed-by: Roger McFarlane <rogerm@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494938}
    Former-commit-id: 88a39e7d492eb3db74d8b160fdc08f6df601c4a4

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -1330,6 +1330,10 @@ void AutofillManager::ImportFormData(const FormStructure& submitted_form) {
       upload_request_.active_experiments.push_back(
           kAutofillUpstreamShowNewUi.name);
     }
+    if (IsAutofillUpstreamShowGoogleLogoExperimentEnabled()) {
+      upload_request_.active_experiments.push_back(
+          kAutofillUpstreamShowGoogleLogo.name);
+    }
 
     // All required data is available, start the upload process.
     payments_client_->GetUploadDetails(upload_request_.profiles,
