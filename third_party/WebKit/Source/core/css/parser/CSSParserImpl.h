2b53362ab5423ada432aec8f47cbf79d7aeaae0c
Darren Shen <shend@chromium.org>
2017-08-16 01:00:23 +0000

    Revert "Revert "[CSSParser] Add Next/Peek for stream parsing of ConsumeDeclarationList.""
    This reverts commit a233529cb1391cbc6bedfb7623a13bcd3abe3a94 [formerly d39de8fbe45814b9b4c75b41f85e472c103f68e8].
    
    Reason for revert: Relanding
    
    Change: Fixed undefined behaviour due to unspecified order of operations with
    side effects (compare Patch 5 with Patch 1).
    
    Original change's description:
    > Revert "[CSSParser] Add Next/Peek for stream parsing of ConsumeDeclarationList."
    > 
    > This reverts commit 9a277bcce441d209a48fad182930d5854ffafb83 [formerly 726787e05f41c9103250fad8b85881544365f77f].
    > 
    > Reason for revert: caused assertion failures in CSSParserImpl.cpp. See Issue 754739.
    > 
    > Original change's description:
    > > [CSSParser] Add Next/Peek for stream parsing of ConsumeDeclarationList.
    > > 
    > > This patch adds Next/Peek methods on CSSParserTokenStream, which
    > > tokenizes on demand. It also offers fast path UncheckedNext/Peek methods
    > > that can be used when we've already called Peek().
    > > 
    > > Using these new features, we can begin to convert CSSParserImpl::
    > > ConsumeDeclarationList to use streams (we add an overload because there
    > > are still other callers that use ranges). Unfortunately, using
    > > CSSParserObservers with streaming requires some refactoring, so for now,
    > > we only call ConsumeDeclarationList when we don't have an observer.
    > > 
    > > Since ConsumeDeclarationList calls lower level parsing functions that
    > > require ranges, we convert streams to ranges using MakeRangeToEOF().
    > > When those lower level parsing functions modify the ranges that they
    > > take, we need to sync those changes with the stream using
    > > UpdatePositionFromRange.
    > > 
    > > Future patches will continue to streamify those lower level parsing
    > > functions, layer by layer.
    > > 
    > > This patch does not change observable parsing behaviour, and should
    > > have minimal performance impact: ranges are inherently faster than
    > > streams, but the unchecked consume/peeks should close the gap.
    > > 
    > > For reviewers:
    > > - CSSParserTokenStream::ConsumeComponentValue is copied from
    > >   CSSParserTokenRange::ConsumeComponentValue.
    > > - The streaming CSSParserTokenStream::ConsumeDeclarationList is adapted
    > >   from the ranges version.
    > > 
    > > Bug: 661854
    > > Change-Id: Id2732f7030d012daf213f963ed0e0506d6eed145
    > > Reviewed-on: https://chromium-review.googlesource.com/597607
    > > Commit-Queue: Darren Shen <shend@chromium.org>
    > > Reviewed-by: meade_UTC10 <meade@chromium.org>
    > > Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#493640}
    > 
    > TBR=meade@chromium.org,shend@chromium.org,bugsnash@chromium.org
    > 
    > Change-Id: If0244556e6746f0cba43148575f0512a190998f5
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: 661854
    > Reviewed-on: https://chromium-review.googlesource.com/612371
    > Reviewed-by: Kenneth Russell <kbr@chromium.org>
    > Commit-Queue: Kenneth Russell <kbr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#493819}
    
    TBR=meade@chromium.org
    CQ_INCLUDE_TRYBOTS=master.tryserver.chromium.win:win_optional_gpu_tests_rel
    
    Change-Id: I497adee4374828b1e8040a2e0c11745b64a5c4d0
    Bug: 661854
    Reviewed-on: https://chromium-review.googlesource.com/611884
    Reviewed-by: Darren Shen <shend@chromium.org>
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Darren Shen <shend@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494646}
    Former-commit-id: 655f24fcfa2938dcbf8f774be7c8db88f031e935

--- third_party/WebKit/Source/core/css/parser/CSSParserImpl.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserImpl.h
@@ -21,6 +21,7 @@ class CSSLazyParsingState;
 class CSSParserContext;
 class CSSParserObserver;
 class CSSParserObserverWrapper;
+class CSSParserTokenStream;
 class StyleRule;
 class StyleRuleBase;
 class StyleRuleCharset;
@@ -140,6 +141,8 @@ class CSSParserImpl {
   StyleRule* ConsumeStyleRule(CSSParserTokenRange prelude,
                               CSSParserTokenRange block);
 
+  void ConsumeDeclarationList(CSSParserTokenStream&, StyleRule::RuleType);
+  // TODO(shend): Remove this overload once we switch over to streams.
   void ConsumeDeclarationList(CSSParserTokenRange, StyleRule::RuleType);
   void ConsumeDeclaration(CSSParserTokenRange, StyleRule::RuleType);
   void ConsumeDeclarationValue(CSSParserTokenRange,
