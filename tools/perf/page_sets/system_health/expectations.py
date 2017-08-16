8c09499b2a044d22ae46de07f315fc0662e3c2f1
Simon <simonhatch@chromium.org>
2017-08-16 17:58:47 +0000

    Disable system_health.common_desktop page for Mac 10.11
    Page:
    - browse:search:google
    
    TBR=rnephew@chromium.org
    
    Bug: 756027
    Change-Id: I5f0790a1b6069f4eb06db7614bca4cc7c31eb5ba
    Reviewed-on: https://chromium-review.googlesource.com/617261
    Commit-Queue: Simon Hatch <simonhatch@chromium.org>
    Reviewed-by: Simon Hatch <simonhatch@chromium.org>
    Reviewed-by: rnephew <rnephew@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494854}
    Former-commit-id: 30fe4e31e1847faaaf041c51b3601c5f5f5678c9

--- tools/perf/page_sets/system_health/expectations.py
+++ tools/perf/page_sets/system_health/expectations.py
@@ -9,8 +9,9 @@ class SystemHealthDesktopCommonExpectations(expectations.StoryExpectations):
     self.DisableStory('browse:news:hackernews',
                       [expectations.ALL_WIN, expectations.ALL_MAC],
                       'crbug.com/676336')
-    self.DisableStory('browse:search:google', [expectations.ALL_WIN],
-                      'crbug.com/673775')
+    self.DisableStory('browse:search:google',
+                      [expectations.ALL_WIN, expectations.MAC_10_11],
+                      'win:crbug.com/673775, mac:crbug.com/756027')
     self.DisableStory('browse:tools:maps', [expectations.ALL],
                       'crbug.com/712694')
     self.DisableStory('browse:tools:earth', [expectations.ALL],
