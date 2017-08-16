373508e05c36c5724cc70c429fe63f8d7d80ab45
Tom Sepez <tsepez@chromium.org>
2017-08-16 17:02:31 +0000

    Use common SandboxTypeFromCommandLine() on mac.
    Split off from https://chromium-review.googlesource.com/c/583586
    
    Bug: 
    Change-Id: I937852ce2a665808febe665e65d625fd9e03c42e
    Reviewed-on: https://chromium-review.googlesource.com/615862
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Tom Sepez <tsepez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494832}
    Former-commit-id: 8a31aa8a46182cba3be783a2937080afb9776533

--- content/common/sandbox_init_mac.cc
+++ content/common/sandbox_init_mac.cc
@@ -11,6 +11,7 @@
 #include "content/common/sandbox_mac.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/sandbox_init.h"
+#include "content/public/common/sandbox_type.h"
 #include "sandbox/mac/seatbelt.h"
 
 namespace content {
@@ -31,68 +32,47 @@ bool InitializeSandbox(int sandbox_type,
   return Sandbox::EnableSandbox(sandbox_type, allowed_dir);
 }
 
+// Fill in |sandbox_type| and |allowed_dir| based on the command line,  returns
+// false if the current process type doesn't need to be sandboxed or if the
+// sandbox was disabled from the command line.
+bool GetSandboxInfoFromCommandLine(int* sandbox_type,
+                                   base::FilePath* allowed_dir) {
+  DCHECK(sandbox_type);
+  DCHECK(allowed_dir);
+
+  *allowed_dir = base::FilePath();  // Empty by default.
+  auto* command_line = base::CommandLine::ForCurrentProcess();
+  std::string process_type =
+      command_line->GetSwitchValueASCII(switches::kProcessType);
+  if (process_type == switches::kUtilityProcess) {
+    *allowed_dir =
+        command_line->GetSwitchValuePath(switches::kUtilityProcessAllowedDir);
+  }
+
+  *sandbox_type = SandboxTypeFromCommandLine(*command_line);
+  if (*sandbox_type == SANDBOX_TYPE_NO_SANDBOX)
+    return false;
+
+  if (command_line->HasSwitch(switches::kV2SandboxedEnabled)) {
+    CHECK(sandbox::Seatbelt::IsSandboxed());
+    // Do not enable the sandbox if V2 is already enabled.
+    return false;
+  }
+
+  return *sandbox_type != SANDBOX_TYPE_INVALID;
+}
+
 }  // namespace
 
 bool InitializeSandbox(int sandbox_type, const base::FilePath& allowed_dir) {
   return InitializeSandbox(sandbox_type, allowed_dir, base::OnceClosure());
 }
 
-// Fill in |sandbox_type| and |allowed_dir| based on the command line,  returns
-// false if the current process type doesn't need to be sandboxed or if the
-// sandbox was disabled from the command line.
-bool GetSandboxTypeFromCommandLine(int* sandbox_type,
-                                   base::FilePath* allowed_dir) {
-  DCHECK(sandbox_type);
-  DCHECK(allowed_dir);
-
-  *sandbox_type = -1;
-  *allowed_dir = base::FilePath();  // Empty by default.
-
-  const base::CommandLine& command_line =
-      *base::CommandLine::ForCurrentProcess();
-  if (command_line.HasSwitch(switches::kNoSandbox))
-    return false;
-  if (command_line.HasSwitch(switches::kV2SandboxedEnabled)) {
-    CHECK(sandbox::Seatbelt::IsSandboxed());
-    // Do not enable the sandbox if V2 is already enabled.
-    return false;
-  }
-
-  std::string process_type =
-      command_line.GetSwitchValueASCII(switches::kProcessType);
-  if (process_type.empty()) {
-    // Browser process isn't sandboxed.
-    return false;
-  } else if (process_type == switches::kRendererProcess) {
-    *sandbox_type = SANDBOX_TYPE_RENDERER;
-  } else if (process_type == switches::kUtilityProcess) {
-    // Utility process sandbox.
-    *sandbox_type = SANDBOX_TYPE_UTILITY;
-    *allowed_dir =
-        command_line.GetSwitchValuePath(switches::kUtilityProcessAllowedDir);
-  } else if (process_type == switches::kGpuProcess) {
-    if (command_line.HasSwitch(switches::kDisableGpuSandbox))
-      return false;
-    *sandbox_type = SANDBOX_TYPE_GPU;
-  } else if (process_type == switches::kPpapiBrokerProcess) {
-    return false;
-  } else if (process_type == switches::kPpapiPluginProcess) {
-    *sandbox_type = SANDBOX_TYPE_PPAPI;
-  } else {
-    // This is a process which we don't know about, i.e. an embedder-defined
-    // process. If the embedder wants it sandboxed, they have a chance to return
-    // the sandbox profile in ContentClient::GetSandboxProfileForSandboxType.
-    return false;
-  }
-  return true;
-}
-
 bool InitializeSandboxWithPostWarmupHook(base::OnceClosure hook) {
   int sandbox_type = 0;
   base::FilePath allowed_dir;
-  if (!GetSandboxTypeFromCommandLine(&sandbox_type, &allowed_dir))
-    return true;
-  return InitializeSandbox(sandbox_type, allowed_dir, std::move(hook));
+  return !GetSandboxInfoFromCommandLine(&sandbox_type, &allowed_dir) ||
+         InitializeSandbox(sandbox_type, allowed_dir, std::move(hook));
 }
 
 bool InitializeSandbox() {
