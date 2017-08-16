0c33be893e4c05eb7b4a82c55692959c533b3f3c
Nicolas Dossou-gbete <dgn@chromium.org>
2017-08-16 14:19:44 +0000

    Reland "[Suggestions] Add modern styles for sign in promo"
    This is a reland of 53122a79ceee231c154fef7acddf00fb8b0ce29d [formerly 70c937134d62aa896d8b891673d475233d9c122d]
    Original change's description:
    > [Suggestions] Add modern styles for sign in promo
    >
    > Adds styles to be used for modern cards and uses them in the
    > existing one and for the new style of sign in promo. Additionally,
    > sign in promos are now at the top in Modern.
    >
    > Preview:
    > https://goo.gl/photos/QjeWSHMtphWKpBebA
    >
    > Bug: 754186
    > Change-Id: I58600912a0c39385a502c7daaaf37cc614f882ab
    > Reviewed-on: https://chromium-review.googlesource.com/611905
    > Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    > Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#494365}
    
    Revert was https://crrev.com/c/615924
    
    Bug: 754186
    Change-Id: I9e7652c781708499466126126653cd314c362687
    Reviewed-on: https://chromium-review.googlesource.com/616566
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494770}
    Former-commit-id: 50141f191674e2e1654259b35eb7070b43b99ee0

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/NewTabPageAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/NewTabPageAdapter.java
@@ -25,6 +25,7 @@ import org.chromium.chrome.browser.offlinepages.OfflinePageBridge;
 import org.chromium.chrome.browser.suggestions.DestructionObserver;
 import org.chromium.chrome.browser.suggestions.SiteSection;
 import org.chromium.chrome.browser.suggestions.SuggestionsCarousel;
+import org.chromium.chrome.browser.suggestions.SuggestionsConfig;
 import org.chromium.chrome.browser.suggestions.SuggestionsRecyclerView;
 import org.chromium.chrome.browser.suggestions.SuggestionsUiDelegate;
 import org.chromium.chrome.browser.suggestions.TileGroup;
@@ -112,7 +113,12 @@ public class NewTabPageAdapter extends Adapter<NewTabPageViewHolder> implements 
             mRoot.addChild(mSiteSection);
         }
 
-        mRoot.addChildren(mSections, mSigninPromo, mAllDismissed, mFooter);
+        if (SuggestionsConfig.useModern()) {
+            mRoot.addChildren(mSigninPromo, mSections, mAllDismissed, mFooter);
+        } else {
+            mRoot.addChildren(mSections, mSigninPromo, mAllDismissed, mFooter);
+        }
+
         if (mAboveTheFoldView == null
                 || ChromeFeatureList.isEnabled(ChromeFeatureList.NTP_CONDENSED_LAYOUT)) {
             mBottomSpacer = null;
@@ -328,7 +334,7 @@ public class NewTabPageAdapter extends Adapter<NewTabPageViewHolder> implements 
     }
 
     @VisibleForTesting
-    int getFirstPositionForType(@ItemViewType int viewType) {
+    public int getFirstPositionForType(@ItemViewType int viewType) {
         int count = getItemCount();
         for (int i = 0; i < count; i++) {
             if (getItemViewType(i) == viewType) return i;
