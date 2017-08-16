04fc0ca0d3759c827a2c2718b7e03ea9d6779490
Florin Malita <fmalita@chromium.org>
2017-08-16 18:48:38 +0000

    More Skia integral-translate bilerp optimizations
    (remove SK_SUPPORT_LEGACY_BILERP2)
    
    https://chromium-review.googlesource.com/575675 only covered clamp/clamp,
    this should work for all tiling combinations.
    
    BUG=744674
    
    Change-Id: I5a0639ba03d45253ec986bb5811d2d584c0863b9
    Reviewed-on: https://chromium-review.googlesource.com/616421
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#494882}
    Former-commit-id: 8f02fda936013f03ac7da5f10fcd89bf92b88eb5

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -198,10 +198,6 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #   define SK_SUPPORT_LEGACY_ANISOTROPIC_MIPMAP_SCALE
 #endif
 
-#ifndef SK_SUPPORT_LEGACY_BILERP2
-#define SK_SUPPORT_LEGACY_BILERP2
-#endif
-
 // Remove this after we fixed all the issues related to the new SDF algorithm
 // (https://codereview.chromium.org/1643143002)
 #ifndef    SK_USE_LEGACY_DISTANCE_FIELDS
