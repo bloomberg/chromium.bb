d9281b068585f2c8dc0e6b33ed77401b50de6580
Nate Fischer <ntfschr@chromium.org>
2017-08-17 05:04:14 +0000

    AW: suppress compile warning
    This suppresses a compile warning for an unchecked reflection call on
    AwSafeBrowsingApiHandler. The class will always have the method to fetch
    user opt-in preference, so it's safe to ignore the warning.
    
    Bug: 755922
    Test: ninja system_webview_google_apk system_webview_apk monochrome_apk
    Change-Id: If14900d9bdc42bbb01d4e9f65a0e19f9f0678d12
    Reviewed-on: https://chromium-review.googlesource.com/618304
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Nate Fischer <ntfschr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495079}
    Former-commit-id: e2d22cf660126d4290da10eb8fd8f6fa60eb0d0c

--- android_webview/java/src/org/chromium/android_webview/AwSafeBrowsingConfigHelper.java
+++ android_webview/java/src/org/chromium/android_webview/AwSafeBrowsingConfigHelper.java
@@ -27,6 +27,7 @@ public class AwSafeBrowsingConfigHelper {
 
     private static Boolean sSafeBrowsingUserOptIn;
 
+    @SuppressWarnings("unchecked")
     public static void maybeInitSafeBrowsingFromSettings(final Context appContext) {
         AwContentsStatics.setSafeBrowsingEnabledByManifest(
                 CommandLine.getInstance().hasSwitch(AwSwitches.WEBVIEW_ENABLE_SAFEBROWSING_SUPPORT)
