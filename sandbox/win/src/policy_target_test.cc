744713cda04eb3f7c99ee80cf62fb22cf3173384
Alex Gaynor <agaynor@mozilla.com>
2017-08-17 23:25:12 +0000

    Added support for using alternate desktops both and with and without alternate winstations in a single process
    Right now it's not possible to have both configurations from a single parent
    process. While I doubt Chromium needs this, it'd be useful for Firefox, where we
    currently have one sandbox that uses an alternate desktop and an alternate
    winstation, and we'd like to enable an alternate desktop for another sandbox but
    it's not ready for an alternate winstation yet.
    
    R=wfh@chromium.org
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: I32adebf31a5627f52c0c98e95da146a4963568e3
    Reviewed-on: https://chromium-review.googlesource.com/615145
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Will Harris <wfh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495376}
    Former-commit-id: 0cb5dadc2b1f84fbbd9c6f75056e38d05a5b07d3

--- sandbox/win/src/policy_target_test.cc
+++ sandbox/win/src/policy_target_test.cc
@@ -361,10 +361,31 @@ TEST(PolicyTargetTest, WinstaPolicy) {
   temp_policy = nullptr;
 }
 
+// Creates multiple policies, with alternate desktops on both local and
+// alternate winstations.
+TEST(PolicyTargetTest, BothLocalAndAlternateWinstationDesktop) {
+  BrokerServices* broker = GetBroker();
+
+  scoped_refptr<TargetPolicy> policy1 = broker->CreatePolicy();
+  scoped_refptr<TargetPolicy> policy2 = broker->CreatePolicy();
+  scoped_refptr<TargetPolicy> policy3 = broker->CreatePolicy();
+
+  ResultCode result;
+  result = policy1->SetAlternateDesktop(false);
+  EXPECT_EQ(SBOX_ALL_OK, result);
+  result = policy2->SetAlternateDesktop(true);
+  EXPECT_EQ(SBOX_ALL_OK, result);
+  result = policy3->SetAlternateDesktop(false);
+  EXPECT_EQ(SBOX_ALL_OK, result);
+
+  policy1->DestroyAlternateDesktop();
+  policy2->DestroyAlternateDesktop();
+  policy3->DestroyAlternateDesktop();
+}
+
 // Launches the app in the sandbox and share a handle with it. The app should
 // be able to use the handle.
 TEST(PolicyTargetTest, ShareHandleTest) {
-
   BrokerServices* broker = GetBroker();
   ASSERT_TRUE(broker != NULL);
 
