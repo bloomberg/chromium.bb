58c73e6261715b273bc0a1203a1680c7e52f9390
Hiroki Nakagawa <nhiroki@chromium.org>
2017-08-17 22:05:58 +0000

    ServiceWorker: Clean up response reader / writer creation in ServiceWorkerWriteToCacheJob
    This is a cleanup and doesn't change behavior.
    
    Before this CL, CreateCacheResponseReader() encapsulates policy decision whether
    to do the byte-for-byte check. This would lose the readability (e.g., creation
    function would generally be expected to return a non-null instance).
    
    This CL inlines response reader / writer creation, moves policy decision code to
    its own function, and adds comments about how to decide the policy.
    
    Bug: n/a
    Change-Id: I2d6055e27241a9f3feaf14ad4e176c107e06f721
    Reviewed-on: https://chromium-review.googlesource.com/618533
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495340}
    Former-commit-id: c4d58bca11c73f010faf19ed32c7779dbd61db48

--- content/browser/service_worker/service_worker_write_to_cache_job.cc
+++ content/browser/service_worker/service_worker_write_to_cache_job.cc
@@ -80,9 +80,6 @@ ServiceWorkerWriteToCacheJob::ServiceWorkerWriteToCacheJob(
       resource_id_(resource_id),
       incumbent_resource_id_(incumbent_resource_id),
       version_(version),
-      has_been_killed_(false),
-      did_notify_started_(false),
-      did_notify_finished_(false),
       weak_factory_(this) {
   DCHECK(version_);
   DCHECK(resource_type_ == RESOURCE_TYPE_SCRIPT ||
@@ -114,9 +111,19 @@ void ServiceWorkerWriteToCacheJob::StartAsync() {
     return;
   }
 
-  cache_writer_.reset(new ServiceWorkerCacheWriter(
-      CreateCacheResponseReader(), CreateCacheResponseReader(),
-      CreateCacheResponseWriter()));
+  // Create response readers only when we have to do the byte-for-byte check.
+  std::unique_ptr<ServiceWorkerResponseReader> compare_reader;
+  std::unique_ptr<ServiceWorkerResponseReader> copy_reader;
+  if (ShouldByteForByteCheck()) {
+    compare_reader =
+        context_->storage()->CreateResponseReader(incumbent_resource_id_);
+    copy_reader =
+        context_->storage()->CreateResponseReader(incumbent_resource_id_);
+  }
+  cache_writer_ = base::MakeUnique<ServiceWorkerCacheWriter>(
+      std::move(compare_reader), std::move(copy_reader),
+      context_->storage()->CreateResponseWriter(resource_id_));
+
   version_->script_cache_map()->NotifyStartedCaching(url_, resource_id_);
   did_notify_started_ = true;
   StartNetRequest();
@@ -487,18 +494,9 @@ net::Error ServiceWorkerWriteToCacheJob::NotifyFinishedCaching(
   return net_error;
 }
 
-std::unique_ptr<ServiceWorkerResponseReader>
-ServiceWorkerWriteToCacheJob::CreateCacheResponseReader() {
-  if (incumbent_resource_id_ == kInvalidServiceWorkerResourceId ||
-      !version_->pause_after_download()) {
-    return nullptr;
-  }
-  return context_->storage()->CreateResponseReader(incumbent_resource_id_);
-}
-
-std::unique_ptr<ServiceWorkerResponseWriter>
-ServiceWorkerWriteToCacheJob::CreateCacheResponseWriter() {
-  return context_->storage()->CreateResponseWriter(resource_id_);
+bool ServiceWorkerWriteToCacheJob::ShouldByteForByteCheck() const {
+  return incumbent_resource_id_ != kInvalidServiceWorkerResourceId &&
+         version_->pause_after_download();
 }
 
 }  // namespace content
