794193cad682f696d3bf912603d1b3836aa3ecbc
David Roger <droger@chromium.org>
2017-08-17 15:07:55 +0000

    [signin] Simplify threading in ChromeSigninHelper unit test
    This CL removes all the thread-hopping from the test, because
    it is unnecessary.
    Everything that was done on the IO thread is now done on the main
    thread, and all the synchronization code is removed.
    
    Bug: 748755
    Change-Id: I887818304e7535a77b6810ae2bda7d6e9a005479
    Reviewed-on: https://chromium-review.googlesource.com/616725
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: David Roger <droger@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495175}
    Former-commit-id: ce4a4f9650be43d6f81bf2068cd59ceb15ce46c7

--- chrome/browser/signin/chrome_signin_helper_unittest.cc
+++ chrome/browser/signin/chrome_signin_helper_unittest.cc
@@ -7,13 +7,11 @@
 #include <memory>
 #include <string>
 
-#include "base/command_line.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
 #include "components/signin/core/browser/scoped_account_consistency.h"
 #include "components/signin/core/common/profile_management_switches.h"
 #include "components/signin/core/common/signin_features.h"
-#include "content/public/browser/browser_thread.h"
 #include "content/public/browser/resource_request_info.h"
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "net/http/http_response_headers.h"
@@ -30,6 +28,7 @@
 namespace {
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
+
 const GURL kGaiaUrl("https://accounts.google.com");
 const char kDiceResponseHeader[] = "X-Chrome-ID-Consistency-Response";
 
@@ -49,86 +48,6 @@ class TestRequestInterceptor : public net::URLRequestInterceptor {
   }
 };
 
-// Helper class managing the thread hopping between UI and IO.
-class TestResponseHelper {
- public:
-  TestResponseHelper() : success_(false) {
-    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-  }
-
-  // Creates a URL request with the Dice response headers.
-  void CreateRequestWithResponseHeaders() {
-    success_ = false;
-    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-    content::BrowserThread::PostTask(
-        content::BrowserThread::IO, FROM_HERE,
-        base::BindOnce(
-            &TestResponseHelper::CreateRequestWithDiceResponseHeaderOnIO,
-            base::Unretained(this)));
-    base::RunLoop().RunUntilIdle();
-    ASSERT_TRUE(success_);
-
-    success_ = false;
-    content::BrowserThread::PostTask(
-        content::BrowserThread::IO, FROM_HERE,
-        base::BindOnce(&TestResponseHelper::CheckHeaderSetOnIO,
-                       base::Unretained(this)));
-    base::RunLoop().RunUntilIdle();
-    ASSERT_TRUE(success_);
-  }
-
-  // Processes the Dice header and check that it was removed.
-  void CheckDiceHeaderRemoved() {
-    success_ = false;
-    DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
-    content::BrowserThread::PostTask(
-        content::BrowserThread::IO, FROM_HERE,
-        base::BindOnce(&TestResponseHelper::CheckDiceHeaderRemovedOnIO,
-                       base::Unretained(this)));
-    base::RunLoop().RunUntilIdle();
-    ASSERT_TRUE(success_);
-  }
-
- private:
-  void CreateRequestWithDiceResponseHeaderOnIO() {
-    DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-    test_request_delegate_ = base::MakeUnique<net::TestDelegate>();
-    request_ = url_request_context_.CreateRequest(
-        kGaiaUrl, net::DEFAULT_PRIORITY, test_request_delegate_.get(),
-        TRAFFIC_ANNOTATION_FOR_TESTS);
-    content::ResourceRequestInfo::AllocateForTesting(
-        request_.get(), content::RESOURCE_TYPE_MAIN_FRAME, nullptr, -1, -1, -1,
-        true, false, false, true, content::PREVIEWS_OFF);
-    net::URLRequestFilter::GetInstance()->AddUrlInterceptor(
-        kGaiaUrl, base::MakeUnique<TestRequestInterceptor>());
-    request_->Start();
-    success_ = true;
-  }
-
-  void CheckHeaderSetOnIO() {
-    DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-    net::HttpResponseHeaders* response_headers = request_->response_headers();
-    ASSERT_TRUE(response_headers);
-    ASSERT_TRUE(request_->response_headers()->HasHeader(kDiceResponseHeader));
-    net::URLRequestFilter::GetInstance()->RemoveUrlHandler(kGaiaUrl);
-    success_ = true;
-  }
-
-  void CheckDiceHeaderRemovedOnIO() {
-    DCHECK_CURRENTLY_ON(content::BrowserThread::IO);
-    // Process the header.
-    signin::ProcessAccountConsistencyResponseHeaders(request_.get(), GURL(),
-                                                     false /* is_incognito */);
-    // Check that the header has been removed.
-    EXPECT_FALSE(request_->response_headers()->HasHeader(kDiceResponseHeader));
-    success_ = true;
-  }
-
-  net::TestURLRequestContext url_request_context_;
-  std::unique_ptr<net::TestDelegate> test_request_delegate_;
-  std::unique_ptr<net::URLRequest> request_;
-  bool success_;
-};
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
 
 }  // namespace
@@ -141,6 +60,9 @@ class ChromeSigninHelperTest : public testing::Test {
   ~ChromeSigninHelperTest() override {}
 
   content::TestBrowserThreadBundle thread_bundle_;
+  net::TestURLRequestContext url_request_context_;
+  std::unique_ptr<net::TestDelegate> test_request_delegate_;
+  std::unique_ptr<net::URLRequest> request_;
 };
 
 #if BUILDFLAG(ENABLE_DICE_SUPPORT)
@@ -148,8 +70,30 @@ class ChromeSigninHelperTest : public testing::Test {
 TEST_F(ChromeSigninHelperTest, RemoveDiceSigninHeader) {
   signin::ScopedAccountConsistencyDiceFixAuthErrors scoped_dice_fix_auth_errors;
 
-  TestResponseHelper test_response_helper;
-  test_response_helper.CreateRequestWithResponseHeaders();
-  test_response_helper.CheckDiceHeaderRemoved();
+  // Create a response with the Dice header.
+  test_request_delegate_ = base::MakeUnique<net::TestDelegate>();
+  request_ = url_request_context_.CreateRequest(kGaiaUrl, net::DEFAULT_PRIORITY,
+                                                test_request_delegate_.get(),
+                                                TRAFFIC_ANNOTATION_FOR_TESTS);
+  content::ResourceRequestInfo::AllocateForTesting(
+      request_.get(), content::RESOURCE_TYPE_MAIN_FRAME, nullptr, -1, -1, -1,
+      true, false, false, true, content::PREVIEWS_OFF);
+  net::URLRequestFilter::GetInstance()->AddUrlInterceptor(
+      kGaiaUrl, base::MakeUnique<TestRequestInterceptor>());
+  request_->Start();
+  base::RunLoop().RunUntilIdle();
+  net::URLRequestFilter::GetInstance()->RemoveUrlHandler(kGaiaUrl);
+
+  // Check that the Dice response header is correctly set.
+  net::HttpResponseHeaders* response_headers = request_->response_headers();
+  ASSERT_TRUE(response_headers);
+  ASSERT_TRUE(request_->response_headers()->HasHeader(kDiceResponseHeader));
+
+  // Process the header.
+  signin::ProcessAccountConsistencyResponseHeaders(request_.get(), GURL(),
+                                                   false /* is_incognito */);
+
+  // Check that the header has been removed.
+  EXPECT_FALSE(request_->response_headers()->HasHeader(kDiceResponseHeader));
 }
 #endif  // BUILDFLAG(ENABLE_DICE_SUPPORT)
