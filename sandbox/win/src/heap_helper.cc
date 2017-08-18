51986b8cffb14e5cdabea16f5288a5fc8e3fc4d9
Liam Murphy <liamjm@chromium.org>
2017-08-18 23:24:32 +0000

    CRSSS lockdown. Support different heap structures for Win32.
    This allows CSRSS lockdown to be supported on x86.
    Win32 on x64 (Wow64) is not supported. This has significantly
    different heaps and ALPC Ports, so will need further investigation.
    Any changes to support this will be in a fresh CL.
    
    Bug: 751809
    Cq-Include-Trybots: master.tryserver.chromium.win:win10_chromium_x64_rel_ng
    Change-Id: I3e850135350ebd4d72d32505bcbcde66c1a17465
    Reviewed-on: https://chromium-review.googlesource.com/621784
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Liam Murphy <liamjm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495741}
    Former-commit-id: 29cf09bdc9d46841b5e4239148160bc5dd4c2510

--- sandbox/win/src/heap_helper.cc
+++ sandbox/win/src/heap_helper.cc
@@ -10,17 +10,72 @@
 #include "base/win/windows_version.h"
 
 namespace sandbox {
+namespace {
+#pragma pack(1)
 
 // These are undocumented, but readily found on the internet.
-#define HEAP_CLASS_8 0x00008000  // CSR port heap
-#define HEAP_CLASS_MASK 0x0000f000
+constexpr DWORD kHeapClass8 = 0x00008000;  // CSR port heap
+constexpr DWORD kHeapClassMask = 0x0000f000;
 
-// This structure is not documented, but the flags field is the only relevant
-// field.
-struct _HEAP {
-  char reserved[0x70];
-  DWORD flags;
+constexpr DWORD kHeapSegmentSignature = 0xffeeffee;
+constexpr DWORD kHeapSignature = 0xeeffeeff;
+
+typedef struct _HEAP_ENTRY {
+  PVOID Data1;
+  PVOID Data2;
+} HEAP_ENTRY, *PHEAP_ENTRY;
+
+// The _HEAP struct is not documented, so char arrays are used to space out the
+// struct of the fields that are not relevant. However, this spacing is
+// different because of the different pointer widths between 32 and 64-bit.
+// So 32 and 64 bit structs are defined.
+struct _HEAP_32 {
+  HEAP_ENTRY HeapEntry;
+  DWORD SegmentSignature;
+  DWORD SegmentFlags;
+  LIST_ENTRY SegmentListEntry;
+  struct _HEAP_32* Heap;
+  char Unknown0[0x24];
+  // Offset 0x40
+  DWORD Flags;
+  // Offset 0x60
+  char Unknown1[0x1c];
+  DWORD Signature;
+  // Other stuff that is not relevant.
 };
+
+struct _HEAP_64 {
+  HEAP_ENTRY HeapEntry;
+  DWORD SegmentSignature;
+  DWORD SegmentFlags;
+  LIST_ENTRY SegmentListEntry;
+  struct _HEAP_64* Heap;
+  char Unknown0[0x40];
+  // Offset 0x70
+  DWORD Flags;
+  // Offset 0x98
+  char Unknown1[0x24];
+  DWORD Signature;
+  // Other stuff that is not relevant.
+};
+
+#if defined(_WIN64)
+using _HEAP = _HEAP_64;
+#else   // defined(_WIN64)
+using _HEAP = _HEAP_32;
+#endif  // defined(_WIN64)
+
+bool ValidateHeap(_HEAP* heap) {
+  if (heap->SegmentSignature != kHeapSegmentSignature)
+    return false;
+  if (heap->Heap != heap)
+    return false;
+  if (heap->Signature != kHeapSignature)
+    return false;
+  return true;
+}
+
+}  // namespace
 
 bool HeapFlags(HANDLE handle, DWORD* flags) {
   if (!handle || !flags) {
@@ -28,7 +83,11 @@ bool HeapFlags(HANDLE handle, DWORD* flags) {
     return false;
   }
   _HEAP* heap = reinterpret_cast<_HEAP*>(handle);
-  *flags = heap->flags;
+  if (!ValidateHeap(heap)) {
+    DLOG(ERROR) << "unable to validate heap";
+    return false;
+  }
+  *flags = heap->Flags;
   return true;
 }
 
@@ -51,7 +110,7 @@ HANDLE FindCsrPortHeap() {
       DLOG(ERROR) << "Unable to get flags for this heap";
       continue;
     }
-    if ((flags & HEAP_CLASS_MASK) == HEAP_CLASS_8) {
+    if ((flags & kHeapClassMask) == kHeapClass8) {
       if (nullptr != csr_port_heap) {
         DLOG(ERROR) << "Found multiple suitable CSR Port heaps";
         return nullptr;
