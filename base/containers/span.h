bc0867d2e42f29aed271c6d95a6cb94e0d8c9598
Daniel Cheng <dcheng@chromium.org>
2017-08-18 01:07:23 +0000

    base::Span: Implement conversion constructors.
    - Implement Span<U> to Span<T> conversions if U* is
      convertible to T*. This allows a Span<int> to be
      used as a Span<const int> without hassle.
    - Implement container-like to Span<T> conversions if
      the container in question implements a data() and
      size() member and is not Span or std::array<T, N>.
    
    Bug: 754077
    Change-Id: Iaf98a2a1f5b6ad4cf331447fed5ae20200b7a253
    Reviewed-on: https://chromium-review.googlesource.com/618448
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495412}
    Former-commit-id: 3aae9a05d87b8e658fe6a3f956c17344b1bee7a9

--- base/containers/span.h
+++ base/containers/span.h
@@ -8,36 +8,158 @@
 #include <stddef.h>
 
 #include <algorithm>
+#include <array>
+#include <type_traits>
+#include <utility>
 
 namespace base {
 
-// A Span represents an array of elements of type T. It consists of a pointer to
-// memory with an associated size. A Span does not own the underlying memory, so
-// care must be taken to ensure that a Span does not outlive the backing store.
-// Spans should be passed by value.
+template <typename T>
+class Span;
+
+namespace internal {
+
+template <typename T>
+struct IsSpanImpl : std::false_type {};
+
+template <typename T>
+struct IsSpanImpl<Span<T>> : std::true_type {};
+
+template <typename T>
+using IsSpan = IsSpanImpl<std::decay_t<T>>;
+
+template <typename T>
+struct IsStdArrayImpl : std::false_type {};
+
+template <typename T, size_t N>
+struct IsStdArrayImpl<std::array<T, N>> : std::true_type {};
+
+template <typename T>
+using IsStdArray = IsStdArrayImpl<std::decay_t<T>>;
+
+template <typename From, typename To>
+using IsLegalSpanConversion = std::is_convertible<From*, To*>;
+
+template <typename Container, typename T>
+using ContainerHasConvertibleData = IsLegalSpanConversion<
+    std::remove_pointer_t<decltype(std::declval<Container>().data())>,
+    T>;
+template <typename Container>
+using ContainerHasIntegralSize =
+    std::is_integral<decltype(std::declval<Container>().size())>;
+
+template <typename From, typename To>
+using EnableIfLegalSpanConversion =
+    std::enable_if_t<IsLegalSpanConversion<From, To>::value>;
+
+// SFINAE check if Container can be converted to a Span<T>. Note that the
+// implementation details of this check differ slightly from the requirements in
+// the working group proposal: in particular, the proposal also requires that
+// the container conversion constructor participate in overload resolution only
+// if two additional conditions are true:
+//
+//   1. Container implements operator[].
+//   2. Container::value_type matches remove_const_t<element_type>.
+//
+// The requirements are relaxed slightly here: in particular, not requiring (2)
+// means that an immutable Span can be easily constructed from a mutable
+// container.
+template <typename Container, typename T>
+using EnableIfSpanCompatibleContainer =
+    std::enable_if_t<!internal::IsSpan<Container>::value &&
+                     !internal::IsStdArray<Container>::value &&
+                     ContainerHasConvertibleData<Container, T>::value &&
+                     ContainerHasIntegralSize<Container>::value>;
+
+}  // namespace internal
+
+// A Span is a value type that represents an array of elements of type T.  It
+// consists of a pointer to memory with an associated size. A Span does not own
+// the underlying memory, so care must be taken to ensure that a Span does not
+// outlive the backing store.
 //
 // Span is somewhat analogous to StringPiece, but with arbitrary element types,
 // allowing mutation if T is non-const.
 //
+// Span is implicitly convertible from C++ arrays, as well as most [1]
+// container-like types that provide a data() and size() method (such as
+// std::vector<T>). A mutable Span<T> can also be implicitly converted to an
+// immutable Span<const T>.
+//
+// Consider using a Span for functions that take a data pointer and size
+// parameter: it allows the function to still act on an array-like type, while
+// allowing the caller code to be a bit more concise.
+//
+// Without span:
+//   // std::string HexEncode(uint8_t* data, size_t size);
+//   std::vector<uint8_t> data_buffer = GenerateData();
+//   std::string r = HexEncode(data_buffer.data(), data_buffer.size());
+//
+// With span:
+//   // std::string HexEncode(base::Span<uint8_t> data);
+//   std::vector<uint8_t> data_buffer = GenerateData();
+//   std::string r = HexEncode(data_buffer);
+//
+// Span differs from the C++ working group proposal in a number of ways:
+// - Span does not define the |element_type| and |index_type| type aliases.
+// - Span does not explicitly default the copy/move constructor/assignment
+//   operators, since MSVC currently complains about constexpr functions that
+//   aren't marked const.
+// - Span does not define operator().
+// - Span does not define bytes(), size_bytes(), as_bytes(), as_mutable_bytes()
+//   for working with spans as a sequence of bytes.
+// - Span has no extent template parameter.
+// - Span has no conversion constructors from std::unique_ptr or
+//   std::shared_ptr.
+// - Span has no reverse iterators.
+// - Span does not define relation operators other than == and !=.
+//
+// [1] std::array<T, N> has its own constructor overload which is not
+//     implemented.
+//
 // TODO(https://crbug.com/754077): Document differences from the working group
 // proposal: http://open-std.org/JTC1/SC22/WG21/docs/papers/2016/p0122r1.pdf.
-// TODO(https://crbug.com/754077): Implement more Span support, such as
-// initialization from containers, and document why this is useful (greater
-// safety since no need to manually pass in data + size)
 template <typename T>
 class Span {
  public:
-  using element_type = T;
+  using value_type = T;
   using pointer = T*;
   using reference = T&;
   using iterator = T*;
   using const_iterator = const T*;
   // TODO(dcheng): What about reverse iterators?
 
+  // Span constructors, copy, assignment, and destructor
   constexpr Span() noexcept : data_(nullptr), size_(0) {}
   constexpr Span(T* data, size_t size) noexcept : data_(data), size_(size) {}
+  // TODO(dcheng): Implement construction from a |begin| and |end| pointer.
   template <size_t N>
-  constexpr Span(T (&array)[N]) noexcept : data_(array), size_(N) {}
+  constexpr Span(T (&array)[N]) noexcept : Span(array, N) {}
+  // TODO(dcheng): Implement construction from std::array.
+  // Conversion from a container that provides |T* data()| and |integral_type
+  // size()|.
+  // TODO(dcheng): Should this be explicit for mutable spans?
+  template <typename Container,
+            typename = internal::EnableIfSpanCompatibleContainer<Container, T>>
+  constexpr Span(Container& container)
+      : Span(container.data(), container.size()) {}
+  // Disallow conversion from a container going out of scope, since the
+  // resulting span will just point to dangling memory. Note that this won't
+  // catch everything. For example, the following will still compile:
+  //   Span<int> ReturnsDanglingSpan() {
+  //     std::vector<int> v = GetVector();
+  //     return v;
+  //   }
+  template <typename Container,
+            typename = internal::EnableIfSpanCompatibleContainer<Container, T>>
+  Span(const Container&&) = delete;
+  ~Span() noexcept = default;
+  // Conversions from spans of compatible types: this allows a Span<T> to be
+  // seamlessly used as a Span<const T>, but not the other way around.
+  template <typename U, typename = internal::EnableIfLegalSpanConversion<U, T>>
+  constexpr Span(const Span<U>& other) : Span(other.data(), other.size()) {}
+  template <typename U, typename = internal::EnableIfLegalSpanConversion<U, T>>
+  constexpr Span(Span<U>&& other) : Span(other.data(), other.size()) {}
 
   // Span subviews
   constexpr Span subspan(size_t pos, size_t count) const {
@@ -88,6 +210,16 @@ template <typename T, size_t N>
 constexpr Span<T> MakeSpan(T (&array)[N]) noexcept {
   return Span<T>(array);
 }
+
+template <typename Container,
+          typename T = typename Container::value_type,
+          typename = internal::EnableIfSpanCompatibleContainer<Container, T>>
+constexpr Span<T> MakeSpan(Container& container) {
+  return Span<T>(container);
+}
+
+// TODO(dcheng): Should there be a const Container&& overload to try to prevent
+// MakeSpan from binding to rvalue containers?
 
 }  // namespace base
 
---
+++ base/containers/span_unittest.nc
@@ -0,0 +1,89 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This is a "No Compile Test" suite.
+// http://dev.chromium.org/developers/testing/no-compile-tests
+
+#include "base/containers/span.h"
+
+#include <array>
+#include <set>
+#include <vector>
+
+namespace base {
+
+class Base {
+};
+
+class Derived : Base {
+};
+
+#if defined(NCTEST_DERIVED_TO_BASE_CONVERSION_DISALLOWED)  // [r"fatal error: no matching constructor for initialization of 'Span<base::Base \*>'"]
+
+// Internally, this is represented as a pointer to pointers to Derived. An
+// implicit conversion to a pointer to pointers to Base must not be allowed.
+// If it were allowed, then something like this would be possible.
+//   Cat** cats = GetCats();
+//   Animals** animals = cats;
+//   animals[0] = new Dog();  // Uhoh!
+void WontCompile() {
+  Span<Derived*> derived_span;
+  Span<Base*> base_span(derived_span);
+}
+
+#elif defined(NCTEST_PTR_TO_CONSTPTR_CONVERSION_DISALLOWED)  // [r"fatal error: no matching constructor for initialization of 'Span<const int \*>'"]
+
+// Similarly, converting a Span<int*> to Span<const int*> requires internally
+// converting T** to const T**. This is also disallowed, as it would allow code
+// to violate the contract of const.
+void WontCompile() {
+  Span<int*> non_const_span;
+  Span<const int*> const_span(non_const_span);
+}
+
+#elif defined(NCTEST_STD_ARRAY_CONVERSION_DISALLOWED)  // [r"fatal error: no matching constructor for initialization of 'Span<int>'"]
+
+// This isn't implemented today. Maybe it will be some day.
+void WontCompile() {
+  std::array<int, 3> array;
+  Span<int> span(array);
+}
+
+#elif defined(NCTEST_CONST_CONTAINER_TO_MUTABLE_CONVERSION_DISALLOWED)  // [r"fatal error: no matching constructor for initialization of 'Span<int>'"]
+
+// A const container should not be convertible to a mutable span.
+void WontCompile() {
+  const std::vector<int> v = {1, 2, 3};
+  Span<int> span(v);
+}
+
+#elif defined(NCTEST_STD_SET_CONVERSION_DISALLOWED)  // [r"fatal error: no matching constructor for initialization of 'Span<int>'"]
+
+// A std::set() should not satisfy the requirements for conversion to a Span.
+void WontCompile() {
+  std::set<int> set;
+  Span<int> span(set);
+}
+
+#elif defined(NCTEST_RVALUE_CONTAINER_CONVERSION_DISALLOWED)  // [r"fatal error: call to deleted constructor of 'Span<int>'"]
+
+// Span's container conversion constructor should not allow it to bind to a
+// container going out of scope.
+std::vector<int> GetData();
+
+void WontCompile() {
+  Span<int> span(GetData());
+}
+
+#elif defined(NCTEST_MAKE_SPAN_FROM_SET_CONVERSION_DISALLOWED)  // [r"fatal error: no matching function for call to 'MakeSpan'"]
+
+// A std::set() should not satisfy the requirements for conversion to a Span.
+void WontCompile() {
+  std::set<int> set;
+  auto span = MakeSpan(set);
+}
+
+#endif
+
+}  // namespace base
