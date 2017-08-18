84e42f2d7e7ef899f783ce129488b8eb41e002f3
Takashi Toyoshima <toyoshim@google.com>
2017-08-18 12:58:48 +0000

    Web MIDI: make the MidiService robust against illegal ipc sequences
    Now the MidiService raises a check failure when an illegal ipc
    sequence is detected. But to run with ipc fuzzer, it should be
    robust against such illegal ipc sequences rather than raising a
    check failure.
    
    BUG=746969, 750011, 747662
    
    Change-Id: Ica788bd5371b2352a011a5d40037ef7e0da3ee48
    Reviewed-on: https://chromium-review.googlesource.com/605156
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495533}
    Former-commit-id: 78f5e86e47e0d85ff9a2d8fd097767f6be8b7997

--- media/midi/midi_service.cc
+++ media/midi/midi_service.cc
@@ -68,8 +68,8 @@ void MidiService::Shutdown() {
 void MidiService::StartSession(MidiManagerClient* client) {
   base::AutoLock lock(lock_);
   if (!manager_) {
-    CHECK(is_dynamic_instantiation_enabled_);
-    CHECK_EQ(0u, active_clients_);
+    DCHECK(is_dynamic_instantiation_enabled_);
+    DCHECK_EQ(0u, active_clients_);
     manager_.reset(MidiManager::Create(this));
     if (!manager_destructor_runner_)
       manager_destructor_runner_ = base::ThreadTaskRunnerHandle::Get();
@@ -80,8 +80,11 @@ void MidiService::StartSession(MidiManagerClient* client) {
 
 void MidiService::EndSession(MidiManagerClient* client) {
   base::AutoLock lock(lock_);
-  CHECK_NE(0u, active_clients_);
-  active_clients_--;
+
+  // MidiService needs to consider invalid EndSession calls without associated
+  // StartSession calls that could be sent from a broken renderer.
+  if (active_clients_)
+    active_clients_--;
 
   // Do nothing if MidiService::Shutdown() already runs.
   if (!manager_)
@@ -103,7 +106,11 @@ void MidiService::DispatchSendMidiData(MidiManagerClient* client,
                                        const std::vector<uint8_t>& data,
                                        double timestamp) {
   base::AutoLock lock(lock_);
-  manager_->DispatchSendMidiData(client, port_index, data, timestamp);
+
+  // MidiService needs to consider invalid DispatchSendMidiData calls without
+  // an open session that could be sent from a broken renderer.
+  if (manager_)
+    manager_->DispatchSendMidiData(client, port_index, data, timestamp);
 }
 
 scoped_refptr<base::SingleThreadTaskRunner> MidiService::GetTaskRunner(
