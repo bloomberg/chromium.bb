6cb446281cfafb79ee9336dfba37aa4fb55ff4b9
Daniel Hintze <hintzed@google.com>
2017-08-18 10:13:38 +0000

    Separate cors vs out-of-blink-cors paths in DocumentThreadableLoader
    This CL introduces scaffolding to separate the CORS-in-blink code
    path from the out-of-blink-CORS code path. It introduces new methods
    named *OutOfBlinkCORS and *BlinkCORS.
    
    The idea is that methods named *BlinkCORS can be safely removed after
    OutOfBlinkCORS has landed, in addition to than unused fields and methods
    while code in methods named *OutOfBlinkCORS would then be moved back to
    the corresponding methods without the suffix.
    
    The actuall functionallity for OutOfBlinkCORS will be added with a later
    CL, so for now *OutOfBlinkCORS method calls simply delegate calls back
    to the corresponding *BlinkCORS counterparts.
    
    Bug: 736308
    Change-Id: I085b40457996f0283f5169bf8950eabcd47405cb
    Reviewed-on: https://chromium-review.googlesource.com/618866
    Reviewed-by: Mike West <mkwst@chromium.org>
    Commit-Queue: Daniel Hintze <hintzed@google.com>
    Cr-Commit-Position: refs/heads/master@{#495516}
    Former-commit-id: 3af39fd92e9b987d20b98abd808f9b6fc562514f

--- third_party/WebKit/Source/core/loader/DocumentThreadableLoader.h
+++ third_party/WebKit/Source/core/loader/DocumentThreadableLoader.h
@@ -137,6 +137,38 @@ class CORE_EXPORT DocumentThreadableLoader final : public ThreadableLoader,
   // consulting ServiceWorker).
   void DispatchInitialRequest(ResourceRequest&);
   void MakeCrossOriginAccessRequest(const ResourceRequest&);
+
+  // TODO(hintzed): CORS handled out of Blink. Code in methods below named
+  // *OutOfBlinkCORS is to be moved back into the corresponding methods
+  // (e.g. those inherited from RawResourceClient) after
+  // https://crbug.com/736308 is fixed (i.e. when CORS is generally handled out
+  // of Blink).
+  void DispatchInitialRequestOutOfBlinkCORS(ResourceRequest&);
+  void MakeCrossOriginAccessRequestOutOfBlinkCORS(const ResourceRequest&);
+  void StartOutOfBlinkCORS(const ResourceRequest&);
+  bool RedirectReceivedOutOfBlinkCORS(Resource*,
+                                      const ResourceRequest&,
+                                      const ResourceResponse&);
+  void HandleResponseOutOfBlinkCORS(unsigned long identifier,
+                                    WebURLRequest::FetchRequestMode,
+                                    WebURLRequest::FetchCredentialsMode,
+                                    const ResourceResponse&,
+                                    std::unique_ptr<WebDataConsumerHandle>);
+  // TODO(hintzed): CORS handled in Blink. Methods below named *BlinkCORS are to
+  // be removed after https://crbug.com/736308 is fixed (i.e. when CORS is
+  // handled out of Blink).
+  void DispatchInitialRequestBlinkCORS(ResourceRequest&);
+  void MakeCrossOriginAccessRequestBlinkCORS(const ResourceRequest&);
+  void StartBlinkCORS(const ResourceRequest&);
+  bool RedirectReceivedBlinkCORS(Resource*,
+                                 const ResourceRequest&,
+                                 const ResourceResponse&);
+  void HandleResponseBlinkCORS(unsigned long identifier,
+                               WebURLRequest::FetchRequestMode,
+                               WebURLRequest::FetchCredentialsMode,
+                               const ResourceResponse&,
+                               std::unique_ptr<WebDataConsumerHandle>);
+
   // Loads m_fallbackRequestForServiceWorker.
   void LoadFallbackRequestForServiceWorker();
   // Issues a CORS preflight.
@@ -211,6 +243,9 @@ class CORE_EXPORT DocumentThreadableLoader final : public ThreadableLoader,
   // m_securityOrigin. In such a case, build a ResourceLoaderOptions with
   // up-to-date values from them and this variable, and use it.
   const ResourceLoaderOptions resource_loader_options_;
+
+  // True when feature OutOfBlinkCORS is enabled (https://crbug.com/736308).
+  bool out_of_blink_cors_;
 
   // Corresponds to the CORS flag in the Fetch spec.
   bool cors_flag_;
