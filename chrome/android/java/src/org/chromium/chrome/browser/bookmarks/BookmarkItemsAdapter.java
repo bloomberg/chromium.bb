ca3c1e70598f9e8ebc514d452bb5f3cfc0724a15
Iulia Harasim <iuliah@google.com>
2017-08-18 15:36:50 +0000

    [Android] Add signin promo and Chrome sync promo in Bookmarks
    This CL adds the layout for the signin promos for Bookmarks and it also
    wires the promo in Bookmarks. Additionally, it includes a sync promo
    in Bookmarks, shown when the user is signed in and has the Chrome sync
    disabled and increases the impression limit for the old promos to 10.
    
    Bug: 737743
    Change-Id: Iff277cfd7920caaaac3d385ded6b91b9a093dadb
    Reviewed-on: https://chromium-review.googlesource.com/601974
    Commit-Queue: Iulia Harasim <iuliah@google.com>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495553}
    Former-commit-id: 5805477f4ffecdc1be5d4786c80119894661078a

--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkItemsAdapter.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkItemsAdapter.java
@@ -5,6 +5,7 @@
 package org.chromium.chrome.browser.bookmarks;
 
 import android.content.Context;
+import android.support.annotation.IntDef;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.text.TextUtils;
@@ -17,32 +18,41 @@ import org.chromium.base.annotations.SuppressFBWarnings;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.bookmarks.BookmarkBridge.BookmarkItem;
 import org.chromium.chrome.browser.bookmarks.BookmarkBridge.BookmarkModelObserver;
-import org.chromium.chrome.browser.bookmarks.BookmarkPromoHeader.PromoHeaderShowingChangeListener;
-import org.chromium.chrome.browser.widget.displaystyle.MarginResizer;
+import org.chromium.chrome.browser.signin.SigninPromoView;
 import org.chromium.components.bookmarks.BookmarkId;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
  * BaseAdapter for {@link RecyclerView}. It manages bookmarks to list there.
  */
-class BookmarkItemsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements
-        BookmarkUIObserver, PromoHeaderShowingChangeListener {
-    private static final int PROMO_HEADER_VIEW = 0;
-    private static final int FOLDER_VIEW = 1;
-    private static final int BOOKMARK_VIEW = 2;
+class BookmarkItemsAdapter
+        extends RecyclerView.Adapter<RecyclerView.ViewHolder> implements BookmarkUIObserver {
+    /**
+     * Specifies the view types that the bookmark manager screen can contain.
+     */
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({ViewType.NEW_PROMO, ViewType.OLD_PROMO, ViewType.FOLDER, ViewType.BOOKMARK})
+    private @interface ViewType {
+        int NEW_PROMO = 0;
+        int OLD_PROMO = 1;
+        int FOLDER = 2;
+        int BOOKMARK = 3;
+    }
 
     private static final int MAXIMUM_NUMBER_OF_SEARCH_RESULTS = 500;
     private static final String EMPTY_QUERY = null;
 
     private final List<List<? extends Object>> mSections;
-    private final List<Object> mPromoHeaderSection = new ArrayList<>();
+
+    // The promo header section will always contain 0 or 1 elements.
+    @ViewType
+    private final List<Integer> mPromoHeaderSection = new ArrayList<>();
     private final List<BookmarkId> mFolderSection = new ArrayList<>();
     private final List<BookmarkId> mBookmarkSection = new ArrayList<>();
-
-    private final List<BookmarkRow> mBookmarkRows = new ArrayList<>();
-    private final List<BookmarkRow> mFolderRows = new ArrayList<>();
 
     private final List<BookmarkId> mTopLevelFolders = new ArrayList<>();
 
@@ -95,7 +105,13 @@ class BookmarkItemsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     }
 
     BookmarkId getItem(int position) {
-        return (BookmarkId) getSection(position).get(toSectionPosition(position));
+        List<?> section = getSection(position);
+
+        // The promo header section does contain bookmark ids.
+        if (section == mPromoHeaderSection) {
+            return null;
+        }
+        return (BookmarkId) section.get(toSectionPosition(position));
     }
 
     private int toSectionPosition(int globalPosition) {
@@ -167,15 +183,16 @@ class BookmarkItemsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     }
 
     @Override
-    public int getItemViewType(int position) {
+    public @ViewType int getItemViewType(int position) {
         List<?> section = getSection(position);
 
         if (section == mPromoHeaderSection) {
-            return PROMO_HEADER_VIEW;
+            assert section.size() == 1 : "Only one element is supported in promo header section!";
+            return mPromoHeaderSection.get(0);
         } else if (section == mFolderSection) {
-            return FOLDER_VIEW;
+            return ViewType.FOLDER;
         } else if (section == mBookmarkSection) {
-            return BOOKMARK_VIEW;
+            return ViewType.BOOKMARK;
         }
 
         assert false : "Invalid position requested";
@@ -183,29 +200,23 @@ class BookmarkItemsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent, @ViewType int viewType) {
         assert mDelegate != null;
 
         switch (viewType) {
-            case PROMO_HEADER_VIEW:
-                ViewHolder promoView = mPromoHeaderManager.createHolder(parent);
-                MarginResizer.createAndAttach(promoView.itemView,
-                        mDelegate.getSelectableListLayout().getUiConfig(),
-                        parent.getResources().getDimensionPixelSize(
-                                R.dimen.signin_and_sync_view_padding),
-                        0);
-                return promoView;
-            case FOLDER_VIEW:
+            case ViewType.NEW_PROMO:
+                return mPromoHeaderManager.createNewPromoHolder(parent);
+            case ViewType.OLD_PROMO:
+                return mPromoHeaderManager.createOldPromoHolder(parent);
+            case ViewType.FOLDER:
                 BookmarkFolderRow folder = (BookmarkFolderRow) LayoutInflater.from(
                         parent.getContext()).inflate(R.layout.bookmark_folder_row, parent, false);
                 folder.onBookmarkDelegateInitialized(mDelegate);
-                mFolderRows.add(folder);
                 return new ItemViewHolder(folder);
-            case BOOKMARK_VIEW:
+            case ViewType.BOOKMARK:
                 BookmarkItemRow item = (BookmarkItemRow) LayoutInflater.from(
                         parent.getContext()).inflate(R.layout.bookmark_item_row, parent, false);
                 item.onBookmarkDelegateInitialized(mDelegate);
-                mBookmarkRows.add(item);
                 return new ItemViewHolder(item);
             default:
                 assert false;
@@ -216,32 +227,21 @@ class BookmarkItemsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
     @SuppressFBWarnings("BC_UNCONFIRMED_CAST")
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        BookmarkId id = getItem(position);
-
-        switch (getItemViewType(position)) {
-            case PROMO_HEADER_VIEW:
+        switch (holder.getItemViewType()) {
+            case ViewType.NEW_PROMO:
+                mPromoHeaderManager.setupSigninPromo((SigninPromoView) holder.itemView);
                 break;
-            case FOLDER_VIEW:
-                ((BookmarkRow) holder.itemView).setBookmarkId(id);
+            case ViewType.OLD_PROMO:
                 break;
-            case BOOKMARK_VIEW:
-                ((BookmarkRow) holder.itemView).setBookmarkId(id);
+            case ViewType.FOLDER:
+                ((BookmarkRow) holder.itemView).setBookmarkId(getItem(position));
+                break;
+            case ViewType.BOOKMARK:
+                ((BookmarkRow) holder.itemView).setBookmarkId(getItem(position));
                 break;
             default:
                 assert false : "View type not supported!";
         }
-    }
-
-    // PromoHeaderShowingChangeListener implementation.
-
-    @Override
-    public void onPromoHeaderShowingChanged(boolean isShowing) {
-        assert mDelegate != null;
-        if (mDelegate.getCurrentState() != BookmarkUIState.STATE_FOLDER) {
-            return;
-        }
-
-        updateHeaderAndNotify();
     }
 
     // BookmarkUIObserver implementations.
@@ -251,7 +251,27 @@ class BookmarkItemsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         mDelegate = delegate;
         mDelegate.addUIObserver(this);
         mDelegate.getModel().addObserver(mBookmarkModelObserver);
-        mPromoHeaderManager = new BookmarkPromoHeader(mContext, this);
+
+        Runnable promoHeaderChangeAction = () -> {
+            assert mDelegate != null;
+            if (mDelegate.getCurrentState() != BookmarkUIState.STATE_FOLDER) {
+                return;
+            }
+
+            boolean wasShowingPromo = !mPromoHeaderSection.isEmpty();
+            updateHeader();
+            boolean willShowPromo = !mPromoHeaderSection.isEmpty();
+
+            if (!wasShowingPromo && willShowPromo) {
+                notifyItemInserted(0);
+            } else if (wasShowingPromo && willShowPromo) {
+                notifyItemChanged(0);
+            } else if (wasShowingPromo && !willShowPromo) {
+                notifyItemRemoved(0);
+            }
+        };
+
+        mPromoHeaderManager = new BookmarkPromoHeader(mContext, promoHeaderChangeAction, mDelegate);
         populateTopLevelFoldersList();
     }
 
@@ -260,7 +280,6 @@ class BookmarkItemsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         mDelegate.removeUIObserver(this);
         mDelegate.getModel().removeObserver(mBookmarkModelObserver);
         mDelegate = null;
-
         mPromoHeaderManager.destroy();
     }
 
@@ -319,8 +338,21 @@ class BookmarkItemsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>
         if (currentUIState == BookmarkUIState.STATE_SEARCHING) return;
 
         assert currentUIState == BookmarkUIState.STATE_FOLDER : "Unexpected UI state";
-        if (mPromoHeaderManager.shouldShow()) {
-            mPromoHeaderSection.add(null);
+
+        switch (mPromoHeaderManager.getPromoState()) {
+            case BookmarkPromoHeader.PromoState.PROMO_NONE:
+                return;
+            case BookmarkPromoHeader.PromoState.PROMO_SIGNIN_NEW:
+                mPromoHeaderSection.add(ViewType.NEW_PROMO);
+                return;
+            case BookmarkPromoHeader.PromoState.PROMO_SIGNIN_OLD:
+                mPromoHeaderSection.add(ViewType.OLD_PROMO);
+                return;
+            case BookmarkPromoHeader.PromoState.PROMO_SYNC:
+                mPromoHeaderSection.add(ViewType.OLD_PROMO);
+                return;
+            default:
+                assert false : "Unexpected value for promo state!";
         }
     }
 
--- chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkPromoHeader.java
+++ chrome/android/java/src/org/chromium/chrome/browser/bookmarks/BookmarkPromoHeader.java
@@ -4,75 +4,120 @@
 
 package org.chromium.chrome.browser.bookmarks;
 
+import android.accounts.Account;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.support.annotation.IntDef;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.VisibleForTesting;
 import org.chromium.base.metrics.RecordUserAction;
+import org.chromium.chrome.R;
+import org.chromium.chrome.browser.ChromeFeatureList;
+import org.chromium.chrome.browser.profiles.Profile;
+import org.chromium.chrome.browser.signin.ProfileDataCache;
 import org.chromium.chrome.browser.signin.SigninAccessPoint;
 import org.chromium.chrome.browser.signin.SigninAndSyncView;
 import org.chromium.chrome.browser.signin.SigninManager;
 import org.chromium.chrome.browser.signin.SigninManager.SignInStateObserver;
+import org.chromium.chrome.browser.signin.SigninPromoController;
+import org.chromium.chrome.browser.signin.SigninPromoView;
+import org.chromium.chrome.browser.widget.displaystyle.MarginResizer;
+import org.chromium.components.signin.AccountManagerFacade;
+import org.chromium.components.signin.AccountsChangeObserver;
+import org.chromium.components.signin.ChromeSigninController;
 import org.chromium.components.sync.AndroidSyncSettings;
 import org.chromium.components.sync.AndroidSyncSettings.AndroidSyncSettingsObserver;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.util.Collections;
+
+import javax.annotation.Nullable;
 
 /**
  * Class that manages all the logic and UI behind the signin promo header in the bookmark
  * content UI. The header is shown only on certain situations, (e.g., not signed in).
  */
-class BookmarkPromoHeader implements AndroidSyncSettingsObserver,
-        SignInStateObserver {
+class BookmarkPromoHeader implements AndroidSyncSettingsObserver, SignInStateObserver,
+                                     ProfileDataCache.Observer, AccountsChangeObserver {
     /**
-     * Interface to listen signin promo header visibility changes.
+     * Specifies the various states in which the Bookmarks promo can be.
      */
-    interface PromoHeaderShowingChangeListener {
-        /**
-         * Called when signin promo header visibility is changed.
-         * @param isShowing Whether it should be showing.
-         */
-        void onPromoHeaderShowingChanged(boolean isShowing);
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({PromoState.PROMO_NONE, PromoState.PROMO_SIGNIN_NEW, PromoState.PROMO_SIGNIN_OLD,
+            PromoState.PROMO_SYNC})
+    @interface PromoState {
+        int PROMO_NONE = 0;
+        int PROMO_SIGNIN_NEW = 1;
+        int PROMO_SIGNIN_OLD = 2;
+        int PROMO_SYNC = 3;
     }
 
-    private static final String PREF_SIGNIN_PROMO_DECLINED =
+    // New signin promo preferences.
+    private static final String PREF_NEW_SIGNIN_PROMO_DECLINED = "signin_promo_bookmarks_declined";
+    // Old signin promo preferences.
+    private static final String PREF_OLD_SIGNIN_PROMO_DECLINED =
             "enhanced_bookmark_signin_promo_declined";
     private static final String PREF_SIGNIN_PROMO_SHOW_COUNT =
             "enhanced_bookmark_signin_promo_show_count";
     // TODO(kkimlabs): Figure out the optimal number based on UMA data.
-    private static final int MAX_SIGNIN_PROMO_SHOW_COUNT = 5;
+    private static final int MAX_SIGNIN_PROMO_SHOW_COUNT = 10;
 
-    private static boolean sShouldShowForTests;
+    private static @Nullable @PromoState Integer sPromoStateForTests;
 
-    private Context mContext;
-    private SigninManager mSignInManager;
-    private boolean mShouldShow;
-    private PromoHeaderShowingChangeListener mShowingChangeListener;
+    private final Context mContext;
+    private final SigninManager mSignInManager;
+    private final Runnable mPromoHeaderChangeAction;
+    private final BookmarkDelegate mBookmarkDelegate;
+    private SigninPromoController mSigninPromoController;
+    private ProfileDataCache mProfileDataCache;
+    private boolean mIsNewPromoShowing;
+    private @PromoState int mPromoState;
 
     /**
      * Initializes the class. Note that this will start listening to signin related events and
      * update itself if needed.
      */
-    BookmarkPromoHeader(Context context,
-            PromoHeaderShowingChangeListener showingChangeListener) {
+    BookmarkPromoHeader(
+            Context context, Runnable promoHeaderChangeAction, BookmarkDelegate bookmarkDelegate) {
         mContext = context;
-        mShowingChangeListener = showingChangeListener;
+        mPromoHeaderChangeAction = promoHeaderChangeAction;
+        mBookmarkDelegate = bookmarkDelegate;
 
         AndroidSyncSettings.registerObserver(mContext, this);
+
+        if (SigninPromoController.shouldShowPromo(SigninAccessPoint.BOOKMARK_MANAGER)) {
+            int imageSize =
+                    mContext.getResources().getDimensionPixelSize(R.dimen.user_picture_size);
+            mProfileDataCache =
+                    new ProfileDataCache(mContext, Profile.getLastUsedProfile(), imageSize);
+            mProfileDataCache.addObserver(this);
+            mSigninPromoController = new SigninPromoController(
+                    mProfileDataCache, SigninAccessPoint.BOOKMARK_MANAGER);
+            AccountManagerFacade.get().addObserver(this);
+        }
 
         mSignInManager = SigninManager.get(mContext);
         mSignInManager.addSignInStateObserver(this);
 
-        updateShouldShow(false);
-        if (shouldShow()) {
-            int promoShowCount = ContextUtils.getAppSharedPreferences()
-                    .getInt(PREF_SIGNIN_PROMO_SHOW_COUNT, 0) + 1;
-            ContextUtils.getAppSharedPreferences().edit()
-                    .putInt(PREF_SIGNIN_PROMO_SHOW_COUNT, promoShowCount).apply();
-            RecordUserAction.record("Signin_Impression_FromBookmarkManager");
+        mPromoState = calculatePromoState();
+        if (mPromoState == PromoState.PROMO_SIGNIN_OLD || mPromoState == PromoState.PROMO_SYNC) {
+            int promoShowCount =
+                    ContextUtils.getAppSharedPreferences().getInt(PREF_SIGNIN_PROMO_SHOW_COUNT, 0);
+            ContextUtils.getAppSharedPreferences()
+                    .edit()
+                    .putInt(PREF_SIGNIN_PROMO_SHOW_COUNT, promoShowCount + 1)
+                    .apply();
+            if (mPromoState == PromoState.PROMO_SIGNIN_OLD) {
+                RecordUserAction.record("Signin_Impression_FromBookmarkManager");
+            }
         }
     }
 
@@ -82,66 +127,158 @@ class BookmarkPromoHeader implements AndroidSyncSettingsObserver,
     void destroy() {
         AndroidSyncSettings.unregisterObserver(mContext, this);
 
+        if (mSigninPromoController != null) {
+            AccountManagerFacade.get().removeObserver(this);
+            mProfileDataCache.removeObserver(this);
+        }
+
         mSignInManager.removeSignInStateObserver(this);
-        mSignInManager = null;
     }
 
     /**
-     * @return Whether it should be showing.
+     * @return The current state of the promo.
      */
-    boolean shouldShow() {
-        return mShouldShow || sShouldShowForTests;
+    @PromoState
+    int getPromoState() {
+        return mPromoState;
     }
 
     /**
-     * @return Signin promo header {@link ViewHolder} instance that can be used with
+     * @return New signin promo header {@link ViewHolder} instance that can be used with
      *         {@link RecyclerView}.
      */
-    ViewHolder createHolder(ViewGroup parent) {
-        SigninAndSyncView.Listener listener = new SigninAndSyncView.Listener() {
-            @Override
-            public void onViewDismissed() {
-                setSigninPromoDeclined();
-                updateShouldShow(true);
-            }
-        };
+    ViewHolder createNewPromoHolder(ViewGroup parent) {
+        View view = LayoutInflater.from(mContext).inflate(
+                R.layout.signin_promo_view_bookmarks, parent, false);
 
-        SigninAndSyncView view =
-                SigninAndSyncView.create(parent, listener, SigninAccessPoint.BOOKMARK_MANAGER);
-        // A MarginResizer is used to apply margins in regular and wide display modes. Remove the
-        // view's lateral padding so that margins can be used instead.
-        ApiCompatibilityUtils.setPaddingRelative(
-                view, 0, view.getPaddingTop(), 0, view.getPaddingBottom());
+        // ViewHolder is abstract and it cannot be instantiated directly.
         return new ViewHolder(view) {};
     }
 
     /**
-     * @return Whether user tapped "No" button on the signin promo header.
+     * @return Singin and sync promo header {@link ViewHolder} instance that can be used with
+     *         {@link RecyclerView}.
      */
-    private boolean wasSigninPromoDeclined() {
-        return ContextUtils.getAppSharedPreferences().getBoolean(
-                PREF_SIGNIN_PROMO_DECLINED, false);
+    ViewHolder createOldPromoHolder(ViewGroup parent) {
+        SigninAndSyncView.Listener listener = this ::setOldSigninPromoDeclined;
+
+        SigninAndSyncView view =
+                SigninAndSyncView.create(parent, listener, SigninAccessPoint.BOOKMARK_MANAGER);
+
+        // A MarginResizer is used to apply margins in regular and wide display modes. Remove
+        // the view's lateral padding so that margins can be used instead.
+        ApiCompatibilityUtils.setPaddingRelative(
+                view, 0, view.getPaddingTop(), 0, view.getPaddingBottom());
+        MarginResizer.createAndAttach(view,
+                mBookmarkDelegate.getSelectableListLayout().getUiConfig(),
+                parent.getResources().getDimensionPixelSize(R.dimen.signin_and_sync_view_padding),
+                0);
+
+        // ViewHolder is abstract and it cannot be instantiated directly.
+        return new ViewHolder(view) {};
     }
 
     /**
-     * Save that user tapped "No" button on the signin promo header.
+     * Configures the new signin promo and records promo impressions.
+     * @param view The view to be configured.
      */
-    private void setSigninPromoDeclined() {
-        SharedPreferences.Editor sharedPreferencesEditor =
-                ContextUtils.getAppSharedPreferences().edit();
-        sharedPreferencesEditor.putBoolean(PREF_SIGNIN_PROMO_DECLINED, true);
-        sharedPreferencesEditor.apply();
+    void setupSigninPromo(SigninPromoView view) {
+        Account[] accounts = AccountManagerFacade.get().tryGetGoogleAccounts();
+        String defaultAccountName = accounts.length == 0 ? null : accounts[0].name;
+
+        if (defaultAccountName != null) {
+            mProfileDataCache.update(Collections.singletonList(defaultAccountName));
+        }
+
+        mSigninPromoController.setAccountName(defaultAccountName);
+        if (!mIsNewPromoShowing) {
+            mIsNewPromoShowing = true;
+            mSigninPromoController.recordSigninPromoImpression();
+        }
+
+        SigninPromoController.OnDismissListener listener = this ::setNewSigninPromoDeclined;
+        mSigninPromoController.setupSigninPromoView(mContext, view, listener);
     }
 
-    private void updateShouldShow(boolean notifyUI) {
-        boolean oldIsShowing = mShouldShow;
-        mShouldShow = AndroidSyncSettings.isMasterSyncEnabled(mContext)
-                && mSignInManager.isSignInAllowed()
-                && !wasSigninPromoDeclined()
-                && ContextUtils.getAppSharedPreferences().getInt(
-                        PREF_SIGNIN_PROMO_SHOW_COUNT, 0) < MAX_SIGNIN_PROMO_SHOW_COUNT;
-        if (oldIsShowing != mShouldShow && notifyUI) {
-            mShowingChangeListener.onPromoHeaderShowingChanged(mShouldShow);
+    /**
+     * Saves that the new promo was declined and updates the UI.
+     */
+    private void setNewSigninPromoDeclined() {
+        SharedPreferences.Editor sharedPreferencesEditor =
+                ContextUtils.getAppSharedPreferences().edit();
+        sharedPreferencesEditor.putBoolean(PREF_NEW_SIGNIN_PROMO_DECLINED, true);
+        sharedPreferencesEditor.apply();
+        mPromoState = calculatePromoState();
+        mPromoHeaderChangeAction.run();
+    }
+
+    /**
+     * Saves that the old promo was dismissed and updates the UI.
+     */
+    private void setOldSigninPromoDeclined() {
+        SharedPreferences.Editor sharedPreferencesEditor =
+                ContextUtils.getAppSharedPreferences().edit();
+        sharedPreferencesEditor.putBoolean(PREF_OLD_SIGNIN_PROMO_DECLINED, true);
+        sharedPreferencesEditor.apply();
+        mPromoState = calculatePromoState();
+        mPromoHeaderChangeAction.run();
+    }
+
+    /**
+     * @return Whether the user declined the new signin promo.
+     */
+    private boolean wasNewSigninPromoDeclined() {
+        return ContextUtils.getAppSharedPreferences().getBoolean(
+                PREF_NEW_SIGNIN_PROMO_DECLINED, false);
+    }
+
+    /**
+     * @return Whether user tapped "No" button on the old signin promo.
+     */
+    private boolean wasOldSigninPromoDeclined() {
+        return ContextUtils.getAppSharedPreferences().getBoolean(
+                PREF_OLD_SIGNIN_PROMO_DECLINED, false);
+    }
+
+    private @PromoState int calculatePromoState() {
+        if (sPromoStateForTests != null) {
+            return sPromoStateForTests;
+        }
+
+        if (!AndroidSyncSettings.isMasterSyncEnabled(mContext)) {
+            return PromoState.PROMO_NONE;
+        }
+
+        // If the user is signed in, then we should show the sync promo if Chrome sync is disabled
+        // and the impression limit has not been reached yet.
+        if (ChromeSigninController.get().isSignedIn()) {
+            boolean impressionLimitNotReached =
+                    ContextUtils.getAppSharedPreferences().getInt(PREF_SIGNIN_PROMO_SHOW_COUNT, 0)
+                    < MAX_SIGNIN_PROMO_SHOW_COUNT;
+            if (!AndroidSyncSettings.isChromeSyncEnabled(mContext) && impressionLimitNotReached) {
+                return PromoState.PROMO_SYNC;
+            }
+            return PromoState.PROMO_NONE;
+        }
+
+        // The signin promo should be shown if signin is allowed, it wasn't declined by the user
+        // and the impression limit wasn't reached.
+        if (ChromeFeatureList.isEnabled(ChromeFeatureList.ANDROID_SIGNIN_PROMOS)) {
+            if (mSignInManager.isSignInAllowed()
+                    && SigninPromoController.shouldShowPromo(SigninAccessPoint.BOOKMARK_MANAGER)
+                    && !wasNewSigninPromoDeclined()) {
+                return PromoState.PROMO_SIGNIN_NEW;
+            }
+            return PromoState.PROMO_NONE;
+        } else {
+            boolean impressionLimitNotReached =
+                    ContextUtils.getAppSharedPreferences().getInt(PREF_SIGNIN_PROMO_SHOW_COUNT, 0)
+                    < MAX_SIGNIN_PROMO_SHOW_COUNT;
+            if (mSignInManager.isSignInAllowed() && impressionLimitNotReached
+                    && !wasOldSigninPromoDeclined()) {
+                return PromoState.PROMO_SIGNIN_OLD;
+            }
+            return PromoState.PROMO_NONE;
         }
     }
 
@@ -149,26 +286,44 @@ class BookmarkPromoHeader implements AndroidSyncSettingsObserver,
 
     @Override
     public void androidSyncSettingsChanged() {
-        updateShouldShow(true);
+        mPromoState = calculatePromoState();
+        mPromoHeaderChangeAction.run();
     }
 
     // SignInStateObserver implementations
 
     @Override
     public void onSignedIn() {
-        updateShouldShow(true);
+        mPromoState = calculatePromoState();
+        mPromoHeaderChangeAction.run();
     }
 
     @Override
     public void onSignedOut() {
-        updateShouldShow(true);
+        mPromoState = calculatePromoState();
+        mPromoHeaderChangeAction.run();
+    }
+
+    // ProfileDataCacheObserver implementation.
+
+    @Override
+    public void onProfileDataUpdated(String accountId) {
+        mPromoHeaderChangeAction.run();
+    }
+
+    // AccountsChangeObserver implementation.
+
+    @Override
+    public void onAccountsChanged() {
+        mPromoHeaderChangeAction.run();
     }
 
     /**
-     * Forces the promo to show for testing purposes.
+     * Forces the promo state to a particular value for testing purposes.
+     * @param promoState The promo state to which the header will be set to.
      */
     @VisibleForTesting
-    public static void setShouldShowForTests() {
-        sShouldShowForTests = true;
+    public static void forcePromoStateForTests(@PromoState int promoState) {
+        sPromoStateForTests = promoState;
     }
 }
