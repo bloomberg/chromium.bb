0d53dce2a249591be057afae1a3ad02a086e32a8
Etienne Pierre-Doray <etiennep@chromium.org>
2017-08-18 17:08:37 +0000

    [Zucchini] Make Patch Reading more robust.
    Fuzzing tests on patch reading reveals OOM related to preallocation.
    This CL fixes those issues:
    - Remove pre-allocation of vector and fill as we go instead.
    - Replace vector of extra targets to map -
      this allows us to detect repeating pools.
    
    Bug: 729154
    Change-Id: Iaf714a30cb9340f95b8d544f33f073bbd82b1315
    Reviewed-on: https://chromium-review.googlesource.com/619277
    Commit-Queue: Etienne Pierre-Doray <etiennep@chromium.org>
    Reviewed-by: Greg Thompson <grt@chromium.org>
    Reviewed-by: Samuel Huang <huangs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495589}
    Former-commit-id: 77342a1eb564dc5066e5eaab2c34e32ace0c483f

--- chrome/installer/zucchini/patch_reader.h
+++ chrome/installer/zucchini/patch_reader.h
@@ -8,6 +8,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include <map>
 #include <vector>
 
 #include "base/debug/stack_trace.h"
@@ -64,7 +65,8 @@ bool ParseVarInt(BufferSource* source, T* value) {
 
 }  // namespace patch
 
-// Each of *Source classes below has an associated "main type", and performs the
+// The *Source classes below are light-weight (i.e., allows copying) visitors to
+// read patch data. Each of them has an associated "main type", and performs the
 // following:
 // - Consumes portions of a BufferSource (required to remain valid for the
 //   lifetime of the object).
@@ -208,35 +210,35 @@ class PatchElementReader {
   PatchElementReader(PatchElementReader&&);
   ~PatchElementReader();
 
-  // If data read from |source| is well-formed, initialize internal state to
+  // If data read from |source| is well-formed, initialize cached sources to
   // read from it, and returns true. Otherwise returns false.
   bool Initialize(BufferSource* source);
 
   const ElementMatch& element_match() const { return element_match_; }
   const Element& old_element() const { return element_match_.old_element; }
   const Element& new_element() const { return element_match_.new_element; }
-  size_t pool_count() const { return extra_targets_.size(); }
 
-  const EquivalenceSource& GetEquivalenceSource() const {
-    return equivalences_;
-  }
-  const ExtraDataSource& GetExtraDataSource() const { return extra_data_; }
-  const RawDeltaSource& GetRawDeltaSource() const { return raw_delta_; }
-  const ReferenceDeltaSource& GetReferenceDeltaSource() const {
+  // The Get*() functions below return copies of cached sources.
+  EquivalenceSource GetEquivalenceSource() const { return equivalences_; }
+  ExtraDataSource GetExtraDataSource() const { return extra_data_; }
+  RawDeltaSource GetRawDeltaSource() const { return raw_delta_; }
+  ReferenceDeltaSource GetReferenceDeltaSource() const {
     return reference_delta_;
   }
-  const TargetSource& GetExtraTargetSource(PoolTag tag) const {
-    DCHECK_LT(tag.value(), extra_targets_.size());
-    return extra_targets_[tag.value()];
+  TargetSource GetExtraTargetSource(PoolTag tag) const {
+    auto pos = extra_targets_.find(tag);
+    return pos != extra_targets_.end() ? pos->second : TargetSource();
   }
 
  private:
   ElementMatch element_match_;
+
+  // Cached sources.
   EquivalenceSource equivalences_;
   ExtraDataSource extra_data_;
   RawDeltaSource raw_delta_;
   ReferenceDeltaSource reference_delta_;
-  std::vector<TargetSource> extra_targets_;
+  std::map<PoolTag, TargetSource> extra_targets_;
 };
 
 // Utility to read a Zucchini ensemble patch. An ensemble patch is the
@@ -258,7 +260,7 @@ class EnsemblePatchReader {
   // Check old / new image file validity, comparing against expected size and
   // CRC32. Return true if file matches expectations, false otherwise.
   bool CheckOldFile(ConstBufferView old_image) const;
-  bool CheckNewFile(ConstBufferView old_image) const;
+  bool CheckNewFile(ConstBufferView new_image) const;
 
   const PatchHeader& header() const { return header_; }
   PatchType patch_type() const { return patch_type_; }
