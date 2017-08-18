a74fc4c1bcb251eace4b7124d9faa0995f6b65ed
Rupert Swarbrick <rupert.swarbrick@argondesign.com>
2017-08-18 15:29:06 +0100

    Hide definitions behind #if ! CONFIG_EXT_PARTITION
    The orders_32x128 and orders_128x32 variables are only used when
    CONFIG_EXT_PARTITION is false. Only define them in that case to avoid
    a compiler warning.
    
    Change-Id: Iebe297eff36b139caa83d039101cf16eb88ca8f8

--- av1/common/reconintra.c
+++ av1/common/reconintra.c
@@ -115,12 +115,6 @@ static const uint16_t orders_8x32[64] = {
   32, 33, 34, 35, 36, 37, 38, 39, 48, 49, 50, 51, 52, 53, 54, 55,
   40, 41, 42, 43, 44, 45, 46, 47, 56, 57, 58, 59, 60, 61, 62, 63,
 };
-static const uint16_t orders_32x128[4] = {
-  0, 1, 2, 3,
-};
-static const uint16_t orders_128x32[4] = {
-  0, 1, 2, 3,
-};
 
 #if CONFIG_EXT_PARTITION
 static const uint16_t orders_16x4[256] = {
@@ -162,6 +156,13 @@ static const uint16_t orders_4x16[256] = {
   169, 170, 171, 172, 173, 174, 175, 184, 185, 186, 187, 188, 189, 190, 191,
   232, 233, 234, 235, 236, 237, 238, 239, 248, 249, 250, 251, 252, 253, 254,
   255,
+};
+#else
+static const uint16_t orders_32x128[4] = {
+  0, 1, 2, 3,
+};
+static const uint16_t orders_128x32[4] = {
+  0, 1, 2, 3,
 };
 #endif
 
