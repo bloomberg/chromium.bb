79424cca5644390c292559256b9fafaf8e41bd11
Charles Harrison <csharrison@chromium.org>
2017-08-19 03:37:39 +0000

    [subresource_filter] Add traces/UMA around ruleset verification
    Not only will the UMA give us latency numbers, but it will ensure
    that we aren't mapping in the ruleset when we shouldn't be.
    
    Bug: 721512
    Change-Id: I705bfccc69b7b7da582532338d625641b399f705
    Reviewed-on: https://chromium-review.googlesource.com/546137
    Commit-Queue: Charlie Harrison <csharrison@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495796}
    Former-commit-id: e7912e820fd868a331d7e512a3353806dfe5a3b2

--- components/subresource_filter/core/common/indexed_ruleset.cc
+++ components/subresource_filter/core/common/indexed_ruleset.cc
@@ -5,7 +5,9 @@
 #include "components/subresource_filter/core/common/indexed_ruleset.h"
 
 #include "base/logging.h"
+#include "base/trace_event/trace_event.h"
 #include "components/subresource_filter/core/common/first_party_origin.h"
+#include "components/subresource_filter/core/common/time_measurements.h"
 #include "url/gurl.h"
 #include "url/origin.h"
 
@@ -58,6 +60,10 @@ void RulesetIndexer::Finish() {
 
 // static
 bool IndexedRulesetMatcher::Verify(const uint8_t* buffer, size_t size) {
+  TRACE_EVENT0(TRACE_DISABLED_BY_DEFAULT("loading"),
+               "IndexedRulesetMatcher::Verify");
+  SCOPED_UMA_HISTOGRAM_MICRO_TIMER(
+      "SubresourceFilter.IndexRuleset.Verify.WallDuration");
   flatbuffers::Verifier verifier(buffer, size);
   return flat::VerifyIndexedRulesetBuffer(verifier);
 }
