499e8a933f352afef90bc046684796235b991bb8
Xiaocheng Hu <xiaochengh@chromium.org>
2017-08-19 20:28:09 +0000

    Reland "Do not create temporary Range in AXLayoutObject::IndexForVisiblePosition"
    The above mentioned function measures length between two positions,
    which doesn't really need a Range object. Hence, this patch changes the
    function not to create a temporary Range object to reduce memory impact.
    
    The original CL (r494877) was reverted due to passing reverted range to
    TextIterator, which is in turn due to the |position| passed to
    AXLayoutObject::IndexForVisiblePosition() being before the Node of the
    AXLayoutObject. This reland patch handles this case by returning 0.
    
    Bug: 388681
    Change-Id: Ib85e8bb11c0f98ba19baaaebfadf65610d647efa
    Reviewed-on: https://chromium-review.googlesource.com/619867
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495823}
    Former-commit-id: 358114c869ef79b1c38944ca107cc9b7a9e9873c

--- third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
+++ third_party/WebKit/Source/modules/accessibility/AXLayoutObject.cpp
@@ -1863,12 +1863,16 @@ int AXLayoutObject::IndexForVisiblePosition(
   if (index_position.IsNull())
     return 0;
 
-  Range* range = Range::Create(*GetDocument());
-  range->setStart(GetNode(), 0, IGNORE_EXCEPTION_FOR_TESTING);
-  range->setEnd(index_position, IGNORE_EXCEPTION_FOR_TESTING);
+  Position start_position = Position::FirstPositionInNode(*GetNode());
+  if (start_position > index_position) {
+    // TODO(chromium-accessibility): We reach here only when passing a position
+    // outside of the node range. This shouldn't happen, but is happening as
+    // found in crbug.com/756435.
+    NOTREACHED();
+    return 0;
+  }
 
-  return TextIterator::RangeLength(range->StartPosition(),
-                                   range->EndPosition());
+  return TextIterator::RangeLength(start_position, index_position);
 }
 
 AXLayoutObject* AXLayoutObject::GetUnignoredObjectFromNode(Node& node) const {
