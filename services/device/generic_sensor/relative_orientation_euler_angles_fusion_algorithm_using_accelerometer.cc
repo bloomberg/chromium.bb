4c94fc4f0c332e096c622d8855f1cf6ce4fd4591
Jun Cai <juncai@chromium.org>
2017-08-21 22:38:34 +0000

    Add unit tests for relative orientation algorithm using accelerometer
    This CL adds unit tests for fusion sensor algorithm that converts
    accelerometer data to relative orientation euler angles.
    
    Bug: 754773, 733707
    Change-Id: I519036cfa589b5a70aa5c33ddb53756e684f5282
    Reviewed-on: https://chromium-review.googlesource.com/614083
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496094}
    Former-commit-id: d0039f2d815fed73edd5f326c39d7504245069b4

--- services/device/generic_sensor/relative_orientation_euler_angles_fusion_algorithm_using_accelerometer.cc
+++ services/device/generic_sensor/relative_orientation_euler_angles_fusion_algorithm_using_accelerometer.cc
@@ -13,19 +13,14 @@
 
 namespace device {
 
-RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer::
-    RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer()
-    : PlatformSensorFusionAlgorithm(
-          mojom::SensorType::RELATIVE_ORIENTATION_EULER_ANGLES,
-          {mojom::SensorType::ACCELEROMETER}) {}
+namespace {
 
-RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer::
-    ~RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer() =
-        default;
-
-bool RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer::
-    GetFusedDataInternal(mojom::SensorType which_sensor_changed,
-                         SensorReading* fused_reading) {
+void ComputeRelativeOrientationFromAccelerometer(double acceleration_x,
+                                                 double acceleration_y,
+                                                 double acceleration_z,
+                                                 double* alpha_in_degrees,
+                                                 double* beta_in_degrees,
+                                                 double* gamma_in_degrees) {
   // Transform the accelerometer values to W3C draft angles.
   //
   // Accelerometer values are just dot products of the sensor axes
@@ -45,7 +40,38 @@ bool RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer::
   // Also note that alpha can't be provided but it's assumed to be always zero.
   // This is necessary in order to provide enough information to solve
   // the equations.
-  //
+  *alpha_in_degrees = 0.0;
+  *beta_in_degrees = kRadToDeg * std::atan2(-acceleration_y, acceleration_z);
+  *gamma_in_degrees = kRadToDeg * std::asin(acceleration_x / kMeanGravity);
+
+  // Convert beta and gamma to fit the intervals in the specification. Beta is
+  // [-180, 180) and gamma is [-90, 90).
+  if (*beta_in_degrees >= 180.0)
+    *beta_in_degrees = -180.0;
+  if (*gamma_in_degrees >= 90.0)
+    *gamma_in_degrees = -90.0;
+
+  DCHECK_GE(*beta_in_degrees, -180.0);
+  DCHECK_LT(*beta_in_degrees, 180.0);
+  DCHECK_GE(*gamma_in_degrees, -90.0);
+  DCHECK_LT(*gamma_in_degrees, 90.0);
+}
+
+}  // namespace
+
+RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer::
+    RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer()
+    : PlatformSensorFusionAlgorithm(
+          mojom::SensorType::RELATIVE_ORIENTATION_EULER_ANGLES,
+          {mojom::SensorType::ACCELEROMETER}) {}
+
+RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer::
+    ~RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer() =
+        default;
+
+bool RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer::
+    GetFusedDataInternal(mojom::SensorType which_sensor_changed,
+                         SensorReading* fused_reading) {
   DCHECK(fusion_sensor_);
 
   SensorReading reading;
@@ -54,31 +80,11 @@ bool RelativeOrientationEulerAnglesFusionAlgorithmUsingAccelerometer::
     return false;
   }
 
-  double acceleration_x = reading.accel.x;
-  double acceleration_y = reading.accel.y;
-  double acceleration_z = reading.accel.z;
-
-  double alpha_in_degrees = 0.0;
-  double beta_in_degrees =
-      kRadToDeg * std::atan2(-acceleration_y, acceleration_z);
-  double gamma_in_degrees =
-      kRadToDeg * std::asin(acceleration_x / kMeanGravity);
-
-  // Convert beta and gamma to fit the intervals in the specification. Beta is
-  // [-180, 180) and gamma is [-90, 90).
-  if (beta_in_degrees >= 180.0)
-    beta_in_degrees = -180.0;
-  if (gamma_in_degrees >= 90.0)
-    gamma_in_degrees = -90.0;
-
-  DCHECK_GE(beta_in_degrees, -180.0);
-  DCHECK_LT(beta_in_degrees, 180.0);
-  DCHECK_GE(gamma_in_degrees, -90.0);
-  DCHECK_LT(gamma_in_degrees, 90.0);
-
-  fused_reading->orientation_euler.x = beta_in_degrees;
-  fused_reading->orientation_euler.y = gamma_in_degrees;
-  fused_reading->orientation_euler.z = alpha_in_degrees;
+  ComputeRelativeOrientationFromAccelerometer(
+      reading.accel.x, reading.accel.y, reading.accel.z,
+      &fused_reading->orientation_euler.z.value() /* alpha_in_degrees */,
+      &fused_reading->orientation_euler.x.value() /* beta_in_degrees */,
+      &fused_reading->orientation_euler.y.value() /* gamma_in_degrees */);
 
   return true;
 }
