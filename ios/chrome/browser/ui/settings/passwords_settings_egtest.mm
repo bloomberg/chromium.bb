4fe9d022c7c3809bd03ae3fdac7cded41bc33448
Vasilii Sukhanov <vasilii@google.com>
2017-08-21 15:44:51 +0000

    Implement an alert on the password view page when no passcode is set up.
    If a user tries to copy/view a password without both PIN and TouchID
    configured then it's an error. A dialog appears. The screenshot is
    attached to the bug.
    
    Bug: 754266
    Change-Id: Ib529b401aff68ed717c306707ebb551b20dbfafc
    Reviewed-on: https://chromium-review.googlesource.com/622105
    Commit-Queue: Dominic Battré <battre@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495944}
    Former-commit-id: 7547cfb0cd9194a3ad66f699d478f0ec5fb66ab3

--- ios/chrome/browser/ui/settings/passwords_settings_egtest.mm
+++ ios/chrome/browser/ui/settings/passwords_settings_egtest.mm
@@ -1224,12 +1224,12 @@ MockReauthenticationModule* SetUpAndReturnMockReauthenticationModule() {
   [GetInteractionForPasswordDetailItem(CopyPasswordButton())
       performAction:grey_tap()];
 
-  NSString* snackbarLabel =
-      l10n_util::GetNSString(IDS_IOS_SETTINGS_SET_UP_SCREENLOCK_MESSAGE);
-  // The tap checks the existence of the snackbar and also closes it.
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(snackbarLabel)]
+  NSString* title =
+      l10n_util::GetNSString(IDS_IOS_SETTINGS_SET_UP_SCREENLOCK_TITLE);
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(title)]
+      assertWithMatcher:grey_sufficientlyVisible()];
+  [[EarlGrey selectElementWithMatcher:chrome_test_util::OKButton()]
       performAction:grey_tap()];
-
   [[EarlGrey selectElementWithMatcher:NavigationBarDoneButton()]
       performAction:grey_tap()];
 }
@@ -1256,14 +1256,28 @@ MockReauthenticationModule* SetUpAndReturnMockReauthenticationModule() {
   [GetInteractionForPasswordDetailItem(ShowPasswordButton())
       performAction:grey_tap()];
 
-  NSString* snackbarLabel =
-      l10n_util::GetNSString(IDS_IOS_SETTINGS_SET_UP_SCREENLOCK_MESSAGE);
-  // The tap checks the existence of the snackbar and also closes it.
-  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(snackbarLabel)]
-      performAction:grey_tap()];
+  NSString* title =
+      l10n_util::GetNSString(IDS_IOS_SETTINGS_SET_UP_SCREENLOCK_TITLE);
+  [[EarlGrey selectElementWithMatcher:grey_accessibilityLabel(title)]
+      assertWithMatcher:grey_sufficientlyVisible()];
+  NSString* learnHow =
+      l10n_util::GetNSString(IDS_IOS_SETTINGS_SET_UP_SCREENLOCK_LEARN_HOW);
+  [[EarlGrey
+      selectElementWithMatcher:chrome_test_util::ButtonWithAccessibilityLabel(
+                                   learnHow)] performAction:grey_tap()];
+  // Check the sub menu is closed due to the help article.
+  NSError* error = nil;
+  [[EarlGrey selectElementWithMatcher:SettingsMenuBackButton()]
+      assertWithMatcher:grey_notNil()
+                  error:&error];
+  GREYAssertTrue(error, @"The settings back button is still displayed");
 
+  // Check the settings page is closed.
+  error = nil;
   [[EarlGrey selectElementWithMatcher:NavigationBarDoneButton()]
-      performAction:grey_tap()];
+      assertWithMatcher:grey_notNil()
+                  error:&error];
+  GREYAssertTrue(error, @"The settings page is still displayed");
 }
 
 @end
