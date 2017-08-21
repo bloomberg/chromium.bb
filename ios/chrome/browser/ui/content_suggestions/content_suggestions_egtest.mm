5caf647fe5cbfde4d7e5ef6616e3100adc9cde9a
Gauthier Ambard <gambard@chromium.org>
2017-08-21 15:22:55 +0000

    Reenable testSwipeToDismissReadingListItem
    The test was failing on iPhone 5s because the toolbar menu was not
    visible. This is because the iPhone 5s screen is smaller, so the test
    has to scroll.
    
    Bug: 756950
    Change-Id: I53cf387ec8a2250c88446180a4607ec958f33ff7
    Reviewed-on: https://chromium-review.googlesource.com/623088
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495937}
    Former-commit-id: 20dd4fb040ec9b74ed7038bbda0a7ac3a2652e88

--- ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
+++ ios/chrome/browser/ui/content_suggestions/content_suggestions_egtest.mm
@@ -189,11 +189,6 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
 // Tests that after dismissing a ReadingList item, it is not displayed on the
 // NTP. But it is still unread in the Reading List surface.
 - (void)testSwipeToDismissReadingListItem {
-  // TODO(crbug.com/756950): Re-enable this test for iPhone idiom.
-  if (!IsIPadIdiom()) {
-    EARL_GREY_TEST_DISABLED(@"Test disabled on iPhone devices.");
-  }
-
   // Add two items to Reading List.
   std::string stdTitle1{"test title1"};
   std::string stdTitle2{"test title2"};
@@ -235,6 +230,7 @@ GREYElementInteraction* CellWithMatcher(id<GREYMatcher> matcher) {
       assertWithMatcher:grey_nil()];
 
   // Open the Reading List surface.
+  ScrollUp();
   [ChromeEarlGreyUI openToolsMenu];
   [[EarlGrey
       selectElementWithMatcher:chrome_test_util::ButtonWithAccessibilityLabelId(
