wjdz99/aom

Add inverse accuracy test for inv_tx_fm1d
Change-Id: I3cc9cb8135ec5e8aa20c8cff901a2f37b3b459ee

test/av1_txfm_test.cc
@@ -76,6 +76,18 @@ void reference_dct_1d(const double *in, double *out, int size) {
   }
 }
 
+void reference_idct_1d(const double *in, double *out, int size) {
+  for (int n = 0; n < size; ++n) {
+    out[n] = 0;
+    for (int k = 0; k < size; ++k) {
+      if (k == 0)
+        out[n] += invSqrt2 * in[k] * cos(PI * (2 * n + 1) * k / (2 * size));
+      else
+        out[n] += in[k] * cos(PI * (2 * n + 1) * k / (2 * size));
+    }
+  }
+}
+
 void reference_adst_1d(const double *in, double *out, int size) {
   for (int k = 0; k < size; ++k) {
     out[k] = 0;

bloomberg/chromium.bb

test/av1_txfm_test.cc
@@ -76,6 +76,18 @@ void reference_dct_1d(const double *in, double *out, int size) {
   }
 }
 
+void reference_idct_1d(const double *in, double *out, int size) {
+  for (int n = 0; n < size; ++n) {
+    out[n] = 0;
+    for (int k = 0; k < size; ++k) {
+      if (k == 0)
+        out[n] += invSqrt2 * in[k] * cos(M_PI * (2 * n + 1) * k / (2 * size));
+      else
+        out[n] += in[k] * cos(M_PI * (2 * n + 1) * k / (2 * size));
+    }
+  }
+}
+
 void reference_adst_1d(const double *in, double *out, int size) {
   for (int k = 0; k < size; ++k) {
     out[k] = 0;