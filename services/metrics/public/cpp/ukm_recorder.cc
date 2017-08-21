87127d17f0bcb68ea64d3e0dbf797b7c9096356c
Alexei Svitkine <asvitkine@chromium.org>
2017-08-21 19:51:54 +0000

    Enable Ukm by default on desktop & Android platforms.
    This is in preparation of rolling it out fully via
    variations server.
    
    BUG=673020
    
    Change-Id: Ic8a8a7b2a8e56d6ec1da3dc66418287db79e4521
    Reviewed-on: https://chromium-review.googlesource.com/621403
    Reviewed-by: Oystein Eftevaag <oysteine@chromium.org>
    Reviewed-by: Steven Holte <holte@chromium.org>
    Commit-Queue: Alexei Svitkine (very slow) <asvitkine@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496033}
    Former-commit-id: b62c5c2af33287e176317aafe498f66f39f27aef

--- services/metrics/public/cpp/ukm_recorder.cc
+++ services/metrics/public/cpp/ukm_recorder.cc
@@ -7,13 +7,18 @@
 #include "base/bind.h"
 #include "base/feature_list.h"
 #include "base/memory/ptr_util.h"
+#include "build/build_config.h"
 #include "services/metrics/public/cpp/ukm_entry_builder.h"
 
 namespace ukm {
 
 UkmRecorder* g_ukm_recorder = nullptr;
 
+#if defined(OS_IOS) || defined(OS_CHROMEOS)
 const base::Feature kUkmFeature = {"Ukm", base::FEATURE_DISABLED_BY_DEFAULT};
+#else
+const base::Feature kUkmFeature = {"Ukm", base::FEATURE_ENABLED_BY_DEFAULT};
+#endif
 
 UkmRecorder::UkmRecorder() = default;
 
