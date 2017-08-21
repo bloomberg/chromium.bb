83a4797e102f6ada031c284f293f35555fae137c
Yoshifumi Inoue <yosin@chromium.org>
2017-08-21 11:51:37 +0000

    Introduce SelectionTemplate::Builder::SetAs{Backward,Forward}Selection()
    This patch introduces |SetAs{Backward,Forward}Selection()| in
    |SelectionTemplate::Builder| to build |SelectionTemplate| with pre-computed
    |IsBaseFirst()| value as a preparation for the patch[1].
    
    This patch also changes |Build()| to check called once since we don't expect
    to call it multiple times.
    
    [1] http://crrev.com/c/609705 Introduce ComputeVisibleSelection() for
    VisibleSelectionTemplate
    
    Bug: 657237
    Change-Id: Ib8e2f03503717df7009f927124f7cf8d95d113d9
    Reviewed-on: https://chromium-review.googlesource.com/609704
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495916}
    Former-commit-id: 61bf6bb8381cc605f43c342697d8e3d98158f91c

--- third_party/WebKit/Source/core/editing/SelectionTemplateTest.cpp
+++ third_party/WebKit/Source/core/editing/SelectionTemplateTest.cpp
@@ -77,4 +77,42 @@ TEST_F(SelectionTest, range) {
   EXPECT_EQ(extent, selection.Extent());
 }
 
+TEST_F(SelectionTest, SetAsBacwardAndForward) {
+  SetBodyContent("<div id='sample'>abcdef</div>");
+
+  Element* sample = GetDocument().getElementById("sample");
+  Position start(Position(sample->firstChild(), 2));
+  Position end(Position(sample->firstChild(), 4));
+  EphemeralRange range(start, end);
+  const SelectionInDOMTree& backward_selection =
+      SelectionInDOMTree::Builder().SetAsBackwardSelection(range).Build();
+  const SelectionInDOMTree& forward_selection =
+      SelectionInDOMTree::Builder().SetAsForwardSelection(range).Build();
+  const SelectionInDOMTree& collapsed_selection =
+      SelectionInDOMTree::Builder()
+          .SetAsForwardSelection(EphemeralRange(start))
+          .Build();
+
+  EXPECT_EQ(TextAffinity::kDownstream, backward_selection.Affinity());
+  EXPECT_FALSE(backward_selection.IsBaseFirst());
+  EXPECT_FALSE(backward_selection.IsDirectional());
+  EXPECT_FALSE(backward_selection.IsNone());
+  EXPECT_EQ(end, backward_selection.Base());
+  EXPECT_EQ(start, backward_selection.Extent());
+
+  EXPECT_EQ(TextAffinity::kDownstream, forward_selection.Affinity());
+  EXPECT_TRUE(forward_selection.IsBaseFirst());
+  EXPECT_FALSE(forward_selection.IsDirectional());
+  EXPECT_FALSE(forward_selection.IsNone());
+  EXPECT_EQ(start, forward_selection.Base());
+  EXPECT_EQ(end, forward_selection.Extent());
+
+  EXPECT_EQ(TextAffinity::kDownstream, collapsed_selection.Affinity());
+  EXPECT_TRUE(collapsed_selection.IsBaseFirst());
+  EXPECT_FALSE(collapsed_selection.IsDirectional());
+  EXPECT_FALSE(collapsed_selection.IsNone());
+  EXPECT_EQ(start, collapsed_selection.Base());
+  EXPECT_EQ(start, collapsed_selection.Extent());
+}
+
 }  // namespace blink
