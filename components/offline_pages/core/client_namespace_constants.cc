d323f3f7bbb67b91b34f03d314b5bb31bbde5739
Justin DeWitt <dewittj@chromium.org>
2017-08-21 22:17:49 +0000

    [Offline Pages] Add warning about histograms in namespace constants.
    All the offline pages histograms are tagged with namespace.  This omission
    was missed because prefetch is still behind a flag.  A comment is added
    in the namespaces file to ensure that authors know to make this change in
    the future.
    
    Bug: None
    Change-Id: I8df115cce0d6ff562d42fbcd693e46b414b6597a
    Reviewed-on: https://chromium-review.googlesource.com/621828
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Filip Gorski <fgorski@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496083}
    Former-commit-id: bbf11d7b15f735ddf510fd44895fc6578e5dbd09

--- components/offline_pages/core/client_namespace_constants.cc
+++ components/offline_pages/core/client_namespace_constants.cc
@@ -6,6 +6,8 @@
 
 namespace offline_pages {
 
+// NOTE: When adding a namespace constant, you MUST add this as a suffix in
+// //tools/metrics/histograms/histograms.xml or risk crashes due to DCHECKs.
 const char kBookmarkNamespace[] = "bookmark";
 const char kLastNNamespace[] = "last_n";
 const char kAsyncNamespace[] = "async_loading";
