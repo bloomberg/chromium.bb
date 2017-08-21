5edec5be8a05df319fcb3ee60fe5eb4bfcd5373f
Henrik Kjellander <kjellander@chromium.org>
2017-08-21 11:37:07 +0000

    Revert "Put BlinkLeakDetector back to core/"
    This reverts commit 5f19a49b5849a9535a1971c8673375ffbf5c197a [formerly 42160bf06117c5c82a27427584c156107d7e8839].
    
    Reason for revert: Breaks Android Builder
    https://luci-logdog.appspot.com/v/?s=chromium%2Fbb%2Fchromium.webkit%2FAndroid_Builder%2F118473%2F%2B%2Frecipes%2Fsteps%2Fcompile%2F0%2Fstdout
    ../../third_party/android_tools/ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/lib/gcc/arm-linux-androideabi/4.9.x/../../../../arm-linux-androideabi/bin/ld.gold: error: obj/third_party/WebKit/Source/core/leak_detector/libleak_detector.a(BlinkLeakDetector.o): multiple definition of 'blink::BlinkLeakDetector::TimerFiredGC(blink::TimerBase*)'
    ../../third_party/android_tools/ndk/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/lib/gcc/arm-linux-androideabi/4.9.x/../../../../arm-linux-androideabi/bin/ld.gold: obj/third_party/WebKit/Source/core/unit_tests/BlinkLeakDetector.o: previous definition here
    
    Original change's description:
    > Put BlinkLeakDetector back to core/
    > 
    > Although BlinkLeakDetector should and will in the future live in controller/, currently it is better for the code to be in core/ to make it easy to add a devtools protocol method.
    > This CL puts the code back to core/leak_detector.
    > 
    > 
    > Bug: 
    > Change-Id: I863bcbdf9bded868a3aed601cfda8666f1ba67cd
    > Reviewed-on: https://chromium-review.googlesource.com/622816
    > Commit-Queue: Yuzu Saijo <yuzus@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Keishi Hattori <keishi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#495907}
    
    TBR=haraken@chromium.org,keishi@chromium.org,yuzus@chromium.org
    
    Change-Id: Ia29383cbd025fc11559cc760c6ba37efb120004c
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/623567
    Reviewed-by: Henrik Kjellander <kjellander@chromium.org>
    Commit-Queue: Henrik Kjellander <kjellander@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#495913}
    Former-commit-id: d7e4b96e299571ec466ebd0173edf33c91465fa9

--- third_party/WebKit/Source/modules/ModulesInitializer.cpp
+++ third_party/WebKit/Source/modules/ModulesInitializer.cpp
@@ -17,7 +17,6 @@
 #include "core/html/HTMLCanvasElement.h"
 #include "core/html/HTMLMediaElement.h"
 #include "core/inspector/InspectorSession.h"
-#include "core/leak_detector/BlinkLeakDetector.h"
 #include "core/offscreencanvas/OffscreenCanvas.h"
 #include "core/origin_trials/OriginTrials.h"
 #include "core/page/ChromeClient.h"
@@ -36,7 +35,6 @@
 #include "modules/audio_output_devices/HTMLMediaElementAudioOutputDevice.h"
 #include "modules/cachestorage/InspectorCacheStorageAgent.h"
 #include "modules/canvas2d/CanvasRenderingContext2D.h"
-#include "modules/compositorworker/AbstractAnimationWorkletThread.h"
 #include "modules/compositorworker/CompositorWorkerThread.h"
 #include "modules/credentialmanager/CredentialManagerClient.h"
 #include "modules/csspaint/CSSPaintImageGeneratorImpl.h"
@@ -274,10 +272,6 @@ void ModulesInitializer::ProvideModulesToPage(Page& page,
 
 void ModulesInitializer::ForceNextWebGLContextCreationToFail() const {
   WebGLRenderingContext::ForceNextWebGLContextCreationToFail();
-}
-
-void ModulesInitializer::CollectAllGarbageForAnimationWorklet() const {
-  AbstractAnimationWorkletThread::CollectAllGarbage();
 }
 
 }  // namespace blink
