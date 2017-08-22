b4b3f162e9f62d13aeb62876b89bd05a623ee373
Morten Stenshorne <mstensho@opera.com>
2017-08-22 21:27:09 +0000

    [LayoutNG] Bring back native support for multicol.
    Still missing: Copy data back into the LayoutObject tree.
    
    Some layout tests now fail, since we're not using the legacy engine to lay out
    multicol. Quite a few crash or freeze, too.
    
    Also brought back some of the unit tests. They needed some changes, because of
    the following:
    
    1. We now create a fragment for each column, from an anonymous constraint
    space. I.e. we have one more level of fragments, and it's the column fragments
    that have their inline offset shifted to the correct column, rather than
    setting it on the first child block, which is what we did last time these tests
    worked.
    
    2. Mistakes introduced when converting test to using setBodyInnerHTML() rather
    than building the layout trees programmatically:
    https://codereview.chromium.org/2725773002
    
    3. New bugs. :)
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Change-Id: I4df85d684f0ddb8f7f0f75d15230a1ab61e9f9a0
    Reviewed-on: https://chromium-review.googlesource.com/591429
    Commit-Queue: Morten Stenshorne <mstensho@opera.com>
    Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    Reviewed-by: Emil A Eklund <eae@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496446}
    Former-commit-id: b7e830ddf678c7daaa41a982fad87156b1bbbe4b

--- third_party/WebKit/Source/core/layout/ng/ng_column_layout_algorithm.h
+++ third_party/WebKit/Source/core/layout/ng/ng_column_layout_algorithm.h
@@ -5,21 +5,31 @@
 #ifndef NGColumnLayoutAlgorithm_h
 #define NGColumnLayoutAlgorithm_h
 
-#include "core/layout/ng/ng_block_layout_algorithm.h"
+#include "core/layout/ng/geometry/ng_logical_size.h"
+#include "core/layout/ng/ng_layout_algorithm.h"
 
 namespace blink {
 
 class NGBlockNode;
+class NGBlockBreakToken;
 class NGBreakToken;
 class NGConstraintSpace;
 
-class CORE_EXPORT NGColumnLayoutAlgorithm : public NGBlockLayoutAlgorithm {
+class CORE_EXPORT NGColumnLayoutAlgorithm
+    : public NGLayoutAlgorithm<NGBlockNode, NGBlockBreakToken> {
  public:
   NGColumnLayoutAlgorithm(NGBlockNode node,
                           NGConstraintSpace* space,
                           NGBreakToken* break_token = nullptr);
 
   RefPtr<NGLayoutResult> Layout() override;
+
+  Optional<MinMaxSize> ComputeMinMaxSize() const override;
+
+ private:
+  RefPtr<NGConstraintSpace> CreateConstraintSpaceForColumns() const;
+
+  NGLogicalSize content_box_size_;
 };
 
 }  // namespace Blink
