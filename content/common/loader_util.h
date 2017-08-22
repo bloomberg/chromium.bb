91026414daa22780a9000fe243466c1884b475d8
Andrey Kosyakov <caseq@chromium.org>
2017-08-22 00:18:19 +0000

    Move BuildDevToolsInfo to loader_util so it can be reused
    This is a follow-up to https://chromium-review.googlesource.com/c/617611
    that removes code duplication of BuildDevToolsInfo() by moving it to a
    place where it can be reused between network service and the browser
    loader.
    
    Bug: 721408
    Change-Id: I5c7446121bfc20a67e11a95d36c2fd0d3aec0649
    Reviewed-on: https://chromium-review.googlesource.com/619686
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496137}
    Former-commit-id: 6fa70190fce6b61ea4beb195a9d08c7a97c0cca2

--- content/common/loader_util.h
+++ content/common/loader_util.h
@@ -5,11 +5,15 @@
 #ifndef CONTENT_COMMON_LOADER_UTIL_H_
 #define CONTENT_COMMON_LOADER_UTIL_H_
 
+#include "base/memory/ref_counted.h"
+
 namespace net {
+class HttpRawRequestHeaders;
 class URLRequest;
 }
 
 namespace content {
+struct ResourceDevToolsInfo;
 struct ResourceResponse;
 
 // Helper utilities shared between network service and ResourceDispatcherHost
@@ -19,6 +23,11 @@ struct ResourceResponse;
 // type of the response.
 bool ShouldSniffContent(net::URLRequest* url_request,
                         ResourceResponse* response);
+
+// Fill ResourceDevToolsInfo based on raw headers.
+scoped_refptr<ResourceDevToolsInfo> BuildDevToolsInfo(
+    const net::URLRequest& request,
+    const net::HttpRawRequestHeaders& raw_request_headers);
 
 }  // namespace content
 
