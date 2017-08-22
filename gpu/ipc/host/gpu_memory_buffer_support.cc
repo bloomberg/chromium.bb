96bdefe017bb49aa491f98720e96f1cc3b12386b
YH Lin <yueherngl@chromium.org>
2017-08-22 22:09:13 +0000

    Revert "cros, gbm: Enable native GMB on Intel."
    This reverts commit 8f16beff94f5fb2cef4baf5968f6fa46acc9ecec [formerly 873c4f8c99332e31f4b8dc9911d2166ddc41dec6].
    
    To make the settings more consistent across different architectures.
    So that the flag can be applied in more consistent way.
    
    BUG=chromium:756673
    
    Change-Id: Ie12a7c06b5ddcf1dd1f88c1f772cbd9b372c1ee1
    Reviewed-on: https://chromium-review.googlesource.com/622029
    Reviewed-by: Dan Erat <derat@chromium.org>
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Commit-Queue: YH Lin <yueherngl@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496465}
    Former-commit-id: b49de005434fb3a51905e35253c70fa1e0609cca

--- gpu/ipc/host/gpu_memory_buffer_support.cc
+++ gpu/ipc/host/gpu_memory_buffer_support.cc
@@ -23,7 +23,7 @@ bool AreNativeGpuMemoryBuffersEnabled() {
     return false;
   }
 
-#if defined(OS_MACOSX) || defined(OS_CHROMEOS) && defined(ARCH_CPU_X86_FAMILY)
+#if defined(OS_MACOSX)
   return !base::CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kDisableNativeGpuMemoryBuffers);
 #else
