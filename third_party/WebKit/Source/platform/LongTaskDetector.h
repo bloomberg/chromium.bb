fee3e0b471951a569be70c319b55fccabfb2e9ea
Deepanjan Roy <dproy@chromium.org>
2017-08-22 19:23:15 +0000

    Adds Singleton Long Task Detector
    In order to detect Time to Interactive, and various other tab loading done or
    main thread quiescence signals, we need to detect long tasks, which is almost
    always defined as main thread tasks longer than 50ms. Since there is only one
    main thread per renderer, and looking at execution duration of every task is
    non-trivially expensive, ideally we should do the task length check once per
    task in a renderer, and notify other modules interested in long tasks only when
    the task is long enough.
    
    This CL adds a singleton LongTaskDetector, that maintains a list of long task
    observers. It inspects task durations by adding itself as a TaskTimeObserver on
    the current thread, and it only does so when it has non-zero long task
    observers.
    
    The 50ms threshold is part of I guideline of RAIL, and is unlikely to change.
    
    Bug: 
    Change-Id: I1e459d08d04649eb08cac936893278251add5aa3
    Reviewed-on: https://chromium-review.googlesource.com/621810
    Commit-Queue: Deepanjan Roy <dproy@chromium.org>
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Shubhie Panicker <panicker@chromium.org>
    Reviewed-by: lpy <lpy@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496399}
    Former-commit-id: 25b54f9d500b0c03e1575d4b8e582fef4d338952

---
+++ third_party/WebKit/Source/platform/LongTaskDetector.h
@@ -0,0 +1,56 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef LongTaskDetector_h
+#define LongTaskDetector_h
+
+#include "platform/PlatformExport.h"
+#include "platform/heap/Handle.h"
+#include "platform/scheduler/base/task_time_observer.h"
+#include "platform/wtf/Noncopyable.h"
+#include "public/platform/WebThread.h"
+
+namespace blink {
+
+class PLATFORM_EXPORT LongTaskObserver : public GarbageCollectedMixin {
+ public:
+  virtual ~LongTaskObserver() {}
+
+  virtual void OnLongTaskDetected(double start_time, double end_time) = 0;
+};
+
+// LongTaskDetector detects tasks longer than kLongTaskThreshold and notifies
+// observers. When it has non-zero LongTaskObserver, it adds itself as a
+// TaskTimeObserver on the main thread and observes every task. When the number
+// of LongTaskObservers drop to zero it automatically removes itself as a
+// TaskTimeObserver.
+class PLATFORM_EXPORT LongTaskDetector final
+    : public GarbageCollectedFinalized<LongTaskDetector>,
+      public scheduler::TaskTimeObserver {
+  WTF_MAKE_NONCOPYABLE(LongTaskDetector);
+
+ public:
+  static LongTaskDetector& Instance();
+
+  void RegisterObserver(LongTaskObserver*);
+  void UnregisterObserver(LongTaskObserver*);
+
+  DECLARE_TRACE();
+
+  static constexpr double kLongTaskThresholdSeconds = 0.05;
+
+ private:
+  LongTaskDetector();
+
+  // scheduler::TaskTimeObserver implementation
+  void WillProcessTask(double start_time) override {}
+  void DidProcessTask(double start_time, double end_time) override;
+  void OnBeginNestedRunLoop() override {}
+
+  HeapHashSet<WeakMember<LongTaskObserver>> observers_;
+};
+
+}  // namespace blink
+
+#endif  // LongTaskDetector_h
---
+++ third_party/WebKit/Source/platform/LongTaskDetectorTest.cpp
@@ -0,0 +1,98 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "platform/LongTaskDetector.h"
+
+#include "platform/CrossThreadFunctional.h"
+#include "platform/testing/TestingPlatformSupport.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+namespace blink {
+
+namespace {
+class TestLongTaskObserver :
+    // This has to be garbage collected since LongTaskObserver uses
+    // GarbageCollectedMixin.
+    public GarbageCollectedFinalized<TestLongTaskObserver>,
+    public LongTaskObserver {
+  USING_GARBAGE_COLLECTED_MIXIN(TestLongTaskObserver);
+
+ public:
+  double last_long_task_start = 0.0;
+  double last_long_task_end = 0.0;
+
+  // LongTaskObserver implementation.
+  void OnLongTaskDetected(double start_time, double end_time) override {
+    last_long_task_start = start_time;
+    last_long_task_end = end_time;
+  }
+};  // Anonymous namespace
+
+}  // namespace
+class LongTaskDetectorTest : public ::testing::Test {
+ public:
+  // Public because it's executed on a task queue.
+  void DummyTaskWithDuration(double duration_seconds) {
+    dummy_task_start_time_ = MonotonicallyIncreasingTime();
+    platform_->AdvanceClockSeconds(duration_seconds);
+    dummy_task_end_time_ = MonotonicallyIncreasingTime();
+  }
+
+ protected:
+  void SetUp() {
+    // For some reason, platform needs to run for non-zero seconds before we
+    // start posting tasks to it. Otherwise TaskTimeObservers don't get notified
+    // of tasks.
+    platform_->RunForPeriodSeconds(1);
+  }
+  double DummyTaskStartTime() { return dummy_task_start_time_; }
+
+  double DummyTaskEndTime() { return dummy_task_end_time_; }
+
+  void SimulateTask(double duration_seconds) {
+    platform_->CurrentThread()->GetWebTaskRunner()->PostTask(
+        BLINK_FROM_HERE,
+        CrossThreadBind(&LongTaskDetectorTest::DummyTaskWithDuration,
+                        CrossThreadUnretained(this), duration_seconds));
+    platform_->RunUntilIdle();
+  }
+
+  ScopedTestingPlatformSupport<TestingPlatformSupportWithMockScheduler>
+      platform_;
+
+ private:
+  double dummy_task_start_time_ = 0.0;
+  double dummy_task_end_time_ = 0.0;
+};
+
+TEST_F(LongTaskDetectorTest, DeliversLongTaskNotificationOnlyWhenRegistered) {
+  TestLongTaskObserver* long_task_observer = new TestLongTaskObserver();
+  SimulateTask(LongTaskDetector::kLongTaskThresholdSeconds + 0.01);
+  EXPECT_EQ(long_task_observer->last_long_task_end, 0.0);
+
+  LongTaskDetector::Instance().RegisterObserver(long_task_observer);
+  SimulateTask(LongTaskDetector::kLongTaskThresholdSeconds + 0.01);
+  double long_task_end_when_registered = DummyTaskEndTime();
+  EXPECT_EQ(long_task_observer->last_long_task_start, DummyTaskStartTime());
+  EXPECT_EQ(long_task_observer->last_long_task_end,
+            long_task_end_when_registered);
+
+  LongTaskDetector::Instance().UnregisterObserver(long_task_observer);
+  SimulateTask(LongTaskDetector::kLongTaskThresholdSeconds + 0.01);
+  // Check that we have a long task after unregistering observer.
+  ASSERT_FALSE(long_task_end_when_registered == DummyTaskEndTime());
+  EXPECT_EQ(long_task_observer->last_long_task_end,
+            long_task_end_when_registered);
+}
+
+TEST_F(LongTaskDetectorTest, DoesNotGetNotifiedOfShortTasks) {
+  TestLongTaskObserver* long_task_observer = new TestLongTaskObserver();
+  LongTaskDetector::Instance().RegisterObserver(long_task_observer);
+  SimulateTask(LongTaskDetector::kLongTaskThresholdSeconds - 0.01);
+  EXPECT_EQ(long_task_observer->last_long_task_end, 0.0);
+
+  SimulateTask(LongTaskDetector::kLongTaskThresholdSeconds + 0.01);
+  EXPECT_EQ(long_task_observer->last_long_task_end, DummyTaskEndTime());
+}
+}  // namespace blink
