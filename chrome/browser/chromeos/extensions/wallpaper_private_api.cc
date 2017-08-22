36d5cfe1588390c4b6fa7b65206917e2333ab9da
xdai <xdai@chromium.org>
2017-08-22 19:35:13 +0000

    Fix the flaky broken wallpaper synchronization.
    It looks like there is a race between chrome.storage.onChanged event and
    the configuration of Chrome sync service. Wallpaper Picker App listens
    to chrome.storage.onChanged event to track wallpaper data changes. Each
    time when when onChanged event is fired, we query the Chrome sync service
    first to get the user's sync perference and then depending on the sync
    preference, we decide if the wallpaper needs to be synced. But it's possible
    that when onChanged event is fired, the Chrome sync service hasn't finished
    configuring yet (this seems only happen for the first time configuration).
    For simplicity, this CL fixed it by periodically checking the profile sync
    service status until the profile sync service has configured successfully
    or has hit the retry limit. 
    
    Bug: 751314
    Change-Id: Idb4f06cbe4ecf91901f4dcef2681a86067951c16
    Reviewed-on: https://chromium-review.googlesource.com/618022
    Commit-Queue: Xiaoqian Dai <xdai@chromium.org>
    Reviewed-by: Biao She <bshe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496405}
    Former-commit-id: 353e156dba1c5031eb6d8ea2987462b0c87e4f51

--- chrome/browser/chromeos/extensions/wallpaper_private_api.cc
+++ chrome/browser/chromeos/extensions/wallpaper_private_api.cc
@@ -66,6 +66,12 @@ namespace record_wallpaper_uma = wallpaper_private::RecordWallpaperUMA;
 
 namespace {
 
+// The time in seconds and retry limit to re-check the profile sync service
+// status. Only after the profile sync service has been configured, we can get
+// the correct value of the user sync preference of "syncThemes".
+constexpr int kRetryDelay = 10;
+constexpr int kRetryLimit = 3;
+
 #if defined(GOOGLE_CHROME_BUILD)
 const char kWallpaperManifestBaseURL[] =
     "https://storage.googleapis.com/chromeos-wallpaper-public/manifest_";
@@ -200,13 +206,53 @@ ExtensionFunction::ResponseAction WallpaperPrivateGetStringsFunction::Run() {
 
 ExtensionFunction::ResponseAction
 WallpaperPrivateGetSyncSettingFunction::Run() {
-  Profile* profile =  Profile::FromBrowserContext(browser_context());
-  browser_sync::ProfileSyncService* sync =
-      ProfileSyncServiceFactory::GetInstance()->GetForProfile(profile);
+  BrowserThread::PostTask(
+      BrowserThread::UI, FROM_HERE,
+      base::BindOnce(&WallpaperPrivateGetSyncSettingFunction::
+                         CheckProfileSyncServiceStatus,
+                     this));
+  return RespondLater();
+}
+
+void WallpaperPrivateGetSyncSettingFunction::CheckProfileSyncServiceStatus() {
   std::unique_ptr<base::DictionaryValue> dict(new base::DictionaryValue());
-  dict->SetBoolean("syncThemes",
-                   sync->GetActiveDataTypes().Has(syncer::THEMES));
-  return RespondNow(OneArgument(std::move(dict)));
+
+  if (retry_number > kRetryLimit) {
+    // It's most likely that the wallpaper synchronization is enabled (It's
+    // enabled by default so unless the user disables it explicitly it remains
+    // enabled).
+    dict->SetBoolean("syncThemes", true);
+    Respond(OneArgument(std::move(dict)));
+    return;
+  }
+
+  Profile* profile =  Profile::FromBrowserContext(browser_context());
+  browser_sync::ProfileSyncService* sync_service =
+      ProfileSyncServiceFactory::GetInstance()->GetForProfile(profile);
+  if (!sync_service) {
+    dict->SetBoolean("syncThemes", false);
+    Respond(OneArgument(std::move(dict)));
+    return;
+  }
+
+  if (sync_service->IsSyncActive() && sync_service->ConfigurationDone()) {
+    dict->SetBoolean("syncThemes",
+                     sync_service->GetActiveDataTypes().Has(syncer::THEMES));
+    Respond(OneArgument(std::move(dict)));
+    return;
+  }
+
+  // It's possible that the profile sync service hasn't finished configuring yet
+  // when we're trying to query the user preference (this seems only happen for
+  // the first time configuration). In this case GetActiveDataTypes() returns an
+  // empty set. So re-check the status later.
+  retry_number++;
+  BrowserThread::PostDelayedTask(
+      BrowserThread::UI, FROM_HERE,
+      base::BindOnce(&WallpaperPrivateGetSyncSettingFunction::
+                         CheckProfileSyncServiceStatus,
+                     this),
+      base::TimeDelta::FromSeconds(retry_number * kRetryDelay));
 }
 
 WallpaperPrivateSetWallpaperIfExistsFunction::
