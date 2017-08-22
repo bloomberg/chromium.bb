f52a0cb85a6a3efd3e4b24b5b56421ba83a9c6ec
Peiyong Lin <lpy@chromium.org>
2017-08-22 18:32:03 +0000

    [GRC] Add PID as property to ProcessCoordinationUnit.
    Instead of setting up Process Coordination Unit with a pid, this patch
    sends a pid when the process is launched. A ProcessCoordinationUnit is
    issued to be created from the Blink side when Blink is initialized,
    however, under some conditions, the process is not yet ready, which
    results in duplication ProcessCoordinationUnit with the same pid
    because GetHandle returns kNullProcessHandle.
    
    BUG=756891
    
    Change-Id: I3dc79da5a3ad2a23bbec7952607634a272bbdfb7
    Reviewed-on: https://chromium-review.googlesource.com/623916
    Reviewed-by: Emily Stark <estark@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Oystein Eftevaag <oysteine@chromium.org>
    Commit-Queue: lpy <lpy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496379}
    Former-commit-id: 19278fd88ddaf88a73d70acd4ff65392fb3531da

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -2160,15 +2160,13 @@ RenderProcessHostImpl::GetProcessResourceCoordinator() {
   if (!resource_coordinator::IsResourceCoordinatorEnabled()) {
     process_resource_coordinator_ =
         base::MakeUnique<resource_coordinator::ResourceCoordinatorInterface>(
-            nullptr, resource_coordinator::CoordinationUnitType::kProcess,
-            base::GetProcId(GetHandle()));
+            nullptr, resource_coordinator::CoordinationUnitType::kProcess);
   } else {
     auto* connection = ServiceManagerConnection::GetForProcess();
     process_resource_coordinator_ =
         base::MakeUnique<resource_coordinator::ResourceCoordinatorInterface>(
             connection ? connection->GetConnector() : nullptr,
-            resource_coordinator::CoordinationUnitType::kProcess,
-            base::GetProcId(GetHandle()));
+            resource_coordinator::CoordinationUnitType::kProcess);
   }
   return process_resource_coordinator_.get();
 }
@@ -3923,6 +3921,10 @@ void RenderProcessHostImpl::OnProcessLaunched() {
     for (auto& observer : observers_)
       observer.RenderProcessReady(this);
   }
+
+  GetProcessResourceCoordinator()->SetProperty(
+      resource_coordinator::mojom::PropertyType::kPID,
+      base::GetProcId(GetHandle()));
 
 #if BUILDFLAG(ENABLE_WEBRTC)
   if (WebRTCInternals::GetInstance()->IsAudioDebugRecordingsEnabled()) {
