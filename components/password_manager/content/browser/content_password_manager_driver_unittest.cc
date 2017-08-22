410f2d243ce829a0b70e50ead6a5a8e33aed90e2
Vadym Doroshenko <dvadym@chromium.org>
2017-08-22 18:39:10 +0000

    [Password Manager] Fix filling when username is given on page.
    On CL https://codereview.chromium.org/2736393003/ it was introduced clearing of passwords except of preferred match before sending to renderer. But in case when the username is given on a page (for example like on account.google.com) not a preferred match but a match that corresponds to the given username should be autofilled. This CL leaves clearing of passwords only in case when passwords should be filled on account select.
    
    Bug: 739870
    Change-Id: Id2571c959588c537d9399df1f5c964e4d7c5c2ef
    Reviewed-on: https://chromium-review.googlesource.com/626060
    Commit-Queue: Vadym Doroshenko <dvadym@chromium.org>
    Reviewed-by: Vasilii Sukhanov <vasilii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496382}
    Former-commit-id: 4e2e8d1b7ed48de0c8ddc096e9daa8c5750f470b

--- components/password_manager/content/browser/content_password_manager_driver_unittest.cc
+++ components/password_manager/content/browser/content_password_manager_driver_unittest.cc
@@ -130,10 +130,8 @@ PasswordFormFillData GetTestPasswordFormFillData() {
   return result;
 }
 
-MATCHER_P(WerePasswordsCleared,
-          should_preferred_password_cleared,
-          "Passwords not cleared") {
-  if (should_preferred_password_cleared && !arg.password_field.value.empty())
+MATCHER(WerePasswordsCleared, "Passwords not cleared") {
+  if (!arg.password_field.value.empty())
     return false;
 
   for (auto& credentials : arg.additional_logins)
@@ -228,13 +226,10 @@ TEST_F(ContentPasswordManagerDriverTest, ClearPasswordsOnAutofill) {
       new ContentPasswordManagerDriver(main_rfh(), &password_manager_client_,
                                        &autofill_client_));
 
-  for (bool wait_for_username : {false, true}) {
     PasswordFormFillData fill_data = GetTestPasswordFormFillData();
-    fill_data.wait_for_username = wait_for_username;
-    EXPECT_CALL(fake_agent_,
-                FillPasswordForm(_, WerePasswordsCleared(wait_for_username)));
+    fill_data.wait_for_username = true;
+    EXPECT_CALL(fake_agent_, FillPasswordForm(_, WerePasswordsCleared()));
     driver->FillPasswordForm(fill_data);
-  }
 }
 
 #if defined(SAFE_BROWSING_DB_LOCAL)
