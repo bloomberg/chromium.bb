d1ab3a370bb39e4df1c969a58d43ca1245f16bcc
Sean Kau <skau@chromium.org>
2017-08-22 17:28:41 +0000

    Prepopulate CupsPrintersManager cache with Synced printers.
    SyncedPrintersManager only emits OnPrintersChanged if printers actually
    change.  Populate the printer cache in CupsPrintersManager when we
    create it.
    
    Bug: 757672
    Change-Id: I83438dd6a6f7d2d3925095c671427aa8881b7688
    Reviewed-on: https://chromium-review.googlesource.com/625277
    Commit-Queue: Sean Kau <skau@chromium.org>
    Reviewed-by: Xiaoqian Dai <xdai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496347}
    Former-commit-id: 330f5e14d2b1e3ac27cd3f11272f20e13b013474

--- chrome/browser/chromeos/printing/cups_printers_manager.cc
+++ chrome/browser/chromeos/printing/cups_printers_manager.cc
@@ -103,6 +103,9 @@ class CupsPrintersManagerImpl : public CupsPrintersManager,
         event_tracker_(event_tracker),
         printers_(kNumPrinterClasses),
         weak_ptr_factory_(this) {
+    // Prime the printer cache with the configured and enterprise printers.
+    printers_[kConfigured] = synced_printers_manager_->GetConfiguredPrinters();
+    printers_[kEnterprise] = synced_printers_manager_->GetEnterprisePrinters();
     synced_printers_manager_observer_.Add(synced_printers_manager_);
 
     // Callbacks may ensue immediately when the observer proxies are set up, so
