ed71a2ff830e0730ad3dce6456ad90a536319f0b
Daichi Hirono <hirono@chromium.org>
2017-08-22 21:43:39 +0000

    Reland "Add DragDropControllerTest to check event target"
    The previous CL crrev.com/496127 was reverted due to memory leak of
    TestWindowDelegate.
    
    The CL uses TestWindowDelegate::CreateSelfDestroyingDelegate() so that
    the delegate will be deleted when the window is deleted.
    
    Also the CL adds RunAllPendingInMessageLoop() so that it processes all
    pending requests when StartDragAndDrop accedentally returned before
    processing drag requests.
    
    Bug: 750997
    Test: DragDropControllerTest.EventTarget
    Change-Id: I522c1b425fa6f4918a92705ace496958f8bab9c4
    Reviewed-on: https://chromium-review.googlesource.com/624771
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Daichi Hirono <hirono@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496454}
    Former-commit-id: f0ae614d02eca58969460f5afb803cc4f9183990

--- ui/aura/mus/drag_drop_controller_mus_unittest.cc
+++ ui/aura/mus/drag_drop_controller_mus_unittest.cc
@@ -12,9 +12,12 @@
 #include "base/task_scheduler/post_task.h"
 #include "base/threading/thread_task_runner_handle.h"
 #include "ui/aura/client/drag_drop_client_observer.h"
+#include "ui/aura/client/drag_drop_delegate.h"
 #include "ui/aura/mus/drag_drop_controller_host.h"
+#include "ui/aura/mus/window_mus.h"
 #include "ui/aura/test/aura_mus_test_base.h"
 #include "ui/aura/test/mus/test_window_tree.h"
+#include "ui/base/dragdrop/drop_target_event.h"
 #include "ui/events/event_utils.h"
 
 namespace aura {
@@ -27,19 +30,46 @@ class DragDropControllerMusTest : public test::AuraMusWmTestBase {
   // test::AuraMusWmTestBase
   void SetUp() override {
     AuraMusWmTestBase::SetUp();
-    drag_drop_controller_ = base::MakeUnique<DragDropControllerMus>(
-        &drag_drop_controller_host_, window_tree());
+    controller_ = base::MakeUnique<DragDropControllerMus>(&controller_host_,
+                                                          window_tree());
+    window_ = std::unique_ptr<aura::Window>(
+        CreateNormalWindow(0, root_window(), nullptr));
   }
 
   void TearDown() override {
-    drag_drop_controller_.reset();
+    window_.reset();
+    controller_.reset();
     AuraMusWmTestBase::TearDown();
   }
 
  protected:
-  std::unique_ptr<DragDropControllerMus> drag_drop_controller_;
+  void PostDragMoveAndDrop() {
+    // Posted task will be run when the inner loop runs in StartDragAndDrop.
+    ASSERT_TRUE(base::ThreadTaskRunnerHandle::IsSet());
+    base::ThreadTaskRunnerHandle::Get()->PostTask(
+        FROM_HERE, base::BindOnce(&DragDropControllerMusTest::DragMoveAndDrop,
+                                  base::Unretained(this)));
+  }
+
+  void StartDragAndDrop() {
+    controller_->StartDragAndDrop(
+        ui::OSExchangeData(), window_->GetRootWindow(), window_.get(),
+        gfx::Point(5, 5), ui::DragDropTypes::DRAG_MOVE,
+        ui::DragDropTypes::DRAG_EVENT_SOURCE_MOUSE);
+  }
+
+  std::unique_ptr<DragDropControllerMus> controller_;
+  std::unique_ptr<aura::Window> window_;
 
  private:
+  void DragMoveAndDrop() {
+    WindowMus* const window_mus = WindowMus::Get(window_.get());
+    controller_->OnDragEnter(window_mus, 0, gfx::Point(5, 20), 0);
+    controller_->OnDragOver(window_mus, 0, gfx::Point(5, 20), 0);
+    controller_->OnCompleteDrop(window_mus, 0, gfx::Point(5, 20), 0);
+    controller_->OnPerformDragDropCompleted(0);
+  }
+
   class TestDragDropControllerHost : public DragDropControllerHost {
    public:
     TestDragDropControllerHost() : serial_(0u) {}
@@ -52,65 +82,88 @@ class DragDropControllerMusTest : public test::AuraMusWmTestBase {
    private:
     uint32_t serial_;
 
-  } drag_drop_controller_host_;
+  } controller_host_;
 
   DISALLOW_COPY_AND_ASSIGN(DragDropControllerMusTest);
 };
 
-class TestObserver : public client::DragDropClientObserver {
- public:
+TEST_F(DragDropControllerMusTest, DragStartedAndEndedEvents) {
   enum class State { kNotInvoked, kDragStartInvoked, kDragEndInvoked };
 
-  TestObserver(DragDropControllerMus* controller)
-      : controller_(controller), state_(State::kNotInvoked) {
-    controller_->AddObserver(this);
-  }
+  class TestObserver : public client::DragDropClientObserver {
+   public:
+    TestObserver() = default;
+    State state() const { return state_; }
 
-  ~TestObserver() override { controller_->RemoveObserver(this); }
+    // Overrides from client::DragDropClientObserver:
+    void OnDragStarted() override {
+      EXPECT_EQ(State::kNotInvoked, state_);
+      state_ = State::kDragStartInvoked;
+    }
+    void OnDragEnded() override {
+      EXPECT_EQ(State::kDragStartInvoked, state_);
+      state_ = State::kDragEndInvoked;
+    }
 
-  State state() const { return state_; }
+   private:
+    State state_{State::kNotInvoked};
 
-  void OnInternalLoopRun() {
-    EXPECT_EQ(State::kDragStartInvoked, state_);
-    controller_->OnPerformDragDropCompleted(0);
-  }
+    DISALLOW_COPY_AND_ASSIGN(TestObserver);
+  } observer;
 
-  // Overrides from client::DragDropClientObserver:
-  void OnDragStarted() override {
-    EXPECT_EQ(State::kNotInvoked, state_);
-    state_ = State::kDragStartInvoked;
-  }
+  controller_->AddObserver(&observer);
+  PostDragMoveAndDrop();
+  StartDragAndDrop();
+  EXPECT_EQ(State::kDragEndInvoked, observer.state());
+  controller_->RemoveObserver(&observer);
+}
 
-  void OnDragEnded() override {
-    EXPECT_EQ(State::kDragStartInvoked, state_);
-    state_ = State::kDragEndInvoked;
-  }
+TEST_F(DragDropControllerMusTest, EventTarget) {
+  enum class State {
+    kNotInvoked,
+    kDragEnteredInvoked,
+    kDragUpdateInvoked,
+    kPerformDropInvoked
+  };
 
- private:
-  DragDropControllerMus* controller_;
-  State state_;
+  class TestDelegate : public client::DragDropDelegate {
+   public:
+    TestDelegate(aura::Window* window) : window_(window) {}
+    State state() const { return state_; }
 
-  DISALLOW_COPY_AND_ASSIGN(TestObserver);
-};
+    // Overrides from client::DragDropClientObserver:
+    void OnDragEntered(const ui::DropTargetEvent& event) override {
+      EXPECT_EQ(State::kNotInvoked, state_);
+      EXPECT_EQ(window_, event.target());
+      state_ = State::kDragEnteredInvoked;
+    }
+    int OnDragUpdated(const ui::DropTargetEvent& event) override {
+      EXPECT_TRUE(State::kDragEnteredInvoked == state_ ||
+                  State::kDragUpdateInvoked == state_);
+      EXPECT_EQ(window_, event.target());
+      state_ = State::kDragUpdateInvoked;
+      return ui::DragDropTypes::DRAG_MOVE;
+    }
+    void OnDragExited() override { ADD_FAILURE(); }
+    int OnPerformDrop(const ui::DropTargetEvent& event) override {
+      EXPECT_EQ(State::kDragUpdateInvoked, state_);
+      EXPECT_EQ(window_, event.target());
+      state_ = State::kPerformDropInvoked;
+      return ui::DragDropTypes::DRAG_MOVE;
+    }
 
-TEST_F(DragDropControllerMusTest, DragStartedAndEndedEvents) {
-  TestObserver observer(drag_drop_controller_.get());
-  std::unique_ptr<aura::Window> window(
-      CreateNormalWindow(0, root_window(), nullptr));
+   private:
+    aura::Window* const window_;
+    State state_{State::kNotInvoked};
 
-  ASSERT_TRUE(base::ThreadTaskRunnerHandle::IsSet());
+    DISALLOW_COPY_AND_ASSIGN(TestDelegate);
+  } delegate(window_.get());
 
-  // Posted task will be run when the inner loop runs in StartDragAndDrop.
-  base::ThreadTaskRunnerHandle::Get()->PostTask(
-      FROM_HERE, base::BindOnce(&TestObserver::OnInternalLoopRun,
-                                base::Unretained(&observer)));
-
-  drag_drop_controller_->StartDragAndDrop(
-      ui::OSExchangeData(), window->GetRootWindow(), window.get(),
-      gfx::Point(5, 5), ui::DragDropTypes::DRAG_MOVE,
-      ui::DragDropTypes::DRAG_EVENT_SOURCE_MOUSE);
-
-  EXPECT_EQ(TestObserver::State::kDragEndInvoked, observer.state());
+  client::SetDragDropDelegate(window_.get(), &delegate);
+  PostDragMoveAndDrop();
+  StartDragAndDrop();
+  EXPECT_EQ(State::kPerformDropInvoked, delegate.state());
+  client::SetDragDropDelegate(window_.get(), nullptr);
 }
 
 }  // namespace
