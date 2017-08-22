fda7d271f72ad475a6e2b4d12c29a6cff48bce28
Walter Korman <wkorman@chromium.org>
2017-08-22 22:02:07 +0000

    Use IsUsingLayerLists() shorthand in two places missed earlier.
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I2b9cb61ce2f9e76a033fd5c1170c4a01d261b0a0
    Reviewed-on: https://chromium-review.googlesource.com/627173
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Commit-Queue: Walter Korman <wkorman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496461}
    Former-commit-id: 2cb2338cd88666b13474529d5a5d0817e4e4b957

--- cc/trees/layer_tree_host.cc
+++ cc/trees/layer_tree_host.cc
@@ -1089,7 +1089,7 @@ void LayerTreeHost::RegisterLayer(Layer* layer) {
   DCHECK(!LayerById(layer->id()));
   DCHECK(!in_paint_layer_contents_);
   layer_id_map_[layer->id()] = layer;
-  if (!settings_.use_layer_lists && layer->element_id()) {
+  if (!IsUsingLayerLists() && layer->element_id()) {
     mutator_host_->RegisterElement(layer->element_id(),
                                    ElementListType::ACTIVE);
   }
@@ -1098,7 +1098,7 @@ void LayerTreeHost::RegisterLayer(Layer* layer) {
 void LayerTreeHost::UnregisterLayer(Layer* layer) {
   DCHECK(LayerById(layer->id()));
   DCHECK(!in_paint_layer_contents_);
-  if (!settings_.use_layer_lists && layer->element_id()) {
+  if (!IsUsingLayerLists() && layer->element_id()) {
     mutator_host_->UnregisterElement(layer->element_id(),
                                      ElementListType::ACTIVE);
   }
