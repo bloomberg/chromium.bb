5d7afad0d0acb746d3748e4aa99c2f91f484e28a
Bo Liu <boliu@chromium.org>
2017-08-22 18:27:15 +0000

    android: Raise importance before lowering
    In the edge case of switching between two tabs of hosted by the same
    process, if importance is lowered on the old tab before raised on the
    new tab, then that process could have its effective importance
    temporarily dropped unexpectedly. On low ram devices, this could mean
    the process is killed by the oom killer, leading to a very bad user
    experience.
    
    Bug: 749347
    Change-Id: I9f6fe4d737808d5a36c3f0f9d7eb41af08898c7f
    Reviewed-on: https://chromium-review.googlesource.com/624719
    Commit-Queue: Bo <boliu@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496374}
    Former-commit-id: a9c115c17670824d1d6035f766439bd9f9052cdc

--- chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/tabmodel/TabModelSelectorImpl.java
@@ -331,9 +331,9 @@ public class TabModelSelectorImpl extends TabModelSelectorBase implements TabMod
 
     @Override
     public void requestToShowTab(Tab tab, TabSelectionType type) {
-        boolean isFromExternalApp = tab != null
-                && tab.getLaunchType() == TabLaunchType.FROM_EXTERNAL_APP;
-
+        boolean isFromExternalApp =
+                tab != null && tab.getLaunchType() == TabLaunchType.FROM_EXTERNAL_APP;
+        Tab tabToDropImportance = null;
         if (mVisibleTab != tab && tab != null && !tab.isNativePage()) {
             TabModelImpl.startTabSwitchLatencyTiming(type);
         }
@@ -347,13 +347,16 @@ public class TabModelSelectorImpl extends TabModelSelectorBase implements TabMod
                     cacheTabBitmap(mVisibleTab);
                 }
                 mVisibleTab.hide();
-                mVisibleTab.setImportance(ChildProcessImportance.NORMAL);
                 mTabSaver.addTabToSaveQueue(mVisibleTab);
             }
+            tabToDropImportance = mVisibleTab;
             mVisibleTab = null;
         }
 
         if (tab == null) {
+            if (tabToDropImportance != null) {
+                tabToDropImportance.setImportance(ChildProcessImportance.NORMAL);
+            }
             notifyChanged();
             return;
         }
@@ -363,7 +366,11 @@ public class TabModelSelectorImpl extends TabModelSelectorBase implements TabMod
         if (mVisibleTab == tab && !mVisibleTab.isHidden()) {
             // The current tab might have been killed by the os while in tab switcher.
             tab.loadIfNeeded();
+            // |tabToDropImportance| must be null, so no need to drop importance.
             return;
+        }
+        if (tabToDropImportance == null) {
+            tabToDropImportance = mVisibleTab;
         }
         mVisibleTab = tab;
 
@@ -372,8 +379,14 @@ public class TabModelSelectorImpl extends TabModelSelectorBase implements TabMod
         // http://crbug.com/316166.
         if (type != TabSelectionType.FROM_EXIT) {
             tab.show(type);
-            mVisibleTab.setImportance(ChildProcessImportance.MODERATE);
             mUma.onShowTab(tab.getId(), tab.isBeingRestored());
+        }
+
+        // Always raise importance before lowering it on old Tab because in case these two Tabs
+        // are hosted by the same process, the process importance is not dropped momentarily.
+        mVisibleTab.setImportance(ChildProcessImportance.MODERATE);
+        if (tabToDropImportance != null) {
+            tabToDropImportance.setImportance(ChildProcessImportance.NORMAL);
         }
     }
 
