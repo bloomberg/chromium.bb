398db1033c0fa41f22b9f6464ff38e2d8e478734
Dave Schuyler <dschuyler@chromium.org>
2017-08-23 00:11:06 +0000

    Revert "[LayoutNG] Bring back native support for multicol."
    This reverts commit b4b3f162e9f62d13aeb62876b89bd05a623ee373 [formerly b7e830ddf678c7daaa41a982fad87156b1bbbe4b].
    
    Reason for revert: <INSERT REASONING HERE>
    
    Based on this comment in the CL summary, "Some layout tests now fail, since we're not using the legacy engine to lay out
    multicol. Quite a few crash or freeze, too."
    
    And that layout tests are failing.
    
    Original change's description:
    > [LayoutNG] Bring back native support for multicol.
    > 
    > Still missing: Copy data back into the LayoutObject tree.
    > 
    > Some layout tests now fail, since we're not using the legacy engine to lay out
    > multicol. Quite a few crash or freeze, too.
    > 
    > Also brought back some of the unit tests. They needed some changes, because of
    > the following:
    > 
    > 1. We now create a fragment for each column, from an anonymous constraint
    > space. I.e. we have one more level of fragments, and it's the column fragments
    > that have their inline offset shifted to the correct column, rather than
    > setting it on the first child block, which is what we did last time these tests
    > worked.
    > 
    > 2. Mistakes introduced when converting test to using setBodyInnerHTML() rather
    > than building the layout trees programmatically:
    > https://codereview.chromium.org/2725773002
    > 
    > 3. New bugs. :)
    > 
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    > Change-Id: I4df85d684f0ddb8f7f0f75d15230a1ab61e9f9a0
    > Reviewed-on: https://chromium-review.googlesource.com/591429
    > Commit-Queue: Morten Stenshorne <mstensho@opera.com>
    > Reviewed-by: Ian Kilpatrick <ikilpatrick@chromium.org>
    > Reviewed-by: Emil A Eklund <eae@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#496446}
    
    TBR=mstensho@opera.com,eae@chromium.org,kojii@chromium.org,ikilpatrick@chromium.org
    
    Change-Id: I63a5c16af3d9c45ec3edb8d923e03449c975273d
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    Reviewed-on: https://chromium-review.googlesource.com/627041
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Commit-Queue: Dave Schuyler <dschuyler@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496517}
    Former-commit-id: 40797dc8f19c1fdb61695dfc67ca6b75448ed4d6

--- third_party/WebKit/Source/core/layout/ng/ng_length_utils.h
+++ third_party/WebKit/Source/core/layout/ng/ng_length_utils.h
@@ -7,8 +7,6 @@
 
 #include "core/CoreExport.h"
 #include "core/layout/MinMaxSize.h"
-#include "core/layout/ng/geometry/ng_box_strut.h"
-#include "core/layout/ng/geometry/ng_logical_size.h"
 #include "core/layout/ng/ng_writing_mode.h"
 #include "platform/text/TextDirection.h"
 #include "platform/wtf/Optional.h"
@@ -16,9 +14,12 @@
 namespace blink {
 class ComputedStyle;
 class LayoutObject;
+class LayoutUnit;
 class Length;
 class NGConstraintSpace;
 class NGLayoutInputNode;
+struct NGBoxStrut;
+struct NGLogicalSize;
 
 enum class LengthResolveType {
   kMinSize,
@@ -130,42 +131,6 @@ CORE_EXPORT LayoutUnit ConstrainByMinMax(LayoutUnit length,
 
 // Returns scrollbar sizes or this layout object.
 NGBoxStrut GetScrollbarSizes(const LayoutObject*);
-
-inline NGBoxStrut CalculateBorderScrollbarPadding(
-    const NGConstraintSpace& constraint_space,
-    const ComputedStyle& style,
-    const LayoutObject* layout_object) {
-  return ComputeBorders(constraint_space, style) +
-         ComputePadding(constraint_space, style) +
-         GetScrollbarSizes(layout_object);
-}
-
-inline NGLogicalSize CalculateBorderBoxSize(
-    const NGConstraintSpace& constraint_space,
-    const ComputedStyle& style,
-    const Optional<MinMaxSize>& min_and_max,
-    LayoutUnit block_content_size = NGSizeIndefinite) {
-  return NGLogicalSize(
-      ComputeInlineSizeForFragment(constraint_space, style, min_and_max),
-      ComputeBlockSizeForFragment(constraint_space, style, block_content_size));
-}
-
-inline NGLogicalSize CalculateContentBoxSize(
-    const NGLogicalSize border_box_size,
-    const NGBoxStrut& border_scrollbar_padding) {
-  NGLogicalSize size = border_box_size;
-  size.inline_size -= border_scrollbar_padding.InlineSum();
-  size.inline_size = std::max(size.inline_size, LayoutUnit());
-
-  // Our calculated block-axis size may still be indefinite. If so, just leave
-  // the size as NGSizeIndefinite instead of subtracting borders and padding.
-  if (size.block_size != NGSizeIndefinite) {
-    size.block_size -= border_scrollbar_padding.BlockSum();
-    size.block_size = std::max(size.block_size, LayoutUnit());
-  }
-
-  return size;
-}
 
 }  // namespace blink
 
