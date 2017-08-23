60a12f49854a673d85925687bb6b3cf8c281b2ff
Michael Lippautz <mlippautz@chromium.org>
2017-08-23 17:34:12 +0000

    [oilpan] Fix tests using WTF::HashTableDeletedValue
    The tests were leaking the implementation details of this value and
    manually casting a pointer to it. This is wrong as we rely on the fact
    that the constructor with the proper type is called.
    
    Bug: chromium:757440
    Change-Id: I6148d9e10dbffca23df0c7680f55b03439c4e1f2
    Reviewed-on: https://chromium-review.googlesource.com/628676
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496723}
    Former-commit-id: cfddb5e9de4115f9a6ff7854bb0f4507dcb7e3e6

--- third_party/WebKit/Source/platform/heap/HeapTest.cpp
+++ third_party/WebKit/Source/platform/heap/HeapTest.cpp
@@ -196,13 +196,13 @@ struct PairWithWeakHandling : public StrongWeakPair {
   // deleted
   // with a pointer to -1 in the first field.
   PairWithWeakHandling(WTF::HashTableDeletedValueType)
-      : StrongWeakPair(reinterpret_cast<IntWrapper*>(-1), nullptr) {}
+      : StrongWeakPair(WTF::kHashTableDeletedValue, nullptr) {}
 
   // Used by the HashTable (via the HashTrait) to skip deleted slots in the
   // table. Recognizes objects that were 'constructed' using the above
   // constructor.
   bool IsHashTableDeletedValue() const {
-    return first == reinterpret_cast<IntWrapper*>(-1);
+    return first.IsHashTableDeletedValue();
   }
 
   // Since we don't allocate independent objects of this type, we don't need
@@ -5819,12 +5819,9 @@ class AllocatesOnAssignment {
     value_ = new IntWrapper(other.value_->Value());
   }
 
-  AllocatesOnAssignment(DeletedMarker)
-      : value_(reinterpret_cast<IntWrapper*>(-1)) {}
+  AllocatesOnAssignment(DeletedMarker) : value_(WTF::kHashTableDeletedValue) {}
 
-  inline bool IsDeleted() const {
-    return value_ == reinterpret_cast<IntWrapper*>(-1);
-  }
+  inline bool IsDeleted() const { return value_.IsHashTableDeletedValue(); }
 
   DEFINE_INLINE_TRACE() { visitor->Trace(value_); }
 
