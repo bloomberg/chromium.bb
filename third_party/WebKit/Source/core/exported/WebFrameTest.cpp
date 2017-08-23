7fa23155d336f4f2b7eccdc72322d04ce105ae02
Pedro Amaral <amaralp@chromium.org>
2017-08-23 01:20:01 +0000

    Fixing non-located context menu selected text
    First a little background on context menu events. A context menu
    event is actually a mouse event with the contextmenu type. This means
    that the context menu event comes with a location. This
    location is then used to know if the triggered menu had selected
    text. This is done in ContextMenuClient::ShowContextMenu() by checking
    if the HitTestResult landed inside of a selection. If it did then
    WebContextMenuData.selected_text is set to the selected text. This causes
    a problem for non-located menus (menus triggered by
    EventHandler::ShowNonLocatedContextMenu(), some examples are keyboard,
    contextual search tap, or touch handle triggered menus). Non-located
    menus set their location to the bottom left corner of the first selection
    rect. This causes an issue when there are multiple
    lines of text and we only select the first one. When the context menu
    is fired it's location is between the first and second lines of text.
    If the css style line-height is small enough (say 0.7 em like in the
    added test) the hit test will hit the second (unselected) line of text
    and we will incorrectly think that the event did not hit a selection.
    This caused the linked bug where the floating popup menu was not shown.
    This CL fixes this problem by having the non-located menu trigger on
    the midpoint of the left edge of the first selection rect instead of on
    the bottom left corner.
    
    Bug: 729969
    Change-Id: Iec640fd57dc949f0f9657c2c473f716e6d70799e
    Reviewed-on: https://chromium-review.googlesource.com/611483
    Reviewed-by: David Bokan <bokan@chromium.org>
    Reviewed-by: Alexandre Elias <aelias@chromium.org>
    Commit-Queue: Pedro Amaral <amaralp@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496542}
    Former-commit-id: 2d5e16ef240fb4da2d79934f3977d2cba243b8e9

--- third_party/WebKit/Source/core/exported/WebFrameTest.cpp
+++ third_party/WebKit/Source/core/exported/WebFrameTest.cpp
@@ -12410,6 +12410,38 @@ TEST_P(ParameterizedWebFrameTest, ContextMenuDataPasswordSelectedText) {
   EXPECT_FALSE(frame.GetMenuData().selected_text.IsEmpty());
 }
 
+TEST_P(ParameterizedWebFrameTest, ContextMenuDataNonLocatedMenu) {
+  ContextMenuWebFrameClient frame;
+  FrameTestHelpers::WebViewHelper web_view_helper;
+  WebViewImpl* web_view = web_view_helper.Initialize(&frame);
+  const std::string& html =
+      "<div style='font-size: 1000%; line-height: 0.7em'>Select me<br/>"
+      "Next line</div>";
+  FrameTestHelpers::LoadHTMLString(web_view->MainFrameImpl(), html,
+                                   ToKURL("about:blank"));
+  web_view->Resize(WebSize(500, 300));
+  web_view->UpdateAllLifecyclePhases();
+  RunPendingTasks();
+  web_view->SetInitialFocus(false);
+  RunPendingTasks();
+
+  WebMouseEvent mouse_event(WebInputEvent::kMouseDown,
+                            WebInputEvent::kNoModifiers,
+                            WebInputEvent::kTimeStampForTesting);
+
+  mouse_event.button = WebMouseEvent::Button::kLeft;
+  mouse_event.SetPositionInWidget(0, 0);
+  mouse_event.click_count = 2;
+  web_view->HandleInputEvent(WebCoalescedInputEvent(mouse_event));
+
+  web_view->ShowContextMenu(kMenuSourceTouch);
+
+  RunPendingTasks();
+  web_view_helper.Reset();
+  EXPECT_EQ(frame.GetMenuData().source_type, kMenuSourceTouch);
+  EXPECT_FALSE(frame.GetMenuData().selected_text.IsEmpty());
+}
+
 TEST_P(ParameterizedWebFrameTest, LocalFrameWithRemoteParentIsTransparent) {
   FrameTestHelpers::WebViewHelper helper;
   helper.InitializeRemote();
