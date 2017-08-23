1e43c72cdf90f8b4d77336e464bfee65d114e8fd
Trent Apted <tapted@chromium.org>
2017-08-23 07:54:02 +0000

    Disable Flaky NewAndOldBehavior/KeyboardControllerTest.KeyboardSizeMultiRootWindow/* on Windows
    TBR=oka@chromium.org
    
    Bug: 757044
    Change-Id: I45a9da9ce9418fae0c5e197e60d1e8a70bfd2188
    Reviewed-on: https://chromium-review.googlesource.com/627347
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496620}
    Former-commit-id: 09521283ba53ee06035be8cfe3059917a9223fbe

--- ui/keyboard/keyboard_controller_unittest.cc
+++ ui/keyboard/keyboard_controller_unittest.cc
@@ -11,6 +11,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/run_loop.h"
 #include "base/test/scoped_task_environment.h"
+#include "build/build_config.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/aura/client/focus_client.h"
 #include "ui/aura/test/aura_test_helper.h"
@@ -371,7 +372,18 @@ TEST_P(KeyboardControllerTest, KeyboardSize) {
   VerifyKeyboardWindowSize(container, keyboard);
 }
 
-TEST_P(KeyboardControllerTest, KeyboardSizeMultiRootWindow) {
+// Flaky on Windows. See http://crbug.com/757044
+#if defined(OS_WIN)
+#define MAYBE_KeyboardSizeMultiRootWindow DISABLED_KeyboardSizeMultiRootWindow
+#else
+#define MAYBE_KeyboardSizeMultiRootWindow KeyboardSizeMultiRootWindow
+#endif
+
+// Since TEST_P does concatenation, macro prescan only occurs if it's invoked
+// indirectly.
+#define TEST_P_INDIRECT(a, b) TEST_P(a, b)
+
+TEST_P_INDIRECT(KeyboardControllerTest, MAYBE_KeyboardSizeMultiRootWindow) {
   aura::Window* container(controller()->GetContainerWindow());
   aura::Window* keyboard(ui()->GetContentsWindow());
   gfx::Rect screen_bounds = root_window()->bounds();
