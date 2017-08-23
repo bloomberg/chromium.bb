c3453c3df046605840d60ee49884c818e9d29fb2
Ben Chan <benchan@chromium.org>
2017-08-23 22:30:44 +0000

    Show PUK unlock dialog if a PUK is required to unlock the SIM.
    This CL updates the SIM unlock UI to show a PUK unlock dialog, instead
    of a PIN unlock dialog, if a PUK is required to unlock the SIM.
    
    BUG=758013
    TEST=Tested the following:
    - When a PIN is required to unlock the SIM, verify that a PIN unlock
      dialog is shown by clicking the "Unlock" button on the settings UI.
    - When a PUK is required to unlock the SIM, verify that a PUK unlock
      dialog is shown by clicking the "Unlock" button on the settings UI.
    
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I5a45929394c540170d026a1f040f25009ee02eb3
    Reviewed-on: https://chromium-review.googlesource.com/627644
    Commit-Queue: Ben Chan <benchan@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496835}
    Former-commit-id: 62a532d02ae7c3f21d1b6405d82a4371a670e44e

--- chrome/browser/resources/settings/internet_page/network_siminfo.js
+++ chrome/browser/resources/settings/internet_page/network_siminfo.js
@@ -200,15 +200,17 @@ Polymer({
   },
 
   /**
-   * Opens the Unlock PIN dialog.
+   * Opens the Unlock PIN / PUK dialog.
    * @param {!Event} event
    * @private
    */
   onUnlockPinTap_: function(event) {
     event.preventDefault();
-    this.error_ = ErrorType.NONE;
-    this.$.unlockPin.value = '';
-    this.$.unlockPinDialog.showModal();
+    if (this.pukRequired_) {
+      this.showUnlockPukDialog_();
+    } else {
+      this.showUnlockPinDialog_();
+    }
   },
 
   /**
@@ -231,6 +233,13 @@ Polymer({
         this.$.unlockPinDialog.close();
       }
     });
+  },
+
+  /** @private */
+  showUnlockPinDialog_: function() {
+    this.error_ = ErrorType.NONE;
+    this.$.unlockPin.value = '';
+    this.$.unlockPinDialog.showModal();
   },
 
   /** @private */
