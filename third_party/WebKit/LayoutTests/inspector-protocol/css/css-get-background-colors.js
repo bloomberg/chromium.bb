c17d85425b79924530347c28403bcaa10433bb2d
Alice Boxhall <aboxhall@chromium.org>
2017-08-23 00:59:40 +0000

    [Devtools] Use content bounds for elements with no direct text descendants
    Bug: 514674
    Change-Id: If1d1cb8bcbf4fa1048a1a9a5d24eafee405d92ef
    Reviewed-on: https://chromium-review.googlesource.com/622112
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Commit-Queue: Alice Boxhall <aboxhall@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496534}
    Former-commit-id: fea67b7035aaca133175c60c8ebc66c655993161

--- third_party/WebKit/LayoutTests/inspector-protocol/css/css-get-background-colors.js
+++ third_party/WebKit/LayoutTests/inspector-protocol/css/css-get-background-colors.js
@@ -17,113 +17,117 @@
 
   testRunner.runTestSuite([
     async function testNoText() {
-      testRunner.log('No text: should be null');
-      await testGetBgColors('.noText > p');
+      testRunner.log('No text (and not a positioned element): should be null');
+      await testGetBgColors('.noText p');
     },
     async function testNoBgColor() {
       testRunner.log('No background color: should be white');
-      await testGetBgColors('.noBgColor > p');
+      await testGetBgColors('.noBgColor p');
     },
     async function testOpaqueBgColor() {
       testRunner.log('Opaque background color: should be red');
-      await testGetBgColors('.opaqueBgColor > p');
+      await testGetBgColors('.opaqueBgColor p');
+    },
+    async function testNoDirectText() {
+      testRunner.log('No direct text: should be red');
+      await testGetBgColors('.noDirectText p');
     },
     async function testLayeredOpaqueBgColors() {
       testRunner.log('Opaque background color in front of another opaque background color: should be blue');
-      await testGetBgColors('.layeredOpaqueBgColors > p');
+      await testGetBgColors('.layeredOpaqueBgColors p');
     },
     async function testOneSemitransparentBgColor() {
       testRunner.log('Semi-transparent background color: should be light red');
-      await testGetBgColors('.semitransparentBgColor > p');
+      await testGetBgColors('.semitransparentBgColor p');
     },
     async function testTwoSemitransparentBgColors() {
       testRunner.log('Two layered semi-transparent background colors: should be medium red');
-      await testGetBgColors('.twoSemitransparentBgColors > p');
+      await testGetBgColors('.twoSemitransparentBgColors p');
     },
     async function testOpaqueGradientBackground() {
       testRunner.log('Opaque gradient: should be red and black');
-      await testGetBgColors('.opaqueGradientBg > p');
+      await testGetBgColors('.opaqueGradientBg p');
     },
     async function testOpaqueGradientBackgroundBehindScrim() {
       testRunner.log('Opaque gradient behind semi-transparent color: should be light red and 50% grey');
-      await testGetBgColors('.opaqueGradientBehindScrim > p');
+      await testGetBgColors('.opaqueGradientBehindScrim p');
     },
     async function testOpaqueGradientBackgroundWithColorBackground() {
       testRunner.log('Opaque gradient and solid color background on same element: should be red and black');
-      await testGetBgColors('.opaqueGradientAndColorBg > p');
+      await testGetBgColors('.opaqueGradientAndColorBg p');
     },
     async function testPartiallyTransparentGradientBackground() {
       testRunner.log('Semi-transparent gradient: should be light red and 50% grey');
-      await testGetBgColors('.semitransparentGradientBg > p');
+      await testGetBgColors('.semitransparentGradientBg p');
     },
     async function testPartiallyTransparentGradientAndColorBackground() {
       testRunner.log('Semi-transparent gradient and solid color on same element: should be dark red and 50% grey');
-      await testGetBgColors('.semitransparentGradientAndColorBg > p');
+      await testGetBgColors('.semitransparentGradientAndColorBg p');
     },
     async function testTwoPartiallyTransparentGradientBackgrounds() {
       testRunner.log('Layered semi-transparent gradients: should be empty array');
-      await testGetBgColors('.twoSemitransparentGradientBgs > p');
+      await testGetBgColors('.twoSemitransparentGradientBgs p');
     },
     async function testPartiallyOverlappingBackground() {
       testRunner.log('Partially overlapping background: should be empty array');
-      await testGetBgColors('.partiallyOverlappingBackground > p');
+      await testGetBgColors('.partiallyOverlappingBackground p');
     },
     async function smallerBackground() {
       testRunner.log('Background smaller than text: should be empty array');
-      await testGetBgColors('.smallerBackground > p');
+      await testGetBgColors('.smallerBackground p');
     },
     async function testObscuredPartiallyOverlappingBackground() {
       testRunner.log('Red background obscuring partially overlapping blue background: should be red only');
-      await testGetBgColors('.obscuredPartiallyOverlappingBackground > p');
+      await testGetBgColors('.obscuredPartiallyOverlappingBackground p');
     },
     async function testBackgroundImage() {
       testRunner.log('Background image: should be empty array');
-      await testGetBgColors('.backgroundImage > p');
+      await testGetBgColors('.backgroundImage p');
     },
     async function testBackgroundImageAndBgColor() {
       testRunner.log('Background image with background color: should be empty array');
-      await testGetBgColors('.backgroundImageAndBgColor > p');
+      await testGetBgColors('.backgroundImageAndBgColor p');
     },
 
     async function testBackgroundImageBehindScrim() {
       testRunner.log('Background image behind scrim: should be semi-transparent white');
-      await testGetBgColors('.backgroundImageBehindScrim > p');
+      await testGetBgColors('.backgroundImageBehindScrim p');
     },
     async function testForegroundImage() {
       testRunner.log('Image behind text: should be empty array');
-      await testGetBgColors('.foregroundImage > p');
+      await testGetBgColors('.foregroundImage p');
     },
     async function testForegroundImageBehindScrim() {
       testRunner.log('Image behind scrim: should be semi-transparent white');
-      await testGetBgColors('.foregroundImageBehindScrim > p');
+      await testGetBgColors('.foregroundImageBehindScrim p');
     },
     async function testForegroundCanvas() {
       testRunner.log('Canvas behind text: should be empty array');
-      await testGetBgColors('.canvas > p');
+      await testGetBgColors('.canvas p');
     },
     async function testForegroundEmbed() {
       testRunner.log('Embed behind text: should be empty array');
-      await testGetBgColors('.embed > p');
+      await testGetBgColors('.embed p');
     },
     async function testForegroundObject() {
       testRunner.log('Object behind text: should be empty array');
-      await testGetBgColors('.object > p');
+      await testGetBgColors('.object p');
     },
     async function testForegroundPicture() {
       testRunner.log('Picture behind text: should be empty array');
-      await testGetBgColors('.picture > p');
+      await testGetBgColors('.picture p');
     },
     async function testForegroundSVG() {
       testRunner.log('SVG behind text: should be empty array');
-      await testGetBgColors('.svg > p');
+      await testGetBgColors('.svg p');
     },
     async function testForegroundVideo() {
       testRunner.log('Video behind text: should be empty array');
-      await testGetBgColors('.video > p');
+      await testGetBgColors('.video p');
     },
     async function testShadowDOM() {
       testRunner.log('Background color in Shadow DOM: should be blue');
-      await testGetBgColors('.shadowDOM > .shadowHost > p');
+      await testGetBgColors('.shadowDOM > .shadowHost p');
     },
   ]);
 });
