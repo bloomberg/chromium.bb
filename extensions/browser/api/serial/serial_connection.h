92a187a1b0c320be721c502e9f1d1f4147dd045a
Han Leon <leon.han@intel.com>
2017-08-23 00:39:24 +0000

    [DeviceService] Use WeakPtrFactory instead of SupportsWeakPtr<> for SerialConnection
    This CL changes device::SerialConnection to hold a base::WeakPtrFactory
    member rather than inherit base::SupportsWeakPtr<> to avoid the subtle
    use-after-destroy issue:
    1.
    SerialConnection dtor starts, destroyes its'
    device::mojom::SerialIoHandlerPtr |io_handler_| member.
    2.
    The SerialIoHandlerPtr dtor triggers some one callback XXX wrapped by
    ScopedCallbackRunner to run.
    3.
    The callback XXX was actually bound with a weak ptr of SerialConnection,
    at this time point the parent class SupportsWeakPtr's dtor has not been
    triggered yet so that the weak ptr is still in a valid state, although
    we are already inside the execution context of SerialConnection dtor.
    
    A base::WeakPtrFactory member at the end of member list will be
    destroyed immediatelly once entering SerialConnection dtor, thus in the
    above step 3 the bound weak ptr should be identified as invalid so that
    callback XXX won't be called at all. This is the expected behavior.
    
    BUG=757756
    
    Change-Id: Ib73ac55dcfba250c4f8b96fb9e3d3e85490ea9a0
    Reviewed-on: https://chromium-review.googlesource.com/626197
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Han Leon <leon.han@intel.com>
    Cr-Commit-Position: refs/heads/master@{#496526}
    Former-commit-id: b9f8dc9da6329c722262a162d750f3096b14a5af

--- extensions/browser/api/serial/serial_connection.h
+++ extensions/browser/api/serial/serial_connection.h
@@ -28,8 +28,7 @@ namespace extensions {
 // corresponds with an open serial port in remote side(Device Service). NOTE:
 // Instances of this object should only be constructed on the IO thread, and all
 // methods should only be called on the IO thread unless otherwise noted.
-class SerialConnection : public ApiResource,
-                         public base::SupportsWeakPtr<SerialConnection> {
+class SerialConnection : public ApiResource {
  public:
   using OpenCompleteCallback = device::mojom::SerialIoHandler::OpenCallback;
   using GetInfoCompleteCallback =
@@ -211,6 +210,8 @@ class SerialConnection : public ApiResource,
   // Closure which is set by client and will be called when |io_handler_|
   // connection encountered an error.
   base::OnceClosure connection_error_handler_;
+
+  base::WeakPtrFactory<SerialConnection> weak_factory_;
 };
 
 }  // namespace extensions
