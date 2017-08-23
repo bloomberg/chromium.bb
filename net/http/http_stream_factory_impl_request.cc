c127c380a569cb151f47aa1bf219147c96e437ce
Helen Li <xunjieli@chromium.org>
2017-08-23 22:25:18 +0000

    Revert "Make HttpStreamFactoryImpl::Request own JobController"
    This reverts commit 2646c7683ee05ef7711a2c1f7a0ace06f84052ce [formerly b6ea9d7648b75a2215126a020bd3c0db0cf1ccab].
    
    Reason for revert: There breaks ReportBrokenAlternativeService()
    
    Original change's description:
    > Make HttpStreamFactoryImpl::Request own JobController
    > 
    > JobController no longer outlives Request after
    > ad673e4c957570bb56f3e94d84f709871f89528b [formerly 7f0e229fb317da182573209639c32529755dd1f5]. This CL simplies the lifetime of
    > JobController for non-preconnects.
    > 
    > Follow-up CLs will clean up unused logic and duplicated param passing.
    > 
    > Bug: 475060, 757548
    > Change-Id: I4542f0f4f6386e3eeae47c41da8d6e98cce4dd83
    > Reviewed-on: https://chromium-review.googlesource.com/627311
    > Reviewed-by: Ryan Hamilton <rch@chromium.org>
    > Reviewed-by: Zhongyi Shi <zhongyi@chromium.org>
    > Commit-Queue: Helen Li <xunjieli@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#496785}
    
    TBR=rch@chromium.org,xunjieli@chromium.org,zhongyi@chromium.org
    
    Change-Id: Id42cc4b5fafbfcba00ae572c3d2505100c93e376
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 475060, 757548
    Reviewed-on: https://chromium-review.googlesource.com/630396
    Reviewed-by: Ryan Hamilton <rch@chromium.org>
    Commit-Queue: Ryan Hamilton <rch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496831}
    Former-commit-id: f62edf73906fa20106211c5f76de5e03278fdf0a

--- net/http/http_stream_factory_impl_request.cc
+++ net/http/http_stream_factory_impl_request.cc
@@ -11,7 +11,6 @@
 #include "base/stl_util.h"
 #include "net/http/bidirectional_stream_impl.h"
 #include "net/http/http_stream_factory_impl_job.h"
-#include "net/http/http_stream_factory_impl_job_controller.h"
 #include "net/log/net_log_event_type.h"
 #include "net/spdy/chromium/spdy_http_stream.h"
 #include "net/spdy/chromium/spdy_session.h"
@@ -20,36 +19,28 @@ namespace net {
 
 HttpStreamFactoryImpl::Request::Request(
     const GURL& url,
+    Helper* helper,
     HttpStreamRequest::Delegate* delegate,
     WebSocketHandshakeStreamBase::CreateHelper*
         websocket_handshake_stream_create_helper,
     const NetLogWithSource& net_log,
-    std::unique_ptr<JobController> job_controller,
-    StreamType stream_type,
-    RequestPriority priority)
+    StreamType stream_type)
     : url_(url),
-      delegate_(delegate),
+      helper_(helper),
       websocket_handshake_stream_create_helper_(
           websocket_handshake_stream_create_helper),
       net_log_(net_log),
-      job_controller_(std::move(job_controller)),
       completed_(false),
       was_alpn_negotiated_(false),
       negotiated_protocol_(kProtoUnknown),
       using_spdy_(false),
-      stream_type_(stream_type),
-      priority_(priority) {
+      stream_type_(stream_type) {
   net_log_.BeginEvent(NetLogEventType::HTTP_STREAM_REQUEST);
 }
 
 HttpStreamFactoryImpl::Request::~Request() {
   net_log_.EndEvent(NetLogEventType::HTTP_STREAM_REQUEST);
-  job_controller_->OnRequestComplete();
-}
-
-void HttpStreamFactoryImpl::Request::Start() {
-  job_controller_->Start(delegate_, websocket_handshake_stream_create_helper_,
-                         net_log_, stream_type_, priority_, this);
+  helper_->OnRequestComplete();
 }
 
 void HttpStreamFactoryImpl::Request::Complete(bool was_alpn_negotiated,
@@ -67,8 +58,8 @@ void HttpStreamFactoryImpl::Request::OnStreamReadyOnPooledConnection(
     const ProxyInfo& used_proxy_info,
     std::unique_ptr<HttpStream> stream) {
   DCHECK(completed_);
-  job_controller_->OnStreamReadyOnPooledConnection(
-      used_ssl_config, used_proxy_info, std::move(stream));
+  helper_->OnStreamReadyOnPooledConnection(used_ssl_config, used_proxy_info,
+                                           std::move(stream));
 }
 
 void HttpStreamFactoryImpl::Request::
@@ -77,20 +68,20 @@ void HttpStreamFactoryImpl::Request::
         const ProxyInfo& used_proxy_info,
         std::unique_ptr<BidirectionalStreamImpl> stream) {
   DCHECK(completed_);
-  job_controller_->OnBidirectionalStreamImplReadyOnPooledConnection(
+  helper_->OnBidirectionalStreamImplReadyOnPooledConnection(
       used_ssl_config, used_proxy_info, std::move(stream));
 }
 
 int HttpStreamFactoryImpl::Request::RestartTunnelWithProxyAuth() {
-  return job_controller_->RestartTunnelWithProxyAuth();
+  return helper_->RestartTunnelWithProxyAuth();
 }
 
 void HttpStreamFactoryImpl::Request::SetPriority(RequestPriority priority) {
-  job_controller_->SetPriority(priority);
+  helper_->SetPriority(priority);
 }
 
 LoadState HttpStreamFactoryImpl::Request::GetLoadState() const {
-  return job_controller_->GetLoadState();
+  return helper_->GetLoadState();
 }
 
 bool HttpStreamFactoryImpl::Request::was_alpn_negotiated() const {
