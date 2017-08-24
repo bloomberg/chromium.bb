939c525f5690a391fbced0873b577ff1c961ace9
Istiaque Ahmed <lazyboy@chromium.org>
2017-08-24 22:40:51 +0000

    BookmarkManagerPrivate: Treat missing bookmark item as non-fatal error.
    It is entirely possible to invoke cut/paste action on a bookmark
    item that was deleted by other means. Treat this as a non-fatal error,
    as opposed to killing the renderer. Also expand this treatment to similar
    startDrag function.
    
    
    
    on 3 dot menu to bring up the contextual menu of a particular bookmark, leave
    the context menu opened. On the second window, delete that bookmark. On
    the first window, select "Copy URL". Expect no more renderer/ crash/
    
    Bug: 739260
    Test: Open two browser windows to chrome://bookmarks. On one window, click
    Change-Id: If452108aedb8bf0d27ffc1853e46f6746c4fc68f
    Reviewed-on: https://chromium-review.googlesource.com/627266
    Reviewed-by: Devlin <rdevlin.cronin@chromium.org>
    Commit-Queue: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497226}
    Former-commit-id: c7b1baa97de6b0d9884bd8e39dac474c2f2e4ee4

--- chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api.cc
+++ chrome/browser/extensions/api/bookmark_manager_private/bookmark_manager_private_api.cc
@@ -371,9 +371,12 @@ void BookmarkManagerPrivateDragEventRouter::ClearBookmarkNodeData() {
 bool ClipboardBookmarkManagerFunction::CopyOrCut(bool cut,
     const std::vector<std::string>& id_list) {
   BookmarkModel* model = GetBookmarkModel();
-  bookmarks::ManagedBookmarkService* managed = GetManagedBookmarkService();
   std::vector<const BookmarkNode*> nodes;
-  EXTENSION_FUNCTION_VALIDATE(GetNodesFromVector(model, id_list, &nodes));
+  if (!GetNodesFromVector(model, id_list, &nodes)) {
+    error_ = "Could not find bookmark nodes with given ids.";
+    return false;
+  }
+  bookmarks::ManagedBookmarkService* managed = GetManagedBookmarkService();
   if (cut && bookmarks::HasDescendantsOf(nodes, managed->managed_node())) {
     error_ = bookmark_keys::kModifyManagedError;
     return false;
@@ -561,8 +564,8 @@ bool BookmarkManagerPrivateStartDragFunction::RunOnReady() {
   BookmarkModel* model =
       BookmarkModelFactory::GetForBrowserContext(GetProfile());
   std::vector<const BookmarkNode*> nodes;
-  EXTENSION_FUNCTION_VALIDATE(
-      GetNodesFromVector(model, params->id_list, &nodes));
+  if (!GetNodesFromVector(model, params->id_list, &nodes))
+    return false;
 
   content::WebContents* web_contents = GetAssociatedWebContents();
   CHECK(web_contents);
