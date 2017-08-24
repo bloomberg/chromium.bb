701e1cd1f956f2b7a12499b2396814c453b3013f
Camille Lamy <clamy@chromium.org>
2017-08-24 15:54:23 +0000

    Revert "Disable 3 NetErrorTabHelperTest with PlzNavigate"
    This reverts commit 73f6a88a75a21899cf62802e63938fe562d2ad41 [formerly 4709304c221cb844ccd6935648dc3454ba17e850].
    
    Reason for revert:
    The tests were fixed on the bot by https://chromium-review.googlesource.com/c/chromium/src/+/621566 before this CL landed.
    
    Original change's description:
    > Disable 3 NetErrorTabHelperTest with PlzNavigate
    > 
    > This CL disables 3 NetErrorTabHelperTests that have been crashing with
    > Plznavigate since
    > https://chromium-review.googlesource.com/c/chromium/src/+/616938 landed.
    > 
    > BUG=757606
    > 
    > Change-Id: Ibb809652d3e0151e47a57114c1a2008ddcfa61df
    > Reviewed-on: https://chromium-review.googlesource.com/629080
    > Reviewed-by: Anthony Berent <aberent@chromium.org>
    > Reviewed-by: Matt Menke <mmenke@chromium.org>
    > Commit-Queue: Camille Lamy <clamy@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#496709}
    
    TBR=aberent@chromium.org,clamy@chromium.org,mmenke@chromium.org
    
    Change-Id: I0673b01eb43c0d38a6185d16a91e4462d47c0af0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 757606
    Reviewed-on: https://chromium-review.googlesource.com/632136
    Reviewed-by: Matt Menke <mmenke@chromium.org>
    Commit-Queue: Camille Lamy <clamy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497063}
    Former-commit-id: e0554309854ad7efc0abd36ac882ac6e00bd3046

--- chrome/browser/net/net_error_tab_helper_unittest.cc
+++ chrome/browser/net/net_error_tab_helper_unittest.cc
@@ -9,7 +9,6 @@
 #include "components/error_page/common/net_error_info.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_handle.h"
-#include "content/public/common/browser_side_navigation_policy.h"
 #include "content/public/test/navigation_simulator.h"
 #include "content/public/test/test_renderer_host.h"
 #include "net/base/net_errors.h"
@@ -370,11 +369,6 @@ TEST_F(NetErrorTabHelperTest, NoDiagnosticsForNonHttpSchemes) {
 
 #if BUILDFLAG(ENABLE_OFFLINE_PAGES)
 TEST_F(NetErrorTabHelperTest, DownloadPageLater) {
-  if (content::IsBrowserSideNavigationEnabled()) {
-    // TODO(clamy): re-enable this test with PlzNavigate. See
-    // http://crbug.com/757606.
-    return;
-  }
   GURL url("http://somewhere:123/");
   LoadURL(url, false /*succeeded*/);
   tab_helper()->OnDownloadPageLater();
@@ -395,20 +389,10 @@ TEST_F(NetErrorTabHelperTest, NoDownloadPageLaterOnNonErrorPage) {
 // deal with pending RFH correctly. This works because most tests only load
 // once. So workaround it by puting each test case in a separate test.
 TEST_F(NetErrorTabHelperTest, NoDownloadPageLaterForNonHttpSchemes1) {
-  if (content::IsBrowserSideNavigationEnabled()) {
-    // TODO(clamy): re-enable this test with PlzNavigate. See
-    // http://crbug.com/757606.
-    return;
-  }
   NoDownloadPageLaterForNonHttpSchemes("file:///blah/blah", false);
 }
 
 TEST_F(NetErrorTabHelperTest, NoDownloadPageLaterForNonHttpSchemes2) {
-  if (content::IsBrowserSideNavigationEnabled()) {
-    // TODO(clamy): re-enable this test with PlzNavigate. See
-    // http://crbug.com/757606.
-    return;
-  }
   NoDownloadPageLaterForNonHttpSchemes("chrome://blah/", false);
 }
 
