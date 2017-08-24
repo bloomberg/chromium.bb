e15ef9e99e3291ed118d94934b5c2d790d58e7ba
Anthony Berent <aberent@chromium.org>
2017-08-24 09:34:54 +0000

    Revert "Android: Add logging_ext.py module."
    This reverts commit 3b5f203070e6a92d9ff8d2ad0a1ddfcc5fab4408 [formerly 9d96ef6894f46439e73300cdecf8ba30285e77f6].
    
    Reason for revert: blocks rolls into clank on ToT bot.
    BUG=758505
    
    Original change's description:
    > Android: Add logging_ext.py module.
    > 
    > Adds color in a better way to pylib code.
    > Additionally added some more useful test logs (such as whether
    > tests pass/fail as they are run and predicted time remaining).
    > 
    > Adding -quiet option which will make the logs as they were
    > previously.
    > 
    > Change-Id: I0cec6334a38544ccbb4a4fde6df65b6b56c2547d
    > Reviewed-on: https://chromium-review.googlesource.com/603916
    > Commit-Queue: Michael Case <mikecase@chromium.org>
    > Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    > Reviewed-by: John Budorick <jbudorick@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#496922}
    
    TBR=mikecase@chromium.org,agrieve@chromium.org,yolandyan@chromium.org,jbudorick@chromium.org
    
    Change-Id: I11ca3412b5b3516bfd7107aa3f0732e7331b9941
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://chromium-review.googlesource.com/631724
    Reviewed-by: Anthony Berent <aberent@chromium.org>
    Commit-Queue: Anthony Berent <aberent@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496992}
    Former-commit-id: d55737c8d3fa961756f4d089ebc1cba10a67592c

--- build/android/pylib/utils/unicode_characters.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2017 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-"""Helper module that contains unicode character constants."""
-
-CHECK = u'\u2714'
-CLOCK = u'\u231A'
-CROSS = u'\u274C'
