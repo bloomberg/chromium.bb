6e4e5d8c02113c826a16184ec8c9701bd58f8f3b
wutao <wutao@chromium.org>
2017-08-24 08:33:31 +0000

    Do not reuse render pass texture if it did not exist in previous frame.
    If a render pass texture did not exist in previous frame, we cannot
    reuse the texture before it has been updated. However, a cached
    render pass still can have no damage from contributing contents.
    To prevent reusing the newly allocated texture, we need to set the
    |has_damage_from_contributing_content| of the cached render pass to
    be true.
    
    Bug: 757912
    Test: on device and linux build.
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I8a9c1415b8ab2a9933a90fab5c28f927f160ad08
    Reviewed-on: https://chromium-review.googlesource.com/627582
    Commit-Queue: Tao Wu <wutao@chromium.org>
    Reviewed-by: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496984}
    Former-commit-id: a84fd4e1c2bd6a9c0d2fcbee5bdfd8845f4ed22d

--- cc/output/direct_renderer.cc
+++ cc/output/direct_renderer.cc
@@ -218,8 +218,15 @@ void DirectRenderer::DecideRenderPassAllocationsForFrame(
 
   for (auto& pass : render_passes_in_draw_order) {
     auto& resource = render_pass_textures_[pass->id];
-    if (!resource)
+    if (!resource) {
       resource = base::MakeUnique<ScopedResource>(resource_provider_);
+
+      // |has_damage_from_contributing_content| is used to determine if previous
+      // contents can be reused when caching render pass and as a result needs
+      // to be true when a new resource is created to ensure that it is updated
+      // and not assumed to already contain correct contents.
+      pass->has_damage_from_contributing_content = true;
+    }
   }
 }
 
