ad0fd4f3292d4dc108622b9455f9e329b64e748f
Yoshifumi Inoue <yosin@chromium.org>
2017-08-24 03:59:10 +0000

    Make Document::AdjustFloatQuadsForScrollAndAbsoluteZoom() as const function
    This patch makes |Document::AdjustFloatQuadsForScrollAndAbsoluteZoom() as
    const function and to take |const LayoutObject&| with changing
    global function |AdjustLayoutUnitForAbsoluteZoom()| to take
    |const LayoutObject&| since it doesn't change state of |Document| and passed
    |LayoutObject| for improving code health.
    
    This patch is a follow-up of the patch[1] which calls
    |Document::AdjustLayoutUnitForAbsoluteZoom()| with |const_cast<T>|.
    
    Following patch will erase |const_cast<T>|.
    
    [1] http://crrev.com/c/625554: Refactor Range::GetBorderAndTextQuads()
    
    Change-Id: I07a7e381bc191e268386d3042a50f2626dace7fd
    Reviewed-on: https://chromium-review.googlesource.com/628006
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Commit-Queue: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496943}
    Former-commit-id: 252ee4157bcf104e2e9aa0ec4f41f98e92e7a79a

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -6461,7 +6461,7 @@ Node* EventTargetNodeForDocument(Document* doc) {
 
 void Document::AdjustFloatQuadsForScrollAndAbsoluteZoom(
     Vector<FloatQuad>& quads,
-    LayoutObject& layout_object) {
+    const LayoutObject& layout_object) const {
   if (!View())
     return;
 
@@ -6475,7 +6475,7 @@ void Document::AdjustFloatQuadsForScrollAndAbsoluteZoom(
 
 void Document::AdjustFloatRectForScrollAndAbsoluteZoom(
     FloatRect& rect,
-    LayoutObject& layout_object) {
+    const LayoutObject& layout_object) const {
   if (!View())
     return;
 
