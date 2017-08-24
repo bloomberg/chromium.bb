bb408559a771345c082d73db8402cd0e426199a2
Ken Rockot <rockot@chromium.org>
2017-08-24 02:44:44 +0000

    Reland "Mojo: Ensure RequestContexts exist for some message APIs"
    This is a reland of 87396c1442ccf8afc26cb85b7beca254c392c990 [formerly 5a9672ea8a60e343cafa73c4d1fb710c84dfd976]
    
    This reland removes the RequestContext from
    MojoExtendSerializedMessagePayload, because it's not necessary after
    all and it significantly impacts the performance of one particular
    test in debug builds, leading to timeouts on slow bots.
    
    Original change's description:
    > Mojo: Ensure RequestContexts exist for some message APIs
    > 
    > In cases where handle serialization fails, various serialized message
    > APIs may lead to handle closure which in turn may trigger watcher
    > notifications. For this reason, a RequestContext must exist in TLS
    > through the extent of these calls.
    > 
    > BUG=740044
    > R=jcivelli@chromium.org
    > 
    > Change-Id: I04549f53365d2d4de3cbd7308d3a88d1bba61cc9
    > Reviewed-on: https://chromium-review.googlesource.com/629617
    > Reviewed-by: Jay Civelli <jcivelli@chromium.org>
    > Commit-Queue: Ken Rockot <rockot@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#496787}
    
    Bug: 740044
    Change-Id: I46ac055f98e60982fa7563436f704752e103d26b
    
    TBR=jcivelli@chromium.org
    
    Change-Id: I46ac055f98e60982fa7563436f704752e103d26b
    Reviewed-on: https://chromium-review.googlesource.com/630957
    Commit-Queue: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#496928}
    Former-commit-id: 43ddb48f0e356bfa6d05c95264653ecb6ec16ffe

--- mojo/edk/system/core.cc
+++ mojo/edk/system/core.cc
@@ -514,6 +514,7 @@ MojoResult Core::AttachSerializedMessageBuffer(MojoMessageHandle message_handle,
                                                uint32_t* buffer_size) {
   if (!message_handle || (num_handles && !handles) || !buffer || !buffer_size)
     return MOJO_RESULT_INVALID_ARGUMENT;
+  RequestContext request_context;
   auto* message = reinterpret_cast<ports::UserMessageEvent*>(message_handle)
                       ->GetMessage<UserMessageImpl>();
   MojoResult rv = message->AttachSerializedMessageBuffer(payload_size, handles,
@@ -557,6 +558,7 @@ MojoResult Core::CommitSerializedMessageContents(
     uint32_t* buffer_size) {
   if (!message_handle)
     return MOJO_RESULT_INVALID_ARGUMENT;
+  RequestContext request_context;
   auto* message = reinterpret_cast<ports::UserMessageEvent*>(message_handle)
                       ->GetMessage<UserMessageImpl>();
   MojoResult rv = message->CommitSerializedContents(final_payload_size);
