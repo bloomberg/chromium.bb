dbba528bc5034c253f461c6bd33ec262df9bc438
Maria Khomenko <mariakhomenko@chromium.org>
2017-08-24 18:18:52 +0000

    Do not show physical web preference on low-end.
    Since we've disabled physical web for low-end devices, do not show the
    preference.
    
    BUG=758358
    
    Change-Id: I7efba61b505416c5dcb20c79ad981d53691485a9
    Reviewed-on: https://chromium-review.googlesource.com/629818
    Reviewed-by: Conley Owens <cco3@chromium.org>
    Commit-Queue: Maria Khomenko <mariakhomenko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497127}
    Former-commit-id: ba096e2f4e770bfe52e0af69a5f67c159633cec8

--- chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
+++ chrome/android/java/src/org/chromium/chrome/browser/preferences/privacy/PrivacyPreferences.java
@@ -14,6 +14,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import org.chromium.base.SysUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
 import org.chromium.chrome.browser.contextualsearch.ContextualSearchFieldTrial;
@@ -104,7 +105,7 @@ public class PrivacyPreferences extends PreferenceFragment
         safeBrowsingPref.setOnPreferenceChangeListener(this);
         safeBrowsingPref.setManagedPreferenceDelegate(mManagedPreferenceDelegate);
 
-        if (!PhysicalWeb.featureIsEnabled()) {
+        if (!PhysicalWeb.featureIsEnabled() || SysUtils.isLowEndDevice()) {
             preferenceScreen.removePreference(findPreference(PREF_PHYSICAL_WEB));
         }
 
