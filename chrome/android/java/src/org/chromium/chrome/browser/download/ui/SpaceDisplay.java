98c93d7bd51129a819346aeb06983db087c459ef
Becky Zhou <huayinz@chromium.org>
2017-08-25 22:29:01 +0000

    [Chrome Home] Info header visual refresh in History/Download
    + History: Combine 3 privacy disclaimer TextViews into 1
    + Download: Change text style and progress bar style
    
    TBR=tedchoc@chromium.org
    
    Bug: 757901
    Change-Id: I2a0dcf6fb67f12e13686c8910ec38828bac994aa
    Reviewed-on: https://chromium-review.googlesource.com/634510
    Commit-Queue: Becky Zhou <huayinz@chromium.org>
    Reviewed-by: Theresa <twellington@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497564}
    Former-commit-id: 48adffaf18bcad5b5eb288e6173fb96111d08443

--- chrome/android/java/src/org/chromium/chrome/browser/download/ui/SpaceDisplay.java
+++ chrome/android/java/src/org/chromium/chrome/browser/download/ui/SpaceDisplay.java
@@ -9,6 +9,9 @@ import android.os.AsyncTask;
 import android.os.Environment;
 import android.os.StatFs;
 import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -109,8 +112,7 @@ public class SpaceDisplay extends RecyclerView.AdapterDataObserver {
     private View mView;
     private View mViewContainer;
     private TextView mSpaceUsedByDownloadsTextView;
-    private TextView mSpaceUsedByOtherAppsTextView;
-    private TextView mSpaceFreeTextView;
+    private TextView mSpaceFreeAndOtherAppsTextView;
     private MaterialProgressBar mSpaceBar;
     private long mFreeBytes;
 
@@ -120,8 +122,8 @@ public class SpaceDisplay extends RecyclerView.AdapterDataObserver {
                                  .inflate(R.layout.download_manager_ui_space_widget, parent, false);
         mView = mViewContainer.findViewById(R.id.space_widget_content);
         mSpaceUsedByDownloadsTextView = (TextView) mView.findViewById(R.id.size_downloaded);
-        mSpaceUsedByOtherAppsTextView = (TextView) mView.findViewById(R.id.size_other_apps);
-        mSpaceFreeTextView = (TextView) mView.findViewById(R.id.size_free);
+        mSpaceFreeAndOtherAppsTextView =
+                (TextView) mView.findViewById(R.id.size_free_and_other_apps);
         mSpaceBar = (MaterialProgressBar) mView.findViewById(R.id.space_bar);
         mFileSystemBytesTask =
                 new StorageSizeTask(true).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
@@ -190,22 +192,28 @@ public class SpaceDisplay extends RecyclerView.AdapterDataObserver {
         Context context = mSpaceUsedByDownloadsTextView.getContext();
         mSpaceUsedByDownloadsTextView.setText(
                 DownloadUtils.getStringForBytes(context, USED_STRINGS, bytesUsedByDownloads));
-        mSpaceUsedByOtherAppsTextView.setText(
-                DownloadUtils.getStringForBytes(context, OTHER_STRINGS, bytesUsedByOtherApps));
-        mSpaceFreeTextView.setText(
-                DownloadUtils.getStringForBytes(context, FREE_STRINGS, mFreeBytes));
+
+        String spaceFree = DownloadUtils.getStringForBytes(context, FREE_STRINGS, mFreeBytes);
+        String spaceUsedByOtherApps =
+                DownloadUtils.getStringForBytes(context, OTHER_STRINGS, bytesUsedByOtherApps);
+        SpannableString spannable = new SpannableString(
+                context.getResources().getString(R.string.download_manager_ui_space_free_and_other,
+                        spaceFree, spaceUsedByOtherApps));
+        ForegroundColorSpan colorSpan = new ForegroundColorSpan(
+                ApiCompatibilityUtils.getColor(context.getResources(), R.color.black_alpha_54));
+        spannable.setSpan(colorSpan, 0, spaceFree.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mSpaceFreeAndOtherAppsTextView.setText(spannable);
 
         // Set a minimum size for the download size so that it shows up in the progress bar.
         long onePercentOfSystem = fileSystemBytes == 0 ? 0 : fileSystemBytes / 100;
         long fudgedBytesUsedByDownloads = Math.max(bytesUsedByDownloads, onePercentOfSystem);
-        long fudgedbytesUsedByOtherApps = Math.max(0, bytesUsedTotal - fudgedBytesUsedByDownloads);
 
         // Indicate how much space has been used as a progress bar.  The percentage used by
         // downloads is shown by the non-overlapped area of the primary and secondary progressbar.
         int percentageUsedTotal = computePercentage(bytesUsedTotal, fileSystemBytes);
-        int percentageOtherApps = computePercentage(fudgedbytesUsedByOtherApps, fileSystemBytes);
+        int percentageDownloaded = computePercentage(fudgedBytesUsedByDownloads, fileSystemBytes);
         mSpaceBar.setProgress(percentageUsedTotal);
-        mSpaceBar.setSecondaryProgress(percentageOtherApps);
+        mSpaceBar.setSecondaryProgress(percentageDownloaded);
 
         for (Observer observer : mObservers) observer.onSpaceDisplayUpdated(this);
     }
