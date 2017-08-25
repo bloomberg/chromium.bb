196ed4d23c4d94ee11e2a9ea82260985307d616a
Scott Graham <scottmg@chromium.org>
2017-08-25 18:42:18 +0000

    Add ScopedTaskEnvironment to V8ValueConverterImplTest test fixture
    This check
    https://gist.github.com/sgraham/72a114262ad5643db77034d8dfea3816 was hit
    on Fuchsia sometimes, depending on what other tests were run
    concurrently with V8ValueConverterImplTest, I guess. Adding
    base::test::ScopedTaskEnvironment seems to be the expected fix.
    
    Bug: 754861
    Change-Id: I562b478115a407aaa02f7356f5ec97a5f9938e68
    Reviewed-on: https://chromium-review.googlesource.com/629579
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Scott Graham <scottmg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497475}
    Former-commit-id: c80dc347a97f2104b6ba1fda41dc6404f4d1234a

--- content/child/v8_value_converter_impl_unittest.cc
+++ content/child/v8_value_converter_impl_unittest.cc
@@ -13,6 +13,7 @@
 #include "base/macros.h"
 #include "base/memory/ptr_util.h"
 #include "base/stl_util.h"
+#include "base/test/scoped_task_environment.h"
 #include "base/test/values_test_util.h"
 #include "base/values.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -211,6 +212,8 @@ class V8ValueConverterImplTest : public testing::Test {
       EXPECT_EQ(base::Value::Type::NONE, temp->GetType());
     }
   }
+
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
 
   v8::Isolate* isolate_;
 
