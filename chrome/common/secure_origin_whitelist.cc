021e663147eb85fbea039a485551885d7b16a53c
Mike West <mkwst@chromium.org>
2017-08-25 15:36:26 +0000

    Secure contexts: remove user-data-dir requirement.
    Requiring developers to create a new profile in order to debug features
    that need secure contexts is overkill. Let's loosen that requirement.
    
    Bug: 757631
    Change-Id: Ic8edf5172f11f6e2bdff94b9d45b1bd9fbbacf7d
    Reviewed-on: https://chromium-review.googlesource.com/625094
    Reviewed-by: Steven Holte <holte@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Emily Stark <estark@chromium.org>
    Commit-Queue: Mike West <mkwst@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497417}
    Former-commit-id: 55dab613843031ab360193116f5d80d0d23308fb

--- chrome/common/secure_origin_whitelist.cc
+++ chrome/common/secure_origin_whitelist.cc
@@ -5,25 +5,27 @@
 #include "chrome/common/secure_origin_whitelist.h"
 
 #include "base/command_line.h"
+#include "base/metrics/histogram_macros.h"
 #include "base/strings/string_split.h"
 #include "chrome/common/chrome_switches.h"
 #include "extensions/common/constants.h"
 
 std::vector<GURL> GetSecureOriginWhitelist() {
   std::vector<GURL> origins;
-  // If kUnsafelyTreatInsecureOriginAsSecure option is given and
-  // kUserDataDir is present, add the given origins as trustworthy
-  // for whitelisting.
+  // If kUnsafelyTreatInsecureOriginAsSecure option is given, then treat the
+  // value as a comma-separated list of origins:
   const base::CommandLine& command_line =
       *base::CommandLine::ForCurrentProcess();
-  if (command_line.HasSwitch(switches::kUnsafelyTreatInsecureOriginAsSecure) &&
-      command_line.HasSwitch(switches::kUserDataDir)) {
+  if (command_line.HasSwitch(switches::kUnsafelyTreatInsecureOriginAsSecure)) {
     std::string origins_str = command_line.GetSwitchValueASCII(
         switches::kUnsafelyTreatInsecureOriginAsSecure);
     for (const std::string& origin : base::SplitString(
              origins_str, ",", base::TRIM_WHITESPACE, base::SPLIT_WANT_ALL))
       origins.push_back(GURL(origin));
   }
+
+  UMA_HISTOGRAM_COUNTS_100("Security.TreatInsecureOriginAsSecure",
+                           origins.size());
 
   return origins;
 }
