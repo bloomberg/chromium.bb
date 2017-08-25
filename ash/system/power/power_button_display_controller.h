85db68692f16d78172ce856cad58fff9320d1f23
Daniel Erat <derat@chromium.org>
2017-08-25 20:01:45 +0000

    chromeos: Add ash::PowerButtonDisplayController.
    Move backlight- and touchscreen-controlling code out of
    ash::TabletPowerButtonController and into a new
    ash::PowerButtonDisplayController class that can be shared
    with ash::PowerButtonController in a later change.
    
    Bug: 758574
    Change-Id: I411067859b822fdee0366b56217255074b42227e
    Reviewed-on: https://chromium-review.googlesource.com/634409
    Reviewed-by: Qiang(Joe) Xu <warx@chromium.org>
    Commit-Queue: Dan Erat <derat@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497510}
    Former-commit-id: fb2a6521d2a372b5a87c8b1040caab9ccc4a0c7a

---
+++ ash/system/power/power_button_display_controller.h
@@ -0,0 +1,86 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ASH_SYSTEM_POWER_POWER_BUTTON_DISPLAY_CONTROLLER_H_
+#define ASH_SYSTEM_POWER_POWER_BUTTON_DISPLAY_CONTROLLER_H_
+
+#include "ash/ash_export.h"
+#include "base/macros.h"
+#include "base/memory/weak_ptr.h"
+#include "chromeos/dbus/power_manager_client.h"
+#include "ui/events/devices/input_device_event_observer.h"
+#include "ui/events/event_handler.h"
+
+namespace ash {
+
+// PowerButtonDisplayController performs display-related tasks (e.g. forcing
+// backlights off or disabling the touchscreen) on behalf of
+// PowerButtonController and TabletPowerButtonController.
+class ASH_EXPORT PowerButtonDisplayController
+    : public chromeos::PowerManagerClient::Observer,
+      public ui::EventHandler,
+      public ui::InputDeviceEventObserver {
+ public:
+  // Screen state as communicated by D-Bus signals from powerd about backlight
+  // brightness changes.
+  enum class ScreenState {
+    // The screen is on.
+    ON,
+    // The screen is off.
+    OFF,
+    // The screen is off, specifically due to an automated change like user
+    // inactivity.
+    OFF_AUTO,
+  };
+
+  PowerButtonDisplayController();
+  ~PowerButtonDisplayController() override;
+
+  ScreenState screen_state() const { return screen_state_; }
+
+  // Updates the power manager's backlights-forced-off state and enables or
+  // disables the touchscreen. No-op if |backlights_forced_off_| already equals
+  // |forced_off|.
+  void SetDisplayForcedOff(bool forced_off);
+
+  // Overridden from chromeos::PowerManagerClient::Observer:
+  void PowerManagerRestarted() override;
+  void BrightnessChanged(int level, bool user_initiated) override;
+  void SuspendDone(const base::TimeDelta& sleep_duration) override;
+  void LidEventReceived(chromeos::PowerManagerClient::LidState state,
+                        const base::TimeTicks& timestamp) override;
+
+  // Overridden from ui::EventHandler:
+  void OnKeyEvent(ui::KeyEvent* event) override;
+  void OnMouseEvent(ui::MouseEvent* event) override;
+
+  // Overridden from ui::InputDeviceObserver:
+  void OnStylusStateChanged(ui::StylusState state) override;
+
+ private:
+  // Sends a request to powerd to get the backlights forced off state so that
+  // |backlights_forced_off_| can be initialized.
+  void GetInitialBacklightsForcedOff();
+
+  // Initializes |backlights_forced_off_|.
+  void OnGotInitialBacklightsForcedOff(bool is_forced_off);
+
+  // Enables or disables the touchscreen, also writing its state to a pref in
+  // local state. The touchscreen is disabled when backlights are forced off.
+  void UpdateTouchscreenStatus();
+
+  // Current screen state.
+  ScreenState screen_state_ = ScreenState::ON;
+
+  // Current forced-off state of backlights.
+  bool backlights_forced_off_ = false;
+
+  base::WeakPtrFactory<PowerButtonDisplayController> weak_ptr_factory_;
+
+  DISALLOW_COPY_AND_ASSIGN(PowerButtonDisplayController);
+};
+
+}  // namespace ash
+
+#endif  // ASH_SYSTEM_POWER_POWER_BUTTON_DISPLAY_CONTROLLER_H_
