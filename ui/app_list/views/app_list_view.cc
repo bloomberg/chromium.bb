35f1a92227b7eb07678515d4ff4c60ae9149e3ea
MinChen <minch@chromium.org>
2017-08-25 01:52:06 +0000

    Smoothing the opacity change of the first row of apps.
    The first row of apps should not be shown if starts with dragging down from
    PEEKING state. And should not be shown suddenly if starts with dragging up
    from PEEKING state.
    
    Bug: 758287
    Change-Id: I19bb4dd4c8c7b7ab72c909a80a56f32d80f3fd5c
    Reviewed-on: https://chromium-review.googlesource.com/633828
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Commit-Queue: min c <minch@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497291}
    Former-commit-id: 38baf643e98992f208cf131dfad1f329a4991851

--- ui/app_list/views/app_list_view.cc
+++ ui/app_list/views/app_list_view.cc
@@ -588,6 +588,8 @@ void AppListView::StartDrag(const gfx::Point& location) {
   initial_drag_point_ = location;
   ConvertPointToScreen(this, &initial_drag_point_);
   initial_window_bounds_ = fullscreen_widget_->GetWindowBoundsInScreen();
+  if (app_list_state_ == PEEKING)
+    drag_started_from_peeking_ = true;
 }
 
 void AppListView::UpdateDrag(const gfx::Point& location) {
@@ -716,6 +718,7 @@ void AppListView::EndDrag(const gfx::Point& location) {
     }
   }
 
+  drag_started_from_peeking_ = false;
   DraggingLayout();
 }
 
