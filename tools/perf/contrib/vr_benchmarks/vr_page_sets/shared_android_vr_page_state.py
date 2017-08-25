9393ad83d6cb358b09b302e56746c17f8b08e741
bsheedy <bsheedy@chromium.org>
2017-08-25 19:17:01 +0000

    Make VR Telemetry tests turn on screen
    Makes SharedAndroidVrPageState inherit from
    AndroidScreenRestorationSharedState, which ensures that the device's
    screen is on, but is otherwise identical to SharedPageState.
    
    Bug: 726906
    Change-Id: Ib25b3247e5af2a6ad30adf68099b6237bca302c6
    Reviewed-on: https://chromium-review.googlesource.com/635187
    Reviewed-by: Charlie Andrews <charliea@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497484}
    Former-commit-id: faa11d3830485f9679bc94119a08955b3ca08115

--- tools/perf/contrib/vr_benchmarks/vr_page_sets/shared_android_vr_page_state.py
+++ tools/perf/contrib/vr_benchmarks/vr_page_sets/shared_android_vr_page_state.py
@@ -5,15 +5,16 @@
 import os
 from core import path_util
 path_util.AddAndroidPylibToPath()
+from page_sets import android_screen_restoration_shared_state as screen_state
 from pylib.utils import shared_preference_utils
 from telemetry.core import android_platform
 from telemetry.core import platform
 from telemetry.core import util
-from telemetry.page import shared_page_state
 from telemetry.internal.platform import android_device
 
 
-class SharedAndroidVrPageState(shared_page_state.SharedPageState):
+class SharedAndroidVrPageState(
+    screen_state.AndroidScreenRestorationSharedState):
   """SharedPageState for VR Telemetry tests.
 
   Performs the same functionality as SharedPageState, but with two main
@@ -21,6 +22,9 @@ class SharedAndroidVrPageState(shared_page_state.SharedPageState):
   1. It is currently restricted to Android
   2. It performs VR-specific setup such as installing and configuring
      additional APKs that are necessary for testing
+
+  Also ensures that the screen is on before the test starts by inheriting from
+  AndroidScreenRestorationSharedState.
   """
   def __init__(self, test, finder_options, story_set):
     # TODO(bsheedy): See about making this a cross-platform SharedVrPageState -
