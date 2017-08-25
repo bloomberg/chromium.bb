3842f8534a5fc764bf7efba52afd91b78a85fcce
Joseph Arhar <jarhar@google.com>
2017-08-25 19:04:23 +0000

    Implement FileProxyWrapperLinux
    This change completes the FileProxyWrapperLinux class for file transfers
    on linux. Its usage will come in a subsequent change which implements
    FileTransferMessageHandler.
    
    Change-Id: I61861eea06d403b4f064a8388b36bf71f480b6d3
    Reviewed-on: https://chromium-review.googlesource.com/624823
    Reviewed-by: Joe Downing <joedow@chromium.org>
    Commit-Queue: Joseph Arhar <jarhar@google.com>
    Cr-Commit-Position: refs/heads/master@{#497480}
    Former-commit-id: 8acbd14464826b095b953bd551fda11c6a3dadd9

--- remoting/host/file_proxy_wrapper.h
+++ remoting/host/file_proxy_wrapper.h
@@ -22,19 +22,49 @@ class FileProxyWrapper {
       ErrorCallback;
   typedef base::Callback<void()> SuccessCallback;
 
-  // Creates a platforms-specific FileProxyWrapper.
+  enum State {
+    // Created, but Init() has not been called yet.
+    kUninitialized = 0,
+
+    // Init() has been called.
+    kInitialized = 1,
+
+    // CreateFile() has been called. The file may or may not exist yet.
+    kFileCreated = 2,
+
+    // Close() has been called. WriteChunk() can no longer be called, but not
+    // all chunks may have been written to disk yet. After chunks are written,
+    // the file will be moved to its target location.
+    kClosing = 3,
+
+    // Close() has been called and succeeded.
+    kClosed = 4,
+
+    // Cancel() has been called or an error occured.
+    kFailed = 5,
+  };
+
+  // Creates a platform-specific FileProxyWrapper.
   static std::unique_ptr<FileProxyWrapper> Create();
 
   FileProxyWrapper();
   virtual ~FileProxyWrapper();
 
+  // |error_callback| must not immediately destroy this FileProxyWrapper.
   virtual void Init(const ErrorCallback& error_callback) = 0;
-  virtual void CreateFile(const std::string& filename,
-                          uint64_t filesize,
+  // TODO(jarhar): Remove |success_callback| from CreateFile(), and instead
+  // allow WriteChunk() to be called immediately after CreateFile(). This also
+  // means that FileTransferMessageHandler will no longer send a
+  // FileTransferResponse with the "READY" state.
+  virtual void CreateFile(const base::FilePath& directory,
+                          const std::string& filename,
                           const SuccessCallback& success_callback) = 0;
   virtual void WriteChunk(std::unique_ptr<CompoundBuffer> buffer) = 0;
+  // TODO(jarhar): Remove |success_callback| from Close() and instead use the
+  // ErrorCallback sent to Init() to signify success after Close().
   virtual void Close(const SuccessCallback& success_callback) = 0;
   virtual void Cancel() = 0;
+  virtual State state() = 0;
 };
 
 }  // namespace remoting
