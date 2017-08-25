1711ad01ed905149d0cb866bcfe647c93290a26b
Ian Vollick <vollick@chromium.org>
2017-08-25 16:06:07 +0000

    [vr] Introduce Binding
    With this CL we introduce the notion of a one way binding. It will
    eventually be used to connect our model to views.
    
    Bug: 758616
    Change-Id: Id970a4e16ffb69dcdcc6331951f608aedf86d0f3
    Reviewed-on: https://chromium-review.googlesource.com/633336
    Commit-Queue: Ian Vollick <vollick@chromium.org>
    Reviewed-by: Christopher Grant <cjgrant@chromium.org>
    Reviewed-by: Tibor Goldschwendt <tiborg@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497424}
    Former-commit-id: 6e0293dd97b4eccac970ace0807c761a4f1107c2

---
+++ chrome/browser/vr/databinding/binding.h
@@ -0,0 +1,98 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_VR_DATABINDING_BINDING_H_
+#define CHROME_BROWSER_VR_DATABINDING_BINDING_H_
+
+#include "base/bind.h"
+#include "base/macros.h"
+#include "base/optional.h"
+#include "chrome/browser/vr/databinding/binding_base.h"
+
+namespace vr {
+
+// This class represents a one-way binding that propagates a change from a
+// source/model property to a sink/view. This is inappropriate for use in the
+// case of, say, an editor view like a text field where changes must alse be
+// propegated back to the model.
+//
+// IMPORTANT: it is assumed that a Binding instance will outlive the model to
+// which it is bound. This class in not appropriate for use with models that
+// come and go in the lifetime of the application.
+template <typename T>
+class Binding : public BindingBase {
+ public:
+  Binding(const base::Callback<T()>& getter,
+          const base::Callback<void(const T&)>& setter)
+      : getter_(getter), setter_(setter) {}
+  ~Binding() override {}
+
+  // This function will check if the getter is producing a different value than
+  // when it was last polled. If so, it will pass that value to the provided
+  // setter. NB: this assumes that T is copyable.
+  void Update() override {
+    T current_value = getter_.Run();
+    if (!last_value_ || current_value != last_value_.value()) {
+      last_value_ = current_value;
+      setter_.Run(current_value);
+    }
+  }
+
+ private:
+  base::Callback<T()> getter_;
+  base::Callback<void(const T&)> setter_;
+  base::Optional<T> last_value_;
+
+  DISALLOW_COPY_AND_ASSIGN(Binding);
+};
+
+// These macros are sugar for constructing a simple binding. It is meant to make
+// setting up bindings a little less painful, but it is not meant to handle all
+// cases. If you need to do something more complex (eg, convert type T before
+// it is propagated), you should use the constructor directly.
+//
+// For example:
+//
+// struct MyModel { int source; };
+// struct MyView {
+//   int sink;
+//   int awesomeness;
+//   void SetAwesomeness(int new_awesomeness) {
+//     awesomeness = new_awesomeness;
+//   }
+// };
+//
+// MyModel m;
+// m.source = 20;
+//
+// MyView v;
+// v.sink = 10;
+// v.awesomeness = 30;
+//
+// auto binding = VR_BIND(int, MyModel, &m, source, MyView, &v, sink = value);
+//
+// Or, equivalently:
+//
+// auto binding = VR_BIND_FIELD(int, MyModel, &m, source, MyView, &v, sink);
+//
+// If your view has a setter, you may find VR_BIND_FUNC handy:
+//
+// auto binding =
+//     VR_BIND_FUNC(int, MyModel, &m, source, MyView, &v, SetAwesomeness);
+//
+#define VR_BIND(T, M, m, Get, V, v, Set)                                    \
+  base::MakeUnique<Binding<T>>(                                             \
+      base::Bind([](M* model) { return model->Get; }, base::Unretained(m)), \
+      base::Bind([](V* view, const T& value) { view->Set; },                \
+                 base::Unretained(v)))
+
+#define VR_BIND_FUNC(T, M, m, Get, V, v, f) \
+  VR_BIND(T, M, m, Get, V, v, f(value))
+
+#define VR_BIND_FIELD(T, M, m, Get, V, v, f) \
+  VR_BIND(T, M, m, Get, V, v, f = value)
+
+}  // namespace vr
+
+#endif  // CHROME_BROWSER_VR_DATABINDING_BINDING_H_
---
+++ chrome/browser/vr/databinding/binding_base.h
@@ -0,0 +1,32 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_VR_DATABINDING_BINDING_BASE_H_
+#define CHROME_BROWSER_VR_DATABINDING_BINDING_BASE_H_
+
+#include "base/memory/weak_ptr.h"
+
+namespace vr {
+
+// Bindings are used to tie models to views. You may, for example, want to bind
+// the visibility of an error indicator to a boolean that signals that the
+// application is exhibiting the error condition.
+class BindingBase {
+ public:
+  BindingBase();
+  virtual ~BindingBase();
+
+  // This function updates the binding. The exact behavior depends on the
+  // subclass. Please see comments on the overridden functions for details.
+  virtual void Update() = 0;
+
+  base::WeakPtr<BindingBase> GetWeakPtr();
+
+ private:
+  base::WeakPtrFactory<BindingBase> weak_ptr_factory_;
+};
+
+}  // namespace vr
+
+#endif  // CHROME_BROWSER_VR_DATABINDING_BINDING_BASE_H_
