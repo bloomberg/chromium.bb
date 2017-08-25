bfb90074bffc2129188cf8c51d9dc3b53d4d39f6
Kevin Marshall <kmarshall@chromium.org>
2017-08-25 22:57:22 +0000

    Fuchsia: Create and register a new default job in BrowserMainLoop.
    This allows the Browser to create its own job so that it can "see"
    child processes for renderers, utility processes, etc, and it does
    so before any processes, or threads, are spawned.
    
    Bug: 746674
    Change-Id: I005b8eb2c89daeb5e9a50f770ef702c66c87b6d1
    Reviewed-on: https://chromium-review.googlesource.com/634285
    Commit-Queue: Kevin Marshall <kmarshall@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Wez <wez@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497578}
    Former-commit-id: d65d47b4668adac4e6ad2f833c67311ba4c7d1e2

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -188,6 +188,13 @@
 #include "media/device_monitors/device_monitor_mac.h"
 #endif
 
+#if defined(OS_FUCHSIA)
+#include <magenta/process.h>
+#include <magenta/syscalls.h>
+
+#include "base/fuchsia/default_job.h"
+#endif  // defined(OS_FUCHSIA)
+
 #if defined(OS_POSIX) && !defined(OS_MACOSX)
 #include "content/browser/renderer_host/render_sandbox_host_linux.h"
 #include "content/browser/zygote_host/zygote_host_impl_linux.h"
@@ -458,6 +465,18 @@ constexpr base::TimeDelta kSwapMetricsInterval =
     base::TimeDelta::FromSeconds(60);
 #endif  // !defined(OS_FUCHSIA)
 
+#if defined(OS_FUCHSIA)
+// Create and register the job which will contain all child processes
+// of the browser process as well as their descendents.
+void InitDefaultJob() {
+  base::ScopedMxHandle handle;
+  mx_status_t result = mx_job_create(mx_job_default(), 0, handle.receive());
+  CHECK_EQ(MX_OK, result) << "mx_job_create(job): "
+                          << mx_status_get_string(result);
+  base::SetDefaultJob(std::move(handle));
+}
+#endif  // defined(OS_FUCHSIA)
+
 }  // namespace
 
 #if defined(USE_X11) && !defined(OS_CHROMEOS)
@@ -606,6 +625,10 @@ void BrowserMainLoop::EarlyInitialization() {
 #if defined(USE_NSS_CERTS)
   // We want to be sure to init NSPR on the main thread.
   crypto::EnsureNSPRInit();
+#endif
+
+#if defined(OS_FUCHSIA)
+  InitDefaultJob();
 #endif
 
   if (parsed_command_line_.HasSwitch(switches::kRendererProcessLimit)) {
