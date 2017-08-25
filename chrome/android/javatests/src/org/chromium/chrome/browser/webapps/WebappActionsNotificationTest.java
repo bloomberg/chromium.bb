a43b705ea58c9c8c1e2f8e7ac508562df0acfc38
Piotr Swigon <piotrs@chromium.org>
2017-08-25 01:48:33 +0000

    [WebApps] Exposes Share and Open in Chrome through Android Notification.
    With this patch standalone web apps on Android will by accompanied by an
    ongoing notification which exposes the website URL, discloses that the
    app runs in Chrome and gives access to 'Share' and 'Open in Chrome'
    actions.
    
    Note that this feature is in its early days and is behind a flag. The
    plan is to iterate on a number of details while it's available on Canary.
    
    Screenshot of how the notification looks like as of this patch:
    Android N: https://goo.gl/photos/V6CnxEWZbSXyMiUF8
    Android M: https://goo.gl/photos/bxRKtzGzU5PFzTua8
    
    Bug: 471623
    Change-Id: I84860ffeab1423701abacaa13037a12836bbe3a5
    Reviewed-on: https://chromium-review.googlesource.com/627522
    Commit-Queue: Piotr Swigon <piotrs@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Reviewed-by: Ilya Sherman <isherman@chromium.org>
    Reviewed-by: Dominick Ng <dominickn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497285}
    Former-commit-id: 48a541dc5c667b718899a0ce8c8a57152d9156be

---
+++ chrome/android/javatests/src/org/chromium/chrome/browser/webapps/WebappActionsNotificationTest.java
@@ -0,0 +1,109 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.webapps;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.service.notification.StatusBarNotification;
+import android.support.annotation.Nullable;
+import android.support.test.filters.SmallTest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.chromium.base.ApplicationStatus;
+import org.chromium.base.test.util.CommandLineFlags;
+import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.MinAndroidSdkLevel;
+import org.chromium.base.test.util.RetryOnFailure;
+import org.chromium.chrome.browser.ChromeFeatureList;
+import org.chromium.chrome.browser.ChromeSwitches;
+import org.chromium.chrome.browser.ChromeTabbedActivity;
+import org.chromium.chrome.browser.ShortcutHelper;
+import org.chromium.chrome.browser.notifications.NotificationConstants;
+import org.chromium.chrome.test.ChromeJUnit4ClassRunner;
+import org.chromium.components.url_formatter.UrlFormatter;
+import org.chromium.content.browser.test.util.Criteria;
+import org.chromium.content.browser.test.util.CriteriaHelper;
+import org.chromium.net.test.EmbeddedTestServerRule;
+
+/**
+ * Tests for a standalone Web App notification governed by {@link WebappActionsNotificationManager}.
+ */
+@RunWith(ChromeJUnit4ClassRunner.class)
+@CommandLineFlags.Add({ChromeSwitches.DISABLE_FIRST_RUN_EXPERIENCE})
+public class WebappActionsNotificationTest {
+    private static final String WEB_APP_PATH = "/chrome/test/data/banners/manifest_test_page.html";
+
+    @Rule
+    public final WebappActivityTestRule mActivityTestRule = new WebappActivityTestRule();
+
+    @Rule
+    public EmbeddedTestServerRule mTestServerRule = new EmbeddedTestServerRule();
+
+    @Before
+    public void startWebapp() throws Exception {
+        mActivityTestRule.startWebappActivity(mActivityTestRule.createIntent().putExtra(
+                ShortcutHelper.EXTRA_URL, mTestServerRule.getServer().getURL(WEB_APP_PATH)));
+        mActivityTestRule.waitUntilSplashscreenHides();
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"Webapps"})
+    @RetryOnFailure
+    @MinAndroidSdkLevel(Build.VERSION_CODES.M) // NotificationManager.getActiveNotifications
+    @CommandLineFlags.Add({"enable-features=" + ChromeFeatureList.PWA_PERSISTENT_NOTIFICATION})
+    public void testNotification() throws Exception {
+        Notification notification = getWebappNotification();
+
+        Assert.assertNotNull(notification);
+        Assert.assertEquals("webapp short name runs in Chrome",
+                notification.extras.getString(Notification.EXTRA_TITLE));
+        Assert.assertEquals(
+                UrlFormatter.formatUrlForDisplay(mTestServerRule.getServer().getURL(WEB_APP_PATH)),
+                notification.extras.getString(Notification.EXTRA_TEXT));
+        Assert.assertEquals("Share", notification.actions[0].title);
+        Assert.assertEquals("Open in Chrome", notification.actions[1].title);
+
+        notification.actions[1].actionIntent.send();
+        CriteriaHelper.pollUiThread(new Criteria() {
+            @Override
+            public boolean isSatisfied() {
+                return ApplicationStatus.getLastTrackedFocusedActivity()
+                               instanceof ChromeTabbedActivity;
+            }
+        });
+
+        Assert.assertNull("Notification should no longer be shown", getWebappNotification());
+    }
+
+    @Test
+    @SmallTest
+    @Feature({"Webapps"})
+    @MinAndroidSdkLevel(Build.VERSION_CODES.M) // NotificationManager.getActiveNotifications
+    public void testNotificationNotEnabled() throws Exception {
+        // Note ChromeFeatureList.PWA_PERSISTENT_NOTIFICATION is not enabled.
+        Assert.assertNull(getWebappNotification());
+    }
+
+    @Nullable
+    private Notification getWebappNotification() {
+        NotificationManager nm =
+                (NotificationManager) mActivityTestRule.getActivity().getSystemService(
+                        Context.NOTIFICATION_SERVICE);
+        for (StatusBarNotification sbn : nm.getActiveNotifications()) {
+            if (sbn.getId() == NotificationConstants.NOTIFICATION_ID_WEBAPP_ACTIONS) {
+                return sbn.getNotification();
+            }
+        }
+        return null;
+    }
+}
