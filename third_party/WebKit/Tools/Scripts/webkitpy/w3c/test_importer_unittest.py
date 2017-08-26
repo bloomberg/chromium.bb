710bf494c2dd26fa4eff25f2616b1a4bce5994f5
Quinten Yearsley <qyearsley@google.com>
2017-08-26 01:03:09 +0000

    When checking CQ status, ignore Blink try bots.
    Bug: 757766
    Change-Id: Ib4a23e531cb3619788a25148328e8318f47fa9a9
    Reviewed-on: https://chromium-review.googlesource.com/628642
    Commit-Queue: Quinten Yearsley <qyearsley@chromium.org>
    Reviewed-by: Philip Jägenstedt <foolip@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497621}
    Former-commit-id: 1be6724d8b936f1aee8fe5e54d574a94051fb164

--- third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_importer_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/w3c/test_importer_unittest.py
@@ -12,6 +12,7 @@ from webkitpy.common.net.git_cl_mock import MockGitCL
 from webkitpy.common.system.executive_mock import MockCall
 from webkitpy.common.system.executive_mock import MockExecutive
 from webkitpy.common.system.log_testing import LoggingTestCase
+from webkitpy.layout_tests.builder_list import BuilderList
 from webkitpy.w3c.chromium_commit_mock import MockChromiumCommit
 from webkitpy.w3c.local_wpt import LocalWPT
 from webkitpy.w3c.test_importer import TestImporter
@@ -67,8 +68,8 @@ class TestImporterTest(LoggingTestCase):
         importer = TestImporter(host)
         # Only the latest job for each builder is counted.
         importer.git_cl = MockGitCL(host, results={
-            Build('builder-a', 120): TryJobStatus('COMPLETED', 'FAILURE'),
-            Build('builder-a', 123): TryJobStatus('COMPLETED', 'SUCCESS'),
+            Build('cq-builder-a', 120): TryJobStatus('COMPLETED', 'FAILURE'),
+            Build('cq-builder-a', 123): TryJobStatus('COMPLETED', 'SUCCESS'),
         })
         success = importer.run_commit_queue_for_cl()
         self.assertTrue(success)
@@ -89,9 +90,9 @@ class TestImporterTest(LoggingTestCase):
             '/mock-checkout/third_party/WebKit/LayoutTests/W3CImportExpectations', '')
         importer = TestImporter(host)
         importer.git_cl = MockGitCL(host, results={
-            Build('builder-a', 120): TryJobStatus('COMPLETED', 'SUCCESS'),
-            Build('builder-a', 123): TryJobStatus('COMPLETED', 'FAILURE'),
-            Build('builder-b', 200): TryJobStatus('COMPLETED', 'SUCCESS'),
+            Build('cq-builder-a', 120): TryJobStatus('COMPLETED', 'SUCCESS'),
+            Build('cq-builder-a', 123): TryJobStatus('COMPLETED', 'FAILURE'),
+            Build('cq-builder-b', 200): TryJobStatus('COMPLETED', 'SUCCESS'),
         })
         importer.fetch_new_expectations_and_baselines = lambda: None
         success = importer.run_commit_queue_for_cl()
@@ -103,6 +104,36 @@ class TestImporterTest(LoggingTestCase):
         self.assertEqual(importer.git_cl.calls, [
             ['git', 'cl', 'try'],
             ['git', 'cl', 'set-close'],
+        ])
+
+    def test_run_commit_queue_for_cl_only_checks_non_blink_bots(self):
+        host = MockHost()
+        host.filesystem.write_text_file(
+            '/mock-checkout/third_party/WebKit/LayoutTests/W3CImportExpectations', '')
+        host.builders = BuilderList({
+            'fakeos_blink_rel': {
+                'port_name': 'test-fakeos',
+                'specifiers': ['FakeOS', 'Release'],
+                'is_try_builder': True,
+            }
+        })
+        importer = TestImporter(host)
+        importer.git_cl = MockGitCL(host, results={
+            Build('fakeos_blink_rel', 123): TryJobStatus('COMPLETED', 'FAILURE'),
+            Build('cq-builder-b', 200): TryJobStatus('COMPLETED', 'SUCCESS'),
+        })
+        importer.fetch_new_expectations_and_baselines = lambda: None
+        success = importer.run_commit_queue_for_cl()
+        self.assertTrue(success)
+        self.assertLog([
+            'INFO: Triggering CQ try jobs.\n',
+            'INFO: CQ appears to have passed; trying to commit.\n',
+            'INFO: Update completed.\n',
+        ])
+        self.assertEqual(importer.git_cl.calls, [
+            ['git', 'cl', 'try'],
+            ['git', 'cl', 'upload', '-f', '--send-mail'],
+            ['git', 'cl', 'set-commit'],
         ])
 
     def test_apply_exportable_commits_locally(self):
