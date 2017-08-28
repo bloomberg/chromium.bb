a173d3032e597fc69d6498f223e02961fe4043bb
Luke Zielinski <lpz@chromium.org>
2017-08-28 20:25:40 +0000

    Adding a Finch control for trigger quota.
    Unconfigured triggers will remain disabled with zero quota (aside
    from security interstitials, which continue to have unlimited quota).
    
    Also added unit tests.
    
    Bug: 708755
    Change-Id: Icc701363faf85cf5923b463b8780ded91cd24255
    Reviewed-on: https://chromium-review.googlesource.com/633744
    Commit-Queue: Luke Z <lpz@chromium.org>
    Reviewed-by: Varun Khaneja (slow) <vakh@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497859}
    Former-commit-id: a8136ae03409044a9563ff0091928914656b59b8

--- components/safe_browsing/triggers/trigger_throttler.cc
+++ components/safe_browsing/triggers/trigger_throttler.cc
@@ -4,36 +4,114 @@
 
 #include "components/safe_browsing/triggers/trigger_throttler.h"
 
+#include "base/metrics/field_trial_params.h"
 #include "base/stl_util.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/strings/string_split.h"
+#include "base/time/default_clock.h"
 #include "base/time/time.h"
+#include "components/safe_browsing/features.h"
 
 namespace safe_browsing {
+const char kTriggerTypeAndQuotaParam[] = "trigger_type_and_quota_csv";
 
 namespace {
 const size_t kUnlimitedTriggerQuota = std::numeric_limits<size_t>::max();
 constexpr base::TimeDelta kOneDayTimeDelta = base::TimeDelta::FromDays(1);
 
-size_t GetDailyQuotaForTrigger(const TriggerType trigger_type) {
+// Predicate used to search |trigger_type_and_quota_list_| by trigger type.
+class TriggerTypeIs {
+ public:
+  explicit TriggerTypeIs(const TriggerType type) : type_(type) {}
+  bool operator()(const TriggerTypeAndQuotaItem& trigger_type_and_quota) {
+    return type_ == trigger_type_and_quota.first;
+  }
+
+ private:
+  TriggerType type_;
+};
+
+void ParseTriggerTypeAndQuotaParam(
+    std::vector<TriggerTypeAndQuotaItem>* trigger_type_and_quota_list) {
+  DCHECK(trigger_type_and_quota_list);
+  // If the feature is disabled we just use the default list. Otherwise the list
+  // from the Finch param will be the one used.
+  if (!base::FeatureList::IsEnabled(kTriggerThrottlerDailyQuotaFeature)) {
+    return;
+  }
+
+  trigger_type_and_quota_list->clear();
+  const std::string& trigger_and_quota_csv_param =
+      base::GetFieldTrialParamValueByFeature(kTriggerThrottlerDailyQuotaFeature,
+                                             kTriggerTypeAndQuotaParam);
+  if (trigger_and_quota_csv_param.empty()) {
+    return;
+  }
+
+  std::vector<std::string> split =
+      base::SplitString(trigger_and_quota_csv_param, ",", base::TRIM_WHITESPACE,
+                        base::SPLIT_WANT_NONEMPTY);
+  // If we don't have the right number of pairs in the csv then don't bother
+  // parsing further.
+  if (split.size() % 2 != 0) {
+    return;
+  }
+  for (size_t i = 0; i < split.size(); i += 2) {
+    // Make sure both the trigger type and quota are integers. Skip them if not.
+    int trigger_type_int = -1;
+    int quota_int = -1;
+    if (!base::StringToInt(split[i], &trigger_type_int) ||
+        !base::StringToInt(split[i + 1], &quota_int)) {
+      continue;
+    }
+    trigger_type_and_quota_list->push_back(
+        std::make_pair(static_cast<TriggerType>(trigger_type_int), quota_int));
+  }
+
+  std::sort(trigger_type_and_quota_list->begin(),
+            trigger_type_and_quota_list->end(),
+            [](const TriggerTypeAndQuotaItem& a,
+               const TriggerTypeAndQuotaItem& b) { return a.first < b.first; });
+}
+
+size_t GetDailyQuotaForTrigger(
+    const TriggerType trigger_type,
+    const std::vector<TriggerTypeAndQuotaItem>& trigger_quota_list) {
   switch (trigger_type) {
     case TriggerType::SECURITY_INTERSTITIAL:
       return kUnlimitedTriggerQuota;
     case TriggerType::AD_SAMPLE:
-      // TODO(lpz): Introduce a finch param to configure quota per-trigger-type.
-      return 0;
+      // These triggers have quota configured via Finch, lookup the value in
+      // |trigger_quota_list|.
+      const auto& trigger_quota_iter =
+          std::find_if(trigger_quota_list.begin(), trigger_quota_list.end(),
+                       TriggerTypeIs(trigger_type));
+      if (trigger_quota_iter != trigger_quota_list.end())
+        return trigger_quota_iter->second;
+
+      break;
   }
-  // By default, unhandled trigger types have no quota.
+  // By default, unhandled or unconfigured trigger types have no quota.
   return 0;
 }
 
 }  // namespace
 
-TriggerThrottler::TriggerThrottler() {}
+TriggerThrottler::TriggerThrottler() : clock_(new base::DefaultClock()) {
+  ParseTriggerTypeAndQuotaParam(&trigger_type_and_quota_list_);
+}
 
 TriggerThrottler::~TriggerThrottler() {}
 
+void TriggerThrottler::SetClockForTesting(
+    std::unique_ptr<base::Clock> test_clock) {
+  clock_ = std::move(test_clock);
+}
+
 bool TriggerThrottler::TriggerCanFire(const TriggerType trigger_type) const {
   // Lookup how many times this trigger is allowed to fire each day.
-  const size_t trigger_quota = GetDailyQuotaForTrigger(trigger_type);
+  const size_t trigger_quota =
+      GetDailyQuotaForTrigger(trigger_type, trigger_type_and_quota_list_);
 
   // Some basic corner cases for triggers that always fire, or disabled
   // triggers that never fire.
@@ -52,18 +130,19 @@ bool TriggerThrottler::TriggerCanFire(const TriggerType trigger_type) const {
   if (trigger_quota > timestamps.size())
     return true;
 
-  // Otherwise, we have more events than quota, check which day they occured on.
-  // Newest events are at the end of vector so we can simply look at the
+  // Otherwise, we have more events than quota, check which day they occurred
+  // on. Newest events are at the end of vector so we can simply look at the
   // Nth-from-last entry (where N is the quota) to see if it happened within
   // the current day or earlier.
-  base::Time min_timestamp = base::Time::Now() - kOneDayTimeDelta;
+  base::Time min_timestamp = clock_->Now() - kOneDayTimeDelta;
   const size_t pos = timestamps.size() - trigger_quota + 1;
   return timestamps[pos] < min_timestamp.ToTimeT();
 }
 
 void TriggerThrottler::TriggerFired(const TriggerType trigger_type) {
   // Lookup how many times this trigger is allowed to fire each day.
-  const size_t trigger_quota = GetDailyQuotaForTrigger(trigger_type);
+  const size_t trigger_quota =
+      GetDailyQuotaForTrigger(trigger_type, trigger_type_and_quota_list_);
 
   // For triggers that always fire, don't bother tracking quota.
   if (trigger_quota == kUnlimitedTriggerQuota)
@@ -71,7 +150,7 @@ void TriggerThrottler::TriggerFired(const TriggerType trigger_type) {
 
   // Otherwise, record that the trigger fired.
   std::vector<time_t>* timestamps = &trigger_events_[trigger_type];
-  timestamps->push_back(base::Time::Now().ToTimeT());
+  timestamps->push_back(clock_->Now().ToTimeT());
 
   // Clean up the trigger events map.
   CleanupOldEvents();
@@ -80,7 +159,8 @@ void TriggerThrottler::TriggerFired(const TriggerType trigger_type) {
 void TriggerThrottler::CleanupOldEvents() {
   for (const auto& map_iter : trigger_events_) {
     const TriggerType trigger_type = map_iter.first;
-    const size_t trigger_quota = GetDailyQuotaForTrigger(trigger_type);
+    const size_t trigger_quota =
+        GetDailyQuotaForTrigger(trigger_type, trigger_type_and_quota_list_);
     const std::vector<time_t>& trigger_times = map_iter.second;
 
     // Skip the cleanup if we have quota room, quotas should generally be small.
@@ -88,7 +168,7 @@ void TriggerThrottler::CleanupOldEvents() {
       return;
 
     std::vector<time_t> tmp_trigger_times;
-    base::Time min_timestamp = base::Time::Now() - kOneDayTimeDelta;
+    base::Time min_timestamp = clock_->Now() - kOneDayTimeDelta;
     // Go over the event times for this trigger and keep timestamps which are
     // newer than |min_timestamp|. We put timestamps in a temp vector that will
     // get swapped into the map in place of the existing vector.
--- components/safe_browsing/triggers/trigger_throttler_unittest.cc
+++ components/safe_browsing/triggers/trigger_throttler_unittest.cc
@@ -4,19 +4,154 @@
 
 #include "components/safe_browsing/triggers/trigger_throttler.h"
 
+#include "base/metrics/field_trial_params.h"
+#include "base/strings/stringprintf.h"
+#include "base/test/scoped_feature_list.h"
+#include "base/test/simple_test_clock.h"
+#include "components/safe_browsing/features.h"
+#include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-namespace safe_browsing {
-namespace {
+using testing::ElementsAre;
 
-TEST(TriggerThrottler, SecurityInterstitialsHaveUnlimitedQuota) {
+namespace safe_browsing {
+
+class TriggerThrottlerTest : public ::testing::Test {
+ public:
+  void SetQuotaForTriggerType(TriggerType trigger_type, size_t max_quota) {
+    trigger_throttler_.trigger_type_and_quota_list_.push_back(
+        std::make_pair(trigger_type, max_quota));
+  }
+
+  TriggerThrottler* throttler() { return &trigger_throttler_; }
+
+  void SetTestClock(base::Clock* clock) {
+    trigger_throttler_.SetClockForTesting(base::WrapUnique(clock));
+  }
+
+  std::vector<time_t> GetEventTimestampsForTriggerType(
+      TriggerType trigger_type) {
+    return trigger_throttler_.trigger_events_[trigger_type];
+  }
+
+ private:
+  TriggerThrottler trigger_throttler_;
+};
+
+TEST_F(TriggerThrottlerTest, SecurityInterstitialsHaveUnlimitedQuota) {
   // Make sure that security interstitials never run out of quota.
-  TriggerThrottler throttler;
   for (int i = 0; i < 1000; ++i) {
-    throttler.TriggerFired(TriggerType::SECURITY_INTERSTITIAL);
-    EXPECT_TRUE(throttler.TriggerCanFire(TriggerType::SECURITY_INTERSTITIAL));
+    throttler()->TriggerFired(TriggerType::SECURITY_INTERSTITIAL);
+    EXPECT_TRUE(
+        throttler()->TriggerCanFire(TriggerType::SECURITY_INTERSTITIAL));
   }
 }
 
-}  // namespace
+TEST_F(TriggerThrottlerTest, SecurityInterstitialQuotaCanNotBeOverwritten) {
+  // Make sure that security interstitials never run out of quota, even if we
+  // try to configure quota for this trigger type.
+  SetQuotaForTriggerType(TriggerType::SECURITY_INTERSTITIAL, 3);
+  for (int i = 0; i < 1000; ++i) {
+    throttler()->TriggerFired(TriggerType::SECURITY_INTERSTITIAL);
+    EXPECT_TRUE(
+        throttler()->TriggerCanFire(TriggerType::SECURITY_INTERSTITIAL));
+  }
+}
+
+TEST_F(TriggerThrottlerTest, TriggerExceedsQuota) {
+  // Ensure that a trigger can't fire more than its quota allows.
+  SetQuotaForTriggerType(TriggerType::AD_SAMPLE, 2);
+
+  // First two triggers should work
+  EXPECT_TRUE(throttler()->TriggerCanFire(TriggerType::AD_SAMPLE));
+  throttler()->TriggerFired(TriggerType::AD_SAMPLE);
+  EXPECT_TRUE(throttler()->TriggerCanFire(TriggerType::AD_SAMPLE));
+  throttler()->TriggerFired(TriggerType::AD_SAMPLE);
+
+  // Third attempt will fail since we're out of quota.
+  EXPECT_FALSE(throttler()->TriggerCanFire(TriggerType::AD_SAMPLE));
+}
+
+TEST_F(TriggerThrottlerTest, TriggerQuotaResetsAfterOneDay) {
+  // Ensure that trigger events older than a day are cleaned up and triggers can
+  // resume firing.
+
+  // We initialize the test clock to several days ago and fire some events to
+  // use up quota. We then advance the clock by a day and ensure quota is
+  // available again.
+  base::SimpleTestClock* test_clock(new base::SimpleTestClock);
+  test_clock->SetNow(base::Time::Now() - base::TimeDelta::FromDays(10));
+  time_t base_ts = test_clock->Now().ToTimeT();
+
+  SetTestClock(test_clock);
+  SetQuotaForTriggerType(TriggerType::AD_SAMPLE, 2);
+
+  // First two triggers should work
+  EXPECT_TRUE(throttler()->TriggerCanFire(TriggerType::AD_SAMPLE));
+  throttler()->TriggerFired(TriggerType::AD_SAMPLE);
+  EXPECT_TRUE(throttler()->TriggerCanFire(TriggerType::AD_SAMPLE));
+  throttler()->TriggerFired(TriggerType::AD_SAMPLE);
+
+  // Third attempt will fail since we're out of quota.
+  EXPECT_FALSE(throttler()->TriggerCanFire(TriggerType::AD_SAMPLE));
+
+  // Also confirm that the throttler contains two event timestamps for the above
+  // two events - since we use a test clock, it doesn't move unless we tell it
+  // to.
+  EXPECT_THAT(GetEventTimestampsForTriggerType(TriggerType::AD_SAMPLE),
+              ElementsAre(base_ts, base_ts));
+
+  // Move the clock forward by 1 day (and a bit) and try the trigger again,
+  // quota should be available now.
+  test_clock->Advance(base::TimeDelta::FromDays(1) +
+                      base::TimeDelta::FromSeconds(1));
+  time_t advanced_ts = test_clock->Now().ToTimeT();
+  EXPECT_TRUE(throttler()->TriggerCanFire(TriggerType::AD_SAMPLE));
+
+  // The previous time stamps should remain in the throttler.
+  EXPECT_THAT(GetEventTimestampsForTriggerType(TriggerType::AD_SAMPLE),
+              ElementsAre(base_ts, base_ts));
+
+  // Firing the trigger will clean up the expired timestamps and insert the new
+  // timestamp.
+  throttler()->TriggerFired(TriggerType::AD_SAMPLE);
+  EXPECT_THAT(GetEventTimestampsForTriggerType(TriggerType::AD_SAMPLE),
+              ElementsAre(advanced_ts));
+}
+
+TEST(TriggerThrottlerTestFinch, ConfigureQuotaViaFinch) {
+  // Make sure that setting the quota param via Finch params works as expected.
+  base::FieldTrialList field_trial_list(nullptr);
+  base::FieldTrial* trial = base::FieldTrialList::CreateFieldTrial(
+      safe_browsing::kTriggerThrottlerDailyQuotaFeature.name, "Group");
+  std::map<std::string, std::string> feature_params;
+  feature_params[std::string(safe_browsing::kTriggerTypeAndQuotaParam)] =
+      base::StringPrintf("%d,%d", TriggerType::AD_SAMPLE, 3);
+  base::AssociateFieldTrialParams(
+      safe_browsing::kTriggerThrottlerDailyQuotaFeature.name, "Group",
+      feature_params);
+  std::unique_ptr<base::FeatureList> feature_list(new base::FeatureList);
+  feature_list->InitializeFromCommandLine(
+      safe_browsing::kTriggerThrottlerDailyQuotaFeature.name, std::string());
+  feature_list->AssociateReportingFieldTrial(
+      safe_browsing::kTriggerThrottlerDailyQuotaFeature.name,
+      base::FeatureList::OVERRIDE_ENABLE_FEATURE, trial);
+  base::test::ScopedFeatureList scoped_feature_list;
+  scoped_feature_list.InitWithFeatureList(std::move(feature_list));
+
+  // The throttler has been configured (above) to allow ad samples to fire three
+  // times per day.
+  TriggerThrottler throttler;
+
+  // First three triggers should work
+  EXPECT_TRUE(throttler.TriggerCanFire(TriggerType::AD_SAMPLE));
+  throttler.TriggerFired(TriggerType::AD_SAMPLE);
+  EXPECT_TRUE(throttler.TriggerCanFire(TriggerType::AD_SAMPLE));
+  throttler.TriggerFired(TriggerType::AD_SAMPLE);
+  EXPECT_TRUE(throttler.TriggerCanFire(TriggerType::AD_SAMPLE));
+  throttler.TriggerFired(TriggerType::AD_SAMPLE);
+
+  // Fourth attempt will fail since we're out of quota.
+  EXPECT_FALSE(throttler.TriggerCanFire(TriggerType::AD_SAMPLE));
+}
 }  // namespace safe_browsing
