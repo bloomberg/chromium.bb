365616a35cdb7fe275e637408e1ff97f3eeda64d
Scott Graham <scottmg@chromium.org>
2017-08-28 07:33:46 +0000

    Fix creation of TaskScheduler in OneShotAccessibilityTreeSearchTest
    The previous creation of TaskScheduler didn't work with other tests in
    the same batch using ScopedTaskEnvironment because it wasn't tearing
    down its created instance. Instead of fixing that, switch to using
    ScopedTaskEnvironment as this fixture doesn't have any special
    requirements.
    
    This showed up as flake on Fuchsia, but is cross-platform.
    
    Bug: 759245, 754861
    Change-Id: I21b3121a7f971064efa82d352071cce0ae29629b
    Reviewed-on: https://chromium-review.googlesource.com/636086
    Commit-Queue: Dominic Mazzoni <dmazzoni@chromium.org>
    Reviewed-by: Dominic Mazzoni <dmazzoni@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497704}
    Former-commit-id: ce4114e91772f4864307481d681110ec2606ae8f

--- content/browser/accessibility/one_shot_accessibility_tree_search_unittest.cc
+++ content/browser/accessibility/one_shot_accessibility_tree_search_unittest.cc
@@ -7,7 +7,7 @@
 #include <memory>
 
 #include "base/macros.h"
-#include "base/task_scheduler/task_scheduler.h"
+#include "base/test/scoped_task_environment.h"
 #include "content/browser/accessibility/browser_accessibility.h"
 #include "content/browser/accessibility/browser_accessibility_manager.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -41,6 +41,8 @@ class MAYBE_OneShotAccessibilityTreeSearchTest : public testing::Test {
  protected:
   void SetUp() override;
 
+  base::test::ScopedTaskEnvironment scoped_task_environment_;
+
   std::unique_ptr<BrowserAccessibilityManager> tree_;
 
  private:
@@ -48,14 +50,6 @@ class MAYBE_OneShotAccessibilityTreeSearchTest : public testing::Test {
 };
 
 void MAYBE_OneShotAccessibilityTreeSearchTest::SetUp() {
-  // In order to mock TestBrowserAccessibilityManager, we need to also make sure
-  // there is a TaskScheduler started.  This is because
-  // BrowserAccessibilityStateImpl posts a few tasks which will assert if there
-  // isn't a TaskScheduler.
-  // See: BrowserAccessibilityStateImpl::BrowserAccessibilityStateImpl().
-  base::TaskScheduler::CreateAndStartWithDefaultParams(
-      "OneShotAccessibilityTreeSearchTest");
-
   ui::AXNodeData root;
   root.id = 1;
   root.SetName("Document");
