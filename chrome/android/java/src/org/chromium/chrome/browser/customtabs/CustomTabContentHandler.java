c36deb17f367addf5171e5df3f6a4beeb8c81203
Yusuf Ozuysal <yusufo@google.com>
2017-08-28 23:27:40 +0000

    [CustomTabs] Pull out bundle extra creation to its own method
    - Extends CustomTabsContentHandler to expose the currently displayed url
    - Pulls out extra bundle to its own method, so that extending classes
    can add their own info if necessary.
    
    BUG=759377
    
    Change-Id: I0de60e5235362f08352a756cde6d3a21fa5a21c5
    Reviewed-on: https://chromium-review.googlesource.com/637128
    Commit-Queue: Yusuf Ozuysal <yusufo@chromium.org>
    Reviewed-by: Benoit L <lizeb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497931}
    Former-commit-id: 4c311cf524efe0e8c9c47d8c3d5159846aa005da

--- chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabContentHandler.java
+++ chrome/android/java/src/org/chromium/chrome/browser/customtabs/CustomTabContentHandler.java
@@ -51,4 +51,9 @@ public interface CustomTabContentHandler {
      */
     boolean updateRemoteViews(RemoteViews remoteViews, int[] clickableIDs,
             PendingIntent pendingIntent);
+
+    /**
+     * @return The current url being displayed to the user.
+     */
+    String getCurrentUrl();
 }
