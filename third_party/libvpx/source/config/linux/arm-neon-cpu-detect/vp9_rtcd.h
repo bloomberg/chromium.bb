616165c676c53955b8a554a1d232c7d7238e65ee
Jerome Jiang <jianj@google.com>
2017-08-28 19:37:58 +0000

    Roll src/third_party/libvpx/source/libvpx/ 6b9c691da..30c261b1e (27 commits)
    https://chromium.googlesource.com/webm/libvpx.git/+log/6b9c691dafc8..30c261b1ebe8
    
    $ git log 6b9c691da..30c261b1e --date=short --no-merges --format='%ad %ae %s'
    2017-08-23 marpan vp9: SVC: Skip NEWMV for small blocks for (0, 0) base_mv.
    2017-08-22 johannkoenig quantize avx: copy implementation to intrinsics
    2017-08-21 johannkoenig quantize neon: round dqcoeff towards zero
    2017-08-10 johannkoenig quantize fp: neon implementation
    2017-08-22 marpan vp9: Condition lighting change detection on CBR mode.
    2017-08-22 johannkoenig quantize ssse3: copy style from sse2
    2017-08-22 johannkoenig quantize sse2: copy opts from ssse3
    2017-08-22 johannkoenig quantize: capture skip block early
    2017-08-21 johannkoenig quantize: ignore skip_block in arm
    2017-08-21 johannkoenig quantize: ignore skip_block in x86
    2017-08-21 johannkoenig quantize fp: ignore skip_block in arm
    2017-08-21 johannkoenig quantize fp: ignore skip_block in x86
    2017-08-02 johannkoenig quantize test: test _fp_ version of quantize
    2017-08-16 johannkoenig Remove skip_block from quantize
    2017-08-18 slavarnway vpx_dsp: get32x32var_avx2() cleanup
    2017-08-07 slavarnway vpx_dsp: vpx_get16x16var_avx2() cleanup
    2017-08-02 yinshiyou-hf vpx_dsp:loongson optimize vpx_subtract_block_c (case 4x4,8x8,16x16) with mmi.
    2017-08-17 jzern highbd_idct32x32*,idct32_34_4x32_quarter_1_2: fix typo
    2017-08-16 johannkoenig quantize: normalize intermediate types
    2017-08-16 jzern inv_txfm_sse2.h: correct idct*/iadst* prototypes
    (...)
    
    Created with:
      roll-dep src/third_party/libvpx/source/libvpx
    R=johannkoenig@google.com
    
    Bug: 
    Change-Id: I950d78242ad44264c1a4f5e1a985956fea24affa
    Reviewed-on: https://chromium-review.googlesource.com/636045
    Reviewed-by: Tom Finegan <tomfinegan@chromium.org>
    Commit-Queue: Tom Finegan <tomfinegan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#497842}
    Former-commit-id: 54d88c84a8368b6e882c6212e33f42b89db4bbae

--- third_party/libvpx/source/config/linux/arm-neon-cpu-detect/vp9_rtcd.h
+++ third_party/libvpx/source/config/linux/arm-neon-cpu-detect/vp9_rtcd.h
@@ -237,7 +237,28 @@ void vp9_quantize_fp_32x32_c(const tran_low_t* coeff_ptr,
                              uint16_t* eob_ptr,
                              const int16_t* scan,
                              const int16_t* iscan);
-#define vp9_quantize_fp_32x32 vp9_quantize_fp_32x32_c
+void vp9_quantize_fp_32x32_neon(const tran_low_t* coeff_ptr,
+                                intptr_t n_coeffs,
+                                int skip_block,
+                                const int16_t* round_ptr,
+                                const int16_t* quant_ptr,
+                                tran_low_t* qcoeff_ptr,
+                                tran_low_t* dqcoeff_ptr,
+                                const int16_t* dequant_ptr,
+                                uint16_t* eob_ptr,
+                                const int16_t* scan,
+                                const int16_t* iscan);
+RTCD_EXTERN void (*vp9_quantize_fp_32x32)(const tran_low_t* coeff_ptr,
+                                          intptr_t n_coeffs,
+                                          int skip_block,
+                                          const int16_t* round_ptr,
+                                          const int16_t* quant_ptr,
+                                          tran_low_t* qcoeff_ptr,
+                                          tran_low_t* dqcoeff_ptr,
+                                          const int16_t* dequant_ptr,
+                                          uint16_t* eob_ptr,
+                                          const int16_t* scan,
+                                          const int16_t* iscan);
 
 void vp9_scale_and_extend_frame_c(const struct yv12_buffer_config* src,
                                   struct yv12_buffer_config* dst,
@@ -274,6 +295,9 @@ static void setup_rtcd_internal(void) {
   vp9_quantize_fp = vp9_quantize_fp_c;
   if (flags & HAS_NEON)
     vp9_quantize_fp = vp9_quantize_fp_neon;
+  vp9_quantize_fp_32x32 = vp9_quantize_fp_32x32_c;
+  if (flags & HAS_NEON)
+    vp9_quantize_fp_32x32 = vp9_quantize_fp_32x32_neon;
 }
 #endif
 
