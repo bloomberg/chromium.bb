03293e8d1f22872d69ba560566db98039ba43b03
Paweł Hajdan, Jr <phajdan.jr@chromium.org>
2017-08-28 11:27:46 +0200

    fetch_end_to_end_test: remove unused, broken configs
    See https://goto.google.com/kinhy (Google-internal)
    for more context.
    
    Bug: none
    Change-Id: I1d255543d99b2d466dbad9f15ca606d663c1f946
    Reviewed-on: https://chromium-review.googlesource.com/637311
    Reviewed-by: William Hesse <whesse@google.com>
    Commit-Queue: Paweł Hajdan Jr. <phajdan.jr@chromium.org>

--- recipes/recipes/fetch_end_to_end_test.py
+++ recipes/recipes/fetch_end_to_end_test.py
@@ -19,10 +19,13 @@ def RunSteps(api):
   api.bot_update.ensure_checkout()
 
   # List fetch configs available in the test depot_tools checkout.
-  fetch_configs = sorted(
+  fetch_configs = set(
       api.path.basename(f)[:-3] for f in api.file.listdir(
           'listdir fetch_configs', api.path['checkout'].join('fetch_configs'))
       if str(f).endswith('.py'))
+
+  # config_util is a helper library, not a config.
+  fetch_configs.discard('config_util')
 
   # Try to run "fetch" for each of the configs. It's important to use
   # the checkout under test.
@@ -30,7 +33,7 @@ def RunSteps(api):
       env={'DEPOT_TOOLS_UPDATE': '0', 'CHROME_HEADLESS': '1'},
       env_prefixes={'PATH': [api.path['checkout']]}):
     with api.step.defer_results():
-      for config_name in fetch_configs:
+      for config_name in sorted(fetch_configs):
         # Create a dedicated temp directory. We want to test checking out
         # from scratch.
         temp_dir = api.path.mkdtemp('fetch_end_to_end_test_%s' % config_name)
