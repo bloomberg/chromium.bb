75e2e39aea325e72364512c3f9b59aac48aeff97
tanvir.rizvi <tanvir.rizvi@samsung.com>
2017-08-29 12:18:56 +0000

    Make SpellChecker not to use EndingVisibleSelection
    EndingVisibleSelection() call does canonicalization,
    which is not needed by SpellChecker, so we can use commands
    SelectionForUndoStep to get the required positions.
    Also we will make EndingVisibleSelection method of editing commands
    as protected method.
    
    Bug: 657256
    
    Change-Id: Ic7cd9dca3bfc47df4e2e3942162a022f6ad0c70a
    Reviewed-on: https://chromium-review.googlesource.com/631762
    Commit-Queue: Tanvir Rizvi <tanvir.rizvi@samsung.com>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498079}
    Former-commit-id: 981ba78ebe26c6f9d75795406ccfd99bc56e8d0e

--- third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
+++ third_party/WebKit/Source/core/editing/spellcheck/SpellChecker.cpp
@@ -80,12 +80,6 @@ bool IsPositionInTextArea(const Position& position) {
   return isHTMLTextAreaElement(text_control);
 }
 
-static bool IsSpellCheckingEnabledFor(const VisibleSelection& selection) {
-  if (selection.IsNone())
-    return false;
-  return SpellChecker::IsSpellCheckingEnabledAt(selection.Start());
-}
-
 SelectionInDOMTree SelectWord(const VisiblePosition& position) {
   // TODO(yosin): We should fix |startOfWord()| and |endOfWord()| not to return
   // null position.
@@ -361,7 +355,7 @@ void SpellChecker::MarkMisspellingsForMovingParagraphs(
 }
 
 void SpellChecker::MarkMisspellingsInternal(const VisibleSelection& selection) {
-  if (!IsSpellCheckingEnabled() || !IsSpellCheckingEnabledFor(selection))
+  if (!IsSpellCheckingEnabled() || !IsSpellCheckingEnabledAt(selection.Start()))
     return;
 
   const EphemeralRange& range = selection.ToNormalizedEphemeralRange();
@@ -385,7 +379,8 @@ void SpellChecker::MarkMisspellingsAfterApplyingCommand(
 
   if (!IsSpellCheckingEnabled())
     return;
-  if (!IsSpellCheckingEnabledFor(cmd.EndingVisibleSelection()))
+
+  if (!IsSpellCheckingEnabledAt(cmd.EndingSelection().Base()))
     return;
 
   // TODO(editing-dev): The use of updateStyleAndLayoutIgnorePendingStylesheets
@@ -423,9 +418,8 @@ void SpellChecker::MarkMisspellingsAfterTypingCommand(
   // selection is never marked, this does a check to see if typing made a new
   // word that is not in the current selection.  Basically, you get this by
   // being at the end of a word and typing a space.
-  VisiblePosition start =
-      CreateVisiblePosition(cmd.EndingVisibleSelection().Start(),
-                            cmd.EndingVisibleSelection().Affinity());
+  const VisiblePosition start = CreateVisiblePosition(
+      cmd.EndingSelection().Start(), cmd.EndingSelection().Affinity());
   VisiblePosition previous = PreviousPositionOf(start);
 
   VisiblePosition word_start_of_previous =
@@ -710,8 +704,9 @@ void SpellChecker::UpdateMarkersForWordsAffectedByEditing(
   // updateStyleAndLayoutIgnorePendingStylesheets to caller. See
   // http://crbug.com/590369 for more details.
   GetFrame().GetDocument()->UpdateStyleAndLayoutIgnorePendingStylesheets();
-  if (!IsSpellCheckingEnabledFor(
-          GetFrame().Selection().ComputeVisibleSelectionInDOMTree()))
+  const VisibleSelection& visible_selection =
+      GetFrame().Selection().ComputeVisibleSelectionInDOMTree();
+  if (!IsSpellCheckingEnabledAt(visible_selection.Base()))
     return;
 
   Document* document = GetFrame().GetDocument();
