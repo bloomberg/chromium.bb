77b2d424182e56e062671327eec91f8b6beb07bd
Bill Budge <bbudge@chromium.org>
2017-08-30 06:44:08 +0000

    Reland "[WASM] Reserve a large block of address space on 32 bit Windows."
    Original change's description:
    > [WASM] Reserve a large block of address space on 32 bit Windows.
    >
    > - Reserves address space on 32 bit Windows, using
    >   base::ReserveAddressSpace. This will be released on the first
    >   failure in AllocPages.
    > - Implements v8::Platform::OnCriticalMemoryPressure in gin, so V8 alloc
    >   failures can also invoke base::ReleaseReservation.
    >
    > Bug: chromium:634547
    > Change-Id: I9bf7b4bb406bbd138c96ed5091b5900fd8137947
    > Reviewed-on: https://chromium-review.googlesource.com/616340
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Chris Palmer <palmer@chromium.org>
    > Reviewed-by: Brad Nelson <bradnelson@chromium.org>
    > Commit-Queue: Bill Budge <bbudge@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#495411}
    
    Change-Id: I0960ad26235ada1d727c0499a4974c216db156a3
    Reviewed-on: https://chromium-review.googlesource.com/641351
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498381}
    Former-commit-id: d1b87f05bd3c9443c11b20dcbe8ae364802e8f32

--- chrome/browser/ui/browser_browsertest.cc
+++ chrome/browser/ui/browser_browsertest.cc
@@ -485,9 +485,8 @@ IN_PROC_BROWSER_TEST_F(BrowserTest, JavascriptAlertActivatesTab) {
   EXPECT_EQ(1, browser()->tab_strip_model()->active_index());
 }
 
-#if defined(OS_WIN)
+#if defined(OS_WIN) && !defined(NDEBUG)
 // http://crbug.com/114859. Times out frequently on Windows.
-// https://crbug.com/757836. Flakily runs out of memory on Windows.
 #define MAYBE_ThirtyFourTabs DISABLED_ThirtyFourTabs
 #else
 #define MAYBE_ThirtyFourTabs ThirtyFourTabs
--- gin/public/v8_platform.h
+++ gin/public/v8_platform.h
@@ -19,6 +19,7 @@ class GIN_EXPORT V8Platform : public v8::Platform {
   static V8Platform* Get();
 
   // v8::Platform implementation.
+  void OnCriticalMemoryPressure() override;
   size_t NumberOfAvailableBackgroundThreads() override;
   void CallOnBackgroundThread(
       v8::Task* task,
