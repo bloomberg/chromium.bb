0e3acf6090999526d204fb4662e812fd9814606c
Anita Woodruff <awdf@chromium.org>
2017-08-30 18:52:00 +0000

    [Android Notification Channels] Enable site channels by default
    - This enables the kSiteNotificationChannels feature flag by default.
    
    - When this flag is enabled we use separate notification channels on
    Android O+ for notifications from different origins (instead of
    sending them all to the Sites channel).
    
    Bug: 700377
    Change-Id: I44e0b30ffa0c6bd24419b2504a1e2ac91b4cd5e6
    Reviewed-on: https://chromium-review.googlesource.com/626298
    Commit-Queue: Peter Beverloo <peter@chromium.org>
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Peter Beverloo <peter@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498552}
    Former-commit-id: caea8a624d066a8f44357ef9d6eca25a7e790956

--- chrome/browser/content_settings/host_content_settings_map_unittest.cc
+++ chrome/browser/content_settings/host_content_settings_map_unittest.cc
@@ -1956,53 +1956,58 @@ TEST_F(HostContentSettingsMapTest, LastModifiedMultipleModifiableProviders) {
 
   TestingProfile profile;
   auto* map = HostContentSettingsMapFactory::GetForProfile(&profile);
-  base::Time t1 = base::Time::Now();
-
-  auto test_clock = base::MakeUnique<base::SimpleTestClock>();
-  test_clock->SetNow(t1);
-  base::SimpleTestClock* clock = test_clock.get();
-  clock->Advance(base::TimeDelta::FromSeconds(1));
-  base::Time t2 = clock->Now();
-  // This will set the Pref Provider clock to t2.
-  map->SetClockForTesting(std::move(test_clock));
-
-  // Modify setting at t2.
   GURL url("https://www.google.com/");
   ContentSettingsPattern pattern =
       ContentSettingsPattern::FromURLNoWildcard(url);
-  map->SetContentSettingDefaultScope(url, GURL(),
-                                     CONTENT_SETTINGS_TYPE_NOTIFICATIONS,
-                                     std::string(), CONTENT_SETTING_ALLOW);
+
+  base::Time t1 = base::Time::Now();
+  auto test_clock = base::MakeUnique<base::SimpleTestClock>();
+  test_clock->SetNow(t1);
+
+  base::SimpleTestClock* clock = test_clock.get();
+  clock->Advance(base::TimeDelta::FromSeconds(1));
+  base::Time t2 = clock->Now();
 
   // Register a provider which reports a modification time of t1.
-  std::unique_ptr<MockUserModifiableProvider> mock_provider =
+  std::unique_ptr<MockUserModifiableProvider> provider =
       base::MakeUnique<MockUserModifiableProvider>();
-  EXPECT_CALL(*mock_provider, GetWebsiteSettingLastModified(
-                                  _, _, CONTENT_SETTINGS_TYPE_NOTIFICATIONS, _))
+  EXPECT_CALL(*provider, GetWebsiteSettingLastModified(
+                             _, _, CONTENT_SETTINGS_TYPE_NOTIFICATIONS, _))
       .WillOnce(Return(t1));
-  MockUserModifiableProvider* weak_mock_provider = mock_provider.get();
+  MockUserModifiableProvider* weak_provider = provider.get();
   map->RegisterUserModifiableProvider(
-      HostContentSettingsMap::NOTIFICATION_ANDROID_PROVIDER,
-      std::move(mock_provider));
+      HostContentSettingsMap::PROVIDER_FOR_TESTS, std::move(provider));
+
+  // Register another provider which reports a modification time of t2.
+  std::unique_ptr<MockUserModifiableProvider> other_provider =
+      base::MakeUnique<MockUserModifiableProvider>();
+  EXPECT_CALL(*other_provider,
+              GetWebsiteSettingLastModified(
+                  _, _, CONTENT_SETTINGS_TYPE_NOTIFICATIONS, _))
+      .WillRepeatedly(Return(t2));
+  MockUserModifiableProvider* weak_other_provider = other_provider.get();
+  map->RegisterUserModifiableProvider(
+      HostContentSettingsMap::OTHER_PROVIDER_FOR_TESTS,
+      std::move(other_provider));
 
   // Expect the more recent modification time to be reported.
   EXPECT_EQ(t2, map->GetSettingLastModifiedDate(
                     pattern, ContentSettingsPattern::Wildcard(),
                     CONTENT_SETTINGS_TYPE_NOTIFICATIONS));
 
-  // Now have registered provider report a more recent modification time.
+  // Now have original provider report a more recent modification time.
   clock->Advance(base::TimeDelta::FromSeconds(1));
   base::Time t3 = clock->Now();
-  EXPECT_CALL(*weak_mock_provider,
-              GetWebsiteSettingLastModified(
-                  _, _, CONTENT_SETTINGS_TYPE_NOTIFICATIONS, _))
+  EXPECT_CALL(*weak_provider, GetWebsiteSettingLastModified(
+                                  _, _, CONTENT_SETTINGS_TYPE_NOTIFICATIONS, _))
       .WillOnce(Return(t3));
 
   // Expect the timestamp from the registered provider to be reported now.
   EXPECT_EQ(t3, map->GetSettingLastModifiedDate(
                     pattern, ContentSettingsPattern::Wildcard(),
                     CONTENT_SETTINGS_TYPE_NOTIFICATIONS));
-  weak_mock_provider->RemoveObserver(map);
+  weak_provider->RemoveObserver(map);
+  weak_other_provider->RemoveObserver(map);
 }
 
 TEST_F(HostContentSettingsMapTest, LastModifiedIsNotRecordedWhenDisabled) {
