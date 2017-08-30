375fcb1439fff1de13511129738c2efe0cbb1fa5
Christopher Cameron <ccameron@chromium.org>
2017-08-30 19:05:37 +0000

    color: Move ICC profile histograms from blink to gfx
    Histogram all new ICC profiles that are created from raw data. Do this
    only when creating the profile from data, not
     - when receiving a profile via IPC
     - when creating a profile from data that is already cached
    
    In histogramming the results of parsing and ICC profile, disambiguate
    several separate states that were previously merged.
    
    The old histogramming code would attempt to numerically approximate
    all channels separately, but not actually use the result. For the
    moment, remove this. Only approximate the combined channels.
    
    R=hubbe
    TBR=chrishtr (third_party/WebKit/...), holte (tools/metrics/...)
    
    Bug: 754701
    Change-Id: Iee03959a8a6f96610cc3977311fcd61da5547ac7
    Reviewed-on: https://chromium-review.googlesource.com/612252
    Commit-Queue: ccameron chromium <ccameron@chromium.org>
    Reviewed-by: Fredrik Hubinette <hubbe@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498558}
    Former-commit-id: 598ce823a09c136e054cc288c0f9146e61b47c3a

--- third_party/WebKit/Source/platform/graphics/ColorBehavior.cpp
+++ third_party/WebKit/Source/platform/graphics/ColorBehavior.cpp
@@ -15,103 +15,9 @@ namespace blink {
 
 namespace {
 
-// This must match ICCProfileAnalyzeResult enum in histograms.xml.
-enum ICCAnalyzeResult {
-  kICCExtractedMatrixAndAnalyticTrFn = 0,
-  kICCExtractedMatrixAndApproximatedTrFn = 1,
-  kICCFailedToApproximateTrFn = 2,
-  kICCFailedToExtractRawTrFn = 3,
-  kICCFailedToExtractMatrix = 4,
-  kICCFailedToParse = 5,
-  kICCProfileAnalyzeLast = kICCFailedToParse
-};
-
 // The output device color space is global and shared across multiple threads.
 SpinLock g_target_color_space_lock;
 gfx::ColorSpace* g_target_color_space = nullptr;
-
-ICCAnalyzeResult HistogramICCProfile(const gfx::ICCProfile& profile) {
-  std::vector<char> data = profile.GetData();
-  sk_sp<SkICC> sk_icc = SkICC::Make(data.data(), data.size());
-  if (!sk_icc)
-    return kICCFailedToParse;
-
-  SkMatrix44 to_xyzd50;
-  bool to_xyzd50_result = sk_icc->toXYZD50(&to_xyzd50);
-  if (!to_xyzd50_result)
-    return kICCFailedToExtractMatrix;
-
-  SkColorSpaceTransferFn fn;
-  bool is_numerical_transfer_fn_result = sk_icc->isNumericalTransferFn(&fn);
-  if (is_numerical_transfer_fn_result)
-    return kICCExtractedMatrixAndAnalyticTrFn;
-
-  // Analyze the numerical approximation of table-based transfer functions.
-  // This should never fail in practice, because any profile from which a
-  // primary matrix was extracted will also provide raw transfer data.
-  SkICC::Tables tables;
-  bool raw_transfer_fn_result = sk_icc->rawTransferFnData(&tables);
-  DCHECK(raw_transfer_fn_result);
-  if (!raw_transfer_fn_result)
-    return kICCFailedToExtractRawTrFn;
-
-  // Analyze the channels separately.
-  std::vector<float> x_combined;
-  std::vector<float> t_combined;
-  for (size_t c = 0; c < 3; ++c) {
-    SkICC::Channel* channels[3] = {&tables.fRed, &tables.fGreen, &tables.fBlue};
-    SkICC::Channel* channel = channels[c];
-    DCHECK_GE(channel->fCount, 2);
-    const float* data = reinterpret_cast<const float*>(
-        tables.fStorage->bytes() + channel->fOffset);
-    std::vector<float> x;
-    std::vector<float> t;
-    for (int i = 0; i < channel->fCount; ++i) {
-      float xi = i / (channel->fCount - 1.f);
-      float ti = data[i];
-      x.push_back(xi);
-      t.push_back(ti);
-      x_combined.push_back(xi);
-      t_combined.push_back(ti);
-    }
-
-    bool nonlinear_fit_converged =
-        gfx::SkApproximateTransferFn(x.data(), t.data(), x.size(), &fn);
-    UMA_HISTOGRAM_BOOLEAN("Blink.ColorSpace.Destination.NonlinearFitConverged",
-                          nonlinear_fit_converged);
-
-    // Record the accuracy of the fit, separating out by nonlinear and
-    // linear fits.
-    if (nonlinear_fit_converged) {
-      float max_error = 0.f;
-      for (size_t i = 0; i < x.size(); ++i) {
-        float fn_of_xi = gfx::SkTransferFnEval(fn, x[i]);
-        float error_at_xi = std::abs(t[i] - fn_of_xi);
-        max_error = std::max(max_error, error_at_xi);
-      }
-      UMA_HISTOGRAM_CUSTOM_COUNTS(
-          "Blink.ColorSpace.Destination.NonlinearFitError",
-          static_cast<int>(max_error * 255), 0, 127, 16);
-    }
-  }
-
-  bool combined_nonlinear_fit_converged = gfx::SkApproximateTransferFn(
-      x_combined.data(), t_combined.data(), x_combined.size(), &fn);
-  if (!combined_nonlinear_fit_converged)
-    return kICCFailedToApproximateTrFn;
-
-  float combined_max_error = 0.f;
-  for (size_t i = 0; i < x_combined.size(); ++i) {
-    float fn_of_xi = gfx::SkTransferFnEval(fn, x_combined[i]);
-    float error_at_xi = std::abs(t_combined[i] - fn_of_xi);
-    combined_max_error = std::max(combined_max_error, error_at_xi);
-  }
-  UMA_HISTOGRAM_CUSTOM_COUNTS(
-      "Blink.ColorSpace.Destination.NonlinearFitErrorCombined",
-      static_cast<int>(combined_max_error * 255), 0, 127, 16);
-
-  return kICCExtractedMatrixAndApproximatedTrFn;
-}
 
 }  // namespace
 
@@ -127,13 +33,8 @@ void ColorBehavior::SetGlobalTargetColorProfile(
     return;
 
   // Attempt to convert the ICC profile to an SkColorSpace.
-  if (profile != gfx::ICCProfile()) {
+  if (profile != gfx::ICCProfile())
     g_target_color_space = new gfx::ColorSpace(profile.GetColorSpace());
-
-    ICCAnalyzeResult analyze_result = HistogramICCProfile(profile);
-    UMA_HISTOGRAM_ENUMERATION("Blink.ColorSpace.Destination.ICCResult",
-                              analyze_result, kICCProfileAnalyzeLast);
-  }
 
   // If we do not succeed, assume sRGB.
   if (!g_target_color_space)
