3f2385bf207c5082ef5100962b8c018ad839d03f
Bill Budge <bbudge@chromium.org>
2017-08-30 06:44:08 +0000

    Reland "[WASM] Reserve a large block of address space on 32 bit Windows."
    Original change's description:
    > [WASM] Reserve a large block of address space on 32 bit Windows.
    >
    > - Reserves address space on 32 bit Windows, using
    >   base::ReserveAddressSpace. This will be released on the first
    >   failure in AllocPages.
    > - Implements v8::Platform::OnCriticalMemoryPressure in gin, so V8 alloc
    >   failures can also invoke base::ReleaseReservation.
    >
    > Bug: chromium:634547
    > Change-Id: I9bf7b4bb406bbd138c96ed5091b5900fd8137947
    > Reviewed-on: https://chromium-review.googlesource.com/616340
    > Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    > Reviewed-by: Chris Palmer <palmer@chromium.org>
    > Reviewed-by: Brad Nelson <bradnelson@chromium.org>
    > Commit-Queue: Bill Budge <bbudge@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#495411}
    
    Change-Id: I0960ad26235ada1d727c0499a4974c216db156a3
    Reviewed-on: https://chromium-review.googlesource.com/641351
    Reviewed-by: Chris Palmer <palmer@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Jeremy Roman <jbroman@chromium.org>
    Commit-Queue: Bill Budge <bbudge@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498381}
    Former-commit-id: d1b87f05bd3c9443c11b20dcbe8ae364802e8f32

--- third_party/WebKit/Source/controller/BlinkInitializer.cpp
+++ third_party/WebKit/Source/controller/BlinkInitializer.cpp
@@ -30,6 +30,7 @@
 
 #include "bindings/core/v8/V8Initializer.h"
 #include "bindings/modules/v8/V8ContextSnapshotExternalReferences.h"
+#include "build/build_config.h"
 #include "core/animation/AnimationClock.h"
 #include "modules/ModulesInitializer.h"
 #include "platform/bindings/Microtask.h"
@@ -69,8 +70,27 @@ static ModulesInitializer& GetModulesInitializer() {
 void Initialize(Platform* platform) {
   Platform::Initialize(platform);
 
+#if !defined(ARCH_CPU_X86_64) && !defined(ARCH_CPU_ARM64) && defined(OS_WIN)
+  // Reserve address space on 32 bit Windows, to make it likelier that large
+  // array buffer allocations succeed.
+  BOOL is_wow_64 = -1;
+  if (!IsWow64Process(GetCurrentProcess(), &is_wow_64))
+    is_wow_64 = FALSE;
+  if (!is_wow_64) {
+    // Try to reserve as much address space as we reasonably can.
+    const size_t kMB = 1024 * 1024;
+    for (size_t size = 512 * kMB; size >= 32 * kMB; size -= 16 * kMB) {
+      if (base::ReserveAddressSpace(size)) {
+        break;
+      }
+    }
+  }
+#endif  // !defined(ARCH_CPU_X86_64) && !defined(ARCH_CPU_ARM64) &&
+        // defined(OS_WIN)
+
   V8Initializer::InitializeMainThread(
       V8ContextSnapshotExternalReferences::GetTable());
+
   GetModulesInitializer().Initialize();
 
   // currentThread is null if we are running on a thread without a message loop.
