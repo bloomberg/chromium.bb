f1d43abfbcf1e15fe981f3e95b18759a95e6701c
Adenilson Cavalcanti <adenilson.cavalcanti@arm.com>
2017-08-31 21:10:52 +0000

    Zlib patch: prevent uninitialized use of state->check
    No need to call the Adler32 checksum function, just set the
    struct field to the expected value.
    
    Upstream bug: madler/zlib#245
    
    Bug: chromium:697481
    Change-Id: Ib972cc2507c8e7ca0b0b48464db33880ef960fb8
    Reviewed-on: https://chromium-review.googlesource.com/644505
    Commit-Queue: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Reviewed-by: Mike Klein <mtklein@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498998}
    Former-commit-id: f4b484415281f09d0bbc0880f5d41dbdde96c209

--- third_party/zlib/contrib/arm/inflate.c
+++ third_party/zlib/contrib/arm/inflate.c
@@ -229,6 +229,8 @@ int stream_size;
     state->strm = strm;
     state->window = Z_NULL;
     state->mode = HEAD;     /* to pass state test in inflateReset2() */
+    /* 1L is the result of adler32() on zero length data */
+    state->check = 1L;
     ret = inflateReset2(strm, windowBits);
     if (ret != Z_OK) {
         ZFREE(strm, state);
