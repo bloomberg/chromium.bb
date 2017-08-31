fdf53c2d781c3685f6faf40774e34ae8916274f7
Tarun Bansal <tbansal@chromium.org>
2017-08-31 01:26:38 +0000

    Add histograms to record raw observations in Network Quality Estimator (NQE).
    Bug: 755794
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Change-Id: Icd925fb3e597788c1bc3db5beb9ab9a703e012b3
    Reviewed-on: https://chromium-review.googlesource.com/636701
    Reviewed-by: Jesse Doherty <jwd@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498705}
    Former-commit-id: d2677a15c24d5b46ebff78b6d9a16f2e1ff8f969

--- net/nqe/network_quality_estimator.cc
+++ net/nqe/network_quality_estimator.cc
@@ -1640,6 +1640,12 @@ void NetworkQualityEstimator::AddAndNotifyObserversOfRTT(
   UMA_HISTOGRAM_ENUMERATION("NQE.RTT.ObservationSource", observation.source,
                             NETWORK_QUALITY_OBSERVATION_SOURCE_MAX);
 
+  base::HistogramBase* raw_observation_histogram = base::Histogram::FactoryGet(
+      std::string("NQE.RTT.RawObservation.") +
+          nqe::internal::GetNameForObservationSource(observation.source),
+      1, 10 * 1000, 50, base::HistogramBase::kUmaTargetedHistogramFlag);
+  raw_observation_histogram->Add(observation.value);
+
   // Maybe recompute the effective connection type since a new RTT observation
   // is available.
   MaybeComputeEffectiveConnectionType();
@@ -1659,6 +1665,12 @@ void NetworkQualityEstimator::AddAndNotifyObserversOfThroughput(
 
   UMA_HISTOGRAM_ENUMERATION("NQE.Kbps.ObservationSource", observation.source,
                             NETWORK_QUALITY_OBSERVATION_SOURCE_MAX);
+
+  base::HistogramBase* raw_observation_histogram = base::Histogram::FactoryGet(
+      std::string("NQE.Kbps.RawObservation.") +
+          nqe::internal::GetNameForObservationSource(observation.source),
+      1, 10 * 1000, 50, base::HistogramBase::kUmaTargetedHistogramFlag);
+  raw_observation_histogram->Add(observation.value);
 
   // Maybe recompute the effective connection type since a new throughput
   // observation is available.
