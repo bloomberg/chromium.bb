b69a13f6785bb7bc1fc76c5e146ed45659095cb8
Henrik Kjellander <kjellander@chromium.org>
2017-08-31 12:04:36 +0000

    Remove files in third_party/webrtc_overrides/webrtc/base.
    These files were kept around in
    https://chromium-review.googlesource.com/554730
    but should be deleted now that the move is completed in WebRTC.
    
    BUG=webrtc:7634
    
    Change-Id: I2002fa4b0dcb183e98cdfd4a6373fc4b98d016fd
    Reviewed-on: https://chromium-review.googlesource.com/641876
    Reviewed-by: Henrik Grunell <grunell@chromium.org>
    Commit-Queue: Henrik Kjellander <kjellander@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498825}
    Former-commit-id: c6f67ca22af1cd509dee7418d72ab1f42deb877e

--- third_party/webrtc_overrides/webrtc/base/logging.h
+++
@@ -1,8 +0,0 @@
-#ifndef THIRD_PARTY_WEBRTC_OVERRIDES_WEBRTC_BASE_LOGGING_H_
-#define THIRD_PARTY_WEBRTC_OVERRIDES_WEBRTC_BASE_LOGGING_H_
-
-// This header is deprecated and is just left here temporarily during
-// refactoring. See https://bugs.webrtc.org/7634 for more details.
-#include "third_party/webrtc_overrides/webrtc/rtc_base/logging.h"
-
-#endif  // THIRD_PARTY_WEBRTC_OVERRIDES_WEBRTC_BASE_LOGGING_H_
