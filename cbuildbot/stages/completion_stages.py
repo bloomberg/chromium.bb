f542336f1ad33cbf13fe2fdecd90a7908d9e46e1
Steven Bennetts <stevenjb@chromium.org>
2017-08-30 19:18:59 -0700

    cros_chromeos_lkgm: Add UploadNewLKGM stage
    BUG=chromium:732579
    TEST=CHROMEOS_LKGM is updated and committed in master-chromium-pfq
    
    Change-Id: Id443c391b98dd34ccb410c3501c39bc28edf0f56
    Reviewed-on: https://chromium-review.googlesource.com/644413
    Tested-by: Steven Bennetts <stevenjb@chromium.org>
    Trybot-Ready: Steven Bennetts <stevenjb@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>

--- cbuildbot/stages/completion_stages.py
+++ cbuildbot/stages/completion_stages.py
@@ -952,8 +952,8 @@ class UpdateChromeosLKGMStage(generic_stages.BuilderStage):
     cmd = ['chrome_chromeos_lkgm', '--lkgm=%s' % manager.current_version]
     # Always do a dryrun for now so that we can check the output and ensure it
     # is doing the correct thing.
-    # TODO(stevenjb): if self._run.options.debug:
-    cmd.append('--dryrun')
+    if self._run.options.debug:
+      cmd.append('--dryrun')
     commands.RunBuildScript(self._build_root, cmd, chromite_cmd=True)
 
 
