d25110cb6c4241585443f4f530128d90a2d64169
Bernhard Bauer <bauerb@chromium.org>
2017-08-31 23:22:16 +0000

    Update "all dismissed" UI for Chrome Home Modern.
    Add a new illustration with a white background (sigh),
    rename the old one to ..._gray, and update the text.
    
    Screenshot: https://goo.gl/photos/DQWHs1W26dF57fgKA
    
    Bug: 754194
    Change-Id: Iecd6684b6e1f485f1cbca0bb96cbb8b6843be4dc
    Reviewed-on: https://chromium-review.googlesource.com/645808
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499068}
    Former-commit-id: 2a3de6795bf6ee9e3dae2313991c8b86f6a7f06f

--- chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/AllDismissedItem.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ntp/cards/AllDismissedItem.java
@@ -6,8 +6,10 @@ package org.chromium.chrome.browser.ntp.cards;
 
 import android.support.annotation.StringRes;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.chromium.chrome.R;
@@ -54,21 +56,30 @@ public class AllDismissedItem extends OptionalLeaf {
             mBodyTextView = itemView.findViewById(R.id.body_text);
 
             Button refreshButton = itemView.findViewById(R.id.action_button);
+            ImageView backgroundView = itemView.findViewById(R.id.image);
             if (FeatureUtilities.isChromeHomeModernEnabled()) {
                 ((ViewGroup) itemView).removeView(refreshButton);
+
+                // Hide the view instead of removing it, because it is used to layout subsequent
+                // views.
+                itemView.findViewById(R.id.title_text).setVisibility(View.GONE);
+                backgroundView.setImageResource(R.drawable.ntp_all_dismissed_white);
             } else {
                 refreshButton.setOnClickListener(v -> {
                     NewTabPageUma.recordAction(NewTabPageUma.ACTION_CLICKED_ALL_DISMISSED_REFRESH);
                     sections.restoreDismissedSections();
                 });
+                backgroundView.setImageResource(R.drawable.ntp_all_dismissed_gray);
             }
         }
 
         public void onBindViewHolder() {
             int hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
             @StringRes
-            int messageId;
-            if (hour >= 0 && hour < 12) {
+            final int messageId;
+            if (FeatureUtilities.isChromeHomeModernEnabled()) {
+                messageId = R.string.ntp_all_dismissed_body_text_modern;
+            } else if (hour >= 0 && hour < 12) {
                 messageId = R.string.ntp_all_dismissed_body_text_morning;
             } else if (hour >= 12 && hour < 17) {
                 messageId = R.string.ntp_all_dismissed_body_text_afternoon;
