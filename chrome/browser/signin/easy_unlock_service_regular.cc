b8c27817be72756dc25e8aed708f7703bd4792ce
Tim Song <tengs@chromium.org>
2017-08-31 22:44:52 +0000

    [EasyUnlock] Fix crash when BLE is disabled through chrome://flags.
    We are seeing crash reports for users who have this flag enabled:
    --disable-proximity-auth-bluetooth-low-energy-discovery
    
    Although we plan on deleting this flag, we still would like to keep it for the
    time being in case we need to revert to EasyUnlock v1.
    
    BUG=757890
    TEST=manually confirmed setup works with flag.
    
    Change-Id: I832195c471315afdcf516572aa6bcebc2c7eac1e
    Reviewed-on: https://chromium-review.googlesource.com/646632
    Reviewed-by: Gustavo Sacomoto <sacomoto@chromium.org>
    Commit-Queue: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499055}
    Former-commit-id: 7fa976fb9f2f1b59dea9034c793eea7c496b61d9

--- chrome/browser/signin/easy_unlock_service_regular.cc
+++ chrome/browser/signin/easy_unlock_service_regular.cc
@@ -491,15 +491,15 @@ void EasyUnlockServiceRegular::SetAutoPairingResult(
 void EasyUnlockServiceRegular::InitializeInternal() {
   proximity_auth::ScreenlockBridge::Get()->AddObserver(this);
 
+  pref_manager_.reset(new proximity_auth::ProximityAuthProfilePrefManager(
+      profile()->GetPrefs()));
+
 #if defined(OS_CHROMEOS)
   // TODO(tengs): Due to badly configured browser_tests, Chrome crashes during
   // shutdown. Revisit this condition after migration is fully completed.
   if (!base::CommandLine::ForCurrentProcess()->HasSwitch(
           proximity_auth::switches::kDisableBluetoothLowEnergyDiscovery) &&
       !base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kTestType)) {
-    pref_manager_.reset(new proximity_auth::ProximityAuthProfilePrefManager(
-        profile()->GetPrefs()));
-
     // Note: There is no local state in tests.
     if (g_browser_process->local_state()) {
       pref_manager_->StartSyncingToLocalState(g_browser_process->local_state(),
