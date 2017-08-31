77fb576c82e012f0772d59ac95fb8ff8a349de54
Henrik Boström <hbos@chromium.org>
2017-08-31 10:25:28 +0000

    Revert "Move fully generated StyleBuilderFunction methods to corresponding CSSPropertyAPIs."
    This reverts commit d2b2e9ea2812cfab2e46e647dc868bc7acaed62b [formerly f8e9b77f8f6b1c037b4a92701c0d35aff18326f4].
    
    Reason for revert:
    Speculative revert, Google Chrome Win fails to compile, it can't link third_party/WebKit/Source/core/css/ libs and this is the onle CL in the blamelist that touches css stuff. First compile error:
    https://uberchromegw.corp.google.com/i/chromium.chrome/builders/Google%20Chrome%20Win/builds/21614
    
    Original change's description:
    > Move fully generated StyleBuilderFunction methods to corresponding CSSPropertyAPIs.
    > 
    > This is almost exactly the same patch as crrev.com/c/625644 prepared by meade@.
    > As meade@ is away at the moment, I am submitting the patch so that we can
    > move forward with the other StyleBuilderFunctions. Differences between this
    > patch and the original patch is based on review comments from bugsnash@:
    > 1. Removed duplicated macros in StyleBuilderFunctions.cpp.tmpl. They are now
    > imported from application_macros.tmpl.
    > 2. In make_css_property_api_headers.py, we no longer need to unpack fields from
    > property as we now return property. 
    > 3. Related to 2, in CSSPropertyAPISubclass.h.tmpl, we now directly access 
    > fields from property. 
    > 
    > This patch only moves StyleBuilderFunctions for properties, which have 
    > API implementations and do not share API implementations with other properties.
    > We would need to consider whether to break up these properties' shared APIs first.
    > 
    > This patch also leaves out custom StyleBuilderFunctions, which will be handled
    > in later patches. 
    > 
    > Also quoting descriptions from original patch:
    > 
    > ================
    > 
    > There is some duplicated code (make_css_property_api_headers.py and
    > make_style_builder.py, also application_macros.tmpl+CSPropertyAPISubclass.tmpl
    > and StyleBuilder.cpp.tmpl). Hopefully this is ok since we can delete the old ones
    > soon. In the mean time I don't think it's worth trying to figure out how to share
    > the code.
    > 
    > Diff of representative changed APIs and StyleBuilder.cpp:
    > https://gist.github.com/wilddamon/c1fa5fc53a0433ef6028e0be5335910b/revisions
    > 
    > StyleBuilderFunctions diff:
    > https://gist.github.com/wilddamon/5d95f4bdec0a8e61c131248b12b57992/revisions
    > 
    > Bug: 751354
    > Change-Id: Ib0168e65628e76232ba3c28e95a23e02b0ebbd4a
    > Reviewed-on: https://chromium-review.googlesource.com/637047
    > Commit-Queue: Jia Meng <jiameng@chromium.org>
    > Reviewed-by: nainar <nainar@chromium.org>
    > Reviewed-by: Renée Wright <rjwright@chromium.org>
    > Reviewed-by: Bugs Nash <bugsnash@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#498790}
    
    TBR=rjwright@chromium.org,nainar@chromium.org,jiameng@chromium.org,bugsnash@chromium.org
    
    Change-Id: Ia321c7d24e8928286c694c56eb6c5fe162b51077
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 751354
    Reviewed-on: https://chromium-review.googlesource.com/645566
    Reviewed-by: Henrik Boström <hbos@chromium.org>
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498805}
    Former-commit-id: a4d7ce3def7a91b7b87eafe5b398dd16aed599de

--- third_party/WebKit/Source/build/scripts/make_style_builder.py
+++ third_party/WebKit/Source/build/scripts/make_style_builder.py
@@ -28,7 +28,6 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import sys
-import types
 
 from core.css import css_properties
 import json5_generator
@@ -41,15 +40,14 @@ def apply_property_naming_defaults(property_):
         if property_[key] is None:
             property_[key] = value
 
-    # TODO(meade): Delete this once all methods are moved to CSSPropertyAPIs.
+    # TODO(shend): Use name_utilities for manipulating names.
+    # TODO(shend): Rearrange the code below to separate assignment and set_if_none
     upper_camel = upper_camel_case(property_['name'])
-    set_if_none(
-        property_, 'name_for_methods', upper_camel.replace('Webkit', ''))
+    set_if_none(property_, 'name_for_methods', upper_camel.replace('Webkit', ''))
     name = property_['name_for_methods']
     simple_type_name = str(property_['type_name']).split('::')[-1]
     set_if_none(property_, 'type_name', 'E' + name)
-    set_if_none(
-        property_, 'getter', name if simple_type_name != name else 'Get' + name)
+    set_if_none(property_, 'getter', name if simple_type_name != name else 'Get' + name)
     set_if_none(property_, 'setter', 'Set' + name)
     set_if_none(property_, 'inherited', False)
     set_if_none(property_, 'initial', 'Initial' + name)
@@ -60,22 +58,9 @@ def apply_property_naming_defaults(property_):
         property_['custom_value'] = True
     if property_['inherited']:
         property_['is_inherited_setter'] = 'Set' + name + 'IsInherited'
-    property_['should_declare_functions'] = \
-        not property_['use_handlers_for'] \
-        and not property_['longhands'] \
-        and not property_['direction_aware'] \
-        and not property_['builder_skip'] \
+    property_['should_declare_functions'] = not property_['use_handlers_for'] and not property_['longhands'] \
+        and not property_['direction_aware'] and not property_['builder_skip'] \
         and property_['is_property']
-    # Functions should only be used in StyleBuilder if the CSSPropertyAPI
-    # class is shared or not implemented yet (shared classes are denoted by
-    # api_class = "some string").
-    property_['use_api_in_stylebuilder'] = \
-        property_['should_declare_functions'] \
-        and not (property_['custom_initial'] or
-                 property_['custom_inherit'] or
-                 property_['custom_value']) \
-        and property_['api_class'] \
-        and isinstance(property_['api_class'], types.BooleanType)
 
 
 class StyleBuilderWriter(css_properties.CSSProperties):
