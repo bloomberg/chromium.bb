e6a7c1665280f51c5357185180c81add2575aac5
Olga Sharonova <olka@chromium.org>
2017-08-31 19:01:33 +0000

    DeviceMonitorMac: pass |device_task_tunner| in the constructor.
    At the moment MediaDevicesManager takes the task runner info from AudioSystem and passes it to DeviceMonitorMac::StartMonitoring().
    When AudioSystem is switched to Mojo it won't be aware of the device task runner any more.
    So we are removing AudioSystem::GetTaskRunner() everywhere (which is here and in MediaStreamManager).
    
    Bug: 740943
    Change-Id: I5031cb3865cea95d6754f8dbf01dc88064962de1
    Reviewed-on: https://chromium-review.googlesource.com/644466
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Commit-Queue: Olga Sharonova <olka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498940}
    Former-commit-id: 6d6c995a6296327105a596efce286777d2e9077d

--- content/browser/renderer_host/media/media_devices_manager.cc
+++ content/browser/renderer_host/media/media_devices_manager.cc
@@ -273,17 +273,10 @@ void MediaDevicesManager::StartMonitoringOnUIThread() {
 
   // TODO(erikchen): Remove ScopedTracker below once crbug.com/458404 is
   // fixed.
-  tracked_objects::ScopedTracker tracking_profile2(
-      FROM_HERE_WITH_EXPLICIT_FUNCTION(
-          "458404 MediaDevicesManager::GetTaskRunner"));
-  const scoped_refptr<base::SingleThreadTaskRunner> task_runner =
-      audio_system_->GetTaskRunner();
-  // TODO(erikchen): Remove ScopedTracker below once crbug.com/458404 is
-  // fixed.
   tracked_objects::ScopedTracker tracking_profile3(
       FROM_HERE_WITH_EXPLICIT_FUNCTION(
           "458404 MediaDevicesManager::DeviceMonitorMac::StartMonitoring"));
-  browser_main_loop->device_monitor_mac()->StartMonitoring(task_runner);
+  browser_main_loop->device_monitor_mac()->StartMonitoring();
 }
 #endif
 
