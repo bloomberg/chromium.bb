90483605b98fa988cadbbc4041e38b81870ec2f5
Pavel Feldman <pfeldman@chromium.org>
2017-08-31 05:02:10 +0000

    DevTools: mark screenshot region with Cmd on Mac.`
    Bug: 
    Change-Id: Ieeafe2a4ce4037fc3dcbedc170ff918373ec3d27
    Reviewed-on: https://chromium-review.googlesource.com/644124
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498756}
    Former-commit-id: b99f4f0c3e1108f9cddc288699ee173ad1ad1be7

--- third_party/WebKit/Source/core/inspector/InspectorOverlayAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorOverlayAgent.cpp
@@ -144,6 +144,12 @@ bool ParseQuad(std::unique_ptr<protocol::Array<double>> quad_array,
   return true;
 }
 
+#if defined(OS_MACOSX)
+const int kCtrlOrMeta = WebInputEvent::kMetaKey;
+#else
+const int kCtrlOrMeta = WebInputEvent::kControlKey;
+#endif
+
 }  // namespace
 
 class InspectorOverlayAgent::InspectorPageOverlayDelegate final
@@ -991,7 +997,7 @@ bool InspectorOverlayAgent::HandleMouseMove(const WebMouseEvent& event) {
   if (!ShouldSearchForNode())
     return false;
 
-  if (event.GetModifiers() & WebInputEvent::kControlKey) {
+  if (event.GetModifiers() & kCtrlOrMeta) {
     InnerHideHighlight();
     hovered_node_for_inspect_mode_.Clear();
     if (screenshot_mode_) {
@@ -1060,7 +1066,7 @@ bool InspectorOverlayAgent::HandleMouseDown(const WebMouseEvent& event) {
   if (!ShouldSearchForNode())
     return false;
 
-  if ((event.GetModifiers() & WebInputEvent::kControlKey) &&
+  if ((event.GetModifiers() & kCtrlOrMeta) &&
       (event.GetModifiers() & WebInputEvent::kLeftButtonDown)) {
     InnerHideHighlight();
     hovered_node_for_inspect_mode_.Clear();
