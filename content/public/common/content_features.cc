7c426cdc46c61c18180a5e1b2b29fa22a4657bf1
Xida Chen <xidachen@chromium.org>
2017-08-31 01:37:10 +0000

    Remove the status for "Canvas2dImageChromium"
    This CL removes the status for the "Canvas2dImageChromium" flag in the
    RuntimeEnabledFeature.json5. This feature should be enabled on Mac only.
    
    This CL fixes a regression here:
    https://chromium-review.googlesource.com/c/chromium/src/+/612850
    where we deprecate the usage of USE_IOSURFACE_FOR_2D_CANVAS.
    So after the above CL, the Canvas2DLayerBridge will also use the
    IOSurface code path whenever the "Canvas2dImageChromium" flag is on.
    
    Bug: 755253
    Change-Id: I55d5edd5cf8ba14b8dc46856246ad4f35113faf6
    Reviewed-on: https://chromium-review.googlesource.com/643149
    Reviewed-by: Antoine Labour <piman@chromium.org>
    Reviewed-by: Erik Chen <erikchen@chromium.org>
    Reviewed-by: Justin Novosad <junov@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#498716}
    Former-commit-id: d6f5cd725ddf5ebc6ec23b3976bc367aa2b08a09

--- content/public/common/content_features.cc
+++ content/public/common/content_features.cc
@@ -48,8 +48,14 @@ const base::Feature kBuggyRSAParser{"BuggyRSAParser",
 
 // If Canvas2D Image Chromium is allowed, this feature controls whether it is
 // enabled.
-const base::Feature kCanvas2DImageChromium{"Canvas2DImageChromium",
-                                           base::FEATURE_ENABLED_BY_DEFAULT};
+const base::Feature kCanvas2DImageChromium {
+  "Canvas2DImageChromium",
+#if defined(OS_MACOSX) || defined(OS_CHROMEOS)
+      base::FEATURE_ENABLED_BY_DEFAULT
+#else
+      base::FEATURE_DISABLED_BY_DEFAULT
+#endif
+};
 
 // Enabled decoding images asynchronously from raster in the renderer
 // compositor.
