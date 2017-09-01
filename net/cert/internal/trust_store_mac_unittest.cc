13511aca05b9c4322c71ab5b6d850621113f74e6
Matt Mueller <mattm@chromium.org>
2017-09-01 18:41:40 +0000

    Use kSecTrustOptionAllowExpired in TrustStoreMacTest.SystemCerts.
    Since the cert in the SecTrustEvaluate call is both the leaf and a (potential)
    root, the kSecTrustOptionAllowExpiredRoot was insufficient,
    and kSecTrustOptionAllowExpired is required.
    
    Re-enables TrustStoreMacTest.SystemCerts.
    
    Bug: 757540
    Change-Id: I3f26a05ba23624218a25737cca0bb753c1e50219
    Reviewed-on: https://chromium-review.googlesource.com/636754
    Reviewed-by: Ryan Sleevi <rsleevi@chromium.org>
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499242}
    Former-commit-id: 37c31877fe599c7c41f7af374105bc353dac7b14

--- net/cert/internal/trust_store_mac_unittest.cc
+++ net/cert/internal/trust_store_mac_unittest.cc
@@ -204,8 +204,7 @@ TEST(TrustStoreMacTest, MultiRootNotTrusted) {
 
 // Test against all the certificates in the default keychains. Confirms that
 // the computed trust value matches that of SecTrustEvaluate.
-// Fails on Mac 10.12. See http://crbug.com/757540.
-TEST(TrustStoreMacTest, DISABLED_SystemCerts) {
+TEST(TrustStoreMacTest, SystemCerts) {
   // Get the list of all certificates in the user & system keychains.
   // This may include both trusted and untrusted certificates.
   //
@@ -284,9 +283,9 @@ TEST(TrustStoreMacTest, DISABLED_SystemCerts) {
                 SecTrustCreateWithCertificates(cert_handle, sec_policy,
                                                trust.InitializeInto()));
       ASSERT_EQ(noErr,
-                SecTrustSetOptions(
-                    trust,
-                    kSecTrustOptionLeafIsCA | kSecTrustOptionAllowExpiredRoot));
+                SecTrustSetOptions(trust, kSecTrustOptionLeafIsCA |
+                                              kSecTrustOptionAllowExpired |
+                                              kSecTrustOptionAllowExpiredRoot));
 
       SecTrustResultType trust_result;
       ASSERT_EQ(noErr, SecTrustEvaluate(trust, &trust_result));
