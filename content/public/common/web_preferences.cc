aae97ea179fb1f786450533dfcbab6e65ea79ace
Tobias Sargeant <tobiasjs@google.com>
2017-09-01 01:18:09 +0000

    Disable ScrollTopLeftInterop in WebView.
    Applications depend on the current behaviour for layout, and may not be
    able to update in time to avoid regressions for users.
    
    BUG=761016
    
    Change-Id: Id7153a62fe1e3a550a049e4b2a6b9a57e6f7891d
    Reviewed-on: https://chromium-review.googlesource.com/645980
    Commit-Queue: Tobias Sargeant <tobiasjs@chromium.org>
    Reviewed-by: Alexandre Elias <aelias@chromium.org>
    Reviewed-by: Rick Byers <rbyers@chromium.org>
    Reviewed-by: John Abd-El-Malek <jam@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499101}
    Former-commit-id: 416174f07b4d1d930fee687ddd0eae67fbc24ee1

--- content/public/common/web_preferences.cc
+++ content/public/common/web_preferences.cc
@@ -206,6 +206,7 @@ WebPreferences::WebPreferences()
       embedded_media_experience_enabled(false),
       css_hex_alpha_color_enabled(true),
       enable_media_download_in_product_help(false),
+      scroll_top_left_interop_enabled(true),
 #endif  // defined(OS_ANDROID)
 #if defined(OS_ANDROID)
       default_minimum_page_scale_factor(0.25f),
--- third_party/WebKit/public/platform/WebRuntimeFeatures.h
+++ third_party/WebKit/public/platform/WebRuntimeFeatures.h
@@ -78,6 +78,7 @@ class WebRuntimeFeatures {
   BLINK_PLATFORM_EXPORT static void EnableCanvas2dImageChromium(bool);
   BLINK_PLATFORM_EXPORT static void EnableColorCorrectRendering(bool);
   BLINK_PLATFORM_EXPORT static void EnableCSSHexAlphaColor(bool);
+  BLINK_PLATFORM_EXPORT static void EnableScrollTopLeftInterop(bool);
   BLINK_PLATFORM_EXPORT static void EnableDatabase(bool);
   BLINK_PLATFORM_EXPORT static void EnableDecodeToYUV(bool);
   BLINK_PLATFORM_EXPORT static void EnableExperimentalCanvasFeatures(bool);
