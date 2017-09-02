c97be7e8f15bd46307145d52eab4f4d0b1d341a5
hkamila <hkamila@google.com>
2017-09-02 00:07:00 +0000

    Added ThreatDetails to chrome://safe-browsing (AutoRefresh)
    Added ThreatDetails fields in the chrome://safe-browsing page.
    When a new ping is sent in ThreatDetails class, the data will
    automatically be shown on the WebUI tabs that are opened. If no WebUI 
    tabs are opened, no data will be recorded.
    
    Bug: 734667
    Change-Id: I9bd3ec7bbd50e0b515abd9aaa2866b6f2abf29e2
    Reviewed-on: https://chromium-review.googlesource.com/621273
    Reviewed-by: Varun Khaneja <vakh@chromium.org>
    Reviewed-by: Jialiu Lin <jialiul@chromium.org>
    Commit-Queue: Kamila Hasanbega <hkamila@google.com>
    Cr-Commit-Position: refs/heads/master@{#499353}
    Former-commit-id: ce59cd0c97049ab4222c5e47fa07fb560dbe94ac

--- components/safe_browsing/browser/threat_details.cc
+++ components/safe_browsing/browser/threat_details.cc
@@ -20,6 +20,7 @@
 #include "components/safe_browsing/browser/threat_details_cache.h"
 #include "components/safe_browsing/browser/threat_details_history.h"
 #include "components/safe_browsing/common/safebrowsing_messages.h"
+#include "components/safe_browsing/web_ui/safe_browsing_ui.h"
 #include "components/safe_browsing_db/hit_report.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_controller.h"
@@ -742,6 +743,12 @@ void ThreatDetails::OnCacheCollectionReady() {
     DLOG(ERROR) << "Unable to serialize the threat report.";
     return;
   }
+
+  BrowserThread::PostTask(
+      content::BrowserThread::UI, FROM_HERE,
+      base::Bind(&WebUIInfoSingleton::AddToReportsSent,
+                 base::Unretained(WebUIInfoSingleton::GetInstance()),
+                 base::Passed(&report_)));
   ui_manager_->SendSerializedThreatDetails(serialized);
 }
 
--- components/safe_browsing/web_ui/safe_browsing_ui.h
+++ components/safe_browsing/web_ui/safe_browsing_ui.h
@@ -6,6 +6,7 @@
 #define COMPONENTS_SAFE_BROWSING_WEBUI_SAFE_BROWSING_UI_H_
 
 #include "base/macros.h"
+#include "components/safe_browsing/proto/csd.pb.h"
 #include "components/safe_browsing/proto/webui.pb.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_controller.h"
@@ -14,6 +15,8 @@
 
 namespace base {
 class ListValue;
+template <typename T>
+struct DefaultSingletonTraits;
 }
 
 namespace safe_browsing {
@@ -22,14 +25,32 @@ class SafeBrowsingUIHandler : public content::WebUIMessageHandler {
   SafeBrowsingUIHandler(content::BrowserContext*);
   ~SafeBrowsingUIHandler() override;
 
+  // Get the experiments that are currently enabled per Chrome instance.
   void GetExperiments(const base::ListValue* args);
+
+  // Get the Safe Browsing related preferences for the current user.
   void GetPrefs(const base::ListValue* args);
+
+  // Get the information related to the Safe Browsing database and full hash
+  // cache.
   void GetDatabaseManagerInfo(const base::ListValue* args);
 
+  // Get the ThreatDetails that have been collected since the oldest currently
+  // open chrome://safe-browsing tab was opened.
+  void GetSentThreatDetails(const base::ListValue* args);
+
+  // Get the new ThreatDetails messages sent from ThreatDetails when a ping is
+  // sent, while one or more WebUI tabs are opened.
+  void NotifyThreatDetailsJsListener(
+      ClientSafeBrowsingReportRequest* threat_detail);
+
+  // Register callbacks for WebUI messages.
   void RegisterMessages() override;
 
  private:
   content::BrowserContext* browser_context_;
+  // List that keeps all the WebUI listener objects.
+  static std::vector<SafeBrowsingUIHandler*> webui_list_;
   DISALLOW_COPY_AND_ASSIGN(SafeBrowsingUIHandler);
 };
 
@@ -42,6 +63,57 @@ class SafeBrowsingUI : public content::WebUIController {
  private:
   DISALLOW_COPY_AND_ASSIGN(SafeBrowsingUI);
 };
+
+class WebUIInfoSingleton {
+ public:
+  static WebUIInfoSingleton* GetInstance();
+
+  // Delete the list of the sent ClientSafeBrowsingReportRequest messages.
+  void ClearReportsSent();
+
+  // Add the new message in reports_sent_ and send it to all the
+  // chrome://safe-browsing opened tabs.
+  void AddToReportsSent(
+      std::unique_ptr<ClientSafeBrowsingReportRequest> report_request);
+
+  // Register the new WebUI listener object.
+  void RegisterWebUIInstance(SafeBrowsingUIHandler* webui);
+
+  // Unregister the WebUI listener object, and clean the list of reports, if
+  // this is last listener.
+  void UnregisterWebUIInstance(SafeBrowsingUIHandler* webui);
+
+  // Get the list of the sent reports that have been collected since the oldest
+  // currently open chrome://safe-browsing tab was opened.
+  const std::vector<std::unique_ptr<ClientSafeBrowsingReportRequest>>&
+  reports_sent() const {
+    return reports_sent_;
+    ;
+  }
+  // Get the list of WebUI listener objects.
+  const std::vector<SafeBrowsingUIHandler*>& webui_instances() const {
+    return webui_instances_;
+  }
+
+ private:
+  WebUIInfoSingleton();
+  ~WebUIInfoSingleton();
+
+  friend struct base::DefaultSingletonTraits<WebUIInfoSingleton>;
+
+  // List of reports sent since since the oldest currently
+  // open chrome://safe-browsing tab was opened.
+  // "ClientSafeBrowsingReportRequest" cannot be const, due to being used by
+  // functions that call AllowJavascript(), which is not marked const.
+  std::vector<std::unique_ptr<ClientSafeBrowsingReportRequest>> reports_sent_;
+
+  // List of WebUI listener objects. "SafeBrowsingUIHandler*" cannot be const,
+  // due to being used by functions that call AllowJavascript(), which is not
+  // marked const.
+  std::vector<SafeBrowsingUIHandler*> webui_instances_;
+  DISALLOW_COPY_AND_ASSIGN(WebUIInfoSingleton);
+};
+
 }  // namespace safe_browsing
 
 #endif  // COMPONENTS_SAFE_BROWSING_WEBUI_SAFE_BROWSING_UI_H_
