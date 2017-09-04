4a15a1ba328669a77ab939ac5b18c84fed915c0e
Hiroki Nakagawa <nhiroki@chromium.org>
2017-09-04 10:53:24 +0000

    ServiceWorker: Remove dependency on ServiceWorkerProviderHost from ServiceWorkerScriptURLLoader
    Dependency on ServiceWorkerProviderHost makes it difficult to write unittests
    because it requires complicated setup routines. This CL removes the dependency.
    
    Bug: 748415
    Change-Id: I799c2368ac6d1b830e7e6780f189580d287158ea
    Reviewed-on: https://chromium-review.googlesource.com/647415
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499476}
    Former-commit-id: 8b27fba0270d402310586cc7afc5ee0562073483

--- content/browser/service_worker/service_worker_script_url_loader_factory.cc
+++ content/browser/service_worker/service_worker_script_url_loader_factory.cc
@@ -22,7 +22,9 @@ ServiceWorkerScriptURLLoaderFactory::ServiceWorkerScriptURLLoaderFactory(
     scoped_refptr<URLLoaderFactoryGetter> loader_factory_getter)
     : context_(context),
       provider_host_(provider_host),
-      loader_factory_getter_(loader_factory_getter) {}
+      loader_factory_getter_(loader_factory_getter) {
+  DCHECK(provider_host_->IsHostToRunningServiceWorker());
+}
 
 ServiceWorkerScriptURLLoaderFactory::~ServiceWorkerScriptURLLoaderFactory() =
     default;
@@ -50,7 +52,8 @@ void ServiceWorkerScriptURLLoaderFactory::CreateLoaderAndStart(
   mojo::MakeStrongBinding(
       base::MakeUnique<ServiceWorkerScriptURLLoader>(
           routing_id, request_id, options, resource_request, std::move(client),
-          context_, provider_host_, loader_factory_getter_, traffic_annotation),
+          provider_host_->running_hosted_version(), loader_factory_getter_,
+          traffic_annotation),
       std::move(request));
 }
 
