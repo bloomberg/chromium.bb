28e61d7fa57a24a71518663e5ed318d1342f1acf
galinap <galinap@google.com>
2017-09-05 15:17:12 +0000

    [Contextual Suggestions] Add modern UI for Contextual Suggestions
    This CL adds modern look for the contextual suggestions carousel.
    It is using the same card layout xml that the article suggestions have.
    
    Bug: 758190
    Change-Id: I992e01cae4e5d9e9cf04329ee6624d575cb80635
    Reviewed-on: https://chromium-review.googlesource.com/649846
    Commit-Queue: Galia Peycheva <galinap@google.com>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499638}
    Former-commit-id: 9aff08c6f3d7d60816c93b148a938aaf2c7ae935

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/ContextualSuggestionsCardViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/ContextualSuggestionsCardViewHolder.java
@@ -9,9 +9,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ntp.cards.NewTabPageViewHolder;
 import org.chromium.chrome.browser.ntp.snippets.SnippetArticle;
+import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.widget.displaystyle.DisplayStyleObserver;
 import org.chromium.chrome.browser.widget.displaystyle.DisplayStyleObserverAdapter;
 import org.chromium.chrome.browser.widget.displaystyle.HorizontalDisplayStyle;
@@ -33,12 +35,16 @@ public class ContextualSuggestionsCardViewHolder extends NewTabPageViewHolder {
 
     public ContextualSuggestionsCardViewHolder(
             ViewGroup recyclerView, UiConfig uiConfig, SuggestionsUiDelegate uiDelegate) {
-        super(LayoutInflater.from(recyclerView.getContext())
-                        .inflate(R.layout.contextual_suggestions_card, recyclerView, false));
+        super(getCardView(recyclerView));
 
         mUiConfig = uiConfig;
         mUiDelegate = uiDelegate;
         mSuggestionsBinder = new SuggestionsBinder(itemView, uiDelegate);
+
+        int startMargin = itemView.getResources().getDimensionPixelOffset(
+                R.dimen.contextual_carousel_space_between_cards);
+        ApiCompatibilityUtils.setMarginStart(
+                (ViewGroup.MarginLayoutParams) itemView.getLayoutParams(), startMargin);
 
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,6 +66,14 @@ public class ContextualSuggestionsCardViewHolder extends NewTabPageViewHolder {
         mSuggestionsBinder.updateFieldsVisibility(/* showHeadline = */ true,
                 /* showDescription = */ false, /* showThumbnail = */ true,
                 /* showThumbnailVideoOverlay = */ false, /* headerMaxLines = */ 3);
+    }
+
+    private static View getCardView(ViewGroup recyclerView) {
+        int res = FeatureUtilities.isChromeHomeModernEnabled()
+                ? R.layout.content_suggestions_card_modern
+                : R.layout.contextual_suggestions_card;
+
+        return LayoutInflater.from(recyclerView.getContext()).inflate(res, recyclerView, false);
     }
 
     public void onBindViewHolder(SnippetArticle suggestion) {
