d06bd19aee46bdb4fd31f1765566469c8e25d10e
Takashi Toyoshima <toyoshim@chromium.org>
2017-09-05 11:14:24 +0000

    ResourceFetcherImpl: explicitly complete the request on cancellation
    With NetworkService is enabled, OnComplete is not called on resetting
    the URLLoader. This patch changes to set |completed_| even on Cancel(),
    and do nothing if OnComplete() is called after the Cancel() call.
    
    Bug: 760581
    Change-Id: I7250a9741b281edd0a4926146ed4fb8d9aba0d14
    Reviewed-on: https://chromium-review.googlesource.com/645326
    Commit-Queue: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499602}
    Former-commit-id: 5b6a55fadbb408cc4468cded3aa6bacf622aa803

--- content/public/renderer/resource_fetcher.h
+++ content/public/renderer/resource_fetcher.h
@@ -39,8 +39,9 @@ class CONTENT_EXPORT ResourceFetcher {
 
   // This will be called asynchronously after the URL has been fetched,
   // successfully or not.  If there is a failure, response and data will both be
-  // empty.  |response| and |data| are both valid until the URLFetcher instance
-  // is destroyed.
+  // empty.  |response| and |data| are both valid until the ResourceFetcher
+  // instance is destroyed.  The callback will not be called if ResourceFetcher
+  // instance is destroyed before complication or cancellation.
   using Callback =
       base::OnceCallback<void(const blink::WebURLResponse& response,
                               const std::string& data)>;
@@ -59,7 +60,8 @@ class CONTENT_EXPORT ResourceFetcher {
                          const std::string& value) = 0;
 
   // DEPRECATED: Starts the request using the specified frame.  Calls |callback|
-  // when done.
+  // when done.  If ResourceFetcher instance is destructed without completion or
+  // cancellation, |callback| will be disposed without being called.
   virtual void Start(blink::WebLocalFrame* frame,
                      blink::WebURLRequest::RequestContext request_context,
                      Callback callback) = 0;
@@ -78,7 +80,9 @@ class CONTENT_EXPORT ResourceFetcher {
   // timeout.  Must be called after a request is started.
   virtual void SetTimeout(const base::TimeDelta& timeout) = 0;
 
-  // Manually cancel the request.
+  // DEPRECATED: Manually cancel the request.  Calls |callback| with a null
+  // WebURLResponse.  New code should not use this method, but just destruct the
+  // instance to abort.
   virtual void Cancel() = 0;
 };
 
