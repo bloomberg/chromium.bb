e6d2ed5910d1422756b2014e1c0f75edd6b33408
Matthew Jones <mdjones@chromium.org>
2017-09-05 22:58:35 +0000

    [Home] Account for incognito in modern tab title
    Titles in Chrome Modern are not always white, incognito still uses a
    dark theme.
    
    BUG=760740
    
    Change-Id: I82800234101f3755dcb3214c65942ae82e59b3d9
    Reviewed-on: https://chromium-review.googlesource.com/651570
    Reviewed-by: Theresa <twellington@chromium.org>
    Commit-Queue: Matthew Jones <mdjones@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499781}
    Former-commit-id: fba6588489fd94a60be80552b0415548540bf936

--- chrome/android/java/src/org/chromium/chrome/browser/compositor/LayerTitleCache.java
+++ chrome/android/java/src/org/chromium/chrome/browser/compositor/LayerTitleCache.java
@@ -126,16 +126,14 @@ public class LayerTitleCache implements TitleCache {
 
         boolean isDarkTheme = tab.isIncognito();
         // The theme might require lighter text.
-        if (!DeviceFormFactor.isTablet()) {
+        if (!DeviceFormFactor.isTablet() && !FeatureUtilities.isChromeHomeModernEnabled()) {
             isDarkTheme |= ColorUtils.shouldUseLightForegroundOnBackground(tab.getThemeColor());
         }
 
         ColorUtils.shouldUseLightForegroundOnBackground(tab.getThemeColor());
         boolean isRtl = tab.isTitleDirectionRtl();
         TitleBitmapFactory titleBitmapFactory =
-                isDarkTheme && !FeatureUtilities.isChromeHomeModernEnabled()
-                ? mDarkTitleBitmapFactory
-                : mStandardTitleBitmapFactory;
+                isDarkTheme ? mDarkTitleBitmapFactory : mStandardTitleBitmapFactory;
 
         Title title = mTitles.get(tabId);
         if (title == null) {
