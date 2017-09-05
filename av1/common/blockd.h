wjdz99/aom

ncobmc-adapt-weight: update the interpolation kernel type
Change-Id: I862c4b5a22c3f5b3d923cc3feecd194d03b4b892

av1/common/blockd.h
@@ -122,12 +122,7 @@ typedef struct superblock_mi_boundaries {
   int mi_col_end;
 } SB_MI_BD;
 
-typedef struct {
-  int KERNEL_TL[MAX_SB_SIZE][MAX_SB_SIZE];
-  int KERNEL_TR[MAX_SB_SIZE][MAX_SB_SIZE];
-  int KERNEL_BL[MAX_SB_SIZE][MAX_SB_SIZE];
-  int KERNEL_BR[MAX_SB_SIZE][MAX_SB_SIZE];
-} NCOBMC_KERNELS;
+typedef struct { int64_t KERNEL[4][MAX_SB_SIZE][MAX_SB_SIZE]; } NCOBMC_KERNELS;
 #endif
 
 typedef struct {

bloomberg/chromium.bb

av1/common/blockd.h
@@ -118,12 +118,7 @@ typedef struct superblock_mi_boundaries {
   int mi_col_end;
 } SB_MI_BD;
 
-typedef struct {
-  int KERNEL_TL[MAX_SB_SIZE][MAX_SB_SIZE];
-  int KERNEL_TR[MAX_SB_SIZE][MAX_SB_SIZE];
-  int KERNEL_BL[MAX_SB_SIZE][MAX_SB_SIZE];
-  int KERNEL_BR[MAX_SB_SIZE][MAX_SB_SIZE];
-} NCOBMC_KERNELS;
+typedef struct { int16_t KERNEL[4][MAX_SB_SIZE][MAX_SB_SIZE]; } NCOBMC_KERNELS;
 #endif
 
 typedef struct {