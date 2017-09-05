9ea0bf2a7e418551f07e600a5875a014192ede16
Raphael Kubo da Costa <raphael.kubo.da.costa@intel.com>
2017-09-05 13:57:23 +0000

    bindings: Do not throw exceptions on IDL attributes with Promise types.
    Instead, turn them into rejected promises, as specified in
    https://heycam.github.io/webidl/#dfn-attribute-getter. Most of the required
    mechanisms were already in place due to the work on bug 627309 (which is
    essentially the same thing but for IDL operations).
    
    Just like we already do for attributes with the [LenientThis] extended
    attribute, we also disable some V8 type checks when an attribute has a
    Promise type so that we can receive any exceptions and turn them into a
    promise rejection, otherwise attempting to e.g. access
    
        my_interface.prototype.myPromiseAttr
    
    would result in V8 throwing a TypeError due to the wrong receiver being
    specified and we would not be able to turn handle the exception.
    
    Bug: 758023
    Change-Id: I45ad4183f6fe7f571e20ac16c3edacfbc87383d8
    Reviewed-on: https://chromium-review.googlesource.com/649627
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Commit-Queue: Raphael Kubo da Costa (rakuco) <raphael.kubo.da.costa@intel.com>
    Cr-Commit-Position: refs/heads/master@{#499618}
    Former-commit-id: bc08fef84a633ea57aa533cf0ef3d94bf15e263a

--- third_party/WebKit/Source/bindings/core/v8/custom/V8PromiseRejectionEventCustom.cpp
+++ third_party/WebKit/Source/bindings/core/v8/custom/V8PromiseRejectionEventCustom.cpp
@@ -4,6 +4,8 @@
 
 #include "bindings/core/v8/V8PromiseRejectionEvent.h"
 
+#include "bindings/core/v8/ExceptionState.h"
+#include "bindings/core/v8/GeneratedCodeHelper.h"
 #include "bindings/core/v8/ScriptPromise.h"
 #include "bindings/core/v8/ScriptValue.h"
 #include "bindings/core/v8/V8BindingForCore.h"
@@ -15,7 +17,22 @@ namespace blink {
 void V8PromiseRejectionEvent::promiseAttributeGetterCustom(
     const v8::FunctionCallbackInfo<v8::Value>& info) {
   v8::Isolate* isolate = info.GetIsolate();
-  PromiseRejectionEvent* event = V8PromiseRejectionEvent::toImpl(info.Holder());
+
+  // This attribute returns a Promise.
+  // Per https://heycam.github.io/webidl/#dfn-attribute-getter, all exceptions
+  // must be turned into a Promise rejection. Returning a Promise type requires
+  // us to disable some of V8's type checks, so we have to manually check that
+  // info.Holder() really points to an instance of the type.
+  PromiseRejectionEvent* event =
+      V8PromiseRejectionEvent::toImplWithTypeCheck(isolate, info.Holder());
+  if (!event) {
+    ExceptionState exception_state(isolate, ExceptionState::kGetterContext,
+                                   "PromiseRejectionEvent", "promise");
+    ExceptionToRejectPromiseScope rejectPromiseScope(info, exception_state);
+    exception_state.ThrowTypeError("Illegal invocation");
+    return;
+  }
+
   ScriptPromise promise = event->promise(ScriptState::Current(isolate));
   if (promise.IsEmpty()) {
     V8SetReturnValue(info, v8::Null(isolate));
