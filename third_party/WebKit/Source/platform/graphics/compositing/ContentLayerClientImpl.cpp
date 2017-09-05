486efadb5c2d412f27059d78864ca76a28299ad7
Jayson Adams <shrike@chromium.org>
2017-09-05 22:22:06 +0000

    Revert "Fix layout test repaint rect overlay and layer tree dump"
    This reverts commit 6056ce0678c34d34348633e6fcac1416efb4f3cd [formerly d27291cb099155a2d203968dff2455d13e244992].
    
    Reason for revert: Test failures on macOS 10.9 and 10.11 in virtual/mojo-loading/http/tests/inspector/application-panel/resources-panel-resource-preview.html
    
    https://uberchromegw.corp.google.com/i/chromium.webkit/builders/WebKit%20Mac10.9/builds/49620
    
    https://uberchromegw.corp.google.com/i/chromium.webkit/builders/WebKit%20Mac10.11/builds/23223
    
    Original change's description:
    > Fix layout test repaint rect overlay and layer tree dump
    > 
    > The new code in repaint_overlay.py now correctly handles transforms
    > (but not 3d transforms yet). Also add tests.
    > 
    > To make the new repaint overlay code work, we must also output the
    > following as transforms for both SPv1 and SPv2:
    > - scroll translation. For SPv1, we output GraphicsLayer::scrollable_area_
    >   ->ScrollPosition() just like SPv2's scroll translation.
    > - paint offset translation: For SPv1, before we output a transform of a
    >   layer, we output the accumulated offset from the ancestor transform
    >   just like SPv2's paint offset translation.
    > The above changes also make many test produce the same result for SPv1
    > and SPv2, enabling us to remove 60+ SPv2 baselines.
    > 
    > Bug: 755409
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    > Change-Id: I5babc49403053215d2b4381439b5223c0a1573df
    > Reviewed-on: https://chromium-review.googlesource.com/648172
    > Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    > Reviewed-by: Philip Rogers <pdr@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#499737}
    
    TBR=wangxianzhu@chromium.org,pdr@chromium.org
    
    Change-Id: I0c13475ac4255051c939937c7006171c2c90e2de
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 755409
    Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_slimming_paint_v2
    Reviewed-on: https://chromium-review.googlesource.com/651129
    Reviewed-by: Jayson Adams <shrike@chromium.org>
    Commit-Queue: Jayson Adams <shrike@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499769}
    Former-commit-id: 04729fc97738e30717d29862b8cfbac19aac882e

--- third_party/WebKit/Source/platform/graphics/compositing/ContentLayerClientImpl.cpp
+++ third_party/WebKit/Source/platform/graphics/compositing/ContentLayerClientImpl.cpp
@@ -47,16 +47,16 @@ static int GetTransformId(const TransformPaintPropertyNode* transform,
   if (it != context.transform_id_map.end())
     return it->value;
 
-  int parent_id = GetTransformId(transform->Parent(), context);
-  if (transform->Matrix().IsIdentity() && !transform->RenderingContextId()) {
-    context.transform_id_map.Set(transform, parent_id);
-    return parent_id;
+  if ((transform->Matrix().IsIdentity() && !transform->RenderingContextId()) ||
+      // Don't output scroll translations in transform tree.
+      transform->ScrollNode()) {
+    context.transform_id_map.Set(transform, 0);
+    return 0;
   }
 
-  int transform_id = context.next_transform_id++;
-  context.transform_id_map.Set(transform, transform_id);
-
+  int parent_id = GetTransformId(transform->Parent(), context);
   auto json = JSONObject::Create();
+  int transform_id = context.next_transform_id++;
   json->SetInteger("id", transform_id);
   if (parent_id)
     json->SetInteger("parent", parent_id);
