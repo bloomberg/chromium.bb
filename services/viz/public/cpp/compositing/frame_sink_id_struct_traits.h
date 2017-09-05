8232573bebabc7aa130021c168acb5a4dc6ed75b
Gary Klassen <gklassen@chromium.org>
2017-09-05 22:46:27 +0000

    viz: move FrameSinkId mojom from cc to viz
    Part of viz restructuring.
    
    Bug: 722935
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I185492edf465a84189d25cda6d4da3cc8a851b4a
    Reviewed-on: https://chromium-review.googlesource.com/647829
    Commit-Queue: Gary Klassen <gklassen@chromium.org>
    Reviewed-by: Saman Sami <samans@chromium.org>
    Reviewed-by: Fady Samuel <fsamuel@chromium.org>
    Reviewed-by: weiliangc <weiliangc@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499774}
    Former-commit-id: aa88d3b4fdfb1faf24c1e9a1406b7033a92a6951

--- services/viz/public/cpp/compositing/frame_sink_id_struct_traits.h
+++ services/viz/public/cpp/compositing/frame_sink_id_struct_traits.h
@@ -2,16 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CC_IPC_FRAME_SINK_ID_STRUCT_TRAITS_H_
-#define CC_IPC_FRAME_SINK_ID_STRUCT_TRAITS_H_
+#ifndef SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_FRAME_SINK_ID_STRUCT_TRAITS_H_
+#define SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_FRAME_SINK_ID_STRUCT_TRAITS_H_
 
-#include "cc/ipc/frame_sink_id.mojom-shared.h"
 #include "components/viz/common/surfaces/frame_sink_id.h"
+#include "services/viz/public/interfaces/compositing/frame_sink_id.mojom-shared.h"
 
 namespace mojo {
 
 template <>
-struct StructTraits<cc::mojom::FrameSinkIdDataView, viz::FrameSinkId> {
+struct StructTraits<viz::mojom::FrameSinkIdDataView, viz::FrameSinkId> {
   static uint32_t client_id(const viz::FrameSinkId& frame_sink_id) {
     return frame_sink_id.client_id();
   }
@@ -20,7 +20,8 @@ struct StructTraits<cc::mojom::FrameSinkIdDataView, viz::FrameSinkId> {
     return frame_sink_id.sink_id();
   }
 
-  static bool Read(cc::mojom::FrameSinkIdDataView data, viz::FrameSinkId* out) {
+  static bool Read(viz::mojom::FrameSinkIdDataView data,
+                   viz::FrameSinkId* out) {
     *out = viz::FrameSinkId(data.client_id(), data.sink_id());
     return true;
   }
@@ -28,4 +29,4 @@ struct StructTraits<cc::mojom::FrameSinkIdDataView, viz::FrameSinkId> {
 
 }  // namespace mojo
 
-#endif  // CC_IPC_FRAME_SINK_ID_STRUCT_TRAITS_H_
+#endif  // SERVICES_VIZ_PUBLIC_CPP_COMPOSITING_FRAME_SINK_ID_STRUCT_TRAITS_H_
