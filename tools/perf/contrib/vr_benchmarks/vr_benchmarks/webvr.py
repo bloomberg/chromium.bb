66cfc8ab3c9fbfb35423936c6855f714809efa3a
Lei Lei <leilei@chromium.org>
2017-09-05 23:46:37 +0000

    Update vr_memory benchmark to make it as a generic benchmark for webvr.
    + Rename it to webvr.sample_pages
    + Add webvr fps and frame time metrics.
    
    Bug: 726906
    Change-Id: I1ea7acc1410fc22fc06eae553382873a41148512
    Reviewed-on: https://chromium-review.googlesource.com/642072
    Commit-Queue: Lei Lei <leilei@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#499802}
    Former-commit-id: 4d6ae07f4c3343cef070dbdec7f9c35771099a6c

--- tools/perf/contrib/vr_benchmarks/vr_benchmarks/webvr.py
+++ tools/perf/contrib/vr_benchmarks/vr_benchmarks/webvr.py
@@ -1,15 +1,19 @@
 # Copyright 2017 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
 from benchmarks import memory
 from core import perf_benchmark
 from telemetry import benchmark
+from telemetry.timeline import chrome_trace_category_filter
+from telemetry.timeline import chrome_trace_config
+from telemetry.web_perf import timeline_based_measurement
 from contrib.vr_benchmarks.vr_page_sets import webvr_sample_pages
 
 
 @benchmark.Owner(emails=['bsheedy@chromium.org', 'leilei@chromium.org'])
-class WebVrMemorySamplePages(perf_benchmark.PerfBenchmark):
-  """Measures WebVR memory on an official sample page with settings tweaked."""
+class XrWebVrStatic(perf_benchmark.PerfBenchmark):
+  """Measures WebVR performance with sample pages."""
 
   @classmethod
   def AddBenchmarkCommandLineArgs(cls, parser):
@@ -21,7 +25,18 @@ class WebVrMemorySamplePages(perf_benchmark.PerfBenchmark):
                            'shared_preference_files/test/')
 
   def CreateCoreTimelineBasedMeasurementOptions(self):
-    return memory.CreateCoreTimelineBasedMemoryMeasurementOptions()
+    memory_categories = ['blink.console', 'disabled-by-default-memory-infra']
+    gpu_categories = ['gpu']
+    category_filter = chrome_trace_category_filter.ChromeTraceCategoryFilter(
+        ','.join(['-*'] + memory_categories + gpu_categories))
+    options = timeline_based_measurement.Options(category_filter)
+    options.config.enable_android_graphics_memtrack = True
+    options.config.enable_platform_display_trace = True
+
+    options.SetTimelineBasedMetrics(['memoryMetric', 'webvrMetric'])
+    options.config.chrome_trace_config.SetMemoryDumpConfig(
+        chrome_trace_config.MemoryDumpConfig())
+    return options
 
   def CreateStorySet(self, options):
     return webvr_sample_pages.WebVrSamplePageSet()
@@ -32,7 +47,7 @@ class WebVrMemorySamplePages(perf_benchmark.PerfBenchmark):
 
   @classmethod
   def Name(cls):
-    return 'vr_memory.webvr_sample_pages'
+    return 'xr.webvr.static'
 
   @classmethod
   def ValueCanBeAddedPredicate(cls, value, is_first_result):
