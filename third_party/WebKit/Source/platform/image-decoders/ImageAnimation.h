45d99dd3eb2215e6204a6bb3ffeae6398c4599e6
Khushal <khushalsagar@chromium.org>
2017-09-05 23:33:23 +0000

    cc: Move repetition count constants to cc/paint.
    To prepare cc for animating images, add repetition count to PaintImage
    that determines the number of loops repeated for the image and move the
    shared constants to cc/paint/.
    
    Bug: 735741
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Change-Id: I101ab0eae52bede9c32192f780a8e14ef307a004
    Reviewed-on: https://chromium-review.googlesource.com/650532
    Reviewed-by: Chris Harrelson <chrishtr@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#499797}
    Former-commit-id: 2f6e908ccdb7e929aef3e3b29b91bd85003f8dab

--- third_party/WebKit/Source/platform/image-decoders/ImageAnimation.h
+++ third_party/WebKit/Source/platform/image-decoders/ImageAnimation.h
@@ -26,26 +26,12 @@
 #ifndef ImageAnimation_h
 #define ImageAnimation_h
 
+#include "cc/paint/image_animation_count.h"
+
 namespace blink {
-
-// GIF and WebP support animation. The explanation below is in terms of GIF,
-// but the same constants are used for WebP, too.
-// GIFs have an optional 16-bit unsigned loop count that describes how an
-// animated GIF should be cycled.  If the loop count is absent, the animation
-// cycles once; if it is 0, the animation cycles infinitely; otherwise the
-// animation plays n + 1 cycles (where n is the specified loop count).  If the
-// GIF decoder defaults to kAnimationLoopOnce in the absence of any loop count
-// and translates an explicit "0" loop count to kAnimationLoopInfinite, then we
-// get a couple of nice side effects:
-//   * By making kAnimationLoopOnce be 0, we allow the animation cycling code in
-//     BitmapImage.cpp to avoid special-casing it, and simply treat all
-//     non-negative loop counts identically.
-//   * By making the other two constants negative, we avoid conflicts with any
-//     real loop count values.
-const int kAnimationLoopOnce = 0;
-const int kAnimationLoopInfinite = -1;
-const int kAnimationNone = -2;
-
+using cc::kAnimationLoopOnce;
+using cc::kAnimationLoopInfinite;
+using cc::kAnimationNone;
 }  // namespace blink
 
 #endif
