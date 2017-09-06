7f14f3d06d0ce583928d6011225260282eee9bcc
erikchen <erikchen@chromium.org>
2017-09-06 19:12:17 +0000

    Remove unused code from memlog_stream.h
    There was some Windows specific code for passing around pipes that is no longer
    necessary now that out of process heap profiling is layered on Mojo and
    services.
    
    Bug: 
    Change-Id: Id69ef914ca70eb7abd89a054af8045f8e42f934b
    Reviewed-on: https://chromium-review.googlesource.com/653298
    Reviewed-by: Brett Wilson <brettw@chromium.org>
    Commit-Queue: Erik Chen <erikchen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500032}
    Former-commit-id: 1ee170b59ffd761aace2526f7112046acdca99c4

--- chrome/common/profiling/memlog_stream.h
+++ chrome/common/profiling/memlog_stream.h
@@ -41,12 +41,6 @@ struct FreePacket {
 };
 #pragma pack(pop)
 
-#if defined(OS_WIN)
-// Prefix for pipe name for communicating between chrome processes and the
-// memlog process. The pipe ID is appended to this to get the pipe name.
-extern const wchar_t kWindowsPipePrefix[];
-#endif  // OS_WIN
-
 }  // namespace profiling
 
 #endif  // CHROME_COMMON_PROFILING_MEMLOG_STREAM_H_
