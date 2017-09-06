e1dd5af3e27cc281032fa6c5e22861fd12049fe1
Regan Hsu <hsuregan@google.com>
2017-09-06 20:12:04 +0000

    [CrOS MultiDevice] Skeleton for DeviceSync API.
    The DeviceSync API syncs metadata about other devices tied to a given Google account.
    It contacts the existing CryptAuth back-end to enroll the current device
    and sync down new data about other devices.
    
    Bug: 752273
    Change-Id: Iee8859dfdb54ba45a0d206784075b3d68d929e8b
    Reviewed-on: https://chromium-review.googlesource.com/616223
    Commit-Queue: Regan Hsu <hsuregan@google.com>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Kyle Horimoto <khorimoto@chromium.org>
    Reviewed-by: Tim Song <tengs@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500058}
    Former-commit-id: a466fc4ffc41c2a2903ba608b9c83a3fd88f9e86

---
+++ components/multidevice/service/device_sync_impl.h
@@ -0,0 +1,42 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef COMPONENTS_MULTIDEVICE_DEVICE_SYNC_IMPL_H_
+#define COMPONENTS_MULTIDEVICE_DEVICE_SYNC_IMPL_H_
+
+#include <memory>
+#include <string>
+
+#include "base/macros.h"
+#include "components/multidevice/service/public/interfaces/device_sync.mojom.h"
+#include "mojo/public/cpp/bindings/interface_ptr_set.h"
+#include "services/service_manager/public/cpp/service_context_ref.h"
+
+namespace multidevice {
+
+// This class syncs metadata about other devices tied to a given Google account.
+// It contacts the back-end to enroll the current device
+// and sync down new data about other devices.
+class DeviceSyncImpl : public device_sync::mojom::DeviceSync {
+ public:
+  explicit DeviceSyncImpl(
+      std::unique_ptr<service_manager::ServiceContextRef> service_ref);
+
+  ~DeviceSyncImpl() override;
+
+  // mojom::DeviceSync:
+  void ForceEnrollmentNow() override;
+  void ForceSyncNow() override;
+  void AddObserver(device_sync::mojom::DeviceSyncObserverPtr observer) override;
+
+ private:
+  const std::unique_ptr<service_manager::ServiceContextRef> service_ref_;
+  mojo::InterfacePtrSet<device_sync::mojom::DeviceSyncObserver> observers_;
+
+  DISALLOW_COPY_AND_ASSIGN(DeviceSyncImpl);
+};
+
+}  // namespace multidevice
+
+#endif  // COMPONENTS_MULTIDEVICE_DEVICE_SYNC_IMPL_H_
