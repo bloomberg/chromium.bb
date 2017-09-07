f62d3902917abdce2a31c038a6770eee28a79288
Xianzhu Wang <wangxianzhu@chromium.org>
2017-09-07 00:51:17 +0000

    Remove DCHECK in LayoutTableBoxComponent...UpdatePaintResult()
    The DCHECK was to ensure the method is called by painters. However,
    for now we may paint outside of InPaint phase, e.g. when printing
    or running rasterize_and_record_micro benchmark.
    
    Remove the DCHECK. As the parameters are only available from painters,
    we can already ensure calling from painters. We also don't have such
    check for other MutableForPainting methods. In the future we can
    consider checks in MutableForPainting constructor after we ensure
    all paintings happen in InPaint phase.
    
    Bug: 761434
    Change-Id: I8d6e2c1352fdc407832a4654cac313c31249700a
    Reviewed-on: https://chromium-review.googlesource.com/653361
    Reviewed-by: Walter Korman <wkorman@chromium.org>
    Commit-Queue: Xianzhu Wang <wangxianzhu@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500163}
    Former-commit-id: 3fc3836447cc95d80ceb036c9a4d0999ee32e6a0

--- third_party/WebKit/Source/core/layout/LayoutTableBoxComponent.cpp
+++ third_party/WebKit/Source/core/layout/LayoutTableBoxComponent.cpp
@@ -42,9 +42,6 @@ bool LayoutTableBoxComponent::DoCellsHaveDirtyWidth(
 void LayoutTableBoxComponent::MutableForPainting::UpdatePaintResult(
     PaintResult paint_result,
     const CullRect& paint_rect) {
-  DCHECK_EQ(layout_object_.GetDocument().Lifecycle().GetState(),
-            DocumentLifecycle::LifecycleState::kInPaint);
-
   // A table row or section may paint large background display item which
   // contains paint operations of the background in each contained cell.
   // The display item can be clipped by the paint rect to avoid painting
