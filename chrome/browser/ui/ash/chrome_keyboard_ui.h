a8a3baa68f9ed057fa443724cd570316a64dd5bc
Getulio Sánchez <valentin2507@gmail.com>
2017-09-07 01:54:16 +0000

    [Chrome] Refactoring: Remove KeyboardUIContent
    ChromeKeyboardUI is the only inheritor of KeyboardUIContent except for tests;
    inheritance graph is KeyboardUI > KeyboardUIContent > ChromeKeyboardUI.
    
    This patch consolidates KeyboardUIContent into ChromeKeyboardUI.
    
    R=oka@chromium.org, sky@chromium.org
    
    Bug: 739083
    Change-Id: I9a83dbd69f4b2bdcbe26a0db8c7e7ee11a9d4fb9
    Reviewed-on: https://chromium-review.googlesource.com/563916
    Commit-Queue: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Mitsuru Oshima <oshima@chromium.org>
    Reviewed-by: Yuichiro Hanada <yhanada@chromium.org>
    Reviewed-by: Keigo Oka <oka@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500180}
    Former-commit-id: fa4328bed19d775652065274b3056cc00e527bc8

--- chrome/browser/ui/ash/chrome_keyboard_ui.h
+++ chrome/browser/ui/ash/chrome_keyboard_ui.h
@@ -9,46 +9,133 @@
 
 #include "base/macros.h"
 #include "content/public/browser/web_contents_observer.h"
-#include "ui/keyboard/content/keyboard_ui_content.h"
+#include "content/public/common/media_stream_request.h"
+#include "ui/aura/window_observer.h"
+#include "ui/base/ime/text_input_type.h"
+#include "ui/keyboard/keyboard_controller_observer.h"
+#include "ui/keyboard/keyboard_export.h"
+#include "ui/keyboard/keyboard_ui.h"
 
+namespace {
+class WindowBoundsChangeObserver;
+}
+namespace aura {
+class Window;
+}
+namespace gfx {
+class Rect;
+}
+namespace wm {
+class Shadow;
+}
 namespace content {
 class BrowserContext;
 class WebContents;
-}
-namespace keyboard {
-class KeyboardController;
-class KeyboardControllerObserver;
 }
 namespace ui {
 class InputMethod;
 }
 
-// Subclass of KeyboardUIContent. It is used by KeyboardController to get
+// Subclass of KeyboardUI. It is used by KeyboardController to get
 // access to the virtual keyboard window and setup Chrome extension functions.
-class ChromeKeyboardUI : public keyboard::KeyboardUIContent,
+class ChromeKeyboardUI : public keyboard::KeyboardUI,
+                         public aura::WindowObserver,
                          public content::WebContentsObserver {
  public:
+  class TestApi {
+   public:
+    explicit TestApi(ChromeKeyboardUI* ui) : ui_(ui) {}
+
+    const content::WebContents* keyboard_contents() const {
+      return ui_->keyboard_contents_.get();
+    }
+
+   private:
+    ChromeKeyboardUI* ui_;
+
+    DISALLOW_COPY_AND_ASSIGN(TestApi);
+  };
+
   explicit ChromeKeyboardUI(content::BrowserContext* context);
   ~ChromeKeyboardUI() override;
 
-  // keyboard::KeyboardUIContent overrides
+  // Requests the audio input from microphone for speech input.
+  void RequestAudioInput(content::WebContents* web_contents,
+                         const content::MediaStreamRequest& request,
+                         const content::MediaResponseCallback& callback);
+
+  // Called when a window being observed changes bounds, to update its insets.
+  void UpdateInsetsForWindow(aura::Window* window);
+
+  // Overridden from KeyboardUI:
+  aura::Window* GetContentsWindow() override;
+  bool HasContentsWindow() const override;
   bool ShouldWindowOverscroll(aura::Window* window) const override;
+  void ReloadKeyboardIfNeeded() override;
+  void InitInsets(const gfx::Rect& new_bounds) override;
+  void ResetInsets() override;
+
+ protected:
+  // aura::WindowObserver overrides:
+  void OnWindowBoundsChanged(aura::Window* window,
+                             const gfx::Rect& old_bounds,
+                             const gfx::Rect& new_bounds) override;
+  void OnWindowDestroyed(aura::Window* window) override;
+  void OnWindowParentChanged(aura::Window* window,
+                             aura::Window* parent) override;
+
+  content::BrowserContext* browser_context() { return browser_context_; }
+
+  const aura::Window* GetKeyboardRootWindow() const;
+
+  virtual content::WebContents* CreateWebContents();
 
  private:
-  // keyboard::KeyboardUIContent overrides
+  friend class TestApi;
+
+  // Loads the web contents for the given |url|.
+  void LoadContents(const GURL& url);
+
+  // Gets the virtual keyboard URL (either the default URL or IME override URL).
+  const GURL& GetVirtualKeyboardUrl();
+
+  // Determines whether a particular window should have insets for overscroll.
+  bool ShouldEnableInsets(aura::Window* window);
+
+  // Adds an observer for tracking changes to a window size or
+  // position while the keyboard is displayed. Any window repositioning
+  // invalidates insets for overscrolling.
+  void AddBoundsChangedObserver(aura::Window* window);
+
+  // Sets shadow around the keyboard. If shadow has not been created yet,
+  // this method creates it.
+  void SetShadowAroundKeyboard();
+
+  // The implementation can choose to setup the WebContents before the virtual
+  // keyboard page is loaded (e.g. install a WebContentsObserver).
+  // SetupWebContents() is called right after creating the WebContents, before
+  // loading the keyboard page.
+  void SetupWebContents(content::WebContents* contents);
+
+  // Overridden from KeyboardUI:
   ui::InputMethod* GetInputMethod() override;
-  void RequestAudioInput(
-      content::WebContents* web_contents,
-      const content::MediaStreamRequest& request,
-      const content::MediaResponseCallback& callback) override;
-  void SetupWebContents(content::WebContents* contents) override;
   void SetController(keyboard::KeyboardController* controller) override;
   void ShowKeyboardContainer(aura::Window* container) override;
 
   // content::WebContentsObserver overrides
   void RenderViewCreated(content::RenderViewHost* render_view_host) override;
 
+  // The BrowserContext to use for creating the WebContents hosting the
+  // keyboard.
+  content::BrowserContext* browser_context_;
+
+  const GURL default_url_;
+
+  std::unique_ptr<content::WebContents> keyboard_contents_;
+  std::unique_ptr<wm::Shadow> shadow_;
+
   std::unique_ptr<keyboard::KeyboardControllerObserver> observer_;
+  std::unique_ptr<WindowBoundsChangeObserver> window_bounds_observer_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeKeyboardUI);
 };
