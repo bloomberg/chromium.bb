bed9cb7bc573f8f213555d36bf44712f42a28c1e
Christopher Lam <calamity@chromium.org>
2017-09-07 06:37:15 +0000

    [MD Bookmarks] Scroll bookmark list and sidebar on drag hover.
    This CL makes internal drags in the bookmark manager scroll the sidebar
    and bookmark list when the drag is hovered over the bottom or top of the
    scrollable sections.
    
    Bug: 748944
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I699b0dff3f410ae9fc856e0bc476f08837854d60
    Reviewed-on: https://chromium-review.googlesource.com/647393
    Reviewed-by: Tim Sergeant <tsergeant@chromium.org>
    Commit-Queue: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500236}
    Former-commit-id: 19c32887d6bb6f7a93a89e76f1944c6a18afcc91

--- chrome/browser/resources/md_bookmarks/dnd_manager.js
+++ chrome/browser/resources/md_bookmarks/dnd_manager.js
@@ -213,6 +213,56 @@ cr.define('bookmarks', function() {
   };
 
   /**
+   * Manages auto scrolling of elements on hover during internal drags. Native
+   * drags do this by themselves.
+   * @constructor
+   */
+  function AutoScroller() {
+    /** @const {number} */
+    this.SCROLL_ZONE_LENGTH = 20;
+    /** @const {number} */
+    this.SCROLL_DISTANCE = 10;
+    /** @const {number} */
+    this.SCROLL_INTERVAL = 100;
+    /** @private {?number} */
+    this.intervalId_ = null;
+  }
+
+  AutoScroller.prototype = {
+    /** @param {!Event} e */
+    update: function(e) {
+      this.reset();
+
+      var scrollParent = e.path.find((el) => {
+        return el.nodeType == Node.ELEMENT_NODE &&
+            window.getComputedStyle(el).overflowY == 'auto';
+      });
+
+      if (!scrollParent)
+        return;
+
+      var rect = scrollParent.getBoundingClientRect();
+      var yDelta = 0;
+      if (e.clientY < rect.top + this.SCROLL_ZONE_LENGTH)
+        yDelta = -this.SCROLL_DISTANCE;
+      else if (e.clientY > rect.bottom - this.SCROLL_ZONE_LENGTH)
+        yDelta = this.SCROLL_DISTANCE;
+
+      this.intervalId_ = window.setInterval(() => {
+        scrollParent.scrollTop += yDelta;
+      }, this.SCROLL_INTERVAL);
+    },
+
+    reset: function() {
+      if (this.intervalId_ == null)
+        return;
+
+      window.clearInterval(this.intervalId_);
+      this.intervalId_ = null;
+    },
+  };
+
+  /**
    * Encapsulates the behavior of the drag and drop indicator which puts a line
    * between items or highlights folders which are valid drop targets.
    * @constructor
@@ -335,6 +385,12 @@ cr.define('bookmarks', function() {
     /** @private {Object<string, function(!Event)>} */
     this.documentListeners_ = null;
 
+    /** @private {?bookmarks.AutoScroller} */
+    this.autoScroller_ = null;
+
+    /** @private {?bookmarks.AutoExpander} */
+    this.autoExpander_ = null;
+
     /**
      * Used to instantly clearDragData in tests.
      * @private {bookmarks.TimerProxy}
@@ -366,6 +422,7 @@ cr.define('bookmarks', function() {
       this.dragInfo_ = new DragInfo();
       this.dropIndicator_ = new DropIndicator();
       this.autoExpander_ = new AutoExpander();
+      this.autoScroller_ = new AutoScroller();
 
       this.documentListeners_ = {
         'mousedown': this.onMousedown_.bind(this),
@@ -437,6 +494,8 @@ cr.define('bookmarks', function() {
 
       // Prevents a native drag from starting.
       e.preventDefault();
+
+      this.autoScroller_.update(e);
 
       // On the first mousemove after a mousedown, calculate the items to drag.
       // This can't be done in mousedown because the user may be shift-clicking
@@ -626,6 +685,7 @@ cr.define('bookmarks', function() {
     /** @private */
     clearDragData_: function() {
       this.dndChip.hide();
+      this.autoScroller_.reset();
       this.internalDragElement_ = null;
       this.mouseDownPos_ = null;
       this.autoExpander_.reset();
@@ -941,6 +1001,8 @@ cr.define('bookmarks', function() {
   };
 
   return {
+    AutoExpander: AutoExpander,
+    AutoScroller: AutoScroller,
     DNDManager: DNDManager,
     DragInfo: DragInfo,
     DropIndicator: DropIndicator,
