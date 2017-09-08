24b0d50593634fe500a97c274673891ab89fa421
Alexander Shalamov <alexander.shalamov@intel.com>
2017-09-08 09:41:58 +0000

    [sensors][android] Avoid updating sensor readings after destruction
    This CL adds guard for the cases when C++ JNI wrapper is destructed
    and Java object is not yet GC'd, therefore, might try invoking native
    callbacks for destructed object.
    
    Bug: 762015
    Change-Id: I45ac23b151e2f9cfeacb93abc5e588e9b8e94ffb
    Reviewed-on: https://chromium-review.googlesource.com/652470
    Commit-Queue: Alexander Shalamov <alexander.shalamov@intel.com>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Reviewed-by: Jun Cai <juncai@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500549}
    Former-commit-id: 53ce124b873f1e0b32e7fca9074ce005d2f62998

--- services/device/generic_sensor/platform_sensor_android.cc
+++ services/device/generic_sensor/platform_sensor_android.cc
@@ -26,7 +26,8 @@ PlatformSensorAndroid::PlatformSensorAndroid(
 }
 
 PlatformSensorAndroid::~PlatformSensorAndroid() {
-  StopSensor();
+  JNIEnv* env = AttachCurrentThread();
+  Java_PlatformSensor_sensorDestroyed(env, j_object_);
 }
 
 mojom::ReportingMode PlatformSensorAndroid::GetReportingMode() {
