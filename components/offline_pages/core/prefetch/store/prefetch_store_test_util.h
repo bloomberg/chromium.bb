a2b71529be5b7016bcf5f20e804a5f3ff35cfcc6
Jian Li <jianli@chromium.org>
2017-09-08 03:39:17 +0000

    Misc fixes to prefetch download
    This is reland of https://chromium-review.googlesource.com/647491
    with the fix for casting error.
    
    1) Create DownloadArchivesTask only when download service is ready.
    This fix removes the queuing logic in PrefetchDownloadImpl.
    2) Delay creating and running DownloadCleanupTask until background
    task has started.
    3) Remove a couple unused methods from PrefetchDownloader.
    4) Add a new test to cover the interaction between prefetch service
    and download service.
    
    TBR=dtrainor@chromium.org
    
    Bug: 701939
    Change-Id: Ifd2ad6408ca3a34cf9aebabc3262aad1c2d0bf2f
    Reviewed-on: https://chromium-review.googlesource.com/656777
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Dmitry Titov <dimich@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500499}
    Former-commit-id: 42b0c4e16ba7a2435f4b33303af9f2f31d0bfb75

--- components/offline_pages/core/prefetch/store/prefetch_store_test_util.h
+++ components/offline_pages/core/prefetch/store/prefetch_store_test_util.h
@@ -79,7 +79,7 @@ class PrefetchStoreTestUtil {
   // Causes the store to behave as if an initialization error occurred.
   void SimulateInitializationError();
 
-  PrefetchStore* store() { return store_.get(); }
+  PrefetchStore* store() { return store_; }
 
   base::SimpleTestClock* clock() { return &clock_; }
 
@@ -88,7 +88,9 @@ class PrefetchStoreTestUtil {
 
   scoped_refptr<base::TestSimpleTaskRunner> task_runner_;
   base::ScopedTempDir temp_directory_;
-  std::unique_ptr<PrefetchStore> store_;
+  // TODO(jianli): Refactor this class to avoid owning the store.
+  std::unique_ptr<PrefetchStore> owned_store_;
+  PrefetchStore* store_;
   base::SimpleTestClock clock_;
 
   DISALLOW_COPY_AND_ASSIGN(PrefetchStoreTestUtil);
