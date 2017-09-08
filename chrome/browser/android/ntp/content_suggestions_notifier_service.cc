35ad37bb5a480378eb193da571d3dbd6709d4d5f
Chris Pickel <sfiera@chromium.org>
2017-09-08 10:47:24 +0000

    Record histogram on notifications opt-out
    This metric is used only pre-O. On O, we need to look for notifications
    being blocked at the system level instead.
    
    Bug: 761359
    Change-Id: Ia7f18a5abfc1de5b3327b5521f6821f057f3e32d
    Reviewed-on: https://chromium-review.googlesource.com/649106
    Commit-Queue: Chris Pickel <sfiera@chromium.org>
    Reviewed-by: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500560}
    Former-commit-id: cf340a2eb631728a4ce6cbb11c060e8a72e1ae34

--- chrome/browser/android/ntp/content_suggestions_notifier_service.cc
+++ chrome/browser/android/ntp/content_suggestions_notifier_service.cc
@@ -299,6 +299,7 @@ void ContentSuggestionsNotifierService::SetEnabled(bool enabled) {
     Enable();
   } else {
     Disable();
+    RecordContentSuggestionsNotificationOptOut(CONTENT_SUGGESTIONS_EXPLICIT);
   }
 }
 
