d119f5e2246da31854a53db17f2f71261edea623
Matt Giuca <mgiuca@chromium.org>
2017-09-08 05:05:40 +0000

    Revert "BlinkGC: Cleanup heap counters"
    This reverts commit f0e6578feff31715672ea3914f32ca413c8bff69 [formerly 5b47e6de1a5ac9b1b2f14a0f6241eaebe778d8f9].
    
    Reason for revert: Speculative revert for breaking 2 WebKit tests.
    
    Very unsure but it's the only CL that looks relevant in the range.
    
    Original change's description:
    > BlinkGC: Cleanup heap counters
    > 
    > - Remove ThreadState::allocated_object_size_ and marked_object_size_ because they are the same as the values in ThreadHeapStats.
    > - Remove atomics from ThreadHeapStats
    > 
    > Bug: 
    > Change-Id: I378a81e5087cd8268853ac4b86206e39db932397
    > Reviewed-on: https://chromium-review.googlesource.com/654362
    > Reviewed-by: Kentaro Hara <haraken@chromium.org>
    > Commit-Queue: Keishi Hattori <keishi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#500487}
    
    TBR=haraken@chromium.org,keishi@chromium.org
    
    Change-Id: Ief9c32fb5d7af05c444200a2e1ceab721a4d0160
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 763235
    Reviewed-on: https://chromium-review.googlesource.com/656997
    Reviewed-by: Matt Giuca <mgiuca@chromium.org>
    Commit-Queue: Matt Giuca <mgiuca@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500511}
    Former-commit-id: 16a22949c41721a9a8b80aa3a0c801dfb54b6bb8

--- third_party/WebKit/Source/platform/heap/ThreadState.h
+++ third_party/WebKit/Source/platform/heap/ThreadState.h
@@ -537,6 +537,11 @@ class PLATFORM_EXPORT ThreadState {
   void leaveStaticReferenceRegistrationDisabledScope();
 #endif
 
+  void ResetHeapCounters();
+  void IncreaseAllocatedObjectSize(size_t);
+  void DecreaseAllocatedObjectSize(size_t);
+  void IncreaseMarkedObjectSize(size_t);
+
   v8::Isolate* GetIsolate() const { return isolate_; }
 
   BlinkGC::StackState GetStackState() const { return stack_state_; }
@@ -739,6 +744,9 @@ class PLATFORM_EXPORT ThreadState {
       kLikelyToBePromptlyFreedArraySize - 1;
   std::unique_ptr<int[]> likely_to_be_promptly_freed_;
 
+  // Stats for heap memory of this thread.
+  size_t allocated_object_size_;
+  size_t marked_object_size_;
   size_t reported_memory_to_v8_;
 
   int gc_age_ = 0;
