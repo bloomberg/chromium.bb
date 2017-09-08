561f39015686945a9e312e498922b63afb1c3751
Niels Möller <nisse@chromium.org>
2017-09-08 10:24:11 +0000

    Align task_queue.h in webrtc_overrides with the webrtc version.
    Rename class TaskQueue::WorkerThread to TaskQueue::Impl, member
    TaskQueue::thread_ to TaskQueue::impl_, and use scoped_refptr.
    
    The intention is to delete task_queue.h from webrtc_overrides soon.
    
    Bug: webrtc:8166
    Change-Id: I2c8e5a76f903f0b34e4aa880197c6b5ec64220b5
    Reviewed-on: https://chromium-review.googlesource.com/657178
    Commit-Queue: Niels Möller <nisse@chromium.org>
    Reviewed-by: Tommi <tommi@chromium.org>
    Reviewed-by: Per Kjellander <perkj@google.com>
    Cr-Commit-Position: refs/heads/master@{#500556}
    Former-commit-id: 6877d1ca417605d8d4502ad387f603d98afa1b52

--- third_party/webrtc_overrides/webrtc/rtc_base/task_queue.cc
+++ third_party/webrtc_overrides/webrtc/rtc_base/task_queue.cc
@@ -12,22 +12,26 @@
 
 #include "base/bind.h"
 #include "base/lazy_instance.h"
+#include "base/memory/ref_counted.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_local.h"
+#include "third_party/webrtc/rtc_base/refcount.h"
+#include "third_party/webrtc/rtc_base/refcountedobject.h"
 
-namespace rtc {
+// Intentionally outside of the "namespace rtc { ... }" block, because
+// here, scoped_refptr should *not* be resolved as rtc::scoped_refptr.
 namespace {
 
-void RunTask(std::unique_ptr<QueuedTask> task) {
+void RunTask(std::unique_ptr<rtc::QueuedTask> task) {
   if (!task->Run())
     task.release();
 }
 
-class PostAndReplyTask : public QueuedTask {
+class PostAndReplyTask : public rtc::QueuedTask {
  public:
   PostAndReplyTask(
-      std::unique_ptr<QueuedTask> task,
-      std::unique_ptr<QueuedTask> reply,
+      std::unique_ptr<rtc::QueuedTask> task,
+      std::unique_ptr<rtc::QueuedTask> reply,
       const scoped_refptr<base::SingleThreadTaskRunner>& reply_task_runner)
       : task_(std::move(task)),
         reply_(std::move(reply)),
@@ -45,25 +49,27 @@ class PostAndReplyTask : public QueuedTask {
     return true;
   }
 
-  std::unique_ptr<QueuedTask> task_;
-  std::unique_ptr<QueuedTask> reply_;
+  std::unique_ptr<rtc::QueuedTask> task_;
+  std::unique_ptr<rtc::QueuedTask> reply_;
   scoped_refptr<base::SingleThreadTaskRunner> reply_task_runner_;
 };
 
 // A lazily created thread local storage for quick access to a TaskQueue.
-base::LazyInstance<base::ThreadLocalPointer<TaskQueue>>::Leaky lazy_tls_ptr =
-    LAZY_INSTANCE_INITIALIZER;
+base::LazyInstance<base::ThreadLocalPointer<rtc::TaskQueue>>::Leaky
+    lazy_tls_ptr = LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
+
+namespace rtc {
 
 bool TaskQueue::IsCurrent() const {
   return Current() == this;
 }
 
-class TaskQueue::WorkerThread : public base::Thread {
+class TaskQueue::Impl : public RefCountInterface, public base::Thread {
  public:
-  WorkerThread(const char* queue_name, TaskQueue* queue);
-  ~WorkerThread() override;
+  Impl(const char* queue_name, TaskQueue* queue);
+  ~Impl() override;
 
  private:
   virtual void Init() override;
@@ -71,21 +77,20 @@ class TaskQueue::WorkerThread : public base::Thread {
   TaskQueue* const queue_;
 };
 
-TaskQueue::WorkerThread::WorkerThread(const char* queue_name, TaskQueue* queue)
+TaskQueue::Impl::Impl(const char* queue_name, TaskQueue* queue)
     : base::Thread(queue_name), queue_(queue) {}
 
-void TaskQueue::WorkerThread::Init() {
+void TaskQueue::Impl::Init() {
   lazy_tls_ptr.Pointer()->Set(queue_);
 }
 
-TaskQueue::WorkerThread::~WorkerThread() {
+TaskQueue::Impl::~Impl() {
   DCHECK(!Thread::IsRunning());
 }
 
 TaskQueue::TaskQueue(const char* queue_name,
                      Priority priority /*= Priority::NORMAL*/)
-    : thread_(
-          std::unique_ptr<WorkerThread>(new WorkerThread(queue_name, this))) {
+    : impl_(new RefCountedObject<Impl>(queue_name, this)) {
   DCHECK(queue_name);
   base::Thread::Options options;
   switch (priority) {
@@ -100,13 +105,12 @@ TaskQueue::TaskQueue(const char* queue_name,
       options.priority = base::ThreadPriority::NORMAL;
       break;
   }
-  bool result = thread_->StartWithOptions(options);
-  CHECK(result);
+  CHECK(impl_->StartWithOptions(options));
 }
 
 TaskQueue::~TaskQueue() {
   DCHECK(!IsCurrent());
-  thread_->Stop();
+  impl_->Stop();
 }
 
 // static
@@ -115,13 +119,13 @@ TaskQueue* TaskQueue::Current() {
 }
 
 void TaskQueue::PostTask(std::unique_ptr<QueuedTask> task) {
-  thread_->task_runner()->PostTask(FROM_HERE,
-                                   base::Bind(&RunTask, base::Passed(&task)));
+  impl_->task_runner()->PostTask(FROM_HERE,
+                                 base::Bind(&RunTask, base::Passed(&task)));
 }
 
 void TaskQueue::PostDelayedTask(std::unique_ptr<QueuedTask> task,
                                 uint32_t milliseconds) {
-  thread_->task_runner()->PostDelayedTask(
+  impl_->task_runner()->PostDelayedTask(
       FROM_HERE, base::Bind(&RunTask, base::Passed(&task)),
       base::TimeDelta::FromMilliseconds(milliseconds));
 }
@@ -130,12 +134,12 @@ void TaskQueue::PostTaskAndReply(std::unique_ptr<QueuedTask> task,
                                  std::unique_ptr<QueuedTask> reply,
                                  TaskQueue* reply_queue) {
   PostTask(std::unique_ptr<QueuedTask>(new PostAndReplyTask(
-      std::move(task), std::move(reply), reply_queue->thread_->task_runner())));
+      std::move(task), std::move(reply), reply_queue->impl_->task_runner())));
 }
 
 void TaskQueue::PostTaskAndReply(std::unique_ptr<QueuedTask> task,
                                  std::unique_ptr<QueuedTask> reply) {
-  thread_->task_runner()->PostTaskAndReply(
+  impl_->task_runner()->PostTaskAndReply(
       FROM_HERE, base::Bind(&RunTask, base::Passed(&task)),
       base::Bind(&RunTask, base::Passed(&reply)));
 }
