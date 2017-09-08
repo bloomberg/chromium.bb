9b57c1f88b41e31cd6108c2dc4896f1060d16e38
Ben Smith <binji@chromium.org>
2017-09-08 00:33:57 +0000

    Only neuter ArrayBufferViews when all are neuterable
    https://crbug.com/254728 was fixed by https://crrev.com/19019002/
    a few years ago. The fix was to only transfer the array's data 
    when no ArrayBufferViews were marked as non-neuterable. This feature 
    is only used for WebAudio, where the AudioBuffer's channel data may
    be in use while it was being detached. That fix is not spec compliant,
    since the spec says that the AudioBuffer's ArrayBuffers may be
    detached at any time. It's likely that this wasn't specified at the
    time however.
    
    ArrayBuffer::Transfer currently does the wrong thing in this case:
    when an ArrayBufferView is marked as non-neuterable, its contents
    are copied to the ArrayBufferContents object, and all ArrayBufferViews
    are removed, but the ArrayBuffer is not neutered. This leaves any
    ArrayBufferView in an inconsistent state; it has a pointer to its
    ArrayBuffer, but the ArrayBuffer does not have a pointer to the
    ArrayBufferView. Subsequent calls to ArrayBuffer::Transfer will
    therefore not be able to check this ArrayBufferView's neuterable
    flag, even though the ArrayBufferView has a pointer to the
    ArrayBuffer's data.
    
    BUG=chromium:761801
    
    Change-Id: I1d24b3f7543675af2d997d44d455003b396d2c6d
    Reviewed-on: https://chromium-review.googlesource.com/653489
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Reviewed-by: Raymond Toy <rtoy@chromium.org>
    Commit-Queue: Ben Smith <binji@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500452}
    Former-commit-id: e84445f6c1d2b4817d3510c94fcb82fd0e12df99

--- third_party/WebKit/Source/platform/wtf/typed_arrays/ArrayBuffer.cpp
+++ third_party/WebKit/Source/platform/wtf/typed_arrays/ArrayBuffer.cpp
@@ -47,20 +47,24 @@ bool ArrayBuffer::Transfer(ArrayBufferContents& result) {
 
   if (all_views_are_neuterable) {
     contents_.Transfer(result);
+
+    while (first_view_) {
+      ArrayBufferView* current = first_view_;
+      RemoveView(current);
+      current->Neuter();
+    }
+
+    is_neutered_ = true;
   } else {
+    // TODO(https://crbug.com/763038): See original bug at
+    // https://crbug.com/254728. Copying the buffer instead of transferring is
+    // not spec compliant but was added for a WebAudio bug fix. The only time
+    // this branch is taken is when attempting to transfer an AudioBuffer's
+    // channel data ArrayBuffer.
     contents_.CopyTo(result);
     if (!result.Data())
       return false;
   }
-
-  while (first_view_) {
-    ArrayBufferView* current = first_view_;
-    RemoveView(current);
-    if (all_views_are_neuterable || current->IsNeuterable())
-      current->Neuter();
-  }
-
-  is_neutered_ = true;
 
   return true;
 }
