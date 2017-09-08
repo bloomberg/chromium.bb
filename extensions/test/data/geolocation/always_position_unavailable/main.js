c5f83f7b491bd8d0dbbd78678e8b61565ee0878c
Conley Owens <cco3@chromium.org>
2017-09-08 21:08:22 +0000

    Report POSITION_UNAVAILABLE when no providers
    When there are no location providers, we currently report
    PERMISSION_DENIED.  It makes more sense to report POSITION_UNAVAILABLE.
    
    BUG=563768
    
    Change-Id: I09ab0618f8b49226dbce3817119a8c0ec5ef0843
    Reviewed-on: https://chromium-review.googlesource.com/540715
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Commit-Queue: Conley Owens <cco3@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500661}
    Former-commit-id: cce85a299110970aa1a9d2e3cae4a2fac42f24d0

--- extensions/test/data/geolocation/always_position_unavailable/main.js
+++ extensions/test/data/geolocation/always_position_unavailable/main.js
@@ -2,11 +2,11 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// This API call should always return permission denied because geolocation
+// This API call should always return position unavailable because geolocation
 // implementation is not complete in app_shell.
 
 function checkErrorCode(error) {
-  if (error.code == error.PERMISSION_DENIED)
+  if (error.code == error.POSITION_UNAVAILABLE)
     chrome.test.succeed();
   else
     chrome.test.fail();
