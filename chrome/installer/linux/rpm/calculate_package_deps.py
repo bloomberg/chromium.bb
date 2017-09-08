9ee945afec5aa4f113a0c5ec24bbd8962f85e0ad
Tom Anderson <thomasanderson@chromium.org>
2017-09-08 00:42:38 +0000

    Add script to calculate and verify RPM dependencies
    BUG=758654
    R=thestig@chromium.org
    NOTRY=true
    
    Change-Id: Ibc62c593a9e29425ef7f0c5f42b1cc95f998b1ed
    Reviewed-on: https://chromium-review.googlesource.com/653722
    Commit-Queue: Thomas Anderson <thomasanderson@chromium.org>
    Reviewed-by: Lei Zhang <thestig@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500455}
    Former-commit-id: 2b50d4264706eb14f1193bf6080c60dc07d4d4ae

---
+++ chrome/installer/linux/rpm/calculate_package_deps.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# Copyright 2017 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Given a binary, uses find-requires to check that its package dependencies
+are satisfiable on all supported rpm-based distros.
+"""
+
+import json
+import os
+import subprocess
+import sys
+
+if len(sys.argv) < 3:
+  print 'Usage: %s binary_path dep_file [shlib1 shlib2 ...]' % sys.argv[0]
+  sys.exit(1)
+
+binary = os.path.abspath(sys.argv[1])
+dep_filename = sys.argv[2]
+bundled_shlibs = [os.path.basename(file) for file in sys.argv[3:]]
+
+proc = subprocess.Popen(['/usr/lib/rpm/find-requires'], stdin=subprocess.PIPE,
+                        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+(stdout, stderr) = proc.communicate(binary + '\n')
+exit_code = proc.wait()
+if exit_code != 0:
+  print 'find-requires failed with exit code ' + str(exit_code)
+  print 'stderr was ' + stderr
+  sys.exit(1)
+
+requires = [] if stdout == '' else stdout.rstrip('\n').split('\n')
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+provides_file = open(os.path.join(script_dir, 'dist-package-provides.json'))
+distro_package_provides = json.load(provides_file)
+
+ret_code = 0
+for distro in distro_package_provides:
+  for requirement in requires:
+    if any([requirement.startswith(shlib) for shlib in bundled_shlibs]):
+      continue
+    if requirement not in distro_package_provides[distro]:
+      print >> sys.stderr, (
+          'Unexpected new dependency %s on distro %s caused by binary %s' % (
+              requirement, distro, os.path.basename(binary)))
+      ret_code = 1
+      continue
+if ret_code == 0:
+  with open(dep_filename, 'w') as dep_file:
+    dep_file.write(stdout)
+sys.exit(ret_code)
