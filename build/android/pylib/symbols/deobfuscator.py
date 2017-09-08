c970fe11b52ba76c4febb93850b59f48bd605e7c
Andrew Grieve <agrieve@chromium.org>
2017-09-08 01:50:36 +0000

    Android: Improve robustness of Deobfuscator during TearDown
    Was causing infinite output in one case, and crash in another.
    
    Bug: 762912
    Change-Id: I8a8eb306d274f34684b06ca3df722800413fb776
    Reviewed-on: https://chromium-review.googlesource.com/655121
    Commit-Queue: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: John Budorick <jbudorick@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500480}
    Former-commit-id: 33f43bb9c944ad7549cdf8d77f0f38f45ae39f9a

--- build/android/pylib/symbols/deobfuscator.py
+++ build/android/pylib/symbols/deobfuscator.py
@@ -5,6 +5,7 @@
 import logging
 import os
 import subprocess
+import threading
 import uuid
 
 from devil.utils import reraiser_thread
@@ -17,7 +18,6 @@ _PER_LINE_TIMEOUT = .002  # Should be able to process 500 lines per second.
 
 class Deobfuscator(object):
   def __init__(self, mapping_path):
-    self._reader_thread = None
     script_path = os.path.join(
         constants.GetOutDirectory(), 'bin', 'java_deobfuscate')
     cmd = [script_path, mapping_path]
@@ -25,13 +25,15 @@ class Deobfuscator(object):
     self._proc = subprocess.Popen(
         cmd, bufsize=1, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
         close_fds=True)
-    self._logged_error = False
+    # Allow only one thread to call TransformLines() at a time.
+    self._lock = threading.Lock()
+    self._closed_called = False
 
   def IsClosed(self):
-    return self._proc.returncode is not None
+    return self._closed_called or self._proc.returncode is not None
 
   def IsBusy(self):
-    return bool(self._reader_thread)
+    return self._lock.locked()
 
   def IsReady(self):
     return not self.IsClosed() and not self.IsBusy()
@@ -50,60 +52,68 @@ class Deobfuscator(object):
     if not lines:
       return []
 
-    # Allow only one thread to communicate with the subprocess at a time.
-    if self._reader_thread:
-      logging.warning('Having to wait for Java deobfuscation.')
-      self._reader_thread.join()
-
-    if self._proc.returncode is not None:
-      if not self._logged_error:
-        logging.warning('java_deobfuscate process exited with code=%d.',
-                        self._proc.returncode)
-        self._logged_error = True
-      return lines
-
-    out_lines = []
+    # Deobfuscated stacks contain more frames than obfuscated ones when method
+    # inlining occurs. To account for the extra output lines, keep reading until
+    # this eof_line token is reached.
     eof_line = uuid.uuid4().hex
+    out_lines = []
 
     def deobfuscate_reader():
       while True:
-        line = self._proc.stdout.readline()[:-1]
-        # Due to inlining, deobfuscated stacks may contain more frames than
-        # obfuscated ones. To account for the variable number of lines, keep
-        # reading until eof_line.
+        line = self._proc.stdout.readline()
+        # Return an empty string at EOF (when stdin is closed).
+        if not line:
+          break
+        line = line[:-1]
         if line == eof_line:
           break
         out_lines.append(line)
 
-    # TODO(agrieve): Can probably speed this up by only sending lines through
-    #     that might contain an obfuscated name.
-    self._reader_thread = reraiser_thread.ReraiserThread(deobfuscate_reader)
-    self._reader_thread.start()
-    try:
-      self._proc.stdin.write('\n'.join(lines))
-      self._proc.stdin.write('\n{}\n'.format(eof_line))
-      self._proc.stdin.flush()
-      timeout = max(_MINIUMUM_TIMEOUT, len(lines) * _PER_LINE_TIMEOUT)
-      self._reader_thread.join(timeout)
-      if self._reader_thread.is_alive():
-        logging.error('java_deobfuscate timed out.')
+    if not self.IsReady():
+      logging.warning('Having to wait for Java deobfuscation.')
+
+    # Allow only one thread to operate at a time.
+    with self._lock:
+      if self.IsClosed():
+        if not self._closed_called:
+          logging.warning('java_deobfuscate process exited with code=%d.',
+                          self._proc.returncode)
+          self.Close()
+        return lines
+
+      # TODO(agrieve): Can probably speed this up by only sending lines through
+      #     that might contain an obfuscated name.
+      reader_thread = reraiser_thread.ReraiserThread(deobfuscate_reader)
+      reader_thread.start()
+
+      try:
+        self._proc.stdin.write('\n'.join(lines))
+        self._proc.stdin.write('\n{}\n'.format(eof_line))
+        self._proc.stdin.flush()
+        timeout = max(_MINIUMUM_TIMEOUT, len(lines) * _PER_LINE_TIMEOUT)
+        reader_thread.join(timeout)
+        if self.IsClosed():
+          logging.warning('Close() called by another thread during join().')
+          return lines
+        if reader_thread.is_alive():
+          logging.error('java_deobfuscate timed out.')
+          self.Close()
+          return lines
+        return out_lines
+      except IOError:
+        logging.exception('Exception during java_deobfuscate')
         self.Close()
-      self._reader_thread = None
-      return out_lines
-    except IOError:
-      logging.exception('Exception during java_deobfuscate')
-      self.Close()
-      return lines
+        return lines
 
   def Close(self):
+    self._closed_called = True
     if not self.IsClosed():
       self._proc.stdin.close()
       self._proc.kill()
       self._proc.wait()
-    self._reader_thread = None
 
   def __del__(self):
-    if not self.IsClosed():
+    if not self._closed_called:
       logging.error('Forgot to Close() deobfuscator')
 
 
@@ -111,23 +121,27 @@ class DeobfuscatorPool(object):
   def __init__(self, mapping_path, pool_size=4):
     self._mapping_path = mapping_path
     self._pool = [Deobfuscator(mapping_path) for _ in xrange(pool_size)]
+    # Allow only one thread to select from the pool at a time.
+    self._lock = threading.Lock()
 
   def TransformLines(self, lines):
-    target_instance = next((x for x in self._pool if x.IsReady()), None)
+    with self._lock:
+      assert self._pool, 'TransformLines() called on a closed DeobfuscatorPool.'
+      # Restart any closed Deobfuscators.
+      for i, d in enumerate(self._pool):
+        if d.IsClosed():
+          logging.warning('Restarting closed Deobfuscator instance.')
+          self._pool[i] = Deobfuscator(self._mapping_path)
 
-    # Restart any closed ones.
-    for i, d in enumerate(self._pool):
-      if d.IsClosed():
-        logging.warning('Restarting closed Deobfuscator instance.')
-        self._pool[i] = Deobfuscator(self._mapping_path)
+      selected = next((x for x in self._pool if x.IsReady()), self._pool[0])
+      # Rotate the order so that next caller will not choose the same one.
+      self._pool.remove(selected)
+      self._pool.append(selected)
 
-    if not target_instance:
-      # No idle ones. Use the first one and cycle so as to not choose it again.
-      target_instance = self._pool[0]
-      self._pool.append(self._pool.pop(0))
-
-    return target_instance.TransformLines(lines)
+    return selected.TransformLines(lines)
 
   def Close(self):
-    for d in self._pool:
-      d.Close()
+    with self._lock:
+      for d in self._pool:
+        d.Close()
+      self._pool = None
