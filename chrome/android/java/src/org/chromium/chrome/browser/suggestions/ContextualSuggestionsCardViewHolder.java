320cfad1730379a03b8b3e4df2f2c2240170b00d
galinap <galinap@google.com>
2017-09-08 21:16:33 +0000

    [Suggestions] Add logs for contextual suggestion interactions
    This CL adds user action recording of the contextual suggestions
    carousel.
    
    Bug: 715477
    Change-Id: I543ad445d2bd1d248f4d905c0f8bbdebfd02450f
    Reviewed-on: https://chromium-review.googlesource.com/635543
    Reviewed-by: Alexei Svitkine <asvitkine@chromium.org>
    Reviewed-by: Nicolas Dossou-Gbété <dgn@chromium.org>
    Commit-Queue: Galia Peycheva <galinap@google.com>
    Cr-Commit-Position: refs/heads/master@{#500671}
    Former-commit-id: 7ba84a8f0c9163570ddf79c61ccd3ffac7c6abd7

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/ContextualSuggestionsCardViewHolder.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/ContextualSuggestionsCardViewHolder.java
@@ -29,7 +29,6 @@ public class ContextualSuggestionsCardViewHolder extends NewTabPageViewHolder {
     private static final double CARD_WIDTH_TO_WINDOW_SIZE_RATIO = 0.9;
     private final SuggestionsBinder mSuggestionsBinder;
     private final SuggestionsUiDelegate mUiDelegate;
-    private final UiConfig mUiConfig;
     private final DisplayStyleObserverAdapter mDisplayStyleObserver;
     private SnippetArticle mSuggestion;
 
@@ -37,7 +36,6 @@ public class ContextualSuggestionsCardViewHolder extends NewTabPageViewHolder {
             ViewGroup recyclerView, UiConfig uiConfig, SuggestionsUiDelegate uiDelegate) {
         super(getCardView(recyclerView));
 
-        mUiConfig = uiConfig;
         mUiDelegate = uiDelegate;
         mSuggestionsBinder = new SuggestionsBinder(itemView, uiDelegate);
 
@@ -49,10 +47,11 @@ public class ContextualSuggestionsCardViewHolder extends NewTabPageViewHolder {
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO(fhorschig): Add metrics to be recorded for the contextual suggestions.
                 int windowDisposition = WindowOpenDisposition.CURRENT_TAB;
                 mUiDelegate.getNavigationDelegate().navigateToSuggestionUrl(
                         windowDisposition, mSuggestion.mUrl);
+
+                SuggestionsMetrics.recordContextualSuggestionOpened();
             }
         });
 
