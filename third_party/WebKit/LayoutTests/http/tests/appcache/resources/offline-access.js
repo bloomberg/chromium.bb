76a059f1e9ac9cc4e6bac00b0a48a90a34024282
Michael Nordman <michaeln@google.com>
2017-09-08 20:37:55 +0000

    [AppCache] Delete flaky VerifyRedirectedSubresourceAppCacheLoad browser test.
    Instead, slightly modify an existing layout test (offline-access.html) to provide test coverage.
    
    Also note we get greater test coverage this way. The deleted browser test only ran with
    kNetworkService enabled, while the layout test runs with that enabled and disabled.
    
    Bug: 762381
    Change-Id: I1ed7636dd4223fdffa0555d3b64323b353437139
    Reviewed-on: https://chromium-review.googlesource.com/656298
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Michael Nordman <michaeln@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500648}
    Former-commit-id: c3ef0164c99594bba1622bad79b8fa3d004616a6

--- third_party/WebKit/LayoutTests/http/tests/appcache/resources/offline-access.js
+++ third_party/WebKit/LayoutTests/http/tests/appcache/resources/offline-access.js
@@ -5,14 +5,27 @@ function test()
     applicationCache.onerror = function() { log("error") }
 
     try {
+        var expectedContent = "Hello, World!";
+        var appCachedResource =
+            "/resources/network-simulator.php?path=/appcache/resources/simple.txt";
+        var redirectToAppCachedResource =
+            "/resources/redirect.php?url=" + appCachedResource;
+
         var req = new XMLHttpRequest;
-        req.open("GET", "/resources/network-simulator.php?path=/appcache/resources/simple.txt", false);
+        req.open("GET", appCachedResource, false);
         req.send(null);
-        if (req.responseText == "Hello, World!")
-            parent.postMessage("done", "*");
-        else
-            alert("FAIL, unexpected response: " + ex);
+        if (req.responseText != expectedContent)
+            throw "wrong response text for appCachedResource";
+
+        req = new XMLHttpRequest;
+        req.open("GET", redirectToAppCachedResource, false);
+        req.send(null);
+        if (req.responseText != expectedContent)
+            throw "wrong response text for redirectToAppCachedResource";
+
+        parent.postMessage("done", "*");
     } catch (ex) {
         alert("FAIL, unexpected error: " + ex);
+        parent.postMessage(ex, "*");
     }
 }
