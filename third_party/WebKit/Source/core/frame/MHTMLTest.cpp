e12e4080d206b2f2bf28097b8b265e6c31895420
Daniel Cheng <dcheng@chromium.org>
2017-09-08 21:16:57 +0000

    Enforce sandbox flags for MHTML in Document instead of DocumentLoader.
    This explicitly forces sandbox flags for all frames loaded from a MHTML
    archive, rather than relying on the standard propagation of sandbox
    flags from a parent to a child frame.
    
    Bug: 763140
    Change-Id: Iadb2e5209a2beb6a1b38136a1e7623c2f311d9ce
    Reviewed-on: https://chromium-review.googlesource.com/656779
    Commit-Queue: Daniel Cheng <dcheng@chromium.org>
    Reviewed-by: Łukasz Anforowicz <lukasza@chromium.org>
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500672}
    Former-commit-id: 072fab6f5eba805ff525505a6e4e7f1e30cfeaa6

--- third_party/WebKit/Source/core/frame/MHTMLTest.cpp
+++ third_party/WebKit/Source/core/frame/MHTMLTest.cpp
@@ -341,6 +341,25 @@ TEST_F(MHTMLTest, EnforceSandboxFlags) {
 
   // The element to be created by the script is not there.
   EXPECT_FALSE(document->getElementById("mySpan"));
+
+  // Make sure the subframe is also sandboxed.
+  LocalFrame* child_frame =
+      ToLocalFrame(GetPage()->MainFrame()->Tree().FirstChild());
+  ASSERT_TRUE(child_frame);
+  Document* child_document = child_frame->GetDocument();
+  ASSERT_TRUE(child_document);
+
+  EXPECT_EQ(kSandboxAll & ~(kSandboxPopups |
+                            kSandboxPropagatesToAuxiliaryBrowsingContexts),
+            child_document->GetSandboxFlags());
+
+  // MHTML document should be loaded into unique origin.
+  EXPECT_TRUE(child_document->GetSecurityOrigin()->IsUnique());
+  // Script execution should be disabled.
+  EXPECT_FALSE(child_document->CanExecuteScripts(kNotAboutToExecuteScript));
+
+  // The element to be created by the script is not there.
+  EXPECT_FALSE(child_document->getElementById("mySpan"));
 }
 
 TEST_F(MHTMLTest, EnforceSandboxFlagsInXSLT) {
