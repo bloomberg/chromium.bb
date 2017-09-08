588b34906e060ef6cdedecfbfeca6e3b3b59a9ac
Tarun Bansal <tbansal@chromium.org>
2017-09-08 01:31:29 +0000

    Do not store ECT if it's value is unknown
    Do not store Effective Connection Type (ECT) in prefs
    if its value is UNKNOWN.
    
    Also, enable caching for Ethernet connections
    in Network Quality Estimator (NQE).
    
    Bug: 762064
    Change-Id: I19b7f51644bdf9b6ccfa7182500b3cba27febd52
    Cq-Include-Trybots: master.tryserver.chromium.android:android_cronet_tester
    Reviewed-on: https://chromium-review.googlesource.com/648651
    Commit-Queue: Tarun Bansal <tbansal@chromium.org>
    Reviewed-by: Ryan Sturm <ryansturm@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500474}
    Former-commit-id: d7718847e9938b80e58148d91555c2bb6eff9b14

--- net/nqe/network_quality_estimator.cc
+++ net/nqe/network_quality_estimator.cc
@@ -1513,6 +1513,8 @@ bool NetworkQualityEstimator::ReadCachedNetworkQualityEstimate() {
 
   if (current_network_id_.type !=
           NetworkChangeNotifier::ConnectionType::CONNECTION_WIFI &&
+      current_network_id_.type !=
+          NetworkChangeNotifier::ConnectionType::CONNECTION_ETHERNET &&
       !disable_offline_check_) {
     return false;
   }
@@ -1733,12 +1735,10 @@ void NetworkQualityEstimator::
     observer.OnEffectiveConnectionTypeChanged(effective_connection_type_);
 
   // Add the estimates of the current network to the cache store.
-  if (effective_connection_type_ != EFFECTIVE_CONNECTION_TYPE_UNKNOWN) {
     network_quality_store_->Add(current_network_id_,
                                 nqe::internal::CachedNetworkQuality(
                                     tick_clock_->NowTicks(), network_quality_,
                                     effective_connection_type_));
-  }
 }
 
 void NetworkQualityEstimator::NotifyObserversOfRTTOrThroughputComputed() const {
@@ -1865,6 +1865,8 @@ void NetworkQualityEstimator::MaybeUpdateNetworkQualityFromCache(
     return;
   if (network_id.type !=
           NetworkChangeNotifier::ConnectionType::CONNECTION_WIFI &&
+      network_id.type !=
+          NetworkChangeNotifier::ConnectionType::CONNECTION_ETHERNET &&
       !disable_offline_check_) {
     return;
   }
