a685e39b5f787dbb689c91a0f40635359383df3e
Michael Nordman <michaeln@google.com>
2017-09-08 20:37:55 +0000

    [AppCache] Delete flaky VerifyRedirectedSubresourceAppCacheLoad browser test.
    Instead, slightly modify an existing layout test (offline-access.html) to provide test coverage.
    
    Also note we get greater test coverage this way. The deleted browser test only ran with
    kNetworkService enabled, while the layout test runs with that enabled and disabled.
    
    Bug: 762381
    Change-Id: I1ed7636dd4223fdffa0555d3b64323b353437139
    Reviewed-on: https://chromium-review.googlesource.com/656298
    Reviewed-by: Victor Costan <pwnall@chromium.org>
    Commit-Queue: Michael Nordman <michaeln@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500648}
    Former-commit-id: c3ef0164c99594bba1622bad79b8fa3d004616a6

--- content/browser/appcache/appcache_browsertest.cc
+++ content/browser/appcache/appcache_browsertest.cc
@@ -3,9 +3,7 @@
 // found in the LICENSE file.
 
 #include <stdint.h>
-#include <set>
 #include "base/command_line.h"
-#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "content/browser/appcache/appcache_subresource_url_factory.h"
@@ -33,38 +31,15 @@ class AppCacheNetworkServiceBrowserTest : public ContentBrowserTest {
   std::unique_ptr<net::test_server::HttpResponse> HandleRequest(
       const net::test_server::HttpRequest& request) {
     request_count_++;
-
-    relative_urls_seen_.insert(request.relative_url);
-
-    // Generate a redirect response for this resource.
-    if (!base::CompareCaseInsensitiveASCII(request.relative_url,
-                                           "/appcache/redirect/logo.png")) {
-      std::unique_ptr<net::test_server::BasicHttpResponse> http_response(
-          new net::test_server::BasicHttpResponse);
-      http_response->set_code(net::HTTP_TEMPORARY_REDIRECT);
-      std::string dest_url = request.base_url.spec();
-      dest_url += "appcache/logo.png";
-      http_response->AddCustomHeader("Location", dest_url.c_str());
-      redirect_count_++;
-      return std::move(http_response);
-    }
     return std::unique_ptr<net::test_server::HttpResponse>();
   }
 
   // Call this to reset the request_count_.
   void Clear() {
     request_count_ = 0;
-    redirect_count_ = 0;
-    relative_urls_seen_.clear();
   }
 
   int request_count() const { return request_count_; }
-
-  int redirect_count() const { return redirect_count_; }
-
-  bool SeenRelativeUrl(const std::string& url) {
-    return relative_urls_seen_.find(url) != relative_urls_seen_.end();
-  }
 
  protected:
   void SetUpCommandLine(base::CommandLine* command_line) override {
@@ -75,11 +50,6 @@ class AppCacheNetworkServiceBrowserTest : public ContentBrowserTest {
  private:
   // Tracks the number of requests.
   int request_count_ = 0;
-  int redirect_count_ = 0;
-
-  // Holds the list of relative URLs seen by the HTTP request handler.
-  std::set<std::string> relative_urls_seen_;
-
   DISALLOW_COPY_AND_ASSIGN(AppCacheNetworkServiceBrowserTest);
 };
 
@@ -132,56 +102,6 @@ IN_PROC_BROWSER_TEST_F(AppCacheNetworkServiceBrowserTest,
   EXPECT_EQ(page_no_manifest, observer.last_navigation_url());
   EXPECT_TRUE(observer.last_navigation_succeeded());
 }
-
-// This test validates that navigating to a page which has an AppCache manifest
-// and subresources which are redirected to resources which exist in the
-// AppCache are loaded from the cache.
-//
-// Disabled because this is flaky (timeout). http://crbug.com/762381
-IN_PROC_BROWSER_TEST_F(AppCacheNetworkServiceBrowserTest,
-                       DISABLED_VerifyRedirectedSubresourceAppCacheLoad) {
-  std::unique_ptr<net::EmbeddedTestServer> embedded_test_server(
-      new net::EmbeddedTestServer());
-
-  embedded_test_server->RegisterRequestHandler(
-      base::Bind(&AppCacheNetworkServiceBrowserTest::HandleRequest,
-                 base::Unretained(this)));
-
-  base::FilePath content_test_data(FILE_PATH_LITERAL("content/test/data"));
-  embedded_test_server->AddDefaultHandlers(content_test_data);
-
-  ASSERT_TRUE(embedded_test_server->Start());
-
-  GURL main_url = embedded_test_server->GetURL(
-      "/appcache/simple_page_redirected_resource_manifest.html");
-
-  base::string16 expected_title = base::ASCIIToUTF16("AppCache updated");
-
-  // Load the main page twice. The second navigation should have AppCache
-  // initialized for the page.
-  // We expect to a redirect response and then a request for the redirected
-  // resource here.
-  TitleWatcher title_watcher_update(shell()->web_contents(), expected_title);
-  EXPECT_TRUE(NavigateToURL(shell(), main_url));
-  EXPECT_EQ(expected_title, title_watcher_update.WaitAndGetTitle());
-  EXPECT_EQ(1, redirect_count());
-  EXPECT_TRUE(SeenRelativeUrl("/appcache/redirect/logo.png"));
-  EXPECT_TRUE(SeenRelativeUrl("/appcache/logo.png"));
-
-  // The second attempt to load the page should serve the resources out of the
-  // cache. Here we expect to see one redirect request. The redirected resource
-  // should be served out of the AppCache.
-  Clear();
-  expected_title = base::ASCIIToUTF16("AppCache image loaded");
-  TitleWatcher title_watcher_image_loaded(shell()->web_contents(),
-                                          expected_title);
-  EXPECT_TRUE(NavigateToURL(shell(), main_url));
-  EXPECT_EQ(1, redirect_count());
-  EXPECT_TRUE(SeenRelativeUrl("/appcache/redirect/logo.png"));
-  EXPECT_FALSE(SeenRelativeUrl("/appcache/logo.png"));
-  EXPECT_EQ(expected_title, title_watcher_image_loaded.WaitAndGetTitle());
-}
-
 #endif
 
 }  // namespace content
