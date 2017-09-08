029599f66cb2ccfee518b33f5ef7607770b5ed7e
Aaron Leventhal <aleventhal@chromium.org>
2017-09-08 18:20:28 +0000

    Remove BeautifulSoup import as it is no longer found at this path and is unused.
    Bug: None
    Change-Id: Ic5e990ffdaef5084dcfe450e61f7e8cccf2ad3ad
    Reviewed-on: https://chromium-review.googlesource.com/658017
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Commit-Queue: Nektarios Paisios <nektar@chromium.org>
    Reviewed-by: Nektarios Paisios <nektar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500624}
    Former-commit-id: db74d3de144d34ae6e9eccfadf0e3db86c129b49

--- tools/accessibility/rebase_dump_accessibility_tree_test.py
+++ tools/accessibility/rebase_dump_accessibility_tree_test.py
@@ -27,10 +27,6 @@ import time
 import urllib
 import urlparse
 
-# Load BeautifulSoup. It's checked into two places in the Chromium tree.
-sys.path.append('third_party/WebKit/Tools/Scripts/webkitpy/thirdparty/')
-from BeautifulSoup import BeautifulSoup
-
 # The location of the DumpAccessibilityTree html test files and expectations.
 TEST_DATA_PATH = os.path.join(os.getcwd(), 'content/test/data/accessibility')
 
