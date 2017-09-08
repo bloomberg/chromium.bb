8e61e9d328fd9e1ed34cd0f3815dc0d01e2464a4
Peter Kotwicz <pkotwicz@chromium.org>
2017-09-08 18:44:04 +0000

    Revert "Split ThumbnailProviderImpl cache into a LRU and a deduplication cache."
    This reverts commit 2a472e6bec7428f1e91f323ab29296d96f146007 [formerly a4f8bff1bb207d38f2b083166fb7b28f248049bd].
    
    Reason for revert: The CL was causing multiple TabStripTest.* to crash
    
    Crash stack:
    E/chromium(18334): [ERROR:java_exception_reporter.cc(38)] java.lang.NullPointerException: Attempt to invoke virtual method 'void org.chromium.base.DiscardableReferencePool.drain()' on a null object reference
    E/chromium(18334): 	at org.chromium.chrome.browser.ChromeApplication.onTrimMemory(ChromeApplication.java:72)
    E/chromium(18334): 	at android.app.ActivityThread.handleTrimMemory(ActivityThread.java:4251)
    E/chromium(18334): 	at android.app.ActivityThread$H.handleMessage(ActivityThread.java:1491)
    E/chromium(18334): 	at android.os.Handler.dispatchMessage(Handler.java:102)
    E/chromium(18334): 	at android.os.Looper.loop(Looper.java:135)
    E/chromium(18334): 	at android.app.ActivityThread.main(ActivityThread.java:5254)
    E/chromium(18334): 	at java.lang.reflect.Method.invoke(Native Method)
    E/chromium(18334): 	at java.lang.reflect.Method.invoke(Method.java:372)
    E/chromium(18334): 	at com.android.internal.os.ZygoteInit$MethodAndArgsCaller.run(ZygoteInit.java:903)
    E/chromium(18334): 	at com.android.internal.os.ZygoteInit.main(ZygoteInit.java:698)
    E/chromium(18334): 
    
    Original change's description:
    > Split ThumbnailProviderImpl cache into a LRU and a deduplication cache.
    > 
    > Bug: 718925
    > Change-Id: I8bfe2d4b743c8fdb014b2112683aba1f74835753
    > Reviewed-on: https://chromium-review.googlesource.com/582958
    > Reviewed-by: Theresa <twellington@chromium.org>
    > Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    > Commit-Queue: Bernhard Bauer <bauerb@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#500574}
    
    TBR=bauerb@chromium.org,mvanouwerkerk@chromium.org,twellington@chromium.org
    
    Change-Id: Ic79a768f6e2c696f01c92e3b569cff04b2e21050
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 718925
    Reviewed-on: https://chromium-review.googlesource.com/658178
    Reviewed-by: Peter Kotwicz <pkotwicz@chromium.org>
    Commit-Queue: Peter Kotwicz <pkotwicz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500630}
    Former-commit-id: a16189bedba98586e1360666460717f88b63d8ee

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsUiDelegateImpl.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsUiDelegateImpl.java
@@ -38,7 +38,7 @@ public class SuggestionsUiDelegateImpl implements SuggestionsUiDelegate {
         mSuggestionsRanker = new SuggestionsRanker();
         mSuggestionsEventReporter = eventReporter;
         mSuggestionsNavigationDelegate = navigationDelegate;
-        mImageFetcher = new ImageFetcher(suggestionsSource, profile, referencePool, host);
+        mImageFetcher = new ImageFetcher(suggestionsSource, profile, host);
 
         mHost = host;
         mReferencePool = referencePool;
