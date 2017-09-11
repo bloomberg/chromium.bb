b6187c9e9993f0162addff74d011bc4d4acf0b00
Peter E Conn <peconn@chromium.org>
2017-09-11 16:16:02 +0000

    🏡 Disable Scroll To Load when Accessibility is enabled.
    Infinite loading provides a bad experience to users of Accessibility
    mode as they may attempt to reach the end of the list. Disable Scroll
    To Load if Accessibility mode is enabled.
    
    Bug: 763884
    Change-Id: I020ba041efafdc01155db12ad85684983f0a49f2
    Reviewed-on: https://chromium-review.googlesource.com/660577
    Reviewed-by: Michael van Ouwerkerk <mvanouwerkerk@chromium.org>
    Commit-Queue: Peter Conn <peconn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500935}
    Former-commit-id: 62a2f72b5209aad7645717e38c02b7e452366fac

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsConfig.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsConfig.java
@@ -9,6 +9,7 @@ import android.content.res.Resources;
 import org.chromium.base.ApiCompatibilityUtils;
 import org.chromium.chrome.R;
 import org.chromium.chrome.browser.ChromeFeatureList;
+import org.chromium.chrome.browser.util.AccessibilityUtil;
 import org.chromium.chrome.browser.util.FeatureUtilities;
 import org.chromium.chrome.browser.widget.displaystyle.UiConfig;
 
@@ -34,6 +35,9 @@ public final class SuggestionsConfig {
      * @return Whether scrolling to the bottom of suggestions triggers a load.
      */
     public static boolean scrollToLoad() {
+        // The scroll to load feature does not work well for users who require accessibility mode.
+        if (AccessibilityUtil.isAccessibilityEnabled()) return false;
+
         return FeatureUtilities.isChromeHomeModernEnabled()
                 && ChromeFeatureList.isEnabled(
                            ChromeFeatureList.CONTENT_SUGGESTIONS_SCROLL_TO_LOAD);
