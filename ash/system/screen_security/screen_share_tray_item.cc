1298365482a316bf0c5afd13ff5b4bea0327463d
Tetsui Ohkubo <tetsui@chromium.org>
2017-09-11 05:48:27 +0000

    Apply system style to screen share notification.
    In new-style notification, system notification has different style from
    other notifications such as web notification.
    This CL applies the system style to screen share notification.
    * The context header and action buttons have accent color.
    * New material design icon is added.
    * The title text is added.
    
    TEST=manual
    BUG=751024
    
    Change-Id: I43c623e6fe5a960289424500292ed75c2d63f2cb
    Reviewed-on: https://chromium-review.googlesource.com/656782
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Reviewed-by: James Cook <jamescook@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#500846}
    Former-commit-id: f79325e1a5a3876901899568acaa956606380b9b

--- ash/system/screen_security/screen_share_tray_item.cc
+++ ash/system/screen_security/screen_share_tray_item.cc
@@ -7,6 +7,7 @@
 #include <utility>
 
 #include "ash/resources/grit/ash_resources.h"
+#include "ash/resources/vector_icons/vector_icons.h"
 #include "ash/shell.h"
 #include "ash/strings/grit/ash_strings.h"
 #include "ash/system/system_notifier.h"
@@ -15,7 +16,9 @@
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/message_center/message_center.h"
 #include "ui/message_center/notification.h"
+#include "ui/message_center/public/cpp/message_center_switches.h"
 
+using message_center::MessageCenter;
 using message_center::Notification;
 
 namespace ash {
@@ -55,14 +58,24 @@ void ScreenShareTrayItem::CreateOrUpdateNotification() {
   data.buttons.push_back(message_center::ButtonInfo(
       l10n_util::GetStringUTF16(IDS_ASH_STATUS_TRAY_SCREEN_SHARE_STOP)));
   ui::ResourceBundle& resource_bundle = ui::ResourceBundle::GetSharedInstance();
-  std::unique_ptr<Notification> notification(new Notification(
-      message_center::NOTIFICATION_TYPE_SIMPLE, kScreenShareNotificationId,
-      help_label_text, base::string16() /* body is blank */,
-      resource_bundle.GetImageNamed(IDR_AURA_UBER_TRAY_SCREENSHARE_DARK),
-      base::string16() /* display_source */, GURL(),
-      message_center::NotifierId(message_center::NotifierId::SYSTEM_COMPONENT,
-                                 system_notifier::kNotifierScreenShare),
-      data, new tray::ScreenNotificationDelegate(this)));
+  std::unique_ptr<Notification> notification =
+      system_notifier::CreateSystemNotification(
+          message_center::NOTIFICATION_TYPE_SIMPLE, kScreenShareNotificationId,
+          message_center::IsNewStyleNotificationEnabled()
+              ? l10n_util::GetStringUTF16(
+                    IDS_ASH_STATUS_TRAY_SCREEN_SHARE_TITLE)
+              : help_label_text,
+          message_center::IsNewStyleNotificationEnabled()
+              ? help_label_text
+              : base::string16() /* body is blank */,
+          resource_bundle.GetImageNamed(IDR_AURA_UBER_TRAY_SCREENSHARE_DARK),
+          base::string16() /* display_source */, GURL(),
+          message_center::NotifierId(
+              message_center::NotifierId::SYSTEM_COMPONENT,
+              system_notifier::kNotifierScreenShare),
+          data, new tray::ScreenNotificationDelegate(this),
+          kNotificationScreenshareIcon,
+          message_center::SystemNotificationWarningLevel::NORMAL);
   notification->SetSystemPriority();
   message_center::MessageCenter::Get()->AddNotification(
       std::move(notification));
