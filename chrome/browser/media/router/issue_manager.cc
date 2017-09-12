2af980d081a4a1ac7afffdebfbbd5e6f13a14aa2
Derek Cheng <imcheng@chromium.org>
2017-09-12 17:57:01 +0000

    [Media Router] Auto-dimiss non-blocking issues part 1.
    This patch implements the following:
    - auto-dismiss notification issues after 1 minute
    - auto-dismiss non-notification non-blocking issue after 5 minutes
    
    This is done by posting a task to clear a given issue after it has been
    added to the IssueManager.
    
    Part 2 will be to dismiss non-blocking issues following a successful
    cast from the Media Router UI.
    
    Bug: 763432
    Change-Id: I6c55d4c272b8fc09b48935c6d4011d893e0cea58
    Reviewed-on: https://chromium-review.googlesource.com/658138
    Commit-Queue: Derek Cheng <imcheng@chromium.org>
    Reviewed-by: mark a. foltz <mfoltz@chromium.org>
    Reviewed-by: Takumi Fujimoto <takumif@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501334}
    Former-commit-id: 398990826f949d2904d710652184e51fea121dd9

--- chrome/browser/media/router/issue_manager.cc
+++ chrome/browser/media/router/issue_manager.cc
@@ -11,16 +11,48 @@
 
 namespace media_router {
 
-IssueManager::IssueManager() : top_issue_(nullptr) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+namespace {
+
+// The number of minutes a NOTIFICATION Issue stays in the IssueManager
+// before it is auto-dismissed.
+constexpr int kNotificationAutoDismissMins = 1;
+
+// The number of minutes a WARNING Issue stays in the IssueManager before it
+// is auto-dismissed.
+constexpr int kWarningAutoDismissMins = 5;
+
+}  // namespace
+
+// static
+base::TimeDelta IssueManager::GetAutoDismissTimeout(
+    const IssueInfo& issue_info) {
+  if (issue_info.is_blocking)
+    return base::TimeDelta();
+
+  switch (issue_info.severity) {
+    case IssueInfo::Severity::NOTIFICATION:
+      return base::TimeDelta::FromMinutes(kNotificationAutoDismissMins);
+    case IssueInfo::Severity::WARNING:
+      return base::TimeDelta::FromMinutes(kWarningAutoDismissMins);
+    case IssueInfo::Severity::FATAL:
+      NOTREACHED() << "FATAL issues should be blocking";
+      return base::TimeDelta();
+  }
+  NOTREACHED();
+  return base::TimeDelta();
 }
 
+IssueManager::IssueManager()
+    : top_issue_(nullptr),
+      task_runner_(content::BrowserThread::GetTaskRunnerForThread(
+          content::BrowserThread::UI)) {}
+
 IssueManager::~IssueManager() {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
 }
 
 void IssueManager::AddIssue(const IssueInfo& issue_info) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   auto it = std::find_if(issues_.begin(), issues_.end(),
                          [&issue_info](const std::unique_ptr<Issue>& issue) {
                            return issue_info == issue->info();
@@ -28,23 +60,36 @@ void IssueManager::AddIssue(const IssueInfo& issue_info) {
   if (it != issues_.end())
     return;
 
-  issues_.push_back(base::MakeUnique<Issue>(issue_info));
+  auto issue = base::MakeUnique<Issue>(issue_info);
+  base::TimeDelta timeout = GetAutoDismissTimeout(issue_info);
+  if (!timeout.is_zero()) {
+    const Issue::Id& issue_id = issue->id();
+    auto auto_dismiss_issue_cb = base::Bind(
+        &IssueManager::ClearIssue, base::Unretained(this), issue->id());
+    auto_dismiss_issue_callbacks_.emplace(
+        issue_id,
+        base::MakeUnique<base::CancelableClosure>(auto_dismiss_issue_cb));
+    task_runner_->PostDelayedTask(FROM_HERE, auto_dismiss_issue_cb, timeout);
+  }
+
+  issues_.push_back(std::move(issue));
   MaybeUpdateTopIssue();
 }
 
 void IssueManager::ClearIssue(const Issue::Id& issue_id) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   issues_.erase(
       std::remove_if(issues_.begin(), issues_.end(),
                      [&issue_id](const std::unique_ptr<Issue>& issue) {
                        return issue_id == issue->id();
                      }),
       issues_.end());
+  auto_dismiss_issue_callbacks_.erase(issue_id);
   MaybeUpdateTopIssue();
 }
 
 void IssueManager::RegisterObserver(IssuesObserver* observer) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   DCHECK(observer);
   DCHECK(!issues_observers_.HasObserver(observer));
 
@@ -55,7 +100,7 @@ void IssueManager::RegisterObserver(IssuesObserver* observer) {
 }
 
 void IssueManager::UnregisterObserver(IssuesObserver* observer) {
-  DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   issues_observers_.RemoveObserver(observer);
 }
 
