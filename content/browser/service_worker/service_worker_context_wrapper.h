4034d42c309f2789ef661a134a21734fe57c6bb2
Matt Falkenhagen <falken@chromium.org>
2017-09-12 02:39:23 +0000

    service worker: Finish OnceCallback migration in ServiceWorkerContext
    This also removes the gmock for ServiceWorkerContext since OnceCallback
    cannot be copied.
    
    Bug: 755477
    Change-Id: I59605c4ebc2d9aa691c73b7140d5e1c86e681198
    TBR: mpearson
    Reviewed-on: https://chromium-review.googlesource.com/654502
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Reviewed-by: Justin Donnelly <jdonnelly@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Commit-Queue: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501158}
    Former-commit-id: da8af67cd4d82b017a1b46e878a85d9f8d50fa8a

--- content/browser/service_worker/service_worker_context_wrapper.h
+++ content/browser/service_worker/service_worker_context_wrapper.h
@@ -125,7 +125,7 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
                                    base::OnceClosure failure_callback) override;
   void StartServiceWorkerForNavigationHint(
       const GURL& document_url,
-      const StartServiceWorkerForNavigationHintCallback& callback) override;
+      StartServiceWorkerForNavigationHintCallback callback) override;
   void StopAllServiceWorkersForOrigin(const GURL& origin) override;
 
   // These methods must only be called from the IO thread.
@@ -256,6 +256,7 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
   friend class base::RefCountedThreadSafe<ServiceWorkerContextWrapper>;
   friend class EmbeddedWorkerTestHelper;
   friend class EmbeddedWorkerBrowserTest;
+  friend class FakeServiceWorkerContextWrapper;
   friend class ForeignFetchRequestHandler;
   friend class ServiceWorkerDispatcherHost;
   friend class ServiceWorkerInternalsUI;
@@ -263,7 +264,6 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
   friend class ServiceWorkerProcessManager;
   friend class ServiceWorkerRequestHandler;
   friend class ServiceWorkerVersionBrowserTest;
-  friend class MockServiceWorkerContextWrapper;
 
   ~ServiceWorkerContextWrapper() override;
 
@@ -303,20 +303,20 @@ class CONTENT_EXPORT ServiceWorkerContextWrapper
 
   void StartServiceWorkerForNavigationHintOnIO(
       const GURL& document_url,
-      const StartServiceWorkerForNavigationHintCallback& callback);
+      StartServiceWorkerForNavigationHintCallback callback);
 
   void DidFindRegistrationForNavigationHint(
-      const StartServiceWorkerForNavigationHintCallback& callback,
+      StartServiceWorkerForNavigationHintCallback callback,
       ServiceWorkerStatusCode status,
       scoped_refptr<ServiceWorkerRegistration> registration);
 
   void DidStartServiceWorkerForNavigationHint(
       const GURL& pattern,
-      const StartServiceWorkerForNavigationHintCallback& callback,
+      StartServiceWorkerForNavigationHintCallback callback,
       ServiceWorkerStatusCode code);
 
   void RecordStartServiceWorkerForNavigationHintResult(
-      const StartServiceWorkerForNavigationHintCallback& callback,
+      StartServiceWorkerForNavigationHintCallback callback,
       StartServiceWorkerForNavigationHintResult result);
 
   // The core context is only for use on the IO thread.
