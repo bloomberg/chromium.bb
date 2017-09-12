ca069c9998bca30b5b8d749666ab5bc5e6b1aa53
Daria Yakovleva <yakovleva@google.com>
2017-09-12 14:07:02 +0000

    Reload google library in policy remover script
    When python script run it could already download
    google library (i.e. because of Google App Engine).
    This library contains wrong google protobuf library.
    Therefore we have to reload google library in the script.
    
    Bug: 764314
    Change-Id: I26d7ea31988d7b818d8c4c702e1614c7d35c85d5
    Reviewed-on: https://chromium-review.googlesource.com/660222
    Commit-Queue: Daria Iakovleva <yakovleva@google.com>
    Reviewed-by: Drew Wilson <atwilson@chromium.org>
    Reviewed-by: Denis Kuznetsov <antrim@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501262}
    Former-commit-id: 579f1f6ef8167f9f4a9c15bbf805c28453a29d93

--- chrome/browser/chromeos/policy/tools/generate_device_policy_remover.py
+++ chrome/browser/chromeos/policy/tools/generate_device_policy_remover.py
@@ -22,6 +22,12 @@ def main():
   sys.path.insert(0, descriptor_pool_path)
   sys.path.append(symbol_database_path)
   sys.path.append(chrome_device_policy_pb2_path)
+  # Make reload google library
+  # which might be already loaded due to Google App Engine
+  # TODO(crbug.com/764314): find better solution how to import protobuf.
+  import google.protobuf
+  reload(google)
+  reload(google.protobuf)
   from chrome_device_policy_pb2 import ChromeDeviceSettingsProto
   with open(off_hours_cleaner_path, 'wt') as file:
     file.write('//\n'
