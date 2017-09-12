00d79f91ba12c08f9c90e2f721b2a64697721dfd
Will Chen <chenwilliam@chromium.org>
2017-09-12 00:18:43 +0000

    DevTools: move http/tests/inspector/network/* tests (part 10)
    Bug: 667560
    Change-Id: Idda9841682e53af0f046a8b2f6dee98563fe4ee3
    Reviewed-on: https://chromium-review.googlesource.com/661217
    Commit-Queue: Will Chen <chenwilliam@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501112}
    Former-commit-id: b9382586e69a8ed220e15d84cf20dcdbcbdb1efa

---
+++ third_party/WebKit/LayoutTests/http/tests/devtools/network/network-disable-cache-preloads.php
@@ -0,0 +1,67 @@
+<?php
+    header("Link: <http://127.0.0.1:8000/resources/dummy.js>;rel=preload;as=script", false);
+?>
+<html>
+<head>
+<script src="../../inspector/inspector-test.js"></script>
+<script src="../../inspector/network-test.js"></script>
+<style>
+    body { overflow: hidden; }
+</style>
+<script>
+function scheduleScriptLoad() {
+    window.setTimeout(loadScript, 0);
+}
+
+function loadScript() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "http://127.0.0.1:8000/resources/dummy.js";
+    script.onload = function() {
+        setTimeout(scriptLoaded, 0);
+    };
+    document.head.appendChild(script);
+}
+
+function scriptLoaded() {
+    var resources = performance.getEntriesByType("resource");
+    var dummies = 0;
+    for (var i = 0; i < resources.length; ++i) {
+        if (resources[i].name.indexOf("dummy.js") != -1)
+            ++dummies;
+    }
+
+    var text;
+    if (dummies == 1)
+        text = document.createTextNode("PASS - 1 resource loaded");
+    else
+        text = document.createTextNode("FAIL - " + dummies + " resources loaded");
+    var result = document.createElement("p");
+    result.appendChild(text);
+    document.body.appendChild(result);
+    console.log("done");
+}
+
+async function test()
+{
+    await InspectorTest.NetworkAgent.setCacheDisabled(true);
+    InspectorTest.reloadPage(step2);
+
+    function step2(msg)
+    {
+        InspectorTest.addConsoleSniffer(done);
+        InspectorTest.evaluateInPage("scheduleScriptLoad()");
+    }
+
+    function done(msg)
+    {
+        InspectorTest.completeTest();
+    }
+}
+</script>
+</head>
+<body onload="runTest(true)">
+    <p>Tests disabling cache from inspector and seeing that preloads are not evicted from memory cache.</p>
+</body>
+</html>
+
