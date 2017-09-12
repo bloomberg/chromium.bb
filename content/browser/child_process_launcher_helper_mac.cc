24da9175bd16ca12a76393fa6f296ca8907998ab
Greg Kerr <kerrnel@chromium.org>
2017-09-12 19:43:45 +0000

    Remove boostrap sandbox code.
    The bootstrap sandbox was replaced by the V2 sandbox.
    
    Bug: 762077
    Change-Id: I2c9aeab22d29a42a2c800a53e0c8a5484e00c043
    Reviewed-on: https://chromium-review.googlesource.com/647686
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Commit-Queue: Greg Kerr <kerrnel@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501364}
    Former-commit-id: 837e59d79dd3706bf43c94c0ed93a323850182a3

--- content/browser/child_process_launcher_helper_mac.cc
+++ content/browser/child_process_launcher_helper_mac.cc
@@ -7,7 +7,6 @@
 #include "base/memory/ptr_util.h"
 #include "base/path_service.h"
 #include "base/posix/global_descriptors.h"
-#include "content/browser/bootstrap_sandbox_manager_mac.h"
 #include "content/browser/child_process_launcher.h"
 #include "content/browser/child_process_launcher_helper.h"
 #include "content/browser/child_process_launcher_helper_posix.h"
@@ -21,8 +20,6 @@
 #include "content/public/common/result_codes.h"
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
 #include "mojo/edk/embedder/scoped_platform_handle.h"
-#include "sandbox/mac/bootstrap_sandbox.h"
-#include "sandbox/mac/pre_exec_delegate.h"
 #include "sandbox/mac/seatbelt_exec.h"
 
 namespace content {
@@ -95,19 +92,6 @@ void ChildProcessLauncherHelper::BeforeLaunchOnLauncherThread(
   // Make sure the MachBroker is running, and inform it to expect a check-in
   // from the new process.
   broker->EnsureRunning();
-
-  const SandboxType sandbox_type = delegate_->GetSandboxType();
-  std::unique_ptr<sandbox::PreExecDelegate> pre_exec_delegate;
-  if (BootstrapSandboxManager::ShouldEnable()) {
-    BootstrapSandboxManager* sandbox_manager =
-        BootstrapSandboxManager::GetInstance();
-    if (sandbox_manager->EnabledForSandbox(sandbox_type)) {
-      pre_exec_delegate = sandbox_manager->sandbox()->NewClient(sandbox_type);
-    }
-  }
-  // options now owns the pre_exec_delegate which will be delete on
-  // AfterLaunchOnLauncherThread below.
-  options->pre_exec_delegate = pre_exec_delegate.release();
 }
 
 ChildProcessLauncherHelper::Process
@@ -127,18 +111,9 @@ ChildProcessLauncherHelper::LaunchProcessOnLauncherThread(
 void ChildProcessLauncherHelper::AfterLaunchOnLauncherThread(
     const ChildProcessLauncherHelper::Process& process,
     const base::LaunchOptions& options) {
-  std::unique_ptr<sandbox::PreExecDelegate> pre_exec_delegate =
-    base::WrapUnique(static_cast<sandbox::PreExecDelegate*>(
-        options.pre_exec_delegate));
-
   MachBroker* broker = MachBroker::GetInstance();
   if (process.process.IsValid()) {
     broker->AddPlaceholderForPid(process.process.Pid(), child_process_id());
-  } else {
-    if (pre_exec_delegate) {
-      BootstrapSandboxManager::GetInstance()->sandbox()->RevokeToken(
-          pre_exec_delegate->sandbox_token());
-    }
   }
 
   // After updating the broker, release the lock and let the child's message be
