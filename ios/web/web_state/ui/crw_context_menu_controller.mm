e5dde2f8970e6db0e992ca62e01bd7159c9c4469
Eugene But <eugenebut@google.com>
2017-09-12 18:43:34 +0000

    Do not cancel system long press gesture if it's not a link press.
    Cancelling system long press gesture cancels all system UI (selection
    and context menu). Cancelling system context menu UI is intentional,
    because  Chrome want to show own context menu. Cancelling system
    selection UI was a bug.
    
    Bug: 763985
    Change-Id: I19fb9b7a90948a2ae9151028d4b7bff53e65b0cf
    Reviewed-on: https://chromium-review.googlesource.com/663780
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Reviewed-by: Kurt Horimoto <kkhorimoto@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501343}
    Former-commit-id: fdaaebe87184d49b49de78b006ea79ef49f14afd

--- ios/web/web_state/ui/crw_context_menu_controller.mm
+++ ios/web/web_state/ui/crw_context_menu_controller.mm
@@ -204,7 +204,14 @@ void CancelTouches(UIGestureRecognizer* gesture_recognizer) {
 }
 
 - (void)longPressGestureRecognizerBegan {
-  [self cancelAllTouches];
+  if ([_DOMElementForLastTouch count]) {
+    // User long pressed on a link or an image. Cancelling all touches will
+    // intentionally suppress system context menu UI.
+    [self cancelAllTouches];
+  } else {
+    // There is no link or image under user's gesture. Do not cancel all touches
+    // to allow system text seletion UI.
+  }
 
   if ([_delegate respondsToSelector:@selector(webView:handleContextMenu:)]) {
     _locationForLastTouch = [_contextMenuRecognizer locationInView:_webView];
