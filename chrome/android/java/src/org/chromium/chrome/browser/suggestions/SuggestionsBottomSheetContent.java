6c17be040f3d5e4a49c9897ccf74e3dfa7a3b0cd
Nicolas Dossou-gbete <dgn@chromium.org>
2017-09-12 17:16:28 +0000

    📰 Report suggestions surface opened on every home sheet impression
    Bug: 762549
    Change-Id: Iebd5c5dc7d3c3d3d46c4acb8bb03142413e82515
    Reviewed-on: https://chromium-review.googlesource.com/663341
    Reviewed-by: Bernhard Bauer <bauerb@chromium.org>
    Reviewed-by: Jan Krcal <jkrcal@chromium.org>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Commit-Queue: Nicolas Dossou-Gbété <dgn@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501322}
    Former-commit-id: 5ecebfe76a99bc03df6e2c46e73b6c38151353c3

--- chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
+++ chrome/android/java/src/org/chromium/chrome/browser/suggestions/SuggestionsBottomSheetContent.java
@@ -128,7 +128,6 @@ public class SuggestionsBottomSheetContent implements BottomSheet.BottomSheetCon
             public void onContentShown(boolean isFirstShown) {
                 if (isFirstShown) {
                     adapter.refreshSuggestions();
-                    mSuggestionsUiDelegate.getEventReporter().onSurfaceOpened();
 
                     maybeUpdateContextualSuggestions();
 
@@ -139,6 +138,12 @@ public class SuggestionsBottomSheetContent implements BottomSheet.BottomSheetCon
                     mRecyclerView.scrollToPosition(0);
                     mRecyclerView.getScrollEventReporter().reset();
                 }
+
+                // TODO(dgn): Temporary workaround to trigger an event in the backend when the
+                // sheet is opened following inactivity. See https://crbug.com/760974. Should be
+                // moved back to the "new opening of the sheet" path once we are able to trigger it
+                // in that case.
+                mSuggestionsUiDelegate.getEventReporter().onSurfaceOpened();
 
                 SuggestionsMetrics.recordSurfaceVisible();
             }
