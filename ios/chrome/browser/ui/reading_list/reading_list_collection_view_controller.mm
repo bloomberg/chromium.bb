ded82761865124c7d8cb4af9ab836eac2af44a8e
Gauthier Ambard <gambard@chromium.org>
2017-09-12 10:58:21 +0000

    Don't trigger long press on ReadingList app bar
    When the user long press the app bar displayed at the top of the
    Reading List collection, the context menu corresponding to the item
    displayed below should not be displayed.
    
    Bug: 761988
    Change-Id: Iee203182aa896096875b04b886db0818bf6226ac
    Reviewed-on: https://chromium-review.googlesource.com/660001
    Reviewed-by: Olivier Robin <olivierrobin@chromium.org>
    Commit-Queue: Gauthier Ambard <gambard@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501232}
    Former-commit-id: 2079cb829384606ea06e2fd1956c101ef9da56d9

--- ios/chrome/browser/ui/reading_list/reading_list_collection_view_controller.mm
+++ ios/chrome/browser/ui/reading_list/reading_list_collection_view_controller.mm
@@ -19,6 +19,7 @@
 #import "ios/chrome/browser/ui/reading_list/reading_list_empty_collection_background.h"
 #import "ios/chrome/browser/ui/reading_list/reading_list_toolbar.h"
 #include "ios/chrome/grit/ios_strings.h"
+#import "ios/third_party/material_components_ios/src/components/AppBar/src/MaterialAppBar.h"
 #import "ios/third_party/material_components_ios/src/components/Palettes/src/MaterialPalettes.h"
 #include "ui/base/l10n/l10n_util_mac.h"
 
@@ -45,7 +46,8 @@ typedef void (^EntryUpdater)(CollectionViewItem* item);
 
 @interface ReadingListCollectionViewController ()<
     ReadingListCollectionViewItemAccessibilityDelegate,
-    ReadingListDataSink> {
+    ReadingListDataSink,
+    UIGestureRecognizerDelegate> {
   // Toolbar with the actions.
   ReadingListToolbar* _toolbar;
   // Action sheet presenting the subactions of the toolbar.
@@ -199,7 +201,7 @@ typedef void (^EntryUpdater)(CollectionViewItem* item);
       [[UILongPressGestureRecognizer alloc]
           initWithTarget:self
                   action:@selector(handleLongPress:)];
-  longPressRecognizer.numberOfTouchesRequired = 1;
+  longPressRecognizer.delegate = self;
   [self.collectionView addGestureRecognizer:longPressRecognizer];
 }
 
@@ -224,7 +226,7 @@ typedef void (^EntryUpdater)(CollectionViewItem* item);
   [super collectionView:collectionView didDeselectItemAtIndexPath:indexPath];
   if (self.editor.editing) {
     // When deselecting an item, if we are editing, we want to update the
-    // toolbar base on the selected items.
+    // toolbar based on the selected items.
     [self updateToolbarState];
   }
 }
@@ -359,6 +361,16 @@ typedef void (^EntryUpdater)(CollectionViewItem* item);
     [self markItemsUnreadAtIndexPath:@[ [self.collectionViewModel
                                          indexPathForItem:entry] ]];
   }
+}
+
+#pragma mark - UIGestureRecognizerDelegate
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer*)gestureRecognizer
+       shouldReceiveTouch:(UITouch*)touch {
+  // Prevent the context menu to be displayed if the long press is done on the
+  // App Bar.
+  CGPoint location = [touch locationInView:self.appBar.navigationBar];
+  return !CGRectContainsPoint(self.appBar.navigationBar.frame, location);
 }
 
 #pragma mark - Private methods
