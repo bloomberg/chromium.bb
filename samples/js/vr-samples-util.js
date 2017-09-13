d73e6182287e3c6c0daa4d3d2ea578a31a2e5a6b
Brandon Jones <bajones@chromium.org>
2017-09-13 13:09:38 -0700

    Added "addVRClickListener" utility to replace "click" event handling
    In Chrome (and possibly other implementations too) Cardboard clicks are
    no longer surfaced as actual click events but as a gamepad with a
    single button. This utility listens for presses of the primary button
    from any gamepad (for simplicity) and fires the provided callback in
    response.

--- samples/js/vr-samples-util.js
+++ samples/js/vr-samples-util.js
@@ -170,12 +170,54 @@ window.VRSamplesUtil = (function () {
       document.removeEventListener("keydown", button.keyListener, false);
   }
 
+  function addVRClickListener(clickCallback) {
+    let lastButtonState = [];
+    let presentingDisplay = null;
+
+    // Set up a loop to check gamepad state while any VRDisplay is presenting.
+    function onClickListenerFrame() {
+      // Only reschedule the loop if a display is still presenting.
+      if (presentingDisplay && presentingDisplay.isPresenting) {
+        presentingDisplay.requestAnimationFrame(onClickListenerFrame);
+      }
+      
+      let gamepads = navigator.getGamepads();
+      for (let i = 0; i < gamepads.length; ++i) {
+        let gamepad = gamepads[i];
+        // Ensure the gamepad is valid and has buttons.
+        if (gamepad &&
+            gamepad.buttons.length) {
+          let lastState = lastButtonState[i] || false;
+          let newState = gamepad.buttons[0].pressed;
+          // If the primary button state has changed from not pressed to pressed 
+          // over the last frame then fire the callback.
+          if (newState && !lastState) {
+            clickCallback(gamepad);
+          }
+          lastButtonState[i] = newState;
+        }
+      }
+    }
+
+    window.addEventListener('vrdisplaypresentchange', (event) => {
+      if (event.display.isPresenting) {
+        let scheduleFrame = !presentingDisplay;
+        presentingDisplay = event.display;
+        if (scheduleFrame)
+          onClickListenerFrame();
+      } else if (presentingDisplay == event.display) {
+        presentingDisplay = null;
+      }
+    });
+  }
+
   return {
     isMobile: isMobile,
     addError: addError,
     addInfo: addInfo,
     addButton: addButton,
     removeButton: removeButton,
-    makeToast: makeToast
+    makeToast: makeToast,
+    addVRClickListener: addVRClickListener
   };
 })();
