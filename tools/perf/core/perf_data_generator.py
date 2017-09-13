e430119be80512b8e77aba6c2c94d732c93a0f3d
Stephen Martinis <martiniss@chromium.org>
2017-09-13 01:50:09 +0000

    //tools/perf: Upload more benchmarks to the flakiness dashboard
    Bug: 717394
    Change-Id: I4f3851f290d996746f209760f00ea8668e6091c8
    Reviewed-on: https://chromium-review.googlesource.com/639577
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Reviewed-by: Sean McCullough <seanmccullough@chromium.org>
    Commit-Queue: Stephen Martinis <martiniss@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501508}
    Former-commit-id: e44b98077549c5901abeeb22d372abdb7ba4dbda

--- tools/perf/core/perf_data_generator.py
+++ tools/perf/core/perf_data_generator.py
@@ -606,10 +606,22 @@ def generate_isolate_script_entry(swarming_dimensions, test_args,
   return result
 
 
-BENCHMARKS_TO_UPLOAD_TO_FLAKINESS_DASHBOARD = ['system_health.common_desktop',
-                                               'system_health.common_mobile',
-                                               'system_health.memory_desktop',
-                                               'system_health.memory_mobile']
+# Manually curated for now. System health is in here because it's an important
+# benchmark. Others are semi randomly chosen; they've failed on the waterfall
+# recently, so should be useful to upload.
+BENCHMARKS_TO_UPLOAD_TO_FLAKINESS_DASHBOARD = [
+    'service_worker.service_worker',
+    'smoothness.tough_texture_upload_cases',
+    'smoothness.tough_webgl_ad_cases',
+    'system_health.common_desktop',
+    'system_health.common_mobile',
+    'system_health.memory_desktop',
+    'system_health.memory_mobile',
+    'v8.browsing_mobile',
+    'v8.browsing_desktop',
+    'v8.runtimestats.browsing_mobile',
+    'v8.runtimestats.browsing_desktop',
+]
 
 
 BENCHMARKS_TO_OUTPUT_HISTOGRAMS = []
