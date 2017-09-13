8982b14c178c199c6671cd80f147110d6845f764
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-09-13 01:36:27 +0000

    [Extensions] Re-enable AppApiTest.AppProcessBackgroundInstances
    This test was disabled for being flaky on XP.  XP is dead.  The test now
    passes 100 runs on Windows.
    
    Bug: 165834
    
    Change-Id: I682cf656065e7006bb947aa7b04dc57d949ac516
    Reviewed-on: https://chromium-review.googlesource.com/664077
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501505}
    Former-commit-id: 3218216702491cb76e071aebf5dba56c6b730bb9

--- chrome/browser/extensions/app_process_apitest.cc
+++ chrome/browser/extensions/app_process_apitest.cc
@@ -273,14 +273,7 @@ IN_PROC_BROWSER_TEST_F(AppApiTest, MAYBE_AppProcessInstances) {
 // Test that hosted apps with the background permission but that set
 // allow_js_access to false also use a process per app instance model.
 // Separate instances should be in separate processes.
-// Flaky on XP: http://crbug.com/165834
-#if defined(OS_WIN)
-#define MAYBE_AppProcessBackgroundInstances \
-    DISABLED_AppProcessBackgroundInstances
-#else
-#define MAYBE_AppProcessBackgroundInstances AppProcessBackgroundInstances
-#endif
-IN_PROC_BROWSER_TEST_F(AppApiTest, MAYBE_AppProcessBackgroundInstances) {
+IN_PROC_BROWSER_TEST_F(AppApiTest, AppProcessBackgroundInstances) {
   TestAppInstancesHelper("app_process_background_instances");
 }
 
