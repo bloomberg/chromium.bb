6d924c36819de8e2b16a393a688ef4344e740944
Kevin Cernekee <cernekee@chromium.org>
2017-09-13 02:29:02 +0000

    Hide UI activity for ARC VPNs
    If a VPN connection is being managed by the OS, we do not want to let
    the user connect manually, manipulate its settings, delete it, or get
    a notification when it disconnects.
    
    BUG=696865
    TEST=manually create an arcvpn and play around with the UI
    
    Change-Id: I6853df43b8e1cc3896c277c761f8de01c498ab99
    Reviewed-on: https://chromium-review.googlesource.com/592544
    Commit-Queue: Kevin Cernekee <cernekee@chromium.org>
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501526}
    Former-commit-id: 4d83ac596e4f77cbcc7892c6d962442775a57838

--- chrome/browser/chromeos/net/network_state_notifier.cc
+++ chrome/browser/chromeos/net/network_state_notifier.cc
@@ -206,7 +206,9 @@ bool NetworkStateNotifier::UpdateDefaultNetwork(const NetworkState* network) {
 void NetworkStateNotifier::UpdateVpnConnectionState(const NetworkState* vpn) {
   if (vpn->path() == connected_vpn_) {
     if (!vpn->IsConnectedState() && !vpn->IsConnectingState()) {
-      ShowVpnDisconnectedNotification(vpn);
+      if (vpn->vpn_provider_type() != shill::kProviderArcVpn) {
+        ShowVpnDisconnectedNotification(vpn);
+      }
       connected_vpn_.clear();
     }
   } else if (vpn->IsConnectedState()) {
