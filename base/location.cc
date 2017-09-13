594b181f83e57fb8ecaf0180e40338b8baed45b1
tzik <tzik@chromium.org>
2017-09-13 00:23:16 +0000

    Take program counter for FROM_HERE in its static constructor
    Before this CL, the FROM_HERE macro directly called GetProgramCounter()
    and passed the value to the Location constructor. However, that costs
    a few bytes for each FROM_HERE invocation.
    
    This CL moves the program counter calculation into a static constructor
    of Location for smaller binary size.
    
    This reduces the stripped binary size on x86_64 Linux by 36864 bytes.
    
    Change-Id: Ic49550abb4bf1e4e29756c7fc876ddfd85b5d3b2
    Reviewed-on: https://chromium-review.googlesource.com/662086
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501475}
    Former-commit-id: 4f05a25a4775dab4e8b9a26ee80b278c5bd67beb

--- base/location.cc
+++ base/location.cc
@@ -2,15 +2,16 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "build/build_config.h"
+#include "base/location.h"
 
 #if defined(COMPILER_MSVC)
 #include <intrin.h>
 #endif
 
-#include "base/location.h"
+#include "base/compiler_specific.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/stringprintf.h"
+#include "build/build_config.h"
 
 namespace base {
 
@@ -77,18 +78,30 @@ LocationSnapshot::LocationSnapshot(const Location& location)
 
 LocationSnapshot::~LocationSnapshot() = default;
 
-//------------------------------------------------------------------------------
 #if defined(COMPILER_MSVC)
-__declspec(noinline)
-#endif
-BASE_EXPORT const void* GetProgramCounter() {
-#if defined(COMPILER_MSVC)
-  return _ReturnAddress();
+#define RETURN_ADDRESS() _ReturnAddress()
 #elif defined(COMPILER_GCC) && !defined(OS_NACL)
-  return __builtin_extract_return_addr(__builtin_return_address(0));
+#define RETURN_ADDRESS() \
+  __builtin_extract_return_addr(__builtin_return_address(0))
 #else
-  return nullptr;
+#define RETURN_ADDRESS() nullptr
 #endif
+
+// static
+NOINLINE Location Location::CreateFromHere(const char* file_name) {
+  return Location(file_name, RETURN_ADDRESS());
+}
+
+// static
+NOINLINE Location Location::CreateFromHere(const char* function_name,
+                                           const char* file_name,
+                                           int line_number) {
+  return Location(function_name, file_name, line_number, RETURN_ADDRESS());
+}
+
+//------------------------------------------------------------------------------
+NOINLINE const void* GetProgramCounter() {
+  return RETURN_ADDRESS();
 }
 
 }  // namespace base
