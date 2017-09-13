f5f4020a4f3e016e383e5c5723edf1d41293a1ab
Xing Liu <xingliu@chromium.org>
2017-09-13 16:54:59 +0000

    Add more response header handling logic in DownloadResponseHandler.
    This CL moves some download http response header handling logic from
    download_request_core.cc to the new class used by the new mojo url
    loader data pipe.
    
    
    Bug: 715630
    Change-Id: I90d0618140009a047d7119d17bd2b0cf8d5329d7
    Reviewed-on: https://chromium-review.googlesource.com/661145
    Commit-Queue: Xing Liu <xingliu@chromium.org>
    Reviewed-by: Min Qin <qinmin@chromium.org>
    Reviewed-by: David Trainor <dtrainor@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501664}
    Former-commit-id: 78d13fd19c25b9daf22936929095adce17ac3fa1

--- content/browser/download/download_request_core.cc
+++ content/browser/download/download_request_core.cc
@@ -236,14 +236,6 @@ bool DownloadRequestCore::OnResponseStarted(
   // with main frames.
   request()->SetPriority(net::IDLE);
 
-  // If the content-length header is not present (or contains something other
-  // than numbers), the incoming content_length is -1 (unknown size).
-  // Set the content length to 0 to indicate unknown size to DownloadManager.
-  int64_t content_length = request()->GetExpectedContentSize() > 0
-                               ? request()->GetExpectedContentSize()
-                               : 0;
-  create_info->total_bytes = content_length;
-
   // Create the ByteStream for sending data to the download sink.
   std::unique_ptr<ByteStreamReader> stream_reader;
   CreateByteStream(base::ThreadTaskRunnerHandle::Get(), GetDownloadTaskRunner(),
@@ -258,35 +250,7 @@ bool DownloadRequestCore::OnResponseStarted(
 
   // Get the last modified time and etag.
   const net::HttpResponseHeaders* headers = request()->response_headers();
-  if (headers) {
-    if (headers->HasStrongValidators()) {
-      // If we don't have strong validators as per RFC 7232 section 2, then
-      // we neither store nor use them for range requests.
-      if (!headers->EnumerateHeader(nullptr, "Last-Modified",
-                                    &create_info->last_modified))
-        create_info->last_modified.clear();
-      if (!headers->EnumerateHeader(nullptr, "ETag", &create_info->etag))
-        create_info->etag.clear();
-    }
-
-    // Grab the first content-disposition header.  There may be more than one,
-    // though as of this writing, the network stack ensures if there are, they
-    // are all duplicates.
-    headers->EnumerateHeader(nullptr, "Content-Disposition",
-                             &create_info->content_disposition);
-
-    if (!headers->GetMimeType(&create_info->original_mime_type))
-      create_info->original_mime_type.clear();
-
-    // Content-Range is validated in HandleSuccessfulServerResponse.
-    // In RFC 7233, a single part 206 partial response must generate
-    // Content-Range. Accept-Range may be sent in 200 response to indicate the
-    // server can handle range request, but optional in 206 response.
-    create_info->accept_range =
-        headers->HasHeaderValue("Accept-Ranges", "bytes") ||
-        (headers->HasHeader("Content-Range") &&
-         headers->response_code() == net::HTTP_PARTIAL_CONTENT);
-  }
+  HandleResponseHeaders(headers, create_info.get());
 
   // GURL::GetOrigin() doesn't support getting the inner origin of a blob URL.
   // However, requesting a cross origin blob URL would have resulted in a
--- content/browser/download/download_utils.h
+++ content/browser/download/download_utils.h
@@ -18,6 +18,7 @@ namespace content {
 
 class DownloadUrlParameters;
 struct ResourceRequest;
+struct DownloadCreateInfo;
 struct DownloadSaveInfo;
 
 // Handle the url request completion status and return the interrupt reasons.
@@ -37,6 +38,11 @@ std::unique_ptr<net::URLRequest> CONTENT_EXPORT CreateURLRequestOnIOThread(
 DownloadInterruptReason CONTENT_EXPORT
 HandleSuccessfulServerResponse(const net::HttpResponseHeaders& http_headers,
                                DownloadSaveInfo* save_info);
+
+// Parse response headers and update |create_info| accordingly.
+CONTENT_EXPORT void HandleResponseHeaders(
+    const net::HttpResponseHeaders* headers,
+    DownloadCreateInfo* create_info);
 
 }  // namespace content
 
