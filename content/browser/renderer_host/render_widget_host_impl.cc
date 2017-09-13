8e79f6bbf64e47030e62a1cb8849f546d0a31444
Jaebaek Seo <jaebaek@chromium.org>
2017-09-13 00:31:00 +0000

    Reland "Scale heights of BrowserControls in compositor by DSF"
    This is a reland of 0813cf0920664ddf98d229b1f4f091e276eb99f8 [formerly 030794ca2b2e0ed6b16b2130c952a8adefa8222f]
    with deviceScaleFactor obtained from global device to avoid
    MSAN problem.
    
    Original change's description:
    > Scale heights of BrowserControls in compositor by DSF
    > 
    > Bug: 737777
    > Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    > Change-Id: I711e05e8747850041f2c799d3b482da34e7e520d
    > Reviewed-on: https://chromium-review.googlesource.com/645906
    > Commit-Queue: Jaebaek Seo <jaebaek@chromium.org>
    > Reviewed-by: Alexandre Elias <aelias@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#500841}
    
    Bug: 737777
    Change-Id: I6a27221ce5baac8573e0f074895a657eaf21183f
    Cq-Include-Trybots: master.tryserver.blink:linux_trusty_blink_rel
    Reviewed-on: https://chromium-review.googlesource.com/662500
    Reviewed-by: Alexandre Elias <aelias@chromium.org>
    Commit-Queue: Jaebaek Seo <jaebaek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501482}
    Former-commit-id: bfe88542d3e3fd92c64c281207b46f13ab4a2513

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -90,6 +90,7 @@
 #include "ui/base/clipboard/clipboard.h"
 #include "ui/base/ui_base_switches.h"
 #include "ui/display/display_switches.h"
+#include "ui/display/screen.h"
 #include "ui/events/blink/web_input_event_traits.h"
 #include "ui/events/event.h"
 #include "ui/events/keycodes/keyboard_codes.h"
@@ -755,9 +756,18 @@ bool RenderWidgetHostImpl::GetResizeParams(ResizeParams* resize_params) {
     // ScaleToCeiledSize(new_size, device_scale_factor) ??
     resize_params->physical_backing_size = view_->GetPhysicalBackingSize();
     resize_params->top_controls_height = view_->GetTopControlsHeight();
+    resize_params->bottom_controls_height = view_->GetBottomControlsHeight();
+    if (IsUseZoomForDSFEnabled() && display::Screen::GetScreen()) {
+      const display::Display& display =
+          display::Screen::GetScreen()->GetPrimaryDisplay();
+      if (display.id() != display::kInvalidDisplayId) {
+        float device_scale = display.device_scale_factor();
+        resize_params->top_controls_height *= device_scale;
+        resize_params->bottom_controls_height *= device_scale;
+      }
+    }
     resize_params->browser_controls_shrink_blink_size =
         view_->DoBrowserControlsShrinkBlinkSize();
-    resize_params->bottom_controls_height = view_->GetBottomControlsHeight();
     resize_params->visible_viewport_size = view_->GetVisibleViewportSize();
     viz::LocalSurfaceId local_surface_id = view_->GetLocalSurfaceId();
     if (local_surface_id.is_valid())
