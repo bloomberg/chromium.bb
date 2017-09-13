71b0513bf50978e9de5418ebea30a706ac5b8b6b
Nathan E. Egge <negge@mozilla.com>
2017-09-13 06:47:24 -0400

    Fix highbd_iht_test with 4, 8, and 16 daala_tx.
    This change fixes a compile error when all three of --enable-daala_dct4,
     --enable-daala_dct8 and --enable-daala_dct16 are enabled at once.
    
    Change-Id: I4942e09fb887afbda2eda6aaacec727b5cbf6f50

--- test/av1_highbd_iht_test.cc
+++ test/av1_highbd_iht_test.cc
@@ -135,7 +135,8 @@ TEST_P(AV1HighbdInvHTNxN, InvTransResultCheck) { RunBitexactCheck(); }
 
 using std::tr1::make_tuple;
 
-#if HAVE_SSE4_1 && CONFIG_HIGHBITDEPTH
+#if HAVE_SSE4_1 && CONFIG_HIGHBITDEPTH && \
+    !(CONFIG_DAALA_DCT4 && CONFIG_DAALA_DCT8 && CONFIG_DAALA_DCT16)
 #if !CONFIG_DAALA_DCT4
 #define PARAM_LIST_4X4                                   \
   &av1_fwd_txfm2d_4x4_c, &av1_inv_txfm2d_add_4x4_sse4_1, \
@@ -225,7 +226,8 @@ const IHbdHtParam kArrayIhtParam[] = {
 
 INSTANTIATE_TEST_CASE_P(SSE4_1, AV1HighbdInvHTNxN,
                         ::testing::ValuesIn(kArrayIhtParam));
-#endif  // HAVE_SSE4_1 && CONFIG_HIGHBITDEPTH
+#endif  // HAVE_SSE4_1 && CONFIG_HIGHBITDEPTH &&
+        //  !(CONFIG_DAALA_DCT4 && CONFIG_DAALA_DCT8 && CONFIG_DAALA_DCT16)
 
 #if HAVE_AVX2 && CONFIG_HIGHBITDEPTH && !CONFIG_DAALA_DCT32
 #define PARAM_LIST_32X32                                   \
