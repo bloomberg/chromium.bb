d87e2583564609e726260688232ae1d0c256c50e
rnephew <rnephew@chromium.org>
2017-09-13 21:58:29 +0000

    [Telemetry] Disable 2 smoothness.key_silk_cases stories that are failing.
    Unexpected Failures:
    * smoothness.key_silk_cases/inbox_app.html?swipe_to_dismiss
    * smoothness.key_silk_cases/masonry.html
    
    Bug: 764825
    Change-Id: I1cc38467f1c50067d692c18471b6466209e6d8bb
    Reviewed-on: https://chromium-review.googlesource.com/665392
    Commit-Queue: rnephew <rnephew@chromium.org>
    Commit-Queue: Tien-Ren Chen <trchen@chromium.org>
    Reviewed-by: Tien-Ren Chen <trchen@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#501766}
    Former-commit-id: 215ac0369f32bc5ced186a01b0f2ba007b09116c

--- tools/perf/benchmarks/smoothness.py
+++ tools/perf/benchmarks/smoothness.py
@@ -267,6 +267,10 @@ class SmoothnessKeySilkCases(_Smoothness):
                           [story_module.expectations.ALL], 'crbug.com/338838')
         self.DisableStory('inbox_app.html?slide_drawer',
                           [story_module.expectations.ALL], 'crbug.com/446332')
+        self.DisableStory('inbox_app.html?swipe_to_dismiss',
+                          [story_module.expectations.ALL], 'crbug.com/764825')
+        self.DisableStory('masonry.html',
+                          [story_module.expectations.ALL], 'crbug.com/764825')
     return StoryExpectations()
 
 
