94e932afac0a9daa373a013f8273b5253062a504
Xiaocheng Hu <xiaochengh@chromium.org>
2017-09-13 07:34:45 +0000

    Reland "Stop TextIterator from emit double newlines after H4~6 elements"
    The original patch caused buildbot failures because it didn't rebaseline
    the newly added wpt innerText getter tests. This is fixed in this one.
    
    Original change's description:
    > Stop TextIterator from emit double newlines after H4~6 elements
    >
    > TextIterator currently may add two consecutive newlines after an
    > H4, H5 or H6 element, while the spec of innerText doesn't have
    > this case at all.
    >
    > This patch removes the extra newline to make TextIterator more
    > spec-complicant.
    >
    > Note: TextIterator should eventually remove double newline emission
    > also for H1~3. This patch removes it only for H4~6 to keep the
    > amount of rebaselined tests small.
    >
    > Bug: 752672,758816
    > Change-Id: I32583c194a829f52411555125e77590e36ccf838
    > Cq-Include-Trybots: master.tryserver.chromium.linux:linux_layout_tests_layout_ng
    > Reviewed-on: https://chromium-review.googlesource.com/661937
    > Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    > Reviewed-by: Koji Ishii <kojii@chromium.org>
    > Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#501297}
    
    TBR=yosin@chromium.org,kojii@chromium.org,xiaochengh@chromium.org,simonp@opera.com
    
    Bug: 752672,758816
    Change-Id: I9f6414635d257a309eca0f7f28882c7ebdc90e94
    Reviewed-on: https://chromium-review.googlesource.com/664122
    Commit-Queue: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501563}
    Former-commit-id: 2925e1d42d5bc7a91c98a7239424104a43672c02

--- third_party/WebKit/Source/core/editing/iterators/TextIterator.cpp
+++ third_party/WebKit/Source/core/editing/iterators/TextIterator.cpp
@@ -657,8 +657,7 @@ static bool ShouldEmitExtraNewlineForNode(Node* node) {
     return true;
 
   if (node->HasTagName(h1Tag) || node->HasTagName(h2Tag) ||
-      node->HasTagName(h3Tag) || node->HasTagName(h4Tag) ||
-      node->HasTagName(h5Tag) || node->HasTagName(h6Tag)) {
+      node->HasTagName(h3Tag)) {
     const ComputedStyle* style = r->Style();
     if (style) {
       int bottom_margin = ToLayoutBox(r)->CollapsedMarginAfter().ToInt();
