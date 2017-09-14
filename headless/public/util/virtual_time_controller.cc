032c231d65e48042be3d424d38ba3cd05fa66162
Eric Seckler <eseckler@chromium.org>
2017-09-14 17:14:14 +0000

    headless: Add VirtualTimeController.
    The VirtualTimeController allows interleaving of repeating tasks with
    the progression of virtual time.
    
    Bug: none
    Change-Id: Ibd81b75e58a83887c22e4555202913f8dfbfc81b
    Reviewed-on: https://chromium-review.googlesource.com/663719
    Commit-Queue: Eric Seckler <eseckler@chromium.org>
    Reviewed-by: Alex Clarke <alexclarke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501974}
    Former-commit-id: d304d42b1b2efe3167829c68383f0e4bcaef3b59

---
+++ headless/public/util/virtual_time_controller.cc
@@ -0,0 +1,162 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "headless/public/util/virtual_time_controller.h"
+
+#include "base/auto_reset.h"
+#include "base/bind.h"
+#include "base/logging.h"
+
+namespace headless {
+
+using base::TimeDelta;
+
+VirtualTimeController::VirtualTimeController(
+    HeadlessDevToolsClient* devtools_client)
+    : devtools_client_(devtools_client) {
+  devtools_client_->GetEmulation()->GetExperimental()->AddObserver(this);
+}
+
+VirtualTimeController::~VirtualTimeController() {
+  devtools_client_->GetEmulation()->GetExperimental()->RemoveObserver(this);
+}
+
+void VirtualTimeController::GrantVirtualTimeBudget(
+    emulation::VirtualTimePolicy policy,
+    int budget_ms,
+    const base::Callback<void()>& set_up_complete_callback,
+    const base::Callback<void()>& budget_expired_callback) {
+  DCHECK(!set_up_complete_callback_);
+  DCHECK(!budget_expired_callback_);
+
+  set_up_complete_callback_ = set_up_complete_callback;
+  budget_expired_callback_ = budget_expired_callback;
+  requested_budget_ = TimeDelta::FromMilliseconds(budget_ms);
+  accumulated_time_ = TimeDelta();
+  virtual_time_policy_ = policy;
+
+  // Notify tasks of new budget request.
+  for (TaskEntry& entry : tasks_)
+    NotifyTaskBudgetRequested(&entry, budget_ms);
+
+  // If there tasks, NotifyTasksAndAdvance is called from TaskReadyToAdvance.
+  if (tasks_.empty())
+    NotifyTasksAndAdvance();
+}
+
+void VirtualTimeController::NotifyTasksAndAdvance() {
+  // The task may call its continue callback synchronously. Prevent re-entrance.
+  if (in_notify_tasks_and_advance_)
+    return;
+
+  base::AutoReset<bool> reset(&in_notify_tasks_and_advance_, true);
+
+  DCHECK(budget_expired_callback_);
+
+  // Give at most as much virtual time as available until the next callback.
+  bool ready_to_advance = true;
+  TimeDelta next_budget = requested_budget_ - accumulated_time_;
+  for (TaskEntry& entry : tasks_) {
+    if (entry.next_execution_time <= total_elapsed_time_)
+      NotifyTaskIntervalElapsed(&entry);
+
+    ready_to_advance &= entry.ready_to_advance;
+    next_budget =
+        std::min(next_budget, entry.next_execution_time - total_elapsed_time_);
+  }
+
+  if (!ready_to_advance)
+    return;
+
+  if (accumulated_time_ >= requested_budget_) {
+    for (const TaskEntry& entry : tasks_)
+      entry.task->BudgetExpired(total_elapsed_time_);
+
+    auto callback = budget_expired_callback_;
+    budget_expired_callback_.Reset();
+    callback.Run();
+    return;
+  }
+
+  SetVirtualTimePolicy(next_budget);
+}
+
+void VirtualTimeController::NotifyTaskIntervalElapsed(TaskEntry* entry) {
+  entry->ready_to_advance = false;
+  entry->next_execution_time = total_elapsed_time_ + entry->interval;
+
+  entry->task->IntervalElapsed(
+      total_elapsed_time_,
+      base::Bind(&VirtualTimeController::TaskReadyToAdvance,
+                 base::Unretained(this), base::Unretained(entry)));
+}
+
+void VirtualTimeController::NotifyTaskBudgetRequested(TaskEntry* entry,
+                                                      int budget_ms) {
+  entry->ready_to_advance = false;
+  entry->task->BudgetRequested(
+      total_elapsed_time_, budget_ms,
+      base::Bind(&VirtualTimeController::TaskReadyToAdvance,
+                 base::Unretained(this), base::Unretained(entry)));
+}
+
+void VirtualTimeController::TaskReadyToAdvance(TaskEntry* entry) {
+  entry->ready_to_advance = true;
+  NotifyTasksAndAdvance();
+}
+
+void VirtualTimeController::SetVirtualTimePolicy(const TimeDelta& next_budget) {
+  last_used_budget_ = next_budget;
+  virtual_time_active_ = true;
+  devtools_client_->GetEmulation()->GetExperimental()->SetVirtualTimePolicy(
+      emulation::SetVirtualTimePolicyParams::Builder()
+          .SetPolicy(virtual_time_policy_)
+          .SetBudget(next_budget.InMilliseconds())
+          .Build(),
+      base::Bind(&VirtualTimeController::SetVirtualTimePolicyDone,
+                 base::Unretained(this)));
+}
+
+void VirtualTimeController::SetVirtualTimePolicyDone(
+    std::unique_ptr<emulation::SetVirtualTimePolicyResult>) {
+  if (set_up_complete_callback_) {
+    set_up_complete_callback_.Run();
+    set_up_complete_callback_.Reset();
+  }
+}
+
+void VirtualTimeController::OnVirtualTimeBudgetExpired(
+    const emulation::VirtualTimeBudgetExpiredParams& params) {
+  // As the DevTools script may interfere with virtual time directly, we cannot
+  // assume that this event is due to AdvanceVirtualTime().
+  if (!budget_expired_callback_)
+    return;
+
+  accumulated_time_ += last_used_budget_;
+  total_elapsed_time_ += last_used_budget_;
+  virtual_time_active_ = false;
+  NotifyTasksAndAdvance();
+}
+
+void VirtualTimeController::ScheduleRepeatingTask(RepeatingTask* task,
+                                                  int interval_ms) {
+  if (virtual_time_active_) {
+    // We cannot accurately modify any previously granted virtual time budget.
+    LOG(WARNING) << "VirtualTimeController tasks should be added while "
+                    "virtual time is paused.";
+  }
+
+  TaskEntry entry;
+  entry.task = task;
+  entry.interval = TimeDelta::FromMilliseconds(interval_ms);
+  entry.next_execution_time = total_elapsed_time_ + entry.interval;
+  tasks_.push_back(entry);
+}
+
+void VirtualTimeController::CancelRepeatingTask(RepeatingTask* task) {
+  tasks_.remove_if(
+      [task](const TaskEntry& entry) { return entry.task == task; });
+}
+
+}  // namespace headless
---
+++ headless/public/util/virtual_time_controller.h
@@ -0,0 +1,115 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef HEADLESS_PUBLIC_UTIL_VIRTUAL_TIME_CONTROLLER_H_
+#define HEADLESS_PUBLIC_UTIL_VIRTUAL_TIME_CONTROLLER_H_
+
+#include "base/callback.h"
+#include "base/time/time.h"
+#include "headless/public/devtools/domains/emulation.h"
+#include "headless/public/headless_devtools_client.h"
+#include "headless/public/headless_export.h"
+
+namespace headless {
+
+// Controls how virtual time progresses. RepeatingTasks can register their
+// interest to be periodically notified about changes to the current virtual
+// time.
+class HEADLESS_EXPORT VirtualTimeController
+    : public emulation::ExperimentalObserver {
+ public:
+  explicit VirtualTimeController(HeadlessDevToolsClient* devtools_client);
+  ~VirtualTimeController() override;
+
+  // Grants |budget_ms| milliseconds of virtual time by applying the provided
+  // |policy|.
+  //
+  // |set_up_complete_callback|, if set, is run after the (initial) policy was
+  // applied via DevTools. |budget_expired_callback| will be called when the
+  // budget has been exhausted.
+  //
+  // Should not be called again until the previous invocation's
+  // budget_expired_callback was executed.
+  void GrantVirtualTimeBudget(
+      emulation::VirtualTimePolicy policy,
+      int budget_ms,
+      const base::Callback<void()>& set_up_complete_callback,
+      const base::Callback<void()>& budget_expired_callback);
+
+  class RepeatingTask {
+   public:
+    virtual ~RepeatingTask() {}
+
+    // Called when the tasks's requested virtual time interval has elapsed.
+    // |virtual_time| is the virtual time duration that has advanced since the
+    // page started loading (millisecond granularity). The task should call
+    // |continue_callback| when it is ready for virtual time to continue
+    // advancing.
+    virtual void IntervalElapsed(
+        const base::TimeDelta& virtual_time,
+        const base::Callback<void()>& continue_callback) = 0;
+
+    // Called when a new virtual time budget grant was requested. The task
+    // should call |continue_callback| when it is ready for virtual time to
+    // continue advancing.
+    virtual void BudgetRequested(
+        const base::TimeDelta& virtual_time,
+        int requested_budget_ms,
+        const base::Callback<void()>& continue_callback) = 0;
+
+    // Called when the latest virtual time budget has been used up.
+    virtual void BudgetExpired(const base::TimeDelta& virtual_time) = 0;
+  };
+
+  // Interleaves execution of the provided |task| with advancing of virtual
+  // time. The task will be notified whenever another |interval_ms| milliseconds
+  // of virtual time have elapsed, as well as when the last granted budget has
+  // been used up.
+  //
+  // To ensure that the task is notified of elapsed intervals accurately, it
+  // should be added while virtual time is paused.
+  void ScheduleRepeatingTask(RepeatingTask* task, int interval_ms);
+  void CancelRepeatingTask(RepeatingTask* task);
+
+ private:
+  struct TaskEntry {
+    RepeatingTask* task;
+    base::TimeDelta interval;
+    base::TimeDelta next_execution_time;
+    bool ready_to_advance = true;
+  };
+
+  // emulation::Observer implementation:
+  void OnVirtualTimeBudgetExpired(
+      const emulation::VirtualTimeBudgetExpiredParams& params) override;
+
+  void NotifyTasksAndAdvance();
+  void NotifyTaskIntervalElapsed(TaskEntry* entry);
+  void NotifyTaskBudgetRequested(TaskEntry* entry, int budget_ms);
+  void TaskReadyToAdvance(TaskEntry* entry);
+
+  void SetVirtualTimePolicy(const base::TimeDelta& next_budget);
+  void SetVirtualTimePolicyDone(
+      std::unique_ptr<emulation::SetVirtualTimePolicyResult>);
+
+  HeadlessDevToolsClient* devtools_client_;  // NOT OWNED
+
+  emulation::VirtualTimePolicy virtual_time_policy_ =
+      emulation::VirtualTimePolicy::ADVANCE;
+  base::Callback<void()> set_up_complete_callback_;
+  base::Callback<void()> budget_expired_callback_;
+
+  bool virtual_time_active_ = false;
+  base::TimeDelta total_elapsed_time_;
+  base::TimeDelta requested_budget_;
+  base::TimeDelta last_used_budget_;
+  base::TimeDelta accumulated_time_;
+
+  std::list<TaskEntry> tasks_;
+  bool in_notify_tasks_and_advance_ = false;
+};
+
+}  // namespace headless
+
+#endif  // HEADLESS_PUBLIC_UTIL_VIRTUAL_TIME_CONTROLLER_H_
