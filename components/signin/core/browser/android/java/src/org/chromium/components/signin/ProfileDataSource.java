5a109a910acb84373f59ee2d4f9ebc9c30370ae1
Boris Sazonov <bsazonov@chromium.org>
2017-09-14 13:31:10 +0000

    [Android] Remove deprecated ProfileDataSource.ProfileData ctor
    This CL removes deprecated ProfileDataSource.ProfileData constructor
    and adds a getter for the account name.
    
    Bug: 746519
    Change-Id: Ie1baff3e3f422f541c03bbdee522eb0c4404665e
    Reviewed-on: https://chromium-review.googlesource.com/664638
    Reviewed-by: Mihai Sardarescu <msarda@chromium.org>
    Commit-Queue: Boris Sazonov <bsazonov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501940}
    Former-commit-id: a00630bd6b81fbe1bc63b887a57b38390f4dd4b8

--- components/signin/core/browser/android/java/src/org/chromium/components/signin/ProfileDataSource.java
+++ components/signin/core/browser/android/java/src/org/chromium/components/signin/ProfileDataSource.java
@@ -17,25 +17,26 @@ public interface ProfileDataSource {
      * Immutable holder for profile data.
      */
     class ProfileData {
-        // TODO(bsazonov): remove @Nullable after removing ctor without this parameter.
-        private final @Nullable String mAccountName;
-
+        private final String mAccountName;
         private final @Nullable Bitmap mAvatar;
         private final @Nullable String mFullName;
         private final @Nullable String mGivenName;
 
-        @Deprecated
-        public ProfileData(
-                @Nullable Bitmap avatar, @Nullable String fullName, @Nullable String givenName) {
-            this(null, avatar, fullName, givenName);
-        }
-
         public ProfileData(String accountName, @Nullable Bitmap avatar, @Nullable String fullName,
                 @Nullable String givenName) {
+            assert accountName != null;
             this.mAccountName = accountName;
             this.mAvatar = avatar;
             this.mFullName = fullName;
             this.mGivenName = givenName;
+        }
+
+        /**
+         * Gets the account email address.
+         * @return the account name.
+         */
+        public String getAccountName() {
+            return mAccountName;
         }
 
         /**
