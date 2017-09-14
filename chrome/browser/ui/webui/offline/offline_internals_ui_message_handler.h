96b93122e84f41ae0545db6eb0bcc66c35043ba4
Justin DeWitt <dewittj@chromium.org>
2017-09-14 19:40:29 +0000

    [Offline Pages] chrome://offline-internals should use the prefetch system.
    This change makes the prefetching buttons use the dispatcher instead of
    making raw network requests themselves.
    
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I4b4bc3137366767f580052eea1b24a1cc3dc7e41
    Reviewed-on: https://chromium-review.googlesource.com/649392
    Commit-Queue: Justin DeWitt <dewittj@chromium.org>
    Reviewed-by: Cathy Li <chili@chromium.org>
    Reviewed-by: Tommy Li <tommycli@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502016}
    Former-commit-id: fa22b6cb35a9ed320ac39f77baa69822ce781764

--- chrome/browser/ui/webui/offline/offline_internals_ui_message_handler.h
+++ chrome/browser/ui/webui/offline/offline_internals_ui_message_handler.h
@@ -21,8 +21,6 @@
 namespace offline_pages {
 class PrefetchService;
 enum class GetRequestsResult;
-class GeneratePageBundleRequest;
-class GetOperationRequest;
 }
 
 namespace offline_internals {
@@ -35,6 +33,7 @@ class OfflineInternalsUIMessageHandler : public content::WebUIMessageHandler {
 
   // WebUIMessageHandler implementation.
   void RegisterMessages() override;
+  void OnJavascriptDisallowed() override;
 
  private:
   // Delete selected list of page ids from the store.
@@ -105,13 +104,6 @@ class OfflineInternalsUIMessageHandler : public content::WebUIMessageHandler {
       std::string callback_id,
       const offline_pages::MultipleItemStatuses& results);
 
-  // Callback for GeneratePageBundle/GetOperation request calls.
-  void HandlePrefetchRequestCallback(
-      std::string callback_id,
-      offline_pages::PrefetchRequestStatus status,
-      const std::string& operation_name,
-      const std::vector<offline_pages::RenderPageInfo>& pages);
-
   // Offline page model to call methods on.
   offline_pages::OfflinePageModel* offline_page_model_;
 
@@ -120,10 +112,6 @@ class OfflineInternalsUIMessageHandler : public content::WebUIMessageHandler {
 
   // Prefetch service for prefetching service logs and actions.
   offline_pages::PrefetchService* prefetch_service_;
-
-  std::unique_ptr<offline_pages::GeneratePageBundleRequest>
-      generate_page_bundle_request_;
-  std::unique_ptr<offline_pages::GetOperationRequest> get_operation_request_;
 
   // Factory for creating references in callbacks.
   base::WeakPtrFactory<OfflineInternalsUIMessageHandler> weak_ptr_factory_;
