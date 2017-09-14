28b6b8a35fe5ece249e2a8def985c79d23688ba1
Robert Liao <robliao@chromium.org>
2017-09-14 01:24:28 +0000

    Experimental Fullscreen Prototype Refinements
    * Changed the button into a circle to see how this feels.
    * Moved from ET_GESTURE_LONG_PRESS to ET_GESTURE_LONG_TAP to sync with
      the context menu show and reduce the chance of accidentally
      dismissing the view. It is possible to long press and then
      accidentally trigger a move, accidentally hiding the button.
    * Require symmetric show and hide input methods: A touch show can only
      be dismissed by a touch hide. This makes the view robust to spurious
      mouse events from a touch event.
    
    BUG=710991
    
    Change-Id: Ib2593e9b5e79d9d9334b11ea60990500f6cd89ca
    Reviewed-on: https://chromium-review.googlesource.com/665599
    Commit-Queue: Robert Liao <robliao@chromium.org>
    Reviewed-by: Peter Kasting <pkasting@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501830}
    Former-commit-id: da6615f6582858b679af4016153546adefa300ef

--- chrome/browser/ui/views/fullscreen_control/fullscreen_control_view.cc
+++ chrome/browser/ui/views/fullscreen_control/fullscreen_control_view.cc
@@ -6,37 +6,52 @@
 
 #include <memory>
 
-#include "base/memory/ptr_util.h"
-#include "chrome/grit/generated_resources.h"
+#include "cc/paint/paint_flags.h"
 #include "third_party/skia/include/core/SkColor.h"
-#include "ui/base/l10n/l10n_util.h"
+#include "ui/gfx/canvas.h"
 #include "ui/gfx/geometry/insets.h"
+#include "ui/gfx/geometry/point_f.h"
+#include "ui/gfx/geometry/size.h"
 #include "ui/views/background.h"
-#include "ui/views/controls/button/label_button.h"
+#include "ui/views/controls/button/button.h"
 #include "ui/views/layout/box_layout.h"
 
 namespace {
 
-// Spacing applied to all sides of the border of the control.
-constexpr int kSpacingInsetsAllSides = 10;
+constexpr int kCircleButtonDiameter = 48;
 
-// Spacing applied between controls of FullscreenControlView.
-constexpr int kBetweenChildSpacing = 10;
+// Spacing applied to all sides of the border of the control.
+constexpr int kSpacingInsetsAllSides = 45;
+
+class CloseFullscreenButton : public views::Button {
+ public:
+  explicit CloseFullscreenButton(views::ButtonListener* listener)
+      : views::Button(listener) {}
+
+ private:
+  void PaintButtonContents(gfx::Canvas* canvas) override {
+    // TODO(robliao): If we decide to move forward with this, use themes.
+    cc::PaintFlags flags;
+    flags.setAntiAlias(true);
+    flags.setColor(SK_ColorDKGRAY);
+    flags.setStyle(cc::PaintFlags::kFill_Style);
+    float radius = kCircleButtonDiameter / 2.0f;
+    canvas->DrawCircle(gfx::PointF(radius, radius), radius, flags);
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(CloseFullscreenButton);
+};
 
 }  // namespace
 
 FullscreenControlView::FullscreenControlView(BrowserView* browser_view)
     : browser_view_(browser_view),
-      exit_fullscreen_button_(new views::LabelButton(
-          this,
-          l10n_util::GetStringUTF16(
-              IDS_FULLSCREEN_EXIT_CONTROL_EXIT_FULLSCREEN))) {
+      exit_fullscreen_button_(new CloseFullscreenButton(this)) {
   AddChildView(exit_fullscreen_button_);
-  SetLayoutManager(new views::BoxLayout(views::BoxLayout::kHorizontal,
-                                        gfx::Insets(kSpacingInsetsAllSides),
-                                        kBetweenChildSpacing));
-  // TODO(robliao): If we decide to move forward with this, use themes.
-  SetBackground(views::CreateSolidBackground(SK_ColorWHITE));
+  SetLayoutManager(new views::BoxLayout(
+      views::BoxLayout::kHorizontal, gfx::Insets(kSpacingInsetsAllSides), 0));
+  exit_fullscreen_button_->SetPreferredSize(
+      gfx::Size(kCircleButtonDiameter, kCircleButtonDiameter));
 }
 
 FullscreenControlView::~FullscreenControlView() = default;
