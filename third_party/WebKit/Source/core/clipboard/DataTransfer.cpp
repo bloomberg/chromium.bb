ba5a8c9519e933ad3edd23ecb949b8fdbfc5a838
Philip Rogers <pdr@chromium.org>
2017-09-14 02:52:46 +0000

    Clip drag images to the viewport
    This patch clips the drag image bounds to the viewport which
    prevents excessive drag image sizes.
    
    Bug: 758902
    Change-Id: I1ad99638683d42992319f032e7d690e728c16591
    Reviewed-on: https://chromium-review.googlesource.com/665171
    Commit-Queue: Philip Rogers <pdr@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#501851}
    Former-commit-id: 29a37905268827ff553fc2ba476ed3ed95b090b8

--- third_party/WebKit/Source/core/clipboard/DataTransfer.cpp
+++ third_party/WebKit/Source/core/clipboard/DataTransfer.cpp
@@ -379,7 +379,11 @@ std::unique_ptr<DragImage> DataTransfer::CreateDragImageForFrame(
   float device_scale_factor = frame.GetPage()->DeviceScaleFactorDeprecated();
   float page_scale_factor = frame.GetPage()->GetVisualViewport().Scale();
 
-  FloatRect device_bounds = DeviceSpaceBounds(css_bounds, frame);
+  // Exclude content not in the visual viewport.
+  auto viewport = frame.GetPage()->GetVisualViewport().VisibleRectInDocument();
+  FloatRect clipped_bounds = Intersection(css_bounds, viewport);
+
+  FloatRect device_bounds = DeviceSpaceBounds(clipped_bounds, frame);
 
   AffineTransform transform;
   transform.Scale(device_scale_factor * page_scale_factor);
