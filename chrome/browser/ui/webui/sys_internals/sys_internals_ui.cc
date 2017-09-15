e757545a21c8406647f80edb42d6e6c2d9db82c6
Chung-Sheng Wu <chungsheng@google.com>
2017-09-15 04:44:39 +0000

    [CrOS] SysInternals: Frontend
    Shows information like CPU usage, Memory status, Zram status.
    
    This patch adds the implementation for the chrome://sys-internals page frontend.
    It gets the data from backend and then uses the LineChart library to show the data.
    
    Bug: 752002
    Cq-Include-Trybots: master.tryserver.chromium.linux:closure_compilation
    Change-Id: I41fd29135b2b40050713cf40050b95b6f7734fbf
    Reviewed-on: https://chromium-review.googlesource.com/659700
    Commit-Queue: Zhong-sheng Wu <chungsheng@google.com>
    Reviewed-by: calamity <calamity@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502156}
    Former-commit-id: 5d8065f713f5a157af82f1b9733a9f71c15f0937

--- chrome/browser/ui/webui/sys_internals/sys_internals_ui.cc
+++ chrome/browser/ui/webui/sys_internals/sys_internals_ui.cc
@@ -20,13 +20,25 @@ SysInternalsUI::SysInternalsUI(content::WebUI* web_ui)
 
   content::WebUIDataSource* html_source =
       content::WebUIDataSource::Create(chrome::kChromeUISysInternalsHost);
+  html_source->SetDefaultResource(IDR_SYS_INTERNALS_HTML);
   html_source->AddResourcePath("index.css", IDR_SYS_INTERNALS_CSS);
   html_source->AddResourcePath("index.js", IDR_SYS_INTERNALS_JS);
+  html_source->AddResourcePath("constants.js", IDR_SYS_INTERNALS_CONSTANT_JS);
   html_source->AddResourcePath("line_chart.css",
                                IDR_SYS_INTERNALS_LINE_CHART_CSS);
   html_source->AddResourcePath("line_chart.js",
                                IDR_SYS_INTERNALS_LINE_CHART_JS);
-  html_source->SetDefaultResource(IDR_SYS_INTERNALS_HTML);
+
+  html_source->AddResourcePath("images/menu.svg",
+                               IDR_SYS_INTERNALS_IMAGE_MENU_SVG);
+  html_source->AddResourcePath("images/info.svg",
+                               IDR_SYS_INTERNALS_IMAGE_INFO_SVG);
+  html_source->AddResourcePath("images/cpu.svg",
+                               IDR_SYS_INTERNALS_IMAGE_CPU_SVG);
+  html_source->AddResourcePath("images/memory.svg",
+                               IDR_SYS_INTERNALS_IMAGE_MEMORY_SVG);
+  html_source->AddResourcePath("images/zram.svg",
+                               IDR_SYS_INTERNALS_IMAGE_ZRAM_SVG);
 
   Profile* profile = Profile::FromWebUI(web_ui);
   content::WebUIDataSource::Add(profile, html_source);
--- chrome/test/data/webui/sys_internals/sys_internals_browsertest.js
+++ chrome/test/data/webui/sys_internals/sys_internals_browsertest.js
@@ -32,6 +32,10 @@ SysInternalsBrowserTest.prototype = {
     'line_chart/scrollbar_test.js',
     'line_chart/sub_chart_test.js',
     'line_chart/unit_label_test.js',
+    'page_drawer_test.js',
+    'page_infopage_test.js',
+    'page_switch_test.js',
+    'page_unit_test.js',
     'test_util.js',
     ROOT_PATH + 'third_party/mocha/mocha.js',
     ROOT_PATH + 'third_party/polymer/v1_0/components-chromium/' +
@@ -66,4 +70,20 @@ TEST_F('SysInternalsBrowserTest', 'LineChart_SubChart', function() {
 
 TEST_F('SysInternalsBrowserTest', 'LineChart_UnitLabel', function() {
   LineChartTest.UnitLabel();
+});
+
+TEST_F('SysInternalsBrowserTest', 'Page_Drawer', function() {
+  PageTest.Drawer();
+});
+
+TEST_F('SysInternalsBrowserTest', 'Page_InfoPage', function() {
+  PageTest.InfoPage();
+});
+
+TEST_F('SysInternalsBrowserTest', 'Page_Switch', function() {
+  PageTest.Switch();
+});
+
+TEST_F('SysInternalsBrowserTest', 'Page_Unit', function() {
+  PageTest.Unit();
 });
