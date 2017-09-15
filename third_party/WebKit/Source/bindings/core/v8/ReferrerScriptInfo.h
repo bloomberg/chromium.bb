90fb1e2bd54cb0bd64ec00a8c519e461973724f9
Kouhei Ueno <kouhei@chromium.org>
2017-09-15 06:51:05 +0000

    [ES6 modules] Introduce ReferrerScriptInfo
    This CL implements ReferrerScriptInfo, which holds a copy of "referencing
    script" info, referenced in the "HostImportModuleDynamically" algorithm:
    https://github.com/tc39/proposal-dynamic-import/blob/master/HTML%20Integration.md#hostimportmoduledynamicallyreferencingscriptormodule-specifier-promisecapability
    
    This info can be encoded into v8::Local<v8::PrimitiveArray> and will be
    attached to v8::ScriptOrigin in future CLs. Dynamic import resolver will
    extract this from the ScriptOrigin of the referencing script in its
    implementation of "HostImportModuleDynamically" algorithm.
    
    Bug: 711706
    Change-Id: I0649b6f0334154cdde910a657eee449b39f5f9c7
    Reviewed-on: https://chromium-review.googlesource.com/668257
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502176}
    Former-commit-id: a788bcd802e5ef17eb5a02b23bff3698281f70dd

---
+++ third_party/WebKit/Source/bindings/core/v8/ReferrerScriptInfo.h
@@ -0,0 +1,57 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef ReferrerScriptInfo_h
+#define ReferrerScriptInfo_h
+
+#include "core/CoreExport.h"
+#include "platform/loader/fetch/AccessControlStatus.h"
+#include "platform/loader/fetch/ResourceLoaderOptions.h"
+#include "platform/wtf/text/TextPosition.h"
+#include "platform/wtf/text/WTFString.h"
+#include "public/platform/WebURLRequest.h"
+#include "v8/include/v8.h"
+
+namespace blink {
+
+// ReferrerScriptInfo carries a copy of "referencing script's" info referenced
+// in HTML Spec: "HostImportModuleDynamically" algorithm.
+// https://github.com/tc39/proposal-dynamic-import/blob/master/HTML%20Integration.md#hostimportmoduledynamicallyreferencingscriptormodule-specifier-promisecapability
+class CORE_EXPORT ReferrerScriptInfo {
+ public:
+  ReferrerScriptInfo() {}
+  ReferrerScriptInfo(WebURLRequest::FetchCredentialsMode credentials_mode,
+                     const String& nonce,
+                     ParserDisposition parser_state)
+      : credentials_mode_(credentials_mode),
+        nonce_(nonce),
+        parser_state_(parser_state) {}
+
+  static ReferrerScriptInfo FromV8HostDefinedOptions(
+      v8::Local<v8::Context>,
+      v8::Local<v8::PrimitiveArray>);
+  v8::Local<v8::PrimitiveArray> ToV8HostDefinedOptions(v8::Isolate*) const;
+
+  WebURLRequest::FetchCredentialsMode CredentialsMode() const {
+    return credentials_mode_;
+  }
+  const String& Nonce() const { return nonce_; }
+  ParserDisposition ParserState() const { return parser_state_; }
+
+ private:
+  // Spec: "referencing script's credentials mode"
+  // The default value is "omit", from Step 5 of [HIMD].
+  // [HIMD]
+  // https://github.com/tc39/proposal-dynamic-import/blob/master/HTML%20Integration.md#hostimportmoduledynamicallyreferencingscriptormodule-specifier-promisecapability
+  WebURLRequest::FetchCredentialsMode credentials_mode_ =
+      WebURLRequest::kFetchCredentialsModeOmit;
+  // Spec: "referencing script's cryptographic nonce"
+  String nonce_;
+  // Spec: "referencing script's parser state"
+  ParserDisposition parser_state_ = kNotParserInserted;
+};
+
+}  // namespace blink
+
+#endif  // ReferrerScriptInfo_h
