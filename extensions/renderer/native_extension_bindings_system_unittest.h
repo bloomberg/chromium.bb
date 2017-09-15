144eebc4214f0352c737b77a669a1a48a25c0cd5
Devlin Cronin <rdevlin.cronin@chromium.org>
2017-09-15 21:09:54 +0000

    [Extensions Bindings] Extract NativeExtensionBindingsSystemTest
    Make NativeExtensionBindingsSystemTest a reusable test interface in
    order to allow other modules to be tested with a functional bindings
    system. This will be important going forward for testing custom
    bindings that rely on extensions knowledge.
    
    Bug: 653596
    Change-Id: I68dfb3ef7e5453adbda3134aa0bc7401a89c0ef9
    Reviewed-on: https://chromium-review.googlesource.com/668035
    Commit-Queue: Devlin <rdevlin.cronin@chromium.org>
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502372}
    Former-commit-id: 37961725b11922c66a189ba6a039b75f3306f0e1

---
+++ extensions/renderer/native_extension_bindings_system_unittest.h
@@ -0,0 +1,148 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EXTENSIONS_RENDERER_NATIVE_EXTENSION_BINDINGS_SYSTEM_UNITTEST_H_
+#define EXTENSIONS_RENDERER_NATIVE_EXTENSION_BINDINGS_SYSTEM_UNITTEST_H_
+
+#include <memory>
+#include <string>
+
+#include "base/macros.h"
+#include "extensions/common/extension.h"
+#include "extensions/common/features/feature.h"
+#include "extensions/renderer/bindings/api_binding_test.h"
+#include "extensions/renderer/bindings/api_binding_types.h"
+#include "extensions/renderer/ipc_message_sender.h"
+#include "extensions/renderer/string_source_map.h"
+#include "extensions/renderer/test_extensions_renderer_client.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+struct ExtensionHostMsg_Request_Params;
+
+namespace base {
+class DictionaryValue;
+}
+
+namespace content {
+class MockRenderThread;
+}
+
+namespace v8 {
+class ExtensionConfiguration;
+}
+
+namespace extensions {
+class NativeExtensionBindingsSystem;
+class ScriptContext;
+class ScriptContextSet;
+struct PortId;
+struct Message;
+
+class TestIPCMessageSender : public IPCMessageSender {
+ public:
+  TestIPCMessageSender();
+  ~TestIPCMessageSender();
+
+  // IPCMessageSender:
+  void SendRequestIPC(ScriptContext* context,
+                      std::unique_ptr<ExtensionHostMsg_Request_Params> params,
+                      binding::RequestThread thread) override {
+    last_params_ = std::move(params);
+  }
+  void SendOnRequestResponseReceivedIPC(int request_id) override {}
+  // The event listener methods are less of a pain to mock (since they don't
+  // have complex parameters like ExtensionHostMsg_Request_Params).
+  MOCK_METHOD2(SendAddUnfilteredEventListenerIPC,
+               void(ScriptContext* context, const std::string& event_name));
+  MOCK_METHOD2(SendRemoveUnfilteredEventListenerIPC,
+               void(ScriptContext* context, const std::string& event_name));
+
+  // Send a message to add/remove a lazy unfiltered listener.
+  MOCK_METHOD2(SendAddUnfilteredLazyEventListenerIPC,
+               void(ScriptContext* context, const std::string& event_name));
+  MOCK_METHOD2(SendRemoveUnfilteredLazyEventListenerIPC,
+               void(ScriptContext* context, const std::string& event_name));
+
+  // Send a message to add/remove a filtered listener.
+  MOCK_METHOD4(SendAddFilteredEventListenerIPC,
+               void(ScriptContext* context,
+                    const std::string& event_name,
+                    const base::DictionaryValue& filter,
+                    bool is_lazy));
+  MOCK_METHOD4(SendRemoveFilteredEventListenerIPC,
+               void(ScriptContext* context,
+                    const std::string& event_name,
+                    const base::DictionaryValue& filter,
+                    bool remove_lazy_listener));
+  MOCK_METHOD2(SendOpenMessagePort,
+               void(int routing_id, const PortId& port_id));
+  MOCK_METHOD3(SendCloseMessagePort,
+               void(int routing_id, const PortId& port_id, bool close_channel));
+  MOCK_METHOD3(SendPostMessageToPort,
+               void(int routing_id,
+                    const PortId& port_id,
+                    const Message& message));
+
+  const ExtensionHostMsg_Request_Params* last_params() const {
+    return last_params_.get();
+  }
+
+ private:
+  std::unique_ptr<ExtensionHostMsg_Request_Params> last_params_;
+
+  DISALLOW_COPY_AND_ASSIGN(TestIPCMessageSender);
+};
+
+// A test harness to instantiate the NativeExtensionBindingsSystem (along with
+// its dependencies) and support adding/removing extensions and ScriptContexts.
+// This is useful for bindings tests that need extensions-specific knowledge.
+class NativeExtensionBindingsSystemUnittest : public APIBindingTest {
+ public:
+  NativeExtensionBindingsSystemUnittest();
+  ~NativeExtensionBindingsSystemUnittest() override;
+
+ protected:
+  // APIBindingTest:
+  void SetUp() override;
+  void TearDown() override;
+  void OnWillDisposeContext(v8::Local<v8::Context> context) override;
+  v8::ExtensionConfiguration* GetV8ExtensionConfiguration() override;
+
+  ScriptContext* CreateScriptContext(v8::Local<v8::Context> v8_context,
+                                     Extension* extension,
+                                     Feature::Context context_type);
+
+  void RegisterExtension(scoped_refptr<const Extension> extension);
+
+  NativeExtensionBindingsSystem* bindings_system() {
+    return bindings_system_.get();
+  }
+  bool has_last_params() const { return !!ipc_message_sender_->last_params(); }
+  const ExtensionHostMsg_Request_Params& last_params() {
+    return *ipc_message_sender_->last_params();
+  }
+  StringSourceMap* source_map() { return &source_map_; }
+  TestIPCMessageSender* ipc_message_sender() { return ipc_message_sender_; }
+  ScriptContextSet* script_context_set() { return script_context_set_.get(); }
+
+ private:
+  ExtensionIdSet extension_ids_;
+  std::unique_ptr<content::MockRenderThread> render_thread_;
+  std::unique_ptr<ScriptContextSet> script_context_set_;
+  std::vector<ScriptContext*> raw_script_contexts_;
+  std::unique_ptr<NativeExtensionBindingsSystem> bindings_system_;
+  // The TestIPCMessageSender; owned by the bindings system.
+  TestIPCMessageSender* ipc_message_sender_ = nullptr;
+
+  std::unique_ptr<ExtensionHostMsg_Request_Params> last_params_;
+
+  StringSourceMap source_map_;
+  TestExtensionsRendererClient renderer_client_;
+
+  DISALLOW_COPY_AND_ASSIGN(NativeExtensionBindingsSystemUnittest);
+};
+
+}  // namespace extensions
+
+#endif  // EXTENSIONS_RENDERER_NATIVE_EXTENSION_BINDINGS_SYSTEM_UNITTEST_H_
