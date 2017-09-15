ca2d522a6d6eb220329781a9729b08316fa6aef0
Jian Li <jianli@chromium.org>
2017-09-15 21:55:45 +0000

    Schedule retry with or without backoff for prefetch background task
    Bug: 701930
    Change-Id: I344f3f37e34a21d12afb654621a15527fb820760
    Reviewed-on: https://chromium-review.googlesource.com/668088
    Commit-Queue: Jian Li <jianli@chromium.org>
    Reviewed-by: Justin DeWitt <dewittj@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502393}
    Former-commit-id: 7fa61a50cf6a2773667974b03a29246498b19aa0

--- components/offline_pages/core/prefetch/prefetch_dispatcher_impl.h
+++ components/offline_pages/core/prefetch/prefetch_dispatcher_impl.h
@@ -59,12 +59,11 @@ class PrefetchDispatcherImpl : public PrefetchDispatcher,
   void DisposeTask();
 
   // Callbacks for network requests.
-  void DidGenerateBundleRequest(PrefetchRequestStatus status,
-                                const std::string& operation_name,
-                                const std::vector<RenderPageInfo>& pages);
-  void DidGetOperationRequest(PrefetchRequestStatus status,
-                              const std::string& operation_name,
-                              const std::vector<RenderPageInfo>& pages);
+  void DidGenerateBundleOrGetOperationRequest(
+      const std::string& request_name_for_logging,
+      PrefetchRequestStatus status,
+      const std::string& operation_name,
+      const std::vector<RenderPageInfo>& pages);
   void LogRequestResult(const std::string& request_name_for_logging,
                         PrefetchRequestStatus status,
                         const std::string& operation_name,
