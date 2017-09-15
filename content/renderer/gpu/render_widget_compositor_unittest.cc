df36b837131342751dae6b8ef043fc2cd1858728
Jonathan <jonross@chromium.org>
2017-09-15 19:09:14 +0000

    Removing unused viz imports from content tests
    Removing some unused viz imports from content tests.
    
    TBR=sky@chromium.org
    
    TEST: compiled content_unittests and content_browsertests
    Bug: 762514, 762526, 764802, 763464
    Change-Id: I30c6bd3e96bb90b4aa4464163b73e1138a724e61
    Reviewed-on: https://chromium-review.googlesource.com/668666
    Reviewed-by: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Scott Violet <sky@chromium.org>
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502326}
    Former-commit-id: 4f507cbe3282cec5b6915962aefab77aa89cd835

--- content/renderer/gpu/render_widget_compositor_unittest.cc
+++ content/renderer/gpu/render_widget_compositor_unittest.cc
@@ -19,7 +19,6 @@
 #include "cc/test/test_context_provider.h"
 #include "cc/test/test_web_graphics_context_3d.h"
 #include "cc/trees/layer_tree_host.h"
-#include "components/viz/common/frame_sinks/begin_frame_args.h"
 #include "components/viz/common/quads/copy_output_request.h"
 #include "content/public/common/screen_info.h"
 #include "content/public/test/mock_render_thread.h"
