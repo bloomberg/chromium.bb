00c10c91505cd4b82186fd18cc312bf3d4d0cd3f
Yoland Yan <yolandyan@chromium.org>
2017-09-15 21:45:13 +0000

    Convert AwVariationsConfigurationServiceTest to JUnit4
    For more on JUnit4 migration, please check src/testing/android/docs/junit4.md
    
    Bug: 640116
    Change-Id: Ib4c3493a00001eb3bd765e6e66239fa1a97e6b9d
    Reviewed-on: https://chromium-review.googlesource.com/669529
    Reviewed-by: Bo <boliu@chromium.org>
    Commit-Queue: Yoland Yan <yolandyan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502388}
    Former-commit-id: e680ea942cc99a36f16a96bee3716c07a31d1ac3

--- android_webview/javatests/src/org/chromium/android_webview/test/variations/AwVariationsConfigurationServiceTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/variations/AwVariationsConfigurationServiceTest.java
@@ -10,9 +10,15 @@ import static org.chromium.android_webview.variations.AwVariationsUtils.SEED_PRE
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.ParcelFileDescriptor;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.MediumTest;
-import android.test.InstrumentationTestCase;
 import android.util.Base64;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import org.chromium.android_webview.AwBrowserProcess;
 import org.chromium.android_webview.variations.AwVariationsConfigurationService;
@@ -21,6 +27,7 @@ import org.chromium.android_webview.variations.AwVariationsUtils;
 import org.chromium.android_webview.variations.AwVariationsUtils.SeedPreference;
 import org.chromium.base.ContextUtils;
 import org.chromium.base.PathUtils;
+import org.chromium.base.test.BaseJUnit4ClassRunner;
 import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.components.variations.firstrun.VariationsSeedFetcher.SeedInfo;
 
@@ -32,14 +39,17 @@ import java.io.IOException;
  * Although AwVariationsConfigurationService is not a JobService, the test case still uses the
  * AwVariationsSeedFetchService which can't work under Android L.
  */
+@RunWith(BaseJUnit4ClassRunner.class)
 @MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP)
 @TargetApi(Build.VERSION_CODES.LOLLIPOP) // JobService requires API level 21.
-public class AwVariationsConfigurationServiceTest extends InstrumentationTestCase {
+public class AwVariationsConfigurationServiceTest {
     private SeedInfo mSeedInfo;
 
-    protected void setUp() throws Exception {
-        ContextUtils.initApplicationContextForTests(
-                getInstrumentation().getTargetContext().getApplicationContext());
+    @Before
+    public void setUp() throws Exception {
+        ContextUtils.initApplicationContextForTests(InstrumentationRegistry.getInstrumentation()
+                                                            .getTargetContext()
+                                                            .getApplicationContext());
         PathUtils.setPrivateDataDirectorySuffix(AwBrowserProcess.PRIVATE_DATA_DIRECTORY_SUFFIX);
 
         mSeedInfo = new SeedInfo();
@@ -49,41 +59,44 @@ public class AwVariationsConfigurationServiceTest extends InstrumentationTestCas
         mSeedInfo.date = "SeedDate";
     }
 
-    protected void tearDown() throws Exception {
+    @After
+    public void tearDown() throws Exception {
         AwVariationsConfigurationService.clearDataForTesting();
-        super.tearDown();
     }
 
     /**
      * Ensure reading and writing seed data in app directory works correctly.
      */
+    @Test
     @MediumTest
     public void testReadAndWriteSeedData() throws IOException {
         File variationsDir = AwVariationsConfigurationService.getOrCreateVariationsDirectory();
         AwVariationsConfigurationService.storeSeedDataToFile(mSeedInfo.seedData, variationsDir);
         String b64SeedData = AwVariationsConfigurationService.getSeedDataForTesting(variationsDir);
-        assertEquals(Base64.encodeToString(mSeedInfo.seedData, Base64.NO_WRAP), b64SeedData);
+        Assert.assertEquals(Base64.encodeToString(mSeedInfo.seedData, Base64.NO_WRAP), b64SeedData);
     }
 
     /**
      * Ensure reading and writing seed preference in app directory works correctly.
      */
+    @Test
     @MediumTest
     public void testReadAndWriteSeedPref() throws IOException {
         File variationsDir = AwVariationsConfigurationService.getOrCreateVariationsDirectory();
         SeedPreference expect = new SeedPreference(mSeedInfo);
         AwVariationsConfigurationService.storeSeedPrefToFile(expect, variationsDir);
         SeedPreference actual = AwVariationsUtils.readSeedPreference(variationsDir);
-        assertNotNull(expect);
-        assertNotNull(actual);
-        assertEquals(expect.signature, actual.signature);
-        assertEquals(expect.country, actual.country);
-        assertEquals(expect.date, actual.date);
+        Assert.assertNotNull(expect);
+        Assert.assertNotNull(actual);
+        Assert.assertEquals(expect.signature, actual.signature);
+        Assert.assertEquals(expect.country, actual.country);
+        Assert.assertEquals(expect.date, actual.date);
     }
 
     /**
      * Ensure copying seed data from the service directory to the app directory works correctly.
      */
+    @Test
     @MediumTest
     public void testCopySeedData() throws IOException {
         File serviceDir = AwVariationsConfigurationService.getOrCreateVariationsDirectory();
@@ -98,13 +111,14 @@ public class AwVariationsConfigurationServiceTest extends InstrumentationTestCas
         String seedDataInServiceDir =
                 AwVariationsConfigurationService.getSeedDataForTesting(serviceDir);
         String seedDataInAppDir = AwVariationsConfigurationService.getSeedDataForTesting(appDir);
-        assertEquals(seedDataInServiceDir, seedDataInAppDir);
+        Assert.assertEquals(seedDataInServiceDir, seedDataInAppDir);
     }
 
     /**
      * Ensure copying seed preference from the service directory to the app directory works
      * correctly.
      */
+    @Test
     @MediumTest
     public void testCopySeedPref() throws IOException {
         File serviceDir = AwVariationsConfigurationService.getOrCreateVariationsDirectory();
@@ -119,10 +133,10 @@ public class AwVariationsConfigurationServiceTest extends InstrumentationTestCas
 
         SeedPreference seedPrefInServiceDir = AwVariationsUtils.readSeedPreference(serviceDir);
         SeedPreference seedPrefInAppDir = AwVariationsUtils.readSeedPreference(appDir);
-        assertNotNull(seedPrefInServiceDir);
-        assertNotNull(seedPrefInAppDir);
-        assertEquals(seedPrefInServiceDir.signature, seedPrefInAppDir.signature);
-        assertEquals(seedPrefInServiceDir.country, seedPrefInAppDir.country);
-        assertEquals(seedPrefInServiceDir.date, seedPrefInAppDir.date);
+        Assert.assertNotNull(seedPrefInServiceDir);
+        Assert.assertNotNull(seedPrefInAppDir);
+        Assert.assertEquals(seedPrefInServiceDir.signature, seedPrefInAppDir.signature);
+        Assert.assertEquals(seedPrefInServiceDir.country, seedPrefInAppDir.country);
+        Assert.assertEquals(seedPrefInServiceDir.date, seedPrefInAppDir.date);
     }
 }
