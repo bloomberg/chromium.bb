b1a6c4fd47d2cfad2092aafe501b25672d975fa0
Hitoshi Yoshida <peria@chromium.org>
2017-09-16 06:34:56 +0000

    bindings: Bind V8's snapshot file to Blink's snapshot
    Before this CL, Blink's snapshot was created from scratch, and it meant
    something in V8's snapshot could be missed.
    
    After this CL, v8_context_snapshot_gerator will create a snapshot
    file which involves all information in V8's snapshot file.
    
    This change is a preparation to remove V8's snapshot file from binded
    resources of Chromium.
    
    
    
    Bug: 750176
    Change-Id: I18050cc8131cd836a7b100c8b28cf0148c4a7f4f
    Reviewed-on: https://chromium-review.googlesource.com/656866
    Commit-Queue: Hitoshi Yoshida <peria@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Yuki Shiino <yukishiino@chromium.org>
    Reviewed-by: Kentaro Hara <haraken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502494}
    Former-commit-id: 440591c306d61c7821b178c6e581080b97b51517

--- third_party/WebKit/Source/platform/bindings/V8PerIsolateData.h
+++ third_party/WebKit/Source/platform/bindings/V8PerIsolateData.h
@@ -288,8 +288,7 @@ class PLATFORM_EXPORT V8PerIsolateData {
   HashMap<const void*, Vector<v8::Eternal<v8::Name>>> eternal_name_cache_;
 
   // Members required for the V8 context snapshot.
-  // v8::Context is created from this blob data image. This needs to be
-  // instantiated before |isolate_holder_| gets instantiated.
+  // v8::Context is created from this blob data image.
   v8::StartupData startup_data_;
   // When taking a V8 context snapshot, we can't keep V8 objects with eternal
   // handles. So we use a special interface map that doesn't use eternal handles
