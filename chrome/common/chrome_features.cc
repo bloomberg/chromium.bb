02ef74d5af3f8b9aeb3a027fe3078a59e1a95978
Marc Treib <treib@chromium.org>
2017-09-18 10:09:16 +0000

    Enable NTPCaptureThumbnail by default
    This enables the features "CaptureThumbnailOnNavigatingAway" and
    "CaptureThumbnailDependingOnTransitionType" by default, and removes the
    corresponding fieldtrial testing config.
    
    Bug: 765588
    Change-Id: I635e51db6615414fee0178c3528421178027885b
    Reviewed-on: https://chromium-review.googlesource.com/668369
    Reviewed-by: Robert Kaplow <rkaplow@chromium.org>
    Commit-Queue: Marc Treib <treib@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502549}
    Former-commit-id: 6263ab04beb58e7ed19b027f211d080aa954dc74

--- chrome/common/chrome_features.cc
+++ chrome/common/chrome_features.cc
@@ -122,7 +122,7 @@ const base::Feature kTabsInCbd {
 // one that qualifies for inclusion in TopSites.
 const base::Feature kCaptureThumbnailDependingOnTransitionType{
     "CaptureThumbnailDependingOnTransitionType",
-    base::FEATURE_DISABLED_BY_DEFAULT};
+    base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Whether to capture page thumbnails when the page load finishes (in addition
 // to any other times this might happen).
@@ -132,7 +132,7 @@ const base::Feature kCaptureThumbnailOnLoadFinished{
 // Whether to capture page thumbnails when navigating away from the current page
 // (in addition to any other times this might happen).
 const base::Feature kCaptureThumbnailOnNavigatingAway{
-    "CaptureThumbnailOnNavigatingAway", base::FEATURE_DISABLED_BY_DEFAULT};
+    "CaptureThumbnailOnNavigatingAway", base::FEATURE_ENABLED_BY_DEFAULT};
 
 // Whether to trigger app banner installability checks on page load.
 const base::Feature kCheckInstallabilityForBannerOnLoad{
