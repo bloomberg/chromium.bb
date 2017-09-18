a53386ae88b23b9888689bae8347c8edc6a3ddbe
Eugene But <eugenebut@google.com>
2017-09-18 22:03:37 +0000

    Revert "Implement 'Open (All) in incognito' of context bar (new iOS bookmark ui)"
    This reverts commit d8e8af0f3e3541d565d15f1b81dda086f48c369c [formerly bd9f6611523cb916f45c623f6e579a98d6152463].
    
    TBR=martiw@chromium.org
    
    Bug: 766297
    Change-Id: Id3cec4ef26bdee293a5110c9273b2f83c008ad9c
    Reviewed-on: https://chromium-review.googlesource.com/671809
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Commit-Queue: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502688}
    Former-commit-id: 24afd864095da031c46e66ff2286cd3f0c5e8550

--- ios/chrome/browser/ui/bookmarks/bookmark_interaction_controller.mm
+++ ios/chrome/browser/ui/bookmarks/bookmark_interaction_controller.mm
@@ -248,49 +248,29 @@ using bookmarks::BookmarkNode;
 - (void)
 bookmarkHomeViewControllerWantsDismissal:(BookmarkHomeViewController*)controller
                         navigationToUrls:(const std::vector<GURL>&)urls {
-  [self bookmarkHomeViewControllerWantsDismissal:controller
-                                navigationToUrls:urls
-                                     inIncognito:_currentBrowserState
-                                                     ->IsOffTheRecord()];
-}
-
-- (void)bookmarkHomeViewControllerWantsDismissal:
-            (BookmarkHomeViewController*)controller
-                                navigationToUrls:(const std::vector<GURL>&)urls
-                                     inIncognito:(BOOL)inIncognito {
   [self dismissBookmarkBrowserAnimated:YES];
 
   if (urls.empty())
     return;
 
-  BOOL openInForegroundTab = YES;
+  BOOL openInCurrentTab = YES;
   for (const GURL& url : urls) {
     DCHECK(url.is_valid());
-    // TODO(crbug.com/695749): Force url to open in non-incognito mode. if
-    // !IsURLAllowedInIncognito(url).
-
-    if (openInForegroundTab) {
-      // Only open the first URL in foreground tab.
-      openInForegroundTab = NO;
+    if (openInCurrentTab) {
+      // Only open the first URL in the current tab.
+      openInCurrentTab = NO;
 
       // TODO(crbug.com/695749): See if we need different metrics for 'Open
-      // all', 'Open all in incognito' and 'Open in incognito'.
+      // All'.
       new_tab_page_uma::RecordAction(_browserState,
                                      new_tab_page_uma::ACTION_OPENED_BOOKMARK);
       base::RecordAction(
           base::UserMetricsAction("MobileBookmarkManagerEntryOpened"));
 
-      if (inIncognito == _currentBrowserState->IsOffTheRecord()) {
-        // Open in current tab if target tab mode is same as current tab mode.
-        [self openURLInCurrentTab:url];
-      } else {
-        // Open in new tab if target tab mode is different from current tab
-        // mode.
-        [self openURLInNewTab:url inIncognito:inIncognito inBackground:NO];
-      }
+      [self openURLInCurrentTab:url];
     } else {
       // Open other URLs (if any) in background tabs.
-      [self openURLInNewTab:url inIncognito:inIncognito inBackground:YES];
+      [self openURLInBackgroundTab:url];
     }
   }  // end for
 }
@@ -302,23 +282,18 @@ bookmarkHomeViewControllerWantsDismissal:(BookmarkHomeViewController*)controller
     NSString* jsToEval = [base::SysUTF8ToNSString(url.GetContent())
         stringByRemovingPercentEncoding];
     [_loader loadJavaScriptFromLocationBar:jsToEval];
-    return;
+  } else {
+    [_loader loadURL:url
+                 referrer:web::Referrer()
+               transition:ui::PAGE_TRANSITION_AUTO_BOOKMARK
+        rendererInitiated:NO];
   }
-  [_loader loadURL:url
-               referrer:web::Referrer()
-             transition:ui::PAGE_TRANSITION_AUTO_BOOKMARK
-      rendererInitiated:NO];
 }
 
-- (void)openURLInNewTab:(const GURL&)url
-            inIncognito:(BOOL)inIncognito
-           inBackground:(BOOL)inBackground {
-  // TODO(crbug.com/695749):  Open bookmarklet in new tab doesn't work.  See how
-  // to deal with this later.
+- (void)openURLInBackgroundTab:(const GURL&)url {
   [_loader webPageOrderedOpen:url
                      referrer:web::Referrer()
-                  inIncognito:inIncognito
-                 inBackground:inBackground
+                 inBackground:YES
                      appendTo:kLastTab];
 }
 
