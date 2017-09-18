908f406f5ed7fe4f588560a484a564bf3d11ef62
David Reveman <reveman@chromium.org>
2017-09-18 20:50:02 +0000

    exo: Implement proper support for configure callbacks.
    This allows compositor locks to be included in shell surface
    configs and gives clients 100ms to produce a frame that takes
    maximize/fullscreen/pinned state changes into account.
    
    The result is seamless animations for clients that behave
    well.
    
    Bug: b/29406058
    Test: exo_unittests --gtest_filter=ShellSurfaceTest.AcknowledgeConfigure
    Change-Id: I1c585b14e87ef8145fb2aec7b6e41ca09c074334
    Reviewed-on: https://chromium-review.googlesource.com/669782
    Reviewed-by: Daniele Castagna <dcastagna@chromium.org>
    Commit-Queue: David Reveman <reveman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502669}
    Former-commit-id: 3acdaebbe2f31d890e22942821c2fe4a3bfd5e88

--- components/exo/shell_surface.h
+++ components/exo/shell_surface.h
@@ -293,7 +293,7 @@ class ShellSurface : public SurfaceTreeHost,
   void OnDisplayMetricsChanged(const display::Display& display,
                                uint32_t changed_metrics) override;
 
-  // Ovrridden ui::CompositorLockClient:
+  // Overridden from ui::CompositorLockClient:
   void CompositorLockTimedOut() override;
 
   aura::Window* shadow_overlay() { return shadow_overlay_.get(); }
@@ -302,15 +302,9 @@ class ShellSurface : public SurfaceTreeHost,
   Surface* surface_for_testing() { return root_surface(); }
 
  private:
+  struct Config;
   class ScopedConfigure;
   class ScopedAnimationsDisabled;
-
-  // Surface state associated with each configure request.
-  struct Config {
-    uint32_t serial;
-    gfx::Vector2d origin_offset;
-    int resize_component;
-  };
 
   // Creates the |widget_| for |surface_|. |show_state| is the initial state
   // of the widget (e.g. maximized).
@@ -361,7 +355,9 @@ class ShellSurface : public SurfaceTreeHost,
 
   // Lock the compositor if it's not already locked, or extends the
   // lock timeout if it's already locked.
-  void EnsureCompositorIsLocked();
+  // TODO(reveman): Remove this when using configure callbacks for orientation.
+  // crbug.com/765954
+  void EnsureCompositorIsLockedForOrientationChange();
 
   views::Widget* widget_ = nullptr;
   aura::Window* parent_;
@@ -386,6 +382,7 @@ class ShellSurface : public SurfaceTreeHost,
   StateChangedCallback state_changed_callback_;
   ConfigureCallback configure_callback_;
   ScopedConfigure* scoped_configure_ = nullptr;
+  std::unique_ptr<ui::CompositorLock> configure_compositor_lock_;
   bool ignore_window_bounds_changes_ = false;
   gfx::Vector2d origin_offset_;
   gfx::Vector2d pending_origin_offset_;
@@ -397,17 +394,18 @@ class ShellSurface : public SurfaceTreeHost,
   gfx::Rect shadow_content_bounds_;
   bool shadow_content_bounds_changed_ = false;
   float shadow_background_opacity_ = 1.0;
-  base::circular_deque<Config> pending_configs_;
+  base::circular_deque<std::unique_ptr<Config>> pending_configs_;
   std::unique_ptr<ash::WindowResizer> resizer_;
   std::unique_ptr<ScopedAnimationsDisabled> scoped_animations_disabled_;
   int top_inset_height_ = 0;
   int pending_top_inset_height_ = 0;
   bool shadow_underlay_in_surface_ = true;
   bool pending_shadow_underlay_in_surface_ = true;
+  // TODO(reveman): Use configure callbacks for orientation. crbug.com/765954
   Orientation pending_orientation_ = Orientation::LANDSCAPE;
   Orientation orientation_ = Orientation::LANDSCAPE;
   Orientation expected_orientation_ = Orientation::LANDSCAPE;
-  std::unique_ptr<ui::CompositorLock> compositor_lock_;
+  std::unique_ptr<ui::CompositorLock> orientation_compositor_lock_;
   bool system_modal_ = false;
   bool non_system_modal_window_was_active_ = false;
   gfx::ImageSkia icon_;
