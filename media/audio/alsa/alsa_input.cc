f49f7ae9545a0e65932f891ffd1ffaa8af2ecf5a
Gene <gene@chromium.org>
2017-09-18 18:25:14 +0000

    Fix issue with accurate timestamping of captures audio stream. It looks like snd_pcm_delay() for Alsa returns maximum latency possible for current internal buffer state, while we need latency from the moment sound was recorded till we read it from the buffer, which should be snd_pcm_avail_update()
    Bug: 
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: I27a8a9f5440868bd0b16ee9cbbf35b723ee66753
    Reviewed-on: https://chromium-review.googlesource.com/656524
    Reviewed-by: Olga Sharonova <olka@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Gene Gutnik <gene@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502612}
    Former-commit-id: 47f28c1d41fd08b95d74ec252e624ecae1d8335d

--- media/audio/alsa/alsa_input.cc
+++ media/audio/alsa/alsa_input.cc
@@ -157,23 +157,6 @@ bool AlsaPcmInputStream::Recover(int original_error) {
   return true;
 }
 
-snd_pcm_sframes_t AlsaPcmInputStream::GetCurrentDelay() {
-  snd_pcm_sframes_t delay = -1;
-
-  // TODO(dalecurtis): This should probably use snd_pcm_htimestamp() so that we
-  // can have |capture_time| directly instead of computing it as Now() - delay.
-  int error = wrapper_->PcmDelay(device_handle_, &delay);
-  if (error < 0)
-    Recover(error);
-
-  // snd_pcm_delay() may not work in the beginning of the stream. In this case
-  // return delay of data we know currently is in the ALSA's buffer.
-  if (delay < 0)
-    delay = wrapper_->PcmAvailUpdate(device_handle_);
-
-  return delay;
-}
-
 void AlsaPcmInputStream::ReadAudio() {
   DCHECK(callback_);
 
@@ -215,8 +198,17 @@ void AlsaPcmInputStream::ReadAudio() {
       audio_bus_->FromInterleaved(audio_buffer_.get(), audio_bus_->frames(),
                                   params_.bits_per_sample() / 8);
 
+      // TODO(dalecurtis): This should probably use snd_pcm_htimestamp() so that
+      // we can have |capture_time| directly instead of computing it as
+      // Now() - available frames.
+      snd_pcm_sframes_t avail_frames = wrapper_->PcmAvailUpdate(device_handle_);
+      if (avail_frames < 0) {
+        LOG(WARNING) << "PcmAvailUpdate(): "
+                     << wrapper_->StrError(avail_frames);
+        avail_frames = 0;  // Error getting number of avail frames, set it to 0
+      }
       base::TimeDelta hardware_delay = base::TimeDelta::FromSecondsD(
-          GetCurrentDelay() / static_cast<double>(params_.sample_rate()));
+          avail_frames / static_cast<double>(params_.sample_rate()));
 
       callback_->OnData(this, audio_bus_.get(),
                         base::TimeTicks::Now() - hardware_delay,
