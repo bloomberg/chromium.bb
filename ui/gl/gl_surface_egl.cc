39c20153fce667f9e0357ae42eb25af31a93a94e
Yuly Novikov <ynovikov@chromium.org>
2017-09-18 18:04:27 +0000

    Revert "Disable surfaceless EGL on Android"
    This reverts commit afa0499cfa47b4388a16c2d68babf6128a1068dd [formerly dd49d9c1c18e3b8639d4adea15e964cfa7bef62e].
    
    Reason for revert: works well after
    https://chromium-review.googlesource.com/646188
    
    Original change's description:
    > Disable surfaceless EGL on Android
    >
    > There are outstanding bugs related to how Android code meant for WebView
    > is interacting with surfaceless EGL. These cause crashing on startup
    > and incorrect rendering on platforms that support surfaceless EGL. Work
    > around the issues by disabling surfaceless EGL on Android to unblock work
    > before a more complete solution can be developed.
    >
    > BUG=382349
    >
    > Review URL: https://codereview.chromium.org/330453003
    >
    > git-svn-id: svn://svn.chromium.org/chrome/trunk/src@276713 0039d316-1c4b-4281-b951-d872f2087c98
    
    BUG=382349
    TEST=Start Chromium, run WebGL Aquarium on Pixel C
    
    Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
    Change-Id: Id8b03aa5a91bfe087ce59c3c7a6b63f6e09a4171
    Reviewed-on: https://chromium-review.googlesource.com/669842
    Reviewed-by: Kenneth Russell <kbr@chromium.org>
    Commit-Queue: Yuly Novikov <ynovikov@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502609}
    Former-commit-id: ef7da40261b33a2e41cc908ef571a05e4f55e0fd

--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -583,13 +583,6 @@ bool GLSurfaceEGL::InitializeOneOff(EGLNativeDisplayType native_display) {
   g_egl_create_context_client_arrays_supported =
       HasEGLExtension("EGL_ANGLE_create_context_client_arrays");
 
-  // TODO(oetuaho@nvidia.com): Surfaceless is disabled on Android as a temporary
-  // workaround, since code written for Android WebView takes different paths
-  // based on whether GL surface objects have underlying EGL surface handles,
-  // conflicting with the use of surfaceless. See https://crbug.com/382349
-#if defined(OS_ANDROID)
-  DCHECK(!g_egl_surfaceless_context_supported);
-#else
   // Check if SurfacelessEGL is supported.
   g_egl_surfaceless_context_supported =
       HasEGLExtension("EGL_KHR_surfaceless_context");
@@ -610,7 +603,6 @@ bool GLSurfaceEGL::InitializeOneOff(EGLNativeDisplayType native_display) {
       context->ReleaseCurrent(surface.get());
     }
   }
-#endif
 
   initialized_ = true;
   return true;
