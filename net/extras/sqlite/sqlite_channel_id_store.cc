65911b175e34126cd0bb6851ad576bddd1b78deb
Nick Harper <nharper@chromium.org>
2017-09-19 22:11:48 +0000

    Don't use crypto::ECPrivateKey::CreateFromEncryptedPrivateKeyInfo in //net
    This function was only used in the SQLite Channel ID store; a migration
    away from it was added in April. This change removes the use of the
    function which until now is only used for migrating old Channel ID db
    formats to the new format. With this change, old databases won't be
    upgradeable.
    
    Bug: 618026
    Change-Id: I245077dd1cb16cb5a53b174a15d7dbdf0e0e9942
    Reviewed-on: https://chromium-review.googlesource.com/669866
    Commit-Queue: Nick Harper <nharper@chromium.org>
    Reviewed-by: David Benjamin <davidben@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502966}
    Former-commit-id: 1a32990c51b6d1bc84211bb1ee73d6c255ff4d20

--- net/extras/sqlite/sqlite_channel_id_store.cc
+++ net/extras/sqlite/sqlite_channel_id_store.cc
@@ -321,98 +321,22 @@ bool SQLiteChannelIDStore::Backend::EnsureDatabaseVersion() {
     }
   }
 
-  // Migrate from previous versions to new version if possible
-  if (cur_version >= 2 && cur_version <= 4) {
-    sql::Statement statement(db_->GetUniqueStatement(
-        "SELECT origin, cert, private_key, cert_type FROM origin_bound_certs"));
-    sql::Statement insert_statement(db_->GetUniqueStatement(
-        "INSERT INTO channel_id (host, private_key, public_key, creation_time) "
-        "VALUES (?, ?, \"\", ?)"));
-    if (!statement.is_valid() || !insert_statement.is_valid()) {
-      LOG(WARNING) << "Unable to update server bound cert database to "
-                   << "version 6.";
-      return false;
-    }
-
-    while (statement.Step()) {
-      if (statement.ColumnInt64(3) != CLIENT_CERT_ECDSA_SIGN)
-        continue;
-      std::string origin = statement.ColumnString(0);
-      std::string cert_from_db;
-      statement.ColumnBlobAsString(1, &cert_from_db);
-      std::vector<uint8_t> encrypted_private_key, private_key;
-      statement.ColumnBlobAsVector(2, &encrypted_private_key);
-      std::unique_ptr<crypto::ECPrivateKey> key(
-          crypto::ECPrivateKey::CreateFromEncryptedPrivateKeyInfo(
-              encrypted_private_key, std::vector<uint8_t>()));
-      if (!key || !key->ExportPrivateKey(&private_key)) {
-        LOG(WARNING) << "Unable to parse encrypted private key when migrating "
-                        "Channel ID database to version 6.";
-        continue;
-      }
-      // Parse the cert and extract the real value and then update the DB.
-      scoped_refptr<X509Certificate> cert(X509Certificate::CreateFromBytes(
-          cert_from_db.data(), static_cast<int>(cert_from_db.size())));
-      if (cert.get()) {
-        insert_statement.Reset(true);
-        insert_statement.BindString(0, origin);
-        insert_statement.BindBlob(1, private_key.data(),
-                                  static_cast<int>(private_key.size()));
-        insert_statement.BindInt64(2, cert->valid_start().ToInternalValue());
-        if (!insert_statement.Run()) {
-          LOG(WARNING) << "Unable to update channel id database to "
-                       << "version 6.";
-          return false;
-        }
-      } else {
-        // If there's a cert we can't parse, just leave it.  It'll get replaced
-        // with a new one if we ever try to use it.
-        LOG(WARNING) << "Error parsing cert for database upgrade for origin "
-                     << statement.ColumnString(0);
-      }
-    }
-  } else if (cur_version == 5) {
-    sql::Statement select(
-        db_->GetUniqueStatement("SELECT host, private_key FROM channel_id"));
-    sql::Statement update(
-        db_->GetUniqueStatement("UPDATE channel_id SET private_key = ?, "
-                                "public_key = \"\" WHERE host = ?"));
-    if (!select.is_valid() || !update.is_valid()) {
-      LOG(WARNING) << "Invalid SQL statements to update Channel ID database to "
-                      "version 6.";
-      return false;
-    }
-
-    while (select.Step()) {
-      std::string host = select.ColumnString(0);
-      std::vector<uint8_t> encrypted_private_key, private_key;
-      select.ColumnBlobAsVector(1, &encrypted_private_key);
-      std::unique_ptr<crypto::ECPrivateKey> key(
-          crypto::ECPrivateKey::CreateFromEncryptedPrivateKeyInfo(
-              encrypted_private_key, std::vector<uint8_t>()));
-      if (!key || !key->ExportPrivateKey(&private_key)) {
-        LOG(WARNING) << "Unable to parse encrypted private key when migrating "
-                        "Channel ID database to version 6.";
-        continue;
-      }
-      update.Reset(true);
-      update.BindBlob(0, private_key.data(),
-                      static_cast<int>(private_key.size()));
-      update.BindString(1, host);
-      if (!update.Run()) {
-        LOG(WARNING) << "UPDATE statement failed when updating Channel ID "
-                        "database to version 6.";
-        return false;
-      }
-    }
-  }
-
   if (cur_version < kCurrentVersionNumber) {
     if (cur_version <= 4) {
       sql::Statement statement(
           db_->GetUniqueStatement("DROP TABLE origin_bound_certs"));
       if (!statement.Run()) {
         LOG(WARNING) << "Error dropping old origin_bound_certs table";
+        return false;
+      }
+    }
+    if (cur_version < 6) {
+      // The old format had the private_key column in a format we no longer
+      // read. Just delete any entries in that format.
+      sql::Statement statement(
+          db_->GetUniqueStatement("DELETE FROM channel_id"));
+      if (!statement.Run()) {
+        LOG(WARNING) << "Error clearing channel_id table";
         return false;
       }
     }
