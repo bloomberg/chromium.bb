2ff63aa536c6e17d0e802a19a790d437d6310308
Chris Blume <cblume@google.com>
2017-09-19 23:07:57 +0000

    Zlib: Use defines for inffast
    Zlib's inffast uses a handful of magic numbers. It would be easier to
    read and maintain if these magic numbers were instead #defines.
    
    NigelTao@ wrote this code to clean up the magic numbers in
    https://chromium-review.googlesource.com/c/chromium/src/+/601694
    
    Nigel gave me permission to make a separate pull request to separate out
    just the magic number cleaning.
    
    BUG=764431
    
    Change-Id: I0a62e31e98d4f3afcc64bd096e62a4b4b175644b
    Reviewed-on: https://chromium-review.googlesource.com/663424
    Commit-Queue: Chris Blume <cblume@google.com>
    Reviewed-by: Adenilson Cavalcanti <cavalcantii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502982}
    Former-commit-id: 5d7c2b78db0e96c769dee8d4ed3a9aeac8ef21d5

--- third_party/zlib/inflate.c
+++ third_party/zlib/inflate.c
@@ -1043,7 +1043,8 @@ int flush;
         case LEN_:
             state->mode = LEN;
         case LEN:
-            if (have >= 6 && left >= 258) {
+            if (have >= INFLATE_FAST_MIN_HAVE &&
+                left >= INFLATE_FAST_MIN_LEFT) {
                 RESTORE();
                 inflate_fast(strm, out);
                 LOAD();
