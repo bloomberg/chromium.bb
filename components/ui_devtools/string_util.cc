c7c8f2c06b859e863eeaef89c16a480ed9550639
Pavel Feldman <pfeldman@chromium.org>
2017-09-19 19:58:58 +0000

    DevTools: escape protocol strings that are originally utf8.
    Bug: 765506
    Change-Id: I16fbe56180f96eb1daf504510a6d07965e226622
    TBR: (api is changing)
    Reviewed-on: https://chromium-review.googlesource.com/667804
    Commit-Queue: Pavel Feldman <pfeldman@chromium.org>
    Reviewed-by: Dmitry Gozman <dgozman@chromium.org>
    Reviewed-by: Aleksey Kozyatinskiy <kozyatinskiy@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#502929}
    Former-commit-id: 74e192b49bfceb69da7ccf1efed1a2d7c07a9e83

--- components/ui_devtools/string_util.cc
+++ components/ui_devtools/string_util.cc
@@ -5,6 +5,7 @@
 #include "components/ui_devtools/string_util.h"
 
 #include "base/strings/string_util.h"
+#include "base/strings/utf_string_conversions.h"
 #include "components/ui_devtools/Protocol.h"
 
 namespace ui_devtools {
@@ -17,6 +18,16 @@ std::unique_ptr<Value> StringUtil::parseJSON(const String& string) {
   return parseJSONCharacters(reinterpret_cast<const uint8_t*>(&string[0]),
                              string.length());
 };
+
+// static
+void StringUtil::builderAppendQuotedString(StringBuilder& builder,
+                                           const String& str) {
+  builder.append('"');
+  base::string16 str16 = base::UTF8ToUTF16(str);
+  escapeWideStringForJSON(reinterpret_cast<const uint16_t*>(&str16[0]),
+                          str16.length(), &builder);
+  builder.append('"');
+}
 
 }  // namespace protocol
 }  // namespace ui_devtools
