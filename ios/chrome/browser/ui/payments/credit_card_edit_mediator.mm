c4b66476ed2e22d6c7927aee3086247e1fb25e2e
Mohamad Ahmadi <mahmadi@chromium.org>
2017-09-20 16:17:14 +0000

    [Payment Request] Always set credit card editor's picker view options.
    The credit card editor's picker view options were not being set after a
    a billing address was selected/created. Therefore the picker view for the
    expiration date field was not appearing any longer.
    
    Bug: 767038
    Change-Id: Iaf417e74085353f24f231131e51225fe3aacca6c
    Reviewed-on: https://chromium-review.googlesource.com/675463
    Commit-Queue: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Marc-Antoine Courteau <macourteau@chromium.org>
    Reviewed-by: Marc-Antoine Courteau <macourteau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503172}
    Former-commit-id: e0039cc3036950ec2efb260434c09d995c7a8084

--- ios/chrome/browser/ui/payments/credit_card_edit_mediator.mm
+++ ios/chrome/browser/ui/payments/credit_card_edit_mediator.mm
@@ -93,13 +93,13 @@ using ::payment_request_util::GetBillingAddressLabelFromAutofillProfile;
   _consumer = consumer;
 
   [self.consumer setEditorFields:[self createEditorFields]];
-  if (self.creditCardExpDateField)
-    [self loadMonthsAndYears];
+  [self loadMonthsAndYears];
 }
 
 - (void)setBillingProfile:(autofill::AutofillProfile*)billingProfile {
   _billingProfile = billingProfile;
   [self.consumer setEditorFields:[self createEditorFields]];
+  [self loadMonthsAndYears];
 }
 
 #pragma mark - PaymentRequestEditViewControllerDataSource
@@ -217,6 +217,7 @@ using ::payment_request_util::GetBillingAddressLabelFromAutofillProfile;
                 atIndex:0];
   }
 
+  DCHECK(self.creditCardExpDateField);
   // Notify the view controller asynchronously to allow for the view to update.
   __weak CreditCardEditViewControllerMediator* weakSelf = self;
   dispatch_async(dispatch_get_main_queue(), ^{
