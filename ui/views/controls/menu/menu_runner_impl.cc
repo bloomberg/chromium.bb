f6d805d9bf3f357c4d1e44a55302f6ca11c0625a
Tim Brown <timbrown@chromium.org>
2017-09-20 23:44:18 +0000

    Linux: Show mnemonics when menu opened w/ keyboard
    Specifically, if the Alt key is being held when the window is opened,
    the mnemonics are shown (F10 is already covered). This matches the
    behavior on Windows.
    
    Bug: 381961
    Test: Press Alt+f (or Alt+e) and see if menu entries have underlines
    Change-Id: If3a2de62e3b53c880e7416be111b62babd78a8f4
    Reviewed-on: https://chromium-review.googlesource.com/670144
    Reviewed-by: Sadrul Chowdhury <sadrul@chromium.org>
    Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    Commit-Queue: Tim Brown <timbrown@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503282}
    Former-commit-id: 2b6f9d465a69cd5a4b8123a8d8d86e45df10f0a0

--- ui/views/controls/menu/menu_runner_impl.cc
+++ ui/views/controls/menu/menu_runner_impl.cc
@@ -17,6 +17,10 @@
 #include "ui/events/win/system_event_state_lookup.h"
 #endif
 
+#if defined(USE_X11)
+#include "ui/events/x/events_x_utils.h"  // nogncheck
+#endif
+
 namespace views {
 namespace internal {
 
@@ -126,8 +130,7 @@ void MenuRunnerImpl::RunMenuAt(Widget* parent,
       (run_types & MenuRunner::SEND_GESTURE_EVENTS_TO_OWNER) != 0);
   controller_ = controller->AsWeakPtr();
   menu_->set_controller(controller_.get());
-  menu_->PrepareForRun(owns_controller_,
-                       has_mnemonics,
+  menu_->PrepareForRun(owns_controller_, has_mnemonics,
                        !for_drop_ && ShouldShowMnemonics(button));
 
   controller->Run(parent, button, menu_, bounds, anchor,
@@ -196,9 +199,9 @@ bool MenuRunnerImpl::ShouldShowMnemonics(MenuButton* button) {
   // Show mnemonics if the button has focus or alt is pressed.
   bool show_mnemonics = button ? button->HasFocus() : false;
 #if defined(OS_WIN)
-  // This is only needed on Windows.
-  if (!show_mnemonics)
-    show_mnemonics = ui::win::IsAltPressed();
+  show_mnemonics |= ui::win::IsAltPressed();
+#elif defined(USE_X11)
+  show_mnemonics |= ui::IsAltPressed();
 #endif
   return show_mnemonics;
 }
