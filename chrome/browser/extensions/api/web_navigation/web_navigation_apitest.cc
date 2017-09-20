3ff828bbbb77cdd8c85f299040a5d79bb3243158
Vitalii Iarko <vitaliii@chromium.org>
2017-09-20 14:00:50 +0000

    Disable Flaky WebNavigationApiTest.Failures on Linux.
    TBR: nasko@chromium.org
    Bug: 746407
    Change-Id: Ib3a12b7d7f1017ac8dd9add813b04ab76dee22b5
    Reviewed-on: https://chromium-review.googlesource.com/674696
    Commit-Queue: vitaliii <vitaliii@chromium.org>
    Reviewed-by: vitaliii <vitaliii@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503127}
    Former-commit-id: 05deaee91a7382f9f7d31938568fed78b1c1f3f9

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -478,9 +478,9 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, SimpleLoad) {
   ASSERT_TRUE(RunExtensionTest("webnavigation/simpleLoad")) << message_;
 }
 
-// Flaky on Windows and Mac. See http://crbug.com/477480 (Windows) and
-// https://crbug.com/746407 (Mac).
-#if defined(OS_WIN) || defined(OS_MACOSX)
+// Flaky on Windows, Mac and Linux. See http://crbug.com/477480 (Windows) and
+// https://crbug.com/746407 (Mac, Linux).
+#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_LINUX)
 #define MAYBE_Failures DISABLED_Failures
 #else
 #define MAYBE_Failures Failures
