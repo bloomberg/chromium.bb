214abda0be94f60761adfcd23938409b0306a368
edchin <edchin@chromium.org>
2017-09-20 22:36:28 +0000

    [ios] Fix threading issue in tab_coordinator_unittest
    Code in chrome_browser_state.mm relies on named threads. This CL
    replaces use of base::test::ScopedTaskEnvironment in
    BrowserCoordinatorTest class with web::TestWebThreadBundle
    so that named threads can be used by legacy code.
    
    Bug: 759761
    Change-Id: Ib4769ccdf66c0270e1f289dd5db2d78a5a9974f3
    Reviewed-on: https://chromium-review.googlesource.com/674383
    Commit-Queue: edchin <edchin@chromium.org>
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503273}
    Former-commit-id: 5fd1d5bcaa1f6fc95c1b409b439a237f50544ed7

--- ios/chrome/browser/ui/coordinators/browser_coordinator_test.h
+++ ios/chrome/browser/ui/coordinators/browser_coordinator_test.h
@@ -7,7 +7,7 @@
 
 #include <memory>
 
-#include "base/test/scoped_task_environment.h"
+#include "ios/web/public/test/test_web_thread_bundle.h"
 #include "testing/platform_test.h"
 
 class Browser;
@@ -22,7 +22,7 @@ class BrowserCoordinatorTest : public PlatformTest {
   Browser* GetBrowser() { return browser_.get(); }
 
  private:
-  base::test::ScopedTaskEnvironment task_environment_;
+  web::TestWebThreadBundle thread_bundle_;
   std::unique_ptr<TestChromeBrowserState> chrome_browser_state_;
   std::unique_ptr<WebStateListDelegate> delegate_;
   std::unique_ptr<Browser> browser_;
