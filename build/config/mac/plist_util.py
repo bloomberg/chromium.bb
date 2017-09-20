c3d6648b83f48b7ac375877da58591275235777a
Sylvain Defresne <sdefresne@chromium.org>
2017-09-20 14:06:09 +0000

    Add support for partial Info.plist from actool.
    Change ios_app_bundle template to merge the generated Info.plist
    with the partial .plist created by the asset catalog compiler to
    support compiling application icon to Assets.car as is required
    for application built with iOS 11.0 SDK.
    
    Quote args of compile_xcasset.py in build/toolchain/mac/BUILD.gn
    as some of them may be empty or contain shell special characters
    (mostly spaces).
    
    Bug: 764286
    Change-Id: I453f3cbb34c82483bace9900c49e0f3b351b2635
    Reviewed-on: https://chromium-review.googlesource.com/671050
    Commit-Queue: Sylvain Defresne <sdefresne@chromium.org>
    Reviewed-by: Scott Graham <scottmg@chromium.org>
    Reviewed-by: Robert Sesek <rsesek@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503129}
    Former-commit-id: d73f3454a10aa2d20f50f1b145148380649e7849

--- build/config/mac/plist_util.py
+++ build/config/mac/plist_util.py
@@ -96,6 +96,12 @@ def SavePList(path, format, data):
   """Saves |data| as a Plist to |path| in the specified |format|."""
   fd, name = tempfile.mkstemp()
   try:
+    # "plutil" does not replace the destination file but update it in place,
+    # so if more than one hardlink points to destination all of them will be
+    # modified. This is not what is expected, so delete destination file if
+    # it does exist.
+    if os.path.exists(path):
+      os.unlink(path)
     with os.fdopen(fd, 'w') as f:
       plistlib.writePlist(data, f)
     subprocess.check_call(['plutil', '-convert', format, '-o', path, name])
