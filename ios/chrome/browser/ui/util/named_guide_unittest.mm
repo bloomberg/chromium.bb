df2735b25f2561cf1cc876be161d57ab9ec1d2c5
Rohit Rao <rohitrao@google.com>
2017-09-20 12:55:41 +0000

    [ios] Adds named layout guides.
    Adds a framework through which a small set of UILayoutGuides can be exposed for
    use across the app.
    
    BUG=None
    
    Change-Id: I18f23d69d77865206d9e189f96186f392797b4c9
    Reviewed-on: https://chromium-review.googlesource.com/673090
    Commit-Queue: Rohit Rao (ping after 24h) <rohitrao@chromium.org>
    Reviewed-by: Mark Cogan <marq@chromium.org>
    Reviewed-by: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503121}
    Former-commit-id: beb7c6a2a02392542bc3d5507986bfc9ecfd498d

---
+++ ios/chrome/browser/ui/util/named_guide_unittest.mm
@@ -0,0 +1,56 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "ios/chrome/browser/ui/util/named_guide.h"
+
+#include "base/logging.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#if !defined(__has_feature) || !__has_feature(objc_arc)
+#error "This file requires ARC support."
+#endif
+
+// Tests that guides are reachable after being added to a view.
+TEST(NamedGuideTest, TestAddAndFind) {
+  GuideName* test_guide = @"NamedGuideTest";
+
+  UIView* view = [[UIView alloc] init];
+  EXPECT_EQ(nil, FindNamedGuide(test_guide, view));
+
+  // The test_guide should be reachable after adding it.
+  EXPECT_NE(nil, AddNamedGuide(test_guide, view));
+  EXPECT_NE(nil, FindNamedGuide(test_guide, view));
+}
+
+// Tests that guides added to a child view are not reachable from the parent.
+TEST(NamedGuideTest, TestGuideOnChild) {
+  GuideName* test_guide = @"NamedGuideTest";
+
+  UIView* view = [[UIView alloc] init];
+  UIView* childView = [[UIView alloc] init];
+  [view addSubview:childView];
+
+  EXPECT_TRUE(AddNamedGuide(test_guide, childView));
+  // This guide should be reachable from the child, but not from the parent.
+  EXPECT_TRUE(FindNamedGuide(test_guide, childView));
+  EXPECT_FALSE(FindNamedGuide(test_guide, view));
+}
+
+// Tests that children can reach guides that are added to ancestors.
+TEST(NamedGuideTest, TestGuideOnAncestor) {
+  GuideName* test_guide = @"NamedGuideTest";
+
+  UIView* view = [[UIView alloc] init];
+  UIView* childView = [[UIView alloc] init];
+  UIView* grandChildView = [[UIView alloc] init];
+  [view addSubview:childView];
+  [childView addSubview:grandChildView];
+
+  // Add a guide to the parent view and ensure that it is reachable from all
+  // three views.
+  EXPECT_TRUE(AddNamedGuide(test_guide, view));
+  EXPECT_TRUE(FindNamedGuide(test_guide, grandChildView));
+  EXPECT_TRUE(FindNamedGuide(test_guide, childView));
+  EXPECT_TRUE(FindNamedGuide(test_guide, view));
+}
