085a1e14031288d2c906b31376c9d7b888761ef1
Aaron Leventhal <aleventhal@chromium.org>
2017-09-21 03:04:18 +0000

    Remove unused method AriaLabelledbyElements
    Unlike its peers AriaOwnsElements and AriaDescribedbyElements, which are used by InspectorAccessibilityAgent.cpp, this one is unused.
    
    Bug: None
    Change-Id: I2109cfbacc1e2be5e66f0afe24220c6e455402ae
    Reviewed-on: https://chromium-review.googlesource.com/675045
    Reviewed-by: Alice Boxhall <aboxhall@chromium.org>
    Commit-Queue: Aaron Leventhal <aleventhal@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503342}
    Former-commit-id: 0ee1e46aa8d9712ae2f69cacb69e68a0e3528079

--- third_party/WebKit/Source/modules/accessibility/AXObject.h
+++ third_party/WebKit/Source/modules/accessibility/AXObject.h
@@ -644,7 +644,6 @@ class MODULES_EXPORT AXObject : public GarbageCollectedFinalized<AXObject> {
   virtual String AriaAutoComplete() const { return String(); }
   virtual void AriaOwnsElements(AXObjectVector& owns) const {}
   virtual void AriaDescribedbyElements(AXObjectVector&) const {}
-  virtual void AriaLabelledbyElements(AXObjectVector&) const {}
   virtual bool AriaHasPopup() const { return false; }
   virtual bool IsEditable() const { return false; }
   bool IsEditableRoot() const;
