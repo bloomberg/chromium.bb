85b2d1a3317c1d294b259e104715df3857ed1431
Yuzhu Shen <yzshen@chromium.org>
2017-09-21 20:18:20 +0000

    Remove some debugging CHECKs.
    TrackedScopedRefPtr has serveral CHECKs for investigating a memroy
    corruption issue. They haven't been triggered after 3 days on Canary.
    Therefore, this CL removes some of them. The remaining ones are not
    expected to be hit either, but they are left in the code to run on more
    users's machines to be extra sure.
    
    BUG=750267,754946
    
    Change-Id: Icd7e6eaedde3c2a21ea4d89449185b144772b562
    Reviewed-on: https://chromium-review.googlesource.com/677491
    Reviewed-by: Ken Rockot <rockot@chromium.org>
    Commit-Queue: Yuzhu Shen <yzshen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503545}
    Former-commit-id: 5fc67636bc053f254c152913b85d845fcae1d525

--- mojo/public/cpp/bindings/tracked_scoped_refptr.h
+++ mojo/public/cpp/bindings/tracked_scoped_refptr.h
@@ -69,19 +69,7 @@ class TrackedScopedRefPtr {
     NullReason null_reason = null_reason_;
     base::debug::Alias(&null_reason);
 
-    // This address has been used to construct a TrackedScopedRefPtr.
-    CHECK(state_ == ALIVE || state_ == DESTROYED);
-
-    // If the previous check succeeds but this fails, it is likely to be
-    // use-after-free problem.
     CHECK_EQ(ALIVE, state_);
-
-    CHECK_NE(DEFAULT_INITIALIZED, null_reason_);
-    CHECK_NE(INITIALIZED_BY_ASSIGN, null_reason_);
-    CHECK_NE(INITIALIZED_BY_MOVE, null_reason_);
-    CHECK_NE(ASSIGNED, null_reason_);
-    CHECK_NE(MOVED_IN, null_reason_);
-    CHECK_NE(MOVED_OUT, null_reason_);
 
     CHECK_EQ(NOT_NULL, null_reason_);
     CHECK(ptr_);
