2824ce14d4fbbdc1035fedbe2a7b85e9661f5204
Khushal <khushalsagar@chromium.org>
2017-09-21 21:51:13 +0000

    blink: Invalidate cached uniqueID if the alpha type changes.
    Changing the alpha type of an image should generate a new uniqueID. Skia
    expects images with the same uniqueID to have a constant alpha type.
    
    R=bsalomon@chromium.org
    
    Bug: 765877
    Change-Id: Iba1196458fe523329e85776037767ab1411ba05d
    Reviewed-on: https://chromium-review.googlesource.com/675323
    Reviewed-by: Brian Salomon <bsalomon@chromium.org>
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Khushal <khushalsagar@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503573}
    Former-commit-id: ffff72ecc6485ac43475d6b41f338a635b9e9875

--- third_party/WebKit/Source/platform/graphics/FrameData.h
+++ third_party/WebKit/Source/platform/graphics/FrameData.h
@@ -56,6 +56,7 @@ struct FrameData {
   bool is_complete_ : 1;
   bool has_alpha_ : 1;
   size_t frame_bytes_;
+  SkAlphaType alpha_type_ = kUnknown_SkAlphaType;
   uint32_t sk_image_unique_id_ = SkiaPaintImageGenerator::kNeedNewImageUniqueID;
 };
 
