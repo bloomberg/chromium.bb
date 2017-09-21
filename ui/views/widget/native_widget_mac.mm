c1f3cedbea6f969afd18564e62f90f46ce18809f
jongkwon.lee <jongkwon.lee@navercorp.com>
2017-09-21 07:25:34 +0000

    Make acceptsFirstResponder returns NO
    BridgedContentView takes away focus from |web_contents| when it's
    clicked on mouse. So make it not accepts FirstResponder. Instead,
    make it first responder initially by NSWindow makeFirstResponder:.
    When other native view become first responder, it can again become
    first responder by it's views::View get focused due to 
    NativeWidgetMac::ClearNativeFocus().
    
    Bug: 766027
    Test: Covered by existing focus manager tests.
    Change-Id: I12c68139e7d42c91aa1bca1b3485064d67d6823e
    Reviewed-on: https://chromium-review.googlesource.com/670479
    Reviewed-by: Trent Apted <tapted@chromium.org>
    Commit-Queue: Trent Apted <tapted@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503380}
    Former-commit-id: d9355bda6a5270dc2103caaf3e2de4ff08aa3ac0

--- ui/views/widget/native_widget_mac.mm
+++ ui/views/widget/native_widget_mac.mm
@@ -132,10 +132,12 @@ void NativeWidgetMac::InitNativeWidget(const Widget::InitParams& params) {
 
   delegate_->OnNativeWidgetCreated(true);
 
-  bridge_->SetFocusManager(GetWidget()->GetFocusManager());
-
   DCHECK(GetWidget()->GetRootView());
   bridge_->SetRootView(GetWidget()->GetRootView());
+  if (auto* focus_manager = GetWidget()->GetFocusManager()) {
+    [window makeFirstResponder:bridge_->ns_view()];
+    bridge_->SetFocusManager(focus_manager);
+  }
 
   // "Infer" must be handled by ViewsDelegate::OnBeforeWidgetInit().
   DCHECK_NE(Widget::InitParams::INFER_OPACITY, params.opacity);
