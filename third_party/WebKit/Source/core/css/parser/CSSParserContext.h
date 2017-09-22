2e612d35157341f3cc5204a138a29c85f11065b6
Fernando Serboncini <fserb@chromium.org>
2017-09-22 03:51:15 +0000

    Creates a new CSSParserContext constructor for workers
    Bug: 722511
    Change-Id: I0fb1f4a027cc1c90d026583e6f3ab3ca03bb801b
    Reviewed-on: https://chromium-review.googlesource.com/677405
    Reviewed-by: Dominik Röttsches <drott@chromium.org>
    Commit-Queue: Fernando Serboncini <fserb@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503647}
    Former-commit-id: b1e6c858391b36c637648685cb87efe3dce18323

--- third_party/WebKit/Source/core/css/parser/CSSParserContext.h
+++ third_party/WebKit/Source/core/css/parser/CSSParserContext.h
@@ -55,6 +55,8 @@ class CORE_EXPORT CSSParserContext
       ReferrerPolicy referrer_policy_override,
       const WTF::TextEncoding& charset = WTF::TextEncoding(),
       SelectorProfile = kDynamicProfile);
+  // This is used for workers, where we don't have a document.
+  static CSSParserContext* Create(const ExecutionContext&);
 
   bool operator==(const CSSParserContext&) const;
   bool operator!=(const CSSParserContext& other) const {
