3c3f9bfda9199d4c603f12170c814197ade37a75
Kouhei Ueno <kouhei@chromium.org>
2017-09-25 10:03:23 +0000

    [ES6 modules] Implement DynamicModuleResolver
    This CL introduces DynamicModuleResolver, which implements
    "Runtime Semantics: HostImportModuleDynamically" interface in TC39 proposal:
    https://tc39.github.io/proposal-dynamic-import/#sec-hostimportmoduledynamically
    
    The implemented algorithm follows the spec proposal:
    https://github.com/whatwg/html/pull/3044
    
    Bug: 711706
    Change-Id: I847d70e902368b99721dca528404724cc170cc4a
    Reviewed-on: https://chromium-review.googlesource.com/623041
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504019}
    Former-commit-id: 180489ebe22b335487c03a99603b11c0e467d75f

---
+++ third_party/WebKit/Source/core/dom/DynamicModuleResolver.h
@@ -0,0 +1,47 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DynamicModuleResolver_h
+#define DynamicModuleResolver_h
+
+#include "core/CoreExport.h"
+#include "platform/heap/Handle.h"
+#include "platform/heap/Visitor.h"
+#include "platform/wtf/text/WTFString.h"
+
+namespace blink {
+
+class Modulator;
+class ReferrerScriptInfo;
+class ScriptPromiseResolver;
+
+// DynamicModuleResolver implements "Runtime Semantics:
+// HostImportModuleDynamically" per spec.
+// https://tc39.github.io/proposal-dynamic-import/#sec-hostimportmoduledynamically
+class CORE_EXPORT DynamicModuleResolver final
+    : public GarbageCollected<DynamicModuleResolver> {
+ public:
+  DECLARE_TRACE();
+
+  static DynamicModuleResolver* Create(Modulator* modulator) {
+    return new DynamicModuleResolver(modulator);
+  }
+
+  // Implements "HostImportModuleDynamically" semantics.
+  // Should be called w/ a valid V8 context.
+  void ResolveDynamically(const String& specifier,
+                          const String& referrer_url,
+                          const ReferrerScriptInfo& referrer_info,
+                          ScriptPromiseResolver*);
+
+ private:
+  explicit DynamicModuleResolver(Modulator* modulator)
+      : modulator_(modulator) {}
+
+  Member<Modulator> modulator_;
+};
+
+}  // namespace blink
+
+#endif  // DynamicModuleResolver_h
