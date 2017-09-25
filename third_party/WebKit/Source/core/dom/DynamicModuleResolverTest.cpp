738283acca2f2181150b00ec0422249b8c7bfa0f
Kouhei Ueno <kouhei@chromium.org>
2017-09-25 10:03:23 +0000

    [ES6 modules] Implement DynamicModuleResolver
    This CL introduces DynamicModuleResolver, which implements
    "Runtime Semantics: HostImportModuleDynamically" interface in TC39 proposal:
    https://tc39.github.io/proposal-dynamic-import/#sec-hostimportmoduledynamically
    
    The implemented algorithm follows the spec proposal:
    https://github.com/whatwg/html/pull/3044
    
    Bug: 711706
    Change-Id: I847d70e902368b99721dca528404724cc170cc4a
    Reviewed-on: https://chromium-review.googlesource.com/623041
    Commit-Queue: Kouhei Ueno <kouhei@chromium.org>
    Reviewed-by: Kinuko Yasuda <kinuko@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Reviewed-by: Yutaka Hirano <yhirano@chromium.org>
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504019}
    Former-commit-id: 180489ebe22b335487c03a99603b11c0e467d75f

---
+++ third_party/WebKit/Source/core/dom/DynamicModuleResolverTest.cpp
@@ -0,0 +1,304 @@
+// Copyright 2017 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "core/dom/DynamicModuleResolver.h"
+
+#include "bindings/core/v8/ReferrerScriptInfo.h"
+#include "bindings/core/v8/ScriptFunction.h"
+#include "bindings/core/v8/ScriptPromiseResolver.h"
+#include "bindings/core/v8/ScriptValue.h"
+#include "bindings/core/v8/V8BindingForCore.h"
+#include "bindings/core/v8/V8BindingForTesting.h"
+#include "core/dom/ModuleScript.h"
+#include "core/loader/modulescript/ModuleScriptFetchRequest.h"
+#include "core/testing/DummyModulator.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "v8/include/v8.h"
+
+namespace blink {
+
+namespace {
+
+constexpr const char* kTestReferrerURL = "https://example.com/referrer.js";
+constexpr const char* kTestDependencyURL = "https://example.com/dependency.js";
+
+class DynamicModuleResolverTestModulator final : public DummyModulator {
+ public:
+  explicit DynamicModuleResolverTestModulator(ScriptState* script_state)
+      : script_state_(script_state) {}
+  ~DynamicModuleResolverTestModulator() override = default;
+
+  void ResolveTreeFetch(ModuleScript* module_script) {
+    ASSERT_TRUE(pending_client_);
+    pending_client_->NotifyModuleTreeLoadFinished(module_script);
+    pending_client_ = nullptr;
+  }
+
+  DECLARE_TRACE();
+
+ private:
+  // Implements Modulator:
+  ScriptState* GetScriptState() final { return script_state_.Get(); }
+
+  ModuleScript* GetFetchedModuleScript(const KURL& url) final {
+    EXPECT_EQ(kTestReferrerURL, url.GetString());
+    ModuleScript* module_script = ModuleScript::CreateForTest(
+        this, ScriptModule(), url, "nonce", kParserInserted,
+        WebURLRequest::kFetchCredentialsModeOmit);
+    return module_script;
+  }
+
+  void FetchTree(const ModuleScriptFetchRequest& request,
+                 ModuleTreeClient* client) final {
+    EXPECT_EQ(kTestDependencyURL, request.Url().GetString());
+
+    pending_client_ = client;
+  }
+
+  ScriptValue ExecuteModule(const ModuleScript* module_script,
+                            CaptureEvalErrorFlag capture_error) final {
+    EXPECT_EQ(CaptureEvalErrorFlag::kCapture, capture_error);
+
+    ScriptState::Scope scope(script_state_.Get());
+    return module_script->Record().Evaluate(script_state_.Get(),
+                                            CaptureEvalErrorFlag::kCapture);
+  }
+
+  ScriptModuleState GetRecordStatus(ScriptModule script_module) final {
+    ScriptState::Scope scope(script_state_.Get());
+    return script_module.Status(script_state_.Get());
+  }
+
+  ScriptValue GetError(const ModuleScript* module_script) final {
+    ScriptState::Scope scope(script_state_.Get());
+    ScriptModule record = module_script->Record();
+    DCHECK(!record.IsNull());
+    return ScriptValue(script_state_.Get(),
+                       record.ErrorCompletion(script_state_.Get()));
+  }
+
+  RefPtr<ScriptState> script_state_;
+  Member<ModuleTreeClient> pending_client_;
+};
+
+DEFINE_TRACE(DynamicModuleResolverTestModulator) {
+  visitor->Trace(pending_client_);
+  DummyModulator::Trace(visitor);
+}
+
+// CaptureExportedStringFunction implements a javascript function
+// with a single argument of type module namespace.
+// CaptureExportedStringFunction captures the exported string value
+// from the module namespace as a blink::String, exposed via CapturedValue().
+class CaptureExportedStringFunction final : public ScriptFunction {
+ public:
+  CaptureExportedStringFunction(ScriptState* script_state,
+                                const String& export_name)
+      : ScriptFunction(script_state), export_name_(export_name) {}
+
+  v8::Local<v8::Function> Bind() { return BindToV8Function(); }
+  bool WasCalled() const { return was_called_; }
+  const String& CapturedValue() const { return captured_value_; }
+
+ private:
+  ScriptValue Call(ScriptValue value) override {
+    was_called_ = true;
+
+    v8::Isolate* isolate = GetScriptState()->GetIsolate();
+    v8::Local<v8::Context> context = GetScriptState()->GetContext();
+
+    v8::Local<v8::Object> module_namespace =
+        value.V8Value()->ToObject(context).ToLocalChecked();
+    v8::Local<v8::Value> exported_value =
+        module_namespace->Get(context, V8String(isolate, export_name_))
+            .ToLocalChecked();
+    captured_value_ = ToCoreString(exported_value->ToString());
+
+    return ScriptValue();
+  }
+
+  const String export_name_;
+  bool was_called_ = false;
+  String captured_value_;
+};
+
+// CaptureErrorFunction implements a javascript function which captures
+// name and error of the exception passed as its argument.
+class CaptureErrorFunction final : public ScriptFunction {
+ public:
+  explicit CaptureErrorFunction(ScriptState* script_state)
+      : ScriptFunction(script_state) {}
+
+  v8::Local<v8::Function> Bind() { return BindToV8Function(); }
+  bool WasCalled() const { return was_called_; }
+  const String& Name() const { return name_; }
+  const String& Message() const { return message_; }
+
+ private:
+  ScriptValue Call(ScriptValue value) override {
+    was_called_ = true;
+
+    v8::Isolate* isolate = GetScriptState()->GetIsolate();
+    v8::Local<v8::Context> context = GetScriptState()->GetContext();
+
+    v8::Local<v8::Object> error_object =
+        value.V8Value()->ToObject(context).ToLocalChecked();
+
+    v8::Local<v8::Value> name =
+        error_object->Get(context, V8String(isolate, "name")).ToLocalChecked();
+    name_ = ToCoreString(name->ToString());
+    v8::Local<v8::Value> message =
+        error_object->Get(context, V8String(isolate, "message"))
+            .ToLocalChecked();
+    message_ = ToCoreString(message->ToString());
+
+    return ScriptValue();
+  }
+
+  bool was_called_ = false;
+  String name_;
+  String message_;
+};
+
+class NotReached final : public ScriptFunction {
+ public:
+  static v8::Local<v8::Function> CreateFunction(ScriptState* script_state) {
+    auto not_reached = new NotReached(script_state);
+    return not_reached->BindToV8Function();
+  }
+
+ private:
+  explicit NotReached(ScriptState* script_state)
+      : ScriptFunction(script_state) {}
+
+  ScriptValue Call(ScriptValue) override {
+    ADD_FAILURE();
+    return ScriptValue();
+  }
+};
+
+}  // namespace
+
+TEST(DynamicModuleResolverTest, ResolveSuccess) {
+  V8TestingScope scope;
+  DynamicModuleResolverTestModulator* modulator =
+      new DynamicModuleResolverTestModulator(scope.GetScriptState());
+
+  auto promise_resolver = ScriptPromiseResolver::Create(scope.GetScriptState());
+  ScriptPromise promise = promise_resolver->Promise();
+
+  auto capture =
+      new CaptureExportedStringFunction(scope.GetScriptState(), "foo");
+  promise.Then(capture->Bind(),
+               NotReached::CreateFunction(scope.GetScriptState()));
+
+  auto resolver = DynamicModuleResolver::Create(modulator);
+  resolver->ResolveDynamically("./dependency.js", kTestReferrerURL,
+                               ReferrerScriptInfo(), promise_resolver);
+
+  v8::MicrotasksScope::PerformCheckpoint(scope.GetIsolate());
+  EXPECT_FALSE(capture->WasCalled());
+
+  KURL url(kParsedURLString, kTestDependencyURL);
+  ScriptModule record = ScriptModule::Compile(
+      scope.GetIsolate(), "export const foo = 'hello';", url.GetString(),
+      kSharableCrossOrigin, WebURLRequest::kFetchCredentialsModeOmit, "",
+      kParserInserted, TextPosition::MinimumPosition(), ASSERT_NO_EXCEPTION);
+  ModuleScript* module_script = ModuleScript::CreateForTest(
+      modulator, record, url, "nonce", kNotParserInserted,
+      WebURLRequest::kFetchCredentialsModeOmit);
+  record.Instantiate(scope.GetScriptState());
+  EXPECT_FALSE(module_script->IsErrored());
+  modulator->ResolveTreeFetch(module_script);
+
+  v8::MicrotasksScope::PerformCheckpoint(scope.GetIsolate());
+  EXPECT_TRUE(capture->WasCalled());
+  EXPECT_EQ("hello", capture->CapturedValue());
+}
+
+TEST(DynamicModuleResolverTest, ResolveSpecifierFailure) {
+  V8TestingScope scope;
+  auto modulator =
+      new DynamicModuleResolverTestModulator(scope.GetScriptState());
+
+  auto promise_resolver = ScriptPromiseResolver::Create(scope.GetScriptState());
+  ScriptPromise promise = promise_resolver->Promise();
+
+  auto capture = new CaptureErrorFunction(scope.GetScriptState());
+  promise.Then(NotReached::CreateFunction(scope.GetScriptState()),
+               capture->Bind());
+
+  auto resolver = DynamicModuleResolver::Create(modulator);
+  resolver->ResolveDynamically("invalid-specifier", kTestReferrerURL,
+                               ReferrerScriptInfo(), promise_resolver);
+
+  v8::MicrotasksScope::PerformCheckpoint(scope.GetIsolate());
+  EXPECT_TRUE(capture->WasCalled());
+  EXPECT_EQ("TypeError", capture->Name());
+  EXPECT_TRUE(capture->Message().StartsWith("Failed to resolve"));
+}
+
+TEST(DynamicModuleResolverTest, FetchFailure) {
+  V8TestingScope scope;
+  auto modulator =
+      new DynamicModuleResolverTestModulator(scope.GetScriptState());
+
+  auto promise_resolver = ScriptPromiseResolver::Create(scope.GetScriptState());
+  ScriptPromise promise = promise_resolver->Promise();
+
+  auto capture = new CaptureErrorFunction(scope.GetScriptState());
+  promise.Then(NotReached::CreateFunction(scope.GetScriptState()),
+               capture->Bind());
+
+  auto resolver = DynamicModuleResolver::Create(modulator);
+  resolver->ResolveDynamically("./dependency.js", kTestReferrerURL,
+                               ReferrerScriptInfo(), promise_resolver);
+
+  EXPECT_FALSE(capture->WasCalled());
+
+  modulator->ResolveTreeFetch(nullptr);
+
+  v8::MicrotasksScope::PerformCheckpoint(scope.GetIsolate());
+  EXPECT_TRUE(capture->WasCalled());
+  EXPECT_EQ("TypeError", capture->Name());
+  EXPECT_TRUE(capture->Message().StartsWith("Failed to fetch"));
+}
+
+TEST(DynamicModuleResolverTest, ExceptionThrown) {
+  V8TestingScope scope;
+  auto modulator =
+      new DynamicModuleResolverTestModulator(scope.GetScriptState());
+
+  auto promise_resolver = ScriptPromiseResolver::Create(scope.GetScriptState());
+  ScriptPromise promise = promise_resolver->Promise();
+
+  auto capture = new CaptureErrorFunction(scope.GetScriptState());
+  promise.Then(NotReached::CreateFunction(scope.GetScriptState()),
+               capture->Bind());
+
+  auto resolver = DynamicModuleResolver::Create(modulator);
+  resolver->ResolveDynamically("./dependency.js", kTestReferrerURL,
+                               ReferrerScriptInfo(), promise_resolver);
+
+  EXPECT_FALSE(capture->WasCalled());
+
+  KURL url(kParsedURLString, kTestDependencyURL);
+  ScriptModule record = ScriptModule::Compile(
+      scope.GetIsolate(), "throw Error('bar')", url.GetString(),
+      kSharableCrossOrigin, WebURLRequest::kFetchCredentialsModeOmit, "",
+      kParserInserted, TextPosition::MinimumPosition(), ASSERT_NO_EXCEPTION);
+  ModuleScript* module_script = ModuleScript::CreateForTest(
+      modulator, record, url, "nonce", kNotParserInserted,
+      WebURLRequest::kFetchCredentialsModeOmit);
+  record.Instantiate(scope.GetScriptState());
+  EXPECT_FALSE(module_script->IsErrored());
+  modulator->ResolveTreeFetch(module_script);
+
+  v8::MicrotasksScope::PerformCheckpoint(scope.GetIsolate());
+  EXPECT_TRUE(capture->WasCalled());
+  EXPECT_EQ("Error", capture->Name());
+  EXPECT_EQ("bar", capture->Message());
+}
+
+}  // namespace blink
