5ee1988e9299e2372440b96e30a351bbc56a789b
Claudio Magni <claudiomagni@chromium.org>
2017-09-25 06:52:57 +0000

    Remove automatic expansion of language list in Settings.
    This is part of a large change for Language Settings.
    This CL only fixes the automatic expansion of the language list, which adds the base language
    whenever a locale-specific one is added.
    The feature is behind a disabled flag.
    
    Bug: 640763, 765077
    Change-Id: Ie7d32c06bef0f933bf1e097a277ac063a9c2b54e
    Reviewed-on: https://chromium-review.googlesource.com/670459
    Commit-Queue: Claudio M <claudiomagni@chromium.org>
    Reviewed-by: Dave Schuyler <dschuyler@chromium.org>
    Reviewed-by: Hajime Hoshi <hajimehoshi@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#503993}
    Former-commit-id: 31f0d7fc389035ee8403ecc22759715ee98042de

--- components/translate/core/browser/translate_prefs.h
+++ components/translate/core/browser/translate_prefs.h
@@ -35,6 +35,10 @@ namespace translate {
 // Feature flag for "Translate UI 2016 Q2" project.
 extern const base::Feature kTranslateUI2016Q2;
 
+// Enables or disables the new improved language settings.
+// These settings support the new UI.
+extern const base::Feature kImprovedLanguageSettings;
+
 // The trial (study) name in finch study config.
 extern const char kTranslateUI2016Q2TrialName[];
 
