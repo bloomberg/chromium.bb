4e3048cce7f779534d772e3d0ee724b8e57cb5ca
Yuqian Li <liyuqian@google.com>
2017-09-25 17:58:33 +0000

    Add guard flag against Skia's small rect AA change
    Bug: 
    Change-Id: I386bf27564dc3a70c4b1e3a497f7c5f5077c67e5
    Reviewed-on: https://chromium-review.googlesource.com/681839
    Reviewed-by: Florin Malita <fmalita@chromium.org>
    Commit-Queue: Florin Malita <fmalita@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504101}
    Former-commit-id: f582672eaaa4201b3bed1cc402d22000c711da45

--- skia/config/SkUserConfig.h
+++ skia/config/SkUserConfig.h
@@ -212,6 +212,10 @@ SK_API void SkDebugf_FileLine(const char* file, int line, bool fatal,
 #define SK_SUPPORT_LEGACY_DELTA_AA
 #endif
 
+#ifndef SK_SUPPORT_LEGACY_SMALLRECT_AA
+#define SK_SUPPORT_LEGACY_SMALLRECT_AA
+#endif
+
 #ifndef SK_SUPPORT_LEGACY_TILED_BITMAPS
 #define SK_SUPPORT_LEGACY_TILED_BITMAPS
 #endif
