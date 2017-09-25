48d45abf6c5d2e0fa9be83df8215df1af4f7988c
rnephew <rnephew@chromium.org>
2017-09-25 18:54:02 +0000

    [Telemetry] Add presubmit check to ensure no one disables benchmarks with decorators.
    Bug: 713222
    Change-Id: I9d13330ef71bd6bfb27232e6472b35b404b19b0a
    Reviewed-on: https://chromium-review.googlesource.com/682109
    Commit-Queue: rnephew <rnephew@chromium.org>
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Cr-Commit-Position: refs/heads/master@{#504114}
    Former-commit-id: 45d54bf2978e4a04e29021e3295e9b04773a5d86

--- tools/perf/core/story_expectation_validator.py
+++ tools/perf/core/story_expectation_validator.py
@@ -10,6 +10,8 @@ from core import path_util
 path_util.AddTelemetryToPath()
 path_util.AddAndroidPylibToPath()
 
+
+from telemetry import decorators
 from telemetry.internal.browser import browser_options
 
 
@@ -20,6 +22,17 @@ CLUSTER_TELEMETRY_BENCHMARKS = [
     ct_benchmark.Name() for ct_benchmark in
     benchmark_finders.GetBenchmarksInSubDirectory(CLUSTER_TELEMETRY_DIR)
 ]
+
+
+# TODO(rnephew): Remove this check when it is the norm to not use decorators.
+def check_decorators(benchmarks):
+  for benchmark in benchmarks:
+    if (decorators.GetDisabledAttributes(benchmark) or
+        decorators.GetEnabledAttributes(benchmark)):
+      raise Exception(
+          'Disabling or enabling telemetry benchmark with decorator detected. '
+          'Please use StoryExpectations instead. Contact rnephew@ for more '
+          'information. \nBenchmark: %s' % benchmark.Name())
 
 
 def validate_story_names(benchmarks):
@@ -43,4 +56,5 @@ def main(args):
   del args  # unused
   benchmarks = benchmark_finders.GetAllBenchmarks()
   validate_story_names(benchmarks)
+  check_decorators(benchmarks)
   return 0
