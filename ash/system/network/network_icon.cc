328d35554bd0b565b3a4d57339d82b81121bf7d0
Sammie Quon <sammiequon@google.com>
2017-09-25 22:06:42 +0000

    a11y: Provide more wireless network information for ChromeVox.
    The wireless network icon on the system tray has some information about
    the signal strength which ChromeVox users miss out on.
    
    Add a accessibility string in addition to convey the signal strength to
    ChromeVox. The strings are weak, medium, strong and should match the
    icon as well as possible.
    
    Test: manual
    Bug: 747468
    Change-Id: Ib86ae9301a74342743fcf7e02aee874b18be4f15
    Reviewed-on: https://chromium-review.googlesource.com/678917
    Reviewed-by: Steven Bennetts <stevenjb@chromium.org>
    Commit-Queue: Sammie Quon <sammiequon@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504179}
    Former-commit-id: 5da5280ecf9e10c20a265ce6ac7fd86fc663373d

--- ash/system/network/network_icon.cc
+++ ash/system/network/network_icon.cc
@@ -1009,5 +1009,29 @@ void PurgeNetworkIconCache() {
   PurgeIconMap(ICON_TYPE_MENU_LIST, network_paths);
 }
 
+SignalStrength GetSignalStrengthForNetwork(
+    const chromeos::NetworkState* network) {
+  if (!network->Matches(NetworkTypePattern::Wireless()))
+    return SignalStrength::NOT_WIRELESS;
+
+  // Decide whether the signal is considered weak, medium or strong based on the
+  // strength index. Each signal strength corresponds to a bucket which
+  // attempted to be split evenly from |kNumNetworkImages| - 1. Remainders go
+  // first to the lowest bucket and then the second lowest bucket.
+  const int index = StrengthIndex(network->signal_strength());
+  const int seperations = kNumNetworkImages - 1;
+  const int bucket_size = seperations / 3;
+
+  const int weak_max = bucket_size + static_cast<int>(seperations % 3 != 0);
+  const int medium_max =
+      weak_max + bucket_size + static_cast<int>(seperations % 3 == 2);
+  if (index <= weak_max)
+    return SignalStrength::WEAK;
+  else if (index <= medium_max)
+    return SignalStrength::MEDIUM;
+
+  return SignalStrength::STRONG;
+}
+
 }  // namespace network_icon
 }  // namespace ash
