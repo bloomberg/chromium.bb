723165751eabf267b8b8e53b134c84d5d1275b1e
Tetsui Ohkubo <tetsui@chromium.org>
2017-09-26 01:41:32 +0000

    Fix rarely shown invalid ripple from notification header.
    After https://crrev.com/c/676963, in rare case, invalid ripple effect is
    shown when the header is clicked. http://screen/29NRt5CTEEm
    
    This CL fixes this by always returning stub InkDrop object.
    
    TEST=manual
    BUG=767246
    
    Change-Id: Ibb5902864474849e665c0e325f4c2b97a80a4d51
    Reviewed-on: https://chromium-review.googlesource.com/681096
    Reviewed-by: Yoshiki Iguchi <yoshiki@chromium.org>
    Commit-Queue: Tetsui Ohkubo <tetsui@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504253}
    Former-commit-id: d248001347734f74771543635bbece4c2a41443b

--- ui/message_center/views/notification_header_view.cc
+++ ui/message_center/views/notification_header_view.cc
@@ -18,6 +18,7 @@
 #include "ui/message_center/vector_icons.h"
 #include "ui/message_center/views/notification_control_buttons_view.h"
 #include "ui/strings/grit/ui_strings.h"
+#include "ui/views/animation/ink_drop_stub.h"
 #include "ui/views/border.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/image_view.h"
@@ -341,6 +342,10 @@ void NotificationHeaderView::SetAccentColor(SkColor color) {
 
 bool NotificationHeaderView::IsExpandButtonEnabled() {
   return expand_button_->visible();
+}
+
+std::unique_ptr<views::InkDrop> NotificationHeaderView::CreateInkDrop() {
+  return base::MakeUnique<views::InkDropStub>();
 }
 
 void NotificationHeaderView::UpdateSummaryTextVisibility() {
