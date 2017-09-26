22d085e5d6df4ce29a8782b33c88d2b3db101766
Hiroki Nakagawa <nhiroki@chromium.org>
2017-09-26 07:13:01 +0000

    Worklet: Fix styles in http/tests/worklet/
    This CL...
    - wraps sourcecodes at the 80th column.
    - uses modern JS features (arrow functions), and
    - removes the unused |opt_path| arg on runImportTests().
    
    Bug: n/a
    Change-Id: I8f73f76b9c3e11a835464796e095d4de155e3593
    Reviewed-on: https://chromium-review.googlesource.com/683776
    Commit-Queue: Hiroki Nakagawa <nhiroki@chromium.org>
    Reviewed-by: Matt Falkenhagen <falken@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504298}
    Former-commit-id: 6837b6bbff1e688106b838f1c7f85fdc724408fd

--- third_party/WebKit/LayoutTests/http/tests/worklet/resources/import-tests.js
+++ third_party/WebKit/LayoutTests/http/tests/worklet/resources/import-tests.js
@@ -2,58 +2,66 @@
 //
 // Usage:
 // runImportTests(workletType);
-function runImportTests(worklet, opt_path) {
-    const path = opt_path || '';
-
-    promise_test(function() {
-        return worklet.addModule(path + 'resources/empty-worklet-script.js').then(function(undefined_arg) {
-            assert_equals(undefined_arg, undefined, 'Promise should resolve with no arguments.');
+function runImportTests(worklet) {
+    promise_test(() => {
+        const kScriptURL = 'resources/empty-worklet-script.js';
+        return worklet.addModule(kScriptURL).then(undefined_arg => {
+            assert_equals(undefined_arg, undefined,
+                          'Promise should resolve with no arguments.');
         });
     }, 'Importing a script resolves the given promise.');
 
-    promise_test(function() {
+    promise_test(() => {
+        const kScriptURL = 'resources/empty-worklet-script.js';
         return Promise.all([
-            worklet.addModule(path + 'resources/empty-worklet-script.js?1'),
-            worklet.addModule(path + 'resources/empty-worklet-script.js?2'),
-            worklet.addModule(path + 'resources/empty-worklet-script.js?3')
-        ]).then(function(undefined_args) {
-            assert_array_equals(undefined_args, [undefined, undefined, undefined], 'Promise should resolve with no arguments.');
+            worklet.addModule(kScriptURL + '?1'),
+            worklet.addModule(kScriptURL + '?2'),
+            worklet.addModule(kScriptURL + '?3')
+        ]).then(undefined_args => {
+            assert_array_equals(undefined_args,
+                                [undefined, undefined, undefined],
+                                'Promise should resolve with no arguments.');
         });
     }, 'Importing scripts resolves all the given promises.');
 
-    promise_test(function() {
-        return worklet.addModule(path + 'resources/import-nested-worklet-script.js').then(function(undefined_arg) {
-            assert_equals(undefined_arg, undefined, 'Promise should resolve with no arguments.');
+    promise_test(() => {
+        const kScriptURL = 'resources/import-nested-worklet-script.js';
+        return worklet.addModule(kScriptURL).then(undefined_arg => {
+            assert_equals(undefined_arg, undefined,
+                          'Promise should resolve with no arguments.');
         });
     }, 'Importing nested scripts resolves the given promise');
 
-    promise_test(function() {
-        return worklet.addModule(path + 'resources/import-cyclic-worklet-script.js').then(function(undefined_arg) {
-            assert_equals(undefined_arg, undefined, 'Promise should resolve with no arguments.');
+    promise_test(() => {
+        const kScriptURL = 'resources/import-cyclic-worklet-script.js';
+        return worklet.addModule(kScriptURL).then(undefined_arg => {
+            assert_equals(undefined_arg, undefined,
+                          'Promise should resolve with no arguments.');
         });
     }, 'Importing cyclic scripts resolves the given promise');
 
-    promise_test(function() {
-        return worklet.addModule(path + 'resources/throwing-worklet-script.js').then(function(undefined_arg) {
-            assert_equals(undefined_arg, undefined, 'Promise should resolve with no arguments.');
+    promise_test(() => {
+        const kScriptURL = 'resources/throwing-worklet-script.js';
+        return worklet.addModule(kScriptURL).then(undefined_arg => {
+            assert_equals(undefined_arg, undefined,
+                          'Promise should resolve with no arguments.');
         });
-    }, 'Importing a script which throws should still resolve the given promise.');
+    }, 'Importing a script which throws should still resolve the given ' +
+       'promise.');
 
-    promise_test(function() {
-        return worklet.addModule(path + 'non-existent-worklet-script.js').then(function() {
-            assert_unreached('addModule should fail.');
-        }).catch(function(error) {
-            assert_equals(error.name, 'AbortError', 'error should be an AbortError.');
-        });
-    }, 'Importing a non-existent script rejects the given promise with an AbortError.');
+    promise_test(t => {
+        const kScriptURL = 'non-existent-worklet-script.js';
+        return promise_rejects(t, new DOMException('', 'AbortError'),
+                               worklet.addModule(kScriptURL));
+    }, 'Importing a non-existent script rejects the given promise with an ' +
+       'AbortError.');
 
-    promise_test(function() {
-        return worklet.addModule('http://invalid:123$').then(function() {
-            assert_unreached('addModule should fail.');
-        }).catch(function(error) {
-            assert_equals(error.name, 'SyntaxError', 'error should be a SyntaxError.');
-        });
-    }, 'Importing an invalid URL should reject the given promise with a SyntaxError.');
+    promise_test(t => {
+        const kInvalidScriptURL = 'http://invalid:123$'
+        return promise_rejects(t, new DOMException('', 'SyntaxError'),
+                               worklet.addModule(kInvalidScriptURL));
+    }, 'Importing an invalid URL should reject the given promise with a ' +
+       'SyntaxError.');
 
     promise_test(() => {
         const kBlob = new Blob([""], {type: 'text/javascript'});
@@ -63,13 +71,10 @@ function runImportTests(worklet, opt_path) {
         });
     }, 'Importing a blob URL should resolve the given promise.');
 
-    promise_test(() => {
+    promise_test(t => {
         const kFileURL = 'file:///empty-worklet-script.js';
-        return worklet.addModule(kFileURL).then(undefined_arg => {
-            assert_unreached('addModule should fail.');
-        }).catch(function(error) {
-            assert_equals(error.name, 'AbortError');
-        });
+        return promise_rejects(t, new DOMException('', 'AbortError'),
+                               worklet.addModule(kFileURL));
     }, 'Importing a file:// URL should reject the given promise.');
 
     promise_test(() => {
@@ -79,11 +84,9 @@ function runImportTests(worklet, opt_path) {
         });
     }, 'Importing a data URL should resolve the given promise.');
 
-    promise_test(() => {
-        return worklet.addModule('about:blank').then(undefined_arg => {
-            assert_unreached('addModule should fail.');
-        }).catch(function(error) {
-            assert_equals(error.name, 'AbortError');
-        });
+    promise_test(t => {
+        const kScriptURL = 'about:blank';
+        return promise_rejects(t, new DOMException('', 'AbortError'),
+                               worklet.addModule(kScriptURL));
     }, 'Importing about:blank should reject the given promise.');
 }
