2747f503a6c221d752923c4ff3083cf6e6a2d467
Mike West <mkwst@chromium.org>
2017-09-26 09:22:23 +0000

    Deprecate `<meta http-equiv="set-cookie" ...>`
    Intent to Deprecate: https://groups.google.com/a/chromium.org/forum/#!msg/blink-dev/0sJ8GUJO0Dw/iMmcXLIGBAAJ
    
    Bug: 767813
    Change-Id: I29868952df3e9c8d5cef85fa39c43a85d850b9e9
    Reviewed-on: https://chromium-review.googlesource.com/678723
    Commit-Queue: Mike West <mkwst@chromium.org>
    Reviewed-by: Eric Lawrence <elawrence@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504322}
    Former-commit-id: d3c852d2ffe2926f0d7d5fe395348fa0710d6513

--- third_party/WebKit/Source/core/loader/HttpEquiv.cpp
+++ third_party/WebKit/Source/core/loader/HttpEquiv.cpp
@@ -7,6 +7,7 @@
 #include "core/css/StyleEngine.h"
 #include "core/dom/Document.h"
 #include "core/dom/ScriptableDocumentParser.h"
+#include "core/frame/Deprecation.h"
 #include "core/frame/LocalFrame.h"
 #include "core/frame/UseCounter.h"
 #include "core/frame/csp/ContentSecurityPolicy.h"
@@ -120,12 +121,8 @@ void HttpEquiv::ProcessHttpEquivRefresh(Document& document,
 void HttpEquiv::ProcessHttpEquivSetCookie(Document& document,
                                           const AtomicString& content,
                                           Element* element) {
-  // FIXME: make setCookie work on XML documents too; e.g. in case of
-  // <html:meta.....>
-  if (!document.IsHTMLDocument())
-    return;
+  Deprecation::CountDeprecation(document, WebFeature::kMetaSetCookie);
 
-  UseCounter::Count(document, WebFeature::kMetaSetCookie);
   if (!document.GetContentSecurityPolicy()->AllowInlineScript(
           element, NullURL(), "", OrdinalNumber(), "",
           ContentSecurityPolicy::InlineType::kBlock,
@@ -134,8 +131,16 @@ void HttpEquiv::ProcessHttpEquivSetCookie(Document& document,
                       WebFeature::kMetaSetCookieWhenCSPBlocksInlineScript);
   }
 
-  // Exception (for sandboxed documents) ignored.
-  document.setCookie(content, IGNORE_EXCEPTION_FOR_TESTING);
+  if (!RuntimeEnabledFeatures::BlockMetaSetCookieEnabled()) {
+    // Exception (for sandboxed documents) ignored.
+    document.setCookie(content, IGNORE_EXCEPTION_FOR_TESTING);
+    return;
+  }
+
+  document.AddConsoleMessage(ConsoleMessage::Create(
+      kSecurityMessageSource, kErrorMessageLevel,
+      String::Format("Blocked setting the `%s` cookie from a `<meta>` tag.",
+                     content.Utf8().data())));
 }
 
 }  // namespace blink
