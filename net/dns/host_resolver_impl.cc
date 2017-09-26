bloomberg/chromium.bb

commit 0b26f20ae1367021af13c377812fdc87233114e0

Retain types used by UMA_HISTOGRAM_ENUMERATION
There will be a stricter type checking in UMA_HISTOGRAM_ENUMERATION:
if |boundary| is enum, |sample| must be an enum as well.

One common type of violation is unnecessary casting to int. This CL
fixes them by retaining the types.

Bug: 661401

TBR: bartfab@chromium.org,dtrainor@chromium.org,xiyuan@chromium.org,cpu@chromium.org,boliu@chromium.org,piman@chromium.org,msw@chromium.org,sdefresne@chromium.org,mmenke@chromium.org
Change-Id: Ib1294421896e73ba89a2c294022697686fee3199
Reviewed-on: https://chromium-review.googlesource.com/575380
Commit-Queue: Wei-Yin Chen (陳威尹) <wychen@chromium.org>
Reviewed-by: Alexei Svitkine (slow) <asvitkine@chromium.org>
Reviewed-by: Bo Liu <boliu@chromium.org>
Reviewed-by: Michael Wasserman <msw@chromium.org>
Reviewed-by: Matt Menke <mmenke@chromium.org>
Reviewed-by: Daniel Cheng <dcheng@chromium.org>
Cr-Commit-Position: refs/heads/master@{#488551}
Former-commit-id: 223326c134a0cbe5358bd317b2a51bb2c7a4fe67

net/dns/host_resolver_impl.cc
@@ -880,7 +880,7 @@ class HostResolverImpl::ProcTask
       RESOLVE_SPECULATIVE_FAIL,
       RESOLVE_MAX,  // Bounding value.
     };
-    int category = RESOLVE_MAX;  // Illegal value for later DCHECK only.
+    Category category = RESOLVE_MAX;  // Illegal value for later DCHECK only.
 
     base::TimeDelta duration = base::TimeTicks::Now() - start_time;
     if (error == OK) {

chrome/browser/android/metrics/launch_metrics.cc
@@ -99,8 +99,8 @@ static void RecordLaunch(JNIEnv* env,
   rappor::SampleDomainAndRegistryFromGURL(g_browser_process->rappor_service(),
                                           rappor_metric_source, url);
 
-  int action = standalone ? HOME_SCREEN_LAUNCH_STANDALONE
-                          : HOME_SCREEN_LAUNCH_SHORTCUT;
+  HomeScreenLaunch action =
+      standalone ? HOME_SCREEN_LAUNCH_STANDALONE : HOME_SCREEN_LAUNCH_SHORTCUT;
   std::string rappor_metric_action = standalone ? "Launch.HomeScreen.Standalone"
                                                 : "Launch.HomeScreen.Shortcut";
 

chrome/browser/captive_portal/captive_portal_service.cc
@@ -92,7 +92,7 @@ void RecordRepeatHistograms(CaptivePortalResult result,
   result_duration_histogram->AddTime(result_duration);
 }
 
-int GetHistogramEntryForDetectionResult(
+CaptivePortalDetectionResult GetHistogramEntryForDetectionResult(
     const captive_portal::CaptivePortalDetector::Results& results) {
   bool is_https = results.landing_url.SchemeIs("https");
   bool is_ip = results.landing_url.HostIsIPAddress();
@@ -119,7 +119,7 @@ int GetHistogramEntryForDetectionResult(
           DETECTION_RESULT_BEHIND_CAPTIVE_PORTAL;
     default:
       NOTREACHED();
-      return -1;
+      return DETECTION_RESULT_COUNT;
   }
 }
 

chrome/browser/chromeos/login/reauth_stats.cc
@@ -22,9 +22,13 @@ void RecordReauthReason(const AccountId& account_id, ReauthReason reason) {
 }
 
 void SendReauthReason(const AccountId& account_id) {
-  int reauth_reason;
-  if (user_manager::known_user::FindReauthReason(account_id, &reauth_reason) &&
-      static_cast<ReauthReason>(reauth_reason) != ReauthReason::NONE) {
+  int reauth_reason_int;
+  if (!user_manager::known_user::FindReauthReason(account_id,
+                                                  &reauth_reason_int)) {
+    return;
+  }
+  ReauthReason reauth_reason = static_cast<ReauthReason>(reauth_reason_int);
+  if (reauth_reason != ReauthReason::NONE) {
     UMA_HISTOGRAM_ENUMERATION("Login.ReauthReason", reauth_reason,
                               NUM_REAUTH_FLOW_REASONS);
     user_manager::known_user::UpdateReauthReason(

chrome/browser/chromeos/login/screens/reset_screen.cc
@@ -72,7 +72,8 @@ void ResetScreen::Show() {
   if (view_)
     view_->Show();
 
-  int dialog_type = -1;  // used by UMA metrics.
+  reset::DialogViewType dialog_type =
+      reset::DIALOG_VIEW_TYPE_SIZE;  // used by UMA metrics.
 
   ContextEditor context_editor = GetContextEditor();
 
@@ -96,7 +97,7 @@ void ResetScreen::Show() {
         weak_ptr_factory_.GetWeakPtr()));
   }
 
-  if (dialog_type >= 0) {
+  if (dialog_type < reset::DIALOG_VIEW_TYPE_SIZE) {
     UMA_HISTOGRAM_ENUMERATION("Reset.ChromeOS.PowerwashDialogShown",
                               dialog_type,
                               reset::DIALOG_VIEW_TYPE_SIZE);
@@ -204,9 +205,10 @@ void ResetScreen::OnToggleRollback() {
 }
 
 void ResetScreen::OnShowConfirm() {
-  int dialog_type = context_.GetBoolean(kContextKeyIsRollbackChecked) ?
-      reset::DIALOG_SHORTCUT_CONFIRMING_POWERWASH_AND_ROLLBACK :
-      reset::DIALOG_SHORTCUT_CONFIRMING_POWERWASH_ONLY;
+  reset::DialogViewType dialog_type =
+      context_.GetBoolean(kContextKeyIsRollbackChecked)
+          ? reset::DIALOG_SHORTCUT_CONFIRMING_POWERWASH_AND_ROLLBACK
+          : reset::DIALOG_SHORTCUT_CONFIRMING_POWERWASH_ONLY;
   UMA_HISTOGRAM_ENUMERATION(
       "Reset.ChromeOS.PowerwashDialogShown",
       dialog_type,
@@ -249,9 +251,9 @@ void ResetScreen::UpdateStatusChanged(
 // Invoked from call to CanRollbackCheck upon completion of the DBus call.
 void ResetScreen::OnRollbackCheck(bool can_rollback) {
   VLOG(1) << "Callback from CanRollbackCheck, result " << can_rollback;
-  int dialog_type = can_rollback ?
-      reset::DIALOG_SHORTCUT_OFFERING_ROLLBACK_AVAILABLE :
-      reset::DIALOG_SHORTCUT_OFFERING_ROLLBACK_UNAVAILABLE;
+  reset::DialogViewType dialog_type =
+      can_rollback ? reset::DIALOG_SHORTCUT_OFFERING_ROLLBACK_AVAILABLE
+                   : reset::DIALOG_SHORTCUT_OFFERING_ROLLBACK_UNAVAILABLE;
   UMA_HISTOGRAM_ENUMERATION("Reset.ChromeOS.PowerwashDialogShown",
                             dialog_type,
                             reset::DIALOG_VIEW_TYPE_SIZE);

chrome/browser/diagnostics/diagnostics_test.cc
@@ -59,18 +59,17 @@ void DiagnosticsTest::RecordOutcome(int outcome_code,
   additional_info_ = additional_info;
   result_ = result;
 #if defined(OS_CHROMEOS)  // Only collecting UMA stats on ChromeOS
+  DiagnosticsTestId id = static_cast<DiagnosticsTestId>(GetId());
   if (result_ == DiagnosticsModel::TEST_OK) {
     // Record individual test success.
-    RecordUMATestResult(static_cast<DiagnosticsTestId>(GetId()),
-                        RESULT_SUCCESS);
+    RecordUMATestResult(id, RESULT_SUCCESS);
   } else if (result_ == DiagnosticsModel::TEST_FAIL_CONTINUE ||
              result_ == DiagnosticsModel::TEST_FAIL_STOP) {
     // Record test failure in summary histogram.
-    UMA_HISTOGRAM_ENUMERATION(
-        "Diagnostics.TestFailures", GetId(), DIAGNOSTICS_TEST_ID_COUNT);
+    UMA_HISTOGRAM_ENUMERATION("Diagnostics.TestFailures", id,
+                              DIAGNOSTICS_TEST_ID_COUNT);
     // Record individual test failure.
-    RecordUMATestResult(static_cast<DiagnosticsTestId>(GetId()),
-                        RESULT_FAILURE);
+    RecordUMATestResult(id, RESULT_FAILURE);
   }
 #endif
 }

chrome/browser/policy/cloud/cloud_policy_invalidator.cc
@@ -410,7 +410,8 @@ bool CloudPolicyInvalidator::IsInvalidationExpired(int64_t version) {
   return invalidation_time < last_fetch_time;
 }
 
-int CloudPolicyInvalidator::GetPolicyRefreshMetric(bool policy_changed) {
+MetricPolicyRefresh CloudPolicyInvalidator::GetPolicyRefreshMetric(
+    bool policy_changed) {
   if (policy_changed) {
     if (invalid_)
       return METRIC_POLICY_REFRESH_INVALIDATED_CHANGED;
@@ -423,8 +424,9 @@ int CloudPolicyInvalidator::GetPolicyRefreshMetric(bool policy_changed) {
   return METRIC_POLICY_REFRESH_UNCHANGED;
 }
 
-int CloudPolicyInvalidator::GetInvalidationMetric(bool is_missing_payload,
-                                                  bool is_expired) {
+PolicyInvalidationType CloudPolicyInvalidator::GetInvalidationMetric(
+    bool is_missing_payload,
+    bool is_expired) {
   if (is_expired) {
     if (is_missing_payload)
       return POLICY_INVALIDATION_TYPE_NO_PAYLOAD_EXPIRED;

chrome/browser/policy/cloud/cloud_policy_invalidator.h
@@ -21,6 +21,7 @@
 #include "components/invalidation/public/invalidation_handler.h"
 #include "components/policy/core/common/cloud/cloud_policy_core.h"
 #include "components/policy/core/common/cloud/cloud_policy_store.h"
+#include "components/policy/core/common/cloud/enterprise_metrics.h"
 #include "components/policy/proto/device_management_backend.pb.h"
 #include "google/cacheinvalidation/include/types.h"
 
@@ -156,11 +157,12 @@ class CloudPolicyInvalidator : public syncer::InvalidationHandler,
 
   // Get the kMetricPolicyRefresh histogram metric which should be incremented
   // when a policy is stored.
-  int GetPolicyRefreshMetric(bool policy_changed);
+  MetricPolicyRefresh GetPolicyRefreshMetric(bool policy_changed);
 
   // Get the kMetricPolicyInvalidations histogram metric which should be
   // incremented when an invalidation is received.
-  int GetInvalidationMetric(bool is_missing_payload, bool is_expired);
+  PolicyInvalidationType GetInvalidationMetric(bool is_missing_payload,
+                                               bool is_expired);
 
   // Determine if invalidations have been enabled longer than the grace period.
   bool GetInvalidationsEnabled();

commit d8d783ad8be6ea63d0fcaf9b2f1769abd17aa784

Fix macOS non-component debug build
This broke in 0b26f20ae136 [formerly 223326c134a0].

Bug: 763398, 768630
Change-Id: Ie5e7eb7a7d3ea01a30b8a262db22931c2ed7d4d9
Reviewed-on: https://chromium-review.googlesource.com/684616
Reviewed-by: Matt Menke <mmenke@chromium.org>
Commit-Queue: Mark Mentovai <mark@chromium.org>
Cr-Commit-Position: refs/heads/master@{#504416}
Former-commit-id: e96c3116e28f97b03c4448120c8cb43a878ed173

net/dns/host_resolver_impl.cc
@@ -918,7 +918,8 @@ class HostResolverImpl::ProcTask
                                        std::abs(os_error),
                                        GetAllGetAddrinfoOSErrors());
     }
-    DCHECK_LT(category, static_cast<int>(RESOLVE_MAX));  // Be sure it was set.
+    DCHECK_LT(static_cast<int>(category),
+              static_cast<int>(RESOLVE_MAX));  // Be sure it was set.
 
     UMA_HISTOGRAM_ENUMERATION("DNS.ResolveCategory", category, RESOLVE_MAX);
   }