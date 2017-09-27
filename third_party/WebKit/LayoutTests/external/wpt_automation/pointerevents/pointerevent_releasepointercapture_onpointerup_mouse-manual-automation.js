11c4f64bdc8dade524c9963e04c770c7a12ec443
Rune Lillesveen <rune@opera.com>
2017-09-27 13:44:35 +0000

    Missing return of Promise in automated pointerevents test.
    Bug: 754222
    Change-Id: Ic0c906535182da26f13246248e34023a4a896b8e
    Reviewed-on: https://chromium-review.googlesource.com/686821
    Reviewed-by: Dave Tapuska <dtapuska@chromium.org>
    Commit-Queue: Rune Lillesveen <rune@opera.com>
    Cr-Commit-Position: refs/heads/master@{#504649}
    Former-commit-id: 4df121ac8463624c8af0a11e56605b1c5ee62165

--- third_party/WebKit/LayoutTests/external/wpt_automation/pointerevents/pointerevent_releasepointercapture_onpointerup_mouse-manual-automation.js
+++ third_party/WebKit/LayoutTests/external/wpt_automation/pointerevents/pointerevent_releasepointercapture_onpointerup_mouse-manual-automation.js
@@ -3,7 +3,7 @@ importAutomationScript('/pointerevents/pointerevent_common_input.js');
 function inject_input() {
   return mouseClickInTarget('#btnCapture').then(function() {
     // To Handle delayed capturing.
-    mouseMoveIntoTarget('#btnCapture');
+    return mouseMoveIntoTarget('#btnCapture');
   });
 }
 
