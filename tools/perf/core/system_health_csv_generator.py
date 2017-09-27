7dbbcdf925c78d8bdf03934ac57e02cc6889cc3f
Lori Oliver <loridoesitbest@gmail.com>
2017-09-27 18:07:48 +0000

    Add disabled platforms column to go/csh-stories
    - PopulateExpectations accepts a dictionary:
        all_expectations = {story_name: [[conditions], reason]}
      Returns disables dictionary:
        disables = {story_name: "Disabled Platforms"}
    
    - generate_system_health_csv_unittest.py tests the new
    PopulateExpectations function.
    
    Bug: chromium:723636
    Change-Id: Ic94b08f53c004bab5aa813399da34dd7d4b09279
    Reviewed-on: https://chromium-review.googlesource.com/674099
    Reviewed-by: Ned Nguyen <nednguyen@google.com>
    Commit-Queue: Lori Oliver <loloangela@google.com>
    Cr-Commit-Position: refs/heads/master@{#504729}
    Former-commit-id: 4ced119ff5eb82dd557015f53437fe96ed6924ad

--- tools/perf/core/system_health_csv_generator.py
+++ tools/perf/core/system_health_csv_generator.py
@@ -3,8 +3,13 @@
 # found in the LICENSE file.
 
 import csv
-import page_sets
+import sys
 
+from core import path_util
+sys.path.insert(1, path_util.GetPerfDir())  # To resolve perf imports
+sys.path.insert(1, path_util.GetTelemetryDir()) # To resolve telemetry imports
+import page_sets
+from page_sets.system_health import expectations
 
 def IterAllSystemHealthStories():
   for s in page_sets.SystemHealthStorySet(platform='desktop'):
@@ -14,18 +19,57 @@ def IterAllSystemHealthStories():
       yield s
 
 
+def PopulateExpectations(all_expectations):
+  """Accepts Expectations and parses out the storyname and disabled platforms.
+
+  Args:
+    all_expectations = {
+        story_name: [[conditions], reason]}
+    conditions: list of disabled platforms for story_name
+    reason: Bug referencing why the test is disabled on the platform
+
+  Returns:
+    A dictionary containing the disabled platforms for each story.
+    disables = {
+        story_name: "Disabled Platforms"}
+  """
+  disables = {}
+  for exp in all_expectations:
+    exp_keys = exp.keys()
+    exp_keys.sort()
+
+    for story in exp_keys:
+      for conditions, _ in exp[story]:
+        conditions_str = ", ".join(map(str, conditions))
+        if story in disables:
+          if conditions_str not in disables[story]:
+            disables[story] += ", " + conditions_str
+        else:
+          disables[story] = conditions_str
+  return disables
+
 def GenerateSystemHealthCSV(file_path):
   system_health_stories = list(IterAllSystemHealthStories())
+  all_expectations = [
+      expectations.SystemHealthDesktopCommonExpectations().AsDict()['stories'],
+      expectations.SystemHealthDesktopMemoryExpectations().AsDict()['stories'],
+      expectations.SystemHealthMobileCommonExpectations().AsDict()['stories'],
+      expectations.SystemHealthMobileMemoryExpectations().AsDict()['stories'],]
+  disabed_platforms = PopulateExpectations(all_expectations)
   system_health_stories.sort(key=lambda s: s.name)
   with open(file_path, 'w') as f:
     csv_writer = csv.writer(f)
     csv_writer.writerow([
-        'Story name', 'Platform', 'Description'])
+        'Story name', 'Platform', 'Description', 'Disabled Platforms'])
     for s in system_health_stories:
       p = s.SUPPORTED_PLATFORMS
       if len(p) == 2:
         p = 'all'
       else:
         p = list(p)[0]
-      csv_writer.writerow([s.name, p, s.GetStoryDescription()])
+      if s.name in disabed_platforms:
+        csv_writer.writerow(
+            [s.name, p, s.GetStoryDescription(), disabed_platforms[s.name]])
+      else:
+        csv_writer.writerow([s.name, p, s.GetStoryDescription(), " "])
   return 0
