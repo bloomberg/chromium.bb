bloomberg/chromium.bb

commit f4db6fa6f45e662a7383a5b8d5402eb287149e4c

Support Opus Ambisonics playback
Opus Ambisonics does not have a prescribed layout, so CHANNEL_LAYOUT_DISCRETE is used. In this case, the audio decoders and renderer also need to know the number of channels.

BUG=693745

Review-Url: https://codereview.chromium.org/2752323002
Cr-Commit-Position: refs/heads/master@{#475035}

Former-commit-id: 162236743c74fa040e822ea4d846215e4543791e

media/filters/ffmpeg_audio_decoder.cc
@@ -227,10 +227,9 @@ bool FFmpegAudioDecoder::FFmpegDecode(
 
       bool is_sample_rate_change =
           av_frame_->sample_rate != config_.samples_per_second();
-      bool is_config_stale =
-          is_sample_rate_change ||
-          channels != ChannelLayoutToChannelCount(config_.channel_layout()) ||
-          av_frame_->format != av_sample_format_;
+      bool is_config_stale = is_sample_rate_change ||
+                             channels != config_.channels() ||
+                             av_frame_->format != av_sample_format_;
 
       // Only consider channel layout changes for AAC.
       // TODO(tguilbert, dalecurtis): Due to http://crbug.com/600538 we need to
@@ -250,8 +249,7 @@ bool FFmpegAudioDecoder::FFmpegDecode(
               << config_.samples_per_second()
               << ", ChannelLayout: " << channel_layout << " vs "
               << config_.channel_layout() << ", Channels: " << channels
-              << " vs "
-              << ChannelLayoutToChannelCount(config_.channel_layout());
+              << " vs " << config_.channels();
           config_.Initialize(config_.codec(), config_.sample_format(),
                              channel_layout, av_frame_->sample_rate,
                              config_.extra_data(), config_.encryption_scheme(),
@@ -264,7 +262,7 @@ bool FFmpegAudioDecoder::FFmpegDecode(
               << "Unsupported midstream configuration change!"
               << " Sample Rate: " << av_frame_->sample_rate << " vs "
               << config_.samples_per_second() << ", Channels: " << channels
-              << " vs " << ChannelLayoutToChannelCount(config_.channel_layout())
+              << " vs " << config_.channels()
               << ", Sample Format: " << av_frame_->format << " vs "
               << av_sample_format_;
           // This is an unrecoverable error, so bail out.
@@ -278,8 +276,7 @@ bool FFmpegAudioDecoder::FFmpegDecode(
       output = reinterpret_cast<AudioBuffer*>(
           av_buffer_get_opaque(av_frame_->buf[0]));
 
-      DCHECK_EQ(ChannelLayoutToChannelCount(config_.channel_layout()),
-                output->channel_count());
+      DCHECK_EQ(config_.channels(), output->channel_count());
       const int unread_frames = output->frame_count() - av_frame_->nb_samples;
       DCHECK_GE(unread_frames, 0);
       if (unread_frames > 0)
@@ -344,11 +341,9 @@ bool FFmpegAudioDecoder::ConfigureDecoder(const AudioDecoderConfig& config) {
   av_frame_.reset(av_frame_alloc());
   av_sample_format_ = codec_context_->sample_fmt;
 
-  if (codec_context_->channels !=
-      ChannelLayoutToChannelCount(config.channel_layout())) {
+  if (codec_context_->channels != config.channels()) {
     MEDIA_LOG(ERROR, media_log_)
-        << "Audio configuration specified "
-        << ChannelLayoutToChannelCount(config.channel_layout())
+        << "Audio configuration specified " << config.channels()
         << " channels, but FFmpeg thinks the file contains "
         << codec_context_->channels << " channels";
     ReleaseFFmpegResources();
@@ -417,9 +412,11 @@ int FFmpegAudioDecoder::GetAudioBuffer(struct AVCodecContext* s,
   DCHECK_GE(frames_required, frame->nb_samples);
 
   ChannelLayout channel_layout =
-      ChannelLayoutToChromeChannelLayout(s->channel_layout, s->channels);
+      config_.channel_layout() == CHANNEL_LAYOUT_DISCRETE
+          ? CHANNEL_LAYOUT_DISCRETE
+          : ChannelLayoutToChromeChannelLayout(s->channel_layout, s->channels);
 
-  if (channel_layout == CHANNEL_LAYOUT_UNSUPPORTED) {
+  if (config_.channel_layout() == CHANNEL_LAYOUT_UNSUPPORTED) {
     DLOG(ERROR) << "Unsupported channel layout.";
     return AVERROR(EINVAL);
   }

media/base/audio_decoder_config.cc
@@ -51,8 +51,10 @@ void AudioDecoderConfig::Initialize(AudioCodec codec,
   seek_preroll_ = seek_preroll;
   codec_delay_ = codec_delay;
 
-  int channels = ChannelLayoutToChannelCount(channel_layout_);
-  bytes_per_frame_ = channels * bytes_per_channel_;
+  // If |channel_layout_| is CHANNEL_LAYOUT_DISCRETE, |channels_| and
+  // |bytes_per_frame_| will be overwritten in SetChannelsForDiscrete()
+  channels_ = ChannelLayoutToChannelCount(channel_layout_);
+  bytes_per_frame_ = channels_ * bytes_per_channel_;
 }
 
 AudioDecoderConfig::~AudioDecoderConfig() {}
@@ -85,7 +87,7 @@ std::string AudioDecoderConfig::AsHumanReadableString() const {
   std::ostringstream s;
   s << "codec: " << GetCodecName(codec())
     << " bytes_per_channel: " << bytes_per_channel()
-    << " channel_layout: " << channel_layout()
+    << " channel_layout: " << channel_layout() << " channels: " << channels()
     << " samples_per_second: " << samples_per_second()
     << " sample_format: " << sample_format()
     << " bytes_per_frame: " << bytes_per_frame()
@@ -94,6 +96,13 @@ std::string AudioDecoderConfig::AsHumanReadableString() const {
     << (extra_data().empty() ? "false" : "true") << " encrypted? "
     << (is_encrypted() ? "true" : "false");
   return s.str();
+}
+
+void AudioDecoderConfig::SetChannelsForDiscrete(int channels) {
+  DCHECK(channel_layout_ == CHANNEL_LAYOUT_DISCRETE ||
+         channels == ChannelLayoutToChannelCount(channel_layout_));
+  channels_ = channels;
+  bytes_per_frame_ = channels_ * bytes_per_channel_;
 }
 
 void AudioDecoderConfig::SetIsEncrypted(bool is_encrypted) {

media/base/audio_decoder_config.h
@@ -63,10 +63,14 @@ class MEDIA_EXPORT AudioDecoderConfig {
   // output only.
   std::string AsHumanReadableString() const;
 
+  // Sets the number of channels if |channel_layout_| is CHANNEL_LAYOUT_DISCRETE
+  void SetChannelsForDiscrete(int channels);
+
   AudioCodec codec() const { return codec_; }
   int bits_per_channel() const { return bytes_per_channel_ * 8; }
   int bytes_per_channel() const { return bytes_per_channel_; }
   ChannelLayout channel_layout() const { return channel_layout_; }
+  int channels() const { return channels_; }
   int samples_per_second() const { return samples_per_second_; }
   SampleFormat sample_format() const { return sample_format_; }
   int bytes_per_frame() const { return bytes_per_frame_; }
@@ -96,6 +100,7 @@ class MEDIA_EXPORT AudioDecoderConfig {
   SampleFormat sample_format_;
   int bytes_per_channel_;
   ChannelLayout channel_layout_;
+  int channels_;
   int samples_per_second_;
   int bytes_per_frame_;
   std::vector<uint8_t> extra_data_;

media/base/fake_audio_renderer_sink.cc
@@ -24,7 +24,8 @@ FakeAudioRendererSink::FakeAudioRendererSink(
       callback_(nullptr),
       output_device_info_(std::string(),
                           OUTPUT_DEVICE_STATUS_OK,
-                          hardware_params) {}
+                          hardware_params),
+      is_optimized_for_hw_params_(true) {}
 
 FakeAudioRendererSink::~FakeAudioRendererSink() {
   DCHECK(!callback_);
@@ -70,7 +71,7 @@ OutputDeviceInfo FakeAudioRendererSink::GetOutputDeviceInfo() {
 }
 
 bool FakeAudioRendererSink::IsOptimizedForHardwareParameters() {
-  return true;
+  return is_optimized_for_hw_params_;
 }
 
 bool FakeAudioRendererSink::CurrentThreadIsRenderingThread() {
@@ -93,6 +94,10 @@ void FakeAudioRendererSink::OnRenderError() {
   DCHECK_NE(state_, kStopped);
 
   callback_->OnRenderError();
+}
+
+void FakeAudioRendererSink::SetIsOptimizedForHardwareParameters(bool value) {
+  is_optimized_for_hw_params_ = value;
 }
 
 void FakeAudioRendererSink::ChangeState(State new_state) {

media/base/fake_audio_renderer_sink.h
@@ -52,6 +52,9 @@ class FakeAudioRendererSink : public AudioRendererSink {
   bool Render(AudioBus* dest, base::TimeDelta delay, int* frames_written);
   void OnRenderError();
 
+  // Enables different tests to have different settings.
+  void SetIsOptimizedForHardwareParameters(bool value);
+
   State state() const { return state_; }
 
  protected:
@@ -63,6 +66,7 @@ class FakeAudioRendererSink : public AudioRendererSink {
   State state_;
   RenderCallback* callback_;
   OutputDeviceInfo output_device_info_;
+  bool is_optimized_for_hw_params_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeAudioRendererSink);
 };

media/ffmpeg/ffmpeg_common.cc
@@ -324,8 +324,22 @@ bool AVCodecContextToAudioDecoderConfig(
   SampleFormat sample_format = AVSampleFormatToSampleFormat(
       codec_context->sample_fmt, codec_context->codec_id);
 
-  ChannelLayout channel_layout = ChannelLayoutToChromeChannelLayout(
-      codec_context->channel_layout, codec_context->channels);
+  // Opus packets coded with channel mapping 2 are Ambisonics signals. If there
+  // is no WebAudio renderer attached, default up/downmixing is applied for
+  // <= 8 channels to enable previewing of content. Currently, mixing is not
+  // supported for > 8 channels.
+  // TODO (flim): Use mixing matrices that are more optimal for previewing
+  // Ambisonics content, and support > 8 channels.
+  bool is_opus_discrete = false;
+  if (codec == kCodecOpus && codec_context->extradata_size >= 19) {
+    int mapping_family = codec_context->extradata[18];
+    is_opus_discrete = mapping_family == 2;
+  }
+  ChannelLayout channel_layout =
+      is_opus_discrete && codec_context->channels > 8
+          ? CHANNEL_LAYOUT_DISCRETE
+          : ChannelLayoutToChromeChannelLayout(codec_context->channel_layout,
+                                               codec_context->channels);
 
   int sample_rate = codec_context->sample_rate;
   switch (codec) {
@@ -371,6 +385,8 @@ bool AVCodecContextToAudioDecoderConfig(
   config->Initialize(codec, sample_format, channel_layout, sample_rate,
                      extra_data, encryption_scheme, seek_preroll,
                      codec_context->delay);
+  if (channel_layout == CHANNEL_LAYOUT_DISCRETE)
+    config->SetChannelsForDiscrete(codec_context->channels);
 
 #if BUILDFLAG(ENABLE_AC3_EAC3_AUDIO_DEMUXING)
   // These are bitstream formats unknown to ffmpeg, so they don't have
@@ -419,8 +435,7 @@ void AudioDecoderConfigToAVCodecContext(const AudioDecoderConfig& config,
 
   // TODO(scherkus): should we set |channel_layout|? I'm not sure if FFmpeg uses
   // said information to decode.
-  codec_context->channels =
-      ChannelLayoutToChannelCount(config.channel_layout());
+  codec_context->channels = config.channels();
   codec_context->sample_rate = config.samples_per_second();
 
   if (config.extra_data().empty()) {

media/ffmpeg/ffmpeg_common_unittest.cc
@@ -125,6 +125,52 @@ TEST_F(FFmpegCommonTest, AVStreamToDecoderConfig) {
   ASSERT_TRUE(found_video);
 }
 
+TEST_F(FFmpegCommonTest, AVStreamToAudioDecoderConfig_OpusAmbisonics_4ch) {
+  base::MemoryMappedFile file;
+  file.Initialize(
+      GetTestDataFilePath("bear-opus-end-trimming-4ch-channelmapping2.webm"));
+  InMemoryUrlProtocol protocol(file.data(), file.length(), false);
+  FFmpegGlue glue(&protocol);
+  ASSERT_TRUE(glue.OpenContext());
+
+  AVFormatContext* format_context = glue.format_context();
+  EXPECT_EQ(static_cast<unsigned int>(1), format_context->nb_streams);
+  AVStream* stream = format_context->streams[0];
+
+  AVCodecParameters* codec_parameters = stream->codecpar;
+  EXPECT_EQ(AVMEDIA_TYPE_AUDIO, codec_parameters->codec_type);
+
+  AudioDecoderConfig audio_config;
+  ASSERT_TRUE(AVStreamToAudioDecoderConfig(stream, &audio_config));
+
+  EXPECT_EQ(kCodecOpus, audio_config.codec());
+  EXPECT_EQ(CHANNEL_LAYOUT_QUAD, audio_config.channel_layout());
+  EXPECT_EQ(4, audio_config.channels());
+}
+
+TEST_F(FFmpegCommonTest, AVStreamToAudioDecoderConfig_OpusAmbisonics_11ch) {
+  base::MemoryMappedFile file;
+  file.Initialize(
+      GetTestDataFilePath("bear-opus-end-trimming-11ch-channelmapping2.webm"));
+  InMemoryUrlProtocol protocol(file.data(), file.length(), false);
+  FFmpegGlue glue(&protocol);
+  ASSERT_TRUE(glue.OpenContext());
+
+  AVFormatContext* format_context = glue.format_context();
+  EXPECT_EQ(static_cast<unsigned int>(1), format_context->nb_streams);
+  AVStream* stream = format_context->streams[0];
+
+  AVCodecParameters* codec_parameters = stream->codecpar;
+  EXPECT_EQ(AVMEDIA_TYPE_AUDIO, codec_parameters->codec_type);
+
+  AudioDecoderConfig audio_config;
+  ASSERT_TRUE(AVStreamToAudioDecoderConfig(stream, &audio_config));
+
+  EXPECT_EQ(kCodecOpus, audio_config.codec());
+  EXPECT_EQ(CHANNEL_LAYOUT_DISCRETE, audio_config.channel_layout());
+  EXPECT_EQ(11, audio_config.channels());
+}
+
 TEST_F(FFmpegCommonTest, TimeBaseConversions) {
   const int64_t test_data[][5] = {
       {1, 2, 1, 500000, 1}, {1, 3, 1, 333333, 1}, {1, 3, 2, 666667, 2},

media/formats/webm/webm_audio_client.cc
@@ -50,7 +50,14 @@ bool WebMAudioClient::InitializeConfig(
   if (channels_ == -1)
     channels_ = 1;
 
-  ChannelLayout channel_layout =  GuessChannelLayout(channels_);
+  bool is_opus_discrete = false;
+  if (audio_codec == kCodecOpus && codec_private.size() >= 19) {
+    int mapping_family = codec_private[18];
+    is_opus_discrete = mapping_family == 2;
+  }
+  ChannelLayout channel_layout = is_opus_discrete && channels_ > 8
+                                     ? CHANNEL_LAYOUT_DISCRETE
+                                     : GuessChannelLayout(channels_);
 
   if (channel_layout == CHANNEL_LAYOUT_UNSUPPORTED) {
     MEDIA_LOG(ERROR, media_log_) << "Unsupported channel count " << channels_;
@@ -82,6 +89,7 @@ bool WebMAudioClient::InitializeConfig(
                      base::TimeDelta::FromMicroseconds(
                          (seek_preroll != -1 ? seek_preroll : 0) / 1000),
                      codec_delay_in_frames);
+  config->SetChannelsForDiscrete(channels_);
   return config->IsValidConfig();
 }
 

commit b268b29423b69c609bc238884efd3576e50dcd8a

Actually check if the channel layout is unsupported.
The conditional in FFmpegAudioDecoder checking for unsupported
channel layouts was not using the right variable. It used the
one from the config rather than the one for the frame (which
is required for config change support).

I think there's no real harm in this besides a DCHECK failure,
but might be worth merging back anyways. This regressed in
M62 with f4db6fa6f45e662a7383a5b8d5402eb287149e4c [formerly 162236743c74fa040e822ea4d846215e4543791e].

BUG=726971
TEST=none

Cq-Include-Trybots: master.tryserver.chromium.android:android_optional_gpu_tests_rel;master.tryserver.chromium.linux:linux_optional_gpu_tests_rel;master.tryserver.chromium.mac:mac_optional_gpu_tests_rel;master.tryserver.chromium.win:win_optional_gpu_tests_rel
Change-Id: Ibcdc5ceeb263c3e247742b4fbfe07a45e64297de
Reviewed-on: https://chromium-review.googlesource.com/686064
Commit-Queue: Dale Curtis <dalecurtis@chromium.org>
Reviewed-by: Thomas Guilbert <tguilbert@chromium.org>
Cr-Commit-Position: refs/heads/master@{#504770}
Former-commit-id: de6207dc6fd2b3cade49e973cbe85e9bc492304a

media/filters/ffmpeg_audio_decoder.cc
@@ -416,7 +416,7 @@ int FFmpegAudioDecoder::GetAudioBuffer(struct AVCodecContext* s,
           ? CHANNEL_LAYOUT_DISCRETE
           : ChannelLayoutToChromeChannelLayout(s->channel_layout, s->channels);
 
-  if (config_.channel_layout() == CHANNEL_LAYOUT_UNSUPPORTED) {
+  if (channel_layout == CHANNEL_LAYOUT_UNSUPPORTED) {
     DLOG(ERROR) << "Unsupported channel layout.";
     return AVERROR(EINVAL);
   }