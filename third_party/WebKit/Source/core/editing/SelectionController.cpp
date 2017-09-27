76693cdfebcc751a16e84f67de1c2f871d63ffec
Akari Asai <akariasai@google.com>
2017-09-27 11:07:08 +0000

    Add ConvertToSelctionInFlatTree() in SelectionTemplate.cpp.
    This patch will add a method called ConvertToSelectionInFlatTree() and
    ConvertToSelectionInDOMTree() in SelectionTemplate.{cpp, h} to support
    conversion between selection in a dom Tree and flat tree.
    This change is for making SelectionModifier works on flat tree.
    
    Bug: 513568
    Change-Id: I78fee984ba77d444bda2276b4099498d04792f17
    Reviewed-on: https://chromium-review.googlesource.com/684814
    Commit-Queue: Akari Asai <akariasai@google.com>
    Reviewed-by: Xiaocheng Hu <xiaochengh@chromium.org>
    Reviewed-by: Yoshifumi Inoue <yosin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504626}
    Former-commit-id: b69f62c9bdc40e7ce1bf59e117ced99a08e6cb9d

--- third_party/WebKit/Source/core/editing/SelectionController.cpp
+++ third_party/WebKit/Source/core/editing/SelectionController.cpp
@@ -78,16 +78,6 @@ DEFINE_TRACE(SelectionController) {
 
 namespace {
 
-SelectionInDOMTree ConvertToSelectionInDOMTree(
-    const SelectionInFlatTree& selection_in_flat_tree) {
-  return SelectionInDOMTree::Builder()
-      .SetAffinity(selection_in_flat_tree.Affinity())
-      .SetBaseAndExtent(ToPositionInDOMTree(selection_in_flat_tree.Base()),
-                        ToPositionInDOMTree(selection_in_flat_tree.Extent()))
-      .SetIsDirectional(selection_in_flat_tree.IsDirectional())
-      .Build();
-}
-
 DispatchEventResult DispatchSelectStart(Node* node) {
   if (!node || !node->GetLayoutObject())
     return DispatchEventResult::kNotCanceled;
--- third_party/WebKit/Source/core/editing/SelectionTemplate.cpp
+++ third_party/WebKit/Source/core/editing/SelectionTemplate.cpp
@@ -413,6 +413,26 @@ SelectionTemplate<
   selection_.ResetDirectionCache();
 }
 
+SelectionInDOMTree ConvertToSelectionInDOMTree(
+    const SelectionInFlatTree& selection_in_flat_tree) {
+  return SelectionInDOMTree::Builder()
+      .SetAffinity(selection_in_flat_tree.Affinity())
+      .SetBaseAndExtent(ToPositionInDOMTree(selection_in_flat_tree.Base()),
+                        ToPositionInDOMTree(selection_in_flat_tree.Extent()))
+      .SetIsDirectional(selection_in_flat_tree.IsDirectional())
+      .Build();
+}
+
+SelectionInFlatTree ConvertToSelectionInFlatTree(
+    const SelectionInDOMTree& selection) {
+  return SelectionInFlatTree::Builder()
+      .SetAffinity(selection.Affinity())
+      .SetBaseAndExtent(ToPositionInFlatTree(selection.Base()),
+                        ToPositionInFlatTree(selection.Extent()))
+      .SetIsDirectional(selection.IsDirectional())
+      .Build();
+}
+
 template <typename Strategy>
 DEFINE_TRACE(SelectionTemplate<Strategy>::InvalidSelectionResetter) {
   visitor->Trace(document_);
