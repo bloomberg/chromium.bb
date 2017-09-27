d7fd341f166932efa1edcf01ea3e9c7f80395d2e
stkhapugin@chromium.org <stkhapugin@chromium.org>
2017-09-27 15:25:55 +0000

    Rename PopupMaterialViewController, PopupMaterialRow (remove "material")
    Renames PopupMaterialViewController->PopupViewController.
    Renames PopupMaterialRow->PopupRow.
    
    Keeps ios/chrome/browser/ui/omnibox/omnibox_popup_material_row.h for
    compatibility with downstream includes. This will be removed in a
    follow-up CL.
    
    This is a reland of 660277.
    
    Change-Id: Id044d5875d82fda271bfbf328476f742f40f7b87
    Reviewed-on: https://chromium-review.googlesource.com/687016
    Reviewed-by: Sylvain Defresne <sdefresne@chromium.org>
    Commit-Queue: Stepan Khapugin <stkhapugin@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504683}
    Former-commit-id: 455e4b5c736194f620469bfe739c4adaf322c88b

--- ios/chrome/browser/ui/omnibox/omnibox_popup_material_row.h
+++ ios/chrome/browser/ui/omnibox/omnibox_popup_material_row.h
@@ -5,36 +5,8 @@
 #ifndef IOS_CHROME_BROWSER_UI_OMNIBOX_OMNIBOX_POPUP_MATERIAL_ROW_H_
 #define IOS_CHROME_BROWSER_UI_OMNIBOX_OMNIBOX_POPUP_MATERIAL_ROW_H_
 
-#import <UIKit/UIKit.h>
+#import "ios/chrome/browser/ui/omnibox/omnibox_popup_row.h"
 
-@class OmniboxPopupTruncatingLabel;
-
-// View used to display an omnibox autocomplete match in the omnibox popup.
-@interface OmniboxPopupMaterialRow : UITableViewCell
-
-// A truncate-by-fading version of the textLabel of a UITableViewCell.
-@property(nonatomic, readonly, strong)
-    OmniboxPopupTruncatingLabel* textTruncatingLabel;
-// A truncate-by-fading version of the detailTextLabel of a UITableViewCell.
-@property(nonatomic, readonly, strong)
-    OmniboxPopupTruncatingLabel* detailTruncatingLabel;
-// A standard UILabel for answers, which truncates with ellipses to support
-// multi-line text.
-@property(nonatomic, readonly, strong) UILabel* detailAnswerLabel;
-
-@property(nonatomic, readonly, strong) UIImageView* imageView;
-@property(nonatomic, readonly, strong) UIImageView* answerImageView;
-@property(nonatomic, readonly, strong) UIButton* appendButton;
-@property(nonatomic, assign) CGFloat rowHeight;
-
-// Initialize the row with the given incognito state. The colors and styling are
-// dependent on whether or not the row is displayed in incognito mode.
-- (instancetype)initWithIncognito:(BOOL)incognito;
-
-// Update the match type icon with the supplied image ID and adjust its position
-// based on the current size of the row.
-- (void)updateLeadingImage:(int)imageID;
-
-@end
+typedef OmniboxPopupRow OmniboxPopupMaterialRow;
 
 #endif  // IOS_CHROME_BROWSER_UI_OMNIBOX_OMNIBOX_POPUP_MATERIAL_ROW_H_
