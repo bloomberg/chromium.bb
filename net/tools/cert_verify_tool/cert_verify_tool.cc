d374401faf19e1669e8b1601035515b4f578b631
Matt Mueller <mattm@chromium.org>
2017-09-27 22:01:50 +0000

    Use base::TaskScheduler::CreateAndStartWithDefaultParams in cert_verify_tool.
    Fixes "Check failed: TaskScheduler::GetInstance()."
    
    Bug: 
    Change-Id: I975bdd95254818f9864cf3e059ad6926bb2985f9
    Reviewed-on: https://chromium-review.googlesource.com/686048
    Commit-Queue: Matt Mueller <mattm@chromium.org>
    Reviewed-by: Eric Roman <eroman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504779}
    Former-commit-id: e05b60951b1186957af5ac355894462ead41dc69

--- net/tools/cert_verify_tool/cert_verify_tool.cc
+++ net/tools/cert_verify_tool/cert_verify_tool.cc
@@ -5,9 +5,10 @@
 #include <iostream>
 
 #include "base/at_exit.h"
+#include "base/callback_helpers.h"
 #include "base/command_line.h"
 #include "base/logging.h"
-#include "base/message_loop/message_loop.h"
+#include "base/task_scheduler/task_scheduler.h"
 #include "base/time/time.h"
 #include "net/tools/cert_verify_tool/cert_verify_tool_util.h"
 #include "net/tools/cert_verify_tool/verify_using_cert_verify_proc.h"
@@ -69,8 +70,9 @@ void PrintUsage(const char* argv0) {
 
 int main(int argc, char** argv) {
   base::AtExitManager at_exit_manager;
-  // TODO(eroman): Is this needed?
-  base::MessageLoopForIO message_loop;
+  base::TaskScheduler::CreateAndStartWithDefaultParams("cert_verify_tool");
+  base::ScopedClosureRunner cleanup(
+      base::BindOnce([] { base::TaskScheduler::GetInstance()->Shutdown(); }));
   if (!base::CommandLine::Init(argc, argv)) {
     std::cerr << "ERROR in CommandLine::Init\n";
     return 1;
