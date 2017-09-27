925c585e6b3833b5ad81699f2ba4cc5bdfa0e3bc
tzik <tzik@chromium.org>
2017-09-27 14:51:44 +0000

    Add external deleter support to base::RefCounted
    The external deleter is needed when the destructor of |T| is unavailable
    for base::RefCounted, e.g. Blink's ref counted classes can be destroyed
    by WTF::RefCounted, but cannot by base::RefCounted.
    In such case, we need to inject the destruction as the traits.
    
    Bug: 763844
    Change-Id: Id7c74d4686915b62a2b843922c976d08d0eb5b13
    Reviewed-on: https://chromium-review.googlesource.com/681954
    Commit-Queue: Taiju Tsuiki <tzik@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504669}
    Former-commit-id: 2e8a148e6c387088004f188c1e5f66169d51a340

--- base/memory/ref_counted.h
+++ base/memory/ref_counted.h
@@ -298,7 +298,17 @@ class BASE_EXPORT ScopedAllowCrossThreadRefCountAccess final {
   static constexpr ::base::subtle::StartRefCountFromOneTag \
       kRefCountPreference = ::base::subtle::kStartRefCountFromOneTag
 
-template <class T>
+template <class T, typename Traits>
+class RefCounted;
+
+template <typename T>
+struct DefaultRefCountedTraits {
+  static void Destruct(const T* x) {
+    RefCounted<T, DefaultRefCountedTraits>::DeleteInternal(x);
+  }
+};
+
+template <class T, typename Traits = DefaultRefCountedTraits<T>>
 class RefCounted : public subtle::RefCountedBase {
  public:
   static constexpr subtle::StartRefCountFromZeroTag kRefCountPreference =
@@ -317,7 +327,7 @@ class RefCounted : public subtle::RefCountedBase {
       // lifetime guarantees of the refcounting system.
       ANALYZER_SKIP_THIS_PATH();
 
-      delete static_cast<const T*>(this);
+      Traits::Destruct(static_cast<const T*>(this));
     }
   }
 
@@ -325,6 +335,9 @@ class RefCounted : public subtle::RefCountedBase {
   ~RefCounted() = default;
 
  private:
+  friend struct DefaultRefCountedTraits<T>;
+  static void DeleteInternal(const T* x) { delete x; }
+
   DISALLOW_COPY_AND_ASSIGN(RefCounted);
 };
 
