bcd0d00934b878dbcf0b6f6a40b62d5a00e95423
Chris Cunningham <chcunningham@chromium.org>
2017-09-26 17:23:55 -0700

    Updating FFmpeg build scripts/configs for M63 roll
    Upstream configure introduced a new flag: --disable-autodetect
    
    This flag is now used in build_ffmpeg.py in place of our one-off
    disabling of autodetected features.
    
    This makes the merge review a little simpler. Merge's should not show
    new CONFIG_* features enabled due to autodetect. Instead, the roller
    (me) should audit any new autodected features since the last roll and
    explicitly enable any that are desried.
    
    New autodetects for this roll were as follows. None are desired/enabled.
     ALSA support
     Apple AppKit framework
     Apple AVFoundation framework
     Apple CoreImage framework
     libjack support
     sndio support
    
    A diff of configure defines is here:
    http://chcunningham-linux.sea.corp.google.com/m63_ffmpeg_config_flag_changes.txt
    
    A few 1's became 0 due to disabled autodetect. None are used in
    production:
     CONFIG_AUDIOTOOLBOX
     CONFIG_LIBXCB*
    
    BUG=766878

--- chromium/scripts/build_ffmpeg.py
+++ chromium/scripts/build_ffmpeg.py
@@ -341,23 +341,17 @@ def main(argv):
       '--disable-lzo',
       '--disable-network',
       '--disable-schannel',
-      '--disable-sdl',
+      '--disable-sdl2',
       '--disable-symver',
       '--disable-xlib',
       '--disable-zlib',
       '--disable-securetransport',
 
-      # Disable hardware decoding options which will sometimes turn on
-      # via autodetect.
-      '--disable-d3d11va',
-      '--disable-dxva2',
-      '--disable-vaapi',
-      '--disable-vda',
-      '--disable-vdpau',
-      '--disable-videotoolbox',
-      '--disable-nvenc',
-      '--disable-cuda',
-      '--disable-cuvid',
+      # Disable automatically detected external libraries. This prevents
+      # automatic inclusion of things like hardware decoders. Each roll should
+      # audit new [autodetect] configure options and add any desired options to
+      # this file.
+      '--disable-autodetect',
 
       # Common codecs.
       '--enable-decoder=vorbis,libopus,flac',
@@ -368,7 +362,11 @@ def main(argv):
 
       # Setup include path so Chromium's libopus can be used.
       '--extra-cflags=-I' + os.path.join(CHROMIUM_ROOT_DIR,
-                                         'third_party/opus/src/include')
+                                         'third_party/opus/src/include'),
+
+      # Disable usage of Linux Performance API. Not used in production code, but
+      # missing system headers break some Android builds.
+      '--disable-linux-perf',
   ])
 
   if target_os == 'android':
