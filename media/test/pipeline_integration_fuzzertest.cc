248816ce092c817e42ea99aae2afbc4a29293704
Matt Wolenetz <wolenetz@chromium.org>
2017-09-28 22:02:53 +0000

    MSE: Fuzz using both old and new buffering APIs
    This change sequentially attempts MSE fuzzing for both enabled and
    disabled feature media::kMseBufferByPts for the same input.
    
    BUG=718641
    
    Change-Id: Id93677cde3c464b740465e4dfe9cfb202395813f
    Reviewed-on: https://chromium-review.googlesource.com/664092
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Reviewed-by: Max Moroz <mmoroz@chromium.org>
    Commit-Queue: Matthew Wolenetz <wolenetz@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505179}
    Former-commit-id: b61fbbdfbb07406e7e293771f09aace9494bf582

--- media/test/pipeline_integration_fuzzertest.cc
+++ media/test/pipeline_integration_fuzzertest.cc
@@ -218,8 +218,22 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     media::ProgressivePipelineIntegrationFuzzerTest test;
     test.RunTest(data, size);
   } else {
-    media::MediaSourcePipelineIntegrationFuzzerTest test;
-    test.RunTest(data, size, MseFuzzerVariantEnumToMimeTypeString(variant));
+    // Sequentially fuzz with new and old MSE buffering APIs.  See
+    // https://crbug.com/718641.
+    {
+      base::test::ScopedFeatureList features_with_buffering_api_forced;
+      features_with_buffering_api_forced.InitAndEnableFeature(
+          media::kMseBufferByPts);
+      media::MediaSourcePipelineIntegrationFuzzerTest test;
+      test.RunTest(data, size, MseFuzzerVariantEnumToMimeTypeString(variant));
+    }
+    {
+      base::test::ScopedFeatureList features_with_buffering_api_forced;
+      features_with_buffering_api_forced.InitAndDisableFeature(
+          media::kMseBufferByPts);
+      media::MediaSourcePipelineIntegrationFuzzerTest test;
+      test.RunTest(data, size, MseFuzzerVariantEnumToMimeTypeString(variant));
+    }
   }
 
   return 0;
