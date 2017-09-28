a9e2e63b94925b8187a9d1710351295cc98db5fd
Mohammad Refaat <mrefaat@chromium.org>
2017-09-28 15:31:26 +0000

    Convert SystemCookieStore to Async.
    * Converts SystemCookieStore to Async
    * Add Async functions to NSHTTPSystemCookieStore and keep the synchronous
    functions.
    * Move creation time management to systemCookieStore.
    * Update cookie_store_ios_presistent_unittest & 
    cookie_store_ios_unittests to account for SystemCookieStore changes 
    * (As CookieStoreIOS is using systemCookieStore internally).
    
    Another CL is in progress to add WKSystemCookieStore & unit_tests for 
    systemCookieStore (to handle async methods) with template for both 
    NSHTTP & WK systemCookieStore.
    
    Bug: 759229, 759227, 767948
    Change-Id: I1c24c82540f4781a0bec77ebfa4a861ca7433391
    Reviewed-on: https://chromium-review.googlesource.com/675804
    Commit-Queue: Mohammad Refaat <mrefaat@chromium.org>
    Reviewed-by: Eugene But <eugenebut@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505033}
    Former-commit-id: 8505818b354fa784d77723c2d8a7123d154770f5

--- ios/chrome/browser/net/chrome_cookie_store_ios_client.h
+++ ios/chrome/browser/net/chrome_cookie_store_ios_client.h
@@ -7,28 +7,18 @@
 
 #include "base/macros.h"
 #include "base/sequenced_task_runner.h"
-#include "base/threading/thread_checker.h"
 #include "ios/net/cookies/cookie_store_ios_client.h"
-
-@protocol BrowsingDataChangeListening;
 
 // Chrome implementation of net::CookieStoreIOSClient. This class lives on the
 // IOThread.
 class ChromeCookieStoreIOSClient : public net::CookieStoreIOSClient {
  public:
-  // Creates a CookieStoreIOSClient with a BrowsingDataChangeListening.
-  // |browsing_data_change_listener| cannot be nil.
-  explicit ChromeCookieStoreIOSClient(
-      id<BrowsingDataChangeListening> browsing_data_change_listener);
+  ChromeCookieStoreIOSClient();
 
   // CookieStoreIOSClient implementation.
-  void DidChangeCookieStorage() const override;
   scoped_refptr<base::SequencedTaskRunner> GetTaskRunner() const override;
 
  private:
-  base::ThreadChecker thread_checker_;
-  // The listener that is informed of change in browsing data.
-  id<BrowsingDataChangeListening> browsing_data_change_listener_;  // Weak.
   DISALLOW_COPY_AND_ASSIGN(ChromeCookieStoreIOSClient);
 };
 
--- ios/chrome/browser/net/chrome_cookie_store_ios_client.mm
+++ ios/chrome/browser/net/chrome_cookie_store_ios_client.mm
@@ -4,31 +4,15 @@
 
 #include "ios/chrome/browser/net/chrome_cookie_store_ios_client.h"
 
-#include "base/logging.h"
-#include "base/task_scheduler/post_task.h"
-#import "ios/chrome/browser/browsing_data/browsing_data_change_listening.h"
 #include "ios/web/public/web_thread.h"
 
 #if !defined(__has_feature) || !__has_feature(objc_arc)
 #error "This file requires ARC support."
 #endif
 
-ChromeCookieStoreIOSClient::ChromeCookieStoreIOSClient(
-    id<BrowsingDataChangeListening> browsing_data_change_listener)
-    : browsing_data_change_listener_(browsing_data_change_listener) {
-  DCHECK(browsing_data_change_listener);
-  DCHECK_CURRENTLY_ON(web::WebThread::IO);
-}
-
-void ChromeCookieStoreIOSClient::DidChangeCookieStorage() const {
-  DCHECK(thread_checker_.CalledOnValidThread());
-
-  [browsing_data_change_listener_ didChangeCookieStorage];
-}
+ChromeCookieStoreIOSClient::ChromeCookieStoreIOSClient() {}
 
 scoped_refptr<base::SequencedTaskRunner>
 ChromeCookieStoreIOSClient::GetTaskRunner() const {
-  DCHECK(thread_checker_.CalledOnValidThread());
-  return base::CreateSequencedTaskRunnerWithTraits(
-      {base::MayBlock(), base::TaskPriority::BACKGROUND});
+  return web::WebThread::GetTaskRunnerForThread(web::WebThread::IO);
 }
