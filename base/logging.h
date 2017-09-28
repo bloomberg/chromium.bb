460a890b634081a01e761327a74f3bbd38dc1087
Xida Chen <xidachen@chromium.org>
2017-09-28 16:48:41 +0000

    Revert "Reland "Make SECURITY_DCHECK play with Albatross.""
    This reverts commit 9b62aba12252f8cc75f43cec445a3eec122d983d [formerly e9d46063e6c95c205b8fa45c1bd1d50507d20fc8].
    
    Reason for revert: <INSERT REASONING HERE>
    Causing test failure:
    https://build.chromium.org/p/chromium.win/builders/Win7%20%2832%29%20Tests/builds/24998
    
    Original change's description:
    > Reland "Make SECURITY_DCHECK play with Albatross."
    > 
    > This is a reland of 49904d5de900e2d19f1481f5f4f9725a24569588 [formerly 06d64c006b98629d50bf81e0d1e84f379405025c]
    > Original change's description:
    > > Make SECURITY_DCHECK play with Albatross.
    > > 
    > > In Albatross builds, DCHECKs can be compiled in, but non-fatal. See
    > > https://docs.google.com/a/chromium.org/document/d/1QY4IbbJ8X6G-6-cMheEkP_mT7ZNPCuUJIW2sr_mEiH4
    > > 
    > > Bug: 596231
    > > Change-Id: I84198f1d3f4122ac7a5bd8a90ed5ea96286b9022
    > > Reviewed-on: https://chromium-review.googlesource.com/685498
    > > Reviewed-by: Wez <wez@chromium.org>
    > > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > > Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    > > Cr-Commit-Position: refs/heads/master@{#504638}
    > 
    > Bug: 596231
    > Change-Id: I2c1223ba264dbdb06e814b92b84adfe9c3c619b7
    > Reviewed-on: https://chromium-review.googlesource.com/687874
    > Reviewed-by: Gabriel Charette <gab@chromium.org>
    > Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    > Commit-Queue: Sigurður Ásgeirsson <siggi@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#505025}
    
    TBR=wez@chromium.org,gab@chromium.org,jochen@chromium.org,siggi@chromium.org
    
    Change-Id: I121cee3d06ce27dcf04bec7a0074de658f6d3426
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 596231
    Reviewed-on: https://chromium-review.googlesource.com/690220
    Reviewed-by: Xida Chen <xidachen@chromium.org>
    Commit-Queue: Xida Chen <xidachen@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505060}
    Former-commit-id: e3a0864f7f4904b3a5032ddb603acb3f06b0d49c

--- base/logging.h
+++ base/logging.h
@@ -823,9 +823,8 @@ const LogSeverity LOG_DCHECK = LOG_FATAL;
 
 #else  // DCHECK_IS_ON()
 
-// There may be users of LOG_DCHECK that are enabled independently
-// of DCHECK_IS_ON(), so default to FATAL logging for those.
-const LogSeverity LOG_DCHECK = LOG_FATAL;
+// This is a dummy value, since the DCHECK implementation is a no-op.
+const LogSeverity LOG_DCHECK = LOG_INFO;
 
 #endif  // DCHECK_IS_ON()
 
