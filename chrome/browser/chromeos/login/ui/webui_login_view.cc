08df360e1baf3be34ec9befaad002752fa84efa6
Yue Li <updowndota@chromium.org>
2017-09-28 01:20:14 +0000

    Fix Tab key focus for voice interaciont OOBE
    Prevent the focus to be moved into the system tray, so that user could
    use tab key to change focus between buttons.
    
    BUG=b/64535533
    
    Change-Id: Id9bbf89c06fe970db011bb42ee684367bb96e6aa
    Reviewed-on: https://chromium-review.googlesource.com/686062
    Commit-Queue: Yue Li <updowndota@chromium.org>
    Reviewed-by: Xiyuan Xia <xiyuan@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504857}
    Former-commit-id: e0c29fc72930dddd0055ce2a696520ff2c3bb8b4

--- chrome/browser/chromeos/login/ui/webui_login_view.cc
+++ chrome/browser/chromeos/login/ui/webui_login_view.cc
@@ -46,6 +46,7 @@
 #include "chromeos/network/network_state_handler.h"
 #include "components/content_settings/core/common/content_settings_pattern.h"
 #include "components/password_manager/core/browser/password_manager.h"
+#include "components/session_manager/core/session_manager.h"
 #include "components/web_modal/web_contents_modal_dialog_manager.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_frame_host.h"
@@ -602,6 +603,13 @@ bool WebUILoginView::MoveFocusToSystemTray(bool reverse) {
   // exit early.
   if (ash_util::IsRunningInMash())
     return true;
+
+  // The focus should not move to the system tray if voice interaction OOOBE is
+  // active.
+  if (LoginDisplayHost::default_host() &&
+      LoginDisplayHost::default_host()->IsVoiceInteractionOobe()) {
+    return false;
+  }
 
   // If shift+tab is used (|reverse| is true) and views-based shelf is shown,
   // focus goes to the shelf widget. If views-based shelf is disabled, focus
