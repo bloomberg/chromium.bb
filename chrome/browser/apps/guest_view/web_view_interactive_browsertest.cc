149936e9ea7e3e50f19238348c31d5469d7d9a7f
Lukasz Anforowicz <lukasza@chromium.org>
2017-09-28 13:01:16 +0000

    Removing callers of the WebContents::GetRenderProcessHost() method.
    WebContents::GetRenderProcessHost is an API that doesn't behave as
    expected with out-of-process iframes, as multiple processes can be
    associated with a single WebContents.  This CL replaces callers of this
    API, so that they go through an equivalent API that explicitly selects
    which frame's process is needed.
    
    This CL was started by using an ad-hoc clang-plugin that replaced
    existing callsites of WebContents::GetRenderProcessHost() with a call to
    something like wc->GetMainFrame()->GetProcess().  This was followed-up
    by manually adding an include of render_frame_host.h and git cl format
    and git cl lint and then a self-review with small tweaks (e.g. sometimes
    using a wc->GetRenderViewHost()->GetProcess() is more appropriate).
    
    This CL was uploaded by git cl split.
    
    R=lazyboy@chromium.org
    
    Bug: 666525
    Change-Id: I0df13be0c92b4e478604274d97386ec57960c332
    Reviewed-on: https://chromium-review.googlesource.com/689111
    Reviewed-by: Istiaque Ahmed <lazyboy@chromium.org>
    Commit-Queue: Łukasz Anforowicz <lukasza@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#504989}
    Former-commit-id: dff8c0e0e2a3f11c8cb3fda814422eae76c09329

--- chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
+++ chrome/browser/apps/guest_view/web_view_interactive_browsertest.cc
@@ -309,8 +309,10 @@ class WebViewInteractiveTestBase : public extensions::PlatformAppBrowserTest {
     guest_observer.Wait();
     content::Source<content::NavigationController> source =
         guest_observer.source();
-    EXPECT_TRUE(source->GetWebContents()->GetRenderProcessHost()->
-        IsForGuestsOnly());
+    EXPECT_TRUE(source->GetWebContents()
+                    ->GetMainFrame()
+                    ->GetProcess()
+                    ->IsForGuestsOnly());
 
     guest_web_contents_ = source->GetWebContents();
     embedder_web_contents_ =
@@ -1545,7 +1547,7 @@ IN_PROC_BROWSER_TEST_P(WebViewInteractiveTest, TextSelection) {
       guest_web_contents()->GetRenderWidgetHostView();
   ASSERT_TRUE(guest_rwhv);
   std::string selected_text = base::UTF16ToUTF8(guest_rwhv->GetSelectedText());
-  ASSERT_TRUE(selected_text.size() >= 10u);
+  ASSERT_GE(selected_text.size(), 10u);
   ASSERT_EQ("AAAAAAAAAA", selected_text.substr(0, 10));
 }
 
@@ -1564,7 +1566,7 @@ IN_PROC_BROWSER_TEST_P(WebViewInteractiveTest, WordLookup) {
 
   auto guest_message_filter =
       browser_client.GetTextInputClientMessageFilterForProcess(
-          guest_web_contents()->GetRenderProcessHost());
+          guest_web_contents()->GetMainFrame()->GetProcess());
   ASSERT_TRUE(guest_message_filter);
 
   // Lookup some string through context menu.
