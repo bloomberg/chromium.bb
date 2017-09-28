9b3e037cc084be6bff5ae94aa9b1ce229519432a
Marc-Antoine Courteau <macourteau@chromium.org>
2017-09-28 17:09:35 +0000

    [Payment Request] State field: allow selecting 1st state.
    If the state field is unpopulated, inserts a blank element at the
    beginning of the list of states, as the first element is selected by
    default when the UIPickerView is shown.
    
    R=mahmadi
    
    Bug: 767972
    Change-Id: Iaa0c4bc29b863d8c521f3e9f0b826eb7ccef408d
    Reviewed-on: https://chromium-review.googlesource.com/689857
    Reviewed-by: Moe Ahmadi <mahmadi@chromium.org>
    Commit-Queue: Marc-Antoine Courteau <macourteau@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505070}
    Former-commit-id: e4b825b4054e3daf7da61f8bae34035b82f347b9

--- ios/chrome/browser/ui/payments/address_edit_mediator.mm
+++ ios/chrome/browser/ui/payments/address_edit_mediator.mm
@@ -175,7 +175,15 @@
   // Notify the view controller asynchronously to allow for the view to update.
   __weak AddressEditMediator* weakSelf = self;
   dispatch_async(dispatch_get_main_queue(), ^{
-    [weakSelf.consumer setOptions:@[ [regions allKeys] ]
+    NSMutableArray<NSString*>* values =
+        [[NSMutableArray alloc] initWithArray:[regions allKeys]];
+    // If the field contains no value, insert an empty value at the beginning
+    // of the list of options, as the first option is selected when the UI
+    // opens. Otherwise, it would be impossible for the user to select the first
+    // option without selecting another option first.
+    if (!weakSelf.regionField.value)
+      [values insertObject:@"" atIndex:0];
+    [weakSelf.consumer setOptions:@[ values ]
                    forEditorField:weakSelf.regionField];
   });
 }
