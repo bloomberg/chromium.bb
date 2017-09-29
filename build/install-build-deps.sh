70cbb8b9b7b3b13b3e7e39f5c67fb50b2e18e772
Dmytro Golovchenko <dgolovchenko@google.com>
2017-09-29 16:40:27 +0000

    Revert "Add libdconf-dev to to install-build-deps.sh"
    Script is failing on goobuntu-trusty_desktop-stable-amd64 due to packages conflict:
    The following packages have unmet dependencies:
     libdconf-dev : Conflicts: libdconf-dev:i386 but 0.20.0-1 is to be installed
     libdconf-dev:i386 : Depends: libglib2.0-dev:i386 (>= 2.25.7) but it is not going to be installed
                         Recommends: libdconf-doc:i386 but it is not installable
                         Conflicts: libdconf-dev but 0.20.0-1 is to be installed
    
    This reverts commit 6de3851e58c01f375dd428b2880d24f04ac08da0 [formerly 5b267c0c4ffb22f07bea43853567c0b8a2ab9c21].
    
    Reason for revert: <INSERT REASONING HERE>
    
    Original change's description:
    > Add libdconf-dev to to install-build-deps.sh
    > 
    > This is the first step in moving from using gconf to dconf.
    > 
    > Bug: 768027
    > Change-Id: Ifb05a6679c38e115b322e0a7caa6c3aad309389e
    > Reviewed-on: https://chromium-review.googlesource.com/691097
    > Commit-Queue: Tim Brown <timbrown@chromium.org>
    > Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    > Reviewed-by: Thomas Anderson <thomasanderson@chromium.org>
    > Cr-Commit-Position: refs/heads/master@{#505183}
    
    TBR=dpranke@chromium.org,thomasanderson@chromium.org,timbrown@chromium.org
    
    Change-Id: Icec37c78711a2016667ee7f134d5db49b918b7d2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 768027
    Reviewed-on: https://chromium-review.googlesource.com/692974
    Commit-Queue: Dirk Pranke <dpranke@chromium.org>
    Reviewed-by: Dirk Pranke <dpranke@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#505391}
    Former-commit-id: 3cd469287f3ce7290d3bfe3f1971aa529b6b1303

--- build/install-build-deps.sh
+++ build/install-build-deps.sh
@@ -165,7 +165,6 @@ dev_list="\
   libcap-dev
   libcups2-dev
   libcurl4-gnutls-dev
-  libdconf-dev
   libdrm-dev
   libelf-dev
   libffi-dev
@@ -233,7 +232,6 @@ lib_list="\
   libcairo2
   libcap2
   libcups2
-  libdconf1
   libexpat1
   libffi6
   libfontconfig1
@@ -379,7 +377,6 @@ nacl_list="\
   libasound2:i386
   libcap2:i386
   libelf-dev:i386
-  libdconf-dev:i386
   libfontconfig1:i386
   libgconf-2-4:i386
   libglib2.0-0:i386
